32b1e16cc5e29971298a4318f2c3abe115049bc3
junOTL <naqp99@gmail.com>
2023-11-02 23:40:02 +0900

    feat: 빈값을 입력한 경우 예외 발생 기능

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -1,4 +1,5 @@
 package lotto.util;
 
 public class ExceptionMessage {
+    public static final String INPUT_VALUE = "[ERROR] 값을 입력해 주세요.";
 }
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,4 +1,9 @@
 package lotto.util;
 
 public class Validator {
+    public static void isEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_VALUE);
+        }
+    }
 }

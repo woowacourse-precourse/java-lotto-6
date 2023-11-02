7bd31b0d97e7547a41d8e1bfb45d137a99399b5c
yuseonjun <seonjun0906@gmail.com>
2023-11-02 17:53:51 +0900

    feat(BuyerLotto): 구매자의 로또 번호를 저장 할 BuyerLotto 클래스 생성

---
+++ src/main/java/lotto/domain/BuyerLotto.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class BuyerLotto {
+
+    private final List<Lotto> buyerLotto;
+
+    public BuyerLotto(List<Lotto> buyerLotto) {
+        this.buyerLotto = buyerLotto;
+    }
+}

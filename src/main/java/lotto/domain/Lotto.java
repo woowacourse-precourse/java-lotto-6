15861bddd2db864f1e07df727ab3c664518830fb
BullChallenger <am23am45@naver.com>
2023-11-02 21:34:13 +0900

    [FEAT & REFACTOR] Lotto 클래스를 domain 패키지로 이동 및 번호 중복에 대한 유효성 검사 추가
    Lotto 클래스의 위치를 domain 패키지 내부로 이전하였다
    
    중복 값이 추가되어 있는지 확인하는 유효성 검증 로직을 작성하였다
    
    기존에 작성되어 있던 validate 메소드의 이름을 validateLottoSize로 변경하여 책임을 명시하였다
    
    validateLottoSize 내부에 하드코딩 되어있던 로또 크기인 6을 상수로 변경하였다
    => 2주차 피드백 반영

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateDuplicateNumber(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 입니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> nonDuplicatedNumbers = numbers.stream().collect(Collectors.toSet());
+        if (nonDuplicatedNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되어 생성되었습니다.");
+        }
+    }
+
+}

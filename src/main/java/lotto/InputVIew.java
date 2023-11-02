803618a5f0190a1dbb289c695f5405fe6f9fc620
Hyeji <hyejijjang10@gmail.com>
2023-11-03 00:26:22 +0900

    feat : define Input View

--- src/main/java/lotto/InputVIew.java
+++ src/main/java/lotto/InputVIew.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputVIew {
     private static final String INPUT_TICKETS_AMOUNT = "구입금액을 입력해 주세요.";
@@ -11,4 +13,22 @@ public class InputVIew {
         System.out.println(INPUT_TICKETS_AMOUNT);
         return Console.readLine();
     }
+    public static List<Integer> inputWinningNumbers(){
+        System.out.println(INPUT_WINNING_NUMBERS);
+        String input = Console.readLine();
+        return toWinningNumbers(input);
+    }
+    public static int inputBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER);
+        return Integer.parseInt(Console.readLine());
+    }
+    private static List<Integer> toWinningNumbers(String input){
+        String[] strWinningNums = input.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        for(String s : strWinningNums){
+            winningNumbers.add(Integer.parseInt(s.trim()));
+        }
+        return winningNumbers;
+    }
+
 }

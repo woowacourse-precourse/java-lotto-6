c83bc85efba393aaec9804cd56c9a3cc9c69b412
jeongjihun <wlgns2234@naver.com>
2023-11-02 23:58:15 +0900

    feat: Lotto 생성 기능 구현
    - List<Integer>를 통해 생성한다.
    - 생성 시 6자리가 아니라면 예외를 발생시킨다.
    - 생성 시 중복되는 숫자가 있다면 예외를 발생시킨다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicated(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 6자리여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicated(List<Integer> numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 중복될 수 없습니다.");
+        }
+    }
 }

91ae6331553cee16a7ae9ec9356c4f0e97d12385
BullChallenger <am23am45@naver.com>
2023-11-02 22:15:47 +0900

    [REFACTOR] Lotto 클래스 내부에 중복과 숫자 범위와 관련된 유효성 검증 로직 추가
    Lotto 클래스에 대한 테스트 코드를 작성하려고 했는데 해당 테스트 코드 내부에 미리 작성되어 있던 주석을 보고 다시 복구
    
    사실 생각해보면 LottoGenerator에서 미리 검증을 했다고는 해도 생성자를 통해 배열 값이 할당되기 때문에 유효성 검증은 필요한 부분인 것 같다

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
@@ -10,6 +12,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
+        validateDuplicateNumber(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
     }
 
@@ -20,6 +24,21 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> nonDuplicatedNumbers = numbers.stream().collect(Collectors.toSet());
+        if (nonDuplicatedNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되어 생성되었습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        numbers.stream().forEach(number -> {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        });
+    }
+
     @Override
     public String toString() {
         return numbers.toString();

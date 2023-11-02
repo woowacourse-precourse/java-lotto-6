f2d50d5809d3ee8f27144a04bbbe087253c7a397
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-03 02:51:11 +0900

    feat: 상수 추가

---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+public class Constants {
+    public static final String NUMBER_REGEXP = "^[0-9]$*";
+    public static final String INITIAL_VALUE = "0";
+    public static final String BLANK = " ";
+    public static final String COMMA = ",";
+    public static final int LOTTO_MAX_SIZE = 6;
+    public static final int REMAINDER = 0;
+    public static final int DIVISION_VALUE = 1000;
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 45;
+}

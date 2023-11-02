65042c4404329f52aa5b54207d70b99bda4e1fc3
UDADDY <gustmd5715@gmail.com>
2023-11-02 19:25:09 +0900

    feat: 별개의 1 ~ 45 무작위 값 6개를 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,18 +1,25 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 import lotto.domain.Validator;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
+    public static final String SPLIT_DELIMETER = ",";
+
     public static int inputCash(){
         int input = Integer.parseInt(Console.readLine());
         Validator.validateCash(input);
         return input;
     }
 
-    public static List<Integer> inputLotto(){
-        return null;
+    public static Lotto inputLotto(){
+        String input = Console.readLine();
+        String[] splitedInput = input.split(SPLIT_DELIMETER);
+        Lotto lotto = new Lotto(Arrays.stream(splitedInput).map(Integer::valueOf).toList());
+        return lotto;
     }
 }

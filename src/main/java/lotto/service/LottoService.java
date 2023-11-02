2e41254753c1aa39b24d738f667cb6244d26fa40
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 05:12:16 +0900

    feat(service): 입력받은 수량만큼 로또를 생성하여 사용자의 로또 리스트에 저장한다.
    - 1부터 45까지의 숫자 중 중복되지 않는 6개의 숫자를 무작위로 선택하여 정렬한 뒤 로또를 생성하여 저장합니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,23 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class LottoService {
+
+    private final List<Lotto> userLottos = new ArrayList<>();
+
+    public List<Lotto> purchase (int purchaseQuantity) {
+        for (int i = 0; i < purchaseQuantity; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.sort(Comparator.naturalOrder());
+            Lotto lotto = new Lotto(lottoNumbers);
+            userLottos.add(lotto);
+        }
+        return userLottos;
+    }
 }

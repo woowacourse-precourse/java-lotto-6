cc9c90c1fa786667b27133c3b517d40ab30d4528
yechop <channetattoo@gmail.com>
2023-11-03 03:02:52 +0900

    refactor(LottoController): Input과 Output을 생성자 주입으로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,14 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.io.Input;
+import lotto.io.Output;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
+        Input input = new Input();
+        Output output = new Output();
+        LottoController lottoController = new LottoController(input, output);
         lottoController.start();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,17 @@
 package lotto.controller;
 
+import lotto.io.Input;
+import lotto.io.Output;
+
 public class LottoController {
+
+    final Input input;
+    final Output output;
+
+    public LottoController(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
 
     public void start() {
     }

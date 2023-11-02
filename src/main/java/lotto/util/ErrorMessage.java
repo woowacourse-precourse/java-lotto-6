f1dba095f8d7d6ce3a74b68f23256d3ed2e0ba4b
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 17:32:08 +0900

    feat(ErrorMessage): 보너스 번호 예외처리 메세지 추가

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
 	
 	LOTTO_SIZE_ERROR("로또의 사이즈는 %s이여야 합니다."),
 	LOTTO_NUMBER_DUPLICATE_ERROR("로또 번호는 중복되지 말아야 합니다."),
+	LOTTO_BONUS_NUMBER_DUPLICATE_ERROR("당첨 번호와 보너스 번호는 중복되지 말아야 합니다."),
 	LOTTO_NUMBER_RANGE_ERROR("로또 번호는 %s부터 %s 사이의 숫자여야 합니다."),
 	PREFIX("[ERROR] ");
 

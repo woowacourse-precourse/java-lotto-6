94f4563ed36709734e1b19ce5fac7c7b8c611563
IMWoo94 <leemeawoo94@gmail.com>
2023-11-03 03:16:29 +0900

    feat : 당첨 로또 번호 전달 DTO 클래스 구현 및 변환 테스트
    당첨 로또 번호 전달 DTO 클래스 구현 및 변환 테스트

---
+++ src/main/java/lotto/dto/WinLottoRequestDto.java
@@ -0,0 +1,18 @@
+package lotto.dto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public record WinLottoRequestDto(String lottoNumber) {
+
+    public List<Integer> lottoStringToList() {
+        List<Integer> lotto = new ArrayList<>();
+        lotto = Arrays
+                .stream(lottoNumber.split(","))
+                .map(num -> Integer.parseInt(num))
+                .toList();
+        
+        return lotto;
+    }
+}

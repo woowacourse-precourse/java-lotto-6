ee1312902748fbbc305cbbc0f7371b477c620d1f
heiler <threepebbles@naver.com>
2023-11-03 04:59:23 +0900

    Feat: Lotto 객체 생성 시 numbers 검증 기능 추가
    숫자가 6개인지 검증
    각 숫자가 1~45 사이 수인지 검증
    중복된 숫자가 없는지 검증

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,19 +1,14 @@
 package lotto.model;
 
 import java.util.List;
+import lotto.validator.Validator;
 
 public class Lotto {    // 인스턴스 변수 추가 ㄴㄴ, 패키지 변경은 가능
     private final List<Integer> numbers;    // 접근 제어자 변경 ㄴㄴ
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Validator.validateNumbers(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     // TODO: 추가 기능 구현

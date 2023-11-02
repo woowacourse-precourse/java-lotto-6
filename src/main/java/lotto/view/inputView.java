74e70163c13ba0b591f20d5d148d889638412099
Yang-soeun <soeun8636@naver.com>
2023-11-02 19:49:06 +0900

    [feat]: 당첨 번호 입력 메소드 구현

--- src/main/java/lotto/view/inputView.java
+++ src/main/java/lotto/view/inputView.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class inputView {
     private final String INPUT_BUYING_COST_MESSAGE = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public String inputBuyingCost(){
         System.out.println(INPUT_BUYING_COST_MESSAGE);
+
+        return Console.readLine();
+    }
+
+    public String inputWinningNumbers(){
+        System.out.println(INPUT_WINNING_NUMBERS);
 
         return Console.readLine();
     }

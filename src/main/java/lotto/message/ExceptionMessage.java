c09c033a68ca9b92198289907aa585b8b844a932
Jiyoung Choi <12201817@inha.edu>
2023-11-02 18:05:33 +0900

    feat(ExceptionMessage) : 구입 금액 예외 처리 구현

---
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -0,0 +1,28 @@
+package lotto.message;
+
+public enum ExceptionMessage {
+    ERROR_TAG("[ERROR] "),
+    INPUT_NOT_NUMBER_MESSAGE("숫자를 입력해야 합니다."),
+    INPUT_UNDER_ZERO_MESSAGE("0보다 큰 숫자를 입력해야 합니다."),
+    INPUT_NOT_MULTIPLE_OF_UNIT_MESSAGE("%d의 배수를 입력해야 합니다.");
+
+
+    private final String exceptionMessage;
+
+    ExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public String getMessage() {
+        return exceptionMessage;
+    }
+
+    public void throwException() {
+        throw new IllegalArgumentException(ERROR_TAG + exceptionMessage);
+    }
+
+    public void throwException(String value) {
+        throw new IllegalArgumentException(ERROR_TAG + String.format(exceptionMessage, value));
+    }
+}
+

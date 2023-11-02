3dadfe72665dec859165bc2588e6a5d798bc469b
IMWoo94 <leemeawoo94@gmail.com>
2023-11-03 02:04:21 +0900

    test : 요청 갯수에 따른 로또 번호 생성 테스트 수정
    요청 갯수에 따른 로또 번호 생성 테스트 수정

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
@@ -9,17 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoMachineTest {
 
-    @Test
-    @DisplayName("로또의 번호 생성 테스트")
-    void createLottoNumberRangeCheckTest() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        List<Integer> createLottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    @ParameterizedTest
+    @DisplayName("요청 갯수 만큼 로또 번호 생성 테스트")
+    @ValueSource(ints = {1, 5, 10, 23})
+    void createLottoNumberRangeCheckTest(int quantity) {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottos = lottoMachine.createLottos(quantity);
 
-        // new Lotto 생성 시 로또 범위 1 ~ 45 / 로또 번호 중복 / 로또 갯수 검증
-        Assertions.assertThatNoException().isThrownBy(() -> new Lotto(createLottoNumber));
+        Assertions.assertThat(lottos.size()).isEqualTo(quantity);
     }
 
     @ParameterizedTest

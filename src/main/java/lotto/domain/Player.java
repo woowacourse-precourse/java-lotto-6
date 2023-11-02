cc2259db33973aacf2fadea0c2e16cac22538c40
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-02 16:49:47 +0900

    feat: Player 구현

---
+++ src/main/java/lotto/domain/Player.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Player {
+
+    private static final int LOTTO_PRICE = 1000;
+    private static final int PERCENTAGE = 100;
+
+    private int purchaseAmount;
+    private List<Lotto> lottos;
+
+    public Player(int purchaseAmount) {
+        // 구입 금액 검증
+        this.purchaseAmount = purchaseAmount;
+        generateLottos();
+    }
+
+    public int getNumberOfPurchases() {
+        return purchaseAmount / LOTTO_PRICE;
+    }
+
+    private void generateLottos() {
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        int numberOfPurchases = getNumberOfPurchases();
+
+        for (int i = 0; i < numberOfPurchases; i++)
+            lottos.add(new Lotto(lottoNumberGenerator.generateLottoNumbers()));
+
+    }
+
+    public double calculateRateOfReturn(int totalPrizeMoney) {
+        return ((double) totalPrizeMoney / (double) purchaseAmount) * PERCENTAGE;
+    }
+}

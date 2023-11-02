4b9dfc195dd2715541d3bf8089bdf970fc78edf0
Arachneee <jhg2819@naver.com>
2023-11-02 17:42:48 +0900

    test: 로또 생성 출력 테스트
    LottoFactory에서 로또 생성하고 출력하는 테스트

---
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoFactoryTest {
+
+    @DisplayName("구입한 금액에 맞는 로또가 발행된다.")
+    @Test
+    void test() {
+        // given
+        int money = 5000;
+
+
+        // when
+        LottoFactory lottoFactory = LottoFactory.create(new RandomNumberGenerator(), money);
+
+        // then
+        assertThat(lottoFactory.getLottoNumbers()).hasSize(5);
+    }
+
+}

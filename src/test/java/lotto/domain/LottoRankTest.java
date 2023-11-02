ac5c3cd30de9c4bfd4c2143ed163aa134d09b003
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 00:34:17 +0900

    test(LottoRankTest): 쓰레기 제거 및 메소드명 수정

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -9,21 +9,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import lotto.service.CheckWinningLottoNumbersService;
-
 public class LottoRankTest {
 
 	@DisplayName("일치한 로또 번호 수와 보너스 번호 유뮤에 따른 로또 상금을 확인한다.")
-	@MethodSource("createCheckWinnerTestValue")
+	@MethodSource("createGetLottoRankTestParameter")
 	@ParameterizedTest
-	void test(int matchedNumCount, boolean isBonusNumber, int expect) {
-		CheckWinningLottoNumbersService service = new CheckWinningLottoNumbersService();
-
+	void checkGetLottoRankMethodReturnValue(int matchedNumCount, boolean isBonusNumber, int expect) {
 		int lottoPrice = LottoRank.getLottoRank(matchedNumCount, isBonusNumber).getPrice();
+		
 		assertEquals(lottoPrice, expect);
 	}
 
-	static Stream<Arguments> createCheckWinnerTestValue() {
+	static Stream<Arguments> createGetLottoRankTestParameter() {
 		return Stream.of(Arguments.of(2, false, 0), Arguments.of(3, false, 5000), Arguments.of(4, false, 50000),
 						Arguments.of(5, false, 1500000), Arguments.of(5, true, 30000000), Arguments.of(6, false, 2000000000));
 	}

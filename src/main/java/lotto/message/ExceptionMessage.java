6a0b6c995374b2bff6806cb5080faf5d43afb3cd
Jiyoung Choi <12201817@inha.edu>
2023-11-02 18:39:32 +0900

    feat(ExceptionMessage) : error tag 선언 위치 변경 & 함수 매개변수 타입 변경

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -1,12 +1,11 @@
 package lotto.message;
 
 public enum ExceptionMessage {
-    ERROR_TAG("[ERROR] "),
     INPUT_NOT_NUMBER_MESSAGE("숫자를 입력해야 합니다."),
     INPUT_UNDER_ZERO_MESSAGE("0보다 큰 숫자를 입력해야 합니다."),
     INPUT_NOT_MULTIPLE_OF_UNIT_MESSAGE("%d의 배수를 입력해야 합니다.");
 
-
+    private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
 
     ExceptionMessage(String exceptionMessage) {
@@ -17,12 +16,16 @@ public enum ExceptionMessage {
         return exceptionMessage;
     }
 
-    public void throwException() {
-        throw new IllegalArgumentException(ERROR_TAG.getMessage() + exceptionMessage);
+    public String getMessage(int formatValue) {
+        return String.format(exceptionMessage, formatValue);
     }
 
-    public void throwException(String value) {
-        throw new IllegalArgumentException(ERROR_TAG.getMessage() + String.format(exceptionMessage, value));
+    public void throwException() {
+        throw new IllegalArgumentException(ERROR_TAG + exceptionMessage);
+    }
+
+    public void throwException(int value) {
+        throw new IllegalArgumentException(ERROR_TAG + String.format(exceptionMessage, value));
     }
 }
 

4452fff320ae93fa2eed629ae0045587a9b34804
Nara Park <skfk6352@naver.com>
2023-11-03 05:41:57 +0900

    <feat>(InputValidator): 사용자 입력값이 null인지 검증하는 메서드 분리   입력값이 빈칸일 경우 IllegalArgumentException이 발생하지만 null일 경우 NullPointerException이 발생하기 때문에 메서드를 분리함

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -5,10 +5,18 @@ import lotto.constant.ExceptionConstant;
 public class InputValidator {
 
     public void validateUserInput(String userInput) {
+        if (isValidateNull(userInput)) {
+            throw new NullPointerException(ExceptionConstant.INPUT_IS_ESSENTIAL.getMessage());
+        }
         if (isValidateBlank(userInput)) {
             throw new IllegalArgumentException(ExceptionConstant.INPUT_IS_ESSENTIAL.getMessage());
         }
     }
+
+    private boolean isValidateNull(String userInput) {
+        return userInput == null;
+    }
+
     private boolean isValidateBlank(String userInput) {
         return userInput.isBlank();
     }

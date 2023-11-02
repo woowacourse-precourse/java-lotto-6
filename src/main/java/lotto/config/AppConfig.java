b0f979f082bbe96cab73fb1086b4c557e2c03f18
0Chord <seaweed.0chord@gmail.com>
2023-11-02 20:00:08 +0900

    feat : appConfig sortService lottoProduceService

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -2,7 +2,11 @@ package lotto.config;
 
 import lotto.adapter.IoAdapter;
 import lotto.adapter.impl.StandardIoAdapter;
+import lotto.service.LottoProduceService;
+import lotto.service.SortService;
 import lotto.service.ValidateService;
+import lotto.service.impl.LottoProduceServiceImpl;
+import lotto.service.impl.SortServiceImpl;
 import lotto.service.impl.ValidateServiceImpl;
 
 public class AppConfig {
@@ -13,5 +17,12 @@ public class AppConfig {
 
     public IoAdapter ioAdapter() {
         return new StandardIoAdapter();
+    }
+
+    public SortService sortService() {
+        return new SortServiceImpl();
+    }
+    public LottoProduceService lottoProduceService() {
+        return new LottoProduceServiceImpl(sortService(), ioAdapter());
     }
 }

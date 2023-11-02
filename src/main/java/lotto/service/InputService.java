bcebeeaa9fa55623b8ed2e2b638634285009a3a7
jongmin Lee <rjsqktod1@gmail.com>
2023-11-03 08:58:12 +0900

    feat(input): 발행 로또 개수 획득기능 추가
    구입 금액에 따른 발행 로또 개수 획득 메서드 추가
    
    Signed-off-by: jongmin Lee <rjsqktod1@gmail.com>

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -6,15 +6,20 @@ public class InputService {
     private int purchaseAmount;
 
     public int readPurchaseAMount() {
+        System.out.println("구입금액을 입력해 주세요.");
         this.purchaseAmount = Integer.parseInt(Console.readLine());
         validateMultipleOfThousand(purchaseAmount);
         return purchaseAmount;
     }
 
     public void validateMultipleOfThousand(int purchaseAmount) {
-        if (purchaseAmount % 1000 == 0) {
+        if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public int getLottoTicketCount() {
+        return readPurchaseAMount() / 1000;
     }
 
 }

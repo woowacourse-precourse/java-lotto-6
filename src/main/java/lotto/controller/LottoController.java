2895282e8218c443cf4838c12008ac5135f023a8
sinyebin <dpqls0625@naver.com>
2023-11-02 22:42:43 +0900

    feat: 구매금액 안내문구 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,13 @@
 package lotto.controller;
 
+import lotto.view.PrintMessage;
+
 public class LottoController {
     public void startLotto(){
+        setAmount();
+    }
 
+    public void setAmount(){
+        PrintMessage.printStart();
     }
 }
---
+++ src/main/java/lotto/view/PrintMessage.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class PrintMessage {
+    private static String START_MESSAGE="구입금액을 입력해 주세요.";
+
+    public static void printStart(){
+        System.out.println(START_MESSAGE);
+    }
+}

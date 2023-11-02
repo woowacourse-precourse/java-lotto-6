e5ceec2acd737a567ec394fd5da890b834025eba
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 18:46:28 +0900

    test(LottoNumberGenerator): DisplayName, 메소드명 수정

--- src/test/java/lotto/generator/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/generator/LottoNumberGeneratorTest.java
@@ -6,22 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.domain.Lotto;
 
 public class LottoNumberGeneratorTest {
-	
-	 @Test
-	    public void testPickUniqueNumbersInRange() {
-		 LottoNumberGenerator generator = new LottoNumberGenerator();
-	        List<Integer> numbers = generator.generate();
 
-	        assertNotNull(numbers);
-	        assertEquals(Lotto.LOTTO_SIZE, numbers.size());
+	@DisplayName("생성된 번호들의 범위와 사이즈를 확인한다.")
+    @Test
+    void checkGeneratedNumbersSizeAndRange() {
+		
+		LottoNumberGenerator generator = new LottoNumberGenerator();
+		List<Integer> numbers = generator.generate();
 
-	        for (int number : numbers) {
-	            assertTrue(number >= Lotto.MIN_NUMBER && number <= Lotto.MAX_NUMBER);
-	        }
-	    }
+        assertNotNull(numbers);
+        assertEquals(Lotto.LOTTO_SIZE, numbers.size());
+        
+        for (int number : numbers) {
+            assertTrue(number >= Lotto.MIN_NUMBER && number <= Lotto.MAX_NUMBER);
+        }
+	}
 }

5616fc50e561e55e5d030a3d0f82be630560c7df
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-02 23:45:08 +0900

    feat: 로또 번호 숫자 범위 유효성 검사
    Lotto 객체 생성시 숫자 범위에 대한 유효성 검사 로직 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,13 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final String INVALID_NUMBER_COUNT = "로또 숫자 개수에 맞도록 입력해주세요.";
+    private static final String INVALID_NUMBER_RANGE = "로또 숫자 범위에 맞도록 입력해주세요.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +18,14 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(INVALID_NUMBER_COUNT);
+        }
+
+        if (numbers.stream().anyMatch(
+                number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER)
+        ) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
         }
     }
 

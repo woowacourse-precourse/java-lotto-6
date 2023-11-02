0b1f2ac68ed57d001e6aea5a85b93bc06b3e14dc
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-02 20:15:03 +0900

    feat(InputView): 구입 금액을 입력받는 기능 추가
    docs: InputView 기능 구현 체크

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.Message;
+
+public class InputView {
+
+    public static String readPurchaseAmount() {
+        Message message = Message.PURCHASE_AMOUNT_INPUT_MESSAGE;
+        System.out.println(message.getMessage());
+        return Console.readLine();
+    }
+}

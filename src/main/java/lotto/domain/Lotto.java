57e77606ef2c453d8775999d61ed589b03a498ce
donghyunkim <ghffu405@gmail.com>
2023-11-02 17:00:05 +0900

    feat: 중복 로또번호 검증메소드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,32 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int LOTTO_NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers){
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if(nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE){
+            throw new IllegalArgumentException("로또 번호들은 중복 될 수 없습니다.");
+        }
+
     }
 
     // TODO: 추가 기능 구현

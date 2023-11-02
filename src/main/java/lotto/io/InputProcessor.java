9d1252567dcd9cff23b6ddfad32cb1845dc9db06
SeoJiWon <sjiwon4491@gmail.com>
2023-11-02 20:08:54 +0900

    feat: 당첨번호 입력 InputProcessor 로직 구현

--- src/main/java/lotto/io/InputProcessor.java
+++ src/main/java/lotto/io/InputProcessor.java
@@ -1,19 +1,28 @@
 package lotto.io;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.User;
 import lotto.io.validator.LottoPurchaseAmountValidator;
+import lotto.io.validator.WinningNumberValidator;
 
 public class InputProcessor {
     private InputProcessor() {
     }
 
-    public static int readLottoPurchaseCount() {
+    public static String readLottoPurchaseCount() {
         System.out.println("구입금액을 입력해 주세요.");
 
         final String userInput = Console.readLine();
         LottoPurchaseAmountValidator.validate(userInput);
 
-        return Integer.parseInt(userInput) / User.LOTTO_PURCHASE_UNIT;
+        return userInput;
+    }
+
+    public static String readWiningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        final String userInput = Console.readLine();
+        WinningNumberValidator.validate(userInput);
+
+        return userInput;
     }
 }

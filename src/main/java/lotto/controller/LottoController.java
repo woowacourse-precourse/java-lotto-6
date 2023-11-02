fd6d982683d25115eaeb8c45a7fba1166cd22360
김승진 <ohksj77@gmail.com>
2023-11-02 16:52:07 +0900

    feat(LottoController): 로또 실행 메서드 및 구매 금액 생성 기능 연결

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import lotto.domain.PurchaseAmount;
+import lotto.io.InputManager;
+import lotto.io.OutputView;
+import lotto.service.LottoService;
+
+public class LottoController {
+
+    private final OutputView outputView;
+    private final InputManager inputManager;
+    private final LottoService lottoService;
+
+    public LottoController(
+            final OutputView outputView,
+            final InputManager inputManager,
+            final LottoService lottoService) {
+        this.outputView = outputView;
+        this.inputManager = inputManager;
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        createPurchaseAmount();
+    }
+
+    private void createPurchaseAmount() {
+        outputView.printPurchaseAmountRequest();
+        final PurchaseAmount purchaseAmount = inputManager.readPurchaseAmount();
+        lottoService.savePurchaseAmount(purchaseAmount);
+    }
+}

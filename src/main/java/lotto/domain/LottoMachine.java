167ae86f8fadb23d7aabd8134390f935252e41cb
donghyunkim <ghffu405@gmail.com>
2023-11-02 17:07:52 +0900

    feat: 로또생성 메소드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,11 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoMachine {
 
     private static LottoMachine lottoMachine = new LottoMachine();
@@ -11,5 +17,19 @@ public class LottoMachine {
         return lottoMachine;
     }
 
-    
+    public List<Lotto> produceLottos(int price){
+        int number = price / 1000;
+
+        List<Lotto> lottos = new List<Lotto>;
+
+        for(int i=0; i<number; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> sortedNumbers = new ArrayList<>(numbers);
+            Collections.sort(sortedNumbers);
+
+            lottos.add(new Lotto(sortedNumbers));
+        }
+        return lottos;
+    }
+
 }

fd0632dc940f6d5410283d41bdb33d07255d4178
FriedHamm <kwh2716@gmail.com>
2023-11-02 19:32:33 +0900

    Feat: 로또 숫자를 입력하라고 요청하는 기능 추가

--- src/main/java/view/Mainview.java
+++ src/main/java/view/Mainview.java
@@ -5,4 +5,9 @@ public class Mainview {
     {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public void requestNumbers()
+    {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }

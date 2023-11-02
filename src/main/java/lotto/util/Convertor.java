aec9ca8823591076937b6702c734ae0c9e853dc9
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-02 20:08:46 +0900

    feat(Convertor) : 쉼표로 split 된 배열 반환 기능 추가

--- src/main/java/lotto/util/Convertor.java
+++ src/main/java/lotto/util/Convertor.java
@@ -1,11 +1,16 @@
 package lotto.util;
 
 public class Convertor {
+    private static final String COMMA = ",";
 
     private Convertor() {
     }
 
     public static int convertStringToInt(String value) {
         return Integer.parseInt(value);
+    }
+
+    public static String[] splitByComma(String input) {
+        return input.split(COMMA);
     }
 }

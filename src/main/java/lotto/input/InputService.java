9e55c03243dfe101bf9253fbce721f25ff89019b
cousim46 <wjdghldns24@naver.com>
2023-11-02 21:18:08 +0900

    feat : 보너스 번호 입력 메서드 추가

--- src/main/java/lotto/input/InputService.java
+++ src/main/java/lotto/input/InputService.java
@@ -6,4 +6,8 @@ public class InputService {
     public static String winNumber() {
         return Console.readLine();
     }
+
+    public static String provideBonusNumber() {
+        return Console.readLine();
+    }
 }

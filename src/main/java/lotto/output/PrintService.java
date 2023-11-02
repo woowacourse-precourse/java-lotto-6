9ed0d375a263d3a2cb8500218e435f8949d93e33
cousim46 <wjdghldns24@naver.com>
2023-11-02 21:24:07 +0900

    feat : 구매개수 출력 메서드 추가

--- src/main/java/lotto/output/PrintService.java
+++ src/main/java/lotto/output/PrintService.java
@@ -9,4 +9,8 @@ public class PrintService {
     public static void requestInputWinNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public static void informPurchaseCount(int count) {
+        System.out.println(count+"개를 구매했습니다.");
+    }
 }

159f7cdf1250aff02a186b6c9a26c207b5ee0abe
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-02 23:46:47 +0900

    feat(GameConfig) : 의존 관계 설정 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,11 @@
 package lotto;
 
-import lotto.view.MessagePrinter;
-import lotto.view.MessageReceiver;
-import lotto.view.ViewValidator;
+import lotto.config.GameConfig;
 
 public class Application {
 
     public static void main(String[] args) {
-        ViewValidator viewValidator = new ViewValidator();
-        MessagePrinter messagePrinter = new MessagePrinter();
-        MessageReceiver messageReceiver = new MessageReceiver(viewValidator);
-
-        GameManager gameManager = new GameManager(messagePrinter, messageReceiver);
+        GameManager gameManager = GameConfig.buildGameManager();
         gameManager.startGame();
     }
 }
---
+++ src/main/java/lotto/config/GameConfig.java
@@ -0,0 +1,29 @@
+package lotto.config;
+
+import lotto.GameManager;
+import lotto.view.MessagePrinter;
+import lotto.view.MessageReceiver;
+import lotto.view.ViewValidator;
+
+public class GameConfig {
+
+    public static GameManager buildGameManager() {
+        ViewValidator viewValidator = viewValidator();
+        MessageReceiver messageReceiver = messageReceiver(viewValidator);
+        MessagePrinter messagePrinter = messagePrinter();
+
+        return new GameManager(messagePrinter, messageReceiver);
+    }
+
+    private static ViewValidator viewValidator() {
+        return new ViewValidator();
+    }
+
+    private static MessagePrinter messagePrinter() {
+        return new MessagePrinter();
+    }
+
+    private static MessageReceiver messageReceiver(final ViewValidator viewValidator) {
+        return new MessageReceiver(viewValidator);
+    }
+}

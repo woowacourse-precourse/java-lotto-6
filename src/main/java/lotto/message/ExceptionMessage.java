48a9b36cff5173745c1ecd9eb7aab8ec9cd54c0f
Jiyoung Choi <12201817@inha.edu>
2023-11-02 22:57:20 +0900

    refactor(ExceptionMessage) : 같은 기능을 수행하는 함수들 하나로 축약

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -3,7 +3,10 @@ package lotto.message;
 public enum ExceptionMessage {
     INPUT_NOT_NUMBER_MESSAGE("숫자를 입력해야 합니다."),
     INPUT_ZERO_OR_LESS_MESSAGE("0보다 큰 숫자를 입력해야 합니다."),
-    INPUT_NOT_MULTIPLE_OF_UNIT_MESSAGE("%d의 배수를 입력해야 합니다.");
+    INPUT_NOT_MULTIPLE_OF_UNIT_MESSAGE("%d의 배수를 입력해야 합니다."),
+    NOT_SAME_WITH_LOTTO_SIZE("로또 숫자는 %d개여야 합니다."),
+    CONSIST_OF_DUPLICATE_NUMBER("로또는 모두 다른 수로 이루어져야 합니다."),
+    IS_NOT_IN_RANGE("%d와 %d사이의 숫자로 이루어져야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
@@ -12,20 +15,12 @@ public enum ExceptionMessage {
         this.exceptionMessage = exceptionMessage;
     }
 
-    public String getMessage() {
-        return exceptionMessage;
+    public String getMessage(Object... values) {
+        return String.format(exceptionMessage, values);
     }
 
-    public String getMessage(int formatValue) {
-        return String.format(exceptionMessage, formatValue);
-    }
-
-    public void throwException() {
-        throw new IllegalArgumentException(ERROR_TAG + exceptionMessage);
-    }
-
-    public void throwException(int value) {
-        throw new IllegalArgumentException(ERROR_TAG + String.format(exceptionMessage, value));
+    public void throwException(Object ... values) {
+        throw new IllegalArgumentException(ERROR_TAG + String.format(exceptionMessage, values));
     }
 }
 

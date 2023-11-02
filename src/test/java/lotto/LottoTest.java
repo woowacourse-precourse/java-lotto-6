7248cddaf39ad484ffb82fadb7cea9813b8488bf
inti0 <142914095+inti0@users.noreply.github.com>
2023-11-03 03:59:47 +0900

    test : LottoNumberGenerator 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
 import lotto.lotto.Lotto;
+import lotto.lotto.LottoNumberGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -26,8 +32,36 @@ class LottoTest {
 
     // 아래에 추가 테스트 작성 가능
     @Test
-    void  테스트(){
-        long maxValue = Long.MAX_VALUE;
-        System.out.println(maxValue);
+    @DisplayName("생성기는 1부터 45까지의 랜덤한 숫자를 반환한다")
+    void 로또숫자생성기_테스트() {
+        //given
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        Set<Integer> createdNumbers = new HashSet<Integer>();
+        int count = 0;
+
+        //when
+        while (createdNumbers.size() < 45 && count < 1000) {
+            List<Integer> lottoNumbers = lottoNumberGenerator.generate();
+            lottoNumbers.forEach(num->createdNumbers.add(num));
+            count++;
+        }
+
+        //then
+        Assertions.assertThat(createdNumbers).contains(1,45);
+        Assertions.assertThat(createdNumbers.size()).isEqualTo(45);
+    }
+
+    @DisplayName("생성기는 중복 숫자를 생성하지 않는다")
+    @RepeatedTest(1000)
+    void 로또숫자생성기_테스트2(){
+        //given
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        List<Integer> lottoNumbers = lottoNumberGenerator.generate();
+
+        //when
+        long count = lottoNumbers.stream().distinct().count();
+
+        //then
+        Assertions.assertThat(count).isEqualTo(6);
     }
 }

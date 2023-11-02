c2a9b4871fa8277e583f1f4f96bc561093ecd5f5
donghyunkim <ghffu405@gmail.com>
2023-11-02 17:20:00 +0900

    feat: 당첨번호 필드,생성자 추가

--- src/main/java/lotto/domain/AnswerLotto.java
+++ src/main/java/lotto/domain/AnswerLotto.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class AnswerLotto {
+
+    private List<Integer> answerNumbers;
+    private int bonusNumber;
+
+    public AnswerLotto(List<Integer> answerNumbers, int bonusNumber) {
+        this.answerNumbers = answerNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    
 }

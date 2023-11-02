a27833a70be630610a693735d9ffa6c318269bc7
heiler <threepebbles@naver.com>
2023-11-03 00:14:00 +0900

    Test: 사용자 입력 화면이 제대로 생성되는지 검사하는 테스트 코드 작성

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+    @Test
+    public void 구입_금액_입력_화면_생성_테스트() {
+        assertThat(InputView.enterBudgetMessage()).isEqualTo("구입금액을 입력해 주세요.");
+    }
+
+    @Test
+    public void 당첨_번호_입력_화면_생성_테스트() {
+        assertThat(InputView.enterWinningLottoTicket()).isEqualTo("당첨 번호를 입력해 주세요.");
+    }
+
+    @Test
+    public void 보너스_번호_입력_화면_생성_테스트() {
+        assertThat(InputView.enterBonusNumber()).isEqualTo("보너스 번호를 입력해 주세요.");
+    }
+}

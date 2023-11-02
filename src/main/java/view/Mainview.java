2019376d84784e0a3561445cd6ad7af53b4be5b5
FriedHamm <kwh2716@gmail.com>
2023-11-02 22:07:46 +0900

    Feat: 결과값을 출력하는 기능 추가

--- src/main/java/view/Mainview.java
+++ src/main/java/view/Mainview.java
@@ -1,18 +1,26 @@
 package view;
 
 public class Mainview {
-    public void askForHowMany()
-    {
+
+    private final static String[] MONEY_OF_WINNING = {"5,000원", "50,000원", "1,500,000원",
+            "30,000,000원", "2,000,000,000원"};
+
+    public void askForHowMany() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void requestNumbers()
-    {
+    public void requestNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
-    public void requestBonusNumber()
-    {
+    public void requestBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printresult(int[] result) {
+        System.out.println("당첨통\n---");
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(3 + i + "개 일치 ("+MONEY_OF_WINNING[i]+" -"+result[i]+"개");
+        }
     }
 }

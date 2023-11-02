5fe5f6b31ca8c06efc46f6797661df02f0c50d65
HyungHoKim <hkim1109@naver.com>
2023-11-02 21:07:33 +0900

    feat(Lotto): 예외 케이스 추가
    중복된 번호 입력 시 예외를 던지는 메서드 duplication() 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static lotto.ExceptionCase.NUMBER_DUPLICATION;
+
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +18,9 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplication(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATION.message());
+        }
+    }
 }

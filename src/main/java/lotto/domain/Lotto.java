ac7089106917be0d41b54372ca64d4c71f745cfb
joo-sang-ah <jsa@interiorteacher.com>
2023-11-03 03:29:53 +0900

    feat: 로또 번호 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
     public static final Integer NUMBER_LENGTH = 6;
     public static final Integer MIN = 1;
     public static final Integer MAX = 45;
@@ -15,9 +17,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != NUMBER_LENGTH
+                || numbers.stream().allMatch(value -> value >= MIN)
+                || numbers.stream().allMatch(value -> value <= MAX)
+                || hasDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 중복되지 않는 숫자여야 합니다.");
         }
+
+    }
+
+    private boolean hasDuplicateNumber(List<Integer> numbers) {
+        List<Integer> distinctValues = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        return distinctValues.size() < numbers.size();
     }
 
     // TODO: 추가 기능 구현

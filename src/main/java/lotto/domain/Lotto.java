761d16035c5ec4cfc8a20b9b9e75e1a186194708
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 16:22:29 +0900

    feat(Lotto): 예외처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,7 +2,14 @@ package lotto.domain;
 
 import java.util.List;
 
+import lotto.util.ErrorMessage;
+
 public class Lotto {
+	
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+	private static final int LOTTO_SIZE = 6;
+	
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +18,35 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    	validateSize(numbers);
+    	validateDuplicateNumber(numbers);
+    	validateNumberOutOfRange(numbers);
+    }
+    
+    private void validateSize(List<Integer> numbers) {
+    	if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_SIZE_ERROR.getForMatMessage(LOTTO_SIZE));
         }
     }
-
-    // TODO: 추가 기능 구현
+    
+    private void validateDuplicateNumber(List<Integer> numbers) {
+    	if (numbers.stream().distinct().count() != numbers.size()){
+    		throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_DUPLICATE_ERROR.getMessage());
+    	}
+    }
+    
+    private void validateNumberOutOfRange(List<Integer> numbers) {
+    	if (numbers.stream().anyMatch(this::isNumberOutOfRange)){
+    		throw new IllegalArgumentException(
+    					ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getForMatMessage(MIN_NUMBER, MAX_NUMBER));
+    	}
+    }
+    
+    private boolean isNumberOutOfRange(int number) {
+    	return number > MAX_NUMBER || number < MIN_NUMBER;
+    }
+ 
+    public int getNumber(int index) {
+    	return numbers.get(index);
+    }
 }

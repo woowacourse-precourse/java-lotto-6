bcd0810726d7c3aea8ef33467089134daa2f68ad
heiler <threepebbles@naver.com>
2023-11-03 01:50:29 +0900

    Feat: 로또 번호의 개수(6개)를 의미하는 상수 추가

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -1,6 +1,7 @@
 package lotto.constant;
 
 public class LottoConstants {
+    public static final int LOTTO_NUMBERS_SIZE = 6;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
 }

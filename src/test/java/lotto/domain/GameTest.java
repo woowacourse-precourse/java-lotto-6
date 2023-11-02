0e96ac1864d252936adee4545bba858de8b0a9b9
joo-sang-ah <jsa@interiorteacher.com>
2023-11-03 02:42:47 +0900

    test: GameTest에 입력값 검증 추가

--- src/test/java/lotto/domain/GameTest.java
+++ src/test/java/lotto/domain/GameTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,48 @@ class GameTest {
         game = new Game();
         game.setWinningNumbers(List.of(1, 2, 3, 4, 5, 6));
         game.setBonusNumber(7);
+    }
+
+    @DisplayName("당첨 번호에 최소값보다 작은 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByLessThanMinimum() {
+        assertThatThrownBy(() -> game.setWinningNumbers(List.of(1, 2, 3, 4, 5, Lotto.MIN - 1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 최대값보다 큰 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByLessThanMax() {
+        assertThatThrownBy(() -> game.setWinningNumbers(List.of(1, 2, 3, 4, 5, Lotto.MAX + 1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 값이 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByDuplicatedNumber() {
+        assertThatThrownBy(() -> game.setWinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 최소값보다 작으면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByLessThanMinimum() {
+        assertThatThrownBy(() -> game.setBonusNumber(Lotto.MIN - 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 최대값보다 크면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByGreaterThanMax() {
+        assertThatThrownBy(() -> game.setBonusNumber(Lotto.MAX + 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByDuplicatedNumber() {
+        assertThatThrownBy(() -> game.setBonusNumber(1))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("당첨 번호와 6개 번호가 일치하면 1등")

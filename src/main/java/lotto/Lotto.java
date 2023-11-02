b09334cae39a73b5892e54f43f6483a2df12ded3
정민규 <fastdodge7@naver.com>
2023-11-02 18:19:28 +0900

    feat(Lotto) : toString() 메소드 오버라이드
    Lotto 클래스의 toString() 메소드를 오버라이드하여 로또 정보 출력 형식에 맞는 문자열을 리턴하도록 만들었습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,9 +11,25 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder msg = new StringBuilder("[");
+
+        String numberList = numbers.stream()
+                .map(element -> element.toString())
+                .collect(Collectors.joining(", "));
+
+        msg.append(numberList).append("]");
+        return msg.toString();
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 정확히 6자리여야 합니다.");
+        }
+        for (Integer num : numbers) {
+            if (num < 1 || num > 45)
+                throw new IllegalArgumentException("각 로또번호는 1 ~ 45 사이의 정수여야 합니다.");
         }
     }
 

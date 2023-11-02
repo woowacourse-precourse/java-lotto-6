e10b7e76eaba4278d897740e1bf6534d76a4e35d
Dalliaaaaa <ajswlsoato@naver.com>
2023-11-03 00:28:11 +0900

    feat: 1000원 단위가 아닐 시 예외

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,10 +8,17 @@ public class Application {
     }
 }
 
-class User {
-    public static void InputMoney() {
+class User{
+    public static void InputMoney(){
         System.out.println("구입금액을 입력해 주세요.");
         String m = Console.readLine();
         int money = Integer.parseInt(m);
+        System.out.println(CorrectMoney(money));
+    }
+    public static int CorrectMoney(int money){
+        if (money % 1000 == 0){
+            return money / 1000;
+        }
+        throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
     }
 }

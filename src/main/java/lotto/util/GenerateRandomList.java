72a7856309cc2047075f129bb8c9a966eab46661
suihub <qkdcrt@gmail.com>
2023-11-03 05:50:58 +0900

    <feat (GenerateRandomList)> : "랜덤 번호 기능 및 오름차순 정렬 구현"
    - 테스트에 맞추어 Util 패키지에 랜덤 번호를 담은 리스트 생성 클래스 구현.
    
    List를 생성해 반환하며, 오름차순을 하는 기능을 구현.

---
+++ src/main/java/lotto/util/GenerateRandomList.java
@@ -0,0 +1,23 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GenerateRandomList {
+    private final static Integer MIN_NUMBER = 1;
+    private final static Integer MAX_NUMBER = 45;
+    private final static Integer NUMBER_SIZE = 6;
+
+    public static List<Integer> createRandomList() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_SIZE);
+        asendingSortList(numbers);
+        return numbers;
+    }
+
+    private static List<Integer> asendingSortList(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
--- src/test/java/lotto/util/GenerateRandomListTest.java
+++ src/test/java/lotto/util/GenerateRandomListTest.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-class GeneratorRandomListTest {
+class GenerateRandomListTest {
     @Test
     @DisplayName("리스트의 숫자는 1~45이다.")
     void listNumberRangeTest() {
-        List<Integer> numbers = GeneratorRandomList.createRandomList();
+        List<Integer> numbers = GenerateRandomList.createRandomList();
 
         for (Integer number : numbers) {
             assertThat(number).isBetween(1, 45);
@@ -21,7 +21,7 @@ class GeneratorRandomListTest {
     @Test
     @DisplayName("리스트의 갯수는 6개이다.")
     void createListByNotSixSize() {
-        List<Integer> numbers = GeneratorRandomList.createRandomList();
+        List<Integer> numbers = GenerateRandomList.createRandomList();
 
         assertThat(numbers.size()).isEqualTo(6);
     }

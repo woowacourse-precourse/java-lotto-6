748f55fc934df35dc96b34d0f3d1546ec697fdeb
yechop <channetattoo@gmail.com>
2023-11-02 19:30:23 +0900

    refactor(Output): printMyLottoCount() 빈줄 추가

--- src/main/java/lotto/io/Output.java
+++ src/main/java/lotto/io/Output.java
@@ -1,9 +1,16 @@
 package lotto.io;
 
+import lotto.domain.Lottos;
+
 public class Output {
 
     public void printInputMoneyMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printMyLottoCount(int money) {
+        System.out.printf("%d개를 구매했습니다.", money/1000);
+        System.out.println();
     }
 
     public void printInputWinningNumbersMessage() {

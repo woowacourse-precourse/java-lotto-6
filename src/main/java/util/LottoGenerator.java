c2766059d045e5c2454ea702128fab08fb7be54e
FriedHamm <kwh2716@gmail.com>
2023-11-02 19:24:55 +0900

    Feat: 로또를 생성하는 기능과 숫자를 생성하는 기능 추가

--- src/main/java/util/LottoGenerator.java
+++ src/main/java/util/LottoGenerator.java
@@ -1,4 +1,20 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
 public class LottoGenerator {
+    private LottoGenerator() {
+    }
+
+    public static List<Lotto> generateLotto(int times)
+    {
+        List<Lotto> lotto = new ArrayList<>();
+
+        for(int i = 0; i < times; i++)
+        {
+           lotto.add(NumberGenerator.generateNumbers());
+        }
+    }
 }
--- src/main/java/util/NumberGenerator.java
+++ src/main/java/util/NumberGenerator.java
@@ -1,4 +1,14 @@
 package util;
 
-public class NumberGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+class NumberGenerator {
+    private NumberGenerator() {
+    }
+
+    static Lotto generateNumbers()
+    {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+    }
 }

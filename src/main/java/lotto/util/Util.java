9512b9a9a219faa67a5d3b207c3a9df172d3e952
UDADDY <gustmd5715@gmail.com>
2023-11-02 19:09:43 +0900

    feat: 금액을 횟수(1,000원 단위)로 변환한다.

--- src/main/java/lotto/util/Util.java
+++ src/main/java/lotto/util/Util.java
@@ -1,11 +1,14 @@
 package lotto.util;
 
+import lotto.domain.LottoConstant;
+
 public class Util {
-    public static int calculateAttempt(int cash){
-        return 0;
+    public static int calculateAttempt(int cash) {
+        int attempt = cash / LottoConstant.CASH_UNIT.getValue();
+        return attempt;
     }
 
-    public static double calculateRateOfReturn(int cash, int returnCash){
+    public static double calculateRateOfReturn(int cash, int returnCash) {
         return 0;
     }
 }

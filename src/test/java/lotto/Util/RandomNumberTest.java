cfad14522feb8a962c1408d3325c91a2b1b11125
Firedrago95 <yonghwa95@naver.com>
2023-11-02 20:06:48 +0900

    refactor(RandomNumberTest) : 상수 위치 이동

--- src/test/java/lotto/Util/RandomNumberTest.java
+++ src/test/java/lotto/Util/RandomNumberTest.java
@@ -12,13 +12,13 @@ import org.junit.jupiter.api.Test;
 
 public class RandomNumberTest {
 
-    public static final int MIN_NUMBER_RANGE = 1;
-    public static final int MAX_NUMBER_RANGE = 45;
     List<Integer> numbers;
+
     @BeforeEach
     public void generateRandomNumbers() {
         numbers = RandomNumber.generateRandomNumbers();
     }
+
     @Test
     @DisplayName("생성된 숫자의 개수는 6개")
     public void sizeTest() {
@@ -30,6 +30,7 @@ public class RandomNumberTest {
     public void uniqueTest() {
         assertTrue(checkUnique(numbers));
     }
+
     @Test
     @DisplayName("각 숫자의 범위는 1~45 사이")
     public void numberRangeTest() {
@@ -39,11 +40,12 @@ public class RandomNumberTest {
     private boolean checkUnique(List<Integer> numbers) {
         Set<Integer> uniqueNumber = new HashSet<>();
         return numbers.stream()
-                .allMatch(number -> uniqueNumber.add(number));
+            .allMatch(number -> uniqueNumber.add(number));
     }
-    
+
     private boolean checkNumberRange(List<Integer> numbers) {
         return numbers.stream()
-            .allMatch(number -> number >= MIN_NUMBER_RANGE && number <= MAX_NUMBER_RANGE);
+            .allMatch(number -> number >= RandomNumber.MIN_NUMBER_RANGE
+                && number <= RandomNumber.MAX_NUMBER_RANGE);
     }
 }

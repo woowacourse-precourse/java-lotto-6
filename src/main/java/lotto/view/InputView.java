305248cf5edb7b9f560fa8d0944fda9cc413a099
heiler <threepebbles@naver.com>
2023-11-02 23:17:39 +0900

    Refactor: 값이 고정된 문자열 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,19 @@
 package lotto.view;
 
 public class InputView {
+    public static final String ENTER_BUDGET_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String ENTER_WINNING_LOTTO_TICKET = "당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
     public static String enterBudgetMessage() {
-        return "구입금액을 입력해 주세요.";
+        return ENTER_BUDGET_MESSAGE;
     }
 
     public static String enterWinningLottoTicket() {
-        return "당첨 번호를 입력해 주세요.";
+        return ENTER_WINNING_LOTTO_TICKET;
     }
 
     public static String enterBonusNumber() {
-        return "보너스 번호를 입력해 주세요.";
+        return ENTER_BONUS_NUMBER;
     }
 }

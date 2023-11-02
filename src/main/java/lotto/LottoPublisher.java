b312570700172abc883f593b26bb9473b6bc061e
AmenPark <puzzlistpam@gmail.com>
2023-11-03 02:14:10 +0900

    refactor: 검증 분리

--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -19,23 +19,24 @@ public class LottoPublisher {
         return instance;
     }
 
-    private Lotto publish(){
+    private Lotto publish() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         numbers.sort(Comparator.comparingInt(o -> o));
         return new Lotto(numbers);
     }
 
-    public List<Lotto> sell(long money) throws IllegalArgumentException{
-        if(money<0){
-            throw new IllegalArgumentException();
-        }
-        if(money % Prize.TICKET_PRICE.getMoney()!=0){
-            throw new IllegalArgumentException();
-        }
+    public List<Lotto> sell(long money) throws IllegalArgumentException {
+        validateMoney(money);
         List<Lotto> lottos = new ArrayList<>();
-        for (long i=0;i<money;i+=Prize.TICKET_PRICE.getMoney()){
+        for (long i = 0; i < money; i += Prize.TICKET_PRICE.getMoney()) {
             lottos.add(publish());
         }
         return lottos;
+    }
+
+    private void validateMoney(long money) throws IllegalArgumentException {
+        if (money % Prize.TICKET_PRICE.getMoney() != 0 || money < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }

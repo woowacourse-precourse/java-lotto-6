6be7f064f5b9eb3cdcf903eec2a5145dac43c572
Firedrago95 <yonghwa95@naver.com>
2023-11-02 17:05:11 +0900

    feat(Validator) : 1000의 배수 유효성 검사 기능 구현

--- src/main/java/lotto/Util/Validator.java
+++ src/main/java/lotto/Util/Validator.java
@@ -1,14 +1,26 @@
 package lotto.Util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
 
     public static final String NULL_OR_EMPTY_MESSAGE = "값을 입력해주세요";
     public static final String EMPTY = "";
+    public static final String THOUSANDS_MULTIPLE_MESSAGE = "1000의 배수를 입력해주세요";
+    public static final String THOUSANDS_MULTIPLE_REGEX = "^[0-9]{1,}000";
 
-    public static void validateNullOrEmpty(String input) throws IllegalArgumentException {
+    protected static void validateNullOrEmpty(String input) throws IllegalArgumentException {
         if (input == null || input.equals(EMPTY)) {
             throw new IllegalArgumentException(NULL_OR_EMPTY_MESSAGE);
         }
     }
 
+    protected static void validateBudgetInThousands(String input) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile(THOUSANDS_MULTIPLE_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw  new IllegalArgumentException(THOUSANDS_MULTIPLE_MESSAGE);
+        }
+    }
 }

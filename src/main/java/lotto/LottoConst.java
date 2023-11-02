4d90260fc91e4e1dd1dfdcada278fec8beb8a820
김승우 <tmd8633@naver.com>
2023-11-02 18:44:34 +0900

    Feat : 상수 추가

---
+++ src/main/java/lotto/LottoConst.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class LottoConst {
+
+    public static final int LOTTO_PRICE = 1000;
+
+    public static final int MIN_LOTTO_NUM = 1;
+
+    public static final int MAX_LOTTO_NUM = 45;
+
+    public static final int LOTTO_COUNT = 6;
+}

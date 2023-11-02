711a25b031fe8d42c71860348c581902a6082e2f
RedJunKi <bing8821@naver.com>
2023-11-03 04:05:51 +0900

    update(lotto): 출력 메세지, 에러 메세지, 숫자 상수 정의

--- src/main/java/lotto/constant/DisplayMessages.java
+++ src/main/java/lotto/constant/DisplayMessages.java
@@ -1,5 +1,26 @@
 package lotto.constant;
 
 public enum DisplayMessages {
+    LOTTO_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    PURCHASE_QUANTITY("%s개를 구매했습니다."),
+    REQUEST_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    MATCH_COUNT("%s개 일치 (%s원) - %s개"),
+    MATCH_COUNT_BONUS("%s개 일치, 보너스 볼 일치 (%s원) - %s개"),
+    TOTAL_RETURN("총 수익률은 %s%%입니다.")
+    ;
 
+    private final String message;
+
+    DisplayMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String format(String... args) {
+        return String.format(message, (Object[]) args);
+    }
 }
--- src/main/java/lotto/constant/ErrorMessages.java
+++ src/main/java/lotto/constant/ErrorMessages.java
@@ -1,4 +1,25 @@
 package lotto.constant;
 
 public enum ErrorMessages {
+    PREFIX("[ERROR]"),
+    SUFFIX(" 다시 입력해 주세요"),
+    INVALID_COUNT("입력하신 번호의 개수가 잘못되었습니다."),
+    DUPLICATE_NUMBER("중복된 번호가 있습니다."),
+    INVALID_RANGE("입력 가능한 번호의 범위는 %s부터 %s까지 입니다."),
+    INVALID_TYPE("번호는 숫자만 입력 가능합니다.")
+    ;
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String format(String... args) {
+        return String.format(message, (Object[]) args);
+    }
 }

2e68ab2df97d5524529125a56487e4f85eb083d3
AmenPark <puzzlistpam@gmail.com>
2023-11-03 02:08:17 +0900

    feat:티켓하나값 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -5,7 +5,8 @@ public enum Prize {
     PRIZE_2ND("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
     PRIZE_3RD("5개 일치 (1,500,000원) - ", 1500000),
     PRIZE_4th("4개 일치 (50,000원) - ", 50000),
-    PRIZE_5th("3개 일치 (5,000원) - ", 5000);
+    PRIZE_5th("3개 일치 (5,000원) - ", 5000),
+    TICKET_PRICE("%s개를 구매했습니다.", 1000);
 
     private final String ui;
     private final int money;

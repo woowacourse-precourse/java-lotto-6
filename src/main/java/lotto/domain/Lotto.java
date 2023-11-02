06396e7a0a134cee8ad114338b410f7511a9e8a9
yuseonjun <seonjun0906@gmail.com>
2023-11-02 18:14:20 +0900

    feat(Lotto): 발행 한 로또 번호는 오름차순으로 정렬하여 보여야한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,28 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortedNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private List<Integer> sortedNumbers(List<Integer> numbers) {
+        List<Integer> sortedLotto = new ArrayList<>(numbers);
+        Collections.sort(sortedLotto);
+        return sortedLotto;
     }
 
     public List<Integer> getNumbers() {

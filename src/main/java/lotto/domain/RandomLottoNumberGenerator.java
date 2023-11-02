f958b5ddf8a4d298a57cd74dfa9421ef873c759d
yechop <channetattoo@gmail.com>
2023-11-03 03:34:42 +0900

    feat(RandomLottoNumberGenerator): 자동로또 만들기 구현
    1 ~ 45 범위 내 랜덤 숫자 6개 생성

--- src/main/java/lotto/domain/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/domain/RandomLottoNumberGenerator.java
@@ -1,5 +1,14 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.io.Constants;
+
+import java.util.List;
+
 public class RandomLottoNumberGenerator {
+
+    public List<Integer> generateRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(Constants.LOTTO_MIN_NUM, Constants.LOTTO_MAX_NUM, Constants.LOTTO_NUMBERS);
+    }
 
 }

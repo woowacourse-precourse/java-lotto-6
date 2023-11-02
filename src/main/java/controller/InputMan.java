55fbf2fb52eb0e55719c04888a3557d5f18fe706
FriedHamm <kwh2716@gmail.com>
2023-11-02 16:21:53 +0900

    Feat: 유저의 금액을 입력 받고 반환하는 기능 추가

--- src/main/java/controller/InputMan.java
+++ src/main/java/controller/InputMan.java
@@ -1,9 +1,18 @@
 package controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import validation.ValidationMan;
 
 class InputMan {
+    private ValidationMan validationMan = new ValidationMan();
+    private final static int EXCEPTION_NUMBER = -1;
     int receiveMoney() {
         String money = Console.readLine();
+        if(!validationMan.validateMoney(money))
+        {
+            return -1;
+        }
+        return Integer.parseInt(money);
     }
+
 }

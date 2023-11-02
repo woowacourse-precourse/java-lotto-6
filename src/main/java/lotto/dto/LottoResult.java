c875dd239efef3ca44119caea11f50f4941e9afb
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-03 01:04:52 +0900

    feat(LottoResult): 꽝은 결과 값에서 제거

--- src/main/java/lotto/dto/LottoResult.java
+++ src/main/java/lotto/dto/LottoResult.java
@@ -1,8 +1,26 @@
 package lotto.dto;
 
+import static lotto.constant.LottoRanking.NONE;
+
 import java.util.Map;
 import lotto.constant.LottoRanking;
 
-public record LottoResult(Map<LottoRanking, Integer> winningCounts, Double profitRate) {
+public final class LottoResult {
 
+    private final Map<LottoRanking, Integer> winningCounts;
+    private final Double profitRate;
+
+    public LottoResult(Map<LottoRanking, Integer> winningCounts, Double profitRate) {
+        winningCounts.remove(NONE);
+        this.winningCounts = winningCounts;
+        this.profitRate = profitRate;
+    }
+
+    public Map<LottoRanking, Integer> getWinningCounts() {
+        return winningCounts;
+    }
+
+    public Double getProfitRate() {
+        return profitRate;
+    }
 }

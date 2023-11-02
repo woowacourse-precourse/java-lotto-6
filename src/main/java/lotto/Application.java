f767b633dc5f49bb7c33b5e34507745b38877617
pumpk <cong2738@gmail.com>
2023-11-02 18:19:34 +0900

    feat: 랜덤번호 뽑기 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,24 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lotto.Lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    // 랜덤 번호 6개 뽑기
+    public List<Integer> randomLottoNumber(){
+        List<Integer> box = new ArrayList<>();
+        while(box.size() != 6) {
+            int rd = Randoms.pickNumberInRange(1, 45);
+            if(box.contains(rd)) continue;
+            box.add(rd);
+        }
+        return box;
+    }
+    
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }

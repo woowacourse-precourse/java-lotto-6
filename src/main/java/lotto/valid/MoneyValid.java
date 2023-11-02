2bdecb23a6cbdac29b232bbe2173d75b0fb84cc4
jongwonpc <c3392302@naver.com>
2023-11-03 08:11:58 +0900

    feat: 구입 금액 입력 받는 기능 검증

---
+++ src/main/java/lotto/valid/MoneyValid.java
@@ -0,0 +1,31 @@
+package lotto.valid;
+
+public class MoneyValid {
+
+    public static int validMoney(String input) {
+        int money = validOnlyNumber(input);
+        validDividing(money);
+        validMinMoney(money);
+        return money;
+    }
+
+    private static int validOnlyNumber(String input) {
+        String reg = "^[0-9]*$";
+        if (!input.matches(reg)) {
+            throw new IllegalArgumentException("[ERROR] 돈은 숫자만 입력 가능합니다.");
+        }
+        return Integer.parseInt(input);
+    }
+
+    private static void validDividing(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 돈은 1000원으로 나누어 떨어져야 합니다.");
+        }
+    }
+
+    private static void validMinMoney(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 돈은 최소 1000원 이상이여야 합니다.");
+        }
+    }
+}

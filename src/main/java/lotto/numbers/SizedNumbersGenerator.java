dbf3d11722c502618eeb9cfd7596b4201dd245be
mainfn <_@mainfn.dev>
2023-11-03 00:33:23 +0900

    feat: 원하는 자리의 숫자 리스트를 생성

---
+++ src/main/java/lotto/numbers/SizedNumbersGenerator.java
@@ -0,0 +1,24 @@
+package lotto.numbers;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public final class SizedNumbersGenerator {
+    private final int startInclusive;
+    private final int endInclusive;
+    private final int count;
+
+    public SizedNumbersGenerator(
+            final int startInclusive,
+            final int endInclusive,
+            final int count
+    ) {
+        this.startInclusive = startInclusive;
+        this.endInclusive = endInclusive;
+        this.count = count;
+    }
+
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+    }
+}

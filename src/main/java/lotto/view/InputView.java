11291ac60b7c0b27957e43b1cfbb43ca03141243
leedohyun <lucky77795@naver.com>
2023-11-02 16:30:19 +0900

    feat(Lotto) : 보너스 번호를 입력받는 기능 구현.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,13 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public Integer inputPurchasePrice() {
+    public String inputPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
     public String inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }

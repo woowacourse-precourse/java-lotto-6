3c2b5d37bc09cc243399d4e36ed04c84a554ccc8
권용하 <yongha.kwon12@gmail.com>
2023-11-02 23:17:03 +0900

    feat : 로또 숫자 유요한 지 검사하는 기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        if (Set.copyOf(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Integer number : numbers) {
+            assertValidNumberRange(number);
+        }
+    }
+
+    private void assertValidNumberRange(Integer number) {
+        if (number > 45 || number < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

1c246a4ffb61eb1b457fb0140b707e04def516a9
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 01:59:18 +0900

    feat(OutputView): 출력 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import lotto.io.OutputViewPrinter;
+import lotto.util.message.ProgressMessage;
+
+public class OutputView {
+
+	private static final OutputViewPrinter PRINTER = new OutputViewPrinter();
+
+	public void printInputMoneyMessage() {
+		PRINTER.printLine(ProgressMessage.INPUT_MONEY.getMessage());
+	}
+
+	public void printBuyedLottoNumberMessage(int buyedCount) {
+		PRINTER.printLine(ProgressMessage.BUYED_LOTTO_NUMBER.getForMatMessage(buyedCount));
+	}
+
+	public void printInputAnswerLottoNumberMessage() {
+		PRINTER.printLine(ProgressMessage.INPUT_ANSWER_LOTTO_NUMBER.getMessage());
+	}
+
+	public void printInputBonusNumberMessage() {
+		PRINTER.printLine(ProgressMessage.INPUT_BONUS_NUMBER.getMessage());
+	}
+
+	public void printWinningStatisticsMessage() {
+		PRINTER.printLine(ProgressMessage.WINNING_STATISTICS.getMessage());
+	}
+
+	public void printTotalReturn(double totalReturn) {
+		PRINTER.printLine(ProgressMessage.TOTAL_RETURN.getForMatMessage(totalReturn));
+	}
+}

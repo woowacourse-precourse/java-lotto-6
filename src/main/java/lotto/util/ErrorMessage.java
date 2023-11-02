8eeb63a97ca209e259c6f6d0a052b2ec4ab0dc7e
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 16:22:11 +0900

    feat(ErrorMessage): 에러메세지를 가진 객체 추가

---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,23 @@
+package lotto.util;
+
+public enum ErrorMessage {
+	
+	LOTTO_SIZE_ERROR("로또의 사이즈는 %s이여야 합니다."),
+	LOTTO_NUMBER_DUPLICATE_ERROR("로또 번호는 중복되지 말아야 합니다."),
+	LOTTO_NUMBER_RANGE_ERROR("로또 번호는 %s부터 %s 사이의 숫자여야 합니다."),
+	PREFIX("[ERROR] ");
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return PREFIX.message + message;
+	}
+	
+	public String getForMatMessage(Object... params) {
+	    return String.format(getMessage(), params);
+	}
+}

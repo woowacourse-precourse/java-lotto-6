1e4552c6f76374ebc6fee06e31957c816950d84c
HyungHoKim <hkim1109@naver.com>
2023-11-02 21:28:57 +0900

    feat(Lotto): 예외 케이스 추가
    번호가 주어진 범위 밖인 경우 예외를 발생시키는 메서드 inRange() 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,15 @@
 package lotto;
 
+import static lotto.ExceptionCase.NUMBERS_SIZE_MISMATCH;
 import static lotto.ExceptionCase.NUMBER_DUPLICATION;
+import static lotto.ExceptionCase.NUMBER_OUT_OF_RANGE;
 
 import java.util.List;
 
 public class Lotto {
+    private static final int SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,14 +18,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        correctSize(numbers);
+        unique(numbers);
+        inRange(numbers);
+    }
+
+    private void correctSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException(NUMBERS_SIZE_MISMATCH.message());
         }
     }
 
-    private void duplication(List<Integer> numbers) {
+    private void unique(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException(NUMBER_DUPLICATION.message());
+        }
+    }
+
+    private void inRange(List<Integer> numbers) {
+        if (numbers.stream().allMatch(number -> number >= MIN_NUMBER && number <= MAX_NUMBER)) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.message());
         }
     }
 }

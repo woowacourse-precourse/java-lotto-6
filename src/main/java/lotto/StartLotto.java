704ab4e43a148e55eef69c94528cd8428093c14e
진환 정 <joung.jinhawn@gmail.com>
2023-11-02 17:23:31 +0900

    feat(StartLotto.java) : StartLotto Class 작성

--- src/main/java/lotto/StartLotto.java
+++ src/main/java/lotto/StartLotto.java
@@ -1,4 +1,18 @@
 package lotto;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class StartLotto {
+    private List<Lotto> lottoList;
+    private int purchasePrice;
+    private List<Integer> randomNumbers;
+    private int bonusNumber;
+
+    public StartLotto() {
+        this.purchasePrice = 0;
+        this.bonusNumber = 0;
+    }
+    
 }

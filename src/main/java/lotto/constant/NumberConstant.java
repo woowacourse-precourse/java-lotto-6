ad20929a64584d761af9fb5fcaabf7b055b1ab7d
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-02 23:42:46 +0900

    refactor(ViewValidator, NumberConstant) : 구입 금액 검증 메소드 분리

---
+++ src/main/java/lotto/constant/NumberConstant.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public enum NumberConstant {
+
+    NumberConstant() {
+    };
+
+    public static final int BUYING_PRICE_UNIT = 1000;
+    public static final int ZERO = 0;
+}
--- src/main/java/lotto/view/ViewValidator.java
+++ src/main/java/lotto/view/ViewValidator.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import static lotto.constant.ExceptionConstant.BUYING_PRICE_INPUT_EXCEPTION;
+import static lotto.constant.NumberConstant.BUYING_PRICE_UNIT;
+import static lotto.constant.NumberConstant.ZERO;
 
 public class ViewValidator {
 
@@ -8,15 +10,23 @@ public class ViewValidator {
         int buyingPrice = Integer.parseInt(buyingPriceText);
 
         try {
-            if (buyingPrice % 1000 != 0) {
-                throw new IllegalArgumentException(BUYING_PRICE_INPUT_EXCEPTION.message);
-            }
+            validateMod(buyingPrice);
         } catch (IllegalArgumentException e) {
-            String exceptionMessage = e.getMessage();
-            System.out.println(exceptionMessage);
+            printExceptionMessage(e);
             return false;
         }
 
         return true;
+    }
+
+    private void validateMod(final int buyingPrice) {
+        if (buyingPrice % BUYING_PRICE_UNIT != ZERO) {
+            throw new IllegalArgumentException(BUYING_PRICE_INPUT_EXCEPTION.message);
+        }
+    }
+
+    private void printExceptionMessage(final IllegalArgumentException e) {
+        String exceptionMessage = e.getMessage();
+        System.out.println(exceptionMessage);
     }
 }

501479e0b0ff3154c5914d567b3819b4da0da47d
pumpk <cong2738@gmail.com>
2023-11-02 19:26:54 +0900

    User 삭제 원상복구

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,10 +3,23 @@ package lotto;
 import java.util.ArrayList;
 import java.util.List;
 
+import lotto.Lotto;
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
-import lotto.User;
 
 public class Application {
+    // 랜덤 번호 6개 뽑기
+    public static List<Integer> setRandomNumbers() {
+        List<Integer> box = new ArrayList<>();
+        while (box.size() != 6) {
+            int rd = Randoms.pickNumberInRange(1, 45);
+            if (box.contains(rd))
+                continue;
+            box.add(rd);
+        }
+        return box;
+    }
+
     private static List<Integer> getUserNumbers() {
         List<Integer> box = new ArrayList<>();
         for (String s : Console.readLine().split(",")) {
@@ -15,14 +28,21 @@ public class Application {
         return box;
     }
 
-    private static void judge(Lotto winNum, User user){
-        int ct = winNum.countMatch(user.lotto.getNumbers());
+    private static int getUserBonus(Lotto userNumbers) {
+        int bonus = Integer.parseInt(Console.readLine());
+        if (userNumbers.lottoContains(bonus))
+            throw new IllegalArgumentException();
+        return bonus;
+    }
+
+    private static void judge(Lotto winNum, Lotto user, int bonus){
+        int ct = winNum.countMatch(user.getNumbers());
         if(ct > 5){
             System.out.println("1등");
             return;
         }
         if(ct > 4) {
-            if (winNum.lottoContains(user.bonus)) System.out.println("2등");
+            if (winNum.lottoContains(bonus)) System.out.println("2등");
             else System.out.println("3등");
             return;
         }
@@ -39,8 +59,9 @@ public class Application {
 
     public static void main(String[] args) {
         Lotto winNumber = new Lotto(getUserNumbers());
-        User user = new User();
+        Lotto user = new Lotto(setRandomNumbers());
+        int bonus = getUserBonus(user);
 
-        judge(winNumber, user);
+        judge(winNumber, user, bonus);
     }
 }

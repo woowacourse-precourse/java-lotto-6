41ad9a3099d471180af63f648bba8f8c5bf63c3c
HyungHoKim <hkim1109@naver.com>
2023-11-03 00:26:38 +0900

    feat(GameController): 로또 당첨 번호 입력
    로또 당첨 번호를 입력하는 메서드 setLotto() 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,23 +1,49 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.model.Lotto;
 import lotto.model.User;
 import lotto.utils.Converter;
 import lotto.validator.GameValidator;
 import lotto.view.InputView;
 
 public class GameController {
-    private final User user;
-    private final int count;
+    private User user;
+    private Lotto lotto;
 
     GameController() {
-        this.count = setCount();
-        this.user = new User(count);
+        setUser();
+        setLotto();
+    }
+
+    private void setUser() {
+        try {
+            this.user = new User(setCount());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setUser();
+        }
     }
 
     private int setCount() {
-        int money = Integer.parseInt(InputView.money());
+        String money = InputView.money();
         GameValidator.validateMoney(money);
         return Converter.moneyToCount(money);
+    }
+
+    private void setLotto() {
+        try {
+            this.lotto = new Lotto(setLottoNumbers());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setLotto();
+        }
+    }
+
+    private List<Integer> setLottoNumbers() {
+        String numbers = InputView.lottoNumbers();
+        GameValidator.validateNumbers(numbers);
+        return Converter.stringToIntArray(numbers);
     }
 
 }

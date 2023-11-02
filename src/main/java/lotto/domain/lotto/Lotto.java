e0083bce4e5ababe68aa3ff3d8edfff5e239b7d4
leedohyun <lucky77795@naver.com>
2023-11-02 16:58:32 +0900

    feat(Lotto) : 1~45의 숫자 6개를 랜덤으로 뽑아 로또를 생성하는 기능 구현.

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain.lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
@@ -8,6 +9,11 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public Lotto createLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
     }
 
     private void validate(List<Integer> numbers) {

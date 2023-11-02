bdd2d27cbccfcd3add5548ab80449c4f33288e69
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 18:51:48 +0900

    feat(LottoRepository): 각 로또를 형식에 따른 로또 번호 메세지 출력기능 추가

--- src/main/java/lotto/domain/LottoRepository.java
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -3,13 +3,26 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class LottoRepository {
+	
+	private static final String DELIMITER = System.lineSeparator();
 	
 	private static final List<Lotto> lottos = new ArrayList<>();
 	
 	public static List<Lotto> getLottos(){
 		return Collections.unmodifiableList(lottos);
+	}
+	
+	public String printLottoNumbers() {
+		StringJoiner strJoiner = new StringJoiner(DELIMITER);
+		
+		for(Lotto lotto : lottos) {
+			strJoiner.add(lotto.printNumbers());
+		}
+		
+		return strJoiner.toString();
 	}
 	
 	public static void addLotto(Lotto lotto) {

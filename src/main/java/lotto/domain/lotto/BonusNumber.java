c5601d5153728f3fe0a5327807035341406edb51
leedohyun <lucky77795@naver.com>
2023-11-02 19:55:46 +0900

    feat(Lotto) : 보너스 번호가 1~45까지의 숫자인지 검증하는 기능 구현.

--- src/main/java/lotto/domain/lotto/BonusNumber.java
+++ src/main/java/lotto/domain/lotto/BonusNumber.java
@@ -7,16 +7,33 @@ public class BonusNumber {
 
     public BonusNumber(String bonusNumber) {
         validate(bonusNumber);
-        this.number = Integer.parseInt(bonusNumber);
+        this.number = parseBonusNum(bonusNumber);
     }
 
     private void validate(String bonusNumber) {
         validateFormat(bonusNumber);
+        validateRange(bonusNumber);
     }
 
     private void validateFormat(String bonusNumber) {
         if (!bonusNumber.matches(bonusNumberRegex)) {
             throw new IllegalArgumentException("[ERROR] 보너스 숫자는 숫자만 입력해야 합니다.");
         }
+    }
+
+    private void validateRange(String bonusNumber) {
+        int bonusNum = parseBonusNum(bonusNumber);
+
+        if (isInvalidRange(bonusNum)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자는 1~45사이의 숫자여야 합니다.");
+        }
+    }
+
+    private static int parseBonusNum(String bonusNumber) {
+        return Integer.parseInt(bonusNumber);
+    }
+
+    private boolean isInvalidRange(int bonusNum) {
+        return bonusNum < 1 || bonusNum > 45;
     }
 }

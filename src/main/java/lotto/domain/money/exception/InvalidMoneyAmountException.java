d4c55a19954262287bc7f77fafd70ddf39eb4987
mainfn <_@mainfn.dev>
2023-11-02 22:49:55 +0900

    docs: `Money` 문서화

--- src/main/java/lotto/domain/money/exception/InvalidMoneyAmountException.java
+++ src/main/java/lotto/domain/money/exception/InvalidMoneyAmountException.java
@@ -1,5 +1,8 @@
-package lotto.domain.money;
+package lotto.domain.money.exception;
 
+/**
+ * Money의 잔액이 음수일 경우 발생하는 예외입니다.
+ */
 public final class InvalidMoneyAmountException extends IllegalArgumentException {
     public static final String INVALID_MONEY_AMOUNT_EXCEPTION_MESSAGE = "잔액은 음수가 될 수 없습니다";
 

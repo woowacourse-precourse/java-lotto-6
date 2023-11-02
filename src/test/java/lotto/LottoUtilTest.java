f78857ee5669bddce5ce01d485328e608a1f97ce
mythpoy <okmlnsunok@gmail.com>
2023-11-02 19:38:18 +0900

    feat: 금액에 맞는 로또들을 만들어내는 util 함수 테스트

--- src/test/java/lotto/LottoUtilTest.java
+++ src/test/java/lotto/LottoUtilTest.java
@@ -11,7 +11,7 @@ class LottoUtilTest {
 
     @DisplayName("로또 구입 금액이 1000원 단위로 떨어지는지 검증")
     @Test
-    void purchaseLotto() {
+    void validateAmount() {
         assertThatThrownBy(() -> LottoUtil.validateAmount(1001))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -21,5 +21,12 @@ class LottoUtilTest {
     void parseNumberToListInteger() {
         assertThat(LottoUtil.parseNumberToListInteger("1,2,3,4,5,6"))
                 .isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @DisplayName("입력한 금액만큼 로또 구매")
+    @Test
+    void purchaseLottoList() {
+        List<Lotto> lottoList = LottoUtil.createLottoList(8000);
+        assertThat(lottoList.size()).isEqualTo(8);
     }
 }

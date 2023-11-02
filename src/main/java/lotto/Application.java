62fdce8cc38b47e1b458c92dda08d8eebdd1fc39
Roista57 <Roista57@gmail.com>
2023-11-02 16:23:26 +0900

    feat: lottoBuyCount 함수 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    static int lottoBuyCount(String money){
+        try{
+            int lottoTotalCount = Integer.parseInt(money);
+            if (lottoTotalCount % 1000 != 0){
+                throw new IllegalArgumentException("구매 금액을 1000원 단위로 작성해주세요.");
+            }
+            return lottoTotalCount;
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력받은 값이 잘못되었습니다.");
+        }
+    }
+    public static void main(String[] args) throws IllegalArgumentException{
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = Console.readLine();
+        int lottoTotalCount = lottoBuyCount(money);
     }
 }

6c43b702220209b2eaa30900bdb016577963316c
이종경 <gyoung3063413@naver.com>
2023-11-02 19:21:33 +0900

    [추가] 사용자에게 금액을 입력 받는 메서드

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -4,11 +4,11 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BuyLotto {
 	static int money;
-	
+
 	public void getMoney() {
 		System.out.println("구입 금액을 입력해주세요.");
 		String userInput = readLine().trim();
 //		System.out.println(userInput);
 	}
-		
+	
 }

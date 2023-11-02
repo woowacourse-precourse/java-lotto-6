d2ab366b60598fc3237f0266c094b6d98e30caab
FriedHamm <kwh2716@gmail.com>
2023-11-02 16:25:12 +0900

    Refactor: 금액 예외확인 코드 수정

--- src/main/java/validation/ValidationMan.java
+++ src/main/java/validation/ValidationMan.java
@@ -5,11 +5,11 @@ public class ValidationMan {
     private final static int STANDARD_MONEY = 1000;
 
     public boolean validateMoney(String money) {
-        if (!(money.matches("\\d+"))) {
+        if (!money.matches("\\d+")) {
             return false;
-            if (Integer.parseInt(money) % STANDARD_MONEY != 0) {
-                return false;
-            }
+        }
+        if (Integer.parseInt(money) % STANDARD_MONEY != 0) {
+            return false;
         }
         return true;
     }

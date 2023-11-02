b49c6c24373f15b168313822e8a888f50e8f596c
이종경 <gyoung3063413@naver.com>
2023-11-02 16:03:33 +0900

    [추가] 구현기능 목록

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) { // 인스턴스
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers) { // 사용자가 입력한 값이 6자인지 확인하는 메서드
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }

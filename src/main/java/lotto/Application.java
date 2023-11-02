ee874a055531ef5b2e7a0526c5cd7174aa5da8a8
koojawon <jawonkoo@naver.com>
2023-11-02 22:49:12 +0900

    feat: 메인 메소드 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+
+        lottoController.purchase();
+
+        lottoController.setWinLotto();
+
+        lottoController.checkLotto();
     }
 }

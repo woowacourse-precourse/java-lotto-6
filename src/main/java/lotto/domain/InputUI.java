428627ebea2e1aa77db38af7db00f6af3f781e0a
mmalfoy <stpmalfoy@gmail.com>
2023-11-03 05:06:40 +0900

    feat(InputUI) : 보너스 번호입력 기능 구현

---
+++ src/main/java/lotto/domain/InputUI.java
@@ -0,0 +1,58 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUI {
+    private final int ERR_NUM = Integer.MIN_VALUE;
+    private int bonusNum;
+
+    public int purchase() {
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            String tempCost = Console.readLine();
+            int cost = checkValidPurchase(tempCost);
+            return cost;
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    public int checkValidPurchase(String tempCost) {
+        try {
+            int cost = Integer.parseInt(tempCost);
+            if (cost < 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 0원 이상이여야 합니다.");
+            }
+            if (cost % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+            }
+            return cost;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 정수여야 합니다.");
+        }
+    }
+
+    public void bonusBall() {
+        try {
+            System.out.println("보너스 번호를 입력해 주세요.");
+            String tempBonus = Console.readLine();
+            bonusNum = checkValidBonusNum(tempBonus);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    public int checkValidBonusNum(String tempBonus) {
+        try {
+            int bonusNum = Integer.parseInt(tempBonus);
+            if (bonusNum < 0 || bonusNum > 45) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            return bonusNum;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 정수여야 합니다.");
+        }
+    }
+
+
+}

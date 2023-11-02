aae255561cc5f934ff3b2924219f9a0d0e4ea734
yechop <channetattoo@gmail.com>
2023-11-02 20:56:08 +0900

    feat(WinningChecker): 로또 등수별 수량을 세는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -30,6 +30,5 @@ public class LottoController {
 
 
 
-
     }
 }
--- src/main/java/lotto/domain/WinningChecker.java
+++ src/main/java/lotto/domain/WinningChecker.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningChecker {
@@ -16,5 +17,40 @@ public class WinningChecker {
 
     public List<Integer> countMatchedNumbersEachLotto() {
         return lottos.matchNumbersEachLotto(winningNumbers, bonusNumber);
+    }
+
+    public List<Integer> countWinningLottos(List<Integer> matchedNumberCountEachLotto) {
+        int rank1 = 0;
+        int rank2 = 0;
+        int rank3 = 0;
+        int rank4 = 0;
+        int rank5 = 0;
+
+        List<Integer> rank = new ArrayList<>();
+        for (int i : matchedNumberCountEachLotto) {
+            if (i == 3) {
+                rank5++;
+            }
+            if (i == 4) {
+                rank4++;
+            }
+            if (i == 5) {
+                rank3++;
+            }
+            if (i == 9) {
+                rank2++;
+            }
+            if (i == 6) {
+                rank1++;
+            }
+        }
+
+        rank.add(rank1);
+        rank.add(rank2);
+        rank.add(rank3);
+        rank.add(rank4);
+        rank.add(rank5);
+
+        return rank;
     }
 }

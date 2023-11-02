7f435a893d9bf04eaef92ea333288282049f3517
donghyunkim <ghffu405@gmail.com>
2023-11-02 16:39:23 +0900

    feat: 정수 검증 메소드 추가

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -2,5 +2,10 @@ package lotto.validation;
 
 public class Validator {
 
+    public static void validateInt(String str){
+        if(str.contains(".")){
+            throw new IllegalArgumentException("정수가 아닙니다.");
+        }
+    }
 
 }

7b92abe495f6dc954598941d95ac75336f7ffbe6
donghyunkim <ghffu405@gmail.com>
2023-11-02 16:55:41 +0900

    feat: 로또머신 싱긅톤 생성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,15 @@
 package lotto.domain;
 
 public class LottoMachine {
+
+    private static LottoMachine lottoMachine = new LottoMachine();
+
+    private LottoMachine() {
+    }
+
+    public static LottoMachine getInstance(){
+        return lottoMachine;
+    }
+
+    
 }

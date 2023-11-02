3a1a8f01540843132b3cb692c880917d3e9be3bf
FriedHamm <kwh2716@gmail.com>
2023-11-02 20:16:45 +0900

    Add: 순위 열거형 상수 추가

--- src/main/java/model/MainModel.java
+++ src/main/java/model/MainModel.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
 public class MainModel {
+    private List<Lotto> computerLotto;
+
+    public MainModel(List<Lotto> computerLotto) {
+        this.computerLotto = computerLotto;
+    }
+
+    public List<Integer> judgeLotto(Lotto userLotto, int bonusNumber)
+    {
+        List<Integer> result = new ArrayList<>();
+
+    }
 }

8b842c0dab705862aa98dc03d191995782ca55e7
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-02 21:19:36 +0900

    feat: 로또 번호 중복 테스트가 성공하는 중복에 대한 예외처리 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,12 +6,19 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
             throw new IllegalArgumentException();
         }
     }

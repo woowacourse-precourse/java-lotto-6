4d2264731129dd6f8338e7313cd6270bb8675201
김승진 <ohksj77@gmail.com>
2023-11-02 17:33:09 +0900

    feat(Lotto): 출력을 위한 로또 숫자 값 확인 기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+
+    private static final int NUM_OF_LOTTO_NUMBER = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(final List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(final List<Integer> numbers) {
+        if (numbers.size() != NUM_OF_LOTTO_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> toValue() {
+        return Collections.unmodifiableList(numbers);
+    }
+}

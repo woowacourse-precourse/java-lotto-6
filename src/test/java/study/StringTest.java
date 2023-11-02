f3e9b2a5347ea44a82685d02186befd55dea1278
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-02 19:28:06 +0900

    test($StringTest): study를 위한 테스트 클래스 생성
    테스트 코드 작성을 위한 학습 도구

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,9 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+}

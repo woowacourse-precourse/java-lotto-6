41bec1c14a2c616ba32ea7585c4462a89b2d97c9
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-02 23:42:26 +0900

    feat(LottoRanking): 로또 순위 enum 생성

---
+++ src/main/java/lotto/constant/LottoRanking.java
@@ -0,0 +1,26 @@
+package lotto.constant;
+
+public enum LottoRanking {
+
+    FIRST("6개 일치", 2000000000),
+    SECOND("5개 일치, 보너스 볼 일치", 30000000),
+    THIRD("5개 일치", 1500000),
+    FOURTH("4개 일치", 50000),
+    FIFTH("3개 일치", 5000);
+
+    private final String winningCondition;
+    private final int prizeMoney;
+
+    LottoRanking(String winningCondition, int prizeMoney) {
+        this.winningCondition = winningCondition;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public String getWinningCondition() {
+        return winningCondition;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}

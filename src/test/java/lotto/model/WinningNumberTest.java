86ab0580607f891afa53b505d7e98a6e1e151689
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-02 22:34:08 +0900

    test: 당첨 번호 생성 테스트

---
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumberTest {
+
+    static final int WINNING_NUMBER_SIZE = 7;
+    static final int WINNING_NUMBER_MIN = 1;
+    static final int WINNING_NUMBER_MAX = 45;
+
+    @Test
+    @DisplayName("당첨 번호 생성 테스트")
+    void createWinningNumber() {
+        WinningNumber winningNumber = new WinningNumber();
+        List<Integer> numbers = winningNumber.getNumbers();
+
+        assertThat(numbers.size()).isEqualTo(WINNING_NUMBER_SIZE);
+        for (Integer number : numbers) {
+            assertThat(number).isBetween(WINNING_NUMBER_MIN, WINNING_NUMBER_MAX);
+        }
+        assertThat(new HashSet<>(numbers).size()).isEqualTo(WINNING_NUMBER_SIZE);
+    }
+}

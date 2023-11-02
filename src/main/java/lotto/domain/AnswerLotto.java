6f673a37f98152e1678e086aaef6a7ccb3119ad5
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 17:32:46 +0900

    feat(AnswerLotto): 당첨 로또번호 객체 추가와 보너스 번호 예외처리 기능 추가

---
+++ src/main/java/lotto/domain/AnswerLotto.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.List;
+
+import lotto.util.ErrorMessage;
+
+public class AnswerLotto extends Lotto{
+	
+	private final BonusNumber bonus;
+	
+	public AnswerLotto(List<Integer> numbers, int bonusNumber){
+		super(numbers);
+		validateBonusNumber(numbers, bonusNumber);
+		this.bonus = new BonusNumber(bonusNumber);
+	}
+	
+	private void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
+		validateDuplicateBonusNumber(numbers, bonusNumber);
+		validateBonusNumberOutOfRange(bonusNumber);
+	}
+	
+	private void validateDuplicateBonusNumber(List<Integer> numbers, int bonusNumber) {
+		if(numbers.contains(bonusNumber)) {
+			throw new IllegalArgumentException(ErrorMessage.LOTTO_BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
+		}
+	}
+	
+	
+	private void validateBonusNumberOutOfRange(int bonusNumber) {
+    	if(isNumberOutOfRange(bonusNumber)) {
+    		throw new IllegalArgumentException(
+					ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getForMatMessage(MIN_NUMBER, MAX_NUMBER));
+    	}
+    }
+
+	public int getBonusNumber() {
+		return bonus.getBonusNumber();
+	}
+}

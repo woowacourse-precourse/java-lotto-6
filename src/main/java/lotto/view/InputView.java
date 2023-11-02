5d3f9818b0e35e580515ad4d3baa1045e7e41375
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 05:05:27 +0900

    feat(view): 사용자에게 로또 구입 금액을 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static String purchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }

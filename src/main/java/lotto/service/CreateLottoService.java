ed2fb21fb37bf162f958b83c37ff65bd6f24ec9e
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 19:55:54 +0900

    feat(CreateLottoService): 로또 생성 기능 추가

--- src/main/java/lotto/service/CreateLottoService.java
+++ src/main/java/lotto/service/CreateLottoService.java
@@ -1,23 +1,11 @@
 package lotto.service;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 import lotto.domain.Lotto;
-import lotto.domain.Money;
 import lotto.generator.LottoNumberGenerator;
 
 public class CreateLottoService {
 	
 	private static final LottoNumberGenerator GENERATOR = new LottoNumberGenerator();
-	
-	public List<Lotto> generateLottos(Money money){
-		int tryCount = money.getTryCount();
-		
-		return Stream.generate(() -> generateLotto())
-						.limit(tryCount)
-						.toList();
-	}
 		
 	private Lotto generateLotto() {
 		return new Lotto(GENERATOR.generate());

067e224cdcea819989820dcc1ef11709c0b9b6eb
yuseonjun <seonjun0906@gmail.com>
2023-11-02 18:10:07 +0900

    feat(LottoController): 구입금액에 맞는 로또 갯수를 발행한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,31 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.domain.Amount;
+import lotto.domain.BuyerLotto;
+import lotto.domain.Lotto;
+import lotto.domain.LottoShop;
+import lotto.domain.RandomNumberGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
+
+    public void run() {
+        LottoShop lottoShop = setting();
+        createBuyerLotto(lottoShop);
+    }
+
+    private LottoShop setting() {
+        return new LottoShop(new RandomNumberGenerator());
+    }
+
+    private BuyerLotto createBuyerLotto(LottoShop lottoShop) {
+        Amount amount = generateAmount();
+        List<Lotto> createBuyerLotto = lottoShop.createByBuyerLotto(amount);
+        OutputView.printBuyerLotto(createBuyerLotto.size(), createBuyerLotto);
+        return new BuyerLotto(createBuyerLotto);
+    }
 
     private Amount generateAmount() {
         int amount = InputView.readPurchaseAmount();

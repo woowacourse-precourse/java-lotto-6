1e62561aa341fa5b68f85871e54f5da004c7fac6
YongHyeon <pyh4616@naver.com>
2023-11-03 01:57:37 +0900

    Feat(Convert 클래스): 문자열을 정수형 리스트로 변환하는 기능을 stringToIntegerList 메서드로 구현

--- src/main/java/lotto/util/Convert.java
+++ src/main/java/lotto/util/Convert.java
@@ -1,8 +1,18 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Convert {
 
     public static Integer stringToInteger(String inputValue) {
         return Integer.parseInt(inputValue);
+    }
+
+    public static List<Integer> stringToIntegerList(String inputValue) {
+        return Arrays.stream(inputValue.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

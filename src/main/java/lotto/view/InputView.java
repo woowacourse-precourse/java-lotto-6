fc8c41657716b6d004e027935f4c9db6fd92f748
heiler <threepebbles@naver.com>
2023-11-03 07:13:01 +0900

    Refactor: 정적 팩토리 메소드 패턴 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,15 +5,15 @@ public class InputView {
     public static final String ENTER_WINNING_LOTTO_TICKET = "당첨 번호를 입력해 주세요.";
     public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String enterBudgetMessage() {
+    public String enterBudgetMessage() {
         return ENTER_BUDGET_MESSAGE;
     }
 
-    public static String enterWinningLottoTicket() {
+    public String enterWinningLottoTicket() {
         return ENTER_WINNING_LOTTO_TICKET;
     }
 
-    public static String enterBonusNumber() {
+    public String enterBonusNumber() {
         return ENTER_BONUS_NUMBER;
     }
 }

e7b11884a75f8e0baf729add984f1752240d591b
hwan2-99 <thh1233123@gmail.com>
2023-11-02 21:59:57 +0900

    refactor: 예외 발생시 "[ERROR]"로 시작하는 에러 메시지를 출력하도록 변경

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,9 +1,10 @@
 package lotto.constant;
 
 public enum ErrorMessage {
-    INPUT_NUMBER_ERROR("숫자만 입력 가능합니다."),
-    INSUFFICIENT_PRICE_MESSAGE("로또를 사기에 돈이 부족합니다."),
-    DIVISIBILITY_CHECK_AMOUNT("1,000원으로 나누어떨어지지 않습니다.");
+    ERROR("[ERROR]"),
+    INPUT_NUMBER_ERROR(ERROR.Message + "숫자만 입력 가능합니다."),
+    INSUFFICIENT_PRICE_MESSAGE(ERROR.Message + "로또를 사기에 돈이 부족합니다."),
+    DIVISIBILITY_CHECK_AMOUNT(ERROR.Message + "1,000원으로 나누어떨어지지 않습니다.");
 
     private final String Message;
 

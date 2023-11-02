87bb5acb09d395f2a0d41573c85c34d753840d66
정민규 <fastdodge7@naver.com>
2023-11-02 18:20:12 +0900

    test(LottoTest): Lotto 클래스의 toString() 메소드 테스트 추가
    Lotto의 toString() 메소드가 적절히 작동하는지 테스트하는 코드가 추가되었습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,6 +22,13 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 객체의 정보를 정상적으로 표시하는지 테스트합니다..")
+    @Test
+    void toString_테스트() {
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isEqualTo("[1, 2, 3, 4, 5, 6, 7]");
     }
 
     // 아래에 추가 테스트 작성 가능

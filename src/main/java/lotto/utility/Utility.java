fc916effca56b92ca3bd272b762afd7032e6acce
seungtoctoc <dltmxor31@naver.com>
2023-11-02 20:35:33 +0900

    feat, refactor: 문자열을 반점으로 나누는 기능 구현 및 메서드명 변경 문자열을 반점을 기준으로 나누는 기능을 구현했습니다. getIntFromString 메서드의 이름을 getNumber로 변경하였고, 매개변수의 이름도 input으로 바꿔주었습니다.

--- src/main/java/lotto/utility/Utility.java
+++ src/main/java/lotto/utility/Utility.java
@@ -1,12 +1,20 @@
 package lotto.utility;
 
+import java.util.List;
+
 public class Utility {
-    public int getIntFromString(String Input) {
+    public int getNumber(String input) {
         try {
-            int number = Integer.parseInt(Input);
+            int number = Integer.parseInt(input);
             return number;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력값이 정수가 아닙니다");
         }
+    }
+
+    public String[] splitInput (String input) {
+        String[] numbers = input.split(",");
+
+        return numbers;
     }
 }

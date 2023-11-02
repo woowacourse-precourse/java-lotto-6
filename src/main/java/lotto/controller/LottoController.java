332f3495fd20f0f5a1ec6045c2761bd410775631
uhanuu <hwryu990927@gmail.com>
2023-11-03 01:34:59 +0900

    feat: lotto 일급 컬렉션

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,14 @@
+package lotto.controller;
+
+import lotto.view.input.LottoBuyInputView;
+
+public class LottoController {
+
+    private final LottoBuyInputView lottoBuyInputView;
+
+    public LottoController(LottoBuyInputView lottoBuyInputView) {
+        this.lottoBuyInputView = lottoBuyInputView;
+    }
+
+
+}
--- src/main/java/lotto/domain/shop/LottoShop.java
+++
@@ -1,4 +0,0 @@
-package lotto.domain.shop;
-
-public class LottoShop {
-}

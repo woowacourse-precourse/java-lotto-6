c1c1b439d7431519377423fed9278da995017f0c
seungtoctoc <dltmxor31@naver.com>
2023-11-02 20:24:52 +0900

    refactor+feat: getPayment메서드 변수명 변경 및 당첨 번호를 입력받는 기능 구현 getPayment메서드 안의 입력값을 뜻하는 변수명을 paymentInput으로 변경하였습니다. 당첨번호를 입력받는 기능을 구현했습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,8 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public String getPayment() {
         System.out.println("구입 금액을 입력해주세요.");
-        String input = Console.readLine();
+        String paymentInput = Console.readLine();
 
-        return input;
+        return paymentInput;
+    }
+
+    public String getNumbers() {
+        System.out.println("당첨 번호를 입력해주세요.");
+        String numbersInput = Console.readLine();
+
+        return numbersInput;
     }
 }

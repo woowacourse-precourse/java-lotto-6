3f9f3c75ae5c9c274a38d6d6035258156e1fe611
jongwonpc <c3392302@naver.com>
2023-11-03 08:28:04 +0900

    feat: 입력 받은 당첨 번호 검증 구현

---
+++ src/main/java/lotto/valid/WinNumberValid.java
@@ -0,0 +1,44 @@
+package lotto.valid;
+
+import java.util.List;
+import lotto.util.ConvertStr;
+
+public class WinNumberValid {
+
+    public static List<Integer> validWinNumber(String input) {
+        List<String> nums = ConvertStr.splitByComma(input);
+        validOnlyNum(nums);
+        List<Integer> intNums = ConvertStr.strListToIntList(nums);
+        validWinNumberSize(intNums);
+        validWinNumberDistinctSize(intNums);
+        validWinNumberValue(intNums);
+        return intNums;
+    }
+
+    private static void validOnlyNum(List<String> nums) {
+        String reg = "^[0-9]*$";
+        for (String number : nums) {
+            if (!number.matches(reg)) {
+                throw new IllegalArgumentException("[ERROR] ,로 구별된 문자들은 숫자만 입력 가능합니다.");
+            }
+        }
+    }
+
+    private static void validWinNumberSize(List<Integer> intNums) {
+        if (intNums.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private static void validWinNumberDistinctSize(List<Integer> intNums) {
+        if (intNums.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 6개 입력해야 합니다.");
+        }
+    }
+
+    private static void validWinNumberValue(List<Integer> intNums) {
+        if (!intNums.stream().allMatch(num -> num >= 1 && num <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 모든 숫자는 1과 45 사이여야 합니다.");
+        }
+    }
+}

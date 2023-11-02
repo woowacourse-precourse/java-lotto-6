e81b75efe00815296c412fdef406200314ccd58c
heukwu <yoo4068@gmail.com>
2023-11-02 20:50:01 +0900

    Feat: 당첨 번호 생성

---
+++ src/main/java/lotto/Const.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class Const {
+    public static final int winningRangeStartNumber = 1;
+    public static final int winningRangeEndNumber = 45;
+    public static final int LottoCount = 6;
+
+}
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateValidate(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +21,21 @@ public class Lotto {
         }
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     // TODO: 추가 기능 구현
+    public static Lotto createWinningNumbers() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(Const.winningRangeStartNumber, Const.winningRangeEndNumber, Const.LottoCount));
+    }
+
+    private void duplicateValidate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!nonDuplicateNumbers.add(number)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }

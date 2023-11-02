40a374bb97dbe248bf48206a65e17a0c732df363
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 05:17:23 +0900

    feat(view): 구매한 로또 수량 및 번호를 출력한다.
    - 사용자의 로또가 들어있는 리스트를 인자로 받아 구매 수량과 로또 번호를 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,23 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
 
     private static final String NEW_LINE = "\n";
-    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
+    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s"+NEW_LINE;
+    private static final String PURCHASE_QUANTITY_MESSAGE_FORMAT = NEW_LINE+"%d개를 구매했습니다."+NEW_LINE;
 
     public static void errorMessage(IllegalArgumentException e) {
-        System.out.printf(ERROR_MESSAGE_FORMAT+NEW_LINE,e.getMessage());
+        System.out.printf(ERROR_MESSAGE_FORMAT,e.getMessage());
+    }
+
+    public static void quantityAndLottoNumbers(List<Lotto> userLottos) {
+        System.out.printf(PURCHASE_QUANTITY_MESSAGE_FORMAT,userLottos.size());
+        for (Lotto lotto : userLottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

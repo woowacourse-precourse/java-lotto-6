4270efefffd3c3ea77a2dd22bea33e5bbbf4c13c
kwondongwook <kwondongwook1@gmail.com>
2023-11-02 22:23:59 +0900

    feat(로또): 중복되지 않는 6개의 번호

---
+++ src/main/java/lotto/domain/number/Lotto.java
@@ -0,0 +1,38 @@
+package lotto.domain.number;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final String INVALID_SIZE = "로또 번호의 개수는 6개여야 합니다.";
+    private static final String DUPLICATES_FOUND = "로또 번호에는 중복된 숫자가 없어야 합니다.";
+    private static final int SIZE = 6;
+    private final Set<Number> numbers;
+
+    public Lotto(Set<Number> numbers) {
+        validateSize(numbers);
+        this.numbers = Set.copyOf(numbers);
+    }
+
+    public static Lotto of(List<Number> numbers) {
+        validateSize(numbers);
+        validateNoDuplicates(numbers);
+        Set<Number> numberSet = new HashSet<>(numbers);
+        return new Lotto(numberSet);
+    }
+
+    private static void validateSize(Collection<Number> numbers) {
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException(INVALID_SIZE);
+        }
+    }
+
+    private static void validateNoDuplicates(List<Number> numbers) {
+        Set<Number> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() < numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATES_FOUND);
+        }
+    }
+}

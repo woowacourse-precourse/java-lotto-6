1e6e768b4c139ad2d82dd3817713def5e6c2f32c
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-02 23:31:02 +0900

    feat: Money를 관리하는 클래스 추가
    입력으로 들어온 money가 숫자인지 체크
    입력으로 들어온 money의 범위가 1000원 단위인지 체크
    이를 어길시 예외 발생

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,62 @@
+package lotto.domain;
+
+import java.util.Objects;
+import lotto.exception.InvalidMoneyRangeException;
+import lotto.exception.NonPositiveIntException;
+
+public class Money {
+
+    private static final int LOTTO_COST = 1000;
+    private static final int ZERO_VALUE = 0;
+
+    private final int amount;
+
+    private Money(final int amount) {
+        this.amount = amount;
+    }
+
+    public static Money from(final String amount) {
+        validateNumericValue(amount);
+        int value = Integer.parseInt(amount);
+        validateRange(value);
+        return new Money(value);
+    }
+
+    private static void validateNumericValue(final String value) {
+        if (!isNumeric(value)) {
+            throw new NonPositiveIntException();
+        }
+    }
+
+    private static boolean isNumeric(final String value) {
+        return value.chars()
+                .allMatch(Character::isDigit);
+    }
+
+    private static void validateRange(final int value) {
+        if (isInValidRange(value)) {
+            throw new InvalidMoneyRangeException();
+        }
+    }
+
+    private static boolean isInValidRange(final int value) {
+        return value % LOTTO_COST != ZERO_VALUE || value < LOTTO_COST;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}

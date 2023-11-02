c0fcc1224eeaba4247c23ede1bd7d6802c703353
정민규 <fastdodge7@naver.com>
2023-11-03 02:14:16 +0900

    feat: 로또 당첨 통계 데이터를 저장하는 기능 추가
    로또 당첨 통계(개별 당첨 횟수, 수익률)을 저장하는 기능이 추가되었습니다.

---
+++ src/main/java/lotto/LottoStatistics.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoStatistics {
+    private Map<LottoResult, Integer> statistics;
+    private Float returnRate;
+
+    public LottoStatistics(Integer totalCost) {
+        statistics = Map.of(
+                LottoResult.OTHER, 0,
+                LottoResult.MATCH3, 0,
+                LottoResult.MATCH4, 0,
+                LottoResult.MATCH5, 0,
+                LottoResult.MATCH5_AND_BONUS, 0,
+                LottoResult.MATCH6, 0
+        );
+    }
+
+    public void calcStatistics(List<Lotto> lottoList, List<Integer> winningNumberList, Integer bonusNumber) {
+        for (Lotto lotto : lottoList) {
+            LottoResult result = lotto.getLottoResult(winningNumberList, bonusNumber);
+            statistics.put(result, statistics.get(result) + 1);
+        }
+        returnRate = calcReturnRate(lottoList.size() * 1000);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder msgBuilder = new StringBuilder("");
+        msgBuilder.append("3개 일치 (5,000원) - ")
+                .append(statistics.get(LottoResult.MATCH3)).append("개");
+        msgBuilder.append("4개 일치 (50,000원) - ")
+                .append(statistics.get(LottoResult.MATCH3)).append("개");
+        msgBuilder.append("5개 일치 (1,500,000원) - ")
+                .append(statistics.get(LottoResult.MATCH3)).append("개");
+        msgBuilder.append("5개 일치, 보너스 볼 일치 (30,000,000원) - ")
+                .append(statistics.get(LottoResult.MATCH3)).append("개");
+        msgBuilder.append("6개 일치 (2,000,000,000원) - ")
+                .append(statistics.get(LottoResult.MATCH3)).append("개");
+        msgBuilder.append("총 수익률은 ").append(returnRate).append("%입니다.");
+        return msgBuilder.toString();
+    }
+
+    private Float calcReturnRate(Integer totalCost) {
+        Integer sum = 0;
+        for (LottoResult key : statistics.keySet()) {
+            sum += statistics.get(key) * key.getPrize();
+        }
+        return Math.round(sum.floatValue() / totalCost * 1000) / 10.0f;
+    }
+}

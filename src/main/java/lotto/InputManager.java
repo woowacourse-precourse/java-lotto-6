d9e2b9a181caf02171e3341ae32099db6dac9eed
ImWonYong <ssassb123@daum.net>
2023-11-03 01:03:45 +0900

    feat(InputManager): 유저에게 입력을 받는다.

--- src/main/java/lotto/InputManager.java
+++ src/main/java/lotto/InputManager.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputManager {
 
+    public static String receiveUserInput() {
+        return removeInputSpace(Console.readLine());
+    }
+
     public static String removeInputSpace(String input) {
         return input.replaceAll(" ", "");
     }

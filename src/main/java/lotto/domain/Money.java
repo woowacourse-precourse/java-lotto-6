9200caca43526c65f162d5a3db149146d5ba16c8
LastCow9000 <sys19912002@hanmail.net>
2023-11-03 06:22:36 +0900

    feat: money nan 검증 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,17 +2,29 @@ package lotto.domain;
 
 public class Money {
     private static final String NULL_OR_EMPTY_ERROR_MESSAGE = "[ERROR] 공백이나 빈 값으로는 로또 구매가 불가능합니다.";
-    
+    private static final String NAN_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력이 불가능합니다.";
     private final int amount;
 
     public Money(String amount) {
         validateNullOrEmpty(amount);
-        this.amount = Integer.parseInt(amount);
+        int parsedAmount = toInt(amount);
+        this.amount = parsedAmount;
     }
 
     private void validateNullOrEmpty(String amount) {
         if (amount == null || amount.isBlank()) {
             throw new IllegalArgumentException(NULL_OR_EMPTY_ERROR_MESSAGE);
         }
+    }
+
+    private int toInt(String amount) {
+        int result;
+        try {
+            result = Integer.parseInt(amount);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(NAN_ERROR_MESSAGE);
+        }
+
+        return result;
     }
 }

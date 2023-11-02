2a8ee5e38128a086dbdad6fac32b1b9b0d15fb2d
donghyunkim <ghffu405@gmail.com>
2023-11-02 19:34:04 +0900

    feat: 당첨결과 찾는 메소드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,17 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.dto.AnswerLotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoService {
+
+    public List<Rank> findWinningResult(List<Lotto> lottos, AnswerLotto answerLotto){
+        return lottos.stream()
+                .map(lotto -> lotto.calculateLottoWinnings(answerLotto))
+                .collect(Collectors.toList());
+    }
 }

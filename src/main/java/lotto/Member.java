2381e446b475f1dd4bce798e050c8a222e7aee2e
pyomin4973 <hyomin8918@naver.com>
2023-11-02 21:43:18 +0900

    Feat(UI): 프로그램 입출력 관련 기능 추가
    - 구입금액 입력받기
    - 구매 수량 출력하기
    - 구매한 로또(들)의 번호 출력하기

---
+++ src/main/java/lotto/Member.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import java.util.List;
+
+public class Member {
+    int price;
+    List<Lotto> memberLotto;
+
+    public Member() {
+    }
+    public void setPrice(String prices){
+
+    }
+}
---
+++ src/main/java/lotto/UI.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.List;
+
+public class UI {
+    Member member=new Member();
+    public UI() {
+    }
+
+    public void priceInput(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String price=camp.nextstep.edu.missionutils.Console.readLine();
+
+        member.setPrice(price);
+    }
+
+    public void purchaseCount(int count){
+        String purchase=count+"개를 구매했습니다.";
+        System.out.println(purchase);
+    }
+
+    public void lottoOutput(List<Lotto> memberLotto){
+
+        for(int i=0;i<memberLotto.size();i++){
+            System.out.println(memberLotto.get(i));
+        }
+
+        System.out.println();
+    }
+}

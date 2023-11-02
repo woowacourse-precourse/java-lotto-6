388f7e4d6d353074334fe1cd4447e60e87ef722a
FriedHamm <kwh2716@gmail.com>
2023-11-02 19:56:05 +0900

    Feat: 유저에게 보너스 숫자를 입력하라고 요청하는 기능 추가

--- src/main/java/view/Mainview.java
+++ src/main/java/view/Mainview.java
@@ -10,4 +10,9 @@ public class Mainview {
     {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public void requestBonusNumber()
+    {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }

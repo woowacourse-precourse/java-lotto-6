de10e82339b7c49fa111c120df42d2ec4518a263
WOONBE <kepull2918@naver.com>
2023-11-02 18:16:09 +0900

    [rename] OutputView의 메서드 이름을 더 구체적으로 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,14 +2,14 @@ package lotto.view;
 
 public class OutputView {
 
-    public static void inputAmount() {
+    public static void requestInputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-    public static void inputWinningNumbers() {
+    public static void requestInputWinningNumbers() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
-    public static void inputBonusNumber() {
+    public static void requestInputBonusNumber() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 

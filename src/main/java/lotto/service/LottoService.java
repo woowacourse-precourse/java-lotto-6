d40e4d59aeb5316239a01b718af877f1e12f0ba3
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-02 19:53:53 +0900

    feat: WinningLotto를 생성하는 메서드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,11 +1,18 @@
 package lotto.service;
 
 import lotto.domain.Player;
+import lotto.domain.WinningLotto;
+
+import java.util.List;
 
 public class LottoService {
 
     public Player createPlayer(int purchaseAmount) {
         return new Player(purchaseAmount);
+    }
+
+    public WinningLotto createWinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        return new WinningLotto(winningNumbers, bonusNumber);
     }
 
 

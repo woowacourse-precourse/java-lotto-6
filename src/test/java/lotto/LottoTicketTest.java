f34a39a87013da62dee4cf59c0d4ceb8a0fc9e43
minjun7410 <minjun7410@gmail.com>
2023-11-02 23:06:44 +0900

    test(LottoTicketTest): 1000원으로 나누어떨어지지 않는 입력값 예외처리 테스트 추가
    * 1000원으로 나누어떨어지지 않는 값을 예외처리하는 테스트 코드를 추가했다.

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.LottoTicket;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,10 +8,15 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTicketTest {
-    @Test
     @ParameterizedTest
-    @ValueSource(strings = {"d", "12d", "__", "*"})
+    @ValueSource(strings = {"d", "12d", "__", "*", "", " ", "10.1"})
     void 입력값_숫자_아닌값_예외처리_테스트(String input) {
+        assertThatThrownBy(() -> new LottoTicket(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10001", "0"})
+    void 나누어떨어지지_않는값_예외처리_테스트(String input) {
         assertThatThrownBy(() -> new LottoTicket(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }

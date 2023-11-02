4c8d3847da5d555c6e3993766b49aad809ecc361
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-02 22:35:41 +0900

    feat: 잘못된 로또 번호 생성으로 예외 발생 시, 에러 메세지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,19 +14,19 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 여야 합니다.");
         }
     }
 
     private void validateDuplicate(List<Integer> numbers) {
         if (numbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 
     private void validateRange(List<Integer> numbers) {
         if (numbers.stream().filter(n -> (n >= 1) && (n <= 45)).count() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 

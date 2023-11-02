1a6b40c8e1ccc3990afd7494000748f5e1b7d7e0
진환 정 <joung.jinhawn@gmail.com>
2023-11-02 17:40:16 +0900

    feat(StartLotto.java) : 로또 구입 금액 입력 파트 구현

--- src/main/java/lotto/StartLotto.java
+++ src/main/java/lotto/StartLotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
@@ -14,5 +15,29 @@ public class StartLotto {
         this.purchasePrice = 0;
         this.bonusNumber = 0;
     }
-    
+
+    private void printMessageForInputPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    private void inputPurchasePrice() {
+        String purchasePrice = Console.readLine();
+
+        checkIsInteger(purchasePrice);
+        checkIs1000wonUnit();
+    }
+
+    private void checkIsInteger(String purchasePrice) {
+        try{
+            this.purchasePrice = Integer.parseInt(purchasePrice);
+        } catch(NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+    }
+
+    private void checkIs1000wonUnit() {
+        if(this.purchasePrice % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

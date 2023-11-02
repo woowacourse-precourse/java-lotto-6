4d304139f53f0edf219d3e83a894edf0c3de289b
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-02 19:55:44 +0900

    [feat] view output 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,19 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
+    public static void printLottoNumbers (String numbers) {
+        System.out.println(numbers);
+    }
+
+    public static void printLottoResult (int sameThree, int sameFour, int sameFive, int sameFiveBonus, int sameAll, int result) {
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - "+sameThree+"개");
+        System.out.println("4개 일치 (50,000원) - "+sameFour+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+sameFive+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+sameFiveBonus+"개");
+        System.out.println("6개 일치 (200,000,000원) - "+sameAll+"개");
+        System.out.println("총 수익률은 "+result+"입니다.");
+    }
 }

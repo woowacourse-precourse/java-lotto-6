e8b033f642511fe23629a8f76b83c9d9df33d11c
donghyunkim <ghffu405@gmail.com>
2023-11-02 19:29:26 +0900

    feat: 당첨 구하는 메소드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.dto.AnswerLotto;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -27,6 +29,27 @@ public class Lotto {
             throw new IllegalArgumentException("로또 번호들은 중복 될 수 없습니다.");
         }
 
+    }
+
+    public Rank calculateLottoWinnings(AnswerLotto answerLotto){
+        List<Integer> answerNumbers = answerLotto.getAnswerNumbers();
+        int bonusNumber = answerLotto.getBonusNumber();
+        int matchedNumber = 0;
+        boolean hasBonusNumber = false;
+
+        for(int i=0; i<LOTTO_NUMBERS_SIZE; i++){
+            for(int j=0; j<LOTTO_NUMBERS_SIZE; j++){
+                if(numbers.get(i) == answerNumbers.get(j)){
+                    matchedNumber++;
+                }
+            }
+        }
+
+        if(matchedNumber == 5){
+            hasBonusNumber = numbers.stream()
+                    .anyMatch(number -> number == bonusNumber);
+        }
+        return Rank.findRankByMatchedNBonusNumber(matchedNumber, hasBonusNumber);
     }
 
     public List<Integer> getNumbers() { //메소드 확인용 지워야됨

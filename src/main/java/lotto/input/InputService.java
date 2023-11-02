c8f19be0c8918a5956eb895380bce6e3120e5fd3
cousim46 <wjdghldns24@naver.com>
2023-11-02 21:15:59 +0900

    feat : 당첨번호 입력 메서드 추가

---
+++ src/main/java/lotto/input/InputService.java
@@ -0,0 +1,9 @@
+package lotto.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputService {
+    public static String winNumber() {
+        return Console.readLine();
+    }
+}

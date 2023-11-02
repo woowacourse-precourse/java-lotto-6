b526cc5a6c87d6f2011b533f0a2e21cd184a48fb
ddolboghi <rhdfyd128@gmail.com>
2023-11-03 01:53:53 +0900

    feat: CLI 입출력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readLine() {
+        return Console.readLine().trim();
+    }
+
+    public void close() {
+        Console.close();
+    }
+}

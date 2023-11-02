c3556129afd66729f17a97299ffe8e8d2d05a13b
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-02 20:16:35 +0900

    기능 구현 목록 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +18,7 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public static int count (Lotto answers) {
+
+    }
 }

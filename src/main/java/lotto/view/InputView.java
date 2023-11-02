bce878128e34ffa6fb7bd6434cdf6e6c3e0194c9
minseo  jo <liging12@naver.com>
2023-11-03 01:38:35 +0900

    feat: 입력 기능 추가 (InputView)
    사용자에게 요청 메시지를 보내고, 요청에 대한 응답 값을 가져오는 기능
    - 로또 구매 금액
    - 당첨 번호 (복수)
    - 보너스 번호

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputLottoPurchaseAmount() {
+        return Console.readLine();
+    }
+
+    public String inputWinningNumbers() {
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        return Console.readLine();
+    }
+}

f8133307232b8545652469499f7d5a987415b690
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-11-02 23:14:32 +0900

    feat: 설정 변수 추가

---
+++ src/main/java/lotto/GameConfig.java
@@ -0,0 +1,31 @@
+package lotto;
+
+public class GameConfig {
+    public final static int MIN_LOTTO = 1;
+    public final static int MAX_LOTTO = 45;
+    public final static int LOTTO_NUMBER = 6;
+    public final static String SEPARATOR = ",";
+    public enum WINNING{
+        FIRST(2_000_000_000, 1),
+        SECOND(30_000_000, 2),
+        THIRD(1_500_000, 3),
+        FOURTH(50_000, 4),
+        FIFTH(5_000, 5);
+
+        private final int price;
+        private final int rank;
+
+        WINNING(int price, int rank){
+            this.price = price;
+            this.rank = rank;
+        }
+
+        public int getPrice(){
+            return price;
+        }
+
+        public int getRank(){
+            return rank;
+        }
+    }
+}

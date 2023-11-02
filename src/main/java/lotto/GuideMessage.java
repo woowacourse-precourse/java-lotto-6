711942ee5d4e9e11439826d52154bcb5fa81b9bc
kchranger <kchranger@gmail.com>
2023-11-02 23:42:58 +0900

    refactor(name) : separate message type by changing constant variable name
    to clarfiy message type

--- src/main/java/lotto/GuideMessage.java
+++ src/main/java/lotto/GuideMessage.java
@@ -2,10 +2,10 @@ package lotto;
 
 public class GuideMessage{
 
-    private static final String MSG_INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String GUIDE_MSG_INPUT_MONEY = "구입금액을 입력해 주세요.";
 
     public static void ofInputMoney(){
-        System.out.println(MSG_INPUT_MONEY);
+        System.out.println(GUIDE_MSG_INPUT_MONEY);
     }
 
 }

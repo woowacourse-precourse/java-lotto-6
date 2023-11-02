584dd350dd9c0b1de08b80fc115f25dd448f58c9
Firedrago95 <yonghwa95@naver.com>
2023-11-02 17:04:21 +0900

    test(ValidatorTest) : 1000의 배수 유효성 검사 테스트 코드 작성

--- src/test/java/lotto/Util/ValidatorTest.java
+++ src/test/java/lotto/Util/ValidatorTest.java
@@ -1,5 +1,6 @@
 package lotto.Util;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,13 +14,30 @@ public class ValidatorTest {
     public void isNullOrEmptyTest() {
         String nullValue = null;
         String empty = "";
-
+        
+        //null 입력 예외 발생
         Throwable nullException = assertThrows(IllegalArgumentException.class,
             () -> Validator.validateNullOrEmpty(nullValue));
         assertTrue(nullException.getMessage().contains(Validator.NULL_OR_EMPTY_MESSAGE));
-
+        // 빈 문자열 예외 발생
         Throwable emptyException = assertThrows(IllegalArgumentException.class,
             () -> Validator.validateNullOrEmpty(empty));
         assertTrue(emptyException.getMessage().contains(Validator.NULL_OR_EMPTY_MESSAGE));
+    }
+
+    @Test
+    @DisplayName("1000 단위 숫자 유효성 검사 테스트")
+    public void validateBudgetInThousandsTest() {
+        String invalidInput = "19400";
+        String validInput = "19000";
+
+        // 유효하지 않은 입력에 대한 테스트
+        Throwable invalidBudgetException = assertThrows(IllegalArgumentException.class,
+            () -> Validator.validateBudgetInThousands(invalidInput));
+        assertTrue(
+            invalidBudgetException.getMessage().contains(Validator.THOUSANDS_MULTIPLE_MESSAGE));
+
+        // 유효한 입력에 대한 테스트
+        assertDoesNotThrow(() -> Validator.validateBudgetInThousands(validInput));
     }
 }

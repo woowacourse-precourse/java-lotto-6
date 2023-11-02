59f0edb53ab4c632e2d790ed349335f83bae6614
heiler <threepebbles@naver.com>
2023-11-03 07:23:30 +0900

    Test: Lotto 객체 생성 시 숫자 범위가 1~45인지 검증하는 로직 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -23,4 +23,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1~45 범위가 아닌 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNumberInRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

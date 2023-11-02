73034c392af2d240634bae55a2ad7dc56cdaea94
leeedohyun <ldh19738@gmail.com>
2023-11-02 23:44:43 +0900

    test: 당첨 번호의 개수 예외 테스트

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -9,7 +9,7 @@ class WinningNumbersTest {
     @Test
     public void 당첨_번호가_1보다_작으면_예외가_발생한다() {
         // given
-        List<Integer> winningNumbers = List.of(0, 1, 2, 3, 4, 5);
+        final List<Integer> winningNumbers = List.of(0, 1, 2, 3, 4, 5);
 
         // then
         Assertions.assertThatThrownBy(() -> new WinningNumbers(winningNumbers))
@@ -19,7 +19,27 @@ class WinningNumbersTest {
     @Test
     public void 당첨_번호가_45보다_크면_예외가_발생한다() {
         // given
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 46);
+        final List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 46);
+
+        // then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호의_개수가_6개보다_작으면_예외가_발생한다() {
+        // given
+        final List<Integer> winningNumbers = List.of(1, 2, 3);
+
+        // then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호의_개수가_6개보다_크면_예외가_발생한다() {
+        // given
+        final List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
 
         // then
         Assertions.assertThatThrownBy(() -> new WinningNumbers(winningNumbers))

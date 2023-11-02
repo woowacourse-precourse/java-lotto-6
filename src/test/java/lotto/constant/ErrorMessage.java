9061357dd1b34c76a5671bbfa602c1ce44255d8a
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-03 03:18:23 +0900

    feat: 에러 메세지 추가

---
+++ src/test/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,19 @@
+package lotto.constant;
+
+public class ErrorMessage {
+
+    public static final String PURCHASE_WRONG
+            = "[ERROR] 구입금액은 1000원 단위로 입력해야합니다.";
+
+    public static final String WINNING_NUMBER_SIZE
+            = "[ERROR] 당첨번호는 6자리가 넘어갈 수 없습니다.";
+
+    public static final String WINNING_NUMBER_DUPLICATE
+            = "[ERROR] 당첨번호는 중복될 수 없습니다.";
+
+    public static final String WINNING_NUMBER_RANGE
+            = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    public static final String BONUS_NUMBER_RANGE
+            = "[ERROR] 보너스번호는 1부터 45 사이의 숫자여야 합니다.";
+}

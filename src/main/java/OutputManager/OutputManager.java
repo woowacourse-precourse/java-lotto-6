3f0020bf35a98b48ab3ec5e88f57262f6f6722f9
정민규 <fastdodge7@naver.com>
2023-11-03 03:08:07 +0900

    fix(OutputManager): 메소드 접근제어자 수정

--- src/main/java/OutputManager/OutputManager.java
+++ src/main/java/OutputManager/OutputManager.java
@@ -5,14 +5,14 @@ import lotto.Lotto;
 import lotto.LottoStatistics;
 
 public class OutputManager {
-    void printLottoData(List<Lotto> lottoList) {
+    public static void printLottoData(List<Lotto> lottoList) {
         System.out.println(lottoList.size() + "개를 구매했습니다.");
         for(Lotto lotto : lottoList) {
             System.out.println(lotto);
         }
     }
 
-    void printTotalResult(LottoStatistics totalResult) {
+    public static void printTotalResult(LottoStatistics totalResult) {
         System.out.println(totalResult);
     }
 }

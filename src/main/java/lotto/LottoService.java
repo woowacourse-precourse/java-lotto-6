4d623d8e3f2a2a5d6b35a2518398ba559ae4143b
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-02 21:39:28 +0900

    feat : 유저가 입력한 구입금액에 대한 유형, 단위, 범위의 예외 기능 추가

---
+++ src/main/java/lotto/LottoService.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import java.util.regex.Pattern;
+
+public class LottoService {
+    private static final String BUY_MONEY_TYPE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 자연수를 입력해야 합니다.";
+    private static final String BUY_MONEY_UNIT_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 1000원 단위의 금액을 입력해야 합니다.";
+    private static final String BUY_MONEY_RANGE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 0원보다 큰 금액을 입력해야 합니다.";
+    private static final Pattern NUMBER_MATCH = Pattern.compile("^[0-9]*$");
+    private static final int LOTTO_PRICE = 1000;
+    private static final int ZERO = 0;
+
+    public void validateBuyMoney(String inputMoney) {
+        validateBuyMoneyType(inputMoney);
+        validateBuyMoneyRange(inputMoney);
+        validateBuyMoneyUnit(inputMoney);
+    }
+
+    private void validateBuyMoneyType(String inputMoney) {
+        if (!NUMBER_MATCH.matcher(inputMoney).matches())
+            throw new IllegalArgumentException(BUY_MONEY_TYPE_EXCEPTION_MESSAGE);
+    }
+
+    private void validateBuyMoneyRange(String inputMoney) {
+        if (toInt(inputMoney) == ZERO)
+            throw new IllegalArgumentException(BUY_MONEY_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    private void validateBuyMoneyUnit(String inputMoney) {
+        if (toInt(inputMoney) % LOTTO_PRICE != ZERO)
+            throw new IllegalArgumentException(BUY_MONEY_UNIT_EXCEPTION_MESSAGE);
+    }
+
+    private int toInt(String inputMoney) {
+        return Integer.parseInt(inputMoney);
+    }
+}

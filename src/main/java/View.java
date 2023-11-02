116d74a381a19540e2435fdde445120108a207ef
Genius-PAK <pyj789123@gmail.com>
2023-11-02 17:04:18 +0900

    feat(View): 구입금액, 로또번호, 보너스 번호를 입력받는 메서드 추가

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,3 +1,5 @@
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class View {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요. ";
     private static final String LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요. ";
@@ -5,19 +7,35 @@ public class View {
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계\n---";
 
 
-    public void display_purchase_amount_message() {
+    public void displayPurchaseAmountMessage() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
     }
 
-    public void display_lotto_number_message() {
+    public void displayLottoNumberMessage() {
         System.out.println(LOTTO_NUMBER_MESSAGE);
     }
 
-    public void display_bonus_number_message() {
+    public void displayBonusNumberMessage() {
         System.out.println(BONUS_NUMBER_MESSAGE);
     }
 
-    public void display_winning_statistics_message() {
+    public void displayWinningStatisticsMessage() {
         System.out.println(WINNING_STATISTICS_MESSAGE);
+    }
+
+    private String getUserInput() {
+        return readLine();
+    }
+
+    public String getPurchaseAmount() {
+        return getUserInput();
+    }
+
+    public String getLottoNumbers() {
+        return getUserInput();
+    }
+
+    public String getBonusNumber() {
+        return getUserInput();
     }
 }

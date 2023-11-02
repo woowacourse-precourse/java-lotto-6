2fafe2a960ca7c4224a35df34956bed2a91d826c
이종경 <gyoung3063413@naver.com>
2023-11-02 19:22:12 +0900

    [추가] 사용자가 입력한 값이 숫자가 맞는지 확인하는 메서드

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -8,7 +8,17 @@ public class BuyLotto {
 	public void getMoney() {
 		System.out.println("구입 금액을 입력해주세요.");
 		String userInput = readLine().trim();
+		this.isValidInput(userInput);
 //		System.out.println(userInput);
 	}
-	
+
+	private void isValidInput(String input) {
+		int userInput;
+		try {
+			userInput = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			System.err.println("[ERROR] 금액은 숫자만 입력이 가능합니다.");
+			this.getMoney();
+		}
+	}
 }

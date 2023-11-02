9ca9264dea9c3e7fcd83c4c6b3fc0bede9002876
KimDabomi <cdabomi@nate.com>
2023-11-02 22:50:27 +0900

    feat: Check the bonus number

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import com.sun.security.auth.NTSidPrimaryGroupPrincipal;
 import java.util.List;
 import lotto.exception.LottoException;
 
@@ -13,5 +14,9 @@ public class Lotto {
 
     public String sortNumbers() {
         return numbers.stream().sorted().map(String::valueOf).reduce((a,b) -> a + ", " + b).orElse("");
+    }
+
+    public boolean hasBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
     }
 }

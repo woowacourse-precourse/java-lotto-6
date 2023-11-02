ba3a3448affc0e108e921c8b4f8bbc78c593c58f
정민규 <fastdodge7@naver.com>
2023-11-03 03:03:52 +0900

    feat: main 함수 구현
    이제 프로그램을 실행시켜 볼 수 있습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import InputManager.InputManager;
+import OutputManager.OutputManager;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Integer totalCost = InputManager.inputCost();
+        List<Integer> winningNumberList = InputManager.inputWinningNumbers();
+        Integer bonusNumber = InputManager.inputBonusNumber(winningNumberList);
+
+        List<Lotto> lottoList = LottoFactory.buyLotto(totalCost);
+        OutputManager.printLottoData(lottoList);
+
+        LottoStatistics result = new LottoStatistics();
+        result.calcStatistics(lottoList, winningNumberList, bonusNumber);
+        OutputManager.printTotalResult(result);
     }
 }

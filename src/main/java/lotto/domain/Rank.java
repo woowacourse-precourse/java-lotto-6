48935539e0a1e98fa31c07e2a29a867af282a3a0
donghyunkim <ghffu405@gmail.com>
2023-11-02 18:20:06 +0900

    feat: Rank Enum타입 생성

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,32 @@
 package lotto.domain;
 
 public enum Rank {
+
+    FIRST_PLACE(6, false, 2000000000),
+    SECOND_PLACE(5, true,30000000),
+    THIRD_PLACE(5, false,1500000),
+    FOURTH_PLACE(4,false,50000),
+    FIFTH_PLACE(3,false,5000);
+
+    private int matchedNumber;
+    private boolean hasBonusNumber;
+    private int winningPrice;
+
+    Rank(int matchedNumber, boolean hasBonusNumber, int winningPrice) {
+        this.matchedNumber = matchedNumber;
+        this.hasBonusNumber = hasBonusNumber;
+        this.winningPrice = winningPrice;
+    }
+
+    public int getMatchedNumber() {
+        return matchedNumber;
+    }
+
+    public boolean isHasBonusNumber() {
+        return hasBonusNumber;
+    }
+
+    public int getWinningPrice() {
+        return winningPrice;
+    }
 }

2e6e788f0164f77e3a2e78d5ebdae943d99aef5c
FriedHamm <kwh2716@gmail.com>
2023-11-02 20:10:30 +0900

    Feat: Lotto 클래스에 오름차순으로 정렬하는 기능과 예외 발생 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +8,13 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] ");
+            throw new IllegalArgumentException("[ERROR] 숫자를 중복 없이 혹은 6자리를 넘지 않았는지 확인하세요.");
         }
     }
 
--- src/main/java/util/LottoGenerator.java
+++ src/main/java/util/LottoGenerator.java
@@ -2,7 +2,9 @@ package util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.Lotto;
 
 public class LottoGenerator {
@@ -24,6 +26,7 @@ public class LottoGenerator {
 
     public static Lotto generateLotto(Integer[] userNumbers)
     {
-        return new Lotto(Arrays.asList(userNumbers));
+        Set<Integer> forDuplication = new HashSet<>(Arrays.asList(userNumbers));
+        return new Lotto(new ArrayList<>(forDuplication));
     }
 }

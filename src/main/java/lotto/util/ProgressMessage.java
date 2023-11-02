7168b49d5c56a3334dea020fe8b131ed8847881b
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 16:37:04 +0900

    feat(ProgressMessage): 게임진행메세지를 가진 객체 추가

---
+++ src/main/java/lotto/util/ProgressMessage.java
@@ -0,0 +1,26 @@
+package lotto.util;
+
+public enum ProgressMessage {
+
+	INPUT_MONEY("구입금액을 입력해 주세요."),
+	BUYED_LOTTO_NUMBER("%d개를 구매했습니다."),
+	INPUT_ANSWER_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
+	INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+	WINNING_STATISTICS("당첨 통계\n" + "---\n"),
+	TOTAL_RETURN("총 수익률은 %,2f입니다.");
+	
+
+	private final String message;
+
+	ProgressMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	public String getForMatMessage(Object... params) {
+	    return String.format(getMessage(), params);
+	}
+}

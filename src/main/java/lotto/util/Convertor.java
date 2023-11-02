9674ad493f4e9a510206ab6979efd7455af993bc
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-02 19:18:24 +0900

    feat(Convertor) : 문자를 정수로 변환하는 기능 추가

---
+++ src/main/java/lotto/util/Convertor.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+public class Convertor {
+
+    private Convertor() {
+    }
+
+    public static int convertStringToInt(String value) {
+        return Integer.parseInt(value);
+    }
+}

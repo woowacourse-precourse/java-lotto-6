b90dcd6d9e2798953ae495b71a3ba9dc50c762ab
ailiartsua <ailiartsual2@gmail.com>
2023-11-02 23:59:12 +0900

    feat : 발행한 로또 출력 기능 추가
    - 로또 수량 및 로또 데이터 출력 기능 추가
    - Lotto 도메인에 숫자 Sorting 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +17,11 @@ public class Lotto {
         }
     }
 
+    public List<Integer> getSortedNumbers() {
+        List<Integer> sortedNumber = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        return sortedNumber;
+    }
     // TODO: 추가 기능 구현
 }

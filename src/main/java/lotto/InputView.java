8c5feaeac0c7507e98662eb17dcb6528412b2b75
bananananaa <crown9924@gmail.com>
2023-11-02 16:48:53 +0900

    inputView클래스 작성

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String inputAmount() {
+        return Console.readLine();
+    }
+
+    public static String inputNumbers() {
+        return Console.readLine();
+    }
+
+    public static String inputBonus() {
+        return Console.readLine();
+    }
+
+}

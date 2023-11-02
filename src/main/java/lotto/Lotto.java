694953136f7ec2f7e9b0fc41af1b3b098cc70381
IMWoo94 <leemeawoo94@gmail.com>
2023-11-02 17:19:57 +0900

    feat : 로또 번호 중복 값 체크 기능 구현
    로또 번호 중복 값 체크 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,25 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        NumberSizeCheck(numbers);
+        DuplicateNumberCheck(numbers);
+    }
+
+    private void NumberSizeCheck(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
+    private void DuplicateNumberCheck(List<Integer> numbers) {
+        long count = numbers
+                .stream()
+                .distinct()
+                .count();
+
+        if (numbers.size() != count) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

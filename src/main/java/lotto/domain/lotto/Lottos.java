e005da41773fc8923fa3d100ac5f1d376f277374
mainfn <_@mainfn.dev>
2023-11-03 01:42:29 +0900

    feat: Lottos 일급 컬렉션 생성

---
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -0,0 +1,22 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+
+/**
+ * 사용자가 구매한 모든 로또를 담는 일급 컬렉션입니다.
+ */
+public final class Lottos {
+    /**
+     * 사용자가 구매한 로또 리스트
+     */
+    private final List<Lotto> lottos;
+
+    /**
+     * 이미 Lotto를 만들 때, LottoNumber가 1~45 범위에 속하는지, 6자리 숫자인지 검증이 끝난 상태이기 때문에 별도의 검증 없이 생성합니다.
+     */
+    public Lottos(final List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+
+}

ea4c2204dd80faa24b7865a085f0d49e3973f6da
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-02 17:12:41 +0900

    test(LottoNumberGenerator): 로또번호를 생성하는 기능 테스트 코드 작성

---
+++ src/test/java/lotto/generator/LottoNumberGeneratorTest.java
@@ -0,0 +1,27 @@
+package lotto.generator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import lotto.domain.Lotto;
+
+public class LottoNumberGeneratorTest {
+	
+	 @Test
+	    public void testPickUniqueNumbersInRange() {
+		 LottoNumberGenerator generator = new LottoNumberGenerator();
+	        List<Integer> numbers = generator.generate();
+
+	        assertNotNull(numbers);
+	        assertEquals(Lotto.LOTTO_SIZE, numbers.size());
+
+	        for (int number : numbers) {
+	            assertTrue(number >= Lotto.MIN_NUMBER && number <= Lotto.MAX_NUMBER);
+	        }
+	    }
+}

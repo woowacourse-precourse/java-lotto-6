4240764402c01050dc9aaf1bb00a1bde880381c5
wns312 <wns312@gmail.com>
2023-11-02 17:37:40 +0900

    feat: 로또 구입 금액 안내 출력 기능 구현

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    InputView inputView;
+    OutputView outputView;
+
+    public LottoGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void play() {
+        outputView.printReadMoneyAmountMessage();
+
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String READ_MONEY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void printReadMoneyAmountMessage() {
+        System.out.println(READ_MONEY_AMOUNT_MESSAGE);
+    }
+}

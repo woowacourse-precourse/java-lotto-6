4f81f9fd3286e2c5e9ce0f42264163c172ba49c0
Arachneee <jhg2819@naver.com>
2023-11-02 17:32:46 +0900

    feat: 로또 번호 검증 예외 구현
    사이즈, 중복, 범위를 검증하는 로직 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,55 @@
+package lotto.domain;
+
+import static lotto.constant.LottoConstant.COUNT;
+import static lotto.exception.ErrorMessage.DUPLICATE_NUMBER;
+import static lotto.exception.ErrorMessage.WRONG_SIZE;
+
+import java.util.List;
+import java.util.Set;
+import lotto.constant.LottoConstant;
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != COUNT.getValue()) {
+            throw LottoException.of(WRONG_SIZE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (isDuplicateNumber(numbers)) {
+            throw LottoException.of(DUPLICATE_NUMBER);
+        }
+    }
+
+    private static boolean isDuplicateNumber(List<Integer> numbers) {
+        return numbers.size() != Set.copyOf(numbers).size();
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (isWrongRange(numbers)) {
+            throw  LottoException.of(ErrorMessage.WRONG_RANGE);
+        }
+    }
+
+    private static boolean isWrongRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < LottoConstant.MIN_NUMBER.getValue() ||
+                        number > LottoConstant.MAX_NUMBER.getValue());
+    }
+
+}
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,7 +3,10 @@ package lotto.exception;
 public enum ErrorMessage {
     NOT_POSITIVE_INTEGER("양의 정수만 입력가능합니다."),
     INVALID_UNIT("잘못된 단위입니다."),
-    NOT_ENOUGH_MONEY("돈이 충분하지 않습니다.");
+    NOT_ENOUGH_MONEY("돈이 충분하지 않습니다."),
+    WRONG_SIZE("로또 번호 갯수가 잘못 되었습니다."),
+    DUPLICATE_NUMBER("중복된 숫자가 있습니다."),
+    WRONG_RANGE("숫자 범위가 잘못되었습니다.");
 
     private final String message;
     private static final String header = "[ERROR] ";

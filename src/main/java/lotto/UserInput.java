3ce2049c8d8cf289cddc2836f42f7c4c55857690
siho-song <thdtlgh234@naver.com>
2023-11-02 20:41:58 +0900

    [Feat] 로또 구입 금액을 입력받는 기능 구현

---
+++ src/main/java/lotto/UserInput.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+    public int receiveMoney(){
+        return Integer.parseInt(Console.readLine());
+    }
+}

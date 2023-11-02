eb3ce34186a38744bedadcbd5302e7ac8806b6e6
yechop <channetattoo@gmail.com>
2023-11-02 18:21:11 +0900

    feat(Output): Output 클래스 생성 구입금액메시지 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,11 @@
 package lotto.controller;
 
+import lotto.io.Output;
+
 public class LottoController {
 
     public void start() {
+        Output output = new Output();
+        output.printInputMoneyMessage();
     }
 }
---
+++ src/main/java/lotto/io/Output.java
@@ -0,0 +1,8 @@
+package lotto.io;
+
+public class Output {
+
+    public void printInputMoneyMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}

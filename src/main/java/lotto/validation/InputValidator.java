fce42df5d5635d2264dd929192c853321b118a62
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-02 18:35:50 +0900

    feat(InputValidator) : 구입 금액에 0입력시 예외 발생

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -3,6 +3,7 @@ package lotto.validation;
 import lotto.enums.ErrorMessage;
 
 public class InputValidator {
+    private static final String ZERO = "0";
 
     private InputValidator() {
     }
@@ -10,6 +11,12 @@ public class InputValidator {
     public static void validateNull(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.NOT_NULL.getMessage());
+        }
+    }
+
+    public static void validateZero(String input) {
+        if (input.equals(ZERO)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ZERO.getMessage());
         }
     }
 }

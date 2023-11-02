499a461abac4175acc8b232adc4f8c43cf9c3bce
ImWonYong <ssassb123@daum.net>
2023-11-03 00:55:41 +0900

    test(InputValidator): 1000원으로 나눠 떨어질 때와 입력이 숫자일 때 아무일 없음을 검증

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -1,14 +1,38 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class InputValidatorTest {
 
     @Test
+    void 입력이_숫자면_아무일_없음() {
+        String input = "10000";
+
+        assertThatCode(() -> InputValidator.validateIsNumeric(input)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 입력이_숫자가_아니면_예외() {
+        String input = "ads";
+
+        assertThatThrownBy(() -> InputValidator.validateIsNumeric(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자를 입력해야 합니다.");
+    }
+
+    @Test
+    void 금액_천원_단위면_아무일_없음() {
+        int number = 100000;
+
+        assertThatCode(() -> InputValidator.validateDivision1000(number)).doesNotThrowAnyException();
+    }
+
+    @Test
     void 구매_금액_천원_단위_아니면_예외() {
         int number = 8400;
-        Assertions.assertThatThrownBy(() -> InputValidator.validateDivision1000(number))
+        assertThatThrownBy(() -> InputValidator.validateDivision1000(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.");
     }

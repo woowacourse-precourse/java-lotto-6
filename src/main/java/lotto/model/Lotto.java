36a4d9f02c803766a28fdccd0a57db2ba575600e
leeedohyun <ldh19738@gmail.com>
2023-11-02 22:45:19 +0900

    feat: 로또 번호 중복 예외 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,13 +3,19 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+
+    private static final String LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE = "[ERROR] 로또 번호가 중복됩니다.";
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
+    // 메서드 이름이랑 내부 매직 넘버는 수정해도 되는 부분인가?
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
@@ -17,4 +23,19 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        if (isLottoSizeEqualToSix(numbers)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isLottoSizeEqualToSix(List<Integer> numbers) {
+        return countUniqueLottoNumbers(numbers) != LOTTO_SIZE;
+    }
+
+    private int countUniqueLottoNumbers(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .distinct()
+                .count();
+    }
 }

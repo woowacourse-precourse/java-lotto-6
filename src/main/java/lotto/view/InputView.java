25a2bc09c88b04980b8ffe506978db5d44ad6d44
WOONBE <kepull2918@naver.com>
2023-11-02 18:22:16 +0900

    [feat] 값의 입력을 담당하는 InputView에 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,26 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+
+    public String inputPurchaseAmount(){
+        OutputView.requestInputAmountMessage();
+        String userInput = readLine();
+        return userInput;
+    }
+
+    public String inputWinningNumbers(){
+        OutputView.requestInputWinningNumbersMessage();
+        String userInput = readLine();
+        return userInput;
+    }
+
+    public String inputBonusNumber(){
+        OutputView.requestInputBonusNumberMessage();
+        String userInput = readLine();
+        return userInput;
+    }
 
 
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,14 +2,14 @@ package lotto.view;
 
 public class OutputView {
 
-    public static void requestInputAmount() {
+    public static void requestInputAmountMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-    public static void requestInputWinningNumbers() {
+    public static void requestInputWinningNumbersMessage() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
-    public static void requestInputBonusNumber() {
+    public static void requestInputBonusNumberMessage() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 

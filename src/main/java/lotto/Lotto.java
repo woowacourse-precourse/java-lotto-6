991fca474ae9a07964fe1132c0d06ce0aadae43c
0O000 <zkzk_001@naver.com>
2023-11-02 18:17:42 +0900

    feat: 당첨 번호 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,17 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개로 이루어져 있습니다.");
+        }
+        for(int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i) < 1 || numbers.get(i) > 45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+        for(int i = 0; i < numbers.size(); i++){
+            if (numbers.contains(numbers.get(i))){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 있으면 안됩니다.");
+            }
         }
     }
 

6192a84afbb61c8d05d7415d53e5b11b08c85100
donghyunkim <ghffu405@gmail.com>
2023-11-02 16:52:41 +0900

    feat: 실행 메소드 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,4 +1,16 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoGame {
+
+    private final InputView inputView;
+
+    public LottoGame() {
+        this.inputView = new InputView();
+    }
+
+    public void run(){
+        inputView.inputPrice();
+    }
 }

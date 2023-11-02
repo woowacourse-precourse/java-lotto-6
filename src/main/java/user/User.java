e5821bf104a625e6fa60075af25f344d246746fa
happyryan <tjdgks120813@gmail.com>
2023-11-03 02:52:46 +0900

    feat(user): add input feature and user input validation for Lotto purchase amount
    Added user input validation to ensure that the Lotto purchase amount is a multiple of 1000, along with the implementation of input feature in the User class.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,2 +1,28 @@
-package user;public class User {
+package user;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    public User() {
+    }
+
+    public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        while (true) {
+            try {
+                int money = Integer.parseInt(Console.readLine());
+                validateInputMoney(money);
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    private int validateInputMoney(int money){
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 로또는 1장당 1000원입니다. 1000원 단위로 구매를 진행해주세요.");
+        }
+        return  money;
+    }
 }
+

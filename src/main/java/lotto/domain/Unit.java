860e76f65a1589b7797e0d04897e04963580582f
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-03 03:06:13 +0900

    feat - 구매 금액 검증 로직 생성

--- src/main/java/lotto/domain/Unit.java
+++ src/main/java/lotto/domain/Unit.java
@@ -2,19 +2,19 @@ package lotto.domain;
 
 public enum Unit {
 
-    PURCHASE_UNIT(1000);
+    PURCHASE_UNIT(1000),
 
+    ZERO(0);
     private final int unit;
 
     Unit(int unit) {
         this.unit = unit;
     }
 
-    private int getUnit() {
-        return unit;
-    }
-
     public static int getPurchaseNumber(int purchasePrice) {
+        if (purchasePrice % PURCHASE_UNIT.unit != 0) {
+            throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해야 합니다.");
+        }
         return purchasePrice / PURCHASE_UNIT.unit;
     }
 }

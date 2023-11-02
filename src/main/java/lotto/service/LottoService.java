06cbd707dad564761bac2a7e2d60d2971b57b6a7
joo-sang-ah <jsa@interiorteacher.com>
2023-11-03 02:43:38 +0900

    test: Service 테스트 코드 추가

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,33 @@
+package lotto.service;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoService {
+
+    public Integer readPurchasePrice() {
+        return null;
+    }
+
+    boolean validatePurchasePrice(Integer price) {
+        return false;
+    }
+
+    public List<Integer> readWinningNumbers() {
+        return null;
+    }
+
+    public Integer readBonusNumber() {
+        return null;
+    }
+
+    public List<Lotto> issueLotto(Integer purchasePrice) {
+        return null;
+    }
+
+    public void printResult() {
+
+    }
+}
---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,30 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoServiceTest {
+
+    private final LottoService lottoService;
+
+    LottoServiceTest(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    @DisplayName("로또 구입 금액 단위가 1,000원이 아니면 false")
+    @Test
+    void invalidPurchasePriceTest() {
+        assertThat(lottoService.validatePurchasePrice(1001)).isFalse();
+    }
+
+    @DisplayName("입력 받은 로또 구입 금액만큼 로또 발행")
+    @Test
+    void createLottoByPurchasePrice() {
+        assertThat(lottoService.issueLotto(2000).size() == 2).isTrue();
+    }
+}

f98883ff13830a3829e90d2149a9d7324ff3fe70
정민규 <fastdodge7@naver.com>
2023-11-03 02:16:29 +0900

    feat: 구매한 로또 정보 출력 기능 추가
    구매한 로또를 출력하는 기능이 추가되었습니다.

---
+++ src/main/java/OutputManager/OutputManager.java
@@ -0,0 +1,14 @@
+package OutputManager;
+
+import java.util.List;
+import lotto.Lotto;
+import lotto.LottoStatistics;
+
+public class OutputManager {
+    void printLottoData(List<Lotto> lottoList) {
+        System.out.println(lottoList.size() + "개를 구매했습니다.");
+        for(Lotto lotto : lottoList) {
+            System.out.println(lotto);
+        }
+    }
+}

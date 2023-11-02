355af7f3667b6ccf8f6bdc8f4cacd41b6a17cd6f
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 02:47:15 +0900

    feat(LottoRepository): 여러개의 로또들을 추가하는 기능 추가

--- src/main/java/lotto/domain/LottoRepository.java
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -6,34 +6,38 @@ import java.util.List;
 import java.util.StringJoiner;
 
 public class LottoRepository {
-	
+
 	private static final String DELIMITER = System.lineSeparator();
-	
-	private static final List<Lotto> lottos = new ArrayList<>();
-	
+
+	private static List<Lotto> lottoRepository = new ArrayList<>();
+
 	private LottoRepository() {
 		throw new AssertionError();
 	}
-	
-	public static List<Lotto> getLottos(){
-		return Collections.unmodifiableList(lottos);
+
+	public static List<Lotto> getLottos() {
+		return Collections.unmodifiableList(lottoRepository);
 	}
-	
+
 	public static String printLottoNumbers() {
 		StringJoiner strJoiner = new StringJoiner(DELIMITER);
-		
-		for(Lotto lotto : lottos) {
+
+		for (Lotto lotto : lottoRepository) {
 			strJoiner.add(lotto.printNumbers());
 		}
-		
+
 		return strJoiner.toString();
 	}
-	
+
 	public static void addLotto(Lotto lotto) {
-		lottos.add(lotto);
+		lottoRepository.add(lotto);
 	}
-	
-    public static void deleteAll() {
-    	lottos.clear();
-    }
+
+	public static void addAllLotto(List<Lotto> lottos) {
+		lottos.forEach(LottoRepository::addLotto);
+	}
+
+	public static void deleteAll() {
+		lottoRepository.clear();
+	}
 }

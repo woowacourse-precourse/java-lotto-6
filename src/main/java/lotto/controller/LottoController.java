f36b009d6f93ed5fe2b7d04bf5c1b6999f4497a9
uhanuu <hwryu990927@gmail.com>
2023-11-03 01:35:38 +0900

    feat: lottoController

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+import lotto.util.LottoFactory;
 import lotto.view.input.LottoBuyInputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
@@ -10,5 +16,15 @@ public class LottoController {
         this.lottoBuyInputView = lottoBuyInputView;
     }
 
+    public Lottos buyLottos(){
+        List<Lotto> lottos = new ArrayList<>();
 
+        int amount = lottoBuyInputView.requestLottoPurchaseAmount();
+
+        for (; amount > 0; amount -= 1000) {
+            lottos.add(LottoFactory.getLotto());
+        }
+
+        return new Lottos(lottos);
+    }
 }

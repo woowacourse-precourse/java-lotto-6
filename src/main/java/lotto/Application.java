c713cd600adb27e082d0b41f95c47f4a0c6a904a
ohhyeon2 <dhgus1235@gmail.com>
2023-11-02 18:04:46 +0900

    [feat] 구매 금액을 입력하는 deposit 메서드 생성
    1000원 단위로 떨어지는 수가 아니라면 예외 처리 후 다시 시작하기 위해 재귀 함수로 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+    private static int deposit() {
+        System.out.println("구매금액을 입력해 주세요.");
+        String depositMoney = Console.readLine();
+        int change = Integer.parseInt(depositMoney);
+        try {
+            if (change % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 나누어지는 금액을 입력해주세요.");
+            }
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            deposit();
+        }
+        return change / 1000;
     }
 }

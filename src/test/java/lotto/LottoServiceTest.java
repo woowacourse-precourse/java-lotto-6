8e61693923929ff35f73c7023524db2557af081d
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-02 22:15:44 +0900

    test : DisplayName의 순서 변경

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -8,8 +8,8 @@ class LottoServiceTest {
 
     LottoService lottoService = new LottoService();
 
-    @Test
     @DisplayName("구입 금액이 숫자가 아닐 경우 예외가 발생한다.")
+    @Test
     void inputMoneyTypeExceptionTest() {
         String money = "100a";
 
@@ -18,8 +18,8 @@ class LottoServiceTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
     @DisplayName("구입 금액이 음수일 경우 예외가 발생한다.")
+    @Test
     void inputMoneyNegativeExceptionTest() {
         String money = "-100";
 
@@ -28,8 +28,8 @@ class LottoServiceTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
     @DisplayName("구입 금액이 1000원 단위가 아닐 경우 예외가 발생한다.")
+    @Test
     void inputMoneyUnitExceptionTest() {
         String money = "500";
 
@@ -38,8 +38,8 @@ class LottoServiceTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
     @DisplayName("구입 금액이 0원일 경우 예외가 발생한다.")
+    @Test
     void inputMoneyRangeExceptionTest() {
         String money = "0";
 

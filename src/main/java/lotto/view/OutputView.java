89f911573cbda34d9505ef4f7382b3ee26506e04
yuseonjun <seonjun0906@gmail.com>
2023-11-02 18:09:33 +0900

    refactor(OutputView): PlayerLottos -> BuyerLotto로 이름 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
 
     private static final String LINE_BREAK = System.lineSeparator();
 
-    public static void printPlayerLottos(int lottoCount, List<Lotto> lottoNumbers) {
+    public static void printBuyerLotto(int lottoCount, List<Lotto> lottoNumbers) {
         System.out.print(LINE_BREAK);
         System.out.println(lottoCount + "개를 구매했습니다.");
         lottoNumbers.forEach(lotto -> System.out.println(lotto.getNumbers()));

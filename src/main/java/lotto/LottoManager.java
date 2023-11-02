a5d6c11d696c2a442d08c34ac4b107458c3a93a3
vega <vega99c@naver.com>
2023-11-02 20:31:33 +0900

    feat(LottoManager): 구입금액에 따른 로또 발행
    - 로또판매 시작 메서드 추가
    - 임의의 로또 번호를 생성하는 메서드 추가
    - 발행 된 로또를 구매자의 myLottos리스트에 추가하는 메서드 구현

---
+++ src/main/java/lotto/LottoManager.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoManager {
+    LottoBuyer lottoBuyer;
+
+    public LottoManager(LottoBuyer buyer) {
+        lottoBuyer = buyer;
+    }
+
+    public void lottoSellingStart() {
+        lottoBuyer.inputPurchaseAmount();
+        publishLotto();
+    }
+
+    public List<Integer> createLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public void publishLotto() {
+        int publishCount = lottoBuyer.calculatePublishCount();
+
+        for (int i = 0; i < publishCount; i++) {
+            lottoBuyer.addMyLottos(new Lotto(createLottoNumber()));
+        }
+    }
+}

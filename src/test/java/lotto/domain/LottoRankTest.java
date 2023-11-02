707dd8f0078849d9825089a449727eac3597f963
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 00:21:20 +0900

    test(LottoRank): 일치된 로또 번호와 보너스 번호의 일치유무에 확인 후 리턴하는 기능 테스트코드 작성

---
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import lotto.service.CheckWinningLottoNumbersService;
+
+public class LottoRankTest {
+
+	@DisplayName("일치한 로또 번호 수와 보너스 번호 유뮤에 따른 로또 상금을 확인한다.")
+	@MethodSource("createCheckWinnerTestValue")
+	@ParameterizedTest
+	void test(int matchedNumCount, boolean isBonusNumber, int expect) {
+		CheckWinningLottoNumbersService service = new CheckWinningLottoNumbersService();
+
+		int lottoPrice = LottoRank.getLottoRank(matchedNumCount, isBonusNumber).getPrice();
+		assertEquals(lottoPrice, expect);
+	}
+
+	static Stream<Arguments> createCheckWinnerTestValue() {
+		return Stream.of(Arguments.of(2, false, 0), Arguments.of(3, false, 5000), Arguments.of(4, false, 50000),
+						Arguments.of(5, false, 1500000), Arguments.of(5, true, 30000000), Arguments.of(6, false, 2000000000));
+	}
+}

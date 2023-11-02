faeb7efc14147fff7c0fa34c425f149740269b4d
jongwonpc <c3392302@naver.com>
2023-11-03 08:19:15 +0900

    feat: 구입한 금액만큼 숫자 크기 순으로 정렬된 로또 구매하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoGame.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+
+    private List<Lotto> lottos = new ArrayList<>();
+
+    public LottoGame(int lottoMoney) {
+        buildLottos(lottoMoney);
+    }
+
+    private void buildLottos(int lottoMoney) {
+        int lottoCount = lottoMoney / 1000;
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> lottoNums = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> sortedLottoNums = lottoNums.stream().sorted().toList();
+            lottos.add(new Lotto(sortedLottoNums));
+        }
+    }
+
+}

188755a6764ce5f341dd66eeab578b075c45b2bb
LastCow9000 <sys19912002@hanmail.net>
2023-11-03 06:43:23 +0900

    feat: money 1000으로 나누어 떨어지는지 검증 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,6 +4,7 @@ public class Money {
     private static final String NULL_OR_EMPTY_ERROR_MESSAGE = "[ERROR] 공백이나 빈 값으로는 로또 구매가 불가능합니다.";
     private static final String NAN_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력이 불가능합니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 1000 이상의 숫자만 입력 가능합니다.";
+    private static final String DIVIDE_ERROR_MESSAGE = "[ERROR] 1000원 단위로만 구매가 가능합니다.";
 
     private final int amount;
 
@@ -11,8 +12,13 @@ public class Money {
         validateNullOrEmpty(amount);
         int parsedAmount = toInt(amount);
         validateRange(parsedAmount);
+        validateDivisibleBy1000(parsedAmount);
 
         this.amount = parsedAmount;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 
     private void validateNullOrEmpty(String amount) {
@@ -35,6 +41,12 @@ public class Money {
     private void validateRange(int amount) {
         if (amount <= 1000) {
             throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDivisibleBy1000(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(DIVIDE_ERROR_MESSAGE);
         }
     }
 }
--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -39,4 +39,14 @@ class MoneyTest {
                 .hasMessage("[ERROR] 1000 이상의 숫자만 입력 가능합니다.")
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("money가 1000 으로 나누어 떨어지지 않을 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1004", "1400400", "33030"})
+    void validateDivideTest(String input) {
+        // when, then
+        assertThatThrownBy(() -> new Money(input))
+                .hasMessage("[ERROR] 1000원 단위로만 구매가 가능합니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

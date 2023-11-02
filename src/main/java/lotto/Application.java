df7d4d6782eb7ff3bf2098869078e1df916700ce
pumpk <cong2738@gmail.com>
2023-11-03 01:03:13 +0900

    feat: 에러 코드 명명, 수익률 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,13 +3,16 @@ package lotto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
-
+import java.lang.Math;
 
 import lotto.Lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    final static IllegalArgumentException numError = new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    final static IllegalArgumentException dupError = new IllegalArgumentException("[ERROR] 로또 번호는 중복할 수 없습니다.");
+
     final static int[] prices = {2_000_000_000, 30_000_000, 1_500_000, 50_000, 5_000, 0};
 
     // 랜덤 번호 6개 뽑기
@@ -30,17 +33,30 @@ public class Application {
         System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> box = new ArrayList<>();
         for (String s : Console.readLine().split(",")) {
+            int n = 0;
+            try {
+                n = Integer.parseInt(s);
+            } catch (Exception e) {
+                throw numError;
+            }
+            if (n > 45) throw numError;
+            
             box.add(Integer.parseInt(s));
         }
         System.out.println();
         return box;
     }
 
-    private static int getUserBonus(Lotto userNumbers) {
+    private static int setUserBonus(Lotto userNumbers) {
         System.out.println("보너스 번호를 입력해 주세요.");
-        int bonus = Integer.parseInt(Console.readLine());
-        if (userNumbers.lottoContains(bonus))
-            throw new IllegalArgumentException();
+        int bonus = 0;
+        try {
+            bonus = Integer.parseInt(Console.readLine());   
+        } catch (Exception e) {
+            throw numError;
+        }
+        if (bonus > 45) throw numError;
+        if (userNumbers.lottoContains(bonus)) throw dupError;
         System.out.println();
         return bonus;
     }
@@ -66,7 +82,7 @@ public class Application {
     private static int setMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
-        if (money%1000 != 0) throw new IllegalArgumentException();
+        if (money%1000 != 0) throw new IllegalArgumentException("[ERROR] 금액은 1000으로 나누어 떨어져야 합니다.");
         System.out.println();
         return money;
     }
@@ -88,6 +104,7 @@ public class Application {
             int idx = judge(winNumber, user.get(i), bonus);
             res[idx]++;
         }
+        System.out.println("당첨통계\n---");
         int i = 4;
         System.out.println(String.format("%d개 일치 (%d) - %d개",target[i],prices[i],res[i--]));
         System.out.println(String.format("%d개 일치 (%d) - %d개",target[i],prices[i],res[i--]));
@@ -98,19 +115,19 @@ public class Application {
     }
 
     public static void calReturnRate(int[] res, int money){
-        int userPrice = 0;
+        float userPrice = 0;
         for (int i = 0; i < res.length; i++) {
             userPrice += prices[i]*res[i];
         }
-        int returnRate = userPrice/money*100;
-        System.out.println(String.format("총 수익률은 %d입니다.", returnRate));
+        float returnRate = Math.round(userPrice/money*1000)/10;
+        System.out.println(String.format("총 수익률은 %.1f입니다.", returnRate));
     }
 
-    public static void main(String[] args) {        
+    public static void main(String[] args) {  
         int m = setMoney();
         List<Lotto> user = setUser(m/1000);
         Lotto winNumber = new Lotto(getUserNumbers());
-        int bonus = getUserBonus(winNumber); 
+        int bonus = setUserBonus(winNumber); 
 
         calReturnRate(resList(user, winNumber, bonus), m);
     }

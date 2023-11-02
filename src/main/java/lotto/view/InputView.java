61b8679f5ce3482e3fe4f58ec253517652394c68
YongHyeon <pyh4616@naver.com>
2023-11-03 02:10:19 +0900

    Feat(InputView 클래스): 당첨 번호를 입력하는 기능을 readWinningNumbers 메서드로 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,17 @@ package lotto.view;
 
 import lotto.util.Convert;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
     public Integer readPurchaseAmount() {
         return Convert.stringToInteger(readLine());
+    }
+
+    public List<Integer> readWinningNumbers() {
+        return Convert.stringToIntegerList(readLine());
     }
 }

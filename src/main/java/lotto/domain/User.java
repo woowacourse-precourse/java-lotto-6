80862f48edbd839cf92832747084aabdd7a9b5be
SeoJiWon <sjiwon4491@gmail.com>
2023-11-02 18:45:55 +0900

    feat: User 로또 구매를 위해 사용한 가격 계산 (with BigDecimal)

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
 public class User {
+    private static final int LOTTO_PURCHASE_UNIT = 1000;
+
     private final List<Lotto> lottos;
 
     private User(final List<Lotto> lottos) {
@@ -16,5 +19,10 @@ public class User {
 
     public List<Lotto> getLottos() {
         return Collections.unmodifiableList(lottos);
+    }
+
+    public BigDecimal calculateLottoPurchaseMoney() {
+        final BigDecimal purchaseAmount = BigDecimal.valueOf(lottos.size());
+        return BigDecimal.valueOf(LOTTO_PURCHASE_UNIT).multiply(purchaseAmount);
     }
 }

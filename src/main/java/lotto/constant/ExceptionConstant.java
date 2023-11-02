431f8f755c2d6f11b603752b727539f0b3220c9e
Nara Park <skfk6352@naver.com>
2023-11-03 06:21:31 +0900

    <test>(InputValidatorTest): 사용자 입력값에 대한 다양한 테스트 코드 구현

--- src/main/java/lotto/constant/ExceptionConstant.java
+++ src/main/java/lotto/constant/ExceptionConstant.java
@@ -2,7 +2,7 @@ package lotto.constant;
 
 public enum ExceptionConstant {
     INPUT_IS_ESSENTIAL("입력값은 필수입니다."),
-    INPUT_IS_NUMBER("입력값은 자연수의 숫자만을 허용합니다."),
+    INPUT_IS_NUMBER("입력값은 정수만을 허용합니다."),
     PURCHASE_MIN_NUMBER("구매 최소 금액은 %s원 입니다."),
     PURCHASE_REMAINDER("구매 금액은 %s원 단위로 가능합니다.");
 

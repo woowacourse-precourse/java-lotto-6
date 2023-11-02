f5f413434439ea5dc2a6a37d9a8d4dfa517a099a
seungtoctoc <dltmxor31@naver.com>
2023-11-02 20:40:57 +0900

    feat: 배열의 길이 검증 기능 구현 배열의 길이가 6인지 검증하는 기능을 구현했습니다.

--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -1,19 +1,25 @@
 package lotto.validation;
 
 public class Validation {
-    public void validateNumber(String Input) {
+    public void validateNumber(String input) {
         try {
-            Integer.parseInt(Input);
+            Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력값이 정수가 아닙니다.");
         }
     }
 
-    public void validateUnit(String Input) {
-        int payment = Integer.parseInt(Input);
+    public void validateUnit(String input) {
+        int payment = Integer.parseInt(input);
 
         if(payment % 1000 != 0) {
             throw new IllegalArgumentException("입력값이 1,000원 단위가 아닙니다.");
+        }
+    }
+
+    public void validateLength(String[] input) {
+        if (input.length != 6) {
+            throw new IllegalArgumentException("입력값이 6개가 아닙니다.");
         }
     }
 }

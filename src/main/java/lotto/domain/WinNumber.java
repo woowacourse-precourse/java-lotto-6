daa848719e4e4ad6b68cef2bc04d6d8ee9517022
jongwonpc <c3392302@naver.com>
2023-11-03 08:28:16 +0900

    feat: 당첨 번호 객체로 구현

---
+++ src/main/java/lotto/domain/WinNumber.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.valid.WinNumberValid;
+
+public class WinNumber {
+
+    private List<Integer> lottoNums;
+
+    public WinNumber(String lottoNum) {
+        List<Integer> lottoNums = WinNumberValid.validWinNumber(lottoNum);
+        this.lottoNums = lottoNums;
+    }
+
+    public List<Integer> getLottoNums() {
+        return lottoNums;
+    }
+}

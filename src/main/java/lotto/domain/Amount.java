a546bc1a4be99184069a079284eb4fbe5a430b43
yuseonjun <seonjun0906@gmail.com>
2023-11-02 17:25:13 +0900

    feat(Amount, AmountTest): 구입금액을 입력할 때 1000원 미만의 값이 입력되면 IllegalArgumentException를 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력한다.

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -7,6 +7,7 @@ public class Amount {
     private final int amount;
 
     public Amount(int amount) {
+        validateAmountRange(amount);
         validateAmount(amount);
         this.amount = amount;
     }
@@ -14,6 +15,12 @@ public class Amount {
     private void validateAmount(int amount) {
         if (amount % ONE_LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력을 해야합니다.");
+        }
+    }
+
+    private void validateAmountRange(int amount) {
+        if (amount < ONE_LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 구입은 1000원부터 가능합니다.");
         }
     }
 }
--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -9,9 +9,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 class AmountTest {
 
     @ParameterizedTest
-    @DisplayName("Amount의 생성자는 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @DisplayName("구입금액은 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
     @ValueSource(ints = {1001, 1002, 1003})
     void Amount_Constructor_When_Not_Divisible_By_1000(int price) {
+        assertThatThrownBy(() -> new Amount(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구입금액은 1000미만의 숫자가 입력되면 예외가 발생한다.")
+    @ValueSource(ints = {999, 998, 997})
+    void Amount_Constructor_When_Less_Than_1000(int price) {
         assertThatThrownBy(() -> new Amount(price))
                 .isInstanceOf(IllegalArgumentException.class);
     }

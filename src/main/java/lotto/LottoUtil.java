bae7f777d0fca377b7cfd98df2318228cffaab4a
mythpoy <okmlnsunok@gmail.com>
2023-11-02 19:38:08 +0900

    feat: 금액에 맞는 로또들을 만들어내는 util 함수 추가

--- src/main/java/lotto/LottoUtil.java
+++ src/main/java/lotto/LottoUtil.java
@@ -1,10 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoUtil {
+
+    public static List<Lotto> createLottoList(int amount) {
+        validateAmount(amount);
+        int size = amount / 1000;
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(numbers));
+        }
+
+        return lottoList;
+    }
 
     public static void validateAmount(int amount) {
         if (amount % 1000 != 0) {

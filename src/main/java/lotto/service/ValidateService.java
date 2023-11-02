fdc10aef0d42a1f7c4a7ce8797394b68355743cd
0Chord <seaweed.0chord@gmail.com>
2023-11-02 22:07:19 +0900

    feat : checkCorrectWinnerNumbersInput method
    당첨번호를 검증하는 메서드

--- src/main/java/lotto/service/ValidateService.java
+++ src/main/java/lotto/service/ValidateService.java
@@ -1,6 +1,10 @@
 package lotto.service;
 
+import java.util.List;
+
 public interface ValidateService {
 
     void checkCorrectMoneyInput(String input);
+
+    List<Integer> checkCorrectWinnerNumbersInput(String input);
 }
--- src/main/java/lotto/service/impl/ValidateServiceImpl.java
+++ src/main/java/lotto/service/impl/ValidateServiceImpl.java
@@ -1,6 +1,10 @@
 package lotto.service.impl;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.exception.DifferentUnitFormatException;
+import lotto.exception.WrongBlankException;
+import lotto.exception.WrongCommasFormatException;
 import lotto.exception.WrongTypeFormatException;
 import lotto.service.ValidateService;
 
@@ -13,11 +17,40 @@ public class ValidateServiceImpl implements ValidateService {
         }
     }
 
+    private static void checkBlankInput(String input) {
+        if (input.equals("")) {
+            throw new WrongBlankException();
+        }
+    }
+
+    private static void checkCorrectCommas(String input, List<String> inputStream) {
+        long count = countCommas(input);
+        if (count != 5 || inputStream.size() != 6) {
+            throw new WrongCommasFormatException();
+        }
+    }
+
+    private static long countCommas(String input) {
+        return input.chars().filter(ch -> ch == ',').count();
+    }
+
     @Override
     public void checkCorrectMoneyInput(String input) {
         Integer money = checkIncludeChar(input);
         if (money % 1000 != 0) {
             throw new DifferentUnitFormatException();
+        }
+    }
+
+    @Override
+    public List<Integer> checkCorrectWinnerNumbersInput(String input) {
+        checkBlankInput(input);
+        List<String> inputStream = Arrays.asList(input.split(","));
+        checkCorrectCommas(input, inputStream);
+        try {
+            return inputStream.stream().map(Integer::parseInt).toList();
+        } catch (NumberFormatException numberFormatException) {
+            throw new WrongTypeFormatException();
         }
     }
 }

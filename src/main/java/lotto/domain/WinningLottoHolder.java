7ea83bf31b6c9263a1f96d2dbfe5eb5f36ada392
SeoJiWon <sjiwon4491@gmail.com>
2023-11-02 18:02:56 +0900

    rename: LottoWinningMachine -> WinningLottoHolder

--- src/main/java/lotto/domain/WinningLottoHolder.java
+++ src/main/java/lotto/domain/WinningLottoHolder.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import static lotto.exception.ExceptionMessage.LottoMachineException.BONUS_NUMBER_MUST_BE_UNIQUE;
 
-public class LottoWinningMachine {
+public class WinningLottoHolder {
     private final Lotto winningLotto;
     private final BonusNumber bonusNumber;
 
-    private LottoWinningMachine(final Lotto winningLotto, final BonusNumber bonusNumber) {
+    private WinningLottoHolder(final Lotto winningLotto, final BonusNumber bonusNumber) {
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-    public static LottoWinningMachine drawWinningLotto(final List<Integer> winningNumbers, final int bonusNumber) {
+    public static WinningLottoHolder drawWinningLotto(final List<Integer> winningNumbers, final int bonusNumber) {
         validateBonusNumberIsDuplicate(winningNumbers, bonusNumber);
-        return new LottoWinningMachine(Lotto.create(winningNumbers), BonusNumber.create(bonusNumber));
+        return new WinningLottoHolder(Lotto.create(winningNumbers), BonusNumber.create(bonusNumber));
     }
 
     private static void validateBonusNumberIsDuplicate(final List<Integer> winningNumbers, final int bonusNumber) {
--- src/test/java/lotto/domain/WinningLottoHolderTest.java
+++ src/test/java/lotto/domain/WinningLottoHolderTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-class LottoWinningMachineTest {
+class WinningLottoHolderTest {
     @Test
     @DisplayName("보너스 번호가 당첨 번호에 중복되면 예외가 발생한다")
     void throwExceptionByBonusNumberIsNotUnique() {
@@ -20,25 +20,25 @@ class LottoWinningMachineTest {
         final int bonusNumber = winningNumbers.get(0);
 
         // when - then
-        assertThatThrownBy(() -> LottoWinningMachine.drawWinningLotto(winningNumbers, bonusNumber))
+        assertThatThrownBy(() -> WinningLottoHolder.drawWinningLotto(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(BONUS_NUMBER_MUST_BE_UNIQUE.message);
     }
 
     @Test
-    @DisplayName("LottoWinningMachine을 생성한다")
+    @DisplayName("WinningLottoHolder를 생성한다")
     void success() {
         // given
         final List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         final int bonusNumber = 7;
 
         // when
-        final LottoWinningMachine lottoWinningMachine = LottoWinningMachine.drawWinningLotto(winningNumbers, bonusNumber);
+        final WinningLottoHolder winningLottoHolder = WinningLottoHolder.drawWinningLotto(winningNumbers, bonusNumber);
 
         // then
         assertAll(
-                () -> assertThat(lottoWinningMachine.getWinningLotteryNumbers()).containsExactlyInAnyOrderElementsOf(winningNumbers),
-                () -> assertThat(lottoWinningMachine.getBonusNumber()).isEqualTo(bonusNumber)
+                () -> assertThat(winningLottoHolder.getWinningLotteryNumbers()).containsExactlyInAnyOrderElementsOf(winningNumbers),
+                () -> assertThat(winningLottoHolder.getBonusNumber()).isEqualTo(bonusNumber)
         );
     }
 }

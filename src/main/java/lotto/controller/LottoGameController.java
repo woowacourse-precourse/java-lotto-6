3aa42083fc4f1769fa660bfb2c774b355657060b
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 06:24:01 +0900

    feat(controller): 사용자에게 로또 구입 금액을 입력받아 그 수량만큼 로또를 구입하고 구매한 갯수와 로또 번호를 출력한다. - 입력받은 구입 금액이 유효하지 않을 경우 에러 문구를 출력하고 다시 입력받는다. - 입력받은 구입 금액이 유효한 값일 경우 로또 판매 가격으로 나눈 값(수량) 만큼 구매한다.

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.service.LottoService;
+import lotto.validator.Validator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoGameController {
 
@@ -17,6 +23,23 @@ public class LottoGameController {
     }
 
     private void lottoPurchase() {
+        String inputPurchaseAmount = getValidInputPurchaseAmount();
+        int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
+        int purchaseQuantity = purchaseAmount / 1000;
+        List<Lotto> userLottos = lottoService.purchase(purchaseQuantity);
+        OutputView.quantityAndLottoNumbers(userLottos);
+    }
+
+    private String getValidInputPurchaseAmount() {
+        while (true) {
+            String inputPurchaseAmount = InputView.purchaseAmount();
+            try {
+                Validator.purchaseAmount(inputPurchaseAmount);
+                return inputPurchaseAmount;
+            } catch (IllegalArgumentException e) {
+                OutputView.errorMessage(e);
+            }
+        }
     }
 
     private void lottoGameInit() {

cf2621153e934beaf7a89c95c366ae8e622dbbd8
uhanuu <hwryu990927@gmail.com>
2023-11-02 22:42:49 +0900

    feat: lottoFactory class 구현

---
+++ src/main/java/lotto/util/LottoFactory.java
@@ -0,0 +1,24 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.List;
+
+public class LottoFactory {
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int LOTTO_LIMIT_VALUE = 6;
+
+
+    private LottoFactory() {
+    }
+
+    public static Lotto getLotto(){
+        return new Lotto(getLottoNumbers());
+    }
+
+    private static List<Integer> getLottoNumbers(){
+        return Randoms.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_LIMIT_VALUE);
+    }
+}
---
+++ src/test/java/lotto/util/LottoFactoryTest.java
@@ -0,0 +1,20 @@
+package lotto.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoFactoryTest {
+
+    @Test
+    @DisplayName("1에서 45 사이의 임의의 수를 반환받을 수 있다.")
+    public void RandomsTest() {
+        // given
+        
+        // when
+        
+        // then
+    }
+
+}

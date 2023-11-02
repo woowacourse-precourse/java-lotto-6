900954ee2c3344a24e1dcbaadf3933c0c6852ce4
Dalliaaaaa <ajswlsoato@naver.com>
2023-11-03 00:44:46 +0900

    feat: 받은 횟수만큼 로또 번호 리스트에 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,12 +17,21 @@ class User{
         System.out.println("구입금액을 입력해 주세요.");
         String m = Console.readLine();
         int money = Integer.parseInt(m);
-        System.out.println(CorrectMoney(money));
+        LottoPaper(CorrectMoney(money));
     }
     public static int CorrectMoney(int money){
         if (money % 1000 == 0){
             return money / 1000;
         }
         throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+    }
+    public static void LottoPaper(int cnt){
+        ArrayList<List<Integer>> mylotto = new ArrayList<>();
+
+        for (int i = 0; i < cnt; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            mylotto.add(numbers);
+        }
+        PrintLottoPaper(cnt, mylotto);
     }
 }

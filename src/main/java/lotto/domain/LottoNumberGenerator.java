b44294816454037979bce4003b8aa492d5474eb7
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-02 16:14:10 +0900

    feat: LottoNumberGenerator 구현

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+}

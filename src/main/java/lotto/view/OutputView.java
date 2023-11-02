5ccf9f2c17a3f58b641f1806207c23951d1d7bfd
minjun7410 <minjun7410@gmail.com>
2023-11-02 23:33:06 +0900

    feat(OutputView): 구입 금액 입력 문구 출력 기능 구현
    * 구입하려는 금액을 입력하라는 문구를 출력하는 기능을 구현했다.
    * 생성할 수 없도록  생성자 접근자를 private로 변경했다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String PURCHASE_PRICE_INPUT_TEXT = "";
+
+    private OutputView () {}
+    public static void printPurchasePriceInputText() {
+        System.out.println(PURCHASE_PRICE_INPUT_TEXT);
+    }
+}

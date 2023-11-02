85a5cbc23ee128f26b5eead031755c55af7be2e4
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-02 18:28:16 +0900

    feat(ErrorMessage) : 에러 메시지 상수값을 갖는 Enum 클래스 추가

---
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -0,0 +1,23 @@
+package lotto.enums;
+
+public enum ErrorMessage {
+    PREFIX("[ERROR] "),
+    NOT_NULL("아무런 값을 입력하지 않았습니다."),
+    NOT_ZERO("구입가격은 0을 입력할 수 없습니다."),
+    NOT_DIVIDE("1000원 단위의 수를 입력해야 합니다."),
+    ONLY_NUMBER("숫자만 입력할 수 있습니다."),
+    NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    ONLY_COMMA("쉼표로 구분하여 입력해야 합니다."),
+    NUMBER_SIZE("6개의 번호를 입력해야 합니다."),
+    NUMBER_DUPLICATION("중복된 번호를 입력할 수 없습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return PREFIX + message;
+    }
+}

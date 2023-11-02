42fab3333b9cc58371a6e682370a12b38705f291
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-02 19:25:33 +0900

    feat - 로또 번호 범위, 중복 검증 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,44 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberSize(numbers);
+        validateNumbersInRange(numbers);
+        validateNumbersIsDuplicated(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("6개의 로또 번호만 입력할 수 있습니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateNumbersInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateLottoNumberInRange(number);
+        }
+    }
+
+    private void validateLottoNumberInRange(int lottoNumber) {
+        if (lottoNumber < LottoNumbers.MIN_LOTTO_NUMBER.getNumber()
+                || lottoNumber > LottoNumbers.MAX_LOTTO_NUMBER.getNumber()) {
+            throw new IllegalArgumentException("로또 번호는 1~45까지의 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private void validateNumbersIsDuplicated(List<Integer> numbers) {
+        Set<Integer> copyNumbers = new HashSet<>();
+        copyNumbers.addAll(numbers);
+        if (copyNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호가 중복되었습니다.");
+        }
+    }
 }

b31666f520f4d676bbfa870c56877dd3182573f9
gusdn0108 <gusdn6671@naver.com>
2023-11-02 17:06:17 +0900

    feat(OutputView): 구입금액을 입력해 주세요. 문구 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,13 @@
 package lotto.view;
 
+import lotto.utils.Message;
+
 public class OutputView {
     private OutputView() {
     }
 
+    public static void printBuyInputPrice(){
+        System.out.println(Message.PRINT_USER_INPUT_BUY_PRICE.getMessage());
+    }
 
 }

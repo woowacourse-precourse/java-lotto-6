370b42e47edefb26bc66864b1a6b0f9b26256d14
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-02 19:09:27 +0900

    feat(InputValidator) : 구입 금액을 천 단위로 입력하지 않았을 경우 예외 처리

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -1,9 +1,11 @@
 package lotto.validation;
 
 import lotto.enums.ErrorMessage;
+import lotto.util.Convertor;
 
 public class InputValidator {
     private static final String ZERO = "0";
+    private static final int STANDARD_OF_DIVIDE = 1000;
 
     private InputValidator() {
     }
@@ -18,5 +20,19 @@ public class InputValidator {
         if (input.equals(ZERO)) {
             throw new IllegalArgumentException(ErrorMessage.NOT_ZERO.getMessage());
         }
+    }
+
+    public static void validateDivisibleByThousand(String input) {
+        int price = Convertor.convertStringToInt(input);
+        if (isNotDivisibleByThousand(price)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_DIVIDE.getMessage());
+        }
+    }
+
+    private static boolean isNotDivisibleByThousand(int price) {
+        if ((price % STANDARD_OF_DIVIDE) != 0) {
+            return true;
+        }
+        return false;
     }
 }

b9c7048c30a4d4524962b62dbbce9abc99d41e4a
IMWoo94 <leemeawoo94@gmail.com>
2023-11-03 02:03:48 +0900

    feat : 요청 갯수 만큼의 로또 번호 생성 메소드 기능 구현
    요청 갯수 만큼의 로또 번호 생성 메소드 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,36 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoMachine {
-    
+
+    private Lotto winLotto;
+
+    public LottoMachine() {
+    }
+
+    public List<Lotto> createLottos(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+        while (quantity > 0) {
+            lottos.add(new Lotto(createLottoNumber()));
+            quantity--;
+        }
+
+        return lottos;
+    }
+
+    private List<Integer> createLottoNumber() {
+        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        lottoSort(lottoNumber);
+
+        return lottoNumber;
+    }
+
+    private void lottoSort(List<Integer> lottoNumber) {
+        Collections.sort(lottoNumber);
+    }
 }

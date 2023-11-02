af42a6a80a12c9c5c93c8ff6033573e8abb445c9
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-02 22:22:49 +0900

    feature: 로또 번호 구현

---
+++ src/main/java/lotto/model/LottoNumber.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumber {
+
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public LottoNumber() {
+        initNumbers();
+    }
+
+    private void initNumbers() {
+        numbers.addAll(Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_SIZE));
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}

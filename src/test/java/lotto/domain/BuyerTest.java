78c3e690e23324818e43c9c5729d355f218e76d2
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-02 23:02:39 +0900

    test: 잘못된 로또 구매 금액 입력 시 예외가 발생하는지 확인하는 테스트 추가
    - 로또 구매 금액이 1,000원보다 적을 경우 예외 발생 확인 테스트
    - 로또 구매 금액의 단위가 1,000원이 아닐 경우 예외 발생 확인 테스트

---
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BuyerTest {
+    @DisplayName("로또 구매 금액이 1,000원 보다 적으면 예외가 발생한다.")
+    @Test
+    void createBuyerUnderAmount() {
+        assertThatThrownBy(() -> new Buyer(999))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또를 한 장 이상 구매해야 합니다. (로또 1장 1,000원");
+    }
+
+    @DisplayName("로또 구매 금액의 단위가 1,000원이 아니면 예외가 발생한다.")
+    @Test
+    void createBuyerWrongUnit() {
+        assertThatThrownBy(() -> new Buyer(1500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또는 1,000원 단위로 구매 가능합니다. (로또 1장 1,000원");
+    }
+}

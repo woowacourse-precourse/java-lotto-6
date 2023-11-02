61d3b53dc61793734200d0342ac5b115f88cab8d
Dalliaaaaa <ajswlsoato@naver.com>
2023-11-02 23:47:57 +0900

    feat: 구입 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User.InputMoney();
+    }
+}
+
+class User {
+    public static void InputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String m = Console.readLine();
+        int money = Integer.parseInt(m);
     }
 }

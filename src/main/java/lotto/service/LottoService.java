55c91185fe448819a0f08e9fc960a12a545fcbdc
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-03 00:09:47 +0900

    feat: 로또를 생성하는 기능 추가
    로또 생성과 관련된 로직을 수행하는 LottoService 클래스를 생성
    - generateLottoNumber 기능을 통해 로또 번호를 생성
    - 생성된 번호를 createLotto 기능을 통해서 Lotto 인스턴스 생성자로 전달하여 Lotto 인스턴스 생성

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.domain.Lotto;
+
+public class LottoService {
+
+    private static final int MIN_RANDOMNUMBER = 1;
+    private static final int MAX_RANDOMNUMBER = 45;
+    private static final int MAX_LOTTO_NUMBER_SIZE = 6;
+
+    public void createLotto() {
+        List<Integer> numbers = new ArrayList<>(generateLottoNumbers());
+        Lotto lotto = new Lotto(numbers);
+    }
+
+    private Set<Integer> generateLottoNumbers() {
+        Set<Integer> numbers = new HashSet<>();
+        do {
+            numbers.add(Randoms.pickNumberInRange(MIN_RANDOMNUMBER, MAX_RANDOMNUMBER));
+        } while (numbers.size() == MAX_LOTTO_NUMBER_SIZE);
+        return numbers;
+    }
+}

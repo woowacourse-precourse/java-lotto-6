99220e64f3f803f82263e20befa86b7abec50e18
gusdn0108 <gusdn6671@naver.com>
2023-11-02 17:31:41 +0900

    feat(InputView): userInput: 사용자 입력 구현 userInputParsedInt : 사용자 입력 값 int parsing

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,17 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private InputView() {
     }
+
+    public static String userInput(){
+        return Console.readLine();
+    }
+
+    public static int userInputParsedInt(String input){
+        return Integer.parseInt(input);
+    }
+
 }

e80f6ee2d1db2de91f473d42085e81fae9c60402
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-02 20:08:25 +0900

    feat(Message): 메시지 관련 enum 클래스 추가
    docs: 메시지 관련 상수 기능 구현 체크

---
+++ src/main/java/lotto/constant/Message.java
@@ -0,0 +1,28 @@
+package lotto.constant;
+
+public enum Message {
+    PURCHASE_AMOUNT_INPUT_MESSAGE("구입금액을 입력해 주세요."),
+    PURCHASED_LOTTO_QUANTITY_MESSAGE("%d개를 구매했습니다."),
+    PURCHASED_LOTTO_NUMBERS_MESSAGE("[%d, %d, %d, %d, %d, %d]"),
+    WINNING_NUMBERS_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS_MESSAGE("당첨 통계" + "\n" +
+            "---" + "\n" +
+            "3개 일치 (5,000원) - %d개" + "\n" +
+            "4개 일치 (50,000원) - %d개" + "\n" +
+            "5개 일치 (1,500,000원) - %d개" + "\n" +
+            "5개 일치, 보너스 번호 일치 (30,000,000원) - %d개" + "\n" +
+            "6개 일치 (2,000,000,000원) - %d개"),
+    // 소수 둘째 자리에서 반올림한다.
+    TOTAL_RETURN_RATE_MESSAGE("총 수익률은 %.2f입니다.");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(Object... args) {
+        return String.format(message, args);
+    }
+}

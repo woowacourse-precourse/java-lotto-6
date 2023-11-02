2681e024b648826b4c4855848b462df5eea42002
huiseung <whtjddn75@gmail.com>
2023-11-02 19:06:52 +0900

    test : LottoGeneratorTest
    - 구입한 로또 개수를 입력 받아 로또를 생성하는 클래스 테스트 작성

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -27,5 +28,20 @@ public class Lotto {
         }
         sb.append("]");
         return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lotto lotto = (Lotto) o;
+
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return numbers != null ? numbers.hashCode() : 0;
     }
 }
---
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LottoGeneratorTest extends NsTest {
+    private LottoGenerator lottoGenerator = new LottoGenerator();
+
+    @Test
+    void test_generate_lottos() {
+        List<Integer> lottoNumbers = List.of(8, 21, 23, 41, 42, 43);
+
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    List<Lotto> lottos = lottoGenerator.generateLottos(1);
+                    assertEquals(new Lotto(lottoNumbers), lottos.get(0));
+                },
+                lottoNumbers
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}

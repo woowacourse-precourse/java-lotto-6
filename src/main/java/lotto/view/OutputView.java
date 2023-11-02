94f47f6dc02d83a3420841299a522c735e49ab35
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 05:14:50 +0900

    feat(view): 사용자에게 에러 메시지를 출력한다.
    - 예외를 인자로 받아 각 예외 메시지를 에러 메시지 형식에 맞게 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,11 @@
 package lotto.view;
 
 public class OutputView {
+
+    private static final String NEW_LINE = "\n";
+    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
+
+    public static void errorMessage(IllegalArgumentException e) {
+        System.out.printf(ERROR_MESSAGE_FORMAT+NEW_LINE,e.getMessage());
+    }
 }

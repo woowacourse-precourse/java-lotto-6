07f1b28aaeb4252ca220493f3ea3ee9b591d7236
yuseonjun <seonjun0906@gmail.com>
2023-11-02 17:56:18 +0900

    feat(LottoShop): 구매자의 로또들을 생성하는 메서드 구현

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,8 +1,22 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoShop {
 
-    public void createByBuyerLotto(Amount amount) {
+    private final NumberGenerator numberGenerator;
+
+    public LottoShop(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public List<Lotto> createByBuyerLotto(Amount amount) {
         int lottoCount = amount.getLottoCount();
+        List<Lotto> playerLottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            playerLottos.add(new Lotto(numberGenerator.generate()));
+        }
+        return playerLottos;
     }
 }

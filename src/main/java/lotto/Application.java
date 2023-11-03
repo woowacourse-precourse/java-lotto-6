5445973508e6887aa8f7d39efc2361a7ac9ba3a8
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 04:04:41 +0900

    Refactor: "로또 변수 추가"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,14 +4,14 @@ import lotto.model.Purchase;
 import lotto.model.RandomLotto;
 import lotto.view.InputView;
 
-import java.io.PipedReader;
-import java.net.CookieHandler;
-import java.util.*;
+import java.util.List;
 
 public class Application {
 
     private static int purchasePieces;
+    private static List<Integer> lottoNum;
     private static final InputView inputView = new InputView();
+    private static final RandomLotto random = new RandomLotto();
 
     public static void purchaseCount(String purchasingAmount) {
         try {
@@ -24,8 +24,8 @@ public class Application {
 
     public static void randomRepeat() {
         for (int i = 0; i < purchasePieces; i++) {
-            RandomLotto random = new RandomLotto();
-            System.out.println(random.listSort());
+            lottoNum = random.listSort(random.generateNum());
+            System.out.println(lottoNum);
         }
     }
 

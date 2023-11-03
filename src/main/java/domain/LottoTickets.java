deb6dc7b3c546826a5e6266a8a6946d080b84cc1
songsunkook <songsunkook@gmail.com>
2023-11-03 22:43:39 +0900

    feat: 로또 티켓 구매 구현

--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -1,4 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LottoTickets {
+    private final List<Lotto> lottoList = new ArrayList<>();
+
+    public void add(int count) {
+        for (int i = 0; i < count; i++) {
+            lottoList.add(new Lotto());
+        }
+    }
+
+    int size() {
+        return lottoList.size();
+    }
 }
--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,4 +1,12 @@
 package domain;
 
 public class Player {
+    private final LottoTickets lottoTickets = new LottoTickets();
+    public void buyLottoTickets(int count) {
+        lottoTickets.add(count);
+    }
+
+    int getTicketCount() {
+        return lottoTickets.size();
+    }
 }

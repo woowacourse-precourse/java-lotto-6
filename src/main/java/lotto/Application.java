43f2ca997900d6227d3317d536ba81a0f532192e
이소정 <grace@isojeong-ui-iMac.local>
2023-11-03 15:07:27 +0900

    수익률 계산 클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("구입금액을 이력해 주세요.");
-        Price price = new Price(Integer.parseInt(Console.readLine()));
+        System.out.println("구입금액을 입력해 주세요.");
+        Price price = new Price(checkExcept());
         int size = price.countLotto(price);
         System.out.println(size + "개를 구매했습니다.");
 
@@ -22,9 +22,18 @@ public class Application {
 
         Lotto.compareTo(lottos, lotto, bonusNumber);
 
-        printResult();
+        printResult(price.price);
     }
 
+    public static int checkExcept(){
+        int inputPrice = 0;
+        try {
+            inputPrice = Integer.parseInt(Console.readLine());
+        } catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] 1,000원 단위로 입력해주세요.");
+        }
+        return inputPrice;
+    }
     public static Lotto makeLotto(String input) {
         List<Integer> numbers = new ArrayList<>();
         for(String n : input.split(",")) {
@@ -34,13 +43,14 @@ public class Application {
         return lotto;
     }
 
-    public static void printResult() {
-        System.out.println("당첨 통계\n---");
+    public static void printResult(int price) {
+        System.out.println("\n당첨 통계\n---");
         System.out.println("3개 일치 (5,000원) - " + Ranks.FIFTH.amount + "개");
         System.out.println("4개 일치 (50,000원) - " + Ranks.FOURTH.amount + "개");
         System.out.println("5개 일치 (1,500,000원) - " + Ranks.THIRD.amount + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Ranks.SECOND.amount + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + Ranks.FIRST.amount + "개");
+        System.out.println("총 수익률은 " + Calculation.calculate(price) + "%입니다.");
     }
 
 
--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -1,22 +1,24 @@
 package lotto;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Price {
-    private final int price;
+    public final int price;
 
     public Price(int price) {
-        validate(price);
+       // validate(price);
         this.price = price;
     }
 
-    private void validate(int price) {
-        if (price % 1000 != 0)
+   /* private void validate(int price) {
+        if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
-    }
+        }
+    }*/
 
     public int countLotto(Price price) {
         return price.price / 1000;

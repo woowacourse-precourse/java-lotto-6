fd2ac3201b570e9b7aa8762a0f37770865d05ea7
junOTL <naqp99@gmail.com>
2023-11-03 22:52:33 +0900

    feat(LottoVendingMachine): 로또 번호 랜덤 생성 기능

--- src/main/java/lotto/domain/LottoVendingMachine.java
+++ src/main/java/lotto/domain/LottoVendingMachine.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class LottoVendingMachine {
     private int lottoTicketNumber;
 
@@ -8,6 +12,11 @@ public class LottoVendingMachine {
 
     public void purchaseLotto(int amount) {
         lottoTicketNumber = amount / 1000;
+        List<Integer> numbers = getRandomNumbers();
+    }
+
+    public List<Integer> getRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     public int getLottoTicketNumber() {

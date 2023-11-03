b5f15216abdc536cfb93da7e86096aaeca0f2bdb
minsang-alt <mazinger97@naver.com>
2023-11-03 19:16:21 +0900

    feat: 로또 클래스 구현
    로또 생성자에서 유효성 검사
    - 필요한 숫자들이 들어가 있는가
    - 중복된 숫자는 없는가
    - 숫자가 범위안에 들어가 있는가 <br>
    로또의 숫자를 반환할때 수정불가능하게 리턴<br>
    해당 toString 메서드를 사용하면 로또 클래스가 보유하고 있는 숫자들 출력

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,50 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateScopeNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 1개의 로또를 발행할 때 6개의 숫자만 존재합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또번호는 중복 숫자가 있어서는 안됩니다.");
+        }
+    }
+
+    private void validateScopeNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+
+    }
+
+
 }

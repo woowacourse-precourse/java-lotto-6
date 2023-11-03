44376e48213e855de661bf5c400280074a530814
WOONBE <kepull2918@naver.com>
2023-11-03 12:43:44 +0900

    [feat] 입력된 값의 중복여부와 배수인지 여부를 검증하는 로직 추가

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -2,6 +2,7 @@ package lotto.validation;
 
 import lotto.view.ErrorMessage;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Validator {
@@ -9,6 +10,8 @@ public class Validator {
     private static final int MAX_NUMBER_LENGTH = 6;
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 45;
+
+    private static final int DIVISOR = 1000;
 
 
     public boolean isValidLength(List<String> list){
@@ -49,6 +52,33 @@ public class Validator {
         return true;
     }
 
+    public boolean isMultipleNumber(int number, int divisor){
+        divisor = DIVISOR;
+        if(number < divisor || number % divisor != 0){
+            ErrorMessage.multipleErrorMessage(divisor);
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    //당첨값 입력시 적용
+    public boolean isDuplicateNumberInList(List<Integer> list){
+        HashSet<Integer> set = new HashSet<>(list);
+        if(list.size() != set.size()){
+            ErrorMessage.duplicatedNumberInListError();
+            throw new IllegalArgumentException();
+        }
+        return true;
+
+    }
+    //보너스값 넣을때 사용
+    public boolean isDuplicateNumber(List<Integer>list, int number){
+        if(list.contains(number)){
+            ErrorMessage.duplicateErrorMessage();
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
 
 
 }

ff2154c6854213419c6c858961eff494e14f6de3
heiler <threepebbles@naver.com>
2023-11-03 20:04:38 +0900

    Test: Lotto 설계에 대한 테스트 코드 추가

---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 1~45 범위가 아닌 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNumberInRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 6, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다른 로또 번호와 비교하여 몇개의 숫자가 일치하는지 개수를 계산하는 함수다. 일치하는 개수가 틀리면 예외가 발생한다.")
+    @Test
+    void getMatchedCount_테스트() {
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 30, 31));
+        assertThat(lotto1.getMatchedCount(lotto2))
+                .isEqualTo(4);
+    }
+
+}

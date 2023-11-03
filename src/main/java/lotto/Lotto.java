f66a51a6d352973f8bb9a1b6a19fbe5e4f8bbe24
SunHyeok <cghhld@naver.com>
2023-11-03 17:09:42 +0900

    fat: 유효성 검사 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         for (int number : numbers) {
             if (number < 1 || number > 45) {

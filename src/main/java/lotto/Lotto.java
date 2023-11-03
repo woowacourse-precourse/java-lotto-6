3d48eec5249341da75af983120accf9c22b560b1
Mingeun Park <mindal99546@gmail.com>
2023-11-03 18:36:35 +0900

    feat: 두 로또에서 겹치는 번호의 개수를 세는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,11 +10,23 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto issue(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public int countHitNumbers(Lotto comparisonTarget) {
+        int hit = 0;
+        for (int number: comparisonTarget.numbers) {
+            if (this.numbers.contains(number))
+                hit++;
+        }
+        return hit;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +24,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("발행한 로또와 당첨번호를 비교하여 일치하는 숫자의 개수를 반환한다.")
+    @Test
+    void countHitNumbers() {
+        // given
+        List<Integer> winningNumbers = List.of(40, 41, 42, 43, 44, 45);
+        Lotto lotto0Hit = Lotto.issue(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto1Hit = Lotto.issue(List.of(1, 2, 3, 4, 5, 45));
+        Lotto lotto6Hit = Lotto.issue(winningNumbers);
+        // when
+        Lotto winningLotto = Lotto.issue(winningNumbers);
+        // then
+        assertThat(lotto0Hit.countHitNumbers(winningLotto)).isEqualTo(0);
+        assertThat(lotto1Hit.countHitNumbers(winningLotto)).isEqualTo(1);
+        assertThat(lotto6Hit.countHitNumbers(winningLotto)).isEqualTo(6);
+    }
 }

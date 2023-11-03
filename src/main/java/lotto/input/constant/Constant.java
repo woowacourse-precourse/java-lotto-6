ca6daef73b31256c743371d58ae26a9923f4035a
leedonghyun <skydh507@ajou.ac.kr>
2023-11-04 02:28:03 +0900

    refactor : 코드 컨벤션 적용

--- src/main/java/lotto/input/constant/Constant.java
+++ src/main/java/lotto/input/constant/Constant.java
@@ -1,9 +1,9 @@
 package lotto.input.constant;
 
 public class Constant {
+
     public static final String PURCHASEAMOUNTINPUT = "구입금액을 입력해 주세요.";
 
     public static final String PURCHASEAMOUNTINPUT_ERROR = "[ERROR] 올바르지 않는 값을 입력했습니다. 1000의 배수의 가격을 입력해주세요.";
-
 
 }

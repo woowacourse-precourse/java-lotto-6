3c07c8d4ec170ec67dbff3f9cc89e68213df0691
kimky0320 <kim3268418@naver.com>
2023-11-03 11:17:30 +0900

    feat: 예외 발생 메시지 추가
    구매한 로또의 갯수와 생성한 로또의 갯수가 다를때 출력

--- src/main/java/lotto/validation/LottoValidator.java
+++ src/main/java/lotto/validation/LottoValidator.java
@@ -1,6 +1,7 @@
 package lotto.validation;
 
 import lotto.domain.Lotto;
+import lotto.view.OutputView;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class LottoValidator {
 
     public static void ticketSizeValidate(List<Lotto> numbers, Integer size) {
         if (numbers.size() != size) {
+            OutputView.errorMessage(LottoErrorMessage.INCORRECT_LOTTO.getMessage());
             throw new IllegalArgumentException();
         }
     }

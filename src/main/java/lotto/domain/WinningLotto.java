3ba369d3f3d79ed6a942d32c91ad48af7abb6858
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-11-03 23:23:42 +0900

    feat: 사용자 당첨 로또 번호 입력을 검증하고, 생성된 로또 중 당첨 숫자에서 몇 개 맞았는지 반환하는 기능 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,75 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.domain.constant.ExceptionMessage.INCLUDE_CHAR;
+import static lotto.domain.constant.ExceptionMessage.INVALID_RANGE_LOTTO_NUMBER;
+
+public class WinningLotto {
+
+    private final Lotto lotto;
+    private final int bonusNum;
+
+    private WinningLotto(Lotto lotto, int bonusNum) {
+        this.lotto = lotto;
+        this.bonusNum = bonusNum;
+    }
+
+    public static WinningLotto createWinningLotto(List<String> numbersInput, String bonusNumInput) {
+        List<Integer> numbers = validateNumbers(numbersInput);
+        int bonusNum = validateBonusNumber(bonusNumInput);
+        duplicateNumbersAndBonusNumber(numbers, bonusNum);
+
+        return new WinningLotto(new Lotto(numbers), bonusNum);
+    }
+
+    public long countMatchLotto(Lotto generateLotto) {
+        return this.lotto
+                .countingMatchNumbers(generateLotto);
+    }
+
+    public boolean matchBonusNum(Lotto generateLotto) {
+        return generateLotto.containsNumber(bonusNum);
+    }
+
+    private static int validateBonusNumber(String bonusNumInput) {
+        includeChar(bonusNumInput);
+        int bonusNum = validateRange(bonusNumInput);
+
+        return bonusNum;
+    }
+
+    private static List<Integer> validateNumbers(List<String> numbersInput) {
+        numbersInput.stream().forEach(WinningLotto::includeChar);
+        List<Integer> numbers = numbersInput.stream()
+                .map(WinningLotto::validateRange)
+                .collect(Collectors.toList());
+
+        return numbers;
+    }
+
+    private static void duplicateNumbersAndBonusNumber(List<Integer> numbers, int bonusNum) {
+        if (numbers.contains(bonusNum)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void includeChar(String numberInput) {
+        if (!numberInput.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(INCLUDE_CHAR.getErrorMessage());
+        }
+    }
+
+    private static int validateRange(String numberInput) {
+        try {
+            int number = Integer.parseInt(numberInput);
+
+            Lotto.validateLottoRange(number);
+
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_RANGE_LOTTO_NUMBER.getErrorMessage());
+        }
+    }
+}

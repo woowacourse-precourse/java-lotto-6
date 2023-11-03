530656ba84c4fe664ba774c4e27cb3e2a6418e3d
sami355-24 <ddung355@gmail.com>
2023-11-03 18:51:58 +0900

    feat(Message/ExceptionMessage): 구매 금액에 대한 예외 발생시 출력할 문구를 관리하는 enum 클래스 생성

---
+++ src/main/java/lotto/Message/ExceptionMessage/PurchaseErrorMessage.java
@@ -0,0 +1,18 @@
+package lotto.Message.ExceptionMessage;
+
+public enum PurchaseErrorMessage {
+
+    RANGE_ERROR_MESSAGE("1000이상 100,000이하의 값만 입력할 수 있습니다. \n 입력값 : %s"),
+    DIVISIBILITY_ERROR_MESSAGE("1000원으로 나누어 떨어지는 수만 입력 가능합니다. \n 입력값 : %s"),
+    ;
+
+    private String message;
+
+    PurchaseErrorMessage(String message) {
+        this.message = PrefixMessage.ERROR_MESSAGE.getMessage() + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

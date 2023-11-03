9f36677b870c1d5c509fad79fe0ee8e186ec2918
unknown <y2kf9a0q@gmail.com>
2023-11-03 18:01:32 +0900

    feat(lotto): Lotto.java - sort(): 로또 번호 오름차순 정렬

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +18,8 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    public void sort() {
+        Collections.sort(numbers);
+    }
 }

d7bd02ae501c0998d2522295e523d81f07e39082
Youngwoo Choi <allnstar@naver.com>
2023-11-03 16:18:35 +0900

    feat: add functions for making a group of sorted lotto

--- src/main/java/lotto/Issue.java
+++ src/main/java/lotto/Issue.java
@@ -1,16 +1,34 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Issue {
     private int lottoQuantity;
+    private List<Lotto> lottoGroup;
 
     void inputCash() {
         int cash = Integer.parseInt(Console.readLine());
         lottoQuantity = cash / 1000;
     }
 
+    private List<Integer> getNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    void makeSortedLotto() {
+        for (int i = 0; i == lottoQuantity; i++) {
+            Lotto transfer = new Lotto(getNumbers());
+
+            transfer.sort();
+            lottoGroup.add(transfer);
+        }
+    }
+
     void playIssue() {
         inputCash();
+        makeSortedLotto();
     }
 }

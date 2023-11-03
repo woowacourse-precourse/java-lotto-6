8e1e0188f558e764f2782e7a9033b8a2173b9da6
ChaeAg <dydkglel123@gmail.com>
2023-11-03 13:44:36 +0900

    refactor(InputView): 로또 번호 입력을 받을 때 숫자로 변환 후 리턴하도록 수정, 동시에 예외 처리 수행

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -2,6 +2,7 @@ package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
@@ -11,8 +12,21 @@ public class InputView {
         return amount;
     }
 
-    public List<String> inputWinningNumbers() {
-        return List.of(Console.readLine().split(","));
+    public List<Integer> inputWinningNumbers() {
+        List<String> winningNumbersStr = List.of(Console.readLine().split(","));
+        if (String.join("", winningNumbersStr).isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 비었습니다.");
+        }
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        try {
+            for (int i = 0; i < winningNumbersStr.size(); i++) {
+                winningNumbers.add(Integer.parseInt(winningNumbersStr.get(i)));
+            }
+            return winningNumbers;
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
+        }
     }
 
     public String inputBonusNumber() {

4281673587b076265487a8e07dc918ac41e7581f
euics <pjh575157@gmail.com>
2023-11-03 11:49:47 +0900

    feat(Exception): validatePurchaseAmount 메서드 구현
    구입금액 단위 검사.
    - 구입 단위 유효성 검사 메서드
    - "[ERROR] 구입금액은 1,000단위이어야 합니다." 출력

--- src/main/java/lotto/exception/PurchaseExceptionMessage.java
+++ src/main/java/lotto/exception/PurchaseExceptionMessage.java
@@ -11,4 +11,10 @@ public class PurchaseExceptionMessage extends IllegalArgumentException{
             throw new PurchaseExceptionMessage("[ERROR] 구입금액이 유효하지 않습니다.");
         }
     }
+
+    public static void validatePurchaseAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new PurchaseExceptionMessage("[ERROR] 구입금액은 1,000단위이어야 합니다.");
+        }
+    }
 }

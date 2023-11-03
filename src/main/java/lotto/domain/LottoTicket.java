82b816192be053e2da40893293341ac7a345448b
euics <pjh575157@gmail.com>
2023-11-03 11:42:52 +0900

    feat(LottoTicket): LottoTicket 기본 메서드 구현(buy, generateRandomLotto, getLottoTickets)
    - `buy`(로또 구매 메서드)
      - 로또의 개수대로 로또 구매 진행
    - `generateRandomLotto`
      - 1~45 사이의 6개 랜덤 번호 생성
    - `getLottoTickets`
      - LottoTickets 반환

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicket {
+    private final List<Lotto> lottoTickets;
+
+    public LottoTicket(){
+        this.lottoTickets = new ArrayList<>();
+    }
+
+    public LottoTicket buy(int amount){
+        int numberOfLotto = amount / 1_000;
+
+        for (int i = 0; i < numberOfLotto; i++) {
+            lottoTickets.add(generateRandomLotto());
+        }
+
+        return this;
+    }
+
+    private Lotto generateRandomLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return lottoTickets;
+    }
+}

100562117450113a3ad5464476782fc516b0f5b1
AmenPark <puzzlistpam@gmail.com>
2023-11-03 19:27:51 +0900

    중복체크 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,32 @@
 package lotto;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> checker = new HashSet<>();
+        for (int i : numbers) {
+            if (checker.contains(i)) {
+                throw new IllegalArgumentException();
+            }
+            checker.add(i);
         }
     }
 

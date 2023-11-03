e13fcf83693170f7ba89e63bb46364aa739193f3
euics <pjh575157@gmail.com>
2023-11-03 14:46:48 +0900

    test: whenGetLottoTickets_thenReturnsCopyOfLottoTicketList 메서드 구현
    새로운 리스트를 반환하여 원본 리스트가 수정되지 않는지 확인하는 메서드

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTicketsTest {
@@ -15,6 +17,19 @@ class LottoTicketsTest {
 
         assertNotNull(lottoTickets);
         assertEquals(5, lottoTickets.getLottoTickets().size());
+    }
+
+    @Test
+    @DisplayName("LottoTickets에서 로또 티켓 목록을 가져오면 복사된 목록이 반환됨")
+    void whenGetLottoTickets_thenReturnsCopyOfLottoTicketList() {
+        int purchaseAmount = 3_000;
+        LottoTickets lottoTickets = LottoTickets.buy(purchaseAmount);
+        List<Lotto> originalTickets = lottoTickets.getLottoTickets();
+
+        List<Lotto> ticketsCopy = lottoTickets.getLottoTickets();
+        ticketsCopy.remove(0);
+
+        assertNotEquals(ticketsCopy.size(), originalTickets.size(), "원본 목록과 복사된 목록의 크기가 달라야 함");
     }
 
 }

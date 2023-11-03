813a893048237710a8a8123d238573d1ab3590fb
alscjf1329 <alscjf1329@gmail.com>
2023-11-03 23:07:57 +0900

    feat(LottoUtil): validateLottoNum 메소드 작성
    인자로 받는 숫자가 로또 번호 조건인 1~45사이에 존재하는지 확인하는 validateLottoNum 메소드를 작성했습니다.

---
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -0,0 +1,12 @@
+package lotto.utils;
+
+import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
+
+public class LottoUtil {
+    public boolean validateLottoNum(int num) {
+        if (1 <= num && num <= 45) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
+        }
+        return true;
+    }
+}

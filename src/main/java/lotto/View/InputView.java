4e86e767b439f5aef304180a90072b43074acfb9
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-03 20:25:17 +0900

    feat(InputView): 구입금액 입력 기능 추가
    docs: 구입금액 입력 체크 완료 및 InputView 기능 목록 추가 완료

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,7 +1,14 @@
 package lotto.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private static final String INPUT_AMOUNT = "구입 금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public String printBuyLotto(){
+        System.out.println(INPUT_AMOUNT);
+        return Console.readLine();
+    }
 }

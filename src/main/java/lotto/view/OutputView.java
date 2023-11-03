0fe3d418e504e0e2c996c1d7b7c6e8f9ea14c80b
qncj5224 <qncj5224@naver.com>
2023-11-03 23:48:21 +0900

    feat : 구입한 로또의 개수를 출력하는 메서드를 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,15 @@
 package lotto.view;
 
 import java.util.List;
+import lotto.controller.InputProcessor;
 
 public class OutputView {
   private final static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
   private final static String INPUT_WINNINGNUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
   private final static String INPUT_BOUNSNUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+  private final static String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+  private lotto.controller.InputProcessor InputProcessor;
+
   public static void printInputMoneyMessage() {
     System.out.println(INPUT_MONEY_MESSAGE);
   }
@@ -16,6 +20,12 @@ public class OutputView {
 
   public static void printInputBonusNumberMessage() {
     System.out.println(INPUT_BOUNSNUMBER_MESSAGE);
+  }
+
+  public void printLottoCountMessage() {
+    int LottoCount = InputProcessor.calculateLottoCount(Integer.parseInt(InputProcessor.getInput));
+    System.out.print(LottoCount);
+    System.out.println(LOTTO_COUNT_MESSAGE);
   }
 
   public String printLottoNumbers(List<Integer> lotto) {

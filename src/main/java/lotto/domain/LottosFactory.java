ed7d6fa2767b5a8de3e76c0939fadee58907b2a4
gosu <q63530@naver.com>
2023-11-03 15:49:41 +0900

    test: LottoFactory 테스트 구현

--- src/main/java/lotto/domain/LottosFactory.java
+++ src/main/java/lotto/domain/LottosFactory.java
@@ -8,21 +8,21 @@ public class LottosFactory {
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
     private static final int LOTTO_SIZE = 6;
-    private static final int AMOUNT_DIVIDE = 1000;
+    private static final int TEN_THOUSAND = 1000;
 
-    public Lottos createLottos(int amount) {
+    public Lottos createLottos(int money) {
         List<Lotto> lottos = new ArrayList<>();
-        validateAmount(amount);
-        int sheet = amount / AMOUNT_DIVIDE;
+        validateAmount(money);
+        int amount = money / TEN_THOUSAND;
 
-        for (int i = 0; i < sheet; i++) {
+        for (int i = 0; i < amount; i++) {
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_SIZE)));
         }
         return new Lottos(lottos);
     }
 
-    private void validateAmount(int amount) {
-        if (amount % AMOUNT_DIVIDE != 0) {
+    private void validateAmount(int money) {
+        if (money % TEN_THOUSAND != 0) {
             throw new IllegalArgumentException("구입 금액은 1000의 배수여야 합니다");
         }
     }

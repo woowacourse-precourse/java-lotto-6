68975c7a3edeac39b1701cbffd35d9546c200eb3
ykmxxi <ykmxxi97@gmail.com>
2023-11-03 11:19:00 +0900

    test(SetTest): Set.contains 메소드 학습 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,6 +10,9 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -30,6 +34,20 @@ public class SetTest {
 
         assertNotEquals(4, result);
         assertEquals(3, result);
+    }
+
+    @ParameterizedTest(name = "입력값 : {0}")
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set.contains(): Set에 값이 존재하면 true 반환")
+    void givenNumbers_whenContains_thenReturnTrue(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest(name = "입력값 : {0}, 결과 : {1}")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set.contains(): Set에 값이 존재하면 true, 존재하지 않으면 false 반환")
+    void givenNumbers_whenContains_thenReturnTrueOrFalse(int number, boolean expected) {
+        assertEquals(expected, numbers.contains(number));
     }
 
 }

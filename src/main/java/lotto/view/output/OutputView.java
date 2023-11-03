9f40812daa84036026f946afefe9d248346e74f0
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-04 01:20:57 +0900

    :sparkles: [Feat] 당첨 내역 출력 (리팩토링 필요)

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -3,9 +3,12 @@ package lotto.view.output;
 import static lotto.constant.PrintOutMessage.PLZ_INPUT_BONUS_NUMBER;
 import static lotto.constant.PrintOutMessage.PLZ_INPUT_PRICE;
 import static lotto.constant.PrintOutMessage.PLZ_INPUT_WINNER_NUMBER;
+import static lotto.constant.PrintOutMessage.PRINT_EARNING_PERCENT;
 import static lotto.constant.PrintOutMessage.PRINT_LOTTO_COUNT;
 import static lotto.constant.PrintOutMessage.PRINT_WINNING_STATISTICS;
 
+import java.util.Map;
+import lotto.constant.PrintOutMessage;
 import lotto.model.Lotto;
 
 public class OutputView {
@@ -31,5 +34,18 @@ public class OutputView {
 
     public void printWinningStatistics() {
         System.out.println(PRINT_WINNING_STATISTICS.message);
+    }
+
+    public void printMatchingCount(Map<Integer, Integer> matchingCount) {
+        System.out.println(PrintOutMessage.THREE_MATCH.message + matchingCount.getOrDefault(3, 0) + "개");
+        System.out.println(PrintOutMessage.FOUR_MATCH.message + matchingCount.getOrDefault(4, 0) + "개");
+        System.out.println(PrintOutMessage.FIVE_MATCH.message + matchingCount.getOrDefault(5, 0) + "개");
+        System.out.println(PrintOutMessage.FIVE_AND_BONUS_MATCH.message + matchingCount.getOrDefault(5, 0) + "개");
+        System.out.println(PrintOutMessage.SIX_MATCH.message + matchingCount.getOrDefault(6, 0) + "개");
+    }
+
+    public void printEarningPercent(int earningMoney, int price) {
+        double earningPercent = Math.round((double) earningMoney / price * 100 * 100) / 100.0;
+        System.out.println(PRINT_EARNING_PERCENT.message + earningPercent + "%입니다.");
     }
 }

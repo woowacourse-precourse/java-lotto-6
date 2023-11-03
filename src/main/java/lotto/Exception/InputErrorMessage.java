c746109455cfcaf93c88b6f1fcf20769a0e2a524
siho-song <thdtlgh234@naver.com>
2023-11-03 15:59:23 +0900

    [Feat] 에러 메시지 관련 Enum클래스 구현

---
+++ src/main/java/lotto/Exception/InputErrorMessage.java
@@ -0,0 +1,21 @@
+package lotto.Exception;
+
+public enum InputErrorMessage {
+    INVALID_UNIT("[ERROR] 로또 구입 금액은 1000원 단위어야 합니다."),
+    INVALID_VALUE("[ERROR] 구입금액은 숫자만 가능합니다."),
+    INVALID_DELIMITER("[ERROR] 번호는 ',' 로 구분되어야 합니다."),
+    INVALID_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATED_NUMBER("[ERROR] 당첨 번호로 중복된 수를 입력할 수 없습니다."),
+    DUPLICATED_BONUS_NUMBER("[ERROR] 당첨 번호와 중복된 보너스 번호를 입력할 수 없습니다."),
+    EXACTLY_ONE_NUMBER("[ERROR] 하나의 수만 입력해야 합니다.");
+
+    private final String value;
+
+    InputErrorMessage(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}

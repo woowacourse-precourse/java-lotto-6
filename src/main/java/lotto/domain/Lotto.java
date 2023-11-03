07b738df7f14bf01f3bd31265f6c2ae5dcf47292
suihub <qkdcrt@gmail.com>
2023-11-03 18:23:24 +0900

    <feat(Lotto)> : "로또 번호를 반환하는 메서드 구현"
    - Lotto 객체의 번호를 변경불가 상태(읽기 전용)로 만들어 반환.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.exception.LottoNumberException;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -10,5 +11,9 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         LottoNumberException.validate(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 }

1f8b9df6da2829e6d559cbe6cc9273207e7bf55e
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-11-03 16:40:46 +0900

    Test: amount 테스트코드 구현

---
+++ src/test/java/lotto/domain/AmountTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import lotto.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AmountTest {
+
+    @Test
+    void 음수가_주어진_경우_예외를_발생한다() {
+        // Given
+        int amount = -1000;
+
+        // When
+        // Then
+        Assertions.assertThatThrownBy(() -> new Amount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.AMOUNT_IS_POSITIVE.message());
+    }
+
+    @Test
+    void 숫자0이_주어진_경우_예외를_발생한다() {
+        // Given
+        int amount = 0;
+
+        // When
+        // Then
+        Assertions.assertThatThrownBy(() -> new Amount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.AMOUNT_IS_POSITIVE.message());
+    }
+
+    @Test
+    void 숫자1000_으로_나누어_떨어지지_않는_경우_예외를_발생한다() {
+        // Given
+        int amount = 1200;
+
+        // When
+        // Then
+        Assertions.assertThatThrownBy(() -> new Amount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.AMOUNT_IS_POSITIVE.message());
+    }
+}

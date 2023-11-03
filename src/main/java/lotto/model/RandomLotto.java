43eb0057afa89c68d000b1c680627169b902f735
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 03:30:10 +0900

    Refactor: "RandomLotto클래스에 정렬기능 추가"

--- src/main/java/lotto/model/RandomLotto.java
+++ src/main/java/lotto/model/RandomLotto.java
@@ -9,7 +9,19 @@ public class RandomLotto {
     private static final int RANDOM_NUM_START = 1;
     private static final int RANDOM_NUM_END = 45;
     private static final int RANDOM_NUM_LENGTH = 6;
+    private final List<Integer> noSortList;
+
+    public RandomLotto() {
+        noSortList = generateNum();
+    }
+
     public List<Integer> generateNum() {
         return Randoms.pickUniqueNumbersInRange(RANDOM_NUM_START, RANDOM_NUM_END, RANDOM_NUM_LENGTH);
+    }
+
+    public List<Integer> listSort() {
+        List<Integer> listSort = new ArrayList<>(noSortList);
+        Collections.sort(listSort);
+        return listSort;
     }
 }

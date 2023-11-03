2506483b8db5e5ed7877e4e95b8c7fe8fb4cd0d7
eora21 <eora21@naver.com>
2023-11-03 18:51:07 +0900

    refactor: 💡 수정 기본적으로 주어진 테스트코드 통과할 수 있도록 작성, 패키지 변경

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,33 @@
+package domain;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateDuplicate(numbers);
+        this.lottoNumbers = convertLottoNumbers(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != Set.of(numbers).size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private List<LottoNumber> convertLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::new)
+                .toList();
+    }
+}

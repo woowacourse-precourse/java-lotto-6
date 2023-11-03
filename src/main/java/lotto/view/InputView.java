69e8cd7a7da4a3cb0e76daeffca20142b0dee5c2
Yang-soeun <soeun8636@naver.com>
2023-11-03 20:35:35 +0900

    [test]: 당첨 번호를 문자열로 입력받으면 리스트로 반환하는 메소드 테스트 코드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.util.Arrays;
 import java.util.List;
 
-public class inputView {
+public class InputView {
     private final String INPUT_BUYING_COST_MESSAGE = "구입금액을 입력해 주세요.";
     private final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";

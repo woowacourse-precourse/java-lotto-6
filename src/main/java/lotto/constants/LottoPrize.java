606142db8cc13785be987b5aa9ddc31ec08bc867
IMWoo94 <leemeawoo94@gmail.com>
2023-11-03 19:16:01 +0900

    refactor : 로또 상수에 상금 추가
    로또 상수에 상금 추가

--- src/main/java/lotto/constants/LottoPrize.java
+++ src/main/java/lotto/constants/LottoPrize.java
@@ -1,20 +1,27 @@
 package lotto.constants;
 
 public enum LottoPrize {
-    FIRST_PRIZE(6),
-    SECOND_PRIZE(-1),
-    THIRD_PRIZE(5),
-    FOURTH_PRIZE(4),
-    NO_PRIZE(0);
+    FIRST_PRIZE(6, 2000000000),
+    SECOND_PRIZE(-1, 30000000),
+    THIRD_PRIZE(5, 1500000),
+    FOURTH_PRIZE(4, 50000),
+    FIFTH_PRIZE(3, 5000),
+    NO_PRIZE(0, 0);
 
     private int matchedNumbers;
+    private int winningPrize;
 
-    LottoPrize(int matchedNumbers) {
+    LottoPrize(int matchedNumbers, int winningPrize) {
         this.matchedNumbers = matchedNumbers;
+        this.winningPrize = winningPrize;
     }
 
     public int getMatchedNumbers() {
         return matchedNumbers;
+    }
+
+    public int getWinningPrize() {
+        return winningPrize;
     }
 
     public static LottoPrize getResultByMatchedNumbers(int matchedNumbers) {

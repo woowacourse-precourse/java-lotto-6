9cb416f6f999045e6019e2a363dae1d1494cf5bb
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 17:15:05 +0900

    feat(): 당첨 번호 입력시 숫자가 6개가 아니면 예외, 당첨 번호가 중복되면 예외

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.Model;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,11 +14,26 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    @Override
+    public String toString() {
+        return numbers +"";
+    }
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicate = new HashSet<>(numbers);
+
+        if (nonDuplicate.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될수 없습니다.");
+        }
+    }
 }

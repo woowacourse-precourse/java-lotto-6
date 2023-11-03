91001b1e430a2d50ad39df3f56e3c160060ec598
현 <sohyun95277@gmail.com>
2023-11-03 15:37:46 +0900

    feat(Prize): 경품에 대한 정보가 담긴 enum
    OutputMessage 에 타입을 맞춰 작성

---
+++ src/main/java/lotto/constant/Prize.java
@@ -0,0 +1,31 @@
+package lotto.constant;
+
+public enum Prize {
+    FIRST(6, "2,000,000,000", 2000000000),
+    SECOND(5, "30,000,000", 30000000),
+    THIRD(5, "1,500,000", 1500000),
+    FOURTH(4, "50,000", 50000),
+    FIFTH(3, "5,000", 5000);
+
+    private final int match;
+    private final String panel;
+    private final int money;
+
+    Prize(int match, String panel, int money) {
+        this.match = match;
+        this.panel = panel;
+        this.money = money;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public String getPanel() {
+        return panel;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}

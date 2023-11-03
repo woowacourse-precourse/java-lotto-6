169547961afc58456a57fc655be3912e0d582f25
kimdongyeon <do121712@naver.com>
2023-11-03 23:08:05 +0900

    feat : LottoController 클래스 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,21 @@
+package lotto.controller;
+
+import lotto.domain.Player;
+import lotto.service.LottoService;
+
+public class LottoController {
+
+    private LottoService lottoService = new LottoService();
+
+    public void run(){
+
+    }
+
+    public void start(){
+
+    }
+
+    public void finish(){
+
+    }
+}

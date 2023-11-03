d787cf518036618df0f425caa099f5e64602c2e9
WonyuChoi <phillip07290@naver.com>
2023-11-03 16:46:54 +0900

    feat: 로또의 개수만 전달받아 로또를 생성

--- src/main/java/model/GameManager.java
+++ src/main/java/model/GameManager.java
@@ -1,12 +1,17 @@
 package model;
 
+import java.util.List;
+
 public class GameManager {
 
-    private GameManager() {
+    private final LottosWithBonus lottos;
+    private LottosWithBonus answerLotto;
 
+    private GameManager(final LottosWithBonus lottos) {
+        this.lottos = lottos;
     }
 
     public static GameManager createDefault(final int lottoCount) {
-        return new GameManager();
+        return new GameManager(LottosWithBonus.createAsManyAsCount(lottoCount));
     }
 }

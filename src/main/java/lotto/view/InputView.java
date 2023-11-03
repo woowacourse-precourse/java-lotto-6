8c0526a73cc8d77c57441a976e1563bc68285990
kimky0320 <kim3268418@naver.com>
2023-11-03 11:15:21 +0900

    refactor: 예외 입력시 에러 발생후 입력 다시받게 수정
    try-catch와 재귀함수 활용하여 구현 정상값 입력할때까지 재귀로 함수 호출하여 정상입력시 입력값 반환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,31 +9,47 @@ import java.util.Arrays;
 public class InputView {
 
     public Integer inputMoney() {
-        OutputView.moneyMessage();
-        String money= Console.readLine();
+        try {
+            OutputView.moneyMessage();
+            String money = Console.readLine();
 
-        Validator.moneyValidate(money);
+            Validator.moneyValidate(money);
 
-        return Integer.parseInt(money);
+            return Integer.parseInt(money);
+        } catch (IllegalArgumentException e) {
+            return inputMoney();
+        }
     }
 
     public ArrayList<Integer> winningNumber() {
-        OutputView.winningMessage();
-        String[] winningNumber = Console.readLine().split(",");
+        try {
+            OutputView.winningMessage();
+            String[] winningNumber = Console.readLine().split(",");
 
-        Validator.winningValidate(winningNumber);
+            Validator.winningValidate(winningNumber);
 
-        return new ArrayList<>(Arrays.stream(winningNumber)
-                                    .map(Integer::parseInt)
-                                    .toList());
+            return createWinningNumber(winningNumber);
+        } catch (IllegalArgumentException e) {
+            return winningNumber();
+        }
     }
 
     public Integer bonusNumber(ArrayList<Integer> winningNumber) {
-        OutputView.bonusMessage();
-        String bonusNumber = Console.readLine();
+        try {
+            OutputView.bonusMessage();
+            String bonusNumber = Console.readLine();
 
-        Validator.bonusValidate(bonusNumber, winningNumber);
+            Validator.bonusValidate(bonusNumber, winningNumber);
 
-        return Integer.parseInt(bonusNumber);
+            return Integer.parseInt(bonusNumber);
+        } catch (IllegalArgumentException e) {
+            return bonusNumber(winningNumber);
+        }
+    }
+
+    private ArrayList<Integer> createWinningNumber(String[] winningNumber) {
+        return new ArrayList<>(Arrays.stream(winningNumber)
+                .map(Integer::parseInt)
+                .toList());
     }
 }

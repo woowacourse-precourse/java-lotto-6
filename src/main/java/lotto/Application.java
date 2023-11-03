d15cafc44be31d951d4c3db25f1e0a5882c6e73f
SeongIl <biuld1234@gmail.com>
2023-11-03 18:32:00 +0900

    feat: add 9. 당첨 결과 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -126,6 +126,14 @@ public class Application {
 
         rateInvestment = String.format(".1f", (sum / 1000.0) * 100);
 
+        // 9. 당첨 결과 출력
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+lottoResult.get(4)+"개");
+        System.out.println("4개 일치 (50,000원) - "+lottoResult.get(3)+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+lottoResult.get(2)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+lottoResult.get(1)+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+lottoResult.get(0)+"개");
 
     }
 }

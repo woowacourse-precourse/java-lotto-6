2d97add60819183eb92f61d8efe47f8416e664b9
anneu <b.imnuj@gmail.com>
2023-11-03 09:37:45 +0900

    feat: 로또가 6개의 번호를 가지는지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final String SIZE_EXCEPTION = "로또는 %d개의 번호로 이뤄져야 합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        validateSize(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(String.format(SIZE_EXCEPTION, LOTTO_NUMBERS_SIZE));
+        }
+    }
 }

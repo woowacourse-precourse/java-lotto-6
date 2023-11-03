7c5e22c3b6cd114115c4e9fa6a448bb126306328
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-04 00:21:41 +0900

    :sparkles: [Feat] main

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.validation.BonusNumberValidator;
+import lotto.validation.PriceValidator;
+import lotto.validation.WinnerNumberValidator;
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PriceValidator priceValidator = new PriceValidator();
+        WinnerNumberValidator winnerNumberValidator = new WinnerNumberValidator();
+        BonusNumberValidator bonusNumberValidator = new BonusNumberValidator();
+
+        InputView inputView = new InputView(priceValidator, winnerNumberValidator, bonusNumberValidator);
+        OutputView outputView = new OutputView();
+
+        LottoGameController lottoGameController = new LottoGameController(inputView, outputView);
+
+        lottoGameController.start();
+
     }
 }

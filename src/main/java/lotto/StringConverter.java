34ef720df3af10e2640e29f6a9cab28655ff0c9f
Yang-soeun <soeun8636@naver.com>
2023-11-03 19:02:11 +0900

    [feat]: 문자열을 정수로 변환하는 메소드 구현

--- src/main/java/lotto/StringConverter.java
+++ src/main/java/lotto/StringConverter.java
@@ -1,11 +1,27 @@
 package lotto;
 
 import java.util.List;
+import lotto.exception.InvalidInput;
 
 public class StringConverter {
-    public List<Integer> convertToIntegerList(List<String> numbers){
+    private final InvalidInput invalidInput = new InvalidInput();
+
+    public List<Integer> convertToIntegerList(List<String> numbers) {
+        validateList(numbers);
         return numbers.stream()
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    public int parseInt(String number) {
+        invalidInput.notIntegerValueException(number);
+
+        return Integer.parseInt(number);
+    }
+
+    private void validateList(List<String> numbers) {
+        for (String number : numbers) {
+            invalidInput.notIntegerValueException(number);
+        }
     }
 }

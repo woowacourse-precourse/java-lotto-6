8958121d367987ab8cc7d8dcdd3cb5ede69faadf
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-03 15:38:00 +0900

    test: 구입금액 테스트 코드 추가

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -5,18 +5,18 @@ import lotto.message.ExceptionMessage;
 public class PurchaseAmountValidator {
     private static final int LOTTO_PRICE = 1000;
 
-    private void validate(int inputAmount) {
+    public static void validate(int inputAmount) {
         validateNaturalNumber(inputAmount);
         validateDivisibleByPrice(inputAmount);
     }
 
-    private void validateNaturalNumber(int inputAmount) {
+    private static void validateNaturalNumber(int inputAmount) {
         if (inputAmount <= 0) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_NATURAL_NUMBER.getMessage());
         }
     }
 
-    private void validateDivisibleByPrice(int inputAmount) {
+    private static void validateDivisibleByPrice(int inputAmount) {
         if (inputAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ExceptionMessage.INDIVISIBLE_AMOUNT.getMessage());
         }

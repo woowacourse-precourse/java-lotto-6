063dbb9297f9056507a99d2e84f8ae63419d427c
leeedohyun <ldh19738@gmail.com>
2023-11-03 13:53:14 +0900

    feat: 당첨 번호 중복 예외 기능 구현

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -8,12 +8,14 @@ public class WinningNumbers {
     private static final int END_NUMBER = 45;
     private static final String WINNING_NUMBER_OUT_OF_RANGE_EXCEPTION_MESSAGE = "[ERROR] 당첨 번호는 1에서 45 사이의 숫자여야 합니다.";
     private static final String INVALID_WINNING_NUMBERS_COUNT_EXCEPTION_MESSAGE = "[ERROR] 당첨 번호는 6개 입력해야 합니다.";
+    private static final String DUPLICATE_WINNING_NUMBERS_EXCEPTION_MESSAGE = "[ERROR] 당첨 번호는 서로 다른 숫자로 입력해야 합니다.";
 
     private final List<Integer> winningNumbers;
 
     public WinningNumbers(List<Integer> winningNumbers) {
         validateNumberInRange(winningNumbers);
         validateWinningNumbersCount(winningNumbers);
+        validateDuplicate(winningNumbers);
         this.winningNumbers = winningNumbers;
     }
 
@@ -35,7 +37,19 @@ public class WinningNumbers {
         }
     }
 
-    private static boolean isNotSameWinningNumbersSize(List<Integer> winningNumbers) {
+    private boolean isNotSameWinningNumbersSize(List<Integer> winningNumbers) {
         return winningNumbers.size() != LottoConstants.LOTTO_SIZE;
+    }
+
+    private void validateDuplicate(List<Integer> winningNumbers) {
+        if (isNotSameWinningNumbersSize(removeDuplicateWinningNumbers(winningNumbers))) {
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBERS_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private List<Integer> removeDuplicateWinningNumbers(List<Integer> winningNumbers) {
+        return winningNumbers.stream()
+                .distinct()
+                .toList();
     }
 }

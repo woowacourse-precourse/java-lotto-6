ef976c6d6738d1f79471a47a03f452bd6a01eaa5
euics <pjh575157@gmail.com>
2023-11-03 14:40:47 +0900

    test: givenValidNumbers_whenLottoCreated_thenSucceeds 메서드 구현
    유효한 로또 번호 목록으로 Lotto 객체 생성 메서드

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,9 +4,11 @@ import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +26,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("유효한 로또 번호 목록으로 Lotto 객체 생성")
+    void givenValidNumbers_whenLottoCreated_thenSucceeds() {
+        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertDoesNotThrow(() -> new Lotto(validNumbers));
+    }
 }

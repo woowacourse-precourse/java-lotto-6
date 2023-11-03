a8f61c6c67859eac3fb9fe24d4f29b2b9e3726ee
YongHyeon <pyh4616@naver.com>
2023-11-03 18:06:44 +0900

    Feat(OutputView 클래스): 당첨 번호 입력 메시지를 출력하는 기능을 printWinningNumbersMessage 메서드로 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,8 +3,13 @@ package lotto.view;
 public class OutputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+
     public void printInputPurchaseAmountMessage() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public void printWinningNumbersMessage() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
     }
 }

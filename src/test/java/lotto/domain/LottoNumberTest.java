c1547138cbd265cccf3ec87f0d0e5a79c8e14252
eora21 <eora21@naver.com>
2023-11-03 16:14:56 +0900

    test: 💍 추가 로또 정상 생성 테스트 작성

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import domain.LottoNumber;
+import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,5 +18,13 @@ public class LottoNumberTest {
     @ValueSource(ints = {-45, -1, 0, 46, 99})
     void createLottoNumberFailTest(int value) {
         assertThrows(IllegalArgumentException.class, () -> new LottoNumber(value));
+    }
+
+    @Test
+    @DisplayName("로또 정상 생성")
+    void createLottoNumberSuccessTest() {
+        assertDoesNotThrow(() -> IntStream.rangeClosed(1, 45)
+                .mapToObj(LottoNumber::new)
+                .toList());
     }
 }

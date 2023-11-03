403be60226baacd0a37c2997fc8c0da0557f8d07
hyundoo <hyundoo1006@gmail.com>
2023-11-03 21:26:52 +0900

    feat: Money 값 받는 기능 추가
    view 패키지 생성, InputView 클래스, OutputView 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.validator.InputMoneyValidator;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputMoneyValidator inputMoneyValidator = new InputMoneyValidator(InputView.inputMoney());
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void inputMessage(){
+        System.out.println(INPUT_MESSAGE);
+    }
+}

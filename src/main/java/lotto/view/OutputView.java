8e42c1dd07ac54efcb3af2bc4854c1d52ea184a3
qncj5224 <qncj5224@naver.com>
2023-11-03 22:23:45 +0900

    feat : 랜덤 로또 번호를 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
   private final static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
   private final static String INPUT_WINNINGNUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
@@ -15,4 +17,19 @@ public class OutputView {
   public static void printInputBonusNumberMessage() {
     System.out.println(INPUT_BOUNSNUMBER_MESSAGE);
   }
+
+  public String printtLottoNumbers(List<Integer> lottoNumbers) {
+    String formattedNumbers = "[";
+
+    for (int i = 0; i < lottoNumbers.size(); i++) {
+      formattedNumbers += lottoNumbers.get(i);
+      if (i < lottoNumbers.size() - 1) {
+        formattedNumbers += ", ";
+      }
+    }
+
+    formattedNumbers += "]";
+    return formattedNumbers;
+  }
+
 }

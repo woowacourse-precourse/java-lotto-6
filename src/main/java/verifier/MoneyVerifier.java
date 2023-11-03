b5e029da5c68989c44ba3c0a6f5140c949e4f883
Hoo-sung <gntjd135@gmail.com>
2023-11-03 15:21:36 +0900

    feat(MoneyVerifier): 로또 구입금액이 1000원으로 나눠떨어지는지 검증하는 checkDivisible 생성

--- src/main/java/verifier/MoneyVerifier.java
+++ src/main/java/verifier/MoneyVerifier.java
@@ -1,24 +1,31 @@
 package verifier;
 
-public class MoneyVerifier implements Verifier{
+public class MoneyVerifier implements Verifier {
     @Override
     public void check(String input) {
         checkNumeric(input);
         checkRange(input);
+        checkDivisible(input);
     }
 
-    private void checkNumeric(String input){
-        try{
+    private void checkNumeric(String input) {
+        try {
             Integer.parseInt(input);
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("로또 구입 금액이 숫자가 아닙니다.");
         }
     }
 
-    private void checkRange(String input){
+    private void checkRange(String input) {
         int money = Integer.parseInt(input);
-        if(money <0)
+        if (money < 0)
             throw new IllegalArgumentException("로또 구입 금액은 0보다 커야 합니다");
+    }
+
+    private void checkDivisible(String input) {
+        int money = Integer.parseInt(input);
+        if (money % 1000 != 0)
+            throw new IllegalArgumentException("로또 구입 금액이 1000원으로 나누어 떨어지지 않습니다.");
     }
 
 

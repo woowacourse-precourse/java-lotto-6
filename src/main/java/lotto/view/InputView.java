24d0ec28aa57927a6cdeed11cf61bd1ee8942191
jun <ggprgrkjh@naver.com>
2023-11-03 16:13:49 +0900

    feat(inputview): 로또 구매 금액 문자 입력 검증 추가 with TDD

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,12 +6,21 @@ public class InputView {
     public static void readBuyAmount() {
         String input = Console.readLine();
         validateSpaceBuyAmount(input);
+        validateNotIntegerBuyAmount(input);
     }
 
 
     public static void validateSpaceBuyAmount(String input) {
         if (input.equals("")) {
             throw new IllegalArgumentException("[ERROR] 공백이 입력되었습니다.");
+        }
+    }
+
+    public static void validateNotIntegerBuyAmount(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
         }
     }
 
--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -4,10 +4,13 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import lotto.view.InputView;
 import net.bytebuddy.pool.TypePool.Resolution.Illegal;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import camp.nextstep.edu.missionutils.Console;
+
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -18,12 +21,27 @@ public class InputViewTest {
         System.setIn(in);
     }
 
+    @AfterEach
+    void closeInputStream(){
+        Console.close();
+    }
+
     @DisplayName("로또 번호의 입력이 공백이 입력되면 예외가 발생한다.")
     @Test
     void readBuyAmount_Space_ExceptionThrown() {
         setInputValues("\n");
         assertThatThrownBy(() -> InputView.readBuyAmount()).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호의 입력에 문자가 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"ABC","HELLO","####","!@#"})
+    void readBuyAmount_NotInteger_ExceptionThrown(String input){
+        setInputValues(input);
+        assertThatThrownBy(()->InputView.readBuyAmount()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 
 
 }

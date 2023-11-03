4264bc59fcf622d7a9cb6920cfdc53a8fa5daaed
anneu <b.imnuj@gmail.com>
2023-11-03 09:44:21 +0900

    feat: 로또에 같은 번호가 없는지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,7 @@ public class Lotto {
 
     private static final int LOTTO_NUMBERS_SIZE = 6;
     private static final String SIZE_EXCEPTION = "로또는 %d개의 번호로 이뤄져야 합니다.";
+    private static final String DUPLICATE_EXCEPTION = "로또에 같은 번호가 존재합니다.";
 
     private final List<Integer> numbers;
 
@@ -16,11 +17,22 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException(String.format(SIZE_EXCEPTION, LOTTO_NUMBERS_SIZE));
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        List<Integer> distinctNumbers = numbers.stream()
+                .distinct()
+                .toList();
+
+        if (distinctNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_EXCEPTION);
         }
     }
 }

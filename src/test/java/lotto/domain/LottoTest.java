f7272cba979e2e0a69c021fae404cf7a4e847255
CHAHANS <herodev0129@gmail.com>
2023-11-03 14:37:50 +0900

    refect(domian) : Lotto 패키지 위치 오류 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,11 @@
-package lotto;
+package lotto.domain;
+
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")

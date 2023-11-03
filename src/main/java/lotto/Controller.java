68bd654de8e2f69781532cb0a791a804bbac70e1
junghunim07 <dlawndgns9@naver.com>
2023-11-04 02:44:24 +0900

    feat(Controller): Lotto 구매 기능 추가

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -3,9 +3,12 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
+    private List<Lotto> lottoTickets = new ArrayList<>();
+
     public int getLottoCount(int userMoney) {
         if (userMoney % Lotto.PRICE != 0) {
             throw new IllegalArgumentException("[ERROR]");
@@ -16,5 +19,12 @@ public class Controller {
 
     public List<Integer> getRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public void purchaseLotto(int count) {
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = new Lotto(getRandomNumbers());
+            lottoTickets.add(lotto);
+        }
     }
 }

34a17dddb061d39eaa5acd51752a29e8414449c6
junsoo <choix712@umn.edu>
2023-11-03 17:16:50 +0900

    feat: 출력을 담당하는 OutputView 클래스 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String TYPE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public static void printPurchaseAmount() {
+        System.out.println(TYPE_PURCHASE_AMOUNT);
+    }
+}

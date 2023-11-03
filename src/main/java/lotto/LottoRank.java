7ee11a7d50423569130fc08316779e6720b4c22e
gusgh075 <gusgh075@naver.com>
2023-11-03 14:53:22 +0900

    FEATURES(LottoRank) : 리스트에 특정 숫자가 있는지 여부를 확인하는 기능

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -3,10 +3,17 @@ package lotto;
 import java.util.List;
 
 public class LottoRank {
+
+
     private int countMatchingNumbers(List<Integer> lottoNumbers, List<Integer> compareNumbers) {
         long count = lottoNumbers.stream()
                 .filter(compareNumbers::contains)
                 .count();
         return (int) count;
+    }
+
+    private boolean isHasNum(List<Integer> compareNumbers, int bonusNum) {
+        boolean hasNum = compareNumbers.stream().anyMatch(n -> n == bonusNum);
+        return hasNum;
     }
 }

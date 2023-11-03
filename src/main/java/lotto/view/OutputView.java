38829655c05db2e62443977d90d3ce77620e8263
gusdn0108 <gusdn6671@naver.com>
2023-11-03 19:05:32 +0900

    feat(OutputView): n개의 구매했습니다 메시지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,8 +6,12 @@ public class OutputView {
     private OutputView() {
     }
 
-    public static void printBuyInputPrice(){
+    public static void printBuyInputPrice() {
         System.out.println(Message.PRINT_USER_INPUT_BUY_PRICE.getMessage());
     }
 
+    public static void printBuyLottoCount(int count) {
+        System.out.println();
+        System.out.println(count + Message.PRINT_USER_BUY_LOTTO_COUNT.getMessage());
+    }
 }

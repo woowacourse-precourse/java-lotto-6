a3ac3bf115d679605a70d7216c2d28ba7df28f26
Leespect <wewqwew153@gmail.com>
2023-11-03 13:31:07 +0900

    Feat : ConstantValues 추가

---
+++ src/main/java/constants/ConstantValues.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class ConstantValues {
+
+    public static final int startLottoNumber = 1;
+    public static final int endLottoNumber = 45;
+    public static final int lottoLength = 6;
+    public static final int lottoCost = 1000;
+    public static final int fivePrize = 5000;
+    public static final int fourPrize = 50000;
+    public static final int threePrize = 1500000;
+    public static final int twoPrize = 30000000;
+    public static final int onePrize = 2000000000;
+
+}

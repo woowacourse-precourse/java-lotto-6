60914e62b85d7a27411d33fdf8ad637156067108
Reddyong <d0209hong@naver.com>
2023-11-03 16:52:19 +0900

    feat : create lottoMoney(), create validateMoney()

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static int inMoney;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 로또 구입 금액 입력
+        lottoMoney();
+
+    }
+
+    public static void lottoMoney() {
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String s = Console.readLine();
+            try {
+                inMoney = Integer.parseInt(s);
+                validateMoney();
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 금액을 잘못 입력하셨습니다.");
+            }
+        }
+    }
+
+    public static void validateMoney() {
+        if (inMoney % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }

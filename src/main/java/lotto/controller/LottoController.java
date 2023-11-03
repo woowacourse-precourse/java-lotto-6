e842a84b519ba22382341856802f610240507e6e
apuhapu <laks852@naver.com>
2023-11-03 16:51:26 +0900

    컨트롤러 유효성 검사 개선
    NumberFormatException를 사전 감지하여 IllegalArgumentException으로 처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,13 +6,25 @@ import lotto.dto.WinningNumberDto;
 import lotto.service.LottoService;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LottoController {
 
     private final LottoService lottoService = new LottoService();
 
+    private final String regex = "[0-9]+";
+    private final Pattern pattern = Pattern.compile(regex);
+
+
     public WinningNumberDto post1stNumber(String input) throws Exception {
         return lottoService.postNormalNumbers(convert2Dto(input));
+    }
+
+    public WinningNumberDto postBonusNumber(WinningNumberDto dto, String input) throws Exception {
+        validateInt(input);
+        int bonusNum = Integer.parseInt(input);
+        return lottoService.postBonusNumber(dto, bonusNum);
     }
 
     private NumbersDto convert2Dto(String str) {
@@ -20,11 +32,18 @@ public class LottoController {
         ArrayList<Integer> result = new ArrayList<Integer>();
 
         for (String s : arr) {
-            // NumberFormatException 발생 우려
+            validateInt(s.trim());
             result.add(Integer.parseInt(s.trim()));
         }
 
         result.sort(null);
         return new NumbersDto(result);
+    }
+
+    private void validateInt(String str) {
+        Matcher matcher = pattern.matcher(str);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("숫자만을 입력해야 합니다.");
+        }
     }
 }

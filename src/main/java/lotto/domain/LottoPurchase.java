12b375e4d1df719d877f928401758773484f23ea
Isaac Han <isaachan233@gmail.com>
2023-11-03 12:22:00 +0900

    feat: LottoPurchase 생성자 생성

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -1,5 +1,13 @@
 package lotto.domain;
 
 public class LottoPurchase {
-    private final int amount;
+    private final Integer amount;
+
+    public LottoPurchase(int amount) {
+        this.amount = amount;
+    }
+
+    public LottoPurchase(String amount) {
+        this.amount = Integer.parseInt(amount);
+    }
 }

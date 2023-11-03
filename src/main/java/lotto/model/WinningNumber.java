d5b9a889da4d87b6b33c92c34b8c49528dba18bd
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-03 12:50:54 +0900

    feat: 당첨 번호 클래스 생성

---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,16 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumber {
+    private List<Integer> value;
+
+    public WinningNumber(final String winningNumbers){
+        validate(winningNumbers);
+    }
+
+    private void validate(final String winningNumbers){
+
+    }
+}
+

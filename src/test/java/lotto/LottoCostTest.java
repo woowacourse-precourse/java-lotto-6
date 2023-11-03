cf5682a1595e95108164c0283b309ff48a8fc828
unknown <winternrabbit@gmail.com>
2023-11-04 02:54:34 +0900

    test: 구입금액이 숫자가 아닐 경우 false가 반환되는지 테스트

--- src/test/java/lotto/LottoCostTest.java
+++ src/test/java/lotto/LottoCostTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoCostTest {
-  
+    LottoCost lottoCost = new LottoCost();
+
+    @DisplayName("구입금액이 숫자가 아닐 경우 false가 반환된다.")
+    @Test
+    void costNotNumber() {
+        assertThat(lottoCost.isNumber("abc123가나다")).isEqualTo(false);
+    }
 }

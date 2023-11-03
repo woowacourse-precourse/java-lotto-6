59fc1aeb7646f73d5953000b4fa84ae5f4df9712
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-03 12:19:45 +0900

    test: buyer 테스트를 purchasing service 로 이동
    금액 확인에 대한 예외 처리를 Buyer 클래스 에서 진행하지 않고 PurchasingService에서 진행하는 것으로 변경했기 때문에 테스트 클래스의 내용 이동

---
+++ src/test/java/lotto/service/PurchasingServiceTest.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import lotto.domain.Buyer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PurchasingServiceTest {
+
+    PurchasingService purchasingService = new PurchasingService();
+
+    @DisplayName("로또 구매 금액이 1,000원 보다 적으면 예외가 발생한다.")
+    @Test
+    void createBuyerUnderAmount() {
+        assertThatThrownBy(() -> purchasingService.purchaseLotto(999))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또를 한 장 이상 구매해야 합니다. (로또 1장 1,000원");
+    }
+
+    @DisplayName("로또 구매 금액의 단위가 1,000원이 아니면 예외가 발생한다.")
+    @Test
+    void createBuyerWrongUnit() {
+        assertThatThrownBy(() -> purchasingService.purchaseLotto(1500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또는 1,000원 단위로 구매 가능합니다. (로또 1장 1,000원");
+    }
+}

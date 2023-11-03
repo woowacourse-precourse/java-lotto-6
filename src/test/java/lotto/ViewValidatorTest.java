d2af7d5ef7d80892da434449fae971a4664964be
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-03 22:28:33 +0900

    test(ViewValidatorTest) : 당첨 번호 숫자 개수 검증

--- src/test/java/lotto/ViewValidatorTest.java
+++ src/test/java/lotto/ViewValidatorTest.java
@@ -4,6 +4,8 @@ import lotto.view.ViewValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ViewValidatorTest {
@@ -31,6 +33,29 @@ class ViewValidatorTest {
 
         // expected
         assertThatThrownBy(() -> viewValidator.parseInt(text))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호 숫자가 6개인지 확인한다.")
+    @Test
+    void validateWinningNumberSize() {
+        // given
+        ViewValidator viewValidator = new ViewValidator();
+        List<String> winningNumbersText = List.of("1", "2", "3", "4", "5", "6");
+
+        // expected
+        viewValidator.validateWinningNumberSize(winningNumbersText);
+    }
+
+    @DisplayName("당첨 번호 숫자가 6개가 아니면 예외가 발생한다.")
+    @Test
+    void validateWinningNumberSizeFail() {
+        // given
+        ViewValidator viewValidator = new ViewValidator();
+        List<String> winningNumbersText = List.of("1", "2", "3", "4", "5");
+
+        // expected
+        assertThatThrownBy(() -> viewValidator.validateWinningNumberSize(winningNumbersText))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

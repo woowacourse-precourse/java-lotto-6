880b8f41cacd7a444d01fbad56a8569b19586e99
hyocheol An <anhyocheol@naver.com>
2023-11-04 07:49:48 +0900

    style : 메서드 선언부 변경

--- src/main/java/lotto/util/InputVaildate.java
+++ src/main/java/lotto/util/InputVaildate.java
@@ -2,11 +2,11 @@ package lotto.util;
 
 public class InputVaildate {
     private final static int MONEY_UNIT = 1000;
-    public static void moneyVaildate(int money) {
+    public void moneyVaildate(int money) {
         //1000단위인 지 검증
         isUnitMoney(money);
     }
-    public static  void isUnitMoney(int money) {
+    public void isUnitMoney(int money) {
         if(money % MONEY_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로만 가능합니다.");
         }

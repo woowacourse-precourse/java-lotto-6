c68cdfdd23bb08fda4614bac73b590bcffa65332
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-11-03 17:37:42 +0900

    feat: 구입 금액 입력 함수 구현(InputView)

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String Input_Lotto_Buy = "구입금액을 입력해 주세요.";
+
+    public static String Input_Lotto_Buy(){
+        System.out.println(Input_Lotto_Buy);
+        return Console.readLine();
+    }
+}

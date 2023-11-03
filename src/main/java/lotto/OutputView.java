00d87548135cc16505e3afb55b8bd328c8b3d646
HwangSeokJin <ghkd3703@gmail.com>
2023-11-04 02:56:40 +0900

    feat: 발행한 로또 수량 및 번호를 출력하는 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,9 +1,19 @@
 package lotto;
 
+import java.util.List;
+
 public class OutputView {
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
+    private static final String LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
 
     public void printErrorMessage(String message) {
         System.out.println(String.format(ERROR_MESSAGE_FORMAT, message));
+    }
+
+    public void printGeneratedLotto(List<String> numbers) {
+        System.out.println(String.format(LOTTO_COUNT_FORMAT, numbers.size()));
+        for (String number : numbers) {
+            System.out.println(number);
+        }
     }
 }

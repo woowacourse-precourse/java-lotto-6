f3bb5f31442af0613b6aa032e4fa655d03d08e56
GiJung <weeds2577@gmail.com>
2023-11-04 01:42:35 +0900

    feat(LottoService): 구매 개수만큼 로또 발행 기능 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,37 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+public class LottoService {
+
+    private static final int startNumber = 1;
+    private static final int endNumber = 45;
+    private static final int lottoNumberCount = 6;
+
+    private List<Lotto> purchaseLotto = new ArrayList<>();
+
+    public void repeatPurchase(int lottoCount) {
+        for(int count = 0; count < lottoCount; count++) {
+            generateLotto();
+        }
+    }
+
+    public List<Lotto> getPurchaseLotto() {
+        return this.purchaseLotto;
+    }
+
+    private void generateLotto() {
+
+        Lotto lotto = new Lotto(pickUniqueNumbersInRange(startNumber, endNumber, lottoNumberCount));
+
+        lotto.sortLotto();
+
+        purchaseLotto.add(lotto);
+    }
+
+}

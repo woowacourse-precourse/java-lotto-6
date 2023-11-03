8c621517f2b33a7e001360a8972111876aca5758
Arachneee <jhg2819@naver.com>
2023-11-03 10:48:58 +0900

    test: 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,12 +2,17 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
+import java.util.stream.Stream;
 import lotto.exception.ErrorMessage;
-import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -26,14 +31,15 @@ class LottoTest {
     }
 
     @DisplayName("로또 번호에 1 ~ 45 범위 밖의 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByWrongRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46, 47})
+    void createLottoByWrongRange(int value) {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, value)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.WRONG_RANGE.getMessage());
     }
 
-    @DisplayName("보너스 번호도 기존 번호와 중복되면 안된다.")
+    @DisplayName("보너스 번호도 기존 번호와 중복되면 예외를 발생한다.")
     @Test
     void validateBonusNumberByDuplicatedNumber() {
         // given
@@ -42,35 +48,49 @@ class LottoTest {
 
         // when then
         assertThatThrownBy(() -> lotto.validateBonusNumber(bonus))
-                .isInstanceOf(LottoException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.DUPLICATE_NUMBER.getMessage());
     }
 
-    @DisplayName("보너스 번호도 범위에 있어야 한다.")
-    @Test
-    void validateBonusNumberByRange() {
+    @DisplayName("보너스 번호도 범위 밖에 있으면 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46, 47})
+    void validateBonusNumberByRange(int value) {
         // given
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        int bonus = 46;
+        int bonus = value;
 
         // when then
         assertThatThrownBy(() -> lotto.validateBonusNumber(bonus))
-                .isInstanceOf(LottoException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.WRONG_RANGE.getMessage());
     }
 
     @DisplayName("다른 로또와 중복 숫자를 카운트 할 수 있다.")
-    @Test
-    void countSameNumber() {
+    @ParameterizedTest
+    @MethodSource("parametersProvider")
+    void countSameNumber(List<Integer> input, int answer) {
         // given
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto answerLotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+        Lotto answerLotto = new Lotto(input);
 
         // when
         int count = lotto.countSameNumber(answerLotto);
 
         // then
-        assertThat(count).isEqualTo(4);
+        assertThat(count).isEqualTo(answer);
+    }
+
+    static Stream<Arguments> parametersProvider() {
+        return Stream.of(
+                arguments(List.of(7, 8, 9, 10, 11, 12), 0),
+                arguments(List.of(1, 7, 8, 9, 10, 11), 1),
+                arguments(List.of(1, 2, 7, 8, 9, 10), 2),
+                arguments(List.of(1, 2, 3, 7, 8, 9), 3),
+                arguments(List.of(1, 2, 3, 4, 7, 8), 4),
+                arguments(List.of(1, 2, 3, 4, 5, 7), 5),
+                arguments(List.of(1, 2, 3, 4, 5, 6), 6)
+        );
     }
 
     @DisplayName("보너스 번호를 포함하고 있으면 true를 반환한다.")
@@ -99,5 +119,18 @@ class LottoTest {
 
         // then
         assertThat(isContain).isFalse();
+    }
+
+    @DisplayName("로또 번호를 리스트 문자 형태로 변활 할 수 있다.")
+    @Test
+    void lottoToString() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        String string = lotto.toString();
+
+        // then
+        assertThat(string).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 }

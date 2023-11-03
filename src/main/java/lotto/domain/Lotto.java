88a7552b5bdace66ab8f902421d90b8828784f36
junghunim07 <dlawndgns9@naver.com>
2023-11-04 00:26:55 +0900

    feat(Lotto): 로또 가격 static final 변수 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,9 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+    public static final int PRICE = 1000;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {

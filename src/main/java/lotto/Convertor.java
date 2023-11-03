f373120719222d4119870a41175b10c22a06eaef
brightfanta <brightfanta@gmail.com>
2023-11-03 18:49:14 +0900

    feat(Convertor): 사용자 입력값 변환 메서드 구현(String->Integer / String->String[]->List<Integer>)

--- src/main/java/lotto/Convertor.java
+++ src/main/java/lotto/Convertor.java
@@ -1,4 +1,26 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Convertor {
+
+    private static final Integer LOTTERY_NUMBERS = 6;
+
+    public static Integer convertInputToInteger(String userInput) {
+        return Integer.parseInt(userInput);
+    }
+
+    public static String[] splitInput(String userInput) {
+        return userInput.split(",");
+    }
+    public static List<Integer> convertToList(String[] userInputArray) {
+        List<Integer> numbersList = new ArrayList<>();
+        for (int i = 0; i < LOTTERY_NUMBERS; i++) {
+            String numberTemp = userInputArray[i];
+            int winningNumber = Integer.parseInt(numberTemp);
+            numbersList.add(winningNumber);
+        }
+        return numbersList;
+    }
 }

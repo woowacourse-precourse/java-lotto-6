d934128abebdd32a58c08b3a834907b2a633d0f8
sh990920 <sh990920@gmail.com>
2023-11-03 16:39:02 +0900

    feat : 숫자 중복 체크 메소드 생성

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,44 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        numbersCheck(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자의 갯수는 6개 입니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    private void numbersCheck(List<Integer> numbers){
+        for(int i = 0; i < numbers.size(); i++){
+            overlap(numbers.get(i), numbers);
+        }
+    }
+
+    private void overlap(int number, List<Integer> numbers){
+        int count = 0;
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) == number){
+                count++;
+            }
+            if(count > 1){
+                throw new IllegalArgumentException("[ERROR] 숫자는 모두 다른 숫자로 이루어져 있어야 합니다.");
+            }
+        }
+    }
+
+    @Override
+    public String toString(){
+        return numbers.toString();
+    }
+
+}
---
+++ src/main/java/lotto/model/Output.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+public interface Output {
+
+    String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    String INPUT_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+
+    String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    String WINNING_STATISTICS = "당첨 통계";
+
+
+
+}

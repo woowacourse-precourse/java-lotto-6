4c6a825b843c90ee2752007d8d98b229b5e38cd6
706com <ehdgusjdbswl@gmail.com>
2023-11-03 23:19:03 +0900

    feat : 복권 생성기(자판기) 도메인 객체 생성

---
+++ src/main/java/lotto/domain/LotteryGenerator.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+public class LotteryGenerator {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final int NUMBER_SIZE = 6;
+
+    private boolean validateMoney(int money){
+
+    }
+}

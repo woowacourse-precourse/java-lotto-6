18558737c2057c58bcbad3e1d57fefd97b00ef9e
kongji <kongji4092@gmail.com>
2023-11-03 17:12:13 +0900

    refactor: 로또 가격 이름 변경
    Cost -> Price

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static final String COST_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 입력값이 숫자가 아닙니다.";
+    public static final String PRICE_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 입력값이 숫자가 아닙니다.";
 
-    public static int getPurchaseLottoCost() {
+    public static int getPurchaseLottoPrice() {
         String cost = Console.readLine();
         return costConvertToInt(cost);
     }
@@ -15,7 +15,7 @@ public class InputView {
         try {
             return Integer.parseInt(cost);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(COST_NOT_INTEGER_ERROR_MESSAGE);
+            throw new IllegalArgumentException(PRICE_NOT_INTEGER_ERROR_MESSAGE);
         }
     }
 }

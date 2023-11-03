72a101076d39cd357a8fb45ea38252938b0e75a0
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 00:15:29 +0900

    test(InputPatternTest): 숫자 검증 정규식 테스트

---
+++ src/test/java/lotto/constants/InputPatternTest.java
@@ -0,0 +1,37 @@
+package lotto.constants;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("입력 패턴 상수에 대해")
+class InputPatternTest {
+
+    @Test
+    @DisplayName("숫자 검증 정규식 장수에 숫자를 입력 안 하면 true를 반환한다.")
+    void When_Input_Not_Numeric_Then_True() {
+        //given
+        String givenNumeric = "pobi";
+
+        //when
+        boolean isNotNumeric = InputPattern.isNotNumeric(givenNumeric);
+
+        //then
+        assertTrue(isNotNumeric);
+    }
+
+    @Test
+    @DisplayName("숫자 검증 정규식 장수에 숫자를 입력 하면 false를 반환한다.")
+    void When_Input_Numeric_Then_False() {
+        //given
+        String givenNumeric = "123";
+
+        //when
+        boolean isNotNumeric = InputPattern.isNotNumeric(givenNumeric);
+
+        //then
+        assertFalse(isNotNumeric);
+    }
+}

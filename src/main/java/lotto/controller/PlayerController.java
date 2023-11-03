5235d0aa91bfc993f5bac9d5e12770d610179243
BullChallenger <am23am45@naver.com>
2023-11-03 23:05:14 +0900

    [FEAT] PlayerController 클래스 작성
    주문 금액을 입력 받고 해당 금액에 대한 유효성 검사를 진행 후, 타입을 변환하여 service 로직으로 금액을 넘긴다

---
+++ src/main/java/lotto/controller/PlayerController.java
@@ -0,0 +1,36 @@
+package lotto.controller;
+
+import static lotto.constant.ExceptionMessage.NON_NUMERIC_ERROR;
+import static lotto.constant.ExceptionMessage.TOO_SMALL_PRICE_TO_BUY;
+import static lotto.constant.LottoInfo.LOTTO_PRICE;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.service.PlayerService;
+
+public class PlayerController {
+
+    private final PlayerService playerService = new PlayerService();
+
+    public List<Lotto> purchaseLotto(String input) {
+        validateIsNumeric(input);
+        Long price = Long.parseLong(input);
+        validateIsMinimumPrice(price);
+        return playerService.purchaseLotto(price);
+    }
+
+    private void validateIsNumeric(String input) {
+        for (char ch : input.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                throw new IllegalArgumentException(NON_NUMERIC_ERROR.getMessage());
+            }
+        }
+    }
+
+    private void validateIsMinimumPrice(Long price) {
+        if (price < LOTTO_PRICE.getInfo()) {
+            throw new IllegalArgumentException(TOO_SMALL_PRICE_TO_BUY.getMessage());
+        }
+    }
+
+}

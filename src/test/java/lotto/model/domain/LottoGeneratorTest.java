34ca04e6ca2dc6b6b47c675836f84e8834c55ea7
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-03 21:51:54 +0900

    test: 로또 번호 생성 테스트

---
+++ src/test/java/lotto/model/domain/LottoGeneratorTest.java
@@ -0,0 +1,41 @@
+package lotto.model.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.AssertDelegateTarget;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+    private LottoGenerator lottoGenerator;
+
+    @BeforeEach
+    void setUp() {
+        lottoGenerator = new LottoGenerator();
+    }
+
+    @Test
+    @DisplayName("로또_번호가_제대로_생성_되었는지")
+    void generateLottoNumbers() {
+        // arrange
+        int lottoSize = LottoGameConstants.NUMBERS_PER_LOTTO.getValue();
+        int minRange = LottoGameConstants.MIN_NUMBER.getValue();
+        int maxRange = LottoGameConstants.MAX_NUMBER.getValue();
+
+        // act
+        List<Integer> lottoNumbers = lottoGenerator.generateLottoNumbers();
+
+        // assert
+        assertThat(lottoNumbers).isNotNull()
+                .size()
+                .isEqualTo(lottoSize);
+        for (int number : lottoNumbers) {
+            assertThat(number >= minRange && number <= maxRange)
+                    .isTrue();
+        }
+    }
+}

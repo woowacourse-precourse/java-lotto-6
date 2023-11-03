81548e9625622743b6e617f35f8b42fb8dcf1cfb
vega <vega99c@naver.com>
2023-11-04 03:26:28 +0900

    feat(LottoManager): 당첨여부 판별한 데이터로 결과 출력 메서드 구현
    - 구현을 해놓고 리팩토링을 하는 방향으로 가보기로 결정
    - 구현하면서 구조까지 다시 생각하려니 더 안풀리는 느낌 때문

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -13,28 +14,41 @@ import java.util.stream.Collectors;
 public class LottoManager {
     final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.\n";
     final String INPUT_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.\n";
+    final String NORMAL_MESSAGE = "%d개 일치 (%s원) - %d개\n";
+    final String SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+    final String TOTAL_RATE_REVENUE_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+    final String WINNING_STATISTICS_MESSAGE = "당첨 통계 \n---\n";
     final int REQUIRED_NUMBER_COUNT = 6;
     final int REQUIRED_BONUS_NUMBER_COUNT = 1;
-    LottoBuyer lottoBuyer;
-    List<Integer> winningNumbers;
-    private int bonusNumber;
-    private Hashtable<Integer, Integer> winningCountHash;
     final int SECOND_GRADE_KEY = 6;
     final int FIRST_MATCH_COUNT = 6;
     final int SECOND_MATCH_COUNT = 5;
     final int FIRST_GRADE_KEY = 7;
     final int MINIMUM_CORRECT_COUNT = 3;
-
-
-
+    final int FIRST_GRADE_MONEY = 2000000000;
+    final int SECOND_GRADE_MONEY = 30000000;
+    final int THIRD_GRADE_MONEY = 1500000;
+    final int FORTH_GRADE_MONEY = 50000;
+    final int FIFTH_GRADE_MONEY = 5000;
+    private Hashtable<Integer, Integer> winningCountHash;
+    Hashtable<Integer, Integer> moneyHash;
+    private int bonusNumber;
+    LottoBuyer lottoBuyer;
+    List<Integer> winningNumbers;
+    private float totalRateOfRevenue;
+    private int totalRevenue;
 
     public LottoManager(LottoBuyer buyer) {
+        totalRateOfRevenue = 0;
+        totalRevenue = 0;
         lottoBuyer = buyer;
         winningCountHash = new Hashtable<Integer, Integer>();
 
         for (int i = 3; i <= 7; i++) {
             winningCountHash.put(i, 0);
         }
+
+        initMoneyHashMap();
     }
 
     public void lottoSellingStart() {
@@ -42,6 +56,36 @@ public class LottoManager {
         publishLotto();
         inputWinningNumbers();
         inputBonusNumber();
+        setWinningResult();
+        announceResult();
+    }
+
+    public void initMoneyHashMap() {
+        moneyHash = new Hashtable<Integer, Integer>();
+
+        moneyHash.put(3, FIFTH_GRADE_MONEY);
+        moneyHash.put(4, FORTH_GRADE_MONEY);
+        moneyHash.put(5, THIRD_GRADE_MONEY);
+        moneyHash.put(6, SECOND_GRADE_MONEY);
+        moneyHash.put(7, FIRST_GRADE_MONEY);
+    }
+
+    public void announceResult() {
+        NumberFormat moneyFormat = NumberFormat.getInstance();
+        System.out.print(WINNING_STATISTICS_MESSAGE);
+        winningCountHash.forEach((key, value) -> {
+            if (key == SECOND_GRADE_KEY) {
+                System.out.printf(SECOND_MESSAGE, SECOND_MATCH_COUNT, moneyFormat.format(moneyHash.get(key)), value);
+            }
+
+            if (key == FIRST_GRADE_KEY) {
+                System.out.printf(NORMAL_MESSAGE, FIRST_MATCH_COUNT, moneyFormat.format(moneyHash.get(key)), value);
+            }
+
+            if ((key != SECOND_GRADE_KEY) && (key != FIRST_GRADE_KEY)) {
+                System.out.printf(NORMAL_MESSAGE, key, moneyFormat.format(moneyHash.get(key)), value);
+            }
+        });
     }
 
     public void inputBonusNumber() {
@@ -92,18 +136,18 @@ public class LottoManager {
     public void setWinningResult() {
         for (Lotto lotto : lottoBuyer.getMyLotts()) {
             int matchedCount = lotto.getWinningCount(winningNumbers);
-
             if(matchedCount >= MINIMUM_CORRECT_COUNT) {
+                if (lotto.getNumbers().contains(getBonusNumber()) && (matchedCount == SECOND_MATCH_COUNT)) {
+                    winningCountHash.put(SECOND_GRADE_KEY, winningCountHash.get(matchedCount) + 1);
+                    continue;
+                }
+
+                if (matchedCount == FIRST_MATCH_COUNT) {
+                    winningCountHash.put(FIRST_GRADE_KEY, winningCountHash.get(matchedCount) + 1);
+                    continue;
+                }
+
                 winningCountHash.put(matchedCount, winningCountHash.get(matchedCount) + 1);
-            }
-
-            if (lotto.getNumbers().contains(getBonusNumber()) && (matchedCount == SECOND_MATCH_COUNT)) {
-                winningCountHash.put(SECOND_GRADE_KEY, winningCountHash.get(matchedCount) + 1);
-                continue;
-            }
-
-            if (matchedCount == FIRST_MATCH_COUNT) {
-                winningCountHash.put(FIRST_GRADE_KEY, winningCountHash.get(matchedCount) + 1);
             }
         }
     }

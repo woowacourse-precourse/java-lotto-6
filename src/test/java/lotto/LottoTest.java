6e199e6f18f88dc86ae3b1eaea3df16b37040f63
suihub <qkdcrt@gmail.com>
2023-11-03 18:48:15 +0900

    <test(LottoTest)> : "문자열 출력 테스트 구현"
    - 실제 메서드 구현 전에 실패하는 테스트를 구현.
    
    이를 성공시키도록 기능 구현 예정.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 아니면 예외가 발생한다.")
@@ -23,5 +23,15 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 번호는 중복된 숫자를 고를 수 없습니다.");
+    }
+
+    @DisplayName("로또 번호를 출력 요구 사항에 맞추어 문자열로 반환한다.")
+    @Test
+    void printLottoNumberByString() {
+        Lotto lotto = new Lotto(List.of(8, 12, 28, 30, 31, 41));
+
+        String lottoNumbers = lotto.getLottoNumberString();
+
+        assertThat(lottoNumbers).isEqualTo("[8, 12, 28, 30, 31, 41]");
     }
 }

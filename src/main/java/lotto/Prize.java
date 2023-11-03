e3a0fdb7eb905d27cd5f0b9470984dfb697a8d0f
Youngwoo Choi <allnstar@naver.com>
2023-11-03 17:28:16 +0900

    feat: add a function for inputting numbers

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Prize {
+    private String groupNumbers;
+
+    void inputNumbers() {
+        groupNumbers = Console.readLine();
+    }
+}

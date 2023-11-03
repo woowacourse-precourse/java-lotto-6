9182cdca4cf54fe0a0d895f7073df3c325fc6862
현 <sohyun95277@gmail.com>
2023-11-03 21:18:29 +0900

    feat(OutputView): 로또 당첨 내역 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
+import static lotto.constant.message.OutputMessage.BONUS_RESULT;
+import static lotto.constant.message.OutputMessage.PRIZE_RESULT;
 import static lotto.constant.message.OutputMessage.PURCHASE_COUNT_RESULT;
 import static lotto.constant.message.OutputMessage.RESULT_MESSAGE;
 
 import java.util.List;
+import java.util.Map;
+import lotto.constant.Prize;
 import lotto.domain.Lotto;
 
 public class OutputView {
@@ -13,13 +17,28 @@ public class OutputView {
 
     public void printLottoCount(int count) {
         String message = String.format(PURCHASE_COUNT_RESULT, count);
-        
+
         System.out.println(message);
     }
 
     public void printLottoNumbers(List<Lotto> tickets) {
         for (Lotto ticket : tickets) {
             System.out.println(ticket.getNumbers());
+        }
+    }
+
+    public void printPrizeResult(Map<Prize, Integer> result) {
+        for (Map.Entry<Prize, Integer> pair : result.entrySet()) {
+            String message = String.format(PRIZE_RESULT,
+                    pair.getKey().getMatch(), pair.getKey().getPanel(), pair.getValue());
+            if (pair.getKey().equals(Prize.SECOND)) {
+                message = String.format(BONUS_RESULT,
+                        pair.getKey().getPanel(), pair.getValue());
+            }
+            if (pair.getKey().equals(Prize.NONE)) {
+                return;
+            }
+            System.out.println(message);
         }
     }
 }

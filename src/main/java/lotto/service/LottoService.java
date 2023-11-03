d4c1d8912e039a55c253b590d25d20115ddd9b26
euics <pjh575157@gmail.com>
2023-11-03 12:18:26 +0900

    feat(Service): displayWinningNumber, changeInputToString 메서드 구현
    당첨번호 입력 및 검증 메서드 구현
    - `displayWinningNumber`
      - 사용자로부터 로또 당첨번호를 입력받아, 해당 번호가 올바른 형식인지 검증
      - 유효하지 않은 입력에 대해 오류 메시지를 출력하고, 다시 입력을 요청
    - `changeInputToString`
      - 문자열 형태의 입력값을 숫자 리스트로 변환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,7 +2,12 @@ package lotto.service;
 
 import lotto.domain.LottoTicket;
 import lotto.exception.PurchaseExceptionMessage;
+import lotto.exception.WinningNumberExceptionMessage;
 import lotto.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoService {
     private final LottoTicket lottoTicket = new LottoTicket();
@@ -27,5 +32,41 @@ public class LottoService {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public List<Integer> displayWinningNumber() {
+        InputView.printWinningNumberPrompt();
+
+        while (true) {
+            try {
+                String winningNumberView = InputView.getWinningNumberView();
+
+                // 로또 번호 형식 검사
+                WinningNumberExceptionMessage.validateLottoNumberFormat(winningNumberView);
+
+                // ','를 기준으로 분리하고 숫자로 변환
+                List<Integer> winningNumbers = changeInputToString(winningNumberView);
+
+                // 로또 번호 길이 검사
+                WinningNumberExceptionMessage.validateWinningNumberLength(winningNumbers);
+
+                // 중복된 로또 번호 검사
+                WinningNumberExceptionMessage.validateDuplicateWinningNumber(winningNumbers);
+
+                // 로또 번호 범위 검사
+                WinningNumberExceptionMessage.validateBoundaryWinningNumber(winningNumbers);
+
+                return winningNumbers;  // 모든 검증이 통과되면 winningNumbers 반환
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> changeInputToString(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)  // 공백 제거
+                .map(Integer::parseInt)  // String을 int로 변환
+                .collect(Collectors.toList());  // List로 변환
     }
 }

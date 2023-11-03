2c58c870e40eccd6d36c65df42bc33ee9c6d3ce8
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-11-03 16:41:56 +0900

    Feat: 예외 발생 시 예외 메시지를 출력하는 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+public class OutputView {
+
+    private OutputView() {
+
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}

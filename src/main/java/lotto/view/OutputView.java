59f39b22cf6453f2062d321b2d1a0a861e404585
eunhee <dmsgml9903@gmail.com>
2023-11-04 05:41:34 +0900

    feat(OutputView): 사용자가 구입한 모든 로또의 숫자를 리스트 형식으로 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,11 +2,24 @@ package lotto.view;
 
 import static java.lang.String.format;
 
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.UserLotteries;
+
 public class OutputView {
 
     private static final String PURCHASED_LOTTO_COUNT_MESSAGE_TEMPLATE = "%s개를 구매했습니다.";
 
     public void printPurchasedCountMessage(Integer purchasedLottoCount) {
         System.out.println(format(PURCHASED_LOTTO_COUNT_MESSAGE_TEMPLATE, purchasedLottoCount));
+    }
+
+    public void printUserLotteries(UserLotteries userLotteries) {
+        List<Lotto> lotteries = userLotteries.getLotteries();
+        lotteries.forEach(this::printLotto);
+    }
+
+    private void printLotto(Lotto lotto) {
+        System.out.println(lotto.getNumbers());
     }
 }

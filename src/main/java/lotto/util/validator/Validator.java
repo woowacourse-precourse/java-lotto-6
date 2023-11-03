86df591f03ba7c769d760dc6bc77b43654ebf968
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-04 08:14:13 +0900

    feat(Validator): 예외처리 기능 추가

--- src/main/java/lotto/util/validator/Validator.java
+++ src/main/java/lotto/util/validator/Validator.java
@@ -1,6 +1,9 @@
 package lotto.util.validator;
 
+import java.util.List;
 import java.util.regex.Pattern;
+
+import lotto.util.message.ErrorMessage;
 
 public class Validator {
 
@@ -21,7 +24,7 @@ public class Validator {
 			throw new IllegalArgumentException(message);
 		}
 	}
-	
+
 	public static void validateStrArrLength(String[] inputValue, int length, String message) {
 		if (inputValue.length != length) {
 			throw new IllegalArgumentException(message);
@@ -30,5 +33,39 @@ public class Validator {
 
 	private static boolean isStringEmpty(String str) {
 		return str == null || str.isBlank();
+	}
+
+	public static void validateSize(List<Integer> numbers, int size, String message) {
+		if (numbers.size() != size) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void validateDuplicateNumber(List<Integer> numbers, String message) {
+		if (numbers.stream().distinct().count() != numbers.size()) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void validateNumbersOutOfRange(List<Integer> numbers, int[] ranges, String message) {
+		if (numbers.stream().anyMatch(number -> isNumberOutOfRange(number, ranges))) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	public static void validateNumbersOutOfRange(int number, int[] ranges, String message) {
+		if (isNumberOutOfRange(number, ranges)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	public static void validateNumberInNumbers(List<Integer> numbers, int number, String message) {
+		if (numbers.contains(number)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	private static boolean isNumberOutOfRange(int number, int[] ranges) {
+		return number > ranges[1] || number < ranges[0];
 	}
 }

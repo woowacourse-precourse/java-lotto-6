a8c9db07f2dc3610bbe73ca9ecefa7918a68ce93
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 10:49:45 +0900

    style(InputView): 기능에 맞는 메소드 이름 변경

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String inputMoneyForBuyingLottos() {
+    public static String inputMoneyForLottos() {
         System.out.println("구입금액을 입력해 주세요.");
         String inputLottoPrice = Console.readLine();
         validate(inputLottoPrice);
@@ -19,12 +19,12 @@ public class InputView {
             }
         } catch (NumberFormatException e) {
             System.out.println("[ERROR] 구입 금액은 1000원 단위로 1000원~20억원 사이로 입력해주세요.");
-            inputMoneyForBuyingLottos();
+            inputMoneyForLottos();
         }
     }
 
-    private static int toInt(String lottosPrice) {
-        return Integer.parseInt(lottosPrice);
+    private static int toInt(String lottoPrice) {
+        return Integer.parseInt(lottoPrice);
     }
 
     private static boolean isNotDivideWithOneThousands(int lottoPrice) {

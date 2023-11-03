4f3d294e852440bbf0726117aead95cfc92ee253
Isaac Han <isaachan233@gmail.com>
2023-11-03 22:23:01 +0900

    test: 양수가 아닌 값으로 생성시 예외 발생 테스트 추가

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.enums.ErrorMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,26 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTicketsTest {
     @DisplayName("생성 테스트")
     @Test
-    void createLottoCollection() {
+    void createLottoTickets() {
         LottoTickets lottoTickets = new LottoTickets(5);
     }
+
+    @DisplayName("양수가 아닐 경우 예외가 발생해야함")
+    @Test
+    void createLottoTicketsByZero() {
+        assertThatThrownBy(() -> new LottoTickets(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.NON_POSITIVE_INPUT_MESSAGE.getMessage());
+        assertThatThrownBy(() -> new LottoTickets(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.NON_POSITIVE_INPUT_MESSAGE.getMessage());
+    }
+
     @Test
     void forLoopToStream() {
         List<Integer> lottoTickets = new ArrayList<>();
@@ -34,17 +48,17 @@ class LottoTicketsTest {
 
         final List<Lotto> lottoTickets1 = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            lottoTickets1.add(new Lotto(List.of(1,2,3,4,5,6)));
+            lottoTickets1.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
         }
         lottoTickets = lottoTickets1;
         //
         final List<Lotto> lottoTickets2 = new ArrayList<>();
-        IntStream.range(0, 10).forEach(i -> lottoTickets2.add(new Lotto(List.of(1,2,3,4,5,6))));
+        IntStream.range(0, 10).forEach(i -> lottoTickets2.add(new Lotto(List.of(1, 2, 3, 4, 5, 6))));
         //
         final List<Lotto> lottoTickets3;
 
-        lottoTickets3 = IntStream.range(0,10)
-                .mapToObj(i -> new Lotto(List.of(1,2,3,4,5,6)))
+        lottoTickets3 = IntStream.range(0, 10)
+                .mapToObj(i -> new Lotto(List.of(1, 2, 3, 4, 5, 6)))
                 .collect(Collectors.toList());
 
         assertThat(lottoTickets2.equals(lottoTickets3)).isFalse();

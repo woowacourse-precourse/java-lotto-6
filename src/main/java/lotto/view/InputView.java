815ddd01f209ae3d56f0b216c0f0b1d64a9c6e7a
HamoHamoHamo <hamo222@naver.com>
2023-11-03 22:45:45 +0900

    feat : InputView, OutputView, ConstantMessage 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    InputView() {
+    }
+
+    public static String input() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import lotto.view.constants.ConstantMessage;
+
+public class OutputView {
+    OutputView() {
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void printNewLine() {
+        System.out.println();
+    }
+}

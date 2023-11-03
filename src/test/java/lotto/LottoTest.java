2126aa1a1e7783293c637684852ca4c11a8338b6
DaeYoungK <hanguk830@naver.com>
2023-11-04 00:23:24 +0900

    test(Lotto): 로또 생성 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoStore;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +27,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 생성 후 로또저장소에 저장한다.")
+    @Test
+    void createLottoAndLottoStore() {
+        Lotto.createLotto(List.of(1, 2, 3, 4, 5, 6));
+
+        List<Lotto> lottos = LottoStore.getInstance().getLotto();
+        Lotto lotto = lottos.get(0);
+        List<Integer> lottoNumbers = lotto.getNumbers();
+
+        assertThat(lottos.size()).isEqualTo(1);
+        assertThat(lottoNumbers.size()).isEqualTo(6);
+        assertThat(lottoNumbers.get(2)).isEqualTo(3);
+    }
 }

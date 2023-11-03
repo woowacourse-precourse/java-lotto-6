cac92cc74fc7cc5f5b8d8dd349cee82994a9dc79
minisyu <iswater.kim@gmail.com>
2023-11-04 02:40:54 +0900

    refactor: lotto 패키지로 이동, 검증 로직 메소드명 변경 및 추가

---
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -0,0 +1,31 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        verifyNumbersSize(numbers);
+        verifyNoDuplication(numbers);
+        this.numbers = numbers;
+    }
+
+    private void verifyNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    private void verifyNoDuplication(List<Integer> numbers) {
+        int uniqueCount = (int) numbers.stream().distinct().count();
+        if (uniqueCount != numbers.size()) {
+            throw new IllegalArgumentException("중복되는 숫자가 있으면 안 됩니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
+

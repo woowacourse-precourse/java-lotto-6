9f2fa5656c72337f09da2756ad081f9e6345d261
yeseong31 <yeseong31@naver.com>
2023-11-03 10:56:31 +0900

    feat(Lotto): Lotto 도메인 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
+import static java.util.function.Predicate.isEqual;
+import static lotto.view.constants.SeparatorConstant.LOTTO_NUMBERS_SEPARATOR;
+
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +20,35 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public int checkWinningNumberCount(List<Integer> targetNumbers) {
+        long count = targetNumbers.stream()
+                .filter(this::compareNumbers)
+                .count();
+
+        return Long.valueOf(count).intValue();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s]", appendLottoNumbers());
+    }
+
+    private boolean compareNumbers(Integer number) {
+        return numbers.stream()
+                .anyMatch(isEqual(number));
+    }
+
+    private String appendLottoNumbers() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = 0; index < numbers.size(); index++) {
+            sb.append(numbers.get(index));
+
+            if (index < numbers.size() - 1) {
+                sb.append(LOTTO_NUMBERS_SEPARATOR);
+            }
+        }
+
+        return sb.toString();
+    }
 }

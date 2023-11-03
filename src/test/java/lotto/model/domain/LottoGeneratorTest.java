a9b0e6b952c775470417292187688a6e96826955
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-03 22:10:06 +0900

    refactor: 안쓰는 import삭제

--- src/test/java/lotto/model/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/model/domain/LottoGeneratorTest.java
@@ -1,11 +1,8 @@
 package lotto.model.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
-import org.assertj.core.api.AssertDelegateTarget;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

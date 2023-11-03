58acdfefbd2aa7f219f661d651159be37a0e5e06
Dreaming-J <kimjjong321@naver.com>
2023-11-03 14:54:39 +0900

    :sparkles: feat. 로또 발행 조건 검증

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (isWrongSize(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또는 6개의 숫자로 이루어져야 합니다.");
+        }
+
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 중복되지 않아야 합니다.");
+        }
+    }
+
+    private boolean isWrongSize(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        return Set.copyOf(numbers)
+                .size() != LOTTO_SIZE;
+    }
+}

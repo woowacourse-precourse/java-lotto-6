c0aa4c02e588ffd05cf5377db0fdb4b252f6aa8c
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-03 18:12:26 +0900

    feat: 구입 금액을 1000원 단위로 입력하지 않을 경우 예외를 발생시키는 기능 구현
    - 로또 구입 금액 입력 기능에 예외 기능을 추가합니다.

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,6 +1,8 @@
 package lotto;
 
 public class ErrorMessage {
-    public static final String CONVERT_ERROR = "[ERROR] 구입금액은 숫자로 입력해주세요.";
+    public static final String CONVERT_AMOUNT_ERROR = "[ERROR] 구입금액은 숫자로 입력해주세요.";
 
+    public static final String INSUFFICIENT_AMOUNT_ERROR = "[ERROR] 구입금액은 1000원 이상이어야 합니다.";
+    public static final String INCORRECT_AMOUNT_ERROR = "[ERROR] 구입금액은 1000원 단위로 작성해 주세요.";
 }
--- src/main/java/lotto/InputService.java
+++ src/main/java/lotto/InputService.java
@@ -4,19 +4,37 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputService {
 
-    public static String getUserInputForPurchaseAmount() {
+    public static int getUserInputForPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
-        return readLine();
+        int purchaseAmount = validateAmount(readLine());
+        validateAmountUnit(purchaseAmount);
+        return purchaseAmount;
     }
 
     private static int validateAmount(String purchaseAmount) {
         try {
             return Integer.parseInt(purchaseAmount);
         } catch (Exception e) {
-            throw new IllegalArgumentException(ErrorMessage.CONVERT_ERROR);
+            throw new IllegalArgumentException(ErrorMessage.CONVERT_AMOUNT_ERROR);
         }
     }
 
+    private static void validateAmountUnit(int purchaseAmount) {
+        checkAmountIsSufficient(purchaseAmount);
+        checkAmountUnitIsCorrect(purchaseAmount);
+    }
+
+    private static void checkAmountUnitIsCorrect(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_AMOUNT_ERROR);
+        }
+    }
+
+    private static void checkAmountIsSufficient(int purchaseAmount) {
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException(ErrorMessage.INSUFFICIENT_AMOUNT_ERROR);
+        }
+    }
 
 
 }

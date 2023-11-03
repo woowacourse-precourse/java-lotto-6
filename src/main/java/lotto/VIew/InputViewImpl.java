12c14a0a280f64076df9c634dcc7571241cafc0a
sami355-24 <ddung355@gmail.com>
2023-11-03 11:29:50 +0900

    feat(View): InputView의 입력 기능 구현
    - 구입 금액 입력받는 기능 구현
    - 당첨번호 입력받는 기능 구현
    - 보너스 번호 입력받는 기능구현

---
+++ src/main/java/lotto/VIew/InputViewImpl.java
@@ -0,0 +1,28 @@
+package lotto.VIew;
+
+import static lotto.Message.InputVIewMessage.*;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputViewImpl implements InputView {
+
+    @Override
+    public String inputMoney() {
+        System.out.println(INPUTMONEYPROMPT.getMessage());
+        return Console.readLine();
+    }
+
+    @Override
+    public String inputWinningNumber() {
+        System.out.println(INPUTWINNINGNUMBERPROMPT.getMessage());
+        return Console.readLine();
+    }
+
+    @Override
+    public String inputBonusNumber() {
+        System.out.println(INPUTBONUSNUMBERPROMPT.getMessage());
+        String bonusNumber = Console.readLine();
+        Console.close();
+        return bonusNumber;
+    }
+}

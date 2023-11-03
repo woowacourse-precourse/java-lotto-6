ce1bddb871c6858d00104bb4a0bcdfd53431c121
PJH <parkjh0858@gmail.com>
2023-11-03 22:28:34 +0900

    feat: 로또 번호 중복 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,29 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
+
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicate(List<Integer> numbers) {
+        Set<Integer> num = new HashSet<>(numbers);
+        if (num.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+        }
+    }
 }

33bf9662e0a34a77648a9e82be431f17a0ada0ec
HiiWee <wpdlzhf159@gmail.com>
2023-11-03 21:43:37 +0900

    fix: 구체적인 예외를 처리하도록 변경

--- src/main/java/lotto/exception/ExceptionResolver.java
+++ src/main/java/lotto/exception/ExceptionResolver.java
@@ -1,25 +1,14 @@
 package lotto.exception;
 
-import java.util.function.Supplier;
-
 public class ExceptionResolver {
 
     private ExceptionResolver() {
     }
 
-    public static <T> T resolveInput(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (Exception e) {
-            printExceptionMessage(e.getMessage());
-            return resolveInput(supplier);
-        }
-    }
-
     public static void resolveProcess(final Runnable runnable) {
         try {
             runnable.run();
-        } catch (Exception e) {
+        } catch (IllegalArgumentException | IllegalStateException e) {
             printExceptionMessage(e.getMessage());
             resolveProcess(runnable);
         }

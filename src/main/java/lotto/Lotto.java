af4407d003b452981f24c531d09d128e17abfde3
gusgh075 <gusgh075@naver.com>
2023-11-03 14:32:50 +0900

    FEATURES(Lotto) : 중복되는 수가 있는지 확인하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,6 +9,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateNumbers(numbers);
+        validateIsUnique(numbers);
         this.numbers = numbers;
     }
 
@@ -19,6 +21,12 @@ public class Lotto {
 
     private void validateNumbers(List<Integer> numbers){
         if(numbers.stream().allMatch(n->n>=1&&n<=45)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateIsUnique(List<Integer> numbers){
+        if(numbers.stream().distinct().count()!=numbers.size()){
             throw new IllegalArgumentException();
         }
     }

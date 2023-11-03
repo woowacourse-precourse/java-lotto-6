e696689023e2063f543b690c99bac5659f34d82a
02ggang9 <02ggang9@gmail.com>
2023-11-03 15:37:02 +0900

    fix(LottoOutputView): 로또 리스트를 프리젠테이션 계층으로 넘기도록 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,12 +3,15 @@ package lotto.controller;
 import lotto.Lotto;
 import lotto.service.LottoService;
 import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
 
 import java.util.List;
 
 public class LottoController {
 
     private final LottoInputView lottoInputView = new LottoInputView();
+    private final LottoOutputView lottoOutputView = new LottoOutputView();
+
     private final LottoService lottoService = new LottoService();
 
     public void run() {
@@ -18,7 +21,7 @@ public class LottoController {
     private void buyLottoTicketProcess() {
         Long amount = lottoInputView.getLottoPurchaseAmountInput();
         List<Lotto> lottos = lottoService.purchaseLotto(amount);
-
+        lottoOutputView.presentLottoList(lottos);
     }
 
 }

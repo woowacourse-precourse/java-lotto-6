8c88007d559614e009e66189fb051f561639b2cc
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 00:04:40 +0900

    feat(ErrorMessage): 1000단위 예외 상수 생성

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.constants;
 
 public enum ErrorMessage {
     ERROR_TAG("[ERROR] "),
-    NOT_NUMERIC_ERROR("숫자를 입력해 주세요");
+    NOT_NUMERIC_ERROR("숫자를 입력해 주세요"),
+    NOT_THOUSAND_UNIT_ERROR("1000단위로 입력을 해주세요");
 
     private final String message;
 

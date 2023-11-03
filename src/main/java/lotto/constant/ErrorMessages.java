894a92c703305982743f669220e98a086f74f7e2
RedJunKi <bing8821@naver.com>
2023-11-04 05:51:39 +0900

    refactor(lotto): WinningNumber 객체 내부 검증로직 작성, 당첨번호 생성로직 분리

--- src/main/java/lotto/constant/ErrorMessages.java
+++ src/main/java/lotto/constant/ErrorMessages.java
@@ -5,7 +5,7 @@ public enum ErrorMessages {
     SUFFIX(" 다시 입력해 주세요"),
     INVALID_COUNT("입력하신 번호의 개수가 잘못되었습니다."),
     DUPLICATE_NUMBER("중복된 번호가 있습니다."),
-    INVALID_RANGE("입력 가능한 번호의 범위는 %s부터 %s까지 입니다."),
+    INVALID_RANGE("입력 가능한 번호의 범위는 %d부터 %d까지 입니다."),
     INVALID_TYPE("번호는 숫자만 입력 가능합니다."),
     INVALID_AMOUNT("입력하신 금액으로 나누어 떨어지지 않습니다.")
     ;
@@ -20,7 +20,7 @@ public enum ErrorMessages {
         return this.message;
     }
 
-    public String format(String... args) {
-        return String.format(message, (Object[]) args);
+    public String format(Object... args) {
+        return String.format(message, args);
     }
 }
---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import lotto.constant.ErrorMessages;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static lotto.constant.NumberConstants.*;
+
+public class WinningNumber {
+
+    private final List<Integer> winningNumber;
+
+    public WinningNumber(List<Integer> winningNumber) {
+        hasCorrectNumberSize(winningNumber);
+        hasNoDuplicateNumber(winningNumber);
+        hasCorrectRange(winningNumber);
+        this.winningNumber = winningNumber;
+    }
+
+    private void hasCorrectNumberSize(List<Integer> winningNumber) {
+        if (winningNumber.size() != WINNING_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX.getMessage() +
+                    ErrorMessages.INVALID_COUNT +
+                    ErrorMessages.SUFFIX.getMessage());
+        }
+    }
+
+    private void hasCorrectRange(List<Integer> winningNumber) {
+        if (winningNumber.stream()
+                .anyMatch(num -> num < NUMBER_RANGE_MIN && num > NUMBER_RANGE_MAX)) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX.getMessage() +
+                    ErrorMessages.INVALID_RANGE.format(NUMBER_RANGE_MIN, NUMBER_RANGE_MAX) +
+                    ErrorMessages.SUFFIX.getMessage());
+        }
+    }
+
+    private void hasNoDuplicateNumber(List<Integer> winningNumber) {
+        Set<Integer> uniqueNumber = new HashSet<>(winningNumber);
+
+        if (uniqueNumber.size() != WINNING_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX.getMessage() +
+                    ErrorMessages.DUPLICATE_NUMBER.getMessage() +
+                    ErrorMessages.SUFFIX.getMessage());
+        }
+    }
+
+    public List<Integer> getWinningNumber() {
+        return winningNumber;
+    }
+}

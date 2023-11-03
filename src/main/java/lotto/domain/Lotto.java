3bd7e47f74126ff23b09a8d651c989b8a576f0dd
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-03 13:53:22 +0900

    feat: Lotto에 대한 검증 추가
    Lotto의 숫자들의 범위가 1부터 45사이인지 체크하는 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,18 +3,37 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(final List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validate(final List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(final List<Integer> numbers) {
+        if (!isAllValidRange(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isAllValidRange(final List<Integer> numbers) {
+        return numbers.stream()
+                .allMatch(this::isValidRange);
+    }
+
+    private boolean isValidRange(final Integer number) {
+        return MIN_NUMBER <= number && number <= MAX_NUMBER;
+    }
 }

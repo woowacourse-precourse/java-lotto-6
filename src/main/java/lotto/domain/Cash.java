f26de3a75db2d9a6da35e37399a3d16da42c1a2e
jongheonleee <qwefghnm1212@gmail.com>
2023-11-03 19:03:18 +0900

    [feat] 검증 메서드 추가

--- src/main/java/lotto/domain/Cash.java
+++ src/main/java/lotto/domain/Cash.java
@@ -2,13 +2,29 @@ package lotto.domain;
 
 public class Cash {
 
-    private static final Integer unit = 1000;
+    private static final Integer UNIT = 1000;
+    private static final Integer ZERO = 0;
     private final Integer amount;
 
     public Cash(final Integer amount) {
+        validateAvailableAmount(amount);
+        validateAvailableCash(amount);
         this.amount = amount;
     }
 
+    private void validateAvailableAmount(Integer amount) {
+        if (amount < UNIT) {
+            throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액을 입력하셔야 합니다.");
+        }
+    }
+
+
+    private void validateAvailableCash(Integer amount) {
+        int remainder = amount % UNIT;
+        if (remainder != ZERO) {
+            throw new IllegalArgumentException("[ERROR] 1000원으로 나누어 떨어지는 금액을 입력하셔야 합니다.");
+        }
+    }
 
 
 }

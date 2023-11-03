3c08a2168d11dfa99925c97fb3dc8045f0dc1ba8
Minjae-An <mj3242@naver.com>
2023-11-03 20:51:10 +0900

    feat : 게임 출력 메시지 정의

---
+++ src/main/java/lotto/view/constants/ConstantMessage.java
@@ -0,0 +1,22 @@
+package lotto.view.constants;
+
+public enum ConstantMessage {
+    ENTERING_PURCHASE_AMOUNT("구입 금액을 입력해 주세요.\n"),
+    GUIDANCE_FOR_PURCHASE_QUANTITY("%d개를 구매했습니다.\n"),
+    ENTERING_WINNING_NUMBER("당첨 번호를 입력해주세요.\n"),
+    ENTERING_BONUS_NUMBER("보너스 번호를 입력해주세요.\n"),
+    WINNING_STATISTICS_NOTICE("당첨 통계\n"),
+    DIVIDER("---\n"),
+    LOTTO_RESULT("%d개 일치 (%d원) - %d개\n"),
+    TOTAL_RETURN("총 수익률은 %.1f%입니다.\n");
+
+    private final String message;
+
+    ConstantMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

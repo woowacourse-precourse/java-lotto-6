418d9221b0ddd62a6608e4bd7b0b6b0d96c3832e
이병수 <qudtn0315@naver.com>
2023-11-03 16:41:53 +0900

    feat (Lotto) : 로또번호 반환하는 메서드 추가 및 에러 메세지 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,12 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호 갯수가 6개가 아닙니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> lottoNumber() {
+        return numbers;
+    }
 }

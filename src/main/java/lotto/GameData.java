2a2f437d2fe74e415d679b789d1957de5687102c
brightfanta <brightfanta@gmail.com>
2023-11-03 15:01:11 +0900

    feat(GameData): 변수 선언

--- src/main/java/lotto/GameData.java
+++ src/main/java/lotto/GameData.java
@@ -1,4 +1,15 @@
 package lotto;
 
+import java.util.List;
+
 public class GameData {
+
+    private Integer budget;
+    private List<Integer> winingNumbers;
+    private Integer bonusNumber;
+    private Integer LotteriesNumber;
+    private Integer winningPrizeByRank;
+    private Double budgetPrizeRatio;
+    private Integer totalPrize;
+
 }

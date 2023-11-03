43880a4e12153dcf6d965402ad58f23e05842468
현 <sohyun95277@gmail.com>
2023-11-03 20:14:40 +0900

    test(LottoCalculatorTest): 모든 로또 등수를 판별하는 기능
    2등, 3등 티켓을 가지고 결과값 판별

--- src/test/java/lotto/domain/LottoCalculatorTest.java
+++ src/test/java/lotto/domain/LottoCalculatorTest.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.List;
+import java.util.Map;
+import lotto.constant.Prize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +24,7 @@ class LottoCalculatorTest {
         assertThat(calculator.countMatchNumbers(lotto)).isEqualTo(3);
     }
 
-    @DisplayName("당첨 번호와 보너스 번호를 함께 비교하는 기능")
+    @DisplayName("2등 당첨이라면 매치 개수는 7을 반환한다")
     @Test
     void countMatchBonusNumbers() {
         numbers.makeWinningNumber("4,12,39,43,1,17");
@@ -29,5 +33,26 @@ class LottoCalculatorTest {
         Lotto lotto = new Lotto(List.of(3, 4, 12, 17, 39, 43));
 
         assertThat(calculator.countMatchNumbers(lotto)).isEqualTo(7);
+    }
+
+    @DisplayName("모든 로또의 등수를 판별하는 기능")
+    @Test
+    void makePrizeResult() {
+        //given
+        numbers.makeWinningNumber("4,12,39,43,1,17");
+        numbers.makeBonusNumber("3");
+        Lotto firstLotto = new Lotto(List.of(3, 4, 12, 17, 39, 43)); // 2등
+        Lotto secondLotto = new Lotto(List.of(1, 4, 12, 17, 43, 45)); // 3등
+        LottoCalculator calculator = new LottoCalculator(numbers);
+        //when
+        calculator.makePrizeResult(List.of(firstLotto, secondLotto));
+        Map<Prize, Integer> result = calculator.getResult();
+        //then
+        assertEquals(1, result.get(Prize.SECOND));
+        assertEquals(1, result.get(Prize.THIRD));
+        assertNotEquals(1, result.get(Prize.FIRST));
+        assertNotEquals(1, result.get(Prize.FOURTH));
+        assertNotEquals(1, result.get(Prize.FIFTH));
+        assertNotEquals(1, result.get(Prize.NONE));
     }
 }

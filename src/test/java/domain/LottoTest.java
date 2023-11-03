5b964d86f0d0d8c9a3e2d484389b82dbeb382d87
songsunkook <songsunkook@gmail.com>
2023-11-03 21:23:52 +0900

    test: 당첨 등수 test 추가

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +24,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    public void 당첨_등수를_정확하게_매기는가() {
+        //given
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto1st = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2nd = new Lotto(List.of(1, 2, 3, 4, 5, 9));
+        Lotto lotto3rd = new Lotto(List.of(1, 2, 3, 4, 5, 9));
+        Lotto lotto4st = new Lotto(List.of(1, 2, 3, 4, 9, 9));
+        Lotto lotto5st = new Lotto(List.of(1, 2, 3, 9, 9, 9));
+
+        //then
+        assertEquals(1, lotto1st.rank(winningLotto, 9));
+        assertEquals(2, lotto2nd.rank(winningLotto, 9));
+        assertEquals(3, lotto4st.rank(winningLotto, 10));
+        assertEquals(4, lotto5st.rank(winningLotto, 9));
+        assertEquals(5, lotto3rd.rank(winningLotto, 9));
+    }
 }

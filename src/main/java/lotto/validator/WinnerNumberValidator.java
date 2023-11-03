70338aacd8aca85ab2512bba4a29a70e3ad04dea
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-03 16:13:17 +0900

    feat: 당첨번호 입력값에 숫자가 아닌 문자가 포함되어있으면 예외처리하는 기능

--- src/main/java/lotto/validator/WinnerNumberValidator.java
+++ src/main/java/lotto/validator/WinnerNumberValidator.java
@@ -1,16 +1,26 @@
 package lotto.validator;
 
 import lotto.message.ExceptionMessage;
+import lotto.util.LottoUtil;
 
 public class WinnerNumberValidator {
 
-    public static void validate(String numbers) {
-
+    public static void validate(String inputNumbers) {
+        validateDividedByComma(inputNumbers);
+        validateNaturalNumber(inputNumbers);
     }
 
-    private static void validateDividedByComma(String numbers) {
-        if (!numbers.contains(",")) {
+    private static void validateDividedByComma(String inputNumbers) {
+        if (!inputNumbers.contains(",")) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_DIVIDED_BY_COMMA.getMessage());
+        }
+    }
+
+    private static void validateNaturalNumber(String inputNumbers) {
+        try {
+            LottoUtil.convert(inputNumbers);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NATURAL_NUMBER.getMessage());
         }
     }
 

d33b59e845108df5f538ba310162ce74107a083c
sami355-24 <ddung355@gmail.com>
2023-11-03 20:21:50 +0900

    feat(Message/ExceptionMessage): 로또 예외 발생 메시지 추가
    - 중복되었을때 예외 발생 안내 문구 추가
    - 로또 번호의 개수가 6개 가 아닐때 발생하는 예외 안내문구 추가

---
+++ src/main/java/lotto/Message/ExceptionMessage/LottoExceptionMessage.java
@@ -0,0 +1,16 @@
+package lotto.Message.ExceptionMessage;
+
+public enum LottoExceptionMessage {
+    LOTTO_SIZE_ERROR_MESSAGE("로또 번호의 수가 6개가 아닙니다."),
+    LOTTO_DUPLICATE_ERROR_MESSAGE("로또 번호가 중복되었습니다.");
+
+    private String message;
+
+    LottoExceptionMessage(String message) {
+        this.message = PrefixMessage.ERROR_MESSAGE.getMessage() + message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}

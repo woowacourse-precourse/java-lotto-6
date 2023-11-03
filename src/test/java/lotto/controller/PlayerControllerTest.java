bc3ef3bc411b3f0cd6f66a941f4d3d14729f8268
BullChallenger <am23am45@naver.com>
2023-11-04 00:53:16 +0900

    [REFACTOR] 기존에 작성되어 있던 PlayerController의 테스트 코드 변경
    생성자를 통한 DI 방식을 채용했기 때문에 의존성 주입을 위한 코드 변경이 이루어졌다

--- src/test/java/lotto/controller/PlayerControllerTest.java
+++ src/test/java/lotto/controller/PlayerControllerTest.java
@@ -1,17 +1,20 @@
 package lotto.controller;
 
+import lotto.domain.Player;
+import lotto.service.LottoService;
+import lotto.service.PlayerService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.constant.ExceptionMessage.NON_NUMERIC_ERROR;
-import static lotto.constant.ExceptionMessage.TOO_SMALL_PRICE_TO_BUY;
+import static lotto.constant.ExceptionMessage.INSUFFICIENT_PRICE_TO_BUY;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerControllerTest {
 
-    private PlayerController playerController = new PlayerController();
+    private PlayerController playerController = new PlayerController(new PlayerService(new LottoService(), new Player()));
 
     @ParameterizedTest
     @DisplayName(value = "입력 값에 숫자 이외의 값이 있으면 예외가 발생하는지 확인")
@@ -27,7 +30,7 @@ class PlayerControllerTest {
     void purchaseLottoByInsufficientAmount() {
         assertThatThrownBy(() -> playerController.purchaseLotto("800"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(TOO_SMALL_PRICE_TO_BUY.getMessage());
+                .hasMessageContaining(INSUFFICIENT_PRICE_TO_BUY.getMessage());
     }
 
 }

26cf416dbf7b7a59b65bf35b8d9c700b22a5c5c9
suihub <qkdcrt@gmail.com>
2023-11-04 01:18:56 +0900

    <test(CustomerTest)> : "구매 금액 최대 제한 테스트 구현"
    - 구매 구입 중 최대 100,000원까지 금액에 제한을 두는 테스트 구현.
    
    - 이후 본 로직 구현

--- src/test/java/lotto/domain/CustomerTest.java
+++ src/test/java/lotto/domain/CustomerTest.java
@@ -13,4 +13,12 @@ class CustomerTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 숫자를 입력해주세요.");
     }
+
+    @DisplayName("로또 구매 금액이 100,000원을 넘어선 안 된다.")
+    @Test
+    void createCustomerByOverMoney() {
+        assertThatThrownBy(() -> new Customer("106000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구매 금액은 최소 1000원부터 최대 10만원까지입니다.");
+    }
 }

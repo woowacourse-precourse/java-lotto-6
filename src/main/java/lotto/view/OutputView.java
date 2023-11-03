f19edce0884c931d8df4cedffb1176c7dfd77e05
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-04 00:27:21 +0900

    refactor(OutputView): 로또 번호 반복 출력 format 개선

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,12 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.domain.Lotto;
 
 public class OutputView {
+    private static final String DIGIT_COMMA_WITH_BLANK = ", ";
     private static final OutputView instance = new OutputView();
 
     public static OutputView getInstance() {
@@ -14,14 +17,19 @@ public class OutputView {
         System.out.printf(Message.OUTPUT_COMPLETE_BUY_LOTTO.message, lotties.size());
         for (Lotto lotto : lotties) {
             List<Integer> numbers = lotto.getNumbers();
-            System.out.printf(Message.OUTPUT_LOTTO_NUMBERS.message,
-                    numbers.get(0), numbers.get(1), numbers.get(2), numbers.get(3), numbers.get(4), numbers.get(5));
+            System.out.printf(Message.OUTPUT_LOTTO_NUMBERS.message, formatter(numbers));
         }
+    }
+
+    private String formatter(List<Integer> numbers) {
+        return numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(DIGIT_COMMA_WITH_BLANK));
     }
 
     private enum Message {
         OUTPUT_COMPLETE_BUY_LOTTO("\n%s개를 구매했습니다.\n"),
-        OUTPUT_LOTTO_NUMBERS("[%d, %d, %d, %d, %d, %d]\n");
+        OUTPUT_LOTTO_NUMBERS("[%s]\n");
 
         private final String message;
 

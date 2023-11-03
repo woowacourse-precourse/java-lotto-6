a02546f3d822ba89a8e82865e60c421acd3af6b1
yu-teacher <yusm1231@gmail.com>
2023-11-03 23:29:12 +0900

    test : 로또번호 출력에 대한 테스트
    금액 입력시 로또번호가 발행되는지 테스트
    숫자가 아닌 입력값에 대한 예외처리
    천 미만의 수 예외처리
    백원단위가 있을 때 예외처리
    
    전부 정상작동함

---
+++ src/test/java/lotto/MyLottoTest.java
@@ -0,0 +1,47 @@
+package lotto;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.Lottos;
+import lotto.view.LottoView;
+import org.junit.jupiter.api.Test;
+
+public class MyLottoTest {
+
+    @Test
+    void 금액_입력시_로또_번호_발행() {
+        String numberStr = "10000";
+
+        Lottos lottos = new Lottos(numberStr);
+        LottoView lottoView = new LottoView();
+
+        lottoView.outputLottoList(lottos);
+    }
+
+    @Test
+    void 숫자가_아닌_것_예외처리(){
+        String numberStr = "ㅂㅈㄷ";
+
+        assertThatThrownBy(() -> new Lottos(numberStr))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 천_미만의_수_예외처리(){
+        String numberStr = "900";
+
+        assertThatThrownBy(() -> new Lottos(numberStr))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 백원단위가_있을_때_예외처리(){
+        String numberStr = "11900";
+
+        assertThatThrownBy(() -> new Lottos(numberStr))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

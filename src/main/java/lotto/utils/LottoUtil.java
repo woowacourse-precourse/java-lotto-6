3170890ddc957c1838b3c24638bee02e59cdf9d5
alscjf1329 <alscjf1329@gmail.com>
2023-11-03 23:43:27 +0900

    refactor(LottoUtil): return 타입 변경
    조건에 부합하지 않으면 예외를 던지기에 return 타입을 void로 하는게 맞다고 생각했습니다.

--- src/main/java/lotto/utils/LottoUtil.java
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -3,10 +3,9 @@ package lotto.utils;
 import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
 
 public class LottoUtil {
-    public static boolean validateLottoNum(int num) {
+    public static void validateLottoNum(int num) {
         if (1 <= num && num <= 45) {
             throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
         }
-        return true;
     }
 }

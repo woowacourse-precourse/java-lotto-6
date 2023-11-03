dc4eb34c1139280016e891e4db6aa0112bfb9985
sangdol <priceelees@gmail.com>
2023-11-03 10:08:36 +0900

    refactor(Lotto): 상수화 및 중복 숫자 검증 과정 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int SIX = 6;
+    private static final String ERROR = "[ERROR] ";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,11 +13,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != SIX) {
+            throw new IllegalArgumentException(ERROR + "6개의 숫자로 이루어져야 합니다.");
+        }
+        if (hasDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(ERROR + "중복된 숫자가 존재할 수 없습니다.");
         }
     }
 
+    private boolean hasDuplicateNumber(List<Integer> numbers) {
+        List<Integer> distinctedNumbers = numbers.stream()
+                .distinct()
+                .toList();
+
+        return !distinctedNumbers.equals(numbers);
+    }
     public boolean isContain(int number) {
         return numbers.contains(number);
     }

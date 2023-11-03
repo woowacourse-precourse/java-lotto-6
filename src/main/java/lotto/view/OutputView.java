8e548b986b555355d313c783ec84e8e48f8b2e11
sangdol <priceelees@gmail.com>
2023-11-03 11:03:39 +0900

    refactor(OutputView): 상수화 및 메시지 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,12 @@ public class OutputView {
     private static final String STATUS = "당첨 통계";
     private static final String THREE_DASH = "---";
     private static final String NEW_LINE = System.getProperty("line.separator");
+    private static final int HUNDREAD = 100;
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
     public void printUserLottos(int lottosCount, String lottos) {
         System.out.printf(PURCHASE_MESSAGE + NEW_LINE, lottosCount);
         System.out.println(lottos);
@@ -18,7 +24,7 @@ public class OutputView {
     }
 
     public void printEarningRate(int purchaseAmount, int revenue) {
-        double earningRate = ((double)revenue / purchaseAmount) * 100;
+        double earningRate = ((double) revenue / purchaseAmount) * HUNDREAD;
         System.out.printf(EARNING_RATE_MESSAGE + NEW_LINE, earningRate);
     }
 }

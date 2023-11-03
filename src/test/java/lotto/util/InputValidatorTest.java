f121a4efe02467f65a0c8ab4e6bc2db4befba72a
Juhyorim <joohr1234@gmail.com>
2023-11-04 05:15:42 +0900

    test: 로또 구입금액이 1000원 단위가 아니면 예외발생 테스트 작성

--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class InputValidatorTest {
     private static final String NO_A_NUMBER_MESSAGE = "로또 금액은 숫자여야 합니다";
     private static final String MIN_PRICE_1000_MESSAGE = "로또 금액은 1000원 이상이어야 합니다";
+    private static final String UNIT_OF_1000_REQUIRED_MESSAGE = "로또 금액은 1000원 단위로 입력되어야 합니다";
 
     private static InputValidator inputValidator;
 
@@ -45,5 +46,14 @@ class InputValidatorTest {
         assertThatThrownBy(() -> inputValidator.validatePrice(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(MIN_PRICE_1000_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1999", "20001", "1001", "999900"})
+    @DisplayName("로또 구입금액이 1000원 단위가 아니면 예외")
+    void 로또_구입금액이_1000원_단위가_아니면_예외(String userInput) {
+        assertThatThrownBy(() -> inputValidator.validatePrice(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(UNIT_OF_1000_REQUIRED_MESSAGE);
     }
 }

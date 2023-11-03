9ab23b4ab5a3754e0f82e03fd8b208e8c12b90e1
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 16:35:51 +0900

    refactor(OutputView): 공백라인 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,38 +1,43 @@
 package lotto.view;
 
-import lotto.domain.LottoRepository;
 import lotto.io.OutputViewPrinter;
 import lotto.util.message.ProgressMessage;
+import lotto.util.message.WinningStatisticsMessage;
 
 public class OutputView {
-
+	
 	private static final OutputViewPrinter PRINTER = new OutputViewPrinter();
+	private static final String NEWLINE = "\n";
 
 	public void printInputMoneyMessage() {
 		PRINTER.printLine(ProgressMessage.INPUT_MONEY.getMessage());
 	}
 
 	public void printBuyedLottoNumberMessage(int buyedCount) {
-		PRINTER.printLine(ProgressMessage.BUYED_LOTTO_NUMBER.getForMatMessage(buyedCount));
+		PRINTER.printLine(NEWLINE + ProgressMessage.BUYED_LOTTO_NUMBER.getForMatMessage(buyedCount));
 	}
 
-	public void printLottoNumbers() {
-		PRINTER.printLine(LottoRepository.printLottoNumbers());
+	public void printLottoNumbers(String message) {
+		PRINTER.printLine(message);
 	}
 
 	public void printInputAnswerLottoNumberMessage() {
-		PRINTER.printLine(ProgressMessage.INPUT_ANSWER_LOTTO_NUMBER.getMessage());
+		PRINTER.printLine(NEWLINE + ProgressMessage.INPUT_ANSWER_LOTTO_NUMBER.getMessage());
 	}
 
 	public void printInputBonusNumberMessage() {
-		PRINTER.printLine(ProgressMessage.INPUT_BONUS_NUMBER.getMessage());
+		PRINTER.printLine(NEWLINE + ProgressMessage.INPUT_BONUS_NUMBER.getMessage());
 	}
 
-	public void printWinningStatisticsMessage() {
-		PRINTER.printLine(ProgressMessage.WINNING_STATISTICS.getMessage());
+	public void printWinningStatisticsTopMessage() {
+		PRINTER.printLine(NEWLINE + WinningStatisticsMessage.TOP.getMessage());
 	}
-
+	
+	public void printWinningStatisticsMessage(String message) {
+		PRINTER.printLine(message);
+	}
+	
 	public void printTotalReturn(double totalReturn) {
-		PRINTER.printLine(ProgressMessage.TOTAL_RETURN.getForMatMessage(totalReturn));
+		PRINTER.printLine(WinningStatisticsMessage.TOTAL_RETURN.getForMatMessage(totalReturn));
 	}
 }

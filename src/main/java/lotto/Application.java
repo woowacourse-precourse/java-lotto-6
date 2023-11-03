1682ab596e067035d2d5116c3f7f13f50375dc58
brightfanta <brightfanta@gmail.com>
2023-11-03 21:39:36 +0900

    feat(Application): 일치번호개수 산출 메서드 호출, 일치번호개수 컬렉션과 로또등수 컬렉션 생성, 일치번호 개수별 로또 등수 저장 기능 구현, 일치번호 5개일 경우 보너스 번호 일치 여부에 따른 2, 3등 순위 할당 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,6 +8,7 @@ public class Application {
     public static void main(String[] args) {
 
         GameData gameData = new GameData();
+        GameStatistics gameStatistics = new GameStatistics();
 
         View.printMessage(ASK_BUDGET);
         String lottoBudgetInput = View.getUserInput();
@@ -39,10 +40,52 @@ public class Application {
         Integer bonusNumberTemp = Convertor.convertInputToInteger(bonusNumberInput);
         gameData.setBonusNumber(bonusNumberTemp);
 
+        List<Integer> winningNumbers = gameData.getWinningNumbers();
+        gameStatistics.generateMatchingNumberCountList();
+        List<Integer> matchingNumberCountList = gameStatistics.getSameNumberCountList();
+        gameStatistics.generateRankList();
+        List<Rank> rankList = gameStatistics.getRankList();
 
+        for (Lotto lotto : lottoList) {
+            int countTemp = 0;
+            List<Integer> lottoNumbers = lotto.getNumbers();
+            for (Integer lottoNumber : lottoNumbers) {
+                countTemp += Controller.countMatchingNumbersByLotteryNumber(winningNumbers, lottoNumber);
+            }
+            matchingNumberCountList.add(countTemp);
+        }// 리팩토링 고민해봐야 함.
+
+        for (Integer matchingNumberCount : matchingNumberCountList) {
+            if (matchingNumberCount.equals(6)) {
+                rankList.add(Rank.FIRST);
+            }
+            if (matchingNumberCount.equals(5)) {
+                rankList.add(Rank.THIRD);
+            }
+            if (matchingNumberCount.equals(4)) {
+                rankList.add(Rank.FOURTH);
+            }
+            if (matchingNumberCount.equals(3)) {
+                rankList.add(Rank.FIFTH);
+            }
+        }
+
+        Integer bonusNumber = gameData.getBonusNumber();
+        for (int i = 0; i < rankList.size(); i++) {
+            boolean bonusFlag;
+            Rank rank = rankList.get(i);
+            if (rank.equals(Rank.THIRD)) {
+                Lotto secondRankCandidateLotto = lottoList.get(i);
+                List<Integer> secondRankCandidateLottoNumbers = secondRankCandidateLotto.getNumbers();
+                bonusFlag = Controller.bonusNumberFlag(secondRankCandidateLottoNumbers, bonusNumber);
+                Controller.changeRankByBonusNumber(rankList, bonusFlag, i);
+            }
+        }
 
         View.printMessage(GAME_STATISTICS);
         View.printMessage(SEPARATE_LINE);
+
+
     }
 
 }

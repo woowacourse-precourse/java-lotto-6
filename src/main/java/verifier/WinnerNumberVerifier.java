8a06a4360ece47b3c446194e0548ce813dd3105a
Hoo-sung <gntjd135@gmail.com>
2023-11-03 16:20:51 +0900

    feat(WinnerNumberVerifier): 당첨번호가 각각 1-45사이의 수인지 검증하는 메서드 생성

--- src/main/java/verifier/WinnerNumberVerifier.java
+++ src/main/java/verifier/WinnerNumberVerifier.java
@@ -1,26 +1,39 @@
 package verifier;
 
-public class WinnerNumberVerifier implements Verifier{
+public class WinnerNumberVerifier implements Verifier {
     @Override
     public void check(String input) {
         checkNumberCount(input);
         checkEachNumeric(input);
+        checkEachRange(input);
     }
 
-    private void checkEachNumeric(String input){
+    private void checkEachNumeric(String input) {
         String[] numbers = input.split(",");
-        for(String number : numbers){
-            try{
+        for (String number : numbers) {
+            try {
                 Integer.parseInt(number);
-            }catch(Exception e){
+            } catch (Exception e) {
                 throw new IllegalArgumentException("입력된 값은 숫자가 아닙니다.");
             }
         }
     }
-    private void checkNumberCount(String input){
+
+    private void checkNumberCount(String input) {
         String[] numbers = input.split(",");
-        if(numbers.length != 6){
+        if (numbers.length != 6) {
             throw new IllegalArgumentException("당첨번호는 6개의 숫자가 아닙니다.");
         }
     }
+
+    private void checkEachRange(String input) {
+        String[] inputs = input.split(",");
+        for (String num : inputs) {
+            int number = Integer.parseInt(num);
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또번호는 1~45사이의 숫자이어야 합니다.");
+            }
+        }
+    }
+
 }

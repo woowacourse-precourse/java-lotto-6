176bda59df1df0fcd7255d27e5346180f13c7a6b
Dreaming-J <kimjjong321@naver.com>
2023-11-03 15:16:59 +0900

    :lipstick: style. 코드 가독성 수정

--- src/test/java/lotto/model/LottoNoTest.java
+++ src/test/java/lotto/model/LottoNoTest.java
@@ -10,8 +10,10 @@ public class LottoNoTest {
     @Test
     void 로또_번호_범위_테스트() {
         assertAll(
-                () -> assertThatThrownBy(() -> new LottoNo(0)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new LottoNo(46)).isInstanceOf(IllegalArgumentException.class)
+                () -> assertThatThrownBy(() -> new LottoNo(0))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new LottoNo(46))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 }

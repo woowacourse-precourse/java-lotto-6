3fc040ae80fa9951634249fcf2497919ecf2e024
KimDabomi <cdabomi@nate.com>
2023-11-03 15:23:48 +0900

    feat: getBonusNumber method

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -30,4 +30,9 @@ public class InputView {
 
         return new Lotto(winningNumbers);
     }
+
+    public static int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
 }

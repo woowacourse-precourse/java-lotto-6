bf145f4ee944bb754454038f45c34db545988af1
jongwonpc <c3392302@naver.com>
2023-11-03 15:55:46 +0900

    feat: 등수와 인원을 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 
 public class OutputView {
 
@@ -13,5 +15,24 @@ public class OutputView {
             System.out.println(lotto.getNumbers());
         }
     }
-    
+
+    public static void printLottoResult(Map<LottoResult, Integer> lottoMap) {
+        System.out.println("당첨 통계\n"
+                + "---");
+
+        for (LottoResult lottoResult : lottoMap.keySet()) {
+            StringBuilder stringBuilder = new StringBuilder(lottoResult.getCount());
+            stringBuilder.append("개 일치");
+            stringBuilder.append(" (");
+            if (lottoResult.isValidBonus()) {
+                stringBuilder.append(", 보너스 볼 일치");
+            }
+            stringBuilder.append(lottoResult.getPrize());
+            stringBuilder.append("원) - ");
+            stringBuilder.append(lottoMap.get(lottoResult));
+            stringBuilder.append("개");
+            System.out.println(stringBuilder);
+        }
+    }
+
 }

437859fcbe187869c224d84ebc1c0ec49254a464
euics <pjh575157@gmail.com>
2023-11-03 14:25:36 +0900

    test: testDuplicatesThrowException 메서드 구현
    중복된 숫자가 있으면 LottoException을 발생 메서드
    - 중복된 숫자가 포함되어 있으면 LottoException 발생

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,33 @@
+package lotto.service;
+
+import lotto.exception.LottoException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoServiceTest {
+
+    @Test
+    @DisplayName("당첨 번호 길이 검증")
+    void testInvalidSizeThrowsException() {
+        List<Integer> numbersWithInvalidSize = Arrays.asList(1, 2, 3, 4); // Incorrect size
+
+        assertThrows(LottoException.class, () -> {
+            LottoException.ensureValidSize(numbersWithInvalidSize);
+        });
+    }
+
+    @Test
+    @DisplayName("중복된 숫자가 있으면 LottoException을 발생")
+    void testDuplicatesThrowException() {
+        List<Integer> numbersWithDuplicates = Arrays.asList(1, 2, 3, 3, 4, 5); // 중복된 숫자 포함
+
+        assertThrows(LottoException.class, () -> {
+            LottoException.ensureDistinctNumbers(numbersWithDuplicates);
+        });
+    }
+}

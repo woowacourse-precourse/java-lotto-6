6765017cad6c2fff5cd73211aa36b1b92c4a745e
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-03 15:53:28 +0900

    test: tests Purchase.java

--- src/test/java/lotto/domain/purchase/PurchaseTest.java
+++ src/test/java/lotto/domain/purchase/PurchaseTest.java
@@ -1,19 +1,55 @@
 package lotto.domain.purchase;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 
 class PurchaseTest {
+
+    @DisplayName("투입한 금액을 1,000원으로 나눈 몫만큼 발행할 로또의 양을 리턴한다. paidPrice에 대한 검증이 이루어지므로 quantity는 자연수이다.")
     @Test
-    void validatePrice() {
+    void makePurchaseTest() {
+        // Given
+        String input = "5000";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        Purchase purchase = new Purchase();
+
+        // When
+        int lottoQuantity = purchase.makePurchase();
+
+        // Then
+        Assertions.assertEquals(5, lottoQuantity);
+    }
+
+    @DisplayName("올바른 금액 투입 시 while문을 잘 탈출하는지 확인한다.")
+    @Test
+    void repeatPurchasingTest() {
         // Given
         Purchase purchase = new Purchase();
 
         // When
-        int validPrice = 2000;
-        int invalidPrice = 1234;
-        int zeroPrice = 0;
+        String validPrice = "8000";
+
+        // Then
+        Assertions.assertDoesNotThrow(() -> purchase.repeatPurchasing(validPrice));
+    }
+
+    @DisplayName("가격에 대한 유효성 검증을 테스트한다.")
+    @Test
+    void validatePriceTest() {
+        // Given
+        Purchase purchase = new Purchase();
+
+        // When
+        String validPrice = "2000";
+        String invalidPrice = "1234";
+        String zeroPrice = "0";
 
         // Then
         Assertions.assertDoesNotThrow(() -> purchase.validatePrice(validPrice));

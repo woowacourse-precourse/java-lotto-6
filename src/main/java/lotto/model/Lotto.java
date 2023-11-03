a8525a8401a637171407aa23775dde18c264f471
YongHyeon <pyh4616@naver.com>
2023-11-03 14:53:24 +0900

    Feat(Lotto 클래스): 로또 번호 중복에 대한 유효성 검사 기능을 validateDuplicate 메서드로 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,12 +1,15 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +20,10 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set duplicateCheck = new HashSet(numbers);
+        if (duplicateCheck.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되는 값이 존재하지 않아야 합니다.");
+        }
+    }
 }

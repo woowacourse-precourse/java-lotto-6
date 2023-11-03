c177f4be0b4361db9d92a26f9c29c6bbef7c89b6
siho-song <thdtlgh234@naver.com>
2023-11-03 13:34:29 +0900

    [Feat] 인풋 메시지 관련 기능 구현
    인풋 메시지와 관련된 기능을 하는 InputView 클래스를 구현하였습니다.

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+public class InputView {
+    private static final String MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    private static void showMessage(String message){
+        System.out.println(message);
+    }
+    public static void moneyMessage(){
+        showMessage(MONEY_MESSAGE);
+    }
+
+    public static void numbersMessage(){
+        showMessage(NUMBERS_MESSAGE);
+    }
+
+    public static void bonusNumberMessage(){
+        showMessage(BONUS_NUMBER_MESSAGE);
+    }
+
+
+}

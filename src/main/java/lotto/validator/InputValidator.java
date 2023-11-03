5c912d7d6df81856540c871716e3dd09e02e233e
doha <dohaahi@naver.com>
2023-11-03 12:17:06 +0900

    refactor(InputValidator): 구입 금액 검증 메소드 static 메소드로 변경

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -3,7 +3,7 @@ package lotto.validator;
 public class InputValidator {
     public static final String WRONG_INPUT_PURCHASE_AMOUNT_MESSAGE = "구입 금액은 숫자로 입력해주세요.";
 
-    public void validatePurchaseAmount(final String input) {
+    public static void validatePurchaseAmount(final String input) {
         try {
             Integer.parseInt(input);
         } catch (Exception e) {

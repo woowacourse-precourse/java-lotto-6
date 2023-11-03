045001372e36969c5d9a4594e4f27824a22dccdf
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-04 02:55:46 +0900

    feat: 값 미전달 예외 처리 추가
    아무런 값을 전달하지 않을 시 예외 처리

--- src/main/java/lotto/validator/InputMoneyValidator.java
+++ src/main/java/lotto/validator/InputMoneyValidator.java
@@ -3,12 +3,20 @@ package lotto.validator;
 public class InputMoneyValidator {
 
     private static final String ERROR = "[ERROR]";
-    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력해주세요";
+    private static final String NULL_ERROR_MESSAGE = "금액을 입력해주세요.";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
     private final String money;
 
     public InputMoneyValidator(String input){
         money = input;
+        isNull();
         isNumError();
+    }
+
+    public void isNull(){
+        if (money.equals("")){
+            throw new IllegalArgumentException(ERROR + NULL_ERROR_MESSAGE);
+        }
     }
 
     public void isNumError(){
@@ -18,4 +26,5 @@ public class InputMoneyValidator {
             throw new IllegalArgumentException(ERROR + NOT_NUMBER_ERROR_MESSAGE);
         }
     }
+
 }

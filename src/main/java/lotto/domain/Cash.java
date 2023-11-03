117417a76e89508f979ee2e159b556609cccfa52
jongheonleee <qwefghnm1212@gmail.com>
2023-11-03 18:53:01 +0900

    [feat] Car 클래스 필드 정의

--- src/main/java/lotto/domain/Cash.java
+++ src/main/java/lotto/domain/Cash.java
@@ -2,4 +2,13 @@ package lotto.domain;
 
 public class Cash {
 
+    private static final Integer unit = 1000;
+    private final Integer amount;
+
+    public Cash(final Integer amount) {
+        this.amount = amount;
+    }
+
+
+
 }

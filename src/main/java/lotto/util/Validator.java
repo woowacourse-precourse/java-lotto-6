780da72811d98d3169321a249269876e7239db71
shin5774 <babo5774@naver.com>
2023-11-03 20:02:32 +0900

    Feat: 로또 금액 1000원 나누어 떨어짐 검증기능 구현
    - 정수 인자값(로또 구매값)이 1000(원)으로 나누어 떨어지는지 검증하는 기능.
    - 예외처리 반환 문구와 로또 한개의 가격(1000원)은 상수로 관리함.

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -4,13 +4,26 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final Pattern NUMBER=Pattern.compile("[0-9]+");
+    private static final String ERROR_MESSAGE_HEADER="[ERROR] ";
     private static final String NOT_INTEGER_MESSAGE="숫자가 아닌 값이 입력되었습니다.";
+    private static final String NOT_DIVIDED_MESSAGE="구매값이 1000원으로 나눠지지 않습니다.";
+    private static final int BUY_UNIT=1000;
     public void numberValidate(String value){
         if(isNotConsistOfNumber(value)){
-            throw new IllegalArgumentException(NOT_INTEGER_MESSAGE);
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER+NOT_INTEGER_MESSAGE);
         }
     }
     private boolean isNotConsistOfNumber(String value){
         return !NUMBER.matcher(value).matches();
+    }
+
+    public void buyCashValidate(long buyCash){
+        if(isNotDividedByUnit(buyCash)){
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER+NOT_DIVIDED_MESSAGE);
+        }
+    }
+
+    private boolean isNotDividedByUnit(long buyCash){
+        return buyCash%BUY_UNIT!=0;
     }
 }

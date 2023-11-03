d9c92c2b64b6d26313c31c33ef6e419a7653eca7
devpanic <yeon.dev.8@gmail.com>
2023-11-03 18:48:14 +0900

    feat : Add userInputPurchaseAmount at DataInput.class

--- src/main/java/lotto/DataInput.java
+++ src/main/java/lotto/DataInput.java
@@ -1,4 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class DataInput {
+    public int userInputPurchaseAmount(){
+        String userPurchaseAmount;
+        userPurchaseAmount = Console.readLine();
+        // Validate
+        return Integer.parseInt(userPurchaseAmount);
+    }
 }

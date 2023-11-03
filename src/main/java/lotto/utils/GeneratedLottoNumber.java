6691ad38fb42ec935cd30fc608af88a812bbc469
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-03 13:51:33 +0900

    refactor: Intger를 사용했던 부분을 Lotto클래스를 활용하도록 변경

--- src/main/java/lotto/utils/GeneratedLottoNumber.java
+++ src/main/java/lotto/utils/GeneratedLottoNumber.java
@@ -1,14 +1,14 @@
 package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.List;
+import lotto.model.Lotto;
 
 public class GeneratedLottoNumber {
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 45;
     private static final int NUMBER_TO_GENERATE = 6;
 
-    public static List<Integer> getRandomNumber(){
-        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE,END_INCLUSIVE,NUMBER_TO_GENERATE);
+    public static Lotto getRandomNumber(){
+        return new Lotto(Randoms.pickUniqueNumbersInRange(START_INCLUSIVE,END_INCLUSIVE,NUMBER_TO_GENERATE));
     }
 }

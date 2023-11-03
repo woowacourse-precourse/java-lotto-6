950961848de0c3a774ecb138a8ab523712df8b9d
yeseong31 <yeseong31@naver.com>
2023-11-03 16:05:28 +0900

    rename(Purchase): Purchase로 이름 변경

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -2,20 +2,20 @@ package lotto.domain;
 
 import static lotto.view.constants.ConsoleMessage.PURCHASE_AMOUNT_ERROR;
 
-public class Price {
+public class Purchase {
 
     private static final int LOTTO_PRICE_UNIT = 1_000;
     private static final int ZERO = 0;
 
     private final Integer amount;
 
-    private Price(Integer amount) {
+    private Purchase(Integer amount) {
         validate(amount);
         this.amount = amount;
     }
 
-    public static Price of(Integer amount) {
-        return new Price(amount);
+    public static Purchase of(Integer amount) {
+        return new Purchase(amount);
     }
 
     public int receiveIssuedLottoCount() {
@@ -23,7 +23,7 @@ public class Price {
     }
 
     private void validate(Integer price) {
-        if (price <= ZERO || price % LOTTO_PRICE_UNIT != ZERO) {
+        if (price < LOTTO_PRICE_UNIT || price % LOTTO_PRICE_UNIT != ZERO) {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR.getMessage());
         }
     }
--- src/test/java/lotto/domain/PurchaseTest.java
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -10,46 +10,46 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class PriceTest {
+class PurchaseTest {
 
     @Nested
-    @DisplayName("[Price] 입력 테스트")
-    class PriceSuccessTest {
+    @DisplayName("[Purchase] 입력 테스트")
+    class PurchaseSuccessTest {
 
         @DisplayName("로또 구입 금액 입력이 정상적인 경우를 확인")
         @ParameterizedTest
         @ValueSource(ints = {1000, 2000, 6000})
         void create_로또_구입_금액_입력이_정상적인_경우를_확인(int amount) {
             assertThatNoException().isThrownBy(()
-                    -> Price.of(amount));
+                    -> Purchase.of(amount));
         }
     }
 
     @Nested
-    @DisplayName("[Price] 예외 테스트")
-    class PriceExceptionTest {
+    @DisplayName("[Purchase] 예외 테스트")
+    class PurchaseExceptionTest {
 
         @DisplayName("로또 구입 금액 입력이 올바르지 않은 경우를 확인")
         @ParameterizedTest
         @ValueSource(ints = {0, 1, 1001, 9999})
         void exception_로또_구입_금액_입력이_올바르지_않은_경우를_확인(int amount) {
-            assertThatThrownBy(() -> Price.of(amount))
+            assertThatThrownBy(() -> Purchase.of(amount))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("[ERROR]");
         }
     }
 
     @Nested
-    @DisplayName("[Price] 기능 테스트")
+    @DisplayName("[Purchase] 기능 테스트")
     class PriceUnitTest {
 
         @DisplayName("구입한 로또의 수 확인")
         @ParameterizedTest
         @CsvSource({"1000, 1", "2000, 2", "6000, 6"})
         void count_구입한_로또의_수_확인(int amount, int count) {
-            Price price = Price.of(amount);
+            Purchase purchase = Purchase.of(amount);
 
-            assertThat(price.receiveIssuedLottoCount())
+            assertThat(purchase.receiveIssuedLottoCount())
                     .isEqualTo(count);
         }
     }

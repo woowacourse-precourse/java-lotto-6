2405be327398f316991b1cc51ac65078d4707a4e
yesjjin99 <yesjjin99@naver.com>
2023-11-04 02:53:24 +0900

    feat: 로또 번호 범위에 대한 예외 처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,11 +15,12 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicated(numbers);
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_SIZE);
         }
     }
 
@@ -27,6 +28,16 @@ public class Lotto {
         long count = numbers.stream().distinct().count();
         if (count != LottoConstant.LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NUMBER);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(this::validateRange);
+    }
+
+    private void validateRange(int n) {
+        if (n < LottoConstant.LOTTO_NUMBER_MIN || n > LottoConstant.LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_RANGE);
         }
     }
 }
--- src/main/java/lotto/utils/message/ErrorMessage.java
+++ src/main/java/lotto/utils/message/ErrorMessage.java
@@ -5,5 +5,7 @@ public class ErrorMessage {
     public static final String INVALID_NUMBER = ERROR_MESSAGE + "숫자를 입력해야 합니다.";
     public static final String INDIVISIBLE_AMOUNT = ERROR_MESSAGE + "구입 금액은 1,000원 단위로 입력해야 합니다.";
     public static final String INVALID_AMOUNT_RANGE = ERROR_MESSAGE + "최소 1,000원 이상의 구입 금액을 입력해야 합니다.";
+    public static final String INVALID_NUMBER_SIZE = ERROR_MESSAGE + "로또 번호의 개수가 6개가 아닙니다.";
     public static final String DUPLICATED_NUMBER = ERROR_MESSAGE + "로또 번호에 중복된 숫자가 존재합니다.";
+    public static final String INVALID_NUMBER_RANGE = ERROR_MESSAGE + "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 }

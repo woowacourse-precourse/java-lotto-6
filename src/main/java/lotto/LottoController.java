8cc63ec6eeae0b6583b39680a4fa794898e06885
HwangSeokJin <ghkd3703@gmail.com>
2023-11-03 23:16:56 +0900

    feat: 구입 금액을 입력받고, 로또 발행기에서 로또를 발행한다

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,2 +1,29 @@
-package lotto;public class LottoController {
+package lotto;
+
+import java.util.List;
+
+public class LottoController {
+    InputView inputView;
+    OutputView outputView;
+    LottoMachine lottoMachine;
+
+    LottoController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        lottoMachine = new LottoMachine();
+    }
+
+    public void start() {
+        List<Lotto> lottos = buyLotto();
+    }
+
+    private List<Lotto> buyLotto() {
+        try {
+            int money = inputView.inputPurchaseAmount();
+            return lottoMachine.buy(money);
+        } catch (IllegalArgumentException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            return buyLotto();
+        }
+    }
 }

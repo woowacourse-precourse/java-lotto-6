24e98cd464a2700dd003f31054ca30f88e1958b8
Potato-Y <projectpotatoy@gmail.com>
2023-11-03 15:55:59 +0900

    feat: 로또 객체가 규정에 준수하는지 검증 추가
    - 로또 번호의 길이 확인
    - 로또 번호 중복 확인
    - 로또 숫자 범위에 만족하는지 확인

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,62 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.exception.LottoNumberDuplicateException;
+import lotto.exception.LottoNumberSizeException;
+import lotto.exception.LottoNumericRangeException;
 
 public class Lotto {
+
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateNumericRange(numbers);
+
         this.numbers = numbers;
     }
 
+    /**
+     * 로또 번호 길이에 대한 검증
+     *
+     * @param numbers
+     */
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new LottoNumberSizeException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    /**
+     * 중복된 숫자가 있는지 검사한다.
+     *
+     * @param numbers 검사할 Integer List
+     */
+    private void validateDuplicate(List<Integer> numbers) {
+        // 중복을 허용하지 않는 Set 자료구조를 사용한다.
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if (numbersSet.size() != numbers.size()) { // 크기를 비교한다.
+            throw new LottoNumberDuplicateException();
+        }
+    }
+
+    /**
+     * 1 ~ 45까지의 숫자를 가지는지 검증한다.
+     *
+     * @param numbers
+     */
+    private void validateNumericRange(List<Integer> numbers) {
+        for (int num :
+                numbers) {
+            if (num < MINIMUM_NUMBER || num > MAXIMUM_NUMBER) {
+                throw new LottoNumericRangeException();
+            }
+        }
+    }
 }

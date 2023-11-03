19e528590520ff0da9846fe3ae02c1bfb303b969
8x15yz <8x15yz@gmail.com>
2023-11-03 17:49:48 +0900

    feat : 금액 입력받기 구현 및 테스트코드 작성완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,12 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try { // 예외처리할 때 에러메세지 내고 "프로그램 종료"
+            InputSystem input = new InputSystem();
+            input.purchase();
+        } catch (Exception e) {
+            System.out.println("[ERROR]");
+        }
     }
+
 }

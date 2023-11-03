37f56102f47c3618cdc28f0640965bfd5db2d6cf
shin5774 <babo5774@naver.com>
2023-11-03 22:09:30 +0900

    Test: 구매액에 따른 로또구매 개수 반환기능 테스트코드
    - 로또 구매액을 통해서 로또의 구매개수를 반환해주는 기능.

---
+++ src/test/java/lotto/LottoGameTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottoGameTest {
+    @DisplayName("로또 구매금액에 따른 구매개수를 반환해주는 기능.")
+    @ParameterizedTest
+    @ValueSource(longs = {1000,5000,0,100000,18000})
+    void 로또_구매_개수(long buyCash){
+        LottoGame lottoGame=new LottoGame();
+        long expected=buyCash/Constant.BUY_UNIT;
+
+        assertThat(lottoGame.getLottoAmount(buyCash)).isEqualTo(expected);
+    }
+}

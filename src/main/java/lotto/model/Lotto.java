73644438a8d8620a6b8f4ef2063a83b575749550
YongHyeon <pyh4616@naver.com>
2023-11-03 21:02:40 +0900

    Refactor(Lotto, WinningNumbers 클래스): 매직 리터럴인 에러 메시지를 상수로 선언

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -9,6 +9,9 @@ public class Lotto {
     private static final int LOTTO_NUMBERS_SIZE = 6;
     private static final int LOTTO_NUMBERS_RANGE_START = 1;
     private static final int LOTTO_NUMBERS_RANGE_END = 45;
+    private static final String LOTTO_NUMBERS_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String LOTTO_NUMBERS_DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되는 값이 존재하지 않아야 합니다.";
+    private static final String LOTTO_NUMBERS_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -21,7 +24,7 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBERS_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(LOTTO_NUMBERS_SIZE_ERROR_MESSAGE);
         }
     }
 
@@ -29,7 +32,7 @@ public class Lotto {
     private void validateDuplicate(List<Integer> numbers) {
         Set duplicateCheck = new HashSet(numbers);
         if (duplicateCheck.size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되는 값이 존재하지 않아야 합니다.");
+            throw new IllegalArgumentException(LOTTO_NUMBERS_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
@@ -41,7 +44,7 @@ public class Lotto {
 
     private void validateSingleNumberRange(int number) {
         if (number > LOTTO_NUMBERS_RANGE_END || number < LOTTO_NUMBERS_RANGE_START) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
+            throw new IllegalArgumentException(LOTTO_NUMBERS_RANGE_ERROR_MESSAGE);
         }
     }
 }
--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -9,6 +9,9 @@ public class WinningNumbers {
     private static final int WINNING_NUMBERS_SIZE = 6;
     private static final int WINNING_NUMBERS_RANGE_END = 45;
     private static final int WINNING_NUMBERS_RANGE_START = 1;
+    private static final String WINNING_NUMBERS_SIZE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 6개여야 합니다.";
+    private static final String WINNING_NUMBERS_DUPLICATE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 중복되는 값이 존재하지 않아야 합니다.";
+    private static final String WINNING_NUMBERS_RANGE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 1부터 45까지의 숫자여야 합니다.";
 
     private final List<Integer> winningNumbers;
 
@@ -21,14 +24,14 @@ public class WinningNumbers {
 
     private void validateSize(List<Integer> winningNumbers) {
         if (winningNumbers.size() != WINNING_NUMBERS_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(WINNING_NUMBERS_SIZE_ERROR_MESSAGE);
         }
     }
 
     private void validateDuplicate(List<Integer> winningNumbers) {
         Set duplicateCheck = new HashSet(winningNumbers);
         if (duplicateCheck.size() != winningNumbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되는 값이 존재하지 않아야 합니다.");
+            throw new IllegalArgumentException(WINNING_NUMBERS_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
@@ -40,7 +43,7 @@ public class WinningNumbers {
 
     private void validateSingleNumberRange(int winningNumber) {
         if (winningNumber > WINNING_NUMBERS_RANGE_END || winningNumber < WINNING_NUMBERS_RANGE_START) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45까지의 숫자여야 합니다.");
+            throw new IllegalArgumentException(WINNING_NUMBERS_RANGE_ERROR_MESSAGE);
         }
     }
 }

7d17b42f55702cbb5e7591971152038c6afbb268
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 00:49:16 +0900

    Feat: "구매 개수 클래스 application에 추가"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,27 @@
 package lotto;
 
+import lotto.model.Purchase;
 import lotto.view.InputView;
 
 import java.util.*;
 
 public class Application {
 
-    public static void main(String[] args) {
+    private static String purchasingAmount;
 
+    private static final InputView inputView = new InputView();
+
+    public static void purchaseCount() {
+        purchasingAmount =inputView.purchasingAmount();
+        try {
+            Purchase purchase = new Purchase(purchasingAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            purchasingAmount =inputView.purchasingAmount();
+        }
+    }
+
+    public static void main(String[] args) {
+        purchaseCount();
     }
 }

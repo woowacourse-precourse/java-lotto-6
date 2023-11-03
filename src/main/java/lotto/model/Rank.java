d13e721896bbc271d3e4d3a2ed7bbb1a898aadd7
koomchang <leahpar0401@gmail.com>
2023-11-03 16:10:20 +0900

    feat: 로또 랭킹을 Rank 라는 enum 으로 구현

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int matchCount;
+    private final int reward;
+
+    Rank(int matchCount, int reward) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+}

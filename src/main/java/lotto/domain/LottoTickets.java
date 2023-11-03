615e6c3c9058048fa5b136cf84618968a9c1984c
Isaac Han <isaachan233@gmail.com>
2023-11-03 22:23:13 +0900

    feat: 양수가 아닌 값으로 생성시 예외 발생 추가

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.enums.ErrorMessages;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,10 +10,16 @@ public class LottoTickets {
     private final List<Lotto> lottoTickets;
 
     public LottoTickets(int numberOfTickets) {
+        validateNumberOfTickets(numberOfTickets);
         lottoTickets = IntStream.range(0, numberOfTickets)
                 // TODO: 랜던 유니크 생성 적용 하기
                 .mapToObj(i -> new Lotto(List.of(1, 2, 3, 4, 5, 6)))
                 .collect(Collectors.toList());
     }
 
+    private void validateNumberOfTickets(int numberOfTickets) {
+        if(numberOfTickets <= 0) {
+            throw new IllegalArgumentException(ErrorMessages.NON_POSITIVE_INPUT_MESSAGE.getMessage());
+        }
+    }
 }

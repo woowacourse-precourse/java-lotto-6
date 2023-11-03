3fa0712f87ee7993413bf1e7f75b4141ffe33ba5
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-03 18:48:12 +0900

    feat(InputView) : add inputLottoWinningNumbers() method

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningNumber;
 
 public class InputView {
 
@@ -9,5 +10,11 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
         return new PurchaseAmount(Integer.parseInt(purchaseAmount));
+    }
+
+    public WinningNumber inputLottoWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbers = Console.readLine();
+        return new WinningNumber(Integer.parseInt(winningNumbers));
     }
 }

3829a5f57b939264ee65f9bc37d3bf28369d6110
doha <dohaahi@naver.com>
2023-11-03 11:50:45 +0900

    feat(InputView): 보너스 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public int inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
@@ -23,5 +24,12 @@ public class InputView {
         return Arrays.stream(input.split(","))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    public int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        String input = readLine();
+
+        return Integer.parseInt(input);
     }
 }

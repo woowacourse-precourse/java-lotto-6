1dbd5da56c5b4f44b05af0ec0b5dde759f0ea185
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-04 02:14:00 +0900

    refactor: record 클래스로 대체
    LottoDto.Result 객체를 record 선언으로 수정

--- src/main/java/lotto/dto/LottoDto.java
+++ src/main/java/lotto/dto/LottoDto.java
@@ -1,13 +1,17 @@
 package lotto.dto;
 
 import lotto.Lotto;
+import lotto.LottoRanking;
+import lotto.LottoResult;
 import lotto.Lottos;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoDto {
 
     public static class Information {
+
         private final List<Lotto> lottos;
 
         private Information(List<Lotto> lottos) {
@@ -20,6 +24,18 @@ public class LottoDto {
 
         public List<Lotto> getLottos() {
             return List.copyOf(lottos);
+        }
+    }
+
+    public record Result(Map<LottoRanking, Integer> result) {
+
+        public static Result from(LottoResult lottoResult) {
+            return new Result(lottoResult.getResult());
+        }
+
+        @Override
+        public Map<LottoRanking, Integer> result() {
+            return Map.copyOf(result);
         }
     }
 }

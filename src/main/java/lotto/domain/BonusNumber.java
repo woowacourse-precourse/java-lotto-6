03cf3c52d7d4df0edbd1114261c153b976c5472f
nahyeon99 <nahyeonee99@gmail.com>
2023-11-03 17:33:37 +0900

    feat(domain/BonusNumber): 보너스 번호 클래스 추가

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class BonusNumber {
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private int number;
+
+    public BonusNumber(int number) {
+        validateRange(number);
+        this.number = number;
+    }
+
+    private void validateRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+        }
+    }
+}

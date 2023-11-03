a4e87fa573dc28c417c03b7ce8a307e5acbd0ade
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-11-03 16:40:19 +0900

    test: 구매금액 1000원미만 입력시 예외처리 테스트 추가
    1000원미만 입력시
    IllegalArgumentException을 발생시키고,
    "[ERROR] 1000 이상의 수를 입력하세요."라는 문구를 출력한다

--- src/test/java/lotto/LottoSellerTest.java
+++ src/test/java/lotto/LottoSellerTest.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mockStatic;
@@ -29,6 +28,16 @@ public class LottoSellerTest {
                 .hasMessageContaining(ERROR_MESSAGE_HEADER + " 숫자만 입력해주세요.");
     }
 
+    @DisplayName("입력한 구매 금액이 1000원 밑이면 예외가 발생한다.")
+    @Test
+    void lessThanLottoPrice() {
+        String userInput = "999";
+        LottoSeller lottoSeller = new LottoSeller();
+        assertThatThrownBy(() -> lottoSeller.getAmount(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE_HEADER + " " + LOTTO_PRICE + " 이상의 수를 입력하세요.");
+    }
+
     @DisplayName("입력한 구매 금액이 1000으로 나누어지지 않으면 예외가 발생한다.")
     @Test
     void notDividedByLottoPrice() {
@@ -39,34 +48,21 @@ public class LottoSellerTest {
                 .hasMessageContaining(ERROR_MESSAGE_HEADER + " " + LOTTO_PRICE + " 단위로만 입력하세요.");
     }
 
-    @DisplayName("자동 생성한 로또에는 잘못된 번호가 없다.")
-    @Test
-    void checkValidAutoLottos() {
-        LottoSeller lottoSeller = new LottoSeller();
-        String validUserInput = "100000";
-        int validAmount = lottoSeller.getAmount(validUserInput);
-        assertThatCode(() -> lottoSeller.makeLottoPaper(validAmount)).doesNotThrowAnyException();
-    }
-
     @DisplayName("구매 금액에 맞는 갯수의 자동 로또(로또 규칙에 맞는 번호를 가짐)를 생성한다.")
     @Test
     void giveLottosValidCountToUser() {
-        String validUserInput = "2000";
-        int validAmount = Integer.parseInt(validUserInput);
+        String validUserInput = "1000";
+        int validAmount;
         LottoSeller lottoSeller = new LottoSeller();
-        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        Lotto lotto2 = new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12));
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
         try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
             mock.when(() -> Randoms.pickUniqueNumbersInRange(anyInt(), anyInt(), anyInt()))
-                    .thenReturn(
-                            List.of(1, 2, 3, 4, 5, 6),
-                            List.of(7, 8, 9, 10, 11, 12)
-                    );
-            lottoSeller.getAmount(validUserInput);
+                    .thenReturn(List.of(1, 2, 3, 4, 5, 6));
+            validAmount = lottoSeller.getAmount(validUserInput);
             List<Lotto> lottos = lottoSeller.giveLotto();
             assertThat(lottos.size()).isEqualTo(validAmount / LOTTO_PRICE);
-            assertThat(lottos.get(0).showNumbers()).isEqualTo(lotto1.showNumbers());
-            assertThat(lottos.get(1).showNumbers()).isEqualTo(lotto2.showNumbers());
+            assertThat(lottos.get(0).showNumbers()).isEqualTo(lotto.showNumbers());
         }
     }
 }

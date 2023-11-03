a16a863ef8df62410773ddb0ae45e4a3385c85f2
gyungchan Jo <namejgc@naver.com>
2023-11-04 03:34:21 +0900

    test(LottoTest): 다른 로또와 일치하는 번호의 개수를 세는 기능 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 class LottoTest {
     @Test
@@ -40,17 +42,36 @@ class LottoTest {
 
     @DisplayName("로또 번호의 숫자들 중 1보다 작거나 45보다 큰 숫자가 있다면 예외가 발생한다.")
     @ParameterizedTest
-    @MethodSource("numbers")
+    @MethodSource("invalidNumbers")
     void createLottoByInvalidNumber(List<Integer> numbers) {
         assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
-    static Stream<List<Integer>> numbers() {
+    static Stream<List<Integer>> invalidNumbers() {
         return Stream.of(
                 List.of(0, 1, 2, 3, 4, 5),
                 List.of(1, 2, 3, 4, 5, 46)
+        );
+    }
+
+    @DisplayName("사용자의 로또와 비교하여 일치하는 번호가 총 몇개인지 알 수 있다.")
+    @ParameterizedTest
+    @MethodSource("userLottoAndNumberOfMatches")
+    void countNumberOfMatches(Lotto userLotto, int expected) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int actual = lotto.countNumberOfMatches(userLotto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> userLottoAndNumberOfMatches() {
+        return Stream.of(
+                arguments(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 6),
+                arguments(new Lotto(List.of(1, 2, 3, 4, 5, 16)), 5),
+                arguments(new Lotto(List.of(1, 2, 3, 4, 15, 16)), 4),
+                arguments(new Lotto(List.of(1, 2, 3, 14, 15, 16)), 3)
         );
     }
 }

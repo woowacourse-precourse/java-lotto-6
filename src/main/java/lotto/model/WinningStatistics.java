18855ce3b3b5de5cbb8ce6835535fe343b5c21a2
Jiyoung Choi <12201817@inha.edu>
2023-11-03 17:07:55 +0900

    feat(WinningStatistics) : 일치 여부를 계산해 당첨 결과 초기화 기능 구현

--- src/main/java/lotto/model/WinningStatistics.java
+++ src/main/java/lotto/model/WinningStatistics.java
@@ -1,2 +1,55 @@
-package lotto.model;public class WinningStatistics {
+package lotto.model;
+
+import lotto.constant.BonusStatus;
+import lotto.constant.Constant;
+
+import java.util.*;
+
+public class WinningStatistics {
+    private static final int SPECIAL_MATCH_COUNT = Constant.SPECIAL_MATCH_COUNT.getValue();
+    private long totalReward = 0;
+    private double rateOfReturn = 0.0;
+    private final Map<WinningPrize, Integer> winningResult = new HashMap<>() {{
+        put(WinningPrize.FIRST_PRIZE, 0);
+        put(WinningPrize.SECOND_PRIZE, 0);
+        put(WinningPrize.THIRD_PRIZE, 0);
+        put(WinningPrize.FOURTH_PRIZE, 0);
+        put(WinningPrize.FIFTH_PRIZE, 0);
+    }};
+
+    WinningStatistics(List<Lotto> purchaseLottos, List<Integer> winningLotto, int bonusNumber) {
+        setWinningResult(purchaseLottos, winningLotto, bonusNumber);
+        setRateOfReturn();
+    }
+
+    private void setWinningResult(List<Lotto> purchaseLottos, List<Integer> winningLotto, int bonusNumber) {
+        for (Lotto purchaseLotto : purchaseLottos) {
+            List<Integer> lotto = purchaseLotto.getNumbers();
+            int matchCount = calculateMatchCount(lotto, winningLotto);
+            BonusStatus bonusStatus = getBonusStatus(matchCount, lotto, bonusNumber);
+            Optional<WinningPrize> winningPrize = WinningPrize.getWinningPrize(matchCount, bonusStatus);
+            if (winningPrize.isPresent()) {
+                winningResult.put(winningPrize.get(), winningResult.get(winningPrize) + 1);
+                totalReward += winningPrize.get().getReward();
+            }
+        }
+    }
+
+    private int calculateMatchCount(List<Integer> purchaseLotto, List<Integer> winningLotto) {
+        return (int) purchaseLotto.stream().filter(winningLotto::contains).count();
+    }
+
+    private BonusStatus getBonusStatus(int matchCount, List<Integer> lotto, int bonusNumber) {
+        if (matchCount != SPECIAL_MATCH_COUNT) {
+            return BonusStatus.NO_MATTER;
+        }
+        if (lotto.contains(bonusNumber)) {
+            return BonusStatus.IS_IN_LOTTO;
+        }
+        return BonusStatus.IS_NOT_IN_LOTTO;
+    }
+
+    private void setRateOfReturn() {
+
+    }
 }

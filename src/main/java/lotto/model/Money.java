767d89752be9dc5da6b00b9a1d61fa8ac33fd03f
jmj005 <jmj00005@naver.com>
2023-11-04 00:13:41 +0900

    feat: 로또 구입 금액 0원 검증 구현

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -2,17 +2,25 @@ package lotto.model;
 
 public class Money {
     private static final int THOUSAND = 1_000;
+    private static final int ZERO = 0;
 
     private final int money;
 
     public Money(int money) {
+        validateZeroMoney(money);
         validateDivideMoney(money);
         this.money = money;
     }
 
+    private void validateZeroMoney(int money) {
+        if (money == ZERO) {
+            throw new IllegalArgumentException("0원은 입력할 수 없습니다.");
+        }
+    }
+
     private void validateDivideMoney(int money) {
-        if (money % THOUSAND != 0) {
-            throw new IllegalArgumentException("1,000 단위만 입력할 수 있습니다.");
+        if (money % THOUSAND != ZERO) {
+            throw new IllegalArgumentException("1,000원 단위만 입력할 수 있습니다.");
         }
     }
 }

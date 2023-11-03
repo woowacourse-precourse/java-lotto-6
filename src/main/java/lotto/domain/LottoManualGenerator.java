52fd8bccea05a63d6d2bd5cb7ba4165ba8fc11c1
minseo  jo <liging12@naver.com>
2023-11-04 01:30:28 +0900

    feat: 로또 당첨 번호 뽑는(수동) 기능 추가 (LottoManualGenerator)
    로또 번호는 원래 정부? 국가? 에서 자동으로 하나씩 뽑지만, 이 프로그램에서는 사용자가 수동으로 입력 한다.

--- src/main/java/lotto/domain/LottoManualGenerator.java
+++ src/main/java/lotto/domain/LottoManualGenerator.java
@@ -1,15 +1,22 @@
 package lotto.domain;
 
-import lotto.exception.LottoNumberException;
+import lotto.exception.LottoException;
+import lotto.vo.LottoNumber;
 
-public class LottoManualGenerator implements LottoGenerator{
-    @Override
-    public Lotto generate() throws LottoNumberException {
-        return null;
-    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-    @Override
-    public Lottos generate(int count) throws LottoNumberException {
-        return null;
+public class LottoManualGenerator {
+    public Lotto generate(List<String> list) throws LottoException {
+        try {
+            Set<LottoNumber> uniqueNumbers = list.stream()
+                    .map(number -> new LottoNumber(Integer.valueOf(number)))
+                    .collect(Collectors.toSet());
+            return new Lotto(new ArrayList<>(uniqueNumbers));
+        } catch (NumberFormatException exception) {
+            throw new LottoException("숫자를 입력해 주세요");
+        }
     }
 }

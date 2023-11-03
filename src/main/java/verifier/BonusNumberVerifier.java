0f37c16121316bf8f894ab5169964976872ec3bc
Hoo-sung <gntjd135@gmail.com>
2023-11-03 16:35:41 +0900

    feat(BonusNumberVerifier): 보너스 번호가 숫자인지 검증하는 메서드 생성

--- src/main/java/verifier/BonusNumberVerifier.java
+++ src/main/java/verifier/BonusNumberVerifier.java
@@ -3,6 +3,14 @@ package verifier;
 public class BonusNumberVerifier implements Verifier{
     @Override
     public void check(String input) {
+        checkNumeric(input);
+    }
 
+    private void checkNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("보너스 번호가 숫자가 아닙니다.");
+        }
     }
 }

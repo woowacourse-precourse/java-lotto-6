b724e3d0f0c13aa054997179de1c24fe55c22d6a
lpromotion <lpromotion00@gmail.com>
2023-11-03 20:04:50 +0900

    1. 로또 구입 금액 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int purchase = 0; // 구입 금액
+        try{
+            // 1-1. 로또 금액 입력 받음
+            System.out.println("구입금액을 입력해 주세요.");
+            purchase = Integer.parseInt(Console.readLine());
+            if(purchase % 1000 != 0){
+                throw new IllegalArgumentException("구매 금액은 1000원 단위여야 합니다.");
+            }
+        } catch (IllegalArgumentException e){
+            // 1-2. 금액이 1000원 단위가 아니면 예외 발생
+            System.out.println("[ERROR] "+e.getMessage());
+        }
+
     }
 }

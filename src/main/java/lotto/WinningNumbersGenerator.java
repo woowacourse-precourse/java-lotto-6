6b80031e649559f8aec48095c2da52903f8c81ea
watermelon3737 <pshrina1@naver.com>
2023-11-03 22:05:36 +0900

    feat(WinningNumbersGenerator) : 구분자로 나눈 값들에 대한 유효성 검사, 숫자 인지, 1 ~ 45 범위 안의 숫자인지 검증하는 메서드 구현 및 getter 생성'

--- src/main/java/lotto/WinningNumbersGenerator.java
+++ src/main/java/lotto/WinningNumbersGenerator.java
@@ -3,9 +3,10 @@ package lotto;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
-public class WinningNumberGenerator {
+public class WinningNumbersGenerator {
     private final int START_NUM = 1;
     private final int END_NUM = 45;
     private final int NUM_COUNT = 6;
@@ -17,6 +18,9 @@ public class WinningNumberGenerator {
 
     public void makeWinningNumbers(final String winningNumbersInput) {
         this.dividedInputByDelimiter(winningNumbersInput);
+        this.validateInputDividedByDelimiter();
+        this.validateDividedInput();
+
     }
 
     public void dividedInputByDelimiter(final String winningNumbersInput) {
@@ -33,5 +37,21 @@ public class WinningNumberGenerator {
         if (noDuplicatedInput.size() != NUM_COUNT) {
             throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
         }
+    }
+
+    public void validateDividedInput() {
+        for (String winningNumberInput : this.inputDividedByDelimiter) {
+            if (winningNumberInput.chars().anyMatch(c -> !Character.isDigit((char) c))) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.");
+            }
+            int winningNumber = Integer.parseInt(winningNumberInput);
+            if (winningNumber > END_NUM || winningNumber < START_NUM) {
+                throw new IllegalArgumentException("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    public String[] getInputDividedByDelimiter() {
+        return inputDividedByDelimiter;
     }
 }

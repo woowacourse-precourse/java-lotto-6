65f8e60d1a8e079fdf46e493b1cb54911a667372
watermelon3737 <pshrina1@naver.com>
2023-11-03 20:01:33 +0900

    feat (LottoStore) : 고객에게 투입된 금액 만큼의 로또 발행에 관련된 로직 구현

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class LottoStore {
     private final int LOTTO_PRICE = 1000;
@@ -10,7 +14,7 @@ public class LottoStore {
     private final int MIN_PRICE = 1000;
 
     private long chargedMoney;
-    private long lottoCount;
+    private long lottoAmount;
     private List<Lotto> lottoPapers;
 
     public void getMoney(final String moneyInput) throws IllegalArgumentException{
@@ -39,5 +43,43 @@ public class LottoStore {
         if (this.chargedMoney % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위 입니다.");
         }
+    }
+
+    public void sellLottos(final Customer customer) {
+        this.calculateLottoAmount();
+        generateAllLottos();
+        customer.getLotto(this.lottoPapers);
+    }
+
+    public void calculateLottoAmount() {
+        this.lottoAmount = this.chargedMoney / LOTTO_PRICE;
+    }
+
+    public void generateAllLottos() {
+        this.lottoPapers = new ArrayList<>();
+
+        LongStream.range(0, this.lottoAmount)
+                .forEach(i -> lottoPapers.add(generateLottoByNumbers(generateLottoNumbers())));
+    }
+
+    public Lotto generateLottoByNumbers(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public List<Integer> generateLottoNumbers() {
+        final List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, NUM_COUNT)
+                .stream()
+                .sorted()
+                .toList();
+
+        return numbers;
+    }
+
+    public long getLottoAmount() {
+        return this.lottoAmount;
+    }
+
+    public long getChargedMoney() {
+        return this.chargedMoney;
     }
 }

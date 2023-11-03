5039d33c9df5ee8b59b4b91258aa073dd22a63cb
yeseong31 <yeseong31@naver.com>
2023-11-03 22:09:59 +0900

    feat(Lotto): 로또 번호 중복 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import static java.util.function.Predicate.isEqual;
 import static java.util.stream.Collectors.toList;
+import static lotto.domain.WinningLotto.LOTTO_NUMBERS_SIZE;
+import static lotto.view.constants.ConsoleMessage.DUPLICATE_NUMBER_ERROR;
 import static lotto.view.constants.SeparatorConstant.LOTTO_NUMBERS_SEPARATOR;
 
 import java.util.List;
@@ -22,7 +24,24 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR.getMessage());
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != LOTTO_NUMBERS_SIZE;
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
     }
@@ -53,10 +72,10 @@ public class Lotto {
     private String appendLottoNumbers() {
         StringBuilder sb = new StringBuilder();
 
-        for (int index = 0; index < numbers.size(); index++) {
+        for (int index = 0; index < LOTTO_NUMBERS_SIZE; index++) {
             sb.append(numbers.get(index));
 
-            if (index < numbers.size() - 1) {
+            if (index < LOTTO_NUMBERS_SIZE - 1) {
                 sb.append(LOTTO_NUMBERS_SEPARATOR);
             }
         }

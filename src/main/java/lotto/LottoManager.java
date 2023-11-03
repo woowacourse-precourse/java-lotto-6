92b44313f328b70cf1e83a4d50fd6f429af6b6b0
AmenPark <puzzlistpam@gmail.com>
2023-11-03 18:22:01 +0900

    feat:로또 구입

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,2 +1,28 @@
-package lotto;public class LottoManager {
+package lotto;
+
+
+import java.util.List;
+
+public class LottoManager {
+    private List<Lotto> lottos;
+
+    public LottoManager() {
+
+    }
+
+    public void run() {
+        ConsoleView.print("구입금액을 입력해 주세요.");
+        getMoney();
+
+    }
+
+    public void getMoney() {
+        long money = ConsoleView.getLongInput();
+        try {
+            lottos = LottoPublisher.getInstance().sell(money);
+        } catch (IllegalArgumentException e) {
+            getMoney();
+        }
+    }
+
 }

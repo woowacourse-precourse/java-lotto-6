aca1b6edf3b51487db42e376f0611473a5d55cc1
YongHyeon <pyh4616@naver.com>
2023-11-03 14:44:13 +0900

    Refactor(Lotto 클래스): 로또 번호 크기에 대한 유효성 검사 기능인 validate 메서드를 validateSize로 변경하고 에러 메시지 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
 
@@ -6,13 +6,13 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 

3cd19d97dc3d71f73d54e07df4193f245d8ab3be
Juhyorim <joohr1234@gmail.com>
2023-11-04 05:04:36 +0900

    feat: 로또 구입금액이 1000원 이상이 아니면 예외발생 기능 구현

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,13 +1,28 @@
 package lotto.util;
 
 public class InputValidator {
-    private static final String NO_A_NUMBER_MESSAGE = "로또 금액은 숫자여야 합니다";
+    private static final String NOT_A_NUMBER_MESSAGE = "로또 금액은 숫자여야 합니다";
+    private static final String MIN_PRICE_1000_MESSAGE = "로또 금액은 1000원 이상이어야 합니다";
     private static final String NUMBER_REGEX = "[0-9]+";
 
     public void validatePrice(String inputPrice) {
         if (isNumber(inputPrice) == false) {
-            throw new IllegalArgumentException(NO_A_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(NOT_A_NUMBER_MESSAGE);
         }
+
+        int price = Integer.parseInt(inputPrice);
+
+        if (isUnder1000(price)) {
+            throw new IllegalArgumentException(MIN_PRICE_1000_MESSAGE);
+        }
+    }
+
+    private boolean isUnder1000(int price) {
+        if (price < 1000) {
+            return true;
+        }
+
+        return false;
     }
 
     private boolean isNumber(String inputPrice) {

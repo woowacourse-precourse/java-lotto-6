3ba40869c6bbda536e6504d4568d040ab2decb63
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-03 15:53:09 +0900

    feat: now makePurchase() returns lottoQuantity

--- src/main/java/lotto/domain/purchase/Purchase.java
+++ src/main/java/lotto/domain/purchase/Purchase.java
@@ -3,22 +3,25 @@ package lotto.domain.purchase;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Purchase {
+
     private int paidPrice;
 
-    public void makePurchase() {
+    public int makePurchase() {
         System.out.println("구입금액을 입력해 주세요.");
-        purchaseLotto();
+        String purchasePrice = Console.readLine();
+        repeatPurchasing(purchasePrice);
         System.out.println();
         int lottoQuantity = paidPrice / 1000;
         System.out.println(lottoQuantity + "개를 구매했습니다.");
+
+        return lottoQuantity;
     }
 
-    public void purchaseLotto() {
+    public void repeatPurchasing(String purchasePrice) {
+
         while (true) {
-            try{
-                String purchasePrice = Console.readLine();
-                paidPrice = Integer.parseInt(purchasePrice);
-                validatePrice(paidPrice);
+            try {
+                validatePrice(purchasePrice);
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
@@ -26,7 +29,10 @@ public class Purchase {
         }
     }
 
-    public void validatePrice(int paidPrice) {
+    public void validatePrice(String purchasePrice) {
+
+        paidPrice = Integer.parseInt(purchasePrice);
+
         if (paidPrice % 1000 != 0) {
             throw new IllegalArgumentException();
         }

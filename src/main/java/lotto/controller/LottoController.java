5673258d4f7395cc53ebfb5a164acb7a33279956
sangdol <priceelees@gmail.com>
2023-11-03 11:48:22 +0900

    refactor(LottoController): 메서드 기능 분리 및 예외 발생시 반복하여 입력받도록 설정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.LottoMaker;
 import lotto.domain.LottoResult;
 import lotto.domain.UserLottos;
@@ -8,9 +10,12 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
+    private static final int ONE_LOTTO_PRICE = 1000;
+    private static final int DEFAULT_INT_VALUE = 0;
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final LottoMaker lottoMaker;
+
 
     public LottoController() {
         this(new LottoMaker());
@@ -20,30 +25,68 @@ public class LottoController {
         this.lottoMaker = lottoMaker;
     }
 
-    public void playGame() {
-        int purchaseAmount = receivePurchaseAmount();
-        UserLottos userLottos = new UserLottos(lottoMaker.createLottosByPrice(purchaseAmount));
-        outputView.printUserLottos(userLottos.getTotalLottos(), userLottos.displayAllLottos());
-
-        WinningLotto winningLotto = lottoMaker.createWinningLottoFromInput(
-                inputView.receiveWinningNumber(),
-                inputView.receiveBonusNumber()
-        );
+    public void play() {
+        UserLottos userLottos = receiveUserLottos();
+        WinningLotto winningLotto = receiveWinningLottoNumber();
 
         LottoResult lottoResult = userLottos.compareAllLottos(winningLotto);
 
-        outputView.printResults(lottoResult.getStatistics());
-        outputView.printEarningRate(purchaseAmount, lottoResult.getTotalRevenue());
+        int purchaseAmount = userLottos.getTotalLottos() * ONE_LOTTO_PRICE;
+        printLottoResultStatistics(lottoResult, purchaseAmount);
+    }
+
+    private UserLottos receiveUserLottos() {
+        int purchaseAmount = receivePurchaseAmount();
+        List<Lotto> userLottoNumbers = lottoMaker.createLottosByPrice(purchaseAmount);
+        UserLottos userLottos = new UserLottos(userLottoNumbers);
+
+        outputView.printUserLottos(userLottos.getTotalLottos(), userLottos.displayAllLottos());
+
+        return userLottos;
     }
 
     private int receivePurchaseAmount() {
-        int purchaseAmount = 0;
+        int purchaseAmount = DEFAULT_INT_VALUE;
         try {
             purchaseAmount = inputView.receivePurchaseAmount();
-        } catch(IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            outputView.printMessage(e.getMessage());
             receivePurchaseAmount();
         }
         return purchaseAmount;
+    }
+
+    private WinningLotto receiveWinningLottoNumber() {
+        String winningNumbers = receiveWinningNumber();
+        int bonusNumber = receiveBonusNumber();
+
+        return lottoMaker.createWinningLottoFromInput(winningNumbers, bonusNumber);
+    }
+
+    private String receiveWinningNumber() {
+        String winningNumber = null;
+        try {
+            winningNumber = inputView.receiveWinningNumber();
+        } catch (IllegalArgumentException e) {
+            outputView.printMessage(e.getMessage());
+            receiveWinningNumber();
+        }
+        return winningNumber;
+    }
+
+    private int receiveBonusNumber() {
+        int bonusNumber = DEFAULT_INT_VALUE;
+        try {
+            bonusNumber = inputView.receiveBonusNumber();
+        } catch (IllegalArgumentException e) {
+            outputView.printMessage(e.getMessage());
+            receiveBonusNumber();
+        }
+        return bonusNumber;
+    }
+
+    private void printLottoResultStatistics(LottoResult lottoResult, int purchaseAmount) {
+        outputView.printResults(lottoResult.getStatistics());
+        outputView.printEarningRate(purchaseAmount, lottoResult.getTotalRevenue());
     }
 }

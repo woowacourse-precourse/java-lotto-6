224c95b239439aeab1a93a1378854435123c00c1
Hoo-sung <gntjd135@gmail.com>
2023-11-03 16:38:51 +0900

    feat(BonusNumberVerifier): 보너스 번호가 1~45 사이의 수인지 검증하는 메서드 생성

--- src/main/java/verifier/BonusNumberVerifier.java
+++ src/main/java/verifier/BonusNumberVerifier.java
@@ -4,6 +4,8 @@ public class BonusNumberVerifier implements Verifier{
     @Override
     public void check(String input) {
         checkNumeric(input);
+        checkRange(input);
+
     }
 
     private void checkNumeric(String input) {
@@ -11,6 +13,13 @@ public class BonusNumberVerifier implements Verifier{
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("보너스 번호가 숫자가 아닙니다.");
+        }
+    }
+
+    private void checkRange(String input){
+        int BonusNumber = Integer.parseInt(input);
+        if(BonusNumber < 1 || BonusNumber >45 ){
+            throw new IllegalArgumentException("보너스 번호는 1-45 사이의 숫자이어야 합니다.");
         }
     }
 }

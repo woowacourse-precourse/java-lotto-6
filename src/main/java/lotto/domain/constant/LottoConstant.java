67a58f5146fd0fede6b48f183e7cdada7bc122e1
wwan13 <wwan13@naver.com>
2023-11-04 03:27:26 +0900

    feat : `LottoConstant` 상수 정의

---
+++ src/main/java/lotto/domain/constant/LottoConstant.java
@@ -0,0 +1,12 @@
+package lotto.domain.constant;
+
+public class LottoConstant {
+
+    public static final int LOTTO_MINIMUM_NUMBER = 1;
+    public static final int LOTTO_MAXIMUM_NUMBER = 45;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int LOTTO_PRICE = 1000;
+
+    private LottoConstant() {
+    }
+}

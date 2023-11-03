5776576d61a90291eabdff7935d597ad030609ea
brightfanta <brightfanta@gmail.com>
2023-11-03 15:20:08 +0900

    feat(View): 기본 인풋 입력, 기늘 흐름 뼈대 메시지 출력 메서드 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
+
+    public String getUserInput() {
+        return Console.readLine();
+    }
+
+    public void printMessage(Notice notice) {
+        System.out.println(notice.getMessage());
+    }
 }

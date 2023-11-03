38b15e182e76416ad4ef86255da661458ba84268
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-04 06:35:57 +0900

    refactor(ErrorMessage): String.format형태의 메세지로 수정

--- src/main/java/lotto/util/message/ErrorMessage.java
+++ src/main/java/lotto/util/message/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
 	LOTTO_BONUS_NUMBER_DUPLICATE_ERROR("당첨 번호와 보너스 번호는 중복되지 말아야 합니다."),
 	LOTTO_NUMBER_RANGE_ERROR("로또 번호는 %s부터 %s 사이의 숫자여야 합니다."),
 	MONEY_UNIT_ERROR("돈의 단위는 %s입니다."),
-	INPUT_ANSWER_LOTTO_NUMBER_ERROR(",로 구분하여 6개의 숫자를 입력해야합니다."),
+	INPUT_ANSWER_LOTTO_NUMBER_ERROR(",로 구분하여 %s개의 숫자를 입력해야합니다."),
 	INPUT_LETTER_ERROR("문자가 아닌 숫자만 입력해야 합니다."),
 	INPUT_EMPTY_ERROR("값을 입력해야 합니다."), 
 	PREFIX("[ERROR] ");

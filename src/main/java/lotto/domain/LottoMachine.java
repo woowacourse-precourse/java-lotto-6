51c4cda54b268d1f589032f1dd45056654ad681e
gumgu <bienlee@naver.com>
2023-11-04 00:07:52 +0900

    [Feat] LottoMachine: 입력받은 금액 만큼 로또 발행 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,13 +3,32 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
 
-    public Lotto issueLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return new Lotto(numbers);
+    private static final Integer LOTTO_PRICE = 1000;
+
+    public List<Lotto> buyLotto(Money money) {
+        List<Lotto> lottoBundle = new ArrayList<>();
+
+        Integer lottoCount = computeLottoCount(money);
+
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto lotto = issueLotto();
+            lottoBundle.add(lotto);
+        }
+
+        return lottoBundle;
+    }
+
+    private Integer computeLottoCount(Money money) {
+        return money.getAmount() / LOTTO_PRICE;
+    }
+
+    private Lotto issueLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 
 }

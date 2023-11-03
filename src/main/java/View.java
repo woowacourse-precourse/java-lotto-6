d611d036493c33da582b44f6bf7766c930542e9d
Genius-PAK <pyj789123@gmail.com>
2023-11-03 14:30:13 +0900

    chore(View): 출력 메시지 상수를 Message 클래스에서 받아오는 것으로 변경

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,26 +1,22 @@
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class View {
-    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요. ";
-    private static final String LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요. ";
-    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요. ";
-    private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계\n---";
 
 
     public void displayPurchaseAmountMessage() {
-        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        System.out.println(Message.PURCHASE_AMOUNT_MESSAGE);
     }
 
     public void displayLottoNumberMessage() {
-        System.out.println(LOTTO_NUMBER_MESSAGE);
+        System.out.println(Message.LOTTO_NUMBER_MESSAGE);
     }
 
     public void displayBonusNumberMessage() {
-        System.out.println(BONUS_NUMBER_MESSAGE);
+        System.out.println(Message.BONUS_NUMBER_MESSAGE);
     }
 
     public void displayWinningStatisticsMessage() {
-        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(Message.WINNING_STATISTICS_MESSAGE);
     }
 
     private String getUserInput() {

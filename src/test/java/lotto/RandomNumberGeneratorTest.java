414eccb298846fd6cde02dbef964684e35610629
zjacom <eephus625@icloud.com>
2023-11-04 00:44:18 +0900

    test(lotto): 랜덤으로 6개의 번호를 추출하는 라이브러리 테스트 작성

---
+++ src/test/java/lotto/RandomNumberGeneratorTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RandomNumberGeneratorTest {
+    RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    List<Integer> randomNumbers;
+
+    @BeforeEach
+    void setUp() {
+        randomNumbers = randomNumberGenerator.createNewRandomNumber();
+    }
+
+    @Test
+    void 생성된_로또의_길이_테스트() {
+        assertThat(randomNumbers).hasSize(6);
+    }
+}

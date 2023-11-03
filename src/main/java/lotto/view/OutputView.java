a173850cf4695415c7fa1e2be07c454a2e8f8eb2
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-03 20:23:22 +0900

    feat(OutputView) : add showBayLottoNumber() method

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,16 @@
 package lotto.view;
 
+import lotto.Lotto;
 import lotto.domain.PurchaseAmount;
 
 public class OutputView {
 
     public void showBayLottoCount(PurchaseAmount purchaseAmount){
         System.out.println(purchaseAmount.getAmount()+"개를 구매했습니다.");
+    }
+
+
+    public static void showBayLottoNumber(Lotto lotto) {
+        System.out.println(lotto.getNumbers().toString());
     }
 }

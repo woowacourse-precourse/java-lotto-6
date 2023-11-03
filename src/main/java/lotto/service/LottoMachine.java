db771159b0807511129c04a496cab1c623280d5c
sonjiseokk <thswltjr11@gmail.com>
2023-11-03 18:19:38 +0900

    feat: LottoMachine 객체 생성

---
+++ src/main/java/lotto/service/LottoMachine.java
@@ -0,0 +1,24 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    public List<Lotto> purchase(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        int quantity = money / 1000;
+        while (lottos.size() != quantity) {
+            lottos.add(new Lotto(makeRandomNumbers()));
+        }
+
+        return lottos;
+    }
+
+    private static List<Integer> makeRandomNumbers(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

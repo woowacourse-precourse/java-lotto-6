dc165c957318e32ca538cd3ec30ffa91d66ffad1
HiiWee <wpdlzhf159@gmail.com>
2023-11-04 01:51:31 +0900

    test: 로또 단위 테스트 추가

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -2,14 +2,9 @@ package lotto.domain.lotto;
 
 import java.util.Collections;
 import java.util.List;
+import lotto.validator.domain.LottoNumbersValidator;
 
 public class Lotto {
-
-    private static final String INVALID_LOTTO_LENGTH = "[ERROR] 로또 번호의 길이는 6개여야 합니다.";
-    private static final String DUPLICATES_LOTTO_NUMBERS = "[ERROR] 로또 번호는 중복을 허용하지 않습니다.";
-    private static final String OUT_OF_RANGE_NUMBER = "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.";
-
-    private static final int LOTTO_LENGTH = 6;
 
     private final List<Integer> numbers;
 
@@ -18,44 +13,8 @@ public class Lotto {
     }
 
     public static Lotto from(final List<Integer> numbers) {
-        validate(numbers);
+        LottoNumbersValidator.validate(numbers);
         return new Lotto(numbers);
-    }
-
-    private static void validate(List<Integer> numbers) {
-        validateLength(numbers);
-        validateDuplicates(numbers);
-        validateRange(numbers);
-    }
-
-    private static void validateRange(final List<Integer> numbers) {
-        if (isOutOfRange(numbers)) {
-            throw new IllegalArgumentException(OUT_OF_RANGE_NUMBER);
-        }
-    }
-
-    private static boolean isOutOfRange(final List<Integer> numbers) {
-        return numbers.stream()
-                .anyMatch(LottoCondition::isNotInRange);
-    }
-
-    private static void validateLength(final List<Integer> numbers) {
-        if (numbers.size() != LOTTO_LENGTH) {
-            throw new IllegalArgumentException(INVALID_LOTTO_LENGTH);
-        }
-    }
-
-    private static void validateDuplicates(final List<Integer> numbers) {
-        if (isDuplicate(numbers)) {
-            throw new IllegalArgumentException(DUPLICATES_LOTTO_NUMBERS);
-        }
-    }
-
-    private static boolean isDuplicate(final List<Integer> numbers) {
-        return numbers.stream()
-                .distinct()
-                .toList()
-                .size() != numbers.size();
     }
 
     public List<Integer> getNumbers() {
@@ -66,7 +25,7 @@ public class Lotto {
         return numbers.contains(number);
     }
 
-    public int calculateWinningCount(final Lotto compareLotto) {
+    public int extractSameCount(final Lotto compareLotto) {
         return (int) numbers.stream()
                 .filter(compareLotto::contains)
                 .count();
--- src/test/java/lotto/domain/lotto/LottoTest.java
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -11,6 +12,7 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
+        // given & when & then
         assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -18,9 +20,43 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
+        // given & when & then
         assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 ~ 45사이의 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void createLotto_exception_outOfRangeNumber() {
+        // given & when & then
+        assertThatThrownBy(() -> Lotto.from(List.of(0, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("특정 숫자가 로또에 존재함을 확인할 수 있다.")
+    @Test
+    void contains() {
+        // given
+        Lotto lotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        boolean result = lotto.contains(1);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("두 개 로또의 비교후 동일한 숫자의 개수를 찾을 수 있다.")
+    @Test
+    void extractSameCount() {
+        // given
+        Lotto firstLotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+        Lotto secondLotto = Lotto.from(List.of(1, 2, 3, 4, 5, 7));
+
+        // when
+        int sameCount = firstLotto.extractSameCount(secondLotto);
+
+        // then
+        assertThat(sameCount).isEqualTo(5);
+    }
 }

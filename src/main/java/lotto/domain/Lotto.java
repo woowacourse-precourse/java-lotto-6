e06e3ce62f90bdaf55a46b635bb3a4c45167f37a
doha <dohaahi@naver.com>
2023-11-03 23:56:17 +0900

    feat (Lotto): 숫자 중복 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,6 +9,8 @@ public class Lotto {
     public static final String INVALID_LOTTO_SIZE_MESSAGE = "당첨 번호는 숫자 " + LOTTO_SIZE + "개를 입력해야 합니다.";
     public static final String INVALID_LOTTO_NUMBER_RANGE_MESSAGE =
             "당첨 번호를 " + MIN_LOTTO_NUMBER + " ~ " + MAX_LOTTO_NUMBER + " 사이의 숫자로 입력해주세요.";
+    public static final String DUPLICATE_NUMBERS_MESSAGE = "당첨 번호를 중복되지 않은 숫자로 입력해주세요.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -19,6 +21,7 @@ public class Lotto {
     private void validate(final List<Integer> numbers) {
         validateNumberSize(numbers);
         validateNumbersInLottoRange(numbers);
+        validateDuplicateNumbers(numbers);
     }
 
     private void validateNumberSize(final List<Integer> numbers) {
@@ -32,6 +35,18 @@ public class Lotto {
 
         if (anyMatch) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    private void validateDuplicateNumbers(final List<Integer> numbers) {
+        for (Integer number : numbers) {
+            long count = numbers.stream()
+                    .filter(num -> num.equals(number))
+                    .count();
+
+            if (count > 1) {
+                throw new IllegalArgumentException(DUPLICATE_NUMBERS_MESSAGE);
+            }
         }
     }
 }

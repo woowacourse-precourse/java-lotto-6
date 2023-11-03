a2b80276521fb904908bdd4180fdc963b5fed6b5
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-03 12:36:54 +0900

    feat: 구매 금액에 맞는 로또 갯수에 따른 당첨 번호를 생성하는 메서드 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,17 +1,33 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lotto.model.LottoCount;
 import lotto.model.PurchaseMoney;
+import lotto.utils.GeneratedLottoNumber;
 import lotto.view.InputView;
 
 public class GameController {
+
+    private static List<List<Integer>> lottos;
+
     public static void start(){
 
-        PurchaseMoney purchaseMoney = new PurchaseMoney(InputPurchaseNumber());
-        LottoCount lottoCount = new LottoCount(purchaseMoney.getValue());
+        PurchaseMoney purchaseMoney = new PurchaseMoney(inputPurchaseNumber()); // 구매 금액 입력
+        LottoCount lottoCount = new LottoCount(purchaseMoney.getValue()); // 구매 금액 -> 로또 갯수 변환
+        generateLottos(lottoCount.getValue()); // 로또 번호 생성
+        System.out.println(lottos);
     }
 
-    private static String InputPurchaseNumber(){
+    private static String inputPurchaseNumber(){
         return InputView.purchaseMoney();
+    }
+
+    private static void generateLottos(int lottoCount){
+        lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(GeneratedLottoNumber.getRandomNumber());
+        }
     }
 }

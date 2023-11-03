5fda5aa3548fc88d4cea99a4caa22b50930f10a9
bokyung <bbookng@gmail.com>
2023-11-03 11:28:23 +0900

    feat : Add Input Class
    Input 클래스 개발

---
+++ src/main/java/lotto/ui/Input.java
@@ -0,0 +1,34 @@
+package lotto.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Input {
+    private static final String PURCHASE_AMOUNT_INPUT = "구입금액을 입력해주세요.";
+    private static final String WINNING_NUMBERS_INPUT = "당첨 번호를 입력해주세요.";
+    private static final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해주세요.";
+    private static final String HASH = ",";
+
+    public long getPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_INPUT);
+        return Long.parseLong(Console.readLine());
+    }
+
+    public String getWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_INPUT);
+        return Console.readLine();
+    }
+
+    public int getBonusNumber() {
+        System.out.println(BONUS_NUMBER_INPUT);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> splitNumbers(String inputNumbers) {
+        return Arrays.stream(inputNumbers.split(HASH))
+                .map(number -> Integer.parseInt(number))
+                .collect(Collectors.toList());
+    }
+}

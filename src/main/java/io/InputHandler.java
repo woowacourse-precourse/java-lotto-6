b3eec7acf39f641b96a09cbead1f803bf716b30a
Hoo-sung <gntjd135@gmail.com>
2023-11-03 17:03:40 +0900

    feat(InputHandler): 로또 구입 금액을 입력받는 readMoney 메서드 생성

--- src/main/java/io/InputHandler.java
+++ src/main/java/io/InputHandler.java
@@ -1,5 +1,6 @@
 package io;
 
+import camp.nextstep.edu.missionutils.Console;
 import verifier.BonusNumberVerifier;
 import verifier.MoneyVerifier;
 import verifier.Verifier;
@@ -7,12 +8,19 @@ import verifier.WinnerNumberVerifier;
 
 public final class InputHandler {
 
-    private final Verifier MoneyVerier = new MoneyVerifier();
-    private final Verifier BonusVerifier = new BonusNumberVerifier();
-    private final Verifier WinnerNumberVerifier = new WinnerNumberVerifier();
+    private static final Verifier moneyVerier = new MoneyVerifier();
+    private static final Verifier bonusVerifier = new BonusNumberVerifier();
+    private static final Verifier winnerNumberVerifier = new WinnerNumberVerifier();
 
     private InputHandler(){
 
+    }
+
+    public static int readMoney(){
+        System.out.println("구입금액을 입력해 주세요");
+        String input = Console.readLine();
+        moneyVerier.check(input);
+        return Integer.parseInt(input);
     }
 
 }

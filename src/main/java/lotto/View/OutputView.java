505aa8f6aa75c9eac60e2b68fa4811cd1b398998
Firedrago95 <yonghwa95@naver.com>
2023-11-03 15:56:23 +0900

    feat(OutputView) : 로또 티켓 출력 기능 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,12 +1,16 @@
 package lotto.View;
 
+import java.util.List;
+
 public class OutputView {
 
     public static void printLottosSize(int size) {
         System.out.println(size+"개를 구매했습니다.");
     }
 
-    public static void printLottosNumbers() {
-
+    public static void printLottosNumbers(List<String> lottos) {
+        for (String lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }

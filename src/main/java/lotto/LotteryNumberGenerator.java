d9c04b6abd454432208895e9d18582bd8026eca1
happyryan <tjdgks120813@gmail.com>
2023-11-03 18:18:26 +0900

    feat(lotto LotteryNumberGenerator): add generateLottos method in LotteryNumberGenerator class
    Added the generateLottos method in the LottoNumberGenerator class to generate a list of Lottos based on the provided number.

--- src/main/java/lotto/LotteryNumberGenerator.java
+++ src/main/java/lotto/LotteryNumberGenerator.java
@@ -1,18 +1,27 @@
 package lotto;
 
 import java.util.*;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LotteryNumberGenerator {
 
-    public LotteryNumberGenerator(){}
 
-    private static List<Integer> generateLotteryNumber(){
+    private static List<Integer> generateLotteryNumber() {
         List<Integer> lottertyNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return lottertyNumbers;
     }
 
-    public static Lotto generateLotto(){
+    public static List<Lotto> generateLottos(int numberOfLottoes) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int lottoNumber = 0; lottoNumber < numberOfLottoes; lottoNumber++) {
+            Lotto lotto = generateLotto();
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    private static Lotto generateLotto() {
         Lotto lotto = new Lotto(generateLotteryNumber());
         return lotto;
     }

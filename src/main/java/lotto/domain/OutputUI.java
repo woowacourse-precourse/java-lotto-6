6c4f55c30b3a75ad8210605b48bcc5ae90084a52
mmalfoy <stpmalfoy@gmail.com>
2023-11-04 06:55:44 +0900

    feat(OutputUI) : 총 수익률 출력 구현

--- src/main/java/lotto/domain/OutputUI.java
+++ src/main/java/lotto/domain/OutputUI.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OutputUI {
 
     public void rate(long winningRate) {
-        System.out.println("총 수익률은 "+String.format("%.1f",winningRate)+"%입니다.");
+        System.out.println("총 수익률은 "+winningRate+"%입니다.");
     }
+
+
 
 }

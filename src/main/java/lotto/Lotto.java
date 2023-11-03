072f8bb000c118a8756801812ebfded147498ac1
이병수 <qudtn0315@naver.com>
2023-11-03 17:06:16 +0900

    feat (Lotto) : 숫자 중복 예외처리에 대한 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +8,25 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 갯수가 6개가 아닙니다.");
+        }
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        HashSet<Integer> duplicateNumber = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (duplicateNumber.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 중복된 숫자가 발생했습니다.");
+            }
+
+            duplicateNumber.add(number);
         }
     }
 

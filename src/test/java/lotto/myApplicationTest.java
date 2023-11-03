4db79094c03eb1fcc86021812a26eee84207d120
yooooonshine <hl3470@naver.com>
2023-11-04 02:32:03 +0900

    Test: 로또 발행 테스트 구현
    지불 금액만큼 로또가 발행되었는 지 테스트, 로또 번호가 오름차순인지 테스트 구현하였다

--- src/test/java/lotto/myApplicationTest.java
+++ src/test/java/lotto/myApplicationTest.java
@@ -1,12 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.domain.User;
+import lotto.utility.GameUtility;
 import lotto.validator.Validator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class myApplicationTest {
 
@@ -46,4 +57,42 @@ public class myApplicationTest {
                     .doesNotThrowAnyException();
         }
     }
+
+    @Nested
+    @DisplayName("로또 발행 테스트")
+    public class 로또_발행테스트 extends NsTest {
+        @ParameterizedTest
+        @DisplayName("지불 금액만큼 로또가 발행되었는 지 테스트한다.")
+        @CsvSource(value = "10000, 10")
+        void 로또_발행테스트(int payment, int lottoAmount) {
+            // given
+            User user;
+            // when
+            user = GameUtility.buyLotties(payment);
+            // then
+            assertEquals(user.getLottiesImpl().getLotties().size(), lottoAmount);
+        }
+
+        @Test
+        @DisplayName("로또 번호가 오름차순인지 테스트 한다.")
+        void 로또_오름차순_저장테스트() {
+            // given
+
+            List<Integer> LottoNumbers = new ArrayList<>(Arrays.asList(8, 21, 23, 41, 42, 43));
+
+            // when, then
+            assertRandomUniqueNumbersInRangeTest(
+                    () -> {
+                        User user;
+                        user = GameUtility.buyLotties(1000);
+                        assertEquals(user.getLottiesImpl().getLotties().get(0).getNumbers(),LottoNumbers);
+                    },
+                    Arrays.asList(43, 41, 42, 23, 21, 8)
+            );
+        }
+
+        @Override
+        protected void runMain() {
+        }
     }
+}

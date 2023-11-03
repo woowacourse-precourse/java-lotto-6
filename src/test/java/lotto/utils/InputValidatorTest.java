e485ce4f4f06717307420326c005cce62639e046
isyoo1213 <moethns@gmail.com>
2023-11-03 16:22:18 +0900

    test: InputValidatorTest 입력값 전처리 테스트 추가 InputValidatorTest - preprocessInputNullTest()   - 유저 입력값 전처리 테스트 - null 데이터 - 실패 - preprocessInputEmptyTest()   - 유저 입력값 전처리 테스트 - 빈 문자열 데이터 - 실패 - preprocessInputNonNumericTest()   - 유저 입력값 전처리 테스트 - NonNumeric 데이터 - 실패 - preprocessInputCorrectTest()   - 유저 입력값 전처리 테스트 - 공백을 포함한 데이터 - 성공

---
+++ src/test/java/lotto/utils/InputValidatorTest.java
@@ -0,0 +1,50 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputValidatorTest {
+    InputValidator inputValidator = new InputValidator();
+
+    @DisplayName("유저 입력값 전처리 테스트 - null 데이터 - 실패")
+    @Test
+    void preprocessInputNullTest() {
+        String testString = null;
+
+        assertThatThrownBy(() -> inputValidator.preprocessUserInput(testString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] null은 입력할 수 없습니다.");
+    }
+
+    @DisplayName("유저 입력값 전처리 테스트 - 빈 문자열 데이터 - 실패")
+    @Test
+    void preprocessInputEmptyTest() {
+        String testString = "";
+
+        assertThatThrownBy(() -> inputValidator.preprocessUserInput(testString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 값을 입력해주세요.");
+    }
+
+    @DisplayName("유저 입력값 전처리 테스트 - NonNumeric 데이터 - 실패")
+    @Test
+    void preprocessInputNonNumericTest() {
+        String testString = "abcd";
+
+        assertThatThrownBy(() -> inputValidator.preprocessUserInput(testString))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자를 입력해주세요.");
+    }
+
+    @DisplayName("유저 입력값 전처리 테스트 - 공백을 포함한 데이터 - 성공")
+    @Test
+    void preprocessInputCorrectTest() {
+        String testString = "50 00";
+        String expectedResult = "5000";
+
+        assertThat(inputValidator.preprocessUserInput(testString)).isEqualTo(expectedResult);
+    }
+}

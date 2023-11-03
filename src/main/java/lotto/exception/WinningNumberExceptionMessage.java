e8efed08aa9b1bb41ea8b9e0a48929f16410d798
euics <pjh575157@gmail.com>
2023-11-03 11:50:42 +0900

    feat(Exception): validateLottoNumberFormat 메서드 구현
    당첨 번호 형식 유효성 검사
    - "[ERROR] 로또 번호는 '1,2,3,4,5,6'과 같은 형식으로 입력해야 합니다." 출력.

---
+++ src/main/java/lotto/exception/WinningNumberExceptionMessage.java
@@ -0,0 +1,18 @@
+package lotto.exception;
+
+import java.util.List;
+
+public class WinningNumberExceptionMessage extends IllegalArgumentException{
+
+    public WinningNumberExceptionMessage(String message){
+        super(message);
+    }
+
+    public static void validateLottoNumberFormat(String input) {
+        String pattern = "^\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}$";
+
+        if (!input.matches(pattern)) {
+            throw new WinningNumberExceptionMessage("[ERROR] 로또 번호는 '1,2,3,4,5,6'과 같은 형식으로 입력해야 합니다.");
+        }
+    }
+}

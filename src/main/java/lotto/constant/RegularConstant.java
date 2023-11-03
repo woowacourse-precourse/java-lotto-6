7ef54b9fcbf2830e0f702c4d6a5ef3cd67006160
DH Jeong <wjdehgns97@gmail.com>
2023-11-04 00:46:53 +0900

    Feat: 일반 상수 구현

---
+++ src/main/java/lotto/constant/RegularConstant.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public class RegularConstant {
+    public static final int START_OF_NUMBER_RANGE = 1;
+    public static final int END_OF_NUMBER_RANGE = 45;
+    public static final int COUNT_OF_NUMBERS_CAN_HAVE = 6;
+    public static final int UNIT_AMOUNT = 1_000;
+    public static final int PERCENT = 100;
+    public static final String DELIMITER = ",";
+}

4aaa70208e75673bdc0e07d557a7fc8a7ab58cea
leedonghyun <skydh507@ajou.ac.kr>
2023-11-03 20:33:17 +0900

    feat : 사용자 금액 입력 기능 구현 - 예외 발생시 에러 문구 표시 후 재입력

---
+++ src/main/java/lotto/input/PurchaseAmountInputView.java
@@ -0,0 +1,28 @@
+package lotto.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class PurchaseAmountInputView {
+    private final InputValidate inputValidate = new InputValidate();
+
+    public Long userAmountInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return validateCheck();
+    }
+
+    public Long validateCheck() {
+        Long amount = null;
+        while (true) {
+            try {
+                String value = Console.readLine();
+                amount = inputValidate.valueValidate(value);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 올바르지 않는 값을 입력했습니다. 1000의 배수의 가격을 입력해주세요.");
+            }
+        }
+        return amount;
+    }
+
+
+}

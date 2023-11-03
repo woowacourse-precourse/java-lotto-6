23bde89954531f4f53ad43d7f2a66eb0f045d9d6
02ggang9 <02ggang9@gmail.com>
2023-11-03 16:43:51 +0900

    feat(LottoResult): 로또 비교 후 결과값을 반환하는 책임 구현

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -6,12 +6,42 @@ public class LottoResult {
 
     private final LottoComparator comparator = new LottoComparator();
 
+    private Integer matchesThree;
+    private Integer matchesFour;
+    private Integer matchFive;
+    private Integer matchFiveAndBonus;
+    private Integer matchSix;
+
     public LottoResult() {
+        this.matchesThree = 0;
+        this.matchesFour = 0;
+        this.matchFive = 0;
+        this.matchFiveAndBonus = 0;
+        this.matchSix = 0;
     }
 
-    public void showLottoResult(List<String> winningNumbers, Integer bonus, List<Lotto> lottos) {
+    public LottoResult showLottoResult(List<String> winningNumbers, Integer bonus, List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
-            comparator.compareLotto(winningNumbers, bonus, lotto);
+            List<Integer> result = comparator.compareLotto(winningNumbers, bonus, lotto);
+            saveResult(result);
+        }
+        return this;
+    }
+
+    private void saveResult(List<Integer> result) {
+        Integer winningNumberMatchCount = result.get(0);
+        Integer bonusMatchCount = result.get(1);
+
+        if (winningNumberMatchCount == 3) {
+            matchesThree++;
+        } else if (winningNumberMatchCount == 4) {
+            matchesFour++;
+        } else if (winningNumberMatchCount == 5 && bonusMatchCount == 1) {
+            matchFiveAndBonus++;
+        } else if (winningNumberMatchCount == 5 && bonusMatchCount == 0) {
+            matchFive++;
+        } else if (winningNumberMatchCount == 6) {
+            matchSix++;
         }
     }
 }

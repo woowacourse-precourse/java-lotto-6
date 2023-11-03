8684d992760cc434cd9cc2399925dd82a1b20004
kyu-wooon <catdog0831@gmail.com>
2023-11-03 13:02:13 +0900

    lotto_random(int lotto_count) 기능 구현
    * * input_price()에서 입력받은 금액 바탕으로 구한 구입 갯수만큼, 랜덤 숫자 6개를 담은 List<Integer>를 생성하여 List<List<Integer>>에 담는다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+    public static List<List<Integer>> numbers_list = new ArrayList<>();
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,6 +16,14 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void lotto_random(int lotto_count){
+        numbers_list = new ArrayList<>();
+        for(int i  = 0; i<lotto_count; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            numbers_list.add(numbers);
         }
     }
 

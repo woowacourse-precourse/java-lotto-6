ff959cec3b4cdc42d729338e0d4db54d845a95bb
Hoo-sung <gntjd135@gmail.com>
2023-11-03 22:41:31 +0900

    feat(LottoSystem): 컨트롤러를 초기화하는 initialize 메서드 생성

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -1,12 +1,24 @@
 package lotto.controller;
 
+import lotto.domain.BonusNumber;
 import lotto.domain.UserLotto;
+import lotto.domain.WinningLotto;
 
 public class LottoSystem {
 
     private UserLotto userLotto;
+    private WinningLotto winningLotto;
+    private BonusNumber bonusNumber;
 
     public void run() {
-
+        initialize();
     }
+
+    private void initialize(){
+        userLotto = new UserLotto();
+        winningLotto = new WinningLotto();
+        bonusNumber = new BonusNumber();
+    }
+
+
 }

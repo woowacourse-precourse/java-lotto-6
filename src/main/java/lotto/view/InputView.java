ad4bce2cb71a269c2e5d44d74bfd15adf396be31
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-03 21:15:27 +0900

    refactor(InputView) : add static to InputView class methods

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,18 +7,18 @@ import lotto.domain.WinningNumber;
 
 public class InputView {
 
-    public PurchaseAmount inputPurchaseAmount() {
+    public static PurchaseAmount inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
         return new PurchaseAmount(Integer.parseInt(purchaseAmount));
     }
 
-    public WinningNumber inputLottoWinningNumbers() {
+    public static WinningNumber inputLottoWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return new WinningNumber(Console.readLine());
     }
 
-    public BonusNumber inputLottoBonusNumbers() {
+    public static BonusNumber inputLottoBonusNumbers() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return new BonusNumber(Console.readLine());
     }

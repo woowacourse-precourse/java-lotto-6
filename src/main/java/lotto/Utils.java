c6cb0c18ee1db351c0e990e8ea359e90bc3941a0
minjun7410 <minjun7410@gmail.com>
2023-11-03 14:29:28 +0900

    feat(Utils): 6개의 1-45 범위 숫자를 랜덤으로 뽑는 기능 구현
    * 숫자를 랜덤으로 뽑는 generateRandomLottoNumbers 메서드를 생성했다.
    * 랜덤으로 뽑았을 때 중복된 숫자가 있는지 검사하는 기능은 pickUniqueNumbersInRange에서 수행한다.

---
+++ src/main/java/lotto/Utils.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Utils {
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
+    private static final int NUMBER_OF_LOTTO = 6;
+    private Utils() {
+    }
+
+    public static List<Integer> generateRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER, NUMBER_OF_LOTTO);
+    }
+
+}

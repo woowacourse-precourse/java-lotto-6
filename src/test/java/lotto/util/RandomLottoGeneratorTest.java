ac83e8bd9b34f8784c3b2807916c4198ea3e8ffb
zangsu <zangsu_@naver.com>
2023-11-03 18:47:25 +0900

    test : create local variable LottoGenerator to generate Lotto instance

--- src/test/java/lotto/util/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/util/RandomLottoGeneratorTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 
 class RandomLottoGeneratorTest {
 
+    LottoGenerator generator = new RandomLottoGenerator();
+
     @Test
     @DisplayName("로또가 정상적으로 생성됨")
     public void 정상_동작() throws Exception{
         Assertions.assertThatNoException()
-                .isThrownBy(() -> RandomLottoGenerator.generate());
+                .isThrownBy(() -> generator.generate());
     }
 
 }

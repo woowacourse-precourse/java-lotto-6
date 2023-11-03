1ca6a35e0d6577c2a9a286329dad0ebfef755658
sami355-24 <ddung355@gmail.com>
2023-11-03 21:24:01 +0900

    feat(Domain): 구매 금액 관련 객체 추가

---
+++ src/main/java/lotto/Domain/Money.java
@@ -0,0 +1,39 @@
+package lotto.Domain;
+
+
+import static lotto.CommonValidation.CommonValidation.hasBlank;
+import static lotto.CommonValidation.CommonValidation.isInteger;
+import static lotto.Message.ExceptionMessage.MoneyErrorMessage.*;
+import static lotto.Util.Util.ConvertStringToInteger;
+
+import lotto.Exception.MoneyException;
+import lotto.Message.ExceptionMessage.MoneyErrorMessage;
+
+public class Money {
+
+    Integer money;
+
+    private Money(String inputMoney) {
+        hasBlank(inputMoney);
+        isInteger(inputMoney);
+        this.money = ConvertStringToInteger(inputMoney);
+        isValidRange(money);
+        isDivisible(money);
+    }
+
+    public static Money of(String inputMoney) {
+        return new Money(inputMoney);
+    }
+
+    private void isValidRange(Integer money) {
+        if (money < 1000 || money > 100000) {
+            throw new MoneyException(RANGE_ERROR_MESSAGE.getMessage(money));
+        }
+    }
+
+    private void isDivisible(Integer money) {
+        if (money % 1000 != 0) {
+            throw new MoneyException(DIVISIBILITY_ERROR_MESSAGE.getMessage(money));
+        }
+    }
+}

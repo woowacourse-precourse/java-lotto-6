e8daa441346ed040b25abdb17a8a855da4e156ab
m2nseop <pms246@naver.com>
2023-11-04 03:55:52 +0900

    refactor: checkDivisibleBy1000 함수 리턴타입 void로 수정

--- src/main/java/lotto/Validator/InputValidator.java
+++ src/main/java/lotto/Validator/InputValidator.java
@@ -5,11 +5,10 @@ public class InputValidator {
     public static final String CAN_NOT_DIVIDE_BY_1000 = "로또 구입 금액은 1000으로 나누어 떨어져야 합니다.";
     public static final String NOT_A_NUMBER = "로또 구입 금액은 숫자로 입력하셔야합니다.";
 
-    public static boolean checkDivisibleBy1000(String input) {
+    public static void checkDivisibleBy1000(String input) {
         if (Integer.parseInt(input) % 1000 != 0) {
             throw new IllegalArgumentException(ERROR + CAN_NOT_DIVIDE_BY_1000);
         }
-        return true;
     }
 
     public static boolean isNumeric(String str) {

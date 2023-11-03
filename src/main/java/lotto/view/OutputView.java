8531b684824346d9545737924d531aa0f7fd75f8
qncj5224 <qncj5224@naver.com>
2023-11-03 20:39:16 +0900

    feat : 구입 금액 요청 문구 출력 메소드 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+  private final static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+
+  public static void printInputMoneyMessage() {
+    System.out.println(INPUT_MONEY_MESSAGE);
+  }
+}

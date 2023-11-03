4a5e4d11af4dfb53891943c3f4ef6a34cf918e4a
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 01:12:05 +0900

    Test: "문자 입력시 오류 발생 추가"

--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -8,6 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PurchaseTest {
 
+    @DisplayName("문자 입력시 오류가 발생한다.")
+    @Test
+    void validateNum() {
+        assertThatThrownBy(() -> new Purchase("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 구입 가격이 1000원 단위가 아니면 오류가 발생한다.")
     @Test
     void validateUnit() {

66b6e53539abf2790beda0631071c2ba53a66e79
leedonghyun <skydh507@ajou.ac.kr>
2023-11-04 02:13:17 +0900

    feat : 결과 출력 기능(생성된 로또 값 출력)

---
+++ src/main/java/lotto/input/view/GeneratedLottoOutputView.java
@@ -0,0 +1,23 @@
+package lotto.input.view;
+
+import java.util.List;
+import lotto.Lotto;
+
+public class GeneratedLottoOutputView {
+    private final List<Lotto> lottos;
+
+    private final Integer lottoNumber
+
+
+    public GeneratedLottoOutputView(List<Lotto> lottos) {
+        this.lottos = lottos;
+        this.lottoNumber = lottos.size();
+    }
+
+    public void printGeneratedLottos() {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+}

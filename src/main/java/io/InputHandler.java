f62b7823a0d850b133a7a52762e0a0647b39434e
Hoo-sung <gntjd135@gmail.com>
2023-11-03 17:32:19 +0900

    feat(InputHandler): 보너스 번호를 입력받는 메서스 생성

--- src/main/java/io/InputHandler.java
+++ src/main/java/io/InputHandler.java
@@ -13,27 +13,37 @@ import java.util.stream.Collectors;
 public final class InputHandler {
 
     private static final Verifier moneyVerifier = new MoneyVerifier();
-    private static final Verifier bonusVerifier = new BonusNumberVerifier();
+    private static final Verifier bonusNumVerifier = new BonusNumberVerifier();
     private static final Verifier winnerNumberVerifier = new WinnerNumberVerifier();
 
-    private InputHandler(){
+    private InputHandler() {
 
     }
 
-    public static int readMoney(){
+    public static int readMoney() {
         System.out.println("구입금액을 입력해 주세요");
         String input = Console.readLine();
         moneyVerifier.check(input);
+
         return Integer.parseInt(input);
     }
 
-    public static List<Integer> readWinnerNumber(){
+    public static List<Integer> readWinnerNumber() {
         System.out.println("당첨 번호를 입력해 주세요");
         String input = Console.readLine();
         winnerNumberVerifier.check(input);
+
         return Arrays.stream(input.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public static Integer readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        bonusNumVerifier.check(input);
+
+        return Integer.parseInt(input);
     }
 
 

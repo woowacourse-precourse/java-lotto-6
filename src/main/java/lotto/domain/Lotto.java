151eef1fd7f4899edeaef88c07ceda089aa08f96
Isaac Han <isaachan233@gmail.com>
2023-11-03 10:49:08 +0900

    feat: 1~45 범위 외의 숫자 검증 기능 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,8 @@
 package lotto.domain;
 
 import lotto.enums.ErrorMessages;
+import lotto.enums.LottoEnums;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -14,11 +14,15 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoEnums.SELECTED_NUMBERS_SIZE.getValue()) {
             throw new IllegalArgumentException(ErrorMessages.OVER_MAX_SIZE_MESSAGE.getMessage());
         }
-        if (numbers.stream().distinct().count() != 6) {
+        if (numbers.stream().distinct().count() != LottoEnums.SELECTED_NUMBERS_SIZE.getValue()) {
             throw new IllegalArgumentException(ErrorMessages.DUPLICATE_NUMBER_MESSAGE.getMessage());
+        }
+        if (numbers.stream().anyMatch(number -> number < LottoEnums.MIN_LOTTO_NUMBER.getValue()
+                        || number > LottoEnums.MAX_LOTTO_NUMBER.getValue())) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_NUMBER_RANGE_MESSAGE.getMessage());
         }
     }
 

7cbc3db34d16ab174d6e1571dd0966a4559a4dcf
gusdn0108 <gusdn6671@naver.com>
2023-11-03 20:52:00 +0900

    feat(Controller): part2 완성 로또목록 생성 및 출력

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,21 +1,40 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoFactory;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Controller {
+    private int lottoCnt;
 
     public void run() {
         part1();
+        part2();
     }
 
     public void part1() {
         OutputView.printBuyInputPrice();
         String userInput = InputView.userInput();
         int userPrice = InputView.userInputParsedInt(userInput);
-        int lottoCnt = userPrice / 1000;
+        lottoCnt = userPrice / 1000;
         OutputView.printBuyLottoCount(lottoCnt);
     }
+
+    public void part2(){
+        List<Lotto> lottoies = new ArrayList<>();
+        for (int i = 0; i < lottoCnt; i++) {
+            Lotto lotto = LottoFactory.generateLotto();
+            lottoies.add(lotto);
+        }
+
+        for (Lotto lottos : lottoies) {
+            System.out.println(lottos);
+        }
+
+    }
+
 
 }

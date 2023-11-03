d535d6a99c083f43661da53d38ee29fbcabfe595
Annjieun <dkslwdms2121@naver.com>
2023-11-03 16:50:00 +0900

    feat(OutputMessage): 결과를 출력하는 메세지들을 모은 ENUM 클래스 추가

---
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public enum OutputMessage {
+    PURCHASE("개를 구매했습니다."),
+    WINNING_STATUS("당첨 통계\n---"),
+    WINNING_RESULT("%d개 일치 (%,d원) - %d개"),
+    TOTAL_RETURN("총 수익률은 %.1f%%입니다.");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

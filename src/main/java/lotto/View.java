c965032e5e1ac4d6a596349073b45089f417e48f
elephantLikesApple <elephant890@naver.com>
2023-11-03 18:43:19 +0900

    feat(View) : 보너스 번호 입력 기능 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -6,27 +6,31 @@ import java.util.List;
 
 public class View {
 
-    public static int readPayment() {
-        System.out.println("구입금액을 입력해 주세요.");
+    public interface Converter<T> {
+        T convert(String input);
+    }
+
+    public static <T> T readNumber(String prompt, Converter<T> converter) {
+        System.out.println(prompt);
 
         while (true) {
             try {
-                return StringUtils.stringToNumber(readLine());
+                return converter.convert(readLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public static List<Integer> readWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+    public static int readPayment() {
+        return readNumber("구입금액을 입력해 주세요.", StringUtils::stringToNumber);
+    }
 
-        while (true) {
-            try {
-                return StringUtils.stringToNumbers(readLine());
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public static List<Integer> readWinningNumbers() {
+        return readNumber("당첨 번호를 입력해 주세요.", StringUtils::stringToNumbers);
+    }
+
+    public static int readBonusNumber() {
+        return readNumber("보너스 번호를 입력해 주세요.", StringUtils::stringToNumber);
     }
 }

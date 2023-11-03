ba13fff64b825e6d4a78e3915c76152bf2fc8655
siho-song <thdtlgh234@naver.com>
2023-11-03 19:01:26 +0900

    [Feat] 로또번호 리스트를 반환하는 기능 구현
    사용자가 입력한 번호 문자열을 Integer 자료형 리스트로 반환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.Utils;
 import lotto.validator.InputValidator;
 import lotto.view.InputView;
 
@@ -15,4 +17,14 @@ public class LottoService {
         inputValidator.validateMoney(totalMoney);
         return totalMoney / LOTTO_PRICE;
     }
+
+    public List<Integer> getLottoNumbers(List<String> inputNumbers) {
+        inputValidator.validateHasOnlyIntegers(inputNumbers);
+        List<Integer> numbers = Utils.convertToIntegers(inputNumbers);
+
+        inputValidator.validateLottoNumber(numbers);
+
+        return numbers;
+    }
+
 }

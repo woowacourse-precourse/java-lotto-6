ce43d26771af1d4525c9c028ca8cc4b54043fedf
sonjiseokk <thswltjr11@gmail.com>
2023-11-03 17:46:06 +0900

    feat: Member 객체 생성

--- src/main/java/lotto/domain/Member.java
+++ src/main/java/lotto/domain/Member.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
+import lotto.Lotto;
+
+import java.util.List;
+
 public class Member {
+    private final int purchasedMoney;
+    private List<Lotto> purchasedLottos;
+
+    public Member(final int purchasedMoney, final List<Lotto> purchasedLottos) {
+        this.purchasedMoney = purchasedMoney;
+        this.purchasedLottos = purchasedLottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return purchasedLottos;
+    }
 }

b5865cf23ea0db9710132c9d9fcb3702d3b9fdcc
leesangjun <hubls@naver.com>
2023-11-03 20:49:14 +0900

    feat: 로또 번호 중복 검증 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,15 +6,22 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateOverSize(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateOverSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        boolean hasDuplicates = numbers.stream().distinct().count() < numbers.size();
+
+        if (hasDuplicates) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

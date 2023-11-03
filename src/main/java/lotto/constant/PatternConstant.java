76183b9bb57930078c3732aa41820ed01d5356b1
DH Jeong <wjdehgns97@gmail.com>
2023-11-04 00:44:37 +0900

    Feat: Pattern 상수 구현

---
+++ src/main/java/lotto/constant/PatternConstant.java
@@ -0,0 +1,11 @@
+package lotto.constant;
+
+import java.util.regex.Pattern;
+
+public final class PatternConstant {
+    public static final Pattern NUMBER_PATTERN = Pattern.compile("(\\+|-)?[0-9]+");
+    public static final Pattern POSITIVE_NUMBER_PATTERN = Pattern.compile("\\+?[1-9][0-9]*");
+    public static final Pattern TRUE_PATTERN = Pattern.compile("true");
+    public static final Pattern FALSE_PATTERN = Pattern.compile("false");
+    public static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Z]([0-9]|[a-zA-Z]|_)*");
+}

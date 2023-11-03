687493edfb998f5c053078963c8fefeff0a489d8
mainfn <_@mainfn.dev>
2023-11-03 16:16:20 +0900

    docs: LottoPrize 문서화

--- src/main/java/lotto/domain/lotto/LottoPrize.java
+++ src/main/java/lotto/domain/lotto/LottoPrize.java
@@ -3,6 +3,9 @@ package lotto.domain.lotto;
 import java.util.List;
 import lotto.domain.money.Money;
 
+/**
+ * WinningLotto와 Lotto를 비교하여 일치하는 번호 및 보너스 번호를 계산하여 로또 등 수를 나타낼 때 사용되는 클래스입니다.
+ */
 public enum LottoPrize {
     FIRST(1, Money.from(2_000_000_000), 6, false),
     SECOND(2, Money.from(30_000_000), 5, true),
@@ -29,6 +32,9 @@ public enum LottoPrize {
         this.hasBonusNumber = hasBonusNumber;
     }
 
+    /**
+     * 일치하는 로또 번호 개수, 보너스 번호 유무에 따라 적절한 등수를 반환합니다. 만약 1~5등에 속하지 않는 경우라면 전부 NONE을 반환합니다.
+     */
     public static LottoPrize of(
             final int matchedCount,
             final boolean hasBonusNumber

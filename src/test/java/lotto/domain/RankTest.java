19fe5bdbaededfd2c91e1ee488b261e189e89fb5
euics <pjh575157@gmail.com>
2023-11-03 14:48:56 +0900

    test: whenGetCountOfMatch_thenReturnsCorrectMatchCount 메서드 구현
    각 Rank의 맞힌 개수가 올바르게 반환하는 메서드
    - `getCountOfMatch` 메서드를 통해 확인

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -16,4 +16,12 @@ class RankTest {
         assertEquals(Rank.FIFTH, Rank.valueOf(3, false), "3개 일치는 FIFTH여야 함");
         assertEquals(Rank.NONE, Rank.valueOf(2, false), "2개 일치는 NONE이어야 함");
     }
+
+    @Test
+    @DisplayName("각 Rank의 맞힌 개수가 올바르게 반환됨")
+    void whenGetCountOfMatch_thenReturnsCorrectMatchCount() {
+        assertEquals(6, Rank.FIRST.getCountOfMatch(), "FIRST는 6개 일치여야 함");
+        assertEquals(5, Rank.SECOND.getCountOfMatch(), "SECOND는 5개 일치여야 함");
+        assertEquals(5, Rank.THIRD.getCountOfMatch(), "THIRD는 5개 일치여야 함");
+    }
 }

3a62d9c10b265a92b1c7a5ad30a5605bb9c887bb
HeeHeeg <qmffndoden@naver.com>
2023-11-03 21:43:58 +0900

    feat(lotto): 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Lotto lotto = new Lotto(numbers);
-        System.out.println(lotto.getNumbers());
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        Integer userPurchaseAmount = Integer.parseInt(purchaseAmount);
+
+        int numberOfLottos = userPurchaseAmount / 1000;
+        System.out.println();
+        System.out.println(numberOfLottos + "개를 구매했습니다.");
+        for (int i = 0; i < numberOfLottos; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

8338821218d6c825b459be8681b138c88fad4fee
HwangSeokJin <ghkd3703@gmail.com>
2023-11-03 22:50:58 +0900

    test: 당첨 번호 입력 테스트 추가

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -4,18 +4,37 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest{
     InputView view = new InputView();
-    
+
     @Test
     void 로또_구입_금액_입력_테스트() {
         command("100d");
-        assertThatThrownBy(() -> {
-            view.inputPurchaseAmount();
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> view.inputPurchaseAmount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 범위_외의_당첨_번호_입력_테스트() {
+        command("46,1,2,3,4,5");
+        assertThatThrownBy(() -> view.inputWinningNumbers())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호_개수_오입력_테스트() {
+        command("1,2,3,4,5,6,7");
+        assertThatThrownBy(() -> view.inputWinningNumbers())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_아닌_당첨_번호_입력_테스트() {
+        command("1,2,3,4,5,e");
+        assertThatThrownBy(() -> view.inputWinningNumbers())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     private void command(final String... args) {

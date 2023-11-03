974d72af37833dcf9b1c3195265ccf31da01d888
gusdn0108 <gusdn6671@naver.com>
2023-11-03 19:05:06 +0900

    feat(Message):개를 구매했습니다 메시지 추가

--- src/main/java/lotto/utils/Message.java
+++ src/main/java/lotto/utils/Message.java
@@ -1,7 +1,8 @@
 package lotto.utils;
 
 public enum Message {
-    PRINT_USER_INPUT_BUY_PRICE("구입금액을 입력해 주세요.");
+    PRINT_USER_INPUT_BUY_PRICE("구입금액을 입력해 주세요."),
+    PRINT_USER_BUY_LOTTO_COUNT("개를 구매했습니다");
 
 
     private final String message;

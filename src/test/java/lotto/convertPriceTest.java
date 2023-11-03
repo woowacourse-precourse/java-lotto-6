6ef2d676b174736ecb240855348e426515f660c6
Jiwon Na <jiwonna52@gmail.com>
2023-11-03 23:34:40 +0900

    Test : 입력받은 문자열 당첨 숫자를 정수형 리스트로 반환하는 테스트 추가

--- src/test/java/lotto/convertPriceTest.java
+++ src/test/java/lotto/convertPriceTest.java
@@ -4,7 +4,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static lotto.controller.InputConverter.convertPrice;
+import static lotto.controller.InputConverter.convertWinningNumber;
 
 public class convertPriceTest {
     @DisplayName("입력받은 가격 정수형으로 후처리")
@@ -13,5 +17,22 @@ public class convertPriceTest {
         String inputPrice = "1000";
         int price = convertPrice(inputPrice);
         Assertions.assertThat(price).isEqualTo(1000);
+    }
+
+    @DisplayName("입력받은 당첨 숫자 정수형 리스트로")
+    @Test
+    void convertWinningNumberTest() {
+        String inputWinningNumber = "1,2,3,4,5,6";
+        List<Integer> winningNumbers = new ArrayList<>();
+        winningNumbers.add(1);
+        winningNumbers.add(2);
+        winningNumbers.add(3);
+        winningNumbers.add(4);
+        winningNumbers.add(5);
+        winningNumbers.add(6);
+
+        List<Integer> returnWinningNumbers = convertWinningNumber(inputWinningNumber);
+
+        Assertions.assertThat(winningNumbers).isEqualTo(returnWinningNumbers);
     }
 }

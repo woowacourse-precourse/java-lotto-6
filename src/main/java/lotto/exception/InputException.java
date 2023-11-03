02362152bfaffe1b2ba76433d12c0a09ab799a69
SeungjunNA <nsjjsng1@gmail.com>
2023-11-03 23:49:47 +0900

    feat : 당첨 번호 입력시 숫자가 아니거나, 1~45사이 값이 아닐 경우 예외 처리 후 다시 입력하는 기능 추가

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -1,16 +1,46 @@
 package lotto.exception;
 
-public class InputException {
+import lotto.view.InputView;
 
-    public void checkThousandUnit(int money){
-        if(money % 1000 != 0){
+public class InputException {
+    public void checkThousandUnit(int money) {
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000단위로 입력해주세요.");
         }
     }
+
+    public void inputExceptionCheck(String amount) {
+        try {
+            int money = toInt(amount);
+            checkThousandUnit(money);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            new InputView().inputAmount();
+        }
+    }
+
     public int toInt(String amount) {
-        if(!amount.matches("^[0-9]*$")){
-            throw new NumberFormatException("[ERROR] 숫자를 입력해주세요.");
+        if (!amount.matches("^[0-9]*$")) {
+            throw new NumberFormatException("[ERROR] 숫자를 입력해 주세요.");
         }
         return Integer.parseInt(amount);
+    }
+
+    public void checkExceptionWinningNumber(String[] numbers) {
+        for (int i = 0; i < 6; i++) {
+            try {
+                int number = toInt(numbers[i]);
+                checkNotLottoNumber(number);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                new InputView().winningNumber();
+            }
+        }
+    }
+
+    public void checkNotLottoNumber(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("1에서 45사이 값을 입력해 주세요.");
+        }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,39 +2,33 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.InputException;
+import lotto.gameUtil.GameLogic;
 
 public class InputView {
     private OutputView outputView;
     private InputException inputException;
+    private GameLogic gameLogic;
     public InputView(){
         outputView = new OutputView();
         inputException = new InputException();
+        gameLogic = new GameLogic();
     }
     public int inputAmount(){
         outputView.amountMessage();
         String amount = Console.readLine();
-        int money = 0;
-        try {
-            money = toInt(amount);
-        }catch (NumberFormatException e){
-            System.out.println(e.getMessage());
-            return inputAmount();
-        }
-        inputException.checkThousandUnit(money);
-        return money;
+        inputException.inputExceptionCheck(amount);
+        return Integer.parseInt(amount);
     }
     public String winningNumber(){
         outputView.winningNumberMessage();
-        return Console.readLine();
+        String winningNumber = Console.readLine();
+        String[] winningNumbers = gameLogic.splitNumber(winningNumber);
+        inputException.checkExceptionWinningNumber(winningNumbers);
+        return winningNumber;
     }
     public int bonusNumber(){
         outputView.bonusNumberMessage();
         return Integer.parseInt(Console.readLine());
     }
-    private int toInt(String amount){
-        if(!amount.matches("^[0-9]*$")){
-            throw new NumberFormatException("[ERROR] 숫자를 입력해 주세요.");
-        }
-        return Integer.parseInt(amount);
-    }
+
 }

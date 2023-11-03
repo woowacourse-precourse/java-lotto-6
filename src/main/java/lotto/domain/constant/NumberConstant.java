4ea6a7fee92e179f63e5ee4a52fee82901353a72
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-04 01:04:28 +0900

    feat: NumberConstant에 LottoService 관련 상수 추가

--- src/main/java/lotto/domain/constant/NumberConstant.java
+++ src/main/java/lotto/domain/constant/NumberConstant.java
@@ -7,6 +7,10 @@ public class NumberConstant {
     public static final int LOTTO_NUMBER_END = 45;
     public static final int LOTTO_PRICE = 1000;
 
+    // SERVICE
+    public static final int MATCHRESULT_INITIAL_NUM = 0;
+    public static final int MATCHRESULT_INCREMENT_NUM = 1;
+
     // TEST CASE
     public static final int TEST_TICKET_NUM = 5;
 }

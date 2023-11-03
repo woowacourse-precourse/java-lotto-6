87efed4267050c30bc0b9e170df9244ff5e4ef08
euics <pjh575157@gmail.com>
2023-11-03 11:29:49 +0900

    feat(InputView): getPurchaseAmountView 메서드 구현
    사용자로부터 구입금액 입력 받는 메서드

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     public static void printPurchasePrompt() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static String getPurchaseAmountView() {
+        String input = Console.readLine();
+        System.out.println();
+
+        return input;
     }
 }

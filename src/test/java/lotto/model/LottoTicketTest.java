67e39dea8da4149bf57703b7f401e86103efed5d
leeedohyun <ldh19738@gmail.com>
2023-11-03 15:23:57 +0900

    test: 로또 생성 개수 테스트

---
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LottoTicketTest {
+
+    @Test
+    public void 로또_발행_기능_테스트() {
+        // given
+        final int purchaseLottoNumber = 3;
+
+        // when
+        final LottoTicket lottoTicket = LottoTicket.generate(purchaseLottoNumber);
+
+        // then
+        Assertions.assertEquals(lottoTicket.getLottoSize(), 3);
+    }
+}

776e305b76bf36554d0f2dea58229d8e2b31add4
Minjae-An <mj3242@naver.com>
2023-11-03 21:15:51 +0900

    feat : 예외 메시지 생성 기능

---
+++ src/main/java/lotto/exception/ExceptionMessageBuilder.java
@@ -0,0 +1,9 @@
+package lotto.exception;
+
+import static lotto.exception.ExceptionMessage.*;
+
+public final class ExceptionMessageBuilder {
+    public static String build(ExceptionMessage exceptionMessage){
+        return String.join(" ", ERROR_LOG.getMessage(), exceptionMessage.getMessage());
+    }
+}

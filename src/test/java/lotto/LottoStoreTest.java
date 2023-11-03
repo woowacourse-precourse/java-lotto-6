32e338f5add224e7a2e39e9f7a51fdc13babc6e5
watermelon3737 <pshrina1@naver.com>
2023-11-03 15:41:23 +0900

    test(LottoStoreTest) : 로또 금액이 1000원 미만 이거나, 1000원 단위가 아닌 경우의 test 구현

--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -23,14 +23,54 @@ public class LottoStoreTest {
         //then
         assertThatThrownBy(() -> lottoStore.validateMoneyInput(englishInMoney))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("숫자만 입력해주세요.");
+                .hasMessage("[ERROR] 숫자만 입력해주세요.")
+                .hasMessageContaining("[ERROR]");
 
         assertThatThrownBy(() -> lottoStore.validateMoneyInput(koreanInMoney))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("숫자만 입력해주세요.");
+                .hasMessage("[ERROR] 숫자만 입력해주세요.")
+                .hasMessageContaining("[ERROR]");
 
         assertThatThrownBy(() -> lottoStore.validateMoneyInput(specialSignInMoney))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("숫자만 입력해주세요.");
+                .hasMessage("[ERROR] 숫자만 입력해주세요.")
+                .hasMessageContaining("[ERROR]");
     }
+
+    @DisplayName("로또를 사기 위한 금액이 1000원 미만이면 예외가 발생한다")
+    @Test
+    void chargedMoneyLessThan1000() {
+        //when
+        LottoStore lottoStore = new LottoStore();
+
+        //given
+        String money = "0";
+        lottoStore.validateMoneyInput(money);
+        lottoStore.chargeMoney(money);
+
+        //then
+        assertThatThrownBy(() -> lottoStore.validateChargedMoney())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입금액은 1000원 부터입니다.")
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("로또를 사기 위한 금액이 1000원 단위가 아닐 경우 예외가 발생한다")
+    @Test
+    void chargedMoneyNotDivideBy1000() {
+        //when
+        LottoStore lottoStore = new LottoStore();
+
+        //given
+        String money = "10001";
+        lottoStore.validateMoneyInput(money);
+        lottoStore.chargeMoney(money);
+
+        //then
+        assertThatThrownBy(() -> lottoStore.validateChargedMoney())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입금액은 1000원 단위 입니다.")
+                .hasMessageContaining("[ERROR]");
+    }
+
 }

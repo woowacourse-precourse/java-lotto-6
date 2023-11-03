83d5aa89b629b15793c41b36e6be4c33a785dac9
Leespect <wewqwew153@gmail.com>
2023-11-03 14:15:03 +0900

    Refactor : ResponseMessages를 enum으로 수정

--- src/main/java/constants/ResponseMessages.java
+++ src/main/java/constants/ResponseMessages.java
@@ -1,32 +1,24 @@
 package constants;
 
-import java.sql.SQLOutput;
 
-public class ResponseMessages {
-    public static String endBuyMessage(int count) {
-        return count + "개를 구매했습니다.";
+public enum ResponseMessages {
+    END_BUY_MESSAGE("개를 구매했습니다."),
+    WIN_STATS_MESSAGE("당첨 통계"),
+    LINE_MESSAGE("---"),
+    WIN_MESSAGE_5("3개 일치 (5,000원) - "),
+    WIN_MESSAGE_4("4개 일치 (50,000원) - "),
+    WIN_MESSAGE_3("5개 일치 (1,500,000원) - "),
+    WIN_MESSAGE_2("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    WIN_MESSAGE_1("6개 일치 (2,000,000,000원) - "),
+    PROFIT_MESSAGE("총 수익률은 ");
+
+    private final String message;
+
+    ResponseMessages(String message) {
+        this.message = message;
     }
 
-    public static final String winStatsMessage = "당첨 통계";
-    public static final String lineMessage = "---";
-
-    public static void winMessage(int win, int count) {
-        String message = "";
-        if (win == 5)
-            message = "3개 일치 (5,000원) - " + count + "개";
-        else if (win == 4)
-            message = "4개 일치 (50,000원) - " + count + "개";
-        else if (win == 3)
-            message = "5개 일치 (1,500,000원) - " + count + "개";
-        else if (win == 2)
-            message = "5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개";
-        else if (win == 1)
-            message = "6개 일치 (2,000,000,000원) - " + count + "개";
-        System.out.println(message);
+    public String getMessage() {
+        return message;
     }
-
-    public static void profitMessage(double profit) {
-        System.out.println("총 수익률은 " + profit + "입니다.");
-    }
-
 }

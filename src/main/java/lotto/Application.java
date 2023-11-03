19b91c290f95c2859adb83eaace405e6b0809e9c
한다현 <handahyeon@handahyeon-ui-MacBookAir.local>
2023-11-04 00:13:15 +0900

    feat: input lotto purchase amount and validate the amount

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static int inputLottoAmount() {
+        int amount = Integer.parseInt(Console.readLine());
+        validateAmount(amount);
+        return amount;
+    }
+
+    public static void validateAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원으로 나누어 떨어져야 합니다.");
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = inputLottoAmount();
     }
 }

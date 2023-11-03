c6801bf06a3322f77f1d6c5e217d0bf51dbcc2ef
devholic22 <hyunjoon.tech@gmail.com>
2023-11-04 01:19:34 +0900

    feat: 컨트롤러 정의
    - 입출력 뷰를 주입받은 컨트롤러 정의
    - 기본 호출 메서드 run 정의

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.input.ConsoleInputView;
+import lotto.view.input.InputView;
+import lotto.view.output.ConsoleOutputView;
+import lotto.view.output.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new ConsoleInputView();
+        OutputView outputView = new ConsoleOutputView();
+        LottoController controller = new LottoController(inputView, outputView);
+
+        controller.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,19 @@
+package lotto.controller;
+
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        
+    }
+}

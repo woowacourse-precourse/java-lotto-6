76e9bd6bb2b8ece1ce01018c7ce4e769a7f7083b
Younggwan <kimyogun78@naver.com>
2023-11-04 01:14:29 +0900

    feat: 로또게임의 결과 출력을 위한 View의 메소드 추가

--- src/main/java/lotto/Controller/MainController.java
+++ src/main/java/lotto/Controller/MainController.java
@@ -3,21 +3,24 @@ package lotto.Controller;
 import lotto.Model.Lotto;
 import lotto.Model.Lottos;
 import lotto.Model.UserLotto;
+import lotto.View.PrintView;
 
 import java.util.Iterator;
 
 public class MainController {
     InputController inputController;
     LottoGeneratorController generatorController;
+    PrintView view;
 
     public MainController(){
         inputController = new InputController();
         generatorController = new LottoGeneratorController();
+        view = new PrintView();
     }
 
     public int[] lottoCheck(Lottos lottos, UserLotto user_lotto){
         // 5, 4, 3, 2, 1등 순
-        int[] rank = {0, 0, 0, 0, 0};
+        int[] rank_cnt = {0, 0, 0, 0, 0};
 
         // 당첨번호 갯수 추출
         Iterator<Lotto> it = lottos.getLottos().iterator();
@@ -26,17 +29,17 @@ public class MainController {
             int cnt = countEqualNumber(lotto, user_lotto);
 
             if(cnt >= 3 && cnt != 5){
-                rank[cnt - 3]++;
+                rank_cnt[cnt - 3]++;
             }else if(cnt == 5){
                 // 보너스번호가 일치하는가?
                 if(lotto.getNumbers().contains(user_lotto.getNumbers().indexOf(user_lotto.getNumbers().size() - 1))){
-                    rank[3]++;
+                    rank_cnt[3]++;
                 }else
-                    rank[2]++;
+                    rank_cnt[2]++;
             }
         }
 
-        return rank;
+        return rank_cnt;
     }
 
     public int countEqualNumber(Lotto lotto, UserLotto user_lotto){
@@ -54,13 +57,29 @@ public class MainController {
         return cnt;
     }
 
+    public float getRevenue(int[] rank_cnt, int[] winner_price, int money){
+        float result = 0;
+        int price = 0;
+
+        for(int i = 0; i < rank_cnt.length; i++){
+            if(rank_cnt[i] > 0)
+                price += winner_price[i];
+        }
+
+        result = (float)price / money * 100;
+
+        return result;
+    }
+
     public void lottoStart(){
+        int[] winnings = {5000, 50000, 1500000, 30000000, 2000000000};
         int money = inputController.inputMoney();
         Lottos lottos = generatorController.lottoGenerate(money);
         UserLotto user_lotto = inputController.inputNumbers();
-        int[] rank = lottoCheck(lottos, user_lotto);
-        for(int i = 0; i < rank.length; i++)
-            System.out.println(rank[i]);
+        int[] rank_cnt = lottoCheck(lottos, user_lotto);
+        float rate = getRevenue(rank_cnt, winnings, money);
+        view.resultPrint(rank_cnt, rate);
+
     }
 
 
--- src/main/java/lotto/View/PrintView.java
+++ src/main/java/lotto/View/PrintView.java
@@ -38,8 +38,25 @@ public class PrintView {
         System.out.println("보너스 번호를 입력해주세요");
     }
 
-    public void resultPrint(){
+    public void resultPrint(int[] rank, float rate){
+        System.out.println("당첨 통계");
+        System.out.println("---");
 
+        String[] str = {
+                "3개 일치 (5,000원) - ",
+                "4개 일치 (50,000원) - ",
+                "5개 일치 (1,500,000원) - ",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+                "6개 일치 (2,000,000,000원) - "
+        };
+
+        // 5, 4, 3, 2, 1등(3개, 4개, 5개, 5개 보너스일치, 6개 일치)
+        for(int i = 0; i < rank.length; i++)
+            System.out.println(str[i] + rank[i] + "개");
+
+        String result_str = "총 수익률은 " + String.format("%.1f", rate);
+        System.out.print(result_str);
+        System.out.print("%입니다.");
     }
 
 }

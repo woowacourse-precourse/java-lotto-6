1d25425534e9a89897b0942f6b011ff856a982a0
minseo  jo <liging12@naver.com>
2023-11-03 20:52:11 +0900

    feat: 로또 수동 생성기 클래스 추가
    당첨 번호를 뽑을 때 사용할 예정

---
+++ src/main/java/lotto/domain/LottoManualGenerator.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import lotto.exception.LottoNumberException;
+
+public class LottoManualGenerator implements LottoGenerator{
+    @Override
+    public Lotto generate() throws LottoNumberException {
+        return null;
+    }
+
+    @Override
+    public Lottos generate(int count) throws LottoNumberException {
+        return null;
+    }
+}

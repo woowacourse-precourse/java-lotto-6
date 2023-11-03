62104b5060465625cf7f0cb99aa464f0f7eb3d3e
Reddyong <d0209hong@naver.com>
2023-11-03 17:19:41 +0900

    feat : create printLottoCount()

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,23 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     private static int inMoney;
+    private static int count;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         // 로또 구입 금액 입력
         lottoMoney();
+        // 로또 수량 출력
+        printLottoCount();
+    }
 
+    public static void printLottoCount() {
+        count = inMoney / 1000;
+        System.out.println(count + "개를 구매했습니다.");
     }
 
     public static void lottoMoney() {

07a90d4132b6efda27eb5aad187ab8801bfea797
hlmg <devhlmg@gmail.com>
2023-11-04 03:55:54 +0900

    feat: 로또 생성 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,7 +15,27 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-    }
 
-    // TODO: 추가 기능 구현
+        HashSet<Integer> distinctNumbers = new HashSet<>();
+        for (Integer number : numbers) {
+            if (distinctNumbers.add(number)) {
+                continue;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        if (numbers.stream().anyMatch(number -> number < 1)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (numbers.stream().anyMatch(number -> number > 45)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
+/*
+제공된 Lotto 클래스를 활용해 구현해야 한다.
+numbers의 접근 제어자인 private을 변경할 수 없다.
+Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
+Lotto의 패키지 변경은 가능하다.
+ */
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,11 @@
 package lotto;
 
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -18,10 +19,34 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1보다 작은 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderMinNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 45보다 큰 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOverMaxNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호를 생성할 수 있다.")
+    @Test
+    void createLotto() throws Exception {
+        Field numbersField = Lotto.class.getDeclaredField("numbers");
+        numbersField.setAccessible(true);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        Lotto lotto = new Lotto(numbers);
+
+        Object actual = numbersField.get(lotto);
+        assertThat(actual).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
 }

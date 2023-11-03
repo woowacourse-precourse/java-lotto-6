c896c965ecdbe120127ce41b87fd6a878f1d59e8
Genius-PAK <pyj789123@gmail.com>
2023-11-03 22:03:19 +0900

    feat(MaginNumber): 프로젝트 전반에서 사용되는 매직 넘버를 관리하는 클래스 추가

---
+++ src/main/java/lotto/MagicNumber.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class MagicNumber {
+    public static final int UNIT_PURCHASE_PRICE = 1000;
+    public static final int VALID_LOTTO_NUMBER_COUNT = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+}

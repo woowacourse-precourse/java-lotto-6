6c5cd8878dc950a5d353234663486706a5059cfe
euics <pjh575157@gmail.com>
2023-11-03 14:57:30 +0900

    test: whenOnlyBonusNumberMatches_thenReturnNoneRank 메서드 구현
    - 보너스 번호만 일치하는 경우 NONE 등수 반환
    - 당첨 번호 중 어느 것도 일치하지 않지만 보너스 번호가 일치하는 경우에도 NONE을 반환하는지 테스트

--- src/test/java/lotto/domain/WinningLottoTicketTest.java
+++ src/test/java/lotto/domain/WinningLottoTicketTest.java
@@ -86,4 +86,17 @@ class WinningLottoTicketTest {
 
         assertEquals(Rank.FIFTH, result);
     }
+
+    @Test
+    @DisplayName("보너스 번호만 일치하는 경우 NONE 등수 반환")
+    void whenOnlyBonusNumberMatches_thenReturnNoneRank() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> playerNumbers = Arrays.asList(7, 8, 9, 10, 11, 12);
+        WinningLottoTicket winningLottoTicket = new WinningLottoTicket(winningNumbers, 7);
+        Lotto lotto = new Lotto(playerNumbers);
+
+        Rank result = winningLottoTicket.match(lotto);
+
+        assertEquals(Rank.NONE, result);
+    }
 }

33600b8ddd5a7be7072f472a8d6f5e14b9a2bdde
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 20:43:42 +0900

    test(PriceTest): 로또구입금액 입력 값 검증 테스트

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -6,12 +6,55 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PriceTest {
+    private Price price;
 
-    @DisplayName("1000원 단위로 잘 나눠지는지 확인")
+    @DisplayName("Price는 숫자로만 이루어져야 한다.")
     @Test
-    void 천원단위로나누기() {
-        Price price = new Price(5000);
+    void isNumber1() {
+        String input = "5000원";
 
-        Assertions.assertThat(price.divideWithOneThousands()).isEqualTo(5);
+        Assertions.assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자만을 입력하세요.(최대 구입금액은 20억입니다)");
+    }
+
+    @DisplayName("Price가 20억~int형 타입 사이일 때 테스트")
+    @Test
+    void isNumber2() {
+        String input = "2000000001";
+
+        Assertions.assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또구입금액은 최대 20억입니다.");
+    }
+
+    @DisplayName("Price가 int타입 넘어갈 때 테스트")
+    @Test
+    void isNumber3() {
+        String input = "4000000001";
+
+        Assertions.assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자만을 입력하세요.(최대 구입금액은 20억입니다)");
+    }
+
+    @DisplayName("Price가 1000원 단위로 떨어지지 않을 때")
+    @Test
+    void divideWithOneThousands() {
+        String input = "1500";
+
+        Assertions.assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1000원 단위로 입력하세요.");
+    }
+
+    @DisplayName("Price가 0원 이하일 때")
+    @Test
+    void isCorrectRange() {
+        String input = "0";
+
+        Assertions.assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또구입금액은 최소 1000원입니다.");
     }
 }

77ff8d5cc5feca6283456c81648cc35e4d49191b
gusdn0108 <gusdn6671@naver.com>
2023-11-03 22:40:50 +0900

    feat(Lotties): Lotto 리스트 일급컬렉션 추가

---
+++ src/main/java/lotto/domain/Lotties.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotties {
+    private final List<Lotto> lotties;
+
+    public Lotties(List<Lotto> lotties) {
+        this.lotties = lotties;
+    }
+
+
+
+}
+
+

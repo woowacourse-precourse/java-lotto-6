a2f0b5a548f25753740819a5fa3f75864ad747b6
newh08 <newh08@gmail.com>
2023-11-03 09:12:39 +0900

    ☑️TEST: UsedMoneyTest 에 DisplayName 추가

--- src/test/java/lotto/domain/player/playermoney/UsedMoneyTest.java
+++ src/test/java/lotto/domain/player/playermoney/UsedMoneyTest.java
@@ -1,10 +1,12 @@
 package lotto.domain.player.playermoney;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UsedMoneyTest {
 
+    @DisplayName("사용하는 금액 만큼 usedMoney 가 업데이트 된다.")
     @Test
     void ConsumeMoneyTest() {
         UsedMoney usedMoney = new UsedMoney(0);

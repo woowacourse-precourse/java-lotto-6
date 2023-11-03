0842a20a5efb5e39b603f9ddd760b45c3a665caf
anneu <b.imnuj@gmail.com>
2023-11-03 09:18:17 +0900

    feat: 구입금액 입력 메세지를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,12 @@
 package lotto.controller;
 
+import lotto.view.Output;
+
 public class LottoController {
 
+    private final Output output = new Output();
+
     public void run() {
-        // TODO: 프로그램 구현
+        output.showMoneyInputMessage();
     }
 }
---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class Output {
+
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void showMoneyInputMessage() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+    }
+}

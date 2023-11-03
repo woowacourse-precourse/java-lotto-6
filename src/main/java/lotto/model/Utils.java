1e4a35cd0f8eac8763bc4ccc04d2690755d18150
leeedohyun <ldh19738@gmail.com>
2023-11-03 13:42:21 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/model/Utils.java
+++ src/main/java/lotto/model/Utils.java
@@ -1,8 +1,12 @@
 package lotto.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Utils {
 
     private static final String INVALID_PURCHASE_AMOUNT_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 숫자만 입력하실 수 있습니다.";
+    private static final String WINNING_NUMBER_SEPARATOR = ",";
 
     private Utils() {
     }
@@ -13,5 +17,17 @@ public final class Utils {
         } catch (NumberFormatException numberFormatException) {
             throw new NumberFormatException(INVALID_PURCHASE_AMOUNT_EXCEPTION_MESSAGE);
         }
+    }
+
+
+    public static List<Integer> convertStringToIntegerList(String string) {
+        return splitWinningNumbers(string).stream()
+                .map(String -> convertStringToInt(string))
+                .toList();
+    }
+
+    private static List<String> splitWinningNumbers(String string) {
+        return Arrays.stream(string.split(WINNING_NUMBER_SEPARATOR))
+                .toList();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,16 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.model.Utils;
 
 public class InputView {
 
     public int inputPurchaseAmount() {
         return Utils.convertStringToInt(Console.readLine());
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        return Utils.convertStringToIntegerList(Console.readLine());
     }
 }

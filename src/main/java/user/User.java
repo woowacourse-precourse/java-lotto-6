201e4904f40d60fc0503ecd00b7f8348d07ef679
happyryan <tjdgks120813@gmail.com>
2023-11-03 17:23:11 +0900

    feat(user User): add validation to check if the bonus number is within the range of 1 to 45 in the User class
    Implemented validation to check whether the bonus number is within the range of 1 to 45 in the User class.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -64,7 +64,7 @@ public class User {
             if (1 > Integer.parseInt(lottoNumber) || Integer.parseInt(lottoNumber) > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
-            if(!lottoNumber.matches("^[1-9][0-9]?$")){
+            if (!lottoNumber.matches("^[1-9][0-9]?$")) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 0으로 시작할 수 없습니다. (ex) 1,8,24,47,35,6");
             }
         }
@@ -82,24 +82,26 @@ public class User {
 
     public String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        while (true){
-            try{
+        while (true) {
+            try {
                 String bonusNumber = Console.readLine();
                 validateSingleNumberInput(bonusNumber);
                 return bonusNumber;
-            }
-            catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private void validateSingleNumberInput(String bonusNumber){
-        if(!bonusNumber.matches("^\\d+$")){
+    private void validateSingleNumberInput(String bonusNumber) {
+        if (!bonusNumber.matches("^\\d+$")) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자 1개만 입력해주세요. (ex) 7");
         }
-        if(!bonusNumber.matches("^[1-9][0-9]?$")){
+        if (!bonusNumber.matches("^[1-9][0-9]?$")) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 0으로 시작할 수 없습니다.  (ex) 7");
+        }
+        if (1 > Integer.parseInt(bonusNumber) || Integer.parseInt(bonusNumber) > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }

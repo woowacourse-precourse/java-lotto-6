e8d238f56d03d6c1f744678cc5f04cbe2bd34503
minjun7410 <minjun7410@gmail.com>
2023-11-03 15:16:02 +0900

    test(LottoTicketTest): 구입한 로또 수만큼 로또를 생성하는 기능 테스트 추가
    * 구입한 로또 수만큼 로또를 생성하는 기능 테스트를 생성했다.

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -5,9 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTicketTest {
+    @Test
+    void 구입한_로또수만큼_로또를_생성하는_기능_테스트() {
+        LottoTicket lottoTicket = new LottoTicket("3");
+        List<Lotto> lottos = lottoTicket.generateLottos();
+        assertThat(lottos.size()).isEqualTo(3);
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"d", "12d", "__", "*", "", " ", "10.1"})
     void 입력값_숫자_아닌값_예외처리_테스트(String input) {

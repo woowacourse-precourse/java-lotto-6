9d807a982c35ca649436980671b14c58c768b913
gumgu <bienlee@naver.com>
2023-11-03 15:31:22 +0900

    [Refactor] Lotto 클래스 패키지 위치 변경(to domain)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,11 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+
+    // TODO: 인스턴스 변수 추가 불가.
+    // TODO: 패키지 변경 가능.
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +13,11 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    // TODO: 접근제한자 private 변경 불가.
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
 }

a9ed388248111988b5a6d58cf0d0a936a7443329
chanwoo yang <moomsss@naver.com>
2023-11-03 20:49:42 +0900

    feat(inputcontroller): 유저 입력을 받고 오류가 있을 시 에러 표시를 나타내고 다시 입력값을 받도록 구현

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -2,15 +2,26 @@ package lotto.validation;
 
 
 import lotto.constants.Constants;
+import lotto.utils.Utils;
 
 public class InputValidation {
+    Utils utils = new Utils();
+    private int totalMoney;
 
-    public void checkRemainder(int money){
+    public int checkAll(String money) throws IllegalArgumentException{
+        checkInteger(money);
+        totalMoney = utils.makeStringToInteger(money);
+        checkRemainder(totalMoney);
+        return totalMoney;
+
+    }
+
+    public void checkRemainder (int money) throws IllegalArgumentException{
         if(money % Constants.LOTTO_PRICE != Constants.ZERO){
              throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요");
         }
     }
-    public void checkInteger(String money){
+    public void checkInteger (String money) throws IllegalArgumentException{
         if(!Constants.INTEGER.matcher(money).matches()){
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요");
         }

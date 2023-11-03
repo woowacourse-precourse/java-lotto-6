246ca4dc6482887246322b4ef4882b17fd29c5dd
yeseong31 <yeseong31@naver.com>
2023-11-03 10:55:34 +0900

    feat(SeparatorConstant): 출력 구분자 상수 정의

--- src/main/java/lotto/view/constants/ConsoleMessage.java
+++ src/main/java/lotto/view/constants/ConsoleMessage.java
@@ -14,10 +14,6 @@ public enum ConsoleMessage {
     LOTTO_NUMBER_MATCH_WITH_BONUS_NUMBER("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
     TOTAL_RETURN("총 수익률은 %.1f%%입니다."),
 
-    // separator
-    RESULT_SUMMARY_SEPARATOR("---"),
-    WINNING_NUMBERS_SEPARATOR(","),
-
     // error
     PURCHASE_AMOUNT_ERROR("[ERROR] 1000원 단위의 금액을 입력해 주세요."),
     WINNING_NUMBERS_ERROR("[ERROR] 1~45 사이의 숫자를 공백 없이 %s로 구분하여 입력해 주세요."),
---
+++ src/main/java/lotto/view/constants/SeparatorConstant.java
@@ -0,0 +1,11 @@
+package lotto.view.constants;
+
+public class SeparatorConstant {
+
+    public static final String RESULT_SUMMARY_SEPARATOR = "---";
+    public static final String WINNING_NUMBERS_SEPARATOR = ",";
+    public static final String LOTTO_NUMBERS_SEPARATOR = ", ";
+
+    private SeparatorConstant() {
+    }
+}

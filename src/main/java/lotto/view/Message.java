769537baf2a9fb31e78a38147a756ab02e145a77
kymin <park48664866@gmail.com>
2023-11-03 20:30:38 +0900

    feat: 메시지 출력 기능 구현
    메시지 출력 기능 구현:
    - 메시지 Enum클래스 구현
    - 메시지 출력 메서드 구현

---
+++ src/main/java/lotto/view/Message.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public enum Message {
+    INPUT_AMOUNT_MESSAGE("구입금액을 입력해 주세요.");
+    INPUT_WINNING_NUM_MESSAGE("당첨 번호를 입력해 주세요.");
+    INPUT_BONUS_NUM_MESSAGE("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+}

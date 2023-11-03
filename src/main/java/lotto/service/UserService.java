2ff1a4d9750d06329fefb07363a133ce01170b29
WOONBE <kepull2918@naver.com>
2023-11-03 18:32:39 +0900

    [feat] UserService에 상금 합계와 수익률을 계산하는 로직 추가

---
+++ src/main/java/lotto/service/UserService.java
@@ -0,0 +1,49 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.User;
+import lotto.repository.LottoRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserService {
+    private final LottoRepository lottoRepository = new LottoRepository();
+
+    private final User user = new User();
+    private final HashMap<Rank, Integer> lottoResult = new HashMap<>();
+
+    private static final int INIT_SUM = 0;
+    private static final int ROUND_CONSTANT_INT_TEN = 10;
+    private static final double ROUND_CONSTANT_DOUBLE_TEN = 10.0;
+    private static final double PERCENT = 100;
+
+
+    public void buyOneLotto(Lotto lotto){
+        lottoRepository.save(lotto);
+    }
+
+    //전체 로또 조회
+    public ArrayList<Lotto> getLotteries(){
+        return lottoRepository.getLotteries();
+    }
+
+    public HashMap<Rank, Integer> getLottoResult() {
+        return lottoResult;
+    }
+
+    public long sumOfPrize(){
+        long sum = INIT_SUM;
+        for(Rank rank : lottoResult.keySet()){
+            sum += rank.getPrize() * lottoResult.get(rank);
+        }
+        return sum;
+    }
+
+    public double getRateOfReturn(){
+        double origin = (sumOfPrize() / (double) user.getPurchaseAmount()) * PERCENT;
+        double round = Math.round(origin * ROUND_CONSTANT_INT_TEN) / ROUND_CONSTANT_DOUBLE_TEN;
+        return round;
+    }
+}

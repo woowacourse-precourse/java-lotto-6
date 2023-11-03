59e0a459644d3fcc1b7129d2a23f9d714b0589e7
현 <sohyun95277@gmail.com>
2023-11-03 13:03:51 +0900

    feat(view): 입출력 메세지 출력 기능
    입력 요청 메세지
    - 금액 입력 요청 메세지
    - 당첨 번호 입력 요청 메세지
    - 보너스 번호 입력 요청 메세지
    
    출력 메세지
    당첨 결과 메세지

---
+++ src/main/java/lotto/constant/message/OutputMessage.java
@@ -0,0 +1,9 @@
+package lotto.constant.message;
+
+public final class OutputMessage {
+    public static final String PURCHASE_COUNT_RESULT = "\n%d개를 구매했습니다.";
+    public static final String RESULT_MESSAGE = "\n당첨 통계\n---";
+    public static final String PRIZE_RESULT = "%d개 일치 (%s원) - %d개";
+    public static final String BONUS_RESULT = "5개 일치, 보너스 볼 일치 (%s원) - %d개";
+    public static final String PROFIT_RATE = "총 수익률은 %f입니다.";
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import static lotto.constant.message.OutputMessage.RESULT_MESSAGE;
+
+public class OutputView {
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+}

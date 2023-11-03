1f251942c87b1245ac97e312c41a78589232b970
YongHyeon <pyh4616@naver.com>
2023-11-03 22:42:07 +0900

    Test(BonusNumberTest 클래스): 보너스 번호를 당첨 번호와 비교하여 중복에 대한 유효성 검사를 하는 기능을 테스트

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -28,4 +28,11 @@ public class BonusNumberTest {
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByDuplicateWithWinningNumbers() {
+        assertThatThrownBy(() -> new BonusNumber(3, List.of(1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

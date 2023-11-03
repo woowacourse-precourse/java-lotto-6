591d0f21f11dd675844b5f1f132dd43a38c7bc37
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-03 13:50:54 +0900

    add : NumberValidator test : NumberValidatorTest
    타입 검증을 위한 NumberValidator.validateNumberType 구현,
    문자열 포함, 공백 포함, 특수문자 포함, null 경우의 테스트 진행

--- src/test/java/lotto/validator/NumberValidatorTest.java
+++ src/test/java/lotto/validator/NumberValidatorTest.java
@@ -3,6 +3,7 @@ package lotto.validator;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.exception.IllegalAmountException;
+import lotto.exception.IllegalNumberTypeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,10 @@ class NumberValidatorTest {
 
     private static final Integer UNDER_MIN_VALUE = 500;
     private static final Integer MIXED_VALUE = 5500;
+    private static final String NUMBERS_CONTAIN_TEXT = "1000S";
+    private static final String NUMBERS_CONTAIN_SPACE = "10 00";
+    private static final String NUMBERS_CONTAIN_SPECIAL_TEXT = "10#00";
+
 
     @DisplayName("단위 금액이 1000원 이하라면 예외를 발생한다.")
     @Test
@@ -18,5 +23,17 @@ class NumberValidatorTest {
                 .isInstanceOf(IllegalAmountException.class);
         assertThatThrownBy(()->NumberValidator.validateMinimumAmount(MIXED_VALUE))
                 .isInstanceOf(IllegalAmountException.class);
+    }
+    @DisplayName("입력된 문자열이 숫자가 아니면 예외를 발생한다.")
+    @Test
+    void validateNumberType() {
+        assertThatThrownBy(()->NumberValidator.validateNumberType(NUMBERS_CONTAIN_TEXT))
+                .isInstanceOf(IllegalNumberTypeException.class);
+        assertThatThrownBy(()->NumberValidator.validateNumberType(NUMBERS_CONTAIN_SPACE))
+                .isInstanceOf(IllegalNumberTypeException.class);
+        assertThatThrownBy(()->NumberValidator.validateNumberType(NUMBERS_CONTAIN_SPECIAL_TEXT))
+                .isInstanceOf(IllegalNumberTypeException.class);
+        assertThatThrownBy(()->NumberValidator.validateNumberType(null))
+                .isInstanceOf(IllegalNumberTypeException.class);
     }
 }

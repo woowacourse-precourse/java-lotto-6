5f8ae51f5b5f896266e13f044a4c786c6eab4c8a
peppone-choi <peppone.choi@gmail.com>
2023-11-04 01:29:47 +0900

    feat: Amount 클래스 구현

--- src/main/java/lotto/enumerate/ErrorCode.java
+++ src/main/java/lotto/enumerate/ErrorCode.java
@@ -4,7 +4,9 @@ public enum ErrorCode {
     LOTTO_NUMBER_OVER_OR_UNDER_SIZE("[ERROR] 1장의 로또 번호의 갯수는 6개여야 합니다."),
     LOTTO_NUMBER_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다."),
     LOTTO_NUMBER_BONUS_DUPLICATE("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."),
-    LOTTO_NUMBER_UNDER_OR_OVER("[ERROR] 로또 번호는 1 ~ 45 사이여야 합니다.");
+    LOTTO_NUMBER_UNDER_OR_OVER("[ERROR] 로또 번호는 1 ~ 45 사이여야 합니다."),
+    AMOUNT_UNIT_IS_NOT_1000_WON("[ERROR] 로또 구입 금액은 1000원 단위여야 합니다."),
+    AMOUNT_IS_UNDER_ZERO_WON("[ERROR] 로또 구입 금액은 음수이거나 0이 될 수 없습니다.");
 
     ErrorCode(String message) {
     }
---
+++ src/main/java/lotto/model/Amount.java
@@ -0,0 +1,46 @@
+package lotto.model;
+
+import static lotto.enumerate.ErrorCode.AMOUNT_IS_UNDER_ZERO_WON;
+import static lotto.enumerate.ErrorCode.AMOUNT_UNIT_IS_NOT_1000_WON;
+import static lotto.util.ExceptionCodeThrow.exceptionCodeThrow;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Amount {
+    private final int amount;
+
+    public Amount(int amount) {
+        validAmount(amount);
+        this.amount = amount;
+    }
+
+    public List<Lotto> buyLotto(int amount) {
+        return getLottos(amount);
+    }
+
+    private ArrayList<Lotto> getLottos(int amount) {
+        ArrayList<Lotto> list = new ArrayList<>();
+        int lottoVolume = amount / 1000;
+        getLottoList(lottoVolume, list);
+        return list;
+    }
+
+    private void getLottoList(int lottoVolume, ArrayList<Lotto> list) {
+        for (int i = 0; i < lottoVolume; i++) {
+            List<Integer> numbers = Randoms
+                    .pickUniqueNumbersInRange(1, 45, 6);
+            list.add(new Lotto(numbers));
+        }
+    }
+
+    private void validAmount(int amount) {
+        if (amount % 1000 != 0) {
+            exceptionCodeThrow(AMOUNT_UNIT_IS_NOT_1000_WON);
+        }
+        if (amount <= 0) {
+            exceptionCodeThrow(AMOUNT_IS_UNDER_ZERO_WON);
+        }
+    }
+}

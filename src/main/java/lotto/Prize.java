0d66212a7c0c1662fda0e077ff9d734213da5b9b
Youngwoo Choi <allnstar@naver.com>
2023-11-03 17:54:02 +0900

    feat: add a function for cutting numbers

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -2,10 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Prize {
     private String groupNumbers;
+    private List<Integer> preLuckyNumber;
+    public Lotto luckyNumber;
 
     void inputNumbers() {
         groupNumbers = Console.readLine();
+    }
+
+    void cutNumbers() {
+        String[] separatedNumbers = groupNumbers.split(",");
+
+        for (int i = 0; i < separatedNumbers.length; i++) {
+            preLuckyNumber.add(Integer.parseInt(separatedNumbers[i]));
+        }
+
+        luckyNumber = new Lotto(preLuckyNumber);
     }
 }

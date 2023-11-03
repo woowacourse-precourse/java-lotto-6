e66ba8b7958a1e4551a6e15c21447f3306dc69f8
Jiwon Na <jiwonna52@gmail.com>
2023-11-03 23:33:52 +0900

    Feat : 입력받은 당첨 숫자를 정수형 리스트로 후처리하는 기능 추가

--- src/main/java/lotto/controller/InputConverter.java
+++ src/main/java/lotto/controller/InputConverter.java
@@ -2,12 +2,22 @@ package lotto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class InputConverter {
     public static int convertPrice(String inputPrice) {
         int price = Integer.parseInt(inputPrice);
         return price;
+    }
+
+    public static List<Integer> convertWinningNumber(String inputWinningNumber) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(inputWinningNumber, ",");
+        while(st.hasMoreTokens()) {
+            int winningNumber = Integer.parseInt(st.nextToken());
+            winningNumbers.add(winningNumber);
+        }
+        return winningNumbers;
     }
 
 }

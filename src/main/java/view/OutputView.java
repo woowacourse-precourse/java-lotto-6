001909cc234df4c11073c101aae1d947f10b53d1
WonyuChoi <phillip07290@naver.com>
2023-11-03 15:00:46 +0900

    feat: 게임 초기화 과정에 필요한 출력문 메서드

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,4 +2,13 @@ package view;
 
 public class OutputView {
 
+    public void askPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void informLottoCount(final int count) {
+        System.out.println();
+        System.out.printf("%d개를 구매했습니다.", count);
+        System.out.println();
+    }
 }

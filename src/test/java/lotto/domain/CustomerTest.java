ce4994bd8e4d6bf2a217dcaf70c6c0f86fb7edc4
suihub <qkdcrt@gmail.com>
2023-11-04 01:43:45 +0900

    <test(CustomerTest)> : "투입 금액 검증 테스트 추가 구현"
    - 1000원보다 적은 금액일 경우에도 예외를 반환하는 테스트 구현

--- src/test/java/lotto/domain/CustomerTest.java
+++ src/test/java/lotto/domain/CustomerTest.java
@@ -16,8 +16,16 @@ class CustomerTest {
 
     @DisplayName("로또 구매 금액이 100,000원을 넘어선 안 된다.")
     @Test
-    void createCustomerByOverMoney() {
+    void createCustomerByOverRange() {
         assertThatThrownBy(() -> new Customer("106000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 최소 1000원부터 최대 100,000원까지의 금액을 입력해주세요.");
+    }
+
+    @DisplayName("로또 구매 금액이 1000원보다 적어선 안 된다.")
+    @Test
+    void createCustomerByUnderRange() {
+        assertThatThrownBy(() -> new Customer("200"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 최소 1000원부터 최대 100,000원까지의 금액을 입력해주세요.");
     }

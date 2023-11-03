4deb15799bc6c17ee9baa318ea75f78d309e0009
KimDabomi <cdabomi@nate.com>
2023-11-03 17:53:56 +0900

    test: showPrizeResult

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -43,8 +43,8 @@ class OutputViewTest {
     }
 
     @Test
-    @DisplayName("당첨내역 출력 확인")
-    void 당첨내역_출력_확인() {
+    @DisplayName("당첨내역 출력 확인 - 5000원")
+    void 당첨내역_3개_일치_출력_확인() {
         Lotto winningNumbers = new Lotto(List.of(2, 4, 6, 8, 10, 12));
         int bonusNumber = 10;
         WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
@@ -58,6 +58,82 @@ class OutputViewTest {
                 "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
                 "6개 일치 (2,000,000,000원) - 0개\n" +
                 "총 수익률은 500.0%입니다.\n";
+        assertThat(byteArrayOutputStream.toString()).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    @DisplayName("당첨내역 출력 확인 - 50,000원")
+    void 당첨내역_4개_일치_출력_확인() {
+        Lotto winningNumbers = new Lotto(List.of(2, 4, 6, 8, 10, 1));
+        int bonusNumber = 10;
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        int purchaseAmount = 1000;
+        outputView.showPrizeResult(lottos, winningLotto, purchaseAmount);
+        String expectedOutput = "\n당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 1개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n" +
+                "총 수익률은 5000.0%입니다.\n";
+        assertThat(byteArrayOutputStream.toString()).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    @DisplayName("당첨내역 출력 확인 - 1,500,000원")
+    void 당첨내역_5개_일치_출력_확인() {
+        Lotto winningNumbers = new Lotto(List.of(2, 4, 6, 8, 3, 1));
+        int bonusNumber = 10;
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        int purchaseAmount = 1000;
+        outputView.showPrizeResult(lottos, winningLotto, purchaseAmount);
+        String expectedOutput = "\n당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 1개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n" +
+                "총 수익률은 150000.0%입니다.\n";
+        assertThat(byteArrayOutputStream.toString()).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    @DisplayName("당첨내역 출력 확인 - 30,000,000원")
+    void 당첨내역_5개_보너스_일치_출력_확인() {
+        Lotto winningNumbers = new Lotto(List.of(2, 4, 6, 8, 3, 1));
+        int bonusNumber = 1;
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        int purchaseAmount = 1000;
+        outputView.showPrizeResult(lottos, winningLotto, purchaseAmount);
+        String expectedOutput = "\n당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n" +
+                "총 수익률은 3000000.0%입니다.\n";
+        assertThat(byteArrayOutputStream.toString()).isEqualTo(expectedOutput);
+    }
+
+    @Test
+    @DisplayName("당첨내역 출력 확인 - 2,000,000,000원")
+    void 당첨내역_6개_일치_출력_확인() {
+        Lotto winningNumbers = new Lotto(List.of(2, 4, 6, 5, 3, 1));
+        int bonusNumber = 10;
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        int purchaseAmount = 1000;
+        outputView.showPrizeResult(lottos, winningLotto, purchaseAmount);
+        String expectedOutput = "\n당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 1개\n" +
+                "총 수익률은 200000000.0%입니다.\n";
         assertThat(byteArrayOutputStream.toString()).isEqualTo(expectedOutput);
     }
 }

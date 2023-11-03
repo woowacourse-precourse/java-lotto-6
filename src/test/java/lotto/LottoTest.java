75693944a3f962b16ed0d90165000aab54eea198
zangsu <zangsu_@naver.com>
2023-11-03 19:14:50 +0900

    test : test Lotto.toString()
    test for :
    - all numbers smaller than 10
    - some numbers smaller than 10, some numbers bigger than 10
    - all number bigger than 10

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,16 @@
 package lotto;
 
+import java.util.stream.Stream;
 import lotto.model.Lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -26,16 +31,23 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
-    @Test
-    public void toStringTest() throws Exception{
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-
-        Assertions.assertThat(lotto.toString())
-                .isEqualTo("[1, 2, 3, 4, 5, 6]");
+    @ParameterizedTest
+    @MethodSource("toStringTestArgs")
+    public void toStringTest(List<Integer> lottoNumbers, String lottoString) throws Exception{
         //given
+        Lotto lotto = new Lotto(lottoNumbers);
 
         //when
-
         //then
+        Assertions.assertThat(lotto.toString())
+                .isEqualTo(lottoString);
+    }
+
+    private static Stream<Arguments> toStringTestArgs(){
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), "[1, 2, 3, 4, 5, 6]"),
+                Arguments.of(List.of(1, 5, 12, 15, 21, 25), "[1, 5, 12, 15, 21, 25]"),
+                Arguments.of(List.of(12, 15, 20, 25, 31, 42), "[12, 15, 20, 25, 31, 42]")
+        );
     }
 }

625d7035bc97bc081febea6ce49b6340fb76444d
Hoo-sung <gntjd135@gmail.com>
2023-11-03 14:44:41 +0900

    feat(MoneyVerifier): 로또 구입금액이 숫자인지 검증하는 checkNumeric 메서드 생성

--- src/main/java/verifier/MoneyVerifier.java
+++ src/main/java/verifier/MoneyVerifier.java
@@ -3,6 +3,15 @@ package verifier;
 public class MoneyVerifier implements Verifier{
     @Override
     public void check(String input) {
-
+        checkNumeric(input);
     }
+
+    private void checkNumeric(String input){
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("로또 구입 금액이 숫자가 아닙니다.");
+        }
+    }
+
 }

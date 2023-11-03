1f41fee5e9989a45c0537861a47974925c79a704
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 00:32:22 +0900

    test(InputValidatorTest): 숫자로 입력하면 정상

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("입력 검증 객체에 대해")
@@ -21,4 +22,13 @@ class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(error);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "12", "1"})
+    @DisplayName("숫자로 입력하면 정상적이다.")
+    void When_InputNumeric_Then_Error(String number) {
+        assertThatCode(() -> inputValidator.validateNumeric(number))
+                .doesNotThrowAnyException();
+    }
+
 }

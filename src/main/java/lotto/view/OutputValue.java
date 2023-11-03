c01a361ae5cfe0aa98c29ca5c58c7c9b4008cb55
GiJung <weeds2577@gmail.com>
2023-11-03 19:05:13 +0900

    refactor(OutputValue): PURCAHSE_AMOUT_MESSAGE에서 PURCHASE_PRICE_MESSAGE로 변수명 변경

--- src/main/java/lotto/view/OutputValue.java
+++ src/main/java/lotto/view/OutputValue.java
@@ -2,10 +2,10 @@ package lotto.view;
 
 public class OutputValue {
 
-    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
 
     public static void purchaseMessage() {
-        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        System.out.println(PURCHASE_PRICE_MESSAGE);
     }
 
 }

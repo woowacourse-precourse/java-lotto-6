458477e089054b7d49d7f1111826f0ff7095b68e
Firedrago95 <yonghwa95@naver.com>
2023-11-03 15:55:52 +0900

    feat(LottoTicket) : 로또 티켓 문자열 반환 기능 구현

---
+++ src/main/java/lotto/Domain/LottoTicket.java
@@ -0,0 +1,30 @@
+package lotto.Domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.Util.NumbersGenerator;
+
+
+public class LottoTicket {
+
+    private final List<Lotto> lottos;
+
+    public LottoTicket(int lottoCount) throws IllegalArgumentException{
+        lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = NumbersGenerator.generateRandomNumbers();
+            lottos.add(new Lotto(numbers));
+        }
+    }
+
+    public int getSize() {
+        return lottos.size();
+    }
+
+    public List<String> getLottoString() {
+        return lottos.stream()
+                .map(lotto -> lotto.toString())
+                .collect(Collectors.toList());
+    }
+}

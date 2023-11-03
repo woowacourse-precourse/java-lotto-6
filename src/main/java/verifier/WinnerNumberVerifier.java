38f49abac220379d9599d8eb714aef338c7fa0ec
Hoo-sung <gntjd135@gmail.com>
2023-11-03 16:02:56 +0900

    feat(WinnerNumberVerifier): 각각의 로또 번호가 숫자인지 검증하는 메서드 생성

--- src/main/java/verifier/WinnerNumberVerifier.java
+++ src/main/java/verifier/WinnerNumberVerifier.java
@@ -3,6 +3,17 @@ package verifier;
 public class WinnerNumberVerifier implements Verifier{
     @Override
     public void check(String input) {
+        checkEachNumeric(input);
+    }
 
+    private void checkEachNumeric(String input){
+        String[] numbers = input.split(",");
+        for(String number : numbers){
+            try{
+                Integer.parseInt(number);
+            }catch(Exception e){
+                throw new IllegalArgumentException("입력된 값은 숫자가 아닙니다.");
+            }
+        }
     }
 }

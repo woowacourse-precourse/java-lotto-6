9da4420cb3fdfda2fbe0206495e7407445831cc1
unknown <y2kf9a0q@gmail.com>
2023-11-03 18:50:17 +0900

    feat(lotto): Game.java - 스켈레톤 코드 작성

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,8 +1,28 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Game {
-    Player player;
-    Lotto winningLotto;
+    private Player player;
+    private Lotto winningLotto;
 
+    public void joinPlayer() {
 
+    }
+
+    public void drawLotto() {
+
+    }
+
+    private int inputMoney() {
+        return 0;
+    }
+
+    private List<Integer> inputWinningNumbers() {
+        return null;
+    }
+
+    private int inputBonusNumber() {
+        return 0;
+    }
 }

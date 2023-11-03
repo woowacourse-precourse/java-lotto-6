826473717a6084e5dc09b0114260b47511d3b587
eora21 <eora21@naver.com>
2023-11-03 22:36:56 +0900

    refactor: 💡 수정 로또 최소 번호, 최대 번호 public으로 변경

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -3,8 +3,8 @@ package domain;
 import exception.OutOfLottoNumberRangeException;
 
 public record LottoNumber(int lottoNumberValue) {
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
     public LottoNumber {
         if (isNotInLottoNumberRange(lottoNumberValue)) {
             throw new OutOfLottoNumberRangeException(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);

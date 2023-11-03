b85bb4baeaa5092bcc9a3644c18f0a7345a70a63
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-04 00:14:50 +0900

    feat: NumberConstant에 로또 가격 상수 추가

--- src/main/java/lotto/domain/constant/NumberConstant.java
+++ src/main/java/lotto/domain/constant/NumberConstant.java
@@ -5,6 +5,7 @@ public class NumberConstant {
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_NUMBER_START = 1;
     public static final int LOTTO_NUMBER_END = 45;
+    public static final int LOTTO_PRICE = 1000;
 
     // TEST CASE
     public static final int TEST_TICKET_NUM = 5;

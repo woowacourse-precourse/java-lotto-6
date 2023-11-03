6d84bebfd94abbd7187a05c6a8b160f6ccf8e7c6
KimDongEon <gold3987@naver.com>
2023-11-03 16:11:36 +0900

    feat(Store) : 로또 발행 구현
    구입 금액 만큼 로또 발행
    - giveLotto() : 구입 금액만큼 로또 발행
    - makeLottoNumber() : 로또 숫자 6개 발행

--- src/main/java/controller/Store.java
+++ src/main/java/controller/Store.java
@@ -2,6 +2,7 @@ package controller;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ public class Store {
 
   public void start() {
     int money = inputMoney();
+    List<Lotto> lottos = giveLotto(money);
 
 
   }
@@ -23,10 +25,27 @@ public class Store {
     return money;
   }
 
+
   public void validateMoney(int money) {
     if(money % 1000 != 0) {
       throw new IllegalArgumentException();
     }
+  }
+
+  public List<Lotto> giveLotto(int money) {
+    int count = money / 1000;
+    System.out.println("\n" + count + "개를 구매했습니다.");
+    List<Lotto> lottos = new ArrayList<>();
+    for(int i = 0; i < count; i++) {
+      List<Integer> number = makeLottoNumber();
+      System.out.println(number);
+      lottos.add(new Lotto(number));
+    }
+    return lottos;
+  }
+
+  public List<Integer> makeLottoNumber() {
+    return Randoms.pickUniqueNumbersInRange(1, 45, 6);
   }
 
 

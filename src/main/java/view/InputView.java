7060df6983314115e4cc2c80507a77cec53dc2d2
song-jae-wan <sjo9810@gmail.com>
2023-11-03 11:54:24 +0900

    feat: InputView 메소드 선언
    - 금액 입력받는 메소드
    - 당첨번호 입력받는 메소드
    - 보너스 번호 입력받는 메소드

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,32 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import config.OutputMessage;
+
 import controller.GameController;
 
 public class InputView {
 
-    public InputView() {
     private final GameController gameController;
+
     public InputView(GameController gameController) {
         this.gameController = gameController;
     }
+
+    public void inputPayment() {
+        System.out.println(OutputMessage.DEPOSIT.getMessage());
+        gameController.lottoAdd(Console.readLine());
     }
 
+    public void inputWinNumber() {
+        System.out.println(OutputMessage.WIN_NUMBER.getMessage());
+        gameController.winningLottoAdd(Console.readLine());
+    }
 
+    public void inputBonusNumber() {
+        System.out.println(OutputMessage.BONUS_NUMBER.getMessage());
+        gameController.BonusLottoAdd(Console.readLine());
+    }
 
 }

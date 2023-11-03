a848c9b076613e02f15fd27cc71970f8bfa1f27c
SeungjunNA <nsjjsng1@gmail.com>
2023-11-03 17:45:55 +0900

    feat : 매칭 결과와 수익률 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import lotto.domain.Enum.Rank;
 import lotto.gameUtil.GameLogic;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,6 +16,7 @@ public class Application {
         OutputView outputView = new OutputView();
         GameLogic gameLogic = new GameLogic();
 
+        Double prize = 0D;
         outputView.amountMessage(); // 구매금액 입력
         int amount = inputView.inputAmount();
 
@@ -26,9 +29,19 @@ public class Application {
         int bonusNumber = inputView.bonusNumber();
 
         for(int i=0;i<lottoList.size();i++){  // 당첨 확인
-            gameLogic.lottoNumberCompare(lottoList.get(i), winningLotto, bonusNumber);
+            int count = gameLogic.matchCount(lottoList.get(i), winningLotto, bonusNumber);
+            boolean isSecond = gameLogic.matchBonus(lottoList.get(i), bonusNumber);
+            gameLogic.matchLotto(count, isSecond);
         }
+        Map<Rank, Integer> ranks = gameLogic.getRanks();
+        prize += ranks.get(Rank.FIRST) * Rank.FIRST.getAmount();
+        prize += ranks.get(Rank.SECOND) * Rank.SECOND.getAmount();
+        prize += ranks.get(Rank.THIRD) * Rank.THIRD.getAmount();
+        prize += ranks.get(Rank.FORTH) * Rank.FORTH.getAmount();
+        prize += ranks.get(Rank.FIFTH) * Rank.FIFTH.getAmount();
 
-
+        outputView.matchingMessage(ranks);
+        System.out.println(prize);
+        System.out.println("총 수익률은 " + (prize * 100) / amount + "%입니다.");
     }
 }

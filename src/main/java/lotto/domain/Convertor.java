a4f942f9625d23165d869759f3c92718d7608514
sangchu <didi1484@gmail.com>
2023-11-03 23:28:44 +0900

    feat: validateDuplicated 메서드 추가 (중복된 숫자 입력시 예외 발생)

--- src/main/java/lotto/domain/Convertor.java
+++ src/main/java/lotto/domain/Convertor.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
-import java.util.Arrays;
+import static java.util.Arrays.stream;
+import static lotto.domain.constants.ExceptionMessage.IS_DUPLICATED;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,15 +10,26 @@ public class Convertor {
     private static final String DELIMITER = ",";
 
     public List<Number> convertToNumbers(String input) {
-        List<Number> numbers = Arrays.stream(separate(input))
+        List<Number> numbers = stream(separate(input))
                 .map(number -> new Number(number.trim()))
                 .collect(Collectors.toList());
         return numbers;
     }
 
     private String[] separate(String input) {
-        String[] inputNumbers = input.split(DELIMITER);
-        return inputNumbers;
+        String[] numbers = input.split(DELIMITER);
+        return numbers;
     }
 
+    private void validateDuplicated(List<Number> numbers) {
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException(IS_DUPLICATED.getMessage());
+        }
+    }
+
+    private boolean isDuplicated(List<Number> number) {
+        long result = number.stream()
+                .distinct().count();
+        return result != number.size();
+    }
 }
--- src/main/java/lotto/domain/constants/ExceptionMessage.java
+++ src/main/java/lotto/domain/constants/ExceptionMessage.java
@@ -2,8 +2,8 @@ package lotto.domain.constants;
 
 public enum ExceptionMessage {
     BE_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    HAS_LETTER("[ERROR] 로또 번호는 문자를 포함할 수 없습니다.");
-
+    HAS_LETTER("[ERROR] 로또 번호는 문자를 포함할 수 없습니다."),
+    IS_DUPLICATED("[ERROR] 로또 번호는 중복된 숫자를 포함할 수 없습니다.");
     private final String message;
 
     ExceptionMessage(String message) {

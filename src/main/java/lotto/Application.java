bb5487ede5714b2d0dfbbc0b3f15c62e09d348c7
brightfanta <brightfanta@gmail.com>
2023-11-03 18:47:39 +0900

    feat(Application): 당첨&보너스 번호 입력 요청 메시지 출력 및 당첨&보너스 번호 입력 메서드 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,8 +6,18 @@ public class Application {
     public static void main(String[] args) {
 
         View.printMessage(ASK_BUDGET);
-        String LottoBudgetInput = View.getUserInput();
-        System.out.println("LottoBudgetInput = " + LottoBudgetInput);
+        String lottoBudgetInput = View.getUserInput();
+        Integer budget = Convertor.convertInputToInteger(lottoBudgetInput);
+
+        View.printMessage(ASK_WINNING_NUMBERS);
+        String winningNumbersInput = View.getUserInput();
+        String[] winningNumbersArray = Convertor.splitInput(winningNumbersInput);
+        Convertor.convertToList(winningNumbersArray);
+
+        Convertor.convertToList(winningNumbersArray);
+        View.printMessage(ASK_BONUS_NUMBERS);
+        String bonusNumberInput = View.getUserInput();
+        Integer bonusNumberTemp = Convertor.convertInputToInteger(bonusNumberInput);
     }
 
 }

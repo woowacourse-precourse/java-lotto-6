0a42f1e4ebc7f62c82bf7830c3d7f02244eb6190
mainfn <_@mainfn.dev>
2023-11-04 03:34:16 +0900

    refactor: `LottoPrize` 필드 타입 동기화

--- src/main/java/lotto/domain/lotto/LottoPrize.java
+++ src/main/java/lotto/domain/lotto/LottoPrize.java
@@ -2,28 +2,46 @@ package lotto.domain.lotto;
 
 import java.util.List;
 import lotto.domain.money.Money;
-import lotto.domain.money.Price;
 
 /**
  * WinningLotto와 Lotto를 비교하여 일치하는 번호 및 보너스 번호를 계산하여 로또 등 수를 나타낼 때 사용되는 클래스입니다.
  */
 public enum LottoPrize {
-    FIRST(1, Price.from(2_000_000_000), 6, false),
-    SECOND(2, Price.from(30_000_000), 5, true),
-    THIRD(3, Price.from(1_500_000), 5, false),
-    FOURTH(4, Price.from(50_000), 4, false),
-    FIFTH(5, Price.from(5_000), 3, false),
-    NONE(0, Price.zero(), 0, false);
+    FIRST(1, Money.from(2_000_000_000), 6, false),
+    SECOND(2, Money.from(30_000_000), 5, true),
+    THIRD(3, Money.from(1_500_000), 5, false),
+    FOURTH(4, Money.from(50_000), 4, false),
+    FIFTH(5, Money.from(5_000), 3, false),
+    NONE(0, Money.zero(), 0, false);
 
+    /**
+     * 모든 enum constant를 List로 보관
+     */
     private static final List<LottoPrize> lottoPrizes = List.of(values());
+
+    /**
+     * 로또 등수
+     */
     private final int place;
-    private final Price price;
+
+    /**
+     * 등수별 당첨 가격
+     */
+    private final Money price;
+
+    /**
+     * 일치하는 로또 번호 수
+     */
     private final int matchedCount;
+
+    /**
+     * 보너스 번호 유무
+     */
     private final boolean hasBonusNumber;
 
     LottoPrize(
             final int place,
-            final Price price,
+            final Money price,
             final int matchedCount,
             final boolean hasBonusNumber
     ) {
@@ -50,7 +68,5 @@ public enum LottoPrize {
                 .orElse(LottoPrize.NONE);
     }
 
-    public Money intoMoney() {
-        return price.intoMoney();
-    }
+
 }

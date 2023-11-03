d7926ccb7fce5f65c53c36b905648d6a24d2b13a
BullChallenger <am23am45@naver.com>
2023-11-03 18:12:00 +0900

    [FEAT] 구매한 로또를 출력하는 메소드와 당첨 번호 입력 문구, 보너스 번호 입력 문구 출력 메소드 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.Lotto;
 
 public class View {
 
@@ -9,11 +11,30 @@ public class View {
     }
 
     public static String input() {
-        return Console.readLine();
+        String input =  Console.readLine();
+        System.out.println();
+        return input;
     }
 
     public static void printPurchaseMessage(int quantity) {
         System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public static void printPurchasedLotto(List<Lotto> purchasedLotto) {
+        for (Lotto lotto : purchasedLotto) {
+            System.out.println(lotto);
+        }
+        System.out.println();
+    }
+
+    public static String printInputNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return input();
+    }
+
+    public static String printInputBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return input();
     }
 
 }

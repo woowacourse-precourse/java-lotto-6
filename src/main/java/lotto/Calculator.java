0618bd5ff1baebcaf793eceeb68f31f78f2a303f
AmenPark <puzzlistpam@gmail.com>
2023-11-03 19:13:45 +0900

    fix: 카운트 버그 픽스

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -12,51 +12,69 @@ public class Calculator {
 
     public Calculator() {
         count = new HashMap<>();
+        init();
     }
-    public void setWinner(List<Integer> winner){
-        this.winner=winner;
+
+    private void init() {
+        for (Prize p : Prize.values()) {
+            count.put(p, 0);
+        }
     }
-    public void setBonus(int bonus) throws IllegalArgumentException{
-        if(winner.contains(bonus)){
+
+    public void setWinner(List<Integer> winner) {
+        this.winner = winner;
+    }
+
+    public void setBonus(int bonus) throws IllegalArgumentException {
+        if (winner.contains(bonus)) {
             throw new IllegalArgumentException();
         }
-        bonusNum=bonus;
+        bonusNum = bonus;
     }
 
 
-    public void checkLotto(Lotto lotto){
-        int containCount=0;
-        for (int num:winner){
-            if(lotto.hasNum(num)){
-                containCount+=2;
+    public void checkLotto(Lotto lotto) {
+        int containCount = 0;
+        for (int num : winner) {
+            if (lotto.hasNum(num)) {
+                containCount += 2;
             }
         }
-        if (lotto.hasNum(bonusNum)){
+        if (lotto.hasNum(bonusNum)) {
             containCount++;
         }
         update(containCount);
     }
-    private void update(int containCount){
+
+    private void update(int containCount) {
         int temp;
-        for(Prize p: Prize.values()){
-            if(p.getSame()<=containCount){
-                temp=count.get(p) + 1;
-                count.put(p,temp);
+        Prize p2 = Prize.PRIZE_2ND;
+        if(p2.getSame()==containCount){
+            temp=count.get(p2)+1;
+            count.put(p2,temp);
+            return;
+        }
+        containCount-=containCount%2;
+        for (Prize p : Prize.values()) {
+            if (p.getSame() == containCount) {
+                temp = count.get(p) + 1;
+                count.put(p, temp);
                 return;
             }
         }
     }
-    public long prizeSum(){
-        long sum=0;
-        for(Prize p:Prize.values()){
-            sum+=p.getMoney() * count.get(p);
+
+    public long prizeSum() {
+        long sum = 0;
+        for (Prize p : Prize.values()) {
+            sum += p.getMoney() * count.get(p);
         }
         return sum;
     }
 
-    public String statistics(){
+    public String statistics() {
         StringJoiner sj = new StringJoiner("\n");
-        for(Prize p:Prize.values()){
+        for (Prize p : Prize.values()) {
             sj.add(String.format("%s%d개", p.getUI(), count.get(p)));
         }
         return sj.toString();

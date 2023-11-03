84e07e81b9901dfff4b757c22db4216a2e3a2783
sami355-24 <ddung355@gmail.com>
2023-11-04 00:54:07 +0900

    feat(Domain): 당첨현황을 관리하는 Prize enum 클래스 생성

---
+++ src/main/java/lotto/Domain/Prize.java
@@ -0,0 +1,29 @@
+package lotto.Domain;
+
+public enum Prize {
+    FIRST_PRIZE(0, 2000000000),
+    SECOND_PRIZE(0, 30000000),
+    THIRD_PRIZE(0, 1500000),
+    FOURTH_PRIZE(0, 50000),
+    FIFTH_PRIZE(0, 5000);
+
+    private Integer count;
+    private final Integer award;
+
+    Prize(Integer count, Integer award) {
+        this.count = count;
+        this.award = award;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void addCount() {
+        this.count+=1;
+    }
+
+    public Integer getAward() {
+        return award;
+    }
+}

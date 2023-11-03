b11e0e8e773e9635731db8b8a52f4baf11b9e50f
leedohyun <lucky77795@naver.com>
2023-11-03 17:02:13 +0900

    refactor(Lotto) : Lotto 예외 수정
    사용자의 입력값에 따른 오류가 아니기 때문에 IllegalArgumentException 대신 RuntimeException을 던지는 것으로 수정.

--- src/main/java/lotto/exception/domain/lotto/LottoDuplicateNumException.java
+++ src/main/java/lotto/exception/domain/lotto/LottoDuplicateNumException.java
@@ -1,6 +1,6 @@
 package lotto.exception.domain.lotto;
 
-public class LottoDuplicateNumException extends IllegalArgumentException {
+public class LottoDuplicateNumException extends RuntimeException {
     private static final String ERROR_MESSAGE = "[ERROR] 로또에 중복된 번호가 존재합니다.";
 
     public LottoDuplicateNumException() {
--- src/main/java/lotto/exception/domain/lotto/LottoSizeException.java
+++ src/main/java/lotto/exception/domain/lotto/LottoSizeException.java
@@ -1,6 +1,6 @@
 package lotto.exception.domain.lotto;
 
-public class LottoSizeException extends IllegalArgumentException {
+public class LottoSizeException extends RuntimeException {
     private static final String ERROR_MESSAGE = "[ERROR] 로또에 6개 번호가 입력되지 않았습니다.";
 
     public LottoSizeException() {

779b446cff366f119a67a000c2e750339e9c3ef0
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 03:01:53 +0900

    Feat: "정렬 기능 추가"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,11 +4,14 @@ import lotto.model.Purchase;
 import lotto.model.RandomLotto;
 import lotto.view.InputView;
 
+import java.io.PipedReader;
+import java.net.CookieHandler;
+import java.util.*;
+
 public class Application {
 
     private static String purchasingAmount;
     private static int purchasingPieces;
-
     private static final InputView inputView = new InputView();
     private static final RandomLotto random = new RandomLotto();
 
@@ -22,7 +25,16 @@ public class Application {
         }
     }
 
+    public static List<Integer> listSort(List<Integer> noSort) {
+        List<Integer> listSort = new ArrayList<>(noSort);
+        Collections.sort(listSort);
+        return listSort;
+    }
+
     public static void main(String[] args) {
         purchaseCount();
+        for (int i = 0; i < purchasingPieces; i++) {
+            listSort(random.generateNum());
+        }
     }
 }

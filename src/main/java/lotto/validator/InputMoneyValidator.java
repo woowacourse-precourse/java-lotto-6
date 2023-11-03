d2293eb29ffc643efa9c352e61feab8e58401e76
hyundoo <hyundoo1006@gmail.com>
2023-11-03 21:28:04 +0900

    feat: 입력 값 숫자 예외 처리 기능 추가
    받아온 money 값이 숫자인지 확인

---
+++ src/main/java/lotto/validator/InputMoneyValidator.java
@@ -0,0 +1,21 @@
+package lotto.validator;
+
+public class InputMoneyValidator {
+
+    private static final String ERROR = "[ERROR]";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력해주세요";
+    private final String money;
+
+    public InputMoneyValidator(String input){
+        money = input;
+        isNumError();
+    }
+
+    public void isNumError(){
+        try{
+            Integer.parseInt(money);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException(ERROR+NOT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+}

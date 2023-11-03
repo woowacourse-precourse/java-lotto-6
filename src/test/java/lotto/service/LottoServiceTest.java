db95defa6bcbe1b8653c484c34f591575a069691
song windows <dkclasltmf@naver.com>
2023-11-03 20:07:41 +0900

    test: 로또 당첨 검증 테스트 수정 및 수익률 검증 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -3,26 +3,34 @@ package lotto.service;
 import lotto.domain.Lotto;
 import lotto.domain.Rank;
 import lotto.domain.Result;
-import lotto.utils.FixedNumberGenerator;
-import lotto.utils.NumberGenerator;
-import lotto.utils.RandomNumberGenerator;
+import lotto.domain.WinningStatistics;
+import lotto.utils.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LottoServiceTest {
 
+    private LottoWinningStrategy lottoWinningStrategy;
+    private LottoService lottoService;
+    private NumberGenerator randomNumberGenerator;
+    private NumberGenerator fixedNumberGenerator;
+
+    @BeforeEach
+    void beforeEach() {
+        lottoWinningStrategy = new DefaultLottoWinningStrategy();
+        lottoService = new LottoService(lottoWinningStrategy);
+        randomNumberGenerator = new RandomNumberGenerator();
+        fixedNumberGenerator = new FixedNumberGenerator(fixedNumberGenerator());
+    }
+
     @DisplayName("로또 구매 수량 검증 테스트")
     @Test
     void buyLottoTest() {
-        LottoService lottoService = new LottoService();
-        NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
         int lottoQuantity = 3;
 
         List<Lotto> lottos = lottoService.buyLottos(randomNumberGenerator, lottoQuantity);
@@ -31,18 +39,17 @@ class LottoServiceTest {
     }
 
     @DisplayName("1등 당첨 검증 테스트")
-    @ParameterizedTest
-    @MethodSource("fixedNumberGenerator")
-    void winnerNumberMatchTest(List<Integer> fixedGeneratedNumbers) {
-        LottoService lottoService = new LottoService();
-        NumberGenerator fixedNumberGenerator = new FixedNumberGenerator(fixedGeneratedNumbers);
+    @Test
+    void winnerNumberMatchTest() {
         int lottoQuantity = 1;
         List<Lotto> lottos = lottoService.buyLottos(fixedNumberGenerator, lottoQuantity);
 
-        List<Integer> winnerNumbers = fixedNumberGenerator().findFirst().get();
+        List<Integer> winnerNumbers = fixedNumberGenerator();
         int bonusNumber = 7;
 
-        List<Result> results = lottoService.matchLotto(lottos, winnerNumbers, bonusNumber);
+        List<Result> results = lottos.stream()
+                .map(lotto -> lotto.determineResult(lottoWinningStrategy, winnerNumbers, bonusNumber))
+                .toList();
 
         for (Result result : results) {
             assertThat(result.getRank()).isEqualTo(Rank.FIRST);
@@ -50,18 +57,17 @@ class LottoServiceTest {
     }
 
     @DisplayName("2등 당첨 검증 테스트")
-    @ParameterizedTest
-    @MethodSource("fixedNumberGenerator")
-    void secondRankTest(List<Integer> fixedGeneratedNumbers) {
-        LottoService lottoService = new LottoService();
-        NumberGenerator fixedNumberGenerator = new FixedNumberGenerator(fixedGeneratedNumbers);
+    @Test
+    void secondRankTest() {
         int lottoQuantity = 1;
         List<Lotto> lottos = lottoService.buyLottos(fixedNumberGenerator, lottoQuantity);
 
         List<Integer> winnerNumbers = List.of(1, 2, 3, 4, 5, 10);
         int bonusNumber = 6;
 
-        List<Result> results = lottoService.matchLotto(lottos, winnerNumbers, bonusNumber);
+        List<Result> results = lottos.stream()
+                .map(lotto -> lotto.determineResult(lottoWinningStrategy, winnerNumbers, bonusNumber))
+                .toList();
 
         for (Result result : results) {
             assertThat(result.getRank()).isEqualTo(Rank.SECOND);
@@ -69,10 +75,23 @@ class LottoServiceTest {
 
     }
 
-    static Stream<List<Integer>> fixedNumberGenerator() {
-        return Stream.of(
-                List.of(1, 2, 3, 4, 5, 6)
-        );
+    @DisplayName("로또 당첨 수익률 검증 테스트")
+    @Test
+    void winningStatisticsTest() {
+        int lottoQuantity = 1;
+        List<Lotto> lottos = lottoService.buyLottos(fixedNumberGenerator, lottoQuantity);
+        List<Integer> winnerNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 10;
+
+        WinningStatistics winningStatistics = lottoService.getWinningStatistics(lottos, winnerNumbers, bonusNumber);
+
+        int expectProfitRate = Rank.FIRST.getPrizeMoney() / 1000 * 100;
+
+        assertThat(winningStatistics.getProfitRate()).isEqualTo(expectProfitRate);
+    }
+
+    static List<Integer> fixedNumberGenerator() {
+        return List.of(1, 2, 3, 4, 5, 6);
     }
 
 }

d9275ca10ed585deb3f9e0cf2db6d5a2db1043d5
alscjf1329 <alscjf1329@gmail.com>
2023-11-03 23:16:32 +0900

    refactor(LottoUtil): static으로 변경
     기능을 더 간편히 쓰기 위해 static으로 변경했습니다.

--- src/main/java/lotto/utils/LottoUtil.java
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -3,7 +3,7 @@ package lotto.utils;
 import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
 
 public class LottoUtil {
-    public boolean validateLottoNum(int num) {
+    public static boolean validateLottoNum(int num) {
         if (1 <= num && num <= 45) {
             throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
         }

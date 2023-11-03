44e4d6c2ed333ce74497bed52f38e86728a3f2ab
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-03 20:24:32 +0900

    feat : 입력 값이 숫자인지 검증
    - 로또 구입 금액이 숫자가 아니면, IllegalArgumentException을 발생시킨다.

--- src/main/java/lotto/Amount.java
+++ src/main/java/lotto/Amount.java
@@ -8,15 +8,40 @@ public class Amount {
     }
 
     public int enterAmount() {
-        int amount = 0;
+        int amount;
 
-        printAmountEnterGuideStatement();
-        String input = Console.readLine();
+        while (true) {
+            try {
+                printAmountEnterGuideStatement();
+                String input = Console.readLine();
+                amount = validateAmount(input);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
         return amount;
     }
 
     private void printAmountEnterGuideStatement() {
         System.out.println("구입 금액을 입력해 주세요");
+    }
+
+    int validateAmount(String input) {
+        int amount = isNum(input);
+        return amount;
+    }
+
+    int isNum(String input) {
+        String regex = "^[0-9]+$";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 금액 값은 숫자여야 합니다.");
+        }
+        return stringToInt(input);
+    }
+
+    int stringToInt(String str) {
+        return Integer.parseInt(str);
     }
 }

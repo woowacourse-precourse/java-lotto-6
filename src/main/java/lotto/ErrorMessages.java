006b413886dbc7102d8b4db192e055ed30227724
unknown <winternrabbit@gmail.com>
2023-11-04 03:29:24 +0900

    refactor: 오류메시지 문구 수정

--- src/main/java/lotto/ErrorMessages.java
+++ src/main/java/lotto/ErrorMessages.java
@@ -1,7 +1,7 @@
 package lotto;
 
 public enum ErrorMessages {
-    ERROR_NOT_NUMBER("[ERROR] 정수만 입력해 주세요"),
+    ERROR_NOT_NUMBER("[ERROR] 양의 정수를 입력해 주세요"),
     ERROR_NOT_MULTIPLE_OF_THOUSAND("[ERROR] 1000의 배수를 입력해 주세요.");
 
     private final String message;

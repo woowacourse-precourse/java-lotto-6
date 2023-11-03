19dba5c33db781423a4727fc42e990489d068ccb
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-04 00:51:44 +0900

    refactor(InputValidator): 에러 메세지 상수화

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -10,32 +10,40 @@ public class InputValidator {
     private static final Integer MINIMUM_NUMBER = 1;
     private static final Integer MAXIMUM_NUMBER = 45;
 
+    // Error message constant
+    private static final String IS_NUMERIC_VALIDATOR_ERROR_MESSAGE = "[ERROR] 입력은 숫자 형태여야 합니다.";
+    private static final String IS_THOUSAND_UNIT_VALIDATOR_ERROR_MESSAGE = "[ERROR] 입력 금액은 1,000원 단위여야 합니다.";
+    private static final String IS_MINIMUM_VALIDATOR_ERROR_MESSAGE = "[ERROR] 입력 금액은 최소 0원보다 커야 합니다.";
+    private static final String IS_IN_RANGE_VALIDATOR_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String IS_NOT_OVERLAP_SIX_VALIDATOR_ERROR_MESSAGE = "[ERROR] 6개의 숫자에 중복된 로또 번호가 존재합니다.";
+    private static final String IS_NOT_OVERLAP_BONUS_VALIDATOR_ERROR_MESSAGE = "[ERROR] 보너스 번호와 중복된 로또 번호가 존재합니다.";
+
     public Boolean isNumericValidator(String number){
         try {
             Integer parsedNumber = Integer.parseInt(number);
             return true;
         }catch(NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 입력은 숫자 형태여야 합니다.");
+            throw new IllegalArgumentException(IS_NUMERIC_VALIDATOR_ERROR_MESSAGE);
         }
     }
 
     public Boolean isThousandUnitValidator(Integer money){
         if(money % MONEY_UNIT != 0){
-            throw new IllegalArgumentException("[ERROR] 입력 금액은 1,000원 단위여야 합니다.");
+            throw new IllegalArgumentException(IS_THOUSAND_UNIT_VALIDATOR_ERROR_MESSAGE);
         }
         return true;
     }
 
     public Boolean isMinimumValidator(Integer money){
         if(money <= MINIMUM_MONEY_STANDARD){
-            throw new IllegalArgumentException("[ERROR] 입력 금액은 최소 0원보다 커야 합니다.");
+            throw new IllegalArgumentException(IS_MINIMUM_VALIDATOR_ERROR_MESSAGE);
         }
         return true;
     }
 
     public Boolean isInRangeValidator(Integer number){
         if(number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(IS_IN_RANGE_VALIDATOR_ERROR_MESSAGE);
         }
         return true;
     }
@@ -43,7 +51,7 @@ public class InputValidator {
         Set<Integer> uniqueNumbers = new HashSet<>();
         for(Integer number : numbers){
             if(!uniqueNumbers.add(number)){
-                throw new IllegalArgumentException("[ERROR] 6개의 숫자에 중복된 로또 번호가 존재합니다.");
+                throw new IllegalArgumentException(IS_NOT_OVERLAP_SIX_VALIDATOR_ERROR_MESSAGE);
             }
         }
         return true;
@@ -51,7 +59,7 @@ public class InputValidator {
 
     public Boolean isNotOverlapBonusValidator(List<Integer> numbers, Integer bonusNumber){
         if(numbers.contains(bonusNumber)){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호와 중복된 로또 번호가 존재합니다.");
+            throw new IllegalArgumentException(IS_NOT_OVERLAP_BONUS_VALIDATOR_ERROR_MESSAGE);
         }
         return true;
     }

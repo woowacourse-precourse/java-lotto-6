ed3ab869de2e2343e16dcb5549b806bec9edd21c
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-04 03:03:25 +0900

    feat: 구입 금액 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchase = Integer.parseInt(Console.readLine());
+        if (purchase % 1000 != 0) {
+            throw new IllegalArgumentException("[Error] 올바른 금액을 입력해주세요.");
+        }
+        System.out.println();
+
 
     }
 }

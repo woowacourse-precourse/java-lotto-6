f5339e07a18e2355d9c58ac96325be2a7f0d2f41
gusgh075 <gusgh075@naver.com>
2023-11-03 15:37:58 +0900

    FEATURES(LottoRepository) : 당첨번호를 생성자에서 설정

--- src/main/java/lotto/LottoRepository.java
+++ src/main/java/lotto/LottoRepository.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class LottoRepository {
     private List<Lotto> lottos;
+    private final Lotto winningLotto;
+    public LottoRepository(Lotto winningLotto){
+        this.winningLotto=winningLotto;
+    }
     public void addLotto(Lotto lotto){
         lottos.add(lotto);
     }

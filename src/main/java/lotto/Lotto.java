799a2a042eba19354e6b1824c198869a6d85be50
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-03 23:46:57 +0900

    Feat: 추가 inputNumber 함수
    사용자로부터 문자열을 입력받아 숫자 리스트로 반환하는 함수. 나중에 유효성 체크 함수 작성 필요

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,11 +15,18 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> inputNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        Integer[] inputNums = Stream.of(input.split(",")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+        List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(inputNums));
+//       입력값 검증 함수
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }

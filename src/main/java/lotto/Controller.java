06642a128f91f3f462b9699ff0b4217bf889ae44
brightfanta <brightfanta@gmail.com>
2023-11-03 19:18:21 +0900

    feat(Controller): 로또 발행 개수 계산 메서드 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,4 +1,9 @@
 package lotto;
 
 public class Controller {
+
+    private static final Integer LOTTERY_PRICE = 1_000;
+    public static Integer calculateLotteryCount(Integer budget) {
+        return budget / LOTTERY_PRICE;
+    }
 }

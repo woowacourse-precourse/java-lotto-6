2f8d7dcb9208b3cf551068ab36a8b7dbf09930b0
Oseuseu <lakingsung@gmail.com>
2023-11-03 22:08:54 +0900

    test: `isInteger` 메서드 테스트 코드 추가
    `isInteger` 메서드가 false 반환하는 상황 테스트 코드 추가

--- src/test/java/lotto/UtilTest.java
+++ src/test/java/lotto/UtilTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UtilTest {
@@ -10,5 +11,11 @@ public class UtilTest {
     @ValueSource(strings = {"532", "123", "6542"})
     void isInteger_true_반환_테스트(String input) {
         assertTrue(LottoUtil.isInteger(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test", "321.12"})
+    void isInteger_false_반환_테스트(String input) {
+        assertFalse(LottoUtil.isInteger(input));
     }
 }

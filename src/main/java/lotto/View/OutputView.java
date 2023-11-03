8b2a37c9dde566b2574318dacbed63f87b32d121
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 12:46:14 +0900

    feat(OutputView): 랜덤 로또 번호들 출력

---
+++ src/main/java/lotto/View/OutputView.java
@@ -0,0 +1,17 @@
+package lotto.View;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.Model.Lotto;
+
+public class OutputView {
+
+    public void printRandomLotto(Lotto lotto) {
+        System.out.println(lotto.toString());
+    }
+}
+
+
+
+
+

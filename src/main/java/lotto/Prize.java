e411949075720eb2e2d442f599fa05c3f62e5644
Youngwoo Choi <allnstar@naver.com>
2023-11-03 23:02:20 +0900

    refactor: add counting bonus to checking numbers

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Prize {
     private String groupNumbers;
+    private int bonusCounts;
     private int luckyCounts;
     private List<Integer> separatedLotto;
     public static List<Integer> luckyNumber;
@@ -40,6 +41,10 @@ public class Prize {
         for (int j = 0; j < separatedLotto.size(); j++) {
             if (separatedLotto.contains(luckyNumber.get(j))) {
                 luckyCounts += 1;
+            }
+
+            if (separatedLotto.contains((bonusNumber))) {
+                bonusCounts += 1;
             }
         }
     }

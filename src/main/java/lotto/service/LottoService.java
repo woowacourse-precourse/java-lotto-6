678929aa0dcfc0dc2a9d08eb0f3992941d919da8
BullChallenger <am23am45@naver.com>
2023-11-03 23:41:50 +0900

    [FEAT] Lotto의 숫자와 입력된 숫자를 비교하여 몇 개가 포함되어 있는지 확인하는 로직 작성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,9 @@
 package lotto.service;
 
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
+import lotto.domain.Player;
 
 public class LottoService {
 
@@ -9,6 +11,18 @@ public class LottoService {
 
     public Lotto generateLotto() {
         return new Lotto(lottoGenerator.generateLottoNumbers());
+    }
+
+    public int compareLotto(Lotto lotto, List<Integer> inputNumbers) {
+        int count = 0;
+
+        for (int number : inputNumbers) {
+            if (lotto.isContainNumber(number)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
 }

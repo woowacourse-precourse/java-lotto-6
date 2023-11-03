9684a24d9c246bc2fae7f3a350a955471e903163
ChaeAg <dydkglel123@gmail.com>
2023-11-03 13:09:17 +0900

    feat(BonusNumber): 로또 보너스 번호 객체 클래스 구현

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,47 @@
+package lotto.model;
+
+public class BonusNumber {
+    private final int bonusNumber;
+
+    private BonusNumber(String bonusNumberStr) {
+        validateNull(bonusNumberStr);
+        validateEmpty(bonusNumberStr);
+        int bonusNumber = validateNotNumber(bonusNumberStr);
+        validateInvalidLottoNumberRange(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static BonusNumber create(String bonusNumber) {
+        return new BonusNumber(bonusNumber);
+    }
+
+    public int getBonusNumber() {
+        return this.bonusNumber;
+    }
+
+    private void validateNull(String bonusNumberStr) {
+        if (bonusNumberStr == null) {
+            throw new IllegalArgumentException("[ERROR] 입력이 null일 수 없습니다.");
+        }
+    }
+
+    private void validateEmpty(String bonusNumberStr) {
+        if (bonusNumberStr.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호를 입력해 주세요.");
+        }
+    }
+
+    private int validateNotNumber(String bonusNumberStr) {
+        try {
+            return Integer.parseInt(bonusNumberStr);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+    }
+
+    private void validateInvalidLottoNumberRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 수여야 합니다.");
+        }
+    }
+}

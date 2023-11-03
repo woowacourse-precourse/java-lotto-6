9c4424751074c721216d6fc2fb3d45d15dbd1c11
yuseonjun <seonjun0906@gmail.com>
2023-11-03 17:02:57 +0900

    feat(BonusNumber): 보너스 번호를 담당할 BonusNumber 클래스 생성

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class BonusNumber {
+
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+}

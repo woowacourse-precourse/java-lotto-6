0bdc9387e2b6e8c32b2142d3f6551d156b8f143f
sangchu <didi1484@gmail.com>
2023-11-03 17:29:54 +0900

    feat: Notice enum 생성 (사용자 UI 메시지 목록)

---
+++ src/main/java/lotto/view/constants/Notice.java
@@ -0,0 +1,19 @@
+package lotto.view.constants;
+
+public enum Notice {
+    ASK_PURCHASE_PRICE("구입금액을 입력해 주세요."),
+    NUMBER_OF_TICKETS("개를 구매했습니다."),
+    ASK_WINNER_NUMBERS("당첨 번호를 입력해 주세요."),
+    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    ;
+
+    private final String message;
+
+    Notice(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

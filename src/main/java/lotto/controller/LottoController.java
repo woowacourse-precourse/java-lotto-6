9d9ced503afd0a40a9e736f32be82b281b507c07
sinyebin <dpqls0625@naver.com>
2023-11-04 02:09:37 +0900

    feat: 발행한 로또 번호 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,22 @@
 package lotto.controller;
 
+import lotto.domain.Lottos;
+import lotto.service.GetLottoTicket;
 import lotto.service.InputLottoAmount;
 
 public class LottoController {
     public void startLotto() {
-        setAmount();
+        int lottoTicketCount = setAmount();
+        Lottos lottos = getLottos(lottoTicketCount);
     }
 
-    public void setAmount() {
+    public int setAmount() {
         int lottoTicketCount = InputLottoAmount.setInputLottoAmount();
+        return lottoTicketCount;
+    }
 
+    public Lottos getLottos(int lottoTicketCount) {
+        Lottos lottos = GetLottoTicket.setLottoTickets(lottoTicketCount);
+        return lottos;
     }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,21 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+    private static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
+
+    public Lotto() {
+        this.numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_SIZE);
+        Collections.sort(this.numbers);
+        System.out.println(numbers);
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);

a3195d88c1c49983cc17ec541d9797f699587f9b
DaeYoungK <hanguk830@naver.com>
2023-11-03 23:09:13 +0900

    feat(RandomNumber): 로또발행용 숫자 배열을 오름차순으로 정렬하여 반환하는 기능 추가

--- src/main/java/lotto/utility/RandomNumber.java
+++ src/main/java/lotto/utility/RandomNumber.java
@@ -2,11 +2,14 @@ package lotto.utility;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumber {
 
     public List<Integer> makeLottoNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumber);
+        return lottoNumber;
     }
 }

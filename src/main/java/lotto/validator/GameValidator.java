406f2172be8d7229dc13a6f755d487dc2284188e
HyungHoKim <hkim1109@naver.com>
2023-11-03 13:07:44 +0900

    feat(GameValidator): 예외 케이스 추가
    보너스 번호가 로또 번호와 중복되거나 번호 범위를 벗어난 경우, 양의 정수가 아닌 경우 예외를 발생시키는 메서드 validateBonusNumber() 구현

--- src/main/java/lotto/validator/GameValidator.java
+++ src/main/java/lotto/validator/GameValidator.java
@@ -1,15 +1,67 @@
 package lotto.validator;
 
-import static lotto.exception.ExceptionCase.MONEY_UNIT_MISMATCH;
+import static lotto.enums.ExceptionCase.INPUT_TYPE_MISMATCH;
+import static lotto.enums.ExceptionCase.MONEY_UNIT_MISMATCH;
+import static lotto.enums.ExceptionCase.NUMBER_DUPLICATION;
+import static lotto.enums.ExceptionCase.NUMBER_OUT_OF_RANGE;
+import static lotto.enums.NumberCondition.LOTTO_MAX;
+import static lotto.enums.NumberCondition.LOTTO_MIN;
+import static lotto.enums.NumberCondition.MONEY_UNIT;
+import static lotto.enums.RegularExpression.NUMBERS_AND_COMMA;
+import static lotto.enums.RegularExpression.POSITIVE_INTEGER;
+
+import java.util.List;
+import lotto.model.Lotto;
 
 public class GameValidator {
-    public static void validateMoney(int money) {
-        if (!isThousandWonUnit(money)) {
+    public static void validateMoney(String money) {
+        if (!isPositiveInteger(money)) {
+            throw new IllegalArgumentException(INPUT_TYPE_MISMATCH.message());
+        }
+        if (!isCorrectUnit(money)) {
             throw new IllegalArgumentException(MONEY_UNIT_MISMATCH.message());
         }
     }
 
-    private static boolean isThousandWonUnit(int money) {
-        return money % 1000 == 0;
+    public static void validateNumbers(String numbers) {
+        if (!isNumbers(numbers)) {
+            throw new IllegalArgumentException(INPUT_TYPE_MISMATCH.message());
+        }
     }
+
+    public static void validateBonusNumber(String number, Lotto lotto) {
+        if (!isPositiveInteger(number)) {
+            throw new IllegalArgumentException(INPUT_TYPE_MISMATCH.message());
+        }
+        int bonusNumber = Integer.parseInt(number);
+        if (!inRange(bonusNumber)) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.message());
+        }
+        if (!unique(bonusNumber, lotto)) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATION.message());
+        }
+    }
+
+
+    private static boolean isPositiveInteger(String money) {
+        return money.matches(POSITIVE_INTEGER.regex());
+    }
+
+    private static boolean isCorrectUnit(String money) {
+        return Integer.parseInt(money) % MONEY_UNIT.number() == 0;
+    }
+
+    private static boolean isNumbers(String numbers) {
+        return numbers.matches(NUMBERS_AND_COMMA.regex());
+    }
+
+    private static boolean inRange(int bonusNumber) {
+        return !(bonusNumber >= LOTTO_MIN.number() && bonusNumber <= LOTTO_MAX.number());
+    }
+
+    private static boolean unique(int bonusNumber, Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        return !numbers.contains(bonusNumber);
+    }
+
 }

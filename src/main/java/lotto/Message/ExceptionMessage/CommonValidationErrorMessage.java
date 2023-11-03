647b1f1fd661ca4df252d44699974aa4fd44127b
sami355-24 <ddung355@gmail.com>
2023-11-03 18:28:32 +0900

    refactor(Message/ExceptionMessage): 유틸리티 로직과 검증 로직 분리에 따른 클래스 명 변경

--- src/main/java/lotto/Message/ExceptionMessage/CommonValidationErrorMessage.java
+++ src/main/java/lotto/Message/ExceptionMessage/CommonValidationErrorMessage.java
@@ -1,14 +1,13 @@
 package lotto.Message.ExceptionMessage;
 
-public enum UtilErrorMessage {
-    CONVERSION_ERROR_MESSAGE("[ERROR] 숫자로 변환할 수 없습니다.\n 입력값 : %s"),
+public enum CommonValidationErrorMessage {
     RANGE_ERROR_MESSAGE("[ERROR] 1 ~ 45 사이의 숫자만 입력해 주세요. \n 입력값 : %s"),
     BLANK_VALUE_ERROR_MESSAGE("[ERROR] 공백은 입력할 수 없습니다.\n"),
     INTEGER_VALUE_ERROR_MESSAGE("[ERROR] 정수만 입력할 수 있습니다.\n 입력값 : %s");
 
     private String message;
 
-    UtilErrorMessage(String message) {
+    CommonValidationErrorMessage(String message) {
         this.message = message;
     }
 

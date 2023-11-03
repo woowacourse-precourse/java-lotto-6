f48974408b24531a24be0fdeb0e15aaa3bd11bd3
sami355-24 <ddung355@gmail.com>
2023-11-03 21:29:26 +0900

    test(Domain): 범위 밖에 구매 금액을 입력하였을때 예외 발생하는지 확인하는 테스트 코드 작성

---
+++ src/test/java/lotto/Domain/MoneyTest.java
@@ -0,0 +1,24 @@
+package lotto.Domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import lotto.Exception.CommonValidationException;
+import lotto.Exception.MoneyException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @DisplayName("범위 밖에 구매금액을 입력했을때 예외발생하는지 확인")
+    @Test
+    void createMoneyByOutOfRange() {
+        String inputMoney1 = "999";
+        String inputMoney2 = "100001";
+
+        assertThrows(MoneyException.class, () -> Money.of(inputMoney1));
+        assertThrows(MoneyException.class, () -> Money.of(inputMoney2));
+    }
+
+
+}

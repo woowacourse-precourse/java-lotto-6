bb5145eca9c3542007123ee2285b442eef28f649
YongHyeon <pyh4616@naver.com>
2023-11-03 15:13:34 +0900

    Feat(Lotto 클래스): 로또 번호 범위에 대한 유효성 검사 기능을 validateRange 메서드로 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -10,6 +10,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -24,6 +25,18 @@ public class Lotto {
         Set duplicateCheck = new HashSet(numbers);
         if (duplicateCheck.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되는 값이 존재하지 않아야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateSingleNumberRange(number);
+        }
+    }
+
+    private void validateSingleNumberRange(int number) {
+        if (number > 45 || number < 1) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
         }
     }
 }

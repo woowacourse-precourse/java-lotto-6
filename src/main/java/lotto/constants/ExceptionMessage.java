91e24b7ad8314277b0ca453904c12ca9bde87afe
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-03 18:51:46 +0900

    feat : 에러 메시지 관련 상수 객체 추가

---
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -0,0 +1,23 @@
+package lotto.constants;
+
+public class ExceptionMessage {
+
+    public static final String ERROR_FORMAT = "[ERROR] %s";
+
+    // 모든 입력 공통
+    public static final String BLANK_INPUT = "입력값이 빈 문자열입니다: ";
+    public static final String SPACE_INCLUDED = "입력값 중간에 공백 문자를 포함할 수 없습니다.";
+    public static final String NOT_INTEGER = "정수로 변환할 수 없는 문자열입니다: ";
+
+    // 구입금액 입력 전용
+    public static final String INDIVISIBLE_TO_THOUSAND = "1000원으로 나누어떨어지지 않는 금액입니다: ";
+    public static final String NOT_POSITIVE = "입력된 금액이 양의 정수가 아닙니다: ";
+
+    // 당첨 번호 입력, 보너스 번호 입력 공통
+    public static final String INVALID_NUMBER = "입력된 번호가 1~45 사이 정수가 아닙니다: ";
+    public static final String DUPLICATED_NUMBER = "이미 입력된 적 있는 번호입니다: ";
+
+    // 당첨 번호 입력 전용
+    public static final String INVALID_COMMA = "쉼표(,)가 없거나 잘못 사용되었습니다: ";
+    public static final String NOT_SIX_NUMBERS = "입력된 당첨 번호가 6개가 아닙니다.";
+}

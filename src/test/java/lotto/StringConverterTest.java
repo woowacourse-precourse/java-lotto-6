59d841ab6f9b45f32f9b30b2dfcab5b5a8bbb5eb
Yang-soeun <soeun8636@naver.com>
2023-11-03 20:51:04 +0900

    [test]: 문자를 정수로 반환하는 메소드 예외 상황 테스트 코드 추가

--- src/test/java/lotto/StringConverterTest.java
+++ src/test/java/lotto/StringConverterTest.java
@@ -28,7 +28,7 @@ class StringConverterTest {
 
     @DisplayName("문자열 리스트에 숫자가 아닌 문자열이 들어있으면 반환할때 예외가 발생한다.")
     @Test
-    void convertToIntegerListWithNotDigit(){
+    void convertToIntegerListWithNotDigit() {
         //given
         StringConverter stringConverter = new StringConverter();
         List<String> numbers = new ArrayList<>();
@@ -42,7 +42,7 @@ class StringConverterTest {
 
     @DisplayName("문자를 정수로 변환한다.")
     @Test
-    void parseInt(){
+    void parseInt() {
         //given
         StringConverter stringConverter = new StringConverter();
         String number = "1";
@@ -53,5 +53,18 @@ class StringConverterTest {
         //then
         assertThat(convertedNumber).isInstanceOf(Integer.class)
                 .isEqualTo(1);
+    }
+
+    @DisplayName("문자가 숫자가 아니면 정수로 변환할때 오류가 발생한다.")
+    @Test
+    void test() {
+        //given
+        StringConverter stringConverter = new StringConverter();
+        String number = "a";
+
+        //when //then
+        assertThatThrownBy(() -> stringConverter.parseInt(number))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("[ERROR] 번호와 금액은 정수여야 합니다.");
     }
 }

03c110cc10c881b7da502d69f29b9f85b5af3370
brightfanta <brightfanta@gmail.com>
2023-11-03 22:11:51 +0900

    feat(Rank): 등수별 상금 계산, 등수별 일치번호개수 선언

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,15 +1,59 @@
 package lotto;
 
 public enum Rank {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000);
+    FIRST(2_000_000_000, 6){
+        @Override
+        public Integer calculatePrizeByRank(int rankCount) {
+            return prize * rankCount;
+        }
+    },
+    SECOND(30_000_000, 5){
+        @Override
+        public Integer calculatePrizeByRank(int rankCount) {
+            return prize * rankCount;
+        }
+    },
+    THIRD(1_500_000, 5){
+        @Override
+        public Integer calculatePrizeByRank(int rankCount) {
+            return prize * rankCount;
+        }
+    },
+    FOURTH(50_000, 4){
+        @Override
+        public Integer calculatePrizeByRank(int rankCount) {
+            return prize * rankCount;
+        }
+    },
+    FIFTH(5_000, 3){
+        @Override
+        public Integer calculatePrizeByRank(int rankCount) {
+            return prize * rankCount;
+        }
+    },
+    DEFAULT(0, 0){
+        @Override
+        public Integer calculatePrizeByRank(int rankCount) {
+            return prize * rankCount;
+        }
+    },
+    ;
 
-    private final Integer prize;
+    protected final Integer prize;
+    private final Integer matchingCount;
 
-    Rank(Integer prize) {
+    Rank(Integer prize, Integer matchingCount) {
         this.prize = prize;
+        this.matchingCount = matchingCount;
     }
+
+    public Integer getPrize() {
+        return prize;
+    }
+
+    public Integer getMatchingCount() {
+        return matchingCount;
+    }
+
+    public abstract Integer calculatePrizeByRank(int rankCount);
 }

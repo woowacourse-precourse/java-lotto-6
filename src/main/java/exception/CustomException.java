e913056ddf9b446d37d4cba2c65cd2e41f4476d5
eora21 <eora21@naver.com>
2023-11-03 15:42:25 +0900

    feat: 🎸 추가 예외를 효율적으로 처리하기 위한 CustomException 작성
    따로 예외처리를 하지 않은 상황 대처 및 요구사항의 [ERROR] 메시지를 효율적으로 출력하기 위해 작성

---
+++ src/main/java/exception/CustomException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public abstract class CustomException extends IllegalArgumentException {
+    private static final String ERROR = "[ERROR] ";
+    public CustomException(String exceptionMessage) {
+        super(ERROR + exceptionMessage);
+    }
+}

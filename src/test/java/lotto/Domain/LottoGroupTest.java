3d4b274e4cfa2106ed2f4411b14ee2484ae2f9c8
sami355-24 <ddung355@gmail.com>
2023-11-03 22:18:45 +0900

    test(Domain): 로또 그룹에 대한 테스트 코드 추가
    - 정상 시나리오 추가
    - 예외 시나리오 추가

---
+++ src/test/java/lotto/Domain/LottoGroupTest.java
@@ -0,0 +1,60 @@
+package lotto.Domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGroupTest {
+
+    @DisplayName("구매가능한 로또의 수와 실제로 구매한 로또의 수를 비교하는 로직 - 정상시나리오")
+    @Test
+    void findLottoCountTest1() {
+        // given
+        Money money1 = Money.of("14000");
+        Money money2 = Money.of("1000");
+
+        // when
+        LottoGroup lottoGroup1 = LottoGroup.from(money1);
+        LottoGroup lottoGroup2 = LottoGroup.from(money2);
+
+        // then
+        assertEquals(14, lottoGroup1.findLottoNumbersSize());
+        assertEquals(1, lottoGroup2.findLottoNumbersSize());
+    }
+
+
+    @DisplayName("구매가능한 로또의 수와 실제로 구매한 로또의 수를 비교하는 로직 - 예외시나리오")
+    @Test
+    void findLottoCountTest2() {
+        // given
+        Money money1 = Money.of("14000");
+        Money money2 = Money.of("1000");
+
+        // when
+        LottoGroup lottoGroup1 = LottoGroup.from(money1);
+        LottoGroup lottoGroup2 = LottoGroup.from(money2);
+
+        // then
+        assertNotEquals(15, lottoGroup1.findLottoNumbersSize());
+        assertNotEquals(2, lottoGroup2.findLottoNumbersSize());
+    }
+
+
+    @DisplayName("인덱스로 로또 조회")
+    @Test
+    void findLottoTest() {
+        // given
+        Money money = Money.of("5000");
+        LottoGroup lottoGroup = LottoGroup.from(money);
+
+        // when
+        for (int i = 0; i < lottoGroup.findLottoNumbersSize(); i++) {
+            lottoGroup.findLottoByIndex(i);
+        }
+
+        // then
+        Assertions.assertThat(true).isTrue();
+    }
+}

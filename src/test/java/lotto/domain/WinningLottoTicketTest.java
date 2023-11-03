b11559c74d66f93ced0d9e02044d4b1a00bd103a
euics <pjh575157@gmail.com>
2023-11-03 14:55:59 +0900

    test: whenFiveNumbersAndBonusNumberMatch_thenReturnSecondRank 메서드 구현
    보너스 번호가 일치하는 경우 SECOND 등수 반환 메서드 구현

--- src/test/java/lotto/domain/WinningLottoTicketTest.java
+++ src/test/java/lotto/domain/WinningLottoTicketTest.java
@@ -21,4 +21,17 @@ class WinningLottoTicketTest {
 
         assertEquals(Rank.FIRST, result);
     }
+
+    @Test
+    @DisplayName("보너스 번호가 일치하는 경우 SECOND 등수 반환")
+    void whenFiveNumbersAndBonusNumberMatch_thenReturnSecondRank() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> playerNumbers = Arrays.asList(1, 2, 3, 4, 5, 7);
+        WinningLottoTicket winningLottoTicket = new WinningLottoTicket(winningNumbers, 7);
+        Lotto lotto = new Lotto(playerNumbers);
+
+        Rank result = winningLottoTicket.match(lotto);
+
+        assertEquals(Rank.SECOND, result);
+    }
 }

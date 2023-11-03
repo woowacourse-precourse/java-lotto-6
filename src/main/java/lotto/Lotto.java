66e5a0e6dfcf09b97daf9d734e747c98e9eea640
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-04 02:26:23 +0900

    fix: 에러 메시지에 '[ERROR]' 문구 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,9 +8,9 @@ public class Lotto {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOTTO_NUMBER_SIZE = 6;
-    private static final String INVALID_NUMBER_COUNT = "로또 숫자 개수에 맞도록 입력해주세요.";
-    private static final String INVALID_NUMBER_RANGE = "로또 숫자 범위에 맞도록 입력해주세요.";
-    private static final String INVALID_DUPLICATION = "로또 숫자는 중복될 수 없습니다.";
+    private static final String INVALID_NUMBER_COUNT = "[ERROR] 로또 숫자 개수에 맞도록 다시 입력해 주세요.";
+    private static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 숫자 범위에 맞도록 다시 입력해 주세요.";
+    private static final String INVALID_DUPLICATION = "[ERROR] 로또 숫자는 중복될 수 없습니다. 다시 입력해 주세요.";
     private static final int DEFAULT_PRICE = 1000;
 
     protected final List<Integer> numbers;

0848de30cbb95ab933922277a46f09413f545cdf
ChaeAg <dydkglel123@gmail.com>
2023-11-03 15:03:52 +0900

    refactor(LottoController): 컨트롤러 내에서 동작별로 메서드 분리

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,69 +1,94 @@
-package lotto.Controller;
+package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.View.InputView;
-import lotto.View.OutputView;
 import lotto.model.Amount;
 import lotto.model.BonusNumber;
 import lotto.model.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
-
-    private static final OutputView outputView = new OutputView();
-    private static final InputView inputView = new InputView();
-
     public void run() {
-        outputView.displayPurchaseGuide();
-        int amount;
+        OutputView.displayPurchaseGuide();
+
+        final Amount lottoCount = inputLottoAmount();
+        OutputView.displayLottoCount(lottoCount.getAmount());
+
+        final List<Lotto> lottoList = buyLotto(lottoCount.getAmount());
+        OutputView.displayLottoNumbers(lottoList);
+
+        OutputView.displayWinningNumberGuide();
+        final Lotto winningLotto = inputWinningNumbers();
+
+        OutputView.displayBonusNumberGuide();
+        final BonusNumber bonusNumber = inputBonusNumber();
+
+        final int[] lottoResultCount = compareWinningNumbers(lottoCount.getAmount(), lottoList, winningLotto, bonusNumber);
+        final int winningAmount = calculateWinnings(lottoResultCount);
+
+        OutputView.displayWinningStatisticsGuide();
+        OutputView.displayWinningStatistics(lottoResultCount);
+
+        double yield = calculateYield(winningAmount, lottoCount.getAmount());
+        OutputView.displayYield(yield);
+    }
+
+    private Amount inputLottoAmount() {
+        Amount amount;
 
         while (true) {
             try {
-                amount = Amount.create(inputView.inputPurchaseAmount()).getAmount();
+                amount = Amount.create(InputView.inputPurchaseAmount());
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
 
-        int lottoCount = amount / 1000;
-        outputView.displayLottoCount(lottoCount);
+        return amount;
+    }
+
+    private List<Lotto> buyLotto(int lottoCount) {
         List<Lotto> lottoList = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             lottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
-        outputView.displayLottoNumbers(lottoList);
+        return lottoList;
+    }
 
-        outputView.displayWinningNumberGuide();
+    private Lotto inputWinningNumbers() {
         List<Integer> winningNumbers;
         Lotto winningLotto;
-
         while (true) {
             try {
-                winningNumbers = inputView.inputWinningNumbers();
+                winningNumbers = InputView.inputWinningNumbers();
                 winningLotto = new Lotto(winningNumbers);
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
+        return winningLotto;
+    }
 
-        outputView.displayBonusNumberGuide();
+    private BonusNumber inputBonusNumber() {
         BonusNumber bonusNumber;
-
         while (true) {
             try {
-                bonusNumber = BonusNumber.create(inputView.inputBonusNumber());
+                bonusNumber = BonusNumber.create(InputView.inputBonusNumber());
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
+        return bonusNumber;
+    }
 
+    private int[] compareWinningNumbers(int lottoCount, List<Lotto> lottoList, Lotto winningLotto, BonusNumber bonusNumber) {
         int[] lottoResultCount = new int[5];
-        double winningAmount = 0;
 
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> lottoNumbers = lottoList.get(i).getNumbers();
@@ -75,33 +100,40 @@ public class LottoController {
             }
             if (count == 6) {
                 lottoResultCount[4]++;
-                winningAmount += 50000;
                 continue;
             }
             if (count == 5 && lottoNumbers.contains(bonusNumber.getBonusNumber())) {
                 lottoResultCount[3]++;
-                winningAmount += 30000000;
                 continue;
             }
             if (count == 5) {
                 lottoResultCount[2]++;
-                winningAmount += 1500000;
                 continue;
             }
             if (count == 4) {
                 lottoResultCount[1]++;
-                winningAmount += 50000;
                 continue;
             }
             if (count == 3) {
                 lottoResultCount[0]++;
-                winningAmount += 5000;
             }
         }
+        return lottoResultCount;
+    }
 
-        outputView.displayWinningStatisticsGuide();
-        outputView.displayWinningStatistics(lottoResultCount);
-        double yield = winningAmount / Double.valueOf(amount) * 100;
-        outputView.displayYield(yield);
+    private int calculateWinnings(int[] lottoResultCount) {
+        int winningAmount = 0;
+
+        winningAmount += lottoResultCount[0] * 5000;
+        winningAmount += lottoResultCount[1] * 50000;
+        winningAmount += lottoResultCount[2] * 1500000;
+        winningAmount += lottoResultCount[3] * 30000000;
+        winningAmount += lottoResultCount[4] * 2000000000;
+
+        return winningAmount;
+    }
+
+    private Double calculateYield(int winningAmount, int lottoCount) {
+        return winningAmount / (lottoCount * 1000.0) * 100;
     }
 }

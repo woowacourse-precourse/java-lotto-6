9aec490902ee86b77a215ff12b9df03a18079045
gyungchan Jo <namejgc@naver.com>
2023-11-04 03:38:44 +0900

    test(LottoTest): 보너스 번호를 포함하는지 확인하는 기능 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
 import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
@@ -15,11 +17,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.*;
 
 class LottoTest {
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
     @Test
     @DisplayName("로또 번호는 1에서 45사이의 중복되지 않는 6개의 숫자를 가진다.")
     void createLotto() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-
         assertThat(lotto).extracting("numbers", InstanceOfAssertFactories.list(Integer.class))
                 .containsExactly(1, 2, 3, 4, 5, 6);
     }
@@ -60,7 +67,6 @@ class LottoTest {
     @ParameterizedTest
     @MethodSource("userLottoAndNumberOfMatches")
     void countNumberOfMatches(Lotto userLotto, int expected) {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int actual = lotto.countNumberOfMatches(userLotto);
 
         assertThat(actual).isEqualTo(expected);
@@ -73,5 +79,14 @@ class LottoTest {
                 arguments(new Lotto(List.of(1, 2, 3, 4, 15, 16)), 4),
                 arguments(new Lotto(List.of(1, 2, 3, 14, 15, 16)), 3)
         );
+    }
+
+    @DisplayName("보너스 번호를 포함하는지 알 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"6, true", "7, false"})
+    void hasBonusNumber(int bonusNumber, boolean expected) {
+        boolean actual = lotto.hasBonusNumber(new BonusNumber(bonusNumber));
+
+        assertThat(actual).isEqualTo(expected);
     }
 }

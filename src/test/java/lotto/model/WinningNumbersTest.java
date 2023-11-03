3c49aee902ad4be2b4f4e9ee8bf7d27d5a1b90da
YongHyeon <pyh4616@naver.com>
2023-11-03 20:53:38 +0900

    Test(WinningNumbersTest 클래스): 당첨 번호 중복에 대한 유효성 검사 기능을 테스트

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -15,4 +15,11 @@ public class WinningNumbersTest {
         assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByDuplicatedNumber() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

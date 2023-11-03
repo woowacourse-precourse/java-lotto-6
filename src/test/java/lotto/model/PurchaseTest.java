32972bf5a959030163fe3927c94b023d6d135caa
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 01:07:08 +0900

    Test: "1000원 단위 테스트 코드 추가"

---
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseTest {
+
+    @DisplayName("로또 구입 가격이 1000원 단위가 아니면 오류가 발생한다.")
+    @Test
+    void validateUnit() {
+        assertThatThrownBy(() -> new Purchase("1200"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

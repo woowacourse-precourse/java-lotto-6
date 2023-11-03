0e65a5226a0bb86cb42d9fb0f9e6b425f82e994c
minseo  jo <liging12@naver.com>
2023-11-03 20:50:18 +0900

    feat: 로또 생성기 인터페이스 추가 (LottoGenerator)
    로또 생성에는, 자동, 수동 방법 두개가 존재한다.
    예를 들어, 사용자가 살 때는 수동 자동 둘다 되지만,
    당첨 번호를 뽑을 때는 수동으로만 뽑아야 한다.

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,39 +1,10 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import lotto.vo.Number;
+import lotto.exception.LottoNumberException;
 
-import java.util.*;
+public interface LottoGenerator {
+    Lotto generate() throws LottoNumberException;
 
-public class LottoGenerator {
-    private static final int LOTTO_SIZE = 6;
-    private static final int LOTTO_MIN_NUMBER = 1;
-    private static final int LOTTO_MAX_NUMBER = 45;
-    
-    public Lottos generateUniqueLottos(int count) {
-        Set<Lotto> uniqueLottos = new HashSet<>();
+    Lottos generate(int count) throws LottoNumberException;
 
-        while (uniqueLottos.size() < count) {
-            List<Integer> randomNumbers = generateRandomNumbers();
-            List<Number> numbers = generateCreateNumbers(randomNumbers);
-            Lotto lotto = generateLotto(numbers);
-            uniqueLottos.add(lotto);
-        }
-
-        return new Lottos(new ArrayList<>(uniqueLottos)); // 명시적 형변환
-    }
-
-    private static List<Integer> generateRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
-    }
-
-    private static List<Number> generateCreateNumbers(List<Integer> randomNumbers) {
-        return randomNumbers.stream()
-                .map(Number::new)
-                .toList();
-    }
-
-    private static Lotto generateLotto(List<Number> randomNumbers) {
-        return new Lotto(randomNumbers);
-    }
 }

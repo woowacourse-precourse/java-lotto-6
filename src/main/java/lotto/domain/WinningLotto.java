e0e31f6c3ad4cee9e0838c1a27f4a48941fe9273
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-03 15:25:48 +0900

    feat: 당첨 로또 추가
    입력으로 로또를 만들고, 보너스 번호와 겹치는지 체크

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.exception.DuplicateNumbersException;
+
+public class WinningLotto {
+
+    private final Lotto lotto;
+    private final Number bonusNumber;
+
+    private WinningLotto(final Lotto lotto, final Number bonusNumber) {
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningLotto of(final List<String> lottoNumbers, final String bonusNumber) {
+        validateLottoNumbers(lottoNumbers, bonusNumber);
+        return createWinningLotto(lottoNumbers, bonusNumber);
+    }
+
+    private static void validateLottoNumbers(final List<String> lottoNumbers, final String bonusNumber) {
+        if (lottoNumbers.contains(bonusNumber)) {
+            throw new DuplicateNumbersException();
+        }
+    }
+
+    private static WinningLotto createWinningLotto(final List<String> lottoNumbers, final String bonusNumber) {
+        Lotto lotto = Lotto.from(lottoNumbers);
+        Number number = Number.from(bonusNumber);
+        return new WinningLotto(lotto, number);
+    }
+}

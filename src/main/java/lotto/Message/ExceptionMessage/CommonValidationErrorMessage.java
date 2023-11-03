be4ca0e8fcf1619c22bc040f8d9bb9af57778bb3
sami355-24 <ddung355@gmail.com>
2023-11-03 18:51:09 +0900

    refactor(Message/ExceptionMessage): 예외 발생시 붙이는 접두사를 enum클래스로 관리하게끔 변경

--- src/main/java/lotto/Message/ExceptionMessage/CommonValidationErrorMessage.java
+++ src/main/java/lotto/Message/ExceptionMessage/CommonValidationErrorMessage.java
@@ -1,14 +1,14 @@
 package lotto.Message.ExceptionMessage;
 
 public enum CommonValidationErrorMessage {
-    RANGE_ERROR_MESSAGE("[ERROR] 1 ~ 45 사이의 숫자만 입력해 주세요. \n 입력값 : %s"),
-    BLANK_VALUE_ERROR_MESSAGE("[ERROR] 공백은 입력할 수 없습니다.\n"),
-    INTEGER_VALUE_ERROR_MESSAGE("[ERROR] 정수만 입력할 수 있습니다.\n 입력값 : %s");
+    RANGE_ERROR_MESSAGE("1 ~ 45 사이의 숫자만 입력해 주세요. \n 입력값 : %s"),
+    BLANK_VALUE_ERROR_MESSAGE("공백은 입력할 수 없습니다.\n"),
+    INTEGER_VALUE_ERROR_MESSAGE("정수만 입력할 수 있습니다.\n 입력값 : %s");
 
     private String message;
 
     CommonValidationErrorMessage(String message) {
-        this.message = message;
+        this.message = PrefixMessage.ERROR_MESSAGE.getMessage() + message;
     }
 
     public String getMessage() {

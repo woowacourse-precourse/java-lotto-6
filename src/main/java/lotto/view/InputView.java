637df8a9069bbe4f97f0ea14f1b05397b7217999
siho-song <thdtlgh234@naver.com>
2023-11-04 01:34:09 +0900

    [Feat] 입력관련 메시지 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,23 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Utils;
 
 public class InputView {
+    private final static String GET_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private final static String GET_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String GET_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     private static void showMessage(String message) {
         System.out.println(message);
     }
 
     public int inputAmount() {
-        showMessage(InputMessage.GET_MONEY.getValue());
+        showMessage(GET_MONEY_MESSAGE);
         return Utils.convertStringToInteger(Console.readLine());
     }
 
     public String inputWinningNumbers() {
-        showMessage(InputMessage.GET_NUMBERS.getValue());
+        showMessage(GET_NUMBERS_MESSAGE);
         return Console.readLine();
     }
 
     public int bonusNumberMessage() {
-        showMessage(InputMessage.GET_BONUS_NUMBER.getValue());
-        return Utils.convertStringToInteger(Console.readLine());
+        showMessage(GET_BONUS_NUMBER_MESSAGE);
+        return Utils.convertStringToInteger(GET_BONUS_NUMBER_MESSAGE);
     }
 }

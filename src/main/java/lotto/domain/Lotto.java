d39dd5c7ed60c0fa1666b49e31914257d29938e7
june-777 <wlwhswnsrl96@gmail.com>
2023-11-03 18:54:46 +0900

    feat(Lotto): 숫자 개수 6개 검증 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,19 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.LottoExceptionMessage;
 
 public class Lotto {
-
+    private static final int REQUIRED_COUNT = 6;
     private final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
         this.lottoNumbers = create(numbers);
+    }
+
+    private void validate(List<Integer> target) {
+        validateSize(target);
     }
 
     private List<LottoNumber> create(List<Integer> numbers) {
@@ -15,5 +21,11 @@ public class Lotto {
                 .map(LottoNumber::new)
                 .distinct()
                 .toList();
+    }
+
+    private void validateSize(List<Integer> target) {
+        if (target.size() != REQUIRED_COUNT) {
+            throw new IllegalArgumentException(LottoExceptionMessage.NOT_SATISFY_REQUIRED_COUNT.getError());
+        }
     }
 }

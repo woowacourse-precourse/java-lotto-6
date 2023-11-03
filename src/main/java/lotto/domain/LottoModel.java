62580ff8876794a613deb1faffe803c2788afddb
mmalfoy <stpmalfoy@gmail.com>
2023-11-04 07:47:53 +0900

    feat(LottoModel) : 로또번호 오름차순 정렬 발행 구현

--- src/main/java/lotto/domain/LottoModel.java
+++ src/main/java/lotto/domain/LottoModel.java
@@ -18,6 +18,33 @@ public class LottoModel {
         this.publishedLottos = new ArrayList<>();
     }
 
+    public void publishLotto(int cost) {
+        List<Integer> randNums;
+        try {
+            for (int i = 0; i < cost; i++) {
+                randNums = generateNewLotto();
+                publishedLottos.add(new Lotto(randNums));
+            }
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    public List<Integer> generateNewLotto() {
+        List<Integer> generated = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return generated;
+    }
+
+    public String publishTicket(List<Integer> lottoNums) {
+        lottoNums.sort(Comparator.naturalOrder());
+        List<String> sortedNums = lottoNums.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+        String Ticket = String.format("[%s]", String.join(",", sortedNums));
+        return Ticket;
+    }
+
+
 
 
 

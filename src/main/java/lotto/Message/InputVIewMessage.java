1389a53139ff9d16fc7314e7caad4176d664988c
sami355-24 <ddung355@gmail.com>
2023-11-03 11:28:48 +0900

    feat(View): InputView에서 사용할 구문을 관리하는 Enum 클래스 생성

---
+++ src/main/java/lotto/Message/InputVIewMessage.java
@@ -0,0 +1,17 @@
+package lotto.Message;
+
+public enum InputVIewMessage {
+
+    INPUTMONEYPROMPT("구입금액을 입력해 주세요."),
+    INPUTWINNINGNUMBERPROMPT("\n당첨 번호를 입력해 주세요."),
+    INPUTBONUSNUMBERPROMPT("\n보너스 번호를 입력해 주세요.");
+    private String message;
+
+    InputVIewMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

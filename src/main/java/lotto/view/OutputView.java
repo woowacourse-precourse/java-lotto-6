4e710b501a4d0b2b457db2101041346804100df7
jun <ggprgrkjh@naver.com>
2023-11-03 16:53:33 +0900

    feat(outputview): 로또 구매 갯수 출력 추가 with TDD

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,7 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public static void printBuyCount(int count) {
+        System.out.println("\n" + count + "개를 구매했습니다.");
+    }
 }
--- src/test/java/lotto/OutputViewTest.java
+++ src/test/java/lotto/OutputViewTest.java
@@ -5,6 +5,8 @@ import java.io.PrintStream;
 import lotto.view.OutputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -28,6 +30,18 @@ public class OutputViewTest {
         OutputView.printRequestBuyAmount();
         assertThat(output.toString()).isEqualTo("구입금액을 입력해 주세요.\n");
     }
+
+    @DisplayName("로또 구매 갯수 메세지 정상 작동 확인 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void printBuyCount_Different_ExceptionThrow(int count){
+        ByteArrayOutputStream output = captureOutputValues();
+        OutputView.printBuyCount(count);
+        assertThat(output.toString()).isEqualTo("\n"+count+"개를 구매했습니다.\n");
+    }
+
+
+
 
 
 }

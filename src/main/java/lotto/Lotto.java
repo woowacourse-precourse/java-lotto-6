2b0081cca0770c85e1a0ed3f11bd594042b58836
pokycookie <0cookieboy0@gmail.com>
2023-11-03 14:27:14 +0900

    feat: 로또 번호 검증 메서드
    - 중복된 숫자 확인 메서드
    - 범위를 벗어난 숫자 확인 메서드
    - Lotto 도메인 로직 테스트 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicates(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +19,15 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicates(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().filter(it -> it < 1 || it > 45).toList().size() > 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

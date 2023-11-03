6df2dfd99059a0c34d805c92ad7295e544e3a7fa
bboodd <92453279+bboodd@users.noreply.github.com>
2023-11-04 07:31:15 +0900

    feat: Lotto class add validate method and test
    로또 클래스의 검증 로직 추가와 테스트

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,37 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers){
+        Set<Integer> setNumbers = new HashSet<>(numbers);
+        if(setNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for(int number : numbers){
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException();
+            }
         }
     }
 
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 숫자가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,45,2,3,4,46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     // 아래에 추가 테스트 작성 가능
 }

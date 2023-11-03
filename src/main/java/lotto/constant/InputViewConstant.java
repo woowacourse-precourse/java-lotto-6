6933a57fb44efd81e86f666dd432c9a39f69b0b7
euics <pjh575157@gmail.com>
2023-11-03 13:51:44 +0900

    refactor(OutputView): InputView 클래스 상수 추출
    수정사항
    - InputView에서 사용되는 상수는 InputViewConstant에서 관리

---
+++ src/main/java/lotto/constant/InputViewConstant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class InputViewConstant {
+    public static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,21 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.InputViewConstant;
 
 public class InputView {
     public static void promptPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(InputViewConstant.PURCHASE_AMOUNT_PROMPT);
     }
 
     public static String readPurchaseAmount() {
         String input = Console.readLine();
         System.out.println();
-
         return input;
     }
 
     public static void promptWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(InputViewConstant.WINNING_NUMBERS_PROMPT);
     }
 
     public static String readWinningNumbers() {
@@ -25,7 +25,7 @@ public class InputView {
     }
 
     public static void promptBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(InputViewConstant.BONUS_NUMBER_PROMPT);
     }
 
     public static String readBonusNumber() {

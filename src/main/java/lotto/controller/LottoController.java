c3d8d43a8d8ec9e41c496724d8a186d6cee5fd62
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 01:28:32 +0900

    feat(LottoController): 구입 수량만큼 로또 생성 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,30 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.RandomNumbers;
 import lotto.view.InputView;
 
 public class LottoController {
     private static int amount;
 
-    private int getPurchaseAmount() {
+    private int getQuantity() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(InputView.inputAmount());
         amount = Integer.parseInt(purchaseAmount.getAmount());
         return amount / 1000;
+    }
+
+    private List<Lotto> purchaseLotto(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+        RandomNumbers randomNumbers = new RandomNumbers();
+
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> lottoNumbers = randomNumbers.generateRandomNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }

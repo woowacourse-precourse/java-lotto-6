26f0a84d2d9034c8758f8f2e8f2f273cc7f160cc
eora21 <eora21@naver.com>
2023-11-03 20:10:23 +0900

    style: 💄 (LottoPurchasingMachine) 수정 검증 메서드명 validate로 통일

--- src/main/java/domain/LottoPurchasingMachine.java
+++ src/main/java/domain/LottoPurchasingMachine.java
@@ -6,18 +6,18 @@ import exception.NotCorrectUnitAmountException;
 public class LottoPurchasingMachine {
     private static final int LOTTO_UNIT_AMOUNT = 1_000;
     public int purchaseOfLottoTickets(int purchaseAmount) {
-        verifyAmount(purchaseAmount);
-        verifyPurchaseAmountWithUnitAmount(purchaseAmount);
+        validateAmount(purchaseAmount);
+        validatePurchaseAmountWithUnitAmount(purchaseAmount);
         return purchaseAmount / LOTTO_UNIT_AMOUNT;
     }
 
-    private void verifyAmount(int purchaseAmount) {
+    private void validateAmount(int purchaseAmount) {
         if (purchaseAmount <= 0) {
             throw new ImpossiblePayAmountException();
         }
     }
 
-    private void verifyPurchaseAmountWithUnitAmount(int purchaseAmount) {
+    private void validatePurchaseAmountWithUnitAmount(int purchaseAmount) {
         if (purchaseAmount % LOTTO_UNIT_AMOUNT != 0) {
             throw new NotCorrectUnitAmountException(LOTTO_UNIT_AMOUNT);
         }

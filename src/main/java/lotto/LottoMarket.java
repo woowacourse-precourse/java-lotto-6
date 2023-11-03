4811795226a412c6d6dca03998dbf88d5647a636
hwangkyojune <shane5969@naver.com>
2023-11-04 00:51:19 +0900

    LottoMarket 뼈대코드 구현

---
+++ src/main/java/lotto/LottoMarket.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoMarket {
+    public List<Lotto> issueLottoes(){
+        return null;
+    }
+
+    public int calculateLottoCount(){
+        return 0;
+    }
+
+    public void validateMoney(){
+    }
+
+}

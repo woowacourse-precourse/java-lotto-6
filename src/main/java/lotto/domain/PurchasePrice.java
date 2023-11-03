448bf7bce551a7241037e59aab23857b77c23801
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-03 19:04:35 +0900

    feat: 구매할 수 있는 로또의 개수를 구하기 구현

--- src/main/java/lotto/domain/PurchasePrice.java
+++ src/main/java/lotto/domain/PurchasePrice.java
@@ -1,8 +1,14 @@
 package lotto.domain;
 
+import static lotto.constant.ErrorMessage.NOT_DIVISIBLE_MESSAGE;
+
 import lotto.validator.InputValidator;
+import lotto.view.Output;
 
 public class PurchasePrice {
+    //static 적어주어야하나
+    private static final int NOT_DIVISIBLE_VALUE = 1;
+    private static final int UNIT_PRICE = 1000;
     private final int price;
 
     private PurchasePrice(String input) {
@@ -17,7 +23,13 @@ public class PurchasePrice {
     private void validate(String input) {
         InputValidator.onlyNumber(input);
         InputValidator.inRange(input);
-        InputValidator.notNegative(input);
+        validateDivisible(Integer.parseInt(input));
+    }
 
+    private void validateDivisible(int price) {
+        if (price % UNIT_PRICE == NOT_DIVISIBLE_VALUE) {
+            Output.errorMessage(NOT_DIVISIBLE_MESSAGE);
+            throw new IllegalArgumentException(NOT_DIVISIBLE_MESSAGE);
+        }
     }
 }

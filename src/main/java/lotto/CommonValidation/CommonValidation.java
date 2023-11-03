e1090d06c479e61688682a9ce5612cd6e40e6b02
sami355-24 <ddung355@gmail.com>
2023-11-03 20:37:21 +0900

    chore: 패키지 이름 수정

--- src/main/java/lotto/CommonValidation/CommonValidation.java
+++ src/main/java/lotto/CommonValidation/CommonValidation.java
@@ -1,27 +1,26 @@
-package lotto.Validation;
+package lotto.CommonValidation;
 
 import static lotto.Message.ExceptionMessage.CommonValidationErrorMessage.*;
 import static lotto.Util.Util.ConvertStringToInteger;
 
 import lotto.Exception.CommonValidationException;
-import lotto.Message.ExceptionMessage.CommonValidationErrorMessage;
-import lotto.Util.Util;
 
 public class CommonValidation {
 
-    public void isValidRange(Integer value) {
+
+    public static void isValidNumberRange(Integer value) {
         if (value < 1 || value > 45) {
             throw new CommonValidationException(RANGE_ERROR_MESSAGE.getMessage(value));
         }
     }
 
-    public void isBlank(String value) {
+    public static void isBlank(String value) {
         if (value.contains(" ")) {
             throw new CommonValidationException(BLANK_VALUE_ERROR_MESSAGE.getMessage(value));
         }
     }
 
-    public void isInteger(String value) {
+    public static void isInteger(String value) {
         try {
             ConvertStringToInteger(value);
         } catch (NumberFormatException e) {
--- src/test/java/lotto/CommonValidation/CommonValidationTest.java
+++ src/test/java/lotto/CommonValidation/CommonValidationTest.java
@@ -1,38 +1,31 @@
-package lotto.Validation;
+package lotto.CommonValidation;
 
+import static lotto.CommonValidation.CommonValidation.isBlank;
+import static lotto.CommonValidation.CommonValidation.isInteger;
+import static lotto.CommonValidation.CommonValidation.isValidNumberRange;
 import static lotto.Message.ExceptionMessage.CommonValidationErrorMessage.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import lotto.Exception.CommonValidationException;
-import lotto.Message.ExceptionMessage.CommonValidationErrorMessage;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CommonValidationTest {
 
-    CommonValidation commonValidation;
-
-    @BeforeEach
-    void setUp() {
-        commonValidation = new CommonValidation();
-    }
-
     @Test
     @DisplayName("숫자가 범위 밖에 있을때 예외가 발생하는지 확인하는 테스트")
-    void isValidRange1() {
+    void isValidRangeTest1() {
         //given
         Integer value1 = 0;
         Integer value2 = 46;
 
         //when, then
-        assertThatThrownBy(() -> commonValidation.isValidRange(value1))
+        assertThatThrownBy(() -> isValidNumberRange(value1))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(RANGE_ERROR_MESSAGE.getMessage(value1));
 
-        assertThatThrownBy(() -> commonValidation.isValidRange(value2))
+        assertThatThrownBy(() -> isValidNumberRange(value2))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(RANGE_ERROR_MESSAGE.getMessage(value2));
     }
@@ -40,14 +33,14 @@ class CommonValidationTest {
 
     @Test
     @DisplayName("숫자가 범위 안에 있을때 정상 수행하는지 확인하는 테스트")
-    void isValidRange2() {
+    void isValidRangeTest2() {
         //given
         Integer value1 = 1;
         Integer value2 = 45;
 
         //when
-        commonValidation.isValidRange(value1);
-        commonValidation.isValidRange(value2);
+        isValidNumberRange(value1);
+        isValidNumberRange(value2);
 
         //then
         assertThat(true).isTrue();
@@ -55,24 +48,24 @@ class CommonValidationTest {
 
     @Test
     @DisplayName("공백이 있을때 예외가 발생하는지 확인하는 테스트")
-    void isBlank1() {
+    void isBlankTest1() {
         //given
         String value = " ";
 
         //when
-        assertThatThrownBy(() -> commonValidation.isBlank(value))
+        assertThatThrownBy(() -> isBlank(value))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(BLANK_VALUE_ERROR_MESSAGE.getMessage(value));
     }
 
     @Test
     @DisplayName("공백이 없을때 정상 동작하는지에 대한 테스트")
-    void isBlank2() {
+    void isBlankTest2() {
         //given
         String value = "1";
 
         //when
-        commonValidation.isBlank(value);
+        isBlank(value);
 
         //then
         assertThat(true).isTrue();
@@ -80,30 +73,30 @@ class CommonValidationTest {
 
     @Test
     @DisplayName("정수가 아닐때 예외가 발생하는지 확인하는 테스트")
-    void isInteger() {
+    void isIntegerTest1() {
         //given
         String value1 = "1.1";
         String value2 = "a";
 
         //when
-        assertThatThrownBy(() -> commonValidation.isInteger(value1))
+        assertThatThrownBy(() -> isInteger(value1))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(INTEGER_VALUE_ERROR_MESSAGE.getMessage(value1));
 
         //then
-        assertThatThrownBy(() -> commonValidation.isInteger(value2))
+        assertThatThrownBy(() -> isInteger(value2))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(INTEGER_VALUE_ERROR_MESSAGE.getMessage(value2));
     }
 
     @Test
     @DisplayName("정수일때 정상 동작하는지에 대한 테스트")
-    void isInteger2() {
+    void isIntegerTest2() {
         //given
         String value = "1";
 
         //when
-        commonValidation.isInteger(value);
+        isInteger(value);
 
         //then
         assertThat(true).isTrue();

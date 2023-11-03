a47d48f02be4c097bcc8efd21f94e0bb90ca26fc
youngkyunIm <yee0230@naver.com>
2023-11-03 16:20:35 +0900

    refactor : 예외처리를 메세지 통일을 위한 장치

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,10 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.IntStream;
+import lotto.exception.ExceptionManager;
 
 public class Lotto {
 

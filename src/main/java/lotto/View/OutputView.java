9846d33b31d08443b7984297d0e89e74d72a2fc0
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 16:08:01 +0900

    feat(OutputView): 구입한 로또 개수 메시지 출력과 랜덤로또번호들 출력 기능 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -3,10 +3,15 @@ package lotto.View;
 import java.util.Arrays;
 import java.util.List;
 import lotto.Model.Lotto;
+import lotto.Model.RandomLotto;
 
 public class OutputView {
 
-    public void printRandomLotto(Lotto lotto) {
+    public static void printLottoPurChaseMessage(RandomLotto randomLotto) {
+        System.out.println();
+        System.out.println(randomLotto.count() + "개를 구매했습니다.");
+    }
+    public static void printRandomLotto(Lotto lotto) {
         System.out.println(lotto.toString());
     }
 }

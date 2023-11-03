2fc99124fbbe76579eb3430785d9e32fec3bba0b
Juhyorim <joohr1234@gmail.com>
2023-11-04 04:50:14 +0900

    test: 로또 구입금액이 숫자가 아니면 예외발생 테스트 작성

---
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -0,0 +1,39 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class InputValidatorTest {
+    private static final String NO_A_NUMBER_MESSAGE = "로또 금액은 숫자여야 합니다";
+
+    private static InputValidator inputValidator;
+
+    @BeforeAll
+    static void init() {
+        //given
+        inputValidator = new InputValidator();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000", "5000", "100000", "10000000"})
+    @DisplayName("로또 구입금액이 정상적인 값일 때")
+    void goodCase(String userInput) {
+        assertDoesNotThrow(() -> {
+            inputValidator.validatePrice(userInput);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"가나다", "ㅇ", "흥", "1000."})
+    @DisplayName("로또 구입금액이 숫자가 아니면 예외")
+    void 로또_구입금액이_숫자가_아니면_예외(String userInput) {
+        assertThatThrownBy(() -> inputValidator.validatePrice(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NO_A_NUMBER_MESSAGE);
+    }
+}

abc6d19375bfc19fcc9345f0ef66875a5190c27c
scarlet0o0 <ghd7673@naver.com>
2023-11-03 19:04:09 +0900

    Feat : InputVIew 입력 받는 메소드 3개 작성

--- src/main/java/lotto/view/InputVIew.java
+++ src/main/java/lotto/view/InputVIew.java
@@ -1,5 +1,23 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+
 public class InputVIew {
+    private static final String COMMA = ",";
+
+    public int promptLottoPurchaseAmount() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public int[] promptLottoWinningNumbers() {
+        return Arrays.stream(Console.readLine().split(COMMA))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    public int promptLottoBonusNumber() {
+        return Integer.parseInt(Console.readLine());
+    }
 
 }

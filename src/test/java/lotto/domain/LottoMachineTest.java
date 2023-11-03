2131c1c0e74f4c1b5bc8f809eede7c0f6cc0dbed
IMWoo94 <leemeawoo94@gmail.com>
2023-11-03 17:51:26 +0900

    test : 로또 당첨 결고 테스트 작성
    로또 당첨 결고 테스트 작성

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoMachineTest {
@@ -20,9 +22,35 @@ class LottoMachineTest {
         Assertions.assertThat(lottos.size()).isEqualTo(quantity);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("로또 당첨 번호 비교 테스트")
-    void lottoNumberWinningComparisonTest() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+    @MethodSource("initWitLottoData")
+    void lottoNumberWinningComparisonTest(
+            List<Integer> winLottoNumber
+            , int bonusNumber
+            , Lotto otherLotto
+            , LottoRank reuslt
+    ) {
+        LottoMachine instance = LottoMachine.getInstance();
+        instance.setWinLotto(new WinLotto(new Lotto(winLottoNumber), bonusNumber));
+
+        Assertions.assertThat(instance.lottoWinningResult(otherLotto)).isEqualTo(reuslt);
+    }
+
+    static Stream<Arguments> initWitLottoData() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 45)
+                        , 44
+                        , new Lotto(List.of(1, 2, 3, 4, 5, 45))
+                        , LottoRank.FIRST_PRIZE),
+                Arguments.of(List.of(2, 18, 3, 4, 5, 6)
+                        , 23
+                        , new Lotto(List.of(2, 18, 3, 4, 5, 23))
+                        , LottoRank.SECOND_PRIZE),
+                Arguments.of(List.of(15, 2, 23, 43, 5, 6)
+                        , 19
+                        , new Lotto(List.of(3, 4, 7, 8, 9, 10))
+                        , LottoRank.NO_PRIZE)
+        );
     }
 }

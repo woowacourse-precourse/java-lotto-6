98776dac208f817782efeaa3ade234d239a46081
Firedrago95 <yonghwa95@naver.com>
2023-11-03 13:50:59 +0900

    refactor(Budget) : 유효성 검사를 Validator 에서 Budget 내부에서 직접 하도록 변경

--- src/main/java/lotto/Domain/Budget.java
+++ src/main/java/lotto/Domain/Budget.java
@@ -1,15 +1,35 @@
 package lotto.Domain;
 
-import lotto.Util.Validator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Budget {
 
+    public static final String NULL_OR_EMPTY_MESSAGE = "[Error] 값을 입력해주세요";
+    public static final String EMPTY = "";
+    public static final String THOUSANDS_MULTIPLE_MESSAGE = "[Error] 1000의 배수를 입력해주세요";
+    public static final String THOUSANDS_MULTIPLE_REGEX = "^[1-9][0-9]*000$";
     public static final int LOTTO_PRICE = 1000;
     private int budgetAmount;
 
-    public Budget(String budget) throws IllegalArgumentException{
-        Validator.validateBudget(budget);
+    public Budget(String budget) throws IllegalArgumentException {
+        validateNullOrEmpty(budget);
+        validateBudgetInThousands(budget);
         budgetAmount = Integer.parseInt(budget);
+    }
+
+    private static void validateNullOrEmpty(String input) throws IllegalArgumentException {
+        if (input == null || input.equals(EMPTY)) {
+            throw new IllegalArgumentException(NULL_OR_EMPTY_MESSAGE);
+        }
+    }
+
+    private static void validateBudgetInThousands(String input) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile(THOUSANDS_MULTIPLE_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(THOUSANDS_MULTIPLE_MESSAGE);
+        }
     }
 
     public int calculateLottoCount() {

20be09c6d65aef9bc519309dab81a4614809279e
sami355-24 <ddung355@gmail.com>
2023-11-04 01:55:40 +0900

    feat(Model): 구매한 로또 한 장과 당첨로또를 비교하는 메서드 작성

--- src/main/java/lotto/Model/LottoResultHandlerModel.java
+++ src/main/java/lotto/Model/LottoResultHandlerModel.java
@@ -1,5 +1,8 @@
 package lotto.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Domain.Lotto;
 import lotto.Domain.LottoGroup;
 import lotto.Domain.LottoResult.LottoResult;
 import lotto.Domain.WinningLotto.WinningLotto;
@@ -11,7 +14,15 @@ public class LottoResultHandlerModel {
     private LottoGroup lottoGroup;
 
     public LottoResultHandlerModel(WinningLotto winningLotto, LottoGroup lottoGroup) {
+        lottoResult = new LottoResult();
         this.winningLotto = winningLotto;
         this.lottoGroup = lottoGroup;
+    }
+
+
+    public Integer checkWinning(Lotto winningLotto, Lotto purchasedLotto) {
+        List<Integer> matchNumbers = new ArrayList<>(purchasedLotto.getNumbers());
+        matchNumbers.retainAll(winningLotto.getNumbers());
+        return matchNumbers.size();
     }
 }

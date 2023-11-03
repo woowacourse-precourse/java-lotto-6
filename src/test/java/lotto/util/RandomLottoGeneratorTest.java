4087d13428f8c554e2e63565acf85aa22cf046cc
zangsu <zangsu_@naver.com>
2023-11-03 18:44:49 +0900

    test : create RandomLottoGeneratorTest
    test to check RandomLottoGenerator.generate() does not throw exception

---
+++ src/test/java/lotto/util/RandomLottoGeneratorTest.java
@@ -0,0 +1,18 @@
+package lotto.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RandomLottoGeneratorTest {
+
+    @Test
+    @DisplayName("로또가 정상적으로 생성됨")
+    public void 정상_동작() throws Exception{
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> RandomLottoGenerator.generate());
+    }
+
+}

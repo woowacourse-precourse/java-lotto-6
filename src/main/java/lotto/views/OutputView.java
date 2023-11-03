8100f83cdbaecf44cc4f0874f31dc3485b6ac63a
Juho Lee <ailiartsuaL2@gmail.com>
2023-11-03 17:36:10 +0900

    refactor : OutputView 리팩토링
    - 문자열 합치는 메서드 추가하여 분리
    - 기능별 줄 바꿈 적용

--- src/main/java/lotto/views/OutputView.java
+++ src/main/java/lotto/views/OutputView.java
@@ -12,42 +12,67 @@ public class OutputView {
     public static final String PURCHASE_LOTTO_OUTPUT = "개를 구매했습니다.";
 
     public void lottoQuantityAndNumber(List<Lotto> publishedLotto) {
-        System.out.println(publishedLotto.size()+PURCHASE_LOTTO_OUTPUT);
+        System.out.println(publishedLotto.size() + PURCHASE_LOTTO_OUTPUT);
         for (Lotto lotto : publishedLotto) {
             List<Integer> sortedNumbers = lotto.getNumbers();
             StringBuilder stringBuilder = new StringBuilder("[");
-            for (Integer number : sortedNumbers) {
-                stringBuilder.append(number);
-                stringBuilder.append(", ");
-            }
-            int length = stringBuilder.length();
-            stringBuilder.delete(length-2,length);
+            stringBuilder.append(joinNumbers(",", sortedNumbers));
             stringBuilder.append("]");
             System.out.println(stringBuilder.toString());
         }
     }
 
-    public void winningRecord(Map<Prize,Integer> lotteryResult){
-        StringBuilder outputBuilder = new StringBuilder("당첨 통계\n"+"---\n");
-        for (Prize prize : lotteryResult.keySet()) {
-            int matchCount = prize.getMatchCount();
-            String reward = Utils.NUMBER_FORMAT_US.format(prize.getReward());
-            int count = lotteryResult.get(prize);
-            outputBuilder.append(matchCount);
-            outputBuilder.append("개 일치");
-            if(Prize.SECOND_REWARD.equals(prize)){
-                outputBuilder.append(", 보너스 볼 일치");
-            }
-            outputBuilder.append(" (");
-            outputBuilder.append(reward);
-            outputBuilder.append("원) - ");
-            outputBuilder.append(count);
-            outputBuilder.append("개\n");
+    private String joinNumbers(CharSequence delimiter, List<Integer> numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer number : numbers) {
+            stringBuilder.append(number);
+            stringBuilder.append(delimiter);
+            stringBuilder.append(" ");
         }
+
+        int length = stringBuilder.length();
+        stringBuilder.delete(length - 2, length);
+
+        return stringBuilder.toString();
+    }
+
+    public void winningRecord(Map<Prize, Integer> lotteryResult) {
+        StringBuilder outputBuilder = new StringBuilder("당첨 통계\n" + "---\n");
+
+        for (Prize prize : lotteryResult.keySet()) {
+            int count = lotteryResult.get(prize);
+            outputBuilder.append(joinPrize(prize, count));
+        }
+
         System.out.println(outputBuilder.toString());
     }
 
-    public void rewardRatioRecord(double rewardRatio){
+    public String joinPrize(Prize prize, int count){
+        StringBuilder outputBuilder = new StringBuilder();
+        int matchCount = prize.getMatchCount();
+        String reward = Utils.NUMBER_FORMAT_US.format(prize.getReward());
+
+        outputBuilder.append(matchCount);
+        outputBuilder.append("개 일치");
+        outputBuilder.append(checkSecondPrize(prize));
+        outputBuilder.append(reward);
+        outputBuilder.append("원) - ");
+        outputBuilder.append(count);
+        outputBuilder.append("개\n");
+
+        return outputBuilder.toString();
+    }
+
+    private String checkSecondPrize(Prize prize) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (Prize.SECOND_REWARD.equals(prize)) {
+            stringBuilder.append(", 보너스 볼 일치");
+        }
+        stringBuilder.append(" (");
+        return stringBuilder.toString();
+    }
+
+    public void rewardRatioRecord(double rewardRatio) {
         StringBuilder outputBuilder = new StringBuilder("총 수익률은 ");
         outputBuilder.append(rewardRatio);
         outputBuilder.append("%입니다.");

2378205ff6320a1eb91863c5e34b42a87f53123b
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-03 23:23:25 +0900

    feat: NumberConstant 추가

---
+++ src/main/java/lotto/domain/constant/NumberConstant.java
@@ -0,0 +1,11 @@
+package lotto.domain.constant;
+
+public class NumberConstant {
+    // LOTTO
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_NUMBER_START = 1;
+    public static final int LOTTO_NUMBER_END = 45;
+
+    // TEST CASE
+    public static final int TEST_TICKET_NUM = 5;
+}

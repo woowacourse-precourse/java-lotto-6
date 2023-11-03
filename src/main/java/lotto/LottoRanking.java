6ecda2e0ee9dcdb3522b48378553bb0c58a38268
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-03 23:34:18 +0900

    feat: 로또 상금 및 당첨 조건에 관한 정보를 담은 LottoRanking 클래스 구현

---
+++ src/main/java/lotto/LottoRanking.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum LottoRanking {
+    FIRST(2_000_000_000, 6),
+    SECOND(30_000_000, 5),
+    THIRD(1_500_000, 5),
+    FOURTH(50_000, 4),
+    FIFTH(5_000, 3),
+    NO_LUCK(0, 2),
+    ;
+
+
+    private final int prize;
+    private final int correctNumberCount;
+
+    LottoRanking(int prize, int correctNumberCount) {
+        this.prize = prize;
+        this.correctNumberCount = correctNumberCount;
+    }
+
+    public static LottoRanking findBy(int correctNumberCount, boolean isCorrectBonusNumber) {
+        if (correctNumberCount == 5) {
+            if (isCorrectBonusNumber) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+
+        return Arrays.stream(values())
+                .filter(rank -> rank.correctNumberCount == correctNumberCount)
+                .findFirst()
+                .orElse(NO_LUCK);
+    }
+}

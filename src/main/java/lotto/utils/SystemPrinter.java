fe73227281f9d5f5271284018a8ae2e8e6e6fc8f
hackertaco <imsa1224@korea.ac.kr>
2023-11-03 21:59:32 +0900

    feat(Printer): add Printer interface and SystemPrinter implementation for expanding output print

---
+++ src/main/java/lotto/utils/SystemPrinter.java
@@ -0,0 +1,14 @@
+package lotto.utils;
+
+public class SystemPrinter implements Printer{
+
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void printf(String format, Object... args) {
+        System.out.printf(format, args);
+    }
+}

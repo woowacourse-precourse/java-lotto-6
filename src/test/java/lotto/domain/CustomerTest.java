1cf6f08c4a650260057489bf636e700e8f5406a7
suihub <qkdcrt@gmail.com>
2023-11-03 20:40:50 +0900

    <test(CustomerTest)> : "손님 객체 검증 테스트 구현"
    - Customer 검증 테스트를 먼저 구현
    - Customer 객체 형태만 먼저 작성

---
+++ src/test/java/lotto/domain/CustomerTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CustomerTest {
+    @DisplayName("숫자를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void createCustomerByNotNumber() {
+        assertThatThrownBy(() -> new Customer("테스트!"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자를 입력해주세요.");
+    }
+}

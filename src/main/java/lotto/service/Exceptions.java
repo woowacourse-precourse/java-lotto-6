9ffed2bdc80f78c27466a69388326d1dff759099
seonghun <sdzx0719@naver.com>
2023-11-03 20:18:27 +0900

    feat(exception): 1000원 단위 금액만 입력가능하도록 예외처리

--- src/main/java/lotto/service/Exceptions.java
+++ src/main/java/lotto/service/Exceptions.java
@@ -1,4 +1,11 @@
 package lotto.service;
 
 public class Exceptions {
+    public void  isInvalidPurchaseAmount(String money){
+        try {
+            int intMoney = Integer.parseInt(money);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력 가능합니다.");
+        }
+    }
 }

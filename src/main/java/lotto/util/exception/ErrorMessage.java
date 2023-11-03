651979fdef2a5e02278892a4b1e5c786b9881c0c
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-03 17:05:32 +0900

    feat: 에러 메세지 추가

--- src/main/java/lotto/util/exception/ErrorMessage.java
+++ src/main/java/lotto/util/exception/ErrorMessage.java
@@ -7,7 +7,8 @@ public enum ErrorMessage {
     INVALID_RANGE("%s 번호는 1부터 45 사이의 숫자여야 합니다."),
     INVALID_LOTTO_COUNT("로또 번호는 6개를 모두 입력해야 합니다."),
     INVALID_BONUS_NUMBER_COUNT("보너스 넘버는 1개만 입력해야 합니다."),
-    INVALID_DUPLICATE_NUMBER("중복된 번호가 있습니다.");
+    INVALID_DUPLICATE_NUMBER("중복된 번호가 있습니다."),
+    INVALID_BLANK("공백은 허용되지 않습니다.");
 
     private final String errorMessage;
 

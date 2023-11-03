bd08d3e892076def41bf99ea440de1f9f395ddbf
HungKungE <jonsun59@gmail.com>
2023-11-03 16:33:14 +0900

    [utils|const] printUtil, constMessage 추가

---
+++ src/main/java/lotto/constValue/ConstMessage.java
@@ -0,0 +1,25 @@
+package lotto.constValue;
+
+public class ConstMessage {
+    public static final String PRICE_PROMPT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_PROMPT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_PROMPT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+    public static final String TOTAL_WINNING_RESULT_MESSAGE = "당첨 통계";
+
+    public static final String FIFTH_PLACE_MESSAGE = "3개 일치 (5,000원) - ";
+    public static final String FOURTH_PLACE_MESSAGE = "4개 일치 (50,000원) - ";
+    public static final String THIRD_PLACE_MESSAGE = "5개 일치 (1,500,000원) - ";
+    public static final String SECOND_PLACE_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String FIRST_PLACE_MESSAGE = "6개 일치 (2,000,000,000원) - ";
+
+    public static final String PLACE_MESSAGE_FOOTER = "개";
+
+    public static final String PROFIT_MESSAGE_HEADER = "총 수익률은 ";
+    public static final String PROFIT_MESSAGE_FOOTER = "%입니다.";
+
+    public static final String VALIDATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String VALIDATE_SIZE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 6개 입력 되어야 합니다.";
+    public static final String VALIDATE_BLINK_ERROR_MESSAGE = "[ERROR] 당첨 번호 입력 시, 공백은 포함될 수 없습니다.";
+}
---
+++ src/main/java/lotto/utils/PrintUtil.java
@@ -0,0 +1,43 @@
+package lotto.utils;
+
+import static lotto.constValue.ConstMessage.*;
+
+public class PrintUtil {
+    public static void printPricePrompt(){
+        System.out.println(PRICE_PROMPT_MESSAGE);
+    }
+
+    public static void printWinningNumberPrompt(){
+        System.out.println(WINNING_NUMBER_PROMPT_MESSAGE);
+    }
+
+    public static void printBonusNumberPrompt(){
+        System.out.println(BONUS_NUMBER_PROMPT_MESSAGE);
+    }
+
+    public static void printLottoCount(String lottoCount){
+        System.out.println(lottoCount + LOTTO_COUNT_MESSAGE);
+    }
+    public static void printTotalWinningResult(){
+        System.out.println(TOTAL_WINNING_RESULT_MESSAGE);
+        System.out.println("---");
+    }
+    public static void printFifthPlace(int count){
+        System.out.println(FIFTH_PLACE_MESSAGE + count + PLACE_MESSAGE_FOOTER);
+    }
+    public static void printFourthPlace(int count){
+        System.out.println(FOURTH_PLACE_MESSAGE + count + PLACE_MESSAGE_FOOTER);
+    }
+    public static void printThirdPlace(int count){
+        System.out.println(THIRD_PLACE_MESSAGE + count + PLACE_MESSAGE_FOOTER);
+    }
+    public static void printSecondPlace(int count){
+        System.out.println(SECOND_PLACE_MESSAGE + count + PLACE_MESSAGE_FOOTER);
+    }
+    public static void printFirstPlace(int count){
+        System.out.println(FIRST_PLACE_MESSAGE + count + PLACE_MESSAGE_FOOTER);
+    }
+    public static void printProfit(double profit){
+        System.out.println(PROFIT_MESSAGE_HEADER + profit + PROFIT_MESSAGE_FOOTER );
+    }
+}

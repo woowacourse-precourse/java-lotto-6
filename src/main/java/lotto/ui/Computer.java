0158273f9a2fed3ae28db9d666314160aca3f20f
SeongIl <biuld1234@gmail.com>
2023-11-03 16:21:33 +0900

    feat: add 리스트 크기 6개 체크

--- src/main/java/lotto/ui/Computer.java
+++ src/main/java/lotto/ui/Computer.java
@@ -57,14 +57,21 @@ public class Computer {
 
     }
 
-    public void checkDuplication(List<Integer> userInput){
-        if(userInput.size() != new HashSet<>(userInput).size()){
+    public void checkDuplication(List<Integer> userInput) {
+        if (userInput.size() != new HashSet<>(userInput).size()) {
             throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해 주세요.");
         }
     }
-    public void isIncluding(List<Integer> lottoNumbers, Integer bonusNumber){
-        if(lottoNumbers.contains(bonusNumber)){
+
+    public void isIncluding(List<Integer> lottoNumbers, Integer bonusNumber) {
+        if (lottoNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해 주세요.");
+        }
+    }
+
+    public void checkSize(List<String> userInputs) {
+        if (userInputs.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해 주세요.");
         }
     }
 

04ec0c048680774dc5988c9d9eb41283ac49baf1
elephantLikesApple <elephant890@naver.com>
2023-11-03 16:18:52 +0900

    feat(Lotto) : 로또 번호가 범위를 벗어난 경우 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,7 +13,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6) { // TODO: 6 상수처리
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개가 필요합니다.");
         }
 
@@ -21,8 +21,10 @@ public class Lotto {
             if (Collections.frequency(numbers, number) > 1) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
             }
+
+            if (number < 1 || 45 < number) { // TODO: 1, 45 상수처리
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.");
+            }
         }
     }
-
-    // TODO: 추가 기능 구현
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,5 +22,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 범위가 1 ~ 45를 벗어난 경우 예외가 발생한다.")
+    @Test
+    void createLottoByInvalidNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 60)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

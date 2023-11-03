8119b59f7bacb3c87141405063332c9f969aee81
HwangSeokJin <ghkd3703@gmail.com>
2023-11-03 23:11:49 +0900

    style: 변수, 함수 이름 변경

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,2 +1,30 @@
-package lotto;public class LottoMachine {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int PRICE = 1000;
+
+    public List<Lotto> buy(int money) throws IllegalArgumentException {
+        validateMoney(money);
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < money / PRICE; i++) {
+            lottos.add(getRandomLotto());
+        }
+        return lottos;
+    }
+
+    private void validateMoney(int cash) {
+        if (cash % PRICE != 0) {
+            throw new IllegalArgumentException("1,000원으로 나누어 떨어지지 않습니다");
+        }
+    }
+
+    private Lotto getRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 }

2332b8a04f84845a37c3c7241e68e8c0a0746017
lee jihyeon <lejinhy@gmail.com>
2023-11-03 23:09:04 +0900

    feat: 입력 뒤에 공백 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,17 +6,26 @@ public class InputView {
 
     public String requestPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Console.readLine();
+        String purchasePrice = Console.readLine();
+        System.out.println();
+
+        return purchasePrice;
     }
 
     public String requestWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        String winningNumbers = Console.readLine();
+        System.out.println();
+
+        return winningNumbers;
     }
 
     public String requestBonusNumbers() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
+        String bonusNumbers = Console.readLine();
+        System.out.println();
+
+        return bonusNumbers;
     }
 
 }

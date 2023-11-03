0f5ac359862d5a44e7acd62b0aa375d431c45780
doha <dohaahi@naver.com>
2023-11-03 17:38:35 +0900

    test(InputValidatorTest): 당첨 번호 검증 기능 test 작성

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -35,4 +35,45 @@ class InputValidatorTest {
                     .hasMessage(InputValidator.WRONG_INPUT_PURCHASE_AMOUNT_MESSAGE);
         }
     }
+
+    @Nested
+    @DisplayName("validateWinningNumber 메소드 test")
+    class validateWinningNumber {
+        @DisplayName("당첨 번호가 숫자와 '" + InputValidator.DELIMITER + "'만으로 이루어져 있으면 검증 성공")
+        @Test
+        void Winning_number_is_matched_winning_numbers_regex() {
+            // given
+            String input = "1,2,3,4,5,6";
+
+            // when
+            // then
+            InputValidator.validateWinningNumbers(input);
+        }
+
+        @DisplayName("당첨 번호에 숫자 이외에 문자가 들어있다면 예외 발생")
+        @Test
+        void Winning_numbers_contain_characters_except_numbers() {
+            // given
+            String input = "A,1,2,3,4,5";
+
+            // when
+            // then
+            assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(InputValidator.WRONG_INPUT_WINNING_NUMBERS_MESSAGE);
+        }
+
+        @DisplayName("당첨 번호가 구분자가 '" + InputValidator.DELIMITER + "' 가 아닐 때 예외 발생")
+        @Test
+        void test() {
+            // given
+            String input = "1.2.3.4.5.6";
+
+            // when
+            // then
+            assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(InputValidator.WRONG_INPUT_WINNING_NUMBERS_MESSAGE);
+        }
+    }
 }

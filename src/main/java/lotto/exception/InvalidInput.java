009dbbd9accf3b13924a6060c52c27dbdb116c8d
Yang-soeun <soeun8636@naver.com>
2023-11-03 19:49:17 +0900

    [feat]: 중복된 숫자면 예외를 터트리는 메소드 수정

--- src/main/java/lotto/exception/InvalidInput.java
+++ src/main/java/lotto/exception/InvalidInput.java
@@ -1,32 +1,45 @@
 package lotto.exception;
 
+import java.util.Collections;
 import java.util.List;
 
 public class InvalidInput {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    private static final int ALLOW_DUPLICATE_NUMBER_COUNT = 1;
 
     private static String message;
 
     public void duplicateNumberException(List<Integer> numbers) {
         message = ExceptionMessage.DUPLICATE_NUMBER.getMessage();
-        throw new IllegalArgumentException(message);
+        for (Integer number : numbers) {
+            isDuplicate(numbers, number);
+        }
     }
 
     public void outOfRangeException(List<Integer> numbers) {
         message = ExceptionMessage.OUT_OF_RANGE.getMessage();
         for (Integer number : numbers) {
-            if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException(message);
-            }
+            isBetweenInRange(number);
         }
     }
-
     public void notIntegerValueException(String number) {
         message = ExceptionMessage.NOT_INTEGER_VALUE.getMessage();
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private static void isDuplicate(List<Integer> numbers, Integer number) {
+        if (Collections.frequency(numbers, number) > ALLOW_DUPLICATE_NUMBER_COUNT) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private static void isBetweenInRange(Integer number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(message);
         }
     }

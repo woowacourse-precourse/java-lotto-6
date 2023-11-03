55d9f45ed457e8ba6694b134e5616c960db6de66
song-jae-wan <sjo9810@gmail.com>
2023-11-03 11:38:31 +0900

    feat: GameController 메소드 선언
    인자로 받는 값들에 대해 처리해줄 메소드 설계

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -13,4 +13,16 @@ public class GameController {
         this.lottoService = lottoService;
         this.gameView = gameView;
     }
+
+    public void lottoAdd(String pay) {
+        // TODO: 인자로 받은 금액 예외 처리 후 로또 구매
+    }
+
+    public void winningLottoAdd(String numbers) {
+        // TODO: 인자로 받은 번호 예외 처리 후 당첨 번호 생성
+    }
+
+    public void BonusLottoAdd(String number) {
+        // TODO: 인자로 받은 번호 예외 처리 후 보너스 번호 생성
+    }
 }

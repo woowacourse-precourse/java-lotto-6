f64cb0c532ac30b67f39123b4f9868726b0d5775
kongji <kongji4092@gmail.com>
2023-11-03 18:06:29 +0900

    feat: 로또 구입 금액에 해당하는 로또 발행 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,12 +4,18 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
-    public void run(){
-        int purchasePrice = getPurchasePrice();
 
+    public void run() {
+        int purchasePrice = getPurchasePrice();
+        int lottoCount = getLottoCount(purchasePrice);
+        OutputView.printPurchaseLotto(lottoCount);
     }
 
-    private int getPurchasePrice(){
+    private int getLottoCount(int purchasePrice) {
+        return purchasePrice / 1000;
+    }
+
+    private int getPurchasePrice() {
         OutputView.printInputPrice();
         return InputView.getPurchaseLottoPrice();
     }

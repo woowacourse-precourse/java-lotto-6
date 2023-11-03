65e122c73884b66cf1e3ca13e48358bd24fe2c61
yuseonjun <seonjun0906@gmail.com>
2023-11-03 17:12:51 +0900

    feat(WinningLotto): 당첨 번호를 담당할 WinningLotto 클래스 생성

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class WinningLotto {
+
+    private final Lotto winningLotto;
+    private final BonusNumber bonusNumber;
+
+    public WinningLotto(Lotto winningLotto, BonusNumber bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+}

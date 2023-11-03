c6d4db2c7cafac53a1d8ad6e2c40f69b05155624
bokyung <bbookng@gmail.com>
2023-11-03 11:29:37 +0900

    feat : Add WinningNumbers Class
    WinningNumbers 추가
    - winningNumbers, bonusNumber 필드 생성
    - 생성자 추가

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+}

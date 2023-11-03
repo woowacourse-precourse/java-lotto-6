00011654bd524b2563e7df2c4d16a2b390cd9983
eora21 <eora21@naver.com>
2023-11-03 17:27:07 +0900

    test: 💍 추가 로또 금액 정상 지불 테스트 작성

--- src/test/java/lotto/domain/LottoShopStaffTest.java
+++ src/test/java/lotto/domain/LottoShopStaffTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import domain.LottoShopStaff;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoShopStaffTest {
@@ -23,4 +27,22 @@ public class LottoShopStaffTest {
         LottoShopStaff lottoShopStaff = new LottoShopStaff();
         assertThrows(IllegalArgumentException.class, () -> lottoShopStaff.payOfLottoTickets(amount));
     }
+
+    @ParameterizedTest
+    @DisplayName("로또 금액 정상 지불 테스트")
+    @MethodSource("payAmountAndExpectLottoTicketsNumber")
+    void payOfLottoTicketsSuccessTest(int payAmount, int expectLottoTicketsNumber) {
+        LottoShopStaff lottoShopStaff = new LottoShopStaff();
+        assertThat(lottoShopStaff.payOfLottoTickets(payAmount))
+                .isEqualTo(expectLottoTicketsNumber);
+    }
+
+    public static Stream<Arguments> payAmountAndExpectLottoTicketsNumber() {
+        return Stream.of(
+                Arguments.of(1_000, 1),
+                Arguments.of(10_000, 10),
+                Arguments.of(100_000, 100)
+        );
+    }
+
 }

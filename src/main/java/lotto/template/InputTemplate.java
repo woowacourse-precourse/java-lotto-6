ed9e278f0390be36b2bb0b2fbcfb296741f52c39
김승우 <tmd8633@naver.com>
2023-11-03 21:26:35 +0900

    Refactor : Template 구조 변경

--- src/main/java/lotto/template/InputTemplate.java
+++ src/main/java/lotto/template/InputTemplate.java
@@ -1,24 +1,28 @@
 package lotto.template;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.valid.Valid;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class InputTemplate {
 
-    public String execute(String message, Callback callback) {
+    public String execute(String message, Callback callback, List<Integer> winNum) {
         try {
             System.out.println(message);
-
             String text = Console.readLine();
-
-            System.out.println();
-
             callback.valid(text);
-
+            Valid.distinctCheck(winNum, text);
+            System.out.println();
             return text;
         } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("값이 존재하지 않습니다.", e);
+            System.out.println("[ERROR] 값이 존재하지 않습니다.");
+            return execute(message, callback, winNum);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return execute(message, callback, winNum);
         }
     }
+
 }

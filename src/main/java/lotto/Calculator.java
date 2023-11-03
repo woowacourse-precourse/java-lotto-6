da2841337dd13261a9fb5f0cfa5ba7a1980f9476
AmenPark <puzzlistpam@gmail.com>
2023-11-03 17:39:01 +0900

    feat: checkLotto
    발행된 로또를 체크해서 등수별 몇 개가 당첨되었는지를 업데이트함

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -7,12 +7,35 @@ import java.util.Map;
 public class Calculator {
     private Map<Prize, Integer> count;
     private final List<Integer> winner;
-    private final int bonus;
+    private final int bonusNum;
 
     public Calculator(List<Integer> winner, int bonus) {
         count = new HashMap<>();
         this.winner=winner;
-        this.bonus=bonus;
+        this.bonusNum=bonus;
     }
 
+
+    public void checkLotto(Lotto lotto){
+        int containCount=0;
+        for (int num:winner){
+            if(lotto.hasNum(num)){
+                containCount+=2;
+            }
+        }
+        if (lotto.hasNum(bonusNum)){
+            containCount++;
+        }
+        update(containCount);
+    }
+    private void update(int containCount){
+        int temp;
+        for(Prize p: Prize.values()){
+            if(p.getSame()<=containCount){
+                temp=count.get(p) + 1;
+                count.put(p,temp);
+                return;
+            }
+        }
+    }
 }

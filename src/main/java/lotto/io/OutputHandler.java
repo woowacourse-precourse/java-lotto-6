ac9dfc5782bbe12c548d535d8f6bad34277a6809
Hoo-sung <gntjd135@gmail.com>
2023-11-03 23:45:26 +0900

    feat(OutputHandler): 로또 번호를 출력하는 printLotto 메서드 생성

--- src/main/java/lotto/io/OutputHandler.java
+++ src/main/java/lotto/io/OutputHandler.java
@@ -1,9 +1,15 @@
 package lotto.io;
 
+
+import java.util.List;
+
 public final class OutputHandler {
 
     private OutputHandler(){
 
     }
 
+    public static void printLotto(List<Integer> lotto){
+        System.out.println(lotto.toString());
+    }
 }

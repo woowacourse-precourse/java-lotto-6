82077312a9fdb0ff72c7baf335d626406ed8875a
leeedohyun <ldh19738@gmail.com>
2023-11-03 14:42:07 +0900

    feat: 입력 메시지 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+public final class OutputView {
+
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    private OutputView() {
+    }
+
+    public static void printPurchaseAmountMessage() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public static void printWinningNumbersMessage() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+    }
+
+    public static void printBonusNumberMessage() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+}

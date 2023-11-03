6a633291fdecaa9110d5fd50ba3c9e3970c11fde
sangdol <priceelees@gmail.com>
2023-11-03 10:14:09 +0900

    feat(LottoMaker): LottoNumberMaker를 이용하여 로또 객체를 얻어내는 기능

---
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoMaker {
+    private static final int ONE_LOTTO_PRICE = 1000;
+    private final LottoNumberMaker lottoNumberMaker;
+
+    public LottoMaker() {
+        this(new LottoNumberMaker());
+    }
+    public LottoMaker(LottoNumberMaker lottoNumberMaker) {
+        this.lottoNumberMaker = lottoNumberMaker;
+    }
+
+    public WinningLotto createWinningLottoFromInput(String numbers, int bonusNumber) {
+        return new WinningLotto(lottoNumberMaker.createByInput(numbers), bonusNumber);
+    }
+
+    public Lotto createOneLotto() {
+        List<Integer> lottoNumbers = lottoNumberMaker.createLottoNumbers();
+        return new Lotto(lottoNumbers);
+    }
+
+    public List<Lotto> createLottosByPrice(int price) {
+        validatePrice(price);
+        int lottosCount = price / ONE_LOTTO_PRICE;
+
+        return createLottosByCount(lottosCount);
+    }
+
+    private void validatePrice(int price) {
+        if (price % ONE_LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 천원 단위여야 합니다.");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 음수일 수 없습니다.");
+        }
+    }
+
+    private List<Lotto> createLottosByCount(int lottosCount) {
+        return IntStream.rangeClosed(1, lottosCount)
+                .mapToObj(order -> createOneLotto())
+                .toList();
+    }
+}

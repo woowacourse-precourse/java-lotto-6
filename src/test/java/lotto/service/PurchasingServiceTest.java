6a7e138507f51ccb3f91978f2383c5255ec0fdfb
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-03 16:53:54 +0900

    test: 파라미터 타입 변경
    purcahseLotto 메소드가 String형을 인자로 전달받도록 변경되어 테스트 코드에서 동일하게 변경

--- src/test/java/lotto/service/PurchasingServiceTest.java
+++ src/test/java/lotto/service/PurchasingServiceTest.java
@@ -12,7 +12,7 @@ class PurchasingServiceTest {
     @DisplayName("로또 구매 금액이 1,000원 보다 적으면 예외가 발생한다.")
     @Test
     void createBuyerUnderAmount() {
-        assertThatThrownBy(() -> purchasingService.purchaseLotto(999))
+        assertThatThrownBy(() -> purchasingService.purchaseLotto("999"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또를 한 장 이상 구매해야 합니다. (로또 1장 1,000원");
     }
@@ -20,7 +20,7 @@ class PurchasingServiceTest {
     @DisplayName("로또 구매 금액의 단위가 1,000원이 아니면 예외가 발생한다.")
     @Test
     void createBuyerWrongUnit() {
-        assertThatThrownBy(() -> purchasingService.purchaseLotto(1500))
+        assertThatThrownBy(() -> purchasingService.purchaseLotto("1500"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또는 1,000원 단위로 구매 가능합니다. (로또 1장 1,000원");
     }

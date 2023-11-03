12055ecadbfc0e2973fb710b8775dd532c726085
sangdol <priceelees@gmail.com>
2023-11-03 11:01:14 +0900

    test(LottoTest): 숫자 범위에 대한 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +27,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("1부터 45까지가 아닌 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("argumentsProvider")
+    void createLottoByOverRangeNumber(String testName, List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                arguments("음수가 존재하는 경우", List.of(-1,2,3,4,5,6)),
+                arguments("0이 존재하는 경우", List.of(0,1,2,3,4,5)),
+                arguments("45를 초과하는 수가 존재하는 경우", List.of(1,2,3,4,5,46))
+        );
+    }
 }

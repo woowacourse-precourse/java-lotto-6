c3bfaeba5cfd6c9ec3daddb49051e61b5e0d6ee2
brightfanta <brightfanta@gmail.com>
2023-11-03 20:03:47 +0900

    feat(View): 전체 로또 구매 개수, 로또별 번호 출력 메서드 구현

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -2,13 +2,25 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class View {
 
     public static String getUserInput() {
-        return Console.readLine();
+        String userInput = Console.readLine();
+        System.out.println();
+        return userInput;
     }
 
     public static void printMessage(Notice notice) {
         System.out.println(notice.getMessage());
+    }
+
+    public static void printLotteryCount(Integer lotteryCount) {
+        System.out.println(lotteryCount + "개를 구매했습니다.");
+    }
+
+    public static void printLotteryNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
     }
 }

37f9e3806dd8d92823ca49bd10ae70fa85df8b69
mainfn <_@mainfn.dev>
2023-11-03 19:43:35 +0900

    feat: LottoPrize의 가격을 불변으로 유지하기 위해 Money의 파생 클래스 Price 도입

--- src/main/java/lotto/domain/lotto/LottoPrize.java
+++ src/main/java/lotto/domain/lotto/LottoPrize.java
@@ -2,32 +2,33 @@ package lotto.domain.lotto;
 
 import java.util.List;
 import lotto.domain.money.Money;
+import lotto.domain.money.Price;
 
 /**
  * WinningLotto와 Lotto를 비교하여 일치하는 번호 및 보너스 번호를 계산하여 로또 등 수를 나타낼 때 사용되는 클래스입니다.
  */
 public enum LottoPrize {
-    FIRST(1, Money.from(2_000_000_000), 6, false),
-    SECOND(2, Money.from(30_000_000), 5, true),
-    THIRD(3, Money.from(1_500_000), 5, false),
-    FOURTH(4, Money.from(50_000), 4, false),
-    FIFTH(5, Money.from(5_000), 3, false),
-    NONE(0, Money.zero(), 0, false);
+    FIRST(1, Price.from(2_000_000_000), 6, false),
+    SECOND(2, Price.from(30_000_000), 5, true),
+    THIRD(3, Price.from(1_500_000), 5, false),
+    FOURTH(4, Price.from(50_000), 4, false),
+    FIFTH(5, Price.from(5_000), 3, false),
+    NONE(0, Price.zero(), 0, false);
 
     private static final List<LottoPrize> lottoPrizes = List.of(values());
     private final int place;
-    private final Money prize;
+    private final Price price;
     private final int matchedCount;
     private final boolean hasBonusNumber;
 
     LottoPrize(
             final int place,
-            final Money prize,
+            final Price price,
             final int matchedCount,
             final boolean hasBonusNumber
     ) {
         this.place = place;
-        this.prize = prize;
+        this.price = price;
         this.matchedCount = matchedCount;
         this.hasBonusNumber = hasBonusNumber;
     }
@@ -49,4 +50,7 @@ public enum LottoPrize {
                 .orElse(LottoPrize.NONE);
     }
 
+    public Money intoMoney() {
+        return price.intoMoney();
+    }
 }

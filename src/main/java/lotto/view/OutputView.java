2a5d7d60f4e92e7029fed9f96deb54de48cb9e7a
HyungHoKim <hkim1109@naver.com>
2023-11-03 15:04:05 +0900

    feat(OutputView): 당첨 내역 출력
    당첨 내역을 출력하는 메서드 printPrizeDetails() 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,26 @@
 package lotto.view;
 
+import static lotto.enums.NumberCondition.LOTTO_SIZE;
+import static lotto.enums.NumberCondition.LOWEST_PRIZE_RANK;
+
+import java.util.Collections;
 import java.util.List;
 import lotto.model.User;
-import lotto.validator.LottoValidator;
+import lotto.utils.Converter;
 
 public class OutputView {
-    private static String COUNT = "%d개를 구매했습니다.";
-    private static String TICKET = "[" + "%s" + "]";
+    private static final String COUNT = "%d개를 구매했습니다.";
+    private static final String TICKET = "[" + "%s" + "]";
+    private static final String NUMBER_DIVISOR = ", ";
+    private static final String PRIZE_DETAILS = "당첨 통계";
+    private static final String LINE = "---";
+    private static final String NTH_PRIZE_DETAIL = "%d개 일치 (%d원) - %d개";
+    private static final String NTH_PRIZE_DETAIL_CONTAINING_BONUS = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
+
+    public static void printCountAndTickets(User user, int count) {
+        printCount(count);
+        printTickets(user, count);
+    }
 
     public static void printCount(int count) {
         System.out.printf((COUNT) + "%n", count);
@@ -21,15 +35,38 @@ public class OutputView {
 
     private static void printTicket(List<Integer> numbers) {
         StringBuilder ticket = new StringBuilder();
-        int lottoSize = LottoValidator.SIZE;
+        int lottoSize = LOTTO_SIZE.number();
+        Collections.sort(numbers);
         for (int i = 0; i < lottoSize; i++) {
             ticket.append(numbers.get(i));
             if (i < lottoSize - 1) {
-                ticket.append(", ");
+                ticket.append(NUMBER_DIVISOR);
             }
         }
         System.out.printf((TICKET) + "%n", ticket);
     }
 
+    public static void printPrizeDetails(int[] nthPrizeNumber) {
+        System.out.println(PRIZE_DETAILS);
+        System.out.println(LINE);
+        for (int rank = LOWEST_PRIZE_RANK.number(); rank > 0; rank--) {
+            int prizeNumber = nthPrizeNumber[rank];
+            printPrizeDetail(prizeNumber, rank);
+        }
+    }
 
+    private static void printPrizeDetail(int prizeNumber, int rank) {
+        if (rank != 2) {
+            System.out.printf((NTH_PRIZE_DETAIL) + "%n",
+                    Converter.rankToSameNumbers(rank),
+                    Converter.rankToPrize(rank),
+                    prizeNumber);
+        }
+        if (rank == 2) {
+            System.out.printf((NTH_PRIZE_DETAIL_CONTAINING_BONUS) + "%n",
+                    Converter.rankToSameNumbers(rank),
+                    Converter.rankToPrize(rank),
+                    prizeNumber);
+        }
+    }
 }

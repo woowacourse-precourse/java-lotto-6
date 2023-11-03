d8a1ccda60d966e2730c137625bcdeb1ffd074df
happyryan <tjdgks120813@gmail.com>
2023-11-03 17:01:41 +0900

    feat(user): add inputBonusNumber method in User class
    - Added the inputBonusNumber method to facilitate the input of the bonus number in the User class.
    - Included the validateSingleNumberInput method for input validation.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -3,6 +3,7 @@ package user;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class User {
@@ -52,8 +53,8 @@ public class User {
 
     private void validateCommaSeparatedInput(List<String> sixLottoNumbers) {
         for (String lottoNumber : sixLottoNumbers) {
-            if (!lottoNumber.matches("[0-9]+")) {
-                throw new IllegalArgumentException("[ERROR] 쉼표(,)를 기준으로 공백없이 입력해야합니다.");
+            if (!lottoNumber.matches("^\\d+$")) {
+                throw new IllegalArgumentException("[ERROR] 쉼표(,)를 기준으로 공백없이 입력해야합니다. (ex) 1,8,24,47,35,6");
             }
         }
     }
@@ -73,6 +74,26 @@ public class User {
                 throw new IllegalArgumentException("[ERROR] 중복된 번호가 입력되었습니다.");
             }
             uniqueNumbers.add(lottoNumber);
+        }
+    }
+
+    public String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        while (true){
+            try{
+                String bonusNumber = Console.readLine();
+                validateSingleNumberInput(bonusNumber);
+                return bonusNumber;
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateSingleNumberInput(String bonusNumber){
+        if(!bonusNumber.matches("^\\d+$")){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자 1개만 입력해주세요. (ex) 7");
         }
     }
 }

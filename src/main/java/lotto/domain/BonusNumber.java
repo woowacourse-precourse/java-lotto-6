7b1ac291e10cb8fe6081c00d6adb6c2fee404147
RedJunKi <bing8821@naver.com>
2023-11-04 06:12:17 +0900

    feat(lotto): BonusNumber 클래스 생성 및 검증로직 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import lotto.constant.ErrorMessages;
+
+import java.util.List;
+
+import static lotto.constant.NumberConstants.*;
+
+public class BonusNumber {
+
+    private final int bonusNumber;
+
+    public BonusNumber(List<Integer> winningNumber, int bonusNumber) {
+        hasNoDuplicateNumber(winningNumber, bonusNumber);
+        hasCorrectRange(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void hasNoDuplicateNumber(List<Integer> winningNumber, int bonusNumber) {
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX.getMessage() +
+                    ErrorMessages.DUPLICATE_NUMBER.getMessage() +
+                    ErrorMessages.SUFFIX.getMessage());
+        }
+    }
+
+    private void hasCorrectRange(int bonusNumber) {
+        if (bonusNumber < NUMBER_RANGE_MAX || bonusNumber > NUMBER_RANGE_MAX) {
+            throw new IllegalArgumentException(ErrorMessages.PREFIX.getMessage() +
+                    ErrorMessages.INVALID_RANGE.format(NUMBER_RANGE_MIN, NUMBER_RANGE_MAX) +
+                    ErrorMessages.SUFFIX.getMessage());
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

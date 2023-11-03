a94db6b27082e5070b3005371f7cd24c4f9f7e35
DohunHyun <dohun.hyun@gmail.com>
2023-11-03 16:39:03 +0900

    Feat : 랜덤 로또 숫자 리스트 만들기

--- src/main/java/lotto/LottoHost.java
+++ src/main/java/lotto/LottoHost.java
@@ -1,10 +1,26 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoHost {
 
 	List<Integer> makeRandomLottoNumber() {
-		return null;
+		List<Integer> randomNumberList = new ArrayList<>();
+
+		HashSet<Integer> numberCheck = new HashSet();
+
+		while(randomNumberList.size() < 6) {
+			int num = Randoms.pickNumberInRange(1, 45);
+			if(!numberCheck.contains(num)) {
+				randomNumberList.add(num);
+				numberCheck.add(num);
+			}
+		}
+
+		return randomNumberList;
 	}
 }

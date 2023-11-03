e85dda7b3f59fd79776c7878f7cea1142372b5f1
sami355-24 <ddung355@gmail.com>
2023-11-03 12:06:08 +0900

    refactor(Mesaage): 입출력 메시지간 통일성을 위해 구입 금액 안내문구 변수명 수정

--- src/main/java/lotto/Message/InputVIewMessage.java
+++ src/main/java/lotto/Message/InputVIewMessage.java
@@ -2,7 +2,7 @@ package lotto.Message;
 
 public enum InputVIewMessage {
 
-    INPUTMONEYPROMPT("구입금액을 입력해 주세요."),
+    INPUTPERCHASEPROMPT("구입금액을 입력해 주세요."),
     INPUTWINNINGNUMBERPROMPT("\n당첨 번호를 입력해 주세요."),
     INPUTBONUSNUMBERPROMPT("\n보너스 번호를 입력해 주세요.");
     private String message;

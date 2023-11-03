c03f74fe4754da5dacfaee3a9d549175835b5c11
qncj5224 <qncj5224@naver.com>
2023-11-03 20:43:23 +0900

    feat : 보너스 번호 요청 문구 출력 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,11 +3,16 @@ package lotto.view;
 public class OutputView {
   private final static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
   private final static String INPUT_WINNINGNUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+  private final static String INPUT_BOUNSNUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
   public static void printInputMoneyMessage() {
     System.out.println(INPUT_MONEY_MESSAGE);
   }
 
   public static void printInputWinningNumberMessage() {
     System.out.println(INPUT_WINNINGNUMBER_MESSAGE);
+  }
+
+  public static void printInputBallsNumberMessage() {
+    System.out.println(INPUT_BOUNSNUMBER_MESSAGE);
   }
 }

cb2dfcb85f5e752a3d2deb9dec0c1c7e463f4ffa
eunhee <dmsgml9903@gmail.com>
2023-11-04 03:53:37 +0900

    feat(UserLotteriesFactory): 사용자에게 입력한 금액에 해당하는 로또는 발행하는 기능 구현
    아래의 내용을 구현:
    - 사용자의 구입 금액으로 몇 개의 로또를 살 수 있는지 계산하는 기능
    - 구입한 로또의 개수만큼 로또를 발행(생성)하는 기능

---
+++ src/main/java/lotto/factory/UserLotteriesFactory.java
@@ -0,0 +1,33 @@
+package lotto.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.UserLotteries;
+
+public class UserLotteriesFactory {
+
+    private final LottoFactory lottoFactory;
+
+    public UserLotteriesFactory(LottoFactory lottoFactory) {
+        this.lottoFactory = lottoFactory;
+    }
+
+    public UserLotteries createFromPurchaseAmount(Integer purchaseAmount) {
+        Integer countOfPurchasedLotto = calculateCountOfPurchasedLotto(purchaseAmount);
+        List<Lotto> userLotteries = createUserLotteries(countOfPurchasedLotto);
+        return new UserLotteries(userLotteries);
+    }
+
+    private List<Lotto> createUserLotteries(Integer countOfPurchasedLotto) {
+        List<Lotto> userLotteries = new ArrayList<>();
+        for (int i = 0; i < countOfPurchasedLotto; i++) {
+            userLotteries.add(lottoFactory.createLotto());
+        }
+        return userLotteries;
+    }
+
+    private Integer calculateCountOfPurchasedLotto(Integer purchaseAmount) {
+        return purchaseAmount / 1000;
+    }
+}
---
+++ src/main/java/lotto/model/UserLotteries.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.List;
+
+public class UserLotteries {
+
+    private final List<Lotto> Lotteries;
+
+    public UserLotteries(List<Lotto> lotteries) {
+        Lotteries = lotteries;
+    }
+}

4de8e1f968672f4c594041e0b80397496de97281
Hoo-sung <gntjd135@gmail.com>
2023-11-03 21:55:17 +0900

    feat(LottoRepository): 사용자가 구매한 로또들을 저장하는 클래스 생성

---
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoRepository {
+
+    private List<Lotto> lottoRepository = new ArrayList<>();
+
+
+}

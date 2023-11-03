7fdd99a82cfcbae51fed40ec40552ab24b0aaa37
YongHyeon <pyh4616@naver.com>
2023-11-03 18:16:03 +0900

    Refactor(OutputView 클래스): 좀 더 명확한 표현을 위해 메시지 변수명과 메서드명에 사용된 'input'의 순서를 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,20 +2,20 @@ package lotto.view;
 
 public class OutputView {
 
-    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public void printInputPurchaseAmountMessage() {
-        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+    public void printPurchaseAmountInputMessage() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
     }
 
-    public void printWinningNumbersMessage() {
-        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+    public void printWinningNumbersInputMessage() {
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
     }
 
-    public void printInputBonusNumberMessage() {
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    public void printBonusNumberInputMessage() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
     }
 
 }

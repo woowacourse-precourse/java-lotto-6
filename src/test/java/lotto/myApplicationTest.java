13cff33714a1a8191079258fe3005059f3135820
yooooonshine <hl3470@naver.com>
2023-11-04 00:04:12 +0900

    Test: 구입 금액을 입력에 대한 테스트 구현
    구입 금액을 입력에 대한 정수인지, 양수인지, 단위가 맞는 지에 대한 단위 테스트와, 구입 금액에 대한 통합 테스트르 구현하였다.

---
+++ src/test/java/lotto/myApplicationTest.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import lotto.validator.Validator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class myApplicationTest {
+
+    @Nested
+    @DisplayName("구입 금액 테스트")
+    public class 구입_금액_테스트 {
+
+        @ParameterizedTest
+        @DisplayName("입력이 정수가 아니면 예외를 날리는 지 테스트한다.")
+        @ValueSource(strings = {",", "a"})
+        void 입력_정수테스트(String input) {
+            Assertions.assertThatThrownBy(() -> Validator.validatePurchaseAmount(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @DisplayName("음수나 0을 넣었을 때 예외를 던지는 지 테스트한다.")
+        @ValueSource(strings = {"-1", "0"})
+        void 입력_음수테스트(String input) {
+            Assertions.assertThatThrownBy(() -> Validator.validatePurchaseAmount(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @DisplayName("로또금액 단위에 안 맞으면 예외를 던지는 지 테스트한다.")
+        @ValueSource(strings = {"100", "1100"})
+        void 입력_단위테스트(String input) {
+            Assertions.assertThatThrownBy(() -> Validator.validatePurchaseAmount(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @DisplayName("구입 금액 통합 테스트")
+        @ValueSource(strings = {"1000", "5000"})
+        void 구입_금액_통합테스트(String input) {
+            Assertions.assertThatCode(() -> Validator.validatePurchaseAmount(input))
+                    .doesNotThrowAnyException();
+        }
+    }
+    }

df78da81cf323b8255d39c12b62f4710316868ae
LastCow9000 <sys19912002@hanmail.net>
2023-11-03 09:21:46 +0900

    refactor: lotto 매직 넘버 상수화, 함수명 변경, 에러 메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,18 +3,19 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 6개의 숫자로만 구성되어 있어야 합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
-
-    // TODO: 추가 기능 구현
 }

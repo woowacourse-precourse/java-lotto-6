44a5839542cf17fe1834c2f79339e5752483fd9e
yechop <channetattoo@gmail.com>
2023-11-04 03:46:35 +0900

    refactor(RandomLottoNumberGenerator): 정렬을 위해 LinkedList로 변경

--- src/main/java/lotto/domain/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/domain/RandomLottoNumberGenerator.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.io.Constants;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RandomLottoNumberGenerator {
 
     public List<Integer> generateRandomNumbers() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(Constants.LOTTO_MIN_NUM, Constants.LOTTO_MAX_NUM, Constants.LOTTO_NUMBERS);
+        List<Integer> randomNumbers = new LinkedList<>(Randoms.pickUniqueNumbersInRange(Constants.LOTTO_MIN_NUM, Constants.LOTTO_MAX_NUM, Constants.LOTTO_NUMBERS));
         Collections.sort(randomNumbers);
         return randomNumbers;
     }

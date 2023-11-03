c730205d74817e684340e401fcc4634b7d902ae3
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-04 02:59:49 +0900

    feat: 로또 가격에 공백값 포함 시 예외 처리

--- src/main/java/lotto/validator/InputMoneyValidator.java
+++ src/main/java/lotto/validator/InputMoneyValidator.java
@@ -5,17 +5,25 @@ public class InputMoneyValidator {
     private static final String ERROR = "[ERROR]";
     private static final String NULL_ERROR_MESSAGE = "금액을 입력해주세요.";
     private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
+    private static final String BLANK_ERROR_MESSAGE = "공백 없이 입력해주세요.";
     private final String money;
 
     public InputMoneyValidator(String input){
         money = input;
         isNull();
+        isContainBlank();
         isNumError();
     }
 
     public void isNull(){
         if (money.equals("")){
             throw new IllegalArgumentException(ERROR + NULL_ERROR_MESSAGE);
+        }
+    }
+
+    public void isContainBlank() {
+        if (money.contains(" ")){
+            throw new IllegalArgumentException(ERROR + BLANK_ERROR_MESSAGE);
         }
     }
 

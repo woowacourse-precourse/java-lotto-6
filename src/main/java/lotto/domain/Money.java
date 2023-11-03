f895d0a3d5d6ef6e45762a55987e5fbb6ca131af
opixxx <opix0306@naver.com>
2023-11-03 18:09:43 +0900

    feat(Money): 구입 금액에 따른 로또 갯수 구하는 기능 추가
    구입 금액에 대한 검증과, 구입 금액에 따른 로또 장수를 구하는 기능

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class Money {
+    private final int money;
+
+    public Money(int money) {
+        validateMoney(money);
+        this.money = money;
+    }
+
+    private void validateMoney(int money) {
+        if (money <= 0 || money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액을 잘못입력했습니다.");
+        }
+    }
+
+    public int getLotto() {
+        return money / 1000;
+    }
+}

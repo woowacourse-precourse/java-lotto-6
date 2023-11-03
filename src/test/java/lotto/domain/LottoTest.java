cf3207f1bcf823214e3a37d53de0c7c9534f7ebf
pokycookie <0cookieboy0@gmail.com>
2023-11-03 18:45:31 +0900

    test: ParameterizedTest 추가
    - 반복적인 assertThat 구문을 ParameterizedTest로 변경
    - 로또 번호가 6개가 아닌 경우 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,12 +26,34 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 개수가 6개가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("wrongLottoSize")
+    void createLottoByWrongSize(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByOverRangeNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+    @ParameterizedTest
+    @MethodSource("wrongLottoRange")
+    void createLottoByOverRangeNumber(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> wrongLottoSize() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7)),
+                Arguments.of(List.of(1, 2, 3, 4, 5))
+        );
+    }
+
+    static Stream<Arguments> wrongLottoRange() {
+        return Stream.of(
+                Arguments.of(List.of(0, 1, 2, 3, 4, 5)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 46)),
+                Arguments.of(List.of(-1, 1, 2, 3, 4, 5))
+        );
     }
 }

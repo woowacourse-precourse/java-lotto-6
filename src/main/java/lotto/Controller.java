f19229b53fc00d58d1d9472b5d9c98b1851c02c2
brightfanta <brightfanta@gmail.com>
2023-11-03 21:31:53 +0900

    feat(Controller): 일치 번호 개수 산출, 보너스 번호 일치 여부 확인, 보너스 번호 일치 시 순위 변경 기능 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,9 +1,36 @@
 package lotto;
 
+import java.util.List;
+
 public class Controller {
 
     private static final Integer LOTTERY_PRICE = 1_000;
     public static Integer calculateLotteryCount(Integer budget) {
         return budget / LOTTERY_PRICE;
+    }
+
+    public static Integer countMatchingNumbersByLotteryNumber(List<Integer> winningNumbers, Integer lotteryNumber) {
+        int matchingCount = 0;
+        for (Integer winningNumber : winningNumbers) {
+            if (lotteryNumber.equals(winningNumber)) {
+                matchingCount++;
+            }
+        }
+        return matchingCount;
+    }
+
+    public static boolean bonusNumberFlag(List<Integer> lotteryNumbers, Integer bonusNumber) {
+        for (Integer lotteryNumber : lotteryNumbers) {
+            if (lotteryNumber.equals(bonusNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void changeRankByBonusNumber(List<Rank> rankList, boolean bonusFlag, int index) {
+        if (bonusFlag) {
+            rankList.set(index, Rank.SECOND);
+        }
     }
 }

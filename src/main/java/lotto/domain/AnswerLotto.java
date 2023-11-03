949ac201097e28b142a4f93790326f8ed01d8780
olsohee <dlthgml0108@daum.net>
2023-11-03 23:36:20 +0900

    feat(AnswerLotto): 클래스 생성 및 생성자와 검증 로직 구현

---
+++ src/main/java/lotto/domain/AnswerLotto.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class AnswerLotto {
+
+    private final Lotto lotto;
+    private final BonusNumber bonusNumber;
+
+    public AnswerLotto(List<Integer> numbers, int bonusNumber) {
+        validate(numbers, bonusNumber);
+        this.lotto = new Lotto(numbers);
+        this.bonusNumber = new BonusNumber(bonusNumber);
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        validateNumberCount(numbers);
+        validateDuplicateNumbers(numbers, bonusNumber);
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers, int bonusNumber) {
+        validateNumbers(numbers);
+        validateWithBonusNumber(numbers, bonusNumber);
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        int nonDuplicatedCount = (int) numbers.stream().distinct().count();
+        if (nonDuplicatedCount != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateWithBonusNumber(List<Integer> numbers, int bonusNumber) {
+        boolean isDuplicated = numbers.stream()
+                .filter(number -> number.equals(bonusNumber))
+                .findFirst().isPresent();
+        if (isDuplicated) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

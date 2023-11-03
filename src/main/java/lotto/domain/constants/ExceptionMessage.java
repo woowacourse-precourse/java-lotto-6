20c24d071996677dcf1efba04a6ec907448c5b0f
sangchu <didi1484@gmail.com>
2023-11-03 22:44:32 +0900

    feat: ExceptionMessage enum 생성 (예외 메시지 목록을 담고 있음)

--- src/main/java/lotto/domain/constants/ExceptionMessage.java
+++ src/main/java/lotto/domain/constants/ExceptionMessage.java
@@ -1,4 +1,16 @@
 package lotto.domain.constants;
 
 public enum ExceptionMessage {
+    BE_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    HAS_LETTER("[ERROR] 로또 번호는 문자를 포함할 수 없습니다.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

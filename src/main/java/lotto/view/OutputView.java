34fbc893459a41f908b25e8b5a9b4d23cfe465c3
현 <sohyun95277@gmail.com>
2023-11-03 16:23:30 +0900

    feat(OutputView): 로또 개수, 숫자 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,25 @@
 package lotto.view;
 
+import static lotto.constant.message.OutputMessage.PURCHASE_COUNT_RESULT;
 import static lotto.constant.message.OutputMessage.RESULT_MESSAGE;
+
+import java.util.List;
+import lotto.domain.Lotto;
 
 public class OutputView {
     public void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printLottoCount(int count) {
+        String message = String.format(PURCHASE_COUNT_RESULT, count);
+        
+        System.out.println(message);
+    }
+
+    public void printLottoNumbers(List<Lotto> tickets) {
+        for (Lotto ticket : tickets) {
+            System.out.println(ticket.getNumbers());
+        }
     }
 }

b64afe53b15d1da1eeb39ac68fe403bc77f83258
eom-tae-in <eti0728@daum.net>
2023-11-04 05:21:38 +0900

    feat: 보너스 번호를 관리하고 검증하는 역할을 하는 BonusNumber 구현

---
+++ src/main/java/lotto/exception/bonus_number/InvalidNumberException.java
@@ -0,0 +1,8 @@
+package lotto.exception.bonus_number;
+
+public class InvalidNumberException extends IllegalArgumentException {
+
+    public InvalidNumberException(final int number) {
+        super(number + "은(는) 올바르지 않은 숫자입니다.");
+    }
+}
---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import static lotto.util.Constant.START_INCLUSIVE;
+import static lotto.util.Constant.END_INCLUSIVE;
+
+import lotto.exception.bonus_number.InvalidNumberException;
+
+public class BonusNumber {
+
+    private final int number;
+
+    public BonusNumber(final int number) {
+        validateNumberInRange(number);
+        this.number = number;
+    }
+
+    private void validateNumberInRange(final int number) {
+        if (!isValidNumber(number)) {
+            throw new InvalidNumberException(number);
+        }
+    }
+
+    private boolean isValidNumber(final int number) {
+        return START_INCLUSIVE.getValue() <= number && number <= END_INCLUSIVE.getValue();
+    }
+}

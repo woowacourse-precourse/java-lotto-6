8c21c2100b0d1ffb4456c89beaadaa243e8e69f9
song-jae-wan <sjo9810@gmail.com>
2023-11-03 11:36:59 +0900

    feat: GameController 필드 선언
    멤버변수로 서비스와 뷰 선언
    생성자에서 객체 주입 받도록 설계

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import service.LottoService;
+
+import view.GameView;
+
 public class GameController {
+
+    private final LottoService lottoService;
+    private final GameView gameView;
+
+    public GameController(LottoService lottoService, GameView gameView) {
+        this.lottoService = lottoService;
+        this.gameView = gameView;
+    }
 }

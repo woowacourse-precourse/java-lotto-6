de0f0e4642480cce355c007e1f7630e8539144fd
sangchu <didi1484@gmail.com>
2023-11-03 22:48:15 +0900

    feat: Number 생성자에 validateType 추가

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -10,7 +10,7 @@ public class Number {
 
 
     public Number(String number) {
-
+        validateType(number);
         this.number = Integer.parseInt(number);
         validateNumberRange(this.number);
     }

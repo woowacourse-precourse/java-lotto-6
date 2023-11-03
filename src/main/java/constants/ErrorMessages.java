64be1f906c4504c3f8aac7c29a7ea01d30201b83
Leespect <wewqwew153@gmail.com>
2023-11-03 14:23:42 +0900

    Feat : ErrorMessages 생성

---
+++ src/main/java/constants/ErrorMessages.java
@@ -0,0 +1,24 @@
+package constants;
+
+public enum ErrorMessages {
+    ERROR_HEADER("[ERROR] "),
+    BUY_EMPTY_ERROR("구입 금액을 입력해야 합니다."),
+    BUY_CHAR_ERROR("구입 금액은 쉼표(,) 없이 숫자로 입력해야 합니다."),
+    BUY_MINUS_ERROR("구입 금액은 음수가 될 수 없습니다."),
+    BUY_CONDITION_ERROR("구입 금액은 1,000원 담위로 입력해야 합니다."),
+    LOTTO_EMPTY_ERROR("당첨 번호를 입력해야 합니다."),
+    LOTTO_CHAR_ERROR("당첨 번호는 쉼표(,)를 기준으로 숫자로 입력해야 합니다."),
+    LOTTO_RANGE_ERROR("로또 번호는 1부터 45사이의 숫자여야 합니다."),
+    LOTTO_LENGTH_ERROR("로또 번호는 6개여야 합니다."),
+    BONUS_EMPTY_ERROR("보너스 번호를 입력해야 합니다.");
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

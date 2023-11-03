1b4a2971e336d8baa45472aff02e243cfb6ca8fa
YongHyeon <pyh4616@naver.com>
2023-11-03 18:10:07 +0900

    Feat(OutputView 클래스): 보너스 번호 입력 메시지를 출력하는 기능을 printInputBonusNumberMessage 메서드로 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void printInputPurchaseAmountMessage() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
@@ -12,4 +13,9 @@ public class OutputView {
     public void printWinningNumbersMessage() {
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
     }
+
+    public void printInputBonusNumberMessage() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
 }

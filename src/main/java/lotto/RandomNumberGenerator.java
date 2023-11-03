b5413fdd57e4fc6a402238ca79430e999d213e13
zjacom <eephus625@icloud.com>
2023-11-04 00:47:26 +0900

    feat(lotto): 랜덤으로 6개의 번호를 추출하는 기능 추가

---
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    public List<Integer> createNewRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

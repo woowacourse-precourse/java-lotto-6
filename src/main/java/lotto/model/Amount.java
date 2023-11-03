2854edfe8f0406cfa6e8bc6e3b2ac2b1ac86df8b
ChaeAg <dydkglel123@gmail.com>
2023-11-03 15:36:29 +0900

    refactor(Amount): 생성자는 생성자 역할만 하도록 리팩토링

--- src/main/java/lotto/model/Amount.java
+++ src/main/java/lotto/model/Amount.java
@@ -3,36 +3,36 @@ package lotto.model;
 public class Amount {
     private final int amount;
 
-    private Amount(String amountStr) {
+    private Amount(int amount) {
+        this.amount = amount;
+    }
+
+    public static Amount create(String amountStr) {
         validateNull(amountStr);
         validateEmpty(amountStr);
         int amount = validateNotNumber(amountStr);
         validateNegativeNumber(amount);
         validateAmountInThousandUnit(amount);
-        this.amount = amount;
-    }
-
-    public static Amount create(String amountStr) {
-        return new Amount(amountStr);
+        return new Amount(amount);
     }
 
     public int getAmount() {
         return this.amount;
     }
 
-    private void validateNull(String amountStr) {
+    private static void validateNull(String amountStr) {
         if (amountStr == null) {
             throw new IllegalArgumentException("[ERROR] 입력값이 null입니다.");
         }
     }
 
-    private void validateEmpty(String amountStr) {
+    private static void validateEmpty(String amountStr) {
         if (amountStr.isEmpty() || amountStr.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 구입 금액을 입력해 주세요.");
         }
     }
 
-    private int validateNotNumber(String amountStr) {
+    private static int validateNotNumber(String amountStr) {
         try {
             return Integer.parseInt(amountStr);
         } catch (NumberFormatException ex) {
@@ -40,13 +40,13 @@ public class Amount {
         }
     }
 
-    private void validateNegativeNumber(int amount) {
+    private static void validateNegativeNumber(int amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 0보다 작을 수 없습니다.");
         }
     }
 
-    private void validateAmountInThousandUnit(int amount) {
+    private static void validateAmountInThousandUnit(int amount) {
         if (amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또는 천원 단위로만 구매할 수 있습니다.");
         }

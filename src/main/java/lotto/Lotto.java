431251088ea29a5f0998a24a22366cd72036710e
seungtoctoc <dltmxor31@naver.com>
2023-11-03 20:35:29 +0900

    refactor: lotto 클래스 생성자에서 검증을 하도록 수정 Lotto 생성자가 호출되면 숫자의 개수, 범위, 중복 여부를 확인하도록 수정하였습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.validation.Validation;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,9 +13,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        Validation validation = new Validation();
+
+        validation.validateSize(numbers);
+        validation.validateRange(numbers);
+        validation.validateDuplication(numbers);
     }
 
     // TODO: 추가 기능 구현

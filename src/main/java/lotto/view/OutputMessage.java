4bdf18f35fe1847d3f527e244e5dc20acd23b4dc
minseo  jo <liging12@naver.com>
2023-11-04 01:31:49 +0900

    refactor: 출력 기능 리팩토링

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,14 +1,22 @@
 package lotto.view;
 
+import lotto.domain.Ranking;
+
 public enum OutputMessage {
     REQUEST_PURCHASE_AMOUNT("구입금액을 입력해 주세요.\n"),
-    REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요.\n"),
-    REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요.\n"),
+    REQUEST_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요.\n"),
+    REQUEST_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n"),
 
-    RESPONSE_PURCHASE_COUNT("%s개를 구매했습니다.\n"),
-    RESPONSE_USER_NUMBERS_SET("[%s]\n"),
+    RESPONSE_PURCHASE_COUNT("\n%d개를 구매했습니다.\n"),
+    RESPONSE_USER_NUMBERS_SET("%s\n"),
     RESPONSE_WINNING_STATISTICS_HEADER("당첨 통계\n---\n"),
-    RESPONSE_WINNING_STATISTICS_BODY("%s"),
+    RESPONSE_WINNING_STATISTICS_BODY(
+            Ranking.FIFTH.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.FIFTH.getWinningAmount()) + "원) - %d개\n" +
+            Ranking.FOURTH.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.FOURTH.getWinningAmount()) + "원) - %d개\n" +
+            Ranking.THIRD.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.THIRD.getWinningAmount()) + "원) - %d개\n" +
+            Ranking.SECOND.getMatchedNumberCount() + "개 일치, 보너스 볼 일치 (" + String.format("%,d", Ranking.SECOND.getWinningAmount()) + "원) - %d개\n" +
+            Ranking.FIRST.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.FIRST.getWinningAmount()) + "원) - %d개\n"
+    ),
     RESPONSE_TOTAL_RETURN("총 수익률은 %s%%입니다.\n");
 
     private final String message;
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.dto.LottoDto;
+
+import java.util.List;
+
 public class OutputView {
     public void requestPurchaseAmount() {
         display(OutputMessage.REQUEST_PURCHASE_AMOUNT);
@@ -13,24 +17,34 @@ public class OutputView {
         display(OutputMessage.REQUEST_BONUS_NUMBER);
     }
 
-    public void responsePurchaseCount(String count) {
+    public void responsePurchaseCount(int count) {
         display(OutputMessage.RESPONSE_PURCHASE_COUNT, count);
     }
 
-    public void responseUserNumbersSet(String result) {
-        display(OutputMessage.RESPONSE_USER_NUMBERS_SET, result);
+    public void responseUserNumbersSet(List<LottoDto> lottos) {
+        for (LottoDto lotto : lottos) {
+            display(OutputMessage.RESPONSE_USER_NUMBERS_SET, lotto.toString());
+        }
     }
 
     public void responseWinningStatisticsHeader() {
         display(OutputMessage.RESPONSE_WINNING_STATISTICS_HEADER);
     }
 
-    public void responseWinningStatisticsBody(String result) {
+    public void responseWinningStatisticsBody2(LottoDto lotto) {
+        display(OutputMessage.RESPONSE_WINNING_STATISTICS_BODY, lotto.getNumbers().subList(0, 5));
+    }
+
+    public void responseWinningStatisticsBody(List<Integer> result) {
         display(OutputMessage.RESPONSE_WINNING_STATISTICS_BODY, result);
     }
 
     public void responseTotalReturn(String result) {
         display(OutputMessage.RESPONSE_TOTAL_RETURN, result);
+    }
+
+    public void displayErrorMessage(String message) {
+        System.err.println(message);
     }
 
     private void display(OutputMessage message, Object... args) {

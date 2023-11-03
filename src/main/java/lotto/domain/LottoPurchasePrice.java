ee1a3524c441a658c679755a82d6d8f58d619da9
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 00:03:19 +0900

    feat(LottoPurchasePrice): 로또 구입 금액을 저장

---
+++ src/main/java/lotto/domain/LottoPurchasePrice.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import lotto.exception.NotThousandUnitException;
+
+public class LottoPurchasePrice {
+
+    private static final Integer UNIT = 1000;
+    private static final Integer NOT_REMAIN = 0;
+
+    private final Integer purchasePrice;
+
+    public LottoPurchasePrice(Integer purchasePrice) {
+        validateThousandUnit(purchasePrice);
+        this.purchasePrice = purchasePrice;
+    }
+
+    private void validateThousandUnit(Integer purchasePrice) {
+        if (isNotThousandUnit(purchasePrice)) {
+             throw new NotThousandUnitException();
+        }
+    }
+
+    private boolean isNotThousandUnit(Integer purchasePrice) {
+        return purchasePrice % UNIT != NOT_REMAIN;
+    }
+}

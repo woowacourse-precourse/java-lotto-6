ff0e5d6e941d5129a70173d2ed3ddf461c6ba2d2
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2023-11-03 21:43:08 +0900

    style: 출력 세로 공백 변경

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -6,36 +6,49 @@ import Config.GameConfig;
 public class OutputView {
     private final static String BEFORE_BUYING = "구입금액을 입력해 주세요.";
     private final static String AFTER_BUYING = "%d개를 구매했습니다.";
-    private final static String GET_BONUS = "당첨 번호를 입력해 주세요.";
+    private final static String GET_WINNING = "당첨 번호를 입력해 주세요.";
+    private final static String GET_BONUS = "보너스 번호를 입력해 주세요.";
     private final static String STATISTICS = "당첨 통계";
     private final static String LINE_SEPARATOR = "---";
     private final static String RANKING_STATISTICS = "%s (%d원) - %d개";
-    private final static String TOTAL_YIELD = "총 수익률은 %s%%입니다.";
+    private final static String TOTAL_YIELD_PREFIX = "총 수익률은 %s";
+    private final static String TOTAL_YIELD_SUFFIX = "입니다.";
 
     public void printMoney(){
         System.out.println(BEFORE_BUYING);
     }
 
-    public void printBuying(){
-        System.out.println(AFTER_BUYING);
+    public void printBuying(List<String> LottoNumbers){
+        System.out.println();
+        System.out.printf(AFTER_BUYING, LottoNumbers.size());
+        System.out.println();
+        for (String LottoNumber : LottoNumbers){
+            System.out.println(LottoNumber);
+        }
+        System.out.println();
+    }
+
+    public void printWinning(){
+        System.out.println(GET_WINNING);
     }
 
     public void printBonus(){
+        System.out.println();
         System.out.println(GET_BONUS);
     }
 
     public void printStatistics(List<Integer> winningCounts, String yield){
+        System.out.println();
         System.out.println(STATISTICS);
         System.out.println(LINE_SEPARATOR);
-        for (int winningCount : winningCounts){
-            int rank = winningCounts.indexOf(winningCount);
+        for (int rank = winningCounts.size(); rank > 0; rank--){
+            int winningCount = winningCounts.get(rank - 1);
             System.out.printf(RANKING_STATISTICS,
                     GameConfig.WINNING.valueOfRank(rank).getMatchAmount(),
                     GameConfig.WINNING.valueOfRank(rank).getPrice(),
                     winningCount);
             System.out.println();
         }
-        System.out.printf(TOTAL_YIELD, yield);
-        System.out.println();
+        System.out.println(String.format(TOTAL_YIELD_PREFIX, yield).concat(TOTAL_YIELD_SUFFIX));
     }
 }

48d939b7b13337e12dcc49e69c9b6bd40dc135e0
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-04 02:52:49 +0900

    feat: 전체적인 로또 프로그램 흐름 관리하는 LottoController 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,91 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.Lotto;
+import lotto.model.Calculator;
+import lotto.model.Rank;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.PurchaseAmountValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private static final int LOTTO_PRICE = 1000;
+    private int purchaseAmount;
+    private Lotto winnerLotto;
+    private List<Lotto> userLottos;
+    private int bonusNumber;
+    private Map<Rank, Integer> result;
+    private Calculator calculator;
+
+    public LottoController() {
+        this.userLottos = new ArrayList<>();
+        this.result = new HashMap<>();
+    }
+
+    public void run() {
+        initPurchaseAmount();
+        generateUserLottos();
+        initWinnerNumber();
+        initBonusNumber();
+        generateResult();
+        displayResult();
+    }
+
+    private void initPurchaseAmount() {
+        while (true) {
+            try {
+                purchaseAmount = InputView.readPurchaseAmount();
+                PurchaseAmountValidator.validate(purchaseAmount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void generateUserLottos() {
+        for (int i = 0; i < purchaseAmount / LOTTO_PRICE; i++) {
+            userLottos.add(Lotto.generateUserLotto());
+        }
+        OutputView.displayUserLottos(userLottos);
+    }
+
+    private void initWinnerNumber() {
+        while (true) {
+            try {
+                List<Integer> numbers = InputView.readLottoNumbers();
+                winnerLotto = new Lotto(numbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void initBonusNumber() {
+        while (true) {
+            try {
+                bonusNumber = InputView.readBonusNumber();
+                BonusNumberValidator.validate(winnerLotto, bonusNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void generateResult() {
+        calculator = new Calculator(winnerLotto, userLottos, bonusNumber);
+        result = calculator.getCalculateResult();
+    }
+
+    private void displayResult() {
+        float rateOfReturn = calculator.calculateRateOfReturn(result);
+        OutputView.displayResult(result, rateOfReturn);
+    }
+
+}

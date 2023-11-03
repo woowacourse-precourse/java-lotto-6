9c0eb6be1964b24c2c2a378c4e2246ee061a976e
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-03 23:33:36 +0900

    feat: 로또 숫자 범위 확인 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Lotto {
 
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> numbers;
@@ -15,11 +17,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         lottoSizeValidate(numbers);
+        lottoNumberRangeValidate(numbers);
     }
 
     private void lottoSizeValidate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void lottoNumberRangeValidate(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+        });
     }
 }

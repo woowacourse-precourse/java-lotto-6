67e1e769ef1e1dca28ba2b07b785994e6ba468c1
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-03 22:48:07 +0900

    <feat> : <변환에 필요한 Parser 클래스 추가>
    String 값을 integer 값으로 변환해주는 메서드와 ',' 을 기준으로 WinningNumber 를 반환해주는 메서드를 추가

---
+++ src/main/java/util/Parser.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+    public List<Integer> parseWinningNumber(String input){
+        return parseIntegerList(parseSplitByComma(input));
+    }
+    public List<String> parseSplitByComma(String input){
+        return new ArrayList<>(Arrays.asList(input.split(",")));
+    }
+    public List<Integer> parseIntegerList(List<String> input){
+        return input.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+    public int parseNumberOfMoney(String input){
+        return Integer.parseInt(input);
+    }
+}

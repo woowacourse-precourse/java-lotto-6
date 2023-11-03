e91f379f627905e60c1ea54dc895e1b79d88a119
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-03 22:11:20 +0900

    refactor(view): 출력에 맞는 형태로 리팩토링

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,6 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public String purchasePrice() {
-        return Console.readLine();
+        return defaultReadLine();
+    }
+
+    private String defaultReadLine() {
+        String input = Console.readLine();
+        System.out.println();
+        return input;
     }
 }

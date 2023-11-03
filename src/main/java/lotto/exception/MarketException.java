de276f1d60418b3ca24ea8e38c7c64a9d0fd61bb
minseo  jo <liging12@naver.com>
2023-11-04 01:32:45 +0900

    feat: MarketException 에러 메시지(상수) 기능 추가
    MarketException 에서 enum 으로 에러 메시지를 관리한다.

--- src/main/java/lotto/exception/MarketException.java
+++ src/main/java/lotto/exception/MarketException.java
@@ -1,7 +1,25 @@
 package lotto.exception;
 
+import static lotto.contant.LottoConstants.LOTTO_PRICE;
+import static lotto.contant.LottoConstants.MAX_LOTTO_PRICE_PER_USER;
+
 public class MarketException extends RuntimeException {
     public static final String ERROR = "[ERROR] ";
+    public enum ErrorMessage {
+        VALID_MONEY("로또 금액은" + LOTTO_PRICE.getValue() + " 원 입니다."),
+        NOT_DIVISIBLE("거스름돈이 존재하지 않습니다. 로또 한 장 금액은 " + LOTTO_PRICE.getValue() + " 원 입니다."),
+        EXCEED_MAX_LOTTO_AMOUNT("개인이 구매 가능한 로또 금액은 최대 "  + MAX_LOTTO_PRICE_PER_USER.getValue() + "원 입니다.");
+
+        private final String message;
+
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 
     public MarketException(String message) {
         super(ERROR + message);

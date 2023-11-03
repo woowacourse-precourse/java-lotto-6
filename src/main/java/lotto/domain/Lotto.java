730da25ed967dcc34e9a48225348a303bc4f30d8
anneu <b.imnuj@gmail.com>
2023-11-03 09:54:25 +0900

    feat: 로또 번호가 1~45 사이의 숫자인지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class Lotto {
 
     private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private static final String SIZE_EXCEPTION = "로또는 %d개의 번호로 이뤄져야 합니다.";
     private static final String DUPLICATE_EXCEPTION = "로또에 같은 번호가 존재합니다.";
+    private static final String RANGE_EXCEPTION = "로또 번호는 %d부터 %d 사이의 숫자여야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -18,6 +21,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -33,6 +37,14 @@ public class Lotto {
 
         if (distinctNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException(DUPLICATE_EXCEPTION);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MIN_LOTTO_NUMBER || MAX_LOTTO_NUMBER < number) {
+                throw new IllegalArgumentException(String.format(RANGE_EXCEPTION, MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+            }
         }
     }
 }

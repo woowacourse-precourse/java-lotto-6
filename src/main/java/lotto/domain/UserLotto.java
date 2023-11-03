d430a81a61e157c0093269e48359fe2138094717
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-03 11:10:52 +0900

    refactor(UserLotto) : 예외처리 시, 메세지 추가
    예외처리에 [ERROR] 메세지 추가함

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -13,13 +13,13 @@ public class UserLotto {
 
     private void validate(List<Integer> userNumbers) {
         if (userNumbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 정확히 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 정확히 6개여야 합니다.");
         }
         if (userNumbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
         if (userNumbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-            throw new IllegalArgumentException("로또 번호는 1부터 45까지의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.");
         }
     }
 }

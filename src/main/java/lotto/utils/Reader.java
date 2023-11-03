ccddf3720583ae61a3120a81ecf84f4edf956b5a
unknown <y2kf9a0q@gmail.com>
2023-11-03 19:07:55 +0900

    feat(lotto): Reader.java

---
+++ src/main/java/lotto/utils/Reader.java
@@ -0,0 +1,13 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Reader {
+    private Reader() {
+
+    }
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+}

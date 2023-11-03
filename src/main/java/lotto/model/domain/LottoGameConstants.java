d553a13eaec2e570170606a4e5212fbdfc56e7f1
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-03 22:50:37 +0900

    feat: 로또 1장당 가격 상수 추가

--- src/main/java/lotto/model/domain/LottoGameConstants.java
+++ src/main/java/lotto/model/domain/LottoGameConstants.java
@@ -4,7 +4,9 @@ public enum LottoGameConstants {
 
     MIN_NUMBER(1),
     MAX_NUMBER(45),
-    NUMBERS_PER_LOTTO(6);
+    NUMBERS_PER_LOTTO(6),
+    PRICE_PER_LOTTO(1000)
+    ;
 
     private final int value;
 

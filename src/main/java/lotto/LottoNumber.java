f7e4c5988e6458549603be8e094d8e7f9fc0015e
giljin <68088970+giljin@users.noreply.github.com>
2023-11-03 17:54:58 +0900

    feat(LottoNumber): 로또 번호 범위 지정

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,38 @@
+ package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_VALUE = 45;
+
+    private final int value;
+
+    LottoNumber(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < MIN_NUMBER || value > MAX_VALUE) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 " + MIN_NUMBER + " ~ " + MAX_VALUE + " 내의 값이 아닙니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}

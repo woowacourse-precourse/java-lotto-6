f12648b884b36942ae3265ef16f687043c46ab9f
SeoYoung <seoyoung7623@naver.com>
2023-11-04 01:47:37 +0900

    feat: 무작위 넘버 객체 생성

---
+++ src/main/java/lotto/model/Number.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Number {
+    private List<Integer> numberList;
+    private WinningGrade grade;
+
+    public Number() {
+        numberList = createNumbers();
+    }
+
+    public List<Integer> createNumbers(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        return numbers;
+    }
+
+    public WinningGrade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(WinningGrade grade) {
+        this.grade = grade;
+    }
+}

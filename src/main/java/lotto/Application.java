2caad5f1b212c8399e10a303d7978c0689445eaf
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-04 03:47:29 +0900

    feat: application에서 controller 생성 및 run

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.console.InputView;
+import lotto.view.console.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new LottoController(
+                new InputView(),
+                new OutputView()
+        ).run();
     }
 }

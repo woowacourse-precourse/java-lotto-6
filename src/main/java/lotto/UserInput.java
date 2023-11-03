d643a106ac1772393c34fde1d475944ebd545bfa
siho-song <thdtlgh234@naver.com>
2023-11-03 21:09:07 +0900

    [Feat] 함수 기능 추가 및 함수명 변경

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -1,14 +1,25 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.InputValidator;
 
 public class UserInput {
 
-    public int receiveMoney(){
-        return Integer.parseInt(Console.readLine());
+    private final InputValidator inputValidator;
+
+    UserInput(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
     }
 
-    public String receiveNumbers(){
+    public int receiveMoney() {
+        String money = Console.readLine();
+        inputValidator.validateHasOnlyInteger(money);
+        return Integer.parseInt(money);
+    }
+
+    public String receiveWinningNumbers() {
         return Console.readLine();
     }
+
+
 }

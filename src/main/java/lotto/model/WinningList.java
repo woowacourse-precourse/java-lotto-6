2b972e99fa3981e85b341d2c12ee36e8b377195c
peppone-choi <peppone.choi@gmail.com>
2023-11-04 04:59:47 +0900

    feat: 당첨 결과를 저장하고 수익률을 계산 할 WinningList 객체 구현

---
+++ src/main/java/lotto/model/WinningList.java
@@ -0,0 +1,60 @@
+package lotto.model;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lotto.record.AmountRecord;
+import lotto.record.LottoNumberRecord;
+import lotto.enumerate.Rank;
+import lotto.record.ProfitRate;
+
+public class WinningList {
+    private Map<Rank, Integer> winningList;
+
+    public WinningList(List<LottoNumberRecord> lottoNumberRecordList, WinningNumber winningNumber) {
+        winningList = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            winningList.put(rank, 0);
+        }
+        for (LottoNumberRecord lotto : lottoNumberRecordList) {
+            Rank rank = calculateRank(lotto, winningNumber);
+            if (rank != null) {
+                winningList.put(rank, winningList.get(rank) + 1);
+            }
+        }
+    }
+
+    public ProfitRate calculateProfitRate(AmountRecord amountRecord) {
+        long sum = winningList.entrySet().stream().mapToLong(this::calculateProfit).sum();
+        return new ProfitRate((double) (sum / amountRecord.amount()));
+    }
+
+    private long calculateProfit(Entry<Rank, Integer> rankIntegerEntry) {
+        Rank rank = rankIntegerEntry.getKey();
+        int value = rankIntegerEntry.getValue();
+        return rank.calculate(value);
+    }
+
+    private Rank calculateRank(LottoNumberRecord lottoNumberRecord, WinningNumber winningNumber) {
+        int matchingNumbers = winningNumber.countMatchingNumbers(lottoNumberRecord);
+        boolean hasMatchingBonusNumber = winningNumber.hasMatchingBonusNumber(lottoNumberRecord);
+
+        if (matchingNumbers == 6) {
+            return Rank.FIRST_PLACE;
+        }
+        if (matchingNumbers == 5 && hasMatchingBonusNumber) {
+            return Rank.SECOND_PLACE;
+        }
+        if (matchingNumbers == 5) {
+            return Rank.THIRD_PLACE;
+        }
+        if (matchingNumbers == 4) {
+            return Rank.FOURTH_PLACE;
+        }
+        if (matchingNumbers == 3) {
+            return Rank.FIFTH_PLACE;
+        }
+        return null;
+    }
+}

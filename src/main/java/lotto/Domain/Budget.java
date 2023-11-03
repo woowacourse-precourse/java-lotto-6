909f594ee4938bfd67b69c47badfe713c296a19a
Firedrago95 <yonghwa95@naver.com>
2023-11-03 12:27:05 +0900

    feat(Budget) : Budget 객체생성 기능 구현

---
+++ src/main/java/lotto/Domain/Budget.java
@@ -0,0 +1,15 @@
+package lotto.Domain;
+
+import lotto.Util.Validator;
+
+public class Budget {
+
+    public static final int LOTTO_PRICE = 1000;
+    private int budgetAmount;
+
+    public Budget(String budget) throws IllegalArgumentException{
+        Validator.validateBudget(budget);
+        budgetAmount = Integer.parseInt(budget);
+    }
+
+}

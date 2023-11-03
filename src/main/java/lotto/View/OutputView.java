b2d12787ec946c99659621dbb0d6f4c105bb5589
Firedrago95 <yonghwa95@naver.com>
2023-11-03 14:11:27 +0900

    feat(OutputView) : 로또 개수 출력 기능 구현

---
+++ src/main/java/lotto/View/OutputView.java
@@ -0,0 +1,12 @@
+package lotto.View;
+
+public class OutputView {
+
+    public static void printLottosSize(int size) {
+        System.out.println(size+"개를 구매했습니다.");
+    }
+
+    public static void printLottosNumbers() {
+
+    }
+}

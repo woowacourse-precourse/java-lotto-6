0ad6b8f6e6ce05a163f1b273e3b22380691494bf
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-11-03 23:43:16 +0900

    fix: immutable collections sort 문제 해결

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -50,7 +50,7 @@ public class LottoGame {
     }
 
     List<Integer> makeLotto(){
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(lotto);
         return lotto;
     }

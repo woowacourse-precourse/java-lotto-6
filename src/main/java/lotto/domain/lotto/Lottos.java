c14e1450ce0e7e039f983e1cb4c71dc72fe580e1
mainfn <_@mainfn.dev>
2023-11-03 19:42:57 +0900

    feat: Deque 형태로 Lottos에서 Lotto를 추출할 수 있도록 변경

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -1,6 +1,10 @@
 package lotto.domain.lotto;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
+import java.util.Optional;
+import lotto.domain.lotto.dto.LottoDrawingResult;
 
 /**
  * 사용자가 구매한 모든 로또를 담는 일급 컬렉션입니다.
@@ -9,14 +13,28 @@ public final class Lottos {
     /**
      * 사용자가 구매한 로또 리스트
      */
-    private final List<Lotto> lottos;
+    private final Deque<Lotto> lottos;
 
     /**
      * 이미 Lotto를 만들 때, LottoNumber가 1~45 범위에 속하는지, 6자리 숫자인지 검증이 끝난 상태이기 때문에 별도의 검증 없이 생성합니다.
      */
     public Lottos(final List<Lotto> lottos) {
-        this.lottos = lottos;
+        this.lottos = new ArrayDeque<>(lottos);
     }
 
+    public Optional<Lotto> nextLotto() {
+        return Optional.ofNullable(lottos.remove());
+    }
 
+    public LottoDrawingResult getResult(final Lottos lottos) {
+//        final EnumMap<LottoPrize, Integer> mp = new EnumMap<>(LottoPrize.class);
+        // TODO:
+        return null;
+    }
+
+//    private LottoPrize draw(final Lotto lotto) {
+//        final int matchedCount = lotto.countContained(lotto);
+//        final boolean hasBonusNumber = lotto.contains(bonusNumber);
+//        return LottoPrize.of(matchedCount, hasBonusNumber);
+//    }
 }

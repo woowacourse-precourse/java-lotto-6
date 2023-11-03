4828275d63f9b62e2f7c595026ce080c2f07f028
ParkDH0809 <znxn1237@gmail.com>
2023-11-03 17:05:11 +0900

    feat: 로또 번호 정렬 및 불변 변환 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = sortList(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private List<Integer> sortList(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return Collections.unmodifiableList(numbers);
+    }
+}

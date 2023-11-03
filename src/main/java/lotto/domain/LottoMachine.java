13d7d53e53d097df19fe05ccc433223523af694d
gumgu <bienlee@naver.com>
2023-11-03 17:49:14 +0900

    [Feat] 로또 발행 기능 구현

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoMachine {
+
+    public Lotto issueLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+}

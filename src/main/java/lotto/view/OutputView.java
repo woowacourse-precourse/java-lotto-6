be05fbf4889fa9c5737ed8a6f63b5023a736702a
minseo  jo <liging12@naver.com>
2023-11-03 11:58:50 +0900

    refactor: 출력 코드 리팩토링 (OutputView)
    내부 클래스에서 enum 의 상수를 이용해서 출력한다.
    가독성도 좋아졌고, 유지보수성에서 더 좋다.
    현재 파라미터에 String 을 가져오는 매서드들이 있는데, Dto 로 바꿀 예정이다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,34 +1,39 @@
 package lotto.view;
 
 public class OutputView {
-    public static class RequestMessage {
-        public static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-        public static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-        public static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public void requestPurchaseAmount() {
+        display(OutputMessage.REQUEST_PURCHASE_AMOUNT);
     }
 
-    /**
-     * 현재는 String을 파라미터로 가져오지만, Dto를 만들 예정.
-     * Dto를 만들지 않고, 문자열로 가져오면 모델이 너무 많은 역할을 한다.
-     */
-    public static class ResponseMessage {
-        public static String purchaseCount(int count) {
-            return String.format("%s개를 구매했습니다.", count);
-        }
+    public void requestWinningNumbers() {
+        display(OutputMessage.REQUEST_WINNING_NUMBERS);
+    }
 
-        public static String winningNumbers(String numbers) {
-            return String.format("[%s]", numbers);
-        }
+    public void requestBonusNumber() {
+        display(OutputMessage.REQUEST_BONUS_NUMBER);
+    }
 
-        public static String winningStatistics(String statistics) {
-            return String.format("""
-                당첨 통계
-                ---
-                %s""", statistics);
-        }
+    public void responsePurchaseCount(String count) {
+        display(OutputMessage.RESPONSE_PURCHASE_COUNT, count);
+    }
 
-        public static String totalReturn(String returnRate) {
-            return String.format("총 수익률은 %s%%입니다.", returnRate);
-        }
+    public void responseWinningNumbers(String result) {
+        display(OutputMessage.RESPONSE_WINNING_NUMBERS, result);
+    }
+
+    public void responseWinningStatisticsHeader() {
+        display(OutputMessage.RESPONSE_WINNING_STATISTICS_HEADER);
+    }
+
+    public void responseWinningStatisticsBody(String result) {
+        display(OutputMessage.RESPONSE_WINNING_STATISTICS_BODY, result);
+    }
+
+    public void responseTotalReturn(String result) {
+        display(OutputMessage.RESPONSE_TOTAL_RETURN, result);
+    }
+
+    private void display(OutputMessage message, Object... args) {
+        System.out.printf(message.getMessage(), args);
     }
 }

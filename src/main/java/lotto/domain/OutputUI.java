ae291e14442f7291386c3ea4435a889fb4766c91
mmalfoy <stpmalfoy@gmail.com>
2023-11-04 07:11:28 +0900

    feat(OutputUI) : 구매 수량및 번호출력 구현

--- src/main/java/lotto/domain/OutputUI.java
+++ src/main/java/lotto/domain/OutputUI.java
@@ -6,9 +6,15 @@ import java.util.List;
 public class OutputUI {
 
     public void rate(long winningRate) {
-        System.out.println("총 수익률은 "+winningRate+"%입니다.");
+        System.out.println("총 수익률은 " + winningRate + "%입니다.");
     }
 
+    public void purchaseLog(int num, String lotto) {
+        System.out.println(num + "개를 구매했습니다.");
+        for (int i = 0; i < num; i++) {
+            System.out.println(lotto);
+        }
 
+    }
 
 }

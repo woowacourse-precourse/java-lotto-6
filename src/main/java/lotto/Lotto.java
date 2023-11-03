897e185b4e56b0d728422c649f0b25d5252b22dd
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-03 23:00:30 +0900

    feat: 티켓 개수만큼 임의 로또 리스트 반환하는 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +18,18 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+
     }
 
-    // TODO: 추가 기능 구현
+
+    public List<Lotto> publishRandomLottos(int ticketNum) {
+        return IntStream.range(0, ticketNum)
+                .mapToObj(i -> generateRandomLotto())
+                .collect(Collectors.toList());
+    }
+
+    private Lotto generateRandomLotto() {
+        return new Lotto(pickUniqueNumbersInRange(1, 45, 6));
+    }
 }

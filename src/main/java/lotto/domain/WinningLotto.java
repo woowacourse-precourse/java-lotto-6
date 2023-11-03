07c0e10b0b17e145195f3754ccb4ca652388959e
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-04 03:41:57 +0900

    feat: WinningLotto domain 작성 - 보너스 숫자 예외 처리(범위, 중복), 랭크 반환

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import lotto.constant.LottoConstant;
+import lotto.exception.ErrorMessage;
+import lotto.exception.InvalidInputException;
+
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public LottoRank match(Lotto lotto) {
+        int matchCount = lotto.match(winningNumbers);
+        boolean matchBonus = lotto.contains(bonusNumber);
+        return LottoRank.of(matchCount, matchBonus);
+    }
+
+    public void validate(Lotto winningNumbers, int bonusNumber) {
+        validateBonusNumberDuplicate(winningNumbers, bonusNumber);
+        validateBonusNumberRange(bonusNumber);
+    }
+
+    private void validateBonusNumberDuplicate(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new InvalidInputException(ErrorMessage.LOTTO_NUMBERS_DUPLICATE);
+        }
+    }
+
+    private void validateBonusNumberRange(int bonusNumber) {
+        if (bonusNumber < LottoConstant.LOTTO_NUMBER_MIN || bonusNumber > LottoConstant.LOTTO_NUMBER_MAX) {
+            throw new InvalidInputException(ErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE);
+        }
+    }
+
+    public Lotto getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

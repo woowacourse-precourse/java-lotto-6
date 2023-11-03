0d50753df652bd20b1d9c0983d70142b9354be20
Leespect <wewqwew153@gmail.com>
2023-11-03 14:11:58 +0900

    Refactor : ConstantValues를 enum으로 수정

--- src/main/java/constants/ConstantValues.java
+++ src/main/java/constants/ConstantValues.java
@@ -1,15 +1,23 @@
 package constants;
 
-public class ConstantValues {
+public enum ConstantValues {
+    START_LOTTO_NUMBER(1),
+    END_LOTTO_NUMBER(45),
+    LOTTO_LENGTH(6),
+    LOTTO_COST(1000),
+    FIVE_PRIZE(5000),
+    FOUR_PRIZE(50000),
+    THREE_PRIZE(1500000),
+    TWO_PRIZE(30000000),
+    ONE_PRIZE(2000000000);
 
-    public static final int startLottoNumber = 1;
-    public static final int endLottoNumber = 45;
-    public static final int lottoLength = 6;
-    public static final int lottoCost = 1000;
-    public static final int fivePrize = 5000;
-    public static final int fourPrize = 50000;
-    public static final int threePrize = 1500000;
-    public static final int twoPrize = 30000000;
-    public static final int onePrize = 2000000000;
+    private final int value;
 
+    ConstantValues(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }

d8f15c9a333c3df6f73b91f0ba0fd266f76e822c
WonyuChoi <phillip07290@naver.com>
2023-11-03 16:44:34 +0900

    feat: 보너스 번호를 포함한 로또 객체의 dto

---
+++ src/main/java/model/dto/LottoResponse.java
@@ -0,0 +1,32 @@
+package model.dto;
+
+import java.util.List;
+import model.Lotto;
+import model.LottoNumber;
+import model.LottoWithBonus;
+
+public class LottoResponse {
+
+    private static final String DELIMITER = ", ";
+
+    private final List<String> lottoResponse;
+
+    private LottoResponse(final List<String> lotto) {
+        this.lottoResponse = lotto;
+    }
+
+    public static LottoResponse from(final LottoWithBonus lottoWithBonus) {
+        Lotto lotto = lottoWithBonus.getLotto();
+        List<String> lottoNumbers = lotto.getNumbers()
+            .stream()
+            .map(LottoNumber::getNumber)
+            .map(String::valueOf)
+            .toList();
+
+        return new LottoResponse(lottoNumbers);
+    }
+
+    public String getJoinedNumbers() {
+        return String.join(DELIMITER, lottoResponse);
+    }
+}

6ee01f8866729db4844e7fb1db3e2c4b349e7f3e
happyryan <tjdgks120813@gmail.com>
2023-11-03 15:30:40 +0900

    refactor(user User): reorder methods in the User class
    Reordered the methods in the User class to enhance code organization and readability.

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -15,18 +15,17 @@ public class User {
             try {
                 int money = Integer.parseInt(Console.readLine());
                 validateInputMoney(money);
-            }
-            catch (IllegalArgumentException e) {
+                return money;
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private int validateInputMoney(int money) {
+    private void validateInputMoney(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또는 1장당 1000원입니다. 1000원 단위로 구매를 진행해주세요.");
         }
-        return money;
     }
 
     public List<String> inputSixLottoNumbers() {
@@ -45,36 +44,35 @@ public class User {
         }
     }
 
-    private List<String> validateSixLottoNumbers(List<String> sixLottoNumbers) {
+    private void validateInputSizeSix(List<String> sixLottoNumbers) {
+        if (sixLottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호 6개를 입력해주세요.");
+        }
+    }
+
+    private void validateCommaSeparatedInput(List<String> sixLottoNumbers) {
+        for (String lottoNumber : sixLottoNumbers) {
+            if (!lottoNumber.matches("[0-9]+")) {
+                throw new IllegalArgumentException("[ERROR] 쉼표(,)를 기준으로 공백없이 입력해야합니다.");
+            }
+        }
+    }
+
+    private void validateSixLottoNumbers(List<String> sixLottoNumbers) {
         for (String lottoNumber : sixLottoNumbers) {
             if (1 > Integer.parseInt(lottoNumber) || Integer.parseInt(lottoNumber) > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
-        return sixLottoNumbers;
     }
 
-    private void validateNoDuplicateNumbers(List<String> sixLottoNumbers){
+    private void validateNoDuplicateNumbers(List<String> sixLottoNumbers) {
         List<String> uniqueNumbers = new ArrayList<>();
         for (String lottoNumber : sixLottoNumbers) {
             if (uniqueNumbers.contains(lottoNumber)) {
                 throw new IllegalArgumentException("[ERROR] 중복된 번호가 입력되었습니다.");
             }
             uniqueNumbers.add(lottoNumber);
-        }
-    }
-
-    private void validateCommaSeparatedInput(List<String> sixLottoNumbers){
-        for (String lottoNumber : sixLottoNumbers) {
-            if (!lottoNumber.matches("[0-9]+")){
-                throw new IllegalArgumentException("[ERROR] 쉼표(,)를 기준으로 공백없이 입력해야합니다.");
-            }
-        }
-    }
-
-    private void validateInputSizeSix(List<String> sixLottoNumbers){
-        if (sixLottoNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호 6개를 입력해주세요.");
         }
     }
 }

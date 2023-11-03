a0591342f9ca6216050e157745fc6353c8baf4a6
doha <dohaahi@naver.com>
2023-11-03 18:00:38 +0900

    refactor(InputValidator): 보너스 번호 검증 기능 구현

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -4,9 +4,11 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
     public static final String WINNING_NUMBERS_REGEX = "^[0-9,]+$";
+    public static final String NUMBER_REGEX = "^[0-9]+$";
     public static final String DELIMITER = ",";
     public static final String WRONG_INPUT_PURCHASE_AMOUNT_MESSAGE = "구입 금액은 숫자로 입력해주세요.";
     public static final String WRONG_INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호는 숫자와 '" + DELIMITER + "'만을 사용하여 입력해주세요.";
+    public static final String WRONG_INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호는 숫자로 입력해주세요.";
 
     public static void validatePurchaseAmount(final String input) {
         try {
@@ -24,5 +26,15 @@ public class InputValidator {
 
     private static boolean isMatchedWinningNumbersRegex(final String input) {
         return Pattern.matches(WINNING_NUMBERS_REGEX, input);
+    }
+
+    public static void validateBonusNumber(final String input) {
+        if (!isMatchedNumberRegex(input)) {
+            throw new IllegalArgumentException(WRONG_INPUT_BONUS_NUMBER_MESSAGE);
+        }
+    }
+
+    private static boolean isMatchedNumberRegex(final String input) {
+        return Pattern.matches(NUMBER_REGEX, input);
     }
 }

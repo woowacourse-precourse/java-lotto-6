d7091145b9cf1521855159e50d65bd07c5053b61
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 11:33:37 +0900

    feat(service): 사용자에게 입력받은 당첨 번호로 로또 게임의 당첨 번호를 초기화한다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoService {
 
     private final List<Lotto> userLottos = new ArrayList<>();
+    private List<Integer> winningNumbers = new ArrayList<>();
 
     public List<Lotto> purchase (int purchaseQuantity) {
         for (int i = 0; i < purchaseQuantity; i++) {
@@ -19,5 +22,12 @@ public class LottoService {
             userLottos.add(lotto);
         }
         return userLottos;
+    }
+
+    public void initWinningNumbers(String[] winningNumbers) {
+        this.winningNumbers = Arrays.stream(winningNumbers)
+                .mapToInt(Integer::valueOf)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }

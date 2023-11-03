7c3401ca8662893242ad65ddca289b0d70c6d1dc
sami355-24 <ddung355@gmail.com>
2023-11-03 20:49:18 +0900

    feat(Domain): 로또 생성할때 필요한 검증 로직 추가
    - 크기 검증 로직추가
    - 중복 검증 로직 추가
    - 1~45사이에 존재하는지 확인하는 로직 추가

---
+++ src/main/java/lotto/Domain/Lotto.java
@@ -0,0 +1,36 @@
+package lotto.Domain;
+
+import static lotto.CommonValidation.CommonValidation.isValueBetween1And45;
+import static lotto.Message.ExceptionMessage.LottoExceptionMessage.*;
+
+import java.util.List;
+import lotto.Exception.LottoException;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    private Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        for (Integer number : numbers) {
+            isValueBetween1And45(number);
+        }
+        this.numbers = numbers;
+    }
+
+    public static Lotto from(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new LottoException(LOTTO_SIZE_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            throw new LottoException(LOTTO_DUPLICATE_ERROR_MESSAGE.getMessage());
+        }
+    }
+}

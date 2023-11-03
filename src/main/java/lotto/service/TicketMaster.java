6e320c2d4043c2cc7253e6e4bd4266e7cc33b0a7
CHAHANS <herodev0129@gmail.com>
2023-11-03 14:38:35 +0900

    refector(service) : TicketMaster checkLotto() 수정

--- src/main/java/lotto/service/TicketMaster.java
+++ src/main/java/lotto/service/TicketMaster.java
@@ -31,6 +31,9 @@ public class TicketMaster {
                 result++;
             }
         }
+        if (thisLotto.contains(bonusNum)){
+            result++;
+        }
         return result;
     }
 

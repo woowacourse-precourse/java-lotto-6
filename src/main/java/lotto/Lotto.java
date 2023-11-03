a339f1a15acc0087a4b8faeda5eb98c0df26f574
hwangkyojune <shane5969@naver.com>
2023-11-03 23:28:12 +0900

    로또 검증 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    static int MIN_NUMBER = 1;
+    static int MAX_NUMBER = 45;
+    static int NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,30 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateAuthorizedLength(numbers);
+        validateDuplicatedNums(numbers);
+        validateProperNumbers(numbers);
+    }
+
+    public void validateAuthorizedLength(List<Integer> numbers){
+        if(numbers.size()!=6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void validateProperNumbers(List<Integer> numbers){
+        for(int i=0 ;i<numbers.size();i++){
+            int lottoNum = numbers.get(i);
+            if(lottoNum>MIN_NUMBER&&lottoNum<MAX_NUMBER){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validateDuplicatedNums(List<Integer> numbers){
+        int distinctNumbersCount = (int)numbers.stream().distinct().count();
+        if(distinctNumbersCount != NUMBER_SIZE){
+            throw new IllegalArgumentException();
+        }
+    }
 }

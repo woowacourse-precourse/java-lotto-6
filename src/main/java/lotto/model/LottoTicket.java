170a9dad88d2f4e45a45728a0270b9eec5452622
qncj5224 <qncj5224@naver.com>
2023-11-03 21:05:17 +0900

    feat : 등수 리스트 생성 매서드 구현

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTicket {
@@ -8,5 +9,10 @@ public class LottoTicket {
   public List<Integer> createRandomLottoNumber() {
     return Randoms.pickUniqueNumbersInRange(1, 45, 6);
   }
+
+  public List<Integer> createRank() {
+    return new ArrayList<>(6);
+  }
+
 
 }

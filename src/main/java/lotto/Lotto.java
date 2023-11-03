8be00d1eb08d1b30f73f4257f0b0ee1b87e35a86
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-03 18:30:48 +0900

    feat(Lotto): 중복 번호 검증 로직 추가
    feat(Lotto): 중복 번호 검증 로직 추가
    
    로또 번호 리스트에서 중복되는 번호가 있는지 확인하는 로직을 추가하여, 중복 번호 입력 시 IllegalArgumentException을 발생시킵니다. 이는 로또 번호의 유니크한 조건을 지켜준다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,32 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int MIN_LOTTE_NUMBER = 1;
+    private static final int MAX_LOTTE_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSixNumberCount(numbers);
+        validateOverlapNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validateSixNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateOverlapNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException("중복된 번호를 입력 하였습니다.");
+            }
         }
     }
 }

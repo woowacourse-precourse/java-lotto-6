cdfe0367d8d91dce079b466c11618b91300135e9
Chenowoo <cjsgusdn14@naver.com>
2023-11-03 18:23:43 +0900

    feat(InputValidation): 로또 구입 금액의 단위를 검증하는 기능 구현 로또 구입 금액이 1,000원 단위가 아니라면 에러 발생

---
+++ src/main/java/lotto/validation/InputValidation.java
@@ -0,0 +1,10 @@
+package lotto.validation;
+
+public class InputValidation {
+
+    public void validatePurchaseAmountUnit(int purchaseAmount) {
+        if (purchaseAmount == 0 || purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+}

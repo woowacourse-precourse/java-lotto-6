7b4cd21e4288eaf8f1a2d3f0c190463208f5856c
한다현 <handahyeon@handahyeon-ui-MacBookAir.local>
2023-11-04 01:12:11 +0900

    feat: input winning numbers

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,4 +1,8 @@
 package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
@@ -14,8 +18,25 @@ public class Application {
         }
     }
 
+    public static Lotto inputWinningNumbers() {
+        List<String> originalNums = new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
+        return new Lotto(convertStringToInt(originalNums));
+    }
+
+    public static List<Integer> convertStringToInt (List<String> original) {
+        List<Integer> convert = new ArrayList<Integer>();
+        for (String num: original){
+            convert.add(Integer.parseInt(num));
+        }
+        return convert;
+    }
+
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         int amount = inputLottoAmount();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Lotto winningLotto = inputWinningNumbers();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonus = Integer.parseInt(Console.readLine());
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+
+    public Lotto() {
+        this.numbers = createRandomNumbers();
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -12,9 +17,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> createRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }

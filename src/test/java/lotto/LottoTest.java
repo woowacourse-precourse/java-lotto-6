3ef2dd3e7463841a72e5bd4a012d850769841323
doha <dohaahi@naver.com>
2023-11-03 21:35:12 +0900

    test (LottoTest): 숫자 개수 검증 test 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,12 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -22,4 +23,36 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @Nested
+    @DisplayName("validateLottoSize 메소드 test")
+    class ValidateLottoSize {
+        @DisplayName("숫자 개수가 " + Lotto.LOTTO_SIZE + "와 일치하면 검증 통과")
+        @Test
+        void Match_lotto_size() {
+            // given
+            List<Integer> numbers1 = List.of(1, 2, 3, 4, 5, 6);
+            List<Integer> numbers2 = List.of(10, 20, 30, 40, 44, 45);
+
+            // when
+            // then
+            Lotto lotto1 = new Lotto(numbers1);
+            Lotto lotto2 = new Lotto(numbers2);
+        }
+
+        @DisplayName("숫자 개수가 " + Lotto.LOTTO_SIZE + "와 일치하지 않으면 예외 발생")
+        @Test
+        void Unmatch_lotto_size() {
+            // given
+            List<Integer> numbers1 = List.of(1, 2, 3, 4, 5);
+            List<Integer> numbers2 = List.of(1, 2, 3, 4, 5, 6, 7);
+
+            // when
+            // then
+            assertThatThrownBy(() -> new Lotto(numbers1)).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(Lotto.INVALID_LOTTO_SIZE_MESSAGE);
+            assertThatThrownBy(() -> new Lotto(numbers2)).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(Lotto.INVALID_LOTTO_SIZE_MESSAGE);
+        }
+    }
 }

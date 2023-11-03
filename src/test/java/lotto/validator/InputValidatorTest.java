f9477c4b1b326d8efd9d9aaf343d9574b9bd8327
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-04 02:56:11 +0900

    test: 값 미전달 예외 처리 테스트 코드 추가

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -6,8 +6,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class InputValidatorTest {
 
     private String ERROR = "[ERROR]";
-    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자를 입력해주세요";
+    private static final String NULL_ERROR_MESSAGE = "금액을 입력해주세요.";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
     private InputMoneyValidator inputMoneyValidator;
+
+    @Test
+    void 값_미전달_예외_처리_테스트(){
+        String input = "";
+        assertThatThrownBy(() -> inputMoneyValidator = new InputMoneyValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR+NULL_ERROR_MESSAGE);
+    }
 
     @Test
     void 입력_값_숫자_판별_예외_처리_테스트(){
@@ -16,4 +25,5 @@ public class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR+NOT_NUMBER_ERROR_MESSAGE);
     }
+
 }

9a35862da42f9b3233e94435ccac6728c19e7d70
scarlet0o0 <ghd7673@naver.com>
2023-11-03 19:06:37 +0900

    Feat : 컨트롤러에 전체 로직 작성

---
+++ src/main/java/lotto/controller/Controller.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+public class Controller {
+    public static void run() {
+        //TODO: 전체 로직
+        // - 1. 로또 구입 금액 입력 받기
+        // - 2. 금액에 맞게 로또 출력하기
+        // - 3. 당첨번호 입력받기
+        // - 4. 보너스 번호 입력 받기
+        // - 5. 수익 계산하기
+    }
+
+}

02d7cbba0a5f3678a44a380d98ef8b412c352cd9
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-03 19:44:36 +0900

    feat(InputView) : 입력 클래스 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,53 @@
+package lotto.view;
+
+import static lotto.constants.ExceptionMessage.BLANK_INPUT;
+import static lotto.constants.ExceptionMessage.ERROR_FORMAT;
+import static lotto.constants.ExceptionMessage.SPACE_INCLUDED;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Validation;
+
+public class InputView {
+
+    private final Validation validation;
+    public InputView(Validation validation) {
+        this.validation = validation;
+    }
+    public String inputPurchaseCost() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputCost = Console.readLine();
+        validateInput(inputCost);
+        return inputCost;
+    }
+
+    public String inputWinnerNumbers() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String inputWinningNum = Console.readLine();
+        validateInput(inputWinningNum);
+        return inputWinningNum;
+    }
+
+    public String inputBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String inputBonusNum = Console.readLine();
+        validateInput(inputBonusNum);
+        return inputBonusNum;
+    }
+
+    public void validateInput(String input) {
+        validateBlankInput(input);
+        validateSpaceChar(input);
+    }
+    public void validateBlankInput(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(String.format(ERROR_FORMAT, BLANK_INPUT+input));
+        }
+    }
+    public void validateSpaceChar(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException(String.format(ERROR_FORMAT, SPACE_INCLUDED+input));
+        }
+    }
+}

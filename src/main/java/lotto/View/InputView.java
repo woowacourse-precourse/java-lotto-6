0452cd5334d26834a48ef79108f1be82d3618b37
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 16:07:10 +0900

    refactor(InputView): 잘못입력하면 다시 입력을 받을 수 있게 수정

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,37 +1,20 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Model.Price;
+import lotto.Model.PriceValidator;
 
 public class InputView {
 
-    public static int inputMoneyForLottos() {
+    public static Price inputPriceForLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        String inputLottoPrice = Console.readLine();
-        validate(inputLottoPrice);
-        return Integer.parseInt(inputLottoPrice);
-    }
-
-    private static void validate(String inputLottoPrice) {
         try {
-            int lottoPrice = toInt(inputLottoPrice);
-            if (isNotDivideWithOneThousands(lottoPrice) || isNotCorrectMoneyRange(lottoPrice)) {
-                throw new NumberFormatException();
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 구입 금액은 1000원 단위로 1000원~20억원 사이로 입력해주세요.");
-            inputMoneyForLottos();
+            PriceValidator priceValidator = new PriceValidator(Console.readLine());
+            return new Price(priceValidator.getInputPrice());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 1000원 단위로 1000원~20억원 사이에서 입력해주세요.");
+            return inputPriceForLotto();
         }
     }
 
-    private static int toInt(String lottoPrice) {
-        return Integer.parseInt(lottoPrice);
-    }
-
-    private static boolean isNotDivideWithOneThousands(int lottoPrice) {
-        return lottoPrice % 1000 != 0;
-    }
-
-    private static boolean isNotCorrectMoneyRange(int lottoPrice) {
-        return lottoPrice <= 0 || lottoPrice > 2000000000;
-    }
 }

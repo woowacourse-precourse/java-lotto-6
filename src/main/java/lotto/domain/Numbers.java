61afb3e2351e9b77eab5ec0b122919cd12659202
hakro <leehr125@naver.com>
2023-11-03 13:08:05 +0900

    refactor, docs(lotto) : utils.* -> domain패키지로 변경. 관련 docs/README 수정

---
+++ src/main/java/lotto/domain/Numbers.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Numbers {
+    public List<Integer> createRandomNumbers() {
+        return null;
+    }
+
+    public String putCommaInNumber() {
+        return null;
+    }
+
+    public float rateOfReturn(int inputPrice, long outputPrice) {
+        return 0;
+    }
+}

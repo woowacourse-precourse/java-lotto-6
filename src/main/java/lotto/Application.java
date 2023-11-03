e8b1925746d1c1bf260d83f42204de38d02025b9
I4CU <129958775+IforCU@users.noreply.github.com>
2023-11-03 15:08:57 +0900

    로또 6자리 뽑아서 배열 형태 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,12 +10,22 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Lotto lotto = new Lotto(winningNumber());
-        lotto.outputLottoNumber();
+//        Lotto lotto = new Lotto(winningNumber());
+//        lotto.outputLottoNumber();
+
+        LottoPick lottoPick = new LottoPick(getMoney());
+        lottoPick.makeLottoPick();
+        lottoPick.outputLottoPick();
+    }
+
+    private static int getMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
     }
 
     // 당첨 번호 받기.
     private static List<Integer> winningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         List<String> inputdate = Arrays.asList(input.split(","));
         List<Integer> winningNumber = new ArrayList<>();
--- src/main/java/lotto/LottoPick.java
+++ src/main/java/lotto/LottoPick.java
@@ -1,31 +1,45 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Comparator;
 
 public class LottoPick {
-    private final List<Integer> numbers;
-    private final int attempt;
+    private final List<Integer>[] numbers;
+    private int attempt;
 
-    public LottoPick(List<Integer> numbers, int attempt) {
-        this.numbers = numbers;
-        this.attempt = attempt;
+    public LottoPick(int attempt) {
+        validate(attempt);
+        this.attempt = attempt/1000;
+        numbers = new List[this.attempt];
     }
 
-    //횟수 받기.
-    public int getMoney(int money) {
 
-
+    public void makeLottoPick() {
+        for(int i = 0; i < attempt; i++) {
+            numbers[i] = randomCreate();
+        }
+        for(int i = 0; i < attempt; i++){
+            numbers[i].sort(Comparator.naturalOrder());
+        }
     }
 
-    private void validate(int money){
-        if(money%1000 != 0){
+    public void outputLottoPick(){
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    // 금액 확인.
+    private void validate(int money) {
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("금액은 1000단위로 나누어 져야 합니다.");
         }
     }
 
-    //
+    // 랜덤 숫자 생성.
     private List<Integer> randomCreate() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }

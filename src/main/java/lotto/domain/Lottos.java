84feac821c838448f9202e11d1d0a840ebec25d4
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-03 21:15:22 +0900

    <feat> : <Lotto 번호를 여러 개 생성하는 Lottos 클래스 구현>
    사용자의 입력에 따라 Lotto 번호를 생성하는 Lottos 클래스 구현

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos(String numberOfPurchases){
+        createLottos(Integer.parseInt(numberOfPurchases));
+    }
+    private void createLottos(int numberOfPurchases){
+        for(int i=0; i<numberOfPurchases; i++){
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+            lottos.add(lotto);
+        }
+    }
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+}

d68027a7eb9fb95fdfaaeacc31e72e5b11c00f62
mainfn <_@mainfn.dev>
2023-11-03 11:02:45 +0900

    feat: 추첨 기능 + 당첨 번호 및 보너스 번호

---
+++ src/main/java/lotto/domain/lotto/WinningLotto.java
@@ -0,0 +1,52 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+import lotto.domain.lotto.boxed.BonusNumber;
+import lotto.domain.lotto.boxed.LottoNumber;
+import lotto.domain.lotto.validator.LottoValidator;
+
+public final class WinningLotto implements LottoTicket {
+    /**
+     * `Lotto` 객체의 유일한 필드이자 6자리의 로또 번호를 나타냅니다.
+     */
+    private final List<LottoNumber> winningNumbers;
+
+    /**
+     * 당첨 로또 번호 외 보너스 번호
+     */
+    private final BonusNumber bonusNumber;
+
+    /**
+     * @apiNote Lotto 생성 시, <h3 color="#bf0f4d">numbers의 요소는 반드시 6개여야 합니다.</h3>
+     */
+    public WinningLotto(
+            final List<Integer> winningNumbers,
+            final int bonusNumber
+    ) {
+        LottoValidator.validateLotto(winningNumbers);
+        this.winningNumbers = LottoTicket.intoLottoNumbers(winningNumbers);
+        this.bonusNumber = new BonusNumber(bonusNumber);
+    }
+
+    public LottoPrize draw(final Lotto lotto) {
+        final int matchedCount = countContained(lotto);
+        final boolean hasBonusNumber = hasBonusNumber(lotto);
+        return LottoPrize.of(matchedCount, hasBonusNumber);
+    }
+
+    /**
+     * 몇 개의 번호가 일치하는지 개수 반환
+     */
+    private int countContained(final Lotto lotto) {
+        return (int) winningNumbers.stream()
+                .filter(lotto::contains)
+                .count();
+    }
+
+    /**
+     * 보너스 번호를 가지고 있는지 체크
+     */
+    private boolean hasBonusNumber(final Lotto lotto) {
+        return lotto.contains(bonusNumber);
+    }
+}

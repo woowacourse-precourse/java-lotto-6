b05f752493278cc4e7ff375c801b396eece216a0
YongHyeon <pyh4616@naver.com>
2023-11-03 20:15:09 +0900

    Feat(WinningNumbers 클래스): 당첨 번호 중복에 대한 유효성 검사 기능을 validateDuplicate 메서드로 구현

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
 
@@ -10,12 +12,20 @@ public class WinningNumbers {
 
     public WinningNumbers(List<Integer> winningNumbers) {
         validateSize(winningNumbers);
+        validateDuplicate(winningNumbers);
         this.winningNumbers = winningNumbers;
     }
 
     private void validateSize(List<Integer> winningNumbers) {
         if (winningNumbers.size() != WINNING_NUMBERS_SIZE) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> winningNumbers) {
+        Set duplicateCheck = new HashSet(winningNumbers);
+        if (duplicateCheck.size() != winningNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되는 값이 존재하지 않아야 합니다.");
         }
     }
 }

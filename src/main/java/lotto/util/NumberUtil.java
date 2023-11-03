05858bfc5542affc63bcfdfe1a1b98b666df578d
JunTaeINC <juntea3523@naver.com>
2023-11-03 15:43:28 +0900

    feat : 숫자 검증 util 구현

---
+++ src/main/java/lotto/util/NumberUtil.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import java.util.regex.Pattern;
+
+public class NumberUtil {
+
+	public static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
+
+	public static boolean isNumeric(String strNum) {
+		if (strNum == null) {
+			return false;
+		}
+		return NUMBER_PATTERN.matcher(strNum).matches();
+	}
+}

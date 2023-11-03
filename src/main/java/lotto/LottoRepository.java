46547efda318cb9773219b883c5e2f6fc01770a1
gusgh075 <gusgh075@naver.com>
2023-11-03 15:43:08 +0900

    FEATURES(LottoRepository) : 등수를 계산하는 기능 추가

--- src/main/java/lotto/LottoRepository.java
+++ src/main/java/lotto/LottoRepository.java
@@ -1,14 +1,25 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoRepository {
     private List<Lotto> lottos;
+    private LottoRank lottoRank;
+    private int[] ranks=new int[6];
     private final Lotto winningLotto;
-    public LottoRepository(Lotto winningLotto){
+    private final int bonusNum;
+    public LottoRepository(Lotto winningLotto,int bonusNum){
         this.winningLotto=winningLotto;
+        this.bonusNum=bonusNum;
+        Arrays.fill(ranks,0);
     }
     public void addLotto(Lotto lotto){
+        calculateRank(lotto);
         lottos.add(lotto);
+    }
+    public void calculateRank(Lotto lotto){
+        int rank = lottoRank.isRank(lotto, winningLotto, bonusNum);
+        ranks[rank-1]++;
     }
 }

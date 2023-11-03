a9fe2aaf66576073b1d16dc05852b0d91fdbfabe
elyudwo <dudqk9696@naver.com>
2023-11-03 23:04:27 +0900

    [Test] : BonusNumber 유효성 검사 메소드 통합

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,37 +12,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BonusNumberTest {
 
-    @DisplayName("정답 번호와 동일한 보너스 번호일 경우 예외가 발생한다.")
-    @Test
-    void validateDuplicateBonusNumber() {
-        // given
-        List<Integer> answerNumber = List.of(1,2,3,4,5,6);
-        int bonus = 1;
-        BonusNumber bonusNumber = new BonusNumber();
-
-        // when, then
-        assertThatThrownBy(() -> bonusNumber.validateDuplicateBonusNumber(answerNumber, bonus))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-
-    static Stream<Arguments> numberSizeParameters() {
+    static Stream<Arguments> Parameters() {
         return Stream.of(
-                Arguments.of(0),
-                Arguments.of(46),
-                Arguments.of(-5)
+                Arguments.of("정답 숫자와 동일한 보너스 번호", List.of(1, 2, 3, 4, 5, 6), 1),
+                Arguments.of("1보다 작은 보너스 번호", List.of(1, 2, 3, 4, 5, 6), 0),
+                Arguments.of("45보다 큰 보너스 번호", List.of(1, 2, 3, 4, 5, 6), 46),
+                Arguments.of("음수인 보너스 번호", List.of(1, 2, 3, 4, 5, 6), -1)
         );
     }
 
-    @DisplayName("보너스 번호가 1~45 사이가 아닐 경우 예외가 발생한다.")
-    @ParameterizedTest
-    @MethodSource("numberSizeParameters")
-    void validateBonusNumberSize(int bonusNumbers) {
-        // given
-        BonusNumber bonusNumber = new BonusNumber();
-
+    @DisplayName("잘못된 보너스 번호인 경우 예외가 발생한다.")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("Parameters")
+    void validateBonusNumber(String testName, List<Integer> answerNumber, int bonusNumbers) {
         // when, then
-        assertThatThrownBy(() -> bonusNumber.validateBonusNumberSize(bonusNumbers))
+        assertThatThrownBy(() -> BonusNumber.of(answerNumber, bonusNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

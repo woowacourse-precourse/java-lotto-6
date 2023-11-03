f8ece8cbd2ba12aae25b45f5246f9ad7575f6677
Lee Yeon Su <yslee0540@gmail.com>
2023-11-03 21:53:38 +0900

    feat(PurchaseAmount): 입력값 1000으로 나누어 떨어지는지 확인하는 기능 추가

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -3,11 +3,15 @@ package lotto.domain;
 import java.util.regex.Pattern;
 
 public class PurchaseAmount {
-    private final String amount;
+
+    private static final int LOTTO_PRICE = 1000;
     private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
+    private static final String WRONG_UNIT_ERROR_MESSAGE = "[ERROR] 입력 금액은 1,000원 단위여야 합니다.";
+    private final String amount;
 
     public PurchaseAmount(String amount) {
         validateNonNumeric(amount);
+        validateUnit(amount);
         this.amount = amount;
     }
 
@@ -19,6 +23,13 @@ public class PurchaseAmount {
         String pattern = "^[0-9]*$";
         if (!Pattern.matches(pattern, input)) {
             throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateUnit(String input) {
+        int num = Integer.parseInt(input);
+        if (num % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(WRONG_UNIT_ERROR_MESSAGE);
         }
     }
 }

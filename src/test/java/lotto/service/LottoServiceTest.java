991844510b33049cc7d515f649eaab17dfbcbf08
kongji <kongji4092@gmail.com>
2023-11-03 19:54:38 +0900

    test: 로또 발급 기능 테스트

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,22 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoServiceTest {
+
+    @DisplayName("개수에 맞게 로또가 발급")
+    @Test
+    void generateLotto() {
+        LottoService lottoService = new LottoService();
+        int generateLottoCount = 4;
+        List<Lotto> lottos = lottoService.generateLotto(generateLottoCount);
+        assertEquals(generateLottoCount, lottos.size());
+    }
+}

a39ebcfbd4960e5e67851f4a569a02418b44b6ea
brightfanta <brightfanta@gmail.com>
2023-11-03 15:02:28 +0900

    feat(Lotto): Lotto 객체 생성과 동시에 6개의 로또 번호 생성 및 검증하도록 생성자 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);;
         validate(numbers);
-        this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {

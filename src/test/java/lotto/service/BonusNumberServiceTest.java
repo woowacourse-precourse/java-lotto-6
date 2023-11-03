7b5daab5dd8c4d7ddc5fa3da99c3aff4b6c0fe61
euics <pjh575157@gmail.com>
2023-11-03 14:19:01 +0900

    test: testEnsureNotDuplicatedWithWinningNumbers 메서드 구현
    당첨 번호와 보너스 번호 중복 검증 메서드
    - 중복되지 않는 보너스 번호
    - 당첨 번호와 중복되는 보너스 번호
      - 두가지 경우에 있어 예외처리 기능 검증

--- src/test/java/lotto/service/BonusNumberServiceTest.java
+++ src/test/java/lotto/service/BonusNumberServiceTest.java
@@ -4,6 +4,8 @@ import lotto.exception.BonusNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -18,5 +20,19 @@ class BonusNumberServiceTest {
         // 유효하지 않은 형식의 보너스 번호
         assertThrows(BonusNumberException.class,
                 () -> BonusNumberException.ensureValidFormat("invalid"));
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 보너스 번호 중복 검증")
+    void testEnsureNotDuplicatedWithWinningNumbers() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // 중복되지 않는 보너스 번호
+        assertDoesNotThrow(() ->
+                BonusNumberException.ensureNotDuplicatedWithWinningNumbers(winningNumbers, 7));
+
+        // 당첨 번호와 중복되는 보너스 번호
+        assertThrows(BonusNumberException.class,
+                () -> BonusNumberException.ensureNotDuplicatedWithWinningNumbers(winningNumbers, 1));
     }
 }

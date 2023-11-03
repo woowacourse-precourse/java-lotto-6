c8fcc18ee1589717b135127183a2ff083793e17c
sungmin306 <another0306@naver.com>
2023-11-03 23:50:24 +0900

    Feat: 입력한 값이 숫자가 아닐경우 예외처리 기능 구현 해당 기능 테스트 코드 작성 추가로 .hasMessage를 통해서 에러 메세지도 뜨는지 확인, 그전 Empty 테스트 코드에도 .hasMessage 사용 isNotEmpty 에서 checkEmpty로 메서드 이름 변경 및 테스트 이름 변경

--- src/main/java/generalexceptionhandler/GeneralExceptionHandler.java
+++ src/main/java/generalexceptionhandler/GeneralExceptionHandler.java
@@ -2,16 +2,27 @@ package generalexceptionhandler;
 
 public class GeneralExceptionHandler {
 
-    public static final String EMPTY_ERROR_MESSAGE = "[ERROR] 구매금액이 1000원 미만입니다.";
+    public static final String EMPTY_ERROR_MESSAGE = "[ERROR] 입력한 값이 존재하지 않습니다.";
+
+    public static final String NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 입력한 값이 숫자가 아닙니다.";
 
     public static int numberExceptionHandler(String inputStr) {
-        isNotEmpty(inputStr);
-        return 1;
+        checkEmpty(inputStr);
+        return checkNumber(inputStr);
     }
 
-    public static void isNotEmpty(String inputStr) {
+    public static void checkEmpty(String inputStr) {
         if (inputStr.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
+        }
+    }
+
+    public static int checkNumber(String inputStr) {
+        try {
+            int returnNumber = Integer.parseInt(inputStr);
+            return returnNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
         }
     }
 }
--- src/test/java/generalexceptionhandler/GeneralExceptionHandlerTest.java
+++ src/test/java/generalexceptionhandler/GeneralExceptionHandlerTest.java
@@ -1,6 +1,9 @@
 package generalexceptionhandler;
 
-import static generalexceptionhandler.GeneralExceptionHandler.isNotEmpty;
+import static generalexceptionhandler.GeneralExceptionHandler.EMPTY_ERROR_MESSAGE;
+import static generalexceptionhandler.GeneralExceptionHandler.NOT_NUMBER_ERROR_MESSAGE;
+import static generalexceptionhandler.GeneralExceptionHandler.checkEmpty;
+import static generalexceptionhandler.GeneralExceptionHandler.checkNumber;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,10 +12,21 @@ import org.junit.jupiter.api.Test;
 public class GeneralExceptionHandlerTest {
     @DisplayName("입력한 글자가 공백이면 예외처리를 발생한다.")
     @Test
-    void isNotEmptyTest() {
+    void checkEmptyTest() {
         String inputStrIsEmpty = "";
-        assertThatThrownBy(() -> isNotEmpty(inputStrIsEmpty))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> checkEmpty(inputStrIsEmpty))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EMPTY_ERROR_MESSAGE);
     }
+
+    @DisplayName("입력한 글자가 숫자가 아닐경우 예외처리를 발생한다.")
+    @Test
+    void checkNumberTest() {
+        String inputStr = "";
+        assertThatThrownBy(() -> checkNumber(inputStr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_NUMBER_ERROR_MESSAGE);
+    }
+
 }
 

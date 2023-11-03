b647ad1a1722ad3da007b369e330d9b8a9e72c02
leedonghyun <skydh507@ajou.ac.kr>
2023-11-04 01:59:21 +0900

    feat : 로또 번호 생성(랜덤수 로또 번호 생성)

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+
+    private final Integer countLotto;
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public LottoGenerator(Integer countLotto) {
+        this.countLotto = countLotto;
+    }
+
+    public List<Lotto> generateLottos() {
+        for (int i = 0; i < countLotto; i++) {
+            lottos.add(generateLotto(generateNumbers()));
+        }
+        return lottos;
+    }
+
+    public Lotto generateLotto(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return new Lotto(numbers);
+    }
+
+    public List<Integer> generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() != 6) {
+            int number = pickNumberInRange(1, 45);
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }
+        return numbers;
+    }
+
+
+}

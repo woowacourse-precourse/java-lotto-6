6f79a452a7c53f5473a8a8dba8753a1a7d6e2e48
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 16:32:43 +0900

    refactor(ProgressMessage): 당첨 통계에 관한 메세지 삭제

--- src/main/java/lotto/util/message/ProgressMessage.java
+++ src/main/java/lotto/util/message/ProgressMessage.java
@@ -5,11 +5,8 @@ public enum ProgressMessage {
 	INPUT_MONEY("구입금액을 입력해 주세요."),
 	BUYED_LOTTO_NUMBER("%s개를 구매했습니다."),
 	INPUT_ANSWER_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
-	INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-	WINNING_STATISTICS("당첨 통계\n" + "---\n"),
-	TOTAL_RETURN("총 수익률은 %,2f입니다.");
+	INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 	
-
 	private final String message;
 
 	ProgressMessage(String message) {

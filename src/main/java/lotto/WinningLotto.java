57084d2efc02027b7e4a0a25d539df12d8e969ac
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-03 23:35:22 +0900

    feat: 당첨 번호 및 보너스 번호 정보를 담고 있는 WinningLotto 클래스 구현

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        super(winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public LottoRanking calculateRanking(Lotto lotto) {
+        int correctCount = (int) lotto.numbers.stream()
+                .filter(numbers::contains)
+                .count();
+
+        boolean isCorrectBonusNumber = lotto.numbers.contains(bonusNumber);
+
+
+        return LottoRanking.findBy(correctCount, isCorrectBonusNumber);
+    }
+}

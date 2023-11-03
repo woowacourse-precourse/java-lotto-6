965194aa1249212034d19f370a4bfcbd32f38f23
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 01:35:58 +0900

    Feat: "구입 개수 확인 기능 추가, 상수 변수 추가"

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -2,11 +2,14 @@ package lotto.model;
 
 public class Purchase {
 
-    private String purchasingAmount;
+    private int PURCHASING_UINT = 1000;
+
+    private final String purchasingAmount;
     public Purchase(String purchasingAmount) {
         this.purchasingAmount = purchasingAmount;
         validateNum(this.purchasingAmount);
         validateUnit(this.purchasingAmount);
+        pieces();
     }
 
     public void validateNum(String purchasingAmount) {
@@ -16,8 +19,12 @@ public class Purchase {
     }
 
     public void validateUnit(String purchasingAmount) {
-        if (Integer.parseInt(purchasingAmount) % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력 가능합니다.");
+        if (Integer.parseInt(purchasingAmount) % PURCHASING_UINT != 0) {
+            throw new IllegalArgumentException("[ERROR] " + PURCHASING_UINT + "원 단위로 입력 가능합니다.");
         }
+    }
+
+    public int pieces() {
+        return Integer.parseInt(purchasingAmount) / PURCHASING_UINT;
     }
 }

4daf3288d4cdd3daad8114c34c323548cfc84f91
gusdn0108 <gusdn6671@naver.com>
2023-11-03 19:03:33 +0900

    feat(Controller): part1 완성 구입금액을 입력해주세요 출력 사용자의 구매가격 입력 n개의 구매완료 출력

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,8 +1,21 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Controller {
 
-    public void run(){
-        System.out.println("hello controller");
+    public void run() {
+        part1();
     }
+
+    public void part1() {
+        OutputView.printBuyInputPrice();
+        String userInput = InputView.userInput();
+        int userPrice = InputView.userInputParsedInt(userInput);
+        int lottoCnt = userPrice / 1000;
+        OutputView.printBuyLottoCount(lottoCnt);
+    }
+
 }

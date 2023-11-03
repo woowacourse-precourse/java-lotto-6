fe1c7df7fbd972abdd2c8778039d9dc6219c682d
Youngwoo Choi <allnstar@naver.com>
2023-11-04 00:46:56 +0900

    feat: add a function for printing prize

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -72,4 +72,13 @@ public class Prize {
             finalResult[4] += 1;
         }
     }
+
+    void printPrize() {
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - " + finalResult[4] + "개");
+        System.out.println("4개 일치 (50,000원) - " + finalResult[3] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + finalResult[2] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + finalResult[1] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + finalResult[0] + "개");
+    }
 }

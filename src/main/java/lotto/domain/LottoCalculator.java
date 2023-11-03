4d2c875e856f4fce15714d2178cd3483e07e1c3b
현 <sohyun95277@gmail.com>
2023-11-03 18:20:47 +0900

    feat(LottoCalculator): 모든 로또의 등수를 계산하는 기능

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -1,13 +1,33 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.constant.Prize;
+
 public class LottoCalculator {
     private final int SPECIAL_MATCH = 7;
     private final Lotto winningNumber;
     private final int bonusNumber;
+    private final Map<Prize, Integer> result = new HashMap<>();
 
     public LottoCalculator(WinningNumbers winningNumbers) {
         this.winningNumber = winningNumbers.getWinningNumber();
         this.bonusNumber = winningNumbers.getBonusNumber();
+    }
+
+    public void makePrizeResult(List<Lotto> lottoTickets) {
+        for (Lotto oneTicket : lottoTickets) {
+            int matchCount = countMatchNumbers(oneTicket);
+            Prize prize = Prize.getByMatch(matchCount);
+
+            if (prize != null) {
+                result.put(prize, result.get(prize) + 1);
+            }
+            if (matchCount == SPECIAL_MATCH) {
+                result.put(Prize.SECOND, result.get(Prize.SECOND) + 1);
+            }
+        }
     }
 
     public int countMatchNumbers(Lotto lotto) {

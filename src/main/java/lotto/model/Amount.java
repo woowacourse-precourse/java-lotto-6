461b93c1525f4b0576833ba5e5460e2dfc56817f
peppone-choi <peppone.choi@gmail.com>
2023-11-04 05:00:53 +0900

    feat: 자료구조 및 final 변경

--- src/main/java/lotto/model/Amount.java
+++ src/main/java/lotto/model/Amount.java
@@ -7,27 +7,32 @@ import static lotto.util.ExceptionCodeThrow.exceptionCodeThrow;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.record.AmountRecord;
 
 public class Amount {
-    private final int amount;
+    private final long amount;
 
-    public Amount(int amount) {
+    public Amount(long amount) {
         validAmount(amount);
         this.amount = amount;
     }
 
-    public List<Lotto> buyLotto(int amount) {
+    public List<Lotto> buyLotto() {
         return getLottos(amount);
     }
 
-    private ArrayList<Lotto> getLottos(int amount) {
+    public AmountRecord toRecord() {
+        return new AmountRecord(amount);
+    }
+
+    private ArrayList<Lotto> getLottos(long amount) {
         ArrayList<Lotto> list = new ArrayList<>();
-        int lottoVolume = amount / 1000;
+        long lottoVolume = amount / 1000;
         getLottoList(lottoVolume, list);
         return list;
     }
 
-    private void getLottoList(int lottoVolume, ArrayList<Lotto> list) {
+    private void getLottoList(long lottoVolume, ArrayList<Lotto> list) {
         for (int i = 0; i < lottoVolume; i++) {
             List<Integer> numbers = Randoms
                     .pickUniqueNumbersInRange(1, 45, 6);
@@ -35,18 +40,18 @@ public class Amount {
         }
     }
 
-    private void validAmount(int amount) {
+    private void validAmount(long amount) {
         unitThousandWonValid(amount);
         amountUnderThousandWonValid(amount);
     }
 
-    private static void amountUnderThousandWonValid(int amount) {
+    private static void amountUnderThousandWonValid(long amount) {
         if (amount <= 0) {
             exceptionCodeThrow(AMOUNT_IS_UNDER_THOUSAND_WON);
         }
     }
 
-    private static void unitThousandWonValid(int amount) {
+    private static void unitThousandWonValid(long amount) {
         if (amount % 1000 != 0) {
             exceptionCodeThrow(AMOUNT_UNIT_IS_NOT_1000_WON);
         }

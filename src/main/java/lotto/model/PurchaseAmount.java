f04c04f20fbc9e6b25f0713607c0c27d0f0cce44
burby1007 <kkh3272@naver.com>
2023-11-03 23:55:41 +0900

    Feat : 구입 금액 숫자가 아닌 경우(ERROR) 추가

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -6,12 +6,21 @@ public class PurchaseAmount {
 
     public static int validated(String input){
         empty(input);
+        int purchaseAmount = notNumber(input);
         return 0; //임시
     }
 
     public static void empty(String input){
         if(input.length() == 0){
             throw new IllegalArgumentException(ErrorMessage.EMPTY_PURCHASE_AMOUNT);
+        }
+    }
+
+    public static int notNumber(String input){
+        try{
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_PURCHASE_AMOUNT);
         }
     }
 

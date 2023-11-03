ba1834332914613ca004da6671292b2d29a6a572
sangdol <priceelees@gmail.com>
2023-11-03 12:49:44 +0900

    refactor(LottoMaker): 예외를 검증할 때, 음수값 여부를 검증하는 것을 1000원 미만 여부를 검증하도록 수정

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class LottoMaker {
+    private static final int ZERO_AMOUNT = 0;
     private static final int ONE_LOTTO_PRICE = 1000;
     private final LottoNumberMaker lottoNumberMaker;
 
@@ -32,11 +33,11 @@ public class LottoMaker {
     }
 
     private void validatePrice(int price) {
-        if (price % ONE_LOTTO_PRICE != 0) {
+        if (price % ONE_LOTTO_PRICE != ZERO_AMOUNT) {
             throw new IllegalArgumentException("[ERROR] 금액은 천원 단위여야 합니다.");
         }
-        if (price < 0) {
-            throw new IllegalArgumentException("[ERROR] 금액은 음수일 수 없습니다.");
+        if (price < ONE_LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 최소 1000원의 금액을 입력해야 합니다.");
         }
     }
 

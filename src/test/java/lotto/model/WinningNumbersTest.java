5a6eb6b44c4e499d312fe46f5d0a14a226924f93
YongHyeon <pyh4616@naver.com>
2023-11-03 20:53:59 +0900

    Test(WinningNumbersTest 클래스): 당첨 번호 범위에 대한 유효성 검사 기능을 테스트

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -21,5 +22,24 @@ public class WinningNumbersTest {
     void createWinningNumbersByDuplicatedNumber() {
         assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Nested
+    @DisplayName("당첨 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    class WinningNumbersOutOfRangeTest {
+
+        @DisplayName("범위의 아래 경곗값인 0을 포함하여 테스트")
+        @Test
+        void createWinningNumbersByOutOfRangeWithBelowValue() {
+            assertThatThrownBy(() -> new WinningNumbers(List.of(0, 2, 3, 4, 5, 6)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("범위의 위 경곗값인 46을 포함하여 테스트")
+        @Test
+        void createWinningNumbersByOutOfRangeWithAboveValue() {
+            assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 46)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }

42a3f153f54c307fca015566099c24013ad82de6
sangdol <priceelees@gmail.com>
2023-11-03 10:52:41 +0900

    refactor(Lotto): 예외 메시지 별도 정의 및 숫자 범위 검증 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,15 @@
 package lotto.domain;
 
+import static lotto.domain.Lotto.LottoErrorMessage.COUNT_NOT_SIX;
+import static lotto.domain.Lotto.LottoErrorMessage.HAS_DUPLICATE_NUBMER;
+import static lotto.domain.Lotto.LottoErrorMessage.NOT_IN_RANGE;
+
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
     private static final int SIX = 6;
-    private static final String ERROR = "[ERROR] ";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,11 +19,19 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != SIX) {
-            throw new IllegalArgumentException(ERROR + "6개의 숫자로 이루어져야 합니다.");
+            throw new IllegalArgumentException(COUNT_NOT_SIX.getErrorMessage());
         }
         if (hasDuplicateNumber(numbers)) {
-            throw new IllegalArgumentException(ERROR + "중복된 숫자가 존재할 수 없습니다.");
+            throw new IllegalArgumentException(HAS_DUPLICATE_NUBMER.getErrorMessage());
         }
+        if (hasNumberNotInRange(numbers)) {
+            throw new IllegalArgumentException(NOT_IN_RANGE.getErrorMessage());
+        }
+    }
+
+    private boolean hasNumberNotInRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < MIN_RANGE || number > MAX_RANGE);
     }
 
     private boolean hasDuplicateNumber(List<Integer> numbers) {
@@ -28,6 +41,7 @@ public class Lotto {
 
         return !distinctedNumbers.equals(numbers);
     }
+
     public boolean isContain(int number) {
         return numbers.contains(number);
     }
@@ -35,5 +49,21 @@ public class Lotto {
     @Override
     public String toString() {
         return numbers.toString();
+    }
+
+    enum LottoErrorMessage {
+        COUNT_NOT_SIX("[ERROR] 6개의 숫자로 이루어져야 합니다."),
+        HAS_DUPLICATE_NUBMER("[ERROR] 중복된 숫자는 존재할 수 없습니다."),
+        NOT_IN_RANGE("[ERROR] 모든 숫자는 1부터 45까지의 값만 가능합니다.");
+
+        private final String errorMessage;
+
+        LottoErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
     }
 }

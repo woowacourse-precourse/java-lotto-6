b58e90b3e450dee2b2cbe4ceee7d463a2096ec23
elyudwo <dudqk9696@naver.com>
2023-11-03 22:44:00 +0900

    [Feat] : 보너스 번호 예외 처리 메서드 통합 호출 기능 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class BonusNumber {
 
+    public void validateBonusNumber(List<Integer> answerNumber, int bonusNumber) {
+        validateBonusNumberSize(bonusNumber);
+        validateDuplicateBonusNumber(answerNumber, bonusNumber);
+    }
+
     public void validateDuplicateBonusNumber(List<Integer> answerNumber, int bonusNumber) {
         if(answerNumber.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] : 정답 번호와 동일한 보너스 번호가 있으면 안됩니다.");

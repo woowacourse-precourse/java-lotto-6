30bf3216faf5e2f1adc32896cc1c47cf34eb2b06
현 <sohyun95277@gmail.com>
2023-11-03 15:09:12 +0900

    feat(Lotto): 검증 기능
    List<Integer> 데이터 타입에 대한 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,48 @@
 package lotto;
 
+import static lotto.constant.LottoInformation.MAX_NUMBER;
+import static lotto.constant.LottoInformation.MIN_NUMBER;
+import static lotto.constant.LottoInformation.NUMBER_COUNT;
+import static lotto.constant.message.ErrorMessage.DUPLICATE_NUMBER;
+import static lotto.constant.message.ErrorMessage.INVALID_NUMBER_RANGE;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_COUNT) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        boolean result = numbers.stream().allMatch(this::isValidRange);
+
+        if (!result) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER);
+        }
+    }
+
+    private boolean isValidRange(int number) {
+        return number >= MIN_NUMBER && number < MAX_NUMBER;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

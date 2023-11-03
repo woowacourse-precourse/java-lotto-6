d6a541137f98017a79f72405ea17bfbb6363d43c
AmenPark <puzzlistpam@gmail.com>
2023-11-03 15:22:50 +0900

    feat: 숫자열입력받기 getListInput

--- src/main/java/lotto/ConsoleView.java
+++ src/main/java/lotto/ConsoleView.java
@@ -1,22 +1,54 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsoleView {
-    public static void print(String output){
+    public static void print(String output) {
         System.out.println(output);
     }
-    public static String getInput(){
+
+    public static String getInput() {
         return Console.readLine();
     }
 
-    public static long getLongInput(){
+    public static long getLongInput() {
         String _input = getInput();
-        try{
+        try {
             return Long.parseLong(_input);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             print("[ERROR] 유효하지 않은 숫자입니다.");
             return getLongInput();
         }
     }
+
+    public static List<Integer> getListInput() {
+        String _input = getInput();
+        List<Integer> parsedOutput = new ArrayList<>();
+        for (String s : _input.split(",")) {
+            try {
+                int number = Integer.parseInt(s);
+                if (parsedOutput.contains(number)) {
+                    throw new IllegalArgumentException();
+                }
+                if(number < 1 || number < 45){
+                    throw new IllegalArgumentException();
+                }
+                parsedOutput.add(number);
+
+            } catch (NumberFormatException e) {
+                print("[ERROR] 올바르지 못한 숫자");
+                return getListInput();
+            } catch (IllegalArgumentException e) {
+                print("[ERROR] 로또 번호는 중복되면 안됩니다.");
+            }
+        }
+        if(parsedOutput.size()!=6){
+            print("[ERROR] 당첨번호는 6개만 입력해주세요.");
+            return getListInput();
+        }
+        return parsedOutput;
+    }
+
 }

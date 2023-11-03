2891e73ec32f587ecabf7ed7c80fc60a83bc83dd
HiiWee <wpdlzhf159@gmail.com>
2023-11-04 02:15:36 +0900

    refactor: 수익률 계산 로직 최적화

--- src/main/java/lotto/dto/WinningResults.java
+++ src/main/java/lotto/dto/WinningResults.java
@@ -3,28 +3,36 @@ package lotto.dto;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import lotto.domain.lotto.LottoReward;
-import lotto.domain.money.MoneyCondition;
+import lotto.domain.lotto.LottoRewardCondition;
+import lotto.domain.money.LottoMoneyCondition;
 
-public record WinningResults(Map<LottoReward, Integer> results) {
+public record WinningResults(Map<LottoRewardCondition, Integer> results) {
 
-    public static WinningResults from(final List<LottoReward> rewards) {
+    public static WinningResults from(final List<LottoRewardCondition> rewards) {
         return new WinningResults(rewards.stream()
                 .collect(Collectors.groupingBy(lottoReward -> lottoReward,
                         Collectors.collectingAndThen(Collectors.counting(), Long::intValue))));
     }
 
-    public int getRewardCount(final LottoReward lottoReward) {
-        return results.getOrDefault(lottoReward, 0);
+    public int getRewardCount(final LottoRewardCondition lottoRewardCondition) {
+        return results.getOrDefault(lottoRewardCondition, 0);
     }
 
-    public double calculatePercentageReturn() {
-        int totalPrize = 0;
-        int count = 0;
-        for (LottoReward key : results.keySet()) {
-            totalPrize += results.get(key) * key.getPrize();
-            count += results.get(key);
-        }
-        return totalPrize / (double) (count * MoneyCondition.MONEY_UNIT.value()) * 100;
+    public double calculateProfitRatio() {
+        int totalPrizeMoney = calculateTotalPrizeMoney();
+        int lottoBuyingCount = getBuyingCount();
+        return totalPrizeMoney / (double) (lottoBuyingCount * LottoMoneyCondition.MONEY_UNIT.value()) * 100;
+    }
+
+    private int getBuyingCount() {
+        return results.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private int calculateTotalPrizeMoney() {
+        return results.entrySet().stream()
+                .mapToInt(result -> result.getValue() * result.getKey().getRewardMoney())
+                .sum();
     }
 }

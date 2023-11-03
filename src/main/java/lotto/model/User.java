de577c7ead3eb0eae78afc055242ae3b9bbf425f
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-11-03 15:47:00 +0900

    feat: 구입 금액에 해당하는 만큼 로또 발행 기능

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,10 +1,19 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private static final int AMOUNT_UNIT = 1000;
     private static final int NO_AMOUNT = 0;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private int amount;
+    private List<Lotto> purchasedLottos;
 
     private User(int amount) {
         this.amount = amount;
@@ -26,5 +35,24 @@ public class User {
         if (amount == NO_AMOUNT) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 0원 이상 입력해야 합니다.");
         }
+    }
+
+    public List<Lotto> getPurchasedLottos() {
+        purchasedLottos = new ArrayList<>();
+        int lottoCount = getLottoCount();
+        for (int i=0; i<lottoCount; i++) {
+            Lotto lotto = generateLottoNumber();
+            purchasedLottos.add(lotto);
+        }
+        return purchasedLottos;
+    }
+
+    private int getLottoCount() {
+        return amount / LOTTO_PRICE;
+    }
+
+    private Lotto generateLottoNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, 6);
+        return new Lotto(numbers);
     }
 }

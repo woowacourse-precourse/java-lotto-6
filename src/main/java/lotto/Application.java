ad485aab16fc08c4b749f7f2f7a076165f15a439
brightfanta <brightfanta@gmail.com>
2023-11-03 19:43:57 +0900

    feat(Application): 1.로또 구입 금액에서 로또 발행 개수 계산, 2.발행 개수 만큼 로또 객체 생성(6개 로또번호 생성), 3.당첨 번호, 보너스 번호 인스턴스 변수에 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,23 +1,38 @@
 package lotto;
 
+import java.util.List;
+
 import static lotto.Notice.*;
 
 public class Application {
     public static void main(String[] args) {
 
+        GameData gameData = new GameData();
+
         View.printMessage(ASK_BUDGET);
         String lottoBudgetInput = View.getUserInput();
         Integer budget = Convertor.convertInputToInteger(lottoBudgetInput);
+        Integer lotteryCount = Controller.calculateLotteryCount(budget);
+        gameData.generateLottoList();
+
+        for (int i = 0; i < lotteryCount; i++) {
+            List<Integer> lotteryNumbers = Lotto.generateLotteryNumbers();
+            List<Lotto> lottoList = gameData.getLottoList();
+            lottoList.add(new Lotto(lotteryNumbers));
+        }
 
         View.printMessage(ASK_WINNING_NUMBERS);
         String winningNumbersInput = View.getUserInput();
         String[] winningNumbersArray = Convertor.splitInput(winningNumbersInput);
-        Convertor.convertToList(winningNumbersArray);
+        List<Integer> winningNumbersTemp = Convertor.convertToList(winningNumbersArray);
+        gameData.setWinningNumbers(winningNumbersTemp);
 
-        Convertor.convertToList(winningNumbersArray);
         View.printMessage(ASK_BONUS_NUMBERS);
         String bonusNumberInput = View.getUserInput();
         Integer bonusNumberTemp = Convertor.convertInputToInteger(bonusNumberInput);
+        gameData.setBonusNumber(bonusNumberTemp);
+
+
 
         View.printMessage(GAME_STATISTICS);
         View.printMessage(SEPARATE_LINE);

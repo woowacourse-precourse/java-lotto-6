4e85016fc4a49150c48785f9dcf990397c32667a
euics <pjh575157@gmail.com>
2023-11-03 14:38:51 +0900

    refactor(Test): PurchaseValidationExceptionTest 클래스 메서드명 수정
    수정사항
    - `whenInputIsNotNumeric_thenThrowPurchaseValidationException` -> `throwsExceptionForNonNumericInput`
    - `whenInputIsNumeric_thenDoesNotThrowException` -> `doesNotThrowExceptionForNumericInput`
    - `whenAmountIsNotMultipleOfTicketPrice_thenThrowPurchaseValidationException` -> `throwsExceptionForInvalidPurchaseAmount`
    - `whenAmountIsMultipleOfTicketPrice_thenDoesNotThrowException` -> `doesNotThrowExceptionForValidPurchaseAmount`
      - 어떤 입력이 주어졌을 때 어떤 결과를 예상하는지를 반영하도록 이름을 변경

--- src/test/java/lotto/exception/PurchaseValidationExceptionTest.java
+++ src/test/java/lotto/exception/PurchaseValidationExceptionTest.java
@@ -9,7 +9,7 @@ class PurchaseValidationExceptionTest {
 
     @Test
     @DisplayName("숫자가 아닌 입력에 대해 PurchaseValidationException 예외 발생")
-    void whenInputIsNotNumeric_thenThrowPurchaseValidationException() {
+    void throwsExceptionForNonNumericInput() {
         String nonNumericInput = "abc";
         assertThrows(PurchaseValidationException.class, () ->
                 PurchaseValidationException.checkIsNumeric(nonNumericInput));
@@ -17,7 +17,7 @@ class PurchaseValidationExceptionTest {
 
     @Test
     @DisplayName("숫자 입력에 대해 예외가 발생하지 않음")
-    void whenInputIsNumeric_thenDoesNotThrowException() {
+    void doesNotThrowExceptionForNumericInput() {
         String numericInput = "12345";
         assertDoesNotThrow(() ->
                 PurchaseValidationException.checkIsNumeric(numericInput));
@@ -25,7 +25,7 @@ class PurchaseValidationExceptionTest {
 
     @Test
     @DisplayName("구매 금액이 로또 티켓 가격 단위의 배수가 아닐 때 PurchaseValidationException 예외 발생")
-    void whenAmountIsNotMultipleOfTicketPrice_thenThrowPurchaseValidationException() {
+    void throwsExceptionForInvalidPurchaseAmount() {
         int invalidAmount = 1550; // 가정: LOTTO_TICKET_PRICE_UNIT가 1000이라고 할 때
         assertThrows(PurchaseValidationException.class, () ->
                 PurchaseValidationException.checkValidPurchaseAmount(invalidAmount));
@@ -33,7 +33,7 @@ class PurchaseValidationExceptionTest {
 
     @Test
     @DisplayName("구매 금액이 로또 티켓 가격 단위의 배수일 때 예외가 발생하지 않음")
-    void whenAmountIsMultipleOfTicketPrice_thenDoesNotThrowException() {
+    void doesNotThrowExceptionForValidPurchaseAmount() {
         int validAmount = 2000; // 가정: LOTTO_TICKET_PRICE_UNIT가 1000이라고 할 때
         assertDoesNotThrow(() ->
                 PurchaseValidationException.checkValidPurchaseAmount(validAmount));

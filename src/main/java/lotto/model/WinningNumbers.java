c9794826dd606fb0e411e3fd1366a93cafa3cb65
YongHyeon <pyh4616@naver.com>
2023-11-03 20:39:42 +0900

    Feat(WinningNumbers 클래스): 당첨 번호 범위에 대한 유효성 검사 기능을 validateRange 메서드로 구현

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -7,12 +7,15 @@ import java.util.Set;
 public class WinningNumbers {
 
     private static final int WINNING_NUMBERS_SIZE = 6;
+    private static final int WINNING_NUMBERS_RANGE_END = 45;
+    private static final int WINNING_NUMBERS_RANGE_START = 1;
 
     private final List<Integer> winningNumbers;
 
     public WinningNumbers(List<Integer> winningNumbers) {
         validateSize(winningNumbers);
         validateDuplicate(winningNumbers);
+        validateRange(winningNumbers);
         this.winningNumbers = winningNumbers;
     }
 
@@ -26,6 +29,18 @@ public class WinningNumbers {
         Set duplicateCheck = new HashSet(winningNumbers);
         if (duplicateCheck.size() != winningNumbers.size()) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되는 값이 존재하지 않아야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> winningNumbers) {
+        for (int winningNumber : winningNumbers) {
+            validateSingleNumberRange(winningNumber);
+        }
+    }
+
+    private void validateSingleNumberRange(int winningNumber) {
+        if (winningNumber > WINNING_NUMBERS_RANGE_END || winningNumber < WINNING_NUMBERS_RANGE_START) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45까지의 숫자여야 합니다.");
         }
     }
 }

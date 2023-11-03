d48448b7e77a357dc508a44d66612830bdf826c6
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-03 22:10:43 +0900

    fix: 2등과 3등을 구분하는 로직 변경

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -3,27 +3,28 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000),
-    NO_RANK(-1, false, 0);
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NO_RANK(-1, 0);
 
     private final int matchingCount;
-    private final boolean isBonusNumberMatch;
     private final int prize;
 
-    private Rank(int matchingCount, boolean isBonusNumberMatch, int prize) {
+    private Rank(int matchingCount, int prize) {
         this.matchingCount = matchingCount;
-        this.isBonusNumberMatch = isBonusNumberMatch;
         this.prize = prize;
     }
 
     public static Rank decideRank(int matchingCount, boolean isBonusNumberMatch) {
+        if (matchingCount == Rank.SECOND.matchingCount && isBonusNumberMatch) {
+            return Rank.SECOND;
+        }
         return Arrays.stream(Rank.values())
                 .filter(rank -> matchingCount == rank.matchingCount)
-                .filter(rank -> isBonusNumberMatch == rank.isBonusNumberMatch)
+                .filter(rank -> rank != Rank.SECOND)
                 .findAny()
                 .orElse(Rank.NO_RANK);
     }

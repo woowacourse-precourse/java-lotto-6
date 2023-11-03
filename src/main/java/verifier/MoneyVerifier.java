fd2c7d6cdfc76438b5bc22fa878179700c8e3b9d
Hoo-sung <gntjd135@gmail.com>
2023-11-03 15:16:45 +0900

    feat(MoneyVerifier): 로또 구입금액이 0보다 큰지 검증하는 메서드 생성

--- src/main/java/verifier/MoneyVerifier.java
+++ src/main/java/verifier/MoneyVerifier.java
@@ -4,6 +4,7 @@ public class MoneyVerifier implements Verifier{
     @Override
     public void check(String input) {
         checkNumeric(input);
+        checkRange(input);
     }
 
     private void checkNumeric(String input){
@@ -13,5 +14,12 @@ public class MoneyVerifier implements Verifier{
             throw new IllegalArgumentException("로또 구입 금액이 숫자가 아닙니다.");
         }
     }
+
+    private void checkRange(String input){
+        int money = Integer.parseInt(input);
+        if(money <0)
+            throw new IllegalArgumentException("로또 구입 금액은 0보다 커야 합니다");
+    }
+
 
 }

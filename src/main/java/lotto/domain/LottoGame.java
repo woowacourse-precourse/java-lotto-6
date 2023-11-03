c22a65ca69c74d65ead568ea1d348097bc65b62d
jongwonpc <c3392302@naver.com>
2023-11-03 15:55:10 +0900

    feat: 당첨 번호와 로또를 비교해 등수를 산출하는 기능 구현

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -2,14 +2,18 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
 
     private List<Lotto> lottos = new ArrayList<>();
+    private Map<LottoResult, Integer> lottoMap = new HashMap<>();
 
     public LottoGame(int lottoMoney) {
         buildLottos(lottoMoney);
+        buildLottoMap();
     }
 
     private void buildLottos(int lottoMoney) {
@@ -21,8 +25,42 @@ public class LottoGame {
         }
     }
 
+    private void buildLottoMap() {
+        for (LottoResult lottoResult : LottoResult.values()) {
+            lottoMap.put(lottoResult, 0);
+        }
+    }
+
+    public void addValueLottoMap(List<Integer> winNums, int bonusNum) {
+        for (Lotto lotto : lottos) {
+            List<Integer> lottoNums = lotto.getNumbers();
+            int count = calculateCounts(lottoNums, winNums);
+            boolean bonusNumContained = isBonusNumContained(lottoNums, bonusNum);
+
+            LottoResult lottoResult = LottoResult.findByCountAndValidBonus(count, bonusNumContained);
+            lottoMap.put(lottoResult, lottoMap.get(lottoResult) + 1);
+        }
+    }
+
+    private int calculateCounts(List<Integer> lottoNums, List<Integer> winNums) {
+        int count = 0;
+        for (int i = 0; i < lottoNums.size(); i++) {
+            if (winNums.contains(lottoNums.get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isBonusNumContained(List<Integer> lottoNums, int bonusNum) {
+        return lottoNums.contains(bonusNum);
+    }
+
     public List<Lotto> getLottos() {
         return lottos;
     }
 
+    public Map<LottoResult, Integer> getLottoMap() {
+        return lottoMap;
+    }
 }

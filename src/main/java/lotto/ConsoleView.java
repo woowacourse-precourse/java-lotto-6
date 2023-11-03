081d4ed9d7d697bc603a8475935d0767b06283d3
AmenPark <puzzlistpam@gmail.com>
2023-11-03 15:13:08 +0900

    feat: Long입력받기

--- src/main/java/lotto/ConsoleView.java
+++ src/main/java/lotto/ConsoleView.java
@@ -9,5 +9,14 @@ public class ConsoleView {
     public static String getInput(){
         return Console.readLine();
     }
-    
+
+    public static long getLongInput(){
+        String _input = getInput();
+        try{
+            return Long.parseLong(_input);
+        }catch (NumberFormatException e){
+            print("[ERROR] 유효하지 않은 숫자입니다.");
+            return getLongInput();
+        }
+    }
 }

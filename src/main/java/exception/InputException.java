7bfa5a16217bf8949f9e769557d58b746e00bc87
song-jae-wan <sjo9810@gmail.com>
2023-11-03 17:29:52 +0900

    feat: 금액 처리 기능 추가
    - 숫자 예외 처리
    - 금액 단위 예외 처리
    - 예외 메세지 출력

--- src/main/java/exception/InputException.java
+++ src/main/java/exception/InputException.java
@@ -1,7 +1,30 @@
 package exception;
 
+import java.util.regex.Pattern;
+
 public class InputException {
 
+    private static final Pattern regex = Pattern.compile("[0-9]+");
 
+    private InputException(){
+    }
+
+    public static int validateInteger(String pay) {
+        if(regex.matcher(pay).matches()) {
+            return Integer.parseInt(pay);
+        }
+        throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+    }
+
+    public static int validatePayUnit(int pay) {
+        if(pay > 0 && pay % 1000 == 0) {
+            return pay;
+        }
+        throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 가능합니다.");
+    }
+
+    public static void printErrorMessage(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
 
 }

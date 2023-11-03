8590d0c1733943b2213afbc384ca59532437398b
shin5774 <babo5774@naver.com>
2023-11-03 19:42:18 +0900

    Refactor: 인자값의 숫자구성 검증기능 코드 개선
    - IllegalArgumentException이 발생할때 반환되는 메시지를 상수로 변경.
    - Pattern.matcher로 확인하는 부분을 별개의 메서드로 분리

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -4,9 +4,13 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final Pattern NUMBER=Pattern.compile("[0-9]+");
+    private static final String NOT_INTEGER_MESSAGE="숫자가 아닌 값이 입력되었습니다.";
     public void numberValidate(String value){
-        if(!NUMBER.matcher(value).matches()){
-            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+        if(isNotConsistOfNumber(value)){
+            throw new IllegalArgumentException(NOT_INTEGER_MESSAGE);
         }
+    }
+    private boolean isNotConsistOfNumber(String value){
+        return !NUMBER.matcher(value).matches();
     }
 }

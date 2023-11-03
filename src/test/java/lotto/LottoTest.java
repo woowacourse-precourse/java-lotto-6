3b710c2d051d702d2af002957c3143ca064fb7c2
euics <pjh575157@gmail.com>
2023-11-03 14:41:40 +0900

    test: whenGetNumbers_thenReturnsUnmodifiableList 메서드 구현
    Lotto 객체에서 번호 목록을 가져오면 변경 불가능한 목록이 반환 메서드

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -31,5 +31,19 @@ class LottoTest {
     void givenValidNumbers_whenLottoCreated_thenSucceeds() {
         List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertDoesNotThrow(() -> new Lotto(validNumbers));
+    }
+
+    @Test
+    @DisplayName("Lotto 객체에서 번호 목록을 가져오면 변경 불가능한 목록이 반환됨")
+    void whenGetNumbers_thenReturnsUnmodifiableList() {
+        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(validNumbers);
+        List<Integer> numbersFromLotto = lotto.getNumbers();
+
+        assertThrows(UnsupportedOperationException.class, () -> {
+            numbersFromLotto.add(7);
+        });
+
+        assertEquals(validNumbers, numbersFromLotto, "로또 번호 목록이 예상과 일치해야 함");
     }
 }

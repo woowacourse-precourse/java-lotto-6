5ce74fa9e2a41f03b895505ac6a8a9d884991652
euics <pjh575157@gmail.com>
2023-11-03 12:24:35 +0900

    feat(Service): getStatistics 메서드 구현
    로또 통계 생성 기능 구현
    - `getStatistics`
      - 구매한 로또 티켓들과 당첨 로또 티켓을 비교하여 각 등수별 당첨 통계를 반환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,8 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoTicket;
+import lotto.domain.Rank;
 import lotto.domain.WinningLottoTicket;
 import lotto.exception.BonusNumberExceptionMessage;
 import lotto.exception.PurchaseExceptionMessage;
@@ -8,7 +10,9 @@ import lotto.exception.WinningNumberExceptionMessage;
 import lotto.view.InputView;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoService {
@@ -96,6 +100,21 @@ public class LottoService {
 
     public WinningLottoTicket makeWinningLottoTicket(List<Integer> winningNumber, int bonusNumber) {
         return new WinningLottoTicket(winningNumber, bonusNumber);
+    }
+
+    public Map<Rank, Integer> getStatistics(LottoTicket lottoTicket, WinningLottoTicket winningLottoTicket) {
+        Map<Rank, Integer> statistics = new HashMap<>();
+
+        for (Rank rank : Rank.values()) {
+            statistics.put(rank, 0);
+        }
+
+        for (Lotto lotto : lottoTicket.getLottoTickets()) {
+            Rank rank = winningLottoTicket.match(lotto);
+            statistics.put(rank, statistics.get(rank) + 1);
+        }
+
+        return statistics;
     }
 
     private List<Integer> changeInputToString(String input) {

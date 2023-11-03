c39bb3ff1827f632470518c65cbcd4861a9e27e9
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-04 00:18:33 +0900

    feat(LottoService): 로또상금들의 합을 구하는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,12 +1,20 @@
 package lotto.service;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Set;
+import java.util.StringJoiner;
 
 import lotto.calculator.LottoCalculator;
 import lotto.domain.AnswerLotto;
+import lotto.domain.LottoRank;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.generator.LottosGenerator;
+import lotto.util.message.WinningStatisticsMessage;
 
 public class LottoService {
 
@@ -24,6 +32,21 @@ public class LottoService {
 
 	public AnswerLotto createAnswerLotto(List<Integer> numbers, int bonusNumber) {
 		return new AnswerLotto(numbers, bonusNumber);
+	}
+
+	public int calculateLottoPriceSum(Lottos lottos, AnswerLotto answerLotto) {
+		EnumMap<LottoRank, Integer> lottRankCounter = countLottoRank(lottos, answerLotto);
+		return calculator.calculateSum(lottRankCounter);
+	}
+
+	private EnumMap<LottoRank, Integer> countLottoRank(Lottos lottos, AnswerLotto answerLotto) {
+		EnumMap<LottoRank, Integer> lottRankCounter = LottoRank.toEnumMap();
+
+		lottos.getLottos().stream().map(lotto -> LottoRank.getMatchedLottoRank(answerLotto, lotto))
+				.filter(Objects::nonNull)
+				.forEach(lottoRank -> lottRankCounter.put(lottoRank, lottRankCounter.get(lottoRank) + 1));
+
+		return lottRankCounter;
 	}
 
 	

65bf060af1610357e8856eea0a53638267b42390
leedohyun <lucky77795@naver.com>
2023-11-03 17:38:48 +0900

    fix(Lotto) : 출력 조건에 맞게 수정.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 import lotto.domain.EarningRate;
 import lotto.domain.LottoCheckResult;
@@ -13,23 +15,34 @@ public class OutputView {
     public void printLotto(LottoTickets lottoTickets) {
         Collection<Lotto> tickets = lottoTickets.getLottoTickets();
 
-        System.out.printf("%d개를 구매했습니다.\n", tickets.size());
-        for (Lotto lotto : tickets) {
-            System.out.println(lotto.getLotto());
-        }
+        System.out.printf("\n%d개를 구매했습니다.\n", tickets.size());
+
+        tickets.stream()
+                .map(Lotto::getLotto)
+                .forEach(lotto -> System.out.println(lotto));
     }
 
     public void printResult(LottoCheckResult lottoCheckResult, EarningRate earningRate) {
-        System.out.println("당첨 통계\n---");
+        System.out.println("\n당첨 통계\n---");
 
         Map<WinningStatus, Integer> result = lottoCheckResult.getResult();
 
+        Locale locale = new Locale("ko", "KR");
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(locale);
+
         for (Map.Entry<WinningStatus, Integer> entry : result.entrySet()) {
             if (entry.getKey().getMatchCount() == 0) continue;
-
-            System.out.printf("%d개 일치 (%d원) - %d개\n", entry.getKey().getMatchCount(), entry.getKey().getPrize(), entry.getValue());
+            if (entry.getKey() == WinningStatus.FIVE_MATCH_WITH_BONUS) {
+                System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n", entry.getKey().getMatchCount(), formatter.format(entry.getKey().getPrize()).substring(1), entry.getValue());
+            } else if (entry.getKey() != WinningStatus.FIVE_MATCH_WITH_BONUS) {
+                System.out.printf("%d개 일치 (%s원) - %d개\n", entry.getKey().getMatchCount(), formatter.format(entry.getKey().getPrize()).substring(1), entry.getValue());
+            }
         }
 
-        System.out.printf("총 수익률은 %.1f%% 입니다.", earningRate.getRate());
+        System.out.printf("총 수익률은 %.1f%%입니다.", earningRate.getRate());
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
     }
 }

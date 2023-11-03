bccdb5ddc5666d371c55a5ff7341b779df0dad65
peppone-choi <peppone.choi@gmail.com>
2023-11-04 00:43:21 +0900

    feat: WinningNumber 초기 구현

--- src/main/java/lotto/enumerate/ErrorCode.java
+++ src/main/java/lotto/enumerate/ErrorCode.java
@@ -3,6 +3,7 @@ package lotto.enumerate;
 public enum ErrorCode {
     LOTTO_NUMBER_OVER_OR_UNDER_SIZE("[ERROR] 1장의 로또 번호의 갯수는 6개여야 합니다."),
     LOTTO_NUMBER_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    LOTTO_NUMBER_BONUS_DUPLICATE("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     LOTTO_NUMBER_UNDER_OR_OVER("[ERROR] 로또 번호는 1 ~ 45 사이여야 합니다.");
 
     ErrorCode(String message) {
---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_BONUS_DUPLICATE;
+import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_UNDER_OR_OVER;
+import static lotto.util.ExceptionCodeThrow.exceptionCodeThrow;
+
+import java.util.List;
+
+public class WinningNumber extends Lotto {
+    private final int bonus;
+
+    public WinningNumber(List<Integer> numbers, int bonus) {
+        super(numbers);
+        validateBonusNumber(numbers, bonus);
+        this.bonus = bonus;
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, int bonus) {
+        bonusNumberWinningNumberDuplicateValidate(numbers, bonus);
+        bonusNumberUnderOverValidate(bonus);
+    }
+
+    private void bonusNumberUnderOverValidate(int bonus) {
+        if (bonus < 0 || bonus > 45) {
+            exceptionCodeThrow(LOTTO_NUMBER_UNDER_OR_OVER);
+        }
+    }
+
+    private void bonusNumberWinningNumberDuplicateValidate(List<Integer> numbers, int bonus) {
+        if (numbers.stream().anyMatch(number -> number == bonus)) {
+            exceptionCodeThrow(LOTTO_NUMBER_BONUS_DUPLICATE);
+        }
+    }
+}

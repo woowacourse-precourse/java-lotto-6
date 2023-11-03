3beca0b61fdfc1f0947c91ce63481a56a35a6e9b
IMWoo94 <leemeawoo94@gmail.com>
2023-11-03 21:18:48 +0900

    refactor : 불필요 메소드 제거
    불필요 메소드 제거

--- src/main/java/lotto/dto/LottoResultResponseDto.java
+++ src/main/java/lotto/dto/LottoResultResponseDto.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public record LottoResultResponseDto(List<Integer> prizeResults, BigDecimal profitRate) {
 
-    public int getPrizeResultCount(int index) {
-        return prizeResults.get(index);
-    }
-
     public String toStringProfitRate() {
         return profitRate.stripTrailingZeros()
                 .toPlainString();

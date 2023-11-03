38bd5edc97693fd14e449309247dca87b20dbfbb
brightfanta <brightfanta@gmail.com>
2023-11-03 15:06:22 +0900

    feat(Rank): 등수 생성 및 등수별 상금값 연결

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,4 +1,15 @@
 package lotto;
 
 public enum Rank {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000);
+
+    private final Integer prize;
+
+    Rank(Integer prize) {
+        this.prize = prize;
+    }
 }

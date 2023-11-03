0662e64151804d973719b16520878468a91606e5
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-03 19:41:47 +0900

    refactor: 로또 숫자 개수 상수로 추출 및 관련 네이밍 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,8 +18,8 @@ public class Lotto {
     }
 
     private static void checkSizeOfLotto(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorMessage.OVER_SIZE_OF_LOTTO_NUMBER);
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.OVER_SIZE_OF_LOTTO);
         }
     }
 

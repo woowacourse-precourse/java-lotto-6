0d4e1d02fbbb6a6bfd937331a7256cc65294c623
yechop <channetattoo@gmail.com>
2023-11-04 03:21:41 +0900

    fix(profitCalculator): FIFTH -> FIRST 수정

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -14,7 +14,7 @@ public class ProfitCalculator {
 
     public double getLottoProfit(int money) {
         int sum = 0;
-        sum += winningLottoCounts.get(Constants.FIFTH_RANK) * Rank.FIRST.getPrize();
+        sum += winningLottoCounts.get(Constants.FIRST_RANK) * Rank.FIRST.getPrize();
         sum += winningLottoCounts.get(Constants.SECOND_RANK) * Rank.SECOND.getPrize();
         sum += winningLottoCounts.get(Constants.THIRD_RANK) * Rank.THIRD.getPrize();
         sum += winningLottoCounts.get(Constants.FOURTH_RANK) * Rank.FOURTH.getPrize();

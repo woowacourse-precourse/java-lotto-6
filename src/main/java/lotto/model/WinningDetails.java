398264b6f3d40c2cc0a5e52a2ffcc63aa033508e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-03 22:57:29 +0900

    feat: 당첨 내역 클래스 생성, 각 로또마다 일치하는 숫자 갯수 판별하는 로직 구현, 보너스 번호 일치 여부 로직 구현

---
+++ src/main/java/lotto/model/WinningDetails.java
@@ -0,0 +1,78 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningDetails {
+    private final List<Lotto> lottos;
+    private final List<Integer> winningNumbers;
+    private final int purchaseMoney;
+    private final int bonusNumber;
+
+    private int[] rank;
+    private boolean bonusMatch;
+
+    public WinningDetails(List<Lotto> lottos, List<Integer> winningNumbers, int purchaseMoney, int bonusNumber) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+        this.purchaseMoney = purchaseMoney;
+        this.bonusNumber = bonusNumber;
+        rank = new int[6];
+
+        checkWinningCount();
+    }
+
+    public int[] getRank(){
+        return rank;
+    }
+
+    private void checkWinningCount(){
+        for (Lotto lotto: lottos) {
+            checkMatchNumbersPerLotto(getMatchWinningNumber(lotto));
+            bonusMatch = false;
+        }
+    }
+
+    private int getMatchWinningNumber(Lotto lotto){
+        int matchNumers = 0;
+        List<Integer> lottoNumbers = lotto.getValue();
+
+        for (Integer number: lottoNumbers) {
+            if(winningNumbers.contains(number)) matchNumers++;
+        }
+
+        if(matchNumers == 5){
+            checkBonusMatch(lottoNumbers);
+        }
+
+        return matchNumers;
+    }
+
+    private void checkMatchNumbersPerLotto(int matchNumbers){
+        if(matchNumbers == 3 && !bonusMatch){
+            rank[5]++;
+        }
+
+        if(matchNumbers == 4 && !bonusMatch){
+            rank[4]++;
+        }
+
+        if(matchNumbers == 5 && !bonusMatch){
+            rank[3]++;
+        }
+
+        if(matchNumbers == 5 && bonusMatch){
+            rank[2]++;
+        }
+
+        if(matchNumbers == 6){
+            rank[1]++;
+        }
+    }
+
+    private void checkBonusMatch(List<Integer> numbers){
+        if(numbers.contains(bonusNumber)){
+            bonusMatch = true;
+        }
+        bonusMatch = false;
+    }
+}

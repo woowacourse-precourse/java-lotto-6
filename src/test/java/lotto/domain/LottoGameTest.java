20028fc27c9d683dda4a3c511dff86029fac37af
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-03 09:51:15 +0900

    test(LottoGameTest) : 구매 수량만틈 로또 번호 생성

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -1,4 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class LottoGameTest {
-  
+
+    @DisplayName("구매 수량만큼 로또 번호 세트를 생성합니다")
+    @Test
+    void createLottoGame() {
+        // given
+        int buyingAmount = 10;
+
+        // when
+        LottoGame lottoGame = LottoGame.createLottoGame(buyingAmount);
+        List<Lotto> lottos = lottoGame.getLottos();
+
+        // then
+        assertThat(lottos.size()).isEqualTo(10);
+    }
 }

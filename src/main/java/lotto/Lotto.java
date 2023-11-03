73554da04771c82c4d05edadfffec9169ff49b77
alscjf1329 <alscjf1329@gmail.com>
2023-11-03 23:23:19 +0900

    feat(Lotto): validateLottoNum을 통해 로또 숫자 확인하는 코드 작성
    validateLottoNum을 통해 numbers에 들어 있는 로또 숫자들이 1~45 범위 내에 있는지 확인하는 코드를 작성했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import java.util.List;
+import lotto.utils.LottoUtil;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.forEach(LottoUtil::validateLottoNum);
         this.numbers = numbers;
     }
 

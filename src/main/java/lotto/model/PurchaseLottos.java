7d08732f2b99d16d8e46cffa6360cb69288e53d6
Jiyoung Choi <12201817@inha.edu>
2023-11-03 15:10:34 +0900

    feat(PurchaseLottos) : 구입한 로또를 저장해 놓는 클래스 구현

---
+++ src/main/java/lotto/model/PurchaseLottos.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import lotto.constant.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchaseLottos {
+    private final LottoMachine lottoMachine;
+    private final int count;
+    private final List<Lotto> purchaseLottos = new ArrayList<>();
+
+    PurchaseLottos(LottoMachine lottoMachine, int purchaseAmount) {
+        this.lottoMachine = lottoMachine;
+        this.count = purchaseAmount / Constant.PURCHASE_AMOUNT_UNIT.getValue();
+        setPurchaseLottos();
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<Lotto> getPurchaseLottos() {
+        return purchaseLottos;
+    }
+
+    private void setPurchaseLottos() {
+        for (int i = 0; i < count; ++i) {
+            purchaseLottos.add(new Lotto(lottoMachine.generate()));
+        }
+    }
+}

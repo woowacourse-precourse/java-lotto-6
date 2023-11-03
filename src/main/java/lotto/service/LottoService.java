d698ed71c7eadc8cbe61e7dbab8db35493453f18
euics <pjh575157@gmail.com>
2023-11-03 12:19:38 +0900

    feat(Service): displayBonusNumber 메서드 구현
    보너스 번호 입력 및 검증 메서드 구현
    - `displayBonusNumber`
      - 사용자로부터 로또 보너스 번호를 입력받아, 해당 번호가 올바른 형식인지 및 중복되지 않는지 검증
      - 유효하지 않은 입력에 대해 오류 메시지를 출력하고, 다시 입력을 요청

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import lotto.domain.LottoTicket;
+import lotto.exception.BonusNumberExceptionMessage;
 import lotto.exception.PurchaseExceptionMessage;
 import lotto.exception.WinningNumberExceptionMessage;
 import lotto.view.InputView;
@@ -57,6 +58,31 @@ public class LottoService {
                 WinningNumberExceptionMessage.validateBoundaryWinningNumber(winningNumbers);
 
                 return winningNumbers;  // 모든 검증이 통과되면 winningNumbers 반환
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int displayBonusNumber(List<Integer> winningNumbers) {
+        InputView.printBonusNumberPrompt();
+
+        while (true) {
+            try {
+                String bonusNumberStr = InputView.getBonusNumber();
+
+                // 보너스 번호 형식 검증
+                BonusNumberExceptionMessage.validateBonusNumberFormat(bonusNumberStr);
+
+                int bonusNumber = Integer.parseInt(bonusNumberStr);
+
+                // 보너스 번호 범위 검증
+                BonusNumberExceptionMessage.validateBoundaryBonusNumber(bonusNumber);
+
+                // 보너스 번호 중복 검증
+                BonusNumberExceptionMessage.validateBonusNumber(winningNumbers, bonusNumber);
+
+                return bonusNumber;  // 모든 검증이 통과되면 bonusNumber 반환
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }

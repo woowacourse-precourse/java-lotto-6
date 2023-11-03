69dc141fc90871b25d66707aa25f9e52f6244a58
kimky0320 <kim3268418@naver.com>
2023-11-03 09:22:27 +0900

    feat: 구매 금액만큼 로또 구매

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,19 +1,26 @@
 package lotto.controller;
 
+import lotto.domain.LottoTickets;
 import lotto.view.InputView;
 
 import java.util.ArrayList;
 
 public class LottoController {
     private final InputView inputView;
+    private final LottoTickets lottoTickets;
 
     public LottoController() {
         this.inputView = new InputView();
+        this.lottoTickets = new LottoTickets();
     }
 
     public void run() {
         Integer money = inputView.inputMoney();
+        lottoTickets.buyLotto(money);
+
         ArrayList<Integer> winningNumber = inputView.winningNumber();
+
+
         Integer bonusNumber = inputView.bonusNumber(winningNumber);
     }
 }
---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTickets {
+    private final Integer PRICE = 1000;
+    private final LottoGenerator lottoGenerator;
+
+    private ArrayList<Lotto> lottoTickets = new ArrayList<>();
+
+    public LottoTickets() {
+        this.lottoGenerator = new LottoGenerator();
+    }
+
+    public ArrayList<Lotto> buyLotto(Integer money) {
+        int ticket = money / PRICE;
+
+        while (lottoTickets.size() != ticket) {
+            List<Integer> lottoNum = lottoGenerator.generate();
+            Lotto lotto = new Lotto(lottoNum);
+            lottoTickets.add(lotto);
+        }
+
+        return lottoTickets;
+    }
+}

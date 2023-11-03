49d9b108ca4e7fd884a398f7edbdd2f3b62addf1
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-03 21:31:14 +0900

    test: 보너스 번호 입력 기능에 대한 테스트 코드 작성
    오타 수정 포함

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class LottoMachineTest {
     @DisplayName("당첨 번호가 입력되지 않으면 예외가 발생한다.")
     @Test
-    void createWinngNumbersByEmpty() {
+    void createWiningNumbersByEmpty() {
         assertThatThrownBy(() -> new LottoMachine("", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 입력 값을 넣어주세요.");
@@ -16,7 +16,7 @@ class LottoMachineTest {
 
     @DisplayName("당첨 번호에 공백이 있으면 예외가 발생한다.")
     @Test
-    void createWinngNumbersByBlack() {
+    void createWiningNumbersByBlack() {
         assertThatThrownBy(() -> new LottoMachine("1, 2", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 공백 없이 쉼표(,)로 구분된 숫자로만 입력해주세요.");
@@ -24,7 +24,7 @@ class LottoMachineTest {
 
     @DisplayName("당첨 번호가 쉼표를 제외하고 숫자가 아니면 예외가 발생한다.")
     @Test
-    void createWinngNumbersByNotNumber() {
+    void createWiningNumbersByNotNumber() {
         assertThatThrownBy(() -> new LottoMachine("a,b,c", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 공백 없이 쉼표(,)로 구분된 숫자로만 입력해주세요.");
@@ -32,7 +32,7 @@ class LottoMachineTest {
 
     @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
-    void createWinngNumbersByOverSize() {
+    void createWiningNumbersByOverSize() {
         assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,6,7", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 6개 여야 합니다.");
@@ -40,7 +40,7 @@ class LottoMachineTest {
 
     @DisplayName("당첨 번호의 개수가 6개 보다 적으면 예외가 발생한다.")
     @Test
-    void createWinngNumbersByUnderSize() {
+    void createWiningNumbersByUnderSize() {
         assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 6개 여야 합니다.");
@@ -48,7 +48,7 @@ class LottoMachineTest {
 
     @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void createWinngNumbersByDuplicateNumber() {
+    void createWiningNumbersByDuplicateNumber() {
         assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,5", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
@@ -56,9 +56,41 @@ class LottoMachineTest {
 
     @DisplayName("당첨 번호의 숫자 범위가 1~45가 아닌 경우 예외가 발생한다.")
     @Test
-    void createWinngNumbersByOverRange() {
+    void createWiningNumbersByOverRange() {
         assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,46", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 입력되지 않으면 예외가 발생한다.")
+    @Test
+    void createBonusNumbersByEmpty() {
+        assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,6", ""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 입력 값을 넣어주세요.");
+    }
+
+    @DisplayName("보너스 번호가 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createBonusNumbersByNotNumber() {
+        assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,6", "a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자만 입력 가능합니다.");
+    }
+
+    @DisplayName("보너스 번호의 숫자 범위가 1~45가 아닌 경우 예외가 발생한다.")
+    @Test
+    void createBonusNumbersByOverRange() {
+        assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,6", "46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createBonusNumbersByDuplicateWiningNumber() {
+        assertThatThrownBy(() -> new LottoMachine("1,2,3,4,5,6", "6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
     }
 }

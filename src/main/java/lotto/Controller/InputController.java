57dca084db676e0e10423512f2a6d855854d8783
ldw <leehan02@hanyang.ac.kr>
2023-11-03 23:02:07 +0900

    feat(lotto): 사용자로부터 보너스 번호를 입력받는 기능 구현

--- src/main/java/lotto/Controller/InputController.java
+++ src/main/java/lotto/Controller/InputController.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputController {
     private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String ENTER_SIX_DIGITS_NUMBERS = "당첨 번호를 입력해 주세요.";
-
+    private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     public String purchaseAmount() {
         System.out.println(ENTER_PURCHASE_AMOUNT);
         return Console.readLine();
@@ -13,6 +13,11 @@ public class InputController {
 
     public String numbers() {
         System.out.println(ENTER_SIX_DIGITS_NUMBERS);
+        return Console.readLine();
+    }
+
+    public String bonusNumbers() {
+        System.out.println(ENTER_BONUS_NUMBER);
         return Console.readLine();
     }
 }

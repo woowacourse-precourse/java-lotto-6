60631440c08f0ade0cb9997788a0e1d01751bdfa
Jiyoung Choi <12201817@inha.edu>
2023-11-03 16:55:50 +0900

    feat(WinningPrize) : 당첨 순위와 관련된 정보들을 저장해 놓는 enum 클래스 추가

--- src/main/java/lotto/model/WinningPrize.java
+++ src/main/java/lotto/model/WinningPrize.java
@@ -1,19 +1,25 @@
 package lotto.model;
 
+import lotto.constant.BonusStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum WinningPrize {
-    FIRST_PRIZE(2_000_000_000, 6),
-    SECOND_PRIZE(30_000_000, 5),
-    THIRD_PRIZE(1_500_000, 5),
-    FOURTH_PRIZE(50_000, 4),
-    FIFTH_PRIZE(5_000, 3),
-    ;
+    FIRST_PRIZE(2_000_000_000, 6, BonusStatus.NO_MATTER),
+    SECOND_PRIZE(30_000_000, 5, BonusStatus.IS_IN_LOTTO),
+    THIRD_PRIZE(1_500_000, 5, BonusStatus.IS_NOT_IN_LOTTO),
+    FOURTH_PRIZE(50_000, 4, BonusStatus.NO_MATTER),
+    FIFTH_PRIZE(5_000, 3, BonusStatus.NO_MATTER);
 
     private final long reward;
     private final int matchCount;
+    private final BonusStatus bonusStatus;
 
-    WinningPrize(long reward, int matchCount) {
+    WinningPrize(long reward, int matchCount, BonusStatus bonusStatus) {
         this.reward = reward;
         this.matchCount = matchCount;
+        this.bonusStatus = bonusStatus;
     }
 
     public long getReward() {
@@ -22,5 +28,15 @@ public enum WinningPrize {
 
     public int getMatchCount() {
         return matchCount;
+    }
+
+    public BonusStatus getBonusStatus() {
+        return bonusStatus;
+    }
+
+    public static Optional<WinningPrize> getWinningPrize(int matchCount, BonusStatus bonusStatus) {
+        return Arrays.stream(WinningPrize.values())
+                .filter(winningPrize -> winningPrize.getMatchCount() == matchCount && winningPrize.getBonusStatus() == bonusStatus)
+                .findAny();
     }
 }

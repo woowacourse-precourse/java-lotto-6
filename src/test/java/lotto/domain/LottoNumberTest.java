78b2f36eb545ee182ca95610c4ee5c21ee53bbac
eora21 <eora21@naver.com>
2023-11-03 16:09:51 +0900

    test: 💍 수정 로또 숫자 테스트 Assertion에서 람다식 미적용 정정 및 로또 번호 클래스 import

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import domain.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,6 +12,6 @@ public class LottoNumberTest {
     @DisplayName("로또 번호 생성 불가 테스트 - 범위 벗어남")
     @ValueSource(ints = {-45, -1, 0, 46, 99})
     void createLottoNumberFailTest(int value) {
-        assertThrows(IllegalArgumentException.class, new LottoNumber(value));
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumber(value));
     }
 }

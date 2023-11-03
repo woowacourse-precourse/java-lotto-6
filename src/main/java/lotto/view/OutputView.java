088a52009bfb5902c5a33fcc15b9224d57f49bab
kylim99 <zxzx05050@naver.com>
2023-11-03 19:44:42 +0900

    fix: 발행 갯수 출력 시 한 줄 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,7 @@
 package lotto.view;
 
 public class OutputView {
-    private final String AMOUNT_MESSAGE = "%d개를 구매했습니다";
+    private final String AMOUNT_MESSAGE = "\n%d개를 구매했습니다";
 
     public void printAmount(int amount){
         System.out.println(String.format(AMOUNT_MESSAGE,amount));

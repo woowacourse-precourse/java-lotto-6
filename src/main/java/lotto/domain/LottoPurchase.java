ff7b6d4772c85bc65708137fb74d89bd6aa2a8df
Isaac Han <isaachan233@gmail.com>
2023-11-03 13:32:31 +0900

    feat: 양수인 값이 들어오는지 검증하는 기능 추가

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -4,10 +4,27 @@ public class LottoPurchase {
     private final Integer amount;
 
     public LottoPurchase(int amount) {
+        validateAmount(amount);
         this.amount = amount;
     }
 
     public LottoPurchase(String amount) {
-        this.amount = Integer.parseInt(amount);
+        int parsedAmount = stringToInt(amount);
+        validateAmount(parsedAmount);
+        this.amount = parsedAmount;
+    }
+
+    private int stringToInt(String amount) {
+        try {
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("[ERROR] 숫자형이 아닌 문자를 입력하였습니다.");
+        }
+    }
+
+    private void validateAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 양수인 값만 입력해주세요.");
+        }
     }
 }

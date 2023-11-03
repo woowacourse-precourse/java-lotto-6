59b6a24e152428f8a0e32bdb54eaf906ddb9bc6f
SeongIl <biuld1234@gmail.com>
2023-11-03 13:45:19 +0900

    refactor: change int -> Integer

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.domain.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        User user =new User();
+        user.purchaseLotto();
     }
 }
--- src/main/java/lotto/ui/Computer.java
+++ src/main/java/lotto/ui/Computer.java
@@ -14,22 +14,20 @@ public class Computer {
         try {
             Integer.parseInt(userInput);
         } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 숫자만(소수점 제외) 입력해주세요.");
         }
     }
 
-    public void checkRemainder(int userinput) {
-        if ((userinput % 1000) != 0) {
+    public void checkRemainder(Integer userInput) {
+        if ((userInput % 1000) != 0) {
             throw new IllegalArgumentException("[ERROR] 1000단위의 숫자를 입력해주세요.");
         }
     }
 
-    public void checkZero(String uesrInput){
-        if(uesrInput.charAt(0) == '0'){
+    public void checkZero(String userInput){
+        if(userInput.charAt(0) == '0'){
             throw new IllegalArgumentException("[ERROR] 숫자 앞에 0을 입력하지 마세요.");
         }
     }
-
-
 
 }

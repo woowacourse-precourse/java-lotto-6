aa95982158fcce813c52883c3cc3fd0bac241e9c
Genius-PAK <pyj789123@gmail.com>
2023-11-03 14:28:01 +0900

    feat(Message): 출력 메시지 상수를 담은 클래스 구현

---
+++ src/main/java/Message.java
@@ -0,0 +1,9 @@
+public class Message {
+    private static final String ERROR = "[ERROR]";
+    public static final String NOT_A_NUMBER_EXCEPTION = ERROR + " 숫자만 입력해주세요. ";
+
+    public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요. ";
+    public static final String LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요. ";
+    public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요. ";
+    public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계\n---";
+}

844bb3013f58f213528f2ed91953cb0ef1a85a92
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-03 18:46:00 +0900

    feature(InputValidator): 입력한 6개의 숫자가 중복되지 않는지 유효성 검사하는 코드 구현

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -2,6 +2,10 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
     public Boolean isNumericValidator(String number){
         try {
@@ -16,4 +20,14 @@ public class InputValidator {
         return (number >= 1 && number <= 45);
     }
 
+    // 유효성 검사 : 중복 숫자 검사
+    private Boolean isNotOverlapValidator(List<Integer> numbers){
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for(Integer number : numbers){
+            if(!uniqueNumbers.add(number)){
+                return false;
+            }
+        }
+        return true;
+    }
 }

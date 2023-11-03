0876173e29d139842ed32d87bfce2d1b92b34b80
HwangSeokJin <ghkd3703@gmail.com>
2023-11-03 21:56:25 +0900

    feat: 당첨 번호를 입력받는다

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,17 +1,27 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String MESSAGE_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String DELIMITER = ",";
 
-    public int inputPurchaseAmount() {
+    public int inputPurchaseAmount() throws IllegalArgumentException{
         System.out.println(MESSAGE_INPUT_PURCHASE_AMOUNT);
-        try {
-            return getInteger();
-        } catch (IllegalArgumentException exception) {
-            return getInteger();
-        }
+        return getInteger();
+    }
+
+    public List<Integer> inputWinningNumbers() throws IllegalArgumentException{
+        System.out.println(MESSAGE_INPUT_WINNING_NUMBERS);
+        String read = Console.readLine();
+        List<Integer> numbers = getNumbers(read);
+        validateCount(numbers, 6);
+        validateRange(numbers, 1, 45);
+        return numbers;
     }
 
     private int getInteger() throws IllegalArgumentException{
@@ -20,11 +30,34 @@ public class InputView {
         return Integer.parseInt(read);
     }
 
+    private List<Integer> getNumbers(String s) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String num : s.split(DELIMITER)) {
+            validateIsNumber(num);
+            numbers.add(Integer.parseInt(num));
+        }
+        return numbers;
+    }
+
     private void validateIsNumber(String s) {
         try {
             Integer.parseInt(s);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException("숫자만 입력해야 합니다");
+        }
+    }
+
+    private void validateCount(List<Integer> numbers, int count) {
+        if (numbers.size() != count) {
+            throw new IllegalArgumentException("입력한 숫자의 개수가 올바르지 않습니다");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers, int startInclusive, int endInclusive) {
+        for (int number : numbers) {
+            if (number < startInclusive || number > endInclusive) {
+                throw new IllegalArgumentException("입력한 숫자의 범위가 올바르지 않습니다");
+            }
         }
     }
 }

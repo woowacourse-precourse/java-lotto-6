c6abe81872012e87ea76c197b41ef4107281c0f6
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 20:00:36 +0900

    refactor(InputView): 당첨로또번호 입력 예외처리 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,9 @@ package lotto.view;
 import java.util.Arrays;
 import java.util.List;
 
+import lotto.domain.Lotto;
 import lotto.io.InputViewReader;
+import lotto.util.message.ErrorMessage;
 import lotto.util.validator.Validator;
 
 public class InputView {
@@ -21,9 +23,10 @@ public class InputView {
 
 	public List<Integer> inputAnswerLottoNumbers() {
 		String inputValue = READER.readLine();
-
 		String[] separatedNumbers = inputValue.split(DELIMITER);
-
+		
+		validateAnswerLottoNumbers(separatedNumbers);
+		
 		return Arrays.stream(separatedNumbers).map(Integer::new)
 						.toList();
 	}
@@ -37,7 +40,16 @@ public class InputView {
 	}
 
 	private void validateInputNumber(String inputValue) {
-		Validator.validateIsEmpty(inputValue);
-		Validator.validateNumberFormat(inputValue);
+		Validator.validateIsEmpty(inputValue, ErrorMessage.INPUT_EMPTY_ERROR.getMessage());
+		Validator.validateNumberFormat(inputValue, ErrorMessage.INPUT_LETTER_ERROR.getMessage());
+	}
+
+	private void validateAnswerLottoNumbers(String[] separatedNumbers) {
+		Validator.validateStrArrLength(separatedNumbers, Lotto.LOTTO_SIZE, 
+					ErrorMessage.INPUT_ANSWER_LOTTO_NUMBER_ERROR.getForMatMessage(Lotto.LOTTO_SIZE));
+
+		for (String number : separatedNumbers) {
+			Validator.validateNumberFormat(number, ErrorMessage.INPUT_LETTER_ERROR.getMessage());
+		}
 	}
 }

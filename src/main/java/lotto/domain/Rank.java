3c34f4c14e36b81a00dd2e4b8405ba98c433dcac
yechop <channetattoo@gmail.com>
2023-11-03 15:53:01 +0900

    feat(Rank): 등수와 상금을 모아놓는 enum 생성

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000);
+
+    private final Integer winningPrize;
+
+    Rank(Integer winningPrize) {
+        this.winningPrize = winningPrize;
+    }
+}

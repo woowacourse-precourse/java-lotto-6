65c9fd353b389a945964f75d7a70a107b6a167ab
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-03 17:19:47 +0900

    test(LottoNumberGenerator): 로또들을 생성하는 기능 테스트 코드 작성

---
+++ src/test/java/lotto/generator/LottosGeneratorTest.java
@@ -0,0 +1,35 @@
+package lotto.generator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import lotto.domain.Lotto;
+import lotto.domain.Money;
+
+public class LottosGeneratorTest {
+
+	private final LottosGenerator lottosGenerator = new LottosGenerator();
+
+	@DisplayName("돈을 1000으로 나눈 몫만큼 Lotto가 생성되는 지 확인한다.")
+	@MethodSource("createGenerateMethodParameter")
+	@ParameterizedTest
+	void checkGenerateLottos(int inputValue, int expect) {
+		Money money = new Money(inputValue);
+		lottosGenerator.setMoney(money);
+
+		List<Lotto> lottos = lottosGenerator.generate();
+
+		assertEquals(lottos.size(), expect);
+	}
+
+	static Stream<Arguments> createGenerateMethodParameter() {
+		return Stream.of(Arguments.of(10000, 10), Arguments.of(2000, 2), Arguments.of(8000, 8));
+	}
+}

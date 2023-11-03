67125a80d8bbab611204aaa7f03ddb975c561b5f
eom-tae-in <eti0728@daum.net>
2023-11-04 05:09:37 +0900

    feat: 당첨 번호를 검증하고 저장하는 역할을 하는 WinningNumber 구현

---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import static lotto.util.Constant.LOTTO_NUMBER_COUNT;
+
+import java.util.List;
+import lotto.exception.lotto.ExistDuplicatedNumberException;
+import lotto.exception.lotto.InvalidLottoNumberException;
+
+public class WinningNumber {
+
+    private final List<Integer> numbers;
+
+    public WinningNumber(final List<Integer> numbers) {
+        validateSixNumbers(numbers);
+        validateDuplicateNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateSixNumbers(final List<Integer> numbers) {
+        if (!hasSixNumbers(numbers)) {
+            throw new InvalidLottoNumberException(numbers);
+        }
+    }
+
+    private boolean hasSixNumbers(final List<Integer> numbers) {
+        return numbers.size() != LOTTO_NUMBER_COUNT.getValue();
+    }
+
+    private void validateDuplicateNumbers(final List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new ExistDuplicatedNumberException(numbers);
+        }
+    }
+
+    private boolean hasDuplicate(final List<Integer> numbers) {
+        int distinctCount = (int) numbers.stream()
+                .distinct()
+                .count();
+        return distinctCount != numbers.size();
+    }
+}

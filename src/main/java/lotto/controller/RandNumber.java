cf175af48bae57d9c76bc9c7ce894dc1059113a5
Jiwon Na <jiwonna52@gmail.com>
2023-11-03 20:43:24 +0900

    Feat : 보너스 랜덤 넘버 기능 추가

--- src/main/java/lotto/controller/RandNumber.java
+++ src/main/java/lotto/controller/RandNumber.java
@@ -6,7 +6,14 @@ import java.util.List;
 
 public class RandNumber {
     public static List<Integer> makeRandNumber() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumbers;
     }
+
+    public static List<Integer> makeBonusNumber() {
+        List<Integer> bonusNumber = Randoms.pickUniqueNumbersInRange(1, 45, 1);
+        return bonusNumber;
+    }
+
+
 }

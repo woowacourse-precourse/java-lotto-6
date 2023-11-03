90ffdf49c373489acc6c576866145ecc93cf1acc
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 00:03:00 +0900

    fix(Lotto): validateLottoNum의 검증 조건 변경
    1보다 작고 45보다 크면 예외를 발생시키게 변경했습니다.

--- src/main/java/lotto/utils/LottoUtil.java
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -4,7 +4,10 @@ import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
 
 public class LottoUtil {
     public static void validateLottoNum(int num) {
-        if (1 <= num && num <= 45) {
+        if (num < 1) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
+        }
+        if (num > 45) {
             throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
         }
     }

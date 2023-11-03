61ae18484658f95cdbc29a0059fd98d8c93d786e
devpanic <yeon.dev.8@gmail.com>
2023-11-03 19:16:32 +0900

    feat : 당첨 번호 입력 기능 추가

--- src/main/java/lotto/DataInput.java
+++ src/main/java/lotto/DataInput.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class DataInput {
     public int userInputPayment(){
@@ -8,6 +11,22 @@ public class DataInput {
         userPayment = Console.readLine();
         // Validate
         return Integer.parseInt(userPayment);
+    }
+
+    public List<Integer> userInputWinningNumbers(){
+        StringTokenizer winningNumbersTokenizer = new StringTokenizer(Console.readLine(), ",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        String curToken;
+        int curNumber;
+
+        while(winningNumbersTokenizer.hasMoreTokens()){
+            curToken = winningNumbersTokenizer.nextToken();
+            curNumber = Integer.parseInt(curToken);
+            // Validate
+            winningNumbers.add(curNumber);
+        }
+
+        return winningNumbers;
     }
 
     public int userInputBonusNumber(){

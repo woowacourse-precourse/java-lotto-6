a2c27d4a74b6ad7b3a47ea179814031dea01fa4d
DaeYoungK <hanguk830@naver.com>
2023-11-03 23:29:53 +0900

    feat(Lotto): 로또를 생성하여 로또저장소에 저장하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -16,5 +16,8 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void createLotto(List<Integer> lottoNumber) {
+        Lotto lotto = new Lotto(lottoNumber);
+        LottoStore.getInstance().addLotto(lotto);
+    }
 }

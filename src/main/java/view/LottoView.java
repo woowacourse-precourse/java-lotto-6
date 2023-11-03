6b217ab281a9ec58ce5cc8f9b6f561b1fe3f11dd
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-03 20:01:28 +0900

    <feat> : <View 기능 추가>
    Lotto 프로그램을 실행하는데 유저에게 보이는 모든 부분을 구현 (통계 출력 부분은 어떻게 자료형을 저장할지 고민 해본 후 구현 예정)

--- src/main/java/view/LottoView.java
+++ src/main/java/view/LottoView.java
@@ -1,11 +1,53 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
 import view.constant.ConstantMessage;
 
+import java.util.List;
+
 public class LottoView {
+    // InputView
+    public static String printAskInputMoney(){
+        printlnConstantMessage(ConstantMessage.ASK_MONEY);
+        return Console.readLine();
+    }
+    public static String printAskInputWinningNumber(){
+        printlnConstantMessage(ConstantMessage.ASK_WINNING_NUMBER);
+        return Console.readLine();
+    }
+    public static String printAskInputBonusNumber(){
+        printlnConstantMessage(ConstantMessage.ASK_BONUS_NUMBER);
+        return Console.readLine();
+    }
+
+    // OutputView
+    public static void printBuyResult(int number){
+        System.out.print(number);
+        printlnConstantMessage(ConstantMessage.BUY_RESULT);
+    }
+    public static void printLottoList(List<Lotto> lottos){
+        for(int i=0; i<lottos.size(); i++){
+            System.out.println(lottos.get(i));
+        }
+        printNewLine();
+    }
+    public static void printWinningResult(){
+        printlnConstantMessage(ConstantMessage.WINNING_RESULT);
+        printlnConstantMessage(ConstantMessage.THREE_DASH);
+        // 당첨 통계 출력 (미구현)
+    }
+    public static void printTotalReturn(double totalReturn){
+        printConstantMessage(ConstantMessage.TOTAL_RETURN);
+        System.out.print(" "+totalReturn);
+        printlnConstantMessage(ConstantMessage.IPNIDA);
+    }
 
     private static void printConstantMessage(ConstantMessage constantMessage){
+        System.out.printf(constantMessage.getMessage());
+    }
+
+    private static void printlnConstantMessage(ConstantMessage constantMessage){
         System.out.println(constantMessage.getMessage());
     }
     private static void printNewLine(){

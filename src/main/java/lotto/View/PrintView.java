ef6956eaeeac4b6d6109c2aab31b6d1187ababca
김영관 <kimyogun78@naver.com>
2023-11-03 13:04:18 +0900

    feat: PrintView 화면 출력 기능 일부 구현

--- src/main/java/lotto/View/PrintView.java
+++ src/main/java/lotto/View/PrintView.java
@@ -1,7 +1,27 @@
 package lotto.View;
 
+import lotto.Model.Lottos;
+
 public class PrintView {
 
+    public void inputMoneyPrint(){
+        System.out.println("구입금액을 입력해주세요.");
+    }
 
+    public void generatedLottosPrint(Lottos lottos){
+
+    }
+
+    public void inputNumbersPrint(){
+        System.out.println("당첨번호를 입력해주세요");
+    }
+
+    public void inputBonusNumberPrint(){
+        System.out.println("보너스 번호를 입력해주세요");
+    }
+
+    public void resultPrint(){
+
+    }
 
 }

f50a52eb580560aea696f8a3fbeb033bf419fe6f
GaBaljaintheroom <junsu1222@naver.com>
2023-11-03 23:25:16 +0900

    feat(ErrorMessage): 숫자 입력 에러 메시지 상수 생성

---
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -0,0 +1,16 @@
+package lotto.constants;
+
+public enum ErrorMessage {
+    ERROR_TAG("[ERROR] "),
+    NOT_NUMERIC_ERROR("숫자를 입력해 주세요");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_TAG.message + message;
+    }
+}

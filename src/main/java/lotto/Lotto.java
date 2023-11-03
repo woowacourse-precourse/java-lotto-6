92c5cc329934a2e41c4f5a942b8d1566391345b2
mmalfoy <stpmalfoy@gmail.com>
2023-11-04 06:34:51 +0900

    feat(Lotto) : 중복 숫자 보유여부 확인 기능추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,36 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicated(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 숫자여야 합니다.");
         }
     }
+
+    private void checkDuplicated(List<Integer> numbers) {
+        Map<Integer, Integer> checkTable = new HashMap<>();
+        for (Integer element : numbers) {
+            if (checkTable.containsKey(element)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 다른 숫자여야 합니다.");
+            }
+            checkTable.put(element,1);
+        }
+    }
+
+
 
     // TODO: 추가 기능 구현
 }

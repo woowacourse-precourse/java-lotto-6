5af39ffcb34fc724289a74c6b94245e2fa318654
sangchu <didi1484@gmail.com>
2023-11-03 17:31:41 +0900

    feat: askPurchaseAmount 메서드 추가 (구입금액 입력 받는 기능)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    public static String askPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return readLine();
+    }
 }

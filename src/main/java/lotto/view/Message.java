2e10d67d26f5d5d546bb033b6778c6fe573d95c2
Jiwon Na <jiwonna52@gmail.com>
2023-11-03 23:00:37 +0900

    Refactor : 로또 장 출력 부분을 InputHandler로 변경

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -5,10 +5,6 @@ public class Message {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void messageAboutLottoCount(int lottoCount) {
-        System.out.println(lottoCount + "개를 구매했습니다.");
-    }
-
     public static void messageAboutUserLottoNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }

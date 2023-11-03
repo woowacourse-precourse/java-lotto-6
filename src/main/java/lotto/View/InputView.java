4c97f20a564772d4dcdce0f9992d39fe6dbbdb48
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 16:59:30 +0900

    refactor(InputView): 에러메시지에 예시 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,6 +1,7 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.lang.reflect.WildcardType;
 import lotto.Model.Price;
 import lotto.Model.PriceValidator;
 
@@ -12,9 +13,16 @@ public class InputView {
             PriceValidator priceValidator = new PriceValidator(Console.readLine());
             return new Price(priceValidator.getInputPrice());
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 1000원 단위로 1000원~20억원 사이에서 입력해주세요.");
+            System.out.println("[ERROR] 1000원 단위로 1000원~20억원 사이에서 입력해주세요. 예시) 3000");
             return inputPriceForLotto();
         }
     }
+
+//    public static Lotto inputWinningLotto() {
+//        System.out.println("당첨 번호를 입력해 주세요.");
+//        try {
+//            WinningLottoValidator winningLottoValidator = new WinningLottoValidator(Console.readLine());
+//        }
+//    }
 
 }

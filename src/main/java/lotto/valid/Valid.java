3ddc2d135f3c3ac2829502b29a9d338056fc5e9b
김승우 <tmd8633@naver.com>
2023-11-03 21:26:02 +0900

    Feat : Valid 구조변경

--- src/main/java/lotto/valid/Valid.java
+++ src/main/java/lotto/valid/Valid.java
@@ -1,8 +1,5 @@
 package lotto.valid;
 
-import lotto.LottoConst;
-
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,10 +9,6 @@ import java.util.regex.Pattern;
 import static lotto.LottoConst.*;
 
 public class Valid {
-
-    private static final Pattern p = Pattern.compile("[^0-9]");
-
-    private static Matcher m;
 
     public static void validPrice(String text) {
         isEmptyOrNull(text);
@@ -29,8 +22,8 @@ public class Valid {
 
     public static void validWinNum(String text) {
         isEmptyOrNull(text);
-        String[] split = text.split(",");
 
+        String[] split = text.split(",");
         if (split.length != LOTTO_COUNT) {
             throw new IllegalArgumentException(LOTTO_COUNT + "자리를 입력해주세요");
         }
@@ -42,13 +35,18 @@ public class Valid {
         distinctCheck(split);
     }
 
-
-
-
     public static void validBonusNum(String text) {
         isEmptyOrNull(text);
         int number = isNumber(text);
         isLottoRange(number);
+    }
+
+    public static void distinctCheck(List<Integer> winNum, String bonusNum) {
+        if (winNum == null) return;
+        int num = Integer.parseInt(bonusNum);
+        if (winNum.contains(num)) {
+            throw new IllegalArgumentException("보너스 번호가 중복되었습니다");
+        }
     }
 
     private static void isEmptyOrNull(String text) {
@@ -58,11 +56,12 @@ public class Valid {
     }
 
     private static int isNumber(String text) {
-        m = p.matcher(text);
+        Pattern p = Pattern.compile("[^0-9 ]");
+        Matcher m = p.matcher(text);
         if (m.find()) {
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
-        int number = Integer.parseInt(text);
+        int number = Integer.parseInt(text.trim());
         if (number <= 0) {
             throw new IllegalArgumentException("값이 올바르지 않습니다");
         }
@@ -72,12 +71,6 @@ public class Valid {
     private static void isLottoRange(int number) {
         if (number < MIN_LOTTO_NUM || number > MAX_LOTTO_NUM) {
             throw new IllegalArgumentException(MIN_LOTTO_NUM + " ~" + MAX_LOTTO_NUM + " 범위에서 입력해주세요");
-        }
-    }
-
-    public static void distinctCheck(List<Integer> winNum, int bonusNum) {
-        if (winNum.contains(bonusNum)) {
-            throw new IllegalArgumentException("보너스 번호가 중복되었습니다");
         }
     }
 

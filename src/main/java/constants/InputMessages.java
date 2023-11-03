ecc5ad25bd9eada19fa31609f8270c51c1715d42
Leespect <wewqwew153@gmail.com>
2023-11-03 13:33:10 +0900

    Feat : InputMessages 구현

---
+++ src/main/java/constants/InputMessages.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class InputMessages {
+
+    public static final String startBuyMessage = "구입금액을 입력해 주세요.";
+    public static final String getWinNumberMessage = "당첨 번호를 입력해 주세요.";
+    public static final String getBonusNumberMessage = "보너스 번호를 입력해 주세요.";
+
+}

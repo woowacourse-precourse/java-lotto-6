09b47c97ce6fbbc1bf31601bb6263d813fed045c
SeungjunNA <nsjjsng1@gmail.com>
2023-11-03 21:59:08 +0900

    feat : 당첨결과 확인시 수익률까지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -21,12 +21,14 @@ public class OutputView {
     public void bonusNumberMessage(){
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
-    public void matchingMessage(Map<Rank, Integer> ranks){
+    public void matchingMessage(Map<Rank, Integer> ranks, double rateOfReturn){
         System.out.println("\n당첨 통계\n---");
         System.out.println("3개 일치 (5,000원) - " + ranks.get(Rank.FIFTH) + "개");
         System.out.println("4개 일치 (50,000원) - " + ranks.get(Rank.FORTH) + "개");
         System.out.println("5개 일치 (1,500,000원) - " + ranks.get(Rank.THIRD) + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + ranks.get(Rank.SECOND) + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + ranks.get(Rank.FIRST) + "개");
+
+        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }

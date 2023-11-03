900da3ea9ca3e9449e4c43d3a392cc6182334e5e
Jiyoung Choi <12201817@inha.edu>
2023-11-03 22:30:06 +0900

    feat(Lotto) : 입력받은 당첨 번호 유효성 검사 기능 구현

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -16,7 +16,7 @@ class LottoTest {
     private static final int MIN_LOTTO_NUMBER = Constant.MIN_LOTTO_NUMBER.getValue();
     private static final int MAX_LOTTO_NUMBER = Constant.MAX_LOTTO_NUMBER.getValue();
 
-    @DisplayName("올바른 로또번호를 입력하면 성공한다.")
+    @DisplayName("올바른 로또를 생성하면 성공한다.")
     @Test
     void createRightLotto() {
         Lotto lotto = new Lotto(List.of(1, 2, 5, 45, 23, 24));
@@ -53,6 +53,77 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.IS_NOT_IN_RANGE.getMessage(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+    }
+
+    @DisplayName("올바른 당첨 로또를 입력하면 성공한다.")
+    @Test
+    void inputRightLotto() {
+        String inputNumbers = "1, 2, 3, 4, 5, 6";
+        Lotto winningLotto = new Lotto(inputNumbers);
+        assertThat(winningLotto.getNumbers()).size().isEqualTo(6);
+    }
+
+    @DisplayName("입력받은 당첨 로또가 비어있다면 예외가 발생한다.")
+    @Test
+    void inputEmptyLotto() {
+        String inputNumbers = "";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.INPUT_NOT_NUMBER_MESSAGE.getMessage());
+    }
+
+    @DisplayName("입력받은 당첨 로또가 공백으로 이루어져 있다면 예외가 발생한다.")
+    @Test
+    void inputBlankLotto() {
+        String inputNumbers = "    ";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.INPUT_NOT_NUMBER_MESSAGE.getMessage());
+    }
+
+    @DisplayName("입력받은 당첨 로또 숫자가 6개를 넘어간다면 예외가 발생한다.")
+    @Test
+    void inputLottoByOverSize() {
+        String inputNumbers = "1, 2, 3, 4, 5, 6, 7";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.NOT_SAME_WITH_LOTTO_SIZE.getMessage(LOTTO_SIZE));
+    }
+
+    @DisplayName("입력받은 당첨 로또 숫자가 6개보다 작다면 예외가 발생한다.")
+    @Test
+    void inputLottoByUnderSize() {
+        String inputNumbers = "1, 2, 3, 4, 5";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.NOT_SAME_WITH_LOTTO_SIZE.getMessage(LOTTO_SIZE));
+    }
+
+    @DisplayName("입력받은 당첨 로또 숫자가 45보다 크다면 예외가 발생한다.")
+    @Test
+    void inputLottoOutOfRange() {
+        String inputNumbers = "1, 2, 3, 4, 5, 46";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.IS_NOT_IN_RANGE.getMessage(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+    }
+
+    @DisplayName("입력받은 당첨 로또 숫자가 1보다 작다면 예외가 발생한다.")
+    @Test
+    void inputLottoOutOfRange2() {
+        String inputNumbers = "1, 2, 3, 4, 5, 0";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.IS_NOT_IN_RANGE.getMessage(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+    }
+
+    @DisplayName("입력받은 당첨 로또 숫자가 모두 다르지 않다면 예외가 발생한다.")
+    @Test
+    void inputLottoByDuplicateNumber() {
+        String inputNumbers = "1, 2, 3, 4, 5, 5";
+        assertThatThrownBy(() -> new Lotto(inputNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.CONSIST_OF_DUPLICATE_NUMBER.getMessage());
     }
 
 }

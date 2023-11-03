676f971211264ac62831c3f002e4b96338631ec2
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-11-04 02:39:09 +0900

    refactor: PurchaseView method change public static -> private

--- src/main/java/lotto/view/PurchaseView.java
+++ src/main/java/lotto/view/PurchaseView.java
@@ -10,23 +10,23 @@ import static lotto.constant.PurchaseMessage.ASK_PURCHASE_PRICE;
 
 public class PurchaseView {
 
-    public static int requestMoney() {
+    public int requestMoney() {
         printPurchaseMessage(ASK_PURCHASE_PRICE);
         String input = Console.readLine();
         validateMoney(input);
         return createFromInput(input);
     }
 
-    public static int createFromInput(String input) {
+    private int createFromInput(String input) {
         int money = Integer.parseInt(input);
         return money;
     }
 
-    public static void printPurchaseMessage(PurchaseMessage purchaseMessage) {
+    private void printPurchaseMessage(PurchaseMessage purchaseMessage) {
         System.out.println(purchaseMessage.getMessage());
     }
 
-    public static void validateMoney(String input) {
+    private void validateMoney(String input) {
         validateNotNull(input);
         validateNotNumber(input);
         validateUnderZero(input);
@@ -34,32 +34,32 @@ public class PurchaseView {
         validateNotDivide(input);
     }
 
-    public static void validateNotNumber(String input) {
+    private void validateNotNumber(String input) {
         if (!input.matches("\\d+")) {
             throw new IllegalArgumentException(INPUT_STRING.getMessage());
         }
     }
 
-    public static void validateNotNull(String input) {
+    private void validateNotNull(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(EMPTY.getMessage());
         }
     }
 
-    public static void validateEnterComma(String input) {
+    private void validateEnterComma(String input) {
         if (input.contains(",")) {
             throw new IllegalArgumentException(NOT_CONTAIN_COMMA.getMessage());
         }
     }
 
-    public static void validateNotDivide(String input) {
+    private void validateNotDivide(String input) {
         int num = Integer.parseInt(input);
         if (num % 1000 != 0) {
             throw new IllegalArgumentException(NOT_DIVIDE.getMessage());
         }
     }
 
-    public static void validateUnderZero(String input) {
+    private void validateUnderZero(String input) {
         int num = Integer.parseInt(input);
         if (num <= 0) {
             throw new IllegalArgumentException(STRANGE_INTEGER.getMessage());

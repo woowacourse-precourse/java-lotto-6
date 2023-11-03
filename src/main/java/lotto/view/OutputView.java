3f1c27a95e9d561fa4fff0edc7a99fd6daabd357
YongHyeon <pyh4616@naver.com>
2023-11-03 19:27:07 +0900

    Feat(OutputView 클래스): 구매 로또 개수를 출력하는 기능을printLottoCount 메서드로 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,11 +3,16 @@ package lotto.view;
 public class OutputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void printPurchaseAmountInputMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+    }
+
+    public void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + LOTTO_PURCHASE_MESSAGE);
     }
 
     public void printWinningNumbersInputMessage() {

7fb0b1c23335fb26813eaaa2a167a6781813db52
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-03 11:50:24 +0900

    feat: 구매 가격을 입력받는 메서드 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,7 +1,13 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class GameController {
     public static void start(){
+        String number = InputPurchaseNumber();
+    }
 
+    private static String InputPurchaseNumber(){
+        return InputView.purchaseMoney();
     }
 }

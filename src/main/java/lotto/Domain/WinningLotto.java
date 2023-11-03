872a08856867c6c5bed2ca8bc4cf78a1bfe63de7
sami355-24 <ddung355@gmail.com>
2023-11-03 23:52:18 +0900

    feat(Domain): 보너스 번호와 주어진 로또 클래스를 가지는 당첨 로또 클래스 구현
    - 검증 로직에 대한 책임은 보너스번호 클래스와 로또 클래스에 전가

---
+++ src/main/java/lotto/Domain/WinningLotto.java
@@ -0,0 +1,59 @@
+package lotto.Domain;
+
+import static lotto.CommonValidation.CommonValidation.hasBlank;
+import static lotto.CommonValidation.CommonValidation.isInteger;
+import static lotto.CommonValidation.CommonValidation.isValueBetween1And45;
+import static lotto.Message.ExceptionMessage.LottoExceptionMessage.LOTTO_DUPLICATE_WITH_BONUS_NUMBER_ERROR_MESSAGE;
+import static lotto.Util.Util.ConvertStringToInteger;
+
+import java.util.LinkedList;
+import java.util.List;
+import lotto.Exception.LottoException;
+
+public class WinningLotto {
+    private static final String DELIMITER = ",";
+
+    Lotto lotto;
+    BonusNumber bonusNumber;
+
+    public WinningLotto(String inputWinningNumbers, String inputBonusNumber) {
+        List<String> dividedByDelimiter = divideByDelimiter(inputWinningNumbers);
+        List<Integer> winningNumbers = generateWinningNumbers(dividedByDelimiter);
+        BonusNumber number = BonusNumber.of(inputBonusNumber);
+        isBonusNumberInWinningNumbers(winningNumbers, number);
+
+        this.lotto = Lotto.from(winningNumbers);
+        this.bonusNumber = number;
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public BonusNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private List<String> divideByDelimiter(String inputWinningNumbers) {
+        return List.of(inputWinningNumbers.split(DELIMITER));
+    }
+
+    private List<Integer> generateWinningNumbers(List<String> dividedByDelimiter) {
+        List <Integer> winningNumbers = new LinkedList<>();
+
+        for (String string : dividedByDelimiter) {
+            hasBlank(string);
+            isInteger(string);
+            Integer integer = ConvertStringToInteger(string);
+            isValueBetween1And45(integer);
+            winningNumbers.add(integer);
+        }
+
+        return winningNumbers;
+    }
+
+    private void isBonusNumberInWinningNumbers(List<Integer> winningNumbers, BonusNumber bonusNumber) {
+        if (winningNumbers.contains(bonusNumber.getBonusNumber()))
+            throw new LottoException(LOTTO_DUPLICATE_WITH_BONUS_NUMBER_ERROR_MESSAGE.getMessage());
+    }
+}

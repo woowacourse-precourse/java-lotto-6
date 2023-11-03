8593fa2c39b7d82ee92178313e31a22a97d56c6d
HwangSeokJin <ghkd3703@gmail.com>
2023-11-04 02:57:43 +0900

    test: 발행한 로또 출력 기능 테스트

--- src/test/java/lotto/OutputViewTest.java
+++ src/test/java/lotto/OutputViewTest.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +14,22 @@ public class OutputViewTest extends NsTest{
     void 에러_메세지_출력_테스트() {
         view.printErrorMessage("test");
         assertThat(output()).isEqualTo("[ERROR] test");
+    }
+
+    @Test
+    void 발행한_로또_출력_테스트() {
+        List<String> numbers = List.of(
+                "[8, 21, 23, 41, 42, 43]",
+                "[3, 5, 11, 16, 32, 38]",
+                "[7, 11, 16, 35, 36, 44]",
+                "[1, 8, 11, 31, 41, 42]");
+        view.printGeneratedLotto(numbers);
+        assertThat(output()).contains(
+                "4개를 구매했습니다.",
+                "[8, 21, 23, 41, 42, 43]",
+                "[3, 5, 11, 16, 32, 38]",
+                "[7, 11, 16, 35, 36, 44]",
+                "[1, 8, 11, 31, 41, 42]");
     }
 
     @Override

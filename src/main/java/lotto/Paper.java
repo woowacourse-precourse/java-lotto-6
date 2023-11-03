6a739e83274ed8d8581235e46a4a6ad8435de876
jsj <tkdlek4186@naver.com>
2023-11-03 15:36:07 +0900

    feat(Paper) : 당첨숫자와 일치하는 숫자의 개수 저장함수 구현

--- src/main/java/lotto/Paper.java
+++ src/main/java/lotto/Paper.java
@@ -3,16 +3,36 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.Collections;
 
 public class Paper {
     private List<Integer> numbers;
-
+    private int correspondNumber;
     public void pickLottoNumber(){
         this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
     }
 
     public List<Integer> getNumber(){
         return numbers;
+    }
+
+    public void checkCorrespondNum(List<Integer> winNumbers, int bonusNumber){
+        int cnt=0;
+        for(int i : winNumbers){
+            if(numbers.contains(i)){
+                cnt++;
+            }
+        }
+        if(cnt==5 && numbers.contains(bonusNumber)){
+            correspondNumber = 7;
+            return;
+        }
+        correspondNumber=cnt;
+    }
+
+    public int getCorrespondNum(){
+        return correspondNumber;
     }
 
 }

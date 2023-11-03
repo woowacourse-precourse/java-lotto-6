4e4966e7007cc8d6530f32792ba8024b05609ad4
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-03 20:43:12 +0900

    feat(Price): 로또구입금액 입력 값 검증 기능 구현 * 문자면 예외, 2억~int타입 이면 예외, 천원단위가 아니면 예외, 0원 이하면 예외

--- src/main/java/lotto/Model/Price.java
+++ src/main/java/lotto/Model/Price.java
@@ -1,14 +1,42 @@
 package lotto.Model;
 
+import static lotto.Utils.toInt;
+
 public class Price {
     int price;
 
-    public Price(int price) {
-        this.price = price;
+    public Price(String input) {
+        validate(input);
+        this.price = Integer.parseInt(input);
     }
 
-    public int divideWithOneThousands() {
-        return price / 1000;
+    private void validate(String input) {
+        int validInput = isNumber(input);
+        divideWithOneThousands(validInput);
+        isCorrectRange(validInput);
     }
 
+    private int isNumber(String input) {
+        try {
+            int priceInput = toInt(input);
+            if (priceInput > 2000000000) {
+                throw new IllegalArgumentException("[ERROR] 로또구입금액은 최대 20억입니다.");
+            }
+            return priceInput;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만을 입력하세요.(최대 구입금액은 20억입니다)");
+        }
+    }
+
+    private void divideWithOneThousands(int validInput) throws IllegalArgumentException {
+        if (validInput % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하세요.");
+        }
+    }
+
+    private void isCorrectRange(int validInput) {
+        if (validInput <= 0) {
+            throw new IllegalArgumentException("[ERROR] 로또구입금액은 최소 1000원입니다.");
+        }
+    }
 }

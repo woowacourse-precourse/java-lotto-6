3986ccb0984cd59a700e7e08d1c314d985c98b77
Juhyorim <joohr1234@gmail.com>
2023-11-04 04:44:29 +0900

    test(ui): 콘솔 출력 메시지 상수로 변경

--- src/test/java/lotto/ui/ConsoleOutputTest.java
+++ src/test/java/lotto/ui/ConsoleOutputTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ConsoleOutputTest {
+    private static final String LOTTO_PRICE_REQUEST = "구입금액을 입력해 주세요.";
+
     @Test
     @DisplayName("로또 구입금액 입력 요청 출력")
     void 로또_구입금액_입력_요청_출력() {
@@ -22,6 +24,6 @@ class ConsoleOutputTest {
 
         //then
         String result = captor.toString().trim();
-        assertThat(result).isEqualTo("구입금액을 입력해 주세요.");
+        assertThat(result).isEqualTo(LOTTO_PRICE_REQUEST);
     }
 }

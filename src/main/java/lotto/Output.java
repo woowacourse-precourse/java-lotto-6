9fc0856776a886fc1cc55fb023c52956e26a49b0
kyu-wooon <catdog0831@gmail.com>
2023-11-03 17:07:41 +0900

    1. 당첨 내역을 저장하는Map <Winner_Rank,Integer>total_winner 생성 2. total_winner를 초기화하는 함수 구현 3. Winner_Rank label 값 변경

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,9 +1,20 @@
 package lotto;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import static lotto.Input.*;
 
 
 public class Output {
+    public static Map<Winner_Rank,Integer> total_winner = new LinkedHashMap<>();
+
+    public static void init_map(){
+        total_winner.put(Winner_Rank.ONE, 0);
+        total_winner.put(Winner_Rank.TWO, 0);
+        total_winner.put(Winner_Rank.THREE, 0);
+        total_winner.put(Winner_Rank.FOUR, 0);
+        total_winner.put(Winner_Rank.FIVE, 0);
+    }
 
     public static void output_price(int lotto_count){
         System.out.println(lotto_count+"개를 구매했습니다.");
@@ -13,31 +24,24 @@ public class Output {
         for(List<Integer> l : numbers_list){
             System.out.println(l.toString());
         }
-
     }
 
     public static void output_winning(){
 
     }
 
-    public static void numbers_list_count(){
+    public static void numbers_list_count(){//numbers_list를 돌며 Rank_Is()를 실행시킨다.
+        init_map();
         for(int i = 0; i<numbers_list.size(); i++){
             int win_count = winning_count(i);
             int bonus_count = bonus_count(i);
             Winner_Rank rank = Rank_Is(win_count, bonus_count);
+            total_winner.put(rank, total_winner.getOrDefault(rank, 0) + 1);
         }
-
-    }
-    public static Winner_Rank TwoOrThree(int win_count, int bonus_count, Winner_Rank rank){
-        if(bonus_count ==1){
-            rank = Winner_Rank.TWO;
-            return rank;
-        }
-        rank = Winner_Rank.THREE;
-        return rank;
+        System.out.println(total_winner.toString());
     }
 
-    public static Winner_Rank Rank_Is(int win_count, int bonus_count){
+    public static Winner_Rank Rank_Is(int win_count, int bonus_count){//win_count 수를 바탕으로 TwoOrThree()를 실행시키거나 랭크값을 반환
         Winner_Rank rank  = Winner_Rank.ZERO;
         if(win_count == 5){
             rank =  TwoOrThree(win_count, bonus_count, rank);
@@ -50,7 +54,16 @@ public class Output {
         return rank;
     }
 
-    public static int winning_count(int i){
+    public static Winner_Rank TwoOrThree(int win_count, int bonus_count, Winner_Rank rank){//보너스 숫자 맞힘 여부로 2등과 3등을 판별하는 함수
+        if(bonus_count ==1){
+            rank = Winner_Rank.TWO;
+            return rank;
+        }
+        rank = Winner_Rank.THREE;
+        return rank;
+    }
+
+    public static int winning_count(int i){//winning값을 체크하는 함수
         List<Integer> numbers_lotto = numbers_list.get(i);
         int win_count = 0;
         for(int j = 0; j<numbers_lotto.size(); j++){
@@ -60,7 +73,7 @@ public class Output {
         }
         return win_count;
     }
-    public static int bonus_count(int i){
+    public static int bonus_count(int i){//bonus값을 체크하는 함수
         List<Integer> numbers_lotto = numbers_list.get(i);
         int bonus_count = 0;
         for(int j = 0; j<numbers_lotto.size(); j++){
--- src/main/java/lotto/Winner_Rank.java
+++ src/main/java/lotto/Winner_Rank.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Winner_Rank {
-    ONE("1등", 6, 2000000000 ),
-    TWO("2등",11, 30000000),
-    THREE("3등",12, 1500000),
-    FOUR("4등",4, 50000),
-    FIVE("5등",3, 5000),
+    ONE("6개 일치 (2,000,000,000원)", 6, 2000000000 ),
+    TWO("5개 일치, 보너스 볼 일치 (30,000,000원)",11, 30000000),
+    THREE("5개 일치 (1,500,000원)",12, 1500000),
+    FOUR("4개 일치 (50,000원)",4, 50000),
+    FIVE("3개 일치 (5,000원)",3, 5000),
     ZERO("꽝", 0, 0);
 
     private final String label;
@@ -54,6 +54,6 @@ public enum Winner_Rank {
     }
 
     public static Winner_Rank valueOfPrice(int price) {
-        return BY_NUMBER.get(price);
+        return BY_PRICE.get(price);
     }
 }

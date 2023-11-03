f931784a1d55c0625290d7e62e17a05c0e2eeff2
flown222 <jangu3384@gmail.com>
2023-11-03 14:04:11 +0900

    feat: 로또 번호 중복을 검증하는 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,14 +14,24 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateLengthCheck(numbers);
-            throw new IllegalArgumentException();
-        }
+        validateDuplicateNumber(numbers);
+    }
 
     private void validateLengthCheck(List<Integer> numbers) {
         if (numbers.size() != LOTTO_LENGTH) {
-            throw new IllegalArgumentException(String.format("로또 번호의 갯수 %d개를 만족해야합니다.",LOTTO_LENGTH));
+            throw new IllegalArgumentException(String.format("로또 번호의 갯수 %d개를 만족해야합니다.", LOTTO_LENGTH));
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        if (getDistinctCount(numbers) != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("숫자는 중복될 수 없습니다.");
+        }
+    }
+
+    private int getDistinctCount(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .distinct()
+                .count();
+    }
 }

836034ba3122ba35f6ba5dfc14ea6117230d8ad9
kongji <kongji4092@gmail.com>
2023-11-03 18:02:34 +0900

    feat: 구입 로또 개수 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,8 +4,13 @@ package lotto.view;
 public class OutputView {
 
     public static final String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASED_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
 
     public static void printInputPrice() {
         System.out.println(INPUT_PURCHASE_MESSAGE);
+    }
+
+    public static void printPurchaseLotto(int lottoCount) {
+        System.out.printf(PURCHASED_LOTTO_COUNT_MESSAGE, lottoCount);
     }
 }

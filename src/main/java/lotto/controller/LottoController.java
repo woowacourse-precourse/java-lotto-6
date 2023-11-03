940885833695e424597bb4bd97541195973ca07b
GiJung <weeds2577@gmail.com>
2023-11-03 19:01:52 +0900

    feat(OutputValue, LottoController): 구입 금액 입력 메세지 출력 기능 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,26 @@
+package lotto.controller;
+
+import lotto.view.OutputValue;
+
+public class LottoController {
+
+    public LottoController() {
+        lottoInit();
+        lottoProcess();
+        lottoEnd();
+    }
+
+    private void lottoInit() {
+
+        OutputValue.purchaseMessage();
+
+    }
+
+    private void lottoProcess() {
+
+    }
+
+    private void lottoEnd() {
+
+    }
+}
---
+++ src/main/java/lotto/view/OutputValue.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class OutputValue {
+
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void purchaseMessage() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+    }
+
+}

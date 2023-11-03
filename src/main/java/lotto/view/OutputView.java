09c71a59ca900f34e00f545f45ed5089e847333b
Jiyoung Choi <12201817@inha.edu>
2023-11-03 22:02:01 +0900

    feat(OutputView) : 사용자에게 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,43 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.model.Lotto;
+import lotto.model.WinningPrize;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputView {
+    private static final String PurchaseLottosCountPrintFormat = "%d개를 구매했습니다.\n";
+    private static final String WinningStatisticsPrintMessage = "당첨 통계\n---";
+    private static final String WinningResultPrintFormat = "%d개 일치 (%d원) - %d개\n";
+    private static final String RateOfReturnPrintFormat = "총 수익률은 %.2f%입니다.\n";
+
+    public void showPurchaseLottos(int purchaseLottosCount, List<Lotto> purchaseLottos) {
+        System.out.printf(PurchaseLottosCountPrintFormat, purchaseLottosCount);
+        for (Lotto purchaseLotto : purchaseLottos) {
+            System.out.println(purchaseLotto);
+        }
+    }
+
+    public void showWinningStatistics(Map<WinningPrize, Integer> winningResult, double rateOfReturn) {
+        showWinningStatisticsMessage();
+        showResult(winningResult);
+        showRateOfReturn(rateOfReturn);
+    }
+
+    private void showWinningStatisticsMessage() {
+        System.out.println(WinningStatisticsPrintMessage);
+    }
+
+    private void showResult(Map<WinningPrize, Integer> winningResult) {
+        for (Map.Entry<WinningPrize, Integer> entry : winningResult.entrySet()) {
+            WinningPrize winningPrize = entry.getKey();
+            int count = entry.getValue();
+            System.out.printf(WinningResultPrintFormat, winningPrize.getMatchCount(), winningPrize.getReward(), count);
+        }
+    }
+
+    private void showRateOfReturn(double rateOfReturn) {
+        System.out.printf(RateOfReturnPrintFormat, rateOfReturn);
+    }
 }

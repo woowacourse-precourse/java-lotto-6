5f1c887eb83abdc2d90ebdfd07f3ba44251e555c
Hoo-sung <gntjd135@gmail.com>
2023-11-03 23:56:48 +0900

    feat(OutputHandler): 발행한 로또 수량을 출력하는 printReceipts 메서드 생성

--- src/main/java/lotto/io/OutputHandler.java
+++ src/main/java/lotto/io/OutputHandler.java
@@ -12,4 +12,8 @@ public final class OutputHandler {
     public static void printLotto(List<Integer> lotto){
         System.out.println(lotto.toString());
     }
+
+    public static void printReceipts(int size){
+        System.out.println(size+"개를 구매했습니다.");
+    }
 }

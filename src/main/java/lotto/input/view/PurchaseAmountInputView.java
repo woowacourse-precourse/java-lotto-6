c405835ef118e484f2f774b3f430ad078485470b
leedonghyun <skydh507@ajou.ac.kr>
2023-11-04 01:50:24 +0900

    refactor : 입력에 필요한 문자열 Constant 클래스로 추출

--- src/main/java/lotto/input/view/PurchaseAmountInputView.java
+++ src/main/java/lotto/input/view/PurchaseAmountInputView.java
@@ -1,5 +1,8 @@
 package lotto.input.view;
 
+import static lotto.input.constant.Constant.PURCHASEAMOUNTINPUT;
+import static lotto.input.constant.Constant.PURCHASEAMOUNTINPUT_ERROR;
+
 import camp.nextstep.edu.missionutils.Console;
 import lotto.input.Validator.InputValidate;
 
@@ -7,7 +10,7 @@ public class PurchaseAmountInputView {
     private final InputValidate inputValidate = new InputValidate();
 
     public Long userAmountInput() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PURCHASEAMOUNTINPUT);
         return validateCheck();
     }
 
@@ -19,7 +22,7 @@ public class PurchaseAmountInputView {
                 amount = inputValidate.valueValidate(value);
                 break;
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 올바르지 않는 값을 입력했습니다. 1000의 배수의 가격을 입력해주세요.");
+                System.out.println(PURCHASEAMOUNTINPUT_ERROR);
             }
         }
         return amount;

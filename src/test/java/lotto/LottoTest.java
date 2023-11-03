1b66db77b9ef2877502e3fb1318641286738ba37
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-11-03 21:56:09 +0900

    test: Lotto 중복, 사이즈 예외 테스트 및 비교 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -18,10 +20,36 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 보너스 숫자가 포함되어 있는지 확인한다.")
+    @Test
+    void containsMatch() {
+        int containsBonus = 5;
+        int notContainsBonus = 7;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        boolean contains = lotto.containsNumber(containsBonus);
+        boolean notContains = lotto.containsNumber(notContainsBonus);
+
+        assertThat(contains).isTrue();
+        assertThat(notContains).isFalse();
+    }
+
+
+    @DisplayName("로또끼리 숫자를 비교해 몇 개의 숫자가 맞는지 반환한다.")
+    @Test
+    void countingMatchNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 9, 10, 11));
+        Lotto lotto2 = new Lotto(List.of(12, 13, 14, 15, 16, 17));
+
+        long count1 = lotto.countingMatchNumbers(lotto1);
+        long count2 = lotto.countingMatchNumbers(lotto2);
+
+        assertThat(count1).isEqualTo(3L);
+        assertThat(count2).isEqualTo(0L);
+    }
 }

81709be546cb2a10e1a68807372431c86d744391
02ggang9 <02ggang9@gmail.com>
2023-11-03 17:06:11 +0900

    feat(LottoResult): 로또 결과를 출력하는 책임 구현

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -11,6 +11,7 @@ public class LottoResult {
     private Integer matchFive;
     private Integer matchFiveAndBonus;
     private Integer matchSix;
+    private Float rateOfReturn;
 
     public LottoResult() {
         this.matchesThree = 0;
@@ -18,6 +19,7 @@ public class LottoResult {
         this.matchFive = 0;
         this.matchFiveAndBonus = 0;
         this.matchSix = 0;
+        this.rateOfReturn = 0.0F;
     }
 
     public LottoResult showLottoResult(List<String> winningNumbers, Integer bonus, List<Lotto> lottos) {
@@ -25,7 +27,18 @@ public class LottoResult {
             List<Integer> result = comparator.compareLotto(winningNumbers, bonus, lotto);
             saveResult(result);
         }
+        calculateRateOfReturn(lottos.size());
         return this;
+    }
+
+    private void calculateRateOfReturn(int size) {
+        int userInputMoney = size * 1000;
+        this.rateOfReturn += (matchesThree * 5000);
+        this.rateOfReturn += (matchesFour * 50000);
+        this.rateOfReturn += (matchFive * 1500000);
+        this.rateOfReturn += (matchFiveAndBonus * 30000000);
+        this.rateOfReturn += (matchSix * 2000000000);
+        this.rateOfReturn = this.rateOfReturn / userInputMoney;
     }
 
     private void saveResult(List<Integer> result) {
@@ -44,4 +57,18 @@ public class LottoResult {
             matchSix++;
         }
     }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "3개 일치 (5,000원) - %d개\n" +
+                        "4개 일치 (50,000원) - %d개\n" +
+                        "5개 일치 (1,500,000원) - %d개\n" +
+                        "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" +
+                        "6개 일치 (2,000,000,000원) - %d개\n" +
+                        "총 수익률은 %.1f%%입니다.",
+                this.matchesThree, this.matchesFour, this.matchFive, this.matchFiveAndBonus, this.matchSix, this.rateOfReturn
+        );
+    }
+
 }

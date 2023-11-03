01cb4f8a51c0aac4a8eae6655f626299e97f081f
Hojoomoney <kimhojoo51@gmail.com>
2023-11-03 17:35:11 +0900

    Feat : 입력한 당첨번호가 중복되는 숫자가 있는지 확인하는 메소드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,17 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+
+	public Lotto(List<Integer> numbers) {
         validate(numbers);
         checkBoundOf(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -23,8 +27,22 @@ public class Lotto {
     private void checkBoundOf(List<Integer> numbers) {
     	for (int number : numbers) {
     		if (number < 1 || number > 45) {
-    			throw new IllegalStateException();
+    			throw new IllegalStateException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     		}
     	}
     }
+    
+    // 중복되는 숫자가 있는지 확인하는 메소드
+    private void checkDuplicate(List<Integer> numbers) {
+    	Set<Integer> newNumbers = new HashSet<>(numbers);
+    	if(newNumbers.size() != numbers.size()) {
+    		throw new IllegalStateException("[ERROR] 중복되지 않는 서로 다른 숫자 6개를 입력해주세요.");
+    	}
+    }
+    
+    
+    public List<Integer> getNumbers() {
+    	return numbers;
+    }
+    
 }

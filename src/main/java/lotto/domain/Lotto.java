02cf4415e9dc7d3511dd4cefe912598ab1693272
sangchu <didi1484@gmail.com>
2023-11-03 23:44:25 +0900

    fix: validate 메서드 수정 (예외 메시지 내용 추가)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.domain.constants.ExceptionMessage.IS_INSUFFICIENT;
+
 import java.util.List;
 
 public class Lotto {
@@ -12,7 +14,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(IS_INSUFFICIENT.getMessage());
         }
     }
 
--- src/main/java/lotto/domain/constants/ExceptionMessage.java
+++ src/main/java/lotto/domain/constants/ExceptionMessage.java
@@ -3,7 +3,8 @@ package lotto.domain.constants;
 public enum ExceptionMessage {
     BE_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     HAS_LETTER("[ERROR] 로또 번호는 문자를 포함할 수 없습니다."),
-    IS_DUPLICATED("[ERROR] 로또 번호는 중복된 숫자를 포함할 수 없습니다.");
+    IS_DUPLICATED("[ERROR] 로또 번호는 중복된 숫자를 포함할 수 없습니다."),
+    IS_INSUFFICIENT("[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다.");
     private final String message;
 
     ExceptionMessage(String message) {

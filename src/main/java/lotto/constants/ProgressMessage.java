73664f535e07928100d266314536fe6fdeff82e1
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 01:18:09 +0900

    feat(ProgressMessage): 로또 구입 금액 메시지 상수

---
+++ src/main/java/lotto/constants/ProgressMessage.java
@@ -0,0 +1,8 @@
+package lotto.constants;
+
+public class ProgressMessage {
+    private ProgressMessage() {
+    }
+
+    public static final String INPUT_LOTTO_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+}

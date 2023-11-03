a3bb08c5a1b6a108e103e3e5e575273e90b150d2
hwangkyojune <shane5969@naver.com>
2023-11-03 23:54:39 +0900

    로또 에러 메세지에 대한 enum 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,6 +6,18 @@ public class Lotto {
     static int MIN_NUMBER = 1;
     static int MAX_NUMBER = 45;
     static int NUMBER_SIZE = 6;
+    enum LottoError {
+        BOUND_LENGTH("[ERROR] 로또 번호는 6개가 필요합니다."),
+        PROPER_NUMS("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+        DUPLICATED_NUMS("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        String errorMessage;
+        LottoError(String errorMessage){
+            this.errorMessage = errorMessage;
+        }
+        public String getErrorMessage(){
+            return errorMessage;
+        }
+    }
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -21,7 +33,8 @@ public class Lotto {
 
     public void validateAuthorizedLength(List<Integer> numbers){
         if(numbers.size()!=6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(
+                    LottoError.BOUND_LENGTH.getErrorMessage());
         }
     }
 
@@ -29,7 +42,9 @@ public class Lotto {
         for(int i=0 ;i<numbers.size();i++){
             int lottoNum = numbers.get(i);
             if(lottoNum>MIN_NUMBER&&lottoNum<MAX_NUMBER){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(
+                        LottoError.PROPER_NUMS.getErrorMessage()
+                );
             }
         }
     }
@@ -37,7 +52,9 @@ public class Lotto {
     public void validateDuplicatedNums(List<Integer> numbers){
         int distinctNumbersCount = (int)numbers.stream().distinct().count();
         if(distinctNumbersCount != NUMBER_SIZE){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(
+                    LottoError.DUPLICATED_NUMS.getErrorMessage()
+            );
         }
     }
 }

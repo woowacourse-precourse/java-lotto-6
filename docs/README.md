a11aec6a3815a9ded7dc1c882e1e13a9e56a8b42
hyerimmy <heyrim2010@naver.com>
2023-11-03 17:19:07 +0900

    feat: MVC 패턴 기반 프로젝트 설계

---
+++ docs/README.md
@@ -0,0 +1,13 @@
+# 우테코 프리코스 3주차
+
+## 🎟️ 로또
+```text
+├─ controller
+│  └─ GameController
+├─ model
+│  └─ Car
+├─ view
+│  ├─ ResultView [게임 실행 결과 출력]
+│  └─ SettingView [게임 설정(자동차 이름, 회수) 문구 출력]
+└─ Application
+```
---
+++ src/main/java/lotto/Controller/Controller.java
@@ -0,0 +1,26 @@
+package lotto.Controller;
+
+import lotto.Model.Buyer;
+
+public class Controller {
+    private Buyer buyer;
+    public void getInputPrice(){
+        //TODO: 구매금액 입력
+    }
+
+    public void purchaseLotto(){
+        //TODO: 로또 구매 내용 출력
+    }
+
+    public void getInputWinningNumbers(){
+        //TODO: 당첨번호 입력
+    }
+
+    public void getInputBonusNumber(){
+        //TODO: 보너스번호 입력
+    }
+
+    public void showWinningResult(){
+        //TODO: 당첨 통계 출력
+    }
+}

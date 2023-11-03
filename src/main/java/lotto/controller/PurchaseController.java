f706b4097945b24b1894bae473b180a8f9578efa
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2023-11-04 00:34:17 +0900

    feat: 금액에 따라 생성할  로또 티켓의 개수 계산 구현

--- src/main/java/lotto/controller/PurchaseController.java
+++ src/main/java/lotto/controller/PurchaseController.java
@@ -1,11 +1,13 @@
 package lotto.controller;
 import lotto.view.LottoPurchaseInput;
-
+import lotto.model.LottoTicketCalculator;
 public class PurchaseController {
 
     private final LottoPurchaseInput inputView = new LottoPurchaseInput();
+    private final LottoTicketCalculator calculation = new LottoTicketCalculator();
 
     public int getPurchaseAmount() {
-        return inputView.requestPurchaseAmount();
+        int amount = inputView.requestPurchaseAmount();
+        return calculation.calculateNumberOfTickets(amount);
     }
 }
---
+++ src/main/java/lotto/model/LottoTicketCalculator.java
@@ -0,0 +1,7 @@
+package lotto.model;
+
+public class LottoTicketCalculator {
+    public int calculateNumberOfTickets(int purchaseAmount) {
+        return purchaseAmount /  1000;
+    }
+}

da74f411b205042d2064f8bf9c656bf37b012bc8
song-jae-wan <sjo9810@gmail.com>
2023-11-03 23:14:35 +0900

    feat: WinningLotto 클래스 싱글톤으로 설계
    - 멤버변수로 Lotto, 보너스 번호 선언

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,4 +1,34 @@
 package domain;
 
-public class WinningLotto {
+public class WinningLotto { // 싱글톤 패턴으로 관리
+
+    private static WinningLotto defaultWinningLotto;
+    private Lotto numbers;
+    private Integer bonusNumber;
+
+    private WinningLotto() {
+    }
+
+    public static WinningLotto getInstance() {
+        if(defaultWinningLotto == null) {
+            defaultWinningLotto = new WinningLotto();
+        }
+        return defaultWinningLotto;
+    }
+
+    public void setNumbers(Lotto lotto) {
+        numbers = lotto;
+    }
+
+    public void setBonusNumber(Integer bonusNumber) {
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Integer bonusNumber) {
+        if(numbers.containsNumber(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복 될 수 없습니다.");
+        }
+    }
+
 }

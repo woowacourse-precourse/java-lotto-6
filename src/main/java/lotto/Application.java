524de7cb68e913879f08e9a875e1b9cfbe9f9980
HYUNJUN\sajun <sajun28@naver.com>
2023-11-03 18:24:09 +0900

    Feat: "구입 금액 입력 기능 추가"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.view.InputView;
+
+import java.util.*;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }

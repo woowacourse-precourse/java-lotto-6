288d045ed98db2af28ea7e92829c0961845293ac
jdohun <wjdehgns97@naver.com>
2023-11-03 18:33:43 +0900

    Feat: Output Message Enum 생성

---
+++ src/main/java/lotto/enums/OutputMessages.java
@@ -0,0 +1,21 @@
+package lotto.enums;
+
+public enum OutputMessages {
+    OUTPUT_COUNT_OF_LOTTO("개를 구매했습니다."),
+    WINNING_STATISTICS("당첨 통계\n---\n"),
+    MATCHES_3_FORMAT("3개 일치 (5,000원) - %d개"),
+    MATCHES_4_FORMAT("4개 일치 (50,000원) - %d개"),
+    MATCHES_5_FORMAT("5개 일치 (1,500,000원) - %d개"),
+    MATCHES_5_ADDITION_BONUS_NUMBER_FORMAT("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    MATCHES_6_FORMAT("6개 일치 (2,000,000,000원) - %d개");
+
+    private final String message;
+
+    OutputMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}

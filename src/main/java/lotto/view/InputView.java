51e80c8b4fb659c6665ff9c79b453ca75a90a4ff
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-04 01:55:35 +0900

    feature(InputView): InputView의 금액 입력 메서드 InputMoney() 구현 - 앞서 구현했던 InputValidator 객체를 이용하여 다양한 유효성 검사를 수행함. - 숫자형인지 유효성 검사 - Integer 형태로 입력을 변경한 후 최소 금액 유효성 검사 - 1000원 단위 유효성 검사 - 예외 발생 시 에러 메세지 출력하도록 함.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,27 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.InputValidator;
+
 public class InputView {
+    private final InputValidator inputValidator;
+
+    public InputView(){
+        inputValidator = new InputValidator();
+    }
+
+    public void InputMoney(){
+        String input = Console.readLine();
+        try{
+            inputValidator.isNumericValidator(input);
+            Integer money = Integer.parseInt(input);
+            inputValidator.isMinimumValidator(money);
+            inputValidator.isThousandUnitValidator(money);
+            // 여기에 static 변수 사용하면 될듯
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public void InputSixNumbers(){}
+    public void InputBonusNumber(){}
 }

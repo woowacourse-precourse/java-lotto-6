9227fef71c63e88f771b5f31bf3a34ec67d77db3
brightfanta <brightfanta@gmail.com>
2023-11-03 19:23:30 +0900

    feat(GameData): 로또 개수, 당첨 번호, 보너스 번호 getter & setter 생성, 당첨통계에 해당하는 변수 이동(->GameStatistics)

--- src/main/java/lotto/GameData.java
+++ src/main/java/lotto/GameData.java
@@ -3,13 +3,32 @@ package lotto;
 import java.util.List;
 
 public class GameData {
-
     private Integer budget;
-    private List<Integer> winingNumbers;
-    private Integer bonusNumber;
     private Integer LotteriesNumber;
-    private Integer winningPrizeByRank;
-    private Double budgetPrizeRatio;
-    private Integer totalPrize;
+    private List<Integer> winningNumbers;
+    private Integer bonusNumber;
 
+    public Integer getLotteriesNumber() {
+        return LotteriesNumber;
+    }
+
+    public void setLotteriesNumber(Integer lotteriesNumber) {
+        LotteriesNumber = lotteriesNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public void setWinningNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(Integer bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
 }

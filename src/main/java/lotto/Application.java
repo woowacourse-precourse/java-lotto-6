1292ac63904b2d741c57839284c1cc91bb6e155a
이소정 <grace@isojeong-ui-iMac.local>
2023-11-03 18:05:24 +0900

    구매금액 입력 시 예외처리 구현 -> 클래스로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,22 +8,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
-        int price = inputPrice(0);
+        int price = Price.inputPrice(0);
     }
 
-    public static int inputPrice(int price) {
-        while (true) {
-            while (true) {
-                try {
-                    price = Integer.parseInt(Console.readLine());
-                    break;
-                } catch (IllegalArgumentException e) {
-                    System.out.println("[ERROR] 숫자로 입력해 주세요.");
-                }
-            }
-            if (price % 1000 == 0) return price;
-            System.out.println("[ERROR] 1000단위로 입력해 주세요.");
-        }
-    }
+
 
 }
--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -6,6 +6,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Price {
 
-
-
+    public static int inputPrice(int price) {
+        while (true) {
+            while (true) {
+                try {
+                    price = Integer.parseInt(Console.readLine());
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println("[ERROR] 숫자로 입력해 주세요.");
+                }
+            }
+            if (price % 1000 == 0) return price;
+            System.out.println("[ERROR] 1000단위로 입력해 주세요.");
+        }
+    }
 }

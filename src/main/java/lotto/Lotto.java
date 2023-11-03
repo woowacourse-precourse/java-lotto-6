8ac7903e2cfbbffeb5cb68d95da188909cd5cf9e
hlmg <devhlmg@gmail.com>
2023-11-04 05:32:50 +0900

    refactor: 메서드 분리, ParameterizedTest 적용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,10 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,25 +16,35 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private void validateDuplicate(List<Integer> numbers) {
         HashSet<Integer> distinctNumbers = new HashSet<>();
         for (Integer number : numbers) {
-            if (distinctNumbers.add(number)) {
-                continue;
+            if (!distinctNumbers.add(number)) {
+                throw new IllegalArgumentException();
             }
-            throw new IllegalArgumentException();
         }
+    }
 
-        if (numbers.stream().anyMatch(number -> number < 1)) {
+    private void validateNumberRange(List<Integer> numbers) {
+        if (hasOutOfRangeNumber(numbers)) {
             throw new IllegalArgumentException();
         }
+    }
 
-        if (numbers.stream().anyMatch(number -> number > 45)) {
-            throw new IllegalArgumentException();
-        }
+    private boolean hasOutOfRangeNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
     }
 }
 /*
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,14 +1,18 @@
 package lotto;
 
-import java.lang.reflect.Field;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -23,30 +27,38 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 1보다 작은 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByUnderMinNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+    private static Stream<List<Integer>> createLottoByUnderMinNumber() {
+        return Stream.of(
+                List.of(0, 1, 2, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5, 46)
+        );
+    }
+
+    @DisplayName("로또 번호에 1보다 작거나 45보다 큰 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource
+    void createLottoByUnderMinNumber(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 45보다 큰 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByOverMaxNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
+    private static Stream<List<Integer>> createLotto() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(40, 41, 42, 43, 44, 45)
+        );
     }
 
     @DisplayName("로또 번호를 생성할 수 있다.")
-    @Test
-    void createLotto() throws Exception {
+    @ParameterizedTest
+    @MethodSource
+    void createLotto(List<Integer> numbers) throws Exception {
         Field numbersField = Lotto.class.getDeclaredField("numbers");
         numbersField.setAccessible(true);
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
 
         Lotto lotto = new Lotto(numbers);
 
         Object actual = numbersField.get(lotto);
-        assertThat(actual).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(actual).isEqualTo(List.copyOf(numbers));
     }
 }

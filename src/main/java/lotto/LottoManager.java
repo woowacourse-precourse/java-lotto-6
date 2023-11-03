721d651312161350326f7ad9ea1320dabb2f0e56
AmenPark <puzzlistpam@gmail.com>
2023-11-03 18:29:33 +0900

    feat: 로또구매내역 출력

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -13,6 +13,7 @@ public class LottoManager {
     public void run() {
         ConsoleView.print("구입금액을 입력해 주세요.");
         getMoney();
+        showLottos();
 
     }
 
@@ -20,10 +21,18 @@ public class LottoManager {
         long money = ConsoleView.getLongInput();
         try {
             lottos = LottoPublisher.getInstance().sell(money);
+            ConsoleView.print(String.format("%d개를 구매했습니다.", lottos.size()));
+
         } catch (IllegalArgumentException e) {
             ConsoleView.print("[ERROR] 1000원 단위 양수로 입력해야 합니다.");
             getMoney();
         }
     }
 
+    public void showLottos(){
+        for(Lotto l: lottos){
+            ConsoleView.print(l.toString());
+        }
+        ConsoleView.print("");
+    }
 }

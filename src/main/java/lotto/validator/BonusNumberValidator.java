e2f22d73553241694f4d0ffa25cdabf1152ff94f
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-03 17:22:10 +0900

    feat: 입력받은 보너스번호가 로또번호 범위 밖에 있으면 예외처리하는 기능

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -3,8 +3,12 @@ package lotto.validator;
 import lotto.message.ExceptionMessage;
 
 public class BonusNumberValidator {
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+
     public static void validate(String inputNumber) {
         validateNaturalNumber(inputNumber);
+        validateNumberInRange(inputNumber);
     }
 
     private static void validateNaturalNumber(String inputNumber) {
@@ -12,6 +16,13 @@ public class BonusNumberValidator {
             Integer.parseInt(inputNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_NATURAL_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateNumberInRange(String inputNumber) {
+        int number = Integer.parseInt(inputNumber);
+        if (number < MIN_LOTTO_NUM || number > MAX_LOTTO_NUM) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBER_NOT_IN_RANGE.getMessage());
         }
     }
 

9301a6c45bc38bd40f7465e93e31ab29c242ae43
ldw <leehan02@hanyang.ac.kr>
2023-11-03 23:00:52 +0900

    feat(lotto): 사용자로부터 6자리 로또 번호를 입력받는 기능 구현

--- src/main/java/lotto/Controller/InputController.java
+++ src/main/java/lotto/Controller/InputController.java
@@ -3,10 +3,16 @@ package lotto.Controller;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputController {
-    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ENTER_SIX_DIGITS_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public String purchaseAmount() {
-        System.out.println(INPUT_PURCHASE_AMOUNT);
+        System.out.println(ENTER_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public String numbers() {
+        System.out.println(ENTER_SIX_DIGITS_NUMBERS);
         return Console.readLine();
     }
 }

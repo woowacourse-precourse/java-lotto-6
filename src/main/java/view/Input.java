d2365f5dc230f54501fb4c94359ad1d74ff79557
jmj005 <jmj00005@naver.com>
2023-11-03 23:24:00 +0900

    feat: 로또 구입 금액 문자 검증 구현

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -11,12 +11,19 @@ public class Input {
         System.out.println(INPUT_MONEY_GUIDE);
         String money = Console.readLine();
         validateMoneyBlank(money);
+        validateMoneyNumeric(money);
         return Integer.parseInt(money);
     }
 
     private static void validateMoneyBlank(String money) {
         if (money.isBlank()) {
             throw new IllegalArgumentException("공백은 입력할 수 없습니다.");
+        }
+    }
+
+    private static void validateMoneyNumeric(String money) {
+        if (money.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
         }
     }
 }

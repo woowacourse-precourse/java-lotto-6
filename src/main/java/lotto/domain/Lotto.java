334be4075aacc24cd01555a62f3afb37d18bc8d4
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-03 11:27:49 +0900

    feat (Lotto) : 중복되지 않은 숫자로 구성되었는지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -21,15 +21,30 @@ public class Lotto {
 
     private void validate(List<Number> numbers) {
         validateSize(numbers);
+        validateDistinct(numbers);
     }
 
     private void validateSize(List<Number> numbers) {
         if (isDifferentFormat(numbers.size())) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또는 숫자 %d개로 구성되어야 합니다".formatted(NUMBER_SIZE));
         }
     }
 
     private boolean isDifferentFormat(int numberSize) {
         return numberSize != NUMBER_SIZE;
+    }
+
+    private void validateDistinct(List<Number> numbers) {
+        if (isOverlapped(numbers)) {
+            throw new IllegalArgumentException("로또의 각 숫자는 중복되지 않아야 합니다");
+        }
+    }
+
+    private boolean isOverlapped(List<Number> numbers) {
+        return numbers.size() > countDistinctNumber(numbers);
+    }
+
+    private int countDistinctNumber(List<Number> numbers) {
+        return (int) numbers.stream().distinct().count();
     }
 }

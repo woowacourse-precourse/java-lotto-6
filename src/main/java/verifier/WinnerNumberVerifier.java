1e4c6cd4330fd0d2bcd2fe570a06bd20123d314a
Hoo-sung <gntjd135@gmail.com>
2023-11-03 16:09:52 +0900

    feat(WinnerNumberVerifier): 당첨 번호가 6개 입력되었는지 검증하는 메서드 생성

--- src/main/java/verifier/WinnerNumberVerifier.java
+++ src/main/java/verifier/WinnerNumberVerifier.java
@@ -3,6 +3,7 @@ package verifier;
 public class WinnerNumberVerifier implements Verifier{
     @Override
     public void check(String input) {
+        checkNumberCount(input);
         checkEachNumeric(input);
     }
 
@@ -14,6 +15,12 @@ public class WinnerNumberVerifier implements Verifier{
             }catch(Exception e){
                 throw new IllegalArgumentException("입력된 값은 숫자가 아닙니다.");
             }
+        }
+    }
+    private void checkNumberCount(String input){
+        String[] numbers = input.split(",");
+        if(numbers.length != 6){
+            throw new IllegalArgumentException("당첨번호는 6개의 숫자가 아닙니다.");
         }
     }
 }

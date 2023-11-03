aafd97145d06206b45d93d22361a452243cec821
alscjf1329 <alscjf1329@gmail.com>
2023-11-03 23:38:47 +0900

    feat(ErrorMessage): 에러 메세지 추가
    6자리, 로또 번호 중복에 대한 에러 메세지를 추가했습니다.

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -1,7 +1,9 @@
 package lotto.message;
 
 public enum ErrorMessage {
-    LOTTO_NUMBER_IS_NOT_IN_RANGE("[ERROR] 로또 번호의 숫자 범위는 1~45까지이다.");
+    LOTTO_NUMBER_IS_NOT_IN_RANGE("[ERROR] 로또 번호의 숫자 범위는 1~45까지이다."),
+    LOTTO_IS_NOT_6DIGITS("[ERROR] 로또는 6자리이다."),
+    LOTTO_NUMBER_IS_DUPLICATED("[ERROR] 로또는 중복되지 않는 숫자로 이루어져 있습니다.");
     private final String message;
 
     ErrorMessage(String message) {

e6731490b64c71cf2ff6c01627db243675eb99ee
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-03 15:55:32 +0900

    feat: 로또번호 입력값을 리스트로 변환하는 기능

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -1,9 +1,18 @@
 package lotto.util;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoUtil {
     public static void sortByAsc(List<Integer> numbers) {
         numbers.sort(null);
+    }
+
+    public static List<Integer> convert(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

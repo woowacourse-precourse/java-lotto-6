cd070ab464549d09907b3cef8401207edc89e803
Chenowoo <cjsgusdn14@naver.com>
2023-11-04 00:32:14 +0900

    feat(InputValidation): 입력받은 로또 구입 금액이 숫자로만 입력됐는지 확인하는 기능 구현 입력받은 로또 구입 금액이 숫자가 아닌 다른 문자가 섞였다면 에러 발생

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -2,6 +2,7 @@ package lotto.validation;
 
 public class InputValidation {
     public int validatePurchaseAmount(String input) {
+        validateInputIsNumeric(input);
         int purchaseAmount = Integer.parseInt(input);
         validatePurchaseAmountIsPositive(purchaseAmount);
         validatePurchaseAmountUnit(purchaseAmount);
@@ -18,6 +19,12 @@ public class InputValidation {
     public void validatePurchaseAmountIsPositive(int purchaseAmount) {
         if (purchaseAmount < 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 양수로 입력해야 합니다.");
+        }
+    }
+
+    public void validateInputIsNumeric(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new NumberFormatException("[ERROR] 숫자만 입력해야 합니다.");
         }
     }
 }

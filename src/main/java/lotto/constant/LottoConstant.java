bc7bd7b04c3fb726361655bd3b9dd2a4f455853c
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-04 03:25:42 +0900

    feat: LottoConstant 작성

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,11 @@
+package lotto.constant;
+
+public class LottoConstant {
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_PRICE = 1000;
+
+    private LottoConstant() {
+    }
+}

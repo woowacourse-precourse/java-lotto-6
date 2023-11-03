664ea9ed8f6d38e4c65b6d8ff654470de66abf64
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-04 03:04:38 +0900

    feat: 로또 번호 생성 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,6 +16,23 @@ public class Application {
         }
         System.out.println();
 
+        int LottoTicket = purchase / 1000;
+        System.out.println(LottoTicket + "개를 구매했습니다.");
+
+        List<List<Integer>> allLotto = new ArrayList<>();
+        for (int i = 0; i < LottoTicket; i++) {
+            List<Integer> oneLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(oneLotto);
+            System.out.print("[");
+            for (int j = 0; j < oneLotto.size(); j++) {
+                if (j == oneLotto.size() - 1) {
+                    System.out.println(oneLotto.get(j) + "]");
+                    break;
+                }
+                System.out.print(oneLotto.get(j) + ", ");
+            }
+            allLotto.add(oneLotto);
+        }
 
     }
 }

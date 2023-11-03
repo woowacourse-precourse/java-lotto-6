2f3d2e7305ca4cf20ca8113e23ee11fa03aa61b6
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-03 23:37:18 +0900

    feat: 로또 관련 로직을 처리하는 LottoManager 클래스 구현

---
+++ src/main/java/lotto/LottoManager.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoManager {
+    public Lottos createLottos(int money) {
+
+        int lottoCount = Lotto.payableCount(money);
+
+        return Lottos.createByCount(
+                lottoCount,
+                () -> Randoms.pickUniqueNumbersInRange(Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER, Lotto.LOTTO_NUMBER_SIZE)
+        );
+    }
+
+    public WinningLotto createWinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    public void calculateLottos(Lottos lottos, WinningLotto winningLotto) {
+        List<LottoRanking> rankings = lottos.calculateRanking(winningLotto);
+    }
+}

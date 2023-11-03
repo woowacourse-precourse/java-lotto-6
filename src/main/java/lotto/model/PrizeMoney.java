00f71aed2f1ba8f7991b2b7feeb511d115fb2105
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-04 01:17:22 +0900

    :sparkles: [Feat] 상금 저장 객체

---
+++ src/main/java/lotto/model/PrizeMoney.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrizeMoney {
+    private final Map<Integer, Integer> prizeMoney;
+
+    public PrizeMoney() {
+        this.prizeMoney = new HashMap<>();
+        this.prizeMoney.put(3, 5000);
+        this.prizeMoney.put(4, 50000);
+        this.prizeMoney.put(5, 1500000);
+        this.prizeMoney.put(6, 2000000000);
+    }
+
+    public int getPrizeMoney(int matchCount) {
+        return this.prizeMoney.getOrDefault(matchCount, 0);
+    }
+
+}

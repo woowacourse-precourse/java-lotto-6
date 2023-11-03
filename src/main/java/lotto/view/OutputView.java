a646b963b209ad4b330f81c27aef14349bbe6ae4
DaeYoungK <hanguk830@naver.com>
2023-11-03 22:38:59 +0900

    feat(OutputView): 컴퓨터가 정보를 출력하는 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+public class OutputView {
+
+    private final String PURCHASE = "개를 구매했습니다.";
+    private final String WINNING_STATISTICS = "당첨 통계\n---\n";
+    private final String RATE_OF_RETURN = "총 수익률은 %f% 입니다.";
+
+    public void printPurchase(int count) {
+        System.out.println(count + PURCHASE);
+    }
+
+    public void printWinningStatistics(String lottoResult) {
+        System.out.println(WINNING_STATISTICS + lottoResult);
+    }
+
+    public void printRateOfReturn(float rate) {
+        System.out.println(String.format(RATE_OF_RETURN,rate));
+    }
+}

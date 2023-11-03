a07e7b91931c917c8878f3664b976042095863e8
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-03 16:13:10 +0900

    Feat: 중복된 숫자가 있는지 검사하는 validateDuplication 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,34 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
+
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+
+        validateSize(numbers);
+        validateDuplication(numbers);
+
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+
+        Set<Integer> testNumbers = Set.copyOf(numbers);
+
+        if(testNumbers.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

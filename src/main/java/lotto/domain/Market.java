58e5bdb80dd6e0d3003b216baeb09823b14c2d08
minseo  jo <liging12@naver.com>
2023-11-04 02:05:04 +0900

    refactor: 로또 판매점 리팩토링(Market)
    로또 판매점에는 로또를 수동, 자동으로 구매 할 수 있다.
    아직 수동은 만들지 않음, 마켓에 있는 상수를 지우고 전국 어디서든 로또의 가격은 같으므로 로또 관련 상수를 LottoConstants 하나로 관리한다.

--- src/main/java/lotto/domain/Market.java
+++ src/main/java/lotto/domain/Market.java
@@ -2,46 +2,47 @@ package lotto.domain;
 
 import lotto.exception.MarketException;
 
+import static lotto.contant.LottoConstants.LOTTO_PRICE;
+import static lotto.contant.LottoConstants.MAX_LOTTO_PRICE_PER_USER;
+
 public class Market {
-    private static final int LOTTO_PRICE = 1000;
-    private static final int MAX_LOTTO_PRICE_PER_USER = 100_000;
-
     private final LottoAutoGenerator lottoAutoGenerator;
+    private final LottoManualGenerator lottoManualGenerator;
 
-    public Market(LottoAutoGenerator lottoAutoGenerator) {
+    public Market(LottoAutoGenerator lottoAutoGenerator, LottoManualGenerator lottoManualGenerator) {
         this.lottoAutoGenerator = lottoAutoGenerator;
+        this.lottoManualGenerator = lottoManualGenerator;
     }
 
-    public Lottos buyLottos(int userMoney) {
+    public Lottos purchaseAutoLottos(int userMoney) {
         validate(userMoney);
-        int lottoCount = userMoney / LOTTO_PRICE;
-
+        int lottoCount = userMoney / LOTTO_PRICE.getValue();
         return lottoAutoGenerator.generate(lottoCount);
     }
 
     private void validate(int money) throws MarketException {
         if (!isValidMoney(money)) {
-            throw new MarketException("로또 금액은 " + LOTTO_PRICE + "원 입니다.");
+            throw new MarketException(MarketException.ErrorMessage.VALID_MONEY.getMessage());
         }
 
         if (!isDivisibleByLottoPrice(money)) {
-            throw new MarketException("거스름돈이 존재하지 않습니다. 로또 한개 금액은 " + LOTTO_PRICE + " 원 입니다.");
+            throw new MarketException(MarketException.ErrorMessage.NOT_DIVISIBLE.getMessage());
         }
 
-        if (isExceedMaxLottoCountPerUser(money)) {
-            throw new MarketException("한 사람당 구매 가능한 로또 금액은 최대 " + MAX_LOTTO_PRICE_PER_USER + "원 입니다.");
+        if (isExceedMaxLottoAmountPerUser(money)) {
+            throw new MarketException(MarketException.ErrorMessage.EXCEED_MAX_LOTTO_AMOUNT.getMessage());
         }
     }
 
     private boolean isValidMoney(int money) {
-        return money >= LOTTO_PRICE;
+        return money >= LOTTO_PRICE.getValue();
     }
 
     private boolean isDivisibleByLottoPrice(int money) {
-        return money % LOTTO_PRICE == 0;
+        return (money % LOTTO_PRICE.getValue()) == 0;
     }
 
-    private boolean isExceedMaxLottoCountPerUser(int money) {
-        return money > MAX_LOTTO_PRICE_PER_USER;
+    private boolean isExceedMaxLottoAmountPerUser(int money) {
+        return money > MAX_LOTTO_PRICE_PER_USER.getValue();
     }
 }

30c8b700f7623c7b26b772f8538262e34853f5d6
Seokhyun Lee <lseokhyun99@gmail.com>
2023-11-03 19:09:24 +0900

    feat(InputView) : 로또 구입 금액, 당첨 번호, 보너스 번호 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,26 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+
+    public static String inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+}

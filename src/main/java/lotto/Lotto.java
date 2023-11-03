f7778df0040d4fce7d937b65fb66ecf9b1e57d5e
Mingeun Park <mindal99546@gmail.com>
2023-11-03 22:17:08 +0900

    feat: 당첨번호 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,16 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int LOTTO_NUMBER_MIN_INCLUDE = 1;
+    private static final int LOTTO_NUMBER_MAX_INCLUDE = 45;
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException {
         validate(numbers);
         this.numbers = numbers;
     }
@@ -23,14 +28,33 @@ public class Lotto {
         return hit;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) throws IllegalArgumentException {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) throws IllegalArgumentException {
+        if (numbers.stream().distinct().collect(Collectors.toList()).size() < LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) throws IllegalArgumentException {
+        for (int number: numbers) {
+            if (number < LOTTO_NUMBER_MIN_INCLUDE || number > LOTTO_NUMBER_MAX_INCLUDE) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 
     public boolean containsGivenNumber(int given) {
         return this.numbers.contains(given);
     }
-
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,6 +24,14 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호에 지정된 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoWithOutRangedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 100, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("발행한 로또와 당첨번호를 비교하여 일치하는 숫자의 개수를 반환한다.")
     @Test
     void countHitNumbers() {

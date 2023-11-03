69725320fa8fe7c23614a4bd3b86312ee43ce9fe
euics <pjh575157@gmail.com>
2023-11-03 12:11:17 +0900

    feat(Controller): start 메서드 구현
    로또 게임 컨트롤러 내에 게임의 전체 흐름을 처리하는 메서드
    - 구매 금액 입력
    - 구매 항목 출력
    - 당첨번호 및 보너스 번호 입력
    - 게임 결과 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,11 +2,36 @@ package lotto.controller;
 
 import lotto.domain.LottoTicket;
 import lotto.domain.Rank;
+import lotto.domain.WinningLottoTicket;
 import lotto.view.OutputView;
 
+import java.util.List;
 import java.util.Map;
 
 public class LottoController {
+
+    private static final LottoService lottoService = new LottoService();
+
+    public void start(){
+        // 구입금액 입력
+        int amount = lottoService.displayPurchase();
+
+        // 구매 항목 출력
+        LottoTicket lottoTickets = lottoService.getLottoTicket(amount);
+        displayLottoTickets(amount, lottoTickets);
+
+        // 당첨번호 입력
+        List<Integer> winningNumbers = lottoService.displayWinningNumber();
+
+        // 보너스 번호 입력
+        int bonusNumber = lottoService.displayBonusNumber(winningNumbers);
+
+        // 이기는 로또 티켓
+        WinningLottoTicket winningLottoTicket = lottoService.makeWinningLottoTicket(winningNumbers, bonusNumber);
+        Map<Rank, Integer> statistics = lottoService.getStatistics(lottoTickets, winningLottoTicket);
+
+        displayResult(statistics, amount);
+    }
 
     private static void displayLottoTickets(int amount, LottoTicket lottoTicket){
         OutputView.printPurchasedLottoTickets(amount, lottoTicket);

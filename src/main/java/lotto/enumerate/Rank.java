b18cdad906b44c2677fb1f66ee46bed15fa4b61c
peppone-choi <peppone.choi@gmail.com>
2023-11-04 03:11:11 +0900

    feat: Java Enum 형태의 Rank 계산기 구현

---
+++ src/main/java/lotto/enumerate/Rank.java
@@ -0,0 +1,20 @@
+package lotto.enumerate;
+
+import java.util.function.Function;
+
+public enum Rank {
+    FIRST_PLACE(value -> (long) (value * 2000000000)),
+    SECOND_PLACE(value -> (long) (value * 30000000)),
+    THIRD_PLACE(value -> (long) (value * 1500000)),
+    FOURTH_PLACE(value -> (long) (value * 50000)),
+    FIFTH_PLACE(value -> (long) (value * 5000));
+    private Function<Integer, Long> expression;
+
+    Rank(Function<Integer, Long> expression) {
+        this.expression = expression;
+    }
+
+    public long calculate(int value) {
+        return expression.apply(value);
+    }
+}

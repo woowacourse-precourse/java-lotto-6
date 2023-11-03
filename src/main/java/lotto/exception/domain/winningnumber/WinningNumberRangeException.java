30b940b5f17d90af5eae21f32002fb1b16408afb
leedohyun <lucky77795@naver.com>
2023-11-03 19:29:35 +0900

    refactor(Lotto) : 당첨 번호 정의 예외 제거.
    당첨 번호도 로또 번호와 같은 조건의 예외가 있기 때문에 공유해서 사용하도록 수정.

--- src/main/java/lotto/exception/domain/winningnumber/WinningNumberRangeException.java
+++
@@ -1,9 +0,0 @@
-package lotto.exception.domain.winningnumber;
-
-public class WinningNumberRangeException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다.";
-
-    public WinningNumberRangeException() {
-        super(ERROR_MESSAGE);
-    }
-}

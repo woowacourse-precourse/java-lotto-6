158c9fad2228c93d098a7b7b5dbd1b176d97ce6b
yeseong31 <yeseong31@naver.com>
2023-11-03 22:03:28 +0900

    feat(WinningLotto): WinningLotto 도메인 기능 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,103 @@
+package lotto.domain;
+
+import static lotto.view.constants.ConsoleMessage.DUPLICATE_NUMBER_ERROR;
+import static lotto.view.constants.ConsoleMessage.WINNING_NUMBERS_ERROR;
+import static lotto.view.constants.SeparatorConstant.WINNING_NUMBERS_SEPARATOR;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class WinningLotto {
+
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
+
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+
+    private final List<Integer> winningNumbers;
+    private final Integer bonusNumber;
+
+    private WinningLotto(List<Integer> winningNumbers, Integer bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningLotto of(List<Integer> winningNumbers, Integer bonusNumber) {
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    private void validate(List<Integer> winningNumbers, Integer bonusNumber) {
+        validateSize(winningNumbers);
+        validateDuplicate(winningNumbers);
+        validateInteger(winningNumbers);
+        validateRange(winningNumbers);
+
+        validateDuplicate(winningNumbers, bonusNumber);
+        validateRange(bonusNumber);
+    }
+
+    private void validateDuplicate(List<Integer> winningNumbers, Integer bonusNumber) {
+        if (hasDuplicate(winningNumbers, bonusNumber)) {
+            throw new IllegalArgumentException(
+                    String.format(WINNING_NUMBERS_ERROR.getMessage(), WINNING_NUMBERS_SEPARATOR));
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> winningNumbers, Integer bonusNumber) {
+        return winningNumbers.stream()
+                .anyMatch(Predicate.isEqual(bonusNumber));
+    }
+
+    private void validateInteger(List<Integer> winningNumbers) {
+        if (hasNull(winningNumbers)) {
+            throw new IllegalArgumentException(
+                    String.format(WINNING_NUMBERS_ERROR.getMessage(), WINNING_NUMBERS_SEPARATOR));
+        }
+    }
+
+    private boolean hasNull(List<Integer> winningNumbers) {
+        return winningNumbers.stream()
+                .anyMatch(Objects::isNull);
+    }
+
+    private void validateRange(List<Integer> winningNumbers) {
+        boolean hasNotValidate = winningNumbers.stream()
+                .anyMatch(this::notValidateRange);
+
+        if (hasNotValidate) {
+            throw new IllegalArgumentException(
+                    String.format(WINNING_NUMBERS_ERROR.getMessage(), WINNING_NUMBERS_SEPARATOR));
+        }
+    }
+
+    private void validateRange(Integer number) {
+        if (notValidateRange(number)) {
+            throw new IllegalArgumentException(
+                    String.format(WINNING_NUMBERS_ERROR.getMessage(), WINNING_NUMBERS_SEPARATOR));
+        }
+    }
+
+    private boolean notValidateRange(Integer number) {
+        return number < MINIMUM_LOTTO_NUMBER || MAXIMUM_LOTTO_NUMBER < number;
+    }
+
+    private void validateDuplicate(List<Integer> winningNumbers) {
+        if (hasDuplicate(winningNumbers)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR.getMessage());       }
+    }
+
+    private boolean hasDuplicate(List<Integer> winningNumbers) {
+        return winningNumbers.stream()
+                .distinct()
+                .count() != LOTTO_NUMBERS_SIZE;
+    }
+
+    private void validateSize(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(
+                    String.format(WINNING_NUMBERS_ERROR.getMessage(), WINNING_NUMBERS_SEPARATOR));
+        }
+    }
+}

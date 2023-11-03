fc31aaaaa00b120bc9a996105c4796196670bf47
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-04 02:27:13 +0900

    refactor: 유효성 검사를 위한 구매 금액 래퍼 클래스 구현
    로또 구매 값을 Money 클래스로 포장

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public class Money {
+
+    private final int money;
+
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("[ERROR] 돈은 음수가 될 수 없습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}

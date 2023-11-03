d2516991f8acc7b96b984fdee978ec9aca3777af
nahyeon99 <nahyeonee99@gmail.com>
2023-11-03 17:20:30 +0900

    feat(domain/Lotto): 로또 검증 기능 추가
    1. 번호 공백, 음수, 범위와 로또 개수에 대한 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,41 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.view.InputView;
 
 public class Lotto {
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 6개의 번호를 입력해야 합니다.";
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        this.numbers = numbers.stream().sorted().toList();
+    private Lotto(List<Integer> numbers) {
+        numbers.forEach(number -> InputView.validateBlank(String.valueOf(number)));
+        numbers.forEach(InputView::validateNegativeInteger);
+        validateSize(numbers.size());
+        validateRange(numbers);
+        this.numbers = numbers.stream().toList();
+    }
+
+    public static Lotto of(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validateSize(int size) {
+        if (size != LOTTO_COUNT) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+            }
+        });
     }
 }

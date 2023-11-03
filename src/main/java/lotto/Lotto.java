b1873467ce4556de27e4bff3407581fc9156f9d4
happyryan <tjdgks120813@gmail.com>
2023-11-03 18:02:48 +0900

    feat(lotto Lotto): add getSixLottoNumbers method in Lotto class
    Added the getSixLottoNumbers method in the Lotto class to retrieve the list of Lotto numbers.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,13 +19,17 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    private void validateNoDuplicateNumbers(List<Integer> numbers){
+    private void validateNoDuplicateNumbers(List<Integer> numbers) {
         List<Integer> uniqueNumbers = new ArrayList<>();
-        for(int number : numbers){
-            if(uniqueNumbers.contains(number)){
+        for (int number : numbers) {
+            if (uniqueNumbers.contains(number)) {
                 throw new IllegalArgumentException("[ERROR] 로또 당첨 번호에 중복된 숫자(%d)가 존재합니다.".formatted(number));
             }
             uniqueNumbers.add(number);
         }
+    }
+
+    public List<Integer> getSixLottoNumbers() {
+        return numbers;
     }
 }

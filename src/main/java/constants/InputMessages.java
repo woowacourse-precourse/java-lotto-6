e4566f91a978f15c4cc124611878da2b01e6c2b4
Leespect <wewqwew153@gmail.com>
2023-11-03 14:16:39 +0900

    Refactor : InputMessages를 enum으로 수정

--- src/main/java/constants/InputMessages.java
+++ src/main/java/constants/InputMessages.java
@@ -1,9 +1,18 @@
 package constants;
 
-public class InputMessages {
+public enum InputMessages {
 
-    public static final String startBuyMessage = "구입금액을 입력해 주세요.";
-    public static final String getWinNumberMessage = "당첨 번호를 입력해 주세요.";
-    public static final String getBonusNumberMessage = "보너스 번호를 입력해 주세요.";
+    START_BUY_MESSAGE("구입금액을 입력해 주세요."),
+    GET_WIN_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    GET_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
 
+    private final String message;
+
+    InputMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

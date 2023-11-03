6b938f87e56a7e67d0c10cbec54f4d7d5f6bc0e5
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-03 16:54:58 +0900

    feature : 출력 기능 추가
    - 같은 줄에 내용 추가하기
    - 내용 출력 후 칸 줄 띄기

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+public class OutputView {
+    private OutputView() {}
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}

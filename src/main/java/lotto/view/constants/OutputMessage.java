b801455b0aa2f7bf57f8223fc7e0faef7e0e5746
HamoHamoHamo <hamo222@naver.com>
2023-11-04 00:24:28 +0900

    style : view 상수 목록 OutputMessage로 이름 변경

--- src/main/java/lotto/view/constants/OutputMessage.java
+++ src/main/java/lotto/view/constants/OutputMessage.java
@@ -1,6 +1,6 @@
 package lotto.view.constants;
 
-public enum ConstantMessage {
+public enum OutputMessage {
     INPUT_PURCHASE_PRICE_MESSAGE("구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
@@ -14,7 +14,7 @@ public enum ConstantMessage {
 
     private final String message;
 
-    ConstantMessage(String message) {
+    OutputMessage(String message) {
         this.message = message;
     }
 

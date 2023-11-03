0c4a90a86a2d18b9236a7c5eef2d468b2bcc76c8
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-03 21:12:48 +0900

    refactor(OutputView) : change showBayLottoCount() method logic

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,11 @@
 package lotto.view;
 
 import lotto.Lotto;
-import lotto.domain.PurchaseAmount;
 
 public class OutputView {
 
-    public void showBayLottoCount(PurchaseAmount purchaseAmount){
-        System.out.println(purchaseAmount.getAmount()+"개를 구매했습니다.");
+    public static void showBayLottoCount(int lottoCount) {
+        System.out.println(String.valueOf(lottoCount) + "개를 구매했습니다.");
     }
 
 

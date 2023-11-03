5a0ab04a3b8a7a30e49689f4286e8a49d822ea06
JP-company <wjsdj2008@gmail.com>
2023-11-03 22:10:02 +0900

    Feat: 로또 구입 금액 입력 기능
    사용자에게 로또 구입 금액을 입력받는 기능을 추가했다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int lottoPrice = UserController.getLottoPrice();
+        System.out.println("입력한 로또 금액 = " + lottoPrice);
     }
 }
---
+++ src/main/java/lotto/UserController.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserController {
+    private UserController() {}
+
+    public static int getLottoPrice() {
+        try {
+            System.out.println("구입 금액을 입력해 주세요.");
+            String userInputLottoPrice = Console.readLine().trim();
+
+            return Validation.getLottoPrice(userInputLottoPrice);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getLottoPrice();
+        }
+    }
+
+}

c38dedbb8b1e473621eacea65810e00ab0558edb
yesjjin99 <yesjjin99@naver.com>
2023-11-04 07:38:41 +0900

    feat: 당첨 번호 예외 처리 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,11 +2,13 @@ package lotto.domain;
 
 import java.util.List;
 import lotto.utils.StringUtils;
+import lotto.validation.LottoNumberValidation;
 
 public class WinningLotto {
     private final List<Integer> numbers;
 
-    public WinningLotto(String numbers) {
-        this.numbers = StringUtils.split(numbers);
+    public WinningLotto(String s) {
+        this.numbers = StringUtils.split(s);
+        new LottoNumberValidation().validate(numbers);
     }
 }
--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import lotto.utils.message.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,5 +30,41 @@ class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(s))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(ErrorMessage.INVALID_SPLIT_DELIMITER);
+    }
+
+    @Test
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    void createWinningLottoByOverSize() {
+        // given
+        String s = "1,2,3,4,5,6,7";
+
+        // when // then
+        assertThatThrownBy(() -> new WinningLotto(s))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_NUMBER_SIZE);
+    }
+
+    @Test
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    void createWinningLottoByDuplicatedNumber() {
+        // given
+        String s = "1,2,3,4,5,5";
+
+        // when // then
+        assertThatThrownBy(() -> new WinningLotto(s))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.DUPLICATED_NUMBER);
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 1~45 사이의 숫자가 아니라면 예외가 발생한다.")
+    void createWinningLottoByWrongRangeNumber() {
+        // given
+        String s = "0,46,3,4,5,6";
+
+        // when // then
+        assertThatThrownBy(() -> new WinningLotto(s))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_NUMBER_RANGE);
     }
 }

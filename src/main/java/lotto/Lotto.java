0bb6f707be4005447a86f62de06e2075e5923403
flown222 <jangu3384@gmail.com>
2023-11-03 14:36:39 +0900

    feat: 로또 번호가 범위를 만족하는지에 대한 검증 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Lotto {
     private static final int LOTTO_LENGTH = 6;
+    private static final int LOTTO_LEFT_BOUND = 1;
+    private static final int LOTTO_RIGHT_BOUND = 45;
 
     private final List<Integer> numbers;
 
@@ -15,6 +17,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateLengthCheck(numbers);
         validateDuplicateNumber(numbers);
+        validateNumbers(numbers);
     }
 
     private void validateLengthCheck(List<Integer> numbers) {
@@ -26,6 +29,21 @@ public class Lotto {
     private void validateDuplicateNumber(List<Integer> numbers) {
         if (getDistinctCount(numbers) != LOTTO_LENGTH) {
             throw new IllegalArgumentException("숫자는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        numbers.forEach(this::validateProperNumber);
+    }
+
+    private void validateProperNumber(Integer number) {
+        if (number < LOTTO_LEFT_BOUND || number > LOTTO_LEFT_BOUND) {
+            throw new IllegalArgumentException(String.format(
+                    "숫자는 %d에서 %d사이여야합니다."
+                    , LOTTO_LEFT_BOUND
+                    , LOTTO_RIGHT_BOUND
+                )
+            );
         }
     }
 
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,4 +22,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자는 중복될 수 없습니다.");
     }
+    
+    @DisplayName("로또 번호의 숫자가 1~45의 범위를 만족하지 않을시 예외가 발생한다.")
+    @Test
+    void createLottoByImproperNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 47)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자는 1에서 45사이여야합니다.");
+    }
 }

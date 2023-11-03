be22cb48d177a7b78c43039d09fa0c6f4d7643b4
ChaeAg <dydkglel123@gmail.com>
2023-11-03 13:26:44 +0900

    feat(Lotto): 로또 객체 클래스 패키지 변경 및 예외 처리

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,43 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateSixNumbersCount(numbers);
+        validateInvalidLottoNumberRange(numbers);
+        validateDuplicateNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validateSixNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateInvalidLottoNumberRange(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또의 번호는 1~45 사이의 수여야 합니다.");
+            }
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        List<Integer> checkNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (checkNumbers.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또의 각 번호는 중복될 수 없습니다.");
+            }
+            checkNumbers.add(number);
+        }
+    }
+}

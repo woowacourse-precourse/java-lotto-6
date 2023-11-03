3e1f39cfe4a32a67443ea8332a502c85cddf60c5
HYUNJUN\sajun <sajun28@naver.com>
2023-11-04 03:28:45 +0900

    Refactor: "클래스명 수정"

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class InputView {
 
-    public String purchasingAmount() {
+    public String purchaseAmount() {
         System.out.println("구입금액을 입력해 주세요");
         return Console.readLine();
     }

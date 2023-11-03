5ed0c04e6eb10b6607645e100506fdfe09f009fc
Hoo-sung <gntjd135@gmail.com>
2023-11-03 22:22:34 +0900

    feat(LottoSystem): 컨트롤러 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.controller.LottoSystem;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new LottoSystem().run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -0,0 +1,12 @@
+package lotto.controller;
+
+import lotto.domain.UserLotto;
+
+public class LottoSystem {
+
+    private UserLotto userLotto;
+
+    public void run() {
+
+    }
+}

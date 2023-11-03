c7872fda4eb177cf3d24b450ff192d251cd55513
sami355-24 <ddung355@gmail.com>
2023-11-03 19:28:58 +0900

    test(Validation): 1~45 범위안에 있을때 정상 동작하는 지 테스트

--- src/test/java/lotto/Validation/CommonValidationTest.java
+++ src/test/java/lotto/Validation/CommonValidationTest.java
@@ -1,6 +1,7 @@
 package lotto.Validation;
 
 import static lotto.Message.ExceptionMessage.CommonValidationErrorMessage.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import lotto.Exception.CommonValidationException;
@@ -21,23 +22,39 @@ class CommonValidationTest {
 
     @Test
     @DisplayName("숫자가 범위 밖에 있을때 예외가 발생하는지 확인하는 테스트")
-    void isValidRange() {
+    void isValidRange1() {
         //given
         Integer value1 = 0;
         Integer value2 = 46;
 
         //when, then
-        Assertions.assertThatThrownBy(() -> commonValidation.isValidRange(value1))
+        assertThatThrownBy(() -> commonValidation.isValidRange(value1))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(RANGE_ERROR_MESSAGE.getMessage(value1));
 
-        Assertions.assertThatThrownBy(() -> commonValidation.isValidRange(value2))
+        assertThatThrownBy(() -> commonValidation.isValidRange(value2))
                 .isInstanceOf(CommonValidationException.class)
                 .hasMessageContaining(RANGE_ERROR_MESSAGE.getMessage(value2));
     }
 
-    @Test
 
+    @Test
+    @DisplayName("숫자가 범위 안에 있을때 정상 수행하는지 확인하는 테스트")
+    void isValidRange2() {
+        //given
+        Integer value1 = 1;
+        Integer value2 = 45;
+
+        //when
+        commonValidation.isValidRange(value1);
+        commonValidation.isValidRange(value2);
+
+        //then
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    @DisplayName("공백이 있을때 예외가 발생하는지 확인하는 테스트")
     void isBlank() {
     }
 

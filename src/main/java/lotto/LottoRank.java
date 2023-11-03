6a77358d7afc65a7ddf1bd5d8f27c25070ede0e1
gusgh075 <gusgh075@naver.com>
2023-11-03 14:55:37 +0900

    FEATURES(LottoRank) : 로또번호와 당첨번호, 그리고 보너스번호를 비교해 등수를 반환하는 기능

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -4,6 +4,34 @@ import java.util.List;
 
 public class LottoRank {
 
+    public int isRank(Lotto lotto, Lotto compare, int bonusNum) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        List<Integer> compareNumbers = compare.getNumbers();
+        int matchingCount = countMatchingNumbers(lottoNumbers, compareNumbers);
+        boolean hasNum = isHasNum(compareNumbers, bonusNum);
+        switch (matchingCount) {
+            case 0, 1, 2 -> {
+                return 6;
+            }
+            case 3 -> {
+                return 5;
+            }
+            case 4 -> {
+                return 4;
+            }
+            case 5 -> {
+                if (!hasNum) {
+                    return 3;
+                } else {
+                    return 2;
+                }
+            }
+            case 6 -> {
+                return 1;
+            }
+        }
+        return -1;
+    }
 
     private int countMatchingNumbers(List<Integer> lottoNumbers, List<Integer> compareNumbers) {
         long count = lottoNumbers.stream()

733fb4f20a05ce34961cb877fb1e69d0a03f48e6
PARKHWANYEONG <welcome0828@naver.com>
2023-11-03 10:51:45 +0900

    feat(view): 사용자에게 보너스 번호를 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String purchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
@@ -14,6 +15,11 @@ public class InputView {
 
     public static String winningNumbers() {
         System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String bonusNumber() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
         return Console.readLine();
     }
 }

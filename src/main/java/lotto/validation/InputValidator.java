cc2d17b4afe237198e0e8527af8333377953346b
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 02:21:29 +0900

    feat : 구매 금액에 대한 유효성 검증 기능 구현

---
+++ src/main/java/lotto/validation/InputValidator.java
@@ -0,0 +1,37 @@
+package lotto.validation;
+
+public class InputValidator {
+
+    private InputValidator() {
+    }
+
+    public static void validatePurchaseAmount(final String purchaseAmount) {
+        if (purchaseAmount.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (!isNumeric(purchaseAmount)) {
+            throw new IllegalArgumentException();
+        }
+        if (!isPositive(purchaseAmount)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isNumeric(final String number) {
+        try {
+            Integer.valueOf(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isPositive(final String amount) {
+        try {
+            Integer numericAmount = Integer.valueOf(amount);
+            return (numericAmount > 0);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}

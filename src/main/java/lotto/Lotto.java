56efc715b3af147cbfcd2d86c91782daeb60b861
Kite_Z <duswl339@ajou.ac.kr>
2023-11-04 21:10:54 +0900

    feat: add compare method and getNumbers method for Lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,4 +17,15 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int compare(List<Integer> _winning) {
+        List<Integer> _same = getNumbers().stream().filter(
+                _number -> {
+                    return _winning.contains(_number);
+                }).toList();
+        return _same.size();
+    }
 }

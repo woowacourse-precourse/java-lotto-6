71344ff759362cea578b8f84ddcdba7d96e401c5
GS_song <20003204@sju.ac.kr>
2023-11-05 01:28:28 +0900

    feat: 랜덤한 숫자들로 구성된 Lotto를 생성하는 RandomLottoGenerator 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,16 +3,21 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
     private LottoResult result;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        result = null;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }
---
+++ src/main/java/lotto/model/RandomLottoGenerator.java
@@ -0,0 +1,16 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomLottoGenerator {
+
+    public Lotto generate() {
+        List<Integer> RandomNums = Randoms.pickUniqueNumbersInRange(Lotto.LOTTO_MIN_NUMBER, Lotto.LOTTO_MAX_NUMBER,
+                Lotto.LOTTO_SIZE);
+        Collections.sort(RandomNums);
+        return new Lotto(RandomNums);
+    }
+}

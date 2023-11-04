93eb5558cc1b8cefb0d789e1a6db74521392628d
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-04 12:26:50 +0900

    refactor(InputView): View는 Model과 분리하기 위해 객체생성하지 않고 입력값만 반환하도록 수정

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -5,41 +5,19 @@ import java.lang.reflect.WildcardType;
 import lotto.Model.Lotto;
 import lotto.Model.Price;
 import lotto.Model.PriceValidator;
+import lotto.Model.WinningLotto;
 //import lotto.Model.WinningLottoValidator;
 
 public class InputView {
 
-    public static Price inputPriceForLotto() {
+    public static String inputPriceForLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        try {
-            String input = Console.readLine();
-            return new Price(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputPriceForLotto();
-        }
+        return Console.readLine();
     }
 
-//        try {
-//            PriceValidator priceValidator = new PriceValidator(Console.readLine());
-//            return new Price(priceValidator.getInputPrice());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println("[ERROR] 1000원 단위로 1000원~20억원 사이에서 입력해주세요. 예시) 3000");
-//            return inputPriceForLotto();
-//        }
+    public static String inputWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
 
-
-//    public static Lotto inputWinningLotto() {
-//        System.out.println("당첨 번호를 입력해 주세요.");
-//
-//
-////        try {
-////            WinningLottoValidator winningLottoValidator = new WinningLottoValidator(Console.readLine());
-////            return new Lotto(winningLottoValidator.getWinningLotto());
-////        } catch (IllegalArgumentException e) {
-////            System.out.println("[ERROR] 숫자와 쉼표로만 당첨 번호를 입력해 주세요.");
-////            return inputWinningLotto();
-////        }
-////    }
-//
+    }
 }

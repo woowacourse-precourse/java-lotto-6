be0a036c83f5a020d5a03237ffc1f60587b32905
Juhyorim <joohr1234@gmail.com>
2023-11-04 22:13:56 +0900

    feat: 금액만큼의 개수의 로또 발행 기능 구현

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import lotto.ui.ConsoleInput;
 import lotto.ui.ConsoleOutput;
@@ -10,16 +9,27 @@ import lotto.ui.Output;
 public class LottoManager {
     private Input input;
     private Output output;
+    private LottoGenerator lottoGenerator;
 
     public LottoManager() {
-        this.input = new ConsoleInput();
-        this.output = new ConsoleOutput();
+        this(new ConsoleInput(), new ConsoleOutput(), new LottoGenerator());
+    }
+
+    public LottoManager(Input input, Output output, LottoGenerator lottoGenerator) {
+        this.input = input;
+        this.output = output;
+        this.lottoGenerator = lottoGenerator;
     }
 
     public List<Lotto> buyLotto() {
         int price = getPrice();
+        int numberOfLotto = getNumberOfLotto(price);
 
-        return new ArrayList<>();
+        return lottoGenerator.generateLottos(numberOfLotto);
+    }
+
+    private int getNumberOfLotto(int price) {
+        return price / 1000;
     }
 
     private int getPrice() {

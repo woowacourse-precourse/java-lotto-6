d303e33487a471ec76594156eeaec39674290463
unknown <winternrabbit@gmail.com>
2023-11-05 03:14:10 +0900

    test: 작업 코드 수정에 따른 테스트 코드 수정

--- src/test/java/lotto/LottoCostTest.java
+++ src/test/java/lotto/LottoCostTest.java
@@ -1,22 +1,23 @@
 package lotto;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoCostTest {
-    LottoCost lottoCost = new LottoCost();
 
-    @DisplayName("구입금액이 숫자가 아닐 경우 false가 반환된다.")
+    @DisplayName("구입금액이 숫자가 아닐 경우 예외가 발생한다.")
     @Test
     void costNotNumber() {
-        assertThat(lottoCost.isNumber("abc123가나다")).isEqualTo(false);
+        assertThatThrownBy(() -> new LottoCost("abc123가나다"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구입금액이 지정한 수(1000)의 배수가 아닐 경우 false가 반환된다.")
+    @DisplayName("구입금액이 지정한 1000의 배수가 예외가 발생한다.")
     @Test
     void costNotMultipleOfUnit() {
-        assertThat(lottoCost.isMultipleOf(LottoCost.UNIT, "1375")).isEqualTo(false);
+        assertThatThrownBy(() -> new LottoCost("1375"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

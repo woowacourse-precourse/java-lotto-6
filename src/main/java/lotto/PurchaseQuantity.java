2f0fc73b6c6de564c2f4fe7de96d0a07613978d4
hlmg <devhlmg@gmail.com>
2023-11-04 12:43:18 +0900

    feat: 구매 수량 생성 기능 구현

---
+++ src/main/java/lotto/PurchaseQuantity.java
@@ -0,0 +1,36 @@
+package lotto;
+
+public class PurchaseQuantity {
+    private static final int unit = 1000;
+    private final int quantity;
+
+    public PurchaseQuantity(int amount) {
+        validate(amount);
+        this.quantity = calculateQuantity(amount);
+    }
+
+    private void validate(int amount) {
+        validateIsZero(amount);
+        validateDivisible(amount);
+    }
+
+    private void validateIsZero(int amount) {
+        if (amount == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivisible(int amount) {
+        if (amount % unit != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int calculateQuantity(int amount) {
+        return amount / unit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
---
+++ src/test/java/lotto/PurchaseQuantityTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PurchaseQuantityTest {
+
+    @DisplayName("구입 금액이 0원이면 예외가 발생한다")
+    @Test
+    void createPurchaseAmountByZero() {
+        int purchaseAmount = 0;
+
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> new PurchaseQuantity(purchaseAmount));
+    }
+
+    @DisplayName("구입 금액이 단위로 나누어 떨어지지 않으면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 999, 1001, 9999})
+    void createPurchaseAmountByNotDivisibleAmount(int purchaseAmount) {
+
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> new PurchaseQuantity(purchaseAmount));
+    }
+
+    @DisplayName("구매 수량을 생성할 수 있다")
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 2000, 100000})
+    void createPurchaseAmount(int purchaseAmount) {
+
+        PurchaseQuantity createdPurchaseQuantity = new PurchaseQuantity(purchaseAmount);
+
+        int actual = createdPurchaseQuantity.getQuantity();
+        Assertions.assertThat(actual).isEqualTo(purchaseAmount / 1000); // TODO: 상수 클래스 만들 때, 1000 변경하기
+    }
+}

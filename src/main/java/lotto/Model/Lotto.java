e1e7ca4c2900a219afe187b608fad902dee9c96a
hgh1472 <hgh1472@naver.com>
2023-11-05 01:12:07 +0900

    refactor : 오름차순 정렬 메서드 인자값 수정

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -2,22 +2,28 @@ package lotto.Model;
 
 import java.util.Collections;
 import java.util.List;
+import lotto.constant.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        sortAscending(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBER_ERROR_MESSAGE.getErrorMessage());
         }
     }
 
-    private void sortAscending() {
+    public List<Integer> getLottoNumbers() {
+        return numbers;
+    }
+
+    private void sortAscending(List<Integer> numbers) {
         Collections.sort(numbers);
     }
 

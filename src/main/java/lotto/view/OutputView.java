ddbc83518894063fad0ef4490b503beb3973bf09
WooYoungDoo <duden5000@naver.com>
2023-11-05 01:11:57 +0900

    feat (OutputView) : 출력 관련 기능 (로또 번호 및 통계 제외)

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+public class OutputView {
+
+    private final static String OUTPUT_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private final static String OUTPUT_AMOUNT_LOTTO_MESSAGE = "%d개를 구매했습니다.";
+    private final static String OUTPUT_WINNER_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public void printPurchaseMoneyMessage() {
+        System.out.println(OUTPUT_PURCHASE_MONEY_MESSAGE);
+    }
+
+    public void printAmountLottoMessage(int ammount) {
+        System.out.printf(OUTPUT_AMOUNT_LOTTO_MESSAGE, ammount);
+    }
+
+    public void printWinnerNumberMessage() {
+        System.out.println(OUTPUT_WINNER_NUMBER_MESSAGE);
+    }
+
+    public void printBonusNumberMessage() {
+        System.out.println(OUTPUT_BONUS_NUMBER_MESSAGE);
+    }
+}

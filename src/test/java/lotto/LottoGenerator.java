8e117c5c5a388701affd70c29771824c5c5b9da5
minsang-alt <mazinger97@naver.com>
2023-11-04 14:10:44 +0900

    feat: 로또 생성기 구현
    - 랜덤으로 숫자 6개를 만들어 리스트에 넣어 반환하는 메서드 구현
    - 발행할 숫자를 인자로 전달하면 그만큼 로또를 생성하여 리스트에 넣어 반환

---
+++ src/test/java/lotto/LottoGenerator.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
+    public List<Integer> createRandomNumbersByAscending() {
+        List<Integer> numbers =
+                Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_NUMBER_SIZE);
+        Collections.sort(numbers);
+        return numbers;
+    }
+
+    public Lotto createOnlyOneLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public List<Lotto> createLottoBundle(int issueCount) {
+        List<Lotto> lottoBundle = new ArrayList<>();
+        for (int i = 0; i < issueCount; i++) {
+            Lotto lotto = createOnlyOneLotto(createRandomNumbersByAscending());
+            lottoBundle.add(lotto);
+        }
+        return lottoBundle;
+    }
+
+}

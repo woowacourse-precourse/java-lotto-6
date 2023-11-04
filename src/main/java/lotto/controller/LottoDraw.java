87760678528a310d1dc4c6a5b227d384d75f308a
Kite_Z <duswl339@ajou.ac.kr>
2023-11-04 21:16:34 +0900

    feat: add Lotto Business Logic

--- src/main/java/lotto/controller/LottoDraw.java
+++ src/main/java/lotto/controller/LottoDraw.java
@@ -1,13 +1,17 @@
 package lotto.controller;
 
 import lotto.model.Customer;
+import lotto.model.LottoWinning;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-import lotto.view.constant.ConstantMessage;
-import static lotto.view.constant.ConstantMessage.PAY_MONEY;
+
+import static lotto.constant.LottoConstant.LOTTO_PRICE;
+import static lotto.view.constant.ConstantMessage.*;
 
 public class LottoDraw {
     private Customer customer;
+    private int numOfLotto;
+    private LottoWinning lottoWinning;
 
     public LottoDraw() {}
     public void start() {
@@ -16,18 +20,44 @@ public class LottoDraw {
         customer = new Customer(InputView.requestPayMoney());
         OutputView.printNewLine();
         OutputView.println(String.valueOf(customer.getMoney()));
-
+        OutputView.printNewLine();
 
         // 로또 발행
+        numOfLotto = customer.getMoney() / LOTTO_PRICE.getLottoConstant();
+        OutputView.printBuyLotto(BUY_LOTTO, numOfLotto);
+        OutputView.printNewLine();
+        for(int i = 0; i < numOfLotto ; i++) {
+            customer.buyLotto();
+        }
+        OutputView.printLottos(customer);
+        OutputView.printNewLine();
 
         // 당첨 번호 입력
+        OutputView.printConstantMessage(WINNING_NUMBER);
+        lottoWinning = new LottoWinning(InputView.requestWinnerNumber());
+        OutputView.printNewLine();
+        OutputView.printLottoWinning(lottoWinning.getWinningNumber());
+        OutputView.printNewLine();
 
         // 보너스 번호 입력
+        OutputView.printConstantMessage(BONUS_NUMBER);
+        lottoWinning.initBonusNumber(InputView.requestBonusNumber());
+        OutputView.printNewLine();
+        OutputView.println(String.valueOf(lottoWinning.getBonusNumber()));
+        OutputView.printNewLine();
+
+        // 당첨 확인
+        customer.calcWinningLotto(lottoWinning.getWinningNumber(), lottoWinning.getBonusNumber());
+        OutputView.println(customer.getWinningLottos().toString());
 
         // 당첨 통계
-    }
+        OutputView.printConstantMessage(WINNING_STATICS);
+        OutputView.printWinningStatics(customer.getWinningLottos());
 
-    private void inputPayMoney() {
+        // 총 수익률 계산
+        customer.calcRateOfReturn();
 
+        // 총 수익률 출력
+        OutputView.printRateOfReturn(customer.getRateOrReturn());
     }
 }

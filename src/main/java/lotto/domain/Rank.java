53c8a4d0fbbb628d1fa3c44fd9e7c3f113b8d0d3
elyudwo <dudqk9696@naver.com>
2023-11-05 00:59:39 +0900

    [Feat] : 로또 등수 반환 기능 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Rank {
     CORRECT_THREE(3, 5000L),
     CORRECT_FOUR(4, 50000L),
@@ -15,6 +18,30 @@ public enum Rank {
         this.correctCount = correctCount;
         this.winningAmount = winningAmount;
     }
+
+    public static Rank of(int correctCount, boolean correctBonus) {
+        List<Rank> ranks = findRanksByCorrectCount(correctCount);
+
+        if (ranks.size() == 2) {
+            return selectRankFromBonus(correctBonus);
+        }
+
+        return ranks.get(0);
+    }
+
+    private static List<Rank> findRanksByCorrectCount(int correctCount) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.getCorrectCount() == correctCount)
+                .toList();
+    }
+
+    private static Rank selectRankFromBonus(boolean correctBonus) {
+        if (correctBonus) {
+            return CORRECT_FIVE_BONUS;
+        }
+        return CORRECT_FIVE;
+    }
+
 
     public int getCorrectCount() {
         return correctCount;

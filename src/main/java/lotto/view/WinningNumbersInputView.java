4102d7044e6da5912c0ac670878f77f74af3677e
leesangjun <hubls@naver.com>
2023-11-04 18:52:47 +0900

    refactor: 당첨 번호 입력받는 기능 수정

--- src/main/java/lotto/view/WinningNumbersInputView.java
+++ src/main/java/lotto/view/WinningNumbersInputView.java
@@ -9,13 +9,14 @@ import java.util.List;
 public class WinningNumbersInputView {
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
-    public static List<Integer> getInput() {
+    public List<Integer> getInput() {
         while (true) {
             System.out.println(WINNING_NUMBERS_MESSAGE);
             String winningNumbersAsString = Console.readLine();
 
             try {
-
+                List<Integer> winningNumbers = WinningNumbersConverter.convertStringToList(winningNumbersAsString);
+                return winningNumbers;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }

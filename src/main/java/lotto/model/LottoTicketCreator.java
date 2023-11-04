ef4a6c480be608ccb8cc8aab042602643ef1cf9e
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2023-11-04 15:05:41 +0900

    feat: 금액에 맞게 로또발행 메서드 구현

---
+++ src/main/java/lotto/model/LottoTicketCreator.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicketCreator {
+    private final LottoNumberGenerator numberGenerator;
+    private final LottoTicketCalculator ticketCalculator;
+
+    public LottoTicketCreator() {
+        this.numberGenerator = new LottoNumberGenerator();
+        this.ticketCalculator = new LottoTicketCalculator();
+    }
+
+    public List<Lotto> createLottoTickets(int purchaseAmount) {
+        int ticketCount = ticketCalculator.calculateNumberOfTickets(purchaseAmount);
+        List<Lotto> tickets = new ArrayList<>();
+
+        for (int i = 0; i < ticketCount; i++) {
+            tickets.add(new Lotto(numberGenerator.generateNumbers()));
+        }
+
+        return tickets;
+    }
+}

da246d92b8a266c3c4ab5c88413bbe527875d2eb
tam31 <wxogud3377@naver.com>
2023-11-05 04:14:09 +0900

    Feat(InputValidation) : 0이하 검사 기능 구현 Docs : 0이하 검사 완료체크

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -6,11 +6,13 @@ public class InputValidation {
     private static final String IS_NULL_MESSAGE = "빈 문자열 입니다";
     private static final String IS_NOT_NUMBER_MESSAGE = "숫자가 아닙니다";
     private static final String IS_EXCEED_NUMBER_RANGE_MESSAGE = "21억을 초과했습니다.";
+    private static final String IS_NOT_NUMBER_Range_MESSAGE = "0이하 입니다";
 
     public void validationMoney(String input) {
         isEmptyValidation(input);
         isNumberValidation(input);
         changeInteger(input);
+        isNumberRangeValidation(input);
     }
 
     private void isEmptyValidation(String input) {
@@ -32,6 +34,12 @@ public class InputValidation {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR + IS_EXCEED_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    private void isNumberRangeValidation(String input) {
+        if (Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException(ERROR + IS_NOT_NUMBER_Range_MESSAGE);
         }
     }
 }

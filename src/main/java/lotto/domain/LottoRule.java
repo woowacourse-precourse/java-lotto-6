ffab80d2b50921fe9fccc5f69091f310bb201563
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 01:06:56 +0900

    feat: LottoRule Enum 클래스 작성

---
+++ src/main/java/lotto/domain/LottoRule.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public enum LottoRule {
+
+    MIN_LOTTO_NUMBER(1),
+    MAX_LOTTO_NUMBER(45),
+    LOTTO_LENGTH(6);
+
+    private int value;
+
+    LottoRule(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

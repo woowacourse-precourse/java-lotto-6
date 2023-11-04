16b1381660f13236b72d9da55e5df63a41f8cf9d
Juhyorim <joohr1234@gmail.com>
2023-11-05 02:45:48 +0900

    test: 수익률 둘째 자리에서 반올림 테스트 작성

--- src/test/java/lotto/domain/ReturnCalculatorTest.java
+++ src/test/java/lotto/domain/ReturnCalculatorTest.java
@@ -11,10 +11,25 @@ class ReturnCalculatorTest {
     @Test
     @DisplayName("수익률 계산")
     void 수익률_계산() {
+        //1000원 내고 5000원 당첨
+        Map<WinningStatus, Integer> statusAndCount = new HashMap<>();
+        statusAndCount.put(WinningStatus.MATCH3, 1);
+
+        assertThat(ReturnCalculator.calculate(statusAndCount, 1)).isEqualTo((double) 5000 / 1000 * 100);
+    }
+
+    @Test
+    @DisplayName("둘째자리 반올림")
+    void 둘째_자리_반올림() {
         //3000원 내고 5000원 당첨
         Map<WinningStatus, Integer> statusAndCount = new HashMap<>();
         statusAndCount.put(WinningStatus.MATCH3, 1);
 
-        assertThat(ReturnCalculator.calculate(statusAndCount, 3)).isEqualTo((double) 5000 / 3000 * 100);
+        double expected = ((double) 5000 / 3000) * 100;
+        expected *= 10;
+        expected = Math.round(expected);
+        expected /= 10;
+
+        assertThat(ReturnCalculator.calculate(statusAndCount, 3)).isEqualTo(expected);
     }
 }

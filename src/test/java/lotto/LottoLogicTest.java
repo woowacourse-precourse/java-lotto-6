0895beab3668c2f60ce0703e0b415586b831712b
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 14:25:48 +0900

    test(LottoLogicTest): 보너스 로또 번호가 1~45 범위를 벗어나면 예외가 발생하는 테스트 작성
    보너스 로또 번호가 1~45 범위를 벗어나면 예외가 발생하는 테스트를 작성했습니다.

--- src/test/java/lotto/LottoLogicTest.java
+++ src/test/java/lotto/LottoLogicTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static lotto.message.ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS;
+import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
@@ -15,5 +16,13 @@ public class LottoLogicTest {
         assertThatThrownBy(() -> new LottoLogic(BigDecimal.valueOf(2002), 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS.getMessage());
+    }
+
+    @Test
+    @DisplayName("보너스 로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    void BonusNumberIsNotInCorrectRange() {
+        assertThatThrownBy(() -> new LottoLogic(BigDecimal.valueOf(2000), 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
     }
 }

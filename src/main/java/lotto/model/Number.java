a90307ff650036e5fff1955006b8e1a226f9fc1d
jeondui <jeondui@gmail.com>
2023-11-04 20:56:55 +0900

    refactor(Number): Number 생성자 정적 팩토리 메소드 방식으로 수정

--- src/main/java/lotto/model/Number.java
+++ src/main/java/lotto/model/Number.java
@@ -6,9 +6,13 @@ public class Number {
     private static final String ERROR_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private final int number;
 
-    public Number(int number) {
-        validateNumber(number);
+    private Number(int number) {
         this.number = number;
+    }
+
+    public static Number of(int number) {
+        validateNumber(number);
+        return new Number(number);
     }
 
     private static void validateNumber(int number) {

2039d6d28874153efda0cc0fc80499fdb3ef930c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-05 02:30:19 +0900

    feat(model): 자동 로또 발행 기능 구현

---
+++ src/main/java/lotto/model/AutoLottoGenerator.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class AutoLottoGenerator {
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+    private static final AutoLottoGenerator instance = new AutoLottoGenerator();
+
+    private AutoLottoGenerator() {
+    }
+
+    public static AutoLottoGenerator getInstance() {
+        return instance;
+    }
+
+    public Lotto generate() {
+        List<Integer> lottoNumbers = pickUniqueRandomNumbers();
+        return new Lotto(lottoNumbers);
+    }
+
+    private List<Integer> pickUniqueRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                MINIMUM_LOTTO_NUMBER,
+                MAXIMUM_LOTTO_NUMBER,
+                LOTTO_SIZE
+        );
+    }
+}

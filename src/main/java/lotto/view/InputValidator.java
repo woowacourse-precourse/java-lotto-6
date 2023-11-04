552764fd465bc49c00ab04d1986eb53b20262ba7
kimdohyung <pdohyung@gmail.com>
2023-11-04 18:58:48 +0900

    feat(InputValidator): 구입 금액 단위가 1000원이 아닐 때 예외 처리 기능 추가

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -4,13 +4,18 @@ public class InputValidator {
     private static final String INVALID_NUMBER_FORMAT_MESSAGE = "숫자 형식 입력이 아닙니다.";
     private static final String INVALID_EMPTY_INPUT_MESSAGE = "입력이 빈칸입니다.";
     private static final String INVALID_CONTAIN_BLANK_INPUT_MESSAGE = "입력에 공백이 포함될 수 없습니다.";
+    private static final String INVALID_BUY_AMOUNT_MESSAGE = "구입 금액을 1,000원 단위로 입력해주세요.";
     private static final String EMPTY = "";
     private static final char BLANK = ' ';
+    private static final int THOUSAND = 1000;
+    private static final int ZERO = 0;
 
     public static int validateBuyAmount(String inputBuyAmount) {
         validateEmpty(inputBuyAmount);
         validateContainBlank(inputBuyAmount);
-        return convertStringToInteger(inputBuyAmount);
+        int buyAmount = convertStringToInteger(inputBuyAmount);
+        validateRestZero(buyAmount);
+        return buyAmount;
     }
 
     private static int convertStringToInteger(String input) {
@@ -33,6 +38,14 @@ public class InputValidator {
 
         if (containBlank) {
             throw new IllegalArgumentException(INVALID_CONTAIN_BLANK_INPUT_MESSAGE);
+        }
+    }
+
+    private static void validateRestZero(int buyAmount) {
+        int rest = buyAmount % THOUSAND;
+
+        if (rest != ZERO) {
+            throw new IllegalArgumentException(INVALID_BUY_AMOUNT_MESSAGE);
         }
     }
 }

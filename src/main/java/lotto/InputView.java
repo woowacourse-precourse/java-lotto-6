9ac5015f0fb146c663de3928898711079f9a20e0
sangxxjin <sj_0368@naver.com>
2023-11-05 03:52:35 +0900

    feat: 당첨번호를 입력받는 메소드 구현 및 상수처리

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final static String PURCHASE_MESSAGE = "구입금액을 입력해 주세요";
+    private final static String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
 
     public static String getPurchaseAmount(){
         System.out.println(PURCHASE_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String get_winning_number(){
+        System.out.println(WINNING_NUMBER_PROMPT);
         return Console.readLine();
     }
 }

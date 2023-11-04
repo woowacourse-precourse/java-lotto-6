7746ff7529704c379afbeeee384e9ddb8d698a13
song-jae-wan <sjo9810@gmail.com>
2023-11-04 22:21:32 +0900

    refactor: 당첨 개수에 대한 상수 리팩터링
    enum으로 당첨수에 따른 당첨금, 출력할 메시지 상수 별도 관리 하도록 수정

---
+++ src/main/java/config/CountMessage.java
@@ -0,0 +1,27 @@
+package config;
+
+public enum CountMessage {
+
+    FIFTH("3개 일치 (5,000원) - %d개\n", 3, 5_000d),
+    FOURTH("4개 일치 (50,000원) - %d개\n", 4, 50_000d),
+    THIRD("5개 일치 (1,500,000원) - %d개\n", 5, 1_500_000d),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", 7, 30_000_000d),
+    FIRST("6개 일치 (2,000,000,000원) - %d개\n", 6, 2_000_000_000d);
+
+    private String message;
+    private Integer count;
+
+    private Double price;
+
+    private CountMessage(String message, Integer count, Double price) {
+        this.message = message;
+        this.count = count;
+        this.price = price;
+    }
+
+    public String getMessage() { return message; }
+
+    public int getCount() { return count; }
+
+    public Double getPrice() { return price; }
+}
--- src/main/java/config/OutputMessage.java
+++ src/main/java/config/OutputMessage.java
@@ -3,15 +3,10 @@ package config;
 public enum OutputMessage {
 
     DEPOSIT("구입금액을 입력해 주세요."),
-    PAYMENT("개를 구매했습니다."),
-    WIN_NUMBER("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    RESULT("당첨 통계\n---"),
-    FIFTH("3개 일치 (5,000원) - %d개"),
-    FOURTH("4개 일치 (50,000원) - %d개"),
-    THIRD("5개 일치 (1,500,000원) - %d개"),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    FIRST("6개 일치 (2,000,000,000원) - %d개"),
+    PAYMENT("\n%d개를 구매했습니다.\n"),
+    WIN_NUMBER("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    RESULT("\n당첨 통계\n---"),
     RATE("총 수익률은 %.1f%%입니다.");
 
     private String message;

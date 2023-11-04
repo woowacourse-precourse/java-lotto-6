b6309d47871527f19cb01773a395f1f85e5a2353
jeondui <jeondui@gmail.com>
2023-11-05 00:35:59 +0900

    test(LottoTest): 로또 번호 개수 유효성 예외 발생 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -20,6 +20,14 @@ class LottoTest {
                 .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
     }
 
+    @DisplayName("로또 번호에 개수 6개 미만 시 예외 발생 테스트")
+    @Test
+    void createLottoSize() {
+        assertThatThrownBy(() -> Lotto.create("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
+    }
+
     @DisplayName("로또 번호 중복 시 예외 발생 테스트")
     @Test
     void createLottoByDuplicatedNumber() {

a6375f6ab7a9f00fc4128652dd4910352955d2e2
MyunghyunNero <mh1kim0910@gmail.com>
2023-11-04 23:26:58 +0900

    feat : LottoCalculater 클래스 생성

---
+++ src/main/java/lotto/domain/LottoCalculater.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class LottoCalculater {
+
+    private User user;
+    private LottoAdmin lottoAdmin;
+
+    public LottoCalculater(User user, LottoAdmin lottoAdmin) {
+        this.user = user;
+        this.lottoAdmin = lottoAdmin;
+    }
+
+
+}

942482e6757899d5171461a47f5e8970a0f261d7
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 19:48:09 +0900

    [test] 당첨금 계산 기능 테스트

--- src/test/java/lotto/domain/PrizesTest.java
+++ src/test/java/lotto/domain/PrizesTest.java
@@ -16,5 +16,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("당첨 내역 계산")
 class PrizesTest {
 
+    @DisplayName("당첨금 계산 성공 테스트")
+    @ParameterizedTest()
+    @MethodSource("countTotalPriceSuccessDummy")
+    void countTotalPriceSuccessTest(Prizes prizes, Double expected) {
+        Double totalPrice = prizes.countTotalPrice();
+        assertEquals(expected, totalPrice);
+    }
 
+
+    static Stream<Arguments> countTotalPriceSuccessDummy() {
+        return Stream.of(
+                Arguments.arguments(
+                        new Prizes(
+                                List.of(LAST_PLACE, FIRST_PLACE, SECOND_PLACE, LAST_PLACE, LAST_PLACE)
+                        ),
+                        230_000_000.0),
+                Arguments.arguments(
+                        new Prizes(
+                                List.of(LAST_PLACE, SECOND_PLACE, LAST_PLACE, SECOND_PLACE)
+                        ),
+                        60_000_000.0),
+                Arguments.arguments(
+                        new Prizes(
+                                List.of(LAST_PLACE, FIRST_PLACE, SECOND_PLACE, FIRST_PLACE, LAST_PLACE)
+                        ),
+                        430_000_000.0)
+        );
+    }
 }

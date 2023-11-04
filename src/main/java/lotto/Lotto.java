17eac0368bc1196812200aea483cf4a081e621d0
SeongIl <biuld1234@gmail.com>
2023-11-04 11:54:40 +0900

    refactor: modify 보너스번호 중복 검사 - 메서드 이름 수정, Lotto 클래스에서 변수를 직접 사용해, 인자 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,34 +12,26 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void checkInclusion(Integer bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해 주세요.");
+        }
+    }
+
+
     private void checkSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해 주세요.");
         }
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    public void checkDuplication(List<Integer> userInput) {
+    private void checkDuplication(List<Integer> userInput) {
         if (userInput.size() != new HashSet<>(userInput).size()) {
             throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해 주세요.");
         }
     }
-
-    public void checkRange(Integer userInput) {
-
-        if ((userInput < 1) || (userInput > 45)) {
-            throw new IllegalArgumentException("[ERROR] 1~45 범위의 숫자를 입력 하세요.");
-        }
-
-    }
-
-    public void isIncluding(List<Integer> lottoNumbers, Integer bonusNumber) {
-        if (lottoNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해 주세요.");
-        }
-    }
-
 }

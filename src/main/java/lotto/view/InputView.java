7648a312c01d813661e79a5b5553163a20eee6f2
Nbowow <nbo5548@naver.com>
2023-11-05 02:28:53 +0900

    Feat: Add InputView 2023.11.05 1. 총 구매한 로또 갯수 (구매할 로또 갯수) 2. 당첨 번호 3. 보너스 번호 입력받는 폼 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+public class InputView {
+
+    public static void requestLottoBuyingPrice() {
+        System.out.println("구매하실 로또 금액을 입력해주세요.");
+    }
+
+    public static void requestLottoWinNumber() {
+        System.out.println("당첨 번호를 입력해주세요.");
+    }
+
+    public static void requestLottoBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
+    }
+}

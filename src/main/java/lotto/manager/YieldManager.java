c14be88c00f47f13d4f547df029224be2ab80251
Hoo-sung <gntjd135@gmail.com>
2023-11-04 14:19:05 +0900

    feat(YieldManager): 수익률 계산하는 makeYield 메서드 생성

--- src/main/java/lotto/manager/YieldManager.java
+++ src/main/java/lotto/manager/YieldManager.java
@@ -1,4 +1,19 @@
 package lotto.manager;
 
+import lotto.domain.Award;
+
+import java.util.List;
+
 public class YieldManager {
+
+    public static float makeYield(List<Award> prizes){
+        float beforeMoney = prizes.size() * 1000;
+        float afterMoney = 0;
+        for(Award award: prizes){
+            afterMoney += award.getReward();
+        }
+
+        float yield = ((float)(afterMoney - beforeMoney) / beforeMoney) * 100;//백분율로 계산
+        return Math.round(yield * 10)/ 10.0f;
+    }
 }

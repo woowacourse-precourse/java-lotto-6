22f28474d2c408e4e206b33306ce0ccee1050ea7
jdohun <wjdehgns97@naver.com>
2023-11-04 12:33:22 +0900

    Test: 발행한 Lotto 번호를 오름차순으로 보여주는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,20 @@
 package lotto.view;
 
+import lotto.domain.model.Lotto;
 import lotto.enums.OutputMessage;
+
+import java.util.List;
 
 public class OutputView {
     public static void showCountOfLotto(int countOfLotto) {
         String format = OutputMessage.OUTPUT_COUNT_OF_LOTTO_FORMAT.getMessage();
 
         System.out.println(String.format(format, countOfLotto));
+    }
+
+    public static void showLotteries(List<Lotto> lotteries) {
+        for (Lotto lotto : lotteries) {
+            lotto.showNumbers();
+        }
     }
 }

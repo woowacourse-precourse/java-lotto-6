34ed3f52072ef6cd0f52028a6f79bb627ddca5e8
mksystem98 <mksystem98@gmail.com>
2023-11-04 23:33:08 +0900

    refactor: pickUniqueNumbersInRange()를 사용하도록 변경
    로또 번호 생성을 위해 랜덤값 추출 방법을 pickUniqueNumbersInRange() 로 변경, 인스턴스 변수 numbers를 final로 선언

--- src/main/java/lotto/Model/LottoTicket.java
+++ src/main/java/lotto/Model/LottoTicket.java
@@ -1,6 +1,6 @@
 package lotto.Model;
 
-import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,46 +9,26 @@ public class LottoTicket {
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final int MAXIMUM_LOTTO_NUMBER = 45;
     private static final int MINIMUM_LOTTO_NUMBER = 1;
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public LottoTicket() {
         this.numbers = generateRandomNumbers();
     }
 
     public LottoTicket(List<Integer> numbers) {
-        this.numbers = new ArrayList<>(numbers);
+        this.numbers = numbers;
     }
 
     private List<Integer> generateRandomNumbers() {
-        List<Integer> potentialNumbers = generatePotentialNumbers();
-        shuffleNumbers(potentialNumbers);
-        List<Integer> selectedNumbers = selectNumbers(potentialNumbers);
-        return sortNumbers(selectedNumbers);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER,
+                LOTTO_NUMBER_COUNT);
+        Collections.sort(randomNumbers);
+        return randomNumbers;
     }
 
-    private List<Integer> generatePotentialNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i=MINIMUM_LOTTO_NUMBER; i<= MAXIMUM_LOTTO_NUMBER; i++) {
-            numbers.add(i);
-        }
-        return numbers;
-    }
-
-    private void shuffleNumbers(List<Integer> numbers) {
-        Collections.shuffle(numbers);
-    }
-
-    private List<Integer> selectNumbers(List<Integer> shuffledNumbers) {
-        return new ArrayList<>(shuffledNumbers.subList(0, LOTTO_NUMBER_COUNT));
-    }
-
-    private List<Integer> sortNumbers(List<Integer> numbers) {
-        Collections.sort(numbers);
-        return numbers;
-    }
 
     public List<Integer> getNumbers() {
-        return new ArrayList<>(numbers);
+        return numbers;
     }
 
 }

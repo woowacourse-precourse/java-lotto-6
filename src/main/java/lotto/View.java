ac8ab6513f9578a98f0e1cd4cd73e9f8df5b1de2
heejae <hjhjhjhj3770@naver.com>
2023-11-04 14:04:46 +0900

    Refactor(View): 공통적으로 출력하는 부분을 처리하는 print() 생성

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -2,10 +2,16 @@ package lotto;
 
 import static lotto.settings.Announcement.PURCHASE_AMOUNT_REQUEST;
 
+import lotto.settings.Announcement;
+
 public class View {
 
     public static void requestPurchaseAmount(){
-        System.out.println(PURCHASE_AMOUNT_REQUEST.getPrint());
+        print(PURCHASE_AMOUNT_REQUEST);
+    }
+
+    private static void print(Announcement announcement){
+        System.out.println(announcement.getPrint());
     }
 
 }

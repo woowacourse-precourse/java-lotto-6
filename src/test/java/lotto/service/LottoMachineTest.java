97135953e6ccdc6fa2aa818757b1191f9025e219
doha <dohaahi@naver.com>
2023-11-04 21:26:52 +0900

    test (LottoMachineTest): 로또 발행 메소드 test 작성

---
+++ src/test/java/lotto/service/LottoMachineTest.java
@@ -0,0 +1,31 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.domain.PurchaseAmount;
+import lotto.domain.dto.LottosDto;
+import lotto.domain.dto.PurchaseAmountDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class LottoMachineTest {
+    @Nested
+    @DisplayName("issuedLottos 메소드 test")
+    class IssuedLottos {
+        @DisplayName("발행된 로또 수는 구입 금액을 " + PurchaseAmount.PURCHASE_AMOUNT_UNIT + "으로 나눈 값과 같다")
+        @Test
+        void Issued_lotto_count_is_equal_to_purchase_amount() {
+            // given
+            PurchaseAmountDto purchaseAmountDto = new PurchaseAmountDto(5000);
+            LottoMachine lottoMachine = new LottoMachine();
+
+            // when
+            LottosDto lottosDto = lottoMachine.issuedLottos(purchaseAmountDto);
+
+            // then
+            assertThat(lottosDto.lottos().size()).isEqualTo(
+                    purchaseAmountDto.amount() / PurchaseAmount.PURCHASE_AMOUNT_UNIT);
+        }
+    }
+}

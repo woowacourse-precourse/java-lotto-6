b7ecf205f7e1e19dda19554e1aa3f86a82f26a06
zzun73 <736300@naver.com>
2023-11-05 00:11:57 +0900

    feat: 당첨 로또 번호 클래스 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningLotto {
+    private static final String DELIMITER = ",";
+    private Lotto lotto;
+    private BonusNumber bonusNumber;
+
+
+    public void createLotto(String lotto) {
+        this.lotto = parseLottoNumber(lotto);
+    }
+
+    public void createBonusNumber(String bonusNumber) {
+        this.bonusNumber = new BonusNumber(bonusNumber);
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public BonusNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private static Lotto parseLottoNumber(String lotto) {
+        List<Integer> parseLotto = new ArrayList<>();
+        for (String s : lotto.split(DELIMITER)) {
+            parseLotto.add(Integer.parseInt(s));
+        }
+        return new Lotto(parseLotto);
+    }
+}

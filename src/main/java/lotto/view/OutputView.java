2c40efb5ac626020c7fe12cef191495c33f4ef19
gundorit <packdev937@gmail.com>
2023-11-04 23:57:52 +0900

    feat (InputView) : 구입 금액을 입력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,13 +2,13 @@ package lotto.view;
 
 public class OutputView {
 
-    private static final String LOTTO_PURCHASE_PROMPT = "구입금액을 입력해 주세요.\n";
+    private static final String LOTTO_PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.\n";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.\n";
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.\n";
 
-    public static void printLottoPurchasePrompt() {
-        System.out.print(LOTTO_PURCHASE_PROMPT);
+    public static void printLottoPurchaseAmountPrompt() {
+        System.out.print(LOTTO_PURCHASE_AMOUNT_PROMPT);
     }
 
     public static void printLottoCount(int lottoCount) {

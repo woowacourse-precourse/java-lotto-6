8b0b25939d2e7143681f8e2752364add0287834a
kmw2378 <rlarla677@gmail.com>
2023-11-04 16:48:45 +0900

    feat(lotto): InputErrorCode에 구매 금액 에러 메시지 필드 추가 구매 금액이 1000원 단위가 아닌 경우 포함시킬 예외 메시지 관련 필드를 추가하였습니다.

--- src/main/java/lotto/exception/errorcode/InputErrorCode.java
+++ src/main/java/lotto/exception/errorcode/InputErrorCode.java
@@ -5,7 +5,8 @@ import static lotto.exception.errorcode.ErrorCodeConstants.ERROR_MESSAGE_PREFIX;
 public enum InputErrorCode {
 
     INVALID_LOTTO_NUMBER_RANGE(ERROR_MESSAGE_PREFIX + "로또 번호의 범위는 1 ~ 45 사이여야 합니다."),
-    INVALID_LOTTO_NUMBERS_SIZE(ERROR_MESSAGE_PREFIX + "로또 번호는 총 6개여야 합니다.");
+    INVALID_LOTTO_NUMBERS_SIZE(ERROR_MESSAGE_PREFIX + "로또 번호는 총 6개여야 합니다."),
+    INVALID_LOTTO_PURCHASE_PRICE(ERROR_MESSAGE_PREFIX + "로또 구매 금액은 1,000원 단위여야 합니다.");
 
     private final String message;
 

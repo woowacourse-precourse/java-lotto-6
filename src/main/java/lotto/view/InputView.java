bd00683a8b54957303a5e2c2f742ac432f10d661
yoonseo <choyoonseo@joyunseoui-MacBookAir.local>
2023-11-04 21:32:38 +0900

    feat: 당첨 번호 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,16 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import static constant.MessageList.INPUT_PURCHASE_AMOUNT_MESSAGE;
+import static constant.MessageList.INPUT_WINNING_NUMBER_MESSAGE;
 
 public class InputView {
     public static String getPurchaseAmountInputFromPlayer() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String getWinningNumberInputFromPlayer() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }

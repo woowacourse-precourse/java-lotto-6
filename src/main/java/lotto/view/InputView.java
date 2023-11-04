7d2968257c5a80a513b8fa9c870a2c60f240eec6
ohjuyoungg <ejrm12@naver.com>
2023-11-04 22:38:16 +0900

    feat: 사용자에게 구매 금액을 입력받는 코드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,16 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.validation.InputValidation;
+
+public class InputView {
+    public static int inputPurchaseAmount() {
+        String input = Console.readLine();
+        InputValidation.validatePurchaseAmountIsBlank(input);
+        int purchaseInput = Integer.parseInt(input);
+        InputValidation.validateInputPurchaseAmount(purchaseInput);
+        InputValidation.validateInputString(input);
+        return purchaseInput;
+    }
 }

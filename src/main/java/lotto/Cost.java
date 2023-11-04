3e075a7885a064ac70fee18833a8187508a5ea4d
Yang-soeun <soeun8636@naver.com>
2023-11-04 16:29:25 +0900

    [feat]: 금액으로 수량을 구하는 메소드 구현

--- src/main/java/lotto/Cost.java
+++ src/main/java/lotto/Cost.java
@@ -1,15 +1,20 @@
 package lotto;
 
-import lotto.view.InputView;
+import lotto.exception.InvalidInput;
 
 public class Cost {
     private final int cost;
+    private static final int UNIT = 10000;
     public Cost(int cost) {
         validate(cost);
         this.cost = cost;
     }
+    public int calculateQuantity(){
+        return cost / UNIT;
+    }
 
     private void validate(int cost){
-        InputView inputView = new InputView();
+        InvalidInput invalidInput = new InvalidInput();
+        invalidInput.notThousandUnitException(cost);
     }
 }

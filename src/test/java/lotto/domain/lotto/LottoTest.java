e38386fbc20baa210fca2e12abcef97acfcad923
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 06:02:39 +0900

    test(LottoTest) : 보너스번호를 가지고 있는지 여부 hasSameNumber 기능 테스트

--- src/test/java/lotto/domain/lotto/LottoTest.java
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain.lotto;
 
 import lotto.domain.lotto.Lotto;
 import org.junit.jupiter.api.Assertions;
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -27,12 +29,14 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
     @Test
     @DisplayName("로또 번호가 1에서 45사이가 아니라면 예외가 발생한다.")
     void 로또번호_예외_발생_테스트() {
         assertThatThrownBy(() -> new Lotto(List.of(14, 46, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
     @Test
     @DisplayName("로또 번호가 중복되지 않고 1에서 45사이의 숫자 6개라면 예외 없이 생성된다.")
     void 로또_생성_테스트() {
@@ -46,5 +50,18 @@ class LottoTest {
         Assertions.assertNotNull(lotto);
         Assertions.assertDoesNotThrow(() -> new Lotto(numbers));
         assertThat(lotto.getNumbers()).isEqualTo(numbers);
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호 가지고 있는지 여부 테스트")
+    @CsvSource(value = {"1,true", "7,false"})
+    void 보너스번호(int number, boolean expected) {
+        // given
+        BonusNumber bonusNumber = new BonusNumber(number);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        // when
+        boolean hasSameNumber = lotto.hasSameNumber(bonusNumber);
+
+        // then
     }
 }

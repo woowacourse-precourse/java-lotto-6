9fbe3e04a6995bdc30004d2952391354ac9148f9
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 23:15:16 +0900

    feat: controller에 도메인 변수 선언

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,6 +1,16 @@
 package controller;
 
+import domain.Lotto;
+import domain.LottoNumberGenerator;
+import domain.Price;
+import domain.repository.LottoRepository;
+import view.InputView;
+
 public class LottoController {
+
+    private Price price;
+    private LottoRepository lottoRepository;
+    private LottoNumberGenerator lottoNumberGenerator;
 
     public void run() {
 

2c60ef5e93e5e546df4134785169d5cab6d4a5e8
maark1106 <mark1106@naver.com>
2023-11-04 22:38:58 +0900

    feat : lotto number가 1~45 범위 내에 있는지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,10 +14,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateLottoNumberCount(numbers);
-        validateUniqueNumberValidate(numbers);
+        validateUniqueNumber(numbers);
+        validateCorrectRange(numbers);
     }
 
-    private void validateUniqueNumberValidate(List<Integer> numbers) {
+    private void validateCorrectRange(List<Integer> numbers) {
+        boolean isValid = numbers.stream()
+                .allMatch(number -> number >= 1 && number <= 45);
+
+        if (!isValid) {
+            throw new IllegalArgumentException("[ERROR] : 숫자가 1에서 45 사이의 범위를 벗어납니다.");
+        }
+    }
+
+    private void validateUniqueNumber(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>();
         for (Integer number : numbers) {
             if (!uniqueNumbers.add(number)) {

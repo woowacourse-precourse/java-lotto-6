cbe833132ba1e2e7f018be837bb1d2453939b9b4
rakyun1 <dbskim234@naver.com>
2023-11-04 03:46:15 -0600

    Feat: add checkRank() 당첨 번호와 보너스 번호를 받아 Lotto클라스 안에 있는 로또 번호와 비교하고 순위를 리턴

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,45 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public RankEnum checkRank(int bonus, int[] winnerNumber){
+        int count = 0;
+        for (int lottoNumber : numbers) {
+            if (Arrays.binarySearch(winnerNumber, lottoNumber) >= 0) {
+                count++;
+            }
+        }
+        if (count == 6){
+            return RankEnum.FIRST;
+        }
+        if (count == 5){
+            if (Arrays.binarySearch(winnerNumber, bonus) >= 0){
+                return RankEnum.SECOND;
+            }
+            return RankEnum.THIRD;
+        }
+        if (count == 4){
+            return RankEnum.FORTH;
+        }
+        if (count == 3){
+            return RankEnum.FIFTH;
+        }
+        return RankEnum.NONE;
+    }
+
 }

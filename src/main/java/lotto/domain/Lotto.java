2dcf4de1bba78d9f2fd53b7b9314f4b1c77c0502
sotogito <108535526+sotogito@users.noreply.github.com>
2023-11-04 22:58:32 +0900

    docs : 유효처리-메인 랜덤 번호 업데이트

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,7 +9,7 @@ public class Lotto {
     private static final int MAX = 45;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers); //money는 input일때 단 한번만 객체르 생성하지만 lotto는 입력일때, 랜덤생성될때 여러 방면에서 객체가 생성되므로 생성자에서 유효검사를 하는게 좋다
+        validate(numbers);
         this.numbers = numbers;
     }
 

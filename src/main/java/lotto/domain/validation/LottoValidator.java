fd8de54c8470cb5f238dc52d6e72da83e93557d4
LeeJiWon <dlwldnjs1009@gmail.com>
2023-11-04 19:31:57 +0900

    feat: 보너스 번호 검증 추가

--- src/main/java/lotto/domain/validation/LottoValidator.java
+++ src/main/java/lotto/domain/validation/LottoValidator.java
@@ -17,4 +17,21 @@ public class LottoValidator {
             throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
+
+    public static void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 볼은 1에서 45 사이의 숫자여야 합니다.");
+        }
+
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public static void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 볼은 1에서 45 사이의 숫자여야 합니다.");
+        }
+        // 당첨 번호 리스트와의 중복 검사는 생략
+    }
 }

6208503fb5c3bcb0c74214a1483b48fa9f21b992
Minjae-An <mj3242@naver.com>
2023-11-04 14:13:26 +0900

    feat : 예외 메시지 빌더 생성 제한

--- src/main/java/lotto/exception/ExceptionMessageBuilder.java
+++ src/main/java/lotto/exception/ExceptionMessageBuilder.java
@@ -3,6 +3,8 @@ package lotto.exception;
 import static lotto.exception.ExceptionMessage.*;
 
 public final class ExceptionMessageBuilder {
+    private ExceptionMessageBuilder(){}
+
     public static String build(ExceptionMessage exceptionMessage){
         return String.join(" ", ERROR_LOG.getMessage(), exceptionMessage.getMessage());
     }

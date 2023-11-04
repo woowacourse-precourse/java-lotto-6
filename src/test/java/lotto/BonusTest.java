f89b4610ec6e1c3cad5783e05046b54d7663ad86
kim <hwiyul_g@daum.net>
2023-11-04 14:10:05 +0900

    Test(Bonus) : 보너스 번호 범위 예외 테스트 케이스 추가

---
+++ src/test/java/lotto/BonusTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BonusTest {
+
+    @DisplayName("보너스 번호에 1~45 숫자 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createBonusByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Bonus(0))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}

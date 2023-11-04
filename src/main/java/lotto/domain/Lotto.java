8e647d935e984f0fa2ff39698a8922029118bfae
gumgu <bienlee@naver.com>
2023-11-04 20:33:20 +0900

    [Refactor] WinLotto가 Lotto를 상속받도록 변경 (검증로직 삭제)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
-    // TODO: 인스턴스 변수 추가 불가.
-    // TODO: 패키지 변경 가능.
+    private static final Integer LOTTO_NUMBERS_COUNT = 6;
+    private static final Integer MINIMUM_LOTTO_RANGE = 1;
+    private static final Integer MAXIMUM_LOTTO_RANGE = 45;
+
+    // TODO: 인스턴스 변수 추가 불가 X.
+    // TODO: 패키지 변경 가능 O.
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,11 +19,31 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    // TODO: 접근제한자 private 변경 불가.
+    // TODO: 접근제한자 private 변경 불가 X.
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!(isMatchedCount(numbers) && isInNumberRange(numbers) && hasNotDuplicates(numbers))) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isMatchedCount(List<Integer> numbers) {
+        return numbers.size() == LOTTO_NUMBERS_COUNT;
+    }
+
+    private boolean isInNumberRange(List<Integer> numbers) {
+        return numbers.stream()
+                .allMatch(number ->
+                        (number >= MINIMUM_LOTTO_RANGE && number <= MAXIMUM_LOTTO_RANGE));
+    }
+
+    private boolean hasNotDuplicates(List<Integer> numbers){
+        return numbers.size() == Set.of(numbers).size();
+    }
+
+
+    // todo: OUTPUT에서 사용. 다른 방법 찾아보기.
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
     }
 
 }
--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -2,40 +2,20 @@ package lotto.domain;
 
 import java.util.List;
 
-public class WinLotto {
+public class WinLotto extends Lotto {
 
-    //todo: condition 묶어서 관리?
-    private static final Integer LOTTO_COUNT = 6;
-    private static final Integer MINIMUM_LOTTO_RANGE = 1;
-    private static final Integer MAXIMUM_LOTTO_RANGE = 45;
-
-    private final List<Integer> numbers;
     private final Integer bonus;
 
     public WinLotto(List<Integer> numbers, Integer bonus) {
-        validate(numbers, bonus);
-        this.numbers = numbers;
+        super(numbers);
+        validateNumber(numbers, bonus);
         this.bonus = bonus;
     }
 
-    private void validate(List<Integer> numbers, Integer bonus) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-
+    private void validateNumber(List<Integer> numbers, Integer bonus) {
         if (numbers.contains(bonus)) {
             throw new IllegalArgumentException();
         }
-
-        if (numbers.stream().allMatch(number -> (number >= MINIMUM_LOTTO_RANGE && number <=45))) {
-            throw new IllegalArgumentException();
-        }
-
-        //todo: 중복값 검증
     }
-
-
-
-
 
 }

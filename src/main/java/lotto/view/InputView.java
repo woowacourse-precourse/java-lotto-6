699233b87a5f177f0a0653a537b125a5782942b6
ParkDH0809 <znxn1237@gmail.com>
2023-11-05 01:02:37 +0900

    fix: 예시에 맞도록 콘솔 출력 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
     public String inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
@@ -17,7 +17,7 @@ public class InputView {
     }
 
     public String inputBonusNumber() {
-        System.out.printf(INPUT_BONUS_NUMBER);
+        System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 }

0fa396ac074c717cf14e606e8bdf14f2f0507e51
tam31 <wxogud3377@naver.com>
2023-11-05 03:30:21 +0900

    Feat(InputValidation) : 숫자 판별 검사 기능 구현 Docs : 숫자 판별 검사 완료 체크

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -4,14 +4,24 @@ public class InputValidation {
 
     private static final String ERROR = "[ERROR] ";
     private static final String IS_NULL_MESSAGE = "빈 문자열 입니다";
+    private static final String IS_NOT_NUMBER_MESSAGE = "숫자가 아닙니다";
 
     public void validationMoney(String input){
         isEmptyValidation(input);
+        isNumberValidation(input);
     }
 
     private void isEmptyValidation(String input) {
         if(input.isBlank()){
             throw new IllegalArgumentException(ERROR + IS_NULL_MESSAGE);
+        }
+    }
+
+    private void isNumberValidation(String input){
+        for(int i = 0; i<input.length() ; i++){
+            if(!Character.isDigit(input.charAt(i))){
+                throw new IllegalArgumentException(ERROR + IS_NOT_NUMBER_MESSAGE);
+            }
         }
     }
 }

7d82850e0ec81c1579d498cebba48bd877e48af1
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 05:10:25 +0900

    feat: 로또 상점 서비스 구현

---
+++ src/main/java/lotto/service/LottoStoreService.java
@@ -0,0 +1,23 @@
+package lotto.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
+
+public class LottoStoreService {
+
+    private final LottoGeneratorService lottoGeneratorService;
+
+    public LottoStoreService(LottoGeneratorService lottoGeneratorService) {
+        this.lottoGeneratorService = lottoGeneratorService;
+    }
+
+    public List<Lotto> purchaseLotto(PurchaseAmount purchaseAmount) {
+        int count = purchaseAmount.affordableCountOfLotto();
+        return IntStream.range(0, count)
+                .mapToObj(i -> lottoGeneratorService.generateLotto())
+                .collect(Collectors.toList());
+    }
+}

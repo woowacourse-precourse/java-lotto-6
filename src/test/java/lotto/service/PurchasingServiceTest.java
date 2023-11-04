ef0e6104ba514c411dfb514e1bf88da12b69a1a2
Gyutae Kim <kiimwf0819@gmail.com>
2023-11-05 02:52:04 +0900

    test: 테스트 코드의 메소드명 수정

--- src/test/java/lotto/service/PurchasingServiceTest.java
+++ src/test/java/lotto/service/PurchasingServiceTest.java
@@ -13,7 +13,7 @@ class PurchasingServiceTest {
 
     @DisplayName("로또 구매 금액이 숫자가 아니면 예외가 발생한다.")
     @Test
-    void createBuyerByNotNumber() {
+    void purchaseLottoBuyerByNotNumber() {
         assertThatThrownBy(() -> purchasingService.purchaseLotto("천원"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 숫자만 입력 가능합니다.");
@@ -21,7 +21,7 @@ class PurchasingServiceTest {
 
     @DisplayName("로또 구매 금액이 1,000원 보다 적으면 예외가 발생한다.")
     @Test
-    void createBuyerByUnderAmount() {
+    void purchaseLottoByUnderAmount() {
         assertThatThrownBy(() -> purchasingService.purchaseLotto("999"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또를 한 장 이상 구매해야 합니다. (로또 1장 1,000원)");
@@ -29,7 +29,7 @@ class PurchasingServiceTest {
 
     @DisplayName("로또 구매 금액의 단위가 1,000원이 아니면 예외가 발생한다.")
     @Test
-    void createBuyerByWrongUnit() {
+    void purchaseLottoByWrongUnit() {
         assertThatThrownBy(() -> purchasingService.purchaseLotto("1500"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또는 1,000원 단위로 구매 가능합니다. (로또 1장 1,000원)");

fd3c16cc7b4559d8be61bb8f42d53dfdd0a83147
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 17:55:59 +0900

    style: 변수명 변경

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -1,10 +1,12 @@
 package util;
 
 public enum ErrorMessage {
+
     ERROR("[ERROR]"),
-    AMOUNT_ISNT_1000WON("구매 금액이 천원 단위가 아닙니다. 구매 금액은 천원 단원로 입력해주세요."),
+    PRICE_ISNT_MULTIPLE_OF_THOUSAND_WON("구매 금액이 천원 단위가 아닙니다. 구매 금액은 천원 단원로 입력해주세요."),
     LOTTO_CONTAIN_DULPICATE_NUMBER("로또 번호는 중복되지 않아야 합니다. 중복되지 않은 숫자를 입력해주세요."),
     OUT_OF_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
     String errorMessage;
 
     ErrorMessage(String errorMessage) {

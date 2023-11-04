91bf07d30b6cfb6b1d0ce2a95c0f698d726cf181
gonixxxx <akdntm946@gmail.com>
2023-11-05 01:24:48 +0900

    feat: add exception to validate bonus number is in lotto number

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -13,17 +13,18 @@ public class InputValidator {
     private static final String INVALID_RANGE = "1부터 45까지의 번호만 입력해주세요.";
     private static final String DUPLICATED = "중복된 숫자가 존재합니다.";
     private static final String INVALID_LOTTO_FORMAT = "숫자와 쉼표를 이용해 입력해주세요.";
+    private static final String INVALID_BONUS_NUMBER = "입력한 당첨 번호 중 보너스 번호가 이미 존재합니다.";
 
     private static final int PRICE = 1000;
     private static final int SIZE = 6;
     private static final int MIN_NUM = 1;
     private static final int MAX_NUM = 45;
 
-    private static final Pattern NOT_NUMBER_PATTERN = Pattern.compile("^[1-9]*$");
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[1-9]*$");
     private static final Pattern INVALID_LOTTO_PATTERN = Pattern.compile("(\\d*,){5}\\d*$");
 
     public static void checkIsNumber(String input) {
-        if (!NOT_NUMBER_PATTERN.matcher(input).matches()) {
+        if (!NUMBER_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException(ERROR + NOT_NUMBER);
         }
     }
@@ -43,7 +44,13 @@ public class InputValidator {
     public static void checkRange(String input) {
         int number = Integer.parseInt(input);
         if (number < MIN_NUM || number > MAX_NUM) {
-            throw new IllegalArgumentException(INVALID_RANGE);
+            throw new IllegalArgumentException(ERROR + INVALID_RANGE);
+        }
+    }
+
+    public static void checkBonusNumberInLotto(List<Integer> lotto, int bonusNumber) {
+        if (lotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR + INVALID_BONUS_NUMBER);
         }
     }
 

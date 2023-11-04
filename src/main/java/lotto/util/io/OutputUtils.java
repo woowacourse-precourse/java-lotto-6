7c23f5830671dac38e7c1fffc51b79ebebefd24d
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 18:17:19 +0900

    [feat] 로또 구매 개수 출력 기능 구현

--- src/main/java/lotto/util/io/OutputUtils.java
+++ src/main/java/lotto/util/io/OutputUtils.java
@@ -2,8 +2,13 @@ package lotto.util.io;
 
 public class OutputUtils {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String NUMBER_OF_PURCHASE_MESSAGE = "%d개를 구매했습니다.";
 
     public static void printPurchaseAmountInputMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+    }
+
+    public static void printNumberOfPurchaseMessage(int number) {
+        System.out.println(String.format(NUMBER_OF_PURCHASE_MESSAGE, number));
     }
 }

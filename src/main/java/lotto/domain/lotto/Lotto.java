4216f55ac2cfc8c938ee44b86d29f3f383af8c1f
minisyu <iswater.kim@gmail.com>
2023-11-05 00:49:26 +0900

    refactor: 예외 메시지 변경, getter 로직 변경, 오름차순 정렬 메소드 추가

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,6 +1,5 @@
 package lotto.domain.lotto;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -12,7 +11,8 @@ public class Lotto {
     Lotto(List<Integer> numbers) {
         verifyNumbersSize(numbers);
         verifyNoDuplication(numbers);
-        this.numbers = numbers;
+        verifyLottoNumberRange(numbers);
+        this.numbers = sort(numbers);
     }
 
     public static Lotto from(List<Integer> numbers) {
@@ -21,14 +21,14 @@ public class Lotto {
 
     private void verifyNumbersSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBERS) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 " + LOTTO_NUMBERS + "개여야 합니다.");
         }
     }
 
     private void verifyNoDuplication(List<Integer> numbers) {
         int uniqueCount = getUniqueCount(numbers);
         if (uniqueCount != numbers.size()) {
-            throw new IllegalArgumentException("중복되는 숫자가 있으면 안 됩니다.");
+            throw new IllegalArgumentException("로또 번호에 중복되는 숫자가 있으면 안 됩니다.");
         }
     }
 
@@ -38,7 +38,7 @@ public class Lotto {
 
     private void verifyLottoNumberRange(List<Integer> numbers) {
         if (isOutOfRange(numbers)) {
-            throw new IllegalArgumentException("1에서 45 사이의 숫자만 가능합니다.");
+            throw new IllegalArgumentException("로또 번호는 " + MIN_NUMBER + "에서 " + MAX_NUMBER + "사이의 숫자만 가능합니다.");
         }
     }
 
@@ -46,8 +46,14 @@ public class Lotto {
         return numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
     }
 
-    public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(numbers);
+    private List<Integer> sort(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return List.copyOf(numbers);
     }
 }
 

caedf91ba66de6402f2b87ab3f1db8a4f6533558
Jiwon Na <jiwonna52@gmail.com>
2023-11-04 11:09:39 +0900

    Test : 로또 번호 오름차순 정렬 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +26,18 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호를 입력하면 오름차순으로 정렬이 된다.")
+    @Test
+    void sortLottoNumberASCETest() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(2);
+        lottoNumbers.add(3);
+        lottoNumbers.add(4);
+        lottoNumbers.add(5);
+        lottoNumbers.add(6);
+        Lotto lotto = new Lotto(lottoNumbers);
+        assertThat(List.of(1, 2, 3, 4, 5, 6)).isEqualTo(lotto.getLotto());
+    }
+
 }

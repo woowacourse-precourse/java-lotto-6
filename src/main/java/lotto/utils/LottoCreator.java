c01b866c22b3e17aab7be92e339ffea080ffe223
whxogus215 <whxogus215@naver.com>
2023-11-04 10:25:32 +0900

    feat(LottoCreator): 생성된 로또 리스트를 숫자 리스트로 반환

--- src/main/java/lotto/utils/LottoCreator.java
+++ src/main/java/lotto/utils/LottoCreator.java
@@ -3,15 +3,23 @@ package lotto.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.Lotto;
 
 public class LottoCreator {
-    private List<Lotto> lottos = new ArrayList<>();
+    private final List<Lotto> lottos = new ArrayList<>();
+
     public void createLotto(int lottoCount) {
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> randomNumbers
                     = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottos.add(new Lotto(randomNumbers));
         }
+    }
+
+    public List<List<Integer>> getCreatedLottos() {
+        return lottos.stream()
+                .map(Lotto::getLottoNumber)
+                .collect(Collectors.toList());
     }
 }

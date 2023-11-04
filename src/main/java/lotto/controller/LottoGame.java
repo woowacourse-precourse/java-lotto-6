1970105aa7e99cf8911ec26e370517f6c8fba93a
Isaac Han <isaachan233@gmail.com>
2023-11-04 13:08:37 +0900

    feat: 로또 구입 금액에 대한 입출력 메소드 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,11 +1,22 @@
 package lotto.controller;
 
-public class LottoGame implements Game{
+import lotto.domain.LottoPurchase;
+import lotto.view.InputView;
 
+public class LottoGame implements Game {
+
+    private LottoPurchase lottoPurchase;
     @Override
     public void run() {
-
+        collectPurchaseAmount();
     }
 
-
+    private void collectPurchaseAmount () {
+        try {
+            lottoPurchase = new LottoPurchase(InputView.lottoPurchaseAmountInput());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            collectPurchaseAmount();
+        }
+    }
 }

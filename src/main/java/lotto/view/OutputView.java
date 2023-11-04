fe38850b9b559864a6e179ca7513dc72e0fb2abd
NahyeChoi <skgp0628@naver.com>
2023-11-05 00:33:35 +0900

    feat(lotto):사용자의 입력을 위한 안내 문구 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,15 @@
 package lotto.view;
 
 public class OutputView {
-
+	public void printPurchaseAmount() {
+		System.out.println("구입금액을 입력해 주세요.");
+	}
+	
+	public void printWinningNumbers() {
+		System.out.println("당첨 번호를 입력해 주세요.");
+	}
+	
+	public void printBonusNumber() {
+		System.out.println("보너스 번호를 입력해 주세요.");
+	}
 }

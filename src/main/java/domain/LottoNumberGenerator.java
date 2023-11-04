cff55e68a5ce253407bd4f8016551154c09289f2
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 23:14:26 +0900

    feat: 로또 번호 생성 메서드 추가

--- src/main/java/domain/LottoNumberGenerator.java
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -1,5 +1,17 @@
 package domain;
 
-public class LottoNumberGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
+import util.ConstantOfLotto;
+
+public class LottoNumberGenerator {
+    private static final int minimumNumber = ConstantOfLotto.minimumOfLottoNumber;
+    private static final int maximumNumber = ConstantOfLotto.maximumOfLottoNumber;
+    private static final int numOfLottoNumber = ConstantOfLotto.numberOfLottoNumbers;
+
+    public List<Integer> createLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(minimumNumber,maximumNumber,numOfLottoNumber);
+    }
 }

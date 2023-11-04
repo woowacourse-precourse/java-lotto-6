d9a697ae7099db1f7daebbc8f5264ce86b26480a
psionic0324 <psionic0324@naver.com>
2023-11-04 22:45:35 +0900

    feat : 당첨번호를 저장하는 클래스 추가
    당첨번호와 보너스번호를 저장하는 WinningLotto클래스 생성

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningLotto() {
+        return
+    }
+}

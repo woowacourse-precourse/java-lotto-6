328504d4a4b2acd0748c7700d95c3e24db52da37
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-05 04:12:00 +0900

    feat:String Util 기능 추가

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -1,15 +1,23 @@
 package lotto.util;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringUtil {
+    private static final String DELIMITER = ",";
+    private static final String DELIMITER_WITH_SPACE = ", ";
 
-    static final String DELIMITER = ",";
     public static List<String> splitWithComma(String input) {
         return List.of(input.split(DELIMITER));
     }
 
     public static int convertStringToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static String convertListToString(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(DELIMITER_WITH_SPACE));
     }
 }

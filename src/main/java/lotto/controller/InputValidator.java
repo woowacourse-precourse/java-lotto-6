316944ae6ccd56aea310a8d9f61ee5a6c27393e7
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-11-05 02:11:22 +0900

    test(InputValidator): 보너스 번호 유효성 검사 코드 추가 - 보너스 번호가 당첨 번호와 중복 되는지 확인 - 보너스 번호가 숫자인지 확인 - 보너스 번호가 1-45 범위인지 확인 - 올바른 입력일 때 예외를 던지지 않는지 확인

--- src/main/java/lotto/controller/InputValidator.java
+++ src/main/java/lotto/controller/InputValidator.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputValidator {
+    String winningNumbers = "123456";
+
     @Test
     @DisplayName("구입 금액이 숫자가 아닐 때 예외를 던진다")
     void validatePurchaseAmount_NotANumber_ThrowsException() {
@@ -19,7 +21,7 @@ class InputValidator {
 
     @Test
     @DisplayName("구입 금액이 정수 범위가 아닐 때 예외를 던진다")
-    void validatePurchaseAmount_NotANumber_ThrowsException() {
+    void validatePurchaseAmount_NotAIntRange_ThrowsException() {
         String input = "1234567890";
 
         assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
@@ -58,7 +60,7 @@ class InputValidator {
 
     @Test
     @DisplayName("당첨 번호가 쉼표로 구분되지 않았을 때 예외를 던진다")
-    void validateWinningNumbers_InvalidCharacters_ThrowsException() {
+    void validateWinningNumbers_SeparateComma_ThrowsException() {
         String input = "1;2;3;4;5;6";
 
         assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
@@ -68,7 +70,7 @@ class InputValidator {
 
     @Test
     @DisplayName("당첨 번호가 숫자로 구성되어 있지 않았을 때 예외를 던진다")
-    void validateWinningNumbers_InvalidCharacters_ThrowsException() {
+    void validateWinningNumbers_NotANumber_ThrowsException() {
         String input = "1,2,3,4,5,a";
 
         assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
@@ -78,7 +80,7 @@ class InputValidator {
 
     @Test
     @DisplayName("당첨 번호가 6개의 숫자로 이루어지지 않았을 때 예외를 던진다")
-    void validateWinningNumbers_InvalidCharacters_ThrowsException() {
+    void validateWinningNumbers_NoLength6_ThrowsException() {
         String input = "1,2,3,4,5";
 
         assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
@@ -87,8 +89,8 @@ class InputValidator {
     }
 
     @Test
-    @DisplayName("당첨 번호 입력에 쉼표가 중복되었을 때 예외를 던진다")
-    void validateWinningNumbers_InvalidCharacters_ThrowsException() {
+    @DisplayName("당첨 번호 입력에 쉼표가 연속되었을 때 예외를 던진다")
+    void validateWinningNumbers_InputContainsConsecutiveComma_ThrowsException() {
         String input = "1,,2,3,4,5,6";
 
         assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
@@ -118,7 +120,7 @@ class InputValidator {
 
     @Test
     @DisplayName("당첨 번호가 정수 허용값을 넘어섰을 때 예외를 던진다")
-    void validateWinningNumbers_InputEndsWithComma_ThrowsException() {
+    void validateWinningNumbers_NotAIntRange_ThrowsException() {
         String input = "1,2,3,4,5,1234567890";
 
         assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
@@ -132,6 +134,45 @@ class InputValidator {
         String input = "1,2,3,4,5,6";
 
         assertThatCode(() -> InputValidator.validateWinningNumbers(input))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복될 때 예외를 던진다")
+    void validateBonusNumber_DuplicateWithWinningNumber_ThrowsException() {
+        String bonusNumber = "6";
+
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(bonusNumber, winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호는 당첨 번호와 달라야 합니다.");
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 숫자가 아닐 때 예외를 던진다")
+    void validatePurchaseAmount_NotANumber_ThrowsException() {
+        String bonusNumber = "abc";
+
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(bonusNumber, winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값은 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 1-45 범위가 아닐 때 예외를 던진다")
+    void validatePurchaseAmount_OverRange_ThrowsException() {
+        String bonusNumber = "47";
+
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(bonusNumber, winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호는 1-45 내여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("올바른 보너스 번호 입력일 때, 예외를 던지지 않는다.")
+    void validateCarNamesInput_InputIsValid_DoesNotThrowException() {
+        String bonusNumber = "7";
+
+        assertThatCode(() -> InputValidator.validateBonusNumber(bonusNumber, winningNumbers))
                 .doesNotThrowAnyException();
     }
 }

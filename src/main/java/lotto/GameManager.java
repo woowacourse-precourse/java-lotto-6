66c2a1e1ced64f687c00d5ba6df2d14be22c0c79
이준희 <juni416@naver.com>
2023-11-04 09:53:00 +0900

    feature: 구매할 로또의 금액이 숫자로만 이뤄지지 않는 경우 예외 처리

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -1,12 +1,33 @@
 package lotto;
 
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class GameManager {
-    public int getPaymentForLottoByRead(String readLine) {
-        int price = Integer.parseInt(readLine);
-        assertPaymentForLotto(price);
-        int numberOfLotto = price / 1000;
-        return numberOfLotto;
+
+    public int getPaymentForLottoByRead() {
+        int lottoAmount = 0;
+        while (true) {
+            if (lottoAmount >= 1000) break;
+            try {
+                lottoAmount = getPaymentForLottoByRead(readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return lottoAmount;
+    }
+
+    protected int getPaymentForLottoByRead(String readLine) {
+
+        try {
+            int price = Integer.parseInt(readLine);
+            assertPaymentForLotto(price);
+            int numberOfLotto = price / 1000;
+            return numberOfLotto;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자로 입력해주세요.");
+        }
     }
 
     private void assertPaymentForLotto(int price) {
--- src/test/java/lotto/GameManagerTest.java
+++ src/test/java/lotto/GameManagerTest.java
@@ -3,7 +3,7 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class GameManagerTest {
 
@@ -15,10 +15,20 @@ public class GameManagerTest {
     }
 
     @Test
-    void 입력받은_로또_번호가_천원으로_나누어_떨어지지_않는_경우_예외_발생(){
+    void 입력받은_금액이_천원으로_나누어_떨어지지_않는_경우_예외_발생(){
         // Arrange
         String payment = "1200";
         // Action, Action
-        assertThrows(IllegalArgumentException.class, () -> gameManager.getPaymentForLottoByRead(payment));
+        assertThatThrownBy(() -> gameManager.getPaymentForLottoByRead(payment))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력받은_금액이_숫자로_이뤄지지않은경우_예외발생(){
+        // Arrange
+        String payment = "100f";
+        // Action, Action
+        assertThatThrownBy(() -> gameManager.getPaymentForLottoByRead(payment))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

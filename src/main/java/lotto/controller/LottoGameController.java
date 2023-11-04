a470fc1f0f7fe7fe58d0e8710085b0c2b7bd5cde
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 03:58:47 +0900

    feat : 로또게임에 대한 컨트롤러 생성

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,19 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        
+    }
+}

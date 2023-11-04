73f2d62e49916cebee96f641ea88f38c2fc86a4d
kchranger <kchranger@gmail.com>
2023-11-04 23:59:51 +0900

    feature(lotto) : issue one lotto

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,6 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class LottoGame {
+
+    private static final int LOTTO_MINIMAL_NUMBER = 1;
+    private static final int LOTTO_MAXIMUM_NUMBER = 45;
+    private static final int COUNT_OF_UNIQUE_NUMBERS = 6;
 
     public LottoGame(){
     }
@@ -11,5 +17,11 @@ public class LottoGame {
             GuideMessage.ofInputMoney();
             money = GameInput.insertMoney();
         }
+        Lotto lotto = issueLotto();
+    }
+
+    public Lotto issueLotto(){
+        return new Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_MINIMAL_NUMBER,
+                LOTTO_MAXIMUM_NUMBER,COUNT_OF_UNIQUE_NUMBERS));
     }
 }

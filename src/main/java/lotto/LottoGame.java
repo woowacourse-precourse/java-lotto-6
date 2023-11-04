bd64a1f3956bba29a8e6a93e4a8983b9dfb7a831
Sunhee Kim <sunni372@icloud.com>
2023-11-05 01:20:43 +0900

    feat: 로또 발행 기능 구현
    구입 금액에 해당하는 개수의 로또를 발행합니다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,12 +1,30 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGame {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final int LOTTO_PRICE = 1000;
+
+    private final List<Lotto> lottos = new ArrayList<>();
 
     public void start() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         int purchaseAmount = Integer.parseInt(Console.readLine());
+
+        int purchasedLottosNumber = purchaseAmount / LOTTO_PRICE;
+        makeLottos(purchasedLottosNumber);
+    }
+
+    private void makeLottos(int lottosNumber) {
+        for (int i = 0; i < lottosNumber; i++) {
+            addLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        }
+    }
+
+    private void addLotto(Lotto lotto) {
+        this.lottos.add(lotto);
     }
 }

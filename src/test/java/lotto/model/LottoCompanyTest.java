4edc89f425c5862293b85248d3a951978c03dc33
devholic22 <hyunjoon.tech@gmail.com>
2023-11-05 00:38:14 +0900

    test: LottoCompany 로또 계산 테스트
    LottoCompany에서 5등 (보너스 미포함), 2등 (보너스 포함) 메서드 테스트

--- src/test/java/lotto/model/LottoCompanyTest.java
+++ src/test/java/lotto/model/LottoCompanyTest.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
+import static lotto.model.LottoConstant.*;
 import static lotto.view.exception.InputException.BONUS_NUMBER_ALREADY_USE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 public class LottoCompanyTest {
 
@@ -31,5 +34,48 @@ public class LottoCompanyTest {
         assertThatThrownBy(() -> LottoCompany.of(goalNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(BONUS_NUMBER_ALREADY_USE.getMessage());
+    }
+
+    @Test
+    @DisplayName("로또 계산 테스트 - 5등 갯수 계산")
+    void calculateMatchLottoTest() {
+        // given
+        List<Lotto> lottos = List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+            new Lotto(List.of(7, 8, 9, 10, 11, 12)),
+            new Lotto(List.of(13, 14, 15, 16, 17, 18))
+        );
+        GoalNumbers goalNumbers = GoalNumbers.from("1,2,3,10,11,12");
+        BonusNumber bonusNumber = BonusNumber.from("14");
+        LottoCompany lottoCompany = LottoCompany.of(goalNumbers, bonusNumber);
+        int expectedMatchLottos = 2;
+
+        // when
+        List<Lotto> matchedLottos = lottoCompany.collectLottosWithSizeExceptBonus(lottos, FIFTH_PRIZE_MATCH.getValue());
+
+        // then
+        assertThat(matchedLottos.size()).isEqualTo(expectedMatchLottos);
+    }
+
+    @Test
+    @DisplayName("로또 계산 테스트 - 2등 갯수 계산")
+    void calculateMatchLottoWithBonusTest() {
+        // given
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(7, 8, 9, 10, 11, 12)),
+                new Lotto(List.of(13, 14, 15, 16, 17, 18))
+        );
+        GoalNumbers goalNumbers = GoalNumbers.from("1,2,3,4,5,12");
+        BonusNumber bonusNumber = BonusNumber.from("6");
+        LottoCompany lottoCompany = LottoCompany.of(goalNumbers, bonusNumber);
+        int expectedMatchLottos = 1;
+
+        // when
+        List<Lotto> matchedLottos = lottoCompany.collectLottosWithSizeIncludeBonus(lottos,
+                SECOND_PRIZE_MATCH.getValue());
+
+        // then
+        assertThat(matchedLottos.size()).isEqualTo(expectedMatchLottos);
     }
 }

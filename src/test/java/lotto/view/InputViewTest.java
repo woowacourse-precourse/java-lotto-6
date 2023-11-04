042dd10c8a84888d965c7032a3c81398f60c66d6
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 22:16:10 +0900

    [test] 입출력 테스트를 위한 추상 클래스 정의

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import lotto.domain.Cash;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("입력 처리")
+class InputViewTest {
+
+}

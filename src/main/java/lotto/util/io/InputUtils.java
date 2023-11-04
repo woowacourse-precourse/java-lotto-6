7f87c1041c2076895a6e5ea2a60e087f38db718c
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 21:27:58 +0900

    [add] 당첨 번호 숫자가 6개가 아닐때 에러 처리 추가

--- src/main/java/lotto/util/io/InputUtils.java
+++ src/main/java/lotto/util/io/InputUtils.java
@@ -9,6 +9,9 @@ import java.util.Set;
 import lotto.util.StringUtils;
 
 public class InputUtils {
+
+    private static final int COUNT_OF_WINNING_NUMBER = 6;
+
     public static int getPurchaseAmount() {
         String money = Console.readLine();
         validateInputMoney(money);
@@ -51,6 +54,9 @@ public class InputUtils {
             if (!isNumberInRange(Integer.parseInt(number))) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+        }
+        if (numbers.size() != COUNT_OF_WINNING_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개이어야 합니다.");
         }
         if (hasDuplicates(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.");

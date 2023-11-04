e20e1a64615e837ceb6232ba4007620d1ac4b1c0
WonyuChoi <phillip07290@naver.com>
2023-11-04 13:29:46 +0900

    feat: 정적 팩토리 메서드 인자 수정

--- src/test/java/model/FinanceManagerTest.java
+++ src/test/java/model/FinanceManagerTest.java
@@ -11,7 +11,7 @@ class FinanceManagerTest {
     @Test
     void 구매_금액을_로또_금액으로_나눈_값을_로또_개수로_반환() {
         // given
-        financeManager = FinanceManager.from("8000");
+        financeManager = FinanceManager.from(8000);
 
         // when & then
         assertEquals(8, financeManager.calculateLottoCount());

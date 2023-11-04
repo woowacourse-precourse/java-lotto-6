92498b413bf18ccd301fbffddf1c8fa05e941a52
shin5774 <babo5774@naver.com>
2023-11-04 15:10:31 +0900

    Feat: 등수 반환 기능 구현
    - stream.filter()를 이용해서 두 결과의 일치여부를 확인.
    - 일치하는 값,즉 등수가 아닐경우 orElse()로 null을 반환.
    - 이는 차후 외부에서 null을 filtering 함으로 미당첨을 제외할수 있음.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum Rank {
     FIRST(6,false,2000000000),
     SECOND(5,true,30000000),
@@ -7,13 +9,29 @@ public enum Rank {
     FOURTH(4,false,50000),
     FIFTH(3,false,5000);
 
-    private int matchCount;
-    private boolean isMatchBonusNumber;
-    private long reward;
+    private final int matchCount;
+    private final boolean isMatchBonusNumber;
+    private final long reward;
 
     Rank(int matchCount, boolean isMatchBonusNumber, long reward){
         this.matchCount=matchCount;
         this.isMatchBonusNumber=isMatchBonusNumber;
         this.reward=reward;
     }
+
+    public static Rank getRank(int matchCount,boolean isMatchBonusNumber){
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.isSameMatchCount(matchCount) && rank.isSameMatchBounsNumber(isMatchBonusNumber))
+                .findFirst().orElse(null);
+    }
+
+    private boolean isSameMatchCount(int matchCount){
+        return matchCount==this.matchCount;
+    }
+
+    private boolean isSameMatchBounsNumber(boolean isMatchBonusNumber){
+        return isMatchBonusNumber==this.isMatchBonusNumber;
+    }
+
+
 }

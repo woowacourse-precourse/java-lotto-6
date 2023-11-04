dbdfbc8f9c10fe19400e39ec83ea6c049a52b610
DongHyun <kimdh0315@naver.com>
2023-11-04 22:58:56 +0900

    feat(main/controller/LottoService) : implement lotto random generator and lotto winning checkor

---
+++ src/main/java/lotto/controller/LottoService.java
@@ -0,0 +1,40 @@
+package lotto.controller;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Constant;
+import lotto.model.Lotto;
+import lotto.model.Rank;
+import lotto.model.Winning;
+
+public class LottoService {
+    public static List<Integer> makeRandomLottoNumber(){
+        return Randoms.pickUniqueNumbersInRange(Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER, Constant.LOTTO_SELECT_NUMBER);
+    }
+
+    public static Rank checkLottoRank(Lotto lotto, Winning winning) {
+        int match_count = 0;
+        boolean bonus_match = false;
+        for (int number : lotto.getNumbers()) {
+            if (winning.getResult().getNumbers().contains(number)) {
+                match_count++;
+            }
+            if (winning.getBonus() == number){
+                bonus_match = true;
+            }
+        }
+
+        return matchRankWithCount(match_count, bonus_match);
+    }
+
+    private static Rank matchRankWithCount(int match_count, boolean bonus_flag){
+        if (match_count == 6) return Rank.FIRST;
+        else if (match_count == 5 && bonus_flag) return Rank.SECOND;
+        else if (match_count == 5) return Rank.THIRD;
+        else if (match_count == 4) return Rank.FOURTH;
+        else if (match_count == 3) return Rank.FIFTH;
+
+        return Rank.LOSE;
+    }
+}

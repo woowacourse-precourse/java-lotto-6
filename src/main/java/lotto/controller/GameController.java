cac7982422efa9f3d6581daf2a912a0258a543c8
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 23:45:38 +0900

    [feat] GameController 클래스 필드 선언

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,4 +1,18 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(
+            InputView inputView,
+            OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
 }

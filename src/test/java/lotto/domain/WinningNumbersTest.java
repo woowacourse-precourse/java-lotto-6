64c5cf20a6c2136044ae7d5846e0ecbd70c44d5f
현 <sohyun95277@gmail.com>
2023-11-04 14:32:35 +0900

    test(WinningNumberTest): 보너스 번호 검증 테스트
    당첨 번호의 경우 Lotto 클래스에서 검수 진행

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,20 +10,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 class WinningNumbersTest {
     WinningNumbers numbers = new WinningNumbers();
 
-    @DisplayName("당첨 번호에 잘못된 값을 입력했을 경우 에러 반환 테스트")
-    @ParameterizedTest
-    @ValueSource(strings = {"3.2.1.5.6.8", "23,41,55,12,3,8", "12,31,42,2,12,4"})
-    void makeWinningNumber(String input) {
-        assertThatThrownBy(() ->
-                numbers.makeWinningNumber(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스 볼에 잘못된 값을 입력했을 경우 에러 반환 테스트")
+    @DisplayName("보너스 볼- 범위를 벗어났거나, 당첨 번호 중복일 경우 예외 발생")
     @ParameterizedTest
     @ValueSource(ints = {-3, 46, 13})
     void makeBonusNumber(int input) {
-        numbers.makeWinningNumber("13,22,4,5,31,17");
+        numbers.makeWinningNumber(List.of(13, 22, 4, 5, 31, 17));
         assertThatThrownBy(() ->
                 numbers.makeBonusNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);

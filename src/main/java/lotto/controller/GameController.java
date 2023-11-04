88711cd9ab633beb8253cc95a023bdb39fa1723f
minsub <chrkb1569@naver.com>
2023-11-04 14:53:53 +0900

    feat : 사용자로부터 값을 입력받고, 이를 출력하기 위한 GameController 생성

---
+++ src/main/java/lotto/controller/GameController.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
+
+// 사용자로부터 값을 입력받고, 이를 출력하기 위한 클래스
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void startGame() {
+        outputView.printCostMessage();
+        int cost = inputView.getCost();
+    }
+}

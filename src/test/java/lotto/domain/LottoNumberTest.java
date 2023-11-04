591830d3a799ee15ec5bb3ab7a1a7f169bc34892
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 01:15:47 +0900

    test: LottoNumber equlas, hashCode 테스트 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,10 +1,11 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -23,5 +24,37 @@ class LottoNumberTest {
     @ValueSource(ints = {0, 46})
     void testOutOfRangeLottoNumberValidation(int number) {
         assertThatThrownBy(() -> LottoNumber.from(number));
+    }
+
+    @DisplayName("동일 값의 로또 자릿 수는 서로 동등하다")
+    @Test
+    void testEqualsWithSameValues() {
+        LottoNumber lottoNumber1 = LottoNumber.from(5);
+        LottoNumber lottoNumber2 = LottoNumber.from(5);
+        assertThat(lottoNumber1).isEqualTo(lottoNumber2);
+    }
+
+    @DisplayName("값이 다른 로또 자릿 수는 동등하지 않다")
+    @Test
+    void testEqualsWithDifferentValues() {
+        LottoNumber lottoNumber1 = LottoNumber.from(5);
+        LottoNumber lottoNumber2 = LottoNumber.from(7);
+        assertThat(lottoNumber1).isNotEqualTo(lottoNumber2);
+    }
+
+    @DisplayName("동일한 값의 로또 자리수는 해시 코드가 동일하다")
+    @Test
+    void testHashCodeWithSameValues() {
+        LottoNumber lottoNumber1 = LottoNumber.from(5);
+        LottoNumber lottoNumber2 = LottoNumber.from(5);
+        assertThat(lottoNumber1).isEqualTo(lottoNumber2);
+    }
+
+    @DisplayName("다른 값의 로또 자리수는 해시 코드가 다르게 생성된다")
+    @Test
+    void testHashCodeWithDifferentValues() {
+        LottoNumber lottoNumber1 = LottoNumber.from(5);
+        LottoNumber lottoNumber2 = LottoNumber.from(7);
+        assertThat(lottoNumber1).isNotEqualTo(lottoNumber2);
     }
 }

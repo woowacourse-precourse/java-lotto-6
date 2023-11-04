0fad91d0b3c488c9c4dc1f6875c2c2dbe750e39a
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 04:19:28 +0900

    test: StringParser 테스트 작성

---
+++ src/test/java/lotto/util/StringParserTest.java
@@ -0,0 +1,18 @@
+package lotto.util;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("문자열 나누기 테스트")
+class StringParserTest {
+
+    @DisplayName("쉼표를 기반으로 문자열을 나눈다.")
+    @Test
+    void testTokenize() {
+        String input = "1,2,3,4,5,6";
+        List<String> tokenized = StringParser.parseToList(input);
+        Assertions.assertThat(tokenized).containsExactly("1", "2", "3", "4", "5", "6");
+    }
+}

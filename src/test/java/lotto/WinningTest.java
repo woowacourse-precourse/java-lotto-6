db194781f7a7a91209a8fd3fb1f4abbbcf5f57fc
Jiwon Na <jiwonna52@gmail.com>
2023-11-04 11:43:30 +0900

    Test : 당첨 번호가 1이상 45이하인지 범위 확인하는 테스트

--- src/test/java/lotto/WinningTest.java
+++ src/test/java/lotto/WinningTest.java
@@ -13,8 +13,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class WinningTest {
     @DisplayName("당첨 로또 번호 길이가 6이 아니면 예외가 발생한다.")
     @Test
-    void winningNumbersLength() {
+    void winningNumbersLengthTest() {
         assertThatThrownBy(() -> new Winning(List.of(1, 2, 3, 4, 5, 6, 7), 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 로또 번호의 숫자 범위가 1보다 작으면 예외가 발생한다.")
+    @Test
+    void checkRangeMinTest() {
+        assertThatThrownBy(() -> new Winning(List.of(-1, 2, 3, 4, 5, 45), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호의 숫자 범위가 45보다 크면 예외가 발생한다.")
+    @Test
+    void checkRangeMaxTest() {
+        assertThatThrownBy(() -> new Winning(List.of(1, 2, 3, 4, 5, 46), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호의 숫자가 45이면 예외가 발생하지 않는다.")
+    @Test
+    void checkRange45Test() {
+        Winning winning = new Winning(List.of(1, 2, 3, 4, 5, 45), 1);
+    }
+
+    @DisplayName("당첨 로또 번호의 숫자가 1이면 예외가 발생하지 않는다.")
+    @Test
+    void checkRange1Test() {
+        Winning winning = new Winning(List.of(1, 2, 3, 4, 5, 6), 1);
+    }
+
 }

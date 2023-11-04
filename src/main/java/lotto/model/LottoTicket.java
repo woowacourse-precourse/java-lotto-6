613f1d65608085c3ceb660cff3f4b76a18d94823
YongHyeon <pyh4616@naver.com>
2023-11-04 20:51:18 +0900

    Feat(LottoTicket 클래스): 로또를 당첨 번호와 비교하여 일치하는 개수를 구하는 기능을 getMatchCountWithWinningNumbers 메서드로 구현

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoTicket {
+
+    private final List<Integer> lottoTicket;
+
+    public LottoTicket(List<Integer> lottoTicket) {
+        this.lottoTicket = lottoTicket;
+    }
+
+    public int getMatchCountWithWinningNumbers(List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (int winningNumber : winningNumbers) {
+            if (lottoTicket.contains(winningNumber)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+}

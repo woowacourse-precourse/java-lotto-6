72d730edbfe8adcf47275c997f2678edaf2bd1a9
Genius-PAK <pyj789123@gmail.com>
2023-11-04 10:00:20 +0900

    feat(GameConstants): MagicNumber에서 GameConstants로 클래스 이름 변경

--- src/main/java/lotto/GameConstants.java
+++ src/main/java/lotto/GameConstants.java
@@ -1,6 +1,6 @@
 package lotto;
 
-public class MagicNumber {
+public class GameConstants {
     public static final int UNIT_PURCHASE_PRICE = 1000;
     public static final int VALID_LOTTO_NUMBER_COUNT = 6;
     public static final int MIN_LOTTO_NUMBER = 1;

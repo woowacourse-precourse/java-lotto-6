6cdf7775a7e40bf2a7b630e8555ff4f648c2c0cb
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-04 15:43:53 +0900

    feat(Bonus): 보너스 숫자가 1~45사이의 숫자가 아니면 예외인 기능 구현

---
+++ src/main/java/lotto/Model/Bonus.java
@@ -0,0 +1,32 @@
+package lotto.Model;
+
+import static lotto.Utils.toInt;
+
+public class Bonus {
+    int bonusNumber;
+
+    public Bonus(String input) {
+        validate(input);
+        this.bonusNumber = toInt(input);
+    }
+
+    private void validate(String input) {
+        int validInput = isNumber(input);
+        isCorrectRange(validInput);
+
+    }
+
+    private int isNumber(String input) {
+        try {
+            return toInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자로만 입력해야 합니다.");
+        }
+    }
+
+    private void isCorrectRange(int validInput) {
+        if (validInput < 1 || validInput > 45) {
+            throw new IllegalArgumentException(("[ERROR] 보너스 번호는 1~45사이의 숫자여야 합니다."));
+        }
+    }
+}

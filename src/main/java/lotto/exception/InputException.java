4441adb5ba7065adb395f8c14803b3e83645337c
SeungjunNA <nsjjsng1@gmail.com>
2023-11-04 23:07:54 +0900

    feat : 로또번호 입력시 6개 맞는지, 같은 번호 있는지 확인하여 예외처리 기능 추가

--- src/main/java/lotto/exception/InputException.java
+++ src/main/java/lotto/exception/InputException.java
@@ -1,21 +1,26 @@
 package lotto.exception;
 
+import lotto.Lotto;
 import lotto.view.InputView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputException {
     public void checkThousandUnit(int money) {
-        if (money % 1000 != 0) {
+        if (money % 1000 != 0 || money == 0) {
             throw new IllegalArgumentException("[ERROR] 1000단위로 입력해주세요.");
         }
     }
 
-    public void inputExceptionCheck(String amount) {
+    public int inputExceptionCheck(String amount) {
         try {
             int money = toInt(amount);
             checkThousandUnit(money);
+            return money;
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            new InputView().inputAmount();
+            return new InputView().inputAmount();
         }
     }
 
@@ -26,21 +31,56 @@ public class InputException {
         return Integer.parseInt(amount);
     }
 
-    public void checkExceptionWinningNumber(String[] numbers) {
-        for (int i = 0; i < 6; i++) {
-            try {
+    public String[] checkExceptionWinningNumber(String[] numbers) {
+        try {
+            checkNumberCount(numbers);
+            for(int i=0;i<6;i++){
                 int number = toInt(numbers[i]);
                 checkNotLottoNumber(number);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                new InputView().winningNumber();
             }
+            checkSameNumber(numbers);
+            return numbers;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return new InputView().winningNumber();
         }
     }
 
     public void checkNotLottoNumber(int number) {
         if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("1에서 45사이 값을 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 1에서 45사이 값을 입력해 주세요.");
+        }
+    }
+    public void checkNumberCount(String[] numbers){
+        if(numbers.length != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 6개를 입력해 주세요.");
+        }
+    }
+
+    public int checkExceptionBonusNumber(String bonusNumber, Lotto winningLotto){
+        try {
+            int number = toInt(bonusNumber);
+            checkNotLottoNumber(number);
+            checkContainsBonusNumber(winningLotto, number);
+            return number;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return new InputView().bonusNumber(winningLotto);
+        }
+    }
+
+    public void checkSameNumber(String[] number){
+        Set<Integer> numbers = new HashSet<>();
+        for(int i=0;i<number.length;i++){
+            numbers.add(Integer.parseInt(number[i]));
+        }
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException("서로 다른 6개의 숫자를 입력하세요.");
+        }
+    }
+    public void checkContainsBonusNumber(Lotto lotto, int bonusNumber){
+        if(lotto.toSet().contains(bonusNumber)){
+            throw new IllegalArgumentException("당첨 번호와 다른 번호를 입력해 주세요.");
         }
     }
 }

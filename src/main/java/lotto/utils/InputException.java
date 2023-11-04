b5adb0d779b0f3e7469ee0ca55b9eb3955b94dc6
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-05 08:25:02 +0900

    feat(view): 당첨 번호 입력 검증 기능 구현

--- src/main/java/lotto/utils/InputException.java
+++ src/main/java/lotto/utils/InputException.java
@@ -1,7 +1,8 @@
 package lotto.utils;
 
 public enum InputException {
-    NOT_DIGIT_FORM("입력은 숫자의 형식이어야 합니다.");
+    NOT_DIGIT_FORM("입력은 숫자의 형식이어야 합니다."),
+    NOT_LOTTO_NUMBERS_FORM("입력은 쉼표(,)로 구분할 수 있어야 합니다.");
 
     private static final String ERROR_SYMBOL = "[ERROR] ";
     private final String message;
--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -1,10 +1,17 @@
 package lotto.view;
 
 import static lotto.utils.InputException.NOT_DIGIT_FORM;
+import static lotto.utils.InputException.NOT_LOTTO_NUMBERS_FORM;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConsoleInputView {
+    private static final String COMMA = ",";
+    private static final String LOTTO_NUMBERS_REGEX = "^\\d+(,\\d+)*$";
+
     public int readPurchaseAmount() {
         try {
             String amount = readLineWithoutGap();
@@ -15,11 +22,34 @@ public class ConsoleInputView {
         }
     }
 
+    public List<Integer> readWinningLotto() {
+        String winningNumbers = readLineWithoutGap();
+        validateLottoNumbersFormat(winningNumbers);
+        return convertToList(winningNumbers);
+    }
+
     private String readLineWithoutGap() {
         return Console.readLine().trim();
     }
 
     private int convertToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private void validateLottoNumbersFormat(String lottoNumbers) {
+        if (isNotValidLottoNumbersFormat(lottoNumbers)) {
+            NOT_LOTTO_NUMBERS_FORM.throwException();
+        }
+    }
+
+    private boolean isNotValidLottoNumbersFormat(String lottoNumbers) {
+        return !lottoNumbers.matches(LOTTO_NUMBERS_REGEX);
+    }
+
+    private List<Integer> convertToList(String winningNumbers) {
+        return Arrays.stream(winningNumbers.split(COMMA))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

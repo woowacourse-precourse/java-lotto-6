d40af36be8cdaa336a59a435c1cb1b546e79b87f
jeondui <jeondui@gmail.com>
2023-11-05 00:30:34 +0900

    feat(Lotto): 당첨 번호의 String 입력값을 Number 리스트로 변환하는 기능

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.exception.Constant;
 import lotto.exception.ErrorMessage;
 
@@ -9,9 +11,22 @@ public class Lotto {
     private final List<Number> numbers; // private 변경 및 필드 추가 불가능
 
     private Lotto(List<Number> numbers) {
-        validateNumbersDuplicate(numbers);
         validateNumbersLength(numbers);
+        validateNumbersDuplicate(numbers);
         this.numbers = numbers;
+    }
+
+    private static List<Number> convertStringToNumbers(String inputNumber) {
+        List<Integer> inputNumbers = convertStringToList(inputNumber);
+        return inputNumbers.stream()
+                .map(Number::of)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> convertStringToList(String inputNumber) {
+        return Arrays.stream(inputNumber.split(Constant.COMMA.getCharValue()))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     private void validateNumbersLength(List<Number> numbers) {

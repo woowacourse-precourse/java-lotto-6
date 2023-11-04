38b2b08bfa7731f032a64e41240fc8d7c2d29452
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 18:15:52 +0900

    feat: 로또 구입 금액의 유효성 검사 코드 작성

--- src/main/java/validator/PriceValidator.java
+++ src/main/java/validator/PriceValidator.java
@@ -1,4 +1,35 @@
 package validator;
 
+import util.ErrorMessage;
+
 public class PriceValidator {
+
+    public void checkPriceValidate(String input) {
+        int price = isPriceContainsOnlyNumber(input);
+        isPricePositive(price);
+        isMultipleOfThousandWon(price);
+    }
+
+    private int isPriceContainsOnlyNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            System.out.println(ErrorMessage.PRICE_CONTAIN_NON_NUMERIC_CHARACTER.getErrorMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isPricePositive(int price) {
+        if(price < 0) {
+            System.out.println(ErrorMessage.PRICE_IS_NEGATIVE.getErrorMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isMultipleOfThousandWon(int price) {
+        if(price % 1000 != 0) {
+            System.out.println(ErrorMessage.PRICE_ISNT_MULTIPLE_OF_THOUSAND_WON.getErrorMessage());
+            throw new IllegalArgumentException();
+        }
+    }
 }

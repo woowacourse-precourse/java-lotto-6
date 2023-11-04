d2e05c2d881876e2e67dac081dec711eac35abba
happyryan <tjdgks120813@gmail.com>
2023-11-04 16:52:22 +0900

    feat(lotto Output): add Output class with print methods
    Created the Output class to handle printing messages and information for the Lotto program. This class includes various print methods for different messages and data related to the Lotto game.

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import java.util.List;
+
+class Output {
+    private static final int PERCENTAGE_MULTIPLIER = 100;
+    private static final String PROMPT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    private Output() {
+    }
+
+    static void printGameStartMessage() {
+        System.out.println(PROMPT_PURCHASE_AMOUNT);
+    }
+
+    static void printUserMoney(int userMoney) {
+        System.out.println(userMoney + "\n");
+    }
+
+    static void printNumberOfLottoes(int numberOfLottoes) {
+        System.out.println(String.format("%d개를 구매했습니다.", numberOfLottoes));
+    }
+
+    static void printLottoSixNumbers(Lotto lotto) {
+        System.out.println(lotto.getSixLottoNumbers());
+    }
+
+    static void printLottoNumbersMessage() {
+        System.out.println(PROMPT_WINNING_NUMBERS);
+    }
+
+    static void printLottoNumbers(List<String> sixLottoNumbers) {
+        System.out.println("\n" + String.join(",", sixLottoNumbers) + "\n");
+    }
+
+    static void printBonusNumberMessage() {
+        System.out.println(PROMPT_BONUS_NUMBER);
+    }
+
+    static void printBonusNumber(String bonusNumber) {
+        System.out.println(bonusNumber + "\n");
+    }
+
+    static void printReturn(int totalWinningAmount, int userMoney) {
+        float profitRate = ((float) totalWinningAmount / userMoney) * PERCENTAGE_MULTIPLIER;
+        System.out.printf(TOTAL_PROFIT_RATE_MESSAGE, profitRate);
+    }
+}

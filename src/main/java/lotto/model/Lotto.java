80da81eb276c58356254d01a8ee8793761ddf85f
wodnj5 <jw991211@gmail.com>
2023-11-04 19:00:11 +0900

    feat: Lotto 기능 추가 및 테스트 완료

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,17 +1,36 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDistinctNumbers(numbers);
+        validateNumbersRange(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDistinctNumbers(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream()
+                .distinct()
+                .count()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        if(numbers.stream()
+                .anyMatch(number -> number < 1 || number > 45)) {
             throw new IllegalArgumentException();
         }
     }

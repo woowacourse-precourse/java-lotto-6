97742f053dfcdf9daf3a6d2ff805b50317bc6d96
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 23:37:56 +0900

    feat(OutputView): 당첨 통계를 안내하는 문구를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,9 +3,14 @@ package lotto.view;
 public class OutputView {
 
     private final static String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
+    private final static String WINNING_STATISTICS = "당첨 통계";
 
     public void printPurchaseQuantity(int quantity) {
         System.out.println(String.format(PURCHASE_QUANTITY, quantity));
+    }
+
+    public void printWinningStatistics() {
+        System.out.println(WINNING_STATISTICS);
     }
 
 }

d1b9aa33132b37ca1beae00784c07bfb76cd375f
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 16:55:28 +0900

    feat(Lotto): 입력된 로또 번호 중 중복값 여부를 검증하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
@@ -8,6 +9,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateUniqueElements(numbers);
         this.numbers = numbers;
     }
 
@@ -17,5 +19,12 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateUniqueElements(List<Integer> numbers) {
+        int size = numbers.size();
+        int size1 = numbers.stream().collect(Collectors.toSet()).size();
+
+        if (size != size1) {
+            throw new IllegalArgumentException("[ERROR] 로또 입력 값은 중복을 허용하지 않습니다.");
+        }
+    }
 }

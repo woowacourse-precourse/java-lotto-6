0bde99d9839a0924e29c7e62083ad3db1d577e6f
jongmee <ra123221@gmail.com>
2023-11-05 01:28:27 +0900

    feat(LottoResponseTest): 로또 번호를 정렬해서 출력 메세지를 생성하는 기능 테스트하기
    - ISSUE: 정렬은 Lotto 클래스에서, 출력 메세지 생성은 LottoResponse 클래스에서 이루어진다. 한 메서드의 기능을 테스트하지 못했다.

---
+++ src/test/java/lotto/utility/vo/LottoResponseTest.java
@@ -0,0 +1,23 @@
+package lotto.utility.vo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.Test;
+
+class LottoResponseTest {
+    @Test
+    void 올바른_형식으로_발행된로또_출력메세지를_생성한다() {
+        // given
+        Lotto 로또 = new Lotto(Arrays.asList(5, 6, 3, 4, 1, 2));
+
+        // when
+        LottoResponse 출력용_응답 = 로또.convertToResponse();
+
+        // then
+        assertEquals("[1, 2, 3, 4, 5, 6]", 출력용_응답.createMessage());
+    }
+
+}

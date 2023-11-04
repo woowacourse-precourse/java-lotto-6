93e2afd7f3c733e59ff568a4392e07ff6e99cd86
KimDabomi <cdabomi@nate.com>
2023-11-05 01:53:34 +0900

    test: InputView getBonusNumber method

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.ErrorHeadMessage;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.AfterEach;
@@ -69,8 +71,64 @@ class InputViewTest {
     @Test
     @DisplayName("보너스 번호 입력 확인")
     void 보너스_번호_입력_확인() {
+        provideInput("7\n");
+        List<Integer> winningLotto = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            winningLotto.add(i);
+        }
+        int bonusNumber = inputView.getBonusNumber(winningLotto);
+        assertThat(bonusNumber).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 중복 입력인 경우 에러 확인")
+    void 보너스_번호_중복_입력_에러_확인() {
         provideInput("3\n");
-        int bonusNumber = inputView.getBonusNumber();
-        assertThat(bonusNumber).isEqualTo(3);
+        List<Integer> winningLotto = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            winningLotto.add(i);
+        }
+        assertThatThrownBy(() -> inputView.getBonusNumber(winningLotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_HEAD_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 음수인 경우 에러 확인")
+    void 보너스_번호_음수_입력_에러_확인() {
+        provideInput("-1\n");
+        List<Integer> winningLotto = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            winningLotto.add(i);
+        }
+        assertThatThrownBy(() -> inputView.getBonusNumber(winningLotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_HEAD_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 1~45 사이의 수가 아닌 경우 에러 확인")
+    void 보너스_번호_범위밖_입력_에러_확인() {
+        provideInput("47\n");
+        List<Integer> winningLotto = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            winningLotto.add(i);
+        }
+        assertThatThrownBy(() -> inputView.getBonusNumber(winningLotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_HEAD_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 숫자가 아닌 경우 에러 확인")
+    void 보너스_번호_문자_입력_에러_확인() {
+        provideInput("40a\n");
+        List<Integer> winningLotto = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            winningLotto.add(i);
+        }
+        assertThatThrownBy(() -> inputView.getBonusNumber(winningLotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_HEAD_MESSAGE);
     }
 }

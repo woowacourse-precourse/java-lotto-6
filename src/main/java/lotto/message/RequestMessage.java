5c6d9779ccb7559175b93817ba602a2ce657bbe5
yshong1998 <yshong1998@gmail.com>
2023-11-05 02:01:00 +0900

    Feat(RequestMessage): 사용자에게 요청하는 메세지를 관리하는 기능 구현

---
+++ src/main/java/lotto/message/RequestMessage.java
@@ -0,0 +1,18 @@
+package lotto.message;
+
+public enum RequestMessage {
+
+    REQUEST_PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
+    REQUEST_WINNING_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    RequestMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

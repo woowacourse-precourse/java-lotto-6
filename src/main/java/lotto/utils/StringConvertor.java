c14e18c1742f06ae47646f30003f73ff2bdbc665
gonixxxx <akdntm946@gmail.com>
2023-11-05 04:48:04 +0900

    feat: add convertor(string to List<Integer>)

--- src/main/java/lotto/utils/StringConvertor.java
+++ src/main/java/lotto/utils/StringConvertor.java
@@ -1,8 +1,15 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringConvertor {
     public int stringToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    public List<Integer> stringToList(String input) {
+        return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
     }
 
 }

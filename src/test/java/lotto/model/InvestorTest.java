875bf4a1e8da282f682fa7597220d84c6eea96da
devholic22 <hyunjoon.tech@gmail.com>
2023-11-05 00:25:24 +0900

    test: Investor 테스트
    - 기존 InvestorServiceTest 삭제 대신 Investor에 대한 테스트로 변경
    - 로또 구입 테스트 및 금액 증가 정상/예외 테스트 구현
    - 음수, null, 빈 문자, 일반 문자, 공백 포함 문자 등에 대해서도 테스트 구현

---
+++ src/test/java/lotto/model/InvestorTest.java
@@ -0,0 +1,95 @@
+package lotto.model;
+
+import static lotto.view.exception.InputException.NEGATIVE_NUMBER_EXCEPTION;
+import static lotto.view.exception.InputException.NUMBER_FORMAT_EXCEPTION;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.List;
+
+public class InvestorTest {
+
+    @Test
+    @DisplayName("정상 생성 테스트")
+    void createInvestorTest() {
+        // given
+        String investorInput = "5000";
+
+        // when & then
+        assertDoesNotThrow(() -> Investor.createDefault(investorInput));
+    }
+
+    @Test
+    @DisplayName("로또 구입 테스트")
+    void buyLottoTest() {
+        // given
+        String investorInput = "5000";
+        Investor investor = Investor.createDefault(investorInput);
+        Shop shop = Shop.createDefault();
+
+        // when
+        List<Lotto> lottos = investor.buyLottosFromShop(shop);
+
+        // then
+        assertThat(lottos.size()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("금액 증가 정상 테스트")
+    void addProfitTest() {
+        // given
+        String investorInput = "5000";
+        Investor investor = Investor.createDefault(investorInput);
+
+        // when
+        investor.addProfitMoney(5000);
+
+        // then
+        assertThat(investor.calculateProfitRate()).isEqualTo(1.00);
+    }
+
+    @Nested
+    @DisplayName("Investor 예외 테스트")
+    class InvestorExceptionTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"abc", "   5  00 "})
+        @NullAndEmptySource
+        @DisplayName("null, 빈 문자, 일반 문자, 공백 포함 문자 입력 시 예외가 발생한다.")
+        void notNumberInputInvestExceptionTest(final String investorInput) {
+            // when & then
+            assertThatThrownBy(() -> Investor.createDefault(investorInput)).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(NUMBER_FORMAT_EXCEPTION.getMessage());
+        }
+
+        @Test
+        @DisplayName("음수 입력 시 예외가 발생한다.")
+        void negativeNumberInputInvestExceptionTest() {
+            // given
+            String investorInput = "-1200";
+
+            // when & then
+            assertThatThrownBy(() -> Investor.createDefault(investorInput)).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(NEGATIVE_NUMBER_EXCEPTION.getMessage());
+        }
+
+        @Test
+        @DisplayName("추가 금액이 0 미만일 시 예외가 발생한다.")
+        void addProfitNegativeExceptionTest() {
+            // given
+            String investorInput = "5000";
+            Investor investor = Investor.createDefault(investorInput);
+
+            // when & then
+            assertThatThrownBy(() -> investor.addProfitMoney(-500)).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(NEGATIVE_NUMBER_EXCEPTION.getMessage());
+        }
+    }
+}

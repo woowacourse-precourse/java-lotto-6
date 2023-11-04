5e1db6d4dbe8c48f2aaf3e7255b9cad34877dab5
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 18:17:58 +0900

    refactor(LottoLogic): 안쓰는 import 제거

--- src/main/java/lotto/domain/LottoLogic.java
+++ src/main/java/lotto/domain/LottoLogic.java
@@ -1,14 +1,8 @@
 package lotto.domain;
 
-import static lotto.message.ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS;
-
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import lotto.Lotto;
 import lotto.dto.LottoNumbersInfo;
 import lotto.dto.LottoPurchaseInfo;
-import lotto.utils.LottoUtil;
 
 public class LottoLogic {
     private LottoPurchaseInfo purchaseInfo;
@@ -16,14 +10,12 @@ public class LottoLogic {
 
 
     public LottoLogic(LottoPurchaseInfo purchaseInfo, LottoNumbersInfo lottoNumbersInfo) {
-
-
         this.purchaseInfo = purchaseInfo;
         this.lottoNumbersInfo = lottoNumbersInfo;
     }
 
 
     public BigDecimal getNumberOfLottoPurchased() {
-        return purchaseInfo.purchaseAmount.divide(purchaseInfo.lottoPrice);
+        return purchaseInfo.numberOfLottoPurchased;
     }
 }

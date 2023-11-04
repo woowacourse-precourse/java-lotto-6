b514fc949fb04707a372edd80c150fc939300a3f
MinseokGo <rhalstjr1999@naver.com>
2023-11-04 20:57:43 +0900

    feat($Prize): 당첨 등수 열거형 선언
    당첨 등수는 1등부터 5등까지이며 열거형 클래스에서 등수와 금액을 관리합니다.

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum Prize {
+    PRIZE_FIRST("2,000,000,000"),
+    PRIZE_SECOND("30,000,000"),
+    PRIZE_THIRD("1,500,000"),
+    PRIZE_FOURTH("50,000"),
+    PRIZE_FIFTH("5,000");
+
+    private final String money;
+
+    Prize(final String money) {
+        this.money = money;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+}

92fe82f69d0cca2c72fe017929b83b3cd8c05a1a
zjacom <eephus625@icloud.com>
2023-11-04 20:06:02 +0900

    test(lotto): 구입 금액이 천원 단위로 나누어 떨어지지 않으면 예외 발생하는 테스트 추가

---
+++ src/test/java/lotto/InputViewTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+    InputView inputView = new InputView();
+    @Test
+    void 구입_금액_천원_단위_예외_테스트() {
+        String purchaseAmount = "1500";
+        assertThatThrownBy(() -> inputView.inputPurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입 금액은 1,000원 단위로 입력 받습니다.");
+    }
+}

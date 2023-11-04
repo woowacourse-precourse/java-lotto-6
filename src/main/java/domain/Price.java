46120665d3374f4005d00a2cd2fa5b88fe30e4da
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 22:23:06 +0900

    feat: 입력된 구매 금액에 따른 로또 개수를 반환하는 메서드 추가

--- src/main/java/domain/Price.java
+++ src/main/java/domain/Price.java
@@ -1,13 +1,18 @@
 package domain;
 
 import domain.validator.PriceUnitValidator;
+import util.PriceOfLotto;
 
 public class Price {
 
     private int price;
     private final PriceUnitValidator priceUnitValidator = new PriceUnitValidator();
-    Price(int price) {
-
+    public Price(int price) {
+        priceUnitValidator.checkPriceUnit(price);
         this.price = price;
+    }
+
+    public int numOfLotto() {
+        return price / PriceOfLotto.priceOfLotto;
     }
 }

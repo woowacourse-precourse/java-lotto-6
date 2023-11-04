5190a3a7bb54b9dfa0f5c745f52e42a6f9bec351
BonSik <rnqhstlr2297@gmail.com>
2023-11-04 18:07:38 +0900

    refactor: 매직 넘버, 예외 메시지 분리

---
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -0,0 +1,19 @@
+package lotto.exception;
+
+public enum ExceptionMessage {
+
+    INVALID_SIZE_LOTTO_NUMBER("로또 번호의 개수는 6개만 가능합니다."),
+    INVALID_RANGE_LOTTO_NUMBER("로또 번호는 1~45까지 숫자만 가능합니다."),
+    DUPLICATION_LOTTO_NUMBER("중복된 로또 번호는 사용할 수 없습니다.")
+    ;
+    private String desc;
+
+    ExceptionMessage(String description) {
+        this.desc = description;
+    }
+
+    public String getDesc(){
+        return this.desc;
+    }
+
+}
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,28 +3,32 @@ package lotto.model;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import lotto.exception.ExceptionMessage;
 
 public class Lotto {
+    private final static int SIZE_LOTTO_NUMBER = 6;
+    private final static int MIN_LOTTO_NUMBER = 1;
+    private final static int MAX_LOTTO_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLength(numbers);
+        validateSize(numbers);
         validateRange(numbers);
         validateDuplication(numbers);
 
         this.numbers = numbers;
     }
 
-    private void validateLength(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_SIZE_LOTTO_NUMBER.getDesc());
         }
     }
 
     private void validateRange(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if(number < 1 || number > 45){
-                throw new IllegalArgumentException("로또 번호는 1~45까지 숫자만 가능합니다.");
+            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(ExceptionMessage.INVALID_RANGE_LOTTO_NUMBER.getDesc());
             }
         }
     }
@@ -33,7 +37,7 @@ public class Lotto {
         Set<Integer> uniqueNumbers = new HashSet<>();
         for (Integer number : numbers) {
             if (!uniqueNumbers.add(number)) {
-                throw new IllegalArgumentException("중복된 로또 번호는 사용할 수 없습니다.");
+                throw new IllegalArgumentException(ExceptionMessage.DUPLICATION_LOTTO_NUMBER.getDesc());
             }
         }
     }

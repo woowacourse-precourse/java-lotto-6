7e8011e6ea5c18953b0f029e75fde198069f3cec
insu <is23193160@gmail.com>
2023-11-04 17:37:38 +0900

    feat: 랜덤한 6자리 1~45 숫자 리스트 반환하는 기능 추가

---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    public static List<Integer> createUniqueNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+    
+}

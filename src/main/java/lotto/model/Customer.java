952bbf0081a10c946377118d941064edce0abf5f
Kite_Z <duswl339@ajou.ac.kr>
2023-11-04 21:13:49 +0900

    feat: add method for customer class
    - initWinningLottos: 당첨된 로또 갯수 초기화 하는 메소드
    - buyLotto: 로또 구매/발행하는 메소드
    - getLottos: 로또 리스트 리턴하는 메소드
    - clacWinningLotto: 당첨되었는지 계산하는 메소드
    - calcRateOfReturn: 수익률 계산하는 메소드
    - getRateOfReturn: 수익률 리턴하는 메소드
    - getWinningLottos: 당첨된 로또 리스트 리턴하는 메소드

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -1,16 +1,66 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static lotto.constant.LottoConstant.*;
 
 public class Customer {
     private final int money;
     private List<Lotto> lottos;
+    private double rateOfReturn;
+    private List<Integer> winningLottos; // [3, 4, 5, 6]
     public Customer(int _money) {
         this.money = _money;
+        this.lottos = new ArrayList<Lotto>();
+        this.winningLottos = new ArrayList<Integer>();
+        initWinningLottos();
     }
     public int getMoney() {
         return money;
+    }
+
+    private void initWinningLottos() {
+        for(int i = 0 ; i < 5 ; i++) {
+            winningLottos.add(0);
+        }
+    }
+
+    public void buyLotto() {
+        lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public void calcWinningLotto(List<Integer> _winning, int _bonusNumber) {
+        for (Lotto _lotto: lottos) {
+            int same = _lotto.compare(_winning);
+            if (same >= 3) {
+                winningLottos.set(same -3, winningLottos.get(same -3)+1);
+            }
+        }
+    }
+
+    public void calcRateOfReturn() {
+        // 수익금 / 구입 금액 * 100
+        rateOfReturn = ((winningLottos.get(0) * THREE_WINNINGS.getLottoConstant())
+                + (winningLottos.get(1) * FOUR_WINNINGS.getLottoConstant())
+                + (winningLottos.get(2) * FIVE_WINNINGS.getLottoConstant())
+                + (winningLottos.get(3) * SIX_WINNINGS.getLottoConstant())
+                + (winningLottos.get(4) * FIVE_BONUS_WINNINGS.getLottoConstant())
+        ) / (double)money * 100 ;
+    }
+
+    public double getRateOrReturn() {
+        return rateOfReturn;
+    }
+
+    public List<Integer> getWinningLottos() {
+        return winningLottos;
     }
 }

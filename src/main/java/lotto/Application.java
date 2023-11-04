49b76d8c46fa134a05f21787b1dea35df66956b4
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-11-05 00:04:24 +0900

    remove: 에러로 인한 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,13 +4,9 @@ import lotto.domain.InputAmount;
 import lotto.view.PrintView;
 
 public class Application {
-    static final PrintView printView = new PrintView();
-    static final InputAmount inputAmount = new InputAmount();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        printView.Input_Lotto_Buy();
-        inputAmount.Input_Lotto_Amount();
     }
 
 
--- src/main/java/lotto/domain/InputAmount.java
+++ src/main/java/lotto/domain/InputAmount.java
@@ -4,24 +4,7 @@ public class InputAmount {
     private static final int Lotto_Cost = 1000;
     private final int Lottos = 0;
 
-
-    public static int Input_Lotto_Amount() {
-        String amount = Console.readLine();
-        boolean flag = Amount_Type_Check(amount);
-        if(flag == false){
-            return Input_Lotto_Amount();
-        }
-        return Integer.parseInt(amount);
-    }
-    private static boolean Amount_Type_Check(String amount){
-        try{
-            Integer.parseInt(amount);
-            return true;
-        }catch (NumberFormatException ex){
-            return false;
-        }
-    }
-
+    
 
 
 }

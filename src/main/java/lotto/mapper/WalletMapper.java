3d19a0e306521dd508da00b5684ae46caad7c052
mainfn <_@mainfn.dev>
2023-11-04 16:23:27 +0900

    feat: `WalletMapper` 구현 및 문서화

---
+++ src/main/java/lotto/mapper/WalletMapper.java
@@ -0,0 +1,19 @@
+package lotto.mapper;
+
+import lotto.controller.dto.input.BuyLottosDto;
+import lotto.domain.money.Money;
+import lotto.domain.money.Wallet;
+
+/**
+ * Dto와 Domain 간의 매핑을 담당
+ */
+public final class WalletMapper {
+
+    /**
+     * BuyLottosDto -> Wallet(Domain) 변환
+     */
+    public Wallet mapFrom(final BuyLottosDto dto) {
+        final Money money = Money.from(dto.getAmount());
+        return new Wallet(money);
+    }
+}

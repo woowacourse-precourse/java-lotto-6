a573cbcbf299b04cd17d280e45f64ae00e522565
pjaehyun <show6756@gmail.com>
2023-11-04 16:47:35 +0900

    feat: 추가 구매한 만큼 로또 번호 생성 함수

--- src/main/java/lotto/LottoCollection.java
+++ src/main/java/lotto/LottoCollection.java
@@ -1,4 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoCollection {
+    public List<Lotto> lottoCollection;
+
+    public LottoCollection(int numberOfLotto) {
+        this.lottoCollection = new ArrayList<>();
+        generateLottoNumbers(numberOfLotto);
+    }
+
+    private void generateLottoNumbers(int numberOfLotto) {
+        for (int i = 0; i < numberOfLotto; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottoCollection.add(lotto);
+        }
+    }
 }

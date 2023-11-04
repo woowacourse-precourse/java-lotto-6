17690ff80b9b6726ac4a1fbdcca456c4c2e12d88
uhanuu <hwryu990927@gmail.com>
2023-11-04 10:36:21 +0900

    feat: 순위의 개수를 Map으로 반환

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -3,6 +3,7 @@ package lotto.domain.lotto;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -13,6 +14,15 @@ public class Lottos {
 
     public int size() {
         return lottos.size();
+    }
+
+    public Map<LottoRank, Long> countLottoRanks(final Lotto winningLotto, final int bonus) {
+        return lottos.stream()
+                .map(lotto -> lotto.determineLottoRank(winningLotto, bonus))
+                .collect(Collectors.groupingBy(
+                        rank -> rank,
+                        Collectors.counting()
+                ));
     }
 
     public List<Lotto> getLottos() {

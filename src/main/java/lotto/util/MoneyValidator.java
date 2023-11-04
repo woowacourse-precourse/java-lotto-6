e4d4c5a9f87daa22c7ac286d25fa145cf3c8760e
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-05 04:34:19 +0900

    feat:MoneyValidator 통합

--- src/main/java/lotto/util/MoneyValidator.java
+++ src/main/java/lotto/util/MoneyValidator.java
@@ -6,13 +6,18 @@ public class MoneyValidator {
 
     private static final int LOTTO_PRICE = 1000;
 
-    public static void validateNumber(int number) {
+    public static void validateMoney(int money) {
+        validateNumber(money);
+        validateThousand(money);
+    }
+
+    private static void validateNumber(int number) {
         if(number < LOTTO_PRICE) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.toString());
         }
     }
 
-    public static void validateThousand(int number) {
+    private static void validateThousand(int number) {
         if(number % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ErrorMessage.ONLY_THOUSAND.toString());
         }

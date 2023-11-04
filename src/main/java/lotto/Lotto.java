b32bddc6381ee9c052dae3053f7d10e32d401600
HSBODo <vinsulill@gmaile.com>
2023-11-04 19:01:02 +0900

    feat : Lotto 클래스에 인스턴스 메서드 추가
    추가한 인서턴스 메서드
    - getNumbers ()
    - lottoNumberDisplay()

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,6 +15,20 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<Integer> getNumbers (){
+        return this.numbers;
+    }
+
+    public void lottoNumberDisplay(){
+        List<Integer> lottoNumbers = getNumbers();
+        StringJoiner displayLottoNumber = new StringJoiner(",","[","]");
+        for(int i = 0 ; i < lottoNumbers.size() ; i ++){
+            String number = String.valueOf(lottoNumbers.get(i));
+            displayLottoNumber.add(number);
+        }
+        System.out.println(displayLottoNumber);
     }
 
     // TODO: 추가 기능 구현

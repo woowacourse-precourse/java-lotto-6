11f8d286e0c5c727353dc4c18f0e9d0fe226138a
shin5774 <babo5774@naver.com>
2023-11-04 15:48:55 +0900

    Test: 로또 구매금액 검증 기능 테스트케이스 추가
    - 0원도 예외처리를 해야하기에 이를 검증하기위해 0을 테스트케이스에 추가함.

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -11,23 +11,23 @@ public class ValidatorTest {
     private static Validator validator;
 
     @BeforeAll
-    static void 테스트_초기설정(){
-        validator=Validator.INSTANCE;
+    static void 테스트_초기설정() {
+        validator = Validator.INSTANCE;
     }
 
     @DisplayName("인자값이 숫자로만 이루어져 있는지 검증하는 기능")
     @ParameterizedTest
-    @ValueSource(strings={"abcs","1a23","aa1"," 111","11 2","   ","-1"})
-    void 숫자_검증_기능(String value){
-        assertThatThrownBy(()->validator.numberValidate(value))
+    @ValueSource(strings = {"abcs", "1a23", "aa1", " 111", "11 2", "   ", "-1"})
+    void 숫자_검증_기능(String value) {
+        assertThatThrownBy(() -> validator.numberValidate(value))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("정수 인자값이 1000원으로 나누어지는지 검증하는 기능")
+    @DisplayName("로또 구매액이 적합한지를 검증하는 기능")
     @ParameterizedTest
-    @ValueSource(ints = {1001,-1500,900})
-    void 로또구매_값_검증(long buyCash){
-        assertThatThrownBy(()->validator.buyCashValidate(buyCash))
+    @ValueSource(ints = {1001, -1500, 900, 0})
+    void 로또구매_값_검증(long buyCash) {
+        assertThatThrownBy(() -> validator.buyCashValidate(buyCash))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

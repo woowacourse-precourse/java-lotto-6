0bbaca15b2c39a704cdcbd398a56bba00be36dde
suihub <qkdcrt@gmail.com>
2023-11-04 13:01:48 +0900

    <feat(WinningLotto)> : "WinningLotto 생성자 구현"
    - 당첨 복권 생성시 인자로 받은 List<String>을
    Integer로 변환해서 담도록 변경.
    - 테스트 코드 일부 변경.

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,13 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningLotto {
+    private final List<Integer> numbers;
+
+    public WinningLotto(List<String> numbers) {
+        this.numbers = numbers.stream()
+                .map(Integer::valueOf)
+                .toList();
+    }
 }

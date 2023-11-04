502306d270bc465d73edd63f05040cdc56d098c3
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 15:45:07 +0900

    refactor(NumberTest): 숫자 값 저장 개수 증가

--- src/test/java/lotto/domain/NumberTest.java
+++ src/test/java/lotto/domain/NumberTest.java
@@ -1,24 +1,26 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("숫자 객체에 대해")
 class NumberTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     @DisplayName("숫자 값을 저장하면 String으로 값을 받을 수 있다.")
-    void Given_CreateNumber_When_saveValue_Then_getString() {
+    void Given_CreateNumber_When_saveValue_Then_getString(Integer value) {
         //given
-        Number number = new Number(3);
+        Number number = new Number(value);
 
         //when
         String numberValue = number.getValue();
 
         //then
-        assertThat(numberValue).isEqualTo("3")
+        assertThat(numberValue).isEqualTo(String.valueOf(value))
                 .isInstanceOf(String.class);
     }
 }

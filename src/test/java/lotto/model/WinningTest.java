0798ee32b73d1ab72852b409348ee2bba6d265ae
whxogus215 <whxogus215@naver.com>
2023-11-04 11:15:19 +0900

    test(Winning): Winning 생성 시 예외 발생 테스트
    - 당첨 번호 숫자가 6개 미만일 때
    - 당첨 번호가 1 ~ 45 범위를 벗어날 때

---
+++ src/test/java/lotto/model/WinningTest.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WinningTest {
+    @Test
+    void 당첨번호_6개_테스트() {
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1,2,3,4,5,6,7));
+
+        Assertions.assertThatThrownBy(() -> new Winning(winningNumbers, 10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자는 6개여야 합니다.");
+    }
+
+    @Test
+    void 당첨번호_범위_테스트() {
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1,53,300,4,5,6));
+
+        Assertions.assertThatThrownBy(() -> new Winning(winningNumbers, 10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+}

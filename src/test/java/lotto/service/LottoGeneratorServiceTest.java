6c414c81223ba89f9730a5052706a7e7a83c1835
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 05:03:27 +0900

    test: 생성된 로또가 검증을 통과하는지 테스트 작성

---
+++ src/test/java/lotto/service/LottoGeneratorServiceTest.java
@@ -0,0 +1,16 @@
+package lotto.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+@DisplayName("자동 로또 발행기 테스트")
+class LottoGeneratorServiceTest {
+
+    @DisplayName("생성된 로또는 검증 요구사항을 통과한다")
+    @RepeatedTest(50)
+    void testGeneratedLottoIsValid() {
+        LottoGeneratorService lottoGeneratorService = new LottoGeneratorService(new LottoNumberGeneratorService());
+        Assertions.assertThatNoException().isThrownBy(lottoGeneratorService::generateLotto);
+    }
+}

32d0d9b237871dc5df0f13a3e506d3cc40748d37
BonSik <rnqhstlr2297@gmail.com>
2023-11-04 18:35:30 +0900

    refactor: 예외 메시지 prefix 추가

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -2,9 +2,9 @@ package lotto.exception;
 
 public enum ExceptionMessage {
 
-    INVALID_SIZE_LOTTO_NUMBER("로또 번호의 개수는 6개만 가능합니다."),
-    INVALID_RANGE_LOTTO_NUMBER("로또 번호는 1~45까지 숫자만 가능합니다."),
-    DUPLICATION_LOTTO_NUMBER("중복된 로또 번호는 사용할 수 없습니다.")
+    INVALID_SIZE_LOTTO_NUMBER("[ERROR] 로또 번호의 개수는 6개만 가능합니다."),
+    INVALID_RANGE_LOTTO_NUMBER("[ERROR] 로또 번호는 1~45까지 숫자만 가능합니다."),
+    DUPLICATION_LOTTO_NUMBER("[ERROR] 중복된 로또 번호는 사용할 수 없습니다.")
     ;
     private String desc;
 

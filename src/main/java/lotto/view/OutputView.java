43c87e20e4852ed7a205840a343bc64c1ee0bfe6
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 23:42:52 +0900

    [feat] 당첨 내역 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -19,7 +19,29 @@ public class OutputView {
         System.out.println();
     }
 
+    public void printStaticResult(Prizes prizes, Cash cash) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Prize prize : Prize.values()) {
+            if (!prize.getCheckBonus()) {
+                System.out.printf("%d개 일치 (%s) - %d개",
+                        prize.getCountOfSameNumbers(),
+                        formatter.format(prize.getReward()),
+                        prizes.countPrize(prize));
+            } else {
+                System.out.printf("%d개 일치, 보너스 볼 일치 (%s) - %d개",
+                        prize.getCountOfSameNumbers(),
+                        formatter.format(prize.getReward()),
+                        prizes.countPrize(prize));
+            }
+            System.out.println();
+        }
 
+        System.out.printf("총 수익률은 %d\"%\"입니다.",
+                prizes.calculateBenefit(cash
+                        .getSeed())
+        );
+    }
 
 
 }

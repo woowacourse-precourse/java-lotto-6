f5b02463339900f54b4b341f0f381c063588f1fc
5tr1ker <tjseocld@naver.com>
2023-11-04 20:29:21 +0900

    feat: 로또 보너스 번호 생성 로직 구현
    로또 보너스 번호 생성 로직 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class LottoNumber {
+
+    private Lotto lotto;
+    private int bonus;
+
+    public LottoNumber(Lotto lotto , int bonus) {
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+
+}

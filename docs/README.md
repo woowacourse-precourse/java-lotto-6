dad23bbc45d34d87c0d044da0f5f491429b84d94
wonkydd <dev.csh.01@gmail.com>
2023-11-04 19:52:00 +0900

    feat: getLottoPurchasePrice 구현

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# TODO
+
+- [x] 구입금액을 입력받는 `getLottoPurchasePrice` 함수를 구현
+- [ ] `getLottoPurchasePrice` 함수에 대한 테스트 코드 추가
+- [ ] 로또를 만들어내는 `makeLottery` 함수를 구현
+- [ ] 당첨 숫자를 입력받는 `getWinningNumbers` 함수를 구현
+- [ ] `getWinningNumbers` 함수에 대한 테스트 코드 추가
+- [ ] 등수 별 당첨내역을 구하는 getMatchCountByRank 함수를 구현
+- [ ] `getMatchCountByRank` 함수에 대한 테스트 코드 추가
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = getLottoPurchasePrice();
+        System.out.println("\n" + money / 1000 + "개를 구매했습니다.");
+    }
+
+    public static int getLottoPurchasePrice() {
+        int money;
+        while (true) {
+            try {
+                String str = Console.readLine();
+                if (!str.matches("^[0-9]*$")) throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+                money = Integer.parseInt(str);
+                if (money % 1000 != 0)  throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 구매할 수 있습니다.");
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return money;
     }
 }

16bff055546ea598514726c9ab343736f2a0db1f
whxogus215 <whxogus215@naver.com>
2023-11-04 11:22:39 +0900

    feat(Winning): 중복 숫자 검증 메서드 구현

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -1,16 +1,20 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.utils.Code;
 
 public class Winning {
     private final String LACK_NUMBER_COUNT = "숫자는 6개여야 합니다.";
     private final String NUMBER_NOT_INRANGE = "번호는 1부터 45 사이의 숫자여야 합니다.";
+    private final String NOT_DUPLICATED = "모든 숫자는 중복되지 않아야 합니다.";
     private final List<Integer> numbers;
     private final int bonusNumber;
 
     public Winning(List<Integer> numbers, int bonusNumber) {
         validateNumberCount(numbers);
+        validateDuplicatedNumber(numbers);
         numbers.forEach(number -> validateNumberInRange(number, Code.WINNING.getCode()));
         this.numbers = numbers;
 
@@ -27,6 +31,13 @@ public class Winning {
     private void validateNumberInRange(int number, String code) {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException(code + NUMBER_NOT_INRANGE);
+        }
+    }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException(NOT_DUPLICATED);
         }
     }
 }

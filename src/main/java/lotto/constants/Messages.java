fb038469b34990cc77edee05ca253eb622a658bd
junest66 <chlwndks333@gmail.com>
2023-11-04 21:56:41 +0900

    style(Messages): 로또 구매 갯수 출력하는 포맷 수정

--- src/main/java/lotto/constants/Messages.java
+++ src/main/java/lotto/constants/Messages.java
@@ -5,7 +5,7 @@ public class Messages {
     public static final String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    public static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
     public static final String LOTTO_NUMBERS_MESSAGE = "[%s]";
     public static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %s입니다.";
 

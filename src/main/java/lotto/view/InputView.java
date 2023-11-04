1a302780f9f4b732b86253acac480d46af694ad4
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-05 03:01:05 +0900

    feat(InputView, Util): 당첨 번호와 보너스 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.util.validator.PurchaseLottoValidator;
 import lotto.util.Util;
 
@@ -23,12 +24,36 @@ public class InputView {
         }
     }
 
+    public List<Integer> inputWinningNumber() {
+        try {
+            System.out.print(Message.INPUT_WINNING_NUMBER.message);
+            List<Integer> winnings = Util.splitNumberByComma(Console.readLine());
+            return winnings;
+        } catch (IllegalArgumentException exception) {
+            printExceptionMessage(exception);
+            return inputWinningNumber();
+        }
+    }
+
+    public int inputBonusNumber() {
+        try {
+            System.out.print(Message.INPUT_BONUS_NUMBER.message);
+            int bonus = Util.convertStringToInt(Console.readLine());
+            return bonus;
+        } catch (IllegalArgumentException exception) {
+            printExceptionMessage(exception);
+            return inputBonusNumber();
+        }
+    }
+
     private void printExceptionMessage(Exception exception) {
         System.out.println(exception.getMessage());
     }
 
     private enum Message {
-        INPUT_PURCHASE_LOTTO("구입금액을 입력해 주세요.\n");
+        INPUT_PURCHASE_LOTTO("구입금액을 입력해 주세요.\n"),
+        INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요.\n"),
+        INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n");
 
         private final String message;
 

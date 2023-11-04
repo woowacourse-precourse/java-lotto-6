c228dff6ecd3d09801e0b6628cace3a64fe5d024
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-05 07:04:02 +0900

    feat(dto): 로또 번호를 출력 형식에 맞게 반환하는 기능 구현

--- src/main/java/lotto/dto/LottoNumbersDTO.java
+++ src/main/java/lotto/dto/LottoNumbersDTO.java
@@ -1,4 +1,17 @@
 package lotto.dto;
 
-public class LottoResultDTO {
+import java.util.List;
+import lotto.model.Lotto;
+
+public class LottoNumbersDTO {
+    private final Lotto lotto;
+
+    public LottoNumbersDTO(Lotto lotto) {
+        this.lotto = lotto;
+    }
+
+    public String getFormattedLottoNumbers() {
+        List<Integer> lottoNumbers = lotto.getLotto();
+        return lottoNumbers.toString();
+    }
 }

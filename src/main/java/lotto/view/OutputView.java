b1491f73fe2a9e7ec4ec9e305a766650667f4a4f
leeedohyun <ldh19738@gmail.com>
2023-11-04 13:58:02 +0900

    feat: 발행한 로또 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,13 @@
 package lotto.view;
 
+import lotto.model.LottoTicket;
+
 public final class OutputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.\n";
 
     private OutputView() {
     }
@@ -13,13 +16,21 @@ public final class OutputView {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
     }
 
-    public static void printWinningNumbersMessage() {
+    public static void printNumberOfLotto(int NumberOfLotto) {
         printEmptyLine();
+        System.out.printf(PURCHASE_MESSAGE, NumberOfLotto);
+    }
+
+    public static void printLottoTicket(LottoTicket lottoTicket) {
+        lottoTicket.getLottos()
+                .forEach(System.out::println);
+    }
+
+    public static void printWinningNumbersMessage() {
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
     }
 
     public static void printBonusNumberMessage() {
-        printEmptyLine();
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
     }
 

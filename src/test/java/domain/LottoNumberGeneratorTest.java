acc717262cc713ffae3b76654bbb65885f77410c
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 23:47:28 +0900

    test: 무작위로 생성된 로또 번호 6개가 중복되지 않는지 테스트

---
+++ src/test/java/domain/LottoNumberGeneratorTest.java
@@ -0,0 +1,23 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class LottoNumberGeneratorTest {
+
+    private final LottoNumberGenerator lottoNumberGenerator= new LottoNumberGenerator();
+
+    @DisplayName("중복되지 않는 6개의 숫자가 생성되는지 확인")
+    @Test
+    void checkThereIsNoDuplicateNumber() {
+        List<Integer> numbers = lottoNumberGenerator.createLottoNumber();
+        List<Integer> distinctNumber = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        Assertions.assertThat(numbers.size()).isEqualTo(distinctNumber.size());
+    }
+}

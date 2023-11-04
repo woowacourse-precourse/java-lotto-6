900953b9d8664927c21bcbd75285a116e5660790
pwrwpw <dlgustjr9912@gmail.com>
2023-11-05 02:23:02 +0900

    fix: 숫자를 명시적으로 입력하여 출력하는 대신 메서드를 사용하여 값 받아오고 출력하도록 수정

--- src/main/java/lotto/view/ExceptionMessages.java
+++ src/main/java/lotto/view/ExceptionMessages.java
@@ -4,11 +4,11 @@ public enum ExceptionMessages {
 
     STRING_TO_INTEGER("입력 값은 숫자로만 이루어져 있어야 합니다"),
     DUPLICATION_LIST("입력 값들은 서로 중복되지 않아야 합니다."),
-    INPUT_LENGTH_WINNING_NUMBER("입력 값의 개수는 6개 여야 합니다."),
-    INPUT_LENGTH_BONUS_NUMBER("입력 값의 개수는 1개 여야 합니다."),
-    ERROR_INPUT_BONUS_NUMBER_RANGE("입력 값은 1,000의 배수여야 합니다."),
-    EXCEED("입력 값은 100,000 이하의 값이어야 합니다."),
-    NUMBER_IN_RANGE("입력 값의 범위는 1부터 45 사이의 숫자여야 합니다."),
+    INPUT_LENGTH_WINNING_NUMBER("입력 값의 개수는 %d개 여야 합니다."),
+    INPUT_LENGTH_BONUS_NUMBER("입력 값의 개수는 %d개 여야 합니다."),
+    ERROR_INPUT_BONUS_NUMBER_RANGE("입력 값은 %d의 배수여야 합니다."),
+    EXCEED("입력 값은 %d 이하의 값이어야 합니다."),
+    NUMBER_IN_RANGE("입력 값의 범위는 %d부터 %d 사이의 숫자여야 합니다."),
     DUPLICATED_NUMBER_IN_LIST("입력 값은 앞서 입력한 값들과 중복되지 않아야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
@@ -21,4 +21,20 @@ public enum ExceptionMessages {
     public void throwException() {
         throw new IllegalArgumentException(exceptionMessage);
     }
+
+    public void throwException(int param1) {
+        String message = String.format(exceptionMessage, param1);
+        throw new IllegalArgumentException(message);
+    }
+
+    public void throwException(int param1, int param2) {
+        String message = String.format(exceptionMessage, param1, param2);
+        throw new IllegalArgumentException(message);
+    }
+
+    public void throwException(int param1, int param2, int param3) {
+        String message = String.format(exceptionMessage, param1, param2, param3);
+        throw new IllegalArgumentException(message);
+    }
+
 }

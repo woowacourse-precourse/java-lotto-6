a683cb6bf4ceb99bf0567b455499e046a92f5b1d
jongmee <ra123221@gmail.com>
2023-11-04 21:45:08 +0900

    test(PurchaseNumberTest): 구입금액이 1000원 단위가 아닐 경우 예외 발생시키는 테스트하기

---
+++ src/test/java/lotto/domain/PurchaseNumberTest.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import static lotto.constants.ErrorMessage.INVALID_PURCHASE_AMOUNT;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PurchaseNumberTest {
+    @Test
+    void 구입금액이_1000원_단위가_아니면_예외가_발생한다() {
+        // given
+        Integer 구입금액 = 2500;
+
+        // when & then
+        assertThatThrownBy(() -> {
+            new PurchaseNumber(구입금액);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(INVALID_PURCHASE_AMOUNT);
+    }
+}

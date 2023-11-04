c0c86e9e2b680540b6269f254296e6b6035477eb
Chenowoo <cjsgusdn14@naver.com>
2023-11-04 17:14:08 +0900

    feat(InputValidation): 당첨 번호가 1부터 45사이의 숫자로 입력됐는지 검증하는 기능 구현

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -72,4 +72,12 @@ public class InputValidation {
         }
     }
 
+    public void validateWinnerNumbersOutOfRange(List<Integer> winnerNumbers) {
+        for (Integer number : winnerNumbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
 }

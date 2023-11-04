5ae166532de31344bb7be0a8e9c521b3cf69acb5
02ggang9 <02ggang9@gmail.com>
2023-11-05 04:31:01 +0900

    chore(LottoOutputMessage): 오타 수정

--- src/main/java/lotto/global/LottoOutputMessage.java
+++ src/main/java/lotto/global/LottoOutputMessage.java
@@ -3,7 +3,7 @@ package lotto.global;
 public enum LottoOutputMessage {
 
     PURCHASED_LOTTO_COUNT("개를 구매했습니다."),
-    LOTTO_RESULT("당철 통계\n---\n"),
+    LOTTO_RESULT("당첨 통계\n---\n"),
     ;
 
     private final String message;

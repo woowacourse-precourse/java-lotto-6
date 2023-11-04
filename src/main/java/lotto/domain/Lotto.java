e84204b657c6bbd930aeddd4fddf62668acb7c8d
JH-TT <leejongho9803@gmail.com>
2023-11-05 01:38:28 +0900

    refactor: Lotto 위치 이동 및 validation 기능 추가
    - 번호 리스트 출력하는 기능 추가
    - 상수값 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.utils.LottoUtils;
+import lotto.validation.Validation;
+
+public class Lotto {
+    protected static final int START = 1;
+    protected static final int END = 45;
+    protected static final int LENGTH = 6;
+    protected final List<Integer> numbers;
+
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = LottoUtils.sortAsc(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        Validation.checkLottoNumberLength(LENGTH, numbers);
+        Validation.checkLottoNumberRange(START, END, numbers);
+        Validation.checkDuplicate(numbers);
+    }
+
+    public void showNumbers() {
+        System.out.println(numbers);
+    }
+}

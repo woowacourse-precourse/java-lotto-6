2c8f37e51d91d8c78976b083fe6c13cf1b4fdd03
hil679 <hil679922@gmail.com>
2023-11-05 01:53:42 +0900

    Feat(setBonusLottoNum): 보너스 넘버 set 메소드 추가
    validate과정까지 진행

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,18 +1,27 @@
 package lotto.domain;
 
+import lotto.validator.BonusLottoNumValidator;
+
 import java.util.ArrayList;
 
 public class WinningLotto {
+    private static BonusLottoNumValidator bonusLottoNumValidator = new BonusLottoNumValidator();
+
     private Lotto lotto;
     private int bonusLottoNum;
 
-    public WinningLotto(Lotto lotto, int bonusLottoNum) {
+    public WinningLotto(Lotto lotto) {
         this.lotto = lotto;
+    }
+
+    public void setBonusLottoNum(int bonusLottoNum) {
+        validateBonusLottoNum(lotto, bonusLottoNum);
         this.bonusLottoNum = bonusLottoNum;
     }
 
-    private void validateBonusLottoNum() {
-
+    private void validateBonusLottoNum(Lotto lotto, int bonusLottoNum) {
+        bonusLottoNumValidator.checkRange(bonusLottoNum);
+        bonusLottoNumValidator.checkDuplicateWinningNumbers(lotto, bonusLottoNum);
     }
 
     public int matchNumberNum(Lotto userLotto) {

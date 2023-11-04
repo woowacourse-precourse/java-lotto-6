6a67f8f27b94ef04ad855871d8e20c4f440a0e12
oyoungsun <oyoungsun1030@gmail.com>
2023-11-04 15:41:45 +0900

    feat(OutputView) : 객체 생성, 에러/입력 메세지 출력 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class OutputView {
+    public static void printException(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public static void printInputMessage(String inputMessage) {
+        System.out.println(inputMessage);
+    }
+}

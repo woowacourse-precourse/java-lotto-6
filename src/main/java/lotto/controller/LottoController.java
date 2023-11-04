0c4085165e2bdbda31cfa8514519fc39508f2691
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-11-04 14:29:15 +0900

    feat: 예외가 발생하면 재입력 받도록 처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,16 +8,42 @@ import lotto.view.OutputView;
 
 public class LottoController {
 
+    private static  User user;
+    private static WinningNumber winningNumber;
+
     public void run() {
-        int amount = InputView.inputAmount();
-        User user = User.from(amount);
+        inputAmount();
+        purchaseLotto();
+        inputWinningNumberAndBonusNumber();
+        printLottoResult();
+    }
 
+    private void inputAmount() {
+        try {
+            int amount = InputView.inputAmount();
+            user = User.from(amount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            inputAmount();
+        }
+    }
+
+    private void purchaseLotto() {
         OutputView.printPurchasedLottos(user.purchaseLottos());
+    }
 
-        String[] winningNumbers = InputView.inputWinningNumbers();
-        int bonusNumber = InputView.inputBonusNumber();
-        WinningNumber winningNumber = WinningNumber.from(winningNumbers, bonusNumber);
+    private void inputWinningNumberAndBonusNumber() {
+        try {
+            String[] winningNumbers = InputView.inputWinningNumbers();
+            int bonusNumber = InputView.inputBonusNumber();
+            winningNumber = WinningNumber.from(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            inputWinningNumberAndBonusNumber();
+        }
+    }
 
+    private void printLottoResult() {
         LottoResult lottoResult = new LottoResult(user, winningNumber);
         OutputView.printLottoResult(lottoResult.getLottoResult(user.getPurchasedLottos(), winningNumber));
         OutputView.printYield(lottoResult.getYield());

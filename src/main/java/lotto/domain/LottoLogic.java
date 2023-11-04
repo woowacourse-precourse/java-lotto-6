919700d8117290433a6411ca3be25fc549f86377
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 14:48:55 +0900

    feat(LottoLogic): numberOfLottoPurchased 추가
    구매한 로또 수를 저장하는 numberOfLottoPurchased 변수를 추가했습니다.

--- src/main/java/lotto/domain/LottoLogic.java
+++ src/main/java/lotto/domain/LottoLogic.java
@@ -8,13 +8,15 @@ import lotto.utils.LottoUtil;
 public class LottoLogic {
     private final BigDecimal purchaseAmount;
     private final int bonusNumber;
+    private final BigDecimal numberOfLottoPurchased;
 
 
-    public LottoLogic(BigDecimal purchaseAmount, int bonusNumber) {
+    public LottoLogic(BigDecimal purchaseAmount, BigDecimal lottoPrice, int bonusNumber) {
         validateMultiplesOf(purchaseAmount, BigDecimal.valueOf(1000));
         LottoUtil.validateLottoNum(bonusNumber);
         this.purchaseAmount = purchaseAmount;
         this.bonusNumber = bonusNumber;
+        numberOfLottoPurchased = purchaseAmount.divide(lottoPrice, BigDecimal.ROUND_DOWN);
     }
 
     private static void validateMultiplesOf(BigDecimal num, BigDecimal factor) {

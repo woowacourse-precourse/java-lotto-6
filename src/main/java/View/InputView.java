23ad7e51f24cc539adff8684c36ee0b68d68f401
nayooniverse <kangna99@naver.com>
2023-11-04 12:31:41 +0900

    feat: 로또 구입 금액 입력 기능 구현 및 예외처리

---
+++ src/main/java/View/InputView.java
@@ -0,0 +1,19 @@
+package View;
+
+public class InputView {
+    public void printInputMoneyMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printInputLottoNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printInputBonusNumberMessage() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+    }
+
+    public void printResultMessage() {
+        System.out.println("당첨 통계");
+    }
+}
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import View.InputView;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        inputView.printInputMoneyMessage();
+        int money;
+        try {
+            money = Integer.parseInt(Console.readLine());
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+        if(money < 0) {
+            throw new IllegalArgumentException("[ERROR] 0 이상의 숫자를 입력해 주세요.");
+        }
+        if(money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000 단위로 입력해 주세요.");
+        }
     }
 }

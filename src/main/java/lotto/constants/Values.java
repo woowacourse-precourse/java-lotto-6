8c3a49a9fbbd3316800ab67956b61bc63b855c14
junest66 <chlwndks333@gmail.com>
2023-11-04 19:06:08 +0900

    feat(constants): 해당 프로젝트에서 사용할 Messages, Values를 상수로 지정

---
+++ src/main/java/lotto/constants/Values.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class Values {
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_PURCHASE_UNIT = 1000;
+    public static final String LOTTO_NUMBER_SEPARATOR = ", ";
+}

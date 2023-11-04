d2ef1f9b94be090ed8c47e6cc7613abdf47d211a
Junobee25 <wnsdhqo@naver.com>
2023-11-04 21:42:59 +0900

    feat(LottoService): generateRandomLottoNumbers 메서드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,8 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,5 +15,16 @@ public class LottoService {
         return Arrays.stream(userInput.split(COMMAS))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public List<List<Integer>> generateRandomLottoNumbers(){
+
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+
+        for (int count = 0; count < 8; count++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            lottoNumbers.add(numbers);
+        }
+        return lottoNumbers;
     }
 }

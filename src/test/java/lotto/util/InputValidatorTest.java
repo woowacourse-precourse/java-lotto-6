ddd7252cb8eebb1adf8ade4ba96a3d1eaafaef83
Yehyeok Bang <qkddpgur318@gmail.com>
2023-11-04 22:52:02 +0900

    feat(util): 입력 값 검증을 위한 InputValidator 클래스 추가
    사용자에게 입력받은 로또 구입 금액을 검증하는 기능을 추가합니다.
    - 로또 구입 금액이 숫자가 아닌 경우 예외가 발생합니다.
    - 로또 구입 금액이 1,000원 이하인 경우 예외가 발생합니다.
    - 로또 구입 금액이 1,000원 단위가 아닌 경우 예외가 발생합니다.

---
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -0,0 +1,37 @@
+package lotto.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputValidatorTest {
+    private final static String VALID_MONEY = "1000";
+    private final static String INVALID_MONEY_NOT_NUMBER = "abc";
+    private final static String INVALID_MONEY_MINIMUM_LIMIT = "999";
+    private final static String INVALID_MONEY_UNIT_LIMIT = "1001";
+
+    @Test
+    @DisplayName("올바른 구입 금액이 입력된 경우")
+    void validatePurchaseMoney() {
+        assertDoesNotThrow(() -> InputValidator.validatePurchaseMoney(VALID_MONEY));
+    }
+
+    @Test
+    @DisplayName("구입 금액이 숫자가 아닌 경우")
+    void validatePurchaseMoneyNotNumber() {
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validatePurchaseMoney(INVALID_MONEY_NOT_NUMBER));
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 미만인 경우")
+    void validatePurchaseMoneyMinimumLimit() {
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validatePurchaseMoney(INVALID_MONEY_MINIMUM_LIMIT));
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우")
+    void validatePurchaseMoneyUnitLimit() {
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validatePurchaseMoney(INVALID_MONEY_UNIT_LIMIT));
+    }
+}

7c8ddc0fc658f23be61115400de675e308e57e89
enduf7686 <enduf768640@gmail.com>
2023-11-04 12:33:23 +0900

    feat(validator): add validateLottoNumber() method

--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class Validator {
@@ -7,13 +8,13 @@ public class Validator {
     private int maxMoney = Integer.MAX_VALUE;
 
     public void validateMoney(String moneyInput) {
-        isNumber(moneyInput);
+        checkNumber(moneyInput);
         checkGoe0(moneyInput);
         checkLoeMaxMoney(moneyInput);
         checkDivisibleBy1000(moneyInput);
     }
 
-    private void isNumber(String moneyInput) {
+    private void checkNumber(String moneyInput) {
         if (!Pattern.matches("^\\d+$", moneyInput)) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
@@ -38,7 +39,35 @@ public class Validator {
     }
 
     public void validateLottoNumber(String lottoNumberInput) {
+        checkForm(lottoNumberInput);
+        checkRange(lottoNumberInput);
+        checkDuplication(lottoNumberInput);
+    }
 
+    private void checkForm(String lottoNumberInput) {
+        if (!Pattern.matches("^\\d+(,\\d+){5}$", lottoNumberInput)) {
+            throw new IllegalArgumentException("번호 양식에 맞게 입력해주세요.");
+        }
+    }
+
+    private void checkRange(String lottoNumberInput) {
+        int numberCount = (int) Arrays.stream(lottoNumberInput.split(","))
+                .mapToInt(Integer::parseInt)
+                .filter(number -> (number > 45 || number < 1))
+                .count();
+        if (numberCount != 6) {
+            throw new IllegalArgumentException("1~45 사이의 수를 입력해주세요.");
+        }
+    }
+
+    private void checkDuplication(String lottoNumberInput) {
+        int numberCount = (int) Arrays.stream(lottoNumberInput.split(","))
+                .mapToInt(Integer::parseInt)
+                .distinct()
+                .count();
+        if (numberCount != 6) {
+            throw new IllegalArgumentException("서로 다른 6개의 숫자를 입력해주세요.");
+        }
     }
 
     public void validateBonusNumber(String bonusNumberInput) {

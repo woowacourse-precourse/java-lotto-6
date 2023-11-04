834b27af8b72abf5f4df9ffd1c0864edebde4e2d
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-04 22:11:57 +0900

    <feat> : <Lottos를 유저의 입력을 받고 생성>
    Lottos 를 유저의 입력을 받고 유효성 검사를 거쳐 Lottos 를 생성하는 메서드 추가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,7 +1,44 @@
 package controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import domain.LottoSystem;
+import domain.Lottos;
+import service.LottoService;
+import util.Parser;
+import util.Validate;
+import view.LottoView;
+
+import java.util.List;
+
 public class LottoController {
+    private final Parser parser = new Parser();
+    private final Validate validate = new Validate();
+    private final int LOTTO_PRICE = 1000;
     public void run(){
 
     }
+
+    private Lottos makeLottos(){
+        return new Lottos(NumberOfLotto(getTotalMoneyByUserInput()));
+    }
+    private String getTotalMoneyByUserInput(){
+        LottoView.printAskInputMoney();
+        String input = Console.readLine();
+        return CheckValidateMoneyInput(input);
+    }
+
+    private String CheckValidateMoneyInput(String input){
+        try {
+            validate.CheckMoneyInput(input);
+            return input;
+        } catch (IllegalArgumentException e){
+            LottoView.printException(e.getMessage());
+            return  getTotalMoneyByUserInput();
+        }
+    }
+
+    private int NumberOfLotto(String input){
+        return parser.parseInteger(input)/LOTTO_PRICE;
+    }
 }
+

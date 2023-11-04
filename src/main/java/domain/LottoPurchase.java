81f57bb2a4429d64ddf37f5042cb5d030685406e
dltmddus <tmddus4671@gmail.com>
2023-11-04 14:18:22 +0900

    feat: 1개 lotto number 랜덤생성 기능 구현

--- src/main/java/domain/LottoPurchase.java
+++ src/main/java/domain/LottoPurchase.java
@@ -1,2 +1,17 @@
-package domain;public class LottoPurchase {
+package domain;
+
+import util.CreateRandomNumberUtil;
+import util.MessageUtil;
+
+import java.util.List;
+
+public class LottoPurchase {
+
+    private Lotto getRandomLottoNum(){
+        // 로또 넘버 하나! 랜덤 생성 하고 lotto 객체 반환
+        List<Integer> randomLottoNums = CreateRandomNumberUtil.getNumbers();
+        Lotto lotto = new Lotto(randomLottoNums);
+        MessageUtil.printPurchaseInfo(lotto.getLottoNumbers());
+        return lotto;
+    }
 }

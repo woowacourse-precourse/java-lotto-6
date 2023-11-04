5e5db6944083379228a2f736ce1bb19e68d43ccd
sangxxjin <sj_0368@naver.com>
2023-11-05 03:56:41 +0900

    feat: 보너스 번호를 입력받는 메소드 구현 및 상수처리

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private final static String PURCHASE_MESSAGE = "구입금액을 입력해 주세요";
     private final static String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    private final static String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
     public static String getPurchaseAmount() {
         System.out.println(PURCHASE_MESSAGE);
@@ -13,6 +14,11 @@ public class InputView {
 
     public static String getWinningNumber() {
         System.out.println(WINNING_NUMBER_PROMPT);
+        return Console.readLine();
+    }
+
+    public static String getBonusNumber() {
+        System.out.println(BONUS_NUMBER_PROMPT);
         return Console.readLine();
     }
 }

1a785ae483bae2377bdda60c9ce06350bbe715d7
eunhee <dmsgml9903@gmail.com>
2023-11-04 18:28:24 +0900

    test(LottoNumberValidatorTest): 보너스 값을 입력 받아 검증하는 기능 테스트
    아래 내용을 테스트:
    - 입력 받은 번호가 보너스 번호에 이미 존재하는지 검증 테스트
    - 입력 받은 번호가 숫자고 1과 45사이에 있는지 검증 테스트

--- src/test/java/lotto/validation/LottoNumberValidatorTest.java
+++ src/test/java/lotto/validation/LottoNumberValidatorTest.java
@@ -3,6 +3,8 @@ package lotto.validation;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -94,4 +96,88 @@ class LottoNumberValidatorTest {
                 .hasMessage("구분자 ,를 연속해서 입력할 수 없습니다.");
     }
 
+    @DisplayName("보너스 번호를 빈 문자열로 입력하면 에외를 발생시킨다.")
+    @Test
+    void emptyBonusNumberThrowIllegalArgumentException() {
+        //given
+        LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
+
+        //when, then
+        assertThatThrownBy(
+                () -> lottoNumberValidator.validateBonusNumber(
+                        "", new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 문자열을 입력할 수 없습니다.");
+    }
+
+    @DisplayName("보너스 번호를 공백으로 입력하면 예외를 발생시킨다.")
+    @Test
+    void blankBonusNumberThrowException() {
+        //given
+        LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
+
+        //when, then
+        assertThatThrownBy(
+                () -> lottoNumberValidator.validateBonusNumber(
+                        "   ", new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("공백으로만 이루어진 문자열을 입력할 수 없습니다.");
+    }
+
+    @DisplayName("숫자와 구분자로만 이루어지지 않은 문자열을 입력하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1.7.8.9.2.4", "1%7%8.9.2.4", "1/7/8/9/2/4", "1 7 8 9 2 4"})
+    void notNumberThrowIllegalArgumentException(String input) {
+        //given
+        LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
+
+        //when, then
+        assertThatThrownBy(
+                () -> lottoNumberValidator.validateBonusNumber(
+                        "string", new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력할 수 있습니다.");
+    }
+
+    @DisplayName("보너스 번호의 범위가 1과 45사이가 아니면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46", "-1", "50"})
+    void notInRangedBonusNumberThrowIllegalArgumentException(String bonusNumber) {
+        //given
+        LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
+
+        //when, then
+        assertThatThrownBy(
+                () -> lottoNumberValidator.validateBonusNumber(
+                        bonusNumber, new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1에서 45 사이의 숫자만 입력할 수 있습니다.");
+    }
+
+    @DisplayName("주어진 로또 번호에 이미 존재하는 값이면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4", "5", "6"})
+    void NumberAlreadyUsedInLottoThrowException() {
+        //given
+        LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
+
+        //when, then
+        assertThatThrownBy(
+                () -> lottoNumberValidator.validateBonusNumber(
+                        "1", new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또에 이미 있는 번호는 입력할 수 없습니다.");
+    }
+
+    @DisplayName("로또에 중복되지 않고 범위 내에 있는 숫자를 입력하면 예외를 발생시키지 않는다.")
+    @Test
+    void notDuplicatedNumberInRangeNotThrowException() {
+        //given
+        LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
+
+        //when, then
+        assertDoesNotThrow(
+                () -> lottoNumberValidator.validateBonusNumber(
+                        "30", new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"))));
+    }
 }

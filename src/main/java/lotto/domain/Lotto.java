a358447e2d200b39215af85c02f735ce9c526828
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-05 02:41:48 +0900

    [feat] domain 당첨 번호 입력값 유효성 검증 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.utility.LottoErrorMessage;
 
 // 입력하는 당첨 번호 6개
 public class Lotto {
@@ -8,15 +11,32 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicates(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     // 입력값이 6개인지
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LottoErrorMessage.WRONG_SIZE_ERROR.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 중복 요소가 있는지
+    private void validateDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(LottoErrorMessage.DUPLICATE_ERROR.getMessage());
+        }
+    }
+
+    // 각 요소가 1부터 45까지인
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(LottoErrorMessage.OUT_RANGE_ERROR.getMessage());
+            }
+        }
+    }
 }

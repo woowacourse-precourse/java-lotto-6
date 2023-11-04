df1bf493d003f3eb438e05b84ca4f739fccb8218
WonyuChoi <phillip07290@naver.com>
2023-11-04 14:55:05 +0900

    refactor: 필요없는 메서드 삭제

--- src/main/java/model/dto/AnswerBonusNumber.java
+++ src/main/java/model/dto/AnswerBonusNumber.java
@@ -1,10 +1,5 @@
 package model.dto;
 
-import model.LottoNumber;
-
 public record AnswerBonusNumber(String answerBonusNumber) {
 
-    public LottoNumber toLottoNumber() {
-        return new LottoNumber(Integer.parseInt(answerBonusNumber));
-    }
 }

69cba76ec1b4f381fa248947d0872ade4715a317
dong2ast <kdh01132006@naver.com>
2023-11-04 20:35:43 +0900

    ✨ feat: 로또 6개의 숫자 오름차순으로 정렬 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
     }
 

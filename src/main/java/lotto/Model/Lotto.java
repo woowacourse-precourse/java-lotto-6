282bc96731d328aa641d5c5601f0e83cce40ac9c
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-04 14:51:28 +0900

    feat(Lotto): 당첨번호의 숫자가 6개 아니면 예외, 중복되면 예외인 기능들 구현

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -9,7 +9,7 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-  //      validate(numbers);
+        validate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
@@ -18,22 +18,22 @@ public class Lotto {
     public String toString() {
         return numbers +"";
     }
-//    private void validate(List<Integer> numbers) {
-//        //validateSize(numbers);
-//        //validateDuplicate(numbers);
-//    }
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
 
-//    private void validateSize(List<Integer> numbers) {
-//        if (numbers.size() != 6) {
-//            throw new IllegalArgumentException();
-//        }
-//    }
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호를 6개 입력해 주세요.");
+        }
+    }
 
-//    private void validateDuplicate(List<Integer> numbers) {
-//        Set<Integer> nonDuplicate = new HashSet<>(numbers);
-//
-//        if (nonDuplicate.size() != 6) {
-//            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될수 없습니다.");
-//        }
-//    }
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicate = new HashSet<>(numbers);
+
+        if (nonDuplicate.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될수 없습니다.");
+        }
+    }
 }

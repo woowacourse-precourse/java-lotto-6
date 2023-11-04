087b90ee7ee828268d51234e9cfbd7694e4bfbc5
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-04 14:42:42 +0900

    feat: 구입금액에 맞게 로또티켓 생성 및 반환하는 기능 구현

---
+++ src/main/java/lotto/model/domain/LottoTicket.java
@@ -0,0 +1,41 @@
+package lotto.model.domain;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoTicket {
+    private final List<Lotto> lottos;
+    private final int pricePerLotto = LottoGameConstants.PRICE_PER_LOTTO.getValue();
+
+    public LottoTicket(int price) {
+        this.lottos = buyLottoTicket(price);
+    }
+
+    private List<Lotto> buyLottoTicket(int price) {
+        validate(price);
+        final int count = price / pricePerLotto;
+        return createLottoTicket(count);
+    }
+
+    private List<Lotto> createLottoTicket(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(lotto -> new Lotto(generateLottoNumbers()))
+                .toList();
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        return lottoGenerator.generateLottoNumbers();
+    }
+
+    private void validate(int price) {
+        if (price % pricePerLotto != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 금액은 1000단위여야 합니다.");
+        }
+    }
+
+    public List<Lotto> getLottoTicket() {
+        return this.lottos;
+    }
+
+}

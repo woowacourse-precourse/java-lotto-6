bab6779651a515b82b6bdaf499cc43e73b8c0453
unknown <winternrabbit@gmail.com>
2023-11-04 21:51:29 +0900

    refactor: 실행 결과 예시에 맞추어 출력 결과 수정

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -4,6 +4,6 @@ public class Output {
     private static final String NUMBER_OF_PURCHAGE = "개를 구매했습니다.";
 
     public void printNumberOfPurchase(int lottoTicket) {
-        System.out.println(lottoTicket + NUMBER_OF_PURCHAGE);
+        System.out.println("\n" + lottoTicket + NUMBER_OF_PURCHAGE);
     }
 }

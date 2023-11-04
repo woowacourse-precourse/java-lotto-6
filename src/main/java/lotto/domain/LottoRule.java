997839d8b0a6698d1bcbb2fe18b3ee8629986e76
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 02:53:47 +0900

    feat: 구매 금액 입력 관련 Rule, Message 추가

--- src/main/java/lotto/domain/LottoRule.java
+++ src/main/java/lotto/domain/LottoRule.java
@@ -4,7 +4,10 @@ public enum LottoRule {
 
     MIN_LOTTO_NUMBER(1),
     MAX_LOTTO_NUMBER(45),
-    LOTTO_LENGTH(6);
+    LOTTO_LENGTH(6),
+    LOTTO_PRICE(1000),
+    ;
+
 
     private int value;
 

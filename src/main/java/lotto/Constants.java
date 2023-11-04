efafe58b393035952a7d155fc4f5e78b6a4b859c
LeeJuWon <woen1016@gmail.com>
2023-11-04 12:40:36 +0900

    Constants 생성

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public class Constants {
+    public static final int RANDOM_START_NUM = 1;
+    public static final int RANDOM_END_NUM = 45;
+    public static final int RANDOM_WINNING_NUM_COUNT = 6;
+    public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
+    public static final String WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    public static final String BOUNS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---";
+    public static final String THREE_MATCHES_MESSAGE = "3개 일치 (5,000원) - ";
+    public static final String FOUR_MATCHES_MESSAGE = "4개 일치 (50,000원) - ";
+    public static final String FIVE_MATCHES_MESSAGE = "5개 일치 (1,500,000원) - ";
+    public static final String SIX_MATCHES_MESSAGE = "6개 일치 (2,000,000,000원) - ";
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String NUMBER_RANGE_ERROR = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+}

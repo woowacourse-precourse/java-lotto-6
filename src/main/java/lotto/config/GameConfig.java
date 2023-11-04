53e950377d2a4c648f860e8b9e0e0f6fb1fcbee6
Minjae-An <mj3242@naver.com>
2023-11-04 15:05:49 +0900

    feat : 게임 설정 상수 정의

---
+++ src/main/java/lotto/config/GameConfig.java
@@ -0,0 +1,10 @@
+package lotto.config;
+
+public final class GameConfig {
+    public static final Integer START_INCLUSIVE=1;
+    public static final Integer END_INCLUSIVE=45;
+    public static final Integer NUMBER_OF_LOTTO_NUMBERS=6;
+    public static final Integer LOTTO_NUMBER_UNIT=1_000;
+
+    private GameConfig(){}
+}

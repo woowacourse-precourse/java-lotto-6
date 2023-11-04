8bd4f0b05f03853ba4e150877d02e90d3c87d186
02ggang9 <02ggang9@gmail.com>
2023-11-04 21:37:18 +0900

    refactor(LottoPublisher): 스트림을 이용해 Lotto 리스트를 생성하도록 수정

--- src/main/java/lotto/LottoPublisher.java
+++ src/main/java/lotto/LottoPublisher.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoPublisher {
 
@@ -13,23 +14,23 @@ public class LottoPublisher {
     }
 
     public List<Lotto> publishLotto(Long purchaseAmount) {
-        long lottoNumber = calculateLottoAmount(purchaseAmount);
-
-        ArrayList<Lotto> lottoList = new ArrayList<>();
-
-        for (int i = 0; i < lottoNumber; i++) {
-            List<Integer> randomNumbersImmutable = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            ArrayList<Integer> randomNumbers = new ArrayList<>(randomNumbersImmutable);
-            Collections.sort(randomNumbers);
-            Lotto lotto = new Lotto(randomNumbers);
-            lottoList.add(lotto);
-        }
-
-        return lottoList;
+        int purchasedLottoCount = calculateLottoAmount(purchaseAmount);
+        return IntStream.range(0, purchasedLottoCount)
+                .mapToObj(i -> generateLotto())
+                .toList();
     }
 
-    private long calculateLottoAmount(Long purchaseAmount) {
-        return purchaseAmount / 1000;
+    private Lotto generateLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted(Integer::compareTo)
+                .toList();
+
+        return new Lotto(randomNumbers);
+    }
+
+    private int calculateLottoAmount(Long purchaseAmount) {
+        return (int) (purchaseAmount / 1000);
     }
 
 }

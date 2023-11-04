cb412013f69f78e5292a24cbdf9e8b9d3e6ef5c4
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 06:38:05 +0900

    feat(LottosMachine) : Lottos구매하는 기능 구현

---
+++ src/main/java/lotto/domain/generator/LottosMachine.java
@@ -0,0 +1,29 @@
+package lotto.domain.generator;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoMoney;
+import lotto.domain.lotto.Lottos;
+
+public class LottosMachine {
+
+    private final LottoGenerator lottoGenerator;
+
+    public LottosMachine(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public Lottos buy(LottoMoney lottoMoney) {
+        int count = lottoMoney.calculatePurchasableCount(1000);
+        List<Lotto> lottos = IntStream.range(0, count)
+                .mapToObj(i -> lottoGenerator.create())
+                .toList();
+
+        return new Lottos(lottos);
+    }
+
+
+
+
+}

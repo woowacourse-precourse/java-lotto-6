68375075d3726aa8ecb5448560f6371b164a1dbe
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 05:07:14 +0900

    test(LottoTest) : 로또 생성 기능 테스트
    1. 로또 번호 1에서 45사이 아니라면 예외
    2. 로또 예외 없이 생성되나 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
-import lotto.domain.Lotto;
+import lotto.domain.lotto.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,6 +27,24 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @Test
+    @DisplayName("로또 번호가 1에서 45사이가 아니라면 예외가 발생한다.")
+    void 로또번호_예외_발생_테스트() {
+        assertThatThrownBy(() -> new Lotto(List.of(14, 46, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 번호가 중복되지 않고 1에서 45사이의 숫자 6개라면 예외 없이 생성된다.")
+    void 로또_생성_테스트() {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
 
-    // 아래에 추가 테스트 작성 가능
+        //when
+        Lotto lotto = new Lotto(numbers);
+
+        //then
+        Assertions.assertNotNull(lotto);
+        Assertions.assertDoesNotThrow(() -> new Lotto(numbers));
+        assertThat(lotto.getNumbers()).isEqualTo(numbers);
+    }
 }

42ae9203d8c34c22af7ee77a5b9b41ddcd11bf4c
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-05 07:43:01 +0900

    Service Singleton Pattern 구현

--- src/main/java/Model/Service.java
+++ src/main/java/Model/Service.java
@@ -1,4 +1,14 @@
 package Model;
 
 public class Service {
+    private Service() {
+    }
+
+    private class Singleton {
+        private static final Service INSTANCE = new Service();
+    }
+
+    public static Service getInstance() {
+        return Singleton.INSTANCE;
+    }
 }

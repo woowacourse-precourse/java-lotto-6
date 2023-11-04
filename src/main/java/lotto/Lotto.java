03d770a9e21b09cd23fb2fc975387fa9d26d069d
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-04 17:46:31 +0900

    remove: 오름차순 정렬 메서드 삭제

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,5 @@
 package lotto;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,7 +7,6 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        sort(numbers);
         this.numbers = numbers;
     }
 
@@ -16,10 +14,6 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-    }
-
-    public void sort(List<Integer> numbers) {
-        Collections.sort(numbers);
     }
 
     public List<Integer> getLotto() {

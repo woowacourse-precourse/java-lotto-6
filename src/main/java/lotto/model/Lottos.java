3d96b8ed2fdb08840db9ac7ef655daae141518cc
hYe807n <rlagkdus55@gmail.com>
2023-11-04 21:08:01 +0900

    Feat: index를 통해 Price enum class 반환 기능 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,6 +1,10 @@
 package lotto.model;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -11,5 +15,19 @@ public class Lottos {
 
     public List<Lotto> getLotto() {
         return lottos;
+    }
+
+    public List<Integer> calculateScore(Lotto answer) {
+        return lottos.stream()
+                .map(lotto -> lotto.calculateScore(answer))
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> sortScore(Lotto answer) {
+        Map<Enum, Integer> scores = new HashMap<>();
+        int i = 0;
+        for (int score: calculateScore(answer)) {
+            scores.put(Price.get, score);
+        }
     }
 }

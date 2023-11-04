e277978dc2d818db6dda8c3f249662046fba66a8
seminchoi <choicco89@gmail.com>
2023-11-04 17:52:29 +0900

    feat(view.InputView) : InputView 클래스 생성, 메소드 정의 -구현부 미완성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import java.util.List;
+
+public class InputView {
+    int readIntLine() {
+        return 0;
+    }
+    List<Integer> readIntListLine() {
+        return List.of(0);
+    }
+}

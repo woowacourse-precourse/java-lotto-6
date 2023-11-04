b53ca4d8f5ecdb1bc1b52df4059aa14083ed73df
voyager003 <miraclex000@gmail.com>
2023-11-05 00:33:02 +0900

    feat(LottoController): add method to LottoController
    ADD inputAmount() method
    - input lotto amount and return LottoAmount

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,23 @@
 package lotto.controller;
 
 
+import lotto.View.ErrorMessageView;
+import lotto.View.InputView;
+import lotto.model.LottoAmount;
+
 public class LottoController {
 
     public void start() {
+        LottoAmount amount = inputAmount();
+    }
 
+    private LottoAmount inputAmount() {
+        try {
+            String LottoAmount = InputView.inputLottoAmount();
+            return new LottoAmount(LottoAmount);
+        } catch (IllegalArgumentException e) {
+            ErrorMessageView.printErrorWrongAmount();
+            return inputAmount();
+        }
     }
 }

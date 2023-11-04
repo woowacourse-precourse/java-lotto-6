94e558268a047328846a0889aacf00363553a93d
김영관 <kimyogun78@naver.com>
2023-11-04 15:07:00 +0900

    fix: Lotto Class 로또번호 중복 체크메소드 추가

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.Model;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +16,28 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if(!duplicationCheck(numbers))
+            throw new IllegalArgumentException();
+    }
+
+    private boolean duplicationCheck(List<Integer> numbers){
+        int[] cnt = new int[46];
+        Arrays.fill(cnt, 0);
+
+        boolean result = true;
+
+        Iterator<Integer> it = numbers.iterator();
+        while(it.hasNext()){
+            cnt[it.next()]++;
+        }
+
+        for(int i = 1; i < cnt.length; i++)
+            if(cnt[i] > 1){
+                result = false;
+                break;
+            }
+
+        return result;
     }
 
     // TODO: 추가 기능 구현

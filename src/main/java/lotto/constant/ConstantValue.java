6ffdf1a7ab441763cf2b96f0bcdcc5827a6b8149
yshong1998 <yshong1998@gmail.com>
2023-11-05 01:59:45 +0900

    Feat(ConstantValue): 규칙이 되는 상수를 관리하는 기능 추가

---
+++ src/main/java/lotto/constant/ConstantValue.java
@@ -0,0 +1,9 @@
+package lotto.constant;
+
+public class ConstantValue {
+
+    public static final Integer START_INCLUSIVE = 1;
+    public static final Integer END_INCLUSIVE = 45;
+    public static final Integer LOTTO_PRICE = 1000;
+    public static final Integer LOTTO_NUMBERS_SIZE = 6;
+}

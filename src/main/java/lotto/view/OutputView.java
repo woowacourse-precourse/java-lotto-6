23e9c14138eb1c45ec6af818e475f14e1b5db86b
whxogus215 <whxogus215@naver.com>
2023-11-05 00:21:19 +0900

    feat(OutputView): 수익률 반환 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.utils.enums.LottoCount;
 
 public class OutputView {
@@ -11,7 +12,10 @@ public class OutputView {
     private final String OUTPUT_LINE = "---";
     public void outputCreatedLottos(List<List<Integer>> lottos) {
         System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
-        lottos.forEach(System.out::println);
+        for (List<Integer> lotto : lottos) {
+            List<Integer> sortedLotto = lotto.stream().sorted().collect(Collectors.toList());
+            System.out.println(sortedLotto);
+        }
     }
 
     public void outputWinningResult(Map<LottoCount, Integer> resultTable) {
@@ -30,5 +34,10 @@ public class OutputView {
             Integer duplicatedNumber = resultTable.get(lottoCount);
             System.out.println(content + " (" + price + ")" + " - " + duplicatedNumber + "개");
         }
+    }
+
+    public void outputWinningRate(long winningTotalPrice, int inputPrice) {
+        double winningRate = (double) winningTotalPrice / inputPrice;
+        System.out.printf("총 수익률은 %.1f%%입니다.", winningRate * 100);
     }
 }

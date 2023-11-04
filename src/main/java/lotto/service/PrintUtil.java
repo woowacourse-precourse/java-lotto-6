b62e498f90b9e2191e15f6e06f9c98f80b3fa72f
Chenowoo <cjsgusdn14@naver.com>
2023-11-04 19:21:23 +0900

    feat(PrintUtil): 구매한 로또 갯수 문구를 출력하는 기능 구현

--- src/main/java/lotto/service/PrintUtil.java
+++ src/main/java/lotto/service/PrintUtil.java
@@ -6,11 +6,17 @@ public class PrintUtil {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void printNumberOfLotto(int number) {
+        System.out.println();
+        System.out.println(number + "개를 구매했습니다.");
+    }
+
     public void printWinnerNumbersInput() {
         System.out.println("당첨 번호를 입력해주세요.");
     }
 
     public void printBonusNumberInput() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해주세요.");
     }
 }

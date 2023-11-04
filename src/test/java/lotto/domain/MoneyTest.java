e154d0adddf09dfbec5e69979d81bcb3bec6a63d
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-04 15:48:50 +0900

    test(Money): ParameterizedTest 테스트 케이스 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -2,30 +2,34 @@ package lotto.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
 
     @DisplayName("금액은 숫자만 가능하다.")
-    @Test
-    void isNumeric() {
-        Assertions.assertThatThrownBy(() -> Money.of("하이"))
+    @ParameterizedTest
+    @ValueSource(strings = {"", "천원", "1000won"})
+    void isNumeric(String moneys) {
+        Assertions.assertThatThrownBy(() -> Money.of(moneys))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자만 입력해 주세요.");
     }
 
     @DisplayName("최소 금액은 1,000원 이상 이여야 한다.")
-    @Test
-    void isOverThousand() {
-        Assertions.assertThatThrownBy(() -> Money.of("500"))
+    @ParameterizedTest
+    @ValueSource(strings = {"-1000", "0", "500"})
+    void isOverThousand(String moneys) {
+        Assertions.assertThatThrownBy(() -> Money.of(moneys))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("최소 금액은 1,000원 입니다.");
     }
 
     @DisplayName("금액은 1,000원 단위여야 한다.")
-    @Test
-    void isDivisibleByThousand() {
-        Assertions.assertThatThrownBy(() -> Money.of("1200"))
+    @ParameterizedTest
+    @ValueSource(strings = {"1200", "1350", "1557"})
+    void isDivisibleByThousand(String moneys) {
+        Assertions.assertThatThrownBy(() -> Money.of(moneys))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("금액은 1,000원 단위로 입력해 주세요.");
     }

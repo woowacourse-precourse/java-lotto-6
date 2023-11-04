094bdcfcfbcc59eb65a39fd4120322ab4f1a220b
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-05 05:03:29 +0900

    feat(Application): 게임 시작점 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        LottoService lottoService = new LottoService(new InputView(), new OutputView());
+        lottoService.run();
     }
 }

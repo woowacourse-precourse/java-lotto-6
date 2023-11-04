885da6b32c3f8ad8ee6ff3cfb3bb6e8143d23912
dong2ast <kdh01132006@naver.com>
2023-11-04 20:21:00 +0900

    ✨ feat: 사용자에게 로또 구입 금액 입력 받기 구현 예외처리 구현 및 예외 복구 방법 적용

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,10 +1,24 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoService {
 
-    private final Integer LOTTO_PRICE = 1000;
+    private static final Integer LOTTO_PRICE = 1000;
 
     public static void run() {
-
+        System.out.println("구입금액을 입력해 주세요.");
+        int tryNum;
+        while (true) {
+            try {
+                tryNum = Integer.parseInt(Console.readLine());
+                if (tryNum%LOTTO_PRICE==0){
+                    break;
+                }
+                System.out.println("로또 구입 금액은 1000원의 양의 정수배만 입력 가능합니다.");
+            } catch (NumberFormatException e) {
+                System.out.println("로또 구입 금액은 1000원의 양의 정수배만 입력 가능합니다.");
+            }
+        }
     }
 }

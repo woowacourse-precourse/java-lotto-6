6ecaebf4dcce1b9a152e45a2cb9a508900b384ad
minsang-alt <mazinger97@naver.com>
2023-11-04 17:10:06 +0900

    feat: 로또 발행 클래스 구현
    - 인자에 전달된 돈이 1000원 단위인지 유효성 검사(전달된 돈만큼 로또 발행)
    - 로또 발행은 LottoGenerator 객체 호출하여 해당 로또들이 저장된 객체를 반환받는다
    - 반환된 객체에서 가지고 있는 로또 수와 실제로 발행해야될 수와 다를 경우 예외처리

---
+++ src/main/java/lotto/LotteryKiosk.java
@@ -0,0 +1,35 @@
+package lotto;
+
+public class LotteryKiosk {
+
+    private static final int UNIT_MONEY = 1000;
+    private final LottoGenerator lottoGenerator;
+
+    public LotteryKiosk(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public Lottos publish(int money) {
+        int issueCount = issueCount(money);
+        Lottos lottoBundle = lottoGenerator.createLottoBundle(issueCount);
+        validateNormalCreate(lottoBundle, issueCount);
+        return lottoBundle;
+    }
+
+
+    public int issueCount(int money) {
+        if (money % UNIT_MONEY != 0 || money <= 0) {
+            throw new IllegalArgumentException("[ERROR] 사용자는 1000원 단위로 구입할 수 있습니다.");
+        }
+        return money / UNIT_MONEY;
+    }
+
+
+    private void validateNormalCreate(Lottos lottos, int issueCount) {
+        if (issueCount != lottos.getSize()) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 로또 발급을 실패했습니다." + "Actual : [" + lottos.getSize() + "] Expected: [" + issueCount + "]");
+        }
+    }
+
+}

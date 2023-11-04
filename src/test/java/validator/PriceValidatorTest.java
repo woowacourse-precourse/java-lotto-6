6619912a1faba90cf1c867ddeee854abf6a3c6d3
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 18:33:51 +0900

    test: 숫자 이외의 문자가 포함되었을 때 에러 발생 여부 검사

--- src/test/java/validator/PriceValidatorTest.java
+++ src/test/java/validator/PriceValidatorTest.java
@@ -1,7 +1,19 @@
 package validator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PriceValidatorTest {
+    private final PriceValidator priceValidator = new PriceValidator();
 
+    @DisplayName("구매 금액이 숫자 이외의 문자가 포함되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"123a","abc","a1000"," 100","  2  "})
+    void priceContainNonNumericCharacter(String input){
+        assertThatThrownBy(() -> priceValidator.checkPriceValidate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

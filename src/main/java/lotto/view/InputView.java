0840b02658312bc6b440d9152cfcc2c22dd38faa
whxogus215 <whxogus215@naver.com>
2023-11-04 11:00:14 +0900

    fix(InputView): 에러코드 [ERROR]로 출력되게 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,11 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Casher;
+import lotto.utils.Code;
 
 public class InputView {
     private final String INPUT_PRICE = "구입금액을 입력해 주세요.";
     private final String NOT_NUMBER = "유효한 숫자 형식이 아닙니다. 다시 입력해 주세요.";
-    private final String ERROR_CODE = "[ERROR]";
 
     public void inputLottoPrice() {
         System.out.println(INPUT_PRICE);
@@ -21,7 +21,7 @@ public class InputView {
                 lottoCount = casher.getLottoCount(Integer.parseInt(inputPrice));
                 break;
             } catch (IllegalArgumentException e) {
-                System.out.println(ERROR_CODE + " " + e.getMessage());
+                System.out.println(Code.Error.getCode() + " " + e.getMessage());
             }
         }
         // 로또 생성기에게 lottoCount를 전달해서 로또 생성 요청

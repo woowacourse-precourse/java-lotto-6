e7510727a79a33de8d14f01bda432462435bf6f4
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-04 20:58:02 +0900

    test(WinningLottoTest): 당첨번호에 보너스 볼이 있는지 검증 테스트 추가

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,6 +1,9 @@
 package lotto;
 
 
+import java.util.List;
+import lotto.Model.Bonus;
+import lotto.Model.Lotto;
 import lotto.Model.WinningLotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -26,5 +29,15 @@ public class WinningLottoTest {
         Assertions.assertThatThrownBy(() -> new WinningLotto(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 쉼표로 끝나는 당첨번호는 입력할 수 없습니다.");
+    }
+
+    @DisplayName("당첨번호에 보너스 번호가 있으면 예외 테스트")
+    @Test
+    void compare() {
+        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6");
+
+        Assertions.assertThatThrownBy(() -> winningLotto.compare(new Bonus("5")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다.");
     }
 }

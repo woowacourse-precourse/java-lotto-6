da7b7cba5b07e45e2bbb5e9ad72a452c4d9ae352
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 15:39:31 +0900

    feat(Validator): 당첨 번호 1~45 사이인지 확인하는 기능 추가

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -5,8 +5,11 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_RANGE_NUM = 1;
+    private static final int MAX_RANGE_NUM = 45;
     private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
     private static final String WRONG_UNIT_ERROR_MESSAGE = "[ERROR] 입력 금액은 1,000원 단위여야 합니다.";
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.";
 
     public static void validateNonNumeric(String input) {
         String pattern = "^[0-9]*$";
@@ -22,9 +25,21 @@ public class Validator {
         }
     }
 
+    public static void validateRange(int input) {
+        if (input < MIN_RANGE_NUM || input > MAX_RANGE_NUM) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+        }
+    }
+
     public static void validateNonNumericNumbers(List<String> input) {
         for (String num : input) {
             validateNonNumeric(num);
+        }
+    }
+
+    public static void validateRangeOfNumbers(List<Integer> numbers) {
+        for (int num : numbers) {
+            validateRange(num);
         }
     }
 }

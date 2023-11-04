017bb42b4778f1005bc88aeff3915254448d2a14
junghunim07 <dlawndgns9@naver.com>
2023-11-04 18:11:12 +0900

    feat(Input): 당첨번호 입력받는 메소드 구현

--- src/main/java/lotto/ui/Input.java
+++ src/main/java/lotto/ui/Input.java
@@ -2,11 +2,28 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Input {
     public int getUserMoney() {
         try {
             int money = Integer.parseInt(Console.readLine());
             return money;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    public List<Integer> getWinningLottoNumber() {
+        List<Integer> winningNumber = new ArrayList<>();
+        try {
+            String input = Console.readLine();
+            String[] numbers = input.split(",");
+            for (int i = 0; i < numbers.length; i++) {
+                winningNumber.add(Integer.parseInt(numbers[i]));
+            }
+            return winningNumber;
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR]");
         }

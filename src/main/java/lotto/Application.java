37455c69cc1de120e9b69c4c1ac82b04aa320ad5
Sanghwi Lee <sanghwi47@naver.com>
2023-11-04 15:20:31 +0900

    feat(Application,Controller) Controller을 통해 InputView와 LottoTicket을 연결하여 로또의 개수를 구하는 기능이 작동하도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,8 @@
 package lotto;
-
+import lotto.controller.Controller;
 public class Application {
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
     }
 }
--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,27 @@
 package lotto.controller;
 
+import lotto.model.LottoTicket;
+import lotto.view.InputView;
+
 public class Controller {
+    InputView inputView;
+    int ticketCount;
+    private static final String ERROR = "[ERROR] ";
+
+    public void run(){
+        lottoSetting();
+    }
+
+    private void lottoSetting() {
+        while (true) {
+            String moneyInput = InputView.inputMoney();
+            try {
+                LottoTicket lottoTicket = new LottoTicket(moneyInput);
+                this.ticketCount = lottoTicket.getTicketCount();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR + e.getMessage());
+            }
+        }
+    }
 }

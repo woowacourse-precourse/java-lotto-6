8b58dfca41cf28bf8151561db7e5bc19afd35d2b
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 14:07:36 +0900

    feat(LottoLogic): 구입 금액을 BigDecimal로 변경
    구입 금액은 int의 범위를 벗어날 수 있기에 제약이 없는 BigDecimal로 변경했습니다.

--- src/main/java/lotto/domain/LottoLogic.java
+++ src/main/java/lotto/domain/LottoLogic.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
 public class LottoLogic {
-    public final int purchaseAmount;
-    public final int bonusNumber;
+    private final BigDecimal purchaseAmount;
+    private final int bonusNumber;
 
 
     public LottoLogic(int purchaseAmount, int bonusNumber) {
+    public LottoLogic(BigDecimal purchaseAmount, int bonusNumber) {
         this.purchaseAmount = purchaseAmount;
         this.bonusNumber = bonusNumber;
     }

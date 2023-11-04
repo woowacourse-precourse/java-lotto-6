ff3d788e379dca45136dfdf04f69b1263989a5cf
junest66 <chlwndks333@gmail.com>
2023-11-04 19:28:23 +0900

    feat(InputView): 사용자의 입력을 받는 함수 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String getInput() {
+        return Console.readLine();
+    }
+}

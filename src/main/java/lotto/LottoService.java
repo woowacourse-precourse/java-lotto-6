f7f820ce6853b07a554a5d0e0081d8f1d6d12d29
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-04 14:12:30 +0900

    feat : 구입한 금액에 대한 로또 번호 생성 기능 추가

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class LottoService {
@@ -8,7 +12,11 @@ public class LottoService {
     private static final String BUY_MONEY_RANGE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 0원보다 큰 금액을 입력해야 합니다.";
     private static final Pattern NUMBER_MATCH = Pattern.compile("^[0-9]*$");
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+    private static final int LOTTO_NUMBER_STORE_SIZE = 6;
     private static final int ZERO = 0;
+    private final List<List<Integer>> lottoNumberStore = new ArrayList<>();
     private final Player player = new Player();
 
     public int calculateLottoTicket(String inputMoney) {
@@ -18,6 +26,17 @@ public class LottoService {
 
     public void settingBuyMoney(int inputMoney) {
         player.settingBuyMoney(inputMoney);
+    }
+
+    public void createLottoByInputBuyMoney() {
+        for (int i = 0; i < player.getBuyMoney(); i++) {
+            lottoNumberStore
+                    .add(Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBER_STORE_SIZE));
+        }
+    }
+
+    public List<List<Integer>> getLottoNumberStore() {
+        return lottoNumberStore;
     }
 
     public void validateBuyMoney(String inputMoney) {

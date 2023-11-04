154fdc5e4add36b7fbd90cbda1d741d75d1aa6c2
Minjae-An <mj3242@naver.com>
2023-11-04 20:56:11 +0900

    feat : 게임 설정 상수 수정
    생성자 위치 최상단으로 수정.
    상금 상수들 추가.

--- src/main/java/lotto/config/GameConfig.java
+++ src/main/java/lotto/config/GameConfig.java
@@ -1,10 +1,17 @@
 package lotto.config;
 
-public final class GameConfig {
-    public static final Integer START_INCLUSIVE=1;
-    public static final Integer END_INCLUSIVE=45;
-    public static final Integer NUMBER_OF_LOTTO_NUMBERS=6;
-    public static final Integer LOTTO_NUMBER_UNIT=1_000;
+public class GameConfig {
+    private GameConfig() {
+    }
 
-    private GameConfig(){}
+    public static final Integer START_INCLUSIVE = 1;
+    public static final Integer END_INCLUSIVE = 45;
+    public static final Integer NUMBER_OF_LOTTO_NUMBERS = 6;
+    public static final Integer LOTTO_NUMBER_UNIT = 1_000;
+
+    public static final Integer THREE_NUMBER_MATCH_PRIZE = 5_000;
+    public static final Integer FOUR_NUMBER_MATCH_PRIZE = 50_000;
+    public static final Integer FIVE_NUMBER_MATCH_PRIZE = 1_500_000;
+    public static final Integer FIVE_NUMBER_MATCH_PRIZE_WITH_BONUS_PRIZE = 30_000_000;
+    public static final Integer TOTAL_NUMBER_MATCH_PRIZE = 2_000_000_000;
 }

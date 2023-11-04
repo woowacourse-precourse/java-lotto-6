cfe7039da6b9291f478580d888f73ba7e72cc60c
dmswl6310 <dmswl6310@gmail.com>
2023-11-05 00:13:25 +0900

    feat: 구입금액이 1000미만 일 때 예외처리
    로또 발행 갯수가 1개 이상이기 위해서 1000원 이상만 가능

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -14,5 +14,9 @@ public class Game {
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("1000원으로 나누어떨어지지 않습니다");
         }
+
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException("구입금액은 1000원 보다 커야합니다.");
+        }
     }
 }

ffc162cc9fd6e29c7ccbf9246dcf914f9adec51f
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 02:47:06 +0900

    feat : 사용자의 로또를 생성하기 위해 랜덤 숫자 6개를 생성하는 기능 구현

---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,16 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.constant.LottoInfo;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    @Override
+    public List<Integer> pickNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LottoInfo.LOTTO_MIN_NUMBER.getValue(),
+                LottoInfo.LOTTO_MAX_NUMBER.getValue(), LottoInfo.LOTTO_SIZE.getValue());
+
+        return numbers;
+    }
+}

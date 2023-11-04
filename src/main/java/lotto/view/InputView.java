40323d162ae837422cb3ca73dd91a687bd73f373
waveofmymind <sjun0913@gmail.com>
2023-11-04 22:07:32 +0900

    feat: 당첨 번호를 입력 받는 기능을 구현한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,22 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String DELIMITER = ",";
 
     public static int readPayMoney() {
         System.out.println(ViewConstraints.INPUT_MONEY.getMessage());
         String input = Console.readLine();
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> readWinningNumbers() {
+        System.out.println(ViewConstraints.INPUT_WINNING_NUMBERS.getMessage());
+        String input = Console.readLine();
+        return Arrays.stream(input.split(DELIMITER)).map(Integer::parseInt).toList();
     }
 }
--- src/main/java/lotto/view/ViewConstraints.java
+++ src/main/java/lotto/view/ViewConstraints.java
@@ -2,7 +2,8 @@ package lotto.view;
 
 enum ViewConstraints {
 
-    INPUT_MONEY("구입금액을 입력해 주세요.");
+    INPUT_MONEY("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요.");
 
     final private String message;
 

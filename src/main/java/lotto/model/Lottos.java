e516bacd963b0a48a39ac97b4819e4e71c965f90
leesangjun <hubls@naver.com>
2023-11-04 16:22:31 +0900

    feat: iterator 구현 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,11 +1,17 @@
 package lotto.model;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class Lottos {
+public class Lottos implements Iterable<Lotto> {
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    @Override
+    public Iterator<Lotto> iterator() {
+        return lottos.iterator();
     }
 }

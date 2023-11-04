a1c075ee32e4b34779f85343ff1ca660095a9c62
howudong <tjdvy963@naver.com>
2023-11-05 00:29:40 +0900

    refactor : Lotto 클래스 생성자 public으로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -12,7 +12,7 @@ public final class Lotto {
 
     private final List<Integer> numbers;
 
-    Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }

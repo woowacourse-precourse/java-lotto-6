68753b373c3b884889f8b403ca2de9468fd8a7f7
siho-song <thdtlgh234@naver.com>
2023-11-04 17:24:49 +0900

    [Feat] 당첨번호,보너스번호를 포함한 클래스 구현
    1. 당첨번호와 보너스번호의 중복값 검증함수 구현
    2. 생성자에서 validate 진행

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+
+    private final LottoNumbers lottoNumbers;
+    private final BonusNumber bonusNumber;
+
+    WinningNumbers(LottoNumbers lottoNumbers, BonusNumber bonusNumber) {
+        validateDuplicated(lottoNumbers, bonusNumber);
+        this.lottoNumbers = lottoNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicated(LottoNumbers lottoNumbers, BonusNumber bonusNumber) {
+        List<Integer> numbers = lottoNumbers.getLottoNumbers();
+        int number = bonusNumber.getBonusNumber();
+
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_BONUS_NUMBER.getValue());
+        }
+    }
+}

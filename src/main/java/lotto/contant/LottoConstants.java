b8478785c5d4ff1cc0706c879838b90c200c2007
minseo  jo <liging12@naver.com>
2023-11-04 13:55:07 +0900

    feat: 로또 상수 기능 추가
    1등과 꼴등을 나타내는 상수를 추가 했다.
    꼴등은 아무것도 당첨되지 않음을 나타낸다. 그럼 1~6등인데
    인데스를 0부터 6으로 한 이유는 범위와 관련된 함수를 사용할 때 [start, end) 이기 떄문이다. [0, 6) 은 0에서 5사이의 인덱스를 나타낸다.

--- src/main/java/lotto/contant/LottoConstants.java
+++ src/main/java/lotto/contant/LottoConstants.java
@@ -5,7 +5,10 @@ public enum LottoConstants {
     LOTTO_MIN_NUMBER(1),   // 로또 번호 최소값
     LOTTO_MAX_NUMBER(45),  // 로또 번호 최대값
     LOTTO_PRICE(1000), // 로또 가격
-    MAX_LOTTO_PRICE_PER_USER(1_000_000); // 개인이 살 수 있는 로또 최대 금액
+    MAX_LOTTO_PRICE_PER_USER(1_000_000), // 개인이 살 수 있는 로또 최대 금액
+    FIRST_RANKING(0),        // 1등
+    LAST_RANKING(6);        // 꼴등 (6등, 꽝)
+
     private final int value;
 
     LottoConstants(int value) {

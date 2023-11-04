408ce03ec27f1e09e713702f2a3d3745b8cb1289
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 20:11:06 +0900

    refactor: enum 클래스 변수 접근제한자 변경

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -9,7 +9,7 @@ public enum ErrorMessage {
     LOTTO_CONTAIN_DULPICATE_NUMBER("[ERROR] 로또 번호는 중복되지 않아야 합니다. 중복되지 않은 숫자를 입력해 주세요."),
     OUT_OF_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
-    String errorMessage;
+    private final String errorMessage;
 
     ErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;

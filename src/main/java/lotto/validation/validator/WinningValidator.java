d481fd24a07e5d422d58d6c3e342ff934d7b79a0
seonghoo1217 <seonghoo1217@naver.com>
2023-11-04 23:44:42 +0900

    fix: 목적에 맞지 않는 네이밍 변경 `WinningValidator`

--- src/main/java/lotto/validation/validator/WinningValidator.java
+++ src/main/java/lotto/validation/validator/WinningValidator.java
@@ -1,20 +1,17 @@
 package lotto.validation.validator;
 
-import lotto.property.ErrorProperty;
-import lotto.property.LottoProperty;
-
 import static lotto.property.ErrorProperty.*;
 import static lotto.property.LottoProperty.*;
 
 public class WinningValidator extends RootValidator{
 
-    public static void winnerOrBonusIsCorrectRange(String winningNumber){
+    public static void winningOrBonusIsCorrectRange(String winningNumber){
         int winningInteger = Integer.parseInt(winningNumber);
         if (winningInteger < LOTTO_START || winningInteger > LOTTO_END){
             throw new IllegalArgumentException(WINNING_IS_NOT_CORRECTLY_RANGE.toString());
         }
     }
-    public static void winnersCountIsOverOrUnder(String winningNumbers){
+    public static void winningCountIsOverOrUnder(String winningNumbers){
         if(winningNumbers.split(DELIMITER).length!=WINNING_SIZE){
             throw new IllegalArgumentException(WINNING_COUNT_IS_OVER_OR_UNDER.toString());
         }
--- src/test/java/lotto/validation/WinningValidatorTest.java
+++ src/test/java/lotto/validation/WinningValidatorTest.java
@@ -2,7 +2,6 @@ package lotto.validation;
 
 import lotto.property.ErrorProperty;
 import lotto.validation.validator.WinningValidator;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,7 +13,7 @@ public class WinningValidatorTest {
     @ValueSource(strings = {"0","-1","46"})
     void 당첨_번호_입력_값_범위_벗어난_경우_테스트(String winning){
         assertThatThrownBy(()->{
-                WinningValidator.winnersCountIsOverOrUnder(winning);
+                WinningValidator.winningCountIsOverOrUnder(winning);
             }
                 ).isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ErrorProperty.WINNING_IS_NOT_CORRECTLY_RANGE.toString());
@@ -24,7 +23,7 @@ public class WinningValidatorTest {
     @ValueSource(strings = {"1,2,3,4,5,6,7","1,2,3,4,5"})
     void 당첨_번호_입력_값_개수_초과_또는_미만_검증_로직_테스트(String winningNumbers){
         assertThatThrownBy(()->{
-                WinningValidator.winnersCountIsOverOrUnder(winningNumbers);
+                WinningValidator.winningCountIsOverOrUnder(winningNumbers);
             }
                 ).isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ErrorProperty.WINNING_COUNT_IS_OVER_OR_UNDER.toString());

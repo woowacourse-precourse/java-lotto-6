166aedeba16540b71750704c433e551b1244b24f
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 20:17:26 +0900

    feat: 구매금액 유효성 검사 후 문자열 반환하도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,20 +2,27 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 import util.InputMessage;
+import view.validator.PriceFormatValidator;
 
 public class InputView {
+
+    private static final PriceFormatValidator priceFormatValidator = new PriceFormatValidator();
     public static String enterPrice() {
         System.out.println(InputMessage.ENTER_PRICE);
-        return Console.readLine();
+        String input = Console.readLine();
+        priceFormatValidator.checkPriceFormat(input);
+        return input;
     }
 
     public static String enterWinningNumbers() {
         System.out.println(InputMessage.ENTER_WINNING_NUMBERS);
-        return Console.readLine();
+        String input = Console.readLine();
+        return input;
     }
 
     public static String enterBonusNumbers() {
         System.out.println(InputMessage.ENTER_BONUS_NUMBERS);
-        return Console.readLine();
+        String input = Console.readLine();
+        return input;
     }
 }

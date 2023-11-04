66c2a55f766e4f01ca3aa650857aa21b1ff5a391
jyeun722 <skqhs7276@gamil.com>
2023-11-05 08:21:52 +0900

    feat: Receive winning numbers from user with type exception

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
+    private static void setLottoWinningNumbers() {
+        try {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            inputLottoWinningNumbers();
+        } catch (IllegalArgumentException e) {
+            setLottoWinningNumbers();
+        }
+    }
+
+    private static void inputLottoWinningNumbers() {
+        String userInputWinningNumbers = Console.readLine();
+        checkSeparator(userInputWinningNumbers);
+    }
+
+    private static void checkSeparator(String numbers) {
+        String regularExpressionNumbers = numbers.replaceAll("[0-9,]", "");
+        System.out.println(regularExpressionNumbers);
+        if (!regularExpressionNumbers.equals("")) {
+            System.out.println("당첨 번호는 숫자만 입력해야 하며 구분자는 쉼표(,)로 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        setLottoWinningNumbers();
     }
 }

21d445c957395b499801b4893879301ea8a3b075
gyu_hyeok <jjookki33@gmail.com>
2023-11-05 03:52:15 +0900

    feat/사용자 입력 기능 구현 - 구입금액 입력 기능 구현 - 당첨 번호 및 보너스 번호 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String setWinNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String setBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String setPurchasePrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+}

e8f23e2788c6795ded71c91feab1400495bb56d1
MyunghyunNero <mh1kim0910@gmail.com>
2023-11-04 21:49:35 +0900

    feat : User 금액 예외처리 생성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -6,21 +6,38 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class User {
+    private static final int ZERO = 0;
+    private static final int LOTTOVALUE = 1000;
 
     private List<Lotto> lottos;
 
     public Integer inputMoney(){
-        String money = Console.readLine();
-        validate(money);
-        return Integer.parseInt(money);
+        String moneyStr = Console.readLine();
+        validateFormat(moneyStr);
+        Integer money = Integer.parseInt(moneyStr);
+        validateValue(money);
+        return money;
     }
 
-    private void validate(String money){
-        validateFormat(money);
+    private void validateValue(Integer money){
+        validateDivide(money);
+        validateZero(money);
     }
 
     private void validateFormat(String money){
         if (!Pattern.compile("\\d+").matcher(money).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivide(Integer money){
+        if (money % LOTTOVALUE != ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateZero(Integer money) {
+        if (money == ZERO) {
             throw new IllegalArgumentException();
         }
     }

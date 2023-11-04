44829c03906c83e3d7596dc90f331fdbb8a95b83
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-04 13:49:37 +0900

    feat: 컨트롤러 기본 생성로직 추가
    money, lottos 생성 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,12 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.util.NumberGenerator;
+import lotto.util.RandomNumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -11,5 +18,30 @@ public class LottoController {
     public LottoController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void start() {
+        Money money = getMoneyFromUser();
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        Lottos lottos = makeLottos(money.buyLotto(), numberGenerator);
+
+    }
+
+    private Money getMoneyFromUser() {
+        outputView.printPurchaseMessage();
+        String input = inputView.readInput();
+        return Money.from(input);
+    }
+
+    private Lottos makeLottos(final int buyCount, final NumberGenerator numberGenerator) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int count = 0; count < buyCount; count++) {
+            List<Integer> generatedNumbers = numberGenerator.generateSortedList();
+            Lotto lotto = Lotto.fromIntegerList(generatedNumbers);
+            lottos.add(lotto);
+        }
+
+        return new Lottos(lottos);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
 public class OutputView {
+
+    private static final String PURCHASE_PROMPT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void printPurchaseMessage() {
+        System.out.println(PURCHASE_PROMPT_MESSAGE);
+    }
+
+    public void printLottos(final int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
+
 }

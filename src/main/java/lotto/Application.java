e436ad713b02a5f2b3c8008dea4d10a3a316612f
박찬건 <jg9870@naver.com>
2023-11-04 22:27:24 +0900

    feat(lotto): 1개의 로또를 발행할 때 중복되지 않는 6개의 숫자를 뽑는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,19 +2,23 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         boolean validPrice = false;
+        int lottoTicket = 0;
         LottoHandler lottoHandler = new LottoHandler();
 
         while (!validPrice) {
             try {
                 OutputHandler.printMessage("구입금액을 입력해 주세요.");
-                lottoHandler.calculateLottoTicketCount(Console.readLine());
+                lottoTicket = lottoHandler.calculateLottoTicketCount(Console.readLine());
                 validPrice = true;
             } catch (IllegalArgumentException e) {
                 OutputHandler.printMessage(e.getMessage());
             }
         }
+        List<List<Integer>> lottoNumbers = lottoHandler.issueLottoNumbers(lottoTicket);
     }
 }
--- src/main/java/lotto/LottoHandler.java
+++ src/main/java/lotto/LottoHandler.java
@@ -1,5 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class LottoHandler {
     private static final int LOTTO_PRICE = 1000;
 
@@ -15,5 +22,13 @@ public class LottoHandler {
             throw new IllegalArgumentException("[ERROR] " + LOTTO_PRICE + "원 단위로만 입력해 주세요.");
         }
         return purchasePrice / LOTTO_PRICE;
+    }
+
+    public List<List<Integer>> issueLottoNumbers(int lottoTicket) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < lottoTicket; i++) {
+            lottoNumbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+        return lottoNumbers;
     }
 }

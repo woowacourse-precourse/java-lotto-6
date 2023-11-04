265bfe03171e61d05502c0c906a22e7b9b792ad2
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-05 03:24:53 +0900

    refactor: 수 다루는 로직 NumberService로 옮김

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,47 +3,23 @@ package lotto.service;
 import lotto.domain.Lotto;
 import lotto.domain.MatchResult;
 
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 import static lotto.service.InputService.*;
-import static lotto.domain.constant.NumberConstant.*;
+import static lotto.service.NumberService.*;
 
 public class LottoService {
     public void run() {
-        Long expense = readExpense();
-        int ticketNum = convertToTicketNum(expense);
-        List<Lotto> randomLottos = Lotto.createRandomLottos(ticketNum);
+        int lottoTicketNum = computeLottoTicketNum();
 
+        List<Lotto> randomLottos = Lotto.createRandomLottos(lottoTicketNum);
         Lotto winLotto = new Lotto(readWinNumbers());
-        int bonusNumber = readBonusNumber();
+        int bonusNum = readBonusNumber();
 
-        Map<MatchResult, Integer> winResult = computeWin(randomLottos, winLotto, bonusNumber);
-    }
+        Map<MatchResult, Integer> result = computeResult(randomLottos, winLotto, bonusNum);
+        long totalPrize = computeTotalPrize(result);
 
-    private int convertToTicketNum(Long expense) {
-        return (int) (expense / LOTTO_PRICE);
-    }
-
-    private Map<MatchResult, Integer> computeWin(List<Lotto> randomLottos, Lotto winLotto, int bonusNumber) {
-        Map<MatchResult, Integer> totalResult = initEnumMap();
-
-        for (Lotto randomLotto : randomLottos) {
-            MatchResult matchResult = Lotto.match(randomLotto, winLotto);
-
-            totalResult.compute(matchResult, (k, v) -> v + MATCHRESULT_INCREMENT_NUM);
-        }
-
-        return totalResult;
-    }
-
-    private Map<MatchResult, Integer> initEnumMap() {
-        EnumMap<MatchResult, Integer> enumMap = new EnumMap<MatchResult, Integer>(MatchResult.class);
-
-        for (MatchResult result : MatchResult.values())
-            enumMap.put(result, 0);
-
-        return initEnumMap();
+        double margin = computeMargin(lottoTicketNum, totalPrize);
     }
 }

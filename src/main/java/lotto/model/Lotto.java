703894e4dead18686ec69ef2b9b7fe8cf5ad6e9a
wodnj5 <jw991211@gmail.com>
2023-11-04 19:28:57 +0900

    feat: BonusNumber가 Lotto에 포함되어있는지 확인하는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -14,6 +14,10 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public boolean containsNumber(BonusNumber bonusNumber) {
+        return numbers.contains(bonusNumber.getValue());
+    }
+
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import lotto.model.BonusNumber;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -30,6 +34,15 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호에 포함되어 있으면 true를 리턴한다.")
+    @Test
+    void validateBonusNumber() {
+
+        assertThat(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))
+                .containsNumber(new BonusNumber(6)))
+                .isEqualTo(true);
     }
     // 아래에 추가 테스트 작성 가능
 }

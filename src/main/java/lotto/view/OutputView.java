f68bb85913af56617d8517cc659da22d29e61974
junest66 <chlwndks333@gmail.com>
2023-11-04 21:55:27 +0900

    refactor(OutputView): 로또 구매 결과 출력하는 함수 변경
    - 결과를 String으로 받아서 단지 출력하는걸로 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,6 @@
 package lotto.view;
 
-import java.util.List;
-import lotto.DTO.LottoDTO;
 import lotto.constants.Messages;
-import lotto.constants.Values;
 
 public class OutputView {
 
@@ -19,12 +16,8 @@ public class OutputView {
         System.out.println(Messages.PROMPT_BONUS_NUMBER);
     }
 
-    public static void printPurchaseLottos(List<LottoDTO> purchasedLottosDTO) {
-        System.out.println(purchasedLottosDTO.size() + Messages.PURCHASE_MESSAGE);
-        purchasedLottosDTO.forEach(lottoDTO -> {
-            String numbers = String.join(Values.LOTTO_NUMBER_SEPARATOR, lottoDTO.numbers());
-            System.out.println(String.format(Messages.LOTTO_NUMBERS_MESSAGE, numbers));
-        });
+    public static void printPurchaseLottoResult(String result) {
+        System.out.println(result);
     }
 
     public static void printResult(String winningResult, String profitPercentage) {

9acb1431c9b1b8f8b36e803d564513ac980c8d0e
DongHyun <kimdh0315@naver.com>
2023-11-04 20:10:37 +0900

    feat(main/model/User) : make User class which stores money for buying lottos and lottos which User bought

---
+++ src/main/java/lotto/model/User.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import java.util.HashMap;
+
+public class User {
+    private int money;
+    private HashMap<Lotto, Rank> lottos;
+
+    public User(int money, HashMap<Lotto, Rank> lottos) {
+        this.money = money;
+        this.lottos = lottos;
+    }
+
+    public void setLottoResult(Lotto lotto, Rank rank){
+        this.lottos.put(lotto, rank);
+    }
+
+    public HashMap<Lotto, Rank> getLottos() {
+        return lottos;
+    }
+}

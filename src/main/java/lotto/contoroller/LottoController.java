47c95792fadb7467f6fb9a73326ab3066185fe4c
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-05 00:11:09 +0900

    [refactoring] controller MVC 패턴 준수를 위한 모델과 뷰 분리

--- src/main/java/lotto/contoroller/LottoController.java
+++ src/main/java/lotto/contoroller/LottoController.java
@@ -1,12 +1,25 @@
 package lotto.contoroller;
 
 import lotto.domain.Purchase;
+import lotto.utility.ChangeNumberType;
 import lotto.view.InputValue;
 
 public class LottoController {
 
-    public void StartLotto() {
-        int input = InputValue.inputPurchase();
-        new Purchase(input);
+    public void startLotto() {
+        int purchaseAmount = getInputPurchase();
+    }
+
+    private int getInputPurchase() {
+        while (true) {
+            try {
+                String input = InputValue.inputPurchase();
+                int purchaseAmount = ChangeNumberType.changeNumberType(input);
+                Purchase purchase = new Purchase(purchaseAmount);
+                return purchase.getAmount();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/lotto/view/InputValue.java
+++ src/main/java/lotto/view/InputValue.java
@@ -1,21 +1,11 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.Purchase;
-import lotto.utility.ChangeNumberType;
 
 public class InputValue {
-    public static int inputPurchase() {
-        while (true) {
-            try {
-                System.out.println(InputMessage.PURCHASE.getMessage());
-                int purchaseAmount = ChangeNumberType.changeNumberType(Console.readLine());
-                Purchase purchase = new Purchase(purchaseAmount);
-                return purchase.getAmount();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public static String inputPurchase() {
+        System.out.println(InputMessage.PURCHASE.getMessage());
+        return Console.readLine();
     }
 
     public static String inputWinningNumbers() {

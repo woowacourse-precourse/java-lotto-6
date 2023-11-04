addf0097bfd8d6ef9b7a098d81d086f7e8082f75
leedonghyun <skydh507@ajou.ac.kr>
2023-11-05 04:11:59 +0900

    refactor : 보너스 번호인지 검사하는 메서드 추출

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import static lotto.input.constant.Constant.BONUS_NUMBER_INDEX;
+import static lotto.input.constant.Constant.FIFTH_STRIKE_COUNT;
+import static lotto.input.constant.Constant.FIRST_STRIKE_COUNT;
+import static lotto.input.constant.Constant.FOURTH_STRIKE_COUNT;
+import static lotto.input.constant.Constant.SECOND_THIRD_STRIKE_COUNT;
 
 import java.util.List;
 import lotto.input.Result;
 
 public class LottoService {
-    
+
     private final List<Integer> winningNumbers;
 
     private final List<Lotto> lottos;
@@ -36,20 +40,24 @@ public class LottoService {
     }
 
     private void extractedResult(int count, List<Integer> userNumbers) {
-        if (count == 6) {
+        if (count == FIRST_STRIKE_COUNT) {
             Result.FIRST.addValue();
         }
-        if (count == 5 && userNumbers.contains(winningNumbers.get(BONUS_NUMBER_INDEX))) {
+        if (count == SECOND_THIRD_STRIKE_COUNT && isBounsNumberStrike(userNumbers)) {
             Result.SECOND.addValue();
         }
-        if (count == 5 && !userNumbers.contains(winningNumbers.get(BONUS_NUMBER_INDEX))) {
+        if (count == SECOND_THIRD_STRIKE_COUNT && !isBounsNumberStrike(userNumbers)) {
             Result.THIRD.addValue();
         }
-        if (count == 4) {
+        if (count == FOURTH_STRIKE_COUNT) {
             Result.FOURTH.addValue();
         }
-        if (count == 3) {
+        if (count == FIFTH_STRIKE_COUNT) {
             Result.FIFTH.addValue();
         }
+    }
+
+    private boolean isBounsNumberStrike(List<Integer> userNumbers) {
+        return userNumbers.contains(winningNumbers.get(BONUS_NUMBER_INDEX));
     }
 }

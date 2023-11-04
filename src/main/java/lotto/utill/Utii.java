52038cce9575550cde19adef32d31a87c4e3e8b1
박승찬 <seungchan141414@gmail.com>
2023-11-04 22:53:45 +0900

    [Refactor(lotto)] 변수명 변경

--- src/main/java/lotto/utill/Utii.java
+++ src/main/java/lotto/utill/Utii.java
@@ -1,7 +1,7 @@
 package lotto.utill;
 
 public class Utii {
-    public static boolean isSameInt(Integer num, Integer winLottoNum) {
-        return winLottoNum == num;
+    public static boolean isSameInt(Integer num_1, Integer num_2) {
+        return num_2 == num_1;
     }
 }

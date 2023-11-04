2f8982c596d45a6a4c14b57ab9d21144dc3cf8c0
liarreez <liarreez@gmail.com>
2023-11-05 00:01:51 +0900

    test : 로또가 주어졌을 때 당첨 번호 갯수에 맞춰 정해진 등수의 결과를 나타내는 테스트 작성

--- src/test/java/lotto/model/LottoResultCheckerTest.java
+++ src/test/java/lotto/model/LottoResultCheckerTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,19 +11,39 @@ public class LottoResultCheckerTest {
     @DisplayName("당첨 번호와 중복된 보너스 번호 입력 시 예외가 발생한다.")
     @Test
     void testWhenBonusNumberDuplicateWinningNumbers() {
-        LottoResultChecker lottoResultChecker = new LottoResultChecker("1,2,3,4,5,6");
+        LottoResultChecker lottoResultChecker = new LottoResultChecker();
+        lottoResultChecker.createWinningNumbers("1,2,3,4,5,6");
 
-        IllegalArgumentException bonusNumberError =
+        IllegalArgumentException bonusNumberException =
                 Assertions.assertThrows(IllegalArgumentException.class,
-                        () -> lottoResultChecker.setBonusNumber("6"));
-        assertThat(bonusNumberError.getMessage()).contains("[ERROR]");
+                        () -> lottoResultChecker.createBonusNumber("6"));
+        assertThat(bonusNumberException.getMessage()).contains("[ERROR]");
     }
 
     @DisplayName("적절한 보너스 번호 입력 시 예외가 발생하지 않는다.")
     @Test
     void testWhenBonusNumberIsValid() {
-        LottoResultChecker lottoResultChecker = new LottoResultChecker("1,2,3,4,5,6");
+        LottoResultChecker lottoResultChecker = new LottoResultChecker();
+        lottoResultChecker.createWinningNumbers("1,2,3,4,5,6");
 
-        Assertions.assertDoesNotThrow(() -> lottoResultChecker.setBonusNumber("20"));
+        Assertions.assertDoesNotThrow(() -> lottoResultChecker.createBonusNumber("20"));
+    }
+
+    @DisplayName("로또가 주어지면 일치한 당첨 번호 갯수에 맞춰서 정해진 등수의 결과를 나타낸다.")
+    @Test
+    void testShowLottoResults() {
+        Client client = new Client();
+        client.addLotto(new Lotto(List.of(1,2,3,4,5,6)));
+        client.addLotto(new Lotto(List.of(1,2,3,10,15,20)));
+        client.addLotto(new Lotto(List.of(1,2,3,4,10,35)));
+        client.addLotto(new Lotto(List.of(1,2,3,4,5,16)));
+        client.addLotto(new Lotto(List.of(11,12,13,14,15,16)));
+        LottoResultChecker lottoResultChecker = new LottoResultChecker();
+        lottoResultChecker.createWinningNumbers("1,2,3,4,5,6");
+        lottoResultChecker.createBonusNumber("16");
+
+        List<Integer> result = lottoResultChecker.showLottoResults(client.getLottos());
+
+        assertThat(result).isEqualTo(List.of(1,1,1,0,1,1));
     }
 }

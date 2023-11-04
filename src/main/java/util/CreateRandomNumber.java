b554436f2964f605b67f90f5d8ae5cf0fbcbc5ee
dltmddus <tmddus4671@gmail.com>
2023-11-04 14:16:42 +0900

    refactor: Util관련 파일 이름 수정

--- src/main/java/util/CreateRandomNumber.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import java.util.List;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
-public class CreateRandomNumber {
-    public List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-}
---
+++ src/main/java/util/MessageUtil.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
+
+import static constant.InputMessage.*;
+import static constant.OutputMassage.*;
+
+public class MessageUtil {
+    public void printPurchaseAmount(){
+        System.out.println(PURCHASE_AMOUNT.getMessage());
+    }
+    public void printWinningNum(){
+        System.out.println(WINNING_NUM.getMessage());
+    }
+    public void printBonusNum(){
+        System.out.println(BONUS_NUM.getMessage());
+    }
+
+    public void printPurchase(int purchaseAmount){
+        System.out.printf(PURCHASE.getMessage(), purchaseAmount);
+    }
+    public void printWinningStatistic(int purchaseAmount){
+        System.out.printf(WINNING_STATISTIC.getMessage(), purchaseAmount);
+    }
+    public void printWinningResult(int matchNum, int price, int num){
+        DecimalFormat decimalFormat = new DecimalFormat(",%d");
+        String priceComma = decimalFormat.format(price);
+        System.out.printf(WINNING_RESULT.getMessage(), matchNum, priceComma, num);
+    }
+    public void printWinningRate(Double rate){
+        System.out.printf(WINNING_RATE.getMessage(), rate);
+    }
+
+    public static void printPurchaseInfo(List<Integer> lottoNum){
+//        System.out.print("[");
+        Collections.sort(lottoNum);
+        System.out.println(lottoNum.toString());
+//        for(List)
+    }
+}

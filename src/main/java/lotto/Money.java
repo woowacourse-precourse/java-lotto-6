a78210dacad7b51d7d50fc25b62816cd8b558817
hunko <rhwlgns4386@naver.com>
2023-11-05 06:05:06 +0900

    feat(Money): 0원 검증로직 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -4,14 +4,26 @@ public class Money {
 
     private final Integer money;
     private final static Integer MIN_UNIT_MONEY = 1000;
+    private final static Integer ZERO = 0;
 
     private Money(Integer money) {
         validateRemainMoney(money);
+        validateZeroMoney(money);
         this.money = money;
     }
 
     public static Money of(Integer money) {
         return new Money(money);
+    }
+
+    private void validateZeroMoney(Integer money) {
+        if (isZero(money)) {
+            throw new IllegalArgumentException("천원 단위로 입력해주세요.");
+        }
+    }
+
+    private boolean isZero(Integer money) {
+        return money.equals(ZERO);
     }
 
     private void validateRemainMoney(Integer money) {

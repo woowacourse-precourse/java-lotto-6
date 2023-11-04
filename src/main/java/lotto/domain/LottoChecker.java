8494727a8a6bde459f7772fbe14fe80dc8469162
junghunim07 <dlawndgns9@naver.com>
2023-11-04 21:24:34 +0900

    feat(LottoChecker): 보너스 번호 비교하는 기능 추가

--- src/main/java/lotto/domain/LottoChecker.java
+++ src/main/java/lotto/domain/LottoChecker.java
@@ -16,14 +16,21 @@ public class LottoChecker {
         return count;
     }
 
-    public List<Integer> compareLottoTickets(List<Lotto> purchaseLottoTickets, Lotto winningLotto) {
+    public List<Integer> compareLottoTickets(List<Lotto> purchaseLottoTickets, Lotto winningLotto, int bonusNumber) {
         List<Integer> counts = new ArrayList<>();
 
         for (int i = 0; i < purchaseLottoTickets.size(); i++) {
             int count = compare(purchaseLottoTickets.get(i), winningLotto);
+            if (count == 5 && includeBonusNumberInLottoNumber(purchaseLottoTickets.get(i), bonusNumber)) {
+                count++;
+            }
             counts.add(count);
         }
 
         return counts;
+    }
+
+    public boolean includeBonusNumberInLottoNumber(Lotto lotto, int bonusNumber) {
+        return lotto.getNumbers().contains(bonusNumber);
     }
 }

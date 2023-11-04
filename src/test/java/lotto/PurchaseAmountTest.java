6b54f42a4cecf8e7863012a87ddabe0c07fdde0c
burby1007 <kkh3272@naver.com>
2023-11-04 22:30:15 +0900

    Test : 수정된 PurchaseAmount.class 맞춰서 수정

--- src/test/java/lotto/PurchaseAmountTest.java
+++ src/test/java/lotto/PurchaseAmountTest.java
@@ -1,45 +1,44 @@
 package lotto;
 
+import lotto.model.Lotto;
 import lotto.model.PurchaseAmount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseAmountTest {
 
-    @DisplayName("구입 금액이 빈 경우 예외가 발생한다.")
+    @DisplayName("구입 금액이 비거나 문자인 경우 에러가 발생한다.")
     @Test
-    void PurchaseAmountIsEmpty() {
-        assertThatThrownBy(() -> PurchaseAmount.empty(""))
+    void createPurchaseAmountByIncludingNotNumber(){
+        assertThatThrownBy(() -> new PurchaseAmount(""))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @DisplayName("구입 금액이 숫자가 아닌 경우 예외가 발생한다.")
-    @Test
-    void PurchaseAmountIsNotNumber(){
-        assertThatThrownBy(() -> PurchaseAmount.notNumber("80c"))
+        assertThatThrownBy(() -> new PurchaseAmount("8000c"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구입 금액이 음수인 경우 예외가 발생한다.")
     @Test
-    void PurchaseAmountIsNegative(){
-        assertThatThrownBy(() -> PurchaseAmount.negativeNumber(-80))
+    void createNegativePurchaseAmount(){
+        assertThatThrownBy(() -> new PurchaseAmount("-80"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구입 금액이 로또 1장 가격(1000원)보다 작을 경우 예외가 발생한다.")
     @Test
-    void PurchaseAmountIsUnder1000(){
-        assertThatThrownBy(() -> PurchaseAmount.under1000(500))
+    void createPurchaseAmountUnder1000(){
+        assertThatThrownBy(() -> new PurchaseAmount("500"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구입 금액이 1,000단위가 아닌 경우 예외가 발생한다.")
     @Test
-    void PurchaseAmountIsNot1000Unit(){
-        assertThatThrownBy(() -> PurchaseAmount.not1000Unit(4200))
+    void createPurchaseAmountBy1000Unit(){
+        assertThatThrownBy(() -> new PurchaseAmount("4200"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

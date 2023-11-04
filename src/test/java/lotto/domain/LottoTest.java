bdaa996e59b6478f5aeb8be46df5e5c3fb11d910
ddrongy <ddrongy@gmail.com>
2023-11-04 16:15:40 +0900

    add: 로또 번호 생성 시 성공 test case 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,9 +4,11 @@ import lotto.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -39,6 +41,14 @@ class LottoTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 47)))
                 .withMessageContaining(ErrorMessage.NUMBER_OUT_OF_RANGE_HIGH.getMessage());
+    }
+
+    @Test
+    @DisplayName("로또 번호가 중복없이 숫자 범위 내에 6개로 이루어진 경우 에러를 발생시키지 않는다.")
+    void validBonusNumber() {
+        assertDoesNotThrow(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 45));
+        });
     }
 
 }

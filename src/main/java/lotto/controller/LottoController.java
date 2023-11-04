3a1610a07e34dd714d7c97277dfe518f2c5a62cd
devholic22 <hyunjoon.tech@gmail.com>
2023-11-04 12:07:00 +0900

    refactor: InvestorService 정의
    - Investor의 행위를 관리할 서비스 정의
    - 컨트롤러에서 Investor, Money, Shop의 의존성 삭제

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,12 @@
 package lotto.controller;
 
-import lotto.model.Investor;
 import lotto.model.Lotto;
-import lotto.model.Money;
 import lotto.model.Prize;
-import lotto.model.Shop;
 import lotto.model.dto.LottoResponse;
 import lotto.model.judge.BonusNumberJudge;
 import lotto.model.judge.GoalNumberJudge;
 import lotto.model.judge.LottoJudge;
+import lotto.service.InvestorService;
 import lotto.view.input.InputView;
 import lotto.view.output.OutputView;
 import java.util.List;
@@ -24,13 +22,10 @@ public class LottoController {
     }
 
     public void run() {
-        outputView.askInvestMoney();
-        String moneyInput = inputView.readLine();
-        Money investMoney = Money.from(moneyInput);
-        Investor investor = Investor.createDefault(investMoney);
-        Shop lottoShop = Shop.createDefault();
-        List<Lotto> lottos = investor.buyLottosFromShop(lottoShop);
+        InvestorService investorService = initInvestorService();
+        List<Lotto> lottos = investorService.buyLottos();
         outputView.printBoughtLottoSize(lottos.size());
+        
         List<LottoResponse> lottoResponses = convertLottoResponses(lottos);
         outputView.printEachLottoNumbers(lottoResponses);
         outputView.askGoalNumbers();
@@ -43,22 +38,29 @@ public class LottoController {
         for (int i = 3; i < 6; i++) {
             List<Lotto> matchLotto = goalJudge.collectLottoWithMatchSize(lottos, i);
             Prize prize = Prize.findByPoint(i * 100);
-            investor.addProfitMoney(prize.getMoney() * matchLotto.size());
+            investorService.addProfit(prize.getMoney() * matchLotto.size());
             outputView.printEachPrize(prize.getCondition(), prize.getMoney(), matchLotto.size());
         }
 
         List<Lotto> secondMatchLotto = bonusJudge.collectLottoWithMatchSize(lottos, 5);
         List<Lotto> secondResultLotto = goalJudge.collectLottoWithMatchSize(secondMatchLotto, 5);
         Prize secondPrize = Prize.findByPoint(550);
-        investor.addProfitMoney(secondPrize.getMoney() * secondResultLotto.size());
+        investorService.addProfit(secondPrize.getMoney() * secondResultLotto.size());
         outputView.printEachPrize(secondPrize.getCondition(), secondPrize.getMoney(), secondResultLotto.size());
 
         List<Lotto> firstLotto = goalJudge.collectLottoWithMatchSize(lottos, 6);
         Prize prize = Prize.findByPoint(600);
-        investor.addProfitMoney(prize.getMoney() * firstLotto.size());
+        investorService.addProfit(prize.getMoney() * firstLotto.size());
         outputView.printEachPrize(prize.getCondition(), prize.getMoney(), firstLotto.size());
 
-        outputView.printProfitRate(investor.calculateProfitRate());
+        outputView.printProfitRate(investorService.calculateProfitRate());
+    }
+
+    private InvestorService initInvestorService() {
+        outputView.askInvestMoney();
+        String investorInput = inputView.readLine();
+
+        return InvestorService.createDefault(investorInput);
     }
 
     private List<LottoResponse> convertLottoResponses(final List<Lotto> lottos) {
---
+++ src/main/java/lotto/service/InvestorService.java
@@ -0,0 +1,38 @@
+package lotto.service;
+
+import lotto.model.Investor;
+import lotto.model.Lotto;
+import lotto.model.Money;
+import lotto.model.Shop;
+import java.util.List;
+
+public class InvestorService {
+
+    private final Investor investor;
+    private final Shop lottoShop;
+
+    private InvestorService(final Investor investor, final Shop lottoShop) {
+        this.investor = investor;
+        this.lottoShop = lottoShop;
+    }
+
+    public static InvestorService createDefault(final String investorInput) {
+        Money investMoney = Money.from(investorInput);
+        Investor investor = Investor.createDefault(investMoney);
+        Shop lottoShop = Shop.createDefault();
+
+        return new InvestorService(investor, lottoShop);
+    }
+
+    public List<Lotto> buyLottos() {
+        return investor.buyLottosFromShop(lottoShop);
+    }
+
+    public void addProfit(final int money) {
+        investor.addProfitMoney(money);
+    }
+
+    public double calculateProfitRate() {
+        return investor.calculateProfitRate();
+    }
+}

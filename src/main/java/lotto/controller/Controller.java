988d4f16fc9365a288ae15e486d544b6a422dbdb
Sanghwi Lee <sanghwi47@naver.com>
2023-11-04 16:14:58 +0900

    feat(Controller, OutputView) 금액에 맞는 로또개수를 출력하는 기능 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,18 +1,22 @@
 package lotto.controller;
 
 import lotto.model.LottoTicket;
+import lotto.service.LottoService;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Controller {
     InputView inputView;
+    LottoService lottoService;
     int ticketCount;
     private static final String ERROR = "[ERROR] ";
 
     public void run(){
-        lottoSetting();
+        lottoTicketSetting();
+        showTicketCount();
     }
 
-    private void lottoSetting() {
+    private void lottoTicketSetting() {
         while (true) {
             String moneyInput = InputView.inputMoney();
             try {
@@ -24,4 +28,9 @@ public class Controller {
             }
         }
     }
+
+    private void showTicketCount() {
+        OutputView.displayTicket_Count(ticketCount);
+    }
+
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,12 @@
 package lotto.view;
 
 public class OutputView {
+
+//    private static final String TICKET_COUNT_MESSAGE ="개를 구매했습니다.";
+    private static final String TICKET_COUNT_MESSAGE ="you buy";
+    public static void displayTicket_Count(int ticketCount) {
+        System.out.println();
+        System.out.println(ticketCount + TICKET_COUNT_MESSAGE);
+    }
+
 }

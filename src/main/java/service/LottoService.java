c2d65be29d248a7e71318245868321227d2497ae
WonyuChoi <phillip07290@naver.com>
2023-11-04 13:23:29 +0900

    feat: 각 매니저 객체에게 명령을 내리는 서비스 객체

---
+++ src/main/java/service/LottoService.java
@@ -0,0 +1,44 @@
+package service;
+
+import java.util.List;
+import model.FinanceManager;
+import model.GameManager;
+import model.LottoTotalResult;
+import model.dto.LottoResponse;
+
+public class LottoService {
+
+    private final FinanceManager financeManager;
+    private final GameManager gameManager;
+
+    private LottoService(final FinanceManager financeManager, final GameManager gameManager) {
+        this.financeManager = financeManager;
+        this.gameManager = gameManager;
+    }
+
+    public static LottoService from(final int purchase) {
+        return new LottoService(FinanceManager.from(purchase),
+            GameManager.from(purchase / 1000));
+    }
+
+    public int getLottoCount() {
+        return financeManager.calculateLottoCount();
+    }
+
+    public List<LottoResponse> getGeneratedLottos() {
+        return gameManager.generateLottos();
+    }
+
+    public void generateAnswerLotto(final List<Integer> answerLottoNumbers,
+        final int answerBonusNumber) {
+        gameManager.generateAnswerLotto(answerLottoNumbers, answerBonusNumber);
+    }
+
+    public LottoTotalResult calculateResult() {
+        return gameManager.calculateResult();
+    }
+
+    public double calculateRateOfReturn(final LottoTotalResult totalResult) {
+        return financeManager.calculateRateOfReturn(totalResult);
+    }
+}

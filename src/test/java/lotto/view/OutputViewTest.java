49f33b938474d4da9dd9a564ff1d4ed79ed2ac4a
Yang-soeun <soeun8636@naver.com>
2023-11-04 18:08:16 +0900

    [test]: 구매한 로또를 출력하는 메소드 테스트 추가

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,6 +29,26 @@ class OutputViewTest {
 
         //then
         Assertions.assertThat(output.toString()).contains("8개를 구매했습니다.");
+    }
+
+    @DisplayName("구매한 로또를 출력한다.")
+    @Test
+    void printLotto() {
+        //given
+        OutputView outputView = new OutputView();
+
+        List<List<Integer>> lottos = List.of(
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38)
+        );
+
+        //when
+        outputView.printLotto(lottos);
+
+        //then
+        Assertions.assertThat(output.toString())
+                .contains("[8, 21, 23, 41, 42, 43]"
+                        , "[3, 5, 11, 16, 32, 38]");
     }
 
 }

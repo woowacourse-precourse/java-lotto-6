44d95fd71648c0f214144290ac7b9c9ccf8ce7a9
kimdohyung <pdohyung@gmail.com>
2023-11-04 18:08:40 +0900

    feat(InputValidator): 입력이 빈칸일 때 예외 처리 기능 추가

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -2,8 +2,11 @@ package lotto.view;
 
 public class InputValidator {
     private static final String INVALID_NUMBER_FORMAT_MESSAGE = "숫자 형식 입력이 아닙니다.";
+    private static final String EMPTY_INPUT_MESSAGE = "입력이 빈칸입니다.";
+    private static final String EMPTY = "";
 
     public static int validateBuyAmount(String inputBuyAmount) {
+        validateEmpty(inputBuyAmount);
         int buyAmount = convertStringToInteger(inputBuyAmount);
         return buyAmount;
     }
@@ -13,6 +16,12 @@ public class InputValidator {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    private static void validateEmpty(String input) {
+        if (input.equals(EMPTY)) {
+            throw new IllegalArgumentException(EMPTY_INPUT_MESSAGE);
         }
     }
 }

5ad1f9ff9dd40faa67f8bcd88243f4465ce882f8
lusate <rayrayr@naver.com>
2023-11-04 21:18:56 +0900

    feat: 랭킹에 따른 결과

--- src/main/java/lotto/model/Ranking.java
+++ src/main/java/lotto/model/Ranking.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import lotto.view.Output;
+
 public enum Ranking {
     FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "), // 1등
     SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "), // 2등
@@ -9,15 +11,46 @@ public enum Ranking {
     NOTHING(0, 0, "");
 
     private static final int MIN_MATCH = 3;
-    private int rank;
-    private int winningPrize;
+    private final int matchCount;
 
-    private String matchMessage;
+    private final int winningPrize;
 
-    Ranking(int rank, int winningPrize, String matchMessage) {
-        this.rank = rank;
+    private final String matchMessage;
+
+    Ranking(int matchCount, int winningPrize, String matchMessage) {
+        this.matchCount = matchCount;
         this.winningPrize = winningPrize;
         this.matchMessage = matchMessage;
     }
 
+    public static Ranking ranking(int matchCount, boolean matchBonus) {
+        if (matchCount < MIN_MATCH) {
+            return NOTHING;
+        }
+
+        if (SECOND.matchCount(matchCount) && matchBonus) {
+            return SECOND;
+        }
+
+        for (Ranking rank : values()) {
+            if (rank.matchCount(matchCount) && rank != SECOND) {
+                return rank;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR]");
+    }
+
+    private boolean matchCount(int count) { //count는 실제로 일치하는 개수
+        return this.matchCount == count;
+    }
+
+    public void printMessage(int count) {
+        if (this != NOTHING) {
+            Output.result_match(matchMessage, count);
+        }
+    }
+
+    public int getWinningPrize() {
+        return winningPrize;
+    }
 }

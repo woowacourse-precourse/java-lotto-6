3724b6ed21d4fceaf0ca98aafcf40394831ea0c9
Haeun Jeong <gkdms4248@gmail.com>
2023-11-04 20:01:56 +0900

    feat: 로또 번호 정렬 기능 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
@@ -13,9 +14,11 @@ public class LottoMachine {
 
     public List<Integer> issueLotto(Generator generator) {
         List<Integer> issuedLotto = generator.generate();
+        Collections.sort(issuedLotto);
         issuedLottos.add(new Lotto(issuedLotto));
         return issuedLotto;
     }
+
 
     public List<Lotto> getIssuedLottos() {
         return issuedLottos;

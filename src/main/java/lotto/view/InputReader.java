27ec11fdaf696d14e4e7051af1305fddb3d01657
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 03:13:32 +0900

    feat: InputReader 작성

---
+++ src/main/java/lotto/view/InputReader.java
@@ -0,0 +1,31 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.PurchaseAmount;
+import lotto.util.StringParser;
+import lotto.util.TypeConverter;
+
+public class InputReader {
+
+    public PurchaseAmount readPurchaseAmount() throws IllegalArgumentException {
+        Integer amount = TypeConverter.StringToInteger(Console.readLine());
+        return PurchaseAmount.from(amount);
+    }
+
+    public Lotto readLotto() throws IllegalArgumentException {
+        List<String> strings = StringParser.parseToList(Console.readLine());
+        List<LottoNumber> lottoNumbers = strings.stream()
+                .map(TypeConverter::StringToInteger)
+                .map(LottoNumber::from)
+                .toList();
+        return Lotto.from(lottoNumbers);
+    }
+
+    public LottoNumber readBonusNumber() throws IllegalArgumentException {
+        Integer bonusNumber = TypeConverter.StringToInteger(Console.readLine());
+        return LottoNumber.from(bonusNumber);
+    }
+}

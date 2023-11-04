2a53ddd025ee4bef7a5dbe4e41173603d8f603fa
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 15:10:29 +0900

    feat(Lotto): 당첨 번호 List 형태로 변환 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
@@ -18,5 +20,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private List<String> convertStringToList(String input) {
+        List<String> numbers = new ArrayList<>(Arrays.asList(input.split(",")));
+        return numbers;
     }
 }

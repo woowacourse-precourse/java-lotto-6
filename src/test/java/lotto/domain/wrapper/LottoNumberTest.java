df811e2b0c9d05d70c14e52013877d6a94d49c3b
devhoya97 <diajbh123@gmail.com>
2023-11-04 15:25:12 +0900

    test(domain): LottoNumber 테스트코드 추가

--- src/test/java/lotto/domain/wrapper/LottoNumberTest.java
+++ src/test/java/lotto/domain/wrapper/LottoNumberTest.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain.wrapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import lotto.utils.ErrorMessage;
+
 class LottoNumberTest {
-  
+    @DisplayName("1~45 사이의 로또 번호를 생성했을 때, 같은 번호를 가지면 같은 객체로 판단 가능한지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10, 25, 45})
+    void compareLottoNumber(int number) {
+        // given, when
+        LottoNumber lottoNumber = new LottoNumber(number);
+        LottoNumber sameLottoNumber = new LottoNumber(number);
+        // then
+        assertThat(lottoNumber).isEqualTo(sameLottoNumber);
+    }
+
+    @DisplayName("1~45 이외의 로또를 생성할 때 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {-3, 0, 46, 100})
+    void createInvalidLottoNumber(int number) {
+        assertThatThrownBy(() -> new LottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getFormattedMessage(1, 45));
+    }
 }

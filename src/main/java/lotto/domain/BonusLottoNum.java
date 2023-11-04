20991c181f7e0f9a4e6ae18bfabe410977a32d53
hil679 <hil679922@gmail.com>
2023-11-04 16:25:27 +0900

    Feat(BonusLottoNum): BonusLottoNum class 뼈대 추가

---
+++ src/main/java/lotto/domain/BonusLottoNum.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+public class BonusLottoNum {
+    private int bonusNum;
+
+    public int getBonusNum() {
+        return 0;
+    }
+
+    private void validateBonusLottoNum() {
+
+    }
+
+    private boolean isInteger() {
+        return true;
+    }
+
+    private void checkInteger() {
+
+    }
+
+    private boolean isInRange() {
+        return true;
+    }
+
+    private void checkRange() {
+
+    }
+
+    private boolean isDuplicateWinningNumbers(){
+        return true;
+    }
+
+    private void checkDuplicateWinningNumbers(){
+
+    }
+}

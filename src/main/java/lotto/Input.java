df6e0b25b8ef627fdd3f5c07fbdb3fe12c7d53eb
idingg <idingg@gmail.com>
2023-11-05 03:08:44 +0900

    feat: 추가 당첨 로또 번호 입력
    Lotto inputWinnerNumber()

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 class Input {
@@ -7,5 +10,13 @@ class Input {
         String raw = Console.readLine();
         Validation.inputMoneyExceptionCheck(raw);
         return Integer.parseInt(raw);
+    }
+
+    static Lotto inputWinnerNumber() {
+        String raw = Console.readLine();
+        Validation.inputWinnerNumberExceptionCheck(raw);
+        List<Integer> numbers = Arrays.stream(raw.split(",")).map(Integer::parseInt).toList();
+        Lotto winnerTicket = new Lotto(numbers);
+        return winnerTicket;
     }
 }

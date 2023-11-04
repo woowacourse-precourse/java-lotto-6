e12422dfb29fe5a7e47c64a71e9019b047cdddb8
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-11-05 00:34:49 +0900

    feat: winningNumbers(당첨번호)  - 불변클래스  - 보너스 번호를 갖고있음  - Lotto와 동일한 validdation을 사용

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import static lotto.utils.LottoValidateUtils.*;
+
+import java.util.List;
+
+public record WinningNumbers(List<Integer> drawnNumbers, int bonusNumber) {
+
+    public WinningNumbers {
+        validateLottoNumbers(drawnNumbers);
+        validateNumberRange(bonusNumber);
+    }
+}
---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import lotto.constant.ErrorMessage;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WinningNumbersTest {
+
+    @Test
+    @DisplayName("유효한 로또 번호와 보너스 번호로 WinningNumbers 객체 생성")
+    public void testValidWinningNumbersCreation() {
+        List<Integer> drawnNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(drawnNumbers, bonusNumber);
+
+        Assertions.assertAll(() -> {
+            assertThat(winningNumbers.drawnNumbers()).isEqualTo(drawnNumbers);
+            assertThat(winningNumbers.bonusNumber()).isEqualTo(bonusNumber);
+        });
+    }
+
+    @Test
+    @DisplayName("보너스 번호 범위가 잘못된 경우 예외 발생")
+    public void testWinningNumbersWithInvalidBonusNumber() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new WinningNumbers(numbers, 50))
+            .withMessage(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getMessage());
+    }
+}

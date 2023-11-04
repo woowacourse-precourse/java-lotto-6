3c1306fc8085bfea49490868d7775ccb762e0aa1
kimky0320 <kim3268418@naver.com>
2023-11-04 09:47:06 +0900

    refactor: 출력 전담 메서드
    출력 전담 메서드 추가 및 이에 따른 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,43 +1,50 @@
 package lotto.view;
 
+import lotto.domain.ConstNum;
 import lotto.domain.LottoTickets;
 import lotto.domain.Result;
 
+import java.text.DecimalFormat;
+
 public class OutputView {
-    private static Integer PRICE=1000;
+    private static DecimalFormat df = new DecimalFormat("#,###");
     public static void moneyMessage() {
-        System.out.println(Message.MONEY.getMessage());
+        printView(Message.MONEY.getMessage());
     }
 
     public static void buyComplete(Integer money) {
-        System.out.printf("\n%d개를 구매했습니다.\n",money/PRICE);
+        String s = String.format("\n%d개를 구매했습니다.", money / ConstNum.PRICE.getNum());
+        printView(s);
     }
 
     public static void printLotto(LottoTickets lottoTickets) {
-        System.out.println(lottoTickets.toString());
+        printView(lottoTickets.toString());
     }
 
     public static void winningMessage() {
-        System.out.println(Message.WINNING_NUM.getMessage());
+        printView(Message.WINNING_NUM.getMessage());
     }
 
     public static void bonusMessage() {
-        System.out.println(Message.BONUS_NUM.getMessage());
+        printView(Message.BONUS_NUM.getMessage());
     }
 
     public static void resultMessage(Result result,Integer money) {
-        System.out.println(Message.RESULT.getMessage());
+        printView(Message.RESULT.getMessage());
 
+        printView(String.format("%d개 일치 (%s원) - %d개",3,df.format(ConstNum.FIFTH_PRIZE.getNum()),result.getCount(5)));
+        printView(String.format("%d개 일치 (%s원) - %d개",4,df.format(ConstNum.FOURTH_PRIZE.getNum()),result.getCount(4)));
+        printView(String.format("%d개 일치 (%s원) - %d개",5,df.format(ConstNum.THIRD_PRIZE.getNum()),result.getCount(3)));
+        printView(String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개", 5, df.format(ConstNum.SECOND_PRIZE.getNum()), result.getCount(2)));
+        printView(String.format("%d개 일치 (%s원) - %d개",6,df.format(ConstNum.FIRST_PRIZE.getNum()),result.getCount(1)));
 
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 3, "5,000", result.getCount(5));
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 4, "50,000", result.getCount(4));
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 5, "1,500,000", result.getCount(3));
-        System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n", 5, "30,000,000", result.getCount(2));
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 6, "2,000,000,000", result.getCount(1));
-
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", result.getProfitPercentage(money));
+        printView(String.format("총 수익률은 %.1f%%입니다.",result.getProfitPercentage(money)));
     }
     public static void errorMessage(String message) {
-        System.out.printf("[ERROR] %s\n",message);
+        printView(String.format("[ERROR] %s",message));
+    }
+
+    private static void printView(String s) {
+        System.out.println(s);
     }
 }

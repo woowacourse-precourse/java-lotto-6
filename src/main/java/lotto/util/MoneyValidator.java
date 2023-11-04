1c41d60376d5ba61ad855a272b21716111b9fa63
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-05 01:31:56 +0900

    feat:MoneyValidator 기능 추가

---
+++ src/main/java/lotto/util/MoneyValidator.java
@@ -0,0 +1,20 @@
+package lotto.util;
+
+import lotto.constant.ErrorMessage;
+
+public class MoneyValidator {
+
+    static final int LOTTO_PRICE = 1000;
+
+    public static void validateNumber(int number) {
+        if(number < LOTTO_PRICE) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.toString());
+        }
+    }
+
+    public static void validateThousand(int number) {
+        if(number % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessage.ONLY_THOUSAND.toString());
+        }
+    }
+}

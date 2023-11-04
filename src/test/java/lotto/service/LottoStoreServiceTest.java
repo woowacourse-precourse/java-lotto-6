0090fc37dadea5738d1f436ce1a58632d989c2ef
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 05:13:45 +0900

    test: 로또 상점이 금액에 맞는 개수의 로또들을 반환하는지 테스트 작성

---
+++ src/test/java/lotto/service/LottoStoreServiceTest.java
@@ -0,0 +1,22 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("로또 상점 테스트")
+class LottoStoreServiceTest {
+
+    @DisplayName("구입 금액에 맞는 개수의 로또를 받을 수 있다")
+    @Test
+    void testObtainedCountOfLotto() {
+        PurchaseAmount purchaseAmount = PurchaseAmount.from(5000);
+        LottoStoreService lottoStoreService = new LottoStoreService(
+                new LottoGeneratorService(new LottoNumberGeneratorService()));
+        List<Lotto> lottos = lottoStoreService.purchaseLotto(purchaseAmount);
+        Assertions.assertThat(lottos.size()).isEqualTo(5);
+    }
+}

d61ead51598df2f71a59ec23f814ee70e942738a
yeeuniii <anna862700@gmail.com>
2023-11-05 03:39:23 +0900

    feat: 로또 숫자 범위 유효성 확인 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,19 +7,39 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        checkValidation(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6 || isDuplicated(numbers)) {
-            throw new IllegalArgumentException();
+    private void checkValidation(List<Integer> numbers) {
+        if (!isValidSize(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개이어야 합니다.");
         }
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되어서는 안됩니다.");
+        }
+        for (Integer number : numbers) {
+            checkValidRange(number);
+        }
+    }
+
+    private boolean isValidSize(List<Integer> numbers) {
+        return numbers.size() == 6;
     }
 
     private boolean isDuplicated(List<Integer> numbers) {
         HashSet<Integer> numbersWithoutDuplication = new HashSet<>(numbers);
         return numbersWithoutDuplication.size() != numbers.size();
+    }
+
+    private void checkValidRange(Integer number) {
+        if (!isValidRange(number)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private boolean isValidRange(Integer number) {
+        return number >= 1 && number <= 45;
     }
 
     public List<Integer> getNumbers() {
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1보다 작은 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderMinimum() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 45보다 큰 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderMaximum() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

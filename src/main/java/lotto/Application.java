184bc8d9ae53aeedd2dc11ccaa004270f9587269
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-04 17:49:26 +0900

    feat: 수익률 계산 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,6 +16,8 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
 
+        System.out.println();
+
         if (money.isBlank()) {
             throw new IllegalStateException("[ERROR] 아무 것도 입력되지 않았습니다.");
         }
@@ -36,20 +38,27 @@ public class Application {
 
         int buyLottoCount = parseMoney / lottoPrice;
 
-        List<Lotto> lotties = new ArrayList<>();
+        System.out.println(buyLottoCount + "개를 구매했습니다.");
+
+        ArrayList<Lotto> lotties = new ArrayList<>();
         for (int i = 0; i < buyLottoCount; i++) {
             Lotto lotto = new Lotto(makeLotto());
             lotties.add(lotto);
+            System.out.println(lotto.getLotto().toString());
         }
+
+        System.out.println();
 
         System.out.println("당첨 번호를 입력해 주세요.");
         String winNumber = Console.readLine();
+
+        System.out.println();
 
         if (winNumber.isBlank()) {
             throw new IllegalStateException("[ERROR] 아무 것도 입력되지 않았습니다.");
         }
 
-        if (winNumber.charAt(0) == ',' || winNumber.charAt(winNumber.length()) == ',') {
+        if (winNumber.charAt(0) == ',' || winNumber.charAt(winNumber.length() - 1) == ',') {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 숫자입니다.");
         }
 
@@ -65,6 +74,8 @@ public class Application {
         String bonusNumber = Console.readLine();
         int parseBounusNumber = Integer.parseInt(bonusNumber);
 
+        System.out.println();
+
         if (bonusNumber.isBlank()) {
             throw new IllegalStateException("[ERROR] 아무 것도 입력되지 않았습니다.");
         }
@@ -77,7 +88,6 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 당첨 번호와 다른 번호를 입력해주세요.");
         }
 
-        int correctCount = 0;
         int threeCorrect = 0;
         int fourCorrect = 0;
         int fiveCorrect = 0;
@@ -86,9 +96,10 @@ public class Application {
         int earnMoney = 0;
         boolean hasBonusNumber = false;
         for (int i = 0; i < lotties.size(); i++) {
+            int correctCount = 0;
             Lotto lotto = lotties.get(i);
             for (int j = 0; j < lotto.getLotto().size(); j++) {
-                if (lotto.getLotto().contains(parseWinNumbers.get(j))) {
+                if (lotto.getLotto().indexOf(parseWinNumbers.get(j)) != -1) {
                     correctCount++;
                 }
 
@@ -123,6 +134,17 @@ public class Application {
             }
         }
 
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + threeCorrect + "개");
+        System.out.println("4개 일치 (50,000원) - " + fourCorrect + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + fiveCorrect + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveBonusCorrect + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + sixCorrect + "개");
+
+        double parseEarnMoney = (double) earnMoney;
+        double rateOfReturn = parseEarnMoney / parseMoney * 100;
+        System.out.println("총 수익률은 " + String.format("%.1f", rateOfReturn) + "%입니다.");
     }
 
     public static List<Integer> makeLotto() {

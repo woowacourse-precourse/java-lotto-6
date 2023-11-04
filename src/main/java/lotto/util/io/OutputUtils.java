60807694cb58de8d3ef65b242301ba292a00b74e
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 19:02:54 +0900

    [feat] 구입 로또 출력 기능 구현

--- src/main/java/lotto/util/io/OutputUtils.java
+++ src/main/java/lotto/util/io/OutputUtils.java
@@ -1,5 +1,9 @@
 package lotto.util.io;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
 public class OutputUtils {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String NUMBER_OF_PURCHASE_MESSAGE = "%d개를 구매했습니다.";
@@ -10,5 +14,9 @@ public class OutputUtils {
 
     public static void printNumberOfPurchaseMessage(int number) {
         System.out.println(String.format(NUMBER_OF_PURCHASE_MESSAGE, number));
+    }
+
+    public static void printPurchaseLottos(List<Lotto> lottos) {
+        lottos.stream().forEach(lotto -> System.out.println(lotto.toString()));
     }
 }

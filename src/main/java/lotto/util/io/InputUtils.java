72258bf5faf0bae9a6fcc3bab506df8f57139d3a
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 19:32:52 +0900

    [feat] 당첨 번호 입력받기 기능 구현

--- src/main/java/lotto/util/io/InputUtils.java
+++ src/main/java/lotto/util/io/InputUtils.java
@@ -1,12 +1,28 @@
 package lotto.util.io;
 
+import static java.util.stream.Collectors.toList;
+
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.util.StringUtils;
 
 public class InputUtils {
     public static int getPurchaseAmount() {
         String money = Console.readLine();
         validateInputMoney(money);
         return Integer.parseInt(money);
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        String input = Console.readLine();
+        if (isNull(input)) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
+        }
+        List<String> numbers = StringUtils.splitByCommaAndTrim(input);
+        validateInputWinningNumbers(numbers);
+        return numbers.stream().map(Integer::parseInt).collect(toList());
     }
 
     private static void validateInputMoney(String money) {
@@ -18,7 +34,35 @@ public class InputUtils {
         }
     }
 
+    private static void validateInputWinningNumbers(List<String> numbers) {
+        for (String number : numbers) {
+            if (!isNumeric(number)) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호 모두 숫자를 입력해야 합니다.");
+            }
+            if (!isNumberInRange(Integer.parseInt(number))) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.");
+        }
+    }
+
     private static boolean isNumeric(String number) {
         return number.matches("\\d+");
     }
+
+    private static boolean isNull(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private static boolean isNumberInRange(int number) {
+        return number >= 1 && number <= 45;
+    }
+
+    private static boolean hasDuplicates(List<String> input) {
+        Set<String> uniqueElements = new HashSet<>(input);
+        return input.size() != uniqueElements.size();
+    }
+
 }

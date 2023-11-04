24657d7b4c17ade1e43ea6921bd4ea83c1773c68
unknown <winternrabbit@gmail.com>
2023-11-05 03:13:17 +0900

    refactor: 예외 처리 로직 수정

--- src/main/java/lotto/LottoCost.java
+++ src/main/java/lotto/LottoCost.java
@@ -2,49 +2,40 @@ package lotto;
 
 public class LottoCost {
     final static int UNIT = 1000;
+    final int cost;
+    final int ticket;
 
-    public int getNumberOfPurchase() {
-        Input input = new Input();
-        String cost = input.buyLotto();
-        boolean isValidated = isNumber(cost) && isMultipleOf(UNIT, cost);
+    public LottoCost(String cost) {
+        isNumber(cost);
+        isMultipleOf(UNIT, cost);
 
-        while (!isValidated) {
-            cost = input.buyLotto();
-            if (isNumber(cost) && isMultipleOf(UNIT, cost)) {
-                isValidated = true;
-            }
-        }
-
-        return Integer.parseInt(cost) / UNIT;
+        this.cost = Integer.parseInt(cost);
+        ticket = this.cost / UNIT;
     }
 
-    public boolean isNumber(String cost) {
+    public int getCost() {
+        return cost;
+    }
+
+    public int getTicket() {
+        return ticket;
+    }
+
+    public void isNumber(String cost) {
         boolean hasOnlyNum = !cost.isEmpty() && cost.chars().allMatch(Character::isDigit);
 
         if (!hasOnlyNum) {
-            try {
-                throw new IllegalArgumentException();
-            } catch (IllegalArgumentException e) {
-                System.out.println(ErrorMessages.ERROR_NOT_NUMBER.getMessage());
-            }
+            throw new IllegalArgumentException(ErrorMessages.ERROR_NOT_NUMBER.getMessage());
         }
-
-        return hasOnlyNum;
     }
 
-    public boolean isMultipleOf(int unit, String cost) {
+    public void isMultipleOf(int unit, String cost) {
         int costValue = Integer.parseInt(cost);
         boolean isRemainderZero
                 = (costValue >= unit && costValue < Integer.MAX_VALUE && costValue % unit == 0);
 
         if (!isRemainderZero) {
-            try {
-                throw new IllegalArgumentException();
-            } catch (IllegalArgumentException e) {
-                System.out.println(ErrorMessages.ERROR_NOT_MULTIPLE_OF_THOUSAND.getMessage());
-            }
+            throw new IllegalArgumentException(ErrorMessages.ERROR_NOT_MULTIPLE_OF_UNIT.getMessage());
         }
-
-        return isRemainderZero;
     }
 }

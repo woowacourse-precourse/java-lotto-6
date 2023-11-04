384236835fb5a3085c3c1c94f8a77fa8b50803fd
suihub <qkdcrt@gmail.com>
2023-11-04 17:14:14 +0900

    <feat(Rank)> : "열거형 등수 구현"
    - 각 등수를 열거형에 추가
    - 상수로 당첨금을 지니며, 이를 반환.

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
 public enum Rank {
+    FIFTH(5000),
+    FOURTH(50000),
+    THIRD(1_500_000),
+    SECOND(30_000_000),
+    FIRST(2_000_000_000);
+
+    private final Integer PRIZE_MONEY;
+
+    Rank(Integer prizeMoney) {
+        this.PRIZE_MONEY = prizeMoney;
+    }
+
+    public Integer getPrizeMoney() {
+        return PRIZE_MONEY;
+    }
 }

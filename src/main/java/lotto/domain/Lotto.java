48d7a56dfa50612a490880d1a0b2dfda8c41e20a
Bellwin98 <jongseunghan@naver.com>
2023-11-04 23:48:06 +0900

    Feat: 로또 숫자를 오름차순으로 정렬하여 반환하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -10,11 +11,14 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getSortedNumbers(){
+        Collections.sort(numbers);
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }

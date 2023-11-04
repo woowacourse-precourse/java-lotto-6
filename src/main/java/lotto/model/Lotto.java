9db58eb974af2f7928483442054e826afe478976
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-04 21:32:27 +0900

    feat: 로또 번호 중복 검증 로직 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,7 +1,6 @@
 package lotto.model;
 
-import lotto.model.Number;
-
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,22 +8,31 @@ public class Lotto {
 
     private final static int LOTTO_SIZE = 6;
 
-    private final List<Number> numbers;
+    private final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
-        this.numbers = numbers.stream()
-                .map(Number::new)
+        validateLottoDuplication(numbers);
+        this.lottoNumbers = numbers.stream()
+                .map(LottoNumber::new)
                 .collect(Collectors.toList());
     }
 
-    public List<Number> getNumbers() {
-        return numbers;
+    public List<LottoNumber> getNumbers() {
+        return Collections.unmodifiableList(lottoNumbers);
     }
 
     private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateLottoDuplication(List<Integer> numbers) {
+        long count = numbers.stream().distinct().count();
+
+        if (count != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다.");
         }
     }
 

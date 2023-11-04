73e20618210fd8110c58483d34bc3e2dec6ab9be
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 21:04:17 +0900

    feat(trimList, convertNumber) : 사용자의 입력을 로또 양식에 맞게 변환하는 기능추가
    docs : 사용자의 입력을 로또 양식에 맞게 변환 부분 체크완료, 숫자변환기 기능설명 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,23 +1,50 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.constant.ExceptionMessage;
 import lotto.util.NumberConverter;
 
 public class WinningNumbers {
+    private static final String COMMA_DELIMITER = ",";
     private final NumberConverter numberConverter;
 
     public WinningNumbers(NumberConverter numberConverter) {
         this.numberConverter = numberConverter;
     }
 
-    public void process(String numbers) {
+    public List<Integer> process(String numbers) {
         validateCommas(numbers);
+        List<String> trimmed = trimList(splitByComma(numbers));
+        return convertNumber(trimmed);
     }
 
     private void validateCommas(String numbers) {
-        if (numbers.startsWith(",") || numbers.endsWith(",") || numbers.contains(",,")) {
+        if (numbers.startsWith(COMMA_DELIMITER) || numbers.endsWith(COMMA_DELIMITER) || numbers.contains(
+                COMMA_DELIMITER + COMMA_DELIMITER)) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_COMMA_USAGE.getMessage());
         }
+    }
+
+    private List<String> splitByComma(String numbers) {
+        return Arrays.asList(numbers.split(COMMA_DELIMITER));
+    }
+
+    private List<String> trimList(List<String> lottonNumbers) {
+        List<String> trimmed = new ArrayList<>();
+        for (String name : lottonNumbers) {
+            trimmed.add(name.trim());
+        }
+        return trimmed;
+    }
+
+    private List<Integer> convertNumber(List<String> trimmed) {
+        return trimmed.stream()
+                .map(String::trim)
+                .map(numberConverter::convert)
+                .collect(Collectors.toList());
     }
 
 }

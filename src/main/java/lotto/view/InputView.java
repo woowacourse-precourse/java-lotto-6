7f50eac7f7571f9f952424dd20959c01ad40a86d
soozzang <soozzang@inha.edu>
2023-11-04 16:02:08 +0900

    Feat(InputView): 당첨 번호 입력받는 메서드, parse해주는 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
@@ -11,4 +13,22 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
         }
     }
+
+    public static ArrayList<Integer> readWinningNum() {
+        return parseWinningNum(readLine());
+    }
+
+    public static ArrayList<Integer> parseWinningNum(String winningNum) {
+        ArrayList<Integer> winningNums = new ArrayList<>();
+        String[] winningNumStr = winningNum.split(",");
+
+        for (String eachWinningNumStr : winningNumStr) {
+            int eachWinningNum = Integer.parseInt(eachWinningNumStr);
+            winningNums.add(eachWinningNum);
+        }
+
+        return winningNums;
+    }
+
+
 }

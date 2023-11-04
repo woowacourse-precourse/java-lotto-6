b81521588f01d6a2b7ffe95451d5bcadda991ef0
song-jae-wan <sjo9810@gmail.com>
2023-11-04 22:01:54 +0900

    feat: 로또 리스트 출력 구현
    View에서 로또 리스트 출력

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,8 +1,15 @@
 package view;
 
+import config.CountMessage;
+import config.OutputMessage;
+
 public class GameView {
 
     public void printGameCount(int count) {
         System.out.printf(OutputMessage.PAYMENT.getMessage(),count);
+    }
+
+    public void printLottoList(String lottoList) {
+        System.out.print(lottoList);
     }
 }

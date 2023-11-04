cf3372df7fa1287533c91ff6fa80ad50b1cfbe31
Brian <bwook9908@gmail.com>
2023-11-04 14:36:55 +0900

    feat: 출력 구현

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -7,6 +7,6 @@ public class Message {
     protected static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     protected static final String STATISTICS_MESSAGE = "당첨 통계";
     protected static final String REVENUE_FRONT_MESSAGE = "총 수익률은 ";
-    protected static final String REVENUE_BACK_MESSAGE = "입니다.";
+    protected static final String REVENUE_BACK_MESSAGE = "%입니다.";
     protected static final String BAR = "---";
 }

78164e1250433b77030defe68faa3a11997656d5
kimwanyoung <dhks2869@gmail.com>
2023-11-04 22:08:22 +0900

    feat : WinningNumber 중복 검증 기능 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,12 +1,25 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class WinningNumber {
 
+    private static final String NO_DUPLICATE_ERROR_MESSAGE = "중복된 숫자는 입력할 수 없습니다.";
     private final List<Integer> winningNumber;
 
     public WinningNumber(List<Integer> winningNumber) {
+        validateDuplicate(winningNumber);
         this.winningNumber = winningNumber;
+    }
+
+    private void validateDuplicate(List<Integer> winningNumber) {
+        if (isDuplicateNumbers(winningNumber)) {
+            throw new IllegalArgumentException(NO_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isDuplicateNumbers(List<Integer> winningNumber) {
+        return new HashSet<>(winningNumber).size() < 6;
     }
 }

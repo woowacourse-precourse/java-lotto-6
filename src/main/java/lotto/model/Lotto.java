af191481da928a93ed8dcc297a35e6ee45aae388
Sanghwi Lee <sanghwi47@naver.com>
2023-11-04 17:33:16 +0900

    feat(Lotto,LottoService) Lotto 에서 유효성 검사를 하며, LottoService 에서 랜덤한 숫자를 생성해 로또 번호 리스트를 만드는 로직을 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,18 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현
+
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,34 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+
 public class LottoService {
+
+    public List<Lotto> purchaseLottoTickets(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> lottoNumbers = generateLottoNumbers();
+            lottos.add(new Lotto(lottoNumbers));
+        }
+        return lottos;
+    }
+
+    private static List<Integer> generateLottoNumbers() {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while (uniqueNumbers.size() < 6) {
+            int number = Randoms.pickNumberInRange(1, 45);
+            uniqueNumbers.add(number);
+        }
+        List<Integer> lottoNumbers = new ArrayList<>(uniqueNumbers);
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
 }

d64c977470a8e2e4bacb62ab81814b9e7859ec2b
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-04 14:46:55 +0900

    refactor: 불필요한 필드 제거
    Message를 하나로 통일

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,33 +4,39 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    BOOM(0, false, "꽝", "(0원)"),
-    FIFTH(3, false, "3개 일치", "(5,000원)"),
-    FOURTH(4, false, "4개 일치", "(50,000원)"),
-    THIRD(5, false, "5개 일치", "(1,500,000원)"),
-    SECOND(5, true, "5개 일치", "보너스 볼 일치 (30,000,000원)"),
-    FIRST(6, false, "6개 일치", "(2,000,000,000원)");
+    BOOM(0, false, "꽝 (0원)"),
+    FIFTH(3, false, "3개 일치 (5,000원)"),
+    FOURTH(4, false, "4개 일치 (50,000원)"),
+    THIRD(5, false, "5개 일치 (1,500,000원)"),
+    SECOND(5, true, "5개 일치 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, false, "6개 일치 (2,000,000,000원)");
 
-    private int sameCount;
-    private boolean sameBonus;
-    private String sameCountMessage;
-    private String price;
+    private final int matchingCount;
+    private final boolean hasBonusNumber;
+    private final String price;
 
-    Rank(int sameCount, boolean sameBonus, String sameCountMessage, String price) {
-        this.sameCount = sameCount;
-        this.sameBonus = sameBonus;
-        this.sameCountMessage = sameCountMessage;
+    Rank(final int matchingCount, final boolean hasBonusNumber, final String price) {
+        this.matchingCount = matchingCount;
+        this.hasBonusNumber = hasBonusNumber;
         this.price = price;
     }
 
     public Rank getRank(int sameCount, boolean sameBonus) {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> isSameRank(rank, sameCount, sameBonus))
+        return Arrays.stream(values())
+                .filter(rank -> rank.isMatchingRank(sameCount, sameBonus))
                 .findAny()
-                .orElse(Rank.BOOM);
+                .orElse(BOOM);
     }
 
-    private boolean isSameRank(final Rank rank, final int sameCount, final boolean sameBonus) {
-        return rank.sameCount == sameCount && rank.sameBonus == sameBonus;
+    private boolean isMatchingRank(final int sameCount, final boolean sameBonus) {
+        return hasFiveCountAndBonus(sameCount, sameBonus) || hasMatchingCount(sameCount);
+    }
+
+    private boolean hasFiveCountAndBonus(final int sameCount, final boolean sameBonus) {
+        return sameCount == 5 && this.hasBonusNumber == sameBonus;
+    }
+
+    private boolean hasMatchingCount(final int sameCount) {
+        return this.matchingCount == sameCount;
     }
 }

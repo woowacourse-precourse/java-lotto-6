898c8eba73abf120a5c57f34970e4b740933545c
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-04 20:23:45 +0900

    <feat> : <당첨 번호 유효성 검사 추가>
    당첨 번호의 길이가 알맞은지 숫자가 맞는지 범위 내에 드는지로 유효성 검사 추가

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -7,40 +7,56 @@ public class Validate {
     private static final String NOT_IN_RANGE_ERROR= "[ERROR] 번호는 1~45 이내에 있어야 합니다.";
     private static final String NOT_NUMBER_ERROR = "[ERROR] 숫자를 입력해야 합니다.";
     private static final String INCORRECT_BONUS_LENGTH_ERROR = "[ERROR] 보너스 번호의 길이가 올바르지 않습니다.";
-    private static final String NOT_DIVIDED_BY_1000_WON = "[ERROR] 1000원으로 나누어 떨어지지 않는 값입니다."
+    private static final String NOT_DIVIDED_BY_1000_WON = "[ERROR] 1000원으로 나누어 떨어지지 않는 값입니다.";
+    private static final String INCORRECT_WINNING_LENGTH_ERROR = "[ERROR] 당첨 번호의 길이가 올바르지 않습니다.";
     public void CheckMoneyInput(String input){ // 로또 구입 금액 체크
         CheckIsNumeric(input);
-        CheckIs1000Divisible(parser.parseNumberOfMoney(input));
+        CheckIs1000Divisible(parser.parseInteger(input));
     }
-    public void CheckWinningNumber(WinningLotto winningLotto){ // 당첨 번호 체크
-
+    public void CheckWinningNumber(String input){ // 당첨 번호 체크
+        String[] Checkinput = input.split(",");
+        CheckWinningNumberLength(Checkinput);
+        
+        for(int i=0; i< Checkinput.length; i++){
+            CheckNumberInRange(parser.parseInteger(Checkinput[i]));
+            CheckIsNumeric(Checkinput[i]);
+        }
     }
     public void CheckBonusNumber(String input){ // 보너스 번호 체크
-        BonusNumberLengthCheck(input);
+        CheckBonusNumberLength(input);
         CheckIsNumeric(input);
-        CheckNumberInRange(parser.parseNumberOfMoney(input));
+        CheckNumberInRange(parser.parseInteger(input));
     }
+
+    // 구입 금액 사용
     private void CheckIs1000Divisible(int input){
         if(input % 1000 != 0){
             throw new IllegalArgumentException(NOT_DIVIDED_BY_1000_WON);
         }
     }
 
+    // 당첨 번호 사용
+    private void CheckWinningNumberLength(String[] Checkinput){
+        if(Checkinput.length != 6){
+            throw new IllegalArgumentException(INCORRECT_WINNING_LENGTH_ERROR);
+        }
+    }
+
+    // 보너스 번호 사용
+    private void CheckBonusNumberLength(String input){
+        if(input.length() != 1){
+            throw new IllegalArgumentException(INCORRECT_BONUS_LENGTH_ERROR);
+        }
+    }
+    // 공통 사용
     private void CheckNumberInRange(int input) {
         if (input < 1 || input > 45) {
             throw new IllegalArgumentException(NOT_IN_RANGE_ERROR);
         }
     }
-
     private void CheckIsNumeric(String input) {
         if(!input.matches("\\d+")){
             throw new IllegalArgumentException(NOT_NUMBER_ERROR);
-        }
-    }
-
-    private void BonusNumberLengthCheck(String input){
-        if(input.length() != 1){
-            throw new IllegalArgumentException(INCORRECT_BONUS_LENGTH_ERROR);
         }
     }
 }

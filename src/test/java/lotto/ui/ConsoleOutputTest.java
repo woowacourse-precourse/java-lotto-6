bcdc06cee858157c61c2e78387590003874b44e9
Juhyorim <joohr1234@gmail.com>
2023-11-04 22:42:57 +0900

    test: 발행한 로또 내역 출력 테스트 작성

--- src/test/java/lotto/ui/ConsoleOutputTest.java
+++ src/test/java/lotto/ui/ConsoleOutputTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +28,25 @@ class ConsoleOutputTest {
         //then
         String result = captor.toString().trim();
         assertThat(result).isEqualTo(LOTTO_PRICE_REQUEST);
+    }
+
+    @Test
+    @DisplayName("로또 내역 출력")
+    void 로또_내역_출력() {
+        //given
+        OutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(32, 33, 34, 35, 36, 37)));
+
+        //when
+        Output output = new ConsoleOutput();
+        output.printPurchasedLotto(lottos);
+
+        //then
+        String result = captor.toString().trim();
+        assertThat(result).contains("2개를 구매했습니다", "[1, 2, 3, 4, 5, 6]", "[32, 33, 34, 35, 36, 37]");
     }
 }

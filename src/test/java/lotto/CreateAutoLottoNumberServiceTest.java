d482ea86d3a9a383322f721a260f9fe367936e75
totocm <typesapk8445@gmail.com>
2023-11-04 22:44:31 +0900

    1.CreateRandomNumberService -> 서비스 로직 끼리의 데이터 교환 -> 본래의 Randoms를 추적할 수 없다 -> 도메인과 서비스의 관계에 대해서 알아보니 -> 이렇게 서비스 안에서 도메인들끼리 엮어서 -> 처리한다고 한다. 2.CreateRandomNumberServiceTest -> 시즌마다 출력해봄 -> 구매 테스트도 살짝해봄

---
+++ src/test/java/lotto/CreateAutoLottoNumberServiceTest.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import lotto.service.domain.lotto.CreateAutoLottoNumberService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CreateAutoLottoNumberServiceTest {
+
+    private CreateAutoLottoNumberService cal = new CreateAutoLottoNumberService();
+    @Test
+    public void LottoOneSeason(){
+        List<Integer> lottoNumbers = cal.getLottoSixNumbers();
+        int bonusNumber = cal.getBonusNumber();
+        System.out.println(lottoNumbers);
+        System.out.println(bonusNumber);
+    }
+    @Test
+    public void LottoTwoSeason(){
+        List<Integer> lottoNumbers = cal.getLottoSixNumbers();
+        int bonusNumber = cal.getBonusNumber();
+        System.out.println(lottoNumbers);
+        System.out.println(bonusNumber);
+    }
+    @Test
+    public void LottoThreeSeason(){
+        List<Integer> lottoNumbers = cal.getLottoSixNumbers();
+        int bonusNumber = cal.getBonusNumber();
+        System.out.println(lottoNumbers);
+        System.out.println(bonusNumber);
+    }
+    @Test
+    public void LottoBuyTest(){
+        int buyPickNumber = 8;
+        int buyMoney = buyPickNumber * 1000;
+        for(int i = 1; i <= buyPickNumber; i++){
+            List<Integer> lottoNumbers = cal.getLottoSixNumbers();
+            System.out.println("---- " + i +" 회차 구매");
+            System.out.println("투입 금액 : " + buyMoney);
+            System.out.println(lottoNumbers);
+            System.out.println("남은 잔액 : " + (buyMoney -= 1000));
+            if(buyMoney == 0){
+                System.out.println("-----------");
+                System.out.println("모든 게임이 종료");
+                System.out.println("-----------");
+            }
+        }
+    }
+}

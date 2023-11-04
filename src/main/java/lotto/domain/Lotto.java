9467650d7b88be49231f2af25b4c2ab64f6078e6
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 22:27:05 +0900

    feat(Lotto): 로또 번호 입력값 범위 검증 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,13 +4,15 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+    private final int LOTTO_RANGE_IN_START_VALUE = 1;
+    private final int LOTTO_RANGE_IN_END_VALUE = 45;
     public static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateUniqueElements(numbers, uniqueNumberSet(numbers));
-
+        validationInRange(numbers);
         this.numbers = numbers;
     }
 
@@ -29,4 +31,16 @@ public class Lotto {
     private HashSet<Integer> uniqueNumberSet(List<Integer> numbers) {
         return new HashSet<>(numbers);
     }
+
+    public void validationInRange(List<Integer> lotto) {
+        if (!lotto.stream().allMatch(this::test)) {
+            throw new IllegalArgumentException("[ERROR] 로또 입력 값은 1이상 45이하의 값만 가능합니다.");
+        }
+    }
+
+    private boolean test(Integer number) {
+        return LOTTO_RANGE_IN_START_VALUE <= number
+                && number <= LOTTO_RANGE_IN_END_VALUE;
+    }
+
 }

e7d13b96c8d4581ad25787e19bdebd760625a718
Junho Lee <ghzm888@gmail.com>
2023-11-04 22:27:25 +0900

    feat : 1~45의 6개의 난수 List를 생성기 작성

---
+++ src/main/java/lotto/global/utils/generator/RandomNumberGenerator.java
@@ -0,0 +1,19 @@
+package lotto.global.utils.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    private static final int MIN_NUMBER_OF_RANGE = 1;
+    private static final int MAX_NUMBER_OF_RANGE = 45;
+
+    @Override
+    public List<Integer> generate() {
+        List<Integer> randomList = Randoms.pickUniqueNumbersInRange(MIN_NUMBER_OF_RANGE, MAX_NUMBER_OF_RANGE, 6);
+        Collections.sort(randomList);
+        return randomList;
+    }
+}

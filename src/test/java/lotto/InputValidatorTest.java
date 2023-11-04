57c04f20f7497be7284822895cd4d61e0dd4030d
ImWonYong <ssassb123@daum.net>
2023-11-05 01:53:28 +0900

    test(InputValidator): 당첨 번호와 중복된 숫자가 있으면 예외 처리 테스트

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -2,9 +2,21 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class InputValidatorTest {
+
+    @Test
+    void 보너스_번호가_로또_번호와_중복이면_예외() {
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6);
+
+        int bonusNumber = 6;
+
+        assertThatThrownBy(() -> InputValidator.validateBonusNumberNoDuplicate(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호와 보너스 번호가 겹칩니다.");
+    }
 
     @Test
     void 로또_번호가_1에서_45가_아니면_예외() {

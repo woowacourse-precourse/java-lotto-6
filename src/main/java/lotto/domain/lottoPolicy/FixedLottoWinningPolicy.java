64ab38283785c0e76b62a5c64a5c1baa082fa80a
Minjae-An <mj3242@naver.com>
2023-11-04 21:54:15 +0900

    feat : 고정 당첨금 지급 정책 구현

---
+++ src/main/java/lotto/domain/lottoPolicy/FixedLottoWinningPolicy.java
@@ -0,0 +1,28 @@
+package lotto.domain.lottoPolicy;
+
+import java.util.List;
+import lotto.domain.prize_calculator.PrizeCalculator;
+
+public class FixedLottoWinningPolicy implements LottoWinningPolicy {
+    private final PrizeCalculator prizeCalculator;
+
+    public FixedLottoWinningPolicy(PrizeCalculator prizeCalculator) {
+        this.prizeCalculator = prizeCalculator;
+    }
+
+    @Override
+    public Integer getWinningPrize(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+        Long matchCount = getMatchCount(lottoNumbers, winningNumbers);
+        return getPrize(matchCount);
+    }
+
+    private Long getMatchCount(List<Integer> lottoNumbers, List<Integer> winningNumbers){
+        return lottoNumbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    private Integer getPrize(Long matchCount){
+        return prizeCalculator.calculatePrize(matchCount);
+    }
+}

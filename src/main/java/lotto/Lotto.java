fb1835bc51be68f9a3b4268c1830bb3ad117a89e
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-04 18:22:51 +0900

    feat: 로또 각 숫자의 중복여부 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,28 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    	sizeValidate(numbers);
+    	distinctValidate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void sizeValidate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
+    private void distinctValidate(List<Integer> numbers) {
+    	Set<Integer> deleteDuplicate = new HashSet<Integer>(numbers);
+    	if (numbers.size() != deleteDuplicate.size()) {
+    		throw new IllegalArgumentException();
+		}
+    }
     // TODO: 추가 기능 구현
 }

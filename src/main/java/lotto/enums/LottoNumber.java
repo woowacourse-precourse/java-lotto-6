4e7ce8d89ca77148b1a09766bc2f344df3d3cb97
paulham <pauljungho.ham@gmail.com>
2023-11-04 15:33:53 +0900

    docs: add login process feat: add enums directory and LottoNumber enums

---
+++ src/main/java/lotto/enums/LottoNumber.java
@@ -0,0 +1,16 @@
+package lotto.enums;
+
+public enum LottoNumber {
+    FIRST(1)
+    , LAST(45)
+    , LEAST_PURCHASE_AMOUNT(1000);
+
+    private final int number;
+    LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+}

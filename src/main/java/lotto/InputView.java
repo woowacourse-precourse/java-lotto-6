e3825fad626ca0f6da3ce6a0b487a732d8e95e0d
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-04 20:02:08 +0900

    feat : 구입금액, 당첨번호, 보너스번호 입력 기능 추가

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readLottoBuyMoney() {
+        return Console.readLine();
+    }
+
+    public String readLottoWinningNumber() {
+        return Console.readLine();
+    }
+
+    public String readLottoBonusNumber() {
+        return Console.readLine();
+    }
+}

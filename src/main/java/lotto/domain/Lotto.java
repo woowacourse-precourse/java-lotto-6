cddb5250816505ce9ddad123634ad6288cfb4da3
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 10:31:59 +0900

    feat(Lotto): 로또 저장 검증

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,55 @@
+package lotto.domain;
+
+import lotto.constants.LottoRule;
+import lotto.exception.LottoNumberDuplicateException;
+import lotto.exception.LottoNumbersSizeException;
+import lotto.exception.LottoRangeException;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        validateLottoRange(numbers);
+        validateLottoDuplicate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != LottoRule.NUMBER_MAX_SIZE) {
+            throw new LottoNumbersSizeException();
+        }
+    }
+
+    private void validateLottoRange(List<Integer> numbers) {
+        long numbersCount = getCountFilterByLottoRange(numbers);
+        if (numbersCount < LottoRule.NUMBER_MAX_SIZE) {
+            throw new LottoRangeException();
+        }
+    }
+
+    private long getCountFilterByLottoRange(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(Lotto::isInRange)
+                .count();
+    }
+
+    private static boolean isInRange(Integer num) {
+        return num < LottoRule.START_NUMBER || num > LottoRule.END_NUMBER;
+    }
+
+    private void validateLottoDuplicate(List<Integer> numbers) {
+        long numbersCount = getCountFilterByDuplicated(numbers);
+        if (numbersCount < LottoRule.NUMBER_MAX_SIZE) {
+            throw new LottoNumberDuplicateException();
+        }
+    }
+
+    private long getCountFilterByDuplicated(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count();
+    }
+}

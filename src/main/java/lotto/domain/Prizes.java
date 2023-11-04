23fd7d199bfc44f5d19b329920e3011cab522e1c
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 15:28:27 +0900

    [feat] Prizes 클래스 필드 정의

--- src/main/java/lotto/domain/Prizes.java
+++ src/main/java/lotto/domain/Prizes.java
@@ -1,4 +1,32 @@
 package lotto.domain;
 
+import org.mockito.junit.MockitoTestRule;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Prizes {
+
+    private final Map<Prize, Integer> prizes;
+
+    public Prizes(List<Prize> prizeDummy) {
+        this.prizes = new HashMap<>();
+        createPrizeMap(prizeDummy);
+    }
+
+    private void createPrizeMap(List<Prize> prizeDummy) {
+        for (Prize prize : prizeDummy) {
+            if (prizes.containsKey(prize)) {
+                prizes.put(prize, prizes.get(prize)+1);
+            } else {
+                prizes.put(prize, 1);
+            }
+        }
+    }
+
+
+
+
 }

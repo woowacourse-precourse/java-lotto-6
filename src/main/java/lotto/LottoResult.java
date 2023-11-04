ba174707b3c686995c81d48b3e8242b086ab9c28
pjaehyun <show6756@gmail.com>
2023-11-04 18:09:35 +0900

    feat: 추가 LottoResult 생성자

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,4 +1,12 @@
 package lotto;
 
 public class LottoResult {
+
+    private final int resultNumbers;
+    private final int bonusNumber;
+
+    public LottoResult(String input1, String input2) {
+        this.resultNumbers = Integer.parseInt(input1);
+        this.bonusNumber = Integer.parseInt(input2);
+    }
 }

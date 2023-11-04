428f661152f2e5050fbe556119ee24ff06163ef5
JungYeon <jyp75580@gmail.com>
2023-11-04 14:54:13 +0900

    feat : 입력 View /  출력 View 기능

---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import java.util.Map;
+
+public class OutputView {
+
+    private static final String OUTPUT_VIEW_PURCHASE = "개를 구매했습니다.";
+
+    public void purchaseLottoCount(int count) {
+        System.out.println(count + OUTPUT_VIEW_PURCHASE);
+    }
+
+    public void LottoNumber(Lotto lotto) {
+        System.out.println(lotto.getNumbers().toString());
+    }
+
+    public void LottoGameResult(Map<String, Integer> winResult) {
+        System.out.println("당첨 통계\n---");
+        for (String result : winResult.keySet()) {
+            System.out.println(result + " - " + winResult.get(result) + "개");
+        }
+    }
+
+    public void totalRating(String rate) {
+        System.out.println("총 수익률은 " + rate + "%입니다");
+    }
+
+
+}

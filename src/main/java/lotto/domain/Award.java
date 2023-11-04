5486de5adeedf42726599192f3203373dad2664a
Hoo-sung <gntjd135@gmail.com>
2023-11-04 10:16:11 +0900

    feat(Award): Enum을 적용한 Award 생성

---
+++ src/main/java/lotto/domain/Award.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public enum Award {
+    ZERO(0),
+    ONE(0),
+    TWO(0),
+    THREE(5_000),
+    FOUR(50_000),
+    FIVE(1_500_000),
+    FIVE_BONUS(30_000_000),
+    SIX(2_000_000_000);
+
+    private final int reward;
+
+    Award(int reward) {
+        this.reward = reward;
+    }
+
+}

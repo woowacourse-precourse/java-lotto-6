bf3d8db9d68ff204238912445441a63e71a09a12
yshong1998 <yshong1998@gmail.com>
2023-11-05 05:03:24 +0900

    Test(LottoTest): 범위 밖 숫자 입력에 대한 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
+import lotto.util.LottoValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,5 +25,21 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("범위 밖의 숫자가 존재하면 예외가 발생한다")
+    @ParameterizedTest
+    @MethodSource("generateInvalidLottoNumbers")
+    void lottoNumbersOutOfRangeTest(List<Integer> lottoNumbers) {
+        assertThatThrownBy(() -> LottoValidator.validateLotto(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> generateInvalidLottoNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 46)),
+                Arguments.of(List.of(0, 1, 2, 3, 4, 5)),
+                Arguments.of(List.of(-3, 4, 5, 6, 7, 8))
+        );
     }
 }

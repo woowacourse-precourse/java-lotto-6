ef2d811be13c8a71c870e7a4b8caf1ce96d740db
KIVSH <kihongkim3@hotmail.com>
2023-11-04 23:44:55 +0900

    feat: update all method

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -7,13 +7,25 @@ public class Output {
         System.out.println(message);
     }
 
-    public static void issuedLotto(int lottoCount, List<Lotto> lottos) {
+    public static void issueLotto(int lottoCount, List<Lotto> lottos) {
         final String purchaseMessage = "개를 구매했습니다.";
         message(Integer.toString(lottoCount).concat(purchaseMessage));
 
         for (Lotto lotto : lottos) {
-            lotto.toString();
+            lotto.getNumbers().toString();
         }
+    }
+
+    public static void winStatistics(List<Integer> winRanks) {
+        final String winStatisticsMessage = "당첨 통계\n---";
+        message(winStatisticsMessage);
+        // continue...
+    }
+
+    public static void returnRate(int totalPrize, int purchaseMoney) {
+        double returnRate = 100.0 * (totalPrize / purchaseMoney);
+        System.out.println(Math.round(returnRate));
+        // continue...
     }
 
 }

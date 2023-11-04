cd2c82d1cba9c53d0110833ba2d55a0e6f46c7a8
WooJJam <jaemin5548@naver.com>
2023-11-05 00:34:26 +0900

    [Feat] 로또 구매 갯수 산정

--- src/main/java/lotto/model/Payment.java
+++ src/main/java/lotto/model/Payment.java
@@ -5,11 +5,19 @@ import lotto.validator.PaymentValidator;
 import java.util.regex.Pattern;
 
 public class Payment {
-    private final int price;
-
+    private final int ticket;
     public Payment(String input) {
         PaymentValidator.validate(input);
-        this.price = Integer.parseInt(input);
+        int price = Integer.parseInt(input);
+        this.ticket = calculateTicketsCount(price);
     }
+
+    public int calculateTicketsCount(int price) {
+        return price/1000;
+    }
+
+   public int getTicket() {
+        return ticket;
+   }
 
 }

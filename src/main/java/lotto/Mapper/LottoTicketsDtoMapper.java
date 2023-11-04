8834fb762d63a89b59138a42784b0131e9de0faf
jmj005 <jmj00005@naver.com>
2023-11-05 02:57:56 +0900

    feat: LottoTickets 객체를 LottoTicketsDto로 매핑하는 메소드 구현

--- src/main/java/lotto/Mapper/LottoTicketsDtoMapper.java
+++ src/main/java/lotto/Mapper/LottoTicketsDtoMapper.java
@@ -1,2 +1,16 @@
-package lotto.Mapper;public class LottoTicketsDtoMapper {
+package lotto.Mapper;
+
+import java.util.List;
+import lotto.dto.LottoDto;
+import lotto.dto.LottoTicketsDto;
+import lotto.model.LottoTickets;
+
+public class LottoTicketsDtoMapper {
+    public static LottoTicketsDto from(LottoTickets lottoTickets) {
+        List<LottoDto> lottoTicketsDto = lottoTickets.getLottoTickets()
+                .stream()
+                .map(LottoDtoMapper::from)
+                .toList();
+        return new LottoTicketsDto(lottoTicketsDto);
+    }
 }

1715ce05bdcf2eb1a9236d2828bba6b419e1b259
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 20:20:46 +0900

    [feat] 입력값 받아오는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,12 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+
+
+    private String getInput() {
+        return Console.readLine();
+    }
 }

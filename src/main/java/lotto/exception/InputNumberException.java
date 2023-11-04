3dd5db25b10fb359ccd86e62b273704b7fe10e99
suihub <qkdcrt@gmail.com>
2023-11-04 14:07:23 +0900

    <feat(InputNumberException)> : "입력 숫자의 범위 검증 기능 구현"
    - 당첨 번호 및 보너스 번호 입력 시, 1~45 사이인지를 검증하는 기능 구현.

--- src/main/java/lotto/exception/InputNumberException.java
+++ src/main/java/lotto/exception/InputNumberException.java
@@ -10,6 +10,7 @@ public class InputNumberException {
 
     public static void inputValidate(List<String> numbers) {
         notNumberValidate(numbers);
+        numberRangeValidate(numbers);
     }
 
     private static void notNumberValidate(List<String> numbers) {
@@ -20,5 +21,16 @@ public class InputNumberException {
                         throw new IllegalArgumentException(ERROR.getMessage() + NUMBER_RANGE.getMessage());
                     }
                 });
+    }
+
+    private static void numberRangeValidate(List<String> numbers) {
+        long count = numbers.stream()
+                .map(Integer::valueOf)
+                .filter((Integer num) -> 1 <= num && num <= 45)
+                .count();
+
+        if (count != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 }

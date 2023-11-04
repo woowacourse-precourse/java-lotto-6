3ce14433ec4bfbae7a40f127077fbd03d66163d5
jyajoo <jyajoo1020@gmail.com>
2023-11-05 00:38:20 +0900

    feat: 1부터 45까지 중복되지 않는 6개의 수 생성
    - LottoSetting에 1, 45, 6에 해당하는 상수 추가
    - NumberGenerator 인터페이스 추가
    - RandomNumberGenerator로 랜덤 수 생성 오버라이딩

--- src/main/java/lotto/constant/LottoSetting.java
+++ src/main/java/lotto/constant/LottoSetting.java
@@ -1,7 +1,11 @@
 package lotto.constant;
 
 public enum LottoSetting {
-    DIVISOR(1000);
+    DIVISOR(1000),
+    MIN_RANDOM_NUMBER(1),
+    MAX_RANDOM_NUMBER(45),
+    LOTTO_NUMBERS(6)
+    ;
 
     private final int value;
 

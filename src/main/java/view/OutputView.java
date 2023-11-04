0de859c866ac11587526090a06911b134b140c5d
star-candy <el0710@naver.com>
2023-11-04 23:30:57 +0900

    feat(lotto): 랜덤 6자리 숫자를 조건에 맞게 출력하는 기능

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,12 +2,17 @@ package view;
 
 import java.util.List;
 import enums.ConstantStringManager;
+import java.util.stream.Collectors;
 public class OutputView {
-    final int firstIndex = 1;
-    final int secondIndex = 2;
-    final int thirdIndex = 3;
-    final int forthIndex = 4;
-    final int fifthIndex = 5;
+    static final int FIRST_INDEX = 1;
+    static final int SECOND_INDEX = 2;
+    static final int THIRD_INDEX = 3;
+    static final int FORTH_INDEX = 4;
+    static final int FIFTH_INDEX = 5;
+    static final String LIST_START_MARK = "[";
+    static final String LIST_END_MARK = "]\n";
+    static final String LIST_SPLIT_MARK = ", ";
+
     public void printLottoPriceInputNotify() {
     String message = ConstantStringManager.LOTTO_PRICE_INPUT_NOTIFY.getMessage();
     System.out.println(message);
@@ -16,6 +21,15 @@ public class OutputView {
     public void printLottoNumberNotify(int number) {
         String message = ConstantStringManager.LOTTO_NUMBER_NOTIFY.getMessage();
         System.out.printf(message, number);
+    }
+
+    public void printRandomNumber(List<Integer> randomNumber) {
+        System.out.print(LIST_START_MARK);
+        String result = randomNumber.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(LIST_SPLIT_MARK));
+        System.out.print(result);
+        System.out.print(LIST_END_MARK);
     }
 
     public void printWinningNumberInputNotify() {
@@ -30,11 +44,11 @@ public class OutputView {
 
     public void printWinningStatics(List<Integer> staticsValue) {
         System.out.print(ConstantStringManager.WINNING_STATICS_NOTIFY);
-        System.out.printf(ConstantStringManager.LOTTO_SAME_THREE.getMessage(), staticsValue.get(firstIndex));
-        System.out.printf(ConstantStringManager.LOTTO_SAME_FOUR.getMessage(), staticsValue.get(secondIndex));
-        System.out.printf(ConstantStringManager.LOTTO_SAME_FIVE.getMessage(), staticsValue.get(thirdIndex));
-        System.out.printf(ConstantStringManager.LOTTO_SAME_FIVE_BONUS.getMessage(), staticsValue.get(forthIndex));
-        System.out.printf(ConstantStringManager.LOTTO_SAME_SIX.getMessage(), staticsValue.get(fifthIndex));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_THREE.getMessage(), staticsValue.get(FIRST_INDEX));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_FOUR.getMessage(), staticsValue.get(SECOND_INDEX));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_FIVE.getMessage(), staticsValue.get(THIRD_INDEX));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_FIVE_BONUS.getMessage(), staticsValue.get(FORTH_INDEX));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_SIX.getMessage(), staticsValue.get(FIFTH_INDEX));
     }
 
     public void printTotalReturn(int totalReturn) {

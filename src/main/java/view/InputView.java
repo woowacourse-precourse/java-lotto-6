5ab4249990fec4e14e3cc1bd82227a3258c0b1b8
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 17:21:33 +0900

    feat: 입력 문구 출력 및 사용자 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,8 +3,22 @@ package view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    private final String ENTER_PRICE = "구입금액을 입력해 주세요.";
-    private final String ENTER_WINNGING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private final String ENTER_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
+    private static final String ENTER_PRICE = "구입금액을 입력해 주세요.";
+    private static String ENTER_WINNGING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static String ENTER_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
 
+    public static String enterPrice() {
+        System.out.println(ENTER_PRICE);
+        return readLine();
+    }
+
+    public static String enterWinningNumbers() {
+        System.out.println(ENTER_WINNGING_NUMBERS);
+        return readLine();
+    }
+
+    public static String enterBonusNumbers() {
+        System.out.println(ENTER_BONUS_NUMBERS);
+        return readLine();
+    }
 }

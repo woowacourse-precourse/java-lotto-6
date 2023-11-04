aa4c63550133f7cbb01b1cfc8d0692bc6b042f81
seminchoi <choicco89@gmail.com>
2023-11-04 20:14:56 +0900

    feat(constant.LottoConstraint) : 로또 제약 조건과 관련된 상수를 정의한 enum 클래스 구현

---
+++ src/main/java/lotto/constant/LottoConstraint.java
@@ -0,0 +1,20 @@
+package lotto.constant;
+
+public enum LottoConstraint {
+    PRICE_PER_LOTTO(1000),
+    MIN_PURCHASE_QUANTITY(1),
+    MAX_PURCHASE_QUANTITY(1000000),
+    MIN_LOTTO_NUMBER(1),
+    MAX_LOTTO_NUMBER(45),
+    ;
+
+    private final int value;
+
+    LottoConstraint(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

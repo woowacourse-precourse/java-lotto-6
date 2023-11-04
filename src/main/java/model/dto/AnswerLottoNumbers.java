74ceda77e3a4ccb2b68a518ab81524d7e5f22a11
WonyuChoi <phillip07290@naver.com>
2023-11-04 13:28:33 +0900

    feat: 입력 숫자들을 지정된 구분자로 나누는 dto

--- src/main/java/model/dto/AnswerLottoNumbers.java
+++ src/main/java/model/dto/AnswerLottoNumbers.java
@@ -2,17 +2,13 @@ package model.dto;
 
 import java.util.Arrays;
 import java.util.List;
-import model.Lotto;
 
 public record AnswerLottoNumbers(String answerLottoNumbers) {
 
     private static final String NUMBER_SEPARATOR = ",";
 
-    public Lotto toLotto() {
-        List<Integer> numbers = Arrays.stream(answerLottoNumbers.split(NUMBER_SEPARATOR))
-            .map(Integer::parseInt)
+    public List<String> divideNumbers() {
+        return Arrays.stream(answerLottoNumbers.split(NUMBER_SEPARATOR))
             .toList();
-
-        return new Lotto(numbers);
     }
 }

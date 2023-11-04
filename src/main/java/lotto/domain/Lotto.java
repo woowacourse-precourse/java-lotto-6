923a359cdd5d265ab903ef77a87e004dddf020a5
zzun73 <736300@naver.com>
2023-11-05 00:13:48 +0900

    feat: 로또 중복 검증 로직 추가 및 패키지 위치 domain으로 변경

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateDuplicateNumber(numbers);
+    }
+
+    private static void validateDuplicateNumber(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(new HashSet<>()::add)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean contain(int bonusNumber) {
+        return this.numbers.contains(bonusNumber);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+}

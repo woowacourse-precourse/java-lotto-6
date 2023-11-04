450c5dcf784dea8ab139f070d2bb4f9768285d71
pwrwpw <dlgustjr9912@gmail.com>
2023-11-05 02:08:16 +0900

    feat: ValidationTest 코드를 토대로 검증 메서드 작성

--- src/main/java/lotto/utils/Validation.java
+++ src/main/java/lotto/utils/Validation.java
@@ -5,7 +5,7 @@ import lotto.view.ExceptionMessages;
 public class Validation {
 
     public static void validateStringToInteger(String string) {
-        if (!string.chars().allMatch(Character::isDigit)) {
+        if (string.trim().isEmpty() || !string.chars().allMatch(Character::isDigit)) {
             ExceptionMessages.STRING_TO_INTEGER.throwException();
         }
     }

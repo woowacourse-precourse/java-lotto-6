a7f329fc20457f31f3da84b435c153f0f873cdfb
mainfn <_@mainfn.dev>
2023-11-04 21:27:13 +0900

    docs: `ConsoleReader`, `ConsoleWriter` 간단 설명

--- src/main/java/io/input/ConsoleReader.java
+++ src/main/java/io/input/ConsoleReader.java
@@ -2,6 +2,9 @@ package io.input;
 
 import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * FD 0번 STDIN으로부터 바이트 열을 읽어옵니다.
+ */
 public final class ConsoleReader implements Reader {
     @Override
     public String readLine() {
--- src/main/java/io/output/ConsoleWriter.java
+++ src/main/java/io/output/ConsoleWriter.java
@@ -1,5 +1,8 @@
-package io.input;
+package io.output;
 
+/**
+ * FD 1번 STDOUT으로 문자열을 출력합니다.
+ */
 public final class ConsoleWriter implements Writer {
     @Override
     public void write(final String message) {

bc7722ee4de226856f6081007e2e54285ee93cb4
김연진 <love_homebody@naver.com>
2023-11-04 17:36:07 +0900

    feat(OutputView): 발행한 로또 수량 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,18 @@
 package lotto.view;
 
 import lotto.constant.OutputMessage;
+import lotto.domain.LottoCounter;
 
 public class OutputView {
 
     public void printPurchaseMessage() {
         System.out.println(OutputMessage.PURCHASE_AMOUNT.getMessage());
+    }
+
+    public void printTicketCount(LottoCounter lottoCounter) {
+        String message = OutputMessage.LOTTO_TICKET_COUNT.getMessage();
+        System.out.printf(message, lottoCounter.getTicketCount());
+        System.out.println();
     }
 
     public void printNewLine() {

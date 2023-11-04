c9650490e4040a7d9dcb3f9d3ddc0d0e425fb55e
lee jihyeon <lejinhy@gmail.com>
2023-11-04 15:32:44 +0900

    feat(Controller): 구입금액만큼 로또 발행하고 출력까지 완료

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,13 +1,21 @@
 package lotto;
 
+import lotto.domain.GenerateLotteries;
 import lotto.domain.PurchasePrice;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoGameController {
     private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private GenerateLotteries generateLotteries;
     private PurchasePrice purchasePrice;
 
     public void startGame() {
         purchasePrice = new PurchasePrice(inputView.requestPurchasePrice());
+
+        generateLotteries = new GenerateLotteries(purchasePrice.getPurchaseCount());
+
+        outputView.printPurchasedLotteries(generateLotteries.getLotteries());
     }
 }

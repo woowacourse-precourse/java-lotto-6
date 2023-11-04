b32978895fd3e029a222542690676d09766c68b1
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-05 01:03:41 +0900

    feat - 로또 번호 비교 로직 구현

---
+++ src/main/java/lotto/service/LottoCalculator.java
@@ -0,0 +1,36 @@
+package lotto.service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumbers;
+import lotto.domain.Ranking;
+import lotto.domain.WinningLotto;
+import lotto.dto.ComparatorRequest;
+
+
+public class LottoCalculator {
+
+    private static final int HUNDRED_PERCENT = 100;
+    private static final double HUNDRED_PERCENT_DOUBLE = 100.0;
+
+    public void compareLotto(ComparatorRequest comparatorRequest) {
+        WinningLotto winningLotto = comparatorRequest.winningLotto();
+        List<Lotto> playerLotto = comparatorRequest.playerNumbers();
+
+        for (Lotto number : playerLotto) {
+            int rank = calculateRanking(number, winningLotto);
+            if (winningLotto.hasBonusNumber(rank, number)) {
+                Ranking.changeCountWhenHasBonusNumber();
+            }
+            Ranking.getWinner(rank);
+        }
+    }
+
+    private int calculateRanking(Lotto number, WinningLotto winningLotto) {
+        Set<Integer> winningNumber = new HashSet<>(winningLotto.numbers().getNumbers());
+        winningNumber.addAll(number.getNumbers());
+        return LottoNumbers.LOTTO_SIZE.getNumber() - winningNumber.size();
+    }
+}

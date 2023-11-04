d1fa048661e0cc1d44716d764424ba07d8f46789
LeeJiWon <dlwldnjs1009@gmail.com>
2023-11-04 18:04:54 +0900

    feat: 로또 랭크 클래스 추가

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(4, 1_500_000),
+    FOURTH(3, 50_000),
+    FIFTH(2, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prizeMoney;
+
+    LottoRank(int matchCount, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}

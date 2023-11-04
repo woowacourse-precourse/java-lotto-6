8a887fcdd425a30d6caab57bff00d8015e9ca23a
yooooonshine <hl3470@naver.com>
2023-11-04 20:00:45 +0900

    Test: 보너스 번호 입력 테스트 구현
    문자_입력_예외_테스트, 로또_번호_범위_테스트, 로또_번호_범위_테스트를 구현하였다.

--- src/test/java/lotto/myApplicationTest.java
+++ src/test/java/lotto/myApplicationTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.User;
 import lotto.domain.WinningNumber;
@@ -226,6 +227,41 @@ public class myApplicationTest {
             // when,then
             Assertions.assertThatCode(() -> WinningNumber.create(intList))
                     .doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    @DisplayName("보너스 번호 입력 테스트")
+    public class 보너스_번호_입력_테스트 {
+        @ParameterizedTest
+        @DisplayName("문자를 입력했을 때 예외를 던지는 지 테스트")
+        @ValueSource(strings = {"a",","," "})
+        void 문자_입력_예외_테스트(String input) {
+            Assertions.assertThatThrownBy(
+                    () -> LottoNumberValidator.validateBonusNumber(input)
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @DisplayName("로또 번호 범위가 주어진 범위를 넘었을 때 예외를 던지는 지 테스트")
+        @ValueSource(ints = {0,55})
+        void 로또_번호_범위_테스트(int input) {
+            Assertions.assertThatThrownBy(
+                    () -> BonusNumber.create(input)
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("당첨 번호와 중복되면 예외를 던지는 지 테스트")
+        void 로또_번호_범위_테스트() {
+            // given
+            List<Integer> numbers= List.of(1,2,3,4,5,6);
+            WinningNumber winningNumber = WinningNumber.create(numbers);
+            int bonusNumber = 6;
+            // when, then
+            Assertions.assertThatThrownBy(
+                    () -> BonusNumber.create(bonusNumber)
+            ).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }

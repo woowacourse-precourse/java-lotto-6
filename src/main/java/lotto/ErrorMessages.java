8f6e02168831ca95914182901e406f7234493326
unknown <winternrabbit@gmail.com>
2023-11-05 03:12:36 +0900

    refactor: 상수명 수정

--- src/main/java/lotto/ErrorMessages.java
+++ src/main/java/lotto/ErrorMessages.java
@@ -2,8 +2,9 @@ package lotto;
 
 public enum ErrorMessages {
     ERROR_NOT_NUMBER("[ERROR] 양의 정수를 입력해 주세요"),
-    ERROR_NOT_MULTIPLE_OF_THOUSAND("[ERROR] " + LottoCost.UNIT + "~" + Integer.MAX_VALUE + " 사이의 " + LottoCost.UNIT
-            + "의 배수를 입력해 주세요.");
+    ERROR_NOT_MULTIPLE_OF_UNIT("[ERROR] " + LottoCost.UNIT + "~" + Integer.MAX_VALUE + " 사이의 " + LottoCost.UNIT
+            + "의 배수를 입력해 주세요."),
+    ERROR_LENGTH_NOT_SIX("[ERROR] 6개의 숫자를 입력해 주세요.");
 
     private final String message;
 

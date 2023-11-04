697c5fb3f2d3327b4af8b48e5c3adf8c3abdb055
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-04 17:18:23 +0900

    feat(parser) : 사용자 입력 데이터 유형 변경 parser 생성

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,4 +1,12 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Parser {
+    public static List<String> parseString(String numbers) {
+        return Arrays.stream(numbers.split(",", -1))
+                .map(String::trim)
+                .toList();
+    }
 }

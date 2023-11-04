6318e4960f86d572a7ca1bc9d32536c75cdb28a9
leesangjun <hubls@naver.com>
2023-11-04 16:00:38 +0900

    feat: 로또 발행 하는 기능

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,12 +1,19 @@
 package lotto.model;
 
-import static lotto.util.LottoInformation.LOTTO_PRICE;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import static lotto.util.LottoInformation.*;
 
 public class LottoMachine {
     private final int numberOfTickets;
+    private final Lottos lottos;
 
     public LottoMachine(int payment) {
         this.numberOfTickets = calculateNumberOfTickets(payment);
+        this.lottos = createLottos(numberOfTickets);
     }
 
     private int calculateNumberOfTickets(int payment) {
@@ -15,5 +22,21 @@ public class LottoMachine {
 
     public int getNumberOfTickets() {
         return numberOfTickets;
+    }
+
+    private Lotto createLotto() {
+        List<Integer> lotto = new ArrayList<>();
+        while (lotto.size() < NUMBER_OF_NUMBERS) {
+            int number = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (!lotto.contains(number)) {
+                lotto.add(number);
+            }
+        }
+        Collections.sort(lotto);
+        return new Lotto(lotto);
+    }
+
+    public Lottos getLottos() {
+        return lottos;
     }
 }

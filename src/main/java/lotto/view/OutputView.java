b9925a4d88028c3f91d0e37257ca6bd53d5a0726
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 20:11:33 +0900

    style(OutputView): 메서드 이름, 접근지시자 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,7 +4,7 @@ public class OutputView {
 
     private final static String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
 
-    private void purchaseQuantity(int quantity) {
+    public void printPurchaseQuantity(int quantity) {
         System.out.println(String.format(PURCHASE_QUANTITY, quantity));
     }
 

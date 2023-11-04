933e900c9605bd15889ff6a89fd90ce53597820c
jeli01 <jeli01@naver.com>
2023-11-05 02:30:22 +0900

    feat: validate division by lotto price

--- src/main/java/lotto/Store.java
+++ src/main/java/lotto/Store.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Store {
     public List<Lotto> sellLottos(Integer money) {
         List<Lotto> lottos = new ArrayList<Lotto>();
+        validateDividedByPrice(money);
 
         for (int i = 0; i < money / 1000; i++) {
             List<Integer> lottoNumbers = pickUniqueNumbersInRange(1, 45, 6);
@@ -16,5 +17,11 @@ public class Store {
         }
 
         return lottos;
+    }
+
+    private void validateDividedByPrice(Integer money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000으로 가격이 나누어 떨어져야 합니다.");
+        }
     }
 }

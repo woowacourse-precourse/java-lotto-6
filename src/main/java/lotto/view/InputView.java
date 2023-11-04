53e23eb3e08eaafaac59b2c32b76c5a4f5f10ffd
Brian <bwook9908@gmail.com>
2023-11-04 14:27:50 +0900

    feat: 입력 구현(예외처리 제외)
    - 로또 구입 금액을 입력받는다.
    - 당첨번호를 입력받는다.
    - 보너스 번호를 입력받는다.

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Converter;
+
+import java.util.List;
+
+public class InputView extends Message{
+    public int buyPriceInput() {
+        System.out.println(INPUT_PRICE_MESSAGE);
+        String input = Console.readLine();
+        // validate code required
+        return Integer.parseInt(input);
+    }
+
+    public List<Integer> winningNumberInput() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        String input = Console.readLine();
+        // validate code required
+        return Converter.stringToIntList(input);
+    }
+
+    public int bonusNumberInput() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        String input = Console.readLine();
+        // validate code required
+        return Integer.parseInt(input);
+    }
+}

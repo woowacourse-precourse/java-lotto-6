e621c77a48202307cce57ccef693870d0e354ebb
MyunghyunNero <mh1kim0910@gmail.com>
2023-11-04 16:54:01 +0900

    feat : lottoController 로직 흐름 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+public class LottoController {
+
+    public void lottoStart(){
+        // 유저가 당첨 금액 입력
+
+        // 로또 어드민이 로또 발행해주고 유저에게 전달
+
+        // 로또 어드민이 당첨 번호 + 보너스 번호 입력
+
+        // 로또 계산기가 당첨 통계 출력
+
+        // 로또 계산기가 수익률 출력
+    }
+}

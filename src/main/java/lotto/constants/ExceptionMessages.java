f377ad292fa614281f8c77fba0df1d9f45413d16
chae401 <chae401@sju.ac.kr>
2023-11-04 18:03:48 +0900

    feat(ExceptionMessages): add exception msg about payment

--- src/main/java/lotto/constants/ExceptionMessages.java
+++ src/main/java/lotto/constants/ExceptionMessages.java
@@ -1,2 +1,9 @@
-package lotto.constants;public class ExceptionMessages {
+package lotto.constants;
+
+public class ExceptionMessages {
+    public static final String CATCH_ERROR = "[ERROR] ";
+    public static final String INVALID_NUMBER = "숫자를 입력해주세요.";
+    public static final String INVALID_PAYMENT = "금액은 1000원 단위로 입력해주세요.";
+    public static final String INPUT_EMPTY = "입력이 되지 않았습니다.";
+
 }

512288d08bac2e4a8ffe83aff262eb624ff18cc1
gonixxxx <akdntm946@gmail.com>
2023-11-05 00:22:52 +0900

    feat: add exception to validate lotto's size is 6

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -1,30 +1,41 @@
 package lotto.utils;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class InputValidator {
+
     private static final String ERROR = "[ERROR] ";
-    private static final String NOT_NUMBER = "숫자를 입력해주세요";
+    private static final String NOT_NUMBER = "숫자를 입력해주세요.";
     private static final String BLANK = "입력이 공백입니다.";
-    private static final int PRICE = 1000;
     private static final String INVALID_PRICE = "1000원 단위의 수를 입력해주세요.";
+    private static final String INVALID_SIZE = "6개의 로또 번호를 입력해주세요.";
+
+    private static final int PRICE = 1000;
+    private static final int SIZE = 6;
     private static final Pattern NOT_NUMBER_PATTERN = Pattern.compile("^[1-9]*$");
 
     public static void checkIsNumber(String input) {
         if (!NOT_NUMBER_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException(ERROR+NOT_NUMBER);
+            throw new IllegalArgumentException(ERROR + NOT_NUMBER);
         }
     }
 
     public static void checkIsBlank(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException(ERROR+BLANK);
+            throw new IllegalArgumentException(ERROR + BLANK);
         }
     }
 
     public static void checkPrice(String input) {
         if (Integer.parseInt(input) % PRICE != 0) {
-            throw new IllegalArgumentException(ERROR+INVALID_PRICE);
+            throw new IllegalArgumentException(ERROR + INVALID_PRICE);
+        }
+    }
+
+    public static void checkLottoSizeIsSix(List<Integer> input) {
+        if (input.size() != SIZE) {
+            throw new IllegalArgumentException(ERROR + INVALID_SIZE);
         }
     }
 

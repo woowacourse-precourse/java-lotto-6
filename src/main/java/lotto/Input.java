f52ae01eec205324cb1a242446b23b82a46bfbb6
KIVSH <kihongkim3@hotmail.com>
2023-11-04 12:43:42 +0900

    feat: compose Input class

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static int amount() {
+        return Integer.parseInt(Console.readLine());
+    }
+}

b3f4842c680e59c78f13a02930fd55f6cbd63b0e
siho-song <thdtlgh234@naver.com>
2023-11-04 22:34:03 +0900

    [Refactor] 함수 이름 변경 및 변수 상수처리

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -5,8 +5,7 @@ import lotto.utils.Utils;
 
 public class LottoNumbers {
     private static final String DELIMITER = ",";
-    private static final String PATTERN_ONLY_INTEGERS = "\\d+";
-    private final int TOTAL_NUMBER_SIZE = 6;
+    private static final int TOTAL_NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public LottoNumbers(String numbers) {
@@ -18,9 +17,8 @@ public class LottoNumbers {
     public List<Integer> getLottoNumbers() {
         return this.numbers;
     }
-    
+
     private List<Integer> convertStringsToIntegers(List<String> numbers) {
-        validateHasOnlyIntegers(numbers);
         return numbers.stream()
                 .map(Utils::convertStringToInteger)
                 .toList();
@@ -32,19 +30,8 @@ public class LottoNumbers {
 
     private void validateLottoNumber(List<Integer> numbers) {
         validateDuplicated(numbers);
-        validateNumbersRange(numbers);
+        validateRange(numbers);
         validateTotalNumbers(numbers);
-    }
-
-    private void validateHasOnlyIntegers(List<String> numbers) {
-        boolean hasOnlyIntegers = numbers.stream()
-                .allMatch(string -> string.matches(PATTERN_ONLY_INTEGERS));
-
-        if (!hasOnlyIntegers) {
-            throw new IllegalArgumentException(
-                    ErrorMessage.INVALID_LOTTO_NUMBERS.getValue()
-            );
-        }
     }
 
     private void validateDuplicated(List<Integer> numbers) {
@@ -55,7 +42,7 @@ public class LottoNumbers {
         }
     }
 
-    private void validateNumbersRange(List<Integer> numbers) {
+    private void validateRange(List<Integer> numbers) {
         boolean isValidNumberRange = numbers.stream()
                 .allMatch(number -> number >= LottoNumberRange.MIN.getValue() &&
                         number <= LottoNumberRange.MAX.getValue());
@@ -69,7 +56,9 @@ public class LottoNumbers {
 
     private void validateTotalNumbers(List<Integer> numbers) {
         if (numbers.size() != TOTAL_NUMBER_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(
+                    ErrorMessage.TOTAL_NUMBER.getValue()
+            );
         }
     }
 

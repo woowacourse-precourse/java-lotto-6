4f9fdcbd9a8cee20d0240327e1827a80cd7e1ed9
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 20:51:19 +0900

    [feat] 수익률 출력 기능 구현

--- src/main/java/lotto/util/io/OutputUtils.java
+++ src/main/java/lotto/util/io/OutputUtils.java
@@ -2,7 +2,6 @@ package lotto.util.io;
 
 import java.util.List;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
 
 public class OutputUtils {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -12,6 +11,7 @@ public class OutputUtils {
     private static final String RESULTS_ANNOUNCEMENT_MESSAGE = "당첨 통계";
     private static final String SEPARATING_LINE = "---";
     private static final String WINNING_DETAIL = "%s (%s원) - %d개";
+    private static final String WINNING_RATE_OF_RETURN_MESSAGE = "총 수익률은 %f%입니다.";
 
     public static void printPurchaseAmountInputMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
@@ -40,5 +40,9 @@ public class OutputUtils {
 
     public static void printWinningDetail(String description, String prize, int number) {
         System.out.println(String.format(WINNING_DETAIL, description, prize, number));
+    }
+
+    public static void printWinningRateOfReturnMessage(Float rate) {
+        System.out.println(String.format(WINNING_RATE_OF_RETURN_MESSAGE, rate));
     }
 }

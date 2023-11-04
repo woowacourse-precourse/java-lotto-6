6e5b113bd3eb721ddb79b15a135533daa0f66f12
aka-nick <bechalrang@gmail.com>
2023-11-04 22:35:34 +0900

    추가(LottoGame) : 구입 금액을 숫자로 입력받는 기능
    구입 금액을 숫자로 입력받는 기능 구현함
    기존 구현 목록의 기능 설명이 빈약하여 부가설명 추가
    
    LottoGame
    README

--- src/main/java/lotto/io/lotto/LottoGame.java
+++ src/main/java/lotto/io/lotto/LottoGame.java
@@ -1,11 +1,13 @@
 package lotto.io.lotto;
 
+import lotto.io.Input;
 import lotto.io.Output;
 
 public class LottoGame {
 
     public void run() {
         Output.consoleLine("구입금액을 입력해 주세요.");
+        int purchaseAmount = Input.consoleNumber();
     }
 
 }

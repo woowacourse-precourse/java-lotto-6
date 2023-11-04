956c7788b69a38920ad228e420070650ebaa143f
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-04 15:31:44 +0900

    docs: TODO list 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,20 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 로또 구입 금액 입력 받기
+
+        // TODO: 로또 번호 출력
+
+        // TODO: 당첨 번호 입력 받기
+
+        // TODO: 보너스 번호 입력 받기
+
+        // TODO: 로또 번호와 당첨 번호 비교
+
+        // TODO: 당첨 통계 출력
+
+        // TODO: 수익률 계산
+
+        // TODO: 수익률 출력
     }
 }

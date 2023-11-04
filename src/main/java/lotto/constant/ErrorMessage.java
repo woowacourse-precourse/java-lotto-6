a641e71717c6f3ce1f8296209fff181db9ca759e
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-05 00:04:24 +0900

    style: 상수화 클래스 정리

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -2,9 +2,9 @@ package lotto.constant;
 
 public class ErrorMessage {
     public static final String NOT_NUMBER_MESSAGE = "[ERROR] 입력은 숫자만 가능합니다!";
-    public static final String OVER_RANGE_MESSAGE = "[ERROR] 입력가능한 숫자의 범위를 넘었습니다!";
-    public static final String NOT_DIVISIBLE_MESSAGE = "[ERROR] 구매금액이 1000원 단위로 입력되어야합니다!";
+    public static final String OVER_NUMBER_RANGE_MESSAGE = "[ERROR] 입력가능한 숫자의 범위를 넘었습니다!";
+    public static final String PRICE_NOT_DIVISIBLE_MESSAGE = "[ERROR] 구매금액이 1000원 단위로 입력되어야합니다!";
     public static final String DUPLICATED_NUMBER_MESSAGE = "[ERROR] 로또 번호가 중복되었습니다!";
-    public static final String DIFFERENT_COUNT_MESSAGE = "[ERROR] 로또 번호의 개수가 맞지않습니다!";
-
+    public static final String NOT_MATCH_COUNT_MESSAGE = "[ERROR] 로또 번호의 개수가 맞지않습니다!";
+    public static final String OVER_VALUE_RANGE_MESSAGE = "[ERROR] 로또 번호의 값이 주어진 범위를 벗어났습니다!";
 }
---
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class OutputMessage {
+    public static final String REQUEST_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_COUNT_DEFAULT_MESSAGE = "개를 구매했습니다.";
+    public static final String REQUEST_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+}

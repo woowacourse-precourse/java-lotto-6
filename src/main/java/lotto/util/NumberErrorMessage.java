6c7f0a9380d4c96f7d8a5db9015ab3d5fe3efeca
leesangjun <hubls@naver.com>
2023-11-04 18:14:20 +0900

    feat: 6개 숫자인지 확인하는 기능

--- src/main/java/lotto/util/NumberErrorMessage.java
+++ src/main/java/lotto/util/NumberErrorMessage.java
@@ -6,7 +6,8 @@ public enum NumberErrorMessage {
     OUT_OF_INT_RANGE("숫자의 범위를 초과했습니다."),
     INVALID_ZERO("0은 유효한 숫자가 아닙니다."),
     NEGATIVE_NUMBER("음수는 허용되지 않습니다."),
-    NOT_DIVISIBLE_BY_THOUSANDS("1000으로 나눠지지 않습니다.");
+    NOT_DIVISIBLE_BY_THOUSANDS("1000으로 나눠지지 않습니다."),
+    NUMBER_IS_NOT_SIX("6자리 숫자가 아닙니다.");
 
     private final String message;
 
--- src/main/java/lotto/util/WinningNumbersValidator.java
+++ src/main/java/lotto/util/WinningNumbersValidator.java
@@ -2,10 +2,20 @@ package lotto.util;
 
 import java.util.List;
 
+import static lotto.util.LottoInformation.NUMBER_OF_NUMBERS
+
 public class WinningNumbersValidator {
     public static void validateWinningNumbers(List<String> numbers) {
         if (numbers.isEmpty()) {
             throw new IllegalArgumentException(NumberErrorMessage.NUMBER_IN_EMPTY.getMessage());
         }
+
+        if (!isSixNumbers(numbers)) {
+            throw new IllegalArgumentException(NumberErrorMessage.NUMBER_IS_NOT_SIX.getMessage());
+        }
+    }
+
+    private static boolean isSixNumbers(List<String> numbers) {
+        return numbers.size() == NUMBER_OF_NUMBERS;
     }
 }

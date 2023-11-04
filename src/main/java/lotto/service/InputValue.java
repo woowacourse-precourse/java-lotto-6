cb19b364b4c2be7370ea53ab557bfb8431831bab
Chenowoo <cjsgusdn14@naver.com>
2023-11-04 14:29:11 +0900

    refactor(InputValue): getUserInput 메서드 명 변경 및 입력값을 받을 때 아무 입력이 없는 경우 예외 발생하는 로직 추가  - getUserInput 메서드 명을 getPurchaseAmountInput로 변경  - 입력값이 아무것도 입력되지 않았을 때 NullPointerException예외 처리

--- src/main/java/lotto/service/InputValue.java
+++ src/main/java/lotto/service/InputValue.java
@@ -2,10 +2,21 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.Lotto;
+import lotto.validation.InputValidation;
 
 public class InputValue {
+    PrintUtil printUtil = new PrintUtil();
+    InputValidation inputValidation = new InputValidation();
 
-    public String getUserInput() {
-        return Console.readLine();
+    public String getPurchaseAmountInput() {
+        try {
+            String userInput = Console.readLine();
+            inputValidation.validateInputIsNull(userInput);
+            return userInput;
+        } catch (NullPointerException nullPointerException) {
+            System.out.println(nullPointerException.getMessage());
+            printUtil.printPurchaseAmountInput();
+            return getPurchaseAmountInput();
+        }
     }
 }

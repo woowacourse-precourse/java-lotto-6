ec5c0709dc581c2c1818397f33744f7228017979
DaeyeonCho <daecho8@gmail.com>
2023-11-04 15:54:06 +0900

    feat: 구매한 로또 티켓 구현
    1-45 사이의 서로 다른 6개의 숫자로 이루어진 로또 티켓을 리스트에 보관하도록 작성했습니다.

--- src/main/java/lotto/game/Game.java
+++ src/main/java/lotto/game/Game.java
@@ -2,8 +2,15 @@ package lotto.game;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 import lotto.domain.Ticket;
 import lotto.type.ErrorCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.stream;
 
 
 public class Game {
@@ -26,6 +33,22 @@ public class Game {
         Ticket ticket = new Ticket();
         ticket.setCount(moneyInput);
         return ticket.getCount();
+    }
+
+    private void showTickets(int totalTicketCount) {
+        System.out.println(totalTicketCount + "개를 구매했습니다.");
+        List<Lotto> tickets = getTickets(totalTicketCount);
+        for (Lotto lotto : tickets) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+    List<Lotto> getTickets(int totalTicketCount) {
+        List<Lotto> tickets = new ArrayList<>();
+        for (int i = 0; i < totalTicketCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            tickets.add(new Lotto(numbers));
+        }
+        return tickets;
     }
 
 }

4c64fe02d46d02cc2afdd617eec4aac4e926bc28
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-04 17:15:05 +0900

    feat(lotto) : 유효성 검사 완료, Validator로 확장

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,36 @@
 package lotto.model;
 
+import static lotto.util.validators.LottoValidator.validateLotto;
+
+import java.util.Collections;
 import java.util.List;
+import lotto.util.wrapper.BonusNumber;
+import lotto.util.wrapper.WinningNumbers;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        try {
+            validateLotto(numbers);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void resultCompare(final WinningNumbers winningNumbers, final BonusNumber bonusNumber) {
+
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
+
--- src/main/java/lotto/util/validators/LottoValidator.java
+++ src/main/java/lotto/util/validators/LottoValidator.java
@@ -1,2 +1,27 @@
-package lotto.util.validators;public class LottoValidator {
+package lotto.util.validators;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.util.exception.DuplicationNumberException;
+import lotto.util.exception.LengthGreaterThanSixException;
+
+public class LottoValidator {
+    public static void validateLotto(List<Integer> numbers) {
+        lengthGreaterThanSix(numbers);
+        NumberDuplicates(numbers);
+    }
+
+    private static void lengthGreaterThanSix(List<Integer> numbers) {
+        if (numbers.size() > 6) {
+            throw LengthGreaterThanSixException.LENGTH_GREATER_THAN_SIX.getException();
+        }
+    }
+
+    private static void NumberDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (numbers.size() != uniqueNumbers.size()) {
+            throw DuplicationNumberException.DUPLICATE_NUMBERS.getException();
+        }
+    }
 }

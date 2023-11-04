691c72e23d6c6a97613e512434bf7d0d1fe1be6f
psy3720 <qkrtmddyd12@naver.com>
2023-11-04 10:15:07 +0900

    feat: 랜덤숫자 생성 기능 추가

---
+++ src/main/java/lotto/NumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class NumberGenerator {
+    private NumberGenerator() {
+    }
+
+    public static List<Integer> createInRangeNumber(int startInclusive, int endInclusive, int count) {
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+    }
+}
---
+++ src/test/java/lotto/NumberGeneratorTest.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumberGeneratorTest {
+
+    @DisplayName("범위를 지정하고 랜덤숫자를 생성하면 범위 내의 숫자가 생성된다")
+    @Test
+    void generateNumber() {
+        int startInclusive = 1;
+        int endInclusive = 45;
+
+        List<Integer> inRangeNumber = NumberGenerator.createInRangeNumber(startInclusive, endInclusive, 6);
+
+        assertThat(inRangeNumber)
+                .allSatisfy(num -> assertThat(num).isBetween(startInclusive, endInclusive));
+    }
+
+    @DisplayName("랜덤숫자를 생성하면 중복되지 않은 숫자가 생성된다")
+    @Test
+    void distinctNumber() {
+        int startInclusive = 1;
+        int endInclusive = 45;
+
+        int count = 6;
+        List<Integer> inRangeNumber = NumberGenerator.createInRangeNumber(startInclusive, endInclusive, count);
+
+        assertThat(count)
+                .isEqualTo(inRangeNumber.stream().distinct().count());
+    }
+}

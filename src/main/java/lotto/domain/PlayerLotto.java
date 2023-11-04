f97f77c79e6e1e05f538f648bc5856714a74afb3
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-04 17:38:07 +0900

    refactor - Player 로또로 네이밍 변경

--- src/main/java/lotto/domain/PlayerLotto.java
+++ src/main/java/lotto/domain/PlayerLotto.java
@@ -1,15 +1,21 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Collections;
 import java.util.List;
 
-public class LottoGenerator {
+public class PlayerLotto {
+
+    private final List<Lotto> playerNumbers;
+
+    public PlayerLotto(List<Lotto> playerNumbers) {
+        this.playerNumbers = playerNumbers;
+    }
 
     public List<Integer> generateSixNumbers() {
         List<Integer> sixNumbers = Randoms.pickUniqueNumbersInRange(LottoNumbers.MIN_LOTTO_NUMBER.getNumber(),
-                LottoNumbers.MAX_LOTTO_NUMBER.getNumber(), 6);
+                LottoNumbers.MAX_LOTTO_NUMBER.getNumber(), LottoNumbers.LOTTO_SIZE.getNumber());
         Collections.sort(sixNumbers);
         return sixNumbers;
     }
-}

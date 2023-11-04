e22f8f11cd7c6535b3d3158dc5283b3a31f15095
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 19:16:09 +0900

    [feat] , 기준으로 분리 및 공백 제거 기능 구현

---
+++ src/main/java/lotto/util/StringUtils.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringUtils {
+
+    public static List<String> splitByCommaAndTrim(String input) {
+        String[] numbers = input.split(",");
+        List<String> splitNumbers = Arrays.stream(numbers).map(String::trim).collect(toList());
+        return splitNumbers;
+    }
+}

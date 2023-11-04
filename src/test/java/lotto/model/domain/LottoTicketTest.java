df7aacda40d30fb0c5ff6469fee9de130abce290
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-04 14:45:37 +0900

    test: 사용하지 않은 import문 제거

--- src/test/java/lotto/model/domain/LottoTicketTest.java
+++ src/test/java/lotto/model/domain/LottoTicketTest.java
@@ -1,11 +1,8 @@
 package lotto.model.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.AbstractSet;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

0feed35ccdc0cdc9560bf5b3846c09ac886711a0
seminss <2007ksm@naver.com>
2023-11-04 16:56:09 +0900

    test: 발행된 번호는 오름차순으로 정렬하여 저장 한다.

--- src/test/java/lotto/service/LottoTicketServiceTest.java
+++ src/test/java/lotto/service/LottoTicketServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static lotto.constant.LottoConfig.*;
 import static org.assertj.core.api.Assertions.*;
@@ -41,20 +42,30 @@ class LottoTicketServiceTest {
     @RepeatedTest(1000)
     @DisplayName("각 로또 번호는 1부터 45 사이의 숫자 중 중복되지 않는 6개로 구성 한다.")
     void generateNoDuplicationLottoNumbers() {
-        Lotto lotto = lottoTicketService.generateSingleLottoNumbers();
-        List<Integer> lottoNumber = lotto.getNumbers();
+        Lotto lotto = lottoTicketService.generateLottoNumbers();
+        List<Integer> lottoNumbers = lotto.getNumbers();
 
         // 로또 번호는 정확히 6개여야 한다.
-        assertEquals(count, lottoNumber.size());
+        assertEquals(count, lottoNumbers.size());
 
         // 모든 로또 번호는 1과 45 사이의 값이어야 한다.
-        assertTrue(lottoNumber.stream()
+        assertTrue(lottoNumbers.stream()
                 .allMatch(n -> n >= startInclusive && n <= endInclusive));
 
         // 로또 번호에 중복된 값이 없어야 한다.
-        assertTrue(lottoNumber.stream()
-                .filter(n -> Collections.frequency(lottoNumber, n) > 1)
+        assertTrue(lottoNumbers.stream()
+                .filter(n -> Collections.frequency(lottoNumbers, n) > 1)
                 .collect(Collectors.toSet())
                 .isEmpty());
+    }
+
+    @Test
+    @DisplayName("발행된 번호는 오름차순으로 정렬하여 저장한다.")
+    void generateAscendingLottoNumbers(){
+        Lotto lotto = lottoTicketService.generateLottoNumbers();
+        List<Integer> lottoNumbers = lotto.getNumbers();
+
+        assertTrue(IntStream.range(0,lottoNumbers.size()-1)
+                .allMatch(i->lottoNumbers.get(i)<lottoNumbers.get(i+1)));
     }
 }

866e5c9117f1de8f743c236ccb958e51357a7bd6
jeondui <jeondui@gmail.com>
2023-11-05 00:45:34 +0900

    test(LottoTest): 당첨 번호의 유효성 검증 테스트 기능

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -36,5 +36,29 @@ class LottoTest {
                 .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_DUPLICATE.get());
     }
 
+    @DisplayName("로또 번호 입력값 없을 때 예외 발생 테스트")
+    @Test
+    void isEmpty() {
+        assertThatThrownBy(() -> Lotto.create(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_NULL.get());
+    }
+
+    @DisplayName("로또 번호 공백 포함 시 예외 발생 테스트")
+    @Test
+    void isContainsBlank() {
+        assertThatThrownBy(() -> Lotto.create("1, 2, 3, 4, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_CONTAINS_BLANK.get());
+    }
+
+    @DisplayName("로또 번호의 입력값이 쉼표(,)로 끝나는 경우 예외 발생 테스트")
+    @Test
+    void isEndsWithComma() {
+        assertThatThrownBy(() -> Lotto.create("1,2,3,4,5,6,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_ENDS_WITH_COMMA.get());
+    }
+
 
 }

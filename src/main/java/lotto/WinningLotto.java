18585b5eb2eab5e2308258743bc99b0a37b2c3f8
MinseokGo <rhalstjr1999@naver.com>
2023-11-04 23:22:24 +0900

    feat($WinningLotto): 당첨 로또를 저장하는 기능
    당첨 번호와 보너스 번호를 저장합니다.

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public WinningLotto(final List<Integer> numbers, final int bonusNumber) {
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+}

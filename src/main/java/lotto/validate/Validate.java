1933dcd91976b663c163b062fed89ffb1a5b575e
leechiyun <wychis28@gmail.com>
2023-11-04 12:39:35 +0900

    feat: 해당 범위에 있는지 검증해주는 기능 구현

--- src/main/java/lotto/validate/Validate.java
+++ src/main/java/lotto/validate/Validate.java
@@ -1,6 +1,7 @@
 package lotto.validate;
 
 public class Validate {
+
     public static void validateInteger(String str) {
         try {
             Integer.parseInt(str);
@@ -13,6 +14,13 @@ public class Validate {
     public static void validateDivideNumber(int number, int divide) {
         if (number % divide != 0) {
             throw new IllegalArgumentException(divide + " 값으로 나누어 떨어지지 않습니다.");
+        }
+    }
+
+    // 입력 값이 해당 범위에 있는지 검증
+    public static void validateNumberInRange(int number, int start, int end) {
+        if (number < start || number > end) {
+            throw new IllegalArgumentException("입력 값이 해당 범위에 없습니다.");
         }
     }
 }

b943ad05f0d8566fc9914ecea03eee14043bd6e0
MingyeomKim <mingyum119@naver.com>
2023-11-04 20:46:47 +0900

    feat(LottoController): 입력한 금액을 통해 발행할 로또 수량을 반환
    getCountFrom(int cost) 함수를 통해 입력한 금액을 발행할 로또의 개수 (count)로 변환한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.model.Lottos;
 import lotto.view.View;
 
 public class LottoController {
@@ -9,5 +10,10 @@ public class LottoController {
 
     private void issue() {
         int cost = Integer.parseInt(View.requestCost());
+        Lottos lottos = Lottos.from(getCountFrom(cost));
+    }
+
+    private int getCountFrom(int cost) {
+        return cost / 1000;
     }
 }

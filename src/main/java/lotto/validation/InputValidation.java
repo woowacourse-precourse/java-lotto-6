206e3e6b874d75112b7375291f0075455015ed1b
ohjuyoungg <ejrm12@naver.com>
2023-11-04 22:14:56 +0900

    feat: 사용자의 구매 금액 입력에 대한 예외 처리 구현

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -1,2 +1,28 @@
-package lotto.validation;public class InputValidation {
+package lotto.validation;
+
+import lotto.utils.constants.Comment;
+import lotto.utils.constants.ExceptionMessage;
+
+public class InputValidation {
+
+    public static String validatePurchaseAmountIsBlank(String purchaseAmount) {
+        if (purchaseAmount == null || purchaseAmount.isBlank()) {
+            throw new IllegalArgumentException(String.valueOf(ExceptionMessage.PURCHASE_AMOUNT_BLANK.getMessage()));
+        }
+        return purchaseAmount;
+    }
+
+    public static int validateInputPurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < 1000 || purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(String.valueOf(ExceptionMessage.PURCHASE_AMOUNT_NOT_THOUSAND.getMessage()));
+        }
+        return purchaseAmount;
+    }
+
+    public static String validateInputString(String purchaseAmount) {
+        if (!purchaseAmount.matches("\\d+")) {
+            throw new IllegalArgumentException(String.valueOf(ExceptionMessage.PURCHASE_AMOUNT_NOT_NUMBER.getMessage()));
+        }
+        return purchaseAmount;
+    }
 }

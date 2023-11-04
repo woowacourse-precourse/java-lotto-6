4faf0c0c8908921c7bfe9bab813850f212511cd8
maark1106 <mark1106@naver.com>
2023-11-04 22:05:06 +0900

    feat : 구입 금액이 빈 문자값인지 검증

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,9 +6,14 @@ public class InputView {
 
     public static int inputPurchaseAmount(){
         String purchaseAmount = Console.readLine();
+        validateNotEmpty(purchaseAmount);
         return Integer.parseInt(purchaseAmount);
     }
 
-
+    private static void validateNotEmpty(String purchaseAmount) {
+        if(purchaseAmount.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] : 입력 값은 공백일 수 없습니다");
+        }
+    }
 
 }

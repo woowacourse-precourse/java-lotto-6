2e8565ca7dc430519617d423a907e8c7b0e453b6
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-04 18:12:54 +0900

    feat(domain): getter 구현 및 unmodifiableList 적용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -9,19 +11,22 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         Collections.sort(numbers);
-        this.numbers = numbers;
+        //아래를 변경해서 copyOf와 stream의 to List를 사용해서 간단하게 불변으로 만들 수 있지만 함수명이 불변을 설정했다는 것을 알 수 있게 설정
+        this.numbers = Collections.unmodifiableList(new ArrayList<>(numbers));
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        LottoValidator.numbersSize(numbers);
+        LottoValidator.numbersDuplication(numbers);
     }
 
     // TODO: 추가 기능 구현
 
     public void show() {
         System.out.println(numbers);
+    }
 
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,15 +1,21 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
     private final List<Lotto> lottos;
 
     private Lottos(List<Lotto> lottos) {
-        this.lottos = lottos;
+        this.lottos = Collections.unmodifiableList(new ArrayList<>(lottos));
     }
 
     public static Lottos from(List<Lotto> lottos) {
         return new Lottos(lottos);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }

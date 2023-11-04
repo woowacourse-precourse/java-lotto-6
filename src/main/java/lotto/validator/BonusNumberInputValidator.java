20d10299064ea447f8d0564a169e05cb897c7107
koomchang <leahpar0401@gmail.com>
2023-11-04 19:48:39 +0900

    feat: 에러 메시지 상수 처리

--- src/main/java/lotto/validator/BonusNumberInputValidator.java
+++ src/main/java/lotto/validator/BonusNumberInputValidator.java
@@ -4,8 +4,8 @@ public class BonusNumberInputValidator implements BasicValidator<String> {
 
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
-    private static final String INPUT_IS_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
-    private static final String INPUT_IS_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 입력값이 1과 45 사이여야 합니다.";
+    private static final String INPUT_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
+    private static final String INPUT_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 입력값이 1과 45 사이여야 합니다.";
 
     @Override
     public void validate(String input) {
@@ -17,14 +17,14 @@ public class BonusNumberInputValidator implements BasicValidator<String> {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INPUT_IS_NOT_NUMERIC);
+            throw new IllegalArgumentException(INPUT_NOT_NUMERIC);
         }
     }
 
     private void isBetweenOneAndFortyFive(String input) {
         int bonusNumber = Integer.parseInt(input);
         if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException(INPUT_IS_NOT_BETWEEN_ONE_AND_FORTY_FIVE);
+            throw new IllegalArgumentException(INPUT_NOT_BETWEEN_ONE_AND_FORTY_FIVE);
         }
     }
 }
--- src/main/java/lotto/validator/MoneyInputValidator.java
+++ src/main/java/lotto/validator/MoneyInputValidator.java
@@ -2,6 +2,11 @@ package lotto.validator;
 
 public class MoneyInputValidator implements BasicValidator<String> {
 
+    private static final String INPUT_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
+    private static final String INPUT_NOT_POSITIVE = "[ERROR] 입력값이 0보다 커야 합니다.";
+    private static final String INPUT_NOT_MULTIPLE_OF_THOUSAND = "[ERROR] 로또 구입 금액은 1000원 단위로 가능합니다.";
+    private static final String INPUT_NOT_MORE_THAN_ONE_THOUSAND = "[ERROR] 로또 구입 금액은 최소 1000원 이상이어야 합니다.";
+
     @Override
     public void validate(String input) {
         isNumeric(input);
@@ -14,28 +19,28 @@ public class MoneyInputValidator implements BasicValidator<String> {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 숫자가 아닙니다.");
+            throw new IllegalArgumentException(INPUT_NOT_NUMERIC);
         }
     }
 
     private void isPositive(String input) {
         int money = Integer.parseInt(input);
         if (money <= 0) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 0보다 커야 합니다.");
+            throw new IllegalArgumentException(INPUT_NOT_POSITIVE);
         }
     }
 
     private void isMultipleOfThousand(String input) {
         int money = Integer.parseInt(input);
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 가능합니다.");
+            throw new IllegalArgumentException(INPUT_NOT_MULTIPLE_OF_THOUSAND);
         }
     }
 
     private void isMoreThanOneThousand(String input) {
         int money = Integer.parseInt(input);
         if (money < 1000) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 최소 1000원 이상이어야 합니다.");
+            throw new IllegalArgumentException(INPUT_NOT_MORE_THAN_ONE_THOUSAND);
         }
     }
 }

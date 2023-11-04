9af924d7e45cf25371e69bed373785383b328340
hyungyeom <rlagusrua3687@gmail.com>
2023-11-04 15:00:51 +0900

    test : 사용자가 구매한 로또 티켓 장수 테스트

--- src/test/java/lotto/domain/PlayerLottoAmountTest.java
+++ src/test/java/lotto/domain/PlayerLottoAmountTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +17,12 @@ class PlayerLottoAmountTest {
     void incorrect_amount(int inputAmount) {
         assertThatThrownBy(() -> new PlayerLottoAmount(inputAmount))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구매한 로또 티켓 장수")
+    void calculateLottoCount() {
+        PlayerLottoAmount playerLottoAmount = new PlayerLottoAmount(8000);
+        assertThat(playerLottoAmount.calculateLottoCount()).isEqualTo(8);
     }
 }

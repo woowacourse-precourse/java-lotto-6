ec6ffe7311b491629beb6c13a0ca89002eb10848
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-04 17:17:07 +0900

    feat(UI) : InputView 구성 완료

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,20 @@
 package lotto.view;
 
 public class InputView {
+    private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ENTER_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String LINE = "\n";
+
+    public static void getAmount() {
+        System.out.println(ENTER_PURCHASE_AMOUNT);
+    }
+
+    public static void getWinningNumbers() {
+        System.out.println(ENTER_WINNING_NUMBERS);
+    }
+
+    public static void getBonusNumber() {
+        System.out.println(LINE + ENTER_BONUS_NUMBER);
+    }
 }

e8fd6d7bbe1e066c04755fae74b2a30547e62a24
hgh1472 <hgh1472@naver.com>
2023-11-05 00:50:51 +0900

    feat : 당첨번호 객체 추가

---
+++ src/main/java/lotto/Model/WinningNumbers.java
@@ -0,0 +1,19 @@
+package lotto.Model;
+
+import java.util.List;
+
+public class WinningNumbers {
+    Lotto winningNumbers;
+    int bonusNumber;
+
+    WinningNumbers(Lotto winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber() {
+
+    }
+
+    private void
+}

7f455b17b16d4fae0a9cbb8fb28a402deb77fb86
HeeHeeg <qmffndoden@naver.com>
2023-11-04 12:48:22 +0900

    feat(lotto): 당첨 번호를 입력받아 쉼표(,)를 기준으로 구분하는 기능 구현, 오름차순 정렬 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,8 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,14 +12,24 @@ public class Application {
         String purchaseAmount = Console.readLine();
         Integer userPurchaseAmount = Integer.parseInt(purchaseAmount);
 
-        int numberOfLottos = userPurchaseAmount / 1000;
         System.out.println();
+        int numberOfLottos = userPurchaseAmount / 1000;
         System.out.println(numberOfLottos + "개를 구매했습니다.");
         for (int i = 0; i < numberOfLottos; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(numbers);
-            Collections.sort(lotto.getNumbers());
+            lotto.getNumbers().sort(Comparator.naturalOrder());
             System.out.println(lotto.getNumbers());
         }
+
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String inputNumber = Console.readLine();
+        List<Integer> winningNumber = Arrays.stream(inputNumber.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println(winningNumber);
     }
 }

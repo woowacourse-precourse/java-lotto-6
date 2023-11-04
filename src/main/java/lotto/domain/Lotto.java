6c495fc661206a7249fa199f569f65fca0c60d4e
dbstn1325 <dbstn1325@naver.com>
2023-11-05 01:44:15 +0900

    feat(Lotto): 발행한 로또 번호가 중복이 있다면  예외처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,7 @@ public class Lotto {
     static final String LOTTO_START_ERROR_MESSAGE = "[ERROR] ";
     static final String LOTTO_NUMBER_LENGTH_INVALID_MESSAGE = "발행한 로또 번호는 6자리 숫자여야 합니다.";
     static final String LOTTO_NUMBER_RANGE_INVALID_MESSAGE = "발행한 로또 번호는 1~45의 숫자여야 합니다.";
+    static final String LOTTO_NUMBER_DUPLICATE_INVALID_MESSAGE = "발행한 로또 번호는 중복이 있으면 안됩니다.";
     private static final int LOTTO_SIX_SIZE = 6;
     private final List<Integer> numbers;
 
@@ -17,6 +18,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateLengthNumber(numbers);
         validateRange(numbers);
+        validateDuplication(numbers);
     }
 
     private static void validateLengthNumber(List<Integer> numbers) {
@@ -36,5 +38,18 @@ public class Lotto {
                 .filter(number -> number < 1 || number > 45)
                 .count();
         return numberCount != 0;
+    }
+
+    private static void validateDuplication(List<Integer> numbers) {
+        if (isDuplication(numbers)) {
+           throw new IllegalArgumentException(LOTTO_START_ERROR_MESSAGE + LOTTO_NUMBER_DUPLICATE_INVALID_MESSAGE);
+        };
+    }
+
+    private static boolean isDuplication(List<Integer> numbers) {
+        long duplication = numbers.stream()
+                .distinct()
+                .count();
+        return duplication != LOTTO_SIX_SIZE;
     }
 }

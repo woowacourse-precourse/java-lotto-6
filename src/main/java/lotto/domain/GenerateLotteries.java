b4f72c2cf1750b6209644adc30c5376bf10de167
lee jihyeon <lejinhy@gmail.com>
2023-11-04 14:46:05 +0900

    feat: 구입 금액만큼 로또를 생성하는 GeneratedLotteries 완성

---
+++ src/main/java/lotto/domain/GenerateLotteries.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateLotteries {
+    private final List<Lotto> lotteries = new ArrayList<>();
+
+    public GenerateLotteries(int count) {
+        for (int i = 0; i < count; i++) {
+            this.lotteries.add(generateRandomNumbers());
+        }
+    }
+
+    private Lotto generateRandomNumbers() {
+        Lotto numbers = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        return numbers;
+    }
+}

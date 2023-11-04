e1da0e59654d0b265434c6c004211487db6d57d3
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 20:03:58 +0900

    [feat] 로또 하나 당첨 정보 얻기 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -17,6 +17,26 @@ public class Lotto {
         }
     }
 
+    public Ranking getRanking(List<Integer> winningNumbers, int bonusNumber) {
+        int sameInWinningNumbers = (int) numbers.stream().filter(n -> winningNumbers.contains(n)).count();
+        if (sameInWinningNumbers == 6) {
+            return Ranking.FIRST;
+        }
+        if (sameInWinningNumbers == 5 && numbers.contains(bonusNumber)) {
+            return Ranking.SECOND;
+        }
+        if (sameInWinningNumbers == 5) {
+            return Ranking.THIRD;
+        }
+        if (sameInWinningNumbers == 4) {
+            return Ranking.FOURTH;
+        }
+        if (sameInWinningNumbers == 3) {
+            return Ranking.FIFTH;
+        }
+        return Ranking.NONE;
+    }
+
     @Override
     public String toString() {
         return numbers.stream()

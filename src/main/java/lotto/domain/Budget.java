a871cdcf71ad86d11faf6e8d730e2dbf1fbe704c
msung99 <msung6924@naver.com>
2023-11-04 16:08:08 +0900

    feat: 구입 금액이 1,000원 보다 작은지에 대해 검증하는 기능 구현

--- src/main/java/lotto/domain/Budget.java
+++ src/main/java/lotto/domain/Budget.java
@@ -3,7 +3,8 @@ package lotto.domain;
 public class Budget {
     private final int budget;
     private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 로또 금액은 숫자만 입력할 수 있습니다.";
-    private static final String DIVIDE_EXCEPTION_MESSAGE = "[ERROR] 로또 구입 최소 금액은 %d원 입니다.";
+    private static final String DIVIDE_EXCEPTION_MESSAGE = "[ERROR] 로또 구입 금액은 %d원 단위로 나누어 떨어져야합니다.";
+    private static final String MINIMUM_PRICE_EXCEPTION_MESSAGE = "[ERROR] 로또 구입 최소 금액은 %d원 입니다.";
     private static final int LOTTO_UNIT_PRICE = 1000;
     private static final int ZERO = 0;
 
@@ -14,7 +15,8 @@ public class Budget {
     public static Budget from(String inputBudget){
         validateNumeric(inputBudget);
         int budget = Integer.parseInt(inputBudget);
-        validateDivideAble(budget);
+        validateDivideResultIsZero(budget);
+        validateMinusPrice(budget);
         return new Budget(budget);
     }
 
@@ -26,9 +28,15 @@ public class Budget {
         }
     }
 
-    private static void validateDivideAble(int budget){
+    private static void validateDivideResultIsZero(int budget){
         if(budget % LOTTO_UNIT_PRICE != ZERO){
             throw new IllegalArgumentException(String.format(DIVIDE_EXCEPTION_MESSAGE, LOTTO_UNIT_PRICE));
+        }
+    }
+
+    private static void validateMinusPrice(int budget){
+        if(budget < LOTTO_UNIT_PRICE){
+            throw new IllegalArgumentException(String.format(MINIMUM_PRICE_EXCEPTION_MESSAGE, LOTTO_UNIT_PRICE));
         }
     }
 }

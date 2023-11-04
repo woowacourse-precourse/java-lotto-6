17cb8e9991f5640ee5845b32c6dc7e7c97138937
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 18:35:11 +0900

    test(BonusNumber): 보너스 번호 입력 검증 테스트

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BonusNumberTest {
+    private static final Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+    @DisplayName("문자 입력하면 예외 발생")
+    @Test
+    void createAmountByString() {
+        String input = "1,2";
+        assertThatThrownBy(() -> new BonusNumber(input, lotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자로만 이루어진 값을 입력해주세요.");
+    }
+
+    @DisplayName("1 미만의 숫자 입력하면 예외 발생")
+    @Test
+    void createAmountByUnder1() {
+        String input = "0";
+        assertThatThrownBy(() -> new BonusNumber(input, lotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.");
+    }
+
+    @DisplayName("45 초과의 숫자 입력하면 예외 발생")
+    @Test
+    void createAmountByOver45() {
+        String input = "46";
+        assertThatThrownBy(() -> new BonusNumber(input, lotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.");
+    }
+
+    @DisplayName("당첨 번호에 있는 값 입력하면 예외 발생")
+    @Test
+    void createAmountByNumberInLotto() {
+        String input = "6";
+        assertThatThrownBy(() -> new BonusNumber(input, lotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복된 숫자가 포함되어 있습니다.");
+    }
+}

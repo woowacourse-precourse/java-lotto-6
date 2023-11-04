1b4e19169ff522fc250285b3f24fec4e32fae743
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 17:34:21 +0900

    [feat] 구입 금액 입력 메세지 출력 기능

---
+++ src/main/java/lotto/util/io/OutputUtils.java
@@ -0,0 +1,9 @@
+package lotto.util.io;
+
+public class OutputUtils {
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printPurchaseAmountInputMessage() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+    }
+}

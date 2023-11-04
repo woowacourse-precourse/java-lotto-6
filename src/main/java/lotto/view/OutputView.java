890909b2c172de0ca5eea9d7d6ac79424962daf0
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 23:50:15 +0900

    feat(OutputView): 일치하는 숫자 당 당첨 금액과 당첨된 갯수를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,14 @@ public class OutputView {
 
     private final static String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
     private final static String WINNING_STATISTICS = "당첨 통계";
+    private final static String LOTTO_RESULT = """
+            ---
+            3개 일치 (5,000원) - %d개
+            4개 일치 (50,000원) - %d개
+            5개 일치 (1,500,000원) - %d개
+            5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+            6개 일치 (2,000,000,000원) - %d개
+            """;
 
     public void printPurchaseQuantity(int quantity) {
         System.out.println(String.format(PURCHASE_QUANTITY, quantity));
@@ -13,4 +21,7 @@ public class OutputView {
         System.out.println(WINNING_STATISTICS);
     }
 
+    public static void printLottoResult() {
+        System.out.println(String.format(LOTTO_RESULT));
+    }
 }

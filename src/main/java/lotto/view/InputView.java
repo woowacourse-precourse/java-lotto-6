129b7f806d6f6d7f591a98705cb5c29f6a3e5e10
kimwanyoung <dhks2869@gmail.com>
2023-11-04 22:05:15 +0900

    feat : WinningNumber 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,20 +2,35 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.BuyAmount;
+import lotto.domain.WinningNumber;
+import lotto.utils.converter.Converter;
 import lotto.utils.validator.Validator;
 
 public class InputView {
 
     private static final String ONLY_NUMBER_MESSAGE = "숫자만 입력 가능합니다.";
+    private static final String INPUT_BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     private InputView() {
     }
 
     public static BuyAmount getBuyAmountFromInput() {
         try {
+            System.out.println(INPUT_BUY_AMOUNT_MESSAGE);
             String buyAmount = read();
             Validator.validateEmpty(buyAmount);
             return new BuyAmount(Long.parseLong(buyAmount));
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ONLY_NUMBER_MESSAGE);
+        }
+    }
+
+    public static WinningNumber getWinningNumberFromInput() {
+        try {
+            System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+            String winningNumber = read();
+            return new WinningNumber(Converter.convertStringToList(winningNumber));
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(ONLY_NUMBER_MESSAGE);
         }

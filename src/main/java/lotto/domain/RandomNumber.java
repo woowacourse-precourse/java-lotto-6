5c143a4bccd7f4f5f46fc8f2e1de33be8806c990
hil679 <hil679922@gmail.com>
2023-11-04 20:24:42 +0900

    Feat(RandomNumber): Create skeleton code of RandomNumber

---
+++ src/main/java/lotto/domain/RandomNumber.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+    public List<Integer> generateRandomNumbers() {
+        return new ArrayList<>();
+    }
+
+    private void sortRandomNumbers() {
+        
+    }
+}

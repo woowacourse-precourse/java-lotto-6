5e6f5c69b457efcf901da172aa95f787e132d7bd
sangchu <didi1484@gmail.com>
2023-11-04 23:36:47 +0900

    feat: printPurchaseResult 메서드 추가 (발행한 로또 수량 및 번호 출력)

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,6 +1,26 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Game {
+    private static final int THOUSAND_UNIT = 1000;
+
     public void start() {
+        int purchaseQuantity = InputView.askPurchaseAmount() / THOUSAND_UNIT;
+        List<Lotto> lottos = issueLottos(purchaseQuantity);
+        OutputView.printPurchaseResult(lottos);
+    }
+
+    private List<Lotto> issueLottos(int purchaseQuantity) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseQuantity; i++) {
+            Lotto lotto = new Lotto(Lotto.issueLotto());
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import static lotto.view.constants.Notice.PURCHASE_RESULT;
+
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
+    public static void printPurchaseResult(List<Lotto> lottos) {
+        int quantity = lottos.size();
+        System.out.println("\n" + quantity + PURCHASE_RESULT.getMessage());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+    }
 }

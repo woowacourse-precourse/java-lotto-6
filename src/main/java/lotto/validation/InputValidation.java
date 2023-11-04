37ec0d8697736904335dac0da0068046d2ebc65f
tam31 <wxogud3377@naver.com>
2023-11-05 04:12:32 +0900

    Feat(InputValidation) : Integer의 형변환 감사 기능 구현 Docs : Integer의 형변환 검사 완료체크

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -5,23 +5,33 @@ public class InputValidation {
     private static final String ERROR = "[ERROR] ";
     private static final String IS_NULL_MESSAGE = "빈 문자열 입니다";
     private static final String IS_NOT_NUMBER_MESSAGE = "숫자가 아닙니다";
+    private static final String IS_EXCEED_NUMBER_RANGE_MESSAGE = "21억을 초과했습니다.";
 
-    public void validationMoney(String input){
+    public void validationMoney(String input) {
         isEmptyValidation(input);
         isNumberValidation(input);
+        changeInteger(input);
     }
 
     private void isEmptyValidation(String input) {
-        if(input.isBlank()){
+        if (input.isBlank()) {
             throw new IllegalArgumentException(ERROR + IS_NULL_MESSAGE);
         }
     }
 
-    private void isNumberValidation(String input){
-        for(int i = 0; i<input.length() ; i++){
-            if(!Character.isDigit(input.charAt(i))){
+    private void isNumberValidation(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
                 throw new IllegalArgumentException(ERROR + IS_NOT_NUMBER_MESSAGE);
             }
+        }
+    }
+
+    private void changeInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR + IS_EXCEED_NUMBER_RANGE_MESSAGE);
         }
     }
 }

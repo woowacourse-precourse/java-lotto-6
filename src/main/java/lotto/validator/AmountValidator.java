1cba58c36330e627733dc5f8e4cf693ae67d8183
1000zoo <cjswldn99@gmail.com>
2023-11-05 03:19:56 +0900

    feat(AmountValidator): 금액 입력 검증 기능 구현

--- src/main/java/lotto/validator/AmountValidator.java
+++ src/main/java/lotto/validator/AmountValidator.java
@@ -1,8 +1,34 @@
 package lotto.validator;
 
 public class AmountValidator implements Validator {
+
+    private final static String INTEGER_PATTERN = "^\\d+$";
+    private final static int DIVISOR_FOR_THOUSAND = 1000;
+
     @Override
     public void validate(String input) {
-        throw new IllegalArgumentException();
+        throwIfNotInteger(input);
+        throwIfNotDividedBy1000(input);
+    }
+
+    private void throwIfNotDividedBy1000(String input) {
+        int inputInteger = parseInt(input);
+        if (inputInteger % DIVISOR_FOR_THOUSAND != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void throwIfNotInteger(String input) {
+        if (!input.matches(INTEGER_PATTERN)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException();
+        }
     }
 }

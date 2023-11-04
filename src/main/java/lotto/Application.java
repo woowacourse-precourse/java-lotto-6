4576f7f7bd638b5cad9e8bbe3bcd22782673359a
minseo  jo <liging12@naver.com>
2023-11-04 13:57:17 +0900

    feat: Application 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.generator.LottoAutoGenerator;
+import lotto.domain.profit.LottoProfitCalculator;
+import lotto.domain.winning.WinningRankingCalculator;
+import lotto.domain.generator.LottoManualGenerator;
+import lotto.domain.store.LottoStore;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoAutoGenerator lottoAutoGenerator = new LottoAutoGenerator();
+        LottoManualGenerator lottoManualGenerator = new LottoManualGenerator();
+        LottoStore lottoStore = new LottoStore(lottoAutoGenerator, lottoManualGenerator);
+        WinningRankingCalculator winningRankingCalculator = new WinningRankingCalculator();
+        LottoProfitCalculator lottoProfitCalculator = new LottoProfitCalculator();
+
+        LottoController lottoController = new LottoController(inputView, outputView, lottoStore, winningRankingCalculator, lottoProfitCalculator);
+        lottoController.run();
     }
 }

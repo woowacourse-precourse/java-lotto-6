03da7abc7ce0badfd52aef3bf046bcf3e62a3e45
혜민 <alddlv81@gmail.com>
2023-11-05 00:10:03 +0900

    feat: 발행한 로또 번호 출력 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,19 @@
 package lotto.controller;
 
+import lotto.domain.GenerateLotto;
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoController {
     public static void run() {
         try {
-            System.out.println("\n" + getPurChaseAmount() + OutputView.printPurchaseLottoCount());
+            int lottoCount = getPurChaseAmount();
+            System.out.println("\n" + lottoCount + OutputView.printPurchaseLottoCount());
+            printLottoTickets(lottoCount);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             run();
@@ -18,5 +24,13 @@ public class LottoController {
         OutputView.printInputPurchaseAmount();
         String lottoCount = InputView.inputPurchaseAmount();
         return PurchaseLotto.getLottoCount(lottoCount);
+    }
+
+    private static void printLottoTickets(int lottoCount) {
+        List<Lotto> lottos = GenerateLotto.generateLottoGroup(lottoCount);
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }

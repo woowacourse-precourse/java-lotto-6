3afddc46bf99cec2620825ef7297ea258c5f8355
이진모 <jinmlee210@naver.com>
2023-11-04 16:47:21 +0900

    feat: 사용자 지불금액이 1000원으로 나누어떨어지는지 확인하는 기능구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -7,6 +7,7 @@ public class User {
 
     public void inputPayment(String input) {
         checkInteger(input);
+        checkRemain();
     }
 
     public void checkInteger(String input) {
@@ -14,6 +15,12 @@ public class User {
             payment = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 금액을 입력하셨습니다.");
+        }
+    }
+
+    public void checkRemain() {
+        if(payment % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
         }
     }
 }

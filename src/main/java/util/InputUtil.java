c80ac95172b9ae9942d0b62b1bbdf430634dd442
pjaehyun <show6756@gmail.com>
2023-11-04 16:53:34 +0900

    feat: 추가 InputUtil Class  및 사용자 입력 함수

---
+++ src/main/java/util/InputUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+    public static String getUserInput(String message) {
+        System.out.print(message);
+        return Console.readLine();
+    }
+}

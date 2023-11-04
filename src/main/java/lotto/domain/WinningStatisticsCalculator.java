31b60192d7cfb9c743c6272d120f96664bd5e995
junest66 <chlwndks333@gmail.com>
2023-11-04 23:14:04 +0900

    feat(WinningStatisticsCalculator): 당첨통계를 계산하는 기능 추가
    - 몇개가 맞았는지 확인하는 함수 추가
    - 보너스 번호가 맞았는지 확인하는 함수 추가

---
+++ src/main/java/lotto/domain/WinningStatisticsCalculator.java
@@ -0,0 +1,46 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.constants.WinningCriteria;
+
+public class WinningStatisticsCalculator {
+
+    private final PurchasedLottos purchasedLottos;
+    private final WinningLotto winningLotto;
+
+    public WinningStatisticsCalculator(PurchasedLottos purchasedLottos, WinningLotto winningLotto) {
+        this.purchasedLottos = purchasedLottos;
+        this.winningLotto = winningLotto;
+    }
+
+    public WinningStatistics calculateWinningStatistics() {
+        WinningStatistics statistics = new WinningStatistics();
+
+        for (Lotto lotto : purchasedLottos.getLottos()) {
+            int matchCount = countMatchingNumbers(lotto);
+            boolean hasBonus = hasBonusNumber(lotto);
+
+            WinningCriteria criteria = WinningCriteria.getRating(matchCount, hasBonus);
+            statistics.addResult(criteria);
+        }
+
+        return statistics;
+    }
+
+    private int countMatchingNumbers(Lotto lotto) {
+        List<Integer> winningNumbers = winningLotto.winningNumbers().getWinningNumber();
+        List<Integer> lottoNumbers = lotto.getNumbers();
+
+        int matchingCount = (int) lottoNumbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+
+        return matchingCount;
+    }
+
+    private boolean hasBonusNumber(Lotto lotto) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        return lottoNumbers.contains(winningLotto.bonusNumber());
+    }
+
+}

8cdc0c6389c0bef5b23c7cf1155725c3d50767d6
voyager003 <miraclex000@gmail.com>
2023-11-04 20:27:01 +0900

    feat(OutputView): add new class and method
    ADD OutputView class, constant, printLottoPurchaseAmountMessage() method
    - class: responsible for print system message
    - constant: containing message 'enter purchase amount'
    - method: print constant

---
+++ src/main/java/lotto/View/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.View;
+
+public class OutputView {
+
+    private static final String LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printLottoPurchaseAmountMessage() {
+        System.out.println(LOTTO_PURCHASE_AMOUNT_MESSAGE);
+    }
+}

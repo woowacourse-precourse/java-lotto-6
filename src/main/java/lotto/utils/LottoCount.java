2d4364c5a54a0de35266da521057e8b5938500a5
whxogus215 <whxogus215@naver.com>
2023-11-04 22:33:42 +0900

    feat(LottoCount): 금액 원 단위 값 추가

--- src/main/java/lotto/utils/LottoCount.java
+++ src/main/java/lotto/utils/LottoCount.java
@@ -3,21 +3,24 @@ package lotto.utils;
 import java.util.Arrays;
 
 public enum LottoCount {
-    THREE("3개 일치",3, 5000),
-    FOUR("4개 일치",4, 50000),
-    FIVE("5개 일치",5, 1500000),
-    SIX("6개 일치",6, 2000000000),
-    BONUS("5개 일치, 보너스 볼 일치", Integer.MAX_VALUE, 30000000),
-    NONE("3개 미만", Integer.MIN_VALUE, 0);
+    THREE("3개 일치", 3, 5000, "5,000원"),
+    FOUR("4개 일치", 4, 50000, "50,000원"),
+    FIVE("5개 일치", 5, 1500000, "1,500,000원"),
+    BONUS("5개 일치, 보너스 볼 일치", Integer.MAX_VALUE,
+            30000000, "30,000,000원"),
+    SIX("6개 일치", 6, 2000000000, "2,000,000,000원"),
+    NONE("3개 미만", Integer.MIN_VALUE, 0, "0원");
 
     private final String content;
     private final long duplicatedCount;
     private final long price;
+    private final String priceCommaFormat;
 
-    LottoCount(String content, long duplicatedCount, long price) {
+    LottoCount(String content, long duplicatedCount, long price, String priceCommaFormat) {
         this.content = content;
         this.duplicatedCount = duplicatedCount;
         this.price = price;
+        this.priceCommaFormat = priceCommaFormat;
     }
 
     public static LottoCount findByCount(long count) {
@@ -37,5 +40,9 @@ public enum LottoCount {
 
     public long getPrice() {
         return price;
+    }
+
+    public String getPriceCommaFormat() {
+        return priceCommaFormat;
     }
 }

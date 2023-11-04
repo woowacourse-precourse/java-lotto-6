a10830951916295091569c7f49240d5edcf8daea
yy8305 <yy8305@kakao.com>
2023-11-04 17:19:26 +0900

    test(공통): 테스트 공통 작업 처리 클래스 추가

---
+++ src/test/java/lotto/TestDefault.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import lotto.constants.PrintMessages;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestDefault {
+    private final String NEW_LINE = PrintMessages.NEW_LINE.getMessage();
+    private final PrintStream originOutput = System.out;
+    private OutputStream outputCaptor;
+
+    @BeforeEach
+    void defaultTestInit() {
+        outputCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputCaptor));
+    }
+
+    @AfterEach
+    void defaultTestCleanUp() {
+        System.setOut(originOutput);
+        Console.close();
+    }
+
+    protected String getConsoleOuputMessage() {
+        return outputCaptor.toString();
+    }
+
+    protected void setInputs(String... inputs) {
+        for (String input : inputs) {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+        }
+    }
+}

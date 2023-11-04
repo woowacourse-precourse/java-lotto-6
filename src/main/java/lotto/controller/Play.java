31d15e2c611cafd7e4b192f4cb739aeb15a45c11
Jiwon Na <jiwonna52@gmail.com>
2023-11-04 16:55:59 +0900

    Feat : 로또 번호와 당첨 번호와 일치한 번호의 수를 반환하는 기능

--- src/main/java/lotto/controller/Play.java
+++ src/main/java/lotto/controller/Play.java
@@ -1,9 +1,26 @@
 package lotto.controller;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Play {
 
     private static final int lottoPrice = 1000;
+    private static final int size = 6;
     public static int calLottoCount(int price) {
         return price / lottoPrice;
     }
+
+    public static int compareLottoAndWinning(List<Integer> lotto, List<Integer> winning) {
+        Set<Integer> uniqueLotto = new HashSet<>(lotto);
+        Set<Integer> uniqueWinning = new HashSet<>(winning);
+
+        uniqueLotto.retainAll(uniqueWinning);
+        return uniqueLotto.size();
+    }
+
+
+
 }

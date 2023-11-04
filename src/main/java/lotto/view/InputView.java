10dc591c55d446a77069e01ecd8ff06abd838f67
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-04 23:49:07 +0900

    feat - InputView 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+public class InputView {
+
+    public void showPurchasePriceMessage(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void showInputWinningNumberMessage(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void showInputBonusNumberMessage(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}

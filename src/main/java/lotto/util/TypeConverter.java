786778003af045d4636112250669a02fb868ba4e
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 03:05:44 +0900

    TypeConverter 유틸 클래스 작성

---
+++ src/main/java/lotto/util/TypeConverter.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+import lotto.view.ErrorMessage;
+
+public class TypeConverter {
+
+    public static Integer StringToInteger(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TYPE.getErrorMessage());
+        }
+    }
+}

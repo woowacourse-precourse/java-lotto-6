7369230447465e603a27a9cf6360420a7a6da22b
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-04 19:03:01 +0900

    feat(GameService) : add createWinningNumbers() method

--- src/main/java/lotto/service/GameService.java
+++ src/main/java/lotto/service/GameService.java
@@ -2,11 +2,17 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GameService {
+    public static List<Integer> createWinningNumbers(String winningNumber) {
+        return Arrays.stream(winningNumber.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
 
     public List<Integer> createLottoNumbers() {
         return IntStream.range(0, 6)

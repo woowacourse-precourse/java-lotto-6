08a3dbb61834ba38be300ec7ba616a973c895308
waveofmymind <sjun0913@gmail.com>
2023-11-04 22:47:32 +0900

    refactor: 로또 번호에 대한 검증을 별도의 클래스로 분리한다.

--- src/main/java/lotto/constraints/ErrorMessage.java
+++ src/main/java/lotto/constraints/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     DUPLICATE_NUMBER("[ERROR] 중복된 숫자가 존재합니다."),
     INVALID_MONEY("[ERROR] 1000원 이상의 금액을 입력해주세요."),
-    NOT_MULTIPLE_PER_PRICE("[ERROR] 1000원 단위로 입력해주세요.");
+    NOT_MULTIPLE_PER_PRICE("[ERROR] 1000원 단위로 입력해주세요."),
+    OUT_OF_RANGE("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요.");
 
 
     public String getMessage() {
---
+++ src/main/java/lotto/domain/LottoValidator.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import lotto.constraints.ErrorMessage;
+
+import java.util.List;
+
+public class LottoValidator {
+
+    public static void validateSize(List<Integer> numbers) {
+        if (numbers.size() != NumberConstraints.LOTTO_PER_NUMBER.value()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateDuplicateNumber(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != NumberConstraints.LOTTO_PER_NUMBER.value()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
+    public static void validateNumbersRange(List<Integer> numbers) {
+        numbers.forEach(LottoValidator::validateNumberRange);
+    }
+
+    public static void validateNumberRange(int numbers) {
+        if (numbers < NumberConstraints.LOTTO_MIN_NUMBER.value() || numbers > NumberConstraints.LOTTO_MAX_NUMBER.value()) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE.getMessage());
+        }
+    }
+}

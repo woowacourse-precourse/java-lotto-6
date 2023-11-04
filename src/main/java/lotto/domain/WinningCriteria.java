21016b85c9f9df0c2b8abcfd1b248f3b2c27cf59
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 21:11:28 +0900

    feat(WinningCriteria): 당첨 등수 및 정보 반환하는 기능 구현

--- src/main/java/lotto/domain/WinningCriteria.java
+++ src/main/java/lotto/domain/WinningCriteria.java
@@ -2,7 +2,7 @@ package lotto.domain;
 
 public enum WinningCriteria {
     FIRST(6, 2_000_000_000, false),
-    SECOND(5, 30000000, true),
+    SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000, false),
     FIFTH(3, 5_000, false),
@@ -28,5 +28,14 @@ public enum WinningCriteria {
 
     public boolean getMatchBonus() {
         return matchBonus;
+    }
+
+    public WinningCriteria getWinningType(int matchCount, boolean matchBonus) {
+        for (WinningCriteria type : WinningCriteria.values()) {
+            if (type.getMatchCount() == matchCount && type.getMatchBonus() == matchBonus) {
+                return type;
+            }
+        }
+        return MISS;
     }
 }

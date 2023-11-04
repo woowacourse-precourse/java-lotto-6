9e024a647af1c3db0a7cb2e1f59c5286b1c2c5a5
hil679 <hil679922@gmail.com>
2023-11-04 20:14:15 +0900

    Feat(PurchasePrice): PurchasePrice 클래스 메서드 선언

---
+++ src/main/java/lotto/domain/PurchasePrice.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+public class PurchasePrice {
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    private void validatePurchasePrice() {
+
+    }
+
+    private boolean isInteger() {
+        return true;
+    }
+
+    private void checkInteger() {
+
+    }
+
+    private boolean isOverMinPrice() {
+        return true;
+    }
+
+    private void checkOverMinPrice() {
+
+    }
+
+    private boolean isRemainderZero() {
+        return true;
+    }
+
+    private void checkRemainderZero() {
+
+    }
+
+
+
+}

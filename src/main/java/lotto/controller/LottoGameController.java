432b3ca423b74e6a50169aa9708dc310054a8f22
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 04:11:45 +0900

    refactor : 출력뷰와 인풋뷰를 묶어서 뷰로 분리

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,78 +1,21 @@
 package lotto.controller;
 
-import static lotto.constant.ErrorMessage.BONUS_NUMBER_CONTAINS_WINNING_ERROR_MESSAGE;
-
-import java.util.List;
 import lotto.Lotto;
-import lotto.util.Converter;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.view.View;
 import lotto.vo.BonusNumber;
 import lotto.vo.TicketQuantity;
 
 public class LottoGameController {
 
-    private final InputView inputView;
-    private final OutputView outputView;
+    private final View view;
 
-    public LottoGameController(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
+    public LottoGameController(View view) {
+        this.view = view;
     }
 
     public void run() {
-        TicketQuantity ticketQuantity = getTicketQuantity();
-        Lotto winningLotto = getWinningLotto();
-        BonusNumber bonusNumber = getBonusNumber(winningLotto.getNumbers());
-
-        outputView.printRequestInputBonusNumberMessage();
-        inputView.inputBonusNumbers();
+        TicketQuantity ticketQuantity = view.getTicketQuantity();
+        Lotto winningLotto = view.getWinningLotto();
+        BonusNumber bonusNumber = view.getBonusNumber(winningLotto.getNumbers());
     }
-
-    private TicketQuantity getTicketQuantity() {
-        outputView.printRequestInputPurchaseAmountMessage();
-        while (true) {
-            try {
-                Integer amount = inputView.inputPurchaseAmount();
-                TicketQuantity ticketQuantity = Converter.convertToTicketCount(amount);
-                outputView.printTicketQuantityMessage(ticketQuantity.quantity());
-                return ticketQuantity;
-            } catch (IllegalArgumentException e) {
-                outputView.printMessage(e.getMessage());
-            }
-        }
-    }
-
-    private Lotto getWinningLotto() {
-        outputView.printRequestInputWinningNumberMessage();
-        while (true) {
-            try {
-                String winningNumbers = inputView.inputWinningNumbers();
-                List<Integer> numbers = Converter.convertToLottoNumbers(winningNumbers);
-                return new Lotto(numbers);
-            } catch (IllegalArgumentException e) {
-                outputView.printMessage(e.getMessage());
-            }
-        }
-    }
-
-    public BonusNumber getBonusNumber(final List<Integer> numbers) {
-        outputView.printRequestInputBonusNumberMessage();
-        while (true) {
-            try {
-                Integer bonusNumber = inputView.inputBonusNumbers();
-                validateBonusNumberContainsWinningNumber(numbers, bonusNumber);
-                return new BonusNumber(bonusNumber);
-            } catch (IllegalArgumentException e) {
-                outputView.printMessage(e.getMessage());
-            }
-        }
-    }
-
-    private void validateBonusNumberContainsWinningNumber(List<Integer> winningNumbers, Integer bonusNumber) {
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(BONUS_NUMBER_CONTAINS_WINNING_ERROR_MESSAGE.getMessage());
-        }
-    }
-
 }
---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,67 @@
+package lotto.view;
+
+import static lotto.constant.ErrorMessage.BONUS_NUMBER_CONTAINS_WINNING_ERROR_MESSAGE;
+
+import java.util.List;
+import lotto.Lotto;
+import lotto.util.Converter;
+import lotto.vo.BonusNumber;
+import lotto.vo.TicketQuantity;
+
+public class View {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public View(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public TicketQuantity getTicketQuantity() {
+        outputView.printRequestInputPurchaseAmountMessage();
+        while (true) {
+            try {
+                Integer amount = inputView.inputPurchaseAmount();
+                TicketQuantity ticketQuantity = Converter.convertToTicketCount(amount);
+                outputView.printTicketQuantityMessage(ticketQuantity.quantity());
+                return ticketQuantity;
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    public Lotto getWinningLotto() {
+        outputView.printRequestInputWinningNumberMessage();
+        while (true) {
+            try {
+                String winningNumbers = inputView.inputWinningNumbers();
+                List<Integer> numbers = Converter.convertToLottoNumbers(winningNumbers);
+                return new Lotto(numbers);
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    public BonusNumber getBonusNumber(final List<Integer> numbers) {
+        outputView.printRequestInputBonusNumberMessage();
+        while (true) {
+            try {
+                Integer bonusNumber = inputView.inputBonusNumbers();
+                validateBonusNumberContainsWinningNumber(numbers, bonusNumber);
+                return new BonusNumber(bonusNumber);
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void validateBonusNumberContainsWinningNumber(final List<Integer> winningNumbers,
+                                                          final Integer bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_CONTAINS_WINNING_ERROR_MESSAGE.getMessage());
+        }
+    }
+}

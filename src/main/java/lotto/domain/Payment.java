f3c81f25f7a787ebb2e6663fea874849e283834b
ykmxxi <ykmxxi97@gmail.com>
2023-11-04 15:09:16 +0900

    refactor(Payment): 매직 넘버와 문자열 상수화

--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -2,6 +2,12 @@ package lotto.domain;
 
 public class Payment {
 
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MAX_AMOUNT = 2_000_000_000;
+    private static final String MIN_AMOUNT_ERROR_MESSAGE = "[ERROR] 최소 구입 금액은 1,000원 입니다.";
+    private static final String UNIT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위 입니다.";
+    private static final String MAX_AMOUNT_ERROR_MESSAGE = "[ERROR] 최대 구입 금액은 2,000,000,000원 입니다.";
+
     private final int amount;
 
     public Payment(final String amount) {
@@ -16,20 +22,20 @@ public class Payment {
     }
 
     private void validateMinimumAmount(final String amount) {
-        if (Integer.parseInt(amount) < 1000) {
-            throw new IllegalArgumentException("[ERROR] 최소 구입 금액은 1,000원 입니다.");
+        if (Integer.parseInt(amount) < LOTTO_PRICE) {
+            throw new IllegalArgumentException(MIN_AMOUNT_ERROR_MESSAGE);
         }
     }
 
     private void validateThousandUnit(final String amount) {
-        if (Integer.parseInt(amount) % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위 입니다.");
+        if (Integer.parseInt(amount) % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(UNIT_ERROR_MESSAGE);
         }
     }
 
     private void validateMaximumAmount(final String amount) {
-        if (Integer.parseInt(amount) > 2_000_000_000) {
-            throw new IllegalArgumentException("[ERROR] 최대 구입 금액은 2,000,000,000원 입니다.");
+        if (Integer.parseInt(amount) > MAX_AMOUNT) {
+            throw new IllegalArgumentException(MAX_AMOUNT_ERROR_MESSAGE);
         }
     }
 

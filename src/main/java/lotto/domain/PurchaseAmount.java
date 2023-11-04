cf51d5c4c9bbdb0185cbd02ef3d0186d0278a8c3
elyudwo <dudqk9696@naver.com>
2023-11-04 19:02:06 +0900

    [Feat] : 로또 구입 예외 처리 메서드 전체 호출 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -2,6 +2,12 @@ package lotto.domain;
 
 public class PurchaseAmount {
 
+    public void validate(String purchaseAmount) {
+        validateAmountNotNumber(purchaseAmount);
+        validateAmountUnderThousand(Integer.parseInt(purchaseAmount));
+        validateDivideByThousand(Integer.parseInt(purchaseAmount));
+    }
+
     public void validateDivideByThousand(int purchaseAmount) {
         if(purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] : 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");

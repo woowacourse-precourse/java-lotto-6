ee21c70bf0e5ce652af1ce860161535d08850370
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-11-04 15:46:12 +0900

    feat: add messages
    feat: add messages
    
    add error messages

--- src/main/java/lotto/common/Message.java
+++ src/main/java/lotto/common/Message.java
@@ -12,7 +12,10 @@ public enum Message {
             "5개 일치 (1,500,000원) - cond5개",
             "5개 일치, 보너스 볼 일치 (30,000,000원) - cond_bonus개",
             "3개 일치 (2,000,000,000원) - cond6개",
-            "총 수익률은 cond%입니다."));
+            "총 수익률은 cond%입니다.")),
+    ERROR_LOTTO_NUMBER(String.format("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.",
+            Range.START.getValue(), Range.END.getValue())),
+    ERROR_PURCHASE_AMOUNT("[ERROR] 구입 금액은 0원 이상이어야 합니다.");
 
     private String message;
 

4caa69ff7a7147a2752a4a16077750d9be00d81b
lesw <lesw1216@gmail.com>
2023-11-05 01:20:48 +0900

    feat: 구매 금액이 1000원 단위인지 검증하는 기능 구현

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -5,15 +5,23 @@ import lotto.utils.ExceptionMessage;
 public class Amount {
 
     private static final int MIN_AMOUNT = 1000;
+    private static final int LOTTO_AMOUNT = 1000;
 
 
     public void calculateTotalLottoCount(int purchaseAmount) {
         validateRange(purchaseAmount);
+        validateDivision(purchaseAmount);
     }
 
     private void validateRange(int purchaseAmount) throws IllegalArgumentException {
         if (purchaseAmount < MIN_AMOUNT) {
             throw new IllegalArgumentException(ExceptionMessage.ERROR_AMOUNT_RANGE);
+        }
+    }
+
+    private void validateDivision(int purchaseAmount) throws IllegalArgumentException {
+        if (purchaseAmount % LOTTO_AMOUNT != 0) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_AMOUNT_DIVISION);
         }
     }
 }
--- src/main/java/lotto/utils/ExceptionMessage.java
+++ src/main/java/lotto/utils/ExceptionMessage.java
@@ -2,4 +2,5 @@ package lotto.utils;
 
 public class ExceptionMessage {
     public static final String ERROR_AMOUNT_RANGE = "[ERROR] 1000원 이상의 금액을 입력하세요.";
+    public static final String ERROR_AMOUNT_DIVISION = "[ERROR] 구매 금액을 1000원 단위로 입력하세요.";
 }

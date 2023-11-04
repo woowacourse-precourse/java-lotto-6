3a4b045c8591c6c7132438357a3ec5dbcc673051
jeli01 <jeli01@naver.com>
2023-11-05 02:22:36 +0900

    feat: add selling lottos function

--- docs/README.md
+++ docs/README.md
@@ -4,26 +4,24 @@
 
 #### 비즈니스 기능
 
-
-- [ ] 금액에 맞는 로또를 발행하는 기능 - Store#sellLottos
-  - [x] 가격이 나누어 떨어지는 지 판단하는 기능 - Store#validateDividedByPrice
+- [x] 금액에 맞는 로또를 발행하는 기능 - Store#sellLottos
+    - [] 가격이 나누어 떨어지는 지 판단하는 기능 - Store#validateDividedByPrice
 
 
 - [ ] 로또 추첨을 받는 기능 - BroadCastingStation#drawLotto
-  - 중복되지 않는 숫자 6개와 보너스 번호 1개여야 한다. 
+    - 중복되지 않는 숫자 6개와 보너스 번호 1개여야 한다.
 
 
 - [ ] 당첨 결과를 확인하는 기능 - Buyer#checkWinning
 
 
 - [ ] 수익률을 계산하는 기능 - Calculator#returnRate
-  - 소수점 둘째 자리에서 반올림 되어야 한다. 
+    - 소수점 둘째 자리에서 반올림 되어야 한다.
 
-  
+
 - [x] 로또 번호가 6개 인지 판단하는 기능 - Lotto#validateSixNumbers
 - [x] 로또 번호가 중복되지 않는지 판단하는 기능 - Lotto#validateDuplicate
 - [x] 로또 번호 입력이 숫자 1~45인지 판단하는 기능 - Lotto#validateRange
-
 
 ---
 
--- src/main/java/lotto/Store.java
+++ src/main/java/lotto/Store.java
@@ -1,9 +1,20 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
-    private void validateDividedByPrice(Integer money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000으로 가격이 나누어 떨어져야 합니다.");
+    public List<Lotto> sellLottos(Integer money) {
+        List<Lotto> lottos = new ArrayList<Lotto>();
+
+        for (int i = 0; i < money / 1000; i++) {
+            List<Integer> lottoNumbers = pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto);
         }
+
+        return lottos;
     }
 }

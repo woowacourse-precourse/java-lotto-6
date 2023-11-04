335c1e853e9420ea28939088c65198997f6d26a0
kimwanyoung <dhks2869@gmail.com>
2023-11-04 15:41:44 +0900

    feat : 구매 금액 문자 입력 검증 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,12 +5,18 @@ import lotto.domain.BuyAmount;
 
 public class InputView {
 
+    private static final String ONLY_NUMBER_MESSAGE = "숫자만 입력 가능합니다.";
+
     private InputView() {
     }
 
     public static BuyAmount getBuyAmountFromInput() {
-        String buyAmount = read();
-        return new BuyAmount(Long.parseLong(buyAmount));
+        try {
+            String buyAmount = read();
+            return new BuyAmount(Long.parseLong(buyAmount));
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ONLY_NUMBER_MESSAGE);
+        }
     }
 
     private static String read() {

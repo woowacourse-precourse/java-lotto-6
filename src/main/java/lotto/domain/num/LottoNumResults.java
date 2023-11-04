ddd81d626bba48d862a9e8ad52cf3193d48cad34
박승찬 <seungchan141414@gmail.com>
2023-11-04 22:38:31 +0900

    [Feat(lotto)] 당첨 번호 모임에 당첨 번호 추첨 번호가 같은지 확인하는 기능을 추가한다.

--- src/main/java/lotto/domain/num/LottoNumResults.java
+++ src/main/java/lotto/domain/num/LottoNumResults.java
@@ -1,16 +1,36 @@
 package lotto.domain.num;
 
+import lotto.utill.Utii;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 당첨 번호와 보너스 번호들의 모임 클래스
  */
 public class LottoNumResults {
+    private WinLottoNums winLottoNums;
+    private BonusLottoNum bonusLottoNum;
+
+    public LottoNumResults(WinLottoNums winLottoNums, BonusLottoNum bonusLottoNum) {
+        this.winLottoNums = winLottoNums;
+        this.bonusLottoNum = bonusLottoNum;
+    }
+
     /**
      * 당첨 번호 추첨 번호가 같은지 확인한다.
      *
-     * @param num 확인하기 위한 번호
+     * @param targetNum 확인하기 위한 번호
      * @return
      */
-    public Boolean isSamePickNum(Integer num) {
+    public Boolean isSamePickNum(Integer targetNum, Integer indexOfSpecial) {
+        Boolean isSame = false;
+
+        if (indexOfSpecial >= 0 && indexOfSpecial <= 4) {
+            return winLottoNums.isSame(targetNum, indexOfSpecial);
+        }
+
         return false;
     }
 

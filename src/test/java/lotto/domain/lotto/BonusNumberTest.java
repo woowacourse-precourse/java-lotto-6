0bada87092a48d1d579ffe567b33c2fac5c52f6f
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 05:50:59 +0900

    test(BonusNumberTest) : hasSameNumber 기능 테스트

--- src/test/java/lotto/domain/lotto/BonusNumberTest.java
+++ src/test/java/lotto/domain/lotto/BonusNumberTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BonusNumberTest {
@@ -22,7 +23,22 @@ class BonusNumberTest {
     @ValueSource(ints = {1,25,45})
     void 보너스번호_생성_성공(int number) {
         assertDoesNotThrow(() -> new BonusNumber(number));
-        Assertions.assertThat(new BonusNumber(number).getBonusNumber()).isEqualTo(number);
+        Assertions.assertThat(new BonusNumber(number).getNumber()).isEqualTo(number);
+    }
+    
+
+    @ParameterizedTest
+    @DisplayName("BonusNumber가 number를 가지고 있는지 여부 테스트")
+    @CsvSource(value = {"1,1,true","1,2,false"})
+    void bonusNumber(int number, int otherNumber, boolean expected) {
+        // given
+        BonusNumber bonusNumber = new BonusNumber(number);
+
+        // when
+        boolean hasSameNumber = bonusNumber.hasSameNumber(otherNumber);
+
+        // then
+        Assertions.assertThat(hasSameNumber).isEqualTo(expected);
     }
 
 }

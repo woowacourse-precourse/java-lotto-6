849a54c04178aaa7fae253bd717e8fe3aa280356
devhoya97 <diajbh123@gmail.com>
2023-11-04 16:58:34 +0900

    feat(service): 로또 하나를 발행하는 기능 구현

--- src/main/java/lotto/service/LottoVendingMachine.java
+++ src/main/java/lotto/service/LottoVendingMachine.java
@@ -1,2 +1,23 @@
-package lotto.service;public class LottoVendingMachine {
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.wrapper.PurchaseAmout;
+import lotto.utils.LottoConstant;
+
+public class LottoVendingMachine {
+    private Lotto createLotto() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() != LottoConstant.LOTTO_NUMBERS_LENGTH.getNumber()) {
+            int randomNumber = Randoms.pickNumberInRange(
+                    LottoConstant.MIN_LOTTO_NUMBER.getNumber(), LottoConstant.MAX_LOTTO_NUMBER.getNumber());
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return new Lotto(randomNumbers);
+    }
 }

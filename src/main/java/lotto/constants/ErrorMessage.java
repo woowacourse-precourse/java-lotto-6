27d5fd07514ce5846620315c81a7bd8873d5146c
jongmee <ra123221@gmail.com>
2023-11-05 02:06:20 +0900

    feat(ErrorMessage): 입력값이 정수가 아닐 때 반환하는 에러 메세지 추가하기

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -3,4 +3,5 @@ package lotto.constants;
 public class ErrorMessage {
     public static final String INVALID_PURCHASE_AMOUNT = "로또를 구입할 금액은 1000원 단위여야 합니다.";
     public static final String INVALID_LOTTO_NUMBER_RANGE = "로또 번호는 1부터 45까지의 정수입니다.";
+    public static final String NOT_INTEGER = "정수를 입력해주세요.";
 }

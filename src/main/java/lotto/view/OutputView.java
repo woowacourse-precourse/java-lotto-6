cdee5a1475e93e421390b57222a134ca8bed2cbc
WOONBE <kepull2918@naver.com>
2023-11-04 14:10:16 +0900

    [refactor] static한 메서드를 non-static하게 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,38 +7,38 @@ import java.util.List;
 
 public class OutputView {
 
-    public static void requestInputAmountMessage() {
+    public void requestInputAmountMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-    public static void requestInputWinningNumbersMessage() {
+    public void requestInputWinningNumbersMessage() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
-    public static void requestInputBonusNumberMessage() {
+    public void requestInputBonusNumberMessage() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
-    private static final int COUNT_ZERO = 0;
-    public static void purchaseComplete(int count) {
+    private final int COUNT_ZERO = 0;
+    public void purchaseMessage(int count) {
         System.out.printf("\n%d개를 구매했습니다.\n", count);
     }
 
-    public static void lottoNumbers(List<Integer> numbers) {
+    public void lottoNumbers(List<Integer> numbers) {
         System.out.println(numbers);
     }
 
-    public static void winningStatistics() {
+    public void winningStatistics() {
         System.out.println("\n당첨 통계");
         System.out.println("---");
     }
 
-    public static void showRankResult(HashMap<Rank, Integer> map) {
+    public void showRankResult(HashMap<Rank, Integer> map) {
         for (Rank rank : Rank.values()) {
             showSingleRankResult(map, rank);
         }
     }
 
-    private static void showSingleRankResult(HashMap<Rank, Integer> map, Rank rank) {
+    private void showSingleRankResult(HashMap<Rank, Integer> map, Rank rank) {
         if (rank.getCount()==COUNT_ZERO) {
             return;
         }
@@ -50,7 +50,7 @@ public class OutputView {
         System.out.printf("%d개 일치 (%s원) - %d개\n", rank.getCount(), rank.getPrizeAmount(), map.get(rank));
     }
 
-    public static void showYieldResult(double yield) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", yield);
+    public void showRateOfReturn(double rate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", rate);
     }
 }

92f51893bdd6153523aba0f1623b1b566fee54a1
kimwanyoung <dhks2869@gmail.com>
2023-11-04 15:34:50 +0900

    feat : 구매 금액 1000원으로 나누어 떨어지는지 검증 구현

--- src/main/java/lotto/domain/BuyAmount.java
+++ src/main/java/lotto/domain/BuyAmount.java
@@ -2,9 +2,17 @@ package lotto.domain;
 
 public class BuyAmount {
 
+    private static final String DIVISION_ERROR_MESSAGE = "1000원으로 나눌 수 있는 금액만 입력 가능합니다.";
     private final int buyAmount;
 
     public BuyAmount(int buyAmount) {
+        validateAbleToDivide(buyAmount);
         this.buyAmount = buyAmount;
+    }
+
+    public void validateAbleToDivide(int buyAmount) {
+        if (buyAmount % 1000 != 0) {
+            throw new IllegalArgumentException(DIVISION_ERROR_MESSAGE);
+        }
     }
 }

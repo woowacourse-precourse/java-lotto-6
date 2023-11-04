3339c94dce338e631e2e41f68810622c340c6d07
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-04 16:31:29 +0900

    feat: 로또 익셉션을 관리하는 클래스 추가

---
+++ src/main/java/lotto/model/domain/LottoException.java
@@ -0,0 +1,21 @@
+package lotto.model.domain;
+
+public class LottoException extends Exception {
+
+    private LottoException() {
+
+    }
+
+    public static void rangeException(int min, int max) {
+        throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 "
+                + min + "이상 " + max + "이하여야 합니다.");
+    }
+
+    public static void duplicateException() {
+        throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+
+    public static void countException(int count) {
+        throw new IllegalArgumentException("[ERROR] 로또 번호는 " + count + "개여야 합니다.");
+    }
+}

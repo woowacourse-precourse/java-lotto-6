b0fb568b591892c91d18107f512b23ffb5b348f9
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-04 20:01:01 +0900

    feat(LottoController): 로또 가게를 나와서 로또 비교 사이트에 들어가는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,14 @@
 package lotto.controller;
 
+import lotto.domain.Lottos;
+
 public class LottoController {
     private final LottoMarket lottoMarket = new LottoMarket();
+    private final LottoCompareSite lottoCompareSite = new LottoCompareSite();
 
     public void play() {
         lottoMarket.enter();
+        Lottos lottos = lottoMarket.goOut();
+        lottoCompareSite.access();
     }
 }
--- src/main/java/lotto/controller/LottoMarket.java
+++ src/main/java/lotto/controller/LottoMarket.java
@@ -10,6 +10,7 @@ import lotto.view.OutputView;
 public class LottoMarket {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
+    private Lottos lottos;
 
     public void enter() {
         outputView.printInputMoneyMessage();
@@ -20,7 +21,12 @@ public class LottoMarket {
     private void buy(Money money) {
         LottoMarketService service = new LottoMarketService(money, new RandomNumbersGenerator());
         outputView.printPurchasedLottoCount(service.getPurchasedCount());
-        Lottos lottos = service.makeLottos();
+        lottos = service.makeLottos();
         outputView.printLottos(lottos.getLottos());
+        goOut();
+    }
+
+    public Lottos goOut() {
+        return lottos;
     }
 }

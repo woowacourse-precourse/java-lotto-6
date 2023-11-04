51e4e07068f2b103bd4aae67371d7f7f1a00d485
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-05 03:24:22 +0900

    feat: NumberService 추가

---
+++ src/main/java/lotto/service/NumberService.java
@@ -0,0 +1,64 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.MatchResult;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import static lotto.domain.constant.NumberConstant.LOTTO_PRICE;
+import static lotto.domain.constant.NumberConstant.MATCHRESULT_INCREMENT_NUM;
+import static lotto.service.InputService.readExpense;
+
+public class NumberService {
+    public static int computeLottoTicketNum() {
+        return (int) (readExpense() / LOTTO_PRICE);
+    }
+
+
+    public static Map<MatchResult, Integer> computeResult(List<Lotto> randomLottos, Lotto winningLotto, int bonusNum) {
+        Map<MatchResult, Integer> totalResult = initEnumMap();
+
+        for (Lotto randomLotto : randomLottos) {
+            MatchResult matchResult = Lotto.match(randomLotto, winningLotto, bonusNum);
+
+            totalResult.compute(matchResult, (k, v) -> v + MATCHRESULT_INCREMENT_NUM);
+        }
+
+        return totalResult;
+    }
+
+    private static Map<MatchResult, Integer> initEnumMap() {
+        EnumMap<MatchResult, Integer> enumMap = new EnumMap<>(MatchResult.class);
+
+        for (MatchResult result : MatchResult.values())
+            enumMap.put(result, 0);
+
+        return enumMap;
+    }
+
+
+    public static long computeTotalPrize(Map<MatchResult, Integer> result) {
+        return result.entrySet().stream()
+                .mapToLong(match -> computePrizeByNum(match.getKey(), match.getValue()))
+                .sum();
+    }
+
+    private static long computePrizeByNum(MatchResult matchResult, int num) {
+        return matchResult.getPrizeMoney() * num;
+    }
+
+
+    public static double computeMargin(int lottoTicketNum, long totalPrize) {
+        return computeDecimalPoint((double) totalPrize / (computeExpense(lottoTicketNum)));
+    }
+
+    private static long computeExpense(int lottoTicketNum) {
+        return ((long) lottoTicketNum) * LOTTO_PRICE;
+    }
+
+    private static double computeDecimalPoint(double margin) {
+        return Math.round(margin * 100) / 100.0;
+    }
+}

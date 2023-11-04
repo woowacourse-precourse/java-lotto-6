9b262315c736d977b4530bfd4449338dd6cd52d9
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 19:17:27 +0900

    test: PriceFormatValidator 테스트 코드 작성

---
+++ src/test/java/view/validator/PriceFormatValidatorTest.java
@@ -0,0 +1,39 @@
+package view.validator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import util.ErrorMessage;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PriceFormatValidatorTest {
+
+    PriceFormatValidator priceFormatValidator = new PriceFormatValidator();
+    @DisplayName("구매 금액이 입력되지 않으면 예외가 발생한다.")
+    @Test
+    void priceIsEmpty() {
+        assertThatThrownBy(() -> priceFormatValidator.checkPriceFormat(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.PRICE_DOESNT_ENTER.getErrorMessage());
+    }
+
+    @DisplayName("구매 금액이 숫자 이외의 문자가 포함되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"123a","abc","a1000"," 100","  2  "})
+    void priceContainNonNumericCharacter(String input) {
+        assertThatThrownBy(() -> priceFormatValidator.checkPriceFormat(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매 금액이 음수이면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1000","-10000","-20000"})
+    void priceIsNegative(String input) {
+        assertThatThrownBy(() -> priceFormatValidator.checkPriceFormat(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

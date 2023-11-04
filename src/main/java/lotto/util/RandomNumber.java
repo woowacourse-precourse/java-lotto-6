a1e802bc115faf6e4e35dcf890d9f547181b1287
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 18:37:51 +0900

    [fix] 오름차순 정렬

--- src/main/java/lotto/util/RandomNumber.java
+++ src/main/java/lotto/util/RandomNumber.java
@@ -1,13 +1,13 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumber {
 
     public static List<Integer> createRandomLottoNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        numbers.stream().forEach(number -> System.out.print(number + " "));
-        return numbers;
+        return numbers.stream().sorted().toList();
     }
 }

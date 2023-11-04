7709e97d2eccc5c7897f1db7658f515489ef40a0
siho-song <thdtlgh234@naver.com>
2023-11-04 22:17:51 +0900

    [Feat] 에러 메시지 수정  및 새로운 케이스 추가

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -1,15 +1,12 @@
 package lotto.domain;
 
 public enum ErrorMessage {
-    INVALID_DELIMITER("[ERROR] 번호는 ',' 로 구분되어야 합니다."),
-    INVALID_LOTTO_NUMBERS("[ERROR] 로또 번호는 숫자만 입력 가능합니다."),
     INVALID_NUMBER_RANGE("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_PURCHASE_AMOUNT_VALUE("[ERROR] 구입금액은 숫자만 입력 가능합니다."),
     INVALID_UNIT("[ERROR] 로또 구입 금액은 1000원 단위어야 합니다."),
+    MIN_AMOUNT("[ERROR] 구입 최소금액은 1000원 이상입니다."),
     DUPLICATED_BONUS_NUMBER("[ERROR] 당첨 번호와 중복된 번호를 입력할 수 없습니다."),
     DUPLICATED_NUMBER("[ERROR] 당첨 번호로 중복된 수를 입력할 수 없습니다."),
-    EXACTLY_ONE_NUMBER("[ERROR] 하나의 수만 입력해야 합니다."),
-    NOT_INTEGER("[ERROR] 정수 값만 입력 가능합니다."),
+    NOT_INTEGER("[ERROR] 번호는 정수 값만 입력 가능합니다."),
     TOTAL_NUMBER("[ERROR] 숫자는 총 6개여야 합니다.");
 
     private final String value;

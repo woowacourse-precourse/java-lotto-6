d1a7468bb0d5c1239e72aabb3c4bbd57b97aa368
sung-yeop <jjabc9459@naver.com>
2023-11-04 20:22:06 +0900

    refactor(InputValidator): validate 로직 수정
    - try-catch-finally 구문을 이용하여 ERROR_MESSAGE를 출력하고 최종적으로 에러를 발생시키도록 구현
    - throw new로 생성된 에러는 외부에서 핸들링하여 다시 로직을 수행하도록 설계 예정

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -5,20 +5,31 @@ public class InputValidator {
 
     // 에러 체크를 사용하는 로직에서는 IllegalArgumentException이 감지되면 해당 로직을 다시 실행하도록 설계 필요
 
-    public boolean validateInputType(String input) {
+    public void validateInputType(String input) {
         try {
             Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE);
+        } finally {
+            throw new IllegalArgumentException();
         }
-        return true;
     }
 
-    public boolean validateInputData(int amount, int unit) {
-        if (amount % unit != 0) {
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+    public void validateInputData(int amount, int unit) {
+        try {
+            validateAmount(amount, unit);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE);
+        } finally {
+            throw new IllegalArgumentException();
         }
-        return true;
+    }
+
+    public IllegalArgumentException validateAmount(int amount, int unit) {
+        if (amount % unit != 0) {
+            return new IllegalArgumentException();
+        }
+        return null;
     }
 
 }

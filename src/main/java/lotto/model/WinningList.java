6dc1a4cc2cb56954cc2c6f94f9ed87e848f72dd2
peppone-choi <peppone.choi@gmail.com>
2023-11-04 16:53:42 +0900

    feat: LottoController 및 전체 로직 구현 및 테스트

--- src/main/java/lotto/model/WinningList.java
+++ src/main/java/lotto/model/WinningList.java
@@ -4,7 +4,6 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 import lotto.enumerate.Rank;
 import lotto.record.AmountRecord;
 import lotto.record.LottoNumberRecord;

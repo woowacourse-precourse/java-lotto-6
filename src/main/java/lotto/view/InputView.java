f3f33c9e8db5a3d9ec0d56871643660304190a5e
JH-TT <leejongho9803@gmail.com>
2023-11-05 01:42:40 +0900

    feat: 입력 메시지 추가
    - 구입할때 메시지
    - winningLotto 입력 시 메시지

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+public class InputView {
+
+    public void inputPrise() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void inputNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+}

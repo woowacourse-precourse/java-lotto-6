8e5d98ebfd96f4c20f8dba53f92352cc7ef19a62
jiwoo <zbqmgldjfh@gmail.com>
2023-11-04 11:17:03 +0900

    feat(Lotto): Lotto 중복숫자 검증로직 추가 구현 완료

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,10 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,9 +13,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (isaInvalidLength(numbers) || isDuplicated(numbers)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isaInvalidLength(List<Integer> numbers) {
+        return numbers.size() != LOTTO_NUMBERS_SIZE;
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        if(Set.copyOf(numbers).size() != numbers.size()) {
+            return true;
+        }
+        return false;
     }
 
     // TODO: 추가 기능 구현

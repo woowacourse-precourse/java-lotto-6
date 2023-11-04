0713580568741d48d9b806c1fd1c5c50aae166bd
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-04 19:26:39 +0900

    feat(lotto): 필요한 정보 enum class 로 선언

---
+++ src/main/java/lotto/message/EarningInformation.java
@@ -0,0 +1,15 @@
+package lotto.message;
+
+public enum EarningInformation {
+    EARNING_FIFTH(5000),
+    EARNING_FOURTH(50000),
+    EARNING_THIRD(1500000),
+    EARNING_SECOND(30000000),
+    EARNING_FIRST(2000000000);
+
+    private final int value;
+    EarningInformation(int value){
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/lotto/message/LottoInformation.java
@@ -0,0 +1,19 @@
+package lotto.message;
+
+public enum LottoInformation {
+
+    LOTTO_PRICE(1000),
+    START_LOTTO_NUMBER(1),
+    LAST_LOTTO_NUMBER(45),
+    TOTAL_LOTTO_NUMBER(6),
+    TOTAL_BONUS_NUMBER(1);
+    private final int value;
+
+    LottoInformation(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

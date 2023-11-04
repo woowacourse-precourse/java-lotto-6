a3f8d572dce5573064544e2f9d0a4089b8a09a0f
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-04 16:17:48 +0900

    feat:로또에 중복되는 숫자가 있을경우 예외를 발생시키는 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateNumberSize(numbers);
+        validateRepetition(numbers);
         this.numbers = numbers;
     }
 
@@ -26,5 +27,19 @@ public class Lotto {
         if(validatedSize != numbers.size()) {
             throw new IllegalArgumentException("[Error] 각 숫자는 1~45 사이여야 합니다.");
         }
+    }
+
+    private void validateRepetition(List<Integer> numbers) {
+        int validatedSize = Math.toIntExact(numbers.stream()
+                .distinct()
+                .count());
+
+        if(validatedSize != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR]: 중복이 되지 않는 숫자로 입력해주세요!");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }

7966c2b21e4bf0a9c72489dd0c92f9d7a8efb17a
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 04:31:11 +0900

    feat : 메인 코드에 컨트롤러 실행 코드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGameController lottoGameController = new LottoGameController(
+                new View(new InputView(), new OutputView()));
+        lottoGameController.run();
     }
 }

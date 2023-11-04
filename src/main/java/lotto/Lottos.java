f4aba2bc64e41fbfffae730bc31a29fa9be66899
hunko <rhwlgns4386@naver.com>
2023-11-05 05:18:57 +0900

    feat: 로또 갯수 만큼 생성 기능 구현

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos from(Integer count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return new Lottos(lottos);
+    }
+
+    public List<Lotto> getList() {
+        return Collections.unmodifiableList(lottos);
+    }
+}
---
+++ src/test/java/lotto/LottosTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottosTest {
+
+    @DisplayName("로또를 여러개 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {1,3,8})
+    public void generateLottoMultipleTimes(Integer loop){
+        Lottos lottos=Lottos.from(loop);
+
+        assertThat(lottos.getList())
+                .hasSize(loop)
+                .doesNotContainNull();
+    }
+
+}

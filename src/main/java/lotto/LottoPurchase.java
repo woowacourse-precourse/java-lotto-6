21d75aa0d6631c3e86e91f714997de86130fa5d7
kcsc2217 <k12002@nate.com>
2023-11-04 17:22:17 +0900

    로또 구매금액 클래스 생성 및 메서드 구현

---
+++ src/main/java/lotto/LottoPurchase.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoPurchase {
+	private String amout;
+	private int number;
+
+	public int purchase() {
+		System.out.println("구매금액을 입력해 주세요.");
+		amout = Console.readLine();
+		return Integer.parseInt(amout);
+	}
+
+	public int numberaCount() {
+		number = purchase() / 1000;
+		return number;
+
+	}
+
+}

259009e5fb6107522f9c8bccc912baa82f746a9f
jongmee <ra123221@gmail.com>
2023-11-05 02:04:59 +0900

    feat(PurchaseAmountRequest): 입력 받은 로또 구매 금액에 대한 VO 클래스 생성하기
    - WHY: 입력값을 정수로 바꾸고 구매 금액을 구매 개수(PurchaseNumber)로 바꾸는 메서드를 포함한다. 입력(View)과 도메인 서비스 로직을 분리하면서 객체 지향을 지키려 도입하였다.

---
+++ src/main/java/lotto/utility/vo/PurchaseAmountRequest.java
@@ -0,0 +1,25 @@
+package lotto.utility.vo;
+
+import static lotto.constants.ErrorMessage.NOT_INTEGER;
+
+import lotto.domain.PurchaseNumber;
+
+public class PurchaseAmountRequest {
+    private final Integer price;
+
+    public PurchaseAmountRequest(String input) {
+        this.price = validateInteger(input);
+    }
+
+    private Integer validateInteger(String input) {
+        try{
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER);
+        }
+    }
+
+    public PurchaseNumber calculatePurchaseNumber() {
+        return new PurchaseNumber(price);
+    }
+}

e33e7c03e48c9bfec5c862f022c240b94f295073
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-11-04 21:49:34 +0900

    refactor: 에러 상수 추가(구매 단위 에러) 및 범위 상수 제거(보너스 숫자 포함 사이즈) 리팩터링

--- src/main/java/lotto/domain/constant/ExceptionMessage.java
+++ src/main/java/lotto/domain/constant/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     INVALID_RANGE_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     INVALID_RANGE_MONEY("유효하지 않은 구매 금액 범위입니다."),
     NOT_BLANK("공백이 아닌 숫자를 입력해주세요."),
-    INCLUDE_CHAR("문자를 제외한 숫자를 입력해주세요");
+    INCLUDE_CHAR("문자를 제외한 숫자를 입력해주세요"),
+    INVALID_UNIT_MONEY("구매 금액 단위에 맞게 입력해주세요.");
 
 
     private static final String ERROR = "[ERROR] ";
--- src/main/java/lotto/domain/constant/RangeConstant.java
+++ src/main/java/lotto/domain/constant/RangeConstant.java
@@ -2,7 +2,6 @@ package lotto.domain.constant;
 
 public enum RangeConstant {
     START_NUM(1), END_NUM(45), SIZE_NUM(6),
-    INPUT_NUM_SIZE_INCLUDE_BONUS_NUM(7),
     MAX_MONEY(Integer.MAX_VALUE), MIN_MONEY(0);
 
 

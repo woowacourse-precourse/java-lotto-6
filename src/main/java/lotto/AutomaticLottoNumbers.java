2e8e7f58d2db1e6efec609e1bf06383fbf8a9e95
cousim46 <wjdghldns24@naver.com>
2023-11-04 23:50:01 +0900

    feat : 로또 번호 오름차순으로 정렬하는 로직 추가

--- src/main/java/lotto/AutomaticLottoNumbers.java
+++ src/main/java/lotto/AutomaticLottoNumbers.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class AutomaticLottoNumbers {
 
     public static List<Integer> create() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> winLottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(winLottoNumbers);
+        return winLottoNumbers;
     }
 }

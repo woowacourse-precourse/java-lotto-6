8cd2e9923022e486cd5fb0982ad1027f1878b907
Maljaaa <Maljaaa@github.com>
2023-11-04 21:39:52 +0900

    refactor(InputView, PurchaseAmounts): parsing 역할 바꿈 - String -> int 메서드는 InputView에서 처리

--- src/main/java/lotto/domain/PurchaseAmounts.java
+++ src/main/java/lotto/domain/PurchaseAmounts.java
@@ -1,22 +1,13 @@
 package lotto.domain;
 
 public class PurchaseAmounts {
-    private final String VALIDATE_NUMBER_CODE = "-?\\\\d+";
     private final int LOTTO_PRICE = 1000;
 
     private int purchaseAmounts;
 
-    public PurchaseAmounts(String userInput) {
-        int parsingUserInput = parsingPurchaseAmounts(userInput);
-        validatePurchaseAmounts(parsingUserInput);
-        this.purchaseAmounts = parsingUserInput;
-    }
-
-    private int parsingPurchaseAmounts(String userInput) {
-        if (!userInput.matches(VALIDATE_NUMBER_CODE)) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다. 다시 입력해주세요.");
-        }
-        return Integer.parseInt(userInput);
+    public PurchaseAmounts(int purchaseAmounts) {
+        validatePurchaseAmounts(purchaseAmounts);
+        this.purchaseAmounts = purchaseAmounts;
     }
 
     private void validatePurchaseAmounts(int purchaseAmount) {

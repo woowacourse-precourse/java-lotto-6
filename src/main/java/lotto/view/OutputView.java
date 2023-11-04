883b00f9fda736f1a8e284277fec83d9022831e0
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-04 23:49:49 +0900

    feat - OutputView 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,35 @@
+package lotto.view;
+
+import java.util.Arrays;
+import lotto.domain.Lotto;
+import lotto.domain.Ranking;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void showPurchaseMessage(int buyingCount) {
+        System.out.println("");
+        System.out.println(buyingCount + "개를 구매했습니다.");
+    }
+
+    public void showPurchasedLottoNumbers(List<Lotto> numbers) {
+        for (Lotto lotto : numbers) {
+            System.out.println(lotto.getNumbers());
+        }
+        System.out.println("");
+    }
+
+    public void showResult() {
+        List<Ranking> rankings = Arrays.stream(Ranking.values()).toList();
+        System.out.println("");
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Ranking ranking : rankings) {
+            System.out.println(ranking.getMessage() + " - " + ranking.getCount() + "개");
+        }
+    }
+    public void showPayOff(double payOff){
+        System.out.println("총 수익률은 " + payOff + "입니다.");
+    }
+}

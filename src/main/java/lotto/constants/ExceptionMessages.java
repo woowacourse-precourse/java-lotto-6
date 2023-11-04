ef7af96b1b0019aeede95c2d457e823bf430e0d3
chae401 <chae401@sju.ac.kr>
2023-11-04 22:58:13 +0900

    feat(ExceptionMessages): add exception msgs about winning nums

--- src/main/java/lotto/constants/ExceptionMessages.java
+++ src/main/java/lotto/constants/ExceptionMessages.java
@@ -5,5 +5,7 @@ public class ExceptionMessages {
     public static final String INVALID_NUMBER = "숫자를 입력해주세요.";
     public static final String INVALID_PAYMENT = "금액은 1000원 단위로 입력해주세요.";
     public static final String INPUT_EMPTY = "입력이 되지 않았습니다.";
+    public static final String INVALID_RANGE = "1~45 사이의 수를 입력해주세요.";
+    public static final String DUPLICATED_NUMBER = "중복된 숫자가 입력되었습니다.";
 
 }

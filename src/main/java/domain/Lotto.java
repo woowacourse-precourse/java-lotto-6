d0e59205630ad11c83255ec3c71c067f11f64866
song-jae-wan <sjo9810@gmail.com>
2023-11-04 22:34:30 +0900

    feat: 로또번호 중복제거 기능 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,18 +1,26 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateSize(numbers);
+        validateSize(Removeduplication(numbers));
         this.numbers = numbers;
+    }
+
+    private List<Integer> Removeduplication(List<Integer> numbers) {
+        Set<Integer> lottoSet = new HashSet<>(numbers);
+        return new ArrayList<>(lottoSet);
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) { // 상수 처리 필요
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복 없는 6개 입니다.");
         }
     }
 

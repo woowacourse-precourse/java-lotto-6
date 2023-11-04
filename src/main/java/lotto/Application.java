915291911c87ac7fcb67493c2ffdce6802ad248b
leedabin2 <12193152@inha.edu>
2023-11-04 18:04:03 +0900

    feat: Add lottoController.buyLottos() method execution code

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
 import lotto.handler.UserHandler;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 //        UserHandler.getAmountFromUser();
-        int amount = UserHandler.getAmountFromUser();
-        UserHandler.getLottoCountFromUser(amount);
+
+        LottoController lottoController = new LottoController();
+        lottoController.buyLottos();
     }
 }
+

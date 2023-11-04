6d06327463286a9846c34d668efc556c5b233c51
shin5774 <babo5774@naver.com>
2023-11-04 15:26:59 +0900

    Test: 총 수익률 반환 기능 테스트코드 작성
    - 총 수익과 구매액을 통해 총 수익률을 반환해주는 기능.

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -4,6 +4,7 @@ import static lotto.Constant.BUY_UNIT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,5 +17,21 @@ public class LottoGameTest {
         long expected=buyCash/BUY_UNIT;
 
         assertThat(lottoGame.getLottoAmount(buyCash)).isEqualTo(expected);
+    }
+
+    @DisplayName("총 수익과 구매금액을 통해 수익률을 반환해주는 기능.")
+    @Test
+    void 수익률_반환(){
+        //given
+        LottoGame lottoGame=new LottoGame();
+        long totalProfit=5000;
+        long buyCash=8000;
+        double expected=62.5;
+
+        //when
+        double result=lottoGame.getTotalProfitRate(totalProfit,buyCash);
+
+        //then
+        assertThat(result).isEqualTo(expected);
     }
 }

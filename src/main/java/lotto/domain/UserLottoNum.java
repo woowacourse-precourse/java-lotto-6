66f632f30b34dd5fe1e64d05b1b9a50c1361f16b
goragoraki <gorakio0126@gmail.com>
2023-11-04 17:28:30 +0900

    feat(UserLottoNum): 사용자 로또 번호 생성 기능 추가
    입력한 로또 번호와 보너스 입력 번호를 검증하고 저장

---
+++ src/main/java/lotto/domain/UserLottoNum.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UserLottoNum {
+    private static final Integer LOTTO_SIZE = 7;
+    private static final Integer MAX_NUM = 45;
+    private static final Integer MIN_NUM = 1;
+
+    private final Lotto userLotto;
+    private final Integer bonusNum;
+
+    public UserLottoNum(List<Integer> userLotto, Integer bonusNum) {
+        this.userLotto = new Lotto(userLotto);
+
+        validateBonusNum(userLotto, bonusNum);
+        this.bonusNum = bonusNum;
+    }
+
+    private void validateBonusNum(List<Integer> userLotto, Integer bonusNum) {
+        validateDuplicate(userLotto, bonusNum);
+        validateRange(bonusNum);
+    }
+
+    private void validateDuplicate(List<Integer> userLotto, Integer bonusNum) {
+        Set<Integer> nonDuplicateNum = new HashSet<>(userLotto);
+        nonDuplicateNum.add(bonusNum);
+        if(nonDuplicateNum.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 중복되었습니다.");
+        }
+    }
+
+    private void validateRange(Integer bonusNum) {
+        if(bonusNum <= MIN_NUM || bonusNum >= MAX_NUM) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 1~45 범위에 없습니다.");
+        }
+    }
+}

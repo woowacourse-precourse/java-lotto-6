a097510503d4d980060aba1cb4c3a55fb489d43b
D0ri123 <lsh81550@gmail.com>
2023-11-05 03:33:13 +0900

    feat(OutputView): 당첨 통계 출력 기능 구현 - 2차 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
+import lotto.model.Ranking;
 
 public class OutputView {
 
@@ -17,5 +20,27 @@ public class OutputView {
             String result = Arrays.toString(lotto.getNumbers().toArray());
             System.out.println(result);
         }
+    }
+
+    public void printLotteryStatistics() {
+        System.out.println();
+        System.out.println("당첨 통계\n---");
+    }
+
+    public void printStatisticsResult(Map<Ranking, Integer> results) {
+        List<Ranking> ordered = Ranking.getAllRankingCase();
+        for(Ranking ranking : ordered) {
+            System.out.printf(ranking.getMessage(), ranking.getMachedCount(), transfer(ranking.getAmount()), results.get(ranking));
+        }
+    }
+
+    private String transfer(int price) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(price);
+    }
+
+    public void printRateOfReturn(float result) {
+        System.out.println();
+        System.out.printf("총 수익률은 %.1f%%입니다.", result);
     }
 }

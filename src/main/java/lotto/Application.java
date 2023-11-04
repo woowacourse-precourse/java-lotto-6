15f5d692d9d4ef4ea98d4a013bd076b971caf88d
3juhwan <13selfesteem91@naver.com>
2023-11-04 23:46:57 +0900

    feat: 보너스 번호 입력 로직

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     private final static String ASK_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String ASK_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String ASK_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
 
@@ -17,6 +18,11 @@ public class Application {
 
     private static String askWinningNumber() {
         System.out.println(ASK_WINNING_NUMBER_MESSAGE);
+        return readLine();
+    }
+
+    private static String askBonusNumberMessage() {
+        System.out.println(ASK_BONUS_NUMBER_MESSAGE);
         return readLine();
     }
 }

6ea5380bfb353828e83272e563ed9b4fcc445dba
oyoungsun <oyoungsun1030@gmail.com>
2023-11-04 12:30:54 +0900

    refactor(Money) : 생성자 성능을 위해 검증 기능 이동

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import lotto.constants.LottoConstants;
+
+public class Money {
+    private int money;
+
+    private Money(int money) {
+        this.money = money;
+    }
+
+    public static Money from(int money) {
+        validate(money);
+        return new Money(money);
+    }
+
+    private static void validate(int money) {
+        if (isDividedByPrice(money)) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 구입 금액은 %d원 단위입니다.", LottoConstants.PRICE));
+        }
+        if (isInMinMoney(money)) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 최소 구입 금액은 %d원 입니다.", LottoConstants.PRICE));
+        }
+    }
+
+    private static boolean isDividedByPrice(int money) {
+        return money % LottoConstants.PRICE.getConstants() != 0;
+    }
+
+    private static boolean isInMinMoney(int money) {
+        return money < LottoConstants.PRICE.getConstants();
+    }
+
+    public int requestLottoCount() {
+        return money / LottoConstants.PRICE.getConstants();
+    }
+
+}

d07782bfe0c32d19e620c6ca26fa15254c7ef815
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 23:13:08 +0900

    feat: 로또 번호 생성 관련 상수 추가

--- src/main/java/util/ConstantOfLotto.java
+++ src/main/java/util/ConstantOfLotto.java
@@ -2,4 +2,7 @@ package util;
 
 public class ConstantOfLotto {
     public static final int priceOfLotto = 1000;
+    public static final int numberOfLottoNumbers = 6;
+    public static final int minimumOfLottoNumber = 1;
+    public static final int maximumOfLottoNumber = 45;
 }

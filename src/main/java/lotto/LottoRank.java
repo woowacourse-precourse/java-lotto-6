fe76064a48d0212aeaf4ee5eca0a0d8187b79b7d
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-04 18:39:00 +0900

    refactor: 로또 랭크 및 상금 ì enum 상수화

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,5 +1,32 @@
 package lotto;
 
 public enum LottoRank {
+    FIRST(6, 0, 2000000000),
+    SECOND(5, 1, 30000000),
+    THIRD(5, 0, 1500000),
+    FOURTH(4, 0, 50000),
+    FIFTH(3, 0, 5000),
+    NONE(0, 0, 0);
 
+    private final int lottoCount;
+    private final int bonusCount;
+    private final int prize;
+
+    LottoRank(int lottoCount, int bonusCount, int prize) {
+        this.lottoCount = lottoCount;
+        this.bonusCount = bonusCount;
+        this.prize = prize;
+    }
+
+    public int getLottoCount() {
+        return lottoCount;
+    }
+
+    public int getBonusCount() {
+        return bonusCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }

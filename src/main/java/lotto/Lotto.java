620bd17aaae262c8c9b3544b9b5427022644e917
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 17:06:36 +0900

    refactor(Lotto): Stream 에서 HashSet 생성자를 사용하는 방식으로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,7 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
@@ -20,10 +20,10 @@ public class Lotto {
     }
 
     private void validateUniqueElements(List<Integer> numbers) {
-        int size = numbers.size();
-        int size1 = numbers.stream().collect(Collectors.toSet()).size();
+        int userInputSize = numbers.size();
+        int userInputUniqueSize = new HashSet<>(numbers).size();
 
-        if (size != size1) {
+        if (userInputSize != userInputUniqueSize) {
             throw new IllegalArgumentException("[ERROR] 로또 입력 값은 중복을 허용하지 않습니다.");
         }
     }

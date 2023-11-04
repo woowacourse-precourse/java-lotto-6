8fc5ee5839b39a6d75c050fd8db3417df0dcddfb
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-04 14:39:47 +0900

    feat: 일급콜렉션인 Lottos.class 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,11 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lottos {
+    public final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
 }

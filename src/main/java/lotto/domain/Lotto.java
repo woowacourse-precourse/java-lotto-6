7aa89f9339d87fbccb8fef0461d699f892dbaa49
goragoraki <gorakio0126@gmail.com>
2023-11-04 17:24:44 +0900

    feat(Lotto): 입력한 로또 번호 검증
    번호가 6개인지 확인
    중복된 번호가 있는지 확인
    1~45 범위에 번호가 있는지 확인
    오름차순으로 로또 번호 저장

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,51 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private static final int MAX_NUM = 45;
+    private static final int MIN_NUM = 1;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortASC(numbers);
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력하신 번호가 6개가 아닙니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNum = new HashSet<>(numbers);
+        if (nonDuplicateNum.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 중복된 로또 번호가 있습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(this::validateRange);
+    }
+
+    private void validateRange(Integer number) {
+        if(number < MIN_NUM || number > MAX_NUM) {
+            throw new IllegalArgumentException("[ERROR] 1~45를 벗어나는 숫자가 있습니다.");
+        }
+    }
+
+    public List<Integer> sortASC(List<Integer> numbers) {
+        return numbers.stream().sorted().collect(Collectors.toList());
+    }
 }

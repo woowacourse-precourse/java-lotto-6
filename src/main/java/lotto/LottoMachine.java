3938078650417e68fbef14e21fd33832dd74f3ec
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-04 18:00:12 +0900

    feat(LottoMachine): 입력된 개수만큼 로또를 발급하는 기능 추가
    전달받은 count 파라미터 만큼의 로또를 객체 리스트로 만들어 반환한다.

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,5 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
-    
+    private List<Lotto> lotteries = new ArrayList<>();
+
+    public List<Lotto> publishLotteries(int count) {
+        while (count > 0) {
+            lotteries.add(this.GenerateLotto());
+            count--;
+        }
+        return lotteries;
+    }
+
+    private Lotto GenerateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER,
+                Lotto.LOTTO_LENGTH);
+        return new Lotto(numbers);
+    }
 }

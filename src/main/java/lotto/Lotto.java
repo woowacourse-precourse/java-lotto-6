b26308d7cd4beb0a8cd7607f9402d5c048d7dadc
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-05 00:51:43 +0900

    feat(Lotto): 중복된 숫자 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +15,16 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+        hasDuplicateNumber(numbers);
+    }
+    private void hasDuplicateNumber(List<Integer> numbers) {
+        final Set<Integer> set = new HashSet<>();
+
+        for (Integer yourInt : numbers) {
+            if (!set.add(yourInt)) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 

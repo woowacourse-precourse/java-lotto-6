5d989bef9b68a0320c55d4b3ae03abbf2a6cf639
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-05 01:48:05 +0900

    feat(Lotto): 로또 비교 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,13 +15,22 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateDuplicateNumber(numbers);
         validateLottoSize(numbers);
+        validateNumberRange(numbers);
     }
 
-    private void validateLottoSize(List<Integer> numbers) {
-        if(numbers.size() != 6){
+    private void validateNumberRange(List<Integer> numbers) {
+        List<Integer> a = numbers.stream().filter(h -> h < 0 || h > 45).toList();
+        if (!a.isEmpty()) {
             throw new IllegalArgumentException();
         }
     }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     private void validateDuplicateNumber(List<Integer> numbers) {
         final Set<Integer> set = new HashSet<>();
 
@@ -32,8 +41,14 @@ public class Lotto {
         }
     }
 
+    public int match(Lotto lotto) {
+        int matchedNumber = 0;
+        for (int i = 0; i < 6; i++) {
+            if (this.numbers.get(i).equals(lotto.numbers.get(i))) {
+                matchedNumber++;
+            }
+        }
+        return matchedNumber;
+    }
 
-
-
-    // TODO: 추가 기능 구현
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,17 +25,27 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호가 유효한 범위 밖에 있으면 예외가 발생한다.")
     @Test
-    void createLottoByValidDigit() {
+    void createLottoByValidRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,-1)))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또의 비교 기능이 정확한 값을 리턴하는지 검사한다")
+    @Test
+    void checkMatchedDigits() {
+        // given
+        Lotto lotto1 = new Lotto(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        Lotto lotto2 = new Lotto(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        // when
+        // then
+        assertThat(lotto1.match(lotto2)).isEqualTo(6);
     }
 }

7ef8488a0f1b8483c8a0e9adf98f72f2ef43f326
혜민 <alddlv81@gmail.com>
2023-11-04 22:55:07 +0900

    feat: PURCHASE_LOTTO_COUNT 메시지 추가

--- src/main/java/lotto/constant/ViewMessage.java
+++ src/main/java/lotto/constant/ViewMessage.java
@@ -3,7 +3,9 @@ package lotto.constant;
 public enum ViewMessage {
     INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+
+    PURCHASE_LOTTO_COUNT("개를 구매했습니다.");
 
     private final String text;
 

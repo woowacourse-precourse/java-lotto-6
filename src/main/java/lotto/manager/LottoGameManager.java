649e9416f840bf2f42244f5695bf7060ac1ce978
irerin07 <counterchord89@gmail.com>
2023-11-04 09:45:59 +0900

    feat: #2 LottoGameManager 클래스 생성
    로또 게임을 전체적으로 관리하는 클래스 생성
    - start() 메서드 추가
    
    Closes #2

---
+++ src/main/java/lotto/manager/LottoGameManager.java
@@ -0,0 +1,14 @@
+package lotto.manager;
+
+/**
+ * @author 민경수
+ * @description lotto manager
+ * @since 2023.11.04
+ **********************************************************************************************************************/
+public class LottoGameManager {
+
+    public void start() {
+
+    }
+    
+}

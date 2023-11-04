891ef8572399a3fa292cdc32f843db1cef444669
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-04 22:07:33 +0900

    <feat> : <LottoService 전체적인 알고리즘을 담는 클래스 생성>
    Lottos 와 LottoSystem 으로 알고리즘을 돌리는 LottoService 생성

---
+++ src/main/java/service/LottoService.java
@@ -0,0 +1,10 @@
+package service;
+
+import domain.LottoSystem;
+import domain.Lottos;
+
+public class LottoService {
+    public void revealLottery(Lottos lottos,LottoSystem lottoSystem){
+
+    }
+}

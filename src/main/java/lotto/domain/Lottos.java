f684371fdccf374517d28d92b8bd573934b2daec
devhoya97 <diajbh123@gmail.com>
2023-11-04 20:26:58 +0900

    feat(domain): 구입한 모든 로또에 대해 출력을 위한 포메팅 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,11 +1,20 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.utils.LottoConstant;
 
 public class Lottos {
     private List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    @Override
+    public String toString() {
+        return lottos.stream()
+                .map(Lotto::toString)
+                .collect(Collectors.joining(LottoConstant.NEW_LINE.get()));
     }
 }

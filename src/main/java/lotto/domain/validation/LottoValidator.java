952948c5ff51b7e71451f8907007cd62c69cc8f9
LeeJiWon <dlwldnjs1009@gmail.com>
2023-11-04 17:37:43 +0900

    feat: 로또 번호 유효성 검사를 위한 Validator 클래스 추가

---
+++ src/main/java/lotto/domain/validation/LottoValidator.java
@@ -0,0 +1,20 @@
+package lotto.domain.validation;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class LottoValidator {
+    public static void validateLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 정확히 6개여야 합니다.");
+        }
+
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+        }
+
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+}

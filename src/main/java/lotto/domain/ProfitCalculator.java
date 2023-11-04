24c649da8f23539a9fcb75bb5a42385f99e505e7
junest66 <chlwndks333@gmail.com>
2023-11-04 23:23:09 +0900

    feat(ProfitCalculator): 당첨동계로 수익률을 계산하는 기능 추가

---
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.text.DecimalFormat;
+import lotto.constants.Values;
+
+public class ProfitCalculator {
+    private final WinningStatistics winningStatistics;
+    private final double totalInvestment;
+
+    public ProfitCalculator(WinningStatistics winningStatistics, PurchasedLottos purchasedLottos) {
+        this.winningStatistics = winningStatistics;
+        this.totalInvestment = calculateTotalInvestment(purchasedLottos);
+    }
+
+    private double calculateTotalInvestment(PurchasedLottos purchasedLottos) {
+        return (double)purchasedLottos.getLottos().size() * Values.LOTTO_PURCHASE_UNIT;
+    }
+
+    public String getProfitPercentage() {
+        double profitRate = calculateProfitRate();
+        DecimalFormat decimalFormat = new DecimalFormat("###,###.#");
+        String formattedPercentage = decimalFormat.format(profitRate);
+        formattedPercentage += "%";
+
+        return formattedPercentage;
+    }
+
+    private double calculateProfitRate() {
+        double totalWinnings = winningStatistics.calculateTotalPrizeAmount();
+        double profitRate = (totalWinnings / totalInvestment) * 100.0;
+        profitRate = Math.round(profitRate * 100.0) / 100.0;
+        return profitRate;
+    }
+
+
+}

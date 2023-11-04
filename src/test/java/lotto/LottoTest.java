495e5a96dab99e5238a56b06390ed6eb5374d59a
WooJJam <jaemin5548@naver.com>
2023-11-04 20:36:40 +0900

    [Test] 로또 숫자의 범위를 범어난 경우 검증 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 범위를 벗어난 숫자를 포함하면 예외가 발생한다.")
+    @Test
+    void 범위를_벗어난_숫자_검증() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

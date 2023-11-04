5c3b9ef92d5cc2aa7dc9344222452e6fd911dd69
JH-TT <leejongho9803@gmail.com>
2023-11-05 01:52:25 +0900

    style(outputView): static으로 선언

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,7 @@
 package lotto.view;
 
 public class OutputView {
-    public void buyAmount(final long amount) {
+    public static void buyAmount(final long amount) {
         System.out.println(amount + "개를 구입했습니다.");
     }
 }

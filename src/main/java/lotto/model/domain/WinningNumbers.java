d03cba69fbdb16d030356f7ae513b15a72065080
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-04 17:05:39 +0900

    feat: 당첨번호와 보너스번호를 관리하는 기능 구현

---
+++ src/main/java/lotto/model/domain/WinningNumbers.java
@@ -0,0 +1,27 @@
+package lotto.model.domain;
+
+
+public class WinningNumbers {
+    private final Lotto lotto;
+    private final int bonus;
+
+    public WinningNumbers(Lotto lotto, int bonus) {
+        validate(lotto, bonus);
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+
+    private void validate(Lotto lotto, int bonus) {
+        if (lotto.getNumbers().contains(bonus)) {
+            LottoException.duplicateException();
+        }
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}

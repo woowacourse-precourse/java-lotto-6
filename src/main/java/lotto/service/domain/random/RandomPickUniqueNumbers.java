b03691501c6bb37f879ec90491500f39acdec337
totocm <typesapk8445@gmail.com>
2023-11-04 21:47:42 +0900

    RandomPickUniqueNumbers 1.getAutoUniqueNumbers( int 랜덤 뽑을 개수) 2.getBonusNumber()
    3. 테스트 완료

--- src/main/java/lotto/service/domain/random/RandomPickUniqueNumbers.java
+++ src/main/java/lotto/service/domain/random/RandomPickUniqueNumbers.java
@@ -6,15 +6,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomPickUniqueNumbers{
+    private static List<Integer> autoUniqueNumbers;
     public static List<Integer> getAutoUniqueNumbers(int pickNumber) {
-        List<Integer> autoUniqueNumbers
+        autoUniqueNumbers
           = Randoms.pickUniqueNumbersInRange(1,45,pickNumber);
         Collections.sort(autoUniqueNumbers);
         return autoUniqueNumbers;
     }
-    public static int getBonusNumber(List<Integer> getAutoUniqueNumbers){
-
-        return 0;
+    public static int getBonusNumber(){
+        int missNumber;
+        do{
+           missNumber = Randoms.pickNumberInRange(1,45);
+        }while (autoUniqueNumbers.contains(missNumber));
+        return missNumber;
     }
 
 
--- src/test/java/lotto/RandomPickUniqueNumbersTest.java
+++ src/test/java/lotto/RandomPickUniqueNumbersTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.service.domain.random.RandomPickUniqueNumbers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,7 +13,16 @@ public class RandomPickUniqueNumbersTest {
         List<Integer> list =
         RandomPickUniqueNumbers.getAutoUniqueNumbers(6);
         System.out.println(list);
+        Assertions.assertNotNull(list);
     }
 
-
+    @Test
+    public void randomBonusTest(){
+        List<Integer> lottoNumberSix =
+          RandomPickUniqueNumbers.getAutoUniqueNumbers(6);
+        int bonusNumber = RandomPickUniqueNumbers.getBonusNumber();
+        System.out.println(lottoNumberSix);
+        System.out.println(bonusNumber);
+        Assertions.assertNotEquals(lottoNumberSix,bonusNumber);
+    }
 }

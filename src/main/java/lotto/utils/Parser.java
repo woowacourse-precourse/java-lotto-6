545e4cf0a9a136d082a963ac9ede31f40ecf1b05
kim <hwiyul_g@daum.net>
2023-11-04 22:26:30 +0900

    feat(Parser) : 당첨 번호 파싱 로직 추가

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,5 +1,10 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
+
 public class Parser {
 
     public static int parseBudget(String budget) {
@@ -9,6 +14,14 @@ public class Parser {
             throw new IllegalArgumentException(
                 ExceptionMessage.INVALID_BUDGET_NON_NUMERIC.getMessage());
         }
+    }
+
+    public static Lotto parseToLotto(String input){
+        List<Integer> numbers = Arrays.stream(input.split(","))
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+        return new Lotto(numbers);
     }
 
 

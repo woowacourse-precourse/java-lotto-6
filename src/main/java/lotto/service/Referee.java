7f2050d60d9acc0d39066a8cd2586ba814c6b1b4
devhoya97 <diajbh123@gmail.com>
2023-11-04 22:09:46 +0900

    refactor(lotto): WinningLotto 객체의 역할이 Lotto의 역할과 중복되므로 제거
    대신 Referee가 당첨 번호와 보너스 번호를 모두 관리하도록 리팩토링함

--- src/main/java/lotto/service/Referee.java
+++ src/main/java/lotto/service/Referee.java
@@ -1,15 +1,18 @@
 package lotto.service;
 
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.Lottos;
-import lotto.domain.WinningLotto;
+import lotto.domain.wrapper.LottoNumber;
 
 public class Referee {
-    private WinningLotto winningLotto;
+    private Lotto winningLotto;
+    private LottoNumber bonusNumber;
     private Lottos playerLottos;
 
     public Referee(List<Integer> winningNumbers, int bonusNumber, Lottos playerLottos) {
-        winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        this.winningLotto = new Lotto(winningNumbers);
+        this.bonusNumber = new LottoNumber(bonusNumber);
         this.playerLottos = playerLottos;
     }
 }

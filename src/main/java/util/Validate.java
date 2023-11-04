2dceabc2aee374487447e88d424d16dd65280ada
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-04 20:43:11 +0900

    <feat> : <입력 값에 빈 칸이 있을 시 에러처리>
    모든 입력 값에 빈 칸이 있을 시 에러처리 하는 메서드 추가

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -4,25 +4,32 @@ import domain.WinningLotto;
 
 public class Validate {
     private final Parser parser = new Parser();
-    private static final String NOT_IN_RANGE_ERROR= "[ERROR] 번호는 1~45 이내에 있어야 합니다.";
+    private static final String NOT_IN_RANGE_ERROR= "[ERROR]번호는 1~45 이내에 있어야 합니다.";
     private static final String NOT_NUMBER_ERROR = "[ERROR] 숫자를 입력해야 합니다.";
-    private static final String INCORRECT_BONUS_LENGTH_ERROR = "[ERROR] 보너스 번호의 길이가 올바르지 않습니다.";
-    private static final String NOT_DIVIDED_BY_1000_WON = "[ERROR] 1000원으로 나누어 떨어지지 않는 값입니다.";
-    private static final String INCORRECT_WINNING_LENGTH_ERROR = "[ERROR] 당첨 번호의 길이가 올바르지 않습니다.";
+    private static final String INCORRECT_BONUS_LENGTH_ERROR = "[ERROR]보너스 번호의 길이가 올바르지 않습니다.";
+    private static final String NOT_DIVIDED_BY_1000_WON = "[ERROR]1000원으로 나누어 떨어지지 않는 값입니다.";
+    private static final String INCORRECT_WINNING_LENGTH_ERROR = "[ERROR]당첨 번호의 길이가 올바르지 않습니다.";
+    private static final String CONTAINS_SPACE_ERROR = "[ERROR] 입력에 빈 칸이 포함되어 있습니다.";
+    private static final String SPACE = " ";
+
     public void CheckMoneyInput(String input){ // 로또 구입 금액 체크
+        CheckEmpty(input);
         CheckIsNumeric(input);
         CheckIs1000Divisible(parser.parseInteger(input));
     }
     public void CheckWinningNumber(String input){ // 당첨 번호 체크
         String[] Checkinput = input.split(",");
+
+        CheckEmpty(input);
         CheckWinningNumberLength(Checkinput);
-        
+
         for(int i=0; i< Checkinput.length; i++){
             CheckNumberInRange(parser.parseInteger(Checkinput[i]));
             CheckIsNumeric(Checkinput[i]);
         }
     }
     public void CheckBonusNumber(String input){ // 보너스 번호 체크
+        CheckEmpty(input);
         CheckBonusNumberLength(input);
         CheckIsNumeric(input);
         CheckNumberInRange(parser.parseInteger(input));
@@ -49,6 +56,11 @@ public class Validate {
         }
     }
     // 공통 사용
+    private void CheckEmpty(String input){
+        if(input.contains(SPACE)){
+            throw new IllegalArgumentException(CONTAINS_SPACE_ERROR);
+        }
+    }
     private void CheckNumberInRange(int input) {
         if (input < 1 || input > 45) {
             throw new IllegalArgumentException(NOT_IN_RANGE_ERROR);

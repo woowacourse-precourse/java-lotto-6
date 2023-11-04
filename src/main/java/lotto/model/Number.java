9ca4a3173170a5b0f4fa6129785483e3f34b5060
jeondui <jeondui@gmail.com>
2023-11-04 21:42:21 +0900

    refactor(Number): Enum 상수 사용으로 코드 수정

--- src/main/java/lotto/model/Number.java
+++ src/main/java/lotto/model/Number.java
@@ -1,9 +1,10 @@
 package lotto.model;
 
+import lotto.exception.Constant;
+import lotto.exception.ErrorMessage;
+
 public class Number {
-    private static final int MIN = 1;
-    private static final int MAX = 45;
-    private static final String ERROR_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
     private final int number;
 
     private Number(int number) {
@@ -16,8 +17,8 @@ public class Number {
     }
 
     private static void validateNumber(int number) {
-        if (number > MAX || number < MIN) {
-            throw new IllegalArgumentException(ERROR_NUMBER_RANGE);
+        if ((number < Constant.LOTTO_NUMBER_MIN.getValue()) || (number > Constant.LOTTO_NUMBER_MAX.getValue())) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NUMBER_RANGE.getMessage());
         }
     }
 

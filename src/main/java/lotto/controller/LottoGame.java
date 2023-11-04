22f7b37a4656d95431c878c75f945989db380879
Isaac Han <isaachan233@gmail.com>
2023-11-04 15:10:10 +0900

    feat: 발행한 로또의 수와 로또의 숫자 를 출력하는 메소드 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,22 +1,33 @@
 package lotto.controller;
 
 import lotto.domain.LottoPurchase;
+import lotto.domain.LottoTickets;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoGame implements Game {
 
     private LottoPurchase lottoPurchase;
+    private LottoTickets lottoTickets;
+
     @Override
     public void run() {
         collectPurchaseAmount();
+        printLottoTickets();
     }
 
-    private void collectPurchaseAmount () {
+    private void collectPurchaseAmount() {
         try {
             lottoPurchase = new LottoPurchase(InputView.lottoPurchaseAmountInput());
+            lottoTickets = lottoPurchase.generateLottoTickets();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             collectPurchaseAmount();
         }
+    }
+
+    private void printLottoTickets() {
+        OutputView.printNumberOfPurchasedLottoTickets(lottoPurchase.getNumberOfTickets());
+        OutputView.printPurchasedLottoNumbers(lottoTickets.getLottoTickets());
     }
 }

dc8777ebf830ca18c8d395814a57430585dd1582
pwrwpw <dlgustjr9912@gmail.com>
2023-11-05 02:07:13 +0900

    test: ValidationTest를 통해 정상적인 숫자 문자열이 들어오는지 테스트코드 작성

--- src/test/java/lotto/ValidationTest.java
+++ src/test/java/lotto/ValidationTest.java
@@ -6,17 +6,48 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @DisplayName("Validation 클래스 테스트")
 class ValidationTest {
     @Test
+    @DisplayName("정상적인 숫자 문자열 검증")
     void 문자열_숫자_검증() {
         // 정상적인 경우, 예외가 발생하지 않아야 함
         assertDoesNotThrow(() -> Validation.validateStringToInteger("123"));
     }
 
     @Test
+    @DisplayName("올바르지 않은 문자열 검증")
     void 문자열_숫자_검증_실패() {
         // 예외가 발생해야 하는 경우
         assertThrows(IllegalArgumentException.class, () -> Validation.validateStringToInteger("abc"));
+    }
+
+    @Test
+    @DisplayName("빈 문자열 검증 실패")
+    void 문자열_숫자_검증_실패_빈문자열() {
+        // 예외가 발생해야 하는 경우
+        assertThrows(IllegalArgumentException.class, () -> Validation.validateStringToInteger(""));
+    }
+
+    @Test
+    @DisplayName("공백 문자열 검증 실패")
+    void 문자열_숫자_검증_실패_공백문자열() {
+        // 예외가 발생해야 하는 경우
+        assertThrows(IllegalArgumentException.class, () -> Validation.validateStringToInteger(" "));
+    }
+
+    @Test
+    @DisplayName("문자열과 숫자 혼합 검증 실패")
+    void 문자열_숫자_검증_실패_문자열_숫자_혼합() {
+        // 예외가 발생해야 하는 경우
+        assertThrows(IllegalArgumentException.class, () -> Validation.validateStringToInteger("123a"));
+    }
+
+    @Test
+    @DisplayName("문자열과 숫자 혼합 및 공백 검증 실패")
+    void 문자열_숫자_검증_실패_문자열_숫자_혼합_공백() {
+        // 예외가 발생해야 하는 경우
+        assertThrows(IllegalArgumentException.class, () -> Validation.validateStringToInteger("123 a"));
     }
 }

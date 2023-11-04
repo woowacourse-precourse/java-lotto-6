9e0fefc03eb5d517b708362c3548f732758cdeba
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-11-05 00:43:23 +0900

    feat: 구입 금액 입력, type검사 함수 구현 (InputAmount)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,11 +4,14 @@ import lotto.domain.InputAmount;
 import lotto.view.PrintView;
 
 public class Application {
+    private static InputAmount inputAmount;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        int Buy_Ticket = Input_Lotto_Buy();
     }
 
-
+    public static int Input_Lotto_Buy(){
+        inputAmount = new InputAmount(PrintView.Input_Lotto_Buy());
+    }
 
 }
--- src/main/java/lotto/domain/InputAmount.java
+++ src/main/java/lotto/domain/InputAmount.java
@@ -1,10 +1,21 @@
 package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
+import java.awt.font.NumericShaper;
+
 public class InputAmount {
     private static final int Lotto_Cost = 1000;
     private final int Lottos = 0;
 
-    
+    public InputAmount(String Input_cash) {
+        int Input_first_cash = handlingNumber(Input_cash);
 
+    }
 
+    private static int handlingNumber(String Input_cash) throws IllegalArgumentException{
+        try{
+            return Integer.parseInt(Input_cash);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
 }

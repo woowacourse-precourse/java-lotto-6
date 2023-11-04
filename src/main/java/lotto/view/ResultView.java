9bb1d4686bdfce64e4de91769c814f3c76abbcee
leedabin2 <12193152@inha.edu>
2023-11-04 18:43:42 +0900

    feat: Add printLottoCount method that prints the issued Lotto quantity

---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import lotto.handler.UserHandler;
+
+import java.util.List;
+
+public class ResultView {
+
+    public static void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumber(List<Integer> numbers){
+        System.out.println(numbers);
+    }
+}

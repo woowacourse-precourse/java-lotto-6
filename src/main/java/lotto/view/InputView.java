d8b7c98ff71187c0c4f3317a804c29663f624ac3
jeongjihun <wlgns2234@naver.com>
2023-11-04 15:40:28 +0900

    feat: 사용자에게 입력 메세지를 출력하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,11 +8,13 @@ public class InputView {
     private static final LottoInputView lottoInputView = new ConsoleLottoInputView();
 
     public static int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = lottoInputView.inputPurchaseAmount();
         return Integer.parseInt(purchaseAmount);
     }
 
     public static List<Integer> inputWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         String winningNumber = lottoInputView.inputWinningNumber();
         return Arrays.stream(winningNumber.split(","))
                 .map(Integer::parseInt)
@@ -20,6 +22,7 @@ public class InputView {
     }
 
     public static int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = lottoInputView.inputBonusNumber();
         return Integer.parseInt(bonusNumber);
     }

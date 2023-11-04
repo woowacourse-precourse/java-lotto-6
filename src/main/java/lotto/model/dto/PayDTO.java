d8a66762149ce8db38f4c37e58dbfff7dff883db
chae401 <chae401@sju.ac.kr>
2023-11-04 18:03:02 +0900

    feat(BeforeScreen): add readPayment method

--- src/main/java/lotto/model/dto/PayDTO.java
+++ src/main/java/lotto/model/dto/PayDTO.java
@@ -1,2 +1,48 @@
-package lotto.model.dto;public class PayDTO {
+package lotto.model.dto;
+import static lotto.constants.ExceptionMessages.CATCH_ERROR;
+import static lotto.constants.ExceptionMessages.INPUT_EMPTY;
+import static lotto.constants.ExceptionMessages.INVALID_NUMBER;
+import static lotto.constants.ExceptionMessages.INVALID_PAYMENT;
+
+import lotto.constants.ExceptionMessages;
+public class PayDTO {
+    private static final int PRICE_OF_LOTTO = 1000;
+    private int payment;
+    private PayDTO(int payment){
+        this.payment = payment;
+    }
+
+    public static PayDTO from(String text){
+        int payment = validatePayment(text);
+        return new PayDTO(payment);
+    }
+
+    private static int validatePayment(String text) {
+        checkNull(text);
+        int payment = toInt(text);
+        checkFollowRule(payment);
+        return payment;
+    }
+
+    private static int toInt(String text) {
+        try{
+            return Integer.parseInt(text);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(CATCH_ERROR + INVALID_NUMBER);
+        }
+    }
+
+    private static void checkFollowRule(int payment) {
+       if(isDividedByLottoPrice(payment)){
+           throw new IllegalArgumentException(CATCH_ERROR + INVALID_PAYMENT);
+       }
+    }
+
+    private static boolean isDividedByLottoPrice(int payment){
+        return payment % PRICE_OF_LOTTO == 0;
+    }
+
+    private static void checkNull(String text) {
+        if(text == null || text.isEmpty()) throw new IllegalArgumentException(CATCH_ERROR + INPUT_EMPTY);
+    }
 }
--- src/main/java/lotto/view/BeforeScreen.java
+++ src/main/java/lotto/view/BeforeScreen.java
@@ -1,2 +1,13 @@
-package lotto.view;public class BeforeScreen {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.dto.PayDTO;
+
+public class BeforeScreen {
+    private static final String REQEUST_PAYMENT= "구입금액을 입력해 주세요.";
+    public static PayDTO readPayment(){
+        System.out.println(REQEUST_PAYMENT);
+        String pay = Console.readLine();
+        return PayDTO.from(pay);
+    }
 }

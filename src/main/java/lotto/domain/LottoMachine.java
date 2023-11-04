aca5604106a186ee6d7f24288ed714c20eebe1fb
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 12:50:21 +0900

    [feat] 로또 발행 메서드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,15 +2,35 @@ package lotto.domain;
 
 import lotto.util.NumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
     private final NumberGenerator numberGenerator;
     private final Cash cash;
 
+
     public LottoMachine(NumberGenerator numberGenerator, Cash cash) {
         this.numberGenerator = numberGenerator;
         this.cash = cash;
     }
+
+    public Lottos purchaseLottos() {
+        List<Lotto> purchasedLottos = new ArrayList<>();
+        while (cash.isAfford()) {
+            cash.spend();
+            Lotto lotto = generateLotto();
+            purchasedLottos.add(lotto);
+        }
+
+        return new Lottos(purchasedLottos);
+    }
+
+    private Lotto generateLotto() {
+        List<Integer> numbers = numberGenerator.generateNumbers();
+        return new Lotto(numbers);
+    }
+
+
 
 }

0db64efc8fcfe653368494fad4dd66370f7e2e9b
honi20 <1594cyh@gmail.com>
2023-11-04 17:36:22 +0900

    test: 구입 금액만큼 로또 발행
    - 발행 개수 맞는지 확인
    - 발행 개수만큼 로또 발행했는지 확인

---
+++ src/test/java/lotto/GlobalTest.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GlobalTest {
+
+    @DisplayName("로또 발행 개수 맞는지 확인")
+    @Test
+    void getLottoCount() {
+        Global global = new Global();
+        int money = 5000;
+        int lottoCount = global.getLottoCount(money);
+        int expectedCount = 5;
+
+        assertThat(lottoCount).isEqualTo(expectedCount);
+    }
+
+    @DisplayName("발행 개수만큼 로또 발행했는지 확인")
+    @Test
+    void getIssuedLottoList() {
+        Global global = new Global();
+        List<Lotto> issuedLottoList = global.getIssuedLottoList(3);
+        int listSize = issuedLottoList.size();
+        int expectedSize = 3;
+
+        assertThat(listSize).isEqualTo(expectedSize);
+    }
+}

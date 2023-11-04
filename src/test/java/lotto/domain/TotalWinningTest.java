02740bb3d6184d00dbcb7e269a73b33697a602eb
Isaac Han <isaachan233@gmail.com>
2023-11-04 11:57:46 +0900

    test: 총 수익률 계산 테스트

--- src/test/java/lotto/domain/TotalWinningTest.java
+++ src/test/java/lotto/domain/TotalWinningTest.java
@@ -1,16 +1,29 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TotalWinningTest {
+    private TotalWinning totalWinning;
+    private static final int first = 2_000_000_000;
+    private static final int second = 30_000_000;
+    private static final int third = 1_500_000;
+    private static final int fourth = 50_000;
+    private static final int fifth = 5_000;
+
+    @BeforeEach
     @Test
     void createTotalWinning() {
-        int first = 2_000_000_000;
-        int third = 1_500_000;
-        TotalWinning totalWinning = new TotalWinning(List.of(first, third));
+        totalWinning = new TotalWinning(List.of(fifth));
+    }
+
+    @Test
+    void getTotalReturn() {
+        assertThat(totalWinning.getTotalReturn(new LottoPurchase(8000))).isEqualTo(62.5);
     }
 }

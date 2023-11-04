2044742c0d63a45eb17655d889662b2917288451
minjun7410 <minjun7410@gmail.com>
2023-11-04 20:46:11 +0900

    feat (Lotto): 당첨 번호들이 중복되지 않았는지 예외 처리하는 기능 구현
    * 중복을 검사하는 메서드 validateDuplicatedNumber를 추가했다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -8,6 +10,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -18,6 +21,11 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> kindOfNumber = new HashSet<>(numbers);
+        if (kindOfNumber.size() == numbers.size()) return;
+        throw new IllegalArgumentException("중복되지 않은 로또 숫자를 입력해주세요.");
+    }
 
     @Override
     public String toString() {

92c7a02f8197a8f7aca9588c11a5c08d499d4d62
WooJJam <jaemin5548@naver.com>
2023-11-04 21:17:49 +0900

    [Feat] Lotto 예외처리 및 검증 구현

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -1,2 +1,30 @@
-package lotto.validator;public class LottoNumberValidator {
+package lotto.validator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoNumberValidator {
+
+    public static void lottoValidate(List<Integer> numbers) {
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        boolean hasDuplicate = numbers.stream()
+                .anyMatch(number -> !uniqueNumbers.add(number));
+        if (hasDuplicate) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 포함되었습니다.");
+        }
+    }
+
+    private static void validateRange(List<Integer> numbers) {
+        boolean isRangeValid = numbers.stream()
+                .allMatch(number -> number >= 1 && number <= 45);
+        if (!isRangeValid) {
+            throw new IllegalArgumentException("[ERROR] 로또 범위는 1 ~ 45 입니다.");
+        }
+    }
 }

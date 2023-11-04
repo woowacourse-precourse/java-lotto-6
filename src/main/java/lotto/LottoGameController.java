296e4e945a9b4fbfbec28d383b093a376e28560c
lee jihyeon <lejinhy@gmail.com>
2023-11-04 20:27:04 +0900

    refactor: 입력 예외처리 도메인 변경사항 적용
    - UserInput을 통해 null, 공백 체크 후 파싱

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,21 +1,28 @@
 package lotto;
 
 import lotto.domain.GenerateLotteries;
+import lotto.domain.Lotto;
 import lotto.domain.PurchasePrice;
+import lotto.domain.UserInput;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
+    private UserInput userInput = new UserInput();
     private GenerateLotteries generateLotteries;
     private PurchasePrice purchasePrice;
+    private Lotto lotto;
 
     public void startGame() {
-        purchasePrice = new PurchasePrice(inputView.requestPurchasePrice());
+        String price = inputView.requestPurchasePrice();
+        purchasePrice = new PurchasePrice(userInput.convertToIntegerIfValid(price));
 
         generateLotteries = new GenerateLotteries(purchasePrice.getPurchaseCount());
-
         outputView.printPurchasedLotteries(generateLotteries.getLotteries());
+
+        String numbers = inputView.requestWinningNumbers();
+        lotto = new Lotto(userInput.convertToIntegerListIfValid(numbers));
     }
 }

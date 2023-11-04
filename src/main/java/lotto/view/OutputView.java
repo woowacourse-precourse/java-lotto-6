f868a279c6bd6123020e36b99753b24523fa9e65
gundorit <packdev937@gmail.com>
2023-11-04 23:48:21 +0900

    feat (OutputView) : 당첨 번호를 입력하는 문구를 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.\n";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.\n";
 
     public static void printLottoPurchase() {
         System.out.print(LOTTO_PURCHASE_MESSAGE);
@@ -12,4 +13,9 @@ public class OutputView {
     public static void printLottoCount() {
         System.out.print(LOTTO_COUNT_MESSAGE);
     }
+
+    public static void printWinningNumbersPrompt() {
+        System.out.print(WINNING_NUMBERS_PROMPT);
+    }
+
 }

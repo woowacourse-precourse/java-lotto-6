6e5b60b2c22c3fa4ab90f86e8580a71c8a6b5b21
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-04 13:06:31 +0900

    [feat] view 사용자 값 입력 기능

--- src/main/java/lotto/view/InputValue.java
+++ src/main/java/lotto/view/InputValue.java
@@ -1,5 +1,20 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputValue {
-    public static String
+    public static String inputPurchase() {
+        System.out.println(InputMessage.PURCHASE);
+        return Console.readLine();
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println(InputMessage.WINNING_NUMBERS);
+        return Console.readLine();
+    }
+
+    public static String inputBounusNumber() {
+        System.out.println(InputMessage.BONUS_NUMBER);
+        return Console.readLine();
+    }
 }

47bdfb43711746f85e42c3a7923b0f03aab68b26
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2023-11-05 00:50:36 +0900

    feat: 스켈레톤 코드 작성

---
+++ src/main/java/lotto/domain/LottoService.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+public class LottoService {
+    public void issueLottos(){
+
+    }
+
+    public void checkWinningNumbers(){
+
+    }
+
+    public void calculatePrizeMoney(){
+
+    }
+
+}

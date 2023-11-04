b66f6c026e685283028f09b534119a3f156b0d98
mainfn <_@mainfn.dev>
2023-11-04 18:19:56 +0900

    test: `LottoStoreTest` 테스트

---
+++ src/test/java/lotto/domain/lotto/LottoStoreTest.java
@@ -0,0 +1,31 @@
+package lotto.domain.lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import lotto.domain.money.Money;
+import lotto.domain.money.Wallet;
+import lotto.domain.testutil.FixedLottoGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+final class LottoStoreTest {
+    @DisplayName("buyUntilOutOfMoney는 가진 잔액을 모두 소진할 때까지 로또를 매입한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {1_000, 10_000, 100_000})
+    void buyUntilOutOfMoney_withMoney_shouldSpendAllMoney(final long amount) {
+        // given
+        final LottoGenerator lottoGenerator = new FixedLottoGenerator(List.of(1, 2, 3, 4, 5, 6));
+        final LottoStore lottoStore = new LottoStore(lottoGenerator);
+        final Wallet wallet = Wallet.with(amount);
+
+        // when
+        final Lottos lottos = lottoStore.buyUntilOutOfMoney(wallet);
+
+        // then
+        assertThat(wallet.hasEqualOrMoreThan(Money.zero())).isTrue();
+        assertThat(lottos.size()).isEqualTo(amount / 1_000);
+    }
+
+}

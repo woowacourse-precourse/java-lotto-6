6d9baa53b7c10cc9142a9c05e13fa618ea297509
ddangme <ssilver121@nav>
2023-11-04 21:33:47 +0900

    test: 구입 금액에 맞는 로또 생성 기능 테스트

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,27 @@
+package lotto.service;
+
+import lotto.constants.Value;
+import lotto.domain.Lotto;
+import lotto.utils.RandomUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoServiceTest {
+
+    LottoService lottoService = new LottoService();
+    RandomUtils randomUtils = new RandomUtils();
+
+    @DisplayName("구매 금액에 맞게 로또가 구입되는지 확인하기")
+    @Test
+    void buyLotto() {
+        int lottoAmount = randomUtils.RandomNumber() * Value.LOTTO_TICKET_PRICE;
+        List<Lotto> userLottos = lottoService.createUserLottos(lottoAmount);
+
+        assertEquals(userLottos.size(), lottoAmount/Value.LOTTO_TICKET_PRICE);
+    }
+
+}

b33974bd269450560eedc5f9d005cd9bbf6469b0
jsj <tkdlek4186@naver.com>
2023-11-04 14:49:14 +0900

    feat(Lotto) : 생성자 수정, 급액만큼 Paper생성함수 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,17 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    public List<Paper> papers;
+
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        papers=new ArrayList<>();
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +21,12 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void createPapers(int n,int bonusNumber){
+        for(int i = 0; i<n;i++){
+            Paper paper=new Paper();
+            paper.checkCorrespondNum(numbers,bonusNumber);
+            paper.checkWinMoney();
+            papers.add(paper);
+        }
+    }
 }

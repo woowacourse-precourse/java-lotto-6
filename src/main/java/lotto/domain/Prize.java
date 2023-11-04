01b6022780b15c54143a7a6d890e24aef988c026
yshong1998 <yshong1998@gmail.com>
2023-11-05 04:52:06 +0900

    Feat(Prize): 일치하는 숫자의 개수에 맞는 순위를 반환하는 기능 구현

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -6,7 +6,8 @@ public enum Prize {
     SECOND(5, 30000000),
     THIRD(5,1500000),
     FOURTH(4,50000),
-    FIFTH(3,5000);
+    FIFTH(3,5000),
+    FAIL(0,0);
 
     private final Integer matchCount;
     private final Integer winningAmount;
@@ -14,5 +15,14 @@ public enum Prize {
     Prize(Integer matchCount, Integer winningAmount) {
         this.matchCount = matchCount;
         this.winningAmount = winningAmount;
+    }
+
+    public static Prize getPrizeByMatchCount(int matchCount){
+        for (Prize prize : values()) {
+            if(prize.matchCount == matchCount){
+                return prize;
+            }
+        }
+        return FAIL;
     }
 }

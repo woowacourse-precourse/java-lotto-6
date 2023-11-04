6aac115fbf2e5071d564531fa1267bccb4b95bb7
doha <dohaahi@naver.com>
2023-11-04 21:35:05 +0900

    refactor (Lotto): 로또 개수 상수명 변경 LOTTO_SIZE -> SIZE

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
-    public static final int LOTTO_SIZE = 6;
+    public static final int SIZE = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     private final List<Integer> numbers;

fb11782ceef9f7421f18ef312b1a54936b240ea5
hyo-limilimee <1001lily@naver.com>
2023-11-05 01:38:59 +0900

    feat: make method about validate lotto number count

--- src/main/java/lotto/Validator/LottoNumberValidator.java
+++ src/main/java/lotto/Validator/LottoNumberValidator.java
@@ -7,6 +7,8 @@ public class LottoNumberValidator {
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final String LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 로또 번호를 입력했습니다.";
+    private static final int REQUIRED_NUMBER_COUNT = 6;
+    private static final String LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개 입력해야 합니다.";
 
     public static void validateIsNumberInRange(int number, Set<Integer> existingNumbers) {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
@@ -18,6 +20,12 @@ public class LottoNumberValidator {
 
         if (existingNumbers.contains(number)) {
             throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateLottoNumbersCount(Set<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != REQUIRED_NUMBER_COUNT) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_COUNT_ERROR_MESSAGE);
         }
     }
 }

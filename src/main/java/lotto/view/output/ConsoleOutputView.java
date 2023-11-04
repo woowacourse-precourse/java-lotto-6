c259ab7a2440bc234c82512971acd27be64a169c
minsub <chrkb1569@naver.com>
2023-11-04 15:05:09 +0900

    feat : 사용자로에게 금액 입력 메세지를 출력한다.

--- src/main/java/lotto/view/output/ConsoleOutputView.java
+++ src/main/java/lotto/view/output/ConsoleOutputView.java
@@ -3,6 +3,6 @@ package lotto.view.output;
 public class ConsoleOutputView implements OutputView {
     @Override
     public void printCostMessage() {
-
+        System.out.println("구입금액을 입력해 주세요.");
     }
 }

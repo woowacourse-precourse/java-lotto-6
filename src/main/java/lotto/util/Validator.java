bfd4fa43b74e90c1bd8fc0d1b518a23a28fe1ccd
shin5774 <babo5774@naver.com>
2023-11-04 15:50:03 +0900

    Refactor: 로또 구매금액 검증 기능 보완
    - 0원을 예외처리하기 위해 값이 0인지 확인하는 기능 추가.

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -7,38 +7,48 @@ import java.util.regex.Pattern;
 
 public enum Validator {
     INSTANCE;
-    private static final int NUMBER_LOW_BOUND=1;
-    private static final int NUMBER_HIGH_BOUND=45;
+    private static final int NUMBER_LOW_BOUND = 1;
+    private static final int NUMBER_HIGH_BOUND = 45;
 
-    private static final Pattern NUMBER=Pattern.compile("[0-9]+");
-    private static final String ERROR_MESSAGE_HEADER="[ERROR] ";
-    private static final String NOT_INTEGER_MESSAGE="숫자가 아닌 값이 입력되었습니다.";
-    private static final String NOT_DIVIDED_MESSAGE="구매값이 1000원으로 나눠지지 않습니다.";
-    public void numberValidate(String value){
-        if(isNotConsistOfNumber(value)){
-            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER+NOT_INTEGER_MESSAGE);
+    private static final Pattern NUMBER = Pattern.compile("[0-9]+");
+    private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
+    private static final String NOT_INTEGER_MESSAGE = "숫자가 아닌 값이 입력되었습니다.";
+    private static final String NOT_DIVIDED_MESSAGE = "구매값이 1000원으로 나눠지지 않습니다.";
+
+    public void numberValidate(String value) {
+        if (isNotConsistOfNumber(value)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + NOT_INTEGER_MESSAGE);
         }
     }
-    private boolean isNotConsistOfNumber(String value){
+
+    private boolean isNotConsistOfNumber(String value) {
         return !NUMBER.matcher(value).matches();
     }
 
-    public void buyCashValidate(long buyCash){
-        if(isNotDividedByUnit(buyCash)){
-            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER+NOT_DIVIDED_MESSAGE);
+    public void buyCashValidate(long buyCash) {
+        if (isNotDividedByUnit(buyCash)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + NOT_DIVIDED_MESSAGE);
+        }
+        if (isZero(buyCash)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + NOT_DIVIDED_MESSAGE);
         }
     }
 
-    private boolean isNotDividedByUnit(long buyCash){
-        return buyCash%BUY_UNIT!=0;
+    private boolean isNotDividedByUnit(long buyCash) {
+        return buyCash % BUY_UNIT != 0;
     }
 
-    public void validateOutOfRange(List<Integer> numbers){
-        List<Integer> outOfRangeNumbers=numbers.stream()
-                .filter((number)->number<NUMBER_LOW_BOUND || number>NUMBER_HIGH_BOUND)
+    private boolean isZero(long buyCash) {
+        return buyCash == 0;
+
+    }
+
+    public void validateOutOfRange(List<Integer> numbers) {
+        List<Integer> outOfRangeNumbers = numbers.stream()
+                .filter((number) -> number < NUMBER_LOW_BOUND || number > NUMBER_HIGH_BOUND)
                 .toList();
 
-        if (!outOfRangeNumbers.isEmpty()){
+        if (!outOfRangeNumbers.isEmpty()) {
             throw new IllegalArgumentException();
         }
     }

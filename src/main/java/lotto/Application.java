eecedf93f25aaa8d2d56e0c622f7537cae216896
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-11-04 22:38:28 +0900

    feat: main함수에 입력받는 내용까지 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,7 +9,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = inputPurchaseAmount();
+        List<Lotto> lottos = generateLottos(purchaseAmount / 1000);
+
+        Lotto winningLotto = new Lotto(inputWinningNumbers());
+        int bonusNumber = inputBonusNumber();
+
     }
     private static int inputPurchaseAmount(){
         System.out.println("구입금액을 입력해 주세요");

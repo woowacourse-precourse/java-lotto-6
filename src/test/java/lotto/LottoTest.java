14bc927d8ae0457db5f9f024700e4171fd2af7e7
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-04 22:16:25 +0900

    test: Lotto 객체를 더 구체적으로 테스트하도록 테스트 메서드 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -13,7 +13,8 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자 6개를 모두 입력해주세요.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -21,13 +22,15 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복이 되지 않는 숫자로 입력");
     }
 
     @DisplayName("로또 번호가 1 ~ 45가 아니라면 예외를 뱉는다.")
     @Test
     void issueLargeNumberLotto() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 99)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("각 숫자는 1~45 사이여야 합니다.");
     }
 }

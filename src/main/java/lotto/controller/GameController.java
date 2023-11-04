86b8315076e54d24a79ba9d9363d3a5f3b127a9e
GS_song <20003204@sju.ac.kr>
2023-11-05 02:51:33 +0900

    feat: GameController 실행 순서 정의

---
+++ src/main/java/lotto/controller/GameController.java
@@ -0,0 +1,35 @@
+package lotto.controller;
+
+import lotto.model.User;
+import lotto.model.WinningLotto;
+
+public class GameController {
+
+    private User user;
+    private WinningLotto winningLotto;
+
+    public void startGame() {
+        setUserLotto();
+        printUserLottos();
+        setWinningLotto();
+        calculateLottoStatistics();
+        printLottoStatistics();
+    }
+
+    private void setUserLotto() {
+    }
+
+    private void printUserLottos() {
+    }
+
+    private void setWinningLotto() {
+    }
+
+    private void calculateLottoStatistics() {
+    }
+
+    private void printLottoStatistics() {
+
+    }
+
+}

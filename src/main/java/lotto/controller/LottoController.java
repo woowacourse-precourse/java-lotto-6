3fc42da8a640d283cf9891d513c7142f956a7e3a
wodnj5 <jw991211@gmail.com>
2023-11-04 22:21:26 +0900

    feat: LottoController 클래스 돈, 당첨 번호, 보너스 번호 입력 기능 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,64 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.LottoGenerator;
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
+import lotto.model.Money;
+import lotto.view.Input;
+import lotto.view.Output;
+
+public class LottoController {
+
+    private Money money;
+    private List<Lotto> lottos;
+    private Lotto winningNumbers;
+    private BonusNumber bonusNumber;
+
+    public void start() {
+        buyLottos();
+        getWinnerNumbers();
+        getBonusNumber();
+    }
+
+    private void buyLottos() {
+        payMoney();
+        generateLottos();
+    }
+
+    private void payMoney() {
+        try {
+            money = new Money(Input.inputMoney());
+            Output.printLottosCount(money);
+        } catch (IllegalArgumentException e) {
+            payMoney();
+        }
+    }
+
+    private void generateLottos() {
+        lottos = new ArrayList<>();
+        for(int i = 0 ; i < money.getCount() ; i++) {
+            Lotto lotto = LottoGenerator.generateLotto();
+            lottos.add(lotto);
+            Output.printLottos(lotto);
+        }
+    }
+
+    private void getWinnerNumbers() {
+        try {
+            winningNumbers = new Lotto(Input.inputWinningNumbers());
+        } catch (IllegalArgumentException e) {
+            getWinnerNumbers();
+        }
+    }
+
+    private void getBonusNumber() {
+        bonusNumber = new BonusNumber(Input.inputBonusNumber());
+        try {
+            winningNumbers.validateBonusNumber(bonusNumber);
+        } catch(IllegalArgumentException e) {
+            getBonusNumber();
+        }
+    }
+}

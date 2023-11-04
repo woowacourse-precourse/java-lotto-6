525d86026e18668feef73a40270f158dbbb29a78
wodnj5 <jw991211@gmail.com>
2023-11-04 19:27:55 +0900

    feat: BonusNumber 클래스 추가

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class BonusNumber {
+
+    private int value;
+
+    public BonusNumber(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

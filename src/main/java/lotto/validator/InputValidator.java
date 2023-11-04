5a49f9e63497aeb13532da96927bf65926461567
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-04 19:05:38 +0900

    feat(InputValidator): 입력된 구입 금액이 숫자인지 검증하는 기능 구현

---
+++ src/main/java/lotto/validator/InputValidator.java
@@ -0,0 +1,13 @@
+package lotto.validator;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    private static final String DIGIT_REGEX = "^[0-9]+$";
+
+    public void validateIsDigit(String number) {
+        if (!Pattern.matches(DIGIT_REGEX, number)) {
+            throw new IllegalArgumentException(ExceptionMessage.IS_NOT_DIGIT.toString());
+        }
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,8 +5,11 @@ import lotto.domain.Money;
 import lotto.validator.InputValidator;
 
 public class InputView {
+    private final InputValidator validator = new InputValidator();
+
     public Money readMoneyToBuyLotto() {
         String input = Console.readLine();
+        validator.validateIsDigit(input);
         return new Money(Integer.parseInt(input));
     }
 }

7573e03343131d17e939f00068037a321da5eb59
jongmee <ra123221@gmail.com>
2023-11-05 02:23:21 +0900

    feat(PurchaseNumber): 구입한 로또 금액이 음수일 경우 예외 처리하기

--- src/main/java/lotto/domain/PurchaseNumber.java
+++ src/main/java/lotto/domain/PurchaseNumber.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static lotto.constants.ErrorMessage.INVALID_PURCHASE_AMOUNT;
+import static lotto.constants.ErrorMessage.NEGATIVE_INTEGER;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +12,18 @@ public class PurchaseNumber {
     private final Integer number;
 
     public PurchaseNumber(Integer purchasePrice) {
-        validate(purchasePrice);
+        validateUnit(purchasePrice);
+        validateRange(purchasePrice);
         this.number = convertToPurchaseNumber(purchasePrice);
     }
 
-    private void validate(Integer purchasePrice) {
+    private void validateRange(Integer purchasePrice) {
+        if(purchasePrice.compareTo(0) < 0) {
+            throw new IllegalArgumentException(NEGATIVE_INTEGER);
+        }
+    }
+
+    private void validateUnit(Integer purchasePrice) {
         if(purchasePrice % MONEY_UNIT != 0) {
             throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT);
         }

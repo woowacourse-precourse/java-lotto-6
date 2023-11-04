482462c6774465a1c1fa3e74025743c2166d7a04
sangchu <didi1484@gmail.com>
2023-11-04 22:35:52 +0900

    fix: askPurchase 메서드 수정 (잘못된 입력시 예외 발생 및 정수 반환)

--- src/main/java/lotto/constants/ExceptionMessage.java
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -4,7 +4,8 @@ public enum ExceptionMessage {
     BE_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     HAS_LETTER("[ERROR] 문자를 포함할 수 없습니다."),
     IS_DUPLICATED("[ERROR] 중복된 숫자를 포함할 수 없습니다."),
-    IS_INSUFFICIENT("[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다.");
+    IS_INSUFFICIENT("[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다."),
+    IS_NOT_THOUSAND_UNIT("[ERROR] 입력하신 금액은 천원 단위가 아닙니다.");
 
     private final String message;
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,33 @@
 package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.constants.ExceptionMessage.HAS_LETTER;
+import static lotto.constants.ExceptionMessage.IS_NOT_THOUSAND_UNIT;
 
 public class InputView {
-    public static String askPurchaseAmount() {
+    public static int askPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return readLine();
+        String input = readLine();
+        validateType(input);
+        int won = Integer.parseInt(input);
+        validateThousandUnit(won);
+        return won;
+    }
+
+    private static void validateType(String input) {
+        if (hasLetter(input)) {
+            throw new IllegalArgumentException(HAS_LETTER.getMessage());
+        }
+    }
+
+    private static boolean hasLetter(String input) {
+        return input.chars()
+                .anyMatch(Character::isLetter);
+    }
+
+    private static void validateThousandUnit(int won) {
+        if (won % 1000 != 0) {
+            throw new IllegalArgumentException(IS_NOT_THOUSAND_UNIT.getMessage());
+        }
     }
 }

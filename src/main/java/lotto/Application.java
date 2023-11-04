282e5816033ab20b0a9fc3d1ec2c37de47659440
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 20:35:44 +0900

    refactor: controller 통해 프로그램이 실행되게 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }

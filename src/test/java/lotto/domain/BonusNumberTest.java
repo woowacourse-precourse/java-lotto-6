96e41a0f041c5b054152703d34179d266c35d14b
kimwanyoung <dhks2869@gmail.com>
2023-11-05 00:24:27 +0900

    test : 보너스 번호 포함하는지 판별 테스트

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class BonusNumberTest {
@@ -16,4 +20,29 @@ public class BonusNumberTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1 ~ 45사이의");
     }
+
+    @ParameterizedTest(name = "로또번호 : {0}, 보너스번호 : {1}, 기대 값 : {2}")
+    @MethodSource("generateLottoAndBonusNumber")
+    @DisplayName("로또에 보너스 번호 포함하는지 판별한다.")
+    void 보너스_번호_포함_테스트(List<Integer> lottoNumber, int bonus, boolean expected) {
+        //given
+        Lotto lotto = new Lotto(lottoNumber);
+        BonusNumber bonusNumber = new BonusNumber(bonus);
+
+        //when
+        boolean actual = bonusNumber.hasBonus(lotto);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> generateLottoAndBonusNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6, true),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 15, false),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 4, true),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 23, false)
+        );
+    }
+
 }

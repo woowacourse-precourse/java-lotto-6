6084733dfa6cc5238d33728430faf9f2f5940d9f
minjun7410 <minjun7410@gmail.com>
2023-11-04 20:37:55 +0900

    test(WinningNumberTest): 1-45 범위의 숫자 예외처리 테스트 추가
    * 1-45 범위를 벗어난 숫자라면 예외처리하는 테스트를 추가했다.
    * 입력값이 숫자인지 예외처리하는 테스트를 추가했다.

---
+++ src/test/java/lotto/WinningNumberTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import lotto.domain.WinningNumber;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningNumberTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"1 d 3 4 5 _", "10.1 3 2"})
+    void 당첨번호_숫자가_아닌_입력_예외처리_테스트(String input) {
+        List<String> inputs = Arrays.asList(input.split(" "));
+        assertThatThrownBy(() -> new WinningNumber(inputs)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10 50 100", "0 10"})
+    void 당첨번호_범위에_벗어난_입력_예외처리_테스트(String input) {
+        List<String> inputs = Arrays.asList(input.split(" "));
+        assertThatThrownBy(() -> new WinningNumber(inputs)).isInstanceOf(IllegalArgumentException.class);
+    }
+}

67e31658c34a509a83b14d58e0f48e8f7e5983d5
leedabin2 <12193152@inha.edu>
2023-11-04 16:38:35 +0900

    feat: Add Main Method Execution Code

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.handler.UserHandler;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        UserHandler.getAmountFromUser();
+
     }
 }

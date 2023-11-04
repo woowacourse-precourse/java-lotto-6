4de11894008e4b5ce5fddc6ed319b56ce37f7358
Minjae-An <mj3242@naver.com>
2023-11-04 21:54:36 +0900

    feat : 고정 당첨금 계산 기능 구현

---
+++ src/main/java/lotto/domain/prize_calculator/FixedPrizeCalculator.java
@@ -0,0 +1,21 @@
+package lotto.domain.prize_calculator;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class FixedPrizeCalculator implements PrizeCalculator {
+    private HashMap<Long, Integer> matchCountPrizeMap;
+
+    public FixedPrizeCalculator(){
+        matchCountPrizeMap=new HashMap<>();
+
+        Arrays.stream(FixedLottoPrize.values())
+                .forEach(fixedLottoPrize ->
+                        matchCountPrizeMap.put(fixedLottoPrize.getMatchCount(), fixedLottoPrize.getPrize()));
+    }
+
+    @Override
+    public Integer calculatePrize(Long matchCount) {
+        return matchCountPrizeMap.getOrDefault(matchCount, 0);
+    }
+}

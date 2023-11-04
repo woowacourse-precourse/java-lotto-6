30f57e071144bb07c59300affc68cced92e4e868
koomchang <leahpar0401@gmail.com>
2023-11-04 19:39:53 +0900

    feat: 보너스 볼 입력에 대한 검증 클래스  생성

---
+++ src/main/java/lotto/validator/BonusNumberInputValidator.java
@@ -0,0 +1,30 @@
+package lotto.validator;
+
+public class BonusNumberInputValidator implements BasicValidator<String> {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final String INPUT_IS_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
+    private static final String INPUT_IS_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 입력값이 1과 45 사이여야 합니다.";
+
+    @Override
+    public void validate(String input) {
+        isNumeric(input);
+        isBetweenOneAndFortyFive(input);
+    }
+
+    private void isNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_NUMERIC);
+        }
+    }
+
+    private void isBetweenOneAndFortyFive(String input) {
+        int bonusNumber = Integer.parseInt(input);
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_BETWEEN_ONE_AND_FORTY_FIVE);
+        }
+    }
+}

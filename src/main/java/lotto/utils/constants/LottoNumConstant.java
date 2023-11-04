7c415090a9aaf87cd45c6b536fb15b0f5f5ce572
ohjuyoungg <ejrm12@naver.com>
2023-11-04 22:50:46 +0900

    feat: 로또 번호 enum 클래스 구현

---
+++ src/main/java/lotto/utils/constants/LottoNumConstant.java
@@ -0,0 +1,18 @@
+package lotto.utils.constants;
+
+public enum LottoNumConstant {
+    LOTTO_MIN_NUM(1),
+    LOTTO_MAX_NUM(45),
+    LOTTO_SIZE(6),
+    LOTTO_AMOUNT_PURCHASE(1000);
+
+    private final int number;
+
+    LottoNumConstant(final int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}

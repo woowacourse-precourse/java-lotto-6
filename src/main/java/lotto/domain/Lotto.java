0a944066e48e1dd899ecd51b8d5975ccfd673291
SeYeol00 <dmot0126@gmail.com>
2023-11-04 22:26:21 +0900

    feat: 로또 생성 클래스 구현
    로또 생성 클래스
    - 횟수에 맞는 여러 가지 로또를 가진 lottos를 생성하여 아웃풋에 넘겨준다.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private static final int SIX = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (isNotRightCount(numbers)) {
+            throw new IllegalArgumentException("여섯 개의 숫자가 생성되지 않았습니다.");
+        }
+    }
+
+    private static boolean isNotRightCount(List<Integer> numbers) {
+        return numbers.size() != SIX;
+    }
+
+    // TODO: 추가 기능 구현
+}

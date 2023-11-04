984cee499a44330e62453769c5c2088ec9aedd42
KimDabomi <cdabomi@nate.com>
2023-11-05 01:51:55 +0900

    feat: BonusNumber exceptions

--- src/main/java/lotto/exception/InputViewException.java
+++ src/main/java/lotto/exception/InputViewException.java
@@ -2,6 +2,12 @@ package lotto.exception;
 
 import static lotto.model.MagicVariable.NONE_PURCHASE_PRICE;
 import static lotto.model.MagicVariable.LOTTO_PRICE;
+import static lotto.model.MagicVariable.NUMBERS_MIN_RANGE;
+import static lotto.model.MagicVariable.NUMBERS_MAX_RANGE;
+
+import java.text.ParseException;
+import java.util.List;
+import lotto.model.Lotto;
 
 
 public class InputViewException extends IllegalArgumentException {
@@ -21,6 +27,29 @@ public class InputViewException extends IllegalArgumentException {
         if (!userAmount.matches("^[0-9]+$")) {
             String LOTTO_PRICE_TYPE_ERROR_MESSAGE = "[ERROR] 구입 금액은 양수여야 합니다.";
             throw new InputViewException(LOTTO_PRICE_TYPE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkBonusTypeException(String userBonusNumber) {
+        if (!userBonusNumber.matches("^[0-9]+$")) {
+            String BONUS_NUMBER_TYPE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 숫자여야 합니다.";
+            throw new InputViewException(BONUS_NUMBER_TYPE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkBonusNumberRangeException(String userBonusNumber) {
+        int bonusNumber = Integer.parseInt(userBonusNumber);
+
+        if (bonusNumber < NUMBERS_MIN_RANGE.getNumber() || bonusNumber > NUMBERS_MAX_RANGE.getNumber()) {
+            String BONUS_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 " + NUMBERS_MIN_RANGE.getNumber() + "에서 " + NUMBERS_MAX_RANGE.getNumber() + " 범위 내의 숫자여야 합니다.";
+            throw new InputViewException(BONUS_NUMBER_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void checkBonusNumberDuplicationException(List<Integer> winningLotto, int bonusNumber) {
+        if (winningLotto.contains(bonusNumber)) {
+            String BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 중복되지 않는 수만 가능합니다.";
+            throw new InputViewException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
         }
     }
 }

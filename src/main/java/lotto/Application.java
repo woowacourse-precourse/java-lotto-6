e5fe291bd8f3b2b08dfc72e6cb75e6f260997d60
SeongIl <biuld1234@gmail.com>
2023-11-04 13:23:01 +0900

    refactor: create 당첨 결과 출력 메서드

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -131,6 +131,17 @@ public class Application {
 
     }
 
+    public static void printResult(List<Integer> lottoResult, Double earningRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + lottoResult.get(4) + "개");
+        System.out.println("4개 일치 (50,000원) - " + lottoResult.get(3) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.get(2) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.get(1) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.get(0) + "개");
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", earningRate);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -162,17 +173,12 @@ public class Application {
         Double earningRate = calculateEarningRate(lottoResult, prizeMoney, user.lottoCount);
 
         // 9. 당첨 결과 출력
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + lottoResult.get(4) + "개");
-        System.out.println("4개 일치 (50,000원) - " + lottoResult.get(3) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.get(2) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.get(1) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.get(0) + "개");
+        printResult(lottoResult, earningRate);
 
-        // 10. 수익률 출력
 
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", earningRate);
+
+
 
     }
+
 }

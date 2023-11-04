e208d99dd621386637e9b0761f1122b72538982f
Yang-soeun <soeun8636@naver.com>
2023-11-04 20:51:20 +0900

    [feat]: 로또가 몇개 일치하는지 구하는 메소드 구현

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -3,7 +3,6 @@ package lotto.constant;
 public enum LottoConstant {
     MIN_NUMBER(1),
     MAX_NUMBER(45),
-
     ALLOW_DUPLICATE_NUMBER_COUNT(1),
     LOTTO_SIZE(6);
 
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static java.util.Collections.nCopies;
+import static lotto.constant.LottoConstant.LOTTO_SIZE;
+
+import java.util.ArrayList;
 import java.util.List;
 import lotto.exception.InvalidInput;
 
@@ -11,6 +15,21 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> winningStatistics(int bonusNumber, List<List<Integer>> myLottoNumbers) {
+        int initSzie = LOTTO_SIZE.getValue();
+        int initNum = 0;
+
+        List<Integer> statistics = new ArrayList<>(nCopies(initSzie, initNum));
+
+        for (List<Integer> myLottoNumber : myLottoNumbers) {
+            int sameNumberScore = getSameNumberScore(bonusNumber, myLottoNumber);
+            int index = sameNumberScore - 2;
+            statistics.set(index, statistics.get(index) + 1);
+        }
+
+        return statistics;
+    }
+
     private void validate(List<Integer> numbers) {
         InvalidInput invalidInput = new InvalidInput();
 
@@ -19,6 +38,25 @@ public class Lotto {
         invalidInput.outOfRangeException(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    private int getSameNumberScore(int bonusNumber, List<Integer> myLottoNumber) {
+        int sameNumberCount = (int) myLottoNumber.stream()
+                .filter(numbers::contains)
+                .count();
+
+        int score = sameNumberCount;
+
+        if (hasBonusNumber(bonusNumber, myLottoNumber) && sameNumberCount == 5 || sameNumberCount == 6) {
+            score++;
+        }
+        return score;
+    }
+
+    private Boolean hasBonusNumber(int bonusNumber, List<Integer> myLottoNumber) {
+        if (myLottoNumber.contains(bonusNumber)) {//보너스 볼 일치
+            return true;
+        }
+
+        return false;
+    }
 
 }

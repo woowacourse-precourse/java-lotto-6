4f0dde9dea6fe39fdc92f6ecaeb4fa8593ee1607
msung99 <msung6924@naver.com>
2023-11-04 18:41:28 +0900

    feat: 생성된 로또의 번호가 1~45 사이의 숫자가 아닌 경우에 대한 예외처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,15 +7,26 @@ import java.util.stream.Collectors;
 public class Lotto {
     private final List<Integer> numbers;
     private static final String SPLIT_CHAR = ", ";
+    private static final int LOTTO_SIZE = 6;
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+    private static final String OUT_OF_RANGE_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION_MESSAGE);
         }
     }
 

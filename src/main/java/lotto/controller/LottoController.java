75b7fdb28789d7c8a87b225d35bde5d4900b96b8
leedabin2 <12193152@inha.edu>
2023-11-04 18:48:19 +0900

    feat(lottoGenerate): Add an ascending sort of lottoGenerate() method

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,7 +3,10 @@ package lotto.controller;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 import lotto.handler.UserHandler;
+import lotto.view.ResultView;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoController {
@@ -12,14 +15,17 @@ public class LottoController {
     int lottoCount = UserHandler.getLottoCountFromUser(amount);
 
     public void buyLottos() {
+        ResultView.printLottoCount(lottoCount);
+
         for (int i = 0; i < lottoCount; i++) {
             Lotto lotto = lottoGenerate();
-            System.out.println(lotto.getNumbers());
+            ResultView.printLottoNumber(lotto.getNumbers());
         }
     }
 
     private Lotto lottoGenerate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         return new Lotto(numbers);
     }
 }

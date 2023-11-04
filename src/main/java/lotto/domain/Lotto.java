09fc9865c68556b9e5187a1b025045bf00b2aefa
KangGeonyoung <kky6335@gmail.com>
2023-11-04 19:26:33 +0900

    feat(Lotto): 로또 번호 6개 생성 - generateLottoNumber(): 1-45 사이의 중복이 없는 로또 번호 6개 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +18,8 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static List<Integer> generateLottoNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }

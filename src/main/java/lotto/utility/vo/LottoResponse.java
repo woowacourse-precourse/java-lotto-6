2c46d664fc25f5e47fdae14a8ff575a0b83a6350
jongmee <ra123221@gmail.com>
2023-11-05 01:08:15 +0900

    feat(LottoResponse): 발행된 로또를 출력할 때 쓰는 Value Object 생성하기
    - WHY: 출력 메세지 생성은 로또 도메인의 주요 관심사가 아니므로 VO로 빼서 관리한다.

---
+++ src/main/java/lotto/utility/vo/LottoResponse.java
@@ -0,0 +1,31 @@
+package lotto.utility.vo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoResponse {
+    private static final String DELIMITER = ", ";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_BRACKET = "]";
+
+    private final List<String> lottoNumbers;
+
+    public LottoResponse(List<Integer> lottoNumbers) {
+        this.lottoNumbers = convertToString(lottoNumbers);
+    }
+
+    private List<String> convertToString(List<Integer> lottoNumbers) {
+        return lottoNumbers.stream()
+            .map(Object::toString)
+            .collect(Collectors.toList());
+    }
+
+    public String createMessage() {
+        String numbers = String.join(DELIMITER, lottoNumbers);
+        StringBuilder builder = new StringBuilder();
+        return builder.append(OPEN_BRACKET)
+            .append(numbers)
+            .append(CLOSE_BRACKET)
+            .toString();
+    }
+}

59b28aa201eecbf874509e42202f538c70116ef7
gundorit <packdev937@gmail.com>
2023-11-04 23:36:00 +0900

    feat (OutputView) : 구매한 로또의 개수에 대한 문구를 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,8 +3,13 @@ package lotto.view;
 public class OutputView {
 
     private static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.\n";
+    private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
 
     public static void printLottoPurchase() {
         System.out.print(LOTTO_PURCHASE_MESSAGE);
+    }
+
+    public static void printLottoCount() {
+        System.out.print(LOTTO_COUNT_MESSAGE);
     }
 }

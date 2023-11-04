f5280ece41c78dc2725d988ad82b648879c3f98c
hongseokhodev <hongseokhodev@gmail.com>
2023-11-04 11:20:12 +0900

    test: winningLotto와 bonus를 전역 변수에서 assertLottoRank 메소드의 지역 변수로 변경

---
+++ src/test/java/lotto/ServiceTest.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ServiceTest {
+    @DisplayName("당첨 내역을 구한다.")
+    @Test
+    void calculateRankList() {
+
+    }
+}

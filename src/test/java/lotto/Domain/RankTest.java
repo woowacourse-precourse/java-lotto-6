8521133c36c23157c0774f06ad0976b9593414dd
Firedrago95 <yonghwa95@naver.com>
2023-11-04 16:47:46 +0900

    test(Rank) : 등수구하기 테스트 코드 작성

---
+++ src/test/java/lotto/Domain/RankTest.java
@@ -0,0 +1,18 @@
+package lotto.Domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RankTest {
+
+    @Test
+    @DisplayName("등수 반환 코드 테스트")
+    public void getRankTest() {
+        Rank rank = Rank.getRank(5,true);
+        Rank expectedRank = Rank.SECOND;
+
+        assertThat(rank).isEqualTo(expectedRank);
+    }
+}

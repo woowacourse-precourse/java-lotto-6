95ad6b94b4e221c94630a6d356cc0a0a9dec9229
김관진 <cozyschooll@gmail.com>
2023-11-05 01:35:32 +0900

    feat: 보너스 번호 입력 안내 문구 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import static lotto.utils.GameMessage.REQUEST_AMOUNT;
+import static lotto.utils.GameMessage.REQUEST_BONUS_NUMBER;
 import static lotto.utils.GameMessage.REQUEST_WIN_NUMBERS;
 
 public class OutputView {
@@ -11,5 +12,9 @@ public class OutputView {
 
     public void printRequestWinNumbers() {
         System.out.println(REQUEST_WIN_NUMBERS.getMessage());
+    }
+
+    public void printRequestBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER.getMessage());
     }
 }

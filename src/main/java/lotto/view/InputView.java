515aa512f2e7a7cdff5f75a1367671a8043e447c
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 20:24:04 +0900

    feat(requestWinningNumbers) : 당첨번호 입력 요구 문구를 출력하고 입력받는 기능추가
    docs : 당첨번호문구 출력 및 입력받는 부분 체크완료

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String MESSAGE_REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_REQUEST_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public String requestPurchaseAmount() {
         System.out.println(MESSAGE_REQUEST_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public String requestWinningNumbers() {
+        System.out.println(MESSAGE_REQUEST_WINNING_NUMBERS);
         return Console.readLine();
     }
 }

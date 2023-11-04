c20913424ffed732f523791623d5b7ad73ad91ab
PgmJun <chltmdwns96@gmail.com>
2023-11-04 23:31:18 +0900

    fix: 예외 발생 시, 메시지 출력하고 재입력받도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,11 +5,23 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
-    public static void main(String[] args) {
-        OutputView outputView = new OutputView();
-        InputView inputView = new InputView(outputView);
+    static OutputView outputView = new OutputView();
+    static InputView inputView = new InputView(outputView);
 
+    public static void main(String[] args) {
+
+        // money 입력 기능
+        Money money;
+        try {
+            money = inputMoneyAmount();
+        } catch (IllegalArgumentException error) {
+            System.out.println(error.getMessage());
+            money = inputMoneyAmount();
+        }
+    }
+
+    private static Money inputMoneyAmount() {
         int amount = inputView.readLottoPurchaseMoney();
-        Money money = new Money(amount);
+        return new Money(amount);
     }
 }

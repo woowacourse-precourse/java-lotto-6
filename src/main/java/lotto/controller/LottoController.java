4e65d1083363bc0f539c947713f1c7826db30e14
kimwanyoung <dhks2869@gmail.com>
2023-11-04 16:32:42 +0900

    feat : LottoController 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,30 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.BuyAmount;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumberGenerator;
+import lotto.domain.Lottos;
+import lotto.domain.LottosDTO;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    public void buyLotto() {
+        BuyAmount buyAmount = InputView.getBuyAmountFromInput();
+        Lottos lottos = createLottosFromAmount(buyAmount);
+        LottosDTO lottosDTO = lottos.toLottosDTO();
+        OutputView.displayAllLottos(lottosDTO);
+    }
+
+    private Lottos createLottosFromAmount(BuyAmount buyAmount) {
+        List<Lotto> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < buyAmount.getAbleToBuyCount(); i++) {
+            Lotto lotto = new Lotto(LottoNumberGenerator.generateNumber());
+            lottoNumbers.add(lotto);
+        }
+        return new Lottos(lottoNumbers);
+    }
+}

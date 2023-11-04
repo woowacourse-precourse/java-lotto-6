27a7c504a671bee98ebbc3c5508fdc939259513e
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-05 01:09:21 +0900

    feat - Ranking 클래스 구현

---
+++ src/main/java/lotto/domain/Ranking.java
@@ -0,0 +1,61 @@
+package lotto.domain;
+
+public enum Ranking {
+    FIFTH(3, "3개 일치 (5,000원)", 0, 5000),
+
+    FOURTH(4, "4개 일치 (50,000원)", 0, 50000),
+
+    THIRD(5, "5개 일치 (1,500,000원)", 0, 1500000),
+    SECOND(-1, "5개 일치, 보너스 볼 일치 (30,000,000)원", 0, 30000000),
+
+    FIRST(6, "6개 일치 (2,000,000,000원)", 0, 2000000000);
+
+    private final int rank;
+    private final String message;
+    private final int money;
+    private int count;
+
+    Ranking(int rank, String message, int count, int money) {
+        this.rank = rank;
+        this.message = message;
+        this.count = count;
+        this.money = money;
+    }
+
+    public static void increaseRankingCount(int count) {
+        for (Ranking ranking : Ranking.values()) {
+            if (ranking.rank == count) {
+                ranking.increaseCount();
+            }
+        }
+    }
+
+    public static int getTotalPrizeMoney() {
+        int sum = 0;
+        for (Ranking ranking : Ranking.values()) {
+            sum += ranking.money * ranking.count;
+        }
+        return sum;
+    }
+
+    public static void changeCountWhenHasBonusNumber() {
+        Ranking.SECOND.increaseCount();
+        Ranking.THIRD.decreaseCount();
+    }
+
+    public void increaseCount() {
+        count++;
+    }
+
+    public void decreaseCount() {
+        count--;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}

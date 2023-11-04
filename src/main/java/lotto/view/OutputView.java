edb7ad527f9b3b596da45c414ccc635f203ef402
WOONBE <kepull2918@naver.com>
2023-11-04 13:09:33 +0900

    [feat] OutputView에 게임의 진행 상황과 결과를 보여주는 로직 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,10 @@
 package lotto.view;
 
+import lotto.domain.Rank;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class OutputView {
 
     public static void requestInputAmountMessage() {
@@ -13,4 +18,39 @@ public class OutputView {
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
+    private static final int COUNT_ZERO = 0;
+    public static void purchaseComplete(int count) {
+        System.out.printf("\n%d개를 구매했습니다.\n", count);
+    }
+
+    public static void lottoNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
+    }
+
+    public static void winningStatistics() {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void showRankResult(HashMap<Rank, Integer> map) {
+        for (Rank rank : Rank.values()) {
+            showSingleRankResult(map, rank);
+        }
+    }
+
+    private static void showSingleRankResult(HashMap<Rank, Integer> map, Rank rank) {
+        if (rank.getCount()==COUNT_ZERO) {
+            return;
+        }
+        if (rank.equals(Rank.SECOND)) {
+            System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n",
+                    rank.getCount(), rank.getPrizeAmount(), map.get(rank));
+            return;
+        }
+        System.out.printf("%d개 일치 (%s원) - %d개\n", rank.getCount(), rank.getPrizeAmount(), map.get(rank));
+    }
+
+    public static void showYieldResult(double yield) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", yield);
+    }
 }

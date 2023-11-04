d5d2ebd1cb35c9ef8a69de177ba7b9d043099c00
hlmg <devhlmg@gmail.com>
2023-11-04 12:15:08 +0900

    feat: 일정 범위의 고유한 숫자를 뽑는 기능 구현

---
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator {
+
+    private RandomNumberGenerator() {
+    }
+
+    public static List<Integer> generateUniqueNumbersInRange(int startInclusive, int endInclusive, int count) {
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+    }
+}
---
+++ src/test/java/lotto/RandomNumberGeneratorTest.java
@@ -0,0 +1,46 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RandomNumberGeneratorTest {
+
+    @DisplayName("start에서 end 사이의 숫자를 뽑을 수 있다")
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            1, 1, 1
+            1, 2, 2
+            1, 45, 30
+            """)
+    void generateInRangeNumbers(int startInclusive, int endInclusive, int count) {
+
+        List<Integer> generated =
+                RandomNumberGenerator.generateUniqueNumbersInRange(startInclusive, endInclusive, count);
+
+        boolean isInRange = generated.stream()
+                .allMatch(number -> number >= startInclusive && number <= endInclusive);
+        assertThat(isInRange).isTrue();
+    }
+
+    @DisplayName("중복되지 않는 숫자 count개를 뽑을 수 있다")
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            1, 1, 1
+            1, 2, 2
+            1, 45, 30
+            """)
+    void generateUniqueNumbersAsCount(int startInclusive, int endInclusive, int count) {
+
+        List<Integer> generated =
+                RandomNumberGenerator.generateUniqueNumbersInRange(startInclusive, endInclusive, count);
+
+        long uniqueNumberCount = generated.stream().mapToInt(number -> number)
+                .distinct()
+                .count();
+        assertThat(uniqueNumberCount).isEqualTo(count);
+    }
+}

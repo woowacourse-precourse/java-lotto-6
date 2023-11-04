2d48c7f1998839b0936fa03eb0b83cea9c200e2d
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-04 15:05:40 +0900

    feat: 구입 금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 일력해 주세요.");
+        int money = Integer.parseInt(Console.readLine());
+
+
     }
 }

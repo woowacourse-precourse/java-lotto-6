548d67b97be74e58db1a4a6b94ca2253a40c29d4
WOONBE <kepull2918@naver.com>
2023-11-04 13:44:51 +0900

    [feat] Parser에 기존 문자열을 숫자 배열로 바꾸는 로직 변경

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,19 +1,30 @@
 package lotto.utils;
 
+import lotto.validation.Validator;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parser {
 
+    private final Validator validator = new Validator();
+
     //추후에 stream으로 변경 가능
-    public List<String> parseInputNumber(String input) {
-        return new ArrayList<>(Arrays.asList(input.split(",")));
+    public List<Integer> parseInputWinningNumbers(String input) {
+         validator.isValidIntegerNumberInList(Arrays.asList(input.split(",")));
+         return Arrays.stream(stringToIntArray(input)).boxed().collect(Collectors.toList());
     }
 
     //일단 만들어놨는데 아마 보너스 숫자는 기존 array에 추가하는 형식
-    public int parseBonusNumber(String input) {
+    public int parseInputStringNumber(String input) {
         return Integer.parseInt(input);
     }
+
+    private int[] stringToIntArray(String string) {
+        return Arrays.stream(string.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+
 
 }

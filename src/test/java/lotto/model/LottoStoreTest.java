57fab768c3c50752b1b0a18fe004b9ee20240bae
liarreez <liarreez@gmail.com>
2023-11-04 15:46:16 +0900

    refactor : 발행할 로또 숫자 검증하는 테스트를 구현이 변함에 따라 리팩토링

--- src/test/java/lotto/model/LottoStoreTest.java
+++ src/test/java/lotto/model/LottoStoreTest.java
@@ -2,7 +2,6 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,17 +12,17 @@ public class LottoStoreTest {
     @Test
     void testThreeLottoIssueWhenThreeThousandWon() {
         int purchaseAmount = 3000;
-        List<Lotto> lottos = lottoStore.issueLotto(purchaseAmount);
+        int purchasedLottoAmount = lottoStore.calculatePurchasedLottoCount(purchaseAmount);
 
-        assertThat(lottos.size()).isEqualTo(3);
+        assertThat(purchasedLottoAmount).isEqualTo(3);
     }
 
     @DisplayName("1000원 당 1장의 로또를 발행해야 한다. - 10000원이면 10장")
     @Test
     void testTenLottoIssueWhenTenThousandWon() {
         int purchaseAmount = 10000;
-        List<Lotto> lottos = lottoStore.issueLotto(purchaseAmount);
+        int purchasedLottoAmount = lottoStore.calculatePurchasedLottoCount(purchaseAmount);
 
-        assertThat(lottos.size()).isEqualTo(10);
+        assertThat(purchasedLottoAmount).isEqualTo(10);
     }
 }

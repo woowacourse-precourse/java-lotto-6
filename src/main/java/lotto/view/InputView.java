609cc631143b20f24f85ca9633304987b8808d95
gundorit <packdev937@gmail.com>
2023-11-05 00:01:07 +0900

    feat (InputView) : 당첨 번호를 입력한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,18 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
     public static int readPurchaseAmount() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static List<Integer> readWinningNumbers() {
+        return Arrays.stream(Console.readLine().split(","))
+            .map(Integer::parseInt)
+            .toList();
     }
 }

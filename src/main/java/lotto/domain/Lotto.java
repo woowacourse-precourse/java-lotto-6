28f77f41ad6fde7a157dbbbea3904de904e88a30
Maljaaa <Maljaaa@github.com>
2023-11-04 21:38:03 +0900

    refactor(InputView, Lotto): 당첨 번호 관련 기능 분리 - validateNumbersSize() : 입력된 당첨 번호가 6개인지 검증 - validateDuplicatedNumbers() : 입력된 당첨 번호가 중복되지 않았는지 검증 - String -> List<Integer> 메서드는 InputView에서 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,18 +3,28 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSize(numbers);
+        validateDuplicatedNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateNumbersSize(List<Integer> numbers) {
+        int WINNING_NUMBERS_SIZE = 6;   // enum으로 작성하자
+
+        if (numbers.size() != WINNING_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다. 다시 입력해주세요.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicatedNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (numbers.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복할 수 없습니다. 다시 입력해주세요.");
+            }
+        }
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,31 +8,32 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String VALIDATE_NUMBER_CODE = "-?\\\\d+";
-    private static final int WINNING_NUMBERS_SIZE = 6;
-    private static final String ENTER_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String ENTER_LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String STANDARD_OF_SPLIT = ",";
     private static final String ENTER_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static String enterPurchaseAmounts() {
+    public static int enterPurchaseAmounts() {
         System.out.println(ENTER_PURCHASE_AMOUNT_MESSAGE);
-        String userInput = readLine();
 
-        return userInput;
+        return parsingPurchaseAmounts(readLine());
     }
 
-    public static List<Integer> enterWinningNumbers() {
+    private static int parsingPurchaseAmounts(String userInput) {
+        if (!userInput.matches(VALIDATE_NUMBER_CODE)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다. 다시 입력해주세요.");
+        }
+        return Integer.parseInt(userInput);
+    }
+
+    public static List<Integer> enterLottoNumbers() {
         System.out.println();
-        System.out.println(ENTER_WINNING_NUMBERS_MESSAGE);
+        System.out.println(ENTER_LOTTO_NUMBERS_MESSAGE);
 
         String userInput = readLine();
 
-        List<Integer> winningNumbers = Arrays.stream(userInput.split(STANDARD_OF_SPLIT))
+        return Arrays.stream(userInput.split(STANDARD_OF_SPLIT))
                 .map(InputView::parsingWinningNumbers)
                 .toList();
-
-        validateWinningNumbersSize(winningNumbers);
-
-        return winningNumbers;
     }
 
     private static int parsingWinningNumbers(String userInput) {
@@ -43,20 +44,13 @@ public class InputView {
         return Integer.parseInt(userInput);
     }
 
-    private static void validateWinningNumbersSize(List<Integer> winningNumbers) {
-        if (winningNumbers.size() != WINNING_NUMBERS_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다. 다시 입력해주세요.");
-        }
-    }
-
     public static int enterBonusNumber() {
         System.out.println();
         System.out.println(ENTER_BONUS_NUMBERS_MESSAGE);
 
         String userInput = readLine();
-        int bonusNumber = parsingBonusNumber(userInput);
 
-        return bonusNumber;
+        return parsingBonusNumber(userInput);
     }
 
     private static int parsingBonusNumber(String userInput) {

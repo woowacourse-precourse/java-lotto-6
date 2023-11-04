caff855d5fe6b7b85844780f486981c49e17d2aa
jsj <tkdlek4186@naver.com>
2023-11-04 15:23:55 +0900

    feat(Lotto) : 생성자 수정, 일치하는 숫자의 개수 저장 및 출력함수 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,23 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
     public List<Paper> papers;
+    public List<Integer> matchingNumbers;
 
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
         papers=new ArrayList<>();
+        matchingNumbers=new ArrayList<>();
+        for(int i=0;i<5;i++){
+            matchingNumbers.add(0);
+        }
     }
 
     private void validate(List<Integer> numbers) {
@@ -30,10 +36,35 @@ public class Lotto {
         }
     }
 
+    public void saveMatchingNumbers(){
+        for(Paper i : papers){
+            if(i.getCorrespondNum()==3){
+                matchingNumbers.set(0,matchingNumbers.get(0)+1);
+            }
+            if(i.getCorrespondNum()==4){
+                matchingNumbers.set(1,matchingNumbers.get(1)+1);
+            }
+            if(i.getCorrespondNum()==5){
+                matchingNumbers.set(2,matchingNumbers.get(2)+1);
+            }
+            if(i.getCorrespondNum()==6){
+                matchingNumbers.set(3,matchingNumbers.get(3)+1);
+            }
+            if(i.getCorrespondNum()==7){
+                matchingNumbers.set(4,matchingNumbers.get(4)+1);
+            }
+        }
+    }
+
     public void printPapers(){
         for(Paper i : papers){
             System.out.println(i.getNumber());
         }
+    }
 
+    public void printMatchingNumbers(){
+        for(int i : matchingNumbers){
+            System.out.println(i);
+        }
     }
 }

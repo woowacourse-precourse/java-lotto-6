1a1fa5e599f547422db1d9719202afbfe320d283
이준희 <juni416@naver.com>
2023-11-04 10:44:50 +0900

    feature: 정답인 로또 번호와 보너스 넘버를 로또에 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,17 @@
 package lotto;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         GameManager gameManager = new GameManager();
-        int lottoAmount =gameManager.getPaymentForLottoByRead();
+        int lottoAmount = gameManager.getPaymentForLottoByRead();
+        List<Lotto> lottos = new ArrayList<>();
+
+        List<Integer> correctNumbers = gameManager.getCorrectLottoNumberByRead();
+        int bonusNumber = gameManager.getBonusLottoNumberByRead();
 
         gameManager.getCorrectLottoNumberByRead();
         gameManager.getBonusLottoNumberByRead();
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,10 +4,22 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int bonusNumber;
+    private List<Integer> correctNumbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public Lotto setBonusNumber(int number) {
+        this.bonusNumber = number;
+        return this;
+    }
+
+    public Lotto setCorrectNumbers(List<Integer> correctNumbers) {
+        this.correctNumbers = correctNumbers;
+        return this;
     }
 
     private void validate(List<Integer> numbers) {

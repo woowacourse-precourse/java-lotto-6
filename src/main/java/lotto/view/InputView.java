84b1fd82db9552da9e3ade3a6ca1c537eb4e8562
olsohee <dlthgml0108@daum.net>
2023-11-04 10:18:06 +0900

    fix(InputView): 인터페이스 내 메서드 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public interface InputView {
 
+    public String getPurchasePrice();
+
+    public String getWinningNumbers();
+
+    public String getAskBonusNumber();
+
     default void print(String printMessage) {
         System.out.println(printMessage);
     }

850099746f7ea7b5f6a497ca363fae050bb05b65
seop-h <jshwang9709@gmail.com>
2023-11-05 02:59:45 +0900

    test(domain): 로또 생성 관련 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.domain.constant.ErrorConst.LOTTO_MORE_THAN_COUNT;
+import static lotto.domain.constant.ErrorConst.LOTTO_OUT_OF_RANGE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,10 +23,41 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다")
+    void createLottoByUnderSize() {
+        assertThatIllegalArgumentException().isThrownBy(() ->{
+            new Lotto(List.of(1, 2, 3, 4, 5));
+        }).withMessage(LOTTO_MORE_THAN_COUNT);
+    }
+
+    @Test
+    @DisplayName("로또 번호 중 45보다 큰 번호가 있으면 예외가 발생한다")
+    void createLottoByOutOfMaximumNumber() {
+        assertThatIllegalArgumentException().isThrownBy(() ->{
+            new Lotto(List.of(1, 2, 3, 4, 5, 46));
+        }).withMessage(LOTTO_OUT_OF_RANGE);
+    }
+
+    @Test
+    @DisplayName("로또 번호 중 1보다 작은 번호가 있으면 예외가 발생한다")
+    void createLottoByOutOfMinimumNumber() {
+        assertThatIllegalArgumentException().isThrownBy(() ->{
+            new Lotto(List.of(1, 2, 3, 0, 5, 6));
+        }).withMessage(LOTTO_OUT_OF_RANGE);
+    }
+
+    @Test
+    @DisplayName("모든 검증을 통과하면 로또 번호가 발행된다")
+    void createLottoSuccessfully() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto).isNotNull();
+
+        List<Integer> numbers = lotto.getNumbers();
+        assertThat(numbers).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
 }

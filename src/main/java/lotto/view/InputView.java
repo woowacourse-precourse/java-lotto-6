f9507c4760e494cc79665641f2d4fad8713377b9
Sanghwi Lee <sanghwi47@naver.com>
2023-11-04 22:33:08 +0900

    feat(InputView) 당첨번호를 입력받는 코드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
 //    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
-//    private static final String INPUT_RESULT_NUMBER = "당첨 번호를 입력해 주세요.";
+//    private static final String INPUT_RESULT_NUMBER = "당첨 번호를 입력해 주세요. (쉼표로 구분하여 입력)";
 //    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     private static final String INPUT_MONEY = "input money";
@@ -16,9 +16,9 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static String inputResultNumber() {
+    public static String[] inputWinningNumber() {
         System.out.println(INPUT_RESULT_NUMBER);
-        return Console.readLine();
+        return Console.readLine().split(",");
     }
 
     public static String inputBonusNumber() {

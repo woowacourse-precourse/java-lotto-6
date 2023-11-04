2774fdd89da657b69a76301fd24d326f6962ef02
Junobee25 <wnsdhqo@naver.com>
2023-11-04 21:46:09 +0900

    refactor(LottoService): 로또 범위 상수 키워드로 수정

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -10,6 +10,9 @@ import java.util.stream.Collectors;
 public class LottoService {
 
     private static final String COMMAS = ",";
+    private static final Integer START_NUMBER = 1;
+    private static final Integer LAST_NUMBER = 45;
+    private static final Integer COUNT = 6;
 
     public List<Integer> convertToLottoIntegerList(String userInput) {
         return Arrays.stream(userInput.split(COMMAS))
@@ -22,7 +25,7 @@ public class LottoService {
         List<List<Integer>> lottoNumbers = new ArrayList<>();
 
         for (int count = 0; count < 8; count++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER,LAST_NUMBER,COUNT);
             lottoNumbers.add(numbers);
         }
         return lottoNumbers;

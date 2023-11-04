3f343f6195d4bc16525289cc06a061356a5d63c6
Jiyoung Choi <12201817@inha.edu>
2023-11-04 17:29:53 +0900

    refactor(Application) : 의존성 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,18 @@
 package lotto;
 
 import lotto.controller.LottoStore;
+import lotto.model.WinningStatistics;
+import lotto.util.RandomLottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoStore lottoStore = new LottoStore();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        WinningStatistics winningStatistics = new WinningStatistics();
+        RandomLottoMachine randomLottoMachine = new RandomLottoMachine();
+        LottoStore lottoStore = new LottoStore(inputView, outputView, randomLottoMachine, winningStatistics);
         lottoStore.visit();
     }
 }

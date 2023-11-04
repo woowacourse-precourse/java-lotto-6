328fb5ba362830c7179162e0309f9c8766ba7c92
oyoungsun <oyoungsun1030@gmail.com>
2023-11-04 15:46:24 +0900

    feat(InputView) : 잘못된 입력시 예외메시지 출력 후 재입력 수행 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.InputValidator;
 
 public class InputView {
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String BONUS_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static String getInput() {
-        return InputValidator.validate(Console.readLine());
+    public static String getInput(String message) {
+        OutputView.printInputMessage(message);
+        try {
+            return InputValidator.validate(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            OutputView.printException(e.getMessage());
+            return getInput(message);
+        }
     }
 
 

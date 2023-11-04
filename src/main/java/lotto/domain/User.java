32b0f041c32d9cade35a0edd53f0798c7b1af4e1
gimdaeyeon <rlaeodus1306@naver.com>
2023-11-04 23:23:57 +0900

    WIP: 로또를 구매하는 사용자 클래스 작성중

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class User {
+
+    private final int PURCHASE_AMOUNT;
+    private List<Lotto> lottos;
+
+    private User(int amount,List<Lotto> lottos){
+        this.PURCHASE_AMOUNT = amount;
+        this.lottos = lottos;
+    }
+
+    
+
+
+
+
 }

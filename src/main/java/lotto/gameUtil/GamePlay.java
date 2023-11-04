c1187deb5eac85a48f1f030d80ae9a19acc21c07
SeungjunNA <nsjjsng1@gmail.com>
2023-11-04 23:09:27 +0900

    refactor : 당첨번호를 보너스번호와 비교하기 위해 bonusNumber메서드에 Lotto인자를 추가

--- src/main/java/lotto/gameUtil/GamePlay.java
+++ src/main/java/lotto/gameUtil/GamePlay.java
@@ -2,6 +2,7 @@ package lotto.gameUtil;
 
 import lotto.Lotto;
 import lotto.domain.Enum.Rank;
+import lotto.exception.InputException;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -11,18 +12,17 @@ import java.util.Map;
 public class GamePlay {
     private InputView inputView;
     private GameLogic gameLogic;
-    private Double prize;
     public GamePlay(){
         inputView = new InputView();
         gameLogic = new GameLogic();
-        prize = 0D;
     }
     public void gamePlay(){
         int amount = inputView.inputAmount(); // 구매금액 입력
         List<Lotto> lottoList = gameLogic.lottoNumber(amount); // 구매한 만큼 로또번호 생성
-        String winningNumberString = inputView.winningNumber(); // 당첨번호 입력
+        String[] winningNumberString = inputView.winningNumber(); // 당첨번호 입력
         Lotto winningLotto = gameLogic.winningNumber(winningNumberString); // 번호에 대한 Lotto객체 생성
-        int bonusNumber = inputView.bonusNumber(); // 보너스 번호 입력
+        int bonusNumber = inputView.bonusNumber(winningLotto); // 보너스 번호 입력
+
 
         for(int i=0;i<lottoList.size();i++){  // 구매한 만큼 로또 당첨 확인
             int count = gameLogic.matchCount(lottoList.get(i), winningLotto, bonusNumber);

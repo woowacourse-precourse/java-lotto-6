b683623cd75b9e7ffb2a6dcd606ed223207cbdb4
Juhyorim <joohr1234@gmail.com>
2023-11-05 02:45:20 +0900

    feat: 로또 수익률 둘째 자리에서 반올림 기능 구현

--- src/main/java/lotto/domain/ReturnCalculator.java
+++ src/main/java/lotto/domain/ReturnCalculator.java
@@ -6,8 +6,16 @@ public class ReturnCalculator {
     public static double calculate(Map<WinningStatus, Integer> statusAndCount, int lottoCount) {
         int returnMoney = sumPrice(statusAndCount);
         int lottoPrice = lottoCount * 1000;
+        double rateOfReturn = ((double) returnMoney / lottoPrice) * 100;
 
-        return ((double) returnMoney / lottoPrice) * 100;
+        return roundFrom2Digit(rateOfReturn);
+    }
+
+    private static double roundFrom2Digit(double rateOfReturn) {
+        rateOfReturn *= 10;
+        rateOfReturn = Math.round(rateOfReturn);
+
+        return rateOfReturn/10;
     }
 
     private static int sumPrice(Map<WinningStatus, Integer> statusAndCount) {

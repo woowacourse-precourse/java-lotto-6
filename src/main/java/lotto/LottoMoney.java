dd07edc14e8992746b7f653394287255fbdaf8e9
jyeun722 <skqhs7276@gamil.com>
2023-11-05 07:27:20 +0900

    feat: Input lotto purchase amount with exception

---
+++ src/main/java/lotto/LottoMoney.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoMoney {
+    private static final int LOTTO_MONEY_UNIT = 1000;
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    private static void purchaseLotto() {
+        try {
+            System.out.println("구매금액을 입력해 주세요.");
+            int lottoMoney = inputLottoMoney();
+            validateLottoMoney(lottoMoney);
+        } catch (IllegalArgumentException e) {
+            purchaseLotto();
+        }
+    }
+
+    private static int inputLottoMoney() {
+        try {
+            int lottoMoney = Integer.parseInt(Console.readLine());
+            return lottoMoney;
+        } catch (NumberFormatException exception) {
+            System.out.println(ERROR_MESSAGE + " 숫자만 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateLottoMoney(int lottoMoney) {
+        if (lottoMoney % LOTTO_MONEY_UNIT != 0) {
+            System.out.println(ERROR_MESSAGE + " 구매금액은 1000원 단위여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void main(String[] args) {
+        purchaseLotto();
+    }
+}

0ce85c2702831e34ca632e67747e61d8c34078d6
pwrwpw <dlgustjr9912@gmail.com>
2023-11-05 02:33:45 +0900

    fix: 중복 코드 삭제 및 메서드명 반환 메시지와 일치하도록 수정

--- src/main/java/lotto/view/ExceptionMessages.java
+++ src/main/java/lotto/view/ExceptionMessages.java
@@ -4,9 +4,8 @@ public enum ExceptionMessages {
 
     STRING_TO_INTEGER("입력 값은 숫자로만 이루어져 있어야 합니다"),
     DUPLICATION_LIST("입력 값들은 서로 중복되지 않아야 합니다."),
-    INPUT_LENGTH_WINNING_NUMBER("입력 값의 개수는 %d개 여야 합니다."),
-    INPUT_LENGTH_BONUS_NUMBER("입력 값의 개수는 %d개 여야 합니다."),
-    ERROR_INPUT_BONUS_NUMBER_RANGE("입력 값은 %d의 배수여야 합니다."),
+    INPUT_COUNT("입력 값의 개수는 %d개 여야 합니다."),
+    INPUT_MULTIPLE_NUMBER("입력 값은 %d의 배수여야 합니다."),
     EXCEED("입력 값은 %d 이하의 값이어야 합니다."),
     NUMBER_IN_RANGE("입력 값의 범위는 %d부터 %d 사이의 숫자여야 합니다."),
     DUPLICATED_NUMBER_IN_LIST("입력 값은 앞서 입력한 값들과 중복되지 않아야 합니다.");

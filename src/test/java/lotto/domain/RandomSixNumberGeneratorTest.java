5957f689cdaec42645d35232bf5b94438f9ae2ec
Juhyorim <joohr1234@gmail.com>
2023-11-04 21:48:08 +0900

    test: 로또의 6개 숫자 오름차순 정렬 테스트 작성

--- src/test/java/lotto/domain/RandomSixNumberGeneratorTest.java
+++ src/test/java/lotto/domain/RandomSixNumberGeneratorTest.java
@@ -1,13 +1,19 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 class RandomSixNumberGeneratorTest {
     private static RandomSixNumberGenerator randomNumberGenerator;
@@ -40,6 +46,23 @@ class RandomSixNumberGeneratorTest {
         assertThat(sixNumber.size()).isEqualTo(6);
         for (Integer number : sixNumber) {
             assertThat(Collections.frequency(sixNumber, number)).isBetween(1, 45);
+        }
+    }
+
+    @Test
+    @Disabled("랜덤한 6개 숫자 정렬")
+    void 랜덤한_6개_숫자_정렬() {
+        try (MockedStatic<Randoms> mock = Mockito.mockStatic(Randoms.class)) {
+            //given
+            List<Integer> mockSixNumbers = Arrays.asList(6, 5, 4, 3, 2, 1);
+            mock.when(() -> Randoms.pickUniqueNumbersInRange(anyInt(), anyInt(), anyInt())).thenReturn(mockSixNumbers);
+
+            //when
+            List<Integer> sixNumber = randomNumberGenerator.getSixNumber();
+
+            //then
+            assertThat(sixNumber.size()).isEqualTo(6);
+            assertThat(sixNumber).containsExactly(1, 2, 3, 4, 5, 6);
         }
     }
 }

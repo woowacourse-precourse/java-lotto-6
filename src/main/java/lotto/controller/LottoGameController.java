f9a902f324d5a9058ada43a731bee2a0c4e8ba0b
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 04:20:43 +0900

    refactor : 메소드를 깔끔하게 하기 위해 분리

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,6 +1,10 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.Lotto;
+import lotto.model.LottoGame;
+import lotto.model.Player;
+import lotto.util.RandomNumberGenerator;
 import lotto.view.View;
 import lotto.vo.BonusNumber;
 import lotto.vo.TicketQuantity;
@@ -14,8 +18,31 @@ public class LottoGameController {
     }
 
     public void run() {
-        TicketQuantity ticketQuantity = view.getTicketQuantity();
-        Lotto winningLotto = view.getWinningLotto();
-        BonusNumber bonusNumber = view.getBonusNumber(winningLotto.getNumbers());
+        TicketQuantity ticketQuantity = initTicketQuantity();
+        Player player = initPlayer(ticketQuantity);
+        LottoGame lottoGame = initLottoGame(player);
+    }
+
+    private TicketQuantity initTicketQuantity() {
+        return view.getTicketQuantity();
+    }
+
+    private Player initPlayer(TicketQuantity ticketQuantity) {
+        return Player.createPlayer(ticketQuantity, new RandomNumberGenerator());
+    }
+
+    private LottoGame initLottoGame(Player player) {
+        Lotto winningLotto = initWinningLotto();
+        BonusNumber bonusNumber = initBonusNumber(winningLotto.getNumbers());
+
+        return LottoGame.createGame(player, winningLotto, bonusNumber);
+    }
+
+    private Lotto initWinningLotto() {
+        return view.getWinningLotto();
+    }
+
+    private BonusNumber initBonusNumber(List<Integer> winningNumbers) {
+        return view.getBonusNumber(winningNumbers);
     }
 }

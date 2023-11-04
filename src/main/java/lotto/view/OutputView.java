0385449bab70ade0cd52a25971eb4a4ebad77019
dltjdn <hana40004@naver.com>
2023-11-05 03:25:42 +0900

    feat(OutputView): 발행한 로또 번호 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public class OutputView {
     private static final String PURCAHSE_AMOUNT_FORMAT = "%d개를 구매했습니다.";
     private static final String WINNING_STATS_HEADER = "당첨 통계";
@@ -9,6 +12,13 @@ public class OutputView {
 
     public void printPurchaseAmount(int purchaseAmount){
         System.out.printf(PURCAHSE_AMOUNT_FORMAT,purchaseAmount);
+    }
+
+    public void printLottos(List<Lotto> lottos){
+        lottos.forEach(lotto -> {
+            lotto.sort();
+            System.out.println(lotto +"\n");
+        });
     }
 
     public void printTotalRateOfReturn(double rateOfReturn){

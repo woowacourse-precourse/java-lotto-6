e38c049d789ac34c6fa0cd17448e27e9bcfeebb1
02ggang9 <02ggang9@gmail.com>
2023-11-04 21:11:42 +0900

    feat(ErrorMessage): 에러 메시지를 관리하는 enum 생성

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public enum WinningLotto {
+
+
+
+}
---
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -0,0 +1,17 @@
+package lotto.global;
+
+public enum ErrorMessage {
+
+    INPUT_NUMBER_IS_INVALID("[유효한 숫자를 입력해주세요."),
+    ;
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

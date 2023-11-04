d1653bc052eed9dd744a7e8fef20f4565d012d44
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 17:10:01 +0900

    feat: 입력 문구 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,10 @@
 package view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    private final String ENTER_PRICE = "구입금액을 입력해 주세요.";
+    private final String ENTER_WINNGING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private final String ENTER_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
 
 }

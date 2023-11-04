f684802bc02c27e93842ef369d15e3333d400a04
mk3058 <kmk3058@icloud.com>
2023-11-04 13:02:22 +0900

    feat(Lotto): Lotto 클래스 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,56 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import message.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        sortAscending(numbers);
+        this.numbers = Collections.unmodifiableList(numbers);
+    }
+
+    private void sortAscending(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!hasCorrectSize(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_COUNT.getMessage("6"));
+        }
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER_EXIST.getMessage());
+        }
+        if (!hasCorrectRange(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_RANGE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasCorrectSize(List<Integer> numbers) {
+        return numbers.size() == 6;
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersWithoutDuplicate = new HashSet<>(numbers);
+
+        return numbersWithoutDuplicate.size() != numbers.size();
+    }
+
+    private boolean hasCorrectRange(List<Integer> numbers) {
+        return numbers.stream().noneMatch(number -> number < 0 || number > 45);
+    }
+
+    public void printNumbers() {
+        System.out.println(this.numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }

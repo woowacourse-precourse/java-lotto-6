846be94d95f62cb9e32ba06677ce73bcea7d9c03
noah <jeongas9810@gmail.com>
2023-11-05 03:58:06 +0900

    feat: Add View class
    View class defines only the strings that will be shown to the users.

---
+++ src/main/java/lotto/View.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public class View {
+    private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.\n";
+    private static final String ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.\n";
+    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요\n";
+    private static final String PRINT_PURCHASE_AMOUNT = "%d개를 구매했습니다.\n";
+    private static final String PRINT_PURCHASE_NUMBER = "[%s]\n";
+    private static final String PRINT_WINNING_STAT = "당첨 통계\n---\n";
+    private static final String PRINT_THREE_MATCHES = "3개 일치 (5,000원) - %d개\n";
+    private static final String PRINT_FOUR_MATCHES = "4개 일치 (50,000원) - 0개\n";
+    private static final String PRINT_FIVE_MATCHES = "5개 일치 (1,500,000원) - 0개\n";
+    private static final String PRINT_FIVE_BONUS_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n";
+    private static final String PRINT_SIX_MATCHES ="6개 일치 (2,000,000,000원) - 0개";
+    private static final String PRINT_YIELD_RATE = "총 수익률은 %.1f입니다.";
+}

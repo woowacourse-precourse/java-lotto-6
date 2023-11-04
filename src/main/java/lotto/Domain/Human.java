ca4b05880fdad612fc468f0d5ca1a7eef93a6ebf
SangHee Lee <lsh277604@gmail.com>
2023-11-05 00:01:28 +0900

    Feat: add new Class(Human) & basic Method

---
+++ src/main/java/lotto/Domain/Human.java
@@ -0,0 +1,29 @@
+package lotto.Domain;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Human {
+    private List<Lotto> lottos = new ArrayList<>();
+    private Map<WinningType, Integer> winningResult = new EnumMap<>(WinningType.class);
+    private Long budget;
+    public Human(Long budget) {
+        this.budget = budget;
+    }
+
+    public boolean buyLotto(){
+        if(budget > 1000){
+            budget -= 1000;
+            return true;
+        }
+        return false;
+    }
+
+    public void addLotto(List<Integer> lottoNumber){
+        Lotto lotto = new Lotto(lottoNumber);
+        lottos.add(lotto);
+    }
+}

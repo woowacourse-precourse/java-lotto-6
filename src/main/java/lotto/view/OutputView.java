e90afe145ab7d26932caec6f52a4a171c009f111
Kite_Z <duswl339@ajou.ac.kr>
2023-11-04 21:16:08 +0900

    feat: add print method for OutputView
    - printBuyLotto: 구입/발행한 로또 수 출
    - printLottos: 로또 번호 출력
    - printLottoWinning: 로또 당첨번호 출력
    - printWinningStatics: 로또 당첨 현황 출력
    - printRateOfReturn: 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,48 @@
 package lotto.view;
 
+import lotto.Lotto;
+import lotto.model.Customer;
 import lotto.view.constant.ConstantMessage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.view.constant.ConstantMessage.*;
 
 public class OutputView {
     public OutputView() {}
 
     public static void printConstantMessage(ConstantMessage message) {
         System.out.print(message);
+    }
+
+    public static void printBuyLotto(ConstantMessage message, int number) {
+        print(String.format(String.valueOf(message), number));
+    }
+
+    public static void printLottos(Customer _customer) {
+        List<Lotto> lotts = _customer.getLottos();
+        for(Lotto lotto: lotts) {
+            println(lotto.getNumbers().toString());
+        }
+    }
+
+    public static void printLottoWinning(List<Integer> _winningNumber) {
+        println(_winningNumber.stream().map(String::valueOf)
+                .collect(Collectors.joining(",")));
+    }
+
+    public static void printWinningStatics(List<Integer> _winning) {
+        printNewLine();
+        println(String.format(String.valueOf(THREE_MATCH), _winning.get(0)));
+        println(String.format(String.valueOf(FOUR_MATCH), _winning.get(1)));
+        println(String.format(String.valueOf(FIVE_MATCH), _winning.get(2)));
+        println(String.format(String.valueOf(FIVE_BONUS_MATCH), _winning.get(4)));
+        println(String.format(String.valueOf(SIX_MATCH), _winning.get(3)));
+    }
+
+    public static void printRateOfReturn(double _rateOfReturn) {
+        println(String.format(String.valueOf(TOTAL_RATE_OF_RETURN), _rateOfReturn+"%"));
     }
 
     public static void printNewLine() {

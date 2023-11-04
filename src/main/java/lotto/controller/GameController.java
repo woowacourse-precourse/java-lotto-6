23e7829fbeeceadb3b2fddbfbe482a77994080cc
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-04 19:14:03 +0900

    refactor: 출력 OutputView 통해 작성

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,7 +1,41 @@
 package lotto.controller;
 
+import lotto.view.OutputView;
+
 public class GameController {
     public void run() {
+        while (true) {
+            try {
+                OutputView.printLottoBuyPrice();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
 
+        OutputView.printBoughtNum();
+
+        OutputView.printLotto();
+
+        while (true) {
+            try {
+                OutputView.printUserNum();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                OutputView.printBonusNum();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+
+        OutputView.printStats();
+
+        OutputView.printLottoResult();
+
+        OutputView.printRateOfReturn();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,44 @@
 package lotto.view;
 
 public class OutputView {
+    public static void printLottoBuyPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printBoughtNum() {
+        System.out.println();
+        System.out.println(lottoNum + "개를 구매했습니다.");
+    }
+
+    public static void printLotto() {
+        allLottoList.forEach(System.out::println);
+    }
+
+    public static void printUserNum() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printBonusNum() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printStats() {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void printLottoResult() {
+        System.out.println("3개 일치 (5,000원) - " + rank5th + "개");
+        System.out.println("4개 일치 (50,000원) - " + rank4th + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + rank3rd + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + rank2nd + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + rank1st + "개");
+    }
+
+    public static void printRateOfReturn() {
+        System.out.println("총 수익률은 " + formattedRateOfReturn + "입니다.");
+    }
 }

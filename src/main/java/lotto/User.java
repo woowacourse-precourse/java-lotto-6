757a3453325f881d4f63b77c1d931a0d0c174753
이진모 <jinmlee210@naver.com>
2023-11-04 16:35:47 +0900

    feat: 사용자 지불금액이 int형식이 맞는지 확인하는 메서드 기능구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -5,7 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class User {
     private static int payment;
 
-    public void inputPayment() {
-        String input = Console.readLine();
+    public void inputPayment(String input) {
+        checkInteger(input);
+    }
+
+    public void checkInteger(String input) {
+        try {
+            payment = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 금액을 입력하셨습니다.");
+        }
     }
 }

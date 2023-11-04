3015e63c1af035ffe4ce3c67ebbe36cd1297d3ae
HYUNJUN\sajun <sajun28@naver.com>
2023-11-05 00:44:31 +0900

    Feat: "로또 구매 개수 및 번호 출력 기능 추가"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.model.Purchase;
 import lotto.model.RandomLotto;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ public class Application {
     private static int purchasePieces;
     private static List<Integer> lottoNum;
     private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
     private static final RandomLotto random = new RandomLotto();
 
     public static void purchaseCount(String purchasingAmount) {
@@ -25,12 +27,13 @@ public class Application {
     public static void randomRepeat() {
         for (int i = 0; i < purchasePieces; i++) {
             lottoNum = random.listSort(random.generateNum());
-            System.out.println(lottoNum);
+            outputView.purchaseNum(lottoNum);
         }
     }
 
     public static void main(String[] args) {
         purchaseCount(inputView.purchaseAmount());
+        outputView.purchasePieces(purchasePieces);
         randomRepeat();
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void purchasePieces(int pieces) {
+        System.out.println("\n" + pieces + "개를 구매했습니다.");
+    }
+
+    public void purchaseNum(List<Integer> lottoNum) {
+        System.out.println(lottoNum);
+    }
+}

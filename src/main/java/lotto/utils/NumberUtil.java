b1abfe3d0d787ef9efaf5d87a30e8f0dd3ed6d64
deohyunpark <qwertyshsh@naver.com>
2023-11-05 03:47:10 +0900

    feat: add NumberUtils parseInt

---
+++ src/main/java/lotto/utils/NumberUtil.java
@@ -0,0 +1,14 @@
+package lotto.utils;
+
+public class NumberUtil {
+
+    private static final String STRING_VALID_LOG = "숫자만 입력해주세요.";
+
+    public static int parseInt(final String argument) {
+        try {
+            return Integer.parseInt(argument);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(STRING_VALID_LOG);
+        }
+    }
+}

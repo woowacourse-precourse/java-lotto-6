1132129adcbaa3058fd33793875a4136a7688c00
hyo-limilimee <1001lily@naver.com>
2023-11-05 01:28:34 +0900

    feat: make method about validate lotto numbers

---
+++ src/main/java/lotto/Validator/LottoNumberValidator.java
@@ -0,0 +1,23 @@
+package lotto.Validator;
+import java.util.Set;
+
+public class LottoNumberValidator {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final String LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 로또 번호를 입력했습니다.";
+
+    public static void validateIsNumberInRange(int number, Set<Integer> existingNumbers) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateIsNumberDuplicate(int number, Set<Integer> existingNumbers) {
+
+        if (existingNumbers.contains(number)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+}
--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -4,13 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.HashSet;
 import java.util.Set;
 import lotto.Validator.LottoPurchaseAmountValidator;
+import lotto.Validator.LottoNumberValidator;
 
 public class InputView {
 
     private final static String INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String INPUT_LOTTO_NUMBER_MESSAGE="당첨 번호를 입력해 주세요.";
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
 
     public static int inputLottoPurchaseAmount() {
         System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
@@ -40,6 +39,8 @@ public class InputView {
 
         for (String token : eachLottoNumbers) {
             int lottoNumber = Integer.parseInt(token);
+            LottoNumberValidator.validateIsNumberInRange(lottoNumber, lottoNumbers);
+            LottoNumberValidator.validateIsNumberDuplicate(lottoNumber, lottoNumbers);
             lottoNumbers.add(lottoNumber);
         }
 

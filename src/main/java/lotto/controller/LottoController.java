d69766eccc5885e2389ae596ef79cb55b723be96
pwrwpw <dlgustjr9912@gmail.com>
2023-11-05 03:35:22 +0900

    feat: 구매금액 만큼 로또 생성하는 메서드 구현
    docs: 구현한 기능 체크 완료

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,13 +2,28 @@ package lotto.controller;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import lotto.domain.User;
+import lotto.service.LottoService;
 import lotto.utils.Utills;
 import lotto.view.InputView;
 
 public class LottoController {
 
-    public void run() {
+    private User user;
+    private final LottoService lottoService;
 
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        beforeStart();
+    }
+
+    private void beforeStart() {
+        user = new User(getInputAmount());
+        lottoService.buyLottoAll(user);
+        // TODO: 로또 구매 후 로또 번호 출력
     }
 
     private int getInputAmount() {
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,13 +2,22 @@ package lotto.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 import lotto.domain.User;
 import lotto.utils.Utills;
 
 public class LottoService {
 
-    private void buyLotto(User user){
+    public void buyLottoAll(User user) {
+        int purchaseAmount = user.getPurchaseAmount();
+        int lottoCount = purchaseAmount / 1000;
+
+        IntStream.range(0, lottoCount)
+                .forEach(i -> buyLottoOne(user));
+    }
+
+    private void buyLottoOne(User user){
         Lotto lotto = generateLottoNumber();
         user.addLotto(lotto);
     }

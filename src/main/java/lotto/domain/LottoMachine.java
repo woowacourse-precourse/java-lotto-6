f6c3c77db90c8635ea028c8b9f58ad9592b02876
LeeJiWon <dlwldnjs1009@gmail.com>
2023-11-04 19:16:26 +0900

    refactor: 로또 번호 생성 로직 Randoms 유틸리티로 변경

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,6 +8,9 @@ import java.util.List;
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     public List<LottoTicket> purchaseTickets(int money) {
         int numberOfTickets = money / LOTTO_PRICE;
@@ -20,11 +24,11 @@ public class LottoMachine {
     }
 
     private LottoTicket generateRandomTicket() {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= 45; i++) {
-            numbers.add(i);
-        }
-        Collections.shuffle(numbers);
-        return new LottoTicket(numbers.subList(0, 6));
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                NUMBER_OF_LOTTO_NUMBERS
+        );
+        return new LottoTicket(randomNumbers);
     }
 }

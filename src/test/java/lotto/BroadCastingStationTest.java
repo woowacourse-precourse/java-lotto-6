f84364722a8bbe2be78cc20945d8a3f8cc8fc64f
jeli01 <jeli01@naver.com>
2023-11-05 02:56:27 +0900

    test: add drawing lotto test

---
+++ src/test/java/lotto/BroadCastingStationTest.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BroadCastingStationTest {
+    @Test
+    @DisplayName("로또의 숫자와 보너스 번호가 겹치면 예외가 발생한다")
+    void validateLottoAndBonus() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Integer bonus = 5;
+        BroadCastingStation broadCastingStation = new BroadCastingStation();
+        Assertions.assertThatThrownBy(() -> {
+                    broadCastingStation.drawLotto(lotto, 5);
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호와 보너스 번호가 중복됩니다.");
+    }
+}

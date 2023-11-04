093ef9063784813bb4ec283c12780052ef628ffb
HSBODo <vinsulill@gmaile.com>
2023-11-04 17:17:07 +0900

    feat : 로도 구입 금액을 입력받는 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 최초 커밋 테스트
+    }
+
+    public static String inputPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputPurchaseAmount = Console.readLine();
+        return inputPurchaseAmount;
     }
 }

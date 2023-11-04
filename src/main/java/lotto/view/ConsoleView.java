6ba916eedc8dbc698ba234aa1a4dd986292b8fed
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-11-04 17:57:47 +0900

    feat: ConsoleView 추가  - Console을 이용하는 View  - 추후 해당 클래스만 수정하면 기존의 View를 수정하지 않고 View 변경 가능  - Java 17에서 추가된 seald permit을 이용해 다른 클래스가 상속하지 못하도록 제어

---
+++ src/main/java/lotto/view/ConsoleView.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public sealed class ConsoleView permits InputView{
+
+    protected void print(final String message) {
+        System.out.print(message);
+    }
+
+    protected void println(final String meesage) {
+        System.out.println(meesage);
+    }
+
+    protected void lineBreak() {
+        System.out.println();
+    }
+
+    protected String read() {
+        return Console.readLine();
+    }
+}

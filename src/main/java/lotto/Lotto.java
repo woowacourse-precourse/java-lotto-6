cce5a840cc8e89f12ed79426daa9c40b0bc8e13e
jeli01 <jeli01@naver.com>
2023-11-05 00:42:27 +0900

    feat: add validation function about range

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateSixNumbers(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
     }
 
     private void validateSixNumbers(List<Integer> numbers) {
@@ -27,6 +28,14 @@ public class Lotto {
         HashSet<Integer> noDuplication = new HashSet<>(numbers);
         if(noDuplication.size() != numbers.size()) {
             throw new IllegalArgumentException("번호가 중복 되었습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for(Integer number : numbers) {
+            if (!(1 <= number && number <= 45)) {
+                throw new IllegalArgumentException("번호는 1~45의 숫자여야 합니다.");
+            }
         }
     }
     // TODO: 추가 기능 구현

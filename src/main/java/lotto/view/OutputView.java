f1935c6dfafad06f7a5ce22c6e8df3b6fcd248f5
Yang-soeun <soeun8636@naver.com>
2023-11-04 18:08:33 +0900

    [feat]: 구매한 로또를 출력하는 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,22 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String NEW_LINE = "\n";
 
     public void printPurchaseMessage(int quantity){
         System.out.println(quantity + PURCHASE_MESSAGE);
+    }
+
+    public void printLotto(List<List<Integer>> lottos) {
+        StringBuilder sb = new StringBuilder();
+
+        for (List<Integer> lotto : lottos) {
+            sb.append(NEW_LINE).append(lotto);
+        }
+
+        System.out.println(sb);
     }
 }

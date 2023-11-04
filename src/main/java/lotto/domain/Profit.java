474bbab53c84bfdaa511f67487002785049aee19
hil679 <hil679922@gmail.com>
2023-11-04 20:31:05 +0900

    Feat(Profit): Create Profit skeleton code

---
+++ src/main/java/lotto/domain/Profit.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class Profit {
+
+    public float calculateProfitRate(PurchasePrice purchasePrice, WinResult winResult) {
+        return 0f;
+    }
+
+    private long calculateWinMoney(WinResult winResult) {
+        return 0L;
+    }
+}

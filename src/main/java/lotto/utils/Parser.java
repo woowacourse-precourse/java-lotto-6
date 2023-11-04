d0d469605be02e5c3df9990347bbded2062e7adc
heiler <threepebbles@naver.com>
2023-11-04 21:06:21 +0900

    Refactor: 콤마 상수화

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
+    private static final String COMMA = ",";
+
     public static List<String> parseWithComma(String userInput) {
-        return Arrays.stream(userInput.split(",", -1))
+        return Arrays.stream(userInput.split(COMMA, -1))
                 .toList();
     }
 }

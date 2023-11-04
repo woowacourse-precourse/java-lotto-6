314bb936565efd275636ebe8806fd6f5706ecb7c
haiseong <wjdgotjd9908@gmail.com>
2023-11-04 16:23:36 +0900

    test(RandomsTest): pickUniqueNumbersInRange 사용해보기 테스트

---
+++ src/test/java/study/RandomsTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+
+public class RandomsTest {
+
+    @Test
+    void pickUniqueNumbersInRange_사용해보기() {
+        List<Integer> numbers = pickUniqueNumbersInRange(1, 45, 6);
+        System.out.println("numbers = " + numbers);
+    }
+}

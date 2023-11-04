f351c92db2bacd14bd8ce9ddc21a9762c5074986
mainfn <_@mainfn.dev>
2023-11-04 18:19:40 +0900

    feat: DI Container 및 Application 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+import lotto.configuration.Configuration;
+
+public final class Application {
+    public static void main(final String[] args) {
+        Configuration
+                .createLottoController()
+                .run();
     }
 }

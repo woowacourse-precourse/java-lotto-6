8fd3e834b19efc611ff14cf859696dfc6d45810c
kanghuijin <rkd8527@naver.com>
2023-11-05 01:23:19 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는 기능 추가 당첨 번호와 보너스 번호를 입력 받고 예외처리를 한다.

--- src/main/java/lotto/Play.java
+++ src/main/java/lotto/Play.java
@@ -8,24 +8,26 @@ import java.util.*;
 public class Play {
 
     public void play() {
-        int lottoAmount = purchase();
-        List<Lotto> lottos = lottoNumbers(lottoAmount);
+        int lottoAmount = inputAmount();
+        List<Lotto> lottos = createLotto(lottoAmount);
 
         for (Lotto list : lottos) {
             System.out.println(list.getNumbers());
         }
+
+        List<Integer> winnerNumbers = new ArrayList<>(inputWinnerNumber());
     }
 
-    // 로또를 구입한다
-    public int purchase() {
+    // 로또를 구입할 금액을 입력받는다.
+    public int inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String price = Console.readLine();
 
         try {
-            validate(price);
+            handleNonThousandUnit(price);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return purchase();
+            return inputAmount();
         }
         int amount = Integer.parseInt(price) / 1000;
 
@@ -35,25 +37,72 @@ public class Play {
     }
 
     // 구입한 개수만큼 로또를 생성한다
-    public List<Lotto> lottoNumbers(int lottoAmount) {
-        List<Lotto> lottoNumbersList = new ArrayList();
+    public List<Lotto> createLotto(int lottoAmount) {
+        List<Lotto> lottoNumbersList = new ArrayList<>();
 
         for (int i = 1; i <= lottoAmount; i++) {
-            lottoNumbersList.add(new Lotto(lottoRandomNumbersProduce()));
+            lottoNumbersList.add(new Lotto(createRandomLottoNumbers()));
         }
         return lottoNumbersList;
     }
 
     // 6개의 랜덤 숫자를 생성하고 오름차순으로 정렬한다
-    public List<Integer> lottoRandomNumbersProduce() {
-        List<Integer> numbers = new ArrayList<Integer>
+    public List<Integer> createRandomLottoNumbers() {
+        List<Integer> numbers = new ArrayList<>
                 (Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(numbers);
+
         return numbers;
     }
 
+    // 당첨 번호와 보너스 번호를 입력받는다.
+    public List<Integer> inputWinnerNumber() {
+        List<Integer> numbers = new ArrayList<>();
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String winnerNumbers = Console.readLine();      //당첨 번호를 입력받음
+
+        try {
+            handleInvalidNumberCount(winnerNumbers);    //당첨 번호 예외처리
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinnerNumber();
+        }
+
+        for (String s : winnerNumbers.split(",")) {
+            numbers.add(Integer.parseInt(s));           //int 타입으로 변환 후 리스트에 추가
+        }
+
+        try {
+            handleDuplicateNumbers(numbers);            //당첨 번호 중복 검사 예외처리
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinnerNumber();
+        }
+
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonusNumber = Console.readLine();
+
+        try {
+            handleInvalidNumberInput(bonusNumber);          //보너스 번호 예외처리
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinnerNumber();
+        }
+
+        numbers.add(Integer.parseInt(bonusNumber));     //int 타입으로 변환 후 리스트에 추가
+
+        try {
+            handleDuplicateNumbers(numbers);            //당첨 번호 중복 검사 예외처리
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinnerNumber();
+        }
+        return numbers;
+    }
+
+    ////////////////////////////////////예외처리////////////////////////////////////
     // 입력받은 금액이 1000원 단위가 아니면 예외 발생
-    public void validate (String price) {
+    public void handleNonThousandUnit(String price) {
         try {
             Double.parseDouble(price);
         } catch (NumberFormatException e) {
@@ -63,4 +112,42 @@ public class Play {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
         }
     }
+
+    // 당첨 번호가 숫자가 아니거나 6개가 아니거나 1~45의 범위가 아니면 예외 발생
+    public void handleInvalidNumberCount(String sixNumber) {
+        String replaceNumber = sixNumber.replace(",","");
+        try {
+            Integer.parseInt(replaceNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        }
+        if (replaceNumber.length() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 번호를 입력해주세요.");
+        }
+        for (String number : sixNumber.split(",")) {
+            if (Integer.parseInt(number) < 1 || Integer.parseInt(number) > 45) {
+                throw new IllegalArgumentException("[ERROR] 1 ~ 45의 번호를 입력해주세요.");
+            }
+        }
+    }
+
+    // 보너스 번호가 숫자가 아니거나 1~45의 범위가 아니면 예외 발생
+    public void handleInvalidNumberInput(String bonusNumber) {
+        try {
+            Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        }
+        if (Integer.parseInt(bonusNumber) < 1 || Integer.parseInt(bonusNumber) > 45) {
+            throw new IllegalArgumentException("[ERROR] 1 ~ 45의 번호를 입력해주세요.");
+        }
+    }
+
+    // 당첨 번호 중복이면 예외 발생
+    public void handleDuplicateNumbers(List<Integer> winnerNumbers) {
+        if (winnerNumbers.size() != winnerNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 번호는 중복되지 않게 입력해주세요.");
+        }
+    }
+
 }
--- src/test/java/lotto/PlayTest.java
+++ src/test/java/lotto/PlayTest.java
@@ -19,14 +19,56 @@ public class PlayTest {
     @DisplayName("1000원 단위가 아니면 예외 발생.")
     @Test
     void 천원_단위_테스트() {
-        assertThatThrownBy(() -> play.validate("10100"))
+        assertThatThrownBy(() -> play.handleNonThousandUnit("10100"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("숫자가 아니면 예외 발생.")
     @Test
-    void 숫자가_맞는지() {
-        assertThatThrownBy(() -> play.validate("1000ㄹ"))
+    void 금액이_숫자가_아니면_예외_발생() {
+        assertThatThrownBy(() -> play.handleNonThousandUnit("1000ㄹ"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 아니면 예외 발생.")
+    @Test
+    void 당첨_번호가_숫자가_아니면_예외_발생() {
+        assertThatThrownBy(() -> play.handleInvalidNumberCount("1000ㄹ"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 번호가 6개가 아니면 예외 발생.")
+    @Test
+    void 당첨_번호가_6개가_아니면_예외_발생() {
+        assertThatThrownBy(() -> play.handleInvalidNumberCount("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 번호가 1~45의 범위를 벗어나면 예외 발생.")
+    @Test
+    void 당첨_번호의_숫자_범위를_넘으면_예외_발생() {
+        assertThatThrownBy(() -> play.handleInvalidNumberCount("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 아니면 예외 발생.")
+    @Test
+    void 보너스_번호가_숫자가_아니면_예외_발생() {
+        assertThatThrownBy(() -> play.handleInvalidNumberCount("1,2,3,4,5,ㅋ"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 보너스번호가 1~45의 범위를 벗어나면 예외 발생.")
+    @Test
+    void 보너스_번호가_숫자_범위를_넘으면_예외_발생() {
+        assertThatThrownBy(() -> play.handleInvalidNumberInput("46"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 당첨 번호가 중복이면 예외 발생.")
+    @Test
+    void 당첨_번호가_중복이면_예외_발생() {
+        assertThatThrownBy(() -> play.handleDuplicateNumbers(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

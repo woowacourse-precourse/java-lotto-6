eb0c7acf596be0b9e5551eec1ab6d3b66a0b6422
kim <hwiyul_g@daum.net>
2023-11-04 14:01:05 +0900

    feat(Lotto) : 로또 번호 중복 검사, 개별 숫자 범위 검사 함수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,7 +14,27 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        validateDuplicateNumbers(numbers);
+        validateNumberRange(numbers);
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicateNumbers(List<Integer> numbers){
+        final int LOTTO_NUMBERS_SIZE = 6;
+        long distinctCount = numbers.stream().distinct().count();
+        if(distinctCount != LOTTO_NUMBERS_SIZE){
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateNumberRange(List<Integer> numbers){
+        final int MIN_NUMBER = 1;
+        final int MAX_NUMBER = 45;
+
+        boolean isOutOfRange = numbers.stream()
+            .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
+
+        if(isOutOfRange){
+            throw new IllegalArgumentException();
+        }
+    }
 }

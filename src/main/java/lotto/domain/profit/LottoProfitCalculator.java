c2e905070c95c1b1087d1d81cc8e04e296ba0aad
minseo  jo <liging12@naver.com>
2023-11-04 12:36:19 +0900

    feat: 로또 당첨 개수 계산 기능 추가 (WinningRankingCalculator)
    로또 당첨의 개수를 센다.
    enum의 key를 이용해, 당첨된 랭킹 개수를 저장했다. (EnumMap)
    
    EnumMap 특징
    - 저장되는 데이터 순서는, enum 상수 순서와 같다. (FIFTH ~ FIRST, 출력 순서와 같음.)
    - EnumMap 은 처음에 어떤 Enum을 저장할 지 class 를 지정한다. 그렇기에 맵에 크기가 달라지지 않는다.(리사이징)) key의 개수는 enum의 상수 개수만 저장하기 떄문이다. 그래서 효율적임.

---
+++ src/main/java/lotto/domain/profit/LottoProfitCalculator.java
@@ -0,0 +1,10 @@
+package lotto.domain.profit;
+
+public class LottoProfitCalculator {
+    // 클래스 내에 로또 수익률을 계산하는 메소드를 구현
+    public double calculateProfit(int[] userNumbers, int[] winningNumbers, int prizeAmount) {
+        // 수익률 계산 로직을 구현
+        // 사용자 번호, 당첨 번호, 상금액을 이용하여 수익률을 계산
+        return 0f;
+    }
+}

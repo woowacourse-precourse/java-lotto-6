587aa0f8d73d2c7d45f6cc5c57a8dcf63e3cb5c3
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 12:25:15 +0900

    feat(LottoUtil): validateMultiplesOf 작성
    들어온 숫자가 약수의 배수인지 검증하는 메소드인 validateMultiplesOf 메소드를 작성했습니다.

--- src/main/java/lotto/utils/LottoUtil.java
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -1,6 +1,9 @@
 package lotto.utils;
 
+import static lotto.message.ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WN_UNITS;
 import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
+
+import java.math.BigDecimal;
 
 public class LottoUtil {
     public static void validateLottoNum(int num) {
@@ -9,6 +12,12 @@ public class LottoUtil {
         }
         if (num > 45) {
             throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_IN_RANGE.getMessage());
+        }
+    }
+
+    public static void validateMultiplesOf(BigDecimal num, BigDecimal factor) {
+        if (num.remainder(factor).compareTo(BigDecimal.ZERO) != 0) {
+            throw new IllegalArgumentException(AMOUNT_IS_NOT_IN_THOUSAND_WN_UNITS.getMessage());
         }
     }
 }

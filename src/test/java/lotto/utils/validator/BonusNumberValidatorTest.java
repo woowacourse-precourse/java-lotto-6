7740e7ebdc7b2a9b410ef76b3d8b5f0e1db14bc2
suhyun9764 <suhyun9764@naver.com>
2023-11-04 22:07:10 +0900

    test: 예외 발생 테스트를 출려값 비교 테스트로 변경

--- src/test/java/lotto/utils/validator/BonusNumberValidatorTest.java
+++ src/test/java/lotto/utils/validator/BonusNumberValidatorTest.java
@@ -1,5 +1,7 @@
 package lotto.utils.validator;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -7,14 +9,19 @@ import lotto.Application;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class BonusNumberValidatorTest {
+class BonusNumberValidatorTest extends NsTest {
 
 
     @Test
     void 숫자_포맷_테스트(){
-        String testInput = "a";
-        Assertions.assertThatThrownBy(()->BonusNumberValidator.validate(testInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자만");
+            assertSimpleTest(() -> {
+                runException("A");
+                assertThat(output()).contains("숫자만");
+            });
+        }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
--- src/test/java/lotto/utils/validator/WinningNumbersValidatorTest.java
+++ src/test/java/lotto/utils/validator/WinningNumbersValidatorTest.java
@@ -1,5 +1,7 @@
 package lotto.utils.validator;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -10,28 +12,28 @@ import org.junit.jupiter.api.Test;
 class WinningNumbersValidatorTest extends NsTest {
 
     @Test
-    void 콤마_위치_테스트() {
+    void 콤마_위치_테스트(){
+        assertSimpleTest(() -> {
+            runException("3000",",1,2,3,4,5,6");
+            assertThat(output()).contains("콤마");
+        });
 
-        String testInput1 = ",1,2,3,4";
-        String testInput2 = "1,2,3,4,";
-
-        assertThatThrownBy(() -> WinningNumbersValidator.validate(testInput1))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("콤마");
-
-        assertThatThrownBy(() -> WinningNumbersValidator.validate(testInput2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("콤마");
+        assertSimpleTest(() -> {
+            runException("3000","1,2,3,4,5,6,");
+            assertThat(output()).contains("콤마");
+        });
     }
 
     @Test
     void 숫자_테스트() {
+        assertSimpleTest(() -> {
+            runException("3000","1,2,3,4,A,5");
+            assertThat(output()).contains("숫자만");
+        });
+    }
 
-        String testInput1 = "1,2,a,4";
-
-
-        assertThatThrownBy(() -> WinningNumbersValidator.validate(testInput1))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자");
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }

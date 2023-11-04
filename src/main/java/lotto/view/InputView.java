c350901dda6c959d52f75cc1f5519ee2bab39eb1
mk3058 <kmk3058@icloud.com>
2023-11-04 17:12:18 +0900

    fix(InputView): 음수가 입력되었을 때 숫자 형식이 아니라는 메시지가 출력되던 문제 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,14 @@ package lotto.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import lotto.message.ErrorMessage;
+import lotto.message.InputMessage;
 
 public class InputView {
 
     public static int getPrice() {
+        InputMessage.PRICE_PROMPT.print();
         String input = readLine();
-        if (isDigit(input)) {
+        if (!isNumbericValue(input)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
         }
         int price = Integer.parseInt(input);
@@ -18,7 +20,18 @@ public class InputView {
         return price;
     }
 
-    private static boolean isDigit(String input) {
-        return input.chars().allMatch(Character::isDigit);
+    private static boolean isNumbericValue(String input) {
+        char[] data = input.toCharArray();
+        int index = 0;
+
+        if (data[index] == '-') {
+            index++;
+        }
+        for (int i = index; i < data.length; i++) {
+            if (!Character.isDigit(data[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }

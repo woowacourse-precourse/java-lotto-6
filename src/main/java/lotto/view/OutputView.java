e896ac91f6c39da2448ce1f7fecc57df656e33e8
gundorit <packdev937@gmail.com>
2023-11-04 23:50:56 +0900

    feat (OutputView) : 보너스 번호를 입력하는 문구를 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     private static final String LOTTO_PURCHASE_PROMPT = "구입금액을 입력해 주세요.\n";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.\n";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.\n";
 
     public static void printLottoPurchasePrompt() {
         System.out.print(LOTTO_PURCHASE_PROMPT);
@@ -16,6 +17,10 @@ public class OutputView {
 
     public static void printWinningNumbersPrompt() {
         System.out.print(WINNING_NUMBERS_PROMPT);
+    }
+
+    public static void printBonusNumberPrompt() {
+        System.out.print(BONUS_NUMBER_PROMPT);
     }
 
 }

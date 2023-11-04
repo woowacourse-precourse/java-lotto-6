903f15eb6fc935058216c210f2dd859f40d3c6ce
KiminSon <kimin0331@naver.com>
2023-11-04 22:57:31 +0900

    feat(Lotto): 생성된 숫자 중복 예외 처리
    validate함수에서 생성된 숫자가 중복 되면 예외 처리함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 생성된 숫자는 6개여야 합니다.");
         }
-    }
 
-    // TODO: 추가 기능 구현
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 생성된 숫자는 서로 다른 숫자여야 합니다.");
+        }
+    }
 }

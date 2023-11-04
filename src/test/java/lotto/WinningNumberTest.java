e9e71d6ddd3c65c4b8e0894678dd833c08975cdd
peppone-choi <peppone.choi@gmail.com>
2023-11-04 15:05:49 +0900

    test: 보너스 번호 및 당첨 번호 테스트 코드 수정

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -11,18 +11,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class WinningNumberTest {
 
     @Test
+    @DisplayName("당첨 번호와 보너스 번호중 하나라도 숫자가 아니면 예외가 발생한다.")
+    void winningNumberNotInteger() {
+        assertThatThrownBy(() -> new WinningNumber("가,나,다,4,a,b", "0"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "보너스"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     @DisplayName("보너스 번호가 1 ~ 45 사이가 아니면 예외가 발생한다.")
     void BonusNumberUnderOrOver() {
-        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6), 0))
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "0"))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6), 48))
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "40"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
     void duplicateWinningNumberAndBonusNumber() {
-        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6), 6))
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "6"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

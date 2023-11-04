8a8462fbec9502827c231ea764c3b2227d7a8b3b
kimwanyoung <dhks2869@gmail.com>
2023-11-04 22:26:06 +0900

    feat : 보너스 번호 객체 BonusNumber 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+public class BonusNumber {
+
+    private static final String OUT_OF_RANGE_ERROR_MESSAGE = "1 ~ 45사이의 숫자만 입력할 수 있습니다.";
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        validateOutOfRange(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateOutOfRange(int bonusNumber) {
+        if (isOutOfRange(bonusNumber)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isOutOfRange(int bonusNumber) {
+        return 45 < bonusNumber || bonusNumber < 1;
+    }
+}

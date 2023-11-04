fbf51b3b8f057c94a4db7c445d3e4c9a88cc71d8
gonixxxx <akdntm946@gmail.com>
2023-11-04 23:45:37 +0900

    feat: add exception to validate input is blank

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -5,11 +5,18 @@ import java.util.regex.Pattern;
 public class InputValidator {
     private static final String ERROR = "[ERROR] ";
     private static final String NOT_NUMBER = "숫자를 입력해주세요";
+    private static final String BLANK = "입력이 공백입니다.";
     private static final Pattern NOT_NUMBER_PATTERN = Pattern.compile("^[1-9]*$");
 
     public static void checkIsNumber(String input) {
         if (!NOT_NUMBER_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException(ERROR+NOT_NUMBER);
+        }
+    }
+
+    public static void checkIsBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(BLANK);
         }
     }
 

8647ffafb9651e768aecc0346faa2c4881131896
Jo TaeHyeong <wpg.jth@gmail.com>
2023-11-05 03:55:23 +0900

    feat: 당첨 번호와 보너스 번호 입력받는 기능 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
+import lotto.domain.BonusNumber;
 import lotto.domain.Money;
+import lotto.domain.WinningNumbers;
 import lotto.util.LottoGuideMessage;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -16,11 +18,59 @@ public class LottoGame {
     }
 
     public void start() {
-        Money money = createMoneyFromUserInput();
+        Money money = repeatUntilValidMoneyCreated();
+        WinningNumbers winningNumbers = repeatUntilValidWinningNumbersCreated();
+        repeatUntilValidBonusNumberAssigned(winningNumbers);
+    }
+
+    private void repeatUntilValidBonusNumberAssigned(WinningNumbers winningNumbers) {
+        while (true) {
+            try {
+                assignBonusNumberToWinningNumbers(winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.showMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void assignBonusNumberToWinningNumbers(WinningNumbers winningNumbers) {
+        String userInput = inputWithMessage(LottoGuideMessage.ENTER_BONUS_NUMBER);
+        winningNumbers.setBonusNumber(new BonusNumber(userInput));
+    }
+
+    private WinningNumbers repeatUntilValidWinningNumbersCreated() {
+        while (true) {
+            try {
+                return createWinningNumbersFromUserInput();
+            } catch (IllegalArgumentException e) {
+                outputView.showMessage(e.getMessage());
+            }
+        }
+    }
+
+    private WinningNumbers createWinningNumbersFromUserInput() {
+        String userInput = inputWithMessage(LottoGuideMessage.ENTER_WINNING_NUMBER);
+        return new WinningNumbers(userInput);
+    }
+
+    private Money repeatUntilValidMoneyCreated() {
+        while (true) {
+            try {
+                return createMoneyFromUserInput();
+            } catch (IllegalArgumentException e) {
+                outputView.showMessage(e.getMessage());
+            }
+        }
     }
 
     private Money createMoneyFromUserInput() {
-        outputView.showMessage(LottoGuideMessage.ENTER_PURCHASE_AMOUNT);
-        return new Money(inputView.getUserInput());
+        String userInput = inputWithMessage(LottoGuideMessage.ENTER_PURCHASE_AMOUNT);
+        return new Money(userInput);
+    }
+
+    private String inputWithMessage(LottoGuideMessage lottoGuideMessage) {
+        outputView.showMessage(lottoGuideMessage.getMessage());
+        return inputView.getUserInput();
     }
 }

3a4b790b7af038654c6ddd8fc17459a855727bda
lesw <lesw1216@gmail.com>
2023-11-05 02:05:15 +0900

    test: 구매 금액이 숫자인지 검증하는 기능 테스트 구현

---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,60 @@
+package lotto.controller;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.utils.ExceptionMessage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoControllerTest {
+
+    LottoController lottoController;
+
+    @BeforeEach
+    void init() {
+        lottoController = new LottoController();
+    }
+
+    @Test
+    @DisplayName("\"\" 입력시 예외 발생")
+    void InputNonToAmount() {
+
+        // Given
+        String inputPurchaseAmount = "";
+
+        // When
+        // Then
+        assertThatThrownBy(() -> lottoController.publishLotto(inputPurchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_BUY_AMOUNT_TYPE);
+    }
+
+    @Test
+    @DisplayName("문자 입력시 예외 발생")
+    void InputCharToAmount() {
+
+        // Given
+        String inputPurchaseAmount = "abc";
+
+        // When
+        // Then
+        assertThatThrownBy(() -> lottoController.publishLotto(inputPurchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_BUY_AMOUNT_TYPE);
+    }
+
+    @Test
+    @DisplayName("공백 입력시 예외 발생")
+    void InputSpaceToAmount() {
+
+        // Given
+        String inputPurchaseAmount = "   ";
+
+        // When
+        // Then
+        assertThatThrownBy(() -> lottoController.publishLotto(inputPurchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_BUY_AMOUNT_TYPE);
+    }
+}

171da60ecfd21016eebb8d014bac94e88c801d29
MingyeomKim <mingyum119@naver.com>
2023-11-04 20:01:05 +0900

    feat(View): 문자열을 입력하는 공통 함수 생성
    문자열을 입력받을 수 있는 공통 함수인 enterMessage()를 생성하고, 금액 요청 함수에서 선언한다.

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -2,15 +2,22 @@ package lotto.view;
 
 import static lotto.view.constants.MessageType.COST_REQUEST_MESSAGE;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.view.constants.MessageType;
 
 public final class View {
     public static String requestCost() {
         printlnMessage(COST_REQUEST_MESSAGE);
+        return enterMessage();
     }
 
     /* Output View */
     private static void printlnMessage(MessageType messageType) {
         System.out.println(messageType.getMessage());
+    }
+
+    /* Input View */
+    private static String enterMessage() {
+        return Console.readLine();
     }
 }

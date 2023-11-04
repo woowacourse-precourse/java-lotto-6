d26cd7af6d44a1a2c225d6c6cde493f5ebf14820
Genius-PAK <pyj789123@gmail.com>
2023-11-04 10:32:59 +0900

    feat(winningNumberConverterTest): winningNumberConverter 클래스 테스트 추가

---
+++ src/test/java/lotto/controller/WinningNumberConverterTest.java
@@ -0,0 +1,31 @@
+package lotto.controller;
+
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumberConverterTest {
+    private WinningNumberConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new WinningNumberConverter();
+    }
+
+    @Test
+    void testConvert() {
+        String winningNumbers = "5,3,2,7,1";
+        Map<Integer, Integer> result = converter.convert(winningNumbers);
+
+        assertThat(result).contains(
+                entry(1, 0),
+                entry(2, 1),
+                entry(3, 2),
+                entry(5, 3),
+                entry(7, 4)
+        );
+    }
+}

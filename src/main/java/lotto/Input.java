86a39c3b72b1ddb8b06b6580d1f982fc0e6d8cb7
unknown <winternrabbit@gmail.com>
2023-11-04 15:49:42 +0900

    refactor: 출력 메시지 문자열 상수 처리

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,8 +3,10 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
+    private static final String REQUEST_COST = "구입금액을 입력해 주세요.";
+
     public String buyLotto() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(REQUEST_COST);
 
         return Console.readLine();
     }

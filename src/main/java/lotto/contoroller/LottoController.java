d0b7523dc225abe45efa02c97f1cbd4258848bc2
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-05 00:56:38 +0900

    [feat] controller 구입 금액 할당 갯수만큼 로또 출력 기능

--- src/main/java/lotto/contoroller/LottoController.java
+++ src/main/java/lotto/contoroller/LottoController.java
@@ -1,13 +1,19 @@
 package lotto.contoroller;
 
 import lotto.domain.Purchase;
+import lotto.service.LottoNumberGenerator;
 import lotto.utility.ChangeNumberType;
 import lotto.view.InputValue;
+import lotto.view.OutputMessage;
 
 public class LottoController {
 
     public void startLotto() {
         int purchaseAmount = getInputPurchase();
+        // 여기는 purchaseAmount를 인자로 하는 메소드 만들어버리기
+        System.out.printf("%n%d%s%n", purchaseAmount / 1000, OutputMessage.PURCHASE.getMessage());
+        generateAndPrintLottoNumbers(purchaseAmount);
+        // 요기까지
     }
 
     private int getInputPurchase() {
@@ -20,6 +26,15 @@ public class LottoController {
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    private void generateAndPrintLottoNumbers(int purchaseAmount) {
+        LottoNumberGenerator numberGenerator = new LottoNumberGenerator();
+        int numberOfTickets = purchaseAmount / 1000;
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            System.out.println(numberGenerator.numbers);
         }
     }
 }

53e9b7a26c51464fde60c16755213c22d2b855ca
elyudwo <dudqk9696@naver.com>
2023-11-04 19:21:44 +0900

    [Test] : 로또 구입 예외 처리 테스트 작성

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class PurchaseAmountTest {
+
+
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("구입 금액이 문자일 경우", "lotto"),
+                Arguments.of("구입 금액이 1000 미만일 경우", "300"),
+                Arguments.of("구입 금액이 1000 으로 나누어 떨어지지 않을 경우", "1500"),
+                Arguments.of("구입 금액이 음수일 경우", "-1000")
+        );
+    }
+
+    @DisplayName("잘못된 구입 금액일 경우 예외가 발생한다.")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("parameters")
+    void validatePurchaseAmount(String testName, String amount) {
+        // given
+        PurchaseAmount purchaseAmount = new PurchaseAmount();
+
+        // when, then
+        assertThatThrownBy(() -> purchaseAmount.validatePurchaseAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

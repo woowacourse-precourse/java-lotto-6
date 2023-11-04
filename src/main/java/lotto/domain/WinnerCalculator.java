2fe234d414e77eaf7a7a557119ce791073877ba4
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 22:11:45 +0900

    feat(WinnerCalculator) : 당첨번호와 발행된 로또를 비교하는 기능추가
    style(OutputView) : 코드 컨벤션에 맞게 메소드명 수정
    docs : 당첨번호와 발행된 로또를 비교 부분 체크완료

---
+++ src/main/java/lotto/domain/WinnerCalculator.java
@@ -0,0 +1,55 @@
+package lotto.domain;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class WinnerCalculator {
+    private static final int INITIAL_MATCH_COUNT = 0;
+    private static final int INCREMENT = 1;
+    private final LottoFactory lottoFactory;
+    private final Lotto winningLotto;
+    private final BonusNumber bonusNumber;
+    private final LinkedHashMap<LottoRank, Integer> winnerCount;
+
+    public WinnerCalculator(LottoFactory lottoFactory, Lotto winningLotto, BonusNumber bonusNumber) {
+        this.lottoFactory = lottoFactory;
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+        this.winnerCount = initializeWinnerCount();
+    }
+
+    private LinkedHashMap<LottoRank, Integer> initializeWinnerCount() {
+        LinkedHashMap<LottoRank, Integer> map = new LinkedHashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            map.put(rank, INITIAL_MATCH_COUNT);
+        }
+        return map;
+    }
+
+    public LinkedHashMap<LottoRank, Integer> calculate() {
+        List<Lotto> tickets = lottoFactory.getTickets();
+
+        for (Lotto ticket : tickets) {
+            int matchCount = getMatchCount(ticket.getNumbers(), winningLotto.getNumbers());
+            updateWinnerCount(matchCount,false);
+        }
+        return winnerCount;
+    }
+
+    private void updateWinnerCount(int matchCount, boolean matchBonus) {
+        LottoRank rank = LottoRank.valueOf(matchCount, false);
+        winnerCount.put(rank, winnerCount.get(rank) + INCREMENT);
+    }
+
+
+    private int getMatchCount(List<Integer> ticketNumbers, List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (int number : ticketNumbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+}
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,11 +6,11 @@ import lotto.domain.Lotto;
 public class OutputView {
     public static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
 
-    public void LottoCount(long lottoCount) {
+    public void displayLottoCount(long lottoCount) {
         System.out.println(lottoCount + LOTTO_PURCHASE_MESSAGE);
     }
 
-    public void LottoReturns(List<Lotto> tickets) {
+    public void displayLottoReturns(List<Lotto> tickets) {
         for (Lotto lotto : tickets) {
             System.out.println(lotto.getNumbers());
         }

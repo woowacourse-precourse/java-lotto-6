6b66589d3ea92c1a94a293c4683781e7c797316a
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 20:10:46 +0900

    refactor: 사용자 입력 메세지를 enum 클래스에서 관리하도록 변경

---
+++ src/main/java/util/InputMessage.java
@@ -0,0 +1,20 @@
+package util;
+
+import view.InputView;
+
+public enum InputMessage {
+
+    ENTER_PRICE("구입금액을 입력해 주세요."),
+    ENTER_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    ENTER_BONUS_NUMBERS("보너스 번호를 입력해 주세요.");
+
+    private final String inputMessage;
+
+    InputMessage(String msg) {
+        this.inputMessage = msg;
+    }
+
+    public String getInputMessage() {
+        return inputMessage;
+    }
+}

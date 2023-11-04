f01dbf459c52e3e756649366eee2fa6201eb2a20
mainfn <_@mainfn.dev>
2023-11-04 21:26:39 +0900

    docs: 검증이 필요한 부분 `@apiNote`로 문서 표시

--- src/main/java/lotto/parser/StrictInputParser.java
+++ src/main/java/lotto/parser/StrictInputParser.java
@@ -1,8 +1,8 @@
-package parser;
+package lotto.parser;
 
 import java.util.Arrays;
 import java.util.List;
-import lotto.view.InputValidator;
+import lotto.validator.InputValidator;
 
 /**
  * 입력 변환을 담당하는 클래스.
@@ -23,6 +23,8 @@ public final class StrictInputParser {
      * `must`가 prefix로 붙은 메소드는 모두 검증이 이루어진다는 사실을 암시.
      * <p>
      * 변환에 실패할 경우 exception throwing이 발생합니다.
+     *
+     * @apiNote 변환 시, <h3 color="#bf0f4d">input은 반드시 숫자로 이루어진 문자열이여야 합니다.</h3>
      */
     public static long mustParseToLong(final String input) {
         InputValidator.validateNumericInput(input);
@@ -30,7 +32,7 @@ public final class StrictInputParser {
     }
 
     /**
-     * 반드시 int로 변환되어야 합니다.
+     * @apiNote 변환 시, <h3 color="#bf0f4d">input은 반드시 숫자로 이루어진 문자열이여야 합니다.</h3>
      */
     public static int mustParseToInt(final String input) {
         InputValidator.validateNumericInput(input);
@@ -38,7 +40,7 @@ public final class StrictInputParser {
     }
 
     /**
-     * 반드시 `List<Integer>`로 변환되어야 합니다.
+     * @apiNote 변환 시, <h3 color="#bf0f4d">input은 반드시 ,로 구분된, 숫자로 이루어진 문자열이여야 합니다.</h3>
      */
     public static List<Integer> mustParseFromCommasSeparatedInputToIntList(final String input) {
         return Arrays.stream(input.split(COMMAS))

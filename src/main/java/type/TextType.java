05f01f287aa8b23bc67ec33392845cc62544d673
win9 <krrr883@gmail.com>
2023-11-05 03:47:18 +0900

    [Feat]: errorType 분리
    일반 출력과 에러 출력을 분리한다

--- src/main/java/type/TextType.java
+++ src/main/java/type/TextType.java
@@ -3,11 +3,9 @@ package type;
 public enum TextType {
     START("구입금액을 입력해 주세요."),
     LOTTO_WIN_NUMBER("당첨 번호를 입력해 주세요"),
+    PRICE_TO_NUMBER("개를 구매했습니다."),
     LOTTO_BONUS("보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS("당첨 통계."),
-    INVALID_PRICE_INPUT("[ERROR] 구입 금액은 1000원 으로 나누어 떨어져야 합니다."),
-    INVALID_WINNING_NUMBER_INPUT("[ERROR] 당첨 번호는 쉼표로 구분하여 6자리를 입력 하여야 합니다."),
-    INVALID_NUMBER_RANGE("[ERROR] 숫자는 1~45 사이의 숫자여야 합니다.");
+    WINNING_STATISTICS("당첨 통계.");
 
     private final String text;
 

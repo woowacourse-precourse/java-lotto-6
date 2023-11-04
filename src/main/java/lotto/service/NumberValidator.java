15bb351fc1917a33ff15f0c5865d5f7b089d8b8d
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-05 01:09:44 +0900

    refactor - 구매 금액에 대한 예외 추가

--- src/main/java/lotto/service/NumberValidator.java
+++ src/main/java/lotto/service/NumberValidator.java
@@ -1,13 +1,28 @@
 package lotto.service;
 
+import java.util.regex.Pattern;
 import lotto.domain.LottoNumbers;
 
 public class NumberValidator {
+
+    private static final String NUMBER = "^[0-9]+$";
 
     public void validateNumberInRange(int lottoNumber) {
         if (lottoNumber < LottoNumbers.MIN_LOTTO_NUMBER.getNumber()
                 || lottoNumber > LottoNumbers.MAX_LOTTO_NUMBER.getNumber()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45까지의 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public void validatePurchasePriceIsNumber(String purchasePrice){
+        if (!Pattern.matches(NUMBER, purchasePrice)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public void validatePurchasePriceUnit(int price){
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다.");
         }
     }
 }

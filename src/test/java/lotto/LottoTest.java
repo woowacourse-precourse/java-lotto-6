e5745d37a4594aa89512aed40e7fe36682e45ba8
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-04 18:50:01 +0900

    test(model): 로또 번호 오름차순 정렬 기능 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import lotto.model.Lotto;
-import lotto.utils.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +9,7 @@ import java.util.List;
 import static lotto.utils.LottoException.DUPLICATED_LOTTO_NUMBER;
 import static lotto.utils.LottoException.LESS_THAN_MINIMUM_LOTTO_NUMBER;
 import static lotto.utils.LottoException.MORE_THEN_MAXIMUM_LOTTO_NUMBER;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -32,6 +32,7 @@ class LottoTest {
     @DisplayName("로또 번호에 1보다 작은 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByLessThanMinimumNumber() {
+        //when, then
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(LESS_THAN_MINIMUM_LOTTO_NUMBER.getMessage());
@@ -40,8 +41,22 @@ class LottoTest {
     @DisplayName("로또 번호에 45보다 큰 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByMoreThanMaximumNumber() {
+        //when, then
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(MORE_THEN_MAXIMUM_LOTTO_NUMBER.getMessage());
+    }
+
+    @DisplayName("로또 번호는 오름차순으로 정렬되어야 한다.")
+    @Test
+    void createLottoByAscendingSort() {
+        //given
+        Lotto lotto = new Lotto(List.of(5,2,3,6,1,4));
+        List<Integer> unmodifiableLotto = lotto.getLotto();
+
+        //then
+        assertThat(unmodifiableLotto)
+                .isSorted()
+                .isSortedAccordingTo(Integer::compareTo);
     }
 }

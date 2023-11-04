11c87b43cc5856adb104a22db381a635c3870960
seokmyungham <hellotjraud@gmail.com>
2023-11-04 16:42:54 +0900

    feat(PurchaseAmount): 클래스 생성 및 검증 메서드 추가
    validateAmount: 구입 금액이 1원 이상인지 검증합니다
    validateIsMultipleOfThousand: 구입 금액이 1000원 단위가 맞는지 검증합니다

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+    private int money;
+
+    public PurchaseAmount(int amount) {
+        validate(amount);
+        this.money = amount;
+    }
+
+    private void validate(int amount) {
+        validateAmount(amount);
+        validateIsMultipleOfThousand(amount);
+    }
+
+    private void validateAmount(int amount) {
+        if (amount < 1) {
+            throw new IllegalArgumentException("구입 금액은 1원 이상이어야 합니다.");
+        }
+    }
+
+    private void validateIsMultipleOfThousand(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("구입 금액이 1000원 단위이어야 합니다.");
+        }
+    }
+}

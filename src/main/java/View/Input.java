864a9bed24f861094ab00efd01c69cab429723ac
unknown <hakie2kim@gmail.com>
2023-11-04 20:35:36 +0900

    feat(src/main/java/View/): read buyers input

--- src/main/java/View/Input.java
+++ src/main/java/View/Input.java
@@ -1,2 +1,20 @@
-package View;public class Input {
+package View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    static String readReceivedAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    static String readPickedNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    static String readBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
+        return Console.readLine();
+    }
 }

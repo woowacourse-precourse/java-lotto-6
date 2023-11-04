d19f3bb9fb4f093927df5e6bdc4494f2172b35fa
김관진 <cozyschooll@gmail.com>
2023-11-05 01:33:32 +0900

    feat:당첨 번호 입력 안내 문구 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,15 @@
 package lotto.view;
 
 import static lotto.utils.GameMessage.REQUEST_AMOUNT;
+import static lotto.utils.GameMessage.REQUEST_WIN_NUMBERS;
 
 public class OutputView {
 
     public void printRequestAmount() {
         System.out.println(REQUEST_AMOUNT.getMessage());
+    }
+
+    public void printRequestWinNumbers() {
+        System.out.println(REQUEST_WIN_NUMBERS.getMessage());
     }
 }

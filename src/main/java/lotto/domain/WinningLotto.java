1a2dd873469f4e69bace8aa95daa5aca92d72fed
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-04 22:11:38 +0900

    feat: 당첨 로또 객체의 검증 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,40 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningLotto {
+    private final Lotto standardNumbers;
+
+    public Lotto getStandardNumbers() {
+        return standardNumbers;
+    }
+
+    public int getSpecialNumber() {
+        return specialNumber;
+    }
+
+    private final int specialNumber;
+
+    public WinningLotto(List<Integer> standardNumbers, int specialNumber) throws IllegalArgumentException{
+        this.standardNumbers = new Lotto(standardNumbers);
+        validateSpecialNumber(specialNumber, standardNumbers);
+        this.specialNumber = specialNumber;
+    }
+
+    private void validateSpecialNumber(int specialNumber, List<Integer> standardNumbers) throws IllegalArgumentException {
+        validateNumberSize(specialNumber);
+        validateUniqueness(specialNumber, standardNumbers);
+    }
+
+    private void validateNumberSize(int specialNumber) throws IllegalArgumentException {
+        if(!(specialNumber >= 0 && specialNumber <= 45)) {
+            throw new IllegalArgumentException("[ERROR]: 보너스 번호도 1부터 45사이여야 합니다.");
+        }
+    }
+
+    private void validateUniqueness(int specialNumber, List<Integer> standardNumbers) throws IllegalArgumentException {
+        if(standardNumbers.contains(specialNumber)) {
+            throw new IllegalArgumentException("[ERROR]: 보너스 번호와 앞서 입력하신 6자리 숫자와 달라야 합니다.");
+        }
+    }
 }
--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,2 +1,27 @@
-package lotto;public class WinningLottoTest {
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import lotto.domain.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+    List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+    @Test
+    @DisplayName("보너스 숫자가 0과 45 사이가 아닐때")
+    void winningLottoValidationTestSpecialNumberOutOfRange() {
+        assertThatThrownBy(() -> new WinningLotto(numbers, 47))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호도 1부터 45사이여야 합니다");
+    }
+
+    @Test
+    @DisplayName("보너스 숫자가 기존 6개 숫자와 겹칠때")
+    void winnngLottoBonusNumberIsNotUnique() {
+        assertThatThrownBy(() -> new WinningLotto(numbers, 3))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("6자리 숫자와 달라야 합니다.");
+    }
 }

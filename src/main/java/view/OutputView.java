a5fd7ff50fe2a879465a809090d5c2da5a20ce99
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 17:21:13 +0900

    feat: 출력 문구 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,7 @@
 package view;
 
 public class OutputView {
-
+    private final String SHOW_AMOUNT_OF_LOTTO = "개를 구매했습니다.";
+    private final String SHOW_TOTAL_RESULT = "당첨 통계";
+    private final String DIVIDER = "---";
 }

54dad5fcc3ae31d49a0472d423e1d497ce276a4b
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 04:46:44 +0900

    feat : 플레이어의 로또를 하나씩 읽어 총 점수를 계산하는 기능 구현

--- src/main/java/lotto/model/LottoGame.java
+++ src/main/java/lotto/model/LottoGame.java
@@ -1,6 +1,10 @@
 package lotto.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import lotto.constant.LottoRank;
 import lotto.vo.BonusNumber;
 
 public class LottoGame {
@@ -19,6 +23,35 @@ public class LottoGame {
         return new LottoGame(player, lotto, bonusNumber);
     }
 
+    public Map<LottoRank, Integer> calculateScore() {
+        Map<LottoRank, Integer> playerRank = new HashMap<>();
+
+        List<Lotto> playerLotteries = player.getLotteries();
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+
+        playerLotteries.forEach(playerLotto ->
+                calculateEachLotto(playerRank, winningNumbers, playerLotto));
+
+        return playerRank;
+    }
+
+    private void calculateEachLotto(Map<LottoRank, Integer> playerRank,
+                                    List<Integer> winningNumbers,
+                                    Lotto playerLottery) {
+        List<Integer> playerNumbers = playerLottery.getNumbers();
+        LottoRank rank = determineRank(countMatchedNumber(winningNumbers, playerNumbers),
+                containBonusNumber(bonusNumber, playerNumbers));
+
+        playerRank.put(rank, playerRank.getOrDefault(rank, 0) + 1);
+    }
+
+    private LottoRank determineRank(Integer matchedCount, boolean hasBonus) {
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> rank.getMatchedCount().equals(matchedCount) && rank.isIncludeBonus() == hasBonus)
+                .findFirst()
+                .orElse(LottoRank.NON_RANK);
+    }
+
     private int countMatchedNumber(List<Integer> winningNumbers, List<Integer> playerNumbers) {
         return (int) playerNumbers.stream()
                 .filter(winningNumbers::contains)
@@ -27,6 +60,7 @@ public class LottoGame {
 
     private boolean containBonusNumber(BonusNumber bonusNumber, List<Integer> playerNumbers) {
         Integer number = bonusNumber.number();
+        
         if (playerNumbers.contains(number)) {
             return true;
         }

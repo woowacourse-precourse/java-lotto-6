9e3e3daa6eb041435cfa8541e5c9b08f75371e6c
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 19:21:23 +0900

    feat(ExceptionMessage) : 예외문구들을 묶어놓은 enum추가
    docs : 공통기능 리스트 작성 및 예외문구enum 체크완료

---
+++ src/main/java/lotto/constant/ExceptionMessage.java
@@ -0,0 +1,24 @@
+package lotto.constant;
+
+public enum ExceptionMessage {
+    INPUT_SIX_NUMBER("6개의 수를 입력해야 합니다"),
+    INPUT_CORRECT_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_NUMBERS("서로 중복되지않는 수를 입력해야 합니다"),
+    INPUT_NUMBER("숫자를 입력해야 합니다"),
+    DUPLICATE_NUMBERS_LOTTO("보너스 번호는 로또 번호와 중복되지 않아야 합니다"),
+    INVALID_COMMA_USAGE("콤마를 올바르게 찍어야합니다"),
+    LOTTO_INPUT_AMOUNT_INCREMENT("1000원 단위로 입력해야 합니다"),
+    INVALID_AMOUNT_MESSAGE("1부터 45 사이의 숫자여야 합니다."),
+    INVALID_AMOUNT_PRICE_MESSAGE("너무 크거나 작은수를 입력했습니다(9,223,372,036,854,775,807이하의 1000단위를 입력하세요)");
+
+    private static final String FIRST_MESSAGE = "[ERROR] ";
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = FIRST_MESSAGE + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

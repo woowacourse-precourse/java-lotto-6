d288d32310610dfd7203ff5460df21c70f069d99
UDADDY <gustmd5715@gmail.com>
2023-11-04 17:36:57 +0900

    feat: printStat 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,6 +3,15 @@ package lotto.view;
 public class OutputView {
     public static void printStat(){
 
+    public static void printStat(OutputLottoResult outputLottoResult) {
+        System.out.println("당첨 통계\n" +
+                "---");
+
+        System.out.printf("3개 일치 (5,000원) - %d개\n", outputLottoResult.getMatch3());
+        System.out.printf("4개 일치 (50,000원) - $d개\n", outputLottoResult.getMatch4());
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", outputLottoResult.getMatch5());
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", outputLottoResult.getMatch5AndBonus());
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", outputLottoResult.getMatch6());
     }
 
     public static void printRateOfReturn(){

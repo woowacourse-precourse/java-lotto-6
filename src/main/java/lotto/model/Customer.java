728041d7586989ea09f0af950949a382cc5c5e4f
Cycrypto <jh01love@naver.com>
2023-11-04 11:51:58 +0900

    feat: 고객이 Money 클래스를 소유한 관계로 변경

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -1,2 +1,14 @@
-package lotto.model;public class Customer {
+package lotto.model;
+
+import java.util.List;
+
+public class Customer {
+    private List<Lotto> lottos;
+    private Money budget;
+    public Customer(Money budget) {
+        this.budget = budget;
+    }
+    public int getLottoCount() {
+        return budget.getMoney() / 1000;
+    }
 }

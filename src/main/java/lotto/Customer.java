dd035f876f5ddd549f343dba5cc32f6272d73839
cousim46 <wjdghldns24@naver.com>
2023-11-04 23:23:39 +0900

    fix : 구매한 로또 번호를 담는 역할을 하는 변수 데이터 타입 변경

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -1,5 +1,8 @@
 package lotto;
 
 import java.util.List;
+import java.util.Map;
 
-public record Customer(Integer buyPrice, List<List<Integer>> lottoNumbers){}
+public record Customer(Integer buyPrice, Map<Integer, List<Integer>> lottoNumbers) {
+
+}

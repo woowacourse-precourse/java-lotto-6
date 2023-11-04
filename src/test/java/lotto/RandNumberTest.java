49a7303935cdc0dcd50c4852ca1b131a86f1db20
Jiwon Na <jiwonna52@gmail.com>
2023-11-04 12:16:48 +0900

    Test : 중복되지 않는 로또 번호 뽑는 기능 테스트

--- src/test/java/lotto/RandNumberTest.java
+++ src/test/java/lotto/RandNumberTest.java
@@ -17,49 +17,20 @@ public class RandNumberTest {
         Assertions.assertThat(numbers.size()).isEqualTo(6);
     }
 
-    @DisplayName("보너스 넘버 1개 뽑기")
-    @Test
-    void createRandomBonusNumberTest() {
-        List<Integer> bonusNumber = makeBonusNumber();
-        Assertions.assertThat(bonusNumber.size()).isEqualTo(1);
-    }
-
-    @DisplayName("랜덤 넘버와 보너스 넘버가 중복되는지 확인")
+    @DisplayName("랜덤 숫자가 중복되면 다시 뽑는다.")
     @Test
     void checkDuplicateTest() {
-        List<Integer> lottoNumber = new ArrayList<>();
-        lottoNumber.add(1);
-        lottoNumber.add(2);
-        List<Integer> bonusNumber = new ArrayList<>();
-        bonusNumber.add(1);
-
-        Assertions.assertThat(checkDuplicate(lottoNumber, bonusNumber)).isEqualTo(false);
-    }
-
-    @DisplayName("랜덤 넘버와 보너스 넘버가 중복되는지 확인")
-    @Test
-    void checkNotDuplicateTest() {
-        List<Integer> lottoNumber = new ArrayList<>();
-        lottoNumber.add(1);
-        lottoNumber.add(2);
-        List<Integer> bonusNumber = new ArrayList<>();
-        bonusNumber.add(3);
-
-        Assertions.assertThat(checkDuplicate(lottoNumber, bonusNumber)).isEqualTo(true);
-    }
-
-    @DisplayName("중복된 숫자가 나오지 않을 때까지 계속 보너스 숫자 뽑기")
-    @Test
-    void reMakeBonusNumberTest() {
-        List<Integer> lottoNumber = new ArrayList<>();
-        lottoNumber.add(1);
-        lottoNumber.add(2);
-        List<Integer> bonusNumber = new ArrayList<>();
-        bonusNumber.add(1);
-
-        List<Integer> resultBonusNumber = reMakeBonusNumber(lottoNumber, bonusNumber);
-        System.out.println(resultBonusNumber.get(0));
-        Assertions.assertThat(resultBonusNumber.get(0)).isNotEqualTo(1);
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        for (int n : checkDuplicate(numbers)) {
+            System.out.println(n);
+        }
+        Assertions.assertThat(checkDuplicate(numbers)).isNotEqualTo(numbers);
     }
 
 }

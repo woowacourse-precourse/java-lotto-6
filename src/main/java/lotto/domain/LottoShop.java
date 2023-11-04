9837c8b4ce7087bd938edad7e76b3da20e588b92
yuseonjun <seonjun0906@gmail.com>
2023-11-04 19:58:30 +0900

    feat(LottoShop): 당첨번호와 보너스 번호를 생성하는 메서드 구현

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -11,12 +11,21 @@ public class LottoShop {
         this.numberGenerator = numberGenerator;
     }
 
-    public List<Lotto> createByBuyerLotto(Amount amount) {
+    public List<Lotto> createByBuyerLotto(int price) {
+        Amount amount = new Amount(price);
         int lottoCount = amount.getLottoCount();
         List<Lotto> buyerLotto = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             buyerLotto.add(new Lotto(numberGenerator.generate()));
         }
         return buyerLotto;
+    }
+
+    public Lotto createByWinningLotto(List<Integer> inputLottoNumber) {
+        return new Lotto(inputLottoNumber);
+    }
+
+    public BonusNumber createByBonusNumber(int number) {
+        return new BonusNumber(number);
     }
 }

74e89eaf6fb5c8096b9c461afba5984d0192f433
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-04 22:09:16 +0900

    feat: 메세지출력을 담당하는 기능 추가

---
+++ src/main/java/lotto/function/PrintMessageFunction.java
@@ -0,0 +1,43 @@
+package lotto.function;
+
+import lotto.constant.ProgressMessage;
+
+import java.util.List;
+
+public class PrintMessageFunction {
+
+    public void printMoneyInput() {
+        System.out.println(ProgressMessage.INPUT_MONEY);
+    }
+
+    public void printWinningNumbers() {
+        System.out.println(ProgressMessage.INPUT_WINNING_NUMBERS);
+    }
+
+    public void printBonusNumber() {
+        System.out.println(ProgressMessage.INPUT_BONUS_NUMBERS);
+    }
+
+    public void printBuyResult(int buyResult) {
+        System.out.println(buyResult + ProgressMessage.BUY_RESULT);
+    }
+
+    public void printLottoNumbers(List<Integer> lottoNumbers, int buyResult) {
+        for(int i=0;i<buyResult;i++){
+            System.out.println(lottoNumbers);
+        }
+    }
+
+    public void printWinningResult() {
+        System.out.println(ProgressMessage.OUTPUT_STATISTICS);
+        System.out.println(ProgressMessage.THREE_WINNING);
+        System.out.println(ProgressMessage.FOUR_WINNING);
+        System.out.println(ProgressMessage.FIVE_WINNING);
+        System.out.println(ProgressMessage.BONUS_WINNING);
+        System.out.println(ProgressMessage.SIX_WINNING);
+    }
+
+    public void printRateOfReturn() {
+        System.out.println(ProgressMessage.RATE_OF_RETURN);
+    }
+}

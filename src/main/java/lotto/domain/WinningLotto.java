b8fc792240746842bba22a01c2d52a2e87df7dfd
seminchoi <choicco89@gmail.com>
2023-11-04 13:26:57 +0900

    feat(domain.WinningLotto) : WinningLotto 클래스, 멤버 변수, 생성자 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+public class WinningLotto {
+    private final Lotto lotto;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto lotto, int bonusNumber) {
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+}

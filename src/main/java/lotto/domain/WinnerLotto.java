aad45cecc0cc0fd3c55c0e0929212093209d771b
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 13:04:24 +0900

    [feat] WinnerLotto 클래스 필드 정의

--- src/main/java/lotto/domain/WinnerLotto.java
+++ src/main/java/lotto/domain/WinnerLotto.java
@@ -1,4 +1,14 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinnerLotto {
+
+    private final List<Integer> winnerNumbers;
+    private final Integer bonusNumber;
+
+    public WinnerLotto(List<Integer> winnerNumbers, Integer bonusNumber) {
+        this.winnerNumbers = winnerNumbers;
+        this.bonusNumber = bonusNumber;
+    }
 }

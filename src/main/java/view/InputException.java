33c743a37b73dfc5159b15651087ca37439b89eb
zzawang <hobin0116@naver.com>
2023-11-05 03:31:00 +0900

    feat: 1,000원 단위 검사 기능 추가
    - 필요한 상수 정의
    - 인자로 받은 값을 Integer형으로 변환한다.
    - 값이 1,000원 단위가 아니면 IllegalArgumentException을 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 전달한다.

--- src/main/java/view/InputException.java
+++ src/main/java/view/InputException.java
@@ -3,12 +3,22 @@ package view;
 public class InputException {
     public static final String ERROR_MESSAGE = "[ERROR] ";
     public static final String CAN_BE_CONVERTED_TO_INTEGER_MESSAGE = "입력하신 값이 숫자가 아닙니다.";
+    public static final int PURCHASE_AMOUNT_UNIT = 1000;
+    public static final int ZERO = 0;
+    public static final String IS_VALID_AMOUNT_MESSAGE = "로또 구입 금액은 1,000원 단위로 입력해야 합니다.";
 
     public static void canBeConvertedToInteger(String userInput) {
         try {
             Integer.parseInt(userInput);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE + CAN_BE_CONVERTED_TO_INTEGER_MESSAGE);
+        }
+    }
+
+    public static void isValidAmount(String userInput) {
+        int amount = Integer.parseInt(userInput);
+        if (amount % PURCHASE_AMOUNT_UNIT > ZERO) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + IS_VALID_AMOUNT_MESSAGE);
         }
     }
 }

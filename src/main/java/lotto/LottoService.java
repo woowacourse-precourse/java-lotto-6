1be62d0e154649564cc464717fa91b34c2d6934a
happyryan <tjdgks120813@gmail.com>
2023-11-04 21:42:14 +0900

    feat(lotto LottoService): implement LottoService Class
    Added the LottoService class to handle the sequence of actions in the lottery program
    - receiving user input for money
    - generating lotto numbers
    - checking results
    - calculating returns.

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,2 +1,43 @@
-package lotto;public class LottoService {
+package lotto;
+
+import user.User;
+
+import java.util.List;
+
+public class LottoService {
+    private final int LOTTO_PRICE = 1000;
+    private final User user = new User();
+
+    public LottoService() {
+    }
+
+    public void run() {
+        Output.printGameStartMessage();
+        int userMoney = user.inputMoney();
+        int numberOfLottoes = userMoney / LOTTO_PRICE;
+
+        Output.printUserMoney(userMoney);
+        Output.printNumberOfLottoes(numberOfLottoes);
+
+        List<Lotto> lottos = LottoGenerator.generateLottos(numberOfLottoes);
+        for (Lotto lotto : lottos) {
+            Output.printLottoSixNumbers(lotto);
+        }
+
+        Output.printLottoNumbersMessage();
+        List<String> sixLottoNumbers = user.inputSixLottoNumbers();
+        Output.printLottoNumbers(sixLottoNumbers);
+
+        Output.printBonusNumberMessage();
+        String bonusNumber = user.inputBonusNumber();
+        Output.printBonusNumber(bonusNumber);
+
+        LottoResultChecker lottoResultChecker = new LottoResultChecker(lottos, sixLottoNumbers, bonusNumber);
+        lottoResultChecker.calculator();
+
+        ReturnCalculator returnCalculator = new ReturnCalculator(lottoResultChecker.lottoResult);
+        returnCalculator.calculrator();
+
+        Output.printReturn(returnCalculator.totalWinningAmount, userMoney);
+    }
 }

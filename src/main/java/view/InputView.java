c4e8a68ff6ef7ba93024cc00f8dd394c006bbfa6
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 20:11:50 +0900

    refactor: 상수 변수로 관리하던 사용자 입력 문구 삭제

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,24 +1,21 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import util.InputMessage;
 
 public class InputView {
-    private static final String ENTER_PRICE = "구입금액을 입력해 주세요.";
-    private static String ENTER_WINNGING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static String ENTER_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
-
     public static String enterPrice() {
-        System.out.println(ENTER_PRICE);
+        System.out.println(InputMessage.ENTER_PRICE);
         return Console.readLine();
     }
 
     public static String enterWinningNumbers() {
-        System.out.println(ENTER_WINNGING_NUMBERS);
+        System.out.println(InputMessage.ENTER_WINNING_NUMBERS);
         return Console.readLine();
     }
 
     public static String enterBonusNumbers() {
-        System.out.println(ENTER_BONUS_NUMBERS);
+        System.out.println(InputMessage.ENTER_BONUS_NUMBERS);
         return Console.readLine();
     }
 }

7a8025a34f221d74f6eb403041fce4383b473c58
leeedohyun <ldh19738@gmail.com>
2023-11-04 13:15:46 +0900

    fix: 입출력 요구 사항에 맞게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -14,10 +14,16 @@ public final class OutputView {
     }
 
     public static void printWinningNumbersMessage() {
+        printEmptyLine();
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
     }
 
     public static void printBonusNumberMessage() {
+        printEmptyLine();
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    private static void printEmptyLine() {
+        System.out.println();
     }
 }

d10a6cb00bc0cca912f618037b002af219ed6f4f
jdohun <wjdehgns97@naver.com>
2023-11-04 18:13:51 +0900

    Refactor: 중복 숫자 검사 기능을 Validator 로 편입

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain.model;
 
+import lotto.domain.validator.Validator;
 import lotto.enums.ErrorMessage;
 
 import java.util.*;
@@ -9,34 +10,18 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        validateDuplicateNumbers(numbers);
+        Validator.validateDuplicateNumbers(numbers);
 
         this.numbers = Collections.unmodifiableList(sortNumbers(numbers));
     }
 
-    private void validate(List<Integer> numbers) { // validate validateCountNumbers
+    private void validate(List<Integer> numbers) { // validate validateCountOfNumbers
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("숫자의 개수가 6개를 넘지 않아야 합니다.");
         }
     }
 
     // TODO: 추가 기능 구현
-    private void validateDuplicateNumbers(List<Integer> numbers) {
-        boolean hasDuplicates = false;
-        Set<Integer> duplicateChecker = new HashSet<>();
-
-        for (Integer number : numbers) {
-            if (!duplicateChecker.add(number)) {
-                hasDuplicates = true;
-                break;
-            }
-        }
-
-        if (hasDuplicates) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_HAS_DUPLICATE_NUMBERS.getMessage());
-        }
-    }
-
     private List<Integer> sortNumbers(List<Integer> numbers) {
         List<Integer> sortedNumbers = new ArrayList<>(numbers);
         Collections.sort(sortedNumbers);

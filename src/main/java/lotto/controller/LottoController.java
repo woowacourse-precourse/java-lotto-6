6578d374e061f25f569eaeea18331b0fe8685016
hyocheol An <anhyocheol@naver.com>
2023-11-05 07:29:41 +0900

    feat : 당첨 번호 입력 요청

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.util.InputVaildate;
+import lotto.view.OutputView;
 import lotto.view.inputView;
 import lotto.service.MoneyToTicket;
 import lotto.service.LottoNumberGenerate;
@@ -11,57 +12,59 @@ import lotto.domain.LottoWinning;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class LottoController {
     InputVaildate inputVaildate = new InputVaildate();
     MoneyToTicket moneyToTicket = new MoneyToTicket();
     LottoNumberGenerate lottoNumberGenerate = new LottoNumberGenerate();
     LottoBuyer lottoBuyer = new LottoBuyer();
+    LottoTicket lottoTicket = new LottoTicket();
+    LottoWinning lottoWinning = new LottoWinning();
     List<int[]> tickets = new ArrayList<>();
     public void run() {
         //로또 구매
-        buyLotto();
+        buyLotto(inputView.getRequestMoney());
 
         //로또 추첨
-//        drawLotto();
+        drawLotto(inputView.getRequestWinningNumber());
             // 당첨번호 입력
             //보너스 번호 입력
         //추첨 결과
             //추첨 결과 안내
             //수익률 안내
     }
-    public void buyLotto() {
+    public void buyLotto(String money) {
         //구매 금액 입력
-        lottoBuyer.setPurchaseAmount(Integer.parseInt(inputView.getRequestMoney()));
+        lottoBuyer.setPurchaseAmount(Integer.parseInt(money));
         //수량으로 변환
         lottoBuyer.setTicketQuantity(moneyToTicket.getTicket(lottoBuyer.getPurchaseAmount()));
 //        int lottoTicket = moneyToTicket.getTicket(moneyForBuyLotto);
         //수량만큼 로또 발행
         tickets = lottoNumberGenerate.getTickets(lottoBuyer.getTicketQuantity());
 
-        System.out.println(lottoBuyer.getPurchaseAmount());
-        System.out.println(lottoBuyer.getTicketQuantity());
-        System.out.println(tickets.size());
-        for (int[] ticket : tickets) {
-            System.out.println(Arrays.toString(ticket));
-        }
+        OutputView.displayPurchaseHistory(lottoBuyer.getTicketQuantity(),tickets);
     }
-    public void drawLotto() {
-        // 당첨 번호 입력
-        String[] inputWinnnigNumbers = (inputView.getRequestWinningNumber().split(","));
+
+    public void drawLotto(String number) {
+        // 형변한 및 유효성 검사
+        String[] strArrNumbers = number.split(",");
+        int[] intArrNumbers = Arrays.stream(strArrNumbers)
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
         // 유효성 검사
-        int[] winnnigNumbers = new int[inputWinnnigNumbers.length];
-        for (int i = 0; i < inputWinnnigNumbers.length; i++) {
-            winnnigNumbers[i] = Integer.parseInt(inputWinnnigNumbers[i]);
-        }
+
+
+        lottoWinning.setNumbers(intArrNumbers);
+
         // 당첨 번호 출력
-        System.out.println(Arrays.toString(winnnigNumbers));
+//        System.out.println(Arrays.toString(winnnigNumbers));
 
         // 보너스 번호 입력
-        int bonusNumbers = Integer.parseInt(inputView.getRequestBonusNumber());
+//        int bonusNumbers = Integer.parseInt(inputView.getRequestBonusNumber());
         // 유효성 검사
 
         // 당첨 번호 출력
-        System.out.println(bonusNumbers);
+//        System.out.println(bonusNumbers);
     }
 }
--- src/main/java/lotto/view/inputView.java
+++ src/main/java/lotto/view/inputView.java
@@ -4,9 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class inputView {
     private static final String REQUEST_MONEY = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String getREQUEST_MONEY() {
+    public static String getRequestMoney() {
         System.out.println(REQUEST_MONEY);
+        return Console.readLine();
+    }
+    public static String getRequestWinningNumber() {
+        System.out.println(REQUEST_WINNING_NUMBER);
+        return Console.readLine();
+    }
+    public static String getRequestBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER);
         return Console.readLine();
     }
 }

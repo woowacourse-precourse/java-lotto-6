1f1a9f057b48ce42230e3a333f6499c227563857
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-11-05 04:00:19 +0900

    feat: 순위 산출 시스템 객체 작성(ResultCalculationSystem.class)
    로또 당첨 결과 와 수익률 관련 클래스

---
+++ src/main/java/lotto/ResultCalculationSystem.java
@@ -0,0 +1,121 @@
+package lotto;
+
+import lotto.dto.Lotto;
+import lotto.dto.NumberType;
+import lotto.dto.Ranking;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class ResultCalculationSystem {
+    private Map<Integer, Ranking> rankingAccumulator = new HashMap<>();
+    private double rateOfReturn;
+
+    public ResultCalculationSystem() {
+        rankingAccumulator.put(5, new Ranking(5, 3, 0, 5000));
+        rankingAccumulator.put(4, new Ranking(4, 4, 0, 50000));
+        rankingAccumulator.put(3, new Ranking(3, 5, 0, 1500000));
+        rankingAccumulator.put(2, new Ranking(2, 5, 1, 30000000));
+        rankingAccumulator.put(1, new Ranking(1, 6, 0, 2000000000));
+    }
+
+    public void makeWinningResult(List<Lotto> lottos, List<Integer> lottoWinningNumbers, int bonusNumber) {
+        List<Pair> winningAndBonusNumbers = initAndSortWinningAndBonusNumbers(lottoWinningNumbers, bonusNumber);
+
+        for (int i = 0; i < lottos.size(); i++) {
+            List<Integer> lottoNumbers = lottos.get(i).getNumbers();
+            checkRankingOfLotto(lottoNumbers, winningAndBonusNumbers);
+        }
+    }
+
+    //죄송합니다... 15줄 제한 요구를 지킬 방법이 생각이 나지 않습니다....
+    private void checkRankingOfLotto(List<Integer> lottoNumbers, List<Pair> winningAndBonusNumbers) {
+        int correctWinningNumber = 0;
+        int correctBonusNumber = 0;
+        int currentPositionOfWinningAndBonusNumbers = 0;
+        int currentPositionOfLottoNumber = 0;
+        while (currentPositionOfLottoNumber < 6 && currentPositionOfWinningAndBonusNumbers < 6) {
+            int lottoNumber = lottoNumbers.get(currentPositionOfLottoNumber);
+            int winningAndBonusNumber = winningAndBonusNumbers.get(currentPositionOfWinningAndBonusNumbers).getNumber();
+            NumberType winningAndBonusNumberType = winningAndBonusNumbers.get(currentPositionOfWinningAndBonusNumbers).getType();
+            if (winningAndBonusNumber == lottoNumber && winningAndBonusNumberType == NumberType.WINNING_NUMBER) {
+                correctWinningNumber++;
+                currentPositionOfWinningAndBonusNumbers++;
+            }
+            if (winningAndBonusNumber == lottoNumber && winningAndBonusNumberType == NumberType.BONUS_NUMBER) {
+                correctBonusNumber++;
+                currentPositionOfWinningAndBonusNumbers++;
+            }
+            if (winningAndBonusNumber > lottoNumber){
+                currentPositionOfLottoNumber++;
+            }
+            if (winningAndBonusNumber < lottoNumber) {
+                currentPositionOfWinningAndBonusNumbers++;
+            }
+        }
+        int ranking = calculateRanking(correctWinningNumber, correctBonusNumber);
+        if (ranking != 0)
+            rankingAccumulator.get(ranking).increaseNumberOfWins();
+    }
+
+    private List<Pair> initAndSortWinningAndBonusNumbers(List<Integer> lottoWinningNumbers, int bonusNumber) {
+        List<Pair> winningAndBonusNumbers = new ArrayList<>();
+        lottoWinningNumbers
+                .iterator()
+                .forEachRemaining(number -> winningAndBonusNumbers.add(new Pair(number, NumberType.WINNING_NUMBER)));
+        winningAndBonusNumbers.add(new Pair(bonusNumber, NumberType.BONUS_NUMBER));
+        winningAndBonusNumbers.sort((o1, o2) -> o1.getNumber() - o2.getNumber());
+        return winningAndBonusNumbers;
+    }
+
+    private int calculateRanking(int correctWinningNumber, int correctBonusNumber) {
+        for (Ranking ranking : rankingAccumulator.values()) {
+            if (correctWinningNumber == ranking.getMatchedWinningNumberAmount() &&
+                    correctBonusNumber == ranking.getMatchedBonusNumberAmount())
+                return ranking.getRank();
+        }
+        return 0;
+    }
+
+    public void calculateRateOfReturn(int desiredPurchaseAmount) {
+        double totalMoney = 0;
+        for (int i = 1; i <= 5; i++) {
+            totalMoney = (double) rankingAccumulator.get(i).getRewardMoney() * rankingAccumulator.get(i).getNumberOfWins();
+        }
+
+        rateOfReturn = (totalMoney / desiredPurchaseAmount * 100);
+    }
+
+    public void printWinningResult() {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+
+        System.out.println("당첨 통계\n---");
+        for (int i = 5; i > 0; i--) {
+            Ranking ranking = rankingAccumulator.get(i);
+            String formattedAmount = decimalFormat.format(ranking.getRewardMoney());
+            String additionalMessage = ", 보너스 볼 일치";
+            if (ranking.getMatchedBonusNumberAmount() == 0)
+                additionalMessage = "";
+            System.out.printf("%d개 일치" + additionalMessage + " (%s원) - %d개\n", ranking.getMatchedWinningNumberAmount(), formattedAmount, ranking.getNumberOfWins());
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", rateOfReturn);
+    }
+
+    private class Pair {
+        int number;
+        NumberType type;
+
+        private Pair(int number, NumberType type) {
+            this.number = number;
+            this.type = type;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public NumberType getType() {
+            return type;
+        }
+    }
+}

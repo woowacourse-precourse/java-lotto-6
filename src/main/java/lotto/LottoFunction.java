0b86eb30ee20d7451c00f1b3abf9b3f273b18742
sosojean <thwlsdl0989@gmail.com>
2023-11-04 22:38:20 +0900

    로또 번호 생성 기능

--- src/main/java/lotto/LottoFunction.java
+++ src/main/java/lotto/LottoFunction.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class LottoFunction {
 
     Validator validator = new Validator();
@@ -10,6 +13,11 @@ public class LottoFunction {
         quotient = Integer.parseInt(input) / 1000;
         validator.isMultipleOfThousand(quotient);
         return quotient;
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 
 }

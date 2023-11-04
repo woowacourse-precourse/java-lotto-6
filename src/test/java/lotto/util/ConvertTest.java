de6795aa9c31dd1cda7da67b26d6df366d6a8c41
YongHyeon <pyh4616@naver.com>
2023-11-04 17:31:26 +0900

    Test(ConvertTest 클래스): 문자열을 정수로 변환하는 과정의 예외 발생 테스트

--- src/test/java/lotto/util/ConvertTest.java
+++ src/test/java/lotto/util/ConvertTest.java
@@ -1,11 +1,13 @@
 package lotto.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ConvertTest {
 
@@ -21,5 +23,26 @@ public class ConvertTest {
         assertThat(integers.get(3)).isEqualTo(4);
         assertThat(integers.get(4)).isEqualTo(5);
         assertThat(integers.get(5)).isEqualTo(6);
+    }
+
+    @Nested
+    @DisplayName("문자열을 정수로 변환하는 과정에서 발생하는 예외를 잡아 고의로 IllegalArgumentException을 발생시킨다.")
+    class StringToIntegerExceptionTest {
+
+        @DisplayName("문자열이 공백이면 예외가 발생한다.")
+        @Test
+        void should_ThrowIllegalArgumentException_When_Blank() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Convert.stringToInteger(""))
+                    .withMessageContaining("[ERROR]");
+        }
+
+        @DisplayName("문자열이 숫자가 아니면 예외가 발생한다.")
+        @Test
+        void should_ThrowIllegalArgumentException_When_NonInteger() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Convert.stringToInteger("a"))
+                    .withMessageContaining("[ERROR]");
+        }
     }
 }

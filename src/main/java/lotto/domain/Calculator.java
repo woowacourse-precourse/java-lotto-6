4cf46dbcc75d6bd4ad9ef3cc60f000c303b21670
UDADDY <gustmd5715@gmail.com>
2023-11-04 17:39:13 +0900

    refactor: 리스트에서 각자 계산으로 변경

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -3,37 +3,32 @@ package lotto.domain;
 import java.util.List;
 
 public class Calculator {
-    public static int calculateReturn(List<LottoResult> lottoResults) {
+    public static int calculateReturn(int lottoCount, boolean isBonus) {
         int sum = 0;
-        for (LottoResult lottoResult : lottoResults) {
-            int lottoCount = lottoResult.getLottoCount();
-            boolean isBonus = lottoResult.getIsBonus();
 
-            // 3개 일치 (5,000원) - 1개
-            // 4개 일치 (50,000원) - 0개
-            // 5개 일치 (1,500,000원) - 0개
-            // 6개 일치 (2,000,000,000원) - 0개
+        // 5개 일치, 보너스 볼 일치 (30,000,000원)
+        if (isBonus && lottoCount == 5) {
+            sum += 30000000;
+            return sum;
+        }
 
-            // 5개 일치, 보너스 볼 일치 (30,000,000원)
-            if (isBonus && lottoCount == 5) {
-                sum += 30000000;
-                continue;
-            }
-
-            switch (lottoCount) {
-                case 3:
-                    sum += 5000;
-                    break;
-                case 4:
-                    sum += 50000;
-                    break;
-                case 5:
-                    sum += 1500000;
-                    break;
-                case 6:
-                    sum += 2000000000;
-                    break;
-            }
+        // 3개 일치 (5,000원) - 1개
+        // 4개 일치 (50,000원) - 0개
+        // 5개 일치 (1,500,000원) - 0개
+        // 6개 일치 (2,000,000,000원) - 0개
+        switch (lottoCount) {
+            case 3:
+                sum += 5000;
+                break;
+            case 4:
+                sum += 50000;
+                break;
+            case 5:
+                sum += 1500000;
+                break;
+            case 6:
+                sum += 2000000000;
+                break;
         }
 
         return sum;

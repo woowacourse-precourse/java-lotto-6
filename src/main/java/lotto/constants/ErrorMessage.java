5eac098a1c3b29a821be629ada640c4366c8b5af
jongmee <ra123221@gmail.com>
2023-11-05 02:25:01 +0900

    test(PurchaseNumberTest): 구입 금액이 음수일 경우 예외 처리하는 메서드 기능 테스트하기

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -2,6 +2,7 @@ package lotto.constants;
 
 public class ErrorMessage {
     public static final String INVALID_PURCHASE_AMOUNT = "로또를 구입할 금액은 1000원 단위여야 합니다.";
+    public static final String NEGATIVE_INTEGER = "양수를 입력해주세요.";
     public static final String INVALID_LOTTO_NUMBER_RANGE = "로또 번호는 1부터 45까지의 정수입니다.";
     public static final String NOT_INTEGER = "정수를 입력해주세요.";
 }
--- src/test/java/lotto/domain/PurchaseNumberTest.java
+++ src/test/java/lotto/domain/PurchaseNumberTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static lotto.constants.ErrorMessage.INVALID_PURCHASE_AMOUNT;
+import static lotto.constants.ErrorMessage.NEGATIVE_INTEGER;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +18,17 @@ class PurchaseNumberTest {
             new PurchaseNumber(구입금액);
         }).isInstanceOf(IllegalArgumentException.class)
             .hasMessage(INVALID_PURCHASE_AMOUNT);
+    }
+
+    @Test
+    void 구입금액이_음수면_예외가_발생한다() {
+        // given
+        Integer 구입금액 = -1000;
+
+        // when & then
+        assertThatThrownBy(() -> {
+            new PurchaseNumber(구입금액);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(NEGATIVE_INTEGER);
     }
 }

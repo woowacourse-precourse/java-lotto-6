715da42c59dd8426d5a16e76ba4a86e36862a68c
JUNG GYUWON <milestogo0905@naver.com>
2023-11-04 23:32:30 +0900

    feat: 서로 다른 랜덤 숫자 6개를 뽑는 기능 추가

--- src/main/java/lotto/model/RnadomNumberGenerator.java
+++ src/main/java/lotto/model/RnadomNumberGenerator.java
@@ -1,9 +1,10 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RnadomNumberGenerator {
     public static List<Integer> generateUniqueSixNumber() {
-        return null;
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }

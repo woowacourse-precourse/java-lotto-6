01d99ade2273c1237b3979d058c6408a629f523e
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-05 02:11:25 +0900

    style: 메서드 네이밍 수정

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -5,6 +5,8 @@ public class ErrorMessage {
     public static final String OVER_NUMBER_RANGE_MESSAGE = "[ERROR] 입력가능한 숫자의 범위를 넘었습니다!";
     public static final String PRICE_NOT_DIVISIBLE_MESSAGE = "[ERROR] 구매금액이 1000원 단위로 입력되어야합니다!";
     public static final String DUPLICATED_NUMBER_MESSAGE = "[ERROR] 로또 번호가 중복되었습니다!";
-    public static final String NOT_MATCH_COUNT_MESSAGE = "[ERROR] 로또 번호의 개수가 맞지않습니다!";
+    public static final String NUMBER_COUNT_DIFFERENT_MESSAGE = "[ERROR] 로또 번호의 개수가 맞지않습니다!";
     public static final String OVER_VALUE_RANGE_MESSAGE = "[ERROR] 로또 번호의 값이 주어진 범위를 벗어났습니다!";
+    public static final String BONUS_NUMBER_DUPLICATED_MESSAGE = "[ERROR] 보너스 번호가 중복되었습니다!";
+    public static final String COMMA_COUNT_DIFFERENT_MESSAGE = "[ERROR] 콤마표시의 개수가 다릅니다!";
 }

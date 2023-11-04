0177171ae22c30c7352234076565866a90ea52ab
이진모 <jinmlee210@naver.com>
2023-11-04 15:04:26 +0900

    feat: 사용자 금액을 입력받는 기능구현

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    private static int payment;
+
+    public void inputPayment() {
+        String input = Console.readLine();
+    }
+}

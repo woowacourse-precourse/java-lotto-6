abbd939265a38c0ddf8a139255d4fac722b7cc69
minseo  jo <liging12@naver.com>
2023-11-04 13:32:26 +0900

    refactor: 수익률 출력 메시지 리팩토링
    컨트롤러는 뷰에 수익률(double)을 보내주고
    뷰는 이 값을을 소수점 1자리, 3자리마다 콤마를 찍어서 보여준다.

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,23 +1,22 @@
 package lotto.view;
 
-import lotto.domain.Ranking;
+import lotto.domain.winning.WinningRanking;
 
 public enum OutputMessage {
     REQUEST_PURCHASE_AMOUNT("구입금액을 입력해 주세요.\n"),
     REQUEST_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요.\n"),
     REQUEST_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n"),
-
     RESPONSE_PURCHASE_COUNT("\n%d개를 구매했습니다.\n"),
     RESPONSE_USER_NUMBERS_SET("%s\n"),
     RESPONSE_WINNING_STATISTICS_HEADER("당첨 통계\n---\n"),
     RESPONSE_WINNING_STATISTICS_BODY(
-            Ranking.FIFTH.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.FIFTH.getWinningAmount()) + "원) - %d개\n" +
-            Ranking.FOURTH.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.FOURTH.getWinningAmount()) + "원) - %d개\n" +
-            Ranking.THIRD.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.THIRD.getWinningAmount()) + "원) - %d개\n" +
-            Ranking.SECOND.getMatchedNumberCount() + "개 일치, 보너스 볼 일치 (" + String.format("%,d", Ranking.SECOND.getWinningAmount()) + "원) - %d개\n" +
-            Ranking.FIRST.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", Ranking.FIRST.getWinningAmount()) + "원) - %d개\n"
+            WinningRanking.FIFTH.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", WinningRanking.FIFTH.getWinningAmount()) + "원) - %d개\n" +
+            WinningRanking.FOURTH.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", WinningRanking.FOURTH.getWinningAmount()) + "원) - %d개\n" +
+            WinningRanking.THIRD.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", WinningRanking.THIRD.getWinningAmount()) + "원) - %d개\n" +
+            WinningRanking.SECOND.getMatchedNumberCount() + "개 일치, 보너스 볼 일치 (" + String.format("%,d", WinningRanking.SECOND.getWinningAmount()) + "원) - %d개\n" +
+            WinningRanking.FIRST.getMatchedNumberCount() + "개 일치 (" + String.format("%,d", WinningRanking.FIRST.getWinningAmount()) + "원) - %d개\n"
     ),
-    RESPONSE_TOTAL_RETURN("총 수익률은 %s%%입니다.\n");
+    RESPONSE_TOTAL_RETURN("총 수익률은 %,.1f%%입니다.\n");
 
     private final String message;
 

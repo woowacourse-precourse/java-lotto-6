2f1bce2f0ca891205b678fa5d05e74ef601f7365
kimwanyoung <dhks2869@gmail.com>
2023-11-05 01:01:41 +0900

    feat : 전체 로또 등수반환 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,5 +16,14 @@ public class Lottos {
         return new LottosDTO(lottos.stream()
                 .map(Lotto::getLotto)
                 .collect(Collectors.toList()));
+    }
+
+    public List<LottoResults> calculateAllOfLottoResult(WinningNumbers winningNumbers) {
+        List<LottoResults> lottoResults = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            LottoResults lottoResult = winningNumbers.calculateLottoResult(lotto);
+            lottoResults.add(lottoResult);
+        }
+        return lottoResults;
     }
 }

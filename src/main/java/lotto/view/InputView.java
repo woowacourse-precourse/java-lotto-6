cf725361b1a4b7c1660decf5e6ce699598e5abc5
maark1106 <mark1106@naver.com>
2023-11-04 22:08:04 +0900

    feat : 구입 금액이 정수형인지 검증하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,25 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     public static int inputPurchaseAmount(){
         String purchaseAmount = Console.readLine();
         validateNotEmpty(purchaseAmount);
+        validateIntegerType(purchaseAmount);
         return Integer.parseInt(purchaseAmount);
+    }
+
+    private static void validateIntegerType(String purchaseAmount) {
+        Pattern pattern = Pattern.compile("^-?\\d+$");
+        Matcher matcher = pattern.matcher(purchaseAmount);
+
+        if(!matcher.matches()){
+            throw new IllegalArgumentException("[ERROR] : 입력 값은 정수 타입이어야 합니다");
+        }
     }
 
     private static void validateNotEmpty(String purchaseAmount) {

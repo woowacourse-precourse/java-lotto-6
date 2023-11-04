f92c73839b39ce73a8f29f66f704478cfc5ca60f
K-KY <kyuyoungk@naver.com>
2023-11-05 07:04:50 +0900

    Test(GenerateLottoNumberTest) : 로또 번호 생성 테스트

---
+++ src/test/java/lotto/GenerateLottoNumberTest.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import lotto.domain.GenerateLottoNumber;
+import org.junit.jupiter.api.Test;
+
+public class GenerateLottoNumberTest extends NsTest {
+
+    @Test
+    void 로또_번호_범위_테스트() {
+        GenerateLottoNumber.generate().forEach(number -> assertThat(number).isBetween(1, 45));
+
+        }
+    @Test
+    void 정적인_로또_번호() {
+            assertThat(GenerateLottoNumber.generate()).isEqualTo(GenerateLottoNumber.generate());
+        }
+
+    @Test
+    void 로또_번호_생성() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run();
+                    assertThat(GenerateLottoNumber.generate()).isEqualTo(List.of(5, 6, 7, 8, 9, 10));
+                },
+                List.of(5, 6, 7, 8, 9, 10)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        GenerateLottoNumber.generate();
+    }
+}

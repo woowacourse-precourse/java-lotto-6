55c62029d25a3ceb1089bbc2998fcc4e53ec890b
voyager003 <miraclex000@gmail.com>
2023-11-05 00:50:16 +0900

    feat(OutputView): add method to OutputView
    ADD LOTTO_PURCHASE_MESSAGE constant, printLottoCount() method
    - constant: containing message 'purchase count'
    - method: print purchase lotto count

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -3,8 +3,13 @@ package lotto.View;
 public class OutputView {
 
     private static final String LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
 
     public static void printLottoPurchaseAmountMessage() {
         System.out.println(LOTTO_PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public static void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + LOTTO_PURCHASE_MESSAGE);
     }
 }

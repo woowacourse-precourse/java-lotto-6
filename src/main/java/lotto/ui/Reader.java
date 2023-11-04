5a0028d893fc23019f40aaa1f4140312caf83791
zangsu <zangsu_@naver.com>
2023-11-04 14:36:49 +0900

    refactor : change validate logic
    before :
    check Regex pattern
    After :
    try parse and catch Exception
    check mod operation and throw Exception

--- src/main/java/lotto/ui/Reader.java
+++ src/main/java/lotto/ui/Reader.java
@@ -3,35 +3,28 @@ package lotto.ui;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import lotto.constance.PrintConst;
 
 public class Reader {
-    static Pattern numericPattern = Pattern.compile("^[\\d]*$");
-    static Pattern mod1000Pattern = Pattern.compile("^[\\d]*+000$");
-    public static int getMoney(){
+
+    public static int getMoney() {
         String inputMoney = Console.readLine();
-        validateMoney(inputMoney);
-        return Integer.parseInt(inputMoney);
+        int money = parseInt(inputMoney);
+        validateMoneyUnit(money);
+        return money;
     }
 
-    private static void validateMoney(String inputMoney) {
-        validateNumeric(inputMoney);
-        validateMod1_000(inputMoney);
-    }
-
-    private static void validateNumeric(String inputMoney) {
-        if (isNotMatch(numericPattern, inputMoney)) {
+    private static int parseInt(String input) {
+        try{
+            return Integer.parseInt(input);
+        }catch (NumberFormatException e){
             throw new IllegalArgumentException("숫자를 입력해 주세요");
         }
     }
 
-    private static void validateMod1_000(String inputMoney){
-        if (isNotMatch(mod1000Pattern, inputMoney)) {
+    private static void validateMoneyUnit(int money) {
+        if(money % 1000 != 0){
             throw new IllegalArgumentException("1000원 단위로 입력해 주세요");
         }
-    }
-
-    private static boolean isNotMatch(Pattern pattern, String input){
-        Matcher matcher = pattern.matcher(input);
-        return !matcher.matches();
     }
 }

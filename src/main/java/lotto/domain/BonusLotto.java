0eb4c18285b62d9f7db2cdc9d48e24b84c1543ae
lee jihyeon <lejinhy@gmail.com>
2023-11-04 21:09:39 +0900

    feat: 로또 번호와의 중복 체크 메소드 추가, 생성자 입력에 로또 번호 추가

--- src/main/java/lotto/domain/BonusLotto.java
+++ src/main/java/lotto/domain/BonusLotto.java
@@ -3,18 +3,25 @@ package lotto.domain;
 public class BonusLotto {
     private final int number;
 
-    public BonusLotto(int number) {
-        validate(number);
+    public BonusLotto(int number, Lotto lottoNumbers) {
+        validate(number, lottoNumbers);
         this.number = number;
     }
 
-    private void validate(int number) {
+    private void validate(int number, Lotto lottoNumbers) {
         checkNumberInRange(number);
+        checkBonusNumberUniqueWithLottoNumbers(number, lottoNumbers);
     }
 
     private void checkNumberInRange(int number) {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException("[ERROR] 1 ~ 45 사이의 숫자를 입력해 주세요.");
+        }
+    }
+
+    private void checkBonusNumberUniqueWithLottoNumbers(int number, Lotto lottoNumbers) {
+        if (lottoNumbers.getNumbers().contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호가 중복되었습니다.");
         }
     }
 }

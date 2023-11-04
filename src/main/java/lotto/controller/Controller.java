790524ac8f49efa7b505dfaf7ead8ba2c892d6fb
ddangme <ssilver121@nav>
2023-11-05 03:50:36 +0900

    refactor: 당첨 번호, 보너스 번호 입력 및 유효성 검사 클래스 분리

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,14 +1,9 @@
 package lotto.controller;
 
-
-import camp.nextstep.edu.missionutils.Console;
-import lotto.constants.ErrorMessage;
 import lotto.constants.Message;
 import lotto.domain.Lotto;
 import lotto.service.InputService;
 import lotto.service.LottoService;
-import lotto.service.ValidationService;
-import lotto.utils.ParseUtils;
 
 import java.util.List;
 
@@ -17,9 +12,6 @@ public class Controller {
 
     InputService inputService = new InputService();
     LottoService lottoService = new LottoService();
-    ValidationService validationService = new ValidationService();
-
-    ParseUtils parseUtils = new ParseUtils();
 
     public void run() {
         createUserLottos();
@@ -28,61 +20,9 @@ public class Controller {
         showResult();
     }
 
-
-    private void showResult() {
-        System.out.println(Message.LOTTO_RESULT_MESSAGE);
-
-    }
-
-
-    private int inputBonusNumber() {
-        System.out.println("\n" + Message.BONUS_NUMBER_REQUEST_MESSAGE);
-
-        while (true) {
-            String inputBonusNumber = Console.readLine();
-            try {
-                int bonusNumber = parseUtils.parseStringToInt(inputBonusNumber);
-                Lotto winningLotto = lottoService.getWinningLotto();
-                validationService.bonusNumberValidation(bonusNumber, winningLotto);
-                return bonusNumber;
-            } catch (IllegalArgumentException e) {
-                System.out.println(ErrorMessage.BONUS_NUMBER_FORMAT.getMessage());
-            }
-        }
-    }
-
-
-
-    private void createWinningLotto() {
-        List<Integer> winningLottoNumbers = inputWinningLotto();
-        int bonusNumber = inputBonusNumber();
-
-        lottoService.setWinningLotto(winningLottoNumbers, bonusNumber);
-    }
-
-    private List<Integer> inputWinningLotto() {
-        System.out.println(Message.WINNING_NUMBER_REQUEST_MESSAGE);
-
-        while (true) {
-            String inputWinningNumbers = Console.readLine();
-            List<Integer> winningLotto = winningLottoValidationProcess(inputWinningNumbers);
-
-            if (winningLotto != null) {
-                return winningLotto;
-            }
-        }
-    }
-
-    public List<Integer> winningLottoValidationProcess(String inputWinningNumbers) {
-        try {
-            List<Integer> winningLotto = parseUtils.parseStringToIntegerList(inputWinningNumbers);
-            validationService.winningLottoNumberValidation(winningLotto);
-
-            return winningLotto;
-        } catch (IllegalArgumentException e) {
-            System.out.println(ErrorMessage.WINNING_NUMBER_FORMAT.getMessage());
-        }
-        return null;
+    public void createUserLottos() {
+        int lottoAmount = inputService.inputAmount();
+        lottoService.createUserLottos(lottoAmount);
     }
 
     private void showUserLottos() {
@@ -96,9 +36,16 @@ public class Controller {
         System.out.println();
     }
 
-    public void createUserLottos() {
-        int lottoAmount = inputService.inputAmount();
-        lottoService.createUserLottos(lottoAmount);
+    private void createWinningLotto() {
+        List<Integer> winningLottoNumbers = inputService.inputWinningLottoNumbers();
+        int bonusNumber = inputService.inputBonusNumber();
+
+        lottoService.setWinningLotto(winningLottoNumbers, bonusNumber);
+    }
+
+    private void showResult() {
+        System.out.println(Message.LOTTO_RESULT_MESSAGE);
+
     }
 
 }
--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -3,12 +3,17 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.constants.ErrorMessage;
 import lotto.constants.Message;
+import lotto.domain.Lotto;
 import lotto.utils.ParseUtils;
+
+import java.util.List;
 
 public class InputService {
 
+    LottoService lottoService = new LottoService();
     ParseUtils parseUtils = new ParseUtils();
     ValidationService validationService = new ValidationService();
+
     public int inputAmount() {
         System.out.println(Message.AMOUNT_REQUEST_MESSAGE);
 
@@ -34,6 +39,62 @@ public class InputService {
 
         return null;
     }
+
+    public List<Integer> inputWinningLottoNumbers() {
+        System.out.println(Message.WINNING_NUMBER_REQUEST_MESSAGE);
+
+        while (true) {
+            String inputWinningNumbers = Console.readLine();
+            List<Integer> winningLottoNumbers = winningLottoNumberValidationProcess(inputWinningNumbers);
+
+            if (winningLottoNumbers != null) {
+                return winningLottoNumbers;
+            }
+        }
+    }
+
+    private List<Integer> winningLottoNumberValidationProcess(String inputWinningNumbers) {
+        try {
+            List<Integer> winningLottoNumbers = parseUtils.parseStringToIntegerList(inputWinningNumbers);
+            validationService.winningLottoNumberValidation(winningLottoNumbers);
+
+            return winningLottoNumbers;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.WINNING_NUMBER_FORMAT.getMessage());
+        }
+
+        return null;
+    }
+
+    public int inputBonusNumber() {
+        System.out.println(Message.BONUS_NUMBER_REQUEST_MESSAGE);
+
+        while (true) {
+            String inputBonusNumber = Console.readLine();
+            Integer bonusNumber = bonusNumberValidationProcess(inputBonusNumber);
+
+            if (bonusNumber != null) {
+                return bonusNumber;
+            }
+        }
+    }
+
+    private Integer bonusNumberValidationProcess(String inputBonusNumber) {
+        try {
+            int bonusNumber = parseUtils.parseStringToInt(inputBonusNumber);
+            Lotto winningLotto = lottoService.getWinningLotto();
+
+            validationService.bonusNumberValidation(bonusNumber, winningLotto);
+
+            return bonusNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.BONUS_NUMBER_FORMAT.getMessage());
+        }
+
+        return null;
+    }
+
+
 
 
 }

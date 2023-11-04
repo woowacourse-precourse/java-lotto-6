6931abd3768a2adcfeac45b5b6d269e827db949e
hYe807n <rlagkdus55@gmail.com>
2023-11-04 10:40:30 +0900

    Feat: 구매 금액이 1,000 원으로 나누어 떨어지지 않으면 예외 발생

--- src/main/java/lotto/controller/StateController.java
+++ src/main/java/lotto/controller/StateController.java
@@ -1,4 +1,31 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
+
 public class StateController {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    private int money;
+
+    public void purchaseLotto() {
+        int money = Integer.parseInt(InputView.readMoney());
+        try {
+            validateMoney(money);
+            this.money = money;
+        }catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            purchaseLotto();
+        }
+    }
+
+    private void validateMoney(int money) {
+        if (money == 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000 원 이상이여야 합니다.");
+        }
+        if ((money % LOTTO_PRICE) != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000 원 단위로 입력해주셔야 합니다.");
+        }
+    }
 }

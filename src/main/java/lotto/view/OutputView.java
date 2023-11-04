abf2fd8746edcfdecb13bfc4c4410664c78e5b18
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 20:22:34 +0900

    feat(displayLottoReturns) : 발행된 로또를 출력하는 기능추가
    docs : 발행로또 출력 부분 체크완료

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
     public static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
 
     public void displayLottoCount(long lottoCount) {
         System.out.println(lottoCount + LOTTO_PURCHASE_MESSAGE);
+    }
+
+    public void displayLottoReturns(List<Lotto> tickets) {
+        for (Lotto lotto : tickets) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

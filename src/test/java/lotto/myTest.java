e0d3ef93f60923e277d49f2309d115bf87a40f48
pumpk <cong2738@gmail.com>
2023-11-04 17:58:54 +0900

    feat: 문자열 정수변환 테스트, 돈입력 매소드 테스트 추가

--- src/test/java/lotto/myTest.java
+++ src/test/java/lotto/myTest.java
@@ -1,15 +1,45 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class myTest extends NsTest {
+
+    @DisplayName("문자열 정수 변환 매소드 테스트")
+    @Test
+    void myParseIntTest() {
+        // given
+        final Application myApp = new Application();
+        final String price = "1000";
+    
+        // when
+        final int money = myApp.myParseInt(price);
+    
+        // then
+        assertThat(money).isEqualTo(1000);
+    }
+
+    @DisplayName("돈 입력 매소드 테스트")
+    @Test
+    void moneySettingTest() {
+        // given
+        final Application myApp = new Application();
+        final String price = "1000";
+    
+        // when
+        final int money = myApp.setMoney(price);
+    
+        // then
+        assertThat(money).isEqualTo(1000);
+    }
     
     @Override
     public void runMain() {

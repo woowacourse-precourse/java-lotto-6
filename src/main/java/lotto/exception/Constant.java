f52bf53e9ab8bfd9acef412a208a59cf2bc9f674
jeondui <jeondui@gmail.com>
2023-11-05 00:18:16 +0900

    feat(Constant): 정수형 문자형 상수를 함께 관리하는 기능

--- src/main/java/lotto/exception/Constant.java
+++ src/main/java/lotto/exception/Constant.java
@@ -3,16 +3,24 @@ package lotto.exception;
 public enum Constant {
 
     LOTTO_NUMBER_MIN(1),
-    LOTTO_NUMBER_MAX(45);
+    LOTTO_NUMBER_MAX(45),
+    LOTTO_NUMBERS_MAX_LENGTH(6),
+    COMMA(","),
+    BLANK(" ");
 
-    private final int value;
+    private final Object value;
 
-    Constant(int value) {
+
+    Constant(Object value) {
         this.value = value;
     }
 
-    public int getValue() {
-        return this.value;
+    public int getIntValue() {
+        return (int) this.value;
+    }
+
+    public String getCharValue() {
+        return (String) this.value;
     }
 
 }

a56ea8b29b26bd36cb7e623622db73f6502322d2
Isaac Han <isaachan233@gmail.com>
2023-11-04 15:23:44 +0900

    feat: 랜덤한 숫자들이 생성하는 메소드 추가

--- src/main/java/lotto/utils/RandomNumbersGenerator.java
+++ src/main/java/lotto/utils/RandomNumbersGenerator.java
@@ -1,9 +1,18 @@
 package lotto.utils;
 
-public class RandomNumbersGenerator implements NumbersGenerator{
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.enums.LottoEnum;
+
+import java.util.List;
+
+public class RandomNumbersGenerator implements NumbersGenerator {
 
     @Override
-    public void generate() {
+    public List<Integer> generate() {
+        return generateRandomNumbers();
+    }
 
+    private List<Integer> generateRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LottoEnum.MIN_LOTTO_NUMBER.getValue(), LottoEnum.MAX_LOTTO_NUMBER.getValue(), LottoEnum.SELECTED_NUMBERS_SIZE.getValue());
     }
 }

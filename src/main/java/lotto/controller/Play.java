12bb610650f6d3524e509a6c43824465638ed359
Jiwon Na <jiwonna52@gmail.com>
2023-11-04 18:41:57 +0900

    Feat : 로또 번호 랜덤 생성 후 로또 수 만큼 lottery에 넣어주는 기능 추가

--- src/main/java/lotto/controller/Play.java
+++ src/main/java/lotto/controller/Play.java
@@ -1,14 +1,50 @@
 package lotto.controller;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import lotto.model.Lotto;
+
+import java.util.*;
+
+import static lotto.controller.InputConverter.convertPrice;
+import static lotto.controller.InputHandler.inputPrice;
+import static lotto.controller.RandNumber.checkDuplicate;
+import static lotto.controller.RandNumber.makeRandNumber;
+import static lotto.controller.Rank.getRank;
+import static lotto.view.Message.messageAboutPrice;
+import static lotto.view.Print.printBoughtLottoCount;
 
 public class Play {
 
     private static final int lottoPrice = 1000;
     private static final int size = 6;
+    public HashMap<Rank, Integer> result;
+    public static List<Lotto> lottery;
+
+    public Play() {
+        result = new HashMap<>();
+        messageAboutPrice();
+        String tmpPrice = inputPrice();
+        int price = convertPrice(tmpPrice);
+
+        int count = calLottoCount(price);
+        printBoughtLottoCount(count);
+        lottery = new ArrayList<>();
+
+        for (int i=0; i<count; i++) {
+            lottery.add(makeLottery());
+        }
+    }
+
+    public static Lotto makeLottery() {
+        Lotto lotto = null;
+
+        List<Integer> number = makeRandNumber();
+        number = checkDuplicate(number);
+        lotto = new Lotto(number);
+
+        return lotto;
+    }
+
+
     public static int calLottoCount(int price) {
         return price / lottoPrice;
     }
@@ -25,7 +61,5 @@ public class Play {
         Set<Integer> uniqueLotto = new HashSet<>(lotto);
         return uniqueLotto.contains(bonus);
     }
-
-
 
 }

10102965c5a2d0b651ed34bfde3664079667faa8
pumpk <cong2738@gmail.com>
2023-11-04 17:38:33 +0900

    테스트생성

---
+++ src/test/java/lotto/myTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class myTest extends NsTest {
+    
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}

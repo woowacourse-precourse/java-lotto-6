17aa4d484b46f03af3f1f51d3fdb313e84bc73c0
kimdohyung <pdohyung@gmail.com>
2023-11-05 00:43:37 +0900

    feat(InputValidator): 보너스 번호 숫자 범위가 1~45가 아닌 경우 예외 처리

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -44,6 +44,7 @@ public class InputValidator {
         validateEmpty(inputBonusNumber);
         validateContainBlank(inputBonusNumber);
         int bonusNumber = convertStringToInteger(inputBonusNumber);
+        validateNumberRange(bonusNumber);
         return bonusNumber;
     }
 

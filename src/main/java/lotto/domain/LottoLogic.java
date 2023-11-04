098c465780d03a394dbd4b03e48f1acf3323b375
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 13:58:39 +0900

    feat(LottoLogic): LottoLogic 작성
    로또의 핵심 로직이 있는 LottoLogic 클래슬 작성했습니다.

---
+++ src/main/java/lotto/domain/LottoLogic.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class LottoLogic {
+    public final int purchaseAmount;
+    public final int bonusNumber;
+
+
+    public LottoLogic(int purchaseAmount, int bonusNumber) {
+        this.purchaseAmount = purchaseAmount;
+        this.bonusNumber = bonusNumber;
+    }
+}

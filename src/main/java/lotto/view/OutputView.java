b928b4618354a11b2041e646af462675ab16dda5
gundorit <packdev937@gmail.com>
2023-11-04 23:24:37 +0900

    feat (OutputView) : 구입 금액을 물어보는 문구를 출력한다

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.\n";
+
+    public static void printLottoPurchase() {
+        System.out.print(LOTTO_PURCHASE_MESSAGE);
+    }
+}

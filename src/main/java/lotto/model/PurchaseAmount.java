324882b40a2c3dc525b67af63848b39779a832c1
insu <is23193160@gmail.com>
2023-11-04 20:34:32 +0900

    feat: 구입할 수 있는 최대 로또 갯수를 반환 하는 기능 추가

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 public class PurchaseAmount {
     private static final String NON_NUMERIC_INPUT_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+    private static final int LOTTO_PRICE = 1000;
     private final int purchaseAmount;
 
     public PurchaseAmount(String inputMoney) {
@@ -11,6 +12,10 @@ public class PurchaseAmount {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NON_NUMERIC_INPUT_MESSAGE);
         }
+    }
+
+    public int getMaxLottoCountForBudget() {
+        return this.purchaseAmount / LOTTO_PRICE;
     }
 
     public int getPurchaseAmount() {

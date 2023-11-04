03f8c32c4daa4668ead3d5425fed02b5cdf90417
jmj005 <jmj00005@naver.com>
2023-11-05 02:51:26 +0900

    feat: LottoTickets 객체를 위한 LottoTicketsDto 생성

--- src/main/java/lotto/dto/LottoTicketsDto.java
+++ src/main/java/lotto/dto/LottoTicketsDto.java
@@ -1,2 +1,15 @@
-package lotto.dto;public class LottoTicketsDTO {
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoTicketsDto {
+    private final List<LottoDto> lottoTickets;
+
+    public LottoTicketsDto(List<LottoDto> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public List<LottoDto> getLottoTickets() {
+        return lottoTickets;
+    }
 }

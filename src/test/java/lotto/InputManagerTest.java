2ad24dadc307fa1a5904cad26e6c95504ef57083
ImWonYong <ssassb123@daum.net>
2023-11-05 00:31:24 +0900

    test(InputManger): 쉼표로 구분해 당첨 번호 입력 리스트 만들기 테스트 및 Input 테스트 형태 변경

--- src/test/java/lotto/InputManagerTest.java
+++ src/test/java/lotto/InputManagerTest.java
@@ -2,14 +2,47 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import org.junit.jupiter.api.Test;
 
 class InputManagerTest {
 
     @Test
+    void 당첨_로또_숫자가_아니면_예외() {
+        String input = "1, 2, 3, a, 4, 5";
+        setInput(input);
+
+        assertThatThrownBy(() -> InputManager.winningLottoInput())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자를 입력해야 합니다.");
+
+        Console.close();
+    }
+
+    @Test
+    void 당첨_로또_입력_받기() {
+        String input = " 1, 3, 4, 5, 6, 7";
+        setInput(input);
+
+        assertThat(InputManager.winningLottoInput()).contains(1, 3, 4, 5, 6, 7);
+
+        Console.close();
+    }
+
+    @Test
     void 입력_공백_제거() {
         String input = "  12,  3 4 5, 55, 11   ";
+        setInput(input);
 
-        assertThat(InputManager.removeInputSpace(input)).isEqualTo("12,345,55,11");
+        assertThat(InputManager.receiveUserInput()).isEqualTo("12,345,55,11");
+
+        Console.close();
+    }
+
+    void setInput(String input) {
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
     }
 }

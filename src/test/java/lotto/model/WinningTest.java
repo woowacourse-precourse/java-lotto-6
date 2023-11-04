312de6514f8236d6629bf1aad6b2cd972e8b8d10
whxogus215 <whxogus215@naver.com>
2023-11-04 11:34:14 +0900

    test(Winning): 당첨번호와 보너스 번호 중복 메서드 테스트

--- src/test/java/lotto/model/WinningTest.java
+++ src/test/java/lotto/model/WinningTest.java
@@ -32,4 +32,23 @@ public class WinningTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("모든 숫자는 중복되지 않아야 합니다.");
     }
+
+    @Test
+    void 당첨번호_보너스번호_중복_테스트() {
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1,2,3,4,5,6));
+        int bonusNumber = 6;
+
+        Assertions.assertThatThrownBy(() -> new Winning(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("모든 숫자는 중복되지 않아야 합니다.");
+    }
+
+    @Test
+    void 당첨번호_보너스번호_올바른입력_테스트() {
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1,2,3,4,5,6));
+        int bonusNumber = 7;
+
+        Assertions.assertThatCode(() -> new Winning(winningNumbers, bonusNumber))
+                .doesNotThrowAnyException();
+    }
 }

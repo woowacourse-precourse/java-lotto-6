26f3fd621a04cf3ef0dddc8205617503e2cf0f88
ks0927 <brian_als@naver.com>
2023-11-04 15:13:22 +0900

    feat(LottoNumber): 로또 번호가 1이상 45이하 인지  검증하는 메서드 추가

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -2,6 +2,9 @@ package lotto;
 
 public class LottoNumber {
     private static final String INVALID_TYPE_ERROR_MESSAGE = "[ERROR] 로또 번호는 숫자여야 합니다.";
+    private static final String INVALID_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1이상 45이하 여야 합니다.";
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
 
     private final int lottoNumber;
 
@@ -14,6 +17,9 @@ public class LottoNumber {
         if (isInvalidType(bonusNumber)) {
             throw new IllegalArgumentException(INVALID_TYPE_ERROR_MESSAGE);
         }
+        if (isInvalidRange(bonusNumber)) {
+            throw new IllegalArgumentException(INVALID_RANGE_ERROR_MESSAGE);
+        }
     }
 
     private boolean isInvalidType(String target) {
@@ -23,5 +29,13 @@ public class LottoNumber {
         } catch (NumberFormatException e) {
             return true;
         }
+    }
+
+    private boolean isInvalidRange(String target) {
+        int lottoNumber = Integer.parseInt(target);
+        if (lottoNumber < MINIMUM_LOTTO_NUMBER || MAXIMUM_LOTTO_NUMBER < lottoNumber) {
+            return true;
+        }
+        return false;
     }
 }

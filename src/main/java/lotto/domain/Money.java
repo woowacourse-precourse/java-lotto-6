e96de7976d09ab4e51ca139d4bd2f59d17ecb00c
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-04 14:04:37 +0900

    feat(Money): 돈이 0이하일 때 예외처리 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -7,10 +7,21 @@ public record Money(int money) {
 	public static final int UNIT = 1000;
 
 	public Money {
+		validate(money);
+	}
+
+	private void validate(int money) {
+		validateZeroOrNegativeNumber(money);
 		validateMoneyUnit(money);
 	}
 
-	private static void validateMoneyUnit(int money) {
+	private void validateZeroOrNegativeNumber(int money) {
+		if (money <= 0) {
+			throw new IllegalArgumentException(ErrorMessage.ZERO_OR_NEGATIVE_NUMBER_ERROR.getMessage());
+		}
+	}
+
+	private void validateMoneyUnit(int money) {
 		if (money % UNIT != 0) {
 			throw new IllegalArgumentException(ErrorMessage.MONEY_UNIT_ERROR.getForMatMessage(UNIT));
 		}

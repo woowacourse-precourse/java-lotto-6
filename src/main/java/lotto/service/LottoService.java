84f01cbc2e6a1c287be26a4d0cce1ea4de97c75c
seminchoi <choicco89@gmail.com>
2023-11-04 17:01:05 +0900

    feat(service.LottoService) : LottoService의 public 메소드 정의 -구현부 미완성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,8 +2,9 @@ package lotto.service;
 
 import lotto.domain.Lotto;
 import lotto.domain.WinningLotto;
+import lotto.dto.LottoReceiptDto;
+import lotto.dto.LottoResultDto;
 import lotto.random.RandomNumberGenerator;
-
 import java.util.List;
 
 public class LottoService {
@@ -14,5 +15,19 @@ public class LottoService {
 
     public LottoService(RandomNumberGenerator randomNumberGenerator) {
         this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    public LottoReceiptDto getLottoReceipt(int payment) {
+        return new LottoReceiptDto();
+    }
+
+    public void generateWinningLottoWithoutBonusNumber(List<Integer> winningLottoNumbers) {
+    }
+
+    public void generateWinningLotto(int bonusNumber) {
+    }
+
+    public LottoResultDto getLottoResult() {
+        return new LottoResultDto();
     }
 }

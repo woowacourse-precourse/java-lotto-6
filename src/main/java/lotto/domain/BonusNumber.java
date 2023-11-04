5093d72f6361931bf689d9d690151aa70ad605fb
junest66 <chlwndks333@gmail.com>
2023-11-04 23:11:33 +0900

    feat(BonusNumber): 보너스번호 유효성 검사하는 기능 추가
    - 당첨번호와 중복된 보너스번호인지 검증
    - 로또 번호의 범위밖인지 검증

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,10 +1,32 @@
 package lotto.domain;
 
+import java.util.List;
+import lotto.constants.Messages;
+import lotto.constants.Values;
+
 public class BonusNumber {
 
     final private int bonusNumber;
 
     public BonusNumber(int bonusNumber, WinningNumbers winningNumbers) {
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber, WinningNumbers winningNumbers) {
+        List<Integer> winningNumber = winningNumbers.getWinningNumber();
+        if (isDuplicateBonusNumber(bonusNumber, winningNumber)) {
+            throw new IllegalArgumentException(Messages.ERROR_DUPLICATE_BONUS_NUMBER);
+        }
+        if(isOutsideRange(bonusNumber)) {
+            throw new IllegalArgumentException(Messages.ERROR_RANGE_BONUS_NUMBER);
+        }
+    }
+
+    private boolean isDuplicateBonusNumber(int bonusNumber, List<Integer> numbers) {
+        return numbers.contains(bonusNumber);
+    }
+
+    private boolean isOutsideRange(int bonusNumber) {
+        return bonusNumber < Values.LOTTO_MIN_NUMBER || bonusNumber > Values.LOTTO_MAX_NUMBER;
     }
 }

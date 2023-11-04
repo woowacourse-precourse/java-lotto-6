3afa5a752ccb5c23558795816ff055df146a69d5
rakyun1 <dbskim234@naver.com>
2023-11-04 03:56:40 -0600

    Feat: add ValidateInput.class 유저의 금액 입력을 유효성 검사하는 메소드와, 당첨 번호 및 보너스 번호의 유효성을 검사하는 메소드를 가지고 있음

--- src/main/java/lotto/ValidateInput.java
+++ src/main/java/lotto/ValidateInput.java
@@ -1,10 +1,55 @@
 package lotto;
 
-public class ValidatInput {
+import static java.lang.Integer.parseInt;
 
-	public ValidatInput() {
+public class ValidateInput {
+	String errorMassage = "[ERROR]";
+	public ValidateInput() {
 
 	}
 
-	public void validate
+	public int validateLottoPrice(String input){
+		int lottoPrice = 0;
+		try {
+			lottoPrice = parseInt(input);
+		}catch (Exception e){
+			throw new IllegalArgumentException(errorMassage + " 정확한 금액을 입력해주세요");
+		}
+
+		if (lottoPrice%1000 != 0){
+			throw new IllegalArgumentException(errorMassage + " 천원 단위로 금액을 입력해주세요");
+		}
+		return lottoPrice/1000;
+	}
+
+	public int[] validateWinningNumber(String winNumber){
+		String[] winningNumber = winNumber.split(",");
+		int[] winningNumbers = new int[6];
+
+		if (winningNumber.length != 6){
+			throw new IllegalArgumentException(errorMassage + " 당첨 번호 입력을 확인해주세요");
+		}
+
+		for (int i = 0; i < 6; i++) {
+			winningNumbers[i] = parseInt(winningNumber[i]);
+		}
+		return winningNumbers;
+	}
+
+	public int validateBonus(String input){
+		int bonus;
+		try {
+			bonus = parseInt(input);
+		}catch (Exception e){
+			throw new IllegalArgumentException(errorMassage + "정확한 숫자를 입력해주세요");
+		}
+
+		if (bonus > 45 || bonus < 1){
+			throw new IllegalArgumentException(errorMassage + "1부터 45까지의 숫자를 입력해주세요");
+		}
+
+		return bonus;
+	}
+
+
 }

8e87d238b6c3b6a3ffe7ba20d738c4b5646a8744
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 05:06:28 +0900

    feat(Lotto) : 로또 생성 기능 구현

---
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -0,0 +1,56 @@
+package lotto.domain.lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateNumberRange(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (isOutOfRange(numbers)) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 로또 번호는 1부터 45 사이여야합니다.");
+        }
+
+    }
+    private boolean isOutOfRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < 1 || number > 45);
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        if (hasDistinctNumberIn(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되었습니다.");
+        }
+    }
+
+    private boolean hasDistinctNumberIn(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .toList()
+                .size()
+                != numbers.size();
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+}

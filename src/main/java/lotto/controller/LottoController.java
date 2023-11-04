9c5c7e058e3e21cdacd4af3dc77a566cbf572847
seminchoi <choicco89@gmail.com>
2023-11-04 17:53:09 +0900

    feat(controller.LottoController) : LottoController 클래스 생성, 멤버 변수, 메소드 정의 -구현부 미완성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final LottoService lottoService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(LottoService lottoService, InputView inputView, OutputView outputView) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+    }
+}

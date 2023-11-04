7a895ee2e11dad622acbd3d9df1381b5b99fb71b
minseo  jo <liging12@naver.com>
2023-11-04 13:50:18 +0900

    feat: 로또 생성기 기능 추가 (자동)
    로또를 자동으로 뽑는 역할을 한다.
    - 로또 판매점에 하나씩 있음

---
+++ src/main/java/lotto/domain/generator/LottoAutoGenerator.java
@@ -0,0 +1,30 @@
+package lotto.domain.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+import lotto.exception.LottoException;
+
+import java.util.*;
+
+import static lotto.contant.LottoConstants.*;
+
+public class LottoAutoGenerator {
+    public Lottos generate(int count) throws LottoException {
+        Set<Lotto> uniqueLottos = new HashSet<>();
+        while (uniqueLottos.size() < count) {
+            List<Integer> randomNumbers = generateRandomNumbers();
+            Lotto lotto = generateLotto(randomNumbers);
+            uniqueLottos.add(lotto);
+        }
+        return new Lottos(new ArrayList<>(uniqueLottos)); // 명시적 형변환
+    }
+
+    private static List<Integer> generateRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER.getValue(), LOTTO_MAX_NUMBER.getValue(), LOTTO_SIZE.getValue());
+    }
+
+    private static Lotto generateLotto(List<Integer> randomLottoNumbers) throws LottoException {
+        return new Lotto(randomLottoNumbers);
+    }
+}

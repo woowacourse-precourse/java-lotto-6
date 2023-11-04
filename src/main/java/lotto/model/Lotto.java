97ccb28f2dabcc84d97c660587c45fff52af1851
jeondui <jeondui@gmail.com>
2023-11-05 00:28:15 +0900

    feat(Lotto): 당첨 번호의 중복성 검사 기능

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -9,6 +9,7 @@ public class Lotto {
     private final List<Number> numbers; // private 변경 및 필드 추가 불가능
 
     private Lotto(List<Number> numbers) {
+        validateNumbersDuplicate(numbers);
         validateNumbersLength(numbers);
         this.numbers = numbers;
     }
@@ -16,6 +17,15 @@ public class Lotto {
     private void validateNumbersLength(List<Number> numbers) {
         if (numbers.size() != Constant.LOTTO_NUMBERS_MAX_LENGTH.getIntValue()) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
+        }
+    }
+
+    private void validateNumbersDuplicate(List<Number> numbers) {
+        if (numbers.stream()
+                .map(Number::getNumber)
+                .distinct()
+                .count() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_DUPLICATE.get());
         }
     }
 

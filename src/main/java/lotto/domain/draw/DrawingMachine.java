4f23d7cb5970667b1628559c8c2706ba7660e487
minisyu <iswater.kim@gmail.com>
2023-11-05 01:37:09 +0900

    refactor: 메소드명 변경, Money의 값 비교 메소드 사용

--- src/main/java/lotto/domain/draw/DrawingMachine.java
+++ src/main/java/lotto/domain/draw/DrawingMachine.java
@@ -7,36 +7,33 @@ import lotto.domain.lotto.Lottos;
 import lotto.domain.lotto.Money;
 
 public class DrawingMachine {
-    private static final int LOTTO_PRICE = 1_000;
     private final NumbersGenerator numbersGenerator;
 
     public DrawingMachine(NumbersGenerator numbersGenerator) {
         this.numbersGenerator = numbersGenerator;
     }
 
-    public Lottos drawLottoTicketsWithGivenMoney(Money currentMoney) {
+    public Lottos drawLottoTickets(Money currentMoney) {
         Money remainingMoney = currentMoney;
-        if (remainingMoney.getMoney() < LOTTO_PRICE) {
+        if (remainingMoney.isNotEnough()) {
             throw new IllegalArgumentException("로또를 더 이상 발행할 수 없습니다.");
         }
-        List<Lotto> purchasedLottoTickets = generateLottoTickets(remainingMoney);
-        return new Lottos(purchasedLottoTickets);
+        List<Lotto> lottoTickets = drawTicketsWith(remainingMoney);
+        return Lottos.from(lottoTickets);
     }
 
-    private List<Lotto> generateLottoTickets(Money remainingMoney) {
+    private List<Lotto> drawTicketsWith(Money remainingMoney) {
         List<Lotto> lottoTickets = new ArrayList<>();
-        while (remainingMoney.getMoney() >= LOTTO_PRICE) {
-            Lotto lotto = createLottoTickets();
+        while (remainingMoney.isEnough()) {
+            Lotto lotto = generateLottoTicket();
             lottoTickets.add(lotto);
             remainingMoney = remainingMoney.decremented();
         }
         return lottoTickets;
     }
 
-    // ref
-    private Lotto createLottoTickets() {
+    private Lotto generateLottoTicket() {
         List<Integer> numbers = numbersGenerator.generate();
         return Lotto.from(numbers);
-        // return new Lotto(numbers); fix. Lotto의 생성자가 default로 선언되었기 때문에 외부에서 선언 불가
     }
 }

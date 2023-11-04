1278526a6bbb2b6e35d6c698497205f56f6831a3
minisyu <iswater.kim@gmail.com>
2023-11-04 21:25:09 +0900

    refactor: MoneyDto에 정적 팩토리 메소드 추가

--- src/main/java/lotto/dto/input/MoneyDto.java
+++ src/main/java/lotto/dto/input/MoneyDto.java
@@ -1,4 +1,7 @@
 package lotto.dto.input;
 
-public record MoneyDto(String value) {
+public record MoneyDto(int money) {
+    public static MoneyDto from(String input) {
+        return new MoneyDto(Integer.parseInt(input));
+    }
 }

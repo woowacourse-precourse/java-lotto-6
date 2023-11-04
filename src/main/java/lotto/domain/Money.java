0bd50754187aba7e988c7eec174f453663f93d6e
LastCow9000 <sys19912002@hanmail.net>
2023-11-04 21:43:09 +0900

    refactor: 중복 코드 제거
    InputView에서 검증 처리하여 중복 메서드 제거

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,20 +1,16 @@
 package lotto.domain;
 
 public class Money {
-    private static final String NULL_OR_EMPTY_ERROR_MESSAGE = "[ERROR] 공백이나 빈 값으로는 로또 구매가 불가능합니다.";
-    private static final String NAN_ERROR_MESSAGE = "[ERROR] 숫자가 아닌 값은 입력이 불가능합니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 1000 이상의 숫자만 입력 가능합니다.";
     private static final String DIVIDE_ERROR_MESSAGE = "[ERROR] 1000원 단위로만 구매가 가능합니다.";
 
     private final long amount;
 
-    public Money(String amount) {
-        validateNullOrEmpty(amount);
-        long parsedAmount = toLong(amount);
-        validateRange(parsedAmount);
-        validateDivisibleBy1000(parsedAmount);
+    public Money(long amount) {
+        validateRange(amount);
+        validateDivisibleBy1000(amount);
 
-        this.amount = parsedAmount;
+        this.amount = amount;
     }
 
     public long getAmount() {
@@ -23,23 +19,6 @@ public class Money {
 
     public int getDividedBy1000Test() {
         return (int) (amount / 1_000);
-    }
-
-    private void validateNullOrEmpty(String amount) {
-        if (amount == null || amount.isBlank()) {
-            throw new IllegalArgumentException(NULL_OR_EMPTY_ERROR_MESSAGE);
-        }
-    }
-
-    private long toLong(String amount) {
-        long result;
-        try {
-            result = Long.parseLong(amount);
-        } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException(NAN_ERROR_MESSAGE);
-        }
-
-        return result;
     }
 
     private void validateRange(long amount) {
--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -7,35 +7,13 @@ import lotto.domain.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
-
-    @DisplayName("money가 null이나 빈 값이면 예외 발생")
-    @ParameterizedTest
-    @NullAndEmptySource
-    void validateNullOrEmptyTest(String input) {
-        // when, then
-        assertThatThrownBy(() -> new Money(input))
-                .hasMessage("[ERROR] 공백이나 빈 값으로는 로또 구매가 불가능합니다.")
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("money가 숫자가 아닌 값이면 예외 발생")
-    @ParameterizedTest
-    @ValueSource(strings = {"돈", "money", "10p"})
-    void validateNanTest(String input) {
-        // when, then
-        assertThatThrownBy(() -> new Money(input))
-                .hasMessage("[ERROR] 숫자가 아닌 값은 입력이 불가능합니다.")
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("money가 1000 미만의 값 이면 예외 발생")
     @ParameterizedTest
     @ValueSource(strings = {"-1", "0", "-9000"})
-    void validateRangeTest(String input) {
+    void validateRangeTest(long input) {
         // when, then
         assertThatThrownBy(() -> new Money(input))
                 .hasMessage("[ERROR] 1000 이상의 숫자만 입력 가능합니다.")
@@ -45,7 +23,7 @@ class MoneyTest {
     @DisplayName("money가 1000 으로 나누어 떨어지지 않을 경우 예외 발생")
     @ParameterizedTest
     @ValueSource(strings = {"1004", "1400400", "33030"})
-    void validateDivideTest(String input) {
+    void validateDivideTest(long input) {
         // when, then
         assertThatThrownBy(() -> new Money(input))
                 .hasMessage("[ERROR] 1000원 단위로만 구매가 가능합니다.")
@@ -59,7 +37,7 @@ class MoneyTest {
             "330000, 330",
             "555550000, 555550"
     })
-    void getDividedBy1000Test(String input, int expected) {
+    void getDividedBy1000Test(long input, int expected) {
         // given
         Money money = new Money(input);
 

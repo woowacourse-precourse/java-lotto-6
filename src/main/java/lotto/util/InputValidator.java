4893c171c19970e448f9f395d45d1ade491edab1
maark1106 <mark1106@naver.com>
2023-11-04 23:14:46 +0900

    refactor : InputView의 검증 로직을 InputValidator로 변경

---
+++ src/main/java/lotto/util/InputValidator.java
@@ -0,0 +1,45 @@
+package lotto.util;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    public static void validatePurchaseAmount(String purchaseAmount) {
+        validateNotEmpty(purchaseAmount);
+        validateIntegerType(purchaseAmount);
+    }
+
+    public static void validateLottoNumber(List<String> lottoNumbers) {
+        validateEachNumberNotEmpty(lottoNumbers);
+        validateEachNumberIntegerType(lottoNumbers);
+    }
+
+    private static void validateEachNumberIntegerType(List<String> lottoNumbers) {
+        for (String lottoNumber : lottoNumbers) {
+            validateIntegerType(lottoNumber);
+        }
+    }
+
+    private static void validateEachNumberNotEmpty(List<String> lottoNumbers) {
+        for (String lottoNumber : lottoNumbers) {
+            validateNotEmpty(lottoNumber);
+        }
+    }
+
+    private static void validateIntegerType(String input) {
+        Pattern pattern = Pattern.compile("^-?\\d+$");
+        Matcher matcher = pattern.matcher(input);
+
+        if(!matcher.matches()){
+            throw new IllegalArgumentException("[ERROR] : 입력 값은 정수 타입이어야 합니다");
+        }
+    }
+
+    private static void validateNotEmpty(String input) {
+        if(input.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] : 입력 값은 공백일 수 없습니다");
+        }
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,42 +5,21 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import lotto.util.InputConvertUtil;
+import lotto.util.InputValidator;
 
 public class InputView {
 
     public static int inputPurchaseAmount(){
         String purchaseAmount = Console.readLine();
-        validateNotEmpty(purchaseAmount);
-        validateIntegerType(purchaseAmount);
+        InputValidator.validatePurchaseAmount(purchaseAmount);
         return Integer.parseInt(purchaseAmount);
     }
 
     public List<Integer> inputLottoNumber(){
         String lottoInput = Console.readLine();
         List<String> lottoNumbers = InputConvertUtil.lottoNumberToLottoList(lottoInput);
-        validateEachNumberNotEmpty(lottoNumbers);
+        InputValidator.validateLottoNumber(lottoNumbers);
         return InputConvertUtil.lottoNumberToIntegerList(lottoNumbers);
-    }
-
-    private void validateEachNumberNotEmpty(List<String> lottoNumbers) {
-        for (String lottoNumber : lottoNumbers) {
-            validateNotEmpty(lottoNumber);
-        }
-    }
-
-    private static void validateIntegerType(String input) {
-        Pattern pattern = Pattern.compile("^-?\\d+$");
-        Matcher matcher = pattern.matcher(input);
-
-        if(!matcher.matches()){
-            throw new IllegalArgumentException("[ERROR] : 입력 값은 정수 타입이어야 합니다");
-        }
-    }
-
-    private static void validateNotEmpty(String input) {
-        if(input.isEmpty()){
-            throw new IllegalArgumentException("[ERROR] : 입력 값은 공백일 수 없습니다");
-        }
     }
 
 }

93a42a8bcb98a62e82c84e870a68e298e06760e2
Firedrago95 <yonghwa95@naver.com>
2023-11-04 15:24:53 +0900

    feat(Lotto) : 일치하는 로또번호 개수 구하는 기능 구현

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -10,13 +10,13 @@ public class Lotto {
     public static final String DUPLICATE_MESSAGE = "[ERROR] 로또 번호에 중복된 값이 있습니다.";
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) throws IllegalArgumentException{
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException {
         validateSize(numbers);
         validateUnique(numbers);
         this.numbers = numbers;
     }
 
-    private void validateUnique(List<Integer> numbers) throws IllegalArgumentException{
+    private void validateUnique(List<Integer> numbers) throws IllegalArgumentException {
         Set<Integer> uniqueNumbers = new HashSet<Integer>();
         if (hasDuplicate(numbers, uniqueNumbers)) {
             throw new IllegalArgumentException(DUPLICATE_MESSAGE);
@@ -27,7 +27,7 @@ public class Lotto {
         return !numbers.stream().allMatch(number -> uniqueNumbers.add(number));
     }
 
-    private void validateSize(List<Integer> numbers) throws IllegalArgumentException{
+    private void validateSize(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(INVALID_SIZE_MESSAGE);
         }
@@ -37,4 +37,11 @@ public class Lotto {
     public String toString() {
         return numbers.toString();
     }
+
+    public int countSameNumbers(Lotto lotto) {
+        return (int)lotto.numbers.stream()
+            .filter(numbers::contains)
+            .count();
+    }
+    
 }

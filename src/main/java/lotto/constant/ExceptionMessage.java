d11710c9bb3082fea55c26072240bd40c75ad079
김연진 <love_homebody@naver.com>
2023-11-05 03:26:51 +0900

    feat(WinningLotto): 당첨 번호와 보너스 번호가 중복되는 숫자일 경우 예외 발생

--- src/main/java/lotto/constant/ExceptionMessage.java
+++ src/main/java/lotto/constant/ExceptionMessage.java
@@ -7,7 +7,8 @@ public enum ExceptionMessage {
     INVALID_INPUT_NUMBER("6개의 숫자를 입력하여 주십시오."),
     DUPLICATE_NUMBER("중복되지 않는 6개의 숫자를 입력하여 주십시오."),
     INCORRECT_RANGE("1~45 사이의 숫자만 입력할 수 있습니다."),
-    NOT_INTEGER("숫자만 입력할 수 있습니다.");
+    NOT_INTEGER("숫자만 입력할 수 있습니다."),
+    DUPLICATE_BONUS_NUMBER("로또 당첨 번호와 보너스 번호가 중복됩니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.Lotto;
+import lotto.constant.ExceptionMessage;
 
 public class WinningLotto {
 
@@ -8,10 +9,16 @@ public class WinningLotto {
     private final BonusNumber bonusNumber;
 
     public WinningLotto(Lotto lotto, BonusNumber bonusNumber) {
+        validateDuplicate(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateDuplicate(Lotto lotto, BonusNumber bonusNumber) {
+        if (lotto.isContain(bonusNumber.getValue())) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_BONUS_NUMBER.getMessage());
+        }
+    }
     public boolean isContain(int number) {
         return lotto.isContain(number);
     }

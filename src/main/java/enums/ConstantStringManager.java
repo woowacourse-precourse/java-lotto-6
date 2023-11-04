c7eca662db09dcc2b79720cd746b8957324dc4f8
star-candy <el0710@naver.com>
2023-11-04 21:30:26 +0900

    feat(lotto): 당첨 통계를 출력하는 기능

--- src/main/java/enums/ConstantStringManager.java
+++ src/main/java/enums/ConstantStringManager.java
@@ -4,6 +4,7 @@ public enum ConstantStringManager {
     LOTTO_NUMBER_NOTIFY("%d개를 구매했습니다.\n"),
     WINNING_NUMBER_INPUT_NOTIFY("당첨 번호를 입력해 주세요"),
     BONUS_NUMBER_INPUT_NOTIFY("보너스 번호를 입력해 주세요"),
+    WINNING_STATICS_NOTIFY("당첨 통계\n___\n"),
     LOTTO_SAME_THREE("3개 일치 (5,000원) - %d개\n"),
     LOTTO_SAME_FOUR("4개 일치 (50,000원) - %d개\n"),
     LOTTO_SAME_FIVE("5개 일치 (1,500,000원) - %d\n"),
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,13 @@
 package view;
 
+import java.util.List;
 import enums.ConstantStringManager;
-
 public class OutputView {
-
+    final int firstIndex = 1;
+    final int secondIndex = 2;
+    final int thirdIndex = 3;
+    final int forthIndex = 4;
+    final int fifthIndex = 5;
     public void printLottoPriceInputNotify() {
     String message = ConstantStringManager.LOTTO_PRICE_INPUT_NOTIFY.getMessage();
     System.out.println(message);
@@ -22,5 +26,14 @@ public class OutputView {
     public void printBonusNumberInputNotify() {
         String message = ConstantStringManager.BONUS_NUMBER_INPUT_NOTIFY.getMessage();
         System.out.println(message);
+    }
+
+    public void printWinningStatics(List<Integer> staticsValue) {
+        System.out.print(ConstantStringManager.WINNING_STATICS_NOTIFY);
+        System.out.printf(ConstantStringManager.LOTTO_SAME_THREE.getMessage(), staticsValue.get(firstIndex));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_FOUR.getMessage(), staticsValue.get(secondIndex));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_FIVE.getMessage(), staticsValue.get(thirdIndex));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_FIVE_BONUS.getMessage(), staticsValue.get(forthIndex));
+        System.out.printf(ConstantStringManager.LOTTO_SAME_SIX.getMessage(), staticsValue.get(fifthIndex));
     }
 }

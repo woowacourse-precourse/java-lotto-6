00f993cbc2868bf540494f8e2f72c465f2eac76f
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 03:05:31 +0900

    feat: 타입 오류 메시지 추가

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -6,7 +6,9 @@ public enum ErrorMessage {
     INVALID_LOTTO_LENGTH("로또 번호의 개수가 맞지 않습니다"),
     INVALID_LOTTO_NUMBER_RANGE("로또 번호의 범위가 맞지 않습니다"),
     NOT_ENOUGH_PURCHASE_AMOUNT("로또를 구입하기에 부족한 금액입니다"),
-    INVALID_PURCHASE_AMOUNT("금액은 로또 가격의 배수로 입력해야 합니다");
+    INVALID_PURCHASE_AMOUNT("금액은 로또 가격의 배수로 입력해야 합니다"),
+    INVALID_TYPE("잘못된 타입입니다"),
+    ;
 
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 

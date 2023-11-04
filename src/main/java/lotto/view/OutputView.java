2e87673d401903425985ac7d987b908e9c1089db
seokmyungham <hellotjraud@gmail.com>
2023-11-04 14:51:53 +0900

    feat(OutputView): 클래스 생성 및 상수 정의
    클래스 내부에서 쓰일 문자열 상수를 정의했습니다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+}

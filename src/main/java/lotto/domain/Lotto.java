5279cee7e8f861f5de988618ba0de62e02baaf81
yooooonshine <hl3470@naver.com>
2023-11-04 11:25:51 +0900

    Refactor: Lotto 클래스 validate함수 추가
    Lotto 일급 컬렉션은 숫자가 6개이고, 중복되면 안되므로 validate함수를 추가하였다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,31 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSize(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        List<Integer> deduplicatedNumbers = numbers.stream()
+                                                .distinct()
+                                                .collect(Collectors.toList());
+        if (deduplicatedNumbers.size() == numbers.size()) {
+            return;
+        }
+        throw new IllegalArgumentException();
     }
 
     public List<Integer> getNumbers() {

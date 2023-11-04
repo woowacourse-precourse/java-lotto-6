0b87ec4a62ef6378b0a7bc28053af89186510977
jongmin Lee <rjsqktod1@gmail.com>
2023-11-04 21:40:09 +0900

    test(amount): 구매금액 관련 테스트 코드 추가
    puchaseAmount의 예외 처리를 테스트하는 코드 추가
    
    Signed-off-by: jongmin Lee <rjsqktod1@gmail.com>

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseAmountTest {
+    @DisplayName("로또 구매 금액이 로또 하나의 금액 미만이라면 예외가 발생한다.")
+    @Test
+    void 로또_최소구매금액_예외처리_검증() {
+        int invalidAmount = 700;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            PurchaseAmount.from(invalidAmount);
+        });
+
+        assertTrue(exception.getMessage().contains("최소 금액 미만의 금액 입력은 불가 하다"));
+    }
+    @DisplayName("로또 구매 금액이 로또 하나 금액의 배수가 아니라면 예외가 발생한다.")
+    @Test
+    void 로또_구매금액_배수_예외처리_검증() {
+        int invalidAmount = 1010;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            PurchaseAmount.from(invalidAmount);
+        });
+
+        assertTrue(exception.getMessage().contains("로또 금액의 배수가 아닌 금액 입력은 불가 하다 "));
+    }
+}

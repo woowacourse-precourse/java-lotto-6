4a693906a799e522e9179000e594413c92641a8d
chae401 <chae401@sju.ac.kr>
2023-11-04 19:19:43 +0900

    feat(BeforeScreen): add printPurchasedLottos method

--- src/main/java/lotto/view/BeforeScreen.java
+++ src/main/java/lotto/view/BeforeScreen.java
@@ -5,9 +5,15 @@ import lotto.model.dto.PayDTO;
 
 public class BeforeScreen {
     private static final String REQEUST_PAYMENT= "구입금액을 입력해 주세요.";
+    private static final String INFO_OF_NUM_OF_LOTTOS = "개를 구매했습니다.";
     public static PayDTO readPayment(){
         System.out.println(REQEUST_PAYMENT);
         String pay = Console.readLine();
         return PayDTO.from(pay);
+    }
+
+    public static void printPurchasedLottos(int numberOfLottos, String resultOfPurchasedLottos) {
+        System.out.println(String.valueOf(numberOfLottos) + INFO_OF_NUM_OF_LOTTOS);
+        System.out.println(resultOfPurchasedLottos);
     }
 }

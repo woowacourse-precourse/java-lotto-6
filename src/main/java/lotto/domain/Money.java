485029a433e1ac4458721f5072b44d2a6e5849ee
SeongIl <biuld1234@gmail.com>
2023-11-04 17:01:13 +0900

    refactor: create Money 클래스

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import lotto.Validator;
+
+public class Money {
+    private Integer money;
+
+    public Money(String userInput) {
+        checkMoney(userInput);
+    }
+
+    private void checkMoney(String userInput) {
+        Validator.checkNumber(userInput);
+        money = Integer.parseInt(userInput);
+
+        Validator.checkPositiveNumber(money);
+        Validator.checkZero(userInput);
+
+        Validator.checkRemainder(money);
+    }
+}

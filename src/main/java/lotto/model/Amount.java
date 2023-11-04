a6f8cd2384d1f8d46d7e9c4fed4dd97f3560835f
jihun <pjh2996@naver.com>
2023-11-04 18:55:43 +0900

    feat : 예외처리_구입 금액이 1,000원 단위인지 확인하는 기능 구현

--- src/main/java/lotto/model/Amount.java
+++ src/main/java/lotto/model/Amount.java
@@ -1,10 +1,20 @@
 package lotto.model;
 
+import static lotto.util.message.Error.MUST_MONEY_UNIT;
+
 public class Amount {
 
+    private final static int MONEY_UNIT = 1000;
     private final int money;
 
     public Amount(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money % MONEY_UNIT != 0 || money <= 0) {
+            throw new IllegalArgumentException(MUST_MONEY_UNIT);
+        }
     }
 }
--- src/main/java/lotto/util/message/Error.java
+++ src/main/java/lotto/util/message/Error.java
@@ -3,4 +3,5 @@ package lotto.util.message;
 public final class Error {
 
     public final static String MUST_INTEGER = "[ERROR] 정수만 입력 가능합니다.";
+    public final static String MUST_MONEY_UNIT = "[ERROR] 금액은 1000원 단위만 가능합니다.";
 }

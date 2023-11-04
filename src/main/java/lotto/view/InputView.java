ea993ccbe8a7bc340907f31cbff48847cf7cdfcf
sangchu <didi1484@gmail.com>
2023-11-04 23:45:54 +0900

    feat: askWinnerNumbers 메서드 추가 (당첨 번호 입력)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.constants.ExceptionMessage.HAS_LETTER;
 import static lotto.constants.ExceptionMessage.IS_NOT_THOUSAND_UNIT;
+import static lotto.view.constants.Notice.ASK_WINNER_NUMBERS;
 
 public class InputView {
     public static int askPurchaseAmount() {
@@ -12,6 +13,12 @@ public class InputView {
         int won = Integer.parseInt(input);
         validateThousandUnit(won);
         return won;
+    }
+
+    public static String askWinnerNumbers() {
+        System.out.println();
+        System.out.println(ASK_WINNER_NUMBERS.getMessage());
+        return readLine();
     }
 
     private static void validateType(String input) {

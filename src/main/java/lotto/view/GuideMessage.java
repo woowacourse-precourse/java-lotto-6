03229e371f8ec568a4be3a2ea44ac67ed4779d9e
박승민 <tmddudals369@naver.com>
2023-11-05 04:08:46 +0900

    feat(outputView): 로또 당첨 번호 입력 안내 메세지를 출력하는 기능 구현

--- src/main/java/lotto/view/GuideMessage.java
+++ src/main/java/lotto/view/GuideMessage.java
@@ -1,7 +1,8 @@
 package lotto.view;
 
 public enum GuideMessage {
-    PURCHASE_GUIDE_MESSAGE("구입금액을 입력해 주세요.");
+    PURCHASE_GUIDE_MESSAGE("구입금액을 입력해 주세요."),
+    WINNING_NUMBERS_GUIDE_MESSAGE("당첨 번호를 입력해 주세요.");
 
     private final String text;
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public void printPurchaseGuideMessage() {
         System.out.println(PURCHASE_GUIDE_MESSAGE.getText());
     }
+
+    public void printWinningNumbersGuideMessage() {
+        System.out.println(WINNING_NUMBERS_GUIDE_MESSAGE.getText());
+    }
 }

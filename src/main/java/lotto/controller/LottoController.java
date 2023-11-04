e2299efb59e1d9a5edeb29d819a025218dc8947c
kongji <kongji4092@gmail.com>
2023-11-04 16:24:23 +0900

    feat: 당첨번호 입력 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -17,6 +17,20 @@ public class LottoController {
         OutputView.printPurchaseLotto(lottoCount);
         Lottos lottos = new Lottos(lottoService.generateLotto(lottoCount));
         OutputView.printLottosValue(lottos.toResponseDto());
+        // 당첨 번호를 입력해 주세요.
+        Lotto userLotto = getUserLottoNumber();
+        // 보너스 번호를 입력해 주세요.
+        OutputView.printGetBonusNumber();
+        InputView.getUserBonusNumber();
+        // 당첨 통계
+
+        // 수익률
+    }
+
+    private static Lotto getUserLottoNumber() {
+        OutputView.printGetInputNumber();
+        List<Integer> userLottoNumbers = InputView.getUserLottoNumber();
+        return new Lotto(userLottoNumbers);
     }
 
     private int getLottoCount(int purchasePrice) {
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -17,5 +20,21 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(PRICE_NOT_INTEGER_ERROR_MESSAGE);
         }
+    }
+
+    public static List<Integer> getUserLottoNumber() {
+        String input = Console.readLine();
+        String[] numbers = getSplit(input);
+        return mapToInteger(numbers);
+    }
+
+    private static String[] getSplit(String input) {
+        return input.split(",");
+    }
+
+    private static List<Integer> mapToInteger(String[] numbers) {
+        return Arrays.stream(numbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

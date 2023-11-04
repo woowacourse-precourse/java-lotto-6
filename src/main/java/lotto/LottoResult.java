922663350360aa565365f3c5383da2d2010b86f2
pjaehyun <show6756@gmail.com>
2023-11-04 18:17:53 +0900

    feat: 추가 문자열 List<Integer>로 변환하는 함수

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,12 +1,25 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoResult {
 
-    private final int resultNumbers;
+    private final List<Integer> resultNumbers;
     private final int bonusNumber;
 
-    public LottoResult(String input1, String input2) {
-        this.resultNumbers = Integer.parseInt(input1);
-        this.bonusNumber = Integer.parseInt(input2);
+    public LottoResult(String resultNumbers, String bonusNumber) {
+        this.resultNumbers = convertToIntegerList(resultNumbers);
+        this.bonusNumber = Integer.parseInt(bonusNumber);
+    }
+
+    private List<Integer> convertToIntegerList(String resultNumbers) {
+        String[] numbers = resultNumbers.split(",");
+        List<Integer> list = new ArrayList<>();
+
+        for (String number : numbers) {
+            list.add(Integer.parseInt(number.trim()));
+        }
+        return list;
     }
 }

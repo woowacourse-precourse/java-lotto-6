98b3ff916ae0892bcc5eef303a5cea73e3f0f47c
sangchu <didi1484@gmail.com>
2023-11-04 23:29:07 +0900

    rename: 변수명 변경

--- src/main/java/lotto/view/constants/Notice.java
+++ src/main/java/lotto/view/constants/Notice.java
@@ -2,10 +2,9 @@ package lotto.view.constants;
 
 public enum Notice {
     ASK_PURCHASE_PRICE("구입금액을 입력해 주세요."),
-    NUMBER_OF_TICKETS("개를 구매했습니다."),
+    PURCHASE_RESULT("개를 구매했습니다."),
     ASK_WINNER_NUMBERS("당첨 번호를 입력해 주세요."),
-    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    ;
+    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     private final String message;
 

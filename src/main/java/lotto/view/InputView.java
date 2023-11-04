a8d1886efe5e0f0e93a514f57033a999f1717bc2
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 02:24:16 +0900

    feat : 뷰에서 입력값에 대한 유효성을 검증하도록 코드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,28 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.InputValidator;
 
 public class InputView {
 
     public Integer inputPurchaseAmount() {
         String amount = Console.readLine();
+        InputValidator.validatePurchaseAmount(amount);
+
         return Integer.valueOf(amount);
     }
 
     public String inputWinningNumbers() {
         String winningNumbers = Console.readLine();
+        InputValidator.validateWinningNumbers(winningNumbers);
+
         return winningNumbers;
     }
 
     public Integer inputBonusNumbers() {
         String bonusNumber = Console.readLine();
+        InputValidator.validateBonusNumber(bonusNumber);
+
         return Integer.valueOf(bonusNumber);
     }
 }

a79ec0c64ca178e29f0401b51ec48350643ebea6
soozzang <soozzang@inha.edu>
2023-11-04 16:12:44 +0900

    Feat(BonusNum): 보너스 번호 클래스 생성 및 생성자와 getter 생성

---
+++ src/main/java/lotto/model/domain/BonusNum.java
@@ -0,0 +1,14 @@
+package lotto.model.domain;
+
+public class BonusNum {
+
+    private final int bonusNum;
+
+    BonusNum(int bonusNum) {
+        this.bonusNum = bonusNum;
+    }
+
+    public int getBonusNum() {
+        return this.bonusNum;
+    }
+}

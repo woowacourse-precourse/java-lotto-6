d3b2bf85bdc9caa87e757fa725001e6686a5de75
acycoco <pink10257@naver.com>
2023-11-05 01:49:52 +0900

    feat(Lotto): 로또 번호 중복을 확인하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,6 +16,16 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isDuplicate(final List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        return numbers.size() != uniqueNumbers.size();
     }
 
     // TODO: 추가 기능 구현

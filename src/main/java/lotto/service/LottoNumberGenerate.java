abef67c083e81890ac1b7bc41edde1c06d9bb8ab
hyocheol An <anhyocheol@naver.com>
2023-11-05 06:01:48 +0900

    refactor : domain class 변경으로 인해 이전 구현한 비즈니스 로직 코드 리팩토링

--- src/main/java/lotto/service/LottoNumberGenerate.java
+++ src/main/java/lotto/service/LottoNumberGenerate.java
@@ -7,11 +7,26 @@ import java.util.List;
 
 public class LottoNumberGenerate {
     RandomNumberGenerate randomNumberGenerate = new RandomNumberGenerate();
-    public List<List<Integer>> getLottoNumber(int lottoTicket) {
-        List<List<Integer>> lottoNumbers = new ArrayList<List<Integer>>();
-        for (int i = 0; i < lottoTicket; i++) {
-            lottoNumbers.add(randomNumberGenerate.getRandomNumber());
+    public List<int[]> getTickets(int ticketQuantity) {
+        List<int[]> tickets = new ArrayList<>();
+        for (int buy = 1; buy <= ticketQuantity; buy++) {
+            int[] numbers = new int[6];
+            numbers = randomNumberGenerate.getRandomNumber().stream()
+                    .mapToInt(i -> i)
+                    .toArray();
+            tickets.add(numbers);
         }
-        return lottoNumbers;
+        return tickets;
+//        List<List<Integer>> lottoNumbers = new ArrayList<List<Integer>>();
+//        for (int i = 0; i < lottoTicket; i++) {
+//            int[] lottoNumber = new int[6];
+//            lottoNumbers = randomNumberGenerate.getRandomNumber().stream()
+//                                .mapToInt(i -> i)
+//                                .toArray();
+//
+//                    Arrays. (randomNumberGenerate.getRandomNumber());
+//            lottoNumbers.add();
+//        }
+//        return lottoNumbers;
     }
 }

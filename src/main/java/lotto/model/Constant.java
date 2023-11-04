802d0f97e97fba60222cb619ad3800a768dbf22c
DongHyun <kimdh0315@naver.com>
2023-11-04 20:40:01 +0900

    feat(main/model/Constant) : define constants variables for lotto game

---
+++ src/main/java/lotto/model/Constant.java
@@ -0,0 +1,16 @@
+package lotto.model;
+
+public class Constant {
+    /* constants for reward price */
+    public static final int FIRST_REWARD = 2_000_000_000;
+    public static final int SECOND_REWARD = 30_000_000;
+    public static final int THIRD_REWARD = 1_500_000;
+    public static final int FOURTH_REWARD = 50_000;
+    public static final int FIFTH_REWARD = 5_000;
+
+    /* constants for lotto information */
+    public static final int LOTTO_START_NUMBER = 1;
+    public static final int LOTTO_END_NUMBER = 45;
+    public static final int LOTTO_SELECT_NUMBER = 6;
+    public static final int LOTTO_PRICE = 1_000;
+}

c16d2b193f706517fe6866a4eca3aa37418f98c1
chae401 <chae401@sju.ac.kr>
2023-11-04 18:45:42 +0900

    feat(LottoController): issue lottos
    금액 만큼 로또 발행하기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,17 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.model.LottoService;
+import lotto.model.dto.PayDTO;
+import lotto.view.BeforeScreen;
+
+public class LottoController {
+    private final LottoService lottoService;
+
+    public LottoController(){
+        PayDTO pay = BeforeScreen.readPayment();
+        this.lottoService = new LottoService(pay.getNumberOfLotto());
+    }
+    public void execute(){
+
+    }
 }
---
+++ src/main/java/lotto/model/LottoService.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoService {
+    private final List<Lotto> lottos;
+    public LottoService(int numberOfLotto){
+        this.lottos = createLottos(numberOfLotto);
+    }
+    private List<Lotto> createLottos(int numberOfLotto){
+        List<Lotto> new_lottos = new ArrayList<>();
+        IntStream.range(0, numberOfLotto).forEach(i -> new_lottos.add(createLotto()));
+        return new_lottos;
+    }
+
+    private Lotto createLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+}

954f59ba3a679e19ca106f8ae35b5dab9f54460d
박찬건 <jg9870@naver.com>
2023-11-04 18:54:34 +0900

    feat(lotto): 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        boolean validPrice = false;
         LottoHandler lottoHandler = new LottoHandler();
-        OutputHandler.printMessage("구입금액을 입력해 주세요.");
-        lottoHandler.calculateLottoTicketCount(Console.readLine());
+
+        while (!validPrice) {
+            try {
+                OutputHandler.printMessage("구입금액을 입력해 주세요.");
+                lottoHandler.calculateLottoTicketCount(Console.readLine());
+                validPrice = true;
+            } catch (IllegalArgumentException e) {
+                OutputHandler.printMessage(e.getMessage());
+            }
+        }
     }
 }

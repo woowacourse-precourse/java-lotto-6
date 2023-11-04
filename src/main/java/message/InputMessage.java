81e4cf836d107a238ceaf48307e1b51a938b08fa
mk3058 <kmk3058@icloud.com>
2023-11-04 11:19:59 +0900

    feat(InputMessage): InputMessage enum 추가

---
+++ src/main/java/message/InputMessage.java
@@ -0,0 +1,17 @@
+package message;
+
+public enum InputMessage {
+    PRICE_PROMPT("구입금액을 입력해 주세요."),
+    WINNING_NUMBER_PROMPT("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_PROMPT("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    public void print() {
+        System.out.println(this.message);
+    }
+}

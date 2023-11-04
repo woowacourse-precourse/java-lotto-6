bd9d8dddecddf8e0ff3430b44d965a5361bf2068
whxogus215 <whxogus215@naver.com>
2023-11-04 10:57:14 +0900

    feat: 출력에 필요한 코드들 Enum으로 구현

---
+++ src/main/java/lotto/utils/Code.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+public enum Code {
+    Error("[ERROR]"),
+    WINNING("당첨"),
+    BONUS("보너스");
+
+    private final String code;
+
+    Code(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}

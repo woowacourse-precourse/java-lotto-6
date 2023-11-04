854f7ce9b7bdd127ec76ee904a5e8e7f3ec4c8d1
happyryan <tjdgks120813@gmail.com>
2023-11-04 13:50:32 +0900

    feat(lotto ReturnCalculator): add ReturnCalculator class with a calculator method
    Created the ReturnCalculator class with a calculator method that calculates the total winning amount based on the provided lotto results.

--- src/main/java/lotto/ReturnCalculator.java
+++ src/main/java/lotto/ReturnCalculator.java
@@ -1,2 +1,21 @@
-package lotto;public class ReturnCalculator {
+package lotto;
+
+import java.util.Map;
+
+public class ReturnCalculator {
+    public int totalWinningAmount = 0;
+    private Map<LottoMessage, Integer> lottoResult;
+
+    public ReturnCalculator(Map<LottoMessage, Integer> lottoResult) {
+
+        this.lottoResult = lottoResult;
+    }
+    public void calculrator(){
+        for(Map.Entry<LottoMessage, Integer> entry : lottoResult.entrySet()){
+            if(entry.getValue() > 0){
+                totalWinningAmount += entry.getKey().winningAmount * entry.getValue();
+            }
+            System.out.println(entry.getKey().message + " - " + entry.getValue() + "개");
+        }
+    }
 }

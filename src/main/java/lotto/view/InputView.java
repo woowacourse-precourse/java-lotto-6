81021714af4f51ea21dadd2c3d70c3d1fb5791b1
whxogus215 <whxogus215@naver.com>
2023-11-04 13:52:57 +0900

    refactor(InputView): 객체가 아닌 리스트 반환
    getWinningNumbers()의 반환 타입을 List<Integer>로 변경
    -> Controller는 로또게임에 필요한 데이터만 필요한 것이지 특정 객체에 대해 알 필요가 없기 때문

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,19 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.BonusNumber;
+import lotto.model.WinningNumbers;
 import lotto.utils.Casher;
 import lotto.utils.Code;
 import lotto.utils.Message;
 
 public class InputView {
     private final String INPUT_PRICE = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING = "당첨 번호를 입력해 주세요.";
+    private final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
 
     public int getLottoCount() {
         System.out.println(INPUT_PRICE);
@@ -24,6 +31,51 @@ public class InputView {
             }
         }
         return lottoCount;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        System.out.println(INPUT_WINNING);
+        String numbers = null;
+        WinningNumbers winning = null;
+        while (true) {
+            numbers = inputValue();
+            try {
+                List<Integer> winningNumbers = inputWinningNumbers(numbers);
+                winning = new WinningNumbers(winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(Code.Error.getCode() + " " + e.getMessage());
+            }
+        }
+        return winning.getNumbers();
+    }
+
+    public int getBonusNumber(List<Integer> winningNumbers) {
+        System.out.println(INPUT_BONUS);
+        String number = null;
+        BonusNumber bonusNumber = null;
+        while (true) {
+            number = inputValue();
+            try {
+                validateNumber(number);
+                bonusNumber = new BonusNumber(winningNumbers, Integer.parseInt(number));
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(Code.Error.getCode() + " " + e.getMessage());
+            }
+        }
+        return bonusNumber.getBonusNumber();
+    }
+
+    private List<Integer> inputWinningNumbers(String numbers) {
+        String[] split = numbers.split(",");
+        Arrays.stream(split).forEach(this::validateNumber);
+        if (split.length != 6) {
+            throw new IllegalArgumentException(Message.LACK_NUMBER_COUNT.getMessage());
+        }
+        return Arrays.stream(split)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     private String inputValue() {

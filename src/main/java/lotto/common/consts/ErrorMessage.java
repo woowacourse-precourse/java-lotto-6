f1807b38aff5dad0bf2fde218661c54ad263a55a
jifrozen0110 <answldjs1836@gmail.com>
2023-11-04 21:52:45 +0900

    feat(InputView) : 정수 입력, 정수 배열 입력 메서드 작성

---
+++ src/main/java/lotto/common/consts/ErrorMessage.java
@@ -0,0 +1,5 @@
+package lotto.common.consts;
+
+public class ErrorMessage {
+    public static final String NumberFormatErrorMessage="[Error] 정수값이 아닙니다.";
+}
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.common.consts.ErrorMessage;
+
+public class InputView {
+
+    public static int nextInt(){
+        return parseInt(Console.readLine());
+    }
+
+    public static int[] nextIntArray(){
+        String[] inputs=Console.readLine().split(",");
+        int[] StringToInteger=new int[inputs.length];
+        for(int i=0;i<inputs.length;i++){
+            StringToInteger[i]=parseInt(inputs[i]);
+        }
+        return StringToInteger;
+    }
+
+    private static int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            OutputView.printError(ErrorMessage.NumberFormatErrorMessage);
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+
+}

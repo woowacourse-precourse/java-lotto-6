07b2d8f06d2f978e3925596e48aa3815b9d74233
손지현_회원/결제개발팀 <hdg12jh@interpark.com>
2023-11-04 15:58:22 +0900

    feat 사용자 입력 클래스 생성

---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+public class Input {
+    public String enterPurchaseAmout() {
+        return readLine();
+    }
+
+    public String enterWinningNumbers() {
+        return readLine();
+    }
+
+    public String enterBonusNumbers() {
+        return readLine();
+    }
+}

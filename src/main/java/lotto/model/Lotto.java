72f5c05a2166312ec35d6f3b4e18459aaaa91ce0
kim <hwiyul_g@daum.net>
2023-11-04 14:46:16 +0900

    feat(Lotto) : 로또 번호 오름 차순 정렬 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,13 +1,15 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -19,22 +21,30 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    private void validateDuplicateNumbers(List<Integer> numbers){
+    private void validateDuplicateNumbers(List<Integer> numbers) {
         final int LOTTO_NUMBERS_SIZE = 6;
         long distinctCount = numbers.stream().distinct().count();
-        if(distinctCount != LOTTO_NUMBERS_SIZE){
+        if (distinctCount != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
     }
-    private void validateNumberRange(List<Integer> numbers){
+
+    private void validateNumberRange(List<Integer> numbers) {
         final int MIN_NUMBER = 1;
         final int MAX_NUMBER = 45;
 
         boolean isOutOfRange = numbers.stream()
             .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
 
-        if(isOutOfRange){
+        if (isOutOfRange) {
             throw new IllegalArgumentException();
         }
     }
+
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        return numbers.stream()
+            .sorted()
+            .collect(Collectors.toList());
+    }
+
 }

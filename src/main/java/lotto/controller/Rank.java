69b2c9eea3661468d9d89cacb3540faedb0cbf6d
Jiwon Na <jiwonna52@gmail.com>
2023-11-04 19:35:30 +0900

    Refactor : Enum에 rank 순위 추가

--- src/main/java/lotto/controller/Rank.java
+++ src/main/java/lotto/controller/Rank.java
@@ -3,17 +3,31 @@ package lotto.controller;
 import java.util.Arrays;
 
 public enum Rank {
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 3000000),
-    First(6, 2000000000);
+    FIFTH(5, 3, 5000),
+    FOURTH(4, 4, 50000),
+    THIRD(3, 5, 1500000),
+    SECOND(2, 5, 3000000),
+    First(1, 6, 2000000000);
 
     private final int correctNum;
     private final int prize;
-    Rank(int correctNum, int prize) {
+    private final int ranking;
+    Rank(int ranking, int correctNum, int prize) {
+        this.ranking = ranking;
         this.correctNum = correctNum;
         this.prize = prize;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public int getCorrectNum() {
+        return correctNum;
+    }
+
+    public int getPrize() {
+        return prize;
     }
 
     public static Rank getRank(int num) {

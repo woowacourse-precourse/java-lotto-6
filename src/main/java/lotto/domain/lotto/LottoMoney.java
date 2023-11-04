897f46a9c8063627ac3c22ea9577b4dff9d44c07
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 06:20:24 +0900

    feat(LottoMoney) : LottoMoney 생성 기능 구현

---
+++ src/main/java/lotto/domain/lotto/LottoMoney.java
@@ -0,0 +1,17 @@
+package lotto.domain.lotto;
+
+public class LottoMoney {
+
+    private int money;
+
+    public LottoMoney(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if(money == 0 || money % 1000 != 0) {
+            throw new IllegalArgumentException("error");
+        }
+    }
+}
---
+++ src/test/java/lotto/domain/lotto/LottoMoneyTest.java
@@ -0,0 +1,27 @@
+package lotto.domain.lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoMoneyTest {
+
+
+    @ParameterizedTest
+    @DisplayName("money가 0이거나 1000원 단위가 아니라면 예외 발생")
+    @ValueSource(ints = {0,50,1500})
+    void 예외(int money) {
+        Assertions.assertThatThrownBy(() -> new LottoMoney(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("money가 0이 아니고 1000원 단위라면 생성 성공")
+    @ValueSource(ints = {1000,10000,200000})
+    void 성공(int money) {
+        assertDoesNotThrow(() -> new LottoMoney(money));
+    }
+}

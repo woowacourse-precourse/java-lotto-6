ab9eb1fc9547e24917efd669e1aaa201920f329e
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-04 16:46:48 +0900

    Feat: 당첨 번호와 로또 번호를 비교하는 기능 구현, 기능의 테스트 코드 구현
    당첨 번호와 로또 번호를 비교해서 맞춘 개수를 세는 match 메서드 구현
    match 메서드의 테스트 코드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,34 +1,76 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private static final int NUMBERS_SIZE = 6;
-    private final List<Integer> numbers;
+	private static final int NUMBERS_SIZE = 6;
+	private static final String HIT_MESSAGE = "개 일치";
+	private static final String BONUS_HIT_MESSAGE = "개 일치, 보너스 볼 일치";
 
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+	public Lotto(List<Integer> numbers) {
 
-        validateSize(numbers);
-        validateDuplication(numbers);
+		validateSize(numbers);
+		validateDuplication(numbers);
 
-        this.numbers = numbers;
-    }
+		Collections.sort(numbers);
 
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != NUMBERS_SIZE) {
-            throw new IllegalArgumentException();
-        }
-    }
+		this.numbers = numbers;
+	}
 
-    private void validateDuplication(List<Integer> numbers) {
+	private void validateSize(List<Integer> numbers) {
+		if (numbers.size() != NUMBERS_SIZE) {
+			throw new IllegalArgumentException();
+		}
+	}
 
-        Set<Integer> testNumbers = Set.copyOf(numbers);
+	private void validateDuplication(List<Integer> numbers) {
 
-        if(testNumbers.size() != NUMBERS_SIZE) {
-            throw new IllegalArgumentException();
-        }
-    }
+		Set<Integer> testNumbers = Set.copyOf(numbers);
+
+		if (testNumbers.size() != NUMBERS_SIZE) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public String match(List<Integer> winningNumber, int bonusNumber) {
+
+		int hitCount = calculateHit(winningNumber);
+
+		if (isBonusHit(bonusNumber, hitCount)) {
+			return hitCount + BONUS_HIT_MESSAGE;
+		}
+
+		return hitCount + HIT_MESSAGE;
+	}
+
+	private int calculateHit(List<Integer> winningNumber) {
+
+		int hitCount = 0;
+
+		for (int comparedNumber : winningNumber) {
+
+			if (isHit(comparedNumber)) {
+				hitCount++;
+			}
+		}
+
+		return hitCount;
+	}
+
+	private boolean isHit(int comparedNumber) {
+		return numbers.contains(comparedNumber);
+	}
+
+	private boolean isBonusHit(int bonusNumber, int hitCount) {
+		return hitCount == 5 && numbers.contains(bonusNumber);
+	}
+
+	public List<Integer> getNumbers() {
+		return this.numbers;
+	}
 
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,27 +1,65 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static java.util.Arrays.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+	@Test
+	void createLottoByOverSize() {
+		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+	@Test
+	void createLottoByDuplicatedNumber() {
+		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
 
-    // 아래에 추가 테스트 작성 가능
+	@DisplayName("당첨 번호 비교 기능 확인")
+	@Test
+	void matchTest() {
+		//given
+		List<Integer> numbers = asList(1,2,3,4,5,6);
+		Lotto lotto = new Lotto(numbers);
+
+		List<Integer> winningNumber = asList(1,2,3,7,8,9);
+		int bonusNumber = 45;
+
+		//when
+		String actualResult = lotto.match(winningNumber, bonusNumber);
+
+		//then
+		String expectedResult = Prize.FIFTH.hitResult;
+		assertEquals(expectedResult, actualResult);
+
+	}
+
+	@DisplayName("보너스 번호 일치 확인")
+	@Test
+	void bonusMatchTest() {
+		//given
+		List<Integer> numbers = asList(1,2,3,4,5,6);
+		Lotto testLotto = new Lotto(numbers);
+
+		List<Integer> winningNumber = asList(1,2,3,4,5,45);
+		int bonusNumber = 6;
+
+		//when
+		String actualResult = testLotto.match(winningNumber, bonusNumber);
+
+		//then
+		String expectedResult = Prize.SECOND.hitResult;
+		assertEquals(expectedResult, actualResult);
+
+	}
+
 }

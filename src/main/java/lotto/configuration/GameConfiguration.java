b3047c15887531e8c3d85620217373b45bc8faab
kmw2378 <rlarla677@gmail.com>
2023-11-04 15:43:30 +0900

    feat(lotto): GameConfiguration 로또 번호 최대/최소, 개수에 대한 상수 필드 추가 로또 번호 최대/최소, 개수에 대한 상수 필드를 추가하였습니다.

--- src/main/java/lotto/configuration/GameConfiguration.java
+++ src/main/java/lotto/configuration/GameConfiguration.java
@@ -1,6 +1,8 @@
 package lotto.configuration;
 
 public final class GameConfiguration {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_SIZE = 6;
     private GameConfiguration() {}
-
 }

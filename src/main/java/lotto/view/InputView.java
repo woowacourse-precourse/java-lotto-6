523adfd30902b7787c383aaf90d3786886ae8873
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 18:48:57 +0900

    feat(InputView) : 구입금액입력문구 출력 및 입력받기 기능 추가
    docs : 해당기능 체크완료

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String MESSAGE_REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public String requestPurchaseAmount() {
+        System.out.println(MESSAGE_REQUEST_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+}

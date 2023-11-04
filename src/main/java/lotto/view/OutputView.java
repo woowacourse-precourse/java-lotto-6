10a5e2c3ebeafc168bbbed54d4fd805d47822a41
seminchoi <choicco89@gmail.com>
2023-11-04 17:51:40 +0900

    feat(view.OutputView) : OutputView 클래스 생성, 메소드 정의 -구현부 미완성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import lotto.constant.ErrorMessage;
+import lotto.constant.InformationMessage;
+import lotto.dto.LottoReceiptDto;
+import lotto.dto.LottoResultDto;
+
+public class OutputView {
+    public void printErrorMessage(ErrorMessage errorMessage) {
+    }
+
+    public void printInformationMessage(InformationMessage informationMessage) {
+    }
+
+    public void printLottoReceipt(LottoReceiptDto lottoReceiptDto) {
+    }
+
+    public void printLottoResult(LottoResultDto lottoResultDto) {
+    }
+
+    public void printMessage(String message) {
+    }
+}

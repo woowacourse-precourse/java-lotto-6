f7208cc60e3b045c75516c6195d1f50ea905fd23
Cycrypto <jh01love@naver.com>
2023-11-04 12:24:11 +0900

    feat:당첨번호를 생성하는 로직 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,10 +3,7 @@ package lotto.controller;
 import static lotto.view.View.requestMoney;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
 import lotto.model.Customer;
-import lotto.model.Lotto;
 import lotto.model.LottoNumber;
 import lotto.model.Money;
 public class LottoController {
@@ -17,19 +14,9 @@ public class LottoController {
 
         System.out.println(customer.getLottoCount()+"개를 구매했습니다.");
         System.out.println("당첨번호를 입력해 주세요");
-        String winningNumber = Console.readLine();
-        List<Integer> winningNumbers = Arrays.stream(winningNumber.split(","))
-                .map(Integer::parseInt)
-                .toList();
 
-        if (winningNumbers.size() != winningNumbers
-                .stream()
-                .distinct()
-                .toList()
-                .size()
-        ){
-            throw new IllegalArgumentException();
-        }
+        String winningNumber = Console.readLine();
+        lottoNumber.createWinningNumber(winningNumber);
 
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 public class LottoNumber {
@@ -9,6 +10,11 @@ public class LottoNumber {
     }
     private void generateLottoNumbers(){
         this.lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45, 6);
+    }
+    public List<Integer> createWinningNumber(String input){
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
     }
     public List<Integer> getLottoNumbers(){
         return this.lottoNumbers;

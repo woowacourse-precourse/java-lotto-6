fe31b2b1ffef3cbec4acf3754aee4d6b36a80467
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-05 01:29:46 +0900

    feat: StringConstant에 상수 추가

--- src/main/java/lotto/domain/constant/StringConstant.java
+++ src/main/java/lotto/domain/constant/StringConstant.java
@@ -3,4 +3,12 @@ package lotto.domain.constant;
 public class StringConstant {
     // INPUT
     public static final String SPLIT_DELIMITER = ",";
+
+    // TEST CASE
+    public static final String TEST_EXPENSE_TYPE = "가나다라";
+    public static final String TEST_EXPENSE_DIVIDE = "12345";
+    public static final String TEST_WINNUMBERS_TYPE = "3,5,다,1,";
+    public static final String TEST_WINNUMBERS_SORT = "6,5,4,3,2,1";
+    public static final String TEST_BONUSNUMBER_TYPE = "가나다라";
+    public static final String TEST_BONUSNUMBER_RANGE = "46";
 }

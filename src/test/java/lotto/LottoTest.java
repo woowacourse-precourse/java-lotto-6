dccacd66ec5fa8d163486eb8f342f4b45a9c5ec7
jeli01 <jeli01@naver.com>
2023-11-05 00:50:08 +0900

    test: add checking error messaging to existing test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -14,7 +14,8 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또는 6개의 숫자여야 합니다.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -22,7 +23,8 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("번호가 중복 되었습니다.");
     }
 
     // 아래에 추가 테스트 작성 가능
@@ -31,6 +33,7 @@ class LottoTest {
     @Test
     void createLottoByRangeOutNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(0,1,2,3,4,5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("번호는 1~45의 숫자여야 합니다.");
     }
 }

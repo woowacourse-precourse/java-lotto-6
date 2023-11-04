92a65615d057e3f3dbebbf23495a6c0f2bbf9f36
yshong1998 <yshong1998@gmail.com>
2023-11-05 04:44:30 +0900

    Feat(ResponseMessage): 사용자에게 응답하는 메세지를 관리하는 기능 구현

---
+++ src/main/java/lotto/message/ResponseMessage.java
@@ -0,0 +1,10 @@
+package lotto.message;
+
+public class ResponseMessage {
+    public static final String BOUGHT_MESSAGE = "%s개를 구매했습니다.";
+    public static final String STATISTICS_MESSAGE = "당첨 통계";
+    public static final String FORMATTED_RESULT_MESSAGE = "%s개 일치%s (%s원) - ";
+    public static final String RATE_OF_BENEFIT_MESSAGE = "\n총 수익률은 %.1f%%입니다.\n";
+
+
+}

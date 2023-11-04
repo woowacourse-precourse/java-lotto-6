b7c4c611926272f1294569aa6730903526df009c
peppone-choi <peppone.choi@gmail.com>
2023-11-04 15:22:41 +0900

    feat: 당첨 내역 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,8 +5,12 @@ import static lotto.enumerate.Message.LOTTO_BUYED;
 import java.util.List;
 
 public class OutputView {
-    public void lottoListPrint(int lottoListNumber, List<String> lottoListString) {
+    public void printLottoList(int lottoListNumber, List<String> lottoListString) {
         System.out.printf((LOTTO_BUYED.getMessage()), lottoListNumber);
         lottoListString.forEach(System.out::println);
     }
+    public void printWinningList(String winningList) {
+        System.out.printf(winningList);
+    }
+
 }

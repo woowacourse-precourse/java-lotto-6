a3071b971f40d97a9c9743bb9d876defd02f3584
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-04 19:05:47 +0900

    style: 프로젝트 뼈대 구현 및 클래스 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,11 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+    // 필드 추가 불가능
+    // numbers의 접근제어자인 private 변경 불가능
+    // 제공된 Lotto 클래스를 활용해 구현해야함
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {

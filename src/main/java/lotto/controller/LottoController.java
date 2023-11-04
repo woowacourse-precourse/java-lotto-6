d38eecda688b8c9930cab47d3e6e014b1135cb83
jaeyoungjang2 <lion85488@gmail.com>
2023-11-04 19:57:47 +0900

    feat: 보너스 번호, 당첨 번호 검증

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,9 @@
 package lotto.controller;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.service.LottoService;
 
@@ -31,6 +34,36 @@ public class LottoController {
             Integer.valueOf(purchasePrice);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("로또 금액은 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private void validateBonusNumber(String bonusNumber) {
+        try {
+            Integer.valueOf(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("보너스 숫자는 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateWinningNumber(String winningNumber) {
+        String[] split = winningNumber.split(",");
+        validateLength(split);
+        validateType(split);
+    }
+
+    private static void validateType(String[] split) {
+        try {
+            for (String s : split) {
+                Integer.valueOf(s);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("로또 금액은 숫자만 입력 가능합니다2.");
+        }
+    }
+
+    private static void validateLength(String[] split) {
+        if (split.length != 6) {
+            throw new IllegalArgumentException("당첩 번호는 6개의 숫자로 입력되어야 합니다.");
         }
     }
 }

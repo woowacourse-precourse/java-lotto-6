47d5677793a7b588f29506b8b67d04fc815ac075
song-jae-wan <sjo9810@gmail.com>
2023-11-04 21:55:22 +0900

    feat: 로또 생성 기능 구현
    인자로 받는 개수만큼 로또 생성 후
    리스트로 리턴

--- src/main/java/util/LottoUtil.java
+++ src/main/java/util/LottoUtil.java
@@ -1,9 +1,21 @@
 package util;
 
 public class LottoUtil {
+
     // 금액 계산기
     public static int countLotto(int pay) {
         return pay / 1000; // TODO: 상수 처리 필요
     }
 
+    // 로또 생성기
+    public static List<Lotto> makeLottoList(int count) {
+        List<Lotto> lottoList = new ArrayList<>();
+        while(count-- > 0) {
+            List<Integer> numbers = new ArrayList<>(
+                    Randoms.pickUniqueNumbersInRange(1, 45, 6)); // TODO: 상수 처리 필요
+            Collections.sort(numbers);
+            lottoList.add(new Lotto(numbers));
+        }
+        return lottoList;
+    }
 }

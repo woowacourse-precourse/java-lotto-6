4f5267482920120a30f090e453a50e6a4dceec84
heejae <hjhjhjhj3770@naver.com>
2023-11-04 13:58:51 +0900

    Feat(View): 프로그램의 모든 출력문을 담당하는 클래스 생성
    구입금액의 입력을 안내하는 requestPurchaseAmount() 생성

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -3,6 +3,7 @@ package lotto;
 public class LottoController {
 
     public void run(){
-
+        View.requestPurchaseAmount();
+        PurchaseAmount purchaseAmount = PurchaseAmount.from(InputService.purchaseAmount());
     }
 }
--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,4 +1,11 @@
 package lotto;
 
+import static lotto.settings.Announcement.PURCHASE_AMOUNT_REQUEST;
+
 public class View {
+
+    public static void requestPurchaseAmount(){
+        System.out.println(PURCHASE_AMOUNT_REQUEST.getPrint());
+    }
+
 }

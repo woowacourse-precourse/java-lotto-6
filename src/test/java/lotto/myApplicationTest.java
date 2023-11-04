787fc7d06d66b40a8b5122ac79d6edfe868ac5b1
yooooonshine <hl3470@naver.com>
2023-11-04 12:25:44 +0900

    Test: 발행한 로또 수량 및 번호를 출력하는 기능 테스트 구현
    발행한 로또 수량 및 번호를 출력하는 기능 테스트를 구현하였다.

--- src/test/java/lotto/myApplicationTest.java
+++ src/test/java/lotto/myApplicationTest.java
@@ -1,22 +1,27 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.controller.LottoGame;
+import lotto.domain.Lotto;
 import lotto.domain.User;
+import lotto.dto.LottoTicketsDTO;
 import lotto.utility.GameUtility;
 import lotto.validator.Validator;
+import lotto.view.OutputView;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class myApplicationTest {
@@ -85,7 +90,7 @@ public class myApplicationTest {
                     () -> {
                         User user;
                         user = GameUtility.buyTickets(1000);
-                        assertEquals(user.getLottoTickets().get(0).getNumbers(),LottoNumbers);
+                        assertEquals(user.getLottoTickets().get(0).getNumbers(), LottoNumbers);
                     },
                     Arrays.asList(43, 41, 42, 23, 21, 8)
             );
@@ -94,5 +99,46 @@ public class myApplicationTest {
         @Override
         protected void runMain() {
         }
+    }
+
+    @Nested
+    @DisplayName("발행한 로또 수량 및 번호를 출력하는 기능")
+    public class 발행_로또_수량_및_번호_테스트 {
+
+        private static ByteArrayOutputStream outputMessage;
+
+        @BeforeEach
+        void setUpStreams() {
+            outputMessage = new ByteArrayOutputStream(); // OutputStream 생성
+            System.setOut(new PrintStream(outputMessage)); // 생성한 OutputStream 으로 설정
+        }
+
+        @AfterEach
+        void restoresStreams() {
+            System.setOut(System.out); // 원상복귀
+        }
+
+        @Test
+        @DisplayName("발행한 로또 수량 및 로또 번호를 형식에 맞게 출력하는 지 테스트한다.")
+        void 발행_로또_수량_및_번호_테스트() {
+            // given
+            Lotto lotto1 = new Lotto(Arrays.asList(8, 21, 23, 41, 42, 43));
+            Lotto lotto2 = new Lotto(Arrays.asList(3, 5, 11, 16, 32, 38));
+            List<Lotto> lottoTickets = new ArrayList<>();
+            lottoTickets.add(lotto1);
+            lottoTickets.add(lotto2);
+            LottoTicketsDTO lottoTicketsDTO = new LottoTicketsDTO(lottoTickets.size(), lottoTickets);
+            // when
+            // then
+
+            OutputView.printLottoTickets(lottoTicketsDTO);
+            assertThat(outputMessage.toString()).contains(
+                    "2개를 구매했습니다.",
+                    "[8, 21, 23, 41, 42, 43]",
+                    "[3, 5, 11, 16, 32, 38]"
+            );
+        }
+
+
     }
 }

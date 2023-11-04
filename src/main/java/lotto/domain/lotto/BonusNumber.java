0fd10c82c03f844fa9cae4c59c8d6602da8236c6
wwan13 <wwan13@naver.com>
2023-11-04 15:52:24 +0900

    feat : `BonusNumber` 기능 구현 - 정수와 로또 번호를 통해 생성할 수 있다. - 보너스 번호가 1~45 사이가 아니라면 예외가 발생한다. - 보너스 번호가 입력받은 로또 번호헤 포함되어 있다면 예외가 발생한다. - 입력 받은 로또 뻔호에 보너스 번호가 포함되어 있는지 판단한다.

---
+++ src/main/java/lotto/domain/lotto/BonusNumber.java
@@ -0,0 +1,42 @@
+package lotto.domain.lotto;
+
+import static lotto.domain.constant.LottoConstant.LOTTO_MAXIMUM_NUMBER;
+import static lotto.domain.constant.LottoConstant.LOTTO_MINIMUM_NUMBER;
+
+public class BonusNumber {
+
+    private final int number;
+
+    private BonusNumber(int number) {
+        this.number = number;
+    }
+
+    public static BonusNumber of(int number, Lotto lotto) {
+        validateRange(number);
+        validateDuplicate(number, lotto);
+        return new BonusNumber(number);
+    }
+
+    private static void validateRange(int number) {
+        if (number < LOTTO_MINIMUM_NUMBER || number > LOTTO_MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException(
+                String.format("보너스 번호는 %d 이상 %d 이하여야 합니다.",
+                    LOTTO_MINIMUM_NUMBER, LOTTO_MAXIMUM_NUMBER)
+            );
+        }
+    }
+
+    private static void validateDuplicate(int number, Lotto lotto) {
+        if (lotto.contains(number)) {
+            throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean containsInLotto(Lotto lotto) {
+        return lotto.contains(number);
+    }
+}

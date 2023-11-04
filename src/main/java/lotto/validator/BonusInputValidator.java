497fb1488bade6b631c3a89460048c18ec488c4c
1000zoo <cjswldn99@gmail.com>
2023-11-05 03:37:59 +0900

    feat(BonusInputValidator): 보너스 번호 입력 검증 기능 구현

--- src/main/java/lotto/validator/BonusInputValidator.java
+++ src/main/java/lotto/validator/BonusInputValidator.java
@@ -1,8 +1,36 @@
 package lotto.validator;
 
 public class BonusInputValidator implements InputValidator {
+
+    private final static String INTEGER_PATTERN = "^\\d+$";
+    private final static int MIN_RANGE = 1;
+    private final static int MAX_RANGE = 45;
+
+
     @Override
     public void validate(String input) {
+        throwIfNotInteger(input);
+        throwIfOutOfRange(input);
+    }
 
+    private void throwIfNotInteger(String input) {
+        if (!input.matches(INTEGER_PATTERN)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void throwIfOutOfRange(String input) {
+        int inputInt = parseInt(input);
+        if (inputInt < MIN_RANGE || inputInt > MAX_RANGE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException();
+        }
     }
 }

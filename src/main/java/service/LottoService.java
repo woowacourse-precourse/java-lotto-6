82bf3d0ca3ddcb92badc3df981726a825a7ba784
song-jae-wan <sjo9810@gmail.com>
2023-11-04 21:48:44 +0900

    feat: LottoService 멤버변수, 생성자 선언
    - WinningLotto 객체, UserLottoVO 객체를 멤버변수로 선언, 생성자에서 객체 주입받는 식으로 설계

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,4 +1,15 @@
 package service;
 
+import VO.UserLottoVO;
+import domain.WinningLotto;
+
 public class LottoService {
+
+    private final WinningLotto winningLotto;
+    private final UserLottoVO userLottoVO;
+
+    public LottoService(WinningLotto winningLotto, UserLottoVO userLottoVO) {
+        this.winningLotto = winningLotto;
+        this.userLottoVO = userLottoVO;
+    }
 }

8a22051af719b6d176230b580456963086dbc5a3
GS_song <20003204@sju.ac.kr>
2023-11-05 02:10:21 +0900

    feat: Lotto 객체에 대한 예외 생성
    1. isCorrectSize : 숫자의 갯수가 LOTTO_SIZE와 동일한지 확인
    2. isDuplicate : 숫자 중에 중복된 숫자가 있는지 확인
    3. isInRangeNumbers : 숫자들이 모두 LOTTO_NUMBER 범위에 있는지 확인

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_MIN_NUMBER = 1;
@@ -11,16 +13,39 @@ public class Lotto {
     private LottoResult result;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
+        validate();
         result = null;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate() {
+        isCorrectSize();
+        isDuplicate();
+        isInRangeNumbers();
+    }
+
+    private void isCorrectSize() {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] " + LOTTO_SIZE + "개의 숫자를 입력하세요.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void isDuplicate() {
+        Set<Integer> noDupBucket = new HashSet<>(numbers);
+        if (noDupBucket.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복하지 않는 숫자만 입력해주세요.");
+        }
+    }
+
+    private void isInRangeNumbers() {
+        numbers.forEach(number -> {
+            if (number < LOTTO_MIN_NUMBER || LOTTO_MAX_NUMBER < number) {
+                throw new IllegalArgumentException("[ERROR] " + LOTTO_MIN_NUMBER + "이상" + LOTTO_MAX_NUMBER + "이하인 값만 입력하세요");
+            }
+        });
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

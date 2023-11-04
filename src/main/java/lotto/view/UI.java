003c8f8996694c9cb9f51de1496a4c5752615f18
doka <posmane5@gmail.com>
2023-11-04 15:07:26 +0900

    feat(UI) : "view)로또 구입 금액 입력" 기능의 inputLottoAmount 메소드, displayMessage 메소드 추가
    - inputLottoAmount() : 사용자로부터 메시지를 입력받고 반환하는 메소드
    
    - displayMessage() : 메시지 입력을 유도하는 상수 문구를 출력하는 메소드

---
+++ src/main/java/lotto/view/UI.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.constant.Instructions;
+
+public class UI {
+
+    public static String inputLottoAmount() {
+        displayMessage(Instructions.MESSAGE_OF_ENTERING_LOTTO_AMOUNT);
+        String lottoAmountInputValue  = Console.readLine();
+        //입력 검증 추가 예정
+        return lottoAmountInputValue;
+    }
+
+    public static void displayMessage(final Instructions instructions) {
+        System.out.println(instructions.getPrompts());
+    }
+
+}

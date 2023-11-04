84c61f08f59f650e04e8936ba75cb125e73126b6
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-04 15:45:45 +0900

    test(BonusTest): 보너스 숫자가 1~45인지 검증하는 테스트 추가

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -1,7 +1,8 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import lotto.Model.Bonus;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,16 @@ public class BonusTest {
     @DisplayName("보너스 숫자가 숫자인지 테스트")
     @Test
     void isNumber() {
-        Assertions.assertThatThrownBy(() -> new Bonus("a"))
+        assertThatThrownBy(() -> new Bonus("a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 숫자로만 입력해야 합니다.");
+    }
+
+    @DisplayName("보너스 숫자가 1~45인지 테스트")
+    @Test
+    void isCorrectRange() {
+        assertThatThrownBy(() -> new Bonus("46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1~45사이의 숫자여야 합니다.");
     }
 }

c08e4c39b47e073530264dda85003a1d79079e64
Hoo-sung <gntjd135@gmail.com>
2023-11-04 12:51:42 +0900

    feat(SettlementSystem): 로또 등수를 계산해 출력하는 메서드 생성

--- src/main/java/lotto/controller/SettlementSystem.java
+++ src/main/java/lotto/controller/SettlementSystem.java
@@ -1,6 +1,11 @@
 package lotto.controller;
 
 import lotto.domain.Award;
+import lotto.domain.BonusNumber;
+import lotto.domain.UserLotto;
+import lotto.domain.WinningLotto;
+import lotto.io.OutputHandler;
+import lotto.manager.AwardManager;
 
 
 import java.util.List;
@@ -9,4 +14,11 @@ public class SettlementSystem {
 
     private List<Award> awards;
 
+    public void makeResult(WinningLotto winningLotto, UserLotto userLotto, BonusNumber bonusNumber) {
+        awards = AwardManager.makeAwards(winningLotto, userLotto, bonusNumber);
+    }
+
+    public void renderResult() {
+        OutputHandler.printReward(awards);
+    }
 }

28eca2cb93ce4dd65fa2590cb13197e6f691b315
mainfn <_@mainfn.dev>
2023-11-04 18:05:20 +0900

    feat: `OutputView` Scaffold 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import lotto.controller.dto.LottosBuyingResult;
+import lotto.domain.lotto.dto.LottosDrawingResult;
+
+public final class OutputView {
+    public void printLottosBuyingResult(final LottosBuyingResult result) {
+        // TODO:
+    }
+
+    public void printLottosDrawingResult(final LottosDrawingResult result) {
+        // TODO:
+    }
+}

4e62fc2776030d8bc84f89668d85c56a08234544
kimwanyoung <dhks2869@gmail.com>
2023-11-04 16:33:19 +0900

    feat : 로또 번호 오름차순 정렬 구현

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoNumberGenerator {
@@ -9,6 +10,8 @@ public class LottoNumberGenerator {
     }
 
     public static List<Integer> generateNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumbers.sort(Comparator.naturalOrder());
+        return lottoNumbers;
     }
 }

2b382cfb0ba53b75cc14bd9c44feb101e0e9811f
hunko <rhwlgns4386@naver.com>
2023-11-05 06:02:37 +0900

    test(MoneyTest): 0원 입력테스트 작성

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,6 +14,12 @@ public class MoneyTest {
     @DisplayName("1000원단위로 입력하지 않으면 예외가 발생한다.")
     public void createByNotThousandWonUnits(Integer money) {
         assertThatThrownBy(() -> Money.of(money)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("0원입력시 예외가 발생한다.")
+    public void createByZeroWon() {
+        assertThatThrownBy(() -> Money.of(0)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }

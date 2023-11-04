cb3026b3ef36b457ac7539ef5fc5664aca029d2f
You Taein <ytn1574@gmail.com>
2023-11-05 00:30:22 +0900

    feat: 사용자 입력 및 출력 기능 구현
    사용자의 금액 입력, 금액에 따른 구매 갯수 출력, 구매한 로또 번호 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Validation validation = new Validation();
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String moneyInput = Console.readLine();
+
+        int purchaseAmount = validation.parsePurchaseAmount(moneyInput);
+        validation.validateUnderZero(purchaseAmount);
+        validation.validateDivideThousand(purchaseAmount);
+
+        int purchaseCount = purchaseAmount / 1000;
+        System.out.println(purchaseCount + "개를 구매했습니다.");
+
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < purchaseCount; i++) {
+            lottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+
+        for (int i = 0; i < lottoList.size(); i++) {
+            System.out.println(lottoList.get(i).toString());
+        }
     }
+
 }

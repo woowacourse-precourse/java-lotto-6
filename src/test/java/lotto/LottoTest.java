93fcd7d53e892205d5682c408455c2b65d37ec50
SungWoo <sungwoo166@gmail.com>
2023-11-04 22:06:41 +0900

    test(Lotto): Implement test to check lotto numbers are sorted

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,12 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -28,5 +28,12 @@ class LottoTest {
     void createLottoByLessSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 오름차순으로 정렬되어 생성된다.")
+    @Test
+    void createLottoSorted() {
+        Lotto lotto = new Lotto(List.of(12, 11, 10, 9, 8, 7));
+        assertThat(lotto.getNumbers()).containsExactly(7, 8, 9, 10, 11, 12);
     }
 }

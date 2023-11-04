70f6da809e0655582f790b4bf3501ccc863559ae
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 12:41:48 +0900

    [feat] LottoMachine 클래스 필드 정의

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import lotto.util.NumberGenerator;
+
+import java.util.List;
+
 public class LottoMachine {
+    private final NumberGenerator numberGenerator;
+    private final Cash cash;
+
+    public LottoMachine(NumberGenerator numberGenerator, Cash cash) {
+        this.numberGenerator = numberGenerator;
+        this.cash = cash;
+    }
+
 }

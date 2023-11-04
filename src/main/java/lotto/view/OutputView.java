4857d5f1f25614cdb9aca0fbc7d4013915e0d827
leeedohyun <ldh19738@gmail.com>
2023-11-04 21:27:09 +0900

    feat: 당첨 내역 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import lotto.model.LottoTicket;
+import lotto.model.Rank;
+import lotto.model.ResultDetails;
 
 public final class OutputView {
 
@@ -8,6 +10,8 @@ public final class OutputView {
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
+    private static final String HORIZONTAL_LINE = "---";
 
     private OutputView() {
     }
@@ -32,6 +36,20 @@ public final class OutputView {
 
     public static void printBonusNumberMessage() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public static void printWinningStatisticsHeader() {
+        printEmptyLine();
+        System.out.println(WINNING_STATISTICS_HEADER);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void printWinningStatistics(ResultDetails resultDetails) {
+        System.out.printf("3개 일치 (5,000원) - %d개\n", resultDetails.getWinnerCountByRank(Rank.FIFTH));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", resultDetails.getWinnerCountByRank(Rank.FOURTH));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", resultDetails.getWinnerCountByRank(Rank.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", resultDetails.getWinnerCountByRank(Rank.SECOND));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", resultDetails.getWinnerCountByRank(Rank.FIRST));
     }
 
     public static void printProfitRate(double profitRate) {

dd62bd2ca62c4c3099ca880d4fa813e63c944428
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-05 03:57:50 +0900

    feat(Lotto): 로또 숫자 중복 예외 로또가 같은 숫자가 있을 때 예외 처리를 하는 메소드를 구현했습니다. README 예외사항을 추가하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -7,6 +7,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +17,12 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicateNumbers(List<Integer> numbers) {
+        long distinctCount = numbers.stream().distinct().count();
+
+        if (distinctCount != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

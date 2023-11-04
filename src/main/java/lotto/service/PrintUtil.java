19878bdfcb6e563b1ec37bb0b91c31f1536d9ccd
Chenowoo <cjsgusdn14@naver.com>
2023-11-04 17:36:07 +0900

    feat(PrintUtil): 보너스 숫자 입력 문구를 출력하는 기능 구현

--- src/main/java/lotto/service/PrintUtil.java
+++ src/main/java/lotto/service/PrintUtil.java
@@ -9,4 +9,8 @@ public class PrintUtil {
     public void printWinnerNumbersInput() {
         System.out.println("당첨 번호를 입력해주세요.");
     }
+
+    public void printBonusNumberInput() {
+        System.out.println("보너스 번호를 입력해주세요.");
+    }
 }

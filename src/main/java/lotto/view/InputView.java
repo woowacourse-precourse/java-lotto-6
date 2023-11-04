7ba17591456f3deb5e4c6d19dbf8eddd4aa91427
usnim <sms897@naver.com>
2023-11-04 17:24:49 +0900

    [feat] 로또 구입 금액 입력 기능 : 입력된 값 검증 기능 -> 숫자 형식 검증 기능 및 예외 발생

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.InvalidNumberFormatException;
+import lotto.validation.ValidateNumberFormat;
 
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
@@ -10,7 +12,20 @@ public class InputView {
     }
 
     public int inputMoney() {
-        String inputMoney = Console.readLine();
+        String inputMoney;
+        while (true) {
+            inputMoney = Console.readLine();
+            try {
+                validateInputMoney(inputMoney);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         return Integer.parseInt(inputMoney);
+    }
+
+    private void validateInputMoney(String inputMoney) {
+        ValidateNumberFormat.validate(inputMoney);
     }
 }

326d1d2452ee41a61c44e0dcef3e6e51ef60d89f
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 04:34:11 +0900

    feat : 로또번호를 출력하기 위해 사용할 로또의 DTO 구현

---
+++ src/main/java/lotto/dto/LottoDto.java
@@ -0,0 +1,24 @@
+package lotto.dto;
+
+import java.util.List;
+import lotto.model.Lotto;
+
+public class LottoDto {
+
+    private final List<Integer> numbers;
+
+    private LottoDto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static List<LottoDto> toDto(List<Lotto> playerLotteries) {
+        return playerLotteries.stream()
+                .map(LottoDto::from)
+                .toList();
+    }
+
+    private static LottoDto from(Lotto playerLotto) {
+        List<Integer> playerNumbers = playerLotto.getNumbers();
+        return new LottoDto(playerNumbers);
+    }
+}

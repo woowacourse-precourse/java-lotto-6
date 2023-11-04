f45f5634f18e281636c0a567fa058214d218d161
02ggang9 <02ggang9@gmail.com>
2023-11-05 03:47:18 +0900

    fix(ErrorMessage): 미사용 에러 메시지 삭제

--- src/main/java/lotto/global/ErrorMessage.java
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -4,7 +4,6 @@ public enum ErrorMessage {
 
     INPUT_NUMBER_IS_INVALID("[ERROR] 유효한 숫자를 입력해주세요."),
     INPUT_NUMBER_IS_NOT_DIGIT("[ERROR] 숫자를 입력해 주세요."),
-    INPUT_NUMBER_LENGTH_NOT_SIX("[ERROR] 숫자 6개를 입력해 주세요.")
     ;
 
     private final String message;

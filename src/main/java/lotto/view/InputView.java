d8ca723bae633c62fdcb1784036dd6248c5fa8c8
Sanghwi Lee <sanghwi47@naver.com>
2023-11-04 15:16:48 +0900

    feat(InputView) 입력을 받는 기능을 구현, 입력메세지는 상수처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,29 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+//    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+//    private static final String INPUT_RESULT_NUMBER = "당첨 번호를 입력해 주세요.";
+//    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    private static final String INPUT_MONEY = "input money";
+    private static final String INPUT_RESULT_NUMBER = "input result number";
+    private static final String INPUT_BONUS_NUMBER = "input bonus number";
+    public static String inputMoney() {
+        System.out.println(INPUT_MONEY);
+        return Console.readLine();
+    }
+
+    public static String inputResultNumber() {
+        System.out.println(INPUT_RESULT_NUMBER);
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        return Console.readLine();
+    }
+
 }

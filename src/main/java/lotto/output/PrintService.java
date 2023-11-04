0f9840f1bf6676a26b7e0609257db86fb13678ea
cousim46 <wjdghldns24@naver.com>
2023-11-04 23:46:54 +0900

    feat : 소비자가 산 로또번호 출력 메서드 추가

--- src/main/java/lotto/output/PrintService.java
+++ src/main/java/lotto/output/PrintService.java
@@ -1,5 +1,9 @@
 package lotto.output;
 
+import java.util.List;
+import java.util.Map;
+import lotto.ListDataTypeConverter;
+
 public class PrintService {
 
     public static void requestInputPurchasePrice() {
@@ -12,5 +16,13 @@ public class PrintService {
 
     public static void informPurchaseCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void buyLottoNumbers(Map<Integer, List<Integer>> lottoNumbers) {
+        for (Integer key : lottoNumbers.keySet()) {
+            String lottoNumber = String.join(", ",
+                ListDataTypeConverter.intergerToString(lottoNumbers.get(key)));
+            System.out.printf("[ %s ]\n", lottoNumber);
+        }
     }
 }

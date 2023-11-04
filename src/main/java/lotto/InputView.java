0df7f74fcb33d5dc08708fd1887fb830d5d00d2a
sangxxjin <sj_0368@naver.com>
2023-11-05 03:52:54 +0900

    refactor: 컨벤션 적용

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private final static String PURCHASE_MESSAGE = "구입금액을 입력해 주세요";
     private final static String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
 
-    public static String getPurchaseAmount(){
+    public static String getPurchaseAmount() {
         System.out.println(PURCHASE_MESSAGE);
         return Console.readLine();
     }

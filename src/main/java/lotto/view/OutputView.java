c02a97d547156c2ea136df0804a9953e718485d8
jifrozen0110 <answldjs1836@gmail.com>
2023-11-05 00:39:46 +0900

    feat(OutputView) : 로또 게임 출력문 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,13 @@
 package lotto.view;
 
 public class OutputView {
+
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String BUY_INFO_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTICS_INFO_MESSAGE = "당첨 통계\n---";
+
     public static void print(String text) {
         System.out.print(text);
     }
@@ -12,4 +19,25 @@ public class OutputView {
     public static void printError(String errorText) {
         System.out.println(errorText);
     }
+
+    public static void printMoneyInputMessage() {
+        println(MONEY_INPUT_MESSAGE);
+    }
+
+    public static void printBuyInfoMessage() {
+        println(BUY_INFO_MESSAGE);
+    }
+
+    public static void printWinningNumbersInputMessage() {
+        println(WINNING_NUMBERS_INPUT_MESSAGE);
+    }
+
+    public static void printBonusNumberInputMessage() {
+        println(BONUS_NUMBER_INPUT_MESSAGE);
+    }
+
+    public static void printWinningStatisticsInfoMessage() {
+        println(WINNING_STATISTICS_INFO_MESSAGE);
+    }
+
 }

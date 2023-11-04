fb6893a455f48580eb5a3e77d445c87ab94b2c7b
kgy1008 <gayeon1008@g.skku.edu>
2023-11-04 15:48:59 +0900

    docs: 예외 사항 수정

--- src/main/java/view/InputLottoNum.java
+++ src/main/java/view/InputLottoNum.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.Lotto;
+
 import java.util.*;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -7,6 +9,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Input {
     private static final String WINNING_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String LOTTO_PRICE = "구입금액을 입력해 주세요.";
 
     private static List<Integer> winningNumber;
 
@@ -17,6 +20,11 @@ public class Input {
         return winningNumber;
     }
 
+    public static int enterPurchaseAmount() {
+        System.out.println(LOTTO_PRICE);
+        return Lotto.checkInteger(readLine());
+    }
+
     public static int createBonusNumber() {
         System.out.println(BONUS_NUMBER);
         return Integer.parseInt(readLine());
@@ -24,16 +32,8 @@ public class Input {
 
     public static void convertToInteger(String input){
         for (String num : input.split(",")){
-            winningNumber.add(checkInteger(num));
+            winningNumber.add(Lotto.checkInteger(num));
         }
     }
 
-    private static int checkInteger(String input){
-        try{
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e){
-            ExceptionMessage.typeException();
-            throw new IllegalArgumentException();
-        }
-    }
 }

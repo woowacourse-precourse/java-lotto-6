537daed89ef1a81979faaa10bdc3b33f0b3a84a6
waveofmymind <sjun0913@gmail.com>
2023-11-04 21:33:25 +0900

    feat: 발급 가능한 로또의 수 만큼 로또를 생성한다.

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+
+    private final NumberGenerator numberGenerator;
+
+    public LottoGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public Lottos generateLotto(Money money) {
+        int numberOfLotto = money.calculateNumberOfLottos();
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> lottoNumbers = numberGenerator.generateNumber();
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto);
+        }
+        return Lottos.of(lottos);
+    }
+}
---
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoGeneratorTest {
+
+    @DisplayName("발급 가능한 로또의 수 만큼 로또 객체를 생성한다.")
+    @Test
+    void createLottosTest() {
+        // given
+        int input = 3000;
+        Money money = Money.of(input);
+        LottoGenerator lottoGenerator = new LottoGenerator(new RandomNumberGenerator());
+
+        // when
+        Lottos lottos = lottoGenerator.generateLotto(money);
+
+        // then
+        Assertions.assertThat(lottos.getLottoCount())
+                .isEqualTo(money.calculateNumberOfLottos());
+    }
+}

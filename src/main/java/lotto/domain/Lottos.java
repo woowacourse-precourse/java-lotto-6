6f1b058208cec95a6fdf4246db79e32c07fe9a7d
doha <dohaahi@naver.com>
2023-11-04 21:22:51 +0900

    feat (Lottos): 여러 개의 로또를 갖는 도메인 구현

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.dto.PurchaseAmountDto;
+import lotto.util.RandomNumberGenerator;
+
+public class Lottos {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+
+    public Lottos(final PurchaseAmountDto purchaseAmountDto) {
+        int purchase_Quantity = purchaseAmountDto.purchaseQuantityLottos();
+
+        for (int quantity = 0; quantity < purchase_Quantity; quantity++) {
+            List<Integer> numbers = RandomNumberGenerator.generate();
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+}

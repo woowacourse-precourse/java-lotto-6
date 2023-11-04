376d42cc6db8187c9beb69c74e123435c1d0e57e
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-05 00:21:41 +0900

    Feat: 추가 보너스 숫자를 입력 받는 함수
    보너스 숫자를 입력받음

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -11,18 +11,25 @@ class User {
     public int inputPurchasingVolume(){
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
+//        입력값 검증 함수
         int volume = Integer.parseInt(input);
-        //        입력값 검증 함수
         return volume/1000;
     }
 
     public List<Integer> inputNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
+//       입력값 검증 함수
         Integer[] inputNums = Stream.of(input.split(",")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
         List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(inputNums));
-//       입력값 검증 함수
         return numbers;
+    }
+
+    public int inputBonusNumber(){
+        String input = Console.readLine();
+//        입력값 검증 함수
+        int number = Integer.parseInt(input);
+        return number;
     }
 
     public List<Integer> getLottoNumbers (){

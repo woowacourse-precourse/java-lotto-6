4439db430daf997be1a3176bae103d6590abd3db
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 19:18:19 +0900

    refactor: 에러 문구 출력 형식 수정

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -3,7 +3,7 @@ package util;
 public enum ErrorMessage {
 
     PRICE_DOESNT_ENTER("[ERROR] 구매 금액이 입력되지 않았습니다. 금액을 다시 입력해 주세요"),
-    PRICE_ISNT_MULTIPLE_OF_THOUSAND_WON("[ERROR] 구매 금액이 천원 단위가 아닙니다. 구매 금액은 천원 단원로 입력해 주세요."),
+    PRICE_ISNT_IN_STANDARD_UNIT("[ERROR] 구매 금액이 천원 단위가 아닙니다. 구매 금액은 천원 단원로 입력해 주세요."),
     PRICE_CONTAIN_NON_NUMERIC_CHARACTER("[ERROR] 구매 금액에 숫자 이외의 문자(공백 포함)가 입력되었습니다. 숫자만 입력해 주세요."),
     PRICE_IS_NEGATIVE("[ERROR] 구매 금액은 0 이상인 정수만 입력해 주세요."),
     LOTTO_CONTAIN_DULPICATE_NUMBER("[ERROR] 로또 번호는 중복되지 않아야 합니다. 중복되지 않은 숫자를 입력해 주세요."),

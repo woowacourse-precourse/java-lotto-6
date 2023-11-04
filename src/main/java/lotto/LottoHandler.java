c7878ae9a4bdd1122eff1e1f51ef17f8dc63d5aa
박찬건 <jg9870@naver.com>
2023-11-04 16:50:54 +0900

    feat(lotto): 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다.

--- src/main/java/lotto/LottoHandler.java
+++ src/main/java/lotto/LottoHandler.java
@@ -5,6 +5,9 @@ public class LottoHandler {
 
     public int calculateLottoTicketCount(String receivedPurchasePrice) {
         int purchasePrice = Integer.parseInt(receivedPurchasePrice);
+        if (purchasePrice % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력해 주세요.");
+        }
         return purchasePrice / LOTTO_PRICE;
     }
 }

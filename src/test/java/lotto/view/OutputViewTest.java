98da9f96a60b33ffb318e3031c7ab95dea3d91d7
yshong1998 <yshong1998@gmail.com>
2023-11-05 05:04:14 +0900

    Test(OutputViewTest): 구매 금액 입력에 따른 출력 테스트 추가

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -2,16 +2,21 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 class OutputViewTest {
 
-    @Test
-    void responseLottosTest(){
+    @DisplayName("구매 금액 입력에 따른 출력 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 2000, 3000, 4000})
+    void responseLottosTest(int purchaseAmount){
         OutputView outputView = new OutputView();
-        List<Lotto> lottos = LottoMachine.generateLottoByPurchaseAmount(8000);
+        List<Lotto> lottos = LottoMachine.generateLottoByPurchaseAmount(purchaseAmount);
         outputView.responseLottos(lottos);
     }
 }

9e1ced7d4f9ea3ced27c8de476cb5ebb062b99cf
JH-TT <leejongho9803@gmail.com>
2023-11-05 02:08:05 +0900

    feat: 보너스 번호 입력시 출력 메시지 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,4 +9,8 @@ public class InputView {
     public static void inputNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public static void inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }

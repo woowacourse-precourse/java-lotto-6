5a74c56d47a638953dde83336989451236e9a8ac
kongji <kongji4092@gmail.com>
2023-11-04 16:23:34 +0900

    feat: 당첨번호 입력 메시지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,7 @@ public class OutputView {
 
     public static final String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String PURCHASED_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    public static final String INPUT_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static void printInputPrice() {
         System.out.println(INPUT_PURCHASE_MESSAGE);
@@ -37,5 +38,9 @@ public class OutputView {
         }
         stringBuilder.append("]");
         System.out.println(stringBuilder.toString());
+    }
+
+    public static void printGetInputNumber() {
+        System.out.println(INPUT_LOTTO_MESSAGE);
     }
 }

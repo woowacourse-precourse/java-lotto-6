76e5e4bf74694c0bbbbbe41e140ac967716a4799
Firedrago95 <yonghwa95@naver.com>
2023-11-04 16:48:17 +0900

    feat(Rank) : 등수 구하기 기능 및 enum 클래스 작성

---
+++ src/main/java/lotto/Domain/Rank.java
@@ -0,0 +1,41 @@
+package lotto.Domain;
+
+public enum Rank {
+    FIRST(2000000000,6,false),
+    SECOND(30000000,5,true),
+    THIRD(1500000,5,false),
+    FOURTH(50000,4,false),
+    FIFTH(5000,3,false),
+    LOSE(0,0,false);
+
+    private int prizeAmount;
+    private int matchedNumbers;
+    private boolean containBonusNum;
+
+    Rank(int prizeAmount, int matchedNumbers, boolean containBonusNum) {
+        this.prizeAmount = prizeAmount;
+        this.matchedNumbers = matchedNumbers;
+        this.containBonusNum = containBonusNum;
+    }
+
+    public static Rank getRank(int sameNumbers, boolean isBonus) {
+        for (Rank rank : Rank.values()) {
+            if (rank.getMatchedNumbers() == sameNumbers && rank.isContainBonusNum() == isBonus) {
+                return rank;
+            }
+        }
+        return LOSE;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
+
+    public int getMatchedNumbers() {
+        return matchedNumbers;
+    }
+
+    public boolean isContainBonusNum() {
+        return containBonusNum;
+    }
+}

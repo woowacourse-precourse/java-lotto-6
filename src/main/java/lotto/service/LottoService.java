f1b6746597c142a988d97dbbdee70b2c3aa3dd14
dong2ast <kdh01132006@naver.com>
2023-11-04 20:31:47 +0900

    ✨ feat: 로또 개수만큼 Lotto 객체 생성 기능 구현 객체 생성마다 6개의 중복되지 않는 숫자 생성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,10 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
 
 public class LottoService {
 
@@ -21,5 +25,9 @@ public class LottoService {
             }
         }
         int lottoCount = tryNum / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
     }
 }

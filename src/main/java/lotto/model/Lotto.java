8620a58d161e29a1bd88590f4c7018633ae79641
MingyeomKim <mingyum119@naver.com>
2023-11-04 20:55:44 +0900

    feat(Lotto): 로또를 생성하는 정적 팩토리 메서드 생성
    기존 생성자를 private 제어자로 변경하고, 로또 객체를 생성하는 정적 팩토리 메서드를 생성한다.

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    private Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public static Lotto from(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // TODO: 추가 기능 구현
+}

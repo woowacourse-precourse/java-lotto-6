a9f6bd3bb68c031b9138eca976b555213cfbb45a
minjun7410 <minjun7410@gmail.com>
2023-11-04 20:41:33 +0900

    feat (WinningNumber): 1-45 범위의 숫자 예외처리 기능 구현
    * 1-45 범위를 벗어난 숫자라면 예외처리하는 메서드를 추가했다.
    * 입력값이 숫자인지 예외처리하는 메서드를 추가했다.

---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+import java.util.List;
+
+public class WinningNumber {
+    private Lotto lottoNumber;
+    public WinningNumber(List<String> inputs) {
+        lottoNumber = new Lotto(inputs.stream()
+                .map(this::convertInputToLottoNumber)
+                .toList());
+    }
+
+    private int convertInputToLottoNumber(String input) {
+        validateIsNumber(input);
+        validateRange(input);
+        return Integer.parseInt(input);
+    }
+    private void validateIsNumber(String input) {
+        if (input.matches("\\d")) return;
+        throw new IllegalArgumentException("당첨 번호는 숫자를 입력해주세요.");
+    }
+
+    private void validateRange(String input) {
+        int number = Integer.parseInt(input);
+        if (number > 0 && number <= 45) return;
+        throw new IllegalArgumentException(("당첨 번호는 1에서 45 숫자를 입력해주세요"));
+    }
+}

710e2bad5a40700624a08f57c5a8523e08e7525a
jifrozen0110 <answldjs1836@gmail.com>
2023-11-04 22:37:16 +0900

    feat(OutputView) : String 출력 기능

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+public class OutputView {
+    public static void print(String text) {
+        System.out.print(text);
+    }
+
+    public static void println(String text) {
+        System.out.println(text);
+    }
+
+    public static void printError(String errorText) {
+        System.out.println(errorText);
+    }
+}

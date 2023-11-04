752d56ed612396ad111e409e2238ee8e9042de71
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 21:00:17 +0900

    feat(InputValidator): 쉼표를 기준으로 구분 검증

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,12 +1,19 @@
 package lotto.validator;
 
 import lotto.constants.InputPattern;
+import lotto.exception.NotDivisionCommaException;
 import lotto.exception.NotNumericException;
 
 public class InputValidator {
     public void validateNumeric(String lottoPurchasePrice) {
         if (InputPattern.isNotNumeric(lottoPurchasePrice)) {
             throw new NotNumericException();
+        }
+    }
+
+    public void validateDivisionComma(String winningNumbers) {
+        if (InputPattern.isNotDivisionComma(winningNumbers)) {
+            throw new NotDivisionCommaException();
         }
     }
 }

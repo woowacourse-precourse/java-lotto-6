86cfaad5f58c1d449ddff4913b64fdfbb44e989d
maark1106 <mark1106@naver.com>
2023-11-04 23:22:27 +0900

    feat : inputView 메세지 출력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,12 +10,14 @@ import lotto.util.InputValidator;
 public class InputView {
 
     public static int inputPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
         InputValidator.validatePurchaseAmount(purchaseAmount);
         return Integer.parseInt(purchaseAmount);
     }
 
     public List<Integer> inputLottoNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
         String lottoInput = Console.readLine();
         List<String> lottoNumbers = InputConvertUtil.lottoNumberToLottoList(lottoInput);
         InputValidator.validateLottoNumber(lottoNumbers);

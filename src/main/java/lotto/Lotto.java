737d6ab0822fe83ff4ff643e764b6c44d3f9dc79
ks0927 <brian_als@naver.com>
2023-11-04 15:57:40 +0900

    feat(Lotto): 로또 번호가 6개 인지 검증하는 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private static final String INVALID_SIZE_ERROR_MESSAGE = "[ERROR] 로또 숫자는 6개여야 합니다.";
+    public static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (isInvalidSize(numbers)) {
+            throw new IllegalArgumentException(INVALID_SIZE_ERROR_MESSAGE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isInvalidSize(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
+
 }

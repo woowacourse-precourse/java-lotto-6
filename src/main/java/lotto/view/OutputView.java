9c3f5861840e80cb2eb521b4a17aaafc17df448e
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-04 19:46:48 +0900

    refactor(OutputView): 출력 요구사항을 맞추기 위해 줄바꿈 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,10 +11,16 @@ public class OutputView {
     }
 
     public void printPurchasedLottoCount(int purchasedCount) {
+        printNewLine();
         System.out.printf(String.format("%d개를 구매했습니다.", purchasedCount));
     }
 
     public void printLottos(List<Lotto> lottos) {
+        printNewLine();
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public void printNewLine() {
+        System.out.println();
     }
 }

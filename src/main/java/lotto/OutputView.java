d261a588bc12ea92d44404f5e62d4514ece4e247
PlutoWooSeok <plutoxxx1014@gmail.com>
2023-11-04 17:29:31 +0900

    feat: Add OutputView func about printMessage

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,2 +1,26 @@
-package lotto;public class OutputView {
+package lotto;
+
+import java.util.HashMap;
+
+public class OutputView {
+    private static final String OUTPUT_LOTTO_PERCHASED_COUNT = "%d개를 구매했습니다.";
+    private static final String OUTPUT_LOTTO_WINNING_STATICS_MESSAGE = "당첨 통계\n---";
+    private static final String OUTPUT_LOTTO_TOTAL_RATE_OF_RETURN = "총 수익률은 %f%%입니다.";
+
+    public static void printLottoPerchasedCount(int numberOfPerchasedCount){
+        System.out.printf(OUTPUT_LOTTO_PERCHASED_COUNT, numberOfPerchasedCount);
+    }
+
+    public static void printStatics(HashMap<LottoData, Integer> lotteryData) {
+        System.out.println(OUTPUT_LOTTO_WINNING_STATICS_MESSAGE);
+
+        for (LottoData lottoData : LottoData.getValues()) {
+            System.out.println(lottoData.getStaticsMessage(lotteryData.get(lottoData)));
+        }
+    }
+
+    public static void printRateOfReturn(float rateOfReturn) {
+        System.out.printf(OUTPUT_LOTTO_TOTAL_RATE_OF_RETURN, rateOfReturn);
+    }
+
 }

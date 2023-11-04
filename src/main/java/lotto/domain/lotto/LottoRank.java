3afc2187b35bf09b861d209610817fdf2de840fd
uhanuu <hwryu990927@gmail.com>
2023-11-04 15:57:58 +0900

    feat: 총 수익률 구하기

--- src/main/java/lotto/domain/lotto/LottoRank.java
+++ src/main/java/lotto/domain/lotto/LottoRank.java
@@ -2,34 +2,40 @@ package lotto.domain.lotto;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
+//View의 계산을 도와주는 로직인데 domain에서 알고 있는거 분리해보기
 public enum LottoRank {
-    NO_RANK(0, 0,
+    NO_RANK(0, count -> 0L,
             count -> System.out.printf("")),
-    FIFTH(3, 5000,
+    FIFTH(3, count -> count * 5000,
             count -> System.out.printf("3개 일치 (5,000원) - %s개\n",count)),
-    FOURTH(4, 50000,
+    FOURTH(4, count -> count * 50000,
             count -> System.out.printf("4개 일치 (50,000원) - %s개\n",count)),
-    THIRD(5, 1500000,
+    THIRD(5, count -> count * 1500000,
             count -> System.out.printf("5개 일치 (1,500,000원) - %s개\n",count)),
-    SECOND(5, 30000000,
+    SECOND(5, count -> count * 30000000,
             count -> System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %s개\n",count)),
-    FIRST(6, 2000000000,
+    FIRST(6, count -> count * 2000000000,
             count -> System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", count));
 
     private static final int CHECK_MATCH_COUNT_VALUE = 5;
     private final int matchCount;
+    private final Function<Long, Long> expression;
     private final Consumer<Long> rankMessage;
-    private final int winningAmount;
 
-    LottoRank(int matchCount, int winningAmount, Consumer<Long> rankMessage) {
+    LottoRank(int matchCount, Function<Long, Long> expression, Consumer<Long> rankMessage) {
         this.matchCount = matchCount;
         this.rankMessage = rankMessage;
-        this.winningAmount = winningAmount;
+        this.expression = expression;
     }
 
     public void accept(long count) {
         rankMessage.accept(count);
+    }
+
+    public long calculate(long count) {
+        return expression.apply(count);
     }
 
     public static LottoRank getRank (int count, boolean isBonus) {
--- src/main/java/lotto/view/output/LottoResultOutputView.java
+++ src/main/java/lotto/view/output/LottoResultOutputView.java
@@ -1,26 +1,38 @@
 package lotto.view.output;
 
 import lotto.domain.lotto.LottoRank;
-import lotto.message.LottoResultMessage;
 
-import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
 public class LottoResultOutputView {
 
+    private static final String WINNING_STATISTICS = "당첨 통계";
+    private static final String DASH_LINE = "---";
+    private static final String TOTAL_PROFIT_FORMAT = "총 수익률은 %.1f%%입니다.";
+    private static final double PERCENTAGE = 100.0;
+
     public void printWinningStatisticsStartMessage() {
-        System.out.println(LottoResultMessage.WINNING_STATISTICS.getMessage());
+        System.out.println(WINNING_STATISTICS);
     }
 
     public void printDashLine() {
-        System.out.println(LottoResultMessage.DASH_LINE.getMessage());
+        System.out.println(DASH_LINE);
     }
 
     public void printLottoAllMatch(Map<LottoRank, Long> lottoRankCountMap) {
         Set<LottoRank> lottoRanks = lottoRankCountMap.keySet();
         lottoRanks.stream()
                 .forEach(lottoRank -> lottoRank.accept(lottoRankCountMap.get(lottoRank)));
+    }
+
+    public void printTotalProfitRate(int amount, Map<LottoRank, Long> lottoRankCountMap) {
+        Set<LottoRank> lottoRanks = lottoRankCountMap.keySet();
+        long totalAmount = lottoRanks.stream()
+                .mapToLong(lottoRank -> lottoRank.calculate(lottoRankCountMap.get(lottoRank)))
+                .sum();
+
+        System.out.printf(TOTAL_PROFIT_FORMAT, (totalAmount / amount) * PERCENTAGE);
     }
 
 }

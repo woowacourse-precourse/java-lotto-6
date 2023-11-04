b44fa9b3c435c50aafdef13190e704ffd2c1b9e4
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 20:21:31 +0900

    [feat] 등수당 당첨 통계 계산하기 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,14 +1,30 @@
 package lotto.domain;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+
 import java.util.List;
+import java.util.Map;
 
 public class Lottos {
 
     private final List<Lotto> lottos;
     private final int number;
+    private Map<Ranking, Integer> winningStatics;
+    private int totalPrize;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
         this.number = lottos.size();
+    }
+
+
+
+    private void calculateWinningStatistics(List<Integer> winningNumbers, int bonusNumber) {
+        winningStatics = lottos.stream()
+                .collect(groupingBy(
+                        lotto -> lotto.getRanking(winningNumbers, bonusNumber),
+                        summingInt(lotto -> 1)
+                ));
     }
 }

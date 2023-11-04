56b729ec815db20c16ed78022caf58dd1664e45a
WOONBE <kepull2918@naver.com>
2023-11-04 13:04:21 +0900

    [feat] 발급받은 로또의 등수를 매기는 로직 추가

--- src/main/java/lotto/service/GameService.java
+++ src/main/java/lotto/service/GameService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import lotto.domain.Game;
 import lotto.domain.Lotto;
 import lotto.domain.Rank;
 import lotto.domain.User;
@@ -11,18 +12,23 @@ import java.util.HashMap;
 public class GameService {
     private final LottoRepository lottoRepository = new LottoRepository();
 
+    private final LottoService lottoService = new LottoService();
+
     private final User user = new User();
     private final HashMap<Rank, Integer> lottoResult = new HashMap<>();
 
+    private HashMap<Rank, Integer> userResult = getLottoResult();
+
     private static final int INIT_SUM = 0;
+
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
     private static final int ROUND_CONSTANT_INT_TEN = 10;
     private static final double ROUND_CONSTANT_DOUBLE_TEN = 10.0;
     private static final double PERCENT = 100;
 
 
-    public void buyOneLotto(Lotto lotto){
-        lottoRepository.save(lotto);
-    }
+
 
     //전체 로또 조회
     public ArrayList<Lotto> getLotteries(){
@@ -56,6 +62,31 @@ public class GameService {
         lottoResult.put(Rank.NO_RANK_TWO, 0);
         lottoResult.put(Rank.NO_RANK_ONE, 0);
         lottoResult.put(Rank.NO_RANK_ZERO, 0);
+    }
+
+    public void checkUserLotteries(User user, Game game){
+        for(Lotto lotto : lottoRepository.getLotteries()){
+            Rank rank = classifyLottoRank(game,lotto);
+            userResult.put(rank, userResult.getOrDefault(rank, ZERO) + ONE);
+        }
+
+    }
+    private int countMatchLottoNumber(Game game, Lotto lotto){
+        return lottoService.countMatchNumber(game.getWinningNumbers(), lotto.getNumbers());
+    }
+
+    private boolean isContainBonusNumber(Game game, Lotto lotto){
+        return lotto.getNumbers().contains(game.getBonusNumber());
+    }
+
+    private Rank classifyLottoRank(Game game, Lotto lotto){
+        int matchCount = countMatchLottoNumber(game,lotto);
+        boolean matchBonusNumber = isContainBonusNumber(game,lotto);
+        Rank rank = Rank.values()[matchCount];
+        if(rank == Rank.THIRD && matchBonusNumber){
+            return Rank.SECOND;
+        }
+        return rank;
     }
 
 }

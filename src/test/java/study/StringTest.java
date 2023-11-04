7b70afc9dc29d59a668feb0a033e09481301d0eb
kseenyoung <kseenyoung@gmail.com>
2023-11-04 16:53:52 +0900

    test: 요구사항 1-1 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,4 +1,22 @@
 package study;
 
+import static  org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class StringTest {
+
+    @Test
+    @Disabled
+    public void test1_1(){
+        String target = "1,2";
+        String[] targets = target.split(",");
+
+        assertThat(targets)
+                .contains("1", "2");
+    }
+
+
 }

a08fe69c66e7d666e51b2ce57dfa92e03d62a253
Hoo-sung <gntjd135@gmail.com>
2023-11-04 12:43:59 +0900

    feat(OutputHandler): Collections.frequency 사용하여, 당첨 통계 출력하는 메서드 생성

--- src/main/java/lotto/io/OutputHandler.java
+++ src/main/java/lotto/io/OutputHandler.java
@@ -1,6 +1,9 @@
 package lotto.io;
 
 
+import lotto.domain.Award;
+
+import java.util.Collections;
 import java.util.List;
 
 public final class OutputHandler {
@@ -15,5 +18,15 @@ public final class OutputHandler {
 
     public static void printReceipts(int size){
         System.out.println(size+"개를 구매했습니다.");
+    }
+
+    public static void printReward(List<Award> awards){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+ Collections.frequency(awards,Award.THREE)+"개");
+        System.out.println("4개 일치 (50,000원) - "+ Collections.frequency(awards,Award.FOUR)+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+ Collections.frequency(awards,Award.FIVE)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+ Collections.frequency(awards,Award.FIVE_BONUS)+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+ Collections.frequency(awards,Award.SIX)+"개");
     }
 }

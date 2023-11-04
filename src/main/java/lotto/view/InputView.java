435871270cdda0618b6b3cf216fcd3337ec209f0
WooYoungDoo <duden5000@naver.com>
2023-11-05 01:00:40 +0900

    feat (InputView) : 입력 관련 기능

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputPurchaseMoney() {
+        return Console.readLine();
+    }
+
+    public String inputWinnerNumber() {
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        return Console.readLine();
+    }
+
+}

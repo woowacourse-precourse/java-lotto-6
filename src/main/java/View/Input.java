b476fecb5f9e874607123cbc237b6ad8918cae4e
unknown <hakie2kim@gmail.com>
2023-11-04 21:39:44 +0900

    feat(src/main/java/lotto/): get received amount from the buyer

--- src/main/java/View/Input.java
+++ src/main/java/View/Input.java
@@ -3,17 +3,17 @@ package View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    static String readReceivedAmount() {
+    public static String readReceivedAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    static String readPickedNumbers() {
+    public static String readPickedNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    static String readBonusNumber() {
+    public static String readBonusNumber() {
         System.out.println("보너스 번호를 입력해주세요.");
         return Console.readLine();
     }
--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,9 +1,33 @@
 package lotto;
 
+import View.Input;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoApplication {
     private static int LOTTO_PRICE = 1000;
+
+    void execute() {
+        int receivedAmount = getReceivedAmount();
+    }
+
+    int getReceivedAmount() {
+        int receivedAmount;
+
+        try {
+            receivedAmount = Integer.parseInt(Input.readReceivedAmount());
+            validateReceivedAmount(receivedAmount);
+        } catch (NumberFormatException nfe) { // 정수값 입력하지 않았을 경우
+            throw new IllegalArgumentException();
+        }
+
+        return receivedAmount;
+    }
+
+    private void validateReceivedAmount(int receivedAmount) {
+        if (!(receivedAmount % LOTTO_PRICE == 0)) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     private Lotto drawLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(Lotto.START_NUMBER, Lotto.END_NUMBER, 6));

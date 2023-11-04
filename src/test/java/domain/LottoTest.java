8007fd7cb83e5deed90f83613bcfb51f06144ba7
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-04 23:37:00 +0900

    test: 로또 번호가 오름차순 정렬되는지 테스트

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -3,9 +3,12 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -22,5 +25,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호는 오름차순으로 정렬 된다.")
+    @Test
+    void isNumberSortByAscendingOrder() {
+        List<Integer> numbers = new ArrayList<>(List.of(6,4,3,2,1,5));
+        Lotto lotto = new Lotto(numbers);
+        Collections.sort(numbers);
+        assertEquals(lotto.getNumbers(),numbers);
+    }
+    
 }

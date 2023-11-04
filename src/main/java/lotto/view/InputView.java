a7f3da77c2f6c0db09609221d469c5a981a2144d
eunhee <dmsgml9903@gmail.com>
2023-11-04 13:42:12 +0900

    feat(InputView): 사용자로부터 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     private final PurchaseAmountValidator purchaseAmountValidator;
 
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
+    private static final String INPUT_DRAWN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public InputView(PurchaseAmountValidator purchaseAmountValidator) {
         this.purchaseAmountValidator = purchaseAmountValidator;
@@ -18,5 +19,11 @@ public class InputView {
         String purchaseAmount = Console.readLine();
         purchaseAmountValidator.validatePurchaseAmount(purchaseAmount);
         return Integer.valueOf(purchaseAmount);
+    }
+
+    public String getDrawnNumbers() {
+        System.out.println(INPUT_DRAWN_NUMBERS_MESSAGE);
+        String drawnNumbers = Console.readLine();
+        return drawnNumbers;
     }
 }

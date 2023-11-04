ba8e3f2cffd359bf540566cf6fbfa609a25c8122
WooYoungDoo <duden5000@naver.com>
2023-11-05 01:15:52 +0900

    feat (OutputView) : 빈줄 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,8 @@ public class OutputView {
     private final static String OUTPUT_AMOUNT_LOTTO_MESSAGE = "%d개를 구매했습니다.";
     private final static String OUTPUT_WINNER_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String BLANK_LINE = "";
+
 
     public void printPurchaseMoneyMessage() {
         System.out.println(OUTPUT_PURCHASE_MONEY_MESSAGE);
@@ -21,5 +23,9 @@ public class OutputView {
 
     public void printBonusNumberMessage() {
         System.out.println(OUTPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public void printBlankLine() {
+        System.out.println(BLANK_LINE);
     }
 }

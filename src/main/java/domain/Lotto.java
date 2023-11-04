e7562411f1d1e3b3d431c831ebccd0bacfd91971
nohheeyeon <heeyeon1213@gmail.com>
2023-11-05 01:49:03 +0900

    feat : 로또 번호 유효성 검사 메서드 구현

---
+++ src/main/java/domain/Lotto.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+            }
+        }
+    }
+}

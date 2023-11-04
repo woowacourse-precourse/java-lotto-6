c45fde3f5550cc5819cd42c462508d63b8001bfe
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-04 20:21:36 +0900

    feat(LottoFactory) : 개수만큼 로또를 발행하는 기능추가
    docs : 로또발행부분 체크완료

---
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoFactory {
+    private final List<Lotto> lottoTickets;
+
+    public LottoFactory(long ticketAcounts) {
+        this.lottoTickets = generateLottos(ticketAcounts);
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return lottoTickets;
+    }
+
+    private List<Lotto> generateLottos(long ticketAcounts) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < ticketAcounts; i++) {
+            lottoTickets.add(makeLotto());
+        }
+        return lottoTickets;
+    }
+
+    private Lotto makeLotto() {
+        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(lotto);
+        return new Lotto(lotto);
+    }
+
+}

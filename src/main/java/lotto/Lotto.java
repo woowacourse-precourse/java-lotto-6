74ed600e4c37b87d151d5838bbecc687dd71d3d0
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 03:52:48 +0900

    feat : 당첨 번호에 대한 비즈니스 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,15 @@
 package lotto;
 
+import static lotto.constant.ErrorMessage.DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE;
+import static lotto.constant.ErrorMessage.INVALID_LOTTO_SIZE_ERROR_MESSAGE;
+import static lotto.constant.ErrorMessage.LARGER_THAN_MAX_LOTTO_NUMBER_ERROR_MESSAGE;
+import static lotto.constant.ErrorMessage.LOWER_THAN_MIN_LOTTO_NUMBER_ERROR_MESSAGE;
+import static lotto.constant.LottoInfo.LOTTO_SIZE;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.constant.LottoInfo;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +20,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_SIZE.getValue()) {
+            throw new IllegalArgumentException(INVALID_LOTTO_SIZE_ERROR_MESSAGE.getMessage());
+        }
+        if (hasDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE.getMessage());
+        }
+        if (hasLargerThanMaxNumber(numbers)) {
+            throw new IllegalArgumentException(LARGER_THAN_MAX_LOTTO_NUMBER_ERROR_MESSAGE.getMessage());
+        }
+        if (hasLessThanMinNumber(numbers)) {
+            throw new IllegalArgumentException(LOWER_THAN_MIN_LOTTO_NUMBER_ERROR_MESSAGE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> numberGroup = new HashSet<>(numbers);
+        return numbers.size() != numberGroup.size();
+    }
+
+    private boolean hasLargerThanMaxNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number > LottoInfo.LOTTO_MAX_NUMBER.getValue());
+    }
+
+    private boolean hasLessThanMinNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < LottoInfo.LOTTO_MIN_NUMBER.getValue());
+    }
 }

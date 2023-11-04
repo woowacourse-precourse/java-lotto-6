73091ab7ab83da5392caf851da74a21676bc9d79
kseenyoung <kseenyoung@gmail.com>
2023-11-04 17:28:07 +0900

    test: Set 요구사항 1 작성 - set의 크기 확인

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,4 +1,23 @@
 package study;
 
+import static  org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
 public class SetTest {
+
+    @Test
+    public void test1(){
+        Set<String> target = new HashSet<>();
+        target.add("item1");
+        target.add("item2");
+
+        assertThat(target)
+                .hasSize(2);
+    }
+
+
+
 }

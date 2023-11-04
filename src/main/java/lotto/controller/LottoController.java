ac03e39c6f7bf20a1971b22f1cdc469ae131d6ba
jaeyoungjang2 <lion85488@gmail.com>
2023-11-04 17:56:58 +0900

    feat: 로또 구입 금액 검증  - 로또는 1000원 단위로만 구입 가능  - 로또 금액은 숫자만 입력 가능

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,29 @@
+package lotto.controller;
+
+public class LottoController {
+
+    public void buyLotteries(String stringPurchasePrice) {
+        Integer purchasePrice = validatePurchasePrice(stringPurchasePrice);
+    }
+
+    private Integer validatePurchasePrice(String stringPurchasePrice) {
+        validatePurchasePriceType(stringPurchasePrice);
+        Integer purchasePrice = Integer.valueOf(stringPurchasePrice);
+        validatePurchasePriceRange(purchasePrice);
+        return purchasePrice;
+    }
+
+    private void validatePurchasePriceRange(Integer purchasePrice) {
+        if (purchasePrice % 1_000 != 0) {
+            throw new IllegalArgumentException("로또는 1000원 단위로만 구매할 수 있습니다.");
+        }
+    }
+
+    private void validatePurchasePriceType(String purchasePrice) {
+        try {
+            Integer.valueOf("abc");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("로또 금액은 숫자만 입력 가능합니다.");
+        }
+    }
+}

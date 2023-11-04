5722bc4d1903fe369f2e07b9cc9397e4b3b3fbf6
KangGeonyoung <kky6335@gmail.com>
2023-11-04 19:03:03 +0900

    feat(InputView): 당첨 번호, 보너스 번호 입력 기능 - printWinningNumber(): 사용자로부터 당첨 번호 입력 받기 - printBonusNumber(): 사용자로부터 보너스 번호 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,4 +8,14 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public static String printWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String printBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }

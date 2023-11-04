f11ee5b6360f389a49bae89ec9028f1a245fe74b
alscjf1329 <alscjf1329@gmail.com>
2023-11-04 17:23:36 +0900

    feat(Result): Result 작성
    Result enum class를 작성했습니다.

---
+++ src/main/java/lotto/message/Result.java
@@ -0,0 +1,38 @@
+package lotto.message;
+
+public enum Result {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    NONE(0);
+    private final int prize;
+
+    Result(int prize) {
+        this.prize = prize;
+    }
+
+    public static Result of(int matchCount, boolean isBonusNumberMatched) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && isBonusNumberMatched) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        return NONE;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}

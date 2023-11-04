46e77afca100a953dc6172cbd42a8fa70fc229c3
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 10:35:59 +0900

    feat(LottoRule): 로또 규칙 상수 생성

---
+++ src/main/java/lotto/constants/LottoRule.java
@@ -0,0 +1,10 @@
+package lotto.constants;
+
+public final class LottoRule {
+    public static final Integer NUMBER_MAX_SIZE = 6;
+    public static final Integer START_NUMBER = 1;
+    public static final Integer END_NUMBER = 45;
+
+    private LottoRule() {
+    }
+}

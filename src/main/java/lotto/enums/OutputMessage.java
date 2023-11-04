aa884ea4f8b5d57dc905453baf9ffdf51af61a6d
jdohun <wjdehgns97@naver.com>
2023-11-04 12:25:07 +0900

    Feat: 구매 수량을 보여주는 FORMAT 수정

--- src/main/java/lotto/enums/OutputMessage.java
+++ src/main/java/lotto/enums/OutputMessage.java
@@ -1,7 +1,7 @@
 package lotto.enums;
 
 public enum OutputMessage {
-    OUTPUT_COUNT_OF_LOTTO("개를 구매했습니다."),
+    OUTPUT_COUNT_OF_LOTTO_FORMAT("%d개를 구매했습니다."),
     WINNING_STATISTICS("당첨 통계\n---"),
     MATCHES_3_FORMAT("3개 일치 (5,000원) - %d개"),
     MATCHES_4_FORMAT("4개 일치 (50,000원) - %d개"),

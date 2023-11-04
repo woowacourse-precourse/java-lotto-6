38bcc107411fec45038c7e9f9c1b60ccdef248ac
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-05 01:45:23 +0900

    refactor: InputView 클래스 분리

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,4 +1,95 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.constants.ErrorMessage;
+import lotto.constants.InputMessage;
+
 public class InputView {
+    public static int getUserPurchaseAmount() {
+        int purchaseAmount;
+
+        while (true) {
+            try {
+                System.out.println(InputMessage.PURCHASE_AMOUNT.getMessage());
+                String input = Console.readLine();
+
+                if (!input.matches("[1-9]\\d*")) {
+                    if (input.matches("[-]\\d+")) {
+                        throw new IllegalArgumentException(ErrorMessage.INVALID_NEGATIVE_PURCHASE.getMessage());
+                    }
+                    if (input.matches("0+")) {
+                        throw new IllegalArgumentException(ErrorMessage.INVALID_ZERO_PURCHASE.getMessage());
+                    }
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_NON_NUMERIC_PURCHASE.getMessage());
+                }
+
+                purchaseAmount = Integer.parseInt(input);
+
+                if (purchaseAmount % 1000 != 0) {
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_AMOUNT_NOT_DIVISIBLE.getMessage());
+                }
+
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return purchaseAmount;
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        List<Integer> lottoNumbers;
+        while (true) {
+            try {
+                System.out.println(InputMessage.LOTTO_NUMBERS.getMessage());
+                String[] numbers = Console.readLine().split(",");
+                lottoNumbers = new ArrayList<>();
+
+                for (int i = 0; i < numbers.length; i++) {
+                    try {
+                        int number = Integer.parseInt(numbers[i]);
+                        lottoNumbers.add(number);
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException(ErrorMessage.INVALID_NON_NUMERIC.getMessage());
+                    }
+                }
+                Lotto lotto = new Lotto(lottoNumbers);
+                return lottoNumbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static int getBonusNumber(List<Integer> lottoNumbers) {
+        System.out.println();
+        int bonusNumber;
+        while (true) {
+            try {
+                System.out.println(InputMessage.BONUS_NUMBER.getMessage());
+                String input = Console.readLine();
+
+                try {
+                    bonusNumber = Integer.parseInt(input);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_NON_NUMERIC_PURCHASE.getMessage());
+                }
+
+                if (bonusNumber < 1 || bonusNumber > 45) {
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
+                }
+
+                if (lottoNumbers.contains(bonusNumber)) {
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_DUPLICATE.getMessage());
+                }
+
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,4 +1,16 @@
 package lotto;
 
+import java.util.List;
+
 public class OutputView {
+    public static void printLottoTickets(List<Integer> numbers) {
+        System.out.print("[");
+        for (int j = 0; j < numbers.size(); j++) {
+            if (j == numbers.size() - 1) {
+                System.out.println(numbers.get(j) + "]");
+                break;
+            }
+            System.out.print(numbers.get(j) + ", ");
+        }
+    }
 }

203b01c7c9699443a1b48c7fca6a63c25e2b9ecc
chaeshee0908 <chaeshee0908@naver.com>
2023-11-04 21:58:16 +0900

    [fix] NONE 재생성

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -5,7 +5,8 @@ public enum Ranking {
     SECOND("5개 일치, 보너스 볼 일치", "30,000,000"),
     THIRD("5개 일치", "1,500,000"),
     FOURTH("4개 일치", "50,000"),
-    FIFTH("3개 일치", "5,000");
+    FIFTH("3개 일치", "5,000"),
+    NONE("등수 밖", "0");
 
     private final String description;
     private final String prize;

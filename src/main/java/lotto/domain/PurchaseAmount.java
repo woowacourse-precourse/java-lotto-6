cc1bb7a7b089783ff27a1d01c0a4e502356026ea
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 15:04:00 +0900

    refactor(PurchaseAmount): Validator 클래스 분리

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,12 +1,8 @@
 package lotto.domain;
 
-import java.util.regex.Pattern;
+import static lotto.util.Validator.*;
 
 public class PurchaseAmount {
-
-    private static final int LOTTO_PRICE = 1000;
-    private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
-    private static final String WRONG_UNIT_ERROR_MESSAGE = "[ERROR] 입력 금액은 1,000원 단위여야 합니다.";
     private final String amount;
 
     public PurchaseAmount(String amount) {
@@ -17,19 +13,5 @@ public class PurchaseAmount {
 
     public String getAmount() {
         return amount;
-    }
-
-    private void validateNonNumeric(String input) {
-        String pattern = "^[0-9]*$";
-        if (!Pattern.matches(pattern, input)) {
-            throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
-        }
-    }
-
-    private void validateUnit(String input) {
-        int num = Integer.parseInt(input);
-        if (num % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(WRONG_UNIT_ERROR_MESSAGE);
-        }
     }
 }
---
+++ src/main/java/lotto/util/Validator.java
@@ -0,0 +1,23 @@
+package lotto.util;
+
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static final int LOTTO_PRICE = 1000;
+    private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자로만 이루어진 값을 입력해주세요.";
+    private static final String WRONG_UNIT_ERROR_MESSAGE = "[ERROR] 입력 금액은 1,000원 단위여야 합니다.";
+
+    public static void validateNonNumeric(String input) {
+        String pattern = "^[0-9]*$";
+        if (!Pattern.matches(pattern, input)) {
+            throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateUnit(String input) {
+        int num = Integer.parseInt(input);
+        if (num % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(WRONG_UNIT_ERROR_MESSAGE);
+        }
+    }
+}

5b302dba39f313653e325982c78d4eb58d7cbe0e
woo <wsk221e@gmail.com>
2023-11-04 15:16:39 +0900

    refactor(TicketsTest): 매직넘버 제거

--- src/test/java/lotto/domain/TicketsTest.java
+++ src/test/java/lotto/domain/TicketsTest.java
@@ -9,18 +9,21 @@ class TicketsTest {
     @DisplayName("Lotto를 입력하면 자신의 리스트에 입력한 Lotto를 추가한다.")
     @Test
     void generateTicketsTest() {
-        // 초기 티켓 갯수 확인
+        // 변수 설정
         Tickets tickets = new Tickets();
+        int BUY_AMOUNT = 2;
+
+        // 초기 티켓 갯수 확인
         int initialCount = tickets.getTicketsCount();
 
         // 새로운 티켓 2장 추가
-        tickets.generateTickets(2);
+        tickets.generateTickets(BUY_AMOUNT);
 
         // 추가한 후 갯수 확인
         int addedCount = tickets.getTicketsCount();
 
         // 티켓이 올바르게 추가되었는지 확인
-        assertTrue(addedCount - initialCount == 2);
+        assertTrue(addedCount - initialCount == BUY_AMOUNT);
     }
 
 }

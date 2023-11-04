1b8cf2b8fd17267d334ab482d9144236a57ba9d3
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-04 14:52:27 +0900

    [test] service 로또 번호 단위 생성 기능 테스트

---
+++ src/test/java/lotto/service/LottoNumberGeneratorTest.java
@@ -0,0 +1,49 @@
+package lotto.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class LottoNumberGeneratorTest {
+
+    @DisplayName("생성 단위 번호의 범위는 1부터 45까지다.")
+    @Test
+    void testGeneratedNumbersRange() {
+        LottoNumberGenerator generator = new LottoNumberGenerator();
+        for (int number : generator.numbers) {
+            assertTrue(number >= 1 && number <= 45);
+        }
+    }
+
+    @DisplayName("생성되는 단위 로또 번호 요소는 총 6개다.")
+    @Test
+    void testGeneratedNumbersSize() {
+        LottoNumberGenerator generator = new LottoNumberGenerator();
+        assertEquals(6, generator.numbers.size());
+    }
+
+    @DisplayName("생성되는 단위 로또 번호 요소는 중복되지 않는다.")
+    @Test
+    void testGeneratedNumbersUniqueness() {
+        LottoNumberGenerator generator = new LottoNumberGenerator();
+        Set<Integer> uniqueNumbers = new HashSet<>(generator.numbers);
+        assertEquals(6, uniqueNumbers.size());
+    }
+
+    @DisplayName("생성되는 단위 로또 번호 요소는 오름차순으로 나열된다.")
+    @Test
+    void testGeneratedNumbersSorting() {
+        LottoNumberGenerator generator = new LottoNumberGenerator();
+        List<Integer> sortedNumbers = new ArrayList<>(generator.numbers);
+        Collections.sort(sortedNumbers);
+        assertEquals(sortedNumbers, generator.numbers);
+    }
+}

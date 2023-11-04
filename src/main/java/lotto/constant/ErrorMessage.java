61860bc6cd1861744b82b48280b7c8c7a309317e
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-05 04:19:14 +0900

    fix:에러메시지 출력시 Error태그 함께 출력

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -2,12 +2,12 @@ package lotto.constant;
 
 public enum ErrorMessage {
     ERROR("[ERROR] "),
-    ONLY_THOUSAND("1000원 단위로 입력하셔야 합니다."),
-    INVALID_INPUT("1000원 이상의 값을 입력하셔야 합니다."),
-    ONLY_INPUT_NUMBER("숫자만 입력하셔야 합니다."),
-    ONLY_SIX_NUMBER("6개의 숫자를 입력하셔야 합니다."),
-    INVALID_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_DUPLICATE("중복된 값은 입력할 수 없습니다.");
+    ONLY_THOUSAND(ERROR+"1000원 단위로 입력하셔야 합니다."),
+    INVALID_INPUT(ERROR+"1000원 이상의 값을 입력하셔야 합니다."),
+    ONLY_INPUT_NUMBER(ERROR+"숫자만 입력하셔야 합니다."),
+    ONLY_SIX_NUMBER(ERROR+"6개의 숫자를 입력하셔야 합니다."),
+    INVALID_RANGE(ERROR+"로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_DUPLICATE(ERROR+"중복된 값은 입력할 수 없습니다.");
 
     private final String message;
 
@@ -17,6 +17,6 @@ public enum ErrorMessage {
 
     @Override
     public String toString() {
-        return (ERROR + message);
+        return message;
     }
 }

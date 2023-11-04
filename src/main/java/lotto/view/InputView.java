989fa73ef42ad6c498049dbcce9ab3617d4c72c4
tam31 <wxogud3377@naver.com>
2023-11-05 04:24:27 +0900

    Feat(InputView) : 예외발생 시 오류 출력 및 재입력 기능 구현 Docs : 예외발생 시 오류 출력 및 재입력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,23 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.InputValidation;
 
 public class InputView {
 
     private static final String PURCHASE_AMOUNT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
 
+    InputValidation inputValidation = new InputValidation();
+
     public String purchaseAmountMoney(){
-        System.out.println(PURCHASE_AMOUNT_MONEY_MESSAGE);
-        return Console.readLine();
+        try{
+            System.out.println(PURCHASE_AMOUNT_MONEY_MESSAGE);
+            String money = Console.readLine();
+            inputValidation.validationMoney(money);
+            return money;
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return purchaseAmountMoney();
+        }
     }
 }

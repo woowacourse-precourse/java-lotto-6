555811db475350def00b24aa461c6eef075ce9d1
박찬건 <jg9870@naver.com>
2023-11-04 17:14:17 +0900

    feat(lotto): 문자를 입력하면 예외가 발생한다.

--- src/main/java/lotto/LottoHandler.java
+++ src/main/java/lotto/LottoHandler.java
@@ -4,7 +4,13 @@ public class LottoHandler {
     private static final int LOTTO_PRICE = 1000;
 
     public int calculateLottoTicketCount(String receivedPurchasePrice) {
-        int purchasePrice = Integer.parseInt(receivedPurchasePrice);
+        int purchasePrice;
+        try {
+            purchasePrice = Integer.parseInt(receivedPurchasePrice);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+        }
+
         if (purchasePrice % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력해 주세요.");
         }

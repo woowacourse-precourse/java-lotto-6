c611b92b2dcad0c64e884aa54d801387439ccb29
irerin07 <counterchord89@gmail.com>
2023-11-05 01:43:06 +0900

    feat: #7 로또 구매 금액 검증 기능 구현
    로또 구매 금액 검증 기능 구현
    - 로또 구매 금액이 로또 가격보다 적은 경우 예외처리
    - 로또 구매 금액이 로또 가격과 딱 맞아떨어지지 않는 경우 예외처리
    
    Closes #7

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,8 +8,8 @@ public class Application {
         // TODO: 프로그램 구현
         InputUI inputUI = new InputUI();
         LottoStore lottoStore = new LottoStore(inputUI);
+
         LottoTickets lottoTickets = lottoStore.buyLotto();
-        int size = lottoTickets.size();
-        System.out.println("size = " + size);
     }
+
 }
--- src/main/java/lotto/domain/LottoPurchaseAmount.java
+++ src/main/java/lotto/domain/LottoPurchaseAmount.java
@@ -15,7 +15,21 @@ public class LottoPurchaseAmount {
 
     public static LottoPurchaseAmount valueOf(int amount) {
         // TODO 검증
+        validateMinimumAmount(amount);
+        validatePurchaseUnit(amount);
         return new LottoPurchaseAmount(amount);
+    }
+
+    private static void validateMinimumAmount(int amount) {
+        if (amount <= Lotto.PRICE) {
+            throw new IllegalArgumentException(Lotto.PRICE + "원보다 작은 금액은 입력하실 수 없습니다.");
+        }
+    }
+
+    private static void validatePurchaseUnit(int amount) {
+        if (amount % Lotto.PRICE != 0) {
+            throw new IllegalArgumentException("로또 구입 금액은 " + Lotto.PRICE + "원 단위로 입력해주세요. 입력한 금액 : " + amount);
+        }
     }
 
     public boolean isOverLottoPrice() {
@@ -25,4 +39,5 @@ public class LottoPurchaseAmount {
     public void subtractOneLottoPrice() {
         value -= Lotto.PRICE;
     }
+
 }

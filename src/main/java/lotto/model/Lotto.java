d2096e5fe2418f64d422e4dee6e22a73639e5af0
hYe807n <rlagkdus55@gmail.com>
2023-11-04 16:24:26 +0900

    Feat: 로또 번호에 중복된 숫자가 있으면 예외 발생

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -23,6 +23,9 @@ public class Lotto {
         if (numbers.stream().anyMatch(number -> number > 45 || number < 1)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호의 숫자 범위는 1~45까지입니다.");
         }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 로또 번호는 입력할 수 없습니다.");
+        }
     }
 
     // TODO: 추가 기능 구현

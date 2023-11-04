fe9e2940f4292fbd001e9490cedcebbe957424b7
김연진 <love_homebody@naver.com>
2023-11-04 18:20:57 +0900

    feat(InputView): 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,23 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
 import lotto.domain.LottoCounter;
+
+import java.util.Arrays;
 
 public class InputView {
 
     public LottoCounter readPurchaseAmount() {
         String input = Console.readLine();
         return new LottoCounter(Integer.parseInt(input));
+    }
+
+    public Lotto readWinningNumber() {
+        String input = Console.readLine();
+        return new Lotto(Arrays.stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList());
     }
 }

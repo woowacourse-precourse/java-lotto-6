f7fe12493287567aa0e50c2c3f7bc56c870ed254
goragoraki <gorakio0126@gmail.com>
2023-11-04 23:16:52 +0900

    feat(OuputView): 로또 구매 개수 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printBuyLotto(Money money) {
+        System.out.printf("%d개를 구매했습니다.\n", money.getTicket());
+    }
+
+}

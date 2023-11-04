fd0dafecdea57099c54d1913cd1fe0fb8597b397
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-04 23:23:08 +0900

    :art: [Refactor] Controller 분리
    이유: 1개 controller에 모든 로직이 있어 결합도 높음

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,20 @@
 package lotto;
 
-import lotto.controller.LottoGameController;
+import java.util.List;
+import lotto.Service.BuyLottoService;
+import lotto.Service.CompareService;
+import lotto.Service.InputPurchasePriceService;
+import lotto.Service.InputService;
+import lotto.Service.StatisticsService;
+import lotto.controller.BuyLottoController;
+import lotto.controller.CompareController;
+import lotto.controller.InputNumberController;
+import lotto.controller.InputPurchasePriceController;
+import lotto.controller.StatisticsController;
+import lotto.dto.LottoResults;
+import lotto.dto.WinnerAndBonusNumber;
+import lotto.model.PurchasePrice;
+import lotto.model.Statistics;
 import lotto.validation.BonusNumberValidator;
 import lotto.validation.PurchasePriceValidator;
 import lotto.validation.WinnerNumberValidator;
@@ -8,6 +22,7 @@ import lotto.view.input.InputView;
 import lotto.view.output.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         PurchasePriceValidator purchasePriceValidator = new PurchasePriceValidator();
         WinnerNumberValidator winnerNumberValidator = new WinnerNumberValidator();
@@ -16,9 +31,26 @@ public class Application {
         InputView inputView = new InputView(purchasePriceValidator, winnerNumberValidator, bonusNumberValidator);
         OutputView outputView = new OutputView();
 
-        LottoGameController lottoGameController = new LottoGameController(inputView, outputView);
+        InputPurchasePriceService inputPurchasePriceService = new InputPurchasePriceService(inputView, outputView);
+        InputPurchasePriceController inputPurchasePriceController = new InputPurchasePriceController(
+                inputPurchasePriceService);
 
-        lottoGameController.start();
+        BuyLottoService buyLottoService = new BuyLottoService(outputView);
+        BuyLottoController buyLottoController = new BuyLottoController(buyLottoService);
 
+        InputService inputService = new InputService(inputView, outputView);
+        InputNumberController inputNumberController = new InputNumberController(inputService);
+
+        CompareService compareService = new CompareService();
+        CompareController compareController = new CompareController(compareService);
+
+        StatisticsService statisticsService = new StatisticsService(outputView);
+        StatisticsController statisticsController = new StatisticsController(statisticsService);
+
+        PurchasePrice purchasePrice = inputPurchasePriceController.purchasePrice();
+        LottoResults lottoResults = buyLottoController.buyLotto(purchasePrice);
+        WinnerAndBonusNumber winnerAndBonusNumber = inputNumberController.inputNumber();
+        List<Statistics> result = compareController.compareNumber(lottoResults, winnerAndBonusNumber);
+        statisticsController.getStatics(result, purchasePrice);
     }
 }

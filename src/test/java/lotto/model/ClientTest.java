b03a69ddfefa2db71bec92527319795c7a84ea1a
liarreez <liarreez@gmail.com>
2023-11-05 00:22:48 +0900

    test : 당첨 결과가 주어졌을 때 수익률을 계산하는 테스트 작성

---
+++ src/test/java/lotto/model/ClientTest.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClientTest {
+    @DisplayName("당첨 결과가 주어졌을 때 수익률을 계산한다. - 7000원으로 5000원 당첨되면 71.4% 수익률")
+    @Test
+    void testCalculateRateOfReturnCaseOne() {
+        Client client = new Client();
+        client.pay("7000");
+        List<Integer> lottoResults = List.of(0,0,0,0,0,1);
+
+        double rateOfReturn = client.calculateRateOfReturn(lottoResults);
+
+        assertThat(rateOfReturn).isEqualTo(71.4);
+    }
+
+    @DisplayName("당첨 결과가 주어졌을 때 수익률을 계산한다. - 13000원으로 55000원 당첨되면 71.4% 수익률")
+    @Test
+    void testCalculateRateOfReturnCaseTwo() {
+        Client client = new Client();
+        client.pay("13000");
+        List<Integer> lottoResults = List.of(0,0,0,0,1,1);
+
+        double rateOfReturn = client.calculateRateOfReturn(lottoResults);
+
+        assertThat(rateOfReturn).isEqualTo(423.1);
+    }
+}

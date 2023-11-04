62c4b1591eb5e974165cbe749f04217c6affed41
Genius-PAK <pyj789123@gmail.com>
2023-11-04 10:29:29 +0900

    feat(WinningNumberConverter): 입력받은 당첨번호를 각 번호에 순서를 붙여 맵으로 변환해주는 클래스 추가

---
+++ src/main/java/lotto/controller/WinningNumberConverter.java
@@ -0,0 +1,41 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.GameConstants;
+
+public class WinningNumberConverter implements Converter<Map<Integer, Integer>> {
+    @Override
+    public Map<Integer, Integer> convert(String WinningNumbers) {
+        String[] splitWinningNumbers = WinningNumbers.split(GameConstants.SEPARATOR);
+
+        List<Integer> numericWinningNumbers = getNumericWinningNumbers(splitWinningNumbers);
+        Collections.sort(numericWinningNumbers);
+
+        return getSequencedWinningNumbers(numericWinningNumbers);
+    }
+
+    private List<Integer> getNumericWinningNumbers(String[] splitWinningNumbers) {
+        List<Integer> numericWinningNumbers = new ArrayList<>();
+
+        for (String splitWinningNumber : splitWinningNumbers) {
+            numericWinningNumbers.add(Integer.parseInt(splitWinningNumber));
+        }
+
+        return numericWinningNumbers;
+    }
+
+    private Map<Integer, Integer> getSequencedWinningNumbers(List<Integer> orderedSplitWinningNumbers) {
+        int sequenceNumber = 0;
+        Map<Integer, Integer> sequencedWinningNumbers = new HashMap<>();
+
+        for (int numericWinningNumber : orderedSplitWinningNumbers) {
+            sequencedWinningNumbers.put(numericWinningNumber, sequenceNumber++);
+        }
+
+        return sequencedWinningNumbers;
+    }
+}

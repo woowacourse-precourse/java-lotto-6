62d0bbaf6038f4b3d28c17576ae0018f27718bc9
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-04 19:32:48 +0900

    feat : 당첨 번호 입력에 대한 공백, 범위 예외 기능 추가

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -6,15 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import static lotto.LottoDetailConstant.*;
+
 public class LottoService {
-    private static final String BUY_MONEY_TYPE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 자연수를 입력해야 합니다.";
     private static final String BUY_MONEY_UNIT_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 1000원 단위의 금액을 입력해야 합니다.";
-    private static final String BUY_MONEY_RANGE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 0원보다 큰 금액을 입력해야 합니다.";
+    private static final String BUY_MONEY_RANGE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 0원 보다 큰 숫자를 입력해야 합니다.";
+    private static final String BUY_MONEY_TYPE_EXCEPTION_MESSAGE = "[ERROR] 구입 금액은 자연수를 입력해야 합니다.";
+    private static final String INPUT_NUMBER_BLANK_EXCEPTION_MESSAGE = "[ERROR] 공백 없이 숫자를 입력해야 합니다.";
+    private static final String INPUT_PLAYER_LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE = "[ERROR] 1 ~ 45의 자연수를 입력해야 합니다.";
     private static final Pattern NUMBER_MATCH = Pattern.compile("^[0-9]*$");
     private static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_START_NUMBER = 1;
-    private static final int LOTTO_END_NUMBER = 45;
-    private static final int LOTTO_NUMBER_STORE_SIZE = 6;
     private static final int ZERO = 0;
     private final List<List<Integer>> lottoNumberStore = new ArrayList<>();
     private final Player player = new Player();
@@ -40,9 +41,15 @@ public class LottoService {
     }
 
     public void validateBuyMoney(String inputMoney) {
+        validateInputNumberBlank(inputMoney);
         validateBuyMoneyType(inputMoney);
         validateBuyMoneyRange(inputMoney);
         validateBuyMoneyUnit(inputMoney);
+    }
+
+    public void validateWinningNumber(String inputWinningNumber) {
+        validateInputNumberBlank(inputWinningNumber);
+        validateInputPlayerLottoNumberRange(toInt(inputWinningNumber));
     }
 
     private void validateBuyMoneyType(String inputMoney) {
@@ -58,6 +65,16 @@ public class LottoService {
     private void validateBuyMoneyUnit(String inputMoney) {
         if (toInt(inputMoney) % LOTTO_PRICE != ZERO)
             throw new IllegalArgumentException(BUY_MONEY_UNIT_EXCEPTION_MESSAGE);
+    }
+
+    private void validateInputNumberBlank(String inputNumber) {
+        if (inputNumber.contains(" ") || inputNumber.equals(""))
+            throw new IllegalArgumentException(INPUT_NUMBER_BLANK_EXCEPTION_MESSAGE);
+    }
+
+    private void validateInputPlayerLottoNumberRange(int inputLottoNumber) {
+        if (!(inputLottoNumber >= LOTTO_START_NUMBER && inputLottoNumber <= LOTTO_END_NUMBER))
+            throw new IllegalArgumentException(INPUT_PLAYER_LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE);
     }
 
     private int toInt(String inputMoney) {

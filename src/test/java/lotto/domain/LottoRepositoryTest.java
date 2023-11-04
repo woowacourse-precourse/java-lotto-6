c4b9a134044cfc3a99d8895dc0d527921f5665f2
Lieslot <rlaxodud1999@gmail.com>
2023-11-04 16:21:41 +0900

    test: 구매한 로또가 잘 저장되는지 테스트

--- src/test/java/lotto/domain/LottoRepositoryTest.java
+++ src/test/java/lotto/domain/LottoRepositoryTest.java
@@ -1,9 +1,27 @@
 package lotto.domain;
 
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoRepositoryTest {
+
+    LottoRepository lottoRepository = LottoRepository.getInstance();
+    LottoFactory lottoFactory = new LottoFactory();
+    @DisplayName("로또가 잘 저장되었는지 확인")
+    @Test
+    void lottoSaveTest() {
+        //given
+        Lotto lotto = lottoFactory.createRandomLotto();
+        //when
+        lottoRepository.save(lotto);
+        List<Lotto> lottos = lottoRepository.showAllLottos();
+        //then
+
+        Assertions.assertThat(lottos).contains(lotto);
+
+    }
 
 
 }

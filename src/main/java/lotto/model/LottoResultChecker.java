b661d3dc84b01af866aea7510a5148ad2b61344d
D0ri123 <lsh81550@gmail.com>
2023-11-05 03:20:13 +0900

    feat(LottoResultChecker): 로또 비교 기능 구현 - 2차 구현
    로또와 당첨번호를 멤버변수로 가지고 있습니다.
    이 둘을 받아서 비교한 후에 enum 클래스인 Ranking 리스트로
    반환합니다.

---
+++ src/main/java/lotto/model/LottoResultChecker.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoResultChecker {
+    private final List<Lotto> issuedLotto;
+    private final WinningLotto winningLotto;
+
+    public LottoResultChecker(List<Lotto> issuedLotto, WinningLotto winningLotto) {
+        this.issuedLotto = issuedLotto;
+        this.winningLotto = winningLotto;
+    }
+
+    public List<Ranking> checkResult() {
+        List<Ranking> results = new ArrayList<>();
+        for(Lotto lotto : issuedLotto) {
+            List<Integer> issuedNumbers = lotto.getNumbers();
+            boolean isMatchedBonusNumber = issuedNumbers.contains(winningLotto.getBonusNumber());
+            issuedNumbers.retainAll(winningLotto.getNumbers());
+            Result result = new Result(issuedNumbers.size(), isMatchedBonusNumber);
+            Ranking rankingOfLotto = result.checkRanking();
+            //TODO: null이 들어왔을 때 처리할 방법도 고안해야 한다.
+            results.add(rankingOfLotto);
+        }
+        return results;
+    }
+
+}

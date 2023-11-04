90d23b0c144ff483b068460fb0b49b34c571e005
Isaac Han <isaachan233@gmail.com>
2023-11-04 12:58:48 +0900

    feat: UI 메소드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,27 @@
 package lotto.view;
 
+/* *
+    입력
+    - 로또 구입 금액을 입력 받는다. 구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.
+    - 당첨 번호를 입력 받는다. 번호는 쉼표(,)를 기준으로 구분한다.
+    - 보너스 번호를 입력 받는다.
+ */
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String lottoPurchaseAmountInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String winningLottoNumbersInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String winningLottoBonusNumberInput() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,29 @@
 package lotto.view;
 
+import lotto.domain.*;
+
+import java.util.List;
+
+/* *
+    - 발행한 로또 수량 및 번호를 출력한다. 로또 번호는 오름차순으로 정렬하여 보여준다.
+    - 당첨 내역을 출력한다.
+    - 수익률은 소수점 둘째 자리에서 반올림한다. (ex. 100.0%, 51.5%, 1,000,000.0%)
+ */
 public class OutputView {
+    public static void printNumberOfPurchasedLottoTickets(int numberOfTickets) {
+        System.out.printf("%d개를 구매했습니다.%n", numberOfTickets);
+    }
+
+    public static void printPurchasedLottoNumbers(List<Integer> lottoNumbers) {
+        // TODO : [8, 21, 23, 41, 42, 43] 형식으로 출
+    }
+
+    public static void printWinningResults(WinningResult winningResult) {
+        // TODO : 5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
+        //System.out.printf("%s개 일치%d(%s원) - %s개&n");
+    }
+
+    public static void printTotalReturn(double totalWinning) {
+        System.out.printf("총 수익률은 %s%입니다.%n", totalWinning);
+    }
 }

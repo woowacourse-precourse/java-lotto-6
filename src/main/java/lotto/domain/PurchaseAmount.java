d929fd75a5ec6133d75bfd855cfb8d354842cc33
elyudwo <dudqk9696@naver.com>
2023-11-04 18:57:18 +0900

    [Feat] : 로또 구입 금액 정수가 아닐 경우 예외 처리 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -7,4 +7,12 @@ public class PurchaseAmount {
             throw new IllegalArgumentException("[ERROR] : 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");
         }
     }
+
+    public void validateAmountNotNumber(String purchaseAmount) {
+        try {
+            Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] : 로또 구입 금액은 정수입니다.");
+        }
+    }
 }

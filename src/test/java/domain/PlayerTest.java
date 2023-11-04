b5e4a1a5ffdb5f66dfb6b7a2612131d0d63a3281
songsunkook <songsunkook@gmail.com>
2023-11-04 17:56:08 +0900

    test: Player 클래스 테스트 추가 및 수정

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -2,9 +2,22 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+
+    @Test
+    public void 구입_금액이_로또_표_값보다_적으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Player().buyLottoTickets(900))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 구입_금액이_나누어떨어지지_않으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Player().buyLottoTickets(1900))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     public void 로또_티켓을_주어진_개수만큼_구매하는가() {
@@ -14,9 +27,9 @@ class PlayerTest {
         Player player3 = new Player();
 
         //when
-        player1.buyLottoTickets(1);
-        player2.buyLottoTickets(2);
-        player3.buyLottoTickets(3);
+        player1.buyLottoTickets(1000);
+        player2.buyLottoTickets(2000);
+        player3.buyLottoTickets(3000);
 
         //then
         assertEquals(1, player1.getTicketCount());

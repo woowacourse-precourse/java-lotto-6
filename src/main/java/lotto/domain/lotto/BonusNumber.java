d758e3e2aff8515cbb0adccc88fe4dc471512d92
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 05:50:46 +0900

    feat(BonusNumber) : hasSameNumber 기능 구현

--- src/main/java/lotto/domain/lotto/BonusNumber.java
+++ src/main/java/lotto/domain/lotto/BonusNumber.java
@@ -2,20 +2,24 @@ package lotto.domain.lotto;
 
 public class BonusNumber {
 
-    private int bonusNumber;
+    private int number;
 
-    public BonusNumber(int bonusNumber) {
-        validate(bonusNumber);
-        this.bonusNumber = bonusNumber;
+    public BonusNumber(int number) {
+        validate(number);
+        this.number = number;
     }
 
-    private void validate(int bonusNumber) {
-        if(bonusNumber < 1 || bonusNumber > 45) {
+    private void validate(int number) {
+        if(number < 1 || number > 45) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이여야합니다.");
         }
     }
 
-    public int getBonusNumber() {
-        return bonusNumber;
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean hasSameNumber(int number) {
+        return this.number == number;
     }
 }

7f11f8e87420d8ad376af72f1d189ce2bef5546b
unknown <winternrabbit@gmail.com>
2023-11-04 23:23:17 +0900

    test: 로또 번호 오름차순 출력, 출력 행수 테스트하는 코드 작성

--- src/test/java/lotto/LottoGenerateTest.java
+++ src/test/java/lotto/LottoGenerateTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoGenerateTest {
-  
+    LottoGenerate lottoGenerate = new LottoGenerate();
+
+    @DisplayName("요청 횟수만큼 로또 번호 묶음 생성되는지 확인")
+    @Test
+    void checkSize() {
+        assertThat(lottoGenerate.getRandomLottoResult(3).size()).isEqualTo(3);
+    }
+
+    @DisplayName("로또 번호 오름차순으로 출력되는지 확인")
+    @Test
+    void checkNumberSortedASC() {
+        List<Integer> result = lottoGenerate.getRandomLottoResult(1).get(0);
+        List<Integer> compare = result.stream().sorted().toList();
+
+        assertThat(result).isEqualTo(compare);
+    }
 }

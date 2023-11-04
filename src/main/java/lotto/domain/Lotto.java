999729be32f2cf258c4bad77d533bfa314f4f5b5
dbstn1325 <dbstn1325@naver.com>
2023-11-05 01:34:17 +0900

    refactor(Lotto): 발행한 로또 번호가 6자리 숫자가 아니면 예외처리하는 기능 내 프로그램 확장성을 위해 변수명과 메서드명을 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Lotto {
     static final String LOTTO_START_ERROR_MESSAGE = "[ERROR] ";
-    static final String LOTTO_NUMBER_SIX_LENGTH_INVALID_MESSAGE = "발행한 로또 번호는 6자리 숫자여야 합니다.";
+    static final String LOTTO_NUMBER_LENGTH_INVALID_MESSAGE = "발행한 로또 번호는 6자리 숫자여야 합니다.";
     static final String LOTTO_NUMBER_RANGE_INVALID_MESSAGE = "발행한 로또 번호는 1~45의 숫자여야 합니다.";
     private static final int LOTTO_SIX_SIZE = 6;
     private final List<Integer> numbers;
@@ -15,13 +15,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateSixLengthNumber(numbers);
+        validateLengthNumber(numbers);
         validateRange(numbers);
     }
 
-    private static void validateSixLengthNumber(List<Integer> numbers) {
+    private static void validateLengthNumber(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIX_SIZE) {
-            throw new IllegalArgumentException(LOTTO_START_ERROR_MESSAGE + LOTTO_NUMBER_SIX_LENGTH_INVALID_MESSAGE);
+            throw new IllegalArgumentException(LOTTO_START_ERROR_MESSAGE + LOTTO_NUMBER_LENGTH_INVALID_MESSAGE);
         }
     }
 

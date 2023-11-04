f5dacee4bc31ac69360793ef3211d684048b1977
minjun7410 <minjun7410@gmail.com>
2023-11-04 22:02:33 +0900

    test(BonusNumberTest): 보너스 번호가 당첨 번호에 속하는지 예외 처리하는 기능 테스트 추가
    * 보너스 번호가 당첨 번호에 속하는지 예외 처리하는 테스트 추가

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BonusNumberTest {
@@ -18,5 +21,12 @@ public class BonusNumberTest {
     @ValueSource(strings = {"46", "0"})
     void 보너스번호_범위밖인값_예외처리_테스트(String input) {
         assertThatThrownBy(() -> new BonusNumber(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스번호_당첨번호_중복_예외처리_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber("2");
+        assertThat(lotto.contains(bonusNumber)).isTrue();
     }
 }

9fbe1369c65dd3f3c7da4b3d96b37ca7a9fce01e
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-04 15:54:00 +0900

    feat: 로또 숫자 생성 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,43 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("구입금액을 일력해 주세요.");
-        int money = Integer.parseInt(Console.readLine());
+        int lottoPrice = 1000;
+        int parseMoney = 0;
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = Console.readLine();
 
+        if (money.isBlank()) {
+            throw new IllegalStateException("[ERROR] 아무 것도 입력되지 않았습니다.");
+        }
 
+        try {
+            parseMoney = Integer.parseInt(money);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println("[ERROR] 숫자 형식이 아닙니다.");
+        }
+
+        if (money.charAt(0) == '0') {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 숫자입니다.");
+        }
+
+        if (parseMoney % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
+        }
+
+        int buyLottoCount = parseMoney / lottoPrice;
+
+    }
+
+    public static List<Integer> makeLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 }

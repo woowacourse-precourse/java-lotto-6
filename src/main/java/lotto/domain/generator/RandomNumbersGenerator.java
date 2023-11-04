00d39221c034eb91801d031da8781e37f438da40
JuSeong1130 <jsmoon1130@naver.com>
2023-11-05 06:22:42 +0900

    feat(RandomNumbersGenerator) : RandomNumbers 구하는 기능 구현

---
+++ src/main/java/lotto/domain/generator/RandomNumbersGenerator.java
@@ -0,0 +1,12 @@
+package lotto.domain.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumbersGenerator implements NumbersGenerator{
+
+    @Override
+    public List<Integer> create() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

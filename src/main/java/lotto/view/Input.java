c440bb87cc3484821b2f21b382a5a02ddcfaf078
wodnj5 <jw991211@gmail.com>
2023-11-04 22:17:50 +0900

    fix: 당첨 번호 입력 메소드 수정

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -3,6 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
 
@@ -21,7 +22,7 @@ public class Input {
             String[] input = Console.readLine().split(",");
             return Arrays.stream(input)
                     .map(Integer::parseInt)
-                    .toList();
+                    .collect(Collectors.toList());
         } catch(NumberFormatException e) {
             throw new IllegalArgumentException();
         }

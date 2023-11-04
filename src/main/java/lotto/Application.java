9ad077d7f74c1d28656aa001b4c0d548e762e190
3juhwan <13selfesteem91@naver.com>
2023-11-04 23:40:15 +0900

    feat: 당첨 번호 입력 로직

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     private final static String ASK_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private final static String ASK_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
 
@@ -11,6 +12,11 @@ public class Application {
 
     private static String askPurchaseAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT_MESSAGE);
+        return readLine();
+    }
+
+    private static String askWinningNumber() {
+        System.out.println(ASK_WINNING_NUMBER_MESSAGE);
         return readLine();
     }
 }

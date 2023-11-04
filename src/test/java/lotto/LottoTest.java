1ec1837ddb63f1c3263d814d967cf3ac097f9bcd
박찬건 <jg9870@naver.com>
2023-11-04 14:35:07 +0900

    test(lotto): 중복된 숫자를 입력하면 예외가 발생한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,10 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복된 숫자를 입력할 수 없습니다.");
     }
 
-    // 아래에 추가 테스트 작성 가능
+
 }

1b3d578fb6aa57d4d12f6836b76a7c33fc26cb4c
손지현_회원/결제개발팀 <hdg12jh@interpark.com>
2023-11-04 15:50:22 +0900

    feat 로또 Output 클래스 생성, 출력값 정리

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,39 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.code.Rank;
+
+public class Output {
+    public void enterPurchaseAmoutMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void checkPurchaseAmoutMessage(int purchaseAmount) {
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+    }
+
+    public void enterWinnigNumbersMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void enterBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void lottoResultTitleMessage() {
+        System.out.println("당첨 통계\n" + "---");
+    }
+
+    public void rateOfReturnMessage(int rateOfReturn) {
+        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
+    }
+
+    public void lottoResultMessage(List<Number> matchCount) {
+        for(Rank rank : Rank.values()) {
+            matchCount.stream()
+                            .forEach(count ->
+                                    System.out.println(rank.getMessage() + " - " + count + "개" )
+                            );
+        }
+    }
+}

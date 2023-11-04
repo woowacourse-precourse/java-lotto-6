ea1d1ba5103ee8a279fdfade815cec50a877f761
dong2ast <kdh01132006@naver.com>
2023-11-04 20:38:59 +0900

    ♻️ refactor: Lotto 객체 domain 패키지로 변경

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import lotto.Lotto;
+import lotto.domain.Lotto;
 
 public class LottoService {
 

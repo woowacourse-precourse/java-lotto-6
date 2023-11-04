45df6ba7aa535ea4c7dce604c9de554720381e77
yshong1998 <yshong1998@gmail.com>
2023-11-05 02:01:21 +0900

    Style(LottoValidator): 불필요한 주석 제거

--- src/main/java/lotto/util/LottoValidator.java
+++ src/main/java/lotto/util/LottoValidator.java
@@ -8,9 +8,6 @@ import static lotto.constant.ConstantValue.START_INCLUSIVE;
 public class LottoValidator {
 
     public static void validateLotto(List<Integer> lottoNumbers) {
-        // 여기까지 왔다면 로또의 숫자는 모두 Integer이기는 하다.
-        // 범위가 1 ~ 45인지
-        // 입력값 안에 중복 입력은 없는지
         validateLottoNumbersOutOfRange(lottoNumbers);
     }
 

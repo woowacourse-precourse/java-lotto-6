c83d57f1e3edec21d5c1a1b2abd196e5280ab37c
Lee Yeon Su <yslee0540@gmail.com>
2023-11-04 21:03:01 +0900

    feat(WinningCriteria): 보너스 번호 일치 여부 추가

--- src/main/java/lotto/domain/WinningCriteria.java
+++ src/main/java/lotto/domain/WinningCriteria.java
@@ -1,18 +1,21 @@
 package lotto.domain;
 
 public enum WinningCriteria {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30000000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30000000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    MISS(0, 0, false);
 
     private final int matchCount;
     private final int prize;
+    private final boolean matchBonus;
 
-    WinningCriteria(int matchCount, int prize) {
+    WinningCriteria(int matchCount, int prize, boolean matchBonus) {
         this.matchCount = matchCount;
         this.prize = prize;
+        this.matchBonus = matchBonus;
     }
 
     public int getMatchCount() {
@@ -21,5 +24,9 @@ public enum WinningCriteria {
 
     public int getPrize() {
         return prize;
+    }
+
+    public boolean getMatchBonus() {
+        return matchBonus;
     }
 }

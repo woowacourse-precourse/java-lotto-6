fc150bcbe33d6a935b3902943efb91006a780513
heejae <hjhjhjhj3770@naver.com>
2023-11-04 10:27:10 +0900

    Feat(WinningAmount): 1등~5등까지 당첨금 설정

---
+++ src/main/java/lotto/WinningAmount.java
@@ -0,0 +1,24 @@
+package lotto;
+
+public enum WinningAmount {
+//    - 1등: 6개 번호 일치 / 2,000,000,000원
+//    - 2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
+//    - 3등: 5개 번호 일치 / 1,500,000원
+//    - 4등: 4개 번호 일치 / 50,000원
+//    - 5등: 3개 번호 일치 / 5,000원
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000);
+
+    private final int prize;
+
+    WinningAmount(int prize) {
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}

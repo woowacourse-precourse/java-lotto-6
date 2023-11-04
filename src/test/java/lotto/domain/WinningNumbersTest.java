146326b2e41d816c54ad77733f25a7529839f5f5
kimwanyoung <dhks2869@gmail.com>
2023-11-05 00:57:08 +0900

    test : 로또 등수 반환 테스트

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -21,6 +21,22 @@ public class WinningNumbersTest {
 
     }
 
+    @ParameterizedTest(name = "당첨 번호 : {0}, 로또 번호 : {1}")
+    @MethodSource("generateLottoAndWinningNumber")
+    @DisplayName("맞은 로또번호의 개수에 따라 등수를 반환한다.")
+    void 등수_테스트(List<Integer> winning, List<Integer> lottoNumber, int bonus,
+                LottoResults expected) {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(winning, bonus);
+        Lotto lotto = new Lotto(lottoNumber);
+
+        //when
+        LottoResults actual = winningNumbers.calculateLottoResult(lotto);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
     static Stream<Arguments> generateDuplicateWinningNumbers() {
         return Stream.of(
                 Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6),
@@ -28,5 +44,15 @@ public class WinningNumbersTest {
                 Arguments.of(List.of(1, 2, 3, 4, 5, 6), 3),
                 Arguments.of(List.of(1, 2, 3, 4, 5, 6), 4)
         );
+    }
+
+    static Stream<Arguments> generateLottoAndWinningNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 12), 12,
+                        LottoResults.SECOND),
+                Arguments.of(List.of(1, 12, 13, 14, 15, 16), List.of(1, 13, 15, 16, 41, 45), 41,
+                        LottoResults.FOURTH),
+                Arguments.of(List.of(1, 31, 32, 33, 41, 45), List.of(1, 11, 12, 13, 21, 25), 42,
+                        LottoResults.FAIL));
     }
 }

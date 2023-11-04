92e26769ddd9d9d64109a8e897bff36da485f9dc
OhJongMin <jmoh7480@gmail.com>
2023-11-04 17:01:35 +0900

    test(lotto): 로또 번호 범위 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -21,4 +21,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호가 1~45 사이가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRangeNumber(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

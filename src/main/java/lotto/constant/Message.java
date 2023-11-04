25e6d234b3cbe91131427b1e7390c66c1f79a1c9
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-05 04:13:39 +0900

    feat:Message 상수 클래스 추가

---
+++ src/main/java/lotto/constant/Message.java
@@ -0,0 +1,26 @@
+package lotto.constant;
+
+public enum Message {
+    INPUT_MONEY("구입금액을 입력해 주세요."),
+    PURCHASE_LOTTO("%d개를 구매했습니다."),
+    INPUT_PRIZE_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    PRIZE_STATISTICS("당첨 통계\n---"),
+    FIFTH_PRIZE("3개 일치 (5,000원) - %d개"),
+    FOURTH_PRIZE("4개 일치 (50,000원) - %d개"),
+    THIRD_PRIZE("5개 일치 (1,500,000원) - %d개"),
+    SECOND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST_PRIZE("6개 일치 (2,000,000,000원) - %d개"),
+    RATE_OF_RETURN("총 수익률은 %.1f%%입니다.");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}

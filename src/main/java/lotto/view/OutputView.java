5ff68519817d5fb4cf4d9b280e3076c6e201847f
gundorit <packdev937@gmail.com>
2023-11-04 23:48:56 +0900

    refactor (OutputView) : 입력을 요구하는 메세지에 대한 네이밍 정책을 변경한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,12 +2,12 @@ package lotto.view;
 
 public class OutputView {
 
-    private static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.\n";
+    private static final String LOTTO_PURCHASE_PROMPT = "구입금액을 입력해 주세요.\n";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.\n";
 
-    public static void printLottoPurchase() {
-        System.out.print(LOTTO_PURCHASE_MESSAGE);
+    public static void printLottoPurchasePrompt() {
+        System.out.print(LOTTO_PURCHASE_PROMPT);
     }
 
     public static void printLottoCount() {

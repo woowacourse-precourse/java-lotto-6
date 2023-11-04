0670f2eafee1f2204cfc8786c53b0bfe3e0e31f0
WonyuChoi <phillip07290@naver.com>
2023-11-04 13:28:53 +0900

    feat: 컨트롤러에 validator도 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,15 @@
 package lotto;
 
 import controller.LottoController;
+import validator.LottoValidator;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        LottoController controller = new LottoController(new InputView(), new OutputView());
+        LottoController controller = new LottoController(new InputView(), new OutputView(),
+            new LottoValidator());
         controller.run();
     }
 }

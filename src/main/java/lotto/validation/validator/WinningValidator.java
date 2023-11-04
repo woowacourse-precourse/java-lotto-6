87fa7c814f8a4abd78bf3cf33446178572ef54bb
seonghoo1217 <seonghoo1217@naver.com>
2023-11-04 23:21:13 +0900

    refactor: 당첨번호 범위 유효성 검사 메서드 목적에 맞게 네이밍 변경

--- src/main/java/lotto/validation/validator/WinningValidator.java
+++ src/main/java/lotto/validation/validator/WinningValidator.java
@@ -6,10 +6,11 @@ import static lotto.property.LottoProperty.*;
 
 public class WinningValidator extends RootValidator{
 
-    public static void winnerIsCorrectRange(String winningNumber){
+    public static void winnerOrBonusIsCorrectRange(String winningNumber){
         int winningInteger = Integer.parseInt(winningNumber);
         if (winningInteger < LOTTO_START || winningInteger > LOTTO_END){
             throw new IllegalArgumentException(ErrorProperty.WINNING_IS_NOT_CORRECTLY_RANGE.toString());
         }
     }
+
 }

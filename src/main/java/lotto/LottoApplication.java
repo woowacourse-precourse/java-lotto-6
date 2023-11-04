e760adb367eb2a2c86a946120b0804112090d799
unknown <hakie2kim@gmail.com>
2023-11-04 21:04:06 +0900

    feat(src/main/java/lotto/): decide the number of lotto

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -3,7 +3,13 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoApplication {
-    Lotto drawLotto() {
+    private static int LOTTO_PRICE = 1000;
+
+    private Lotto drawLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(Lotto.START_NUMBER, Lotto.END_NUMBER, 6));
+    }
+
+    private int decideNumberOfLotto(int receivedAmount) {
+        return receivedAmount / LOTTO_PRICE;
     }
 }

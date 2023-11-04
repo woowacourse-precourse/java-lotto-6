c7cd3138255c1cb4bd731eed751402805de3a98a
Lee Yeon Su <yslee0540@gmail.com>
2023-11-05 00:30:49 +0900

    feat(LottoResult): 로또 결과 저장하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,8 +1,20 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class LottoResult {
+    private final HashMap<WinningCriteria, Integer> result;
+
+    public LottoResult(List<Lotto> purchaseLotto, Lotto winningNumbers, int bonusNumber) {
+        this.result = new HashMap<>();
+    }
+
+    public HashMap<WinningCriteria, Integer> getResult() {
+        return result;
+    }
+
     public int getMatchNumbers(Lotto purchaseLotto, Lotto winningLotto) {
         return (int)purchaseLotto.getNumbers().stream()
                 .filter(number -> winningLotto.getNumbers().stream().anyMatch(Predicate.isEqual(number)))

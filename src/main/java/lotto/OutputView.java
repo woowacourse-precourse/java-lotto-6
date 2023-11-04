c824defbcc49891b6561da0771f0e438654aa681
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-04 18:33:57 +0900

    feat : 당첨 번호 입력 문구 출력 기능 추가

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 public class OutputView {
     private static final String REQUEST_INPUT_BUY_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-
+    private static final String BUY_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String REQUEST_INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     public void printRequestInputBuyMoney() {
         System.out.println(REQUEST_INPUT_BUY_MONEY_MESSAGE);
     }
 
     public void printLottoTicket(int buyTicket) {
         printEmptyLine();
-        System.out.println(buyTicket + "개를 구매했습니다.");
+        System.out.println(buyTicket + BUY_COUNT_MESSAGE);
     }
 
     public void printLottoNumbers(List<List<Integer>> lottoNumberStore) {
@@ -20,6 +21,11 @@ public class OutputView {
             Collections.sort(lottoNumber);
             System.out.println(lottoNumber);
         }
+    }
+
+    public void printRequestInputWinningNumber() {
+        printEmptyLine();
+        System.out.println(REQUEST_INPUT_WINNING_NUMBER_MESSAGE);
     }
 
     public void printExceptionMessage(String message) {

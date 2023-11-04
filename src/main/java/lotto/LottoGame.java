4413a7090d7ff9fc454d407256b3ab479408ac40
wogus <ind07152@naver.com>
2023-11-04 23:27:32 +0900

    기능 목록 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,9 +1,12 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class LottoGame {
     private Lotto answer;
     private Lotto[] consumerAnswer;
     public void gameStart(){
-
+        int price=Integer.parseInt(readLine());
+        int num=price/1000;
     }
 }

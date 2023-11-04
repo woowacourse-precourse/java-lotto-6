c2717f64fd2eb6835f3656e91f357a91dd6055f8
Jung Yunho <a01049048063@gmail.com>
2023-11-04 21:36:07 +0900

    feat(Lotto): 중복된 숫자가 있을경우 예외처리

--- src/main/java/lotto/constant/ExceptionMessage.java
+++ src/main/java/lotto/constant/ExceptionMessage.java
@@ -4,7 +4,9 @@ public enum ExceptionMessage {
     ERROR("[ERROR] "),
     INVALID_NUMERIC_INPUT("숫자만 입력해주세요."),
     OUT_OF_NUMBER_RANGE("숫자가 0~9 범위를 넘어갔습니다."),
-    INVALID_DIVIDE_THOUSAND("1000으로 나누어 떨어지는 수가 아닙니다.");
+    INVALID_DIVIDE_THOUSAND("1000으로 나누어 떨어지는 수가 아닙니다."),
+    INVALID_LOTTO_LENGTH("로또 번호가 6자리가 아닙니다."),
+    HAS_DUPLICATE_NUMBER("중복된 숫자가 있습니다.");
 
     private final String message;
 
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,37 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.constant.ExceptionMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoLength(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private static boolean isLengthSix(List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+
+    private void validateLottoLength(List<Integer> numbers) {
+        if (isLengthSix(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_LENGTH.toValue());
         }
     }
 
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        if (isHaveDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.HAS_DUPLICATE_NUMBER.toValue());
+        }
+    }
 
+    private boolean isHaveDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> values = new HashSet<>(numbers);
+        return values.size() < numbers.size();
+    }
 }

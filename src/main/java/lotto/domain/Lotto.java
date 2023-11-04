b0e4609a8aaccc561225b5295b6fdcc726c87ceb
JongMinCh0i <wonderland314592@gmail.com>
2023-11-04 17:23:47 +0900

    refactor(Lotto): 로또 번호 중복을 검증하는 기능 내부에서 Set 을 구현하는 메서드 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,22 +9,24 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        validateUniqueElements(numbers);
+        validateUniqueElements(numbers, uniqueNumberSet(numbers));
+
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개입니다.");
         }
     }
 
-    private void validateUniqueElements(List<Integer> numbers) {
-        int userInputSize = numbers.size();
-        int userInputUniqueSize = new HashSet<>(numbers).size();
-
-        if (userInputSize != userInputUniqueSize) {
+    private void validateUniqueElements(List<Integer> numbers, HashSet<Integer> uniqueNumberSet) {
+        if (numbers.size() != uniqueNumberSet.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 입력 값은 중복을 허용하지 않습니다.");
         }
+    }
+
+    private HashSet<Integer> uniqueNumberSet(List<Integer> numbers) {
+        return new HashSet<>(numbers);
     }
 }

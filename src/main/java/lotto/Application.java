5029d3e78fd8bf6288de171b336ca5d805e894fb
3juhwan <13selfesteem91@naver.com>
2023-11-04 23:38:05 +0900

    feat: 구입 금액 입력 로직

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    private final static String ASK_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    private static String askPurchaseAmount() {
+        System.out.println(ASK_PURCHASE_AMOUNT_MESSAGE);
+        return readLine();
     }
 }

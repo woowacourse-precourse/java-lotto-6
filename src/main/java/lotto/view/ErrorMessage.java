cdeed791b92dfaab2a0816ae373c14272935f1c8
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-05 02:36:03 +0900

    feat: ErrorMessage enum 클래스 작성

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -2,15 +2,16 @@ package lotto.view;
 
 public enum ErrorMessage {
 
-    ERROR_MESSAGE_PREFIX("[ERROR] "),
     DUPLICATED_LOTTO("로또 번호에 중복이 있습니다"),
     INVALID_LOTTO_LENGTH("로또 번호의 개수가 맞지 않습니다"),
     INVALID_LOTTO_NUMBER_RANGE("로또 번호의 범위가 맞지 않습니다");
 
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+
     private final String message;
 
     public String getErrorMessage() {
-        return ERROR_MESSAGE_PREFIX.message + message;
+        return ERROR_MESSAGE_PREFIX + message;
     }
 
     ErrorMessage(String message) {

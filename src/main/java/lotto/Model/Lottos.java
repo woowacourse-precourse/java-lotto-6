92a1695d03d15f5851130846d3780fed58f5e77f
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-04 21:54:48 +0900

    feat(Lottos): 랜덤로또들을 추가할 기능 구현

---
+++ src/main/java/lotto/Model/Lottos.java
@@ -0,0 +1,16 @@
+package lotto.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos() {
+        lottos = new ArrayList<>();
+    }
+
+    public void add(Lotto lotto) {
+        lottos.add(lotto);
+    }
+}

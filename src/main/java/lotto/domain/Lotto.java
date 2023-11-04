4159abe2ff29eae943ca1d1451f6b701ada11d60
sotogito <108535526+sotogito@users.noreply.github.com>
2023-11-04 22:57:13 +0900

    feat : 6자리 로또 유효 검사

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int MIN = 1;
+    private static final int MAX = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers); //money는 input일때 단 한번만 객체르 생성하지만 lotto는 입력일때, 랜덤생성될때 여러 방면에서 객체가 생성되므로 생성자에서 유효검사를 하는게 좋다
@@ -13,7 +16,25 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        } else if (!isWithinRangeNum(numbers, MIN, MAX)) {
+            throw new IllegalArgumentException();
+        } else if (!isNotDuplicateNum(numbers)) {
+            throw new IllegalArgumentException();
+        } else if (numbers.contains("")) {
+            throw new IllegalArgumentException();
         }
+
+
+    }
+
+    private boolean isNotDuplicateNum(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> Collections.frequency(numbers, number) == 1);
+    }
+
+    private boolean isWithinRangeNum(List<Integer> numbers, int min, int max) {
+        return numbers.stream()
+                .allMatch(number -> number >= min && number <= max);
     }
 
     public List<Integer> getNumbers() {
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,7 +5,7 @@ import lotto.domain.Money;
 
 public class Input {
 
-    private final String MESSAGE_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
 
     public int getPurchaseAmount() {
         System.out.println(MESSAGE_INPUT_AMOUNT);

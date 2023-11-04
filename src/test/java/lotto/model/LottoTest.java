83a12e156aa4dd930ae3e5c0cb6a32daa5d1a9ef
jeondui <jeondui@gmail.com>
2023-11-05 00:35:35 +0900

    test(LottoTest): 로또 번호 중복 입력 시 예외 발생 테스트 기능

---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import lotto.exception.ErrorMessage;
+import lotto.model.Lotto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+
+    @DisplayName("로또 번호의 개수 6개 초과 시 예외 발생 테스트")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> Lotto.create("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
+    }
+
+    @DisplayName("로또 번호 중복 시 예외 발생 테스트")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> Lotto.create("1,2,3,4,5,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_DUPLICATE.get());
+    }
+
+
+}

4ae9d2ea7f239712778fa411e368c7e26521eeba
jongheonleee <qwefghnm1212@gmail.com>
2023-11-04 13:45:30 +0900

    [feat] Lottos 클래스에 getLottos() 메서드 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
@@ -8,5 +9,9 @@ public class Lottos {
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
     }
 }

b505f7105d98fbd006e9b91c5825384eca2c9d9d
doha <dohaahi@naver.com>
2023-11-04 23:44:08 +0900

    refactor (LottoMachine): Lotto를 LottoDto로 변환하는 기능 구현

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -1,8 +1,10 @@
 package lotto.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.dto.LottoDto;
 import lotto.domain.dto.LottosDto;
 import lotto.domain.dto.PurchaseAmountDto;
 
@@ -11,6 +13,17 @@ public class LottoMachine {
     public LottosDto issuedLottos(final PurchaseAmountDto purchaseAmountDto) {
         List<Lotto> lottos = new Lottos(purchaseAmountDto).getLottos();
 
-        return new LottosDto(lottos);
+        return new LottosDto(toLottoDto(lottos));
+    }
+
+    private List<LottoDto> toLottoDto(final List<Lotto> lottos) {
+        List<LottoDto> lottoStore = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            LottoDto lottoDto = new LottoDto(lotto.getNumbers());
+            lottoStore.add(lottoDto);
+        }
+
+        return lottoStore;
     }
 }

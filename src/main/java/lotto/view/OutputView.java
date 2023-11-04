855fca8b48fdebc3deab2612730472467b95d509
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-05 00:47:46 +0900

    feat : OutputView , InputView 추가
    입력을 담당하는 InputView,
    출력을 담당하는 OutputView

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class OutputView {
+    public static void printMessage(String message){
+        System.out.println(message);
+    }
+    public static void printErrorMessage(String errorMessage){
+        System.out.println(errorMessage);
+    }
+
+}

f689e1595776fababcc00c4a3eff1e635626ef5d
D0ri123 <lsh81550@gmail.com>
2023-11-05 03:28:39 +0900

    feat(Result): 비교 결과를 담고 있는 클래스 생성 - 2차 구현
    로또 한 장에 대한 비교 결과를 가지고 있습니다.
    동일한 번호 개수와 보너스 번호 포함 여부를 가지고, 랭킹을 판단합니다.

---
+++ src/main/java/lotto/model/Result.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Result {
+
+    private final int matchingNumber;
+    private final boolean isMatchedBonus;
+
+    public Result(int matchingNumber, boolean isMatchedBonus) {
+        this.matchingNumber = matchingNumber;
+        this.isMatchedBonus = isMatchedBonus;
+    }
+
+    public int getMatchingNumber() {
+        return matchingNumber;
+    }
+
+    public boolean getIsMatchedBonus() {
+        return isMatchedBonus;
+    }
+
+    public Ranking checkRanking() {
+        List<Ranking> allRankingCase = Ranking.getAllRankingCase();
+        for(Ranking ranking : allRankingCase) {
+            if((ranking.getMachedCount() == matchingNumber) && (ranking.getHasBonus().contains(isMatchedBonus))) {
+                return ranking;
+            }
+        }
+        //TODO: null이 아닌 다른 예외로 처리하기
+        return null;
+    }
+}

e8047c4064ec8be1321a175c6c702c0ee9e559a9
jsj <tkdlek4186@naver.com>
2023-11-04 16:51:20 +0900

    feat(Papers) : Lotto클래스에 있는 여러 Paper 저장관련 리스트 및 함수 분리

---
+++ src/main/java/lotto/Papers.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Papers {
+    public List<Paper> papers=new ArrayList<>();
+    public void createPapers(int n){
+        for(int i = 0; i<n;i++){
+            Paper paper=new Paper();
+            papers.add(paper);
+        }
+    }
+    public void creatCorrespondNum(List<Integer>numbers,int bonusNumber){
+        for(Paper i : papers){
+            i.checkCorrespondNum(numbers,bonusNumber);
+        }
+    }
+    public void creatWinMoney(){
+        for(Paper i : papers){
+            i.checkWinMoney();
+        }
+    }
+
+    public void printPapers(){
+        for(Paper i : papers){
+            i.printPaperNumbers();
+        }
+    }
+}

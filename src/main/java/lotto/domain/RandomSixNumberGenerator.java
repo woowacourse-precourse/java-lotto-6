527fab9e834d2c12948df2d2f165d2f452fae89b
Juhyorim <joohr1234@gmail.com>
2023-11-04 21:46:46 +0900

    feat: 로또의 6개 숫자 오름차순 정렬 기능 구현

--- src/main/java/lotto/domain/RandomSixNumberGenerator.java
+++ src/main/java/lotto/domain/RandomSixNumberGenerator.java
@@ -1,11 +1,18 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomSixNumberGenerator {
     public List<Integer> getSixNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        return getSortedList(numbers);
+    }
+
+    private List<Integer> getSortedList(List<Integer> numbers) {
+        Collections.sort(numbers);
 
         return numbers;
     }

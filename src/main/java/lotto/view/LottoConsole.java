e133d216082c98db55786ac9ac5c6b27b0354bae
suddiyo <suddiyo@naver.com>
2023-11-04 13:37:13 +0900

    feat: 출력 관련 상수 LottoConstants 작성

---
+++ src/main/java/lotto/view/LottoConsole.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class LottoConsole {
+    public static void print(String console) {
+        System.out.print(console);
+    }
+}
---
+++ src/main/java/lotto/view/LottoConstants.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+public class LottoConstants {
+    public static final String MSG_PURCHASE_MONEY_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String MSG_PURCHASE_COUNT_PROMPT = "%d개를 구매했습니다.";
+    public static final String MSG_LOTTO_PREFIX = "[";
+    public static final String MSG_LOTTO_SUFFIX = "]";
+    public static final String MSG_LOTTO_DELIMITER = ", ";
+    public static final String MSG_WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String MSG_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    public static final String MSG_WINNING_STATUS_HEADER = "당첨 통계";
+    public static final String MSG_WINNING_STATUS_DIVIDER = "---";
+    public static final String FORMAT_WINNING_STATUS = "%d개 일치 (%d원) - %d개";
+    public static final String FORMAT_RATE_OF_RETURN = "총 수익률은 %d%%입니다.";
+}

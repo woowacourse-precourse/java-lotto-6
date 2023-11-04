311e74ce47d9397ef4c2ce98c2b17743b8efb8c6
Sunhee Kim <sunni372@icloud.com>
2023-11-05 01:23:06 +0900

    feat: 로또 번호 생성 기능 구현
    1~45까지의 중복되지 않는 숫자 6개를 로또 번호로 생성합니다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGame {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_OF_NUMBERS = 6;
 
     private final List<Lotto> lottos = new ArrayList<>();
 
@@ -20,8 +24,12 @@ public class LottoGame {
 
     private void makeLottos(int lottosNumber) {
         for (int i = 0; i < lottosNumber; i++) {
-            addLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+            addLotto(new Lotto(makeRandomLottoNumbers()));
         }
+    }
+
+    private List<Integer> makeRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_OF_NUMBERS);
     }
 
     private void addLotto(Lotto lotto) {

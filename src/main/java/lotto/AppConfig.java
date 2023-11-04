2b0eec7dd5f23e2af1783059534bf417824b87e0
02ggang9 <02ggang9@gmail.com>
2023-11-04 20:22:32 +0900

    feat(AppConfig): 객체 주입 책임을 수행하도록 변경

--- src/main/java/lotto/AppConfig.java
+++ src/main/java/lotto/AppConfig.java
@@ -1,11 +1,31 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.repository.LottoRepository;
+import lotto.service.LottoService;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
 
 public class AppConfig {
 
     public static LottoController getLottoController() {
-        return new LottoController();
+        return new LottoController(getLottoInputView(), getLottoOutputView(), getLottoService());
+    }
+
+    private static LottoService getLottoService() {
+        return new LottoService(getLottoRepository());
+    }
+
+    private static LottoRepository getLottoRepository() {
+        return new LottoRepository();
+    }
+
+    private static LottoOutputView getLottoOutputView() {
+        return new LottoOutputView();
+    }
+
+    private static LottoInputView getLottoInputView() {
+        return new LottoInputView();
     }
 
 }

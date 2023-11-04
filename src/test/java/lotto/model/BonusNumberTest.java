4c785af9bea8e04dbe694aa96cddb79c71988921
devholic22 <hyunjoon.tech@gmail.com>
2023-11-04 23:10:18 +0900

    test: BonusNumber null, 빈 문자, 공백 포함 문자에 대해서도 테스트
    BonusNumber 생성 시 null, 빈 문자, 공백 포함 문자에 대해서도 예외가 발생하는지 테스트

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -8,6 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BonusNumberTest {
 
@@ -23,12 +26,11 @@ public class BonusNumberTest {
     @DisplayName("보너스 번호 예외 테스트")
     class BonusNumberExceptionTest {
 
-        @Test
-        @DisplayName("숫자가 아닌 값이면 예외가 발생한다.")
-        void notNumberInputExceptionTest() {
-            // given
-            String numberInput = "abc";
-
+        @ParameterizedTest
+        @DisplayName("문자, null, 빈 문자, 공백 포함 문자가 있으면 안 된다.")
+        @NullAndEmptySource
+        @ValueSource(strings = {"abc", "   5  00 "})
+        void notNumberInputExceptionTest(final String numberInput) {
             // when & then
             assertThatThrownBy(() -> BonusNumber.from(numberInput)).isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(NUMBER_FORMAT_EXCEPTION.getMessage());

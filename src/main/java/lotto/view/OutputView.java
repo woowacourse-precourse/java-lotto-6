d634263045236fe08b9ddefc31625913cef83829
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 02:26:21 +0900

    feat : 출력해야 하는 메시지에 대해 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,5 +5,19 @@ public class OutputView {
     public void printRequestInputPurchaseAmountMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-    
+
+    public void printTicketQuantityMessage(Integer quantity) {
+        System.out.println();
+        String message = String.format("%s개를 구매했습니다.", quantity);
+        System.out.println(message);
+    }
+
+    public void printRequestInputWinningNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printRequestInputBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
 }

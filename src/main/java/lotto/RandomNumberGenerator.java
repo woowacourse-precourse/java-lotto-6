25a9bb6f40a8ec04bb0cc508b8ca0b4667f9c7e5
zjacom <eephus625@icloud.com>
2023-11-04 22:46:30 +0900

    feat(lotto): RandomNumberGenerator 수정
    6개의 번호를 랜덤으로 추출하고 오름차순으로 변경하는 기능 추가

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -1,10 +1,13 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator {
     public List<Integer> createNewRandomNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(randomNumber);
+        return randomNumber;
     }
 }

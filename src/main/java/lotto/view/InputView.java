68813febd317afec9c06f679ece7af26d165e7c6
coenffl <>
2023-11-04 22:13:55 +0900

    Feat: implementation of inputView

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import lotto.constant.message.InputMessage;
+import lotto.util.InputUtil;
+
+public class InputView {
+    private final InputUtil inputUtil = new InputUtil();
+//    public String getInputPurchase() {
+//        return getInputWithMessage(InputMessage.INPUT_PURCHASE.getMessage());
+//    }
+//
+//    public String getInputWinningNumber() {
+//        return getInputWithMessage(InputMessage.INPUT_NUMBER.getMessage());
+//    }
+//
+//    public String getInputBonusNumber(){
+//        return getInputWithMessage(InputMessage.INPUT_BONUS.getMessage());
+//    }
+
+    private String getInputWithMessage(String message){
+        System.out.println(message);
+        return inputUtil.getInput();
+    }
+}

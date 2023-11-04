f93ba452f9dbad08223200ecc93509ce2d6ef70c
kmw2378 <rlarla677@gmail.com>
2023-11-04 16:46:41 +0900

    feat(lotto): GameConfiguration 에 로또 한 개당 금액 상수 필드 추가 로또 한 개당 가격(=1000) 상수 필드를 추가하였습니다.

--- src/main/java/lotto/configuration/GameConfiguration.java
+++ src/main/java/lotto/configuration/GameConfiguration.java
@@ -4,5 +4,6 @@ public final class GameConfiguration {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOTTO_NUMBER_SIZE = 6;
+    public static final int LOTTO_PRICE_PER_UNIT = 1000;
     private GameConfiguration() {}
 }

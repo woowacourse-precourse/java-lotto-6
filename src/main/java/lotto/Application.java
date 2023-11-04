001ce4c47b9c92b6c1a2102ecd23b8d8dad6cb14
jinsoo Kim <jinsoo335@naver.com>
2023-11-04 23:18:12 +0900

    feat: 로또 번호 생성 기능, 당첨 번호 입력 기능 추가
    로또 번호 생성의 경우 구매 금액을 1000으로 나눈 몫 만큼의 로또 번호를 생성한다.
    
    당첨 번호 입력 기능의 경우 중복 없이 1 ~ 45의 숫자를 입력 받도록 한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,47 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            int buyMoney = Lotto.inputBuyMoney();
+            List<Lotto> lottoList = new ArrayList<>();
+
+            createLottos(buyMoney, lottoList);
+            printLottos(lottoList);
+
+            List<Integer> correctNumbers = Lotto.inputCorrectNumbers();
+            Integer bonusNumber = Lotto.inputBonusNumber(correctNumbers);
+
+
+
+        } catch (NumberFormatException ex) {
+            System.out.println("[ERROR] 입력은 숫자로 입력되어야 합니다.");
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void createLottos(int buyMoney, List<Lotto> lottoList) {
+        for(int i = 0; i < buyMoney / 1000; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            Lotto lotto = new Lotto(numbers);
+
+            lottoList.add(lotto);
+        }
+    }
+
+
+    private static void printLottos(List<Lotto> lottoList) {
+        for (Lotto lotto : lottoList) {
+            lotto.printLotto();
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -19,26 +20,86 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    public int inputBuyMoney(){
+    
+    // 잘못 된 입력이 들어왔을 경우 -1 반환
+    public static int inputBuyMoney() {
+        System.out.println("구입 금액을 입력해주세요.");
 
         String input = Console.readLine();
-        int money;
-
-        try {
-            money = Integer.parseInt(input);
-
-            if(money % 1000 != 0){
-                throw new IllegalArgumentException();
-            }
-
-        } catch (NumberFormatException ex){
-            money = -1;
-            System.out.println("[ERROR] 구매 금액은 숫자로 입력되어야 합니다.");
-        } catch (IllegalArgumentException ex){
-            money = -1;
-            System.out.println("[ERROR] 구매 금액은 1000으로 나누어 떨어져야 합니다.");
-        }
-
+        int money = validateBuyMoney(input);
         return money;
     }
+
+    private static int validateBuyMoney(String input) {
+        int money = -1;
+
+        money = Integer.parseInt(input);
+
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000으로 나누어 떨어져야 합니다.");
+        }
+        return money;
+    }
+
+
+    public static List<Integer> inputCorrectNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        String input = Console.readLine();
+
+        for(String str : input.split(",")){
+            numbers.add(Integer.parseInt(str));
+        }
+
+        validateCorrectNumbers(numbers);
+
+        return numbers;
+    }
+
+    private static void validateCorrectNumbers(List<Integer> numbers) {
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개 입력해야 합니다.");
+        }
+
+        for (Integer number : numbers) {
+            if(number > 45 || number < 1){
+                throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1 ~ 45 사이여야 합니다.");
+            }
+        }
+
+        Set<Integer> validSet = new HashSet<>(numbers);
+        if(validSet.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 중복 없이 6개 입력해야 합니다.");
+        }
+    }
+
+    public void printLotto(){
+        System.out.println(this.numbers);
+    }
+
+    public static Integer inputBonusNumber(List<Integer> correctNumbers) {
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        Integer bonusNumber = Integer.parseInt(input);
+
+        validateBonusNumber(correctNumbers, bonusNumber);
+
+        return bonusNumber;
+    }
+
+    private static void validateBonusNumber(List<Integer> correctNumbers, Integer bonusNumber) {
+        if(bonusNumber < 1 || bonusNumber > 45){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 숫자를 입력해야 합니다.");
+        }
+
+        if(correctNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
+        }
+    }
+
+
 }

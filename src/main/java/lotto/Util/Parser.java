372e453a17269b95b57ec1744ed70b127f8162f7
Firedrago95 <yonghwa95@naver.com>
2023-11-04 10:46:45 +0900

    fear(Parser) : 입력번호 변환 기능 구현

---
+++ src/main/java/lotto/Util/Parser.java
@@ -0,0 +1,59 @@
+package lotto.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    public static final String NULL_EMPTY_MESSAGE = "[ERROR] 값을 입력해주세요";
+    public static final String INVALID_FORM_MESSAGE = "[ERROR] 쉼표로 구분된 6개의 숫자를 입력해주세요";
+    public static final String INVALID_RANGE_MESSAGE = "[ERROR] 1~45 사이의 숫자를 입력해주세요";
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
+    public static List<Integer> convertStringToNumbers(String winningNumber)
+        throws IllegalArgumentException {
+        validateWinningNum(winningNumber);
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] numbers = winningNumber.split(",");
+        Arrays.stream(numbers)
+            .forEach(number -> winningNumbers.add(Integer.parseInt(number)));
+        return winningNumbers;
+    }
+
+    private static void validateWinningNum(String winningNumber) throws IllegalArgumentException {
+        validateNullOrEmpty(winningNumber);
+        validateForm(winningNumber);
+        validateNumberRange(winningNumber);
+    }
+
+    private static void validateNumberRange(String winningNumber) throws IllegalArgumentException {
+        String[] numbers = winningNumber.split(",");
+        int[] winNumbers = Arrays.stream(numbers)
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        for (int number : winNumbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
+            }
+        }
+    }
+
+    private static void validateForm(String winningNumber) throws IllegalArgumentException {
+        String regex = "\\d+,\\d+,\\d+,\\d+,\\d+,\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(winningNumber);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INVALID_FORM_MESSAGE);
+        }
+    }
+
+    private static void validateNullOrEmpty(String winningNumber) throws IllegalArgumentException {
+        if (winningNumber == null || winningNumber.equals("")) {
+            throw new IllegalArgumentException(NULL_EMPTY_MESSAGE);
+        }
+    }
+}

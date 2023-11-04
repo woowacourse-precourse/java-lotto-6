dd1ab9549a10aacadb1f3ef1ce5347d8cff2b349
zzawang <hobin0116@naver.com>
2023-11-05 05:21:24 +0900

    feat: 생성한 로또 번호 출력 기능 추가
    - lottoList 목록을 반복하면서 각 Lotto 객체에 대해 아래 작업을 수행한다.
        - Lotto 객체로부터 로또 번호 리스트를 가져온다.
        - 로또 번호 리스트를 오름차순으로 정렬한다.
        - 정렬된 로또 번호 리스트를 문자열로 변환하고, 각 번호 사이를 ", "로 구분한 문자열을 "["과 "]" 사이에 출력한다.

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,6 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,12 +9,16 @@ import lotto.Lotto;
 
 public class Output {
     public static final String PURCHASE_AMOUNT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String LOTTO_NUMBERS_SEPARATOR = "[%s]";
+    private static final String LOTTO_NUMBERS_JOIN_SEPARATOR = ", ";
 
     public static List<Lotto> getLottoNumbers(int purchaseAmount) {
         int lottoAmount = purchaseAmount / 1000;
         System.out.printf(PURCHASE_AMOUNT_MESSAGE + "%n", lottoAmount);
 
         List<Lotto> lottoList = generateLottoNumbers(lottoAmount);
+
+        printLottoNumbers(lottoList);
 
         return lottoList;
     }
@@ -22,5 +27,20 @@ public class Output {
         return IntStream.range(0, lottoAmount)
                 .mapToObj(i -> new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
                 .collect(Collectors.toList());
+    }
+
+    private static void printLottoNumbers(List<Lotto> lottoList) {
+        lottoList.forEach(lotto -> {
+            List<Integer> numbers = lotto.getNumbers();
+            String lottoNumbers = formatLottoNumbers(numbers);
+            System.out.printf(LOTTO_NUMBERS_SEPARATOR + "%n", lottoNumbers);
+        });
+    }
+
+    private static String formatLottoNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(LOTTO_NUMBERS_JOIN_SEPARATOR));
     }
 }

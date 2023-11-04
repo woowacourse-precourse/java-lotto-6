72aea2f7870a59c97462f9669e66d06b9bc7f8eb
msung99 <msung6924@naver.com>
2023-11-04 15:44:24 +0900

    feat: 구입 금액 1,000원 으로 나누어 떨어지는지 검증하는 기능 구현

--- src/main/java/lotto/domain/Budget.java
+++ src/main/java/lotto/domain/Budget.java
@@ -2,7 +2,8 @@ package lotto.domain;
 
 public class Budget {
     private final int budget;
-    private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "";
+    private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 로또 금액은 숫자만 입력할 수 있습니다.";
+    private static final String DIVIDE_EXCEPTION_MESSAGE = "[ERROR] 로또 구입 최소 금액은 1000원 입니다.";
 
     private Budget(int budget){
         this.budget = budget;
@@ -11,6 +12,7 @@ public class Budget {
     public static Budget from(String inputBudget){
         validateNumeric(inputBudget);
         int budget = Integer.parseInt(inputBudget);
+        validateDivideAble(budget);
         return new Budget(budget);
     }
 
@@ -19,6 +21,12 @@ public class Budget {
             Integer.parseInt(inputBudget);
         } catch (NumberFormatException e){
             throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validateDivideAble(int budget){
+        if(budget % 1000 != 0){
+            throw new IllegalArgumentException(DIVIDE_EXCEPTION_MESSAGE);
         }
     }
 }

b4af4321eaf05714225c0cf7e326c0d61508ef06
koomchang <leahpar0401@gmail.com>
2023-11-04 19:32:44 +0900

    feat: InputView에서 ã구입금액 입력값 ê 관련 validator를 생성자를 통해 주입

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,11 +3,19 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import java.util.stream.Stream;
+import lotto.validator.BasicValidator;
 
 public class InputView {
 
+    private final BasicValidator<String> moneyInputValidator;
+
+    public InputView(BasicValidator<String> moneyInputValidator) {
+        this.moneyInputValidator = moneyInputValidator;
+    }
+
     public int inputMoney() {
         String input = Console.readLine();
+        moneyInputValidator.validate(input);
         return Integer.parseInt(input);
     }
 

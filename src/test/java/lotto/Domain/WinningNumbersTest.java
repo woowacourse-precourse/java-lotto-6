691291a9478ba8498f90e1e90c334ac025cd75aa
Firedrago95 <yonghwa95@naver.com>
2023-11-04 16:49:04 +0900

    test(WinnigNumbersTest) : 등수별 개수 구하기 테스트 코드 작성

--- src/test/java/lotto/Domain/WinningNumbersTest.java
+++ src/test/java/lotto/Domain/WinningNumbersTest.java
@@ -1,8 +1,11 @@
 package lotto.Domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +17,7 @@ public class WinningNumbersTest {
         List<Integer> invalidList = List.of(1, 2, 3, 4, 5, 6);
         String bonusNumber = "6";
 
-        assertThatThrownBy(() -> new WinnigNumbers(invalidList,bonusNumber)).isInstanceOf(
+        assertThatThrownBy(() -> new WinningNumbers(invalidList, bonusNumber)).isInstanceOf(
             IllegalArgumentException.class);
     }
 
@@ -24,7 +27,30 @@ public class WinningNumbersTest {
         List<Integer> invalidList = List.of(1, 2, 3, 4, 5, 6);
         String bonusNumber = "46";
 
-        assertThatThrownBy(() -> new WinnigNumbers(invalidList,bonusNumber)).isInstanceOf(
+        assertThatThrownBy(() -> new WinningNumbers(invalidList, bonusNumber)).isInstanceOf(
             IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 등수별 개수 구하는 기능 테스트")
+    public void getRankCountTest() {
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), "7");
+        List<Lotto> lottoTicket = List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+            new Lotto(List.of(1, 2, 3, 4, 5, 7)),
+            new Lotto(List.of(1, 2, 3, 4, 5, 15)),
+            new Lotto(List.of(1, 2, 3, 4, 15, 25)),
+            new Lotto(List.of(1, 2, 3, 15, 25, 35)),
+            new Lotto(List.of(1, 2, 15, 25, 35, 45))
+        );
+
+        Map<Rank,Integer> rankCount = winningNumbers.getRankCount(lottoTicket);
+
+        assertEquals(rankCount.get(Rank.FIRST),1);
+        assertEquals(rankCount.get(Rank.SECOND),1);
+        assertEquals(rankCount.get(Rank.THIRD),1);
+        assertEquals(rankCount.get(Rank.FOURTH),1);
+        assertEquals(rankCount.get(Rank.FIFTH),1);
+        assertEquals(rankCount.get(Rank.LOSE),1);
     }
 }

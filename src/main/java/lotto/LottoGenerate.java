31a222ea219a139e4c49f6ee9f35274a023bea04
unknown <winternrabbit@gmail.com>
2023-11-04 23:22:09 +0900

    feat: 구매한 로또 매수만큼 로또 번호 출력하는 기능 구현

--- src/main/java/lotto/LottoGenerate.java
+++ src/main/java/lotto/LottoGenerate.java
@@ -1,2 +1,25 @@
-package lotto;public class LottoGenerate {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class LottoGenerate {
+    public HashMap<Integer, List<Integer>> getRandomLottoResult(int lottoTicket) {
+        HashMap<Integer, List<Integer>> lottoGenerateResult = new HashMap<>();
+
+        for (int i = 0; i < lottoTicket; i++) {
+            lottoGenerateResult.put(i, createRandomLotto());
+        }
+
+        return lottoGenerateResult;
+    }
+
+    private List<Integer> createRandomLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumbers);
+
+        return lottoNumbers;
+    }
 }

f564555787cdb1b7b988cfa602047ea528432b57
mk3058 <kmk3058@icloud.com>
2023-11-04 15:54:28 +0900

    feat(OutputView): OutputView 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.message.OutputMessage;
+
+public class OutputView {
+
+    public static void printIssueResults(List<Lotto> lottos) {
+        OutputMessage.ISSUED_LOTTO_COUNT.print(String.valueOf(lottos.size()));
+        lottos.forEach(Lotto::printNumbers);
+    }
+}

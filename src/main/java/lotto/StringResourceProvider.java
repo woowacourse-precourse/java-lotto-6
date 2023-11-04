1793cd817b2f3e8dff18c25214acc661886fc10e
kimdeokyoung <dudejrss@naver.com>
2023-11-04 14:42:32 +0900

    test: 로또 구입금액을 입력 받는 기능에 대한 테스트 코드 작성 (#1)
    * test: "구입금액을 입력해 주세요."를 출력한다
    * test: 로또 구입금액이 정수가 아닐 시 IllegalArgumentException을 발생시킨다
    * test: 로또 구입금액이 정수가 아닐 시 "[ERROR]"로 시작하는 에러 메시지 내용을 출력한다
    * test: 로또 구입금액이 정수가 아닐 시 예외 발생 후 종료되지 않는다
    * test: 로또 구입금액이 음수일 때 IllegalArgumentException을 발생시킨다
    * test: 로또 구입금액이 음수 일 때 "[ERROR]"로 시작하는 에러 메시지 내용을 출력한다
    * test: 로또 구입금액이 음수 일 때 예외 발생 후 종료되지 않는다
    * test: 로또 구입금액이 1000원으로 나누어 떨어지지 않는 경우, IllegalArgumentException을 발생시킨다
    * test: 로또 구입금액이 1000원으로 나누어 떨어지지 않는 경우, "[ERROR]"로 시작하는 에러 메시지 내용을 출력한다
    * test: 로또 구입금액이 1000원으로 나누어 떨어지지 않는 경우, 예외 발생 후 종료되지 않는다
    * docs: 새로운 기능목록 추가

---
+++ src/main/java/lotto/StringResourceProvider.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public class StringResourceProvider {
+
+    // TEXT
+    public static final String INPUT_PURCHASE_AMOUNT_TEXT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBERS_TEXT = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_TEXT = "보너스 번호를 입력해 주세요.";
+    public static final String OUTPUT_LOTTERY_RESULT = "당첨 통계";
+
+    // ERROR
+    public static final String ERROR_MESSAGE_TAG = "[ERROR]";
+    public static final String PURCHASE_AMOUNT_CANNOT_CONVERT_TO_INTEGER_TEXT = "구입금액이 정수가 아닙니다.";
+    public static final String PURCHASE_AMOUNT_CANNOT_BE_NEGATIVE_INTEGER_TEXT = "구입금액은 양수야 합니다.";
+    public static final String PURCHASE_AMOUNT_MUST_BE_DIVIDE_BY_PURCHASE_AMOUNT = "구입금액은 1000으로 나눌 수 있어야 합니다.";
+}
---
+++ src/test/java/lotto/application/input/Application_PurchaseAmountTest.java
@@ -0,0 +1,143 @@
+package lotto.application.input;
+
+import static lotto.StringResourceProvider.ERROR_MESSAGE_TAG;
+import static lotto.StringResourceProvider.INPUT_BONUS_NUMBER_TEXT;
+import static lotto.StringResourceProvider.INPUT_PURCHASE_AMOUNT_TEXT;
+import static lotto.StringResourceProvider.INPUT_WINNING_NUMBERS_TEXT;
+import static lotto.StringResourceProvider.OUTPUT_LOTTERY_RESULT;
+import static lotto.StringResourceProvider.PURCHASE_AMOUNT_CANNOT_BE_NEGATIVE_INTEGER_TEXT;
+import static lotto.StringResourceProvider.PURCHASE_AMOUNT_CANNOT_CONVERT_TO_INTEGER_TEXT;
+import static lotto.StringResourceProvider.PURCHASE_AMOUNT_MUST_BE_DIVIDE_BY_PURCHASE_AMOUNT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import lotto.MyApplicationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("[Application] 구입금액에 대한 테스트")
+public class Application_PurchaseAmountTest extends MyApplicationTest {
+    private static final Pattern ErrorMessagePattern = Pattern.compile(
+            String.format("^\\%s", ERROR_MESSAGE_TAG));
+
+    @Test
+    void 구입금액_입력_문구를_출력한다() {
+        run("1000", "123456", "7");
+
+        assertThat(outputs())
+                .contains(INPUT_PURCHASE_AMOUNT_TEXT);
+    }
+
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 IllegalArgumentException이 발생한다")
+    @MethodSource("getStringSourceCannotConvertToNumber")
+    void 로또_구입금액이_정수가_아닐_시_IllegalArgumentException을_발생시킨다(String invalidInput) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> {
+                    runWithInvalidPurchaseAmount(invalidInput);
+                });
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 에러메시지 내용을 출력한다")
+    @MethodSource("getStringSourceCannotConvertToNumber")
+    void 로또_구입금액이_정수가_아닐_시_에러_메시지_내용을_출력한다(String invalidInput) {
+        runWithInvalidPurchaseAmount(invalidInput);
+
+        assertThatPrintErrorMessageWith(PURCHASE_AMOUNT_CANNOT_CONVERT_TO_INTEGER_TEXT);
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 어플리케이션이 예외 발생 후 종료되지 않는다")
+    @MethodSource("getStringSourceCannotConvertToNumber")
+    void 로또_구입금액이_정수가_아닐_시_예외_발생_후_종료되지_않는다(String invalidInput) {
+        runWithInvalidPurchaseAmount(invalidInput);
+
+        assertThatApplicationNotExists();
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 IllegalArgumentException이 발생한다")
+    @MethodSource("getStringSourceIsNegativeInteger")
+    void 로또_구입금액이_음수일_때_IllegalArgumentException을_발생시킨다(String invalidInput) {
+        assertThatIllegalArgumentException().
+                isThrownBy(() -> {
+                    runWithInvalidPurchaseAmount(invalidInput);
+                });
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 에러메시지 내용을 출력한다")
+    @MethodSource("getStringSourceIsNegativeInteger")
+    void 로또_구입금액이_음수일_때_에러_메시지_내용을_출력한다(String invalidInput) {
+        runWithInvalidPurchaseAmount(invalidInput);
+
+        assertThatPrintErrorMessageWith(PURCHASE_AMOUNT_CANNOT_BE_NEGATIVE_INTEGER_TEXT);
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 어플리케이션이 예외 발생 후 종료되지 않는다")
+    @MethodSource("getStringSourceIsNegativeInteger")
+    void 로또_구입금액이_음수일_때_예외_발생_후_종료되지_않는다(String invalidInput) {
+        runWithInvalidPurchaseAmount(invalidInput);
+
+        assertThatApplicationNotExists();
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 IllegalArgumentException이 발생한다")
+    @MethodSource("getStringSourceCannotDivideByUnit")
+    void 로또_구입금액이_1000원으로_나누어_떨어지지_않는_경우_IllegalArgumentException을_발생시킨다(String invalidInput) {
+        assertThatIllegalArgumentException().
+                isThrownBy(() -> {
+                    runWithInvalidPurchaseAmount(invalidInput);
+                });
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 에러메시지 내용을 출력한다")
+    @MethodSource("getStringSourceCannotDivideByUnit")
+    void 로또_구입금액이_1000원으로_나누어_떨어지지_않는_경우_에러_메시지_내용을_출력한다(String invalidInput) {
+        runWithInvalidPurchaseAmount(invalidInput);
+
+        assertThatPrintErrorMessageWith(PURCHASE_AMOUNT_MUST_BE_DIVIDE_BY_PURCHASE_AMOUNT);
+    }
+
+    @ParameterizedTest(name = "''{0}''을 입력했을 시 어플리케이션이 예외 발생 후 종료되지 않는다")
+    @MethodSource("getStringSourceCannotDivideByUnit")
+    void 로또_구입금액이_1000원으로_나누어_떨어지지_않는_경우_예외_발생_후_종료되지_않는다(String invalidInput) {
+        runWithInvalidPurchaseAmount(invalidInput);
+
+        assertThatApplicationNotExists();
+    }
+
+    private static Stream<String> getStringSourceCannotConvertToNumber() {
+        return Stream.of("abcd", "123z1", "123 456", "123,456", "123456.0", "");
+    }
+
+    private static Stream<String> getStringSourceIsNegativeInteger() {
+        return Stream.of("-1", "-0", "-1000");
+    }
+
+    private static Stream<String> getStringSourceCannotDivideByUnit() {
+        return Stream.of("1001", "999", "0");
+    }
+
+    private static void runWithInvalidPurchaseAmount(String invalidInput) {
+        run(invalidInput, "1000", "123456", "7");
+    }
+
+    private static void assertThatPrintErrorMessageWith(String message) {
+        assertThat(outputs())
+                .filteredOn(output -> ErrorMessagePattern.matcher(output).find())
+                .hasSize(1)
+                .first()
+                .asString()
+                .contains(message);
+    }
+
+    private static void assertThatApplicationNotExists() {
+        assertThat(outputs())
+                .containsSubsequence(INPUT_PURCHASE_AMOUNT_TEXT,
+                        INPUT_WINNING_NUMBERS_TEXT,
+                        INPUT_BONUS_NUMBER_TEXT,
+                        OUTPUT_LOTTERY_RESULT);
+    }
+}

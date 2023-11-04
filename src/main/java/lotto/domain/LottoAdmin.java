63664fb5813f4935f71f12119f9b34efac8bc5e0
MyunghyunNero <mh1kim0910@gmail.com>
2023-11-04 23:20:24 +0900

    feat : LottoAdmin 클래스 생성(당첨 번호 입력)

--- src/main/java/lotto/domain/LottoAdmin.java
+++ src/main/java/lotto/domain/LottoAdmin.java
@@ -8,20 +8,45 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoAdmin {
-    private static final Pattern PATTERN = Pattern.compile("(\\d{1,2},){5}\\d{1,2}");
+    private final Pattern WINNINGPATTERN = Pattern.compile("(\\d{1,2},){5}\\d{1,2}");
+    private final Pattern BONUSPATTERN = Pattern.compile("\\d+");
 
-    private List<Integer> winningNumber;
+    private final Integer MIN_RANGE = 1;
+    private final Integer MAX_RANGE = 45;
+
+    private Lotto winningNumber;
+    private Integer bonusNumber;
 
     public void inputWinningNumber() {
-        String result = Console.readLine();
-        validateInput(result);
-        this.winningNumber = Stream.of(result.split(","))
+        String winningNums = Console.readLine();
+        validateWinningInput(winningNums);
+        this.winningNumber = new Lotto(Stream.of(winningNums.split(","))
                 .map(Integer::parseInt)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
-    private void validateInput(String inputValue) {
-        if (!PATTERN.matcher(inputValue).matches()) {
+    public void inputBonusNumber() {
+        String bonusStr = Console.readLine();
+        validateBonusInput(bonusStr);
+        Integer bonusNum = Integer.parseInt(bonusStr);
+        validateRange(bonusNumber);
+        this.bonusNumber = bonusNum;
+    }
+
+    private void validateWinningInput(String inputValue) {
+        if (!WINNINGPATTERN.matcher(inputValue).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateBonusInput(String inputValue) {
+        if (!BONUSPATTERN.matcher(inputValue).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(Integer number) {
+        if (!(MIN_RANGE <= number && number <= MAX_RANGE)) {
             throw new IllegalArgumentException();
         }
     }

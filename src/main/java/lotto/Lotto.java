02eb07b0729bf2e72cad3476286a9db942ba333a
booyaChael <chaeeul2022@gmail.com>
2023-11-04 13:02:27 +0900

    doc(README): 기능 구현목록 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,10 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    //Lotto에 인스턴스 변수 추가 불가
+    //패키지 변경은 가능
+
+    //private 변경 금지
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {

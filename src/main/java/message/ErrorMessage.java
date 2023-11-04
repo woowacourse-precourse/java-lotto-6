f141a65228004795f76d1e56ddc4aec6bfcbeda6
mk3058 <kmk3058@icloud.com>
2023-11-04 11:30:55 +0900

    feat(ErrorMessage): message getter 추가

--- src/main/java/message/ErrorMessage.java
+++ src/main/java/message/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
     NEGATIVE_PRICE_VALUE("금액은 0 이상의 정수 이어야 합니다."),
     INVALID_INPUT_FORMAT("숫자와 쉼표(,)만 입력 되어야 합니다."),
     INVALID_NUMBER_RANGE("1 이상 45 이하의 수 만 입력 되어야 합니다."),
-    INVALID_NUMBER_COUNT("%s 개의 수가 입력 되어야 합니다."),
+    INVALID_NUMBER_COUNT("%s개의 수가 입력 되어야 합니다."),
     DUPLICATE_NUMBER_EXIST("중복된 숫자가 존재 하지 않아야 합니다.");
 
     private static final String errorPrefix = "[ERROR] ";
@@ -24,5 +24,13 @@ public enum ErrorMessage {
 
     public void print(String prefix) {
         System.out.printf(errorPrefix + message, prefix);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessage(String value) {
+        return String.format(errorPrefix + message, value);
     }
 }

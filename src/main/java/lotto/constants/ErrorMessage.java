8dee93d19eb42b1de4a1388c203aeab228535f74
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 21:01:44 +0900

    feat(ErrorMessage): 쉼표 구분 에러 메시지 작성

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     NOT_THOUSAND_UNIT_ERROR("1000단위로 입력을 해주세요"),
     LOTTO_NUMBER_SIZE_ERROR("로또의 개수가 6이 아닙니다."),
     LOTTO_NUMBER_RANGE_ERROR("로또 숫자 범위가 1 부터 45가 아닙니다."),
-    LOTTO_NUMBER_DUPLICATE_ERROR("로또 숫자가 중복되어 있습니다.");
+    LOTTO_NUMBER_DUPLICATE_ERROR("로또 숫자가 중복되어 있습니다."),
+    WINNING_NUMBERS_DIVISION_COMMA_ERROR("당첨 번호는 쉼표(,)를 기준으로 구분한다.");
 
     private final String message;
 

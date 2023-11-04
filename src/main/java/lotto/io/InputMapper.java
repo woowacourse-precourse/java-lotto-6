19cd76fc2cd360cd294d9f0e474f9c29da24c34d
GaBaljaintheroom <junsu1222@naver.com>
2023-11-04 21:04:43 +0900

    feat(InputMapper): 당첨 번호 Lotto 객체로 변환

--- src/main/java/lotto/io/InputMapper.java
+++ src/main/java/lotto/io/InputMapper.java
@@ -1,11 +1,22 @@
 package lotto.io;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoPurchasePrice;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class InputMapper {
 
     public LottoPurchasePrice toLottoPurchasePrice(String lottoPurchasePrice) {
         Integer purchasePrice = Integer.parseInt(lottoPurchasePrice);
         return new LottoPurchasePrice(purchasePrice);
+    }
+
+    public Lotto toLotto(String winningNumbers) {
+        List<Integer> wingingNumbers = Arrays.stream(winningNumbers.split(","))
+                .map(Integer::parseInt)
+                .toList();
+        return new Lotto(wingingNumbers);
     }
 }

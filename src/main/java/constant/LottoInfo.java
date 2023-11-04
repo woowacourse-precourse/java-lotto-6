b508b4afcbbef129eb22afc5468b6e17d35c6ce9
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 02:32:14 +0900

    feat : 로또의 정보에 대한 상수를 enum을 활용하여 작성

---
+++ src/main/java/constant/LottoInfo.java
@@ -0,0 +1,19 @@
+package constant;
+
+public enum LottoInfo {
+
+    ONE_LOTTO_PRICE(1000),
+    LOTTO_MIN_NUMBER(1),
+    LOTTO_MAX_NUMBER(45),
+    LOTTO_SIZE(6);
+
+    private Integer value;
+
+    LottoInfo(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}

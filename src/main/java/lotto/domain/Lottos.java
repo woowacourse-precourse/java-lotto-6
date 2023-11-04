6e706baaaa348bd789a7d4fff11fa94b41fd430d
waveofmymind <sjun0913@gmail.com>
2023-11-04 23:23:42 +0900

    style: 자바 컨벤션 스타일에 맞게 팩토리 메서드 위치를 변경한다.

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class Lottos {
     private final List<Lotto> lottos;
 
-    public static Lottos of(List<Lotto> lottos) {
-        return new Lottos(lottos);
-    }
-
     private Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public static Lottos of(List<Lotto> lottos) {
+        return new Lottos(lottos);
     }
 
     public int getLottoCount() {

2cc876f59e59c56455625dcaa6176da7da803916
yuseonjun <seonjun0906@gmail.com>
2023-11-04 20:01:17 +0900

    feat(LottoController): 당첨 번호를 생성한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,11 @@
 package lotto.controller;
 
 import java.util.List;
-import lotto.domain.Amount;
+import lotto.domain.BonusNumber;
 import lotto.domain.BuyerLotto;
 import lotto.domain.Lotto;
 import lotto.domain.LottoShop;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -19,15 +20,26 @@ public class LottoController {
     private BuyerLotto createBuyerLotto(LottoShop lottoShop) {
         Amount amount = generateAmount();
         List<Lotto> createBuyerLotto = lottoShop.createByBuyerLotto(amount);
+
         OutputView.printBuyerLotto(createBuyerLotto.size(), createBuyerLotto);
+
         return new BuyerLotto(createBuyerLotto);
     }
 
-    private Amount generateAmount() {
+    private WinningLotto createWinningLotto() {
+        List<Integer> winningLotto = generateWinningLotto();
+        Lotto createWinningLotto = lottoShop.createByWinningLotto(winningLotto);
+
+        int bonusNumber = generateBonusNumber();
+        BonusNumber createBonusNumber = lottoShop.createByBonusNumber(bonusNumber);
+
+        return new WinningLotto(createWinningLotto, createBonusNumber);
+    }
+
+    private int generateAmount() {
         while (true) {
             try {
-                int amount = InputView.readPurchaseAmount();
-                return new Amount(amount);
+                return InputView.readPurchaseAmount();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }

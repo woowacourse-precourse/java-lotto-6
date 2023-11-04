325c3fc7b2b64b0d9bd53cdc120fabedc252152c
doha <dohaahi@naver.com>
2023-11-04 23:45:00 +0900

    refactor (OutputView): 개행 문자 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,9 +4,9 @@ package lotto.view;
 import lotto.domain.dto.PurchaseAmountDto;
 
 public class OutputView {
-    public static final String LOTTO_PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.";
+    public static final String LOTTO_PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
 
     public void printPurchaseQuantityLottos(final PurchaseAmountDto purchaseAmountDto) {
-        System.out.printf(LOTTO_PURCHASE_RESULT_MESSAGE, purchaseAmountDto.purchaseQuantityLottos());
+        System.out.printf("\n" + LOTTO_PURCHASE_RESULT_MESSAGE, purchaseAmountDto.purchaseQuantityLottos());
     }
 }

0d188bd15d72fdb6320d23c4167b221cdd467441
unknown <winternrabbit@gmail.com>
2023-11-05 00:33:09 +0900

    refactor: 예시와 동일하게 출력되도록 문구 수정

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,9 +1,22 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Output {
     private static final String NUMBER_OF_PURCHAGE = "개를 구매했습니다.";
 
     public void printNumberOfPurchase(int lottoTicket) {
         System.out.println("\n" + lottoTicket + NUMBER_OF_PURCHAGE);
+    }
+
+    public void printRandomLottoNumbers(int lottoTicket) {
+        LottoGenerate lottoGenerate = new LottoGenerate();
+
+        HashMap<Integer, List<Integer>> result = lottoGenerate.getRandomLottoResult(lottoTicket);
+
+        for (int key : result.keySet()) {
+            System.out.println(result.get(key));
+        }
     }
 }

e78e1ee0f581a969ca01106772c491b6e9f22a60
Hoo-sung <gntjd135@gmail.com>
2023-11-05 20:48:01 +0900

    fix(BonusNumerVerifier): checkNumeric 메서드 로직 수정

--- src/main/java/lotto/verifier/BonusNumberVerifier.java
+++ src/main/java/lotto/verifier/BonusNumberVerifier.java
@@ -3,6 +3,8 @@ package lotto.verifier;
 import lotto.system.Constant;
 import lotto.system.ExceptionMessage;
 
+import java.math.BigInteger;
+
 public class BonusNumberVerifier implements Verifier{
     @Override
     public void check(String input) {
@@ -13,8 +15,8 @@ public class BonusNumberVerifier implements Verifier{
 
     private void checkNumeric(String input) {
         try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
+            new BigInteger(input);
+        } catch (Exception e) {
             throw new IllegalArgumentException(ExceptionMessage.IS_NOT_NUMERIC);
         }
     }

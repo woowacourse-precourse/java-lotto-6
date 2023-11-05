bcb958b5fdfb35f92a3ff8f876409139bc13fc49
sleeg00 <slee000220@gmail.com>
2023-11-05 19:26:11 +0900

    Feat: (Input, Output)에 대한 class 생성 및 의존성주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,8 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoGame lottoGame = new LottoGame(inputView, outputView);
     }
 }
---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class LottoGame {
+    private InputView inputView;
+    private OutputView outputView;
+
+    public LottoGame(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+}

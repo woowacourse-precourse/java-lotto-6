75a1bef1119bd4f7259ac84f0ff7b67d790e3e7c
songsunkook <songsunkook@gmail.com>
2023-11-06 01:43:00 +0900

    feat: LottoController 골격 구성

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import view.InputView;
+import view.OutputView;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void buyTickets() {
+
+    }
+
+    public void drawLotto() {
+
+    }
+
+    public void printWinningStatistics() {
+
+    }
+}
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import controller.LottoController;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(
+                new InputView(), new OutputView()
+        );
+        lottoController.buyTickets();
+        lottoController.drawLotto();
+        lottoController.printWinningStatistics();
     }
 }

1f1f628a627563bee2537b49fbdf5e195981f57e
kcw0360 <kcw0360@naver.com>
2023-11-05 19:34:48 +0900

    feat: 당첨 번호 입력 중복 및 숫자 범위 예외 처리

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,18 +1,48 @@
 package lotto.model;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class WinningNumber {
 
-    private static final Pattern winningNumberInputPattern = Pattern.compile("\\d,\\d,\\d,\\d,\\d,\\d");
+    private static final Pattern winningNumberInputPattern = Pattern.compile(
+            "\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}");
+    private List<Integer> winningNumbers;
 
     public void inputWinningNumber(String input) {
         validateWinningNumber(input);
+        List<Integer> inputNumbers = Arrays.stream(input.split(","))
+                .map(Integer::parseInt).toList();
+        validateNumberRange(inputNumbers);
+        validateDuplication(inputNumbers);
+        this.winningNumbers = inputNumbers;
+    }
+
+    private static void validateDuplication(List<Integer> inputNumbers) {
+        Set<Integer> validateNumbers = new HashSet<>(inputNumbers);
+        if (validateNumbers.size() < 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않은 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void validateNumberRange(List<Integer> inputNumbers) {
+        for (Integer inputNumber : inputNumbers) {
+            if (inputNumber < 1 || inputNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또는 1부터 45까지의 숫자만 사용됩니다.");
+            }
+        }
     }
 
     private static void validateWinningNumber(String input) {
         if (!winningNumberInputPattern.matcher(input).matches()) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 당첨 번호 입력 양식입니다.");
         }
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
     }
 }
--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -4,19 +4,43 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class WinningNumberTest {
 
+    WinningNumber winningNumber;
+
+    @BeforeEach
+    void beforeEach() {
+        this.winningNumber = new WinningNumber();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"1,2,삼,4,5,6", "!!!", "1,2,3, 4", "1,2,3, ,5,6", "1,2,3,4,5"})
     void 당첨_번호_입력_양식(String input) {
-        WinningNumber winningNumber = new WinningNumber();
-
         assertThatCode(() -> winningNumber.inputWinningNumber("1,2,3,4,5,6")).doesNotThrowAnyException();
         assertThatThrownBy(() -> winningNumber.inputWinningNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 올바르지 않은 당첨 번호 입력 양식입니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0,1,2,3,4,5", "1,2,3,4,5,46"})
+    void 당첨_번호_범위(String input) {
+        assertThatCode(() -> winningNumber.inputWinningNumber("1,2,3,4,5,6")).doesNotThrowAnyException();
+        assertThatThrownBy(() -> winningNumber.inputWinningNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또는 1부터 45까지의 숫자만 사용됩니다.");
+    }
+
+    @Test
+    void 당첨_번호_중복() {
+        assertThatCode(() -> winningNumber.inputWinningNumber("1,2,3,4,5,6")).doesNotThrowAnyException();
+        assertThatThrownBy(() -> winningNumber.inputWinningNumber("1,1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 중복되지 않은 숫자를 입력해주세요.");
     }
 }

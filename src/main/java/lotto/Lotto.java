c1c209c749ae1ed9d8b5b585b6df9a78c7128953
Son JunHyung <kingjh1125@gmail.com>
2023-11-05 17:35:20 +0900

    feat (Lotto): 로또 숫자 범위에 대한 예외 처리
    로또 숫자 범위 대한 프로덕션 코드를 구현하여 통과하였습니다:
    - validateRange 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final Pattern lottoPattern = Pattern.compile("[1-9]|[1-3][0-9]|4[0-5]");
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -13,6 +15,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers){
@@ -27,6 +30,13 @@ public class Lotto {
         }
     }
 
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (!lottoPattern.matcher(Integer.toString(number)).matches()) {
+                throw new IllegalArgumentException();
+            }
+        }
 
-    // TODO: 추가 기능 구현
+    }
+
 }

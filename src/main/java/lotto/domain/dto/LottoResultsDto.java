4d1a880563d5cc69b31a0ec0ddba9429cf30d825
newh08 <newh08@gmail.com>
2023-11-06 02:36:15 +0900

    🔥 REFACTOR: LottoResultsDto 에서 LottoPrizeMoney 삭제
    Profit 계산 LottoResultsService 로 위임해 LottoResultsDto 에서 Profit 계산 관련 로직 삭제

--- src/main/java/lotto/domain/dto/LottoResultsDto.java
+++ src/main/java/lotto/domain/dto/LottoResultsDto.java
@@ -2,23 +2,16 @@ package lotto.domain.dto;
 
 import java.util.Collections;
 import java.util.Map;
-import lotto.domain.lottoresult.LottoPrizeMoney;
 import lotto.domain.lottoresult.LottoResult;
 
 public class LottoResultsDto {
     private final Map<LottoResult, Integer> lottoResultsData;
-    private final LottoPrizeMoney lottoPrizeMoney;
 
-    public LottoResultsDto(Map<LottoResult, Integer> lottoResultsData, LottoPrizeMoney lottoPrizeMoney) {
+    public LottoResultsDto(Map<LottoResult, Integer> lottoResultsData) {
         this.lottoResultsData = lottoResultsData;
-        this.lottoPrizeMoney = lottoPrizeMoney;
     }
 
     public Map<LottoResult, Integer> getLottoResultsData() {
         return Collections.unmodifiableMap(lottoResultsData);
-    }
-
-    public long getTotalLottoPrizeMoney() {
-        return lottoPrizeMoney.getLottoPrizeMoney();
     }
 }

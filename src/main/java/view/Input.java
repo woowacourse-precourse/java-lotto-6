4ff7fb18b30a12c97dffaf41a42bc8641e7e3192
zzawang <hobin0116@naver.com>
2023-11-05 23:55:41 +0900

    feat: 로또 보너스 번호 입력 기능 추가
    - 필요한 상수 정의
    - 정의한 문자열 상수를 출력한다.
    - 사용자로부터 Console.readLine()을 통해 문자열을 입력받는다.
    - InputException의 canBeConvertedToInteger 함수를 통해 Integer형으로 변환 가능한지 검증한다.
    - InputException의 isNumberInRange 함수를 통해 값이 1에서 45사이인지 검증한다.
    - InputException의 isInWinningNumbers 함수를 통해 보너스 번호와 당첨 번호의 중복 여부를 검증한다.
    - 사용자가 잘못된 값을 입력하여 검사를 통과하지 못한다면 에러 메시지를 출력 후 입력을 다시 받는다.
    - 검사를 모두 통과한 사용자 입력을 Integer형으로 변환하여 return한다.

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class Input {
     private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String WINNING_NUMBER_SEPARATOR = ",";
 
     public static int getPurchaseAmount() {
@@ -66,5 +67,28 @@ public class Input {
         return Arrays.stream(userInputs)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public static int getBonusNumber(List<Integer> winningNumber) {
+        String userInput;
+
+        System.out.println(ENTER_BONUS_NUMBER);
+        do {
+            userInput = Console.readLine();
+        } while (!validateBonusNumber(winningNumber, userInput));
+
+        return Integer.parseInt(userInput);
+    }
+
+    private static boolean validateBonusNumber(List<Integer> winningNumber, String userInput) {
+        try {
+            InputException.canBeConvertedToInteger(userInput);
+            InputException.isNumberInRange(userInput);
+            InputException.isInWinningNumbers(winningNumber, userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
     }
 }

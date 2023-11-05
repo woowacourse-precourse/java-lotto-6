911edb7c1b93aa16179838935be3c350a60dc45d
song-jae-wan <sjo9810@gmail.com>
2023-11-05 16:29:54 +0900

    feat: 객체 주입 클래스 싱글턴으로 설계

--- src/main/java/config/LottoLauncher.java
+++ src/main/java/config/LottoLauncher.java
@@ -1,4 +1,31 @@
 package config;
 
+import VO.UserLottoVO;
+import controller.GameController;
+import domain.WinningLotto;
+import service.LottoService;
+import view.GameView;
+import view.InputView;
+
 public class LottoLauncher {
+
+    private static LottoLauncher defaultLottoLauncher;
+    private InputView inputView;
+    private GameController gameController;
+    private LottoService lottoService;
+    private WinningLotto winningLotto;
+    private UserLottoVO userLottoVO;
+    private GameView gameView;
+
+    private LottoLauncher() {
+    }
+
+    public static LottoLauncher getInstance() {
+        if(defaultLottoLauncher == null) {
+            defaultLottoLauncher = new LottoLauncher();
+        }
+        return defaultLottoLauncher;
+    }
+
+
 }

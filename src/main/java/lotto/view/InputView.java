13705631b7b87cae8eac7cafbfb3a96851a17507
k9want <zndwlrnjr26@daum.net>
2023-11-05 20:24:01 +0900

    feat: 입력 문구 - 구입 금액을 입력해 주세요.

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class InputView {
+    private static final String USER_INPUT_BUY_ACCOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void inputBuyLottoMoney() {
+        System.out.println(USER_INPUT_BUY_ACCOUNT_MESSAGE);
+    }
+}

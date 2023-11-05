60950c1df5702a651da467e9480ba2d2b618e0fc
witwint <witwint1234@gmail.com>
2023-11-05 15:05:55 +0900

    feat: view 예외처리 인터페이스 구현

---
+++ src/main/java/lotto/view/Exception.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public interface Exception {
+
+	void InputIsNumber(IllegalArgumentException e);
+
+	void InputIsMultiple(IllegalArgumentException e);
+
+	void InputIsNotNull(IllegalArgumentException e);
+
+}

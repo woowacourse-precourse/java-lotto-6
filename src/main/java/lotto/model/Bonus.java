b6c6fb90680b11cb8d74a2b22110c3eb2b07fb4d
WooJJam <jaemin5548@naver.com>
2023-11-05 22:16:32 +0900

    [Refactor] Bonus 예외검증 validator 분리

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -1,45 +1,17 @@
 package lotto.model;
 
-import java.util.List;
-import java.util.regex.Pattern;
+import lotto.validator.BonusValidator;
 
 public class Bonus {
     private int number;
-    private static final Pattern BONUS_REGEX = Pattern.compile("^[0-9]+$");
-
 
     public Bonus(Lotto lotto, String inputBonusNumber) {
-        validate(lotto, inputBonusNumber);
+        BonusValidator.validate(lotto, inputBonusNumber);
         this.number = stringToInteger(inputBonusNumber);
     }
 
-    public void validate(Lotto lotto, String inputBonusNumber) {
-        validateNumeric(inputBonusNumber);
-        int bonusNumber = stringToInteger(inputBonusNumber);
-        validateDuplicate(lotto.getNumbers(), bonusNumber);
-        validateRange(bonusNumber);
-    }
-
-    public int stringToInteger(String inputBonusNumber) {
+    private int stringToInteger(String inputBonusNumber) {
         return Integer.parseInt(inputBonusNumber);
-    }
-
-    public void validateNumeric(String inputBonusNumber) {
-        if (!BONUS_REGEX.matcher(inputBonusNumber).matches()) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 정수만 입력 가능합니다.");
-        }
-    }
-
-    public void validateDuplicate(List<Integer> winningLottoNumbers, int bonusNumber) {
-        if(winningLottoNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호와 중복된 숫자는 입력할 수 없습니다.");
-        }
-    }
-
-    public void validateRange(int inputBonusNumber) {
-        if(inputBonusNumber < 1 || inputBonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 사이만 입력 가능합니다.");
-        }
     }
 
     public int getBonusNumber() {
---
+++ src/main/java/lotto/validator/BonusValidator.java
@@ -0,0 +1,39 @@
+package lotto.validator;
+
+import lotto.model.Lotto;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class BonusValidator {
+    private static final Pattern BONUS_REGEX = Pattern.compile("^[0-9]+$");
+
+    public static void validate(Lotto lotto, String inputBonusNumber) {
+        validateNumeric(inputBonusNumber);
+        int bonusNumber = stringToInteger(inputBonusNumber);
+        validateDuplicate(lotto.getNumbers(), bonusNumber);
+        validateRange(bonusNumber);
+    }
+
+    private static int stringToInteger(String inputBonusNumber) {
+        return Integer.parseInt(inputBonusNumber);
+    }
+
+    private static void validateNumeric(String inputBonusNumber) {
+        if (!BONUS_REGEX.matcher(inputBonusNumber).matches()) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 정수만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> winningLottoNumbers, int bonusNumber) {
+        if(winningLottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 중복된 숫자는 입력할 수 없습니다.");
+        }
+    }
+
+    public static void validateRange(int inputBonusNumber) {
+        if(inputBonusNumber < 1 || inputBonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 사이만 입력 가능합니다.");
+        }
+    }
+}

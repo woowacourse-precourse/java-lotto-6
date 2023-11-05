99272c09cba0541bd07ea2bf8c30b51babc2be9f
bellring <starringbell@gmail.com>
2023-11-05 15:48:49 +0900

    feat(Lotto) : 로또 번호 유효성 로직 작성
    - 에러처리를 위해 별도의 enum 생성
    - 로또 숫자가 총 6개인지 검증하는 기능 구현
    - 로또 숫자가 1~45범위 이내인지 검증하는 기능 구현
    - 로또 숫자에 중복이 존재하는지 검증하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,43 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import lotto.enums.LottoError;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+	private void validate(List<Integer> numbers) {
+		validateSize(numbers);
+		validateRange(numbers);
+		validateRedundant(numbers);
+	}
 
-    // TODO: 추가 기능 구현
+	private void validateSize(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException(LottoError.INVALID_SIZE_INPUT.getErrorDescription());
+		}
+	}
+
+	private void validateRange(List<Integer> numbers) {
+		for (int num : numbers) {
+			if (num < 1 || num > 45) {
+				throw new IllegalArgumentException(LottoError.INVALID_RANGE_INPUT.getErrorDescription());
+			}
+		}
+	}
+
+	private void validateRedundant(List<Integer> numbers) {
+		Set<Integer> NotRedundantNumbers = new HashSet<>(numbers);
+		if (numbers.size() != NotRedundantNumbers.size()) {
+			throw new IllegalArgumentException(LottoError.INVALID_REDUNDANT_INPUT.getErrorDescription());
+		}
+	}
 }

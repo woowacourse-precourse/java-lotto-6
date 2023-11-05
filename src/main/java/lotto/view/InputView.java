0a9d8f922cbb7afd704eefd3bfeaa95b479e7333
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-06 04:51:23 +0900

    feat(InputView) : 당첨 번호 입력 안내 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,4 +7,9 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public String readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }

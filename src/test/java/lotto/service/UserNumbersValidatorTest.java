0337698318a613b9c3ec506ec05732e936fbd9f8
Heejun-0219 <heejun0219@gmail.com>
2023-11-06 00:30:36 +0900

    test: 구분자로 시작하거나 끝나는지, 중복 구분자 있는지 확인 테스트

--- src/test/java/lotto/service/UserNumbersValidatorTest.java
+++ src/test/java/lotto/service/UserNumbersValidatorTest.java
@@ -47,7 +47,7 @@ public class UserNumbersValidatorTest {
     @DisplayName("유저 로또 번호 입력 사이즈 예외 테스트")
     @Test
     void testPostChangeUserNumbersException() {
-        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1,2,3,4,5,6,7)))
+        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -55,7 +55,7 @@ public class UserNumbersValidatorTest {
     @DisplayName("유저 로또 번호 입력 중복 예외 테스트")
     @Test
     void testByDuplication() {
-        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1,2,3,4,5,5)))
+        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -63,7 +63,7 @@ public class UserNumbersValidatorTest {
     @DisplayName("유저 로또 번호 입력 최대 예외 테스트")
     @Test
     void testByMaxvalue() {
-        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1,2,3,4,5,46)))
+        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -71,7 +71,31 @@ public class UserNumbersValidatorTest {
     @DisplayName("유저 로또 번호 입력 최소 예외 테스트")
     @Test
     void testByMinvalue() {
-        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1,2,3,4,5,0)))
+        assertThatThrownBy(() -> UserNumbersValidator.postValidate(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("유저 로또 번호 입력 구분자 시작 예외 테스트")
+    @Test
+    void errorByStartDelimiter() {
+        assertThatThrownBy(() -> UserNumbersValidator.preValidate(",1,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("유저 로또 번호 입력 구분자 끝 예외 테스트")
+    @Test
+    void errorByEndDelimiter() {
+        assertThatThrownBy(() -> UserNumbersValidator.preValidate("1,2,3,4,5,6,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("유저 로또 번호 입력 구분자 중복 예외 테스트")
+    @Test
+    void errorByDuplicationDelimiter() {
+        assertThatThrownBy(() -> UserNumbersValidator.preValidate("1,2,3,4,5,,6"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }

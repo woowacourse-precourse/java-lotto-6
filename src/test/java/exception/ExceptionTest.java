c1aa14c599030c17cb94de453f08cb104e33e346
song-jae-wan <sjo9810@gmail.com>
2023-11-05 18:05:12 +0900

    Test: 예외 테스트
    - 숫자예외
    - 숫자단위 예외
    - 숫자범위 예외

---
+++ src/test/java/exception/ExceptionTest.java
@@ -0,0 +1,31 @@
+package exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static exception.InputException.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest {
+
+    @DisplayName("숫자가 아니라면 예외가 발생")
+    @Test
+    void valueByNumber() {
+        assertThatThrownBy(() -> validateInteger("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1000 단위의 숫자가 아니라면 예외가 발생")
+    @Test
+    void numberByPayUnit() {
+        assertThatThrownBy(() -> validatePayUnit(-2000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1-45 사이의 숫자가 아니라면 예외 발생")
+    @Test
+    void numberByInRange() {
+        assertThatThrownBy(() -> validateRange(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

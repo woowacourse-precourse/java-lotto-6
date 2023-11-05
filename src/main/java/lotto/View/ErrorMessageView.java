7dda3b490d36998970ed39f33d89ed364541eed6
voyager003 <miraclex000@gmail.com>
2023-11-05 12:25:20 +0900

    feat(ErrorMessageView): add method
    ADD constant, printErrorWrongNumberRange() method
    - constant: containing message winning number range
    - method: print constant message

--- src/main/java/lotto/View/ErrorMessageView.java
+++ src/main/java/lotto/View/ErrorMessageView.java
@@ -6,8 +6,14 @@ public class ErrorMessageView {
 
     static final String ERROR_WRONG_AMOUNT = "로또 구입 금액은 1000원 단위로 입력해야 합니다.";
 
+    static final String ERROR_WRONG_NUMBER_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
 
     public static void printErrorWrongAmount() {
         System.out.println(ERROR_COMMON + ERROR_WRONG_AMOUNT);
+    }
+
+    public static void printErrorWrongNumberRange() {
+        System.out.println(ERROR_COMMON + ERROR_WRONG_NUMBER_RANGE);
     }
 }

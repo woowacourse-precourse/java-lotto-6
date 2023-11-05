48894e285bd3dd297273592e7c87a5a6d3cc4fdf
song-jae-wan <sjo9810@gmail.com>
2023-11-05 23:12:03 +0900

    Test: 테스트 코드 줄바꿈

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -26,7 +26,8 @@ public class WinningLottoTest {
     @Test
     void createCountByLotto() {
         winningLotto.setNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
-        assertThat(winningLotto.countWinNumber(new Lotto(List.of(1, 2, 3, 4, 5, 6)))).isEqualTo(6);
+        assertThat(winningLotto.countWinNumber(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)))).isEqualTo(6);
     }
 
     @DisplayName("당첨번호와 로또번호가 5개 일치할때 보너스번호도 일치한다면 7을 반환한다.")
@@ -34,7 +35,8 @@ public class WinningLottoTest {
     void createCountBySecondWin() {
         winningLotto.setNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
         winningLotto.setBonusNumber(7);
-        assertThat(winningLotto.countWinNumber(new Lotto(List.of(1, 2, 3, 4, 5, 7)))).isEqualTo(7);
+        assertThat(winningLotto.countWinNumber(
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)))).isEqualTo(7);
     }
 
 }

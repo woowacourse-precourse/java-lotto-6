bfcac16b35e70aa80437f9a4d2eccfc57f03f630
nohheeyeon <heeyeon1213@gmail.com>
2023-11-05 14:14:54 +0900

    feat : LottoTicket클래스 생성자 구현

---
+++ src/main/java/domain/LottoTicket.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class LottoTicket {
+    private final List<Lotto> lottoNumbers;
+
+    public LottoTicket(List<Lotto> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+}

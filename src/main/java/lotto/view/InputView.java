7e439f6081dd4a38507c7d7bf8ba90830666da5b
Nbowow <nbo5548@naver.com>
2023-11-05 16:07:21 +0900

    Update: InputView 2023.11.05 입력까지 받을 수 있도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,35 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
 
-    public static void requestLottoBuyingPrice() {
+    private static List<Integer> winningNumberList;
+
+    public static String requestLottoBuyingPrice() {
         System.out.println("구매하실 로또 금액을 입력해주세요.");
+        return Console.readLine();
     }
 
-    public static void requestLottoWinNumber() {
+    public static List<Integer> requestLottoWinningNumber() {
         System.out.println("당첨 번호를 입력해주세요.");
+        return stringToList(Console.readLine());
     }
 
-    public static void requestLottoBonusNumber() {
+    private static List<Integer> stringToList(String winningNumber) {
+        String[] result = winningNumber.split(",");
+        winningNumberList = new ArrayList<>();
+        for (int i = 0; i < result.length; i++) {
+            winningNumberList.add(Integer.parseInt(result[i]));
+        }
+        return winningNumberList;
+    }
+
+    public static int requestLottoBonusNumber() {
         System.out.println("보너스 번호를 입력해주세요.");
+        return Integer.parseInt(Console.readLine());
     }
 }

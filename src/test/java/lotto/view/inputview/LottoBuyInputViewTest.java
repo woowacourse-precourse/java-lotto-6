6f62b9d5be6ff9a7ca75a259a10671ce68567180
howudong <tjdvy963@naver.com>
2023-11-05 17:31:54 +0900

    test : LottoBuyInputView에 대한 테스트 코드 작성 완료

---
+++ src/test/java/lotto/view/inputview/LottoBuyInputViewTest.java
@@ -0,0 +1,45 @@
+package lotto.view.inputview;
+
+import lotto.Error;
+import lotto.view.InputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoBuyInputViewTest {
+    private final InputView inputView = new LottoBuyInputView();
+
+    @Test
+    @DisplayName("해당 입력들에 대해 예외가 발생해야한다.")
+    void 구입금액_예외_발생_테스트() {
+        //given
+        List<String> testParam = getTestParam();
+
+        //when
+        String join = String.join("\n", testParam);
+        System.setIn(new ByteArrayInputStream(join.getBytes()));
+        for (String input : testParam) {
+            Map<String, String> param = new HashMap<>();
+            //then
+            assertThatThrownBy(() -> inputView.read(param))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(Error.View.NOT_NUMBER.getText());
+        }
+
+    }
+
+    public List<String> getTestParam() {
+        return List.of("-1m1  ",
+                "1-000  ",
+                "1  -0- 00 00 0 0 00 0 ",
+                "가나다132  ",
+                "  ",
+                "  . ");
+    }
+}

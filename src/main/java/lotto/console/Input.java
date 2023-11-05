6575242e4ee23a4a24971790ff07202f9d95ca7a
yeonsu <jack0810@kookmin.ac.kr>
2023-11-06 00:59:59 +0900

    feat: make all input functions while there is no err input

--- src/main/java/lotto/console/Input.java
+++ src/main/java/lotto/console/Input.java
@@ -2,6 +2,11 @@ package lotto.console;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Asset;
+import lotto.domain.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
     public static Asset askMoney(){
@@ -15,13 +20,24 @@ public class Input {
         }
     }
 
-    public static String askWinningNum(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+    public static Lotto askWinningNum(){
+        try{
+            System.out.println("당첨 번호를 입력해 주세요.");
+            String inputString = Console.readLine();
+
+            List<Integer> numbers = Arrays.stream(inputString.split(","))
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+
+            return new Lotto(numbers);
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] : " + e.getMessage());
+            return askWinningNum();
+        }
     }
 
-    public static String askBonusNum(){
+    public static Integer askBonusNum(){
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
+        return Integer.valueOf(Console.readLine());
     }
 }
--- src/main/java/lotto/service/Execute.java
+++ src/main/java/lotto/service/Execute.java
@@ -2,9 +2,14 @@ package lotto.service;
 
 import lotto.console.Input;
 import lotto.domain.Asset;
+import lotto.domain.Lotto;
 
 public class Execute {
     public void start(){
-        Input.askMoney();
+        Asset asset = Input.askMoney();
+        Lotto winningNum = Input.askWinningNum();
+        Integer bonusNum = Input.askBonusNum();
+
+        
     }
 }

e7aa6602bdc43d5d0c91206c41e56718422cb131
Finn <wjstodsss@naver.com>
2023-11-06 07:49:25 +0900

    feat - IllegalArgumentException이 발생하면 [ERROR]로 시작하는 View의 에러 메세지 출력 기능

--- src/main/java/lotto/views/MessageManager.java
+++ src/main/java/lotto/views/MessageManager.java
@@ -5,14 +5,22 @@ import java.util.ResourceBundle;
 public class MessageManager {
     private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
     private static final ResourceBundle exceptionMessages = ResourceBundle.getBundle("exception-messages");
+
+    //General Messages
     public static void getPurchaseAmountPromptMessage() {
         System.out.println(messages.getString("purchaseAmountPrompt.message"));
+    }
+    public static void getProgramClosePromoptMessage() {
+        System.err.println(messages.getString("programClosePromopt.message"));
     }
 
 
     //Exception Messages
     public static String getValidateNonIntegerMessage() {
         return exceptionMessages.getString("validateNonInteger.message");
+    }
+    public static String getValidateDivisibleByThousandMessage() {
+        return exceptionMessages.getString("validateDivisibleByThousand.message");
     }
 
 }

34d79474b4bdfbe4390a3fce951c81854ea17a5f
3juhwan <13selfesteem91@naver.com>
2023-11-05 11:13:09 +0900

    feat: 구입 금액에 대한 유효값 검증 로직

---
+++ src/main/java/lotto/domain/Deposit.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+public class Deposit {
+    private static final String NOT_A_NUMBER_MESSAGE = "[ERROR] 로또 구입 금액은 숫자여야 합니다. ";
+    private static final String NOT_A_UNIT_OF_THOUSAND_MESSAGE = "[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다. ";
+    private static final String OUT_OF_RANGE_MESSAGE = "[ERROR] 로또 구입 금액은 1,000원 이상, 100,000원 이하여야 합니다. ";
+    private static final String NUMERAL_REGEX = "[0-9]+";
+    private static final Integer MIN_AMOUNT = 1_000;
+    private static final Integer MAX_AMOUNT = 100_000;
+
+    private final Integer amount;
+
+    public Deposit(String amount) {
+        validateDeposit(amount);
+        this.amount = Integer.getInteger(amount);
+    }
+
+    private void validateDeposit(String amount) {
+        if (!amount.matches(NUMERAL_REGEX)) {
+            throw new IllegalArgumentException(NOT_A_NUMBER_MESSAGE);
+        }
+        if (Integer.parseInt(amount) % 10 != 0) {
+            throw new IllegalArgumentException(NOT_A_UNIT_OF_THOUSAND_MESSAGE);
+        }
+        if (Integer.parseInt(amount) > MAX_AMOUNT
+                || Integer.parseInt(amount) < MIN_AMOUNT) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_MESSAGE);
+        }
+    }
+
+    public Integer calculateBuyableLottoAmount() {
+        return amount / 10;
+    }
+}

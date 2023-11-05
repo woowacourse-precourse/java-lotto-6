252705ef1899468159832b6df8412a98a28be17e
kkw2758 <dtc3229@gmail.com>
2023-11-05 21:00:34 +0900

    Feat(AnswerLotto) : 당첨번호에 중복 되는 숫자가 있으면 예외 처리 기능 추가

--- src/main/java/lotto/AnswerLotto.java
+++ src/main/java/lotto/AnswerLotto.java
@@ -12,15 +12,27 @@ public class AnswerLotto {
 
     private void validateAnswerLotto(List<Integer> numbers) {
         validateLength(numbers);
+        validateDuplicateMember(numbers);
     }
 
     private void validateLength(List<Integer> numbers) {
         if (checkLength(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("당첨번호의 길이가 6이 아닙니다");
         }
     }
 
     private boolean checkLength(List<Integer> numbers) {
         return numbers.size() != 6;
+    }
+
+    private void validateDuplicateMember(List<Integer> numbers) {
+        if (checkHasDuplicateMember(numbers)) {
+            throw new IllegalArgumentException("당첨번호에 중복된 숫자가 포함되어 있습니다.");
+        }
+    }
+    private boolean checkHasDuplicateMember(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != numbers.size();
     }
 }

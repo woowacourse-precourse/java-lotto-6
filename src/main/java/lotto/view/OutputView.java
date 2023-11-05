f37a1e0cdd45065feecffc908deb0d7ecf93244c
WooYoungDoo <duden5000@naver.com>
2023-11-05 14:18:04 +0900

    feat (OutputView) : 로또 구매 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,11 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private final static String OUTPUT_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    private final static String OUTPUT_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    private final static String OUTPUT_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
     private final static String OUTPUT_WINNER_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String BLANK_LINE = "";
@@ -27,5 +29,9 @@ public class OutputView {
 
     public void printBlankLine() {
         System.out.println(BLANK_LINE);
+    }
+
+    public void printPurchaseLottoNumbers(List<List<Integer>> purchaseLottoNumbers) {
+        purchaseLottoNumbers.forEach(purchaseLottoNumber -> System.out.println(purchaseLottoNumber.toString()));
     }
 }

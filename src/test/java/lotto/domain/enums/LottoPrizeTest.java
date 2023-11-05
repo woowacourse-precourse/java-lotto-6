d54644bb67aa8683cdb0a52845f5a108beebc542
gundorit <packdev937@gmail.com>
2023-11-05 11:35:31 +0900

    test (LottoPrize) : 일치 번호에 따라 정확한 상금을 반환하는지 테스트 한다

---
+++ src/test/java/lotto/domain/enums/LottoPrizeTest.java
@@ -0,0 +1,20 @@
+package lotto.domain.enums;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("LottoPrize 클래스")
+class LottoPrizeTest {
+
+    @DisplayName("일치 번호에 따라 정확한 상금을 찾아 반환한다.")
+    @Test
+    void testFindLottoPrize() {
+        assertEquals(LottoPrize.FIFTH, LottoPrize.findLottoPrize(3, false));
+        assertEquals(LottoPrize.FOURTH, LottoPrize.findLottoPrize(4, false));
+        assertEquals(LottoPrize.THIRD, LottoPrize.findLottoPrize(5, false));
+        assertEquals(LottoPrize.SECOND, LottoPrize.findLottoPrize(4, true));
+        assertEquals(LottoPrize.FIRST, LottoPrize.findLottoPrize(6, false));
+    }
+}

4c9b0d9567ed8b970abfb06caca3c1d9914040f6
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-05 16:42:11 +0900

    feat: 메시지 유틸리티 클래스 추가

---
+++ src/main/java/lotto/utils/MessageConstant.java
@@ -0,0 +1,19 @@
+package lotto.utils;
+
+public final class MessageConstant {
+
+    // Input Message
+    public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    // Output Message
+    public static final String RESULT = "당첨 통계";
+    public static final String LINE = "---";
+
+    // Error Message
+
+
+    private MessageConstant() {
+    }
+}

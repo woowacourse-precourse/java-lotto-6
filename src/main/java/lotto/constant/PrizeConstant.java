c1eebff4c41a3d8a0f5f7a8aaf87a35c0e424366
Yang-soeun <soeun8636@naver.com>
2023-11-05 16:30:49 +0900

    [feat]: 등수에 관란 이넘 클래스 생성

---
+++ src/main/java/lotto/constant/PrizeConstant.java
@@ -0,0 +1,17 @@
+package lotto.constant;
+
+public enum PrizeConstant {
+
+    FIRST(7,"6개 일치 (2,000,000,000원) - "),
+    SECOND(6, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, "5개 일치 (1,500,000원) - "),
+    FOURTH(4, "4개 일치 (50,000원) - "),
+    FIFTH(3, "3개 일치 (5,000원) - ");
+    private final int score;
+    private final String message;
+
+    PrizeConstant(int score, String message) {
+        this.score = score;
+        this.message = message;
+    }
+}

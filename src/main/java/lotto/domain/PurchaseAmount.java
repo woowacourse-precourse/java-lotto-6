34df0f7b614abbf2818f1a0987a8655d12287125
Hanjaemo <hjm2530@gmail.com>
2023-11-06 04:25:21 +0900

    feat(PurchaseAmount): 구입 금액 검증 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -2,10 +2,28 @@ package lotto.domain;
 
 public class PurchaseAmount {
 
+    private static final int MIN_PURCHASE_AMOUNT = 1000;
+    private static final int MAX_PURCHASE_AMOUNT = 100_000;
+
     private int amount;
 
     public PurchaseAmount(int amount) {
+        validateUnit(amount);
+        validateRange(amount);
         this.amount = amount;
+    }
+
+    private void validateUnit(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위어야 합니다.");
+        }
+    }
+
+    private void validateRange(int amount) {
+        if (amount < MIN_PURCHASE_AMOUNT || amount > MAX_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 %,d원 이상 %,d원 이하여야 합니다."
+                    .formatted(MIN_PURCHASE_AMOUNT, MAX_PURCHASE_AMOUNT));
+        }
     }
 
     public int decrease(int lottoPrice) {

3f1cd54b638e74f84134fcf6f9e5569a5a212426
huhdy32 <mong3268@gmail.com>
2023-11-05 20:30:58 +0900

    feat: 로또번호 중복, 범위 검사 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,9 +11,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6 || invalidRange(numbers) || isDuplicated(numbers)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean invalidRange(List<Integer> numbers) {
+        for (int i : numbers) {
+            if (i > 45 || i < 1) return true;
+        }
+        return false;
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        for (int i=0; i< numbers.size(); i++){
+            int number = numbers.get(i);
+            if (numbers.lastIndexOf(number) != i){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override

c0d3d9f1f8992f4ecd07365d20f61c27b3b84d61
권장순 <jsoonworld@gmail.com>
2023-11-06 05:50:02 +0900

    feat(Lotto) : Lotto#validate lotto 번호 size가 6이 아닐 때, 에러 메세지 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -12,9 +12,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }

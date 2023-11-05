4c0cbdb460fa36c2f764cbd5f35624f2200a1464
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 02:22:38 +0900

    feat: lottoservice 클래스 구현 및 생성, 기능 구현

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -1,2 +1,18 @@
-package lotto.model;public class LottoService {
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.controller.InputController;
+
+public class LottoService {
+
+    public int calculateTicketCount() {
+        return InputController.inputPlayerAmount();
+    }
+    public List<Lotto> createLottos(int ticketCount) {
+        return IntStream.range(0, ticketCount)
+                .mapToObj(i -> new Lotto(LottoNumbers.generateRandomNum()))
+                .collect(Collectors.toList());
+    }
 }

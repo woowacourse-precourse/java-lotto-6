fbf36294f231a3643366f489f243f9c57b2a8c34
minsun <elinnane0@gmail.com>
2023-11-05 15:04:35 +0900

    🚑 feat(Lotto) : 사용자 입력 오류 발생 후 다시 입력받도록 수정

--- src/main/java/lotto/LottoWin.java
+++ src/main/java/lotto/LottoWin.java
@@ -22,7 +22,33 @@ public class LottoWin {
     }
 
     public void setLottoWinNumbers() {
-        String input = Console.readLine();
+        while (true) {
+            try {
+                String input = Console.readLine();
+                List<Integer> numbers = splitNumbers(input);
+                winNumbers.getNumbers().addAll(numbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void setWinBonusNumber() {
+        while (true) {
+            try {
+                int input = Integer.parseInt(Console.readLine().trim());
+                validateNumberRange(input);
+                validateUniqueBonusNumber(input);
+                this.bonusNumber = input;
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    private List<Integer> splitNumbers(String input) {
         String[] numbersString = input.split(",\\s*"); // Split the input string by commas and optional whitespace
         if (numbersString.length != 6) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해야 합니다.");
@@ -31,29 +57,23 @@ public class LottoWin {
         List<Integer> numbers = new ArrayList<>();
         for (String number : numbersString) {
             int num = Integer.parseInt(number.trim());
-            if (num < 1 || num > 45) {
-                throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력해야 합니다.");
-            }
+            validateNumberRange(num);
             numbers.add(num);
         }
-    
-        winNumbers.getNumbers().addAll(numbers);
+        return numbers;
     }
     
-    
-
-    public void setWinBonusNumber() {
-        int input = Integer.parseInt(Console.readLine().trim());
-    
-        if (input < 1 || input > 45) {
+    private void validateNumberRange(int num) {
+        if (num < 1 || num > 45) {
             throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력해야 합니다.");
         }
+    }
     
+    private void validateUniqueBonusNumber(int input) {
         for (Integer number : winNumbers.getNumbers()) {
             if (number == input) {
                 throw new IllegalArgumentException("[ERROR] 이미 선택한 번호입니다.");
             }
         }
-        this.bonusNumber = input;
-    }    
+    }
 }

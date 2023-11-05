66a5e9a6fbd9b151d4f3fec740686b6251ada837
jmj005 <jmj00005@naver.com>
2023-11-06 01:39:27 +0900

    refactor: 보너스 번호 유효성 검증 메서드 이동

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -2,8 +2,21 @@ package lotto.model;
 
 public class BonusNumber {
     private final int bonusNumber;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
 
     public BonusNumber(int bonusNumber) {
+        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        if (isInvalidRange(bonusNumber)) {
+            throw new IllegalArgumentException("1~45 사이만 입력 가능합니다.");
+        }
+    }
+
+    private boolean isInvalidRange(int bonusNumber) {
+        return bonusNumber < MIN_RANGE || bonusNumber > MAX_RANGE;
     }
 }
--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -5,26 +5,14 @@ public class WinningLotto {
     private final BonusNumber bonusNumber;
 
     public WinningLotto(Lotto winningLotto, BonusNumber bonusNumber) {
-//        validateBonusNumberRange(bonusNumber);
-//        validateBonusNumberRange(bonusNumber);
-//        validateBonusNumberUnique(bonusNumber);
+//      validateBonusNumberUnique(bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-//    private void validateBonusNumberRange(int bonusNumber) {
-//        if (!isValidRange(bonusNumber)) {
-//            throw new IllegalArgumentException("보너스 번호는 1~45 사이만 입력 가능합니다.");
-//        }
-//    }
-//
 //    private void validateBonusNumberUnique(int bonusNumber) {
 //        if (winningLotto.contains(bonusNumber)) {
 //            throw new IllegalArgumentException("당첨 번호화 중복된 번호는 입력할 수 없습니다.");
 //        }
-//    }
-//
-//    private boolean isValidRange(int number) {
-//        return MIN_RANGE <= number && number <= MAX_RANGE;
 //    }
 }

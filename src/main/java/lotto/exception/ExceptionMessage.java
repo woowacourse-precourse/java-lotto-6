3e69cb12de89444dd18f131b1033e5562ca66186
devholic22 <hyunjoon.tech@gmail.com>
2023-11-06 03:38:50 +0900

    refactor: NumberFormatException 발생 시 예외 메시지 수정
    int 범위를 초과한 입력의 경우에도 NumberFormatException이 발생하기에 예외 메시지에 반영

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -2,7 +2,7 @@ package lotto.exception;
 
 public enum ExceptionMessage {
 
-    NUMBER_FORMAT_EXCEPTION("숫자여야 합니다."),
+    NUMBER_FORMAT_EXCEPTION("숫자가 아니거나 너무 큰 수, 또는 너무 작은 수를 입력하였습니다."),
     NEGATIVE_NUMBER_EXCEPTION("양수 (0 포함)여야 합니다."),
     NUMBER_DUPLICATE_EXCEPTION("숫자들은 모두 독립적이어야 합니다."),
     MONEY_REMAIN_EXCEPTION("구입 금액은 나누어 떨어져야 합니다."),

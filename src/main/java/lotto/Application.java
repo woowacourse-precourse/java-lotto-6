8340b83b2e1802dc84fa2a01f18529320dd832ae
hsyoodev <hsyoodev@gmail.com>
2023-11-05 23:29:21 +0900

    feat(lotto): 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해주세요.");
+        int price = Integer.parseInt(Console.readLine());
+        LottoGenerator generator = new LottoGenerator();
+        List<Lotto> lottos = generator.createLottoList(price);
     }
 }

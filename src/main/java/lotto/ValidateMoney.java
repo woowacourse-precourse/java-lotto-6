40e77a708136bd6435683fbef6e4db3ad1b41e8e
K-KY <kyuyoungk@naver.com>
2023-11-06 02:18:30 +0900

    Feat(ValidateMoney) : 입력된 금액을 검증

---
+++ src/main/java/lotto/ValidateMoney.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+
+public class ValidateMoney {
+    private final static int MAX_VALUE = 1000000;
+
+    public static ThrowingCallable isOverThousand(int money) {
+        if (money >= 1000) {
+            return null;
+        }
+        throw new IllegalArgumentException("[ERROR] 천보다 큰 금액을 입력해주세요.");
+    }
+
+
+    public static ThrowingCallable isUnitFollowed(int money) {
+        if (money % 1000 == 0) {
+            return null;
+        }
+        throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 입력 해주세요.");
+    }
+
+    public static ThrowingCallable isNumberInRange(int money) {
+        if (money < MAX_VALUE) {
+            return null;
+        }
+        throw new IllegalArgumentException("[ERROR] 100만을 넘는 금액은 입력 할 수 없습니다.");
+    }
+}

dabd8ab3ede043e60352ca63959d3ec5d4c28a55
cousim46 <wjdghldns24@naver.com>
2023-11-05 20:24:29 +0900

    fix : record 클래스 -> 일반 클래스로 변경

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -1,8 +1,33 @@
 package lotto;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public record Customer(Integer buyPrice, Map<Integer, List<Integer>> lottoNumbers) {
+public class Customer {
+    private final static Integer MIN_MONEY = 1000;
+    private final Integer buyPrice;
+    private final Map<Integer, List<Integer>> lottoNumbers = new LinkedHashMap<>();
+    private Integer winPrice;
+
+    private Customer(Integer buyPrice,Integer winPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public static Customer create(Integer buyPrice) {
+        if(buyPrice % MIN_MONEY != 0 || buyPrice == 0 ) {
+            throw new IllegalArgumentException();
+        }
+        return new Customer(buyPrice, 0);
+    }
+
+    public void putLottoNumber(int key, List<Integer> value) {
+        lottoNumbers.put(key, value);
+    }
+
+    public Map<Integer, List<Integer>> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
 
 }

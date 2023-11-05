379880b78ddfd13e5db7492c6134fd7ee426e53d
권장순 <jsoonworld@gmail.com>
2023-11-06 05:54:47 +0900

    refactoring(Lotto) : 에러 메시지 안내 수정 및 REAMDE에 반영 (로또 번호 -> 당첨 번호)

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -12,7 +12,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
         }
     }
 }

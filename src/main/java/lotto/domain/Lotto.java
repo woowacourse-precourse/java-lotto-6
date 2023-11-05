825aae1d0278b11601e690bb9db0fc126a89dd8b
hunko <rhwlgns4386@naver.com>
2023-11-05 18:07:08 +0900

    feat(Lotto): 범위 밖의 입력 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,11 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateSameItem(numbers);
+        validateInRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,8 +18,14 @@ public class Lotto {
         return Collections.unmodifiableList(numbers);
     }
 
+    private void validateInRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(i -> 1 <= i && i <= 45)){
+            throw new IllegalArgumentException("1~45사이의 값만 입력해주세요.");
+        }
+    }
+
     private void validateSameItem(List<Integer> numbers) {
-        if(numbers.stream().distinct().count()!=numbers.size()){
+        if (numbers.stream().distinct().count() != numbers.size()) {
             throw new IllegalArgumentException("중복된 값을 입력하지 마세요.");
         }
     }

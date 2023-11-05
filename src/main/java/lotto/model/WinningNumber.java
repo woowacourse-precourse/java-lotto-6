f80453fd7dd0f6139c0b9764e3c0430b1b1c9509
sangxxjin <sj_0368@naver.com>
2023-11-05 17:03:13 +0900

    feat: 담청 번호를 가지고 List로 변환해주고 중복 검사 및 범위 내의 값인지 검사 하고 예외 처리한다. 메시지는 상수처리

---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningNumber {
+    private List<Integer> winningNumber;
+    private static final String DUPLICATE_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호에 중복된 숫자가 포함되어 있습니다.";
+    private static final String OUT_OF_RANGE_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 1에서 45 사이의 숫자여야 합니다."
+
+    public WinningNumber(String winningNumbers) {
+        List<Integer> winningNumber = Arrays.stream(winningNumbers.split(",")).map(String::trim).map(Integer::parseInt)
+                .toList();
+        validateWinningNumbersForDuplicates(winningNumber);
+        validateWinningNumbersForRange(winningNumber);
+        this.winningNumber = winningNumber;
+    }
+
+    public List<Integer> getWinningNumber() {
+        return winningNumber;
+    }
+
+    private void validateWinningNumbersForDuplicates(List<Integer> winningNumber) {
+        List<Integer> uniqueNumbers = winningNumber.stream().distinct().toList();
+        if (winningNumber.size() != uniqueNumbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBERS_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateWinningNumbersForRange(List<Integer> winningNumber) {
+        if (winningNumber.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_WINNING_NUMBERS_ERROR_MESSAGE);
+        }
+    }
+
+}

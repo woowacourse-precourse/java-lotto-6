32f433af151dc785409840a0660650f6359f746e
Seonoo <tjsdn22@naver.com>
2023-11-05 19:19:56 +0900

    feat : 당첨번호 입력 기능 추가 1. 사용자가 당첨번호를 입력 시 Lotto 클래스를 활용 2. 로또의 범위 내에서만 입력이 가능하고, 문자열이 들어가면 안되고 빈 값도 들어가지 않도록 예외처리를 추가 3. 보너스 번호를 입력할 수 있도록 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -19,11 +19,20 @@ public class Application {
         Map<Integer, List<Integer>> userCountRandomLotto = generateAndDisplayNumbers(count);
         printGenerateAndDisplayNumbers(userCountRandomLotto);
 
-
-        List<Integer> inputWinningNumber = inputWinningNumber();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String numbers = readLine();
+        List<Integer> pickWinnerNumber = inputWinningNumberVerification(inputWinningNumber(numbers));
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = readLine();
+        List<Integer> bonusNum = inputBounsNumber(pickWinnerNumber, bonusNumber);
+        System.out.println("bonusNum = " + bonusNum);
 
 
     } // main
+
+    public static Integer convertStringToInt(String str) throws NumberFormatException {
+        return Integer.parseInt(str);
+    }
 
     public static List<Integer> pickUniqueNumber(){ // 복권의 랜덤한 값을 출력한 후 반환
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -32,7 +41,7 @@ public class Application {
     public static Integer inputMoney(String money){ // 사용자가 뽑고 싶은 만큼 돈 투입
         while (true) {
             try {
-                Integer userMoney = moneyDiscrimination(Integer.valueOf(money));
+                Integer userMoney = convertStringToInt(money);
                 return userMoney; // 유효한 입력이면 반환하고 메서드 종료
             } catch (NumberFormatException | NullPointerException e) {
                 System.out.println("[ERROR] 유효하지 않은 숫자입니다. 다시 시도하세요.");
@@ -42,6 +51,8 @@ public class Application {
             money = readLine(); // 새로운 입력을 받습니다.
         }
     } // inputMoney
+
+
 
     public static Integer moneyDiscrimination(Integer money){ // 입력한 돈이 맞게 들어왔는지 검증하는 메소드
         if (money % 1000 != 0) {
@@ -74,25 +85,82 @@ public class Application {
 
     } // printGenerateAndDisplayNumbers
 
-    public static List<Integer> inputWinningNumber(){ // 사용자가 입력하는 당첨금액을 콤마를 제거한 숫자로만 만들기
-
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String numbers = readLine();
-
-        String[] strNumbers = numbers.split(",");
+    public static List<Integer> inputWinningNumber(String numbers){ // 사용자가 입력하는 당첨금액을 콤마를 제거한 숫자로만 만들기
         List<Integer> intNumbers = new ArrayList<>();
 
-        for(String strNumber : strNumbers){
-            intNumbers.add(Integer.valueOf(strNumber));
+        while(true) {
+            String[] strNumbers = numbers.split(",");
+            boolean isValidInput = checkWinningNumberValidity(strNumbers, intNumbers);
+
+
+            if(isValidInput){
+                break;
+            }
+
+            numbers = readLine();
         }
 
         return intNumbers;
     } // inputWinningNumber
 
-    public static List<Integer> inputWinningNumberVerification(List<Integer> inputWinningNumber){ // 사용자가 입력하는 당첨금액이 맞게 입력했는지 검증하는 메소드
+    public static boolean checkWinningNumberValidity(String[] strNumbers, List<Integer> intNumbers){ // 당첨번호 예외처리
+        boolean isValidInput = true;
 
-         Lotto lotto = new Lotto(inputWinningNumber);
-         return lotto.getNumbers();
+        for (String strNumber : strNumbers) {
+            try {
+                intNumbers.add(convertStringToInt(strNumber.trim()));
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효하지 않은 숫자입니다. 다시 시도하세요.");
+                isValidInput = false;
+                break;
+            }
+        }
+
+        return isValidInput;
+    } // checkWinningNumberValidity
+
+    public static List<Integer> inputWinningNumberVerification(List<Integer> inputWinningNumber){ // 사용자가 입력하는 당첨금액이 맞게 입력했는지 검증하는 메소드
+        Lotto lotto = null;
+
+        while(true) {
+            try {
+                lotto = new Lotto(inputWinningNumber);
+                return lotto.getNumbers();
+            } catch (IllegalArgumentException | NullPointerException e) {
+                System.out.println("[ERROR] 1~45까지 중 6자리를 입력해주세요");
+            }
+
+            String str = readLine();
+            inputWinningNumber = inputWinningNumber(str);
+        }
+
     } // inputWinningNumberVerification
+
+    public static List<Integer> inputBounsNumber(List<Integer> winnerNumber, String bonusNumber){ // 보너스 번호 입력
+        while(true) {
+            try {
+                int bonusNum = convertStringToInt(bonusNumber);
+                if (!userInputNumber(bonusNum)) {
+                    throw new IllegalArgumentException();
+                }
+                winnerNumber.add(bonusNum);
+                return winnerNumber;
+            } catch (NumberFormatException | NullPointerException e) {
+                System.out.println("[ERROR] 유효하지 않은 숫자입니다. 다시 시도하세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 1~45까지 중 6자리를 입력해주세요");
+            }
+            String str = readLine();
+            bonusNumber = str;
+        }
+    } // inputBounsNumber
+    
+    public static boolean userInputNumber(Integer num){ // 당첨번호, 보너스 번호가 1 ~ 45만 입력했는지 검증 메소드
+
+        if(num <= 0 || num > 45){
+            return false;
+        }
+        return true;
+    } // userInputNumber
 
 }

ed8567e9ed22883bedb24c3bfcc29d83f97b18d1
Son JunHyung <kingjh1125@gmail.com>
2023-11-05 17:01:34 +0900

    test (LottoTest): 테스트 추가
    로또 번호 검증에 대한 테스트를 추가하였습니다:
    - 숫자 범위에 맞지 않는 숫자가 입력될 경우
    - 번호를 입력하지 않은 경우

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,22 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위에 맞지 않는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNumberRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호를 입력하지 않을 경우 예외가 발생한다.")
+    @Test
+    void createLottoByNull() {
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

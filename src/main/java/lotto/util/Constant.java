805a32f33d82f3eafa1365278ed9112b9e51cfa6
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-05 23:40:44 +0900

    Feat: 보너스 번호 입력 관련 상수 추가

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -3,6 +3,7 @@ package lotto.util;
 public class Constant {
     public static final String GET_PURCHASE_AMOUNT_TXT = "구입금액을 입력해 주세요.";
     public static final String GET_WINNING_NUMBER_TXT = "당첨 번호를 입력해 주세요.";
+    public static final String GET_BONUS_NUMBER_TXT = "보너스 번호를 입력해 주세요.";
 
     public static final String LOTTO_AMOUNT = "{0}개를 구매했습니다.";
     public static final String WINNING_STATISTICS = "{0}개 일치 ({1}원) - {2}개";

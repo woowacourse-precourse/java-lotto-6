75e841bd1adcbf39e83ba9b40c4099d618c5369a
doha <dohaahi@naver.com>
2023-11-05 22:24:12 +0900

    feat (OutputView): 당첨 결과 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,12 +2,19 @@ package lotto.view;
 
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lotto.domain.Rank;
+import lotto.domain.dto.DrawingResultDto;
 import lotto.domain.dto.LottoDto;
 import lotto.domain.dto.LottosDto;
 import lotto.domain.dto.PurchaseAmountDto;
 
 public class OutputView {
     public static final String LOTTO_PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
+    public static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---";
+    public static final String DRAW_RESULT_EXCLUDING_SECOND_MESSAGE = "%d개 일치, (%d원) - %d개\n";
+    public static final String DRAWING_RESULT_FOR_SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개\n";
 
     public void printPurchaseQuantityLottos(final PurchaseAmountDto purchaseAmountDto) {
         System.out.printf("\n" + LOTTO_PURCHASE_RESULT_MESSAGE, purchaseAmountDto.purchaseQuantityLottos());
@@ -24,5 +31,34 @@ public class OutputView {
 
     private String convertFrom(final LottoDto lottoDto) {
         return String.join(", ", String.valueOf(lottoDto.numbers()));
+    }
+
+    public void printDrawingResult(final DrawingResultDto drawingResultDto) {
+        Map<Rank, Integer> results = drawingResultDto.drawingResults();
+
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+
+        for (Entry<Rank, Integer> rankIntegerEntry : results.entrySet()) {
+            if (rankIntegerEntry.getKey() == Rank.NONE) {
+                continue;
+            }
+
+            if (rankIntegerEntry.getKey() == Rank.SECOND) {
+                printSecondRankResult(rankIntegerEntry);
+                continue;
+            }
+
+            printNonSecondRankResults(rankIntegerEntry);
+        }
+    }
+
+    private void printNonSecondRankResults(Entry<Rank, Integer> rankIntegerEntry) {
+        System.out.printf(DRAW_RESULT_EXCLUDING_SECOND_MESSAGE, rankIntegerEntry.getKey().getMatchNumberCount(),
+                rankIntegerEntry.getKey().getWinningAmount(), rankIntegerEntry.getValue());
+    }
+
+    private void printSecondRankResult(Entry<Rank, Integer> rankIntegerEntry) {
+        System.out.printf(DRAWING_RESULT_FOR_SECOND_MESSAGE, rankIntegerEntry.getKey().getMatchNumberCount(),
+                rankIntegerEntry.getKey().getWinningAmount(), rankIntegerEntry.getValue());
     }
 }

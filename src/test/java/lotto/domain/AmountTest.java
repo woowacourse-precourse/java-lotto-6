7b32f938af7e096fb198be588cc4d572fe55d04b
gundorit <packdev937@gmail.com>
2023-11-05 13:16:45 +0900

    feat (Amount) : 금액이 1000원으로 나누어 떨어지는지 테스트 한다

---
+++ src/test/java/lotto/domain/AmountTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Amount 클래스")
+class AmountTest {
+
+    @DisplayName("금액이 1000원으로 나누어 떨어지지 않는다면 예외를 발생한다.")
+    @Test
+    void createAmountByNumber() {
+        assertThatThrownBy(() -> new Amount(1001))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}

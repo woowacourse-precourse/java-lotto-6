54f5e9f28661c0410899953b29d4fd9408607dab
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-06 07:51:07 +0900

    feat: 로또 당첨번호 입력 기능 완성 1. 유저로부터 로또 당첨 번호 입력 기능 완성 2. 입력 값에 대한 예외처리 기능 완성 3. 예외 발생 시 재실행 기능 완성

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class View {
 
@@ -33,13 +36,7 @@ public class View {
                 System.out.print("구매 금액을 입력해 주세요: ");
                 String input = Console.readLine();
                 int amount = Integer.parseInt(input);
-
-                if (amount <= 0) {
-                    throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE.name());
-                }
-                if (amount < 1000) {
-                    throw new IllegalArgumentException(ErrorMessage.BELOW_MINIMUM_AMOUNT.name());
-                }
+                validateAmount(amount);
                 return amount;
             } catch (NumberFormatException e) {
                 printErrorMessage(ErrorMessage.INVALID_NUMBER);
@@ -48,8 +45,79 @@ public class View {
             }
         }
     }
+    public static Set<Integer> getWinningNumbers() {
+        while (true) {
+            try {
+                System.out.print("당첨 번호를 입력해 주세요 (쉼표로 구분): ");
+                String input = Console.readLine();
+                return parseWinningNumbers(input);
+            } catch (IllegalArgumentException e) {
+                printErrorMessage(ErrorMessage.valueOf(e.getMessage()));
+            }
+        }
+    }
+
+    private static Set<Integer> parseWinningNumbers(String input) {
+        if (!input.matches("(\\d+,){5}\\d+")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT.name());
+        }
+
+        String[] numberStrings = input.split(",");
+        if (numberStrings.length != 6) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_NUMBER_COUNT.name());
+        }
+
+        Set<Integer> numbers = new HashSet<>();
+        for (String numberString : numberStrings) {
+            int number = Integer.parseInt(numberString);
+            validateWinningNumber(number);
+            if (!numbers.add(number)) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.name());
+            }
+        }
+        return numbers;
+    }
+
+    private static void validateWinningNumber(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE.name());
+        }
+    }
+
+    public static int getBonusNumber(Set<Integer> winningNumbers) {
+        while (true) {
+            try {
+                System.out.print("보너스 번호를 입력해 주세요: ");
+                String input = Console.readLine();
+                int bonusNumber = Integer.parseInt(input);
+                validateBonusNumber(bonusNumber, winningNumbers);
+                return bonusNumber;
+            } catch (NumberFormatException e) {
+                printErrorMessage(ErrorMessage.INVALID_RANGE);
+            } catch (IllegalArgumentException e) {
+                printErrorMessage(ErrorMessage.valueOf(e.getMessage()));
+            }
+        }
+    }
+
+    private static void validateBonusNumber(int number, Set<Integer> winningNumbers) {
+        validateWinningNumber(number); // Reuse the validation for winning numbers
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.name());
+        }
+    }
+
+    private static void validateAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE.name());
+        }
+        if (amount < 1000) { // 로또 가격이 1000원이라고 가정
+            throw new IllegalArgumentException(ErrorMessage.BELOW_MINIMUM_AMOUNT.name());
+        }
+    }
 
     private static void printErrorMessage(ErrorMessage errorMessage) {
         System.out.println(ERROR_PREFIX + errorMessage.getMessage());
     }
+
 }

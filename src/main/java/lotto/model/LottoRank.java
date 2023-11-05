85a78be1612f3da0467fbad21e5c306631763ca3
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-06 01:20:26 +0900

    refactor(model): 당첨 통계 메시지 및 getter 구현

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -3,21 +3,23 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    NONE(0, false, 0, "0개 일치 (0원)"),
+    FIFTH(3, false, 5_000, "3개 일치 (5,000원)"),
+    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)");
 
     private final int matchCount;
     private final boolean bonusNumberExists;
     private final int prize;
+    private final String message;
 
-    LottoRank(int matchCount, boolean bonusNumberExists, int prize) {
+    LottoRank(int matchCount, boolean bonusNumberExists, int prize, String message) {
         this.matchCount = matchCount;
         this.bonusNumberExists = bonusNumberExists;
         this.prize = prize;
+        this.message = message;
     }
 
     public static LottoRank findRankByMatchCount(int matchCount, boolean bonusNumberExists) {
@@ -26,5 +28,17 @@ public enum LottoRank {
                 .filter(lottoRank -> lottoRank.bonusNumberExists == bonusNumberExists)
                 .findAny()
                 .orElse(NONE);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }

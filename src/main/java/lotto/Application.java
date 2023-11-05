4ef5678ea546ee02496e4f3e8d6251f162b3bd6c
jifrozen0110 <answldjs1836@gmail.com>
2023-11-06 02:51:22 +0900

    refactor(controller) : LottoGameController 메서드 분리
    * refactor(controller) : LottoGameController 의존성 관리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import lotto.controller.LottoGameController;
+import lotto.domain.LottoCreator;
+import lotto.domain.LottoNumberGenerator;
+import lotto.domain.NumberGenerator;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumberGenerator numberGenerator = new LottoNumberGenerator();
+        LottoCreator lottoCreator = new LottoCreator(numberGenerator);
         try {
-            LottoGameController lottoGameController = new LottoGameController();
+            LottoGameController lottoGameController = new LottoGameController(lottoCreator, numberGenerator);
             lottoGameController.start();
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -5,57 +5,71 @@ import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.domain.LottoComparator;
 import lotto.domain.LottoCreator;
-import lotto.domain.LottoNumberGenerator;
 import lotto.domain.Lottos;
 import lotto.domain.LottosResult;
 import lotto.domain.Money;
+import lotto.domain.NumberGenerator;
 import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
 
+    private final LottoCreator lottoCreator;
+    private final NumberGenerator numberGenerator;
+
+    public LottoGameController(LottoCreator lottoCreator, NumberGenerator numberGenerator) {
+        this.lottoCreator = lottoCreator;
+        this.numberGenerator = numberGenerator;
+    }
+
     public void start() {
         int moneyAmount = askMoney();
-        int lottoCount = getLottoCount(moneyAmount);
-        Lottos lottos = getLottos(lottoCount);
-        WinningLotto winningLotto = getWinningLotto();
+        int lottoCount = calculateLottoCount(moneyAmount);
+        Lottos lottos = generateLottos(lottoCount);
+        WinningLotto winningLotto = createWinningLotto();
         printLottosResult(moneyAmount, lottos, winningLotto);
     }
 
-    private static void printLottosResult(int moneyAmount, Lottos lottos, WinningLotto winningLotto) {
+    private void printLottosResult(int moneyAmount, Lottos lottos, WinningLotto winningLotto) {
         LottoComparator lottoComparator = new LottoComparator(lottos, winningLotto);
         LottosResult lottosResult = lottoComparator.compareLottoAndWinningLotto();
         lottosResult.calculateRateOfReturn(moneyAmount);
         OutputView.print(lottosResult.toString());
     }
 
-    private static WinningLotto getWinningLotto() {
-        OutputView.printWinningNumbersInputMessage();
-        int[] winningNumbers = InputView.nextIntArray();
-        Lotto winningNumber = new Lotto(Arrays.stream(winningNumbers).boxed().collect(Collectors.toList()));
-        OutputView.printBonusNumberInputMessage();
-        int bonusNumber = InputView.nextInt();
+    private WinningLotto createWinningLotto() {
+        Lotto winningNumber = inputWinningNumbers();
+        int bonusNumber = inputBonusNumber();
         WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
         return winningLotto;
     }
 
-    private static Lottos getLottos(int lottoCount) {
-        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        LottoCreator lottoCreator = new LottoCreator(lottoNumberGenerator);
+    private Lotto inputWinningNumbers() {
+        OutputView.printWinningNumbersInputMessage();
+        int[] winningNumbers = InputView.nextIntArray();
+        return new Lotto(Arrays.stream(winningNumbers).boxed().collect(Collectors.toList()));
+    }
+
+    private int inputBonusNumber() {
+        OutputView.printBonusNumberInputMessage();
+        return InputView.nextInt();
+    }
+
+    private Lottos generateLottos(int lottoCount) {
         Lottos lottos = lottoCreator.createLottos(lottoCount);
         OutputView.printLottos(lottos);
         return lottos;
     }
 
-    private static int getLottoCount(int moneyAmount) {
+    private int calculateLottoCount(int moneyAmount) {
         Money money = new Money(moneyAmount);
         int lottoCount = money.getDivideValue();
         OutputView.printBuyInfoMessage(lottoCount);
         return lottoCount;
     }
 
-    private static int askMoney() {
+    private int askMoney() {
         OutputView.printMoneyInputMessage();
         int moneyAmount = InputView.nextInt();
         return moneyAmount;

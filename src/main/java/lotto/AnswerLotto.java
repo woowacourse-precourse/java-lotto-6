a1f89bd0417305c12c1223b92ce77d36c770b37a
jhon3242 <rldotnr2@gmail.com>
2023-11-05 12:40:43 +0900

    test : 정답 로또 실패 테스트 추가

---
+++ src/main/java/lotto/AnswerLotto.java
@@ -0,0 +1,4 @@
+package lotto;
+
+public record AnswerLotto(Lotto lotto, int bonus) {
+}
---
+++ src/test/java/lotto/AnswerLottoTest.java
@@ -0,0 +1,60 @@
+package lotto;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class AnswerLottoTest {
+    @DisplayName("정상 정답 로또인 경우 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @MethodSource("validAnswerLottoProvider")
+    void createAnswerLottoWithValid(Lotto lotto, int bonus) {
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> new AnswerLotto(lotto, bonus));
+    }
+
+    static Stream<Arguments> validAnswerLottoProvider() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(20, 39, 22, 40, 17, 32)), 13),
+                Arguments.of(new Lotto(List.of(45, 1, 26, 12, 21, 8)), 4),
+                Arguments.of(new Lotto(List.of(7, 37, 19, 43, 5, 29)), 35)
+        );
+    }
+
+
+    @DisplayName("보너스 숫자가 1~45 숫자가 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidBonusProvider")
+    void createAnswerLottoWithInvalidBonus(Lotto lotto, int bonus) {
+        Assertions.assertThatThrownBy(() -> new AnswerLotto(lotto, bonus))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> invalidBonusProvider() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(12, 31, 24, 9, 37, 4)), 0),
+                Arguments.of(new Lotto(List.of(5, 18, 27, 42, 11, 38)), 46),
+                Arguments.of(new Lotto(List.of(21, 8, 33, 10, 15, 30)), -2)
+        );
+    }
+
+    @DisplayName("로또 숫자와 보너스 숫자가 겹치는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("duplicateNumberProvider")
+    void createAnswerLottoWithDuplicate(Lotto lotto, int bonus) {
+        Assertions.assertThatThrownBy(() -> new AnswerLotto(lotto, bonus))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> duplicateNumberProvider() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(2, 43, 6, 29, 19, 34)), 6),
+                Arguments.of(new Lotto(List.of(7, 28, 14, 44, 23, 3)), 3),
+                Arguments.of(new Lotto(List.of(35, 16, 41, 25, 36, 13)), 35)
+        );
+    }
+}

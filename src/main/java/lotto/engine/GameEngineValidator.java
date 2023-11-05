6f5971ad719fc2e855262ef6e892ec120ae01185
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-11-05 20:16:01 +0900

    feat(GameEngineValidator) : 로또 번호를 검증한다.

--- src/main/java/lotto/engine/GameEngineValidator.java
+++ src/main/java/lotto/engine/GameEngineValidator.java
@@ -1,11 +1,21 @@
 package lotto.engine;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GameEngineValidator {
+    public static final String NOT_LOTTO_NUMBER_EXCEPTION_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final int LOTTO_NUMBER_MIN_VALUE = 1;
+    public static final int LOTTO_NUMBER_MAX_VALUE = 45;
+    public static final String DUPLICATE_LOTTO_NUMBER_EXCEPTION_MESSAGE = "로또번호는 중복값을 가지고 있으면 안됩니다.";
+    public static final String LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE = "로또 넘버개수는 %s가 아니라 %s 여야합니다.";
+    public static final String NOT_NULL_LOTTO_NUMBERS_EXCEPTION_MESSAGE = "로또 넘버는 null이 될수 없습니다.";
+    private static final int LOTTO_NUMBER_SIZE = 6;
     private static final String NOT_NUMBER_EXCEPTION_MESSAGE = "%s는 숫자가 아닙니다.";
     private static final String NOT_POSITIVE_NUMBER_EXCEPTION_MESSAGE = "%s는 양수가 아닙니다.";
+    private static final Long ZERO = 0L;
     private static final String NOT_THOUSAND_RATIO_NUMBER_EXCEPTION_MESSAGE = "%s는 천의 배수가 아닙니다.";
     private static final Long THOUSAND = 1_000L;
-    private static final Long ZERO = 0L;
 
     void checkPrice(String price) {
         checkIsNumber(price);
@@ -30,6 +40,53 @@ public class GameEngineValidator {
     private void checkIsThousandRatio(Long number) {
         if (number % THOUSAND != 0) {
             throw new IllegalArgumentException(String.format(NOT_THOUSAND_RATIO_NUMBER_EXCEPTION_MESSAGE, number));
+        }
+    }
+
+    void checkReadLineLottoNumbers(String readLine) {
+        checkIsNotNull(readLine);
+        List<String> lottoNumbers = Arrays.stream(readLine.split(",")).toList();
+        for (String lottoNumber : lottoNumbers) {
+            checkIsNumber(lottoNumber);
+        }
+        checkLottoNumbers(lottoNumbers.stream().map(Long::parseLong).toList());
+    }
+
+    void checkLottoNumbers(List<? extends Number> lottoNumbers) {
+        checkIsNotNull(lottoNumbers);
+        checkLottoNumbersSize(lottoNumbers);
+        checkLottoNumbersDuplicate(lottoNumbers);
+        checkIsLottoNumberRange(lottoNumbers);
+    }
+
+    private void checkIsNotNull(Object lottoNumbers) {
+        if (lottoNumbers == null) {
+            throw new IllegalArgumentException(NOT_NULL_LOTTO_NUMBERS_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void checkIsLottoNumberRange(List<? extends Number> lottoNumbers) {
+        for (Number lottoNumber : lottoNumbers) {
+            if (lottoNumber.longValue() < LOTTO_NUMBER_MIN_VALUE || lottoNumber.longValue() > LOTTO_NUMBER_MAX_VALUE) {
+                throw new IllegalArgumentException(NOT_LOTTO_NUMBER_EXCEPTION_MESSAGE);
+            }
+        }
+    }
+
+    private <T> void checkLottoNumbersDuplicate(List<T> lottoNumbers) {
+        int originalSize = lottoNumbers.size();
+        int duplicateDeleteSize = lottoNumbers.stream()
+                .distinct()
+                .toList().size();
+        if (originalSize != duplicateDeleteSize) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private <T> void checkLottoNumbersSize(List<T> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(String.format(LOTTO_NUMBER_SIZE_EXCEPTION_MESSAGE, lottoNumbers.size(),
+                    LOTTO_NUMBER_SIZE));
         }
     }
 }
--- src/test/java/lotto/engine/GameEngineValidatorTest.java
+++ src/test/java/lotto/engine/GameEngineValidatorTest.java
@@ -1,8 +1,13 @@
 package lotto.engine;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -13,11 +18,61 @@ class GameEngineValidatorTest {
         return new String[]{null, "", " ", "1q", "qwe", "1o", "★", "!", "Q", "*", "+"};
     }
 
+    private static Stream<Arguments> 로또번호_숫자가_아닌경우() {
+        return Stream.of(
+                Arguments.of("", ""),
+                Arguments.of(" ", " "),
+                Arguments.of("1q", "1q"),
+                Arguments.of("qwe", "qwe"),
+                Arguments.of("★", "★"),
+                Arguments.of("1o", "1o"),
+                Arguments.of("!", "!"),
+                Arguments.of("Q", "Q"),
+                Arguments.of("*", "*"),
+                Arguments.of("+", "+"),
+                Arguments.of("1,23,q,2,3,5", "q")
+        );
+    }
+
+    private static Stream<List<Integer>> 여섯개의_사이즈_아닌_배열() {
+        return Stream.of(
+                List.of(1),
+                List.of(1, 2),
+                List.of(1, 2, 3),
+                List.of(1, 2, 3, 4),
+                List.of(1, 2, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5, 6, 7)
+        );
+    }
+
+    private static Stream<List<Integer>> 로또숫자가6개인데_중복이있는경우() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 1),
+                List.of(1, 2, 3, 4, 2, 1),
+                List.of(1, 2, 2, 2, 2, 1),
+                List.of(1, 2, 3, 4, 4, 3)
+        );
+    }
+
+    private static Stream<List<Integer>> 로또숫자가_1부터_45사이가_아닌_경우() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 46),
+                List.of(0, 2, 3, 4, 5, 45)
+        );
+    }
+
+    private static Stream<List<Integer>> 로또숫자가_중복되지않고_6개_1부터_45사이인_경우() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 45),
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(40, 41, 42, 43, 44, 45)
+        );
+    }
+
     @BeforeEach
     void setUp() {
         gameEngineValidator = new GameEngineValidator();
     }
-
 
     @ParameterizedTest
     @MethodSource("숫자가_아닌경우")
@@ -49,4 +104,93 @@ class GameEngineValidatorTest {
         Assertions.assertThatCode(() -> gameEngineValidator.checkPrice(price))
                 .doesNotThrowAnyException();
     }
+
+    @Test
+    void 입력한_로또넘버가_null이면_예외가나온다() {
+        Assertions.assertThatCode(() -> gameEngineValidator.checkReadLineLottoNumbers(null))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 넘버는 null이 될수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또번호_숫자가_아닌경우")
+    void 입력한_로또넘버가_숫자가_아니면_예외가나온다(String readLine, String exceptionValue) {
+        Assertions.assertThatCode(() -> gameEngineValidator.checkReadLineLottoNumbers(readLine))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("%s는 숫자가 아닙니다.", exceptionValue));
+    }
+
+    @ParameterizedTest
+    @MethodSource("여섯개의_사이즈_아닌_배열")
+    void 입력한_로또넘버사이즈가_6가_아니면_예외가나온다(List<Integer> lottoNumbers) {
+        String readLine = lottoNumbers.stream()
+                .map(x -> Integer.toString(x))
+                .collect(Collectors.joining(","));
+        Assertions.assertThatCode(() -> gameEngineValidator.checkReadLineLottoNumbers(readLine))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("로또 넘버개수는 %s가 아니라 %s 여야합니다.", lottoNumbers.size(), 6));
+    }
+
+    @ParameterizedTest
+    @MethodSource({"로또숫자가6개인데_중복이있는경우"})
+    void 입력한_로또숫자가_중복이_있는경우면_예외가나온다(List<Integer> lottoNumbers) {
+        String readLine = lottoNumbers.stream()
+                .map(x -> Integer.toString(x))
+                .collect(Collectors.joining(","));
+        Assertions.assertThatCode(() -> gameEngineValidator.checkReadLineLottoNumbers(readLine))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또번호는 중복값을 가지고 있으면 안됩니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또숫자가_1부터_45사이가_아닌_경우")
+    void 압력한_로또숫자가_1부터_45사이가_아니면_예외가나온다(List<Integer> lottoNumbers) {
+        String readLine = lottoNumbers.stream()
+                .map(x -> Integer.toString(x))
+                .collect(Collectors.joining(","));
+        Assertions.assertThatCode(() -> gameEngineValidator.checkReadLineLottoNumbers(readLine))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또숫자가_중복되지않고_6개_1부터_45사이인_경우")
+    void 입력한_로또숫자가_중복되지않고_6개_1부터_45사이면_예외가_나오지않는다(List<Integer> lottoNumbers) {
+        String readLine = lottoNumbers.stream()
+                .map(x -> Integer.toString(x))
+                .collect(Collectors.joining(","));
+        Assertions.assertThatCode(() -> gameEngineValidator.checkReadLineLottoNumbers(readLine))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 로또넘버가_null이면_예외가나온다() {
+        Assertions.assertThatCode(() -> gameEngineValidator.checkLottoNumbers(null))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 넘버는 null이 될수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource({"로또숫자가6개인데_중복이있는경우"})
+    void 로또숫자가_중복이_있는경우면_예외가나온다(List<Integer> lottoNumbers) {
+        Assertions.assertThatCode(() -> gameEngineValidator.checkLottoNumbers(lottoNumbers))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("로또번호는 중복값을 가지고 있으면 안됩니다.", lottoNumbers.size(), 6));
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또숫자가_1부터_45사이가_아닌_경우")
+    void 로또숫자가_1부터_45사이가_아니면_예외가나온다(List<Integer> lottoNumbers) {
+        Assertions.assertThatCode(() -> gameEngineValidator.checkLottoNumbers(lottoNumbers))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또숫자가_중복되지않고_6개_1부터_45사이인_경우")
+    void 로또숫자가_중복되지않고_6개_1부터_45사이면_예외가_나오지않는다(List<Integer> lottoNumbers) {
+        Assertions.assertThatCode(() -> gameEngineValidator.checkLottoNumbers(lottoNumbers))
+                .doesNotThrowAnyException();
+    }
+
 }

a59a152802ba9d220d864628d62fa71bebb3f6c8
bellring <starringbell@gmail.com>
2023-11-05 15:27:27 +0900

    refactor(InputMoney): 별도의 enum으로 error정의

--- src/main/java/lotto/InputMoney.java
+++ src/main/java/lotto/InputMoney.java
@@ -6,19 +6,19 @@ public class InputMoney {
 	public InputMoney(String userInput) {
 		validateInputIsNumber(userInput);
 		Integer inputMoney = convertStringToInteger(userInput);
-		validate(inputMoney);
+		validateInputUnit(inputMoney);
 		this.money = inputMoney;
 	}
 
-	private void validate(Integer money) {
+	private void validateInputUnit(Integer money) {
 		if (money % 1000 != 0) {
-			throw new IllegalArgumentException("[ERROR] 급액을 천원 단위로 입력해 주세요.");
+			throw new IllegalArgumentException(PriceInputError.INVALID_UNIT_INPUT.getErrorDescription());
 		}
 	}
 
 	private void validateInputIsNumber(String input) {
 		if (!Character.isDigit(input.charAt(0))) {
-			throw new IllegalArgumentException("[ERROR] 금액으로 숫자를 입력해 주세요.");
+			throw new IllegalArgumentException(PriceInputError.INVALID_TYPE_INPUT.getErrorDescription());
 		}
 	}
 

9329aa41b413c215bfeb8f95aa7610d08e1f810a
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-05 23:27:47 +0900

    🎯feat(view/InputParser) : 당첨번호 string -> List<Integer> 변환 기능 구현

--- src/main/java/lotto/view/InputParser.java
+++ src/main/java/lotto/view/InputParser.java
@@ -1,8 +1,19 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class InputParser {
     // 구입금액 string > double (int 범위 넘어설 경우 대비)
     public double convertPurchaseAmount(String amount) {
         return Double.parseDouble(amount);
+    }
+
+    // 담첨번호 String > List<Integer>
+    public List<Integer> convertWinningNumbers(String numbers) {
+        return Stream.of(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

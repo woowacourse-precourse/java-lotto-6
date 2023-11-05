1549e96b0b04168b774c774c9ac720c15224dc60
jihyuk <kwlgur0416@gmail.com>
2023-11-06 02:19:51 +0900

    test : 로또 도메인 규칙 테스트 작성
    - 로또 번호 개수 예외 테스트
    - 로또 번호 중복 예외 테스트
    - 로또 번호 범위 예외 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,27 +1,54 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
+import static lotto.Constants.MAX_RANDOM_NUMBER;
+import static lotto.Constants.MIN_RANDOM_NUMBER;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.model.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
+
+    @DisplayName("로또 번호의 개수가 6개가 아니라면 예외가 발생한다")
+    @ParameterizedTest
+    @MethodSource("generateOutOfSize")
+    void createLottoOutOfSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1에서 45 사이에서 벗어난 수가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("generateOutOfRange")
+    void createLottoByOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<List<Integer>> generateOutOfSize() {
+        return Stream.of(
+            List.of(1, 2, 3, 4, 5),
+            List.of(1, 2, 3, 4, 5, 6, 7)
+        );
+    }
+
+    private static Stream<List<Integer>> generateOutOfRange() {
+        return Stream.of(
+            List.of(1, 2, 3, 4, 5, MIN_RANDOM_NUMBER - 1),
+            List.of(1, 2, 3, 4, 5, MAX_RANDOM_NUMBER + 1)
+        );
+    }
+    
 }

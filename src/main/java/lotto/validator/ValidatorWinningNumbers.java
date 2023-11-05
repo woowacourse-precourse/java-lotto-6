4586cad46b1c2ab2245808892e328a49386ee145
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-05 17:35:25 +0900

    feat: 당첨번호 입력값 중복 검사

--- src/main/java/lotto/validator/ValidatorWinningNumbers.java
+++ src/main/java/lotto/validator/ValidatorWinningNumbers.java
@@ -1,7 +1,15 @@
 package lotto.validator;
 
+import java.util.HashSet;
+import java.util.Set;
+import lotto.model.WinningNumbers;
+
 public class ValidatorWinningNumbers {
     public ValidatorWinningNumbers() {
     }
 
+    private boolean checkForDuplicates(WinningNumbers winningNumbers) {
+        Set<Integer> numbers = new HashSet<>(winningNumbers.getWinningNumbers());
+        return numbers.size() == winningNumbers.getWinningNumbers().size();
+    }
 }

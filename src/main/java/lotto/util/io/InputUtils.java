2296e25aa8ffcdb61c910631bcd578e307ba74d7
chaeshee0908 <chaeshee0908@naver.com>
2023-11-05 19:57:08 +0900

    [refact] 당첨 번호 유효성 검사 함수 로직 분리(15줄 이내)

--- src/main/java/lotto/util/io/InputUtils.java
+++ src/main/java/lotto/util/io/InputUtils.java
@@ -48,18 +48,22 @@ public class InputUtils {
 
     private static void validateInputWinningNumbers(List<String> numbers) {
         for (String number : numbers) {
-            if (!isNumeric(number)) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호 모두 숫자를 입력해야 합니다.");
-            }
-            if (!isNumberInRange(Integer.parseInt(number))) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
+            validateInputWinningNumber(number);
         }
         if (numbers.size() != COUNT_OF_WINNING_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개이어야 합니다.");
         }
         if (hasDuplicates(numbers)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복된 숫자가 없어야 합니다.");
+        }
+    }
+
+    private static void validateInputWinningNumber(String number) {
+        if (!isNumeric(number)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 모두 숫자를 입력해야 합니다.");
+        }
+        if (!isNumberInRange(Integer.parseInt(number))) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 

137096913ece21d68866e5667e4aba61a8334f3d
insu <is23193160@gmail.com>
2023-11-05 17:30:07 +0900

    refactor: 사용하지 않는 상수 제거

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -1,7 +1,6 @@
 package lotto.model;
 
 public class PurchaseAmount {
-    private static final String NON_NUMERIC_INPUT_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
     private static final int LOTTO_PRICE = 1000;
     private final int purchaseAmount;
 

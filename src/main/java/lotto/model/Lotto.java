26d1ff2bad3a8fb68d91ff2db7c0ab71e7145109
hangillee <skfcb10@naver.com>
2023-11-06 01:58:58 +0900

    feat(model): Add new features for Lotto model
    Add new features for Lotto model:
    - 6개의 숫자를 리스트로 받아 자신의 로또 번호로 저장한다.
    - 해당 로또의 당첨 등수를 판별하고 상금 정보를 반환한다.
    - 해당 로또가 저장한 번호와 당첨 번호가 일치하는 개수를 반환한다.
    - 해당 로또가 저장한 번호와 보너스 번호가 일치하는지 판별한다.
    - 로또 번호를 문자열화 한다.
    - 로또 번호가 6개가 아닐 경우 예외가 발생한다.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import lotto.enums.Prize;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +20,27 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public int getPrize(List<Integer> winningNumbers, int bonusNumber) {
+        int winCount = checkWinCount(winningNumbers);
+        boolean isSecondWinner = checkBonusNumber(bonusNumber);
+        return Prize.getPrize(winCount, isSecondWinner);
+    }
+
+    private int checkWinCount(List<Integer> winningNumbers) {
+        long winCount = numbers.stream()
+                .filter(number -> winningNumbers.stream()
+                        .anyMatch(Predicate.isEqual(number)))
+                .count();
+        return Math.toIntExact(winCount);
+    }
+
+    private boolean checkBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+    public String toString() {
+        return numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
 }

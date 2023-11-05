a3f1515688b7352959aee5636d397c980f86264a
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-05 23:19:26 +0900

    refactor: 당첨번호 관련 에러메시지 등록

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -9,9 +9,13 @@ public enum ErrorMessage {
 
     // 로또 관련 에러
     LOTTO_NUMBER_COUNT_INVALID_ERROR("[ERROR] 로또 번호는 6개의 숫자여야 합니다."),
-    LOTTO_NUMBER_RANGE_INVALID_ERROR("[ERROR] 로또 번호는 1~45 사이여야 합니다.."),
-    LOTTO_NUMBER_DUPLICATION_ERROR("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    LOTTO_NUMBER_RANGE_INVALID_ERROR("[ERROR] 로또 번호는 1부터 45 사이여야 합니다."),
+    LOTTO_NUMBER_DUPLICATION_ERROR("[ERROR] 로또 번호는 중복될 수 없습니다."),
 
+    // 당첨번호 관련 에러
+    WINNING_NUMBER_COUNT_INVALID_ERROR("[ERROR] 당첨 번호는 6개여야 합니다."),
+    WINNING_NUMBER_RANGE_INVALID_ERROR("[ERROR] 당첨 번호는 1부터 45 사이어야 합니다."),
+    WINNING_NUMBER_DUPLICATION_ERROR("[ERROR] 당첨 번호는 중복될 수 없습니다.");
 
     private final String message;
 

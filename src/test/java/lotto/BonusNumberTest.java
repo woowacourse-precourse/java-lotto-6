217a06adf6c8157485a9eab662232d2b6ab0924a
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 01:00:12 +0900

    test: 보너스 숫자에 대한 테스트코드 작성

---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.model.BonusNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BonusNumberTest {
+    @Test
+    public void 보너스_숫자가_범위를_벗어났을_때_테스트() {
+        int bonusNumber = 500;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new BonusNumber(bonusNumber));
+    }
+
+    @Test
+    public void 유효한_보너스_숫자_테스트() {
+        int bonusNumber = 7;
+        Assertions.assertDoesNotThrow(() -> new BonusNumber(bonusNumber));
+    }
+}

1534459c1b72f0d8079b0f0d47d805dc5ad0d02a
GO-TE <alsrudxo9810@naver.com>
2023-11-06 01:07:14 +0900

    fix(InputView): 구입 금액 입력 조건 추가 inputBudget() : - 양의 정수만 받는 조건 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
         String budget = Console.readLine();
         try {
             intBudget = Integer.parseInt(budget);
-            if (intBudget % 1000 != 0) { // 하드코딩 제거
+            if (intBudget % 1000 != 0 && intBudget > 0) { // 하드코딩 제거
                 throw new IllegalArgumentException("1000원 단위 정수로 입력해 주세요."); // 하드코딩 제거
             }
         } catch (NumberFormatException exception) {

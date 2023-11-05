bcd6ffe1b1f3774ed9cc2df272db6f5d06358400
metishonora <metishonora@icloud.com>
2023-11-05 11:18:55 +0900

    test: 구입 금액 입력받기 테스트 생성

---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import lotto.view.InputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoControllerTest {
+    @DisplayName("숫자가 아닌 구입 금액")
+    @Test
+    void requestInteger() {
+        // given
+        InputView inputView = InputView.getInstance();
+        String input = "\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        // when
+        // then
+        assertThatThrownBy(inputView::requestInteger)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구입 금액은 숫자로 주어져야 합니다.");
+    }
+}

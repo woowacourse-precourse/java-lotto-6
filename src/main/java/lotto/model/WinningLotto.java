e7f1e96ff561c8dee0c8f04d82d9ac8ae63af624
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 00:43:15 +0900

    refactor(WinningLotto): validate 메서드 분리

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -8,9 +8,7 @@ public class WinningLotto {
     private final BonusNumber bonusNumber;
 
     public WinningLotto(WinningNumber winningNumber, BonusNumber bonusNumber) {
-        LottoNumberValidator.validateDuplicateBonusNumber(
-                winningNumber.getWinningNumbers(),
-                bonusNumber.getBonusNumber());
+        validate(winningNumber, bonusNumber);
 
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
@@ -22,5 +20,11 @@ public class WinningLotto {
 
     public BonusNumber getBonusNumber() {
         return bonusNumber;
+    }
+
+    private void validate(WinningNumber winningNumber, BonusNumber bonusNumber) {
+        LottoNumberValidator.validateDuplicateBonusNumber(
+                winningNumber.getWinningNumbers(),
+                bonusNumber.getBonusNumber());
     }
 }

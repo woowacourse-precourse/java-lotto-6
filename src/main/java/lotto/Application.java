6149ae58e9b3f35ea7d0d29ad21c920064170f3a
yummygyudon <bang2brew@gmail.com>
2023-11-06 04:21:36 +0900

    FEAT : Runner 싱글톤 객체에게 게임 실행 명령을 내리는 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.presentation.LottoGameRunner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoGameRunner.getRunner().run();
     }
 }

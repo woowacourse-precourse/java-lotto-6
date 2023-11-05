c3b61da475f4ffc536d826b668bec10652400274
dj <fjzl12@naver.com>
2023-11-05 20:34:17 +0900

    fix(Message): 구매한 로또 출력 문구 수정

--- src/main/java/lotto/domain/Message.java
+++ src/main/java/lotto/domain/Message.java
@@ -3,7 +3,7 @@ package lotto.domain;
 public enum Message {
     ERROR("[ERROR] "),
     INPUT_COST("구입금액을 입력해 주세요."),
-    PURCHASE_COMMENT("%d개를 구매했습니다."),
+    PURCHASE_COMMENT("\n%d개를 구매했습니다.\n"),
     INPUT_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
     RESULT("당첨 통계\n---");

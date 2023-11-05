7ad05e5d9a493f7a584cd731ae9788c93ec10bd7
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-05 18:52:42 +0900

    feat(util) : 입력에 대한 Parser 구현

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
+    private static final String SPLIT_REGEX = ",";
+
     public static List<String> parseString(String numbers) {
-        return Arrays.stream(numbers.split(",", -1))
+        return Arrays.stream(numbers.split(SPLIT_REGEX))
                 .map(String::trim)
                 .toList();
     }

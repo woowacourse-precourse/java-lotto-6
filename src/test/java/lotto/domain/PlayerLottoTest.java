502fe94e2542543a52612fe7d5e346812787cb4c
june-777 <wlwhswnsrl96@gmail.com>
2023-11-06 02:13:11 +0900

    test(PlayerLottoTest): 최종 성적을 계산하는 메서드의 반환값 변경으로 인한 테스트 로직 수정 및 테스트

--- src/test/java/lotto/domain/PlayerLottoTest.java
+++ src/test/java/lotto/domain/PlayerLottoTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,44 +12,36 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class PlayerLottoTest {
 
-    Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 45, 43));
-    LottoNumber bonusNumber = new LottoNumber(6);
-
     @Nested
     @DisplayName("최종 성적을 계산하는 기능 테스트")
     class calculateResultTest {
         @Test
-        void totalTest() {
+        @DisplayName("1 ~ 5등을 각각 1회씩 획득")
+        void randomTest1() {
             // given
-            PlayerLotto playerLotto = makeLottoTickets();
+            PlayerLotto playerLotto = createPlayerLotto();
             Lotto winningNumber = new Lotto(List.of(1, 3, 2, 4, 7, 5));
             LottoNumber bonusNumber = new LottoNumber(27);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.FIRST, Rank.SECOND, Rank.THIRD, Rank.FOURTH, Rank.FIFTH);
+            assertEachRankCounts(eachRankCounts, 1, 1, 1, 1, 1, 0);
         }
 
         @Test
-        @DisplayName("당첨 번호와 5개 일치하고, 보너스 번호 일치")
-        void criticalBranchTest1() {
+        @DisplayName("2, 4, 5등을 각각 1회씩 획득")
+        void randomTest2() {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(List.of(1, 45, 3, 4, 2, 6))));
+            PlayerLotto playerLotto = createPlayerLotto();
+            Lotto winningNumber = new Lotto(List.of(1, 2, 3, 5, 27, 9));
+            LottoNumber bonusNumber = new LottoNumber(7);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
+            System.out.println(eachRankCounts);
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.SECOND);
-        }
-
-        @Test
-        @DisplayName("당첨 번호와 5개 일치하고, 보너스 번호 불일치")
-        void criticalBranchTest2() {
-            // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(List.of(1, 45, 3, 4, 2, 21))));
-            // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
-            // then
-            Assertions.assertThat(ranks).containsExactly(Rank.THIRD);
+            assertEachRankCounts(eachRankCounts, 0, 1, 0, 1, 1, 2);
         }
 
         @ParameterizedTest
@@ -57,13 +50,14 @@ class PlayerLottoTest {
         @DisplayName("1등 테스트 - 6개 일치, 보너스 볼 일치 여부 상관 없음")
         void firstRankWhetherBonusMatchOrNot(String playerLottoInput, String winningNumberInput, int bonusNumberInput) {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(create(playerLottoInput))));
-            Lotto winningNumber = new Lotto(create(winningNumberInput));
+            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(parser(playerLottoInput))));
+            Lotto winningNumber = new Lotto(parser(winningNumberInput));
             LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.FIRST);
+            assertEachRankCounts(eachRankCounts, 1, 0, 0, 0, 0, 0);
         }
 
         @ParameterizedTest
@@ -72,50 +66,47 @@ class PlayerLottoTest {
         @DisplayName("2등 테스트 - 5개 일치, 보너스 볼 일치")
         void secondTest(String playerLottoInput, String winningNumberInput, int bonusNumberInput) {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(create(playerLottoInput))));
-            Lotto winningNumber = new Lotto(create(winningNumberInput));
+            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(parser(playerLottoInput))));
+            Lotto winningNumber = new Lotto(parser(winningNumberInput));
             LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.SECOND);
+            assertEachRankCounts(eachRankCounts, 0, 1, 0, 0, 0, 0);
         }
 
         @ParameterizedTest
         @CsvSource(value = {"1,2,3,4,5,6:1,2,3,4,6,21:7",
-                "11,12,13,14,15,16:16,15,14,13,12,29:17"}, delimiter = ':')
+                "11,12,13,14,15,16:16,15,14,13,12,29:17",
+                "1,45,3,4,2,21:1,2,3,4,45,43:6"}, delimiter = ':')
         @DisplayName("3등 테스트 - 5개 일치, 보너스 볼 불일치")
         void thirdTest(String playerLottoInput, String winningNumberInput, int bonusNumberInput) {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(create(playerLottoInput))));
-            Lotto winningNumber = new Lotto(create(winningNumberInput));
+            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(parser(playerLottoInput))));
+            Lotto winningNumber = new Lotto(parser(winningNumberInput));
             LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.THIRD);
+            assertEachRankCounts(eachRankCounts, 0, 0, 1, 0, 0, 0);
         }
 
-        @Test
-        @DisplayName("당첨 번호와 4개 일치하고, 보너스 번호 일치")
-        void forthTestWhenBonusMatch() {
+        @ParameterizedTest
+        @CsvSource(value = {"1,2,3,4,5,6:1,2,3,4,45,43:6",
+                "21,22,23,24,25,26:22,24,25,26,1,2:11"}, delimiter = ':')
+        @DisplayName("4등 테스트 - 당첨 번호 4개 일치, 보너스 볼 일치 여부 상관 없음")
+        void forthTestWhenBonusMatch(String playerLottoInput, String winningNumberInput, int bonusNumberInput) {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))));
+            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(parser(playerLottoInput))));
+            Lotto winningNumber = new Lotto(parser(winningNumberInput));
+            LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.FOURTH);
-        }
-
-        @Test
-        @DisplayName("당첨 번호와 4개 일치하고, 보너스 번호 불일치하는 경우")
-        void fourthTestWhenBonusNotMatch() {
-            // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(List.of(1, 2, 3, 23, 4, 5))));
-            // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
-            // then
-            Assertions.assertThat(ranks).containsExactly(Rank.FOURTH);
+            assertEachRankCounts(eachRankCounts, 0, 0, 0, 1, 0, 0);
         }
 
         @ParameterizedTest
@@ -124,13 +115,14 @@ class PlayerLottoTest {
         @DisplayName("5등 테스트 - 3개 일치, 보너스 볼 일치 여부 상관 없음")
         void fifthTest(String playerLottoInput, String winningNumberInput, int bonusNumberInput) {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(create(playerLottoInput))));
-            Lotto winningNumber = new Lotto(create(winningNumberInput));
+            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(parser(playerLottoInput))));
+            Lotto winningNumber = new Lotto(parser(winningNumberInput));
             LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.FIFTH);
+            assertEachRankCounts(eachRankCounts, 0, 0, 0, 0, 1, 0);
         }
 
         @ParameterizedTest
@@ -148,30 +140,42 @@ class PlayerLottoTest {
                 "40,41,42,43,44,45:1,2,3,4,5,6:40"}, delimiter = ':')
         void noneTest(String playerLottoInput, String winningNumberInput, int bonusNumberInput) {
             // given
-            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(create(playerLottoInput))));
-            Lotto winningNumber = new Lotto(create(winningNumberInput));
+            PlayerLotto playerLotto = new PlayerLotto(List.of(new Lotto(parser(playerLottoInput))));
+            Lotto winningNumber = new Lotto(parser(winningNumberInput));
             LottoNumber bonusNumber = new LottoNumber(bonusNumberInput);
             // when
-            List<Rank> ranks = playerLotto.calculateResult(winningNumber, bonusNumber);
+            FinalGrade finalGrade = playerLotto.calculateFinalGrade(winningNumber, bonusNumber);
+            EnumMap<Rank, Integer> eachRankCounts = finalGrade.getEachRankCounts();
             // then
-            Assertions.assertThat(ranks).containsExactly(Rank.NONE);
+            assertEachRankCounts(eachRankCounts, 0, 0, 0, 0, 0, 1);
         }
     }
 
-    private static List<Integer> create(String playerLottoInput) {
-        List<Integer> list = Arrays.stream(playerLottoInput.split(","))
-                .map(Integer::parseInt)
-                .toList();
-        return list;
+    private static void assertEachRankCounts(EnumMap<Rank, Integer> eachRankCounts,
+                                             int firstCounts, int secondCounts, int thirdCounts,
+                                             int fourthCounts, int fifthCounts, int noneCounts) {
+        Assertions.assertThat(eachRankCounts)
+                .containsEntry(Rank.FIRST, firstCounts)
+                .containsEntry(Rank.SECOND, secondCounts)
+                .containsEntry(Rank.THIRD, thirdCounts)
+                .containsEntry(Rank.FOURTH, fourthCounts)
+                .containsEntry(Rank.FIFTH, fifthCounts)
+                .containsEntry(Rank.NONE, noneCounts);
     }
 
-    private PlayerLotto makeLottoTickets() {
-        List<Lotto> playerLotto = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)),
+    private static List<Integer> parser(String playerLottoInput) {
+        return Arrays.stream(playerLottoInput.split(","))
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private PlayerLotto createPlayerLotto() {
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)),
                 new Lotto(List.of(1, 2, 3, 5, 7, 27)),
                 new Lotto(List.of(1, 4, 5, 7, 2, 28)),
                 new Lotto(List.of(7, 4, 2, 3, 30, 37)),
                 new Lotto(List.of(2, 3, 7, 11, 12, 13)));
-        return new PlayerLotto(playerLotto);
+        return new PlayerLotto(lottos);
     }
 
 }

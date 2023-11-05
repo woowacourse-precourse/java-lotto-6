0ee3f93ea8445bfcf8cefda9394fc4aa98ffe875
Joyce <by_eunae@naver.com>
2023-11-05 19:37:04 +0900

    feat : 당첨 번호 입력 및 예외 처리 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,10 +3,12 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class LottoGame {
-
 
 
     public LottoGame() {
@@ -26,15 +28,49 @@ public class LottoGame {
     public int getPurchaseAmount() {
         System.out.println("구매 금액을 입력해 주세요.");
         int purchaseAmount = Integer.parseInt(Console.readLine());
-        isValidAmount(purchaseAmount);
+        validateAmount(purchaseAmount);
 
         return purchaseAmount;
     }
 
-    private void isValidAmount(int purchaseAmount) {
+    private void validateAmount(int purchaseAmount) {
+
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해 주세요.");
         }
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        List<Integer> winningNumbers;
+        System.out.println("당첨 번호를 입력해 주세요.");
+        winningNumbers = Arrays.stream(Console.readLine().split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        validateWinningNumbers(winningNumbers);
+
+        return winningNumbers;
+    }
+
+    private void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해 주세요.");
+        }
+
+        boolean areNumbersInRange = winningNumbers.stream()
+                .anyMatch(number -> 1 <= number && 45 >= number);
+        if (!areNumbersInRange) {
+            throw new IllegalArgumentException("[ERROR] 1~45 사이의 숫자만 입력이 가능합니다.");
+        }
+
+        boolean areNumbersUnique = winningNumbers.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .values()
+                .stream()
+                .anyMatch(count -> count <= 1);
+        if (!areNumbersUnique) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+        }
+
     }
 
 }
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -1,12 +1,13 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import org.assertj.core.api.Assert;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,6 +27,7 @@ class LottoGameTest {
             assertThat(result).isEqualTo(9000);
         }
     }
+
     @Test
     @DisplayName("getPurchaseAmount 함수에서 입력받은 값이 1,000원 단위가 아닐 경우 IllegalArgumentException에러 발생")
     void getPurchaseAmount_함수에서_입력값이_1000원_단위가_아니면_IllegalArgumentException에러_발생() {
@@ -37,4 +39,67 @@ class LottoGameTest {
             assertThrows(IllegalArgumentException.class, lottoGame::getPurchaseAmount);
         }
     }
+
+    @Test
+    @DisplayName("inputWinningNumbers 함수에서 당첨 번호를 조건에 맞게 입력한 경우 그대로 출력")
+    void input_WinningNumbers_함수에서_당첨번호_조건에_맞에_입력하면_그대로_출력() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("1,2,3,4,5,6");
+            LottoGame lottoGame = new LottoGame();
+            //when
+            List<Integer> result = lottoGame.inputWinningNumbers();
+            //then
+            assertThat(result).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6));
+        }
+    }
+
+    @Test
+    @DisplayName("inputWinningNumbers 함수에서 당첨 번호를 5개 입력받은 경우 IllegalArgumentException에러 발생")
+    void input_WinningNumbers_함수에서_당첨번호를_5개_입력받은_경우_IllegalArgumentException에러_발생() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("1,2,3,4,5");
+            LottoGame lottoGame = new LottoGame();
+            //when,then
+            assertThrows(IllegalArgumentException.class, lottoGame::inputWinningNumbers);
+        }
+    }
+
+    @Test
+    @DisplayName("inputWinningNumbers 함수에서 당첨 번호를 7개 입력받은 경우 IllegalArgumentException에러 발생")
+    void input_WinningNumbers_함수에서_당첨번호를_7개_입력받은_경우_IllegalArgumentException에러_발생() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("1,2,3,4,5,6,7");
+            LottoGame lottoGame = new LottoGame();
+            //when,then
+            assertThrows(IllegalArgumentException.class, lottoGame::inputWinningNumbers);
+        }
+    }
+
+    @Test
+    @DisplayName("inputWinningNumbers 함수에서 당첨 번호를 1~45 사이의 숫자를 입력하지 않은 경우 IllegalArgumentException에러 발생")
+    void input_WinningNumbers_함수에서_당첨번호를_1에서_45사이의_숫자를_입력하지_않은_경우_IllegalArgumentException에러_발생() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("1,2,67,4,5,6,7");
+            LottoGame lottoGame = new LottoGame();
+            //when,then
+            assertThrows(IllegalArgumentException.class, lottoGame::inputWinningNumbers);
+        }
+    }
+
+    @Test
+    @DisplayName("inputWinningNumbers 함수에서 당첨 번호에 중복된 숫자 입력할 경우 IllegalArgumentException에러 발생")
+    void input_WinningNumbers_함수에서_당첨번호에_중복된_숫자를_입력할_경우_IllegalArgumentException에러_발생() {
+        try (MockedStatic<Console> mock = Mockito.mockStatic(Console.class)) {
+            //given
+            mock.when(Console::readLine).thenReturn("1,2,2,4,5,6,7");
+            LottoGame lottoGame = new LottoGame();
+            //when,then
+            assertThrows(IllegalArgumentException.class, lottoGame::inputWinningNumbers);
+        }
+    }
+
 }

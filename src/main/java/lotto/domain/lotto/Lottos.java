7f21910f4e0e6f79dd87e3bdb35c0cf2d151183e
minisyu <iswater.kim@gmail.com>
2023-11-06 01:20:36 +0900

    feat: Lotto 일급 컬렉션 Lottos 생성

---
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -0,0 +1,19 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos from(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+    public List<Lotto> getLottos() {
+        return List.copyOf(lottos);
+    }
+}

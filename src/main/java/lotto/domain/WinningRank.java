fbf901cb25aa83744767188f46c07010ba8eb998
msung99 <msung6924@naver.com>
2023-11-05 12:43:43 +0900

    feat: WinningRank 의 열거형 상수들 정의

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -1,4 +1,21 @@
 package lotto.domain;
 
 public enum WinningRank {
+    FIRST(6, false, 2000000000, "6개 일치"),
+    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, false, 1500000, "5개 일치"),
+    FOURTH(4, false, 50000, "4개 일치"),
+    FIFTH(3, false, 5000, "3개 일치"),
+
+    private final int match;
+    private final boolean isBonus;
+    private final int prize;
+    private final String message;
+
+    WinningRank(int match, boolean isBonus, int prize, String message){
+        this.match = match;
+        this.isBonus = isBonus;
+        this.prize = prize;
+        this.message = message;
+    }
 }

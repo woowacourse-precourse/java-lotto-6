12c5f537225df64c3a145b6b82e4e6e639995caf
hosung-222 <gik11kr@naver.com>
2023-11-05 19:31:38 +0900

    faat : 구입 금액에 해당하는 로또 발행하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,11 +3,19 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.domain.LottoGenerator;
 
 public class Application {
     public static void main(String[] args) {
 
         int money = Integer.parseInt(Console.readLine());
 
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<List<Integer>> tickets = lottoGenerator.getTickets(money);
+
+        System.out.println(tickets.size() + "개를 구매했습니다.");
+        for (int i = 0; i < tickets.size() ; i++) {
+            System.out.println(tickets.get(i));
+        }
     }
 }
--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,7 +1,23 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGenerator {
     public static final int LOTTO_PRICE = 1000;
 
+    public List<List<Integer>> getTickets(int money){
+        List<List<Integer>> allTickets = new ArrayList<>();
+        int tickets = money/1000;
+        for (int i = 0; i <tickets ; i++) {
+            allTickets.add(generateTicket());
+        }
+        return allTickets;
+    }
+
+    public List<Integer> generateTicket(){
+        return  Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 
 }

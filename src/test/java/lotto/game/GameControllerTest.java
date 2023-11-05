027941fa989899e43762593a364fe74f52277ef3
gihwanJang <gihwan319@gmail.com>
2023-11-05 18:44:40 +0900

    test : (출력기능) 당첨 내역 출력 테스트 작성

--- src/test/java/lotto/game/GameControllerTest.java
+++ src/test/java/lotto/game/GameControllerTest.java
@@ -26,4 +26,29 @@ public class GameControllerTest {
             List.of(8, 23, 21, 41, 42, 43)
         );
     }
+
+    @Test
+    void 당첨_내역_출력() {
+        // given
+        int price = 4000;
+        // when & then
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                game.purchaseLotto(price);
+                game.setWinningLotto(List.of(8, 21, 23, 11, 16, 32), 7);
+                assertThat(game.getLottoWinningResult()).contains(
+                    "3개 일치 (5,000원) - 2개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 0개",
+                    "총 수익률은 250.0%입니다."
+                );
+            },
+            List.of(8, 23, 21, 41, 42, 43),
+            List.of(3, 5, 11, 16, 32, 38),
+            List.of(7, 11, 16, 35, 36, 44),
+            List.of(1, 8, 11, 31, 41, 42)
+        );
+    }
 }

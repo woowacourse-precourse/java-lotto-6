d9f899605547aac980f4bbd889eb8bdbb33cf786
enduf7686 <enduf768640@gmail.com>
2023-11-05 15:22:41 +0900

    feat(lotto): add Lottos record

--- src/main/java/lotto/lotto/Lottos.java
+++ src/main/java/lotto/lotto/Lottos.java
@@ -2,5 +2,16 @@ package lotto.lotto;
 
 import java.util.List;
 
-public record Lottos(List<Lotto> lottos) {
+public record Lottos(List<Lotto> lottoList) {
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Lotto lotto : lottoList) {
+            sb.append(lotto.getNumbers().toString())
+                    .append("\n");
+        }
+
+        return sb.toString();
+    }
 }

c6c4db7ce959dd3b4381cec12d92f67b63197a52
LUKR7Q <ybkim0628@naver.com>
2023-11-05 22:59:52 +0900

    printRevenueRate(double EarningRate): 총 수익률을 출력하는 메서드입니다. EarningRate를 소수점 한 자리까지 출력하고 메시지와 함께 출력합니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,4 +16,7 @@ public class OutputView {
         System.out.println(message + numberOfMatch + "개");
     }
 
+    public static void printRevenueRate(double EarningRate) {
+        System.out.println("총 수익률은 " + String.format("%.1f", EarningRate) + "%입니다.");
+    }
 }

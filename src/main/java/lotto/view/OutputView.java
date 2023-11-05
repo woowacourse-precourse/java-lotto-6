44823f422df8aadb7edc852796d6af5eb97069c4
oyoungsun <oyoungsun1030@gmail.com>
2023-11-05 16:54:22 +0900

    feat (OutputView) : 수익률을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,6 +10,9 @@ public class OutputView {
     private final static String LOTTO_PRINT_PREFIX = "[";
     private final static String LOTTO_PRINT_SUFFIX = "]";
     private final static String WINNING_COUNT_MESSAGE = "당첨 통계\n---";
+    private final static String SPACE = " ";
+    private final static String DASH = " - ";
+    private final static String RATEOFRESULT_MESSAGE = "총 수익률은 %.2f%입니다.";
 
 
     public static void printException(String errorMessage) {
@@ -31,22 +34,26 @@ public class OutputView {
         }
     }
 
-    private static void printResultCount(List<MatchNumber> matcheNumbers, List<Integer> matchCount) {
+    private static void printResultCount(List<Integer> matchCount) {
         System.out.printf(String.format(WINNING_COUNT_MESSAGE));
         printEmpty();
 
         StringBuilder result;
-
         for (int i = 0; i < MatchNumber.getMembers().size(); i++) {
             MatchNumber match = MatchNumber.getMembers().get(i);
             result = new StringBuilder();
-            result.append(match.getResultMessage()).append(" ");
+            result.append(match.getResultMessage()).append(SPACE);
             result.append(match.getMoneyMessage());
-            result.append(" - ");
+            result.append(DASH);
             result.append(String.format("%d개", matchCount.get(i)));
             System.out.print(result);
             printEmpty();
         }
+    }
+
+    private static void printRateOfResult(float rateOfResult) {
+        System.out.printf(String.format(RATEOFRESULT_MESSAGE, rateOfResult * 100));
+        printEmpty();
     }
 
     private static void printEmpty() {

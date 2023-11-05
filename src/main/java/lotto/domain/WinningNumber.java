b4faa3051a2d64b649833b09eae4b9181cd839f7
itsmo1031 <itsmo1031@gmail.com>
2023-11-06 00:43:44 +0900

    feat(domain): WinningNumber생성 시 보너스 번호에 대한 중복 검증 과정 추가
    - 보너스 번호가 로또 번호에 포함되어 있는지 확인하고 포함되어 있다면 IllegalArgumentException을 발생시키도록 하는 validate()메서드 작성

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.constant.ExceptionMessage.DUPLICATE;
+
 import lotto.constant.WinnerType;
 
 public class WinningNumber {
@@ -7,8 +9,15 @@ public class WinningNumber {
     private final int bonus;
 
     public WinningNumber(Lotto winningNumber, int bonus) {
+        validate(winningNumber, bonus);
         this.winningNumber = winningNumber;
         this.bonus = bonus;
+    }
+
+    private void validate(Lotto winningNumber, int bonus) {
+        if (winningNumber.contains(bonus)) {
+            throw new IllegalArgumentException(DUPLICATE.toString());
+        }
     }
 
     public WinnerType calcWinner(Lotto lotto) {

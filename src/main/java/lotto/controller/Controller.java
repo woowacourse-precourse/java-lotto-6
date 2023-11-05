cf85c0603462bcfd6ec593aa9659d9a24bfd36ee
woo <wsk221e@gmail.com>
2023-11-05 16:05:14 +0900

    refactor(Controller): 게임 시작 로직 명확하게 분리
    - startGame: 게임 시작 로직을 private 메소드로 분리함으로써 가독성 개선

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -14,7 +14,17 @@ public class Controller {
     PrizeManager prize = new PrizeManager();
     Validator valid = new Validator();
 
+
     public void startGame() {
+        getUserBuyAmount();
+        buyTickets();
+        String winning = getUserPrizingNumbers();
+        String bonus = getUserBonusNumbers();
+        displayGameResult(winning, bonus);
+    }
+
+
+    private void getUserBuyAmount() {
         while (true) {
             try {
                 String amount = input.getUserBuyAmount();
@@ -24,33 +34,43 @@ public class Controller {
                 output.displayBuyError();
             }
         }
+    }
+
+    private void buyTickets() {
         List<String> list = lotto.getTickets();
         output.displayBoughtTickets(list);
+    }
 
+    private String getUserPrizingNumbers() {
         String winning;
         while (true) {
             try {
                 winning = input.getPrizingNumbers();
                 valid.validateWinningNumber(winning);
-                break;
+                return winning;
             } catch (IllegalArgumentException e) {
                 output.displayWinningError();
             }
         }
+    }
 
+    private String getUserBonusNumbers() {
         String bonus;
         while (true) {
             try {
                 bonus = input.getBonusNumbers();
                 valid.validateBonusNumber(bonus);
-                break;
+                return bonus;
             } catch (IllegalArgumentException e) {
                 output.displayBonusError();
             }
         }
+    }
 
+    private void displayGameResult(String winning, String bonus) {
         List<Integer> points = lotto.checkTicketPoints(winning, bonus);
         prize.checkTicketAndAddPrizes(points);
+
         List<Integer> counts = prize.getPrizeCounts();
         output.displayRankCounts(counts);
 

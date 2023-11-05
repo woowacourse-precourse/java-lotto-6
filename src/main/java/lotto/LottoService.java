164477bd9ca68d55f5a16e56512abf1881ee4cb1
seoweed <darkweed1106@gmail.com>
2023-11-05 20:53:20 +0900

    발행한 로또 출력메소드 및 당첨, 보너스 번호를 입력받는 메소드 생성

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,6 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class LottoService {
 
@@ -13,6 +18,39 @@ public class LottoService {
         int lottoCount = moneyInput / 1000;
         System.out.printf("%d개를 구입했습니다.\n", lottoCount);
         return lottoCount;
+    }
+
+    public List<Integer> randomNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
+    }
+
+    public List<Lotto> lottoIssuance(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i <lottoCount; i++) {
+            Lotto lotto = new Lotto(randomNumber());
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    public void lottosPrint(List<Lotto> lottos) {
+        for (int i = 0; i <lottos.size(); i++) {
+            System.out.println(lottos.get(i).toString());
+        }
+    }
+
+    public String[] winningNumberInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] winningNumber = Console.readLine().split(",");
+        return winningNumber;
+    }
+
+    public int bonusNumberInput() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+
     }
 
 
--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoServiceTest {
@@ -15,4 +17,26 @@ class LottoServiceTest {
         int lottoCount = lottoService.lottoCount(10000);
         Assertions.assertThat(10).isEqualTo(lottoCount);
     }
+
+    @Test
+    @DisplayName("랜덤한 6자리 숫자를 생성")
+    void randomNumber() {
+        LottoService lottoService = new LottoService();
+        List<Integer> randomNumber = lottoService.randomNumber();
+
+        Assertions.assertThat(6).isEqualTo(randomNumber.size());
+    }
+
+    @Test
+    @DisplayName("입렵받은 갯수만큼 로또를 발행")
+    void lottoIssuance() {
+        LottoService lottoService = new LottoService();
+        List<Lotto> lottos = lottoService.lottoIssuance(3);
+
+        Assertions.assertThat(3).isEqualTo(lottos.size());
+        for (int i = 0; i < lottos.size(); i++) {
+            Assertions.assertThat(lottos.get(i)).isInstanceOf(Lotto.class);
+        }
+    }
+
 }

5c58298a34ff1135087e0bc848ac2a6dd9b42b5b
Sanghwi Lee <sanghwi47@naver.com>
2023-11-05 11:45:00 +0900

    feat(BonusNumber) BonusNumber에 대한 유효성 검사를 진행하는 코드 작성

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,49 @@
+package lotto.model;
+
+public class BonusNumber {
+    private static final String ERROR_INVALID_INPUT_TYPE = "유효하지 않은 타입입니다. 숫자로 입력해주세요.";
+    private static final String ERROR_NULL_OR_EMPTY_INPUT = "입력값이 없거나 공백입니다.";
+    private static final String ERROR_INPUT_NUMBER_NOT_IN_RANGE = "1-45 사이의 값을 입력해 주세요.";
+    private static final String ERROR_DUPLICATED_WITH_WINNING_NUMBER = "보너스 숫자는 당첨번호와 중복될 수 없습니다.";
+    private int bonusNumber;
+
+    public BonusNumber(String bonusNumberString, WinningNumbers winningNumbers) {
+        validateNotNullOrEmpty(bonusNumberString);
+        int checkingBonusNumber = parseAndValidateSingleNumber(bonusNumberString);
+        validateBonusNumber(checkingBonusNumber, winningNumbers);
+        this.bonusNumber = checkingBonusNumber;
+    }
+
+    public int getNumber() {
+        return bonusNumber;
+    }
+
+    private void validateNotNullOrEmpty(String numberStr) {
+        if (numberStr == null || numberStr.trim().isEmpty()) {
+            throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY_INPUT);
+        }
+    }
+
+    private int parseAndValidateSingleNumber(String numberStr) {
+        int parsedNumber;
+        try {
+            parsedNumber = Integer.parseInt(numberStr.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_INVALID_INPUT_TYPE);
+        }
+        validateNumberInRange(parsedNumber);
+        return parsedNumber;
+    }
+
+    private void validateNumberInRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ERROR_INPUT_NUMBER_NOT_IN_RANGE);
+        }
+    }
+
+    private void validateBonusNumber(int number, WinningNumbers winningNumbers) {
+        if (winningNumbers.getNumbers().contains(number)) {
+            throw new IllegalArgumentException(ERROR_DUPLICATED_WITH_WINNING_NUMBER);
+        }
+    }
+}

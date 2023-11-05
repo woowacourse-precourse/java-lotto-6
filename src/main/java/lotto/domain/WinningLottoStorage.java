1eed4454d5304f403204008bb5d3c35f30ea79ab
elyudwo <dudqk9696@naver.com>
2023-11-05 17:22:32 +0900

    [Feat] : 당첨 로또 저장소 초기화 기능 구현

---
+++ src/main/java/lotto/domain/WinningLottoStorage.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class WinningLottoStorage {
+
+    private Map<Rank, Integer> rankStorage;
+
+    private void initRankStorage() {
+        rankStorage.put(Rank.CORRECT_ZERO, 0);
+        rankStorage.put(Rank.CORRECT_ONE, 0);
+        rankStorage.put(Rank.CORRECT_TWO, 0);
+        rankStorage.put(Rank.CORRECT_THREE, 0);
+        rankStorage.put(Rank.CORRECT_FOUR, 0);
+        rankStorage.put(Rank.CORRECT_FIVE, 0);
+        rankStorage.put(Rank.CORRECT_FIVE_BONUS, 0);
+        rankStorage.put(Rank.CORRECT_SIX, 0);
+    }
+}

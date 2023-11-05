6d2c69f1caf6099d9a46bd3117e11ac01c41e565
leechiyun <wychis28@gmail.com>
2023-11-05 12:08:25 +0900

    feat: LottoShop 테스트 기능 구현

---
+++ src/test/java/lotto/LottoShopTest.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.List;
+import lotto.generator.LottoGenerator;
+import lotto.shop.LottoShop;
+import lotto.validate.Validate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LottoShopTest {
+    private Validate validate;
+
+    @BeforeEach
+    public void setUp() {
+        validate = new Validate();
+    }
+
+    @Test
+    public void 금액_입력_시_로또_발행() {
+        // Given: 로또 발행 숫자 값을 고정
+        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
+        when(lottoGenerator.getLotto()).thenReturn(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        int money = 10000;
+
+        // When: LottoShop 객체의 buyLotto 메서드 실행
+        LottoShop lottoShop = new LottoShop(lottoGenerator, validate);
+        List<Lotto> lottos = lottoShop.buyLotto(money);
+
+        // THen: 10000원 입력시, 10개의 lotto 발행
+        int expectedSize = 10;
+
+        assertEquals(expectedSize, lottos.size());
+    }
+}

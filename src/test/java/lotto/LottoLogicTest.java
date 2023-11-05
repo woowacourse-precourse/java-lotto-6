0bbc11f195294b4c289b7924ada651f006d95dce
alscjf1329 <alscjf1329@gmail.com>
2023-11-05 14:38:49 +0900

    feat(LottoLogicTest): 로또의 총 수익률을 계산하는 테스트 작성
    로또의 총 수익률을 계산하는 getProfitRate 메소드에 대해 테스트를 작성했습니다.

--- src/test/java/lotto/LottoLogicTest.java
+++ src/test/java/lotto/LottoLogicTest.java
@@ -1,20 +1,27 @@
 package lotto;
 
-import static lotto.message.ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS;
-import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 import java.util.List;
 import lotto.domain.LottoLogic;
 import lotto.dto.LottoNumbersInfo;
 import lotto.dto.LottoPurchaseInfo;
+import lotto.message.LottoResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoLogicTest {
+    @Test
+    @DisplayName("로또의 총 수익률을 계산한다.")
+    void calculateTotalProfitRate() {
+        LottoPurchaseInfo purchaseInfo = new LottoPurchaseInfo(new BigDecimal(1000), Lotto.PRICE);
+        LottoNumbersInfo numbersInfo = new LottoNumbersInfo(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))),
+                List.of(1, 2, 3, 4, 5, 6), 1);
+        assertTrue(new LottoLogic(purchaseInfo, numbersInfo).getProfitRate()
+                == (float) LottoResult.FIRST.getPrize() / 1000);
 
+    }
 
 
 }

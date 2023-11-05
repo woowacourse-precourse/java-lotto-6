8046ccc54c0a086753e9e65fd3e879ba56ff80f5
unknown <winternrabbit@gmail.com>
2023-11-06 01:18:01 +0900

    refactor: 구입금액 관련 로직 수정

--- src/main/java/lotto/LottoCost.java
+++ src/main/java/lotto/LottoCost.java
@@ -4,11 +4,10 @@ public class LottoCost {
     private final int cost;
     private final int ticket;
 
-    public LottoCost(String cost) {
-        isNumber(cost);
+    public LottoCost(int cost) {
         isMultipleOfUnit(cost);
 
-        this.cost = Integer.parseInt(cost);
+        this.cost = cost;
         ticket = this.cost / Config.UNIT;
     }
 
@@ -20,18 +19,8 @@ public class LottoCost {
         return ticket;
     }
 
-    private void isNumber(String cost) {
-        boolean hasOnlyNum = !cost.isEmpty() && cost.chars().allMatch(Character::isDigit);
-
-        if (!hasOnlyNum) {
-            throw new IllegalArgumentException(ErrorMessages.ERROR_NOT_NUMBER.getMessage());
-        }
-    }
-
-    private void isMultipleOfUnit(String cost) {
-        int costValue = Integer.parseInt(cost);
-        boolean isRemainderZero
-                = (costValue >= Config.UNIT && costValue < Integer.MAX_VALUE && costValue % Config.UNIT == 0);
+    private void isMultipleOfUnit(int cost) {
+        boolean isRemainderZero = (cost >= Config.UNIT && cost < Integer.MAX_VALUE && cost % Config.UNIT == 0);
 
         if (!isRemainderZero) {
             throw new IllegalArgumentException(ErrorMessages.ERROR_NOT_MULTIPLE_OF_UNIT.getMessage());
--- src/test/java/lotto/LottoCostTest.java
+++ src/test/java/lotto/LottoCostTest.java
@@ -6,18 +6,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoCostTest {
-
-    @DisplayName("구입금액이 숫자가 아닐 경우 예외가 발생한다.")
-    @Test
-    void costNotNumber() {
-        assertThatThrownBy(() -> new LottoCost("abc123가나다"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("구입금액이 지정한 1000의 배수가 예외가 발생한다.")
     @Test
     void costNotMultipleOfUnit() {
-        assertThatThrownBy(() -> new LottoCost("1375"))
+        assertThatThrownBy(() -> new LottoCost(1375))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

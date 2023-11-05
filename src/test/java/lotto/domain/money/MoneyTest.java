398925a0d205c894719807e71ce9fc99cb36a9f4
sangdol <priceelees@gmail.com>
2023-11-05 15:44:53 +0900

    test(MoneyTest): 1억부터 100억까지의 모든 단위를, 문자열 형태로 정확히 변환하는지 확인

--- src/test/java/lotto/domain/money/MoneyTest.java
+++ src/test/java/lotto/domain/money/MoneyTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain.money;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class MoneyTest {
-  
+    @DisplayName("금액을 컴마로 구분하고, 원을 붙인 문자열로 정확하게 변환하는지 확인")
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void parseToWonWithComma(long amount, String expectedResult) {
+        Money money = new Money(amount);
+        assertThat(money.parseToWonWithComma()).isEqualTo(expectedResult);
+    }
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                arguments(1L, "1원"),
+                arguments(10L, "10원"),
+                arguments(100L, "100원"),
+                arguments(1000L, "1,000원"),
+                arguments(10000L, "10,000원"),
+                arguments(100000L, "100,000원"),
+                arguments(1000000L, "1,000,000원"),
+                arguments(10000000L, "10,000,000원"),
+                arguments(100000000L, "100,000,000원"),
+                arguments(1000000000L, "1,000,000,000원"),
+                arguments(10000000000L, "10,000,000,000원")
+        );
+    }
 }

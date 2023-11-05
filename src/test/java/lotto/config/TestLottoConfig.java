3e4e897b44dba32acbbfbb51a2f3310574c80b56
seminchoi <choicco89@gmail.com>
2023-11-05 13:53:52 +0900

    test(config.TestLottoConfig) : 테스트에 사용할 LottoConfig 구현체 구현

---
+++ src/test/java/lotto/config/TestLottoConfig.java
@@ -0,0 +1,35 @@
+package lotto.config;
+
+import lotto.TestConstant;
+import lotto.controller.LottoController;
+import lotto.random.RandomNumberGenerator;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class TestLottoConfig implements LottoConfig {
+    @Override
+    public RandomNumberGenerator randomNumberGenerator() {
+        return (min, max) -> TestConstant.normalNumbers;
+    }
+
+    @Override
+    public LottoService lottoService() {
+        return new LottoService(randomNumberGenerator());
+    }
+
+    @Override
+    public InputView inputView() {
+        return new InputView();
+    }
+
+    @Override
+    public OutputView outputView() {
+        return new OutputView();
+    }
+
+    @Override
+    public LottoController lottoController() {
+        return new LottoController(lottoService(), inputView(), outputView());
+    }
+}

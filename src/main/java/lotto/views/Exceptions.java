11bb0d69e3dc5990acfffd0fabc8f48042461f29
74059 <shyeone@naver.com>
2023-11-05 13:10:52 +0900

    refactor: 하드 코딩한 부분 static final 이용하여 수정

--- src/main/java/lotto/views/Exceptions.java
+++ src/main/java/lotto/views/Exceptions.java
@@ -1,15 +1,22 @@
 package lotto.views;
 
 public class Exceptions {
+    public Exceptions() {
+    }
+
+    static final String TYPE_ERROR_MESS = "[ERROR] 숫자로만 입력해 주세요.";
+    static final String POSITIVE_ERROR_MESS = "[ERROR] 양수로만 입력해 주세요.";
+    static final String DIVISIBLE_1000_MESS = "[ERROR] 로또 하나 당 금액은 1,000원 입니다. 1,000 단위로 작성해주세요. (ex. 13개 구입 시, 13000 으로 입력)";
+
     public static void exceptionNotInt() {
-        System.out.println("[ERROR] 숫자로만 입력해 주세요.");
+        System.out.println(TYPE_ERROR_MESS);
     }
 
     public static void execptionNotPositive() {
-        System.out.println("[ERROR] 양수로만 입력해 주세요.");
+        System.out.println(POSITIVE_ERROR_MESS);
     }
 
     public static void exceptionNotDivisible1000() {
-        System.out.println("[ERROR] 로또 하나 당 금액은 1,000원 입니다. 1,000 단위로 작성해주세요. (ex. 13개 구입 시, 13000 으로 입력)");
+        System.out.println(DIVISIBLE_1000_MESS);
     }
 }

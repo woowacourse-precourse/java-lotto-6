5f6a244cb6c28a9a3635d32dd6eae47fd4c7c9e4
kymin <park48664866@gmail.com>
2023-11-05 12:58:37 +0900

    feat: 메시지 출력 기능 추가 구현
    메시지 출력 기능 추가 구현:
    - 메시지 Enum클래스 요소 추가
    - 메시지 출력 메서드 추가

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -1,9 +1,24 @@
 package lotto.view;
 
 public enum Message {
-    INPUT_AMOUNT_MESSAGE("구입금액을 입력해 주세요.");
-    INPUT_WINNING_NUM_MESSAGE("당첨 번호를 입력해 주세요.");
-    INPUT_BONUS_NUM_MESSAGE("보너스 번호를 입력해 주세요.");
+    INPUT_AMOUNT("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUM("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUM("보너스 번호를 입력해 주세요."),
+    OUTPUT_RESULT("당첨 통계\n---"),
+    SAME3("3개 일치 (5,000원) - %d개"),
+    SAME4("4개 일치 (50,000원) - %d개"),
+    SAME5("5개 일치 (1,500,000원) - %d개"),
+    SAME5_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    SAME6("6개 일치 (2,000,000,000원) - %d개"),
+    RATE_OF_RETURN("총 수익률은 %f%입니다.");
 
     private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,19 @@
 package lotto.view;
 
 public class Output {
-    
+    public void inputAmountMsg() {
+        System.out.println(Message.INPUT_AMOUNT);
+    }
+
+    public void inputWinningNumMsg() {
+        System.out.println(Message.INPUT_WINNING_NUM);
+    }
+
+    public void inputBonusNumMsg() {
+        System.out.println(Message.INPUT_BONUS_NUM);
+    }
+
+    public void printResult() {
+        System.out.println(Message.OUTPUT_RESULT);
+    }
 }

a348c5e1df18b14da9f43a8b1ce81eb744aac37d
SUJI-J <suzy852@naver.com>
2023-11-05 17:37:49 +0900

    feat: 상수 메세지 생성

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class Constants {
+    public static final String REQUEST_BUY_MONEY = "구입금액을 입력해 주세요.";
+    public static final String REQUEST_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
+}

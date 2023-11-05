207a1c8bf03b58edb74d1c77dd6f291af4908522
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 00:48:23 +0900

    feat(Customer): 로또 구매 기능 구현

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -1,2 +1,22 @@
-package lotto.domain;public class Customer {
+package lotto.domain;
+
+import java.util.List;
+
+public class Customer {
+    private final Cash cash;
+    private final LottoSeller lottoSeller;
+    private List<Lotto> lottos;
+
+    public Customer(Cash cash, LottoSeller lottoSeller) {
+        this.cash = cash;
+        this.lottoSeller = lottoSeller;
+    }
+
+    public void purchaseLottos() {
+        this.lottos = lottoSeller.sellLottos(cash);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }

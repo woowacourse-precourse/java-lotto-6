9b6c67f430f531e7e619fd40d1c4c3af34c13b07
whxogus215 <whxogus215@naver.com>
2023-11-05 11:05:31 +0900

    feat(Casher): 에러 메시지 public static으로 변경 - 테스트 활용

--- src/main/java/lotto/utils/Casher.java
+++ src/main/java/lotto/utils/Casher.java
@@ -2,8 +2,8 @@ package lotto.utils;
 
 public class Casher {
     private final int LOTTO_PRICE = 1000;
-    private final String NOT_ENOUGH_PRICE = "금액이 부족합니다. 최소금액 : ";
-    private final String NOT_LOTTO_UNIT = "로또 금액 단위로 입력해야 합니다. 입력단위 : ";
+    public static final String NOT_ENOUGH_PRICE = "금액이 부족합니다. 최소금액 : ";
+    public static final String NOT_LOTTO_UNIT = "로또 금액 단위로 입력해야 합니다. 입력단위 : ";
 
     public int getLottoCount(int price) {
         if (price < LOTTO_PRICE) {

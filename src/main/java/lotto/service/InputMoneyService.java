328b415a10b40ffc3cccfe27b22e33ac642e1e64
김효준 <khyojun1104@gmail.com>
2023-11-05 16:22:55 +0900

    feat(lotto): 금액이 1000원 단위 아닌 경우 에 대한 예외 처리를 위한 기능 구현!

--- src/main/java/lotto/service/InputMoneyService.java
+++ src/main/java/lotto/service/InputMoneyService.java
@@ -8,7 +8,7 @@ public class InputMoneyService {
     public void getRightMoneyProcess(Validator validator, String money) {
         try{
             validator.validateInputMoneyIsNumber(money);
-
+            validator.validateNumberUnitIsThousand(Long.parseLong(money));
         }catch (IllegalArgumentException e){
             throw new IllegalArgumentException(e.getMessage());
         }
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -10,5 +10,13 @@ public class Validator {
         }
     }
 
+    public void validateNumberUnitIsThousand(Long money) {
+        try{
+            if(money % 1000 !=0)
+                throw new IllegalArgumentException();
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위여야 합니다!");
+        }
 
+    }
 }

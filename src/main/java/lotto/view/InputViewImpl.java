f50f8e10c17e0c942fdbd374d8c3bc34ac57c34d
jiwoo <zbqmgldjfh@gmail.com>
2023-11-05 11:46:42 +0900

    feat(InputView): InputView 구현

---
+++ src/main/java/lotto/view/InputViewImpl.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import lotto.domain.LottoNumber;
+import lotto.domain.Money;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputViewImpl implements InputView {
+
+    @Override
+    public Money readUserPurchaseMoney() {
+        String purchaseMoney = readLine();
+        return new Money(Long.parseLong(purchaseMoney));
+    }
+
+    @Override
+    public List<LottoNumber> readUserLottoNumbers() {
+        return Arrays.stream(readLine().trim().split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public LottoNumber readUserBonusNumber() {
+        int bonusNumber = Integer.parseInt(readLine().trim());
+        return new LottoNumber(bonusNumber);
+    }
+}

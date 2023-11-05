80c3b2ff87422bd03c82c97f54e40ee5a1ac03db
hyebin <xxico89@gmail.com>
2023-11-05 23:28:59 +0900

    [Feat] Lotto 클래스 내 로또 번호 숫자 범위 검증 메소드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,14 +7,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateLottoNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoNumber(List<Integer> numbers) {
+        for (int n : numbers) {
+            if (!(0 < n && n < 46)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
 }

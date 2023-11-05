66408c2a781278fddef3d6ec2ca4df9399b573f1
siwol025 <cyk99115695@gmail.com>
2023-11-06 01:09:02 +0900

    test(PrizeTest): 당첨 기준에 맞게 등수를 구분하는지 테스트

---
+++ src/test/java/lotto/domain/PrizeTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PrizeTest {
+
+    @DisplayName("당첨 기준에 맞게 등수를 구분하는지 테스트")
+    @Test
+    void determineRank() {
+        int winningNumberCount = 5;
+        boolean isBonusNumberMatched = true;
+        Prize expectedPlace = Prize.SECOND_PLACE;
+
+        Prize determineRank = Prize.determineRank(winningNumberCount, isBonusNumberMatched);
+
+        assertThat(determineRank).isEqualTo(expectedPlace);
+    }
+}

d760b4394a68b9c0835650502cd71cfb43cd02ad
Dowon <3120kang@gmail.com>
2023-11-05 17:10:41 +0900

    Feat(Customer): 돈을 지불하는 기능 구현

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -1,16 +1,19 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Customer {
 
     private int money;
+    private List<Lotto> lottos;
 
     public Customer(String money){
         this.money=Integer.parseInt(money);
     }
 
-    public List<lotto> pay(int money){
-
-        return null;
+    public void pay(int money){
+        IssuingMachine issuingMachine=IssuingMachine.turnOn(money);
+        lottos=issuingMachine.issueLotto();
     }
 
     public void validateMoney(){

1a83b6778bda9f7ada309328211075bd03641ca7
SUJI-J <suzy852@naver.com>
2023-11-05 18:02:34 +0900

    feat: 구입한 로또 개수 출력

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,9 +1,15 @@
 package lotto;
 
 import static lotto.Constants.REQUEST_BUY_MONEY;
+import static lotto.Constants.TICKET_COUNT;
 
 public class OutputView {
     public static void printRequestMoney() {
         System.out.println(REQUEST_BUY_MONEY);
+    }
+
+    public static void printTicketCount(int ticketCount) {
+        System.out.println();
+        System.out.println(ticketCount + TICKET_COUNT);
     }
 }

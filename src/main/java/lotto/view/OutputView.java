c99038d5f9829eef953a45fe609a150aff0daeab
mk3058 <kmk3058@icloud.com>
2023-11-05 20:35:02 +0900

    feat(OutputView): printGameResult 함수 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import lotto.domain.Lotto;
+import lotto.message.LottoResult;
 import lotto.message.Output;
 
 public class OutputView {
@@ -9,5 +12,16 @@ public class OutputView {
     public static void printIssueResults(List<Lotto> lottos) {
         Output.ISSUED_LOTTO_COUNT.print(lottos.size());
         lottos.forEach(Lotto::printNumbers);
+    }
+
+    public static void printGameResult(Map<LottoResult, Integer> result) {
+        Set<LottoResult> keys = result.keySet();
+
+        Output.RESULT.print();
+        keys.forEach(
+                key -> {
+                    key.print(result.get(key));
+                }
+        );
     }
 }

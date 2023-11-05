58a3db6197587de0ba3fdb76cb734ebbdea85287
constant94-dev <constant94@naver.com>
2023-11-05 20:29:33 +0900

    feat(view) 사용자 입력 메서드 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,31 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.services.Amount;
+import lotto.services.Bonus;
+import lotto.services.Prize;
+
+public class InputView {
+
+    public int inputForAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String amount = Console.readLine();
+
+        return new Amount().createAmount(amount);
+    }
+
+    public List<Integer> inputForPrizeNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String prize = Console.readLine();
+
+        return new Prize().createPrizeNumber(prize);
+    }
+
+    public int inputForBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonus = Console.readLine();
+
+        return new Bonus().createBonusNumber(bonus);
+    }
+}

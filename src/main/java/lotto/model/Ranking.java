1a45b3ece03a7a8c033889997a194e1b34fdc3fd
kyoo0115 <internetkyoomin@gmail.com>
2023-11-05 18:00:16 +0900

    feat: 입력 구현

---
+++ src/main/java/lotto/model/Ranking.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+public enum LottoPrize {
+    FIRST_PRIZE(6, 2000000000),
+    SECOND_PRIZE(5, 30000000),
+    THIRD_PRIZE(5, 1500000),
+    FOURTH_PRIZE(4, 50000),
+    FIFTH_PRIZE(3, 5000),
+    NO_PRIZE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    LottoPrize(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    // Getter methods
+    public int getMatchCount() { return matchCount; }
+    public int getPrize() { return prize; }
+
+    // Additional methods for checking matches and determining prize
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,3 +1,28 @@
 package lotto.view;
 
-public class InputView {}
+import static lotto.controller.InputController.numberList;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+    private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_LOTTO_WINNING = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static String lottoMoneyInput() {
+        System.out.println(INPUT_LOTTO_AMOUNT);
+        return Console.readLine();
+    }
+
+    public static List<Integer> inputLottoWinningNum() {
+        System.out.println(INPUT_LOTTO_WINNING);
+        return numberList(Console.readLine());
+    }
+
+    public static String bonusNumInput() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        return Console.readLine();
+    }
+}

2bdc8bb506d78b18a448d6ca745ae5f7d05751f1
Finn <wjstodsss@naver.com>
2023-11-05 19:53:12 +0900

    feat - View 사용자 입력 기능(Console)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,18 @@
 package lotto;
 
+import lotto.controllers.InputProcessor;
+
 import static lotto.views.MessageManager.getPurchaseAmountPromptMessage;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         getPurchaseAmountPromptMessage();
+
+        InputProcessor.readLine();
+
+
+
+        InputProcessor.close();
     }
 }

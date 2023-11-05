7c22c343ff1e96bbf0a75119ace31695dc91b9f7
ykmxxi <ykmxxi97@gmail.com>
2023-11-05 19:12:11 +0900

    feat(LottoGameController): 발행한 로또 수량 및 번호를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
+import lotto.domain.Lottos;
 import lotto.domain.Payment;
+import lotto.domain.strategy.AutoIssueStrategy;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,6 +10,8 @@ public class LottoGameController {
 
     public void run() {
         Payment payment = getPayment();
+        Lottos lottos = issueLottosByAuto(payment.calculatePurchasedLottoCount());
+        printPurchaseHistory(payment, lottos);
     }
 
     private Payment getPayment() {
@@ -18,6 +22,15 @@ public class LottoGameController {
             System.out.println(illegalArgumentException.getMessage());
             return getPayment();
         }
+    }
+
+    private Lottos issueLottosByAuto(final int totalCount) {
+        return new Lottos(totalCount, new AutoIssueStrategy());
+    }
+
+    private void printPurchaseHistory(final Payment payment, final Lottos lottos) {
+        OutputView.printPurchasedLottoCount(payment);
+        OutputView.printIssuedLottosNumbers(lottos.getLottoNumbersDto());
     }
 
 }

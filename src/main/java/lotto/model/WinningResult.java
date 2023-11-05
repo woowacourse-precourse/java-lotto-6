b5e682001f007261cd9d4295257c1b2eabb1f645
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 01:17:35 +0900

    feat(WinningResult): 생성자에 WinningResult 초기화 메서드 추가

--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -8,9 +8,19 @@ public class WinningResult {
 
     private final Map<LottoRank, Integer> winningResult = new HashMap<>();
 
+    public WinningResult() {
+        initWinningResult();
+    }
+
     public void addLottoRank(LottoRank lottoRank) {
         int winningResultValue = getWinningResultValue(lottoRank);
         winningResult.put(lottoRank, ++winningResultValue);
+    }
+
+    private void initWinningResult() {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            winningResult.put(lottoRank, GameConfig.ZERO.getValue());
+        }
     }
 
     private int getWinningResultValue(LottoRank lottoRank) {

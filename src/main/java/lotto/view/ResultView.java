da108dc362a77002b1f1ca5ee73cf56e86bd6b33
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 02:20:25 +0900

    feat: resultView 클래스 구현

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,3 +1,15 @@
 package lotto.view;
 
-public class ResultView {}
+import java.util.List;
+import lotto.model.Lotto;
+
+public class ResultView {
+    public static final String TICKET_COUNT = "개를 구매했습니다.";
+    public static void printTicketCount(int count) {
+        System.out.println(count + TICKET_COUNT);
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        lottos.forEach(System.out::println);
+    }
+}

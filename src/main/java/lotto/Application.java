24cc94c568e5e1f539b34504c82c0d45a5fe0f6b
sonjiseokk <thswltjr11@gmail.com>
2023-11-05 11:56:43 +0900

    fix: [ERROR] 메시지 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,14 @@ import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        LottoService lottoService = new LottoService(new LottoMachine());
-        LottoController lottoController = new LottoController(lottoService);
+        try {
+            // TODO: 프로그램 구현
+            LottoService lottoService = new LottoService(new LottoMachine());
+            LottoController lottoController = new LottoController(lottoService);
 
-        lottoController.play();
-
+            lottoController.play();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]" + e.getMessage());
+        }
     }
 }

d177518d33cc978d58691370c2f75f1961768597
dltjdn <hana40004@naver.com>
2023-11-05 18:31:02 +0900

    feat(OutputView): 당첨 내역 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,26 +2,36 @@ package lotto.view;
 
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.WinningInfo;
 
 public class OutputView {
     private static final String PURCAHSE_AMOUNT_FORMAT = "%d개를 구매했습니다.";
     private static final String WINNING_STATS_HEADER = "당첨 통계";
     private static final String DASH_LINE = "---";
-
+    private static final String WINNING_STATS_FORMAT = "%s (%,d원) - %d개";
     private static final String TOTAL_RATE_OF_RETURN_FORMAT = "총 수익률은 %.1f%%입니다.";
 
-    public void printPurchaseAmount(int purchaseAmount){
-        System.out.printf(PURCAHSE_AMOUNT_FORMAT,purchaseAmount);
+    public void printPurchaseAmount(int purchaseAmount) {
+        System.out.printf(PURCAHSE_AMOUNT_FORMAT, purchaseAmount);
     }
 
-    public void printLottos(List<Lotto> lottos){
+    public void printLottos(List<Lotto> lottos) {
         lottos.forEach(lotto -> {
             lotto.sort();
-            System.out.println(lotto +"\n");
+            System.out.println(lotto + "\n");
         });
     }
 
-    public void printTotalRateOfReturn(double rateOfReturn){
-        System.out.printf(TOTAL_RATE_OF_RETURN_FORMAT,rateOfReturn);
+    public void printWinningHeader() {
+        System.out.println(WINNING_STATS_HEADER);
+        System.out.println(DASH_LINE);
+    }
+
+    public void printWinnerStatus(WinningInfo winningInfo, int count){
+        System.out.printf(WINNING_STATS_FORMAT, winningInfo.matchedCount, winningInfo.price, count);
+    }
+
+    public void printTotalRateOfReturn(double rateOfReturn) {
+        System.out.printf(TOTAL_RATE_OF_RETURN_FORMAT, rateOfReturn);
     }
 }

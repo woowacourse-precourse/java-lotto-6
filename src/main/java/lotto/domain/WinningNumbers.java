027383cd326a8127bdab2ebf8ba8ea1e606d6c0b
Mr.Kim <joajy98@gmail.com>
2023-11-05 15:25:40 +0900

    feat: 당첨 번호 및 보너스 번호를 가지는 클래스 구현

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public class WinningNumbers {
+
+    private final Lotto winningNumbers;
+
+    private final int bonusNumber;
+
+    public WinningNumbers(Lotto winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber(){
+        return bonusNumber;
+    }
+}

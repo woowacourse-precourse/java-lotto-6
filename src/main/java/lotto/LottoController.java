33e21e6d51f3fc1a3c6364d94dc66fcc2145d2c5
SUJI-J <suzy852@naver.com>
2023-11-05 19:41:18 +0900

    feat: 로또 번호 생성 기능 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,16 +1,29 @@
 package lotto;
 
-import static lotto.OutputView.printTicketCount;
+import lotto.InputView;
 
+import static lotto.OutputView.printTicketCount;
+import static lotto.OutputView.printLottoList;
 
 public class LottoController {
+
     public void start() {
-        int ticketCount = inputLottoMoney();
+        Money money = inputLottoMoney();
+        int ticketCount = money.getTicketCount();
         printTicketCount(ticketCount);
+
+        Lottos lottos = getLottos(money);
+        printLottoList(lottos);
     }
 
-    private int inputLottoMoney() {
-        String inputMoney = InputView.inputRequestMoney();
-        return new Money(inputMoney).getTicketCount();
+    private Money inputLottoMoney() {
+        int ticket = InputView.inputRequestMoney();
+        return new Money(ticket);
+    }
+
+    private Lottos getLottos(Money money) {
+        GenerateLotto generateLotto = new GenerateLotto();
+        return new Lottos(generateLotto.lottoGroup((money.getTicketCount())));
     }
 }
+

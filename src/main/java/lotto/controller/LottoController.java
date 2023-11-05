f56db5a13e2c1b2b481a22b753af253fd922e8f4
JongukYang <didwhddnr123@naver.com>
2023-11-05 16:58:27 +0900

    feat: 입력값 정수 변환 메서드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,17 @@
 package lotto.controller;
 
 import lotto.validator.InputPurchaseAmountValidator;
+import lotto.view.InputConverter;
 import lotto.view.InputPreprocessor;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-
-import java.util.List;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
     private final InputPurchaseAmountValidator purchaseAmountValidator;
     private final InputPreprocessor preprocessor;
+    private final InputConverter converter;
     private static final int LOTTO_PRICE = 1000;
 
     public LottoController() {
@@ -19,19 +19,20 @@ public class LottoController {
         this.inputView = new InputView(this.outputView);
         this.purchaseAmountValidator = new InputPurchaseAmountValidator();
         this.preprocessor = new InputPreprocessor();
+        this.converter = new InputConverter();
     }
 
     public void play() {
-        String purchaseAmount = getValidatedPurchaseAmount();
+        int purchaseAmount = getValidatedPurchaseAmount();
     }
 
-    private String getValidatedPurchaseAmount() {
+    private int getValidatedPurchaseAmount() {
         while (true) {
             try {
                 String input = receivePurchaseAmount();
                 input = trimInput(input);
                 purchaseAmountValidator.validate(input);
-                return input;
+                return converter.convertToInteger(input);
             } catch (IllegalArgumentException e) {
                 outputView.printInputErrorMessage(e.getMessage());
             }
---
+++ src/main/java/lotto/view/InputConverter.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class InputConverter {
+    public static int convertToInteger(String input) throws NumberFormatException{
+        return Integer.parseInt(input);
+    }
+}

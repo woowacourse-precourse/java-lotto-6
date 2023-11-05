e70b51a2085025024eb842a3e233defa69a73d83
JiEung2 <https://github.com/JiEung2>
2023-11-05 22:44:59 +0900

    Feat: InputView, convertToInt 구현
    문자열로 입력된 숫자를 int형으로 바꾸는 메서드 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static int convertToInt(String buyPrice){
+        return Integer.parseInt(buyPrice);
+    }
+}

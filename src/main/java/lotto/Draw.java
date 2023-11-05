4ccebea0a34038f282ae8fed82ab6eb64bd75777
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-05 20:56:15 +0900

    feat : 입력 번호의 숫자 여부 검증

--- src/main/java/lotto/Draw.java
+++ src/main/java/lotto/Draw.java
@@ -10,6 +10,13 @@ public class Draw {
         String[] strings = input.split(",");
     }
 
+    void isInt(String str) {
+        String regex = "^[0-9]+$";
+        if (!str.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
+        }
+    }
+
     private void printWinningLottoNumsEnterGuideStatement() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }

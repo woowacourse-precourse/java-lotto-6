f3ac24bf2511fff2b749cba174d836d2d329d883
kimwanyoung <dhks2869@gmail.com>
2023-11-05 15:44:19 +0900

    refactor : OutputView 매직 리터럴 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,6 +11,14 @@ import lotto.domain.LottosDTO;
 public class OutputView {
 
     private static final String CURRENT_BOUGHT_LOTTO_MESSAGE = "개를 구매했습니다.";
+    private static final String TOTAL_RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+    private static final String FIFTH_RANK_MESSAGE = "3개 일치 (5,000원) - %d개\n";
+    private static final String FOURTH_RANK_MESSAGE = "4개 일치 (50,000원) - %d개\n";
+    private static final String THIRD_RANK_MESSAGE = "5개 일치 (1,500,000원) - %d개\n";
+    private static final String SECOND_RANK_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
+    private static final String FIRST_RANK_MESSAGE = "6개 일치 (2,000,000,000원) - %d개\n";
+    private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String DIVIDE_LINE = "---";
 
     private OutputView() {
     }
@@ -26,27 +34,24 @@ public class OutputView {
     }
 
     public static void displayAllLottosStatistics(LottoResultsDTO lottoResultsDTO) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(DIVIDE_LINE);
         printLottosStatistics(lottoResultsDTO.getResult());
     }
 
     public static void displayRateOfReturn(LottoResultsDTO lottoResultsDTO) {
         Map<LottoResults, Integer> result = lottoResultsDTO.getResult();
-        int lottoCount = result.values().stream()
-                .reduce(0, Integer::sum);
-        double rateOfReturn = (double) calculateWinningAmount(result) / (lottoCount * 1000L);
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", rateOfReturn * 100);
+        long buyLottoAmount = calculateLottoCount(result) * 1000L;
+        double rateOfReturn = (double) calculateWinningAmount(result) / buyLottoAmount;
+        System.out.printf(TOTAL_RATE_OF_RETURN_MESSAGE, rateOfReturn * 100);
     }
 
     private static void printLottosStatistics(Map<LottoResults, Integer> result) {
-        System.out.printf("3개 일치 (5,000원) - %d개\n", result.getOrDefault(LottoResults.FIFTH, 0));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", result.getOrDefault(LottoResults.FOURTH, 0));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", result.getOrDefault(LottoResults.THIRD, 0));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",
-                result.getOrDefault(LottoResults.SECOND, 0));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n",
-                result.getOrDefault(LottoResults.FIRST, 0));
+        System.out.printf(FIFTH_RANK_MESSAGE, result.getOrDefault(LottoResults.FIFTH, 0));
+        System.out.printf(FOURTH_RANK_MESSAGE, result.getOrDefault(LottoResults.FOURTH, 0));
+        System.out.printf(THIRD_RANK_MESSAGE, result.getOrDefault(LottoResults.THIRD, 0));
+        System.out.printf(SECOND_RANK_MESSAGE, result.getOrDefault(LottoResults.SECOND, 0));
+        System.out.printf(FIRST_RANK_MESSAGE, result.getOrDefault(LottoResults.FIRST, 0));
     }
 
     private static long calculateWinningAmount(Map<LottoResults, Integer> result) {
@@ -57,5 +62,11 @@ public class OutputView {
             totalAmount += winningAmount * rankCount;
         }
         return totalAmount;
+    }
+
+    private static int calculateLottoCount(Map<LottoResults, Integer> result) {
+        return result.values()
+                .stream()
+                .reduce(0, Integer::sum);
     }
 }

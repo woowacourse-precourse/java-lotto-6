07b7dba6f0b2650008504134e1a1e9b08ea6ddd7
seminss <2007ksm@naver.com>
2023-11-05 15:31:59 +0900

    feat: Lotto 객체 내부 검증 추가
    LottoTest 성공

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,22 +1,47 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.constant.LottoConfig.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateCounts(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    /*private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }*/
+    private void validateCounts(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_COUNT_NUMBER.getValue()) {
             throw new IllegalArgumentException();
         }
     }
 
-    public List<Integer> getNumbers(){
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream()
+                .anyMatch(n -> n < LOTTO_START_NUMBER.getValue() || n > LOTTO_END_NUMBER.getValue())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getNumbers() {
         return numbers;
     }
 }

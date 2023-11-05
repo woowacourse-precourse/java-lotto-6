2e410b78ad1a253eef81a6900a92359f862a82d2
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 13:23:32 +0900

    feat(purchasing): prompt for purchase amount until a positive integer is entered from console

--- src/main/java/lotto/Askable.java
+++ src/main/java/lotto/Askable.java
@@ -1,4 +1,8 @@
 package lotto;
 
 public interface Askable {
+    String inputPurchaseAmount = "구입금액을 입력해주세요.";
+    String nonPositiveIntegerError = "[ERROR] 구입금액은 양의 정수여야 합니다.";
+
+    <T> T ask();
 }
--- src/main/java/lotto/purchasing/PurchaseAmount.java
+++ src/main/java/lotto/purchasing/PurchaseAmount.java
@@ -1,4 +1,47 @@
 package lotto.purchasing;
 
-public class PurchaseAmount {
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.function.Predicate;
+import lotto.Askable;
+
+public class PurchaseAmount implements Askable {
+    String input;
+
+    @Override
+    public String ask() {
+        System.out.println(inputPurchaseAmount);
+
+        do {
+            input = readLine();
+
+        } while (!validate(input));
+
+        System.out.println();
+
+        return input;
+    }
+
+    private boolean validate(String input) {
+        boolean corretInput = true;
+
+        try {
+            if (!isPositiveInteger.test(input)) {
+                throw new IllegalArgumentException(nonPositiveIntegerError);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            corretInput = false;
+        }
+
+        return corretInput;
+    }
+
+    private final Predicate<String> isPositiveInteger = input -> {
+        try {
+            return Integer.parseInt(input) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    };
 }

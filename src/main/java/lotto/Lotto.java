e874057f41c271e65721320dc5ef1e81cef89f66
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-05 22:10:35 +0900

    feat : 예외 발생 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,21 +19,21 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개입니다.");
         }
     }
 
     private void isWithinRange(List<Integer> numbers) {
         for (Integer num : numbers) {
             if (!(num >= lottoRangeFirstNum && num <= lottoRangeLastNum)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45입니다.");
             }
         }
     }
 
     private void hasDuplicateNum(List<Integer> numbers) {
         if (Set.copyOf(numbers).size() != numbers.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
         }
     }
 }

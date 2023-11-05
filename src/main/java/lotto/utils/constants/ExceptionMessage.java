54f2e9f11a487aeb68865ead082f9750fe10bc35
ohjuyoungg <ejrm12@naver.com>
2023-11-05 16:39:23 +0900

    feat: 당첨 번호 입력에 대한 예외 메시지 추가 설정

--- src/main/java/lotto/utils/constants/ExceptionMessage.java
+++ src/main/java/lotto/utils/constants/ExceptionMessage.java
@@ -1,9 +1,13 @@
 package lotto.utils.constants;
 
 public enum ExceptionMessage {
-    PURCHASE_AMOUNT_NOT_NUMBER("숫자로 된 구매 금액을 입력해주세요."),
-    PURCHASE_AMOUNT_BLANK("아무 값도 입력하지 않았습니다. 숫자로 된 구매 금액을 입력해주세요."),
-    PURCHASE_AMOUNT_NOT_THOUSAND("1,000원 단위의 구매 금액을 입력해주세요.");
+    INPUT_STRING_NOT_NUMBER("문자열이 아닌 숫자로 된 값을 입력해주세요."),
+    INPUT_NUMBER_BLANK("아무 값도 입력하지 않았습니다. 숫자로 된 값을 입력해주세요."),
+    PURCHASE_AMOUNT_NOT_THOUSAND("1,000원 단위의 구매 금액을 입력해주세요."),
+    WINNING_LOTTO_NUMBER("6개의 숫자를 콤마로 구분하여 입력해주세요."),
+    WINNING_LOTTO_DUPULICATED_NUMBER("중복된 숫자를 입력했습니다."),
+    WINNIG_LOTTO_INCLUDED_VALUE_NUMBER("1에서 45 사이의 숫자를 입력해야 합니다."),
+    WINNIG_LOTTO_SIX_VALUE_NUMBER("6개의 숫자를 입력해야 합니다.");
 
     private final String message;
 

c3ea684ef03396684533697bb9256fd105f134b1
minsub <chrkb1569@naver.com>
2023-11-06 01:48:38 +0900

    feat : 오류 메세지를 관리하기 위한 열거형 클래스 생성

---
+++ src/main/java/lotto/exception/GameExceptionMessage.java
@@ -0,0 +1,19 @@
+package lotto.exception;
+
+// 예외가 발생하는 경우, 오류와 함께 사용자에게 반환할 메세지
+public enum GameExceptionMessage {
+    INPUT_VALUE_IS_NOT_NUMBER("유효하지 않은 값을 입력하셨습니다. 숫자가 맞는지 확인해주세요."),
+    INPUT_VALUE_NOT_VALID_FORMAT("로또를 구매할 수 있는 금액은 1,000 단위로 입력해야합니다. 다시 입력해주세요."),
+    INPUT_VALUE_NOT_VALID_LENGTH("로또 번호는 6개의 수로 이루어져야합니다. 다시 한 번 확인해주세요."),
+    INPUT_VALUE_NOT_VALID_RANGE("1이상, 45이하의 값을 입력해주세요.")
+    ;
+    private final String message;
+
+    GameExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}

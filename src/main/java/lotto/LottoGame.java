fd5204289e6dc0a60674a015d6adca84c479e0bf
sunwooiljoon <wnsdl1659@naver.com>
2023-11-05 15:36:19 +0900

    feat : 당첨 번호 및 보너스 번호 입력 기능 추가
    WinningLotto
    - Lotto 상속 받아 보너스 번호 추가
    - 보너스 번호 검증

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,23 +1,45 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoGame {
     private List<Lotto> lottos;
     private MoneyToBuy moneyToBuy;
+    private WinningLotto winningLotto;
 
     public LottoGame() {
     }
 
     public void run() {
         requestMoneyToBuy();
-
+        requestWinningLotto();
     }
 
     private void requestMoneyToBuy() {
         System.out.println(OutputMessage.REQUEST_MONEY_TO_BUY.getMessage());
         String string = Console.readLine();
-        moneyToBuy = MoneyToBuy.stringToMoney(string);
+        moneyToBuy = new MoneyToBuy(Integer.parseInt(string));
+    }
+
+    private void requestWinningLotto() {
+        System.out.println(OutputMessage.REQUEST_WINNING_NUMBERS.getMessage());
+        String winningLottoNumbers = Console.readLine();
+
+        System.out.println(OutputMessage.REQUEST_BONUS_NUMBER.getMessage());
+        String bonusNumber = Console.readLine();
+
+        winningLotto = new WinningLotto(convertStringToList(winningLottoNumbers), Integer.parseInt(bonusNumber));
+    }
+
+    private List<Integer> convertStringToList(String string) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.stream(string.split(",")).toList().forEach(str -> {
+            list.add(Integer.parseInt(str));
+        });
+
+        return list;
     }
 }
---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+    private Integer bonusNumber;
+    public WinningLotto(List<Integer> numbers, Integer bonusNumber) {
+        super(numbers);
+        validBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validBonusNumber(Integer bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

d72ecdc8179ca33e66f94b11c2234addb9131df1
손지현_회원/결제개발팀 <hdg12jh@interpark.com>
2023-11-05 15:30:53 +0900

    feat Rank Enum 생성

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,5 +1,10 @@
 package lotto.domain;
 
+
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Rank {
     FIFTH(3,5000, "3개 일치 (5,000원)"),
     FOURTH(4,50000, "4개 일치 (50,000원)"),
@@ -16,6 +21,35 @@ public enum Rank {
     private final int count;
     private final int amount;
     private final String message;
+
+    public static final Map<Rank, Integer> rankCount = new EnumMap<>(Rank.class);
+    static {
+        for(Rank rank : Rank.values()){
+            rankCount.put(rank, 0);
+        }
+    }
+    public static void addRankCount(Rank rank){
+        rankCount.put(rank, rankCount.get(rank)+1);
+    }
+
+    public static int getRankCount(Rank rank){
+        return rankCount.get(rank);
+    }
+
+    public static double getTotalEarning() {
+        int totalEarning = 0;
+        for(Rank rank : Rank.values()) {
+            totalEarning += rank.amount * getRankCount(rank);
+        }
+        return totalEarning;
+    }
+    private static final Map<Integer, Rank> ranking = new HashMap<>(){{
+        put(FIFTH.count, FIFTH);
+        put(FOURTH.count, FOURTH);
+        put(THIRD.count, THIRD);
+        put(FIRST.count, FIRST);
+    }};
+    public static Rank getRank(int count){return ranking.get(count);}
 
     public int getCount() {
         return count;

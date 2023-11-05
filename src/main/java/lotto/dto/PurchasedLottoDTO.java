fb4d65ae12be8bc0d9f80401bba0b7458fd51b05
howudong <tjdvy963@naver.com>
2023-11-05 15:22:09 +0900

    refactor : 내부 클래스를 record로 간소화

--- src/main/java/lotto/dto/PurchasedLottoDTO.java
+++ src/main/java/lotto/dto/PurchasedLottoDTO.java
@@ -8,27 +8,9 @@ public final class PurchasedLottoDTO {
     private PurchasedLottoDTO() {
     }
 
-    public static class Input {
-        private final Long totalPayPrice;
-
-        public Input(Long totalPayPrice) {
-            this.totalPayPrice = totalPayPrice;
-        }
-
-        public Long getTotalPayPrice() {
-            return totalPayPrice;
-        }
+    public record Input(Long totalPayPrice) {
     }
 
-    public static class Output {
-        private final List<Lotto> purchasedLotto;
-
-        public Output(List<Lotto> purchasedLotto) {
-            this.purchasedLotto = purchasedLotto;
-        }
-
-        public List<Lotto> getPurchasedLotto() {
-            return purchasedLotto;
-        }
+    public record Output(List<Lotto> purchasedLotto) {
     }
 }

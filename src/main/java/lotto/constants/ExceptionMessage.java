f2f6bbb015c8a260039687a15331104bd77639e5
nove1080 <skwpqjq@gmail.com>
2023-11-05 15:56:12 +0900

    feat: 예외 메시지 상수 추가

---
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -0,0 +1,22 @@
+package lotto.constants;
+
+public enum ExceptionMessage {
+
+    LABEL("[ERROR]"),
+    NOT_DIVISIBLE_1000("1,000원 단위로만 입력 가능합니다."),
+    OUT_OF_RANGE("1,000 ~ 2,147,483,000 사이의 값만 허용합니다."),
+    INVALID_LOTTO_NUMBER_FORMAT("공백없이 콤마(,)를 기준으로 구분하여 숫자를 입력하세요."),
+    DUPLICATE_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    DUPLICATE_LOTTO_NUMBER("당첨 번호는 중복될 수 없습니다.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}

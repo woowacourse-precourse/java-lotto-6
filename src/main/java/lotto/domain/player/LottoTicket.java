f9ccd2191ec5bf5fbcae110981cb731f0eca60eb
newh08 <newh08@gmail.com>
2023-11-06 07:15:31 +0900

    ♻️REFACTOR: LottoTicket 생성자 제한범위, 변수명 변경

--- src/main/java/lotto/domain/player/LottoTicket.java
+++ src/main/java/lotto/domain/player/LottoTicket.java
@@ -3,14 +3,11 @@ package lotto.domain.player;
 import java.util.function.Supplier;
 import lotto.domain.lotto.LottoBundle;
 import lotto.domain.lotto.Lotto;
-import lotto.domain.player.playermoney.PlayerWallet;
 
 public class LottoTicket {
-    private static final int LOTTO_PRICE = 1000;
-
     private final int lottoTicket;
 
-    private LottoTicket(int lottoTicket) {
+    public LottoTicket(int lottoTicket) {
         this.lottoTicket = lottoTicket;
     }
 
@@ -22,9 +19,9 @@ public class LottoTicket {
         if (lottoTicket == 0) {
             return makeZeroLottoTicket();
         }
-        addLottoToLottoBundle(randomLotto.get(), lottoBundle);
+        addLottoToLottoBundle(randomLottoSupplier.get(), lottoBundle);
         int newTicketCount = lottoTicket - 1;
-        return new LottoTicket(newTicketCount).changeAllTicketToLotto(randomLotto, lottoBundle);
+        return new LottoTicket(newTicketCount).changeAllTicketToLotto(randomLottoSupplier, lottoBundle);
     }
 
     private static void addLottoToLottoBundle(Lotto randomLotto, LottoBundle lottoBundle) {

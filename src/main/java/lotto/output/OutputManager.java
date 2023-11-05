8f6d051447092a2caca82d9a80c9ee9252efe143
robinjoon <robin980108@naver.com>
2023-11-06 01:48:47 +0900

    feat(입력 가이드 출력 기능) : 기능 구현
    하위 기능들이 단순 문자열 출력 기능이므로 한번에 구현했다.

--- src/main/java/lotto/output/OutputManager.java
+++ src/main/java/lotto/output/OutputManager.java
@@ -4,4 +4,16 @@ public class OutputManager {
     public static void print(Object outPutContents) {
         System.out.println(outPutContents);
     }
+
+    public static void printLottoBuyMoneyInputGuide() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printAnswerNumbersInputGuide() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printBonusNumberInputGuide() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }

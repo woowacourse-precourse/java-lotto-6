5320d04ce289305231db412d4969327064e99332
JiEung2 <https://github.com/JiEung2>
2023-11-05 21:30:44 +0900

    Test: 입력된 로또 구입 금액이 숫자가 아닐 때 예외 처리 테스트 구현
    입력된 로또 구입 금액이 숫자가 아닐 때 로또 구입 금액은 숫자를 입력해야 합니다. 라는 에러메시지가 출력되는지 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -10,6 +10,16 @@ import org.junit.jupiter.api.Test;
 public class InputViewTest {
 
     @Test
+    void 입력된_로또_구입_금액이_숫자가_아닐_때(){
+        //given
+        String buyPrice = "천원";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new BuyPriceValidator(buyPrice));
+        //then
+        assertThat(e.getMessage()).isEqualTo("[ERROR] 로또 구입 금액은 숫자를 입력해야 합니다.");
+    }
+
+    @Test
     void 입력된_로또_구입_금액이_1000으로_나누어_떨어지지_않을_때(){
         //given
         String buyPrice = "12500";
@@ -18,4 +28,5 @@ public class InputViewTest {
         //then
         assertThat(e.getMessage()).isEqualTo("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야 합니다.");
     }
+
 }

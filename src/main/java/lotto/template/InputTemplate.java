952d918bcdcdc1809f610109961d241a79db8d6c
김승우 <tmd8633@naver.com>
2023-11-05 18:59:55 +0900

    Refactor : 재 입력부분 문제 해결

--- src/main/java/lotto/template/InputTemplate.java
+++ src/main/java/lotto/template/InputTemplate.java
@@ -9,18 +9,16 @@ import java.util.NoSuchElementException;
 public class InputTemplate {
 
     public String execute(String message, Callback callback) {
-        try {
-            System.out.println(message);
-            String text = Console.readLine();
-            callback.valid(text);
-            System.out.println();
-            return text;
-        } catch (NoSuchElementException e) {
-            System.out.println("[ERROR] 값이 존재하지 않습니다.");
-            return execute(message, callback);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] " + e.getMessage());
-            return execute(message, callback);
+        while (true) {
+            try {
+                System.out.println(message);
+                String text = Console.readLine();
+                callback.valid(text);
+                System.out.println();
+                return text;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
         }
     }
 

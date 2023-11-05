8de743539a24b7d9340f677bebc611d5b6d3eb9e
sung-yeop <jjabc9459@naver.com>
2023-11-05 23:20:05 +0900

    feat(InputController): 금액의 유효성 검사 기능 추가
    - 사용자가 입력한 금액의 타입과 단위의 유효성을 검사하는 기능 추가

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,15 +1,26 @@
-package lotto.view;
+package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validator.InputValidator;
 
-public class InputView {
-    private final Inputvali
+public class InputController {
+    private final InputValidator inputValidator;
 
-    public int inputAmount() {
-        String input = Console.readLine();
-
-
+    public InputController(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
     }
 
+    // Input : 1090 / Result : [ERROR] 출력 이후 다시 데이터 받음
+    public int inputAmountType() {
+        String input = Console.readLine();
+        int amount = 0;
+        try {
+            inputValidator.validateInputType(input);
+            amount = Integer.parseInt(input);
+            inputValidator.validateInputData(amount);
+        } catch (IllegalArgumentException e) {
+            inputAmountType();
+        }
+        return amount;
+    }
 }

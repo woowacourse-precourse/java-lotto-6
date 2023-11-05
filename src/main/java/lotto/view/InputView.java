1cc135697a99a6c0ac32ae4378b2450127d90123
linirini <2001yerin@naver.com>
2023-11-05 19:10:50 +0900

    feat : implement input purchase price

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import lotto.domain.Bonus;
 import lotto.domain.Lotto;
 import lotto.domain.Price;
@@ -7,7 +9,14 @@ import lotto.domain.Price;
 public class InputView{
 
     public Price inputPurchasePrice(){
-        return null;
+        System.out.println("구입 금액을 입력해주세요.");
+        int price = 0;
+        try{
+            price = Integer.parseInt(readLine());
+        }catch (NumberFormatException NFE){
+            throw new IllegalArgumentException();
+        }
+        return new Price(price);
     }
 
     public Lotto inputWinningNumbers(){

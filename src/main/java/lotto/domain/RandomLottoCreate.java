43382a1994c8ca6129003f0a12458c7cbd05dd4d
JongMinCh0i <wonderland314592@gmail.com>
2023-11-05 19:21:24 +0900

    feat(RandomLottoCreate): 랜덤 로또 번호 생성 기능 추가

---
+++ src/main/java/lotto/domain/RandomLottoCreate.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import static lotto.domain.LottoValue.LOTTO_RANGE_IN_END_VALUE;
+import static lotto.domain.LottoValue.LOTTO_RANGE_IN_START_VALUE;
+import static lotto.domain.LottoValue.LOTTO_SIZE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomLottoCreate {
+
+    public List<Lotto> create(int quantity) {
+        return IntStream.range(0, quantity)
+                .mapToObj(i -> generate())
+                .map(Lotto::new)
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_IN_START_VALUE,
+                LOTTO_RANGE_IN_END_VALUE,
+                LOTTO_SIZE);
+    }
+}

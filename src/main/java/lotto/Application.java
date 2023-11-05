8c3b86fc1fc7cb759ca64210df9a40e36fdc54be
wonchul98 <shinwc1998@naver.com>
2023-11-05 19:34:16 +0900

    2. 당첨 번호 입력 받기
    (기능 요구 사항 2번)
    1. Lotto class 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,49 @@
 package lotto;
+
 import lotto.Amount;
 
+import java.util.*;
+import java.util.Vector;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static Vector<Integer> lottoNumberVector = new Vector<>(LOTTO_NUMBERS_SIZE);
+    private static int bonusNumber;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Amount amount = new Amount(); //가격을 입력으로 받고 validationm함
+        Amount amount = new Amount(); //가격을 입력으로 받고 validation함
+
+        Lotto WinLotto = getWinNumbers(); // 로또 당첨 번호를 입력받음
 
     }
+
+    private static List<Integer> parseLottoNumbers(String lottoNums) { //입력 -> List<Integer>
+        String[] tokens = lottoNums.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String token : tokens) {
+            int number = Integer.parseInt(token.trim()); // 쉼표 다음에 공백이 올 수 있으므로 trim()을 사용
+            numbers.add(number);
+        }
+        return numbers;
+    }
+
+    private static Lotto getWinNumbers(){
+        while(true){
+            try {
+                System.out.print("당첨 번호를 입력하세요: ");
+                String LottoNums = Console.readLine();
+                List<Integer> LottoNumsIntList = parseLottoNumbers(LottoNums);
+                Lotto winLotto = new Lotto(LottoNumsIntList);
+                return winLotto;
+            }catch (NumberFormatException e) { //ParseInt에서 던지는 exception
+                System.out.println("[ERROR] 숫자 형식이 올바르지 않습니다.");
+            } catch (IllegalArgumentException e) { //Lotto class에서 던지는 exception
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,12 +14,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (!numbers.stream().allMatch(number -> number >= 1 && number <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 겹치지 않는 6자리 수를 입력해 주세요.");
         }
     }
 
-
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != LOTTO_NUMBERS_SIZE;
+    }
 
     // TODO: 추가 기능 구현
 }

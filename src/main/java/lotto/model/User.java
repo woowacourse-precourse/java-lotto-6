85b21ec8810d884fc4abfe857e28bd52f3540327
강승민 <rkdtmdals999@naver.com>
2023-11-05 20:27:00 +0900

    [feat] 유저가 구입한 정보를 담을 클래스 생성

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,2 +1,23 @@
-package lotto.model;public class User {
+package lotto.model;
+
+public class User {
+    private Integer purchaseAmount;
+    private Integer purchaseLottoNumber;
+
+    public Integer getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public void setPurchaseAmount(Integer purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public Integer getPurchaseLottoNumber() {
+        return purchaseLottoNumber;
+    }
+
+    public void setPurchaseLottoNumber(Integer purchaseLottoNumber) {
+        this.purchaseLottoNumber = purchaseLottoNumber;
+    }
+
 }

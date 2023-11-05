29d71989b9d898ab367a8188e91e19193053c140
LEE-sh1673 <lsh901673@gmail.com>
2023-11-06 00:30:16 +0900

    feat: 구매 가능한 로또 개수를 구하는 기능 추가

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -4,10 +4,13 @@ import static lotto.model.ErrorType.MONEY_CANT_DIVIDED_BY_PRIZE;
 import static lotto.model.ErrorType.MONEY_LESS_THEN_PRIZE;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Money {
 
-    private static final long LOTTO_PRIZE = 1_000L;
+    private static final BigDecimal LOTTO_PRIZE = BigDecimal.valueOf(1_000L);
+    private static final RoundingMode DEFAULT_ROUNDING = RoundingMode.HALF_EVEN;
+    private static final int INITIAL_SCALE = 0;
 
     private final BigDecimal amount;
 
@@ -26,14 +29,18 @@ public class Money {
     }
 
     private boolean isLessThenPrize(final long amount) {
-        return amount < LOTTO_PRIZE;
+        return amount < LOTTO_PRIZE.longValue();
     }
 
     private boolean isDividedByPrize(final long amount) {
-        return amount % LOTTO_PRIZE == 0;
+        return amount % LOTTO_PRIZE.longValue() == 0;
     }
 
     public static Money of(final long amount) {
         return new Money(amount);
+    }
+
+    public long calculateTicketCount() {
+        return amount.divide(LOTTO_PRIZE, INITIAL_SCALE, DEFAULT_ROUNDING).longValue();
     }
 }
--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -1,10 +1,12 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
@@ -26,5 +28,16 @@ public class MoneyTest {
         assertThatThrownBy(() -> Money.of(1001L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_PREFIX);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"5000:5", "50000:50", "1500000:1500"}, delimiter = ':')
+    @DisplayName("구매 가능한 로또 개수를 반환한다.")
+    void givenPurchaseAmount_Then_NumberOfTicketsReturns(
+            final long amount,
+            final long ticketCount
+    ) {
+        final Money money = Money.of(amount);
+        assertThat(money.calculateTicketCount()).isEqualTo(ticketCount);
     }
 }

977e7b309dd1d0e831fe213772fd65f009affbaa
alscjf1329 <alscjf1329@gmail.com>
2023-11-05 13:08:44 +0900

    test(LottoPurchaseInfoTest): 구매 금액에 맞게 lotto 개수를 생성하는 테스트 작성
    코드 구조 변경에 따라 LottoPurchaseInfo 객체를 통해 구매 금액에 맞게 lotto 개수를 생성하는 테스트를 작성했습니다.

--- src/test/java/lotto/LottoPurchaseInfoTest.java
+++ src/test/java/lotto/LottoPurchaseInfoTest.java
@@ -3,9 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
-import java.util.List;
-import lotto.domain.LottoLogic;
-import lotto.dto.LottoNumbersInfo;
+import lotto.dto.LottoPurchaseInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +11,7 @@ public class LottoPurchaseInfoTest {
     @Test
     @DisplayName("구매 금액에 맞게 lotto 개수를 생성한다.")
     void createNumberOfLottoPurchasedByPurchasedAmount() {
-        assertThat(new LottoLogic(new lotto.dto.LottoPurchaseInfo(BigDecimal.valueOf(321000), Lotto.PRICE),
-                new LottoNumbersInfo(List.of(), 1)).getNumberOfLottoPurchased()
+        assertThat(new LottoPurchaseInfo(new BigDecimal(321000), Lotto.PRICE).numberOfLottoPurchased
                 .compareTo(BigDecimal.valueOf(321)))
                 .isEqualTo(0);
     }

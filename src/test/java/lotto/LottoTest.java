bb3b876892583d48d7792d60d9c619c2b063c063
idingg <idingg@gmail.com>
2023-11-05 18:58:25 +0900

    test: 추가 로또 번호 예외처리

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +25,53 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 빈 입력이면 예외가 발생한다.")
+    @Test
+    void createLottoByEmptyNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 범위가 1~45가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 50, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 음수면 예외가 발생한다.")
+    @Test
+    void createLottoByNaturalNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, -3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 소수면 예외가 발생한다.")
+    @Test
+    void createLottoByFloatNumber() {
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck("1,2,3,4.1,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByNotANumber() {
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck("1,2,3,4,%,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 쉼표로 구분되지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoByNoComma() {
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck("1,2,3,4,5#6"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck(",1,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck("1,2,3,4,5,6,"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck("1,2,3,4,5,,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validation.inputWinnerNumberExceptionCheck("1,2,,,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

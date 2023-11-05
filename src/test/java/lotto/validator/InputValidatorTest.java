677d00777d9cbb2af25f0c0bd0f7d1b75cb177a0
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-05 16:44:42 +0900

    test: 당첨 번호 예외 처리 테스트 추가
    예외 처리 잘 발동하는 지 ERROR와 해당 에러 메세지
    판독 테스트

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -1,6 +1,10 @@
 package lotto.validator;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
@@ -10,12 +14,16 @@ public class InputValidatorTest {
     private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
     private static final String BLANK_ERROR_MESSAGE = "공백 없이 입력해주세요.";
     private static final String DIVISION_ERROR_MESSAGE = "1,000 단위로 입력해주세요.";
-    private InputMoneyValidator inputMoneyValidator;
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자를 적으면 안 됩니다.";
+    private static final String SIZE_OVER_MESSAGE = "6개의 번호를 입력해주세요.";
+    private static final String RANGE_OVER_MESSAGE = "1~45사이의 번호를 입력해주세요.";
+    private MoneyValidator inputMoneyValidator;
+    private WinningNumValidator winningNumValidator;
 
     @Test
     void 값_미전달_예외_처리_테스트(){
         String input = "";
-        assertThatThrownBy(() -> inputMoneyValidator = new InputMoneyValidator(input))
+        assertThatThrownBy(() -> inputMoneyValidator = new MoneyValidator(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR+NULL_ERROR_MESSAGE);
     }
@@ -23,7 +31,7 @@ public class InputValidatorTest {
     @Test
     void 공백_포함_예외_처리_테스트(){
         String input = "ad sf";
-        assertThatThrownBy(() -> inputMoneyValidator = new InputMoneyValidator(input))
+        assertThatThrownBy(() -> inputMoneyValidator = new MoneyValidator(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR+BLANK_ERROR_MESSAGE);
     }
@@ -31,7 +39,7 @@ public class InputValidatorTest {
     @Test
     void 입력_값_숫자_판별_예외_처리_테스트(){
         String input = "adsf";
-        assertThatThrownBy(() -> inputMoneyValidator = new InputMoneyValidator(input))
+        assertThatThrownBy(() -> inputMoneyValidator = new MoneyValidator(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR+NOT_NUMBER_ERROR_MESSAGE);
     }
@@ -39,9 +47,49 @@ public class InputValidatorTest {
     @Test
     void 입력_값_1000_배수_예외_처리_테스트(){
         String input = "1001";
-        assertThatThrownBy(() -> inputMoneyValidator = new InputMoneyValidator(input))
+        assertThatThrownBy(() -> inputMoneyValidator = new MoneyValidator(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR+DIVISION_ERROR_MESSAGE);
+    }
+
+    @Test
+    void 당첨_번호_숫자_외_입력_예외_처리_테스트(){
+        String input = "1,2,3,a,5";
+        assertThatThrownBy(() -> winningNumValidator = new WinningNumValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR+NOT_NUMBER_ERROR_MESSAGE);
+    }
+
+    @Test
+    void 당첨_번호_Size_초과_예외_처리_테스트(){
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
+        assertThatThrownBy(() -> winningNumValidator = new WinningNumValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR+SIZE_OVER_MESSAGE);
+    }
+
+    @Test
+    void 당첨_번호_Size_미달_예외_처리_테스트(){
+        List<Integer> input = List.of(1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> winningNumValidator = new WinningNumValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR+SIZE_OVER_MESSAGE);
+    }
+
+    @Test
+    void 당첨_번호_중복_예외_처리_테스트(){
+        List<Integer> input = List.of(1, 1, 3, 4, 5, 6);
+        assertThatThrownBy(() -> winningNumValidator = new WinningNumValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR+DUPLICATE_ERROR_MESSAGE);
+    }
+
+    @Test
+    void 당첨_번호_범위_예외_처리_테스트(){
+        List<Integer> input = List.of(1, 49, 3, 4, 5, 6);
+        assertThatThrownBy(() -> winningNumValidator = new WinningNumValidator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR+RANGE_OVER_MESSAGE);
     }
 
 }

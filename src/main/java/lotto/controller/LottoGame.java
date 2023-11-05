da7c3f67f5240455d4f9c5aa30bf876346764548
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-06 01:04:07 +0900

    Feat: 로또 구매금액 입력

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,2 +1,33 @@
-package lotto.controller;public class LottoGame {
+package lotto.controller;
+
+import lotto.domain.PurchaseManager;
+import lotto.view.InputView;
+
+public class LottoGame {
+    private static InputView inputView;
+    private static PurchaseManager purchaseManager;
+
+    public LottoGame(){
+        inputView = new InputView();
+        //todo outputView
+    }
+
+    public void startGame(){
+        purchase();
+    }
+    private void purchase(){
+        boolean validateInput=false;
+        while(!validateInput){
+            validateInput = validatePurchaseAmount(inputView.inputPurchaseAmount());
+        }
+    }
+    private boolean validatePurchaseAmount(String purchaseAmount){
+        try {
+            purchaseManager = new PurchaseManager(purchaseAmount);
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
--- src/main/java/lotto/domain/PurchaseManager.java
+++ src/main/java/lotto/domain/PurchaseManager.java
@@ -1,2 +1,35 @@
-package lotto.domain;public class PurchaseManager {
+package lotto.domain;
+
+import lotto.util.Constant;
+import lotto.util.ErrorConstant;
+
+import java.util.regex.Pattern;
+
+public class PurchaseManager {
+    private Integer purchaseNumber;
+
+    public PurchaseManager(String purchaseAmount){
+        isNumber(purchaseAmount);
+        this.purchaseNumber = Integer.parseInt(purchaseAmount);
+        canPurchase();
+        hasNoChange();
+    }
+
+    private void isNumber(String purchaseAmount) {
+        if(!Pattern.matches(Constant.REGEXP_PATTERN_NUMBER, purchaseAmount)){
+            throw new IllegalArgumentException(ErrorConstant.NOT_A_NUMBER);
+        }
+    }
+
+    private void canPurchase() {
+        if(purchaseNumber<Constant.LOTTO_PRICE){
+            throw new IllegalArgumentException(ErrorConstant.CANNOT_PURCHASE);
+        }
+    }
+
+    private void hasNoChange() {
+        if(purchaseNumber%Constant.LOTTO_PRICE!=0){
+            throw new IllegalArgumentException(ErrorConstant.HAS_CHANGE);
+        }
+    }
 }

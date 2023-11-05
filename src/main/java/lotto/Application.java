2cf9020d3e5bb901f04312e963879416ce732c7a
Yoo KyeongMi <yookyungmi02@gmail.com>
2023-11-05 19:57:42 +0900

    feat(LottoManager): 사용자의 구입금액에 따른 로또 생성 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoInputController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoInputController inputController = LottoInputController.getInstance();
+        
     }
 }
---
+++ src/main/java/lotto/domain/LottoManager.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
+public class LottoManager {
+    private static final LottoManager lottoManager = new LottoManager();
+    private static final int LOTTO_PRICE = 1000;
+
+    private LottoManager() {
+    }
+
+    public LottoManager getInstance() {
+        return lottoManager;
+    }
+
+    public List<Lotto> purchaseLottos(int purchaseAmount) {
+        int numberOfLottos = purchaseAmount / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < numberOfLottos; i++) {
+            lottos.add(Lotto.generate());
+        }
+
+        return lottos;
+    }
+}

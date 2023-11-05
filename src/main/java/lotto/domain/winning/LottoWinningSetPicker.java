1b37464f5b8e74eee4087ac9806e64b37f6482cc
minseo  jo <liging12@naver.com>
2023-11-05 16:35:35 +0900

    refactor: 당첨 번호, 보너스 볼 뽑는 기능, 따로 클래스 분리
    LottoWinningSetPicker 추가

---
+++ src/main/java/lotto/domain/winning/LottoWinningSetPicker.java
@@ -0,0 +1,29 @@
+package lotto.domain.winning;
+
+import lotto.domain.generator.LottoManualGenerator;
+import lotto.domain.lotto.Lotto;
+import lotto.exception.LottoException;
+import lotto.vo.BonusNumber;
+
+import java.util.List;
+
+public class LottoWinningSetPicker {
+
+    private static final String delimiter = ",";
+
+    private final LottoManualGenerator lottoManualGenerator = new LottoManualGenerator();
+
+    public Lotto pickWinningNumbers(String input) throws LottoException {
+        List<String> splitLottoNumbers = splitLottoNumbers(input);
+        return lottoManualGenerator.generate(splitLottoNumbers);
+    }
+
+    public BonusNumber pickBonusNumber(String input) {
+        return new BonusNumber(Integer.parseInt(input));
+    }
+
+    private static List<String> splitLottoNumbers(String input) {
+        return List.of(input.split(delimiter));
+    }
+
+}

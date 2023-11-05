444ac89e3584d19ae4b6ec89dc1460b0b47d8dc5
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-05 17:35:26 +0900

    style (WinningStatisticsDto) : field 이름 변경
    - price -> lottoPrice로 변경
    - 어떤 가격을 의미하는지를 명시하지 않아 변경함

--- src/main/java/lotto/dto/WinningStatisticsDto.java
+++ src/main/java/lotto/dto/WinningStatisticsDto.java
@@ -3,7 +3,7 @@ package lotto.dto;
 import java.util.Map;
 import lotto.domain.WinningRanking;
 
-public record WinningStatisticsDto(Map<WinningRanking, Integer> countOfRanking, int price) {
+public record WinningStatisticsDto(Map<WinningRanking, Integer> countOfRanking, int lottoPrice) {
 
     private static final int NOT_EXIST = 0;
     private static final int RATE_TO_PERCENTAGE = 100;
@@ -12,12 +12,12 @@ public record WinningStatisticsDto(Map<WinningRanking, Integer> countOfRanking, 
         long sum = sumOfPrice();
         long totalCount = calculateTotalCount();
 
-        return (double) sum / (totalCount * price) * RATE_TO_PERCENTAGE;
+        return (double) sum / (totalCount * lottoPrice) * RATE_TO_PERCENTAGE;
     }
 
     private long sumOfPrice() {
         return countOfRanking.keySet().stream()
-                .mapToLong(rank -> sumOfPrice(rank, countOfRanking.getOrDefault(rank,NOT_EXIST)))
+                .mapToLong(rank -> sumOfPrice(rank, countOfRanking.getOrDefault(rank, NOT_EXIST)))
                 .sum();
     }
 

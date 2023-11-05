eb72bbf449a155292723698dc2ed0ff5d4505096
ykmxxi <ykmxxi97@gmail.com>
2023-11-05 19:15:19 +0900

    feat(OutputView): 당첨 번호 입력 가이드를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
 
     private static final String PAYMENT_GUIDE = "구입금액을 입력해 주세요.";
     private static final String PURCHASED_LOTTO_COUNT_GUIDE = "%d개를 구매했습니다.";
+    private static final String WINNING_LOTTO_NUMBERS_GUIDE = "당첨 번호를 입력해 주세요.";
 
     private OutputView() {
     }
@@ -27,6 +28,10 @@ public class OutputView {
         System.out.println();
         lottoNumbersDto.getLottosNumbers()
                 .forEach(System.out::println);
+    }
+
+    public static void printWinningLottoNumbersGuide() {
+        System.out.println(WINNING_LOTTO_NUMBERS_GUIDE);
     }
 
 }

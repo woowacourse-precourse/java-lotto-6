0b8ce8549281eebbe363c059bb22c8f3aa0a5698
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-11-05 18:04:23 +0900

    feat: LottoPurcharseService  - 로또구매를 담당하는 서비스 객체

---
+++ src/main/java/lotto/service/LottoPurchaseService.java
@@ -0,0 +1,17 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoStore;
+
+public final class LottoPurchaseService {
+    private final LottoStore lottoStore;
+
+    public LottoPurchaseService(LottoStore lottoStore) {
+        this.lottoStore = lottoStore;
+    }
+
+    public List<Lotto> buyLottos(int investmentAmount) {
+        return lottoStore.purchaseLottos(investmentAmount);
+    }
+}

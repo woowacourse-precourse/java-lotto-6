cd8aa99635da868a6ae317ebd4b38db0393510ab
giwoong <dev.choi0409@gmail.com>
2023-11-05 17:37:58 +0900

    feat: 당첨 번호 입력, 보너스 번호 입력 구현

--- src/main/java/lotto/controller/GameMachine.java
+++ src/main/java/lotto/controller/GameMachine.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumbers;
 import lotto.domain.RandomLottoNumbers;
@@ -14,7 +15,7 @@ public class GameMachine {
     }
 
     public void start() {
-        // 구입 금액 입력 , 1000으로 나눈다.
+        // 사용자가 구입 금액 입력 , 1000으로 나눈다.
         System.out.println("구입금액을 입력해 주세요.");
         int inputPrice = Integer.parseInt(Console.readLine());
         int count = inputPrice / 1000;
@@ -28,6 +29,21 @@ public class GameMachine {
             System.out.println(lotto.getNumbers());
         }
 
+        // 사용자가 당첨 번호 6자리를 입력.
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<String> inputWinningNumbers = List.of(Console.readLine().split(","));
+        // 당첨 번호 int형 list로 변환
+        List<Integer> winningNumbers = inputWinningNumbers.stream()
+                .map(Integer::parseInt)
+                .toList();
+
+        // 보너스 번호 입력
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int inputBonusNumber = Integer.parseInt(Console.readLine());
+
+        
     }
 
 }

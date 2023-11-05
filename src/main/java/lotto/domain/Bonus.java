88a70fd4eb41abdeeefb33fd60bba28aea93613c
msung99 <msung6924@naver.com>
2023-11-05 10:54:18 +0900

    feat: 보너스 번호를 1~45 사이의 숫자를 입력받았는지 체크하는 기능 구현

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -3,6 +3,9 @@ package lotto.domain;
 public class Bonus {
     private final int bonus;
     private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 보너스 번호는 숫자만 입력할 수 있습니다.";
+    private static final String NUMBER_PARSE_EXCEPTION_MESSAGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자를 입력해야 합니다.";
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
 
     private Bonus(int bonus){
         validateOutOfRange(bonus);
@@ -23,6 +26,8 @@ public class Bonus {
     }
 
     public void validateOutOfRange(int bonus){
-
+        if(bonus < MIN_NUMBER || bonus > MAX_NUMBER){
+            throw new IllegalArgumentException(NUMBER_PARSE_EXCEPTION_MESSAGE);
+        }
     }
 }

85d5873930e503da53a3fe46c21677e68d7040fc
cheolhyeon <tghy4@naver.com>
2023-11-05 17:38:00 +0900

    feat : 로또 번호를 생성해서 리스트에 담아 로또 객체를 생성하는 기능과, 사용자에게 입력받은 금액만큼 로또 객체를 생성해서 리스트에 담는 기능 구현

--- src/main/java/lottogenerate/LottoGenerator.java
+++ src/main/java/lottogenerate/LottoGenerator.java
@@ -2,7 +2,10 @@ package lottogenerate;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lotto.Lotto;
 import user.User;
 
@@ -11,28 +14,37 @@ public class LottoGenerator {
     private static final int MIN_LOTTO_NUMBER_RANGE = 1;
     private static final int MAX_LOTTO_NUMBER_COUNT = 6;
     private User user;
+    private final List<Lotto> lottoList;
 
     public LottoGenerator(User user) {
+        this.lottoList = new ArrayList<>();
         this.user = user;
     }
-    public Lotto createLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange
-                (MIN_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_COUNT);
+    public List<Lotto> generateLotto() {
+        return IntStream.range(0, lottoTicket())
+                .mapToObj(i -> createLotto())
+                .collect(Collectors.toList());
+        // 반환한 로또 정보를 뒤져가지고 사용자가 입력한 위닝넘버와 비교를 해야함.
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+
+    private Lotto createLotto() {
+        List<Integer> numbers = getLottoNumbers();
+        numbers.sort(Comparator.naturalOrder());
+
         return new Lotto(numbers);
     }
 
-    public List<Lotto> generateLotto() {
-
-        List<Lotto> lottoList = new ArrayList<>();
-
-        for (int i = 0; i < lottoTicket(); i++) {
-            lottoList.add(createLotto());
-        }
-        return lottoList;
-        // 반환한 로또 정보를 뒤져가지고 사용자가 입력한 위닝넘버와 비교를 해야함.
-
+    private static List<Integer> getLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange
+                (MIN_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_COUNT);
     }
+
     private int lottoTicket() {
         return Integer.parseInt(user.getInputBuyLottoTicket()) / 100;
     }
+
 }

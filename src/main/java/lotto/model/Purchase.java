44e6800af5f628578f86828084e6271f3f015f1d
Yehyeok Bang <qkddpgur318@gmail.com>
2023-11-05 15:25:06 +0900

    feat(model): 구매를 담당하는 클래스 구현
    기존 입력 값을 검증하는 클래스를 제거합니다.
    - 구매를 담당하는 클래스의 객체가 스스로 값을 검증할 수 있게 변경합니다.
    - 테스트 코드도 이에 맞게 변경합니다.

---
+++ src/main/java/lotto/model/Purchase.java
@@ -0,0 +1,57 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Purchase<T> {
+    private final static int PURCHASE_MONEY_UNIT = 1_000;
+    private final static String PURCHASE_MONEY_NOT_NUMBER = "[ERROR] 구입 금액은 숫자여야 합니다.";
+    private final static String PURCHASE_MONEY_MINIMUM_LIMIT = "[ERROR] 구입 금액은 1000원 이상이어야 합니다.";
+    private final static String PURCHASE_MONEY_UNIT_LIMIT = "[ERROR] 구입 금액은 1000원 단위여야 합니다.";
+    private final static String PURCHASE_ITEMS_SIZE_NOT_MATCH = "[ERROR] 구입 금액과 구매 개수가 일치하지 않습니다.";
+
+    private final int money;
+    private List<T> items;
+
+    public Purchase(String inputMoney) {
+        this.money = validatePurchaseMoney(inputMoney);
+    }
+
+    private int validatePurchaseMoney(String inputMoney) {
+        int money;
+        try {
+            money = Integer.parseInt(inputMoney);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PURCHASE_MONEY_NOT_NUMBER);
+        }
+
+        if (money < PURCHASE_MONEY_UNIT) {
+            throw new IllegalArgumentException(PURCHASE_MONEY_MINIMUM_LIMIT);
+        }
+
+        if (money % PURCHASE_MONEY_UNIT != 0) {
+            throw new IllegalArgumentException(PURCHASE_MONEY_UNIT_LIMIT);
+        }
+
+        return money;
+    }
+
+    public void purchaseItems(List<T> items) {
+        this.items = validatePurchaseItems(items);
+    }
+
+    private List<T> validatePurchaseItems(List<T> items) {
+        if (items.size() != money / PURCHASE_MONEY_UNIT) {
+            throw new IllegalStateException(PURCHASE_ITEMS_SIZE_NOT_MATCH);
+        }
+
+        return items;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -1,11 +1,11 @@
-package lotto.util;
+package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class InputValidatorTest {
+class PurchaseTest {
     private final static String VALID_MONEY = "1000";
     private final static String INVALID_MONEY_NOT_NUMBER = "abc";
     private final static String INVALID_MONEY_MINIMUM_LIMIT = "999";
@@ -14,24 +14,24 @@ class InputValidatorTest {
     @Test
     @DisplayName("올바른 구입 금액이 입력된 경우")
     void validatePurchaseMoney() {
-        assertDoesNotThrow(() -> InputValidator.validatePurchaseMoney(VALID_MONEY));
+        assertDoesNotThrow(() -> new Purchase<Lotto> (VALID_MONEY));
     }
 
     @Test
     @DisplayName("구입 금액이 숫자가 아닌 경우")
     void validatePurchaseMoneyNotNumber() {
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validatePurchaseMoney(INVALID_MONEY_NOT_NUMBER));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase<Lotto> (INVALID_MONEY_NOT_NUMBER));
     }
 
     @Test
     @DisplayName("구입 금액이 1000원 미만인 경우")
     void validatePurchaseMoneyMinimumLimit() {
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validatePurchaseMoney(INVALID_MONEY_MINIMUM_LIMIT));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase<Lotto> (INVALID_MONEY_MINIMUM_LIMIT));
     }
 
     @Test
     @DisplayName("구입 금액이 1000원 단위가 아닌 경우")
     void validatePurchaseMoneyUnitLimit() {
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validatePurchaseMoney(INVALID_MONEY_UNIT_LIMIT));
+        assertThrows(IllegalArgumentException.class, () -> new Purchase<Lotto> (INVALID_MONEY_UNIT_LIMIT));
     }
 }

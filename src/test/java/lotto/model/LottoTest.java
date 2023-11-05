ce8e719f32cd12e9b3776955397a2f6b82b85306
metishonora <metishonora@icloud.com>
2023-11-05 21:26:57 +0900

    test: Lotto 테스트 클래스 생성

---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @DisplayName("번호 일치 개수")
+    @Test
+    void count() {
+        // given
+        List<Integer> first = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> second = List.of(2, 3, 4, 5, 6, 7);
+
+        // when
+        Lotto firstLotto = new Lotto(first);
+        Lotto secondLotto = new Lotto(second);
+        Long result = firstLotto.countSameNumbers(secondLotto);
+
+        // then
+        assertThat(result).isEqualTo(5L);
+    }
+
+    @DisplayName("보너스 번호 포함 여부")
+    @Test
+    void containsNumber() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        Boolean result = lotto.containsNumber(6);
+        Boolean result2 = lotto.containsNumber(20);
+
+        // then
+        assertThat(result).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+}

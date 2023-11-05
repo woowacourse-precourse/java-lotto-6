ad2f4eb0029eeb352d034cf8155356f4a98ab792
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-05 14:41:56 +0900

    test(PurchaseProcessorTest) : 입력 가격이 범위를 초과할 경우 예외 발생 확인 테스트 추가

--- src/test/java/lotto/domain/PurchaseProcessorTest.java
+++ src/test/java/lotto/domain/PurchaseProcessorTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.math.BigInteger;
 import lotto.constant.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,15 @@ class PurchaseProcessorTest {
         assertThatThrownBy(() -> new PurchaseProcessor("abc"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.INPUT_NUMBER.getMessage());
+    }
+
+    @DisplayName("입력 가격이 범위를 초과할 경우 예외 발생")
+    @Test
+    void priceOutOfRange() {
+        assertThatThrownBy(
+                () -> new PurchaseProcessor(BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE).toString()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.INVALID_AMOUNT_PRICE_MESSAGE.getMessage());
     }
 
 }

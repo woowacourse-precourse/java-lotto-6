458f049437445a0b7f5c7569d968bd72eb6fec61
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-06 02:12:45 +0900

    feat: 사용자가 입력하는 로또번호중 보너스 숫자를 제외한 6개 숫자 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,5 @@
 package lotto.view;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
@@ -11,13 +9,22 @@ public class InputView {
         List<Integer> lottoNumbers;
         System.out.println("당첨 번호를 입력해주세요.");
         try {
-            lottoNumbers = Arrays.stream(readLine().split(","))
-                    .map(Integer::parseInt)
-                    .toList();
+            lottoNumbers = stringListToIntList(Arrays.stream(Console.readLine().split(",")).toList());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputWinningLottoStandardNumber();
         }
         return lottoNumbers;
+    }
+
+    private List<Integer> stringListToIntList(List<String> stringInput) throws IllegalArgumentException{
+        try {
+            return stringInput.stream()
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR]: 숫자를 입력 하셔야하고 각 숫자는 ,로 구분되어야 합니다.");
+        }
     }
 }

2cd363cb38b04311b516657a6397ba46a57dfa10
kimdohyung <pdohyung@gmail.com>
2023-11-05 23:40:52 +0900

    feat(WinningLottoNumbers): 보너스 번호가 당첨 번호와 중복된 경우 예외 처리

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -3,11 +3,13 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningLottoNumbers {
+    private static final String INVALID_BONUS_NUMBER_DUPLICATE_MESSAGE = "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningLottoNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
+        validateBonusNumberDuplicate(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
@@ -17,5 +19,11 @@ public class WinningLottoNumbers {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    private void validateBonusNumberDuplicate(int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_DUPLICATE_MESSAGE);
+        }
     }
 }

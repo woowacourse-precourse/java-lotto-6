0d131c17d26622d8920975803a46a1f6456d00f1
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-05 19:27:05 +0900

    feat(profit) : 수익률 계산기 구현

--- src/main/java/lotto/model/ProfitCalculator.java
+++ src/main/java/lotto/model/ProfitCalculator.java
@@ -4,4 +4,25 @@ package lotto.model;
  * 수익률을 계산하는 클래스
  */
 public class ProfitCalculator {
+    private static final double ROUNDING_SCALE = 10.0;
+    private static final int THOUSAND = 1000;
+    private static final int PERCENTAGE = 100;
+
+    public Double calculateProfit(Long properties, PurchaseAmount amount) {
+        Integer purchaseAmount = exchangePurchaseAmount(amount);
+        Double result = profitReport(properties, purchaseAmount);
+        return roundedProfitRate(result);
+    }
+
+    private Double roundedProfitRate(Double result) {
+        return Math.round(result * ROUNDING_SCALE) / ROUNDING_SCALE;
+    }
+
+    private Double profitReport(final Long properties, final int purchaseAmount) {
+        return ((double) properties / purchaseAmount) * PERCENTAGE;
+    }
+
+    private Integer exchangePurchaseAmount(final PurchaseAmount amount) {
+        return amount.exchangeAmount() * THOUSAND;
+    }
 }

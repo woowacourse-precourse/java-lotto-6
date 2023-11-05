098944cb415ae25e0c305d2b6ee74efdd4218144
totocm <typesapk8445@gmail.com>
2023-11-05 09:05:31 +0900

    시즌마다 Lotto의 번호를 담아둘 VO 생성

---
+++ src/main/java/lotto/model/vo/SeasonLottoResultVO.java
@@ -0,0 +1,13 @@
+package lotto.model.vo;
+
+import java.util.List;
+public class SeasonLottoResultVO {
+    private List<Integer> autoLottoNumber;
+    private int autoBonusNumber;
+    public List<Integer> getAutoLottoNumber() {
+        return autoLottoNumber;
+    }
+    public int getAutoBonusNumber() {
+        return autoBonusNumber;
+    }
+}

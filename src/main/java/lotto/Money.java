bf3d9dd13ac796c4a80cadb30c115dc6e2549caf
kkw2758 <dtc3229@gmail.com>
2023-11-05 12:48:46 +0900

    Feat(Money) : 구입 금액이 1,000원으로 나누어 떨어지지 않으면 예외처리한다.

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -5,10 +5,20 @@ public class Money {
     private final int money;
 
     public Money(int money) {
+        validateMoney(money);
         this.money = money;
     }
 
     public int getLottoCount() {
         return money / 1000;
+    }
+
+    private void validateMoney(int money) {
+        if (!checkMoney(money)) {
+            throw new IllegalArgumentException("1,000원으로 나누어 떨어지지 않습니다.");
+        }
+    }
+    private boolean checkMoney(int money) {
+        return money % 1000 == 0;
     }
 }

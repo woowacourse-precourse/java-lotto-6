395e28eba34b3d676befcb56c5d4a08abc3dd269
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 02:37:59 +0900

    test(WinningNumbers): 당첨 번호와 보너스 중복을 검증하는 테스트 코드 구현

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class WinningNumbersTest {
-  
+    @DisplayName("당첨 번호와 보너스 번호의 중복을 검증한다.")
+    @Test
+    void checkWinningAndBonusNumbersDuplication() {
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(4);
+
+        assertThatThrownBy(() -> new WinningNumbers(winningNumber, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

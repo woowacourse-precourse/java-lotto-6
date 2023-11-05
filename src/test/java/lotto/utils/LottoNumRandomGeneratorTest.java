25a44706d74f8d633d1b6a0e55037a81f5840b55
mina <qkralsdk991@naver.com>
2023-11-05 17:08:46 +0900

    Test : 랜덤 로또 번호 생성 및 오름차순 정렬 테스트

--- src/test/java/lotto/utils/LottoNumRandomGeneratorTest.java
+++ src/test/java/lotto/utils/LottoNumRandomGeneratorTest.java
@@ -1,2 +1,20 @@
-package lotto.utils;public class LottoNumRnadomGeneratorTest {
+package lotto.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LottoNumRandomGeneratorTest {
+
+    @Test
+    public void 오름차순정렬테스트(){
+        LottoNumRandomGenerator lottoNumRandomGenerator = new LottoNumRandomGenerator();
+
+        List<Integer> numbers = lottoNumRandomGenerator.setRandomNum();
+        lottoNumRandomGenerator.sortRandomNum(numbers);
+
+        for(int data:numbers){
+            System.out.print(data+" ");
+        }
+    }
 }

83015ee5713cb59801baf45ecf89b2c48068ba0a
gonixxxx <akdntm946@gmail.com>
2023-11-05 20:39:40 +0900

    feat(Rank): add method to return prize money

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,18 +1,28 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
     SIX(6, 2000000000),
     FIVE_BONUS(5, 30000000),
     FIVE(5, 1500000),
     FOUR(4, 50000),
-    THREE(3, 5000);
+    THREE(3, 5000),
+    ECT(0, 0);
 
     private final int matchingNumbers;
-    private final int money;
+    private final int prize;
 
-    Rank(int matchingNumbers, int money) {
+    Rank(int matchingNumbers, int prize) {
         this.matchingNumbers = matchingNumbers;
-        this.money = money;
+        this.prize = prize;
+    }
+
+    public Rank getPrize(int number) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.matchingNumbers == number)
+                .findAny()
+                .orElse(ECT);
     }
 
 }

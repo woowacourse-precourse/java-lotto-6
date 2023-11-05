692b171a73f825253ecfeebd1a03a338515b795a
doha <dohaahi@naver.com>
2023-11-05 21:40:56 +0900

    feat (LottoDto): 당첨 로또와 일치하는 개수, 보너스 숫자 유무 찾는 기능 구현

--- src/main/java/lotto/domain/dto/LottoDto.java
+++ src/main/java/lotto/domain/dto/LottoDto.java
@@ -3,4 +3,11 @@ package lotto.domain.dto;
 import java.util.List;
 
 public record LottoDto(List<Integer> numbers) {
+    public int matchWinningLottoCount(final WinningLottoDto winningLottoDto) {
+        return (int) numbers.stream().filter(i -> numbers.contains(winningLottoDto.numbers())).count();
+    }
+
+    public boolean hasBonusNumber(final BonusNumberDto bonusNumberDto) {
+        return numbers.stream().anyMatch(i -> i.equals(bonusNumberDto.number()));
+    }
 }

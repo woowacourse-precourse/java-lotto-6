a83e0cf9faeb69e23f92c49ddbc76155241b8e5c
김연진 <love_homebody@naver.com>
2023-11-05 15:55:13 +0900

    test(Lotto): Lotto 정상값 입력 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -29,5 +30,38 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("로또 번호 안에 입력받은 숫자가 존재할 경우 true를 반환한다.")
+    void containNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int number = 1;
+
+        boolean isContain = lotto.isContain(number);
+
+        assertThat(isContain).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또 번호 안에 입력받은 숫자가 존재하지 않을 경우 false를 반환한다.")
+    void notContainNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int number = 45;
+
+        boolean isContain = lotto.isContain(number);
+
+        assertThat(isContain).isFalse();
+    }
+
+    @Test
+    @DisplayName("입력받은 인덱스에 존재하는 숫자를 반환한다.")
+    void getNumberByIndex() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int index = 0;
+
+        int number = lotto.getNumberByIndex(index);
+
+        assertThat(number).isEqualTo(1);
     }
 }

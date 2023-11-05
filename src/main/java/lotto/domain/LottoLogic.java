92c78978474bd3c2fd87c4d71fe1f26e74e006f8
alscjf1329 <alscjf1329@gmail.com>
2023-11-05 13:06:13 +0900

    feat(LottoLogic): getResult 작성
    로또 결과를 알려주는 getResult 메소드를 작성 했습니다.

--- src/main/java/lotto/domain/LottoLogic.java
+++ src/main/java/lotto/domain/LottoLogic.java
@@ -1,19 +1,27 @@
 package lotto.domain;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
 import lotto.dto.LottoNumbersInfo;
 import lotto.dto.LottoPurchaseInfo;
+import lotto.message.LottoResult;
 
 public class LottoLogic {
     private LottoPurchaseInfo purchaseInfo;
     private LottoNumbersInfo lottoNumbersInfo;
+    private List<Integer> winningNumbers;
 
 
-    public LottoLogic(LottoPurchaseInfo purchaseInfo, LottoNumbersInfo lottoNumbersInfo) {
+    public LottoLogic(LottoPurchaseInfo purchaseInfo, LottoNumbersInfo lottoNumbersInfo, List<Integer> winningNumbers) {
         this.purchaseInfo = purchaseInfo;
         this.lottoNumbersInfo = lottoNumbersInfo;
+        this.winningNumbers = winningNumbers;
     }
 
+    public HashMap<LottoResult, BigDecimal> getResult() {
+        return lottoNumbersInfo.getResult(winningNumbers);
+    }
 
     public BigDecimal getNumberOfLottoPurchased() {
         return purchaseInfo.numberOfLottoPurchased;

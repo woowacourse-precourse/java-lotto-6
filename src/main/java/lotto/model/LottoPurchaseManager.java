d0a73a7a1e8eea87311efafa1171927fa8d58e28
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-06 04:36:16 +0900

    feat: 로또 티켓을 생성하는 로직 구현

--- src/main/java/lotto/model/LottoPurchaseManager.java
+++ src/main/java/lotto/model/LottoPurchaseManager.java
@@ -2,27 +2,41 @@ package lotto.model;
 
 import lotto.exception.Validation;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoPurchaseManager {
 
     private static final int LOTTO_TICKET_PRICE = 1000;
 
     private final int purchaseAmount;
-    private int lottoTicketCount;
-    private List<LottoResult> winningTicket;
+    private final int lottoTicketCount;
+    private final List<LottoResult> winningTicket;
+    private final List<Lotto> lottoTickets;
 
     public LottoPurchaseManager(int purchaseAmount,
                                 int lottoTicketCount,
-                                List<LottoResult> winningTicket) {
+                                List<LottoResult> winningTicket,
+                                LottoNumberGenerator lottoNumberGenerator) {
         Validation.validatePurchaseAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
         this.lottoTicketCount = lottoTicketCount;
         this.winningTicket = winningTicket;
+        this.lottoTickets = generateLottoTickets(lottoNumberGenerator);
+    }
+
+    public int getLottoTicketCount() {
+        return lottoTicketCount;
     }
 
     public static int getLottoTicketPrice() {
         return LOTTO_TICKET_PRICE;
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return Collections.unmodifiableList(lottoTickets);
     }
 
     public int calculateLottoTicketCount() {
@@ -33,6 +47,16 @@ public class LottoPurchaseManager {
         double profitRate = ((double) calculateTotalPrize() / purchaseAmount) * 100;
 
         return roundToSecondDecimalPlace(profitRate);
+    }
+
+    private List<Lotto> generateLottoTickets(LottoNumberGenerator lottoNumberGenerator) {
+        return IntStream.range(0, lottoTicketCount)
+                .mapToObj(i -> lottoNumberGenerator.generateRandomLottoNumbers())
+                .map(numbers -> numbers.stream()
+                        .map(LottoNumber::new)
+                        .collect(Collectors.toList()))
+                .map(Lotto::new)
+                .collect(Collectors.toList());
     }
 
     private double roundToSecondDecimalPlace(double value) {

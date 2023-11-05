af2789548ace66a8915ee1d803fcd1a54e9eea7b
dgh06175 <dgh06175@gmail.com>
2023-11-05 15:57:26 +0900

    fix: 에러 메세지 오타 수정

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public enum ErrorMessage {
-    LOTTO_NUMBER_FORMAT_ERROR("로또 번호의 개수가 6개가 아닙니다."),
+    LOTTO_NUMBER_FORMAT_ERROR("로또 번호 입력 형식이 아닙니다."),
     LOTTO_NUMBER_COUNT_ERROR("로또 번호의 개수가 6개가 아닙니다."),
     LOTTO_NUMBER_DUPLICATED_ERROR("로또 번호가 중복됐습니다."),
     LOTTO_NUMBER_RANGE_ERROR("로또 번호의 범위가 1~45에 맞지 않습니다."),

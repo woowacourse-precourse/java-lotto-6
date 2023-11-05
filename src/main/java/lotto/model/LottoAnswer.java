390d3277a84a12ee63ad98c63df1df808a9a422b
1000zoo <cjswldn99@gmail.com>
2023-11-06 01:08:19 +0900

    feat(LottoAnswer): 정답 로또번호 클래스

---
+++ src/main/java/lotto/model/LottoAnswer.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+public class LottoAnswer {
+
+    private final Lotto answer;
+    private final BonusNumber bonusNumber;
+
+    public LottoAnswer(Lotto answer, BonusNumber bonusNumber) {
+        this.answer = answer;
+        this.bonusNumber = bonusNumber;
+    }
+}

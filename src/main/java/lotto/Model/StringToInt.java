b2b34c7436c0b536a6fc3aff4e87353db7157741
theplayer1003 <theplayer1003@gmail.com>
2023-11-05 21:47:16 +0900

    feat(StringToList.java): 작성
    - 입력받은 문자열을 구분자로 나눠 리스트에 담는 클래스
    
    feat(StringToInt.java): 업데이트
    
    - 변수명을 통일성있고 알아보기 쉽게 변경
    
    docs(README.md): 업데이트
    
    - 작성한 기능 목록 체크 박스 체크

--- src/main/java/lotto/Model/StringToInt.java
+++ src/main/java/lotto/Model/StringToInt.java
@@ -1,13 +1,13 @@
 package lotto.Model;
 
 public class StringToInt {
-    private int tmp;
+    private int valueToReturn;
 
     StringToInt(String input){
-        tmp = Integer.parseInt(input);
+        valueToReturn = Integer.parseInt(input);
     }
 
     public int getInteger(){
-        return tmp;
+        return valueToReturn;
     }
 }

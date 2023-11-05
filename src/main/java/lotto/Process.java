592ceb65852a7aca9c94177b66b403381faf307e
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-05 23:27:47 +0900

    feat(, Process-generateLottoNumbers()): 하나의 로또에 대해서 6개 번호 생성 함수 구현

---
+++ src/main/java/lotto/Process.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Process {
+    private static final int LOTTO_NUMEBER_RANGE_START = 1;
+    private static final int LOTTO_NUMEBER_RANGE_END = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    //로또 개수 환산하기
+    public int countLottos(int purchaseAmount){
+        int purchaseCount;
+
+        purchaseCount = purchaseAmount / 1000;
+
+        return purchaseCount;
+    }
+
+
+    //로또 개수만큼 로또 번호 생성
+    public List<Integer> generateLottoNumbers(){
+        List<Integer> lottoNumbers;
+
+        //값을 하드 코딩하지 않는 - 2주차 피드백
+        lottoNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_NUMEBER_RANGE_START, LOTTO_NUMEBER_RANGE_END, LOTTO_NUMBER_COUNT);
+
+        return lottoNumbers;
+    }
+
+//    Lotto(lottoNumbers);
+
+
+
+}

eb343a0e77f83526f7ecd26ddcb59d2ed1a5ea18
msung99 <msung6924@naver.com>
2023-11-05 10:45:04 +0900

    feat: 보너스 번호가 숫자 형식이 아닐경우 예외가 발생하는 기능 구현

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -2,12 +2,27 @@ package lotto.domain;
 
 public class Bonus {
     private final int bonus;
+    private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 보너스 번호는 숫자만 입력할 수 있습니다.";
 
     private Bonus(int bonus){
+        validateOutOfRange(bonus);
         this.bonus = bonus;
     }
 
     public static Bonus from(String bonus){
-        return new Bonus(Integer.parseInt(bonus));
+        return new Bonus(convertBonusNumber(bonus));
+    }
+
+    private static int convertBonusNumber(String bonusNumber){
+        try{
+            Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(NUMBER_FORMAT_EXCEPTION_MESSAGE);
+        }
+        return Integer.parseInt(bonusNumber);
+    }
+
+    public void validateOutOfRange(int bonus){
+
     }
 }

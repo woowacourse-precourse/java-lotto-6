44e125deb75635a62e6a48e1dcb853a0db7cb285
isyoo1213 <moethns@gmail.com>
2023-11-05 16:03:05 +0900

    refactor: LottoService 상수 필드 삭제 및 임포트 LottoService - 기존 클래스에 정의한 필드를 삭제하고 LottoConstants 클래스의 상수 참조

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,14 +2,12 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.constants.LottoConstants.*;
+
 public class LottoService {
-    private static final int LOTTO_START_NUMBER = 1;
-    private static final int LOTTO_END_NUMBER = 45;
-    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
 
     public List<Integer> sortNumbersByAsc(List<Integer> integerList) {
         Collections.sort(integerList);
@@ -17,6 +15,6 @@ public class LottoService {
     }
 
     private List<Integer> generateRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, NUMBER_OF_LOTTO_NUMBERS);
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER, NUMBER_OF_LOTTO_NUMBERS);
     }
 }

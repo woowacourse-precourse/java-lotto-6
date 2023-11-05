1c85c3a48d3c85fb2722adc1fd013803e561a199
shining013 <nevertheless113@naver.com>
2023-11-05 20:28:26 +0900

    feat: InputView winningNumberInput 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,13 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String purchaseInput() {
-        System.out.println("구입금액을 입력해 주세요.");
-        return Console.readLine();
-    }
+        public static String purchaseInput() {
+                System.out.println("구입금액을 입력해 주세요.");
+                return Console.readLine();
+        }
+
+        public static String winningNumerInput() {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                return Console.readLine();
+        }
 }

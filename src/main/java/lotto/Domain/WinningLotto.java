9771e696a54dc99e6e27b556d03028f4610c2c53
chansoo <chansoo38@gmail.com>
2023-11-05 17:15:46 +0900

    <Feat> WinningLotto - 당첨 번호와 보너스 번호를 가지는 당첨로또 객체를 생성한다.

---
+++ src/main/java/lotto/Domain/WinningLotto.java
@@ -0,0 +1,18 @@
+package lotto.Domain;
+
+import java.util.List;
+import lotto.Domain.Validator.WinningLottoValidator;
+
+public class WinningLotto {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> winningNumbers, int bonusNumber) {
+    }
+}

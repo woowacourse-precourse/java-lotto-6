cc40a6d57112cc2465bacc9af57c55c298e801ed
junghunim07 <dlawndgns9@naver.com>
2023-11-05 20:49:27 +0900

    feat(Calculator): 지불액, 수익, 수익률 계산 기능 추가

---
+++ src/main/java/lotto/domain/Calculator.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Calculator {
+    public float rateOFProfit(List<Integer> result, int lottoCount) {
+        int pay = calculatePayAmount(lottoCount);
+        int income = calculateProfit(result);
+        return (float) income / pay * 100;
+    }
+
+    public int calculatePayAmount(int lottoCount) {
+        return lottoCount * Lotto.PRICE;
+    }
+
+    public int calculateProfit(List<Integer> result) {
+        int income = 0;
+        int index = 0;
+        for (Lotto.Prize prize : Lotto.Prize.values()) {
+            income += result.get(index++) * prize.getPrizeMoney();
+        }
+        return income;
+    }
+}

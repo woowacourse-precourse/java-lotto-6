bc3778987cf152dc14917e68eb3c200a46001d22
master <assert2914@gmail.com>
2023-11-05 14:01:36 +0900

    feat: 보너스 번호 범위검증로직 추가

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -4,18 +4,14 @@ import java.util.List;
 
 public class LottoMatcher {
     public static final String ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER = "보너스 번호가 당첨번호와 중복됩니다.";
+    public static final String ERROR_MESSAGE_BONUS_NUMBER_OUT_OF_RANGE = "보너스 번호가 범위를 벗어났습니다.";
 
     private final Lotto winning;
     private final int bonusNumber;
 
     public LottoMatcher(Lotto winning, int bonusNumber) {
+        validateBonusNumber(winning, bonusNumber);
         this.winning = winning;
-
-        List<Integer> winningNumbers = winning.getNumbers();
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER);
-        }
-
         this.bonusNumber = bonusNumber;
     }
 
@@ -33,5 +29,17 @@ public class LottoMatcher {
         }
 
         return new MatchResult(correctNumber, correctBonusNumber);
+    }
+
+    private void validateBonusNumber(Lotto winning, int bonusNumber) {
+        var rule = LottoNumberRule.BASIC;
+        if (bonusNumber < rule.getMinLottoNumber() || bonusNumber > rule.getMaxLottoNumber()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BONUS_NUMBER_OUT_OF_RANGE);
+        }
+
+        List<Integer> winningNumbers = winning.getNumbers();
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER);
+        }
     }
 }
--- src/test/java/lotto/LottoMatcherTest.java
+++ src/test/java/lotto/LottoMatcherTest.java
@@ -6,12 +6,14 @@ import lotto.domain.LottoMatcher;
 import lotto.domain.MatchResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoMatcherTest {
-    @DisplayName("당첨번호와 보너스번호는 중복될 수 없다.")
+    @DisplayName("당첨번호와 보너스번호가 중복시 예외를 발생한다.")
     @Test
     void createWithDuplicatedBonusNumber() {
         Lotto winning = new Lotto(List.of(1, 2, 3, 4, 5, 6));
@@ -19,6 +21,17 @@ public class LottoMatcherTest {
         assertThatThrownBy(() -> new LottoMatcher(winning, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(LottoMatcher.ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER);
+    }
+
+    @DisplayName("보너스번호가 범위를 벗어날 시 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    void createWithOutRangedBonusNumber(int bonusNumber) {
+        Lotto winning = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> new LottoMatcher(winning, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LottoMatcher.ERROR_MESSAGE_BONUS_NUMBER_OUT_OF_RANGE);
     }
 
     @DisplayName("제대로된 매칭결과를 반환한다.")

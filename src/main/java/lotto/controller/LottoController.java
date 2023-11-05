2cfd3ef3a015981521f858dc347b92bb436f0014
koomchang <leahpar0401@gmail.com>
2023-11-05 23:42:20 +0900

    feat: LottoController 클래스 구현 및 run 메소드에서 로또 번호 출력 확인

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,23 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
+    LottoGenerator lottoGenerator = new LottoGenerator();
+
+    public void run() {
+        outputView.askForMoney();
+        int money = inputView.inputMoney();
+        int lottoCount = money / 1000;
+        outputView.printLottoCount(lottoCount);
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto generatedLotto = lottoGenerator.generate();
+            outputView.printLottoNumbers(generatedLotto.getNumbers());
+        }
+    }
 }

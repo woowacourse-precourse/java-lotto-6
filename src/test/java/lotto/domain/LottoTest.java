d0b9de21187406d82f4e8aa5d3d819b7a52efd1d
SeongUk52 <rkfka7849@gmail.com>
2023-11-05 19:14:14 +0900

    test(Lotto): 범위를 벗어난 번호에 대한 예외처리 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -24,5 +24,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45 범위를 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatThrownBy(() -> Lotto.of(List.of(55, 68, 111, -30, 0, 40)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

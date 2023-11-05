e1d491c868bf66866e0e320868fe2c540d88be72
hgh1472 <hgh1472@naver.com>
2023-11-06 01:15:29 +0900

    feat : 구입금액이 1000보다 낮을 때 에러메세지 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -2,6 +2,7 @@ package lotto.constant;
 
 public enum ErrorMessage {
     MONEY_ERROR_MESSAGE("[ERROR] 구입금액은 1000원으로 나누어 떨어져야 합니다."),
+    NOT_ENOUGH_MONEY_ERROR_MESSAGE("[ERROR] 로또를 구매할 수 없습니다."),
     RANGE_ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     NOT_INT_ERROR_MESSAGE("[ERROR] 구입금액은 숫자여야 합니다."),
     NOT_SIX_NUMBER_ERROR_MESSAGE("[ERROR] 당첨 번호는 6자리 숫자입니다."),

5ec27283ef87adf90454524b340f25c8c3f9a0d2
yshong1998 <yshong1998@gmail.com>
2023-11-06 03:10:48 +0900

    Fix(ResponseMessage): 출력 양식에 맞도록 줄바꿈 수정 및 구분선 추가

--- src/main/java/lotto/message/ResponseMessage.java
+++ src/main/java/lotto/message/ResponseMessage.java
@@ -1,10 +1,10 @@
 package lotto.message;
 
 public class ResponseMessage {
-    public static final String BOUGHT_MESSAGE = "%s개를 구매했습니다.";
-    public static final String STATISTICS_MESSAGE = "당첨 통계";
-    public static final String FORMATTED_RESULT_MESSAGE = "%s개 일치%s (%s원) - ";
-    public static final String RATE_OF_BENEFIT_MESSAGE = "\n총 수익률은 %.1f%%입니다.\n";
-
+    public static final String BOUGHT_MESSAGE = "\n%s개를 구매했습니다.";
+    public static final String STATISTICS_MESSAGE = "\n당첨 통계\n";
+    public static final String DIVISION_LINE = "---";
+    public static final String FORMATTED_RESULT_MESSAGE = "%s(%s원) - %s개\n";
+    public static final String RATE_OF_BENEFIT_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
 
 }

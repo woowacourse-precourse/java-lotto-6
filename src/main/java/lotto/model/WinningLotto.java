a50ec8bc47e489c2f5dd30b78032862383a18ea6
kim <hwiyul_g@daum.net>
2023-11-05 22:36:35 +0900

    refactor(WinningLotto) : 레코드로 변경 및 매칭 로직 LottoMatcher로 분리

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,18 +1,11 @@
 package lotto.model;
 
-import java.util.Arrays;
-import java.util.List;
 import lotto.utils.ExceptionMessage;
 
-public class WinningLotto {
+public record WinningLotto(Lotto lotto, Bonus bonus) {
 
-    private final Lotto lotto;
-    private final Bonus bonus;
-
-    public WinningLotto(final Lotto lotto, final Bonus bonus) {
+    public WinningLotto {
         validate(lotto, bonus);
-        this.lotto = lotto;
-        this.bonus = bonus;
     }
 
     private void validate(Lotto lotto, Bonus bonus) {
@@ -26,38 +19,5 @@ public class WinningLotto {
         }
     }
 
-    public LottoResult matchLottos(final List<Lotto> userLottos) {
-        LottoResult lottoResult = new LottoResult();
-        for (Lotto userLotto : userLottos) {
-            MatchCount matchCount = matchLotto(userLotto);
-            lottoResult.addResult(matchCount);
-        }
-        return lottoResult;
-    }
-
-    private MatchCount matchLotto(Lotto userLotto) {
-        int matchedCount = getMatchedCount(userLotto);
-        if (hasBonusMatch(userLotto, matchedCount)) {
-            return MatchCount.FIVE_AND_BONUS_MATCH;
-        }
-        return findMatchCountByMatchedNumber(matchedCount);
-    }
-
-    private int getMatchedCount(Lotto userLotto) {
-        return (int) lotto.getNumbers().stream()
-            .filter(userLotto::contains)
-            .count();
-    }
-
-    private boolean hasBonusMatch(Lotto userLotto, int matchedCount) {
-        return matchedCount == 5 && userLotto.contains(bonus.bonusNumber());
-    }
-
-    private MatchCount findMatchCountByMatchedNumber(int matchedCount) {
-        return Arrays.stream(MatchCount.values())
-            .filter(matchCount -> matchCount.getMatchCount() == matchedCount)
-            .findFirst()
-            .orElse(MatchCount.DEFAULT_NONE);
-    }
 
 }

8a6e8e6d08eacafc809eb06a09c85d589d24ba1f
yubin <dbqls2821@gmail.com>
2023-11-05 22:08:23 +0900

    feat: 보너스 번호에 대한 예외사항 enum 상수 추가

--- src/main/java/lotto/domain/DomainException.java
+++ src/main/java/lotto/domain/DomainException.java
@@ -7,6 +7,9 @@ enum DomainException {
     LOTTO_SIZE(ERROR.message + "로또는 6개여야 합니다."),
     LOTTO_DUPLICATION(ERROR.message + "로또번호에 중복되는 숫자가 있습니다."),
 
+    BONUS_RANGE(ERROR.message + "보너스 번호는 1에서 45 사이 숫자여야 합니다."),
+    BONUS_DUPLICATION(ERROR.message + "보너스 번호에 로또번호와 중복되는 숫자가 존재합니다."),
+
     PRICE_LESS_THAN_OR_EQUAL_TO_ZERO(ERROR.message + "구입 금액이 0 이하입니다."),
     PRICE_NOT_DIVISIBLE_BY_UNIT(ERROR.message + "구입 금액은 " + Constraint.LOTTO_PRICE.getValue() + "의 배수여야 합니다."),
     ;

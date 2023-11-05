448281c8b368571a7b510d218f4e33e06453e6e0
doha <dohaahi@naver.com>
2023-11-05 22:22:18 +0900

    style (BonusNumber): 공백 제거

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,14 +1,9 @@
 package lotto.domain;
 
-import lotto.validator.BonusNumberValidator;
-
 public class BonusNumber {
-
     private final int number;
 
     public BonusNumber(int number) {
         this.number = number;
     }
-
-
 }

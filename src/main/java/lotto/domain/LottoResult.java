702b97666508b41f0b0fd7fe30dacb49836767dd
khabh1 <kshababe1@gmail.com>
2023-11-05 21:45:57 +0900

    refactor(LottoResult): 로또 리스트를 인자로 받아 인스턴스 생성하도록 수정

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.List;
 import java.util.StringJoiner;
 import lotto.config.Message;
 
@@ -7,7 +8,14 @@ public class LottoResult {
 
     private final StringJoiner lottoResultMaker = new StringJoiner(Message.NEW_LINE);
 
-    public void addLotto(Lotto lotto) {
+    public static LottoResult from(List<Lotto> lottos) {
+        LottoResult lottoResult = new LottoResult();
+        lottos.forEach(lottoResult::addLotto);
+
+        return lottoResult;
+    }
+
+    private void addLotto(Lotto lotto) {
         lotto.addedToResult(lottoResultMaker);
     }
 
--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -11,14 +11,13 @@ class LottoResultTest {
     @Test
     void 올바르게_로또_결과_문자열_생성() {
         // Given
-        LottoResult lottoResult = new LottoResult();
         List<Integer> firstLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> secondLottoNumbers = List.of(7, 8, 9, 10, 11, 12);
+        List<Lotto> lottos = List.of(Lotto.from(firstLottoNumbers), Lotto.from(secondLottoNumbers));
         String expectedResult = firstLottoNumbers + Message.NEW_LINE + secondLottoNumbers;
 
         // When
-        lottoResult.addLotto(Lotto.from(firstLottoNumbers));
-        lottoResult.addLotto(Lotto.from(secondLottoNumbers));
+        LottoResult lottoResult = LottoResult.from(lottos);
         String actualResult = lottoResult.getLottoResult();
 
         // Then

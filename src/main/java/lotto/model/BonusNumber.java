18d30f093daa85c24c22174098bf3c8065dd5816
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 00:35:33 +0900

    feat: 보너스 숫자를 검증하는 기능

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,4 +1,20 @@
 package lotto.model;
 
 public class BonusNumber {
+    private int bonusNumber;
+
+    public BonusNumber(int inputBonusNumber) {
+        validateBonusNumber(inputBonusNumber);
+        this.bonusNumber = inputBonusNumber;
+    }
+
+    public void validateBonusNumber(int inputBonusNumber) {
+        validateBonusNumberRange(inputBonusNumber);
+    }
+
+    private void validateBonusNumberRange(int inputBonusNumber) {
+        if (inputBonusNumber < 1 || inputBonusNumber > 45) {
+            throw new IllegalArgumentException("보너스 숫자는 1과 45 사이의 숫자를 입력해야 합니다.");
+        }
+    }
 }

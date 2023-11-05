ef3ec4cfe256455fa03b785fe8afd16df0c0f23d
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 15:52:30 +0900

    feat(purchasing): generate unique random numbers within a predefined range

--- src/main/java/lotto/purchasing/Generator.java
+++ src/main/java/lotto/purchasing/Generator.java
@@ -1,4 +1,12 @@
 package lotto.purchasing;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generator {
+    private List<Integer> generate(int minNumber, int maxNumber, int size) {
+        return new ArrayList<>(pickUniqueNumbersInRange(minNumber, maxNumber, size));
+    }
 }

1af229096ca9b75abbb6974c03f4ca47101a7a71
gonixxxx <akdntm946@gmail.com>
2023-11-06 01:08:34 +0900

    style(InputView): delete line

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,6 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
     private static final String PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";

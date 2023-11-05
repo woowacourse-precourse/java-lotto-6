769810991071479df4788dd6619ad98fa37f8e9b
yubin <dbqls2821@gmail.com>
2023-11-05 15:36:56 +0900

    feat: 예외 메세지 enum 상수화

---
+++ src/main/java/lotto/ExceptionMessage.java
@@ -0,0 +1,19 @@
+package lotto;
+
+enum ExceptionMessage {
+    ERROR("[ERROR] "),
+    LOTTO_RANGE(ERROR + "로또 번호는 1에서 45 사이 숫자여야 합니다."),
+    LOTTO_SIZE(ERROR + "로또는 6개여야 합니다."),
+    LOTTO_DUPLICATION(ERROR + "로또번호에 중복되는 숫자가 있습니다."),
+    ;
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

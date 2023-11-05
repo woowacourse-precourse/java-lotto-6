561746f6fd3acd5ab7e2d6b733f839714f40c8f9
JaeY0ungJung <hanol5216@gmail.com>
2023-11-05 18:24:26 +0900

    feat: make a function that issues lotto with six numbers and print to concole

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,18 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.Converter;
 import lotto.constant.Number;
 import lotto.constant.ExceptionMessage;
+import lotto.domain.Lotto;
 import lotto.view.InputHandler;
 import lotto.view.OutputHandler;
 
 public class LottoController {
     int ticketCount;
+
+    List<Lotto> lottos = new ArrayList<>();
 
     public void start() throws IllegalArgumentException{
         OutputHandler.requirePaymentPrice();
@@ -15,6 +20,11 @@ public class LottoController {
         OutputHandler.printEmptyLine();
         ticketCount = getLotteryCount(paymentPrice);
         OutputHandler.sayTicketCount(ticketCount);
+        for (int i = 0; i < ticketCount; i++) {
+            Lotto lotto = new Lotto();
+            lottos.add(lotto);
+            OutputHandler.printLottoNumbers(lotto.getNumbers());
+        }
     }
 
     int getLotteryCount(String inputPay) throws IllegalArgumentException{
--- src/main/java/lotto/view/OutputHandler.java
+++ src/main/java/lotto/view/OutputHandler.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.List;
 import lotto.constant.SystemMessage;
 
 public class OutputHandler {
@@ -7,11 +8,17 @@ public class OutputHandler {
     public static void requirePaymentPrice() {
         System.out.println(SystemMessage.REQUIRE_PAYMENT_PRICE);
     }
+
     public static void sayTicketCount(int ticketCount) {
         System.out.println(ticketCount + SystemMessage.SAY_TICKET_COUNT_FORMAT);
     }
+
     public static void printEmptyLine() {
         System.out.println();
+    }
+
+    public static void printLottoNumbers(List<Integer> lottoNumbers) {
+        System.out.println(lottoNumbers);
     }
 
 }

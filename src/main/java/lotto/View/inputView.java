58e22518aa0ec8e2bc505f777dfc17abf6195a21
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-11-05 21:49:38 +0900

    feat(inputView) : getInputWinningNumber 메서드 작성

--- src/main/java/lotto/View/inputView.java
+++ src/main/java/lotto/View/inputView.java
@@ -1,12 +1,15 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.Controller.Validator;
 
 public class inputView {
     static final String GET_INPUT_PURCHASE_AMOUNT_MESSAGE = "당첨 번호를 입력해 주세요.";
     static final String GET_INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    static final String GET_INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static int getInputPurchaseAmount() {
         System.out.println(GET_INPUT_PURCHASE_AMOUNT_MESSAGE);
@@ -22,6 +25,23 @@ public class inputView {
         }
     }
 
+    public static List<Integer> getInputWinningNumber(){
+        System.out.println(GET_INPUT_WINNING_NUMBERS_MESSAGE);
+        String input = Console.readLine();
+        while(true){
+            try {
+                List<Integer> list = Arrays.stream(input.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                return list;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println(GET_INPUT_WINNING_NUMBERS_MESSAGE);
+                input = Console.readLine();
+            }
+        }
+    }
+
     public static int getInputBonusNumber() {
         System.out.println(GET_INPUT_BONUS_NUMBER_MESSAGE);
         String input = Console.readLine();
@@ -31,7 +51,7 @@ public class inputView {
                 return number;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
-                System.out.println(GET_INPUT_PURCHASE_AMOUNT_MESSAGE);
+                System.out.println(GET_INPUT_BONUS_NUMBER_MESSAGE);
                 input = Console.readLine();
             }
         }

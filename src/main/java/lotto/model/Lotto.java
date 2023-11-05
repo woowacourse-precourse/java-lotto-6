e861e6fa8e05a1b6f5ba81dad911042afb8cef6a
insu <is23193160@gmail.com>
2023-11-05 15:38:37 +0900

    feat: 로또 번호 중복 검사하는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Lotto {
     private final static int LOTTO_NUMBER_COUNT = 6;
     private static final String LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "[ERROR] 로또 숫자 6개를 입력해주세요.";
+    private static final String NON_DUPLICATE_NUMBER_ERROR_MESSAGE = "[ERROR] 중복되지 않은 숫자를 입력해주세요.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,6 +17,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         checkNumbersSize(numbers);
+        checkForDuplicates(numbers);
+    }
+
+    private void checkForDuplicates(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(NON_DUPLICATE_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     private void checkNumbersSize(List<Integer> numbers) {

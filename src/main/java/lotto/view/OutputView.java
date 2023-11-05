98648256a5e2e7a928234c0481167a7b9ce0af2d
leesh <qwqw8019@naver.com>
2023-11-06 01:55:21 +0900

    feat: 발행한 로또 수량 및 번호 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String BUY_LOTTO_MESSAGE = "%s개를 구매했습니다.";
+    private static final String BUY_LOTTO_MESSAGE = "%d개를 구매했습니다.";
     private static final String INPUT_WINNING_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_LOTTO_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS = "당첨 통계";
@@ -18,6 +20,14 @@ public class OutputView {
 
     public static void notifyInputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
+    }
+
+    public static void notifyLottoPurchase(List<List<Integer>> lottoList) {
+        int amount = lottoList.size();
+
+        System.out.println(BUY_LOTTO_MESSAGE.formatted(amount));
+
+        lottoList.forEach(System.out::println);
     }
 
     public static void notifyInputWinningLottoNumber() {

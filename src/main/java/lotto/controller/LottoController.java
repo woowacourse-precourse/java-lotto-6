dcdf3c8e0e8e700a692d5d26429ffbd17924637c
acs0209 <annjae4676@naver.com>
2023-11-06 02:17:00 +0900

    feat: 구매 개수 저장 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,19 +2,24 @@ package lotto.controller;
 
 import static lotto.view.constant.InputMessage.GET_PURCHASE_AMOUNT_MESSAGE;
 
+import lotto.domain.LottoPurchase;
+import lotto.service.LottoPurchaseService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 
     private final InputView inputView;
+    private final LottoPurchaseService lottoPurchaseService;
 
     public LottoController() {
         this.inputView = new InputView();
+        this.lottoPurchaseService = new LottoPurchaseService();
     }
 
     public void play() {
         String purchaseAmount = getPurchaseAmount();
+        LottoPurchase lottoPurchase = lottoPurchaseService.createLottoPurchase(purchaseAmount);
     }
 
     public String getPurchaseAmount() {

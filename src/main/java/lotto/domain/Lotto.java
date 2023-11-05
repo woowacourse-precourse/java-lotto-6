191c88377870a264ba2a7aa6cf563332038d18ce
tam31 <wxogud3377@naver.com>
2023-11-05 16:21:14 +0900

    Feat(Lotto) : 6개의 랜덤번호의 중복 검사 기능 구현 Docs : 로또 객체의 상태 행동 완료체크

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,11 +9,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateValidation(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void duplicateValidation(List<Integer> numbers) {
+        Set<Integer> removeDuplicationNumber = new HashSet<>(numbers);
+        if(removeDuplicationNumber.size() != numbers.size()){
             throw new IllegalArgumentException();
         }
     }

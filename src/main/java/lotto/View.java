df3e55240df5163be42677624564973b58011970
shin5774 <babo5774@naver.com>
2023-11-05 09:30:46 +0900

    Feat: 보너스 번호 입력기능 구현
    - 해당 로직과 앞에서 구현한, 구매금액을 입력받는 로직이 동일하기에 이를 재활용함.
    - 두 로직의 공통점인 '숫자를 입력받는다'라는 것을 나타내기 위해 메서드명을 변경함.

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -6,35 +6,35 @@ import java.util.List;
 import lotto.util.Validator;
 
 public class View {
-    public long inputLottoBuyCash(){
-        String input=readLine();
+    public long inputNumericValue() {
+        String input = readLine();
 
         Validator.INSTANCE.numberValidate(input);
 
         return Long.parseLong(input);
     }
 
-    public void printUserLottos(List<Lotto> userLottos){
-        System.out.println(userLottos.size()+"개를 구매했습니다.");
+    public void printUserLottos(List<Lotto> userLottos) {
+        System.out.println(userLottos.size() + "개를 구매했습니다.");
         userLottos.forEach(this::printLotto);
     }
 
-    private void printLotto(Lotto lotto){
+    private void printLotto(Lotto lotto) {
         System.out.println(lotto.getNumbersMessage());
     }
 
-    public List<Integer> inputWinningNumbers(){
-        List<String> inputNumbers=split(readLine());
+    public List<Integer> inputWinningNumbers() {
+        List<String> inputNumbers = split(readLine());
         inputNumbers.forEach(Validator.INSTANCE::numberValidate);
 
         return toIntegerList(inputNumbers);
     }
 
-    private List<String> split(String input){
+    private List<String> split(String input) {
         return List.of(input.split(","));
     }
 
-    private List<Integer> toIntegerList(List<String> inputNumbers){
+    private List<Integer> toIntegerList(List<String> inputNumbers) {
         return inputNumbers.stream().map(Integer::parseInt).toList();
     }
 

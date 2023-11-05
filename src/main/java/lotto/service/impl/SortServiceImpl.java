8707b32f39bf35c3b57dea10987927d09085d8ce
0Chord <seaweed.0chord@gmail.com>
2023-11-05 16:40:13 +0900

    refactor : sorting method
    내부 객체 변경으로 인한 새로운 정렬 방식 추가

--- src/main/java/lotto/service/impl/SortServiceImpl.java
+++ src/main/java/lotto/service/impl/SortServiceImpl.java
@@ -1,13 +1,11 @@
 package lotto.service.impl;
 
-import java.util.Collections;
 import java.util.List;
 import lotto.service.SortService;
 
 public class SortServiceImpl implements SortService {
     @Override
     public List<Integer> sortLottoAsc(List<Integer> numbers) {
-        Collections.sort(numbers);
-        return numbers;
+        return numbers.stream().sorted().toList();
     }
 }

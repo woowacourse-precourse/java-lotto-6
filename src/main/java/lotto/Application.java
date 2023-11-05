c82701a2337e7812c1deb71da6dc433849af497f
74059 <shyeone@naver.com>
2023-11-05 11:15:11 +0900

    style: 변수 이름 스타일 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,7 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lotto_controller = new LottoController();
-        lotto_controller.run();
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }

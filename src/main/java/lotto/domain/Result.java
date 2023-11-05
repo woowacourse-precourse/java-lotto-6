d8803902a45a4c7303b9cafc9306a2040697d4c5
김효준 <khyojun1104@gmail.com>
2023-11-06 02:27:39 +0900

    feat(lotto): 보너스 번호 관련 체크에 관한 변수 추가!

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -2,22 +2,23 @@ package lotto.domain;
 
 public enum Result {
 
-    FIFTH(3, 5000, 0),
-    FORTH(4, 50000, 0),
-    THIRD(5,1500000, 0),
-    SECOND(5, 30000000, 0),
-    FIRST(6, 2000000000, 0);
+    FIFTH(3, 5000, 0, false),
+    FORTH(4, 50000, 0, false),
+    THIRD(5,1500000, 0, false),
+    SECOND(5, 30000000, 0, true),
+    FIRST(6, 2000000000, 0, false);
 
     private int sameCount;
-
     private int money;
     private int resultCount;
 
+    private boolean bonus;
 
-    Result(int sameCount, int money, int resultCount) {
+    Result(int sameCount, int money, int resultCount, boolean isBonus) {
         this.sameCount = sameCount;
         this.money = money;
         this.resultCount = 0;
+        this.bonus=isBonus;
     }
 
     public int getSameCount() {
@@ -30,6 +31,10 @@ public enum Result {
 
     public int getResultCount() {
         return resultCount;
+    }
+
+    public boolean isBonus() {
+        return bonus;
     }
 
     public void addCount(){

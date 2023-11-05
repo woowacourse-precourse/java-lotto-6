1ffde3994faa3fa66fdebd6c7f804a1361cd69de
reddevilmidzy <midzy0228@gmail.com>
2023-11-05 21:40:30 +0900

    test: 정상 로직 테스트 추가

--- src/test/java/lotto/model/PurchaseAmountTest.java
+++ src/test/java/lotto/model/PurchaseAmountTest.java
@@ -1,9 +1,11 @@
 package lotto.model;
 
+import lotto.constant.Rule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseAmountTest {
@@ -22,5 +24,14 @@ class PurchaseAmountTest {
     void createInvalidUnit(Integer value) {
         assertThatThrownBy(() -> PurchaseAmount.of(value).exchangeLottoTicket())
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입금액을 로또 티켓으로 반환한다.")
+    @ParameterizedTest(name = "{displayName} value:{0}")
+    @ValueSource(ints = {1000, 3000, 15000, 100000})
+    void createValidAmount(Integer value) {
+        PurchaseAmount amount = PurchaseAmount.of(value);
+        assertThat(amount.exchangeLottoTicket())
+                .isEqualTo(amount.getAmount() / Rule.MONEY_UNIT);
     }
 }

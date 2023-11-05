9a978eef1096f3ee4d9e5189ec3ac58a8bd65c01
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-05 14:21:45 +0900

    feat (Lotteries) : 구입 금액에 따라 로또를 생성하는 기능 구현

---
+++ src/main/java/lotto/domain/Lotteries.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lotteries {
+
+    private static final int LOTTO_PRICE = 1_000;
+
+    private final List<Lotto> lottos;
+
+    private Lotteries(List<Lotto> lottos) {
+        this.lottos = Objects.requireNonNull(lottos);
+    }
+
+    public static Lotteries from(int purchaseAmount) {
+        validate(purchaseAmount);
+        int countOfLotto = purchaseAmount / LOTTO_PRICE;
+        return new Lotteries(RandomLottoGenerator.generate(countOfLotto));
+    }
+
+    private static void validate(int purchaseAmount) {
+        validatePositive(purchaseAmount);
+        validatePrice(purchaseAmount);
+    }
+
+    private static void validatePositive(int purchaseAmount) {
+        if (isNegativeOrZero(purchaseAmount)) {
+            throw new IllegalArgumentException("구입 금액 양수 이어야 합니다");
+        }
+    }
+
+    private static boolean isNegativeOrZero(int purchasePrice) {
+        return purchasePrice <= 0;
+    }
+
+    private static void validatePrice(int purchaseAmount) {
+        if (isNotDividedByLottoPrice(purchaseAmount)) {
+            String exceptionMessage = "구입 금액은 한 장의 가격(%d)으로 나누어 떨어져야 합니다".formatted(LOTTO_PRICE);
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+    }
+
+    private static boolean isNotDividedByLottoPrice(int purchaseAmount) {
+        return purchaseAmount % LOTTO_PRICE != 0;
+    }
+}

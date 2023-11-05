2648ea477108ff8b320c62aa22a3ecb21e696a8c
dragon0622 <iamdragon.dev@gmail.com>
2023-11-05 19:47:45 +0900

    feat(Lotto) : Input 클래스 내의 메소드 skeleton code 작성

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class Input {
+    public static void money(){
+
+    }
+
+    public static void winningNumbers(){
+
+    }
+
+    public static void bonusNumber(){
+
+    }
+}

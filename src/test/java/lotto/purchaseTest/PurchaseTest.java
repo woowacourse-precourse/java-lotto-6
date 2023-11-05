81670c2e301ca330f87394ff99ecc143dbe76d6c
Mr.Kim <joajy98@gmail.com>
2023-11-06 01:51:51 +0900

    chore: 테스트 세분화 및 가독성을 위한 테스트명 변경

--- src/test/java/lotto/purchaseTest/PurchaseTest.java
+++ src/test/java/lotto/purchaseTest/PurchaseTest.java
@@ -1,14 +1,15 @@
 package lotto.purchaseTest;
 
-import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.*;
 
+import static lotto.service.LottoService.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PurchaseFunctionTest {
+public class PurchaseTest {
 
     public void init(String input){
         OutputStream out = new ByteArrayOutputStream();
@@ -17,15 +18,16 @@ public class PurchaseFunctionTest {
         System.setIn(in);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"8000"})
     @DisplayName("구입_금액에_해당하는_만큼_로또를_발행하는_테스트")
-    public void purchaseAndPublishTest(){
+    public void purchaseAndPublishTest(String money){
         //given
-        String money = "8000";
         init(money);
-        int wantBuyAmount = 8;
-        int purchasedAmount = LottoService.enterPurchaseAmount();
+
         //when
+        int wantBuyAmount = Integer.parseInt(money) / 1000;
+        int purchasedAmount = enterPurchaseAmount();
 
         //then
         assertThat(purchasedAmount)

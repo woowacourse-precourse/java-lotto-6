ef791d0657de0d4cf2d5cc899d273b26c733d0d2
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-05 23:36:11 +0900

    Fix: 접근제어자 수정

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -1,8 +1,8 @@
 package lotto.util;
 
 public class Constant {
-    private static final String GET_PURCHASE_AMOUNT_TXT = "구입금액을 입력해 주세요.";
-    private static final String GET_WINNING_NUMBER_TXT = "당첨 번호를 입력해 주세요.";
+    public static final String GET_PURCHASE_AMOUNT_TXT = "구입금액을 입력해 주세요.";
+    public static final String GET_WINNING_NUMBER_TXT = "당첨 번호를 입력해 주세요.";
 
     public static final String LOTTO_AMOUNT = "{0}개를 구매했습니다.";
     public static final String WINNING_STATISTICS = "{0}개 일치 ({1}원) - {2}개";

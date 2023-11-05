c93d3dc5a190813361776e96fb3fdd0c10ca3720
minisyu <iswater.kim@gmail.com>
2023-11-06 01:25:01 +0900

    refactor: LottoDto 멤버 변수 박싱 제거

--- src/main/java/lotto/dto/output/LottoDto.java
+++ src/main/java/lotto/dto/output/LottoDto.java
@@ -2,14 +2,10 @@ package lotto.dto.output;
 
 import java.util.List;
 import lotto.domain.lotto.Lotto;
-import lotto.dto.LottoNumberDto;
 
-public record LottoDto(List<LottoNumberDto> lottoNumbers) {
+public record LottoDto(List<Integer> lottoNumbers) {
     public static LottoDto toLottoDto(Lotto lotto) {
-        List<Integer> numbers = lotto.getNumbers();
-        List<LottoNumberDto> lottoNumbers = numbers.stream()
-                .map(LottoNumberDto::new)
-                .toList();
+        List<Integer> lottoNumbers = lotto.getLottoNumbers();
         return new LottoDto(lottoNumbers);
     }
 }

b21f85aaab66f4a6b70e97daaa2447ff072f844c
Haeun Jeong <gkdms4248@gmail.com>
2023-11-05 17:48:27 +0900

    feat: 등수 별 금액 enum 추가

---
+++ src/main/java/lotto/LottoCriteria.java
@@ -0,0 +1,31 @@
+package lotto;
+
+public enum LottoCriteria {
+    FIRST_PLACE("1", 6, 2000000000),
+    SECOND_PLACE("2", 5, 30000000),
+    THIRD_PLACE("3", 5, 1500000),
+    FOURTH_PLACE("4", 4, 50000),
+    FIFTH_PLACE("5", 3, 5000);
+
+    private final String place;
+    private final int count;
+    private final long amount;
+
+    LottoCriteria(final String place, final int count, final long amount) {
+        this.place = place;
+        this.count = count;
+        this.amount = amount;
+    }
+
+    public final String getPlace() {
+        return place;
+    }
+
+    public final int getCount() {
+        return count;
+    }
+
+    public final long getAmount() {
+        return amount;
+    }
+}

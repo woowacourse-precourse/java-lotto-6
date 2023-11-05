406938035870ef3043116b341b7f45d667d53635
dj <fjzl12@naver.com>
2023-11-05 18:50:33 +0900

    feat(Message): 기본 입력 메시지 구현

--- src/main/java/lotto/domain/Message.java
+++ src/main/java/lotto/domain/Message.java
@@ -1,5 +1,22 @@
 package lotto.domain;
 
 public enum Message {
+    ERROR("[ERROR] "),
+    INPUT_COST("구입금액을 입력해 주세요."),
+    INPUT_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
+    RESULT("당첨 통계\n---");
+    private String message;
 
+    Message(String message) {
+        this.message = message;
+    }
+
+    public final String getMessage() {
+        return message;
+    }
+
+    public final String getErrorMessage() {
+        return ERROR + message;
+    }
 }

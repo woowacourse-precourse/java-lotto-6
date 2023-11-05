90c4224d06035033dd9843a5773b6fe92fbfdca2
beomsic <qjatjr29@khu.ac.kr>
2023-11-05 12:57:38 +0900

    :test_tube: Test: 중복되지 않는 로또 사이즈만큼의 로또 번호 생성 테스트
    - 정해진 숫자만큼 로또 번호를 생성하는지 테스트
    - 중복되지 않는 숫자로만 이루어졌는지 테스트

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -3,7 +3,9 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,7 +21,8 @@ class LottoNumberTest {
 
         // when
         // then
-        assertThatThrownBy(() -> LottoNumber.of(number)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> LottoNumber.generateLottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -29,17 +32,36 @@ class LottoNumberTest {
         int number = 46;
         // when
         // then
-        assertThatThrownBy(() -> LottoNumber.of(number)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> LottoNumber.generateLottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 10 , 20, 30, 45})
-    @DisplayName("로또 번호는 1 ~ 45 사이의 값으로  이루어져있다.")
-    void createLottoNumber(int number) {
+    @DisplayName("로또 번호는 1 ~ 45 사이의 값으로 이루어져있다.")
+    void generateLottoNumber(int number) {
         // given
 
         // when
         // then
-        assertThat(LottoNumber.of(number)).isNotNull();
+        assertThat(LottoNumber.generateLottoNumber(number))
+                .isNotNull();
+    }
+
+    @Test
+    @DisplayName("로또 사이즈만큼 로또 번호를 생성한다.")
+    void generateLottoNumbers() {
+        // given
+        int lottoSize = 6;
+
+        // when
+        List<LottoNumber> lottoNumbers = LottoNumber.generateLottoNumbers(lottoSize);
+
+        // then
+        assertAll(
+                () -> assertThat(lottoNumbers).isNotEmpty(),
+                () -> assertThat(lottoNumbers).hasSize(lottoSize),
+                () -> assertThat(lottoNumbers).doesNotHaveDuplicates()
+        );
     }
 }

923e471e42200bb641f9fbf86128a0163eafb90d
hyebin <xxico89@gmail.com>
2023-11-06 00:41:55 +0900

    [Feat] 로또 번호 중복 체크 기능 추가 - Lotto.java

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,6 +10,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateLottoNumber(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -22,6 +25,14 @@ public class Lotto {
             if (!(0 < n && n < 46)) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+
+        if (numbers.size() != numberSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 

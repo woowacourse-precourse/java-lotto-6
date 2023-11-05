9b8ae854c2e884c2403bea4e579d954014a9f7cd
LUKR7Q <ybkim0628@naver.com>
2023-11-05 19:35:40 +0900

    validate(numbers): 로또 번호가 6개인지 확인하고, 아닐 경우 에러 메시지를 출력하고 예외를 던집니다.

--- src/main/java/lotto/service/Lotto.java
+++ src/main/java/lotto/service/Lotto.java
@@ -1,8 +1,25 @@
 package lotto.service;
 
+import java.util.Collections;
+import java.util.List;
+import lotto.view.ExceptionMessage;
 
 public class Lotto {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        Collections.sort(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            ExceptionMessage.sizeException();
+            throw new IllegalArgumentException();
+        }
+    }
 
 }

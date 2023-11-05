ce7e16f1e50baf8ee657da3b200cd4e9d4b860c5
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-05 18:12:36 +0900

    refactor(LottoMessage): PURCHASED_LOTTO_NUMBERS_MESSAGE 변수명에서 LOTTO_NUMBERS_OUTPUT_MESSAGE 으로 변경
    docs: 로또 메시지 관련 상수 변수명 변경

--- src/main/java/lotto/constant/LottoMessage.java
+++ src/main/java/lotto/constant/LottoMessage.java
@@ -3,7 +3,7 @@ package lotto.constant;
 public enum LottoMessage {
     PURCHASE_AMOUNT_INPUT_MESSAGE("구입금액을 입력해 주세요."),
     PURCHASED_LOTTO_QUANTITY_MESSAGE("%d개를 구매했습니다."),
-    PURCHASED_LOTTO_NUMBERS_MESSAGE("[%d, %d, %d, %d, %d, %d]"),
+    LOTTO_NUMBERS_OUTPUT_MESSAGE("[%d, %d, %d, %d, %d, %d]"),
     WINNING_NUMBERS_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS_MESSAGE("당첨 통계" + "\n" +

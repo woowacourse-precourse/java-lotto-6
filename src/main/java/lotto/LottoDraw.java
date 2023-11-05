15974984a040b603530f21dc604658b0380bd78e
minsang-alt <mazinger97@naver.com>
2023-11-05 14:34:58 +0900

    feat: 당첨번호를 저장하는 로또 당첨 클래스 구현
    - 로또 클래스와 동일한 유효성 검사와 추가적으로 보너스 번호에 대한 유효성 검사도 추가
    - 로또 클래스와 마찬가지로 로또 당첨 리스트를 반환할때 불변 객체로 반환

---
+++ src/main/java/lotto/LottoDraw.java
@@ -0,0 +1,51 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoDraw {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public LottoDraw(List<Integer> numbers, int bonusNumber) {
+        validateSize(numbers);
+        validateDuplicate(numbers, bonusNumber);
+        validateScopeNumber(numbers, bonusNumber);
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers);
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개의 숫자와 1개의 보너스 번호만 존재합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers, int bonusNumber) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호 내에는 중복 숫자가 있어서는 안됩니다.");
+        } else if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호와 보너스번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateScopeNumber(List<Integer> numbers, int bonusNumber) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+    }
+
+}

a28a19bf44f81e79784267bf667eaa575222bbd1
IM-GYURI <kgulr0517@gmail.com>
2023-11-05 20:14:38 +0900

    feat: PrizeResult 클래스 생성

---
+++ src/main/java/lotto/domain/PrizeResult.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class PrizeResult {
+    private final Map<Prize, Integer> prizeResult;
+
+    public PrizeResult() {
+        prizeResult = new EnumMap<Prize, Integer>(Prize.class);
+        Arrays.stream(Prize.values())
+                .forEach(prize -> prizeResult.put(prize, 0));
+    }
+}

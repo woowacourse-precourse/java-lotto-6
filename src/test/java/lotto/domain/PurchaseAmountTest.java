583d8cb38f278901531bc24f9acc7be708e784f9
Hanjaemo <hjm2530@gmail.com>
2023-11-06 04:25:43 +0900

    test(PurchaseAmountTest): 구입 금액 검증 기능 단위 테스트

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -6,7 +6,37 @@ import org.junit.jupiter.api.Test;
 
 class PurchaseAmountTest {
 
-    private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_PURCHASE_AMOUNT = 1_000;
+    private static final int MAX_PURCHASE_AMOUNT = 100_000;
+
+    @DisplayName("구입 금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void createPurchaseAmount_Fail_ByInvalidUnit() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new PurchaseAmount(1200))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입 금액은 1,000원 단위어야 합니다.");
+    }
+
+    @DisplayName("구입 금액이 최솟값보다 작으면 예외가 발생한다.")
+    @Test
+    void createPurchaseAmount_Fail_ByLessThanMinimum() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new PurchaseAmount(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입 금액은 %,d원 이상 %,d원 이하여야 합니다."
+                        .formatted(MIN_PURCHASE_AMOUNT, MAX_PURCHASE_AMOUNT));
+    }
+
+    @DisplayName("구입 금액이 최댓값보다 크면 예외가 발생한다.")
+    @Test
+    void createPurchaseAmount_Fail_ByLessThanMaximum() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new PurchaseAmount(1000000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입 금액은 %,d원 이상 %,d원 이하여야 합니다."
+                        .formatted(MIN_PURCHASE_AMOUNT, MAX_PURCHASE_AMOUNT));
+    }
 
     @DisplayName("decrease를 호출하면 구입 금액이 로또 가격만큼 감소한다.")
     @Test
@@ -15,7 +45,7 @@ class PurchaseAmountTest {
         PurchaseAmount purchaseAmount = new PurchaseAmount(2000);
 
         // when
-        int decreasedAmount = purchaseAmount.decrease(LOTTO_PRICE);
+        int decreasedAmount = purchaseAmount.decrease(1000);
 
         // then
         Assertions.assertThat(decreasedAmount).isEqualTo(1000);

ac7941c519859a3a6b436454b2641c1257a4e6e3
km9404 <kmh940408@gmail.com>
2023-11-06 07:43:05 +0900

    feat (InputView): 로또 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,19 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     public long inputAmount() {
         String input = Console.readLine();
         validateAmount(input);
         return Long.parseLong(input);
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        String input = Console.readLine();
+        return StringToIntConversion(input);
     }
 
     public static void validateAmount(String input) {
@@ -19,5 +27,16 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자 입력이 필요합니다.");
         }
+    }
+
+    private List<Integer> StringToIntConversion(String input) {
+        String[] stringWinningNumber = input.split(",");
+        List<Integer> intWinningNumber = new ArrayList<>();
+
+        for (String number : stringWinningNumber) {
+            validateStringToLongConversion(number);
+            intWinningNumber.add(Integer.parseInt(number));
+        }
+        return intWinningNumber;
     }
 }

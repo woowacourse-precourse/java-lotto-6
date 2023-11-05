a3174415b37a5bb0f016ef64cee010efdf6cee5d
ljkhyeong <jolri24@gmail.com>
2023-11-06 00:37:56 +0900

    feat: 구매한 가격만큼 로또리스트 생성 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,16 +1,26 @@
 package lotto.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import lotto.domain.ErrorType;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.common.ErrorType;
 import lotto.domain.Lotto;
+
 
 public class LottoService {
 
 	private static final int MIN_LOTTO_PRICE = 1000;
 
 	public List<Lotto> purchaseLotto(int price) {
+		validate(price);
 
+		int lottoAmount = price / MIN_LOTTO_PRICE;
+
+		return IntStream.range(0, lottoAmount)
+			.mapToObj(i -> generateRandomLotto())
+			.collect(Collectors.toList());
 	}
 
 	private void validate(int price) {

3380383698e40adcc0520e662f16c110eabcf3e8
Hanjaemo <hjm2530@gmail.com>
2023-11-06 05:42:50 +0900

    feat(Application, LottoController): 로또 게임 실행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.LottoIssuer;
+import lotto.domain.LottoManager;
+import lotto.domain.ProfitRateCalculator;
+import lotto.domain.WinningDetails;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoManager lottoManager = new LottoManager(
+                new LottoIssuer(),
+                new WinningDetails(),
+                new ProfitRateCalculator()
+        );
+        LottoController lottoController = new LottoController(lottoManager);
+        lottoController.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,55 @@
+package lotto.controller;
+
+import java.util.List;
+import java.util.function.Supplier;
+import lotto.domain.Lotto;
+import lotto.domain.LottoManager;
+import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningNumbers;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final LottoManager lottoManager;
+
+    public LottoController(LottoManager lottoManager) {
+        this.lottoManager = lottoManager;
+    }
+
+    public void run() {
+        PurchaseAmount purchaseAmount = repeatReadForInvalid(this::getPurchaseAmount);
+        List<Lotto> issuedLottos = lottoManager.issueLotto(purchaseAmount);
+        List<String> lottos = issuedLottos.stream()
+                .map(Lotto::toString)
+                .toList();
+        OutputView.printIssuedLottoDetails(lottos);
+
+        WinningNumbers winningNumbers = repeatReadForInvalid(this::getWinningNumbers);
+        lottoManager.addRankToWinningDetails(issuedLottos, winningNumbers);
+        OutputView.printWinningDetails(lottoManager.getWinningDetailsToString());
+
+        double profitRate = lottoManager.calculateProfitRate(purchaseAmount);
+        OutputView.printProfitRate(profitRate);
+    }
+
+    private PurchaseAmount getPurchaseAmount() {
+        return new PurchaseAmount(InputView.readPurchaseAmount());
+    }
+
+    private WinningNumbers getWinningNumbers() {
+        return new WinningNumbers(
+                InputView.readWinningNumbers(),
+                InputView.readBonusNumber()
+        );
+    }
+
+    private <T> T repeatReadForInvalid(Supplier<T> reader) {
+        try {
+            return reader.get();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return repeatReadForInvalid(reader);
+        }
+    }
+}

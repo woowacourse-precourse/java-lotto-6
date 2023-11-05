f620aee1a6e2887d91f1f78f599e412b241384ae
권장순 <jsoonworld@gmail.com>
2023-11-06 06:10:57 +0900

    feat(Lotto) : Lotto#validate에 로또 번호의 중복을 검증하는 로직을 추가하고 Lotto#hasDuplicates 를 개발함.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,12 +14,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
         for (Integer number : numbers) {
             if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("당첨 번호는 1~45 사이여야 합니다.");
+                throw new IllegalArgumentException("로또 번호는 1~45 사이여야 합니다.");
             }
         }
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
     }
 }

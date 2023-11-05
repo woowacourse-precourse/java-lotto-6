316b845b9b51bcba1a3e156c465a42e957130266
KangGeonyoung <kky6335@gmail.com>
2023-11-05 15:10:11 +0900

    feat(OutputView): 당첨 통계 출력 - printTotalResult(int fifth, int fourth, int third, int second, int first): 당첨 통계 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,4 +16,16 @@ public class OutputView {
             System.out.println(purchasedLotto.get(i).getLotto());
         }
     }
+
+    public static void printTotalResult(int fifth, int fourth, int third, int second, int first) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + fifth + "개");
+        System.out.println("4개 일치 (50,000원) - " + fourth + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + third + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + first + "개");
+    }
+
 }

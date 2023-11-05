5e5db0fa2571ac8f9933977619f2c50bd320f7fd
newh08 <newh08@gmail.com>
2023-11-06 07:17:08 +0900

    ♻️REFACTOR: AppConfig LottoController 생성자 변경으로 인한 Appconfig 수정

--- src/main/java/lotto/AppConfig.java
+++ src/main/java/lotto/AppConfig.java
@@ -4,18 +4,14 @@ import static lotto.util.ModelAndViewConverter.getModelAndViewConverter;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LottoController;
-import lotto.domain.lotto.LottoBundle;
 import lotto.domain.lottoresult.LottoResultsRepository;
-import lotto.domain.service.LottoPurchaseService;
-import lotto.domain.service.LottoResultsService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class AppConfig {
 
     public LottoController getLottoController() {
-        return new LottoController(inputView(), outputView(), getModelAndViewConverter(), lottoPurchaseService(),
-                lottoResultsService());
+        return new LottoController(inputView(), outputView(), getModelAndViewConverter(), lottoResultsRepository());
     }
 
     private InputView inputView() {
@@ -24,18 +20,6 @@ public class AppConfig {
 
     private OutputView outputView() {
         return new OutputView(getModelAndViewConverter());
-    }
-
-    private LottoPurchaseService lottoPurchaseService() {
-        return new LottoPurchaseService(lottoBundle());
-    }
-
-    private LottoBundle lottoBundle() {
-        return new LottoBundle();
-    }
-
-    private LottoResultsService lottoResultsService() {
-        return new LottoResultsService(lottoResultsRepository());
     }
 
     private LottoResultsRepository lottoResultsRepository() {

5c83a84f87a854c4de69ffe3ee4dd29198ccea5a
Heejun-0219 <heejun0219@gmail.com>
2023-11-06 00:30:59 +0900

    fix: 구분자로 시작하거나 끝나는지, 중복 구분자 있는지 확인 문구 추가

--- src/main/java/lotto/service/UserNumbersValidateEnum.java
+++ src/main/java/lotto/service/UserNumbersValidateEnum.java
@@ -3,6 +3,8 @@ package lotto.service;
 public enum UserNumbersValidateEnum {
     PRE_NUMBERS_ERROR("[ERROR] 숫자만 입력 가능합니다."),
     PRE_NUMBERS_DELIMITER_ERROR("[ERROR] 구분자와 숫자만 입력 가능합니다."),
+    DELIMITER_START_END_ERROR("[ERROR] 구분자로 시작하거나 끝날 수 없습니다."),
+    DELIMITER_DOUBLE_ERROR("[ERROR] 구분자는 연속으로 올 수 없습니다."),
     LOTTO_NUMBERS_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     LOTTO_NUMBERS_DUPLICATE_ERROR("[ERROR] 로또 번호는 중복이 불가능합니다."),
     LOTTO_NUMBERS_SIZE_ERROR("[ERROR] 로또 번호는 6개만 가능합니다."),

363a8eece5e7561c986bc9896df83a3873c13bd2
sleeg00 <slee000220@gmail.com>
2023-11-05 19:51:14 +0900

    Feat: 로또 구입 금액 예외처리

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -4,6 +4,13 @@ public class User {
     private long purchaseAmount;
 
     public void setPurchaseAmount(long purchaseAmount) {
+        validate(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
+    }
+
+    private void validate(long purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }

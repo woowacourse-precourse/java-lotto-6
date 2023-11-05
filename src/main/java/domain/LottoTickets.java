958d45842872e3bc8615a7d62e7c7bfa60067dd1
songsunkook <songsunkook@gmail.com>
2023-11-06 00:41:14 +0900

    rename: lottoList 네이밍 변경

--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -1,7 +1,6 @@
 package domain;
 
 import constant.ConstantNumber;
-import constant.WinningPrize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,29 +8,29 @@ import java.util.List;
 public class LottoTickets {
     public static final int TICKET_COST = 1000;
 
-    private final List<Lotto> lottoList;
+    private final List<Lotto> lottoes;
 
     public LottoTickets() {
-         lottoList = new ArrayList<>();
+         lottoes = new ArrayList<>();
     }
 
-    LottoTickets(List<Lotto> lottoList) {
-        this.lottoList = lottoList;
+    LottoTickets(List<Lotto> lottoes) {
+        this.lottoes = lottoes;
     }
 
     public void add(int count) {
         for (int i = 0; i < count; i++) {
-            lottoList.add(new Lotto());
+            lottoes.add(new Lotto());
         }
     }
 
     int size() {
-        return lottoList.size();
+        return lottoes.size();
     }
 
     public List<Integer> getRanks(WinningLotto winningLotto) {
         List<Integer> ranks = createList(ConstantNumber.COUNT_OF_WINNING_PRIZE.get());
-        lottoList.stream()
+        lottoes.stream()
                 .mapToInt(lotto -> lotto.rank(winningLotto))
                 .filter(rank -> rank != -1)
                 .forEach(rank -> ranks.set(rank - 1, ranks.get(rank - 1) + 1));

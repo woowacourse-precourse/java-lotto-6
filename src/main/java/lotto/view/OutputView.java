cc4515e40f64d7f2ae63b4383451c863439bf2c4
gyungchan Jo <namejgc@naver.com>
2023-11-06 01:09:47 +0900

    fix(OutputView): 당첨 통계에 대한 결과를 출력 형식에 맞게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,7 +8,8 @@ import java.util.Map;
 
 public class OutputView {
     private static final String NEWLINE = System.lineSeparator();
-    private static final String NUMBER_OF_LOTTOS_MESSAGE_FORMAT = "%d개를 구매했습니다." + NEWLINE;
+
+    private static final String NUMBER_OF_LOTTOS_MESSAGE_FORMAT = "%d개를 구매했습니다.";
 
     public void printLottos(List<LottoDto> lottos) {
         newLine();
@@ -21,7 +22,7 @@ public class OutputView {
     }
 
     private void printLottoCount(int size) {
-        System.out.printf(NUMBER_OF_LOTTOS_MESSAGE_FORMAT, size);
+        System.out.println(String.format(NUMBER_OF_LOTTOS_MESSAGE_FORMAT, size));
     }
 
     private void printLottoNumbers(List<LottoDto> lottos) {
@@ -40,11 +41,11 @@ public class OutputView {
         System.out.println("당첨 통계" + NEWLINE + "---");
 
         Map<String, Integer> result = lottoResultDto.getResult();
-        System.out.println("3개 일치 (5,000원) - " + result.getOrDefault("FIFTH", 0));
-        System.out.println("4개 일치 (50,000원) - " + result.getOrDefault("FOURTH", 0));
-        System.out.println("5개 일치 (1,500,000원) - " + result.getOrDefault("THIRD", 0));
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.getOrDefault("SECOND", 0));
-        System.out.println("6개 일치 (2,000,000,000원) - " + result.getOrDefault("FIRST", 0));
+        System.out.println("3개 일치 (5,000원) - " + result.getOrDefault("FIFTH", 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.getOrDefault("FOURTH", 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.getOrDefault("THIRD", 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.getOrDefault("SECOND", 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.getOrDefault("FIRST", 0) + "개");
     }
 
     public void printRateOfResult(double rateOfReturn) {

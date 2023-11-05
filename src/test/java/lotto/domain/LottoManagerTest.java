84987942599711883d22ab75360ccb0a818d2568
Juhyorim <joohr1234@gmail.com>
2023-11-06 02:23:11 +0900

    test: 리팩토링 후 테스트코드 에러 수정
    InputValidator의 로직을 LottoManager로 옮기는 리팩토링 후, 두 클래스에 대한 테스트코드 수정

--- src/test/java/lotto/domain/LottoManagerTest.java
+++ src/test/java/lotto/domain/LottoManagerTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -36,5 +38,65 @@ class LottoManagerTest {
 
         //then
         verify(lottoGenerator).generateLottos(userInput / 1000);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000", "5000", "100000", "10000000"})
+    @DisplayName("로또 구입금액이 정상적인 값일 때")
+    void goodCase(int userInput) {
+        //given
+        Input input = mock(ConsoleInput.class);
+        Output output = mock(ConsoleOutput.class);
+        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
+
+        when(input.getPrice()).thenReturn(userInput);
+        doNothing().when(output).printLottoPriceRequest();
+        when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());
+
+        LottoManager lottoManager = new LottoManager(input, output, lottoGenerator, new WinningNumbersManager());
+        lottoManager.buyLotto();
+
+        //then: printError가 호출되지 않는지 확인
+        verify(output, never()).printError(anyString());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"999", "1", "100", "30"})
+    @DisplayName("로또 구입금액이 1000원 미만이면 예외")
+    void 로또_구입금액이_1000원_미만이면_예외(int userInput) {
+        //given
+        Input input = mock(ConsoleInput.class);
+        Output output = mock(ConsoleOutput.class);
+        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
+
+        when(input.getPrice()).thenReturn(userInput, 1000);
+        doNothing().when(output).printLottoPriceRequest();
+        when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());
+
+        LottoManager lottoManager = new LottoManager(input, output, lottoGenerator, new WinningNumbersManager());
+        lottoManager.buyLotto();
+
+        //then: printError가 호출되는지 확인
+        verify(output).printError(anyString());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1999", "20001", "1001", "999900"})
+    @DisplayName("로또 구입금액이 1000원 단위가 아니면 예외")
+    void 로또_구입금액이_1000원_단위가_아니면_예외(int userInput) {
+        //given
+        Input input = mock(ConsoleInput.class);
+        Output output = mock(ConsoleOutput.class);
+        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
+
+        when(input.getPrice()).thenReturn(userInput, 1000);
+        doNothing().when(output).printLottoPriceRequest();
+        when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());
+
+        LottoManager lottoManager = new LottoManager(input, output, lottoGenerator, new WinningNumbersManager());
+        lottoManager.buyLotto();
+
+        //then: printError가 호출되는지 확인
+        verify(output).printError(anyString());
     }
 }
--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -9,9 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class InputValidatorTest {
-    private static final String NO_A_NUMBER_MESSAGE = "로또 금액은 숫자여야 합니다";
-    private static final String MIN_PRICE_1000_MESSAGE = "로또 금액은 1000원 이상이어야 합니다";
-    private static final String UNIT_OF_1000_REQUIRED_MESSAGE = "로또 금액은 1000원 단위로 입력되어야 합니다";
+    private static final String NOT_A_NUMBER_MESSAGE = "입력은 숫자여야 합니다";
 
     private static InputValidator inputValidator;
 
@@ -22,38 +20,11 @@ class InputValidatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1000", "5000", "100000", "10000000"})
-    @DisplayName("로또 구입금액이 정상적인 값일 때")
-    void goodCase(String userInput) {
-        assertDoesNotThrow(() -> {
-            inputValidator.validatePrice(userInput);
-        });
-    }
-
-    @ParameterizedTest
     @CsvSource(value = {"가나다", "ㅇ", "흥", "1000."})
-    @DisplayName("로또 구입금액이 숫자가 아니면 예외")
-    void 로또_구입금액이_숫자가_아니면_예외(String userInput) {
-        assertThatThrownBy(() -> inputValidator.validatePrice(userInput))
+    @DisplayName("입력이 숫자가 아니면 예외")
+    void 입력이_숫자가_아니면_예외(String userInput) {
+        assertThatThrownBy(() -> inputValidator.validateIsNumber(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(NO_A_NUMBER_MESSAGE);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"999", "1", "100", "30"})
-    @DisplayName("로또 구입금액이 1000원 미만이면 예외")
-    void 로또_구입금액이_1000원_미만이면_예외(String userInput) {
-        assertThatThrownBy(() -> inputValidator.validatePrice(userInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MIN_PRICE_1000_MESSAGE);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"1999", "20001", "1001", "999900"})
-    @DisplayName("로또 구입금액이 1000원 단위가 아니면 예외")
-    void 로또_구입금액이_1000원_단위가_아니면_예외(String userInput) {
-        assertThatThrownBy(() -> inputValidator.validatePrice(userInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(UNIT_OF_1000_REQUIRED_MESSAGE);
+                .hasMessageContaining(NOT_A_NUMBER_MESSAGE);
     }
 }

677d38e0fc0dcaf6b563e6dedff9f56780119098
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 20:26:26 +0900

    FIX(LottoNumberVerifier): verifyParseNumber 메서드 구현

--- src/main/java/lotto/verifier/LottoNumberVerifier.java
+++ src/main/java/lotto/verifier/LottoNumberVerifier.java
@@ -1,9 +1,21 @@
 package lotto.verifier;
 
+import lotto.exception.ExceptionMsg;
+
 public class LottoNumberVerifier {
     private final String winningNumbers;
 
     public LottoNumberVerifier(String winningNumbers) {
         this.winningNumbers = winningNumbers;
     }
+
+
+    private int verifyParseNumber(String numberStr) {
+        try {
+            return Integer.parseInt(numberStr.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMsg.NOT_INT_INPUT.getMessage());
+        }
+    }
+
 }

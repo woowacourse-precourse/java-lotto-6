509b0fd7f9d690385bdf5b001718c23d787b614c
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-05 17:38:19 +0900

    feat (OutputView) : 당첨 결과 출력 기능 구현
    - 당첨 정보와 수익률을 출력함

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,12 @@ package lotto.view;
 
 import static java.util.stream.Collectors.joining;
 
+import java.text.DecimalFormat;
+import java.util.List;
+import lotto.domain.WinningRanking;
 import lotto.dto.LotteriesDto;
 import lotto.dto.LottoDto;
+import lotto.dto.WinningStatisticsDto;
 
 public class OutputView {
 
@@ -11,6 +15,19 @@ public class OutputView {
     private static final String NUMBER_DELIMITER = ", ";
     private static final String NUMBER_PREFIX = "[";
     private static final String NUMBER_SUFFIX = "]\n";
+
+    private static final String WINNING_STATISTICS_TITLE = """
+                        
+            당첨 통계
+            ---
+            """;
+    private static final String WINNING_RANKING_FORMAT = "%d개 일치 (%s원) - %d개\n";
+    private static final String WINNING_RANKING_BONUS_MATCHED_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+    private static final String RATE_OF_RETURN_FORMAT = "총 수익률은 %s%%입니다.\n";
+    private static final List<WinningRanking> OUTPUT_ORDER_OF_RANKING = List.of(WinningRanking.FIFTH,
+            WinningRanking.FOURTH, WinningRanking.THIRD, WinningRanking.SECOND, WinningRanking.FIRST);
+    private static final DecimalFormat NUMBER_FORMATTER = new DecimalFormat("###,###.#");
+    private static final int NOT_EXIST = 0;
 
     private static final String EXCEPTION_MESSAGE_FORMAT = "[ERROR] %s\n";
 
@@ -33,6 +50,43 @@ public class OutputView {
                 .sorted()
                 .map(Object::toString)
                 .collect(joining(NUMBER_DELIMITER, NUMBER_PREFIX, NUMBER_SUFFIX));
+    }
+
+    public void printWinningStatistics(WinningStatisticsDto winningStatistics) {
+        print(WINNING_STATISTICS_TITLE);
+        printStatisticsByOrderedRanking(winningStatistics);
+        printRateOfReturn(winningStatistics.getRateOfReturnPercentage());
+    }
+
+    private void printStatisticsByOrderedRanking(WinningStatisticsDto winningStatistics) {
+        for (WinningRanking ranking : OUTPUT_ORDER_OF_RANKING) {
+            int count = winningStatistics.countOfRanking().getOrDefault(ranking, NOT_EXIST);
+            String message = makeRankingView(ranking, count);
+            print(message);
+        }
+    }
+
+    private String makeRankingView(WinningRanking ranking, int count) {
+        if (ranking.isBonusCorrect()) {
+            return WINNING_RANKING_BONUS_MATCHED_FORMAT
+                    .formatted(ranking.getCountOfCorrect(), toPrintedNumber(ranking.getPrice()), count);
+        }
+        return WINNING_RANKING_FORMAT
+                .formatted(ranking.getCountOfCorrect(), toPrintedNumber(ranking.getPrice()), count);
+    }
+
+    private void printRateOfReturn(double ratePercentage) {
+        String printedRate = toPrintedNumber(ratePercentage);
+        String message = RATE_OF_RETURN_FORMAT.formatted(printedRate);
+        print(message);
+    }
+
+    private String toPrintedNumber(long number) {
+        return NUMBER_FORMATTER.format(number);
+    }
+
+    private String toPrintedNumber(double number) {
+        return NUMBER_FORMATTER.format(number);
     }
 
     public void printExceptionMessage(Exception exception) {

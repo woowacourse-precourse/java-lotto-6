e105c17a40784f91e7941c3e445f808eb4f14a68
nahyeon99 <nahyeonee99@gmail.com>
2023-11-05 18:56:09 +0900

    feat(domain/Prize): Enum 미당첨 상수값 추가
    1. 당첨되지 못한 경우도 추가
    2. 매칭 개수 필드 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,17 +1,20 @@
 package lotto.domain;
 
 public enum Prize {
-    FIRST(1,  2_000_000_000),
-    SECOND(2,30_000_000),
-    THIRD(3, 1_500_000),
-    FORTH(4, 50_000),
-    FIFTH(5, 5_000);
+    FIRST(1, 6,  2_000_000_000),
+    SECOND(2, 5, 30_000_000),
+    THIRD(3, 5, 1_500_000),
+    FORTH(4, 4, 50_000),
+    FIFTH(5, 3, 5_000),
+    NONE(6, 0, 0);
 
     private final int rank;
+    private final int matchingCount;
     private final int winningAmount;
 
-    Prize(int rank, int winningAmount) {
+    Prize(int rank, int matchingCount, int winningAmount) {
         this.rank = rank;
+        this.matchingCount = matchingCount;
         this.winningAmount = winningAmount;
     }
 
@@ -21,8 +24,12 @@ public enum Prize {
             case 5 -> Prize.THIRD;
             case 4 -> Prize.FORTH;
             case 3 -> Prize.FIFTH;
-            default -> throw new RuntimeException("RuntimeException");
+            default -> Prize.NONE;
         };
+    }
+
+    public int getMatchingCount() {
+        return matchingCount;
     }
 
     public int getWinningAmount() {

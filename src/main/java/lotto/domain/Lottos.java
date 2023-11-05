c8d805f5f11c1320a8a818a2e10a07a5b7dbeced
siwol025 <cyk99115695@gmail.com>
2023-11-06 00:12:03 +0900

    feat(Lottos): 매개변수로 들어온 만큼 Lotto를 생성하는 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,8 +1,25 @@
 package lotto.domain;
 
-public class Lottos {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-    public static Lottos createLottos() {
-        return null;
+public class Lottos {
+    private List<Lotto> lottos;
+
+    private Lottos(final List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos createLottos(final int lottoAmount) {
+        List<Lotto> lottos = IntStream.range(0, lottoAmount)
+                .mapToObj(i -> LottoFactory.createLotto())
+                .collect(Collectors.toList());
+
+        return new Lottos(lottos);
+    }
+
+    public int getLottoAmount() {
+        return lottos.size();
     }
 }

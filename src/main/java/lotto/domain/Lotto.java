26b9f3f36ef279ab2e8c183cb49801967f547843
doha <dohaahi@naver.com>
2023-11-05 13:02:12 +0900

    style (Lotto): 공백 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,7 +9,6 @@ public class Lotto {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
 
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {

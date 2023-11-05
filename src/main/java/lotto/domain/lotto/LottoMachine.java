a78d553311caeee2712806fb7118c603536e18d8
PgmJun <chltmdwns96@gmail.com>
2023-11-06 01:28:53 +0900

    refactor: LottoMachine 매직넘버 제거

--- src/main/java/lotto/domain/lotto/LottoMachine.java
+++ src/main/java/lotto/domain/lotto/LottoMachine.java
@@ -6,9 +6,14 @@ import java.util.List;
 import lotto.domain.Money;
 
 public class LottoMachine {
+    private static final int LOTTO_PURCHASE_UNIT = 1_000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_AMOUNT = 6;
+
 
     public List<Lotto> purchaseLottos(Money money) {
-        int count = money.getAmount() / 1_000;
+        int count = money.getAmount() / LOTTO_PURCHASE_UNIT;
 
         return generateLottos(count);
     }

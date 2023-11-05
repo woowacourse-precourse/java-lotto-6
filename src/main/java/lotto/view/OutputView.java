42c212906fa27d422a19276e77db915c714095ec
carol <carol072400@gmail.com>
2023-11-06 00:29:01 +0900

    feat(lotto): 등수별 로또 티켓 당첨 개수를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -17,8 +17,14 @@ public class OutputView {
         }
     }
 
-    public void printWinningLotteryTickets(Map<Integer, Integer> winningLotteryTickets) {
-        System.out.println(winningLotteryTickets);
+    public void printWinningLotteryTickets(Map<Integer, Integer> winningTicketsCount) {
+
+        System.out.println("3개 일치 (5,000원) - " + winningTicketsCount.get(5) + "개");
+        System.out.println("4개 일치 (50,000원) - " + winningTicketsCount.get(4) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + winningTicketsCount.get(3) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningTicketsCount.get(2) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + winningTicketsCount.get(1) + "개");
+
     }
 
 }

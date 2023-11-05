b824b836d0577f6c3e374011f33b9629641ec6e9
haiseong <wjdgotjd9908@gmail.com>
2023-11-06 04:17:30 +0900

    feat(Money): 에러 메시지 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class Money {
     private static final long TICKET_PRICE = 1000L;
+    private static final String MINIMUM_BUDGET_ERROR_MESSAGE = "1000원 이상으로 금액을 입력해야합니다.";
+    private static final String NOT_DIVISIBLE_ERROR_MESSAGE = "구입금액은 1000원 단위이어야 합니다.";
 
     private final long money;
 
@@ -11,19 +13,19 @@ public class Money {
     }
 
     private void validate(long money) {
-        validateDivisible(money);
         validateMinimumBudget(money);
-    }
-
-    private void validateDivisible(long money) {
-        if (money % TICKET_PRICE != 0L) {
-            throw new IllegalArgumentException();
-        }
+        validateDivisible(money);
     }
 
     private void validateMinimumBudget(long money) {
         if (money < TICKET_PRICE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(MINIMUM_BUDGET_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateDivisible(long money) {
+        if (money % TICKET_PRICE != 0L) {
+            throw new IllegalArgumentException(NOT_DIVISIBLE_ERROR_MESSAGE);
         }
     }
 

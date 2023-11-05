7ea5b2b2fb64728ce9471c47fa7ee6a43c5db411
KIVSH <kihongkim3@hotmail.com>
2023-11-05 21:45:35 +0900

    refactor: change Rank enum

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,18 +1,31 @@
 package lotto;
 
 public enum Rank {
-    THREE("3개", 5000),
-    FOUR("4개", 50000),
-    FIVE("5개", 1500000),
-    BONUS("보너스", 30000000),
-    SIX("6개", 2000000000);
+    THREE(3, false, 5000),
+    FOUR(4, false, 50000),
+    FIVE(5, false, 1500000),
+    BONUS(5, true, 30000000),
+    SIX(6, false, 2000000000);
 
-    private final String message;
-
+    private final int matchCount;
+    private final boolean matchBonus;
     private final int prizeMoney;
 
-    Rank(String message, int prizeMoney) {
-        this.message = message;
+    Rank(int matchCount, boolean matchBonus, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
         this.prizeMoney = prizeMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isMatchBonus() {
+        return matchBonus;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
     }
 }

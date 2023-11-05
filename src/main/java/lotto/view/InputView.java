4cc905a717ae77a6f898504028a57498cac7f25b
sudhdkso <waga1009@gmail.com>
2023-11-06 01:24:01 +0900

    feat: 로또 입력에 대한 함수 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    private static final String LOTTO_NUMBERS_DELIMITER = ",";
+
+    private InputView() {
+    }
+
+    public String getLottoCount() {
+        //TODO "구입금액을 입력해 주세요." 출력
+        return Console.readLine();
+
+    }
+
+    public List<String> getLottoWinningNumbers() {
+        //TODO "당첨 번호를 입력해 주세요." 출력
+        String winningNumbers = Console.readLine();
+        return Arrays.asList(winningNumbers.split(LOTTO_NUMBERS_DELIMITER));
+    }
+
+    public String getBonusNumber() {
+        //TODO "보너스 번호를 입력해 주세요." 출력
+        return Console.readLine();
+    }
+}

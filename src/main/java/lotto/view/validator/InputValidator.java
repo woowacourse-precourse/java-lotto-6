bd670c10cc7daf09e9379bf082f8843c51e3230d
gyungchan Jo <namejgc@naver.com>
2023-11-05 22:58:24 +0900

    feat(InputValidator): 구입 금액에 대한 입력이 숫자인지 검증하는 로직 구현

--- src/main/java/lotto/view/validator/InputValidator.java
+++ src/main/java/lotto/view/validator/InputValidator.java
@@ -2,12 +2,30 @@ package lotto.view.validator;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
     private static final String INVALID_INPUT_EXCEPTION_MESSAGE = "입력은 공백일 수 없습니다.";
+    private static final String NOT_DIGIT_EXCEPTION_MESSAGE = "구입 금액에 대한 입력은 숫자만 가능합니다.";
+
+    private static final Pattern ONLY_DIGIT = Pattern.compile("-?[0-9]+");
 
     protected static void validateInput(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException(INVALID_INPUT_EXCEPTION_MESSAGE);
         }
+    }
+
+    public static void validatePurchaseAmount(String input) {
+        validateInput(input);
+
+        if (isNotDigit(input)) {
+            throw new IllegalArgumentException(NOT_DIGIT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static boolean isNotDigit(String input) {
+        return !ONLY_DIGIT.matcher(input)
+                .matches();
     }
 }
--- src/test/java/lotto/view/validator/InputValidatorTest.java
+++ src/test/java/lotto/view/validator/InputValidatorTest.java
@@ -3,6 +3,7 @@ package lotto.view.validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -15,5 +16,14 @@ class InputValidatorTest {
         assertThatThrownBy(() -> InputValidator.validateInput(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력은 공백일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "!", "12A"})
+    @DisplayName("구입 금액에 대한 입력이 숫자가 아니면 예외가 발생한다.")
+    void validateNumber(String input) {
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구입 금액에 대한 입력은 숫자만 가능합니다.");
     }
 }

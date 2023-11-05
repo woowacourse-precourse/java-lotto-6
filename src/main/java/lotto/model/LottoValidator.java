b1d312e656476d11bd3636e44d1f0f5b0f13f59a
Jung So Young <soyoung5611@gmail.com>
2023-11-05 17:19:37 +0900

    feat. Lotto 번호들을 검수하는 클래스

---
+++ src/main/java/lotto/model/LottoValidator.java
@@ -0,0 +1,57 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoValidator {
+
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
+    public static void validate(List<Integer> numbers) {
+        validateLottoNumbers(numbers);
+        validateDuplicated(numbers);
+        validateRange(numbers);
+    }
+
+    private static void validateLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(LottoError.LOTTO_NUMBER_COUNT.getMessage());
+        }
+    }
+
+    private static void validateDuplicated(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LottoError.LOTTO_NUMBER_DUPLICATE.getMessage());
+        }
+    }
+
+    private static void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER)) {
+            throw new IllegalArgumentException(LottoError.LOTTO_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private static void validateBonusRange(int number) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(LottoError.LOTTO_BONUS_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private static void validateBonusDuplicated(List<Lotto> numbers, int bonusNumber) {
+        if (numbers.stream().anyMatch(lotto -> lotto.isContain(bonusNumber))) {
+            throw new IllegalArgumentException(LottoError.LOTTO_BONUS_NUMBER_DUPLICATE.getMessage());
+        }
+    }
+
+    public static int validateBonus(List<Lotto> numbers, int bonusNumber) {
+        validateBonusRange(bonusNumber);
+        validateBonusDuplicated(numbers, bonusNumber);
+        return bonusNumber;
+    }
+
+
+
+
+
+}

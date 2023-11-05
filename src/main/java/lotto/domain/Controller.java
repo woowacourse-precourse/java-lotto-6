635c0a41fd815fc13a22b4a9fe64ba10d8a7aca6
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-05 16:54:42 +0900

    feat: 구매 개수만큼 로또 번호 발행

--- src/main/java/lotto/domain/Controller.java
+++ src/main/java/lotto/domain/Controller.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
 import lotto.model.LottoTicket;
 import lotto.model.Purchase;
 import lotto.validator.ValidatorPurchaseAmount;
@@ -25,9 +29,41 @@ public class Controller {
 
         LottoTicket lottoTicket = new LottoTicket(calculateLottoTicketCount(purchase));
         view.displayPurchaseQuantityMessage(lottoTicket);
+
+        displayLottoNumbers(generateLottoNumbersList(generateLottoTickets(lottoTicket)));
     }
 
     public int calculateLottoTicketCount(Purchase purchase) {
-        return Integer.parseInt(purchase.getPurchaseAmount())/1000;
+        return Integer.parseInt(purchase.getPurchaseAmount()) / 1000;
     }
+
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public List<Lotto> generateLottoTickets(LottoTicket lottoTicket) {
+        List<Lotto> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < lottoTicket.getLottoTicketCount(); i++) {
+            List<Integer> numbers = generateLottoNumbers();
+            Lotto lotto = new Lotto(numbers);
+            lottoNumbers.add(lotto);
+        }
+        return lottoNumbers; //user class 에 저장해야하나 ?
+    }
+
+    public List<Object> generateLottoNumbersList(List<Lotto> lottoNumbers) {
+        List<Object> numbers = new ArrayList<>();
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            Lotto lotto = lottoNumbers.get(i);
+            numbers.add(lotto.getLottoNumbers());
+        }
+        return numbers;
+    }
+
+    public void displayLottoNumbers(List<Object> numbers) {
+        for (Object number : numbers) {
+            System.out.println(number);
+        }
+    }
+
 }

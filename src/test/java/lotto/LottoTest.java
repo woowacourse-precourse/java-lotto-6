d8f95fa088c57038175a3e9a32e5b5a9609c58cb
김승우 <tmd8633@naver.com>
2023-11-05 19:25:57 +0900

    Feat : 객체 테스트케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,16 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.WinNum;
+import lotto.enums.LottoEnum;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static lotto.enums.LottoEnum.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +30,30 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("당첨결과값 확인")
+    @Test
+    void countResult() {
+        // given
+            // 내 로또번호 생성
+            List<Integer> lottoList = Arrays.asList(1, 2, 3, 4, 5, 6);
+            Lotto lotto = new Lotto(lottoList);
+
+            List<Integer> lottoList2 = Arrays.asList(1, 2, 3, 4, 5, 7);
+            Lotto lotto2 = new Lotto(lottoList2);
+
+
+            // 당첨번호, 보너스 번호 생성
+            List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);// 당첨번호
+            int bonusNum = 7;
+            WinNum winNum = new WinNum(list, bonusNum);
+
+        // when
+        lotto.countResult(winNum);
+        lotto2.countResult(winNum);
+
+        // then
+        Assertions.assertThat(W1.getResult()).isEqualTo(1);
+        Assertions.assertThat(W2.getResult()).isEqualTo(1);
+    }
 }

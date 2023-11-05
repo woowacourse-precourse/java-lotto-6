19918829cc17d6b6af9bf52d11527989ceaa6353
You Taein <ytn1574@gmail.com>
2023-11-05 23:04:21 +0900

    feat: 사용자 입력 검증 기능 구현
    당첨번호 숫자가 6개가 아닐 경우 예외발생, 숫자 혹은 구분자(,) 이외의 문자가 포함되어있을 경우 예외발생, 입력된 당첨번호가 범위내(1 ~ 45)의 숫자가 아닐 경우 예외발생

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -14,14 +14,40 @@ public class Validation {
     }
 
     public void validateUnderZero(int money) {
-        if(money < 0) {
+        if (money < 0) {
             throw new IllegalArgumentException("[ERROR] : 0보다 큰 숫자를 입력해야 합니다.");
         }
     }
 
     public void validateDivideThousand(int money) {
-        if(money%1000 != 0) {
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] : 입력 금액은 1000 단위여야 합니다.");
         }
     }
+
+    public String[] validateWinNumbersFormat(String winNumbersInput) {
+        String[] splitWinNumbersInput = winNumbersInput.split(",");
+        if (splitWinNumbersInput.length != 6) {
+            throw new IllegalArgumentException("[ERROR] : 당첨 번호는 6개여야 합니다.");
+        }
+        for (int i = 0; i < winNumbersInput.length(); i++) {
+            if (!Character.isDigit(winNumbersInput.charAt(i)) && winNumbersInput.charAt(i) != ',') {
+                throw new IllegalArgumentException("[ERROR] : 숫자, 구분자(,)만 입력 가능합니다.");
+            }
+        }
+        return splitWinNumbersInput;
+    }
+
+    public int[] validateWinNumbersInRange(String[] splitWinNumbersInput) {
+        int[] winNumbers = new int[6];
+        for (int i = 0; i < splitWinNumbersInput.length; i++) {
+            int temp = Integer.parseInt(splitWinNumbersInput[i]);
+            if (temp < 0 || temp > 45) {
+                throw new IllegalArgumentException("[ERROR] : 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            winNumbers[i] = temp;
+        }
+        return winNumbers;
+    }
+
 }

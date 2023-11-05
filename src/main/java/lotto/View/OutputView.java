e650315946eb85f63d00c545ba6f89c1c5defe7a
YONGHWA\ekore <yonghwa95@naver.com>
2023-11-05 14:10:58 +0900

    feat(OutputView) : 등수 출력, 수익률 출력 기능 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,16 +1,31 @@
 package lotto.View;
 
+import lotto.Domain.Rank;
+
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
     public static void printLottosSize(int size) {
-        System.out.println(size+"개를 구매했습니다.");
+        System.out.println(String.format("%d개를 구매했습니다.", size));
     }
 
     public static void printLottosNumbers(List<String> lottos) {
         for (String lotto : lottos) {
             System.out.println(lotto);
         }
+    }
+
+    public static void printRankCount(Map<Rank,Integer> rankCount) {
+        System.out.println(Rank.FIFTH.getMatchedNumbers()+"개 일치 (5,000원) - "+rankCount.getOrDefault(Rank.FIFTH,0)+"개");
+        System.out.println(Rank.FOURTH.getMatchedNumbers()+"개 일치 (50,000원) - "+rankCount.getOrDefault(Rank.FOURTH,0)+"개");
+        System.out.println(Rank.THIRD.getMatchedNumbers()+"개 일치 (1,500,000원) - "+rankCount.getOrDefault(Rank.THIRD,0)+"개");
+        System.out.println(Rank.SECOND.getMatchedNumbers()+"개 일치, 보너스 볼 일치 (30,000,000원) - "+rankCount.getOrDefault(Rank.SECOND,0)+"개");
+        System.out.println(Rank.FIRST.getMatchedNumbers()+"개 일치 (2,000,000,000원) - "+rankCount.getOrDefault(Rank.FIRST,0)+"개");
+    }
+
+    public static void printReturnRate(double returnRate) {
+        System.out.println("총 수익률은 " + returnRate + "%입니다.");
     }
 }

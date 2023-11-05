dd980ac1f43b4ad94287a1efa8cb2fe0ed2a095e
Lee Geun Woo <gamebdy@naver.com>
2023-11-06 03:51:40 +0900

    :sparkles: method 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
+
 public class Application {
+    public static int numberOfLotto;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startLotto();
     }
+
+    public static void startLotto() {
+        buyLotto();
+        writeNumber();
+
+
+    }
+
+    public static void buyLotto() {
+
+    }
+
+    public static void writeNumber() {
+        writeOriginNumber();
+        writeBonusNumber();
+    }
+
+    public static void writeOriginNumber() {
+
+    }
+
+    public static void writeBonusNumber() {
+
+    }
+
+
 }
+

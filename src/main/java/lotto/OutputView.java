9fc1d80ee73facf915866a90822c32407e329d43
SUJI-J <suzy852@naver.com>
2023-11-05 17:42:03 +0900

    feat: 구입 금액 메세지 출력

---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import static lotto.Constants.REQUEST_BUY_MONEY;
+
+public class OutputView {
+    public static void printRequestMoney() {
+        System.out.println(REQUEST_BUY_MONEY);
+    }
+}

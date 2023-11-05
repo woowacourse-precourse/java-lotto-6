704121b70ee0bf09ab34084264ebeaa727db5bec
WooYoungDoo <duden5000@naver.com>
2023-11-05 22:40:09 +0900

    feat : 로또 생성 시 검증 로직

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,17 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
+import static lotto.constant.ErrorConstant.ERROR_PREFIX;
+
 public class Lotto {
+
+    private static final int CORRECT_NUMBERS_SIZE = 6;
+    private static final String NUMBERS_SIZE_NOT_SIX_EXCEPTION_MESSAGE = "발행 로또 번호의 개수는 6개여야합니다.";
+    private static final String NUMBERS_DUPLICATE_EXCEPTION_MESSAGE = "발행 로또 번호는 중복될 수 없습니다.";
+    private static final String NUMBERS_WRONG_RANGE_EXCEPTION_MESSAGE = "발행 로또 번호는 1~45의 범위여야합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,12 +20,35 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (isNumbersSizeNotSix(numbers)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + NUMBERS_SIZE_NOT_SIX_EXCEPTION_MESSAGE);
+        }
+        if (isNumbersDuplicate(numbers)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + NUMBERS_DUPLICATE_EXCEPTION_MESSAGE);
+        }
+        if (isNumbersWrongRange(numbers)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + NUMBERS_WRONG_RANGE_EXCEPTION_MESSAGE);
         }
     }
 
+    private boolean isNumbersSizeNotSix(List<Integer> numbers) {
+        return numbers.size() != CORRECT_NUMBERS_SIZE;
+    }
+
+    private boolean isNumbersDuplicate(List<Integer> numbers) {
+        long numbersDistinctSize = numbers.stream()
+                .distinct().count();
+        return numbersDistinctSize != CORRECT_NUMBERS_SIZE;
+    }
+
+    private boolean isNumbersWrongRange(List<Integer> numbers) {
+        long correntNumbersRangeCount = numbers.stream()
+                .filter(number -> number >= 1 && number <= 45)
+                .count();
+        return correntNumbersRangeCount != CORRECT_NUMBERS_SIZE;
+    }
+
     public List<Integer> getNumbers() {
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 }

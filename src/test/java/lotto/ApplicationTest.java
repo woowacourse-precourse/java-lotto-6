9ca26b025e8f3eea209bf802ac946ec7bf2fd6df
whxogus215 <whxogus215@naver.com>
2023-11-05 11:31:21 +0900

    refactor: 에러 테스트 error 패키지로 이동

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -6,8 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
-import lotto.utils.Casher;
-import lotto.utils.enums.ErrorMessage;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -256,56 +254,6 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
-        });
-    }
-
-    @Test
-    void 예외_테스트_가격_1000원_미만() {
-        assertSimpleTest(() -> {
-            runException("900");
-            assertThat(output())
-                    .contains(ERROR_MESSAGE)
-                    .contains(Casher.NOT_ENOUGH_PRICE);
-        });
-    }
-
-    @Test
-    void 예외_테스트_가격_1000원_단위X() {
-        assertSimpleTest(() -> {
-            runException("1300");
-            assertThat(output())
-                    .contains(ERROR_MESSAGE)
-                    .contains(Casher.NOT_LOTTO_UNIT);
-        });
-    }
-
-    @Test
-    void 예외_테스트_당첨번호_미입력() {
-        assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5");
-            assertThat(output())
-                    .contains(ERROR_MESSAGE)
-                    .contains(ErrorMessage.LACK_NUMBER_COUNT.getMessage());
-        });
-    }
-
-    @Test
-    void 예외_테스트_당첨번호_중복() {
-        assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,5");
-            assertThat(output())
-                    .contains(ERROR_MESSAGE)
-                    .contains(ErrorMessage.NOT_DUPLICATED.getMessage());
-        });
-    }
-
-    @Test
-    void 예외_테스트_보너스번호_미입력() {
-        assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,6"," ");
-            assertThat(output())
-                    .contains(ERROR_MESSAGE)
-                    .contains(ErrorMessage.NOT_BLANK_NUMBER.getMessage());
         });
     }
 

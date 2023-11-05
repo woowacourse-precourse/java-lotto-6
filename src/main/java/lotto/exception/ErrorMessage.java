ebf8f2d7b3f853b9716e8b51f7625b752421890f
leesh <qwqw8019@naver.com>
2023-11-05 16:33:30 +0900

    feat: 로또 도메인 에러 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,7 +1,12 @@
 package lotto.exception;
 
 public enum ErrorMessage {
+    // Lotto Validation
     INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복되지 않아야 합니다."),
+
+    // Winning Lotto Validation
     INVALID_BONUS_NUMBER_RANGE("보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_BONUS_NUMBER("보너스 번호가 당첨 번호와 중복되었습니다.");
 

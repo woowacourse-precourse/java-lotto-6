71d0955041a299e924770102b6bbe80df3275578
beongjun1 <beongjun1@naver.com>
2023-11-05 18:45:34 +0900

    feat:로또 발행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
     private static final Integer lottoPrice = 1000;
@@ -9,7 +12,14 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         Integer inputMoney = InputException();
-
+        Integer n = inputMoney / lottoPrice;
+        Lotto lottoArray[] = new Lotto[n];
+        System.out.println("\n"+n+"개를 구매했습니다.");
+        for (int i = 0; i < n; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoArray[i] = new Lotto(numbers);
+            System.out.println(lottoArray[i].getNumbers());
+        }
 
     }
 
@@ -20,10 +30,10 @@ public class Application {
             if (inputMoney % lottoPrice != 0) {
                 throw new IllegalArgumentException("[ERROR] 입력 단위는 1,000원 단위여야 합니다.");
             }
-            money=inputMoney;
+            money = inputMoney;
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            money=InputException();
+            money = InputException();
         }
         return money;
     }

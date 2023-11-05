9b19a2ed2bc66393ef168fdbb762605d694403c4
youngbeom-Kim <abcd450196@gmail.com>
2023-11-06 02:49:22 +0900

    test: 로또 금액 입력 테스트 추가

--- src/test/java/lotto/util/LottoValidationTest.java
+++ src/test/java/lotto/util/LottoValidationTest.java
@@ -2,6 +2,7 @@ package lotto.util;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoPurchase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,18 @@ class LottoValidationTest {
 
         assertThatThrownBy(() -> lottoPurchase.lottoPurchaseCount(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @DisplayName("구입 금액에 숫자가 아닌 다른 문자가 들어갈 경우 예외 발생")
+    @Test
+    void createLottoNonNumber() {
+        LottoPurchase lottoPurchase = new LottoPurchase();
+        String input = "5500l";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            lottoPurchase.lottoPurchaseCount(input);
+        });
     }
 
 }

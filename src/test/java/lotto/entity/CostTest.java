8a37dba94f9fa46d536995f70acff126559d1b99
seonghoo1217 <seonghoo1217@naver.com>
2023-11-06 03:52:10 +0900

    test: cost 유효한 값 기반의 객체 생성

--- src/test/java/lotto/entity/CostTest.java
+++ src/test/java/lotto/entity/CostTest.java
@@ -2,9 +2,11 @@ package lotto.entity;
 
 import static org.assertj.core.api.Assertions.*;
 
+import lotto.property.MethodProperty;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
 
 public class CostTest {
 
@@ -22,15 +24,16 @@ public class CostTest {
     }
 
     @Test
-    void Cost_생성_유효한_값인_경우_테스트(){
+    void Cost_생성_유효한_값인_경우_테스트() throws NoSuchFieldException, IllegalAccessException {
         //given
-        String purchaseCost = "8000";
-        Long targetCost = Long.parseLong(purchaseCost);
+        String inputPurchaseCost = "8000";
+        Long targetCost = Long.parseLong(inputPurchaseCost);
 
         //when
-        Cost cost = new Cost(purchaseCost);
+        Cost cost = new Cost(inputPurchaseCost);
+        Long purchaseCost = cost.getFieldValue(MethodProperty.PURCHASE_COST);
 
         //then
-        assertThat(cost.getPurchaseCost()).isEqualTo(targetCost);
+        assertThat(purchaseCost).isEqualTo(targetCost);
     }
 }

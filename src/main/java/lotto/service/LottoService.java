36d54d0e798db2831eef81bc1657f3ee5c868fd7
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-06 03:19:27 +0900

    feat: add empty lotto service

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,19 @@
+package lotto.service;
+
+public class LottoService {
+    public void buyLotto(){
+
+    }
+
+    public void saveWinningNumbers(){
+
+    }
+
+    public void saveBonusNumber(){
+
+    }
+
+    public void calcLotto(){
+
+    }
+}

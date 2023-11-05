9e73069fc0b844ffce66d66498d487880a32054f
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-11-06 01:39:16 +0900

    feat: 로또 랜덤 번호출력 (Application)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,16 @@
 package lotto;
 
 import java.awt.print.PrinterGraphics;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.domain.InputAmount;
+import lotto.domain.LotteryTicket;
 import lotto.view.PrintView;
 
 public class Application {
     private static InputAmount inputAmount;
+    //private static List<Integer> printlotto = new ArrayList<>();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         start_Lotto();
@@ -13,14 +18,20 @@ public class Application {
     public static void start_Lotto(){
         int Buy_Ticket = Input_Lotto_Buy();
         PrintView.Output_Lotto_Buy(Buy_Ticket);
-        Lottery_Ticket(Buy_Ticket);
+        Print_Lotto(Buy_Ticket);
     }
     public static int Input_Lotto_Buy(){
         inputAmount = new InputAmount(PrintView.Input_Lotto_Buy());
         return inputAmount.Used_Lotto();
     }
-    public static void Lottery_Ticket(int Buy_Ticket){
-
+    public static void Print_Lotto(int Buy_Ticket) {
+        LotteryTicket lotteryticket = new LotteryTicket();
+       // printlotto = new ArrayList<>();
+        for (int i = 0; i < Buy_Ticket; i++) {
+            //printlotto = lotteryticket.RandomLottoNum();
+            System.out.println( lotteryticket.RandomLottoNum());
+        }
     }
+
 
 }
--- src/main/java/lotto/view/PrintView.java
+++ src/main/java/lotto/view/PrintView.java
@@ -6,12 +6,14 @@ public class PrintView {
     private static final String Input_Lotto_Buy = "구입금액을 입력해 주세요.";
     private static final String Output_Lotto_Buy = "개를 구매했습니다.";
 
+
     public static String Input_Lotto_Buy(){
         System.out.println(Input_Lotto_Buy);
         return Console.readLine();
     }
 
     public static void Output_Lotto_Buy(int price){
+        System.out.println();
         System.out.println(price + Output_Lotto_Buy);
     }
 

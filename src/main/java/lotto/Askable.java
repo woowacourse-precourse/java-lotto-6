bc9dde6ecefe0323996172f2396f4dd8720190c0
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 17:22:40 +0900

    feat(winning): enforce winning number input within valid range
    Update console input prompt to repeatedly request winning numbers until entries fall within the acceptable range

--- src/main/java/lotto/Askable.java
+++ src/main/java/lotto/Askable.java
@@ -2,8 +2,14 @@ package lotto;
 
 public interface Askable {
     String inputPurchaseAmount = "구입금액을 입력해주세요.";
+    String inputWinningNumbers = "당첨 번호를 입력해주세요.";
+    String inputBonusNumber = "보너스 번호를 입력해 주세요.";
     String nonPositiveIntegerError = "[ERROR] 구입금액은 양의 정수여야 합니다.";
     String nonThousandUnitError = "[ERROR] 구입금액은 1000원 단위여야 합니다.";
+    String numberRangeError = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    String duplicateNumbersError = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
+    String WinningNumberAmountError = "[ERROR] 로또 번호는 6개여야 합니다.";
+    String BonusNumberAmountError = "[ERROR] 보너스 번호는 1개여야 합니다.";
 
     <T> T ask();
 }
--- src/main/java/lotto/winning/WinningNumbers.java
+++ src/main/java/lotto/winning/WinningNumbers.java
@@ -1,4 +1,57 @@
 package lotto.winning;
 
-public class WinningNumbers {
+import static lotto.Settings.MINIMUM;
+import static lotto.Settings.MAXIMUM;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import lotto.Askable;
+
+public class WinningNumbers implements Askable {
+    @Override
+    public List<Integer> ask() {
+        System.out.println(inputWinningNumbers);
+
+        String input;
+        List<Integer> convertedInput;
+        do {
+            input = readLine();
+            convertedInput = convertInput(input);
+
+        } while (!validate(convertedInput));
+
+        System.out.println();
+
+        return convertedInput;
+    }
+
+    private boolean validate(List<Integer> convertedInput) {
+        boolean corretInput = true;
+
+        try {
+            if (!isCorrectRange.test(convertedInput)) {
+                throw new IllegalArgumentException(numberRangeError);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            corretInput = false;
+        }
+
+        return corretInput;
+    }
+
+    private List<Integer> convertInput(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private final Predicate<List<Integer>> isCorrectRange = input ->
+            input.stream().mapToInt(Integer::intValue).min().getAsInt() >= MINIMUM.getNumber() &&
+                    input.stream().mapToInt(Integer::intValue).max().getAsInt() <= MAXIMUM.getNumber();
 }

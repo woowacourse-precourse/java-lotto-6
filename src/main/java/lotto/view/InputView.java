b1d933985166d22e2fb9f096d76a9c1a07a78109
jeondui <jeondui@gmail.com>
2023-11-06 03:27:40 +0900

    refactor(InputView): 줄바꿈 문자 상수로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.common.Constant;
 
 public class InputView {
-    private static final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요";
+    private static final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_PRIZE_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
@@ -13,12 +14,12 @@ public class InputView {
     }
 
     public static String inputPrizeNumber() {
-        System.out.println(INPUT_LOTTO_PRIZE_NUMBER);
+        System.out.println(Constant.LINE.getCharValue() +INPUT_LOTTO_PRIZE_NUMBER);
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.println(Constant.LINE.getCharValue() +INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 }

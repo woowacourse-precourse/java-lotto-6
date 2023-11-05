9a982be8bb373cd45c87de9e9c4b8f2cfac3c468
elyudwo <dudqk9696@naver.com>
2023-11-06 00:26:55 +0900

    [Refactor] : PurchaseAmount 객체 예외 처리 방식 변경
    숫자 판별 예외처리 기능 삭제 (사용자 입력 시 예외처리)

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -4,32 +4,23 @@ public class PurchaseAmount {
 
     private final int purchaseAmount;
 
-    private PurchaseAmount(String purchaseAmount) {
+    private PurchaseAmount(int purchaseAmount) {
         validatePurchaseAmount(purchaseAmount);
-        this.purchaseAmount = Integer.parseInt(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
     }
 
-    public static PurchaseAmount from(String purchaseAmount) {
+    public static PurchaseAmount from(int purchaseAmount) {
         return new PurchaseAmount(purchaseAmount);
     }
 
-    private void validatePurchaseAmount(String purchaseAmount) {
-        validatePurchaseAmountNotNumber(purchaseAmount);
-        validatePurchaseAmountUnderThousand(Integer.parseInt(purchaseAmount));
-        validatePurchaseAmountDivideByThousand(Integer.parseInt(purchaseAmount));
+    private void validatePurchaseAmount(int purchaseAmount) {
+        validatePurchaseAmountUnderThousand(purchaseAmount);
+        validatePurchaseAmountDivideByThousand(purchaseAmount);
     }
 
     private void validatePurchaseAmountDivideByThousand(int purchaseAmount) {
         if(purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] : 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");
-        }
-    }
-
-    private void validatePurchaseAmountNotNumber(String purchaseAmount) {
-        try {
-            Integer.parseInt(purchaseAmount);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] : 로또 구입 금액은 정수입니다.");
         }
     }
 

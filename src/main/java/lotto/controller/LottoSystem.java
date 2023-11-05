317bd1b9d6bf7b9bf9df674abd235ddd4ca89f06
Hoo-sung <gntjd135@gmail.com>
2023-11-05 09:07:36 +0900

    fix(LottoSystem): RuntimeVerifier 의존성 주입 및 예외 발생시 그 부분부터 다시 입력 받도록 수정

--- src/main/java/lotto/controller/LottoSystem.java
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -3,12 +3,14 @@ package lotto.controller;
 import lotto.domain.BonusNumber;
 import lotto.domain.UserLotto;
 import lotto.domain.WinningLotto;
+import lotto.verifier.RuntimeVerifier;
 
 public class LottoSystem {
 
     private UserLotto userLotto;
     private WinningLotto winningLotto;
     private BonusNumber bonusNumber;
+    private final RuntimeVerifier runtimeVerifier = new RuntimeVerifier();
 
     public void run() {
 
@@ -22,7 +24,16 @@ public class LottoSystem {
     private void initialize() {
         userLotto = new UserLotto();
         winningLotto = new WinningLotto();
-        bonusNumber = new BonusNumber();
+        while (true) {
+            try {
+                bonusNumber = new BonusNumber();
+                runtimeVerifier.checkBonusNumberExistsInWinningLotto(winningLotto, bonusNumber);
+                return;
+            } catch (IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 
 

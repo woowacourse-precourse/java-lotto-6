750afa2880b277aa41412d4932d415995a352dbb
eunhee <dmsgml9903@gmail.com>
2023-11-05 13:38:17 +0900

    test(LottoTest): 주어진 번호가 자신의 번호에 존재하는지 계산하는 기능 테스트
    아래 기능을 테스트:
    - 하나의 번호를 주면 그 번호가 존재하는지 확인하는 기능 테스트
    - 로또 객체를 주면 로또 객체에서 동일한 객체가 몇개 있는지 반환하는 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,18 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -38,5 +45,58 @@ class LottoTest {
 
         //then
         assertThat(lotto.getNumbers()).usingRecursiveComparison().isEqualTo(List.of(1, 4, 5, 7, 11, 36));
+    }
+
+    @DisplayName("로또 번호에 존재하는 값을 주면 true를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5, 7, 11, 4, 1, 36})
+    void hasNumberReturnTrue(Integer input) {
+        //given
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(5, 7, 11, 4, 1, 36)));
+
+        //when
+        Boolean hasNumber = lotto.hasNumber(input);
+
+        //then
+        assertThat(hasNumber).isTrue();
+    }
+
+    @DisplayName("로또 번호에 존재하지 않는 값을 주면 false를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {12, 6, 42, 22, 27, 3})
+    void hasNumberReturnFalse(Integer input) {
+        //given
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(5, 7, 11, 4, 1, 36)));
+
+        //when
+        Boolean hasNumber = lotto.hasNumber(input);
+
+        //then
+        assertThat(hasNumber).isFalse();
+    }
+
+
+    static Stream<Arguments> createLottoNumbers() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(5, 7, 11, 4, 1, 36), 6),
+                Arguments.of(Arrays.asList(5, 7, 11, 4, 1, 2), 5),
+                Arguments.of(Arrays.asList(5, 7, 11, 4, 22, 2), 4),
+                Arguments.of(Arrays.asList(3, 45, 10, 44, 22, 2), 0)
+        );
+    }
+
+    @DisplayName("주어진 로또와 같은 번호가 있는 수만큼 반환한다.")
+    @ParameterizedTest
+    @MethodSource("createLottoNumbers")
+    void countMatchNumber(List<Integer> lottoNumbers, Integer result) {
+        //given
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(5, 7, 11, 4, 1, 36)));
+        Lotto lotto2 = new Lotto(new ArrayList<>(lottoNumbers));
+
+        //when
+        Integer countMatchNumber = lotto.countMatchNumber(lotto2);
+
+        //then
+        assertThat(countMatchNumber).isEqualTo(result);
     }
 }

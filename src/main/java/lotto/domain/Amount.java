19270f8e75a1408230cddb8748cba48127132821
mina <qkralsdk991@naver.com>
2023-11-05 18:37:53 +0900

    Feat : 총 구입 금액 입력 기능 구현

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,2 +1,23 @@
-package lotto.domain;public class Amount {
+package lotto.domain;
+
+public class Amount {
+    private final int total;
+    private int count;
+
+    public Amount(int total, int ticketPrice) {
+        this.total = total;
+        count = calculateCount(total,ticketPrice);
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    private int calculateCount(int total,int ticketPrice) {
+        return total/ticketPrice;
+    }
+
+
+
+    //TODO : 예외처리
 }

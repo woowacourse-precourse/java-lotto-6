f9aad98b21d0d1e423280f315bb8dce4e861243c
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-05 15:02:23 +0900

    feat: 구입금액에 잘못된 값을 입력할 경우 예외발생

--- src/main/java/lotto/validator/ValidatorPurchaseAmount.java
+++ src/main/java/lotto/validator/ValidatorPurchaseAmount.java
@@ -4,6 +4,21 @@ import lotto.model.Purchase;
 public class ValidatorPurchaseAmount {
     public ValidatorPurchaseAmount() {
     }
+
+    private void validatePurchaseAmount(Purchase purchase) {
+        if (!isPurchaseAmountValid(purchase)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isPurchaseAmountValid(Purchase purchase) {
+        boolean unitOfMoneyValid = isThousandWonMultiple(purchase);
+        boolean minValueValid = isLessThan1000(purchase);
+        boolean numberValid = isNotNumeric(purchase);
+        boolean nonEmpty = isEmpty(purchase);
+        return unitOfMoneyValid && minValueValid && numberValid && nonEmpty;
+    }
+
     private boolean isThousandWonMultiple(Purchase purchase) {
         int returnAmount = Integer.parseInt(purchase.getPurchaseAmount());
         return returnAmount % 1000 == 0;

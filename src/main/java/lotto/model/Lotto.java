10f833897da8be377d41781fd9ce2e2231638039
IE-MangChi <hsm983278@gmail.com>
2023-11-05 16:03:57 +0900

    feat: 로또 기능 추가
    ㅇ 생성자 검증 사용
    - validateNumbersSizeIs6 : 로또 사이즈가 6인지 검증
    - validateDuplicateNumbers : 로또 번호에 중복이 있는지 검증
    - validateRangeNumbers : 로또 숫자 범위가 올바른지 검증

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,15 +6,36 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSizeIs6(numbers);
+        validateDuplicateNumbers(numbers);
+        validateRangeNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateNumbersSizeIs6(List<Integer> numbers) {
+        if (IsSizeSix(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개만 가능합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        if (hasDuplicated(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호는 입력될 수 없습니다.");
+        }
+    }
+
+    private void validateRangeNumbers(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number < 1 || number > 45)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 잘못된 숫자 범위는 입력될 수 없습니다."));
+    }
+
+    private static boolean IsSizeSix(List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+
+    private static boolean hasDuplicated(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
 }

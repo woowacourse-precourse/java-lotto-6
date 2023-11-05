00e117ddc3504fe343ea892b2941d701344c6662
songsunkook <songsunkook@gmail.com>
2023-11-05 15:14:17 +0900

    feat: 당첨 상금 enum 추가

---
+++ src/main/java/constant/WinningPrize.java
@@ -0,0 +1,21 @@
+package constant;
+
+public enum WinningPrize {
+    RANK_1ST(2000000000),
+    RANK_2ND(30000000),
+    RANK_3RD(1500000),
+    RANK_4TH(50000),
+    RANK_5TH(5000),
+    COUNT_OF_WINNING_PRIZE(5),
+    ;
+
+    private final int rank;
+
+    WinningPrize(int rank) {
+        this.rank = rank;
+    }
+
+    public int get() {
+        return rank;
+    }
+}

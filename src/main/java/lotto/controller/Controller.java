0f394e6c28b2e3286a079762fa39158e4648e24c
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-11-06 05:27:34 +0900

    프로그램 실행 시작하기

---
+++ src/main/java/lotto/controller/Controller.java
@@ -0,0 +1,18 @@
+package lotto.controller;
+
+public class Controller {
+	public Controller() {}
+	
+	public void run() {
+        try {
+            start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+	}
+
+	public void start() {
+		
+	}
+
+}

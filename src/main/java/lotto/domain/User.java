960e22992eb4617e05e86897df6bf088fd28fda9
SeongIl <biuld1234@gmail.com>
2023-11-05 17:10:25 +0900

    style: apply 자바 컨벤션

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -6,10 +6,9 @@ import lotto.ui.Output;
 
 public class User {
 
-    public Integer lottoCount; // public 수정?
+    public Integer lottoCount;
     public List<Lotto> lottos;
     Money money;
-
 
     public void payMoney() {
         money = new Money(Input.get());
@@ -17,14 +16,13 @@ public class User {
 
     public void purchaseLotto() {
         boolean isPurchasing = true;
+
         while (isPurchasing) {
             try {
-
                 payMoney();
                 lottoCount = money.countThousand();
                 lottos = LottoGenerator.createLottos(lottoCount);
                 isPurchasing = false;
-
             } catch (IllegalArgumentException ex) {
                 Output.printError(ex);
             }

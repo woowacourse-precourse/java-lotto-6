0886c0fdc650824e75965e8d73ae2afce24f49ee
gihwanJang <gihwan319@gmail.com>
2023-11-05 17:56:31 +0900

    feat : (게임기능) 구입 금액에 해당하는 만큼 로또를 발행 기능

--- src/main/java/lotto/game/Game.java
+++ src/main/java/lotto/game/Game.java
@@ -1,7 +1,10 @@
 package lotto.game;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.lotto.Lotto;
 import lotto.lotto.WinningLotto;
 
@@ -10,9 +13,19 @@ public class Game {
     private WinningLotto winningLotto;
 
     public void purchaseLotto(int price) {
+        purchasedLottos = IntStream.range(0, price / 1000)
+                .mapToObj(i -> publishLotto())
+                .collect(Collectors.toList());
     }
 
     public String getPurchasedLottosInfo() {
-        return null;
+        String lottosSizeInfo = purchasedLottos.size() + "개를 구매했습니다.\n";
+        return lottosSizeInfo + purchasedLottos.stream()
+                .map(Lotto::toString)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private Lotto publishLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 }

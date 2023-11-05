20f0d4e0c601d2f017db97204008ee8f0ae4fa45
hYe807n <rlagkdus55@gmail.com>
2023-11-06 05:14:50 +0900

    Feat: Bonus 객체의 숫자가 당첨 복권의 숫자와 중복되지 않는 지 체크

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -6,8 +6,8 @@ public class Bonus {
 
     private final int number;
 
-    public Bonus(int number) {
-        validate(number);
+    public Bonus(int number, List<Integer> answerLotto) {
+        validate(number, answerLotto);
         this.number = number;
     }
 
@@ -15,9 +15,12 @@ public class Bonus {
         return this.number;
     }
 
-    private void validate(int number) {
+    private void validate(int number, List<Integer> answerLotto) {
         if (number > 45 || number < 1) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호의 숫자 범위는 1~45까지입니다.");
+        }
+        if (answerLotto.contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자는 로또 번호와 중복될 수 없습니다.");
         }
     }
 }

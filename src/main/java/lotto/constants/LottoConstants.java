71f7cb0a99b97135fd40ab804cec57d0f3013899
Ju-Won99 <wndnjs1102@naver.com>
2023-11-05 14:36:14 +0900

    controller 패키지

--- src/main/java/lotto/constants/LottoConstants.java
+++
@@ -1,7 +0,0 @@
-package lotto.constants;
-
-public class LottoConstants {
-    public static int NUMBER_OF_LOTTO_NUMBERS = 6;
-    public static int MINIMUM_LOTTO_NUMBER = 1;
-    public static int MAXIMUM_LOTTO_NUMBER = 45;
-}

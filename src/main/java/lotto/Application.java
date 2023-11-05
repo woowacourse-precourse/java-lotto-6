45d657eb1e8e6e1a6a1e2ad370f4ff77da0c8ba1
hyebin <xxico89@gmail.com>
2023-11-05 21:12:57 +0900

    [Feat] 로또 수량 및 번호 발행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = Integer.parseInt(Console.readLine());
+
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액이 1,000원 단위가 아닙니다.");
+        }
+        int amount = money / 1000;
+        System.out.println(amount+"개를 구매했습니다.\n");
+
+        List<List<Integer>> lottos = new ArrayList<>();
+
+        for (int i=0; i<amount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(numbers);
+        }
+
+        System.out.println(lottos);
     }
 }

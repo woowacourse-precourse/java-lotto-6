d9c71f05fc78f6ce594aa4367860578b3eead595
Mingeun Park <mindal99546@gmail.com>
2023-11-05 21:19:31 +0900

    feat: IO계층 입력값 검증 구현

---
+++ src/main/java/lotto/UserInputReader.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class UserInputReader {
+
+    private static final String pureNumberPattern = "^\\d+$";
+    private static final String seperator = ",";
+
+    public static Integer readPureNumber() throws IllegalArgumentException {
+        String rawInput = Console.readLine().trim();
+        validatePureNumber(rawInput);
+        return Integer.parseInt(rawInput);
+    }
+
+    public static List<Integer> readMultiplePureNumbers() throws IllegalArgumentException {
+        String rawInput = Console.readLine().trim();
+        validateMultipleNumbers(rawInput);
+        return Arrays.stream(rawInput.split(seperator))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public static void close() {
+        Console.close();
+    }
+
+    public static void validatePureNumber(String rawInput) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile(pureNumberPattern);
+        Matcher matcher = pattern.matcher(rawInput);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateMultipleNumbers(String rawInput) throws IllegalArgumentException {
+        for (String token : rawInput.split(seperator)) {
+            validatePureNumber(token.trim());
+        }
+    }
+}

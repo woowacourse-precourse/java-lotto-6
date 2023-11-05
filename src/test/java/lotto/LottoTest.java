5631be043a1401602adeb707867c93ebafbf9079
ts3083 <ts04031@gmail.com>
2023-11-05 23:04:47 +0900

    test: LottoTest validate 및 정렬 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -25,4 +27,29 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 0이하의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoOutOfRangeUnder0() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 45이상의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoOutOfRangeOver45() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 오름차순 정렬")
+    @Test
+    void checkSortedArray() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        Lotto lotto = new Lotto(Arrays.asList(45, 1, 24, 10, 3, 36));
+        List<Integer> expectedResult = Arrays.asList(1, 3, 10, 24, 36, 45);
+        lotto.sort();
+        Assertions.assertEquals(expectedResult, lotto.getNumbers());
+    }
 }

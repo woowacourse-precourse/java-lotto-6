b8ec98a7e6c5001883213edfb8a3c0e4427d81a0
ts3083 <ts04031@gmail.com>
2023-11-06 01:49:22 +0900

    feat: LottoConstants 1000, 0 상수 추가

--- src/main/java/lotto/domain/LottoConstants.java
+++ src/main/java/lotto/domain/LottoConstants.java
@@ -4,7 +4,9 @@ public enum LottoConstants {
 
     NUMBERS_MAX_SIZE(6),
     NUMBER_MAX(45),
-    NUMBER_MIN(1);
+    NUMBER_MIN(1),
+    THOUSAND(1000),
+    ZERO(0);
 
     private final int value;
 

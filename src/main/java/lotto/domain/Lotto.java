f82b60533e9c6ed19f044171a5d74aab767f68c9
elyudwo <dudqk9696@naver.com>
2023-11-05 23:45:35 +0900

    [Feat] : 중복되는 로또 번호 예외 처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,31 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException("[ERROR] : 중복된 숫자가 로또 번호에 있으면 안됩니다.");
+            }
         }
     }
 

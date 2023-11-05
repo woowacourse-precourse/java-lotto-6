18d7e38b470d77189b2313c6d47d89cbbedb95d7
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-05 18:46:58 +0900

    fix: Rank 생성 로직 수정
    올바른 Rank를 반환해주도록 수정

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -4,40 +4,48 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    BOOM(0, false, 0, "꽝 (0원)"),
-    FIFTH(3, false, 5000, "3개 일치 (5,000원)"),
-    FOURTH(4, false, 50000, "4개 일치 (50,000원)"),
-    THIRD(5, false, 1500000, "5개 일치 (1,500,000원)"),
-    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원)");
+    BOOM(0, 0, "꽝 (0원)"),
+    FIFTH(3, 5000, "3개 일치 (5,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
     private static final int FIVE_COUNT = 5;
 
     private final int matchingCount;
-    private final boolean hasBonusNumber;
     private final long price;
     private final String description;
 
-    Rank(final int matchingCount, final boolean hasBonusNumber, final long price, final String description) {
+    Rank(final int matchingCount, final long price, final String description) {
         this.matchingCount = matchingCount;
-        this.hasBonusNumber = hasBonusNumber;
         this.price = price;
         this.description = description;
     }
 
     public static Rank of(final int count, final boolean hasBonus) {
+        if (isFiveMatch(count)) {
+            return getRankForFiveMatchesWithBonus(hasBonus);
+        }
+        return getRankByMatchingCount(count);
+    }
+
+    private static boolean isFiveMatch(final int count) {
+        return count == FIVE_COUNT;
+    }
+
+    private static Rank getRankForFiveMatchesWithBonus(final boolean hasBonus) {
+        if (hasBonus) {
+            return SECOND;
+        }
+        return THIRD;
+    }
+
+    private static Rank getRankByMatchingCount(final int count) {
         return Arrays.stream(values())
-                .filter(rank -> rank.matchesRank(count, hasBonus))
+                .filter(rank -> rank.matchesCount(count))
                 .findAny()
                 .orElse(BOOM);
-    }
-
-    private boolean matchesRank(final int count, final boolean hasBonus) {
-        return matchesFiveCount(count, hasBonus) || matchesCount(count);
-    }
-
-    private boolean matchesFiveCount(final int count, final boolean hasBonus) {
-        return count == FIVE_COUNT && hasBonusNumber == hasBonus;
     }
 
     private boolean matchesCount(final int count) {

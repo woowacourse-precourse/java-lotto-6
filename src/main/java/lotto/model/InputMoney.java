ecbc68c7975a8a1ade8d24740eb8031ea8bbc224
U-keun Song <sukeun319@gmail.com>
2023-11-06 04:42:57 +0900

    Feat : InputMoney 클래스 생성
    입력되는 값이 유효한 금액인지 확인하는 코드를 포함한 클래스를 생성했다. 유효한 값이라면 메서드를 통해 1000으로 나눈 몫을 반환할 수 있다.

---
+++ src/main/java/lotto/model/InputMoney.java
@@ -0,0 +1,36 @@
+package lotto.model;
+
+import lotto.util.InputNumber;
+
+public class InputMoney extends InputNumber {
+    InputMoney(String input) {
+        super(input);
+    }
+
+    public static InputMoney getInstance(String input) {
+        return new InputMoney(input);
+    }
+
+    @Override
+    public Long sendInputData() {
+        return this.number % 1000;
+    }
+
+    public void validate() {
+        if (isNegative()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (notDivisibleByUnit()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNegative() {
+        return this.number < 0;
+    }
+
+    private boolean notDivisibleByUnit() {
+        return number % 1000 != 0;
+    }
+}

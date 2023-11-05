bb5602e37e028587d777ce1281aaf5a786e3a539
kes02 <hijin1220@naver.com>
2023-11-05 12:53:14 +0900

    Feat: Input Phrase
    사용자가 입력할 때 무엇을 입력 해야할지 안내해주는 문구

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class InputView {
+    public static void requestMoney() {
+        System.out.println("구입금액을 입력해주세요.");
+    }
+
+    public static void requestLotto() {
+        System.out.println("당첨 번호를 입력해주세요.");
+    }
+
+    public static void requestPlusLotto() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}

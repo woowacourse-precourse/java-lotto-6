d24e96f6378edce350b41bd950ba4505236482f0
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-05 23:48:09 +0900

    Feat: 추가 findByMatchCount 함수
    일치하는 숫자 개수와 보너스 숫자로 일치하는 열거형을 찾아서 반환하는 함수

--- src/main/java/lotto/MatchingCount.java
+++ src/main/java/lotto/MatchingCount.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public enum MatchingCount {
 
-    THREE(3, 5000, false),
-    FOUR(4, 50000, false),
-    FIVE(5, 15000000, false),
-    FIVE_BONUS(5, 30000000, true),
-    SIX(6, 2000000000, false);
+    THREE(3, 5000,false),
+    FOUR(4, 50000,false),
+    FIVE(5, 15000000,false),
+    FIVE_BONUS(5, 30000000,true),
+    SIX(6,2000000000, false);
 
     private final int matchCount;
     private final int prizeMoney;
@@ -17,4 +21,16 @@ public enum MatchingCount {
         this.prizeMoney = prizeMoney;
         this.bonusNumber = bonusNumber;
     }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public static MatchingCount findByMatchCount (int count, boolean hasBonus){
+        return Arrays.stream(MatchingCount.values())
+                .filter( MatchingCount -> MatchingCount.matchCount == count)
+                .filter( MatchingCount -> MatchingCount.bonusNumber == hasBonus)
+                .findAny()
+                .orElse(null);
+   }
 }

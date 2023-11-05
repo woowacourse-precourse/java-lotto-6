836586e6993d774cad92aa7b2ec29238158e6c4a
GaBaljaintheroom <junsu1222@naver.com>
2023-11-05 09:48:17 +0900

    feat(WinningLotto): 당첨 로또 Getter 작성

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.List;
+
 public class WinningLotto {
     private final Lotto lotto;
     private final BonusNumber bonusNumber;
@@ -7,5 +10,14 @@ public class WinningLotto {
     public WinningLotto(Lotto lotto, BonusNumber bonusNumber) {
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getLotto() {
+        List<Integer> winningLotto = lotto.getNumbers();
+        return Collections.unmodifiableList(winningLotto);
+    }
+
+    public BonusNumber getBonusNumber() {
+        return bonusNumber;
     }
 }

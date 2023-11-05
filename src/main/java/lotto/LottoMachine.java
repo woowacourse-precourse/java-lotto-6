523892401a463cf0baf0db66e273fad98e22fb23
Jinnie Kim <jinniek48@gmail.com>
2023-11-06 06:16:50 +0900

    feat: 로또 구입 금액 예외 처리 추가 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,34 +1,33 @@
 package lotto;
 
-import static java.lang.Integer.parseInt;
-
-import camp.nextstep.edu.missionutils.Console;
-
 public class LottoMachine {
 
     private int lottoCount;
 
-    public LottoMachine() {
-        int money = insertMoney();
-        lottoCount = money / 1000;
+    public void insertMoney(String input) {
+        notParsableNumber(input);
+        notValidNumber(Integer.parseInt(input));
+        notDividedWithThousand(Integer.parseInt(input));
+
+        lottoCount = Integer.parseInt(input) / 1000;
     }
 
-    private int insertMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
-        int money = parseInt(Console.readLine());
-
+    private void notParsableNumber(String input) {
         try {
-            notDividedWithThousand(money);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return insertMoney();
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력한 값을 정수로 변환할 수 없습니다.");
         }
-
-        return money;
     }
 
-    private void notDividedWithThousand(int money) {
-        int remainder = money % 1000;
+    private void notValidNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("[ERROR] 입력한 값이 0 이하의 숫자입니다.");
+        }
+    }
+
+    private void notDividedWithThousand(int number) {
+        int remainder = number % 1000;
 
         if (remainder > 0) {
             throw new IllegalArgumentException("[ERROR] 1000으로 나눠 떨어지지 않습니다.");

ae331cb61a5075ed6a4f3624499a302894fb115d
Lieslot <rlaxodud1999@gmail.com>
2023-11-05 13:41:27 +0900

    test: 총 수익 산출하는 기능 테스트 작성

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -22,6 +22,8 @@ public class WinningStatistics {
     }
 
 
+    public long showRevenue() {
 
+    }
 
 }
--- src/test/java/lotto/domain/WinningStaticsTest.java
+++ src/test/java/lotto/domain/WinningStaticsTest.java
@@ -1,20 +1,26 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.Random;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningStaticsTest {
 
+    List<Rank> winningRanks;
+
+    @BeforeEach
+    void initWinningRanks() {
+        //given
+        winningRanks = List.of(Rank.FIRST, Rank.SECOND, Rank.FOURTH);
+    }
+
     @DisplayName("당첨 등수들을 바탕으로 당첨 등수의 갯수를 저장하는 기능")
     @Test
     void winningRankStoreTest() {
-        //given
-        List<Rank> winningRanks = List.of(Rank.FIRST, Rank.SECOND, Rank.FOURTH);
+
         //when
         WinningStatistics statistics = WinningStatistics.from(winningRanks);
         //then
@@ -23,8 +29,19 @@ public class WinningStaticsTest {
         assertThat(statistics.showCountOf(Rank.SECOND)).isEqualTo(1);
         assertThat(statistics.showCountOf(Rank.THIRD)).isEqualTo(0);
 
+    }
+
+    @DisplayName("당첨 등수들을 바탕으로 총 수익을 산출하는기능")
+    @Test
+    void revenueCalculationTest() {
+        //when
+        WinningStatistics winningStatistics = WinningStatistics.from(winningRanks);
+        //then
+        long expectedValue = 23000050000L;
+        assertThat(winningStatistics.showRevenue()).isEqualTo(expectedValue);
 
     }
+
 
 
 }

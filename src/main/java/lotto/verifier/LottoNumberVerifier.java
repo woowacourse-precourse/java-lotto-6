39a73b7f67e1e9e472ef1a06cd20f315829a8a64
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 20:42:54 +0900

    FIX(LottoNumberVerifier): verifyNumberRange 메서드 구현

--- src/main/java/lotto/verifier/LottoNumberVerifier.java
+++ src/main/java/lotto/verifier/LottoNumberVerifier.java
@@ -17,5 +17,9 @@ public class LottoNumberVerifier {
             throw new IllegalArgumentException(ExceptionMsg.NOT_INT_INPUT.getMessage());
         }
     }
-
+    private void verifyNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 값이어야 합니다: " + number);
+        }
+    }
 }

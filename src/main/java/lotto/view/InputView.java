90e97fec3acbf8455002d96cd20bc37b3d36ffc2
Gyeongho Yang <chchch1213@naver.com>
2023-11-05 20:02:34 +0900

    feat(view): 구입 금액을 입력 받는 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.constant.GeneralMessage.ASK_BUY_PRICE;
+
+public class InputView {
+    public static String askBuyPrice() {
+        OutputView.printGeneralMessage(ASK_BUY_PRICE);
+        return Console.readLine();
+    }
+}

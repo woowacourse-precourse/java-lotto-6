1c07165a08b83f1f49aff17e73add4657db9f528
linirini <2001yerin@naver.com>
2023-11-06 04:05:48 +0900

    test : test for get lotto count by Price

--- src/test/java/lotto/domain/PriceTest.java
+++ src/test/java/lotto/domain/PriceTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,6 +25,20 @@ class PriceTest{
     @ValueSource(ints = {1000, 10000, 11000, 510000, 11000})
     void 구입_금액_단위(int price){
         assertThatNoException().isThrownBy(() -> new Price(price));
+    }
+
+    @DisplayName("구입 금액으로 발행할 로또 개수를 구한다.")
+    @Test
+    void 구입_금액으로_로또_개수_반환(){
+        //given
+        int purchasePrice = 101000;
+        Price price = new Price(purchasePrice);
+
+        //when
+        int lottoCount = price.getLottoCount();
+
+        //then
+        assertThat(lottoCount).isEqualTo(101);
     }
 
 }

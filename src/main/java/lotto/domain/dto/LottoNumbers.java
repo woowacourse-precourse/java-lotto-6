a1f033b588b4071d9337c2ed1aee9a21dbd20561
Oseuseu <lakingsung@gmail.com>
2023-11-06 02:55:54 +0900

    feat: `LottoNumbers` 생성자 구현

---
+++ src/main/java/lotto/domain/dto/LottoNumbers.java
@@ -0,0 +1,11 @@
+package lotto.domain.dto;
+
+import java.util.List;
+
+public class LottoNumbers {
+    private final List<Integer> numbers;
+
+    public LottoNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}

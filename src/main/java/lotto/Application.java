bc6d9e46136a759bd7efa0454999e395f5e68f4a
dd <12202340@inha.edu>
2023-11-05 16:57:26 +0900

    feat: Exception을 catch해서 메세지를 출력하도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,15 @@ package lotto;
 import lotto.controller.LottoGameController;
 
 public class Application {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
     public static void main(String[] args) {
-        LottoGameController lottoGameController = new LottoGameController();
-        lottoGameController.startGame();
+        try {
+            LottoGameController lottoGameController = new LottoGameController();
+            lottoGameController.startGame();
+        } catch (Exception e) {
+            System.out.println(ERROR_PREFIX + e.getMessage());
+        }
+
     }
 }

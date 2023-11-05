7f9a594daa4e5533a8f86b17b0445db948add7b5
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-05 21:04:21 +0900

    refactor: now lottoTicket is declared with new ArrayList

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -24,7 +24,7 @@ public class Lotto {
     }
 
     public static List<Integer> produceLottoTicket() {
-        List<Integer> lottoTicket = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoTicket = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(lottoTicket);
         return lottoTicket;
     }
@@ -33,7 +33,7 @@ public class Lotto {
         System.out.println(lottoTicket.toString());
     }
 
-    public static List<Lotto> gatherLottoTickets(int quantity) {
+    public static List<Lotto> gatherLottoTickets(Integer quantity) {
         List<Lotto> lottoTicketList = new ArrayList<>();
         for (int producedLotto = 1; producedLotto <= quantity; producedLotto++) {
             List<Integer> lottoTicket = produceLottoTicket();
@@ -45,5 +45,13 @@ public class Lotto {
         }
         System.out.println();
         return lottoTicketList;
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 }

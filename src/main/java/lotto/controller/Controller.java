3315dfbe753bdac87ade36d478e9e5ca342a3733
mina <qkralsdk991@naver.com>
2023-11-05 19:06:50 +0900

    Feat : 플레이어 입력 기능 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,2 +1,43 @@
-package lotto.controller;public class Controller {
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Amount;
+import lotto.domain.Lotto;
+import lotto.utils.Parser;
+import lotto.view.RequestMessage;
+
+import java.util.List;
+
+public class Controller {
+
+    private static final int TICKET_PRICE=1000;
+
+    Parser parser = new Parser();
+
+    public void run(){
+        startLotto();
+    }
+
+    private void startLotto(){
+        Amount amount = requestPlayerAmountInput();
+        Lotto winningNum = requestPlayerWinningInput();
+        int bonusNum = requestPlayerBonusInput();
+    }
+
+    private Amount requestPlayerAmountInput(){
+        RequestMessage.requestTotalPrice();
+        int playerAmountInput = parser.stringToInteger(Console.readLine());
+        return new Amount(playerAmountInput, TICKET_PRICE);
+    }
+
+    private Lotto requestPlayerWinningInput(){
+        RequestMessage.requestWinningNum();
+        List<Integer> playerWinningInput = parser.translatePlayerInputStringToInt(Console.readLine());
+        return new Lotto(playerWinningInput);
+    }
+
+    private int requestPlayerBonusInput(){
+        RequestMessage.requestBonusNum();
+        return parser.stringToInteger(Console.readLine());
+    }
 }

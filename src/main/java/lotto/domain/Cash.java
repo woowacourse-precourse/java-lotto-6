968b9985a784aafffac31d6ab4b96a51a383e055
SungKyum Kim <sungkyum1@naver.com>
2023-11-05 23:10:18 +0900

    feat(Cash): Cash 클래스 & 1000원 단위 검증 기능 구현

--- src/main/java/lotto/domain/Cash.java
+++ src/main/java/lotto/domain/Cash.java
@@ -1,2 +1,18 @@
-package lotto.domain;public class Cash {
+package lotto.domain;
+
+public class Cash {
+    private static final int LOTTO_PRICE = 1000;
+
+    Integer amount;
+
+    public Cash(Integer amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(Integer amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

f2854cf44979b747998d3b6eb3a8942584850e0e
ddingmin <ddingminyt@gmail.com>
2023-11-06 00:43:48 +0900

    구현: 사용자 입력 받는 기능을 구현한다.

---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,43 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+	public static final String COMMA_DELIMITER = ",";
+
+	public static int purchaseAmount() {
+		System.out.println("구입금액을 입력해 주세요.");
+		return toInteger(Console.readLine());
+	}
+
+	public static List<Integer> winningNumbers() {
+		System.out.println("당첨 번호를 입력해 주세요.");
+		return toIntegerList(Console.readLine());
+	}
+
+	public static int bonusNumber() {
+		System.out.println("보너스 번호를 입력해 주세요.");
+		return toInteger(Console.readLine());
+	}
+
+	private static List<Integer> toIntegerList(String str) {
+		return Arrays.stream(str.split(COMMA_DELIMITER))
+			.map(Input::toInteger)
+			.toList();
+	}
+
+	private static int toInteger(String str) {
+		if (isNumeric(str)) {
+			return Integer.parseInt(str);
+		}
+		throw new IllegalArgumentException("[Error] 숫자로 변환할 수 없습니다.");
+	}
+
+	private static boolean isNumeric(String str) {
+		return str != null && str.matches("[0-9.]+");
+	}
+}

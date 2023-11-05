1c7d92f485b7b98d8e76b94f4e397c7434ee735a
yummygyudon <bang2brew@gmail.com>
2023-11-05 16:09:38 +0900

    FEAT : 전역적 출력 역할을 수행하는 출력 객체 구현
    - Scope : Presentation 계층

---
+++ src/main/java/lotto/global/util/channel/print/Printer.java
@@ -0,0 +1,13 @@
+package lotto.global.util.channel.print;
+
+public abstract class Printer {
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+
+    public static void printBlankLine() {
+        System.out.println();
+    }
+
+}

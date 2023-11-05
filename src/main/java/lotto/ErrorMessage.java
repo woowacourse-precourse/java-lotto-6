e06b4a44e12da6c251961cf96dcf7cbecf5b06fd
noah <jeongas9810@gmail.com>
2023-11-06 04:11:44 +0900

    feat: Add ErrorMessage
    Error message defined for exception processing.

---
+++ src/main/java/lotto/ErrorMessage.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public enum ErrorMessage {
+    NOT_SEPARATED_1000("1000원 단위의 숫자만 입력해 주세요."),
+    NOT_A_NUMBER("숫자만 입력해 주세요.")
+    ;
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        String baseErrorMessage = "[ERROR] ";
+        return baseErrorMessage + errorMessage;
+    }
+}

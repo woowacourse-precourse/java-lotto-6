b910fe5b5215fb090386be1b71beec5a9e27137e
gyungchan Jo <namejgc@naver.com>
2023-11-06 00:06:48 +0900

    feat(OutputView): 당첨 내역을 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
 import lotto.dto.LottoDto;
+import lotto.dto.LottoResultDto;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String NUMBER_OF_LOTTOS_MESSAGE_FORMAT = "%d개를 구매했습니다.";
@@ -21,10 +23,21 @@ public class OutputView {
         StringBuilder numbersMessageBuilder = new StringBuilder();
 
         for (LottoDto lotto : lottos) {
-            numbersMessageBuilder.append(lotto.getNumbers())
+            numbersMessageBuilder.append(lotto.numbers())
                     .append(NEWLINE);
         }
 
         System.out.println(numbersMessageBuilder);
+    }
+
+    public void printWinningStatistics(LottoResultDto lottoResultDto) {
+        System.out.println("당첨 통계" + NEWLINE + "---");
+
+        Map<String, Integer> result = lottoResultDto.getResult();
+        System.out.println("3개 일치 (5,000원) - " + result.getOrDefault("FIFTH", 0));
+        System.out.println("4개 일치 (50,000원) - " + result.getOrDefault("FOURTH", 0));
+        System.out.println("5개 일치 (1,500,000원) - " + result.getOrDefault("THIRD", 0));
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.getOrDefault("SECOND", 0));
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.getOrDefault("FIRST", 0));
     }
 }

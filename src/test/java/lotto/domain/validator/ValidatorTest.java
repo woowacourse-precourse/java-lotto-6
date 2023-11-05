d5bc11e228cb8f3c0cbb719233f898b249e5dece
jdohun <wjdehgns97@naver.com>
2023-11-05 15:04:20 +0900

    Test: 추가한 검증 기능 테스트
    ValidatorTest.java
    validateDuplicateNumbers()
    hasCommasWithoutSurroundingValues()
    validateCountOfValues()
    validateNumberInRange()

--- src/test/java/lotto/domain/validator/ValidatorTest.java
+++ src/test/java/lotto/domain/validator/ValidatorTest.java
@@ -1,20 +1,46 @@
 package lotto.domain.validator;
 
+import lotto.enums.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorTest {
+
+    private static List<List<Integer>> provideListsForTesting() {
+        return Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5, 5),
+                Arrays.asList(4, 5, 6, 6, 7, 7),
+                Arrays.asList(7, 8, 8, 9, 9, 6)
+        );
+    }
+
+    static Stream<Arguments> stringArraysProvider() {
+        // 테스트 데이터를 제공하는 데이터 소스 메서드
+        return Stream.of(
+                Arguments.of((Object) new String[]{"1", "2", "3"}),
+                Arguments.of((Object) new String[]{"1", "2", "3", "4", "5"}),
+                Arguments.of((Object) new String[]{"1", "2", "3", "4", "5", "6", "7"})
+        );
+    }
 
     @DisplayName("공백이나 빈값이 입력되면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "   "})
     void validateExistValue(String whiteSpace) {
         assertThatThrownBy(() -> Validator.validateExistValue(whiteSpace))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_NON_EXISTENT_VALUE.getMessage());
     }
 
     @DisplayName("맨 앞자리가 0으로 시작하거나 숫자가 아닌 값이 입력되면 예외가 발생한다.")
@@ -22,7 +48,8 @@ class ValidatorTest {
     @ValueSource(strings = {"0", "원", "1000d"})
     void validateNumericInput(String notNumber) {
         assertThatThrownBy(() -> Validator.validateNumericInput(notNumber))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_NOT_NUMERIC_VALUE.getMessage());
     }
 
     @DisplayName("1000으로 나눠지지 않는 숫자가 입력되면 예외가 발생한다.")
@@ -30,6 +57,43 @@ class ValidatorTest {
     @ValueSource(ints = {1, 21, 132, 1001, 20002})
     void validateDivisibleBy1000(int indivisible) {
         assertThatThrownBy(() -> Validator.validateDivisibleBy1000(indivisible))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_INDIVISIBLE_BY_1000.getMessage());
+    }
+
+    @DisplayName("중복 숫자가 존재하면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("provideListsForTesting")
+    void validateDuplicateNumbers(List<Integer> testList) {
+        assertThatThrownBy(() -> Validator.validateDuplicateNumbers(testList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_HAS_DUPLICATE_NUMBERS.getMessage());
+    }
+
+    @DisplayName("쉼표 전후에 아무런 값이 없으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,,,3,4,5,6", ",,1,2,3,4,5,6", "1,2,3,4,5,6,"})
+    void hasCommasWithoutSurroundingValues(String valuesSeparatedByCommas) {
+        assertThatThrownBy(() -> Validator.hasCommasWithoutSurroundingValues(valuesSeparatedByCommas))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_CONTAIN_CONSECUTIVE_COMMAS.getMessage());
+    }
+
+    @DisplayName("6개로 이루어진 값이 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("stringArraysProvider")
+    void validateCountOfValues(String[] splitValues) {
+        assertThatThrownBy(() -> Validator.validateCountOfValues(splitValues))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_COUNT_OF_VALUES.getMessage());
+    }
+
+    @DisplayName("1~45 에 해당하는 숫자 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {46, 471, 192, 2023, 2022})
+    void validateNumberInRange(int testNum) {
+        assertThatThrownBy(() -> Validator.validateNumberInRange(testNum))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_OUT_OF_RANGES.getMessage());
     }
 }

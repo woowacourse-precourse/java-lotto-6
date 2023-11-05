def09cd26df9a299086c16b8bb413b5e4e95a53d
ohhyeonn <ohhyeon120@gmail.com>
2023-11-06 05:51:10 +0900

    feat : add test 당첨번호 개수 추출 기능 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
 import lotto.Model.Lotto;
+import lotto.Model.Prize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +27,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("당첨번호 개수 추출기능 확인")
+    @Test
+    void checkCorrectWinningNumber() {
+        Lotto loto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = new ArrayList<>();
+        winningNumbers.add(1);
+        winningNumbers.add(2);
+        winningNumbers.add(3);
+        winningNumbers.add(4);
+        winningNumbers.add(5);
+        winningNumbers.add(6);
+        Integer bonusNumber = 7;
+
+        Prize prize = loto.checkWinningNumber(winningNumbers , bonusNumber);
+
+        assertThat(prize).isEqualTo(Prize.SIX);
+    }
 }

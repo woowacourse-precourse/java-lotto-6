b96fd387577ec0e1a6624fef7f17383bed1fc51f
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-11-05 19:11:11 +0900

    FEAT(Message) / create

---
+++ src/main/java/lotto/util/Message.java
@@ -0,0 +1,17 @@
+package lotto.util;
+
+public enum Message {
+    GET_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    GET_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
+    TEXT_RESULT("당첨 통계\n---");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

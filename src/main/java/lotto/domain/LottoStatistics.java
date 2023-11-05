65bd7371104ca1fbde261ca33a048bd7930c23b0
Chenowoo <cjsgusdn14@naver.com>
2023-11-06 02:18:26 +0900

    feat(LottoStatistics): 총 수익률을 계산하는 기능 구현

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,7 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
-
+import java.util.Map;
 
 public class LottoStatistics {
 
@@ -26,5 +26,16 @@ public class LottoStatistics {
         return "NoBonus";
     }
 
+    public String calculateTotalProfit(Map<String, Integer> summary, List<String> lottoPlacePrize, int lottoPurchaseAmount) {
+        double profit = 0;
 
+        int lottoPlacePrizeIdx = 0;
+        for (Map.Entry<String, Integer> entry : summary.entrySet()) {
+            int lottoPrize = Integer.parseInt(lottoPlacePrize.get(lottoPlacePrizeIdx));
+            profit += entry.getValue() * lottoPrize;
+            lottoPlacePrizeIdx++;
+        }
+
+        return String.format("%.1f", profit/(lottoPurchaseAmount*1000)*100);
+    }
 }

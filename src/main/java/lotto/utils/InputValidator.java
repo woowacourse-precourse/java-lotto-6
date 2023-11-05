5b148c54e5090e424e5a45fe1b86a1f0936fca82
elyudwo <dudqk9696@naver.com>
2023-11-06 00:43:57 +0900

    [Feat] : 당첨 번호가 숫자가 아닌 경우 예외 처리 기능 구현.

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -1,5 +1,8 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputValidator {
 
     public static void validateAmountNotNumber(String purchaseAmount) {
@@ -7,6 +10,27 @@ public class InputValidator {
             Integer.parseInt(purchaseAmount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] : 로또 구입 금액은 정수입니다.");
+        }
+    }
+
+    public static void validateAnswerNumberIsNumber(String answerNumber) {
+        List<String> answerNumbers = parseAnswerNumberToList(answerNumber);
+        validateNumbersAreInteger(answerNumbers);
+    }
+
+    private static List<String> parseAnswerNumberToList(String answerNumber) {
+        return Arrays.stream(answerNumber.split(","))
+                .filter(s -> !s.isEmpty())
+                .toList();
+    }
+
+    private static void validateNumbersAreInteger(List<String> numbers) {
+        for (String number : numbers) {
+            try {
+                Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] : 당첨 번호는 정수입니다.");
+            }
         }
     }
 }

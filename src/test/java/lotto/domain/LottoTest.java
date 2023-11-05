0014252eb6bf6a2bf1118c312c22794144b10b15
leesh <qwqw8019@naver.com>
2023-11-05 16:58:09 +0900

    test: 로또 번호 범위, 중복 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,20 +6,20 @@ import static org.junit.jupiter.api.Named.named;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lotto.Lotto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@TestInstance(Lifecycle.PER_CLASS)
 public class LottoTest {
     private Lotto testLotto;
 
@@ -34,24 +34,57 @@ public class LottoTest {
         List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
         // When
         testLotto = new Lotto(lottoNumbers);
-        String expectedNumbers = "1,2,3,4,5,6";
+        String expectedNumbers = lottoNumbers.stream()
+                .map(number -> Integer.toString(number))
+                .collect(Collectors.joining(","));
 
         // Then
-        assertThat(testLotto.getNumber()).isEqualTo(expectedNumbers);
+        assertThat(testLotto.getNumberString()).isEqualTo(expectedNumbers);
 
     }
 
-    @ParameterizedTest(name = "번호 {0}개의 로또를 생성하면 예외가 발생한다.")
-    @MethodSource("provideInvalidLottoNumber")
-    void 예외_테스트(List<Integer> lottoNumbers) {
-        assertThatThrownBy(() -> testLotto = new Lotto(lottoNumbers))
-                .isInstanceOf(IllegalArgumentException.class);
+    @Nested
+    class 예외_테스트 {
+        @ParameterizedTest(name = "숫자 {0}을 포함하는 로또를 생성하면 예외가 발생한다.")
+        @MethodSource("provideInvalidRangeLottoNumber")
+        void 숫자_범위_예외(List<Integer> lottoNumbers) {
+            assertThatThrownBy(() -> testLotto = new Lotto(lottoNumbers))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest(name = "번호 {0}개의 로또를 생성하면 예외가 발생한다.")
+        @MethodSource("provideInvalidCountLottoNumber")
+        void 숫자_개수_예외(List<Integer> lottoNumbers) {
+            assertThatThrownBy(() -> testLotto = new Lotto(lottoNumbers))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("2개의 중복된 숫자를 입력하면 예외가 발생한다.")
+        void 중복_숫자_예외() {
+            // Given
+            List<Integer> lottoNumbers = List.of(1, 1, 2, 3, 4, 5);
+            
+            // When
+            assertThatThrownBy(() -> testLotto = new Lotto(lottoNumbers))
+                    // Then
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        private static Stream<Arguments> provideInvalidRangeLottoNumber() {
+            return Stream.of(
+                    arguments(named("0", List.of(0, 2, 3, 4, 5, 6))),
+                    arguments(named("46", List.of(1, 2, 3, 4, 5, 46)))
+            );
+        }
+
+        private static Stream<Arguments> provideInvalidCountLottoNumber() {
+            return Stream.of(
+                    arguments(named("0", List.of())),
+                    arguments(named("7", List.of(1, 2, 3, 4, 5, 6, 7)))
+            );
+        }
+
     }
 
-    private Stream<Arguments> provideInvalidLottoNumber() {
-        return Stream.of(
-                arguments(named("0", List.of())),
-                arguments(named("7", List.of(1, 2, 3, 4, 5, 6, 7)))
-        );
-    }
 }

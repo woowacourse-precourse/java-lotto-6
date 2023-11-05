26aa348ec9d6c8d6a254ebc0fbe2a52ebc7e6715
김승진 <ohksj77@gmail.com>
2023-11-05 18:13:41 +0900

    feat(WinningCombination): 당첨 결과 확인 기능 구현

--- src/main/java/lotto/domain/WinningCombination.java
+++ src/main/java/lotto/domain/WinningCombination.java
@@ -1,12 +1,53 @@
 package lotto.domain;
 
+import lotto.constant.WinningGrade;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public final class WinningCombination {
 
+    private static final int START_INDEX = 0;
     private final WinningNumbers winningNumbers;
     private final BonusNumber bonusNumber;
 
     public WinningCombination(final WinningNumbers winningNumbers, final BonusNumber bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public WinningResult calculateWinningLottos(final Lottos lottos) {
+        final List<WinningGrade> winningGrades = winningNumbers.compare(lottos);
+
+        return IntStream.range(START_INDEX, winningGrades.size())
+                .mapToObj(index -> mapToProperWinningGrade(lottos, index, winningGrades))
+                .collect(
+                        Collectors.collectingAndThen(
+                                Collectors.toMap(
+                                        Function.identity(),
+                                        winningGrade -> numOfValues(winningGrade, winningGrades)),
+                                WinningResult::new));
+    }
+
+    private WinningGrade mapToProperWinningGrade(
+            final Lottos lottos, final int index, final List<WinningGrade> winningGrades) {
+        final WinningGrade winningGrade = winningGrades.get(index);
+        return getProperWinningGrade(lottos, index, winningGrade);
+    }
+
+    private WinningGrade getProperWinningGrade(
+            final Lottos lottos, final int index, final WinningGrade winningGrade) {
+        if (winningGrade.isPossibleSecondWinner()) {
+            return bonusNumber.checkSecondWinning(lottos.getByIndex(index));
+        }
+        return winningGrade;
+    }
+
+    private Integer numOfValues(
+            final WinningGrade winningGrade, final List<WinningGrade> winningGrades) {
+        final long count = winningGrades.stream().filter(winningGrade::equals).count();
+        return Long.valueOf(count).intValue();
     }
 }

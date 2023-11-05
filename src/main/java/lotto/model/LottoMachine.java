3a79a9e8f17f2dbaf416739f28e078392df0c6a2
권장순 <jsoonworld@gmail.com>
2023-11-06 06:49:23 +0900

    feat(LottoMachine) : 로또 번호 발행의 기능 중 일부를 담당하는 LottoMachine#generateLotto 를 구현했고 이 메서드는 각 로또 티켓에 는 중복되지 않는 6개의 숫자(1~45)가 랜덤으로 할당되는 역할을 맡고 있다.

---
+++ src/main/java/lotto/model/LottoMachine.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoMachine {
+
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public Lotto generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
+        return new Lotto(numbers);
+    }
+
+}

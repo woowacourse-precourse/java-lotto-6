89423a93134272489499388e08201a925d0ceaac
Nara Park <skfk6352@naver.com>
2023-11-06 07:07:42 +0900

    <feat>: 로또 숫자와 당첨 숫자를 비교한 결과를 관리하는 도메인 클래스 생성

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lotto.constant.PriceConstant;
+
+public class LottoResult {
+
+    Lotteries lotteries;
+    WinningLotto winningLotto;
+    Map<Integer, Integer> resultMap = new ConcurrentHashMap<>();
+
+    private LottoResult(Lotteries lotteries, WinningLotto winningLotto) {
+        this.lotteries = lotteries;
+        this.winningLotto = winningLotto;
+        this.resultMap = generateResultMap();
+    }
+
+    public static LottoResult of(Lotteries lotteries, WinningLotto winningLotto) {
+        return new LottoResult(lotteries, winningLotto);
+    }
+
+    private Map<Integer, Integer> generateResultMap() {
+        resultMap.put(PriceConstant.FIFTH_PLACE.getCount(), 0);
+        resultMap.put(PriceConstant.FOURTH_PLACE.getCount(), 0);
+        resultMap.put(PriceConstant.THIRD_PLACE.getCount(), 0);
+        resultMap.put(PriceConstant.SECOND_PLACE.getCount(), 0);
+        resultMap.put(PriceConstant.FIRST_PLACE.getCount(), 0);
+
+        return resultMap;
+    }
+}

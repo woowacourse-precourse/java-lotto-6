abe14c0d70a1e8d60f86eaf6d419c5f658765647
kimwanyoung <dhks2869@gmail.com>
2023-11-05 15:20:54 +0900

    feat : Lotto 범위 밖 숫자 검증 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class Lotto {
 
     private static final String NO_DUPLICATE_ERROR_MESSAGE = "중복된 숫자는 입력할 수 없습니다.";
+    private static final String OUT_OF_RANGE_ERROR_MESSAGE = "1 ~ 45사이의 숫자만 입력할 수 있습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOutOfRange(numbers);
         this.numbers = numbers;
     }
 
@@ -32,7 +34,19 @@ public class Lotto {
         }
     }
 
+    private void validateOutOfRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (isOutOfRange(number)) {
+                throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
+            }
+        }
+    }
+
     private boolean hasDuplicateNumber(List<Integer> numbers) {
         return new HashSet<>(numbers).size() != 6;
+    }
+
+    private boolean isOutOfRange(int number) {
+        return 45 < number || number < 1;
     }
 }

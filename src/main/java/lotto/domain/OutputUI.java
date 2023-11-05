4eb66001f6da7dcc60f4f9208e919443c07af6e8
mmalfoy <stpmalfoy@gmail.com>
2023-11-06 02:48:38 +0900

    feat(OutputUI) : 당첨된 로또 갯수 출력기능 구현

--- src/main/java/lotto/domain/OutputUI.java
+++ src/main/java/lotto/domain/OutputUI.java
@@ -1,20 +1,49 @@
 package lotto.domain;
 
-import java.util.Arrays;
-import java.util.List;
+import lotto.Lotto;
+import lotto.data.Rewards;
+
+import java.util.*;
 
 public class OutputUI {
+
+    private Rewards rewards;
 
     public void rate(long winningRate) {
         System.out.println("총 수익률은 " + winningRate + "%입니다.");
     }
 
-    public void purchaseLog(int num, String lotto) {
+    public void purchaseLog(int num, List<Lotto> publishedLottos) {
         System.out.println(num + "개를 구매했습니다.");
-        for (int i = 0; i < num; i++) {
-            System.out.println(lotto);
+        for (Lotto lotto : publishedLottos) {
+            System.out.println(lotto.getNumbers());
         }
 
     }
+
+    public void winnings(HashMap<Rewards, Integer> resultAll) {
+        Rewards[] rewards = Rewards.values();
+        Arrays.sort(rewards, Comparator.comparingInt(Rewards::correctLottos));
+        for (Rewards reward : rewards) {
+            System.out.printf("%s %s - %d개",reward.getNotifyMessege(),moneyEdit(reward)
+                    ,resultAll.get(reward));
+        }
+    }
+
+    public String moneyEdit(Rewards reward) {
+        int money = reward.money();
+        String beforeEdit = Integer.toString(money);
+        String reversedEdit = new StringBuilder(beforeEdit).reverse().toString();
+        StringBuilder formatted = new StringBuilder();
+        for (int i = 0; i < reversedEdit.length(); i++) {
+            if (i > 0 && i % 3 == 0) {
+                formatted.append(",");
+            }
+            formatted.append(reversedEdit.charAt(i));
+        }
+        String result = "("+formatted.reverse().toString()+"원)";
+        return result;
+    }
+
 
 }

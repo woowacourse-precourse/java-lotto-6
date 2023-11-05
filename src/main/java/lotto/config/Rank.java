43f9d74e633bd5db42db78e3727c614cf51f94d6
jinny-l <jinny.l.dev@gmail.com>
2023-11-05 15:59:16 +0900

    ✨feat: 로또 당첨 기준과 상금 정보를 담고 있는 `Rank` 클래스 구현

---
+++ src/main/java/lotto/config/Rank.java
@@ -0,0 +1,31 @@
+package lotto.config;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
+
+    public final int matchedCount;
+    public final boolean matchesBonusNumber;
+    public final int prize;
+
+    Rank(int matchedCount, boolean matchesBonusNumber, int prize) {
+        this.matchedCount = matchedCount;
+        this.matchesBonusNumber = matchesBonusNumber;
+        this.prize = prize;
+    }
+
+    public static Rank of(int matchedCount, boolean matchesBonusNumber) {
+        return Arrays.stream(Rank.values())
+                .filter(rank ->
+                        rank.matchedCount == matchedCount
+                                && rank.matchesBonusNumber == matchesBonusNumber)
+                .findAny()
+                .orElseThrow(); // TODO: 예외 만들기
+    }
+}

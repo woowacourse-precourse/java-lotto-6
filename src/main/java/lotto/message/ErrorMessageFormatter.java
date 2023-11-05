d075cf96cc3c01f6c76bbf944903ac74f7919d36
Jaehoon-So <jhspacelover@naver.com>
2023-11-06 04:12:42 +0900

    feat: 에러 메세지 포매터 정의

---
+++ src/main/java/lotto/message/ErrorMessageFormatter.java
@@ -0,0 +1,15 @@
+package lotto.message;
+
+public class ErrorMessageFormatter {
+
+    public static String ERROR_FLAG = "[ERROR] ";
+
+    public static String makeErrorMessageWith(String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ERROR_FLAG)
+            .append(message);
+
+        return sb.toString();
+    }
+
+}

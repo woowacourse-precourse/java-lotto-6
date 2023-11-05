234351bc9ed4881a87065a40b591c0e33a7dd268
choiejae <ejae8319@gmail.com>
2023-11-05 19:47:40 +0900

    feat(InputView): 입력 기능 구현
    - 구입 금액 입력 받기
    - 당첨 번호 입력 받기
    - 보너스 번호 입력 받기

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_LOTTO_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public String inputPurchaseAmount() {
+        return inputForMessage(PURCHASE_AMOUNT_INPUT_MESSAGE);
+    }
+
+    public String inputWinningLotto() {
+        return inputForMessage(WINNING_LOTTO_INPUT_MESSAGE);
+    }
+
+    public String inputBonusNumber() {
+        return inputForMessage(BONUS_NUMBER_INPUT_MESSAGE);
+    }
+
+    private String inputForMessage(String message) {
+        System.out.println(message);
+
+        return input();
+    }
+
+    private String input() {
+        return Console.readLine();
+    }
+}

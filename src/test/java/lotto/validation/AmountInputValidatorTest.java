7932d63e63b599361f68bef6197185fbedb394ea
acs0209 <annjae4676@naver.com>
2023-11-05 15:46:39 +0900

    test: 구입 금액 입력값에 대한 테스트 추가
    입력값이 양수가 아닐 경우 예외가 발생한다.

--- src/test/java/lotto/validation/AmountInputValidatorTest.java
+++ src/test/java/lotto/validation/AmountInputValidatorTest.java
@@ -1,10 +1,13 @@
 package lotto.validation;
 
 import static lotto.validation.enumType.AmountInput.NUMERIC_FORMAT_MESSAGE;
+import static lotto.validation.enumType.AmountInput.POSITIVE_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AmountInputValidatorTest {
 
@@ -21,5 +24,16 @@ class AmountInputValidatorTest {
         assertThatThrownBy(() -> inputValidation.validateInputNumericFormat(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(NUMERIC_FORMAT_MESSAGE.getValue());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "-400"})
+    @DisplayName("입력값이 양수가 아니면 예외가 발생하는 테스트")
+    void testValidatePositive(String input) {
+        AmountInputValidator inputValidation = new AmountInputValidator();
+
+        assertThatThrownBy(() -> inputValidation.validatePositive(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(POSITIVE_MESSAGE.getValue());
     }
 }

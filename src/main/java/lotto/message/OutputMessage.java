cb57323ac892ed80273b43a0609f2da179383f19
alscjf1329 <alscjf1329@gmail.com>
2023-11-05 16:47:32 +0900

    feat(OutputMessage): printLottos 추가
    lotto들을 출력하는 printLottos 메소드를 추가했습니다.

--- src/main/java/lotto/message/OutputMessage.java
+++ src/main/java/lotto/message/OutputMessage.java
@@ -1,5 +1,8 @@
 package lotto.message;
 
+import java.util.List;
+import lotto.Lotto;
+
 public enum OutputMessage {
     ASK_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     ASK_WINNING_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
@@ -12,5 +15,10 @@ public enum OutputMessage {
 
     public void printMessage() {
         System.out.println(message);
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        lottos.forEach(System.out::println);
     }
 }

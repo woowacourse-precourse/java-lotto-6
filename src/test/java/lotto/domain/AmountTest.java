b51ccf0b7926f47352db5658652b525d2ffa120e
mina <qkralsdk991@naver.com>
2023-11-05 18:38:08 +0900

    Test : 총 구입 금액 입력 기능 테스트

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -1,2 +1,14 @@
-package lotto.domain;public class AmountTest {
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AmountTest {
+
+    @Test
+    public void 도메인생성테스트(){
+        Amount amount = new Amount(10000, 100);
+
+        Assertions.assertThat(amount.getCount()).isEqualTo(100);
+    }
 }

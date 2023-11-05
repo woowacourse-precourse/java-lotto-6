1cc62c3d498f7356296d599b179e5e40ca2a8cba
prao <pch5521@gmail.com>
2023-11-05 15:48:01 +0900

    feat(LottoException): 게임 전역에 사용되는 예외 클래스 생성
    - IllegalArgumentException 상속

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,15 @@
+package lotto.exception;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoException extends IllegalArgumentException{
+    private LottoException(ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+    }
+
+    public static LottoException of(ErrorMessage errorMessage) {
+        Console.close();
+        return new LottoException(errorMessage);
+    }
+}

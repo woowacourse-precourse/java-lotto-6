adc3268c1c2043043f192c4681885d873a71eab8
김민정 <mjeong@sookmyung.ac.kr>
2023-11-05 15:11:08 +0900

    feat: 보너스 번호를 검증하는 기능 구현

--- src/main/java/lotto/WinningAndBonusNumberValidator.java
+++ src/main/java/lotto/WinningAndBonusNumberValidator.java
@@ -13,6 +13,12 @@ public class WinningAndBonusNumberValidator {
         return new Lotto(winningNumber);
     }
 
+    public int validateBonusNumber(List<Integer> winningNumber, int bonusNumber) {
+        validateNumberRange(bonusNumber);
+        validateDuplicate(winningNumber, bonusNumber);
+        return bonusNumber;
+    }
+
     private void validateNumberRange(int number) {
         if (number < SortedRandomNumberGenerator.START_RANGE || number > SortedRandomNumberGenerator.END_RANGE) {
             throw new IllegalArgumentException();
@@ -22,6 +28,12 @@ public class WinningAndBonusNumberValidator {
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() < numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers, int number) {
+        if (numbers.contains(number)) {
             throw new IllegalArgumentException();
         }
     }

7e25b34a9b2a5b7cdc234af9e0e5d338850a5583
seminchoi <choicco89@gmail.com>
2023-11-05 22:02:38 +0900

    feat(service.LottoService) : 지불 금액을 통해 자동 랜덤 로또 번호를 생성하고 영수증을 반환하는 기능 구현

--- src/main/java/lotto/dto/LottoReceiptDto.java
+++ src/main/java/lotto/dto/LottoReceiptDto.java
@@ -1,6 +1,12 @@
 package lotto.dto;
 
+import lotto.domain.Lotto;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record LottoReceiptDto(int purchaseCount, List<LottoDto> lottos) {
+    public static LottoReceiptDto from(int purchaseCount, List<Lotto> lottos) {
+        List<LottoDto> lottoDtos = lottos.stream().map(LottoDto::of).toList();
+        return new LottoReceiptDto(purchaseCount, lottoDtos);
+    }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,24 +1,47 @@
 package lotto.service;
 
+import lotto.constant.LottoConstraint;
 import lotto.domain.Lotto;
+import lotto.domain.Payment;
 import lotto.domain.WinningLotto;
 import lotto.dto.LottoReceiptDto;
 import lotto.dto.LottoResultDto;
 import lotto.random.RandomNumberGenerator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
     private final RandomNumberGenerator randomNumberGenerator;
+    private final List<Lotto> lottos = new ArrayList<>();
 
-    private List<Lotto> lottos;
     private WinningLotto winningLotto;
 
     public LottoService(RandomNumberGenerator randomNumberGenerator) {
         this.randomNumberGenerator = randomNumberGenerator;
     }
 
-    public LottoReceiptDto getLottoReceipt(int payment) {
-        return new LottoReceiptDto();
+    public LottoReceiptDto getLottoReceipt(int amount) {
+        int purchaseLottoCount = getPurchaseLottoCount(amount);
+        purchaseLottos(purchaseLottoCount);
+        return LottoReceiptDto.from(purchaseLottoCount, lottos);
+    }
+
+    private int getPurchaseLottoCount(int amount) {
+        Payment payment = new Payment(amount);
+        return payment.calculatePurchaseLottoCount();
+    }
+
+    private void purchaseLottos(int purchaseLottoCount) {
+        for (int count = 0; count < purchaseLottoCount; count++) {
+            List<Integer> generatedNumbers = generateLottoNumbers();
+            lottos.add(new Lotto(generatedNumbers));
+        }
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        return randomNumberGenerator.pickUniqueNumbersInRange(
+                LottoConstraint.MIN_LOTTO_NUMBER.getValue(), LottoConstraint.MAX_LOTTO_NUMBER.getValue()
+        );
     }
 
     public void generateWinningLottoWithoutBonusNumber(List<Integer> winningLottoNumbers) {

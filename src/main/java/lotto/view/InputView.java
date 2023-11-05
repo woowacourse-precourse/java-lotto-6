05d87dad8782a9342f42c27f2f38ccf567e8ca27
김관진 <cozyschooll@gmail.com>
2023-11-05 16:47:04 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
     public Integer inputPurchasePrice() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> inputWinNumbers() {
+        return new ArrayList<>(Arrays.stream(Console.readLine().split(","))
+                .map(Integer::valueOf)
+                .toList());
     }
 }

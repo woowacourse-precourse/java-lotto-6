15f413f55544f0b8de18bd32cf2ba36828210763
opixxx <opix0306@naver.com>
2023-11-05 09:14:44 +0900

    feat(LottoController): 로또 게임 초기 기능 구현
    구입 금액을 입력받고, 발행된 로또 리스트 출력하는 기능 구현함

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,30 @@
+package lotto.controller;
+
+import static lotto.view.OutputView.printLottoQuantity;
+
+import java.util.List;
+import lotto.domain.GenerateLotto;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.view.InputView;
+
+public class LottoController {
+    private final InputView inputView;
+    public LottoController(InputView inputView) {
+        this.inputView = inputView;
+    }
+    public void play() {
+        Money money = inputView.inputMoney();
+        printLottoQuantity(money);
+
+        GenerateLotto generateLotto = new GenerateLotto();
+        int count = money.getLottoQuantity();
+        List<Lotto> lotto = generateLotto.getLottoNumbers(count);
+
+        Lottos lottos = new Lottos(lotto);
+
+    }
+
+
+}

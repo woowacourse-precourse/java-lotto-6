5bd59bf64b0c94c36311e93bafb2c1b1fc737bed
khabh1 <kshababe1@gmail.com>
2023-11-05 15:25:56 +0900

    feat(LottoMachine): 스켈레톤 코드 및 기능 테스트 작성

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,7 @@
+package lotto.domain;
+
+import java.util.List;
+
+public interface LottoMachine {
+    List<Lotto> generateLottos(int lottoCount);
+}
---
+++ src/main/java/lotto/domain/RandomLottoMachine.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class RandomLottoMachine implements LottoMachine {
+
+    @Override
+    public List<Lotto> generateLottos(int lottoCount) {
+        return null;
+    }
+}

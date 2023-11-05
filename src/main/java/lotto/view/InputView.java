a653f2e3546f835ccf9735800fe35181f655a37a
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 18:17:30 +0900

    Feat(InputView): receivePurchaseAmountMsg 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,11 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String receivePurchaseAmountMsg(){
+        OutputView.printInputPurchaseAmountMsg();
+        return Console.readLine();
+    }
 }

a61010901cafa8d53b20f2d3a88920b4e5d382ca
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 01:11:50 +0900

    test(lottoMachine): 구입 금액에 맞게 로또 수량이 결정되는가 테스트

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class LottoMachineTest {
+    private LottoMachine lottoMachine = new LottoMachine();
+
+    @Test
+    void 구입_금액에_맞게_로또_수량이_결정되는가() {
+        int lottoPrice = 1_000;
+        int lottoQuantity = 10;
+
+        assertThat(
+                lottoMachine.calculateLottoQuantity(lottoPrice * lottoQuantity))
+                .isEqualTo(lottoQuantity);
+    }
+}

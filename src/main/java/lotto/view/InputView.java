dca075246e990218b3bd8fef1b2e2232ae383299
siho-song <thdtlgh234@naver.com>
2023-11-05 17:06:13 +0900

    [Refactor] 함수 이동 및 변수명 변경
    1. 보너스넘버 클래스를 삭제 함으로써 , 보너스 넘버 클래스에서 수행했던 검증을 InputView에서 실행

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.ErrorMessage;
+import lotto.domain.LottoNumberRange;
 import lotto.utils.Utils;
 
 public class InputView {
-    private final static String GET_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private final static String GET_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String GET_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String GET_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
@@ -13,7 +15,7 @@ public class InputView {
     }
 
     public static int inputAmount() {
-        showMessage(GET_MONEY_MESSAGE);
+        showMessage(GET_AMOUNT_MESSAGE);
         return Utils.convertStringToInteger(Console.readLine());
     }
 
@@ -24,6 +26,18 @@ public class InputView {
 
     public static int inputBonusNumber() {
         showMessage(GET_BONUS_NUMBER_MESSAGE);
-        return Utils.convertStringToInteger(Console.readLine());
+        int bonusNumber = Utils.convertStringToInteger(Console.readLine());
+        validateNumberRange(bonusNumber);
+        return bonusNumber;
+    }
+
+    public static void validateNumberRange(int number) {
+        boolean isValidRange = (number >= LottoNumberRange.MIN.getValue() && number <= LottoNumberRange.MAX.getValue());
+
+        if (!isValidRange) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.INVALID_NUMBER_RANGE.getValue()
+            );
+        }
     }
 }

29a586c7c53404feffb408ab4af473782dc9da26
YoungJun-L <dudwns328@naver.com>
2023-11-05 15:40:13 +0900

    refactor: 테스트 이름을 도메인 중심으로 변경

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
-    @DisplayName("금액의 범위가 맞고 1000원 단위일 경우 Money를 성공적으로 생성한다.")
+    @DisplayName("구입 금액의 범위가 맞고 1,000 단위일 경우 성공적으로 생성한다.")
     @Test
     void createMoney() {
         Long value = 1_000L;
@@ -17,7 +17,7 @@ class MoneyTest {
         assertDoesNotThrow(() -> new Money(value));
     }
 
-    @DisplayName("금액이 1,000 미만 100,000,000,000 초과일 경우 Money를 생성할 때 예외가 발생한다.")
+    @DisplayName("구입 금액이 1,000 미만이거나 100,000,000,000 초과일 경우 Money를 생성할 때 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(longs = {999L, 100_000_000_001L})
     void createMoneyByInvalidRange(Long input) {
@@ -25,7 +25,7 @@ class MoneyTest {
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("금액이 1000원 단위가 아닌 경우 Money를 생성할 때 예외가 발생한다.")
+    @DisplayName("구입 금액이 1,000 단위가 아닐 경우 Money를 생성할 때 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(longs = {1_100L, 10_000_000_001L})
     void createMoneyByInvalidUnit(Long input) {

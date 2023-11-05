d5a3c0f91b472a491ede8fa3cae71f2f16842e6a
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-11-05 18:11:22 +0900

    test: 랭크 카운트 추가 기능

---
+++ src/test/java/lotto/RankCounterTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import lotto.model.Rank;
+import lotto.model.RankCounter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RankCounterTest {
+
+    @DisplayName("랭크 카운트를 추가한다.")
+    @Test
+    void 랭크_카운트_추가() {
+        // given
+        RankCounter rankCounter = RankCounter.create();
+        Rank rank = Rank.FIRST;
+
+        // when
+        rankCounter.addRank(rank);
+
+        // then
+        assertEquals(1, rankCounter.getRankCount(rank));
+    }
+}

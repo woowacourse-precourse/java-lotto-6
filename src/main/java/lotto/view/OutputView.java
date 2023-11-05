0acf86198f240873dfce157eb9d2649568b27eeb
jun <ggprgrkjh@naver.com>
2023-11-05 23:54:26 +0900

    feat(outputview): 당첨 결과 출력 기능 with TDD

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -26,4 +26,14 @@ public class OutputView {
     public static void printRequestBonusNumber() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
+
+    public static void printWinningResult(int[] result) {
+
+        System.out.println(3 + "개 일치 (5,000원) - " + result[1] + "개");
+        System.out.println(4 + "개 일치 (50,000원) - " + result[2] + "개");
+        System.out.println(5 + "개 일치 (1,500,000원) - " + result[3] + "개");
+        System.out.println(5 + "개 일치, 보너스 볼 일치 (30,000,000원) - " + result[4] + "개");
+        System.out.println(6 + "개 일치 (2,000,000,000원) - " + result[5] + "개");
+
+    }
 }
--- src/test/java/lotto/OutputViewTest.java
+++ src/test/java/lotto/OutputViewTest.java
@@ -71,14 +71,24 @@ public class OutputViewTest {
 
     @DisplayName("보너스 번호 입력 요청 출력 테스트")
     @Test
-    void printRequestBonusNumber_EqualMessage_Success(){
+    void printRequestBonusNumber_EqualMessage_Success() {
         ByteArrayOutputStream output = captureOutputValues();
         OutputView.printRequestBonusNumber();
         assertThat(output.toString()).isEqualTo("\n보너스 번호를 입력해 주세요.\n");
     }
 
-
-
+    @DisplayName("당첨 결과 출력 테스트")
+    @Test
+    void printWinningResult_EqaulMessage_Success() {
+        ByteArrayOutputStream output = captureOutputValues();
+        int[] result = {0, 1, 2, 3, 4, 5};
+        OutputView.printWinningResult(result);
+        assertThat(output.toString()).isEqualTo("3개 일치 (5,000원) - 1개\n"
+                + "4개 일치 (50,000원) - 2개\n"
+                + "5개 일치 (1,500,000원) - 3개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - 4개\n"
+                + "6개 일치 (2,000,000,000원) - 5개\n");
+    }
 
 
 }

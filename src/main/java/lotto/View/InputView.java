cb520f9086078e8217a3cf10b77b62c6db7a6ab5
m2nseop <pms246@naver.com>
2023-11-06 03:07:39 +0900

    feat: 예외처리 후 다시 입력받기 기능 구현

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,5 +1,7 @@
 package lotto.View;
 
+import static lotto.Util.InputValidator.isNumber;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -8,12 +10,25 @@ public class InputView {
     private static final String INPUT_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String inputLottoPurchaseAmount() {
-        System.out.println(INPUT_LOTTO_PUCAHSE_AMOUNT_MESSAGE);
-        return Console.readLine();
+//        System.out.println(INPUT_LOTTO_PUCAHSE_AMOUNT_MESSAGE);
+        boolean validInput = false;
+        String input="";
+        while (!validInput) {
+            try {
+                System.out.println(INPUT_LOTTO_PUCAHSE_AMOUNT_MESSAGE);
+                input = Console.readLine();
+                validInput = isNumber(input);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return input;
     }
 
     public static String inputLottoWinningNumber(){
         System.out.println("\n" + INPUT_LOTTO_WINNING_NUMBER_MESSAGE);
+
         return Console.readLine();
     }
 

402bb9cdd90a0fce0edd0fcffa08a6e18aa83f24
박승찬 <seungchan141414@gmail.com>
2023-11-05 21:19:52 +0900

    [Feat(lotto)] Utill 문자 입력 기능 추가.

--- src/main/java/lotto/utill/Utii.java
+++ src/main/java/lotto/utill/Utii.java
@@ -1,7 +1,15 @@
 package lotto.utill;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Utii {
     public static boolean isSameInt(Integer num_1, Integer num_2) {
         return num_2 == num_1;
+    }
+
+    public static String InputStringLine() {
+        String inputStringLine = Console.readLine();
+
+        return inputStringLine;
     }
 }

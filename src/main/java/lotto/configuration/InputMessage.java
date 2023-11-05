24101024b81d7518825ef05d57c25f9ed8916cb4
Junobee25 <wnsdhqo@naver.com>
2023-11-06 02:50:29 +0900

    refactor(InputMessage): 구입금액 메세지 추가

--- src/main/java/lotto/configuration/InputMessage.java
+++ src/main/java/lotto/configuration/InputMessage.java
@@ -1,9 +1,9 @@
 package lotto.configuration;
 
 public enum InputMessage {
-    BUY_LOTTO_COUNT("개를 구매했습니다."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요");
 
     private String message;
 

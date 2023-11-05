7cd056626efc7089af48da73bde0d27e8f7feffa
IE-MangChi <hsm983278@gmail.com>
2023-11-05 21:58:51 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는 기능, 당첨 통계를 계산하여 넘기는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,13 @@
 package lotto.controller;
 
 import lotto.dto.PurchaseResult;
-import lotto.model.Lottos;
-import lotto.model.Purchase;
+import lotto.dto.WinningResult;
+import lotto.model.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
@@ -18,6 +21,8 @@ public class LottoController {
 
     public void run() {
         Purchase purchase = lottoPurchaeByMoney(Integer.parseInt(inputView.inputMoney()));
+        WinningNumbers winningNumbers = createWinningNumbers();
+        LottResult(purchase,winningNumbers);
     }
 
     private Purchase lottoPurchaeByMoney(Integer money) {
@@ -29,4 +34,21 @@ public class LottoController {
         return purchase;
     }
 
+    private WinningNumbers createWinningNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        String[] split = inputView.inputWinningNumbers().split(",");
+        for (String s : split) {
+            numbers.add(Integer.parseInt(s));
+        }
+        int i = Integer.parseInt(inputView.inputBonusNumber());
+        WinningNumbers winningNumbers = new WinningNumbers(new Lotto(numbers), i);
+        return winningNumbers;
+    }
+
+    private void LottResult(Purchase purchase, WinningNumbers winningNumbers) {
+        LottoResult lottoResult = new LottoResult(purchase, winningNumbers);
+        float rateResult = ((float) lottoResult.calculatePriceSum() * 100) / purchase.getMoney();
+        WinningResult winningResult = new WinningResult(lottoResult.calculateRank(), rateResult);
+        outputView.outputWinningResult(winningResult);
+    }
 }

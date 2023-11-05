c907712953c876ecd939d3cf4b2319b63e93acbb
baksakcci <pshsh910@gmail.com>
2023-11-05 22:00:42 +0900

    refactor: 메서드 나누기

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,16 +9,29 @@ public class OutputView {
     private static final String QUANTITY_OUTPUT_MESSAGE = "개를 구매했습니다.";
 
     public void printPurchaseHistory(PurchaseHistoryDto purchaseHistoryDto) {
-        System.out.println(purchaseHistoryDto.getCount() + QUANTITY_OUTPUT_MESSAGE);
+        printCount(purchaseHistoryDto);
         List<List<Integer>> lottoNumbers = purchaseHistoryDto.getLottoNumbers();
         for (List<Integer> lottoNumber : lottoNumbers) {
-            System.out.print("[");
-            String s = lottoNumber.stream()
-                    .map(String::valueOf)
-                    .collect(Collectors.joining(", "));
-            System.out.print(s);
-            System.out.print("]");
-            System.out.println();
+            printLottoNumberHistory(lottoNumber);
         }
+    }
+
+    private void printCount(PurchaseHistoryDto purchaseHistoryDto) {
+        System.out.println(purchaseHistoryDto.getCount() + QUANTITY_OUTPUT_MESSAGE);
+    }
+
+    private void printLottoNumberHistory(List<Integer> lottoNumber) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        builder.append(getLottoNumber(lottoNumber));
+        builder.append("]");
+        builder.append(System.lineSeparator());
+        System.out.println(builder);
+    }
+
+    private String getLottoNumber(List<Integer> lottoNumber) {
+        return lottoNumber.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
     }
 }

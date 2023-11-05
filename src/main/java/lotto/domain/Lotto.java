bb307e3f135369811d8531be0e68f0b9d69f939e
Woongbae Yun <devbelly@naver.com>
2023-11-05 15:56:43 +0900

    refactor : 하드코딩된 값을 상수로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -16,7 +16,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }

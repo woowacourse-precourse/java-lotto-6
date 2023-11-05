133f127f18dd696f335a92208e4886aff0f3c962
정영록 <s9285s@naver.com>
2023-11-05 18:43:41 +0900

    feat : 랜덤 숫자 오름차순 정렬

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -2,12 +2,16 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Utils {
 
     public List<Integer> generateRandomNumbers() {
 
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(randomNumbers);
+
+        return randomNumbers;
     }
 }

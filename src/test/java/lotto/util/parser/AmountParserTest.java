dcc67899df23ae563df88281f1486686cd43a65b
hyunjin <guswlsdl0121@gmail.com>
2023-11-05 23:50:56 +0900

    test: 로또 구입 금액 입력 예외 케이스 단위 테스트 작성

---
+++ src/test/java/lotto/util/parser/AmountParserTest.java
@@ -0,0 +1,40 @@
+package lotto.util.parser;
+
+import lotto.constant.InputMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AmountParserTest {
+
+    private final InputParser<Integer> parser = new AmountParser();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "2000", "3000"})
+    @DisplayName("입력값이 비어 있지 않고 정수로 변환될 때 올바른 값을 반환해야 한다.")
+    void testParseValidInput(String input) {
+        int result = parser.parse(input);
+        assertEquals(Integer.parseInt(input), result);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   "})
+    @DisplayName("입력값이 null이거나 빈 공간일 때 IllegalArgumentException을 던져야 한다.")
+    void testParseInvalidInput(String input) {
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(input),
+                InputMessage.ERROR_AMOUNT_IS_BLANK);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "1000abc", "xyz123"})
+    @DisplayName("입력값이 숫자가 아닐 때 IllegalArgumentException을 던져야 한다.")
+    void testParseNonNumericInput(String input) {
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(input),
+                InputMessage.ERROR_AMOUNT_NOT_A_NUMBER);
+    }
+}
---
+++ src/test/java/lotto/util/validator/AmountValidatorTest.java
@@ -0,0 +1,29 @@
+package lotto.util.validator;
+
+import lotto.constant.InputMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AmountValidatorTest {
+
+    private final InputValidator<Integer> validator = new AmountValidator();
+
+    @ParameterizedTest
+    @ValueSource(ints = {500, 200, -1000})
+    @DisplayName("금액이 로또 가격 미만일 때 IllegalArgumentException을 던져야 한다.")
+    void testValidateAmountLessThanLottoPrice(int amount) {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(amount),
+                InputMessage.ERROR_AMOUNT_SMALL_THAN_THOUSAND);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1500, 2500, 11001})
+    @DisplayName("금액이 1000원 단위가 아닐 때 IllegalArgumentException을 던져야 한다.")
+    void testValidateAmountNotMultipleOfThousand(int amount) {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(amount),
+                InputMessage.ERROR_AMOUNT_NOT_MULTIPLE_OF_THOUSAND);
+    }
+}

7edc231c46612feaf0119a659eb582c403cf9049
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-05 18:30:40 +0900

    feat(Lottos): 로또 번호를 생성하는 기능 추가
    docs: Lottos 기능 목록 체크

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,12 +1,21 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.constant.GameConfig;
 
 public class Lottos {
 
     private final List<Lotto> lottos = new ArrayList<>();
 
     public Lottos(PurchaseAmount purchaseAmount) {
+    }
+
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                GameConfig.MIN_LOTTO_NUMBER.getValue(),
+                GameConfig.MAX_LOTTO_NUMBER.getValue(),
+                GameConfig.WINNING_NUMBERS_COUNT.getValue());
     }
 }

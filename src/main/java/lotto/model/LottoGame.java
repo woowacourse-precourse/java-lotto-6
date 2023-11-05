1481aeb55c30e6d89ce9f7b5c8de29ed9728ba6d
WooYoungDoo <duden5000@naver.com>
2023-11-05 13:07:43 +0900

    feat (LottoGame) : 로또 게임 모델 생성(로또 개수 및 수익률 담당)

---
+++ src/main/java/lotto/model/LottoGame.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+public class LottoGame {
+
+    private int lottoCount;
+    private float profitRate;
+
+    public int getLottoCount() {
+        return lottoCount;
+    }
+
+    public float getProfitRate() {
+        return profitRate;
+    }
+
+    public void setLottoCount(int lottoCount) {
+        this.lottoCount = lottoCount;
+    }
+
+    public void setProfitRate(float profitRate) {
+        this.profitRate = profitRate;
+    }
+}

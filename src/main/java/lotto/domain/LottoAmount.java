7ec246aef016364bbebedc64271c42d7cc8c6156
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 03:06:49 +0900

    feat: 금액이 1000원 단위가 아닐 경우 에외를 발생하는 기능 구현

--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static lotto.domain.LottoConfig.LOTTO_PRICE;
+import static lotto.enums.ErrorMassage.INCORRECT_AMOUNT_UNIT;
 import static lotto.enums.ErrorMassage.NOT_ENOUGH_AMOUNT;
 
 public class LottoAmount {
@@ -8,12 +9,19 @@ public class LottoAmount {
 
     public LottoAmount(int amount) {
         validateLottoPrice(amount);
+        validateAmountUnit(amount);
         this.amount = amount;
     }
 
     private void validateLottoPrice(int amount) {
         if (amount < LOTTO_PRICE.getValue()) {
             throw new IllegalArgumentException(NOT_ENOUGH_AMOUNT.getMassage());
+        }
+    }
+
+    private void validateAmountUnit(int amount) {
+        if (amount % LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException(INCORRECT_AMOUNT_UNIT.getMassage());
         }
     }
 }

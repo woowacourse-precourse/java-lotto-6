98f0d5c7a30ed7b116a0d92153f3cdd9ce72b168
chanhoim <cksgh201@naver.com>
2023-11-05 21:51:47 +0900

    fix(lotto): 로또 번호가 오름차순으로 정렬되게  수정

--- src/main/java/lotto/domain/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoNumberGenerator.java
@@ -12,6 +12,8 @@ public class LottoNumberGenerator implements NumberGenerator {
     @Override
     public List<Integer> generate() {
         return Randoms.pickUniqueNumbersInRange(MINIMUM_RANGE.getValue(),
-                MAXIMUM_RANGE.getValue(), NUMBER_COUNT.getValue());
+                MAXIMUM_RANGE.getValue(), NUMBER_COUNT.getValue()).stream()
+                .sorted()
+                .toList();
     }
 }

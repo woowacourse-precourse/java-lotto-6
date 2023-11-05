72bd6006d3886d0ddd078527786ad94e38ec2f67
chae401 <chae401@sju.ac.kr>
2023-11-05 17:58:13 +0900

    test(RankingTest): Ranking에 대한 테스트 코드

--- src/test/java/lotto/model/LottoServiceTest.java
+++ src/test/java/lotto/model/LottoServiceTest.java
@@ -1,4 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.model;
+
+import lotto.model.dto.PayDTO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 class LottoServiceTest {
-  
+    LottoService lottoService;
+
 }

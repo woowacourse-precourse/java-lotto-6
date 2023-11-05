b64017a3dc1217de575a45a2920040d510f3b2a0
노정완 <jeongwan.noh@gmail.com>
2023-11-06 03:05:18 +0900

    feat: 중복된 숫자 검증 메서드 구현

--- src/main/java/lotto/domain/lottery/Lotto.java
+++ src/main/java/lotto/domain/lottery/Lotto.java
@@ -1,17 +1,26 @@
 package lotto.domain.lottery;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicates(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }

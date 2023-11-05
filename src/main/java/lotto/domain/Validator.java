ec8afa5101908c574d92ca2bfef8ee7b9d00ac36
dj <fjzl12@naver.com>
2023-11-05 22:06:45 +0900

    refactor(Validator): 로또 구입 금액이 올바른지 검증만 하도록 수정

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -1,15 +1,12 @@
 package lotto.domain;
 
-import lotto.ui.InputView;
-
 public class Validator {
     private int cost;
 
-    public int validateCost(String cost) {
+    public void validateCost(String cost) {
         parseToInt(cost);
         isPositive();
         isPerThousand();
-        return this.cost / 1000;
     }
 
     private void parseToInt(String cost) {
@@ -17,8 +14,8 @@ public class Validator {
             this.cost = Integer.parseInt(cost);
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println("구입 금액 숫자로 해야하는 에러");
+            throw new IllegalArgumentException();
         }
-        InputView.readCost();
     }
 
     private void isPositive() {
@@ -28,8 +25,8 @@ public class Validator {
             }
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println("음수이거나 값이 너무 작은 문제");
+            throw new IllegalArgumentException();
         }
-        InputView.readCost();
     }
 
     private void isPerThousand() {
@@ -39,8 +36,8 @@ public class Validator {
             }
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println("천원 단위로 안나눠지는 에러");
+            throw new IllegalArgumentException();
         }
-        InputView.readCost();
     }
 }
 

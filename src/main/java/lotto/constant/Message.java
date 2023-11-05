86a6e00cebb99b3119c38bacb9419acc3b4f62ef
Cycrypto <jh01love@naver.com>
2023-11-05 21:33:36 +0900

    feat:메시지 관리용 열거형 클래스 생성

--- src/main/java/lotto/constant/Message.java
+++ src/main/java/lotto/constant/Message.java
@@ -1,5 +1,17 @@
 package lotto.constant;
 
 public enum Message {
+    INPUT_BUDGET("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBER("당첨번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    PURCHASED_LOTTO_COUNT_MESSAGE("개를 구매했습니다.")
+    ;
 
+    private final String message;
+    Message(String s) {
+        this.message = s;
+    }
+    public String getMessage() {
+        return message;
+    }
 }

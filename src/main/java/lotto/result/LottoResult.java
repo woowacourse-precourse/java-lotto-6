c14a58b0b1592365d367069ce7f9817560089478
leechiyun <wychis28@gmail.com>
2023-11-05 14:52:04 +0900

    feat: 당첨 통계 출력 기능 구현 - LottoResult

--- src/main/java/lotto/result/LottoResult.java
+++ src/main/java/lotto/result/LottoResult.java
@@ -2,13 +2,15 @@ package lotto.result;
 
 public class LottoResult {
     private LottoResultType lottoResultType;
+    private int money;
 
-    public LottoResult(LottoResultType lottoResultType) {
+    public LottoResult(int money, LottoResultType lottoResultType) {
+        this.money = money;
         this.lottoResultType = lottoResultType;
     }
 
     // 수익률 계산
-    public double calculateRateOfReturn(int money) {
+    public double calculateRateOfReturn() {
         double rateOfReturn = 0;
 
         Long sum = 0L;
@@ -19,5 +21,21 @@ public class LottoResult {
         rateOfReturn = sum / money;
 
         return rateOfReturn;
+    }
+
+    // 당첨 통계 출력
+    public void printWinnigStatistics() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("당첨 통계").append("\n");
+        sb.append("---").append("\n");
+
+        for (LottoResultType type: LottoResultType.values()) {
+            type.print();
+        }
+
+        sb.append("총 수익률은 ").append(String.format("%.2f", calculateRateOfReturn())).append("%입니다.");
+
+        System.out.println(sb);
     }
 }

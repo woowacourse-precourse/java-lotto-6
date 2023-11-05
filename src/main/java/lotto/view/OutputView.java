f710113bb31908aa6065cf8cb16c4351dc0d0be0
Yang-soeun <soeun8636@naver.com>
2023-11-05 20:37:14 +0900

    [feat]: 총 수익률을 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,18 @@
 package lotto.view;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import lotto.constant.PrizeConstant;
 
 public class OutputView {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String STATISTIC_MESSAGE = "당첨 통계";
+    private static final String PROFITABILITY_MESSAGE = "총 수익률은 ";
     private static final String NEW_LINE = "\n";
     private static final String SPLIT_LINE = "---";
     private static final String PER = "개";
+    private static final String PERCENT = "%입니다.";
     private static final int START = 3;
 
     public void printLotto(int quantity, List<List<Integer>> myLottoNumbers) {
@@ -32,5 +36,9 @@ public class OutputView {
         }
 
         System.out.println(sb);
+    }
+
+    public void printProfitability(float profitability){
+        System.out.println(PROFITABILITY_MESSAGE + String.format("%.1f", profitability) + PERCENT);
     }
 }

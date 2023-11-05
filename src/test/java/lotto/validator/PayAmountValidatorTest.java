d8f2428d51cce3999875f9ec005e71a0d303ef66
liarreez <liarreez@gmail.com>
2023-11-05 15:30:57 +0900

    test : 20억 이상의 값 입력 시 예외가 발생하는 테스트 작성
    비정상적으로 많은 구매를 막기 위해 20억 이상 구매 시 예외를 처리하는 테스트 작성

--- src/test/java/lotto/validator/PayAmountValidatorTest.java
+++ src/test/java/lotto/validator/PayAmountValidatorTest.java
@@ -15,7 +15,7 @@ public class PayAmountValidatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {" ", "   ", "\t", "\n"})
-    void testWhenPurchaseAmountIsNullOrBlank(String payAmount) {
+    void testWhenPayAmountIsNullOrBlank(String payAmount) {
         IllegalArgumentException payAmountException =
                 Assertions.assertThrows(IllegalArgumentException.class,
                         () -> payAmountValidator.validate(payAmount));
@@ -25,7 +25,17 @@ public class PayAmountValidatorTest {
     @DisplayName("구매 금액으로 숫자가 아닌 값 입력 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"오천원", "five", "$", "5$"})
-    void testWhenPurchaseAmountIsNotNumeric(String payAmount) {
+    void testWhenPayAmountIsNotNumeric(String payAmount) {
+        IllegalArgumentException payAmountException =
+                Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> payAmountValidator.validate(payAmount));
+        assertThat(payAmountException.getMessage()).contains("[ERROR]");
+    }
+
+    @DisplayName("구매 금액으로 10자리를 초과하는 값 입력 시 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000000000000000000000000000000000000000000000","1234567891234567989000"})
+    void testWhenPayAmountDigitOverTen(String payAmount) {
         IllegalArgumentException payAmountException =
                 Assertions.assertThrows(IllegalArgumentException.class,
                         () -> payAmountValidator.validate(payAmount));
@@ -35,7 +45,17 @@ public class PayAmountValidatorTest {
     @DisplayName("구매 금액으로 1,000원으로 나누어 떨어지지 않는 값 입력 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"0", "500", "1300", "39900", "123456789"})
-    void testWhenPurchaseAmountCannotDivideOneThousand(String payAmount) {
+    void testWhenPayAmountCannotDivideOneThousand(String payAmount) {
+        IllegalArgumentException payAmountException =
+                Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> payAmountValidator.validate(payAmount));
+        assertThat(payAmountException.getMessage()).contains("[ERROR]");
+    }
+
+    @DisplayName("구매 금액으로 20억을 넘는 값 입력 시 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"2000001000","5000000000"})
+    void testWhenPayAmountIsOverTwoBillion(String payAmount) {
         IllegalArgumentException payAmountException =
                 Assertions.assertThrows(IllegalArgumentException.class,
                         () -> payAmountValidator.validate(payAmount));

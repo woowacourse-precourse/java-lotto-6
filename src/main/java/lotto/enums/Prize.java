c9e6477526cc396eda219db615c8026c05c6022f
insu <is23193160@gmail.com>
2023-11-05 18:51:25 +0900

    feat: 1~6등까지 상금과 숫자 갯수를 선언해놓은 열거형 추가

---
+++ src/main/java/lotto/enums/Prize.java
@@ -0,0 +1,18 @@
+package lotto.enums;
+
+public enum Prize {
+    FIRST(6,2000000000L),
+    SECOND(5,30000000L),
+    THIRD(5,1500000L),
+    FOURTH(4,50000L),
+    FIFTH(3,5000L),
+    SIXTH(0,0L);
+    private final int collectCount;
+    private final Long prize;
+
+    Prize(int collectCount, long prize) {
+        this.collectCount = collectCount;
+        this.prize = prize;
+    }
+
+}

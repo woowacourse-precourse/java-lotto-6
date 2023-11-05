759958aabc5880569d1a6d79113e048659715cbf
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-05 22:07:52 +0900

    feat: 로또 번호, 보너스 번호를 입력받기 위한 View 생성

---
+++ src/main/java/lotto/view/input/LottoInputView.java
@@ -0,0 +1,11 @@
+package lotto.view.input;
+
+public class LottoInputView {
+    public void printInputWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printInputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}

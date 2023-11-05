f4c1fd23b476c265d6be7378f05b119dd6bb4b18
박승찬 <seungchan141414@gmail.com>
2023-11-05 12:30:31 +0900

    [Feat(lotto)] 로또 봉투의 정보를 주는 기능 추가.

--- src/main/java/lotto/domain/lotto/LottoEnvelop.java
+++ src/main/java/lotto/domain/lotto/LottoEnvelop.java
@@ -1,7 +1,5 @@
 package lotto.domain.lotto;
 
-import lotto.domain.lotto.Lotto;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,5 +16,20 @@ public class LottoEnvelop {
 
     public Integer size() {
         return lottoEnvelops.size();
+    }
+
+    public StringBuilder giveInformation() {
+        int sizeLottoEnvelop = lottoEnvelops.size();
+        StringBuilder information = new StringBuilder();
+        Lotto lotto = null;
+        StringBuilder lottoInformation = null;
+
+        for (int i = 0; i < sizeLottoEnvelop; i++) {
+            lotto = lottoEnvelops.get(i);
+            lottoInformation = lotto.giveInformation();
+            information.append(lottoInformation + "\n");
+        }
+
+        return information;
     }
 }

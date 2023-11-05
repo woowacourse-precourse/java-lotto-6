df06db3369c36eee18ca508f97458ef9875efea4
seminss <2007ksm@naver.com>
2023-11-05 16:27:52 +0900

    test: 입력 받은 보너스 번호의 유효성을 검사 한다.

---
+++ src/test/java/lotto/bonusNumber/BonusNumberTest.java
@@ -0,0 +1,23 @@
+package lotto.bonusNumber;
+
+import lotto.model.BonusNumber;
+import lotto.model.WinningNumbers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BonusNumberTest {
+    @Test
+    void validate_1에서_45_사이의_중복_없는_6자리는_참이다() {
+        BonusNumber bonusNumber = new BonusNumber(1);
+        assertThat(bonusNumber.getNumber()).isEqualTo(1);
+    }
+    @Test
+    void validate_범위를_벗어나면_거짓을_반환한다() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> new BonusNumber(-1));
+    }
+}
---
+++ src/test/java/lotto/bonusNumber/BonusNumberValidatorTest.java
@@ -0,0 +1,26 @@
+package lotto.bonusNumber;
+
+import lotto.validator.BonusNumberValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class BonusNumberValidatorTest {
+    BonusNumberValidator bonusNumberValidator = new BonusNumberValidator();
+
+    @Test
+    void validate_WinningNumbers와_중복이_없으면_참을_반환한다() {
+        String userInput = "11";
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        bonusNumberValidator.validateAndParse(userInput, winningNumbers);
+    }
+
+    @Test
+    void validate_WinningNumbers와_중복이_있으면_거짓을_반환한다() {
+        String userInput = "11";
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 11);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> bonusNumberValidator.validateAndParse(userInput, winningNumbers));
+    }
+}

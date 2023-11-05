f69878e19c72f776a3026eec32f364a912727ae9
unknown <winternrabbit@gmail.com>
2023-11-05 23:01:56 +0900

    feat: 보너스 번호가 1~45 사이의 숫자가 아닌 경우 예외처리하는 기능 구현

--- src/main/java/lotto/LottoBonus.java
+++ src/main/java/lotto/LottoBonus.java
@@ -1,2 +1,20 @@
-package lotto;public class LottoBonus {
+package lotto;
+
+public class LottoBonus {
+    private final int bonusNumber;
+
+    public LottoBonus(int bonusNumber) {
+        isNumberInRange(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void isNumberInRange(int bonusNumber) {
+        if (bonusNumber < Config.MIN_LOTTO_NUMBER || bonusNumber > Config.MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessages.ERROR_OUT_OF_RANGE_LOTT_NUMBER.getMessage());
+        }
+    }
 }

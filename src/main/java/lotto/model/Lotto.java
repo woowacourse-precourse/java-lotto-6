58862330a575dc382063e93d89892384a15398e3
ParkSoyun <soyp@naver.com>
2023-11-05 18:11:34 +0900

    Fix: Lotto.java의 validate 부분 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,8 +6,11 @@ import static lotto.constant.LottoRuleConstant.LOTTO_MIN_NUM;
 import static lotto.constant.LottoRuleConstant.LOTTO_SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -20,19 +23,49 @@ public class Lotto {
     }
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        validateLottoNumber(numbers);
+
+        this.numbers = new ArrayList<>(numbers);
+
+        this.numbers.sort(Comparator.naturalOrder());
     }
 
     private List<Integer> createAutoLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUM, LOTTO_MAX_NUM, LOTTO_SIZE);
     }
 
+    private void validateLottoNumber(List<Integer> numbers) {
+        checkNumberCount(numbers);
+        checkDuplicateNumber(numbers);
+        checkRange(numbers);
+    }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void checkNumberCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개입니다.");
         }
+    }
+
+    private void checkDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+
+        if (numbers.size() != numberSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 가질 수 없습니다.");
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> checkMinimum(number) || checkMaximum(number))) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1이상 45이하의 수만 가능합니다.");
+        }
+    }
+
+    private boolean checkMinimum(Integer number) {
+        return number < LOTTO_MIN_NUM;
+    }
+
+    private boolean checkMaximum(Integer number) {
+        return number > LOTTO_MAX_NUM;
     }
 
     @Override

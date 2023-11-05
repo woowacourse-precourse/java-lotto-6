744f45fdea9804d9b557109e1c1da7b8c03e2d5c
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 01:59:35 +0900

    feat(OutputView): 당첨 통계를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,11 +2,30 @@ package lotto.view;
 
 import java.util.List;
 import lotto.constant.LottoMessage;
+import lotto.model.LottoRank;
 
 public class OutputView {
 
     public static void printLottoNumbers(List<Integer> numbers) {
         LottoMessage message = LottoMessage.LOTTO_NUMBERS_OUTPUT_MESSAGE;
         System.out.println(message.getMessage(numbers.toArray()));
+    }
+
+    public static void printLottoRank(LottoRank lottoRank, int count) {
+        LottoMessage message = getLottoRankMessage(lottoRank.isMatchBonus());
+
+        System.out.println(message.getMessage(
+                lottoRank.getMatchCount(),
+                lottoRank.getWinningMoney(),
+                count)
+        );
+    }
+
+    private static LottoMessage getLottoRankMessage(boolean matchBonus) {
+        if (matchBonus) {
+            return LottoMessage.WINNING_STATISTICS_BONUS_MESSAGE;
+        }
+
+        return LottoMessage.WINNING_STATISTICS_MESSAGE;
     }
 }

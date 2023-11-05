58a58760f146d2e8db280690272efec8ae0f1350
minsub <chrkb1569@naver.com>
2023-11-05 20:12:02 +0900

    feat : 사용자로부터 당첨 번호와 보너스 정보를 입력받는다.

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,17 +1,22 @@
 package lotto.controller;
 
 import lotto.domain.Player;
+import lotto.util.NumberFactory;
 import lotto.view.input.InputView;
 import lotto.view.output.OutputView;
+
+import java.util.List;
 
 // 사용자로부터 값을 입력받고, 이를 출력하기 위한 클래스
 public class GameController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final NumberFactory numberFactory;
 
-    public GameController(InputView inputView, OutputView outputView) {
+    public GameController(InputView inputView, OutputView outputView, NumberFactory numberFactory) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.numberFactory = numberFactory;
     }
 
     public void startGame() {
@@ -20,5 +25,19 @@ public class GameController {
 
         Player player = new Player(cost);
         outputView.printPurchaseResult(player.getLottoCount());
+
+        for(int lotto = 0; lotto < player.getLottoCount(); lotto++) {
+            List<Integer> generatedLotto = numberFactory.getNumbers();
+            player.buyLotto(generatedLotto);
+            outputView.printLotto(generatedLotto);
+        }
+
+        outputView.printWinnerNumberMessage();
+
+        List<String> winnerNumber = inputView.getWinnerNumber();
+
+        outputView.printBonusNumberMessage();
+
+        String bonusNumber = inputView.getBonusNumber();
     }
 }

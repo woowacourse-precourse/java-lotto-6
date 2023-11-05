cfd8a4e0a89d39c3fe424e1ad5adb41b93f3726f
kim <hwiyul_g@daum.net>
2023-11-05 12:22:11 +0900

    feat(LottoController) : View와 Model을 이용해 전체 흐름 관리 로직 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,51 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.model.Bonus;
+import lotto.model.Budget;
+import lotto.model.Lotto;
+import lotto.model.LottoPurchaser;
+import lotto.model.LottoResult;
+import lotto.model.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoPurchaser lottoPurchaser;
+
+    public LottoController(final InputView inputView, final OutputView outputView,
+        final LottoPurchaser lottoPurchaser) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoPurchaser = lottoPurchaser;
+    }
+
+    public void run() {
+        try {
+            Budget budget = inputView.inputBudget();
+            List<Lotto> purchasedLottos = purchaseLottos(budget);
+            WinningLotto winningLotto = inputView.inputWinningLotto();
+            printLottoResult(purchasedLottos, winningLotto, budget);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            run();
+        }
+    }
+
+    private List<Lotto> purchaseLottos(Budget budget) {
+        List<Lotto> purchasedLottos = lottoPurchaser.purchase(budget);
+        outputView.printUserLottos(purchasedLottos);
+        return purchasedLottos;
+    }
+
+    private void printLottoResult(List<Lotto> purchasedLottos, WinningLotto winningLotto,
+        Budget budget) {
+        LottoResult lottoResult = winningLotto.matchLottos(purchasedLottos);
+        outputView.printLottoResult(lottoResult, budget.inputMoney());
+    }
+
+
+}

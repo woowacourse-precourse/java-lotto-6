d488a8cf7388a1a4f71c8e6d46ecb1eb1210fa0d
Park Eunhyeong <silbroeh@gmail.com>
2023-11-05 23:40:00 +0900

    feat : Bonus클래스 - validate()메소드 구현

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -1,14 +1,21 @@
 package lotto;
 
+import java.util.List;
+
 public class Bonus {
     private final int number;
 
-    public Bonus(int number) {
-        validate(number);
+    public Bonus(int number, List<Integer> numbers) {
+        validate(number, numbers);
         this.number = number;
     }
 
     public int getNumber() {
         return this.number;
+    }
+
+    private void validate(int number, List<Integer> numbers) {
+        validateAllRange(number);
+        validateNoDuplicate(number, numbers);
     }
 }

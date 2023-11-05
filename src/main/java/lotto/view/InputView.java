9f8cb4e6fe2b537bd097bf28135bcf5814f90c5c
ohjuyoungg <ejrm12@naver.com>
2023-11-05 16:36:25 +0900

    feat: 당첨 번호를 입력하는 메소드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Collections;
 import java.util.List;
 import lotto.service.LottoService;
 import lotto.validation.InputValidation;
@@ -8,10 +9,20 @@ import lotto.validation.InputValidation;
 public class InputView {
     public static int inputPurchaseAmount() {
         String input = Console.readLine();
-        InputValidation.validatePurchaseAmountIsBlank(input);
+        InputValidation.validateIsBlank(input);
         int purchaseInput = Integer.parseInt(input);
         InputValidation.validateInputPurchaseAmount(purchaseInput);
         InputValidation.validateInputString(input);
         return LottoService.amountPurchaseDivide(purchaseInput);
+    }
+
+    public static List<Integer> inputWinningNum() {
+        String input = Console.readLine();
+        InputValidation.validateIsBlank(input);
+        List<Integer> winNumberList = InputValidation.validateInputWinningNumber(input);
+        InputValidation.validateNoDuplicates(winNumberList);
+        InputValidation.validateNumberRange(winNumberList);
+        InputValidation.validateSixNumbers(winNumberList);
+        return winNumberList;
     }
 }

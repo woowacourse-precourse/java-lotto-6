8b68c9e041ca47a3cfd4dc94d428c4dd3478079a
yshong1998 <yshong1998@gmail.com>
2023-11-06 03:14:29 +0900

    Feat(LottoService): 통계를 계산하는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -18,4 +18,24 @@ public class LottoService {
         lottoRepository.save(lottos);
         return lottos;
     }
+
+    public WinningStatistics generateStatistics(List<Prize> winPrizes) {
+        WinningStatistics winningStatistics = new WinningStatistics();
+        winningStatistics.acceptWinPrizes(winPrizes);
+        return winningStatistics;
+    }
+
+    public List<Prize> calculateTotalPrizes(List<Integer> winningNumbers, Integer bonusNumber) {
+        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), bonusNumber);
+        List<Lotto> lottos = lottoRepository.findAll();
+        return calculatePrize(lottos, winningLotto);
+    }
+
+    private List<Prize> calculatePrize(List<Lotto> lottos, WinningLotto winningLotto) {
+        return lottos.stream().map(lotto -> {
+            int matchNumber = winningLotto.compareLotto(lotto);
+            boolean isBonusNumberMatch = winningLotto.compareBonusNumber(lotto);
+            return Prize.getPrizeByMatchCount(matchNumber, isBonusNumberMatch);
+        }).toList();
+    }
 }

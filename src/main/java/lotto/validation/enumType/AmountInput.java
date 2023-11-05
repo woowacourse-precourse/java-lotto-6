96a9434c95026f62a99ab1affcbdec36495dba4e
acs0209 <annjae4676@naver.com>
2023-11-05 16:34:00 +0900

    refactor: 상수 메시지 값 변경

--- src/main/java/lotto/validation/enumType/AmountInput.java
+++ src/main/java/lotto/validation/enumType/AmountInput.java
@@ -1,9 +1,9 @@
 package lotto.validation.enumType;
 
 public enum AmountInput {
-    NUMERIC_FORMAT_MESSAGE("숫자만 입력할 수 있습니다."),
-    POSITIVE_MESSAGE("양수 값을 입력해야 합니다."),
-    VALUE_DIVIDE_MESSAGE("1000원 단위로 값을 입력해야 합니다."),
+    NUMERIC_FORMAT_MESSAGE("[ERROR] 숫자만 입력할 수 있습니다."),
+    POSITIVE_MESSAGE("[ERROR] 양수 값을 입력해야 합니다."),
+    VALUE_DIVIDE_MESSAGE("[ERROR] 1000원 단위로 값을 입력해야 합니다."),
     ONE_LOTTO_PRICE(1000),
     POSITIVE_CHECK_NUMBER(1);
 

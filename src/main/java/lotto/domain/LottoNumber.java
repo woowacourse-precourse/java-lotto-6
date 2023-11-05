2f25c660f60abd6a315350d9cfe9fb376e1ec7e3
Isaac Han <isaachan233@gmail.com>
2023-11-05 14:03:53 +0900

    feat: 로또 넘버 객체 생성 및 생성자 설정

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import lotto.enums.ErrorMessages;
+import lotto.enums.LottoEnum;
+
+public class LottoNumber {
+    private final int number;
+
+    public LottoNumber(String stringNumber) {
+        try {
+            int parsedNumber = Integer.parseInt(stringNumber);
+            validate(parsedNumber);
+            this.number = parsedNumber;
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException(ErrorMessages.NON_NUMERIC_INPUT_MESSAGE.getMessage());
+        }
+    }
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < LottoEnum.MIN_LOTTO_NUMBER.getValue()
+                || number > LottoEnum.MAX_LOTTO_NUMBER.getValue()) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_NUMBER_RANGE_MESSAGE.getMessage());
+        }
+    }
+}

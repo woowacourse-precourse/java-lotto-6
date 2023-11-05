bb171dd55871b30f063f07cdaba1655c360ef097
sudhdkso <waga1009@gmail.com>
2023-11-06 01:37:03 +0900

    feat: 6개의 랜덤 숫자를 생성할 수 있는 클래스 추가

---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,18 @@
+package lotto.util;
+
+import static lotto.domain.enums.LottoConstants.LOTTO_SIZE;
+import static lotto.domain.enums.LottoConstants.MAX_LOTTO_NUM;
+import static lotto.domain.enums.LottoConstants.MIN_LOTTO_NUM;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    public RandomNumberGenerator() {
+
+    }
+
+    public static List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_SIZE);
+    }
+}

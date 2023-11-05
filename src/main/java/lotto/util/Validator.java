c4eea9f32bd76bb52b11fcbdf3815356215be7c6
sudhdkso <waga1009@gmail.com>
2023-11-06 02:00:12 +0900

    feat: 로또 번호 중복 및 범위 체크 기능 추가

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,6 +1,38 @@
 package lotto.util;
 
+import static lotto.domain.enums.LottoConstants.LOTTO_SIZE;
+import static lotto.domain.enums.LottoConstants.MAX_LOTTO_NUM;
+import static lotto.domain.enums.LottoConstants.MIN_LOTTO_NUM;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
+
+    public static void validateNumbersRange(List<Integer> numbers) {
+        numbers.stream()
+                .forEach(number -> validateNumberRange(number));
+    }
+
+    public static void validateNumberRange(int number) {
+        if (number < MIN_LOTTO_NUM || number > MAX_LOTTO_NUM) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateDuplicateLottoNumbers(List<Integer> numbers) {
+        Set<Integer> unDuplicatedNumbers = new HashSet<>(numbers);
+        if (unDuplicatedNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public static int validateNumeric(String number) {
         try {

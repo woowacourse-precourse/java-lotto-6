bd020a2fcad080d23d56be67ecd3f42bd2cbb927
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-06 01:24:05 +0900

    feat(lotto): 로또를 구입하는 기능 구현
    - README.md 업데이트
    - 로또를 구입하는 기능 purchaseLotto()
    - 로또 수량을 출력하는 기능 getAmount()

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,10 +1,24 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
 public class Purchase {
     private final int amount;
+    private final List<Lotto> purchasedLotto = new ArrayList<>();
 
-    public Purchase(String amount) {
-        this.amount = Integer.parseInt(amount);
+    public Purchase(final String amount) {
+        this.amount = Integer.parseInt(amount) / 1000;
+        purchaseLotto();
+    }
+
+    public void purchaseLotto() {
+        for (int i = 0; i < this.amount; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            purchasedLotto.add(lotto);
+        }
     }
 
     public int getAmount() {

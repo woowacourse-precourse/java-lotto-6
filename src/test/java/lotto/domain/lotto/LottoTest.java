299575afc5c64d1b0e73e80cf7a4b8d9f0da0bd5
박승찬 <seungchan141414@gmail.com>
2023-11-05 12:03:07 +0900

    [Test(lotto)] 로또 오른 차순으로 정렬하는 기능 테스트

---
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -0,0 +1,34 @@
+package lotto.domain.lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class LottoTest {
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+    }
+
+    @DisplayName("로또를 오른 차순으로 정리")
+    @Test
+    void organizeLotto() {
+        // when
+        List<Integer> result = lotto.organizeLotto();
+        System.out.println("result = " + result);
+
+        // than
+        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void showInformation() {
+    }
+}

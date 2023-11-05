e2f6d8297ab225c4b48fe04bc9321bab72fe627c
박승찬 <seungchan141414@gmail.com>
2023-11-05 16:06:24 +0900

    [Feat(lotto)] 통계의 결과를 보여주는 기능 추가.

--- src/main/java/lotto/domain/user/Statistic.java
+++ src/main/java/lotto/domain/user/Statistic.java
@@ -1,21 +1,33 @@
 package lotto.domain.user;
 
+import lotto.config.Config;
+import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoEnvelop;
 import lotto.domain.num.LottoNumResults;
+import lotto.utill.Utii;
 
 /**
  * 사용자의 통계 클래스
  */
 public class Statistic {
     private LottoEnvelop lottoEnvelop;
-    private LottoNumResults lottoNumResults;
+    private Judgment judgment;
 
-    private Integer countSameWinNum;
-    private Boolean isSameBonusNum;
+    private Integer countOfFirstPlace;
+    private Integer countOfSecondPlace;
+    private Integer countOfThirdPlace;
+    private Integer countOfFourthPlace;
+    private Integer countOfFifthPlace;
+
 
     public Statistic(LottoEnvelop lottoEnvelop, LottoNumResults lottoNumResults) {
         this.lottoEnvelop = lottoEnvelop;
-        this.lottoNumResults = lottoNumResults;
+        this.judgment = Config.judgment(lottoNumResults);
+        this.countOfFirstPlace = 0;
+        this.countOfSecondPlace = 0;
+        this.countOfThirdPlace = 0;
+        this.countOfFourthPlace = 0;
+        this.countOfFifthPlace = 0;
     }
     // 당첨은 1등부터 5등까지 있다. 당첨 기준과 금액은 아래와 같다.
     //  1등: 6개 번호 일치 / 2,000,000,000원
@@ -32,16 +44,51 @@ public class Statistic {
 //    6개 일치 (2,000,000,000원) - 0개
 //    총 수익률은 62.5%입니다.
 
-    public StringBuilder show() {
-        StringBuilder result = new StringBuilder();
+    private void execution() {
         // 봉투들의 로또 갯수 만큼 반복
         for (int i = 0; i < lottoEnvelop.size(); i++) {
             // 로또 0~5 까지의 번호와, 보너스 번호중 몇개가 같은지 확인
-//            lottoEnvelop.
+            Lotto lotto = lottoEnvelop.takeLotto(i);
+            Integer countSame = judgment.countSameWinNum(lotto);
+            Boolean isSameBonus = judgment.isSameBonusNum(lotto);
+
+            if (Utii.isSameInt(countSame, 6)) {
+                // 1등: 6개 번호 일치 / 2,000,000,000원
+                countOfFirstPlace = countOfFirstPlace + 1;
+            }
+
+            if (Utii.isSameInt(countSame, 5) && isSameBonus) {
+                // 2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
+                countOfSecondPlace = countOfSecondPlace + 1;
+            }
+
+            if (Utii.isSameInt(countSame, 5) && !isSameBonus) {
+                // 3등: 5개 번호 일치 / 1,500,000원
+                countOfThirdPlace = countOfThirdPlace + 1;
+            }
+
+            if (Utii.isSameInt(countSame, 4)) {
+                // 4등: 4개 번호 일치 / 50,000원
+                countOfFourthPlace = countOfFourthPlace + 1;
+            }
+
+            if (Utii.isSameInt(countSame, 3)) {
+                // 5등: 3개 번호 일치 / 5,000원
+                countOfFifthPlace = countOfFifthPlace + 1;
+            }
         }
+    }
 
-        // 
+    public StringBuilder show() {
+        StringBuilder resultString = new StringBuilder();
 
-        return result;
+        execution();
+        resultString.append("3개 일치 (5,000원) - " + countOfFifthPlace + "개\n");
+        resultString.append("4개 일치 (50,000원) - " + countOfFourthPlace + "개\n");
+        resultString.append("5개 일치 (1,500,000원) - " + countOfThirdPlace + "개\n");
+        resultString.append("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countOfSecondPlace + "개\n");
+        resultString.append("6개 일치 (2,000,000,000원) - " + countOfFirstPlace + "개\n");
+
+        return resultString;
     }
 }

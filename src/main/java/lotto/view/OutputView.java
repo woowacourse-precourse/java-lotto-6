2bce558593dafcbad115782bad2ebf34bfb916b5
ohjuyoungg <ejrm12@naver.com>
2023-11-05 14:14:20 +0900

    feat: 로또 n개를 구매했습니다. 문구 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,17 @@
 package lotto.view;
 
+import lotto.service.LottoService;
 import lotto.utils.constants.Comment;
 
 public class OutputView {
 
     public static void printGameStartMessage() {
         System.out.println(Comment.INPUT_PURCHASE_AMOUNT.getComment());
-        InputView.inputPurchaseAmount();
+    }
+
+    public static void printLottoPurchaseAmount(int lottoNumber) {
+        LottoService.amountPurchaseDivide(lottoNumber);
+        System.out.println();
+        System.out.println(lottoNumber + Comment.OUTPUT_PURCHASE_AMOUNT.getComment());
     }
 }

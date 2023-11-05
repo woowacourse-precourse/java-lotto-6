b68fd85e4b138dc7ec8bd3736f82d358344703c2
sung-yeop <jjabc9459@naver.com>
2023-11-05 23:14:57 +0900

    refactor(LottoConstants, LottoInputViewConstants) : 상수 내용 변경
    - 천원 단위를 의미하는 unit 추가
    - 출력을 위한 메세지를 상수화하는 클래스 생성

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -3,6 +3,7 @@ package lotto.constant;
 public class LottoConstants {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int unit = 1000;
 
     private LottoConstants() {
     }
---
+++ src/main/java/lotto/controller/InputController.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.InputValidator;
+
+public class InputView {
+    private final Inputvali
+
+    public int inputAmount() {
+        String input = Console.readLine();
+
+
+    }
+
+}

8106cf4ecbf63ffb3c3ee6a9747674176ea0ebc1
Jinnie Kim <jinniek48@gmail.com>
2023-11-06 05:49:17 +0900

    feat: 로또 구입 금액 예외 처리 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -6,11 +6,34 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class LottoMachine {
 
-    private int money;
+    private int lottoCount;
 
-    private void insertMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
-        money = parseInt(Console.readLine());
+    public LottoMachine() {
+        int money = insertMoney();
+        lottoCount = money / 1000;
     }
+
+    private int insertMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = parseInt(Console.readLine());
+
+        try {
+            notDividedWithThousand(money);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return insertMoney();
+        }
+
+        return money;
+    }
+
+    private void notDividedWithThousand(int money) {
+        int remainder = money % 1000;
+
+        if (remainder > 0) {
+            throw new IllegalArgumentException("[ERROR] 1000으로 나눠 떨어지지 않습니다.");
+        }
+    }
+
 
 }

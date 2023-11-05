12d7df1ea39120d9ee3dbfb3ff1213e58297a259
kichang_98 <richkc2006@naver.com>
2023-11-05 17:57:04 +0900

    1번+2번+3번+ 예외처리 1번

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입 금액을 입력해주세요.");
+        // 구입 금액을 입력
+        int userMoney = getValidMoney();
+
+        // 사용자 게임 수 출력
+        System.out.println();
+        System.out.println(userMoney / 1000 + "개를 구매했습니다.");
+    }
+
+    // 사용자 입력 금액이 유효한지 확인하는 메서드
+    public static int getValidMoney() {
+        while (true) {
+            try {
+                int userMoney = getMoney();
+                correctMoney(userMoney);
+                return userMoney;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    // 사용자에게 구입 금액을 입력 받는 메서드
+    public static int getMoney() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+    // 사용자 구입 금액이 정확한지 확인하는 메서드
+    public static void correctMoney(int userMoney) {
+        if (userMoney % 1000 != 0 || userMoney <= 0) {
+            // IllegalArgumentException를 발생시키고 오류 메시지 출력
+            throw new IllegalArgumentException("[ERROR] 천원 단위로 금액을 입력해 주세요.");
+        }
     }
 }

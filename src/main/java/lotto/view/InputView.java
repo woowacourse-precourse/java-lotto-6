1fce3272382f2ca5a36c7e00f854f7b790bfc012
howudong <tjdvy963@naver.com>
2023-11-05 22:01:45 +0900

    refactor : InputView와 OutputView 인터페이스의 파라미터가 관련된 DTO만 Map의 값으로 가질 수 있게 함

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
+import lotto.dto.DTO;
+
 import java.util.Map;
 
 public interface InputView {
-    void read(Map<String, String> parameter);
+    void read(Map<String, ? extends DTO.Input> parameter);
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
+import lotto.dto.DTO;
+
 import java.util.Map;
 
 public interface OutputView {
-    void view(Map<String, Object> model);
+    void view(Map<String, ? extends DTO.Output> model);
 }

e446a4fb11145254601f47a652a4f8705290b95d
song-jae-wan <sjo9810@gmail.com>
2023-11-05 21:36:24 +0900

    Test: 로또 정렬 기능 단위 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -29,5 +30,12 @@ class LottoTest {
     void createLottoByInRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호를 조회할때 오름차순으로 정렬된다.")
+    @Test
+    void findLottoBySortedNumber() {
+        Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 }

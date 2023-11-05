4597ce1f463b560a64d2be170f43f8f98c989244
whxogus215 <whxogus215@naver.com>
2023-11-05 09:17:35 +0900

    style: 100열 넘지 않도록 줄바꿈

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -4,13 +4,14 @@ import java.util.List;
 import lotto.utils.enums.Code;
 import lotto.utils.InputValidator;
 
-public class WinningNumbers extends Lotto{
+public class WinningNumbers extends Lotto {
     private final List<Integer> numbers;
 
     public WinningNumbers(List<Integer> numbers) {
         super(numbers);
 
-        numbers.forEach(number -> InputValidator.validateNumberInRange(number, Code.WINNING.getCode()));
+        numbers.forEach(
+                number -> InputValidator.validateNumberInRange(number, Code.WINNING.getCode()));
         this.numbers = numbers;
     }
 }

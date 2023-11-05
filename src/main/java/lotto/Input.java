8bbf36b7ee9b02ed1544f1fddcccf1723b550317
unknown <winternrabbit@gmail.com>
2023-11-05 22:10:00 +0900

    feat: 보너스 번호 입력 받는 기능 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     private static final String REQUEST_COST = "구입금액을 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public String buyLotto() {
         System.out.println(REQUEST_COST);
@@ -14,6 +15,12 @@ public class Input {
 
     public String winningNumbers() {
         System.out.println(REQUEST_WINNING_NUMBER);
+
+        return Console.readLine();
+    }
+
+    public String bonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER);
 
         return Console.readLine();
     }

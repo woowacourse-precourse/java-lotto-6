99b6055c4169f13c04b981c5999433bcce66c553
Jay <injaesong0@gmail.com>
2023-11-06 03:47:54 +0900

    feat: implement input money with proper exceptions

--- src/main/java/lotto/Communicator.java
+++ src/main/java/lotto/Communicator.java
@@ -1,19 +1,54 @@
 package lotto;
 
+import java.math.BigDecimal;
+import java.util.NoSuchElementException;
+
 public class Communicator {
+    private static final Reader DEFAULT_READER = new ConsoleReader();
     private static final Printer DEFAULT_PRINTER = new ConsolePrinter();
 
+    private final Reader reader;
     private final Printer printer;
 
     public Communicator() {
-        this(DEFAULT_PRINTER);
+        this(DEFAULT_READER, DEFAULT_PRINTER);
     }
 
-    public Communicator(Printer printer) {
+    public Communicator(Reader reader, Printer printer) {
+        this.reader = reader;
         this.printer = printer;
     }
 
-    public void printStart() {
-        printer.print("구입금액을 입력해주세요.");
+    public BigDecimal instructBuy() {
+        BigDecimal payment = getMoneyWithPrompt("구입금액을 입력해주세요.");
+        validatePositive(payment);
+        validateDivisible(payment);
+        return payment;
+    }
+
+    private BigDecimal getMoneyWithPrompt(String prompt) {
+        printer.print(prompt);
+        return getNonNullMoney();
+    }
+
+    private BigDecimal getNonNullMoney() {
+        try {
+            return reader.readMoney();
+        } catch (NoSuchElementException exception) {
+            throw new IllegalArgumentException("입력 금액이 비어 있습니다.");
+        }
+    }
+
+    private void validateDivisible(BigDecimal payment) {
+        BigDecimal remainder = payment.remainder(new BigDecimal(1000));
+        if (remainder.compareTo(BigDecimal.ZERO) != 0) {
+            throw new IllegalArgumentException("구매 금액은 1000원 단위여야 합니다.");
+        }
+    }
+
+    private void validatePositive(BigDecimal payment) {
+        if (payment.compareTo(BigDecimal.ZERO) < 1) {
+            throw new IllegalArgumentException("0보다 큰 금액을 입력해주세요.");
+        }
     }
 }
--- src/test/java/lotto/CommunicatorTest.java
+++ src/test/java/lotto/CommunicatorTest.java
@@ -1,31 +1,90 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import camp.nextstep.edu.missionutils.Console;
 
 class CommunicatorTest {
+    static final Communicator communicator = new Communicator();
+
+    ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void cleanUp() {
+        System.setOut(System.out);
+        System.setIn(System.in);
+        Console.close();
+    }
 
     @Test
-    @DisplayName("시작 문구 입력을 출력한다.")
-    void test_PrintStart() {
-        //given
-        Communicator communicator = new Communicator();
-
+    @DisplayName("금액 입력 문구를 출력한다.")
+    void test_InstructorBuy_PrintPrompt() {
         //when
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-        communicator.printStart();
+        assertThatThrownBy(communicator::instructBuy);
         String output = outputStream.toString();
 
         //then
         assertThat(output).contains("구입금액을 입력해주세요.");
+    }
 
-        //clean
-        System.setOut(System.out);
+    @ParameterizedTest(name = "{0}원을 입력 시 출력 문구 : {1}")
+    @MethodSource("stringAndStringProvider")
+    @DisplayName("입력 받는 금액이 잘못되면 IllegalArgumentException을 발생시킨다.")
+    void test_InstructBuyFailed(String input, String expectedMessage) {
+        //given
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //when
+        Exception exception = assertThrows(IllegalArgumentException.class, communicator::instructBuy);
+
+        //then
+        assertThat(exception.getMessage()).isEqualTo(expectedMessage);
+    }
+
+    @Test
+    @DisplayName("구매 금액 입력을 성공한다.")
+    void test_InstructBuySuccessful() {
+        //given
+        String input = "5000";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //when
+        BigDecimal payment = communicator.instructBuy();
+
+        //then
+        assertThat(payment).isEqualTo(new BigDecimal(input));
+    }
+
+    static Stream<Arguments> stringAndStringProvider() {
+        return Stream.of(
+                Arguments.of("", "입력 금액이 비어 있습니다."),
+                Arguments.of(" ", "올바른 숫자 형식이 아닙니다."),
+                Arguments.of("10 000", "올바른 숫자 형식이 아닙니다."),
+                Arguments.of("0", "0보다 큰 금액을 입력해주세요."),
+                Arguments.of("-10000", "0보다 큰 금액을 입력해주세요."),
+                Arguments.of("5500", "구매 금액은 1000원 단위여야 합니다.")
+        );
     }
 }

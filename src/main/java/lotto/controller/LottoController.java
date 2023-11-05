bead62260494c85a4957acd13a3e699a0032a586
WooYoungDoo <duden5000@naver.com>
2023-11-05 12:37:31 +0900

    feat (LottoController) : 입력 값 검증 서비스 추가 및 예외 처리 잡아서 반복

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.service.ValidateService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -7,16 +8,29 @@ public class LottoController {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
+    private final ValidateService validateService = new ValidateService();
 
     public void playGame() {
-        String inputPurchaseMoney = purchaseMoneyProcess();
+        printPurchaseMoneyMessage();
+        String inputPurchaseMoney = inputPurchaseMoneyProcess();
+        printBlankLine();
+
+
     }
 
-    private String purchaseMoneyProcess() {
+    private void printPurchaseMoneyMessage() {
         outputView.printPurchaseMoneyMessage();
-        String inputPurchaseMoney = inputView.inputPurchaseMoney();
-        printBlankLine();
-        return inputPurchaseMoney;
+    }
+
+    private String inputPurchaseMoneyProcess() {
+        try {
+            String inputPurchaseMoney = inputView.inputPurchaseMoney();
+            validateService.validateInputPurchaseMoney(inputPurchaseMoney);
+            return inputPurchaseMoney;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputPurchaseMoneyProcess();
+        }
     }
 
     private void printBlankLine() {

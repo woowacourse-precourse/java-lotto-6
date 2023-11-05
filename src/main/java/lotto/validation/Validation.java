f23a7ae631b32e3fe8ab2ba24242333378bfe2c1
JH-TT <leejongho9803@gmail.com>
2023-11-05 15:16:54 +0900

    feat: 에러 메시지 추가

--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -2,12 +2,14 @@ package lotto.validation;
 
 import java.util.HashSet;
 import java.util.List;
+import lotto.view.ErrorMessage;
 
 public class Validation {
     private static final int LOTTO_PRISE = 1000;
     private static final int ZERO = 0;
     public static void checkIsDivideByThousand(final String input) {
         if (Integer.parseInt(input) % LOTTO_PRISE != 0) {
+            ErrorMessage.isNotDivideByLottoAmount();
             throw new IllegalArgumentException();
         }
     }
@@ -15,12 +17,14 @@ public class Validation {
     public static void checkIsNatural(final int input) {
         // checkIsNumber를 통과했기 때문에 0인지만 확인하면 된다.
         if (input == ZERO) {
+            ErrorMessage.isNotNatural();
             throw new IllegalArgumentException();
         }
     }
 
     public static void checkLottoNumberLength(final int length, final List<Integer> lotto) {
         if (lotto.size() != length) {
+            ErrorMessage.isNotMatchLottoLength();
             throw new IllegalArgumentException();
         }
     }
@@ -33,18 +37,21 @@ public class Validation {
 
     public static void checkNumberIsBetweenStartAndEnd(final int start, final int end, final int num) {
         if (num < start || num > end) {
+            ErrorMessage.isNotInRange();
             throw new IllegalArgumentException();
         }
     }
 
     public static void checkDuplicate(final List<Integer> lotto) {
         if (new HashSet<>(lotto).size() != lotto.size()) {
+            ErrorMessage.isDuplicatedLotto();
             throw new IllegalArgumentException();
         }
     }
 
     public static void checkLottoNumbersHasBonusNumber(final List<Integer> lotto, final int bonus) {
         if (lotto.contains(bonus)) {
+            ErrorMessage.lottoContainsBonusNumber();
             throw new IllegalArgumentException();
         }
     }
@@ -58,6 +65,7 @@ public class Validation {
     public static void checkIsNumber(final String numbers) {
         for (char c : numbers.toCharArray()) {
             if (!Character.isDigit(c)) {
+                ErrorMessage.isNotNumber();
                 throw new IllegalArgumentException();
             }
         }

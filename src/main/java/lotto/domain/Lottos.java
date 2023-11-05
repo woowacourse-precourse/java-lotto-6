dd5610a30538c5245736edd6663439507d53c73d
tam31 <wxogud3377@naver.com>
2023-11-06 00:42:05 +0900

    Feat(Lottos) : 구매한 로또 번호의 저장 요청 Docs : 구매한 로또 번호의 저장 요청 완료체크

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lottos {
+
+    private Lotto lotto;
+    private Bonus bonus;
+
+    public Lottos() {
+    }
+
+    public Lottos(Lotto lotto, Bonus bonus) {
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+
+    public void collectLottoNumbers(User user, Lotto lotto, Bonus bonus) {
+        user.storePurchasedLotto(new Lottos(lotto, bonus));
+    }
+}

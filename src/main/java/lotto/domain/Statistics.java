c44da35677ec2fdc8b4cab0cde035342fc401084
DaeYoungK <hanguk830@naver.com>
2023-11-05 12:09:49 +0900

    feat(Statistics): 통계 계산을 위한 enum 추가

---
+++ src/main/java/lotto/domain/Statistics.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public enum Statistics {
+
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    FIVE_BONUS(5, 30000000),
+    SIX(6, 2000000000);
+
+    private int count;
+    private int amount;
+
+    Statistics(int count, int amount) {
+        this.count = count;
+        this.amount = amount;
+    }
+}

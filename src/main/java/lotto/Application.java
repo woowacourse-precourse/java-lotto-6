10d7becd2a35c3847deae94fef0f0b959d14f360
yuseonjun <seonjun0906@gmail.com>
2023-11-05 15:19:09 +0900

    feat(Application): main 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.domain.LottoShop;
+import lotto.domain.RandomNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
+        LottoController lottoController = new LottoController(new LottoShop(new RandomNumberGenerator()));
         lottoController.run();
     }
 }

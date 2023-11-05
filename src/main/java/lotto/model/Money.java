ac8d7d7e0955284d342a9023acf56bfa2a1acf27
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-05 20:04:26 +0900

    feat(Money): 사용자가 로또 구입 금액을 입력할 수 있어야 한다.

---
+++ src/main/java/lotto/model/Money.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import lotto.config.LottoConfig;
+import lotto.util.Validator;
+
+public class Money {
+
+    private final int amount;
+
+    private Money(int amount) {
+        this.amount = amount;
+    }
+
+    public static Money from(String input) {
+        int parsedAmount = Validator.validateAndParseInteger(input);
+        validatePurchaseAmount(parsedAmount);
+        return new Money(parsedAmount);
+    }
+
+    private static void validatePurchaseAmount(int amount) {
+        if (amount <= 0 || amount % LottoConfig.LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+}

9dbfd1dd159099fc8a9da12e3d664c02fc10d366
namhoon-kim97 <kskcmh5723@gmail.com>
2023-11-06 00:38:19 +0900

    refactor: 상수 public final -> private static final로 변경

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -4,9 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomNumberGenerator {
-    public final int NUM_OF_LOTTO = 6;
-    public final int START_NUM_OF_LOTTO = 1;
-    public final int END_NUM_OF_LOTTO = 45;
+    private static final int NUM_OF_LOTTO = 6;
+    private static final int START_NUM_OF_LOTTO = 1;
+    private static final int END_NUM_OF_LOTTO = 45;
+
     public List<Integer> generateRandomNumber() {
         return Randoms.pickUniqueNumbersInRange(START_NUM_OF_LOTTO, END_NUM_OF_LOTTO, NUM_OF_LOTTO);
     }

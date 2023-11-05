c9a24cd5aecf165281bf976f285145d0e383c466
dh1010a <dh1010a@naver.com>
2023-11-05 20:03:21 +0900

    feat: 당첨 통계 출력 기능 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import java.util.List;
 import java.util.stream.Collectors;
+import lotto.dto.IssuedLottoDto;
+import lotto.dto.WinningStatisticsDto;
 
 public class View {
 
@@ -14,13 +17,13 @@ public class View {
 
     public void printNumberOfPurchasedLotto(int number) {
         System.out.printf(ViewMessage.NUMBER_OF_PURCHASED_LOTTO_FORMAT.getMessage(), number);
-        System.out.println();
     }
 
-    public void printIssuedLottoNumbers(List<IssuedLottosDto> lottos) {
-        for (IssuedLottosDto lotto : lottos) {
-            String message = lotto.getNumbers.stream()
-                    .map(IssuedLottosDto::getLottoNumbers)
+    public void printIssuedLottoNumbers(List<IssuedLottoDto> lottos) {
+        for (IssuedLottoDto lotto : lottos) {
+            List<Integer> numbers = lotto.numbers();
+            String message = numbers.stream()
+                    .map(Object::toString)
                     .collect(Collectors.joining(", "));
             System.out.println(OPEN_SQUARE_BRACKETS + message + CLOSE_SQUARE_BRACKETS);
         }
@@ -32,6 +35,22 @@ public class View {
 
     public void printRequestBonusNumber() {
         System.out.println(ViewMessage.REQUEST_BONUS_NUMBER_MESSAGE.getMessage());
+    }
+
+    public void printWinningStatistics(WinningStatisticsDto winDto) {
+        System.out.println(ViewMessage.WINNING_STATISTICS_MESSAGE.getMessage());
+        System.out.println(ViewMessage.DIVIDER_MESSAGE.getMessage());
+        printWinningCount(winDto);
+        System.out.printf(ViewMessage.WINNING_MATCHES_SIX_FORMAT.getMessage(), winDto.earningRate());
+    }
+
+    private void printWinningCount(WinningStatisticsDto winDto) {
+        System.out.printf(ViewMessage.WINNING_MATCHES_THREE_FORMAT.getMessage(), winDto.threeMatches());
+        System.out.printf(ViewMessage.WINNING_MATCHES_FOUR_FORMAT.getMessage(), winDto.fourMatches());
+        System.out.printf(ViewMessage.WINNING_MATCHES_FIVE_NO_BONUS_FORMAT.getMessage(), winDto.fiveMatches());
+        System.out.printf(ViewMessage.WINNING_MATCHES_FIVE_WITH_BONUS_FORMAT.getMessage(),
+                winDto.fiveMatchesWithBonus());
+        System.out.printf(ViewMessage.WINNING_MATCHES_SIX_FORMAT.getMessage(), winDto.sixMatches());
     }
 
 

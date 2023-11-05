574305d221fad33abb67d769b688bc2e19116069
slowcloud <sju9417@gmail.com>
2023-11-05 11:52:40 +0900

    feat: Application에서 LottoController를 실행시키는 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import util.Printer.Printer;
+import util.Scanner.Scanner;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputView inputView = new InputView(new Scanner());
+        OutputView outputView = new OutputView(new Printer());
+        LottoController lottoController = new LottoController(inputView, outputView);
+        lottoController.play();
+
     }
 }

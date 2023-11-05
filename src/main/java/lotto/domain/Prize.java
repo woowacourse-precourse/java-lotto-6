7d8eaddcdcb2432a658b5f5ad29cd7fff185e5d4
IM-GYURI <kgulr0517@gmail.com>
2023-11-05 20:14:02 +0900

    style: 줄 바꿈

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -3,11 +3,21 @@ package lotto.domain;
 import java.util.function.BiPredicate;
 
 public enum Prize {
-    FIFTH(5000, 3, (matchedLottoNumber, containBonusNumber) -> matchedLottoNumber == 3),
-    FOURTH(50_000, 4, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
-    THIRD(1_500_000, 5, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
-    SECOND(30_000_000, 5, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
-    FIRST(2_000_000_000, 6, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
+    FIFTH(5_000,
+            3,
+            (matchedLottoNumber, containBonusNumber) -> matchedLottoNumber == 3),
+    FOURTH(50_000,
+            4,
+            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
+    THIRD(1_500_000,
+            5,
+            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
+    SECOND(30_000_000,
+            5,
+            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
+    FIRST(2_000_000_000,
+            6,
+            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
 
 
     private final int money;

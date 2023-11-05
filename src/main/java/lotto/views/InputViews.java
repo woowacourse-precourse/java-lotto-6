9255ed6a38ec895e8bf89eabe776d77cd2ebf45a
74059 <shyeone@naver.com>
2023-11-05 13:13:55 +0900

    style: 코드 자동 정렬

--- src/main/java/lotto/views/InputViews.java
+++ src/main/java/lotto/views/InputViews.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputViews {
     static final String PURCHASE_INPUT_MESS = "구입금액을 입력해 주세요.";
+
     public static String readPurchaseAmount() {
         System.out.println(PURCHASE_INPUT_MESS);
         return Console.readLine();

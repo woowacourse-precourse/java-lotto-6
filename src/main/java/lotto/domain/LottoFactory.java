b34e69df947ba879ee9d86dab982e95b87cd6246
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-05 17:06:52 +0900

    refactor : 매직넘버 제거

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -6,6 +6,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoFactory {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBERS_COUNT = 6;
     private final List<Lotto> lottoTickets;
 
     public LottoFactory(long ticketAcounts) {
@@ -25,7 +28,8 @@ public class LottoFactory {
     }
 
     private Lotto makeLotto() {
-        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List<Integer> lotto = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBERS_COUNT));
         Collections.sort(lotto);
         return new Lotto(lotto);
     }
--- src/main/java/lotto/util/ResultFormatter.java
+++ src/main/java/lotto/util/ResultFormatter.java
@@ -1,26 +1,25 @@
 package lotto.util;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 import lotto.domain.LottoRank;
 
 public class ResultFormatter {
     private static final String MATCH_FORMAT = "%d개 일치%s (%s원) - %d개\n";
     private static final String BONUS_PART = ", 보너스 볼 일치";
     private static final String NONE = "NONE";
+    private static final String EMPTY = "";
 
     public String createWinningStatistics(LinkedHashMap<LottoRank, Integer> winnerCount) {
-        StringBuilder statisticsBuilder = new StringBuilder();
-        for (LottoRank rank : LottoRank.values()) {
-            if (rank.name().equals(NONE)) {
-                continue;
-            }
-            statisticsBuilder.append(createRankMessage(rank, winnerCount.getOrDefault(rank, 0)));
-        }
-        return statisticsBuilder.toString();
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> !rank.name().equals(NONE))
+                .map(rank -> createRankMessage(rank, winnerCount.getOrDefault(rank, 0)))
+                .collect(Collectors.joining());
     }
 
     private String createRankMessage(LottoRank rank, int count) {
-        String bonusPart = "";
+        String bonusPart = EMPTY;
         if (rank.isMatchBonus()) {
             bonusPart = BONUS_PART;
         }
@@ -30,4 +29,5 @@ public class ResultFormatter {
                 String.format("%,d", rank.getPrize()),
                 count);
     }
+
 }

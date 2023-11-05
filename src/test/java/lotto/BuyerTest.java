9addc2a37e1d0721d2ac35eb80e65508b10e87cc
깃SeoMoonK <sunghoon1216@naver.com>
2023-11-05 16:13:43 +0900

    Feat: 당첨 결과 조회에 대한 실패하는 테스트 작성 자신이 구매한 로또를 기반으로 결과를 받는 테스트케이스 임시 작성

--- src/test/java/lotto/BuyerTest.java
+++ src/test/java/lotto/BuyerTest.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.*;
@@ -32,7 +34,19 @@ public class BuyerTest {
             assertThatThrownBy(() -> buyerService.buy(tMoney[testIdx]))
                     .isInstanceOf(IllegalArgumentException.class);
         }
+    }
 
+    @Test
+    @DisplayName("구매자는 복권 위원회에게 당첨 결과 조회를 요청할 수 있음")
+    void checkTest() {
+
+        Buyer testBuyer = new Buyer();
+        List<Lotto> testLottos = new ArrayList<>();
+        testLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        testLottos.add(new Lotto(List.of(5, 6, 7, 8, 9, 10)));
+        testBuyer.setLottos(testLottos);
+
+        assertThat(testBuyer.tryCheck()).isInstanceOf(HashMap.class);
     }
 
 

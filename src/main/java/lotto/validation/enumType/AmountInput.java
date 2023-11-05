66463b09a2b1d1a875ab11353e5f8bebfd9d16f4
acs0209 <annjae4676@naver.com>
2023-11-05 16:21:57 +0900

    feat: 구입 금액 검증에 대한 enum 타입 상수 구현

---
+++ src/main/java/lotto/validation/enumType/AmountInput.java
@@ -0,0 +1,28 @@
+package lotto.validation.enumType;
+
+public enum AmountInput {
+    NUMERIC_FORMAT_MESSAGE("숫자만 입력할 수 있습니다."),
+    POSITIVE_MESSAGE("양수 값을 입력해야 합니다."),
+    VALUE_DIVIDE_MESSAGE("1000원 단위로 값을 입력해야 합니다."),
+    ONE_LOTTO_PRICE(1000),
+    POSITIVE_CHECK_NUMBER(1);
+
+    private String message;
+    private int number;
+
+    AmountInput(final String message) {
+        this.message = message;
+    }
+
+    AmountInput(final int number) {
+        this.number = number;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}

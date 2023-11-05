663a6deff83413ffe62df58317b8af93d54d387b
김효준 <khyojun1104@gmail.com>
2023-11-05 15:23:10 +0900

    feat(lotto): application, controller 초기셋팅! view 연결, application -> controller로 연결!

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        LottoController lottoController = new LottoController(InputView.getInstance(), OutputView.getInstance());
+
+        lottoController.start();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,23 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+
+    private InputView inputView;
+    private OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+
+
+
+
+    }
+}

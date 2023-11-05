68d124345758c9117358ddff18e4a71a170b65c4
kimtaesoo99 <kimtaesoo7@naver.com>
2023-11-05 18:49:17 +0900

    test: Lottos 테스트 추가

---
+++ src/test/java/lotto/model/LottosTest.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.util.RandomNumberGenerator;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class LottosTest {
+
+    @Test
+    void 구매한_수만큼_로또가_생성된다() {
+        // given
+        long buyCount = 10;
+        Lottos lottos = Lottos.of(buyCount, new RandomNumberGenerator());
+
+        // when
+        List<Lotto> purchasedLottos = lottos.getPurchasedLottos();
+
+        // then
+        assertThat(purchasedLottos).hasSize((int) buyCount);
+    }
+}

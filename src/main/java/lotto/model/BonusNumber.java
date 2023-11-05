24e089783d89db9da1e804d8e9b2ac606e3e7693
insu <is23193160@gmail.com>
2023-11-05 18:11:59 +0900

    feat: 보너스 번호가 1~45 사이의 숫자가 아닐 시 예외 처리하는 기능 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,10 +1,24 @@
 package lotto.model;
 
 public class BonusNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final String NUMBER_OUT_OF_RANGE_ERROR_MESSAGE = "[ERROR] 숫자는 1에서 45 사이어야 합니다.";
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
+        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        checkNumberRange(bonusNumber);
+    }
+
+    private void checkNumberRange(int bonusNumber) {
+        if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE_ERROR_MESSAGE);
+        }
     }
 
 }

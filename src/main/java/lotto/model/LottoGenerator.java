8fe0a958e38e179501b9e9d4b4facd795f946e25
heiler <threepebbles@naver.com>
2023-11-05 21:04:58 +0900

    Comment: generateLottoTickets 설명 수정

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -8,6 +8,6 @@ public interface LottoGenerator {
     // 로또 1개를 만들어서 반환
     Lotto generate();
 
-    // 금액만큼 로또를 만들어 반환
-    List<Lotto> generateLottoTickets(int budget);
+    // quantity개수만큼 로또를 만들어 반환
+    List<Lotto> generateLottoTickets(int quantity);
 }

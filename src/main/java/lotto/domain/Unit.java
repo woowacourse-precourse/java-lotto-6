208b4074f1441277d6f5fb0395fb5ba1945d0789
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-06 08:49:55 +0900

    refactor - 메서드 분리, 메서드명 변경

--- src/main/java/lotto/domain/Unit.java
+++ src/main/java/lotto/domain/Unit.java
@@ -3,18 +3,26 @@ package lotto.domain;
 public enum Unit {
 
     PURCHASE_UNIT(1000),
-
     ZERO(0);
+
     private final int unit;
 
     Unit(int unit) {
         this.unit = unit;
     }
 
-    public static int getPurchaseNumber(int purchasePrice) {
-        if (purchasePrice % PURCHASE_UNIT.unit != 0) {
-            throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해야 합니다.");
-        }
+    public static int calculateLottoTicketCanPurchase(int purchasePrice) {
+        validatePurchasePrice(purchasePrice);
         return purchasePrice / PURCHASE_UNIT.unit;
+    }
+
+    private static void validatePurchasePrice(int purchasePrice) {
+        if (purchasePrice % PURCHASE_UNIT.unit != ZERO.unit) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    public int getValue() {
+        return unit;
     }
 }

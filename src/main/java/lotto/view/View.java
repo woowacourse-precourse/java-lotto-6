67af17e5eb1ea6a243de7fbf233312570d38dccb
huhdy32 <mong3268@gmail.com>
2023-11-05 15:39:43 +0900

    style: View 인터페이스 함수별 띄어쓰기

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,13 +1,18 @@
 package lotto.view;
+
 import java.util.List;
 
 public interface View {
     String getPurchaseAmount();
+
     String getWinningNumbers();
+
     String getBonusNumber();
 
     void printAllLotto(List lottos);
+
     void printWinningDetails(List scores);
+
     void printTotalReturn(float totalReturn);
 
     void printError(String errorMessage);

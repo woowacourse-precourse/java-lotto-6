c949aacddf802cda37a8f239898c5f63daceeb76
devhoya97 <diajbh123@gmail.com>
2023-11-05 20:16:14 +0900

    style(test): 패키지를 이동시키지 않은 이유 설명 및 테스트 명을 더 직관적으로 변경

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,19 +1,20 @@
 package lotto;
 
-import java.util.Arrays;
-import lotto.domain.Lotto;
-import lotto.domain.wrapper.LottoNumber;
-import lotto.utils.Prize;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.wrapper.LottoNumber;
+import lotto.utils.LottoConstant;
+import lotto.utils.Prize;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+// domain 패키지 밑에 있는 것이 자연스럽지만 LottoTest의 패키지 이동에 대한 설명이 없으므로 그대로 두었습니다.
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -25,20 +26,8 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("로또가 가진 번호들을 의도한 포멧에 맞게 반환한다.")
-    @Test
-    void getLottoPrintFormat() {
-        // given
-        Lotto lotto = new Lotto(List.of(35,20,9,8,13,5));
-        // when
-        String lottoPrintFormat = lotto.toString();
-        // then
-        assertThat(lottoPrintFormat).isEqualTo("[5, 8, 9, 13, 20, 35]");
     }
 
     @DisplayName("당첨 번호와 로또 번호를 비교하여 등수를 반환한다.")
@@ -53,15 +42,26 @@ class LottoTest {
     }, delimiter = ':')
     void getPrize(String inputNumbers, String expectedPrize) {
         // given
-        List<Integer> numbers = Arrays.stream(inputNumbers.split(","))
+        List<Integer> numbers = Arrays.stream(inputNumbers.split(LottoConstant.DELIMITER.get()))
                 .map(Integer::parseInt)
                 .toList();
         Lotto lotto = new Lotto(numbers);
-        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         LottoNumber bonusNumber = new LottoNumber(7);
         // when
         Prize prize = lotto.getPrize(winningLotto, bonusNumber);
         // then
         assertThat(prize.name()).isEqualTo(expectedPrize);
+    }
+
+    @DisplayName("로또가 가진 번호들을 오름차순 정렬, 쉼표로 구분, 대괄호로 감싼 뒤 반환한다.")
+    @Test
+    void getLottoPrintFormat() {
+        // given
+        Lotto lotto = new Lotto(List.of(35, 20, 9, 8, 13, 5));
+        // when
+        String lottoPrintFormat = lotto.toString();
+        // then
+        assertThat(lottoPrintFormat).isEqualTo("[5, 8, 9, 13, 20, 35]");
     }
 }

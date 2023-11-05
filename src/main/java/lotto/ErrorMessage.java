b9ac0cea604c8511afbe8c049c030ac3f092880d
nhlee98 <e5slnh9876@gmail.com>
2023-11-05 17:03:29 +0900

    refactor: 로또 검증 로직을 하나로 통합
    로또의 숫자가 중복되는지 검증하는 로직과 6개의 숫자인지 검증하는 로직을 하나로 통합한다.

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -2,8 +2,7 @@ package lotto;
 
 public enum ErrorMessage {
     LOTTO_NUMBER_RANGE_ERROR("로또 번호는 %d부터 %d 사이의 숫자여야 합니다."),
-    LOTTO_LENGTH_ERROR("로또 번호는 %d개여야 합니다."),
-    LOTTO_DUPLICATE_ERROR("로또 번호에는 중복된 숫자가 있을 수 없습니다.");
+    LOTTO_LENGTH_ERROR("로또 번호는 중복되지 않은 %d개의 수여야 합니다.");
 
     public static final String HEADER = "[ERROR]";
 
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
 import static lotto.ErrorMessage.LOTTO_LENGTH_ERROR;
-import static lotto.ErrorMessage.LOTTO_DUPLICATE_ERROR;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public final class Lotto {
     private static final int LOTTO_LENGTH = 6;
@@ -12,34 +12,19 @@ public final class Lotto {
     private final Set<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
-        List<LottoNumber> lottoNumbers = numbers.stream()
+        this(numbers.stream()
                 .map(LottoNumber::valueOf)
-                .toList();
-
-        validate(lottoNumbers);
-        this.numbers = Set.copyOf(lottoNumbers);
+                .collect(Collectors.toSet()));
     }
 
-    private void validate(List<LottoNumber> numbers) {
-        validateLength(numbers);
-        validateDuplicate(numbers);
+    private Lotto(Set<LottoNumber> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
     }
 
-    private void validateLength(List<LottoNumber> numbers) {
-        if (numbers.size() != LOTTO_LENGTH) {
+    private void validate(Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_LENGTH) {
             throw new IllegalArgumentException(String.format(LOTTO_LENGTH_ERROR.getMessage(), LOTTO_LENGTH));
         }
-    }
-
-    private void validateDuplicate(List<LottoNumber> numbers) {
-        if (numbers.size() != countDistinctNumbers(numbers)) {
-            throw new IllegalArgumentException(LOTTO_DUPLICATE_ERROR.getMessage());
-        }
-    }
-
-    private int countDistinctNumbers(List<LottoNumber> numbers) {
-        return (int) numbers.stream()
-                .distinct()
-                .count();
     }
 }

a3ea9c9c86841054e67d48600efdd51b3cae1bd4
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 02:24:45 +0900

    fix: private를 public으로 수정

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -1,2 +1,8 @@
-package lotto.utils;public class Constants {
+package lotto.utils;
+
+public class Constants {
+    public static final int CNT_LOTTO_NUMBER = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final String DELIMETER = ",";
 }

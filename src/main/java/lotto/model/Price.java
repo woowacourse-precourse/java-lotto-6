f284e76493846628fb8ee26cc6e89a4b4453c44d
hYe807n <rlagkdus55@gmail.com>
2023-11-05 23:28:49 +0900

    Feat: 맞춘 score를 통해 Price 객체 반환

--- src/main/java/lotto/model/Price.java
+++ src/main/java/lotto/model/Price.java
@@ -1,27 +1,40 @@
 package lotto.model;
 
-import java.util.StringJoiner;
-
 public enum Price {
-     FIRST("6개 일치", "(2,000,000,000원)"),
-     SECOND("5개 일치, 보너스 볼 일치 ", "(30,000,000원)"),
-     THIRD("5개 일치", "(1,500,000원)"),
-     FORTH("4개 일치", "(50,000원)"),
-     FIFTH("3개 일치", "(5,000원)");
+    FIRST(6, "6개 일치 ", 2000000000),
+    SECOND(5, "5개 일치, 보너스 볼 일치 ", 30000000),
+    THIRD(5, "5개 일치 ", 1500000),
+    FORTH(4, "4개 일치 ", 50000),
+    FIFTH(3, "3개 일치 ", 5000);
 
-     private String guideline;
-     private String reward;
+    private final String guideline;
+    private final int reward;
+    private final int score;
 
-     private Price(String guideline, String reward) {
-          this.guideline = guideline;
-          this.reward = reward;
-     }
+    Price(int score, String guideline, int reward) {
+        this.guideline = guideline;
+        this.reward = reward;
+        this.score = score;
+    }
 
-     public String getGuideline(Price price) {
-           return price.guideline;
-     }
+    public String getGuideline() {
+        return this.guideline;
+    }
 
-     public String getReward(Price price) {
-          return price.reward;
-     }
+    public int getReward() {
+        return reward;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public static Price findByValue(int score) {
+        for (Price type : Price.values()) {
+            if (type.getScore() == score) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 }

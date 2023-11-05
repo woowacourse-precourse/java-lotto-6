164ba04c20280ab87ec5e388419c3c621e2fb0a0
newh08 <newh08@gmail.com>
2023-11-05 09:57:15 +0900

    ♻️ REFACTOR: Application 개선, LottoController 개선에 따른 변경.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,12 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.util.ModelAndViewConverter;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController(new ModelAndViewConverter(),
-                new InputView(),new OutputView());
+        LottoController lottoController = new LottoController(new InputView(), new OutputView());
         lottoController.doLottoGame();
     }
 }

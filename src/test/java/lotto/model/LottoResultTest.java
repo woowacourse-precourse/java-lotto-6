69b8cc0ea0cfb73bf0760b8a13578828073d86c6
YoonbeomSo <ybum1224@naver.com>
2023-11-05 20:38:23 +0900

    test: 당첨 통계 출력 기능 테스트

---
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoResultTest {
+
+    @DisplayName("3등 당첨 내역이 아닌 경우 false 를 반환해야함.")
+    @Test
+    void THIRD_LANK_당첨_내역_확인_테스트() {
+        LottoResult lottoResult = new LottoResult(LottoRank.FIRST_RANK, 3);
+        assertThat(lottoResult.isLottoThirdRank()).isFalse();
+    }
+
+}

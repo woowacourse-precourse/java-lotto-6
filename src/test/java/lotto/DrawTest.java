14f3791f8ccdc8d8376f9a5a2ea80bb6292f64fd
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-05 20:56:32 +0900

    test : 입력 번호의 숫자 여부 검증 테스트

--- src/test/java/lotto/DrawTest.java
+++ src/test/java/lotto/DrawTest.java
@@ -3,7 +3,12 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class DrawTest {
+
+    Draw draw = new Draw();
 
     @Test
     void 문자열_분리_테스트() {
@@ -16,5 +21,18 @@ public class DrawTest {
 
         //then
         Assertions.assertThat(result).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    void 입력값이_숫자가_아니면_예외_발생() {
+        assertThatThrownBy(() -> draw.isInt("a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+
+    @Test
+    void 입력값이_숫자면_예외_발생_X() {
+        assertThatNoException()
+                .isThrownBy(() -> draw.isInt("45"));
     }
 }

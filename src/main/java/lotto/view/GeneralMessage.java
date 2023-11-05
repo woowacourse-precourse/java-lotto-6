5c6839e57149067ecdf7d5007bb5818b0c3fa91f
Gyeongho Yang <chchch1213@naver.com>
2023-11-05 19:27:11 +0900

    feat(view): 구입 금액 입력 질문 상수 추가

---
+++ src/main/java/lotto/view/GeneralMessage.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+public enum GeneralMessage {
+    ASK_BUY_PRICE("구입금액을 입력해 주세요.");
+
+    private final String message;
+
+    GeneralMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

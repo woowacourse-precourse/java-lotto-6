c844cf1484276834ce75397feaa19ce6c451b054
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-05 23:53:11 +0900

    feat: 당첨 번호 입력 받는 함구 수현

--- src/main/java/lotto/service/LottoServiceImpl.java
+++ src/main/java/lotto/service/LottoServiceImpl.java
@@ -1,14 +1,15 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lotto.domain.Lotto;
 import lotto.util.LottoVO;
+import lotto.util.MatchType;
 import lotto.util.UiVO;
-import lotto.util.UiVO.MatchType;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoServiceImpl implements LottoService {
@@ -20,11 +21,12 @@ public class LottoServiceImpl implements LottoService {
      */
     @Override
     public int calculatePurchaseCount() {
+
         while (true) {
             try {
-                String inputPurchaseAmount = Console.readLine();
+                String purchaseAmountText = Console.readLine();
 
-                int amount = stringToInt(inputPurchaseAmount);
+                int amount = stringToInt(purchaseAmountText);
 
                 validatePurchaseAmount(amount);
 
@@ -55,7 +57,20 @@ public class LottoServiceImpl implements LottoService {
      */
     @Override
     public List<Integer> inputWinningNumbers() {
-        return null;
+
+        while (true) {
+            try {
+                String inputText = Console.readLine();
+                List<Integer> winningNumbers = convertStringToNumbers(inputText);
+
+                validateNumberCount(winningNumbers);
+                validateEachNumber(winningNumbers);
+
+                return winningNumbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     /**
@@ -105,7 +120,7 @@ public class LottoServiceImpl implements LottoService {
 
     private void validatePurchaseAmount(int amount) {
 
-        if (!isNonNegative(amount)) {
+        if (!isGreaterThanThousand(amount)) {
             throw new IllegalArgumentException(UiVO.getPurchaseAmountInputMinException());
         }
 
@@ -118,8 +133,8 @@ public class LottoServiceImpl implements LottoService {
         return amount % 1000 == 0;
     }
 
-    private boolean isNonNegative(int amount) {
-        return amount > 0;
+    private boolean isGreaterThanThousand(int amount) {
+        return amount >= 1000;
     }
 
     private int calculateLottoCount(int amount) {
@@ -129,8 +144,45 @@ public class LottoServiceImpl implements LottoService {
     }
 
     private Lotto generateLottoNumbers() {
+
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
         return new Lotto(numbers);
+    }
+
+    private List<Integer> convertStringToNumbers(String inputText) {
+
+        try {
+            String[] numberStrings = removeSpaces(inputText).split(",");
+            return Arrays.stream(numberStrings)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new IllegalArgumentException(UiVO.getWinningNumberFormatException());
+        }
+    }
+
+    private String removeSpaces(String text) {
+        return text.replaceAll(" ", "");
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
+
+        if (numbers.size() != LottoVO.getLottoNumberCount()) {
+            throw new IllegalArgumentException(UiVO.getWinningNumberCountException());
+        }
+    }
+
+    private void validateEachNumber(List<Integer> numbers) {
+
+        for (Integer number : numbers) {
+            if (!isInRange(number)) {
+                throw new IllegalArgumentException(UiVO.getLottoNumberException());
+            }
+        }
+    }
+
+    private boolean isInRange(int number) {
+        return (number >= LottoVO.getLottoMinValue()) && (number <= LottoVO.getLottoMaxValue());
     }
 }

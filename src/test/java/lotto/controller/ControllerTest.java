e4ad1434202fdc0e6e68660412c630c686cfbb89
wodnj5 <jw991211@gmail.com>
2023-11-05 19:20:58 +0900

    test: 예제 테스트 추가

--- src/test/java/lotto/controller/ControllerTest.java
+++ src/test/java/lotto/controller/ControllerTest.java
@@ -8,9 +8,9 @@ import java.util.List;
 import lotto.Application;
 import org.junit.jupiter.api.Test;
 
-public class controllerTest extends NsTest {
+public class ControllerTest extends NsTest {
     @Test
-    void 기능_테스트() {
+    void controllerTest1() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
                     run("2000", "1,2,3,4,5,6", "45");
@@ -27,6 +27,31 @@ public class controllerTest extends NsTest {
                 },
                 List.of(1, 2, 3, 4, 5, 43),
                 List.of(1, 2, 3, 4, 5, 45)
+        );
+    }
+
+    @Test
+    void controllerTest2() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("4000", "1,2,3,5,6,9", "24");
+                    assertThat(output()).contains(
+                            "4개를 구매했습니다.",
+                            "[1, 3, 5, 6, 9, 24]",
+                            "[2, 4, 6, 8, 10, 12]",
+                            "[3, 6, 9, 12, 15, 18]",
+                            "[4, 8, 12, 16, 20, 24]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(1, 3, 5, 6, 9, 24),
+                List.of(2, 4, 6, 8, 10, 12),
+                List.of(3, 6, 9, 12, 15, 18),
+                List.of(4, 8, 12, 16, 20, 24)
         );
     }
 

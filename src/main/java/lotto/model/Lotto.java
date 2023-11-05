0a74d3badadf7dd7e9c646e474c8534d81676cc4
peppone-choi <peppone.choi@gmail.com>
2023-11-05 21:30:44 +0900

    refactor: Lotto 객체 내 멤버변수 원시값 포장 객체인 LottoNumbers 및 일급 콜렉션 LottoList 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,82 +1,24 @@
 package lotto.model;
 
-import static lotto.enumerate.ConfigInteger.LOTTO_END_NUMBER;
-import static lotto.enumerate.ConfigInteger.LOTTO_NUMBER_COUNT;
-import static lotto.enumerate.ConfigInteger.LOTTO_PRICE;
-import static lotto.enumerate.ConfigInteger.LOTTO_START_NUMBER;
-import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_DUPLICATE;
-import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_OVER_OR_UNDER_SIZE;
-import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_UNDER_OR_OVER;
-import static lotto.util.ExceptionCodeThrow.exceptionCodeThrow;
-
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
 import lotto.record.LottoNumberRecord;
 
 public class Lotto {
-    private final List<Integer> numbers; // TODO : NumbersList 일급 컬렉션 구현
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    private final LottoNumbers numbers; // TODO : NumbersList 일급 컬렉션 구현
+
+    public Lotto(LottoNumbers numbers) {
         this.numbers = numbers;
     }
 
     public LottoNumberRecord toRecord() {
-        return new LottoNumberRecord(numbers);
+        return new LottoNumberRecord(numbers.getLottoNumbers());
     }
 
     public String makeLottoNumberString() {
         return numbers
-                .stream().sorted()
+                .getLottoNumbers().stream()
+                .sorted()
                 .toList()
                 .toString();
     }
 
-    private static void getLottoNumber(ArrayList<Lotto> list) {
-        List<Integer> numbers = Randoms
-                .pickUniqueNumbersInRange(LOTTO_START_NUMBER.getInt(), LOTTO_END_NUMBER.getInt(),
-                        LOTTO_NUMBER_COUNT.getInt());
-        list.add(new Lotto(numbers));
-    }
-
-    private void getLottoList(long lottoVolume, ArrayList<Lotto> list) {
-        for (int i = 0; i < lottoVolume; i++) {
-            getLottoNumber(list);
-        }
-    }
-
-    private ArrayList<Lotto> getLottos(Cash amountCash) {
-        ArrayList<Lotto> list = new ArrayList<>();
-        long lottoVolume = amountCash.cash() / LOTTO_PRICE.getInt();
-        getLottoList(lottoVolume, list);
-        return list;
-    }
-
-    public List<Lotto> buyLotto(Cash amountCash) {
-        return getLottos(amountCash);
-    }
-
-    private void validate(List<Integer> numbers) {
-        lottoNumberSizeValidate(numbers);
-        LottoNumberDuplicateValidate(numbers);
-        lottoNumberUnderOverValidate(numbers);
-    }
-
-    private void lottoNumberUnderOverValidate(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(number -> number < LOTTO_START_NUMBER.getInt() || number > LOTTO_END_NUMBER.getInt())) {
-            exceptionCodeThrow(LOTTO_NUMBER_UNDER_OR_OVER);
-        }
-    }
-
-    private void LottoNumberDuplicateValidate(List<Integer> numbers) {
-        if (numbers.size() != numbers.stream().distinct().count()) {
-            exceptionCodeThrow(LOTTO_NUMBER_DUPLICATE);
-        }
-    }
-
-    private void lottoNumberSizeValidate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBER_COUNT.getInt()) {
-            exceptionCodeThrow(LOTTO_NUMBER_OVER_OR_UNDER_SIZE);
-        }
-    }
 }
---
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -0,0 +1,49 @@
+package lotto.model;
+
+import static lotto.enumerate.ConfigInteger.LOTTO_END_NUMBER;
+import static lotto.enumerate.ConfigInteger.LOTTO_NUMBER_COUNT;
+import static lotto.enumerate.ConfigInteger.LOTTO_START_NUMBER;
+import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_DUPLICATE;
+import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_OVER_OR_UNDER_SIZE;
+import static lotto.enumerate.ErrorCode.LOTTO_NUMBER_UNDER_OR_OVER;
+import static lotto.util.ExceptionCodeThrow.exceptionCodeThrow;
+
+import java.util.List;
+
+public class LottoNumbers {
+    private final List<Integer> lottoNumbers;
+
+    public LottoNumbers(List<Integer> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        lottoNumberSizeValidate(numbers);
+        LottoNumberDuplicateValidate(numbers);
+        lottoNumberUnderOverValidate(numbers);
+    }
+
+
+    private void lottoNumberUnderOverValidate(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < LOTTO_START_NUMBER.getInt() || number > LOTTO_END_NUMBER.getInt())) {
+            exceptionCodeThrow(LOTTO_NUMBER_UNDER_OR_OVER);
+        }
+    }
+
+    private void LottoNumberDuplicateValidate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            exceptionCodeThrow(LOTTO_NUMBER_DUPLICATE);
+        }
+    }
+
+    private void lottoNumberSizeValidate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT.getInt()) {
+            exceptionCodeThrow(LOTTO_NUMBER_OVER_OR_UNDER_SIZE);
+        }
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+}

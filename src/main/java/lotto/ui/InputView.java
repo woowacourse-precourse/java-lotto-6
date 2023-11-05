956719fe5edbf6e470ddcddad5e440d86e45b25f
aammddkkzxc <ideal0427@naver.com>
2023-11-05 21:31:57 +0900

    feat: 구입 금액 입력 기능 구현

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,8 +1,13 @@
 package lotto.ui;
 
-public class InputView {
-    public void inputPurchaseMoney() {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+    public static final String PURCHASE_MONEY_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public String inputPurchaseMoney() {
+        System.out.println(PURCHASE_MONEY_REQUEST_MESSAGE);
+        return Console.readLine();
     }
 
     public void inputWinningNumber() {

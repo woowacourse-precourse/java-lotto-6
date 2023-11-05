ec729c1a3e73721cb436b291a80180bea6ed8a53
YuJeongYun <yunyj99@naver.com>
2023-11-05 22:57:09 +0900

    feat: 당첨 번호와 보너스 번호 담고 있는 LottoWinning 클래스 추가

---
+++ src/main/java/lotto/domain/LottoWinning.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import lotto.constant.ErrorMessage;
+
+import java.util.List;
+
+/**
+ * 당첨 번호와 보너스 번호 담고 있는 클래스
+ */
+public class LottoWinning {
+    private Lotto winningNum;
+    private LottoNumber bonusNum;
+
+    public LottoWinning() {
+    }
+
+    public LottoWinning(List<Integer> winningNum, int bonusNum) {
+        validateDuplicate(winningNum, bonusNum);
+        this.winningNum = new Lotto(winningNum);
+        this.bonusNum = new LottoNumber(bonusNum);
+    }
+
+    public Lotto getWinningNum() {
+        return winningNum;
+    }
+
+    public LottoNumber getBonusNum() {
+        return bonusNum;
+    }
+
+    // 보너스 숫자가 중복된 값이면 예외 발생
+    private void validateDuplicate(List<Integer> winningNum, int bonusNum) {
+        if (winningNum.stream().anyMatch(s -> s == bonusNum)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUM.getMessage());
+        }
+    }
+}

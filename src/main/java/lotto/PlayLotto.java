46f4dd96ab5f7fe7bdd28000930a4663f65544a3
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-05 19:50:28 +0900

    feat(lotto): 구입 금액에 해당하는 로또번호 생성

--- src/main/java/lotto/PlayLotto.java
+++ src/main/java/lotto/PlayLotto.java
@@ -1,15 +1,26 @@
 package lotto;
 
-import org.junit.jupiter.params.shadow.com.univocity.parsers.common.processor.core.AbstractMasterDetailListProcessor;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class PlayLotto {
 
+    private final int MIN_LOTTO_NUMBER = 1;
+    private final int MAX_LOTTO_NUMBER = 45;
+    private final int LOTTO_NUM_CNT = 6;
+
     Money money;
+    List<List<Integer>> lottoNumbers = new ArrayList<>();
+    int lottoCnt;
 
     public void play(){
         getMoney();
+        makeLotto();
     }
 
     public void getMoney(){
@@ -19,5 +30,27 @@ public class PlayLotto {
             System.out.println(e.getMessage());
             getMoney();
         }
+    }
+
+    public void makeLotto(){
+        lottoCnt = Integer.parseInt(money.amount) / 1000;
+
+        for (int i=0; i<lottoCnt; i++){
+            lottoNumbers.add(getGeneratedRandomNumbers());
+        }
+    }
+
+    public List<Integer> getGeneratedRandomNumbers(){
+
+        List<Integer> curNumbers = new ArrayList<>();
+        while (curNumbers.size() < LOTTO_NUM_CNT){
+            int randomNumber = Randoms.pickNumberInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+            if (!curNumbers.contains(randomNumber)) {
+                curNumbers.add(randomNumber);
+            }
+        }
+
+        Collections.sort(curNumbers);
+        return curNumbers;
     }
 }

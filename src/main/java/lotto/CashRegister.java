dd557406e0d6cb6baa887f2bdf42590c21bb39ec
김민정 <mjeong@sookmyung.ac.kr>
2023-11-05 23:16:40 +0900

    refactor: 구입 금액을 추적하기 위한 amount 변수 추가

--- src/main/java/lotto/CashRegister.java
+++ src/main/java/lotto/CashRegister.java
@@ -1,11 +1,17 @@
 package lotto;
 
 public class CashRegister {
-    private final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_PRICE = 1000;
+    private int amount;
 
     public int calculateLottoCount(int amount) {
         validate(amount);
+        this.amount = amount;
         return amount / LOTTO_PRICE;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 
     private void validate(int amount) {

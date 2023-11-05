7ef41296984245e44e52b359ed1e91dc26d76427
kimwanyoung <dhks2869@gmail.com>
2023-11-05 15:04:24 +0900

    refactor : 랜덤번호 생성 후 정렬하는 기능 삭제

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Comparator;
 import java.util.List;
 
 public class LottoNumberGenerator {
@@ -11,7 +10,6 @@ public class LottoNumberGenerator {
 
     public static List<Integer> generateNumber() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        lottoNumbers.sort(Comparator.naturalOrder());
         return lottoNumbers;
     }
 }

2cd0a1c8ccfd580e5a02502c4361c520b3ecb1b4
siho-song <thdtlgh234@naver.com>
2023-11-05 18:06:23 +0900

    [Feat] 당첨통계 관련 클래스 구현

---
+++ src/main/java/lotto/domain/WinningStatistic.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningStatistic {
+
+    private final Map<Rank, Integer> result = new EnumMap<Rank, Integer>(Rank.class);
+
+    public WinningStatistic(List<Rank> ranks) {
+        ranks.forEach(rank -> result.put(rank, result.get(rank) + 1));
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return this.result;
+    }
+    
+}

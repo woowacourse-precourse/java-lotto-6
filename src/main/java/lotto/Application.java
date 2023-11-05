7baa27e5be22c7b50683b4035d98fd029e176337
beongjun1 <beongjun1@naver.com>
2023-11-05 18:28:40 +0900

    feat:입력 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final Integer lottoPrice = 1000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        Integer inputMoney = InputException();
+
+
     }
+
+    private static Integer InputException() {
+        Integer inputMoney = Integer.valueOf(Console.readLine());
+        Integer money;
+        try {
+            if (inputMoney % lottoPrice != 0) {
+                throw new IllegalArgumentException("[ERROR] 입력 단위는 1,000원 단위여야 합니다.");
+            }
+            money=inputMoney;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            money=InputException();
+        }
+        return money;
+    }
+
 }

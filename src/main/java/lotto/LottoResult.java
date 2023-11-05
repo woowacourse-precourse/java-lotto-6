300de1b4704d09c679e7f3664fef89f32bb1f6e5
Haeun Jeong <gkdms4248@gmail.com>
2023-11-05 17:47:01 +0900

    feat: 수익률 계산해주는 기능 추가

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,11 +1,19 @@
 package lotto;
 
+import static lotto.LottoCriteria.FIFTH_PLACE;
+import static lotto.LottoCriteria.FIRST_PLACE;
+import static lotto.LottoCriteria.FOURTH_PLACE;
+import static lotto.LottoCriteria.SECOND_PLACE;
+import static lotto.LottoCriteria.THIRD_PLACE;
+
 import java.util.List;
+import java.util.Map;
 
 public class LottoResult {
 
     private final WinningNumber winningNumber;
     private final List<Lotto> issuedLotto;
+    private Map<String, Long> result;
 
     public LottoResult(final WinningNumber winningNumber, final List<Lotto> issuedLotto) {
         this.winningNumber = winningNumber;
@@ -13,6 +21,17 @@ public class LottoResult {
     }
 
     public long matchingCount(Lotto lotto) {
-        return lotto.getNumbers().stream().filter(winningNumber.lotto().getNumbers()::contains).count();
+        return lotto.getNumbers().stream()
+                .filter(winningNumber.lotto().getNumbers()::contains)
+                .count();
+    }
+
+    public float returnCalculation(final Map<String, Long> result, Long price) {
+        Long totalAmount = result.get(FIFTH_PLACE.getPlace()) * FIFTH_PLACE.getAmount()
+                + result.get(FOURTH_PLACE.getPlace()) * FOURTH_PLACE.getAmount()
+                + result.get(THIRD_PLACE.getPlace()) * THIRD_PLACE.getAmount()
+                + result.get(SECOND_PLACE.getPlace()) * SECOND_PLACE.getAmount()
+                + result.get(FIRST_PLACE.getPlace()) * FIRST_PLACE.getAmount();
+        return (float) totalAmount / price * 100;
     }
 }

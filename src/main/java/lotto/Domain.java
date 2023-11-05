e2194b2bea464468fdcd274b61bdf8566e4b89e5
hwangkyojune <shane5969@naver.com>
2023-11-05 22:10:51 +0900

    Domain 구현

--- src/main/java/lotto/Domain.java
+++ src/main/java/lotto/Domain.java
@@ -1,21 +1,44 @@
 package lotto;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Domain {
-    public Rank ranking(){
-        return null;
+    static private final int DECIMAL_POINT = 1;
+    public Rank ranking(List<Integer> winningNums,Integer bonus,Lotto lotto){
+        int hit = (int)lotto.getNumbers().stream()
+                .filter(num-> winningNums.contains(num))
+                .count();
+
+        int bonusHit = 0;
+        if(hit == 5){
+            if(lotto.getNumbers().contains(bonus)){
+                bonusHit++;
+            }
+        }
+        Rank rank = Rank.getRank(hit,bonusHit);
+        return rank;
     }
 
-    public List<Rank> allRanking(){
-        return null;
+    public List<Rank> allRanking(List<Integer> winningNums,Integer bonus,List<Lotto> lottoes){
+        return lottoes.stream()
+                .map(lotto -> ranking(winningNums,bonus,lotto))
+                .collect(Collectors.toList());
     }
 
-    public int calculateProfit(){
-        return 0;
+    public int calculateProfit(List<Rank> rankingResults){
+        return rankingResults.stream()
+                .mapToInt(Rank::getPrizeMoney)
+                .sum();
     }
 
-    public int calculateProfitRate(){
-        return 0;
+    public double calculatePercent(long money, long profit){
+        double percent = (double)profit/money*100;
+        BigDecimal bigDecimal = new BigDecimal(percent);
+        double roundedPercent = bigDecimal
+                .setScale(DECIMAL_POINT, RoundingMode.HALF_UP).doubleValue();
+        return roundedPercent;
     }
 }

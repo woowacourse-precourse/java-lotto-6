c6984a94fe60e6b575f0e05a110192849a4ee501
hoiae <donald819@naver.com>
2023-11-06 00:26:13 +0900

    feat: Lotto클래스에 필요한 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.ErrorMessages;
 
 public class Lotto {
@@ -12,10 +16,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> NumbersWithDuplicatesRemove = new HashSet<>(numbers);
+        if (NumbersWithDuplicatesRemove.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessages.NOT_A_MULTIPLE_OF_1000.getMessage());
+        }
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorMessages.NOT_SIX_NUMBERS.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+    public void printLottoNumbers() {
+        System.out.println(this.numbers);
+    }
+
+    public int compareWithTheCorrectAnswer(List<Integer> answer) {
+        int matchedNumberCount = 0;
+        for (int i = 0; i < 6; i++) {
+            if (answer.contains(this.numbers.get(i))) {
+                matchedNumberCount++;
+            }
+        }
+        return matchedNumberCount;
+    }
+
+    public boolean compareWithBonusNumber(int bonus){
+        return this.numbers.contains(bonus);
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +27,31 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호를 출력하는 지 확인한다.")
+    @Test
+    void printLottoNumbers(){
+        //return타입이 void이며, console에 출력되는 내용을 확인하기 위한 설정
+        PrintStream standardOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        new Lotto(List.of(1,2,3,4,5,6)).printLottoNumbers();
+        String result = outputStream.toString();
+        String expected = "[1, 2, 3, 4, 5, 6]" + System.lineSeparator();
+        assertThat(result).isEqualTo(expected);
+
+        System.setOut(standardOut);
+    }
+
+    @DisplayName("당첨번호와 비교하여 일치하는 숫자의 개수를 반환한다.")
+    @Test
+    void compareWithTheCorrectAnswerTest(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        List<Integer> answer = List.of(1,2,3,4,5,6);
+
+        //3개일치
+        assertThat(new Lotto(List.of(4,5,6,7,8,9)).compareWithTheCorrectAnswer(answer)).isEqualTo(3);
+        //4개일치
+        assertThat(new Lotto(List.of(3,4,5,6,7,8)).compareWithTheCorrectAnswer(answer)).isEqualTo(4);
+    }
 }

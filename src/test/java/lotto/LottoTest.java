00e60a40eb2f9bda326e22dee9466607a1e2c7fd
huhdy32 <mong3268@gmail.com>
2023-11-05 21:22:32 +0900

    test(LottoTest): getScore() 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,12 @@ package lotto;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -28,9 +32,26 @@ class LottoTest {
     @Test
     @DisplayName("로또 번호를 정확히 출력하는지 확인한다.")
     void printLottoNumber() {
-        assertThat(new Lotto(List.of(1,2,3,4,5,6)).toString())
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6)).toString())
                 .isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @ParameterizedTest
+    @MethodSource("provide_getScore_TestData")
+    @DisplayName("점수를 제대로 리턴하는지 확인")
+    void provideWinningNumber(List<Integer> winningNumbers, int bonusNumber, int[] expectedResult) {
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6))
+                .getScore(winningNumbers, bonusNumber))
+                .contains(expectedResult);
+    }
+
+    private static Stream<Arguments> provide_getScore_TestData() {
+        return Stream.of(
+                Arguments.of(List.of(1,2,3,4,5,6),7,new int[] {6, 0}),
+                Arguments.of(List.of(1,2,3,4,5,10),6,new int[] {5, 1}),
+                Arguments.of(List.of(1,2,3,4,5,10),45,new int[] {5, 0})
+
+        );
+    }
+
 }

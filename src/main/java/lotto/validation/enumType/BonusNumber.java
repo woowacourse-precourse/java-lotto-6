37d6dc3673c52735cd3af5edefc51c5df27abdf3
acs0209 <annjae4676@naver.com>
2023-11-06 00:27:12 +0900

    feat: BonusNumber의 상수 구현

---
+++ src/main/java/lotto/validation/enumType/BonusNumber.java
@@ -0,0 +1,16 @@
+package lotto.validation.enumType;
+
+public enum BonusNumber {
+
+    DUPLICATION_BONUS_NUMBER("[ERROR] 보너스 번호와 당첨 번호는 중복 될 수 없습니다.");
+
+    private String message;
+
+    BonusNumber(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

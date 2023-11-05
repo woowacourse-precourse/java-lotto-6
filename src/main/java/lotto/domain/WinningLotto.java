33c5dbac6df0d7ff7a65572e37e5293b0811694b
maark1106 <mark1106@naver.com>
2023-11-05 12:25:06 +0900

    feat : 입력받은 보너스 숫자 검증하는 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,12 +4,32 @@ import java.util.List;
 
 public class WinningLotto {
 
-    private final Lotto winningLotto;
+    private final Lotto answerLotto;
     private final int bonusNumber;
 
-    public WinningLotto(Lotto winningLotto, int bonusNumber) {
-        this.winningLotto = winningLotto;
+    public WinningLotto(Lotto answerLotto, int bonusNumber) {
+        this.answerLotto = answerLotto;
+        validateBonus(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonus(int bonusNumber) {
+        validateCorrectRange(bonusNumber);
+        validateDuplicateNumber(bonusNumber);
+    }
+
+    private void validateDuplicateNumber(int bonusNumber) {
+        if (answerLotto.getNumbers()
+                .stream()
+                .anyMatch(number -> number == bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] : 보너스 숫자는 당첨 번호와 중복될 수 없습니다");
+        }
+    }
+
+    private void validateCorrectRange(int bonusNumber) {
+        if (bonusNumber < 1 && bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] : 숫자가 1에서 45 사이의 범위를 벗어납니다.");
+        }
     }
 
 }

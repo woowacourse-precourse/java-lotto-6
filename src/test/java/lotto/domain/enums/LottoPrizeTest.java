f87070048fd5834eb7e4e82c23986b3d18192bce
gundorit <packdev937@gmail.com>
2023-11-05 11:53:35 +0900

    test (LottoPrize) : 로또 당첨에 해당하지 않는다면 낙첨을 반환하는지 테스트 한다

--- src/test/java/lotto/domain/enums/LottoPrizeTest.java
+++ src/test/java/lotto/domain/enums/LottoPrizeTest.java
@@ -10,11 +10,19 @@ class LottoPrizeTest {
 
     @DisplayName("일치 번호에 따라 정확한 상금을 찾아 반환한다.")
     @Test
-    void testFindLottoPrize() {
+    void returnLottoPrizeIfNumbersMatched() {
         assertEquals(LottoPrize.FIFTH, LottoPrize.findLottoPrize(3, false));
         assertEquals(LottoPrize.FOURTH, LottoPrize.findLottoPrize(4, false));
         assertEquals(LottoPrize.THIRD, LottoPrize.findLottoPrize(5, false));
         assertEquals(LottoPrize.SECOND, LottoPrize.findLottoPrize(4, true));
         assertEquals(LottoPrize.FIRST, LottoPrize.findLottoPrize(6, false));
+    }
+
+    @DisplayName("로또 당첨에 해당하지 않는다면 낙첨을 반환한다.")
+    @Test
+    void returnLoseIfNumbersNotMatched() {
+        assertEquals(LottoPrize.LOSE, LottoPrize.findLottoPrize(2, false));
+        assertEquals(LottoPrize.LOSE, LottoPrize.findLottoPrize(1, true));
+
     }
 }

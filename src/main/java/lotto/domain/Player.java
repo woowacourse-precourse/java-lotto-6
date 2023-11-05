09c422dc2e4e7a1be74c6cb175a54add8a915def
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-05 14:48:33 +0900

    feat: 로또 구입금액 검증 메서드 작성

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -8,11 +8,15 @@ public class Player {
     private static final int LOTTO_PRICE = 1000;
     private static final int PERCENTAGE = 100;
 
+    private static final String SMALL_PURCHASE_AMOUNT_ERROR_MESSAGE = "로또 구입금액은 1,000원 이상이어야 합니다.";
+    private static final String NOT_DIVIDE_1000_ERROR_MESSAGE = "로또 구입금액은 1,000원 단위로 입력해야 합니다.";
+
+
     private int purchaseAmount;
     private List<Lotto> lottos = new ArrayList<>();
 
     public Player(int purchaseAmount) {
-        // 구입 금액 검증
+        validatePurchaseAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
         generateLottos();
     }
@@ -32,6 +36,21 @@ public class Player {
 
     public double calculateRateOfReturn(int totalPrizeMoney) {
         return ((double) totalPrizeMoney / (double) purchaseAmount) * PERCENTAGE;
+    }
+
+    public void validatePurchaseAmount(int purchaseAmount) {
+        validateLeast1000(purchaseAmount);
+        validateDivisibleBy1000(purchaseAmount);
+    }
+
+    public void validateLeast1000(int purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE)
+            throw new IllegalArgumentException(SMALL_PURCHASE_AMOUNT_ERROR_MESSAGE);
+    }
+
+    public void validateDivisibleBy1000(int purchaseAmount) {
+        if ((purchaseAmount % LOTTO_PRICE) != 0)
+            throw new IllegalArgumentException(NOT_DIVIDE_1000_ERROR_MESSAGE);
     }
 
     public List<Lotto> getLottos() {

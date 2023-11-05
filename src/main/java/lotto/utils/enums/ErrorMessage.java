686cb8a569f372cd22bfb023cdf5b1332d7ce9ab
whxogus215 <whxogus215@naver.com>
2023-11-05 11:26:05 +0900

    refactor(ErrorMessage): 숫자 -> 번호로 표현 변경
    해당 메시지가 출력될 때는 당첨 번호나 보너스 번호를 입력하는 것이기
    때문에 숫자보다는 번호라는 표현이 더 자연스럽다고 판단

--- src/main/java/lotto/utils/enums/ErrorMessage.java
+++ src/main/java/lotto/utils/enums/ErrorMessage.java
@@ -2,10 +2,10 @@ package lotto.utils.enums;
 
 public enum ErrorMessage {
     NOT_NUMBER("유효한 숫자 형식이 아닙니다. 다시 입력해 주세요."),
-    LACK_NUMBER_COUNT("숫자는 6개여야 합니다."),
+    LACK_NUMBER_COUNT("번호는 6개여야 합니다."),
     NUMBER_NOT_INRANGE("번호는 1부터 45 사이의 숫자여야 합니다."),
-    NOT_DUPLICATED("모든 숫자는 중복되지 않아야 합니다."),
-    NOT_BLANK_NUMBER("숫자를 입력해주세요.");
+    NOT_DUPLICATED("모든 번호는 중복되지 않아야 합니다."),
+    NOT_BLANK_NUMBER("번호를 입력해주세요.");
 
     private final String message;
 

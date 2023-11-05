04b6a235f9a2d0e947d6fff090cc10006c1aad4b
siho-song <thdtlgh234@naver.com>
2023-11-05 17:12:32 +0900

    [Feat] 해당 등수를 반환하는 함수 구현

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,24 +1,33 @@
 package lotto.domain;
 
-import java.util.List;
-
 public class WinningNumbers {
 
     private final LottoNumbers lottoNumbers;
-    private final BonusNumber bonusNumber;
+    private final int bonusNumber;
 
-    public WinningNumbers(LottoNumbers lottoNumbers, BonusNumber bonusNumber) {
+    public WinningNumbers(LottoNumbers lottoNumbers, int bonusNumber) {
         validateDuplicated(lottoNumbers, bonusNumber);
         this.lottoNumbers = lottoNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateDuplicated(LottoNumbers lottoNumbers, BonusNumber bonusNumber) {
-        List<Integer> numbers = lottoNumbers.getLottoNumbers();
-        int number = bonusNumber.getBonusNumber();
+    public LottoNumbers getLottoNumbers() {
+        return lottoNumbers;
+    }
 
-        if (numbers.contains(number)) {
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validateDuplicated(LottoNumbers lottoNumbers, int bonusNumber) {
+        if (lottoNumbers.getLottoNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_BONUS_NUMBER.getValue());
         }
+    }
+
+    public Rank announceRanking(Lotto lotto) {
+        int matchingNumberCounts = lotto.compareNumbersWithWinningNumber(this.lottoNumbers);
+        boolean hasBonusNumber = lotto.contains(this.bonusNumber);
+        return Rank.of(matchingNumberCounts, hasBonusNumber);
     }
 }

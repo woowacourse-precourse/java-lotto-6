ca0d4445819fbc476a2c953c645b92c6cce5bbd7
yooooonshine <hl3470@naver.com>
2023-11-05 20:19:48 +0900

    Test: 수익률 계산 테스트 구현
    수익률 계산 테스트를 구현하였다.

---
+++ src/test/java/lotto/RateOfResultTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.utility.GameUtility;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("수익률 계산 테스트")
+public class RateOfResultTest {
+    @Test
+    @DisplayName("입력 대비 수익률을 계산하는 테스트")
+    @CsvSource(value = "1000,10,100.00")
+    void 입력_대비_수익률_테스트(int winningPrize, int payment, double rateOfResult) {
+        // when, then
+        Assertions.assertEquals(GameUtility.calculateRateOfReturn(winningPrize, payment),rateOfResult);
+    }
+}

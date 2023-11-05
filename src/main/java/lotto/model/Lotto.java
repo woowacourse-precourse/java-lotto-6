04b243c0515d58b08fb5a0c0a474c298246c91f1
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 16:52:04 +0900

    simplify : 가독성을 위해 enum 상수 static import

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,17 +4,18 @@ import static lotto.constant.ErrorMessage.DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE;
 import static lotto.constant.ErrorMessage.INVALID_LOTTO_SIZE_ERROR_MESSAGE;
 import static lotto.constant.ErrorMessage.LARGER_THAN_MAX_LOTTO_NUMBER_ERROR_MESSAGE;
 import static lotto.constant.ErrorMessage.LOWER_THAN_MIN_LOTTO_NUMBER_ERROR_MESSAGE;
+import static lotto.constant.LottoInfo.LOTTO_MAX_NUMBER;
+import static lotto.constant.LottoInfo.LOTTO_MIN_NUMBER;
 import static lotto.constant.LottoInfo.LOTTO_SIZE;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import lotto.constant.LottoInfo;
 import lotto.util.NumberGenerator;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(final List<Integer> numbers) {
@@ -44,12 +45,12 @@ public class Lotto {
 
     private boolean hasLargerThanMaxNumber(final List<Integer> numbers) {
         return numbers.stream()
-                .anyMatch(number -> number > LottoInfo.LOTTO_MAX_NUMBER.getValue());
+                .anyMatch(number -> number > LOTTO_MAX_NUMBER.getValue());
     }
 
     private boolean hasLessThanMinNumber(final List<Integer> numbers) {
         return numbers.stream()
-                .anyMatch(number -> number < LottoInfo.LOTTO_MIN_NUMBER.getValue());
+                .anyMatch(number -> number < LOTTO_MIN_NUMBER.getValue());
     }
 
     public static Lotto createPlayerLotto(final NumberGenerator numberGenerator) {
@@ -58,7 +59,6 @@ public class Lotto {
 
     private static List<Integer> getPlayerLottoNumbers(final NumberGenerator numberGenerator) {
         List<Integer> randomNumbers = numberGenerator.pickNumbers();
-        randomNumbers = new ArrayList<>(randomNumbers);
         Collections.sort(randomNumbers);
 
         return randomNumbers;

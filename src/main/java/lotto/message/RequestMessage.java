ca0dd892fb0f2a818b3821326aac6c0c3a48fcac
yshong1998 <yshong1998@gmail.com>
2023-11-06 03:10:27 +0900

    Fix(RequestMessage): 출력 양식에 맞도록 줄바꿈 수정

--- src/main/java/lotto/message/RequestMessage.java
+++ src/main/java/lotto/message/RequestMessage.java
@@ -3,8 +3,8 @@ package lotto.message;
 public enum RequestMessage {
 
     REQUEST_PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
-    REQUEST_WINNING_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
-    REQUEST_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
+    REQUEST_WINNING_NUMBERS_MESSAGE("\n당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_NUMBER_MESSAGE("\n보너스 번호를 입력해 주세요.");
 
     private final String message;
 

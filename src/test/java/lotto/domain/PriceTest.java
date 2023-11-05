142d1b2aa812bc9578a0fd1585e5c12ab21d80f3
linirini <2001yerin@naver.com>
2023-11-05 19:36:50 +0900

    feat : test for exception of purchase price unit

---
+++ src/test/java/lotto/domain/PriceTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.util.ExceptionEnum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PriceTest{
+
+    @DisplayName("구입 금액이 1000원 단위가 아니면 예외 처리한다.")
+    @Test
+    void 구입_금액_단위_예외_처리(){
+        //given
+        int purchasePrice = 10100;
+
+        //when & then
+        assertThatThrownBy(() -> new Price(purchasePrice)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionEnum.INVALID_PURCHASE_PRICE_UNIT.getMessage());
+    }
+
+}

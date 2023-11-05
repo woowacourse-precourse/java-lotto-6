41c42ed88d5774a7fed6f3c9923595df19e2c5e4
jongmee <ra123221@gmail.com>
2023-11-05 16:35:58 +0900

    feat(WinningNumberRequest): 입력 받은 당첨 번호에 대한 VO 클래스 생성하기

---
+++ src/main/java/lotto/utility/vo/WinningNumberRequest.java
@@ -0,0 +1,40 @@
+package lotto.utility.vo;
+
+import static lotto.constants.ErrorMessage.NOT_INTEGER;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+
+public class WinningNumberRequest {
+    private static final String DELIMITER = ",";
+
+    private final List<Integer> numbers;
+
+    public WinningNumberRequest(String input) {
+        List<String> parsedInput = parseInput(input);
+        this.numbers = validateInteger(parsedInput);
+    }
+
+    private List<String> parseInput(String input) {
+        String[] parsedInput = input.split(DELIMITER);
+        return Arrays.stream(parsedInput)
+            .map(String::trim)
+            .collect(Collectors.toList());
+    }
+
+    private List<Integer> validateInteger(List<String> parsedInput) {
+        try {
+            return parsedInput.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER);
+        }
+    }
+
+    public Lotto convertToValidLotto() {
+        return new Lotto(numbers);
+    }
+}

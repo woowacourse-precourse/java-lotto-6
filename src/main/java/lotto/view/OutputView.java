d466f4aebb8495d873a024b632b41a3277027bba
YongHyeon <pyh4616@naver.com>
2023-11-05 19:53:07 +0900

    Feat(OutputView 클래스): 당첨 통계를 출력하는 기능을 printWinningStatistics 메서드로 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import lotto.model.Rank;
+
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -8,6 +11,13 @@ public class OutputView {
     private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
+    private static final String WINNING_STATISTICS_DIVIDER = "---";
+    private static final String FIFTH_PLACE_RESULT_MESSAGE = "3개 일치 (5,000원) - %d개\n";
+    private static final String FOURTH_PLACE_RESULT_MESSAGE = "4개 일치 (50,000원) - %d개\n";
+    private static final String THIRD_PLACE_RESULT_MESSAGE = "5개 일치 (1,500,000원) - %d개\n";
+    private static final String SECOND_PLACE_RESULT_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
+    private static final String FIRST_PLACE_RESULT_MESSAGE = "6개 일치 (2,000,000,000원) - %d개\n";
 
     public void printPurchaseAmountInputMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
@@ -29,5 +39,16 @@ public class OutputView {
 
     public void printBonusNumberInputMessage() {
         System.out.println("\n" + BONUS_NUMBER_INPUT_MESSAGE);
+    }
+
+    public void printWinningStatistics(Map<Rank, Integer> winningStatistics) {
+        System.out.println();
+        System.out.println(WINNING_STATISTICS_HEADER);
+        System.out.println(WINNING_STATISTICS_DIVIDER);
+        System.out.printf(FIFTH_PLACE_RESULT_MESSAGE, winningStatistics.get(Rank.FifthPlace));
+        System.out.printf(FOURTH_PLACE_RESULT_MESSAGE, winningStatistics.get(Rank.FourthPlace));
+        System.out.printf(THIRD_PLACE_RESULT_MESSAGE, winningStatistics.get(Rank.ThirdPlace));
+        System.out.printf(SECOND_PLACE_RESULT_MESSAGE, winningStatistics.get(Rank.SecondPlace));
+        System.out.printf(FIRST_PLACE_RESULT_MESSAGE, winningStatistics.get(Rank.FirstPlace));
     }
 }

a47d6a9cbca432d49ed67e1cc3b4210b63fd54d0
1000zoo <cjswldn99@gmail.com>
2023-11-06 02:02:46 +0900

    feat(Rank): 두 번호의 차이와, 보너스번호 포함 여부로 Rank 반환

--- src/main/java/lotto/model/LottoAnswer.java
+++ src/main/java/lotto/model/LottoAnswer.java
@@ -1,8 +1,5 @@
 package lotto.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LottoAnswer {
 
     private final Lotto answer;
@@ -14,28 +11,8 @@ public class LottoAnswer {
     }
 
     public Rank getRankOf(Lotto lotto) {
-        Set<Integer> answerSet = answer.toSet();
-        Set<Integer> lottoSet = lotto.toSet();
-
-        Set<Integer> remain = new HashSet<>(answerSet);
-        remain.removeAll(lottoSet);
-
-        if (remain.size() == 0) {
-            return Rank.FIRST;
-        }
-        if (remain.size() == 1) {
-            if (lottoSet.contains(bonusNumber.number())) {
-                return Rank.SECOND;
-            }
-            return Rank.THIRD;
-        }
-        if (remain.size() == 2) {
-            return Rank.FOURTH;
-        }
-        if (remain.size() == 3) {
-            return Rank.FIFTH;
-        }
-
-        return Rank.NONE;
+        int difference = answer.getDifferenceBetween(lotto);
+        boolean containsBonusNumber = lotto.isContainBonusNumber(bonusNumber);
+        return Rank.findBy(difference, containsBonusNumber);
     }
 }
--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,10 +1,28 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    NONE
+    FIRST(0, false),
+    SECOND(1, true),
+    THIRD(1, false),
+    FOURTH(2, false),
+    FIFTH(3, false),
+    NONE(4, false);
+
+    private final int difference;
+    private final boolean containsBonusNumber;
+
+    Rank(int difference, boolean containsBonusNumber) {
+        this.difference = difference;
+        this.containsBonusNumber = containsBonusNumber;
+    }
+
+    public static Rank findBy(int difference, boolean containsBonusNumber) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.difference == difference)
+                .filter(rank -> rank.containsBonusNumber == containsBonusNumber)
+                .findFirst()
+                .orElse(Rank.NONE);
+    }
 }

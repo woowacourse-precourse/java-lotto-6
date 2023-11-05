0159ebccc57e577a336bac3904d43bfc6ced876d
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-11-05 21:59:54 +0900

    feat(outputView) : Changes 메서드 구현

--- src/main/java/lotto/View/outputView.java
+++ src/main/java/lotto/View/outputView.java
@@ -1,7 +1,17 @@
 package lotto.View;
 
+import java.util.List;
+
 public class outputView {
     public static void displayLottoCount(int count){
         System.out.println(count+"개를 구매했습니다.");
+    }
+
+    public void displayLottoNumberList(List<Integer> numbers) {
+        System.out.print("로또 번호 리스트: ");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
     }
 }

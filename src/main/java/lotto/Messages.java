3531dffb5a6a6d203a38824f733b0ee5b225fb91
freekyr7529 <freekyr7529@gmail.com>
2023-11-05 14:20:52 +0900

    refactor: 메시지 enum 클래스로 분리
    View 내부에 존재하던 메시지를 관리하는 interface를 enum으로 수정 후 외부 클래스로 추출하였다.

---
+++ src/main/java/lotto/Messages.java
@@ -0,0 +1,22 @@
+package lotto;
+
+public enum Messages {
+    PRICE_INPUT_GUIDE("구입금액을 입력해 주세요."),
+    WINNING_NUMBER_INPUT_GUIDE("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT_GUIDE("보너스 번호를 입력해 주세요."),
+    TICKET_AMOUNT("개를 구매했습니다."),
+    RESULT_TITLE("당첨 통계\n---"),
+    AMOUNT("개"),
+    RATE_OF_RETURN_HEAD("총 수익률은 "),
+    RATE_OF_RETURN_TAIL("%입니다.");
+
+    private final String msg;
+
+    Messages(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -4,17 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class View {
-    private interface Messages {
-        String PRICE_INPUT_GUIDE = "구입금액을 입력해 주세요.";
-        String WINNING_NUMBER_INPUT_GUIDE = "당첨 번호를 입력해 주세요.";
-        String BONUS_NUMBER_INPUT_GUIDE = "보너스 번호를 입력해 주세요.";
-        String TICKET_AMOUNT = "개를 구매했습니다.";
-        String RESULT_TITLE = "당첨 통계\n---";
-        String AMOUNT = "개";
-        String RATE_OF_RETURN_HEAD = "총 수익률은 ";
-        String RATE_OF_RETURN_TAIL = "%입니다.";
-    }
-
     List<String> hitResultMessages = new ArrayList<>(){{
         add("3개 일치 (5,000원) - ");
         add("4개 일치 (50,000원) - ");
@@ -24,11 +13,11 @@ public class View {
     }};
 
     public void priceInputGuideMsg() {
-        System.out.println(Messages.PRICE_INPUT_GUIDE);
+        System.out.println(Messages.PRICE_INPUT_GUIDE.getMsg());
     }
 
     public void printLottoInfo(List<Lotto> tickets) {
-        System.out.println(tickets.size() + Messages.TICKET_AMOUNT);
+        System.out.println(tickets.size() + Messages.TICKET_AMOUNT.getMsg());
 
         for (Lotto ticket : tickets) {
             printLottoNumbers(ticket);
@@ -40,24 +29,24 @@ public class View {
     }
 
     public void winningNumberInputGuideMsg() {
-        System.out.println(Messages.WINNING_NUMBER_INPUT_GUIDE);
+        System.out.println(Messages.WINNING_NUMBER_INPUT_GUIDE.getMsg());
     }
 
     public void bonusNumberInputGuideMsg() {
-        System.out.println(Messages.BONUS_NUMBER_INPUT_GUIDE);
+        System.out.println(Messages.BONUS_NUMBER_INPUT_GUIDE.getMsg());
     }
 
     public void hitResultTitle() {
-        System.out.println(Messages.RESULT_TITLE);
+        System.out.println(Messages.RESULT_TITLE.getMsg());
     }
 
     public void printHitResult(List<Integer> result) {
         for (int idx = 0; idx < result.size(); idx++) {
-            System.out.println(hitResultMessages.get(idx) + result.get(idx) + Messages.AMOUNT);
+            System.out.println(hitResultMessages.get(idx) + result.get(idx) + Messages.AMOUNT.getMsg());
         }
     }
 
     public void printRateOfReturn(double rate) {
-        System.out.println(Messages.RATE_OF_RETURN_HEAD + rate + Messages.RATE_OF_RETURN_TAIL);
+        System.out.println(Messages.RATE_OF_RETURN_HEAD.getMsg() + rate + Messages.RATE_OF_RETURN_TAIL.getMsg());
     }
 }

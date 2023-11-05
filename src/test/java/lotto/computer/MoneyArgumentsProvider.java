981228df6124e3352e3220cfffe84e4751ad3ea5
enduf7686 <enduf768640@gmail.com>
2023-11-05 15:51:50 +0900

    test(computer): add RandomLottoGenerator test

--- src/test/java/lotto/computer/MoneyArgumentsProvider.java
+++ src/test/java/lotto/computer/MoneyArgumentsProvider.java
@@ -7,7 +7,16 @@ import org.junit.jupiter.params.provider.Arguments;
 public class MoneyArgumentsProvider {
 
     static Stream<Arguments> provideCorrectInput() {
-        return IntStream.rangeClosed(1, 100)
-                .mapToObj(Arguments::of);
+        return IntStream.rangeClosed(1, 1000)
+                .mapToObj(num ->
+                        Arguments.of(Integer.parseInt(num + "000"))
+                );
+    }
+
+    static Stream<Arguments> provideExceptionInput() {
+        return IntStream.rangeClosed(1001, 2000)
+                .mapToObj(num ->
+                        Arguments.of(Integer.parseInt(num + "000"))
+                );
     }
 }
---
+++ src/test/java/lotto/computer/RandomLottoGeneratorTest.java
@@ -0,0 +1,34 @@
+package lotto.computer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class RandomLottoGeneratorTest {
+
+    private RandomLottoGenerator randomLottoGenerator;
+
+    public RandomLottoGeneratorTest() {
+        randomLottoGenerator = new RandomLottoGenerator();
+    }
+
+    @DisplayName("RandomLottoGenerator 랜덤 로또 생성 테스트")
+    @ParameterizedTest
+    @MethodSource("lotto.computer.MoneyArgumentsProvider#provideCorrectInput")
+    void createUserLottosByCorrectInput(int money) {
+        randomLottoGenerator.setMoney(money);
+        assertThat(randomLottoGenerator.createUserLottos().lottoList().size())
+                .isEqualTo(money / 1000);
+    }
+
+    @DisplayName("RandomLottoGenerator 설정 시 로또 구입 금액이 설정한 범위 밖일 때 예외 테스트")
+    @ParameterizedTest
+    @MethodSource("lotto.computer.MoneyArgumentsProvider#provideExceptionInput")
+    void createUserLottosByExceptionInput(int money) {
+        assertThatThrownBy(() -> randomLottoGenerator.setMoney(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

72ba8097c7ed4c548db30ca159b844a9b3a689ea
정영록 <s9285s@naver.com>
2023-11-05 16:37:46 +0900

    feat : random 숫자 생성 기능 구현

---
+++ src/main/java/lotto/util/Utils.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Utils {
+
+    public List<Integer> generateRandomNumbers() {
+
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

dbc1b66ae4a196f6cf2edeff3b081d56cb44689f
JongMinCh0i <wonderland314592@gmail.com>
2023-11-05 18:18:17 +0900

    feat(LottoStore): 로또 판매 수량 계산 기능 추가

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -4,13 +4,18 @@ public class LottoStore {
 
     private static final int LOTTO_PRICE = 1000;
 
-    public void getLottoPrice(int money) {
+    public int getLottoPrice(int money) {
         validInputMoney(money);
+        return sellLotto(money);
     }
 
     private void validInputMoney(int money) {
         if (money % LOTTO_PRICE != 0 || money <= 0) {
             throw new IllegalArgumentException("[ERROR] 로또 금액은 1000원 단위로 판매할 수 있습니다.");
         }
+    }
+
+    private int sellLotto(int money) {
+        return money / LOTTO_PRICE;
     }
 }

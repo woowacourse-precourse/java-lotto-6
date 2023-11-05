c695b312d4570784b719fefebf294d098afc6890
song-jae-wan <sjo9810@gmail.com>
2023-11-05 23:11:14 +0900

    Test: 당첨된 로또 개수 반환 기능 테스트

--- src/test/java/util/LottoUtilTest.java
+++ src/test/java/util/LottoUtilTest.java
@@ -13,10 +13,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static util.LottoUtil.*;
 
 public class LottoUtilTest {
+
     @DisplayName("매입 금액에 따른 로또수 반환")
     @Test
     void countByPayment() {
         final int pay = 5000;
         assertThat(countLotto(pay)).isEqualTo(5);
+    }
+
+    @DisplayName("사용자의 로또와 당첨번호를 비교해 등수로 분류한 배열로 반환")
+    @Test
+    void countWinLottoByRank() {
+        final WinningLotto winningLotto = WinningLotto.getInstance();
+        final UserLottoVO userLottoVO = new UserLottoVO();
+        winningLotto.setNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        winningLotto.setBonusNumber(7);
+        userLottoVO.setLottoList(List.of(
+                new Lotto(List.of(1, 2, 3, 7, 8 ,9)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7))));
+
+        assertThat(Arrays.toString(countWinLotto(winningLotto, userLottoVO)))
+                .isEqualTo("[0, 0, 0, 1, 0, 0, 1, 1]");
     }
 }

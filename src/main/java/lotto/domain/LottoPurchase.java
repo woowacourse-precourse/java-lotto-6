9eb1a31bb5979b969ad93b05e613a66a5601ddcb
Jae-Young98 <ygy0102@gmail.com>
2023-11-05 18:29:18 +0900

    feat(LottoPurchase): 투입 금액 검증 기능 추가

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class LottoPurchase {
     public static List<Lotto> buyLotto(long money) {
+        validateMoney(money);
         List<Lotto> lottos = new ArrayList<>();
 
         long purchaseCount = money / 1000;
@@ -18,16 +19,34 @@ public class LottoPurchase {
         return lottos;
     }
 
-    private static Lotto makeLotto() {
-        return new Lotto(sortedNumbers(makeNumbers()));
+    private static void validateMoney(long money) {
+        if (isLackMoney(money)) {
+            throw new IllegalArgumentException("금액은 1,000원 이상 투입 되어야 합니다.");
+        }
+
+        if (isWrongMoney(money)) {
+            throw new IllegalArgumentException("금액은 1,000원 단위로 투입 되어야 합니다.");
+        }
     }
 
-    private static List<Integer> makeNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    private static boolean isLackMoney(long money) {
+        return money < 1000;
+    }
+
+    private static boolean isWrongMoney(long money) {
+        return money % 1000 != 0;
+    }
+
+    private static Lotto makeLotto() {
+        return new Lotto(sortedNumbers(makeNumbers()));
     }
 
     private static List<Integer> sortedNumbers(List<Integer> numbers) {
         Collections.sort(numbers);
         return numbers;
+    }
+
+    private static List<Integer> makeNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }

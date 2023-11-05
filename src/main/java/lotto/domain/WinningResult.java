8c73830475fc0f10feb9cc7c2826fa7619091e56
Isaac Han <isaachan233@gmail.com>
2023-11-06 01:35:34 +0900

    refactor: 생성자 및 getCount 로직 수정

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -3,21 +3,26 @@ package lotto.domain;
 import lotto.enums.Rank;
 
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 
 public class WinningResult {
+    private static final int INITIAL_NUMBER = 0;
     private final Map<Rank, Integer> winningResult;
 
     public WinningResult(LottoTickets lottoTickets, WinningLotto winningLotto) {
         winningResult = new EnumMap<>(Rank.class);
 
-        for(Lotto lotto : lottoTickets.getLottoTickets()) {
-            winningResult.put(winningLotto.match(lotto), winningResult.getOrDefault(winningLotto.match(lotto),0) + 1);
+        for (Lotto lotto : lottoTickets.getLottoTickets()) {
+            Rank rank = winningLotto.match(lotto);
+            if(rank == null) continue;
+            winningResult.put(rank, winningResult.getOrDefault(rank, 0) + 1);
         }
     }
 
-    public int getCount(String rank) {
+    public int getCount(Rank rank) {
+        if(winningResult.get(rank) == null) {
+            return INITIAL_NUMBER;
+        }
         return winningResult.get(rank);
     }
 }

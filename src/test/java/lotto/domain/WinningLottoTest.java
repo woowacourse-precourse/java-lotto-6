622924b461020f2ca451158b81712c2ce790f4a4
Lieslot <rlaxodud1999@gmail.com>
2023-11-05 12:41:25 +0900

    feat: 당첨 로또와 구매한 로또를 대조하여 당첨 등수를 산출한다.

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -14,13 +14,19 @@ public class WinningLottoTest {
     void rankCalculateTest() {
         //given
         WinningLotto winningLotto = new WinningLotto(List.of("1", "2", "3", "5", "7", "8"), "9");
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 5, 7, 9));
+        Lotto lotto3 = new Lotto(List.of(1, 2, 11, 12, 13, 14));
         //when
-        Rank rank = winningLotto.calculateRankWith(lotto);
+        Rank rank4 = winningLotto.calculateRankWith(lotto1);
+        Rank rank2 = winningLotto.calculateRankWith(lotto2);
+        Rank rankNone = winningLotto.calculateRankWith(lotto3);
 
         //then
-        Assertions.assertThat(rank).isEqualTo(Rank.FOURTH);
+        Assertions.assertThat(rank4).isEqualTo(Rank.FOURTH);
+        Assertions.assertThat(rank2).isEqualTo(Rank.SECOND);
+        Assertions.assertThat(rankNone).isEqualTo(Rank.NONE);
+
     }
 
 }

0a77ec341928a3b1dc41f1775467b4f03db453f5
jeondui <jeondui@gmail.com>
2023-11-06 03:09:23 +0900

    test(WinningNumbersTest): 에러메시지 수정 및 파라미터 올바르게 변경(lottoTickets -> lotto)하여 테스트 추가

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -8,39 +8,39 @@ import org.junit.jupiter.api.Test;
 
 class WinningNumbersTest {
 
-    private final LottoTickets lottoTickets = LottoTickets.generateTickets(5);
+    private final Lotto lotto = Lotto.create("1,2,3,4,5,6");
 
     @DisplayName("보너스 번호가 1개가 아닌 경우 예외 발생 테스트")
     @Test
     void isNotOntBonusNumber() {
 
-        assertThatThrownBy(() -> WinningNumbers.createBonusNumber(lottoTickets, "10,2"))
+        assertThatThrownBy(() -> WinningNumbers.createWinningNumbers(lotto, "10,2"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_IS_NOT_ONE_BONUS_NUMBER.get());
+                .hasMessageContaining(ErrorMessage.BONUS_NUMBER_IS_NOT_ONE.get());
     }
 
     @DisplayName("보너스 번호 입력 널값인 경우 예외 발생 테스트")
     @Test
     void validateBonusNumber() {
-        assertThatThrownBy(() -> WinningNumbers.createBonusNumber(lottoTickets, ""))
+        assertThatThrownBy(() -> WinningNumbers.createWinningNumbers(lotto, ""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_BONUS_NUMBER_NULL.get());
+                .hasMessageContaining(ErrorMessage.BONUS_NUMBER_NULL.get());
     }
 
     @DisplayName("보너스 번호에 공백 포함된 경우 예외 발생 테스트")
     @Test
     void validateBonusNumber2() {
-        assertThatThrownBy(() -> WinningNumbers.createBonusNumber(lottoTickets, " 10"))
+        assertThatThrownBy(() -> WinningNumbers.createWinningNumbers(lotto, " 10"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_BONUS_NUMBER_CONTAINS_BLANK.get());
+                .hasMessageContaining(ErrorMessage.BONUS_NUMBER_CONTAINS_BLANK.get());
     }
 
-    @DisplayName("보너스 번호에 공백만 포함된 경우 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호와 보너스 번호와 중복인 경우 예외 발생 테스트")
     @Test
-    void validateBonusNumber3() {
-        assertThatThrownBy(() -> WinningNumbers.createBonusNumber(lottoTickets, "    "))
+    void createWinningNumbers() {
+        assertThatThrownBy(() -> WinningNumbers.createWinningNumbers(lotto, "1"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_BONUS_NUMBER_CONTAINS_BLANK.get());
+                .hasMessageContaining(ErrorMessage.BONUS_NUMBER_DUPLICATE.get());
     }
 
 

2ea4009bc0dbfeafffab01bbd51c8033a28597f5
noah <jeongas9810@gmail.com>
2023-11-06 07:48:27 +0900

    feat: Add 'matchCount' field in Rank
    Fields for future output have been added to the Rank class.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,28 +3,40 @@ package lotto;
 import java.util.stream.Stream;
 
 public enum Rank {
-    NONE(-1, true, 0), FIFTH(3, false, 5000.0), FOURTH(4, false, 50000.0), THIRD(5, false, 1500000.0), SECOND(5, true, 30000000.0), FIRST(6, false, 2000000000.0);
+    NONE(-1, true, 0, 0),
+    FIFTH(3, false, 5000.0, 0),
+    FOURTH(4, false, 50000.0, 0),
+    THIRD(5, false, 1500000.0, 0),
+    SECOND(5, true, 30000000.0, 0),
+    FIRST(6, false, 2000000000.0, 0)
+    ;
 
     private final int hits;
     private final boolean isMatchBonusNumber;
     private final double prize;
+    private int matchCount;
 
-    Rank(int hits, boolean isMatchBonusNumber, double prize) {
+    Rank(int hits, boolean isMatchBonusNumber, double prize, int matchCount) {
         this.hits = hits;
         this.isMatchBonusNumber = isMatchBonusNumber;
         this.prize = prize;
+        this.matchCount = matchCount;
     }
 
     public int getHits() {
         return hits;
     }
 
-    public boolean isMatchBonusNumber() {
-        return isMatchBonusNumber;
-    }
-
     public double getPrize() {
         return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public void increaseMatchCount() {
+        this.matchCount += 1;
     }
 
     public static Rank findRank(int hits, boolean isMatchBonusNumber) {

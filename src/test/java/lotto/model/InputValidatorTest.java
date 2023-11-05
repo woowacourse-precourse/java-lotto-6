c2c528221fc63a898d8e202ec7254c25d0fbedeb
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-05 21:39:44 +0900

    test: 문자열 리스트 검증 테스트 기능 개발
    - 숫자가 아닌 다른 값 입력 검증 테스트
    - 범위 밖 숫자 검증 테스트

---
+++ src/test/java/lotto/model/InputValidatorTest.java
@@ -0,0 +1,63 @@
+package lotto.model;
+
+import static lotto.model.enums.ErrorMessage.NOT_CORRECT_INPUT_MESSAGE;
+import static lotto.model.enums.ErrorMessage.OUT_OF_RANGE_NUMBER_MESSAGE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class InputValidatorTest {
+    InputValidator inputValidator;
+
+    @BeforeEach
+    void setUp() {
+        inputValidator = new InputValidator();
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("notIntegerParameter")
+    @DisplayName("숫자가 아닐 시 예외 발생")
+    void invalidInput(String testName, List<String> input) {
+        assertThatThrownBy(() -> inputValidator.validateInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NOT_CORRECT_INPUT_MESSAGE.getMessage());
+    }
+
+    private static Stream<Arguments> notIntegerParameter() {
+        return Stream.of(
+                // null, 공백 입력 시 예외
+                Arguments.of("Null 예외 발생", List.of("1", "2", "3", "4", "")),
+                Arguments.of("공백 예외 발생", List.of("1", "2", "3", "4", " ")),
+
+                // 정수가 아닌 값 입력 시 예외
+                Arguments.of("소수 입력 예외 발생", List.of("1", "2", "3", "4", "0.5")),
+                Arguments.of("문자 입력 예외 발생", List.of("1", "2", "3", "4", "안녕")),
+                Arguments.of("특수 문자 입력 예외 발생", List.of("1", "2", "3", "4", "5 + 1"))
+        );
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("outOfRangeParameter")
+    @DisplayName("범위 밖의 숫자 예외 발생")
+    void outOfRangeInput(String testName, List<String> input) {
+        assertThatThrownBy(() -> inputValidator.validateInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(OUT_OF_RANGE_NUMBER_MESSAGE.getMessage());
+    }
+
+    private static Stream<Arguments> outOfRangeParameter() {
+        return Stream.of(
+                // 최댓값 초과 시 오류 발생
+                Arguments.of("Null 예외 발생", List.of("1", "2", "3", "4", "46")),
+
+                // 최솟값 미만 시 오류 발생
+                Arguments.of("소수 입력 예외 발생", List.of("1", "2", "3", "4", "0"))
+        );
+    }
+}

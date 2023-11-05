ede67cab76448d527aa4223d84446d662cff39b2
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-05 22:12:53 +0900

    [FEAT] bonusNumber 입력 함수 구현
    Message.InputMessage의 enum 클래스를 사용해서 보너스 번호의 입력 메세지를 출력하고, bonusNumber 값을 받는다.

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -13,4 +13,9 @@ public class InputView {
         System.out.println(InputMessage.WINNING_NUMBER);
         return Console.readLine();
     }
+
+    public static String bonusNumber() {
+        System.out.println(InputMessage.BONUS_NUMBER);
+        return Console.readLine();
+    }
 }

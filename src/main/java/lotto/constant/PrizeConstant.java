8e49b01af0bd743ef1237325c808f70ebdf325d2
Yang-soeun <soeun8636@naver.com>
2023-11-05 20:36:14 +0900

    [feat]: 상금 필드 추가, 점수로 상금 얻는 메소드 추가

--- src/main/java/lotto/constant/PrizeConstant.java
+++ src/main/java/lotto/constant/PrizeConstant.java
@@ -2,17 +2,19 @@ package lotto.constant;
 
 public enum PrizeConstant {
 
-    FIRST(7,"6개 일치 (2,000,000,000원) - "),
-    SECOND(6, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(5, "5개 일치 (1,500,000원) - "),
-    FOURTH(4, "4개 일치 (50,000원) - "),
-    FIFTH(3, "3개 일치 (5,000원) - ");
+    FIRST(7,"6개 일치 (2,000,000,000원) - ", 2000000000),
+    SECOND(6, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
+    THIRD(5, "5개 일치 (1,500,000원) - ", 1500000),
+    FOURTH(4, "4개 일치 (50,000원) - ",50000),
+    FIFTH(3, "3개 일치 (5,000원) - ",5000);
     private final int score;
     private final String message;
+    private final int reward;
 
-    PrizeConstant(int score, String message) {
+    PrizeConstant(int score, String message, int reward) {
         this.score = score;
         this.message = message;
+        this.reward = reward;
     }
 
     public static String getMessageByScore(int score) {
@@ -23,5 +25,15 @@ public enum PrizeConstant {
         }
 
         return "일치하는 메시지가 없습니다.";
+    }
+
+    public static int getRewardByScore(int score) {
+        for (PrizeConstant prize : PrizeConstant.values()) {
+            if (prize.score == score) {
+                return prize.reward;
+            }
+        }
+
+        return 0;
     }
 }

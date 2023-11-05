cca09b32d7a72067610e221cb68c98d52acd6bcd
Heejun-0219 <heejun0219@gmail.com>
2023-11-05 20:27:31 +0900

    feat(Lotto): 로또 번호 타당성 확인

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,67 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import lotto.domain.LottoNumbersValidateEnum;
+import lotto.service.MoneyValidateEnum;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortOfLotto(numbers);
     }
 
     private void validate(List<Integer> numbers) {
+        // 사이즈 검증
+        validateSize(numbers);
+        // 중복 검증
+        validateDuplicatedNumber(numbers);
+        // 범위 검증
+        validateRange(numbers);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LottoNumbersValidateEnum.LOTTO_NUMBERS_SIZE_ERROR.get());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private static void validateDuplicatedNumber(List<Integer> numbers) {
+        boolean duplicate = numbers.stream().distinct().count() != numbers.size();
+
+        if (duplicate) {
+            throw new IllegalArgumentException(LottoNumbersValidateEnum.LOTTO_NUMBERS_DUPLICATE_ERROR.get());
+        }
+    }
+
+    private static void validateRange(List<Integer> numbers) {
+        maxValidate(numbers);
+        minValidate(numbers);
+    }
+
+    private static void maxValidate(List<Integer> numbers) {
+        if (Collections.max(numbers) > 45) {
+            throw new IllegalArgumentException(LottoNumbersValidateEnum.LOTTO_NUMBERS_RANGE_ERROR.get());
+        }
+    }
+
+    private static void minValidate(List<Integer> numbers) {
+        if (Collections.min(numbers) < 1) {
+            throw new IllegalArgumentException(LottoNumbersValidateEnum.LOTTO_NUMBERS_RANGE_ERROR.get());
+        }
+    }
+
+    // 로또 오름차순 정렬
+    private static List<Integer> sortOfLotto(List<Integer> numbers) {
+        List<Integer> result = new ArrayList<Integer>(numbers);
+        Collections.sort(result);
+        return result;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

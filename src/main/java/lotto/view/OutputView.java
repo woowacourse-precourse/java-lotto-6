49552bb45fd14ede723292db331fdba72d845e4e
Mr.Kim <joajy98@gmail.com>
2023-11-05 19:05:44 +0900

    feat: 구매한 로또에 대한 정보 출력(구매 수량, 로또 번호)

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,39 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
+
+    private static final String LOTTO_NUMBER_DELIMITER = ", ";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_BRACKET = "]";
     private static final int DIVIDER_FOR_COUNT_PURCHASE_AMOUNT = 1000;
     private static final String PURCHASE_COUNT_ALERT = "개를 구매했습니다.";
+    private static final int LOTTO_NUMBERS_COUNT = 6;
+
 
 
     public static void printPurchaseAmount(int purchaseMoney){
         int purchaseAmount = purchaseMoney / DIVIDER_FOR_COUNT_PURCHASE_AMOUNT;
         System.out.println(purchaseAmount + PURCHASE_COUNT_ALERT);
+    }
+
+    public static void printAllPurchaseLotto(List<Lotto> lottoNumbers) {
+        for (Lotto purchasedNumbers : lottoNumbers) {
+            printPurchaseLotto(purchasedNumbers);
+        }
+        System.out.println();
+    }
+
+    public static void printPurchaseLotto(Lotto purchasedNumbers) {
+        System.out.print(OPEN_BRACKET);
+        List<Integer> lottoNumbers = purchasedNumbers.getLotto();
+        for (int i = 0;i < LOTTO_NUMBERS_COUNT - 1; ++i) {
+            int number = lottoNumbers.get(i);
+            System.out.print(number + LOTTO_NUMBER_DELIMITER);
+        }
+        System.out.println(lottoNumbers.get(LOTTO_NUMBERS_COUNT - 1) + CLOSE_BRACKET);
     }
 }

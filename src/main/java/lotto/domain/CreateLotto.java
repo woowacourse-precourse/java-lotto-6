3c21ca36f9d8cc73cf591e1ef4894d4a21543a6f
김두종 <2019102076@khu.ac.kr>
2023-11-06 07:38:43 +0900

    feat: 1~45 사이의 서로 다른 6개의 수로 구성된 로또 번호 생성 기능 추가 - domain package 생성 - 테스트 코드 추가

---
+++ src/main/java/lotto/domain/CreateLotto.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateLotto {
+    public Lotto create() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(randomNumbers);
+    }
+}
---
+++ src/test/java/lotto/domain/CreateLottoTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreateLottoTest {
+    private CreateLotto createLotto;
+
+    @DisplayName("랜덤한 숫자로 로또 클래스를 생성한다.")
+    @Test
+    void createLottoByOverSize() {
+        createLotto = new CreateLotto();
+        assertThat(createLotto.create()).isInstanceOf(Lotto.class);
+    }
+}

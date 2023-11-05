ddb480b589b1b93d9c61b69cb95b8c804d7e2397
yubin <dbqls2821@gmail.com>
2023-11-05 17:15:21 +0900

    feat: 구입 금액이 0 이하일 경우 던지는 메시지 상수 추가

--- src/main/java/lotto/domain/DomainException.java
+++ src/main/java/lotto/domain/DomainException.java
@@ -2,9 +2,12 @@ package lotto.domain;
 
 enum DomainException {
     ERROR("[ERROR] "),
+
     LOTTO_RANGE(ERROR.message + "로또 번호는 1에서 45 사이 숫자여야 합니다."),
     LOTTO_SIZE(ERROR.message + "로또는 6개여야 합니다."),
     LOTTO_DUPLICATION(ERROR.message + "로또번호에 중복되는 숫자가 있습니다."),
+
+    PRICE_LESS_THAN_OR_EQUAL_TO_ZERO(ERROR.message + "구입 금액이 0 이하입니다."),
     ;
 
     private final String message;

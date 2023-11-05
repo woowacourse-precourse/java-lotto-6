b0c5ed66049d287cc1eb2c26d7227ffa07add73e
mk3058 <kmk3058@icloud.com>
2023-11-05 20:31:27 +0900

    fix(LottoResult): 출력 형식 수정(개행 추가)

--- src/main/java/lotto/message/LottoResult.java
+++ src/main/java/lotto/message/LottoResult.java
@@ -1,11 +1,11 @@
 package lotto.message;
 
 public enum LottoResult {
-    THREE_NUMBERS_MATCHES("3개 일치 (5,000원) - %d개"),
-    FOUR_NUMBERS_MATCHES("4개 일치 (50,000원) - %d개"),
-    FIVE_NUMBERS_MATCHES("5개 일치 (1,500,000원) - %d개"),
-    FIVE_AND_BONUS_NUMBERS_MATCHES("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    SIX_NUMBERS_MATCHES("6개 일치 (2,000,000,000원) - %d개");
+    THREE_NUMBERS_MATCHES("3개 일치 (5,000원) - %d개\n"),
+    FOUR_NUMBERS_MATCHES("4개 일치 (50,000원) - %d개\n"),
+    FIVE_NUMBERS_MATCHES("5개 일치 (1,500,000원) - %d개\n"),
+    FIVE_AND_BONUS_NUMBERS_MATCHES("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    SIX_NUMBERS_MATCHES("6개 일치 (2,000,000,000원) - %d개\n");
 
     private final String message;
 
@@ -17,19 +17,19 @@ public enum LottoResult {
         System.out.printf(message, count);
     }
 
-    public static String getMessageByResult(int matchedNumberCount, boolean isBonusMatched) {
+    public static LottoResult getMessageByResult(int matchedNumberCount, boolean isBonusMatched) {
         if (matchedNumberCount == 6) {
-            return SIX_NUMBERS_MATCHES.message;
+            return SIX_NUMBERS_MATCHES;
         }
         if (matchedNumberCount == 5 && isBonusMatched) {
-            return FIVE_AND_BONUS_NUMBERS_MATCHES.message;
+            return FIVE_AND_BONUS_NUMBERS_MATCHES;
         }
         if (matchedNumberCount == 5) {
-            return FIVE_NUMBERS_MATCHES.message;
+            return FIVE_NUMBERS_MATCHES;
         }
         if (matchedNumberCount == 4) {
-            return FOUR_NUMBERS_MATCHES.message;
+            return FOUR_NUMBERS_MATCHES;
         }
-        return THREE_NUMBERS_MATCHES.message;
+        return THREE_NUMBERS_MATCHES;
     }
 }

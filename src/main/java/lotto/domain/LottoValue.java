120a7c7bb7bc49d6bd539584bcc0b5c0c001096d
JongMinCh0i <wonderland314592@gmail.com>
2023-11-05 13:12:14 +0900

    refactor(LottoValue): 로또의 숫자값을 관리하는 객체 추가

---
+++ src/main/java/lotto/domain/LottoValue.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class LottoValue {
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_RANGE_IN_START_VALUE = 1;
+    public static final int LOTTO_RANGE_IN_END_VALUE = 45;
+
+    private LottoValue() {
+    }
+}

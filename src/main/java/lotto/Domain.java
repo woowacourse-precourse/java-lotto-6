477df3a154336dbc950d31ff3eb3e6a444235483
hwangkyojune <shane5969@naver.com>
2023-11-05 15:48:42 +0900

    Domain 뼈대 코드 작성

---
+++ src/main/java/lotto/Domain.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.List;
+
+public class Domain {
+    public Rank ranking(){
+        return null;
+    }
+
+    public List<Rank> allRanking(){
+        return null;
+    }
+
+    public int calculateProfit(){
+        return 0;
+    }
+
+    public int calculateProfitRate(){
+        return 0;
+    }
+}

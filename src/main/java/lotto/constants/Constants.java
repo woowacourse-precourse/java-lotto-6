a22c32e91af2741201b3686c4e8cca948c7ede54
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-05 23:30:20 +0900

    test: LottoTest 수정

--- src/main/java/lotto/constants/Constants.java
+++ src/main/java/lotto/constants/Constants.java
@@ -1,11 +1,6 @@
 package lotto.constants;
 
 public enum Constants {
-    SAME_3(5000),
-    SAME_4(50000),
-    SAME_5(1500000),
-    SAME_5_BONUS(30000000),
-    SAME_6(2000000000),
     LOTTO_PRICE(1000),
     MIN_NUMBER(1),
     MAX_NUMBER(45),

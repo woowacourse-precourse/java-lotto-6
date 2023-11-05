ac0ca71d053f8b270d696c5913629a866b0ff999
kkkkimtaehyeon <kth1927@naver.com>
2023-11-06 04:23:24 +0900

    test : 생성된 로또 번호 자료형 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class OutputView {
     private static final String PURCHASED_LOTTO_AMOUNT = "개를 구매했습니다.";
 
-    public void printPieceOfLottoAmount(int pieceOfLotto){
+    public static void printPieceOfLottoAmount(int pieceOfLotto){
         System.out.print(pieceOfLotto);
         System.out.println(PURCHASED_LOTTO_AMOUNT);
     }
-    public void printAllLottos(ArrayList<List<Integer>> allLottos){
-        for(List<Integer> lotto : allLottos){
+    public static void printAllLottos(ArrayList<List<String>> allLottos){
+        for(List<String> lotto : allLottos){
             System.out.println(lotto);
         }
 

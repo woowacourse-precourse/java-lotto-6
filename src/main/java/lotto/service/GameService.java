aa41b4b05e2124740ee51831d12e79eb4841d26f
minsub <chrkb1569@naver.com>
2023-11-05 22:37:38 +0900

    feat : 일치하는 숫자의 수와 보너스 당첨 여부를 확인하는 GameService 생성

---
+++ src/main/java/lotto/service/GameService.java
@@ -0,0 +1,21 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class GameService {
+    public int getMatchedLottoNumber(Lotto myLotto, List<Integer> prizeNumber) {
+        int matchedCount = 0;
+
+        for(int lottoNumber : myLotto.getNumbers()) {
+            if(prizeNumber.contains(lottoNumber)) matchedCount++;
+        }
+
+        return matchedCount;
+    }
+
+    public boolean getBonusResult(Lotto myLotto, int bonusNumber) {
+        return myLotto.getNumbers().contains(bonusNumber);
+    }
+}

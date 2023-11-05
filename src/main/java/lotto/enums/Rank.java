9ebb689749ff2be26d267e1ad5407f5f0f4a26c3
Isaac Han <isaachan233@gmail.com>
2023-11-06 01:36:11 +0900

    refactor: 당첨 상금 추가 및 가져오는 메소드 추가

--- src/main/java/lotto/enums/Rank.java
+++ src/main/java/lotto/enums/Rank.java
@@ -1,18 +1,20 @@
 package lotto.enums;
 
 public enum Rank {
-    FIRST(6,false),
-    SECOND(5,true),
-    THIRD(5,false),
-    FOURTH(4,false),
-    FIFTH(3,false);
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000);
 
     private final int matchCount;
     private final boolean containBonus;
+    private final int winningAmount;
 
-    Rank(int matchCount, boolean containBonus) {
+    Rank(int matchCount, boolean containBonus, int winningAmount) {
         this.matchCount = matchCount;
         this.containBonus = containBonus;
+        this.winningAmount = winningAmount;
     }
 
     public int getMatchedCount() {
@@ -21,5 +23,9 @@ public enum Rank {
 
     public boolean containsBonus() {
         return this.containBonus;
+    }
+
+    public int getWinningAmount() {
+        return this.winningAmount;
     }
 }

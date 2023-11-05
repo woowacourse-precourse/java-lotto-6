8b448035ccdfe3e6a9fe74c9abf34511f0e01853
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-05 14:33:32 +0900

    feat : Rank 로또 등수를 알려주는 기능 추가

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NOTHING(0, 0);
+
+    private final int countOfMatch;
+    private final long lotteryWinnings;
+
+    Rank(int countOfMatch, long lotteryWinnings) {
+        this.countOfMatch = countOfMatch;
+        this.lotteryWinnings = lotteryWinnings;
+    }
+
+    public static Rank valueOf(int countOfMatch, boolean containsBonus) {
+
+        if (isSecond(countOfMatch, containsBonus)) {
+            return SECOND;
+        }
+
+        return checkRank(countOfMatch);
+    }
+
+    private static boolean isSecond(int countOfMatch, boolean containsBonus) {
+        return countOfMatch == 5 && containsBonus;
+    }
+
+    private static Rank checkRank(int countOfMatch) {
+        return Arrays.stream(Rank.values()).filter(rank -> isSame(countOfMatch, rank)).findFirst().orElse(NOTHING);
+    }
+
+    private static boolean isSame(int countOfMatch, Rank rank) {
+        return rank != SECOND && rank.countOfMatch == countOfMatch;
+    }
+}

c547d06ff21ec928717ef304222fe8265ea5a920
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-05 21:03:01 +0900

    feat: combined with result

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.lotto.Lotto;
+import lotto.domain.result.Result;
 import lotto.domain.userpick.UserPick;
 import lotto.domain.purchase.Purchase;
 
@@ -9,20 +10,16 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         Purchase purchase = new Purchase();
-        int quantity = purchase.makePurchase();
         UserPick userPick = new UserPick();
+        Result result = new Result();
+
+        Integer quantity = purchase.makePurchase();
 
         List<Lotto> lottoTicketList = Lotto.gatherLottoTickets(quantity);
 
-        List<Object> pickedNumbers = userPick.pickNumbers();
+        List<Integer> userLottoNumbers = userPick.userLottoNumbers();
+        Integer userBonusNumber = userPick.userBonusNumber(userLottoNumbers);
 
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + "n" + "개");
-        System.out.println("4개 일치 (50,000원) - " + "n" + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + "n" + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + "n" + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + "n" + "개");
-        System.out.println("총 수익률은 " + "n" + "%입니다.");
+        result.showTotalResult(lottoTicketList, userLottoNumbers, userBonusNumber, quantity);
     }
 }

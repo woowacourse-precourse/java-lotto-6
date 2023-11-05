0777426fc3d2b2739bde9384c4b03c8153647680
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-11-06 05:43:48 +0900

    금액이 숫자인지 체크

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Controller {
+	String money;
+	boolean tfCheck;
+	int checkMoney;
+	
 	public Controller() {}
 	
 	public void run() {
@@ -12,7 +18,31 @@ public class Controller {
 	}
 
 	public void start() {
-		
+		System.out.println("구입금액을 입력해 주세요.");
+		howMoney();
 	}
+	
+	//금액 입력
+	public int howMoney() {
+		money = Console.readLine();
+		for(int i = 0; i <money.length() ; i++) {
+			checkMoney();
+		}
+		if(tfCheck = true) {
+			checkMoney = Integer.parseInt(money);
+		}
+		
+		return checkMoney;
+	}
+	
+	//금액이 숫자인지 체크
+	public boolean checkMoney() {
+		if(money.matches(".*[0-9].*")) {
+			tfCheck = true;
+		}
+		
+		return tfCheck;
+	}
+	
 
 }

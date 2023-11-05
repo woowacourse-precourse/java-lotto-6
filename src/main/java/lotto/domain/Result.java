5a914fd369af792ce1ae10cf68794c83220db8ce
74059 <shyeone@naver.com>
2023-11-05 19:22:12 +0900

    feat: enum 이용하여 result 구성

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public enum Result {
+    matches3("3개 일치 (5,000원)", 5000, 0),
+    matches4("4개 일치 (50,000원)", 50000, 0),
+    matches5("5개 일치 (1,500,000원)", 1500000,  0),
+    matches5_bonus("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000, 0),
+    matches6("6개 일치 (2,000,000,000원)", 2000000000, 0)
+    ;
+
+    // 문자열을 저장할 필드
+    private String matchesN;
+    private int amount, numOfMatches;
+
+    private Result(String matchesN, int amount, int numOfMatches) {
+        this.matchesN = matchesN;
+        this.amount = amount;
+        this.numOfMatches = numOfMatches;
+    }
+
+    public String getMatchesN() {
+        return matchesN;
+    }
+
+    public int getNumOfMatches() {
+        return numOfMatches;
+    }
+}

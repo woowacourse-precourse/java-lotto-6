c23af91ce5290fef20b88e6e08c4920251ff8694
yummygyudon <bang2brew@gmail.com>
2023-11-06 04:14:04 +0900

    FEAT : LottoGame 애플리케이션 내 Service 계층인 `LottoGame` 클래스 구현
    - 외부로는 `LottoGameInfo` 내의 DTO로만 정보공개

---
+++ src/main/java/lotto/application/LottoGame.java
@@ -0,0 +1,97 @@
+package lotto.application;
+
+import lotto.domain.Lotto;
+import lotto.domain.repository.LottoRepository;
+import lotto.domain.exception.LottoException;
+import lotto.global.exception.GlobalError;
+import lotto.global.exception.GlobalException;
+import lotto.global.constant.enums.MatchResultType;
+
+import java.util.*;
+
+public class LottoGame {
+    private static final int LOTTO_PRICE = 1_000;
+    private final LottoRepository lottoRepository;
+
+    private Lotto winningLotto;
+    private int winningBonus;
+
+    private Map<MatchResultType, Long> resultBoard;
+
+    private LottoGame(List<Lotto> purchasedLottos) {
+        initBoard();
+        this.lottoRepository = LottoRepository.createRepository(purchasedLottos);
+    }
+
+    public static LottoGame createLottoGame(long paymentAmount) {
+        long quantity = calculateLottoQuantity(paymentAmount);
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (long purchase = 0; purchase < quantity; purchase++) {
+            lottos.add(LottoMachine.issueAutomaticLotto());
+        }
+        return new LottoGame(lottos);
+    }
+
+    private static long calculateLottoQuantity(long paymentAmount) {
+        return paymentAmount / LOTTO_PRICE;
+    }
+
+    public void registerWinningNumbers(List<Integer> winningNumbers) throws LottoException{
+        this.winningLotto = LottoMachine.issueManualLotto(winningNumbers);
+    }
+
+    public void registerWinningBonus(int winningBonus) {
+        this.winningBonus = winningBonus;
+    }
+
+    public LottoGameInfo.PurchaseStatus checkPurchaseStatus() {
+        return new LottoGameInfo.PurchaseStatus(
+                lottoRepository.count(),
+                lottoRepository.lottos()
+        );
+    }
+
+    public LottoGameInfo.MatchResult checkMatchResult() throws GlobalException{
+        if (lottoRepository.lottos().isEmpty()) {
+            throw new GlobalException(GlobalError.NOT_PAID_YET);
+        }
+        matchWinning();
+        return new LottoGameInfo.MatchResult(
+                resultBoard
+        );
+    }
+    public LottoGameInfo.RevenueInfo checkRevenue() {
+        long totalReward = 0L;
+        for (Map.Entry<MatchResultType, Long> eachResult: resultBoard.entrySet()) {
+            MatchResultType matchType = eachResult.getKey();
+            totalReward += matchType.calculateReward(eachResult.getValue());
+        }
+        return new LottoGameInfo.RevenueInfo(
+                lottoRepository.count() * LOTTO_PRICE,
+                totalReward
+        );
+    }
+
+    private void matchWinning() {
+        initBoard();
+        for (Lotto lotto : lottoRepository.lottos()) {
+            MatchResultType matchType = LottoMachine.match(lotto, winningLotto, winningBonus);
+            if (!matchType.equals(MatchResultType.BOOM)) {
+                Long currentPoint = resultBoard.get(matchType);
+                resultBoard.put(matchType, currentPoint + 1);
+            }
+        }
+    }
+
+    private void initBoard() {
+        Map<MatchResultType, Long> board = Map.of(
+                MatchResultType.MATCH_THREE, 0L,
+                MatchResultType.MATCH_FOUR, 0L,
+                MatchResultType.MATCH_FIVE, 0L,
+                MatchResultType.MATCH_FIVE_WITH_BONUS, 0L,
+                MatchResultType.MATCH_SIX, 0L
+        );
+        this.resultBoard = new HashMap<>(board);
+    }
+}

582f26dab0753f6493a5071d26487e722b2ccd20
PgmJun <chltmdwns96@gmail.com>
2023-11-06 00:25:37 +0900

    refactor: Rank Enum에 matchCount 필드 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,18 +1,20 @@
 package lotto.domain;
 
 public enum Rank {
-    FIFTH(5_000, "3개 일치 (5,000원) - %d개\n"),
-    FOURTH(50_000, "4개 일치 (50,000원) - %d개\n"),
-    THIRD(1_500_000, "5개 일치 (1,500,000원) - %d개\n"),
-    SECOND(30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
-    FIRST(2_000_000_000, "6개 일치 (2,000,000,000원) - %d개\n"),
+    FIFTH(3, 5_000, "3개 일치 (5,000원) - %d개\n"),
+    FOURTH(4, 50_000, "4개 일치 (50,000원) - %d개\n"),
+    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - %d개\n"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - %d개\n"),
     ;
 
-    Rank(int prizeMoney, String winningMessage) {
+    Rank(int matchCount, int prizeMoney, String winningMessage) {
+        this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
         this.winningMessage = winningMessage;
     }
 
+    private final int matchCount;
     private final int prizeMoney;
     private final String winningMessage;
 
@@ -26,13 +28,13 @@ public enum Rank {
     }
 
     public static Rank judgeBy(int matchCount, boolean isBonusNumberMatched) {
-        if (matchCount == 6) {
+        if (matchCount == FIRST.matchCount) {
             return Rank.FIRST;
-        } else if (matchCount == 5 && isBonusNumberMatched) {
+        } else if (matchCount == SECOND.matchCount && isBonusNumberMatched) {
             return Rank.SECOND;
-        } else if (matchCount == 5) {
+        } else if (matchCount == THIRD.matchCount) {
             return Rank.THIRD;
-        } else if (matchCount == 4) {
+        } else if (matchCount == FOURTH.matchCount) {
             return Rank.FOURTH;
         }
         return Rank.FIFTH;

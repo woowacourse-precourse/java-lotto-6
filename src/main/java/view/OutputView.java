383a1892c775a17a39956e63b7f1831dccb2afa2
songsunkook <songsunkook@gmail.com>
2023-11-06 01:10:26 +0900

    feat: OutputView 추가

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,37 @@
+package view;
+
+import static constant.OutputMessage.*;
+
+public class OutputView {
+    public void inputPurchaseAmount() {
+        System.out.print(INPUT_PURCHASE_AMOUNT.get());
+    }
+
+    public void buyLottoes() {
+        System.out.print(BUY_LOTTOES.get());
+    }
+
+    public void inputWinningNumber() {
+        System.out.print(INPUT_WINNING_NUMBER.get());
+    }
+
+    public void inputBonusNumber() {
+        System.out.print(INPUT_BONUS_NUMBER.get());
+    }
+
+    public void inputWinningStatistics() {
+        System.out.print(INPUT_WINNING_STATISTICS.get());
+    }
+
+    public void matchCount() {
+        System.out.print(MATCH_COUNT.get());
+    }
+
+    public void matchCountRank2() {
+        System.out.print(MATCH_COUNT_RANK_2.get());
+    }
+
+    public void totalRateOfReturn() {
+        System.out.print(TOTAL_RATE_OF_RETURN.get());
+    }
+}

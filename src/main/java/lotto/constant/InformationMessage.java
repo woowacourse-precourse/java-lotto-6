9efb5d415ce0293566276e8fae4ca5f46e0b7146
seminchoi <choicco89@gmail.com>
2023-11-06 00:52:23 +0900

    feat(controller.LottoController) : 로또를 구매하고 구매 결과를 출력하는 기능 구현

--- src/main/java/lotto/constant/InformationMessage.java
+++ src/main/java/lotto/constant/InformationMessage.java
@@ -1,4 +1,16 @@
 package lotto.constant;
 
 public enum InformationMessage {
+    GUIDE_INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요.")
+    ;
+
+    private final String message;
+
+    InformationMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import lotto.constant.InformationMessage;
+import lotto.dto.LottoReceiptDto;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -16,5 +18,13 @@ public class LottoController {
     }
 
     public void run() {
+        purchaseLotto();
+    }
+
+    private void purchaseLotto() {
+        outputView.printInformationMessage(InformationMessage.GUIDE_INPUT_PURCHASE_AMOUNT);
+        long amount = inputView.readIntLine();
+        LottoReceiptDto lottoReceipt = lottoService.getLottoReceipt(amount);
+        outputView.printLottoReceipt(lottoReceipt);
     }
 }

df1f5e9d97ba82d45798cb52e69c5b9657fac6c6
mumpuris <hst990124@naver.com>
2023-11-05 23:19:31 +0900

    LottoMachine에서 기능이 위임되므로 테스트 코드 수정

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -14,51 +14,7 @@ class LottoMachineTest {
 
     @BeforeEach
     public void setUp() {
-        lottoMachine = LottoMachine.getInstance();
-    }
-
-    @DisplayName("당첨 번호를 세팅하고 가져오는 기능 테스트")
-    @Test
-    public void given_SetWinningNumbers_when_GetWinningNumbers_then_ReturnsSetNumbers() {
-        List<Integer> winningNumbers = List.of(1,2,3,4,5,6);
-        lottoMachine.setWinningNumbers(winningNumbers);
-
-        assertEquals(winningNumbers, lottoMachine.getWinningNumbers());
-    }
-
-    @DisplayName("보너스 번호를 세팅하고 가져오는 기능 테스트")
-    @Test
-    public void given_setBonusNum_when_GetBonusNum_then_returnSetBonusNum() {
-        int bonusNum = 25;
-        lottoMachine.setBonusNum(bonusNum);
-
-        assertEquals(bonusNum, lottoMachine.getBonusNum());
-    }
-
-    @DisplayName("당첨 번호 세팅 시 유효성 검사 실패 테스트")
-    @Test
-    public void given_winningNumbers_when_SetWinningNumbers_then_ThrowsIllegalArgumentException() {
-        List<Integer> invalidNumbers = List.of(1, 2, 3, 4, 5);
-
-        assertThrows(IllegalArgumentException.class, () -> lottoMachine.setWinningNumbers(invalidNumbers));
-    }
-
-    @DisplayName("보너스 번호 세팅 시 유효성 검사 실패 테스트")
-    @Test
-    public void given_InvalidBonusNum_when_SetBonusNum_then_ThrowsIllegalArgumentException() {
-        int bonusNum = 0;
-
-        assertThrows(IllegalArgumentException.class, () -> lottoMachine.setBonusNum(bonusNum));
-    }
-
-    @DisplayName("보너스 번호가 당첨 번호와 중복될 때 유효성 검사 실패 테스트")
-    @Test
-    public void given_DuplicateBonusNum_when_SetBonusNum_then_ThrowsIllegalArgumentException() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
-        lottoMachine.setWinningNumbers(winningNumbers);
-
-        int duplicateBonusNum = 6;
-        assertThrows(IllegalArgumentException.class, () -> lottoMachine.setBonusNum(duplicateBonusNum));
+        lottoMachine = new LottoMachine();
     }
 
     @DisplayName("5000원이 주어질 때, 5장의 로또를 계산한다.")

be7be0b7c118138330ff120de0eeec30d6860bf3
noah <jeongas9810@gmail.com>
2023-11-06 04:54:31 +0900

    feat: Add getPurchaseQuantity() in start method
    Invoke the controller and view to receive a value from the user.

--- src/main/java/lotto/LottoPlay.java
+++ src/main/java/lotto/LottoPlay.java
@@ -1,10 +1,12 @@
 package lotto;
 
 public class LottoPlay implements Play{
+    private final Controller controller = new Controller();
+    private final View view = new View();
 
     @Override
     public void start() {
-        getPurchaseQuantity();
+        int purchaseQuantity = getPurchaseQuantity();
         printMyLottoNumber();
         readWinningNumber();
         readBonusNumber();
@@ -12,8 +14,21 @@ public class LottoPlay implements Play{
         printYieldRate();
     }
 
-    private void getPurchaseQuantity() {
+    private int getPurchaseQuantity() {
+        int purchaseQuantity = 0;
 
+        do {
+            try {
+                System.out.print(view.getAskPurchaseAmount());
+                purchaseQuantity = controller.getPurchaseQuantity();
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorMessage.NOT_A_NUMBER.getErrorMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(ErrorMessage.NOT_SEPARATED_1000.getErrorMessage());
+            }
+        } while (purchaseQuantity == 0);
+
+        return purchaseQuantity;
     }
 
     private void printMyLottoNumber() {

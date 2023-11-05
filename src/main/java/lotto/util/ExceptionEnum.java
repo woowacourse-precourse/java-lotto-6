ef965d5aef5f0b2e6f93578ef9f182ed6f05138b
linirini <2001yerin@naver.com>
2023-11-05 19:18:00 +0900

    feat : implement error message and enum for invalid number format of price input

--- src/main/java/lotto/util/ExceptionEnum.java
+++ src/main/java/lotto/util/ExceptionEnum.java
@@ -1,4 +1,13 @@
 package lotto.util;
 
 public enum ExceptionEnum{
+
+    NOT_NUMBER_FORMAT("Your price Input is not a number format.");
+
+    private final String NOTICE = "[ERROR] ";
+    private String message;
+
+    ExceptionEnum(String message){
+        this.message = NOTICE + message;
+    }
 }

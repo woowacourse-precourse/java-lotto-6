9297d62d38a355603882d84b5b24dad1db7c5037
howudong <tjdvy963@naver.com>
2023-11-05 23:22:58 +0900

    refactor : WinningLottoDTO가 DTO.Input을 상속하도록 하고, getter,setter 추가

--- src/main/java/lotto/dto/WinningLottoDTO.java
+++ src/main/java/lotto/dto/WinningLottoDTO.java
@@ -2,5 +2,25 @@ package lotto.dto;
 
 import lotto.domain.Lotto;
 
-public record WinningLottoDTO(Lotto winningLottoNumbers, int bonus) {
+public final class WinningLottoDTO extends DTO.Input {
+    private Lotto lotto;
+    private Integer bonus;
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public void setLotto(Lotto lotto) {
+        validateOverride(this.lotto);
+        this.lotto = lotto;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Integer bonus) {
+        validateOverride(this.bonus);
+        this.bonus = bonus;
+    }
 }

67668801ec6dbd7f4076f6e334e7195453d5b70b
yunseung <yseungun.21@gmail.com>
2023-11-06 00:36:48 +0900

    refactor(BonusNumberInput): 보너스 번호에 대한 책임 이전
    - 보너스 번호와 관련된 코드 LottoGame -> BonusNumberInput 이전

---
+++ src/main/java/game/BonusNumberInput.java
@@ -0,0 +1,22 @@
+package game;
+
+import static constant.ConstantMessage.BONUS_NUMBER_REQUEST_MESSAGE;
+
+import base.Converter;
+import camp.nextstep.edu.missionutils.Console;
+
+public class BonusNumberInput {
+    public int getBonusNumber(Converter converter) {
+        String bonusNumber = receiveBonusNumber();
+        return converter.stringToInteger(bonusNumber);
+    }
+
+    private String receiveBonusNumber() {
+        printBonusNumberRequestMessage();
+        return Console.readLine();
+    }
+
+    private void printBonusNumberRequestMessage() {
+        System.out.println(BONUS_NUMBER_REQUEST_MESSAGE);
+    }
+}

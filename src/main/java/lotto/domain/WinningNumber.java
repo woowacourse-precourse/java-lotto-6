e5ff8da1e1c4e7758be6e156ce7ff53629c22a77
Annjieun <dkslwdms2121@naver.com>
2023-11-05 16:56:18 +0900

    refactor(WinningNumber): 중복된 당첨 번호가 존재하는지 확인하는 로직 변경

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
+import static lotto.constant.LottoNumber.MAXIMUM_LOTTO_NUMBER;
+import static lotto.constant.LottoNumber.MINIMUM_LOTTO_NUMBER;
 import static lotto.constant.LottoNumber.WINNING_NUMBER_LENGTH;
+import static lotto.constant.message.ErrorMessage.DUPLICATE_WINNING_NUMBER;
 import static lotto.constant.message.ErrorMessage.INVALID_LENGTH_WINNING_NUMBER;
 import static lotto.constant.message.ErrorMessage.INVALID_RANGE_WINNING_NUMBER;
 import static lotto.constant.message.ErrorMessage.NON_INTEGER_WINNING_NUMBER;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +15,7 @@ public class WinningNumber {
     private final List<Integer> winningNumber;
 
     public WinningNumber(List<String> numbers) {
-        winningNumber = validateWinningNumber(numbers);
+        this.winningNumber = validateWinningNumber(numbers);
     }
 
     private List<Integer> validateWinningNumber(List<String> numbers) {
@@ -45,16 +47,14 @@ public class WinningNumber {
 
     private void checkNumbersRange(List<String> numbers) {
         if (numbers.stream()
-                .anyMatch(number -> Integer.parseInt(number) < 1 || Integer.parseInt(number) > 45)) {
+                .anyMatch(number -> Integer.parseInt(number) < MINIMUM_LOTTO_NUMBER.getNumber() || Integer.parseInt(number) > MAXIMUM_LOTTO_NUMBER.getNumber())) {
             throw new IllegalArgumentException(INVALID_RANGE_WINNING_NUMBER.getMessage());
         }
     }
 
     private void checkNumbersDuplicate(List<String> numbers) {
-        HashSet<String> uniqueNumbers = new HashSet<>();
-
-        if (numbers.stream().anyMatch(name -> !uniqueNumbers.add(name))) {
-            throw new IllegalArgumentException();
+        if (numbers.stream().distinct().count() != WINNING_NUMBER_LENGTH.getNumber()) {
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBER.getMessage());
         }
     }
 }

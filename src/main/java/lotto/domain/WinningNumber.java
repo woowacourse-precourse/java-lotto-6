3ffddd3f8e6b7cf0dd3cd2441b7ea4f68fa4d708
Jae-Young98 <ygy0102@gmail.com>
2023-11-05 17:22:36 +0900

    feat(WinningNumber): 로또 당첨 번호 검증 기능 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,12 +1,34 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumber {
-    private Lotto lotto;
-    private int bonusNumber;
+    private final Lotto lotto;
+    private final int bonusNumber;
 
     public WinningNumber(Lotto lotto, int bonusNumber) {
+        validateBonusNumber(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto lotto, int bonusNumber) {
+        List<Integer> winningNumber = lotto.getNumbers();
+        if (isWrongRange(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 1~45 사이의 숫자가 입력 되어야 합니다.");
+        }
+
+        if (isContainInLotto(winningNumber, bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복 될 수 없습니다.");
+        }
+    }
+
+    private boolean isWrongRange(int bonusNumber) {
+        return bonusNumber < 1 || bonusNumber > 45;
+    }
+
+    private boolean isContainInLotto(List<Integer> lotto, int bonusNumber) {
+        return lotto.contains(bonusNumber);
     }
 
     public Lotto getWinningNumber() {

a208719aa6ab865f5ab52c9c17454a0798b2b795
LEE-sh1673 <lsh901673@gmail.com>
2023-11-06 00:49:47 +0900

    feat: 로또 번호를 비교하는 기능 추가

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -2,10 +2,13 @@ package lotto.model;
 
 import static lotto.model.ErrorType.LOTTO_NUMBER_OUT_OF_RANGE;
 
+import java.util.Objects;
+
 public class LottoNumber {
 
     private static final int MIN = 1;
     private static final int MAX = 45;
+
     private final int number;
 
     private LottoNumber(final int number) {
@@ -25,5 +28,26 @@ public class LottoNumber {
 
     public static LottoNumber of(final int number) {
         return new LottoNumber(number);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final LottoNumber other = (LottoNumber) o;
+        return matchNumber(other);
+    }
+
+    private boolean matchNumber(final LottoNumber other) {
+        return number == other.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -1,9 +1,11 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -26,5 +28,19 @@ public class LottoNumberTest {
     void givenNumberInRange_Then_NoExceptionOccurs(final int number) {
         assertThatCode(() -> LottoNumber.of(number))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("로또 번호가 서로 같은 숫자를 가지는 경우 비교 결과는 참이다.")
+    void givenSameNumber_Then_TrueReturns() {
+        final LottoNumber number = LottoNumber.of(1);
+        assertThat(number.equals(LottoNumber.of(1))).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또 번호가 서로 다른 숫자를 가지는 경우 비교 결과는 거짓이다.")
+    void givenDifferentNumber_Then_TrueReturns() {
+        final LottoNumber number = LottoNumber.of(1);
+        assertThat(number.equals(LottoNumber.of(45))).isFalse();
     }
 }

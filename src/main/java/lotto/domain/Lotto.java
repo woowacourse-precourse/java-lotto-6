0c56d7e2f051210d50155f5ff46cad6cd45dd3ad
Hoo-sung <gntjd135@gmail.com>
2023-11-05 09:50:43 +0900

    refactor(Lotto): 스트림 사용하여, compareWithAnotherLotto 메서드 리펙토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,11 @@
 package lotto.domain;
 
 import lotto.io.OutputHandler;
-import lotto.system.Constant;
 import lotto.util.Util;
 import lotto.verifier.LottoVerifier;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -23,21 +21,18 @@ public class Lotto {
         new LottoVerifier().check(forCheck);
     }
 
-    public boolean contains(int number){
+    public boolean contains(int number) {
         return numbers.contains(number);
     }
 
-    public void printLottoInfo(){
+    public void printLottoInfo() {
         OutputHandler.printLotto(numbers);
     }
 
-    public int compareWithAnotherLotto(Lotto another){
-        int count=0;
-        for(int i = 0; i< Constant.LOTTO_SIZE; i++){
-            if(another.contains(numbers.get(i)))
-                count++;
-        }
-        return count;
+    public int compareWithAnotherLotto(Lotto another) {
+        return (int) numbers.stream()
+                .filter(another::contains)
+                .count();
     }
 
 }

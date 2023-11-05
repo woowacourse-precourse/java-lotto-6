e2b6b79703da836a03aedf8e52a8ce6cc4d3564f
Nara Park <skfk6352@naver.com>
2023-11-06 01:06:27 +0900

    <feat>(Lotto): 로또 번호가 유효한 숫자 범위 안에 있는지 검증하는 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.List;
 import lotto.constant.ExceptionConstant;
+import lotto.constant.NumberConstant;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -9,6 +10,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateDuplication(numbers);
+        validateNumberSize(numbers);
         this.numbers = numbers;
     }
 
@@ -21,6 +23,15 @@ public class Lotto {
     private void validateDuplication(List<Integer> numbers) {
         if (numbers.stream().distinct().count() != 6) {
             throw new IllegalArgumentException(ExceptionConstant.LOTTO_NUMBER_DUPliCATE.getMessage());
+        }
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < NumberConstant.LOTTO_MIN_NUMBER.getNumber()
+                    || NumberConstant.LOTTO_MAX_NUMBER.getNumber() < number) {
+                throw new IllegalArgumentException(ExceptionConstant.LOTTO_NUMBER_SIZE.getMessage());
+            }
         }
     }
 }

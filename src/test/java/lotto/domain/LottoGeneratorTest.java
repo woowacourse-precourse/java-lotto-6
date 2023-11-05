3df57a327c6f8d211973614ca1f532f19e240d07
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-05 20:09:07 +0900

    test: 구입 최소 금액 검증 기능 테스트 코드 추가

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test;
 
 class LottoGeneratorTest {
 
-	
+	@DisplayName("구입 금액이 로또 1장 가격보다 적으면 예외가 발생한다.")
+    @Test
+    void paidAffordable() {
+        assertThatThrownBy(() -> new LottoGenerator(900))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }

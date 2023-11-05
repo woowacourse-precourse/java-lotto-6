796e07eeb6b4fc6ab10e412e8451162a2a007b59
kkkkimtaehyeon <kth1927@naver.com>
2023-11-06 01:24:33 +0900

    feat : 로또 자료형 변경

--- src/main/java/lotto/model/GenerateLottoNumbers.java
+++ src/main/java/lotto/model/GenerateLottoNumbers.java
@@ -7,27 +7,10 @@ import java.util.stream.Collectors;
 
 public class GenerateLottoNumbers {
     private ArrayList<List<String>> allLottos = new ArrayList<>();
-    /*
-}
 
-    public GenerateLottoNumbers(int pieceOfLotto){
-        generateAllLottos(pieceOfLotto);
+    public GenerateLottoNumbers(int numberOfLotto){
+        generateAllLottos(numberOfLotto);
     }
-
-
-    private List<String> generateEachLottoNumbers(){
-        return Randoms.pickUniqueNumbersInRange(1,45,6).stream().map(String::valueOf).toList();
-
-    private void generateAllLottos(int pieceOfLotto){
-        for(int index = 0; index < pieceOfLotto; index++){
-            allLottos.add(generateEachLottoNumbers());
-        }
-    }
-    public ArrayList<List<String>> getAllLottos(){
-        return allLottos;
-    }
-
-     */
     private List<String> generateEachLotto(){
         return Randoms.pickUniqueNumbersInRange(1,45,6).stream().map(String::valueOf).toList();
     }

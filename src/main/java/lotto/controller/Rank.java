53ec2f98375b4e4736275d83d0da7be6125f7916
Jiwon Na <jiwonna52@gmail.com>
2023-11-05 15:55:44 +0900

    Refactor : 출력 용 가격 추가

--- src/main/java/lotto/controller/Rank.java
+++ src/main/java/lotto/controller/Rank.java
@@ -3,19 +3,21 @@ package lotto.controller;
 import java.util.Arrays;
 
 public enum Rank {
-    FIFTH(5, 3, "5,000"),
-    FOURTH(4, 4, "50,000"),
-    THIRD(3, 5, "1,500,000"),
-    SECOND(2, 5, "30,000,000"),
-    First(1, 6, "2,000,000,000");
+    FIFTH(5, 3, 5000, "5,000"),
+    FOURTH(4, 4, 50000, "50,000"),
+    THIRD(3, 5, 1500000, "1,500,000"),
+    SECOND(2, 5, 30000000, "30,000,000"),
+    First(1, 6, 2000000000, "2,000,000,000");
 
-    private final int correctNum;
-    private final String prize;
     private final int ranking;
-    Rank(int ranking, int correctNum, String prize) {
+    private final int correctNum;
+    private final int prize;
+    private final String prizePrint;
+    Rank(int ranking, int correctNum, int prize, String prizePrint) {
         this.ranking = ranking;
         this.correctNum = correctNum;
         this.prize = prize;
+        this.prizePrint = prizePrint;
     }
 
     public int getRanking() {
@@ -26,8 +28,12 @@ public enum Rank {
         return correctNum;
     }
 
-    public String getPrize() {
+    public int getPrize() {
         return prize;
+    }
+
+    public String getPrizePrint() {
+        return prizePrint;
     }
 
     public static Rank getRank(int num) {

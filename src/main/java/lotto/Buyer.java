f8848a3657b94652bfd996c78f8f280e576cab4d
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-05 12:53:53 +0900

    feat(Buyer): 자신의 복권 번호를 제공하는 기능 추가
    getMyLotteries() 메서드를 구현하여 자신의 복권 번호를 제공

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -21,4 +21,9 @@ public class Buyer {
         double seedMoney = 1000 * purchaseAmount;
         return (double) Math.round((prizeSum / seedMoney) * 1000) / 10;
     }
+
+    // myLotteries를 할당하지 않은 영우 get요청을 한다면 illegalstateExcept 추가하기
+    public List<Lotto> getMyLotteries() {
+        return myLotteries;
+    }
 }

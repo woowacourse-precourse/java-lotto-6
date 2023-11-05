770b3b7b2e82ed60e034d6da176f36aacbf976ed
Nara Park <skfk6352@naver.com>
2023-11-05 23:12:25 +0900

    <feat>(Lotteries): 로또 구매 금액이 최소 구매 금액을 넘는지 검증하는 메서드 구현

--- src/main/java/lotto/domain/Lotteries.java
+++ src/main/java/lotto/domain/Lotteries.java
@@ -18,12 +18,19 @@ public class Lotteries {
     private int generateLotteries(String userInput) {
         int totalPrice = Integer.parseInt(userInput);
         validateMultiplesOfThousand(totalPrice);
+        validateMinimumPrice(totalPrice);
         return totalPrice / NumberConstant.LOTTO_PRICE.getNumber();
     }
 
     private void validateMultiplesOfThousand(int totalPrice) {
         if (totalPrice / NumberConstant.LOTTO_PRICE.getNumber() != 0) {
             throw new IllegalArgumentException(ExceptionConstant.PURCHASE_REMAINDER.getMessage());
+        }
+    }
+
+    private void validateMinimumPrice(int totalPrice) {
+        if (totalPrice < NumberConstant.LOTTO_PRICE.getNumber()) {
+            throw new IllegalArgumentException(ExceptionConstant.PURCHASE_MIN_NUMBER.getMessage());
         }
     }
 }

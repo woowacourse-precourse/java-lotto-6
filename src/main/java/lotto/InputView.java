0ea3f78a362a694fe9239ecf560d29aa436609ad
SUJI-J <suzy852@naver.com>
2023-11-05 19:40:25 +0900

    feat: 금액이 숫자인지 검증 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -5,8 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 import static lotto.OutputView.printRequestMoney;
 
 public class InputView {
-    public static String inputRequestMoney() {
+    public static int inputRequestMoney() {
         printRequestMoney();
-        return Console.readLine();
+        String inputMoney = Console.readLine();
+        return validateNumber(inputMoney);
+    }
+
+    private static int validateNumber(String inputMoney) {
+        inputBlank(inputMoney);
+        return inputNumeric(inputMoney);
+    }
+
+    private static void inputBlank(String inputMoney) {
+        if (inputMoney.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static int inputNumeric(String inputMoney) {
+        int amount;
+        try {
+            amount = Integer.parseInt(inputMoney);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+        return amount;
     }
 }

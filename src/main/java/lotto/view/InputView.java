0fcbbd985ed5a854aa426c20113c9212979d78e8
김관진 <cozyschooll@gmail.com>
2023-11-05 16:40:53 +0900

    feat: 구입 금액 입력 기능 구현 view/InputView 파일 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public Integer inputPurchasePrice() {
+        return Integer.parseInt(Console.readLine());
+    }
+}

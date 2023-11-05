43c3f281a2675b927db00309b25a050a31d5f55a
Chan <33cks1423@naver.com>
2023-11-05 17:14:27 +0900

    feat: 로또 예외 처리 구현 및 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.utils.ErrorMessage;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,8 +15,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
         }
+        numbers.forEach(this::validateSize);
+        validateDuplicatedNumber(numbers);
+    }
+
+    public static Lotto of(List<Integer> numbers) {
+        return new Lotto(numbers);
     }
 
     public void checkDuplicatedNumber(int number) {
@@ -24,5 +31,33 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
+    private void validateSize(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
 }
--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.utils;
 public enum ErrorMessage {
     INVALID_LOTTO_PAYMENT("로또 구입 금액은 1000원 이상이어야 합니다."),
     DUPLICATED_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
-    INVALID_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INVALID_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_LOTTO_SIZE("로또 번호는 6개여야 합니다.");
 
     private final String message;
 

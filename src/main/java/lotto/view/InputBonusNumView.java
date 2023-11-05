1473c2dbd87bdc085976ef31782acc39efdacfa9
goragoraki <gorakio0126@gmail.com>
2023-11-05 19:37:13 +0900

    fix(InputBonusNumView): 에러 문구가 로또 번호로 잘못 표기 되는 버그 수정
    에러 메세지 수정
    ERROR_MESSAGE: [ERROR] 로또 번호는 숫자여야 합니다. -> [ERROR] 보너스 번호는 숫자여야 합니다.

--- src/main/java/lotto/view/InputBonusNumView.java
+++ src/main/java/lotto/view/InputBonusNumView.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class InputBonusNumView extends InputView {
     private static final Pattern PATTERN = Pattern.compile("\\d+");
     private static final String INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String ERROR_MESSAGE = "[ERROR] 구입 금액은 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 보너스 번호는 숫자여야 합니다.";
 
     public Integer getValue() {
         System.out.println(INPUT_MESSAGE);

bee39d61b9bc40c40c82c157f88e9eed60325b2c
이진모 <jinmlee210@naver.com>
2023-11-05 20:28:27 +0900

    feat: 몇장의 로또를 샀는지 장수를 저장하는 변수 추가

--- src/main/java/lotto/util/User.java
+++ src/main/java/lotto/util/User.java
@@ -5,19 +5,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
     private static int paymentAmount;
+    private static int manyLottoTicket;
 
     public void inputPaymentAmount() {
         String input = Console.readLine();
         checkPaymentAmount(input);
+        getManyLottoTicket();
     }
 
-    public void checkPaymentAmount(String input){
+    public void checkPaymentAmount(String input) {
         paymentAmount = Exception.checkInvalidNumber(input);
         Exception.checkUnitPaymentAmount(paymentAmount);
         Exception.checkRangePaymentAmount(paymentAmount);
     }
 
-    public int getManyLottoTicket() {
-        return paymentAmount / 1000;
+    public void getManyLottoTicket() {
+        manyLottoTicket = paymentAmount / 1000;
     }
 }

9361ae60f3cd39631f1b23ede485da08cceff648
YongHyeon <pyh4616@naver.com>
2023-11-05 20:02:36 +0900

    Refactor(OutputView 클래스): 메서드에 있던 줄바꿈을 상수로 옮기고, 상수에 출력 옵션을 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,9 +8,9 @@ import java.util.Map;
 public class OutputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
-    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String LOTTO_PURCHASE_MESSAGE = "\n%d개를 구매했습니다.\n";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "\n보너스 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
     private static final String WINNING_STATISTICS_DIVIDER = "---";
     private static final String FIFTH_PLACE_RESULT_MESSAGE = "3개 일치 (5,000원) - %d개\n";
@@ -24,7 +24,7 @@ public class OutputView {
     }
 
     public void printLottoCount(int lottoCount) {
-        System.out.println("\n" + lottoCount + LOTTO_PURCHASE_MESSAGE);
+        System.out.printf(LOTTO_PURCHASE_MESSAGE, lottoCount);
     }
 
     public void printLottoTickets(List<List<Integer>> lottoTickets) {
@@ -34,11 +34,11 @@ public class OutputView {
     }
 
     public void printWinningNumbersInputMessage() {
-        System.out.println("\n" + WINNING_NUMBERS_INPUT_MESSAGE);
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
     }
 
     public void printBonusNumberInputMessage() {
-        System.out.println("\n" + BONUS_NUMBER_INPUT_MESSAGE);
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
     }
 
     public void printWinningStatistics(Map<Rank, Integer> winningStatistics) {

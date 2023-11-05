39516fbc7c10143fb813b07bd99fa45aca6cd86c
alscjf1329 <alscjf1329@gmail.com>
2023-11-05 16:42:44 +0900

    feat(LottoUtil): generateLottos 작성
    랜덤 로또 리스트를 생성해주는 generateLottos 메소드를 작성했습니다.

--- src/main/java/lotto/utils/LottoUtil.java
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -1,6 +1,13 @@
 package lotto.utils;
 
+import static lotto.message.ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS;
 import static lotto.message.ErrorMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
 
 public class LottoUtil {
     public static void validateLottoNum(int num) {
@@ -12,5 +19,13 @@ public class LottoUtil {
         }
     }
 
+    public static List<Lotto> generateLottos(BigDecimal numberOfLottoPurchased, LottoGenerator lottoGenerator) {
+        List<Lotto> lottos = new ArrayList<>();
+        while (numberOfLottoPurchased.compareTo(BigDecimal.ZERO) > 0) {
+            lottos.add(lottoGenerator.generate());
+            numberOfLottoPurchased = numberOfLottoPurchased.subtract(BigDecimal.ONE);
+        }
+        return lottos;
+    }
 
 }

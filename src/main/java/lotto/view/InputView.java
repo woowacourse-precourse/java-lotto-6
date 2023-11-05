1fa097ce99cd6751e030cf89dccb12e5c513a722
shining013 <nevertheless113@naver.com>
2023-11-05 20:29:56 +0900

    feat: InputView bonusNumberInput 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,4 +12,9 @@ public class InputView {
                 System.out.println("당첨 번호를 입력해 주세요.");
                 return Console.readLine();
         }
+
+        public static String bonusNumberInput() {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                return Console.readLine();
+        }
 }

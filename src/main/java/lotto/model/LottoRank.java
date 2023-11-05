cd14ef79dac55376b811d2593777b6b937e4497f
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 00:37:47 +0900

    feat(LottoRank): MISS Rank 추가

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -9,7 +9,8 @@ public enum LottoRank {
     SECOND(5, true, 30000000),
     THIRD(5, false, 1500000),
     FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000);
+    FIFTH(3, false, 5000),
+    MISS(0, false, 0);
 
     private final int matchCount;
     private final boolean matchBonus;

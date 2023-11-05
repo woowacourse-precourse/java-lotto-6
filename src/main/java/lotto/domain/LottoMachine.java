8a27a895d60a43afffaf21302e79f52887924a25
mumpuris <hst990124@naver.com>
2023-11-05 23:19:04 +0900

    로또 머신에서 필요없는 책임을 분리하여 LottoManager 기능 위임

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -6,37 +6,8 @@ import java.util.List;
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_SIZE = 6;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
 
-    private List<Integer> winningNumbers; //매주 토요일 오후 8시 35분
-    private int bonusNum;
-    private static final LottoMachine instance = new LottoMachine();
-
-    private LottoMachine() {}
-
-    public static LottoMachine getInstance() {
-        return instance;
-    }
-
-    public void setWinningNumbers(List<Integer> winningNumbers) {
-        validateLottoNumbers(winningNumbers);
-        this.winningNumbers = winningNumbers;
-    }
-
-    public List<Integer> getWinningNumbers() {
-        return winningNumbers;
-    }
-
-    public void setBonusNum(Integer bonusNum) {
-        validateBonusNumber(bonusNum);
-        this.bonusNum = bonusNum;
-    }
-
-    public Integer getBonusNum() {
-        return bonusNum;
-    }
+    public LottoMachine() {}
 
     public int calculateNumberOfTickets(int paidAmount) {
         validatePurchaseAmount(paidAmount);
@@ -59,28 +30,6 @@ public class LottoMachine {
         }
         if (amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
-        }
-    }
-
-    private void validateLottoNumbers(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
-        }
-        for (int number : numbers) {
-            validateNumberInRange(number);
-        }
-    }
-
-    private void validateBonusNumber(int number) {
-        validateNumberInRange(number);
-        if (winningNumbers.contains(number)) {
-            throw new IllegalArgumentException("보너스 번호가 당첨 번호와 중복됩니다.");
-        }
-    }
-
-    private void validateNumberInRange(int number) {
-        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException("로또 번호는 1부터 45 사이여야 합니다.");
         }
     }
 }

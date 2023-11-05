14d7a67bade98b5c847a61fc300a0877ab765409
kimbeomjoon99 <tnldls99@naver.com>
2023-11-05 22:31:48 +0900

    Feat: add prize calculation feature

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -72,10 +72,71 @@ public class Lotto {
 	
 	private List<List<Integer>> generateRandomLottoNumber(int lottoTrials) {
 		List<List<Integer>> randomNumbers = new ArrayList<List<Integer>>();
+		
 		for (int i = 0; i < lottoTrials; i++) {
 			randomNumbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
 		}
 		
 		return randomNumbers;
+	}
+	
+	private int checkCorrectNumber(int index, List<Integer> numbers, List<List<Integer>> randomNumbers) {
+		int correctNumbers = 0;
+		
+		for (int i = 0; i < 6; i++) {
+			if (numbers.contains(randomNumbers.get(index).get(i))){
+				correctNumbers++;
+			}
+		}
+		
+		return correctNumbers;
+	}
+	
+	private int getPrize(int correctNumbers, int index, List<Integer> rankCount) {
+		if (correctNumbers == 6) {
+			rankCount.set(1, rankCount.get(1) + 1);
+			return Rank.FIRST.prize();
+		} else if (correctNumbers == 5 && randomNumbers.get(index).contains(bonusNumber)) {
+			rankCount.set(2, rankCount.get(2) + 1);
+			return Rank.SECOND.prize();
+		} else if (correctNumbers == 5) {
+			rankCount.set(3, rankCount.get(3) + 1);
+			return Rank.THIRD.prize();
+		} else if (correctNumbers == 4) {
+			rankCount.set(4, rankCount.get(4) + 1);
+			return Rank.FOURTH.prize();
+		} else if (correctNumbers == 3) {
+			rankCount.set(5, rankCount.get(5) + 1);
+			return Rank.FIFTH.prize();
+		}
+		return 0;
+	}
+	
+	private List<Integer> initializeCount(){
+		List<Integer> rankCount = new ArrayList<Integer>();
+		
+		for (int i = 0; i < 6; i++) {
+			rankCount.add(0);
+		}
+		
+		return rankCount;
+	}
+	
+	protected int calculatePrize(int lottoTrials, List<Integer> numbers, List<List<Integer>> randomNumbers) {
+		int prize = 0;
+		List<Integer> rankCount = initializeCount();
+		
+		for (int i = 0; i < lottoTrials; i++) {
+			int correctNumbers = checkCorrectNumber(i, numbers, randomNumbers);
+			prize += getPrize(correctNumbers, i, rankCount);
+		}
+		
+		return prize;
+	}
+	
+	protected double calculateReturnRatio(int prize, int lottoTrials) {
+		double ratio = Math.round(prize / lottoTrials) / 10.0;
+		
+		return ratio;
 	}
 }
---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum Rank {
+	FIRST(2000000000, "6개 일치 (2,000,000,000원)"),
+	SECOND(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+	THIRD(1500000, "5개 일치 (150,0000원)"),
+	FOURTH(50000, "4개 일치 (50,000원)"),
+	FIFTH(5000, "3개 일치 (5,000원)");
+	
+	private final int prize;
+	private final String message;
+	
+	Rank (int prize, String message) {
+		this.prize = prize;
+		this.message = message;
+	}
+	
+	public int prize() {
+		return this.prize;
+	}
+	
+	public String message() {
+		return this.message;
+	}
+}

86de01e1a6f96aae80e9138bd38815ec626f26a7
siwol025 <cyk99115695@gmail.com>
2023-11-06 00:15:33 +0900

    test(LottoTest): 원하는 개수만큼 로또가 생성되는지 테스트

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+    @DisplayName("원하는 개수만큼 로또가 생성되는지 테스트")
+    @Test
+    void createLottos() {
+        int lottoAmount = 5;
+
+        Lottos lottos = Lottos.createLottos(lottoAmount);
+        int createdLottoAmount = lottos.getLottoAmount();
+
+        assertThat(createdLottoAmount).isEqualTo(lottoAmount);
+    }
+}

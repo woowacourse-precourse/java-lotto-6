d613047fc0c7e8e615d94fec34c9a41ef03d7909
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-05 18:27:31 +0900

    feat(Lottos): 클래스 생성자 추가

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos(PurchaseAmount purchaseAmount) {
+    }
+}

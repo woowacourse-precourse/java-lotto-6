af7f3a0ce2d694c738865b7eb95b6f2613eaa1df
K-KY <kyuyoungk@naver.com>
2023-11-06 02:19:05 +0900

    Test(ValidateMoney) : ValidateMoney 테스트

---
+++ src/test/java/lotto/ValidateMoneyTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidateMoneyTest {
+
+    @Test
+    void 값이_천보다_큰지_검증() {
+        assertThatThrownBy(() -> ValidateMoney.isOverThousand(999)).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 천원_단위인지_검증() {
+        assertThatThrownBy(() -> ValidateMoney.isUnitFollowed(1234)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 너무_큰_값() {
+        assertThatThrownBy(() -> ValidateMoney.isNumberInRange(1000001)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

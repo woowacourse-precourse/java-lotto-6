78293d182647f7b110611a392b3112c780658930
GaBaljaintheroom <junsu1222@naver.com>
2023-11-05 09:46:53 +0900

    feat(Number): 숫자 Getter 작성

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -8,6 +8,10 @@ public class Number {
         this.value = value;
     }
 
+    public Integer getCurValue() {
+        return value;
+    }
+
     public String getValue() {
         return String.valueOf(value);
     }

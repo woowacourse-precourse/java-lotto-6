e50189294f52b6960645a46d9883ee89e9dbb4cb
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-05 23:17:51 +0900

    feat: 당첨번호를 검증하는 기능 중복, 갯수, 범위를 검증하는 기능

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,12 +1,48 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.utils.Constants;
+import lotto.utils.ErrorMessage;
 
 public class WinningNumbers {
     private List<Integer> winningNumbers;
 
     public WinningNumbers(List<Integer> winningNumbersIntegerList) {
-        // validateWinningNumbers(winningNumbers);
+        validateWinningNumbers(winningNumbersIntegerList);
         this.winningNumbers = winningNumbersIntegerList;
     }
+
+    public void validateWinningNumbers(List<Integer> winningNumbersIntegerList) {
+        validateWinningNumbersRange(winningNumbersIntegerList);
+        validateWinningNumberDuplication(winningNumbersIntegerList);
+        validateWinningNumbersCount(winningNumbersIntegerList);
+
+    }
+
+    private void validateWinningNumbersRange(List<Integer> winningNumbersIntegerList) {
+        for (int number : winningNumbersIntegerList) {
+            if (number < Constants.LOTTO_MIN_NUMBER || number > Constants.LOTTO_MAX_NUMBER) {
+                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE_INVALID_ERROR.getMessage());
+            }
+        }
+    }
+
+    private void validateWinningNumberDuplication(List<Integer> winningNumbersIntegerList) {
+        Set<Integer> numberSet = new HashSet<>();
+        for (int number : winningNumbersIntegerList) {
+            if (!numberSet.add(number)) {
+                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_DUPLICATION_ERROR.getMessage());
+            }
+        }
+    }
+
+    private void validateWinningNumbersCount(List<Integer> winningNumbersIntegerList) {
+        if (winningNumbersIntegerList.size() != Constants.LOTTO_NUMBER_COUNTS) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_COUNT_INVALID_ERROR.getMessage());
+        }
+    }
+
+
 }

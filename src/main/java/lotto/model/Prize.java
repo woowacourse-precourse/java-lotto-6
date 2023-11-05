e5aeff5a46fd55cadbb418a23f9057c8a8365292
WooJJam <jaemin5548@naver.com>
2023-11-06 01:53:47 +0900

    [Style] bonus 여부 true로 변경

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -2,7 +2,7 @@ package lotto.model;
 
 public enum Prize {
     FIRST(6, 2_000_000_000, 0),
-    SECOND(5, 30_000_000, 0, false),
+    SECOND(5, 30_000_000, 0, true),
     THIRD(5, 1_500_000, 0),
     FOURTH(4, 50_000,0),
     FIFTH(3, 5_000,0),

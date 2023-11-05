78deb1f9c2c4d0edd912dd1ab7d58aa1368d8a12
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-05 22:54:32 +0900

    🎯feat(util) : 출력 기능 구현

---
+++ src/main/java/lotto/util/util.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+public class util {
+    public static void println(Object o) {
+        System.out.println(o);
+    }
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+}

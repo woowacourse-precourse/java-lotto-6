7639464b0ab2f81d3ca00fefd814c393c956ce11
Potato-Y <projectpotatoy@gmail.com>
2023-11-05 22:20:33 +0900

    feat: 통계를 위해 필요한 데이터 있는지 확인 로직 추가
    - 기능에 필요한 예외 추가
    - 확인 메서드 추가

--- src/main/java/lotto/service/LottoWalletService.java
+++ src/main/java/lotto/service/LottoWalletService.java
@@ -1,6 +1,10 @@
 package lotto.service;
 
+import java.util.EnumMap;
+import java.util.Map;
+import lotto.exception.NonVariableException;
 import lotto.model.LottoBonusNumber;
+import lotto.model.LottoRank;
 import lotto.model.LottoWallet;
 import lotto.model.LottoWinningNumbers;
 
@@ -9,6 +13,7 @@ public class LottoWalletService {
     private LottoWallet lottoWallet = null;
     private LottoWinningNumbers winningNumbers = null;
     private LottoBonusNumber bonusNumber = null;
+    private Map<LottoRank, Integer> ranks = new EnumMap<>(LottoRank.class);
 
     /**
      * 사용자의 로또 지갑을 저장한다.
@@ -23,4 +28,24 @@ public class LottoWalletService {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
+
+    public void winningStatistics() {
+        readyCheck();
+    }
+
+    /**
+     * 로또 통계 계산에 필요한 데이터가 있는지 확인한다.
+     */
+    private void readyCheck() {
+        if (lottoWallet == null) {
+            throw new NonVariableException("lottoWallet");
+        }
+        if (winningNumbers == null) {
+            throw new NonVariableException("winningNumbers");
+        }
+        if (bonusNumber == null) {
+            throw new NonVariableException("bonusNumber");
+        }
+    }
+
 }

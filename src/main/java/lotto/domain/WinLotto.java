2e2ea79a584e98ac90f6d6ea9afdf816125da97e
gumgu <bienlee@naver.com>
2023-11-06 00:09:14 +0900

    [Feat] 당첨 보너스 번호 범위 검증기능 추가

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 public class WinLotto extends Lotto {
 
+    private static final Integer MINIMUM_LOTTO_RANGE = 1;
+    private static final Integer MAXIMUM_LOTTO_RANGE = 45;
+
     final Integer bonus;
 
     public WinLotto(List<Integer> numbers, Integer bonus) {
         super(numbers);
         validateNumber(numbers, bonus);
+        validateBonusRange(bonus);
         this.bonus = bonus;
     }
 
@@ -16,6 +20,16 @@ public class WinLotto extends Lotto {
         if (numbers.contains(bonus)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateBonusRange(Integer bonus) {
+        if (isInNumberRange(bonus)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInNumberRange(Integer number) {
+        return (number >= MINIMUM_LOTTO_RANGE && number <= MAXIMUM_LOTTO_RANGE);
     }
 
 }

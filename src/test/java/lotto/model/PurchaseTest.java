8d16ad6249861b0bff44348179a6bc83fac5096d
jihun <pjh2996@naver.com>
2023-11-05 22:26:54 +0900

    test : 금액에 맞게 로또의 갯수를 계산하여 저장하는 로직 테스트

--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -9,14 +9,23 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class PurchaseTest {
 
-    private Purchase amount;
-
     @ParameterizedTest
     @CsvSource({"0", "700", "7777", "12500"})
     @DisplayName("구매 금액은 1,000원 단위만 가능하다.")
-    public void inputMoneyValidate(int input) {
-        Assertions.assertThatThrownBy(() -> new Purchase(input))
+    public void inputMoneyValidate(int money) {
+        Assertions.assertThatThrownBy(() -> new Purchase(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(MUST_MONEY_UNIT);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1000,1", "7000,7", "10000,10", "777000,777"})
+    @DisplayName("금액에 맞게 로또의 갯수를 저장한다.")
+    public void purchaseLottoCount(int money, int expect) {
+        Purchase purchase = new Purchase(money);
+
+        int result = purchase.getCount();
+
+        Assertions.assertThat(result).isEqualTo(expect);
     }
 }

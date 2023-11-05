a15466bd3882f3e4e5b0fa26c654e0c564235f30
doka <posmane5@gmail.com>
2023-11-05 14:32:58 +0900

    feat(LottoController) : controller) 사용자 구입 입력값에 대한 로또 발행 요청 기능 추가
    - buyLottoTicketRequest() : LottoStore 객체 생성후 입력값에 맞게 로또 발행 요청후 생성된 LottoStore 객체 반환.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -7,13 +7,20 @@ public class LottoController {
 
     public void start() {
         String lottoPurchaseRequest = lottoOrderRequest();
-
+        LottoStore LottoPurchaseReceipt = buyLottoTicketRequest(lottoPurchaseRequest);
     }
 
     public String lottoOrderRequest() {
 
         String lottoPurchaseRequest = UI.sendLottoPurchaseRequest();
         return lottoPurchaseRequest;
+    }
+
+    public LottoStore buyLottoTicketRequest(String lottoPurchaseRequest) {
+
+        LottoStore lottoStore = new LottoStore();
+        lottoStore.buyLottoTickets(lottoPurchaseRequest);
+        return lottoStore;
     }
 
 }

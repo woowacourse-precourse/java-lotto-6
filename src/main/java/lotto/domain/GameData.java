c7e3aa6e9915c3a153c1c7ad218a4c0fbfb8d00f
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 19:44:02 +0900

    Feat(GameData): 당첨번호, 보너스번호, 생성된 로또 번호들을 관리하는 GameData 클래스 구현

--- src/main/java/lotto/domain/GameData.java
+++ src/main/java/lotto/domain/GameData.java
@@ -1,4 +1,27 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class GameData {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+    private final List<Lotto> lottos;
+
+    public GameData(List<Integer> winningNumbers, int bonusNumber, List<Lotto> lottos) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+        this.lottos = lottos;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }

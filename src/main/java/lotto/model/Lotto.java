5279ac0304526c45f1c3a80e252fcad9900bcb63
jmj005 <jmj00005@naver.com>
2023-11-06 01:34:47 +0900

    refactor: 로또 유효성 검증 메서드 이동

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,10 +1,13 @@
 package lotto.model;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
 
     private final List<Integer> numbers;
 
@@ -16,8 +19,28 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("6개를 입력해야 합니다.");
         }
+        if (isInvalidRange(numbers)) {
+            throw new IllegalArgumentException("1~45 사이만 입력 가능합니다.");
+        }
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException("중복된 번호가 존재합니다.");
+        }
+    }
+
+    private boolean isInvalidRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_RANGE || number > MAX_RANGE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDuplicate(List<Integer> numbers) {
+        HashSet<Integer> unique = new HashSet<>(numbers);
+        return unique.size() != numbers.size();
     }
 
     public List<Integer> getNumbers() {
--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,60 +1,30 @@
 package lotto.model;
 
-import java.util.HashSet;
-import java.util.List;
-
 public class WinningLotto {
-    private static final int MIN_RANGE = 1;
-    private static final int MAX_RANGE = 45;
-    private static final int LOTTO_SIZE = 6;
+    private final Lotto winningLotto;
+    private final BonusNumber bonusNumber;
 
-    private final List<Integer> winningLotto;
-    private final int bonusNumber;
-
-    public WinningLotto(List<Integer> winningLotto, int bonusNumber) {
-        validateWinningLottoRange(winningLotto);
-        validateWinningLottoUnique(winningLotto);
-        validateWinningLottoLength(winningLotto);
-        validateBonusNumberRange(bonusNumber);
-        validateBonusNumberUnique(bonusNumber);
+    public WinningLotto(Lotto winningLotto, BonusNumber bonusNumber) {
+//        validateBonusNumberRange(bonusNumber);
+//        validateBonusNumberRange(bonusNumber);
+//        validateBonusNumberUnique(bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateWinningLottoRange(List<Integer> winningLotto) {
-        for (Integer lottoNumber : winningLotto) {
-            if (!isValidRange(lottoNumber)) {
-                throw new IllegalArgumentException("당첨 번호는 1~45 사이만 입력 가능합니다.");
-            }
-        }
-    }
-
-    private void validateWinningLottoUnique(List<Integer> winningLotto) {
-        HashSet<Integer> unique = new HashSet<>(winningLotto);
-        if (unique.size() != winningLotto.size()) {
-            throw new IllegalArgumentException("중복된 번호가 존재합니다.");
-        }
-    }
-
-    private void validateWinningLottoLength(List<Integer> winningLotto) {
-        if (winningLotto.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("당첨 번호 6개를 입력해야 합니다.");
-        }
-    }
-
-    private void validateBonusNumberRange(int bonusNumber) {
-        if (!isValidRange(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 1~45 사이만 입력 가능합니다.");
-        }
-    }
-
-    private void validateBonusNumberUnique(int bonusNumber) {
-        if (winningLotto.contains(bonusNumber)) {
-            throw new IllegalArgumentException("당첨 번호화 중복된 번호는 입력할 수 없습니다.");
-        }
-    }
-
-    private boolean isValidRange(int number) {
-        return MIN_RANGE <= number && number <= MAX_RANGE;
-    }
+//    private void validateBonusNumberRange(int bonusNumber) {
+//        if (!isValidRange(bonusNumber)) {
+//            throw new IllegalArgumentException("보너스 번호는 1~45 사이만 입력 가능합니다.");
+//        }
+//    }
+//
+//    private void validateBonusNumberUnique(int bonusNumber) {
+//        if (winningLotto.contains(bonusNumber)) {
+//            throw new IllegalArgumentException("당첨 번호화 중복된 번호는 입력할 수 없습니다.");
+//        }
+//    }
+//
+//    private boolean isValidRange(int number) {
+//        return MIN_RANGE <= number && number <= MAX_RANGE;
+//    }
 }

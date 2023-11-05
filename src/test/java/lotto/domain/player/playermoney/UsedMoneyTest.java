93d3842668724d56fb33abf1501e729dc7e9a424
newh08 <newh08@gmail.com>
2023-11-06 06:35:06 +0900

    🔥️REFACTOR: UsedMoneyTest 단순 로직 테스트 삭제.

--- src/test/java/lotto/domain/player/playermoney/UsedMoneyTest.java
+++ src/test/java/lotto/domain/player/playermoney/UsedMoneyTest.java
@@ -11,14 +11,6 @@ import org.junit.jupiter.api.Test;
 
 public class UsedMoneyTest {
 
-    @DisplayName("사용하는 금액 만큼 usedMoney 가 업데이트 된다.")
-    @Test
-    void ConsumeMoneyTest() {
-        UsedMoney usedMoney = makeZeroUsedMoney();
-        usedMoney = usedMoney.updateUsedMoney(2000);
-        assertThat(usedMoney.getUsedMoney()).isEqualTo(2000);
-    }
-
     @DisplayName("8000원 사용 해 5000원이 당첨되면 profit 은 62.5 가 된다.")
     @Test
     void ProfitCalculateTest() {

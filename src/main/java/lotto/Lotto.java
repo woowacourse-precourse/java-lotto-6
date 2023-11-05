a994f07764da8dafe75ba4e9e65e2adcfa416dd6
Heojamong <wlaud0823@naver.com>
2023-11-06 02:14:08 +0900

    feat: LottoTest 중복된 숫자 체크 코드 구현 셋을 이용하여 numbers의 사이즈와 set의 사이즈를 비교하여 사이즈가 다를경우 중복된 숫자가 있는 것으로 판결

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicationValidate(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +19,15 @@ public class Lotto {
         }
     }
 
+    private void duplicationValidate(List<Integer> numbers) {
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() < numbers.size();
+    }
     // TODO: 추가 기능 구현
 }

24df73104134499365b6abbc105f5a11025bb5b0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-05 21:44:32 +0900

    feat(inputView): 구입 금액이 숫자가 아닌 경우 예외 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,13 @@ package lotto.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    private static final String ONLY_NUMERIC_INPUT_FOR_MONEY = "[ERROR] 구입금액은 숫자만 입력 가능합니다.";
     public int inputMoney() {
-        return Integer.parseInt(readLine());
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            new IllegalArgumentException(ONLY_NUMERIC_INPUT_FOR_MONEY);
+        }
+        return inputMoney();
     }
 }

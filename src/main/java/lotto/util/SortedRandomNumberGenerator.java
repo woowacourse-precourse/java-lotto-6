672134d304903630d4877473e4b75910d7ed3cc7
김민정 <mjeong@sookmyung.ac.kr>
2023-11-05 19:02:37 +0900

    refactor: 로또 숫자 생성 클래스 이동

--- src/main/java/lotto/util/SortedRandomNumberGenerator.java
+++ src/main/java/lotto/util/SortedRandomNumberGenerator.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class SortedRandomNumberGenerator {
     public static final int START_RANGE = 1;
     public static final int END_RANGE = 45;
-    private static final int COUNT = 6;
+    public static final int COUNT = 6;
 
     public List<Integer> generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, COUNT);

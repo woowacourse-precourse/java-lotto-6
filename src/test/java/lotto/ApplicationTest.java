384cff8b2b8562d5e42bca9bb3c32ab9ccfb9aa9
whxogus215 <whxogus215@naver.com>
2023-11-05 10:51:50 +0900

    test(OutputView): 3등일 때, 수익률 테스트

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -5,7 +5,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.text.DecimalFormat;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -115,6 +114,40 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 기능_테스트_3등_수익률() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 2, 3, 4, 5, 10]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 18,750.0%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 2, 3, 4, 5, 10)
+        );
+    }
+
+    @Test
     void 기능_테스트_4등_수익률() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
@@ -181,42 +214,6 @@ class ApplicationTest extends NsTest {
                 List.of(11, 12, 13, 14, 15, 17)
         );
     }
-
-    @Test
-    void 소수점_테스트() {
-        long winningPrice = 5500;
-        int inputPrice = 8000;
-        double rate = (double) winningPrice / inputPrice;
-        System.out.println(rate);
-        DecimalFormat decimalFormat = new DecimalFormat("###,###.0%");
-        String format = decimalFormat.format(rate);
-        System.out.println(format);
-
-        long winningPrice2 = 0;
-        int inputPrice2 = 8000;
-        double rate2 = (double) winningPrice2 / inputPrice2;
-        System.out.println(rate2);
-        DecimalFormat decimalFormat2 = new DecimalFormat("###,###.0%");
-        String format2 = decimalFormat2.format(rate2);
-        System.out.println(format2);
-
-        long winningPrice3 = 2000000000;
-        int inputPrice3 = 8000;
-        double rate3 = (double) winningPrice3 / inputPrice3;
-        System.out.println(rate3);
-        DecimalFormat decimalFormat3 = new DecimalFormat("###,###.0%");
-        String format3 = decimalFormat3.format(rate3);
-        System.out.println(format3);
-
-        long winningPrice4 = 253;
-        int inputPrice4 = 5000;
-        double rate4 = (double) winningPrice4 / inputPrice4;
-        System.out.println(rate4);
-        DecimalFormat decimalFormat4 = new DecimalFormat("###,###.0%");
-        String format4 = decimalFormat4.format(rate4);
-        System.out.println(format4);
-    }
-
 
     @Test
     void 예외_테스트() {

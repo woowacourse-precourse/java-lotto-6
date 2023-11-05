d21f2edc5b5adcf0b84967879bc09187bc89a05d
chae401 <chae401@sju.ac.kr>
2023-11-05 16:32:09 +0900

    test(LottoTest): 당첨번호와의 비교

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,14 +1,22 @@
-package lotto;
-
-import lotto.model.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import lotto.model.dto.WinningNumDTO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoTest {
+    WinningNumDTO winningNum;
+
+    @BeforeEach
+    void beforeEach() {
+        winningNum = WinningNumDTO.of("1,2,3,4,5,6", "7");
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,6 +32,38 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("view 에 전달할 로또 번호의 문자열 생성")
+    @Test
+    void getSelectedNumbers() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        // when
+        String selectedNumbers = lotto.getSelectedNumbers();
+        // then
+        Assertions.assertThat(selectedNumbers).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
 
+    @DisplayName("[로또 결과] 당첨 번호와 동일한 수가 3개 있고, 보너스 번호는 맞지 않는 경우")
+    @Test
+    void 당첨번호와_동일한수_3_보너스_false() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
+        // when
+        Ranking ranking = lotto.checkResult(winningNum);
+        Ranking expectedRanking = Ranking.of(3, false);
+        // then
+        Assertions.assertThat(ranking).isEqualTo(expectedRanking);
+    }
+
+    @DisplayName("[로또 결과] 당첨 번호와 동일한 수가 5개, 보너스 번호가 맞을 경우")
+    @Test
+    void 당첨번호와_동일한수_5_보너스_true() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        // when
+        Ranking ranking = lotto.checkResult(winningNum);
+        Ranking expectedRanking = Ranking.of(5, true);
+        // then
+        Assertions.assertThat(ranking).isEqualTo(expectedRanking);
+    }
 }

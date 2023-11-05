6359003edfafd93764e03e714737d9ade03f99ce
sangxxjin <sj_0368@naver.com>
2023-11-05 16:14:11 +0900

    feat: 천원 단위로 입력하지 않았을때 예외처리 구현 및 상수처리

--- src/main/java/lotto/model/LottoTicketQuantity.java
+++ src/main/java/lotto/model/LottoTicketQuantity.java
@@ -4,14 +4,23 @@ public class LottoTicketQuantity {
 
     public int lottoTicketQuantity;
     private static final int LOTTO_PRICE = 1000;
+    private static final String THOUSANDS_ERROR_MESSAGE = "[ERROR] 천원 단위로 입력해주세요.";
 
     public int LottoTicketQuantity(int purchaseAmount) {
+        nonDivisibleAmount(purchaseAmount);
         this.lottoTicketQuantity = calculateLottoTicketQuantity(purchaseAmount);
+
         return lottoTicketQuantity;
     }
 
     private int calculateLottoTicketQuantity(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    private void nonDivisibleAmount(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(THOUSANDS_ERROR_MESSAGE);
+        }
     }
 
 }

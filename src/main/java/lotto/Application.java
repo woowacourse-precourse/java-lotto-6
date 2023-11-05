155a27cdbb1ec0bc7f50e0b31a4febf8a888eb93
yoosusang <dbtntkd456@naver.com>
2023-11-06 01:06:37 +0900

    feat: Application 추가 - 사용자가 구매 금액을 입력한다. - 호스트가 구매 금액에 맞게 로또를 발행한다. 이떄 숫자는 6개로 랜덤하게 설정한다. - 사용자가 당첨 번호와 보너스 번호를 입력한다. - 호스트가 당첨을 통계한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.object.Host;
+import lotto.object.Lotto;
+import lotto.object.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User user = new User();
+        Host host = new Host();
+        user.purchaseAmountInput();
+        host.lottoIssuance(user);
+        user.lottoWinningNumberInput();
+        user.bonusNumberInput();
+        host.winningStatistics(user);
+
     }
 }

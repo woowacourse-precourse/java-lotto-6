9fd08df985fc1a4d71c428851cd2bf0aa0efc7d3
wns312 <wns312@gmail.com>
2023-11-06 02:14:14 +0900

    test(LottoPurchaseCostTest): 구매 금액 테스트코드 작성

---
+++ src/test/java/lotto/domain/model/LottoPurchaseCostTest.java
@@ -0,0 +1,80 @@
+package lotto.domain.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.constant.IllegalArgumentExceptionType;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+class LottoPurchaseCostTest {
+    private static final int ONE = 1;
+    private static final int INVALID_PURCHASE_AMOUNT_ZERO = 0;
+    private static final int INVALID_PURCHASE_AMOUNT_NEGATIVE_THOUSAND = -1000;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MAX_PURCHASE_AMOUNT = 2_000_000_000;
+
+    @Test
+    void 최소금액_실패_테스트() {
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThatThrownBy(() -> new LottoPurchaseCost(INVALID_PURCHASE_AMOUNT_ZERO))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentExceptionType.INSUFFICIENT_MONEY.getMessage());
+        softAssertions.assertThatThrownBy(() -> new LottoPurchaseCost(INVALID_PURCHASE_AMOUNT_NEGATIVE_THOUSAND))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentExceptionType.INSUFFICIENT_MONEY.getMessage());
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    void 최대금액_실패_테스트() {
+        int exceededAmount = MAX_PURCHASE_AMOUNT + LOTTO_PRICE;
+
+        assertThatThrownBy(() -> new LottoPurchaseCost(exceededAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentExceptionType.MAX_MONEY_EXCEEDED.getMessage());
+    }
+
+    @Test
+    void 금액_나머지_실패_테스트() {
+        int remainFailAmount = LOTTO_PRICE + ONE;
+
+        assertThatThrownBy(() -> new LottoPurchaseCost(remainFailAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentExceptionType.MONEY_REMAINS_EXIST.getMessage());
+    }
+
+    @Test
+    void 성공_테스트() {
+        int exampleAmount = LOTTO_PRICE * LOTTO_PRICE;
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThatCode(() -> new LottoPurchaseCost(LOTTO_PRICE)).doesNotThrowAnyException();
+        softAssertions.assertThatCode(() -> new LottoPurchaseCost(MAX_PURCHASE_AMOUNT)).doesNotThrowAnyException();
+        softAssertions.assertThatCode(() -> new LottoPurchaseCost(exampleAmount)).doesNotThrowAnyException();
+        softAssertions.assertAll();
+    }
+
+    @Test
+    void 로또_구매_비용_조회_테스트() {
+        LottoPurchaseCost lottoPurchaseCost1 = new LottoPurchaseCost(LOTTO_PRICE);
+        LottoPurchaseCost lottoPurchaseCost2 = new LottoPurchaseCost(MAX_PURCHASE_AMOUNT);
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(lottoPurchaseCost1.getCost()).isEqualTo(LOTTO_PRICE);
+        softAssertions.assertThat(lottoPurchaseCost2.getCost()).isEqualTo(MAX_PURCHASE_AMOUNT);
+        softAssertions.assertAll();
+    }
+
+    @Test
+    void 로또_구매_개수_테스트() {
+        LottoPurchaseCost lottoPurchaseCost1 = new LottoPurchaseCost(LOTTO_PRICE);
+        LottoPurchaseCost lottoPurchaseCost2 = new LottoPurchaseCost(MAX_PURCHASE_AMOUNT);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(lottoPurchaseCost1.getLottoAmount()).isEqualTo(ONE);
+        softAssertions.assertThat(lottoPurchaseCost2.getLottoAmount())
+                .isEqualTo(MAX_PURCHASE_AMOUNT / LOTTO_PRICE);
+        softAssertions.assertAll();
+    }
+}

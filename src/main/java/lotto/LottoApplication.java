3fcea2cfada32c7b05d9142ad952de45a0365362
unknown <hakie2kim@gmail.com>
2023-11-05 15:24:30 +0900

    feat(src/main/java/lotto/): get picked numbers from the buyer

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -3,14 +3,26 @@ package lotto;
 import View.Input;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoApplication {
     private static int LOTTO_PRICE = 1000;
 
     void execute() {
         int receivedAmount = getReceivedAmount();
+
+        Lotto pickedNumbers = getPickedNumbers();
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < getNumberOfLotto(receivedAmount); i++) {
+            lottos.add(drawLotto());
+        }
     }
 
-    int getReceivedAmount() {
+    private int getReceivedAmount() {
         int receivedAmount;
 
         try {
@@ -29,11 +41,24 @@ public class LottoApplication {
         }
     }
 
+    private Lotto getPickedNumbers() {
+        try {
+            List<Integer> pickedNumbers = Arrays.stream(Input.readPickedNumbers().trim().split(","))
+                    .map(Integer::valueOf)
+                    .toList(); // String[]을 List<Integer>로 변환 
+            
+            return new Lotto(pickedNumbers);
+        } catch(NumberFormatException nfe) { // picekdNumbers 중 정수값이 없을 경우
+            throw new IllegalArgumentException();
+        }
+        
+    }
+
     private Lotto drawLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(Lotto.START_NUMBER, Lotto.END_NUMBER, 6));
     }
 
-    private int decideNumberOfLotto(int receivedAmount) {
+    private int getNumberOfLotto(int receivedAmount) {
         return receivedAmount / LOTTO_PRICE;
     }
 }

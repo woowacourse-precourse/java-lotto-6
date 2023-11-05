0b5d07daafd7b0a2eca1ff826c0a8541ebf72a50
ddingmin <ddingminyt@gmail.com>
2023-11-05 22:49:55 +0900

    구현: 당첨 로또 번호 클래스를 구현한다.
    - 일반 번호를 추가한다.
    - 보너스 번호를 추가한다.
    - 당첨 번호와 일치하는지 확인한다.
    - 보너스 번호와 일치하는지 확인한다.
    - 당첨 번호의 개수가 올바른지 확인한다.
    - 보너스 번호가 존재하는지 확인한다.

---
+++ src/main/java/lotto/domain/LottoWinner.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoWinner {
+	public static final int SIZE = 6;
+	public static final String DUPLICATE_LOTTO_NUMBER_MESSAGE = "[Error] 중복된 로또 값이 존재합니다.";
+	private final List<LottoNumber> normalLotto = new ArrayList<>();
+	private LottoNumber bonusNumber;
+
+	public void addNormal(LottoNumber lottoNumber) {
+		validate(lottoNumber);
+		normalLotto.add(lottoNumber);
+	}
+
+	public void addBonus(LottoNumber lottoNumber) {
+		validate(lottoNumber);
+		bonusNumber = lottoNumber;
+	}
+
+	public boolean hasNormal(LottoNumber lottoNumber) {
+		return normalLotto.contains(lottoNumber);
+	}
+
+	public boolean hasBonus(LottoNumber lottoNumber) {
+		return bonusNumber.equals(lottoNumber);
+	}
+
+	public boolean isCorrectSize() {
+		return normalLotto.size() == SIZE;
+	}
+
+	public boolean hasCorrectBonusNumber() {
+		return bonusNumber != null;
+	}
+
+	private void validate(LottoNumber lottoNumber) {
+		if (isDuplicate(lottoNumber)) {
+			throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_MESSAGE);
+		}
+	}
+
+	private boolean isDuplicate(LottoNumber lottoNumber) {
+		return normalLotto.contains(lottoNumber);
+	}
+
+}

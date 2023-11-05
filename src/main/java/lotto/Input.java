164f5ab91382e8ec9ee9a4efa81976203288e61e
KIVSH <kihongkim3@hotmail.com>
2023-11-05 10:41:35 +0900

    feat: update all method

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,9 +1,42 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
-    public static int amount() {
-        return Integer.parseInt(Console.readLine());
+    public static int purchaseAmount() throws IllegalArgumentException {
+        int amount = 0;
+
+        try {
+            amount = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException();
+        }
+
+        return amount;
+    }
+
+    public static List<Integer> winNumbers() {
+        String[] numbers = Console.readLine().split(",");
+        List<Integer> winNumbers = new ArrayList<>();
+
+        for (String number : numbers) {
+            winNumbers.add(Integer.parseInt(number));
+        }
+
+        return winNumbers;
+    }
+
+    public static int bonusNumber() throws IllegalArgumentException {
+        int bonus = 0;
+
+        try {
+            bonus = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException();
+        }
+
+        return bonus;
     }
 }

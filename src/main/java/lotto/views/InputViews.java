4b344eaa4b636f3db57098475602d74daa5dca41
74059 <shyeone@naver.com>
2023-11-05 13:13:41 +0900

    refactor: 하드 코딩한 부분 static final 이용하여 수정

--- src/main/java/lotto/views/InputViews.java
+++ src/main/java/lotto/views/InputViews.java
@@ -3,8 +3,9 @@ package lotto.views;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputViews {
+    static final String PURCHASE_INPUT_MESS = "구입금액을 입력해 주세요.";
     public static String readPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PURCHASE_INPUT_MESS);
         return Console.readLine();
     }
 }

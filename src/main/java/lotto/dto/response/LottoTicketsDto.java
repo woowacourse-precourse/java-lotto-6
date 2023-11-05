f76dbbe66d9f172f83ede6d8904803fde7037069
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-06 02:24:57 +0900

    refactor(LottoTicketsDto): Lottos 객체를 저장하도록 변경

--- src/main/java/lotto/dto/response/LottoTicketsDto.java
+++ src/main/java/lotto/dto/response/LottoTicketsDto.java
@@ -1,23 +1,28 @@
 package lotto.dto.response;
 
 import lotto.model.Lotto;
+import lotto.model.Lottos;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoTicketsDto {
-    private final List<String> lottoTickets;
+    private final Lottos lottoTickets;
 
-    public LottoTicketsDto(List<Lotto> lottos) {
-        this.lottoTickets = lottos.stream()
+    private LottoTicketsDto(Lottos lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public static LottoTicketsDto from(Lottos lottoTickets) {
+        return new LottoTicketsDto(lottoTickets);
+    }
+
+    public List<String> getFormattedLottoTickets() {
+        return lottoTickets.getLottoTickets().stream()
                 .map(lotto -> lotto.getNumbers().stream()
                         .map(Object::toString)
                         .collect(Collectors.joining(", ")))
                 .collect(Collectors.toList());
-    }
-
-    public List<String> getLottoTickets() {
-        return lottoTickets;
     }
 }

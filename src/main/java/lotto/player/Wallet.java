1f58ffdba13bc1fdc48f62e0b97cda4a14fd09c6
gimdongha <d11210920@gmail.com>
2023-11-05 17:00:30 +0900

    feat(Wallet) : 구매 금액을 저장하고 검증하는 로직
    - buyMoney를 입력받는 inputBuyMoney 메소드
    - 숫자만 입력됐는지를 검증하는 checkChar 메소드
    - checkChar 메소드를 실행한 후 1000원 단위인지, 금액이 입력이 됐는지를 검증하는 validateMoney 메소드

--- src/main/java/lotto/player/Wallet.java
+++ src/main/java/lotto/player/Wallet.java
@@ -26,4 +26,28 @@ public class Wallet {
             System.out.println(lotto.getLottoInfo());
         }
     }
+
+    public void inputBuyMoney() {
+        System.out.println(BUYMESSAGE);
+        String input;
+        input = readLine();
+        validateMoney(input);
+        buyMoney = Integer.parseInt(input);
+        System.out.println();
+    }
+
+    public void validateMoney(String input) {
+        checkChar(input);
+        int money = Integer.parseInt(input);
+        if (money % PRICE.getValue() != 0)
+            throw new IllegalArgumentException("[ERROR] " + PRICE.getValue() + "원 단위로 입력해주세요");
+        if (money == 0) throw new IllegalArgumentException("[ERROR] 금액을 입력해 주세요");
+    }
+
+    public void checkChar(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) < '0' || input.charAt(i) > '9')
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요");
+        }
+    }
 }

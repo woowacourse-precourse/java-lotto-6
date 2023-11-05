0b3f9b077a288bf862fba484214bb7817120faa3
SeongIl <biuld1234@gmail.com>
2023-11-05 14:37:10 +0900

    refactor: change ranking -> description - 사용하지 않는 랭킹을 출력문으로 변경

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,20 +1,19 @@
 package lotto;
 
 public enum Prize {
-
-    FIRST(6, "1등", 2_000_000_000),
-    SECOND(5, "2등", 30_000_000),
-    THIRD(5, "3등", 1_500_000),
-    FOURTH(4, "4등", 50_000),
-    FIFTH(3, "5등", 5_000);
+    FIRST(6, "6개 일치 (2,000,000,000원) - ", 2_000_000_000),
+    SECOND(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30_000_000),
+    THIRD(5, "5개 일치 (1,500,000원) - ", 1_500_000),
+    FOURTH(4, "4개 일치 (50,000원) - ", 50_000),
+    FIFTH(3, "3개 일치 (5,000원) - ", 5_000);
 
     private final Integer matchingNumber;
-    private final String ranking;
+    private final String description;
     private final Integer prizeAmount;
 
-    Prize(Integer matchingNumber, String ranking, Integer prizeAmount) {
+    Prize(Integer matchingNumber, String description, Integer prizeAmount) {
         this.matchingNumber = matchingNumber;
-        this.ranking = ranking;
+        this.description = description;
         this.prizeAmount = prizeAmount;
     }
 
@@ -22,8 +21,8 @@ public enum Prize {
         return matchingNumber;
     }
 
-    public String getRanking() {
-        return ranking;
+    public String getDescription() {
+        return description;
     }
 
     public int getPrizeAmount() {

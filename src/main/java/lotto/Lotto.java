cb301a40f713bd7b6704c38d21ea16e854831349
SunHyeok <cghhld@naver.com>
2023-11-05 14:25:25 +0900

    refector: 유효성 검사 코드 모듈화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,9 +11,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumberOfNumbers(numbers);
+        validateRangeOfNumbers(numbers);
+    }
+
+    private void validateNumberOfNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validateRangeOfNumbers(List<Integer> numbers) {
         for (int number : numbers) {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");

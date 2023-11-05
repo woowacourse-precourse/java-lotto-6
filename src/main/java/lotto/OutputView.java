9e0bdc058b86922f4d6ed105af312c3c21bc8e58
dokkisan <dokkisan.wang@gmail.com>
2023-11-06 03:31:27 +0900

    feat(output-view): 구매한 로또 개수 출력 기능 구현

---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class OutputView {
+
+    public void print(int purchaseQuantity) {
+        final String message = "개를 구매했습니다.";
+        System.out.println(purchaseQuantity + message);
+    }
+}

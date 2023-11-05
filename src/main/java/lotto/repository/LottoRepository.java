54722449279678fb35ed685aade1addb7b2149ea
김승우 <tmd8633@naver.com>
2023-11-05 18:49:18 +0900

    Refactor : Callback 패턴 동적처리, 인자 3개 이하 유지로 변경

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -1,5 +1,6 @@
 package lotto.repository;
 
+import lotto.template.Callback;
 import lotto.template.InputTemplate;
 import lotto.valid.Valid;
 
@@ -11,12 +12,12 @@ public class LottoRepository {
 
     private final InputTemplate inputTemplate = new InputTemplate();
     public int getPrice() {
-        String price = inputTemplate.execute("구입금액을 입력해 주세요.", text -> Valid.validPrice(text), null);
+        String price = inputTemplate.execute("구입금액을 입력해 주세요.", text -> Valid.validPrice(text));
         return Integer.parseInt(price);
     }
 
     public List<Integer> getWinNum() {
-        String winNumStr = inputTemplate.execute("당첨 번호를 입력해 주세요.", text -> Valid.validWinNum(text), null);
+        String winNumStr = inputTemplate.execute("당첨 번호를 입력해 주세요.", text -> Valid.validWinNum(text));
         String[] split = winNumStr.split(",");
         return Arrays.stream(split)
             .mapToInt(Integer::parseInt)
@@ -25,7 +26,7 @@ public class LottoRepository {
     }
 
     public int getBonusNum(List<Integer> winNum) {
-        String bonusNumStr = inputTemplate.execute("보너스 번호를 입력해 주세요.", text -> Valid.validBonusNum(text), winNum);
+        String bonusNumStr = inputTemplate.execute("보너스 번호를 입력해 주세요.", text -> Valid.distinctCheck(winNum, text));
         return Integer.parseInt(bonusNumStr);
     }
 
--- src/main/java/lotto/template/InputTemplate.java
+++ src/main/java/lotto/template/InputTemplate.java
@@ -8,20 +8,19 @@ import java.util.NoSuchElementException;
 
 public class InputTemplate {
 
-    public String execute(String message, Callback callback, List<Integer> winNum) {
+    public String execute(String message, Callback callback) {
         try {
             System.out.println(message);
             String text = Console.readLine();
             callback.valid(text);
-            Valid.distinctCheck(winNum, text);
             System.out.println();
             return text;
         } catch (NoSuchElementException e) {
             System.out.println("[ERROR] 값이 존재하지 않습니다.");
-            return execute(message, callback, winNum);
+            return execute(message, callback);
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] " + e.getMessage());
-            return execute(message, callback, winNum);
+            return execute(message, callback);
         }
     }
 

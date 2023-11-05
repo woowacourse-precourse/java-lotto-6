542737dcebdf5fd2b29c0283e3cebafb68ef629b
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 19:58:49 +0900

    Feat(LottoPurchaseVerifier): 로또 구매 금액 숫자인지 1000의 배수인지 판별하는 verifyInt 메서드 구현

--- src/main/java/lotto/verifier/LottoPurchaseVerifier.java
+++ src/main/java/lotto/verifier/LottoPurchaseVerifier.java
@@ -7,11 +7,25 @@ public class LottoPurchaseVerifier {
 
     public LottoPurchaseVerifier(String purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
+        verifyNull();
+        verifyInt();
     }
 
-    public void verifyNull(int purchaseAmount) {
+    public void verifyNull() {
         if (this.purchaseAmount.isEmpty()) {
             throw new IllegalArgumentException(ExceptionMsg.NULL_INPUT.getMessage());
+        }
+    }
+
+    public void verifyInt(){
+        try {
+            int purchaseAmountInt = Integer.parseInt(purchaseAmount);
+            if(purchaseAmountInt % 1000 != 0){
+                throw new IllegalArgumentException(ExceptionMsg.NOT_MULTIPLE_THOUSAND.getMessage());
+            }
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(ExceptionMsg.NOT_INT_INPUT.getMessage());
+
         }
     }
 

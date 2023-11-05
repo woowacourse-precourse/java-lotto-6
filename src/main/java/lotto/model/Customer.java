6aa32849b3570f4b291513f7bc47b1f0c5d73e25
ehtjsv2 <ehtjsv2@naver.com>
2023-11-05 22:12:32 +0900

    Feat: 로또를 구매하는 기능 구현

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -1,4 +1,11 @@
 package lotto.model;
 
+import java.util.List;
+
 public class Customer {
+    private List<Lotto> lottos;
+
+    public void buyLotto(Money money){
+        this.lottos = LottoGenerator.issueLottoTickets(money);
+    }
 }
---
+++ src/test/java/lotto/CustomerTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import lotto.model.Customer;
+import lotto.model.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+
+    @DisplayName("고객이 로또를 살 수 있습니다.")
+    @Test
+    void buyLotto() {
+        // given
+        Customer customer = new Customer();
+        Money money = new Money("3000");
+        // when
+
+        // then
+        assertDoesNotThrow(() -> customer.buyLotto(money));
+    }
+}

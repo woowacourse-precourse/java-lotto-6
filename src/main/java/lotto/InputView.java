0a882c92eefc1b09d6b99a7f77b3b3e29b02e8da
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-11-05 23:27:27 +0900

    refactor: 구입금액 입력 기능 수정

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,8 +7,7 @@ public class InputView {
     public static int inputMoney(){
         System.out.println("구입금액을 입력해 주세요.");
         String userMoney = Console.readLine();
-        int userInputMoney = Integer.parseInt(userMoney);
-        return userInputMoney;
+        return InputValidator.purchaseMoney(userMoney);
     }
 
 }

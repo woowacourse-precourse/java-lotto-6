86460816432a7a4b80e9b6f1a1f6091e83374b87
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 01:41:23 +0900

    feat(BonusNumber): 보너스 번호 클래스 및 검증 로직 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,2 +1,19 @@
-package lotto.domain;public class BonusNumber {
+package lotto.domain;
+
+public class BonusNumber {
+    private final int MIN_LOTTO_NUMBER = 1;
+    private final int MAX_LOTTO_NUMBER = 1;
+
+    Integer number;
+
+    public BonusNumber(Integer number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(Integer number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

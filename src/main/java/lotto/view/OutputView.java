dc317ad8f0c611e54cee23bda6f908112195cc68
wonjun <wonjon0930@daum.net>
2023-11-05 20:46:14 +0900

    chore: rename OutputConstant.newLine to NEW_LINE

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,15 +5,15 @@ import lotto.view.message.OutputMessage;
 
 public class OutputView {
     public void askUserToInsertMoneyToBuyLotto(){
-        printMessage(OutputMessage.ASK_TO_INSERT_MONEY_FOR_LOTTO.getMessage() + OutputConstant.newLine);
+        printMessage(OutputMessage.ASK_TO_INSERT_MONEY_FOR_LOTTO.getMessage() + OutputConstant.NEW_LINE);
     }
 
     public void askUserToInsertLottoWinningNumbers(){
-        printMessage(OutputMessage.ASK_TO_INSERT_LOTTO_WINNING_NUMBERS.getMessage() + OutputConstant.newLine);
+        printMessage(OutputMessage.ASK_TO_INSERT_LOTTO_WINNING_NUMBERS.getMessage() + OutputConstant.NEW_LINE);
     }
 
     public void askUserToInsertBonusNumber(){
-        printMessage(OutputMessage.ASK_TO_INSERT_LOTTO_BONUS_NUMBER.getMessage() + OutputConstant.newLine);
+        printMessage(OutputMessage.ASK_TO_INSERT_LOTTO_BONUS_NUMBER.getMessage() + OutputConstant.NEW_LINE);
     }
 
     private void printMessage(String message){
--- src/main/java/lotto/view/constant/OutputConstant.java
+++ src/main/java/lotto/view/constant/OutputConstant.java
@@ -1,5 +1,5 @@
 package lotto.view.constant;
 
 public class OutputConstant {
-    public static final String newLine = System.lineSeparator();
+    public static final String NEW_LINE = System.lineSeparator();
 }

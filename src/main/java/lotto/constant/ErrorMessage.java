f3f82868002522e8608619a013a154cb3068be97
ikjo39 <chi4321@naver.com>
2023-11-06 01:14:22 +0900

    feat: 로또구매자 클래스 및 구매 금액 검증 로직 추가
    - 구매 금액이 0원 이하면 예외 발생
    - 구매 금액이 로또 한개 가격으로 나눠떨어지지 않으면 예외발생

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
     INITIAL_ERROR_TEXT("[ERROR] "),
     BLANK_INPUT("입력값은 공백일 수 없습니다."),
     INPUT_NOT_DIGIT("입력값은 숫자여야 합니다."),
-    EXCEEDED_MAXIMUM_NUMBER_FORMAT(String.format("최대로 입력할 수 있는 수를 초과합니다. (최대: %d)", Long.MAX_VALUE));
+    EXCEEDED_MAXIMUM_NUMBER_FORMAT(String.format("최대로 입력할 수 있는 수를 초과합니다. (최대: %d)", Long.MAX_VALUE)),
+    MONEY_LESS_THAN_ZERO("구매금액은 0 이하일 수 없습니다."),
+    MONEY_WITH_REMAINDER("구매금액은 1개의 로또 금액으로 나누어 떨어져야합니다.");
 
     private final String message;
 
---
+++ src/main/java/lotto/model/LottoOwner.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import static lotto.constant.ErrorMessage.MONEY_LESS_THAN_ZERO;
+import static lotto.constant.ErrorMessage.MONEY_WITH_REMAINDER;
+
+public class LottoOwner {
+    private static final long LOTTO_PRICE = 1000;
+
+    private final long money;
+
+    private LottoOwner(long money) {
+        validateMoneyMoreThanZero(money);
+        validateDividedByDivisorWithoutRemainder(money);
+        this.money = money;
+    }
+
+    public static LottoOwner from(long money) {
+        return new LottoOwner(money);
+    }
+
+    private void validateMoneyMoreThanZero(long money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException(MONEY_LESS_THAN_ZERO.toString());
+        }
+    }
+
+    private void validateDividedByDivisorWithoutRemainder(long money) {
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(MONEY_WITH_REMAINDER.toString());
+        }
+    }
+}

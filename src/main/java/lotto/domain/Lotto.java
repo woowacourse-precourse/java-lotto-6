2c6671bdfe890ea69654be09c1e8573004c3db41
Jiwon Lee <jiwon718@users.noreply.github.com>
2023-11-05 23:57:59 +0900

    feat: 로또 번호 중복 확인 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,15 +4,29 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    public static final int NUMBERS_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateNumbers(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateNumbersValue(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_SIZE) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumbersValue(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (numbers.contains(number)) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 

e3ff17ab528babaf6e07e36156cba256a09a33eb
Ju-Won99 <wndnjs1102@naver.com>
2023-11-05 14:20:32 +0900

    feat : 로또 생성자 생성 및 생성관련 오류 추가

---
+++ src/main/java/lotto/constants/ExceptionMessages.java
@@ -0,0 +1,22 @@
+package lotto.constants;
+
+public enum ExceptionMessages {
+    NULL_INPUT("null은 입력할 수 없습니다."),
+    EMPTY_INPUT("값을 입력해주세요."),
+    NOT_NUMERIC_INPUT("숫자를 입력해주세요."),
+    WRONG_RANGE_INPUT("1,000원 이상의 구매 금액을 입력해주세요."),
+    NOT_DIVISIBLE_INPUT("1,000원 단위의 구매 금액을 입력해주세요."),
+    WRONG_AMOUNT_LOTTO_NUMBERS("로또 구매를 위한 6개의 당첨번호를 입력해주세요."),
+    WRONG_RANGE_LOTTO_NUMBERS("로또 번호는 1~45의 숫자를 입력해주세요.");
+
+    private static final String ERROR_TAG = "[ERROR] ";
+    private final String exceptionMessages;
+
+    ExceptionMessages(String exceptionMessages) {
+        this.exceptionMessages = ERROR_TAG + exceptionMessages;
+    }
+
+    public void throwException() {
+        throw new IllegalArgumentException(exceptionMessages);
+    }
+}
---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import lotto.constants.ExceptionMessages;
+import lotto.constants.LottoConstants;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = List.copyOf(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (isNotSixNumbers(numbers)) {
+            ExceptionMessages.WRONG_AMOUNT_LOTTO_NUMBERS.throwException();
+        }
+        if (isWrongRangeNumbers(numbers)) {
+            ExceptionMessages.WRONG_RANGE_LOTTO_NUMBERS.throwException();
+        }
+    }
+
+    private boolean isNotSixNumbers(List<Integer> numbers) {
+        return numbers.size() != LottoConstants.NUMBER_OF_LOTTO_NUMBERS;
+    }
+
+    private boolean isWrongRangeNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LottoConstants.MINIMUM_LOTTO_NUMBER || number > LottoConstants.MAXIMUM_LOTTO_NUMBER) {
+               return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}

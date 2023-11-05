69af2c107e5c0c22ff25f0c27660566ecf42d449
GO-TE <alsrudxo9810@naver.com>
2023-11-06 01:05:25 +0900

    feat(InputView): 구입금액 입력 기능 구현 inputBudget() : - 로또 구입 금액을 입력한다 - 1000원으로 나누어 떨어지지 않을때 예외 처리한다 - 정수가 아닐 때 예외 처리한다 - 구입금액을 리턴한다

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public int inputBudget() { // 기능 나누기 -> validator
+        int intBudget;
+        String budget = Console.readLine();
+        try {
+            intBudget = Integer.parseInt(budget);
+            if (intBudget % 1000 != 0) { // 하드코딩 제거
+                throw new IllegalArgumentException("1000원 단위 정수로 입력해 주세요."); // 하드코딩 제거
+            }
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("1000원 단위 정수로 입력해 주세요."); // 하드코딩 제거
+        }
+        return intBudget;
+    }
+
+
+}

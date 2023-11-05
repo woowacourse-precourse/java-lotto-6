cfc5e5bfdaf8f32729b442476ade8c8435c798f1
hil679 <hil679922@gmail.com>
2023-11-06 03:19:55 +0900

    Feat(sumWinMoney): 얻은 돈의 합을 계산하는 기능 구현

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -1,12 +1,20 @@
 package lotto.domain;
 
+import lotto.util.WinnerRank;
+
 public class Profit {
 
     public float calculateProfitRate(PurchasePrice purchasePrice, WinResult winResult) {
         return 0f;
     }
 
-    private long calculateWinMoney(WinResult winResult) {
-        return 0L;
+    private float sumWinMoney(WinResult winResult) {
+        float moneyPrize = 0;
+        for(WinnerRank winnerRank : WinnerRank.values()){
+            int key = winnerRank.getValue();
+            moneyPrize += winResult.getWinResultValue(key) * winnerRank.getPrizeMoney();
+        }
+
+        return moneyPrize;
     }
 }

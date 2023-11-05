69747b5ca452b88a4cf21646ac71be6ba36e33ff
김민정 <mjeong@sookmyung.ac.kr>
2023-11-05 14:07:11 +0900

    feat: 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     public static int getAmount() {
@@ -13,5 +15,28 @@ public class InputView {
                 throw new IllegalArgumentException();
             }
         }
+    }
+
+    public static List<Integer> getWinningNumber() {
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                return convertStringToList(input);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static List<Integer> convertStringToList(String input) {
+        String[] splitValues = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String value : splitValues) {
+            String trimmedValue = value.trim();
+            int number = Integer.parseInt(trimmedValue);
+            numbers.add(number);
+        }
+        return numbers;
     }
 }

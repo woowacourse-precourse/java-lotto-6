1d184da9f391dd192bb28ffe19c8dda7e3383ae1
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-05 20:04:15 +0900

    feat(Lottos): 각 로또 번호의 비교결과 제공 기능 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,7 +1,11 @@
 package lotto.model;
 
+import lotto.config.Rank;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -14,4 +18,18 @@ public class Lottos {
         return new Lottos(lottos);
     }
 
+    public Map<Rank, Long> calculateResults(List<Integer> winningNumbers, int bonusNumber) {
+        return lottos.stream()
+                .map(lotto -> determineRank(lotto, winningNumbers, bonusNumber))
+                .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
+    }
+
+    private Rank determineRank(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
+        long matchCount = lotto.getNumbers().stream()
+                .filter(winningNumbers::contains)
+                .count();
+        boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
+
+        return Rank.valueOf(matchCount, bonusMatch);
+    }
 }

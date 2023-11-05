4f5b12a8ff780b85189bc1c2ff0c504ba07f711f
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-05 12:42:30 +0900

    feat(Buyer): 구매자 클래스 Buyer 추가
    객체 생성시 구매할 복권 수량과 복권 머신을 파라미터로 입력 받는다.

---
+++ src/main/java/lotto/Buyer.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+
+public class Buyer {
+    private final int purchaseAmount;
+    private final LottoMachine lottoMachine;
+    private List<Lotto> myLotteries;
+
+
+    public Buyer(int purchaseAmount, LottoMachine lottoMachine) {
+        this.purchaseAmount = purchaseAmount;
+        this.lottoMachine = lottoMachine;
+    }
+}

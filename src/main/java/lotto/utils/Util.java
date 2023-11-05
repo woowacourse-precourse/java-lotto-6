ef40223a90e3367c54f5788c48c9af616d98d1d5
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 02:21:18 +0900

    feat: Util 클래스 추가 기능 구현

--- src/main/java/lotto/utils/Util.java
+++ src/main/java/lotto/utils/Util.java
@@ -1,3 +1,26 @@
 package lotto.utils;
 
-public class Util {}
+import static lotto.utils.Constants.DELIMETER;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.view.ExceptionMessage;
+
+public class Util {
+    public static int convertToInt(String inputNumber) {
+        try {
+            return Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
+            ExceptionMessage.numberFormatException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static List<Integer> numberList(String winningNumber) {
+        return Arrays.stream(winningNumber.split(DELIMETER))
+                .map(String::trim) // to remove any leading or trailing spaces
+                .map(Util::convertToInt) // assuming convertToInt is a static method in Util
+                .collect(Collectors.toList());
+    }
+}

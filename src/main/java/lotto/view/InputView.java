bd863727f4fdd8b76b9055f549fd6ee90904876e
LUKR7Q <ybkim0628@naver.com>
2023-11-05 22:48:52 +0900

    inputPlayerAmount(): 플레이어로부터 금액을 입력받는 메서드입니다.

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputView {
+    private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_LOTTO_WINNING = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    private static List<Integer> winningNumberList;
+
+    public static String inputPlayerAmount() {
+        System.out.println(INPUT_LOTTO_AMOUNT);
+        return Console.readLine();
+    }
+
+}

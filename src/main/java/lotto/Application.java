cdec789c9904a8003aa4e73acddefb218734674f
rakyun1 <dbskim234@naver.com>
2023-11-04 22:55:37 -0600

    Refactor: refactor Application 사용자의 입력 값이 에러를 던질 경우에 프로그램이 끝나지 않고 다시 돌도록 수정했고, Applicatoin 안에서 가지고 있던 로또 등수를 Result 클라스를 추가해서 분리함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,70 +2,61 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        List<Lotto> lottos = new ArrayList<>();
-        ValidateInput validate = new ValidateInput();
-        LottoNumberFactory factory = new LottoNumberFactory();
 
-        System.out.println("구입금액을 입력해 주세요.");
-        String tryCount = readLine();
-        int lottoPrice = validate.validateLottoPrice(tryCount);
+	public static void main(String[] args) {
+		ValidateInput validate = new ValidateInput();
+		LottoNumberFactory factory = new LottoNumberFactory();
+		System.out.println("구입금액을 입력해 주세요.");
 
-        System.out.println();
-        System.out.println(lottoPrice+"개를 구매했습니다.");
+		int count = -1;
 
-        lottos = factory.makeLotto(lottoPrice);
+		while (count < 0){
+			try {
+				String moneyInput = readLine();
+				count = validate.validateLottoPrice(moneyInput);
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
 
-        System.out.println();
-        System.out.println("당첨 번호를 입력해주세요.");
-        int[] winningNumbers = validate.validateWinningNumber(readLine());
-        System.out.println();
+		System.out.println(count+"개를 구매했습니다.");
 
-        System.out.println("보너스 번호를 입력하세요.");
-        int bonus = validate.validateBonus(readLine());
-        System.out.println();
+		List<Lotto> lottos = factory.makeLotto(count);
 
-        Arrays.sort(winningNumbers);
-        int total = 0;
+		System.out.println();
+		System.out.println("당첨 번호를 입력해주세요.");
+		String winningInput = readLine();
+		List<Integer> winningNumbers = validate.validateWinningNumber(winningInput);
 
-        List<RankEnum> ranks = new ArrayList<>();
+		System.out.println();
+		System.out.println("보너스 번호를 입력하세요.");
 
-        for (Lotto lotto : lottos) {
-           RankEnum rank = lotto.checkRank(bonus, winningNumbers);
-           ranks.add(rank);
-        }
+		final int bonus = validate.validateBonus(readLine());
 
-        List<RankEnum> first = ranks.stream().filter(s -> s == RankEnum.FIRST).toList();
-        List<RankEnum> second = ranks.stream().filter(s -> s == RankEnum.SECOND).toList();
-        List<RankEnum> third = ranks.stream().filter(s -> s == RankEnum.THIRD).toList();
-        List<RankEnum> forth = ranks.stream().filter(s -> s == RankEnum.FORTH).toList();
-        List<RankEnum> fifth = ranks.stream().filter(s -> s == RankEnum.FIFTH).toList();
+		List<Result> results = lottos.stream().map(lotto -> lotto.checkRank(winningNumbers, bonus)).toList();
+		int total = results.stream().mapToInt(Result::getPrize).sum();
 
-        total = ranks.stream().mapToInt(Application::prize).sum();
+		double profitRate = ((double) (total * 100) / (count * 1000));
+		profitRate = Math.round(profitRate * 100.0) / 100.0;
 
-        double profitRate = ((double) (total - lottoPrice * 1000) / (lottoPrice * 1000)) * 100;
+		List<Result> first = results.stream().filter(result -> result.getRank() == RankEnum.FIRST).toList();
+		List<Result> second = results.stream().filter(result -> result.getRank() == RankEnum.SECOND).toList();
+		List<Result> third = results.stream().filter(result -> result.getRank() == RankEnum.THIRD).toList();
+		List<Result> forth = results.stream().filter(result -> result.getRank() == RankEnum.FORTH).toList();
+		List<Result> fifth = results.stream().filter(result -> result.getRank() == RankEnum.FIFTH).toList();
 
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5000원)" + fifth.size());
-        System.out.println("4개 일치 (50000원)" + forth.size());
-        System.out.println("5개 일치 (1,500,000원)" + third.size());
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원)" + second.size());
-        System.out.println("6개 일치 (2,000,000,000원)" + first.size());
-        System.out.printf("총 수익률은 %.2f%% 입니다.\n", profitRate);
-    }
+		System.out.println();
+		System.out.println("당첨 통계");
+		System.out.println("---");
+		System.out.println("3개 일치 (5,000원) - " + fifth.size() + "개");
+		System.out.println("4개 일치 (50,000원) - " + forth.size() + "개");
+		System.out.println("5개 일치 (1,500,000원) - " + third.size() + "개");
+		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second.size() + "개");
+		System.out.println("6개 일치 (2,000,000,000원) - " + first.size() + "개");
+		System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+	}
 
-    public static int prize(RankEnum rank){
-        if (rank == RankEnum.FIRST) return 2000000000;
-        if (rank == RankEnum.SECOND) return 30000000;
-        if (rank == RankEnum.THIRD) return 1500000;
-        if (rank == RankEnum.FORTH) return 50000;
-        if (rank == RankEnum.FIFTH) return 5000;
-        return 0;
-    }
 }

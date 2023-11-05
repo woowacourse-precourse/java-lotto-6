19690bb7fafba398306a041c2fd135fabff72355
HeeHeeg <qmffndoden@naver.com>
2023-11-05 15:50:54 +0900

    refactor(lotto): 로또 생성하는 로직 메서드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -15,15 +15,8 @@ public class Application {
         int numberOfLottos = userPurchaseAmount / 1000;
         System.out.println(numberOfLottos + "개를 구매했습니다.");
 
-        List<Lotto> purchasedLottos = new ArrayList<>();
-        for (int i = 0; i < numberOfLottos; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Lotto lotto = new Lotto(numbers);
-            List<Integer> sortedNumbers = new ArrayList<>(lotto.getNumbers());
-            sortedNumbers.sort(Comparator.naturalOrder());
-            System.out.println(sortedNumbers);
-            purchasedLottos.add(lotto);
-        }
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Lotto> purchasedLottos = lottoGenerator.generateLottos(numberOfLottos);
 
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoGenerator {
+
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public List<Lotto> generateLottos(int numberOfLottos) {
+        List<Lotto> purchasedLottos = new ArrayList<>();
+        for (int i = 0; i < numberOfLottos; i++) {
+            Lotto lotto = generateSingleLotto();
+            List<Integer> sortedNumbers =getSortedLottoNumbers(lotto);
+            System.out.println(sortedNumbers);
+            purchasedLottos.add(lotto);
+        }
+        return purchasedLottos;
+    }
+
+    private Lotto generateSingleLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
+        Lotto lotto = new Lotto(numbers);
+        return lotto;
+    }
+
+    private List<Integer> getSortedLottoNumbers(Lotto lotto) {
+        List<Integer> sortedNumbers = new ArrayList<>(lotto.getNumbers());
+        sortedNumbers.sort(Comparator.naturalOrder());
+        return sortedNumbers;
+    }
+}

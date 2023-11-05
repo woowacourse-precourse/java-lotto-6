143f7d026284df1f8b284881ae7ab6eae8f0a76c
seminchoi <choicco89@gmail.com>
2023-11-05 14:58:10 +0900

    feat(domain.Payment) : 로또 가격 단위에 맞는 금액인지 검증하는 기능 구현

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,4 +1,24 @@
 package lotto.constant;
 
 public enum ErrorMessage {
+    PREFIX("[ERROR] %s"),
+    INDIVISIBLE_AMOUNT(String.format(
+            "%d원 단위의 금액을 입력해야 합니다.",
+            LottoConstraint.PRICE_PER_LOTTO.getValue()
+    )),
+    OUT_RANGE_AMOUNT(String.format(
+            "%d원 이상, %d원 이하의 금액을 입력해야 합니다.",
+            LottoConstraint.MIN_AMOUNT.getValue(),
+            LottoConstraint.MAX_AMOUNT.getValue()
+    )),
+    ;
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return String.format(PREFIX.message, this.message);
+    }
 }
--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -1,10 +1,28 @@
 package lotto.domain;
 
+import lotto.constant.ErrorMessage;
+import lotto.constant.LottoConstraint;
+
 public class Payment {
     private final long amount;
 
     public Payment(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        validateDivisibleAmount(amount);
+    }
+
+    private void validateDivisibleAmount(int amount) {
+        if (isIndivisibleAmount(amount)) {
+            throw new IllegalArgumentException(ErrorMessage.INDIVISIBLE_AMOUNT.getMessage());
+        }
+    }
+
+    private boolean isIndivisibleAmount(int amount) {
+        return amount % LottoConstraint.PRICE_PER_LOTTO.getValue() != 0;
     }
 
     public int calculatePurchaseLottoCount() {

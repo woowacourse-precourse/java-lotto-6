fe823df2cfd85959729247ef631520a985415ece
HiiWee <wpdlzhf159@gmail.com>
2023-11-05 21:33:22 +0900

    refactor: 일급컬렉션 도메인 getter 불변 컬렉션 반환하도록 변경

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -1,8 +1,14 @@
 package lotto.domain.lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public record Lottos(List<Lotto> lottos) {
+
+    @Override
+    public List<Lotto> lottos() {
+        return Collections.unmodifiableList(lottos);
+    }
 
     public List<LottoRewardCondition> createCompareResults(final WinningLotto winningLotto) {
         return lottos.stream()

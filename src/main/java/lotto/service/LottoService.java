60803b03cd75ba201f32558c499e4a25f6383224
Mr.Kim <joajy98@gmail.com>
2023-11-06 01:19:54 +0900

    refactor: 구분 문자 상수 선언 및 static 메서드 가독성을 위한 클래스 add on demand

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,37 +4,37 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 import lotto.domain.Rank;
 import lotto.domain.WinningNumbers;
-import lotto.validator.PurchaseAmountValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static lotto.validator.LottoValidator.*;
+import static lotto.validator.PurchaseAmountValidator.*;
+import static lotto.view.InputView.*;
 
 public class LottoService {
 
     private static final int DIVIDER_MONEY_TO_EXCHANGE_AMOUNT = 1000;
+    private static final String DELIMITER = ",";
 
     public static int enterPurchaseAmount() {
-        InputView.noticeToGetPurchaseAmount();
+        noticeToGetPurchaseAmount();
         String inputMoney = Console.readLine();
         int money = Integer.parseInt(inputMoney);
         //천 원 단위로 입력받았는지 검증
-        PurchaseAmountValidator.validateMoney(inputMoney, DIVIDER_MONEY_TO_EXCHANGE_AMOUNT);
+        validateMoney(inputMoney, DIVIDER_MONEY_TO_EXCHANGE_AMOUNT);
 
         OutputView.printPurchaseAmount(money);
         return money / DIVIDER_MONEY_TO_EXCHANGE_AMOUNT;
     }
 
     public static Lotto enterWinningNumbers() {
-        InputView.noticeToGetWinningNumbers();
-        String[] numbers = Console.readLine().split(",");
+        noticeToGetWinningNumbers();
+        String[] numbers = Console.readLine().split(DELIMITER);
         //중복 값 입력받았는지 여부 확인
-//        LottoValidator.
-        List<Integer> inputNumbers = new ArrayList<>();
-        for (String number : numbers) {
-            inputNumbers.add(Integer.parseInt(number));
-        }
+        List<Integer> inputNumbers = validateLottoNumbers(numbers);
+
         return new Lotto(inputNumbers);
     }
 

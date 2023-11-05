4774b78fd49d1d431716da7a7ee59474a6dbfa0f
noxknow <chris2769@naver.com>
2023-11-05 16:37:47 +0900

    feat : 구입 금액이 1000원으로 나눠 떨어지는지 확인하는 기능 구현

--- src/main/java/lotto/domain/wrapper/Money.java
+++ src/main/java/lotto/domain/wrapper/Money.java
@@ -1,7 +1,6 @@
-package lotto.domain;
+package lotto.domain.wrapper;
 
-import static lotto.handler.ErrorHandler.INVALID_NEGATIVE_INPUT;
-import static lotto.handler.ErrorHandler.INVALID_TYPE;
+import static lotto.handler.ErrorHandler.*;
 
 public class Money {
 
@@ -10,6 +9,7 @@ public class Money {
     private Money(String money) {
         this.purchaseAmount = validateType(money);
         validateMoney(purchaseAmount);
+        validateDivision(purchaseAmount);
     }
 
     public static Money create(String money) {
@@ -25,8 +25,14 @@ public class Money {
     }
 
     private void validateMoney(int purchaseAmount) {
-        if (purchaseAmount < 0) {
+        if (purchaseAmount <= 0) {
             throw INVALID_NEGATIVE_INPUT.getException();
+        }
+    }
+
+    private void validateDivision(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw INVALID_DIVISION.getException();
         }
     }
 }
--- src/main/java/lotto/handler/ErrorHandler.java
+++ src/main/java/lotto/handler/ErrorHandler.java
@@ -3,7 +3,8 @@ package lotto.handler;
 public enum ErrorHandler {
 
     INVALID_TYPE("숫자로 변환될 수 없는 타입입니다."),
-    INVALID_NEGATIVE_INPUT("0미만의 값은 들어갈 수 없습니다.");
+    INVALID_NEGATIVE_INPUT("0이하의 값은 들어갈 수 없습니다."),
+    INVALID_DIVISION("입력값은 1000으로 나누어 떨어져야 합니다.");
 
     private final String errorMessage;
 

ffe107e41d65da440f2f35a78e01ed6efd0e4706
cherBlair <cher.blair.h@gmail.com>
2023-11-05 14:29:35 +0900

    feat: 로또 당첨 번호 입력 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
         lottoController.buyLotto();
+        lottoController.checkLottoResult();
 
     }
 }

647c986cd9bec1113ffe904ab63d9b8428bc6166
ohhyeonn <ohhyeon120@gmail.com>
2023-11-06 07:16:33 +0900

    feat : add test 랜덤 넘버 생성함수가 중복되지 않는 6자리수를 리턴하는지 체크
    add checkLottoNumberDuplicated

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.Service.LottoMachine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class LottoMachineTest {
@@ -14,6 +15,17 @@ public class LottoMachineTest {
         Integer lottoCount = LottoMachine.lottoCount(price);
 
         assertThat(lottoCount).isEqualTo(7);
+    }
+
+
+    @DisplayName("랜덤 넘버 생성함수가 중복되지 않는 6자리수를 리턴하는지 체크")
+    @Test
+    void checkLottoNumberDuplicated() {
+        List<Integer> randomNumbers = LottoMachine.makeRandomNumbers();
+
+        int count = (int) randomNumbers.stream().distinct().count();
+
+        assertThat(count).isEqualTo(6);
     }
 
 }

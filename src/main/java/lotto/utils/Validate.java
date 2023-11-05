60ad64eeba17c46dabc78c1b6741622d7f1f5885
ghdeo <goldencorn7@naver.com>
2023-11-05 22:05:43 +0900

    feat: Add purchaseAmount validator

--- src/main/java/lotto/utils/Validate.java
+++ src/main/java/lotto/utils/Validate.java
@@ -1,5 +1,18 @@
 package lotto.utils;
 
 public class Validate {
+    public static int purchaseAmount(String userInput) {
+        try {
+            int amount = Integer.parseInt(userInput);
+
+            if (amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력 하세요.");
+            }
+
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력 하세요.");
+        }
+    }
 
 }

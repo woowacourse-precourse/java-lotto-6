2df3010f14b634daaa7dab2c1e7116d89a0ff833
itsmo1031 <itsmo1031@gmail.com>
2023-11-05 16:49:09 +0900

    test(exception): 모든 ExceptionMessage Enum에 대해 테스트하도록 변경
    @ParameterizedTest와 @EnumSource를 활용하여 모든 Enum 상수에 대해 테스트하도록 구조 변경

--- src/test/java/lotto/constant/ExceptionMessageTest.java
+++ src/test/java/lotto/constant/ExceptionMessageTest.java
@@ -3,15 +3,14 @@ package lotto.constant;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 public class ExceptionMessageTest {
-    @Test
+    @ParameterizedTest
     @DisplayName("Exception 메세지는 [ERROR]로 시작해야 한다.")
-    void exceptionMessagePrefix() {
-        // given
-        ExceptionMessage exceptionMessage = ExceptionMessage.DUPLICATE;
-
+    @EnumSource(value = ExceptionMessage.class)
+    void exceptionMessagePrefix(ExceptionMessage exceptionMessage) {
         // when
         String expectExceptionMessagePrefix = "[ERROR]";
 

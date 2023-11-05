d490dc30f1fc124ea6eeae2c40d961f7f9f71393
voyager003 <miraclex000@gmail.com>
2023-11-05 12:27:49 +0900

    feat(LottoController): add method to LottoController
    ADD inputWinningNumber() method
    - return WinningNumber and catch exception

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,12 +6,14 @@ import lotto.View.InputView;
 import lotto.View.OutputView;
 import lotto.model.LottoAmount;
 import lotto.model.LottoTickets;
+import lotto.model.WinningNumber;
 
 public class LottoController {
 
     public void start() {
         LottoAmount amount = inputAmount();
         buyLotto(amount);
+        inputWinningNumber();
     }
 
     private LottoAmount inputAmount() {
@@ -29,5 +31,15 @@ public class LottoController {
         LottoTickets lottoTickets = new LottoTickets(amount.getLottoCount());
         OutputView.printLottoCount(amount.getLottoCount());
         OutputView.printLottoTickets(lottoTickets);
+    }
+
+    private WinningNumber inputWinningNumber() {
+        try {
+            String winningNum = InputView.inputWinningLottoNumbers();
+            return new WinningNumber(winningNum);
+        } catch (IllegalArgumentException e) {
+            ErrorMessageView.printErrorWrongNumberRange();
+            return inputWinningNumber();
+        }
     }
 }

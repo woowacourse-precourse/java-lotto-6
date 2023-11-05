0fcb9a93e3ea8ecf5a604e37a8ae7f7fe2534ba8
msung99 <msung6924@naver.com>
2023-11-05 11:38:35 +0900

    feat: 당첨 번호 입력 요구 출력문 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -27,6 +27,7 @@ public class LottoController {
     }
 
     private WinningLotto drawLotto(){
+        InputView.printRequireWinningNumbersMessage();
         Lotto lotto = lottoGenerator.generateWinningLotto();
         Bonus bonus = Bonus.from(InputView.getBonusInput());
         return WinningLotto.of(lotto, bonus);
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,8 @@ import lotto.domain.Budget;
 
 public class InputView {
     private static final String REQUIRE_BUDGET_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String REQUIRE_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String REQUIRE_BONUS_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    private static final String REQUIRE_WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
 
     public static String getBudgetInput(){
         System.out.println(REQUIRE_BUDGET_MESSAGE);
@@ -15,5 +16,9 @@ public class InputView {
     public static String getBonusInput(){
         System.out.println(REQUIRE_BONUS_MESSAGE);
         return Console.readLine();
+    }
+
+    public static void printRequireWinningNumbersMessage(){
+        System.out.println(REQUIRE_WINNING_NUMBERS_MESSAGE);
     }
 }

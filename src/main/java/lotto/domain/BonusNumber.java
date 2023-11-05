b84123766420924ce518e6ea3609bb87237f995a
yubin <dbqls2821@gmail.com>
2023-11-05 22:32:17 +0900

    feat: BonusNumber validation 로직 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,4 +1,18 @@
 package lotto.domain;
 
 record BonusNumber(Integer bonus, Lotto lotto) {
+    BonusNumber {
+        validateRange();
+        validateDuplication();
+    }
+
+    void validateRange() {
+        if(bonus < Constraint.MIN.getValue() || bonus > Constraint.MAX.getValue())
+            throw new IllegalArgumentException(DomainException.BONUS_RANGE.getMessage());
+    }
+
+    void validateDuplication() {
+        if(lotto.isContainBonusNumber(bonus))
+            throw new IllegalArgumentException(DomainException.BONUS_DUPLICATION.getMessage());
+    }
 }

f6a05173615a6886507d7c3fd89ffa77b650b356
freekyr7529 <freekyr7529@gmail.com>
2023-11-05 12:07:02 +0900

    feat: 입력 기능 구현
    - 입력 기능을 담당하는 InputHandler 클래스를 구현하였다.
    - 로또 구입 금액을 입력받는 inputPrice 메서드
    - 당첨 번호를 입력받는 inputAnswerNumbers 메서드
    - 보너스 번호를 입력받는 inputBonusNumber 메서드
    - 당첨 번호와 보너스 번호간의 중복을 확인하는 isAnswerHasThatNumber 메서드

---
+++ src/main/java/lotto/InputHandler.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputHandler {
+    private static final InputValidator validator = new InputValidator();
+
+    public int inputPrice() {
+        String input = Console.readLine();
+        return validator.validatePrice(input);
+    }
+
+    public List<String> inputAnswerNumbers() {
+        String input = Console.readLine();
+        return validator.validateAnswerNumbers(input);
+    }
+
+    public int inputBonusNumber(List<String> answer) {
+        String input = Console.readLine();
+        boolean duplicatedFlag = isAnswerHasThatNumber(answer, input);
+        return validator.validateBonusNumber(input, duplicatedFlag);
+    }
+
+    private boolean isAnswerHasThatNumber(List<String> answer, String bonusNumber) {
+        return answer.contains(bonusNumber);
+    }
+}

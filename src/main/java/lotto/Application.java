8aa9d85ddf77309c6d71f9bafbced236b587f3f4
방준호 <yureka0920@naver.com>
2023-11-05 21:20:32 +0900

    3. 당첨번호를 입력받는다. 6개 입력이며 1~45까지의 범위내의 중복되지 않아야함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,10 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -13,11 +10,43 @@ public class Application {
     public static void main(String[] args) {
         int inputTry = readInputPrice();
         List<Lotto> lottoList = makeLottoNumber(inputTry);
+        List<Integer> correctNumberList= readInputCorrectNumber();
+    }
+
+    private static List<Integer> readInputCorrectNumber() {
+        List<Integer> correctNumberList = new ArrayList<>();
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String inputCorrectNumber = readLine();
+                for (String correctNumber : inputCorrectNumber.split(",")) {
+                    int correctNum = Integer.parseInt(correctNumber);
+                    validateOverNumber45(correctNum);
+                    correctNumberList.add(correctNum);
+                }
+                validateListSizeIs6(correctNumberList.size(), 6, "[ERROR] 6개의 당첨번호를 입력해주세요");
+                return correctNumberList;
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void validateListSizeIs6(int size, int i, String s) {
+        if ((size) != i) {
+            throw new IllegalArgumentException(s);
+        }
+    }
+
+    private static void validateOverNumber45(int correctNum) {
+        if (correctNum > 45) {
+            throw new IllegalArgumentException("[ERROR] 45이하의 숫자를 선택해주세요");
+        }
     }
 
     private static List<Lotto> makeLottoNumber(int inputTry) {
         List<Lotto> lottoList = new ArrayList<>();
-        for(int i = 0; i< inputTry; i++){
+        for (int i = 0; i < inputTry; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(numbers);
             Lotto lotto = new Lotto(numbers);
@@ -32,12 +61,12 @@ public class Application {
 
     private static int readInputPrice() {
         int inputTry;
-        while(true){
+        while (true) {
             try {
                 System.out.println("구입금액을 입력해 주세요.");
                 int inputPrice = Integer.parseInt(readLine());
                 validateThousandUnit(inputPrice);
-                inputTry= (inputPrice/1000);
+                inputTry = (inputPrice / 1000);
                 return inputTry;
             } catch (IllegalArgumentException e) {
                 e.printStackTrace();
@@ -45,9 +74,7 @@ public class Application {
         }
     }
 
-    private static void validateThousandUnit (int inputPrice) {
-        if((inputPrice %1000)!=0){
-            throw new IllegalArgumentException("[ERROR] 구입금액 단위는 1000원으로 나누어 떨어져야 합니다.");
-        }
+    private static void validateThousandUnit(int inputPrice) {
+        validateListSizeIs6(inputPrice % 1000, 0, "[ERROR] 구입금액 단위는 1000원으로 나누어 떨어져야 합니다.");
     }
 }

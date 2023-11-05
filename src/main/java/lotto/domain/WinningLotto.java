a0bd901c7432eb8e366dc297d2c43018af47da48
doha <dohaahi@naver.com>
2023-11-06 03:40:39 +0900

    refactor (WinningLotto): 멤버 변수 지역 변수로 수정

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,10 +4,8 @@ import java.util.List;
 import lotto.validator.WinningLottoValidator;
 
 public class WinningLotto {
-    private final List<Integer> numbers;
 
     public WinningLotto(List<Integer> numbers) {
         WinningLottoValidator.validateWinningNumbers(numbers);
-        this.numbers = numbers;
     }
 }

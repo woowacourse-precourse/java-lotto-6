d04e0eb4b572b7fd6599abcd9dfc0fc66a385b3f
shining013 <nevertheless113@naver.com>
2023-11-05 19:37:49 +0900

    modify: purchaseInput 메서드 View로 이동

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static void printPurchasePrompt() {
+    public static String printPurchasePrompt() {
         System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
     }
 
 }

34291c18149975a2ccb33fe6310811f78d0577da
dltmddus <tmddus4671@gmail.com>
2023-11-05 14:45:02 +0900

    feat: ERROR 발생 후 그 시점부터 재시작 기능 구현

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,10 +1,11 @@
 package constant;
 
 public class ErrorMessage {
-    public static final String PURCHASE_TYPE = "구매 금액은 숫자로만 이루어져야 합니다.";
-    public static final String PURCHASE_REMAINDER = "구매 금액은 1000으로 나누어 떨어져야 합니다.";
-    public static final String LOTTO_NUMBER_COUNT_SIX = "당첨 번호는 6개입니다.";
-    public static final String LOTTO_NUMBER_DUPLICATE = "당첨 번호는 중복될 수 없습니다.";
-    public static final String LOTTO_NUMBER_RANGE = "당첨 번호는 1이상 45이하입니다.";
-    public static final String BONUS_TYPE = "보너스 번호는 숫자로만 이루어져야 합니다.";
+    private static final String ERROR = "[ERROR] ";
+    public static final String PURCHASE_TYPE = ERROR + "구매 금액은 숫자로만 이루어져야 합니다.";
+    public static final String PURCHASE_REMAINDER = ERROR + "구매 금액은 1000으로 나누어 떨어져야 합니다.";
+    public static final String LOTTO_NUMBER_COUNT_SIX = ERROR + "당첨 번호는 6개입니다.";
+    public static final String LOTTO_NUMBER_DUPLICATE = ERROR + "당첨 번호는 중복될 수 없습니다.";
+    public static final String LOTTO_NUMBER_RANGE = ERROR + "당첨 번호는 1이상 45이하입니다.";
+    public static final String BONUS_TYPE = ERROR + "보너스 번호는 숫자로만 이루어져야 합니다.";
 }
--- src/main/java/lotto/LottoRun.java
+++ src/main/java/lotto/LottoRun.java
@@ -7,20 +7,62 @@ import util.ValidationUtil;
 import java.util.List;
 
 public class LottoRun {
-//    private final InputUtil inputUtil = new InputUtil();
-//    private final ValidationUtil validationUtil = new ValidationUtil();
     private final MessageUtil messageUtil = new MessageUtil();
     private final LottoPurchase lottoPurchase = new LottoPurchase();
     private final LottoWinningNumber lottoWinningNumber = new LottoWinningNumber();
     private final LottoBonusNumber lottoBonusNumber = new LottoBonusNumber();
-//    private final LottoResultCompute lottoResultCompute = new LottoResultCompute();
-    public void run(){
+    public void run() {
         messageUtil.printPurchaseAmount();
-        LottoPurchase purchaseInfo = lottoPurchase.getLottoPurchaseInfo();
-        LottoWinningNumber winningNumInfo = lottoWinningNumber.getLottoWinningNumInfo();
-        LottoBonusNumber bonusNumInfo = lottoBonusNumber.getLottoBonusNumInfo();
-        LottoResultCompute resultCompute = new LottoResultCompute(winningNumInfo, bonusNumInfo);
+
+        LottoPurchase purchaseInfo = null;
+        LottoWinningNumber winningNumInfo = null;
+        LottoBonusNumber bonusNumInfo = null;
+        LottoResultCompute resultCompute;
+
+        purchaseInfo = getValidLottoPurchaseInfo(purchaseInfo);
+        winningNumInfo = getValidWinningNumInfo(winningNumInfo);
+        bonusNumInfo = getValidBonusNumInfo(bonusNumInfo);
+        resultCompute = new LottoResultCompute(winningNumInfo, bonusNumInfo);
         resultCompute.getLottoResultComputeInfo(purchaseInfo.getUserLottos(), purchaseInfo.getUserLottoAmount());
     }
 
+    private LottoPurchase getValidLottoPurchaseInfo(LottoPurchase purchaseInfo){
+        purchaseInfo = null;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                purchaseInfo = lottoPurchase.getLottoPurchaseInfo();
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return purchaseInfo;
+    }
+    private LottoWinningNumber getValidWinningNumInfo(LottoWinningNumber winningNumInfo) {
+        winningNumInfo = null;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                winningNumInfo = lottoWinningNumber.getLottoWinningNumInfo();
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return winningNumInfo;
+    }
+    private LottoBonusNumber getValidBonusNumInfo(LottoBonusNumber bonusNumInfo) {
+        bonusNumInfo = null;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                bonusNumInfo = lottoBonusNumber.getLottoBonusNumInfo();
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return bonusNumInfo;
+    }
 }

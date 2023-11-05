819215156c9a669387b82d0260eef8f35c9b710f
Hojoomoney <kimhojoo51@gmail.com>
2023-11-05 22:09:52 +0900

    Feat : 오름차순으로 정렬하는 메소드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,48 +1,49 @@
 package lotto;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Lotto {
-    private final List<Integer> numbers;
-
+	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        checkBoundOf(numbers);
-        checkDuplicate(numbers);
-        this.numbers = numbers;
-    }
+		validate(numbers);
+		checkBoundOf(numbers);
+		checkDuplicate(numbers);
+		this.numbers = new ArrayList<>(numbers);
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자입니다.");
+		}
+	}
 
-    // TODO: 추가 기능 구현
-    
-    // 1부터 45이하인 숫자인지 검사하는 메소드
-    private void checkBoundOf(List<Integer> numbers) {
-    	for (int number : numbers) {
-    		if (number < 1 || number > 45) {
-    			throw new IllegalStateException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-    		}
-    	}
-    }
-    
-    // 중복되는 숫자가 있는지 확인하는 메소드
-    private void checkDuplicate(List<Integer> numbers) {
-    	Set<Integer> newNumbers = new HashSet<>(numbers);
-    	if(newNumbers.size() != numbers.size()) {
-    		throw new IllegalStateException("[ERROR] 중복되지 않는 서로 다른 숫자 6개를 입력해주세요.");
-    	}
-    }
-    
-    
-    public List<Integer> getNumbers() {
-    	return numbers;
-    }
-    
+	// TODO: 추가 기능 구현
+
+	// 1부터 45이하인 숫자인지 검사하는 메소드
+	private void checkBoundOf(List<Integer> numbers) {
+		for (int number : numbers) {
+			if (number < 1 || number > 45) {
+				throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+			}
+		}
+	}
+
+	// 중복되는 숫자가 있는지 확인하는 메소드
+	private void checkDuplicate(List<Integer> numbers) {
+		Set<Integer> newNumbers = new HashSet<>(numbers);
+		if (newNumbers.size() != numbers.size()) {
+			throw new IllegalArgumentException("[ERROR] 중복되지 않는 서로 다른 숫자 6개를 입력해주세요.");
+		}
+	}
+
+	// 오름차순으로 정렬하는 메소드
+	public List<Integer> sortInAsd() {
+		Collections.sort(numbers);
+		return numbers;
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
 }

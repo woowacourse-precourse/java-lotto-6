2bd91500225d3dbad0904f912516eb29a7e0e5e4
insu <is23193160@gmail.com>
2023-11-05 17:37:36 +0900

    feat: 입력 받은 문자열 리스트를 Integer리스트로 반환하는 기능 추가

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -1,13 +1,22 @@
 package lotto.util;
 
+import java.util.List;
+
 public class Converter {
     private static final String NON_NUMERIC_INPUT_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
-    public static int stringToInt(String input){
+
+    public static int stringToInt(String inputNumber) {
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(inputNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NON_NUMERIC_INPUT_MESSAGE);
         }
     }
-    
+
+    public static List<Integer> stringListToIntegers(List<String> inputNumbers) {
+        return inputNumbers.stream()
+                .map(Converter::stringToInt)
+                .toList();
+    }
+
 }

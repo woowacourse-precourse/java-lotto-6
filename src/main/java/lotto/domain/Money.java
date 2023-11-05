3eeea4f7c0e801f9b7c796d7f10273312816ff5d
sangdol <priceelees@gmail.com>
2023-11-05 14:14:31 +0900

    feat(Money): 돈과 관련된 정보를 저장하는 기능

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.text.DecimalFormat;
+import lotto.constant.Constant;
+
+public record Money(long amount) {
+    public static final Money ZERO = new Money(0);
+    private static final String WON = "원";
+    private static final String DECIMAL_FORMAT = "###,###";
+
+    public Money add(long money) {
+        return new Money(amount + money);
+    }
+
+    public Money sum(Money money) {
+        return new Money(amount + money.amount());
+    }
+
+    public Money multiplyByCount(long count) {
+        return new Money(amount * count);
+    }
+
+    public String parseToWonWithComma() {
+        DecimalFormat formatter = new DecimalFormat(DECIMAL_FORMAT);
+        return formatter.format(amount) + WON;
+    }
+
+    public boolean isLessThan(Money other) {
+        return amount < other.amount();
+    }
+
+    public boolean cantDivided(Money other) {
+        return amount % other.amount() != Constant.ZERO;
+    }
+}

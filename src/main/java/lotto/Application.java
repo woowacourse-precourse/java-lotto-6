daea7aa6a187f0a549da14bb717fe5a9108fe51b
haiseong <wjdgotjd9908@gmail.com>
2023-11-06 04:48:55 +0900

    feat(Application): LottoController와 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController controller = new LottoController(new OutputView());
+        controller.start();
     }
 }

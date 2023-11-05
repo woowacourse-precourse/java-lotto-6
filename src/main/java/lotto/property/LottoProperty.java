fb57ca605ff8eed2c95047ea7cdc5b8fea0146db
seonghoo1217 <seonghoo1217@naver.com>
2023-11-05 14:37:20 +0900

    style: 불필요한 공백 제거

--- src/main/java/lotto/property/LottoProperty.java
+++ src/main/java/lotto/property/LottoProperty.java
@@ -4,6 +4,5 @@ public class LottoProperty {
     public static final Integer LOTTO_START = 1;
     public static final Integer LOTTO_END = 45;
     public static final String DELIMITER = ",";
-
     public static final Integer WINNING_SIZE = 6;
 }

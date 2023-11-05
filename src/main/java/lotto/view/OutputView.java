e45feac68458b3ff6e0c35b8221fab9b54dacfab
jiwoo <zbqmgldjfh@gmail.com>
2023-11-05 11:46:51 +0900

    feat(OutputView): OutputView 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public interface OutputView {
+
+    void writeLottoPurchaseMoney();
+
+    void writeGeneratedLotto(List<Lotto> lottos);
+
+    void writeLottoWinningNumbers();
+
+    void writeLottoBonusNumber();
+
+    void writeResult();
+
+    void writeBlank();
+}
---
+++ src/main/java/lotto/view/OutputViewImpl.java
@@ -0,0 +1,41 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputViewImpl implements OutputView {
+
+    @Override
+    public void writeLottoPurchaseMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    @Override
+    public void writeGeneratedLotto(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+        lottos.forEach(System.out::println);
+    }
+
+    @Override
+    public void writeLottoWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    @Override
+    public void writeLottoBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    @Override
+    public void writeResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        // TODO : 결과 출력
+    }
+
+    @Override
+    public void writeBlank() {
+        System.out.println();
+    }
+}

4b4442ccd3b94270673575c66c930fd5cc5ef53d
isyoo1213 <moethns@gmail.com>
2023-11-05 17:20:20 +0900

    feat: Lottos 모든 로또 저장 및 조회 기능 추가 Lottos - addLotto() 추가 - getLottos() 추가 README.md - 기능 명세 구현 체크

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public void addLotto(Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+    public List<Lotto> getLottos() {
+        return List.copyOf(lottos);
+    }
+}

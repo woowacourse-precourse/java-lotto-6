2c9fba0edaf8cea0cd61dad239e483c82d3ea5fe
nahyeon99 <nahyeonee99@gmail.com>
2023-11-05 19:34:03 +0900

    docs(docs/README): 기능 구현 사항 업데이트
    1. 구현한 목록 체크
    2. 추가 고려 사항 추가 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
         LottoController.run();
+        Console.close();
     }
 }

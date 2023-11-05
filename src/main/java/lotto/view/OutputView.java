1f038c1b442f6ac9c2d35bbea469a7b26bd4ee73
oyoungsun <oyoungsun1030@gmail.com>
2023-11-05 16:20:03 +0900

    feat(OutputView) : 로또 자동발행 목록을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,36 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+
 public class OutputView {
-    
+    private final static String AUTO_BUY_MESSAGE = "%d개를 구매했습니다.";
+    private final static String LOTTO_PREFIX = "[";
+    private final static String LOTTO_SUFFIX = "]";
+
+
     public static void printException(String errorMessage) {
         System.out.println(errorMessage);
+    }
+
+    public static void printAutoLottos(final List<Lotto> autoLottos, int lottoCount) {
+        System.out.printf(String.format(AUTO_BUY_MESSAGE), lottoCount);
+        printEmpty();
+
+        StringBuilder result;
+        for (Lotto lotto : autoLottos) {
+            result = new StringBuilder();
+            result.append(LOTTO_PREFIX);
+            lotto.getLotto().stream().map(String::valueOf).collect(Collectors.joining(","));
+            result.append(LOTTO_SUFFIX);
+            System.out.print(result);
+            printEmpty();
+        }
+    }
+
+    private static void printEmpty() {
+        System.out.println();
     }
 
 }

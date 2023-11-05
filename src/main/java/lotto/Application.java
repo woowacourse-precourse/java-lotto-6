8e6f89e62213129c1dfc7352b8f942311b1ac8ea
dj <fjzl12@naver.com>
2023-11-05 18:52:15 +0900

    feat(Application): 로또 게임 시작 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.startGame();
     }
 }

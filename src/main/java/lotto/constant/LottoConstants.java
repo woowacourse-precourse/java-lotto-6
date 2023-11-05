0c415651ec9d1d33ac10f9db8c2c18cae6520ace
minseo  jo <liging12@naver.com>
2023-11-05 23:54:17 +0900

    fix: 개인 최대 구매 금액 변경
    100만원 에서 10만원으로 변경

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -5,7 +5,7 @@ public enum LottoConstants {
     LOTTO_MIN_NUMBER(1),    // 로또 번호 최소값
     LOTTO_MAX_NUMBER(45),   // 로또 번호 최대값
     LOTTO_PRICE(1000),      // 로또 가격
-    MAX_LOTTO_PRICE_PER_USER(1_000_000), // 개인이 살 수 있는 로또 최대 금액
+    MAX_LOTTO_PRICE_PER_USER(100_000), // 개인이 살 수 있는 로또 최대 금액
     FIRST_RANKING(0),       // 로또 당첨 - 1등
     LAST_RANKING(6);        // 로또 당첨 - 꼴등 (6등, 꽝)
 

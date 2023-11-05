cf0ddeb2278c8f6085d996f7fa1764d8a8d64f70
GiJung <weeds2577@gmail.com>
2023-11-05 15:00:02 +0900

    refactor(Lotto): 숫자 하드 코딩을 상수로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -22,7 +22,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != lottoNumberCount) {
             throw new IllegalArgumentException();
         }
     }

cf469c3268dd28b32be6f841e0c082bdc2d7df36
ldw <leehan02@hanyang.ac.kr>
2023-11-06 01:43:03 +0900

    fix(lotto): 사용자로부터 입력 받은 구입 금액이 유효할 때까지 반복적으로 입력 받도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,18 +1,32 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.ErrorMessages;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class InputView {
     private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String ENTER_SIX_DIGITS_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public String budget() {
-        System.out.println(ENTER_PURCHASE_AMOUNT);
-        String budget = Console.readLine();
-        validateBlank(budget);
+//    public String budget() {
+//        System.out.println(ENTER_PURCHASE_AMOUNT);
+//        String budget = Console.readLine();
+//        validateBlank(budget);
+//
+//        return budget;
+//    }
 
-        return budget;
+    public String budget(Predicate<String> isValid) {
+        return Stream.generate(() -> {
+                    System.out.println(ENTER_PURCHASE_AMOUNT);
+                    return Console.readLine();
+                })
+                .filter(isValid)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessages.NO_CONTENT.value()));
     }
 
     public String mainNumbers() {
@@ -31,9 +45,13 @@ public class InputView {
         return bonusNumber;
     }
 
-    public void validateBlank(String purchaseAmount) throws IllegalArgumentException  {
+    public void validateBlank(String purchaseAmount) throws IllegalArgumentException {
         if (purchaseAmount.isBlank()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static boolean isValid(String input) {
+        return input.isBlank();
     }
 }

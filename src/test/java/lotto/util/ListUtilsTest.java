0036fcfc80ee48ab2ab9a248403446ffcba92d5a
Haeun Jeong <gkdms4248@gmail.com>
2023-11-05 23:38:19 +0900

    test: string 을 integer list 로 변환 기능 테스트 추가

---
+++ src/test/java/lotto/util/ListUtilsTest.java
@@ -0,0 +1,26 @@
+package lotto.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ListUtilsTest {
+
+    @DisplayName("string 을 List<Integer> 로 변환하는 기능")
+    @Test
+    public void stringToIntegerList() {
+        // given
+        String inputLotto = "1,2,3,4,5,6";
+        ListUtils listUtils = new ListUtils();
+
+        // when
+        List<Integer> lotto = listUtils.stringToIntegerList(inputLotto);
+
+        // then
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertEquals(expected, lotto);
+    }
+}

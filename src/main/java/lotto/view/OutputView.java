18f1d0eade4da02829f8cfe3c5d2191f5688938a
sehee <tpgml701@naver.com>
2023-11-06 01:21:56 +0900

    feat: 구매금액 안내 메시지 출력 테스트 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,9 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+public class OutputView {
+
+    public String inputMoney() {
+        System.out.println("구매금액을 입력해주세요.");
+        return "구매금액을 입력해주세요.";
+    }
 }

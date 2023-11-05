5817964a16fc984cef142a98ad61ca50b5438ceb
minjun7410 <minjun7410@gmail.com>
2023-11-05 17:48:40 +0900

    test(BonusNumberTest): 보너스 번호가 포함 되어 있는지 계산하는 기능 테스트 추가
    * 보너스 번호 포함 계산 테스트 추가
    * 보너스 번호 미포함 계산 테스트 추가

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -18,4 +18,18 @@ public class BonusNumberTest {
     void 보너스번호_범위밖인값_예외처리_테스트(int input) {
         assertThatThrownBy(() -> new BonusNumber(input)).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 보너스번호_포함하는지_테스트() {
+        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+        BonusNumber bonusNumber = new BonusNumber(3);
+        assertThat(winningLotto.contains(bonusNumber)).isTrue();
+    }
+
+    @Test
+    void 보너스번호_포함하지않는지_테스트() {
+        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        assertThat(winningLotto.contains(bonusNumber)).isFalse();
+    }
 }

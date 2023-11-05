fe3a44af2dd3462b60afcdc4465800bddebae816
8x15yz <8x15yz@gmail.com>
2023-11-06 01:39:46 +0900

    feat : 로또 입력받고 검증 구현완료

--- src/main/java/lotto/input/Number.java
+++
@@ -1,4 +0,0 @@
-package lotto.input;
-
-public class Number {
-}
--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -73,6 +74,9 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
+
+
 
     @Override
     public void runMain() {

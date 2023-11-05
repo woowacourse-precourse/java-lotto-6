98da07926386f8d7c918ca6bb604d8165a07212c
강승민 <rkdtmdals999@naver.com>
2023-11-05 19:52:29 +0900

    [feat] 로또 번호 생성 메소드 구현

--- src/main/java/lotto/controller/LottoManager.java
+++ src/main/java/lotto/controller/LottoManager.java
@@ -1,5 +1,13 @@
 package lotto.controller;
 
+import static lotto.utility.Constants.LOTTO_LENGTH;
+import static lotto.utility.Constants.MAX_LOTTO_NUMBER;
+import static lotto.utility.Constants.MIN_LOTTO_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
 import lotto.view.UserView;
 
 public class LottoManager {
@@ -9,7 +17,15 @@ public class LottoManager {
         user = new User();
         userView = new UserView();
     }
+
     public void start() {
         userView.printBuyMessageBefore();
+    }
+
+    private Lotto generateLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_LENGTH);
+        Lotto newLotto = new Lotto(numbers);
+        return newLotto;
     }
 }

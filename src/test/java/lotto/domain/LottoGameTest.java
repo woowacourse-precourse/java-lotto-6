b6cf296f1f0c25127bb3849690b8d81c02e1689b
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-05 13:41:36 +0900

    test(LottoGameTest) : 당첨 통계 정보 반환

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoGameTest {
 
-    @DisplayName("구매 수량만큼 로또 번호 세트를 생성합니다")
+    @DisplayName("구매 수량만큼 로또 번호 세트를 생성한다.")
     @Test
     void createLottoGame() {
         // given
@@ -23,5 +23,20 @@ class LottoGameTest {
         // then
         assertThat(lottos.size()).isEqualTo(10);
         assertThat(lottoGame.getBuyingAmount()).isEqualTo(10);
+    }
+
+    @DisplayName("당첨 번호 정보로부터 당첨 통계 정보를 반환한다.")
+    @Test
+    void calculateWinning() {
+        // given
+        WinningLotto winningLotto = WinningLotto.of(List.of(1, 2, 3, 4, 5, 6));
+        int buyingPrice = 10000;
+        LottoGame lottoGame = LottoGame.createLottoGame(buyingPrice);
+
+        // when
+        WinningResult winningResult = lottoGame.calculateWinning(winningLotto);
+
+        // then
+        assertThat(winningResult).isNotNull();
     }
 }

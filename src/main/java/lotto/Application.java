576a7f18091b615e05e00d85a25e63e637597adc
jeli01 <jeli01@naver.com>
2023-11-06 03:09:15 +0900

    feat: add main method for lotto program

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Integer payment = UI.inputMoney();
+        List<Lotto> lottos = Store.sellLottos(payment);
+        UI.printPurchases(lottos);
 
+        List<Integer> winningNumbers = UI.inputWinningNumbers();
+        Integer bonusNumber = UI.inputBonusNumber();
+        Lotto lotto = new Lotto(winningNumbers);
+        LottoDrawResult drawResult = BroadCastingStation.drawLotto(lotto, bonusNumber);
+        UI.printWinningStats(lottos, drawResult);
     }
 }

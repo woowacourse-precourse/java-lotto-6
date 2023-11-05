087e9fd9d88b403e88912aea7f617eb379448b9a
jmj005 <jmj00005@naver.com>
2023-11-06 00:50:51 +0900

    feat: 보너스 번호 당첨 번호 중복 검증 구현

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -16,6 +16,7 @@ public class WinningLotto {
         validateWinningLottoUnique(winningLotto);
         validateWinningLottoLength(winningLotto);
         validateBonusNumberRange(bonusNumber);
+        validateBonusNumberUnique(bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
@@ -44,6 +45,12 @@ public class WinningLotto {
     private void validateBonusNumberRange(int bonusNumber) {
         if (!isValidRange(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 1~45 사이만 입력 가능합니다.");
+        }
+    }
+
+    private void validateBonusNumberUnique(int bonusNumber) {
+        if (winningLotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException("당첨 번호화 중복된 번호는 입력할 수 없습니다.");
         }
     }
 

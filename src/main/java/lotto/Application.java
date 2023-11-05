fc654b3a74ad55720641dfb312343a438f6c8a8a
정영록 <s9285s@naver.com>
2023-11-05 21:32:02 +0900

    feat : 서비스 로직 및 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.LottoGameController.LottoGameController;
+import lotto.util.Utils;
+import lotto.util.Input;
+import lotto.util.Output;
+
 public class Application {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IllegalArgumentException {
+
+
         // TODO: 프로그램 구현
+
+        LottoGameController lottoGameController = new LottoGameController(new Input(), new Output(), new Utils());
+        lottoGameController.start();
     }
 }

d4df5d08a67a49dc61308fa14379e09a6438e1d6
jwAhn1020 <takartin@gmail.com>
2023-11-05 23:59:01 +0900

    feat : LottoStart 생성 후 게임 진행 순서 멘트 메서드 생성

---
+++ src/main/java/lotto/LottoStart.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoStart {
+
+    private static final String HOW_MUCH_BUY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String MAKE_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String MAKE_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static void run(){
+
+        firstGameProgress();
+        secondGameProgress();
+        thirdGameProgress();
+
+    }
+
+
+    public static void firstGameProgress(){
+        System.out.print(LottoStart.HOW_MUCH_BUY_MESSAGE);
+    }
+
+    private static void secondGameProgress() {
+        System.out.print(LottoStart.MAKE_WINNING_NUMBER);
+    }
+
+    private static void thirdGameProgress() {
+        System.out.print(LottoStart.MAKE_BONUS_NUMBER);
+    }
+
+
+}

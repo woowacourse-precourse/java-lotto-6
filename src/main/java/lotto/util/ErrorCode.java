7239e1e735576149190961c9c4dcf0fa91f1a4e0
metishonora <metishonora@icloud.com>
2023-11-05 22:00:46 +0900

    fix: 에러 메시지 변경

--- src/main/java/lotto/util/ErrorCode.java
+++ src/main/java/lotto/util/ErrorCode.java
@@ -4,14 +4,14 @@ public enum ErrorCode {
     PURCHASE_NOT_DIVISIBLE("구입 단위는 1000원이어야 합니다."),
     PURCHASE_NOT_POSITIVE("구입 금액은 양수여야 합니다."),
     PURCHASE_NOT_INTEGER("구입 금액은 숫자로 주어져야 합니다."),
-    LOTTO_FORMAT("로또 번호가 형식에 맞지 않습니다."),
+    LOTTO_FORMAT("쉼표(,)로 구분한 6개의 숫자를 입력해야 합니다."),
     LOTTO_ILLEGAL_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     LOTTO_DUPLICATED("중복된 번호가 존재합니다."),
-    LOTTO_ORDER("오름차순으로 입력해야 합니다."),
     BONUS_ILLEGAL_NUMBER("보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
     BONUS_DUPLICATED("보너스 번호가 중복되었습니다.");
 
     private final String message;
+
     ErrorCode(String message) {
         this.message = message;
     }

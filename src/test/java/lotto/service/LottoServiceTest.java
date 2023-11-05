aa61b8bdc33e6af231b1da39574a953d146aabaf
isyoo1213 <moethns@gmail.com>
2023-11-05 19:11:43 +0900

    test: LottoServiceTest 유저/당첨 로또 생성 테스트 LottoServiceTest - createUserLottosTest()   - 유저 구매 로또 생성 테스트 - 정상 데이터 - 성공 - createAnswerLottosTest()   - 당첨 로또 생성 테스트 - 정상 데이터 - 성공

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import lotto.model.Lotto;
+import lotto.model.Lottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoServiceTest {
     LottoService lottoService = new LottoService();
+
+    @DisplayName("유저 구매 로또 생성 테스트 - 정상 데이터 - 성공")
+    @Test
+    void createUserLottosTest() {
+        int testAmount = 3;
+        Lottos testLottos =  lottoService.createUserLottos(testAmount);
+        assertThat(testLottos.getLottos().size()).isEqualTo(3);
+    }
+
+    @DisplayName("당첨 로또 생성 테스트 - 정상 데이터 - 성공")
+    @Test
+    void createAnswerLottosTest() {
+        List<Integer> testLottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto testLotto =  lottoService.createAnswerLotto(testLottoNumbers);
+        assertThat(testLotto.getNumbers()).isEqualTo(testLottoNumbers);
+    }
 
     @DisplayName("Integer 리스트 오름차순 정렬 테스트 - 성공")
     @Test

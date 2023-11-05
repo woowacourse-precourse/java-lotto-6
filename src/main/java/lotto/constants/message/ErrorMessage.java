5c5bda8568912a056f3ac8c92ba55f3b07146a43
hyerimmy <heyrim2010@naver.com>
2023-11-05 18:43:10 +0900

    feat: 에러 메시지 정적 변수 정의 - prefix로 에러 문구 머릿말 설정

---
+++ src/main/java/lotto/constants/message/ErrorMessage.java
@@ -0,0 +1,10 @@
+package lotto.constants.message;
+
+public class ErrorMessage {
+    private static final String prefix = "[Error] ";
+    public static final String DUPLICATE_NUMBER = prefix + "중복되지 않는 번호를 입력해주세요.";
+    public static final String INVALID_PURCHASE_AMOUNT = prefix + "%d원 단위의 금액을 입력해주세요.";
+    public static final String INVALID_WINNING_NUMBER = prefix + "로또 번호는 %d부터 %d 사이의 숫자여야 합니다.";
+    public static final String INVALID_BONUS_NUMBER = prefix + "보너스 번호는 %d부터 %d 사이의 숫자여야 합니다.";
+
+}

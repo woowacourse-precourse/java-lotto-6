baef9c19fb66784f8d7aafcca20c0aef8e26d6be
yeahdy <yejine98@naver.com>
2023-11-05 22:02:21 +0900

    Feat: 로또 게임, 에러코드 정의

---
+++ src/main/java/constants/GameConstant.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class GameConstant {
+
+    public static final int DEFAULT_LOTTO_NUMBER_LENGTH = 6;
+    public static final int MIN_AMOUNT_COUNT = 0;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+
+}

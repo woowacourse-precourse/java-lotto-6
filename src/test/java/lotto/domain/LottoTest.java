599b3c4e05e1e5ee41d029151d952d1524577af5
gundorit <packdev937@gmail.com>
2023-11-05 13:00:50 +0900

    test (Lotto) : 로또 번호와 당첨 번호를 비교하여 당첨 여부를 테스트 한다

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
+import lotto.domain.enums.LottoPrize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static lotto.domain.Numbers.createNumbersList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Lotto 클래스")
@@ -30,5 +32,15 @@ class LottoTest {
     void createLottoByNull() {
         assertThatThrownBy(() -> new Lotto(null))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 당첨 번호를 비교하여 당첨 여부를 판단한다.")
+    @Test
+    void compareLottoNumbersToWinningNumbers() {
+        Lotto lotto = new Lotto(Numbers.createNumbersList(List.of(1, 2, 3, 4, 5, 6)));
+        WinningLotto winningLotto = new WinningLotto(
+            Numbers.createNumbersList(List.of(1, 2, 3, 4, 5, 6)), new Number(7));
+
+        assertThat(lotto.matches(winningLotto)).isEqualTo(LottoPrize.FIRST);
     }
 }

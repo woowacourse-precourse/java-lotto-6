a27b848c09cbbd62591e2ac1658f322eca04ad98
mmalfoy <stpmalfoy@gmail.com>
2023-11-06 03:44:36 +0900

    feat(LottoModel) : 당첨된 로또 테이블 생성구현

--- src/main/java/lotto/domain/LottoModel.java
+++ src/main/java/lotto/domain/LottoModel.java
@@ -2,27 +2,36 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
+import lotto.data.Rewards;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoModel {
 
     private final InputUI inputUI;
+    private final OutputUI outputUI;
     private List<Lotto> publishedLottos;
+    private int totalEarnedMoney;
+    private HashMap<Rewards, Integer> winningTable;
+
 
     public LottoModel() {
         this.inputUI = new InputUI();
+        this.outputUI = new OutputUI();
         this.publishedLottos = new ArrayList<>();
+        this.winningTable = new HashMap<>();
     }
 
-    public void publishLotto(int cost) {
+    public void publishLotto(int numOfLotto) {
         List<Integer> randNums;
         try {
-            for (int i = 0; i < cost; i++) {
+            for (int i = 0; i < numOfLotto; i++) {
                 randNums = generateNewLotto();
+                randNums.sort(Comparator.naturalOrder());
                 publishedLottos.add(new Lotto(randNums));
             }
         } catch (IllegalArgumentException e) {
@@ -40,7 +49,7 @@ public class LottoModel {
         List<String> sortedNums = lottoNums.stream()
                 .map(String::valueOf)
                 .collect(Collectors.toList());
-        String Ticket = String.format("[%s]", String.join(",", sortedNums));
+        String Ticket = String.format("[%s]", String.join(", ", sortedNums));
         return Ticket;
     }
 
@@ -48,6 +57,17 @@ public class LottoModel {
     public String computeRate(double winningRate) {
         return String.format("%.1f", winningRate);
     }
+
+
+    public HashMap<Rewards, Integer> initWinningTable() {
+        HashMap<Rewards, Integer> winningTable = new HashMap<>();
+        Rewards[] rewards = Rewards.values();
+        for (Rewards reward : rewards) {
+            winningTable.put(reward, 0);
+        }
+        return winningTable;
+    }
+
 
 
 }

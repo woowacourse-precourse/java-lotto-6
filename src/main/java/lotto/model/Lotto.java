63a2ddeaa81599f28eeef666f38caf4bc17102de
HoeSeong123 <ghltjd3@gmail.com>
2023-11-05 21:26:18 +0900

    feat(Lotto) : 당첨번호 입력에 대한 예외 처리(중복된 숫자)

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -12,8 +12,24 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateHasSixSize(numbers);
+        validateIsDuplicate(numbers);
+    }
+
+    private void validateHasSixSize(List<Integer> input) {
+        if (input.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateIsDuplicate(List<Integer> input) {
+        long inputSize = input.size();
+        long uniqueInputSize = input.stream()
+                .distinct()
+                .count();
+
+        if (inputSize != uniqueInputSize) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않게 입력해야 합니다.");
         }
     }
 

4b31927ce49e74472f1d170d758773fc992e7bb1
qncj5224 <qncj5224@naver.com>
2023-11-05 15:28:54 +0900

    feat : String[] 리스트 변환 메서드 구현

--- src/main/java/lotto/controller/InputProcessor.java
+++ src/main/java/lotto/controller/InputProcessor.java
@@ -1,17 +1,24 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputProcessor {
     private static final int LOTTO_PRICE = 1000;
+  public String getInput;
 
-    public int calculateLottoCount(int purchaseAmount) {
-      return purchaseAmount / LOTTO_PRICE;
+  public int calculateLottoCount(int money) {
+      return money / LOTTO_PRICE;
     }
 
-    private String[] lottoNumberSplit(String lottoWinningNumber) {
+    public static String[] lottoNumberSplit(String lottoWinningNumber) {
       return lottoWinningNumber.split(" ,");
+    }
+
+    public static List<String> winningNumberSplit(String[] lottoNumberSplit) {
+      List<String> winningNumber = Arrays.asList(lottoNumberSplit);
+      return winningNumber;
     }
 
 }

28942955b0cc719c92aa5d4fab57b2a65c143a20
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-06 08:35:32 +0900

    feat: 로또 발행기능 출력 테스트

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import static lotto.Lotto.*;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
+
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_TEST_PURCHASE = 10000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int numberOfPurchasedLottos = LOTTO_TEST_PURCHASE/LOTTO_PRICE;
+
+        for (int i = 0; i < numberOfPurchasedLottos; i++){
+            List<Integer> numbers = Lotto.getNumbers();
+            System.out.println(numbers);
+        }
     }
 }

478d9c56b7522e4628a37a622618c3dd1222e793
jihun <pjh2996@naver.com>
2023-11-05 22:11:43 +0900

    feat : 금액에 맞게 구입할 로또의 갯수를 계산해서 저장하는 기능 구현

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -6,10 +6,12 @@ public class Purchase {
 
     private final static int MONEY_UNIT = 1000;
     private final int money;
+    private final int count;
 
     public Purchase(int money) {
         validate(money);
         this.money = money;
+        this.count = money / 10;
     }
 
     private void validate(int money) {

53dcd4c4498a19f7b278b4a9690d83cfe4a733e1
노정완 <jeongwan.noh@gmail.com>
2023-11-05 16:33:34 +0900

    refactor: enum 기반 로또 번호 생성 메서드 분리

--- src/main/java/lotto/domain/lottery/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/lottery/LottoNumberGenerator.java
@@ -5,10 +5,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.domain.constants.LottoConstraint.*;
+
 public class LottoNumberGenerator {
-    private static List<Integer> generateLottoNumber() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+    private static List<Integer> generateSortedLottoNumber() {
+        List<Integer> numbers = generateSingleLottoNumber();
         Collections.sort(numbers);
         return numbers;
+    }
+
+    private static List<Integer> generateSingleLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(
+                NUMBER_RANGE_MINIMUM.getValue(),
+                NUMBER_RANGE_MAXIMUM.getValue(),
+                6
+        );
     }
 }

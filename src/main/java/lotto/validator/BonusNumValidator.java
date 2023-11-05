2207c7f0625235bd260b175eb2de8fc7be0076eb
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-06 03:36:08 +0900

    feat: BonusNum 예외 처리 클래스 추가
    미입력, 숫자 이외 입력, 범위를 벗어난 입력, 당첨 숫자와
    중복된 숫자 입력을 예외 처리 해준다.

---
+++ src/main/java/lotto/validator/BonusNumValidator.java
@@ -0,0 +1,45 @@
+package lotto.validator;
+
+import java.util.List;
+
+public class BonusNumValidator {
+
+    private static final String ERROR = "[ERROR]";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
+    private static final String RANGE_OVER_MESSAGE = "1~45사이의 번호를 입력해주세요.";
+    private static final String NULL_ERROR_MESSAGE = "보너스 번호를 입력해주세요.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "당첨 숫자들과 다른 숫자를 입력해주세요.";
+
+    public BonusNumValidator(String number, List<Integer> winningNum){
+        isNull(number);
+        isNumError(number);
+        isRangeOver(number);
+        isDuplicate(number, winningNum);
+    }
+
+    public void isNull(String number){
+        if (number.equals("")){
+            throw new IllegalArgumentException(ERROR + NULL_ERROR_MESSAGE);
+        }
+    }
+
+    public void isNumError(String number){
+        try{
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR + NOT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public void isRangeOver(String number){
+        if (Integer.parseInt(number) > 45 || Integer.parseInt(number) < 1){
+            throw new IllegalArgumentException(ERROR + RANGE_OVER_MESSAGE);
+        }
+    }
+
+    public void isDuplicate(String number, List<Integer> winningNum){
+        if (winningNum.contains(Integer.parseInt(number))){
+            throw new IllegalArgumentException(ERROR + DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+}

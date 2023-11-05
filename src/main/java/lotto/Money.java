915afd51e9afd54fa0a34140557e610520a7d701
Junobee25 <wnsdhqo@naver.com>
2023-11-06 02:56:21 +0900

    feat(Money): Money 클래스 생성

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public class Money {
+
+    private final Long money;
+
+    public Money(Long money) {
+        this.money = money;
+    }
+
+    public Long getMoney() {
+        return this.money;
+    }
+}

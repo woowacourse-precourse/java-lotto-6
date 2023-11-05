4366c0f1e79123f171aadbd09ea2f9d85a3914b1
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-05 17:42:32 +0900

    feat(LottoNumberValidator): 중복된 숫자 입력 시 예외 처리 기능 추가
    docs: LottoNumberValidator 기능 목록 체크

--- src/main/java/lotto/validation/LottoNumberValidator.java
+++ src/main/java/lotto/validation/LottoNumberValidator.java
@@ -1,5 +1,7 @@
 package lotto.validation;
 
+import java.util.HashSet;
+import java.util.List;
 import lotto.constant.ExceptionMessage;
 import lotto.constant.GameConfig;
 
@@ -11,6 +13,15 @@ public class LottoNumberValidator {
 
         if (number < minNumber.getValue() || number > maxNumber.getValue()) {
             ExceptionMessage message = ExceptionMessage.OUT_OF_RANGE_NUMBER_EXCEPTION;
+            throw new IllegalArgumentException(message.getMessage());
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> deduplicatedNumbers = new HashSet<>(numbers);
+
+        if (deduplicatedNumbers.size() != numbers.size()) {
+            ExceptionMessage message = ExceptionMessage.DUPLICATE_NUMBER_EXCEPTION;
             throw new IllegalArgumentException(message.getMessage());
         }
     }

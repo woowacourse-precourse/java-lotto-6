cfbf2e6b265456629fdcc3dc1ac7cda792530ed5
jdohun <wjdehgns97@naver.com>
2023-11-05 17:22:30 +0900

    Refactor: 당첨 번호와 보너스 번호를 분리
    WinningNumber.java
    BonusNumber.java

---
+++ src/main/java/lotto/domain/model/BonusNumber.java
@@ -0,0 +1,14 @@
+package lotto.domain.model;
+
+public class BonusNumber {
+
+    int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    protected int getBonusNumber() {
+        return this.bonusNumber;
+    }
+}
--- src/main/java/lotto/domain/model/WinningNumber.java
+++ src/main/java/lotto/domain/model/WinningNumber.java
@@ -1,14 +1,26 @@
 package lotto.domain.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumber {
-    private  List<Integer> regularWinningNumbers;
-    private int bonusNumber;
+    private final List<Integer> regularWinningNumbers;
 
-    public WinningNumber(List<Integer> regularWinningNumbers, int bonusNumber) {
+    public WinningNumber(List<Integer> regularWinningNumbers) {
         this.regularWinningNumbers = regularWinningNumbers;
-        this.bonusNumber = bonusNumber;
+    }
+
+    public void validateAlreadyHaveNumber(int bonusNumber) {
+        Set<Integer> duplicateChecker = new HashSet<>(regularWinningNumbers);
+
+        if (!duplicateChecker.add(bonusNumber)) {
+            throw new IllegalArgumentException("당첨 번호에 포함되지 않은 숫자를 입력해야 합니다.");
+        }
+    }
+
+    protected List<Integer> getRegularWinningNumbers() {
+        return regularWinningNumbers;
     }
 
 }

86aa8add6e1ec2c9d3961187a27b84bbd6e17cae
toychip <dlawnsgud427@naver.com>
2023-11-05 22:57:14 +0900

    refactor: 중복 method 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,26 +3,38 @@ package lotto.view;
 public class OutputView {
 
     public static void askAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        print("구입금액을 입력해 주세요.");
     }
 
     public static void showTickets(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+        print(count + "개를 구매했습니다.");
     }
 
     public static void askWinningNum() {
-        System.out.println("당첨 번호를 입력해주세요.");
+        print("당첨 번호를 입력해주세요.");
     }
 
     public static void askBonusNum() {
-        System.out.println("보너스 번호를 입력해주세요.");
+        print("보너스 번호를 입력해주세요.");
     }
 
     public static void showResults() {
-        System.out.println("당첨 통계\n---");
+        print("당첨 통계\n---");
     }
 
     public static void showYield(double yield) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", yield);
+        print("총 수익률은 %.1f%%입니다.", yield);
+    }
+
+    private static void print(String str) {
+        System.out.println(str);
+    }
+
+    private static void print(String str, double yield) {
+        System.out.printf(str, yield);
+    }
+
+    private static void print(int count, String str) {
+        System.out.println(count + str);
     }
 }

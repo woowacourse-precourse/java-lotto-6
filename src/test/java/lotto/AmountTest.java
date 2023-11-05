562e039b944c0527f5449ee35035ba1e6607272e
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-11-05 15:46:31 +0900

    test: 구입 금액 검증, 로또 발행 기능

---
+++ src/test/java/lotto/AmountTest.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lotto.model.Amount;
+import lotto.utils.Constants;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AmountTest {
+
+    @DisplayName("구입 금액이 1,000원 단위가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4500, 12230, 6666})
+    void 구입_금액_입력값_단위_검증(int amount) {
+        assertThatThrownBy(() -> Amount.from(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 금액이 0원이면 예외가 발생한다.")
+    @Test
+    void 구입_금액_입력값_0원이_아닌지_검증() {
+        assertThatThrownBy(() -> Amount.from(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 금액만큼 로또를 발행한다.")
+    @Test
+    void 구입_금액만큼_로또_발행() {
+        // given
+        int amount = 2000;
+        Amount userAmount = Amount.from(amount);
+
+        // when
+        int lottoCount = userAmount.getLottoCount();
+
+        // then
+        assertEquals(amount/Constants.LOTTO_PRICE, lottoCount);
+    }
+}
--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -7,27 +7,9 @@ import lotto.model.Lotto;
 import lotto.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.List;
 
 public class UserTest {
-
-    @DisplayName("구입 금액이 1,000원 단위가 아니면 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(ints = {4500, 12230, 6666})
-    void 구입_금액_입력값_단위_검증(int amount) {
-        assertThatThrownBy(() -> User.from(amount))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("구입 금액이 0원이면 예외가 발생한다.")
-    @Test
-    void 구입_금액_입력값_0원이_아닌지_검증() {
-        assertThatThrownBy(() -> User.from(0))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @DisplayName("구입 금액만큼 로또를 발행한다.")
     @Test

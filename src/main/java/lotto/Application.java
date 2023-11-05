e52d50c5ae9e2e0016649cf3aa1a3befdaf25f71
HeeHeeg <qmffndoden@naver.com>
2023-11-05 16:13:34 +0900

    refactor(lotto): 출력하는 메서드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,32 +1,30 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
+        PrintOut printOut = new PrintOut();
+
+        printOut.purchaseGuide();
         Integer userPurchaseAmount = Integer.parseInt(Console.readLine());
 
-        System.out.println();
         int numberOfLottos = userPurchaseAmount / 1000;
-        System.out.println(numberOfLottos + "개를 구매했습니다.");
+        printOut.confirmPurchase(numberOfLottos);
 
         LottoGenerator lottoGenerator = new LottoGenerator();
         List<Lotto> purchasedLottos = lottoGenerator.generateLottos(numberOfLottos);
 
-        System.out.println();
-        System.out.println("당첨 번호를 입력해 주세요.");
+        printOut.inputWinningNumber();
         List<Integer> winningNumber = Arrays.stream(Console.readLine().split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        System.out.println();
-        System.out.println("보너스 번호를 입력해 주세요.");
+        printOut.inputBonusNumber();
         int bonusNumber = Integer.parseInt(Console.readLine());
 
         LottoResult lottoResult = new LottoResult();
@@ -46,16 +44,7 @@ public class Application {
 
         double roundedReturn = Math.round(totalReturn * 10.0) / 10.0;
 
-        System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + lottoResult.getCount(WinningCriteria.FIFTH_PLACE) + "개");
-        System.out.println("4개 일치 (50,000원) - " + lottoResult.getCount(WinningCriteria.FOURTH_PLACE) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.getCount(WinningCriteria.THIRD_PLACE) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.getCount(WinningCriteria.SECOND_PLACE) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.getCount(WinningCriteria.FIRST_PLACE) + "개");
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", roundedReturn);
-
+        printOut.getStatistics(lottoResult, roundedReturn);
     }
 
     private static WinningCriteria getWinningCriteria(Lotto lotto, List<Integer> winningNumber, int bonusNumber) {
---
+++ src/main/java/lotto/PrintOut.java
@@ -0,0 +1,37 @@
+package lotto;
+
+public class PrintOut {
+
+    public void purchaseGuide() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void confirmPurchase(int numberOfLottos) {
+        System.out.println();
+        System.out.println(numberOfLottos + "개를 구매했습니다.");
+    }
+
+    public void inputWinningNumber() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void inputBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void getStatistics(LottoResult lottoResult, double roundedReturn) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + lottoResult.getCount(WinningCriteria.FIFTH_PLACE) + "개");
+        System.out.println("4개 일치 (50,000원) - " + lottoResult.getCount(WinningCriteria.FOURTH_PLACE) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.getCount(WinningCriteria.THIRD_PLACE) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.getCount(WinningCriteria.SECOND_PLACE) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.getCount(WinningCriteria.FIRST_PLACE) + "개");
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", roundedReturn);
+    }
+
+
+}

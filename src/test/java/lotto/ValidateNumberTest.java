9f569b891a8af12bc118c80c14fe8dc4b83ee6bc
K-KY <kyuyoungk@naver.com>
2023-11-06 02:27:20 +0900

    Test(ValidateNumberTest) : 숫자 검증 테스트

---
+++ src/test/java/lotto/ValidateNumberTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidateNumberTest {
+    @Test
+    void 숫자인지_검증() {
+        assertThatThrownBy(() -> ValidateNumber.isNumber("100o")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 검증_통과() {
+        assertThatCode(() -> ValidateNumber.isNumber("1000")).doesNotThrowAnyException();
+    }
+}

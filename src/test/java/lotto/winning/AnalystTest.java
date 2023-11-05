7bfea738537d8a1efb5b71b32c6e82ee99db4048
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 18:20:10 +0900

    test(winning): ensure correct count and sum of each winning prize category

---
+++ src/test/java/lotto/winning/AnalystTest.java
@@ -0,0 +1,35 @@
+package lotto.winning;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class AnalystTest {
+    Analyst analyst = new Analyst();
+
+    HashMap<String, List<Integer>> mockUpdatedPrizes =
+            analyst.updatePrizes(
+                    List.of(2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6),
+                    List.of(4, 3, 3, 2, 1, 1, 0, 1, 2, 0, 0, 1));
+
+    @Test
+    public void countPrizeTest() {
+        assertThat(mockUpdatedPrizes.get("first").get(PrizeIndex.COUNT.getNumber())).isEqualTo(1);
+        assertThat(mockUpdatedPrizes.get("second").get(PrizeIndex.COUNT.getNumber())).isEqualTo(1);
+        assertThat(mockUpdatedPrizes.get("third").get(PrizeIndex.COUNT.getNumber())).isEqualTo(2);
+        assertThat(mockUpdatedPrizes.get("fourth").get(PrizeIndex.COUNT.getNumber())).isEqualTo(3);
+        assertThat(mockUpdatedPrizes.get("fifth").get(PrizeIndex.COUNT.getNumber())).isEqualTo(4);
+    }
+
+    @Test
+    public void sumPrizeTest() {
+        assertThat(mockUpdatedPrizes.get("first").get(PrizeIndex.PRIZE.getNumber())).isEqualTo(2000000000);
+        assertThat(mockUpdatedPrizes.get("second").get(PrizeIndex.PRIZE.getNumber())).isEqualTo(30000000);
+        assertThat(mockUpdatedPrizes.get("third").get(PrizeIndex.PRIZE.getNumber())).isEqualTo(1500000 * 2);
+        assertThat(mockUpdatedPrizes.get("fourth").get(PrizeIndex.PRIZE.getNumber())).isEqualTo(50000 * 3);
+        assertThat(mockUpdatedPrizes.get("fifth").get(PrizeIndex.PRIZE.getNumber())).isEqualTo(5000 * 4);
+    }
+}

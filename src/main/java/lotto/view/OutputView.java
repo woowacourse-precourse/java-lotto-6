2adfceb522e530cedcf1642679ae703ba3c0b45a
WooYoungDoo <duden5000@naver.com>
2023-11-05 13:10:19 +0900

    refactor (OutputView) : 로또 개수 변수명 통일

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 public class OutputView {
 
     private final static String OUTPUT_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    private final static String OUTPUT_AMOUNT_LOTTO_MESSAGE = "%d개를 구매했습니다.";
+    private final static String OUTPUT_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
     private final static String OUTPUT_WINNER_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String BLANK_LINE = "";
@@ -13,8 +13,8 @@ public class OutputView {
         System.out.println(OUTPUT_PURCHASE_MONEY_MESSAGE);
     }
 
-    public void printAmountLottoMessage(int ammount) {
-        System.out.printf(OUTPUT_AMOUNT_LOTTO_MESSAGE, ammount);
+    public void printLottoCountMessage(int ammount) {
+        System.out.printf(OUTPUT_LOTTO_COUNT_MESSAGE, ammount);
     }
 
     public void printWinnerNumberMessage() {

a6a2c1041853dd9499ba499d35cc859f3871d14b
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-05 15:30:16 +0900

    feat(model): 로또 티켓 수량

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+public class LottoTicket {
+    private final int count;
+
+    public LottoTicket(int count) {
+        this.count = count;
+    }
+
+    public int getLottoTicketCount() {
+        return count;
+    }
+}

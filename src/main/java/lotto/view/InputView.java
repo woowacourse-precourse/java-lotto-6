a84065d19e40d34f28fe3bdd2008c8bab9b55789
GO-TE <alsrudxo9810@naver.com>
2023-11-06 01:31:49 +0900

    feat(InputView): 로또 당첨 번호 입력 기능 구현 inputWinningNumber() : - 콤마가 존재하는지 검증 - 1~45 사이 정수가 아니라면 예외 처리 - 중복 유무 검증 및 숫자가 6개인지 검증

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import camp.nextstep.edu.missionutils.Console;
+
+
 
 public class InputView {
 
@@ -16,6 +22,36 @@ public class InputView {
             throw new IllegalArgumentException("1000원 단위 정수로 입력해 주세요."); // 하드코딩 제거
         }
         return intBudget;
+    }
+
+    public ArrayList<Integer> inputWinningNumber() { // 기능 나누기 -> validator
+        ArrayList<Integer> winningNumber = new ArrayList<>();
+        String input = Console.readLine();
+        int intNumber;
+
+        if (!input.contains(",")) { // 입력에 ","가 존재하는지?
+            throw new IllegalArgumentException("콤마(,)로 구분하여 1~45 사이의 수를 중복 없이 6개 입력해 주세요."); // 하드 코딩 제거
+        }
+        String[] splitInput = input.split(","); // 하드 코딩 제거
+        for (String number : splitInput) {
+            try {
+                intNumber = Integer.parseInt(number);
+                if (!(intNumber >= 1 && intNumber <= 45)) {
+                    throw new IllegalArgumentException("콤마(,)로 구분하여 1~45 사이의 수를 중복 없이 6개 입력해 주세요."); // 하드 코딩 제거
+                }
+                winningNumber.add(intNumber);
+            } catch (NumberFormatException exception) {
+                throw new IllegalArgumentException("콤마(,)로 구분하여 1~45 사이의 수를 중복 없이 6개 입력해 주세요."); // 하드 코딩 제거
+            }
+        }
+
+        Set<Integer> SetNumber = new HashSet<>(winningNumber);
+        if (SetNumber.size() != 6) { // 중복 유무 검증 및 6개인지 검증 / 하드 코딩 제거
+            throw new IllegalArgumentException("콤마(,)로 구분하여 1~45 사이의 수를 중복 없이 6개 입력해 주세요."); // 하드 코딩 제거)
+        }
+
+        return winningNumber;
+
     }
 
 

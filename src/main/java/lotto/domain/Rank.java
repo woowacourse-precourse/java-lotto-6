495955b0b786315080530b86df9d7295a75bd87c
siho-song <thdtlgh234@naver.com>
2023-11-06 01:54:34 +0900

    [Refactor] 랭크 순서변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000, false),
-    SECOND(5, 30000000, true),
-    THIRD(5, 1500000, false),
-    FOURTH(4, 50000, false),
     FIFTH(3, 5000, false),
+    FOURTH(4, 50000, false),
+    THIRD(5, 1500000, false),
+    SECOND(5, 30000000, true),
+    FIRST(6, 2000000000, false),
     LOSING_TICKET(0, 0, false);
 
     private final int matchingCount;

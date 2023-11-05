364bd53002a922a25c265d2cd73cfa82a8c0733b
siho-song <thdtlgh234@naver.com>
2023-11-05 19:06:21 +0900

    [Refactor] 상수명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,26 +6,26 @@ import lotto.domain.LottoNumberRange;
 import lotto.utils.Utils;
 
 public class InputView {
-    private final static String GET_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private final static String GET_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private final static String GET_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private final static String AMOUNT_FORMAT = "구입금액을 입력해 주세요.";
+    private final static String WINNING_NUMBER_FORMAT = "당첨 번호를 입력해 주세요.";
+    private final static String BONUS_NUMBER_FORMAT = "보너스 번호를 입력해 주세요.";
 
     private static void showMessage(String message) {
         System.out.println(message);
     }
 
     public static int inputAmount() {
-        showMessage(GET_AMOUNT_MESSAGE);
+        showMessage(AMOUNT_FORMAT);
         return Utils.convertStringToInteger(Console.readLine());
     }
 
     public static String inputWinningNumbers() {
-        showMessage(GET_NUMBERS_MESSAGE);
+        showMessage(WINNING_NUMBER_FORMAT);
         return Console.readLine();
     }
 
     public static int inputBonusNumber() {
-        showMessage(GET_BONUS_NUMBER_MESSAGE);
+        showMessage(BONUS_NUMBER_FORMAT);
         int bonusNumber = Utils.convertStringToInteger(Console.readLine());
         validateNumberRange(bonusNumber);
         return bonusNumber;

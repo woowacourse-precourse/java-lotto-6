7da5554c0f7035a8bcd9636a269c0bb3512d65a2
mainfn <_@mainfn.dev>
2023-11-05 12:26:31 +0900

    refactor: 당첨 통계 결과 컴포넌트 템플릿 리팩토링

--- src/main/java/component/output/DrawingStatisticComponent.java
+++ src/main/java/component/output/DrawingStatisticComponent.java
@@ -5,13 +5,17 @@ import lotto.controller.dto.output.LottosDrawingResult;
 
 public final class DrawingStatisticComponent implements Component {
 
-    private static final String HEADER = "\n당첨 통계\n---\n";
-    private static final String FIRST_LINE = "3개 일치 (5,000원) - %d개\n";
-    private static final String SECOND_LINE = "4개 일치 (50,000원) - %d개\n";
-    private static final String THIRD_LINE = "5개 일치 (1,500,000원) - %d개\n";
-    private static final String FOURTH_LINE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
-    private static final String FIFTH_LINE = "6개 일치 (2,000,000,000원) - %d개\n";
-    private static final String FOOTER = "총 수익률은 %.1f%%입니다.";
+    private static final String TEMPLATE = """
+             
+                    당첨 통계
+                    ---
+                    3개 일치 (5,000원) - %d개
+                    4개 일치 (50,000원) - %d개
+                    5개 일치 (1,500,000원) - %d개
+                    5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+                    6개 일치 (2,000,000,000원) - %d개
+                    총 수익률은 %.1f%%입니다.
+            """.replaceAll("( ){2,}", "");
     private final LottosDrawingResult data;
 
     public DrawingStatisticComponent(final LottosDrawingResult data) {
@@ -21,7 +25,7 @@ public final class DrawingStatisticComponent implements Component {
     @Override
     public String render() {
         return String.format(
-                HEADER + FIRST_LINE + SECOND_LINE + THIRD_LINE + FOURTH_LINE + FIFTH_LINE + FOOTER,
+                TEMPLATE,
                 data.fifthCount(),
                 data.fourthCount(),
                 data.thirdCount(),

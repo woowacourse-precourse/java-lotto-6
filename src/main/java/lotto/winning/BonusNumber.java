9d9743c10ff33b3e8dabc58135e6aa36372f5bab
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 17:37:14 +0900

    feat(winning): enforce bonus number input within valid range

--- src/main/java/lotto/winning/BonusNumber.java
+++ src/main/java/lotto/winning/BonusNumber.java
@@ -1,4 +1,56 @@
 package lotto.winning;
 
-public class BonusNumber {
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.Settings.MINIMUM;
+import static lotto.Settings.MAXIMUM;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import lotto.Askable;
+
+public class BonusNumber implements Askable {
+    @Override
+    public Integer ask() {
+        System.out.println(inputBonusNumber);
+
+        String input;
+        List<Integer> convertedInput;
+        do {
+            input = readLine();
+            convertedInput = convertInput(input);
+
+        } while (!validate(convertedInput));
+
+        System.out.println();
+
+        return convertedInput.get(0);
+    }
+
+    private boolean validate(List<Integer> convertedInput) {
+        boolean corretInput = true;
+
+        try {
+            if (!isCorrectRange.test(convertedInput)) {
+                throw new IllegalArgumentException(numberRangeError);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            corretInput = false;
+        }
+
+        return corretInput;
+    }
+
+    private List<Integer> convertInput(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private final Predicate<List<Integer>> isCorrectRange = input ->
+            input.stream().mapToInt(Integer::intValue).min().getAsInt() >= MINIMUM.getNumber() &&
+                    input.stream().mapToInt(Integer::intValue).max().getAsInt() <= MAXIMUM.getNumber();
 }

a922e21ed089c1291e98cdee5efb2b49c05e311c
JongukYang <didwhddnr123@naver.com>
2023-11-05 16:36:22 +0900

    fix: 예외 메세지는 항상 [ERROR]로 시작

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -2,10 +2,10 @@ package lotto.view;
 
 public enum ErrorMessage {
 
-    INPUT_DOES_NOT_BLANK("Input cannot be blank."),
-    INPUT_MUST_BE_NUMERIC("Input must be a numeric value."),
-    INPUT_MUST_BE_POSITIVE("Input must be a positive number."),
-    INPUT_MUST_BE_MULTIPLE_OF_LOTTO_PRICE_UNIT("Input must be in multiples of 1000.");
+    INPUT_DOES_NOT_BLANK("[ERROR] Input cannot be blank."),
+    INPUT_MUST_BE_NUMERIC("[ERROR] Input must be a numeric value."),
+    INPUT_MUST_BE_POSITIVE("[ERROR] Input must be a positive number."),
+    INPUT_MUST_BE_MULTIPLE_OF_LOTTO_PRICE_UNIT("[ERROR] Input must be in multiples of 1000.");
 
     private final String message;
 

ff6569033232b36b5c62ab3c2084341ce7f05f60
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-06 00:59:46 +0900

    fix: fix split regex
    split("")를 split(",")으로 수정

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -6,7 +6,7 @@ import java.util.List;
 // 여기서 리팩터링 진행할 것
 public class Converter {
     public static List<Integer> toIntegerList(String input) {
-        return Arrays.stream(input.trim().split(""))
+        return Arrays.stream(input.trim().split(","))
                 .mapToInt(Integer::parseInt)
                 .boxed().toList();
     }

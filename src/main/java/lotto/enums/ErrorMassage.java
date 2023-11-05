7e8d418a3b6fe80c0ab5d2ad1e8bfe54f65e2cc6
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 02:51:52 +0900

    feat: 구분 기호로 구분되어 있지 않을 때 사용하는 에러 메시지 추가

--- src/main/java/lotto/enums/ErrorMassage.java
+++ src/main/java/lotto/enums/ErrorMassage.java
@@ -4,6 +4,7 @@ import static lotto.domain.LottoConfig.LOTTO_COUNT;
 import static lotto.domain.LottoConfig.LOTTO_MAX_NUMBER;
 import static lotto.domain.LottoConfig.LOTTO_MIN_NUMBER;
 import static lotto.domain.LottoConfig.LOTTO_PRICE;
+import static lotto.enums.Delimiter.*;
 
 public enum ErrorMassage {
     OUT_OF_RANGE_LOTTO_NUMBER(String.format("로또 번호는 %d ~ %d 사이의 숫자여야 합니다.",
@@ -12,6 +13,7 @@ public enum ErrorMassage {
     DUPLICATE_LOTTO_NUMBER("중복된 로또 번호가 있습니다."),
     NOT_ENOUGH_AMOUNT(String.format("로또 한 장의 가격(%d원)보다 적습니다.", LOTTO_PRICE.getValue())),
     INCORRECT_AMOUNT_UNIT(String.format("구매 금액은 %d원 단위여야 합니다.", LOTTO_PRICE.getValue())),
+    NOT_SEPARATE_BY_DELIMITER(String.format("%s(%s)로 구분되어야 합니다.", COMMA.getKorName(), COMMA.getValue())),
     ;
 
     private static final String ERROR = "[ERROR] ";

2312d018c1693db55d34e93ef39becd830d833a1
노정완 <jeongwan.noh@gmail.com>
2023-11-05 15:37:04 +0900

    feat: Enum 기반 로또 제약조건 상수 추가

--- src/main/java/lotto/domain/constants/LottoConstraint.java
+++ src/main/java/lotto/domain/constants/LottoConstraint.java
@@ -1,4 +1,19 @@
 package lotto.domain.constants;
 
-public class LottoConstraint {
+public enum LottoConstraint {
+    LOTTO_PRICE(1000),
+
+    NUMBER_RANGE_MINIMUM(1),
+    NUMBER_RANGE_MAXIMUM(45);
+
+    private final int value;
+
+    LottoConstraint(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }

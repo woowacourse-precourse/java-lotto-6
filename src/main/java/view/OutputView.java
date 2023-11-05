60cafc81d4847593cb1fa25fc35f69d42378a722
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-05 16:15:54 +0900

    feat: 로또 개수 출력 메서드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,10 @@
 package view;
 
+import util.OutputMessage;
+
 public class OutputView {
 
+    public static void printNumOfLotto(int number) {
+        System.out.println(number+ OutputMessage.SHOW_AMOUNT_OF_LOTTO.getOutputMessage());
+    }
 }

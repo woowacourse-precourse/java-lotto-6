3cc2c59e9cf586eca8ded941a679f9d1d674782d
hjhan <hjhan.0621@gmail.com>
2023-11-06 03:54:24 +0900

    feat: 구매한 로또 개수 출력하는 메세지 추가

--- src/main/java/lotto/output/OutputView.java
+++ src/main/java/lotto/output/OutputView.java
@@ -1,7 +1,8 @@
 package lotto.output;
 
 public enum OutputView {
-    INPUT_COST("구입금액을 입력해 주세요.");
+    INPUT_COST("구입금액을 입력해 주세요."),
+    NUMBER_OF_LOTTOS("개를 구매했습니다.");
 
     private final String message;
 

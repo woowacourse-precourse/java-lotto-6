3f41db4309b70a2ae1430d4a0fb16c2ac667a201
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-05 23:08:10 +0900

    🎯feat(view/InputView) : 구입금액 입력 받기 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,8 +8,14 @@ import camp.nextstep.edu.missionutils.Console;
 * */
 public class InputView {
     String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+
+    // 구입금액 입력 메세지 출력
     public void printInputPurchaseAmountMessage() {
         println(INPUT_PURCHASE_AMOUNT);
     }
 
+    // 구입금액 입력 받기
+    public String inputPurchaseAmount() {
+        return Console.readLine();
+    }
 }

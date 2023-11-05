3db932b72a76f9fbd92f14f4a864ca33faff5c51
shin5774 <babo5774@naver.com>
2023-11-05 09:25:22 +0900

    Feat: 당첨 로또 생성 기능 구현
    - 인자로 받은 정수 리스트를 통해 당첨번호로 이루어진 로또를 만듬.

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 public class LottoGameController {
 
-    public Lottos purchaseLotto(long buyCash){
-        LottoService service=new LottoService();
-        long buyAmount= service.getLottoAmount(buyCash);
+    public Lottos purchaseLotto(long buyCash) {
+        LottoService service = new LottoService();
+        long buyAmount = service.getLottoAmount(buyCash);
 
-        List<Lotto> lottos=LottoGenerator.INSTANCE.generate((int)buyAmount);
+        List<Lotto> lottos = LottoGenerator.INSTANCE.generate((int) buyAmount);
 
         return new Lottos(lottos);
+    }
+
+    public Lotto generateWinningLotto(List<Integer> winningNumbers) {
+        return new Lotto(winningNumbers);
     }
 }

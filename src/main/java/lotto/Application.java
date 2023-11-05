401743984214846d9e04c4199a25d0112a41cdde
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-05 21:52:00 +0900

    feat: 구입금액을 요청하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public int requestPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String stringPurchaseAmount = Console.readLine();
+        return Integer.parseInt(stringPurchaseAmount);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }

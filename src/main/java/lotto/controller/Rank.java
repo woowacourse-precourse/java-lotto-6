f7989bac0a7b270098858fcbfd96a0d7fe7ad857
Jiwon Na <jiwonna52@gmail.com>
2023-11-05 16:02:47 +0900

    Refactor : prizePrint를 출력을 위한 문장 자체로 변경

--- src/main/java/lotto/controller/Rank.java
+++ src/main/java/lotto/controller/Rank.java
@@ -3,11 +3,11 @@ package lotto.controller;
 import java.util.Arrays;
 
 public enum Rank {
-    FIFTH(5, 3, 5000, "5,000"),
-    FOURTH(4, 4, 50000, "50,000"),
-    THIRD(3, 5, 1500000, "1,500,000"),
-    SECOND(2, 5, 30000000, "30,000,000"),
-    First(1, 6, 2000000000, "2,000,000,000");
+    FIFTH(5, 3, 5000, "3개 일치 (5,000원) - "),
+    FOURTH(4, 4, 50000, "4개 일치 (50,000원) - "),
+    THIRD(3, 5, 1500000, "5개 일치 (1,500,000원) - "),
+    SECOND(2, 5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    First(1, 6, 2000000000, "6개 일치 (2,000,000,000원) - ");
 
     private final int ranking;
     private final int correctNum;

4d83e66524744dcc7ba494e0a4076412a5dc9f71
mj.kim <alswo9853@gmail.com>
2023-11-05 21:03:50 +0900

    feat: 로또 구입 금액 입력 기능

---
+++ src/main/java/lotto/domain/Vendor.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Vendor {
+    private int money;
+
+    public int setMoney() {
+        int numOfLotto = 0;
+        while (true) {
+            try {
+                this.money = insertMoney();
+                numOfLotto = calculateLottoNum(this.money);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return numOfLotto;
+    }
+
+    public int insertMoney() {
+        try {
+            String tmp = Console.readLine();
+            return Integer.parseInt(tmp);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 양의 점수만 입력하실 수 있습니다.");
+        }
+    }
+
+    public int calculateLottoNum(int money) {
+        if(money < 0){
+            throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
+        }
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로만 입력해주세요.");
+        }
+        return money / 1000;
+    }
+
+
+}

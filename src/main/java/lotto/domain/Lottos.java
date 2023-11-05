ed373d88ebd3de96c563ac5640dd153624696c0d
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-05 18:00:14 +0900

    feat: 로또 번호 생성 및 로또 객체 생성 메서드 구현
    LottoTickets 클래스를 Lottos로 변경
    번호 생성 create 메서드와 로또 객체 생성을 위한 generateNumber 메서드 추가

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos;
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public Lottos(LottoNumberGenerator lottoNumberGenerator, int numberOfLottos) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+        this.lottos = create(numberOfLottos);
+    }
+
+    private List<Lotto> create(int numberOfLottos) {
+        List<Lotto> lottoBundle = new ArrayList<>();
+        for (int count = 0; count < numberOfLottos; count++) {
+            Lotto lotto = generateNumber();
+            lottoBundle.add(lotto);
+        }
+        return lottoBundle;
+    }
+    private Lotto generateNumber() {
+        List<Integer> numbers = lottoNumberGenerator.generateNumbers();
+        return new Lotto(numbers);
+    }
+}

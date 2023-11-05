171b23258076ced2a0a6fc861882aad8a56534b8
Isaac Han <isaachan233@gmail.com>
2023-11-05 14:28:31 +0900

    test: 보너스 번호 LottoNumber 적용 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 class WinningLottoTest {
     @Test
     void createWinningLotto() {
-        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), "7");
-        winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), new LottoNumber(7));
+        winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), new LottoNumber(7));
     }
 }

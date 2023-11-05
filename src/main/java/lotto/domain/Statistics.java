e5827cb51ac83cc9e08238bcf013ce6178120b10
DaeYoungK <hanguk830@naver.com>
2023-11-05 12:38:59 +0900

    feat(Statistics): 당첨번호와 일치하는 개수를 받아 enum 값을 반환하는 기능 추가

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Statistics {
 
     THREE(3, 5000),
     FOUR(4, 50000),
     FIVE(5, 1500000),
     FIVE_BONUS(5, 30000000),
-    SIX(6, 2000000000);
+    SIX(6, 2000000000),
+    NON(0,0);
 
     private int count;
     private int amount;
@@ -14,5 +17,18 @@ public enum Statistics {
     Statistics(int count, int amount) {
         this.count = count;
         this.amount = amount;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static Statistics getStatistics(int matchCount) {
+        return Arrays.stream(Statistics.values())
+                .filter(data -> data.count == matchCount).findAny().orElse(NON);
     }
 }

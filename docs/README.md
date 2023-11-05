4e6de5108157fd58894cf3e4e62a8b6ab952e5fc
Ji <zeegeon@naver.com>
2023-11-06 00:23:34 +0900

    init program

---
+++ docs/README.md
@@ -0,0 +1,18 @@
+### 로또
+
+> LottoController Class 
+> 전체 게임을 컨트롤 하는 컨트롤러
+> 
+> playLotto() : 시작할 때 실행되는 함수 <br>
+>   > start() : 구매금액을 입력받고 적절한 입력값인지 판단<br>
+>   > generateLottoNumber : 로또 번호를 생성 <br>
+>   > middle() : 당첨번호들을 입력 <br>
+>   > calculate() : 당첨결과 계산 <br>
+>   > printEnd() : 당첨 통계 출력 <br>
+
+> Lotto Class
+>   * validate()
+>   * 6자리가 아닐경우 예외 발생
+
+> Util Class
+>   * 값의 적절유무 등을 계산하는 메서드
---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,32 @@
+package lotto;
+
+
+public class LottoController {
+
+    private Lotto lotto;
+
+    public void playLotto() {
+        start();
+        generateLottoNumber();
+        middle();
+        calculate();
+        printEnd();
+    }
+
+    private void printEnd() {
+    }
+
+    private void calculate() {
+    }
+
+    private void middle() {
+    }
+
+    private void generateLottoNumber() {
+    }
+
+    private void start() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+}

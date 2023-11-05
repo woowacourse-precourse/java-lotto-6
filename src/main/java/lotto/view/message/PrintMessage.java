46bbc1444e3d967933db1c9a854b996c59eab8ef
xogus3492 <wkdxogus3492@gmail.com>
2023-11-05 21:03:52 +0900

    Fix: 로또 구매 출력문 수정

--- src/main/java/lotto/view/message/PrintMessage.java
+++ src/main/java/lotto/view/message/PrintMessage.java
@@ -8,7 +8,7 @@ public enum PrintMessage {
     BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요."),
 
     // OutputView
-    LOTTO_COUNT_OUTPUT_MESSAGE("개를 구입했습니다.");
+    LOTTO_COUNT_OUTPUT_MESSAGE("개를 구매했습니다.");
 
     private final String message;
 

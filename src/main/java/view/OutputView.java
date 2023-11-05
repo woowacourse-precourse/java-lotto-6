3497face33d04dd6d63cd5276eed5d3bb052f56b
kkw2758 <dtc3229@gmail.com>
2023-11-05 20:35:30 +0900

    Feat(OutputView) : "보너스 번호를 입력해주세요." 문구를 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public static void printAnswerLottoNumberInputMessage() {
         System.out.println("당첨 번호를 입력해주세요.");
     }
+
+    public static void printBonusNumberInputMessage() {
+        System.out.println("보너스 번호를 입력해주세요.");
+    }
 }

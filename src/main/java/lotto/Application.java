8905e8bd25c84deb91edd884c468a251c35023b2
hyebin <xxico89@gmail.com>
2023-11-06 00:44:23 +0900

    [Feat] 구입 금액 숫자가 아닌 경우 예외처리 추가 및 구입 금액 입력 기능 모듈화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,4 +1,5 @@
 package lotto;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -9,19 +10,42 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Application {
+
+    private static int isInt(String value) {
+        for (char c : value.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+            }
+        }
+
+        return Integer.parseInt(value);
+    }
+
+    private static int inputPurchaseMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int money;
+
+        try {
+            money = isInt(Console.readLine());
+            if (money % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액이 1,000원 단위가 아닙니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputPurchaseMoney();
+        }
+        return money;
+    }
+
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(Console.readLine());
-
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액이 1,000원 단위가 아닙니다.");
-        }
+        int money = inputPurchaseMoney();
         int amount = money / 1000;
-        System.out.println("\n"+amount+"개를 구매했습니다.");
+        System.out.println("\n" + amount + "개를 구매했습니다.");
 
         List<Lotto> lottos = new ArrayList<Lotto>();
 
-        for (int i=0; i<amount; i++) {
+        for (int i = 0; i < amount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(numbers);
             System.out.println(lotto.getNumbers());
@@ -51,12 +75,12 @@ public class Application {
             correctLotto.add(count);
         }
 
-        System.out.println(prizeStatus.THREE.getNum() + "개 일치 (" + prizeStatus.THREE.getPrice() + ") - " + Collections.frequency(correctLotto, 3)+"개");
-        System.out.println(prizeStatus.FOUR.getNum() + "개 일치 (" + prizeStatus.FOUR.getPrice() + ") - " + Collections.frequency(correctLotto, 4)+"개");
-        System.out.println(prizeStatus.FIVE.getNum() + "개 일치 (" + prizeStatus.FIVE.getPrice() + ") - " + Collections.frequency(correctLotto, 5)+"개");
-        System.out.println(prizeStatus.FIVEBONUS.getNum() + "개 일치 (" + prizeStatus.FIVEBONUS.getPrice() + ") - " + Collections.frequency(correctLotto, 105)+"개");
-        System.out.println(prizeStatus.SIX.getNum() + "개 일치 (" + prizeStatus.SIX.getPrice() + ") - " + Collections.frequency(correctLotto, 6)+"개");
+        System.out.println(prizeStatus.THREE.getNum() + "개 일치 (" + prizeStatus.THREE.getPrice() + ") - " + Collections.frequency(correctLotto, 3) + "개");
+        System.out.println(prizeStatus.FOUR.getNum() + "개 일치 (" + prizeStatus.FOUR.getPrice() + ") - " + Collections.frequency(correctLotto, 4) + "개");
+        System.out.println(prizeStatus.FIVE.getNum() + "개 일치 (" + prizeStatus.FIVE.getPrice() + ") - " + Collections.frequency(correctLotto, 5) + "개");
+        System.out.println(prizeStatus.FIVEBONUS.getNum() + "개 일치 (" + prizeStatus.FIVEBONUS.getPrice() + ") - " + Collections.frequency(correctLotto, 105) + "개");
+        System.out.println(prizeStatus.SIX.getNum() + "개 일치 (" + prizeStatus.SIX.getPrice() + ") - " + Collections.frequency(correctLotto, 6) + "개");
 
     }
-    }
+}
 

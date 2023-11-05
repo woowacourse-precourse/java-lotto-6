00c14c64d7b4838e0ff26e3975f3c3521f9202bd
jmj005 <jmj00005@naver.com>
2023-11-06 02:27:32 +0900

    test: 보너스 번호 범위 성공 테스트 추가

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,18 @@ public class BonusNumberTest {
     @DisplayName("보너스 번호 범위 실패 테스트")
     void invalidRange(int bonusNumber) {
         assertThrows(IllegalArgumentException.class,
+                () -> new BonusNumber(bonusNumber));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1",
+            "33",
+            "45"
+    })
+    @DisplayName("보너스 번호 범위 성공 테스트")
+    void validRange(int bonusNumber) {
+        assertDoesNotThrow(
                 () -> new BonusNumber(bonusNumber));
     }
 }

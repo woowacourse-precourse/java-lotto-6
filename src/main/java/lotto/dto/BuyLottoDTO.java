64387fdd5678467480f8ea74ed9cc47926032798
howudong <tjdvy963@naver.com>
2023-11-05 23:24:23 +0900

    refactor : 값을 덮어쓰지 못하도록 validate 추가

--- src/main/java/lotto/dto/BuyLottoDTO.java
+++ src/main/java/lotto/dto/BuyLottoDTO.java
@@ -14,6 +14,7 @@ public final class BuyLottoDTO {
         }
 
         public void setBuyPrice(Long buyPrice) {
+            validateOverride(this.buyPrice);
             this.buyPrice = buyPrice;
         }
     }

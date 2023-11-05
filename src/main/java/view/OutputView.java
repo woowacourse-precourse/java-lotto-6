960728bae0e6047fac662c0b3732722bb1214b33
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-05 16:27:58 +0900

    feat: 로또 번호들을 출력하는 함수 및 빈줄 출력 함수 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,9 +2,19 @@ package view;
 
 import util.OutputMessage;
 
+import java.util.List;
+
 public class OutputView {
 
     public static void printNumOfLotto(int number) {
         System.out.println(number+ OutputMessage.SHOW_AMOUNT_OF_LOTTO.getOutputMessage());
+    }
+
+    public static void printLottoTickets(List<Integer> numbers) {
+        System.out.println(numbers);
+    }
+
+    public static void printEmptyLine() {
+        System.out.println();
     }
 }

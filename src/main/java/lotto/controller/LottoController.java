652be81b2df607b4d8a4550b80b924eafdd2e059
youngbeom-Kim <abcd450196@gmail.com>
2023-11-06 03:36:50 +0900

    feat: lottoPublisher 메서드 Controller 생성 및 중복 유효성 검사 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,28 +1,43 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoPurchase;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoController {
 
     LottoPurchase lottoPurchase = new LottoPurchase();
+    LottoNumber lottoNumber = new LottoNumber();
 
     public void run() {
-        purchaseLotto();
+        int lottoAmount = purchaseLotto();
+        publishLotto(lottoAmount);
     }
 
-    public void purchaseLotto() {
+    public int purchaseLotto() {
         while (true) {
             OutputView.lottoPurchaseInputMessage();
             try {
-                lottoPurchase.lottoPurchaseCount(InputView.input());
+                int lottoCount = lottoPurchase.lottoPurchaseCount(InputView.input());
                 OutputView.newLine();
-                break;
+                return lottoCount;
             } catch (IllegalArgumentException e) {
                 System.out.println(e);
             }
         }
+    }
+
+    public void publishLotto(int lottoCount) {
+        OutputView.lottoPurchaseCheckOutputMessage(lottoCount);
+
+        lottoNumber.lottoTickets(lottoCount);
     }
 
 }
--- src/main/java/lotto/util/validation/LottoValidation.java
+++ src/main/java/lotto/util/validation/LottoValidation.java
@@ -2,6 +2,10 @@ package lotto.util.validation;
 
 import lotto.util.ErrorThrower;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.config.ErrorMessage.LOTTO_DISTINCT_ERROR;
 import static lotto.config.ErrorMessage.ONLY_NUMBER_ERROR;
 
 public class LottoValidation {
@@ -10,6 +14,16 @@ public class LottoValidation {
 
         if (!input.matches("[0-9]+")) {
             ErrorThrower.throwIllegalArgumentException(ONLY_NUMBER_ERROR.getMessage());
+        }
+
+    }
+
+    public void validatorDuplicatedNumber(List<Integer> lottoNumbers) {
+
+        lottoNumbers = lottoNumbers.stream().distinct().collect(Collectors.toList());
+
+        if (lottoNumbers.size() != 6) {
+            ErrorThrower.throwIllegalArgumentException(LOTTO_DISTINCT_ERROR.getMessage());
         }
 
     }

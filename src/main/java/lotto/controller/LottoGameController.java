035f522abc54b0b9de982bfbedbfbc85b6d9108b
dd <12202340@inha.edu>
2023-11-05 14:47:58 +0900

    feat: 발행한 로또 수량과 숫자를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,9 +1,11 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.domain.generator.LottoGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoGameController {
     private Money money;
@@ -12,5 +14,13 @@ public class LottoGameController {
     public void startGame() {
         money = Money.from(InputView.inputAmount());
         lottos = new Lottos(LottoGenerator.generateLottos(money.getLottoCount()));
+        printLottos();
+    }
+
+    private void printLottos() {
+        OutputView.printLottoCountMessage(money.getLottoCount());
+        for (Lotto lotto : lottos.getLottos()) {
+            OutputView.printLottos(lotto.getNumber());
+        }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_COUNT_MESSAGE = "\n%d개를 구매했습니다.";
 
     public static final void printInputAmountMessage() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
+    }
+
+    public static final void printLottoCountMessage(int lottoCount) {
+        System.out.println(String.format(LOTTO_COUNT_MESSAGE, lottoCount));
+    }
+
+    public static final void printLottos(List<Integer> lottoNumbers) {
+        System.out.println(lottoNumbers);
     }
 }

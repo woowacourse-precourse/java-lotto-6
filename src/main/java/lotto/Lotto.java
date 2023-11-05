eacba2463cadf3d1aa679f5e4ff84b831814354a
dabeann <dabin4078@daum.net>
2023-11-05 12:48:51 +0900

    add: validate

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,8 +11,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers == null || numbers.size() != 6 || !numbers.stream().allMatch(n -> n >= 1 && n <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 6개 숫자여야 합니다.");
+        }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 

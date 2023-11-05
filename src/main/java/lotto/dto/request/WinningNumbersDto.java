e310b29f40e9dc1a4839d4ac8f305c7be58ea4f5
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-05 20:05:23 +0900

    feat(WinningNumbersDto): 사용자가 당첨 번호 6개와 보너스 번호 1개를 입력할 수 있어야 한다.

---
+++ src/main/java/lotto/dto/request/WinningNumbersDto.java
@@ -0,0 +1,27 @@
+package lotto.dto.request;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningNumbersDto {
+
+    private final List<Integer> numbers;
+    private final int bonus;
+
+    private WinningNumbersDto(List<Integer> numbers, int bonus) {
+        this.numbers = numbers;
+        this.bonus = bonus;
+    }
+
+    public static WinningNumbersDto of(List<Integer> numbers, int bonus) {
+        return new WinningNumbersDto(numbers, bonus);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}

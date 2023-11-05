c7c8c01e2bfc32a5ae820a6865a20232066d763f
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-05 16:34:05 +0900

    feat: 당첨 번호 예외 처리 클래스 추가
    생성 시 String을 보내면 List<Integer> 형태로
    바꿀 수 있는 지 체크
    생성 시 List<Integer>를 보내면 중복, 크기, 번호 범위를
    체크 해준다.

---
+++ src/main/java/lotto/validator/WinningNumValidator.java
@@ -0,0 +1,52 @@
+package lotto.validator;
+
+import java.util.List;
+
+public class WinningNumValidator {
+
+    private static final String ERROR = "[ERROR]";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자를 적으면 안 됩니다.";
+    private static final String SIZE_OVER_MESSAGE = "6개의 번호를 입력해주세요.";
+    private static final String RANGE_OVER_MESSAGE = "1~45사이의 번호를 입력해주세요.";
+
+    public WinningNumValidator(String input){
+        for(String number : input.split(",")) {
+            isNumError(number);
+        }
+    }
+
+    public WinningNumValidator(List<Integer> numbers){
+        isDuplicate(numbers);
+        isSizeOver(numbers);
+        isRangeOver(numbers);
+    }
+
+    public void isNumError(String number){
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR + NOT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public void isSizeOver(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ERROR + SIZE_OVER_MESSAGE);
+        }
+    }
+
+    public void isRangeOver(List<Integer> numbers) {
+        for(Integer number : numbers){
+            if (number>45 || number<1){
+                throw new IllegalArgumentException(ERROR + RANGE_OVER_MESSAGE);
+            }
+        }
+    }
+
+    public void isDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()){
+            throw new IllegalArgumentException(ERROR + DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+}

bffe02a0ad2f1235730d6af3244f8d6a29bded42
koomchang <leahpar0401@gmail.com>
2023-11-05 21:36:06 +0900

    feat: 당첨 번호를 입력받을 때 중복 되는 경우 예외 처리

--- src/main/java/lotto/validator/WinningNumbersInputValidator.java
+++ src/main/java/lotto/validator/WinningNumbersInputValidator.java
@@ -1,5 +1,7 @@
 package lotto.validator;
 
+import java.util.Arrays;
+
 public class WinningNumbersInputValidator implements BasicValidator<String> {
 
     private static final int MIN_LOTTO_NUMBER = 1;
@@ -9,12 +11,14 @@ public class WinningNumbersInputValidator implements BasicValidator<String> {
     private static final String INPUT_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
     private static final String INPUT_NOT_SIX_NUMBERS = "[ERROR] 입력값이 6자리가 아닙니다.";
     private static final String INPUT_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 입력값이 1과 45 사이여야 합니다.";
+    private static final String INPUT_DUPLICATED = "[ERROR] 입력값들이 중복되지 않아야 합니다.";
 
     @Override
     public void validate(String input) {
         validateAllNumbersIfNumeric(input);
         validateInputNumbersCount(input);
         validateInputNumbersBetweenOneAndFortyFive(input);
+        validateDuplicatedNumber(input);
     }
 
     private void validateAllNumbersIfNumeric(String input) {
@@ -51,5 +55,13 @@ public class WinningNumbersInputValidator implements BasicValidator<String> {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(INPUT_NOT_BETWEEN_ONE_AND_FORTY_FIVE);
         }
+    }
+
+    private void validateDuplicatedNumber(String input) {
+        String[] numbers = input.split(COMMA);
+        if (Arrays.stream(numbers).distinct().count() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException(INPUT_DUPLICATED);
+        }
+
     }
 }

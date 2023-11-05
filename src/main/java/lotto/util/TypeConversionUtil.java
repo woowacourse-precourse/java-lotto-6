f883c4510d71d9d28ff8bec9407d47114b4f5046
WooYoungDoo <duden5000@naver.com>
2023-11-05 16:13:21 +0900

    feat (TypeConversionUtil) : 문자열에서 숫자 리스트로 변환 기능

--- src/main/java/lotto/util/TypeConversionUtil.java
+++ src/main/java/lotto/util/TypeConversionUtil.java
@@ -1,8 +1,21 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TypeConversionUtil {
 
+    private static final String COMMA_SPLIT = ",";
     public static int StringToInt(String inputNumber) {
         return Integer.parseInt(inputNumber);
+    }
+    
+    public static List<Integer> StringToIntegerList(String inputWinningNumber) {
+        List<String> strWinningNumber = Arrays.asList(inputWinningNumber.split(COMMA_SPLIT));
+
+        return strWinningNumber.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

ba50ece235ab9ff13474c3f7c577441314e52f76
Yu JeongMin <polar522@naver.com>
2023-11-05 19:34:55 +0900

    기능1. 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 1장 당 1000원의 로또를 발행해야 한다.  1000원으로 나눠 떨어지지 않는 경우 예외처리를 한다.  구현 ---> buyLottos 메서드를 구현해서 main에서 구입금액 amount를 입력받은 후 1000의 배수가 아니면 IllegalArgumentException을 발생시킴

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+            final int LOTTO_PRICE = 1000;
+            Lotto win_lotto = (Lotto) Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            // 기능1. 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 1장 당 1000원의 로또를 발행해야 한다.
+            // 1000원으로 나눠 떨어지지 않는 경우 예외처리를 한다.
+            System.out.println("구입금액을 입력해 주세요.");
+            int amount = Integer.parseInt(Console.readLine());
+
+            try {
+                // buyLottos 메서드를 구현하여 1000원으로 나눠떨어지지 않는 경우 IllegalArgumentException을 발생시키도록 함.
+                List<Lotto> lottos = Lotto.buyLottos(amount);
+                System.out.println(amount / LOTTO_PRICE + "개를 구매했습니다.");
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR]");
+        }
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +20,18 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+        // 기능1. 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 1장 당 1000원의 로또를 발행해야 한다.
+        // 1000원으로 나눠 떨어지지 않는 경우 예외처리를 한다.
+        public static List<Lotto> buyLottos(int amount) {
+                if(amount % 1000 != 0) {
+                        throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력 가능합니다.");
+                }
+                int numberOfLottos = amount / 1000;
+                List<Lotto> lottos = new ArrayList<>();
+                for (int i = 0; i < numberOfLottos; i++) {
+
+                }
+                return lottos;
+        }
 }

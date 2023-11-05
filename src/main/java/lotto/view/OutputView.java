89692c74270ea76e969cd87c2744946dec3a41ff
Gyeongho Yang <chchch1213@naver.com>
2023-11-05 19:53:16 +0900

    feat(view): 메세지를 출력해주는 OutputView Class 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.constant.GeneralMessage;
+
+public class OutputView {
+    public static void printGeneralMessage(GeneralMessage generalMessage) {
+        System.out.println(generalMessage.getMessage());
+    }
+}

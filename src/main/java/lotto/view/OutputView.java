5180f0a33b3947afd57db91a8df2700a3e5bd645
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-11-06 03:04:14 +0900

    feat: 로또 랜덤 숫자 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
 
     public void printLottoCount(int lottoAmount) {
-        System.out.printf("\n%d개를 구매했습니다.", lottoAmount);
+        System.out.printf("%n%d개를 구매했습니다.%n", lottoAmount);
+    }
+
+    public void printLottoList(List<Lotto> lottoList) {
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto);
+        }
     }
 
 }

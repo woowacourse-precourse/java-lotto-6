3140dd0a9c2c9432446b546f28569b296ecdc554
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-05 23:07:26 +0900

    feat: 당첨 로또번호중 기본 6자리 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,23 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+    public List<Integer> inputWinningLottoStandardNumber() {
+        List<Integer> lottoNumbers;
+        System.out.println("당첨 번호를 입력해주세요.");
+        try {
+            lottoNumbers = Arrays.stream(readLine().split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinningLottoStandardNumber();
+        }
+        return lottoNumbers;
+    }
 }

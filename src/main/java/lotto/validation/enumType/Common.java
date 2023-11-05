12aff0b73edb193fac53b82579af9849489522f3
acs0209 <annjae4676@naver.com>
2023-11-06 00:53:35 +0900

    feat: null, 빈값, 공백 검증에 대한 상수 구현

---
+++ src/main/java/lotto/validation/enumType/Common.java
@@ -0,0 +1,18 @@
+package lotto.validation.enumType;
+
+public enum Common {
+
+    NULL_MESSAGE("[ERROR] 값에 null이 들어올 수 없습니다."),
+    EMPTY_MESSAGE("[ERROR] 값에 빈 값이 들어올 수 없습니다."),
+    SPACE_MESSAGE("[ERROR] 값에 공백만 들어올 수 없습니다.");
+
+    private String message;
+
+    Common(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

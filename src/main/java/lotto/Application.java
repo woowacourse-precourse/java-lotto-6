9967385f3498b570d62cfdb976bf536167aabd21
Dalliaaaaa <ajswlsoato@naver.com>
2023-11-05 20:02:17 +0900

    refactor: 구입금액 입력과 로또 출력 class로 나누기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,42 +7,47 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        User.InputMoney();
+        MyLotto.LottoPaper(Money.InputMoney());
     }
 }
 
-class User{
-    public static void InputMoney(){
-        while(true) {
+class Money {
+    public static int InputMoney() {
+        while (true) {
             System.out.println("구입금액을 입력해 주세요.");
             String m = Console.readLine();
             try {
                 int money = Integer.parseInt(m);
-                LottoPaper(CorrectMoney(money));
-                break;
+                return CorrectMoney(money);
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 제대로 된 값을 입력해주세요.");
             }
         }
     }
-    public static int CorrectMoney(int money){
-        if (money % 1000 == 0){
+
+    public static int CorrectMoney(int money) {
+        if (money % 1000 == 0) {
             return money / 1000;
         }
         throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
     }
-    public static void LottoPaper(int cnt){
+}
+
+class MyLotto{
+    public static void LottoPaper(int cnt) {
         ArrayList<List<Integer>> mylotto = new ArrayList<>();
 
-        for (int i = 0; i < cnt; i++){
+        for (int i = 0; i < cnt; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             mylotto.add(numbers);
         }
         PrintLottoPaper(cnt, mylotto);
     }
-    public static void PrintLottoPaper(int cnt, ArrayList<List<Integer>> lotto){
+
+    public static void PrintLottoPaper(int cnt, ArrayList<List<Integer>> mylotto){
+        System.out.println();
         System.out.println(cnt+"개를 구매했습니다.");
-        for (List<Integer> row : lotto) {
+        for (List<Integer> row : mylotto) {
             System.out.println(row);
         }
     }

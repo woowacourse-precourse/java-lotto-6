f2aca132a3ad489387f5a23321b504657ec020a3
unknown <winternrabbit@gmail.com>
2023-11-05 20:53:08 +0900

    refact: 출력 내용 변경

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -7,7 +7,8 @@ public class Output {
     private static final String NUMBER_OF_PURCHAGE = "개를 구매했습니다.";
 
     public void printNumberOfPurchase(int lottoTicket) {
-        System.out.println("\n" + lottoTicket + NUMBER_OF_PURCHAGE);
+        System.out.println();
+        System.out.println(lottoTicket + NUMBER_OF_PURCHAGE);
     }
 
     public void printRandomLottoNumbers(HashMap<Integer, List<Integer>> randomLottoNumbers) {

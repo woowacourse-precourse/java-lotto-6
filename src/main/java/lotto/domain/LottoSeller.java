6f8a3bc0ffab6f28e9ead8c2a8a7c27990d50952
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 00:02:28 +0900

    feat(LottoSeller): 금액에 맞는 로또를 판매하는 기능 구현

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,2 +1,27 @@
-package lotto.domain;public class LottoSeller {
+package lotto.domain;
+
+import lotto.util.LottoNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoSeller {
+
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public LottoSeller(LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    public List<Lotto> sellLottos(Cash cash) {
+        Integer lottoCounts = cash.getAmount() / 1000;
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCounts; i++) {
+            List<Integer> lottoNumbers = lottoNumberGenerator.generateLottoNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
 }

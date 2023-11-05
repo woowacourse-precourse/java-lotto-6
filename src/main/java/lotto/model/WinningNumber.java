7e4e420967b1c775b56b3ba4c617fa04094ddbfa
sangxxjin <sj_0368@naver.com>
2023-11-05 17:27:37 +0900

    feat: 유효한 숫자인지 검사하고 예외 메소드 추가 및 상수처리

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -8,9 +8,11 @@ public class WinningNumber {
     private static final String DUPLICATE_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호에 중복된 숫자가 포함되어 있습니다.";
     private static final String OUT_OF_RANGE_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 1에서 45 사이의 숫자여야 합니다.";
     private static final String EXCESS_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 6개를 초과할 수 없습니다.";
-
+    private static final String INVALID_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨 번호가 유효하지 않습니다.";
     public WinningNumber(String winningNumbers) {
-        List<Integer> winningNumber = Arrays.stream(winningNumbers.split(",")).map(String::trim).map(Integer::parseInt)
+        List<Integer> winningNumber = Arrays.stream(winningNumbers.split(","))
+                .map(String::trim)
+                .map(this::parseWinningNumber)
                 .toList();
         validateWinningNumbersForDuplicates(winningNumber);
         validateWinningNumbersForRange(winningNumber);
@@ -20,6 +22,14 @@ public class WinningNumber {
 
     public List<Integer> getWinningNumber() {
         return winningNumber;
+    }
+
+    private int parseWinningNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     private void validateWinningNumbersForDuplicates(List<Integer> winningNumber) {

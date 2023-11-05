8998e885cbfcaf32e6a8214f5403152275bf7fff
황중섭 <jshwang9709@gmail.com>
2023-11-06 02:47:56 +0900

    feat(ui): 보너스 번호 입력 메시지 추가

--- src/main/java/lotto/ui/constant/MessageConst.java
+++ src/main/java/lotto/ui/constant/MessageConst.java
@@ -4,5 +4,6 @@ public class MessageConst {
 
     public static final String PURCHASE_REQUEST = "구입금액을 입력해 주세요.";
     public static final String WINNING_MAIN_REQUEST = "당첨 번호를 입력해 주세요.";
+    public static final String WINNING_BONUS_REQUEST = "보너스 번호를 입력해 주세요.";
 
 }

e9d889d5395aa6248a9403e36ab3b126178ac071
KangGeonyoung <kky6335@gmail.com>
2023-11-05 22:36:49 +0900

    feat(LottoController): 잘못된 입력에 대한 재입력 기능 - EnterPurchaseMoney(): 구매 금액을 잘못 입력할 경우 재입력 - EnterWinNumber(): 당첨 번호를 잘못 입력할 경우 재입력 - EnterBonusNumber(List<Integer> winNumber): 보너스 번호를 잘못 입력할 경우 재입력

--- src/main/java/lotto/domain/controller/LottoController.java
+++ src/main/java/lotto/domain/controller/LottoController.java
@@ -22,18 +22,47 @@ public class LottoController {
     }
 
     public void start() {
-        lottoSaleSystem.isValidMoney(InputView.inputPurchaseMoney());
+        EnterPurchaseMoney();
 
         OutputView.printPurchaseCount(lottoSaleSystem.getPurchaseCount());
         generateLotto(lottoSaleSystem.getPurchaseCount());
         OutputView.printPurchasedLotto(purchasedLotto);
 
-        List<Integer> winNumber = lottoWinnerSystem.isValidWinNumber(InputView.inputWinningNumber());
-        List<Integer> bonusNumber = lottoWinnerSystem.isValidBonusNumber(InputView.inputBonusNumber(), winNumber);
+        List<Integer> winNumber = EnterWinNumber();
+        List<Integer> bonusNumber = EnterBonusNumber(winNumber);
 
         lottoWinnerSystem.isWinnerLotto(purchasedLotto, winNumber, bonusNumber);
         lottoWinnerSystem.printLottoResult();
         OutputView.printProfit(LottoSaleSystem.calculateProfit());
+    }
+
+    private List<Integer> EnterBonusNumber(List<Integer> winNumber) {
+        try {
+            List<Integer> bonusNumber = lottoWinnerSystem.isValidBonusNumber(InputView.inputBonusNumber(), winNumber);
+            return bonusNumber;
+        } catch (IllegalArgumentException errorMessage) {
+            System.out.println(errorMessage.getMessage());
+            return EnterBonusNumber(winNumber);
+        }
+    }
+
+    private List<Integer> EnterWinNumber() {
+        try {
+            List<Integer> winNumber = lottoWinnerSystem.isValidWinNumber(InputView.inputWinningNumber());
+            return winNumber;
+        } catch (IllegalArgumentException errorMessage) {
+            System.out.println(errorMessage.getMessage());
+            return EnterWinNumber();
+        }
+    }
+
+    private void EnterPurchaseMoney() {
+        try {
+            lottoSaleSystem.isValidMoney(InputView.inputPurchaseMoney());
+        } catch (IllegalArgumentException errorMessage) {
+            System.out.println(errorMessage.getMessage());
+            EnterPurchaseMoney();
+        }
     }
 
     private void generateLotto(int purchaseCount) {

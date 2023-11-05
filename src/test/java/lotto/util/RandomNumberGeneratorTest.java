269a40f45a011fa086e48d091cfeedd70ce1e0e3
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-05 17:55:27 +0900

    test(feat) : 난수 생성기의 테스트 작성 - 1과 45사이의 숫자를 6개 뽑는 지 테스트

---
+++ src/test/java/lotto/util/RandomNumberGeneratorTest.java
@@ -0,0 +1,33 @@
+package lotto.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import lotto.constant.LottoInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RandomNumberGeneratorTest {
+
+    @DisplayName("1과 45사이의 숫자를 6개 뽑는다.")
+    @Test
+    void pickNumbersTest() {
+        // given
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+
+        // when
+        List<Integer> numbers = numberGenerator.pickNumbers();
+
+        // then
+        Assertions.assertEquals(numbers.size(), 6);
+        Assertions.assertEquals(numbers.size(), 6);
+
+        Integer min = LottoInfo.LOTTO_MIN_NUMBER.getValue();
+        Integer max = LottoInfo.LOTTO_MAX_NUMBER.getValue();
+
+        for (int number : numbers) {
+            assertTrue(number >= min && number <= max);
+        }
+    }
+}

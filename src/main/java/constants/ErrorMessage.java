89946cd0fa445680dd7b777eb61075a7a7c899f3
kkw2758 <dtc3229@gmail.com>
2023-11-05 21:12:02 +0900

    Refactor(ErrorMessage) : 에러 메세지를 관리하는 Enum형의 ErrorMessage 생성

---
+++ src/main/java/constants/ErrorMessage.java
@@ -0,0 +1,17 @@
+package constants;
+
+public enum ErrorMessage {
+    ANSWER_LOTTO_LENGTH_ERROR("당첨번호의 길이가 6이 아닙니다"),
+    ANSWER_LOTTO_HAS_DUPLICATE_NUMBER_ERROR("당첨번호에 중복된 숫자가 포함되어 있습니다."),
+    ANSWER_LOTTO_NOT_IN_RANGE_ERROR("당첨번호에 1 ~ 45 범위 외의 숫자가 포함되어 있습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

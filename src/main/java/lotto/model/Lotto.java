f75d3757d311bfba6a3b3254d95d5f2ac5be6353
huhdy32 <mong3268@gmail.com>
2023-11-05 21:02:22 +0900

    feat: 당첨번호를 통해 점수 얻는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -16,17 +16,33 @@ public class Lotto {
         }
     }
 
+    // TODO: 추가 기능 구현
+    public int[] getScore(final List<Integer> winningNumbers, final int bonusNumber) {
+        int[] score = new int[2];
+        for (int winningNumber : winningNumbers) {
+            if (numbers.contains(winningNumber)) {
+                score[0]++;
+            }
+        }
+        if (score[0] == 5 && numbers.contains(bonusNumber)) {
+            score[1]++;
+        }
+        return score;
+    }
+
     private boolean invalidRange(List<Integer> numbers) {
         for (int i : numbers) {
-            if (i > 45 || i < 1) return true;
+            if (i > 45 || i < 1) {
+                return true;
+            }
         }
         return false;
     }
 
     private boolean isDuplicated(List<Integer> numbers) {
-        for (int i=0; i< numbers.size(); i++){
+        for (int i = 0; i < numbers.size(); i++) {
             int number = numbers.get(i);
-            if (numbers.lastIndexOf(number) != i){
+            if (numbers.lastIndexOf(number) != i) {
                 return true;
             }
         }
@@ -37,5 +53,4 @@ public class Lotto {
     public String toString() {
         return numbers.toString();
     }
-    // TODO: 추가 기능 구현
 }

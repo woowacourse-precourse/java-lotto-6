62d01bc12b24006219cb552847defa2267d7d0dd
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 15:41:54 +0900

    test(purchasing): verify error messages for invalid purchase amounts
    Add tests to ensure error messages are displayed when the purchase amount entered is a non-integer or not divisible by 1000

---
+++ src/test/java/lotto/purchasing/PurchaseAmountTest.java
@@ -0,0 +1,44 @@
+package lotto.purchasing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PurchaseAmountTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PurchaseAmount purchaseAmount = new PurchaseAmount();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setIn(System.in);
+        System.setOut(System.out);
+        Console.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "eight thousand", "8001" })
+    void inAccurateInputs(String mockInput) {
+        System.setIn(new ByteArrayInputStream(mockInput.getBytes()));
+
+        try {
+            purchaseAmount.ask();
+        } catch (NoSuchElementException ignored) {
+        }
+
+        assertThat(outputStream.toString()).contains("[ERROR]");
+    }
+}

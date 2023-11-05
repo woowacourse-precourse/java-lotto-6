86c96285fb4a4276e5d4fa97ace63511593d53e9
dltjdn <hana40004@naver.com>
2023-11-05 18:48:26 +0900

    feat(ErrorLogger): 에러 메시지 출력 기능

---
+++ src/main/java/lotto/ErrorLogger.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class ErrorLogger {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    public static void log(IllegalArgumentException e) {
+        System.out.println(ERROR_PREFIX + e.getMessage());
+    }
+}

5ddd73f35e68be703f651b4c34aa1e7781d90f25
hoiae <donald819@naver.com>
2023-11-06 00:33:36 +0900

    feat: Lotto클래스 test코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,15 +3,16 @@ package lotto;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private Lotto lotto;
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -27,6 +28,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @BeforeEach
+    void setUp(){
+        lotto = new Lotto(List.of(1,2,3,4,5,6));
+    }
     @DisplayName("로또 번호를 출력하는 지 확인한다.")
     @Test
     void printLottoNumbers(){
@@ -35,7 +40,8 @@ class LottoTest {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
-        new Lotto(List.of(1,2,3,4,5,6)).printLottoNumbers();
+//        new Lotto(List.of(1,2,3,4,5,6)).printLottoNumbers();
+        lotto.printLottoNumbers();
         String result = outputStream.toString();
         String expected = "[1, 2, 3, 4, 5, 6]" + System.lineSeparator();
         assertThat(result).isEqualTo(expected);
@@ -46,12 +52,19 @@ class LottoTest {
     @DisplayName("당첨번호와 비교하여 일치하는 숫자의 개수를 반환한다.")
     @Test
     void compareWithTheCorrectAnswerTest(){
-        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
         List<Integer> answer = List.of(1,2,3,4,5,6);
-
         //3개일치
         assertThat(new Lotto(List.of(4,5,6,7,8,9)).compareWithTheCorrectAnswer(answer)).isEqualTo(3);
         //4개일치
         assertThat(new Lotto(List.of(3,4,5,6,7,8)).compareWithTheCorrectAnswer(answer)).isEqualTo(4);
+    }
+
+    @DisplayName("보너스 번호가 일치하는지 확인한다.")
+    @Test
+    void compareWithBonusNumber(){
+        //일치
+        assertThat(lotto.compareWithBonusNumber(1)).isTrue();
+        //불일치
+        assertThat(lotto.compareWithBonusNumber(7)).isFalse();
     }
 }

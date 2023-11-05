03008261c2072247dd9e224f78be7113dc1687d5
0O000 <zkzk_001@naver.com>
2023-11-05 22:52:42 +0900

    feat: 로또 번호 오름차순

--- src/main/java/lotto/Model/LottoRandom.java
+++ src/main/java/lotto/Model/LottoRandom.java
@@ -3,13 +3,17 @@ package lotto.Model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoRandom {
+
     public List getBuyLotto(int k) {
         List<List<Integer>> numbers = new ArrayList<>();
         for (int i = 0; i < k; i++) {
             numbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(numbers.get(i));
+            System.out.println(numbers.get(i));
         };
         return numbers;
     }

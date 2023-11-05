f43fb51ab0ebc187740145e8f04b27b18e96b1cb
Oseuseu <lakingsung@gmail.com>
2023-11-06 05:00:35 +0900

    feat: `LottoAnswer` 클래스 `getNumbers` 메서드 추가
    커밋 포함 내용
    1. `LottoAnswer` 클래스를 레코드 클래스로 리팩터링
    2. `LottoAnswer::getNumbers` 메서드 추가

--- src/main/java/lotto/domain/dto/LottoAnswer.java
+++ src/main/java/lotto/domain/dto/LottoAnswer.java
@@ -1,11 +1,9 @@
 package lotto.domain.dto;
 
-public class LottoAnswer {
-    private final LottoNumbers lotto;
-    private final int bonus;
+import java.util.List;
 
-    public LottoAnswer(LottoNumbers lotto, int bonus) {
-        this.lotto = lotto;
-        this.bonus = bonus;
+public record LottoAnswer(LottoNumbers lotto, int bonus) {
+    public List<Integer> getNumbers() {
+        return lotto.numbers();
     }
 }

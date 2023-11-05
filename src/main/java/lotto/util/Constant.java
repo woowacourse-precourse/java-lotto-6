91f653d37ff3e66ef00a2b3bf6dc37faadd8d6b5
GO-TE <alsrudxo9810@naver.com>
2023-11-06 00:46:30 +0900

    feat(Constant): 상수 클래스 추가 - 로또 게임에서 사용하는 상수 추가

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+public class Constant {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_MINIMUM_NUMBER = 1;
+    public static final int LOTTO_MAXIMUM_NUMBER = 45;
+    public static final int LOTTO_WINNING_NUMBER_LENGTH = 6;
+    public static final int ZERO = 0;
+
+}

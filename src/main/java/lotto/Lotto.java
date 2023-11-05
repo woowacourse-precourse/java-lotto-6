39d1442d6460d4da13fd869ece4ff625e3418211
minoooo119 <minoth0119@naver.com>
2023-11-05 20:13:33 +0900

    [FIX] 숫자 range 확인 함수 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,17 +16,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-        if(!isRightRangeNum(numbers)){
+        if(isOutOfRangeNum(numbers)){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
-    private boolean isRightRangeNum(List<Integer> numbers){
+    private boolean isOutOfRangeNum(List<Integer> numbers){
         for(Integer num:numbers){
             if(num>45||num<1){
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     // TODO: 추가 기능 구현

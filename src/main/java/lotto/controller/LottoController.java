c6424ec0e0f7007954e747137522810c96f2ba44
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-06 02:32:36 +0900

    feat: add empty lotto controller

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,15 @@
+package lotto.controller;
+
+public class LottoController {
+    public void buyLotto(){
+
+    }
+
+    public void setWinningNumbers(){
+
+    }
+
+    public void getWinningStatistic(){
+
+    }
+}

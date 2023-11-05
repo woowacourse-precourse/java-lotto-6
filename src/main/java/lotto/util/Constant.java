a92ba7fdd2e67216b73dc7cfc35decea2c8c09fc
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-11-05 18:50:11 +0900

    FEAT(Constant) / create

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,16 @@
+package lotto.util;
+
+public class Constant {
+    public static final int MIN_LOTTO_VALUE = 1;
+    public static final int MAX_LOTTO_VALUE = 45;
+
+    public static final String SPLIT_UNIT = ",";
+    public static final String PURCHASE_NOTICE = "%s개를 구매했습니다.";
+    public static final String LOTTO_NOTICE = "[%d, %d, %d, %d, %d, %d]";
+    public static final String LOTTO_3_MATCH = "3개 일치 (5,000원) - %d개";
+    public static final String LOTTO_4_MATCH = "4개 일치 (50,000원) - %d개";
+    public static final String LOTTO_5_MATCH = "5개 일치 (1,500,000원) - %d개";
+    public static final String LOTTO_5_BONUS_MATCH = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String LOTTO_6_MATCH = "6개 일치 (2,000,000,000원) - %d개";
+    public static final String PROFIT_NOTICE = "총 수익률은 %f입니다.";
+}

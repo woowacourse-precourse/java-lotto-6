11fddc2160db26b786910e939006d142c7ca092f
Maljaaa <Maljaaa@github.com>
2023-11-06 01:56:19 +0900

    refactor(PurchaseAmounts): getter 추가
    - 구입 금액과 구입 금액에 따른 로또 장 수에 대한 getter 생성

--- src/main/java/lotto/domain/PurchaseAmounts.java
+++ src/main/java/lotto/domain/PurchaseAmounts.java
@@ -6,15 +6,25 @@ public class PurchaseAmounts {
     private final int LOTTO_PRICE = 1000;
 
     private int price;
+    private int count;
 
     public PurchaseAmounts(int price) {
         validatePurchaseAmounts(price);
         this.price = price;
+        this.count = price / LOTTO_PRICE;
     }
 
     private void validatePurchaseAmounts(int price) {
         if (price % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(PRICE_ERROR.getMessage());
         }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCount() {
+        return count;
     }
 }

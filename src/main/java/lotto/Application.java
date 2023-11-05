0c2260a0897e72e2ddd4282b4b0d0706a2f2f05c
706com <ehdgusjdbswl@gmail.com>
2023-11-06 05:15:47 +0900

    feat : 컨트롤러 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,10 +2,16 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.BuyingLottoController;
+import lotto.controller.WinningNumberController;
 
 public class Application {
     public static void main(String[] args) {
         BuyingLottoController buyingController = new BuyingLottoController();
+        WinningNumberController winningController = new WinningNumberController();
+
+        buyingController.buyLotto();
+        winningController.drawWinningNumber();
+
         Console.close();
     }
 }

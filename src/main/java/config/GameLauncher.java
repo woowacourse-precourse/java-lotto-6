091e5a044bc8962d1d3c3b9c4776cb905a7f36c7
song-jae-wan <sjo9810@gmail.com>
2023-11-05 16:53:31 +0900

    feat: 프로그램 실행 기능 구현

--- src/main/java/config/GameLauncher.java
+++ src/main/java/config/GameLauncher.java
@@ -1,4 +1,16 @@
 package config;
 
+import view.InputView;
+
 public class GameLauncher {
+
+    public static void run() {
+        GameConfig config = GameConfig.getInstance();
+        InputView inputView = config.inputView();
+
+        inputView.inputPayment();
+        inputView.inputWinNumber();
+        inputView.inputBonusNumber();
+    }
+
 }

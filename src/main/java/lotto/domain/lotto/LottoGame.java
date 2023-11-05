ab56c9af3cc116775866b014f8f54957bda27601
penrose15 <seongju99@naver.com>
2023-11-05 23:29:25 +0900

    Feat: Lotto게임 진행하는 LottoGame생성

---
+++ src/main/java/lotto/domain/lotto/LottoGame.java
@@ -0,0 +1,54 @@
+package lotto.domain.lotto;
+
+import lotto.global.constant.BonusNumberMatch;
+import lotto.global.constant.LottoRank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoGame {
+    private Lottos lottos;
+    private WinningNumbers winningNumbers;
+
+    public LottoGame(Lottos lottos, WinningNumbers winningNumbers) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+
+    }
+
+    public void generateWinningNumbers(String winningNormalNumber, String bonusNumber) {
+        generateWinningNormalNumbers(winningNormalNumber);
+        generateBonusNumber(bonusNumber);
+    }
+
+    private void generateWinningNormalNumbers(String winningNormalNumber) {
+        winningNumbers.generateWinningNormalNumbers(winningNormalNumber);
+    }
+
+    private void generateBonusNumber(String bonusNumber) {
+        winningNumbers.generateBonusNumber(bonusNumber);
+    }
+
+    public Map<LottoRank, Integer> calculateLottoResults() {
+        Map<LottoRank, Integer> rankMap = new HashMap<>();
+
+        List<Lotto> lottos = this.lottos.getLottos();
+        for (Lotto lotto : lottos) {
+            int matchNumber = winningNumbers.compareWinningNormalNumberWithLotto(lotto);
+            BonusNumberMatch bonusNumberMatch = winningNumbers.compareBonusNumberWithLotto(lotto, matchNumber);
+
+            LottoRank rank = LottoRank.getRank(matchNumber, bonusNumberMatch);
+            calculateRankMap(rankMap, rank);
+        }
+        return rankMap;
+    }
+
+    private void calculateRankMap(Map<LottoRank, Integer> rankMap, LottoRank rank) {
+        int rankCount = rankMap.getOrDefault(rank, 0);
+        rankCount += 1;
+        rankMap.put(rank, rankCount);
+    }
+
+
+}

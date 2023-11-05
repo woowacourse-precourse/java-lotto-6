57adb145393f6f5173138fb9220ed1cadd22f61e
ykmxxi <ykmxxi97@gmail.com>
2023-11-05 16:39:28 +0900

    feat(InputValidator): 입력 값이 빈 문자열이나 공백만 입력한 경우 예외 처리 기능 구현

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class InputValidator {
 
     private static final String NULL_ERROR_MESSAGE = "[ERROR] 입력 값이 null 입니다.";
+    private static final String BLANK_ERROR_MESSAGE = "[ERROR] 값을 입력해 주세요.";
 
     private InputValidator() {
         throw new IllegalStateException("[ERROR] 해당 클래스는 생성자를 호출할 수 없습니다.");
@@ -12,6 +13,7 @@ public class InputValidator {
 
     public static void validate(final String input) {
         validateNull(input);
+        validateEmptyOrWhitespaces(input);
     }
 
     private static void validateNull(final String input) {
@@ -19,5 +21,12 @@ public class InputValidator {
             throw new IllegalArgumentException(NULL_ERROR_MESSAGE);
         }
     }
+
+    private static void validateEmptyOrWhitespaces(final String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
+        }
+    }
+
 
 }
--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
@@ -19,6 +20,17 @@ class InputValidatorTest {
                 .isThrownBy(() -> InputValidator.validate(input))
                 .withMessageStartingWith(error)
                 .withMessageContaining("입력 값이 null 입니다.");
+    }
+
+    @ParameterizedTest(name = "입력값 : {0}")
+    @ValueSource(strings = {"", "  "})
+    @DisplayName("입력 공통 예외 처리: 빈 문자열인 경우")
+    void givenEmptyAndWhitespaces_whenValidate_thenThrowException(String input) {
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputValidator.validate(input))
+                .withMessageStartingWith(error)
+                .withMessageContaining("값을 입력해 주세요.");
     }
 
 }

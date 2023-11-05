dd628be2ac59bd4100714deffede45812a36cbbc
bellring <starringbell@gmail.com>
2023-11-05 17:20:33 +0900

    feat(InputMoney): 사용자가 0을 입력했을시 예외 발생

--- src/main/java/lotto/InputMoney.java
+++ src/main/java/lotto/InputMoney.java
@@ -1,32 +1,23 @@
 package lotto;
 
+import lotto.enums.PriceInputError;
+
 public class InputMoney {
 	private final Integer money;
 
-	public InputMoney(String userInput) {
-		validateInputIsNumber(userInput);
-		Integer inputMoney = convertStringToInteger(userInput);
-		validateInputUnit(inputMoney);
-		this.money = inputMoney;
+	public InputMoney(Integer userInput) {
+		validateInputUnit(userInput);
+		this.money = userInput;
 	}
 
 	private void validateInputUnit(Integer money) {
-		if (money % 1000 != 0) {
+		if (money == 0 || money % 1000 != 0) {
 			throw new IllegalArgumentException(PriceInputError.INVALID_UNIT_INPUT.getErrorDescription());
 		}
-	}
-
-	private void validateInputIsNumber(String input) {
-		if (!Character.isDigit(input.charAt(0))) {
-			throw new IllegalArgumentException(PriceInputError.INVALID_TYPE_INPUT.getErrorDescription());
-		}
-	}
-
-	private Integer convertStringToInteger(String userInput) {
-		return Integer.parseInt(userInput);
 	}
 
 	public Integer getMoney(){
 		return this.money;
 	}
+
 }

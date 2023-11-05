7d15bc70874aa30dff07d9d5fb0a0a5fccdf5688
Yang-soeun <soeun8636@naver.com>
2023-11-06 00:56:46 +0900

    [feat]: 통계결과, 수익률을 가지는 Result 클래스 생성

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.constant.PrizeConstant;
+
+public class Result {
+    private final static int PERCENT = 10;
+    private final List<Integer> statistics;
+
+    public Result(WinningLotto winningLotto, MyLotto myLotto) {
+        this.statistics = myLotto.getStatistics(winningLotto);
+    }
+
+    public List<Integer> getStatistics() {
+        return statistics;
+    }
+
+    public float getProfitability(int quantity) {
+        float totalReward = getTotalReward();
+        return totalReward / (quantity * PERCENT);
+    }
+
+    private float getTotalReward() {
+        int totalReward = 0;
+        for (int i = 0; i < statistics.size(); i++) {
+            int reward = PrizeConstant.getRewardByScore(i);
+            int count = statistics.get(i);
+            totalReward += (reward * count);
+        }
+        return (float) totalReward;
+    }
+
+}

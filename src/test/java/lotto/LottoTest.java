1db575fdf1ffa0df5bd0a17de1c5f73e698a2a0e
reddevilmidzy <midzy0228@gmail.com>
2023-11-05 12:47:28 +0900

    chore: 패키지 이동한 테스트 파일 원상복구

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
-package lotto.model;
+package lotto;
 
+import lotto.model.Bonus;
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;

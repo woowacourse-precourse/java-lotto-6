7d261c93050bf4fec388e0a9b4dbc05b687f90d1
ay-eonii <naayen@naver.com>
2023-11-06 00:24:51 +0900

    feat(view): 구입금액 입력 메시지 출력

---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class View {
+    public void output(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/lotto/view/constant/Message.java
@@ -0,0 +1,5 @@
+package lotto.view.constant;
+
+public class Message {
+    public final static String INPUT_COST = "구입금액을 입력해 주세요.";
+}

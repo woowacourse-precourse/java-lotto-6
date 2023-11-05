d7319dc38d852807c2d39931a6feb5ffa9eb5ec2
Woongbae Yun <devbelly@naver.com>
2023-11-05 15:54:00 +0900

    feat : 로또 번호가 중복되면 IllegalArgumentException 예외 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,16 +3,27 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final Integer LOTTO_SIZE = 6;
+    private static final String NOT_SIX_NUMBERS_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되선 안됩니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(NOT_SIX_NUMBERS_ERROR_MESSAGE);
         }
     }
 }

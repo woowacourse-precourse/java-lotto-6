e346a1a6688897e659093079fedf6e8e20da68e0
jaegyeongkim <dongji_11@naver.com>
2023-11-06 00:57:56 +0900

    feat: 로또 번호에 대한 검증 기능 구현

---
+++ src/main/java/utils/numberValidation.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class numberValidation {
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+
+    public void isOverMaxNumber(int number) {
+        if (number > MAX_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isUnderMinNumber(int number) {
+        if (number < MIN_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

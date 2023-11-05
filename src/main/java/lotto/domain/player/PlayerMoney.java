2981b852ce1384aa6576da0def8e9228a3014bf2
le2sky <leehaneul990623@gmail.com>
2023-11-06 00:12:02 +0900

    refactor : 수익률 계산 기능 BigDecimal 사용하도록 개선

--- src/main/java/lotto/domain/player/PlayerMoney.java
+++ src/main/java/lotto/domain/player/PlayerMoney.java
@@ -2,10 +2,14 @@ package lotto.domain.player;
 
 import lotto.domain.common.Money;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Objects;
 
 class PlayerMoney {
 
+    private static final int PERCENTAGE_MULTIPLIER = 100;
+    private static final int ROUND_SCALE = 3;
     private static final String UNKNOWN_MONEY_MESSAGE = "알 수 없는 금액입니다.";
 
     private final Money money;
@@ -29,6 +33,12 @@ class PlayerMoney {
     public double calculateEarningRate(Money totalReturn) {
         checkMoneyNonNull(totalReturn);
 
-        return totalReturn.divide(money) * 100;
+        return calculateEarningRate(BigDecimal.valueOf(money.getValue()), BigDecimal.valueOf(totalReturn.getValue()));
+    }
+
+    private double calculateEarningRate(BigDecimal initialValue, BigDecimal totalValue) {
+        return totalValue.divide(initialValue, ROUND_SCALE, RoundingMode.HALF_UP)
+                .multiply(BigDecimal.valueOf(PERCENTAGE_MULTIPLIER))
+                .doubleValue();
     }
 }

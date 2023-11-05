45825f64c6fe8cf758724216a9bdd00172c05915
Isaac Han <isaachan233@gmail.com>
2023-11-05 20:39:46 +0900

    feat: 랭크 이넘 생성

---
+++ src/main/java/lotto/enums/Rank.java
@@ -0,0 +1,25 @@
+package lotto.enums;
+
+public enum Rank {
+    FIRST(6,false),
+    SECOND(5,true),
+    THIRD(5,false),
+    FOURTH(4,false),
+    FIFTH(3,false);
+
+    private int matchCount;
+    private boolean containBonus;
+
+    Rank(int matchCount, boolean containBonus) {
+        this.matchCount = matchCount;
+        this.containBonus = containBonus;
+    }
+
+    public int getMatchedCount() {
+        return this.matchCount;
+    }
+
+    public boolean containsBonus() {
+        return this.containBonus;
+    }
+}

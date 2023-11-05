561965d55ec440c9752981460e0b21cb2d56cde6
wochae <officialwochae@gmail.com>
2023-11-05 22:20:32 +0900

    feat(view) : moneyInputView features at least one
    최소한 한 장의 발급이 가능한 금액이어야 한다.

--- src/main/java/lotto/MoneyInputView.java
+++ src/main/java/lotto/MoneyInputView.java
@@ -6,6 +6,7 @@ public class MoneyInputView {
 
     private String input;
     private int money;
+    private static final int ONE_ISSUE_LOTTO = 1000;
 
     public MoneyInputView() {
         validate();
@@ -15,6 +16,7 @@ public class MoneyInputView {
         input = Console.readLine();
         try {
             nonZero();
+            leastOneIssue();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             validate();
@@ -27,5 +29,11 @@ public class MoneyInputView {
             throw new IllegalArgumentException("[ERROR] 0 이 아닌 숫자를 입력해야 합니다.");
         }
         money = Integer.parseInt(input);
+    }
+
+    private void leastOneIssue() {
+        if (ONE_ISSUE_LOTTO > money) {
+            throw new IllegalArgumentException("[ERROR] 최소한 1000원 이상이어야 합니다.");
+        }
     }
 }

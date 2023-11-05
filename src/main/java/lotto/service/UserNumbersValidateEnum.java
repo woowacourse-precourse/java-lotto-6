7455f1dfb571dff077dae94dd1d9e34eb7225340
Heejun-0219 <heejun0219@gmail.com>
2023-11-05 22:46:05 +0900

    fix: 에러 메세지 추가

--- src/main/java/lotto/service/UserNumbersValidateEnum.java
+++ src/main/java/lotto/service/UserNumbersValidateEnum.java
@@ -5,7 +5,8 @@ public enum UserNumbersValidateEnum {
     PRE_NUMBERS_DELIMITER_ERROR("[ERROR] 구분자와 숫자만 입력 가능합니다."),
     LOTTO_NUMBERS_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     LOTTO_NUMBERS_DUPLICATE_ERROR("[ERROR] 로또 번호는 중복이 불가능합니다."),
-    LOTTO_NUMBERS_SIZE_ERROR("[ERROR] 로또 번호는 6개만 가능합니다.");
+    LOTTO_NUMBERS_SIZE_ERROR("[ERROR] 로또 번호는 6개만 가능합니다."),
+    DUPLICATE_NUMBERS_BONUS_ERROR("[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
 
     private String message;
 

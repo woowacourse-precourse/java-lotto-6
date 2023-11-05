195f1bc657ffe7381d2a2c1a23c6366de525c008
siwol025 <cyk99115695@gmail.com>
2023-11-06 01:54:54 +0900

    test(Prizes): 상금 수량을 증가시키는 기능 테스트

--- src/test/java/lotto/domain/PrizesTest.java
+++ src/test/java/lotto/domain/PrizesTest.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PrizesTest {
@@ -12,8 +15,18 @@ class PrizesTest {
     void setUp() {
         prizes = Prizes.createPrizes();
     }
+
+    @DisplayName("상금 수량을 증가시키는 기능 테스트")
     @Test
     void increasePrizeAmount() {
+        Prize prize = Prize.FIFTH_PLACE;
+        int expectedAmount = 2;
+
+        prizes.increasePrizeAmount(prize);
+        prizes.increasePrizeAmount(prize);
+        int prizeAmount = prizes.getPrizeAmount(prize);
+
+        assertThat(prizeAmount).isEqualTo(expectedAmount);
     }
 
     @Test

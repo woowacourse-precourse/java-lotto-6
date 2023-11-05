4a9cb47bfff0c0d99eb78e0ff18ea126691c6984
노정완 <jeongwan.noh@gmail.com>
2023-11-05 17:35:41 +0900

    feat: 로또 게임 컨트롤러 로직 초기 구현

--- src/main/java/lotto/controller/LotteryGameController.java
+++ src/main/java/lotto/controller/LotteryGameController.java
@@ -1,7 +1,31 @@
 package lotto.controller;
 
-public class LotteryGameController {
-    public void run() {
+import lotto.domain.lottery.LottoNumberGenerator;
+import lotto.view.OutputView;
+import lotto.view.InputView;
 
+import static lotto.domain.constants.LottoConstraint.LOTTO_PRICE;
+
+public class LotteryGameController {
+
+    private final InputView inputView = new InputView();
+    private final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+
+    private int lottoAmount;
+
+    public void run() {
+        OutputView.requestPurchaseAmountMessage();
+        int purchaseAmount = amountOfLottos();
+
+        purchaseLottos(purchaseAmount);
+        OutputView.returnLottery(lottoAmount);
+
+        OutputView.requestWinningNumberMessage();
+
+        OutputView.requestBonusNumberMessage();
+
+        OutputView.returnWinningStats();
     }
+
+
 }

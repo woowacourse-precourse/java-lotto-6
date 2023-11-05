99a236e5febf36d18b70443f7e21cd2168868423
ohjuyoungg <ejrm12@naver.com>
2023-11-05 16:35:38 +0900

    feat: 사용자가 당첨 번호를 잘못된 값으로 입력했을 경우 예외 처리 메소드 구현

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -1,28 +1,74 @@
 package lotto.validation;
 
-import lotto.utils.constants.Comment;
+import java.util.Arrays;
+import java.util.List;
 import lotto.utils.constants.ExceptionMessage;
 
 public class InputValidation {
 
-    public static String validatePurchaseAmountIsBlank(String purchaseAmount) {
+    private static final String COMMA = ",";
+
+    public static void validateIsBlank(String purchaseAmount) {
         if (purchaseAmount == null || purchaseAmount.isBlank()) {
-            throw new IllegalArgumentException(String.valueOf(ExceptionMessage.PURCHASE_AMOUNT_BLANK.getMessage()));
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_NUMBER_BLANK.getMessage());
         }
-        return purchaseAmount;
     }
 
     public static int validateInputPurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < 1000 || purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException(String.valueOf(ExceptionMessage.PURCHASE_AMOUNT_NOT_THOUSAND.getMessage()));
+            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_NOT_THOUSAND.getMessage());
         }
         return purchaseAmount;
     }
 
     public static String validateInputString(String purchaseAmount) {
         if (!purchaseAmount.matches("\\d+")) {
-            throw new IllegalArgumentException(String.valueOf(ExceptionMessage.PURCHASE_AMOUNT_NOT_NUMBER.getMessage()));
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_STRING_NOT_NUMBER.getMessage());
         }
         return purchaseAmount;
+    }
+
+    public static List<Integer> validateInputWinningNumber(String winningNum) {
+        List<String> numberStrings = Arrays.asList(winningNum.split(COMMA));
+        validateMinimumSize(numberStrings);
+        List<Integer> numberList = parseToIntegers(numberStrings);
+        validateNoDuplicates(numberList);
+        validateNumberRange(numberList);
+        validateSixNumbers(numberList);
+        return numberList;
+    }
+
+    public static void validateMinimumSize(List<String> numberStrings) {
+        if (numberStrings.size() < 6) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNING_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    public static List<Integer> parseToIntegers(List<String> numberStrings) {
+        return numberStrings.stream()
+            .map(Integer::parseInt)
+            .toList();
+    }
+
+    public static void validateNoDuplicates(List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNING_LOTTO_DUPULICATED_NUMBER.getMessage());
+        }
+    }
+
+    public static boolean hasDuplicate(List<Integer> numbers) {
+        return numbers.stream().distinct().count() < numbers.size();
+    }
+
+    public static void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNIG_LOTTO_INCLUDED_VALUE_NUMBER.getMessage());
+        }
+    }
+
+    public static void validateSixNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNIG_LOTTO_SIX_VALUE_NUMBER.getMessage());
+        }
     }
 }

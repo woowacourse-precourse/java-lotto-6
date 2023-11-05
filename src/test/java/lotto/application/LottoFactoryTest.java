5b3289b2fe69d3bc01c45ac748126f146595ac9e
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 01:19:03 +0900

    test: 자동으로 로또를 생성하는지 테스트

---
+++ src/test/java/lotto/application/LottoFactoryTest.java
@@ -0,0 +1,19 @@
+package lotto.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.Test;
+
+class LottoFactoryTest {
+    private final LottoFactory lottoFactory = new LottoFactory();
+
+    @Test
+    void 자동_로또를_생성한다() {
+        // when
+        Lotto lotto = lottoFactory.createLottoByAuto();
+
+        // then
+        assertNotNull(lotto);
+    }
+}

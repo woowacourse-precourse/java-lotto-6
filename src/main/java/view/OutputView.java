860f8ee9e5230648b1467ef295bbbed2ddc31ab7
slowcloud <sju9417@gmail.com>
2023-11-05 11:36:00 +0900

    feat: OutputView에서 구입 금액 입력 메시지를 출력하는 기능과 예외 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,8 +11,16 @@ public class OutputView {
         this.printer = printer;
     }
 
+    public void printGetMoney() {
+        printer.println("구입금액을 입력해 주세요.");
+    }
+
     public void printLottoPack(LottoPack lottoPack) {
         printer.println(lottoPack.toString());
+    }
+
+    public void printException(Exception exception) {
+        printer.println(exception.getMessage());
     }
 
 }

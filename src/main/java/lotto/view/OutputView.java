9b5f8286ea81cbaaeb3d5cca49f682e7b743ee2f
JongMinCh0i <wonderland314592@gmail.com>
2023-11-05 19:32:28 +0900

    fix(OutputView): static final 키워드 순서 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,9 +6,9 @@ import lotto.domain.Lotto;
 
 public class OutputView {
 
-    private final static String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
-    private final static String WINNING_STATISTICS = "당첨 통계";
-    private final static String LOTTO_RESULT = """
+    private static final String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
+    private static final String WINNING_STATISTICS = "당첨 통계";
+    private static final String LOTTO_RESULT = """
             ---
             3개 일치 (5,000원) - %d개
             4개 일치 (50,000원) - %d개

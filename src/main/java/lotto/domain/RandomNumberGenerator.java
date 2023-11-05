a31269f479aeee064693f169be6691269123b433
linirini <2001yerin@naver.com>
2023-11-06 03:39:38 +0900

    feat : implement RandomNumbers generator sort

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -2,12 +2,18 @@ package lotto.domain;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator{
 
     public static List<Integer> createUniqueRandomNumbers(){
-        return pickUniqueNumbersInRange(1,45,6);
+        return sortRandomNumbers(pickUniqueNumbersInRange(1,45,6));
+    }
+
+    private static List<Integer> sortRandomNumbers(List<Integer> randomNumbers){
+        Collections.sort(randomNumbers);
+        return randomNumbers;
     }
 
 }

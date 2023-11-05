becddc6b7119622d9998c4228f7d5a9423f47cb1
sujnkim <blackout3210@naver.com>
2023-11-05 22:02:28 +0900

    feat: 로또 개수 만큼의 로또 번호를 생성하는 generator 추가

--- src/main/java/lotto/utils/Generator.java
+++ src/main/java/lotto/utils/Generator.java
@@ -1,7 +1,9 @@
 package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Generator {
@@ -11,5 +13,13 @@ public class Generator {
 
     public static List<Integer> generateRandomNumber(){
         return Randoms.pickUniqueNumbersInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER, RANDOM_NUMBER_AMOUNT);
+    }
+
+    public static List<Lotto> generateLottoByAmount(int amount){
+        List<Lotto> list = new ArrayList<>();
+        for(int cnt=0; cnt<amount;cnt++){
+            list.add(new Lotto(Generator.generateRandomNumber()));
+        }
+        return list;
     }
 }

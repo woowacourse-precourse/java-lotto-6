9ae98af6260654924f29f78afac6091c43f2f145
doha <dohaahi@naver.com>
2023-11-06 03:47:47 +0900

    feat (Application): 로또 게임 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoMachine lottoMachine = new LottoMachine();
+        LottoController lottoController = new LottoController(inputView, outputView, lottoMachine);
+
+        try {
+            lottoController.start();
+        } catch (Exception e) {
+            System.out.printf("[ERROR]: %s", e.getMessage());
+        }
     }
 }

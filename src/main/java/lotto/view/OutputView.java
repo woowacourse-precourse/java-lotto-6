adbcfa3ebfb1c3ca4140eebfb5e55108c66571db
IE-MangChi <hsm983278@gmail.com>
2023-11-05 21:50:21 +0900

    feat: 당첨 통계 출력 기능 추가
    - outputWinningResult: 당첨 통계 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.dto.PurchaseResult;
+import lotto.dto.WinningResult;
 import lotto.model.Lotto;
 
 public class OutputView {
@@ -10,5 +11,21 @@ public class OutputView {
         for (Lotto lotto : result.lottos()) {
             System.out.println(lotto);
         }
+        System.out.println();
+    }
+
+    public void outputWinningResult(WinningResult result) {
+        System.out.print("당첨 통계\n---\n");
+
+        System.out.printf(
+                "3개 일치 (5,000원) - %d개\n"
+                + "4개 일치 (50,000원) - %d개\n"
+                + "5개 일치 (1,500,000원) - %d개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
+                + "6개 일치 (2,000,000,000원) - %d개\n"
+                , result.rank()[5], result.rank()[4],
+                result.rank()[3], result.rank()[2], result.rank()[1]
+        );
+        System.out.printf("총 수익률은 %.1f%%입니다.", result.rateOfReturn());
     }
 }

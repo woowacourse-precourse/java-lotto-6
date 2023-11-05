b56c8c24375b7b5b3a3c8b31e990b227f399671d
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-05 19:08:43 +0900

    feat(lotto): 구입 금액이 1000원보다 작은경우 예외처리

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -8,6 +8,7 @@ public class Money {
 
     public Money(String amount) {
         validateFormat(amount);
+        validateMinAmount(amount);
         validateNumber(amount);
         this.amount = amount;
     }
@@ -15,6 +16,12 @@ public class Money {
     private void validateFormat(String amount) {
         if (!amount.matches("[0-9]+")){
             throw new IllegalArgumentException("[ERROR] 입력 형식에 맞지 않습니다.");
+        }
+    }
+
+    private void validateMinAmount(String amount) {
+        if (Integer.parseInt(amount) < 1000){
+            throw new IllegalArgumentException("[ERROR] 최소 금액보다 작습니다.");
         }
     }
 

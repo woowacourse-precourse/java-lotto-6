815db2c309da4d943fa759887363fc42e8d816fd
nohheeyeon <heeyeon1213@gmail.com>
2023-11-05 14:21:45 +0900

    feat : WinningNumbers클래스 생성자 구현

--- src/main/java/domain/WinningNumbers.java
+++ src/main/java/domain/WinningNumbers.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class WinningNumbers {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningNumbers(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
 }

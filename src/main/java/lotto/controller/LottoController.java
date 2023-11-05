5dd7932616b8e74502dc55f1fa3347f5768f41c5
cherBlair <cher.blair.h@gmail.com>
2023-11-05 14:05:52 +0900

    feat: 로또 번호 생성 및 검증 코드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,11 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
 import lotto.model.Money;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -10,11 +16,26 @@ public class LottoController {
     OutputView outputView = new OutputView();
     Money money = new Money();
 
+    private final Set<Lotto> lottos = new HashSet<>();
+
     public void buyLotto() {
         String input = inputView.showInputMoneyMessage();
         money.inputMoney(input);
         int quantity = money.countQuantity();
         outputView.showQuantityMessage(quantity);
+        generateLotto(quantity);
+    }
 
+    public void generateLotto(int quantityOfLotto) {
+        for (int i = 0; i < quantityOfLotto; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+            showGeneratedLottos(numbers);
+        }
+    }
+
+    private void showGeneratedLottos(List<Integer> LottoNumbers) {
+        outputView.showLotto(LottoNumbers.stream().sorted().collect(Collectors.toList()));
     }
 }
--- src/main/java/lotto/util/Message.java
+++ src/main/java/lotto/util/Message.java
@@ -6,4 +6,6 @@ public class Message {
     public final static String EXCPTION_NOT_A_NUM = prefixMessage + "로또 번호는 숫자만 입력 가능합니다.";
     public static final String EXCPTION_LESS_THAN_THOUSNAD = prefixMessage + "1,000 이상의 값을 입력해주세요.";
     public static final String EXCPTION_DIVIDE_INTO_THOUSNAD = prefixMessage + "입력 값은 1,000으로 나누어 떨어져야합니다.";
+    public static final String EXCPTION_NOT_A_SIX = prefixMessage + "로또 번호는 6개여야 합니다.";
+    public static final String EXCPTION_DUPLICATED = prefixMessage + "로또 번호는 중복기입될 수 없습니다.";
 }

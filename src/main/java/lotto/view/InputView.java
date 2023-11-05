c1502311c606a78086e65b607a53c0a4ced1c860
IE-MangChi <hsm983278@gmail.com>
2023-11-05 21:50:45 +0900

    fix: 입력받고 한줄 개행 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,16 +8,22 @@ public class InputView {
 
     public String inputMoney() {
         System.out.print("구입금액을 입력해 주세요.\n");
-        return inputValidate(Console.readLine());
+        String input = inputValidate(Console.readLine());
+        System.out.println();
+        return input;
     }
 
     public String inputWinningNumbers() {
         System.out.print("당첨 번호를 입력해 주세요.\n");
-        return inputValidate(Console.readLine());
+        String input = inputValidate(Console.readLine());
+        System.out.println();
+        return input;
     }
 
     public String inputBonusNumber() {
         System.out.print("보너스 번호를 입력해 주세요.\n");
-        return inputValidate(Console.readLine());
+        String input = inputValidate(Console.readLine());
+        System.out.println();
+        return input;
     }
 }

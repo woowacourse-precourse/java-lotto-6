9c679ced1e65e6908a61b475269131ca57cfc540
seongha <11pi885@gmail.com>
2023-11-05 22:12:33 +0900

    feat : 로또 등수와 상금을 관리하는 Enum 생성

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public enum Rank {
+
+    FIRST(6, "2,000,000,000"),
+    SECOND(5, "30,000,000"),
+    THIRD(5, "1,500,000"),
+    FOURTH(4, "50,000"),
+    FIFTH(3, "5,000");
+
+    private Integer ranking;
+    private String money;
+
+    Rank(Integer ranking, String money) {
+        this.ranking = ranking;
+        this.money = money;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+}

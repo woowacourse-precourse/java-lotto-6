dc77ea9f97e240074ea867e0e45c838ce025703a
dragon0622 <iamdragon.dev@gmail.com>
2023-11-05 19:51:14 +0900

    feat(Lotto) : money 메소드 추가

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,8 +1,11 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    public static void money(){
-
+    public static int money(){
+        int money = Integer.parseInt(Console.readLine());
+        //TODO: exception 추가
+        return money;
     }
 
     public static void winningNumbers(){

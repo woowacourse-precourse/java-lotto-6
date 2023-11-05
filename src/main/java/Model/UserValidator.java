c2bb0568a56be997492697cea6e33efb9ca58852
2021110806 <2021110806@dgu.ac.kr>
2023-11-05 19:14:53 +0900

    feat(UserValidator.java): add validateMoneyInRange function 사용자가 입력한 로또 구입 금액이 1,000원 이상 1,000,000,000원 이하인지 확인합니다.

--- src/main/java/Model/UserValidator.java
+++ src/main/java/Model/UserValidator.java
@@ -2,10 +2,18 @@ package Model;
 
 public class UserValidator {
 
+    private static final int MIN_MONEY_RANGE = 1000;
+    private static final int MAX_MONEY_RANGE = 1000000000;
     private static final int MONEY_MAX_LENGTH = 10;
 
     void validateMoneyLength(String money) {
         if (money.length() > MONEY_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    void validateMoneyInRange(int money) {
+        if (money >= MIN_MONEY_RANGE && money <= MAX_MONEY_RANGE) {
             throw new IllegalArgumentException();
         }
     }

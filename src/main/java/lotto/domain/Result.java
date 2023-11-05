e760493d39ccf73c9cc7143f05e96cff9b4e20ef
inkyu-yoon <tryingmybest24h@gmail.com>
2023-11-05 22:27:30 +0900

    chore: add private access modifier

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -5,18 +5,14 @@ import lotto.constants.WinningInfo;
 import java.util.Optional;
 
 public class Result {
-    int numberOfMatch;
-    boolean isBonusPrize;
-    Optional<WinningInfo> winningInfo;
+    private Optional<WinningInfo> winningInfo;
 
-    public Result(int numberOfMatch, boolean isBonusPrize, Optional<WinningInfo> winningInfo) {
-        this.numberOfMatch = numberOfMatch;
-        this.isBonusPrize = isBonusPrize;
+    public Result(Optional<WinningInfo> winningInfo) {
         this.winningInfo = winningInfo;
     }
 
     public static Result createResult(int numberOfMatch, boolean isBonusPrize) {
-        return new Result(numberOfMatch, isBonusPrize, WinningInfo.of(numberOfMatch, isBonusPrize));
+        return new Result(WinningInfo.of(numberOfMatch, isBonusPrize));
     }
 
     public Optional<WinningInfo> getWinningInfo() {

c20d7c31d4db27cbce99ec30b916d466a156b171
ljkhyeong <jolri24@gmail.com>
2023-11-05 21:55:43 +0900

    feat: Lotto 클래스 검증 로직 추가
    - 로또 번호가 1~45 사이의 숫자가 아닌 경우
    - 로또 번호가 중복된 경우

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+	private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_PRICE = 1000;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6 || areUnique(numbers) || areInRange(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
+    private boolean areUnique(List<Integer> numbers) {
+        return numbers.stream().distinct().count() == numbers.size();
+    }
+
+	private boolean areInRange(List<Integer> numbers) {
+		return numbers.stream().allMatch(num -> num >= MIN_LOTTO_NUMBER && num <= MAX_LOTTO_NUMBER);
+	}
 }

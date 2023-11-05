f4b9e1ce753dc8d1f82844edc8891d844fb1fdf3
hjhan <hjhan.0621@gmail.com>
2023-11-06 02:17:18 +0900

    test(LottosTest): 로또 구매비용을 입력받아 Lottos 객체 생성 테스트
    - 구매비용이
     ㄴ 정수가 아닌 숫자가 들어올 때
     ㄴ Null 또는 Empty 값이 들어올 때
     ㄴ 1000의 배수가 아닌 숫자가 들어올 때
     ㄴ 적절한 값이 들어올 때

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import lotto.errors.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottosTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"k", "1.2"})
+    @DisplayName("정수가 아닌 숫자가 들어올 때 예외처리")
+    void test_Lottos_1(String inputCost) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lottos(inputCost))
+                .withMessage(ErrorMessage.WRONG_NUMBER_FORMAT.getMessage());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("Null 또는 Empty 값이 들어올 때 예외처리")
+    void test_Lottos_2(String inputCost) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lottos(inputCost))
+                .withMessage(ErrorMessage.WRONG_NUMBER_FORMAT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2001"})
+    @DisplayName("1000의 배수가 아닌 숫자가 들어올 때 예외처리")
+    void test_Lottos_3(String inputCost) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lottos(inputCost))
+                .withMessage(ErrorMessage.WRONG_COST.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "21000", "512000"})
+    @DisplayName("적절한 값이 들어올 때 테스트 통과")
+    void test_Lottos_4(String inputCost) {
+        assertThatCode(() -> new Lottos(inputCost))
+                .doesNotThrowAnyException();
+    }
+}

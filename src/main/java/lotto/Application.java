561ddd678dc64f5980b56c174346b696ba818dc9
noah <jeongas9810@gmail.com>
2023-11-06 04:20:39 +0900

    feat: Define basic functions within the start method
    Basic functions have been defined. initialize in Application.main()

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,7 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        Play play = null;
-        play.start();
+        Play lottoPlay = new LottoPlay();
+        lottoPlay.start();
     }
 }
---
+++ src/main/java/lotto/LottoPlay.java
@@ -0,0 +1,39 @@
+package lotto;
+
+public class LottoPlay implements Play{
+
+    @Override
+    public void start() {
+        getPurchaseQuantity();
+        printMyLottoNumber();
+        readWinningNumber();
+        readBonusNumber();
+        printWinningStat();
+        printYieldRate();
+    }
+
+    private void getPurchaseQuantity() {
+
+    }
+
+    private void printMyLottoNumber() {
+
+    }
+
+    private void readWinningNumber() {
+
+    }
+
+    private void readBonusNumber() {
+
+    }
+
+    private void printWinningStat() {
+
+    }
+
+    private void printYieldRate() {
+
+    }
+
+}

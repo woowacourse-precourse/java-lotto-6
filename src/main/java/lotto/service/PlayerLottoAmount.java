dd8e6da5c9c115fcbb30efa04623dc9e6fc2e67c
LUKR7Q <ybkim0628@naver.com>
2023-11-05 19:59:27 +0900

    validateNumber(amount): 입력된 금액이 숫자인지 확인하고, 아니라면 에러 메시지를 출력하고 예외를 던집니다. validateAmount(amount): 금액의 유효성을 검사하는 메서드로, 자연수이고 로또 가격으로 나누어 떨어져야 합니다. 금액을 필드 amount에 할당합니다.

--- src/main/java/lotto/service/PlayerLottoAmount.java
+++ src/main/java/lotto/service/PlayerLottoAmount.java
@@ -1,7 +1,14 @@
 package lotto.service;
 
+import lotto.view.ExceptionMessage;
+
 public class PlayerLottoAmount {
     private static final int LOTTO_MIN_AMOUNT = 1000;
     private final int amount;
 
+    public PlayerLottoAmount(String amount) {
+        int amountNum = validateNumber(amount);
+        validateAmount(amountNum);
+        this.amount = amountNum;
+    }
 }

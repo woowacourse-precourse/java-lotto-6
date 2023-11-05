70c2dbe4ad3e2accc2f7f9b1395e8056f289b94b
강승민 <rkdtmdals999@naver.com>
2023-11-05 18:51:48 +0900

    [feat] 당첨 번호, 보너스 번호 문자열 상수 추가 및 출력 메소드 구현

--- src/main/java/lotto/utility/Constants.java
+++ src/main/java/lotto/utility/Constants.java
@@ -8,7 +8,8 @@ public class Constants {
     public static final String CLOSE_SQUARE_BRACKET = "]";
     public static final String BUY_MESSAGE_BEFORE = "구입금액을 입력해 주세요.";
     public static final String BUY_MESSAGE_AFTER = "개를 구매했습니다.";
-
+    public static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATISTIC = "당첨 통계";
     public static final String ERROR_LOTTO_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private Constants() {}
--- src/main/java/lotto/view/UserView.java
+++ src/main/java/lotto/view/UserView.java
@@ -3,6 +3,10 @@ package lotto.view;
 
 import static lotto.utility.Constants.BUY_MESSAGE_AFTER;
 import static lotto.utility.Constants.BUY_MESSAGE_BEFORE;
+import static lotto.utility.Constants.INPUT_BONUS_NUMBER_MESSAGE;
+import static lotto.utility.Constants.INPUT_WINNING_NUMBERS_MESSAGE;
+import static lotto.utility.Constants.LINE;
+import static lotto.utility.Constants.WINNING_STATISTIC;
 
 public class UserView {
     public UserView() {}
@@ -11,5 +15,16 @@ public class UserView {
     }
     public void printBuyMessageAfter() {
         System.out.println(BUY_MESSAGE_AFTER);
+    }
+    public void printInputWinningNumbersMessage() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+    }
+    public void printInputBonusNumberMessage() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+    public void printWinningStatistic() {
+        System.out.println(WINNING_STATISTIC);
+        System.out.println(LINE.repeat(3));
+        // 당첨 통계 메시지 출력 코드 구현 예정
     }
 }

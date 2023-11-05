f742e98c989d3d8d47bb4e25441531fdbe6876fe
ghdeo <goldencorn7@naver.com>
2023-11-05 22:07:00 +0900

    feat: Add read input methods

--- src/main/java/lotto/utils/Input.java
+++ src/main/java/lotto/utils/Input.java
@@ -1,8 +1,35 @@
 package lotto.utils;
 
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
+    public static int readPurchaseAmount() {
+        String userInput = readLine();
+        return Validate.purchaseAmount(userInput);
+    }
 
+    public static Lotto readLottery() {
+        String userInput = readLine();
+        String regex = ",";
+        String[] numberStrings = userInput.split(regex);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String numberString : numberStrings) {
+            int number = Validate.lotteryNumber(numberString);
+            numbers.add(number);
+        }
+
+        return new Lotto(numbers);
+    }
+
+    public static int readBonusNumber() {
+        String userInput = readLine();
+        return Validate.lotteryNumber(userInput);
+    }
 
 }

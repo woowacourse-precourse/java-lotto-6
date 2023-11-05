0d6fa48960177852e4f5766e27cd39ce5869a998
sunwooiljoon <wnsdl1659@naver.com>
2023-11-05 14:41:08 +0900

    refactor : 로또 구입 금액 입력 수정
    MoneyToBuy 클래스
     문자에 대한 예외사항은 추후에 분리하여 함

--- src/main/java/lotto/MoneyToBuy.java
+++ src/main/java/lotto/MoneyToBuy.java
@@ -4,21 +4,11 @@ public class MoneyToBuy {
     private final Integer value;
 
     public MoneyToBuy(Integer value) {
+        validate(value);
         this.value = value;
     }
 
-    public static MoneyToBuy stringToMoney(String string) {
-        Integer integer;
-        try {
-            integer = Integer.parseInt(string);
-            validate(integer);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-        return new MoneyToBuy(integer);
-    }
-
-    private static void validate(Integer value) {
+    private void validate(Integer value) {
         if (value <= 0) {
             throw new IllegalArgumentException();
         }

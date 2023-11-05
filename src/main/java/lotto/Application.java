673faf252a6ecce50ec714ac5905eb3497999c68
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-05 22:05:37 +0900

    feat: 구입금액을 요청하는 메서드에 예외처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,9 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     public int requestPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String stringPurchaseAmount = Console.readLine();
-        return Integer.parseInt(stringPurchaseAmount);
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String stringPurchaseAmount = Console.readLine();
+
+            try {
+                return Integer.parseInt(stringPurchaseAmount);
+            } catch (NumberFormatException e) {
+                System.err.println("[ERROR] 올바른 입력이 아닙니다. 구입금액을 입력해 주세요.");
+            }
+        }
     }
 
     public static void main(String[] args) {

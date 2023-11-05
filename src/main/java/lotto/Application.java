84d895fff190e05318631b3c60a4ff24fe06920b
Finn <wjstodsss@naver.com>
2023-11-05 19:39:57 +0900

    feat - '구입금액을 입력해 주세요.' 일반 메세지 출력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import static lotto.views.MessageManager.getPurchaseAmountPromptMessage;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        getPurchaseAmountPromptMessage();
     }
 }
--- src/main/java/lotto/views/MessageManager.java
+++ src/main/java/lotto/views/MessageManager.java
@@ -1,4 +1,11 @@
 package lotto.views;
 
+import java.util.ResourceBundle;
+
 public class MessageManager {
+    private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
+
+    public static void getPurchaseAmountPromptMessage() {
+        System.out.println(messages.getString("purchaseAmountPrompt.message"));
+    }
 }

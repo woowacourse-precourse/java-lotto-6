4181fb5bf5c0dc7564afbbc2708a194b873b2a91
권장순 <jsoonworld@gmail.com>
2023-11-06 06:16:20 +0900

    refactoring(Lotto) : Lotto#validate 에러 메세지 개선

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -14,15 +14,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 입력하신 로또 번호의 개수가 올바르지 않습니다. 정확히 6개의 숫자를 입력해야 합니다.");
         }
         for (Integer number : numbers) {
             if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("로또 번호는 1~45 사이여야 합니다.");
+                throw new IllegalArgumentException("[ERROR] 입력하신 로또 번호 중 올바르지 않은 범위의 숫자가 포함되어 있습니다. 로또 번호는 1부터 45 사이의 값이어야 합니다.");
             }
         }
         if (hasDuplicates(numbers)) {
-            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+            throw new IllegalArgumentException( "[ERROR] 로또 번호 중 중복이 포함되어 있습니다. 모든 로또 번호는 고유해야 합니다.");
         }
     }
 

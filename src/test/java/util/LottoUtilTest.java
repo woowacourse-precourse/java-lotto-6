00719c5631427f4f75f486856c450b8c38b9734d
song-jae-wan <sjo9810@gmail.com>
2023-11-05 23:10:17 +0900

    Test: 매입금액에 따른 로또 개수 반환 기능 테스트

--- src/test/java/util/LottoUtilTest.java
+++ src/test/java/util/LottoUtilTest.java
@@ -1,4 +1,22 @@
 package util;
 
+import java.util.Arrays;
+import java.util.List;
+
+import VO.UserLottoVO;
+import domain.Lotto;
+import domain.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static util.LottoUtil.*;
+
 public class LottoUtilTest {
+    @DisplayName("매입 금액에 따른 로또수 반환")
+    @Test
+    void countByPayment() {
+        final int pay = 5000;
+        assertThat(countLotto(pay)).isEqualTo(5);
+    }
 }

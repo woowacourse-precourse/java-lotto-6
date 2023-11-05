4e06003b8071405ef3973581bba86b590fd061d5
YongJOoOo <we072200@naver.com>
2023-11-05 16:41:20 +0900

    feat: Lotto클래스에 필요한 validate 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,12 +12,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        LottoValidator.validate(numbers);
     }
-
-    // TODO: 추가 기능 구현
 
     public List<Integer> getNumbers() {
         return numbers;
---
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -0,0 +1,26 @@
+package lotto.validator;
+
+import java.util.List;
+
+public class LottoValidator {
+    public static void validate(List<Integer> numbers) {
+        validateLottoNumbersSize(numbers);
+        validateLottoNumbersDuplicate(numbers);
+    }
+
+    private static void validateLottoNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateLottoNumbersDuplicate(List<Integer> numbers) {
+        List<Integer> disticedNumbers = numbers.stream()
+                .distinct()
+                .toList();
+
+        if (disticedNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 서로 중복되면 안됩니다.");
+        }
+    }
+}

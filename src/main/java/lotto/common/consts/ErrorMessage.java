4621abf3e73d71808570788ba7269b974d44ba4d
jifrozen0110 <answldjs1836@gmail.com>
2023-11-05 22:23:12 +0900

    fix(ErrorMessage) : 문자열 오타 수정

--- src/main/java/lotto/common/consts/ErrorMessage.java
+++ src/main/java/lotto/common/consts/ErrorMessage.java
@@ -1,10 +1,10 @@
 package lotto.common.consts;
 
 public class ErrorMessage {
-    public static final String NUMBER_FORMAT_ERROR_MESSAGE = "[Error] 정해진 숫자 타입이 아닙니다.";
-    public static final String NOT_DIVIDE_ZERO_ERROR_MESSAGE = "[Error] 0으로 나누어 떨어지지 않습니다.";
-    public static final String NULL_ERROR_MESSAGE = "[Error] 해당 값이 NULL 입니다.";
-    public static final String NOT_POSITIVE_ERROR_MESSAGE = "[Error] 양수값이 아닙니다.";
-    public static final String LOTTO_CONTAINS_BONUS_ERROR_MESSAGE = "[Error] 로또 번호에 보너스 번호가 포함됩니다.";
+    public static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 정해진 숫자 타입이 아닙니다.";
+    public static final String NOT_DIVIDE_ZERO_ERROR_MESSAGE = "[ERROR] 0으로 나누어 떨어지지 않습니다.";
+    public static final String NULL_ERROR_MESSAGE = "[ERROR] 해당 값이 NULL 입니다.";
+    public static final String NOT_POSITIVE_ERROR_MESSAGE = "[ERROR] 양수값이 아닙니다.";
+    public static final String LOTTO_CONTAINS_BONUS_ERROR_MESSAGE = "[ERROR] 로또 번호에 보너스 번호가 포함됩니다.";
 
 }

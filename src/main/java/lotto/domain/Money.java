32488706eba62f3f5b792549822dbd3267de5634
IM-GYURI <kgulr0517@gmail.com>
2023-11-05 19:58:12 +0900

    feat: validateZero(), validateDivideMoney() 작성

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public class Money {
+    private final int money;
+
+    private static final int ZERO = 0;
+    private static final int THOUSAND = 1000;
+
+    public Money(int money) {
+        this.money = money;
+    }
+
+    private void validate(int inputValue) {
+        validateZero(inputValue);
+        validateDivideMoney(inputValue);
+    }
+
+    private void validateZero(int inputValue) {
+        if (inputValue == ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivideMoney(int inputValue) {
+        if (inputValue % THOUSAND != ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

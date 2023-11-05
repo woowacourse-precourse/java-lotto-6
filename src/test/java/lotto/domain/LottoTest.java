a0582bcd79a04e208c1ad72bcc5def2bfc6ed81c
yuseonjun <seonjun0906@gmail.com>
2023-11-05 16:30:08 +0900

    test(LottoTest): 로또 안에 번호가 포함되어 있으면 true를 반환하는 isContainsLottoNumber 메서드 테스트 구현

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,11 +2,14 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -30,6 +33,15 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 47)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 안에 번호가 포함되어 있으면 true를 반환한다.")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void isContainsLottoNumber_Method_Test(int number) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertTrue(lotto.isContainsLottoNumber(number));
     }
 
     @Nested

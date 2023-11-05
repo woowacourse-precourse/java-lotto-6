ec08bf8f7276a703e4078a5a0958815a927d8c2d
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-11-06 01:34:45 +0900

    Refactor: Money클래스 상수 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 public class Money {
+    private static final String MONEY_NOT_UNDER_ONE_LOTTO_ERROR = "로또 구입 금액은 1000원 이상이어야 합니다";
+    private static final String MONEY_NOT_MULTIPLE_OF_EACH_LOTTO = "로또 구입 금액은 1000원 단위로 입력해주세요";
     private static final int EACH_LOTTO = 1000;
     private static final int ZERO = 0;
 
@@ -8,11 +10,11 @@ public class Money {
 
     public Money(int amount) {
         if (amount < EACH_LOTTO) {
-            throw new IllegalArgumentException("로또 구입 금액은 1000원 이상이어야 합니다");
+            throw new IllegalArgumentException(MONEY_NOT_UNDER_ONE_LOTTO_ERROR);
         }
 
         if (amount % EACH_LOTTO != ZERO) {
-            throw new IllegalArgumentException("로또 구입 금액은 1000원 단위로 입력해주세요");
+            throw new IllegalArgumentException(MONEY_NOT_MULTIPLE_OF_EACH_LOTTO);
         }
 
         this.amount = amount;

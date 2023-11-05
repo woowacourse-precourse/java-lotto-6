a73a05594cb95fd96a4b3ade3ad969ce6b20d2e9
seonghoo1217 <seonghoo1217@naver.com>
2023-11-05 18:33:31 +0900

    feat: 구입 금액 최소 금액 입력 케이스 유효성 검사 로직 작성

--- src/main/java/lotto/validation/validator/CostValidator.java
+++ src/main/java/lotto/validation/validator/CostValidator.java
@@ -6,6 +6,12 @@ import static lotto.property.LottoProperty.*;
 
 public class CostValidator extends RootValidator{
 
+    public static void CostIsStandardUnder(String inputPurchaseCost){
+        Long purchaseCost = Long.parseLong(inputPurchaseCost);
+        if (purchaseCost<COST_UNIT_STANDARD){
+            throw new IllegalArgumentException(ErrorProperty.COST_IS_STATNDARD_UNDER.toString());
+        }
+    }
     public static void CostFormatIsCorrect(String inputPurchaseCost){
         Long purchaseCost = Long.parseLong(inputPurchaseCost);
         if (purchaseCost % COST_UNIT_STANDARD != COST_UNIT_TARGET){

1546db6826abe34a24995fdb68a0c902b23928d8
Hanjaemo <hjm2530@gmail.com>
2023-11-06 04:59:12 +0900

    feat(Lotto): 로또 번호 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,44 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import lotto.constants.LottoConstants;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicated(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LottoConstants.LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 %d개여야 한다."
+                    .formatted(LottoConstants.LOTTO_NUMBERS_SIZE));
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        long numbersSizeWithValidRange = numbers.stream()
+                .filter(number ->
+                        LottoConstants.MIN_LOTTO_NUMBER <= number && number <= LottoConstants.MAX_LOTTO_NUMBER)
+                .count();
+        if (numbers.size() != numbersSizeWithValidRange) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 %d이상 %d이하여야 한다."
+                    .formatted(LottoConstants.MIN_LOTTO_NUMBER, LottoConstants.MAX_LOTTO_NUMBER));
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (distinctNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 달라야 한다.");
         }
     }
 

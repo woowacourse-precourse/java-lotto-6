e75e7c28190a891fc09b53326a6f4b2e8e92240b
jcw1031 <jcw001031@gmail.com>
2023-11-05 14:14:39 +0900

    feat: 구매 금액 검증 로직
    구매 금액이 1,000원 이상(1개 이상 구매)이 아니거나 1,000원으로 나누어 떨어지지 않는 경우 예외 발생

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,14 @@
 package lotto.controller;
 
+import lotto.validator.LottoValidator;
 import lotto.view.InputView;
 
 public class LottoController {
 
+    public static final int LOTTO_PRICE = 1_000;
+
     public void start() {
         int purchaseAmount = InputView.enterLottoPurchaseAmount();
+        LottoValidator.validatePurchaseAmount(purchaseAmount);
     }
 }
---
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -0,0 +1,15 @@
+package lotto.validator;
+
+public class LottoValidator {
+
+    private static final int LOTTO_PRICE = 1_000;
+
+    public static void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 로또는 1개 이상 구매해야 합니다.");
+        }
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또는 1,000원 단위로 구매할 수 있습니다.");
+        }
+    }
+}

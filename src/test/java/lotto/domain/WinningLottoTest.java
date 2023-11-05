b444748e6a675e7b06d2cc918d1b7c1b10f6b051
Isaac Han <isaachan233@gmail.com>
2023-11-05 14:33:00 +0900

    test: 보너스 번호가 당첨 번호에 포함되면 예외 발생 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,13 +1,26 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class WinningLottoTest {
+
+    WinningLotto winningLotto;
+    @BeforeEach
     @Test
     void createWinningLotto() {
-        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), new LottoNumber(7));
         winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), new LottoNumber(7));
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호에 포함되면 예외 발생")
+    @Test
+    void createWinningLottoByContainNumber() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), new LottoNumber(2)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

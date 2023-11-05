6cd07fe8aff7a9f036cf9d5c5408ab37ac7ca093
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-05 19:27:44 +0900

    feat(numberResult) : 로또, 당첨 번호 검증기 구현

--- src/main/java/lotto/model/ResultComparator.java
+++ src/main/java/lotto/model/ResultComparator.java
@@ -1,7 +1,36 @@
 package lotto.model;
 
+import static lotto.util.enums.LottoResult.*;
+
+import java.util.Optional;
+
 /**
  * 로또 번호와 당첨 번호를 비교하는 클래스
  */
 public class ResultComparator {
+    private static final int MATCH_5 = 5;
+
+    private final WinningNumbers winningNumbers;
+    private final BonusNumber bonusNumber;
+
+    public ResultComparator(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Optional<String> result(Lotto lotto) {
+        Integer match = resultCompare(lotto);
+        if (hasMatchedBonus(lotto, match)) {
+            return Optional.ofNullable(MATCH_5_BONUS.getDescription());
+        }
+        return fromMatchCount(match);
+    }
+
+    private Integer resultCompare(Lotto lotto) {
+        return lotto.countMatchingNumbers(winningNumbers);
+    }
+
+    private boolean hasMatchedBonus(Lotto lotto, Integer matchCount) {
+        return lotto.hasMatchedBonus(bonusNumber) && matchCount == MATCH_5;
+    }
 }

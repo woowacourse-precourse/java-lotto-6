8b5897038d2b96da08a595cf26e159043ace98e1
YongJOoOo <we072200@naver.com>
2023-11-05 21:34:53 +0900

    test: LottoGenerator 테스트 추가

---
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lotto.vo.Lotto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+
+    @DisplayName("입력한 수 만큼 로또가 생성됨")
+    @Test
+    void generateLottoSizeCheck() {
+        // given
+        int lottoCount = 10;
+
+        // when
+        List<Lotto> resultList = LottoGenerator.generateLottoList(BigDecimal.valueOf(lottoCount));
+
+        // then
+        Assertions.assertThat(resultList.size()).isEqualTo(lottoCount);
+    }
+
+    @Disabled // 너무 오래걸려서 제외함
+    @DisplayName("Integer.MAX_VALUE 보다 많은 로또를 생성하면, list 사이즈는 Integer.MAX_VALUE 로 출력됨")
+    @Test
+    void generateLottoLargeSizeCheck() {
+        // given
+        long lottoCount = (long) Integer.MAX_VALUE + 1L;
+
+        // when
+        List<Lotto> resultList = LottoGenerator.generateLottoList(BigDecimal.valueOf(lottoCount));
+
+        // then
+        Assertions.assertThat(resultList.size()).isEqualTo(lottoCount);
+    }
+}

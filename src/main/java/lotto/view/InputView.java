4d42af5721249af8e495fcd80293c8c5b2cbb7c8
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-05 22:39:07 +0900

    refactor: 메소드이름 수정 가독성 향상을 위해 validateInteger()의 이름을 수정하였음

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,12 +7,12 @@ import lotto.utils.GameMessage;
 public class InputView {
     public static int getMoneyInput() {
         System.out.println(GameMessage.ENTER_PURCHASE_AMOUNT.getMessage());
-        String inputMoney = Console.readLine();
-        validateInteger(inputMoney);
+        String inputMoney = readLine();
+        validateIntegerForInputMoney(inputMoney);
         return Integer.parseInt(inputMoney);
     }
 
-    private static void validateInteger(String input) {
+    private static void validateIntegerForInputMoney(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_MONEY_NOT_INTEGER_ERROR.getMessage());
         }

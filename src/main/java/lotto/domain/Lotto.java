bab566a535d419faefdb53e13d2ee4db5dbd0f94
yechop <channetattoo@gmail.com>
2023-11-06 01:22:32 +0900

    feat(Lotto): Lotto를 생성할때 검증 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,5 @@
 package lotto.domain;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateNumbersSize(numbers);
+        validateDuplicatedNumber(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != Constants.LOTTO_NUMBERS) {
+            throw new IllegalArgumentException("[ERROR] 6자리 숫자를 입력해 주세요.");
         }
     }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 6자리 숫자를 입력해 주세요.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 각 번호는 1 이상 45 이하로 입력해 주세요.");
+        }
+    }
+
 
     public List<Integer> getNumbers() {
         return numbers;

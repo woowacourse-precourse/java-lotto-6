e94e203a9782440e4f4846353e2fb955f567b757
shining013 <nevertheless113@naver.com>
2023-11-05 20:25:08 +0900

    modify: 구입금액 입력받는 메서드 이름 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String printPurchasePrompt() {
+    public static String PurchaseInput() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }

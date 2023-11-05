a7685e7519766d9717c6f37969f6a6f59d6c1456
mumpuris <hst990124@naver.com>
2023-11-05 23:03:23 +0900

    코드 리팩토링 (람다 함수 사용과 관련된 경고 발생한 이유로)

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -70,9 +70,7 @@ class LottoMachineTest {
     @DisplayName("800원이 주어질 때, 예외가 발생한다.")
     @Test
     public void given_800Won_when_CalculateNumberOfTickets_then_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            lottoMachine.calculateNumberOfTickets(800);
-        });
+        assertThrows(IllegalArgumentException.class, () -> lottoMachine.calculateNumberOfTickets(800));
     }
 
     @Test
@@ -86,8 +84,6 @@ class LottoMachineTest {
     @Test
     @DisplayName("4500원이 주어질 때, 에러가 발생한다.")
     public void given_4500Won_when_Purchase_Lottos_then_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, ()->{
-            lottoMachine.purchaseLottos(4500);
-        });
+        assertThrows(IllegalArgumentException.class, ()-> lottoMachine.purchaseLottos(4500));
     }
 }

89dacdb817970d1c60472469d8903beeae82f1cb
hwan2-99 <thh1233123@gmail.com>
2023-11-05 13:05:08 +0900

    test: 로또 구매개수 확인 테스트 구현

---
+++ src/test/java/lotto/domain/LottoPurchaseTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.constant.ErrorMessage;
+import lotto.domain.Lotto;
+import lotto.domain.LottoPurchase;
+import lotto.service.InputService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoPurchaseTest {
+    private LottoPurchase lottoPurchase;
+    @BeforeEach
+    void setUp(){
+        lottoPurchase = new LottoPurchase();
+    }
+
+    @Test
+    @DisplayName("로또 구매 테스트")
+    void lottoPurchaseAmountTest(){
+        int purchaseAmount = 5;
+        List<Lotto> purchaseLottos = lottoPurchase.purchaseLottoNumbers(purchaseAmount);
+
+        assertThat(purchaseAmount).isEqualTo(purchaseLottos.size());
+    }
+
+}

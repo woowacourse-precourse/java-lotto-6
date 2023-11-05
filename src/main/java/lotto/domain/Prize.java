33b601d30afcf9399d46c8548e000789e8d261eb
yshong1998 <yshong1998@gmail.com>
2023-11-06 03:05:41 +0900

    Refactor(Prize): 일치 개수에 따른 메세지를 변수로 갖고 있도록 변경

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -2,27 +2,51 @@ package lotto.domain;
 
 public enum Prize {
 
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5,1500000),
-    FOURTH(4,50000),
-    FIFTH(3,5000),
-    FAIL(0,0);
+    ZERO(0,"0개 일치 ",0),
+    ONE(1,"1개 일치 ",0),
+    TWO(2,"2개 일치 ",0),
+    THREE(3,"3개 일치 ",5_000),
+    FOUR(4, "4개 일치 ",50_000),
+    FIVE(5, "5개 일치 ",1_500_000),
+    FIVE_BONUS(5, "5개 일치, 보너스 볼 일치 ",30_000_000),
+    SIX(6, "6개 일치 ",2_000_000_000);
+
+
 
     private final Integer matchCount;
+    private final String prizeMessage;
     private final Integer winningAmount;
 
-    Prize(Integer matchCount, Integer winningAmount) {
+    Prize(Integer matchCount, String prizeMessage, Integer winningAmount) {
         this.matchCount = matchCount;
+        this.prizeMessage = prizeMessage;
         this.winningAmount = winningAmount;
     }
 
-    public static Prize getPrizeByMatchCount(int matchCount){
-        for (Prize prize : values()) {
-            if(prize.matchCount == matchCount){
-                return prize;
-            }
+    public static Prize getPrizeByMatchCount(int matchCount, boolean isBonusNumberMatch){
+        Prize[] prizes = values();
+        if (matchCount == 5){
+            return isFiveOrFiveBonus(isBonusNumberMatch);
         }
-        return FAIL;
+        return prizes[matchCount];
+    }
+
+    public static Prize isFiveOrFiveBonus(boolean isBonusNumberMatch){
+        if (isBonusNumberMatch){
+            return FIVE_BONUS;
+        }
+        return FIVE;
+    }
+
+    public Integer getMatchCount() {
+        return matchCount;
+    }
+
+    public Integer getWinningAmount() {
+        return winningAmount;
+    }
+
+    public String getPrizeMessage(){
+        return prizeMessage;
     }
 }

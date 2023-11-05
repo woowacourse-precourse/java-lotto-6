24b2773ae843980f846ab644ba3c0aadee29166c
노정완 <jeongwan.noh@gmail.com>
2023-11-05 15:40:26 +0900

    feat: 구입한 로또 번호 생성 로직 구현

--- src/main/java/lotto/domain/lottery/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/lottery/LottoNumberGenerator.java
@@ -1,4 +1,14 @@
 package lotto.domain.lottery;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
 public class LottoNumberGenerator {
+    private static List<Integer> generateLottoNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
+    }
 }

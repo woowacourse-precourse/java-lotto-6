55ed80fa48b4178f3e6c72fe7b0dc5b956f892d1
bbbig5360 <bbbig5360>
2023-11-06 02:17:31 +0900

    로또 구입 금액 입력 + 구입 금액의 예외 처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
+import lotto.Lotto;
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 
 public class Application {
+    final static int lottolength = 6;
+    final static int coinStandard = 1000; 
+    static int inputCoin;
+    static List<List<Integer>> lottoTotal = new ArrayList<>();
+
+    public static void CoinValidate(int inputCoin){
+        if (inputCoin % coinStandard != 0)
+            System.out.println("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");        
+    }
+
+    public static void GetCoin(){
+        System.out.println("구입금액을 입력해 주세요.");
+        inputCoin = Integer.parseInt(Console.readLine());
+        CoinValidate(inputCoin);
+        inputCoin = inputCoin / coinStandard;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GetCoin();        
+        for(int i=0; i<inputCoin; i++){
+
+        }
+        
     }
 }

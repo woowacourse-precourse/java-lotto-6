cd9e37358484dc34251b45aba36a26e111bbca41
Haeun Jeong <gkdms4248@gmail.com>
2023-11-05 20:38:44 +0900

    refactor: 로또 당첨번호와 수익률 계산 리팩토링

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -5,7 +5,10 @@ import static lotto.LottoCriteria.FIRST_PLACE;
 import static lotto.LottoCriteria.FOURTH_PLACE;
 import static lotto.LottoCriteria.SECOND_PLACE;
 import static lotto.LottoCriteria.THIRD_PLACE;
+import static lotto.LottoRule.PERCENT;
+import static lotto.LottoRule.PRICE;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,25 +16,60 @@ public class LottoResult {
 
     private final WinningNumber winningNumber;
     private final List<Lotto> issuedLotto;
-    private Map<String, Long> result;
+    private Map<LottoCriteria, Long> rankingResult;
 
     public LottoResult(final WinningNumber winningNumber, final List<Lotto> issuedLotto) {
         this.winningNumber = winningNumber;
         this.issuedLotto = issuedLotto;
+        rankingResult = new HashMap<>();
+        rankingResult.put(FIRST_PLACE, 0L);
+        rankingResult.put(SECOND_PLACE, 0L);
+        rankingResult.put(THIRD_PLACE, 0L);
+        rankingResult.put(FOURTH_PLACE, 0L);
+        rankingResult.put(FIFTH_PLACE, 0L);
+        saveResult();
     }
 
-    public long matchingCount(Lotto lotto) {
-        return lotto.getNumbers().stream()
+    private void saveResult() {
+        issuedLotto.stream().forEach(lotto -> matchingCount(lotto));
+    }
+
+    public void matchingCount(Lotto lotto) {
+        Long count = lotto.getNumbers().stream()
                 .filter(winningNumber.lotto().getNumbers()::contains)
                 .count();
+        if (count == FIFTH_PLACE.getCount()) {
+            rankingResult.merge(FIFTH_PLACE, 1L, Long::sum);
+        }
+        if (count == FOURTH_PLACE.getCount()) {
+            rankingResult.merge(FOURTH_PLACE, 1L, Long::sum);
+        }
+        if (count == THIRD_PLACE.getCount() && !isBonusContain(lotto)) {
+            rankingResult.merge(THIRD_PLACE, 1L, Long::sum);
+        }
+        if (count == SECOND_PLACE.getCount() && isBonusContain(lotto)) {
+            rankingResult.merge(SECOND_PLACE, 1L, Long::sum);
+        }
+        if (count == FIRST_PLACE.getCount()) {
+            rankingResult.merge(FIRST_PLACE, 1L, Long::sum);
+        }
     }
 
-    public float returnCalculation(final Map<String, Long> result, Long price) {
-        Long totalAmount = result.get(FIFTH_PLACE.getPlace()) * FIFTH_PLACE.getAmount()
-                + result.get(FOURTH_PLACE.getPlace()) * FOURTH_PLACE.getAmount()
-                + result.get(THIRD_PLACE.getPlace()) * THIRD_PLACE.getAmount()
-                + result.get(SECOND_PLACE.getPlace()) * SECOND_PLACE.getAmount()
-                + result.get(FIRST_PLACE.getPlace()) * FIRST_PLACE.getAmount();
-        return (float) totalAmount / price * 100;
+    public boolean isBonusContain(Lotto lotto) {
+        return lotto.getNumbers().contains(winningNumber.bonus());
+    }
+
+    public float returnCalculation() {
+        Long totalAmount = rankingResult.get(FIFTH_PLACE) * FIFTH_PLACE.getAmount()
+                + rankingResult.get(FOURTH_PLACE) * FOURTH_PLACE.getAmount()
+                + rankingResult.get(THIRD_PLACE) * THIRD_PLACE.getAmount()
+                + rankingResult.get(SECOND_PLACE) * SECOND_PLACE.getAmount()
+                + rankingResult.get(FIRST_PLACE) * FIRST_PLACE.getAmount();
+
+        return (float) totalAmount / (issuedLotto.size() * PRICE.getValue()) * PERCENT.getValue();
+    }
+
+    public Map<LottoCriteria, Long> getRankingResult() {
+        return rankingResult;
     }
 }
--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -1,20 +1,21 @@
 package lotto;
 
+import static lotto.LottoCriteria.FIFTH_PLACE;
+import static lotto.LottoCriteria.FIRST_PLACE;
+import static lotto.LottoCriteria.FOURTH_PLACE;
+import static lotto.LottoCriteria.THIRD_PLACE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoResultTest {
-    private LottoResult lottoResult;
 
     @BeforeEach
     void setUp() {
-
     }
 
     @DisplayName("번호가 3개 일치할 경우")
@@ -23,14 +24,15 @@ public class LottoResultTest {
         // given
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto issuedlotto = new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9));
-        lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
 
         // when
-        long result = lottoResult.matchingCount(issuedlotto);
+        LottoResult lottoResult = new LottoResult(
+                new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
+        Map<LottoCriteria, Long> result = lottoResult.getRankingResult();
 
         // then
-        int expected = 3;
-        assertEquals(expected, result);
+        int expected = 1;
+        assertEquals(expected, result.get(FIFTH_PLACE));
     }
 
     @DisplayName("번호가 4개 일치할 경우")
@@ -39,14 +41,14 @@ public class LottoResultTest {
         // given
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto issuedlotto = new Lotto(Arrays.asList(3, 4, 5, 6, 10, 11));
-        lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
+        LottoResult lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
 
         // when
-        long result = lottoResult.matchingCount(issuedlotto);
+        Map<LottoCriteria, Long> result = lottoResult.getRankingResult();
 
         // then
-        int expected = 4;
-        assertEquals(expected, result);
+        int expected = 1;
+        assertEquals(expected, result.get(FOURTH_PLACE));
     }
 
     @DisplayName("번호가 5개 일치할 경우")
@@ -55,14 +57,14 @@ public class LottoResultTest {
         // given
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto issuedlotto = new Lotto(Arrays.asList(2, 3, 4, 5, 6, 11));
-        lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
+        LottoResult lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
 
         // when
-        long result = lottoResult.matchingCount(issuedlotto);
+        Map<LottoCriteria, Long> result = lottoResult.getRankingResult();
 
         // then
-        int expected = 5;
-        assertEquals(expected, result);
+        int expected = 1;
+        assertEquals(expected, result.get(THIRD_PLACE));
     }
 
     @DisplayName("번호가 6개 일치할 경우")
@@ -71,32 +73,35 @@ public class LottoResultTest {
         // given
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto issuedlotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
+        LottoResult lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
 
         // when
-        long result = lottoResult.matchingCount(issuedlotto);
+        Map<LottoCriteria, Long> result = lottoResult.getRankingResult();
 
         // then
-        int expected = 6;
-        assertEquals(expected, result);
+        int expected = 1;
+        assertEquals(expected, result.get(FIRST_PLACE));
     }
 
-    @DisplayName("3개 일치 1개일 경우 62.5 수익률을 반환")
+    @DisplayName("8개 복권 중 3개 일치 1개일 경우 62.5 수익률을 반환")
     @Test
     public void returnCalculation() {
         // given
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        Lotto issuedlotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        lottoResult = new LottoResult(new WinningNumber(winningLotto, 7), Arrays.asList(issuedlotto));
-        Map<String, Long> result = new HashMap<>();
-        result.put("1", 0L);
-        result.put("2", 0L);
-        result.put("3", 0L);
-        result.put("4", 0L);
-        result.put("5", 1L);
+        Lotto issuedLotto1 = new Lotto(Arrays.asList(8, 21, 23, 41, 42, 43));
+        Lotto issuedLotto2 = new Lotto(Arrays.asList(3, 5, 11, 16, 32, 38));
+        Lotto issuedLotto3 = new Lotto(Arrays.asList(7, 11, 16, 35, 36, 44));
+        Lotto issuedLotto4 = new Lotto(Arrays.asList(1, 8, 11, 31, 41, 42));
+        Lotto issuedLotto5 = new Lotto(Arrays.asList(13, 14, 16, 38, 42, 45));
+        Lotto issuedLotto6 = new Lotto(Arrays.asList(7, 11, 30, 40, 42, 43));
+        Lotto issuedLotto7 = new Lotto(Arrays.asList(2, 13, 22, 32, 38, 45));
+        Lotto issuedLotto8 = new Lotto(Arrays.asList(1, 3, 5, 14, 22, 45));
 
         // when
-        Float price = lottoResult.returnCalculation(result, 8000L);
+        LottoResult lottoResult = new LottoResult(new WinningNumber(winningLotto, 7),
+                Arrays.asList(issuedLotto1, issuedLotto2, issuedLotto3, issuedLotto4,
+                        issuedLotto5, issuedLotto6, issuedLotto7, issuedLotto8));
+        Float price = lottoResult.returnCalculation();
 
         // then
         Float expected = 62.5f;

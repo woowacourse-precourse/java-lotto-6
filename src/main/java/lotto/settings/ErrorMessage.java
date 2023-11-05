3c3aa6dcd8ffea8579a61ac0c68308d522936c3a
heejae <hjhjhjhj3770@naver.com>
2023-11-05 10:30:44 +0900

    Refactor(ErrorMessage): 모든 에러 메세지앞에 "[Error]"를 붙였다

--- src/main/java/lotto/settings/ErrorMessage.java
+++ src/main/java/lotto/settings/ErrorMessage.java
@@ -5,12 +5,12 @@ import static lotto.settings.LottoRange.*;
 public enum ErrorMessage {
     //숫자인지
     //천원단위인지
-    INVALID_NUMBER_FORMAT("숫자를 입력해야 합니다."),
-    INVALID_UNIT_FORMAT("구입금액은 1,000원 단위여야 합니다"),
-    CONTAIN_SPACE("공백이 포함되어 있습니다."),
-    INVALID_COUNT(String.format("로또번호는 %d개여야 합니다.", SIZE.getNumber())),
-    INVALID_RANGE(String.format("로또 번호는 %d ~ %d 사이여야합니다",MIN_VALUE.getNumber(), MAX_VALUE.getNumber())),
-    DUPLICATE_NUM("중복된 번호가 있습니다.");
+    INVALID_NUMBER_FORMAT("[ERROR] 숫자를 입력해야 합니다."),
+    INVALID_UNIT_FORMAT("[ERROR] 구입금액은 1,000원 단위여야 합니다"),
+    CONTAIN_SPACE("[ERROR] 공백이 포함되어 있습니다."),
+    INVALID_COUNT(String.format("[ERROR] 로또번호는 %d개여야 합니다.", SIZE.getNumber())),
+    INVALID_RANGE(String.format("[ERROR] 로또 번호는 %d ~ %d 사이여야합니다",MIN_VALUE.getNumber(), MAX_VALUE.getNumber())),
+    DUPLICATE_NUM("[ERROR] 중복된 번호가 있습니다.");
 
     //공백이있는지
     //6개인지

0ced182f6642bd22b6da457ba4a65ead82a2d38e
minsub <chrkb1569@naver.com>
2023-11-05 22:40:07 +0900

    feat : Main 함수에 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.service.GameService;
+import lotto.util.LottoNumberFactory;
+import lotto.view.input.ConsoleInputView;
+import lotto.view.output.ConsoleOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController(new ConsoleInputView(),
+                new ConsoleOutputView(),
+                new LottoNumberFactory(), new GameService());
+
+        gameController.startGame();
     }
 }

68b9e2040edaba96b8019905e91fa120301b5722
peppone-choi <peppone.choi@gmail.com>
2023-11-05 22:42:03 +0900

    chore: 패키지와 레코드 수정

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -1,11 +1,10 @@
 package lotto;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import lotto.model.WinningNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class WinningNumberTest {

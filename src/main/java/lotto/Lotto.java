5e4e6d77b530eaff11390bafb928a216b757d45b
Son JunHyung <kingjh1125@gmail.com>
2023-11-05 16:26:16 +0900

    feat (Lotto): 중복 예외 처리
    중복 숫자 테스트에 대한 프로덕션 코드를 작성하여 통과하였습니다:
    - validate에서 validateSize 분리
    - validateDuplicate 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers){
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
+
+    private void validateDuplicate(List<Integer> numbers){
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     // TODO: 추가 기능 구현
 }

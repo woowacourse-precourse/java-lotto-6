9ecff04fb3f940f2e020d07af0714e921ef7ccdf
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-05 20:09:11 +0900

    feat(LottoMachine): 입력한 금액에 따라 자동으로 로또를 발행한다.

---
+++ src/main/java/lotto/model/LottoMachine.java
@@ -0,0 +1,46 @@
+package lotto.model;
+
+import lotto.config.LottoConfig;
+import lotto.dto.response.LottoTicketsDto;
+import lotto.util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoMachine {
+
+    private final NumberGenerator numberGenerator;
+
+    private LottoMachine(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public static LottoMachine of(NumberGenerator numberGenerator) {
+        return new LottoMachine(numberGenerator);
+    }
+
+    public LottoTicketsDto purchaseLottoTickets(Money money) {
+        int ticketCount = getTicketCount(money);
+        List<Lotto> lottoTickets = IntStream.range(0, ticketCount)
+                .mapToObj(i -> createLottoTicket())
+                .collect(Collectors.toList());
+        return new LottoTicketsDto(lottoTickets);
+    }
+
+    private Lotto createLottoTicket() {
+        return new Lotto(generateLottoNumbers());
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        return numberGenerator.generate(LottoConfig.START_OF_LOTTO_NUMBER.getValue()
+                , LottoConfig.END_OF_LOTTO_NUMBER.getValue()
+                , LottoConfig.LOTTO_NUMBER_COUNT.getValue());
+    }
+
+    private static int getTicketCount(Money money) {
+        return money.getAmount() / LottoConfig.LOTTO_PRICE.getValue();
+    }
+
+}

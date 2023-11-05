382a9c033092abe8bcd41e67c3dca2d7887cf940
dbstn1325 <dbstn1325@naver.com>
2023-11-05 23:18:04 +0900

    feat(LottoMathcher): 사용자가 구매한 로또 번호 여러개와 당첨 번호 간 일치하는 번호의 갯수를 구하는 기능 구현

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMatcher {
 
     private long correctWinningCount;
     private boolean correctBonus;
+    private List<Long> correctWinningsCount = new ArrayList<>();
+    private List<Boolean> correctBonuses = new ArrayList<>();
 
     public LottoMatcher(Lotto userNumbers, Lotto winningNumber) {
         this.correctWinningCount = calculateCorrectWinning(userNumbers, winningNumber);
@@ -12,6 +17,20 @@ public class LottoMatcher {
     public LottoMatcher(Lotto userNumbers, Lotto winningNumber, int bonusNumber) {
         this.correctWinningCount = calculateCorrectWinning(userNumbers, winningNumber);
         this.correctBonus = calculateCorrectBonus(userNumbers, bonusNumber);
+    }
+
+    public LottoMatcher(List<Lotto> userNumbers, Lotto winningNumber, int bonusNumber) {
+        for (Lotto userNumber : userNumbers) {
+            long winning = calculateCorrectWinning(userNumber, winningNumber);
+            boolean bonus = calculateCorrectBonus(userNumber, bonusNumber);
+
+            correctBonuses.add(bonus);
+            if(bonus) {
+                correctWinningsCount.add(winning + 1);
+                continue;
+            }
+            correctWinningsCount.add(winning);
+        }
     }
 
     public long calculateCorrectWinning(Lotto userNumbers, Lotto winningNumbers) {
@@ -43,5 +62,13 @@ public class LottoMatcher {
 
     public boolean isCorrectBonus() {
         return this.correctBonus;
+    }
+
+    public List<Long> getCorrectWinningsCount() {
+        return correctWinningsCount;
+    }
+
+    public List<Boolean> getCorrectBonuses() {
+        return correctBonuses;
     }
 }

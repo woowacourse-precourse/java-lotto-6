eb8b7fb5eb18025e5ced0930e53eb69948514c56
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 01:30:42 +0900

    feat: static block으로 로또 넘버 객체를 미리 캐싱

--- src/main/java/lotto/application/LottoFactory.java
+++ src/main/java/lotto/application/LottoFactory.java
@@ -5,19 +5,28 @@ import static lotto.domain.LottoConfig.LOTTO_MAX_NUMBER;
 import static lotto.domain.LottoConfig.LOTTO_MIN_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 
 public class LottoFactory {
+    private static final Map<Integer, LottoNumber> LOTTO_NUMBER_CACHE = new HashMap<>();
+
+    static {
+        IntStream.rangeClosed(LOTTO_MIN_NUMBER.getValue(), LOTTO_MAX_NUMBER.getValue())
+                .forEach(i -> LOTTO_NUMBER_CACHE.put(i, new LottoNumber(i)));
+    }
+
     public Lotto createLottoByAuto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 LOTTO_MIN_NUMBER.getValue(), LOTTO_MAX_NUMBER.getValue(), LOTTO_COUNT.getValue()
         );
-        List<LottoNumber> lottoNumbers = numbers.stream()
-                .map(LottoNumber::new)
-                .toList();
-
-        return new Lotto(lottoNumbers);
+        return new Lotto(numbers.stream()
+                .map(LOTTO_NUMBER_CACHE::get)
+                .toList()
+        );
     }
 }

7df446d234e089e4ae7528764905a186d38dcfe5
GiJung <weeds2577@gmail.com>
2023-11-06 01:35:51 +0900

    style(OutputValue): IED의 코드 자동 정렬 기능으로 코드 정렬

--- src/main/java/lotto/view/OutputValue.java
+++ src/main/java/lotto/view/OutputValue.java
@@ -5,13 +5,9 @@ import java.util.List;
 public class OutputValue {
 
     private static final String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
-
     private static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
-
     private static final String WIN_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
 
     public static void purchaseMessage() {
         System.out.println(PURCHASE_PRICE_MESSAGE);
@@ -25,9 +21,15 @@ public class OutputValue {
         System.out.println(numbers.toString());
     }
 
-    public static void changeLine() { System.out.println(); }
+    public static void changeLine() {
+        System.out.println();
+    }
 
-    public static void winLottoNumberMessage() { System.out.println(WIN_LOTTO_NUMBER_MESSAGE); }
+    public static void winLottoNumberMessage() {
+        System.out.println(WIN_LOTTO_NUMBER_MESSAGE);
+    }
 
-    public static void bonusNumberMessage() { System.out.println(BONUS_NUMBER_MESSAGE); }
+    public static void bonusNumberMessage() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+    }
 }

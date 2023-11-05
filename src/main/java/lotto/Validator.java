7d792047dbec8ce0199ec4417fc826d7967cf8b2
brightfanta <brightfanta@gmail.com>
2023-11-05 11:06:12 +0900

    feat(Errors): 입력값이 빈값일 때 예외발생 및 메시지 출력

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,4 +1,17 @@
 package lotto;
 
 public class Validator {
+
+    static Boolean validationFlag = true;
+
+    public static boolean isInputEmpty(String userInput) {
+        try {
+            if (userInput.isEmpty()) {
+                throw new IllegalArgumentException("");
+            }
+        } catch (IllegalArgumentException e) {
+            return true;
+        }
+        return false;
+    }
 }

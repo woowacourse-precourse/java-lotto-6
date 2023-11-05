909299aab42f8a5f0da5efb26d307619330792d1
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 00:17:53 +0900

    feat(ProfitRate): 클래스 생성 및 반환 기능 구현
    docs: ProfitRate 기능 목록 체크

---
+++ src/main/java/lotto/model/ProfitRate.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class ProfitRate {
+
+    private final double profitRate;
+
+    public ProfitRate(double profitRate) {
+        this.profitRate = profitRate;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+}

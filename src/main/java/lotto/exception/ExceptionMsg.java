e4e6ecc817cb0934af95f82b13ed581c1a8e8209
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 19:19:07 +0900

    Feat(ExceptionMsg): ExcetionMsg 클래스 구현

--- src/main/java/lotto/exception/ExceptionMsg.java
+++ src/main/java/lotto/exception/ExceptionMsg.java
@@ -1,4 +1,14 @@
 package lotto.exception;
 
-public class ExceptionMsg {
+public enum ExceptionMsg {
+    NULL_INPUT("아무런 입력이 없습니다.\n"),
+    NOT_INT_TRY("입력이 숫자가 아닙니다.\n"),
+    NOT_MULTIPLE_THOUSAND("구입금액이 1000의 배수가 아닙니다\n"),
+    DUPLICATE_LOTTO_NUM("로또 번호가 중복될 수 없습니다.\n");
+
+    private final String message;
+
+    ExceptionMsg(String message) {
+        this.message = message;
+    }
 }

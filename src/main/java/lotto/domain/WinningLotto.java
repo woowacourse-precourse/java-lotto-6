5865b96cae07ecb3c619bbad883b0668d8a7e51b
JH-TT <leejongho9803@gmail.com>
2023-11-05 15:16:29 +0900

    feat: 보너스 번호 반환하는 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.List;
 import lotto.validation.Validation;
-import lotto.view.OutputView;
 
 public class WinningLotto extends Lotto {
     private final int bonus;
@@ -10,6 +9,10 @@ public class WinningLotto extends Lotto {
         super(numbers);
         validate(bonus);
         this.bonus = Integer.parseInt(bonus);
+    }
+
+    public int getBonus() {
+        return bonus;
     }
 
     public void validate(final String bonus) {

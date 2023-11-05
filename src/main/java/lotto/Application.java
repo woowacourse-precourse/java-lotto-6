5304742469589571d612f632faf297aaeffd3b23
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-05 21:56:53 +0900

    feat(Application - main()): 로또 구매 가격 & 구매 개수 입력부분 구현 (이후 개별 함수화해서 리팩토링 예정)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        int purchaseAmount;
+        int purchaseCount = 0;
+
         System.out.println("구입금액을 입력해 주세요.");
+
+        UserInput userInput = new UserInput();
+
+        while(UserInput.repeatPurchase) {
+            purchaseAmount = userInput.getPurchaseAmount();
+            purchaseCount = userInput.validatePurchaseAmount(purchaseAmount);
+        }
+
+        System.out.println();
+        System.out.printf("%d개를 구매했습니다.%n", purchaseCount); //
 
 
     }

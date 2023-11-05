8a171992657c42e46b24cba8349e110e2aa23e56
김효준 <khyojun1104@gmail.com>
2023-11-05 18:35:40 +0900

    feat(lotto): 중복되게 당첨 번호를 입력하였는지 확인하는 기능 구현!

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,30 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateLotto = new HashSet<>(numbers);
+        List<Integer> numberNonDuplicate = new ArrayList<>(nonDuplicateLotto);
+        if(numberNonDuplicate.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복 없이 6개여야 합니다!");
+        }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다!");
         }
     }
 

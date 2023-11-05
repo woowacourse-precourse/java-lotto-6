aadbbc29875d9b685ba7b236996f0d56e43c39c8
minjun7410 <minjun7410@gmail.com>
2023-11-05 21:19:26 +0900

    test(WinningNumber): 등수 계산 테스트 케이스 추가
    * FIRST를 포함한 다른 결과들을 테스트하는 케이스를 추가했다.

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -4,10 +4,13 @@ import lotto.domain.BonusNumber;
 import lotto.domain.WinningNumber;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -21,14 +24,26 @@ public class WinningNumberTest {
         assertThatThrownBy(() -> new WinningNumber(lotto, bonusNumber)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 당첨번호갯수_보너스포함여부_등수_계산_테스트() {
-        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        BonusNumber bonusNumber = new BonusNumber(7);
+    @ParameterizedTest
+    @MethodSource("generateInput")
+    void 당첨번호갯수_보너스포함여부_등수_계산_테스트(List<Integer> lottoNumbers, int bonus, String result) {
+        Lotto winningLotto = new Lotto(lottoNumbers);
+        BonusNumber bonusNumber = new BonusNumber(bonus);
         WinningNumber winningNumber = new WinningNumber(winningLotto, bonusNumber);
 
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
 
-        assertThat(winningNumber.calculateRank(lotto).getValue()).isEqualTo("FIRST");
+        assertThat(winningNumber.calculateRank(lotto).name()).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> generateInput() {
+        return Stream.of(
+                Arguments.of(List.of(1,2,3,4,5,6), 10, "FIRST"),
+                Arguments.of(List.of(1,2,3,4,5,7), 6, "SECOND"),
+                Arguments.of(List.of(1,2,3,4,5,7), 10, "THIRD"),
+                Arguments.of(List.of(1,2,3,4,7,8), 10, "FOURTH"),
+                Arguments.of(List.of(1,2,3,7,8,9), 10, "FIFTH"),
+                Arguments.of(List.of(10,11,12,13,14,15), 16, "FAIL")
+        );
     }
 }

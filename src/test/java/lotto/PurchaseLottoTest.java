b90501a3154dcae03ecb8443d4392a8cee56c4c1
혜민 <alddlv81@gmail.com>
2023-11-05 19:49:17 +0900

    test: 이전 코드 리팩토링에 따라 테스트 코드 수정

--- src/test/java/lotto/PurchaseLottoTest.java
+++ src/test/java/lotto/PurchaseLottoTest.java
@@ -10,40 +10,35 @@ public class PurchaseLottoTest {
     @DisplayName("로또 구매 금액에 공백이 포함되면 예외가 발생한다.")
     @Test
     void purchaseLottoBySpace() {
-        String purchaseAmount = "10 00";
-        assertThatThrownBy(() -> PurchaseLotto.validate(purchaseAmount))
+        assertThatThrownBy(() -> new PurchaseLotto("10 00"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구매 금액이 숫자가 아닌 경우 예외가 발생한다.")
     @Test
     void purchaseLottoByNonNumber() {
-        String purchaseAmount = "1000a";
-        assertThatThrownBy(() -> PurchaseLotto.validate(purchaseAmount))
+        assertThatThrownBy(() -> new PurchaseLotto("1000a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구매 금액의 첫 숫자가 0이면 예외가 발생한다.")
     @Test
     void purchaseLottoByLeadingZero() {
-        String purchaseAmount = "01000";
-        assertThatThrownBy(() -> PurchaseLotto.validate(purchaseAmount))
+        assertThatThrownBy(() -> new PurchaseLotto("01000"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구매 금액이 1000원 단위가 아니면 예외가 발생한다.")
     @Test
     void purchaseLottoByNonMultipleOf1000() {
-        String purchaseAmount = "1500";
-        assertThatThrownBy(() -> PurchaseLotto.validate(purchaseAmount))
+        assertThatThrownBy(() -> new PurchaseLotto("1500"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구매 금액이 10만원을 초과하면 예외가 발생한다.")
     @Test
     void purchaseLottoByOverMaxAmount() {
-        String purchaseAmount = "110000";
-        assertThatThrownBy(() -> PurchaseLotto.validate(purchaseAmount))
+        assertThatThrownBy(() -> new PurchaseLotto("110000"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

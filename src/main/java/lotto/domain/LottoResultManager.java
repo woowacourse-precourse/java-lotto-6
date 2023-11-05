bedd81836b1e1a79d05889079f86982809eab48d
sotogito <108535526+sotogito@users.noreply.github.com>
2023-11-06 00:41:26 +0900

    docs : 주의해야할 점 업데이트, 리팩토링 항목 추가

--- src/main/java/lotto/domain/LottoResultManager.java
+++ src/main/java/lotto/domain/LottoResultManager.java
@@ -1,4 +1,8 @@
 package lotto.domain;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class LottoResultManager {
+    private final Map<Rank,Integer> lottoResult = new EnumMap<Rank, Integer>(Rank.class);
 }
--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -28,7 +28,6 @@ public enum Rank {
         return Rank.NOTHING;
     }
 
-
     public int getMatchCount(){
         return matchCount;
     }

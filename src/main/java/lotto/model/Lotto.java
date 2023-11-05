aea515409123c0e69f437b076338dcaff1653f40
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-06 03:29:31 +0900

    feat: 입력받은 로또 번호를 쉼표를 기준으로 분리하는 로직 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import lotto.exception.Validation;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,12 +13,14 @@ public class Lotto {
 
     private final List<LottoNumber> lottoNumbers;
 
-    public Lotto(List<Integer> numbers) {
-        Validation.validateLottoSize(numbers);
-        Validation.validateLottoDuplication(numbers);
-        this.lottoNumbers = numbers.stream()
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
+    public Lotto(String lottoNumbers) {
+        this.lottoNumbers = parseLottoNumbers(lottoNumbers);
+    }
+
+    public Lotto(List<LottoNumber> lottoNumbers) {
+        Validation.validateLottoSize(lottoNumbers);
+        Validation.validateLottoDuplication(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
     public static int getLottoSize() {
@@ -26,5 +29,13 @@ public class Lotto {
 
     public List<LottoNumber> getNumbers() {
         return Collections.unmodifiableList(lottoNumbers);
+    }
+
+    public static List<LottoNumber> parseLottoNumbers(String lottoNumbers) {
+
+        return Arrays.stream(lottoNumbers.split(","))
+                .map(String::trim)
+                .map(number -> new LottoNumber(Integer.parseInt(number)))
+                .collect(Collectors.toList());
     }
 }

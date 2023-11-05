460de0ebbd67cfb81fc2d73b0a67c21c19d26d16
gonixxxx <akdntm946@gmail.com>
2023-11-05 15:32:54 +0900

    fix(StringConvertor): add static keyword

--- src/main/java/lotto/utils/StringConvertor.java
+++ src/main/java/lotto/utils/StringConvertor.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringConvertor {
-    public int stringToInt(String input) {
+    public static int stringToInt(String input) {
         return Integer.parseInt(input);
     }
 
-    public List<Integer> stringToList(String input) {
+    public static List<Integer> stringToList(String input) {
         return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
     }
 

1984003b61ddac8be1b944cccc0cf24f1049ae04
myeongju00 <j51677767@gmail.com>
2023-11-06 03:18:16 +0900

    [feat] : 로또 구입 금액의 예외 처리에 대한 테스트 코드 작성

---
+++ src/test/java/lotto/model/MoneyTest.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @Test
+    void 구입_금액이_숫자가_아닌_경우_예외_발생() {
+        assertThatThrownBy(() -> new Money("가나다"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_1000원_단위가_아닌_경우_예외_발생() {
+        assertThatThrownBy(() -> new Money("1500"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_0원인_경우_예외_발생() {
+        assertThatThrownBy(() -> new Money("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

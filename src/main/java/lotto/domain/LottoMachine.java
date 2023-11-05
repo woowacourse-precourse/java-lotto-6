6e1d62cd87fce1adfe3f16254c6534b466c64d71
mumpuris <hst990124@naver.com>
2023-11-05 22:43:42 +0900

    앞에서 사용한 Validation 로직 추가 추후 1번 더 반복 사용시 해당 로직 Validation Class 추가 고민

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     private List<Integer> winningNumbers; //매주 토요일 오후 8시 35분
     private int bonusNum;
@@ -18,6 +21,7 @@ public class LottoMachine {
     }
 
     public void setWinningNumbers(List<Integer> winningNumbers) {
+        validateLottoNumbers(winningNumbers);
         this.winningNumbers = winningNumbers;
     }
 
@@ -26,6 +30,7 @@ public class LottoMachine {
     }
 
     public void setBonusNum(Integer bonusNum) {
+        validateBonusNumber(bonusNum);
         this.bonusNum = bonusNum;
     }
 
@@ -54,6 +59,28 @@ public class LottoMachine {
         }
         if (amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    private void validateLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        }
+        for (int number : numbers) {
+            validateNumberInRange(number);
+        }
+    }
+
+    private void validateBonusNumber(int number) {
+        validateNumberInRange(number);
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException("보너스 번호가 당첨 번호와 중복됩니다.");
+        }
+    }
+
+    private void validateNumberInRange(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이여야 합니다.");
         }
     }
 }

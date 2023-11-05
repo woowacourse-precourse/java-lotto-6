cdaf36901f447a68b27dbc8c637de63a7fd62481
heejae <hjhjhjhj3770@naver.com>
2023-11-05 10:52:17 +0900

    Feat(Lotto): 산 로또의 사이즈가 6이고 1~45사이인지 체크
    validateSize() - 사이즈 체크
    validateRange() - 범위 체크

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,30 @@
 package lotto.domain;
 
+import static lotto.settings.ErrorMessage.INVALID_COUNT;
+import static lotto.settings.ErrorMessage.INVALID_RANGE;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if(number<1 || number>45){
+                throw new IllegalArgumentException(INVALID_RANGE.getMessage());
+            }
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_COUNT.getMessage());
         }
     }
 
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -6,19 +6,27 @@ import static lotto.settings.LottoRange.MIN_VALUE;
 import static lotto.settings.LottoRange.SIZE;
 
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.view.View;
 
 public class LottoService {
 
     public static void createRandomNumber(int purchaseCount){
         while(purchaseCount>0){
-            List<Integer> numbers = pickUniqueNumbersInRange(MIN_VALUE.getNumber(), MAX_VALUE.getNumber(),
-                    SIZE.getNumber());
-            String[] array = numbers.stream().map(String::valueOf).toArray(String[]::new);
+            Lotto lotto = new Lotto(getRandomNumbers());
+
+            String[] array = getRandomNumbers().stream().map(String::valueOf).toArray(String[]::new);
             String join = String.join(",", array);
             System.out.println("["+join+"]");
             purchaseCount --;
         }
+    }
+
+    private static List<Integer> getRandomNumbers() {
+        return pickUniqueNumbersInRange(
+                MIN_VALUE.getNumber(),
+                MAX_VALUE.getNumber(),
+                SIZE.getNumber());
     }
 
     public static void createWinningNumber(){

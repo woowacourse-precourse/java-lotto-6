d7b8135604ba2ef065b41dfbb4d449b9df68dfa2
Sanghwi Lee <sanghwi47@naver.com>
2023-11-05 15:29:44 +0900

    feat(Controller) NumberMatching에서 구현한 로직에 따라 OutputView에서 출력 할 수 있는 기능을 수행하도록 코드를 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,25 +1,29 @@
 package lotto.controller;
 
-import lotto.model.BonusNumber;
-import lotto.model.LottoTicket;
-import lotto.model.WinningNumbers;
+import lotto.model.*;
 import lotto.service.LottoService;
+import lotto.service.NumberMatchingService;
+import lotto.service.EarningRateService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-import lotto.model.Lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Controller {
-    InputView inputView;
+//    InputView inputView;
     LottoService lottoService;
-
-    int ticketCount;
+    private List<List<Integer>> allYourNums;  //////////////
+    private List<Integer> winningNumbers; /////////////
+    private int ticketCount;
+    private int bonusNumber;       /////////
     private static final String ERROR = "[ERROR] ";
 
     public Controller() {
-        this.inputView = new InputView();
+//        this.inputView = new InputView();
         this.lottoService = new LottoService();
     }
 
@@ -29,6 +33,8 @@ public class Controller {
         lottoGameSetting();
         WinningNumbers winningNumbers = winningNumberSetting();
         BonusNumber bonusNumber = bonusNumberSetting(winningNumbers);
+        showSystemMessage();
+        showWinningResults();
     }
 
     private void lottoTicketSetting() {
@@ -50,13 +56,24 @@ public class Controller {
 
     private void lottoGameSetting() {
         List<Lotto> lottoNumber = lottoService.purchaseLottoTickets(ticketCount);
+        this.allYourNums = convertToNumbersList(lottoNumber);                       ///////////
         OutputView.displayLottoNumber(lottoNumber);
     }
 
-    private WinningNumbers winningNumberSetting() {
+    private List<List<Integer>> convertToNumbersList(List<Lotto> lottoTickets) {
+        // Lotto 객체들을 번호의 리스트로 변환하는 로직
+        List<List<Integer>> numbersList = new ArrayList<>();
+        for (Lotto lotto : lottoTickets) {
+            numbersList.add(lotto.getNumbers());
+        }
+        return numbersList;
+    }
+
+    private  WinningNumbers winningNumberSetting() {
         while (true) {
             try {
                 String[] winningNumbers = InputView.inputWinningNumber();
+                this.winningNumbers = convertToIntegerList(winningNumbers);       ////////////
                 return new WinningNumbers(winningNumbers);
             } catch (IllegalArgumentException e) {
                 System.out.println(ERROR + e.getMessage());
@@ -64,15 +81,40 @@ public class Controller {
         }
     }
 
+    private List<Integer> convertToIntegerList(String[] numberStrings) {            /////////////
+        return Arrays.stream(numberStrings)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
     private BonusNumber bonusNumberSetting(WinningNumbers winningNumbers) {
         while (true) {
             try {
                 String bonusNumberString = InputView.inputBonusNumber();
+                this.bonusNumber = Integer.parseInt(bonusNumberString); ///////////
                 return new BonusNumber(bonusNumberString, winningNumbers);
             } catch (IllegalArgumentException e) {
                 System.out.println(ERROR + e.getMessage());
                 // 유효하지 않은 입력에 대한 처리
             }
         }
+    }
+    private void showSystemMessage() {
+        OutputView.displaySystemMessage();
+    }
+
+    private void showWinningResults() {
+        // NumberMatchingService의 객체 생성 또는 Controller에 멤버 변수로 선언해야 합니다.
+        NumberMatchingService numberMatchingService = new NumberMatchingService();
+
+        // 당첨 결과 계산
+        EnumMap<LottoPrize, Integer> winCount = numberMatchingService.calculateResults(allYourNums, winningNumbers, bonusNumber);
+
+        // 결과 출력
+        OutputView.displayLottoResult(winCount);
+
+        EarningRateService earningRateService = new EarningRateService();
+        double earningsRate = earningRateService.calculateEarningsRate(winCount, ticketCount);
+        OutputView.displayEarningsRate(earningsRate);
     }
 }

08cb134768fcf42ce20f63da35b99961985eff61
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 00:44:12 +0900

    test(Customer): 로또 구매 기능 테스트 함수 구현

--- src/test/java/lotto/domain/CustomerTest.java
+++ src/test/java/lotto/domain/CustomerTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import lotto.util.LottoNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CustomerTest {
-  
+    @DisplayName("입력한 금액만큼 로또를 구매한다.")
+    @Test
+    void purchaseLottos() {
+        // given
+        Cash cash = new Cash(3000);
+        LottoSeller lottoSeller = new LottoSeller(new LottoNumberGenerator());
+        Customer customer = new Customer(cash, lottoSeller);
+
+        // when
+        customer.purchaseLottos();
+
+        // than
+        assertThat(customer.getLottos()).isEqualTo(3);
+    }
 }

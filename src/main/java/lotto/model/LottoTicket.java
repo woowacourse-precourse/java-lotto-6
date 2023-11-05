33b48a4e3062ed260378c135d10bd723ece93181
SunHyeok <cghhld@naver.com>
2023-11-05 14:47:53 +0900

    feat: 로또 티켓 관리 기능

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+import lotto.Lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicket {
+    private List<Lotto> tickets;
+
+    public LottoTicket(int numberOfTickets) {
+        tickets = new ArrayList<>();
+        for (int i = 0; i < numberOfTickets; i++) {
+            List<Integer> lottoNumbers = generateRandomLottoNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            tickets.add(lotto);
+        }
+    }
+
+    public List<Lotto> getTickets() {
+        return tickets;
+    }
+
+    private List<Integer> generateRandomLottoNumbers() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumbers;
+    }
+}
+
+

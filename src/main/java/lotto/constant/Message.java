df1de31fafc5c1b9175a323e7dd7e7550b022208
Cycrypto <jh01love@naver.com>
2023-11-05 22:08:37 +0900

    feat: 당첨 통계 메시지 상수화

--- src/main/java/lotto/constant/Message.java
+++ src/main/java/lotto/constant/Message.java
@@ -4,7 +4,8 @@ public enum Message {
     INPUT_BUDGET("구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBER("당첨번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    PURCHASED_LOTTO_COUNT_MESSAGE("개를 구매했습니다.")
+    PURCHASED_LOTTO_COUNT_MESSAGE("개를 구매했습니다."),
+    RESULT_MESSAGE("당첨 통계")
     ;
 
     private final String message;

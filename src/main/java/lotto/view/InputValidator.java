f335caf3454aa6de46e7519cda400273e39be18a
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-05 17:47:50 +0900

    feat(InputView) : 중복된 당첨 번호 입력 시, 예외 처리
    1. 중복된 당첨 번호 입력 시 예외처리함

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
+import lotto.exception.DuplicateInputException;
 import lotto.exception.InvalidInputException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
     private static final String INVALID_INPUT_MESSAGE = "[ERROR] 금액에는 정수를 입력해주세요.";
@@ -14,5 +20,16 @@ public class InputValidator {
 
     private boolean isNumeric(String inputMoney) {
         return inputMoney.matches(NUMERIC);
+    }
+
+    public void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (isDuplicate(winningNumbers)) {
+            throw new DuplicateInputException();
+        }
+    }
+
+    private boolean isDuplicate(List<Integer> winningNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winningNumbers);
+        return uniqueNumbers.size() < winningNumbers.size();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.DuplicateInputException;
 import lotto.exception.InvalidInputException;
 
 import java.util.Arrays;
@@ -31,12 +32,19 @@ public class InputView {
 
 
     public List<Integer> inputWinningNumbers() {
-        String inputWinningNumbers = Console.readLine();
-        List<Integer> winningNumbers = Arrays.stream(inputWinningNumbers.split(SPLIT_STANDARD))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        while (true) {
+            try {
+                String inputWinningNumbers = Console.readLine();
+                List<Integer> winningNumbers = Arrays.stream(inputWinningNumbers.split(SPLIT_STANDARD))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                inputValidator.validateWinningNumbers(winningNumbers);
 
-        return winningNumbers;
+                return winningNumbers;
+            } catch (DuplicateInputException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public Integer inputBonusNumber() {

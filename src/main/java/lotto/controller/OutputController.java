8141137d0ce89b0c08acc5561889469477cf5a34
heiler <threepebbles@naver.com>
2023-11-05 20:22:38 +0900

    Refactor: model을 이용해 사용자에게 보여질 String 텍스트로 변환하는 기능을 OutputView에서 OutputController로 이동

--- src/main/java/lotto/controller/OutputController.java
+++ src/main/java/lotto/controller/OutputController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
 import java.util.List;
+import java.util.Map;
+import lotto.constant.LottoPrize;
 import lotto.model.Lotto;
 import lotto.model.WinningStatistics;
 import lotto.view.OutputView;
@@ -13,12 +15,52 @@ public class OutputController {
     }
 
     public void printLottoTickets(List<Lotto> lottoTickets) {
-        outputView.updateLottoTicketsUI(lottoTickets);
-        outputView.renderLottoTicketsUI();
+        String countText = creatLottoTicketsCountText(lottoTickets);
+        String ticketsText = createLottoTicketsText(lottoTickets);
+
+        outputView.updateLottoTicketsCountText(countText);
+        outputView.updateLottoTicketsText(ticketsText);
+        outputView.renderLottoTicketsCountText();
+        outputView.renderLottoTicketsText();
     }
 
     public void printWinningStatistics(WinningStatistics winningStatistics) {
-        outputView.updateWinningStatisticsUI(winningStatistics);
-        outputView.renderWinningStatisticsUI();
+        String statisticsTest = createWinningStatisticsText(winningStatistics);
+        String rateOfReturnText = createRateOfReturnText(winningStatistics);
+
+        outputView.updateStatisticsText(statisticsTest);
+        outputView.updateRateOfReturnText(rateOfReturnText);
+        outputView.renderStatisticsText();
+        outputView.renderRateOfReturnText();
+    }
+
+
+    public String creatLottoTicketsCountText(List<Lotto> lottoTickets) {
+        return String.valueOf(lottoTickets.size());
+    }
+
+    public String createLottoTicketsText(List<Lotto> lottoTickets) {
+        StringBuilder sb = new StringBuilder();
+        lottoTickets.forEach(lotto -> {
+            sb.append(lotto.toString()).append("\n");
+        });
+        return sb.toString();
+    }
+
+    public String createWinningStatisticsText(WinningStatistics winningStatistics) {
+        Map<LottoPrize, Integer> prizeCounter = winningStatistics.getPrizeCounter();
+        StringBuilder sb = new StringBuilder();
+        for (LottoPrize lottoPrize : LottoPrize.values()) {
+            if (lottoPrize == LottoPrize.NOTHING) {
+                continue;
+            }
+            int count = prizeCounter.get(lottoPrize);
+            sb.append(String.format(lottoPrize.getStatisticsFormat(), count)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String createRateOfReturnText(WinningStatistics winningStatistics) {
+        return String.valueOf(winningStatistics.getRateOfReturn());
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,76 +1,53 @@
 package lotto.view;
 
-import java.util.List;
-import java.util.Map;
-import lotto.constant.LottoPrize;
-import lotto.model.Lotto;
-import lotto.model.WinningStatistics;
-
 public class OutputView {
-    public static final String LOTTO_TICKETS_COUNT_FORMAT = "%d개를 구매했습니다.";
-    public static final String STATISTICS_HEADER = "당첨 통계\n---\n";
-    public static final String RATE_OF_RETURN_FORMAT = "총 수익률은 %.1f%%입니다.";
+    public static final String LOTTO_TICKETS_COUNT_FORMAT = "%s개를 구매했습니다.";
+    public static final String LOTTO_TICKETS_FORMAT = "%s";
+    public static final String STATISTICS_FORMAT = "당첨 통계\n---\n%s";
+    public static final String RATE_OF_RETURN_FORMAT = "총 수익률은 %s%%입니다.";
 
     private String lottoTicketsCountText;
     private String lottoTicketsText;
-    private String statisticsText;
+    private String winningStatisticsText;
     private String rateOfReturnText;
 
     public OutputView() {
         // default text
         lottoTicketsCountText = LOTTO_TICKETS_COUNT_FORMAT;
-        lottoTicketsText = "";
-        statisticsText = STATISTICS_HEADER;
+        lottoTicketsText = LOTTO_TICKETS_FORMAT;
+        winningStatisticsText = STATISTICS_FORMAT;
         rateOfReturnText = RATE_OF_RETURN_FORMAT;
     }
 
-    public void renderLottoTicketsUI() {
-        System.out.println(lottoTicketsCountText);
+    public void renderLottoTicketsCountText() {
         System.out.println(lottoTicketsText);
     }
 
-    public void renderWinningStatisticsUI() {
-        System.out.println(statisticsText);
+    public void renderLottoTicketsText() {
+        System.out.println(lottoTicketsCountText);
+    }
+
+    public void renderStatisticsText() {
+        System.out.println(winningStatisticsText);
+    }
+
+    public void renderRateOfReturnText() {
         System.out.println(rateOfReturnText);
     }
 
-    public void updateLottoTicketsUI(List<Lotto> lottoTickets) {
-        lottoTicketsCountText = creatLottoTicketsCountText(lottoTickets);
-        lottoTicketsText = createLottoTicketsText(lottoTickets);
+    public void updateLottoTicketsCountText(String count) {
+        lottoTicketsCountText = String.format(LOTTO_TICKETS_COUNT_FORMAT, count);
     }
 
-    public void updateWinningStatisticsUI(WinningStatistics winningStatistics) {
-        statisticsText = createStatisticsText(winningStatistics);
-        rateOfReturnText = createRateOfReturnText(winningStatistics);
+    public void updateLottoTicketsText(String lottoTickets) {
+        lottoTicketsText = String.format(LOTTO_TICKETS_FORMAT, lottoTickets);
     }
 
-    public String createRateOfReturnText(WinningStatistics winningStatistics) {
-        return String.format(RATE_OF_RETURN_FORMAT, winningStatistics.getRateOfReturn());
+    public void updateStatisticsText(String winningStatistics) {
+        winningStatisticsText = String.format(STATISTICS_FORMAT, winningStatistics);
     }
 
-    public String creatLottoTicketsCountText(List<Lotto> lottoTickets) {
-        return String.format(LOTTO_TICKETS_COUNT_FORMAT, lottoTickets.size());
-    }
-
-    public String createLottoTicketsText(List<Lotto> lottoTickets) {
-        StringBuilder sb = new StringBuilder();
-        lottoTickets.forEach(lotto -> {
-            sb.append(lotto.toString()).append("\n");
-        });
-        return sb.toString();
-    }
-
-    public String createStatisticsText(WinningStatistics winningStatistics) {
-        Map<LottoPrize, Integer> prizeCounter = winningStatistics.getPrizeCounter();
-        StringBuilder sb = new StringBuilder();
-        sb.append(STATISTICS_HEADER);
-        for (LottoPrize lottoPrize : LottoPrize.values()) {
-            if (lottoPrize == LottoPrize.NOTHING) {
-                continue;
-            }
-            int count = prizeCounter.get(lottoPrize);
-            sb.append(String.format(lottoPrize.getStatisticsFormat(), count)).append("\n");
-        }
-        return sb.toString();
+    public void updateRateOfReturnText(String rateOfReturn) {
+        rateOfReturnText = String.format(RATE_OF_RETURN_FORMAT, rateOfReturn);
     }
 }

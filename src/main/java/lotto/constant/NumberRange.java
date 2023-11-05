a03095e25494dcb4c865523b0b348f6085e8b1c2
hgh1472 <hgh1472@naver.com>
2023-11-06 02:07:01 +0900

    feat : enum 로또 번호 갯수 추가

--- src/main/java/lotto/constant/NumberRange.java
+++ src/main/java/lotto/constant/NumberRange.java
@@ -2,12 +2,15 @@ package lotto.constant;
 
 public enum NumberRange {
     LOWEST_NUMBER(1),
-    HIGHEST_NUMBER(45);
+    HIGHEST_NUMBER(45),
+    LOTTO_NUMBER_COUNT(6);
 
     private final int number;
+
     NumberRange(int number) {
         this.number = number;
     }
+
     public int getRangeNumber() {
         return number;
     }

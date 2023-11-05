643855f8796cf8c9c0bb28668006a4b3615b0835
Yoon-HaeJun <0681hj@naver.com>
2023-11-06 01:50:59 +0900

    로또 구매 금액 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.BuyPrice;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BuyPrice buyPrice = new BuyPrice();
+        String input = Console.readLine();
+        int price = Integer.parseInt(input);
+        price = BuyPrice.buylotto(price);
+        System.out.println(price);
     }
 }
--- src/main/java/lotto/domain/BuyPrice.java
+++ src/main/java/lotto/domain/BuyPrice.java
@@ -1,4 +1,7 @@
 package lotto.domain;
 
 public class BuyPrice {
+    static public int buylotto(int price) {
+        return price / 1000;
+    }
 }

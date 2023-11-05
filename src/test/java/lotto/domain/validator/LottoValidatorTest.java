8bc947bb62ddc7b7089c40aa88ec0335ea350c3b
minisyu <iswater.kim@gmail.com>
2023-11-06 03:02:53 +0900

    test: LottoValidator 테스트 코드 작성

---
+++ src/test/java/lotto/domain/validator/LottoValidatorTest.java
@@ -0,0 +1,91 @@
+package lotto.domain.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.lotto.BonusNumber;
+import lotto.domain.lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoValidatorTest {
+
+    @DisplayName("로또 번호가 6개가 아닌 경우 예외 발생")
+    @Test
+    void should_Throw_Exception_When_Numbers_Size_Is_Not_Six() {
+        // given
+        List<Integer> numbers1 = List.of(1, 2, 4);
+        List<Integer> numbers2 = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        // when
+        // then
+        assertThatThrownBy(() ->
+                LottoValidator.verifyNumbersSize(numbers1))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() ->
+                LottoValidator.verifyNumbersSize(numbers2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,1,2,3,4,5", "1,2,3,4,5,5", "1,1,2,2,3,3"})
+    void should_Throw_Exception_When_Numbers_Contain_Duplicates(String str) {
+        // given
+        List<Integer> numbers = Arrays.stream(str.split(","))
+                .map(Integer::parseInt)
+                .toList();
+
+        // when
+        // then
+        assertThatThrownBy(() ->
+                LottoValidator.verifyNoDuplication(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1에서 45 사이의 범위를 벗어나는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1, 2, 40, 45, 34, 23", "50, 60, 70, 80, 90, 100"})
+    void should_Throw_Exception_When_Number_Is_Out_Of_Range(String str) {
+        // given
+        List<Integer> numbers = Arrays.stream(str.split(","))
+                .map(Integer::parseInt)
+                .toList();
+        // when
+        // then
+        assertThatThrownBy(() ->
+                LottoValidator.verifyLottoNumberRange(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1에서 45 사이의 범위를 벗어나는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, 50, 100})
+    void should_Throw_Exception_When_BonusNumber_Is_Out_Of_Range(int number) {
+        // when
+        // then
+        assertThatThrownBy(() ->
+                LottoValidator.verifyRange(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복된 경우 예외 발생")
+    @Test
+    void should_Throw_Exception_When_BonusNumber_Is_Duplicate_With_WinningNumbers() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto winningNumbers = Lotto.from(numbers);
+
+        BonusNumber bonusNumber = BonusNumber.from(1);
+
+        // when
+        // then
+        assertThatThrownBy(() ->
+                LottoValidator.verifyBonusIsNotContainWinningNumbers(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

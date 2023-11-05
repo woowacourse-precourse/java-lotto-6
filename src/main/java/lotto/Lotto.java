65a72feba0d420976c291bed770f389cebf7c69b
km9404 <kmh940408@gmail.com>
2023-11-06 03:46:46 +0900

    feat (Lotto): 당첨 번호 중복 입력 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateDuplicatedNumber(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> validator = new HashSet<>();
+        validator.addAll(numbers);
+        if (validator.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
     // TODO: 추가 기능 구현
 }

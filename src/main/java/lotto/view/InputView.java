1671ce36013d5dbffea9a3bd48ad18233ea76d39
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-05 23:42:08 +0900

    Feat: inputView작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,23 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Constant;
+
+
+public class InputView {
+
+    public String inputPurchaseAmount(){
+        System.out.println(Constant.GET_PURCHASE_AMOUNT_TXT);
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber(){
+        System.out.println(Constant.GET_WINNING_NUMBER_TXT);
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber(){
+        System.out.println(Constant.GET_BONUS_NUMBER_TXT);
+        return Console.readLine();
+    }
 }

bcf00a3fe94de4801c379ae06b2939944bd77f8a
eunhee <dmsgml9903@gmail.com>
2023-11-05 23:42:32 +0900

    feat(UserLotteries): 실제 당첨 로또 번호와 비교하여 사용자가 당첨된 유형 별 개수를 구하는 기능 구현

--- src/main/java/lotto/model/UserLotteries.java
+++ src/main/java/lotto/model/UserLotteries.java
@@ -1,6 +1,10 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lotto.constants.LottoPrize;
 
 public class UserLotteries {
 
@@ -21,4 +25,37 @@ public class UserLotteries {
     public List<Lotto> getLotteries() {
         return lotteries;
     }
+
+    public Map<LottoPrize, Long> findWinningCount(WinningLotto winningLotto) {
+        List<LottoPrize> lottoPrizes = filterDrawnLotto(winningLotto);
+        Map<LottoPrize, Long> countLottoPrize = findCountLottoPrize(lottoPrizes);
+        putAllLottoPrizeCount(countLottoPrize);
+        return countLottoPrize;
+    }
+
+    private Map<LottoPrize, Long> findCountLottoPrize(List<LottoPrize> lottoPrizes) {
+        return lottoPrizes.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private List<LottoPrize> filterDrawnLotto(WinningLotto winningLotto) {
+        List<LottoPrize> lottoPrizes = lotteries.stream()
+                .filter(winningLotto::isDrawnLotto)
+                .map(winningLotto::findLottoPrizeType)
+                .toList();
+        return lottoPrizes;
+    }
+
+    private void putAllLottoPrizeCount(Map<LottoPrize, Long> winningCount) {
+        for (LottoPrize lottoPrize : LottoPrize.values()) {
+            putIfNotHaveLottoPrizeKey(winningCount, lottoPrize);
+        }
+    }
+
+    private void putIfNotHaveLottoPrizeKey(Map<LottoPrize, Long> winningCount, LottoPrize lottoPrize) {
+        if (!winningCount.containsKey(lottoPrize)) {
+            winningCount.put(lottoPrize, 0L);
+        }
+    }
+
 }

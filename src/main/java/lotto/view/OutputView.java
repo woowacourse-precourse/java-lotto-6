d96d2c7598fa340c1b7c49b16c70a16cf550fe1a
KangGeonyoung <kky6335@gmail.com>
2023-11-05 15:08:22 +0900

    feat(OutputView): 구매한 로또 개수 출력 - printPurchaseCount(int purchaseCount): 구매한 로또 개수 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import lotto.domain.model.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void printPurchaseCount(int purchaseCount) {
+        System.out.println();
+        System.out.println(purchaseCount + "개를 구매했습니다.");
+    }
+
+}

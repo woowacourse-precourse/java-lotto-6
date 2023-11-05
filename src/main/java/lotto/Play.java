b8f209f53e50001536e5f9c24b07b11dee5a319f
daeni-dang <de021@naver.com>
2023-11-05 22:03:19 +0900

    feat: 당첨 번호 입력받기

--- src/main/java/lotto/Play.java
+++ src/main/java/lotto/Play.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Play {
 
     private final int lottoCnt;   // 구입한 로또 개수
+    private final int lottoNumber = 6;  // 로또는 숫자 6개를 가진다
 
     public Play(int cost) {
         // 구매 금액이 1000원으로 나누어 떨어지지 않는 경우 예외 발생
@@ -18,10 +22,59 @@ public class Play {
         this.lottoCnt = cost / 1000;
     }
 
+    // 각 로또의 6자리 숫자 랜덤 생성
     List<Integer> getLottoNums() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(numbers);
+        Collections.sort(numbers);  // 오름차순으로 정렬
         return numbers;
+    }
+
+    // 당첨 번호 입력받기
+    List<Integer> getAnswerNums() {
+        while (true) {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            List<String> nums = Arrays.asList(Console.readLine().split(","));
+            if (!checkIs6Num(nums))    // 6개의 숫자가 입력되었는지 확인
+                continue;
+            List<Integer> answerNums = checkEachNumRange(nums); // 각 숫자가 1~45 사이인지 확인
+            if (answerNums != null)
+                return answerNums;
+        }
+    }
+
+    // 6개의 숫자가 입력되었는지 확인
+    boolean checkIs6Num(List<String> nums) {
+        try {
+            if (nums.size() != lottoNumber) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또 번호를 6개 입력해주세요.");
+        }
+        return false;
+    }
+
+    void checkNumRange(int num) {
+        if (num <= 0 || num > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 숫자가 1~45범위인지 확인
+    List<Integer> checkEachNumRange(List<String> nums) {
+        try {
+            List<Integer> result = new ArrayList<>();
+            for (String n : nums) {
+                int eachNum = Integer.parseInt(n);
+                checkNumRange(eachNum);
+                result.add(Integer.parseInt(n));
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return null;
     }
 
     public void startGame() {
@@ -32,5 +85,6 @@ public class Play {
             System.out.println(numbers);
             lottos.add(new Lotto(numbers));
         }
+        List<Integer> answerNums = getAnswerNums();
     }
 }

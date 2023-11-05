e371f28f0a06cbc9f68e9c08d4c5559b84ebd584
gyungchan Jo <namejgc@naver.com>
2023-11-05 22:26:59 +0900

    refactor(PrizeMoneyRepository): 당첨 순위별 상금 목록 추가
    1~5등이 아닌 순위의 상금 추가

--- src/main/java/lotto/repository/PrizeMoneyRepository.java
+++ src/main/java/lotto/repository/PrizeMoneyRepository.java
@@ -6,14 +6,22 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class PrizeMoneyRepository {
+    private static final int FIRST_PRIZE_MONEY = 2000000000;
+    private static final int SECOND_PRIZE_MONEY = 30000000;
+    private static final int THIRD_PRIZE_MONEY = 1500000;
+    private static final int FOURTH_PRIZE_MONEY = 50000;
+    private static final int FIFTH_PRIZE_MONEY = 5000;
+    private static final int NOTHING_PRIZE_MONEY = 0;
+
     private static Map<LottoRanking, Integer> store = new EnumMap<>(LottoRanking.class);
 
     static {
-        store.put(LottoRanking.FIRST, 2000000000);
-        store.put(LottoRanking.SECOND, 30000000);
-        store.put(LottoRanking.THIRD, 1500000);
-        store.put(LottoRanking.FOURTH, 50000);
-        store.put(LottoRanking.FIFTH, 5000);
+        store.put(LottoRanking.FIRST, FIRST_PRIZE_MONEY);
+        store.put(LottoRanking.SECOND, SECOND_PRIZE_MONEY);
+        store.put(LottoRanking.THIRD, THIRD_PRIZE_MONEY);
+        store.put(LottoRanking.FOURTH, FOURTH_PRIZE_MONEY);
+        store.put(LottoRanking.FIFTH, FIFTH_PRIZE_MONEY);
+        store.put(LottoRanking.NOTHING, NOTHING_PRIZE_MONEY);
     }
 
     public int findByLottoRanking(LottoRanking lottoRanking) {

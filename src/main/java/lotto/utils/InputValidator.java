0335543d7efde7c0a46ca6176b43d36ff597f274
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-06 02:57:01 +0900

    feat: 당첨번호 6개 입력값에 대한 예외처리 구현
    [ERROR] 숫자가 아닌 값이 포함되어 있음
    [ERROR] 1부터 45까지 범위를 벗어난 숫자 포함
    [ERROR] 중복된 숫자가 포함됨
    [ERROR] 보너스 번호를 제외한 당첨 번호 개수가 아닌 경우

---
+++ src/main/java/lotto/utils/InputValidator.java
@@ -0,0 +1,40 @@
+package lotto.utils;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputValidator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int WINNING_NUMBERS_COUNT = 6;
+
+    public int validateNonNumeric(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+        }
+    }
+
+    public void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 1에서 45 사이어야 합니다.");
+            }
+        }
+    }
+
+    public void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
+
+    public void validateWinningNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != WINNING_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호의 개수를 만족시키지 못합니다.");
+        }
+    }
+}

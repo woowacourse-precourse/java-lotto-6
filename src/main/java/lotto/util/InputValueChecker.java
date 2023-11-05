c7e9d6f4fb61e6b67fb18d7ff83ed8c2999a6dd9
minsub <chrkb1569@naver.com>
2023-11-06 01:50:12 +0900

    feat : 사용자로부터 입력 받은 값의 유효성을 판단하기 위한 클래스 생성

---
+++ src/main/java/lotto/util/InputValueChecker.java
@@ -0,0 +1,62 @@
+package lotto.util;
+
+import lotto.exception.NotValidInputException;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static lotto.exception.GameExceptionMessage.*;
+import static lotto.util.GameConstant.*;
+
+public class InputValueChecker {
+    private final String REGEXP_NUMBER_PATTERN = "\\d{1,}"; // 숫자만 입력 가능하다.
+    private final String REGEXP_COST_PATTERN = "^.*0{3,}$"; // 1000 단위의 수만 입력 가능하다.
+
+    // 사용자 로또 구매 금액의 유효성을 판단한다.
+    public void checkCostValidation(String cost) throws NotValidInputException {
+        checkNumberValidation(cost);
+        checkFormatValidation(cost);
+    }
+
+    // 사용자가 입력하는 당첨 번호의 유효성을 판단한다.
+    public void checkWinnerNumberValidation(List<String> winnerNumber) throws NotValidInputException {
+        checkLengthValidation(winnerNumber);
+        for(String number : winnerNumber) {
+            checkLottoNumberValidation(number);
+        }
+    }
+
+    // 사용자가 입력하는 보너스 번호의 유효성을 판단한다.
+    public void checkLottoNumberValidation(String bonusNumber) throws NotValidInputException {
+        checkNumberValidation(bonusNumber);
+        checkRangeValidation(bonusNumber);
+    }
+
+    // 사용자로부터 입력 받은 값이 숫자가 아닐 경우, 오류를 반환한다.
+    private void checkNumberValidation(String inputValue) {
+        if(!Pattern.matches(REGEXP_NUMBER_PATTERN, inputValue)) {
+            throw new NotValidInputException(INPUT_VALUE_IS_NOT_NUMBER.getMessage());
+        }
+    }
+
+    // 사용자로부터 입력 받은 값이 1000 단위가 아닐 경우, 오류를 반환한다.
+    private void checkFormatValidation(String inputValue) {
+        if(!Pattern.matches(REGEXP_COST_PATTERN, inputValue)) {
+            throw new NotValidInputException(INPUT_VALUE_NOT_VALID_FORMAT.getMessage());
+        }
+    }
+
+    // 사용자가 입력한 당첨 번호의 길이가 유효하지 않을 경우, 오류를 반환한다.
+    private void checkLengthValidation(List<String> inputValue) {
+        if(inputValue.size() != CONSTANT_LOTTO_LENGTH.getConstant())
+            throw new NotValidInputException(INPUT_VALUE_NOT_VALID_LENGTH.getMessage());
+    }
+
+    // 사용자로부터 입력 받은 값이 유효한 로또 번호가 아닐 경우, 오류를 반환한다.
+    private void checkRangeValidation(String inputValue) {
+        int lottoValue = Integer.parseInt(inputValue);
+
+        if(lottoValue < CONSTANT_LOTTO_MIN_NUMBER.getConstant() || lottoValue > CONSTANT_LOTTO_MAX_NUMBER.getConstant())
+            throw new NotValidInputException(INPUT_VALUE_NOT_VALID_RANGE.getMessage());
+    }
+}

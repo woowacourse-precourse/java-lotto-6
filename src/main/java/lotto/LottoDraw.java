2b5073850c9eeadf83ccacb85c7bdf9826ef36be
Chenowoo <cjsgusdn14@naver.com>
2023-11-06 02:13:46 +0900

    feat(LottoDraw): 통계 결과를 계산하고 출력하는 기능 구현  - 입력받은 값과 로또 번호로 통계 결과를 계산  - 계산된 통계 결과를 출력

--- src/main/java/lotto/LottoDraw.java
+++ src/main/java/lotto/LottoDraw.java
@@ -1,11 +1,20 @@
 package lotto;
 
 import lotto.domain.*;
+import lotto.service.PrintUtil;
+
+import java.util.*;
 
 public class LottoDraw {
+    private final List<String> lottoPlacePrice = new ArrayList<>(Arrays.asList("5000", "50000", "1500000", "30000000", "2000000000"));
+
+    Map<Integer, Map<String, Integer>> lottoPrizeSummary;
 
     LottoNumber lottoNumber = new LottoNumber();
     LottoNumbers lottoNumbers = new LottoNumbers();
+    LottoStatistics lottoStatistics = new LottoStatistics();
+    LottoPrize lottoPrize = new LottoPrize();
+    PrintUtil printUtil = new PrintUtil();
 
     public void startLottoDraw() {
 
@@ -13,7 +22,7 @@ public class LottoDraw {
         Lotto winnerNumbers = pickWinnerNumbers();
         int bonusNumber = pickBonusNumber(winnerNumbers);
 
-
+        printSummarizeWithLottoNumbers(lottoNumbers, winnerNumbers, bonusNumber);
 
     }
 
@@ -34,5 +43,59 @@ public class LottoDraw {
         return lottoNumber.inputBonusNumber(lotto);
     }
 
+    public Map<String, Integer> printSummarizeWithLottoNumbers(Lotto[] lottoNumbers, Lotto winnerNumbers, int bonusNumber) {
+        lottoPrizeSummary = lottoPrize.initLottoPrize();
+        Map<String, Integer> summary = summarizeLottoResults(lottoNumbers, winnerNumbers, bonusNumber);
+        printLottoResults();
+        return summary;
+    }
 
+    public Map<String, Integer> summarizeLottoResults(Lotto[] lottoNumbers, Lotto winnerNumbers, int bonusNumber) {
+        Map<String, Integer> summary = innerMap();
+        for (int i = 0; i < lottoNumbers.length; i++) {
+            int winCount = lottoStatistics.compareLottoNumbersWithWinnerNumbers(lottoNumbers[i], winnerNumbers);
+            String checkBonus = lottoStatistics.compareLottoNumbersWithBonusNumber(lottoNumbers[i], bonusNumber);
+
+            if (winCount < 3) continue;
+            if (winCount != 5) checkBonus = "NoBonus";
+
+            String getKey = winCount + checkBonus;
+            summary.put(getKey, summary.getOrDefault(getKey, 0) + 1);
+            lottoPrizeSummary.put(winCount, summary);
+        }
+        return summary;
+    }
+
+    public void printLottoResults() {
+        printUtil.printWinnerStatistics();
+        int lottoPlacePriceIdx = 0;
+        for (int winCount = 3; winCount <= 6; winCount++, lottoPlacePriceIdx++) {
+
+            if (winCount == 5) {
+                int count = lottoPrizeSummary.get(winCount).get("5NoBonus");
+                System.out.println(winCount + "개 일치 (" + lottoPlacePrice.get(lottoPlacePriceIdx).replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",") + "원) - " + count + "개");
+                lottoPlacePriceIdx++;
+                count = lottoPrizeSummary.get(winCount).get("5Bonus");
+                System.out.println(winCount + "개 일치, 보너스 볼 일치 (" + lottoPlacePrice.get(lottoPlacePriceIdx).replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",") + "원) - " + count + "개");
+                continue;
+            }
+            String str = winCount + "NoBonus";
+            int count = lottoPrizeSummary.get(winCount).get(str);
+            System.out.println(winCount + "개 일치 (" + lottoPlacePrice.get(lottoPlacePriceIdx).replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",") + "원) - " + count + "개");
+        }
+    }
+
+    public Map<String, Integer> innerMap() {
+        Map<String, Integer> innerMap = new HashMap<>();
+
+        for (int winCount = 3; winCount <= 6; winCount++) {
+            String str = winCount + "NoBonus";
+            innerMap.put(str, 0);
+            if (winCount == 5) {
+                str = winCount + "Bonus";
+                innerMap.put(str, 0);
+            }
+        }
+        return innerMap;
+    }
 }

3c61c9ebc76886f687ca99ada464863fe483c0e9
chae401 <chae401@sju.ac.kr>
2023-11-05 15:38:32 +0900

    refactor(Ranking): add function as a field of Enum constant
    if 로 관리하던 조건을 열거형 상수의 필드에 추가해 true인 열거형 상수를 리턴하도록 함.

--- src/main/java/lotto/model/Ranking.java
+++ src/main/java/lotto/model/Ranking.java
@@ -1,32 +1,31 @@
 package lotto.model;
 
+import java.util.Objects;
+import java.util.function.Function;
+
 public enum Ranking {
-    FAIL("", 0),
-    FIFTH("3개 일치 (5,000원) - ", 5000),
-    FOURTH("4개 일치 (50,000원) - ", 50000),
-    THIRD("5개 일치 (1,500,000원) - ", 1500000),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
-    FIRST("6개 일치 (2,000,000,000원) - ", 2000000000);
+    FAIL("", 0, (correct, bonus) -> false),
+    FIFTH("3개 일치 (5,000원) - ", 5000, (correct, bonus) -> correct == 3),
+    FOURTH("4개 일치 (50,000원) - ", 50000, (correct, bonus) -> correct == 4),
+    THIRD("5개 일치 (1,500,000원) - ", 1500000, (correct, bonus) -> correct == 5 && !bonus),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000, (correct, bonus) -> correct == 5 && bonus),
+    FIRST("6개 일치 (2,000,000,000원) - ", 2000000000, (correct, bonus) ->  correct== 6);
 
     private String condition;
     private int profit;
+    private RankingFunction function;
 
-    private Ranking(String condition, int profit) {
+    private Ranking(String condition, int profit, RankingFunction function) {
         this.condition = condition;
         this.profit = profit;
+        this.function = function;
     }
 
     public static Ranking of(int correct, boolean isBonus) {
-        if (correct == 6) {
-            return FIRST;
-        } else if (correct == 5 && isBonus) {
-            return SECOND;
-        } else if (correct == 5) {
-            return THIRD;
-        } else if (correct == 4) {
-            return FOURTH;
-        } else if (correct == 3) {
-            return FIFTH;
+        for (Ranking ranking : Ranking.values()) {
+            if (ranking.function.apply(correct, isBonus)) {
+                return ranking;
+            }
         }
         return FAIL;
     }
@@ -38,4 +37,8 @@ public enum Ranking {
     public String getCondition() {
         return condition;
     }
+}
+
+interface RankingFunction {
+    boolean apply(int correct, boolean isBonus);
 }

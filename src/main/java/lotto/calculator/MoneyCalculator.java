c5a8387a398c0a207ab012551338e5253f60505e
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-05 17:24:06 +0900

    Revert "refactor : MoneyCalculator.calculateRateOfReturn"
    This reverts commit e59f76b995ba2051557989624bd39d833c22772b.

--- src/main/java/lotto/calculator/MoneyCalculator.java
+++ src/main/java/lotto/calculator/MoneyCalculator.java
@@ -30,9 +30,9 @@ public class MoneyCalculator{
     public void calculateRateOfReturn(Integer amountQuantity){
         int investmentMoney = amountQuantity * MIN_VALUE.getAmount();
         BigDecimal findInvestmentMoney = new BigDecimal(investmentMoney);
-        BigDecimal findTotalMoney = new BigDecimal(this.totalMoney);
-        this.rateOfReturn = findTotalMoney.divide(findInvestmentMoney)
-                .multiply(BigDecimal.valueOf(100)).setScale(1,RoundingMode.HALF_UP);
+        BigDecimal totalMoney = new BigDecimal(this.totalMoney);
+        BigDecimal profit = totalMoney.subtract(findInvestmentMoney);
+        this.rateOfReturn = profit.divide(findInvestmentMoney, 1, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100));
     }
 
     public Integer getTotalMoney() {

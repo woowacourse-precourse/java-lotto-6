d5afd43fd536f63bb6bec302beaf44b1dc253e5c
ghdeo <goldencorn7@naver.com>
2023-11-05 22:22:15 +0900

    feat: Add checking duplicated number method

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.utils.Validate;
+
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +16,8 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        Validate.hasDuplicateNumbers(this.numbers);
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/utils/Validate.java
+++ src/main/java/lotto/utils/Validate.java
@@ -1,5 +1,9 @@
 package lotto.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validate {
     public static int purchaseAmount(String userInput) {
         try {
@@ -26,6 +30,17 @@ public class Validate {
             return number;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력 하세요.");
+        }
+    }
+
+    public static void hasDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>();
+        for (Integer number : numbers) {
+            if (numberSet.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 숫자를 중복 되지 않게 입력 하세요.");
+            }
+
+            numberSet.add(number);
         }
     }
 

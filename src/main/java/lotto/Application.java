cddb232e1a2be6f5209e94faeace723457440285
dlswns <dlswns8679@naver.com>
2023-11-05 19:51:18 +0900

    feat- 로또 게임 개발

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,105 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.Lotto.*;
+
 public class Application {
     public static void main(String[] args) {
+        int bonusNumber = 0;
         // TODO: 프로그램 구현
+        // 구입 금액 입력
+        int purchaseAmount = 0;
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                purchaseAmount = Integer.parseInt(readLine());
+                if (purchaseAmount % 1000 == 0) {
+                    break;
+                }
+                System.out.println("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+            }
+            catch(NumberFormatException e){
+                System.out.println("[ERROR] 잘못된 입력입니다. 숫자를 입력하세요.");
+            }
+        }
+        // 로또 발행
+        int numberOfLotto = purchaseAmount / 1000;
+        List<List<Integer>> lottoList = new ArrayList<>();
+        for (int i = 0; i < numberOfLotto; i++) {
+            lottoList.add(generateRandomLotto());
+        }
+
+        //구매했다고 알림
+        System.out.println("\n" + numberOfLotto + "개를 구매했습니다.");
+        for (int i = 0; i < numberOfLotto; i++) {
+            System.out.print("[");
+            for (int j = 0; j < 5; j++) {
+                System.out.print(lottoList.get(i).get(j) + ", ");
+            }
+            System.out.println(lottoList.get(i).get(5) + "]");
+        }
+
+        // 당첨 번호 입력
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        Set<Integer> numbersSet = new LinkedHashSet<>();
+        while(true) {
+            System.out.println("\n당첨 번호를 입력해 주세요.");
+            try {
+                String input = readLine();
+                String[] numbers = input.split(",");
+                if (numbers.length != 6) {
+                    throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력하세요.");
+                }
+
+
+                for (int i = 0; i < 6; i++) {
+                    winningNumbers.add(Integer.parseInt(numbers[i]));
+                    numbersSet.add(Integer.parseInt(numbers[i]));
+                    if (winningNumbers.get(i) < 1 || winningNumbers.get(i) > 45) {
+                        throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                    }
+                }
+                if (numbersSet.size() != 6) {
+                    throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력하세요.");
+                }
+                System.out.println("\n보너스 번호를 입력해 주세요.");
+                bonusNumber = Integer.parseInt(readLine());
+                if (bonusNumber < 1 || bonusNumber > 45) {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // 로또 당첨 확인
+        int[] results = new int[6];
+        for (List<Integer> lotto : lottoList) {
+            int matchedNumbers = countMatchedNumbers(lotto, winningNumbers);
+            boolean hasBonusNumber = hasBonusNumber(lotto, bonusNumber);
+            int prize = getPrize(matchedNumbers, hasBonusNumber);
+            if (prize > 0) {
+                results[prize]++;
+            }
+        }
+
+        // 결과 출력
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + results[5] +"개");
+        System.out.println("4개 일치 (50,000원) - " + results[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + results[3] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + results[2] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + results[1] + "개");
+        double totalPrizeAmount = calculateTotalPrizeAmount(results);
+        double profitRate = (totalPrizeAmount / purchaseAmount) * 100;
+        String formattedProfitRate = String.format("%,f", profitRate);
+        System.out.printf("총 수익률은 %.1f%%입니다.", profitRate);
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,93 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        Set<Integer> numbersSet = new LinkedHashSet<>();
+        for(int i = 0; i<numbers.size(); i++)
+        {
+            numbersSet.add(numbers.get(i));
+        }
+        if(numbersSet.size() != 6){
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현
+    public static List<Integer> generateRandomLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers2 = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.addAll(numbers2);
+        Collections.sort(numbers);
+        return numbers;
+    }
+    private static boolean contains(List<Integer> array, int value) {
+        for (int element : array) {
+            if (element == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int countMatchedNumbers(List<Integer> lotto, List<Integer> winningNumbers) {
+        int count = 0;
+        for (int number : lotto) {
+            if (contains(winningNumbers, number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean hasBonusNumber(List<Integer> lotto, int bonusNumber) {
+
+        return contains(lotto, bonusNumber);
+    }
+
+    static int getPrize(int matchedNumbers, boolean hasBonusNumber) {
+        if (matchedNumbers == 6) {
+            return 1;
+        }
+        if (matchedNumbers == 5 && hasBonusNumber) {
+            return 2;
+        }
+        if (matchedNumbers == 5) {
+            return 3;
+        }
+        if (matchedNumbers == 4) {
+            return 4;
+        }
+        if (matchedNumbers == 3) {
+            return 5;
+        }
+        return 0;
+    }
+
+    static int getPrizeAmount(int prize) {
+        if(prize == 3) return 1500000;
+        if(prize == 4) return 50000;
+        if(prize == 5) return 5000;
+        if(prize == 2) return 30000000;
+        if(prize == 1) return 2000000000;
+        return 0;
+    }
+
+    static int correctRate(int rank){
+        if(rank == 1) return 6;
+        if(rank == 2 || rank == 3) return 5;
+        if(rank == 4) return 4;
+        if(rank == 5) return 3;
+        return 0;
+
+    }
+
+    static double calculateTotalPrizeAmount(int[] results) {
+        double totalAmount = 0;
+        for (int i = 1; i <= 5; i++) {
+            int prize = i;
+            int prizeAmount = getPrizeAmount(prize);
+            int count = results[i];
+            totalAmount += prizeAmount * count;
+        }
+        return totalAmount;
+    }
 }

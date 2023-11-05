189518d6143aeb9c7642397797b155a963f1b938
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-05 15:48:10 +0900

    test: Money 클래스에 대한 테스트코드 작성

---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import lotto.model.Money;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MoneyTest {
+    @Test
+    public void 로또_구매_단위_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(5500));
+    }
+
+    @Test
+    public void 로또_최대금액_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(100_000_001));
+    }
+
+    @Test
+    public void 로또_최소금액_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(100));
+    }
+
+    @Test
+    public void 로또_구매개수_테스트() {
+        Money money = new Money(10000);
+        Assertions.assertEquals(10, money.calculateLottoCount());
+    }
+}

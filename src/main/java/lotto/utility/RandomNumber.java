6b639ab0c308fab4ae6b7b92e5dd9a3e5caf9ce1
DaeYoungK <hanguk830@naver.com>
2023-11-05 19:13:00 +0900

    fix(RandomNumber): 정렬 로직 분리하고 stream 으로 정렬하도록 변경

--- src/main/java/lotto/utility/RandomNumber.java
+++ src/main/java/lotto/utility/RandomNumber.java
@@ -2,14 +2,22 @@ package lotto.utility;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomNumber {
 
+    private final int START_NUMBER = 1;
+    private final int END_NUMBER = 45;
+    private final int NUMBER_COUNT = 6;
+
     public List<Integer> makeLottoNumber() {
-        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(lottoNumber);
+        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBER_COUNT);
+        sortLottoNumber(lottoNumber);
         return lottoNumber;
+    }
+
+    private void sortLottoNumber(List<Integer> lottoNumber) {
+        lottoNumber.stream().collect(Collectors.toList());
     }
 }

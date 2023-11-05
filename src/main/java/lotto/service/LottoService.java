e48f9ad3757fbc0268a30299a1a0b1d96636c7fb
isyoo1213 <moethns@gmail.com>
2023-11-05 19:05:37 +0900

    feat: 로또 번호 및 보너스 번호 생성 기능 추가 LottoService() - createuserLottos() 추가 - createAnswerLotto() 추가 README.md - 기능 명세 구현 체크

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,8 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+import lotto.model.Lottos;
 
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +10,21 @@ import java.util.List;
 import static lotto.constants.LottoConstants.*;
 
 public class LottoService {
+
+    public Lottos createUserLottos(int amount) {
+        Lottos lottos = new Lottos();
+        for (int i = 0; i < amount; i++) {
+            List<Integer> randomNumbers = generateRandomNumbers();
+            sortNumbersByAsc(randomNumbers);
+            Lotto lotto = new Lotto(randomNumbers);
+            lottos.addLotto(lotto);
+        }
+        return lottos;
+    }
+
+    public Lotto createAnswerLotto(List<Integer> lottoNumbers) {
+        return new Lotto(lottoNumbers);
+    }
 
     public List<Integer> sortNumbersByAsc(List<Integer> integerList) {
         Collections.sort(integerList);

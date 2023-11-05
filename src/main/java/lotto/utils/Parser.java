a31e30f6ddd64abf3a6bcba4ee421d4beb2f45bb
mina <qkralsdk991@naver.com>
2023-11-05 19:04:45 +0900

    Feat : 당첨 번호 입력 기능 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,2 +1,21 @@
-package lotto.utils;public class Parser {
+package lotto.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+
+    public List<Integer> translatePlayerInputStringToInt(String playerInputString){
+        List<String> playerInput = new ArrayList<>(Arrays.asList(playerInputString.split(",")));
+        List<Integer> playerInputInt=playerInput.stream()
+                .map(s->Integer.parseInt(s))
+                .collect(Collectors.toList());
+
+        return playerInputInt;
+    }
+    public int stringToInteger(String playerInputNum) {
+        return Integer.parseInt(playerInputNum);
+    }
 }

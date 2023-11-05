7fb383d28447ced4866a43042700d5378722bd1e
suihub <qkdcrt@gmail.com>
2023-11-06 07:13:51 +0900

    <feat(WinningLotto)> : "등수 결과를 리스트에 담아 반환하는 기능 구현"
    - 넘겨받은 로또의 숫자만큼 당첨 여부를 판단해 등수를 리스트에 담아 반환하는 기능 구현.
    - 번호를 비교해 일치하는 숫자를 반환하는 기능 구현.
    - 5개를 맞췄을 경우, 보너스 숫자를 맞췄는지 여부를 확인하는 기능 구현.

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.exception.ErrorMessage.*;
 import static lotto.exception.InputNumberException.*;
 import static lotto.exception.LottoNumberException.*;
+import static lotto.util.NumberConstant.*;
 
 public class WinningLotto {
     private final List<Integer> numbers;
@@ -29,5 +31,33 @@ public class WinningLotto {
         if (numbers.contains(number)) {
             throw new IllegalArgumentException(ERROR.getMessage() + BONUS_DUPLICATE.getMessage());
         }
+    }
+
+    public List<Rank> judgeLottoGame(List<Lotto> allLotto) {
+        List<Rank> ranks = new ArrayList<>();
+
+        for (Lotto lotto : allLotto) {
+            List<Integer> target = lotto.getLottoNumbers();
+            Integer countNum = countWinningNumber(target);
+            if (checkBonus(countNum, target)) {
+                countNum = SECOND_NUMBER_NUM;
+            }
+            ranks.add(Rank.resultRank(countNum));
+        }
+        return ranks;
+    }
+
+    private Integer countWinningNumber(List<Integer> target) {
+        int count = 0;
+        for (Integer num : target) {
+            if (numbers.contains(num)) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    private boolean checkBonus(Integer countNum, List<Integer> target) {
+        return countNum.equals(THIRD_NUMBER_NUM) && target.contains(bonusNumber);
     }
 }

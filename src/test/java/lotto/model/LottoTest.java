748c732df825afd44e1780745a0e8deea1701b88
jeondui <jeondui@gmail.com>
2023-11-06 03:04:39 +0900

    test(LottoTest): 에러메시지, 테스트명 수정

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,64 +1,59 @@
 package lotto.model;
 
-import lotto.exception.ErrorMessage;
-import lotto.model.Lotto;
-import org.junit.jupiter.api.BeforeEach;
+import lotto.common.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
-    @DisplayName("로또 번호의 개수 6개 초과 시 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호의 개수 6개 초과 시 예외 발생 테스트")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> Lotto.create("1,2,3,4,5,6,7"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBERS_LENGTH.get());
     }
 
-    @DisplayName("로또 번호에 개수 6개 미만 시 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호에 개수 6개 미만 시 예외 발생 테스트")
     @Test
     void createLottoSize() {
         assertThatThrownBy(() -> Lotto.create("1,2,3,4,5"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBERS_LENGTH.get());
     }
 
-    @DisplayName("로또 번호 중복 시 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호 중복 시 예외 발생 테스트")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> Lotto.create("1,2,3,4,5,5"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_DUPLICATE.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBERS_DUPLICATE.get());
     }
 
-    @DisplayName("로또 번호 입력값 없을 때 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호 입력값 없을 때 예외 발생 테스트")
     @Test
     void isEmpty() {
         assertThatThrownBy(() -> Lotto.create(""))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_NULL.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBERS_NULL.get());
     }
 
-    @DisplayName("로또 번호 공백 포함 시 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호 공백 포함 시 예외 발생 테스트")
     @Test
     void isContainsBlank() {
         assertThatThrownBy(() -> Lotto.create("1, 2, 3, 4, 5, 6"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_CONTAINS_BLANK.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBERS_CONTAINS_BLANK.get());
     }
 
-    @DisplayName("로또 번호의 입력값이 쉼표(,)로 끝나는 경우 예외 발생 테스트")
+    @DisplayName("당첨 로또 번호의 입력값이 쉼표(,)로 끝나는 경우 예외 발생 테스트")
     @Test
     void isEndsWithComma() {
         assertThatThrownBy(() -> Lotto.create("1,2,3,4,5,6,"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_NUMBERS_ENDS_WITH_COMMA.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBERS_ENDS_WITH_COMMA.get());
     }
-
 
 }

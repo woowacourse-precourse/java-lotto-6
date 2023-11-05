78b3c99e2d1d94d10de26da392b050865082f9ea
jifrozen0110 <answldjs1836@gmail.com>
2023-11-05 17:13:35 +0900

    feat(domain) : 당첨 번호와 보너스 번호를 담는 도메인 생성

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,9 +1,13 @@
 package lotto.controller;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
 import lotto.domain.LottoCreator;
 import lotto.domain.LottoNumberGenerator;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -19,6 +23,13 @@ public class LottoGameController {
         LottoCreator lottoCreator = new LottoCreator(lottoNumberGenerator);
         Lottos lottos = lottoCreator.createLottos(lottoCount);
         OutputView.printLottos(lottos);
+        OutputView.printWinningNumbersInputMessage();
+        int[] winningNumbers = InputView.nextIntArray();
+        Lotto winningNumber = new Lotto(Arrays.stream(winningNumbers).boxed().collect(Collectors.toList()));
+        OutputView.printBonusNumberInputMessage();
+        int bonusNumber = InputView.nextInt();
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
 
     }
 }
---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class WinningLotto {
+    private Lotto lotto;
+    private int bonusNumber;
+
+    public WinningLotto(Lotto lotto, int bonusNumber) {
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+
+}

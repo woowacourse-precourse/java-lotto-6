e2d2b4ee5bab2741f360de5d33471204b13ee7e8
ks0927 <brian_als@naver.com>
2023-11-05 18:23:36 +0900

    test(PurchasedLottos): PurchasedLottos 클래스 테스트

---
+++ src/test/java/lotto/PurchasedLottosTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import lotto.generator.NumberGenerator;
+import lotto.generator.RandomNumberGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PurchasedLottosTest {
+
+    @DisplayName("로또를 장당 1000원에 구입 금액에 해당하는 만큼 로또를 발행해야 한다.")
+    @Test
+    public void createPurchasedLottos_ShouldGenerateCorrectNumberOfLottos() {
+        NumberGenerator generator = new RandomNumberGenerator();
+        Money money = new Money("5000");
+        PurchasedLottos purchasedLottos = PurchasedLottos.createPurchasedLottos(generator, money);
+
+        Assertions.assertThat(purchasedLottos.purchasedLottosCount()).isEqualTo(5);
+    }
+
+}

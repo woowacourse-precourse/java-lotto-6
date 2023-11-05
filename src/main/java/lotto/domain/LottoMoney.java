579afa165b999c278e4418a269e0d032b2f2b735
34suuuuu <psh7492@gmail.com>
2023-11-05 18:47:35 +0900

    feat: 로또구입 금액 입력과 예외처리 기능 추가

---
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import lotto.exception.LottoMoneyDivideException;
+import lotto.exception.LottoMoneyLessException;
+
+public class LottoMoney {
+    private static final int LOTTO_MONEY_DIVIDE = 1000;
+    private static int lottoMoney;
+
+    private LottoMoney(int lottoMoney) {
+        this.lottoMoney = lottoMoney;
+    }
+
+    public int getLottoMoney(){
+        return lottoMoney;
+    }
+
+    public int changeToLottoCnt(){
+        return lottoMoney / LOTTO_MONEY_DIVIDE;
+    }
+
+    private static void validate(int lottoMoney) {
+        if (lottoMoney < LOTTO_MONEY_DIVIDE) {
+            throw new LottoMoneyLessException();
+        }
+        if (lottoMoney % LOTTO_MONEY_DIVIDE != 0) {
+            throw new LottoMoneyDivideException();
+        }
+    }
+}

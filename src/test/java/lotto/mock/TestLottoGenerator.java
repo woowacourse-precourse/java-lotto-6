f062935ef23104dc9233b664c1778072ff211259
dltjdn <hana40004@naver.com>
2023-11-06 01:44:45 +0900

    test(TestLottoGenerator): 클래스 정의
    * 항상 생성될 때 지정된 숫자로 이루어진 로또를 반환

---
+++ src/test/java/lotto/mock/TestLottoGenerator.java
@@ -0,0 +1,17 @@
+package lotto.mock;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoGenerator;
+
+public class TestLottoGenerator implements LottoGenerator {
+    private final List<Integer> numbers;
+
+    public TestLottoGenerator(List<Integer> numbers){
+        this.numbers = numbers;
+    }
+    @Override
+    public Lotto generate() {
+        return new Lotto(numbers);
+    }
+}

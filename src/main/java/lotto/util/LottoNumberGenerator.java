f702940ae21089bbaeaac885e3bda8390f4edf7e
SungKyum Kim <sungkyum1@naver.com>
2023-11-05 23:45:00 +0900

    docs(README): 기능명세서 수정(로또 번호 생성 역할 담당 유틸 클래스)

---
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    public Integer generateNumber() {
+        return Randoms.
+    }
+}

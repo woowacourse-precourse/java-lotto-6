07f1a52656431e419119977d6a50b85f273c6fb7
DH Jeong <wjdehgns97@gmail.com>
2023-11-05 14:40:47 +0900

    Fix: 쉼표가 올바르게 입력되었는지 확인하도록 패턴 수정

--- src/main/java/lotto/constant/PatternConstant.java
+++ src/main/java/lotto/constant/PatternConstant.java
@@ -4,5 +4,5 @@ import java.util.regex.Pattern;
 
 public final class PatternConstant {
     public static final Pattern NUMBER_PATTERN = Pattern.compile("^[1-9][0-9]*");
-    public static final Pattern HAS_COMMAS_WITHOUT_SURROUNDING_VALUES_PATTERNS = Pattern.compile("^,|,$|[^,],,[^,]");
+    public static final Pattern HAS_COMMAS_WITHOUT_SURROUNDING_VALUES_PATTERNS = Pattern.compile("^[^,],|,[^,]|,$");
 }

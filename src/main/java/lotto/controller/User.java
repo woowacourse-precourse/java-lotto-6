e685213374f955fffdccef2ee6adb498e2f55d97
강승민 <rkdtmdals999@naver.com>
2023-11-05 19:52:07 +0900

    [feat] 유저 구입금액 입력 구현

--- src/main/java/lotto/controller/User.java
+++ src/main/java/lotto/controller/User.java
@@ -1,4 +1,17 @@
 package lotto.controller;
 
+import static lotto.utility.Constants.LOTTO_PRICE;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+    public User() {}
+    public Integer inputPurchaseAmount() {
+        String userInput = Console.readLine();
+        Integer purchaseAmount = Integer.parseInt(userInput);
+        return purchaseAmount;
+    }
+    public Integer calculatePurchaseLottoNumber(Integer purchaseAmount) {
+        return purchaseAmount / LOTTO_PRICE;
+    }
 }

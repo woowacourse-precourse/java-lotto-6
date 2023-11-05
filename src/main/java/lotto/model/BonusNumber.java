23d9f2d5fd72f83e9fc7d0ae5bfafeb43007fb00
koomchang <leahpar0401@gmail.com>
2023-11-05 22:17:01 +0900

    feat: 보너스 번호 클래스를 모델에 구현

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,2 +1,42 @@
-package lotto.model;public class BonusNumber {
+package lotto.model;
+
+import java.util.Objects;
+
+public class BonusNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private final int number;
+
+    public BonusNumber(int number) {
+        validateNumberRange(number);
+        this.number = number;
+    }
+
+    private void validateNumberRange(Integer number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BonusNumber that = (BonusNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }

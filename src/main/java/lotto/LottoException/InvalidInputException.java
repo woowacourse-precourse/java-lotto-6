de6bd3d06dd14a49deb60920a1393b18f64780b6
su-giana <giananews@gmail.com>
2023-11-05 18:05:49 +0900

    feat : 당첨 번호 입력 로직 구현

---
+++ src/main/java/lotto/LottoException/InvalidInputException.java
@@ -0,0 +1,9 @@
+package lotto.LottoException;
+
+public class InvalidInputException extends IllegalArgumentException
+{
+    public InvalidInputException(String message)
+    {
+        super(message);
+    }
+}
--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,27 +1,93 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.LottoException.InvalidInputException;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputView
 {
+    private int getValidCost()
+    {
+        try
+        {
+            int cost = Integer.parseInt(Console.readLine());
+            if(cost % 1000 == 0)
+            {
+                return cost;
+            }
+
+            throw new InvalidInputException("[ERROR] 금액은 1000으로 나누어 떨어져야 합니다.");
+        }
+        catch (NumberFormatException e)
+        {
+            throw new InvalidInputException("[ERROR] 입력 횟수가 숫자가 아닙니다.");
+        }
+    }
+
     private int getCost() throws IllegalArgumentException
     {
         while(true)
         {
             try
             {
-                int cost = Integer.parseInt(Console.readLine());
-                if(cost % 1000 != 0)
-                {
-                    System.out.println("[Error] 금액은 1000으로 나누어 떨어져야 합니다.");
-                    continue;
-                }
-
-                return cost;
+                return getValidCost();
             }
-            catch (NumberFormatException e)
+            catch (InvalidInputException e)
             {
-                System.out.println("[ERROR] 입력 횟수가 숫자가 아닙니다.");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> getValidWinningLotteryNumber()
+    {
+        try
+        {
+            List<Integer> winningLotteryNumber = Arrays.stream(Console.readLine().split(","))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
+
+            if(winningLotteryNumber.size() != 6)
+            {
+                throw new InvalidInputException("[ERROR] 당첨 번호의 길이가 6이 아닙니다.");
+            }
+
+            if(winningLotteryNumber.stream().anyMatch(num -> num<1 || num>45))
+            {
+                throw new InvalidInputException("[ERROR] 당첨 번호는 1~45 사이어야 합니다.");
+            }
+
+            Set<Integer> winningLotteryNumberSet = new HashSet<>(winningLotteryNumber);
+            if(winningLotteryNumberSet.size() != winningLotteryNumber.size())
+            {
+                throw new InvalidInputException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다");
+            }
+
+            return winningLotteryNumber;
+        }
+        catch (NumberFormatException e)
+        {
+            throw new InvalidInputException("[ERROR] 입력된 당첨 번호에 숫자가 아닌 것이 있습니다.");
+        }
+    }
+
+    private List<Integer> getWinningLotteryNumber()
+    {
+        while(true)
+        {
+            try
+            {
+                return getValidWinningLotteryNumber();
+            }
+            catch (InvalidInputException e)
+            {
+                System.out.println(e.getMessage());
             }
         }
     }

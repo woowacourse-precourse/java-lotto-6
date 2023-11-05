f73860e0b862e96e978e35dee86b0577d6891408
jihyuk <kwlgur0416@gmail.com>
2023-11-05 23:24:51 +0900

    test : 구매 금액 입력 기능 테스트 작성
    : 구매 금액 입력 정상 로직 테스트
    : 빈 값 예외 테스트
    : 숫자에 대한 예외 테스트

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,56 @@
+package lotto.view;
+
+import static lotto.exception.InputErrorCode.BLANK_INPUT;
+import static lotto.exception.InputErrorCode.NOT_INTEGER_INPUT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import lotto.exception.InputException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    @AfterEach
+    void close() {
+        Console.close();
+    }
+
+    @Test
+    void 구매_가격을_입력받는다() {
+        final String input = "1000";
+        command(input);
+
+        int purchaseAmount = InputView.inputLottoPurchaseAmount();
+
+        assertThat(purchaseAmount).isEqualTo(1000);
+    }
+
+    @Test
+    void 구매_가격이_숫자가_아니라면_예외를_던진다() {
+        final String input = "poby";
+        command(input);
+
+        assertThatThrownBy(InputView::inputLottoPurchaseAmount)
+            .isInstanceOf(InputException.class)
+            .hasMessageContaining(NOT_INTEGER_INPUT.getMessage());
+    }
+
+    @Test
+    void 구매_가격이_빈값이라면_예외를_던진다() {
+        final String input = "\n";
+        command(input);
+
+        assertThatThrownBy(InputView::inputLottoPurchaseAmount)
+            .isInstanceOf(InputException.class)
+            .hasMessageContaining(BLANK_INPUT.getMessage());
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
+}

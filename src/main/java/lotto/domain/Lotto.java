a8bfc25825bfd1ade3bb55041269228b678aaab8
haero77 <shineecard5@gmail.com>
2023-11-06 02:31:16 +0900

    refactor: 로또 생성 시 중복된 숫자에 대한 예외 처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    public static final int DEFAULT_NUMBER_SIZE = 6;
 
     private final List<Integer> numbers;
 
@@ -13,9 +17,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateNumberSize(numbers);
+        validateNotDuplicated(numbers);
+    }
+
+    private static void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != DEFAULT_NUMBER_SIZE) {
+            throw new IllegalArgumentException("Size of lotto number should be %d.".formatted(DEFAULT_NUMBER_SIZE));
         }
+    }
+
+    private void validateNotDuplicated(List<Integer> numbers) {
+        if (duplicatedNumberExists(numbers)) {
+            throw new IllegalArgumentException(
+                    "Some elements of %s are duplicated.".formatted(Arrays.toString(numbers.toArray()))
+            );
+        }
+    }
+
+    private boolean duplicatedNumberExists(List<Integer> numbers) {
+        return numbers.size() != Set.copyOf(numbers).size();
     }
 
     public int getNumbersSize() {

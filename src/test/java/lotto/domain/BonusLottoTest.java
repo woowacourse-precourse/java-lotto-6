e26ed894d4c7d74117d35c1c1ccac307b3e12020
sotogito <108535526+sotogito@users.noreply.github.com>
2023-11-05 15:41:40 +0900

    feat : 보너스점수 예외처리 테스트코드 작성, 통과

---
+++ src/test/java/lotto/domain/BonusLottoTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BonusLottoTest {
+
+    @Test
+    void 범위_밖의_번호를_입력_받았을_땨_예외_처리() {
+        int bonusNumber = 50;
+
+        assertThatThrownBy(() -> new BonusLotto(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

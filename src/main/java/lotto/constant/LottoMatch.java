52fda7e100afb1fd4c5b5fd57540f4fd39b65090
Oseuseu <lakingsung@gmail.com>
2023-11-06 04:36:24 +0900

    feat: `LottoMatch` Enum 클래스 구현
    로또 번호 매칭 개수 Enum

---
+++ src/main/java/lotto/constant/LottoMatch.java
@@ -0,0 +1,21 @@
+package lotto.constant;
+
+public enum LottoMatch {
+    SIX_MATCH(6, "6개 일치"),
+    FIVE_MATCH(5, "5개 일치"),
+    FIVE_MATCH_WITH_BONUS(5, "5개 일치, 보너스 번호 일치"),
+    FOUR_MATCH(4, "4개 일치"),
+    THREE_MATCH(3, "3개 일치"),
+    TWO_MATCH(2, "2개 일치"),
+    ONE_MATCH(1, "1개 일치"),
+    NO_MATCH(0, "0개 일치")
+    ;
+
+    private final int matchCount;
+    private final String description;
+
+    LottoMatch(int matchCount, String description) {
+        this.matchCount = matchCount;
+        this.description = description;
+    }
+}

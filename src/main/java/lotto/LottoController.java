0460db30fff51170304a5b39ffc8e23e62837339
이종경 <gyoung3063413@naver.com>
2023-11-05 14:14:37 +0900

    [수정] MVC 패턴에 맞게 '사용자에게 당첨 번호를 입력받는 메서드' 수정

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class LottoController {
 	private Lotto randomLotto;
 	private LottoView LottoView;
 	private List<List<Integer>> randomLottos;
+	private List<Integer> winLotto;
 
 	public LottoController() {
 		randomLottos = new ArrayList<>();
@@ -22,13 +26,41 @@ public class LottoController {
 		for (int i = 0; i < count; i++) {
 			List<Integer> randomNumbers = pickUniqueNumbersInRange(1, 45, 6);
 			randomLotto = new Lotto(randomNumbers);
-			LottoView.printLottos(randomNumbers);
+			LottoView.printLottos(randomLotto.getNumbers());
 			randomLottos.add(randomNumbers);
 		}
+	}
+
+	public List<Integer> setWinNumbers(String[] input) {
+		List<Integer> winNumbers = new ArrayList<>();
+
+		for (String str : input) {
+			try {
+				int num = Integer.parseInt(str.trim());
+				winNumbers.add(num);
+			} catch (NumberFormatException e) {
+				LottoView.getWinLotto();
+			}
+
+		}
+		return winNumbers;
+	}
+
+	public void getWinNumbers() {
+		String[] winNumbers = LottoView.getWinLotto();
+		winLotto = this.setWinNumbers(winNumbers);
+		this.addBonusNumber();
+//		System.out.println(winLotto);
+	}
+
+	public void addBonusNumber() {
+		int bonusNumber = LottoView.getBonusNumber();
+		winLotto.add(bonusNumber);
 	}
 
 	public void processGame() {
 		int count = this.getCount();
 		this.createRandomLotto(count);
+		this.getWinNumbers();
 	}
 }
--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -2,20 +2,23 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoView {
 	public int getMoney() { // 사용자의 금액 입력을 받는 메서드
 		while (true) {
 			try {
 				System.out.println("구입 금액을 입력해주세요.");
-				int userInput = Integer.parseInt(readLine().trim()); //사용자의 입력을 받는다
-				this.validateMoney(userInput); // 사용자의 입력값의 유효성 검사를 실행한다
+				int userInput = Integer.parseInt(readLine().trim()); // 사용자의 입력을 받는다
+				this.validateMoney(userInput); // 사용자의 입력값에 대한 유효성 검사를 실행한다
 				System.out.printf("%n%s개를 구매했습니다 %n", userInput / 1000); // 사용자가 구매한 갯수를 알려준다.
 				return userInput / 1000;
 			} catch (NumberFormatException e) {
 				System.err.println("[ERROR] 금액은 숫자만 입력이 가능합니다.");
-			} catch (IllegalArgumentException e) {
+			} catch (IllegalStateException e) {
 				System.err.println("[ERROR] 금액은 천원으로 나누어 떨어져야합니다.");
 			}
 		}
@@ -23,15 +26,56 @@ public class LottoView {
 
 	private void validateMoney(int input) { // 사용자가 입력한 값이 1000으로 나누어 떨어지는지 예외처리
 		if (input % 1000 != 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalStateException();
 		}
 	}
 
 	public void printLottos(List<Integer> numbers) {
 		System.out.println(numbers);
 	}
-//	public int printBuyLotto() {
-//		System.out.printf("%n%s개를 구매하셨습니다. %n", count);
-//		return count;
-//	}
+
+	public String[] getWinLotto() { // 사용자의 당첨번호를 입력받는 메서드
+		while (true) {
+			try {
+				System.out.printf("%n당첨번호를 입력해 주세요.%n");
+				String[] userInput = (readLine().trim()).split(","); // 사용자의 입력을 받는다 
+				this.validateNumber(userInput); // 사용자의 입력값에 대한 유효성 검사를 실행한다
+				return userInput;
+			} catch (NumberFormatException e) {
+				System.err.println("[ERROR] 번호는 숫자로 입력해주세요.");
+			} catch (IllegalArgumentException e) {
+				System.err.println("[ERROR] 콤마(,)로 구분하여 입력해주세요.");
+			} catch (IllegalStateException e) {
+				System.err.println("[ERROR]1~45사이의 번호를 중복되지 않게 6개 입력해주세요.");
+			}
+		}
+	}
+
+	private void validateNumber(String[] input) {
+		Set<Integer> numbers = new HashSet<>();
+		if (input.length != 6) {
+			throw new IllegalStateException();
+		}
+		for (String numStr : input) {
+			int num;
+			num = Integer.parseInt(numStr);
+			if (num < 1 || num > 45 || !numbers.add(num)) {
+				System.out.println("중복" + numbers);
+				throw new IllegalStateException();
+			}
+		}
+
+	}
+
+	public int getBonusNumber() {
+		while (true) {
+			try {
+				System.out.printf("%n보너스 번호를 입력해 주세요.%n");
+				int userInput = Integer.parseInt(readLine().trim());
+				return userInput;
+			} catch (NumberFormatException e) {
+				System.err.println("[ERROR] 번호는 숫자로 입력해주세요.");
+			}
+		}
+	}
 }

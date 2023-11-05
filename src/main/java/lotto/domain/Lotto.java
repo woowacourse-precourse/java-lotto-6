d1c44fd48161dd81d0dc6e66bc28813f69aa2b27
GiJung <weeds2577@gmail.com>
2023-11-05 14:58:35 +0900

    feat(Lotto): 로또 번호 예외 처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,22 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int startNumber = 1;
+    private static final int endNumber = 45;
+    private static final int lottoNumberCount = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbersRangeCheck(numbers);
+        duplicateCheck(numbers);
         sortLotto(numbers);
         this.numbers = numbers;
     }
@@ -24,8 +33,25 @@ public class Lotto {
         return this.numbers;
     }
 
-    public void sortLotto(List<Integer> numbers) {
+    private void sortLotto(List<Integer> numbers) {
         Collections.sort(numbers);
     }
 
+    private void numbersRangeCheck(List<Integer> numbers) {
+        for(int number : numbers)
+            rangeCheck(number);
+    }
+
+    private void rangeCheck(int number) {
+        if(number > endNumber || number < startNumber) throw new IllegalArgumentException();
+    }
+
+    private void duplicateCheck(List<Integer> numbers) {
+
+        Set<Integer> noDuplicateNumbers = new HashSet<>();
+        for(int number : numbers)
+            noDuplicateNumbers.add(number);
+
+        if(noDuplicateNumbers.size() != numbers.size()) throw new IllegalArgumentException();
+    }
 }

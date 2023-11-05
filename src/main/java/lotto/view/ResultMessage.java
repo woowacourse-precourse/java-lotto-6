56880d7d30e780a67a2eb2e4faae088f514ad79d
leesangjun <hubls@naver.com>
2023-11-05 14:22:04 +0900

    feat: 수익률 출력하는 기능

--- src/main/java/lotto/view/ResultMessage.java
+++ src/main/java/lotto/view/ResultMessage.java
@@ -1,17 +1,18 @@
 package lotto.view;
 
-public enum WinningStatisticsMessage {
+public enum ResultMessage {
     RESULT("당첨 통계"),
     DIVIDING_LINE("---"),
     THREE_NUMBERS("3개 일치 (5,000원) - "),
     FOUR_NUMBERS("4개 일치 (50,000원) - "),
     FIVE_NUMBERS("5개 일치 (1,500,000원) - "),
     FIVE_NUMBERS_AND_BONUS_NUMBER("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    SIX_NUMBERS("6개 일치 (2,000,000,000원) - ");
+    SIX_NUMBERS("6개 일치 (2,000,000,000원) - "),
+    TOTAL_PROFIT("총 수익률은 %.2f 입니다");
 
     private final String message;
 
-    WinningStatisticsMessage(String message) {
+    ResultMessage(String message) {
         this.message = message;
     }
 
--- src/main/java/lotto/view/ResultOutputView.java
+++ src/main/java/lotto/view/ResultOutputView.java
@@ -11,22 +11,26 @@ public class ResultOutputView {
     }
 
     public void printWinningStatistics() {
-        System.out.println(WinningStatisticsMessage.RESULT.getMessage());
-        System.out.println(WinningStatisticsMessage.DIVIDING_LINE.getMessage());
-        System.out.println(WinningStatisticsMessage.THREE_NUMBERS.getMessage()
+        System.out.println(ResultMessage.RESULT.getMessage());
+        System.out.println(ResultMessage.DIVIDING_LINE.getMessage());
+        System.out.println(ResultMessage.THREE_NUMBERS.getMessage()
                 + lottoStatistics.getThreeNumbersMatch()
                 + UINT_WORD);
-        System.out.println(WinningStatisticsMessage.FOUR_NUMBERS.getMessage()
+        System.out.println(ResultMessage.FOUR_NUMBERS.getMessage()
                 + lottoStatistics.getFourNumbersMatch()
                 + UINT_WORD);
-        System.out.println(WinningStatisticsMessage.FIVE_NUMBERS.getMessage()
+        System.out.println(ResultMessage.FIVE_NUMBERS.getMessage()
                 + lottoStatistics.getFiveNumbersMatch()
                 + UINT_WORD);
-        System.out.println(WinningStatisticsMessage.FIVE_NUMBERS_AND_BONUS_NUMBER.getMessage()
+        System.out.println(ResultMessage.FIVE_NUMBERS_AND_BONUS_NUMBER.getMessage()
                 + lottoStatistics.getFiveNumbersAndBonusNumberMatch()
                 + UINT_WORD);
-        System.out.println(WinningStatisticsMessage.SIX_NUMBERS.getMessage()
+        System.out.println(ResultMessage.SIX_NUMBERS.getMessage()
                 + lottoStatistics.getSixNumbersMatch()
                 + UINT_WORD);
+    }
+
+    public void printTotalProfit(double totalProfit) {
+        System.out.print(String.format(String.valueOf(ResultMessage.TOTAL_PROFIT), totalProfit));
     }
 }

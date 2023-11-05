a131463e9db8bdce71d4532da81ff3459a07997b
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 01:29:18 +0900

    feat(WinningNumber): 당첨 번호 클래스 및 검증 로직 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,2 +1,32 @@
-package lotto.domain;public class WinningNumber {
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final int MIN_LOTTO_NUMBER = 1;
+    private final int MAX_LOTTO_NUMBER = 1;
+    List<Integer> numbers;
+
+    public WinningNumber(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (checkNumbersCount(numbers) || hasDuplicates(numbers) || hasOutOfRangeNumbers(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkNumbersCount(List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
+    private boolean hasOutOfRangeNumbers(List<Integer> numbers) {
+        return numbers.stream().anyMatch(number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER);
+    }
 }

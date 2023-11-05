72da00318594efd144edb1be389c493155b73349
김승진 <ohksj77@gmail.com>
2023-11-05 18:03:26 +0900

    feat(WinningNumbers): 당첨 번호 중복 검사 기능 구현

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     NOT_NUMBER("입력값은 숫자여야 합니다."),
     NOT_INITIALIZED("도메인 객체가 초기화되지 않았습니다."),
     INVALID_WINNING_NUMBERS("잘못된 당첨 번호 형식입니다."),
-    INVALID_NUMBER_RANGE("숫자의 범위가 잘못되었습니다.");
+    INVALID_NUMBER_RANGE("숫자의 범위가 잘못되었습니다."),
+    NOT_UNIQUE_WINNING_NUMBERS("당첨 번호에 중복이 있습니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.constant.ErrorMessage;
+import lotto.constant.WinningGrade;
+
 import java.util.List;
 
 public final class WinningNumbers {
@@ -7,6 +10,21 @@ public final class WinningNumbers {
     private final Lotto lotto;
 
     public WinningNumbers(final List<Integer> numbers) {
+        validate(numbers);
         this.lotto = new Lotto(numbers);
+    }
+
+    private void validate(final List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_UNIQUE_WINNING_NUMBERS.toValue());
+        }
+    }
+
+    private boolean hasDuplicate(final List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
+
+    public List<WinningGrade> compare(final Lottos lottos) {
+        return lottos.compare(lotto);
     }
 }

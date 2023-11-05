c498a823f885cde5887301cdb94b26aa23f74a47
constant94-dev <constant94@naver.com>
2023-11-05 20:33:05 +0900

    feat(player) 메서드 삭제 및 인스턴스 변수와 생성자 구현

--- src/main/java/lotto/player/Player.java
+++ src/main/java/lotto/player/Player.java
@@ -1,16 +1,15 @@
 package lotto.player;
 
+import java.util.List;
+
 public class Player {
+    int amount;
+    List<Integer> prizeNumbers;
+    int bonusNumber;
 
-    public void inputForAmount(){
-
-    }
-
-    public void inputForPrizeNumber(){
-
-    }
-
-    public void inputForBonusNumber(){
-
+    public Player(List<Integer> prizeNumbers, int bonusNumber, int amount) {
+        this.prizeNumbers = prizeNumbers;
+        this.bonusNumber = bonusNumber;
+        this.amount = amount;
     }
 }

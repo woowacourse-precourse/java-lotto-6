a1babcdced7f436dc616bf70359f2ec9c7fb9c2c
chanhoim <cksgh201@naver.com>
2023-11-05 21:46:30 +0900

    feat(lotto): 로또 번호 중복검사 구현

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain.lotto;
 
+import static lotto.domain.lotto.LottoNumberConfig.NUMBER_COUNT;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +13,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if(validateSize(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
+        }
+        if(validateDuplication(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean validateSize(List<Integer> numbers) {
+        return  (numbers.size() != NUMBER_COUNT.getValue());
+    }
+
+    private boolean validateDuplication(List<Integer> numbers) {
+        return (numbers.stream()
+                .distinct()
+                .count() != NUMBER_COUNT.getValue());
+    }
+
+
 }

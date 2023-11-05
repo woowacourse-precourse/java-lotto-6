5b76f869d31fca531807ccd5786d5c2224d4626b
minsub <chrkb1569@naver.com>
2023-11-06 01:57:25 +0900

    refactor : LottoNumberFactory 상수값을 열거형 클래스의 값으로 변경

--- src/main/java/lotto/util/LottoNumberFactory.java
+++ src/main/java/lotto/util/LottoNumberFactory.java
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
+import static lotto.util.GameConstant.*;
+
 // 로또 번호를 생성하기 위한 클래스
 public class LottoNumberFactory implements NumberFactory {
     @Override
     public List<Integer> getNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(
+                CONSTANT_LOTTO_MIN_NUMBER.getConstant(),
+                CONSTANT_LOTTO_MAX_NUMBER.getConstant(),
+                CONSTANT_LOTTO_LENGTH.getConstant());
     }
 }

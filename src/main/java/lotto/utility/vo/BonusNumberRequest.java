66c601056d39c0d614aa25d522602745f9b7017f
jongmee <ra123221@gmail.com>
2023-11-05 18:15:42 +0900

    feat(BonusNumberRequest): 입력 받은 보너스 넘버에 대한 VO 클래스 생성하기

---
+++ src/main/java/lotto/utility/vo/BonusNumberRequest.java
@@ -0,0 +1,24 @@
+package lotto.utility.vo;
+
+import static lotto.constants.ErrorMessage.NOT_INTEGER;
+
+public class BonusNumberRequest {
+    private final Integer number;
+
+    public BonusNumberRequest(String input) {
+        String cleanedInput = cleanseWhitespace(input);
+        this.number = validateInteger(cleanedInput);
+    }
+
+    private String cleanseWhitespace(String input) {
+        return input.trim();
+    }
+
+    private Integer validateInteger(String input) {
+        try {
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER);
+        }
+    }
+}

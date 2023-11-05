c36f0cfdf1b84186457f4021a5a38e0cec9d0427
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 03:06:18 +0900

    test: 금액이 1000원 단위가 아닐 경우 에외가 발생하는지 테스트

--- src/test/java/lotto/domain/LottoAmountTest.java
+++ src/test/java/lotto/domain/LottoAmountTest.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
+import static lotto.enums.ErrorMassage.INCORRECT_AMOUNT_UNIT;
 import static lotto.enums.ErrorMassage.NOT_ENOUGH_AMOUNT;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoAmountTest {
     @Test
@@ -15,5 +19,14 @@ class LottoAmountTest {
         assertThatThrownBy(() -> new LottoAmount(amount))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NOT_ENOUGH_AMOUNT.getMassage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1001, 1234, 2010, 2023, 3400, 4802, 6990, 9999, 10500})
+    void 구매_금액이_1000원_단위가_아닐_경우_예외가_발생한다(int amount) {
+        // when & then
+        assertThatThrownBy(() -> new LottoAmount(amount))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INCORRECT_AMOUNT_UNIT.getMassage());
     }
 }

5bc34492d4f07c065e5db5324427094fe734d36e
jongmee <ra123221@gmail.com>
2023-11-05 16:21:49 +0900

    feat(Lotto): WinningNumber 도메인 클래스를 삭제하고 Lotto 도메인 클래스에 병합하기
    - WHY: WinningNumber(당첨번호) 역시 로또 번호의 성질과 모두 같으므로 굳이 다른 도메인으로 나눌 필요가 없고 불필요한 코드 중복이 발생한다. 따라서 해당 도메인 클래스를 삭제하였다. 병합하면서 Lotto 도메인 클래스에 숫자 범위를 확인하는 validation 메서드를 추가하였다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import static lotto.constants.Condition.MAXIMUM_VALUE;
+import static lotto.constants.Condition.MINIMUM_VALUE;
 import static lotto.constants.ErrorMessage.DUPLICATED_LOTTO_NUMBER;
+import static lotto.constants.ErrorMessage.INVALID_LOTTO_NUMBER_RANGE;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -13,6 +16,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
         validateDuplicate(numbers);
         this.numbers = numbers;
     }
@@ -20,6 +24,14 @@ public class Lotto {
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for(Integer number: numbers) {
+            if(number.compareTo(MINIMUM_VALUE) < 0 || number.compareTo(MAXIMUM_VALUE) > 0) {
+                throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_RANGE);
+            }
         }
     }
 

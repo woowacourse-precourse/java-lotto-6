6e1bf9f878a5890c179dc90e60846c5ec4d54dc9
ldw <leehan02@hanyang.ac.kr>
2023-11-06 03:02:55 +0900

    fix(lotto): 구입한 로또들의 번호를 출력하는 기능에서 각 로또 번호를 대괄호로 묶어서 출력하도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,17 +3,34 @@ package lotto.view;
 import lotto.model.Lotto;
 
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    private static final String BUY_SOME_AMOUNT_OF_LOTTO = "%d개를 구매했습니다.";
+    private static final String DELIMITER = ",";
+    private static final String PREFIX = "[";
+    private static final String SUFFIX = "]";
+
 
     public void printLottos(List<Lotto> lottos) {
+//        String lottoList = lottos.stream()
+//                .map(lotto -> String.join(",", lotto
+//                        .getNumbers()
+//                        .stream()
+//                        .map(Object::toString)
+//                        .toList()))
+//                .collect(Collectors.joining("\n"));
+
+        StringJoiner joiner = new StringJoiner(DELIMITER, PREFIX, SUFFIX);
         String lottoList = lottos.stream()
-                .map(lotto -> String.join(",", lotto.getNumbers().stream()
-                        .map(Object::toString)
-                        .toList()))
+                .map(lotto -> {
+                    lotto.getNumbers().forEach(number -> joiner.add(number.toString()));
+                    return joiner.toString();
+                })
                 .collect(Collectors.joining("\n"));
 
+        System.out.printf(BUY_SOME_AMOUNT_OF_LOTTO, lottos.size());
         System.out.println(lottoList);
     }
 

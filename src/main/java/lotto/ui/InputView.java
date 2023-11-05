dcb8b981ed22dccde544f9fd707adb027f206ad4
aammddkkzxc <ideal0427@naver.com>
2023-11-05 21:37:04 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String PURCHASE_MONEY_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_REQUEST_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String inputPurchaseMoney() {
         System.out.println(PURCHASE_MONEY_REQUEST_MESSAGE);
@@ -17,7 +18,9 @@ public class InputView {
         return Console.readLine();
     }
 
-    public void inputBonusNumber() {
-
+    public String inputBonusNumber() {
+        System.out.println();
+        System.out.println(BONUS_NUMBER_REQUEST_MESSAGE);
+        return Console.readLine();
     }
 }

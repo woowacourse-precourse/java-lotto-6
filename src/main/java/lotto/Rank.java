8852871a7755724689c3622cd2440b71ebe11061
shin5774 <babo5774@naver.com>
2023-11-05 10:56:48 +0900

    Feat: 등수별 출력 문구 반환기능 구현
    - 등수별로 출력해야하는 문구중 공통부분을 상수로 구현함.
    - 보너스 숫자 일치에 따른 문구 차이를 적용하기 위해 분기를 이용해 추가 문자열을 수정
    - StringJoiner를 이용해 문자열간 띄어쓰기 적용함.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,22 +1,28 @@
 package lotto;
 
 import java.util.Arrays;
+import java.util.StringJoiner;
 
 public enum Rank {
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000);
+    FIRST(6, false, 2000000000, "(2,000,000,000원)"),
+    SECOND(5, true, 30000000, "(30,000,000원)"),
+    THIRD(5, false, 1500000, "(1,500,000원)"),
+    FOURTH(4, false, 50000, "(50,000원)"),
+    FIFTH(3, false, 5000, "(5,000원)");
+
+    private static final String CORRECT_MESSAGE = "개 일치";
+    private static final String BONUS_NUMBER_MESSAGE = ", 보너스 볼 일치";
 
     private final int matchCount;
     private final boolean isMatchBonusNumber;
     private final long reward;
+    private final String rewardMessage;
 
-    Rank(int matchCount, boolean isMatchBonusNumber, long reward) {
+    Rank(int matchCount, boolean isMatchBonusNumber, long reward, String rewardMessage) {
         this.matchCount = matchCount;
         this.isMatchBonusNumber = isMatchBonusNumber;
         this.reward = reward;
+        this.rewardMessage = rewardMessage;
     }
 
     public static Rank getRank(int matchCount, boolean isMatchBonusNumber) {
@@ -35,6 +41,20 @@ public enum Rank {
 
     public long getRankReward(long count) {
         return reward * count;
+    }
+
+    public String getRankMessage() {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        String correctMessages = CORRECT_MESSAGE;
+
+        if (isMatchBonusNumber) {
+            correctMessages += BONUS_NUMBER_MESSAGE;
+        }
+
+        stringJoiner.add(String.format("%s" + correctMessages, matchCount));
+        stringJoiner.add(rewardMessage);
+
+        return stringJoiner.toString();
     }
 
 }

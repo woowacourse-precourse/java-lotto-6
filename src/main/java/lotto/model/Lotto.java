95dfcc474cb74126132d7d78590d5d8e626c7b4d
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-05 18:00:18 +0900

    feat: 생성된 로또를 검증하는 기능

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,21 +1,45 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.utils.Constants;
+import lotto.utils.ErrorMessage;
 
 public class Lotto {
-    // 필드 추가 불가능
-    // numbers의 접근제어자인 private 변경 불가능
-    // 제공된 Lotto 클래스를 활용해 구현해야함
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        // validate(numbers);
+        validateLotto(numbers);
         this.numbers = numbers;
+    }
+
+    public void validateLotto(List<Integer> numbers) {
+        validateCount(numbers);
+        validateRange(numbers);
+        validateUniqueNumbers(numbers);
     }
 
     private void validateCount(List<Integer> numbers) {
         if (numbers.size() != Constants.LOTTO_NUMBER_COUNTS) {
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_INVALID_ERROR.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_COUNT_INVALID_ERROR.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number > Constants.LOTTO_MIN_NUMBER || number < Constants.LOTTO_MAX_NUMBER) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_INVALID_ERROR.getMessage());
+            }
+        }
+    }
+
+    private void validateUniqueNumbers(List<Integer> numbers) {
+        List<Integer> check = new ArrayList<>();
+        for (int number : numbers) {
+            if (check.contains(number)) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_DUPLICATION_ERROR.getMessage());
+            }
+            check.add(number);
         }
     }
 }

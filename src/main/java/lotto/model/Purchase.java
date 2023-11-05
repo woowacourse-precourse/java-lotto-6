4b4292028fd9e4a76f649a7f20fa0a2513240bdf
IE-MangChi <hsm983278@gmail.com>
2023-11-05 17:47:25 +0900

    feat: Purchase 기능 추가
    구매한 로또들과 돈을 다루는 클래스
    - getLottos: 구매한 로또 리스트 반환

---
+++ src/main/java/lotto/model/Purchase.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Purchase {
+    private Lottos lottos;
+    private Money money;
+
+    private Purchase(Lottos lottos, Money money) {
+        this.lottos = lottos;
+        this.money = money;
+        lottos.addLotto(money.availableLottoCount());
+    }
+
+    public static Purchase purchase(Lottos lottos, Integer money) {
+        return new Purchase(lottos, new Money(money));
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos.getLottos();
+    }
+}

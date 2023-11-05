d5018eb8c53b1f26f973811a180f9f2b0b8b84eb
doha <dohaahi@naver.com>
2023-11-05 22:25:50 +0900

    feat (LottoMachine): 로또 추첨 기능 구현

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -2,11 +2,16 @@ package lotto.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.DrawingResults;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.Rank;
+import lotto.domain.dto.BonusNumberDto;
+import lotto.domain.dto.DrawingResultDto;
 import lotto.domain.dto.LottoDto;
 import lotto.domain.dto.LottosDto;
 import lotto.domain.dto.PurchaseAmountDto;
+import lotto.domain.dto.WinningLottoDto;
 
 public class LottoMachine {
 
@@ -25,5 +30,26 @@ public class LottoMachine {
         }
 
         return lottoStore;
+    }
+
+    public DrawingResultDto draw(final LottosDto lottosDto, final WinningLottoDto winningLottoDto,
+                                 final BonusNumberDto bonusNumberDto) {
+        List<LottoDto> lottos = lottosDto.lottos();
+
+        DrawingResults drawingResults = new DrawingResults();
+
+        for (LottoDto lotto : lottos) {
+            int matchedWinningLottoCount = lotto.matchWinningLottoCount(winningLottoDto);
+            boolean hasBonusNumber = lotto.hasBonusNumber(bonusNumberDto);
+
+            Rank calculateResult = Rank.calculate(matchedWinningLottoCount, hasBonusNumber);
+            drawingResults.count(calculateResult);
+        }
+
+        return toDrawingResultDto(drawingResults);
+    }
+
+    private DrawingResultDto toDrawingResultDto(final DrawingResults drawingResults) {
+        return new DrawingResultDto(drawingResults.getResults());
     }
 }

871e0c5d00240a9140e30b656c5d44424964ac8d
qudcks8084 <https://github.com/qudcks8084>
2023-11-05 22:29:10 +0900

    [MODIFY] 사용자 입력을 받는 두개의 기능에 대한 리팩토리 진행

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -2,11 +2,14 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoController {
 
     // instance
+    private static final int UNIT = 1000;
     private final LottoDB lottoDB;
     private final LottoView lottoView;
 
@@ -19,40 +22,25 @@ public class LottoController {
 
     // Method
     public void start() {
-        getLottoCountFromUser();
+        setLottoCountFromUser();
         setUserLottoNumbers();
         setLottoWinningNumbers();
     }
 
-    public void getLottoCountFromUser() throws IllegalArgumentException {
+    public void setLottoCountFromUser() throws IllegalArgumentException {
         boolean pass = true;
         do {
             try {
                 lottoView.userCountInputAnnouncement();
-                String userInputStringLottoCount = Console.readLine();
-                int userInputIntegerLottoCount = convertStringToInteger(userInputStringLottoCount);
-                checkUserInputIsThousandUnit(userInputIntegerLottoCount);
-                lottoDB.setUserLottoCount(userInputIntegerLottoCount);
+                String userInputString = Console.readLine();
+                int userInputInteger = convertStringToInteger(userInputString);
+                checkUserInputIsThousandUnit(userInputInteger);
+                lottoDB.saveUserLottoCount(userInputInteger / UNIT);
                 pass = false;
             } catch (IllegalArgumentException e) {
                 lottoView.printError(e.getMessage());
             }
         } while (pass);
-    }
-
-    public Integer convertStringToInteger(String Input) throws IllegalArgumentException {
-        try {
-            return Integer.parseInt(Input);
-        } catch (NumberFormatException | NullPointerException e) {
-            throw new IllegalArgumentException("[ERROR] : 입력된 값이 정수가 아닙니다.");
-        }
-    }
-
-    public void checkUserInputIsThousandUnit(int userInput) throws IllegalArgumentException {
-        int countMod = userInput % 1000;
-        if (countMod != 0) {
-            throw new IllegalArgumentException("[ERROR] : 입력된 값이 1,000단위가 아닙니다.");
-        }
     }
 
     public void setUserLottoNumbers() {
@@ -70,15 +58,46 @@ public class LottoController {
         do {
             try {
                 lottoView.userInputLottoWinningNumberAnnouncement();
-                lottoDB.clearWinningNumbers();
-                lottoDB.getWinningNumbers();
-                lottoDB.convertStringListToIntegerList();
-                this.lotto = lottoDB.getLottoInstance();
-                lotto.test();
+                String userInputString = Console.readLine();
+                String[] userInputStringArray = userInputString.split(",");
+                List<Integer> userInputIntegerArray = convertStringListToIntegerList(userInputStringArray);
+                this.lotto = new Lotto(userInputIntegerArray);
                 pass = false;
             } catch (IllegalArgumentException e) {
                 lottoView.printError(e.getMessage());
             }
         } while (pass);
+    }
+
+    public ArrayList<Integer> convertStringListToIntegerList(String[] input) throws IllegalArgumentException {
+        ArrayList<String> ArrayString = new ArrayList<>(Arrays.asList(input));
+        ArrayList<Integer> ArrayInteger = new ArrayList<>();
+        for (String stringNumbers : ArrayString) {
+            int integerNumber = convertStringToInteger(stringNumbers);
+            checkWinningNumbersInRange(integerNumber);
+            ArrayInteger.add(integerNumber);
+        }
+        return ArrayInteger;
+    }
+
+    public Integer convertStringToInteger(String stringInput) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(stringInput);
+        } catch (NumberFormatException | NullPointerException e) {
+            throw new IllegalArgumentException("[ERROR] : 입력된 값이 정수가 아닙니다.");
+        }
+    }
+
+    public void checkWinningNumbersInRange(int checkNumber) throws IllegalArgumentException {
+        if (checkNumber < 1 || checkNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] : 입력된 값이 1-45사이의 값이 아닙니다.");
+        }
+    }
+
+    public void checkUserInputIsThousandUnit(int userInput) throws IllegalArgumentException {
+        int countMod = userInput % UNIT;
+        if (countMod != 0) {
+            throw new IllegalArgumentException("[ERROR] : 입력된 값이 1,000단위가 아닙니다.");
+        }
     }
 }
--- src/main/java/lotto/LottoDB.java
+++ src/main/java/lotto/LottoDB.java
@@ -1,27 +1,17 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LottoDB {
 
     // instance
     private final ArrayList<Integer> userLottoNumbers;
-    private final ArrayList<String> winningNumbersString;
-    private final ArrayList<Integer> winningNumbersInteger;
     private int userLottoCount;
 
     public LottoDB() {
         userLottoNumbers = new ArrayList<>();
-        winningNumbersString = new ArrayList<>();
-        winningNumbersInteger = new ArrayList<>();
         userLottoCount = 0;
-    }
-
-    public void setUserLottoCount(int userLottoCount) throws IllegalArgumentException {
-        this.userLottoCount = userLottoCount / 1000;
     }
 
     public int getUserLottoCount() {
@@ -32,42 +22,8 @@ public class LottoDB {
         userLottoNumbers.addAll(lottoNumbers);
     }
 
-    public void clearWinningNumbers() {
-        winningNumbersString.clear();
-        winningNumbersInteger.clear();
-    }
-
-    public void getWinningNumbers() {
-        String input = Console.readLine();
-        String[] inputList = input.split(",");
-        winningNumbersString.addAll(Arrays.asList(inputList));
-    }
-
-    public void convertStringListToIntegerList() throws IllegalArgumentException {
-        for (String stringNumbers : winningNumbersString) {
-            int integerNumber = convertStringToInteger(stringNumbers);
-            checkWinningNumbersInRange(integerNumber);
-            winningNumbersInteger.add(integerNumber);
-        }
-    }
-
-    public Integer convertStringToInteger(String stringInput) throws IllegalArgumentException {
-        try {
-            return Integer.parseInt(stringInput);
-        } catch (NumberFormatException | NullPointerException e) {
-            throw new IllegalArgumentException("[ERROR] : 입력된 값이 정수가 아닙니다.");
-        }
-    }
-
-    public void checkWinningNumbersInRange(int checkNumber) throws IllegalArgumentException {
-        if (checkNumber < 1 || checkNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] : 입력된 값이 1-45사이의 값이 아닙니다.");
-        }
-    }
-
-    public Lotto getLottoInstance() throws IllegalArgumentException {
-        return new Lotto(winningNumbersInteger);
-
+    public void saveUserLottoCount(int userLottoCountIn) {
+        this.userLottoCount = userLottoCountIn;
     }
 
 }

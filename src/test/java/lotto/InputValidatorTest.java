42bdb78af32eac1694a9cc519e5963a845e0bada
seondays <agseon9@gmail.com>
2023-11-05 21:19:00 +0900

    test(InputValidatorTest): 테스트하는 메서드명 변경
    public 메서드를 이용, 간접적으로 테스트하기 위해 테스트하는 메서드명 변경 및 init 수정

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -1,34 +1,37 @@
-는package lotto;
+package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.utils.InputValidator;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputValidatorTest {
+    InputValidator validator;
+
     @Test
-    @BeforeAll
+    @BeforeEach
     void init() {
-        InputValidator validator = new InputValidator();
+        validator = new InputValidator();
     }
+
     @ParameterizedTest
     @DisplayName("입력이 빈 값이면 예외가 발생한다")
-    @ValueSource(strings = {""," "})
-    void createInputIsNull(String input) {
-        assertThatThrownBy(() -> validator.validateBlankInput(input))
+    @ValueSource(strings = {"", " "})
+    void createInputIsBlank(String input) {
+        assertThatThrownBy(() -> validator.validatePurchaseCostInputView(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 빈 값을 입력할 수 없습니다");
     }
 
     @ParameterizedTest
     @DisplayName("입력이 정수가 아니면 예외가 발생한다")
-    @ValueSource(strings = {"234","abc"})
-    void createInputIsNull(String input) {
-        assertThatThrownBy(() -> validator.validateBlankInput(input))
+    @ValueSource(strings = {"로또", "abc"})
+    void createInputIsNotInt(String input) {
+        assertThatThrownBy(() -> validator.validatePurchaseCostInputView(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 정수가 아닌 다른 문자는 입력할 수 없습니다");
     }
@@ -36,7 +39,7 @@ public class InputValidatorTest {
     @Test
     @DisplayName("금액 입력이 1000이상 100,000이하의 정수가 아니면 예외가 발생한다")
     void createCostInputOutOfSize() {
-        assertThatThrownBy(() -> validator.validateCostOutofSize("900"))
+        assertThatThrownBy(() -> validator.validatePurchaseCostInputView("900"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 구매 금액은 1000원 이상 100,000원 이하로만 가능합니다");
 
@@ -45,7 +48,7 @@ public class InputValidatorTest {
     @Test
     @DisplayName("입력된 금액이 1000원 단위가 아니면 예외가 발생한다")
     void createCostInputNotInUnit() {
-        assertThatThrownBy(() -> validator.validateCostNotInUnit(1200))
+        assertThatThrownBy(() -> validator.validatePurchaseCostInputView("1200"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 구매 금액은 1000원 단위로만 가능합니다");
     }

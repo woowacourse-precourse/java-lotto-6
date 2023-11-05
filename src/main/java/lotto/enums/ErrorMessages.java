16f314bf904c01c30a6f7aace63073bf84da9f3c
Isaac Han <isaachan233@gmail.com>
2023-11-06 01:29:36 +0900

    feat: 당첨 결과 없을시 메세지 생성

--- src/main/java/lotto/enums/ErrorMessages.java
+++ src/main/java/lotto/enums/ErrorMessages.java
@@ -10,7 +10,8 @@ public enum ErrorMessages {
     NON_THOUSAND_INPUT_MESSAGE("1,000원 단위의 수를 입력해주세요."),
     WINNING_NUMBERS_INCLUDE_BONUS_MESSAGE("당첨 번호에 보너스 번호가 포함되어 있습니다. 다른 수를 적어주세요."),
 
-    TICKETS_WAS_NOT_CREATED("어떠한 LottoTicket 도 없습니다. LottoPurchase 의 generateLottoTickets (로또 생성) 후 호출 될 수 있습니다.");
+    TICKETS_WAS_NOT_CREATED("어떠한 LottoTicket 도 없습니다. LottoTicket 생성해주세요. LottoPurchase 의 generateLottoTickets (로또 생성) 후 호출 될 수 있습니다."),
+    RESULT_WAS_NOT_CREATED("어떠한 WinningResult 도 없습니다. WinningResult 를 생성해주세요.")
     ;
 
     private final String message;

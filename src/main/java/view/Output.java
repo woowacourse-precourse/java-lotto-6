282208509978e486811ca94469c81dc5dab3514e
zzawang <hobin0116@naver.com>
2023-11-06 05:44:02 +0900

    refactor: Output 클래스가 출력 기능만을 담당하도록 수정 및 출력 형식에 맞게 수정
    - Output 클래스의 로또 번호 생성 기능을 삭제하고 출력 기능만을 담당하도록 수정하였다.
    - 한 줄 건너띄기용으로 사용되는 "\n"을 상수로 정의하였다.
    - 출력 형식에 맞추어 필요한 곳에 상수를 사용하였다.

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,39 +1,22 @@
 package view;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import lotto.Lotto;
 
 public class Output {
     private static final String PURCHASE_AMOUNT_MESSAGE = "%d개를 구매했습니다.";
     private static final String LOTTO_NUMBERS_SEPARATOR = "[%s]";
     private static final String LOTTO_NUMBERS_JOIN_SEPARATOR = ", ";
+    private static final String LINE_BREAK = "\n";
 
-    public static List<Lotto> getLottoNumbers(int purchaseAmount) {
-        int lottoAmount = purchaseAmount / 1000;
-        System.out.printf(PURCHASE_AMOUNT_MESSAGE + "%n", lottoAmount);
-
-        List<Lotto> lottoList = generateLottoNumbers(lottoAmount);
-
-        printLottoNumbers(lottoList);
-
-        return lottoList;
-    }
-
-    private static List<Lotto> generateLottoNumbers(int lottoAmount) {
-        return IntStream.range(0, lottoAmount)
-                .mapToObj(i -> new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
-                .collect(Collectors.toList());
-    }
-
-    private static void printLottoNumbers(List<Lotto> lottoList) {
+    public static void getLottoNumbers(List<Lotto> lottoList) {
+        System.out.printf(LINE_BREAK + PURCHASE_AMOUNT_MESSAGE + LINE_BREAK, lottoList.size());
         lottoList.forEach(lotto -> {
             List<Integer> numbers = lotto.getNumbers();
             String lottoNumbers = formatLottoNumbers(numbers);
-            System.out.printf(LOTTO_NUMBERS_SEPARATOR + "%n", lottoNumbers);
+            System.out.printf(LOTTO_NUMBERS_SEPARATOR + LINE_BREAK, lottoNumbers);
         });
     }
 

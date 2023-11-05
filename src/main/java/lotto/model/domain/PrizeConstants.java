336de494bffd94337d55aa46944bfc27da7a818e
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-05 14:08:40 +0900

    refactor: 상금 상수의 확장성을 위해 THREE_MATCH 미만의 상수들도 추가

--- src/main/java/lotto/model/domain/PrizeConstants.java
+++ src/main/java/lotto/model/domain/PrizeConstants.java
@@ -1,11 +1,15 @@
 package lotto.model.domain;
 
 public enum PrizeConstants {
+    NOT_MATCH(0),
+    ONE_MATCH(0),
+    TWO_MATCH(0),
     THREE_MATCH(5000),
     FOUR_MATCH(50000),
     FIVE_MATCH(1500000),
     FIVE_MATCH_WITH_BONUS(30000000),
-    SIX_MATCH(2000000000);
+    SIX_MATCH(2000000000),
+    ;
 
     private final int prize;
 

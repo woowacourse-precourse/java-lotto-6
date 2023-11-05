f1644c736a202953c4a4a5eb8be8bfd3fac185bf
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-11-06 06:42:08 +0900

    feat(dto): 등수별로 당첨된 로또 개수 리스트로 반환하는 기능 구현

--- src/main/java/lotto/dto/WinningLottoResult.java
+++ src/main/java/lotto/dto/WinningLottoResult.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.model.Lotto;
 import lotto.model.LottoRank;
 import lotto.model.WinningLotto;
@@ -19,12 +20,17 @@ public class WinningLottoResult {
         ranks.forEach(rank -> result.put(rank, 0));
     }
 
-    public void countWinningResult(WinningLotto winningLotto, Lotto lotto) {
+    public void countWinningResult(final WinningLotto winningLotto, final Lotto lotto) {
         LottoRank rank = winningLotto.calculateLottoRank(lotto);
         result.put(rank, result.get(rank) + 1);
     }
 
-    public int getCountByLottoRank(LottoRank rank) {
+    public int getCountByLottoRank(final LottoRank rank) {
         return result.get(rank);
+    }
+
+    public List<Integer> getCounts() {
+        return result.values().stream()
+                .collect(Collectors.toUnmodifiableList());
     }
 }

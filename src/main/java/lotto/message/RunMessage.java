2aa0837b802f516a3ab7441332445ee9ab218f50
josephuk77 <josephuk77@gmail.com>
2023-11-06 02:33:41 +0900

    feat(RunMesseage): 총 수익률 출력 메세지 추가

--- src/main/java/lotto/message/RunMessage.java
+++ src/main/java/lotto/message/RunMessage.java
@@ -6,12 +6,14 @@ public enum RunMessage {
     PRINT_LOTTO_COUNT("%d개를 구매했습니다.\n"),
     INPUT_WINNING_MESSAGE("당첨 번호를 입력해 주세요.\n"),
     INPUT_BONUS_MESSAGE("보너스 번호를 입력해 주세요.\n"),
-    PRINT_WINNING_STATISTIC("당첨 통계\n---\n"),
+    PRINT_WINNING_STATISTIC_MESSAGE("당첨 통계\n---\n"),
     PRINT_WINNING_FIRST("6개 일치 (2,000,000,000원) - %d개\n"),
     PRINT_WINNING_SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
     PRINT_WINNING_THIRD("5개 일치 (1,500,000원) - %d개\n"),
     PRINT_WINNING_FOURTH("4개 일치 (50,000원) - %d개\n"),
-    PRINT_WINNING_FIFTH("3개 일치 (5,000원) - %d개\n");
+    PRINT_WINNING_FIFTH("3개 일치 (5,000원) - %d개\n"),
+    PRINT_WINNING_STATISTIC("총 수익률은 %.1f%%입니다.\n");
+
 
     private final String message;
 

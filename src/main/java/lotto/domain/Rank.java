6f3bba161a60d4adb745abe9e4de76a57b43188f
june-777 <wlwhswnsrl96@gmail.com>
2023-11-05 18:54:47 +0900

    refactor(Rank): 보너스 매칭 여부 상수를 List로 수정하고 등수 계산 로직을 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,20 +1,21 @@
 package lotto.domain;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public enum Rank {
-    FIRST(6, false, new BigDecimal(2_000_000_000L)),
-    SECOND(5, true, new BigDecimal(30_000_000L)),
-    THIRD(5, false, new BigDecimal(1_500_000L)),
-    FOURTH(4, false, new BigDecimal(50_000L)),
-    FIFTH(3, false, new BigDecimal(5_000L)),
-    NONE(0, false, new BigDecimal(0L));
+    FIRST(6, List.of(true, false), new BigDecimal(2_000_000_000L)),
+    SECOND(5, List.of(true), new BigDecimal(30_000_000L)),
+    THIRD(5, List.of(false), new BigDecimal(1_500_000L)),
+    FOURTH(4, List.of(true, false), new BigDecimal(50_000L)),
+    FIFTH(3, List.of(true, false), new BigDecimal(5_000L)),
+    NONE(0, List.of(true, false), new BigDecimal(0L));
 
     private final int matchCounts;
-    private final boolean matchBonus;
+    private final List<Boolean> matchBonus;
     private final BigDecimal price;
 
-    Rank(int matchCounts, boolean matchBonus, BigDecimal price) {
+    Rank(int matchCounts, List<Boolean> matchBonus, BigDecimal price) {
         this.matchCounts = matchCounts;
         this.matchBonus = matchBonus;
         this.price = price;
@@ -22,7 +23,7 @@ public enum Rank {
 
     public static Rank calculateRank(final int matchCounts, final boolean matchBonus) {
         for (Rank rank : values()) {
-            if (rank.matchCounts == matchCounts && rank.matchBonus == matchBonus) {
+            if (rank.matchCounts == matchCounts && rank.matchBonus.contains(matchBonus)) {
                 return rank;
             }
         }

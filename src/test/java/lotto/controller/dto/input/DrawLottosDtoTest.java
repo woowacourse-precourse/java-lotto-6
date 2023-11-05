c1a2a32a95133e95af8054f671f9432f4eae814f
mainfn <_@mainfn.dev>
2023-11-05 11:58:09 +0900

    test: `DrawLottosDto` 테스트 케이스

---
+++ src/test/java/lotto/controller/dto/input/DrawLottosDtoTest.java
@@ -0,0 +1,55 @@
+package lotto.controller.dto.input;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+final class DrawLottosDtoTest {
+    @DisplayName("유효한 로또 번호와 보너스 번호로 DrawLottosDto를 생성할 수 있다.")
+    @Test
+    void DrawLottosDto_withValidLottoNumbersAndBonusNumber_shouldCreateInstance() {
+        // given
+        final List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        final int bonusNumber = 7;
+
+        // when
+        final DrawLottosDto drawLottosDto = new DrawLottosDto(lottoNumbers, bonusNumber);
+
+        // then
+        assertThat(drawLottosDto.getLottoNumbers()).isEqualTo(lottoNumbers);
+        assertThat(drawLottosDto.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+    @DisplayName("유효하지 않은 로또 번호 입력으로 DrawLottosDto 생성 시 예외 발생.")
+    @ParameterizedTest
+//    @NullAndEmptySource
+    @ValueSource(strings = {"123", "xxx", "1.2.3"})
+    void DrawLottosDto_withInvalidLottoNumbers_shouldThrowException(final String lottoNumbersInput) {
+        // given
+        final String bonusNumberInput = "7";
+        // when
+        // then
+        assertThatThrownBy(() ->
+                DrawLottosDto.from(lottoNumbersInput, bonusNumberInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("유효하지 않은 보너스 번호 입력으로 DrawLottosDto 생성 시 예외 발생.")
+    @ParameterizedTest
+//    @NullAndEmptySource
+    @ValueSource(strings = {"x", ".", "1000j"})
+    void DrawLottosDto_withInvalidBonusNumber_shouldThrowException(final String bonusNumberInput) {
+        // given
+        final String lottoNumbersInput = "1,2,3,4,5,6";
+        // when
+        // then
+        assertThatThrownBy(() ->
+                DrawLottosDto.from(lottoNumbersInput, bonusNumberInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

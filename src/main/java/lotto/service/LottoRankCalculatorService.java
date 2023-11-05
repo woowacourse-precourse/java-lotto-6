219c8d1dbfcca943d9050af0b10a4bb73ae8bae9
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-06 05:48:29 +0900

    refactor: LottoRankCalculatorService의 로또 결과 계산 메서드가 LottoResult 타입을 반환하도록 개선

--- src/main/java/lotto/service/LottoRankCalculatorService.java
+++ src/main/java/lotto/service/LottoRankCalculatorService.java
@@ -1,19 +1,24 @@
 package lotto.service;
 
+import java.util.EnumMap;
 import java.util.List;
-import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
 import lotto.domain.WinningNumbers;
 
 public class LottoRankCalculatorService {
 
-    public List<LottoRank> calculateRanks(List<Lotto> lottos, WinningNumbers winningNumbers) {
-        return lottos.stream()
+    public LottoResult calculateRanks(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        EnumMap<LottoRank, Integer> ranks = new EnumMap<>(LottoRank.class);
+
+        lottos.stream()
                 .map(lotto -> LottoRank.of(
                         winningNumbers.countMatchingNumber(lotto),
                         winningNumbers.bonusNumberMatch(lotto)))
-                .collect(Collectors.toList());
+                .forEach(rank -> ranks.put(rank, ranks.getOrDefault(rank, 0) + 1));
+
+        return LottoResult.from(ranks);
     }
 
 }
--- src/test/java/lotto/service/LottoRankCalculatorServiceTest.java
+++ src/test/java/lotto/service/LottoRankCalculatorServiceTest.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
 import lotto.domain.WinningNumbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,29 +15,19 @@ import org.junit.jupiter.api.Test;
 class LottoRankCalculatorServiceTest {
 
 
-    @DisplayName("로또 리스트를 전달해서 등수리스트를 반환받는 메서드 테스트")
+    @DisplayName("로또 리스트를 전달해서 결과를 반환받는다")
     @Test
     void testLottoRankList() {
         Lotto winningLotto = createLotto(List.of(1, 2, 3, 4, 5, 6));
         LottoNumber bonusNumber = LottoNumber.from(15);
         WinningNumbers winningNumbers = WinningNumbers.of(winningLotto, bonusNumber);
 
-        Lotto issuedLotto2 = createLotto(List.of(1, 2, 3, 4, 5, 6)); //1등
-        Lotto issuedLotto1 = createLotto(List.of(1, 2, 3, 4, 5, 15)); //2등
-        Lotto issuedLotto3 = createLotto(List.of(1, 2, 3, 4, 5, 11)); //3등
-        Lotto issuedLotto4 = createLotto(List.of(1, 2, 3, 5, 9, 10)); //4등
-        Lotto issuedLotto5 = createLotto(List.of(1, 2, 3, 17, 18, 19)); //5등
-        Lotto issuedLotto6 = createLotto(List.of(7, 8, 9, 10, 11, 12)); //NONE
-        Lotto issuedLotto7 = createLotto(List.of(7, 8, 9, 10, 11, 12)); //NONE
-        List<Lotto> lottos = List.of(issuedLotto1, issuedLotto2, issuedLotto3, issuedLotto4, issuedLotto5,
-                issuedLotto6, issuedLotto7);
+        Lotto issuedLotto1 = createLotto(List.of(1, 2, 3, 4, 5, 6)); //1등
+        List<Lotto> lottos = List.of(issuedLotto1);
 
         LottoRankCalculatorService lottoRankCalculatorService = new LottoRankCalculatorService();
-        List<LottoRank> ranks = lottoRankCalculatorService.calculateRanks(lottos, winningNumbers);
-        Assertions.assertThat(ranks)
-                .containsExactlyInAnyOrder(LottoRank.FIRST, LottoRank.SECOND, LottoRank.THIRD, LottoRank.FOURTH,
-                        LottoRank.FIFTH,
-                        LottoRank.NONE, LottoRank.NONE);
+        LottoResult result = lottoRankCalculatorService.calculateRanks(lottos, winningNumbers);
+        Assertions.assertThat(result.getCountByRank(LottoRank.FIRST)).isEqualTo(1);
     }
 
     private Lotto createLotto(List<Integer> numbers) {

7bc1ee05f1b3bab47beaba6b17b9ff91d1c56d1e
hil679 <hil679922@gmail.com>
2023-11-05 22:55:23 +0900

    Feat(isBonusNumContain): 보너스 번호가 포함되어 있는지 확인하는 메소드 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -27,6 +27,6 @@ public class WinningLotto {
     }
 
     public boolean isBonusNumContain(Lotto userLotto) {
-        return true;
+        return userLotto.hasSameNumber(bonusLottoNum);
     }
 }

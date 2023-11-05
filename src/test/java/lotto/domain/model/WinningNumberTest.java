024b9c37ed8342f75613a5fb9df79e5fd1578d69
DH Jeong <wjdehgns97@gmail.com>
2023-11-05 19:08:25 +0900

    Feat: 당첨 번호 생성 테스트
    WinningNumberTest.java
    createWinningNumberByOverSize()
    createWinningNumberByDuplicatedNumber()
    createWinningNumberByOutOfRangeNumber()

---
+++ src/test/java/lotto/domain/model/WinningNumberTest.java
@@ -0,0 +1,37 @@
+package lotto.domain.model;
+
+import lotto.enums.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningNumberTest {
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOverSize() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_COUNT_OF_VALUES.getMessage());
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByDuplicatedNumber() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_HAS_DUPLICATE_NUMBERS.getMessage());
+    }
+
+    @DisplayName("당첨 번호 중 1~45의 범위에 속하지 않는 숫자가 존재하면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 65, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_OUT_OF_RANGES.getMessage());
+    }
+}

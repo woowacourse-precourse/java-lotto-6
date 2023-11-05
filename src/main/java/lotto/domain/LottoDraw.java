e8ba67a73751b04723886c8ac621e4f4212e7ff9
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-05 21:22:30 +0900

    feat : LottoDraw 생성 중 보너스 번호 와 로또 번호가 중복인지 검증 기능 구현
    LottoDraw Domain 생성자에서 검증
    LottoException 발생해 Exception 구분
    이에 따른 , 테스트 구현

--- src/main/java/lotto/domain/LottoDraw.java
+++ src/main/java/lotto/domain/LottoDraw.java
@@ -1,8 +1,17 @@
 package lotto.domain;
 
+import lotto.exception.LottoException;
+import lotto.exception.LottoExceptionMessage;
+
 public record LottoDraw(Lotto winningLotto , int bonusNumber) {
     public LottoDraw(Lotto winningLotto , int bonusNumber){
+        validateLottoContainBonusNumber(winningLotto,bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+    private static void validateLottoContainBonusNumber(Lotto winningLotto,int bonusNumber){
+        if (winningLotto.getNumbers().contains(bonusNumber)){
+            throw new LottoException(LottoExceptionMessage.DUPLICATE_BONUS_NUMBER);
+        }
     }
 }
--- src/test/java/lotto/domain/LottoDrawTest.java
+++ src/test/java/lotto/domain/LottoDrawTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.exception.LottoException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,4 +16,16 @@ public class LottoDrawTest {
         LottoDraw lottoDraw = new LottoDraw(lotto,bonusNumber);
         Assertions.assertEquals(LottoDraw.class,lottoDraw.getClass());
     }
+    @Test
+    @DisplayName("로또 번호들에 있는 보너스 번호를 입력하면 예외를 발생한다.")
+    void throwExceptionWhenLottoContainBonusNumber(){
+        var randomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        Lotto lotto = new Lotto(randomNumbers);
+        var duplicatedBonusNumber = lotto.getNumbers().get(0);
+        Assertions.assertThrows(LottoException.class,()->{
+            new LottoDraw(lotto,duplicatedBonusNumber);
+        });
+
+    }
+
 }

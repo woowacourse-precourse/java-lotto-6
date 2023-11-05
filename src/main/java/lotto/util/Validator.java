d8ecfcfb22b7fa67f23c0a0de423991f869c777a
Yerinnnn <shinyerin0216@gmail.com>
2023-11-05 22:14:05 +0900

    Feat: 입력 받은 당첨 번호가 사용 가능한 번호인지 검증

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,9 +1,26 @@
 package lotto.util;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Validator {
 
     public static void purchasePrice(int purchasePrice) {
         if (purchasePrice % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void winNumberNum(List<Integer> winNumber) {
+        if (winNumber.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void winNumberDuplication(List<Integer> winNumber) {
+        Set<Integer> setWinNumber = winNumber.stream().collect(Collectors.toSet());
+        if (winNumber.size() != setWinNumber.size()) {
             throw new IllegalArgumentException();
         }
     }

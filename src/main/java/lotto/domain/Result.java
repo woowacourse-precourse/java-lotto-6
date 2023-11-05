80708c37c223eda5b9c6b0b6b5a063a939c5b682
yubin <dbqls2821@gmail.com>
2023-11-05 23:08:41 +0900

    feat: 일치 결과를 상수로 가지는 Result enum 클래스 추가

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+enum Result {
+    SIX_MATCH(6, "6개 일치", 2_000_000_000),
+    FIVE_MATCH_WITH_BONUS(5, "5개 일치, 보너스 볼 일치", 30_000_000),
+    FIVE_MATCH(5, "5개 일치", 1_500_000),
+    FOUR_MATCH(4, "4개 일치", 50_000),
+    THREE_MATCH(3, "3개 일치", 5_000),
+    ;
+
+    private final Integer match;
+    private final String content;
+    private final Integer reward;
+
+    Result(Integer match, String content, Integer reward) {
+        this.match = match;
+        this.content = content;
+        this.reward = reward;
+    }
+
+    public static Result of(Integer matchCount, boolean isBonusMatched) {
+        if (matchCount.equals(FIVE_MATCH_WITH_BONUS.reward) && isBonusMatched) return FIVE_MATCH_WITH_BONUS;
+        return Arrays.stream(values())
+                .filter(result -> matchCount.equals(result.match))
+                .findFirst()
+                .orElseThrow();
+    }
+}

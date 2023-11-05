43cab09c3c999cc70c261460690b8b8faca184eb
Son JunHyung <kingjh1125@gmail.com>
2023-11-05 18:55:41 +0900

    refactor (RegexPattern): 정규표현식 분리
    Lotto에서 정규표현식과 관련된 상수를 분리하였습니다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,11 @@
 package lotto;
 
 import java.util.List;
-import java.util.regex.Pattern;
+import util.RegexPattern;
 import util.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private final Pattern lottoPattern = Pattern.compile("[1-9]|[1-3][0-9]|4[0-5]");
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -33,7 +32,7 @@ public class Lotto {
 
     private void validateRange(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (!lottoPattern.matcher(Integer.toString(number)).matches()) {
+            if (!RegexPattern.lotto.matcher(Integer.toString(number)).matches()) {
                 throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE.getErrorMessage());
             }
         }
---
+++ src/main/java/util/RegexPattern.java
@@ -0,0 +1,8 @@
+package util;
+
+import java.util.regex.Pattern;
+
+public class RegexPattern {
+    public static final Pattern lotto = Pattern.compile("[1-9]|[1-3][0-9]|4[0-5]");
+
+}

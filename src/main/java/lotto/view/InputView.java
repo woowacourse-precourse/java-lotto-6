a88ad96475e483d040904dede5e0d930534f87fd
swoolee97 <swoolee97@ajou.ac.kr>
2023-11-06 07:03:17 +0900

    refactor: 당첨 번호를 입력받을 때 쉼표로 구분하라는 문구 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,7 +8,7 @@ import lotto.message.ExceptionMessage;
 
 public class InputView {
     private static final String READ_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String READ_WINNER_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String READ_WINNER_NUMBERS_MESSAGE = "당첨 번호를 쉼표(,)로 구분하여 입력해 주세요.";
     private static final String READ_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요";
 
     public static int readPurchaseAmount() {
@@ -17,7 +17,7 @@ public class InputView {
             String inputPurchaseAmount = Console.readLine();
             return Integer.parseInt(inputPurchaseAmount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ExceptionMessage.INPUT_NOT_NUMBER.getMessage());
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NATURAL_NUMBER.getMessage());
         }
     }
 
@@ -29,7 +29,7 @@ public class InputView {
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ExceptionMessage.INPUT_NOT_NUMBER.getMessage());
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NATURAL_NUMBER.getMessage());
         }
     }
 
@@ -39,7 +39,7 @@ public class InputView {
         try {
             return Integer.parseInt(inputBonusNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ExceptionMessage.INPUT_NOT_NUMBER.getMessage());
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NATURAL_NUMBER.getMessage());
         }
     }
 }

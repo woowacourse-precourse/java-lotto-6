6da6b8984aee024d4666c31dc3bb59745c5d9979
rakyun1 <dbskim234@naver.com>
2023-11-04 22:44:22 -0600

    Refactor: add validateDuplicateNumber(), add checkRank() 로또 번호 중복에 대한 예외처리 함수를 만들고, Result 값으로 리턴하는 등수를 체크하는 함수를 만듦

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,15 @@
 package lotto;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -17,29 +19,38 @@ public class Lotto {
         }
     }
 
-    public RankEnum checkRank(int bonus, int[] winnerNumber){
+    private void validateDuplicateNumber(List<Integer> numbers){
+		Set<Integer> duplicated = new HashSet<>(numbers);
+        if (duplicated.size() != 6){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Result checkRank(List<Integer> winnerNumber, int bonus){
         int count = 0;
+
         for (int lottoNumber : numbers) {
-            if (Arrays.binarySearch(winnerNumber, lottoNumber) >= 0) {
+            if (winnerNumber.contains(lottoNumber)) {
                 count++;
             }
         }
+
         if (count == 6){
-            return RankEnum.FIRST;
+            return new Result(RankEnum.FIRST, 2000000000);
         }
         if (count == 5){
-            if (Arrays.binarySearch(winnerNumber, bonus) >= 0){
-                return RankEnum.SECOND;
+            if (numbers.contains(bonus)){
+                return new Result(RankEnum.SECOND, 30000000);
             }
-            return RankEnum.THIRD;
+            return new Result(RankEnum.THIRD, 1500000);
         }
         if (count == 4){
-            return RankEnum.FORTH;
+            return new Result(RankEnum.FORTH, 50000);
         }
         if (count == 3){
-            return RankEnum.FIFTH;
+            return new Result(RankEnum.FIFTH, 5000);
         }
-        return RankEnum.NONE;
+        return new Result(RankEnum.NONE, 0);
     }
 
 }

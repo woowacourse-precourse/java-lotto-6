e7469dc82fe12a23f6f70f683e5f15d937612ca1
junghunim07 <dlawndgns9@naver.com>
2023-11-05 21:21:10 +0900

    feat(Output): 당첨 내역 출력

--- src/main/java/lotto/ui/Output.java
+++ src/main/java/lotto/ui/Output.java
@@ -12,4 +12,14 @@ public class Output {
     public void notifyLottoNumbers(List<Integer> lottoTickets) {
         System.out.println(lottoTickets);
     }
+
+    public void notifyWinningDetails(List<Integer> result) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("3개 일치 (5000원) - %d개\n", result.get(4));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", result.get(3));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", result.get(2));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", result.get(1));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", result.get(0));
+    }
 }

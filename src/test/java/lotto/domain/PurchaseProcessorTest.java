01e7e4b8db6edd7ba8581a893ba8a6cf3addf43d
sungwon2598 <tjddnjs2598@gmail.com>
2023-11-05 14:37:23 +0900

    test(PurchaseProcessorTest) : 입력 가격이 숫자 형식이 아닐 경우 예외 발생 확인 테스트 추가

--- src/test/java/lotto/domain/PurchaseProcessorTest.java
+++ src/test/java/lotto/domain/PurchaseProcessorTest.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import lotto.constant.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,14 @@ class PurchaseProcessorTest {
     void validPrice() {
         PurchaseProcessor purchaseProcessor = new PurchaseProcessor("3000");
         assertThat(purchaseProcessor.getLottoCount()).isEqualTo(3);
+    }
+
+    @DisplayName("입력 가격이 숫자 형식이 아닐 경우 예외 발생")
+    @Test
+    void invalidPriceFormat() {
+        assertThatThrownBy(() -> new PurchaseProcessor("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.INPUT_NUMBER.getMessage());
     }
 
 }

77ba7f45b6189249dd7c8108a745d2e68899bb25
woong2e <tlswodnd12345@naver.com>
2023-11-06 02:56:19 +0900

    feat: 입력받는 기능 구현 (구매 금액, 당첨 번호, 보너스 번호)

--- src/main/java/lotto/domain/Input.java
+++ src/main/java/lotto/domain/Input.java
@@ -2,26 +2,97 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Input {
     private static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTERY_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     private Input() {
     }
 
-    public String readPurchaseMoney() {
+    public static int askHowManyLottoPurchase() {
+        while (true) {
+            try {
+                String sum = readPurchaseMoney();
+                int money = validateType(sum);
+                validateMoneyRange(money);
+                return money;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    private static String readPurchaseMoney() {
         System.out.println(PURCHASE_MESSAGE);
         return Console.readLine();
     }
 
-    public String readWinningLotteryNumbers() {
+    private static void validateMoneyRange(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException("[ERROR] 자연수를 입력하시오");
+        }
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하시오");
+        }
+    }
+
+
+    public static List<Integer> askTheWinningLotteryNumbers() {
+        while (true) {
+            try {
+                String numberString = readTheWinningLotteryNumbers();
+                List<Integer> numbers = separateString(numberString);
+                return numbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static String readTheWinningLotteryNumbers() {
         System.out.println(WINNING_LOTTERY_NUMBERS_MESSAGE);
         return Console.readLine();
     }
 
-    public String readBonusNumber() {
+    private static List<Integer> separateString(String numberString) {
+        return Arrays.stream(numberString.split(","))
+                .map(Input::validateType)
+                .collect(Collectors.toList());
+    }
+
+    public static int askTheWinningLotteryBonusNumber() {
+        while (true) {
+            try {
+                String bonus = readTheWinningLotteryBonusNumber();
+                int bonusNumber = validateType(bonus);
+                validateNumberRange(bonusNumber);
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static String readTheWinningLotteryBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
         return Console.readLine();
+    }
+
+    private static void validateNumberRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+
+    private static int validateType(String sum) {
+        try {
+            return Integer.parseInt(sum);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력하십시오");
+        }
     }
 }

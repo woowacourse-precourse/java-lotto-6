ae586e562d9c2a7a64255427a4147a9516a476ab
goodchoi <jangu3384@gmail.com>
2023-11-06 01:24:17 +0900

    feat: 로또 번호를 조건에 맞게 생성하는 기능 추가

---
+++ src/main/java/lotto/model/RadomNumbersGeneratePolicy.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.constant.LottoConstant;
+
+public class RadomNumbersGeneratePolicy {
+    public List<Integer> generate() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() != LottoConstant.SIZE) {
+            final Integer newNumber = pickRandomNumber();
+            updateNumbers(numbers, newNumber);
+        }
+        return numbers;
+    }
+
+    private void updateNumbers(List<Integer> numbers, final Integer newNumber) {
+        if (!numbers.contains(newNumber)) {
+            numbers.add(newNumber);
+        }
+    }
+
+    private Integer pickRandomNumber() {
+        return Randoms.pickNumberInRange(
+                LottoConstant.MIN_NUMBER,
+                LottoConstant.MAX_NUMBER
+        );
+    }
+}

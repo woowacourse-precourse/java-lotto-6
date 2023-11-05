9a5511f6d123510f2f76133b66704468c6e78f28
Hajuuu <hpt0127@naver.com>
2023-11-05 20:45:33 +0900

    feat(Application): createLotto 작성
    로또 구입 수량만큼 로또를 발행하여 오름차순으로 정렬해 lottos에 저장하는 createLotto 함수 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,44 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+
+    private static final String START = "구입금액을 입력해 주세요.";
+    private static final int LOTTO_PRICE = 1000;
+    private static final String PURCHASE = "개를 구매했습니다.";
+    private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(START);
+        int amount = Integer.parseInt(readLine());
+        int numberOfLotto = amount / LOTTO_PRICE;
+
+        System.out.println();
+        System.out.println(numberOfLotto + PURCHASE);
+        List<Lotto> lottos = createLotto(numberOfLotto);
+        printLotto(lottos);
+    }
+
+    public static List<Lotto> createLotto(int numberOfLotto) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> lotto = Lotto.randomNumber();
+            Collections.sort(lotto);
+            lottos.add(new Lotto(lotto));
+        }
+        return lottos;
+    }
+
+    public static void printLotto(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }

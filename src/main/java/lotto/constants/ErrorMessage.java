7f786c87a56ab4842dd7d3ad28edab477b7fd23d
nove1080 <skwpqjq@gmail.com>
2023-11-05 23:44:04 +0900

    refactor: 구매 가격 예외 메시지 이름 재정의

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.constants;
 public enum ErrorMessage {
 
     LABEL("[ERROR]"),
-    NOT_DIVISIBLE_1000("1,000원 단위로만 입력 가능합니다."),
+    NOT_DIVISIBLE_TICKET_PRICE(Lotto.COST.getValue() + "원 단위로만 입력 가능합니다."),
     OUT_OF_RANGE("1,000 ~ 2,147,483,000 사이의 값만 허용합니다."),
     INVALID_LOTTO_NUMBER_FORMAT("공백없이 콤마(,)를 기준으로 구분하여 숫자를 입력하세요."),
     DUPLICATE_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
@@ -18,5 +18,9 @@ public enum ErrorMessage {
     @Override
     public String toString() {
         return message;
+    }
+
+    public String getLabeledErrorMessage() {
+        return LABEL + " " + message;
     }
 }

43e2b026a5e062559280807ea4336f12dd2717cb
jeondui <jeondui@gmail.com>
2023-11-06 03:07:28 +0900

    test(LottoTicketTest): amount값을 String형으로 전달하는 방식으로 테스트 수정

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -2,7 +2,6 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +10,11 @@ class LottoTicketTest {
     @DisplayName("로또 티켓 생성 테스트")
     @Test
     void generateTickets() {
-        int ticketCount = 5;
+        String amount = "8000";
+        LottoCount count = LottoCount.createLottoCount(amount);
 
-        LottoTickets lottoTickets = LottoTickets.generateTickets(ticketCount);
-        List<LottoTicket> tickets = lottoTickets.getTickets();
-
-        assertThat(tickets.size()).isEqualTo(ticketCount);
+        LottoTickets lottoTickets = LottoTickets.generateTickets(count);
+        assertThat(lottoTickets.getTickets().size()).isEqualTo(count.getLottoCount());
     }
 
 }

9bb716c31cf21df264aff06f77bd07d1f6a6d49f
ykmxxi <ykmxxi97@gmail.com>
2023-11-05 15:37:46 +0900

    refactor(LottosTest): 테스트 메소드 위치 이동

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,10 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.EnumMap;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -100,68 +98,6 @@ class LottoTest {
 
         // then
         assertEquals(expected, result);
-    }
-
-    @ParameterizedTest(name = "입력값 : {0}, 기대 순위 : {1}, 기대 개수 : {2}")
-    @MethodSource("provideDataForGetRankResult")
-    @DisplayName("로또마다 순위를 구해 결과를 반환: key(순위)-value(개수)")
-    void givenData_whenGetRankResult_thenReturnResult(List<Integer> numbers, Rank expectedRank, int expectedCount) {
-        // given
-        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
-        Lottos userLottos = new Lottos(List.of(new Lotto(numbers)));
-
-        // when
-        EnumMap<Rank, Integer> result = userLottos.getRankResult(winningLotto);
-
-        // then
-        assertThat(result.containsKey(expectedRank)).isTrue();
-        assertEquals(expectedCount, result.get(expectedRank));
-    }
-
-    static Stream<Arguments> provideDataForGetRankResult() {
-        return Stream.of(
-                Arguments.of(List.of(1, 2, 3, 4, 5, 6), Rank.FIRST, 1),
-                Arguments.of(List.of(1, 2, 3, 4, 5, 7), Rank.SECOND, 1),
-                Arguments.of(List.of(1, 2, 3, 4, 5, 8), Rank.THIRD, 1),
-                Arguments.of(List.of(1, 2, 3, 4, 7, 8), Rank.FOURTH, 1),
-                Arguments.of(List.of(1, 2, 3, 7, 8, 9), Rank.FIFTH, 1),
-                Arguments.of(List.of(1, 2, 7, 8, 9, 10), Rank.UNRANKED, 1)
-        );
-    }
-
-    @ParameterizedTest(name = "입력값 : {0}, 기대값 : {1}")
-    @MethodSource("provideDataForCalculateTotalReward")
-    @DisplayName("총 당첨 금액 계산")
-    void givenData_whenCalculateTotalReward_thenReturnCorrectReward(List<Lotto> lottos, long expected) {
-        // given
-        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
-        Lottos userLottos = new Lottos(lottos);
-
-        // when
-        long result = userLottos.calculateTotalReward(userLottos.getRankResult(winningLotto));
-
-        // then
-        assertEquals(expected, result);
-    }
-
-    static Stream<Arguments> provideDataForCalculateTotalReward() {
-        return Stream.of(
-                Arguments.of(
-                        List.of(
-                                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                                new Lotto(List.of(1, 2, 3, 4, 5, 7))
-                        ), 4_030_000_000L
-                ),
-                Arguments.of(
-                        List.of(
-                                new Lotto(List.of(1, 2, 3, 4, 5, 8)),
-                                new Lotto(List.of(1, 2, 3, 4, 7, 8)),
-                                new Lotto(List.of(1, 2, 3, 4, 7, 8)),
-                                new Lotto(List.of(1, 2, 3, 7, 8, 9))
-                        ), 1_605_000L
-                )
-        );
     }
 
 }

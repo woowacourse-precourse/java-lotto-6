cf301bac2786ac0ab90a1bd2169ddc7258c5b2c6
enduf7686 <enduf768640@gmail.com>
2023-11-05 13:58:55 +0900

    refactor(lotto): edit lotto class

--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -11,20 +11,43 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public int compare(Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        return (int) IntStream.range(0, 6)
+                .filter(i -> this.numbers.contains(numbers.get(i)))
+                .count();
+    }
+
     private void validate(List<Integer> numbers) {
+        checkListSize(numbers);
+        checkNumberRange(numbers);
+        checkDuplication(numbers);
+    }
+
+    private void checkListSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자를 6개 입력해 주세요.");
+        }
+    }
+
+    private void checkNumberRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number > 45 || number < 1) {
+                throw new IllegalArgumentException("1~45 사이의 숫자를 입력해 주세요.");
+            }
+        });
+    }
+
+    private void checkDuplication(List<Integer> numbers) {
+        int count = (int) numbers.stream()
+                .distinct()
+                .count();
+        if (numbers.size() != count) {
+            throw new IllegalArgumentException("숫자가 중복되지 않게 입력해 주세요.");
         }
     }
 
     public List<Integer> getNumbers() {
         return numbers;
-    }
-
-    public int compare(Lotto lotto) {
-        List<Integer> numbers = lotto.getNumbers();
-        return (int) IntStream.range(0, 6)
-                .filter(i -> this.numbers.get(i) == numbers.get(i))
-                .count();
     }
 }

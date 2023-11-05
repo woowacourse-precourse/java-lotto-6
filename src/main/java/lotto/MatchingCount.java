af3a27051c80979cc21578d80d92ea3e26058b18
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-05 20:11:35 +0900

    Feat: 추가 bonusNumber 필드
    보너스 숫자 일치시 0.5 추가하는 방식에서 새로운 필드로 판별하는 방식으로 변경.

--- src/main/java/lotto/MatchingCount.java
+++ src/main/java/lotto/MatchingCount.java
@@ -2,17 +2,19 @@ package lotto;
 
 public enum MatchingCount {
 
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 15000000),
-    FIVE_BONUS(5.5f, 30000000),
-    SIX(6, 2000000000);
+    THREE(3, 5000, false),
+    FOUR(4, 50000, false),
+    FIVE(5, 15000000, false),
+    FIVE_BONUS(5, 30000000, true),
+    SIX(6, 2000000000, false);
 
-    private final float matchCount;
+    private final int matchCount;
     private final int prizeMoney;
+    private final boolean bonusNumber;
 
-    MatchingCount(float matchCount, int prizeMoney) {
+    MatchingCount(int matchCount, int prizeMoney, boolean bonusNumber) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.bonusNumber = bonusNumber;
     }
 }

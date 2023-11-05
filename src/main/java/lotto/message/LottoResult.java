2f4dc2e256e40c7d8c44a887cc6f9af9af13ed00
mk3058 <kmk3058@icloud.com>
2023-11-05 21:10:15 +0900

    feat(LottoResult): 당첨금 계산 기능 추가

--- src/main/java/lotto/message/LottoResult.java
+++ src/main/java/lotto/message/LottoResult.java
@@ -1,20 +1,26 @@
 package lotto.message;
 
 public enum LottoResult {
-    THREE_NUMBERS_MATCHES("3개 일치 (5,000원) - %d개\n"),
-    FOUR_NUMBERS_MATCHES("4개 일치 (50,000원) - %d개\n"),
-    FIVE_NUMBERS_MATCHES("5개 일치 (1,500,000원) - %d개\n"),
-    FIVE_AND_BONUS_NUMBERS_MATCHES("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
-    SIX_NUMBERS_MATCHES("6개 일치 (2,000,000,000원) - %d개\n");
+    THREE_NUMBERS_MATCHES("3개 일치 (5,000원) - %d개\n", 5000),
+    FOUR_NUMBERS_MATCHES("4개 일치 (50,000원) - %d개\n", 50000),
+    FIVE_NUMBERS_MATCHES("5개 일치 (1,500,000원) - %d개\n", 1500000),
+    FIVE_AND_BONUS_NUMBERS_MATCHES("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", 30000000),
+    SIX_NUMBERS_MATCHES("6개 일치 (2,000,000,000원) - %d개\n", 2000000000);
 
     private final String message;
+    private final int price;
 
-    LottoResult(String message) {
+    LottoResult(String message, int price) {
         this.message = message;
+        this.price = price;
     }
 
     public void print(int count) {
         System.out.printf(message, count);
+    }
+
+    public int yield(int count) {
+        return this.price * count;
     }
 
     public static LottoResult getMessageByResult(int matchedNumberCount, boolean isBonusMatched) {

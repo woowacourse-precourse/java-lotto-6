05c2549d0d1f2f9ff363e9295c86a0a28ab7ddda
minoooo119 <minoth0119@naver.com>
2023-11-05 20:07:04 +0900

    [ADD] 숫자 range 확인 function 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,8 +12,19 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if(!isRightRangeNum(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+    private boolean isRightRangeNum(List<Integer> numbers){
+        for(Integer num:numbers){
+            if(num>45||num<1){
+                return false;
+            }
+        }
+        return true;
     }
 
     // TODO: 추가 기능 구현

cff8d0170838be83a585f4f0613bcc0258a9cac5
yshong1998 <yshong1998@gmail.com>
2023-11-06 03:09:27 +0900

    Feat(WinningStatistics): 수익율을 계산하는 기능 구현

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import lotto.message.ResponseMessage;
+
+import java.text.DecimalFormat;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WinningStatistics {
@@ -19,5 +23,24 @@ public class WinningStatistics {
         }
     }
 
+    public void acceptWinPrizes(List<Prize> winPrizes) {
+        for (Prize prize : winPrizes) {
+            Integer count = statistics.get(prize);
+            statistics.put(prize, ++count);
+        }
+    }
 
+    public String calculateRateOfReturn(Integer purchaseAmount){
+        Integer reward = calculateTotalReward();
+        return String.format(
+                ResponseMessage.RATE_OF_BENEFIT_MESSAGE, (double) reward/purchaseAmount * 100);
+    }
+
+    private Integer calculateTotalReward() {
+        Integer reward = 0;
+        for (Prize prize : statistics.keySet()) {
+            reward += (prize.getWinningAmount() * statistics.get(prize));
+        }
+        return reward;
+    }
 }

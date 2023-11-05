0a940344e73eed0bcf79904c6dfa9d0323a1354b
Jiwon Na <jiwonna52@gmail.com>
2023-11-05 17:47:22 +0900

    Refactor : 당첨 금액을 구하는 메소드 calSumPrize를 Static 클래스로 변경

--- src/main/java/lotto/controller/Statistic.java
+++ src/main/java/lotto/controller/Statistic.java
@@ -1,9 +1,18 @@
 package lotto.controller;
 
+import java.util.HashMap;
+
 public class Statistic {
     public static int calLottoCount(int price, int lottoPrice) {
         return price / lottoPrice;
     }
-    
+
+    public static int calSumPrize(HashMap<Rank, Integer> result) {
+        int sum = 0;
+        for (Rank rank : result.keySet()) {
+            sum += (rank.getPrize()*result.get(rank));
+        }
+        return sum;
+    }
 
 }

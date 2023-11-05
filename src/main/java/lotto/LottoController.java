17993a90a6c181026c11ebab6957a8ecd987a14a
hlmg <devhlmg@gmail.com>
2023-11-06 04:14:29 +0900

    feat: 게임 흐름 제어 기능 구현

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        int purchaseAmount = inputView.inputPurchaseAmount();
+        List<Lotto> purchasedLottos = lottoService.purchase(purchaseAmount);
+        outputView.printPurchasedLottos(purchasedLottos);
+
+        Lotto winningLotto = new Lotto(inputView.inputWinningNumbers());
+        LottoNumber bonusNumber = new LottoNumber(inputView.inputBonusNumber());
+        WinningNumber winningNumber = new WinningNumber(winningLotto, bonusNumber);
+
+        Result winningResult = lottoService.getWinningResult(purchasedLottos, winningNumber);
+        outputView.printWinningResult(winningResult);
+    }
+}

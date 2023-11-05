755a1620e169204bb2c0b48aba9738ad18a6c7aa
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 02:38:55 +0900

    feat(WinningNumbers): WinningNumbers 클래스 & 중복 검증 로직 구현

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,2 +1,18 @@
-package lotto.domain;public class WinningNumbers {
+package lotto.domain;
+
+public class WinningNumbers {
+    private final WinningNumber winningNumber;
+    private final BonusNumber bonusNumber;
+
+    public WinningNumbers(WinningNumber winningNumber, BonusNumber bonusNumber) {
+        validate(winningNumber, bonusNumber);
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(WinningNumber winningNumber, BonusNumber bonusNumber) {
+        if (winningNumber.getNumbers().contains(bonusNumber.getNumber())) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

d4085a727548d1078c4e905edd1c88eecd8e5dd6
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-05 22:01:55 +0900

    style(constant): 상수값 위치 변경 및 추가

--- src/main/java/lotto/constant/WinningConstant.java
+++ src/main/java/lotto/constant/WinningConstant.java
@@ -5,6 +5,5 @@ public class WinningConstant {
     public static final int WINNING_MAX_COUNT = 6;
     public static final int INIT_WINNING_COUNT = 0;
     public static final int ONE_WINNING_COUNT = 1;
-    public static final String WINNING_RESULT_FORMAT = "%d개 일치 (%s) - %d개";
-    public static final String WINNING_SECOND_FORMAT = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final int RATIO_BASE = 100;
 }

7889b39fd1107bad1552c18959e5db605fc74be6
bellring <starringbell@gmail.com>
2023-11-05 20:54:24 +0900

    Fix(Lotto): 정렬 로직 변경
    - 사용자가 입력한 수를 정렬해 저장하는 로직에서 테스트시 List.of로 입력되는 값이 정렬 불가능한 Immutable 리스트이기 때문에 복사해 정렬 해 저장하는 방식으로 번경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -12,8 +13,9 @@ public class Lotto {
 
 	public Lotto(List<Integer> numbers) {
 		validate(numbers);
-		Collections.sort(numbers);
-		this.numbers = numbers;
+		List<Integer> sortedNumbers = new ArrayList<>(numbers);
+		Collections.sort(sortedNumbers);
+		this.numbers = sortedNumbers;
 	}
 
 	private void validate(List<Integer> numbers) {

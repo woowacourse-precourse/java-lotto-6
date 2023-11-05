6cefdfe5103fd1d3a3f9c1241901fde164f63354
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-11-06 01:54:26 +0900

    feat: 구매 개수 출력 작동 기능 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,9 +1,19 @@
-package lotto;
+package lotto.controller;
+
+import lotto.domain.Counter;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+
 
 public class GameController {
 
     public static void start(){
         int userMoney = InputView.inputMoney();
+        Counter counter = new Counter();
+        int lottoAmount = counter.generateLottoCount(userMoney);
+        OutputView outputView = new OutputView();
+        outputView.printLottoCount(lottoAmount);
     }
 
 }

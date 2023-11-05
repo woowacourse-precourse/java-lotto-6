8302ab6d8d5d22bd62a11c6c472e3fe5cb3b1395
dragon0622 <iamdragon.dev@gmail.com>
2023-11-05 19:16:15 +0900

    feat(Lotto) : output 클래스 내의 메소드 skeleton code 작성

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,28 @@
+package lotto;
+
+public class Output {
+    public static void startMessage(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void howManyLottoMessage(int countLotto){
+        System.out.println(countLotto + "개를 구매했습니다.");
+    }
+
+    public static void winningNumberGuide(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void bonusNumberGuide(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void resultLotto(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void RateOfReturn(){
+
+    }
+}

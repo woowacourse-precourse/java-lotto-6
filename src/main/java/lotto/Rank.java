7d8635c8cd5b70a0862b1013ff2a496f97dd4243
KIVSH <kihongkim3@hotmail.com>
2023-11-05 22:27:05 +0900

    feat: add about outputMessage Rank enum

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,20 +1,22 @@
 package lotto;
 
 public enum Rank {
-    THREE(3, false, 5000),
-    FOUR(4, false, 50000),
-    FIVE(5, false, 1500000),
-    BONUS(5, true, 30000000),
-    SIX(6, false, 2000000000);
+    THREE(3, false, 5000, "3개 일치 (5,000원) - "),
+    FOUR(4, false, 50000, "4개 일치 (50,000원) - "),
+    FIVE(5, false, 1500000, "5개 일치 (1,500,000원) - "),
+    BONUS(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX(6, false, 2000000000, "6개 일치 (2,000,000,000원) - ");
 
     private final int matchCount;
     private final boolean matchBonus;
     private final int prizeMoney;
+    private final String outputMessage;
 
-    Rank(int matchCount, boolean matchBonus, int prizeMoney) {
+    Rank(int matchCount, boolean matchBonus, int prizeMoney, String outputMessage) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prizeMoney = prizeMoney;
+        this.outputMessage = outputMessage;
     }
 
     public int getMatchCount() {
@@ -27,5 +29,9 @@ public enum Rank {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getOutputMessage() {
+        return outputMessage;
     }
 }

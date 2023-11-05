880878e86b0bd17457e6aecf1ddee10c046aeea0
KiminSon <kimin0331@naver.com>
2023-11-05 22:55:53 +0900

    refactor(InputView): 입력 금액 예외 처리
    validatePurchaseAmount함수로 입력 금액 예외 처리 부분을 하나로 합침

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,11 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String WHITE_SPACE = " ";
     private static final String VALIDATE_CONTAIN_WHITE_SPACE_MESSAGE = "[ERROR] 공백이 없는 값이어야 합니다.";
-    private static final String VALIDATE_STRING_TO_INT_MESSAGE = "[ERROR] 입력 값은 숫자여야 합니다.";
-    private static final String VALIDATE_PURCHASE_AMOUNT_RANGE_MESSAGE = "[ERROR] 입력 값은 1,000이상, 1,000,000이하의 숫자여야 합니다.";
-    private static final String VALIDATE_THOUSAND_MULTIPLE_MESSAGE = "[ERROR] 입력 값은 1,000의 배수여야 합니다.";
+    private static final String VALIDATE_STRING_TO_INT_MESSAGE = "[ERROR] 입력 값은 유효한 범위의 숫자여야 합니다.";
     private static final int MIN_PURCHASE_AMOUNT = 1000;
     private static final int MAX_PURCHASE_AMOUNT = 10000000;
 
@@ -18,34 +15,19 @@ public class InputView {
     }
 
     public void validatePurchaseAmount(String purchaseAmount) {
-        validateContainWhiteSpace(purchaseAmount);
-        validateStringToInt(purchaseAmount);
-        validatePurchaseAmountRange(Integer.parseInt(purchaseAmount));
-        validateThousandMultiple(Integer.parseInt(purchaseAmount));
-    }
-
-    public void validateContainWhiteSpace(String purchaseAmount) {
-        if (purchaseAmount.contains(WHITE_SPACE)) {
+        if (purchaseAmount.contains(" ")) {
             throw new IllegalArgumentException(VALIDATE_CONTAIN_WHITE_SPACE_MESSAGE);
         }
-    }
-
-    public void validateStringToInt(String purchaseAmount) {
         try {
-            Integer.parseInt(purchaseAmount);
+            if (Integer.parseInt(purchaseAmount) % MIN_PURCHASE_AMOUNT != 0) {
+                throw new IllegalArgumentException("[ERROR] 입력 값은 1,000의 배수여야 합니다.");
+            }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(VALIDATE_STRING_TO_INT_MESSAGE);
         }
-    }
-
-    public void validatePurchaseAmountRange(int purchaseAmount) {
-        if (purchaseAmount < MIN_PURCHASE_AMOUNT || purchaseAmount > MAX_PURCHASE_AMOUNT)
-            throw new IllegalArgumentException(VALIDATE_PURCHASE_AMOUNT_RANGE_MESSAGE);
-    }
-
-    public void validateThousandMultiple(int purchaseAmount) {
-        if (purchaseAmount % MIN_PURCHASE_AMOUNT != 0) {
-            throw new IllegalArgumentException(VALIDATE_THOUSAND_MULTIPLE_MESSAGE);
+        int purchaseAmountNumber = Integer.parseInt(purchaseAmount);
+        if (purchaseAmountNumber < MIN_PURCHASE_AMOUNT || purchaseAmountNumber > MAX_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 입력 값은 1,000이상, 1,000,000이하의 숫자여야 합니다.");
         }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,12 @@
 package lotto.view;
 
 public class OutputView {
-    private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String NEXT_LINE = "\n";
 
     public static void printPurchaseAmountRequest() {
-        System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
     public static void printNextLine() {
-        System.out.print(NEXT_LINE);
+        System.out.print("\n");
     }
 }

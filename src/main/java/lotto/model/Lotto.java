5901d0d9f269ad3089cccf7c11091073ee43ccd9
chosunghyun18 <krismon@naver.com>
2023-11-05 16:56:53 +0900

    feat : 로또 번호에 중복된 숫자가 있으면 예외가 발생한다

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> givenNumbers = new HashSet<>(numbers);
+        if(numbers.size() != givenNumbers.size()) throw  new IllegalArgumentException();
+    }
 
-    // TODO: 추가 기능 구현
 }

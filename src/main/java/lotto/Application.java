69580638d0f7c7d9b41359eb6d151d42dcca2624
songyi <song21677@naver.com>
2023-11-05 09:13:23 +0900

    feat: 로또 구입 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println(InputMessage.purchaseAmount);
+        String purchaseAmount = Console.readLine();
     }
 }

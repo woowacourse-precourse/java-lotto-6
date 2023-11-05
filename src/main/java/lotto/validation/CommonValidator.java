4c24c525f4ec6224f5e4f466d765fc77145bc9a6
acs0209 <annjae4676@naver.com>
2023-11-06 00:52:30 +0900

    feat: null, 빈값, 공백에 대해 검증하는 기능 구현

---
+++ src/main/java/lotto/validation/CommonValidator.java
@@ -0,0 +1,40 @@
+package lotto.validation;
+
+import static lotto.validation.enumType.Common.EMPTY_MESSAGE;
+import static lotto.validation.enumType.Common.NULL_MESSAGE;
+import static lotto.validation.enumType.Common.SPACE_MESSAGE;
+
+public class CommonValidator {
+
+    public void validateInputNull(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(NULL_MESSAGE.getMessage());
+        }
+    }
+
+    public void validateInputEmpty(String input) {
+        if (isEmptyValue(input)) {
+            throw new IllegalArgumentException(EMPTY_MESSAGE.getMessage());
+        }
+    }
+
+    private boolean isEmptyValue(String input) {
+        return input.isEmpty();
+    }
+
+    public void validateInputSpace(String input) {
+        if (isWhitespaceOnly(input)) {
+            throw new IllegalArgumentException(SPACE_MESSAGE.getMessage());
+        }
+    }
+
+    private boolean isWhitespaceOnly(String input) {
+        return input.trim().isEmpty();
+    }
+
+    public void validateNullEmptySpace(String input) {
+        validateInputNull(input);
+        validateInputEmpty(input);
+        validateInputSpace(input);
+    }
+}

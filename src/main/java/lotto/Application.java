8a75d2b1c3156ab01ed7a9120c9df5f5bca9835c
맹순영 <symaeng98@naver.com>
2023-11-05 12:26:06 +0900

    feat: 기본 구조 확립

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        // TODO: input validation
+//        validate(input);
+
+        int purchaseAmount = Integer.parseInt(input);
+        // TODO: 구입 금액에 따라 로또 구매하기
+
+
+        System.out.println("{lottoCnt}"+"개를 구매했습니다.");
+        // TODO: 구입한 로또들 출력
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winInput = Console.readLine();
+        // TODO: winInput validation
+//        validate(winInput); // 1 ~ 45 사이의 중복되지 않는 숫자 6개여야함
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonus = Console.readLine();
+        // TODO: bonus number validation
+//        validate(bonus); // 1 ~ 45 사이의 숫자여야함
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        // TODO: 당첨 통계 내기
     }
 }

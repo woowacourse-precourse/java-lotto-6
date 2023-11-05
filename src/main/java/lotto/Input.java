cf5081e44515e8adc45869c133aa2935d6c46c8b
dragon0622 <iamdragon.dev@gmail.com>
2023-11-05 20:06:20 +0900

    feat(Lotto) : howManyLotto 클래스, issuanceLotto 생성

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public static int money(){
         int money = Integer.parseInt(Console.readLine());
-        //TODO: exception 추가
+        //TODO: exception 추가 - money가 1000으로 나누어 떨어지지 않을때
         return money;
     }
 
---
+++ src/main/java/lotto/howManyLotto.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class howManyLotto {
+    public static int issuanceLotto(int money){
+        int lottoCnt = money/1000;
+        return lottoCnt;
+    }
+}

5498886612c3652bb73e9cc3d2a8e30a3cd6f1b4
noah <jeongas9810@gmail.com>
2023-11-06 01:20:56 +0900

    feat: Add return in View
    Returns each string that exists in the view class.

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class View {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.\n";
     private static final String ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.\n";
@@ -7,10 +10,46 @@ public class View {
     private static final String PRINT_PURCHASE_AMOUNT = "%d개를 구매했습니다.\n";
     private static final String PRINT_PURCHASE_NUMBER = "[%s]\n";
     private static final String PRINT_WINNING_STAT = "당첨 통계\n---\n";
-    private static final String PRINT_THREE_MATCHES = "3개 일치 (5,000원) - %d개\n";
-    private static final String PRINT_FOUR_MATCHES = "4개 일치 (50,000원) - 0개\n";
-    private static final String PRINT_FIVE_MATCHES = "5개 일치 (1,500,000원) - 0개\n";
-    private static final String PRINT_FIVE_BONUS_MATCHES = "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n";
-    private static final String PRINT_SIX_MATCHES ="6개 일치 (2,000,000,000원) - 0개";
+    private static final String PRINT_MATCHES = "%d개 일치 (%d원) - %d개";
     private static final String PRINT_YIELD_RATE = "총 수익률은 %.1f입니다.";
+    private static final String PRINT_SEPARATOR = ", ";
+
+    public static String getAskPurchaseAmount() {
+        return ASK_PURCHASE_AMOUNT;
+    }
+
+    public static String getAskWinningNumber() {
+        return ASK_WINNING_NUMBER;
+    }
+
+    public static String getAskBonusNumber() {
+        return ASK_BONUS_NUMBER;
+    }
+
+    public static String getPrintPurchaseAmount(int purchaseAmount) {
+        return String.format(PRINT_PURCHASE_AMOUNT, purchaseAmount);
+    }
+
+    public static String getPrintPurchaseNumber(Lotto lotto) {
+        List<String> getLottoNumber = lotto.getLottoNumber().stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
+
+        String lottoNumber = String.join(PRINT_SEPARATOR, getLottoNumber);
+
+        return String.format(PRINT_PURCHASE_NUMBER, lottoNumber);
+    }
+
+    public static String getPrintWinningStat() {
+        return PRINT_WINNING_STAT;
+    }
+
+    public static String getPrintMatches(Rank rank, int matchCount) {
+        return String.format(PRINT_MATCHES, rank.getRank(), (int)rank.getPrize(), matchCount);
+    }
+
+    public static String getPrintYieldRate(double yieldRate) {
+        return String.format(PRINT_YIELD_RATE, yieldRate);
+    }
+
 }

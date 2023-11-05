09f4a31c7ca0015f855c40a9ee5a06bdbf5e29f6
jinsoo Kim <jinsoo335@naver.com>
2023-11-05 19:21:58 +0900

    style, refactor: Lotto 클래스에 없어야 할 메소드 분리, sort 수정
    Lotto 클래스에 없어도 되는 메소드들을 Application.java에 옮겨둠
    
    테스트의 통과를 위해 기존의 로또 번호를 생성할 때 정렬해서 저장하는 방법에서 출력할 때만 정렬해서 보여주도록 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,26 +1,24 @@
 package lotto;
 
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
-            int buyMoney = Lotto.inputBuyMoney();
+            int buyMoney = inputBuyMoney();
             List<Lotto> lottoList = new ArrayList<>();
 
             createLottos(buyMoney, lottoList);
             printLottos(lottoList);
 
-            List<Integer> correctNumbers = Lotto.inputCorrectNumbers();
-            Integer bonusNumber = Lotto.inputBonusNumber(correctNumbers);
+            List<Integer> correctNumbers = inputCorrectNumbers();
+            Integer bonusNumber = inputBonusNumber(correctNumbers);
 
             Map<Position, Integer> winCheckMap = new LinkedHashMap<>();
             initWinCheckMap(winCheckMap);
@@ -35,20 +33,99 @@ public class Application {
     }
 
 
+    public static int inputBuyMoney() {
+        System.out.println("구입 금액을 입력해주세요.");
+
+        String input = Console.readLine();
+        int money = validateBuyMoney(input);
+        return money;
+    }
+
+    private static int validateBuyMoney(String input) {
+        int money;
+
+        money = Integer.parseInt(input);
+
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000으로 나누어 떨어져야 합니다.");
+        }
+
+        return money;
+    }
+
+
+    public static List<Integer> inputCorrectNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        String input = Console.readLine();
+
+        for(String str : input.split(",")){
+            numbers.add(Integer.parseInt(str));
+        }
+
+        validateCorrectNumbers(numbers);
+
+        return numbers;
+    }
+
+    private static void validateCorrectNumbers(List<Integer> numbers) {
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개 입력해야 합니다.");
+        }
+
+        for (Integer number : numbers) {
+            if(number > 45 || number < 1){
+                throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1 ~ 45 사이여야 합니다.");
+            }
+        }
+
+        Set<Integer> validSet = new HashSet<>(numbers);
+        if(validSet.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 중복 없이 6개 입력해야 합니다.");
+        }
+    }
+
+
     private static void createLottos(int buyMoney, List<Lotto> lottoList) {
         for(int i = 0; i < buyMoney / 1000; i++){
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
             Lotto lotto = new Lotto(numbers);
-
             lottoList.add(lotto);
         }
     }
 
 
     private static void printLottos(List<Lotto> lottoList) {
+        System.out.println(lottoList.size() + "개를 구매했습니다.");
+
         for (Lotto lotto : lottoList) {
             lotto.printLotto();
+        }
+    }
+
+
+    public static Integer inputBonusNumber(List<Integer> correctNumbers) {
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        Integer bonusNumber = Integer.parseInt(input);
+
+        validateBonusNumber(correctNumbers, bonusNumber);
+
+        return bonusNumber;
+    }
+
+    private static void validateBonusNumber(List<Integer> correctNumbers, Integer bonusNumber) {
+        if(bonusNumber < 1 || bonusNumber > 45){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 숫자를 입력해야 합니다.");
+        }
+
+        if(correctNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
         }
     }
 
@@ -72,14 +149,14 @@ public class Application {
 
 
     private static void printLottoResult(int buyMoney, Map<Position, Integer> winCheckMap) {
-        int totalPrice = 0;
-
         System.out.println("당첨 통계\n---------");
 
+        int totalPrice = 0;
         for (Position position : winCheckMap.keySet()) {
             totalPrice += winCheckMap.get(position) * position.getPositionMoney();
 
-            System.out.print(position.getPositonPrint() + " (" + position.getPositionMoney() +"원)");
+            DecimalFormat df = new DecimalFormat("###,###");
+            System.out.print(position.getPositonPrint() + " (" + df.format(position.getPositionMoney()) +"원)");
             System.out.println(" - " + winCheckMap.get(position) + "개");
         }
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,57 +17,6 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-    }
-
-    // TODO: 추가 기능 구현
-    
-    // 잘못 된 입력이 들어왔을 경우 -1 반환
-    public static int inputBuyMoney() {
-        System.out.println("구입 금액을 입력해주세요.");
-
-        String input = Console.readLine();
-        int money = validateBuyMoney(input);
-        return money;
-    }
-
-    private static int validateBuyMoney(String input) {
-        int money = -1;
-
-        money = Integer.parseInt(input);
-
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000으로 나누어 떨어져야 합니다.");
-        }
-        return money;
-    }
-
-
-    public static List<Integer> inputCorrectNumbers(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-
-        List<Integer> numbers = new ArrayList<>();
-
-        String input = Console.readLine();
-
-        for(String str : input.split(",")){
-            numbers.add(Integer.parseInt(str));
-        }
-
-        validateCorrectNumbers(numbers);
-
-        return numbers;
-    }
-
-    private static void validateCorrectNumbers(List<Integer> numbers) {
-        if(numbers.size() != 6){
-            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개 입력해야 합니다.");
-        }
-
-        for (Integer number : numbers) {
-            if(number > 45 || number < 1){
-                throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1 ~ 45 사이여야 합니다.");
-            }
-        }
 
         Set<Integer> validSet = new HashSet<>(numbers);
         if(validSet.size() != 6){
@@ -75,30 +24,11 @@ public class Lotto {
         }
     }
 
+    // TODO: 추가 기능 구현
     public void printLotto(){
-        System.out.println(this.numbers);
-    }
-
-    public static Integer inputBonusNumber(List<Integer> correctNumbers) {
-        System.out.println("보너스 번호를 입력해 주세요.");
-
-        String input = Console.readLine();
-
-        Integer bonusNumber = Integer.parseInt(input);
-
-        validateBonusNumber(correctNumbers, bonusNumber);
-
-        return bonusNumber;
-    }
-
-    private static void validateBonusNumber(List<Integer> correctNumbers, Integer bonusNumber) {
-        if(bonusNumber < 1 || bonusNumber > 45){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 숫자를 입력해야 합니다.");
-        }
-
-        if(correctNumbers.contains(bonusNumber)){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
-        }
+        List<Integer> printLotto = new ArrayList<>(this.numbers);
+        Collections.sort(printLotto);
+        System.out.println(printLotto);
     }
 
 
@@ -126,5 +56,4 @@ public class Lotto {
         }
         return null;
     }
-
 }

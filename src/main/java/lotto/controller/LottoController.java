910cbf1dd0c4e0cdb9e5919fa771cdd1e997fa5b
hyuna <monicajo0@naver.com>
2023-11-04 23:41:30 -0700

    feat: 뼈대 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,18 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    public final InputView inputView;
+    public final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start(){
+
+    }
+}

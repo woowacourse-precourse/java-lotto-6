c69e0a21a1271bfe7f0c72d5671b61ba7487c4ad
HoeSeong123 <ghltjd3@gmail.com>
2023-11-05 22:06:56 +0900

    test(PurchaseAmount) : 구입 금액 예외 처리에 대한 테스트 코드 작성

---
+++ src/test/java/lotto/model/PurchaseAmountTest.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PurchaseAmountTest {
+
+    @DisplayName("구입 금액이 1000원보다 적은 경우 예외가 발생한다.")
+    @Test
+    void createPurchaseAmountByUnderMinimumAmount() {
+        assertThatThrownBy(() -> new PurchaseAmount(900))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외가 발생한다..")
+    @Test
+    void createPurchaseAmountByIndivisibleNumber() {
+        assertThatThrownBy(() -> new PurchaseAmount(1234))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

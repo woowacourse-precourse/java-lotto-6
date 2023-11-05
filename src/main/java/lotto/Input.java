2fb1688d2d491098249d3108114cd8209ec9f5b0
unknown <winternrabbit@gmail.com>
2023-11-05 19:12:00 +0900

    refactor: 정적 메서드로 변경

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -6,13 +6,13 @@ public class Input {
     private static final String REQUEST_COST = "구입금액을 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
 
-    public String buyLotto() {
+    public static String buyLotto() {
         System.out.println(REQUEST_COST);
 
         return Console.readLine();
     }
 
-    public String winningNumbers() {
+    public static String winningNumbers() {
         System.out.println(REQUEST_WINNING_NUMBER);
 
         return Console.readLine();

3b3caf41d79475c40282a6e81132647a2d0e0c9b
howudong <tjdvy963@naver.com>
2023-11-05 23:20:50 +0900

    feat : DTO에 이미 값이 있는 필드에 값을 입력하지 못하게 하는 검사 추가

--- src/main/java/lotto/dto/DTO.java
+++ src/main/java/lotto/dto/DTO.java
@@ -1,5 +1,7 @@
 package lotto.dto;
 
+import static lotto.Error.View.CAN_NOT_OVERRIDE;
+
 public abstract class DTO {
     private DTO() {
     }
@@ -8,5 +10,11 @@ public abstract class DTO {
     }
 
     public abstract static class Output extends DTO {
+    }
+
+    protected void validateOverride(Object object) {
+        if (object != null) {
+            throw new IllegalArgumentException(CAN_NOT_OVERRIDE.getText());
+        }
     }
 }

d8638ac2cfa64af41c309c0aed750b8fe7417859
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-11-06 01:50:52 +0900

    feat(constant): 상수 정의
    - 상수 정의 진행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.Constant;
 import lotto.domain.Purchase;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println(Constant.amount);
         Purchase purchase = new Purchase(Console.readLine());
-        System.out.println(purchase.getAmount());
+
+        System.out.println("\n" + purchase.getAmount() + Constant.purchaseCount);
         purchase.getPurchasedLotto();
+
+        System.out.println(Constant.winningNumber);
     }
 }
---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class Constant {
+    public static final String amount = "구입금액을 입력해 주세요.";
+    public static final String purchaseCount = "개를 구매했습니다.";
+    public static final String winningNumber = "\n당첨 번호를 입력해 주세요.";
+}

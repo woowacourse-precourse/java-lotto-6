5d5d983f654e3a2c10caa175d26ec33e1f18b6c8
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-05 13:19:17 +0900

    로또 클래스 - validate 메소드 구현
    사용자 입력에 따라서 다양한 예외처리 구문을 추가했습니다.
    - 로또 번호는 6개여야합니다.
    - 로또 번호를 중복 되면 안됩니다.
    - 로또 번호는 1부터 45사이의 수여야 합니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers == null || numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 정확히 6개여야 합니다.");
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
-
     // TODO: 추가 기능 구현
 }

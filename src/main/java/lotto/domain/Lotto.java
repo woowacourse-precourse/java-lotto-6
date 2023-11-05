fa844b2c7b16d67c1ecfdb4a92dcdaf222dd89f1
siho-song <thdtlgh234@naver.com>
2023-11-05 17:51:14 +0900

    [Refactor] 함수 이동 및 함수 이름변경
    로또와 당첨번호를 비교하는 행위가 로또에서 이루어지는게 맞다고 생각하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,14 +8,22 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        sortAscending(numbers);
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
+        sortAscending(numbers);
         return this.numbers;
     }
 
+    public int compareLottoWithWinningNumber(LottoNumbers winningNumber) {
+        List<Integer> numbers = winningNumber.getLottoNumbers();
+        return (int) numbers.stream().filter(this::contains).count();
+    }
+
+    public boolean contains(Integer bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {

f93fa698129f5f6a9a0929c9e981fa8507550b06
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-05 13:33:26 +0900

    refactor(LottoGame) : 필드 삭제 및 메소드 분리

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -10,24 +10,26 @@ public final class LottoGame {
 
     private final List<Lotto> lottos;
     private final int buyingAmount;
-    private final int buyingPrice;
 
-    private LottoGame(final List<Lotto> lottos, final int buyingAmount, final int buyingPrice) {
+    public LottoGame(final List<Lotto> lottos, final int buyingAmount) {
         this.lottos = lottos;
         this.buyingAmount = buyingAmount;
-        this.buyingPrice = buyingPrice;
     }
 
     public static LottoGame createLottoGame(final int buyingPrice) {
         List<Lotto> lottos = new ArrayList<>();
-        int buyingAmount = buyingPrice / BUYING_PRICE_UNIT;
+        int buyingAmount = calculateBuyingAmount(buyingPrice);
 
         for (int i = MIN_BUYING_AMOUNT; i < buyingAmount; i++) {
             List<Integer> numbers = Lotto.pickUniqueNumbersInRange();
             Lotto lotto = Lotto.createLotto(numbers);
             lottos.add(lotto);
         }
-        return new LottoGame(lottos, buyingAmount, buyingPrice);
+        return new LottoGame(lottos, buyingAmount);
+    }
+
+    private static int calculateBuyingAmount(final int buyingPrice) {
+        return buyingPrice / BUYING_PRICE_UNIT;
     }
 
     public WinningResult calculateWinning(final WinningLotto winningLotto) {

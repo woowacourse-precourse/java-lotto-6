aedfba476abac7e70aebd0ad2ec49a0923b43e71
Yang-soeun <soeun8636@naver.com>
2023-11-06 01:37:50 +0900

    [feat]: LottoShop 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,26 @@
 package lotto;
 
 import java.util.List;
-import lotto.service.LottoService;
-import lotto.service.LottoServiceImpl;
+import lotto.domain.Cost;
+import lotto.domain.Lotto;
+import lotto.domain.LottoShop;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoService lottoService = new LottoServiceImpl();
-        List<List<Integer>> myLottoNumbers = lottoService.buyLotto();
+        InputView inputView = new InputView();
+        StringConverter stringConverter = new StringConverter();
+        Cost cost = new Cost(inputView.inputBuyingCost());
 
+        LottoShop lottoShop = new LottoShop(cost);
+        lottoShop.receiveMyLottos();
+
+        List<Integer> winningNumbers = stringConverter.convertToIntegerList(inputView.inputWinningNumbers());
+        int bonusNumber = inputView.inputBonusNumber();
+
+        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), bonusNumber);
+        lottoShop.checkResult(winningLotto);
     }
 }

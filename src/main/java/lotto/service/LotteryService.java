33bb8de938d205812c6b1cbd042a84b893147008
ghdeo <goldencorn7@naver.com>
2023-11-05 23:03:44 +0900

    feat: Add winningLottery

--- src/main/java/lotto/service/LotteryService.java
+++ src/main/java/lotto/service/LotteryService.java
@@ -1,5 +1,17 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.utils.Input;
+import lotto.utils.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LotteryService {
+    private Lotto winningLottery;
+
+    public void setWinningLottery() {
+        this.winningLottery = Input.readLottery();
+    }
 
 }

113a41b50f36e07b40254d0cb05e4ebbd4bf1110
JongMinCh0i <wonderland314592@gmail.com>
2023-11-05 19:49:06 +0900

    feat(OutputView): 로또 수익률을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ import lotto.domain.Lotto;
 
 public class OutputView {
 
+    private static final String RATE_OF_RETURN = "총 수익률은 %2.f%% 입니다.";
     private static final String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String LOTTO_RESULT = """
@@ -25,6 +26,10 @@ public class OutputView {
         IntStream.range(0, lottos.size())
                 .mapToObj(index -> lottos.get(index).getNumbers())
                 .forEach(System.out::println);
+    }
+
+    public void printRateOfReturn(double rateOfReturn) {
+        System.out.println(String.format(RATE_OF_RETURN, rateOfReturn));
     }
 
     public void printWinningStatistics() {

32e0e271e1d8b482587b35e490b014cdd4219903
leeedohyun <ldh19738@gmail.com>
2023-11-05 20:20:08 +0900

    feat: 입력 금액이 음수인 경우 예외 처리 기능 구현

--- src/main/java/lotto/model/MoneyValidator.java
+++ src/main/java/lotto/model/MoneyValidator.java
@@ -3,11 +3,17 @@ package lotto.model;
 public class MoneyValidator {
 
     private static final String MONEY_UNIT_EXCEPTION_MESSAGE = "[ERROR] 입력 금액은 1000원 단위여야 합니다.";
+    private static final String NEGATIVE_AMOUNT_EXCEPTION_MESSAGE = "[ERROR] 입력 금액은 음수이면 안 됩니다.";
 
     private MoneyValidator() {
     }
 
     public static void validateMoney(final int money) {
+        validateMultipleOfThousand(money);
+        validateNegativeAmount(money);
+    }
+
+    private static void validateMultipleOfThousand(final int money) {
         if (isNotMultipleOfUnit(money)) {
             throw new IllegalArgumentException(MONEY_UNIT_EXCEPTION_MESSAGE);
         }
@@ -15,5 +21,11 @@ public class MoneyValidator {
 
     private static boolean isNotMultipleOfUnit(final int money) {
         return money % LottoConstants.LOTTO_PRICE_UNIT != 0;
+    }
+
+    private static void validateNegativeAmount(final int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException(NEGATIVE_AMOUNT_EXCEPTION_MESSAGE);
+        }
     }
 }

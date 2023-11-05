30d78dbff6e8e27637efd010c74220964b0c3a98
ohjuyoungg <ejrm12@naver.com>
2023-11-05 14:15:25 +0900

    feat: 로또 구매 금액 로직 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,2 +1,18 @@
-package lotto.controller;public class GameController {
+package lotto.controller;
+
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class GameController {
+
+    public void play() {
+        OutputView.printGameStartMessage();
+        int purchaseInput = InputView.inputPurchaseAmount();
+        lottoPurchaseNum(purchaseInput);
+    }
+
+    private void lottoPurchaseNum(int lottoNum) {
+        OutputView.printLottoPurchaseAmount(lottoNum);
+    }
 }

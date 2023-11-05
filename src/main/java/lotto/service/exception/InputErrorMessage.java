2d7fb61a976104433332816e31091fa38e5c5456
706com <ehdgusjdbswl@gmail.com>
2023-11-06 05:13:29 +0900

    add : 당첨번호 입력 예외처리 추가

--- src/main/java/lotto/service/exception/InputErrorMessage.java
+++ src/main/java/lotto/service/exception/InputErrorMessage.java
@@ -2,11 +2,16 @@ package lotto.service.exception;
 
 public enum InputErrorMessage {
 
+    // 금액 입력 예외목록
     WRONG_INPUT_MONEY_UNIT("[ERROR] 1,000원 단위로 입력해야 합니다."),
-
     OVER_RANGE_MONEY("[ERROR] 50,000원 보다는 작게 입력해야 합니다."),
+    INVALID_NUMBER("[ERROR] 모두 숫자로 입력해야 합니다."),
 
-    INVALID_NUMBER("[ERROR] 모두 숫자로 입력해야 합니다.");
+
+    // 당첨번호 입력 예외목록
+    WRONG_NUMBER_SIZE("[ERROR] 숫자 6개를 입력해야 합니다."),
+    WRONG_NUMBER_RANGE("[ERROR] 1~45 사이를 입력해야 합니다."),
+    DUPLICATE_NUMBER("[ERROR] 숫자가 중복되면 안됩니다.");
 
     private final String ErrorMessage;
 

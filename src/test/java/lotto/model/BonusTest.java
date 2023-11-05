741ad36c059c8e12f8ea0edadbfcd11037896529
reddevilmidzy <midzy0228@gmail.com>
2023-11-05 20:27:41 +0900

    test: Bonus 메서드 테스트 추가

--- src/test/java/lotto/model/BonusTest.java
+++ src/test/java/lotto/model/BonusTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BonusTest {
@@ -14,5 +17,23 @@ class BonusTest {
     void createOutOfRange(Integer value) {
         assertThatThrownBy(() -> new Bonus(value))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 숫자와 로또 숫자가 겹치면 True를 반환한다.")
+    @ParameterizedTest(name = "{displayName} value : {0}")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void checkBonusContainsLottoNumbers(Integer bonusNumber) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Bonus bonus = new Bonus(bonusNumber);
+        assertThat(bonus.hasBonusNumber(lotto)).isTrue();
+    }
+
+    @DisplayName("보너스 숫자와 로또 숫자가 겹치면 False를 반환한다.")
+    @ParameterizedTest(name = "{displayName} value : {0}")
+    @ValueSource(ints = {7, 10, 45})
+    void checkBonusNotContainsLottoNumbers(Integer bonusNumber) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Bonus bonus = new Bonus(bonusNumber);
+        assertThat(bonus.hasBonusNumber(lotto)).isFalse();
     }
 }

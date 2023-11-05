e952212b2ab702776eea18fd75cb2d0735f6fe8e
hil679 <hil679922@gmail.com>
2023-11-05 21:48:04 +0900

    Feat(Lotto): 한 숫자가 해당 로또 번호 안에 있는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,14 @@
-package lotto;
+package lotto.domain;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +28,22 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("해당 번호가 현재 확인하는 로또 번호에 포함 되어 있는지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 6})
+    void hasLottoSameNumber(int number) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.hasSameNumber(number)).isTrue();
+    }
+
+    @DisplayName("해당 번호가 현재 확인하는 로또 번호에 포함되지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {7, 8})
+    void hasNotLottoSameNumber(int number) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.hasSameNumber(number)).isTrue();
+    }
 }

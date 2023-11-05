08fded0eb1dfde207ee60f9714d767453d6568b6
Heejun-0219 <heejun0219@gmail.com>
2023-11-06 00:35:38 +0900

    refactor: run() 함수 큰 단위 실행으로 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,19 +8,18 @@ public class LottoController {
     private LottoService lotto = new LottoService();
 
     public void run() {
+        lottoMaking();
+        userInputMaking();
+        resultLotto();
+        // 로또 티켓의 개수만큼 반복 및 당첨번호 및 보너스번호와 비교하여 결과 출력
+    }
+
+    private void lottoMaking() {
         userInputMoney();
         // 구입금액에 따른 예외 처리 및 로또 티켓 구매
         printLottoCount();
         // 로또 티켓의 개수만큼 반복 및 중복 없이 출력 (OutputView에 로또 출력 구현 필요)
-        userInputLottoNumbers();
         printLottoTicket();
-        // 당첨번호 및 보너스번호 기억할 객체 선택
-        userInputBonusNumber();
-        // 당첨번호 및 보너스번호 예외 처리
-        // 로또번호와 당첨번호 중복 처리
-        makeLuckyNumbers();
-        resultLotto();
-        // 로또 티켓의 개수만큼 반복 및 당첨번호 및 보너스번호와 비교하여 결과 출력
     }
 
     private void userInputMoney() {
@@ -39,6 +38,15 @@ public class LottoController {
 
     private void printLottoTicket() {
         OutputView.printLottoTicket(lotto.getLottoTickets());
+    }
+
+    private void userInputMaking() {
+        userInputLottoNumbers();
+        // 당첨번호 및 보너스번호 기억할 객체 선택
+        userInputBonusNumber();
+        // 당첨번호 및 보너스번호 예외 처리
+        // 로또번호와 당첨번호 중복 처리
+        makeLuckyNumbers();
     }
 
     private void userInputBonusNumber() {

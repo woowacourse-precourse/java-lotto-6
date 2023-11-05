f6c7033c015e2c1174ee609616eb411c474434e0
yh-skku <yh03061@g.skku.edu>
2023-11-05 17:35:23 +0900

    feat: 사용자에게 구입 금액을 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
+class MoneyInput{
+
+    static int money;
+
+
+    static void inputMoney(){
+
+        System.out.println("구입 금액을 입력해 주세요.");
+        String input = Console.readLine();
+        Console.close();
+        money = Integer.parseInt(input);
+
+    }
+
+    static void countLottoPapers(){
+        money/=1000;
+    }
+
+}
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        MoneyInput.inputMoney();
+        MoneyInput.countLottoPapers();
+        System.out.println(MoneyInput.money);
     }
 }

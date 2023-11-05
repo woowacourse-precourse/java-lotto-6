6e37969dc8e7441373b887f69ccdcb28fa6c3680
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-11-05 21:17:18 +0900

    feat(GameEngine) : 가격을 받아, 로또를 만든다.

---
+++ src/main/java/lotto/engine/GameEngine.java
@@ -0,0 +1,28 @@
+package lotto.engine;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+import lotto.numbergenerator.NumberGenerator;
+
+public class GameEngine {
+    private final GameEngineValidator gameEngineValidator;
+    private final NumberGenerator numberGenerator;
+    private List<Lotto> lottos;
+
+    public GameEngine(GameEngineValidator gameEngineValidator, NumberGenerator numberGenerator) {
+        this.gameEngineValidator = gameEngineValidator;
+        this.numberGenerator = numberGenerator;
+    }
+
+    public void createLottos(String readLine) {
+        gameEngineValidator.checkPrice(readLine);
+        lottos = new ArrayList<>();
+        long lottoSize = Long.parseLong(readLine) / 1000;
+        for (long i = 0; i < lottoSize; i++) {
+            List<Integer> lottoNumbers = numberGenerator.generateNumbers(1, 45, 6);
+            gameEngineValidator.checkLottoNumbers(lottoNumbers);
+            lottos.add(new Lotto(lottoNumbers));
+        }
+    }
+}

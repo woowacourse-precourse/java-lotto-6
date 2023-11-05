7cd67fc3a8ed411e4435e62d0e15e86786f6c462
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-05 23:34:33 +0900

    Feat: 상수 작성

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+public class Constant {
+    private static final String GET_PURCHASE_AMOUNT_TXT = "구입금액을 입력해 주세요.";
+    private static final String GET_WINNING_NUMBER_TXT = "당첨 번호를 입력해 주세요.";
+
+    public static final String LOTTO_AMOUNT = "{0}개를 구매했습니다.";
+    public static final String WINNING_STATISTICS = "{0}개 일치 ({1}원) - {2}개";
+    public static final String WINNING_STATISTICS_BONUS = "{0}개 일치, 보너스 볼 일치 ({1}원) - {2}개";
+    public static final String RATE_OF_RETURN = "총 수익률은 {0}%입니다.";
+
+    public static final String LOTTO_NUMBER_SEPARATOR = ", ";
+    public static final String WINNING_NUMBER_SEPARATOR = ",";
+}

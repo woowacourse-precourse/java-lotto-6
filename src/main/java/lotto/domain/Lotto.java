e6960886d20d93693e8a2c391e500231be9e8afb
InGyu Moon <ansdlsrb1121@naver.com>
2023-11-05 23:22:55 +0900

    feat: 생성한 로또번호에 중복이 있을경우 IllegalArgumentException 발생시킴

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,7 +14,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또번호가 6개가 아닙니다.");
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또에 중복된 숫자가 존재하면 안됩니다.");
         }
     }
 

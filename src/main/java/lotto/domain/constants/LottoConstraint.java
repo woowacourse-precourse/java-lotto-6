f8694e05b6d937023ddee3a0f389dfcf10d5d073
노정완 <jeongwan.noh@gmail.com>
2023-11-06 00:18:50 +0900

    feat: 당첨 금액 구현

--- src/main/java/lotto/domain/constants/LottoConstraint.java
+++ src/main/java/lotto/domain/constants/LottoConstraint.java
@@ -4,7 +4,13 @@ public enum LottoConstraint {
     LOTTO_PRICE(1000),
 
     NUMBER_RANGE_MINIMUM(1),
-    NUMBER_RANGE_MAXIMUM(45);
+    NUMBER_RANGE_MAXIMUM(45),
+
+    FIRST_REWARD(2000000000),
+    SECOND_REWARD(30000000),
+    THIRD_REWARD(1500000),
+    FOURTH_REWARD(50000),
+    FIFTH_REWARD(5000);
 
     private final int value;
 

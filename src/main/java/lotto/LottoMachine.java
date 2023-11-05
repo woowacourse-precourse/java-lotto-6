3996d45436c0791f494dbc3b83a2ac4e514b90ac
cloudchamb3r <jizon0123@protonmail.com>
2023-11-05 21:18:07 +0900

    fix. lotto machine errors

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -7,19 +7,18 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import utils.RandomUtil;
 
-import
 public class LottoMachine {
     final int lottoPrice = 1000;
     public List<Lotto> buyLottos() {
         var inputPrice = Integer.parseInt(Console.readLine());
         var lottoCount = inputPrice / lottoPrice;
 
-        var lottos = IntStream.of(lottoCount)
-                .mapToObj((i) -> new Lotto(RandomUtil.createUniqueRandomListOf(1, 45, 6)))
+        var lottos = IntStream.rangeClosed(1, lottoCount)
+                .mapToObj((i) -> new Lotto(RandomUtil.createSortedUniqueRandomListOf(1, 45, 6)))
                 .collect(Collectors.toList());
         System.out.println(String.format("%d개를 구매했습니다.", lottoCount));
         for (var lotto : lottos) {
-            System.out.println(lotto);
+            System.out.println(lotto.toString());
         }
         return lottos;
     }
@@ -49,9 +48,9 @@ public class LottoMachine {
         System.out.println("3개 일치 (5,000원) - " + match3 +"개");
         match4 = lottos.stream().filter(lotto -> lotto.matchCount(winnings) == 4).count();
         System.out.println("4개 일치 (50,000원) - " + match4 +"개");
-        match4 = lottos.stream().filter(lotto -> lotto.matchCount(winnings) == 5 && !lottos.matchBonus(bonus)).count();
+        match4 = lottos.stream().filter(lotto -> lotto.matchCount(winnings) == 5 && !lotto.matchBonus(bonus)).count();
         System.out.println("5개 일치 (1,500,000원) - " + match5 +"개");
-        match5Bonus = lottos.stream().filter(lotto -> lotto.matchCount(winnings) == 5 && lottos.matchBonus(bonus)).count();
+        match5Bonus = lottos.stream().filter(lotto -> lotto.matchCount(winnings) == 5 && lotto.matchBonus(bonus)).count();
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + match5 +"개");
         match6 = lottos.stream().filter(lotto -> lotto.matchCount(winnings) == 6).count();
         System.out.println("6개 일치 (2,000,000,000원) - " + match6 +"개");
@@ -63,8 +62,7 @@ public class LottoMachine {
                 30000000L * match5Bonus +
                 2000000000L * match6;
 
-        var profitRate = ((double)profit / lottoPrice * lottos.size()) * 100;
-        System.out.println(String.format("총 수익률은 %.2f입니다", profitRate));
+        var profitRate = ((double)profit / (lottoPrice * lottos.size())) * 100;
+        System.out.println(String.format("총 수익률은 %.1f%%입니다", profitRate));
     }
-
 }

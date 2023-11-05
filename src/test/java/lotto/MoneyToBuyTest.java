accc88d3f14d2c876aba0ee2755bf250bfc2ef76
sunwooiljoon <wnsdl1659@naver.com>
2023-11-05 15:46:51 +0900

    test : MoneyToBuyTest 테스트 추가
    MoneyToBuy 검증
    - 금액이 0이하일 때
    - 금액이 천원단위가 아닐 때

---
+++ src/test/java/lotto/MoneyToBuyTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyToBuyTest {
+
+    @Test
+    @DisplayName("금액이 0이하 에러 발생1")
+    void createMoneyToBuyByUnderValue_1() {
+        assertThatThrownBy(() -> new MoneyToBuy(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("금액이 0이하 에러 발생2")
+    void createMoneyToBuyByUnderValue_2() {
+        assertThatThrownBy(() -> new MoneyToBuy(-123))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("금액이 천원단위가 아니면 에러 발생1")
+    void createMoneyToBuyByWrongValue_1() {
+        assertThatThrownBy(() -> new MoneyToBuy(1001))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("금액이 천원단위가 아니면 에러 발생2")
+    void createMoneyToBuyByWrongValue_2() {
+        assertThatThrownBy(() -> new MoneyToBuy(123))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

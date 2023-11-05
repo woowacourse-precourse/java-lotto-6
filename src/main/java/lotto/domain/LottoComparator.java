94f04365c4b99b56b39a6700307182a24010add2
jifrozen0110 <answldjs1836@gmail.com>
2023-11-06 02:40:06 +0900

    refactor(LottoComparator) : null값 체크하는 validate 추가
    * test(domain) : LottoComparator를 통해 LottosResult를 반환한다.

--- src/main/java/lotto/domain/LottoComparator.java
+++ src/main/java/lotto/domain/LottoComparator.java
@@ -1,12 +1,24 @@
 package lotto.domain;
 
+import lotto.common.consts.ErrorMessage;
+
 public class LottoComparator {
     private Lottos lottos;
     private WinningLotto winningLotto;
 
     public LottoComparator(Lottos lottos, WinningLotto winningLotto) {
+        validate(lottos, winningLotto);
         this.lottos = lottos;
         this.winningLotto = winningLotto;
+    }
+
+    private void validate(Lottos lottos, WinningLotto winningLotto) {
+        if (lottos == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_ERROR_MESSAGE);
+        }
+        if (winningLotto == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_ERROR_MESSAGE);
+        }
     }
 
 

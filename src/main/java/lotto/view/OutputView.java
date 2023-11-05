0711931e847a83245ddfdf4bf9890b2ab6ea85a4
eunhee <dmsgml9903@gmail.com>
2023-11-06 00:34:52 +0900

    feat(OutputView): 당첨 통계 문구와 구분선을 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,8 @@ import lotto.model.UserLotteries;
 public class OutputView {
 
     private static final String PURCHASED_LOTTO_COUNT_MESSAGE_TEMPLATE = "%s개를 구매했습니다.";
+    private static final String LOTTO_RESULT_MESSAGE = "당첨 통계";
+    private static final String LOTTO_RESULT_LINE_MESSAGE = "---";
 
     public void printPurchasedCountMessage(Integer purchasedLottoCount) {
         System.out.println(format(PURCHASED_LOTTO_COUNT_MESSAGE_TEMPLATE, purchasedLottoCount));
@@ -21,5 +23,10 @@ public class OutputView {
 
     private void printLotto(Lotto lotto) {
         System.out.println(lotto.getNumbers());
+    }
+
+    public void printLottoResultMessage() {
+        System.out.println(LOTTO_RESULT_MESSAGE);
+        System.out.println(LOTTO_RESULT_LINE_MESSAGE);
     }
 }

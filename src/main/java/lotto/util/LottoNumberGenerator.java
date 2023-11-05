b17fcedf52315708c57116ba045f9141442d6d64
SungKyum Kim <sungkyum1@naver.com>
2023-11-05 23:48:22 +0900

    refactor(LottoNumberGenerator): 매직 넘버 분리

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -5,7 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoNumberGenerator {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int NUM_LOTTO_NUMBERS = 6;
+
     public List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUM_LOTTO_NUMBERS);
     }
 }

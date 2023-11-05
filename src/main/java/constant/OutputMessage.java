c9840eb9c93444eaecc331b37d4984e0d8f0be44
songsunkook <songsunkook@gmail.com>
2023-11-06 00:59:14 +0900

    feat: 출력 메시지 enum 추가

---
+++ src/main/java/constant/OutputMessage.java
@@ -0,0 +1,25 @@
+package constant;
+
+import java.text.DecimalFormat;
+
+public enum OutputMessage {
+    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요.\n"),
+    BUY_LOTTOES("\n%d개를 구매했습니다.\n"),
+    INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요.\n"),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n"),
+    INPUT_WINNING_STATISTICS("\n당첨 통계\n---\n"),
+    MATCH_COUNT("%d개 일치 (%s원) - %d개\n"),
+    MATCH_COUNT_RANK_2("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n"),
+    TOTAL_RATE_OF_RETURN("총 수익률은 %s%입니다.\n"),
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return message;
+    }
+}

fff1cc194418dd6b2a69691db615a022bf500f72
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-05 22:12:39 +0900

    feat: 로또 수와 로또 번호를 저장하는 도메인 추가

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.List;
+
+
+public class User {
+    private final int amount;
+    private final List<Lotto> lottos;
+
+    public User(int amount, List<Lotto> lottos) {
+        this.amount = amount;
+        this.lottos = lottos;
+    }
+
+}

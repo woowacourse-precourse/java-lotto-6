96e0195ac6582df73b8c826b4e83caa6d7b5ab40
ChaeAg <dydkglel123@gmail.com>
2023-11-05 19:54:59 +0900

    refactor(OutputView): 메서드가 한 가지 일만 하도록 리팩토링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,11 +7,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final StringBuilder stringBuilder = new StringBuilder();
-
+    private static final int RESET_LENGTH = 0;
     private static final String PURCHASE_INPUT_GUIDE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
-    private static final int RESET_LENGTH = 0;
     private static final String WINNING_NUMBER_INPUT_GUIDE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_GUIDE = "\n" + "보너스 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS_INFO = "당첨 통계";
@@ -20,6 +18,7 @@ public class OutputView {
     private static final String BONUS_BALL_MATCH_MESSAGE = ", 보너스 볼 일치";
     private static final String LOTTO_MATCH_FORMAT = "%d개 일치%s (%,d원) - %d개";
     private static final String YIELD_FORMAT = "총 수익률은 %.1f%%입니다.";
+    private static final StringBuilder stringBuilder = new StringBuilder();
 
     public static void displayPurchaseGuide() {
         System.out.println(PURCHASE_INPUT_GUIDE);
@@ -53,19 +52,13 @@ public class OutputView {
                 .append("\n");
     }
 
-    public static void displayWinningStatistics(final List<LottoMatch> lottoResultCount) {
+    public static void displayWinningStatistics(final List<LottoMatch> winningResults) {
         for (LottoMatch lottoMatch : LottoMatch.values()) {
             if (lottoMatch == LottoMatch.NOTHING_MATCH) {
                 continue;
             }
-            String bonusBallMessage = EMPTY_MESSAGE;
-            if (lottoMatch == LottoMatch.FIVE_AND_BONUS_MATCH) {
-                bonusBallMessage = BONUS_BALL_MATCH_MESSAGE;
-            }
-
-            long matchCount = lottoResultCount.stream()
-                    .filter(match -> match == lottoMatch)
-                    .count();
+            String bonusBallMessage = determineBonusBallMessage(lottoMatch);
+            long matchCount = countMatch(winningResults, lottoMatch);
 
             stringBuilder.append(String.format(LOTTO_MATCH_FORMAT,
                     lottoMatch.getMatching(),
@@ -73,6 +66,19 @@ public class OutputView {
                     lottoMatch.getAmount(),
                     matchCount)).append("\n");
         }
+    }
+
+    private static String determineBonusBallMessage(LottoMatch lottoMatch) {
+        if (lottoMatch == LottoMatch.FIVE_AND_BONUS_MATCH) {
+            return BONUS_BALL_MATCH_MESSAGE;
+        }
+        return EMPTY_MESSAGE;
+    }
+
+    private static long countMatch(List<LottoMatch> winningResults, LottoMatch lottoMatch) {
+        return winningResults.stream()
+                .filter(match -> match == lottoMatch)
+                .count();
     }
 
     public static void displayYield(final double yield) {

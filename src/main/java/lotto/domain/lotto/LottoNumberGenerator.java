6a13115b60510d39152a6f9bacffea6621804342
chanhoim <cksgh201@naver.com>
2023-11-05 21:26:42 +0900

    feat(lotto): 로또 번호 생성 구현

---
+++ src/main/java/lotto/domain/lotto/LottoNumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.domain.lotto;
+
+import static lotto.domain.lotto.LottoNumberConfig.MINIMUM_RANGE;
+import static lotto.domain.lotto.LottoNumberConfig.MAXIMUM_RANGE;
+import static lotto.domain.lotto.LottoNumberConfig.NUMBER_COUNT;
+
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoNumberGenerator implements NumberGenerator {
+
+    @Override
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_RANGE.getValue(),
+                MAXIMUM_RANGE.getValue(), NUMBER_COUNT.getValue());
+    }
+}

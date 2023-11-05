4b8914c93306d0b3348dd3f0bf65f9b346d276c3
LUKR7Q <ybkim0628@naver.com>
2023-11-05 23:03:37 +0900

    countMatch(Lotto winningLotto): 현재 로또 번호와 당첨 번호를 비교하여 일치하는 숫자의 개수를 반환합니다.

--- src/main/java/lotto/service/Lotto.java
+++ src/main/java/lotto/service/Lotto.java
@@ -27,4 +27,10 @@ public class Lotto {
         return numbers;
     }
 
+    public int countMatch(Lotto winningLotto) {
+        return (int) numbers.stream().
+                filter(winningLotto::containNumber).
+                count();
+    }
+
 }

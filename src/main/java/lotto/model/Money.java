2dc54d906f924b86dd8b8747a7c25cb7fbd98265
gonixxxx <akdntm946@gmail.com>
2023-11-06 03:26:32 +0900

    fix(Money): add stringToInt convertor
    fix calNumberOfLotto()

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,8 +1,10 @@
 package lotto.model;
 
 import lotto.utils.InputValidator;
+import lotto.utils.StringConvertor;
 
 public class Money {
+
     private static final int LOTTO_PRICE = 1000;
     private final String money;
 
@@ -15,8 +17,8 @@ public class Money {
         InputValidator.checkMoney(money);
     }
 
-    public int calNumberOfLotto(int money) {
-        return money / LOTTO_PRICE;
+    public int calNumberOfLotto() {
+        return StringConvertor.stringToInt(money) / LOTTO_PRICE;
     }
 
 }

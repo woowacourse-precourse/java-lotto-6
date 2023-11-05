283e4aca19808a24efb24babe2b60fafa3826a3a
hyunchuljung <guscjf0903@naver.com>
2023-11-06 02:23:21 +0900

    :memo: Test : 로또티켓 번호 생성에 따른 테스트코드 작성
    정상적으로 로또티켓 도출하는지 확인하는 코드 작성.
    각각의 예외상황마다 예외가 제대로 동작하는지 확인코드 작성.

---
+++ src/test/java/mytest/LottoTicketGenerateTest.java
@@ -0,0 +1,80 @@
+package mytest;
+
+import lotto.model.LottoTicketEntity;
+import lotto.service.LottoTicketGenerate;
+import lotto.service.LottoValidateRegistry;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LottoTicketGenerateTest {
+    @Test
+    void setLottoTicketNumberTest() {
+        LottoTicketGenerate lottoTicketGenerate = new LottoTicketGenerate();
+        LottoTicketEntity lottoTicketEntity = lottoTicketGenerate.setLottoTicketsNumber();
+
+        System.out.println(lottoTicketEntity.getLottoNumbers());
+        assertNotNull(lottoTicketEntity);
+
+        // 6개의 숫자가 생성되었는지 확인
+        assertEquals(6, lottoTicketEntity.getLottoNumbers().size());
+        // 1~45 사이의 숫자가 생성되었는지 확인
+        for (int number : lottoTicketEntity.getLottoNumbers()) {
+            assertTrue(number >= 1 && number <= 45);
+        }
+        // 중복된 숫자가 있는지 확인
+        assertEquals(6, new HashSet<>(lottoTicketEntity.getLottoNumbers()).size());
+    }
+
+    @ParameterizedTest
+    @MethodSource("overSizeLottoNumbers")
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외 발생")
+    void lottoByOverSizeTest(List<Integer> lottoNumbers) {
+        assertThrows(IllegalArgumentException.class, () -> LottoValidateRegistry.lottoTicketValidate(lottoNumbers));
+    }
+    private static Stream<Arguments> overSizeLottoNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7, 8)),
+                Arguments.of(List.of(32, 18, 22, 34, 11, 6, 44))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("duplicateLottoNumbers")
+    @DisplayName("중복된 숫자가 발생 시 예외 발생")
+    void duplicateLottoTest(List<Integer> lottoNumbers) {
+        assertThrows(IllegalArgumentException.class, () -> LottoValidateRegistry.lottoTicketValidate(lottoNumbers));
+    }
+    private static Stream<Arguments> duplicateLottoNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 3, 4, 5)),
+                Arguments.of(List.of(11, 22, 35, 35, 44, 7)),
+                Arguments.of(List.of(34, 34, 22, 34, 11, 6))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeLottoNumbers")
+    @DisplayName("1 ~ 45 범위안에 없을 시 예외 발생")
+    void rangeLottoTest(List<Integer> lottoNumbers) {
+        assertThrows(IllegalArgumentException.class, () -> LottoValidateRegistry.lottoTicketValidate(lottoNumbers));
+    }
+    private static Stream<Arguments> rangeLottoNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(0, 11, 34, 56, 7, 8)),
+                Arguments.of(List.of(77, 11, 2, 3, 4, 5)),
+                Arguments.of(List.of(-1, 11, 2, 3, 4, 5)),
+                Arguments.of(List.of(0, 11, 2, 3, 4, 5))
+                );
+    }
+
+}

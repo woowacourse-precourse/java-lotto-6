c071d312d9945596718b0b813d9c4d147a5083ba
jsj <tkdlek4186@naver.com>
2023-11-05 15:44:27 +0900

    feat(main) : 로또시작 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Play play=new Play();
+        play.playLotto();
+
     }
 }

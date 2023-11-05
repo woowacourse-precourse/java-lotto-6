63127d0b0d65cd43fbe7f45dd49c9831c46c5c05
hyuna <monicajo0@naver.com>
2023-11-05 05:46:05 -0800

    refactor(Constants): 코드 구조 변경

--- src/main/java/lotto/model/Constants.java
+++ src/main/java/lotto/model/Constants.java
@@ -1,18 +1,8 @@
 package lotto.model;
 
-public enum Constants {
-    MIN_LOTTO_NUMBER(1),
-    MAX_LOTTO_NUMBER(45),
-    LOTTO_NUMBER_COUNT(6),
-    PURCHASE_UNIT(1000);
-
-    private int value;
-
-    Constants(int value){
-        this.value = value;
-    }
-
-    public int getValue(){
-        return this.value;
-    }
+public class Constants {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int PURCHASE_UNIT = 1000;
 }

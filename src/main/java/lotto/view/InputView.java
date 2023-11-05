da18726ef80e580911e8589035c6975dc19d2168
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-05 23:25:43 +0900

    🎯feat(view/InputView) : 당첨번호 입력 메세지 출력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 * */
 public class InputView {
     String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     // 구입금액 입력 메세지 출력
     public void printInputPurchaseAmountMessage() {
@@ -18,4 +19,10 @@ public class InputView {
     public String inputPurchaseAmount() {
         return Console.readLine();
     }
+
+    // 당첨번호 입력 메세지 출력
+    public void printInputWinningNumberMessage() {
+        println(INPUT_WINNING_NUMBER);
+    }
+
 }

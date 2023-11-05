082deee2c2fafc3a344d30c1a61bab5fba014afc
GO-TE <alsrudxo9810@naver.com>
2023-11-06 02:55:29 +0900

    feat(Lotto): 로또 당첨 결과 확인 기능 winningStatus() : - 로또 당첨번호와 보너스 번호를 받는다 - 로또의 당첨 결과를 리턴한다 getRank() : - 로또의 당첨결과를 문자열로 바꾼다

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +18,37 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    public String winningStatus(ArrayList<Integer> winningNumber, int bonusNumber) {
+        int count = 0;
+        boolean bonus = false;
+        for (int number : numbers) {
+            if (winningNumber.contains(number)) {
+                count++;
+            }
+            if (number == bonusNumber) {
+                bonus = true;
+            }
+        }
+        return getRank(count, bonus);
+    }
+
+    private String getRank(int count, boolean bonus) {
+        if (count == 6) {
+            return "1등";
+        }
+        if (count == 5 && bonus) {
+            return "2등";
+        }
+        if (count == 5 && !bonus) {
+            return "3등";
+        }
+        if (count == 4) {
+            return "4등";
+        }
+        if (count == 3) {
+            return "5등";
+        }
+        return "꽝";
+    }
 }

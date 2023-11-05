280b878b550a65f43d918e94bb18b1f27e520cbf
JH-TT <leejongho9803@gmail.com>
2023-11-06 00:52:25 +0900

    refactor: 당첨 번호 초기화함수 서비스단으로 분리

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -14,41 +14,23 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGame {
+    private static final int LOTTO_PRISE = 1000;
+    private static final int PERCENT = 100;
     private final LottoService lottoService = new LottoService();
     private final HashMap<Rank, Integer> result = new HashMap<>();
-    private final int PERCENT = 100;
-    private long payment;
     private Buyer buyer;
     private WinningLotto winningLotto;
     private long reward;
     public void run() {
         buyLotto();
-        initWinningLotto(inputWinningLotto(), inputBonusNumber());
+        initWinningLotto();
         initResult();
         matchLottos();
         showResult();
     }
 
-    private List<Integer> inputWinningLotto() {
-        try {
-            InputView.inputNumbers();
-            return LottoUtils.stringToList(InputUtil.inputStringWithTrim());
-        } catch (IllegalArgumentException e) {
-            return inputWinningLotto();
-        }
-    }
-
-    private String inputBonusNumber() {
-        try {
-            InputView.inputBonusNumber();
-            return InputUtil.inputStringWithTrim();
-        } catch (IllegalArgumentException e) {
-            return inputBonusNumber();
-        }
-    }
-
-    private void initWinningLotto(List<Integer> lotto, String bonus) {
-        winningLotto = new WinningLotto(lotto, bonus);
+    private void initWinningLotto() {
+        winningLotto = lottoService.initWinningLotto();
     }
 
     private void buyLotto() {
@@ -88,6 +70,10 @@ public class LottoGame {
 
     private void showResult() {
         OutputView.showResult(result);
-        OutputView.showRateOfReturn(((double) reward / payment) * PERCENT);
+        OutputView.showRateOfReturn(((double) reward / getAmount()) * PERCENT);
+    }
+
+    private long getAmount() {
+        return (long) buyer.getLottos().size() * LOTTO_PRISE;
     }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,16 +1,46 @@
 package lotto.service;
 
+import java.util.List;
 import lotto.domain.Buyer;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
 import lotto.utils.InputUtil;
+import lotto.utils.LottoUtils;
 import lotto.validation.Validation;
+import lotto.view.InputView;
 
 public class LottoService {
 
     public Buyer purchaseLotto() {
         try {
+            InputView.inputPrise();
             return new Buyer(lottoPayment());
         } catch (IllegalArgumentException e) {
             return purchaseLotto();
+        }
+    }
+
+    public WinningLotto initWinningLotto() {
+        List<Integer> correctNumbers = inputWinningLotto();
+        return inputBonusNumberAndGenerateWinningLotto(correctNumbers);
+    }
+
+    private List<Integer> inputWinningLotto() {
+        try {
+            InputView.inputNumbers();
+            List<Integer> integers = LottoUtils.stringToList(InputUtil.inputStringWithTrim());
+            return new Lotto(integers).getNumbers();
+        } catch (IllegalArgumentException e) {
+            return inputWinningLotto();
+        }
+    }
+
+    private WinningLotto inputBonusNumberAndGenerateWinningLotto(List<Integer> numbers) {
+        try {
+            InputView.inputBonusNumber();
+            return new WinningLotto(numbers, InputUtil.inputStringWithTrim());
+        } catch (IllegalArgumentException e) {
+            return inputBonusNumberAndGenerateWinningLotto(numbers);
         }
     }
 
@@ -19,9 +49,13 @@ public class LottoService {
     }
 
     private String amountValidation(String amount) {
-        Validation.checkIsNumber(amount);
-        Validation.checkIsNatural(Integer.parseInt(amount));
+        validateNumber(amount);
         Validation.checkIsDivideByThousand(amount);
         return amount;
+    }
+
+    private void validateNumber(String num) {
+        Validation.checkIsNumber(num);
+        Validation.checkIsNatural(Integer.parseInt(num));
     }
 }

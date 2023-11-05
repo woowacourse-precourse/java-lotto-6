c617e8d78513d0db91d9b973daacdbd3597a4f9c
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-06 03:49:16 +0900

    feat: 메세지 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String INPUT_BUY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUM_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUM_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String LOTTO_RESULT_MESSAGE = "당첨 통계";
 
@@ -13,6 +14,10 @@ public class OutputView {
 
     public static void inputWinningNumMessage(){
         System.out.println(INPUT_WINNING_NUM_MESSAGE);
+    }
+
+    public static void inputBonusNumMessage(){
+        System.out.println(INPUT_BONUS_NUM_MESSAGE);
     }
 
     public static void printBlank(){

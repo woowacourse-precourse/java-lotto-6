4ba9d92bf70a33003032d558e4d156ac7823c292
jaegyeongkim <dongji_11@naver.com>
2023-11-06 00:35:41 +0900

    feat: 문자열을 정수형으로 변환하는 기능 구현

--- src/main/java/utils/Converter.java
+++ src/main/java/utils/Converter.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Converter {
 
-    public static int stringToInt(String input){
+    public static int stringToInt(String input) {
         return Integer.parseInt(input);
     }
 }

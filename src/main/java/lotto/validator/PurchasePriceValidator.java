6f3f34b3ed7028de93be87a0d1630bce37837d3e
hil679 <hil679922@gmail.com>
2023-11-06 01:27:25 +0900

    Feat(RemainderZero): 1000으로 나누어 떨어지는지 확인하는 기능 구현

--- src/main/java/lotto/validator/PurchasePriceValidator.java
+++ src/main/java/lotto/validator/PurchasePriceValidator.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 public class PurchasePriceValidator {
+    private final int THOUSAND = 1000;
 
     private boolean isOverMinPrice() {
         return true;
@@ -10,11 +11,13 @@ public class PurchasePriceValidator {
 
     }
 
-    private boolean isRemainderZero() {
-        return true;
+    private boolean isRemainderNotZero(int price) {
+        return price % THOUSAND != 0;
     }
 
-    private void checkRemainderZero() {
-
+    public void checkRemainderZero(int price) {
+        if (isRemainderNotZero(price)) {
+            throw new IllegalArgumentException("[ERROR] 1000으로 나눠지지 않습니다. 1000원 단위로 입력해주세요.");
+        }
     }
 }

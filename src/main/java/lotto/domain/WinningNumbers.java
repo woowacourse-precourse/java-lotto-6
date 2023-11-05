27392040802e3b97c1dcba0b0c3f22c76b2413bb
slowcloud <sju9417@gmail.com>
2023-11-05 17:13:16 +0900

    fix: WinningNumbers에서 입력받은 숫자 길이, 중복 검사를 수행하도록 수정

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import lotto.constant.LottoConfig;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
 
@@ -11,10 +15,25 @@ public class WinningNumbers {
     }
 
     public static WinningNumbers createWinningNumbers(List<Integer> numbers) {
+        validateLength(numbers);
+        validateDuplication(numbers);
         List<WinningNumber> winningNumbers = numbers.stream()
                 .map(WinningNumber::new)
                 .toList();
         return new WinningNumbers(winningNumbers);
+    }
+
+    private static void validateLength(List<Integer> winningNumbers) {
+        if(winningNumbers.size() != LottoConfig.LOTTO_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateDuplication(List<Integer> winningNumbers) {
+        Set<Integer> set = new HashSet<>(winningNumbers);
+        if(set.size() != winningNumbers.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean contains(Integer number) {

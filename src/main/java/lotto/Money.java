aa1f21e529c8b680776b57b7a2722125c1ac3a0e
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-05 18:59:56 +0900

    feat(lotto): 구입 금액이 1000으로 나누어 떨어지지 않으면 예외처리

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -8,13 +8,19 @@ public class Money {
 
     public Money(String amount) {
         validateFormat(amount);
+        validateNumber(amount);
         this.amount = amount;
     }
 
     private void validateFormat(String amount) {
         if (!amount.matches("[0-9]+")){
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException("[ERROR] 입력 형식에 맞지 않습니다.");
         }
     }
 
+    private void validateNumber(String amount) {
+        if (Integer.parseInt(amount) % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 1000으로 나누어 떨어지지 않습니다.");
+        }
+    }
 }

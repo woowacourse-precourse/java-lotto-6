31d6d93076ff938be193d7d614dea005cb6ae138
김승우 <tmd8633@naver.com>
2023-11-05 20:00:49 +0900

    Refactor : application  try-catch문 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,12 +4,6 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-
-            LottoController.run();
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] " + e.getMessage());
-        }
+        LottoController.run();
     }
 }

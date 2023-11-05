21f953b16ea5a05eec9363bc742bd2173c5068f9
ohjuyoungg <ejrm12@naver.com>
2023-11-05 17:35:35 +0900

    feat: 로또 보너스 번호를 입력받는 메소드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,28 +1,39 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Collections;
 import java.util.List;
 import lotto.service.LottoService;
 import lotto.validation.InputValidation;
 
 public class InputView {
+
     public static int inputPurchaseAmount() {
-        String input = Console.readLine();
-        InputValidation.validateIsBlank(input);
+        String input = inputNotBlank();
+        InputValidation.validateInputString(input);
         int purchaseInput = Integer.parseInt(input);
         InputValidation.validateInputPurchaseAmount(purchaseInput);
-        InputValidation.validateInputString(input);
         return LottoService.amountPurchaseDivide(purchaseInput);
     }
 
     public static List<Integer> inputWinningNum() {
-        String input = Console.readLine();
-        InputValidation.validateIsBlank(input);
+        String input = inputNotBlank();
         List<Integer> winNumberList = InputValidation.validateInputWinningNumber(input);
         InputValidation.validateNoDuplicates(winNumberList);
         InputValidation.validateNumberRange(winNumberList);
         InputValidation.validateSixNumbers(winNumberList);
         return winNumberList;
+    }
+
+    public static int inputBonusNum() {
+        String input = inputNotBlank();
+        InputValidation.validateInputString(input);
+        int bonusNumber = InputValidation.validateBonusNumberRange(input);
+        return bonusNumber;
+    }
+
+    private static String inputNotBlank() {
+        String input = Console.readLine();
+        InputValidation.validateIsBlank(input);
+        return input;
     }
 }

ecf07c4db3f76436117910c2bc8e5eab15754d00
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 17:39:31 +0900

    test(winning): ensure error handling for invalid bonus number
    - Verify that an error is thrown when a number outside the 1 to 45 range is entered.
    - Confirm that exactly one bonus number are required.

---
+++ src/test/java/lotto/winning/BonusNumberTest.java
@@ -0,0 +1,43 @@
+package lotto.winning;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BonusNumberTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    BonusNumber bonusNumber = new BonusNumber();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setIn(System.in);
+        System.setOut(System.out);
+        Console.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "99", "1,2" })
+    void inAccurateInputs(String mockInput) {
+        System.setIn(new ByteArrayInputStream(mockInput.getBytes()));
+
+        try {
+            bonusNumber.ask();
+        } catch (NoSuchElementException ignored) {
+        }
+
+        assertThat(outputStream.toString()).contains("[ERROR]");
+    }
+}

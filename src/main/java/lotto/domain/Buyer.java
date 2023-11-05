7ac9ce4a64689c3c39f659d7a299dc9030c7753b
ay-eonii <naayen@naver.com>
2023-11-06 00:42:26 +0900

    feat(Buyer): 구매 로또 개수 가져오기

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.view.constant.Exception;
 
 public class Buyer {
+    private final static int price = 1000;
     private final int cost;
 
     public Buyer(int cost) {
@@ -10,8 +11,12 @@ public class Buyer {
         this.cost = cost;
     }
 
+    public int getBuyCount() {
+        return this.cost / price;
+    }
+
     private void validate(int cost) {
-        if (cost % 1000 != 0) {
+        if (cost % price != 0) {
             throw new IllegalArgumentException(Exception.COST_MOD);
         }
     }

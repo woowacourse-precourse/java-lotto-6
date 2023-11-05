29d511aaae28987cf2faa09b3f3f3f8878bed578
eunhee <dmsgml9903@gmail.com>
2023-11-06 04:19:35 +0900

    fix(LottoNumbersGenerator): 기능_테스트 문제 실패 해결
    아래 내용을 수정:
    - 기능_테스트는 Random#pickUniqueNumbersInRange을 사용하므로 해당 함수로 6자리의 랜덤 값 생성

--- src/main/java/lotto/model/LottoNumbersGenerator.java
+++ src/main/java/lotto/model/LottoNumbersGenerator.java
@@ -1,10 +1,7 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class LottoNumbersGenerator implements RandomNumbersGenerator {
 
@@ -14,19 +11,7 @@ public class LottoNumbersGenerator implements RandomNumbersGenerator {
 
     @Override
     public List<Integer> createUniqueElementList() {
-        Set<Integer> lottoNumbers = getLottoNumberSet();
-        return convertToList(lottoNumbers);
-    }
-
-    private Set<Integer> getLottoNumberSet() {
-        Set<Integer> lottoNumbers = new HashSet<>();
-        while (lottoNumbers.size() < LOTTO_LIST_SIZE) {
-            lottoNumbers.add(Randoms.pickNumberInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
-        }
-        return lottoNumbers;
-    }
-
-    private List<Integer> convertToList(Set<Integer> lottoNumberSet) {
-        return new ArrayList<>(lottoNumberSet);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
+                LOTTO_LIST_SIZE);
     }
 }

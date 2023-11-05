92ddb9c7ac5f1204302ecc7a1d2fd3d7d2d52593
Hanjaemo <hjm2530@gmail.com>
2023-11-06 05:25:37 +0900

    test(WinningNumbersTest): 당첨 번호와 보너스 번호 검증 기능 단위 테스트

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class WinningNumbersTest {
 
-    @DisplayName("")
+    private static final int WINNING_NUMBERS_SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
+    @DisplayName("로또 번호와 당첨 번호 및 보너스 비교를 비교해서 당첨 등수를 반환한다.")
     @ParameterizedTest
     @MethodSource("parametersForDetermineRankTest")
     void determineRank_Success(Lotto lotto, Rank rank) {
@@ -30,5 +35,74 @@ class WinningNumbersTest {
                 Arguments.of(new Lotto(List.of(1, 2, 3, 7, 9, 12)), Rank.FIFTH),
                 Arguments.of(new Lotto(List.of(1, 2, 9, 10, 11, 12)), Rank.MISS)
         );
+    }
+
+    @DisplayName("당첨 번호의 개수가 6개보다 크면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByOverSize() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7), 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 %d개여야 한다."
+                        .formatted(WINNING_NUMBERS_SIZE));
+    }
+
+    @DisplayName("당첨 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByUnderSize() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3), 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 %d개여야 한다."
+                        .formatted(WINNING_NUMBERS_SIZE));
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByDuplicatedNumber() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5), 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 서로 달라야 한다.");
+    }
+
+    @DisplayName("당첨 번호 중에서 1보다 작은 번호가 존재하면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByNumberIsLessThanMinimum() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 0), 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 %d이상 %d이하여야 한다."
+                        .formatted(MIN_NUMBER, MAX_NUMBER));
+    }
+
+    @DisplayName("당첨 번호 중에서 45보다 큰 번호가 존재하면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByNumberIsMoreThanMaximum() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 66), 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 %d이상 %d이하여야 한다."
+                        .formatted(MIN_NUMBER, MAX_NUMBER));
+    }
+
+    @DisplayName("보너스 번호가 1보다 작으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByBonusNumberIsLessThanMinimum() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 %d이상 %d이하여야 한다."
+                        .formatted(MIN_NUMBER, MAX_NUMBER));
+    }
+
+    @DisplayName("보너스 번호가 45보다 크면 예외가 발생한다.")
+    @Test
+    void createWinningNumbers_Fail_ByBonusNumberIsMoreThanMaximum() {
+        // when, then
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 77))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 %d이상 %d이하여야 한다."
+                        .formatted(MIN_NUMBER, MAX_NUMBER));
     }
 }

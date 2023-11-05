7c3676837be8b224285af1a8fc74794efb53d048
tam31 <wxogud3377@naver.com>
2023-11-06 00:31:22 +0900

    Refactor(Computer) : 화폐 금액 별 로또 번호 수집 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,19 @@
 package lotto.controller;
 
 import lotto.domain.Computer;
+import lotto.domain.Lottos;
 import lotto.domain.User;
 import lotto.view.InputView;
 
 public class LottoController {
 
     User user;
+    Computer computer;
     InputView inputView = new InputView();
 
-    public void run(){
+    public void run() {
         user = new User(inputView.purchaseAmountMoney());
+        computer = new Computer();
+        computer.drawRandomNumber(user, new Lottos());
     }
 }
--- src/main/java/lotto/domain/Computer.java
+++ src/main/java/lotto/domain/Computer.java
@@ -1,17 +1,18 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.List;
 
 public class Computer {
 
     private Lotto lotto;
     private Bonus bonus;
 
-    public void drawRandomNumber(int money) {
+    public void drawRandomNumber(User user, Lottos lottos) {
+        int money = user.getMoney();
         for (int drawLotto = 0; drawLotto < money / 1000; drawLotto++) {
             lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             bonus = new Bonus(lotto);
+            lottos.collectLottoNumbers(user, lotto, bonus);
         }
     }
 }

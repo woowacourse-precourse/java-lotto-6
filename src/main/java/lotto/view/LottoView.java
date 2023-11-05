88d9c09cb4d9f0a6910f2c458de77f1df8fdc59f
권장순 <jsoonworld@gmail.com>
2023-11-06 07:17:48 +0900

    feat(LottoView#inputMoney) : 구입 금액을 입력 받는 inputMoney()를 구현.

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,7 +1,14 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoView {
     public LottoView() {
-        
+
+    }
+
+    public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
     }
 }

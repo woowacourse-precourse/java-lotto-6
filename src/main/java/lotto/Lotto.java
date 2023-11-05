4659a6572e5c919f635c92ad8df4af30e95fbf9b
SeungHyunKee <keeseunghyun@gmail.com>
2023-11-05 23:57:04 +0900

    feat: Lotto.java(enum error)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,15 +6,31 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        if(numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개 숫자를 입력해야 합니다.");
+        }
+
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+        }
+
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
-    // TODO: 추가 기능 구현
+    public int countMatchingNumbers(Lotto winningLotto) {
+        int count = 0;
+        for (int number : numbers) {
+            if (winningLotto.getNumbers().contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public boolean containsNumber(int number) {
+        return numbers.contains(number);
+    }
 }

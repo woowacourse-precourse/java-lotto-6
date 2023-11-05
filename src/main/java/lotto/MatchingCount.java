830a729bda3d0188eb5399395c3f88813a69d140
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-05 18:40:13 +0900

    Refactor: 고침 FIVEBONUS 열거형
    단어간 구분자 추가

--- src/main/java/lotto/MatchingCount.java
+++ src/main/java/lotto/MatchingCount.java
@@ -5,7 +5,7 @@ public enum MatchingCount {
     THREE(3, 5000),
     FOUR(4, 50000),
     FIVE(5, 15000000),
-    FIVEBONUS(5.5f, 30000000),
+    FIVE_BONUS(5.5f, 30000000),
     SIX(6, 2000000000);
 
     private final float matchCount;

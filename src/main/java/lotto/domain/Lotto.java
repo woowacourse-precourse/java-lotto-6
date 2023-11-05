81b5befb426d04eb1dad4d8c66e25741cf801ee9
sujnkim <blackout3210@naver.com>
2023-11-05 15:26:13 +0900

    feat: Lotto 생성 시 중복된 number를 사용할 경우 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -14,7 +14,18 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if(isDuplicated(numbers)){
+            throw new IllegalArgumentException();
+        }
+
+    }
+    private boolean isDuplicated(List<Integer> numbers){
+        long distinctNumber = numbers.stream()
+                .distinct()
+                .count();
+
+        return distinctNumber < numbers.size();
     }
 
-    // TODO: 추가 기능 구현
 }

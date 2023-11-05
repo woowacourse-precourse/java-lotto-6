d89a22acc734bd75a6fd6033c2c9bb94072ea6c6
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-06 01:01:26 +0900

    feat(Lotto) : 입력받은 당첨 번호가 중복인 경우 예외처리 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() < 6;
+    }
+}

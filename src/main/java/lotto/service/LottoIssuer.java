72b7f5ee27f6c487bb4594b5c1d4435ba30dbb81
reddevilmidzy <midzy0228@gmail.com>
2023-11-06 00:42:13 +0900

    docs: TODO 주석 제거

--- src/main/java/lotto/service/LottoIssuer.java
+++ src/main/java/lotto/service/LottoIssuer.java
@@ -6,8 +6,6 @@ import lotto.model.PurchaseAmount;
 import java.util.List;
 import java.util.stream.IntStream;
 
-//TODO: Generate와 차이점이 무엇일지 고민해보기
-//TODO: Lotto 이 일급 컬렉션을 제대로 활용하지 못하고 있다. 적극 활용
 public class LottoIssuer {
 
     private final Integer lottoTicket;

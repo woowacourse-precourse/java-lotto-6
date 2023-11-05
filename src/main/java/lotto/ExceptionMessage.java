a5f1f3642d7cca434e6abedb6e65c26fa0341386
yubin <dbqls2821@gmail.com>
2023-11-05 16:10:51 +0900

    fix: enum내부 string 수정

--- src/main/java/lotto/ExceptionMessage.java
+++ src/main/java/lotto/ExceptionMessage.java
@@ -2,9 +2,9 @@ package lotto;
 
 enum ExceptionMessage {
     ERROR("[ERROR] "),
-    LOTTO_RANGE(ERROR + "로또 번호는 1에서 45 사이 숫자여야 합니다."),
-    LOTTO_SIZE(ERROR + "로또는 6개여야 합니다."),
-    LOTTO_DUPLICATION(ERROR + "로또번호에 중복되는 숫자가 있습니다."),
+    LOTTO_RANGE(ERROR.message + "로또 번호는 1에서 45 사이 숫자여야 합니다."),
+    LOTTO_SIZE(ERROR.message + "로또는 6개여야 합니다."),
+    LOTTO_DUPLICATION(ERROR.message + "로또번호에 중복되는 숫자가 있습니다."),
     ;
 
     private final String message;

2eacf5e3045f1f59343d7e84a57f0dcb8840841e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-05 16:21:39 +0900

    refactoring : 로또 결제하는 로직 분리
    기존 : purchaseLottoWithPayment 에서 , List 생성 후 LottoPaper 까지 생성
    변경 : createLottoPaper 에서 LottoPaper 생성 , purchase 에서 List 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -10,15 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
-    public LottoPaper purchaseLottoWithPayment(Payment payment){
+    public LottoPaper createLottoPaperWithPayment(Payment payment){
         int amount = payment.amount();
+
+        List<Lotto> lottoTickets = purchaseLottoWithAmount(amount);
+        LottoPaper lottoPaper = new LottoPaper(amount,lottoTickets);
+
+        return lottoPaper;
+    }
+    public List<Lotto> purchaseLottoWithAmount(int amount){
         int lottoTicketSize = LottoTicketCalculator.calculateLottoTicketQuantityWithAmount(amount);
         List<Lotto> lottoTickets = new ArrayList<>();
 
         for (int i = 0; i < lottoTicketSize; i++) {
             lottoTickets.add(issueLottoTicket());
         }
-        return new LottoPaper(amount,lottoTickets);
+
+        return lottoTickets;
     }
     private Lotto issueLottoTicket(){
         List<Integer> lottoNumbers = LottoNumberGenerator.generateLottoNumbers();

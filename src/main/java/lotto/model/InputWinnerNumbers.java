4512d0c775e450c4c3dfaf1602e3cea015b60110
U-keun Song <sukeun319@gmail.com>
2023-11-06 04:44:06 +0900

    Feat : InputWinnerNumbers 클래스 생성
    입력된 값이 유효한 리스트인지 확인하고, 숫자가 로또 번호로 사용할 수 있는 숫자인지 확인한다.

---
+++ src/main/java/lotto/model/InputWinnerNumbers.java
@@ -0,0 +1,51 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.util.InputList;
+
+public class InputWinnerNumbers extends InputList<Integer> {
+    InputWinnerNumbers(String input) {
+        super(input);
+    }
+
+    public static InputWinnerNumbers getInstance(String input) {
+        return new InputWinnerNumbers(input);
+    }
+
+    public void validate() {
+        if (notLottoNumbers()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public Lotto sendInputData() {
+        return new Lotto(list);
+    }
+
+    protected List<Integer> convertElements(List<String> input) {
+        return input.stream()
+                .map(this::checkNumber)
+                .collect(Collectors.toList());
+    }
+
+    private Integer checkNumber(String element) {
+        Integer number;
+        try {
+            number = Integer.parseInt(element);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return number;
+    }
+
+    private boolean notLottoNumbers() {
+        for (Integer number:list) {
+            if (number < 1 || number > 45) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

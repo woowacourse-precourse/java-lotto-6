02eba87b708678c1d9e85e0dc6ed2d2633b42d8e
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-05 15:41:34 +0900

    refactor:메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import static lotto.constant.GeneralConstant.INT_NULL;
+
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
@@ -11,72 +13,111 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
-    private final LottoCompany lottoCompany = LottoCompany.INSTANCE;
-    private final User user = User.INSTANCE;
-
+    private static boolean success;
+    private static String input;
+    private static List<Lotto> userLotto;
     public static void run() {
-        boolean success;
-        String input;
+        inputMoney();
+        printLottos();
+        inputPrizeNumbers();
+        inputBonusNumber();
+        matchLottoByCompany();
+        printRateOfReturn();
+    }
+
+    private static void inputMoney() {
         success = false;
         while (!success) {
-            try {
-                input = InputView.inputMoney();
-                NumberValidator.validateChangeNumber(input);
-                User.purchaseLottos(StringUtil.convertStringToInt(input));
-                success = true;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+            tryInputMoney();
         }
+    }
 
+    private static void tryInputMoney() {
+        try {
+            input = InputView.inputMoney();
+            int money = changeStringToInt(input);
+            User.purchaseLotto(money);
+            success = true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-        List<Lotto> lottos = User.getLottos();
-        OutputView.printPurchase(lottos.size());
-        OutputView.printLottos(lottos);
+    private static void printLottos() {
+        userLotto = User.getMyLotto();
+        OutputView.printPurchase(userLotto.size());
+        OutputView.printLottos(userLotto);
+    }
 
-
+    private static void inputPrizeNumbers() {
         success = false;
         while (!success) {
-            try {
-                input = InputView.inputPrizeNumber();
-                List<String> splitString = StringUtil.splitWithComma(input);
-                for(String string : splitString) {
-                    NumberValidator.validateChangeNumber(string);
-                }
-
-                List<Integer> prizeNumbers = new ArrayList<Integer>();
-                for(String string : splitString) {
-                    prizeNumbers.add(StringUtil.convertStringToInt(string));
-                }
-                LottoCompany.setPrizeNumbers(prizeNumbers);
-                success = true;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+            tryInputPrizeNumbers();
         }
+    }
 
+    private static void tryInputPrizeNumbers() {
+        try {
+            input = InputView.inputPrizeNumber();
+            setPrizeNumbers(input);
+            success = true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void setPrizeNumbers(String input) {
+        List<String> splitString = StringUtil.splitWithComma(input);
+        List<Integer> prizeNumbers = new ArrayList<Integer>();
+        for(String string : splitString) {
+            prizeNumbers.add(changeStringToInt(string));
+        }
+        LottoCompany.setPrizeNumbers(prizeNumbers);
+    }
+
+    private static void inputBonusNumber() {
         success = false;
         while (!success) {
-            try {
-                input = InputView.inputBonusNumber();
-                NumberValidator.validateChangeNumber(input);
-                LottoCompany.setBonusNumber(StringUtil.convertStringToInt(input));
-                success = true;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+            tryInputBonusNumber();
         }
+    }
 
-
-        for(Lotto lotto : lottos) {
-            int tmp = LottoCompany.matchPrize(lotto.getNumbers());
-            if(tmp != -1){
-                User.plusPrizeCount(tmp);
-            }
+    private static void tryInputBonusNumber() {
+        try {
+            input = InputView.inputBonusNumber();
+            setBonusNumber(input);
+            success = true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
+    }
 
+    private static void setBonusNumber(String input) {
+        int bonusNumber = changeStringToInt(input);
+        LottoCompany.setBonusNumber(bonusNumber);
+    }
+
+    private static void matchLottoByCompany() {
+        for(Lotto lotto : userLotto) {
+            int rank = LottoCompany.matchPrize(lotto.getNumbers());
+            receivePrize(rank);
+        }
+    }
+
+    private static void receivePrize(int rank) {
+        if(rank != INT_NULL){
+            User.plusPrizeCount(rank);
+        }
+    }
+
+    private static void printRateOfReturn() {
         User.calcRateOfReturn();
         OutputView.printPrizeStatistics(User.getPrizeCounts());
         OutputView.printRateOfReturn(User.getRateOfReturn());
+    }
+
+    private static int changeStringToInt(String input) {
+        NumberValidator.validateChangeNumber(input);
+        return StringUtil.convertStringToInt(input);
     }
 }

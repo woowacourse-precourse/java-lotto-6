98bd4678d5f9dccb48cd501febe4541c796493e3
SeongIl <biuld1234@gmail.com>
2023-11-05 12:45:27 +0900

    refactor(Prize): add matchingNumber

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -2,20 +2,24 @@ package lotto;
 
 public enum Prize {
 
-    FIRST("1등", 2_000_000_000),
-    SECOND("2등", 30_000_000),
-    THIRD("3등", 1_500_000),
-    FOURTH("4등", 50_000),
-    FIFTH("5등", 5_000);
+    FIRST(6,"1등", 2_000_000_000),
+    SECOND(5,"2등", 30_000_000),
+    THIRD(5,"3등", 1_500_000),
+    FOURTH(4,"4등", 50_000),
+    FIFTH(3,"5등", 5_000);
 
-
+    private final Integer matchingNumber;
     private final String ranking;
-    private final int prizeAmount;
+    private final Integer prizeAmount;
 
-    Prize(String ranking, int prizeAmount) {
-
+    Prize(Integer matchingNumber, String ranking, Integer prizeAmount) {
+        this.matchingNumber = matchingNumber;
         this.ranking = ranking;
         this.prizeAmount = prizeAmount;
+    }
+
+    public Integer getMatchingNumber() {
+        return matchingNumber;
     }
 
     public String getRanking() {

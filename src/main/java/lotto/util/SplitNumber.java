70b8a1958511b10e86167412e35a05c5777eb2d8
Juhyorim <joohr1234@gmail.com>
2023-11-06 04:17:31 +0900

    feat: 당첨번호를 입력받을 때 입력이 20억을 넘어가면 예외발생 기능 구현

--- src/main/java/lotto/util/SplitNumber.java
+++ src/main/java/lotto/util/SplitNumber.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class SplitNumber {
     private static final String NOT_A_NUMBER_MESSAGE = "입력은 숫자여야 합니다";
+    private static final String UNDER_20_BILLION_REQUIRED = "입력은 20억 이하여야 합니다";
     private static final String NUMBER_REGEX = "[0-9]+";
 
     public static List<Integer> splitNumber(String inputNumber) {
@@ -29,6 +30,23 @@ public class SplitNumber {
     private static void validateIsNumber(String number) {
         if (number.matches(NUMBER_REGEX) == false) {
             throw new IllegalArgumentException(NOT_A_NUMBER_MESSAGE);
+        }
+
+        validateIsUnder20Billion(number);
+    }
+
+    private static void validateIsUnder20Billion(String input) {
+        if (input.length() > 10) {
+            throw new IllegalArgumentException(UNDER_20_BILLION_REQUIRED);
+        }
+
+        try {
+            int number = Integer.parseInt(input);
+            if (number > 2000000000) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(UNDER_20_BILLION_REQUIRED);
         }
     }
 }

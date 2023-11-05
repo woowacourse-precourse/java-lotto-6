169a0abb045d44645e49023c225ecb921fe1d4ac
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-05 12:52:29 +0900

    Feat: 각 등수의 맞춘 개수와 상금을 조회하는 기능 구현

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,19 +1,26 @@
 package lotto;
 
 public enum Prize {
-	FIFTH("5등", "3개 일치", 5000),
-	FOURTH("4등", "4개 일치", 50000),
-	THIRD("3등", "5개 일치", 1500000),
-	SECOND("2등", "5개 일치, 보너스 볼 일치", 30000000),
-	FIRST("1등", "6개 일치", 2000000000);
+	FIFTH("3개 일치", 5000),
+	FOURTH("4개 일치", 50000),
+	THIRD("5개 일치", 1500000),
+	SECOND("5개 일치, 보너스 볼 일치", 30000000),
+	FIRST("6개 일치", 2000000000);
 
-	final String rank;
-	final String hitResult;
-	final long money;
+	private final String hitResult;
+	private final long money;
 
-	Prize(String rank, String hitCount, long money) {
-		this.rank = rank;
+	Prize(String hitCount, long money) {
+
 		this.hitResult = hitCount;
 		this.money = money;
+	}
+
+	public String getHitResult() {
+		return hitResult;
+	}
+
+	public long getMoney() {
+		return money;
 	}
 }

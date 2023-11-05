d98469d7f1fb089a334e8e80add6a5a3fa1d15c7
yewonahn <yewonahn@naver.com>
2023-11-06 00:13:21 +0900

    feat: 사용자로부터 로또 당첨번호 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,35 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public static int inputMoney() {
-        String stringMoney =readLine();
+        OutputView.printInputMoneyMsg();
+
+        String stringMoney = readLine();
         int money = Integer.parseInt(stringMoney);
+
         return money;
     }
-    public static void inputWinningNumbers() {
+    public static List<String> inputWinningNumbers() {
+        OutputView.printInputWinningNumbersMsg();
 
+        String userInputWinningNumbers = readLine();
+        String[] splitWinningNumbers = userInputWinningNumbers.split(",", -1);
+        List<String> winningNumbers = new ArrayList<>(Arrays.asList(splitWinningNumbers));
+
+        // [예외처리] 입력한 당첨 번호 중에 공백이 있는 경우
+        // [예외처리] 중복된 숫자를 입력한 경우
+        // [예외처리] 범위에서 벗어나는 숫자를 입력한 경우
+
+        return winningNumbers;
     }
     public void inputBonusNumber() {
-
     }
 }

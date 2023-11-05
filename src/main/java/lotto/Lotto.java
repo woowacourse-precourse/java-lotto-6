44a97674848034e6c0b74a8458967c09cfc8d9ba
Hajuuu <hpt0127@naver.com>
2023-11-05 20:25:52 +0900

    feat(Application): printLotto 작성
    로또 번호를 출력하는 함수 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -18,8 +18,13 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    public List<Integer> randomNumber() {
+    public static List<Integer> randomNumber() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return randomNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 }

b43937e1d4fca247f08887706f0a16f9823c6f26
강승민 <rkdtmdals999@naver.com>
2023-11-05 20:26:15 +0900

    [refactor] UserInput은 공통으로 쓰이므로 static 메소드로 변경

--- src/main/java/lotto/controller/UserInput.java
+++ src/main/java/lotto/controller/UserInput.java
@@ -6,12 +6,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
     public UserInput() {}
-    public Integer inputPurchaseAmount() {
+    public static Integer PurchaseAmount() {
         String userInput = Console.readLine();
         Integer purchaseAmount = Integer.parseInt(userInput);
         return purchaseAmount;
     }
-    public Integer calculatePurchaseLottoNumber(Integer purchaseAmount) {
+    public static Integer calculatePurchaseLottoNumber(Integer purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
     }
 }

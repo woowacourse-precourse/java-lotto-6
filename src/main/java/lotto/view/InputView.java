1f1c6c62964029a4c425c5ba32e2a1020b04ebd6
706com <ehdgusjdbswl@gmail.com>
2023-11-06 05:12:20 +0900

    feat : 당첨번호 입력 view 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,8 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_MONEY_TO_BUY = "구입금액을 입력해 주세요.";
 
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+
     public static String inputMoneyToBuyMessage(){
         System.out.println(INPUT_MONEY_TO_BUY);
         return Console.readLine();
     }
+    public static String inputWinningNumberMessage(){
+        System.out.println(INPUT_WINNING_NUMBER);
+        return Console.readLine();
+    }
+
 }

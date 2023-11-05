40d9d017f58d0cf2dc8fd5f6b99069afd2776c91
Heejun-0219 <heejun0219@gmail.com>
2023-11-05 21:02:20 +0900

    feat(Tickets): 로또 티켓 리스트 기능 생성

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,9 +1,33 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 
 public class LottoTickets {
-    private List<Lotto> Tickets;
+    private static int ticketCount;
+    private static List<Lotto> Tickets = new ArrayList<>();
 
+    public LottoTickets(int money) {
+        ticketCount = TicketCount(money);
+        makeLottoTickets();
+    }
+
+    public static int getTicketCount() {
+        return ticketCount;
+    }
+
+    public static List<Lotto> getTickets() {
+        return Tickets;
+    }
+
+    private static int TicketCount(int money) {
+        return money / 1000;
+    }
+
+    private static void makeLottoTickets() {
+        for (int i = 0; i < ticketCount; i++) {
+            Tickets.add(new Lotto(LottoNumbersGenerator.generate()));
+        }
+    }
 }

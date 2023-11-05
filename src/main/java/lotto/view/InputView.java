9172d9252a206645df11b1d37ccbb8a267d23e5b
김효준 <khyojun1104@gmail.com>
2023-11-05 15:15:26 +0900

    feat(lotto): InputView, OutputView 싱글톤 패턴으로 초기 셋팅!

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+public class InputView {
+
+
+    private static InputView instance;
+
+
+    private InputView() {
+    }
+
+
+    public static InputView getInstance() {
+        if(instance==null){
+            instance = new InputView();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static OutputView instance;
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        if (instance == null) {
+            instance = new OutputView();
+        }
+        return instance;
+    }
+
+
+}

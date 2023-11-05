ed5ad531674ad0bcbc70f653bbd33665f8aef1b3
jmj005 <jmj00005@naver.com>
2023-11-06 03:40:58 +0900

    feat: 로또 결과를 위한 LottoResult 클래스 생성

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,2 +1,11 @@
-package lotto.model;public class LottoResult {
+package lotto.model;
+
+import java.util.EnumMap;
+
+public class LottoResult {
+    private final EnumMap<Rank, Integer> lottoResult;
+
+    public LottoResult(EnumMap<Rank, Integer> lottoResult) {
+        this.lottoResult = lottoResult;
+    }
 }

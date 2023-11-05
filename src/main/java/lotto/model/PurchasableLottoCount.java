e60d78abaa9d8097295f47abc53732d9e4a0cc32
soochan <tncksdl05@gmail.com>
2023-11-05 17:13:03 +0900

    refactor(PurchasableLottoCount): 구매가능한 로또 개수 범위값 조정
    현실 도메인을 반영하여 구매가능한 로또 개수 범위값 조정

--- src/main/java/lotto/model/PurchasableLottoCount.java
+++ src/main/java/lotto/model/PurchasableLottoCount.java
@@ -3,8 +3,9 @@ package lotto.model;
 import java.util.Objects;
 
 public final class PurchasableLottoCount {
-    private static final String MINIMUM_COUNT_EXCEPTION_FORMAT = "로또 구입 개수는 %d개 이상이어야 합니다.";
-    private static final int MINIMUM_COUNT = 1;
+    private static final String MINIMUM_COUNT_EXCEPTION_FORMAT = "로또 구입 개수는 %d개 이상 %d개 이하로 구매할 수 있습니다.";
+    private static final int MAX_COUNT = 100;
+    private static final int MIN_COUNT = 1;
 
     private final int count;
 
@@ -14,10 +15,14 @@ public final class PurchasableLottoCount {
     }
 
     private void validate(int count) {
-        if (count < MINIMUM_COUNT) {
-            String exceptionMessage = String.format(MINIMUM_COUNT_EXCEPTION_FORMAT, MINIMUM_COUNT);
+        if (isOutOfRange(count)) {
+            String exceptionMessage = String.format(MINIMUM_COUNT_EXCEPTION_FORMAT, MIN_COUNT, MAX_COUNT);
             throw new IllegalArgumentException(exceptionMessage);
         }
+    }
+
+    private static boolean isOutOfRange(int count) {
+        return count < MIN_COUNT || count > MAX_COUNT;
     }
 
     public static PurchasableLottoCount from(int lottoCount) {

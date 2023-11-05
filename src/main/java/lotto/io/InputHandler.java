d20dd0bbabbcbf2ee297911ef1da431406d40f44
DooDuZ <sin9158@naver.com>
2023-11-06 08:04:49 +0900

    feat: 입출력 기능 추가

---
+++ src/main/java/lotto/io/InputHandler.java
@@ -0,0 +1,12 @@
+package lotto.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public String readLine(){
+        return Console.readLine();
+    }
+    public void close(){
+        Console.close();
+    }
+}
---
+++ src/main/java/lotto/io/OutputHandler.java
@@ -0,0 +1,7 @@
+package lotto.io;
+
+public class OutputHandler {
+    public void print(String message){
+        System.out.println(message);
+    }
+}

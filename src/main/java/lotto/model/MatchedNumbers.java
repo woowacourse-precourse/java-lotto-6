f0bd5969083292d3684d378d85e14a26f218e28a
kkkkimtaehyeon <kth1927@naver.com>
2023-11-06 01:56:24 +0900

    feat : 전체 로또에서 맞춘 각각의 로또 번호 갯수를 리스트에 저장하고 반환하는 기능 구현

--- src/main/java/lotto/model/MatchedNumbers.java
+++ src/main/java/lotto/model/MatchedNumbers.java
@@ -5,20 +5,23 @@ import java.util.HashSet;
 import java.util.List;
 
 public class MatchedNumbers {
-
-    /*
-    private void matchedNumberCounter(ArrayList<List<String>> winLottoNumber, ArrayList<String> myLottoNumber){
-        for(List<String> winNumbers : winLottoNumber){
-            if(winNumbers.stream().anyMatch(lotto -> lo)){
-
-            }
-        }
-
+    private final ArrayList<String> LOTTO_MATCHED_COUNT_LIST;
+    public MatchedNumbers(ArrayList<List<String>> winLottoNumber, ArrayList<String> myLottoNumber){
+        this.LOTTO_MATCHED_COUNT_LIST = matchedNumberCounterAll(winLottoNumber,myLottoNumber);
     }
 
-     */
 
-    public static int matchedNumberFinder(List<String> winLottoNumber, ArrayList<String> myLottoNumber){
+    private ArrayList<String> matchedNumberCounterAll(ArrayList<List<String>> winLottoNumber, ArrayList<String> myLottoNumber){
+        ArrayList<String> matchedLottos = new ArrayList<>();
+        for(List<String> winLotto : winLottoNumber){
+            matchedLottos.add(String.valueOf(matchedNumberCountEach(winLotto,myLottoNumber)));
+        }
+        return matchedLottos;
+    }
+
+
+
+    public static int matchedNumberCountEach(List<String> winLottoNumber, ArrayList<String> myLottoNumber){
         HashSet<String> winHash = new HashSet<>(winLottoNumber);
         HashSet<String> myHash = new HashSet<>(myLottoNumber);
         winHash.retainAll(myHash);

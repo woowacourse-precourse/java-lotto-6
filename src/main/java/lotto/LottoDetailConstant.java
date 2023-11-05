2a46f379300c0170958d0d4ae7b734e458773008
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-05 21:36:39 +0900

    feat : 로또의 길이, 시작 숫자, 최대 숫자에 대한 상수 추가

---
+++ src/main/java/lotto/LottoDetailConstant.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class LottoDetailConstant {
+    public static final int LOTTO_NUMBER_STORE_SIZE = 6;
+    public static final int LOTTO_START_NUMBER = 1;
+    public static final int LOTTO_END_NUMBER = 45;
+}

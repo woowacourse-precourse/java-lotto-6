d37ab20f6fb477fd221eeb86c81e812507bbfee9
Kim JunHyun <jjag1015@naver.com>
2023-11-06 06:29:58 +0900

    feat(lotto): 로또 번호 생성 기능 추가

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoGenerator {
+    public static Lotto makeNewLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return new Lotto(numbers);
+    }
+
+    public static Lottos makeNewLottos(Integer count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(makeNewLotto());
+        }
+        return new Lottos(lottos);
+    }
 }

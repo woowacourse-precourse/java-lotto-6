179a8ad1efa425f0aac9834e88945859644181d4
YONGHWA\ekore <yonghwa95@naver.com>
2023-11-05 14:11:59 +0900

    refactor(NumberGenerator) : 변경 가능한 List 만들기 위해 new ArrayList 반환하도록 수정

--- src/main/java/lotto/Util/NumbersGenerator.java
+++ src/main/java/lotto/Util/NumbersGenerator.java
@@ -1,15 +1,16 @@
 package lotto.Util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumbersGenerator {
     public static final int MIN_NUMBER_RANGE = 1;
     public static final int MAX_NUMBER_RANGE = 45;
-    public static final int LOTTO_NUMBER_COUNT = 6;
 
     public static List<Integer> generateRandomNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        List<Integer> numbers = new ArrayList(Randoms.pickUniqueNumbersInRange(1,45,6));
         numbers.sort(Integer::compareTo);
         return numbers;
     }

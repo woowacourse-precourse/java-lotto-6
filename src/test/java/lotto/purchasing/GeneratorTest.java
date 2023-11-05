df8dff35feb04e2231e2c5b2253f53a03c99e7ca
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 16:20:53 +0900

    test(purchasing): verify generated random numbers are unique

---
+++ src/test/java/lotto/purchasing/GeneratorTest.java
@@ -0,0 +1,20 @@
+package lotto.purchasing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class GeneratorTest {
+
+    @Test
+    public void noDuplicateNumbersTest() {
+        Generator generator = new Generator(new PurchaseAmount());
+
+        List<Integer> mockLotto =
+                generator.issue(1, 1, 6, 6).get(0).getNumbers();
+
+        assertThat(mockLotto).isEqualTo(Arrays.asList(1,2,3,4,5,6));
+    }
+}

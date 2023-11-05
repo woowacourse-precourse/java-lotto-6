e0bc02bc31a2dbfc624fa827d921167111029244
YongJOoOo <we072200@naver.com>
2023-11-05 21:43:54 +0900

    test: ApplicationTest에 잘못된 입력값을 받았을 때 Game이 다시 입력받는지 확인하는 test를 추가함

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,13 +1,13 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -27,6 +27,46 @@ class ApplicationTest extends NsTest {
                             "[7, 11, 30, 40, 42, 43]",
                             "[2, 13, 22, 32, 38, 45]",
                             "[1, 3, 5, 14, 22, 45]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 62.5%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
+    }
+
+    @DisplayName("잘못 입력했을 때 다시 입력받음")
+    @Test
+    void re_input_test() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1234", "8000", "1,2,3,4,5,5", "1,2,3,4,5,a", "1,2,3,4,5,46", "1,2,3,4,5,6", "1", "7");
+                    assertThat(output()).contains(
+                            "[ERROR} 구매 금액은 1000원 단위로 작성되어야 합니다.",
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]",
+                            "[ERROR] 당첨 숫자는 서로 중복되면 안됩니다.",
+                            "[ERRPR] 당첨 번호 포맷이 잘못되었습니다.",
+                            "[ERRPR] 당첨 번호 포맷이 잘못되었습니다.",
+                            "[ERROR] 보너스 숫자는 당첨 번호 6개와 중복되면 안됩니다.",
                             "3개 일치 (5,000원) - 1개",
                             "4개 일치 (50,000원) - 0개",
                             "5개 일치 (1,500,000원) - 0개",

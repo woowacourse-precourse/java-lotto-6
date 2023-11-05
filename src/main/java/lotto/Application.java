c225a91fc5701f5aa54f7caf6e5c0884f9cbaa3c
방준호 <yureka0920@naver.com>
2023-11-05 17:08:31 +0900

    1. 로또 구입금액을 입력받는다. 단위는 1000원, 1000원으로 나누어 떨어지지 않으면 예외처리.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        while(true){
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                int inputPrice = Integer.valueOf(readLine());
+                validateThousandUnit(inputPrice);
+                return;
+            } catch (IllegalArgumentException e) {
+                e.getMessage();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void validateThousandUnit (int inputPrice) {
+        if((inputPrice %1000)!=0){
+           /* System.out.println();*/
+            throw new IllegalArgumentException("[ERROR] 구입금액 단위는 1000원으로 나누어 떨어져야 합니다.");
+        }
     }
 }

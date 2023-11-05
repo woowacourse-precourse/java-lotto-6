10596e4f8472d53b24e7a5d666e6ff8e5845facb
GO-TE <alsrudxo9810@naver.com>
2023-11-06 01:50:28 +0900

    refacot(LottoMaker): 하드 코딩 제거 - 1, 45, 6 상수로 변경

--- src/main/java/lotto/model/LottoMaker.java
+++ src/main/java/lotto/model/LottoMaker.java
@@ -1,5 +1,9 @@
 package lotto.model;
 
+import static lotto.util.Constant.LOTTO_MINIMUM_NUMBER;
+import static lotto.util.Constant.LOTTO_MAXIMUM_NUMBER;
+import static lotto.util.Constant.LOTTO_WINNING_NUMBER_LENGTH;
+
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -7,6 +11,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoMaker {
     public List<Integer> getLotto() { // 로또 만들기
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MINIMUM_NUMBER, LOTTO_MAXIMUM_NUMBER, LOTTO_WINNING_NUMBER_LENGTH);
     }
 }

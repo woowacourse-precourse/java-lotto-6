8fd1923c8685c4b2f2e33a9dc200df16a40bb0ec
gonixxxx <akdntm946@gmail.com>
2023-11-05 15:04:51 +0900

    feat: add Lottos class to manage lotto numbers

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}

a8c24774a8e3b762ee3080cf1496db84d9fba034
carol <carol072400@gmail.com>
2023-11-05 18:59:36 +0900

    feat(lotto): 로또를 발행하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++
@@ -1,7 +0,0 @@
-package lotto.controller;
-
-public class LottoController {
-
-
-
-}
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,30 +8,31 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    private static List<Integer> issueTicket() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
-    }
-
-    public static List<List<Integer>> issueTickets(int ticketCount) {
-        List<List<Integer>> lotteryTickets = new ArrayList<>();
-        for(int i =0; i<ticketCount; i++) {
-            lotteryTickets.add(issueTicket());
-        }
-        return lotteryTickets;
-    }
-
     public Lotto(List<Integer> numbers) {
-
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 티켓 한 장에는 6개의 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 6개의 로또 번호를 입력해야 합니다.");
+        }
+
+        for (int number : numbers) {
+            if(1>number || 45<number)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 6개의 로또 번호는 중복되면 안됩니다.");
         }
     }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+
 
     // TODO: 추가 기능 구현
 }

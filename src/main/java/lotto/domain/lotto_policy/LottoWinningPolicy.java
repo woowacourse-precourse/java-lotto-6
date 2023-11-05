0aecb8b489fd299c9f635a4d8398e109807f7471
Minjae-An <mj3242@naver.com>
2023-11-05 17:40:43 +0900

    feat : 인터페이스 삭제

--- src/main/java/lotto/domain/lotto_policy/LottoWinningPolicy.java
+++
@@ -1,7 +0,0 @@
-package lotto.domain.lotto_policy;
-
-import java.util.List;
-
-public interface LottoWinningPolicy {
-    Integer getWinningPrize(List<Integer> lottoNumbers, List<Integer> winningNumbers);
-}

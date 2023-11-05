7266eff3cd4c4460dce7fb8128f9d7f198873470
sangxxjin <sj_0368@naver.com>
2023-11-05 17:07:41 +0900

    feat: 당첨 번호가 6개 초과일때 예외처리 구현

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -7,6 +7,7 @@ public class WinningNumber {
     private List<Integer> winningNumber;
     private static final String DUPLICATE_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호에 중복된 숫자가 포함되어 있습니다.";
     private static final String OUT_OF_RANGE_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 1에서 45 사이의 숫자여야 합니다.";
+    private static final String EXCESS_WINNING_NUMBERS_ERROR_MESSAGE = "[ERROR] 당첨 번호는 6개를 초과할 수 없습니다.";
 
     public WinningNumber(String winningNumbers) {
         List<Integer> winningNumber = Arrays.stream(winningNumbers.split(",")).map(String::trim).map(Integer::parseInt)
@@ -30,6 +31,12 @@ public class WinningNumber {
     private void validateWinningNumbersForRange(List<Integer> winningNumber) {
         if (winningNumber.stream().anyMatch(number -> number < 1 || number > 45)) {
             throw new IllegalArgumentException(OUT_OF_RANGE_WINNING_NUMBERS_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateWinningNumbersCount(List<Integer> winningNumber) {
+        if (winningNumber.size() > 6) {
+            throw new IllegalArgumentException(EXCESS_WINNING_NUMBERS_ERROR_MESSAGE);
         }
     }
 

b8bc9ae4c4a79b098282ddfe83927fd2146244c9
LUKR7Q <ybkim0628@naver.com>
2023-11-05 23:10:30 +0900

    inputPlayerAmount(): 플레이어로부터 구입 금액을 입력받고, PlayerLottoAmount 객체를 생성하여 로또 티켓 수를 계산합니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,22 @@
 package lotto.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.service.Lotto;
+import lotto.service.PlayerLottoAmount;
 import lotto.service.WinningResult;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     public LottoController() {
     }
 
+    private static final int TICKET_PRICE = 1000;
+    private static final int PERCENTAGE = 100;
 
+    private static PlayerLottoAmount playerLottoAmount;
+    private static List<Integer> lotto = new ArrayList<>();
     private static List<Lotto> lottoList;
     private static WinningResult winningResult;
 
@@ -33,6 +40,10 @@ public class LottoController {
 
     }
 
+    public int inputPlayerAmount() {
+        playerLottoAmount = new PlayerLottoAmount(InputView.inputPlayerAmount());
+        return playerLottoAmount.calculateLottoCount();
+    }
 
 }
 

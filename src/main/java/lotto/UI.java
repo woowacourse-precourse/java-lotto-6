1d7611d5f291a4f5e356dc99eec6cc47cefcddd6
jeli01 <jeli01@naver.com>
2023-11-06 02:17:48 +0900

    fix: catch print purchases function error
    change static number to dynamic number print.

--- src/main/java/lotto/UI.java
+++ src/main/java/lotto/UI.java
@@ -17,7 +17,7 @@ public class UI {
     }
 
     public static void printPurchases(List<Lotto> lottos) {
-        System.out.println("8개를 구매했습니다.");
+        System.out.println(lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             printLotto(lotto);
         }

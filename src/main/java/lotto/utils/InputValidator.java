f9678e06c5f5bfe3c5efe542132c47a9f6307f43
isyoo1213 <moethns@gmail.com>
2023-11-05 20:09:04 +0900

    feat: InputValidator 당첨번호 입력값 변환 기능 추가 InputValidator - convertInputToLottoNumbers() 추가   - castingSTringToIntegerList() README.md - 기능 명세 구현 체크

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -2,7 +2,15 @@ package lotto.utils;
 
 import lotto.constants.ExceptionMessages;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class InputValidator {
+    private static final char LOTTO_NUMBER_SEPARATOR = ',';
+    private static final int NUMBER_OF_SEPARATOR = 5;
+
     public String preprocessUserInput(String userInput) {
         if (isNull(userInput)) {
             ExceptionMessages.NULL_INPUT.throwException();
@@ -17,6 +25,28 @@ public class InputValidator {
 
     public int convertInputToPaymentAmount(String preprocessedInput) {
         return castStringToInt(preprocessedInput);
+    }
+
+    public List<Integer> convertInputToLottoNumbers(String preprocessedInput) {
+        return castingStringToIntegerList(preprocessedInput);
+    }
+
+    private List<Integer> castingStringToIntegerList(String preprocessedInput) {
+        if (isNotEnoughSeparators(preprocessedInput)) {
+            ExceptionMessages.WRONG_SEPARATOR_NUMBERS.throwException();
+        }
+        List<Integer> convertedInput =
+        Stream.of(preprocessedInput.split(String.valueOf(LOTTO_NUMBER_SEPARATOR)))
+                .mapToInt(Integer::parseInt)
+                .boxed().toList();
+        return convertedInput;
+    }
+
+    private boolean isNotEnoughSeparators(String preprocessedInput) {
+        int seperatorCount = (int) preprocessedInput.chars()
+                .filter(inputString -> inputString == LOTTO_NUMBER_SEPARATOR)
+                .count();
+        return seperatorCount != NUMBER_OF_SEPARATOR;
     }
 
     private int castStringToInt(String preprocessedInput) {

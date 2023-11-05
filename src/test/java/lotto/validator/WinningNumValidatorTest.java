4bdf8a865229c8c676654ce1ffe0e040c73e1f0d
chae401 <chae401@sju.ac.kr>
2023-11-05 16:45:28 +0900

    test(WinningnUmValidatorTest): 당첨 번호에 중복된 수가 입력된 경우

--- src/test/java/lotto/validator/WinningNumValidatorTest.java
+++ src/test/java/lotto/validator/WinningNumValidatorTest.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 import static lotto.constants.ExceptionMessages.CATCH_ERROR;
+import static lotto.constants.ExceptionMessages.DUPLICATED_NUMBER;
 import static lotto.constants.ExceptionMessages.INPUT_EMPTY;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,14 +11,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumValidatorTest {
-    @DisplayName("공백을 입력한 경우")
+    @DisplayName("[당첨 번호 입력 검증] 공백을 입력한 경우")
     @Test
-    void 공백_입력한_경우() {
+    void validateWinningNumTest1() {
         //given
         String text = "";
         // then
         Assertions.assertThatThrownBy(() -> WinningNumValidator.validateWinningNums(text))
                 .hasMessage(CATCH_ERROR + INPUT_EMPTY);
+    }
+
+    @DisplayName("[당첨 번호 입력 검증] 숫자가 중복된 경우")
+    @Test
+    void validateWinningNumTest2() {
+        //given
+        String text = "1,2,2,3,4,5";
+        // then
+        Assertions.assertThatThrownBy(() -> WinningNumValidator.validateWinningNums(text))
+                .hasMessage(CATCH_ERROR + DUPLICATED_NUMBER);
     }
 
 }

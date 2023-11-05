36a439a7f2357d8c8926309e73da4709f9aebce0
rakyun1 <dbskim234@naver.com>
2023-11-04 22:46:54 -0600

    Refactor: fix Function, contructor 생성자를 기본 생성자로 바꾸고UI기능을 빼서 함수가 한 가지 일만 하도록 리펙토링함

--- src/main/java/lotto/LottoNumberFactory.java
+++ src/main/java/lotto/LottoNumberFactory.java
@@ -2,20 +2,22 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoNumberFactory {
-	public List<Integer> lottoNumbers;
 	public LottoNumberFactory() {
-		this.lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-		System.out.println(lottoNumbers);
+
 	}
 
 	public List<Lotto> makeLotto(int count){
 		List<Lotto> lottoHouse = new ArrayList<>();
 		for (int i = 0; i < count; i++){
-			LottoNumberFactory factory = new LottoNumberFactory();
-			Lotto lotto = new Lotto(factory.lottoNumbers);
+			List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+			Collections.sort(lottoNumbers);
+			System.out.println(lottoNumbers);
+			Lotto lotto = new Lotto(lottoNumbers);
 			lottoHouse.add(lotto);
 		}
 		return lottoHouse;

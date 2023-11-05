dd276740cd62ad20434637291b6176ed491b2c70
노정완 <jeongwan.noh@gmail.com>
2023-11-05 16:32:15 +0900

    feat: 구입한 로또, 당첨 통계 출력 메서드 생성

--- src/main/java/lotto/domain/LotteryMessageBuilder.java
+++ src/main/java/lotto/domain/LotteryMessageBuilder.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
 public class LotteryMessageBuilder {
+
+    public String returnLottoList() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
+    public String returnWinningLottoList() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
+    public String returnLotteryProfit() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
+    public String returnFinalLotteryStats() {
+        return returnWinningLottoList() + returnLotteryProfit();
+    }
 }

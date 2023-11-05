0c80566bc267f594ca239236598d793dd2f0bbcd
gijin <rkrhdlf1546@naver.com>
2023-11-05 20:04:03 +0900

    뷰 수정 완료

--- src/main/java/lotto/LottoView/LottoView.java
+++ src/main/java/lotto/LottoView/LottoView.java
@@ -1,27 +1,46 @@
 package lotto.LottoView;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+import java.util.Map;
+
 public class LottoView {
     public int inputMoney(){
+        System.out.println();
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 
+    public void showTickets(int countLotto, List<Integer> lottos){
+        System.out.println();
+        System.out.println(countLotto);
+        for(int lotto : lottos){
+            System.out.println(lotto);
+        }
+    }
+
     public String winNumber(){
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요");
         return Console.readLine();
     }
 
     public int bonusNumber(){
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요");
         return Integer.parseInt(Console.readLine());
     }
 
-    public void showTickets(){
-        System.out.println("개를 구매했습니다.");
+    public void showResult(Map<String, Integer> result){
+        System.out.println();
+        System.out.println("당첨 통계\n---");
+        result.forEach((rank, count)->
+                System.out.println(rank + " - " + count+"개")
+        );
     }
 
-    public void showResult(){
-        System.out.println("당첨 통계\n---");
+    public void showRate(String yieldRate){
+        System.out.println("총 수익률은 " + yieldRate + "입니다.");
     }
+
 }

6ed480f9b74be3de7ffbcabb7ddc7f500941af15
waveofmymind <sjun0913@gmail.com>
2023-11-05 20:02:21 +0900

    feat: 순위를 열거형으로 구현하고, 매개변수 테스트를 이용해서 테스트한다.

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    MISS(0, 0, false);
+
+    private final int matchCount;
+    private final int winningMoney;
+    private final boolean isBonus;
+
+    Rank(int matchCount, int winningMoney, boolean isBonus) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+        this.isBonus = isBonus;
+    }
+
+    public static Rank valueOf(int matchCount, boolean isBonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount && rank.isBonus == isBonus)
+                .findFirst()
+                .orElse(MISS);
+    }
+}

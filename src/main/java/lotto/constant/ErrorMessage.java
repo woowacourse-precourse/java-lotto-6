db79b93725f0031ca14235d2caf428cc073a2f18
YuJeongYun <yunyj99@naver.com>
2023-11-06 01:55:32 +0900

    refactor: 에러 메세지 상수에서 사용하지 않는 code 필드 제거

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -4,17 +4,17 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public enum ErrorMessage {
-    BLANK_VALUE(000, "값을 입력해 주세요."),
-    NOT_NUMBER(001, "숫자를 입력해 주세요."),
-    OVER_MAXIMUM_PRICE(002, "한 번에 구입 가능한 최대 금액은 " + Constants.MAXIMUM_PURCHASE_PRICE + "원 입니다."),
-    UNDER_MINIMUM_PRICE(003, Constants.PRICE_PER_PIECE + "원 이상 입력해 주세요."),
-    NOT_INDIVISIBLE_PRICE(004, Constants.PRICE_PER_PIECE + "원 단위로 입력해 주세요."),
-    NOT_IN_NUMBER_RANGE(005, "로또 번호는 " + Constants.MIN_VALUE + "부터 " + Constants.MAX_VALUE + " 사이의 숫자여야 합니다."),
-    DUPLICATE_NUM(006, "중복된 값이 있습니다."),
-    WINNING_CNT(007, Constants.WINNING_NUMBER_CNT + "개의 값을 입력해 주세요.");
+    BLANK_VALUE("값을 입력해 주세요."),
+    NOT_NUMBER("숫자를 입력해 주세요."),
+    OVER_MAXIMUM_PRICE("한 번에 구입 가능한 최대 금액은 " + Constants.MAXIMUM_PURCHASE_PRICE + "원 입니다."),
+    UNDER_MINIMUM_PRICE(Constants.PRICE_PER_PIECE + "원 이상 입력해 주세요."),
+    NOT_INDIVISIBLE_PRICE(Constants.PRICE_PER_PIECE + "원 단위로 입력해 주세요."),
+    NOT_IN_NUMBER_RANGE("로또 번호는 " + Constants.MIN_VALUE + "부터 " + Constants.MAX_VALUE + " 사이의 숫자여야 합니다."),
+    DUPLICATE_NUM("중복된 값이 있습니다."),
+    WINNING_CNT(Constants.WINNING_NUMBER_CNT + "개의 값을 입력해 주세요.");
 
     // enum에서 상수 사용하기 위한 중첩 클래스
-    private static class Constants { // 정적 멤버 클래스
+    private static class Constants {
         static DecimalFormat df = new DecimalFormat("###,###");
 
         private static final String MAXIMUM_PURCHASE_PRICE = df.format(LottoValue
@@ -28,23 +28,10 @@ public enum ErrorMessage {
         private static final int WINNING_NUMBER_CNT = LottoValue.valueOf("WINNING_NUMBER_CNT").getValue();
     }
 
-    private int code;
     private String message;
 
-    private ErrorMessage(int code, String message) {
-        this.code = code;
+    private ErrorMessage(String message) {
         this.message = message;
-    }
-
-    public static ErrorMessage valueOfName(int errorCode) {
-        return Arrays.stream(values())
-                .filter(value -> value.code == errorCode)
-                .findAny()
-                .orElse(null);
-    }
-
-    public int getCode() {
-        return code;
     }
 
     public String getMessage() {

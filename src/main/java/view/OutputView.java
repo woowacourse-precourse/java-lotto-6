498189f87846ad8f75dfea3d110395818d7e21e5
kkw2758 <dtc3229@gmail.com>
2023-11-05 20:50:18 +0900

    Refactor(OutputView) : OutputMessage에서 관리하는 상수 OutputView 클래스에 적용

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,19 +1,21 @@
 package view;
 
+import constants.OutputMessage;
+
 public class OutputView {
     public static void printMoneyInputMessage() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(OutputMessage.MONEY_INPUT_MESSAGE.getMessage());
     }
 
     public static void printAnswerLottoNumberInputMessage() {
-        System.out.println("당첨 번호를 입력해주세요.");
+        System.out.println(OutputMessage.ANSWER_LOTTO_INPUT_MESSAGE.getMessage());
     }
 
     public static void printBonusNumberInputMessage() {
-        System.out.println("보너스 번호를 입력해주세요.");
+        System.out.println(OutputMessage.BONUS_NUMBER_INPUT_MESSAGE.getMessage());
     }
 
     public static void printLottoResultMessage() {
-        System.out.println("당첨 통계");
+        System.out.println(OutputMessage.LOTTO_RESULT_MESSAGE.getMessage());
     }
 }

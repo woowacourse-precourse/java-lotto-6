6c7bca72ef6a74401dc9cd070c1943efd4ef6967
SeongUk52 <rkfka7849@gmail.com>
2023-11-05 22:40:09 +0900

    feat(SystemMessage): 시스템 메시지 상수 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.system.SystemMessage;
+
+public class OutputView {
+    public static void inputMoney() {
+        System.out.println(SystemMessage.INPUT_MONEY.getMessage());
+    }
+}

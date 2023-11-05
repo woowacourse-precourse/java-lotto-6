bd67e15570118174ec269d870e320b0ecf87c0e2
qocksdlf123 <qocksdlf123@naver.com>
2023-11-05 17:53:16 +0900

    Fix(OutputView): TOTAL_PROFIT_RATE_MGS 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,11 +2,11 @@ package lotto.view;
 
 public class OutputView {
     private final static String INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요.\n";
-    private final static String PURCHASE_MSG = "개를 구매했습니다.\n";
+    private final static String PURCHASE_MSG = "%d개를 구매했습니다.\n";
     private final static String INPUT_LOTTO_NUMBER_MSG = "당첨 번호를 입력해 주세요.\n";
     private final static String INPUT_BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요.\n";
     private final static String WINNING_STATISTICS_MSG = "당첨 통계\n" + "---\n";
     private final static String WINNING_RESULT_MSG = "%d개 일치 (%d원) - %d개";
-    private final static String TOTAL_PROFIT_RATE_MGS = "총 수익률은 %d%입니다.";
+    private final static String TOTAL_PROFIT_RATE_MGS = "총 수익률은 %.2f%%입니다.\n";
 
 }

e37f9e93235a3debb18dbbcc3cf8edd716f609e8
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-06 03:36:36 +0900

    feat: BonusNum 입력 받아오는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,19 +1,23 @@
 package lotto.controller;
 
+import lotto.domain.BonusNum;
 import lotto.domain.Money;
 import lotto.domain.WinningNum;
+import lotto.util.MakeLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 
-    private WinningNum winningNum;
     private Money money;
+    private WinningNum winningNum;
+    private BonusNum bonusNum;
 
     public LottoController(){
         inputMoney();
+        MakeLotto makeLotto = new MakeLotto(money.getLottoTickets());
         inputWinningNum();
-        System.out.println(winningNum.getWinningNum());
+        inputBonusNum();
     }
 
     public Money inputMoney(){
@@ -21,6 +25,7 @@ public class LottoController {
         while(true){
             try{
                 money = new Money(InputView.inputMoney());
+                OutputView.printBlank();
                 return money;
             }catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
@@ -29,11 +34,25 @@ public class LottoController {
     }
 
     public WinningNum inputWinningNum(){
+        OutputView.printBlank();
         OutputView.inputWinningNumMessage();
         while(true){
             try{
                 winningNum = new WinningNum(InputView.inputWinningNum());
                 return winningNum;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public BonusNum inputBonusNum(){
+        OutputView.printBlank();
+        OutputView.inputBonusNumMessage();
+        while(true){
+            try {
+                bonusNum = new BonusNum(InputView.inputBonusNum(), winningNum.getWinningNum());
+                return bonusNum;
             }catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }

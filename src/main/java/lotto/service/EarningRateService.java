3c89c70218f2e47a697dd89360496c77642b635b
Sanghwi Lee <sanghwi47@naver.com>
2023-11-05 15:26:25 +0900

    feat(EarningRateService) 수익률을 구하는 기능을 구현

---
+++ src/main/java/lotto/service/EarningRateService.java
@@ -0,0 +1,24 @@
+package lotto.service;
+
+import lotto.model.LottoPrize;
+
+import java.util.EnumMap;
+
+public class EarningRateService {
+
+    public double calculateEarningsRate(EnumMap<LottoPrize, Integer> winCount, int numberOfTicketsPurchased) {
+        int earn = calculateEarnings(winCount);
+        int purchaseAmount = numberOfTicketsPurchased * 1000;
+        return calculateProfitRate(earn, purchaseAmount);
+    }
+
+    private int calculateEarnings(EnumMap<LottoPrize, Integer> winCount) {
+        return winCount.entrySet().stream()
+                .mapToInt(e -> e.getValue() * (int)e.getKey().getPrize())
+                .sum();
+    }
+
+    private double calculateProfitRate(int earn, int purchaseAmount) {
+        return (double) earn / purchaseAmount * 100;
+    }
+}

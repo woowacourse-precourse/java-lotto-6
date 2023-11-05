cf0a9076a91cde683f7dcbe6ba5095121a50bf4b
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-05 18:29:13 +0900

    Lotto.validate 입력 받은 로또 수 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,15 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
+//        Purchase purchase = new Purchase();
+//        purchase.process();
 
-        //로또를 생성한다.
-        Purchase purchase = new Purchase();
-        purchase.process();
+        System.out.println("당첨 번호를 입력해 주세요.");
 
+        List<Integer> numbers = List.of(Console.readLine().split(",")).stream()
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
 
-        // 로또 번호를 넘기고 6개가 맞는지 검증을 받는다.
-//        Lotto lotto = new Lotto();
+        Lotto lotto = new Lotto(numbers);
     }
 }

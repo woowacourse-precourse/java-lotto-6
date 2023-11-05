22d5f950d19d95e4273357098f3892924a65bb29
ikjo39 <chi4321@naver.com>
2023-11-06 01:23:49 +0900

    feat: 로또 번호 검증 로직 추가 및 예외 메세지 추가
    - 로또 번호 중복 시 예외 발생
    - 로또 번호 개수 이상 있을 시 예외 발생
    - 지정된 번호 내 로또번호가 아닐 시 예외 발생
    - 정적 팩토리 메서드 적용

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -6,7 +6,11 @@ public enum ErrorMessage {
     INPUT_NOT_DIGIT("입력값은 숫자여야 합니다."),
     EXCEEDED_MAXIMUM_NUMBER_FORMAT(String.format("최대로 입력할 수 있는 수를 초과합니다. (최대: %d)", Long.MAX_VALUE)),
     MONEY_LESS_THAN_ZERO("구매금액은 0 이하일 수 없습니다."),
-    MONEY_WITH_REMAINDER("구매금액은 1개의 로또 금액으로 나누어 떨어져야합니다.");
+    MONEY_WITH_REMAINDER("구매금액은 1개의 로또 금액으로 나누어 떨어져야합니다."),
+    DUPLICATED_LOTTO_NUMBERS("로또 번호들은 중복될 수 없습니다."),
+    INVALID_LOTTO_NUMBERS_AMOUNT("로또 번호 개수가 올바르지 않습니다."),
+    NOT_IN_RANGE_LOTTO_NUMBER("로또 번호는 지정된 숫자 범위 내의 있어야 합니다.")
+    ;
 
     private final String message;
 
---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,53 @@
+package lotto.model;
+
+import static lotto.constant.ErrorMessage.DUPLICATED_LOTTO_NUMBERS;
+import static lotto.constant.ErrorMessage.INVALID_LOTTO_NUMBERS_AMOUNT;
+import static lotto.constant.ErrorMessage.NOT_IN_RANGE_LOTTO_NUMBER;
+
+import java.util.List;
+
+public class Lotto {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int AMOUNT_OF_NUMBERS = 6;
+
+    private final List<Integer> numbers;
+
+    private Lotto(List<Integer> numbers) {
+        validateNumbersDuplicated(numbers);
+        validateNumbersAmount(numbers);
+        validateIsNumberInCorrectRange(numbers);
+        this.numbers = numbers;
+    }
+
+    public static Lotto from(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validateNumbersDuplicated(List<Integer> numbers) {
+        long deduplicatedCount = numbers.stream()
+                .distinct()
+                .count();
+        if (deduplicatedCount != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBERS.toString());
+        }
+    }
+
+    private void validateNumbersAmount(List<Integer> numbers) {
+        if (numbers.size() != AMOUNT_OF_NUMBERS) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_AMOUNT.toString());
+        }
+    }
+
+    private void validateIsNumberInCorrectRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (isNumberInCorrectRange(number)) {
+                throw new IllegalArgumentException(NOT_IN_RANGE_LOTTO_NUMBER.toString());
+            }
+        }
+    }
+
+    private boolean isNumberInCorrectRange(int lottoNumber) {
+        return lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER;
+    }
+}

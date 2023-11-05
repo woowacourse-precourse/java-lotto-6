a483904162d33ee4a347a470f1ddf1fa297648ab
DaeYoungK <hanguk830@naver.com>
2023-11-05 13:08:05 +0900

    feat(Statistics): 보너스 번호를 받아 해당하는 enum 반환하는 기능 추가

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum Statistics {
 
@@ -11,6 +12,7 @@ public enum Statistics {
     SIX(6, 2000000000),
     NON(0,0);
 
+    private static final int STANDARD_BONUS_COUNT = 4;
     private int count;
     private int amount;
 
@@ -27,8 +29,15 @@ public enum Statistics {
         return amount;
     }
 
-    public static Statistics getStatistics(int matchCount) {
+    public static Statistics getStatistics(int matchCount,int bonusNumber, List<Integer> winningLotto) {
+        if (isBonusNumber(matchCount, bonusNumber, winningLotto)) {
+            return FIVE_BONUS;
+        }
         return Arrays.stream(Statistics.values())
                 .filter(data -> data.count == matchCount).findAny().orElse(NON);
+    }
+
+    private static boolean isBonusNumber(int matchCount, int bonusNumber, List<Integer> winningLotto) {
+        return matchCount == STANDARD_BONUS_COUNT && winningLotto.contains(bonusNumber);
     }
 }

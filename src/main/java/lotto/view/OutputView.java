44324310e6fa8f622f080b3f2e6b045da0a6bf2f
goragoraki <gorakio0126@gmail.com>
2023-11-05 16:48:17 +0900

    feat(OutputView): 당첨 내역 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,8 @@
 package lotto.view;
 
-import lotto.domain.Lotto;
-import lotto.domain.Lottos;
-import lotto.domain.Money;
+import lotto.domain.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class OutputView {
@@ -14,5 +13,29 @@ public class OutputView {
     public static void printLottos(Lottos lottos) {
         lottos.getLottos().forEach(System.out::println);
         System.out.println();
+    }
+
+    public static void printRankReuslt(RankResult rankResult) {
+        System.out.println("당첨 통계\n---");
+        Arrays.stream(Rank.values())
+                .filter(rank -> !rank.equals(Rank.EMPTY))
+                .forEach(rank -> System.out.println(getPrintResult(rank, rankResult)));
+    }
+
+    private static String getPrintResult(Rank rank, RankResult rankResult) {
+        if(rank == Rank.SECOND) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개"
+                    , rank.getMatchLottoCnt()
+                    , String.format("%,d", rank.getMoney())
+                    , rankResult.getRankResultCnt(rank));
+        }
+        return String.format("%d개 일치 (%s원) - %d개"
+                , rank.getMatchLottoCnt()
+                , String.format("%,d", rank.getMoney())
+                , rankResult.getRankResultCnt(rank));
+    }
+
+    public static void printBenefitRate(BenefitRate benefitRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", benefitRate.getBenefit());
     }
 }

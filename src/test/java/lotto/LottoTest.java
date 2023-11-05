d34ba4e1e4c66aec222c95b6170093f3f41a51e4
박찬건 <jg9870@naver.com>
2023-11-05 17:21:56 +0900

    refactor(lotto): 에러 문구 변경

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -13,7 +13,7 @@ class LottoTest {
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 6개의 숫자를 입력해주세요.");
+                .hasMessage("[ERROR] 6개의 숫자를 입력해 주세요.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -29,7 +29,7 @@ class LottoTest {
     void createLottoByOutOfRangeNumberLessThan1() {
         assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1 이상 45 이하의 숫자를 입력해주세요.");
+                .hasMessage("[ERROR] 1 이상 45 이하의 숫자를 입력해 주세요.");
     }
 
     @DisplayName("로또 번호가 1보다 작거나 45보다 크면 예외가 발생한다. 45보다 큰 경우")
@@ -37,6 +37,6 @@ class LottoTest {
     void createLottoByOutOfRangeNumberGreaterThan45() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1 이상 45 이하의 숫자를 입력해주세요.");
+                .hasMessage("[ERROR] 1 이상 45 이하의 숫자를 입력해 주세요.");
     }
 }

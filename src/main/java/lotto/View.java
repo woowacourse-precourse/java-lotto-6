5c26aae2b4d6b344abf9b0f929371d37f71181cd
noah <jeongas9810@gmail.com>
2023-11-06 06:21:43 +0900

    style: Missing dot

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class View {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.\n";
     private static final String ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.\n";
-    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요\n";
+    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n";
     private static final String PRINT_PURCHASE_AMOUNT = "%d개를 구매했습니다.\n";
     private static final String PRINT_PURCHASE_NUMBER = "[%s]\n";
     private static final String PRINT_WINNING_STAT = "당첨 통계\n---\n";

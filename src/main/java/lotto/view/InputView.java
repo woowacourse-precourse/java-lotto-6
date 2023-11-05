164a8bfa5131a0fb2f273310712ee81281e06506
HoeSeong123 <ghltjd3@gmail.com>
2023-11-05 20:32:35 +0900

    feat(InputView) : 구입금액 입력에 대한 예외 처리(공백, 숫자가 아닌 경우)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,14 @@ public class InputView {
 
     public static String readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Console.readLine().trim();
+        String purchaseAmount = Console.readLine().trim();
+        validateIsNumeric(purchaseAmount);
+        return purchaseAmount;
+    }
+
+    private static void validateIsNumeric(String input) {
+        if (!input.chars().allMatch(Character::isDigit) || input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
     }
 }

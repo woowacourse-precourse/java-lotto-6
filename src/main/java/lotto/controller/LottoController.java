fcb23e0d54cdeee0bebbd0622cc2852dbe2bf6df
sonjiseokk <thswltjr11@gmail.com>
2023-11-05 13:28:09 +0900

    test: 로또 밸리데이터 테스트 코드

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -24,12 +24,16 @@ public class LottoController {
         OutputView.purchaseResult(member);
 
         List<Integer> winningNumberInput = InputView.getWinningNumberInput();
-        Lotto winnerLotto = new Lotto(winningNumberInput);
-        int bonusNumber = InputView.getBonusNumberInput(winningNumberInput);
-        WinningNumber winningNumber = new WinningNumber(winnerLotto, bonusNumber);
+        WinningNumber winningNumber = generateWinningNumber(winningNumberInput);
 
         List<Rank> awardsResult = lottoService.awards(member, winningNumber);
         OutputView.gameResult(awardsResult,member);
+    }
+
+    private static WinningNumber generateWinningNumber(final List<Integer> winningNumberInput) {
+        Lotto winnerLotto = new Lotto(winningNumberInput);
+        int bonusNumber = InputView.getBonusNumberInput(winningNumberInput);
+        return new WinningNumber(winnerLotto, bonusNumber);
     }
 
     private static Member generatedMember(final int money, final List<Lotto> purchasedLottos) {

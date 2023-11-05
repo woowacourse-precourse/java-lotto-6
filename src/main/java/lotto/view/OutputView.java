1ce7133212518502b755e5c5f795d35cafb2b51b
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-06 02:53:46 +0900

    feat: outputView 출력 내용 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,9 +2,24 @@ package lotto.view;
 
 public class OutputView {
 
-    private static final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_BUY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUM_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String LOTTO_RESULT_MESSAGE = "당첨 통계";
 
-    public static void inputMessage(){
-        System.out.println(INPUT_MESSAGE);
+    public static void inputBuyMessage(){
+        System.out.println(INPUT_BUY_MESSAGE);
+    }
+
+    public static void inputWinningNumMessage(){
+        System.out.println(INPUT_WINNING_NUM_MESSAGE);
+    }
+
+    public static void printBlank(){
+        System.out.println();
+    }
+
+    public static void ticketCountMessage(int ticketCount){
+        System.out.println(ticketCount+TICKET_COUNT_MESSAGE);
     }
 }

b443f1413b6b6fdcd7b829dc60b6b2ad5d2225a8
Lee ChanHui <ing3616062@naver.com>
2023-11-05 15:00:35 +0900

    Feat: 로또 생성 구현 Game 클래스에 createLotto 메소드로 로또를 여러개 생성해 필드에 생성된 lottos 리스트에 담는다.

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,17 +1,23 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
     private int AMOUNT;
     private InputView inputView;
     private OutputView outputView;
+    private final List<Lotto> lottos;
 
     public Game() {
         inputView = new InputView();
         outputView = new OutputView();
+        lottos = new ArrayList<>();
     }
 
     public void run(){
@@ -19,13 +25,20 @@ public class Game {
         AMOUNT = purchaseAmountInput();
 
         outputView.purchasesNumberView(AMOUNT);
-        
+        createLotto(AMOUNT);    //로또 생성
     }
 
     public int purchaseAmountInput(){
         //예외 처리 추가해야 함
 
         return Integer.parseInt(Console.readLine());
+    }
+
+    public void createLotto(int AMOUNT){
+        for(int i=0; i<AMOUNT; i++){
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
     }
 
 }

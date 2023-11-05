ce791aef982147a044a9d545fdfa05a57d45f28f
Minjae-An <mj3242@naver.com>
2023-11-05 17:45:06 +0900

    feat : 게임 시작 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.Game;
+import lotto.domain.lotto.LottoManager;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game(new InputView(), new OutputView(), new LottoManager());
+        game.start();
     }
 }

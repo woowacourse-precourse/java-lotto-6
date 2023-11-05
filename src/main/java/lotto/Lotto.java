c3ec2a90400bbb770d378562ba9b6673f2b665d5
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-05 14:20:37 +0900

    feat(Lotto): getter함수 구현 (생성된 로또 번호 가져오는 기능)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,11 +2,13 @@ package lotto;
 
 import java.util.List;
 
+//구매한 로또 개수만큼 객체를 새로 생성해준다! - 붕어빵 느낌
 public class Lotto {
     private final List<Integer> numbers;
     //접근 제어자 변경 불가능
     //필드(인스턴스 변수) 추가 불가능
 
+    //생성자로서, 랜덤으로 뽑힌 번호 6개 ArrayList를 인자로 받아들복
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -14,9 +16,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[]");
+            throw new IllegalArgumentException("[ERROR] ");
         }
     }
 
     // TODO: 추가 기능 구현
+
+    //number가 private으로 접근이 제어되었기 때문에, getter 메소드 구현
+    private List<Integer> getLottoNumbers() {
+        return numbers;
+    }
+
+
+
 }
+

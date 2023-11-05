c9c38c53642602b8f56be1fd63e8778634aff8b6
yshong1998 <yshong1998@gmail.com>
2023-11-06 03:16:57 +0900

    Feat(OutputView): 통계를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,25 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
-import lotto.message.ResponseMessage;
+import lotto.domain.WinningStatistics;
 
 import java.util.List;
 
+import static lotto.message.ResponseMessage.*;
+
 public class OutputView {
+
     public void responseLottos(List<Lotto> lottos) {
-        String boughtMessage = String.format(ResponseMessage.BOUGHT_MESSAGE, lottos.size());
+        String boughtMessage = String.format(BOUGHT_MESSAGE, lottos.size());
         System.out.println(boughtMessage);
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
+    }
+
+    public void responseWinningStatistics(WinningStatistics winningStatistics, Integer purchaseAmount){
+        System.out.println(STATISTICS_MESSAGE + DIVISION_LINE);
+        System.out.println(winningStatistics.toString()
+                + winningStatistics.calculateRateOfReturn(purchaseAmount));
     }
 }

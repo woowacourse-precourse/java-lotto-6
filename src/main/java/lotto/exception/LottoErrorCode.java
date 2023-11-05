ac052a47d7d7e8e5f114f01c95047d182ccf067c
jihyuk <kwlgur0416@gmail.com>
2023-11-06 03:31:46 +0900

    feat : 로또 티켓 발행 기능 구현
    - Lotto를 상송한 LottoTicket 활용
    - LottoTicketFactory로 티켓들을 발행
    - 티켓 발행 기능 예외 처리 작성

--- src/main/java/lotto/exception/LottoErrorCode.java
+++ src/main/java/lotto/exception/LottoErrorCode.java
@@ -8,14 +8,14 @@ import static lotto.Constants.TICKET_PRICE;
 public enum LottoErrorCode {
     NOT_INTEGER_INPUT("숫자만 입력이 가능합니다."),
     BLANK_INPUT("빈 값은 입력이 불가능 합니다."),
-    NOT_POSITIVE_INTEGER_INPUT("양수만 입력이 가능합니다."),
+
+    PURCHASE_AMOUNT_NOT_PRICE_UNIT(String.format("티켓 가격 %d원 단위로 구매가 가능합니다.", TICKET_PRICE)),
+    PURCHASE_AMOUNT_NOT_POSITIVE("양수만 입력이 가능합니다."),
 
     LOTTO_NUMBER_DUPLICATE("로또 숫자들은 중복이 불가능합니다."),
     LOTTO_NUMBER_RANGE(
         String.format("로또 숫자들은 %d와 %d 숫자로 구성되어야 합니다.", MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)),
-    LOTTO_NUMBER_SIZE(String.format("로또는 %d개의 숫자로 구성되어야 합니다.", LOTTO_NUMBER_COUNT)),
-
-    TICKET_PRICE_UNIT(String.format("티켓 가격 %d원 단위로 구매가 가능합니다.", TICKET_PRICE));
+    LOTTO_NUMBER_SIZE(String.format("로또는 %d개의 숫자로 구성되어야 합니다.", LOTTO_NUMBER_COUNT));
 
     private final String message;
 

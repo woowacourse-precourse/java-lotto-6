11e341d2fa1add1ea6c3eb777c5310ca2e9c4db4
metishonora <metishonora@icloud.com>
2023-11-05 21:26:36 +0900

    feat: Lotto 클래스 생성

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public Long countSameNumbers(Lotto another) {
+        return numbers.stream()
+                .filter(another.numbers::contains)
+                .count();
+    }
+
+    public Boolean containsNumber(Integer number) {
+        return numbers.contains(number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        return this.numbers == ((Lotto) o).numbers;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+}

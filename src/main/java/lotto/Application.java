b97c7e862b0528c2d5f711e75dedbcd1486b1a0a
juhwan5 <lavistrike770@gmail.com>
2023-11-06 02:58:38 +0900

    Feat Main 1
    로또 구입 금액 입력받는 기능 구현
    들여쓰기 제한, 메소드 길이 제한, else문 제한을 지키며 구현함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int lotto_purchase_amount = inputLottoPurchase();
+        
+    }
+
+    static int inputLottoPurchase(){
+        int lotto_purchase_amount;
+
+        System.out.println("구입금액을 입력해 주세요");
+        while(true){
+            try {
+                lotto_purchase_amount = stringToInt(Console.readLine());
+                checkMultiple1000(lotto_purchase_amount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return lotto_purchase_amount;
+    }
+
+    static int stringToInt(String value) throws IllegalArgumentException{
+        int num;
+        try {
+            num = Integer.parseInt(value);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 값을 입력해야 합니다.");
+        }
+        return num;
+    }
+
+    static void checkMultiple1000(int num) throws IllegalArgumentException{
+        if (num % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 1000의 배수를 입력해야 합니다.");
+        }
     }
 }

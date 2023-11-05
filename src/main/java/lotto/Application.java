942f8231e7b16df4d35b4b513ea875a751905653
강승민 <rkdtmdals999@naver.com>
2023-11-05 19:09:35 +0900

    [feat] LottoManager class와 Application class 연동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoManager;
+
 public class Application {
+    public static LottoManager lottoManager;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        lottoManager = new LottoManager();
+        lottoManager.start();
     }
 }
--- src/main/java/lotto/controller/LottoManager.java
+++ src/main/java/lotto/controller/LottoManager.java
@@ -1,4 +1,15 @@
 package lotto.controller;
 
+import lotto.view.UserView;
+
 public class LottoManager {
+    private User user;
+    private UserView userView;
+    public LottoManager() {
+        user = new User();
+        userView = new UserView();
+    }
+    public void start() {
+        userView.printBuyMessageBefore();
+    }
 }

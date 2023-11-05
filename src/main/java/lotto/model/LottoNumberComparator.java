16de37b2fc0107ce6e4cfd64c30b561f571b416a
ChaeAg <dydkglel123@gmail.com>
2023-11-06 02:43:23 +0900

    refactor(LottoNumberComparator): 테스트를 위한 equals 메서드 오버라이딩

--- src/main/java/lotto/model/LottoNumberComparator.java
+++ src/main/java/lotto/model/LottoNumberComparator.java
@@ -32,4 +32,12 @@ public class LottoNumberComparator {
                 .filter(winningLotto.getWinningLotto().getNumbers()::contains)
                 .count();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumberComparator lottoNumberComparator = (LottoNumberComparator) o;
+        return winningLotto == lottoNumberComparator.winningLotto;
+    }
 }

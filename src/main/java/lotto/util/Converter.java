d6ba807555f659d6d3440e5070991ccb32375efc
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-06 00:57:46 +0900

    feat: add converting string to integer list method
    string에서 integer list 변환 기능 추가

---
+++ src/main/java/lotto/util/Converter.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+// 여기서 리팩터링 진행할 것
+public class Converter {
+    public static List<Integer> toIntegerList(String input) {
+        return Arrays.stream(input.trim().split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed().toList();
+    }
+}
+

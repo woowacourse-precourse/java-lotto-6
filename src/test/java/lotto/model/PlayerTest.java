d84eefe061fe7075775f0ccd7115e20273534bfd
heiler <threepebbles@naver.com>
2023-11-05 22:01:27 +0900

    Test: Player 테스트 코드 추가

--- src/test/java/lotto/model/PlayerTest.java
+++ src/test/java/lotto/model/PlayerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
-    private static WinningStatistics getWinningStatistics() {
+    private static WinningStatistics getFixedWinningStatistics() {
         // 고정된 번호의 로또 생성기
         LottoGenerator lottoGenerator = new LottoNotRandomGenerator();
         LottoMachine lottoMachine = new LottoMachine(lottoGenerator);
@@ -30,11 +30,10 @@ public class PlayerTest {
         return player.getWinningStatistics();
     }
 
-    @DisplayName("player가 계산한 당첨 통계가 예상과 틀리면 테스트에 실패한다.")
+    @DisplayName("player가 계산한 당첨 통계(수익률 제외)가 예상과 틀리면 테스트에 실패한다.")
     @Test
-    void getWinningStatistics_test() {
+    void prizeCounter_test() {
         // given
-        WinningStatistics winningStatistics = getWinningStatistics();
         Map<LottoPrize, Integer> expected = new HashMap<>() {{
             put(LottoPrize.FIRST, 1);   // 1등 1개
             put(LottoPrize.SECOND, 0);   // 2등 0개
@@ -45,10 +44,38 @@ public class PlayerTest {
         }};
 
         // when
-        Map<LottoPrize, Integer> prizeCounter = winningStatistics.getPrizeCounter();
+        WinningStatistics winningStatistics = getFixedWinningStatistics();
+        Map<LottoPrize, Integer> actual = winningStatistics.getPrizeCounter();
 
         // then
-        assertThat(prizeCounter).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("player가 계산한 수익률 예상과 틀리면 테스트에 실패한다.")
+    @Test
+    void calculateRateOfReturn_test() {
+        // given
+        Map<LottoPrize, Integer> expectedPrizeCounter = new HashMap<>() {{
+            put(LottoPrize.FIRST, 1);   // 1등 1개
+            put(LottoPrize.SECOND, 0);   // 2등 0개
+            put(LottoPrize.THIRD, 1);   // 3등 1개
+            put(LottoPrize.FOURTH, 1);   // 4등 1개
+            put(LottoPrize.FIFTH, 3);   // 5등 3개
+            put(LottoPrize.NOTHING, 2);   // 꽝 2개
+        }};
+        double expected = 0;
+        for (LottoPrize lottoPrize : expectedPrizeCounter.keySet()) {
+            Long count = Long.valueOf(expectedPrizeCounter.get(lottoPrize));
+            Long prize = lottoPrize.getPrize();
+            expected += prize * count;
+        }
+
+        // when
+        WinningStatistics winningStatistics = getFixedWinningStatistics();
+        double actual = winningStatistics.getRateOfReturn();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
 

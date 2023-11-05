fb33f2bd1c55a2b9d39ef6ddd24f28d35b7bfab2
nahyeon99 <nahyeonee99@gmail.com>
2023-11-05 16:09:06 +0900

    refactor(domain/WinningLotto): Lotto -> WinningLotto 상속 관계로 변경
    1. Lotto(조상 계층) -> WinningLotto(자손 계층) 관계
    2. Lotto : player 로또 번호를 관리한다. 랜덤 번호로, 검증없이 생성한다.
    2. WinningLotto:
    - 당첨 번호를 관리한다. 사용자 입력을 받기 떄문에, 검증을 추가적으로 실시한다.
    - getPrize(): player 로또 번호와 당첨 로또 번호를 비교하여, 등수를 계산한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,54 +1,15 @@
 package lotto.domain;
 
 import java.util.List;
-import lotto.view.InputView;
 
 public class Lotto {
-    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개를 입력해야 합니다.";
-    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복 없이 입력해야 합니다.";
-
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final int LOTTO_COUNT = 6;
-
     private final List<Integer> numbers;
 
-    private Lotto(List<Integer> numbers) {
-        numbers.forEach(number -> InputView.validateBlank(String.valueOf(number)));
-        numbers.forEach(InputView::validateNegativeInteger);
-        validateSize(numbers.size());
-        validateRange(numbers);
-        validateDuplicate(numbers);
-
+    public Lotto(List<Integer> numbers) {
         this.numbers = numbers.stream().toList();
-    }
-
-    public static Lotto of(List<Integer> numbers) {
-        return new Lotto(numbers);
     }
 
     public List<Integer> getNumbers() {
         return numbers;
-    }
-
-    private void validateSize(int size) {
-        if (size != LOTTO_COUNT) {
-            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
-        }
-    }
-
-    private void validateRange(List<Integer> numbers) {
-        numbers.forEach(number -> {
-            if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
-            }
-        });
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
-        if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
-        }
     }
 }
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,13 +1,70 @@
 package lotto.domain;
 
-public class WinningLotto {
-    private final Lotto lotto;
+import java.util.List;
+import lotto.view.InputView;
 
-    private WinningLotto(Lotto lotto) {
-        this.lotto = lotto;
+public class WinningLotto extends Lotto {
+    private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개를 입력해야 합니다.";
+    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복 없이 입력해야 합니다.";
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+
+    private BonusNumber bonusNumber;
+
+    public WinningLotto(List<Integer> numbers) {
+        super(numbers);
+        numbers.forEach(number -> InputView.validateBlank(String.valueOf(number)));
+        numbers.forEach(InputView::validateNegativeInteger);
+        validateSize(numbers.size());
+        validateRange(numbers);
+        validateDuplicate(numbers);
     }
 
-    public static WinningLotto of(Lotto lotto) {
-        return new WinningLotto(lotto);
+    public void setBonusNumber(BonusNumber bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    @Override
+    public List<Integer> getNumbers() {
+        return super.getNumbers();
+    }
+
+    public Prize getPrize(Lotto playerLotto) {
+        int matchingCount = countMatchingNumbers(playerLotto);
+        Prize prize = Prize.matchingCountOf(matchingCount);
+
+        if (prize.equals(Prize.THIRD) && playerLotto.getNumbers().contains(bonusNumber.getNumber())) {
+            prize = Prize.SECOND;
+        }
+        return prize;
+    }
+
+    private int countMatchingNumbers(Lotto playerLotto) {
+        return (int) super.getNumbers().stream()
+                .filter(playerLotto.getNumbers()::contains)
+                .count();
+    }
+
+    private void validateSize(int size) {
+        if (size != LOTTO_COUNT) {
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
+            }
+        });
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
     }
 }

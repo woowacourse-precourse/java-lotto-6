4a6ba2606ef39ffc093f57b9b102629e6e489c74
Woongbae Yun <devbelly@naver.com>
2023-11-05 22:16:58 +0900

    feat : 로또 등수를 나타내는 Enum class 추가

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(-1, 0);
+
+    private final int matchCount;
+    private final int winningMoney;
+
+    Rank(int matchCount, int winningMoney) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+    }
+
+    public static Rank valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == 5 && matchBonus) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        return MISS;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+}

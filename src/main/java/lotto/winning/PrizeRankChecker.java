20d065c620b6ef4379d4c956102db459ab44298e
suntisfied <147501927+suntisfied@users.noreply.github.com>
2023-11-05 17:58:43 +0900

    feat(winning): add matching number calculation between entire purchased lottos and winning numbers

--- src/main/java/lotto/winning/PrizeRankChecker.java
+++ src/main/java/lotto/winning/PrizeRankChecker.java
@@ -1,8 +1,21 @@
 package lotto.winning;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.Lotto;
 
 public class PrizeRankChecker {
+
+    public List<Integer> computeMatchedNumberCounts(List<Lotto> lottos, List<Integer> Numbers) {
+        List<Integer> matchedNumberCounts = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            matchedNumberCounts.add(calculateNumberMatch(lotto.getNumbers(), Numbers));
+        }
+
+        return matchedNumberCounts;
+    }
+
     private int calculateNumberMatch(List<Integer> lotto, List<Integer> numbers) {
         return (int) lotto.stream()
                 .filter(numbers::contains)

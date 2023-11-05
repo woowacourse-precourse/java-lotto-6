b6d6e096f546dba13b3d83de081406b7fb64ea0b
jeondui <jeondui@gmail.com>
2023-11-06 03:05:38 +0900

    test(LottoCountTest): 에러메시지, 테스트명 수정 및 구매 금액의 유효성 검증 테스트 추가

--- src/test/java/lotto/model/LottoCountTest.java
+++ src/test/java/lotto/model/LottoCountTest.java
@@ -3,7 +3,7 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.exception.ErrorMessage;
+import lotto.common.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +12,7 @@ class LottoCountTest {
     @DisplayName("구매 금액에 맞는 로또 수량을 발행하는지 테스트")
     @Test
     void createLottoCount() {
-        int amount = 10000;
+        String amount = "10000";
         LottoCount lottoCount = LottoCount.createLottoCount(amount);
         assertThat(lottoCount.getLottoCount()).isEqualTo(10);
     }
@@ -20,19 +20,37 @@ class LottoCountTest {
     @DisplayName("구매 금액이 올바르지 않은 경우 수량 예외 발생 테스트")
     @Test
     void isDivisible() {
-        int amount = 10001;
+        String amount = "10001";
         assertThatThrownBy(() -> LottoCount.createLottoCount(amount))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_ONE_TICKET_PRICE.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_AMOUNT_IS_NOT_DIVISIBLE.get());
     }
 
     @DisplayName("최소 구매 금액보다 적은 경우 수량 예외 발생 테스트")
     @Test
     void isMinAmount() {
-        int amount = 500;
+        String amount = "500";
         assertThatThrownBy(() -> LottoCount.createLottoCount(amount))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.ERROR_LOTTO_MIN_TICKET_PRICE.get());
+                .hasMessageContaining(ErrorMessage.LOTTO_COUNT_MIN_TICKET_PRICE.get());
+    }
+
+    @DisplayName("구매 금액이 널값인 경우 예외 발생 테스트")
+    @Test
+    void isNull() {
+        String amount = "";
+        assertThatThrownBy(() -> LottoCount.createLottoCount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.LOTTO_AMOUNT_NULL.get());
+    }
+
+    @DisplayName("구매 금액에 공백이 포함된 경우 예외 발생 테스트")
+    @Test
+    void isContainsBlank() {
+        String amount = "10000 ";
+        assertThatThrownBy(() -> LottoCount.createLottoCount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.LOTTO_AMOUNT_CONTAINS_BLANK.get());
     }
 
 }

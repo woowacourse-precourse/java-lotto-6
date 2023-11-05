423aaad8f8cc95a15363739691719046d38c1390
rakyun1 <dbskim234@naver.com>
2023-11-05 03:54:20 -0700

    Test: test code for LottoNumberFactory.class add checkMakeLottoFunctionLottoSize() 생성된 로또의 개수가 6개가 맞는지 확인

--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -13,7 +13,7 @@ public class LottoFactoryTest {
 		List<Lotto> testLotto = LottoNumberFactory.makeLotto(10);
 		assertEquals(testLotto.size(), 10);
 	}
-	
+
 	@Test
 	void checkMakeLottoFunctionDuplicate() {
 		List<Lotto> testLotto = LottoNumberFactory.makeLotto(10);
@@ -22,6 +22,15 @@ public class LottoFactoryTest {
 			List<Integer> numbers = lotto.getNumbers();
 			Set<Integer> numberSet = new HashSet<>(numbers);
 			assertEquals(numbers.size(), numberSet.size());
+		}
+	}
+
+	@Test
+	void checkMakeLottoFunctionLottoSize(){
+		List<Lotto> testLotto = LottoNumberFactory.makeLotto(10);
+		for (Lotto lotto : testLotto){
+			List<Integer> numbers = lotto.getNumbers();
+			assertEquals(numbers.size(), 6);
 		}
 	}
 }

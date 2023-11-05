4a72fe07e9a6b9a0090945b6de2b28ca0dc0b900
doka <posmane5@gmail.com>
2023-11-05 15:34:24 +0900

    feat(LottoStoreClerk) : domain) 로또 발행 기능 추가 - createLottoTickets() : 입력 받은 금액값에 맞게 로또를 발행시키는 기능을 총괄적으로 처리 하는 메소드
    - checkLottoOrderAmount() : 금액이 올바른값 인지 확인 기능 메소드 아닐시 IllegalArgumentException 예외 생성
    
    - handlingOrderAmount() : 입력 받은 주문 금액값 처리 메소드
    
    - lottoGenerater() : 주문 갯수에 맞게 로또 리스트를 반환 하는 메소드

---
+++ src/main/java/lotto/domain/LottoStoreClerk.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import static lotto.domain.Util.toNumeric;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoStoreClerk {
+
+    public static final int MINIMUM_AMOUNT_IN_UNITS = 1000;
+    public static final int NO_REMAINDER = 0;
+    public static final int GENERATE_START_NUMBER = 0;
+    public static final int LOTTO_MINIMUM_NUMBER = 1;
+    public static final int LOTTO_MAXIMUM_NUMBER = 45;
+    public static final int COUNT_OF_LOTTO_NUMBER = 6;
+
+
+    public static List<Lotto> createLottoTickets(String orderInputAmount) {
+        int lottoTicketsNumber = handlingOrderAmount(orderInputAmount);
+        return lottoGenerater(lottoTicketsNumber);
+    }
+
+    public static int handlingOrderAmount(String orderInputAmount) {
+        return calculateNumberOfTickets(toNumeric(orderInputAmount));
+    }
+
+    public static int calculateNumberOfTickets(int lottoOrderAmount) {
+        checkLottoOrderAmount(lottoOrderAmount);
+        int numberOfLottoTickets = lottoOrderAmount / MINIMUM_AMOUNT_IN_UNITS;
+        return numberOfLottoTickets;
+
+    }
+
+    public static void checkLottoOrderAmount(int lottoOrderAmount) {
+        if (lottoOrderAmount < MINIMUM_AMOUNT_IN_UNITS || lottoOrderAmount % MINIMUM_AMOUNT_IN_UNITS
+                != NO_REMAINDER) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public static List<Lotto> lottoGenerater(int lottoTicketNumber) {
+        List<Lotto> generatedLottoTickets = new ArrayList<>();
+        for (int i = GENERATE_START_NUMBER; i < lottoTicketNumber; i++) {
+            generatedLottoTickets.add(
+                    new Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_MINIMUM_NUMBER, LOTTO_MAXIMUM_NUMBER,
+                            COUNT_OF_LOTTO_NUMBER)));
+        }
+        return generatedLottoTickets;
+    }
+
+}

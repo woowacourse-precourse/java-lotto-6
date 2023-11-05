f90cd1bd1231f65569fc04bd29436f6eee0e4ade
yubin <dbqls2821@gmail.com>
2023-11-05 21:13:46 +0900

    feat: OutputPort 인터페이스 추가

--- src/main/java/lotto/io/OutputPort.java
+++ src/main/java/lotto/io/OutputPort.java
@@ -1,5 +1,13 @@
 package lotto.io;
 
+import java.util.List;
+
 public interface OutputPort {
-    
+    void printErrorMessage(String message);
+    void printInputPurchaseAmount();
+    void printNumberOfLottos(Integer numberOfLottos);
+    void printCreatedLottos(List<List<Integer>> lottos);
+    void printInputWinningNumbers();
+    void printInputBonusNumber();
+    void printStatistics(double statistics);
 }

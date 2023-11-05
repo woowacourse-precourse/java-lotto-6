6d5b05b898eaa1024e834243053e038de32baf42
Haebin <joker7011@naver.com>
2023-11-05 14:23:01 +0900

    refactor: `Jackpot` -> `Lotto` 클래스 상속 구조 변경

--- src/main/java/lotto/domain/lottery/Jackpot.java
+++ src/main/java/lotto/domain/lottery/Jackpot.java
@@ -1,34 +1,26 @@
 package lotto.domain.lottery;
 
-import lotto.exception.ErrorMessage;
-import lotto.exception.LottoException;
-
 import java.util.List;
 
-public class Jackpot {
-    private static final String DELIMITER = ",";
+public final class Jackpot extends Lotto {
+    private final int bonusNumber;
 
-    private final List<JackpotNumber> numbers;
-
-    private Jackpot(final List<JackpotNumber> jackpotNumbers) {
-        this.numbers = jackpotNumbers;
+    private Jackpot(
+            final List<Integer> numbers,
+            final int bonusNumber
+    ) {
+        super(numbers);
+        this.bonusNumber = bonusNumber;
     }
 
-    public static Jackpot from(final List<JackpotNumber> jackpotNumbers) {
-        return new Jackpot(jackpotNumbers);
+    public Jackpot of(
+            final List<Integer> numbers,
+            final int bonusNumber
+    ) {
+        return new Jackpot(numbers, bonusNumber);
     }
 
-    public static void validateEndsWithComma(final String input) {
-        if (isEndsWithDelimiter(input)) {
-            throw LottoException.from(ErrorMessage.ENDS_WITH_DELIMITER);
-        }
-    }
-
-    private static boolean isEndsWithDelimiter(String input) {
-        return input.endsWith(DELIMITER);
-    }
-
-    public List<JackpotNumber> getNumbers() {
-        return numbers;
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 }

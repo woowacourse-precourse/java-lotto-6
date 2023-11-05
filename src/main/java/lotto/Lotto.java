39d1c9a0dfb4755f2c6d62851af1c5b801f11599
ks0927 <brian_als@naver.com>
2023-11-05 15:37:42 +0900

    feat(Lotto): 로또 번호 중복 검증메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,21 +1,34 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private static final String INVALID_SIZE_ERROR_MESSAGE = "[ERROR] 로또 숫자는 6개여야 합니다.";
+    private static final String INVALID_DISTINCT_ERROR_MESSAGE = "[ERROR] 로또 숫자는 중복되면 안됩니다.";
     public static final int LOTTO_SIZE = 6;
 
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = convertLottoNumbers(numbers);
+    }
+
+    private List<LottoNumber> convertLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(n -> new LottoNumber(n.toString()))
+                .collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
         if (isInvalidSize(numbers)) {
             throw new IllegalArgumentException(INVALID_SIZE_ERROR_MESSAGE);
+        }
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException(INVALID_DISTINCT_ERROR_MESSAGE);
         }
     }
 
@@ -23,4 +36,8 @@ public class Lotto {
         return numbers.size() != LOTTO_SIZE;
     }
 
+    private boolean isDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        return set.size() != numbers.size();
+    }
 }

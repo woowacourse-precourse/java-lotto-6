f8f695c6a264cfa1161c8f59b5b4766bb7714ab9
dbstn1325 <dbstn1325@naver.com>
2023-11-05 19:16:08 +0900

    feat(LottoMatcher): 로또 번호 하나와 보너스 번호를 포함한 당첨 번호 간 일치하는 번호의 갯수를 구하는 기능 구현

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -3,9 +3,15 @@ package lotto.domain;
 public class LottoMatcher {
 
     private long correctWinningCount;
+    private boolean correctBonus;
 
     public LottoMatcher(Lotto userNumbers, Lotto winningNumber) {
         this.correctWinningCount = calculateCorrectWinning(userNumbers, winningNumber);
+    }
+
+    public LottoMatcher(Lotto userNumbers, Lotto winningNumber, int bonusNumber) {
+        this.correctWinningCount = calculateCorrectWinning(userNumbers, winningNumber);
+        this.correctBonus = calculateCorrectBonus(userNumbers, bonusNumber);
     }
 
     public long calculateCorrectWinning(Lotto userNumbers, Lotto winningNumbers) {
@@ -16,7 +22,26 @@ public class LottoMatcher {
         return correctCount;
     }
 
+    public boolean calculateCorrectBonus(Lotto userNumbers, int bonusNumber) {
+        return userNumbers.getLottoNumbers().contains(bonusNumber);
+    }
+
     public long getCorrectWinningCount() {
         return this.correctWinningCount;
+    }
+
+    public long getAllCorrectCount() {
+        long allCorrectCount = 0;
+
+        if (isCorrectBonus()) {
+            allCorrectCount += 1;
+        }
+
+        allCorrectCount += this.correctWinningCount;
+        return allCorrectCount;
+    }
+
+    public boolean isCorrectBonus() {
+        return this.correctBonus;
     }
 }

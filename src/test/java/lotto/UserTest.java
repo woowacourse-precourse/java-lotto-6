7881e113b341c7aa3f2ca3bb67b1f2e11e71d38c
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2023-11-05 15:50:14 +0900

    test: 로또 발행 기능 수정

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,22 +1,21 @@
 package lotto;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import lotto.model.Lotto;
 import lotto.model.User;
+import lotto.utils.Constants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class UserTest {
 
-    @DisplayName("구입 금액만큼 로또를 발행한다.")
+    @DisplayName("로또를 구입하면, 구입 금액만큼 로또를 발행한다.")
     @Test
     void 구입_금액_만큼_로또_발행() {
         // given
         int amount = 8000;
-        int lottoPrice = 1000;
         User user = User.from(amount);
 
         // when
@@ -24,6 +23,6 @@ public class UserTest {
         List<Lotto> purchasedLottos = user.getPurchasedLottos();
 
         // then
-        assertEquals(amount/lottoPrice, purchasedLottos.size());
+        assertEquals(amount/Constants.LOTTO_PRICE, purchasedLottos.size());
     }
 }

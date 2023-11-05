82d37db063c0babd18163bf485460bcd4a5fbfb6
newh08 <newh08@gmail.com>
2023-11-06 07:14:52 +0900

    ♻️REFACTOR: LottoPrizeMoney 수익률 계산 로직 이 및 자료형 long -> double 변경

--- src/main/java/lotto/domain/lottoresult/LottoPrizeMoney.java
+++ src/main/java/lotto/domain/lottoresult/LottoPrizeMoney.java
@@ -1,9 +1,12 @@
 package lotto.domain.lottoresult;
 
-public class LottoPrizeMoney {
-    private final long lottoPrizeMoney;
+import lotto.domain.player.Profit;
 
-    private LottoPrizeMoney(long lottoPrizeMoney) {
+public class LottoPrizeMoney {
+    private final static int HUNDRED_FOR_CALCULATE_PERCENTAGE = 100;
+    private final double lottoPrizeMoney;
+
+    private LottoPrizeMoney(double lottoPrizeMoney) {
         this.lottoPrizeMoney = lottoPrizeMoney;
     }
 
@@ -11,11 +14,11 @@ public class LottoPrizeMoney {
         return new LottoPrizeMoney(0);
     }
 
-    public LottoPrizeMoney updateLottoPrizeMoney(LottoResult lottoResult) {
+    LottoPrizeMoney updateLottoPrizeMoney(LottoResult lottoResult) {
         return new LottoPrizeMoney(lottoPrizeMoney + lottoResult.getPrizeMoney());
     }
 
-    public long getLottoPrizeMoney() {
-        return lottoPrizeMoney;
+    public Profit calculateProfit(int usedMoney) {
+        return new Profit(((lottoPrizeMoney / usedMoney) * HUNDRED_FOR_CALCULATE_PERCENTAGE));
     }
 }

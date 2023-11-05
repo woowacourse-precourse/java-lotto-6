e950710621a9dedabece05ee630acf43762ec901
JongukYang <didwhddnr123@naver.com>
2023-11-05 20:12:21 +0900

    fix: 메서드 이름 수정

--- src/main/java/lotto/validator/InputBonusNumberValidator.java
+++ src/main/java/lotto/validator/InputBonusNumberValidator.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public class InputBonusNumberValidator {
     public void validate(String input) {
-        validatePurchaseAmountIsBlank(input);
-        validatePurchaseAmountIsNumeric(input);
-        validatePurchaseAmountIsPositive(input);
-        validateWinningLotteryNumbersIsBetweenValidRange(input);
+        validateBonusNumberIsBlank(input);
+        validateBonusNumberIsNumeric(input);
+        validateBonusNumberIsPositive(input);
+        validateBonusNumberIsBetweenValidRange(input);
     }
 
     public void validateBonusNumberIsDuplicatedWithWinningLotteryNumbers(String input, List<Integer> winningLotteryNumbers) {
@@ -23,13 +23,13 @@ public class InputBonusNumberValidator {
         }
     }
 
-    private void validatePurchaseAmountIsBlank(String input) {
+    private void validateBonusNumberIsBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_DOES_NOT_BLANK.getMessage());
         }
     }
 
-    private void validatePurchaseAmountIsNumeric(String input) {
+    private void validateBonusNumberIsNumeric(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -37,13 +37,13 @@ public class InputBonusNumberValidator {
         }
     }
 
-    private void validatePurchaseAmountIsPositive(String input) {
+    private void validateBonusNumberIsPositive(String input) {
         if (Integer.parseInt(input) <= 0) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_MUST_BE_POSITIVE.getMessage());
         }
     }
 
-    private void validateWinningLotteryNumbersIsBetweenValidRange(String input) {
+    private void validateBonusNumberIsBetweenValidRange(String input) {
         int number = Integer.parseInt(input);
         if (number < LottoConstant.MIN_LOTTO_NUMBER.getValue() || number > LottoConstant.MAX_LOTTO_NUMBER.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getMessage());

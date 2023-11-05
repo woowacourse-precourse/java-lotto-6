1ebc2a2bdb46d1d33696744c63a252b2555d8cfa
5jeong <wjdgns4385@naver.com>
2023-11-05 21:40:25 +0900

    [Feat] 로또 결과 모델 구현
    - 등수 판단기능 구현
    - 로또결과 저장기능 구현

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,61 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class LottoResult {
+
+    private final LinkedHashMap<LottoRank, Integer> lottoResult = new LinkedHashMap<>();
+
+    private final ArrayList<Lotto> purchaseLottos;
+    private final WinningLotto winningLotto;
+
+    public LottoResult(ArrayList<Lotto> purchaseLottos, WinningLotto winningLotto) {
+        this.purchaseLottos = purchaseLottos;
+        this.winningLotto = winningLotto;
+        initLottoResult();
+    }
+
+    private void initLottoResult() {
+        lottoResult.put(LottoRank.NO_RNAK, 0);
+        lottoResult.put(LottoRank.FIFTH, 0);
+        lottoResult.put(LottoRank.FOURTH, 0);
+        lottoResult.put(LottoRank.THIRD, 0);
+        lottoResult.put(LottoRank.SECOND, 0);
+        lottoResult.put(LottoRank.FIRST, 0);
+    }
+
+
+    private void saveLottoResult(LottoRank lottoRank) {
+        lottoResult.put(lottoRank, lottoResult.get(lottoRank) + 1);
+    }
+
+
+    private LottoRank judgeRank(int matchCount, boolean containBonusNumber) {
+
+        if (matchCount == 5 && containBonusNumber) {
+            return LottoRank.SECOND;
+        }
+        for (LottoRank lottoRank : LottoRank.values()) {
+            if (lottoRank.getMatchCount() == matchCount) {
+                return lottoRank;
+            }
+        }
+        return LottoRank.NO_RNAK;
+
+    }
+
+    public void judgeResult() {
+        for (Lotto lotto : purchaseLottos) {
+            int matchCount = lotto.matchCount(winningLotto);
+            boolean containBonusNumber = lotto.containLottoNumber(winningLotto.getBonusNumber());
+            LottoRank lottoRank = judgeRank(matchCount, containBonusNumber);
+            saveLottoResult(lottoRank);
+        }
+    }
+
+    public HashMap<LottoRank, Integer> getLottoResult() {
+        return lottoResult;
+    }
+}

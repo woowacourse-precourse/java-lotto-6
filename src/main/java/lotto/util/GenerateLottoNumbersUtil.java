3ef2c7a8b7f4712311a8d571375b70d38b9ae019
WooYoungDoo <duden5000@naver.com>
2023-11-05 13:16:09 +0900

    feat (GenerateLottoNumbersUtil) : 로또 랜덤 번호 생성 로직

---
+++ src/main/java/lotto/util/GenerateLottoNumbersUtil.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class GenerateLottoNumbersUtil {
+
+    public static List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

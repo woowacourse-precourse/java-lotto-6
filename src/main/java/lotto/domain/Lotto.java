b9a85df8c6b08d1c682f2003c397268bab70870a
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-06 01:06:23 +0900

    feat(Lotto) : 입력받은 당첨 번호가 1~45를 벗어나는 경우 예외처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -21,10 +21,19 @@ public class Lotto {
         if (hasDuplicate(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+
+        if (!isValidRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     private boolean hasDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         return uniqueNumbers.size() < 6;
+    }
+
+    private boolean isValidRange(List<Integer> numbers) {
+        return numbers.stream()
+            .allMatch(i -> i >= 1 && i <= 45);
     }
 }

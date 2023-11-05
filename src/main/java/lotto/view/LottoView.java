528f1c8eb9ad99a7d11eed4f739260e86e557b87
권장순 <jsoonworld@gmail.com>
2023-11-06 07:22:55 +0900

    feat(LottoView#inputWinningNumbers) : 당첨 번호를 입력 받는 메서드 구현.

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,6 +2,10 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class LottoView {
     public LottoView() {
 
@@ -11,4 +15,15 @@ public class LottoView {
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
+
+    public List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요. (쉼표로 구분)");
+        return Stream.of(Console.readLine().split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    
+
 }

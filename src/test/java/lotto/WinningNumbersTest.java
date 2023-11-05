df0c6c3ee8e0a4eabe539caa8285ad06ade3e8a2
hongseokhodev <hongseokhodev@gmail.com>
2023-11-05 17:07:54 +0900

    test: LottoFixture를 사용하도록 수정

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -8,56 +8,54 @@ import org.junit.jupiter.api.Test;
 
 public class WinningNumbersTest {
     private void assertLottoRank(Lotto lotto, Rank expected) {
-        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        int bonus = 7;
-        WinningNumbers winningNumbers = new WinningNumbers(winningLotto, bonus);
+        WinningNumbers winningNumbers = LottoFixture.standard();
         assertEquals(winningNumbers.rank(lotto), expected);
     }
     @DisplayName("6개가 맞을 경우 1등이다.")
     @Test
     void sixToFirst() {
-        assertLottoRank(new Lotto(List.of(1, 2, 3, 4, 5, 6)), Rank.FIRST);
+        assertLottoRank(LottoFixture.SIX.toLotto(), Rank.FIRST);
     }
 
     @DisplayName("5개가 맞고 보너스 번호가 맞을 경우 2등이다.")
     @Test
     void fiveWithBonusToSecond() {
-        assertLottoRank(new Lotto(List.of(1, 2, 3, 4, 5, 7)), Rank.SECOND);
+        assertLottoRank(LottoFixture.FIVE_WITH_BONUS.toLotto(), Rank.SECOND);
     }
 
     @DisplayName("5개가 맞고 보너스 번호가 틀릴 경우 3등이다.")
     @Test
     void fiveWithOutBonusToThird() {
-        assertLottoRank(new Lotto(List.of(1, 2, 3, 4, 5, 8)), Rank.THIRD);
+        assertLottoRank(LottoFixture.FIVE_WITHOUT_BONUS.toLotto(), Rank.THIRD);
     }
 
     @DisplayName("4개가 맞을 경우 4등이다.")
     @Test
     void fourToFourth() {
-        assertLottoRank(new Lotto(List.of(1, 2, 3, 4, 7, 8)), Rank.FOURTH);
+        assertLottoRank(LottoFixture.FOUR.toLotto(), Rank.FOURTH);
     }
 
     @DisplayName("3개가 맞을 경우 5등이다.")
     @Test
     void ThreeToFifth() {
-        assertLottoRank(new Lotto(List.of(1, 2, 3, 7, 8, 9)), Rank.FIFTH);
+        assertLottoRank(LottoFixture.THREE.toLotto(), Rank.FIFTH);
     }
 
     @DisplayName("2개가 맞을 경우 낙첨이다.")
     @Test
     void TwoToBlank() {
-        assertLottoRank(new Lotto(List.of(1, 2, 8, 9, 10, 11)), Rank.BLANK);
+        assertLottoRank(LottoFixture.TWO.toLotto(), Rank.BLANK);
     }
 
     @DisplayName("1개가 맞을 경우 낙첨이다.")
     @Test
     void OneToBlank() {
-        assertLottoRank(new Lotto(List.of(1, 8, 9, 10, 11, 12)), Rank.BLANK);
+        assertLottoRank(LottoFixture.ONE.toLotto(), Rank.BLANK);
     }
 
     @DisplayName("0개가 맞을 경우 낙첨이다.")
     @Test
     void NoneToBlank() {
-        assertLottoRank(new Lotto(List.of(8, 9, 10, 11, 12, 13)), Rank.BLANK);
+        assertLottoRank(LottoFixture.NONE.toLotto(), Rank.BLANK);
     }
 }

0779c468acb1824e510327ee09d9f5c2aa2a2292
JH-TT <leejongho9803@gmail.com>
2023-11-05 15:48:34 +0900

    feat: 보너스 번호가 있는지 확인하는 기능 추가
    - 3등인 경우를 제외하고는 전부 false처리해서 등수 계산에 용이하도록 구현

--- src/main/java/lotto/utils/LottoUtils.java
+++ src/main/java/lotto/utils/LottoUtils.java
@@ -3,12 +3,14 @@ package lotto.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.validation.Validation;
 
 public class LottoUtils {
     private static final int START = 1;
     private static final int END = 45;
     private static final int LENGTH = 6;
+    private static final int THRID_OR_SECOND = 5;
     private static final String DELIMITER = ",";
     private static final int LOTTO_PRISE = 1000;
     public static List<Integer> generateRandomLottoNumbers() {
@@ -36,4 +38,10 @@ public class LottoUtils {
     public static int matchLotto(List<Integer> buyerLotto, List<Integer> winningLotto) {
         return (int) buyerLotto.stream().filter(winningLotto::contains).count();
     }
+
+    public static boolean matchBonus(int cnt, Lotto lotto, int bonusNumber) {
+        if (cnt != THRID_OR_SECOND) return false;
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+
 }

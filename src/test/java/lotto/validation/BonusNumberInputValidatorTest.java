7f7c29297678d0d634ea3ec61ed42757e0092696
acs0209 <annjae4676@naver.com>
2023-11-05 21:05:57 +0900

    test: 보너스 번호와 당첨번호의 중복에 대해 검증하는 테스트 구현

---
+++ src/test/java/lotto/validation/BonusNumberInputValidatorTest.java
@@ -0,0 +1,23 @@
+package lotto.validation;
+
+import static lotto.validation.enumType.BonusNumber.DUPLICATION_BONUS_NUMBER;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberInputValidatorTest {
+
+    @Test
+    @DisplayName("보너스 번호와 당첨번호가 중복이 되면 예외가 발생하는 테스트 ")
+    void testValidateBonusNumberDuplication() {
+        BonusNumberInputValidator inputValidation = new BonusNumberInputValidator();
+
+        String numbers = "1,2,3,4,5,6";
+        String number = "6";
+
+        assertThatThrownBy(() -> inputValidation.validateBonusNumberDuplication(numbers, number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DUPLICATION_BONUS_NUMBER.getMessage());
+    }
+}

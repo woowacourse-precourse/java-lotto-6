f2516d234adcccd15497e657917a36c5745936fb
YongHyeon <pyh4616@naver.com>
2023-11-05 16:28:10 +0900

    Feat(WinningStatistics 클래스): 로또의 당첨 결과를 확인하여 당첨 통계에 추가하는 기능을 checkWinningResult 메서드로 구현

--- src/main/java/lotto/model/WinningStatistics.java
+++ src/main/java/lotto/model/WinningStatistics.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class WinningStatistics {
 
+    private static final int SECOND_OR_THIRD_PLACE_STANDARD = 5;
+
     private final Map<Rank, Integer> winningStatistics;
 
     public WinningStatistics(Map<Rank, Integer> winningStatistics) {
@@ -15,5 +17,34 @@ public class WinningStatistics {
         for (Rank rank : Rank.values()) {
             winningStatistics.put(rank, 0);
         }
+    }
+
+    public void checkWinningResult(int matchingCount, boolean isBonusNumberMatch) {
+        for (Rank rank : Rank.values()) {
+            if (matchingCount == rank.matchingCount) {
+                checkSecondOrThirdPlace(rank, isBonusNumberMatch);
+                checkRest(rank);
+            }
+        }
+    }
+
+    private void checkSecondOrThirdPlace(Rank rank, boolean isBonusNumberMatch) {
+        if (isSecondOrThirdPlace(rank, isBonusNumberMatch)) {
+            addWinningResult(rank);
+        }
+    }
+
+    private boolean isSecondOrThirdPlace(Rank rank, boolean isBonusNumberMatch) {
+        return (rank.matchingCount == SECOND_OR_THIRD_PLACE_STANDARD) && (isBonusNumberMatch == rank.isBonusNumberMatch);
+    }
+
+    private void checkRest(Rank rank) {
+        if (rank.matchingCount != SECOND_OR_THIRD_PLACE_STANDARD) {
+            addWinningResult(rank);
+        }
+    }
+
+    private void addWinningResult(Rank rank) {
+        winningStatistics.put(rank, winningStatistics.get(rank) + 1);
     }
 }

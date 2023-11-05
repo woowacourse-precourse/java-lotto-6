30c950f71b13069f5c00b2dcb328382889de2077
TaeHye0n <thk001229@gmail.com>
2023-11-05 16:58:32 +0900

    feat(Lotto): 당첨 번호 저장 기능  - 길이, 중복 검증 후 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,35 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int LOTTO_NUMBER_LENGTH = 6;
+    private static final String EXCEED_LENGTH_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String DUPLICATE_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복이 없어야 합니다.";
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(final List<Integer> numbers) {
+        validateLottoNumberLength(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoNumberLength(final List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_LENGTH) {
+            throw new IllegalArgumentException(EXCEED_LENGTH_ERROR_MESSAGE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(final List<Integer> numbers) {
+        if (hasDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean hasDuplicateNumber(final List<Integer> numbers) {
+        return (new HashSet<>(numbers).size() != numbers.size());
+    }
+
 }

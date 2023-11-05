b85c9123b5bcdf85618f0b707942da195a0eac36
insu <is23193160@gmail.com>
2023-11-05 17:19:41 +0900

    test: 사용하지 않는 테스트 제거

--- src/test/java/lotto/model/PurchaseAmountTest.java
+++ src/test/java/lotto/model/PurchaseAmountTest.java
@@ -10,23 +10,15 @@ class PurchaseAmountTest {
     @Test
     @DisplayName("PurchaseAmount가 잘 초기화 되는지 확인")
     void initPurchaseAmount() {
-        PurchaseAmount purchaseAmount = new PurchaseAmount("50000");
+        PurchaseAmount purchaseAmount = new PurchaseAmount(50000);
 
         assertThat(purchaseAmount.getPurchaseAmount()).isEqualTo(50000);
     }
 
     @Test
-    @DisplayName("숫자가 아닌 값을 입력받으면 예외 처리")
-    void inputNonNumericPurchaseAmount() {
-        assertThatThrownBy(() -> new PurchaseAmount("a"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 숫자를 입력해주세요.");
-    }
-
-    @Test
     @DisplayName("구입할 수 있는 최대 로또 갯수를 잘 반환하는지 확인")
     void getMaxLottoCountForBudget() {
-        PurchaseAmount purchaseAmount = new PurchaseAmount("50000");
+        PurchaseAmount purchaseAmount = new PurchaseAmount(50000);
 
         assertThat(purchaseAmount.getMaxLottoCountForBudget()).isEqualTo(50);
     }

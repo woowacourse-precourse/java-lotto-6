1a141258623f78b2db126aaba2709d474cefd63a
yuseonjun <seonjun0906@gmail.com>
2023-11-05 15:51:17 +0900

    test(LottoShopTest): @BeforeEach 사용으로 간결하게 리팩터링

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -3,19 +3,26 @@ package lotto.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoShopTest {
 
+    private NumberGenerator numberGenerator;
+    private LottoShop lottoShop;
+
+    @BeforeEach
+    void setUp() {
+        numberGenerator = new TestRandomGenerator();
+        lottoShop = new LottoShop(numberGenerator);
+    }
+
     @ParameterizedTest
     @DisplayName("구매금액에 따른 로또 개수 확인 테스트")
     @ValueSource(ints = {1000, 2000, 3000})
-    void Create_By_BuyerLotto_With_TestRandomGenerator(int price) {
-        NumberGenerator numberGenerator = new TestRandomGenerator();
-        LottoShop lottoShop = new LottoShop(numberGenerator);
-
+    void createByBuyerLottoWithTestRandomGenerator(int price) {
         List<Lotto> buyerLotto = lottoShop.createByBuyerLotto(price);
 
         assertEquals(buyerLotto.size(), price / 1000);

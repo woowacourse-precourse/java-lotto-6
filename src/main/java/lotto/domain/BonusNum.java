39bf3372dd13813964cbfac233a2ac8ca8142141
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-06 03:35:18 +0900

    feat: BonusNum 관리 클래스 추가

---
+++ src/main/java/lotto/domain/BonusNum.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import lotto.validator.BonusNumValidator;
+
+import java.util.List;
+
+public class BonusNum {
+
+    private int bonusNum;
+
+    public BonusNum(String number, List<Integer> winningNum){
+        validate(number, winningNum);
+        this.bonusNum = Integer.parseInt(number);
+    }
+
+    public void validate(String number, List<Integer> winningNum){
+        BonusNumValidator bonusNumValidator = new BonusNumValidator(number, winningNum);
+    }
+
+    public int getBonusNum(){
+        return bonusNum;
+    }
+}

48b1e6c148f9a9fbdedf5d07cbaed309c20978a0
Hyeonbin2379 <hyeonbin2379@gmail.com>
2023-11-06 02:54:39 +0900

    refactor(InputView) : 문자열로 입력된 당첨 번호 검사 메소드 분리 당첨 번호에 관한 문자열 입력값 전체를 대상으로 한 유효성 검사 메소드 별도 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ import static lotto.constants.ErrorMessage.SPACE_INCLUDED;
 import static lotto.constants.LottoIOMessage.BONUS_NUM_INPUT;
 import static lotto.constants.LottoIOMessage.BUYING_COST_INPUT;
 import static lotto.constants.LottoIOMessage.WINNING_NUM_INPUT;
+import static lotto.constants.MarksAndConstants.SPACE;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.util.Validation;
@@ -22,8 +23,7 @@ public class InputView {
     public String inputWinnerNumbers() {
         System.out.println(WINNING_NUM_INPUT);
         String inputWinningNum = Console.readLine();
-        validateInput(inputWinningNum);
-        Validation.validateComma(inputWinningNum);
+        validateInputWinnerNum(inputWinningNum);
         return inputWinningNum;
     }
 
@@ -38,14 +38,17 @@ public class InputView {
         validateBlankInput(input);
         validateSpaceChar(input);
     }
+    public void validateInputWinnerNum(String input) {
+        validateInput(input);
+        Validation.validateComma(input);
+    }
     private void validateBlankInput(String input) {
-
         if (input.isBlank()) {
             throw new IllegalArgumentException(String.format(ERROR_FORMAT, BLANK_INPUT+input));
         }
     }
     private void validateSpaceChar(String input) {
-        if (input.contains(" ")) {
+        if (input.contains(SPACE)) {
             throw new IllegalArgumentException(String.format(ERROR_FORMAT, SPACE_INCLUDED+input));
         }
     }

c56df34109d104d196372851f9af123be776c311
dj <fjzl12@naver.com>
2023-11-05 20:35:10 +0900

    feat(OutputView): 발행한 로또 출력하는 기능 구현

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,8 +1,18 @@
 package lotto.ui;
 
-public class OutputView {
-    public static void printLotto(int amount){
+import lotto.Lotto;
 
+import java.util.List;
+
+import static lotto.domain.Message.PURCHASE_COMMENT;
+
+public class OutputView {
+    public static void printLotto(List<Lotto> myLotto) {
+        System.out.printf(PURCHASE_COMMENT.getMessage(), myLotto.size());
+        for (Lotto lotto : myLotto) {
+            System.out.println(lotto.getNumbers());
+        }
+        System.out.println();
     }
 
 }

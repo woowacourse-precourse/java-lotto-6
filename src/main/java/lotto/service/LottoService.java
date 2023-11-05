e005da5802dbe60e57c195753de396c2c1a3ee3d
WooYoungDoo <duden5000@naver.com>
2023-11-05 14:18:42 +0900

    feat (LottoService) : 로또 구매 번호 생성 및 정렬

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,7 +2,12 @@ package lotto.service;
 
 import lotto.model.LottoGame;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static lotto.util.CalculateUtil.divideByThousands;
+import static lotto.util.GenerateLottoNumbersUtil.generateLottoNumber;
 import static lotto.util.TypeConversionUtil.StringToInt;
 
 public class LottoService {
@@ -14,5 +19,30 @@ public class LottoService {
         int lottoCount = divideByThousands(purchaseMoney);
         lottoGame.setLottoCount(lottoCount);
         return lottoCount;
+    }
+
+    public List<List<Integer>> purchaseLottoNumberProcess() {
+        List<List<Integer>> purchaseLottoNumbers = generateLottoNumbers();
+        setPurchaseLottoNumbers(purchaseLottoNumbers);
+        return purchaseLottoNumbers;
+    }
+
+    private void setPurchaseLottoNumbers(List<List<Integer>> purchaseLottoNumbers) {
+        lottoGame.setPurchaseLottoNumbers(purchaseLottoNumbers);
+    }
+
+    private List<List<Integer>> generateLottoNumbers() {
+        List<List<Integer>> purchaseLottoNumbers = new ArrayList<>();
+        int lottoCount = lottoGame.getLottoCount();
+        for (int i = 0; i < lottoCount; i ++) {
+            List<Integer> lottoNumber = generateLottoNumber();
+            sortLottoNumber(lottoNumber);
+            purchaseLottoNumbers.add(lottoNumber);
+        }
+        return purchaseLottoNumbers;
+    }
+
+    private static void sortLottoNumber(List<Integer> lottoNumber) {
+        Collections.sort(lottoNumber);
     }
 }

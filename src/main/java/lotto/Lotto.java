a7a2e6e92b49323b6a38c40cc255e0ef381fd974
dj <fjzl12@naver.com>
2023-11-05 20:32:17 +0900

    feat(Lotto): 로또 번호 중복 및 숫자 범위 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 개수 오류");
+        }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("로또 중복 오류");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또 내 숫자 오류");
+            }
         }
     }
 

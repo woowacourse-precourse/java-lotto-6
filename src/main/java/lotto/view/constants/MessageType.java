652bcc6b3a78a3d6c291990b103fdeac37a73ba1
MingyeomKim <mingyum119@naver.com>
2023-11-05 20:05:59 +0900

    feat(MessageType): 발행한 로또 수량 출력 메시지를 상수화
    MessageType에서 발행한 로또의 수량을  출력한다.

--- src/main/java/lotto/view/constants/MessageType.java
+++ src/main/java/lotto/view/constants/MessageType.java
@@ -1,7 +1,8 @@
 package lotto.view.constants;
 
 public enum MessageType {
-    COST_REQUEST_MESSAGE("구입금액을 입력해 주세요.");
+    COST_REQUEST_MESSAGE("구입금액을 입력해 주세요."),
+    LOTTO_COUNT_MESSAGE("%s개를 구매했습니다.");
 
     private final String message;
 

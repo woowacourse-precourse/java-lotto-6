719918de090ccabfb6f5decb02fbb2bb13b3abe8
pomecrenate <kk37325940@gmail.com>
2023-11-05 20:11:30 +0900

    :sparkles: feat: 구입 금액, 당첨 번호, 보너스 번호 질문 메서드

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,29 @@
 package lotto.view;
 
+import static lotto.view.constants.ConstantMessage.REQUEST_PURCHASE_AMOUNT;
+import static lotto.view.constants.ConstantMessage.REQUEST_WINNING_NUMBER;
+import static lotto.view.constants.ConstantMessage.REQUEST_BONUS_NUMBER;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.constants.ConstantMessage;
+
 public class InputView {
+    public static String requestPurchaseAmount() {
+        printConstantMessage(REQUEST_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public static String requestWinningNumber() {
+        printConstantMessage(REQUEST_WINNING_NUMBER);
+        return Console.readLine();
+    }
+
+    public static String requestBonusNumber() {
+        printConstantMessage(REQUEST_BONUS_NUMBER);
+        return Console.readLine();
+    }
+
+    public static void printConstantMessage(ConstantMessage constantMessage) {
+        System.out.println(constantMessage.getMessage());
+    }
 }

219d6b808aee24f9938679ef585a965b5f54f394
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-05 20:55:11 +0900

    refactor : 검증 함수 static , private 으로 변경
    Lotto 의 validate 함수 static 으로 변경
    Payment 의 validate 함수 private 으로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,29 +13,25 @@ public class Lotto implements LottoConstant {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbers(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
     public List<Integer> getNumbers(){
         return numbers;
     }
-    private void validate(List<Integer> numbers) {
-        validateNumbers(numbers);
-
-    }
-    private void validateNumbers(List<Integer>numbers){
+    private static void validateNumbers(List<Integer>numbers){
         validateNumbersSizeIsPickCount(numbers);
         validateNumberInRange(numbers);
     }
 
-    private void validateNumbersSizeIsPickCount(List<Integer>numbers){
+    private static void validateNumbersSizeIsPickCount(List<Integer>numbers){
         if (numbers.size() != PICK_COUNT) {
             throw new LottoException(LottoExceptionMessage.INVALID_PICK_COUNT);
         }
     }
 
-    private void validateNumberInRange(List<Integer>numbers){
+    private static void validateNumberInRange(List<Integer>numbers){
         for (Integer number : numbers){
             if (number<MIN_NUMBER || number>MAX_NUMBER)
                 throw new LottoException(LottoExceptionMessage.INVALID_NUMBER);
--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -6,16 +6,19 @@ import lotto.exception.PaymentExceptionMessage;
 
 public record Payment (int amount) implements PaymentConstant {
     public Payment(int amount){
-        validateAmountIsPositiveNumber(amount);
-        validateAmountIsAligned(amount);
+        validateAmount(amount);
         this.amount = amount;
     }
-    public static void validateAmountIsPositiveNumber(int amount){
+    private static void validateAmount(int amount){
+        validateAmountIsPositiveNumber(amount);
+        validateAmountIsAligned(amount);
+    }
+    private static void validateAmountIsPositiveNumber(int amount){
         if(amount<=0){
             throw new PaymentException(PaymentExceptionMessage.INVALID_VALUE);
         }
     }
-    public static void validateAmountIsAligned(int amount){
+    private static void validateAmountIsAligned(int amount){
         if(amount % PAYMENT_UNIT !=0){
             throw new PaymentException(PaymentExceptionMessage.INVALID_AMOUNT);
         }

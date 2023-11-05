14121f7f97d45e14966ed5d829573b1f39452585
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-05 18:35:42 +0900

    Lotto.validate 입력 받은 로또 번호 검증 (1~45사이의 번호)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 6개를 입력해주세요.");
+        }
+
+        for (Integer number : numbers) {
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이의 수를 입력해주세요.");
+            }
         }
     }
-
-    // TODO: 추가 기능 구현
 }

fa7c850a7384ffa9443da935f0f9fc320596eab6
prao <pch5521@gmail.com>
2023-11-05 15:50:30 +0900

    feat(InputValidator): 입력 유효성 검사 도구 생성
    - validateEmpty: 입력 값이 비어있으면 예외를 던진다.
    - validateEndsWithComma: 입력 값이 쉼표(,)로 끝나면 예외를 던진다.
    - validatePurchaseAmount: 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외를 던진다.
    - validateLottoNumberCount: 로또 번호의 개수가 LOTTO_COUNT(6)과 같지 않다면 예외를 던진다.
    - validateLottoNumberRange: 로또 번호가 정해진 범위에 속하지 않으면 예외를 던진다.

---
+++ src/main/java/lotto/validator/InputValidator.java
@@ -0,0 +1,55 @@
+package lotto.validator;
+
+import static lotto.domain.LottoCondition.LOTTO_COUNT;
+import static lotto.domain.LottoCondition.LOTTO_PRICE;
+import static lotto.domain.LottoCondition.MAXIMUM_LOTTO_NUMBER;
+import static lotto.domain.LottoCondition.MINIMUM_LOTTO_NUMBER;
+import static lotto.domain.Symbols.DELIMITER;
+import static lotto.exception.ErrorMessage.EMPTY;
+import static lotto.exception.ErrorMessage.ENDS_WITH_DELIMITER;
+import static lotto.exception.ErrorMessage.INVALID_LOTTO_NUMBER_COUNT_INPUT;
+import static lotto.exception.ErrorMessage.INVALID_LOTTO_NUMBER_INPUT;
+import static lotto.exception.ErrorMessage.INVALID_PURCHASE_AMOUNT;
+
+import java.util.List;
+import lotto.exception.LottoException;
+
+public class InputValidator {
+
+    private InputValidator() {
+    }
+
+    public static void validateEmpty(final String input) {
+        if (input.isEmpty()) {
+            throw LottoException.of(EMPTY);
+        }
+    }
+
+    public static void validateEndsWithComma(final String input) {
+        if (input.endsWith(DELIMITER.getSymbol())) {
+            throw LottoException.of(ENDS_WITH_DELIMITER);
+        }
+    }
+
+    public static void validatePurchaseAmount(final int input) {
+        if (input % LOTTO_PRICE.getValue() != 0) {
+            throw LottoException.of(INVALID_PURCHASE_AMOUNT);
+        }
+    }
+
+    public static void validateLottoNumberCount(final List<Integer> input) {
+        if (input.size() != LOTTO_COUNT.getValue()) {
+            throw LottoException.of(INVALID_LOTTO_NUMBER_COUNT_INPUT);
+        }
+    }
+
+    public static void validateLottoNumberRange(final int input) {
+        if (isNumberOutOfRange(input)) {
+            throw LottoException.of(INVALID_LOTTO_NUMBER_INPUT);
+        }
+    }
+
+    private static boolean isNumberOutOfRange(final int input) {
+        return input > MAXIMUM_LOTTO_NUMBER.getValue() || input < MINIMUM_LOTTO_NUMBER.getValue();
+    }
+}

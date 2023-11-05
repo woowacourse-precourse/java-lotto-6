8a51dfec13c06d0ae0eba3e4ad263556f4437099
sangchu <didi1484@gmail.com>
2023-11-05 17:02:10 +0900

    feat: WINNER_RESULT 필드 추가

--- src/main/java/lotto/constants/Notice.java
+++ src/main/java/lotto/constants/Notice.java
@@ -4,8 +4,8 @@ public enum Notice {
     ASK_PURCHASE_PRICE("구입금액을 입력해 주세요."),
     PURCHASE_RESULT("개를 구매했습니다."),
     ASK_WINNER_NUMBERS("당첨 번호를 입력해 주세요."),
-    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
-
+    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    WINNER_RESULT("당첨 통계\n---");
     private final String message;
 
     Notice(String message) {

11cced8ee7759713b5934f8ca5aca10928c86060
unknown <winternrabbit@gmail.com>
2023-11-05 20:36:47 +0900

    style: 출력 문구 수정

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,15 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private static final String REQUEST_COST = "구입금액을 입력해 주세요.";
-    private static final String REQUEST_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
-    public static String buyLotto() {
+    public String buyLotto() {
         System.out.println(REQUEST_COST);
 
         return Console.readLine();
     }
 
-    public static String winningNumbers() {
+    public String winningNumbers() {
         System.out.println(REQUEST_WINNING_NUMBER);
 
         return Console.readLine();

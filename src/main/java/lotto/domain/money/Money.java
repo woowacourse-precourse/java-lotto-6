84160b713ada58cbde9a7f3ba497b935bb396010
minisyu <iswater.kim@gmail.com>
2023-11-06 01:20:10 +0900

    refactor: Money 클래스 내에 검증 로직 제거

--- src/main/java/lotto/domain/money/Money.java
+++ src/main/java/lotto/domain/money/Money.java
@@ -1,5 +1,7 @@
 package lotto.domain.money;
 
+import lotto.domain.validator.MoneyValidator;
+
 public class Money {
     public static final int LOTTO_PRICE = 1_000;
     private static final int POSITIVE_VALUE = 0;
@@ -7,47 +9,23 @@ public class Money {
     private final int money;
 
     private Money(int money) {
-        verifyPositiveMoney(money);
+        MoneyValidator.verifyPositiveMoney(money);
         this.money = money;
     }
 
     public static Money fromInitialMoney(int money) {
-        verifyMinAmount(money);
-        verifyMaxAmount(money);
-        verifyDivisibleBy(money);
+        MoneyValidator.verifyMinAmount(money);
+        MoneyValidator.verifyMaxAmount(money);
+        MoneyValidator.verifyDivisibleBy(money);
         return new Money(money);
     }
 
     public static Money fromRemainingMoney(int money) {
-        verifyMaxAmount(money);
-        verifyDivisibleBy(money);
+        MoneyValidator.verifyMaxAmount(money);
+        MoneyValidator.verifyDivisibleBy(money);
         return new Money(money);
     }
-
-    private static void verifyDivisibleBy(int money) {
-        if (money % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("구입 금액은 1000원 단위로 입력해주세요.");
-        }
-    }
-
-    private static void verifyMinAmount(int money) {
-        if (money < LOTTO_PRICE) {
-            throw new IllegalArgumentException("구입 금액은 최소 1000원 이상으로 입력해야 합니다.");
-        }
-    }
-
-    private static void verifyMaxAmount(int money) {
-        if (money > MAX_MONEY) {
-            throw new IllegalArgumentException("금액은 100,000원 이하로 입력해야 합니다.");
-        }
-    }
-
-    private void verifyPositiveMoney(int money) {
-        if (money < POSITIVE_VALUE) {
-            throw new IllegalArgumentException("구입 금액은 음수가 될 수 없습니다.");
-        }
-    }
-
+    
     public Money decremented() {
         return Money.fromRemainingMoney(money - LOTTO_PRICE);
     }

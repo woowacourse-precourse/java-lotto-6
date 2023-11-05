ebd9b5020d33bf2b6684c05e6248fadeb71937ba
nova4212 <ghkdtmdgur59@gmail.com>
2023-11-05 16:05:42 +0900

    fix : 로또 번호 정렬 기능 수정

--- src/main/java/lotto/GenLotto.java
+++ src/main/java/lotto/GenLotto.java
@@ -1,13 +1,12 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 
 public class GenLotto {
-    private static List<Integer> lotto;
     private static final int Count = 6;
     private static List<Lotto> Lottos;
     public List<Lotto> makeLottoNum(int Chance){
@@ -18,8 +17,8 @@ public class GenLotto {
         return Lottos;
     }
     private static Lotto makelotto(){
-        lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        lotto.sort(Comparator.naturalOrder());
+        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(lotto);
         printLottos(lotto);
         return new Lotto(lotto);
     }

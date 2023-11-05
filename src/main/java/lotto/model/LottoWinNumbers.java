9e02414480d80ac261a2ecfe4ae7c9a4f06b778d
kkkkimtaehyeon <kth1927@naver.com>
2023-11-06 01:23:27 +0900

    feat : 예외처리 조건 수정

--- src/main/java/lotto/model/LottoWinNumbers.java
+++ src/main/java/lotto/model/LottoWinNumbers.java
@@ -7,16 +7,15 @@ import java.util.List;
 
 public class LottoWinNumbers {
     private final ArrayList<String> LOTTO_WIN_NUMBERS;
-    private static final String SPECIAL_REGEX = "[!@#$%^&*()_+=\\[\\]{};':\"\\\\|.<>/?-]";
-    private static final String NUMERIC_REGEX = "[0-9]";
+    private static final String SPECIAL_REGEX = "[!@#$%^&*()_+=\\[\\]{};':\"\\\\|<>/?]+";
+    private static final String NUMERIC_REGEX = "[0-9]+$";
     public LottoWinNumbers(String winNumbers){
         String[] set = winNumbers.split(",");
         isWrongDelimiter(set);
         isNotDigits(set);
+        isNotNumeric(set);
         isWrongNumberRange(set);
         isSameNumber(set);
-        //음수 예외처리 추가 필요 -> numberFormatException
-        //실수 예외처리 추가 필요 -> numberFormatException
         this.LOTTO_WIN_NUMBERS = determineWinNumbers(set);
     }
     public ArrayList<String> getLottoWinNumbers(){
@@ -36,15 +35,22 @@ public class LottoWinNumbers {
     }
     private void isNotDigits(String[] set) throws IllegalArgumentException{
         for(String number : set){
-            if(!number.matches(".*" + NUMERIC_REGEX + ".*")){
-                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 문자가 포함되어 있습니다");
+            if(number.contains("-") || number.contains(".")){
+                throw new IllegalArgumentException("[ERROR] 숫자는 1이상 45이하의 정수 입니다.");
+            }
+        }
+    }
+    private void isNotNumeric(String[] set) throws IllegalArgumentException{
+        for(String number : set){
+            if(!number.matches(NUMERIC_REGEX)){
+                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 문자가 포함되어 있습니다.");
             }
         }
     }
     private void isWrongNumberRange(String[] set) throws IllegalArgumentException{
         for(String number : set){
             if(Integer.parseInt(number) < 1 || Integer.parseInt(number) > 45){
-                throw new IllegalArgumentException("[ERROR] 숫자의 입력은 1이상 45이하의 정수입니다.");
+                throw new IllegalArgumentException("[ERROR] 숫자의 입력은 1이상 45이하 입니다.");
             }
         }
     }

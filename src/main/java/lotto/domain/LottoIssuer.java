4e6e4ef5dfd537899431d6474ceac9a9e8087dec
Hanjaemo <hjm2530@gmail.com>
2023-11-05 15:48:22 +0900

    feat(LottoIssuer): 구입 금액만큼 로또 발행 기능 구현

---
+++ src/main/java/lotto/domain/LottoIssuer.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoIssuer {
+
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBERS_COUNT = 6;
+
+    public List<Lotto> issue(PurchaseAmount purchaseAmount) {
+        List<Lotto> lottos = new ArrayList<>();
+        do {
+            lottos.add(getLotto());
+        } while (purchaseAmount.decrease(LOTTO_PRICE) > 0);
+        return List.copyOf(lottos);
+    }
+
+    private Lotto getLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBERS_COUNT));
+    }
+}

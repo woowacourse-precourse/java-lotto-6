78b2d19dde973ec4a57f77f26cf2568b50006e67
kes02 <hijin1220@naver.com>
2023-11-06 01:19:26 +0900

    Minor: Modify Format
    테스트 코드에서 format이 매치가 안 되어 오류 발생
    - '개' 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -21,7 +21,4 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -19,23 +19,23 @@ public class OutputView {
     }
 
     public static void printThreeCorrect(int three) {
-        System.out.println("3개 일치 (5,000원) - " + three);
+        System.out.println("3개 일치 (5,000원) - " + three + "개");
     }
 
     public static void printFourCorrect(int four) {
-        System.out.println("4개 일치 (50,000원) - " + four);
+        System.out.println("4개 일치 (50,000원) - " + four+ "개");
     }
 
     public static void printFiveCorrect(int five) {
-        System.out.println("5개 일치 (1,500,000원) - " + five);
+        System.out.println("5개 일치 (1,500,000원) - " + five+ "개");
     }
 
     public static void printFiveBonusCorrect(int five_bonus) {
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + five_bonus);
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + five_bonus+ "개");
     }
 
     public static void printSixCorrect(int six) {
-        System.out.println("6개 일치 (2,000,000,000원) - " + six);
+        System.out.println("6개 일치 (2,000,000,000원) - " + six+ "개");
     }
 
     public static void printRevenue(int[] lotto, int money) {
@@ -45,8 +45,8 @@ public class OutputView {
         printFiveBonusCorrect(lotto[7]);
         printSixCorrect(lotto[6]);
 
-        float revenue = (lotto[3]*5000 + lotto[4]*50000 + lotto[5]*1500000 + lotto[6]*30000000 + lotto[7]*2000000000)/money;
-        System.out.printf("총 수익률은 %.1f%%입니다.", revenue);
+        float revenue = (float)(lotto[3]*5000 + lotto[4]*50000 + lotto[5]*1500000 + lotto[6]*30000000 + lotto[7]*2000000000)/money;
+        System.out.printf("총 수익률은 %.1f%%입니다.", revenue * 100);
     }
 
     public static void printException(String message) {

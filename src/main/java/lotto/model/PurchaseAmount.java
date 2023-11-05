27c47ee62b19d1c497d6abb509f4030d4a6e2271
HoeSeong123 <ghltjd3@gmail.com>
2023-11-05 20:37:18 +0900

    feat(PurchaseAmount) : 구입금액 입력에 대한 예외 처리(1,000원보다 적은 금액)

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -4,11 +4,23 @@ public class PurchaseAmount {
     private int purchaseAmount;
 
     public PurchaseAmount(int purchaseAmount) {
-        validateIsDivisible(purchaseAmount);
+        validate(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
 
-    private static void validateIsDivisible(int input) {
+    private void validate(int input) {
+        validateIsAboveMinimumAmount(input);
+        validateIsDivisible(input);
+    }
+
+
+    private void validateIsAboveMinimumAmount(int input) {
+        if (input < 1000) {
+            throw new IllegalArgumentException("[ERROR] 최소 금액은 1,000원입니다.");
+        }
+    }
+
+    private void validateIsDivisible(int input) {
         if (input % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해야 합니다.");
         }

55faa88cd426eafc84b768cdee3d2473ed3271f5
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-05 16:59:24 +0900

    feat: 구매 금액 1000 미만 예외 처리 추가

--- src/main/java/lotto/validator/MoneyValidator.java
+++ src/main/java/lotto/validator/MoneyValidator.java
@@ -1,44 +1,50 @@
 package lotto.validator;
 
-public class InputMoneyValidator {
+public class MoneyValidator {
 
     private static final String ERROR = "[ERROR]";
     private static final String NULL_ERROR_MESSAGE = "금액을 입력해주세요.";
     private static final String BLANK_ERROR_MESSAGE = "공백 없이 입력해주세요.";
     private static final String NOT_NUMBER_ERROR_MESSAGE = "숫자만 입력해주세요.";
+    private static final String UNDER_1000_MESSAGE = "1,000 이상으로 입력해주세요.";
     private static final String DIVISION_ERROR_MESSAGE = "1,000 단위로 입력해주세요.";
-    private final String money;
 
-    public InputMoneyValidator(String input){
-        money = input;
-        isNull();
-        isContainBlank();
-        isNumError();
-        isDivision();
+    public MoneyValidator(String input){
+        isNull(input);
+        isContainBlank(input);
+        isNumError(input);
+        isUnder1000(input);
+        isDivision(input);
     }
 
-    public void isNull(){
-        if (money.equals("")){
+    public void isNull(String input){
+        if (input.equals("")){
             throw new IllegalArgumentException(ERROR + NULL_ERROR_MESSAGE);
         }
     }
 
-    public void isContainBlank() {
-        if (money.contains(" ")){
+    public void isContainBlank(String input) {
+        if (input.contains(" ")){
             throw new IllegalArgumentException(ERROR + BLANK_ERROR_MESSAGE);
         }
     }
 
-    public void isNumError(){
+    public void isNumError(String input){
         try {
-            Integer.parseInt(money);
+            Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR + NOT_NUMBER_ERROR_MESSAGE);
         }
     }
 
-    public void isDivision(){
-        if (Integer.parseInt(money)%1000 != 0){
+    public void isUnder1000(String input){
+        if (Integer.parseInt(input)<1000){
+            throw new IllegalArgumentException(ERROR + UNDER_1000_MESSAGE);
+        }
+    }
+
+    public void isDivision(String input){
+        if (Integer.parseInt(input)%1000 != 0){
             throw new IllegalArgumentException(ERROR + DIVISION_ERROR_MESSAGE);
         }
     }

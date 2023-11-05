cbabbc1436814c3957a94a87ce5882b22c8d85ec
Dawin <dawin002@naver.com>
2023-11-05 15:08:02 +0900

    feat(RandomGenerator): 랜덤 숫자 생성 기능 추가
    랜덤한 숫자를 생성하는 기능 추가:
    - List<Integer> createNumbers()
    - 1~45 범위의 중복되지 않은 6자리 숫자를 리스트로 반환

---
+++ src/main/java/lotto/model/RandomGenerator.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+
+public class RandomGenerator {
+    public List<Integer> createNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

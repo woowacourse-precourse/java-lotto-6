7a68474f2abd4db31ea8a4d2e8dd80b8b6d90e51
slowcloud <sju9417@gmail.com>
2023-11-05 16:42:13 +0900

    feat: LottoRank에서 상금을 구하는 기능 추가

--- src/main/java/lotto/constant/LottoRank.java
+++ src/main/java/lotto/constant/LottoRank.java
@@ -2,16 +2,17 @@ package lotto.constant;
 
 public enum LottoRank {
 
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    FIVE_BONUS(7);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    FIVE_BONUS(0, 30_000_000),
+    SIX(6, 2_000_000_000);
 
-    private final int winningCount;
+    private final int winningCount, prize;
 
-    LottoRank(int winningCount) {
+    LottoRank(int winningCount, int prize) {
         this.winningCount = winningCount;
+        this.prize = prize;
     }
 
     public static LottoRank findRank(int winningCount, int bonus) {

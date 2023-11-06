1503fc0a792fbfadd6ecd22a12037d84a61c0859
Zeus6768 <akaps6768@gmail.com>
2023-11-06 23:24:52 +0900

    fix: 요구사항에 맞는 출력을 위한 입력 메시지 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,29 +11,29 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.%n";
 
-    private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS = "%n당첨 번호를 입력해 주세요.%n";
     private static final String DELIMITER_COMMA = ",";
 
-    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_NUMBER = "%n보너스 번호를 입력해 주세요.%n";
 
     public int askPurchaseAmount() {
-        System.out.println(ASK_PURCHASE_AMOUNT);
+        System.out.printf(ASK_PURCHASE_AMOUNT);
         String purchaseAmount = readLine();
         validateInteger(purchaseAmount);
         return Integer.parseInt(purchaseAmount);
     }
 
     public List<Integer> askWinningNumbers() {
-        System.out.println(ASK_WINNING_NUMBERS);
+        System.out.printf(ASK_WINNING_NUMBERS);
         String[] numbers = readLine().split(DELIMITER_COMMA);
         validateIntegerArray(numbers);
         return Arrays.stream(numbers).map(Integer::parseInt).sorted().toList();
     }
 
     public int askBonusNumber() {
-        System.out.println(ASK_BONUS_NUMBER);
+        System.out.printf(ASK_BONUS_NUMBER);
         String bonusNumber = readLine();
         validateInteger(bonusNumber);
         return Integer.parseInt(bonusNumber);

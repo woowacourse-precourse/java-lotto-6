0077659e2174e20cb75890e8e913d6e13dcfe050
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2023-11-06 14:52:41 +0900

    feat: validateInteger 기능 추가

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -2,4 +2,5 @@ package lotto;
 
 public class Constant {
     public static final String GET_MONEY_MSG = "구입금액을 입력해 주세요.\n";
+    public static final String INPUT_NUMBER_EXCEPTION_MSG = "[ERROR] 숫자를 입력해야 합니다.\n";
 }
--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import static lotto.Constant.*;
+
 public class Validation {
     public static int validateMoney(String input){
-        return 0;
+        int money = 0;
+        try{
+            money = validateInteger(input);
+            validateDivideBy1000(money);
+        } catch(IllegalArgumentException e){
+            System.out.print(e.getMessage());
+            UI.getMoney();
+        }
+        return money;
+    }
+    private static int validateInteger(String input){
+        int money;
+        try {
+            money = Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(INPUT_NUMBER_EXCEPTION_MSG);
+        }
+        return money;
+    }
+
+    private static void validateDivideBy1000(int money){
+
     }
 }

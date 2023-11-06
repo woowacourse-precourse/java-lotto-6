8f57074b9fac64657de4a229fd136f2667e35449
gumgu <bienlee@naver.com>
2023-11-06 14:44:20 +0900

    [Style] 불필요한 메서드 분리 제거

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class LottoResult {
 
-    private static final Integer DEFAULT_VALUE = 1;
+    private static final Integer DEFAULT_VALUE = 0;
 
     private final Map<Ranking, Integer> results;
 
@@ -27,11 +27,7 @@ public class LottoResult {
     }
 
     private void updateRankingCount(Map<Ranking, Integer> results, Ranking ranking) {
-        results.put(ranking, findValueOrDefault(results, ranking) + 1);
-    }
-
-    private static Integer findValueOrDefault(Map<Ranking, Integer> results, Ranking ranking) {
-        return results.getOrDefault(ranking, DEFAULT_VALUE);
+        results.put(ranking, results.getOrDefault(ranking, DEFAULT_VALUE) + 1);
     }
 
     public Double calculateEarningRatio(Money money) {

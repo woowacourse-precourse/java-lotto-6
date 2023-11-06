9de3796ea63e15c775360293794c1f58c192256d
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 22:42:06 +0900

    refactor: 출력 메세지 리팩토링

--- src/main/java/lotto/view/contants/OutputMessage.java
+++ src/main/java/lotto/view/contants/OutputMessage.java
@@ -3,12 +3,14 @@ package lotto.view.contants;
 public enum OutputMessage {
     BLANK(" "),
     DASH("-"),
+    COMMA(","),
     OPEN_PARENTHESIS("("),
     CLOSE_PARENTHESIS(")"),
 
     WINNING_STATISTICS("당첨 통계\n---"),
     SAME("%s개 일치"),
-    AMOUNT("(%s원)"),
+    BONUS("보너스 볼 일치"),
+    AMOUNT("%s원"),
     COUNT("%s개"),
     RATE_OF_RETURN("총 수익률은 %s%%입니다.");
 
@@ -21,6 +23,4 @@ public enum OutputMessage {
     public String getValue() {
         return this.message;
     }
-
-
 }

f51a5b4ab2c9f419b29ddbb7e09fec318b247f91
IToriginal <gitshineit@gmail.com>
2023-11-07 01:05:01 +0900

    :white_check_mark: [test] #3 입력값이 정수가 아닌 경우 예외 테스트

--- src/test/java/lotto/util/CensorTest.java
+++ src/test/java/lotto/util/CensorTest.java
@@ -37,9 +37,21 @@ class CensorTest {
         String inputEnter = "";
         String inputSpace = " ";
 
-        assertThrows(IllegalArgumentException.class, () -> Censor.validatePurchaseAmount(inputNull));
-        assertThrows(IllegalArgumentException.class, () -> Censor.validatePurchaseAmount(inputEnter));
-        assertThrows(IllegalArgumentException.class, () -> Censor.validatePurchaseAmount(inputSpace));
+        assertThrows(IllegalArgumentException.class,
+                () -> Censor.validatePurchaseAmount(inputNull));
+        assertThrows(IllegalArgumentException.class,
+                () -> Censor.validatePurchaseAmount(inputEnter));
+        assertThrows(IllegalArgumentException.class,
+                () -> Censor.validatePurchaseAmount(inputSpace));
+    }
+
+    @DisplayName("입력 값이 정수가 아니라면, 예외가 발생한다.")
+    @Test
+    void validatePurchaseAmountMethodNumericTest() {
+        String input = "팔천원";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> Censor.validatePurchaseAmount(input));
     }
 
 }

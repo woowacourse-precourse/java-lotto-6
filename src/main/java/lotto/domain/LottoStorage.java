f3c87506872e010b070f40d6e982f34c3fd23963
kimdoha <kkhhjj3393@gmail.com>
2023-11-06 21:10:39 +0900

    feat(LottoStorage): 구입 금액 기반으로 로또 발행 개수(=티켓) 반환

--- src/main/java/lotto/domain/LottoStorage.java
+++ src/main/java/lotto/domain/LottoStorage.java
@@ -1,15 +1,29 @@
 package lotto.domain;
 
 
+import lotto.common.Constant;
+import lotto.common.ErrorMessage;
+
 public class LottoStorage {
 
     private final WinningLotto winningLotto;
     private final Integer totalPrice;
 
-    public LottoStorage(WinningLotto winningLotto, Integer totalPrice) {
+
+    public LottoStorage(final WinningLotto winningLotto, final Integer totalPrice) {
+        validate(totalPrice);
+
         this.winningLotto = winningLotto;
         this.totalPrice = totalPrice;
     }
 
+    public Integer getTicketCount() {
+        return this.totalPrice / Constant.LOTTO_TICKET_PRICE;
+    }
 
+    private void validate(final Integer totalPrice) {
+        if (totalPrice % Constant.LOTTO_TICKET_PRICE > 0) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_PURCHASE_AMOUNT_INVALID_ERROR.getMessage());
+        }
+    }
 }

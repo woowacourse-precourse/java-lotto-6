3ddfd5d5aeacb08d340725069fdbc67fd674e785
JiEung2 <https://github.com/JiEung2>
2023-11-06 22:24:52 +0900

    Add: Constants 추가
    숫자에 대한 패턴과 숫자가 아닐 때 에러 메시지 추가

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -9,7 +9,9 @@ public class Constants {
     public static final String WINNING_NUMBER_DUPLICATE_ERROR = "각각 다른 숫자를 입력해야 합니다.";
     public static final String WINNING_NUMBER_STRING_ERROR = "당첨 번호는 '1,2'와 같이 숫자쉼표숫자의 형태로 입력해야하고 총 6개의 숫자를 입력해야 합니다.";
     public static final String WINNING_NUMBER_RANGE_ERROR = "당첨 번호는 1부터 45사이의 숫자를 입력해야 합니다.";
+    public static final String WINNING_NUMBER_ERROR = "당첨 번호는 숫자를 입력해야 합니다.";
     public static final Pattern BUY_PRICE_PATTERN = Pattern.compile("^[0-9]*$");
+    public static final Pattern WINNING_NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
     public static final Pattern WINNING_NUMBER_RANGE = Pattern.compile("^[0-9]|[1-3][0-9]|4[0-5]*$");
     public static final Pattern WINNING_NUMBER_STRING_PATTERN = Pattern.compile(
             "^[a-zA-Z0-9!@#$%^&*()_+]+(,[a-zA-Z0-9!@#$%^&*()_+]+){5}$");

6e1d2c1c919d119628169f75ec5274db7cb206fc
King's meow <mingee641@gmail.com>
2023-11-06 17:21:25 +0900

    feat: 입력받은 당첨번호 검증 및 형변환을 위한 initWinningNumbers() 메서드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,15 +1,36 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.util.Validator;
 
 
 public class LottoService {
+
+    private List<Integer> winningNumbers;
 
 
 
     public int initMoneyToCount(String inputMoney) {
         int money = Validator.isNumber(inputMoney);
         return Validator.isDivide(money);
+    }
+
+
+    public List<Integer> initWinningNumbers(String inputWinningNumbers) {
+        String[] inputNumbers = inputWinningNumbers.split(",");
+
+        winningNumbers = new ArrayList<>();
+
+        for (String inputNumber : inputNumbers) {
+            int num = Validator.isNumber(inputNumber);
+            Validator.isSmallAndBig(num);
+            winningNumbers.add(num);
+        }
+
+        Validator.isSize(winningNumbers);
+        Validator.isDuplicated(winningNumbers);
+        return winningNumbers;
     }
 
 

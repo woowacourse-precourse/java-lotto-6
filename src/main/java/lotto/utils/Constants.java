012445dd4f5dfcbef420efb0c10e78ffc95b5a21
donghyun <kdhdh001@naver.com>
2023-11-07 00:43:54 +0900

    refactor: ErrorMessage 클래스 삭제, Constants 클래스 추가

---
+++ src/main/java/lotto/utils/Constants.java
@@ -0,0 +1,8 @@
+package lotto.utils;
+
+public class Constants {
+    public static final String ERROR = "[ERROR] ";
+    public static final String NON_NUMERIC_MSG = ERROR + "구입금액은 숫자로 입력해야 합니다.";
+    public static final String NOT_DIVISIBLE_MSG = ERROR + "구입금액이 로또 가격인 1000원으로 나누어 떨어져야 합니다.";
+
+}

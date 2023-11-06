c05bf56e979e9533fc6673bb6d7c4316a0119066
KimKyuHwan <vkdls754@gmail.com>
2023-11-06 17:28:38 +0900

    feat(Lotto): 범위, 중복 validation 기능 추가 및 패키지 변경

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateNumberOfNumbers(numbers);
+        validateUniqueNumbers(numbers);
+    }
+
+    private void validateNumberOfNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+}

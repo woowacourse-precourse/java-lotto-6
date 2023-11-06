099f2ba683512e5d733ab19beaa66d4e5742e802
hyeonwoo <ziczin7176@gmail.com>
2023-11-06 23:54:36 +0900

    test: 구입 금액 검증 테스트

--- src/test/java/lotto/validationTest/InputValidationTest.java
+++ src/test/java/lotto/validationTest/InputValidationTest.java
@@ -19,4 +19,14 @@ public class InputValidationTest {
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력값이 숫자가 아닙니다.");
     }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우")
+    public void isThousandMultipleTest() {
+        assertThatThrownBy(() -> {
+            inputValidation.isThousandMultiple("5500");
+        })
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액이 1000원 단위가 아닙니다.");
+    }
 }

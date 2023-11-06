1c16f1cbe6f53ee42abc74ba8b7ac0d169b8a7ba
DaeYoungK <hanguk830@naver.com>
2023-11-07 01:02:54 +0900

    feat(Validator): 입력된 데이터에 대한 검증 기능 추가

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -1,19 +1,57 @@
 package lotto.validation;
 
-import lotto.domain.ErrorMessage;
+import static lotto.domain.ErrorMessage.*;
 
 public class Validator {
 
-    public int validNumber(String number) {
-        return checkType(number);
+    private static final int LOTTO_PRICE = 1000;
+
+    public int validWinning(String winningNumber) {
+        int convertWinning = checkType(winningNumber);
+        checkRange(convertWinning);
+
+        return convertWinning;
     }
 
-    private int checkType(String number) {
+    public int validBonus(String bonus) {
+        int convertBonus = checkType(bonus);
+        checkRange(convertBonus);
+
+        return convertBonus;
+    }
+
+    public int validPurchase(String amount) {
+        int convertAmount = checkType(amount);
+        checkRemain(convertAmount);
+
+        return convertAmount;
+    }
+
+    public void checkRange(int number) {
+        if (isLottoRange(number)) {
+            return;
+        }
+        getDescription(LOTTO_RANGE);
+        throw new IllegalArgumentException();
+    }
+
+    private boolean isLottoRange(int number) {
+        return number > 0 && number < 46;
+    }
+
+    public void checkRemain(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            getDescription(PRICE_REMAIN);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int checkType(String number) {
         Integer resultNumber;
         try {
             resultNumber = Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            ErrorMessage.getDescription(ErrorMessage.TYPE_INT);
+            getDescription(TYPE_INT);
             throw new IllegalArgumentException(e);
         }
         return resultNumber;

6525e9a7e26490da088753fe0dc950f411e85f47
minisyu <iswater.kim@gmail.com>
2023-11-06 21:49:25 +0900

    refactor: InputValidatorTest validator 패키지로 이동

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -1,8 +1,7 @@
-package lotto.view;
+package lotto.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.validator.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;

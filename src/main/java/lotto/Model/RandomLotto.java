0315f54481ce040980ddb2bbd8c83f63d5e01e27
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-06 17:42:48 +0900

    feat(RandomLotto): 발행할 랜덤로또개수 세는 기능 구현

--- src/main/java/lotto/Model/RandomLotto.java
+++ src/main/java/lotto/Model/RandomLotto.java
@@ -1,24 +1,13 @@
 package lotto.Model;
 
-import java.util.List;
-
 public class RandomLotto {
     private int count;
-    private List<Lotto> randomLottos;
 
-
-    public RandomLotto(Price price, Lottos lottos) {
-        count = price.divideWithOneThousands();
-        this.randomLottos = lottos.createLottos(count);
+    public RandomLotto(Price price) {
+        this.count = price.divideWithOneThousands();
     }
 
     public int getCount() {
         return count;
     }
-
-    public List<Lotto> getRandomLottos() {
-        return randomLottos;
-    }
-
-
 }

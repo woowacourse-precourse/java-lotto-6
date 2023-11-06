275369e35bf092324bb14232dad8fccf0a0ef312
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-06 21:39:57 +0900

    feat(LottoMachine): interpret requirements to enum variable

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,5 +1,21 @@
 package lotto.domain;
 
 public enum Rank {
-    // Rank 상수를 저장한다
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private int count;
+    private boolean bonus;
+
+    private long award;
+
+    Rank(int count, boolean bonus, long award){
+        this.count = count;
+        this.bonus = bonus;
+        this.award = award;
+    }
 }

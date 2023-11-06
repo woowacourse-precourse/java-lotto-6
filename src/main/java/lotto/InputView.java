8cf7b8a6bdd38fdc2b01ca9aa5b4fb71d15bfe38
hyeonjilee <skylar12200@gmail.com>
2023-11-06 09:11:33 +0900

    feat(NumberValidator) : Integer 범위 유효성 검증 기능 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,10 +3,10 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String inputPurchase() {
+    public static int inputPurchase() {
         String rawPurchase = input("구입금액을 입력해 주세요.");
         validateNumber(rawPurchase);
-        return rawPurchase;
+        return Integer.parseInt(rawPurchase);
     }
 
     private static void validateNumber(String value) {
--- src/main/java/lotto/NumberValidator.java
+++ src/main/java/lotto/NumberValidator.java
@@ -3,12 +3,13 @@ package lotto;
 import java.util.regex.Pattern;
 
 public class NumberValidator implements Validator {
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
 
     @Override
     public void validate(Object value) {
         validateNull((String) value);
         validateNumber((String) value);
+        validateRange((String) value);
     }
 
     private void validateNull(String value) {
@@ -20,6 +21,14 @@ public class NumberValidator implements Validator {
     private void validateNumber(String value) {
         if (!NUMBER_PATTERN.matcher(value).matches()) {
             throw new IllegalArgumentException("숫자로 입력해야합니다.");
+        }
+    }
+
+    private void validateRange(String rawValue) {
+        try {
+            Integer.parseInt(rawValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정상적인 범위 내의 숫자를 입력해야합니다.");
         }
     }
 

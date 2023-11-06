0a7f36899d4c75cbe7759adc3d2ab4076d84045d
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-06 13:44:56 +0900

    feat: 당첨 번호 정수 외 값 입력시 예외처리

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -8,15 +8,22 @@ public class WinningNumber {
     private List<Integer> winningNumber;
     private static final String DELIMITER = ",";
     private static final String OUT_RANGE_ERROR_MESSAGE = "[ERROR] 범위를 벗어난 숫자 입니다.";
+    private static final String NULL_INPUT_ERROR_MESSAGE = "[ERROR] 금액을 정확히 입력해 주세요.";
+    private static final String NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해 주세요.";
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
 
     public WinningNumber(String userValue) {
         validateNullValue(userValue);
-        this.winningNumber = Arrays
-                .stream(userValue.split(DELIMITER))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        try {
+            this.winningNumber = Arrays
+                    .stream(userValue.split(DELIMITER))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
+        }
         validInRangeNumber();
     }
 
@@ -29,7 +36,7 @@ public class WinningNumber {
     }
 
     void validateNullValue(String value) {
-        if (value.isBlank()) throw new IllegalArgumentException("[ERROR] 금액을 정확히 입력해 주세요.");
+        if (value.isBlank()) throw new IllegalArgumentException(NULL_INPUT_ERROR_MESSAGE);
     }
 
     public List<Integer> getWinningNumber(){

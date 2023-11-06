63de6aef122aebda1fb5abca234c592742cca323
ldw <leehan02@hanyang.ac.kr>
2023-11-07 02:07:47 +0900

    test(lotto): 당첨 번호와 일치하는 숫자가 몇 개인지 계산한 결과를 집계하는 기능에 결과가 2등인 경우에 대한 단위 테스트 추가

--- src/main/java/lotto/model/Aggregator.java
+++ src/main/java/lotto/model/Aggregator.java
@@ -1,8 +1,6 @@
 package lotto.model;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Aggregator {
     private final List<Integer> winningNumbers;
--- src/test/java/lotto/model/AggregatorTest.java
+++ src/test/java/lotto/model/AggregatorTest.java
@@ -9,29 +9,46 @@ import java.util.List;
 public class AggregatorTest {
     @Test
     void result_6개_당첨_1개와_0개_당첨_1개_인경우() {
-        Aggregator aggregator = new Aggregator();
-
         List<Lotto> lottos = new ArrayList<>();
         lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
         lottos.add(new Lotto(List.of(11,12,13,14,15,16)));
 
         List<Integer> winningNumbers = List.of(1,2,3,4,5,6);
+        int bonusNumber = 39;
 
-        List<Integer> result = aggregator.result(lottos, winningNumbers);
+        Aggregator aggregator = new Aggregator(winningNumbers, bonusNumber);
+
+        List<Integer> result = aggregator.result(lottos);
         Assertions.assertThat(result).isEqualTo(List.of(6, 0));
     }
 
     @Test
-    void result_구입한_로또_번호들_중에서_일치하는_번호가_하나도_없는_경우() {
-        Aggregator aggregator = new Aggregator();
+    void result_당첨_번호와_5개가_일치하고_보너스번호가_일치하는_경우() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,40,41,42,43,44)));
+        lottos.add(new Lotto(List.of(11,12,13,14,15,16)));
 
+        List<Integer> winningNumbers = List.of(40,41,42,43,44,45);
+        int bonusNumber = 1;
+        Aggregator aggregator = new Aggregator(winningNumbers, bonusNumber);
+
+
+        List<Integer> result = aggregator.result(lottos);
+        Assertions.assertThat(result).isEqualTo(List.of(Rule.SECOND_RANK.value(), 0));
+    }
+
+    @Test
+    void result_구입한_로또_번호들_중에서_일치하는_번호가_하나도_없는_경우() {
         List<Lotto> lottos = new ArrayList<>();
         lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
         lottos.add(new Lotto(List.of(11,12,13,14,15,16)));
 
         List<Integer> winningNumbers = List.of(40,41,42,43,44,45);
+        int bonusNumber = 39;
+        Aggregator aggregator = new Aggregator(winningNumbers, bonusNumber);
 
-        List<Integer> result = aggregator.result(lottos, winningNumbers);
+
+        List<Integer> result = aggregator.result(lottos);
         Assertions.assertThat(result).isEqualTo(List.of(0, 0));
     }
 }

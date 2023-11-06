308e921662b79b861a481ed810fe0a586d475030
jang010505 <jang010505@inu.ac.kr>
2023-11-07 07:54:00 +0900

    feat: Lotto에 toString 추가 및 validate 검증 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import static lotto.constant.ErrorMessageConstant.DUPLICATE_NUMBER_ERROR_MESSAGE;
 import static lotto.constant.SystemConstant.NUMBER_COUNT;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,12 +15,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        numbers = numbers.stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+
         if (numbers.size() != NUMBER_COUNT) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE.getErrorMessage());
         }
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 }

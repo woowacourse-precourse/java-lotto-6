f792b6925e554986b00ac2f2b874e266fd1893a8
jun <ggprgrkjh@naver.com>
2023-11-06 20:53:26 +0900

    refactor(lottochecker): createResult 로직 lottochecker 클래스를 통해 분리

--- src/main/java/lotto/model/Game.java
+++ src/main/java/lotto/model/Game.java
@@ -13,35 +13,8 @@ public class Game {
     }
 
     public int[] createResult(Lottos lottos, List<Integer> winningNumbers, int bonusNumber) {
-        int[] result = new int[6];
-        for (Lotto lotto : lottos.getLottos()) {
-            int index = checkAnswer(lotto.compareLottoNumbers(winningNumbers),
-                    lotto.compareBonusNumber(bonusNumber));
-            result[index]++;
-        }
-        return result;
-    }
-
-
-    public int checkAnswer(long count, boolean bonus) {
-        int index = 0;
-
-        if (count == 3) {
-            index = 1;
-        }
-        if (count == 4) {
-            index = 2;
-        }
-        if (count == 5 && !bonus) {
-            index = 3;
-        }
-        if (count == 5 && bonus) {
-            index = 4;
-        }
-        if (count == 6 && !bonus) {
-            index = 5;
-        }
-        return index;
+        LottoChecker lottoChecker = new LottoChecker();
+        return lottoChecker.createResult(lottos, winningNumbers, bonusNumber);
     }
 
 

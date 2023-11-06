9049c647a660c9f4e51a5cc86a5cb7b0901f05a8
le2sky <leehaneul990623@gmail.com>
2023-11-07 05:34:20 +0900

    feat : 에러 메시지 출력 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -12,6 +12,7 @@ class OutputView {
     private static final String PRINT_ISSUANCE_RESULT_SIZE_FORMAT = "%n%d개를 구매했습니다.%n";
     private static final String PRINT_EACH_PRIZE_RESULT_FORMAT = "%s (%s) - %d개%n";
     private static final String PRINT_EARNING_RATE_FORMAT = "총 수익률은 %s%%입니다.";
+    private static final String PRINT_ERROR_MESSAGE_FORMAT = "[ERROR] %s%n";
     private static final String PRINT_STATISTICS_HEADER = "당첨 통계";
     private static final String PRINT_STATISTICS_HEADER_DIVIDER = "---";
 
@@ -67,5 +68,9 @@ class OutputView {
 
     public static void printEarningRate(double earningRate) {
         System.out.format(PRINT_EARNING_RATE_FORMAT, earningRate);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.format(PRINT_ERROR_MESSAGE_FORMAT, message);
     }
 }

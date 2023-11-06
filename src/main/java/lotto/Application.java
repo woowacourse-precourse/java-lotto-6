d167d78b34135425023abe5aa39082803ff7e604
prao <pch5521@gmail.com>
2023-11-06 19:39:57 +0900

    feat(Application): 게임 실행
    - Game.start()를 통해 게임 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.controller.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game.start();
     }
 }

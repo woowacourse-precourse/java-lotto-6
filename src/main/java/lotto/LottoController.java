e9f8374576568a8636a4a4ff826f332da160d050
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-07 08:29:19 +0900

    feat : 예외가 발생하면 구입금액을 재입력 받는 기능 추가

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,34 @@
+package lotto;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
+    }
+
+    public void play() {
+        settingBuyMoney();
+    }
+
+    private void settingBuyMoney() {
+        int inputMoney = inputPlayerBuyMoney();
+        lottoService.settingBuyMoney(inputMoney);
+        outputView.printLottoTicket(inputMoney);
+    }
+
+    private int inputPlayerBuyMoney() {
+        try {
+            outputView.printRequestInputBuyMoney();
+            String readLottoBuyMoney = inputView.readLottoBuyMoney();
+            return lottoService.calculateLottoTicket(readLottoBuyMoney);
+        } catch (IllegalArgumentException exception) {
+            outputView.printExceptionMessage(exception.getMessage());
+            return inputPlayerBuyMoney();
+        }
+    }
+}

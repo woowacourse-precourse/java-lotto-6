a9735fd56ccbc37a98bfa3ff4466bb3a17f1101e
JoonKyo <joonkyolee@gmail.com>
2023-11-06 23:02:21 +0900

    feat(Lotto): 로또 번호에 대한 유효성을 검증하는 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,14 @@
-package lotto;
+package lotto.domain;
+
+import static lotto.exception.ErrorMessage.LOTTO_NUMBER_HAS_DUPLICATED_NUMBER;
+import static lotto.exception.ErrorMessage.LOTTO_NUMBER_HAS_OUT_OF_BOUND_NUMBER;
+import static lotto.exception.ErrorMessage.LOTTO_SIZE_IS_NOT_VALID;
+import static lotto.constraint.LottoConstraint.TOTAL_NUMBERS_OF_LOTTO;
+import static lotto.constraint.LottoConstraint.LOTTO_MINIMUM_BOUND;
+import static lotto.constraint.LottoConstraint.LOTTO_MAXIMUM_BOUND;
 
 import java.util.List;
+import lotto.exception.Exception;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +19,41 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateLottoSize(numbers);
+        validateDuplicatedNumber(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (!isValidLottoSize(numbers)) {
+            throw Exception.from(LOTTO_SIZE_IS_NOT_VALID);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        if (hasDuplicatedNumber(numbers)) {
+            throw Exception.from(LOTTO_NUMBER_HAS_DUPLICATED_NUMBER);
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers){
+        if(!isInRange(numbers)){
+            throw Exception.from(LOTTO_NUMBER_HAS_OUT_OF_BOUND_NUMBER);
+        }
+    }
+
+    private boolean isValidLottoSize(List<Integer> numbers) {
+        return numbers.size() == TOTAL_NUMBERS_OF_LOTTO.getValue();
+    }
+
+    private boolean hasDuplicatedNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != TOTAL_NUMBERS_OF_LOTTO.getValue();
+    }
+
+    private boolean isInRange(List<Integer> numbers) {
+        return numbers.stream()
+                .allMatch(number -> number <= LOTTO_MINIMUM_BOUND.getValue() && number <= LOTTO_MAXIMUM_BOUND.getValue());
+    }
 }

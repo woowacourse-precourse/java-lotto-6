9a24b3abe162dedf31040653a4a86f78cebcfd95
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-06 21:56:53 +0900

    feat(LottoController) : 게임 시작 기능 구현

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+public class LottoGameController {
+
+    public void playGame() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String s = Console.readLine();
+    }
+
+}

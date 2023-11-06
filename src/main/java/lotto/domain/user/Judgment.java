cb0a461fb372a90116e0bc5f0d0108982ce379a8
dmdrk1414 <seungchan141414@gmail.com>
2023-11-06 21:42:22 +0900

    [Refactor(lotto)] 상수 이름 변경

--- src/main/java/lotto/domain/user/Judgment.java
+++ src/main/java/lotto/domain/user/Judgment.java
@@ -5,8 +5,9 @@ import lotto.domain.num.LottoTargetNumResults;
 
 public class Judgment {
     // TODO: 11/6/23 상수 관리
-    private static final Integer SIZE_LOTTO = 6;
+    private static final Integer SIZE_LOTTO_NUMBERS = 6;
     private static final Integer BONUS_NUM_INDEX = 6;
+    private static final Integer COUNT_NUMBER = 1;
     private LottoTargetNumResults lottoTargetNumResults;
 
     public Judgment(LottoTargetNumResults lottoTargetNumResults) {
@@ -17,11 +18,11 @@ public class Judgment {
         Integer countSameWinNum = 0;
         Integer numOfLotto = 0;
 
-        for (int i = 0; i < SIZE_LOTTO; i++) {
+        for (int i = 0; i < SIZE_LOTTO_NUMBERS; i++) {
             numOfLotto = lotto.getNumber(i);
 
             if (lottoTargetNumResults.isSameWinNums(numOfLotto)) {
-                countSameWinNum = countSameWinNum + 1;
+                countSameWinNum = countSameWinNum + COUNT_NUMBER;
             }
         }
         return countSameWinNum;
@@ -30,7 +31,7 @@ public class Judgment {
     public Boolean isSameBonusNum(Lotto lotto) {
         Integer numOfLotto = 0;
 
-        for (int i = 0; i < SIZE_LOTTO; i++) {
+        for (int i = 0; i < SIZE_LOTTO_NUMBERS; i++) {
             numOfLotto = lotto.getNumber(i);
 
             if (lottoTargetNumResults.isSameBonusNum(numOfLotto, BONUS_NUM_INDEX)) {

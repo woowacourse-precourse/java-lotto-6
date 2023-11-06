ac533bd4211dd2ede7df1a88bfbeec1c249ddb65
AJN <buzz781@gmail.com>
2023-11-06 18:01:19 +0900

    refactor : validate 메소드명 수정

--- src/main/java/lotto/model/InputData.java
+++ src/main/java/lotto/model/InputData.java
@@ -11,8 +11,8 @@ public class InputData {
 		while(!isValidInput) {
 			try {
 				inputData = Integer.parseInt(Console.readLine());
-				Validate.isMinimumAmount(inputData);//최소금액검사
-				Validate.isThousandMultiple(inputData);//1000단위인지
+				Validate.minimumAmount(inputData);//최소금액검사
+				Validate.thousandMultiple(inputData);//1000단위인지
 				isValidInput = true;
 			} catch (IllegalArgumentException error) {
 				System.out.println(error.getMessage());
@@ -28,7 +28,7 @@ public class InputData {
 		while(!isValidInput) {
 			try {
 				inputData = Console.readLine();
-				Validate.isValidSixNumberInput(inputData);
+				Validate.validSixNumberInput(inputData);
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
--- src/main/java/lotto/validate/Validate.java
+++ src/main/java/lotto/validate/Validate.java
@@ -4,15 +4,18 @@ import java.util.*;
 
 public class Validate {
 	//입력받은 숫자가 1000단위인지
-	public static void isThousandMultiple(int inputData) {
+	public static void thousandMultiple(int inputData) {
 		if(inputData % 1000 !=0) throw new IllegalArgumentException("[ERROR] 1000단위의 숫자로 입력 해주세요.");
 	}
 	//입력받은 숫자가 1000미만인지
-	public static void isMinimumAmount(int inputData) {
+	public static void minimumAmount(int inputData) {
 	    if (inputData < 1000) throw new IllegalArgumentException("[ERROR] 1000이상의 숫자로 입력 해주세요.");
 	}
+	//1~45의 숫자가 맞는지
+	 
+	
 	//6개의 숫자를 중복없이 입력한게 맞는지
-	public static void isValidSixNumberInput(String inputData) {
+	public static void validSixNumberInput(String inputData) {
 		Set<Integer> sixNumber = new HashSet<>();
 		for(String number : inputData.split(",")) {
 			sixNumber.add(Integer.parseInt(number));

9478024e24033cb71be31ecf5f97b39d8bf7e720
kes02 <hijin1220@naver.com>
2023-11-06 19:32:57 +0900

    Test: Lotto Test
    6개의 로또 번호의 개수, 문자, 공백에 따른 Exception 처리
    - 숫자가 아닌 것이 입력된 경우를 처리

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,56 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 인풋_테스트() {
+        assertSimpleTest(() -> {
+            run("8000", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains("8개를 구매했습니다.");
+        });
+    }
+
+    @Test
+    void 리스트_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("4000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "4개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 0.0%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42)
+        );
+    }
+
+    @Test
+    void 숫자가_아닌게_입력된_경우 () {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,a,5,6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스숫자가_아닌게_입력된_경우 () {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "a");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +25,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 개수가 부족하면 예외가 발생한다.")
+    @Test
+    void createLottoByLowSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 범위가 다른 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNotRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2, 49, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
     // 아래에 추가 테스트 작성 가능
 }

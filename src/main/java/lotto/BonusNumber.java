0843238aa78cdf0b97e7439049d713cd8203826f
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-06 17:57:33 +0900

    feat(lotto): 보너스 번호가 범위를 벗어나면 예외처리

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -9,6 +9,7 @@ public class BonusNumber {
 
     public BonusNumber(String number, List<Integer> lottoNumbers) {
         validate(number, lottoNumbers);
+        validateNumberRange(Integer.parseInt(number));
         this.number = number;
         this.lottoNumbers = lottoNumbers;
     }
@@ -16,6 +17,12 @@ public class BonusNumber {
     public void validate(String number, List<Integer> lottoNumbers){
         if (lottoNumbers.contains(Integer.parseInt(number))){
             throw new IllegalArgumentException("[ERROR] 로또 번호와 중복입니다.");
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45사이의 숫자가 아닙니다.");
         }
     }
 }

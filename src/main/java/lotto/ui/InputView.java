9aec2184cf926bb40bd3241dc0455ce55a020dd7
dj <fjzl12@naver.com>
2023-11-06 19:53:26 +0900

    refactor(InputView): 입력 단계에서 출력만을 담당하도록 수정

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -7,20 +7,17 @@ import static lotto.domain.Message.INPUT_BONUS_NUMBERS;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String readCost() {
+    public static void readCost() {
         System.out.println(INPUT_COST.getMessage());
-        return Console.readLine();
     }
 
 
-    public static String readNumbers() {
+    public static void readNumbers() {
         System.out.println(INPUT_NUMBERS.getMessage());
-        return Console.readLine();
     }
 
-    public static String readBonusNumber() {
+    public static void readBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBERS.getMessage());
-        return Console.readLine();
     }
 
 }

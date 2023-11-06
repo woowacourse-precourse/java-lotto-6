db9ac35722c8907710f282097c1e18e9c6f92111
Sung Hyobin <hellmir610@gmail.com>
2023-11-07 05:53:36 +0900

    feat: Add input funciton to choose purchase amount of lotto

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,9 +1,11 @@
 package lotto.controller;
 
+import lotto.view.InputHandler;
 import lotto.view.Printer;
 
 public class LottoGameController {
     public static void startLottoGame() {
         Printer.printPurchaseAmountChoiceMessage();
+        InputHandler.getInputMessage();
     }
 }
---
+++ src/main/java/lotto/view/InputHandler.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public static String getInputMessage() {
+        return Console.readLine();
+    }
+}

fd1aaafe6107f6010b791382cb02d982a563a758
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-06 18:51:56 +0900

    feat: 로또 당첨 통계 및 수익률 출력 로직 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,10 @@
 package lotto.view;
 
-import lotto.model.Lotto;
-import lotto.model.LottoNumber;
-import lotto.model.LottoPurchaseManager;
+import lotto.model.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
@@ -22,5 +22,44 @@ public class OutputView {
 
             System.out.println(numberValues);
         }
+    }
+
+    public static void outputResult(LottoPurchaseManager lottoPurchaseManager,
+                                    WinningLotto winningLotto) {
+        List<Lotto> lottoTickets = lottoPurchaseManager.getLottoTickets();
+
+        Map<LottoResult, Integer> winningStatisticsCount = new HashMap<>();
+
+        for (Lotto ticket : lottoTickets) {
+
+            LottoResult lottoResult = winningLotto.compareWithWinningNumbers(ticket.getNumbers());
+            winningStatisticsCount.put(lottoResult, winningStatisticsCount.getOrDefault(lottoResult, 0) + 1);
+        }
+
+        printWinningStatistics(winningStatisticsCount);
+        printReturnRate(lottoPurchaseManager);
+    }
+
+    private static void printWinningStatistics(Map<LottoResult, Integer> winningStatisticsCount) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        for (LottoResult lottoResult : LottoResult.values()) {
+            String matchBonus = getMatchBonusString(lottoResult);
+            System.out.println(lottoResult.getMatchCount() + "개 일치" + matchBonus + " (" + lottoResult.getPrizeMoney() + ") - " + winningStatisticsCount + "개");
+        }
+    }
+
+    private static void printReturnRate(LottoPurchaseManager lottoPurchaseManager) {
+        double returnRate = lottoPurchaseManager.calculateProfitRate();
+        System.out.println("총 수익률은 " + String.format("%.1f", returnRate) + "%입니다.");
+    }
+
+    private static String getMatchBonusString(LottoResult lottoResult) {
+        if (lottoResult.isMatchBonus()) {
+            return ", 보너스 볼 일치";
+        }
+
+        return "";
     }
 }

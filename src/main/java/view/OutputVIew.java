c71f7b1daf1dd36cc87e6ff9d827de799d147d2c
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 20:17:44 +0900

    feat(OutputView.java): add printWinningResultTitle function 당첨 결과 타이틀을 출력하는 기능을 구현하였습니다.

--- src/main/java/view/OutputVIew.java
+++ src/main/java/view/OutputVIew.java
@@ -3,9 +3,11 @@ package view;
 public class OutputVIew {
 
     static private int earningRate;
+    static final private String lineSeparator = System.lineSeparator();
     static final String MONEY_INPUT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
     static final String PURCHASING_RESULT_MESSAGE = "개를 구매했습니다.";
     static final String WINNING_NUMBER_INPUT_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
+    static final String WINNING_RESULT_TITLE = "당첨 통계" + lineSeparator + "---" + lineSeparator;
     static final String EARNING_RATE_RESULT_MESSAGE = "총 수익률은" + earningRate + "입니다.";
 
     void  printMoneyInputRequestMessage() {
@@ -18,6 +20,10 @@ public class OutputVIew {
 
     void printWinningNumberInputRequestMessage() {
         System.out.println(WINNING_NUMBER_INPUT_REQUEST_MESSAGE);
+    }
+
+    void printWinningResultTitle() {
+        System.out.println(WINNING_RESULT_TITLE);
     }
 
     void printEarningRateResultMessage(int earningRate) {

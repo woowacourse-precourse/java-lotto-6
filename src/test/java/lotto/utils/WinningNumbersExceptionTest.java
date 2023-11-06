f3cfdbf7cd4452aeeffcb83ff7f368d6c28ce06d
honeydanji <tjdwls3680@naver.com>
2023-11-06 16:10:46 +0900

    TEST : 당첨 번호 입력에 대한 테스트 코드 작성
    // 주어진 상황에서 예외가 잘 발생하는 지 체크
    - 초과 입력 시
    - 숫자가 아닌 경우
    - 입력 범위 벗어난 경우
    - 중복 입력한 경우
    - 쉼표 구분이 없는 경우

---
+++ src/test/java/lotto/utils/WinningNumbersExceptionTest.java
@@ -0,0 +1,40 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WinningNumbersExceptionTest {
+
+    WinningNumbersException winningNumbersException = new WinningNumbersException();
+
+    @Test
+    void 당첨_번호_6개_초과_입력 () {
+        assertThrows(IllegalArgumentException.class, () ->
+                winningNumbersException.exccedsLength("1,2,3,4,5,6"), "[ERROR]입력을 초과했습니다.");
+    }
+
+    @Test
+    void 당첨_번호_숫자가_아닌_경우 () {
+        assertThrows(IllegalArgumentException.class, () ->
+                winningNumbersException.notNumeric("abcd"), "[ERROR]숫자를 입력하세요.");
+    }
+
+    @Test
+    void 당첨_번호_범위_벗어난_경우 () {
+        assertThrows(IllegalArgumentException.class, () ->
+                winningNumbersException.outsideRange("1,2,3,4,5,46"), "[ERROR]입력 범위를 벗어났습니다.");
+    }
+
+    @Test
+    void 당첨_번호_중복_입력 () {
+        assertThrows(IllegalArgumentException.class, () ->
+                winningNumbersException.duplicateNumber("1,1,2,2,3,4"), "[ERROR]입력이 중복되었습니다.");
+    }
+
+    @Test
+    void 당첨_번호_쉼표_없을_경우 () {
+        assertThrows(IllegalArgumentException.class, () ->
+                winningNumbersException.nonComma("1 2 3 4 5 6"), "[ERROR]쉼표로 구분해주세요.");
+    }
+}

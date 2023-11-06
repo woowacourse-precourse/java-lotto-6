abdb3ce641c5c15426d886e0a76b30b3b0857d17
dmdrk1414 <seungchan141414@gmail.com>
2023-11-06 22:15:48 +0900

    [Refactor(lotto)] 메서드의 분리

--- src/main/java/lotto/domain/user/RateResult.java
+++ src/main/java/lotto/domain/user/RateResult.java
@@ -3,23 +3,39 @@ package lotto.domain.user;
 import lotto.utill.Utii;
 
 public class RateResult {
-    public Double calculateRate(Integer useMoney, Integer totalWinMoney) {
-        Double rate = ((double) totalWinMoney / useMoney) * 100;
+    // TODO: 11/6/23 상수 정리
+    private static final Integer ZERO_MONEY = 0;
+    private static final Double ZERO_RATE = 0.0;
 
-        if (Utii.isSameInt(useMoney, 0)) {
-            return 0.0;
+    public Double calculateRate(Integer useMoney, Integer totalWinMoney) {
+        Double rate = getRate(useMoney, totalWinMoney);
+
+        if (isSameZERO(useMoney)) {
+            return ZERO_RATE;
         }
 
-        return Math.round(rate * 10) / 10.0;
+        return roundTwoDecimalPlaces(rate);
     }
 
     public StringBuilder showRate(Integer useMoney, Integer totalWinMoney) {
         StringBuilder result = new StringBuilder();
         Double rate = calculateRate(useMoney, totalWinMoney);
 
-        // TODO: 11/6/23 상수 정리 
+        // TODO: 11/6/23 상수 정리
         result.append("총 수익률은 " + rate + "%입니다.");
 
         return result;
+    }
+
+    private double roundTwoDecimalPlaces(Double rate) {
+        return Math.round(rate * 10) / 10.0;
+    }
+
+    private boolean isSameZERO(Integer useMoney) {
+        return Utii.isSameInt(useMoney, ZERO_MONEY);
+    }
+
+    private double getRate(Integer useMoney, double totalWinMoney) {
+        return (totalWinMoney / useMoney) * 100;
     }
 }

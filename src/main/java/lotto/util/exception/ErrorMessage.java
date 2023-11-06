e65370711eadf0d65f6d01b337a4becfdb7f4c34
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-06 21:24:02 +0900

    refactor: 에러 메세지 수정

--- src/main/java/lotto/util/exception/ErrorMessage.java
+++ src/main/java/lotto/util/exception/ErrorMessage.java
@@ -5,8 +5,7 @@ public enum ErrorMessage {
     INVALID_DIVISIBLE_BY_THOUSAND("1000으로 나누어 떨어져야 합니다."),
     INVALID_ZERO_NUMBER("0이상의 숫자로 입력해야 합니다."),
     INVALID_RANGE("%s 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_LOTTO_COUNT("로또 번호는 6개를 모두 입력해야 합니다."),
-    INVALID_BONUS_NUMBER_COUNT("보너스 넘버는 1개만 입력해야 합니다."),
+    INVALID_LOTTO_COUNT("로또 번호는 6개를 입력해야 합니다."),
     INVALID_DUPLICATE_NUMBER("중복된 번호가 있습니다."),
     INVALID_BLANK("공백은 허용되지 않습니다.");
 

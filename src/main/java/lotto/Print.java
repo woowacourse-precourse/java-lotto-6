24f3d4d2ef5d6dc0f57587739786838085a15f8a
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-06 15:27:24 +0900

    feat : 로또 수량 및 번호 출력

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class Print {
 
+    public void printPurchasedLottos(List<Lotto> lottos) {
+        printNumbersOfLotto(lottos.size());
+        for (Lotto lotto: lottos) {
+            System.out.println(sort(lotto).toString());
+        }
+        System.out.println();
+    }
+
+    private void printNumbersOfLotto(int num) {
+        System.out.println(num + "개를 구매했습니다.");
+    }
+
     List<Integer> sort(Lotto lotto) {
         List<Integer> numbers = new ArrayList<>(lotto.getNumbers());
         numbers.sort(null);

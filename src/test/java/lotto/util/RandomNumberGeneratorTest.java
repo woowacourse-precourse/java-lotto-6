ac994ef6aaaa890284206582b5e593a2c5e77afa
gywns0417 <gywns0417@gmail.com>
2023-11-06 23:16:46 +0900

    style: RandomNumberGeneratorTest 테스트 설명 수정

--- src/test/java/lotto/util/RandomNumberGeneratorTest.java
+++ src/test/java/lotto/util/RandomNumberGeneratorTest.java
@@ -7,7 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
 import java.util.List;
-import lotto.validator.PurchaseAmountValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,26 +22,28 @@ class RandomNumberGeneratorTest {
             success_NumberAmount();
         }
 
-        @DisplayName("랜덤 생성된 번호가 최소 최대 범위를 벗어나지 않으면 예외처리 발생 안함")
+        @DisplayName("랜덤 생성된 번호가 최소 최대 범위를 벗어나지 않으면 통과")
         @Test
         void success_NumberRange() {
             for (int i = 0; i < 100; i++) {
+                // given
                 List<Integer> numbers = RandomNumberGenerator.generate();
                 int min = Collections.min(numbers);
                 int max = Collections.max(numbers);
-
+                // when, then
                 assertThat(min).isGreaterThanOrEqualTo(LOTTO_NUMBER_MIN.getValue());
                 assertThat(max).isLessThanOrEqualTo(LOTTO_NUMBER_MAX.getValue());
             }
         }
 
-        @DisplayName("랜덤 생성된 번호가 6개이면 예외처리 발생 안함")
+        @DisplayName("랜덤 생성된 번호가 6개이면 통과")
         @Test
         void success_NumberAmount() {
             for (int i = 0; i < 100; i++) {
+                // given
                 List<Integer> numbers = RandomNumberGenerator.generate();
                 int length = numbers.size();
-
+                //when, then
                 assertThat(length).isEqualTo(LOTTO_NUMBER_AMOUNT_MAX.getValue());
             }
         }

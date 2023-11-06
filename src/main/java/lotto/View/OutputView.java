c42f49246cde5fc5087f4524b528e863064fe7d7
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-06 19:04:37 +0900

    feat(OutputView): 당첨내역출력, 수익률출력, 당첨내역과 랜덤로또번호간의 결과 출력을 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,6 +1,11 @@
 package lotto.View;
 
+import java.util.Map;
+import lotto.Model.Lottos;
 import lotto.Model.RandomLotto;
+import lotto.Model.Rank;
+import lotto.Model.Rate;
+import lotto.Model.Result;
 
 public class OutputView {
 
@@ -9,8 +14,22 @@ public class OutputView {
         System.out.println(randomLotto.getCount() + "개를 구매했습니다.");
     }
 
-    public static void printRandomLottoNumbers(RandomLotto randomLotto) {
-        randomLotto.getRandomLottos().forEach(System.out::println);
+    public static void printRandomLottoNumbers(Lottos lottos) {
+        lottos.getLottos().forEach(System.out::println);
+    }
+
+    public static void printWinningResult(Result result) {
+        Map<Rank, Integer> results = result.getRankAndCounting();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : results.keySet()) {
+            if(rank.compareTo(Rank.NONE) == 0) continue;
+            System.out.println(Rank.getMessage(rank) + " - " + results.get(rank) + "개");
+        }
+    }
+
+    public static void printWinningRate(double winningRate) {
+        System.out.println("총 수익률은 " + winningRate+"%입니다.");
     }
 }
 

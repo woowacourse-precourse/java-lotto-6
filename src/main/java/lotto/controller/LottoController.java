a8d22a8f5fef30a603b84e6d89d9892827e323d8
SJ70 <seojae70@gmail.com>
2023-11-06 22:01:23 +0900

    Feat: 로또 발행

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,13 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.domain.cash.Cash;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumbersGenerator;
+import lotto.domain.lotto.strategy.PickNumbersStrategy;
+import lotto.domain.lotto.strategy.PickRandomNumbersStrategy;
+import lotto.domain.shop.LottoShop;
+import lotto.dto.LottoNumbersDTO;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -9,7 +16,14 @@ public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
 
+    private final PickNumbersStrategy pickNumbersStrategy = new PickRandomNumbersStrategy();
+    private final LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator(pickNumbersStrategy);
+
+    private final LottoShop lottoShop = new LottoShop();
+
     private Cash purchaseCash;
+    private int lotteriesCount;
+    private List<Lotto> lotteries;
 
     public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
@@ -18,16 +32,28 @@ public class LottoController {
 
     public void run() {
         inputPurchaseCash();
+        generateLotteries();
     }
 
     private void inputPurchaseCash() {
         // 구입금액을 입력해 주세요.
         try {
             purchaseCash = new Cash(inputView.inputNumber());
+            lotteriesCount = lottoShop.countPurchasableAmount(purchaseCash.amount());
         } catch (IllegalArgumentException e) {
             outputView.print(e.getMessage());
             inputPurchaseCash();
         }
+    }
+
+    private void generateLotteries() {
+        // n개를 구매했습니다.
+        List<LottoNumbersDTO> lottoNumbersDTOs = lottoNumbersGenerator.generateByCount(lotteriesCount);
+        lotteries = lottoNumbersDTOs.stream()
+                .map(LottoNumbersDTO::numbers)
+                .map(Lotto::new)
+                .toList();
+        // 발행번호들 출력
     }
 
 }

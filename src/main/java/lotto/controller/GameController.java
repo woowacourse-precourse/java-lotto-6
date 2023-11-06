c8e4797eaa3f9244f0ce4dc2e5bd079e7116cad1
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-06 16:59:11 +0900

    feat: 로또 리스트 객체 생성 및 로또 생성시 로또 리스트로 등록 기능 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -6,14 +6,19 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class GameController {
+    private final LottoMachine lottoMachine;
+    private final Calculator calculator;
+    private final Lottos lottos;
     private final PurchaseAmount purchaseAmount;
     private final int coin;
-    private final LottoMachine lottoMachine;
+
     private WinningNumber winningNumber;
     private BonusNumber bonusNumber;
 
     public GameController() {
         this.lottoMachine = new LottoMachine();
+        this.calculator = new Calculator();
+        this.lottos = new Lottos();
         this.purchaseAmount = new PurchaseAmount(InputView.inputPurchaseAmount());
         this.coin = Parser.parseAmountToCoin(purchaseAmount);
         OutputView.printNumberOfLottoPurchase(coin);
@@ -22,6 +27,7 @@ public class GameController {
     public void run() {
         for (int i = 0; i < coin; i++) {
             Lotto lotto = lottoMachine.createLotto();
+            lottos.addLotto(lotto);
             System.out.println(lotto.toString());
         }
 
---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(){
+        this.lottos = new ArrayList<>();
+    }
+
+    public void addLotto(Lotto lotto){
+        lottos.add(lotto);
+    }
+}

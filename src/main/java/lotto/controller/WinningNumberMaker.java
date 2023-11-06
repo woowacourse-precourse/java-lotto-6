b172be8fbee8667912d6a57ecb47ae9737db64c9
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 19:49:43 +0900

    Feat: 보너스 번호가 당첨 번호와 중복될 경우 오류 발생 기능 구현
    보너스 번호가 당첨 번호와 중복될 경우 오류가 발생하는 validateDuplicationBonusNumber 메서드 구현
    해당 기능의 에러 메세지 출력 기능 구현

--- src/main/java/lotto/controller/WinningNumberMaker.java
+++ src/main/java/lotto/controller/WinningNumberMaker.java
@@ -1,16 +1,22 @@
 package lotto.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import lotto.constant.NumbersOption;
+import lotto.output.ErrorPrinter;
 import lotto.validation.NumbersValidation;
 
 public class WinningNumberMaker {
 
+	private static List<Integer> winningNumber;
+
 	public static List<Integer> make(String inputNumber) {
 
-		List<Integer> winningNumber = Arrays.stream(inputNumber.split(NumbersOption.SEPARATOR))
+		winningNumber = new ArrayList<>();
+
+		winningNumber = Arrays.stream(inputNumber.split(NumbersOption.SEPARATOR))
 			.map(String::trim)
 			.distinct()
 			.peek(NumbersValidation::validateOnlyNumber)
@@ -21,7 +27,6 @@ public class WinningNumberMaker {
 		NumbersValidation.validateSize(winningNumber);
 
 		return winningNumber;
-
 	}
 
 	public static int makeBonusNumber(String inputBonusNumber) {
@@ -29,7 +34,18 @@ public class WinningNumberMaker {
 		NumbersValidation.validateOnlyNumber(inputBonusNumber);
 		NumbersValidation.validateRange(inputBonusNumber);
 
-		return Integer.parseInt(inputBonusNumber);
+		int bonusNumber = Integer.parseInt(inputBonusNumber);
+
+		validateDuplicationBonusNumber(bonusNumber);
+
+		return bonusNumber;
+	}
+
+	private static void validateDuplicationBonusNumber(int bonusNumber) {
+		if(winningNumber.contains(bonusNumber)) {
+			ErrorPrinter.printBonusNumberError();
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
--- src/main/java/lotto/output/ErrorPrinter.java
+++ src/main/java/lotto/output/ErrorPrinter.java
@@ -4,13 +4,14 @@ import lotto.constant.NumbersOption;
 
 public class ErrorPrinter {
 
-	private static final String ERROR_SYMBOL = "[ERROR]";
+	private static final String ERROR_SYMBOL = "[ERROR] ";
 	private static final String ONLY_NUMBER_MESSAGE = "숫자만 입력해주세요";
 	private static final String OUT_OF_RANGE_MESSAGE = NumbersOption.MIN + "~" + NumbersOption.MAX + "의 숫자만 입력해주세요.";
 	private static final String OUT_OF_SIZE_MESSAGE = "중복되지 않는 " + NumbersOption.SIZE + "개의 숫자만 입력해주세요.";
 	private static final String SEPARATOR_ERROR_MESSAGE = "구분자(" + NumbersOption.SEPARATOR + ")만을 이용해 나눠주세요";
 	private static final int LOTTO_PRIZE = 1000;
 	private static final String PURCHASE_UNIT_ERROR_MESSAGE = LOTTO_PRIZE + "원 단위로만 입력해주세요.";
+	private static final String BONUS_NUMBER_DUPLICATION_MESSAGE = "당첨 번호와 보너스 번호에 중복이 있습니다.";
 
 	public static void printOnlyNumberError() {
 		System.out.println(ERROR_SYMBOL + ONLY_NUMBER_MESSAGE);
@@ -30,6 +31,9 @@ public class ErrorPrinter {
 
 	public static void printPurchaseUnitError() {
 		System.out.println(ERROR_SYMBOL + PURCHASE_UNIT_ERROR_MESSAGE);
+	}
+	public static void printBonusNumberError() {
+		System.out.println(ERROR_SYMBOL + BONUS_NUMBER_DUPLICATION_MESSAGE);
 	}
 
 }

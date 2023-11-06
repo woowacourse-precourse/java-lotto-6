503daac8bd9b981890dac91cfdab8b5d4d7af5c8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 15:25:43 +0900

    feat(winningNumber): 당첨 숫자가 1~45 범위에 맞는지 검증

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,21 +1,32 @@
 package lotto;
 
+import static lotto.Exception.INVALID_WINNING_NUMBER_RANGE;
 import static lotto.Exception.WINNING_NUMBERS_SIZE_SHOULD_BE_SIX;
 
 import java.util.List;
 
 public class WinningNumbers {
     private static final int WINNING_NUMBERS_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public WinningNumbers(List<Integer> numbers) {
         validateNumberSize(numbers);
+        numbers.forEach(this::validateNumberRange);
+
         this.numbers = numbers;
     }
 
     private void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != WINNING_NUMBERS_SIZE) {
             throw new IllegalArgumentException(WINNING_NUMBERS_SIZE_SHOULD_BE_SIX.getMessage());
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_RANGE.getMessage());
         }
     }
 }

4d37a9c7066cccb958c3c583a6a153cac1570551
Seyeon <wjdtpdus828@naver.com>
2023-11-07 00:13:48 +0900

    feat(Lotto): 숫자 길이, 범위, 중복에 대한 예외처리를 진행하는 메소드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.constant.Errors;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -7,18 +9,39 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
 
         Collections.sort(numbers);
+
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplication(numbers);
+
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers(){
         return numbers;
     }
-    private void validate(List<Integer> numbers) {
+
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Errors.WINNING_NUMBER_WRONG_SIZE_MESSAGE.getErrorMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for(int i : numbers){
+            if (i < 1 || i > 45){
+                throw new IllegalArgumentException(Errors.WINNING_NUMBER_WRONG_RANGE_MESSAGE.getErrorMessage());
+            }
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers){
+        for(int i = 1; i < numbers.size(); i++){
+            if(numbers.get(i) == numbers.get(i-1)){
+                throw new IllegalArgumentException(Errors.WINNIG_NUMBER_DUPLICATE_MESSAGE.getErrorMessage());
+            }
         }
     }
 

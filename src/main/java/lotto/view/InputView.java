b7a9a195d3254ae3ec096c791d9835f46b9ddcad
waveofmymind <sjun0913@gmail.com>
2023-11-06 22:50:16 +0900

    refactor: ViewConstraints 네이밍 변경에 따른 코드를 수정한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,21 +10,21 @@ public class InputView {
     private static final String DELIMITER = ",";
 
     public static int readPayMoney() {
-        System.out.println(ViewConstraints.INPUT_MONEY.getMessage());
+        System.out.println(ViewMessage.INPUT_MONEY.getMessage());
         String input = Console.readLine();
         System.out.println();
         return Integer.parseInt(input);
     }
 
     public static List<Integer> readWinningNumbers() {
-        System.out.println(ViewConstraints.INPUT_WINNING_NUMBERS.getMessage());
+        System.out.println(ViewMessage.INPUT_WINNING_NUMBERS.getMessage());
         String input = Console.readLine();
         System.out.println();
         return Arrays.stream(input.split(DELIMITER)).map(Integer::parseInt).toList();
     }
 
     public static int readBonusNumber() {
-        System.out.println(ViewConstraints.INPUT_BONUS_NUMBER.getMessage());
+        System.out.println(ViewMessage.INPUT_BONUS_NUMBER.getMessage());
         String input = Console.readLine();
         System.out.println();
         return Integer.parseInt(input);

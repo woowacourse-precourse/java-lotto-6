d80882f96a40da5f0da925c5460cc226b6c4bd7f
고경남 <rhrudska987@naver.com>
2023-11-06 19:39:53 +0900

    docs: README 파일 작성
    로또 예시 출력 결과 및 기능 명세서 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +19,11 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public String printNumbers() {
+        return this.numbers.toString();
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }

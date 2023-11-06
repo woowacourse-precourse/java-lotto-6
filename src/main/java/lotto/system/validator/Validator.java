e378a4addf7b44c71b65d9a1228e80a4e7842a4a
Yeriimii <seojs0511@naver.com>
2023-11-06 13:58:26 +0900

    feat(Validator): 입력받은 값이 아라비아 숫자인지 검증할 수 있다

--- src/main/java/lotto/system/validator/Validator.java
+++ src/main/java/lotto/system/validator/Validator.java
@@ -1,8 +1,13 @@
 package lotto.system.validator;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+    private static final Pattern ARABIC_NUMBER_PATTERN = Pattern.compile("^[0-9]?$");
+
     private enum ExceptionMessage {
-        EMPTY_VALUE_NOT_ALLOWED("빈 값을 입력할 수 없습니다.");
+        EMPTY_VALUE_NOT_ALLOWED("빈 값을 입력할 수 없습니다."),
+        INVALID_ARABIC_NUMBER("아라비아 숫자만 입력할 수 있습니다.");
         private final String message;
 
         ExceptionMessage(String message) {
@@ -13,6 +18,12 @@ public class Validator {
     public void validateNotEmpty(String input) {
         if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException(ExceptionMessage.EMPTY_VALUE_NOT_ALLOWED.message);
+        }
+    }
+
+    public void validateArabicNumber(String input) {
+        if (!ARABIC_NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_ARABIC_NUMBER.message);
         }
     }
 }

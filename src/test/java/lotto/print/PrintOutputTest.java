ac844055973926d6c66059e98cfdbb47476c77a4
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 15:07:32 +0900

    test: 로또 번호 출력 테스트
    로또 번호 출력 테스트

---
+++ src/test/java/lotto/print/PrintOutputTest.java
@@ -0,0 +1,25 @@
+package lotto.print;
+
+import lotto.Lotto;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.print.PrintOutput.printRandomLottosNumbers;
+
+class PrintOutputTest {
+
+    @Test
+    void printRandomLottosNumbersTest() {
+        List<Lotto> lottos = new ArrayList<>();
+        Lotto lotto1=new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto2=new Lotto(List.of(7,8,9,10,11,12));
+        Lotto lotto3=new Lotto(List.of(13,14,15,16,17,18));
+        lottos.add(lotto1);
+        lottos.add(lotto2);
+        lottos.add(lotto3);
+
+        printRandomLottosNumbers(lottos);
+    }
+}

c180f9552d2b63e9f37b9ee3aa401c4e410b9d01
moong2 <www.castlehi@gmail.com>
2023-11-06 21:41:09 +0900

    refactor: add invalid long type exception to enum

--- src/main/java/lotto/model/util/ExceptionMessage.java
+++ src/main/java/lotto/model/util/ExceptionMessage.java
@@ -8,7 +8,9 @@ public enum ExceptionMessage {
     INVALID_INPUT_TYPE("숫자 형식으로 입력해주세요."),
     NOT_POSITIVE_INPUT(MINIMUM_PRICE.getPrice() + "원 이상의 로또 구입 금액을 입력해주세요."),
     BIGGER_THAN_MAX(MAXIMUM_PRICE.getPrice() + "원 이하의 로또 구입 금액을 입력해주세요."),
-    NOT_DIVIDE_BY_LOTTO_PRICE(LOTTO_PRICE.getPrice() + "원 단위의 금액을 입력해주세요.");
+    NOT_DIVIDE_BY_LOTTO_PRICE(LOTTO_PRICE.getPrice() + "원 단위의 금액을 입력해주세요."),
+
+    INVALID_LONG_TYPE("올바른 형식의 숫자를 입력해주세요.");
 
     private final String ERROR_PREFIX = "[ERROR] ";
     private final String message;

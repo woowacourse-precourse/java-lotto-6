a9565123e73e9da441e596d7453971183dcf1580
jhsseonn <hs125bt@gmail.com>
2023-11-06 19:07:21 +0900

    feat: 로또머신 객체 생성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class LottoMachine {
+
+    private int lottoPurchase;
+    private List<Lotto> lottos;
+    private HashMap<LottoRank, Integer> lottoRanks;
+    private int lottoReturn;
+    private int lottoReturnRate;
+
+    public LottoMachine(int lottoPurchase, List<Lotto> lottos, HashMap<LottoRank, Integer> lottoRanks) {
+        this.lottoPurchase = lottoPurchase;
+        this.lottos = lottos;
+        this.lottoRanks = lottoRanks;
+        this.lottoReturn = 0;
+        this.lottoReturnRate = 0;
+    }
+
 }

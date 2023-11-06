7c260ffb8906517396b92758d2c40164232fe49d
JisooPyo <audwl326@gmail.com>
2023-11-07 01:07:29 +0900

    feat(LottoService): 로또를 발행할 때 중복되지 않는 6개의 숫자를 뽑는다.

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,10 +1,14 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class LottoService {
 	Validator validator = new Validator();
+	final int lottoRangeStart = 1;
+	final int lottoRangeEnd = 45;
+	final int countOfLotto = 6;
 
 	public long getNumberOfLottoTickets(String cost) throws IllegalArgumentException {
 		validator.isComposedOfNumbers(cost);
@@ -18,13 +22,12 @@ public class LottoService {
 	public List<Lotto> issueLottos(long numberOfLottoTickets) throws IllegalArgumentException {
 		List<Lotto> lottos = new ArrayList<>();
 		for (int i = 1; i <= numberOfLottoTickets; i++) {
-			List<Integer> numbers = issueLotto();
+			List<Integer> numbers =
+					Randoms.pickUniqueNumbersInRange(lottoRangeStart, lottoRangeEnd, countOfLotto);
 			Lotto lotto = new Lotto(numbers);
+			lottos.add(lotto);
 		}
-		return null;
+		return lottos;
 	}
 
-	private List<Integer> issueLotto() {
-		return null;
-	}
 }

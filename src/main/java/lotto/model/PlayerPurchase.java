076c03af1deb36c99b0927830b86844005ed8c2e
lusate <rayrayr@naver.com>
2023-11-06 17:47:45 +0900

    feat: 구매 금액 검증

--- src/main/java/lotto/model/PlayerPurchase.java
+++ src/main/java/lotto/model/PlayerPurchase.java
@@ -1,12 +1,14 @@
 package lotto.model;
 
+import static lotto.errorMessage.ExceptionErrorMessage.PURCHASE_LOTTO_PRICE;
+
 public class PlayerPurchase {
     private static final int Lotto_Price = 1000;
-    private int inputPrice; // 내가 입력한 금액
+    private final int inputPrice; // 내가 입력한 금액
 
     public PlayerPurchase(String amount) { // 내가 입력했을 때 8000은 문자열
         int lotto_Price = convertToInt(amount);
-        validatePriceCount(lotto_Price);
+        validateLottoPrice(lotto_Price);
         this.inputPrice = lotto_Price;
     }
 
@@ -20,20 +22,10 @@ public class PlayerPurchase {
         return Integer.parseInt(amount);
     }
 
-    private void validatePriceCount(int amount) {
-        validateLottoPrice(amount);
-        validateLottoCount(amount);
-    }
 
     private void validateLottoPrice(int amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("구입 금액은 0보다 커야 한다");
-        }
-    }
-
-    private void validateLottoCount(int amount) {
-        if (amount % Lotto_Price != 0) {
-            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다");
+        if (amount % Lotto_Price != 0 || amount <= 0) {
+            throw new IllegalArgumentException(PURCHASE_LOTTO_PRICE);
         }
     }
 }

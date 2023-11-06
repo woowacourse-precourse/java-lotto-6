85ebcb89f5b9f5fa99cf3a48bdbc7eb92b039def
Hyun Woo <hwsa10041@gmail.com>
2023-11-07 01:34:26 +0900

    feat : 로또 구매 프로세스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.Jackpot;
+import lotto.model.Lotto;
+import lotto.model.Purchase;
+import lotto.model.Result;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        // 로또 구매
+        String amount = inputView.inputPurchaseAmount();
+        Purchase purchase = lottoController.purcahseLottos(amount);
+        int purchaseCount = purchase.getPurchaseCount();
+        outputView.printPurchaseCount(purchaseCount);
+        
     }
 }

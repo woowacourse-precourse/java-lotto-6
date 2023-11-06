94d24cdcfc725816c8d52d75e1e3de7691c5eb17
minseo  jo <liging12@naver.com>
2023-11-06 22:04:07 +0900

    refactor: 로또 수익률 계산 리팩토링
    당첨 금액의 자료형을 int 에서 long 으로 변환
    if 문으로 enum 모든 부분을 비교하는 대신, for 을 이용

--- src/main/java/lotto/service/LottoProfitService.java
+++ src/main/java/lotto/service/LottoProfitService.java
@@ -4,6 +4,7 @@ import lotto.constant.LottoConstants;
 import lotto.domain.LottoWinningRanking;
 
 import java.util.EnumMap;
+import java.util.Map;
 
 public class LottoProfitService {
     private static final LottoProfitService instance = new LottoProfitService();
@@ -16,9 +17,9 @@ public class LottoProfitService {
 
     public double calculateProfit(EnumMap<LottoWinningRanking, Integer> winningRankingCountMap, int lottoCount) {
         int purchaseAmount = calculatePurchaseAmount(lottoCount);
-        int winningAmount = calculateWinningAmount(winningRankingCountMap);
+        long totalWinningAmount = calculateWinningAmount(winningRankingCountMap);
 
-        double profit = (double) (winningAmount) / purchaseAmount * 100.0;
+        double profit = (double) (totalWinningAmount) / purchaseAmount * 100.0;
         return Math.round(profit * 10.0) / 10.0;
     }
 
@@ -26,26 +27,26 @@ public class LottoProfitService {
         return lottoCount * LottoConstants.LOTTO_PRICE.getValue();
     }
 
-    private int calculateWinningAmount(EnumMap<LottoWinningRanking, Integer> winningRankingCountMap) {
-        int totalWinningAmount = 0;
+    private long calculateWinningAmount(EnumMap<LottoWinningRanking, Integer> winningRankingCountMap) {
+        long totalWinningAmount = 0;
 
-        // (1등, 2등, 3등)은 한번만 더한다. (원래 n명이 1, 2, 3등에 당첨 되면 그 금액을 n으로 나눠 가진다.
-        // 현재는 사용자가 한명 관점에서 계산하므로, 공통된 사람의 중복여부는 관리하지 않는다.
-        if (winningRankingCountMap.getOrDefault(LottoWinningRanking.FIRST, 0) > 0) {
-            totalWinningAmount += LottoWinningRanking.FIRST.getWinningAmount();
+        for (Map.Entry<LottoWinningRanking, Integer> value : winningRankingCountMap.entrySet()) {
+            if (value.getValue() > 0) {
+                totalWinningAmount += calculateWinningAmountForRanking(value.getKey(), value.getValue());
+            }
         }
-        if (winningRankingCountMap.getOrDefault(LottoWinningRanking.SECOND, 0) > 0) {
-            totalWinningAmount += LottoWinningRanking.SECOND.getWinningAmount();
-        }
-        if (winningRankingCountMap.getOrDefault(LottoWinningRanking.THIRD, 0) > 0) {
-            totalWinningAmount += LottoWinningRanking.THIRD.getWinningAmount();
-        }
-
-        // 4등과 5등은 상금을 중복해서 더함 (4, 5등은 n명이 걸려도 모두 같은 정해진 금액을 받는다.)
-        totalWinningAmount += LottoWinningRanking.FOURTH.getWinningAmount() * winningRankingCountMap.getOrDefault(LottoWinningRanking.FOURTH, 0);
-        totalWinningAmount += LottoWinningRanking.FIFTH.getWinningAmount() * winningRankingCountMap.getOrDefault(LottoWinningRanking.FIFTH, 0);
 
         return totalWinningAmount;
+    }
+
+    private long calculateWinningAmountForRanking(LottoWinningRanking ranking, int count) {
+        if (ranking.getWinningAmount() >= LottoWinningRanking.THIRD.getWinningAmount()) {
+            return ranking.getWinningAmount();
+        } else if (ranking.getWinningAmount() <= LottoWinningRanking.FOURTH.getWinningAmount()) {
+            return ranking.getWinningAmount() * count;
+        }
+
+        return 0;
     }
 
 }

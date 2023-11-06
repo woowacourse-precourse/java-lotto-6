274c6c70a5b0ba943ce2d3b1174da8033bac2a0a
gunwoong <gwj0421@gmail.com>
2023-11-06 21:33:44 +0900

    test : 주어진 로또 번호가 정해진 개수를 채우지 못했을 경우 검증 테스트 추가

--- src/test/java/lotto/validation/DomainValidateTest.java
+++ src/test/java/lotto/validation/DomainValidateTest.java
@@ -2,6 +2,10 @@ package lotto.validation;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.config.ConstantNum.*;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -20,4 +24,25 @@ class DomainValidateTest {
         assertThatCode(() -> DomainValidate.validateNumberInRange(num)).doesNotThrowAnyException();
     }
 
+    @Test
+    void testLottoNumberCnt() {
+        // given
+        List<Integer> lotto = new ArrayList<>();
+        List<Integer> bonus = new ArrayList<>();
+        for (int i = LOTTO_NUMBER_MIN.getNum(); i < LOTTO_NUMBER_MIN.getNum() + LOTTO_NUMBER_CNT.getNum(); i++) {
+            lotto.add(i);
+        }
+        for (int i = LOTTO_NUMBER_MIN.getNum(); i < LOTTO_NUMBER_MIN.getNum() + LOTTO_BONUS_NUMBER_CNT.getNum(); i++) {
+            bonus.add(i);
+        }
+        List<Integer> wrongLotto = new ArrayList<>(lotto);
+        List<Integer> wrongBonus = new ArrayList<>(bonus);
+        wrongLotto.add(lotto.get(0));
+        wrongBonus.add(bonus.get(0));
+
+        // when & then
+        assertThatThrownBy(() -> DomainValidate.validateNumberOfLottoNumbers(wrongBonus))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
+        assertThatCode(() -> DomainValidate.validateNumberOfLottoNumbers(lotto)).doesNotThrowAnyException();
+    }
 }

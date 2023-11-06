a1375d7733187f06aa0cb0ca45574c22c110495c
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-07 00:46:12 +0900

    feat(InputValidator) : 입력 금액 1,000원 단위로 나누어 떨어지는지 검사하는 기능 구현

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.global.ResponseStatus;
+import lotto.global.error.ErrorMessage;
 
 import static lotto.global.constant.LottoConstant.LOTTO_PRICE;
 
@@ -8,7 +9,8 @@ public class InputValidator {
 
     public static ResponseStatus purchasePriceCheck(int lottoPurchasePrice){
         if(lottoPurchasePrice < LOTTO_PRICE || lottoPurchasePrice % LOTTO_PRICE != 0){
-
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_PURCHASE_PRICE.getMessage());
         }
+        return ResponseStatus.OK;
     }
 }

9904995c587d2eee1c8f75d4f36e8a7335a8132f
JayG-5 <jg55552758@gmail.com>
2023-11-07 02:11:20 +0900

    feat: 구매 내역, 당첨 내역, 수익률, 소수점 둘째자리에서 반올림 출력 구현

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,32 +1,62 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.GameSetting;
 import lotto.domain.Lotto;
+import lotto.domain.Winning;
 import lotto.utils.RandomNumberGenerator;
 import lotto.view.Message;
 import lotto.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
-    private int bonusNumber;
-    private List<Integer> numbers;
+    private final List<Winning> winnings;
     private final List<Lotto> tickets = new ArrayList<>();
+    private Integer money;
+    private List<Integer> numbers;
+    private Integer bonusNumber;
 
-    public void start() {
-
-        buyTicket(View.requestMoney());
-        this.numbers = View.requestNumbers();
-        this.bonusNumber = View.requestNumberBonus(this.numbers);
+    public Game(List<Winning> winnings) {
+        this.winnings = winnings;
     }
 
-    private void buyTicket(Integer ticketEach) {
+    public void start() {
+        money = View.requestMoney();
+        buyTicket(money);
+        numbers = View.requestNumbers();
+        bonusNumber = View.requestNumberBonus(this.numbers);
+        evaluateWinning();
+        printWinningResult();
+        calculatePNL();
+        Console.close();
+    }
+
+    private void buyTicket(Integer money) {
+        final int ticketEach = money / GameSetting.TICKET_PRICE.getValue();
         View.printMessageWithArgs(Message.RESPONSE_TICKETS, ticketEach);
         for (int i = 0; i < ticketEach; i++) {
-            final  List<Integer> numbers = RandomNumberGenerator.create();
+            final List<Integer> numbers = RandomNumberGenerator.create();
             tickets.add(new Lotto(numbers));
-            View.printMessageWithArgs(Message.RESPONSE_TICKET_NUMBERS,numbers.toString());
+            View.printMessageWithArgs(Message.RESPONSE_TICKET_NUMBERS, numbers.toString());
         }
         View.printNewLine();
+    }
+
+    private void evaluateWinning() {
+        tickets.forEach(lotto -> lotto.evaluateWinning(numbers, bonusNumber, winnings));
+    }
+
+    private void printWinningResult() {
+        View.printMessage(Message.RESPONSE_FINAL_TITLE);
+        winnings.forEach(Winning::printResult);
+    }
+
+    private void calculatePNL() {
+        final int profit = winnings.stream().mapToInt(Winning::calculatePrize).sum();
+        final float pnl = (float) (Math.round((float) profit / money * 10) / 10.0);
+        View.printMessageWithArgs(Message.RESPONSE_PNL,pnl);
     }
 }

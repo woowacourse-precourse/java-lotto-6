3ba91d7fa4ea35b185ad177b4a09c6488a3744b3
siho-song <thdtlgh234@naver.com>
2023-11-06 23:55:51 +0900

    [Test] LottoStore 클래스 단위테스트 구현

---
+++ src/test/java/lotto/LottoStoreTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.domain.LottoStore;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoStoreTest {
+
+    @DisplayName("총 발행개수 만큼의 로또를 발행")
+    @Test
+    void publishLottoesByTotalLottoNumber() {
+        int totalLottoNumber = 8;
+        LottoStore lottoStore = new LottoStore(totalLottoNumber);
+
+        int result = lottoStore.getLottoes().size();
+        int expected = 8;
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+}

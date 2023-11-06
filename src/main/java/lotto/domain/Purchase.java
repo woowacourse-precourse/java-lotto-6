2b51f3250b6d4951ad1d126d23921e2d0a978d57
yang <hjyang@epience.com>
2023-11-06 17:21:00 +0900

    🎯feat(domain/Purchase) : 구매 개수 조회 기능 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -4,13 +4,17 @@ package lotto.domain;
 import static lotto.validate.InputValidation.THOUSAND;
 
 public class Purchase {
-    int quantity;
+    private int quantity;
     public Purchase(int quantity) {
         this.quantity = quantity;
     }
 
     public void calculateQuantity(String purchaseAmount) {
         quantity = Integer.parseInt(purchaseAmount) / THOUSAND;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
 }

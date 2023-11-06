9871f8779c97e5638f960bace2037b74b7449582
sujnkim <blackout3210@naver.com>
2023-11-06 12:05:20 +0900

    feat: 당첨 번호와 보너스 번호를 가지는 WinningLotto 클래스 생성

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+public class WinningLotto {
+
+    private final Lotto winningNumber;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumber, int bonusNumber) {
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+
+
+
+}

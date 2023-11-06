6229381663ae8e4f5ed33ccd9745b8869e58de54
Identityless <rlandwnd555@naver.com>
2023-11-07 00:20:19 +0900

    feat : 구입 금액 입력 기능 구현

--- src/main/java/lotto/Inputter.java
+++ src/main/java/lotto/Inputter.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Inputter {
     private static Inputter inputter;
 
@@ -12,4 +14,15 @@ public class Inputter {
         return inputter;
     }
 
+    public int purchase() {
+        int won;
+
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            won = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요.");
+        }
+        return won;
+    }
 }

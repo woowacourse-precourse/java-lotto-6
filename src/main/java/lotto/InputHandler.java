9035de251de1d4b26385c4ab824723987c0a8a6c
HeeHeeg <qmffndoden@naver.com>
2023-11-06 20:58:39 +0900

    feat(lotto): 당첨 번호에 숫자가 아닌 값을 입력하면 예외 처리하는 기능 구현

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,10 +10,16 @@ import java.util.stream.Collectors;
 public class InputHandler {
 
     public List<Integer> getWinningNumber() {
-        return Arrays.stream(Console.readLine().split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<Integer> winningNumber = new ArrayList<>();
+        try {
+            winningNumber = Arrays.stream(Console.readLine().split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+        return winningNumber;
     }
 
     public int getInputNumber() {

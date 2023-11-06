17a60f8932c5a8fc315320842b14d9973ee683d3
woong2e <tlswodnd12345@naver.com>
2023-11-07 02:34:45 +0900

    feat: 로또게임 사용자 로또 당첨 확인 기능

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,11 +1,32 @@
 package lotto.domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class User {
     private final List<Lotto> lottos;
 
     public User(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public Map<Rank, Integer> checkResult(WinningLotto winningLotto) {
+        Map<Rank, Integer> result = initResult();
+
+        for (Lotto lotto : lottos) {
+            Rank rank = WinningLotto.compare(lotto);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+
+        return result;
+    }
+    private Map<Rank, Integer> initResult() {
+        Map<Rank, Integer> result = new HashMap<>();
+
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+        return result;
     }
 }

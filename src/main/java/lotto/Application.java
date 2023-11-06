c7fe8833ae388f75291d7e67164434bdb61425c8
chaerlo127 <monynony0203@gmail.com>
2023-11-06 14:00:57 +0900

    feat: [2] 구매한 로또 출력 + 열 정리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,19 +1,34 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int cash = inputLottoPayCount();
+        int count = cash / 1000;
+        System.out.println(count + "개를 구매했습니다.");
+        List<List<Integer>> lottoList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            numbers.sort(Integer::compareTo);
+            lottoList.add(numbers);
+        }
+        lottoList.forEach(i -> {
+            System.out.println(i.toString());
+        });
     }
 
     private static int inputLottoPayCount() {
         System.out.println("구입금액을 입력해 주세요.");
-        try{
+        try {
             int cash = inputLottoCash();
             checkNotThousandAndNegativeException(cash);
             return cash;
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
             inputLottoPayCount();
         }
@@ -26,10 +41,10 @@ public class Application {
 
     private static int inputLottoCash() {
         int count;
-        try{
+        try {
             count = Integer.parseInt(Console.readLine());
             return count;
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자 형식으로 입력해주세요.");
         }
     }

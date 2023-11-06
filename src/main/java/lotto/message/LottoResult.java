1a982150475b9d2329d6d2d3a39f7e9137bc96ad
alscjf1329 <alscjf1329@gmail.com>
2023-11-06 18:41:45 +0900

    feat(LottoPurchaseManager): LottoResult에 message 추가
    LottoResult enum class에 조건을 알려 주는 message를 추가했습니다.

--- src/main/java/lotto/message/LottoResult.java
+++ src/main/java/lotto/message/LottoResult.java
@@ -1,16 +1,18 @@
 package lotto.message;
 
 public enum LottoResult {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    NONE(0);
+    FIRST(2_000_000_000, "6개 일치 (2,000,000,000원)"),
+    SECOND(30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(1_500_000, "5개 일치 (1,500,000원)"),
+    FOURTH(50_000, "4개 일치 (50,000원)"),
+    FIFTH(5_000, "3개 일치 (5,000원)"),
+    NONE(0, "꽝");
     private final int prize;
+    private final String message;
 
-    LottoResult(int prize) {
+    LottoResult(int prize, String message) {
         this.prize = prize;
+        this.message = message;
     }
 
     public static LottoResult of(int matchCount, boolean isBonusNumberMatched) {
@@ -34,5 +36,9 @@ public enum LottoResult {
 
     public int getPrize() {
         return prize;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }

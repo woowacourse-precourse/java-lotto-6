fc96861f2d646d72037b58004bf98c06235d5458
oxdjww <oxdjww@gmail.com>
2023-11-07 03:25:33 +0900

    feat : `2.2.1`, `2.2.2`, `2.2.3` 로또 생성 시 예외발생 기능 추가 완료 (크기, 범위, 중복)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,14 @@
 package lotto.domain;
 
+import static lotto.domain.constants.LottoConfig.LOTTO_NUMBER_MAX;
+import static lotto.domain.constants.LottoConfig.LOTTO_NUMBER_MIN;
+import static lotto.domain.constants.LottoConfig.LOTTO_SIZE;
+import static lotto.exception.ErrorMessage.DUPLICATE_LOTTO_NUMBERS;
+import static lotto.exception.ErrorMessage.OUT_OF_RANGE_LOTTO_NUMBERS;
+
 import java.util.Collections;
 import java.util.List;
+import lotto.exception.LottoException;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,7 +19,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateDuplicateNumbers(numbers);
+        validateNumbersRange(numbers);
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        if (numbers.stream()
+                .allMatch
+                        (num -> num >= LOTTO_NUMBER_MIN.getValue() && num <= LOTTO_NUMBER_MAX.getValue())) {
+            throw LottoException.of(OUT_OF_RANGE_LOTTO_NUMBERS);
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        if (numbers.stream()
+                .distinct().count()
+                != numbers.size()) {
+            throw LottoException.of(DUPLICATE_LOTTO_NUMBERS);
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE.getValue()) {
             throw new IllegalArgumentException();
         }
     }
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,6 +2,8 @@ package lotto.exception;
 
 public enum ErrorMessage {
     INVALID_PURCHASE_AMOUNT("구매 단위는 1,000원 단위여야 합니다."),
+    DUPLICATE_LOTTO_NUMBERS("하나의 로또에 중복된 숫자가 있습니다."),
+    OUT_OF_RANGE_LOTTO_NUMBERS("하나 이상의 로또 내 숫자가 유효한 범위 밖입니다."),
     NOT_NUMERIC_PURCHASE_AMOUNT("입력된 구매 금액이 정수가 아닙니다.");
     private final String message;
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";

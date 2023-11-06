7c7f5ad39e2fe3e03b48aba17d37b71a3e61c5e2
minisyu <iswater.kim@gmail.com>
2023-11-06 12:53:20 +0900

    feat: 당첨 번호 및 보너스 번호와 구입한 로또를 비교하여 결과를 생성하는 DrawingMachine 생성

---
+++ src/main/java/lotto/domain/draw/DrawingMachine.java
@@ -0,0 +1,41 @@
+package lotto.domain.draw;
+
+import java.util.EnumMap;
+import java.util.List;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoRank;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.lotto.WinningCombination;
+
+public class DrawingMachine {
+
+    public DrawingResult draw(WinningCombination winningCombination, Lottos totalLottoTickets) {
+        EnumMap<LottoRank, Integer> drawingResult = evaluateDrawingResult(winningCombination, totalLottoTickets);
+        return new DrawingResult(drawingResult);
+    }
+
+    private EnumMap<LottoRank, Integer> evaluateDrawingResult(WinningCombination winningCombination,
+                                                              Lottos totalLottoTickets) {
+        EnumMap<LottoRank, Integer> drawingResult = initializeDrawingResult();
+        List<Lotto> lottoTickets = totalLottoTickets.getLottos();
+        lottoTickets.forEach(
+                lottoTicket -> updateDrawingResult(drawingResult, winningCombination.getLottoRankFrom(lottoTicket)));
+        return drawingResult;
+    }
+
+    private EnumMap<LottoRank, Integer> initializeDrawingResult() {
+        EnumMap<LottoRank, Integer> drawingResult = new EnumMap<>(LottoRank.class);
+        for (LottoRank rank : LottoRank.ranks) {
+            drawingResult.put(rank, 0);
+        }
+        return drawingResult;
+    }
+
+    private void updateDrawingResult(EnumMap<LottoRank, Integer> drawingResult, LottoRank lottoRank) {
+        if (drawingResult.containsKey(lottoRank)) {
+            drawingResult.replace(lottoRank, drawingResult.get(lottoRank) + 1);
+            return;
+        }
+        drawingResult.put(lottoRank, 1);
+    }
+}

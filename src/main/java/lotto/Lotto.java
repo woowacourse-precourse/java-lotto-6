e9da8b50e6ae6c4aae05f48ebd6e69cc7501676f
giljin <68088970+giljin@users.noreply.github.com>
2023-11-06 17:20:51 +0900

    feat(Lotto): 로또 금액 상수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static int PRICE = 1000;
     private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {

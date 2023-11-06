cda16b0959acc27d30e054811e897e61ed9b78bc
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 10:43:47 +0900

    feat(ErrorMessage) : 보너스 번호 관련 에러메세지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -2,9 +2,11 @@ package lotto;
 
 public enum ErrorMessage {
     ERROR("[ERROR]"),
-    WRONG_RANGE_ERROR_MESSAGE(ERROR + "로또 번호는 1~45사이의 숫자입니다."),
-    WRONG_SIZE_ERROR_MESSAGE(ERROR + "로또 번호는 6개의 숫자입니다."),
-    HAS_DUPLICATION_ERROR_MESSAGE(ERROR + "로또 번호에 중복이 있으면 안됩니다.");
+    LOTTO_WRONG_RANGE_ERROR_MESSAGE(ERROR.get() + "로또 번호는 1~45사이의 숫자입니다."),
+    LOTTO_WRONG_SIZE_ERROR_MESSAGE(ERROR.get() + "로또 번호는 6개의 숫자입니다."),
+    LOTTO_HAS_DUPLICATION_ERROR_MESSAGE(ERROR.get() + "로또 번호에 중복이 있으면 안됩니다."),
+    BONUS_DUPLICATION_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 로또 번호와 중복되면 안됩니다."),
+    BONUS_WRONG_RANGE_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 1~45사의 숫자입니다.");
 
     private final String message;
 

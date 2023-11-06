cbbaab9f26d7d3e04083068e2f9027c6948cfede
yeseong31 <yeseong31@naver.com>
2023-11-06 17:37:37 +0900

    feat(LottoGameController): 당첨 통계 메서드 호출

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -5,20 +5,24 @@ import lotto.domain.Payment;
 import lotto.domain.WinningLotto;
 
 public class LottoGameController {
-    
+
     private final PaymentController paymentController;
     private final PurchaseController purchaseController;
     private final WinningLottoController winningLottoController;
-    
+    private final StatisticsController statisticsController;
+
     public LottoGameController() {
         this.paymentController = new PaymentController();
         this.purchaseController = new PurchaseController();
         this.winningLottoController = new WinningLottoController();
+        this.statisticsController = new StatisticsController();
     }
-    
+
     public void start() {
         final Payment payment = paymentController.receivePayment();
         final Lotteries lotteries = purchaseController.purchaseLotteries(payment);
         final WinningLotto winningLotto = winningLottoController.receiveWinningLotto();
+
+        statisticsController.receiveGameResult(lotteries, winningLotto);
     }
 }

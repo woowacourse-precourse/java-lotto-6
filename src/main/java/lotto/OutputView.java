d90b876b6fdbeda70cab3c378f664a313bd484ff
SUJI-J <suzy852@naver.com>
2023-11-06 23:07:29 +0900

    feat: 구매한 개수에 맞춰 로또 번호 출력

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static lotto.Constants.REQUEST_BUY_MONEY;
 import static lotto.Constants.TICKET_COUNT;
+import static lotto.Constants.REQUEST_WINNING_NUMBERS;
 
 public class OutputView {
     public static void printRequestMoney() {
@@ -9,12 +10,18 @@ public class OutputView {
     }
 
     public static void printTicketCount(int ticketCount) {
-        System.out.println();
         System.out.println(ticketCount + TICKET_COUNT);
     }
 
     public static void printLottoList(Lottos lottos) {
         lottos.getLottos().forEach(System.out::println);
+    }
+
+    public static void printWinningNumber() {
+        System.out.println(REQUEST_WINNING_NUMBERS);
+    }
+
+    public static void printBlankLine() {
         System.out.println();
     }
 }

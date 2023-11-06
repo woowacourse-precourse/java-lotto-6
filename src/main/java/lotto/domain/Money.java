0456d7a08baeafc36ca7a17359705f2c03f0cd3e
김민정 <mjeong@sookmyung.ac.kr>
2023-11-06 16:34:24 +0900

    refactor: 잘못된 로또 구입 금액에 대한 에러 메세지 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class Money {
     private static final int LOTTO_PRICE = 1000;
+    public static final String INSUFFICIENT_AMOUNT_MESSAGE = String.format("로또 1장의 가격은 %,d원입니다.", LOTTO_PRICE);
+    public static final String NOT_DIVISIBLE_MESSAGE = String.format("%,d원으로 나누어 떨어지지 않습니다.", LOTTO_PRICE);
     private final int amount;
 
     public Money(int amount) {
@@ -17,10 +19,10 @@ public class Money {
 
     private void validate(int amount) {
         if (amount < LOTTO_PRICE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INSUFFICIENT_AMOUNT_MESSAGE);
         }
         if (amount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_DIVISIBLE_MESSAGE);
         }
     }
 }

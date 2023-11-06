f5764ab4f5f429e003248d6457f9152b5c638911
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 20:10:23 +0900

    feat(OutputView.java): add printEarningRateResultMessage function 수익률을 인자로 입력받아 수익률 안내 메시지를 출력하는 기능을 구현하였습니다.

--- src/main/java/view/OutputVIew.java
+++ src/main/java/view/OutputVIew.java
@@ -2,9 +2,11 @@ package view;
 
 public class OutputVIew {
 
+    static private int earningRate;
     static final String MONEY_INPUT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
     static final String PURCHASING_RESULT_MESSAGE = "개를 구매했습니다.";
     static final String WINNING_NUMBER_INPUT_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
+    static final String EARNING_RATE_RESULT_MESSAGE = "총 수익률은" + earningRate + "입니다.";
 
     void  printMoneyInputRequestMessage() {
         System.out.println(MONEY_INPUT_REQUEST_MESSAGE);
@@ -16,5 +18,10 @@ public class OutputVIew {
 
     void printWinningNumberInputRequestMessage() {
         System.out.println(WINNING_NUMBER_INPUT_REQUEST_MESSAGE);
+    }
+
+    void printEarningRateResultMessage(int earningRate) {
+        this.earningRate = earningRate;
+        System.out.println(EARNING_RATE_RESULT_MESSAGE);
     }
 }

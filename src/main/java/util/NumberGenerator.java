0507bba27ff39c1d334f7b79aaa58ebfbea473e8
daneng4 <daneng4@naver.com>
2023-11-06 19:36:33 +0900

    feat: 생성한 로또 번호 정렬 기능 추가

--- src/main/java/util/NumberGenerator.java
+++ src/main/java/util/NumberGenerator.java
@@ -1,11 +1,20 @@
 package util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class NumberGenerator {
 
+    List<Integer> randomNumbers = new ArrayList<>();
     public List<Integer> generate(){
-        return Randoms.pickUniqueNumbersInRange(1,45,6);
+        randomNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        sortRandomNumbers(randomNumbers);
+        return randomNumbers;
+    }
+
+    public void sortRandomNumbers(List<Integer> randomNumbers){
+        Collections.sort(this.randomNumbers);
     }
 }

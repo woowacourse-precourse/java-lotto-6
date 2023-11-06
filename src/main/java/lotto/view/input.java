f7a9b70c2285a60fa4245cfa395bb443fba79a17
gahyeon <1000ji1@naver.com>
2023-11-06 16:12:18 +0900

    Feat : 보너스 번호 입력

--- src/main/java/lotto/view/input.java
+++ src/main/java/lotto/view/input.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class input {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static String read() {
         return readLine();
@@ -20,4 +21,8 @@ public class input {
         return read();
     }
 
+    public static String bonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        return read();
+    }
 }

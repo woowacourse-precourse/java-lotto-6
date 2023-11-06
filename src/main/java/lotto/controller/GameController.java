4e4a3f7ccd4758b034382c4d3890f9bae5c3c25f
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 19:19:59 +0900

    Feat: 로또 게임을 진행하는 기능 구현

---
+++ src/main/java/lotto/controller/GameController.java
@@ -0,0 +1,48 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import lotto.Lotto;
+import lotto.domain.LottosMaker;
+import lotto.domain.SummaryMaker;
+import lotto.output.ResultPrinter;
+
+public class GameController {
+
+	int purchaseTime;
+	int bonusNumber;
+	List<Lotto> lottos;
+	List<Integer> winningNumber;
+
+	MakerController makerController = new MakerController();
+	SummaryMaker summaryMaker = new SummaryMaker();
+
+	public void playGame() {
+		operateLottos();
+		operateSummary();
+	}
+
+	private void operateLottos() {
+
+		purchaseTime = makerController.makeCorrectPurchaseTime();
+
+		lottos = new ArrayList<>(LottosMaker.make(purchaseTime));
+		ResultPrinter.printLottos(lottos, purchaseTime);
+
+		winningNumber = makerController.makeCorrectWinningNumber();
+		bonusNumber = makerController.makeCorrectBonusNumber();
+
+	}
+
+	private void operateSummary() {
+
+		Map<String, Integer> summary = summaryMaker.summarizeHit(lottos, winningNumber, bonusNumber);
+		double yield = summaryMaker.calculateYield(purchaseTime);
+
+		ResultPrinter.printSummary(summary);
+		ResultPrinter.printYield(yield);
+	}
+
+}

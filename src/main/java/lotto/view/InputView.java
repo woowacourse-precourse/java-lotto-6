b52247d30e9177c78bf31a0a9739ddfda68982e8
SCY <helenason@naver.com>
2023-11-07 02:31:34 +0900

    feat: 사용자로부터 보너스 번호 입력받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
 
     private static final String NOTICE_INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
     private static final String NOTICE_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String NOTICE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final int ONE_LOTTO_PRICE = 1000;
     private static final String DELIMITER = ",";
     private static final String FORMAT_OF_NUMERIC = "[0-9]+";
@@ -26,13 +27,19 @@ public class InputView {
         }
     }
 
-    public void inputWinNumber() {
+    public void inputWinningNumber() {
         System.out.println(NOTICE_INPUT_WINNING_NUMBER);
         String input = Console.readLine();
         List<String> inputNumbers = List.of(input.split(DELIMITER));
         validateInputNumbersType(inputNumbers);
         List<Integer> winningNumbers = stringListToIntList(inputNumbers);
         Lotto winningLotto = new Lotto(winningNumbers);
+    }
+
+    public void inputBonusNumber() {
+        System.out.println(NOTICE_INPUT_BONUS_NUMBER);
+        String input = Console.readLine();
+        int bonusNumber = stringToInt(input);
     }
 
     private List<Integer> stringListToIntList(List<String> before) {

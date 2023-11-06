681e3a683455c9f29ee893e9b7adc4608084c392
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 15:29:25 +0900

    chore : 기능에 따른 주석 추가

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -15,17 +15,20 @@ public class Purchase {
         return new Purchase(purchaseCount);
     }
 
+    // 구매 금액에 따른 구매 개수 계산
     private static int calculatePurchaseCount(int purchaseAmount) {
         validateAmountInThousands(purchaseAmount);
         return purchaseAmount / 1000;
     }
 
+    // 구매 금액이 1000원 단위인지 검증
     private static void validateAmountInThousands(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
         }
     }
 
+    // 구매 금액이 숫자로만 이루어져있는지 검증
     private static void validateNumeric(String input) {
         if (!input.matches(NUMBER_PATTERN)) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자로만 이루어져야 합니다.");

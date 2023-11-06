f756216ca3283cd58490f8631702554502e89118
Infikei <hckhong@naver.com>
2023-11-07 00:41:15 +0900

    feat: 로또 구입 금액을 담당하는 Amount 클래스 생성
    - 상수, 인스턴스 변수, 생성자 추가

---
+++ src/main/java/lotto/domain/Amount.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public class Amount {
+    private static final String NOT_DIVIDED_BY_PRICE_EXCEPTION_MESSAGE = "구입금액은 1000원 단위로 나누어떨어져야 합니다.";
+    private static final String NOT_POSITIVE_EXCEPTION_MESSAGE = "구입금액은 양수여야 합니다.";
+    private static final int LOTTO_PRICE = 1000;
+
+    private final int amount;
+    private final int numberOfLottos;
+
+    public Amount(int amount) {
+        this.amount = amount;
+        this.numberOfLottos = 1;
+    }
+}

adddbfa394f51a8a22f87a8476110e31aec9bbed
transfer_kk <kseysh123@naver.com>
2023-11-06 20:02:59 +0900

    feat : 로또 번호를 생성하는 기능 추가

--- src/main/java/lotto/domain/service/LottoGenerator.java
+++ src/main/java/lotto/domain/service/LottoGenerator.java
@@ -1,2 +1,11 @@
-package lotto.domain.service;public class LottoGenerator {
+package lotto.domain.service;
+
+import java.util.List;
+import lotto.domain.model.Lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+public class LottoGenerator {
+    public Lotto generateLotto(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 }

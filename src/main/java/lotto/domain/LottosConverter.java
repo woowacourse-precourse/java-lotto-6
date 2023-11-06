688ade66986985c136ebe1484d37a98682a9368c
yubin <dbqls2821@gmail.com>
2023-11-06 18:04:48 +0900

    feat: LottosConverter 내부 구현 로직 생성

--- src/main/java/lotto/domain/LottosConverter.java
+++ src/main/java/lotto/domain/LottosConverter.java
@@ -8,6 +8,15 @@ import java.util.StringJoiner;
 public final class LottosConverter implements OutputConverter<List<List<Integer>>> {
     @Override
     public String convert(List<List<Integer>> lottos) {
+        StringBuilder output = new StringBuilder();
+        lottos.forEach(lotto -> {
+                    output.append(Constant.START_PREFIX.value);
+                    StringJoiner temp = new StringJoiner(Constant.DELIMITER.value);
+                    lotto.forEach(number -> temp.add(number.toString()));
+                    output.append(temp);
+                    output.append(Constant.END_PREFIX.value);
+                });
+        return output.toString();
     }
 
     enum Constant {

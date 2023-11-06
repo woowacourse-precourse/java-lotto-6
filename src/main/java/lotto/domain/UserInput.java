7685fa43513e64d3f5762f162907d38c2dadddb8
HYOAN <aspqll@naver.com>
2023-11-06 21:15:29 +0900

    feat(lotto): 사용자 입력 기능 구현

--- src/main/java/lotto/domain/UserInput.java
+++ src/main/java/lotto/domain/UserInput.java
@@ -1,15 +1,38 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserInput {
     public UserInput() {
     }
-    public int getUserPurchaseAmount() {
+
+    public String input() {
+        String input = Console.readLine();
+        try {
+            checkInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return input;
+    }
+
+    public int getUserPurchaseAmount(String inputPurchaseAmount) {
         return 0;
     }
-    public int getUserWinningNumbers() {
+
+    public int getUserWinningNumbers(String inputWinningNumbers) {
         return 0;
     }
+
     public int getUserBonusNumber() {
         return 0;
+    }
+
+    public void checkInt(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+        }
     }
 }

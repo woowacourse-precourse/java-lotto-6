02237705493c83aa633bb200cc2f0842dcaf9393
JuSeong1130 <jsmoon1130@naver.com>
2023-11-06 10:03:23 +0900

    feat(WinningRank) : findWinningRank기능 구현
    matchCount와 hasBonusNumber를 기준으로 WinningRank(당첨 등수)를 찾아준다.

---
+++ src/main/java/lotto/domain/result/WinningRank.java
@@ -0,0 +1,39 @@
+package lotto.domain.result;
+
+import java.util.Arrays;
+
+public enum WinningRank {
+    FIFTH(3, false, 5000),
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000),
+    EMPTY(-1,false,0);
+
+    private int count;
+    private boolean bonusNumber;
+    private int money;
+
+    WinningRank(int count, boolean bonusNumber, int money) {
+        this.count = count;
+        this.bonusNumber = bonusNumber;
+        this.money = money;
+    }
+
+    public static WinningRank findWinningRank(int matchCount, boolean hasBonusNumber) {
+        if(matchCount == 5) {
+            return Arrays.stream(WinningRank.values())
+                    .filter(winningResult -> winningResult.count == matchCount && winningResult.bonusNumber == hasBonusNumber)
+                    .findAny()
+                    .orElse(EMPTY);
+        }
+        return Arrays.stream(WinningRank.values())
+                .filter(winningResult -> winningResult.count == matchCount)
+                .findAny()
+                .orElse(EMPTY);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}

ca0d9a10f44dd3bf6fa4ba1c90cdec3de5c2e642
Ji <zeegeon@naver.com>
2023-11-06 22:32:44 +0900

    feat: lotto number generate method, input winning numbers method

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,13 +1,19 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static lotto.Util.checkNumberValidate;
-import static lotto.Util.checkTotalPrice;
+import static lotto.Util.*;
 
 public class LottoController {
 
-    private Lotto lotto;
+    private List<Lotto> lottos;
     private int totalLottoCnt;
+
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
 
     public void playLotto() {
         start();
@@ -22,18 +28,35 @@ public class LottoController {
         this.totalLottoCnt = checkTotalPrice(checkNumberValidate(readLine()));
     }
 
-    private void printEnd() {
+    private void generateLottoNumber() {
+        List<Lotto> newLotto = new ArrayList<>();
+        
+        // 로또 횟수만큼 반복
+        for(int i=0; i<totalLottoCnt; ++i) {
+            // 6자리 랜덤 변수 생성
+            List<Integer> numbers = new ArrayList<>();
+
+            //생성한 변수 Lotto 객체에 주입
+            Lotto a = new Lotto(numbers);
+
+            // newLotto 배열에 추가
+            lottos.add(a);
+        }
+
+        this.lottos = newLotto;
+    }
+
+    private void middle() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        winningNumbers = winningNumberCalculate(readLine());
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        bonusNumber = winningNumberCalculate(winningNumbers, readLine());
     }
 
     private void calculate() {
     }
 
-    private void middle() {
+    private void printEnd() {
     }
-
-    private void generateLottoNumber() {
-    }
-
-
-
 }
--- src/main/java/lotto/Util.java
+++ src/main/java/lotto/Util.java
@@ -1,6 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.sort;
+
 public class Util {
+
+    private static final String error1 = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String error2 = "[ERROR] 보너스 번호는 중복될 수 없습니다.";
 
     public static Integer checkNumberValidate(String line) {
         try {
@@ -13,5 +21,30 @@ public class Util {
     public static Integer checkTotalPrice(Integer price) {
         if(price % 1000 != 0) throw new IllegalArgumentException();
         return price / 1000;
+    }
+
+    public static List<Integer> winningNumberCalculate(String line) {
+        String[] temp = line.split(",");
+
+        List<Integer> re = new ArrayList<>();
+
+        for(String a: temp) {
+            re.add(Integer.parseInt(a));
+        }
+        sort(re);
+
+        return re;
+    }
+
+    public static Integer winningNumberCalculate(List<Integer> winningNumbers, String line) {
+        try {
+            int temp = Integer.parseInt(line);
+            if(winningNumbers.contains(temp)) throw new IllegalArgumentException(error2);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+
+
+        return 1;
     }
 }

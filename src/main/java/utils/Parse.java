8b5ad2cc41da1e7522191400c652d2d74236b53f
happyryan <tjdgks120813@gmail.com>
2023-11-07 00:58:24 +0900

    feat(utils): Add Parse class to convert List of String to List of Integer
    Created the Parse class with a static method parseInteger that takes a List of String as input and returns a List of Integer by converting each element in the input list.

--- src/main/java/utils/Parse.java
+++ src/main/java/utils/Parse.java
@@ -1,2 +1,14 @@
-package utils;public class Parse {
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parse {
+    public static List<Integer> parseInteger(List<String> stringArray) {
+        List<Integer> IntegerArray = new ArrayList<>();
+        for (String string : stringArray) {
+            IntegerArray.add(Integer.parseInt(string));
+        }
+        return IntegerArray;
+    }
 }

d29390dce0e09c1929921d2041dbca478d26dc74
NAM GIBEOM <rlqja0523@naver.com>
2023-11-07 01:24:27 +0900

    feat: 당첨내역을 출력하는 기능 구현

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -16,4 +16,25 @@ public class OutputView {
         lottoTicket.lottoTicket().forEach(System.out::println);
         System.out.println();
     }
+
+    public void printWinningResult(WinningResult winningResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        Arrays.stream(Prize.PRIZES)
+                .filter(prize -> prize != Prize.NONE)
+                .forEach(p -> System.out.printf(getFormat(p),
+                        p.getMatchedCount(), convertFormat(p.getWinningMoney()), winningResult.countOf(p)));
+    }
+
+    private String getFormat(Prize prize) {
+        if (prize == Prize.SECOND) {
+            return "%d개 일치, 보너스 볼 일치 (%s원) - %d개%n";
+        }
+        return "%d개 일치 (%s원) - %d개%n";
+    }
+
+    private String convertFormat(int winningMoney) {
+        DecimalFormat formatter = new DecimalFormat("###,###");
+        return formatter.format(winningMoney);
+    }
 }

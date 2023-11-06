72c166ba92434f1a20c7016d01e922705289df7f
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-07 03:43:07 +0900

    feat(LottoResultDto): 로또 모음에서 하나의 로또와 주어진 로또의 match기능 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -15,6 +15,28 @@ public class Lottos {
         return Lottos.size();
     }
 
+
+    public int match(Lotto a, int matchedNumber) {
+        int res = 0;
+        for (Lotto lotto : Lottos) {
+            if (a.match(lotto) == matchedNumber) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public int match(Lotto a, int matchedNumber, int bonusNumber) {
+        int res = 0;
+        for (Lotto lotto : Lottos) {
+            if (a.match(lotto) == matchedNumber && lotto.contain(bonusNumber)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+
     public MyLottosDto toDto() {
         List<List<Integer>> result = new ArrayList<>();
         for (Lotto lotto : Lottos) {

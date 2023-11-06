525cf426a7a835a0cba5cf9c8f2a7b81d0a9ed06
seunghye218 <seunghye@student.42seoul.kr>
2023-11-06 15:11:09 +0900

    refactor(Lotto): Separate function, Use constants

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,9 +5,15 @@ import java.util.List;
 public class Lotto {
 
     private final List<Integer> numbers;
+    static private final int NUMBERS_SIZE = 6;
     static private final int MIN_NUMBER = 1;
     static private final int MAX_NUMBER = 45;
-    static private final int SIZE_NUMBER = 46;
+    static private final int AVAILABLE_NUMBER = 46;
+    static private final String PROMPT_ERROR = "[ERROR]";
+    static private final String PROMPT_EXCEPTION_SIZE = PROMPT_ERROR + " 로또 번호는 " + NUMBERS_SIZE + "개여야 합니다.";
+    static private final String PROMPT_EXCEPTION_OUT_OF_RANGE =
+            PROMPT_ERROR + " 로또 번호는 " + MIN_NUMBER + "~" + MAX_NUMBER + " 사이의 수여야 합니다.";
+    static private final String PROMPT_EXCEPTION_DUPLICATE = PROMPT_ERROR + " 로또 번호는 서로 다른 수여야 합니다.";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -16,19 +22,26 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        checkSize(numbers);
+        checkForEachNumber(numbers);
+    }
 
-        final boolean[] checkDuplicateNumber = new boolean[SIZE_NUMBER];
+    private void checkForEachNumber(List<Integer> numbers) {
+        final boolean[] checkDuplicateNumber = new boolean[AVAILABLE_NUMBER];
         for (int number : numbers) {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 수여야 합니다.");
+                throw new IllegalArgumentException(PROMPT_EXCEPTION_OUT_OF_RANGE);
             }
             if (checkDuplicateNumber[number]) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 다른 수여야 합니다.");
+                throw new IllegalArgumentException(PROMPT_EXCEPTION_DUPLICATE);
             }
             checkDuplicateNumber[number] = true;
+        }
+    }
+
+    private void checkSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException(PROMPT_EXCEPTION_SIZE);
         }
     }
 }

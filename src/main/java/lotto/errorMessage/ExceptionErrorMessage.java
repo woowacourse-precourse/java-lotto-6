75faf15c43f946ca903f4a8b1e9954c8a29dd3cb
lusate <rayrayr@naver.com>
2023-11-06 17:44:08 +0900

    feat: 예외 메시지 설정

---
+++ src/main/java/lotto/errorMessage/ExceptionErrorMessage.java
@@ -0,0 +1,9 @@
+package lotto.errorMessage;
+
+public class ExceptionErrorMessage {
+    public static final String PURCHASE_LOTTO_PRICE = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
+    public static final String LOTTO_SIZE = "[ERROR] 당첨 번호는 6개만 입력해야 합니다.";
+    public static final String LOTTO_DUPLICATE = "[ERROR] 숫자가 중복되었습니다";
+    public static final String LOTTO_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INPUT_ONLY_NUMBER = "[ERROR] 로또 구입 금액은 숫자만 입력해야 합니다";
+}

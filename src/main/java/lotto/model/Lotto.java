f8299cce6a6aaa6e2d4bc685efa1f968c7c89d5a
gmu1026 <curiouscomponent@gmail.com>
2023-11-07 03:30:40 +0900

    feat: 로또 값 범위 검증 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 public class Lotto {
     private static final int NUMBERS_LENGTH = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,6 +18,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateNumbersLength(numbers);
         validateDuplicateNumbers(numbers);
+        validateRangeOverNumber(numbers);
     }
 
     private void validateNumbersLength(List<Integer> numbers) {
@@ -27,6 +30,16 @@ public class Lotto {
     private void validateDuplicateNumbers(List<Integer> numbers) {
         Set<Integer> numbersToSet = new HashSet<>(numbers);
         if (numbersToSet.size() != NUMBERS_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRangeOverNumber(List<Integer> numbers) {
+        long overNumberCount = numbers.stream()
+                .filter(number -> number > LOTTO_MAX_NUMBER
+                        || number < LOTTO_MIN_NUMBER).count();
+
+        if (overNumberCount != 0) {
             throw new IllegalArgumentException();
         }
     }

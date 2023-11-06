873ee372cce2bf70e9118917531e25f1d7eaf60e
PARKHWANYEONG <welcome0828@naver.com>
2023-11-07 00:17:39 +0900

    test(validator): 로또 구입 금액 입력값에 대한 테스트 추가
    - 유효한 구입 금액이 입력되었을때 예외가 발생하지 않는지 확인.
    - 유효하지 않은 구입 금액이 입력되었을 때 예외가 발생하는지 확인하고 각 예외 상황별 메시지가 맞는지 확인.

---
+++ src/test/java/lotto/validator/ValidatorTest.java
@@ -0,0 +1,37 @@
+package lotto.validator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class ValidatorTest {
+
+    @DisplayName("구입 금액이 유효할 경우 에러가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000","2000","50000","100000"})
+    void 유효한_구입금액_입력값_테스트(String input) {
+        assertThatCode(() -> Validator.purchaseAmount(input))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("입력값에 따라 설정한 예외 메시지와 예외를 발생시킨다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            " :정수만 입력이 가능합니다.",
+            "bab2 :정수만 입력이 가능합니다.",
+            "0:최소 구매 금액은 1000원 입니다.",
+            "800:최소 구매 금액은 1000원 입니다.",
+            "-800:최소 구매 금액은 1000원 입니다.",
+            "2100:1000원 단위로만 입력이 가능합니다.",},delimiter = ':')
+    void 유효하지_않은_구입금액_입력값_테스트(String input,String expectedErrorMessage) {
+        assertThatThrownBy(() -> Validator.purchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(expectedErrorMessage);
+    }
+}

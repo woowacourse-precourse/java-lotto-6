5df9dfee84b992575d893822ac2c1ed56a8dd42e
elyudwo <dudqk9696@naver.com>
2023-11-06 20:31:59 +0900

    [Test] : 로또 번호 문자열 변환 테스트 구현

--- src/test/java/lotto/utils/StringUtilTest.java
+++ src/test/java/lotto/utils/StringUtilTest.java
@@ -1,8 +1,11 @@
 package lotto.utils;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 class StringUtilTest {
@@ -17,7 +20,20 @@ class StringUtilTest {
         int purchaseAmount = StringUtil.stringToInt(inputValue);
 
         // then
-        Assertions.assertThat(purchaseAmount).isEqualTo(5000);
+        assertThat(purchaseAmount).isEqualTo(5000);
+    }
+
+    @DisplayName("정답 로또 번호 문자열을 Integer List 로 변환한다.")
+    @Test
+    void stringToList() {
+        // given
+        String inputValue = "1,2,3,4,5,6";
+
+        // when
+        List<Integer> answerLotto = StringUtil.StringToList(inputValue);
+
+        // then
+        assertThat(answerLotto).contains(1, 2, 3, 4, 5, 6);
     }
 
     @DisplayName("총 수익률 double 값을 문자열 형식으로 변환한다.")
@@ -30,6 +46,6 @@ class StringUtilTest {
         String printReturnValue = StringUtil.doubleToString(rateOfReturn);
 
         // then
-        Assertions.assertThat(printReturnValue).isEqualTo("총 수익률은 62.5%입니다.");
+        assertThat(printReturnValue).isEqualTo("총 수익률은 62.5%입니다.");
     }
 }

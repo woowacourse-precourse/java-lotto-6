43b8702407255a17c3b0e12ff9710c91c279da3d
hackertaco <imsa1224@korea.ac.kr>
2023-11-06 14:54:15 +0900

    feat(RankGroup, RankTemplate): add model for aggregate result

---
+++ src/main/java/lotto/RankGroup.java
@@ -0,0 +1,53 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum RankGroup {
+    FIRST(false, 6, 0, 2000000000, RankTemplate.FIRST),
+    SECOND(true, 6, 0, 30000000, RankTemplate.SECOND),
+    THIRD(false,5, 0, 1500000, RankTemplate.THIRD),
+    FOURTH(false,4, 0, 50000, RankTemplate.FOURTH),
+    FIFTH(false, 3, 0, 5000, RankTemplate.FIFTH)
+    ;
+    private boolean haveBonusNumber;
+
+    private int lottoNumberCount;
+    private int result;
+    private int reward;
+    private RankTemplate template;
+    RankGroup(boolean haveBonusNumber, int lottoNumberCount, int result, int reward, RankTemplate template){
+        this.haveBonusNumber = haveBonusNumber;
+        this.lottoNumberCount = lottoNumberCount;
+        this.result = result;
+        this.reward = reward;
+        this.template = template;
+    }
+
+    public static Optional<RankGroup> findByLotto(boolean bonus, int lotto){
+        return Arrays.stream(RankGroup.values())
+                .filter(rankGroup -> rankGroup.haveBonusNumber == bonus && rankGroup.lottoNumberCount == lotto)
+                .findAny();
+    }
+
+    public static void increaseResult(RankGroup group){
+        group.result++;
+    }
+
+    public static int getTotalReward(){
+        int total = 0;
+        for(RankGroup rankGroup: RankGroup.values()){
+            total += rankGroup.result * rankGroup.reward;
+        }
+        return total;
+    }
+    public String getTemplate(){
+        return template.getTemplate();
+    }
+    public int getReward(){
+        return reward;
+    }
+    public int getResult(){
+        return result;
+    }
+}
---
+++ src/main/java/lotto/RankTemplate.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum RankTemplate {
+    FIRST("6개 일치"),
+    SECOND("5개 일치, 보너스 볼 일치"),
+    THIRD("5개 일치"),
+    FOURTH("4개 일치"),
+    FIFTH("3개 일치"),
+    EMPTY("없음");
+    private final String template;
+
+    RankTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+}

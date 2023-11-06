b137d02e4de92be4966fd74954d7c101360a4ea2
dukcode <duk9741@gmail.com>
2023-11-06 15:59:10 +0900

    test: 당첨 번호와 중복되는 보너스 번호 입력 불가능 테스트 구현

--- src/test/java/lotto/LottoBonusNumberCreateTest.java
+++ src/test/java/lotto/LottoBonusNumberCreateTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -11,36 +12,52 @@ class LottoBonusNumberCreateTest {
     @Test
     public void LottoBonusNumberCreate_를_생성할_수_있다() throws Exception {
         // given
-        int bonusNumber = 1;
+        LottoWinningNumberCreate lottoWinningNumberCreate = new LottoWinningNumberCreate(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
 
         // when
         // then
         Assertions.assertThatCode(() -> {
-            new LottoBonusNumberCreate(bonusNumber);
+            new LottoBonusNumberCreate(lottoWinningNumberCreate, bonusNumber);
         }).doesNotThrowAnyException();
     }
 
     @Test
     public void 범위_미만의_보너스_번호로_LottoBonusNumberCreate_생성_시_예외가_발생한다() throws Exception {
         // given
+        LottoWinningNumberCreate lottoWinningNumberCreate = new LottoWinningNumberCreate(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = LottoConstant.LOTTO_START_NUMBER - 1;
 
         // when
         // then
         Assertions.assertThatThrownBy(() -> {
-            new LottoBonusNumberCreate(bonusNumber);
+            new LottoBonusNumberCreate(lottoWinningNumberCreate, bonusNumber);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 범위_초과의_보너스_번호로_LottoBonusNumberCreate_생성_시_예외가_발생한다() throws Exception {
         // given
+        LottoWinningNumberCreate lottoWinningNumberCreate = new LottoWinningNumberCreate(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = LottoConstant.LOTTO_END_NUMBER + 1;
 
         // when
         // then
         Assertions.assertThatThrownBy(() -> {
-            new LottoBonusNumberCreate(bonusNumber);
+            new LottoBonusNumberCreate(lottoWinningNumberCreate, bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호와_중복된_보너스_번호로_LottoBonusNumberCreate_생성_시_예외가_발생한다() throws Exception {
+        // given
+        LottoWinningNumberCreate lottoWinningNumberCreate = new LottoWinningNumberCreate(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 1;
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> {
+            new LottoBonusNumberCreate(lottoWinningNumberCreate, bonusNumber);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }

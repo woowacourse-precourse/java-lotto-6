d4ea327b7ad8b0b38eaca58aff3fad83e41e70f0
HYOAN <aspqll@naver.com>
2023-11-06 22:49:59 +0900

    feat(lotto): 구입금액 단위에 대한 예외처리 메서드 테스트코드 구현

--- src/test/java/lotto/domain/UserInputCheckerTest.java
+++ src/test/java/lotto/domain/UserInputCheckerTest.java
@@ -1,4 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserInputCheckerTest {
-  
+    private UserInputChecker userInputChecker;
+
+    @BeforeEach
+    void sutUp() {
+        userInputChecker = new UserInputChecker();
+    }
+
+    @Test
+    void 구입금액_확인() {
+        assertThrows(IllegalArgumentException.class, () -> userInputChecker.checkInputUnit(10010));
+    }
 }

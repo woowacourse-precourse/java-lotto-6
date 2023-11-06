c965aa13f6d1652fbcb2987306b4d9598dbfbb5d
minisyu <iswater.kim@gmail.com>
2023-11-06 11:41:41 +0900

    refactor: LottoController try-catch문 제거, LottoController에 당첨 번호와 보너스 번호 입력을 통한 최종 통계 표시 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
 import lotto.dto.input.MoneyDto;
+import lotto.dto.input.WinningCombinationDto;
+import lotto.dto.output.DrawingResultDto;
 import lotto.dto.output.LottosDto;
 import lotto.service.LottoService;
 import lotto.view.InputView;
@@ -19,24 +21,24 @@ public class LottoController {
 
     public void run() {
         // 입력
-        MoneyDto money = getAmountFromUserInput();
+        MoneyDto money = inputView.inputMoney();
         // 실행
-        LottosDto lottoTickets = service.drawLottoTicketsWithGivenMoney(money);
+        LottosDto lottoTickets = service.purchaseLottoTickets(money);
         // 출력
         printPurchasedLottoTickets(lottoTickets);
 
+        // 입력
+        WinningCombinationDto winningNumbers = inputView.inputWinningNumbers();
+        //BonusNumberDto bonusNumber = getBonusNumberFromUserInput();
+        // 실행
+        //DrawingResultDto result = service.findPrize(winningNumbers, bonusNumber);
+        DrawingResultDto result = service.findPrize(winningNumbers);
+        // 출력
+        outputView.printResult(result);
     }
 
     private void printPurchasedLottoTickets(LottosDto lottoTickets) {
-        outputView.printLottoTickets(lottoTickets);
+        outputView.printPurchasedLottoTickets(lottoTickets);
     }
 
-    private MoneyDto getAmountFromUserInput() {
-        try {
-            return inputView.inputMoney();
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] " + e.getMessage());
-            return getAmountFromUserInput();
-        }
-    }
 }

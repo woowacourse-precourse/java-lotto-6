be9eada9810fbbcdd37876992de616adeb3315c6
devholic22 <hyunjoon.tech@gmail.com>
2023-11-06 15:26:40 +0900

    refactor: 예외 메시지에 LottoRule 적용되도록 수정
    LottoRule의 값에 따라 예외 메시지가 달라질 수 있도록 서로 연결

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,13 +1,21 @@
 package lotto.exception;
 
+import static lotto.constants.LottoRule.LOTTO_COST_REMAIN_VALUE;
+import static lotto.constants.LottoRule.LOTTO_NUMBER_LENGTH;
+import static lotto.constants.LottoRule.MAXIMUM_NUMBER;
+import static lotto.constants.LottoRule.MINIMUM_NUMBER;
+
 public enum ExceptionMessage {
 
     NUMBER_FORMAT_EXCEPTION("숫자가 아니거나 너무 큰 수, 또는 너무 작은 수를 입력하였습니다."),
     NEGATIVE_NUMBER_EXCEPTION("양수 (0 포함)여야 합니다."),
     NUMBER_DUPLICATE_EXCEPTION("숫자들은 모두 독립적이어야 합니다."),
-    MONEY_REMAIN_EXCEPTION("구입 금액은 나누어 떨어져야 합니다."),
-    GOAL_NUMBER_SIZE_EXCEPTION("올바른 로또 정답 갯수가 아닙니다."),
-    UNVALID_GOAL_NUMBER("로또에서 생성될 수 없는 숫자가 있습니다."),
+    MONEY_REMAIN_EXCEPTION("구입 금액은 " + LOTTO_COST_REMAIN_VALUE.getValue() + "으로 나누어 떨어져야 합니다."),
+    GOAL_NUMBER_SIZE_EXCEPTION("올바른 로또 정답 갯수가 아닙니다. 정답 갯수는 " + LOTTO_NUMBER_LENGTH.getValue() + "개여야 합니다."),
+    UNVALID_GOAL_NUMBER(
+            "로또에서 생성될 수 없는 숫자가 있습니다. 로또 번호는 " + MINIMUM_NUMBER.getValue() + " ~ " +
+            MAXIMUM_NUMBER.getValue() + " 사이여야 합니다."
+    ),
     BONUS_NUMBER_ALREADY_USE("정답 번호에 없는 숫자여야만 합니다.");
 
     private static final String DEFAULT_MESSAGE = "[ERROR] ";

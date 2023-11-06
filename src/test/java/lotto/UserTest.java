b384ec3149f474f6ca503159d421e75b3a0a4b98
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 19:33:38 +0900

    test(UserTest.java): add createUserMoneyRange test 사용자가 프로그램에서 지정한 로또 구입 금액의 최솟값보다 작은 값을 입력하거나 최댓값보다 큰 값을 입력하였을 때 IllegalArgumentException이 발생하는지 검증하였습니다.

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -15,4 +15,46 @@ public class UserTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("금액이 int의 최댓값을 초과하면 예외가 발생한다.")
+    @Test
+    void createUserWithOverIntegerRange() {
+        assertThatThrownBy(() -> new User("2147483648"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 int의 최댓값을 초과하면 예외가 발생한다.")
+    @Test
+    void createUserWithTooBigNumber() {
+        assertThatThrownBy(() -> new User("9999999999999999999999"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 int의 최솟값 미만이면 예외가 발생한다.")
+    @Test
+    void createUserWithUnderIntegerRange() {
+        assertThatThrownBy(() -> new User("-2147483649"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 int의 최솟값 미만이면 예외가 발생한다.")
+    @Test
+    void createUserWithTooSmallNumber() {
+        assertThatThrownBy(() -> new User("-9999999999999999999999"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 1000원 미만이면 예외가 발생한다.")
+    @Test
+    void createUserWithUnderMinMoneyRange() {
+        assertThatThrownBy(() -> new User("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 1000원 미만이면 예외가 발생한다.")
+    @Test
+    void createUserWithOverMaxMoneyRange() {
+        assertThatThrownBy(() -> new User("1000001000"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

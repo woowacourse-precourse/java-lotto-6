c6b2df208e453b79c70e72e4a4d547883177f2e8
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 00:29:29 +0900

    feat: add money to count function to user lotto model

--- src/main/java/lotto/model/UserLotto.java
+++ src/main/java/lotto/model/UserLotto.java
@@ -1,4 +1,13 @@
 package lotto.model;
 
+import java.util.List;
+
 public class UserLotto {
+
+    private int lottoCount;
+    private List<Lotto> lottos;
+
+    private void moneyToLotto(int money) {
+        this.lottoCount = money / 1000;
+    }
 }

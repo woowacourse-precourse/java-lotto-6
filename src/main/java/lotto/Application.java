b8dc8f7f190c751fec133e8920f8f20885d6bcac
kangyuseok <kingyuseok@naver.com>
2023-11-06 20:20:26 +0900

    Feat: 게임 시작을 호출하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import static lotto.controller.LottoGame.lottoGameStart;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        lottoGameStart();
     }
 }

80f941aa289642ad277c2faaadc99c4289ce78f1
jiohh <dkswlgh99@naver.com>
2023-11-06 20:33:06 +0900

    feat(domain): Cost 자료구조에 검증 코드 결합
    생성자에 검증 결합

--- src/main/java/lotto/domain/Cost.java
+++ src/main/java/lotto/domain/Cost.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
 
+import lotto.validation.CostValidator;
+
 public class Cost {
     private final int number;
 
     public Cost(int number) {
+        CostValidator.validateParsedCost(number);
         this.number = number;
     }
 }

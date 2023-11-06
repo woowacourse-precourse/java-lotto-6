0fe050f96e91340e72be462f6f3b82b0b855d817
isyoo1213 <moethns@gmail.com>
2023-11-06 17:25:24 +0900

    feat: InputView 입력 기능 시 출력구문 추가 InputView - askPaymentAmount() - askAnswerLottoNumbers() - askBonusNumber()

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,15 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String askPaymentAmount() {
+        System.out.println("1,000원 단위의 로또 구매 금액을 입력해주세요.");
         return Console.readLine();
     }
 
     public String askAnswerLottoNumbers() {
+        System.out.println("쉼표(,)를 구분자로 6자리 당첨 번호를 입력해주세요.");
         return Console.readLine();
     }
 
     public String askBonusNumber() {
-        System.out.println("보너스 번호를 입력해주세요.");
+        System.out.println("보너스 번호 1자리를 입력해주세요.");
         return Console.readLine();
     }
 }

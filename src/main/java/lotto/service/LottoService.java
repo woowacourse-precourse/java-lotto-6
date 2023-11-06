61a635af4da39a92f341ce9bc249b0a111a633d0
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-07 04:12:56 +0900

    feat(service): 랜덤 생성 로또 번호가 오름차순으로 생성되는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,10 +1,13 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoService {
     public List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumbers.sort(Comparator.naturalOrder());
+        return lottoNumbers;
     }
 }

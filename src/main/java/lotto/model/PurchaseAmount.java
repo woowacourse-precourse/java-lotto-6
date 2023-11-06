d8d2b71bc29e4c436dc2dbd8b7b90181390ac013
insu <is23193160@gmail.com>
2023-11-06 16:35:30 +0900

    feat: 사용자가 1000 단위로 금액을 입력 하지 않을 시 예외 처리하는 기능 추가

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -3,6 +3,7 @@ package lotto.model;
 public class PurchaseAmount {
     private static final int LOTTO_PRICE = 1000;
     private static final String INPUT_LEAST_PRICE_ERROR_MESSAGE = "[ERROR] 최소 1000원 이상의 돈을 입력해주세요.";
+    private static final String INPUT_MULTIPLE_OF_THOUSAND_MESSAGE = "[ERROR] 1000원 단위의 금액만 입력해주세요.";
     private final int purchaseAmount;
 
     public PurchaseAmount(int inputMoney) {
@@ -12,6 +13,17 @@ public class PurchaseAmount {
 
     private void validate(int inputMoney) {
         checkLeastLottoPrice(inputMoney);
+        checkMultipleOfThousand(inputMoney);
+    }
+
+    private void checkMultipleOfThousand(int inputMoney) {
+        if (isNotMultipleOfThousand(inputMoney)) {
+            throw new IllegalArgumentException(INPUT_MULTIPLE_OF_THOUSAND_MESSAGE);
+        }
+    }
+
+    private boolean isNotMultipleOfThousand(int inputMoney) {
+        return inputMoney % LOTTO_PRICE != 0;
     }
 
     private void checkLeastLottoPrice(int inputMoney) {

7e9090f11abe9ed351baea056f8b264c19d1e101
chansoo <chansoo38@gmail.com>
2023-11-06 23:20:43 +0900

    <Test> WinningLottoTest - 로또 당첨 번호와 로또 번호를 비교해 결과를 생성하는 기능을 테스트한다.

--- src/test/java/lotto/Domain/WinningLottoTest.java
+++ src/test/java/lotto/Domain/WinningLottoTest.java
@@ -15,4 +15,69 @@ class WinningLottoTest {
                 .isInstanceOf(
                         IllegalArgumentException.class);
     }
+
+    @DisplayName("로또번호가 당첨번호와 모두 일치한다면 1등을 반한한다.")
+    @Test
+    void returnFirstRankWhenHitSix() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+
+        Assertions.assertThat(winningLotto.getRankOf(lotto)).isEqualTo(Rank.FIRST);
+    }
+    @DisplayName("로또번호가 당첨번호와 5개 일치하고 보너스 번호를 가지고 있다면 2등을 반한한다.")
+    @Test
+    void returnSecondRankWhenHitFiveAndBonus() {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        Lotto myLotto = new Lotto(List.of(1,2,3,4,5,7));
+
+        Assertions.assertThat(winningLotto.getRankOf(myLotto)).isEqualTo(Rank.SECOND);
+    }
+    @DisplayName("로또번호가 당첨번호와 5개 일치하고 보너스 번호를 포함하지않는다면 3등을 반한한다.")
+    @Test
+    void returnThirdRankWhenHitFive() {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        Lotto myLotto = new Lotto(List.of(1,2,3,4,5,8));
+
+        Assertions.assertThat(winningLotto.getRankOf(myLotto)).isEqualTo(Rank.THIRD);
+    }
+    @DisplayName("로또번호가 당첨번호와 4개 일치하면 4등을 반한한다.")
+    @Test
+    void returnFourthRankWhenHitFour() {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        Lotto myLotto = new Lotto(List.of(1,2,3,4,8,9));
+
+        Assertions.assertThat(winningLotto.getRankOf(myLotto)).isEqualTo(Rank.FOURTH);
+    }
+    @DisplayName("로또번호가 당첨번호와 3개 일치하면 5등을 반한한다.")
+    @Test
+    void returnFifthRankWhenHitThree() {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        Lotto myLotto = new Lotto(List.of(1,2,3,8,9,10));
+
+        Assertions.assertThat(winningLotto.getRankOf(myLotto)).isEqualTo(Rank.FIFTH);
+    }
+    @DisplayName("로또번호가 당첨번호와 2개보다 적게 일치하면 등수가 없다.")
+    @Test
+    void returnNoRankWhenHitLowerThanTwo() {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningLotto winningLotto = new WinningLotto(winningNumber, bonusNumber);
+
+        Lotto myLotto = new Lotto(List.of(1,2,8,9,10,11));
+
+        Assertions.assertThat(winningLotto.getRankOf(myLotto)).isEqualTo(Rank.NOTHING);
+    }
 }

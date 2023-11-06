6e26b6f54d05a5ede55de104e8fa442bda3fff42
hil679 <hil679922@gmail.com>
2023-11-07 05:06:23 +0900

    Fix(RandomNumber): sort로 인해 test 실패 수정
    - immutableList를 따로 sort하여 오류 발생

--- src/main/java/lotto/domain/RandomNumber.java
+++ src/main/java/lotto/domain/RandomNumber.java
@@ -2,19 +2,16 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomNumber {
     public List<Integer> generateRandomNumbers() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-
-        sortRandomNumbers(randomNumbers);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
 
         return randomNumbers;
-    }
-
-    private void sortRandomNumbers(List<Integer> randomNumbers) {
-        randomNumbers.sort(Comparator.naturalOrder());
     }
 }

b1723313550da08e7491a379d3d43b79854ab904
LeeJJ07 <jkj9513@naver.com>
2023-11-06 15:15:13 +0900

    feat : 입력된 보너스 번호가 유효한 입력인지 판별하는 기능 구현
    입력된 보너스 번호가 숫자로 입력이 된지, 1~45 사이의 숫자인지, 기존 복권에 있는 번호와 중복되지 않은지 확인하는 기능 구현

--- src/main/java/lotto/controller/CheckValidateInput.java
+++ src/main/java/lotto/controller/CheckValidateInput.java
@@ -1,35 +1,57 @@
 package lotto.controller;
 
+import java.util.List;
+
 public class CheckValidateInput {
-	
+
 	public int money(String money) {
-		
-		if(!isInteger(money))
+
+		if (!isInteger(money))
 			throw new IllegalArgumentException();
-		if(!isUnits1000(Integer.parseInt(money)))
+		if (!isUnits1000(Integer.parseInt(money)))
 			throw new IllegalArgumentException();
-		if(!inRange(Integer.parseInt(money)))
+		if (!inRangeMoney(Integer.parseInt(money)))
 			throw new IllegalArgumentException();
 
 		return Integer.parseInt(money);
 	}
 
-	private boolean isInteger(String money) {
+
+
+	private boolean isUnits1000(int money) {
+		return (money % 1000) == 0;
+	}
+
+	private boolean inRangeMoney(int money) {
+		return (money > 0) && (money < Integer.MAX_VALUE);
+	}
+
+	public int bonusNumber(String bonusNumber, List<Integer> lotto) {
+
+		if(!isInteger(bonusNumber))
+			throw new IllegalArgumentException();
+		if(!inRangeBonusNumber(Integer.parseInt(bonusNumber)))
+			throw new IllegalArgumentException();
+		if(numberInLotto(Integer.parseInt(bonusNumber), lotto))
+			throw new IllegalArgumentException();
+		
+		return Integer.parseInt(bonusNumber);
+	}
+	
+	private boolean numberInLotto(int bonusNumber, List<Integer> lotto) {
+		return lotto.contains(bonusNumber);
+	}
+	
+	private boolean inRangeBonusNumber(int bonusNumber) {
+		return (bonusNumber > 0) && (bonusNumber <= 45);
+	}
+
+	private boolean isInteger(String number) {
 		try {
-			Integer.parseInt(money);
+			Integer.parseInt(number);
 			return true;
 		} catch (NumberFormatException ex) {
 			return false;
 		}
 	}
-	
-	private boolean isUnits1000(int money) {
-		return (money%1000) == 0;	
-	}
-	
-	private boolean inRange(int money) {
-		return (money>0) && (money<Integer.MAX_VALUE);
-	}
-
-
 }

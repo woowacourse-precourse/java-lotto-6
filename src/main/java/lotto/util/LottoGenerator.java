fa8eb2ae8747ae04218b0fc2b916bf160b052688
Mr.Kim <joajy98@gmail.com>
2023-11-06 17:46:25 +0900

    feat: 로또 번호 생성 후 반환 시 정렬 후 반환하도록 수정

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -1,15 +1,16 @@
-package lotto.domain;
+package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
 
     public static List<Lotto> generateRandomLottos(int purchaseAmount){
         List<Lotto> lottos = new ArrayList<>();
-        System.out.println(purchaseAmount);
         for(int i = 0; i < purchaseAmount; ++i){
             lottos.add(generateRandomLotto());
         }
@@ -18,6 +19,7 @@ public class LottoGenerator {
 
     public static Lotto generateRandomLotto(){
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumbers = lottoNumbers.stream().sorted().collect(Collectors.toList());
         return new Lotto(lottoNumbers);
     }
 }

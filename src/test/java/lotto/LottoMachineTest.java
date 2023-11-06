9a3598249b0c02ed9ee1f4dda0b90d041b2ff3d0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 15:53:29 +0900

    refactor(lottoMachine): 다른 테스트들과 코드 스타일 맞추기

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -2,7 +2,6 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
@@ -10,14 +9,10 @@ class LottoMachineTest {
 
     @Test
     void 로또_수량에_맞게_로또_리스트가_생성되는가() {
-        // given
         int lottoPrice = 1_000;
         int lottoQuantity = 10;
 
-        // when
-        List<Lotto> actual = lottoMachine.buyLottos(lottoPrice * lottoQuantity);
-
-        // then
-        assertThat(actual).hasSize(lottoQuantity);
+        assertThat(lottoMachine.buyLottos(lottoPrice * lottoQuantity))
+                .hasSize(lottoQuantity);
     }
 }

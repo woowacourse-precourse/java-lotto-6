9082db9829be090e06daa8fce42c59921dfef096
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:37:35 +0900

    feat: 보너스 번호 검증 기능 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -7,4 +7,14 @@ public class BonusNumber {
     public BonusNumber(Integer bonusNumber) {
         this.bonusNumber = bonusNumber;
     }
+
+    private static void validateBonusNumber(Integer bonusNumber) {
+        validateBonusNumberRange(bonusNumber);
+    }
+
+    private static void validateBonusNumberRange(Integer bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 범위의 정수이어야 합니다.");
+        }
+    }
 }

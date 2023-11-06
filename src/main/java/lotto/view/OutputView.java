79dd663fb9e4be24c668df7217d2e9768a800b48
Mr.Kim <joajy98@gmail.com>
2023-11-06 17:47:04 +0900

    refactor: 출력 구문에서만 사용할 만한 상수를 제외한 나머지 상수를 Constant로 이동

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,23 +6,24 @@ import lotto.domain.Rank;
 import java.util.List;
 
 import static lotto.service.LottoService.*;
+import static lotto.util.Constant.DIVIDER_FOR_COUNT_PURCHASE_QUANTITY;
+import static lotto.util.Constant.LOTTO_NUMBERS_COUNT;
 
 public class OutputView {
 
-    private static final String LOTTO_NUMBER_DELIMITER = ", ";
+    private static final String LOTTO_NUMBER_DELIMITER_FOR_OUTPUT = ", ";
     private static final String OPEN_BRACKET = "[";
     private static final String CLOSE_BRACKET = "]";
-    private static final int DIVIDER_FOR_COUNT_PURCHASE_AMOUNT = 1000;
     private static final String PURCHASE_COUNT_ALERT = "개를 구매했습니다.";
-    private static final int LOTTO_NUMBERS_COUNT = 6;
 
     private static final String WINNING_STATUS = "당첨 통계\n---";
     private static final String TOTAL_EARNING_RATIO = "총 수익률은 %.1f%%입니다.\n";
 
 
     public static void printPurchaseAmount(int purchaseMoney){
-        int purchaseAmount = purchaseMoney / DIVIDER_FOR_COUNT_PURCHASE_AMOUNT;
+        int purchaseAmount = purchaseMoney / DIVIDER_FOR_COUNT_PURCHASE_QUANTITY;
         System.out.println(purchaseAmount + PURCHASE_COUNT_ALERT);
+        System.out.println();
     }
 
     public static void printAllPurchaseLotto(List<Lotto> lottoNumbers) {
@@ -37,7 +38,7 @@ public class OutputView {
         List<Integer> lottoNumbers = purchasedNumbers.getLotto();
         for (int i = 0;i < LOTTO_NUMBERS_COUNT - 1; ++i) {
             int number = lottoNumbers.get(i);
-            System.out.print(number + LOTTO_NUMBER_DELIMITER);
+            System.out.print(number + LOTTO_NUMBER_DELIMITER_FOR_OUTPUT);
         }
         System.out.println(lottoNumbers.get(LOTTO_NUMBERS_COUNT - 1) + CLOSE_BRACKET);
     }

b0c3a92fb070bf2ad0ce0dc480a4e5eedb002179
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-06 16:50:39 +0900

    feat : 입력금액이 0원 이하인 경우에 대한 예외처리 추가

--- src/main/java/lotto/Model/CacheValidator.java
+++ src/main/java/lotto/Model/CacheValidator.java
@@ -4,11 +4,20 @@ public class CacheValidator {
 
 
     public static void exceptCache(Integer insertData){
-        if(validateThousand(insertData)){
-            throw new IllegalArgumentException("금액은 1000원 단위로 입력되어야 합니다.");
+        if(validateZero(insertData)){
+            throw new IllegalArgumentException("[ERROR] 입력 금액은 0원을 초과해야 합니다.");
         }
+
+        if(validateThousand(insertData)){
+            throw new IllegalArgumentException("[ERROR] 입력 금액은 1000원 단위로 입력되어야 합니다.");
+        }
+
     }
     private static boolean validateThousand(Integer insertData){
         return insertData%1000 != 0;
+    }
+
+    private static boolean validateZero(Integer insertData){
+        return insertData <= 0;
     }
 }

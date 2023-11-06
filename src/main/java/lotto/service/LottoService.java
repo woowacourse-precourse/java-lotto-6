a239ca87cca43554eb61f23a140224d6b1558983
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-06 23:55:01 +0900

    feat(LottoService) : 구입 금액이 1000원 단위로 나누어 떨어지지 않을 경우 예외처리

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,9 +3,24 @@ package lotto.service;
 public class LottoService {
 
     private static final int LOTTO_TICKET_PRICE = 1000;
+    private static final String FIRST_ERROR_MESSAGE = "[ERROR] ";
+    private static final String PURCHASE_AMOUNT_DIVIDE_EXCEPTION_MESSAGE = "로또 구입 금액은 1,000원으로 나누어 떨어져야합니다.";
 
     public int calculateNumberOfLottoTickets(String purchaseAmount) {
-        int amount = Integer.parseInt(purchaseAmount);
-        return amount / LOTTO_TICKET_PRICE;
+        validateLottoPurchaseAmount(purchaseAmount);
+        return Integer.parseInt(purchaseAmount) / LOTTO_TICKET_PRICE;
     }
+
+    public void validateLottoPurchaseAmount(String lottoPurchaseAmount) {
+
+        if (isRestOfLottoPurchaseAmountDivideBy1000NotZero(lottoPurchaseAmount)) {
+            throw new IllegalArgumentException(FIRST_ERROR_MESSAGE + PURCHASE_AMOUNT_DIVIDE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isRestOfLottoPurchaseAmountDivideBy1000NotZero(String lottoPurchaseAmount) {
+        int amount = Integer.parseInt(lottoPurchaseAmount);
+        return amount % LOTTO_TICKET_PRICE != 0;
+    }
+
 }

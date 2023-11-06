baa4bd545a12138f6b9a48839274e6cc754904a6
sinminseok12 <sinminseok12@naver.com>
2023-11-07 01:26:33 +0900

    feat(InputView): 입력 검증 기능 적용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,25 +10,26 @@ import static lotto.utils.Parser.parseToIntegers;
 public class InputView {
     private static final String INPUT_BUY_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WIN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE =  "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public static Integer inputPurchaseAmount() {
         System.out.println(INPUT_BUY_MONEY_MESSAGE);
         String input = Console.readLine();
-        //todo 구입 금액 검증
+        InputValidator.validatePurchaseAmount(input);
         return parseToInteger(input);
     }
 
     public static List<Integer> inputWinNumbers() {
         System.out.println(INPUT_WIN_NUMBERS_MESSAGE);
         String input = Console.readLine();
-        //todo 당첨 번호 검증 기능
+        InputValidator.validateWinNumbers(input);
         return parseToIntegers(input);
     }
 
     public static Integer inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         String input = Console.readLine();
-        //todo 보너스 번호 검증 기능
+        InputValidator.validateBonusNumber(input);
         return parseToInteger(input);
     }
 }

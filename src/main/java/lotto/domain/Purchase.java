9b80fd20af356ed83b1d3bf7dba3c74b6fa425d0
potatomoong <star901210@hanmail.net>
2023-11-07 02:44:43 +0900

    Feat: 구입금액 입력받는 기능 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,4 +1,12 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Purchase {
+    private static final int LOTTO_PRICE = 1000;
+
+    public static int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
 }

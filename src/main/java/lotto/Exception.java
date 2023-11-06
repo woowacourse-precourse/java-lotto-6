11fbb5ec2d02a0313cf86d2aa6a91f48cd8f1967
KIVSH <kihongkim3@hotmail.com>
2023-11-06 23:10:31 +0900

    feat: add overLimitNumberRange method in Exception class

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -7,29 +7,33 @@ public class Exception {
         try {
             Integer.parseInt(inputData);
         } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.\n");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
 
     public static void notThousandWon(String inputData) throws IllegalArgumentException {
+        final int thousandWon = 1000;
+        final int zero = 0;
         int amount = Integer.parseInt(inputData);
 
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력 가능합니다.\n");
+        if (amount == zero || amount % thousandWon != zero) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력 가능합니다.");
         }
     }
 
     public static void overLimitCount(int inputSize, int limitCount) throws IllegalArgumentException {
-        if (inputSize > limitCount) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개만 입력 가능합니다.\n");
+        if (inputSize != limitCount) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개만 입력 가능합니다.");
         }
     }
 
     public static void duplicateWinNumbers(List<Integer> winNumbers) throws IllegalArgumentException {
+        final int firstIndex = 0;
+
         for (int i = 0; i < winNumbers.size(); i++) {
-            int number = winNumbers.remove(0);
+            int number = winNumbers.remove(firstIndex);
             if (winNumbers.contains(number)) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호는 서로 다른 숫자만 입력 가능합니다.\n");
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 서로 다른 숫자만 입력 가능합니다.");
             }
             winNumbers.add(number);
         }
@@ -37,7 +41,16 @@ public class Exception {
 
     public static void duplicateBonusNumber(List<Integer> winNumbers, int bonusNumber) throws IllegalArgumentException {
         if (winNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 서로 다른 숫자만 입력 가능합니다.\n");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 서로 다른 숫자만 입력 가능합니다.");
+        }
+    }
+
+    public static void overLimitNumberRange(int number) throws IllegalArgumentException {
+        final int startRangeNumber = 1;
+        final int endRangeNumber = 45;
+
+        if (number < startRangeNumber || number > endRangeNumber) {
+            throw new IllegalArgumentException("[ERROR] 번호는 1부터 45 사이의 숫자만 입력 가능합니다.");
         }
     }
 }

c515c296d42c94b0f2a2ac6f1a569893f652d6b3
IE-MangChi <hsm983278@gmail.com>
2023-11-06 17:24:23 +0900

    design: 개행 문자 추가

--- src/main/java/lotto/view/constant/viewMessage.java
+++ src/main/java/lotto/view/constant/viewMessage.java
@@ -3,11 +3,11 @@ package lotto.view.constant;
 public class viewMessage {
 
     public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.\n";
-    public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.\n";
-    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n";
+    public static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.\n";
+    public static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.\n";
 
-    public static final String OUTPUT_PURCHASE_RESULT = "%d개를 구매했습니다.\n";
-    public static final String OUTPUT_WINNING_STATISTICS_INTRO = "당첨 통계\n---\n";
+    public static final String OUTPUT_PURCHASE_RESULT = "\n%d개를 구매했습니다.\n";
+    public static final String OUTPUT_WINNING_STATISTICS_INTRO = "\n당첨 통계\n---\n";
     public static final String OUTPUT_WINNING_STATISTICS_RESULT =
             "3개 일치 (5,000원) - %d개\n" +
                     "4개 일치 (50,000원) - %d개\n" +

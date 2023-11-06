06c6bf965d109dcbf24ac6de71bc8b52996a4f71
jhsseonn <hs125bt@gmail.com>
2023-11-06 19:36:39 +0900

    feat: 로또 구입 금액 입력받기

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,4 +1,11 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoView {
+
+    public String inputLottoPurchase(){
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
 }

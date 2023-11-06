75db19f723125eef233e49a409d3518450242cd5
D0ri123 <lsh81550@gmail.com>
2023-11-06 15:32:29 +0900

    refactor(InputValidator): 중복 메서드 정리 - 3차 구현
    중복되는 메서드들을 default로 선언하여 인터페이스에 구현했습니다.

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,7 +1,21 @@
 package lotto.util;
 
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
 public interface InputValidator {
 
     boolean getInputUntilValid();
+
+    default void getInput(Supplier<String> requestMethod, BiConsumer<InputValidator, String> setInfoMethod) {
+        while (getInputUntilValid()) {
+            try {
+                String input = requestMethod.get();
+                setInfoMethod.accept(this, input);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
 }

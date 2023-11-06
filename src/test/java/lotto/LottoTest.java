1f6715acce89f403b82b0ff0615481985763ec28
slowcloud <sju9417@gmail.com>
2023-11-06 16:11:36 +0900

    test: Lotto에 대한 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,33 +11,46 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Lotto 테스트")
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+
+    @DisplayName("생성자 테스트")
+    @Nested
+    class constructor {
+
+        @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+        @Test
+        void createLottoByOverSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+        @Test
+        void createLottoByDuplicatedNumber() {
+            // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
+    
+    @DisplayName("toString 테스트")
+    @Nested
+    class toString {
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @DisplayName("입력한 로또 번호가 정렬되어서 출력되어야 한다.")
+        @Test
+        void printLottoWithUnsortedIntegers() {
 
-    @DisplayName("입력한 로또 번호가 정렬되어서 출력되어야 한다.")
-    @Test
-    void printLottoWithUnsortedIntegers() {
+            List<Integer> numbers = new ArrayList<>(List.of(6,5,4,3,2,1));
+            Lotto lotto = new Lotto(numbers);
 
-        List<Integer> numbers = new ArrayList<>(List.of(6,5,4,3,2,1));
-        Lotto lotto = new Lotto(numbers);
+            String string = lotto.toString();
+            String expect = List.of(1,2,3,4,5,6).toString();
 
-        String string = lotto.toString();
-        String expect = List.of(1,2,3,4,5,6).toString();
+            assertEquals(string, expect);
 
-        assertEquals(string, expect);
+        }
 
     }
 

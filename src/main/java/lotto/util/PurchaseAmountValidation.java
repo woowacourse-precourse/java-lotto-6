65f02c7902a3e8ef71d981f499f4b093fbc3ea6a
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-06 21:28:55 +0900

    fix: 잘못된 연산 수정
    division -> modular

--- src/main/java/lotto/util/PurchaseAmountValidation.java
+++ src/main/java/lotto/util/PurchaseAmountValidation.java
@@ -1,10 +1,13 @@
 package lotto.util;
 
+import lotto.util.exception.ErrorMessage;
+
 import static lotto.util.Constants.DIVISION_VALUE;
 import static lotto.util.Constants.REMAINDER;
 public class PurchaseAmountValidation {
     private PurchaseAmountValidation(){}
-    public static boolean divisibleThousand(final String money) {
-        return Integer.parseInt(money) / DIVISION_VALUE == REMAINDER;
+    public static void divisibleThousand(final String amount) {
+        if(Integer.parseInt(amount) % DIVISION_VALUE != REMAINDER)
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DIVISIBLE_BY_THOUSAND.getErrorMessage());
     }
 }

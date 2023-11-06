db80c9632a923e4852a2a3eb7995388aad0c7626
EHCTH <lees90896@gmail.com>
2023-11-06 14:15:22 +0900

    모든 사용자의 입력을 받을수있게끔 나눴음

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,17 @@
-package lotto.view;public class InputView {
+package lotto.view;
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.ErrorCheck;
+public class InputView {
+    public static String buyLotto() {
+        return Console.readLine();
+    }
+    public static String choiceBonusLottoNum() {
+        return Console.readLine();
+    }
+
+    public static String choiceMainLottoNum() {
+        String choice = Console.readLine();
+        ErrorCheck.isSpaceValue(choice);
+        return choice;
+    }
 }

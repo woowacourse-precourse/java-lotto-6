97dc4ef70ab8cc1d82ea412ec4441863d9b88167
Mr.Kim <joajy98@gmail.com>
2023-11-06 13:19:44 +0900

    refactor: 단순 dto 개념 정도의 클래스를 record로 변경하여 코드 가독성 향상 및 단순화

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,21 +1,5 @@
 package lotto.domain;
 
-public class WinningNumbers {
+public record WinningNumbers(Lotto winningNumbers, int bonusNumber) {
 
-    private final Lotto winningNumbers;
-
-    private final int bonusNumber;
-
-    public WinningNumbers(Lotto winningNumbers, int bonusNumber) {
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
-    }
-
-    public Lotto getWinningNumbers() {
-        return winningNumbers;
-    }
-
-    public int getBonusNumber(){
-        return bonusNumber;
-    }
 }

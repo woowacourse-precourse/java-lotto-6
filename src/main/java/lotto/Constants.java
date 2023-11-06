9fd5764e1fcaadf6818c62f67f935825a7961f8b
jjminsuh <minsuh3203@gmail.com>
2023-11-07 03:50:10 +0900

    fix: 에러 메세지 오타 수정

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
     public static final String BOUGHT_LOTTO_COUNT = "개를 구매했습니다.";
     public static final String ENTER_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String ERROR_NOT_VALID_PURCHASE_AMOUNT = "[Error] 1000원 단위의 금액을 입력해 주세요.";
-    public static final String ERROR_NOT_VALID_LOTTO_NUMBERS = "[Error] 로또는 1부터 45 사이의 서로 다른 6개의 숫자로 이루어져야 합니다.";
-    public static final String ERROR_NOT_VALID_BONUS_NUMBER = "[Error] 보너스 숫자는 당첨 숫자와 겹치지 않는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_NOT_VALID_PURCHASE_AMOUNT = "[ERROR] 1000원 단위의 금액을 입력해 주세요.";
+    public static final String ERROR_NOT_VALID_LOTTO_NUMBERS = "[ERROR] 로또는 1부터 45 사이의 서로 다른 6개의 숫자로 이루어져야 합니다.";
+    public static final String ERROR_NOT_VALID_BONUS_NUMBER = "[ERROR] 보너스 숫자는 당첨 숫자와 겹치지 않는 1부터 45 사이의 숫자여야 합니다.";
     public static final int LOTTO_NUMBER_COUNT = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;

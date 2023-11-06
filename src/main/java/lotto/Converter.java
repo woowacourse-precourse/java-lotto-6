0cac03320e7d44c9b324286c9df211286435fcde
aammddkkzxc <ideal0427@naver.com>
2023-11-06 18:59:50 +0900

    feat: 당첨 번호 변환 기능 구현

--- src/main/java/lotto/Converter.java
+++ src/main/java/lotto/Converter.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
+    private static final String NUMBER_DELIMITER = ",";
+
     public static int convertToNumeric(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> convertWinningNumber(String winningNumber) {
+        String[] seperatedWinningNumber = winningNumber.split(NUMBER_DELIMITER);
+        List<Integer> convertedWinningNumber = new ArrayList<>();
+
+        for (String number : seperatedWinningNumber) {
+            convertedWinningNumber.add(convertToNumeric(number));
+        }
+
+        return convertedWinningNumber;
     }
 }

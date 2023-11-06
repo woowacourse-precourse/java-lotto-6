85ebfcf5d04eca5fcc2b7a634479e0b93c48a51d
Infikei <hckhong@naver.com>
2023-11-07 03:02:41 +0900

    feat: 구입한 로또 발행하는 기능 구현
    - 로또 번호를 랜덤하게 생성하는 기능 구현

--- src/main/java/lotto/domain/LottosPurchased.java
+++ src/main/java/lotto/domain/LottosPurchased.java
@@ -1,13 +1,28 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottosPurchased {
+    private static final int LOTTO_RANGE_BEGIN_NUMBER = 1;
+    private static final int LOTTO_RANGE_END_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
     private static final int LOTTO_PRICE = 1000;
 
     private List<Lotto> lottos;
 
     public LottosPurchased(int amount) {
+        lottos = new ArrayList<>();
         int numberOfLottos = amount / LOTTO_PRICE;
+
+        for (int i = 0; i < numberOfLottos; i++) {
+            lottos.add(new Lotto(generateRandomLottoNumbers()));
+        }
+    }
+
+    private List<Integer> generateRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_BEGIN_NUMBER, LOTTO_RANGE_END_NUMBER, LOTTO_SIZE);
     }
 }

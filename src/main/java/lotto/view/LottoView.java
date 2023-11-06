f5f1e34763c61b094feb6e4d6af1166528d610c8
권장순 <jsoonworld@gmail.com>
2023-11-06 09:27:16 +0900

    refactoring(LottoView#inputMoney) : View의 역할만 하게 만들고 검증 부분은 분리.

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -10,19 +10,15 @@ public class LottoView {
     public LottoView() {
 
     }
-    // TODO : 검증 단계는 model이나 controller에서 처리하도록 수정
     public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         try {
-            System.out.println("구입금액을 입력해 주세요.");
-            int money = Integer.parseInt(Console.readLine());
-            if (money < 1000 || money % 1000 != 0) {
-                throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위의 정수여야 합니다.");
-            }
-            return money;
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입 금액으로 숫자를 입력해야 합니다.");
         }
     }
+
 
     // TODO: 예외처리 부분 구현 / model이나 controller에서 처리하도록 수정
     public List<Integer> inputWinningNumbers() {

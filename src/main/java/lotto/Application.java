2e1630f253883a48615efeece5c17fd3306f76fa
Jokyo0612 <jgh010612@gmail.com>
2023-11-06 18:32:59 +0900

    feat : 초기입력 금액을 통해 구매 로또 개수 산출 기능 구현 money_for_lotto의 호출명을 없애 코드 단순화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -34,13 +34,19 @@ class money_for_lotto{
         }
     }
 
+    public static int repeatTime(int realNumber){
+        return realNumber/1000;
+    }
+
 }
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         String inputMoney = Console.readLine();
-        money_for_lotto money = new money_for_lotto();
-        money.realMoney(inputMoney);
+        int realMoney = money_for_lotto.realMoney(inputMoney);
+        int repeatTime = money_for_lotto.repeatTime(realMoney);
+
+
     }
 }

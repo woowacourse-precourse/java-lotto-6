c9a088cf7918ea8cbac1c357148a437f02cb9cd9
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 14:36:12 +0900

    refactor: lotto 관련 enum 다시 추가 README.md에 enum 적용 글을 다시 보고 enum 사용시기에 대해서 다시 찾아본 결과 enum으로 따로 관리하는게 좋다 판단하여 다시 추가하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,10 @@
 package lotto.domain;
 
+import lotto.domain.constants.LottoRule;
+
 import java.util.List;
 
 public class Lotto {
-    private static final int LOTTO_MIN_NUMBER = 1;
-    private static final int LOTTO_MAX_NUMBER = 45;
-    private static final int LOTTO_SIZE = 6;
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -21,21 +19,22 @@ public class Lotto {
     }
 
     private void lottoSizeValidate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
+        if (numbers.size() != LottoRule.LOTTO_SIZE.getValue()) {
             throw new IllegalArgumentException();
         }
     }
 
     private void lottoNumberRangeValidate(List<Integer> numbers) {
         numbers.forEach(number -> {
-            if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            if (number < LottoRule.LOTTO_MIN_NUMBER.getValue()
+                    || number > LottoRule.LOTTO_MAX_NUMBER.getValue()) {
                 throw new IllegalArgumentException();
             }
         });
     }
 
     private void lottoDuplicateNumberValidate(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+        if (numbers.stream().distinct().count() != LottoRule.LOTTO_SIZE.getValue()) {
             throw new IllegalArgumentException();
         }
     }
---
+++ src/main/java/lotto/domain/constants/LottoRule.java
@@ -0,0 +1,19 @@
+package lotto.domain.constants;
+
+public enum LottoRule {
+
+    PRICE(1000),
+    LOTTO_SIZE(6),
+    LOTTO_MIN_NUMBER(1),
+    LOTTO_MAX_NUMBER(6);
+
+    private final int value;
+
+    LottoRule(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}

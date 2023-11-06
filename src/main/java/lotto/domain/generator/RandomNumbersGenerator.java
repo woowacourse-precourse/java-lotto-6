7278f56cc1bdeb5eea6341eba59b1c72c6d69f43
JuSeong1130 <jsmoon1130@naver.com>
2023-11-06 09:20:46 +0900

    refactor(RandomNumbersGenerator) : sorted 적용하여 리팩토링
    기존 요구사항이 sort되어 나오게 되는것이 있어 stream 이용 sort

--- src/main/java/lotto/domain/generator/RandomNumbersGenerator.java
+++ src/main/java/lotto/domain/generator/RandomNumbersGenerator.java
@@ -7,6 +7,9 @@ public class RandomNumbersGenerator implements NumbersGenerator{
 
     @Override
     public List<Integer> create() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted()
+                .toList();
     }
 }

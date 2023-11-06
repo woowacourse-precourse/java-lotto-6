3fe8c4db185f67231fe3b8d659de37f3a16e5735
진환 정 <joung.jinhawn@gmail.com>
2023-11-06 15:58:33 +0900

    test(StartLottoTest.java) : 구입 금액 1000원 단위가 아닐 경우 예외 발생 테스트 작성

--- src/test/java/lotto/StartLottoTest.java
+++ src/test/java/lotto/StartLottoTest.java
@@ -1,20 +1,14 @@
 package lotto;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StartLottoTest {
     private static StartLotto test;
+
     @DisplayName("테스트할 StartLotto 객체 먼저 생성")
     @BeforeEach
     void init() {
@@ -28,5 +22,14 @@ public class StartLottoTest {
             String input = "10dkfk";
             test.checkPurchasePrice(input);
         }).isInstanceOf(NumberFormatException.class);
+    }
+
+    @DisplayName("구입 금액이 1000원 단위가 아니라면 예외가 발생한다.")
+    @Test
+    void checkPurchasePriceNot1000wonUnit() {
+        assertThatThrownBy(() -> {
+            String input = "101";
+            test.checkPurchasePrice(input);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }

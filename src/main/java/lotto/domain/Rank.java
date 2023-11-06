4c6bdbdbf3079bfb53dab2251e5f5bc181e82fa5
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 23:51:12 +0900

    feat(Rank): 매칭된 숫자 개수에 맞는 랭킹 반환 기능 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,44 +1,31 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.EnumSet;
 
-public enum WinningStatistic {
+public enum Rank {
     FIRST(6, 2_000_000_000),
     SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    FIFTH(3, 5_000),
+    NONE(0, 0);
 
     private final Integer matchingNumbers;
     private final Integer prizeAmount;
 
-    WinningStatistic(Integer matchingNumbers, Integer prizeAmount) {
+    Rank(Integer matchingNumbers, Integer prizeAmount) {
         this.matchingNumbers = matchingNumbers;
         this.prizeAmount = prizeAmount;
     }
 
-    public static WinningStatistic getRankTypeByMatchNumbers(Integer matchingNumbers, boolean hasBonusNumber) {
-        if (matchingNumbers == 5 && hasBonusNumber) {
+    public static Rank getRankByMatchingNumbersAndBonus(Integer matchingNumbers, boolean isBonusNumberMatch) {
+        if (matchingNumbers == 5 && isBonusNumberMatch) {
             return SECOND;
         }
 
         return EnumSet.range(FIRST, FIFTH).stream()
                 .filter(prize -> prize.matchingNumbers.equals(matchingNumbers))
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
-    }
-
-    public static Integer calculatePrize(Integer matchingNumbers, boolean hasBonusNumber) {
-
-        if (matchingNumbers == 5 && hasBonusNumber) {
-            return SECOND.prizeAmount;
-        }
-
-        return Arrays.stream(WinningStatistic.values())
-                .filter(prize -> prize.matchingNumbers.equals(matchingNumbers))
-                .findFirst()
-                .map(prize -> prize.prizeAmount)
-                .orElseThrow(IllegalArgumentException::new);
+                .orElse(Rank.NONE);
     }
 }

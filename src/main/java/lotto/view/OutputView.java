1b2de069eadfdbded9d294be621e3d40abe9c08a
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 03:48:09 +0900

    feat(OutputView): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,13 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.WinningYield;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class OutputView {
@@ -11,5 +17,27 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public static void displayWinningDetails(List<Rank> ranks, WinningYield winningYield) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+
+        List<Rank> reversedRanks = new ArrayList<>(Arrays.asList(Rank.values()));
+        Collections.reverse(reversedRanks);
+
+
+        for (Rank rank : reversedRanks) {
+            long count = ranks.stream().filter(r -> r == rank).count();
+            int matchingNumbers = rank.getMatchingNumbers();
+
+            if (!rank.name().equals("NONE")) {
+                String prizeAmountFormatted = NumberFormat.getIntegerInstance().format(rank.getPrizeAmount());
+                System.out.println(matchingNumbers + "개 일치 (" + prizeAmountFormatted + "원) - " + count + "개");
+            }
+        }
+
+        double yield = winningYield.getYield();
+        System.out.println("총 수익률은 " + yield + "%입니다.");
     }
 }

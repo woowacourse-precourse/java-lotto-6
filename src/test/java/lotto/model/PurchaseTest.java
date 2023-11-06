f9f45ed8c886f460267935968730109ded72f190
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 15:28:10 +0900

    test : 구매금액을 검증하는 단위 테스트 작성

--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseTest {
 
@@ -18,6 +19,30 @@ class PurchaseTest {
 
         // then
         assertThat(purchase.getPurchaseCount()).isEqualTo(11);
+    }
+
+    @DisplayName("입력된 구매금액이 숫자가 아니라면 Exception 발생한다.")
+    @Test
+    void validateNumeric() {
+        // given
+        String input = "1000a";
+
+        // when // then
+        assertThatThrownBy(() -> Purchase.fromPurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매 금액은 숫자로만 이루어져야 합니다.");
+    }
+
+    @DisplayName("입력된 구매금액이 1000원 단위가 아니라면 Exception 발생한다.")
+    @Test
+    void validateAmountInThousands() {
+        // given
+        String input = "1001";
+
+        // when // then
+        assertThatThrownBy(() -> Purchase.fromPurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
     }
 
 }

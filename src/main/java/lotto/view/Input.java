ae973afe501250ec37e43c7323836537cadc1665
KimSehoon <shggm2000@gmail.com>
2023-11-07 02:50:06 +0900

    feat: getWinningNumbers method 구현
    - 당첨 번호를 입력 받는 getWinningNumbers method 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,9 +1,22 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Input {
-    public int getPurchaseAmount(){
+    public int getPurchaseAmount() {
         return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> getWinningNumbers() {
+        String inputNumbers = Console.readLine();
+
+        return Arrays.stream(inputNumbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
     }
 }

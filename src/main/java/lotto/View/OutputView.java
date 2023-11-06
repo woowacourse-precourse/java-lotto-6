88a3b3b383b5c4f28d8ad574929b72a0dd503e67
Firedrago95 <yonghwa95@naver.com>
2023-11-06 10:50:16 +0900

    refactor(OutputView) : 결과 출력 기능 메서드 분리

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -17,15 +17,26 @@ public class OutputView {
         }
     }
 
-    public static void printRankCount(Map<Rank,Integer> rankCount) {
-        System.out.println(Rank.FIFTH.getMatchedNumbers()+"개 일치 (5,000원) - "+rankCount.getOrDefault(Rank.FIFTH,0)+"개");
-        System.out.println(Rank.FOURTH.getMatchedNumbers()+"개 일치 (50,000원) - "+rankCount.getOrDefault(Rank.FOURTH,0)+"개");
-        System.out.println(Rank.THIRD.getMatchedNumbers()+"개 일치 (1,500,000원) - "+rankCount.getOrDefault(Rank.THIRD,0)+"개");
-        System.out.println(Rank.SECOND.getMatchedNumbers()+"개 일치, 보너스 볼 일치 (30,000,000원) - "+rankCount.getOrDefault(Rank.SECOND,0)+"개");
-        System.out.println(Rank.FIRST.getMatchedNumbers()+"개 일치 (2,000,000,000원) - "+rankCount.getOrDefault(Rank.FIRST,0)+"개");
+    public static void printRankCount(Map<Rank, Integer> rankCount) {
+        printRank(Rank.FIFTH, "5,000", rankCount);
+        printRank(Rank.FOURTH, "50,000", rankCount);
+        printRank(Rank.THIRD, "1,500,000", rankCount);
+        printSecondRank(Rank.SECOND, "30,000,000", rankCount);
+        printRank(Rank.FIRST, "2,000,000,000", rankCount);
     }
 
     public static void printReturnRate(double returnRate) {
         System.out.println("총 수익률은 " + returnRate + "%입니다.");
+    }
+
+    private static void printRank(Rank rank, String prize, Map<Rank, Integer> rankCount) {
+        int count = rankCount.getOrDefault(rank, 0);
+        System.out.println(rank.getMatchedNumbers() + "개 일치 (" + prize + "원) - " + count + "개");
+    }
+
+    private static void printSecondRank(Rank rank, String prize, Map<Rank, Integer> rankCount) {
+        int count = rankCount.getOrDefault(rank, 0);
+        System.out.println(
+            rank.getMatchedNumbers() + "개 일치, 보너스 볼 일치 (" + prize + "원) - " + count + "개");
     }
 }

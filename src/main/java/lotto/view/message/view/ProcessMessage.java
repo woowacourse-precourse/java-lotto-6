e48e6e83490f929c2cf7a3a4a53597e4f2d99c0e
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-06 14:21:54 +0900

    refactor: 출력할 문구 전체적으로 수정

--- src/main/java/lotto/view/message/view/ProcessMessage.java
+++ src/main/java/lotto/view/message/view/ProcessMessage.java
@@ -2,9 +2,9 @@ package lotto.view.message.view;
 
 public enum ProcessMessage {
     PLEASE_INPUT_BUDGET("구입금액을 입력해 주세요.\n"),
-    PURCHASE_LOTTO("개를 구매했습니다.\n"),
-    PLEASE_INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요.\n"),
-    PLEASE_INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    PURCHASE_LOTTO("\n%d개를 구매했습니다.\n"),
+    PLEASE_INPUT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요.\n"),
+    PLEASE_INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n");
 
     private String message;
 
--- src/main/java/lotto/view/message/view/ResultMessage.java
+++ src/main/java/lotto/view/message/view/ResultMessage.java
@@ -1,16 +1,15 @@
 package lotto.view.message.view;
 
 public enum ResultMessage {
+    START_TO_SHOW_STATISTICS("\n당첨 통계\n---\n"),
+    MATCHES_3_NUMBERS("3개 일치 (5,000원) - %d개\n"),
+    MATCHES_4_NUMBERS("4개 일치 (50,000원) - %d개\n"),
+    MATCHES_5_NUMBERS("5개 일치 (1,500,000원) - %d개\n"),
+    MATCHES_5_NUMBERS_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    MATCHES_6_NUMBERS("6개 일치 (2,000,000,000원) - %d개\n"),
+    PROFIT_PERCENT("총 수익률은 %.1f%%입니다.");
 
-    START_TO_SHOW_STATISTICS("당첨 통계"),
-    MATCHES_3_NUMBERS("3개 일치 (5,000원) - %d개"),
-    MATCHES_4_NUMBERS("4개 일치 (50,000원) - %d개"),
-    MATCHES_5_NUMBERS("5개 일치 (1,500,000원) - %d개"),
-    MATCHES_5_NUMBERS_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    MATCHES_6_NUMBERS("6개 일치 (2,000,000,000원) - %d개"),
-    PROFIT_PERCENT("총 수익률은 %0.1f%%입니다.");
-
-    private String message;
+    private final String message;
 
     ResultMessage(String message){
         this.message =message;

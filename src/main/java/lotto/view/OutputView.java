e0c3efa20b71594a2e56078fd599ac8805e50074
yeseong31 <yeseong31@naver.com>
2023-11-06 22:50:38 +0900

    feat(OutputView): 당첨 통계 문구 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,19 +1,23 @@
 package lotto.view;
 
-import static java.lang.String.*;
+import static java.lang.String.format;
 import static lotto.view.ConsoleMessage.ENTER_BONUS_NUMBER;
 import static lotto.view.ConsoleMessage.ENTER_PURCHASE_AMOUNT;
 import static lotto.view.ConsoleMessage.ENTER_WINNING_NUMBERS;
+import static lotto.view.ConsoleMessage.RESULTS_SUMMARY;
 import static lotto.view.ConsoleMessage.SUCCESS_PURCHASE_LOTTO;
+import static lotto.view.ConsoleMessage.TOTAL_RETURN;
+import static lotto.view.SeparatorConstant.RESULT_SUMMARY_SEPARATOR;
 
 import java.util.List;
+import lotto.domain.RankingResult;
 
 public class OutputView {
-    
+
     public static void printEnterPurchaseAmount() {
         println(ENTER_PURCHASE_AMOUNT.getMessage());
     }
-    
+
     public static void printLotteriesInfo(List<String> lotteries) {
         printNewLine();
         println(format(SUCCESS_PURCHASE_LOTTO.getMessage(), lotteries.size()));
@@ -22,29 +26,39 @@ public class OutputView {
         }
         printNewLine();
     }
-    
+
     public static void printEnterWinningNumbers() {
         printNewLine();
         println(ENTER_WINNING_NUMBERS.getMessage());
     }
-    
+
     public static void printEnterBonusNumber() {
         printNewLine();
         println(ENTER_BONUS_NUMBER.getMessage());
     }
-    
+
+    public static void printResultsSummary(RankingResult rankingResult) {
+        printNewLine();
+        println(RESULTS_SUMMARY.getMessage());
+        println(RESULT_SUMMARY_SEPARATOR);
+
+        for (String message : rankingResult.receiveRankingResultInfo()) {
+            println(message);
+        }
+    }
+
+    public static void printResultsProfitability(double profitability) {
+        println(format(TOTAL_RETURN.getMessage(), profitability));
+    }
+
     public static void printErrorMessage(IllegalArgumentException exception) {
         System.out.println(exception.getMessage());
     }
-    
-    private static void print(final String message) {
-        System.out.print(message);
-    }
-    
+
     private static void println(final String message) {
         System.out.println(message);
     }
-    
+
     private static void printNewLine() {
         System.out.println();
     }

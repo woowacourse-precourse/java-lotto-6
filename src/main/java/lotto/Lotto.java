30bcf3aea18ab44e5d8496174caa3bcdec4001e0
cheolhyeon <tghy4@naver.com>
2023-11-06 18:45:46 +0900

    refactor : 검증과 관련된 코드를 Validator 클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,36 +1,20 @@
 package lotto;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import user.User;
 
 public class Lotto {
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-        if (hasDuplicate(numbers)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private boolean hasDuplicate(List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>(numbers);
-        return set.size()!= numbers.size();
-    }
-    // TODO: 추가 기능 구현
-
 }
+
+
+
+
+

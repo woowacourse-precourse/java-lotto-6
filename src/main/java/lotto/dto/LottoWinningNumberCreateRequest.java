cf552cfc6d5026a6db9072950b18e46125e0470f
dukcode <duk9741@gmail.com>
2023-11-06 15:35:57 +0900

    feat: 로또 당첨 번호 생성 요청 클래스 구현
    - 입력 숫자 아닐 시 예외 발생

--- src/main/java/lotto/dto/LottoWinningNumberCreateRequest.java
+++ src/main/java/lotto/dto/LottoWinningNumberCreateRequest.java
@@ -1,4 +1,34 @@
 package lotto.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoWinningNumberCreateRequest {
+
+    private List<Integer> numbers;
+
+    public LottoWinningNumberCreateRequest(String winningNumbers) {
+        String[] numbers = winningNumbers.split(",");
+
+        this.numbers = new ArrayList<>();
+        for (String stringNumber : numbers) {
+            int number = validateAndParseNumber(stringNumber);
+            this.numbers.add(number);
+        }
+    }
+
+    private static int validateAndParseNumber(String stringNumber) {
+        int number;
+        try {
+            number = Integer.parseInt(stringNumber);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("당첨 번호는 숫자 형식이여야 합니다.");
+        }
+        return number;
+    }
+
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

2dce7828f3950ef03bdcebf67f0cc14dc848a4e7
jang010505 <jang010505@inu.ac.kr>
2023-11-07 05:30:00 +0900

    feat: LottoResult 내부 코드 작성

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -2,12 +2,11 @@ package lotto.domain;
 
 public enum LottoResult {
     MISS(0, false, 0, ""),
-    FIFTH(0, false, 0, ""),
-    FOURTH(0, false, 0, ""),
-    THIRD(0, false, 0, ""),
-    SECOND(0, false, 0, ""),
-    FIRST(0, false, 0, "");
-
+    FIFTH(3, false, 5000, "3개 일치 (5,000원) - %d개"),
+    FOURTH(4, false, 50000, "4개 일치 (50,000원) - %d개"),
+    THIRD(5, false, 1500000, "5개 일치 (1,500,000원) - %d개"),
+    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원) - %d개");
 
     private final int count;
     private final boolean bonus;
@@ -21,7 +20,12 @@ public enum LottoResult {
         this.message = message;
     }
 
-    public static LottoResult valueOf(int count, boolean bonus){
+    public static LottoResult valueOf(int count, boolean bonus) {
+        for (LottoResult result : values()) {
+            if (result.count == count && result.bonus == bonus) {
+                return result;
+            }
+        }
 
         return MISS;
     }

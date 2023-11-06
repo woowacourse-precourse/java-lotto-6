becc1f31b20fc9d8f0b07b13b4ed77866ae3f291
MingyeomKim <mingyum119@naver.com>
2023-11-07 08:36:13 +0900

    feat(DrawnNumbersDto): 컨트롤러에서 도메인으로 전달되는 dto 객체 정의
    뷰에서 넘어온 당첨 번호와 보너스 번호를 DrawnNumber 도메인에게 전달하기 위해 dto 객체를 생성한다.

---
+++ src/main/java/lotto/dto/DrawnNumbersDto.java
@@ -0,0 +1,47 @@
+package lotto.dto;
+
+import static lotto.view.constants.SymbolType.INPUT_SEPARATOR;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.global.exception.ErrorMessage;
+import lotto.global.exception.LottoException;
+
+public class DrawnNumbersDto {
+    private List<Integer> winningNumbers;
+    private Integer bonusNumber;
+
+    private DrawnNumbersDto(List<Integer> winningNumbers, Integer bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static DrawnNumbersDto of(String winningNumbers, String bonusNumber) {
+        return new DrawnNumbersDto(
+                parseWinningNumbers(winningNumbers),
+                parseBonusNumber(bonusNumber)
+        );
+    }
+
+    private static List<Integer> parseWinningNumbers(String winningNumbers) {
+        try {
+            return Arrays.stream(split(winningNumbers))
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw LottoException.from(ErrorMessage.NOT_NUMBER_ERROR);
+        }
+    }
+
+    private static String[] split(String winningNumbers) {
+        return winningNumbers.split(INPUT_SEPARATOR.getSymbol());
+    }
+
+    private static Integer parseBonusNumber(String bonusNumber) {
+        try {
+            return Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw LottoException.from(ErrorMessage.NOT_NUMBER_ERROR);
+        }
+    }
+}

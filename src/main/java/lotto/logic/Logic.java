616c68c44fd542efa16ff88aaa8077a917502449
huhdy32 <mong3268@gmail.com>
2023-11-06 15:53:07 +0900

    feat: 지불 금액 입력, 예외에 대한 에러처리 기능 추가

--- src/main/java/lotto/logic/Logic.java
+++ src/main/java/lotto/logic/Logic.java
@@ -1,2 +1,11 @@
-package lotto.logic;public interface Logic {
+package lotto.logic;
+
+public interface Logic {
+    int START_RANGE = 1;
+    int END_RANGE = 45;
+    int NUMBER_COUNT = 6;
+
+    void start();
+
+    void run();
 }
--- src/main/java/lotto/logic/LottoLogic.java
+++ src/main/java/lotto/logic/LottoLogic.java
@@ -1,2 +1,61 @@
-package lotto.logic;public class LottoLogic {
+package lotto.logic;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+import lotto.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoLogic implements Logic {
+    private final View view;
+    private boolean running = true;
+    private List<Lotto> lotteries = new ArrayList<>();
+
+    public LottoLogic(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void start() {
+        while (running) {
+            run();
+        }
+    }
+
+    @Override
+    public void run() {
+        int payment = getPurchaseAmount();
+    }
+
+    private int getPurchaseAmount() {
+        int payment = 0;
+        while(payment > 0){
+            try {
+                String input = view.getPurchaseAmount();
+                payment = validPayment(input);
+            } catch (IllegalArgumentException e){
+                view.printError(e.getMessage());
+            }
+        }
+        return payment;
+    }
+
+    private int validPayment(String input) {
+        int payment;
+        try {
+            payment = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("지불 금액은 정수여야 합니다.");
+        }
+        if (payment <= 0) {
+            throw new IllegalArgumentException("지불 금액은 0 보다 커야 합니다.");
+        } else if (payment % 1000 != 0) {
+            throw new IllegalArgumentException("지불 금액은 1000원 단위여야 합니다.");
+        }
+        return payment;
+    }
+
 }

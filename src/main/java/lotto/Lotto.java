752dff952da87da82e542e562cc0c8e71f39cb85
BINBEAN777 <peanutchan@naver.com>
2023-11-06 23:44:23 +0900

    Refactor : 로또 중복 방지 예외 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,14 +1,17 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.List;
-
+import camp.nextstep.edu.missionutils.Randoms;
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        checkDuplication(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -18,15 +21,14 @@ public class Lotto {
         }
     }
 
-    private void checkDuplication(List<Integer> numbers) {
-        List<Integer> numbergroup = new ArrayList<>();
-        for (Integer number : numbers) {
-            numbergroup.add(number);
-            if (numbergroup.contains(number)) {
-                throw new IllegalArgumentException();
-            }
+    private void checkDuplicate(List<Integer>numbers){
+        Set<Integer> checkDuplication = new HashSet<>(numbers);
+        if(checkDuplication.size()!=6){
+            throw new IllegalArgumentException();
         }
     }
+
+
 
     // TODO: 추가 기능 구현
 }

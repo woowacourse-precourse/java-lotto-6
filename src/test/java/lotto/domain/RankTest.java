db726305bf308c7c71d099549413fb4621ae55a5
Mr.Kim <joajy98@gmail.com>
2023-11-06 14:51:02 +0900

    test: Rank 클래스 기능 테스트(당첨 횟수 카운팅, 총 당첨 금액)

---
+++ src/test/java/lotto/domain/RankTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static lotto.domain.Rank.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class RankTest {
+
+    @Test
+    @DisplayName("당첨된 등수에 따라 당첨 횟수가 증가하는 테스트")
+    public void increaseCountTest() {
+        final int matchResult = 5;
+        final boolean hasBonusNumber = false;
+        increaseRankResult(matchResult, hasBonusNumber);
+        assertThat(THIRD.getWinningAmount())
+                .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("총 당첨 금액을 확인하는 테스트")
+    public void totalEarningMoneyTest(){
+        final int matchResult = 4;
+        final boolean hasBonusNumber = false;
+        increaseRankResult(matchResult, hasBonusNumber);
+        assertThat(FOURTH.getMoney())
+                .isEqualTo(50000);
+    }
+}

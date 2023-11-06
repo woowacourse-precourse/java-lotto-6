40f2bd36dea5efc4857d5c01cc9c0e3303d6d037
delena0702 <delena0702@gmail.com>
2023-11-06 20:30:16 +0900

    feat: LottoSimulationResult 구현

---
+++ src/main/java/lotto/LottoSimulationResult.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoSimulationResult {
+    List<Integer> resultCount;
+
+    public LottoSimulationResult() {
+        this.resultCount = new ArrayList<>(LottoMatchResult.values().length);
+    }
+
+    public void add(LottoMatchResult result) {
+        int value = resultCount.get(result.getIndex());
+        resultCount.set(value + 1, result.getIndex());
+    }
+
+    public float calculateReturn() {
+        int totalAmount = 0, returnAmount = 0;
+
+        for (LottoMatchResult matchResult : LottoMatchResult.values()) {
+            totalAmount += LottoSimulator.LOTTO_PRICE;
+            returnAmount += matchResult.getReward();
+        }
+
+        return (float)returnAmount / totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        for (LottoMatchResult matchResult : LottoMatchResult.values()) {
+            result.append(String.format("%s - %d개", matchResult, resultCount.get(matchResult.getIndex())));
+        }
+        result.append(String.format("총 수익률은 %.2f%%입니다.", calculateReturn() * 100));
+
+        return result.toString();
+    }
+}

70975789252dc92a5c52cde440c54218dca5f342
고동환\eurdr <eurdream98@naver.com>
2023-11-06 15:28:45 +0900

    feature($lotto.view.Outview): Add calculateLottoCount method
    - 입력받은 가격을 가지고 로또 몇장을 구입하는지 계산하여 나타내주는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void askAmountForLotto() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+    public static void calculateLottoCount(){
+        final int LOTTO_COUNT = InputView.LOTTO_PRICE/1000;
+        System.out.println(LOTTO_COUNT+"개를 구매했습니다.");
+    }
 }

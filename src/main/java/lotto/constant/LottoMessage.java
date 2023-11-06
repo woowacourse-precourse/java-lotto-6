b867e4bc6618f1c159a750be0cd89776316e6b1c
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 20:05:05 +0900

    feat(LottoMessage): 천단위 쉼표(,) 구분

--- src/main/java/lotto/constant/LottoMessage.java
+++ src/main/java/lotto/constant/LottoMessage.java
@@ -7,9 +7,9 @@ public enum LottoMessage {
     WINNING_NUMBERS_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS_TITLE_MESSAGE("당첨 통계" + "\n" + "---"),
-    WINNING_STATISTICS_MESSAGE("%d개 일치 (%d원) - %d개"),
-    WINNING_STATISTICS_BONUS_MESSAGE("%d개 일치, 보너스 볼 일치 (%d원) - %d개"),
-    TOTAL_PROFIT_RATE_MESSAGE("총 수익률은 %.1f%%입니다.");
+    WINNING_STATISTICS_MESSAGE("%d개 일치 (%,d원) - %d개"),
+    WINNING_STATISTICS_BONUS_MESSAGE("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
+    TOTAL_PROFIT_RATE_MESSAGE("총 수익률은 %,.1f%%입니다.");
 
     private final String message;
 

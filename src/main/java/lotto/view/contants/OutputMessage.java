c2e6f4dc52f2725fdb4cd7892633b6ea75c6ce77
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 22:37:05 +0900

    feat: 출력 메세지 enum 추가

---
+++ src/main/java/lotto/view/contants/OutputMessage.java
@@ -0,0 +1,26 @@
+package lotto.view.contants;
+
+public enum OutputMessage {
+    BLANK(" "),
+    DASH("-"),
+    OPEN_PARENTHESIS("("),
+    CLOSE_PARENTHESIS(")"),
+
+    WINNING_STATISTICS("당첨 통계\n---"),
+    SAME("%s개 일치"),
+    AMOUNT("(%s원)"),
+    COUNT("%s개"),
+    RATE_OF_RETURN("총 수익률은 %s%%입니다.");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getValue() {
+        return this.message;
+    }
+
+
+}

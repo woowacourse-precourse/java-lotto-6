866600bb66ff372e5a07fa9c03499d6fbcca21f4
jinsu868 <mjinsu098@gmail.com>
2023-11-06 10:31:50 +0900

    feat(view): inputView에서 숫자 입력 받는 기능 구현, 숫자 인지 판별하는 validation 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,34 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.validator.InputValidator;
+
+public class InputView {
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public int getNumber() {
+        String inputNumber = Console.readLine();
+        inputValidator.validateNumber(inputNumber);
+        return Integer.parseInt(inputNumber);
+    }
+
+    public List<Integer> getNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        List<String> inputNumbers = Arrays.asList(Console.readLine());
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            inputValidator.validateNumber(inputNumbers.get(i));
+        }
+
+        return inputNumbers.stream()
+                .mapToInt(Integer::parseInt)
+                .boxed().collect(Collectors.toList());
+    }
+}

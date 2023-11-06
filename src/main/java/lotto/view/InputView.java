c72f00b37c8c576d374e83cda9c18a8add4ce387
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-06 23:13:54 +0900

    feat(InputView): 출력 요구사항 기능 적용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,14 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.dto.request.PurchaseAmountDto;
+import lotto.dto.request.WinningNumbersDto;
+import lotto.util.Validator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
     private InputView() {
@@ -7,6 +16,24 @@ public class InputView {
 
     public static InputView getInstance() {
         return LazyHolder.INSTANCE;
+    }
+
+    public PurchaseAmountDto readPurchaseAmount() {
+        String amount = Console.readLine();
+        return PurchaseAmountDto.from(amount);
+    }
+
+    public WinningNumbersDto readWinningNumbers() {
+        List<Integer> numbers = parseNumbers(Console.readLine());
+        int bonus = Validator.validateAndParseInteger(Console.readLine());
+
+        return WinningNumbersDto.of(numbers, bonus);
+    }
+
+    private static List<Integer> parseNumbers(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(Validator::validateAndParseInteger)
+                .collect(Collectors.toList());
     }
 
 

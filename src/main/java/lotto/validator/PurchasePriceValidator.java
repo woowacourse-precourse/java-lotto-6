3e19ca8706fe243a9510719520da681bdf777f87
hyuna <monicajo0@naver.com>
2023-11-06 01:05:33 -0800

    refactor(Validator): 추상화 클래스 구현

--- src/main/java/lotto/validator/PurchasePriceValidator.java
+++ src/main/java/lotto/validator/PurchasePriceValidator.java
@@ -1,31 +1,10 @@
 package lotto.validator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import lotto.exception.ExceptionMessage;
-import lotto.model.Constants;
-
-public class PurchasePriceValidator {
-    private static final String ONLY_NUMBER_REGEX = "[0-9]+";
-    private static final Pattern ONLY_NUMBER_PATTERN = Pattern.compile(ONLY_NUMBER_REGEX);
-
+public class PurchasePriceValidator extends Validator {
+    @Override
     public void validate(String purchasePrice) {
-        purchasePrice = purchasePrice.replace(" ", "");
+        purchasePrice = removeSpace(purchasePrice);
         validateOnlyNumber(purchasePrice);
-        validateRange(Integer.parseInt(purchasePrice));
-    }
-
-    private void validateOnlyNumber(String purchasePrice) {
-        Matcher matcher = ONLY_NUMBER_PATTERN.matcher(purchasePrice);
-
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException(ExceptionMessage.NO_NUMBER_EXCEPTION.getMessage());
-        }
-    }
-
-    private void validateRange(int purchasePrice) {
-        if (purchasePrice % Constants.PURCHASE_UNIT != 0) {
-            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_UNIT_EXCEPTION.getMessage());
-        }
+        validatePurchasePriceRange(Integer.parseInt(purchasePrice));
     }
 }
---
+++ src/main/java/lotto/validator/Validator.java
@@ -0,0 +1,31 @@
+package lotto.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lotto.exception.ExceptionMessage;
+import lotto.model.Constants;
+
+public abstract class Validator {
+    private static final String ONLY_NUMBER_REGEX = "[0-9]+";
+    private static final Pattern ONLY_NUMBER_PATTERN = Pattern.compile(ONLY_NUMBER_REGEX);
+
+    public abstract void validate(String input);
+
+    String removeSpace(String input) {
+        return input.replace(" ", "");
+    }
+
+    void validateOnlyNumber(String input) {
+        Matcher matcher = ONLY_NUMBER_PATTERN.matcher(input);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ExceptionMessage.NO_NUMBER_EXCEPTION.getMessage());
+        }
+    }
+
+    void validatePurchasePriceRange(int purchasePrice) {
+        if (purchasePrice % Constants.PURCHASE_UNIT != 0) {
+            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_UNIT_EXCEPTION.getMessage());
+        }
+    }
+}

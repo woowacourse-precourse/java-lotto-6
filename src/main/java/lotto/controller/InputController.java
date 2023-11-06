13dda12d23c1263f41789e52abd025f94a8613b2
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 21:22:36 +0900

    feat: 인풋 컨트롤러 기능 조정

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,5 +1,16 @@
 package lotto.controller;
 
-public class InputController {
+import java.util.List;
+import lotto.model.PlayerLottoAmount;
+import lotto.view.InputView;
 
+public class InputController {
+    public static int inputPlayerAmount() {
+        PlayerLottoAmount playerLottoAmount = new PlayerLottoAmount(InputView.lottoMoneyInput());
+        return playerLottoAmount.calculateLottoCount();
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        return InputView.inputLottoWinningNum();
+    }
 }

03edbf9f1790f92476e111dc193dfdbf0aac1dd0
honeydanji <tjdwls3680@naver.com>
2023-11-06 15:38:38 +0900

    FEAT : 구입 금액 입력에 대한 예외 처리 코드 작성
    // 완료
    - 문자를 입력한 경우 예외 테스트 통과
    - 1000단위로 나누어지지 않을 경우 예외 테스트 통과
    
    // 추가 구현
    - 예외가 발생하면 구입 금액 입력 재호출

---
+++ src/main/java/lotto/utils/PurchaseAmountException.java
@@ -0,0 +1,22 @@
+package lotto.utils;
+
+import static lotto.utils.ExceptionMessage.NON_DIVISIBLE_BY_1000;
+import static lotto.utils.ExceptionMessage.NOT_NUMBER;
+
+public class PurchaseAmountException {
+
+    public void notNullNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.valueOf(NOT_NUMBER));
+        }
+    }
+
+    public void notDivisible(String input) {
+        int numberInput = Integer.parseInt(input);
+        if(numberInput % 1000 != 0) {
+            throw new IllegalArgumentException(String.valueOf(NON_DIVISIBLE_BY_1000));
+        }
+    }
+}

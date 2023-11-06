d6ed1050ab83db2ed8cf9cf0000ed48f0ab5e9dd
heondong9265 <heondong0723@gmail.com>
2023-11-07 00:40:24 +0900

    feat(lotto): LottoUi Function Implement

--- src/main/java/lotto/View/LottoUi.java
+++ src/main/java/lotto/View/LottoUi.java
@@ -1,29 +1,29 @@
 package lotto.View;
 
+import java.util.List;
+
 public class LottoUi {
 
-    public static void getMoneyView() {
-
+    public static void inputMoneyView() {
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void lottoCountView() {
-
+    public static void lottoCountView(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
     }
 
-    public static void lottoNumbersView() {
-
+    public static void lottoNumbersView(List<Integer> lotto) {
+        for (int number : lotto) {
+            System.out.print(number + " ");
+        }
     }
 
     public static void answerLottoView() {
-
+        System.out.println(("당첨 번호를 입력해 주세요."));
     }
 
     public static void answerBonusNumberView() {
-
-    }
-
-    public static void correctAnswerView() {
-
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 
 }

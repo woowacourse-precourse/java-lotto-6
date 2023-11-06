ab1d2579ea1588192a6dd9ab90979801ef63eb2a
ch1hyun <och2510@gmail.com>
2023-11-06 15:43:09 +0900

    refactor: Remove instance field of Lotto

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,10 +3,6 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
-    private static final Integer COUNT_OF_LOTTO_NUMBERS = 6;
-    private static final Integer START_RANGE_OF_NUMBER = 1;
-    private static final Integer END_RANGE_OF_NUMBER = 45;
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -18,24 +14,22 @@ public class Lotto {
     }
 
     private void validateCount(List<Integer> numbers) {
-        if (numbers.size() != COUNT_OF_LOTTO_NUMBERS) {
+        if (numbers.size() != COUNT_OF_LOTTO_NUMBERS) { // 상수 enum 추가
             throw new IllegalArgumentException(); // 에러문 추가
         }
     }
 
     private void validateDuplicate(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != COUNT_OF_LOTTO_NUMBERS) {
+        if (numbers.stream().distinct().count() != COUNT_OF_LOTTO_NUMBERS) { // 상수 enum 추가
             throw new IllegalArgumentException(); // 에러문 추가
         }
     }
 
     private void validateEachNumber(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number < START_RANGE_OF_NUMBER || END_RANGE_OF_NUMBER < number) {
+            if (number < START_RANGE_OF_NUMBER || END_RANGE_OF_NUMBER < number) { // 상수 enum 추가
                 throw new IllegalArgumentException(); // 에러문 추가
             }
         }
     }
-
-    // 로또 번호 비교 로직 추가
 }

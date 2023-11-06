a9f1504fdcb07707083feaafb4f8c928e3ae48d6
dbstn1325 <dbstn1325@naver.com>
2023-11-07 07:26:23 +0900

    feat(BonusNumber): 보너스 번호가 숫자가 아니면 IllegalArgumentException 예외처리 기능 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class BonusNumber {
+    private static final String BONUS_NUMBER_MUST_NUMERIC_MESSAGE = "[ERROR] 보너스 번호는 숫자여야 합니다.";
+    private final int bonusNumber;
+
+    private BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static BonusNumber from(String bonusNumber) {
+        try {
+            int convertBonusNumber = Integer.parseInt(bonusNumber);
+            return new BonusNumber(convertBonusNumber);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException(BONUS_NUMBER_MUST_NUMERIC_MESSAGE);
+        }
+    }
+}

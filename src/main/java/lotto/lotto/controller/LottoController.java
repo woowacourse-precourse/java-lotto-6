f72767bccf3a544c9555ad73f4af521242e8cc6e
황준혁 <hyug0819@gmail.com>
2023-11-07 01:26:16 +0900

    feat : Lotto 관리를 위한 Controller 구현

---
+++ src/main/java/lotto/lotto/controller/LottoController.java
@@ -0,0 +1,17 @@
+package lotto.lotto.controller;
+
+import lotto.user.controller.UserController;
+
+public class LottoController {
+    private LottoController(){ }
+    private static final LottoController instance = new LottoController();
+
+    public static LottoController getInstance(){
+        return instance;
+    }
+
+    public void start(){
+        UserController userController = UserController.getInstance();
+        userController.purchaseInput();
+    }
+}

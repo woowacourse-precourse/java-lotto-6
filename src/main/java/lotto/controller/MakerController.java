98823c674cdfc827a69e3e0ebdeeac4d856b1437
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 18:07:30 +0900

    Feat: 잘못된 값을 입력 받으면 적절한 값을 입력할 때까지 반복하는 기능 구현
    로또 구매 횟수 구하는 기능,
    당첨 번호를 만드는 기능,
    보너스 번호를 만드는 기능
    위 3가지 기능과 관련된 잘못된 값을 입력 받으면 다시 입력을 받아 작동하는 기능 구현

---
+++ src/main/java/lotto/controller/MakerController.java
@@ -0,0 +1,85 @@
+package lotto.controller;
+
+import java.util.List;
+
+import lotto.input.InputMachine;
+import lotto.validation.NumbersValidation;
+
+public class MakerController {
+	boolean errorSign;
+
+	InputMachine inputMachine = new InputMachine();
+
+	public int makeCorrectPurchaseTime() {
+
+		String inputPurchaseAmount;
+
+		do {
+			inputPurchaseAmount = inputMachine.inputPurchaseAmount();
+			errorSign = hasPurchaseError(inputPurchaseAmount);
+
+		} while (errorSign);
+
+		return PurchaseTimeMaker.makePurchaseTime(inputPurchaseAmount);
+	}
+
+	private boolean hasPurchaseError(String purchaseAmount) {
+
+		try {
+			PurchaseTimeMaker.makePurchaseTime(purchaseAmount);
+			return false;
+
+		} catch (IllegalArgumentException illegalArgumentException) {
+			return true;
+		}
+	}
+
+	public List<Integer> makeCorrectWinningNumber() {
+
+		String inputWinningNumber;
+
+		do {
+			inputWinningNumber = inputMachine.inputWinningNumber();
+			errorSign = hasWinningNumberError(inputWinningNumber);
+		} while (errorSign);
+
+		return WinningNumberMaker.make(inputWinningNumber);
+	}
+
+	private boolean hasWinningNumberError(String inputWinningNumber) {
+
+		try {
+			NumbersValidation.validateSeparator(inputWinningNumber);
+			WinningNumberMaker.make(inputWinningNumber);
+
+			return false;
+		} catch (IllegalArgumentException illegalArgumentException) {
+			return true;
+		}
+	}
+
+	public int makeCorrectBonusNumber() {
+
+		String inputBonusNumber;
+
+		do {
+			inputBonusNumber = inputMachine.inputBonusNumber();
+			errorSign = hasBonusNumberError(inputBonusNumber);
+		} while (errorSign);
+
+		return WinningNumberMaker.makeBonusNumber(inputBonusNumber);
+	}
+
+	private boolean hasBonusNumberError(String inputBonusNumber) {
+		try {
+
+			WinningNumberMaker.makeBonusNumber(inputBonusNumber);
+			return false;
+
+		} catch (IllegalArgumentException illegalArgumentException) {
+			return true;
+		}
+
+	}
+
+}

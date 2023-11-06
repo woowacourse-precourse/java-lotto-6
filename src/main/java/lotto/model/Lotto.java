5dac5d80deced7029ec8198c395e581f2ad38d79
minkyole <7719s@nave.com>
2023-11-06 16:46:19 +0900

    feat(Lotto.java) : 로또 번호 범위 밖의 숫자가 들어오면 예외 발생 기능 구현
    - 로또 번호 범위(1~45) 밖의 숫자가 들어오면 예외를 발생시키는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,8 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MIN_LOTTO_NUMBER = 1;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,11 +16,22 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-        if (validateDuplicateNumber(numbers)) {
+        if (validateDuplicatedNumber(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 가질 수 없습니다");
         }
+        if (vaildateOutOfRangeNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 지정된 범위안의 숫자만 가질 수 있습니다 범위 "
+                    + MIN_LOTTO_NUMBER + "~" + MAX_LOTTO_NUMBER);
+        }
     }
-    private boolean validateDuplicateNumber(List<Integer> numbers) {
-        return (numbers.stream().distinct().count() < numbers.size());
+    private boolean validateDuplicatedNumber(List<Integer> numbers) {
+        return (numbers.stream()
+                .distinct()
+                .count() < numbers.size());
+    }
+
+    private boolean vaildateOutOfRangeNumber(List<Integer> numbers) {
+      return (numbers.stream()
+                .anyMatch(num -> num < MIN_LOTTO_NUMBER || num > MAX_LOTTO_NUMBER));
     }
 }

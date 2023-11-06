f450a67a4da352e97fefb3a409c231cd75d1b01b
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-06 22:03:12 +0900

    feat(InputView): 줄 띄우기 추가
    fix(InputView): 구입 금액 입력 메서드 이름 변경 printBuyLotto >> printLottoAmount

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -7,18 +7,25 @@ public class InputView {
     private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public String printBuyLotto(){
+    public String printLottoAmount(){
+        printSpace();
         System.out.println(INPUT_AMOUNT);
         return Console.readLine();
     }
 
     public String printInputWinningNumber(){
+        printSpace();
         System.out.println(INPUT_WINNING_NUMBER);
         return Console.readLine();
     }
 
     public String printInputBonusNumber(){
+        printSpace();
         System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
+    }
+
+    private void printSpace(){
+        System.out.println();
     }
 }

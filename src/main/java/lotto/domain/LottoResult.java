4236b89ce5acfbac686c44fb0e0736d12701b713
sunghun <nokdonwithkj211@gmail.com>
2023-11-06 15:38:01 +0900

    feat(LottoResult): 당첨 통계 출력을 위한 description 필드 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -7,18 +7,19 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum LottoResult {
-    ZERO(0, 0),
-    ONE(1, 0),
-    TWO(2, 0),
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    FIVE_AND_BONUS(7, 30000000), // 특별 추가 사항.. 이게 옳은 방법인지는 모르겠다?
-    SIX(6, 2000000000)
+    ZERO(0, 0, "0개 일치"),
+    ONE(1, 0, "1개 일치"),
+    TWO(2, 0, "2개 일치"),
+    THREE(3, 5000, "3개 일치"),
+    FOUR(4, 50000, "4개 일치"),
+    FIVE(5, 1500000, "5개 일치"),
+    FIVE_AND_BONUS(7, 30000000, "5개 일치, 보너스 볼 일치"), // 특별 추가 사항.. 이게 옳은 방법인지는 모르겠다?
+    SIX(6, 2000000000, "6개 일치")
     ;
 
     private final int sameNumberCount;
     private final int winningAmount;
+    private final String description;
 
     private static Map<Integer, LottoResult> lottoResultMap = Collections.unmodifiableMap(
             Arrays.stream(values())
@@ -29,9 +30,10 @@ public enum LottoResult {
         return lottoResultMap.get(sameNumberCount);
     }
 
-    LottoResult(int sameNumberCount, int winningAmount) {
+    LottoResult(int sameNumberCount, int winningAmount, String description) {
         this.sameNumberCount = sameNumberCount;
         this.winningAmount = winningAmount;
+        this.description = description;
     }
 
     public int getSameNumberCount() {

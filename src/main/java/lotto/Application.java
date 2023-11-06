7e4668d6b668ab304d4b46711b3af193e5e0884c
naahnoey <naahyin20@sookmyung.ac.kr>
2023-11-06 19:00:51 +0900

    feat: 로또 구입금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    static int askPurchaseAmount() {
+        System.out.println("구매금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 }

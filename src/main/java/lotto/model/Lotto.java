2a16eaff7121f8b4672d502e97d629d4c40486ea
poow810 <poow810@naver.com>
2023-11-06 16:29:44 +0900

    feat($Lotto): add validation method Add new event to ($Lotto): - add validation method

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,7 +2,9 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.enums.LottoNumber;
 
 public class Lotto {
@@ -18,6 +20,12 @@ public class Lotto {
         if (numbers.size() != LottoNumber.COUNT.getValue()) {
             throw new IllegalArgumentException();
         }
+
+        Set<Integer> numberSet = new HashSet<>();
+        for (Integer number : numbers)
+            if (!numberSet.add(number)) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 중복되지 않은 수여야 합니다.");
+            }
     }
 
     // TODO: 추가 기능 구현
@@ -27,5 +35,21 @@ public class Lotto {
                 LottoNumber.MAX.getValue(), LottoNumber.COUNT.getValue());
         Collections.sort(numbers);
         return numbers;
+    }
+
+    // 당첨 번호 확인
+    public int countNumber(List<Integer> winNumbers) {
+        int count = 0;
+        for (Integer number : numbers) {
+            if (winNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 보너스 번호 포함 여부 확인
+    public boolean containBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
     }
 }

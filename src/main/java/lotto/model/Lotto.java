c0fafae92a857ac8ef526cd0f8d080c3e30e542a
minkyole <7719s@nave.com>
2023-11-06 16:03:06 +0900

    feat(Lotto.java, LottoTest.java) : 로또 번호가 중복된경우 예외처리
    - 로또번호가 중복된 경우 예외처리 구현
    - LottoTest.java 주석 제거 (기존주석 TODO: 이 테스트가 통과할 수 있게 구현 코드 작성)

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -14,7 +14,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (validateDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 가질 수 없습니다");
+        }
     }
-
-    // TODO: 추가 기능 구현
+    private boolean validateDuplicateNumber(List<Integer> numbers) {
+        return (numbers.stream().distinct().count() < numbers.size());
+    }
 }

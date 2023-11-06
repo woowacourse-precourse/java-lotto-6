70769651e607ea40a4ce4638f097593edc48b878
sehee <tpgml701@naver.com>
2023-11-06 20:58:29 +0900

    test: private 확인 테스트 추가

--- src/test/java/lotto/dto/LottoTest.java
+++ src/test/java/lotto/dto/LottoTest.java
@@ -4,6 +4,7 @@ import lotto.dto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,4 +25,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("numbers private 접근 금지 테스트")
+    @Test
+    void numbers_private() {
+        // Given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6)); // YourClass는 테스트하려는 클래스 이름으로 대체해야 합니다.
+        // When
+        try {
+            Field privateNumbersField = Lotto.class.getDeclaredField("lottoNumbers");
+            privateNumbersField.get(lotto); // private 필드에 접근 시 예외가 발생해야 함
+            throw new AssertionError("private로 수정 해야 합니다.");
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+        }
+    }
 }

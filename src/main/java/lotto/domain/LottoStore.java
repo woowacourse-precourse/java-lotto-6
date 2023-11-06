c961dd12884ac1bcf2546655a0bacbcc856aeadb
JongMinCh0i <wonderland314592@gmail.com>
2023-11-06 21:52:33 +0900

    refactor(LottoStore): 매직 리터럴 제거

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -3,6 +3,7 @@ package lotto.domain;
 public class LottoStore {
 
     private static final int LOTTO_PRICE = 1000;
+    public static final String LOTTO_PRICE_PER_THOUSAND = "[ERROR] 로또 금액은 %d원 단위로 판매할 수 있습니다.";
 
     public int getLottoPrice(int money) {
         validInputMoney(money);
@@ -11,7 +12,7 @@ public class LottoStore {
 
     private void validInputMoney(int money) {
         if (money % LOTTO_PRICE != 0 || money <= 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 금액은 1000원 단위로 판매할 수 있습니다.");
+            throw new IllegalArgumentException(LOTTO_PRICE_PER_THOUSAND);
         }
     }
 

280da4ce870d442f9d5cc33bb941f395a3f06067
TaeHye0n <thk001229@gmail.com>
2023-11-06 23:01:57 +0900

    test(winningLotto): 객체 생성 성공 테스트 작성

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static lotto.domain.enums.ErrorMessage.DUPLICATE_NUMBER_ERROR_MESSAGE;
 import static lotto.domain.enums.ErrorMessage.NUMBER_OUT_OF_RANGE_ERROR_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +25,12 @@ class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), -1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NUMBER_OUT_OF_RANGE_ERROR_MESSAGE.getMessage());
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 보너스 번호를 포함 한 객체 생성")
+    void createWinningLotto() {
+        assertDoesNotThrow(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7));
     }
 
 

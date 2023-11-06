c3e9bf07affa3277303df2d4c37e65d0a1f3d527
juno-junho <ssmm0205@naver.com>
2023-11-07 06:12:49 +0900

    feat: 구매 금액 입력 시 예외 검증 로직 구현

---
+++ src/main/java/lotto/domain/Price.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+public class Price {
+
+    private static final int MAX_INPUT= 2_100_000_000;
+    private static final int LOTTO_PRICE = 1_000;
+
+    private final int price;
+
+    public Price(int price) {
+        validate(price);
+        this.price = price;
+    }
+
+    private void validate(int price) {
+        if (isLowerThanMinimumPrice(price)) {
+            throw new IllegalArgumentException();
+        }
+        if (isDivisibleByMinimumPrice(price)) {
+            throw new IllegalArgumentException();
+        }
+        if (isHigherThanMaxInput(price)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isLowerThanMinimumPrice(int price) {
+        return price < LOTTO_PRICE;
+    }
+
+    private boolean isDivisibleByMinimumPrice(int price) {
+        return price % LOTTO_PRICE != 0;
+    }
+
+    private boolean isHigherThanMaxInput(int price) {
+        return price > MAX_INPUT;
+    }
+
+}
---
+++ src/test/java/lotto/domain/PriceTest.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class PriceTest {
+
+    @ParameterizedTest(name = "로또 가격이 {0}원이면 예외를 발생한다")
+    @ValueSource(ints = {-1, 0})
+    void 로또_구매_가격이_최소_로또_금액_미만이면_예외를_발생한다(int price) {
+        assertThatThrownBy(() -> new Price(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "로또 가격이 {0}원이면 예외를 발생한다")
+    @ValueSource(ints = {1001, 1500, 2001})
+    void 로또_구매_가격이_최소_로또_금액으로_나누어_떨어지지_않으면_예외를_발생한다(int price) {
+        assertThatThrownBy(() -> new Price(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "로또 가격이 {0}원이면 예외를 발생하지 않는다")
+    @ValueSource(ints = {1000, 2000, 3000, 10000000, 2_100_000_000})
+    void 로또_구매_가격이_최소_로또_금액_이상이고_최소_로또_금액으로_나누어_떨어지면_예외를_발생하지_않는다(int price) {
+        assertThatNoException().isThrownBy(() -> new Price(price));
+    }
+
+    @Test
+    void 로또_구매_가격이_21억을_넘으면_예외를_발생한다() {
+        assertThatThrownBy(() -> new Price(2_100_000_001))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

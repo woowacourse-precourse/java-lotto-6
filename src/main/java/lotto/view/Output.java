79add90519d854546150a599111a3c042d4a2b01
KimSehoon <shggm2000@gmail.com>
2023-11-07 02:24:23 +0900

    feat: printProfitPercentage method 구현
    - 총 수익률을 출력하는 printProfitPercentage method 구현

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -3,24 +3,28 @@ package lotto.view;
 import static lotto.message.Message.*;
 
 public class Output {
-    public static void printWinningStatisticsMessage(){
+    public static void printWinningStatisticsMessage() {
         System.out.println(WINNING_STATISTICS.message());
         System.out.println(SEPARATOR.message());
     }
 
-    public static void printPurchaseAmountMessage(){
+    public static void printPurchaseAmountMessage() {
         System.out.println(PURCHASE_AMOUNT.message());
     }
 
-    public static void printPurchaseLottoQuantityMessage(int lottoQuantity){
-        System.out.println(lottoQuantity+"개를 구매했습니다.");
+    public static void printPurchaseLottoQuantityMessage(int lottoQuantity) {
+        System.out.println(lottoQuantity + "개를 구매했습니다.");
     }
 
-    public static void printWinningNumberMessage(){
+    public static void printWinningNumberMessage() {
         System.out.println(WINNING_NUMBER.message());
     }
 
-    public static void printBonusNumberMessage(){
+    public static void printBonusNumberMessage() {
         System.out.println(BONUS_NUMBER.message());
+    }
+
+    public static void printProfitPercentage(int profitPercentage) {
+        System.out.println("총 수익률은 " + profitPercentage + "입니다.");
     }
 }

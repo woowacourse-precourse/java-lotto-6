6d8d2cb507095e8fd0168eb5215de5ced8b12745
jalddak <jalddak98@gmail.com>
2023-11-06 16:20:18 +0900

    test - 보너스 번호 입력 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import static lotto.view.ErrorMessage.NOT_NUMBER;
+import static lotto.view.InputView.inputIntegerData;
 import static lotto.view.InputView.inputIntegerListData;
 import static lotto.view.InputView.inputLongData;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -72,5 +73,32 @@ public class InputViewTest {
         //when
         assertThatThrownBy(() -> inputIntegerListData())
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("int 형 자료 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1423", "412312", "123123", "223123", "213123", "2147483647"})
+    void 정상테스트_inputIntegerData(String input) {
+        //given
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        String expect = input;
+        //when
+        String result = inputIntegerData();
+        //then
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @DisplayName("int 형 자료로 변환되지 않으면 예외발생")
+    @ParameterizedTest
+    @ValueSource(strings = {" 1423", "412312 ", "123 123", "223a123", "a213123", "9223372036854775808", "2147483648"})
+    void 예외테스트_inputIntegerData(String input) {
+        //given
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        //when
+        assertThatThrownBy(() -> inputIntegerData())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_NUMBER.getMessage());
     }
 }

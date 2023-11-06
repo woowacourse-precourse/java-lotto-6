e260978eaa18fdced1a172f7caaaaae6d4b5fa7e
Son JunHyung <kingjh1125@gmail.com>
2023-11-06 15:03:20 +0900

    test (Amount): 테스트 추가
    로또 구입 금액 검증에 대한 테스트를 추가하였습니다:
    - 0 이하의 수가 입력될 경우
    - 1,000원으로 나누어 떨어지지 않는 경우

---
+++ src/test/java/lotto/AmountTest.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AmountTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    @DisplayName("로또 구입 금액이 0 이하면 예외가 발생한다.")
+    @Test
+    void createAmountByNegative() {
+        assertSimpleTest(() -> {
+            runException("-1");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createAmountByDivideNumber() {
+        assertSimpleTest(() -> {
+            runException("100");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}

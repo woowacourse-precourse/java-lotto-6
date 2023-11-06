cbaa517228336cbd34e09e48abf832091d7f19b5
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 19:35:35 +0900

    rename: LottoResultCheckerTest getResult -> check

--- src/test/java/lotto/LottoResultCheckerTest.java
+++ src/test/java/lotto/LottoResultCheckerTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoResultCheckerTest {
     @DisplayName("로또 번호와 당첨 번호를 비교해 결과를 반환한다.")
     @Test
-    public void getResult() throws Exception {
+    public void check() throws Exception {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
 

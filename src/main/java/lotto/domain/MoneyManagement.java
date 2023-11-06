5515fbee03d803a9fcacf21a476d3a5aa970ca05
toychip <dlawnsgud427@naver.com>
2023-11-06 13:21:51 +0900

    feat: 로또 구매 금액이 숫자인지 검증하는 메서드 추가

--- src/main/java/lotto/domain/MoneyManagement.java
+++ src/main/java/lotto/domain/MoneyManagement.java
@@ -3,11 +3,24 @@ package lotto.domain;
 public class MoneyManagement {
     private final int quantity;
 
-    public MoneyManagement(int userInput) {
+    public MoneyManagement(final String userInput) {
         validate(userInput);
-        quantity = userInput;
+        this.quantity = toInt(userInput);
     }
 
-    private void validate(int userInput) {
+    private void validate(final String userInput) {
+        validNumber(userInput);
+        int userAmount = toInt(userInput);
     }
+
+    private void validNumber(final String userInput) {
+        if (!userInput.matches("%d")) {
+            throw new IllegalArgumentException("금액은 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private int toInt(String userInput) {
+        return Integer.parseInt(userInput);
+    }
+
 }

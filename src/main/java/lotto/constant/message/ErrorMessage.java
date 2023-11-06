e9c18fa8ae3295c3f330d34d3a6339bec7ad32a6
hyerimmy <heyrim2010@naver.com>
2023-11-07 00:06:17 +0900

    refactor: 변수명 상세하게 수정 - INVALID_PURCHASE_AMOUNT -> INVALID_PURCHASE_AMOUNT_UNIT

--- src/main/java/lotto/constant/message/ErrorMessage.java
+++ src/main/java/lotto/constant/message/ErrorMessage.java
@@ -8,7 +8,7 @@ public class ErrorMessage {
     private static final String prefix = "[ERROR] ";
     public static final String NUMBER_FORMAT_ERROR = prefix + "숫자만 입력 가능합니다.";
     public static final String DUPLICATE_NUMBER = prefix + "중복되는 숫자입니다.";
-    public static final String INVALID_PURCHASE_AMOUNT
+    public static final String INVALID_PURCHASE_AMOUNT_UNIT
            = prefix + String.format("%d원 단위의 금액만 입력 가능합니다.", PURCHASE_AMOUNT_UNIT);
     public static final String INVALID_LOTTO_NUMBER_RANGE
             = prefix + String.format("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", MIN_IN_NUMBER, MAX_IN_NUMBER);

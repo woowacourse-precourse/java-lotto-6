e2f57a8b227a14b1336b96a83382e82d779d56ec
donghyun <kdhdh001@naver.com>
2023-11-07 04:25:27 +0900

    feat: 당첨 번호 입력값 6개, 중복, 범위 유효성 구현

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -11,6 +11,8 @@ public class Constants {
     public static final String NOT_DIVISIBLE_MSG = ERROR + "구입금액이 로또 가격인 1000원으로 나누어 떨어져야 합니다.";
     public static final String EMPTY_INPUT_MSG = ERROR + "당첨 번호를 입력해주세요.";
     public static final String MISSING_COMMA_MSG = ERROR + "당첨 번호는 쉼표(,)로 구분하여 입력해주세요.";
-    public static final String NOT_NUMBER_MSG = ERROR + "당첨 번호는 숫자로 입력해주세요.";
+    public static final String INVALID_NUM_COUNT_MSG = ERROR + "당첨 번호는 6개를 입력해야 합니다.";
+    public static final String INVALID_NUM_RANGE_MSG = ERROR + "당첨 번호와 보너스 번호는 1부터 45까지의 정수를 입력해야 합니다.";
+    public static final String DUPLICATE_NUM_MSG = ERROR + "중복된 번호가 있습니다. 각 번호는 서로 다른 숫자여야 합니다.";
 
 }

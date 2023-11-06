49bafb8c9e0a404a52e4f51a889e2702206cd560
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 22:17:05 +0900

    feat: added additional constant for magic number 5

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
     public static final int CNT_LOTTO_NUMBER = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
-    public static final String DELIMETER = ",";
+    public static final String DELIMITER = ",";
+    public static final int MATCH_COUNT_FOR_BONUS = 5;
+    public static final int LOTTO_TICKET_PRICE = 1000;
 }

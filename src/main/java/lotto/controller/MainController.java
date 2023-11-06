741d9f9fe241b1957e9d4477031c8aaed80e6be1
oxdjww <oxdjww@gmail.com>
2023-11-07 04:27:20 +0900

    feat : `3.1` 당첨 번호 입력 안내문 출력 기능 구현 완료

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import static lotto.view.constants.ViewMessage.GET_WINNING_NUMBER;
+
 import lotto.domain.Buyer;
 import lotto.domain.Lottos;
 import lotto.view.OutputView;
@@ -14,6 +16,11 @@ public class MainController {
     public static void run() {
         Buyer buyer = buyerController.createBuyer();
         Lottos lottoTicket = lottoController.createLottoTicket();
+
         OutputView.printMessage(lottoTicket);
+
+        OutputView.printMessage(GET_WINNING_NUMBER);
+
+
     }
 }
--- src/main/java/lotto/view/constants/ViewMessage.java
+++ src/main/java/lotto/view/constants/ViewMessage.java
@@ -6,7 +6,8 @@ public enum ViewMessage {
     GET_PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
     LOTTO_PURCHASE_NOTICE_MESSAGE(
             String.format("%d개를 구매했습니다.", LOTTO_COUNT.getValue())
-    );
+    ),
+    GET_WINNING_NUMBER("당첨 번호를 입력해 주세요.");
     private final String message;
 
     ViewMessage(String message) {

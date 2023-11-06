88d53c8a2008ed982834f6ca94cb0624950ba6e8
YoungJun-L <dudwns328@naver.com>
2023-11-06 19:38:51 +0900

    feat: 안내 메세지 출력 기능 구현

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,4 +1,33 @@
 package lotto.ui;
 
 public class OutputView {
+    private static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String STATISTICS_MESSAGE = """
+        당첨 통계
+        ---
+        """;
+
+    public OutputView() {
+    }
+
+    public void printInputPriceMessage() {
+        System.out.println(INPUT_PRICE_MESSAGE);
+    }
+
+    public void printInputWinningNumberMessage() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+    }
+
+    public void printInputBonusNumberMessage() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public void printBoughtLottoNumbers() {
+    }
+
+    public void printStatistics() {
+        System.out.println(STATISTICS_MESSAGE);
+    }
 }

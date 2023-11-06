d297db94366d1b350c04bacb9b59fcf5d44da3c5
tackyu97 <tack970913@gmail.com>
2023-11-06 16:18:30 +0900

    feat: 구입할 로또 갯수 계산 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -10,5 +10,9 @@ public class Money {
         validator.money(inputMoney);
         this.money = inputMoney;
     }
-    
+
+    public int toLottoAmount() {
+        return Integer.valueOf(money) % 1000;
+    }
+
 }

6bf44d494463ced718986d29bba16da7d15d8e90
JAY-Winter <rubat0@naver.com>
2023-11-07 01:18:12 +0900

    feat: 통계 Model

--- src/test/java/lotto/StatisticsMachineTest.java
+++ src/test/java/lotto/StatisticsMachineTest.java
@@ -1,4 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
-class StatisticsMachineTest {
-  
+package lotto;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class StatisticsMachineTest extends NsTest {
+
+    @Test
+    void 통계_구하기() {
+        InputLottoNumber inputLottoNumber = new InputLottoNumber("1,3,5,7,9,11");
+        InputBonusNumber inputBonusNumber = new InputBonusNumber("6");
+
+        assertRandomUniqueNumbersInRangeTest(
+            () -> {
+                RandomNumber randomNumber = new RandomNumber();
+                Lotto lotto = new Lotto(randomNumber.getRandomNumber());
+                StatisticsMachine statisticsMachine = new StatisticsMachine(inputLottoNumber,
+                    inputBonusNumber, lotto);
+                statisticsMachine.calculate();
+
+                System.out.println("randomNumber = " + randomNumber.getRandomNumber());
+            },
+            List.of(1, 2, 3, 4, 5, 6)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
 }

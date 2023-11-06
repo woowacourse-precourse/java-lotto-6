a7d7139aca3575c2587d474d6e72d914cd12efcf
chaeshee0908 <chaeshee0908@naver.com>
2023-11-06 16:18:06 +0900

    [add] Ranking 일치 개수, 보너스 일치 여부 항목 추가

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,23 +1,35 @@
 package lotto.domain;
 
 public enum Ranking {
-    FIRST("6개 일치", "2,000,000,000"),
-    SECOND("5개 일치, 보너스 볼 일치", "30,000,000"),
-    THIRD("5개 일치", "1,500,000"),
-    FOURTH("4개 일치", "50,000"),
-    FIFTH("3개 일치", "5,000"),
-    NONE("등수 밖", "0");
+    FIRST("6개 일치", 6, false, "2,000,000,000"),
+    SECOND("5개 일치, 보너스 볼 일치", 5, true, "30,000,000"),
+    THIRD("5개 일치", 5, false, "1,500,000"),
+    FOURTH("4개 일치", 4, false, "50,000"),
+    FIFTH("3개 일치", 3, false, "5,000"),
+    NONE("등수 밖", 0, false, "0");
 
     private final String description;
+    private final int matchNumber;
+    private final boolean matchBonus;
     private final String prize;
 
-    private Ranking(String description, String prize) {
+    private Ranking(String description, int matchNumber, boolean matchBonus, String prize) {
         this.description = description;
+        this.matchNumber = matchNumber;
+        this.matchBonus = matchBonus;
         this.prize = prize;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
+    }
+
+    public boolean isMatchBonus() {
+        return matchBonus;
     }
 
     public String getPrize() {

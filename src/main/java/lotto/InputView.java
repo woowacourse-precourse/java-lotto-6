f0cefb8bea4a9ff8b988067da95565e83c836658
donghoonyeom <dua5410773@naver.com>
2023-11-07 01:23:07 +0900

    feat(input): InputView 클래스 업데이트
    사용자 입력을 처리하는 InputView 클래스를 업데이트했습니다. 당첨 번호 및 보너스 번호를 입력받는 `getWinningNumbers()` 와 `getBonusNumber(Set<Integer> winningNumbers)` 함수를 추가하고, 사용자의 입력에 대한 유효성 검사 및 예외 처리를 수행합니다.

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class InputView {
 
@@ -24,6 +26,54 @@ public class InputView {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return getPurchaseAmount();
+        }
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        try {
+            System.out.println("당첨 번호를 입력해 주세요");
+            String input = playerInput().trim(); // 입력된 문자열 앞뒤 공백 제거
+            String[] numberStrings = input.split(",");
+
+            // 중복된 숫자 체크를 위한 Set
+            Set<Integer> uniqueNumbers = new HashSet<>();
+
+            // 입력된 문자열이 숫자이고 중복되지 않는지 확인
+            for (String numberString : numberStrings) {
+                int number = Integer.parseInt(numberString);
+                if (number < 1 || number > 45 || !uniqueNumbers.add(number)) {
+                    throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 중복되지 않는 숫자 6개를 쉼표로 구분하여 입력해 주세요.");
+                }
+            }
+
+            if (uniqueNumbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 중복되지 않는 숫자 6개를 입력해 주세요.");
+            }
+
+            return StringUtil.convertToIntegerList(input, ",");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getWinningNumbers();
+        }
+    }
+
+    public static int getBonusNumber(Set<Integer> winningNumbers) {
+        try {
+            System.out.println("보너스 번호를 입력해 주세요");
+            int bonusNumber = Integer.parseInt(playerInput().trim());
+
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력해 주세요.");
+            }
+
+            if (winningNumbers.contains(bonusNumber)) {
+                throw new IllegalArgumentException("[ERROR] 이미 당첨 번호로 선택된 숫자입니다.");
+            }
+
+            return bonusNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getBonusNumber(winningNumbers);
         }
     }
 }

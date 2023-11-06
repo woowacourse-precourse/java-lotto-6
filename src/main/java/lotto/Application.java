000c013197601cce40bd7424eb2e1d2d97d3cc49
totocm <typesapk8445@gmail.com>
2023-11-06 21:23:51 +0900

    모든 로직을 구현완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,14 +16,14 @@ public class Application {
     private static final LottoController lottoController = new LottoController();
     public static void main(String[] args) {
 
-        LottoResultPrint lottoResultPrint = new LottoResultPrint();
-
+       LottoResultPrint lottoResultPrint = new LottoResultPrint();
+/*
         lottoResultPrint.addResult(3,false);
         lottoResultPrint.addResult(4,false);
         lottoResultPrint.addResult(4,false);
         lottoResultPrint.addResult(5,false);
 
-        lottoResultPrint.lottoResultPrint();
+        lottoResultPrint.lottoResultPrint();*/
         //3개 일치 ( 5,000원 ) - 1개
         //4개 일치 ( 50,000월 ) - 2개
         //5개 일치 ( 1,500,000원 ) - 1개
@@ -39,6 +39,7 @@ public class Application {
         buyPriceInfo = lottoController.buyInfo(buyPriceInfo);
 
         int buyNumber = buyPriceInfo.getBuyNumber();
+        // 여기에 티켓들의 정보들이 다 담겨있음 -> 조회 필요
         List<SeasonLottoResultVO> autoLottoTicket = lottoController
                                                     .createAutoLottoBuyChoice(buyPriceInfo
                                                     .getBuyNumber());
@@ -50,14 +51,11 @@ public class Application {
         //유저가 입력받은 값을 받아서 Lotto에 저장하는 구문
         Lotto userLottoNumbers = lottoController
                                 .userInputMasterLottoNumbers();
-        System.out.println(userLottoNumbers);
         LottoBonus userLottoBonusNumber;
 
         //유저 보너스 번호 입력하는 구간
         LottoBonus lottoBonus = lottoController
                                 .userInputMasterLottoBonusNumber(userLottoNumbers);
-        System.out.println(lottoBonus);
-
         // 번호들 출력되는 구문
         lottoController.showAutoLottoTicks(buyNumber,autoLottoTicket);
 
@@ -66,8 +64,27 @@ public class Application {
 
         // 통계 이넘부터 구현하면 됨
 
+        // --- 결과담기
 
+        for (SeasonLottoResultVO ticket : autoLottoTicket){
+            int matchPoint = 0;
+            boolean bonusMatch = false;
 
+            for(int number : ticket.getAutoLottoNumber()){
+                if(userLottoNumbers.getNumbers().contains(number)){
+                    //일치하는 것 디버깅
+                    System.out.println(userLottoNumbers.getNumbers().contains(number));
+                    matchPoint++;
+                    System.out.println(matchPoint);
+                }
+            }
+            if (lottoBonus.getNumber().contains(ticket.getAutoBonusNumber())){
+                bonusMatch = true;
+            }
+            lottoResultPrint.addResult(matchPoint,bonusMatch);
+        }
+
+        lottoResultPrint.lottoResultPrint();
 
 
 

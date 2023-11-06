bcefa2403dd0098a6938a1cb52bf4ceebe9730d2
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 18:20:49 +0900

    feat: 플레이어 입력 확인 기능 개선 및 구현

--- src/main/java/lotto/model/PlayerLottoAmount.java
+++ src/main/java/lotto/model/PlayerLottoAmount.java
@@ -1,2 +1,46 @@
-package lotto.model;public class PlayerLottoAmount {
+package lotto.model;
+
+import lotto.view.ExceptionMessage;
+
+public class PlayerLottoAmount {
+    private static final int LOTTO_MIN_AMOUNT = 1000;
+    private final int amount;
+
+    public PlayerLottoAmount(String amount) {
+        int amountNum = validateNumber(amount);
+        validateAmount(amountNum);
+        this.amount = amountNum;
+    }
+
+    private void validateAmount(int amount) {
+        validateNatural(amount);
+        validateDivisible(amount);
+    }
+
+    public int calculateLottoCount() {
+        return amount / 1000;
+    }
+
+    private static int validateNumber(String amount) {
+        try{
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            ExceptionMessage.numberException();
+            throw new IllegalStateException();
+        }
+    }
+
+    private void validateNatural(int amount) {
+        if (amount <= 0) {
+            ExceptionMessage.naturalException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivisible(int amount) {
+        if (amount % LOTTO_MIN_AMOUNT != 0) {
+            ExceptionMessage.divisibleException();
+            throw new IllegalArgumentException();
+        }
+    }
 }

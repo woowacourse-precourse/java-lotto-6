cdfe17f6c75a12d3a6646640dd96b62f6839b894
cousim46 <wjdghldns24@naver.com>
2023-11-06 22:58:58 +0900

    fix : 정렬하는 로직 삭제

--- src/main/java/lotto/AutomaticLottoNumbers.java
+++ src/main/java/lotto/AutomaticLottoNumbers.java
@@ -1,14 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class AutomaticLottoNumbers {
 
     public static List<Integer> create() {
-        List<Integer> winLottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(winLottoNumbers);
-        return winLottoNumbers;
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }

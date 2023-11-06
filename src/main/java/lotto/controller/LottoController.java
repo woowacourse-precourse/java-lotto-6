ff11de542241bd46f3740353a7eecc4daa747d18
shkisme <shkisme0130@gmail.com>
2023-11-06 20:36:59 +0900

    feat: 구매 금액을 입력받고 로또 개수를 도출하여 로또 리스트를 생성하는 기능 구현 (LottoController)

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import static lotto.model.Lotto.generateLotto;
+
+import java.util.ArrayList;
+import java.util.List;
 import lotto.generator.NumberGenerator;
+import lotto.model.Lotto;
+import lotto.model.Lottos;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -13,5 +19,24 @@ public class LottoController {
         this.inputView = inputView;
         this.outputView = outputView;
         this.numberGenerator = numberGenerator;
+    }
+
+    public void run() {
+        int amounts = inputView.inputAmount();
+        int lottoCount = getLottoCount(amounts);
+
+        Lottos lottos = generateLottos(lottoCount);
+    }
+
+    private int getLottoCount(int amounts) {
+        return amounts / 1000;
+    }
+
+    private Lottos generateLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(generateLotto(numberGenerator));
+        }
+        return new Lottos(lottos);
     }
 }

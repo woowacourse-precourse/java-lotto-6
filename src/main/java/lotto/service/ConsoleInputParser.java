1b725a8ae226fa0fdd608e4f2e519c59bd56a0da
devhoya97 <diajbh123@gmail.com>
2023-11-06 15:33:51 +0900

    refactor(view): 입력을 파싱하는 역할을 서비스 객체에게 위임함
    기존에 입력을 파싱하던 역할을 InputView에서 하는 것은
    적절하지 않다고 판단하여 service단에 파싱만을 위한 객체 생성함

---
+++ src/main/java/lotto/service/ConsoleInputParser.java
@@ -0,0 +1,51 @@
+package lotto.service;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.wrapper.BonusNumber;
+import lotto.domain.wrapper.PurchaseAmount;
+import lotto.utils.ErrorMessage;
+import lotto.utils.LottoConstant;
+
+public class ConsoleInputParser {
+    public PurchaseAmount toPurchaseAmount(String input) {
+        validateInteger(input);
+        return new PurchaseAmount(Integer.parseInt(input));
+    }
+
+    public Lotto toLotto(String input) {
+        List<Integer> numbers = Arrays.stream(input.split(LottoConstant.DELIMITER.get()))
+                .peek(this::validateInteger)
+                .map(Integer::parseInt)
+                .toList();
+        return new Lotto(numbers);
+    }
+
+    public BonusNumber toBounsNumber(String input, Lotto winningLotto) {
+        validateInteger(input);
+        return new BonusNumber(Integer.parseInt(input), winningLotto);
+    }
+
+    private void validateInteger(String input) {
+        validateNotNull(input);
+        validateNotEmpty(input);
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_INTEGER_INPUT.getWithPrefix());
+        }
+    }
+
+    private void validateNotNull(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_INPUT.getWithPrefix());
+        }
+    }
+
+    private void validateNotEmpty(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getWithPrefix());
+        }
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,76 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
-import lotto.domain.wrapper.LottoNumber;
-import lotto.domain.wrapper.PurchaseAmount;
-import lotto.utils.ErrorMessage;
-import lotto.utils.LottoConstant;
 
 public class InputView {
-    public int getPurchaseAmount() {
-        String input = Console.readLine();
-        try {
-            validateNotNull(input);
-            validateNotEmpty(input);
-            validateParsedToInteger(input);
-            new PurchaseAmount(Integer.parseInt(input));
-        } catch (IllegalArgumentException illegalArgumentException) {
-            System.out.println(illegalArgumentException.getMessage());
-            getPurchaseAmount();
-        }
-        return Integer.parseInt(input);
-    }
-
-    public List<Integer> getWinningNumbers() {
-        String input = Console.readLine();
-        validateNotNull(input);
-        validateNotEmpty(input);
-        return Arrays.stream(input.split(LottoConstant.DELIMITER.get()))
-                .peek(this::validateParsedToInteger)
-                .map(Integer::parseInt)
-                .toList();
-    }
-
-    public int getBonusNumber(List<Integer> winningNumbers) {
-        String input = Console.readLine();
-        try {
-            validateBonusNumber(input, winningNumbers);
-            LottoNumber bonusNumber = new LottoNumber(Integer.parseInt(input));
-        } catch (IllegalArgumentException illegalArgumentException) {
-            System.out.println(illegalArgumentException.getMessage());
-            getBonusNumber(winningNumbers);
-        }
-        return Integer.parseInt(input);
-    }
-
-    private void validateBonusNumber(String input, List<Integer> winningNumbers) {
-        validateNotNull(input);
-        validateNotEmpty(input);
-        validateParsedToInteger(input);
-        if (winningNumbers.contains(Integer.parseInt(input))) {
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_DUPLICATION.getWithPrefix());
-        }
-    }
-
-    private void validateNotNull(String input) {
-        if (input == null) {
-            throw new IllegalArgumentException(ErrorMessage.NULL_INPUT.getWithPrefix());
-        }
-    }
-
-    private void validateNotEmpty(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getWithPrefix());
-        }
-    }
-
-    private void validateParsedToInteger(String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_INTEGER_INPUT.getWithPrefix());
-        }
+    public String getInputFromConsole() {
+        return Console.readLine();
     }
 }

d9fa9fb159d2822a5b47f5d9aa247fb79942a21f
seminchoi <choicco89@gmail.com>
2023-11-06 15:33:47 +0900

    refactor(constant.InformationMessage) : 수익률 출력 메세지 띄어쓰기 맞춤

--- src/main/java/lotto/constant/InformationMessage.java
+++ src/main/java/lotto/constant/InformationMessage.java
@@ -12,7 +12,7 @@ public enum InformationMessage {
     SEPARATOR_LINE("---"),
     GENERAL_RANK_RESULT_FORMAT("%d개 일치 (%,d원) - %d개"),
     SECOND_RANK_RESULT_FORMAT("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
-    PROFIT_RATE_FORMAT("총 수익률은 %,.1f%% 입니다."),
+    PROFIT_RATE_FORMAT("총 수익률은 %,.1f%%입니다."),
     ;
 
     private final String message;

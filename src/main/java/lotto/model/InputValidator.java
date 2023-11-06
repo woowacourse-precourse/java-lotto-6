0e6b5ee6ad0acbf724d6b563335fa4468de134be
SungWoo <sungwoo166@gmail.com>
2023-11-06 13:45:22 +0900

    feat: Implement IO view with InputValidator

---
+++ src/main/java/lotto/model/InputValidator.java
@@ -0,0 +1,91 @@
+package lotto.model;
+
+import static lotto.exception.ExceptionMessage.EMPTY_INPUT;
+import static lotto.exception.ExceptionMessage.INVALID_DUPLICATE_NUMBER;
+import static lotto.exception.ExceptionMessage.INVALID_INPUT_NOT_INTEGER_ERROR;
+import static lotto.exception.ExceptionMessage.INVALID_NUMBER_RANGE;
+import static lotto.exception.ExceptionMessage.INVALID_PURCHASE_AMOUNT;
+import static lotto.exception.ExceptionMessage.INVALID_WINNING_NUMBER_LENGTH;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class InputValidator {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public int validateInputMoney(String stringInteger) {
+        validateNumeric(stringInteger);
+        int purchaseMoney = Integer.parseInt(stringInteger);
+        validatePurchaseAmount(purchaseMoney);
+        return purchaseMoney;
+    }
+
+    public void validateNumeric(String stringNumber) {
+        if (!stringNumber.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(INVALID_INPUT_NOT_INTEGER_ERROR.getErrorMessage());
+        }
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount <= 0 || purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT.getErrorMessage());
+        }
+    }
+
+    public int validateBonusNumber(String stringInteger) {
+        validateNumeric(stringInteger);
+        return Integer.parseInt(stringInteger);
+    }
+
+    public List<Integer> validateAndParseWinningNumbers(String winningNumbersInput) {
+        validateInput(winningNumbersInput);
+        List<Integer> numbers = parseWinningNumbers(winningNumbersInput);
+        validateWinningNumbersCount(numbers);
+        validateNoDuplicateNumbers(numbers);
+        validateRangeOfNumbers(numbers);
+        return numbers;
+    }
+
+    private void validateInput(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_INPUT.getErrorMessage());
+        }
+    }
+
+    private List<Integer> parseWinningNumbers(String winningNumbersInput) {
+        try {
+            return Arrays.stream(winningNumbersInput.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_NOT_INTEGER_ERROR.getErrorMessage());
+        }
+    }
+
+    private void validateWinningNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_LENGTH.getErrorMessage());
+        }
+    }
+
+    private void validateNoDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(INVALID_DUPLICATE_NUMBER.getErrorMessage());
+        }
+    }
+
+    private void validateRangeOfNumbers(List<Integer> numbers) {
+        boolean outOfRange = numbers.stream()
+                .anyMatch(number -> number < 1 || number > 45);
+
+        if (outOfRange) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE.getErrorMessage());
+        }
+    }
+}
---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,85 @@
+package lotto.view;
+
+import static lotto.view.LottoMessage.BONUS_BALL_MATCH_MESSAGE;
+import static lotto.view.LottoMessage.ENTER_BONUS_NUMBER;
+import static lotto.view.LottoMessage.ENTER_WINNING_LOTTO_NUMBER;
+import static lotto.view.LottoMessage.MATCH_AND_PRIZE_MESSAGE;
+import static lotto.view.LottoMessage.PURCHASE_AMOUNT_MESSAGE;
+import static lotto.view.LottoMessage.PURCHASE_LOTTO_AMOUNT_MESSAGE;
+import static lotto.view.LottoMessage.TOTAL_PROFIT_RATE_MESSAGE;
+import static lotto.view.LottoMessage.WINNING_STATISTICS_MESSAGE;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
+import lotto.model.LottoResult;
+import lotto.model.Rank;
+
+public class View {
+
+    public String requestPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public void printPurchasedLottoAmount(int purchasedLottoTickets) {
+        System.out.printf(PURCHASE_LOTTO_AMOUNT_MESSAGE.getMessage(), purchasedLottoTickets);
+    }
+
+    public void printLottoNumbers(Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        System.out.println(numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", ", "[", "]")));
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+
+    public String requestWinningNumbers() {
+        printMessage(ENTER_WINNING_LOTTO_NUMBER.getMessage());
+        return Console.readLine();
+    }
+
+    public String requestBonusNumber() {
+        printMessage(ENTER_BONUS_NUMBER.getMessage());
+        return Console.readLine();
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+
+    public void displayResults(LottoResult lottoResult) {
+        printMessage(WINNING_STATISTICS_MESSAGE.getMessage());
+        printRankResults(lottoResult);
+        printTotalProfitRate(lottoResult.getProfitRate());
+    }
+
+    private void printRankResults(LottoResult lottoResult) {
+        Arrays.stream(Rank.values())
+                .filter(rank -> rank != Rank.NONE)
+                .sorted(Comparator.comparingInt(Rank::ordinal).reversed())
+                .forEach(rank -> {
+                    String bonusMatchMessage = rank == Rank.SECOND ? BONUS_BALL_MATCH_MESSAGE.getMessage() : "";
+                    String message = String.format(
+                            MATCH_AND_PRIZE_MESSAGE.getMessage(),
+                            rank.getMatchCount(),
+                            bonusMatchMessage,
+                            rank.getPrizeMoney(),
+                            lottoResult.getLottoResult().getOrDefault(rank, 0)
+                    );
+                    printMessage(message);
+                });
+    }
+
+    private void printTotalProfitRate(BigDecimal profitRate) {
+        printMessage(String.format(TOTAL_PROFIT_RATE_MESSAGE.getMessage(), profitRate));
+    }
+}

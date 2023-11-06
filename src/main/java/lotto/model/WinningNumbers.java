9705b42458a260c07a9cfd2ee58d93332ed044f1
sehee <tpgml701@naver.com>
2023-11-06 21:35:48 +0900

    feat: WinningNumbers 유효 검사 기능 추가

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class WinningNumbers {
@@ -10,6 +11,7 @@ public class WinningNumbers {
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         validateDuplicate(winningNumbers);
+        validateDuplicateBonus(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
@@ -18,6 +20,14 @@ public class WinningNumbers {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(winningNumbers);
         if (nonDuplicateNumbers.size() != 6) {
             throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateDuplicateBonus(List<Integer> winningNumbers, int bonusNumber) {
+        boolean hasDuplicate = winningNumbers.stream()
+                .anyMatch(number -> number == bonusNumber);
+        if (hasDuplicate) {
+            throw new IllegalArgumentException("당첨 번호에 보너스번호와 같은 번호가 있습니다.");
         }
     }
 

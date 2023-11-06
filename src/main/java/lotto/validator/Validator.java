b75764953e6f02a3bafafe45f26c99590c772c16
DooDuZ <sin9158@naver.com>
2023-11-07 01:17:55 +0900

    refactor: 정수 판별 메서드 내부 수정
    try/catch 통한 처리로 변경

--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -1,8 +1,13 @@
 package lotto.validator;
 
+import lotto.game.ErrorMessage;
 public class Validator {
-    public static boolean isInteger(String inputMessage) throws IllegalArgumentException{
-        Integer.parseInt(inputMessage);
+    public static boolean isInteger(String inputMessage) {
+        try{
+            Integer.parseInt(inputMessage);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(ErrorMessage.REQUEST_INTEGER_MESSAGE.getMessage());
+        }
         return true;
     }
     public static boolean validateNumberRange(int value, int min, int max){

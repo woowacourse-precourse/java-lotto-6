ac0e01fa00bfad986f2d6d52b228801c1bb1b58d
JoonKyo <joonkyolee@gmail.com>
2023-11-06 17:09:35 +0900

    feat(InputValidator): 초기 입력에 대한 검증을 진행하는 메서드 추가

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -1,2 +1,34 @@
-package lotto.view;public class InputValidator {
+package lotto.view;
+
+import static lotto.exception.ErrorMessage.INPUT_IS_NULL;
+import static lotto.exception.ErrorMessage.INPUT_IS_EMPTY;
+
+import lotto.exception.Exception;
+
+public class InputValidator {
+
+    public static void validateInput(String input) {
+        checkNull(input);
+        checkEmpty(input);
+    }
+
+    private static void checkNull(String input) {
+        if (isNull(input)) {
+            throw Exception.of(INPUT_IS_NULL);
+        }
+    }
+
+    private static void checkEmpty(String input) {
+        if (!hasValue(input)) {
+            throw Exception.of(INPUT_IS_EMPTY);
+        }
+    }
+
+    private static boolean isNull(String input) {
+        return input == null;
+    }
+
+    private static boolean hasValue(String input) {
+        return !input.isBlank();
+    }
 }

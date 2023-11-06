a395d2ab39c6259fed341085b056122167f69887
DooDuZ <sin9158@naver.com>
2023-11-07 03:39:25 +0900

    feat: Message 클래스 추가

---
+++ src/main/java/lotto/game/Messasge.java
@@ -0,0 +1,10 @@
+package lotto.game;
+
+public class Messasge {
+    public static final String ERROR = "[ERROR]";
+    public static final String REQUEST_PRICE = "구입금액을 입력해 주세요.";
+    public static final String REQUEST_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String RESULT = "당첨 통계";
+    public static final String DELIMITER = "---";
+}

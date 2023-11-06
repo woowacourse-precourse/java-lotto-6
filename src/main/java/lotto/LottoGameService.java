bbf7fe060edc8ba266eb61e5c99b4966df79dd60
wda067 <mg6359@naver.com>
2023-11-07 03:19:20 +0900

    feat(LottoGameService): 당첨 번호를 컬렉션으로 변환하는 기능 구현

--- src/main/java/lotto/LottoGameService.java
+++ src/main/java/lotto/LottoGameService.java
@@ -1,23 +1,40 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoGameService {
 
     private static final int lottoPrice = 1000;
+    private static final int initialCount = 0;
     private final List<List<Integer>> purchasedLottoNumbers = new ArrayList<>();
+    private final EnumMap<LottoRank, Integer> lottoRakingMap = new EnumMap<>(LottoRank.class);
+
+    public LottoGameService() {
+        for (LottoRank rank : LottoRank.values()) {
+            lottoRakingMap.put(rank, initialCount);
+        }
+    }
 
     public void generateLottoNumbers() {
         Lotto lotto = new Lotto(LottoNumbersGenerator.generateLottoNumbers());
         purchasedLottoNumbers.add(lotto.getNumbers());
     }
 
-    public int getLottoPurchaseCount(int lottoPurchaseAmount) {
+    public int getLottoTicketCount(int lottoPurchaseAmount) {
         return lottoPurchaseAmount / lottoPrice;
     }
 
     public List<List<Integer>> getPurchasedLottoNumbers() {
         return purchasedLottoNumbers;
+    }
+
+    public List<Integer> convertWinningNumbersToCollection(String lottoWinningNumbers) {
+        return Stream.of(lottoWinningNumbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

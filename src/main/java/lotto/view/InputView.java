8658bcb568f47521d551a8f9c25876c02beb303e
MinseokGo <rhalstjr1999@naver.com>
2023-11-07 01:16:10 +0900

    refactor($InputView): 싱글톤 패턴 적용
    InputView에 싱글톤 패턴을 적용하였습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,15 @@ package lotto.view;
 import lotto.utils.Constants;
 
 public class InputView {
+    private static final InputView inputView = new InputView();
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return inputView;
+    }
+
     public void printPurchaseMoneyMessage() {
         System.out.println(Constants.REQUEST_INPUT_PURCHASE_MONEY_MESSAGE);
     }

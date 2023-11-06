fbd81e3d67428fc3e3612d596269b2cff9dcd723
Haeun Jeong <gkdms4248@gmail.com>
2023-11-06 20:15:41 +0900

    test: 보너스 번호 검사 기능 테스트 추가

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -1,5 +1,6 @@
 package lotto.util;
 
+import static lotto.util.ErrorMessage.INPUT_BONUS_DUPLICATE;
 import static lotto.util.ErrorMessage.INPUT_DUPLICATE_NUMBER;
 import static lotto.util.ErrorMessage.INPUT_NOT_IN_RANGE;
 import static lotto.util.ErrorMessage.INPUT_NOT_NUMBER;
@@ -7,6 +8,8 @@ import static lotto.util.ErrorMessage.INPUT_OUT_OF_SIZE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -22,7 +25,7 @@ public class ValidatorTest {
         validator = new Validator();
     }
 
-    @DisplayName("구입 금액에 숫자가 아닌 글자를 입력하면 에러 발생")
+    @DisplayName("구입 금액에 숫자가 아닌 문자를 입력하면 에러 발생")
     @ParameterizedTest
     @CsvSource({"1000d", "100 0", "abcd", "1000.0"})
     public void inputMoneyNotNumber(String money) {
@@ -56,7 +59,7 @@ public class ValidatorTest {
         assertEquals(exception.getMessage(), INPUT_NOT_NUMBER.getMessage());
     }
 
-    @DisplayName("당첨 번호에 숫자가 아닌 글자를 입력하면 에러 발생")
+    @DisplayName("당첨 번호에 숫자가 아닌 문자를 입력하면 에러 발생")
     @ParameterizedTest
     @ValueSource(strings = {"1,a,3,4,5,6", "1,2,3,4,5,pobi", "1,2,3,4,5,-1"})
     public void inputNotNumberException(String winningNumber) {
@@ -66,7 +69,7 @@ public class ValidatorTest {
         assertEquals(exception.getMessage(), INPUT_NOT_NUMBER.getMessage());
     }
 
-    @DisplayName("당첨 번호에 1-45 가 아닌 숫자를 입력하면 에러 발생")
+    @DisplayName("당첨 번호에 1-45 가 아닌 문자를 입력하면 에러 발생")
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3,46,5,6", "1,2,3,4,5,0"})
     public void inputNotINRANGEException(String winningNumber) {
@@ -76,7 +79,7 @@ public class ValidatorTest {
         assertEquals(exception.getMessage(), INPUT_NOT_IN_RANGE.getMessage());
     }
 
-    @DisplayName("당첨 번호에 6개 미만을 입력하면 에러 발생")
+    @DisplayName("당첨 번호에 6개가 아닌 개수를 입력하면 에러 발생")
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3,4,5", "6,7,8,9,10,11,12"})
     public void inputOutOfSizeException(String winningNumber) {
@@ -94,5 +97,44 @@ public class ValidatorTest {
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> validator.validateWinningNumber(winningNumber));
         assertEquals(exception.getMessage(), INPUT_DUPLICATE_NUMBER.getMessage());
+    }
+
+    @DisplayName("보너스 번호에 숫자가 아닌 문자를 입력하면 에러 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"ㅇ", "1s", " "})
+    public void inputBonusNotNumberException(String bonusNumber) {
+        // given
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateBonusNumber(lotto, bonusNumber));
+        assertEquals(exception.getMessage(), INPUT_NOT_NUMBER.getMessage());
+    }
+
+    @DisplayName("보너스 번호에 숫자가 아닌 문자를 입력하면 에러 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46", "100"})
+    public void inputBonusNotInRangeException(String bonusNumber) {
+        // given
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateBonusNumber(lotto, bonusNumber));
+        assertEquals(exception.getMessage(), INPUT_NOT_IN_RANGE.getMessage());
+    }
+
+    @DisplayName("보너스 번호에 당첨 번호와 반복되는 숫자를 입력하면 에러 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "6"})
+    public void inputBonusDuplicateException(String bonusNumber) {
+        // given
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        // when & then
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateBonusNumber(lotto, bonusNumber));
+        assertEquals(exception.getMessage(), INPUT_BONUS_DUPLICATE.getMessage());
     }
 }

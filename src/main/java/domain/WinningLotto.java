781ddb7073a24a32160f66c289cf194bdbd4d7be
mango <esudatcom@gmail.com>
2023-11-06 23:02:11 +0900

    feat: 당첨번호 & 보너스볼 관리하는 WinningLotto 클래스 구현

---
+++ src/main/java/domain/WinningLotto.java
@@ -0,0 +1,30 @@
+package domain;
+
+import domain.enums.LottoRank;
+
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber){
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+        validateBonusNumber();
+    }
+
+    public void validateBonusNumber(){
+        if (winningNumbers.getNumbers().contains(bonusNumber)){
+            this new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
+        }
+    }
+
+    public LottoRank match(Lotto ticket){
+        long matchCount = ticket.getNumbers().stream()
+                .filter(winningNumbers.getNumbers()::contains)
+                .count();
+
+        boolean matchBonus = ticket.getNumbers().contains(bonusNumber);
+
+        return LottoRank.valueOf((int) matchCount, matchBonus);
+    }
+}

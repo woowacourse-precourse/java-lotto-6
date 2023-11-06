e58446b6c87de4cd9b52b14db05a6cfacebfab6b
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 13:24:42 +0900

    feat: 값에 공백이 포함되어있을 때 예외가 발생하는 기능 구현

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,12 +1,20 @@
 package lotto.validator;
 
+import static lotto.enums.ErrorMassage.CONTAIN_SPACE;
 import static lotto.enums.ErrorMassage.IS_BLANK;
 
 public class InputValidator {
+    private static final String SPACE = " ";
 
     public void validateBlank(final String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException(IS_BLANK.getMassage());
+        }
+    }
+
+    public void validateContainSpace(final String input) {
+        if (input.contains(SPACE)) {
+            throw new IllegalArgumentException(CONTAIN_SPACE.getMassage());
         }
     }
 }

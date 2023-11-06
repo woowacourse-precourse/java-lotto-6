3b5f2398cfaa5c8c2db32e4ad9f9019d30aa8def
seonghun <sdzx0719@naver.com>
2023-11-06 16:31:03 +0900

    feat(exception): 보너스 번호 입력시 1~45까지만 입력 가능 예외처리

--- src/main/java/lotto/service/Exceptions.java
+++ src/main/java/lotto/service/Exceptions.java
@@ -13,4 +13,15 @@ public class Exceptions {
             throw new IllegalArgumentException("로또 번호는 6개를 입력해주셔야 합니다.");
         }
     }
+
+    public void isInvalidNumberOfBonusNumber(String bonus){
+        try {
+            int bonusNumber = Integer.parseInt(bonus);
+            if(bonusNumber <= 1 || bonusNumber >= 45){
+                throw new IllegalArgumentException("보너스 번호의 값은 1~45까지 입력 가능합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다.");
+        }
+    }
 }

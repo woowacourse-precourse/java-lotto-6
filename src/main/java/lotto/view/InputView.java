d0bd7c0ab74e63643fc9b013c19dd937f7de0096
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-07 08:11:52 +0900

    refactor(InputView) : 구분기호 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String DELIMITER = ",";
+
     public static String readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
@@ -10,7 +12,7 @@ public class InputView {
 
     public static String readWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        return Console.readLine().replaceAll(DELIMITER," ");
     }
 
     public static String readBonusNumber() {

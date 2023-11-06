36d64777d36a96c557158115ee561c5fc7d365f1
노정완 <jeongwan.noh@gmail.com>
2023-11-06 09:54:46 +0900

    feat: 예외 처리 문구 enum 클래스 생성

--- src/main/java/lotto/domain/validation/ExceptionMessage.java
+++ src/main/java/lotto/domain/validation/ExceptionMessage.java
@@ -1,4 +1,16 @@
 package lotto.domain.validation;
 
 public enum ExceptionMessage {
+    EXCEPTION_MESSAGE_PREFIX("[ERROR]"),
+    NUMBER_FORMAT_EXCEPTION("1000원 단위의 숫자를 입력해주세요.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

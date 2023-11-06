0cfe1d47773fae9adcb98d473ee4e2da1ad2a7ed
1Bronze <khdkkhdd@gmail.com>
2023-11-06 23:15:48 +0900

    feat(lotto): add validation in lotto vendor

--- src/main/java/lotto/error/message/InvalidStateErrorMessage.java
+++ src/main/java/lotto/error/message/InvalidStateErrorMessage.java
@@ -3,7 +3,9 @@ package lotto.error.message;
 public enum InvalidStateErrorMessage implements ErrorMessage {
     LOTTO_NUMBERS_COUNT_NOT_SIX("로또 번호는 6개여야 합니다."),
     LOTTO_NUMBERS_DUPLICATE("6개의 로또 번호는 중복될 수 없습니다."),
-    LOTTO_NUMBERS_OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    LOTTO_NUMBERS_OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    PURCHASE_AMOUNT_UNDER_THOUSAND("구입 금액은 1000원 이상이어야 합니다."),
+    PURCHASE_AMOUNT_NOT_MULTIPLE_OF_THOUSAND("구입 금액은 1000의 배수만 가능합니다.");
 
     private final String message;
 
--- src/main/java/lotto/lotto/LottoVendor.java
+++ src/main/java/lotto/lotto/LottoVendor.java
@@ -1,11 +1,17 @@
 package lotto.lotto;
 
+import static lotto.error.message.InvalidStateErrorMessage.PURCHASE_AMOUNT_NOT_MULTIPLE_OF_THOUSAND;
+import static lotto.error.message.InvalidStateErrorMessage.PURCHASE_AMOUNT_UNDER_THOUSAND;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.error.exception.InvalidStateException;
 
 public class LottoVendor {
     public List<Lotto> generateLottos(Integer purchaseAmount) {
+        validate(purchaseAmount);
+
         List<Lotto> lottos = new ArrayList<>();
         Integer remainingLottos = purchaseAmount / 1000;
 
@@ -19,5 +25,14 @@ public class LottoVendor {
     private Lotto generateLotto() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(lottoNumbers);
+    }
+
+    private void validate(Integer purchaseAmount) {
+        if (purchaseAmount < 1000) {
+            throw new InvalidStateException(PURCHASE_AMOUNT_UNDER_THOUSAND.getMessage(), purchaseAmount);
+        }
+        if (purchaseAmount % 1000 != 0) {
+            throw new InvalidStateException(PURCHASE_AMOUNT_NOT_MULTIPLE_OF_THOUSAND.getMessage(), purchaseAmount);
+        }
     }
 }

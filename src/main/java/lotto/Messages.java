7d61e927b2a114ccdcab1b5a86559dbdb384f450
i3kae <snoger1234@gmail.com>
2023-11-06 14:30:26 +0900

    feat : 1000으로 나눠떨어지지 않을 때 에러메시지 추가

--- src/main/java/lotto/Messages.java
+++ src/main/java/lotto/Messages.java
@@ -3,7 +3,8 @@ package lotto;
 public class Messages {
     public enum ErrorMessage{
         NONNUMERICCHAR("[ERROR] 숫자가 아닌 문자가 입력되었습니다."),
-        NULLSTRING("[ERROR] 비어있는 문자열이 입력되었습니다.");
+        NULLSTRING("[ERROR] 비어있는 문자열이 입력되었습니다."),
+        NOTDIVIED1000("[ERROR] 1000으로 나누어 떨어지지 않습니다.");
 
         private String errorMessage;
         private ErrorMessage(String message){

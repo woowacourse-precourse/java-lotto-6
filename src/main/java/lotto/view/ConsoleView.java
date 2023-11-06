2e7ac24cb593192d34600eab41c88505d493c90c
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-11-06 20:02:58 +0900

    feat: OutputView  - 출력하는 역할을 가진 뷰 객체

--- src/main/java/lotto/view/ConsoleView.java
+++ src/main/java/lotto/view/ConsoleView.java
@@ -2,14 +2,14 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public sealed class ConsoleView permits InputView {
+public sealed class ConsoleView permits InputView, OutputView {
 
     protected void print(final String message) {
         System.out.print(message);
     }
 
-    protected void println(final String meesage) {
-        System.out.println(meesage);
+    protected void println(final String message) {
+        System.out.println(message);
     }
 
     protected void lineBreak() {
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,43 @@
+package lotto.view;
+
+import lotto.constant.Rank;
+import lotto.domain.Lotto;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public final class OutputView extends ConsoleView {
+
+    private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String RESULT_HEADER = "당첨 통계";
+    private static final String DIVISION_LINE = "---";
+    private static final String RESULT_FORMAT_WITH_BONUS = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String RESULT_FORMAT = "%d개 일치 (%s원) - %d개";
+    private static final String ROI_FORMAT = "총 수익률은 %.1f%%입니다.";
+    private static final String PRIZE_WITH_COMMA = "%,d";
+
+    public void displayLottos(List<Lotto> lottos) {
+        println(String.format(LOTTO_COUNT_MESSAGE, lottos.size()));
+        lottos.forEach(lotto -> println(lotto.numbers().toString()));
+    }
+
+    public void displayResults(List<Rank> ranks, double roi) {
+        println(RESULT_HEADER);
+        println(DIVISION_LINE);
+
+        Rank[] reversedRanks = Rank.values();
+        Arrays.sort(reversedRanks, Collections.reverseOrder());
+
+        for (Rank rank : reversedRanks) {
+            if (rank != Rank.NONE) {
+                int count = (int) ranks.stream()
+                    .filter(rank::equals)
+                    .count();
+                String format = rank == Rank.SECOND ? RESULT_FORMAT_WITH_BONUS : RESULT_FORMAT;
+                println(String.format(format, rank.getMatchCount(), String.format(PRIZE_WITH_COMMA, rank.getPrize()), count));
+            }
+        }
+        println(String.format(ROI_FORMAT, roi));
+    }
+}

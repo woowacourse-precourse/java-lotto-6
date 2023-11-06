8a1e92983517b7d050837ec80c393df203343236
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 01:21:38 +0900

    test: add check lotto count to user lotto test

--- src/test/java/lotto/UserLottoTest.java
+++ src/test/java/lotto/UserLottoTest.java
@@ -23,4 +23,18 @@ public class UserLottoTest {
         assertThatThrownBy(() -> new UserLotto(900))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("구입 금액에 따라 적절한 개수 구매")
+    @Test
+    void checkLottoCount() {
+        //given
+        int money = 3000;
+        int lottoCount = 3;
+
+        //when
+        UserLotto userLotto = new UserLotto(3000);
+
+        //then
+        Assertions.assertThat(userLotto.getLottoCount()).isEqualTo(lottoCount);
+    }
 }

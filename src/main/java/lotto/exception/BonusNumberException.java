3976746af410abbf102eca49b332c920a8ad373c
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 03:25:13 +0900

    feat(BonusNumberException) : 보너스 번호 예외처리 코드 작성 - isNumber() - isNumberOnlyOne() - isRightRange() - isBlank() - isDuplicateWithWinnerNum()

---
+++ src/main/java/lotto/exception/BonusNumberException.java
@@ -0,0 +1,21 @@
+package lotto.exception;
+
+import java.util.List;
+
+public class BonusNumberException {
+
+    public void isNumber(String input) {
+    }
+
+    public void isRightRange(String input) {
+    }
+
+    public void isBlank(String input) {
+    }
+
+    public void isDuplicateWithWinnerNum(int bonusNum, List<Integer> winnerNum) {
+    }
+
+    public void isNumberOnlyOne(String input) {
+    }
+}

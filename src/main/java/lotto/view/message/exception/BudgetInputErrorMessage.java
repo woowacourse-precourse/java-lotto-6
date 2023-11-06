69be7a19f172ed806dc9a58af52ab921131c08a5
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-06 14:10:19 +0900

    refactor: (ErrorMessage) 예외 문구 추가 및 수정

--- src/main/java/lotto/view/message/exception/BudgetInputErrorMessage.java
+++ src/main/java/lotto/view/message/exception/BudgetInputErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.view.message.exception;
 
 public enum BudgetInputErrorMessage {
     LESS_THAN_1000("[ERROR] 1000원 이상부터 구매가 가능합니다."),
-    NOT_NUMBER("[ERROR] 금액 필드에는 숫자만 입력 가능합니다.");
+    NOT_NUMBER("[ERROR] 금액은 숫자만 입력 가능합니다."),
+    DIVISIBLE_BY_THOUSAND("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
 
     private final String message;
     BudgetInputErrorMessage(String message) {

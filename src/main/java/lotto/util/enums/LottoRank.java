a1dc3f61950fa6bff640faef5efccaadacdfa420
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-06 19:15:35 +0900

    feat(LottoRank): 당첨 등수에 대한 enum 생성

---
+++ src/main/java/lotto/util/enums/LottoRank.java
@@ -0,0 +1,19 @@
+package lotto.util.enums;
+
+public enum LottoRank {
+    FIRST("6개 일치 (2,000,000,000원)"),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD("5개 일치 (1,500,000원)"),
+    FOURTH("4개 일치 (50,000원)"),
+    FIFTH("3개 일치 (5,000원)");
+
+    private final String content;
+
+    LottoRank(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

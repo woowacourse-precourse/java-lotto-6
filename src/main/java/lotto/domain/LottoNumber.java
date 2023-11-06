959ef374e1b8cb35485e61839dd6f730d788f71b
jae woo Jung <awfjol2008@naver.com>
2023-11-06 16:58:36 +0900

    feat: 로또 숫자 검증 기능 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -5,6 +5,17 @@ public class LottoNumber {
     private Integer lottoNumber;
 
     public LottoNumber(Integer lottoNumber) {
+        validateLottoNumber(lottoNumber);
         this.lottoNumber = lottoNumber;
+    }
+
+    private void validateLottoNumber(Integer lottoNumber) {
+        validateRange(lottoNumber);
+    }
+
+    private void validateRange(Integer lottoNumber) {
+        if (lottoNumber < 1 || lottoNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 각각의 당첨 번호는 1~45 범위의 정수이어야 합니다.");
+        }
     }
 }

95bd7dad5aef5f52c6f6ab7d9ccada733fd3f7ad
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-06 21:20:19 +0900

    style: import문 수정

--- src/main/java/lotto/util/LottoValidation.java
+++ src/main/java/lotto/util/LottoValidation.java
@@ -7,20 +7,28 @@ import java.util.Set;
 import static lotto.util.Constants.MIN_RANGE;
 import static lotto.util.Constants.MAX_RANGE;
 import static lotto.util.Constants.LOTTO_MAX_SIZE;
+import static lotto.util.exception.ErrorMessage.INVALID_RANGE;
+import static lotto.util.exception.ErrorMessage.INVALID_LOTTO_COUNT;
+import static lotto.util.exception.ErrorMessage.INVALID_DUPLICATE_NUMBER;
+
 public class LottoValidation {
     private LottoValidation(){}
 
-    public static boolean isPermitRange(final String number){
-        return Integer.parseInt(number) >= MIN_RANGE && Integer.parseInt(number) <= MAX_RANGE;
+    public static void isPermitRange(final int number,final String type){
+        if(number < MIN_RANGE || number > MAX_RANGE)
+            throw new IllegalArgumentException(String.format(INVALID_RANGE.getErrorMessage(),type));
     }
-    public static boolean lottoCountIsSix(final List<Integer> lotto){
-        return lotto.size() == LOTTO_MAX_SIZE;
+    public static void lottoCountIsSix(final List<Integer> lotto){
+        if(!(lotto.size() == LOTTO_MAX_SIZE))
+            throw new IllegalArgumentException(INVALID_LOTTO_COUNT.getErrorMessage());
     }
-    public static boolean duplicateLottoNumber(final List<Integer> lotto){
+    public static void duplicateLottoNumber(final List<Integer> lotto){
         Set<Integer> lottoCount = new HashSet<>(lotto);
-        return lotto.size() == lottoCount.size();
+        if(lottoCount.size() != LOTTO_MAX_SIZE)
+            throw new IllegalArgumentException(INVALID_DUPLICATE_NUMBER.getErrorMessage());
     }
-    public boolean duplicationLottoAndBonusNumber(final List<Integer> lotto, final String bonusNumber){
-        return lotto.contains(Integer.parseInt(bonusNumber));
+    public static void duplicationLottoAndBonusNumber(final List<Integer> lotto, final String bonusNumber){
+        if(lotto.contains(Integer.parseInt(bonusNumber)))
+            throw new IllegalArgumentException(INVALID_DUPLICATE_NUMBER.getErrorMessage());
     }
 }

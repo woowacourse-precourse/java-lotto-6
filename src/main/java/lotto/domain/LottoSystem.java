8de0f411a365bdd0b350d6e561008dfbafc3d699
jae woo Jung <awfjol2008@naver.com>
2023-11-06 20:34:02 +0900

    feat: LottoSystem 클래스 멤버 변수 추가

---
+++ src/main/java/lotto/domain/LottoSystem.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.player.Player;
+
+import java.util.List;
+
+public class LottoSystem {
+
+    private List<LottoNumber> winningLottoNumbers;
+    private Player player;
+}

5f7a68c45ca834c6821840d91bb18b94db131c3e
pumpk <cong2738@gmail.com>
2023-11-06 15:08:54 +0900

    refactor: application 클래스 나누기에 의한 테스트 삭제

--- src/test/java/lotto/myTest.java
+++ src/test/java/lotto/myTest.java
@@ -5,30 +5,14 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class myTest extends NsTest {
 
-    @DisplayName("문자열 정수 변환 매소드 테스트")
-    @Test
-    void myParseIntTest() {
-        // given
-        final Application myApp = new Application();
-        final String price = "1000";
-    
-        // when
-        final int money = myApp.myParseInt(price);
-    
-        // then
-        assertThat(money).isEqualTo(1000);
-    }
-    
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 }

49b39bf29ca775e47fe55ba9eae2823ef4d0f209
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 15:18:55 +0900

    feat(ErrorMessage,LottoConstance) : 요금에 관련된 상수 및 에러메세지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -6,7 +6,10 @@ public enum ErrorMessage {
     LOTTO_WRONG_SIZE_ERROR_MESSAGE(ERROR.get() + "로또 번호는 6개의 숫자입니다."),
     LOTTO_HAS_DUPLICATION_ERROR_MESSAGE(ERROR.get() + "로또 번호에 중복이 있으면 안됩니다."),
     BONUS_DUPLICATION_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 로또 번호와 중복되면 안됩니다."),
-    BONUS_WRONG_RANGE_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 1~45사의 숫자입니다.");
+    BONUS_WRONG_RANGE_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 1~45사의 숫자입니다."),
+    PAYMENT_NOT_DIVISIBLE_BY_1000(ERROR.get() + "잔돈 없어요. 알맞게 내주세요."),
+    PAYMENT_LESS_THAN_1000(ERROR.get() + "로또는 장당 천원 입니다."),
+    PAYMENT_MORE_THAN_100000(ERROR.get() + "로또는 한 번에 최대 10만원까지만 구매 가능합니다.");
 
     private final String message;
 
--- src/main/java/lotto/LottoConstance.java
+++ src/main/java/lotto/LottoConstance.java
@@ -3,14 +3,17 @@ package lotto;
 public enum LottoConstance {
     MIN_NUMBER_LOTTO_RANGE(1),
     MAX_NUMBER_LOTTO_RANGE(45),
-    LOTTO_SIZE(6);
+    LOTTO_SIZE(6),
+    MIN_PAYMENT(1000),
+    MAX_PAYMENT(100000);
 
     private final int number;
-    LottoConstance(int number){
+
+    LottoConstance(int number) {
         this.number = number;
     }
 
-    public int get(){
+    public int get() {
         return number;
     }
 

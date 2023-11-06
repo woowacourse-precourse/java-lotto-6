5cad6e0e4d9559289e8abda32183a49ae4e30392
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-06 16:02:59 +0900

    refactor: 필드의 상수 제거 후, enum의 Constants 사용

--- src/main/java/lotto/utils/GeneratedLottoNumber.java
+++ src/main/java/lotto/utils/GeneratedLottoNumber.java
@@ -1,13 +1,12 @@
 package lotto.utils;
 
+import static lotto.enums.Constants.*;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GeneratedLottoNumber {
-    private static final int START_INCLUSIVE = 1;
-    private static final int END_INCLUSIVE = 45;
-    private static final int NUMBER_TO_GENERATE = 6;
 
     public static String getRandomNumber(){
-        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE,END_INCLUSIVE,NUMBER_TO_GENERATE).toString().replaceAll("[\\['\\]]","");
+        return Randoms.pickUniqueNumbersInRange(MIN_INCLUSIVE.getValue(), MAX_INCLUSIVE.getValue(), LOTTO_NUMBER_SIZE.getValue()).toString().replaceAll("[\\['\\]]","");
     }
 }

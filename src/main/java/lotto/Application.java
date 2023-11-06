c50bcbef1a3f5e3ca1a9e59523a9a36078daf9ea
choji <waf268@gmail.com>
2023-11-06 10:42:07 +0900

    docs : README의 기능구현 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
     }
 }

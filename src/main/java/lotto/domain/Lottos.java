e1b069c408bfe8c6611c09729281bbe6d945be7b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-07 05:45:14 +0900

    feat : Lottos toString 재정의, size가져오기

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,23 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    @Override
+    public String toString() {
+        return lottos.stream()
+                .map(lotto -> lotto.toString() + "\n")
+                .collect(Collectors.joining());
+    }
+
+    public int getSize() {
+        return lottos.size();
+    }
 }

0db9b61365b0f30d3140f9c39668f2342c94adf1
olsohee <dlthgml0108@daum.net>
2023-11-06 20:14:53 +0900

    style(PurchasePrice): 공백 추가

--- src/main/java/lotto/domain/lotto/userLotto/PurchasePrice.java
+++ src/main/java/lotto/domain/lotto/userLotto/PurchasePrice.java
@@ -4,7 +4,7 @@ import static lotto.domain.lotto.LottoCondition.PRICE;
 
 public class PurchasePrice {
 
-    private static final String PURCHASE_PRICE_ERROR_MESSAGE = "구입 금액은 " + PRICE.getValue()+ "원 단위로 입력해야 합니다.";
+    private static final String PURCHASE_PRICE_ERROR_MESSAGE = "구입 금액은 " + PRICE.getValue() + "원 단위로 입력해야 합니다.";
     private final int purchasePrice;
 
     public PurchasePrice(int purchasePrice) {

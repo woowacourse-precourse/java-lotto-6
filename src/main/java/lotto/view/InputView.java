13b81f9eae8efb7fcf2f4d6ee493cd0b49f86240
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-06 20:51:11 +0900

    refactor(InputView): 중복되는 예외처리 제거 및, 변수명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,19 +3,18 @@ package lotto.view;
 import java.util.Arrays;
 import java.util.List;
 
-import lotto.domain.Lotto;
 import lotto.io.InputViewReader;
 import lotto.util.message.ErrorMessage;
 import lotto.util.validator.Validator;
 
 public class InputView {
-	
+
 	private static final String DELIMITER = ",";
 
 	private final InputViewReader reader;
-	
+
 	public InputView() {
-		reader =  new InputViewReader();
+		reader = new InputViewReader();
 	}
 
 	public int inputMoney() {
@@ -30,17 +29,15 @@ public class InputView {
 		String inputValue = reader.readLine();
 		String[] inputValues = inputValue.split(DELIMITER);
 
-		List<Integer> numbers = convertInputValuesToCollection(inputValues);
-
-		validateInputAnswerLottoNumbers(numbers);
+		validateAnswerLottoNumbers(inputValues);
 
 		return convertStrArrToCollection(inputValues);
 	}
 
-	public int inputBonusNumber(List<Integer> numbers) {
+	public int inputBonusNumber() {
 		String inputValue = reader.readLine();
 
-		validateInputBonusNumber(numbers, inputValue);
+		validateInputBonusNumber(inputValue);
 
 		return Integer.parseInt(inputValue);
 	}
@@ -49,52 +46,18 @@ public class InputView {
 		validateisNumberFormat(inputValue);
 	}
 
-	private List<Integer> convertInputValuesToCollection(String[] inputValues) {
-		Validator.validateSeparatedTextLength(inputValues, Lotto.LOTTO_SIZE,
-				ErrorMessage.INPUT_ANSWER_LOTTO_NUMBER_ERROR.getFormattedMessage(Lotto.LOTTO_SIZE));
-
+	private void validateAnswerLottoNumbers(String[] inputValues) {
 		for (String inputValue : inputValues) {
 			validateisNumberFormat(inputValue);
 		}
-
-		return convertStrArrToCollection(inputValues);
 	}
 
 	private List<Integer> convertStrArrToCollection(String[] strArr) {
-		return Arrays.stream(strArr).map(Integer::valueOf)
-				.toList();
+		return Arrays.stream(strArr).map(Integer::valueOf).toList();
 	}
 
-	private void validateInputAnswerLottoNumbers(List<Integer> numbers) {
-		int[] numberRange = { Lotto.MIN_NUMBER, Lotto.MAX_NUMBER };
-
-		validateAnswerLottoNumbers(numbers, numberRange);
-	}
-
-	private void validateAnswerLottoNumbers(List<Integer> numbers, int[] numberRange) {
-		Validator.validateNumbersSize(numbers, Lotto.LOTTO_SIZE,
-				ErrorMessage.LOTTO_SIZE_ERROR.getFormattedMessage(Lotto.LOTTO_SIZE));
-		Validator.validateDuplicateNumber(numbers, ErrorMessage.LOTTO_NUMBER_DUPLICATE_ERROR.getMessage());
-		Validator.validateNumbersOutOfRange(numbers, numberRange,
-				ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getFormattedMessage(numberRange[0], numberRange[1]));
-	}
-
-	private void validateInputBonusNumber(List<Integer> numbers, String inputValue) {
-		int[] numberRange = { Lotto.MIN_NUMBER, Lotto.MAX_NUMBER };
-		Integer number;
-
+	private void validateInputBonusNumber(String inputValue) {
 		validateisNumberFormat(inputValue);
-
-		number = Integer.parseInt(inputValue);
-
-		validateBonusNumber(numbers, number, numberRange);
-	}
-
-	private void validateBonusNumber(List<Integer> numbers, Integer number, int[] numberRange) {
-		Validator.validateNumberOutOfRange(number, numberRange,
-				ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getFormattedMessage(numberRange[0], numberRange[1]));
-		Validator.validateNumberInNumbers(numbers, number,
-				ErrorMessage.LOTTO_BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
 	}
 
 	private void validateisNumberFormat(String inputValue) {

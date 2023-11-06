25342f64f2d74a08453b0d8fe6747f7e133db284
KimSehoon <shggm2000@gmail.com>
2023-11-07 03:41:04 +0900

    feat: ascendingNumbers method 구현
    - numbers를 오름차순 정렬하는 ascendingNumbers method 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,7 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = ascendingNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +17,9 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private List<Integer> ascendingNumbers(List<Integer> numbers){
+        Collections.sort(numbers);
+        return numbers;
+    }
+
 }

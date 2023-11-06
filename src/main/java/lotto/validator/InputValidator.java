aee5d9e64a1d71ca6556b99d3a32ff66f3c44c24
bamm2 <xxknu88@naver.com>
2023-11-06 18:15:09 +0900

    feat(InputValidator): 1000원 단위로 나누어 떨어지는 금액인지 판단하는 기능

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import static lotto.exception.ExceptionMessage.DIFFERENT_FORMAT_EXCEPTION_MESSAGE;
 import static lotto.exception.ExceptionMessage.NOT_NUMBER_EXCEPTION_MESSAGE;
 import static lotto.exception.ExceptionMessage.NULL_POINTER_EXCEPTION_MESSAGE;
 
@@ -7,16 +8,34 @@ import lotto.exception.LottoApplicationException;
 
 public class InputValidator {
 
-    public void validateNotNull(String input){
-        if(input == null || input.isEmpty() ){
+    public InputValidator() {
+    }
+
+    public void validateNotNull(String input) {
+        if (input==null || input.isEmpty()) {
             throw new LottoApplicationException(NULL_POINTER_EXCEPTION_MESSAGE);
         }
     }
 
-    public void validateNumber(String input){
-        if(!input.chars().allMatch(Character::isDigit)){
+    public void validateNumber(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
             throw new LottoApplicationException(NOT_NUMBER_EXCEPTION_MESSAGE);
         }
+    }
+
+    public void validatePurchasePrice(String input) {
+        if (!availDividePrice(input)) {
+            throw new LottoApplicationException(DIFFERENT_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean availDividePrice(String input) {
+        Integer price = convertPrice(input);
+        return price >= 1000 && price % 1000==0;
+    }
+
+    private Integer convertPrice(String input) {
+        return Integer.parseInt(input);
     }
 
 }

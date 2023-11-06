8150ae60e766f5cba30fefe51c81594d68b122e6
moong2 <www.castlehi@gmail.com>
2023-11-07 00:36:02 +0900

    refactor: make lotto details to enum

--- src/main/java/lotto/util/LottoDetails.java
+++ src/main/java/lotto/util/LottoDetails.java
@@ -1,2 +1,18 @@
-package lotto.util;public enum LottoDetails {
+package lotto.util;
+
+public enum LottoDetails {
+    COUNT_OF_LOTTO_NUMBER(6),
+
+    MINIMUM_OF_LOTTO_NUMBER(1),
+    MAXIMUM_OF_LOTTO_NUMBER(45);
+
+    private final Integer details;
+
+    LottoDetails(Integer details) {
+        this.details = details;
+    }
+
+    public Integer getDetails() {
+        return details;
+    }
 }

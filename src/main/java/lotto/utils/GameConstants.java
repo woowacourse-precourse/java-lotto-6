ac8b73f86a28db13d0b5ce116a2579aa516038bf
Genius-PAK <pyj789123@gmail.com>
2023-11-06 18:43:13 +0900

    chore(GameConstants) : 순위 별 금액을 나타내는 상수 추가

--- src/main/java/lotto/utils/GameConstants.java
+++ src/main/java/lotto/utils/GameConstants.java
@@ -1,10 +1,18 @@
-package lotto;
+package lotto.utils;
 
 public class GameConstants {
     public static final int UNIT_PURCHASE_PRICE = 1000;
-    public static final int VALID_LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_NUMBER_COUNT = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
 
     public static final String SEPARATOR = ",";
+
+    public static final int FIRST_PRIZE_AMOUNT = 2000000000;
+    public static final int SECOND_PRIZE_AMOUNT = 30000000;
+    public static final int THIRD_PRIZE_AMOUNT = 1500000;
+    public static final int FOURTH_PRIZE_AMOUNT = 50000;
+    public static final int FIFTH_PRIZE_AMOUNT = 5000;
+
+
 }

0d771ed1b0e296e1ac4de6712eee5c04f18eea27
kimminsu <xjfcnfw3@naver.com>
2023-11-06 21:47:05 +0900

    fix: 랭킹에서 잘못된 결과가 나타나는 버그 수정

--- src/main/java/lotto/ranking/Ranking.java
+++ src/main/java/lotto/ranking/Ranking.java
@@ -3,11 +3,12 @@ package lotto.ranking;
 import java.util.Arrays;
 
 public enum Ranking {
-    FIRST(6, 2_000_000_000, false),
-    SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000, false),
-    FOURTH(4, 50_000, false),
+
     FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false),
     NOTHING(0, 0, false);
 
     private final int matchCount;
@@ -28,13 +29,25 @@ public enum Ranking {
     }
 
     private boolean isMatchRankingCondition(int matchCount, boolean isBonusMatch) {
-        if (this.equals(Ranking.SECOND)) {
-            return this.matchCount == matchCount && isBonusMatch;
+        if (isDuplicatedMatchCountCondition(matchCount)) {
+            return this.isBonusMatch == isBonusMatch && this.matchCount == matchCount;
         }
         return this.matchCount == matchCount;
     }
 
+    private boolean isDuplicatedMatchCountCondition(int matchCount) {
+        return Ranking.SECOND.matchCount == matchCount;
+    }
+
     public Integer calculatePrize(int rankCount) {
         return rankCount * price;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }

c7b5a25d2685086acb2ce3fb20d4c086a4429146
seokmyungham <hellotjraud@gmail.com>
2023-11-07 01:33:31 +0900

    feat(Rank): Enum 클래스 생성 및 getRank 메서드 추가
    getRank: 로또 당첨 번호 개수와 보너스 숫자 당첨 여부를 매개 변수로 받아 순위를 반환합니다

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
+
+    private final int count;
+    private final int winningAmount;
+
+    Rank(int count, int winningAmount) {
+        this.count = count;
+        this.winningAmount = winningAmount;
+    }
+
+    public static Rank getRank(int count, boolean hasBonusNumber) {
+        Rank rank = Arrays.stream(values())
+                .filter(r -> r.count == count)
+                .findFirst()
+                .orElse(null);
+
+        if (rank == SECOND && !hasBonusNumber) {
+            rank = THIRD;
+        }
+
+        return rank;
+    }
+}

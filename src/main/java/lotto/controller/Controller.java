81f1fdc9df7ecf59b0c4033153f20c5f10fe9397
ay-eonii <naayen@naver.com>
2023-11-06 23:03:04 +0900

    feat(controller): 구매 개수 출력

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -4,12 +4,20 @@ import lotto.domain.Buyer;
 import lotto.view.View;
 import lotto.view.constant.Message;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.view.constant.Message.OUTPUT_BUY;
+
 public class Controller {
 
     private final static View view = new View();
     public void start() {
         view.output(Message.INPUT_COST);
         Buyer buyer = getCost();
+
+        int count = buyer.getBuyCount();
+        view.output(String.format(OUTPUT_BUY, count));
     }
 
     private Buyer getCost() {
--- src/main/java/lotto/view/constant/Message.java
+++ src/main/java/lotto/view/constant/Message.java
@@ -2,4 +2,5 @@ package lotto.view.constant;
 
 public class Message {
     public final static String INPUT_COST = "구입금액을 입력해 주세요.";
+    public final static String OUTPUT_BUY = "\n%d개를 구매했습니다.";
 }

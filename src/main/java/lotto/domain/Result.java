5c82303cd929e86503c8e8dd08c248d6c5286154
kssumin <201566@jnu.ac.kr>
2023-11-06 20:15:26 +0900

    refactor(Result) : 컨벤션에 따라 생성자를 메서드보다 생성자를 위로 위치 변경

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -4,16 +4,16 @@ public class Result {
     private final int matchCount;
     private final boolean matchBonus;
 
+    public Result(int matchCount, boolean matchBonus) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+    }
+
     public int getMatchCount() {
         return matchCount;
     }
 
     public boolean isMatchBonus() {
         return matchBonus;
-    }
-
-    public Result(int matchCount, boolean matchBonus) {
-        this.matchCount = matchCount;
-        this.matchBonus = matchBonus;
     }
 }

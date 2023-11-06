d7e3425635e46c33d24306d62f702c4642fe9612
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 11:03:40 +0900

    style: given, when, then 주석 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -22,12 +22,16 @@ class LottoNumberTest {
     @ParameterizedTest
     @MethodSource("provideLottoNumbers")
     void 숫자가_LOTTO_MIN_NUMBER과_LOTTO_MAX_NUMBER사이일_경우_객체를_올바르게_생성한다(int number) {
+        // when & then
         assertDoesNotThrow(() -> LottoNumber.from(number));
     }
 
     @Test
     void 숫자가_LOTTO_MIN_NUMBER보다_작을_경우_예외가_발생한다() {
+        // given
         int number = MIN_LOTTO_NUMBER.getValue() - 1;
+
+        // when & then
         assertThatThrownBy(() -> LottoNumber.from(number))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage(OUT_OF_RANGE_LOTTO_NUMBER.getMassage());
@@ -35,7 +39,10 @@ class LottoNumberTest {
 
     @Test
     void 숫자가_LOTTO_MAX_NUMBER보다_클_경우_예외가_발생한다() {
+        // given
         int number = MAX_LOTTO_NUMBER.getValue() + 1;
+
+        // when & then
         assertThatThrownBy(() -> LottoNumber.from(number))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage(OUT_OF_RANGE_LOTTO_NUMBER.getMassage());

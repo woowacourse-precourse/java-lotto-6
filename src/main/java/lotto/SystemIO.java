ecddaf5517b8d74fba4bda31f146b922ebd3e079
AruJoy <wkrjsdl5632@gmail.com>
2023-11-06 15:26:39 +0900

    Feat: 당첨번호/ 보너스번호 요구 메세지 & 입력 구현

--- src/main/java/lotto/SystemIO.java
+++ src/main/java/lotto/SystemIO.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class SystemIO {
     public static void requestPurchaseAmount() {
@@ -12,16 +16,25 @@ public class SystemIO {
     }
 
     public static List<Integer> readWinningNumbers() {
-        List<Integer> winningNumbers = null;
+        List<Integer> winningNumbers = new ArrayList<>();
+        List<String> stringWinningNumbers = Arrays.asList(readLine().split(","));
+        for (String stringWinningNumber : stringWinningNumbers) {
+            try {
+                winningNumbers.add(Integer.parseInt(stringWinningNumber));
+            } catch (NumberFormatException e) {
+                throw new IllegalStateException();
+            }
+        }
         return winningNumbers;
     }
 
-    public static void showTickets(Map<Long,List<Integer>> tickets) {
+    public static void showTickets(Map<Long, List<Integer>> tickets) {
         System.out.println(tickets.size() + "개를 구매했습니다.");
         for (List<Integer> ticket : tickets.values()) {
             System.out.println(ticket);
         }
     }
+
     public static void showResult() {
     }
 }

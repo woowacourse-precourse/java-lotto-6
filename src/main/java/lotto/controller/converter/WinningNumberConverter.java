2223ff36992faf5fa07c3c8beecba5257e6a7602
Genius-PAK <pyj789123@gmail.com>
2023-11-06 20:27:08 +0900

    chore(WinningNumberConverter): 숫자 번호에 순서를 가질 필요가 없으므로, 반환값을 Map에서 Set으로 변경

--- src/main/java/lotto/controller/converter/WinningNumberConverter.java
+++ src/main/java/lotto/controller/converter/WinningNumberConverter.java
@@ -1,23 +1,19 @@
 package lotto.controller.converter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import lotto.utils.GameConstants;
 
-public class WinningNumberConverter implements Converter<Map<Integer, Integer>> {
+public class WinningNumberConverter implements Converter<Set<Integer>> {
     @Override
-    public Map<Integer, Integer> convert(String WinningNumbers) {
+    public Set<Integer> convert(String WinningNumbers) {
         String[] splitWinningNumbers = WinningNumbers.split(GameConstants.SEPARATOR);
-
-        List<Integer> numericWinningNumbers = getNumericWinningNumbers(splitWinningNumbers);
-
-        return getSequencedWinningNumbers(numericWinningNumbers);
+        
+        return getNumericWinningNumbers(splitWinningNumbers);
     }
 
-    private List<Integer> getNumericWinningNumbers(String[] splitWinningNumbers) {
-        List<Integer> numericWinningNumbers = new ArrayList<>();
+    private Set<Integer> getNumericWinningNumbers(String[] splitWinningNumbers) {
+        Set<Integer> numericWinningNumbers = new HashSet<>();
 
         for (String splitWinningNumber : splitWinningNumbers) {
             numericWinningNumbers.add(Integer.parseInt(splitWinningNumber));
@@ -26,14 +22,4 @@ public class WinningNumberConverter implements Converter<Map<Integer, Integer>> 
         return numericWinningNumbers;
     }
 
-    private Map<Integer, Integer> getSequencedWinningNumbers(List<Integer> orderedSplitWinningNumbers) {
-        int sequenceNumber = 0;
-        Map<Integer, Integer> sequencedWinningNumbers = new HashMap<>();
-
-        for (int numericWinningNumber : orderedSplitWinningNumbers) {
-            sequencedWinningNumbers.put(numericWinningNumber, sequenceNumber++);
-        }
-
-        return sequencedWinningNumbers;
-    }
 }

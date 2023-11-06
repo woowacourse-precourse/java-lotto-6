8255328d3ddede068990ddee8f8c761653e57e1b
KimSehoon <shggm2000@gmail.com>
2023-11-07 02:17:26 +0900

    feat: printPurchaseAmountMessage method 구현
    - 구입 금액 문구를 출력하는 printPurchaseAmountMessage method 구현

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -7,4 +7,8 @@ public class Output {
         System.out.println(WINNING_STATISTICS.message());
         System.out.println(SEPARATOR.message());
     }
+
+    public static void printPurchaseAmountMessage(){
+        System.out.println(PURCHASE_AMOUNT.message());
+    }
 }

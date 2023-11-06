98e394d7c988f5edff805c660e646707f523e406
SeungGwa123 <skwan123@naver.com>
2023-11-07 04:17:50 +0900

    Feat: Add Winning
    Winning enum 추가 작성
    각 등수에 맞는 출력값 및 금액 미리 작성

--- src/main/java/Money/Winning.java
+++ src/main/java/Money/Winning.java
@@ -1,2 +1,24 @@
-package Money;public enum Winning {
+package Money;
+
+public enum Winning {
+    one(2000000000, "6개 일치 (2,000,000,000원) - "),
+    two(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    three(1500000, "5개 일치 (1,500,000원) - "),
+    four(50000, "4개 일치 (50,000원) - "),
+    five(5000, "3개 일치 (5,000원) - ");
+    private final int rank;
+    private final String message;
+
+    Winning(int rank, String message) {
+        this.rank = rank;
+        this.message = message;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

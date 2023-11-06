75bf7c0abcc542302d3eac391d4f941263701c18
dukcode <duk9741@gmail.com>
2023-11-06 14:57:19 +0900

    test: 로또 생성 시 오름차순 정렬 테스트 추가

---
+++ src/test/java/lotto/RandomLottoGeneratorTest.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class RandomLottoGeneratorTest {
+
+    private RandomLottoGenerator lottoGenerator = new RandomLottoGenerator();
+
+    @Test
+    public void 로또_번호는_오름차순으로_정렬된다() throws Exception {
+        // given
+        Lotto lotto = lottoGenerator.generate();
+
+        // when
+        List<Integer> numbers = lotto.getNumbers();
+
+        //then
+        for (int i = 1; i < numbers.size(); ++i) {
+            assertThat(numbers.get(i)).isGreaterThan(numbers.get(i - 1));
+        }
+
+    }
+
+}

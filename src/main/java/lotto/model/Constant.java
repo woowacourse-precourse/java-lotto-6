653af7a741898200ba3eabacb621ae6cd5ec0fdc
taehoon <xogns1514@gmail.com>
2023-11-06 19:13:21 +0900

    feat(LottoPlayerNumber): 로또 발행 기능
    - 1~45 수에서 중복되지 않는 6개의 숫자를 뽑는다.

--- src/main/java/lotto/model/Constant.java
+++ src/main/java/lotto/model/Constant.java
@@ -3,4 +3,7 @@ package lotto.model;
 public final class Constant {
 
     public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
 }
---
+++ src/main/java/lotto/model/LottoPlayerNumber.java
@@ -0,0 +1,36 @@
+package lotto.model;
+
+import static lotto.model.Constant.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LottoPlayerNumber {
+    private final List<Integer> lottoPlayerNumber;
+
+    public LottoPlayerNumber(List<Integer> lottoPlayerNumber) {
+        this.lottoPlayerNumber = generateLottoNumber();
+    }
+
+    public List<Integer> getLottoPlayerNumber() {
+        return lottoPlayerNumber;
+    }
+
+    private List<Integer> generateLottoNumber() {
+        Set<Integer> lottoPlayerNumber = new HashSet<>();
+
+        while (lottoPlayerNumber.size() <= LOTTO_NUMBER_COUNT) {
+            lottoPlayerNumber.add(Randoms.pickNumberInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX));
+        }
+        return sortLottoNumber(lottoPlayerNumber);
+    }
+
+    private static List<Integer> sortLottoNumber(Set<Integer> lottoPlayerNumber) {
+        return lottoPlayerNumber.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}

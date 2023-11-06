5c3ad196679c459c798c5c93f2f1464ac27d1701
alenjb <alenjb@naver.com>
2023-11-07 05:33:19 +0900

    🎨fix(NumberGenerator): 보너스 번호를 생성하는 부분을 추가
    보너스 번호를 생성하기 위해 처음 추첨 시 7개의 숫자를 생성하도록 변경함.
    보너스 번호를 추첨한 번호들에서 추출하게함.
    보너스 번호의 getter를 구현함.

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 public class NumberGenerator {
     private List<Integer> lottoNumbers;
+    private int bonusNumber;
 
     public void generateLottoNumbers() {
-        lottoNumbers = camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumbers = camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(1, 45, 7);
+        int randomNumber = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 6);
+        bonusNumber = lottoNumbers.get(randomNumber); //보너스 번호를 추출
+        lottoNumbers.remove(randomNumber); // 리스트에서 보너스 번호를 제거
     }
 
     public List<Integer> getLottoNumbers() {
         return lottoNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 }

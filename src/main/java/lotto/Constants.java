de155d790ccfe09fd02157a00359e552e8775fa1
daneng4 <daneng4@naver.com>
2023-11-06 18:09:49 +0900

    remove: 클래스 삭제

--- src/main/java/lotto/Constants.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public class Constants {
-
-    public static final String REQUIRE_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
-    public static final String REQUIRE_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    public static final String REQUIRE_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String WINNING_STATISTICS = "당첨 통계";
-}

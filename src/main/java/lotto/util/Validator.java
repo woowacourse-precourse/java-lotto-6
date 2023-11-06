5f6d9bc43f58b0a883b53c64a5b94ddc56d110e4
Yerinnnn <shinyerin0216@gmail.com>
2023-11-06 15:50:37 +0900

    Refactor: 매직 넘버를 상수로 치환

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,19 +1,24 @@
 package lotto.util;
 
+import lotto.view.InputView;
+
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Validator {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUM = 6;
+    private static final int BONUS_NUMBER_NUM = 1;
 
     public static void purchasePrice(int purchasePrice) {
-        if (purchasePrice % 1000 != 0) {
+        if (purchasePrice % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException();
         }
     }
 
     public static void winNumberNum(List<Integer> winNumber) {
-        if (winNumber.size() != 6) {
+        if (winNumber.size() != LOTTO_NUM) {
             throw new IllegalArgumentException();
         }
     }
@@ -24,4 +29,16 @@ public class Validator {
             throw new IllegalArgumentException();
         }
     }
+
+    public static void bonusNumberNum(int bonusNumber) {
+        if (bonusNumber != BONUS_NUMBER_NUM) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+//    public static void checkPositiveOrNegative(int num) {
+//        if (num < 0) {
+//            throw new IllegalArgumentException();
+//        }
+//    }
 }

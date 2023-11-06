bd4ed4be8eee388efc79b0773f0f9d636c753c20
Han <yulmam1@naver.com>
2023-11-06 14:56:48 +0900

    feat(Parser) : 입력값이 정수가 아닐 경우 예외처리 하는 기능 추가

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -2,7 +2,10 @@ package lotto.util;
 
 public class Parser {
     public static int stringToInt(String message) {
-
-        return Integer.parseInt(message);
+        try{
+            return Integer.parseInt(message);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[예외처리] 정수가 아닙니다.");
+        }
     }
 }

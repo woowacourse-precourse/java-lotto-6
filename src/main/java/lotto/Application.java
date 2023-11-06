725ca9a1e1cd0497e5adb095e35df6beab2ccacc
DevFrog92 <yongsu1481@gmail.com>
2023-11-06 14:34:29 +0900

    feat: implement Application main method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import lotto.domain.checker.Checker;
+import lotto.domain.checker.LottoChecker;
+import lotto.domain.customer.Customer;
+import lotto.domain.customer.LottoCustomer;
+import lotto.domain.generator.AutoTicketGenerator;
+import lotto.domain.generator.TicketGenerator;
+import lotto.domain.seller.Seller;
+import lotto.domain.seller.TicketSeller;
+import lotto.util.reader.InputReader;
+import lotto.util.reader.MagicNumberInputReader;
+import lotto.util.reader.MoneyInputReader;
+import lotto.util.RandomNumberUtil;
+import lotto.util.reader.LottoResultInputReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader moneyInputReader = new MoneyInputReader();
+        TicketGenerator generator = new AutoTicketGenerator(RandomNumberUtil.getInstance());
+        Seller seller = new TicketSeller(generator);
+        Customer customer = new LottoCustomer(moneyInputReader);
+
+        customer.buyTicket(seller);
+
+        InputReader resultInputReader = new LottoResultInputReader();
+        InputReader magicNumberInputReader = new MagicNumberInputReader();
+        Checker checker = new LottoChecker(resultInputReader, magicNumberInputReader);
+
+        customer.checkLottoResult(checker);
     }
 }

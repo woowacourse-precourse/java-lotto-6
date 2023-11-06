7adb50e4363a423b49b60d0637c6f20347f339dd
twinson37 <twinson37@hanyang.ac.kr>
2023-11-07 01:46:02 +0900

    Feat: 사용자 입력 검증 로직 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,18 +1,56 @@
 package lotto;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class LottoGame {
 
-    List<Integer> winningNumbers = new ArrayList<>();
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 
     public List<Integer> getWinningNumbers() {
         return winningNumbers;
     }
 
-    public LottoGame(List<Integer> winningNumbers) {
+    public LottoGame(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers,bonusNumber);
         this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        if(bonusNumber<0||bonusNumber>45){
+            throw new IllegalArgumentException();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int number: numbers){
+
+            if(set.isEmpty()){
+               set.add(number);
+               continue;
+            }
+
+            if (set.contains(number)){
+
+                throw new IllegalArgumentException();
+
+            }
+
+            set.add(number);
+
+
+        }
     }
 
 }

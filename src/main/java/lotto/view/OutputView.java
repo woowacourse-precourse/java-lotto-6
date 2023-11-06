41b2ebce3d3ccd182e2c04dbdffd3f674d9f2cf9
ChanUi <hcu55@naver.com>
2023-11-07 05:16:05 +0900

    feat: 당첨 번호 입력 문구 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OutputView {
     private static final String PRINT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PRINT_COUNT_PURCHASE_LOTTO_MESSAGE = "개를 구매했습니다.";
+    private static final String PRINT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public void printLottoPurchaseAmountInputMessage() {
         System.out.println(PRINT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
@@ -17,5 +18,10 @@ public class OutputView {
 
     public void printIssuedLottoNumbers(List<Integer> generateLottoNumbers) {
         System.out.println(generateLottoNumbers.toString());
+    }
+
+    public void printWinningNumbersInputMessage() {
+        System.out.println();
+        System.out.println(PRINT_WINNING_NUMBERS_MESSAGE);
     }
 }

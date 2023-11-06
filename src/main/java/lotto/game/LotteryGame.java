f98d27a6e1bfe43ee56ea5cd20038add56ea0d43
DooDuZ <sin9158@naver.com>
2023-11-07 05:34:29 +0900

    fix: 입력된 숫자 범위 및 중복 오류 수정

--- src/main/java/lotto/game/LotteryGame.java
+++ src/main/java/lotto/game/LotteryGame.java
@@ -1,6 +1,7 @@
 package lotto.game;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -32,7 +33,6 @@ public class LotteryGame {
         outputHandler.print(Messasge.REQUEST_PRICE);
         lottoList = getLottoList(getPrice());
         outputHandler.print(Messasge.getLottoListMessage(lottoList));
-        outputHandler.print(Messasge.REQUEST_NUMBERS);
         winNumber = getWinNumber();
     }
 
@@ -67,10 +67,17 @@ public class LotteryGame {
     private WinNumber getWinNumber() {
         while (true) {
             try {
+                outputHandler.print(Messasge.REQUEST_NUMBERS);
                 List<Integer> numbers = getLottoNumbers();
-                return new WinNumber(numbers, getBonusNumber(numbers));
+                LottoNumberValidator.validateLottoNumbersRange(numbers);
+
+                outputHandler.print(Messasge.REQUEST_BONUS_NUMBER);
+                int bonusNumber = getBonusNumber(numbers);
+                LottoNumberValidator.validateSingleNumberRange(bonusNumber);
+
+                return new WinNumber(numbers, bonusNumber);
             } catch (IllegalArgumentException e) {
-                outputHandler.print(e.getMessage());
+                outputHandler.print(Messasge.ERROR + e.getMessage());
             }
         }
     }
@@ -79,13 +86,7 @@ public class LotteryGame {
         while (true) {
             try {
                 String[] inputNumbers = inputHandler.readLine().split(",");
-                Integer[] numbers = new Integer[inputNumbers.length];
-                for (int i = 0 ; i<inputNumbers.length; i++) {
-                    LottoNumberValidator.isInteger(inputNumbers[i].trim());
-                    numbers[i] = Integer.parseInt(inputNumbers[i].trim());
-                }
-                // 출력 위치 변경 필요
-                outputHandler.print(Messasge.REQUEST_BONUS_NUMBER);
+                Integer[] numbers = toIntegerArray(inputNumbers);
                 return Arrays.asList(numbers);
             } catch (IllegalArgumentException e) {
                 outputHandler.print(Messasge.ERROR + e.getMessage());
@@ -93,9 +94,16 @@ public class LotteryGame {
         }
     }
 
+    private Integer[] toIntegerArray(String[] inputNumbers){
+        Integer[] numbers = new Integer[inputNumbers.length];
+        for (int i = 0; i < inputNumbers.length; i++) {
+            LottoNumberValidator.isInteger(inputNumbers[i].trim());
+            numbers[i] = Integer.parseInt(inputNumbers[i].trim());
+        }
+        return numbers;
+    }
+
     private int getBonusNumber(List<Integer> numbers) {
-        // test
-        System.out.println(numbers.toString());
         while (true) {
             try {
                 String inputNumber = inputHandler.readLine();
@@ -109,23 +117,23 @@ public class LotteryGame {
         }
     }
 
-    private Map<Rank, Integer> getResult(){
+    private Map<Rank, Integer> getResult() {
         Map<Rank, Integer> result = new HashMap<>();
-        for ( Rank rank : Rank.values()){
+        for (Rank rank : Rank.values()) {
             result.put(rank, 0);
         }
-        for (Lotto lotto : lottoList){
+        for (Lotto lotto : lottoList) {
             Rank rank = lotto.getRank(winNumber);
-            result.put(rank, result.getOrDefault(rank, 0)+1);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
         }
         return result;
     }
 
-    private double getProfitRate(int cost){
+    private double getProfitRate(int cost) {
         long totalReward = 0L;
 
-        for(Rank rank : result.keySet()){
-            totalReward += rank.reward * result.get(rank) ;
+        for (Rank rank : result.keySet()) {
+            totalReward += rank.reward * result.get(rank);
         }
 
         return (double) totalReward / cost * 100;

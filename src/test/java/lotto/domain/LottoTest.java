7316dd2b1486d8b3bc19f24d089b8762e41022f8
OikimiO <etkim02@naver.com>
2023-11-07 07:20:57 +0900

    test: 로또 예외 및 정상 출력 테스트
    - 구매 로또 번화와 담청내역 일치 확인 테스트
    - 로또 범위에 일치하지 않는 숫자 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,11 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -21,5 +20,27 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 범위에 일치하지 않는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createOverRange(){
+        assertThatThrownBy(() -> new Lotto(List.of(46, 0, -1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매 로또 번호와 당첨 내역이 일치하는지 확인한다.")
+    @Test
+    void checkSameNumber(){
+        // given
+        List<Integer> numbers = List.of(1,2,3,4,5,6);
+        List<Integer> ticket = List.of(2,3,14,21,26,30);
+        Lotto lotto = new Lotto(numbers);
+
+        // when
+        int sameCount = lotto.sameTicket(ticket);
+
+        // then
+        assertThat(sameCount).isEqualTo(2);
     }
 }

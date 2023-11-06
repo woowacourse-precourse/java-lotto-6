d7bcec49b3522d320491ff0045473ea7bfbaa75c
sinminseok12 <sinminseok12@naver.com>
2023-11-06 19:42:12 +0900

    refactor(Lotto, WinLotto): 로또 번호 필드 타입을 Integer -> LottoNumber 로 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,27 +3,25 @@ package lotto.model;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private static final Integer LOTTO_SIZE = 6;
-    private static final Integer NUMBER_MIN_RANGE = 1;
-    private static final Integer NUMBER_MAX_RANGE = 45;
-    private static final String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String LOTTO_SIZE_ERROR = "[ERROR] 로또 번호 갯수는 6개여야 합니다.";
     private static final String LOTTO_DUPLICATE_ERROR = "[ERROR] 중복되는 숫자는 허용하지 않습니다.";
 
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateDuplicateNumber(numbers);
-        validateNumbersRange(numbers);
-        this.numbers = numbers;
+        this.numbers = toLottoNumbers(numbers);
     }
 
-    private void validateNumbersRange(List<Integer> numbers) {
-        numbers.stream()
-                .forEach(Lotto::validateNumberRange);
+    private List<LottoNumber> toLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 
     private void validateLottoSize(List<Integer> numbers) {
@@ -36,12 +34,6 @@ public class Lotto {
         Set<Integer> setNumbers = new HashSet<>(numbers);
         if (setNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException(LOTTO_DUPLICATE_ERROR);
-        }
-    }
-
-    private static void validateNumberRange(int number) {
-        if (number < NUMBER_MIN_RANGE || number > NUMBER_MAX_RANGE) {
-            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR);
         }
     }
 
--- src/main/java/lotto/model/WinLotto.java
+++ src/main/java/lotto/model/WinLotto.java
@@ -2,10 +2,11 @@ package lotto.model;
 
 public class WinLotto {
     private final Lotto lotto;
-    private final Integer bonusNumber;
+    private final LottoNumber bonusNumber;
 
-    public WinLotto(Lotto lotto, Integer bonusNumber) {
+    public WinLotto(Lotto lotto, LottoNumber bonusNumber) {
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
+
 }

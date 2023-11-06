a8f77ddc5ebe03fbd11c959f58236d3fc767dec5
jae woo Jung <awfjol2008@naver.com>
2023-11-06 19:44:27 +0900

    feat: Lotto 클래스 생성자 파라미터 변경 및 그에 따른 변경사항 적용

--- src/main/java/lotto/domain/player/lotto/Lotto.java
+++ src/main/java/lotto/domain/player/lotto/Lotto.java
@@ -1,28 +1,34 @@
- package lotto.domain.player.lotto;
+package lotto.domain.player.lotto;
 
- import java.util.ArrayList;
- import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
- public class Lotto {
+public class Lotto {
     private final List<LottoNumber> numbers;
 
-    private Lotto(List<LottoNumber> numbers) {
+    private Lotto(List<Integer> numbers) {
         validateLottoNumber(numbers);
-        this.numbers = new ArrayList<>(numbers);
+        this.numbers = new ArrayList<>(generateLottoNumbers(numbers));
     }
 
-    public static Lotto create(List<LottoNumber> numbers) {
+    public static Lotto create(List<Integer> numbers) {
         return new Lotto(numbers);
     }
 
-    private static void validateLottoNumber(List<LottoNumber> numbers) {
+    private static void validateLottoNumber(List<Integer> numbers) {
         validateLottoNumbersLength(numbers);
     }
 
-    private static void validateLottoNumbersLength(List<LottoNumber> numbers) {
+    private static void validateLottoNumbersLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호의 총 길이는 6이어야 합니다.");
         }
+    }
+
+    private static List<LottoNumber> generateLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::create)
+                .toList();
     }
 
     public List<LottoNumber> getNumbers() {

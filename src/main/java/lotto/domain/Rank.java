5f22500060de24dd5dd1f844feaf808f1cd4b57e
hunko <rhwlgns4386@naver.com>
2023-11-06 21:41:40 +0900

    refactor(Rank): 매직 넘버 제거

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,23 +5,42 @@ import java.util.Optional;
 import java.util.function.BiPredicate;
 
 public enum Rank {
-    SIX((similarity, hasBonus) -> similarity.equals(6)),
-    FIVE_BONUS((similarity, hasBonus) -> similarity.equals(5) && hasBonus),
-    FIVE((similarity, hasBonus) -> similarity.equals(5) && !hasBonus),
-    FOUR((similarity, hasBonus) -> similarity.equals(4)),
-    THREE((similarity, hasBonus) -> similarity.equals(3));
-    
-    private final BiPredicate<Integer, Boolean> condition;
+    SIX(6),
+    FIVE_BONUS(5),
+    FIVE(5),
+    FOUR(4),
+    THREE(3);
 
-    Rank( BiPredicate<Integer, Boolean> condition) {
-        this.condition = condition;
+    private final int count;
+
+    Rank(int count) {
+        this.count = count;
     }
 
-    public static Optional<Rank> findRank(Integer similarity,Boolean hasBonus){
-        return Arrays.stream(values()).filter(rank -> rank.hasRank(similarity,hasBonus)).findFirst();
+    public static Optional<Rank> findRank(Integer similarity, Boolean hasBonus) {
+        return Arrays.stream(RankCondition.values())
+                .filter(rank -> rank.isMeetCondition(similarity, hasBonus))
+                .map(rankCondition -> rankCondition.rank)
+                .findFirst();
     }
 
-    private boolean hasRank(Integer similarity,Boolean hasBonus){
-        return condition.test(similarity,hasBonus);
+
+    private enum RankCondition {
+        SIX_CONDITION((similarity, hasBonus) -> similarity.equals(SIX.count), SIX),
+        FIVE_BONUS_CONDITION((similarity, hasBonus) -> similarity.equals(FIVE_BONUS.count) && hasBonus, FIVE_BONUS),
+        FIVE_CONDITION((similarity, hasBonus) -> similarity.equals(FIVE.count) && !hasBonus, FIVE),
+        FOUR_CONDITION((similarity, hasBonus) -> similarity.equals(FOUR.count), FOUR),
+        THREE_CONDITION((similarity, hasBonus) -> similarity.equals(THREE.count), THREE);
+        private final BiPredicate<Integer, Boolean> condition;
+        private final Rank rank;
+
+        RankCondition(BiPredicate<Integer, Boolean> condition, Rank rank) {
+            this.condition = condition;
+            this.rank = rank;
+        }
+
+        private boolean isMeetCondition(Integer similarity, Boolean hasBonus) {
+            return condition.test(similarity, hasBonus);
+        }
     }
 }

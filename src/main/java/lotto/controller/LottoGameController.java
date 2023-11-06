c163a0919cbc09c15a2c050247eb3a89769610bd
dd <12202340@inha.edu>
2023-11-06 20:47:09 +0900

    refactor: Controller 구조 리팩토링

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,33 +1,24 @@
 package lotto.controller;
 
-import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.domain.PrizeResult;
 import lotto.domain.WinningLotto;
-import lotto.domain.generator.LottoGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
-    private Money money;
-    private Lottos lottos;
-    private WinningLotto winningLotto;
-
     public void startGame() {
-        money = Money.from(InputView.inputAmount());
-        lottos = new Lottos(LottoGenerator.generateLottos(money.getLottoCount()));
-        printLottos();
-        winningLotto = WinningLotto.of(InputView.inputWinningNumber(),
-                InputView.inputBonusNumber());
-        PrizeResult result = new PrizeResult(lottos, winningLotto);
-        OutputView.printResults(result.getResults(), money.getProfit(result.getPrizeMoney()));
-    }
-
-    private void printLottos() {
+        Money money = Money.from(InputView.inputAmount());
         OutputView.printLottoCountMessage(money.getLottoCount());
-        for (Lotto lotto : lottos.getLottos()) {
-            OutputView.printLottos(lotto.getNumber());
-        }
+
+        Lottos lottos = Lottos.createByGenerator(money.getLottoCount());
+        lottos.getLottos().forEach(lotto -> OutputView.printLottos(lotto.getNumber()));
+
+        WinningLotto winningLotto = WinningLotto.of(InputView.inputWinningNumber(),
+                InputView.inputBonusNumber());
+
+        PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
+        OutputView.printResults(prizeResult.getResults(), money.getProfit(prizeResult.getPrizeMoney()));
     }
 }

ad13f9940e3a084516349ef85fc86270fb83a1fd
IM-GYURI <kgulr0517@gmail.com>
2023-11-07 02:29:04 +0900

    style: 주석 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,6 @@ import lotto.controller.MainController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         try {
             MainController mainController = new MainController();
             mainController.start();

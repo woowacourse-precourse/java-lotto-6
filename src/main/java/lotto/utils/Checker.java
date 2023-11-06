e635cf84f463727c6f34fc25d9b3d0879ec28ffa
JayG-5 <jg55552758@gmail.com>
2023-11-06 21:36:01 +0900

    feat: checker 입력 유효성 검사 추가

--- src/main/java/lotto/utils/Checker.java
+++ src/main/java/lotto/utils/Checker.java
@@ -1,19 +1,43 @@
 package lotto.utils;
 
+import lotto.exception.LottoException;
+import lotto.exception.Message;
+
 import java.util.HashSet;
 import java.util.List;
 
 public class Checker {
 
-    public static boolean isUnique(List<Integer> integerList) {
-        return new HashSet<>(integerList).size() == integerList.size();
-    }
 
     public static boolean isPositive(Integer number) {
         return number > 0;
     }
 
-    public static boolean is1k(Integer number){
+    public static boolean is1k(Integer number) {
         return number % 1000 == 0;
+    }
+
+    public static void validateUnique(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw LottoException.of(Message.NUMBER_UNIQUE);
+        }
+    }
+
+    public static void validateSize(List<Integer> numbers, int size) {
+        if (numbers.size() != size) {
+            throw LottoException.of(Message.NUMBER_SIZE);
+        }
+    }
+
+    public static void validateRange(List<Integer> numbers, int startNum, int endNum) {
+        for (int number : numbers) {
+            if (number < startNum || number > endNum) {
+                throw LottoException.of(Message.NUMBER_RANGE);
+            }
+        }
+    }
+
+    public static boolean isValidateRange(Integer number, int startNum, int endNum) {
+        return number >= startNum && number <= endNum;
     }
 }

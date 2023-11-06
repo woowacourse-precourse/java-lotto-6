30dc89a9a880b0a9a8240d34bc4e88ec3d1b4c05
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-06 10:28:47 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,17 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 	private static Integer money;
+	private static List<Integer> winningNumberList;
 
 	public static void main(String[] args) {
 		setMoney();
-		
+		setWinningNumberList();
 	}
 
 	private static void setMoney() {
@@ -25,6 +29,16 @@ public class Application {
 		}
 	}
 
+	public static void setWinningNumberList() {
+		try {
+			System.out.println("당첨 번호를 입력해 주세요.");
+			winningNumberList = readNumberList();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			setWinningNumberList();
+		}
+	}
+
 	private static Integer readNumber() {
 		Integer number = null;
 		try {
@@ -33,5 +47,24 @@ public class Application {
 			throw new IllegalArgumentException("[ERROR] 구입금액은 숫자만 입력 가능합니다!");
 		}
 		return number;
+	}
+
+	private static List<Integer> readNumberList() {
+		List<Integer> numberList = new ArrayList<>();
+		try {
+			String[] inputList = Console.readLine().split(",");
+
+			if (inputList.length != 6) {
+				throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개를 입력해야 합니다!");
+			}
+
+			for (String input : inputList) {
+				Integer number = Integer.parseInt(input);
+				numberList.add(number);
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 입력 가능합니다!");
+		}
+		return numberList;
 	}
 }

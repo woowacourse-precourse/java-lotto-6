1701c54572909c07925e932b25e879c0f386659b
ykmxxi <ykmxxi97@gmail.com>
2023-11-06 15:51:59 +0900

    refactor(NumberFormatConverter): 패턴 문자열을 상수화

--- src/main/java/lotto/util/NumberFormatConverter.java
+++ src/main/java/lotto/util/NumberFormatConverter.java
@@ -4,16 +4,19 @@ import java.text.DecimalFormat;
 
 public final class NumberFormatConverter {
 
+    private static final String REWARD_PATTERN = "#,###원";
+    private static final String YIELD_PATTERN = "#,##0.0";
+
     private NumberFormatConverter() {
     }
 
     public static String convertToRewardFormat(final int reward) {
-        DecimalFormat rewardFormat = new DecimalFormat("#,###원");
+        DecimalFormat rewardFormat = new DecimalFormat(REWARD_PATTERN);
         return rewardFormat.format(reward);
     }
 
     public static String convertToYieldFormat(final double yield) {
-        DecimalFormat yieldFormat = new DecimalFormat("#,##0.0");
+        DecimalFormat yieldFormat = new DecimalFormat(YIELD_PATTERN);
         return yieldFormat.format(yield);
     }
 

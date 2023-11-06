6bf94e31cf0abe7a9b41ae6361ea6316298f357f
yechop <channetattoo@gmail.com>
2023-11-06 18:49:41 +0900

    test(RandomLottoNumberGeneratorTest): 생성된 6자리 숫자가 정렬되어있는지 테스트 구현

--- src/test/java/lotto/domain/RandomLottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/RandomLottoNumberGeneratorTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,16 +12,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RandomLottoNumberGeneratorTest {
 
+    RandomLottoNumberGenerator randomLottoNumberGenerator;
+    List<Integer> randomNumbers;
+    @BeforeEach
+    void setUp() {
+        randomLottoNumberGenerator = new RandomLottoNumberGenerator();
+        randomNumbers = randomLottoNumberGenerator.generateRandomNumbers();
+    }
+
     @Test
-    @DisplayName("6자리 숫자를 생성한다.")
-    void generateRandomNumbersTest() {
+    @DisplayName("중복되지 않는6자리 숫자를 생성한다.")
+    void generateSixNumbersTest() {
         //given
-        RandomLottoNumberGenerator randomLottoNumberGenerator = new RandomLottoNumberGenerator();
 
         //when
-        List<Integer> randomNumbers = randomLottoNumberGenerator.generateRandomNumbers();
 
         //then
-        assertThat(randomNumbers.size()).isEqualTo(6);
+        assertThat(randomNumbers.stream().distinct().count()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("생성한 6자리 숫자는 오름차순으로 정렬되어 있다.")
+    void sortedTest() {
+        //given
+        boolean flag = true;
+
+        //when
+        for (int i = 1; i < randomNumbers.size(); i++) {
+            if (randomNumbers.get(i - 1) > randomNumbers.get(i)) {
+                flag = false;
+            }
+        }
+
+        //then
+        Assertions.assertThat(flag).isEqualTo(true);
     }
 }

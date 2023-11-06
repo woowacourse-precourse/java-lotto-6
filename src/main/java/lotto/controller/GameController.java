429045863d38a3b2ac749f13f5b44aaa9b29faed
grax <grax@192.168.35.119>
2023-11-07 03:50:58 +0900

    feat(lotto): create playLotto() method

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,5 +1,15 @@
 package lotto.controller;
 
-public class GameController {
+import lotto.view.Printer;
 
+public class GameController {
+	
+	Printer printer = new Printer();
+	
+	
+	public void playLotto() {
+		printer.printDirection("구입금액을 입력해 주세요.\n");
+		
+		
+	}
 }

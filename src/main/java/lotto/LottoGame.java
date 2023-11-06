f774e18416722f661c16ad9cdd66c6918c5b7cbd
Seunghyun-Jeong <95638057+Seunghyun-Jeong@users.noreply.github.com>
2023-11-06 15:05:23 +0900

    feat: 입력한 로또 구입 금액에 맞게 로또 발행 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,9 +3,17 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class LottoGame {
     public void execute() {
         int purchaseMoney = purchase();
+
+        int purchaseLottoCount = getPurchaseLottoCount(purchaseMoney);
+
+        List<Lotto> purchaseLottos = getPurchaseLotto(purchaseLottoCount);
     }
 
     private int purchase() {
@@ -20,6 +28,27 @@ public class LottoGame {
                 System.out.println("[ERROR] 구입금액은 1,000단위 숫자여야 합니다.");
             }
         }
+    }
+
+    private int getPurchaseLottoCount(int purchaseMoney) {
+        int purchaseLottoCount = (purchaseMoney / 1000);
+        System.out.println(purchaseLottoCount + "개를 구매했습니다.");
+        return purchaseLottoCount;
+    }
+
+    private List<Lotto> getPurchaseLotto(int lottoCount) {
+        List<Lotto> purchaseLottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(lottoNumbers);
+            purchaseLottos.add(new Lotto(lottoNumbers));
+        }
+
+        for (Lotto lotto : purchaseLottos) {
+            lotto.printNumber();
+        }
+
+        return purchaseLottos;
     }
 
     private void validatePurchaseMoney(int purchaseMoney) {

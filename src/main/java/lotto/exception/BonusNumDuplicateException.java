090754a94a884e794c16dec02194fca3c91fffb3
34suuuuu <psh7492@gmail.com>
2023-11-06 12:22:28 +0900

    feat: bonusNum 중복 예외처리 기능 추가

--- src/main/java/lotto/exception/BonusNumDuplicateException.java
+++ src/main/java/lotto/exception/BonusNumDuplicateException.java
@@ -1,2 +1,9 @@
-package lotto.exception;public class BonusNumDuplicateException {
+package lotto.exception;
+
+public class BonusNumDuplicateException extends IllegalArgumentException {
+    private static final String ERROR_MESSAGE = "당첨 번호에 보너스 번호가 포함되어 있습니다.";
+
+    public BonusNumDuplicateException(){
+        super(ERROR_MESSAGE);
+    }
 }

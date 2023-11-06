4d45e9ec5ddff21d1ebf40b97e662c3de1822abb
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-06 22:20:38 +0900

    refactor(OutputView) : 문자열 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,12 +8,17 @@ import lotto.domain.Profit;
 import lotto.enums.Rank;
 
 public class OutputView {
+    private static final String INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTICS = "당첨 통계\n---";
+    private static final String RATE_FORMAT = "#,##0.0";
 
     private OutputView() {
     }
 
     public static void printPurchaseMessage() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_PURCHASE_PRICE);
     }
 
     public static void printPurchaseCount(LottoTicket lottoTicket) {
@@ -26,29 +31,27 @@ public class OutputView {
     }
 
     public static void printWinningNumberMessage() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(INPUT_WINNING_NUMBER);
     }
 
     public static void printBonusNumberMessage() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER);
     }
 
     public static void printWinningResult(LottoResult lottoResult) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(WINNING_STATISTICS);
         Arrays.stream(Rank.values())
                 .forEach(rank -> System.out.printf(rank.getMatchInfo(), lottoResult.findMatchCountFor(rank)));
         printNewLine();
     }
 
     public static void printRateOfReturn(Profit profit) {
-        DecimalFormat df = new DecimalFormat("#,##0.0");
+        DecimalFormat df = new DecimalFormat(RATE_FORMAT);
         System.out.printf("총 수익률은 %s%%입니다.", df.format(profit.getRateOfReturn()));
     }
 
     public static void printErrorMessageFor(IllegalArgumentException exception) {
         System.out.println(exception.getMessage());
-        printNewLine();
     }
 
     public static void printNewLine() {

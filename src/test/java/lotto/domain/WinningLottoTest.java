8a3d8fbb1ecf88a5993a7b24a8b5e63f1d53d0c6
jihwankim128 <jihwan1535@nate.com>
2023-11-07 04:33:23 +0900

    test: 로또 번호에 대한 당첨결과 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("당첨 번호 발행 테스트")
 class WinningLottoTest {
+    private WinningLotto winningLotto;
+    private Lotto lotto;
     @Test
     void 보너스_번호_범위_테스트() {
         assertThatThrownBy(()-> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 0))
@@ -20,5 +23,48 @@ class WinningLottoTest {
         assertThatThrownBy(()-> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 5))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 로또 번호에 보너스 번호와 중복된 번호가 존재합니다.");
+    }
+
+    @Test
+    void 당첨_1등_테스트() {
+        assertThat(new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 7)
+                .lottoNumbersMatch(new Lotto(List.of(1, 2, 3, 4, 5, 6)))).isEqualTo(6);
+    }
+
+    @Test
+    void 당첨_2등_테스트() {
+        winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 7);
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        assertThat(winningLotto.lottoNumbersMatch(lotto)).isEqualTo(5);
+        assertThat(winningLotto.lottoBonusNumberMatch(lotto)).isTrue();
+    }
+
+
+    @Test
+    void 당첨_3등_테스트() {
+        winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 7);
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        assertThat(winningLotto.lottoNumbersMatch(lotto)).isEqualTo(5);
+        assertThat(winningLotto.lottoBonusNumberMatch(lotto)).isFalse();
+    }
+
+
+    @Test
+    void 당첨_4등_테스트() {
+        assertThat(new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 7)
+                .lottoNumbersMatch(new Lotto(List.of(1, 2, 3, 4, 8, 7)))).isEqualTo(4);
+    }
+
+
+    @Test
+    void 당첨_5등_테스트() {
+        assertThat(new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 7)
+                .lottoNumbersMatch(new Lotto(List.of(1, 2, 3, 10, 11, 45)))).isEqualTo(3);
+    }
+
+    @Test
+    void 미당첨_테스트() {
+        assertThat(new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 6, 5)), 7)
+                .lottoNumbersMatch(new Lotto(List.of(45, 44, 43, 42, 6, 5)))).isEqualTo(0);
     }
 }

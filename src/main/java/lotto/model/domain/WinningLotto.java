3f2927bedba640f1a6aa9f07eedc2aa232e6e7c5
D0ri123 <lsh81550@gmail.com>
2023-11-06 14:28:44 +0900

    feat(WinningLotto): 당첨 번호 검증 기능 구현 - 3차 구현
    입력값이 예외처리가 됐을 때, 값이 할당되지 않습니다.
    그래서 null 일 경우 계속해서 입력받을 수 있도록 하기 위해서
    InputValidator를 implements했습니다.

--- src/main/java/lotto/model/domain/WinningLotto.java
+++ src/main/java/lotto/model/domain/WinningLotto.java
@@ -1,29 +1,66 @@
 package lotto.model.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.util.InputValidator;
 
-public class WinningLotto {
+public class WinningLotto implements InputValidator {
 
-    private final List<Integer> numbers;
-    private Bonus bonusNumber;
+    private List<Integer> numbers;
+    private Bonus bonusNumber; //TODO: bonus 제거해야함
 
-    public WinningLotto(String numbers) {
-        this.numbers = parseInput(numbers);
+    @Override
+    public boolean getInputUntilValid() {
+        return numbers == null;
     }
 
-    private List<Integer> parseInput(String numbers) {
-        List<Integer> parsedList = new ArrayList<>();
-        String[] parsed = numbers.split(",");
-
-        for(String stringInput : parsed) {
-            parsedList.add(Integer.parseInt(stringInput));
-        }
-        return parsedList;
+    public void setWinningNumber(String numbers) {
+        List<String> inputDividedByComma = validateDelimiterComma(numbers);
+        validateSixElements(inputDividedByComma);
+        List<Integer> integerNumbers = validateWinningNumberIsNumeric(inputDividedByComma);
+        validateNumberBetweenInRange(integerNumbers, 1, 45);
+        this.numbers = integerNumbers;
     }
 
     public void addBonus(Bonus bonus) {
         this.bonusNumber = bonus;
+    }
+
+    public List<String> validateDelimiterComma(String numbers) {
+        if (!numbers.matches(".*,.*")) {
+            throw new IllegalArgumentException("[ERROR] 쉼표로 구분하여 입력해주세요.");
+        }
+        return List.of(numbers.split(","));
+    }
+
+    public void validateSixElements(List<String> numbers) {
+        Set<String> set = new HashSet<>(numbers);
+        if(set.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 서로 다른 6개의 당첨 번호를 입력해주세요.");
+        }
+    }
+
+    public List<Integer> validateWinningNumberIsNumeric(List<String> numbers) {
+        List<Integer> integerNumbers = new ArrayList<>();
+        for (String number : numbers) {
+            try {
+                int numberToInteger = Integer.parseInt(number);
+                integerNumbers.add(numberToInteger);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+            }
+        }
+        return integerNumbers;
+    }
+
+    private void validateNumberBetweenInRange(List<Integer> integerNumbers, int min, int max) {
+        for(int number : integerNumbers) {
+            if (number < min || number > max) {
+                throw new IllegalArgumentException("[ERROR] 1에서 45사이의 숫자로 입력해주세요.");
+            }
+        }
     }
 
     public List<Integer> getNumbers() {

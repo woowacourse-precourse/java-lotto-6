8acb51b4f3d08d02bd6d9d37f3e3e6db9c228245
jdohun <wjdehgns97@naver.com>
2023-11-06 19:42:51 +0900

    Feat: 소수 첫번째 자리 반올림 상수 생성
    RegularConstant.java

--- src/main/java/lotto/constant/RegularConstant.java
+++ src/main/java/lotto/constant/RegularConstant.java
@@ -7,4 +7,5 @@ public class RegularConstant {
     public static final int UNIT_AMOUNT = 1_000;
     public static final int PERCENT = 100;
     public static final String DELIMITER = ",";
+    public static final int FIRST_DECIMAL_PLACE = 1;
 }

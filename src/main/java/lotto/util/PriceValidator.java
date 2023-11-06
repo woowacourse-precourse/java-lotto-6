84c10510e3bce3002ebc2edb18277b6355f3ee62
Juhyorim <joohr1234@gmail.com>
2023-11-06 20:47:38 +0900

    fix: PriceValidator 객체를 생성할 수 없도록 private 생성자 추가

--- src/main/java/lotto/util/PriceValidator.java
+++ src/main/java/lotto/util/PriceValidator.java
@@ -4,6 +4,9 @@ public class PriceValidator {
     private static final String MIN_PRICE_1000_MESSAGE = "금액은 1000원 이상이어야 합니다";
     private static final String UNIT_OF_1000_REQUIRED_MESSAGE = "금액은 1000원 단위로 입력되어야 합니다";
 
+    private PriceValidator() {
+    }
+
     public static void validatePrice(Integer price) {
         if (isUnder1000(price)) {
             throw new IllegalArgumentException(MIN_PRICE_1000_MESSAGE);

4a3b51002ba1a84d5690edcf035bac8cd7a7be5b
koomchang <leahpar0401@gmail.com>
2023-11-06 22:07:26 +0900

    feat: 로또 컨트롤러에서 구현한 로직들이 순서대로 잘 작동하도록 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,10 @@
 package lotto.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.LottoGenerator;
+import lotto.model.LottoResult;
 import lotto.model.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -17,16 +19,19 @@ public class LottoController {
         int money = inputView.inputMoney();
         int lottoCount = money / 1000;
         outputView.printLottoCount(lottoCount);
+        List<Lotto> lottoArrayList = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             Lotto generatedLotto = lottoGenerator.generate();
+            lottoArrayList.add(generatedLotto);
             outputView.printLottoNumbers(generatedLotto.getNumbers());
         }
+        LottoResult lottoResult = new LottoResult(lottoArrayList);
         outputView.askForWinningNumbers();
         List<Integer> winningNumbers = inputView.inputWinningNumbers();
         outputView.askForBonusNumber();
         int bonusNumber = inputView.inputBonusNumber();
         WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
-        outputView.printWinningStatistics();
-
+        outputView.printWinningStatistics(lottoResult.checkResult(winningLotto));
+        outputView.printTotalProfit(lottoResult.calculateReturnRate(lottoResult.checkResult(winningLotto), money));
     }
 }

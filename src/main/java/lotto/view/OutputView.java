93e48146d3316294a90f9a4fd9c00ce6b9b3bacb
elyudwo <dudqk9696@naver.com>
2023-11-06 12:39:11 +0900

    [Feat] : 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Rank;
+import lotto.domain.WinningLottoStorage;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,5 +21,23 @@ public class OutputView {
         rankPrinter.put(Rank.CORRECT_FIVE, "5개 일치 (1,500,000원) - %d개");
         rankPrinter.put(Rank.CORRECT_FIVE_BONUS, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개");
         rankPrinter.put(Rank.CORRECT_SIX, "6개 일치 (2,000,000,000원) - %d개");
+    }
+
+    public void printLottoResult(WinningLottoStorage winningLottoStorage) {
+        for (Map.Entry<Rank, String> ranks : rankPrinter.entrySet()) {
+            Rank rank = ranks.getKey();
+            int numberOfItems = getLottoResultByRank(rank, winningLottoStorage);
+
+            System.out.println(formatResultString(rank, numberOfItems));
+        }
+    }
+
+    private String formatResultString(Rank rank, int numberOfItems) {
+        String formatString = rankPrinter.get(rank);
+        return String.format(formatString, numberOfItems);
+    }
+
+    private int getLottoResultByRank(Rank rank, WinningLottoStorage winningLottoStorage) {
+        return winningLottoStorage.getRank(rank);
     }
 }

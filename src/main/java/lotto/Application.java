64f172604c6927a901f1ef52bb21e8158eae8d20
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-06 10:14:34 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	private static Integer money;
+
+	public static void main(String[] args) {
+		setMoney();
+		
+	}
+
+	private static void setMoney() {
+		try {
+			System.out.println("구입금액을 입력해 주세요.");
+			money = readNumber();
+
+			if (money % 1000 != 0) {
+				throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력 가능합니다!");
+			}
+
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			setMoney();
+		}
+	}
+
+	private static Integer readNumber() {
+		Integer number = null;
+		try {
+			number = Integer.parseInt(Console.readLine());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 구입금액은 숫자만 입력 가능합니다!");
+		}
+		return number;
+	}
 }

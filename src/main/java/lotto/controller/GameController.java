5f59eac3967a24a9be8a8a3485aa31948fc802e4
Sunhee Kim <sunni372@icloud.com>
2023-11-07 01:51:27 +0900

    feat: 당첨 내역을 출력 기능 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,14 +1,17 @@
 package lotto.controller;
 
-import static lotto.view.SystemMessage.INPUT_BONUS;
-import static lotto.view.SystemMessage.INPUT_PURCHASE_AMOUNT;
-import static lotto.view.SystemMessage.INPUT_WINNING_LOTTO;
-import static lotto.view.SystemMessage.OUTPUT_PURCHASE_AMOUNT;
+import static lotto.view.message.SystemMessage.INPUT_BONUS;
+import static lotto.view.message.SystemMessage.INPUT_PURCHASE_AMOUNT;
+import static lotto.view.message.SystemMessage.INPUT_WINNING_LOTTO;
+import static lotto.view.message.SystemMessage.OUTPUT_PURCHASE_AMOUNT;
+import static lotto.view.message.SystemMessage.OUTPUT_RESULT;
 
+import java.util.HashMap;
 import lotto.domain.Referee;
 import lotto.domain.Win;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import lotto.view.message.RankMessage;
 
 public class GameController {
     public void run() {
@@ -21,6 +24,7 @@ public class GameController {
 
         Referee referee = new Referee();
         referee.compare(lottoController.getLottos(), win.getWinningLotto(), win.getBonus());
+        printResult(referee);
     }
 
     private void printLottoAmountAndLottos(LottoController lottoController) {
@@ -41,5 +45,14 @@ public class GameController {
     private static int readBonus() {
         OutputView.printMessage(INPUT_BONUS.getMessage());
         return Integer.parseInt(InputView.read());
+    }
+
+    private void printResult(Referee referee) {
+        OutputView.printMessage(OUTPUT_RESULT.getMessage());
+        HashMap<Integer, Integer> result = referee.getResult();
+        for (int i = 5; i > 0; i--) {
+            String rankResult = String.format(RankMessage.findByRank(i).getMessage(), result.get(i));
+            OutputView.printMessage(rankResult);
+        }
     }
 }
--- src/main/java/lotto/view/message/SystemMessage.java
+++ src/main/java/lotto/view/message/SystemMessage.java
@@ -1,10 +1,11 @@
-package lotto.view;
+package lotto.view.message;
 
 public enum SystemMessage {
     INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     INPUT_WINNING_LOTTO("\n당첨 번호를 입력해 주세요."),
     INPUT_BONUS("\n보너스 번호를 입력해 주세요."),
-    OUTPUT_PURCHASE_AMOUNT("\n%d개를 구매했습니다.");
+    OUTPUT_PURCHASE_AMOUNT("\n%d개를 구매했습니다."),
+    OUTPUT_RESULT("\n당첨 통계\n---");
 
     private final String message;
 

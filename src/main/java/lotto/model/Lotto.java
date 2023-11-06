5ad03426032946b9d2517b35b71171bc6f307aad
Genius-PAK <pyj789123@gmail.com>
2023-11-06 17:02:20 +0900

    feat(Lotto): 주어진 Lotto 클래스에 iterator 반환, 문자열 변환 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,12 @@
-package lotto;
+package lotto.model;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Lotto {
+    private static final String DISPLAY_BEGIN_MARK = "[";
+    private static final String DISPLAY_END_MARK = "]";
+    private static final String DISPLAY_SEPARATE_MARK = ", ";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,6 +18,24 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public Iterator<Integer> getIterator() {
+        return numbers.iterator();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder formattedLottoNumbers = new StringBuilder();
+        formattedLottoNumbers.append(DISPLAY_BEGIN_MARK);
+        for (int index = 0; index < numbers.size(); index++) {
+            formattedLottoNumbers.append(numbers.get(index));
+            if (index != numbers.size() - 1) {
+                formattedLottoNumbers.append(DISPLAY_SEPARATE_MARK);
+            }
+        }
+        formattedLottoNumbers.append(DISPLAY_END_MARK);
+        return formattedLottoNumbers.toString();
     }
 
     // TODO: 추가 기능 구현

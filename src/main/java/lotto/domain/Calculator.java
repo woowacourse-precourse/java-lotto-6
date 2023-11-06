82e0013e991825438da5cea4e0f9659e18c63063
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 22:29:20 +0900

    refactor: calculateRankCount()를 Calculator로 이동

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Calculator {
+    Map<Rank, Integer> rankCount = new HashMap<>();
+
     public static int calculateTotalPrize(Map<Rank, Integer> rankCount) {
         int totalPrize = 0;
         for (Rank rank : Rank.values()) {
@@ -13,5 +17,21 @@ public class Calculator {
 
     public double calculateEarningRate(int totalPrize, Money money) {
         return ((double) totalPrize / money.getMoney()) * 100;
+    }
+
+    public Map<Rank, Integer> calculateRankCount(List<Lotto> userLottos, WinningNumbers winningNumbers,
+                                                 BonusNumber bonusNumber) {
+        for (Lotto lotto : userLottos) {
+            int countOfMatch = 0;
+            for (Integer number : lotto.getNumbers()) {
+                if (winningNumbers.contains(number)) {
+                    countOfMatch++;
+                }
+            }
+            boolean matchBonus = lotto.getNumbers().contains(bonusNumber.getBonusNumber());
+            Rank rank = Rank.calculateRank(countOfMatch, matchBonus);
+            rankCount.put(rank, rankCount.getOrDefault(rank, 0) + 1);
+        }
+        return rankCount;
     }
 }

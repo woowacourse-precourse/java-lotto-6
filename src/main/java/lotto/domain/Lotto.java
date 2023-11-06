3529cccfdd842649796857c0be45742c57a268df
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 23:55:20 +0900

    refactor: Lotto 내 메소드 리팩토링 메소드 분리, 메소드명 변경하였음

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,22 +15,26 @@ public class Lotto {
     }
 
     private void validateLotto(List<Integer> numbers) {
-        validateCount(numbers);
-        validateRange(numbers);
-        validateUniqueNumbers(numbers);
+        validateLottoCount(numbers);
+        validateLottoRange(numbers);
+        validateLottoUniqueNumbers(numbers);
     }
 
-    private void validateCount(List<Integer> numbers) {
+    private void validateLottoCount(List<Integer> numbers) {
         if (numbers.size() != Constants.LOTTO_NUMBER_COUNTS) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_COUNT_INVALID_ERROR.getMessage());
         }
     }
 
-    private void validateRange(List<Integer> numbers) {
+    private void validateLottoRange(List<Integer> numbers) {
         for (int number : numbers) {
-            if (!isWithinRange(number)) {
-                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_INVALID_ERROR.getMessage());
-            }
+            validateRange(number);
+        }
+    }
+
+    private void validateRange(int number) {
+        if (!isWithinRange(number)) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_INVALID_ERROR.getMessage());
         }
     }
 
@@ -38,8 +42,12 @@ public class Lotto {
         return number >= Constants.LOTTO_MIN_NUMBER && number <= Constants.LOTTO_MAX_NUMBER;
     }
 
-    private void validateUniqueNumbers(List<Integer> numbers) {
+    private void validateLottoUniqueNumbers(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        validateDuplication(uniqueNumbers, numbers);
+    }
+
+    private void validateDuplication(Set<Integer> uniqueNumbers, List<Integer> numbers) {
         if (uniqueNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_DUPLICATION_ERROR.getMessage());
         }

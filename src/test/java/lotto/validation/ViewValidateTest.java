0c1af0b2a3c0529ac74872943fe627d85db656c4
gunwoong <gwj0421@gmail.com>
2023-11-06 20:33:00 +0900

    test : 공백 및 빈 문자열일 경우 검증

---
+++ src/test/java/lotto/validation/ViewValidateTest.java
@@ -0,0 +1,25 @@
+package lotto.validation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ViewValidateTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void validateSpaceOrEmpty() {
+        // given
+        String spaceContent = " ";
+        String nothingContent = "";
+        String content = "123";
+
+        // when & then
+        assertThatThrownBy(() -> ViewValidate.validateNotSpaceOrEmpty(spaceContent))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
+        assertThatThrownBy(() -> ViewValidate.validateNotSpaceOrEmpty(nothingContent))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
+        assertThatCode(() -> ViewValidate.validateNotSpaceOrEmpty(content)).doesNotThrowAnyException();
+    }
+}

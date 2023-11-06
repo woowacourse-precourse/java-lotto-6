79dbdf9f35d93a27f76aa7f236e45c900d99fde4
koomchang <leahpar0401@gmail.com>
2023-11-07 02:49:08 +0900

    refactor: OutputView에 있던 로또 숫자 일치 갯수와 관련된 문자열을 Rank 열거형으로 옮긴 후 리팩토링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,17 +5,18 @@ import java.util.Map;
 import lotto.model.Rank;
 
 public class OutputView {
-    private static final String NEW_LINE = "\n";
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String COMMA = ", ";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_BRACKET = "]";
     private static final String ASK_FOR_MONEY = "구입금액을 입력해 주세요.";
     private static final String ASK_FOR_WINNING_NUMBERS = NEW_LINE + "당첨 번호를 입력해 주세요.";
     private static final String PURCHASED_LOTTO_COUNT = NEW_LINE + "%d개를 구매했습니다.";
     private static final String ASK_FOR_BONUS_NUMBER = NEW_LINE + "보너스 번호를 입력해 주세요.";
-    private static final String THREE_MATCH = "3개 일치 (5,000원) - %d개";
-    private static final String FOUR_MATCH = "4개 일치 (50,000원) - %d개";
-    private static final String FIVE_MATCH = "5개 일치 (1,500,000원) - %d개";
-    private static final String FIVE_MATCH_WITH_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
-    private static final String SIX_MATCH = "6개 일치 (2,000,000,000원) - %d개";
+    private static final String WINNING_STATISTICS = NEW_LINE + "당첨 통계";
+    private static final String WINNING_STATISTICS_HEADER = "---";
     private static final String TOTAL_PROFIT = "총 수익률은 %.1f%%입니다.";
+    private static final int DEFAULT_RESULT = 0;
 
 
     public void askForMoney() {
@@ -23,21 +24,21 @@ public class OutputView {
     }
 
     public void printLottoCount(int lottoCount) {
-        System.out.println(String.format(PURCHASED_LOTTO_COUNT, lottoCount));
+        System.out.printf(PURCHASED_LOTTO_COUNT + NEW_LINE, lottoCount);
     }
 
     public void printLottoNumbers(List<Integer> numbers) {
-        System.out.print("[");
+        System.out.print(OPEN_BRACKET);
         for (int i = 0; i < numbers.size(); i++) {
             System.out.print(numbers.get(i));
             printCommaBetweenNumbers(numbers, i);
         }
-        System.out.println("]");
+        System.out.println(CLOSE_BRACKET);
     }
 
     private void printCommaBetweenNumbers(List<Integer> numbers, int i) {
         if (i != numbers.size() - 1) {
-            System.out.print(", ");
+            System.out.print(COMMA);
         }
     }
 
@@ -50,16 +51,21 @@ public class OutputView {
     }
 
     public void printWinningStatistics(Map<Rank, Integer> result) {
-        System.out.println(NEW_LINE + "당첨 통계");
-        System.out.println("---");
-        System.out.println(String.format(THREE_MATCH, result.getOrDefault(Rank.FIFTH, 0)));
-        System.out.println(String.format(FOUR_MATCH, result.getOrDefault(Rank.FOURTH, 0)));
-        System.out.println(String.format(FIVE_MATCH, result.getOrDefault(Rank.THIRD, 0)));
-        System.out.println(String.format(FIVE_MATCH_WITH_BONUS, result.getOrDefault(Rank.SECOND, 0)));
-        System.out.println(String.format(SIX_MATCH, result.getOrDefault(Rank.FIRST, 0)));
+        System.out.println(WINNING_STATISTICS);
+        System.out.println(WINNING_STATISTICS_HEADER);
+        for (Rank rank : Rank.values()) {
+            if (isMissRank(rank)) {
+                continue;
+            }
+            System.out.printf(rank.getMessage() + NEW_LINE, result.getOrDefault(rank, DEFAULT_RESULT));
+        }
+    }
+
+    private boolean isMissRank(Rank rank) {
+        return rank == Rank.MISS;
     }
 
     public void printTotalProfit(double profit) {
-        System.out.println(String.format(TOTAL_PROFIT, profit));
+        System.out.printf((TOTAL_PROFIT) + NEW_LINE, profit);
     }
 }

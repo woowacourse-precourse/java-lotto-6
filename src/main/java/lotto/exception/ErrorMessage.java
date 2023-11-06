c297e91e8369492000f526ff3d2182abeada290a
JoonKyo <joonkyolee@gmail.com>
2023-11-06 23:00:34 +0900

    feat(ErrorMessage): 로또 번호에 대한 예외 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.exception;
 
 import static lotto.constraint.PurchaseAmountConstraint.MINIMUM_PURCHASE_UNIT_AMOUNT;
+import static lotto.constraint.LottoConstraint.TOTAL_NUMBERS_OF_LOTTO;
 
 public enum ErrorMessage {
     INPUT_IS_NULL("null 값이 입력 되었습니다."),
@@ -10,7 +11,12 @@ public enum ErrorMessage {
     PURCHASE_AMOUNT_IS_NOT_NUMBER("구입금액이 수가 아닙니다."),
     PURCHASE_AMOUNT_IS_NEGATIVE_NUMBER("구입금액에 음수 값이 포함되어 있습니다."),
     PURCHASE_AMOUNT_IS_LESS_THAN_MINIMUM_AMOUNT(String.format("구입금액이 %d원보다 적은 금액입니다.", MINIMUM_PURCHASE_UNIT_AMOUNT.getValue())),
-    PURCHASE_AMOUNT_IS_NOT_DIVIDED_UNIT_AMOUNT(String.format("구입금액이 %d원으로 나눠지지 않습니다." , MINIMUM_PURCHASE_UNIT_AMOUNT.getValue()));
+    PURCHASE_AMOUNT_IS_NOT_DIVIDED_UNIT_AMOUNT(String.format("구입금액이 %d원으로 나눠지지 않습니다." , MINIMUM_PURCHASE_UNIT_AMOUNT.getValue())),
+
+    LOTTO_NUMBER_HAS_NOT_NUMBER_FORMAT("로또 번호 중 수가 아닌 값이 존재합니다."),
+    LOTTO_NUMBER_HAS_OUT_OF_BOUND_NUMBER("범위를 벗어난 로또 번호가 존재합니다."),
+    LOTTO_NUMBER_HAS_DUPLICATED_NUMBER("로또 번호 중 중복된 값이 존재합니다."),
+    LOTTO_SIZE_IS_NOT_VALID(String.format("총 로또 번호의 개수가 %d가 아닙니다.", TOTAL_NUMBERS_OF_LOTTO.getValue()));
 
     private static final String ERROR_HEAD = "[ERROR] ";
     private final String errorMessage;

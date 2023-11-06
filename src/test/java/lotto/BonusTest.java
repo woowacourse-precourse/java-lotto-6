1c5056c4f768f99b499ea98320aad60ef9f520cf
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-06 17:26:37 +0900

    test(BonusTest): 보너스 숫자가 int타입을 벗어날 때의 테스트 추가

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -8,12 +8,20 @@ import org.junit.jupiter.api.Test;
 
 public class BonusTest {
 
-    @DisplayName("보너스 숫자가 숫자인지 테스트")
+    @DisplayName("보너스 숫자가 문자면 예외 테스트")
     @Test
-    void isNumber() {
+    void isNumber1() {
         assertThatThrownBy(() -> new Bonus("a"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 보너스 번호는 숫자로만 입력해야 합니다.");
+                .hasMessage("[ERROR] 보너스 번호는 1~45사이의 숫자로만 입력해야 합니다.");
+    }
+
+    @DisplayName("보너스 숫자가 int타입을 벗어나면 예외 테스트")
+    @Test
+    void isNumber2() {
+        assertThatThrownBy(() -> new Bonus("10000000000000000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1~45사이의 숫자로만 입력해야 합니다.");
     }
 
     @DisplayName("보너스 숫자가 1~45인지 테스트")

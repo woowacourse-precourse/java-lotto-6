474917bbda80c9cad882a1341273f9a8ed1c6ab3
jongmee <ra123221@gmail.com>
2023-11-06 22:08:23 +0900

    refactor(Rank): 등수 Enum의 순서를 바꾸고 멤버 변수의 getter를 추가하기
    - WHY: getter를 지양하고 있지만 등수 enum의 멤버 변수는 서비스 로직에서 사용하기 보다 출력 시 사용된다. 코드를 컴팩트하게 작성하기 위해 getter를 사용했다.

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(2000000000L, 6, false),
-    SECOND(30000000L, 5, true),
-    THIRD(1500000L, 5, false),
-    FOURTH(50000L, 4, false),
+    UNRANKED(0L, 0, false),
     FIFTH(5000L, 3, false),
-    UNRANKED(0L, 0, false);
+    FOURTH(50000L, 4, false),
+    THIRD(1500000L, 5, false),
+    SECOND(30000000L, 5, true),
+    FIRST(2000000000L, 6, false);
 
     private Long price;
     private Integer criterion;
@@ -34,5 +34,17 @@ public enum Rank {
 
     private boolean checkBonusRequiring(boolean requireBonus) {
         return this.requireBonus == requireBonus;
+    }
+
+    public Integer getCriterion() {
+        return criterion;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public boolean requireBonusNumber() {
+        return requireBonus;
     }
 }

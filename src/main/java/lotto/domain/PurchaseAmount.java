4ba633b8cb7e77f3219f9af6b22163bd356de7d8
kangyuseok <kingyuseok@naver.com>
2023-11-07 01:40:58 +0900

    Feat: 구입 금액의 일급클래스 적용

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import static lotto.service.Validator.validateDividedBy1000Won;
+import static lotto.service.Validator.validateLess1000Won;
+import static lotto.service.Validator.validateNumber;
+
+public class PurchaseAmount {
+    private final String purchaseAmount;
+
+    public PurchaseAmount(String purchaseAmount){
+        validateNumber(purchaseAmount);
+        validateLess1000Won(purchaseAmount);
+        validateDividedBy1000Won(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+}

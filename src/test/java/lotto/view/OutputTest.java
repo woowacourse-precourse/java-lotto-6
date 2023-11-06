dde3b8c6ff57441cf60e525a7e4b6cfa025a6a4f
bin <gimgyubin1225@gmail.com>
2023-11-07 01:27:05 +0900

    test: 출력값을 제대로 테스트하지 못하던 오류 해결
    테스트 구조상 출력값을 제대로 테스트하지 못함
    input stream에 기대값을 넣고 출력값을 확인하기 때문
    trim으로 crlf, lf 의 구분으로 인해 테스트가 실패하는 것을 방지함

--- src/test/java/lotto/view/OutputTest.java
+++ src/test/java/lotto/view/OutputTest.java
@@ -1,8 +1,7 @@
 package lotto.view;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
@@ -16,49 +15,46 @@ public class OutputTest {
 
     private static ByteArrayOutputStream outputMessage;
 
-
-    protected void systemIn(String input){
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-    }
-    protected String getOutput(){
+    protected String getOutput() {
         return outputMessage.toString();
     }
+
     @BeforeEach
-    void setUpStreams(){
+    void setUpStreams() {
         outputMessage = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputMessage));
     }
 
     @AfterEach
-    void restoresStreams(){
+    void restoresStreams() {
         System.setOut(System.out);
     }
 
     @Test
     @DisplayName("금액 입력 메세지 출력")
-    void printWinningStatistics(){
-        List<Integer> winningStatistics = new ArrayList<>(List.of(0,0,0,0,1));
+    void printWinningStatistics() {
+        List<Integer> winningStatistics = new ArrayList<>(List.of(0, 0, 0, 0, 1));
         String expectedMessage = "당첨 통계\n"
                 + "---\n"
                 + "3개 일치 (5,000원) - 1개\n"
                 + "4개 일치 (50,000원) - 0개\n"
                 + "5개 일치 (1,500,000원) - 0개\n"
                 + "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n"
-                + "6개 일치 (2,000,000,000원) - 0개\n";
+                + "6개 일치 (2,000,000,000원) - 0개";
 
-        systemIn(expectedMessage);
         Output.printWinningStatistics(winningStatistics);
 
-        assertThat(getOutput().contains(expectedMessage));    }
+        assertEquals(expectedMessage, getOutput().trim());
+    }
 
     @Test
     @DisplayName("총 수익률 출력")
-    void printTotalReturn(){
+    void printTotalReturn() {
         double totalReturn = 62.5;
-        String expectedMessage ="총 수익률은 62.5%입니다.\n";
-        systemIn(expectedMessage);
+        String expectedMessage = "총 수익률은 62.5%입니다.";
+
         Output.printTotalReturn(totalReturn);
 
-        assertThat(getOutput().contains(expectedMessage));
+        assertEquals(expectedMessage, getOutput().trim());
     }
 }

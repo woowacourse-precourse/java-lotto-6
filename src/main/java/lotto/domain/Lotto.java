1a7d5c4d8e0f6eff7c9fa3c22d9245d3fae5c944
opixxx <opix0306@naver.com>
2023-11-06 19:32:17 +0900

    refactor(Lotto): 로또 번호가 6개가 아닐때 출력할 메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -18,7 +18,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 입력해야 합니다.");
         }
     }
 

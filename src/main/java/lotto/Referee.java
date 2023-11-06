8130be490751dfb4e384dcf51d109d8ebacabee0
zzawang <hobin0116@naver.com>
2023-11-06 19:36:59 +0900

    feat: 로또 결과 계산 기능 추가
    - 로또 당첨번호와 보너스 번호 일치 여부에 따라 1 ~ 5 등을 결정한다.

---
+++ src/main/java/lotto/Referee.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Referee {
+    private static Rank calculatePrize(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
+        Set<Integer> userNumbers = new HashSet<>(lotto.getNumbers());
+        Set<Integer> winningNumberSet = new HashSet<>(winningNumbers);
+
+        boolean hasBonusNumber = userNumbers.contains(bonusNumber);
+
+        long matchingNumbers = countMatchingNumbers(userNumbers, winningNumberSet);
+
+        return calculatePrizeBasedOnMatching(matchingNumbers, hasBonusNumber);
+    }
+
+    private static Rank calculatePrizeBasedOnMatching(long matchingNumbers, boolean hasBonusNumber) {
+        if (matchingNumbers == Rank.FIRST.getMatchCount() && !hasBonusNumber) {
+            return Rank.FIRST;
+        } else if (matchingNumbers == Rank.SECOND.getMatchCount() && hasBonusNumber) {
+            return Rank.SECOND;
+        } else if (matchingNumbers == Rank.THIRD.getMatchCount()) {
+            return Rank.THIRD;
+        } else if (matchingNumbers == Rank.FOURTH.getMatchCount()) {
+            return Rank.FOURTH;
+        } else if (matchingNumbers == Rank.FIFTH.getMatchCount()) {
+            return Rank.FIFTH;
+        }
+        return Rank.FAIL;
+    }
+
+    private static long countMatchingNumbers(Set<Integer> userNumbers, Set<Integer> winningNumberSet) {
+        return userNumbers.stream()
+                .filter(winningNumberSet::contains)
+                .count();
+    }
+}

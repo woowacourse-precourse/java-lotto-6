2ddf98aa68310081ae2c1bb26a59203d61d52284
psrom <psrom721@gmail.com>
2023-11-07 01:57:49 +0900

    feat(winning number test): 당첨 번호 입력 기능 테스트

--- src/main/java/lotto/input/WinningNumbers.java
+++ src/main/java/lotto/input/WinningNumbers.java
@@ -25,26 +25,36 @@ public class WinningNumbers {
         return winningNumbers;
     }
 
-    private static void validateInput(String[] inputNumbers, Set<Integer> winningNumbers) {
+    protected static void validateInput(String[] inputNumbers, Set<Integer> winningNumbers) {
         if (inputNumbers.length != 6) {
             throw new IllegalArgumentException("[ERROR] 숫자 6개를 입력해주세요.");
         }
         for (String number : inputNumbers) {
-            validateNumber(number, winningNumbers);
+            int winningNum = validateNumberFormat(number.trim());
+            validateNumberRange(winningNum);
+            validateDuplicate(winningNum, winningNumbers);
         }
     }
 
-    private static void validateNumber(String number, Set<Integer> winningNumbers) {
+
+    private static int validateNumberFormat(String number) {
         try {
-            int numberInput = Integer.parseInt(number.trim());
-            if (numberInput < 1 || numberInput > 45) {
-                throw new IllegalArgumentException("[ERROR] 1과 45 사이의 숫자 6개를 입력해주세요.");
-            }
-            if (!winningNumbers.add(numberInput)) {
-                throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자 6개를 입력해주세요.");
-            }
+            return Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력이 있습니다. 숫자 6개를 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력이 있습니다.");
         }
     }
+
+    private static void validateNumberRange(int numberInput) {
+        if (numberInput < 1 || numberInput > 45) {
+            throw new IllegalArgumentException("[ERROR] 1과 45 사이의 숫자 6개를 입력해주세요.");
+        }
+    }
+
+    private static void validateDuplicate(int numberInput, Set<Integer> winningNumbers) {
+        if (!winningNumbers.add(numberInput)) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자 6개를 입력해주세요.");
+        }
+    }
+
 }

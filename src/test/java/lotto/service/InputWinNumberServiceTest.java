a2563bf56c883accf04c88b72f69d87ec266354b
김효준 <khyojun1104@gmail.com>
2023-11-06 19:52:48 +0900

    test(lotto): , 가 올바르게 들어갔는지 확인하는 작업! 첫 번째! 기능 테스트 구현!

--- src/test/java/lotto/service/InputWinNumberServiceTest.java
+++ src/test/java/lotto/service/InputWinNumberServiceTest.java
@@ -1,20 +1,55 @@
 package lotto.service;
 
+import static lotto.constant.Delimiter.COMMA;
+import static lotto.constant.ErrorMessage.MONEY_ONLY_NUMBER;
+import static lotto.constant.ErrorMessage.WINNUMBER_COMMA_DELIMITER;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.util.Validator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputWinNumberServiceTest {
 
+    Validator validator;
+    @BeforeEach
+    void init(){
+        validator = new Validator();
+    }
+
     @Test
     void checkRightWinnerNumbers() {
+
+
+//        List<String> splitedInputWinNumbers = List.of(
+//            inputWinnerNumbers.split(COMMA.getDelimiter()));
+//        validator.validateRightCommaCount(inputWinnerNumbers, splitedInputWinNumbers);
+//        validator.validateWinnumberOnlyNumber(splitedInputWinNumbers);
+//        winNumbers = splitedInputWinNumbers.stream().map(Integer::parseInt)
+//            .collect(Collectors.toList());
     }
 
-    @Test
-    void convertedWinnerNumbers() {
+
+    @ParameterizedTest
+    @ValueSource(strings = {",,,", "1,2,3,"})
+    @DisplayName("당첨 번호 입력에 올바르게 ,갯수 확인! split할 때 추가적인 경우 확인을 위함!")
+    void validateRightCommaCount(String inputWinNumbers) {
+        //given
+        List<String> splitedInputWinNumbers = List.of(
+            inputWinNumbers.split(COMMA.getDelimiter()));
+
+        Assertions.assertThatThrownBy(() -> validator.validateRightCommaCount(inputWinNumbers, splitedInputWinNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(WINNUMBER_COMMA_DELIMITER.getMessage());
     }
 
-    @Test
-    void init() {
-    }
+
+
+
 }

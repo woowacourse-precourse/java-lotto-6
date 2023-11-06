d8e6d75db15c4eccd30432cd7d5cf1d679056371
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-06 13:10:42 +0900

    feat: 로또 당첨번호 및 보너스 번호 입력 기능 추가 1. 로또 당첨번호 빛 보너스 번호를 입력받는 로직 추가 2. 입력값에 대한 유효값 평가 함수 추가 3. 상황에 맟는 에러 메세지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,25 +12,79 @@ public class Application {
     private static final int LOTTO_TEST_PURCHASE = 10000;
 
 
-
-
     public static void main(String[] args) {
         int numberOfPurchasedLottos = LOTTO_TEST_PURCHASE/LOTTO_PRICE;
+
+        List<Lotto> purchasedLottos = new ArrayList<>();
 
         for (int i = 0; i < numberOfPurchasedLottos; i++){
             Lotto lotto = Lotto.generate();
             List<Integer> numbers = lotto.getNumbers();
+            purchasedLottos.add(lotto);
             System.out.println(numbers);
         }
-        System.out.println("당첨 번호를 쉼표로 구분하여 입력하세요.");
-        String winningLottoNumbersInput = Console.readLine();
+
         List<Integer> winningNumbers = new ArrayList<>();
-        for (String s : winningLottoNumbersInput.split(",")) {
-            String trim = s.trim();
-            Integer parseInt = Integer.parseInt(trim);
-            winningNumbers.add(parseInt);
+        int bonusNumber = 0;
+        boolean isValidInput = false;
+
+        while (!isValidInput) {
+            try {
+                System.out.println("당첨 번호를 쉼표로 구분하여 입력하세요.");
+                String winningLottoNumbersInput = Console.readLine();
+
+                winningNumbers = Arrays.stream(winningLottoNumbersInput.split(","))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+                validateWinningNumbers(winningNumbers);
+                isValidInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 입력값은 숫자로만 구성되어야 합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        System.out.println("입력된 당첨 번호: " + winningNumbers);
+
+        isValidInput = false; // 보너스 번호에 대해 다시 false로 설정
+
+        // 보너스 번호를 올바르게 입력할 때까지 반복
+        while (!isValidInput) {
+            try {
+                System.out.println("보너스 번호를 입력하세요.");
+                bonusNumber = Integer.parseInt(Console.readLine().trim());
+
+                validateBonusNumber(bonusNumber, winningNumbers);
+                isValidInput = true; // 유효성 검사 통과 시
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 입력값은 숫자로만 구성되어야 합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println("입력된 당첨 번호: " + winningNumbers + " + 보너스 번호: " + bonusNumber);
+        // 여기서 당첨 번호, 보너스 번호와 구매한 로또 번호들을 비교하는 로직을 구현할 수 있습니다.
     }
 
+    private static void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (winningNumbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (winningNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+    private static void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
 }

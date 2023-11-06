bc18c35a20f50fb636d007f22d2da112bb38c7df
sihyunjojo <si4018@naver.com>
2023-11-06 16:50:52 +0900

    refactor: END_RANGE 변경

--- src/main/java/lotto/generator/RandomNumberGenerator.java
+++ src/main/java/lotto/generator/RandomNumberGenerator.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class RandomNumberGenerator {
     private static final int STARTING_RANGE = 1;
-    private static final int END_RANGE = 9;
+    private static final int END_RANGE = 45;
     private static final int COUNT = 6;
 
     public static List<Integer> generateUniqueRandomValues(){

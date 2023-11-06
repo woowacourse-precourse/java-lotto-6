7e59488634fb01c95b1a9b0db86d3e6359de4267
hongseokhodev <hongseokhodev@gmail.com>
2023-11-06 22:56:24 +0900

    feat: 예외 상황 시 [ERROR]로 시작되는 메시지를 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,11 +8,15 @@ public class OutputView {
     }
     public void printStats(String description, int amount, int number) {
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.println(description + " (" + decimalFormat.format(amount) + "원) - " + number + "개");
+        System.out.println(description + " (" + decimalFormat.format(amount) + "원) - " + number + "다");
     }
 
     public void printRateOfReturn(double rateOfReturn) {
         DecimalFormat decimalFormat = new DecimalFormat("#.0");
         System.out.println("총 수익률은 " + decimalFormat.format(rateOfReturn) + "%입니다.");
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
     }
 }

7e05ec433c31559da624698cf588e20edac0e93f
kseenyoung <kseenyoung@gmail.com>
2023-11-06 18:21:36 +0900

    feat: 사용자가 가진 돈 입력 예외처리

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,4 +2,28 @@ package lotto;
 
 public class User {
 
+    private int money;
+    private int canGetTickets;
+
+    User(int money){
+        this.money = money;
+        while(true){
+            try{
+
+            } catch (IllegalArgumentException e){
+
+            }
+        }
+    }
+
+    /**
+     * 천원단위로 나눠 떨어지지 않을 때 예외처리
+     * */
+    private int canGetTickets(int money){
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException();
+        }
+        return money / 1000;
+    }
+
 }

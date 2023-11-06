6944e1029ff91ee70eb11a18b4f442ffa8402cfb
김민정 <mjeong@sookmyung.ac.kr>
2023-11-06 16:07:34 +0900

    refactor: InputView 클래스 이동

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,11 @@
-package lotto;
+package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
-    public static int getAmount() {
+    public static int getMoney() {
         while (true) {
             try {
                 System.out.println("구입금액을 입력해 주세요.");
@@ -20,6 +20,7 @@ public class InputView {
     public static List<Integer> getWinningNumber() {
         while (true) {
             try {
+                System.out.println();
                 System.out.println("당첨 번호를 입력해 주세요.");
                 String input = Console.readLine();
                 return convertStringToList(input);
@@ -31,18 +32,16 @@ public class InputView {
 
     private static List<Integer> convertStringToList(String input) {
         String[] splitValues = input.split(",");
-        List<Integer> numbers = new ArrayList<>();
-        for (String value : splitValues) {
-            String trimmedValue = value.trim();
-            int number = Integer.parseInt(trimmedValue);
-            numbers.add(number);
-        }
-        return numbers;
+        return Arrays.stream(splitValues)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
     }
 
     public static int getBonusNumber() {
         while (true) {
             try {
+                System.out.println();
                 System.out.println("보너스 번호를 입력해 주세요.");
                 String input = Console.readLine();
                 return Integer.parseInt(input);

15df79c495cda40e262d8f3afc08c6380fb2afcc
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-06 17:36:22 +0900

    Feat: implementing lotto validate

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,10 +14,19 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(); // 로또 번호는 6자리여야 합니다.
         }
 
-    }
+        for (Integer number : numbers) {
+            if (!(number >= 1 && number <= 45)) {// if 입력된 숫자 1 ~ 45 사이 아닐 때 {
+            throw new IllegalArgumentException(); // 로또 번호는 1부터 45 사이의 숫자여야 합니다.
+            }
+        }
 
-    // TODO: 추가 기능 구현
+        Set<Integer> uniqueNumber = new HashSet<>(numbers);
+
+        if (uniqueNumber.size() != numbers.size()) {
+            throw new IllegalArgumentException(); // 입력된 로또 번호에는 중복이 존재하지 않아야 합니다.
+        }
+    }
 }

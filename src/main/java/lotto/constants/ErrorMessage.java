0fcd5f52504056295364c087060d2f98a7bee8af
ParkDH0809 <znxn1237@gmail.com>
2023-11-06 22:58:07 +0900

    feat: 에러 메시지 목록 이름 변경 및 내용 추가

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -1,17 +1,20 @@
 package lotto.constants;
 
 public enum ErrorMessage {
-    ERROR_IS_NOT_NUMBER("올바른 입력을 하지 않았습니다."),
-    ERROR_CANT_DIVIDED_BY_LOTTO_PRICE("올바른 금액을 입력하지 않았습니다."),
-    ERROR_IS_NOT_A_VALID_FORMAT("올바른 형식에 맞춰 입력하지 않았습니다."),
-    ERROR_IS_NOT_INTERVAL_VALUE("숫자 범위를 벗어난 입력을 하였습니다.");
+    ERROR_INVALID_NUMBER("올바를 숫자를 입력하지 않았습니다."),
+    ERROR_INVALID_AMOUNT("올바른 금액을 입력하지 않았습니다."),
+    ERROR_INVALID_FORMAT("올바른 형식에 맞춰 입력하지 않았습니다."),
+    ERROR_OUT_OF_RANGE("숫자 범위를 벗어난 입력을 하였습니다."),
+    ERROR_DUPLICATE_NUMBER("중복된 숫자를 입력하였습니다.");
 
+    private static final String PREFIX = "[ERROR] ";
     private final String message;
+
     ErrorMessage(String message) {
         this.message = message;
     }
 
     public String getMessage() {
-        return "[ERROR] " + message;
+        return PREFIX + message;
     }
 }

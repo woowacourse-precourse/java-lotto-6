2670e7733514f79f5dd00eaa63e51856f242d6bd
hongseokhodev <hongseokhodev@gmail.com>
2023-11-06 23:12:36 +0900

    feat: 당첨 번호를 입력받는다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,26 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     public int readAmount() {
         String input = Console.readLine();
         try {
             return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> readWinningNumbers() {
+        String input = Console.readLine();
+        String[] split = input.split(",");
+        try {
+            return Arrays.stream(split)
+                    .map(Integer::parseInt)
+                    .toList();
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }

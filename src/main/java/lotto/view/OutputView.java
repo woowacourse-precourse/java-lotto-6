ffb3c5eb2ac31e71c9921dbe8eb348b3b3bf43bf
HSBODo <vinsulill@gmaile.com>
2023-11-06 23:38:07 +0900

    refactor : lottoResultDisplay 메서드에서 LottoResultModel 받아서 UI 구성하도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.model.LottoResultModel;
 
 import java.util.List;
 import java.util.Map;
@@ -20,17 +21,15 @@ public class OutputView {
         System.out.println("");
     }
 
-    public void lottoResultDisplay(Map<String,String> lottoWinningResult){
+    public void lottoResultDisplay(LottoResultModel lottoResultModel){
         System.out.println("");
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + lottoWinningResult.get("3개일치")+"개");
-        System.out.println("4개 일치 (50,000원) - " + lottoWinningResult.get("4개일치")+"개");
-        System.out.println("5개 일치 (1,500,000원) - " + lottoWinningResult.get("5개일치")+"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoWinningResult.get("5개일치_보너스일치")+"개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + lottoWinningResult.get("6개일치")+"개");
-        System.out.println("총 수익률은 "+  lottoWinningResult.get("총 수익률") +"%입니다.");
+        System.out.println("3개 일치 (5,000원) - " + lottoResultModel.getThreeMatch()+"개");
+        System.out.println("4개 일치 (50,000원) - " + lottoResultModel.getFourMatch()+"개");
+        System.out.println("5개 일치 (1,500,000원) - " + lottoResultModel.getFiveMatch()+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResultModel.getFiveBonusMatch()+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResultModel.getSixMatch()+"개");
+        System.out.println("총 수익률은 "+  lottoResultModel.getTotalReturnRate() +"%입니다.");
     }
-
-
 }

0c0d49730d1e9bcd47fd207faaa484f6aec37749
dmong <dt2980_hee@naver.com>
2023-11-06 13:56:12 +0900

    feat: LottoNumber 작성

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
 public class LottoNumber {
+    private final int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber create(int number) {
+        return new LottoNumber(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }

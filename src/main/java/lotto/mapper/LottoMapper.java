0276c38c899117a7efc9e7e995bf0a2b78eda03a
minisyu <iswater.kim@gmail.com>
2023-11-06 15:52:37 +0900

    refactor: LottoMapper에 당첨 번호 및 보너스 번호 변환 기능과 당첨 결과를 Dto로 변환하는 기능 추가

--- src/main/java/lotto/mapper/LottoMapper.java
+++ src/main/java/lotto/mapper/LottoMapper.java
@@ -1,9 +1,17 @@
 package lotto.mapper;
 
+import java.util.EnumMap;
 import java.util.List;
+import lotto.domain.draw.DrawingResult;
+import lotto.domain.lotto.BonusNumber;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoRank;
 import lotto.domain.lotto.Lottos;
+import lotto.domain.lotto.WinningCombination;
 import lotto.domain.money.Money;
 import lotto.dto.input.MoneyDto;
+import lotto.dto.input.WinningCombinationDto;
+import lotto.dto.output.DrawingResultDto;
 import lotto.dto.output.LottoDto;
 import lotto.dto.output.LottosDto;
 
@@ -19,5 +27,25 @@ public class LottoMapper {
                 .map(LottoDto::toLottoDto)
                 .toList();
         return new LottosDto(lottosDto);
+    }
+
+    public static WinningCombination mapFrom(WinningCombinationDto winningCombinationDto) {
+        List<Integer> numbers = winningCombinationDto.winningNumbers();
+        Lotto winningNumbers = Lotto.from(numbers);
+        int number = winningCombinationDto.bonusNumber();
+        BonusNumber bonusNumber = BonusNumber.from(number);
+        return WinningCombination.of(winningNumbers, bonusNumber);
+    }
+
+    public static DrawingResultDto mapFrom(DrawingResult drawingResult) {
+        EnumMap<LottoRank, Integer> result = drawingResult.getDrawingResult();
+        return new DrawingResultDto(
+                result.get(LottoRank.FIRST),
+                result.get(LottoRank.SECOND),
+                result.get(LottoRank.THIRD),
+                result.get(LottoRank.FOURTH),
+                result.get(LottoRank.FIFTH),
+                drawingResult.calculateProfitRate()
+        );
     }
 }

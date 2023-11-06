7cab0ca66acdb83b5453dc6b2ac9258080d0e8e9
jang010505 <jang010505@inu.ac.kr>
2023-11-07 06:58:29 +0900

    feat: InputUtil 내부 코드 작성

--- src/main/java/lotto/utils/InputUtil.java
+++ src/main/java/lotto/utils/InputUtil.java
@@ -1,15 +1,64 @@
 package lotto.utils;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.WinningLotto;
+import lotto.validator.InputValidator;
+import lotto.view.ErrorView;
+import lotto.view.InputView;
 
 public class InputUtil {
-    public static int inputLottoPrice() {
+    private static String input() {
+        return Console.readLine();
+    }
 
-        return 0;
+    public static int inputLottoPrice() {
+        InputView.inputLottoPriceMessage();
+
+        try {
+            String price = input();
+            return InputValidator.validateLottoPriceInput(price);
+        } catch (IllegalArgumentException e) {
+            ErrorView.printErrorMessage(e.getMessage());
+            return inputLottoPrice();
+        }
     }
 
     public static WinningLotto inputWinningLotto() {
+        try {
+            Lotto lotto = inputLottoNumber();
+            int bonusNumber = inputBonusNumber();
+            return new WinningLotto(lotto, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            ErrorView.printErrorMessage(e.getMessage());
+            return inputWinningLotto();
+        }
+    }
 
-        return null;
+    private static Lotto inputLottoNumber() {
+        InputView.inputLottoNumber();
+
+        try {
+            String lottoNumber = input();
+            List<Integer> winningLotto = InputValidator.validateLottoNumberInput(lottoNumber);
+            return new Lotto(winningLotto);
+        }
+        catch (IllegalArgumentException e) {
+            ErrorView.printErrorMessage(e.getMessage());
+            return inputLottoNumber();
+        }
+    }
+
+    private static int inputBonusNumber() {
+        InputView.inputBonusNumber();
+
+        try {
+            String bonusNumber = input();
+            return InputValidator.validateBonusNumberInput(bonusNumber);
+        } catch (IllegalArgumentException e) {
+            ErrorView.printErrorMessage(e.getMessage());
+            return inputBonusNumber();
+        }
     }
 }

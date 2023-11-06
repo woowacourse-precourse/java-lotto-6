a8a6762bb460665412bd54ac1b7eb818576d8124
34suuuuu <psh7492@gmail.com>
2023-11-06 12:24:48 +0900

    refactor: lotto package 생성 후 관련 클래스들 이동

--- src/main/java/lotto/domain/lotto/WinningLotto.java
+++ src/main/java/lotto/domain/lotto/WinningLotto.java
@@ -1,29 +1,29 @@
-package lotto.domain;
+package lotto.domain.lotto;
 
+import lotto.domain.rank.Rank;
 import lotto.exception.BonusNumDuplicateException;
 
 public class WinningLotto {
     private final Lotto lotto;
-    private final LottoNumber bonusNumber;
+    private final int bonusNumber;
 
 
-    private WinningLotto(Lotto lotto, LottoNumber bonusNumber){
+    private WinningLotto(Lotto lotto, int bonusNumber){
         validate(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
 
-    public static void validate(Lotto lotto, LottoNumber bonusNumber) {
+    public static void validate(Lotto lotto, int bonusNumber) {
         if (lotto.containBonusNum(bonusNumber)) {
             throw new BonusNumDuplicateException();
         }
     }
 
-    public boolean contains(LottoNumber lottoNum){
-        return lotto.contains(lottoNum);
+    public Rank matchCnt(Lotto playerNumber) {
+        int matchCnt = playerNumber.countSameNum(lotto);
+        boolean bonusMatch = playerNumber.containBonusNum(bonusNumber);
+        return Rank.of(matchCnt, bonusMatch);
     }
 
-    public LottoNumber getBonusNumber(){
-        return bonusNumber;
-    }
 }

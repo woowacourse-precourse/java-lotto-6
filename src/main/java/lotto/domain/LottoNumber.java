5f061052749527300741429e201755ffe75d32e0
inpink <inpink@kakao.com>
2023-11-06 23:53:49 +0900

    feat: 로또번호 정수값, 문자값을 매개변수로 받아 LottoNumber 객체 생성하는 정적 팩터리 메서드

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import static lotto.configuration.IntegerConstants.MAX_NUMBER_RANGE;
+import static lotto.configuration.IntegerConstants.MIN_NUMBER_RANGE;
+import static lotto.messages.ErrorMessages.INVALID_LOTTO_NUMBER_RANGE_MESSAGE;
+
+import lotto.util.ExceptionUtil;
+import lotto.validation.IntegerValidator;
+
+public class LottoNumber {
+    int number;
+
+    private LottoNumber(int number) {
+        validateInRange(number);
+        this.number = number;
+    }
+
+    public static LottoNumber create(int input) {
+        return new LottoNumber(input);
+    }
+
+    public static LottoNumber create(String input) {
+        return new LottoNumber(processStringToInt(input));
+    }
+
+}

a61870c80c02d72bf366156a2936cead1711fd01
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 18:08:42 +0900

    feat(LottoTest.java): add createLottoByNonNumericCharacter test 로또 번호 사용자 입력값에 숫자가 아닌 문자가 있으면 IllegalArgumentException을 발생시키는 기능을 테스트하였습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import Model.Lotto;
+import Model.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +20,14 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 숫자가 아닌 문자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNonNumericCharacter() {
+        assertThatThrownBy(() -> new WinningLotto("1,8,3,21,a,37","42"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

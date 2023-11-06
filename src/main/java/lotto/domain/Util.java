84ff71974eb4e6c27fc69bd9017bc5e77bfc307b
doka <posmane5@gmail.com>
2023-11-06 22:27:18 +0900

    feat(Util) : 콤마를 기준으로 문자열을 List로 바꿔 반환하는 유틸 메소드 추가

--- src/main/java/lotto/domain/Util.java
+++ src/main/java/lotto/domain/Util.java
@@ -1,12 +1,22 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Util {
 
-    public static int toNumeric(String orderInputAmount) {
+    public static int toNumeric(String stringNumber) {
         try {
-            return Integer.parseInt(orderInputAmount);
-        } catch (NumberFormatException ex) {
+            return Integer.parseInt(stringNumber.strip());
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static List<Integer> stringToArrayList(String inputNumbers) {
+        List<Integer> numbers = Arrays.asList(inputNumbers.split(",")).stream().map(Util::toNumeric)
+                .collect(Collectors.toList());
+        return numbers;
     }
 }

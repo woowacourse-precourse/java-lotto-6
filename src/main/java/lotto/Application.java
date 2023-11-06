587bd06402fc9af134edc95b4ad1c4fbe2f54773
이준희 <juni416@naver.com>
2023-11-06 19:31:49 +0900

    feature: 출력 구문 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,11 +8,9 @@ public class Application {
         GameManager gameManager = new GameManager();
         Consumer consumer = new Consumer();
 
+        System.out.println("구입금액을 입력해 주세요.");
         int lottoAmount = gameManager.getPaymentForLottoByRead(consumer);
-
-        List<Integer> winningNumbers = gameManager.getWinningLottoNumberByRead();
-        int bonusNumber = gameManager.getBonusLottoNumberByRead();
-
+        System.out.println();
 
         for (int i = 0; i < lottoAmount; i++) {
             Lotto lotto = new Lotto(gameManager.createRandomNumber());
@@ -21,9 +19,20 @@ public class Application {
 
         System.out.println(lottoAmount + "개를 구매했습니다.");
         consumer.lotto.forEach(lotto -> System.out.println(lotto.getNumbers()));
+        System.out.println();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = gameManager.getWinningLottoNumberByRead();
+        System.out.println();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = gameManager.getBonusLottoNumberByRead();
+        System.out.println();
 
         gameManager.setWinningNumbersAndBonusNumber(winningNumbers, bonusNumber);
 
+        System.out.println("당첨 통계");
+        System.out.println("---");
         gameManager.printWinning(consumer.lotto);
         System.out.println("총 수익률은 " + gameManager.getProfitRate(consumer.lotto, consumer.getPayment()) + "%입니다.");
     }

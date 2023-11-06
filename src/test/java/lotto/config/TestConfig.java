52be1f93a093bb44bf6cf054e68aaa66ab7a8bcc
jinny-l <jinny.l.dev@gmail.com>
2023-11-06 17:10:56 +0900

    ✅test: 입력 기능 테스트 코드 작성

---
+++ src/test/java/lotto/config/TestConfig.java
@@ -0,0 +1,15 @@
+package lotto.config;
+
+public final class TestConfig {
+
+    public static final int LOTTO_PRICE_UNIT = 1000;
+    public static final int MONET_MIN_NUMBER = LOTTO_PRICE_UNIT;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final String LOTTO_NUMBER_INPUT_DELIMITER = ",";
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR]";
+
+    private TestConfig() {
+    }
+}

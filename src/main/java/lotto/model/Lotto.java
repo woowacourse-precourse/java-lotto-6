665496ea5c0ac468e461869d5b7fbd01a1828206
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 15:43:47 +0900

    feat : 중복된 로또번호가 있는지 검증하는 메서드 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,33 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSixNumbers(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private static void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되어서는 안됩니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateSixNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개로 이루어져야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }

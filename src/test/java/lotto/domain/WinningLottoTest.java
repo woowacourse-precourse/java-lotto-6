0d39d2f643bf2225921ac83b41b570a56589fa3f
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 21:14:13 +0900

    refactor: 로또 몇개 맞는지 테스트 코드 리팩토링

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -28,17 +28,18 @@ public class WinningLottoTest {
     @ParameterizedTest
     @DisplayName("구입한 로또가 당첨 로또와 몇개 맞는지 테스트")
     @MethodSource("buyLottos")
-    void buyLottoCompareWinningLotto(Lotto buyLotto, int matchCount) {
-        Assertions.assertThat(winningLotto.matchCount(buyLotto)).isEqualTo(matchCount);
+    void buyLottoCompareWinningLotto(Lotto buyLotto, LottoRank lottoRank) {
+        Assertions.assertThat(winningLotto.lottoResult(buyLotto)).isEqualTo(lottoRank);
     }
 
     static Stream<Arguments> buyLottos() {
         return Stream.of(
-                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 5, 6)), LottoRank.FIRST.getCount()),
-                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 5, 8)), LottoRank.THIRD.getCount()),
-                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 9, 10)), LottoRank.FOURTH.getCount()),
-                Arguments.arguments(new Lotto(List.of(1, 2, 3, 9, 10, 11)), LottoRank.FIFTH.getCount()),
-                Arguments.arguments(new Lotto(List.of(1, 2, 7, 9, 10, 11)), LottoRank.LOSE.getCount())
+                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 5, 6)), LottoRank.FIRST),
+                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 5, 7)), LottoRank.SECOND),
+                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 5, 9)), LottoRank.THIRD),
+                Arguments.arguments(new Lotto(List.of(1, 2, 3, 4, 9, 10)), LottoRank.FOURTH),
+                Arguments.arguments(new Lotto(List.of(1, 2, 3, 9, 10, 11)), LottoRank.FIFTH),
+                Arguments.arguments(new Lotto(List.of(1, 2, 7, 9, 10, 11)), LottoRank.LOSE)
         );
     }
 }

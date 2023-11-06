dad916f935fc82d8c058ca433b5bd2fa8ae427e3
Jinnie Kim <jinniek48@gmail.com>
2023-11-06 21:19:16 +0900

    feat: lottoMachine에 입출력을 담당하는 메서드 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,8 +10,33 @@ import lotto.Exception.MoneyException;
 public class LottoMachine {
 
     private int lottoCount;
+    private List<Lotto> lottoBundle;
 
-    public void insertMoney(String input) {
+    public void insertMoneyView() {
+
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            insertMoney(Console.readLine());
+            System.out.println();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            insertMoneyView();
+        }
+
+    }
+
+    public List<Lotto> lottoBundleView() {
+        printLottoBundle();
+
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        for (Lotto lotto : lottoBundle) {
+            System.out.println(lotto.lottoFormat());
+        }
+
+        return lottoBundle;
+    }
+
+    private void insertMoney(String input) {
         MoneyException.notParsableNumber(input);
         MoneyException.notValidNumber(Integer.parseInt(input));
         MoneyException.notDividedWithThousand(Integer.parseInt(input));
@@ -18,21 +44,27 @@ public class LottoMachine {
         lottoCount = Integer.parseInt(input) / 1000;
     }
 
-    List<Integer> generateLottoNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        LottoNumbersException.validateLottoNumbers(numbers);
-        return numbers;
-    }
-
-    public List<Lotto> giveLottoBundle() {
-        List<Lotto> lottoBundle = new ArrayList<Lotto>();
+    private void printLottoBundle() {
+        lottoBundle = new ArrayList<Lotto>();
 
         for (int i = 0; i < lottoCount; i++) {
             Lotto processingLotto = new Lotto(generateLottoNumbers());
             lottoBundle.add(processingLotto);
         }
+    }
 
-        return lottoBundle;
+    private List<Integer> generateLottoNumbers() {
+        List<Integer> numbers;
+
+        try {
+            numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            LottoNumbersException.validateLottoNumbers(numbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return generateLottoNumbers();
+        }
+
+        return numbers;
     }
 
 }

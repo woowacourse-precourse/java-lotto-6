ad8c5d5cfd1c95ee40f744a7a1111153b3368191
YongJOoOo <we072200@naver.com>
2023-11-07 08:56:32 +0900

    refactor: WinningNumberValidator 에서 변수 이름 및 메시지 오타 수정
    - 정해진 코드 컨벤션 라인 넘어가지 않도록 수정함

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -3,11 +3,11 @@ package lotto.validator;
 import java.util.List;
 
 public class WinningNumberValidator {
-    private static final String WINNING_NUMBER_PATTERN = "^(?:(?:[1-9]|[1-3][0-9]|4[0-5]),){5}(?:[1-9]|[1-3][0-9]|4[0-5])$";
+    private static final String NUMBER_PATTERN = "^(?:(?:[1-9]|[1-3][0-9]|4[0-5]),){5}(?:[1-9]|[1-3][0-9]|4[0-5])$";
 
     public static void validateFormat(String input) throws IllegalArgumentException {
-        if (!input.matches(WINNING_NUMBER_PATTERN)) {
-            throw new IllegalArgumentException("[ERRPR] 당첨 번호 포맷이 잘못되었습니다.");
+        if (!input.matches(NUMBER_PATTERN)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 포맷이 잘못되었습니다.");
         }
     }
 
@@ -17,9 +17,7 @@ public class WinningNumberValidator {
                 .toList();
 
         if (disticedNumbers.size() != input.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 서로 중복되면 안됩니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 끼리는 서로 중복되면 안됩니다.");
         }
     }
 }
-
-

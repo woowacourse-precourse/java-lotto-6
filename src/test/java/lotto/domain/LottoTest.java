89cec195e9f56224b8c93c6bee0b1217f4a42eef
edgar6bf <edgar6bf@gmail.com>
2023-11-06 19:07:31 +0900

    test: LottoTest 인스턴스 정상 생성 테스트 케이스 추가
    Lotto 클래스의 인스턴스가 정상적으로 생성되는 것을 검증하는 테스트 로직을 추가하였다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,9 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+
+    @DisplayName("유효한 로또 번호를 입력하면 Lotto 클래스의 인스턴스가 생성된다.")
+    @Test
+    void createLottoByInvalidNumber() {
+        // Given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 40);
+
+        // When
+        Lotto lotto = new Lotto(numbers);
+
+        // Then
+        assertThat(lotto).isNotNull();
+        assertThat(lotto.getNumbers()).isEqualTo(numbers);
+    }
 
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test

2bebdc3cc08b99c46387e1eae9de6478edd4243e
maark1106 <mark1106@naver.com>
2023-11-06 19:11:59 +0900

    test : 구입 금액 범위 검증하는 테스트

---
+++ src/test/java/lotto/domain/UserTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @DisplayName("구입 금액이 1000원보다 작을 때 오류 발생")
+    @Test
+    void purchaseAmountLessThanCorrectRange() {
+        assertThatThrownBy(() -> new User(900))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입 금액이 10억원보다 클 때 오류 발생")
+    @Test
+    void purchaseAmountOverThanCorrectRange() {
+        assertThatThrownBy(() -> new User(2100000000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

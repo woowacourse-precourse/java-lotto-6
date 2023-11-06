9606f5ce51842de2839160227329113f8b9f35e3
jihyuk <kwlgur0416@gmail.com>
2023-11-07 03:39:33 +0900

    feat : 로또 당첨 결과 출력 기능 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,24 @@
 package lotto.view;
 
+import lotto.model.LottoResult;
 import lotto.model.LottoTickets;
 
 public class OutputView {
 
     private static final String LOTTO_TICKETS_COUNT_MESSAGE = "%d개를 구매했습니다.";
 
+    private static final String LOTTO_TICKETS_RESULT_MESSAGE = "당첨 통계\n---";
+
     public static void printLottoTickets(final LottoTickets tickets) {
         final String ticketCountPrintFormat = String.format(LOTTO_TICKETS_COUNT_MESSAGE,
             tickets.getTicketsCount());
         System.out.println(ticketCountPrintFormat);
         System.out.println(tickets.toOutputString());
+    }
+
+    public static void printLottoResult(final LottoResult result) {
+        System.out.println(LOTTO_TICKETS_RESULT_MESSAGE);
+        System.out.println(result.toOutputString());
     }
 
 }

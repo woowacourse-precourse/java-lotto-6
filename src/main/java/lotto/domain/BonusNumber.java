fd28668b6268cbc89b2b8b2fdeaa86378b523e5c
JaeHunJang <wuddlaa@gmail.com>
2023-11-06 22:36:20 +0900

    feat(BonusNumber): 당첨번호와 중복 검증 메소드 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,14 +1,25 @@
 package lotto.domain;
 
+import lotto.config.ErrorMessage;
 import lotto.util.validator.BonusNumberValidator;
+
+import java.util.List;
 
 public class BonusNumber {
     private final int bonusNumber;
 
-    BonusNumber(String number) {
+    BonusNumber(List<Integer> winningNumbers, String number) {
         new BonusNumberValidator(number);
         this.bonusNumber = Integer.parseInt(number);
+        validateExistNumber(winningNumbers, this.bonusNumber);
     }
+
+    private void validateExistNumber(List<Integer> numbers, int number) {
+        if(numbers.contains(number)) {
+            throw new IllegalArgumentException(ErrorMessage.EXIST_NUMBER.getMessage());
+        }
+    }
+
     public int getBonusNumber() {
         return bonusNumber;
     }

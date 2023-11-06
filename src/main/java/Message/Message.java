9b2ac526d4b09298cbaf58ba218f3810dcfa3e99
SeungGwa123 <skwan123@naver.com>
2023-11-07 04:14:10 +0900

    Style: Easy to Read
    읽기 쉽게 정리

--- src/main/java/Message/Message.java
+++ src/main/java/Message/Message.java
@@ -1,12 +1,12 @@
 package Message;
 
 public enum Message {
-    moneyOutOfRange(-1,"[ERROR] 구입 금액은 정수여야 합니다."),
-    moneyInvalidFormat(-2,"[ERROR] 구입 금액은 1000원 단위의 양의 정수여야 합니다."),
-    numberIncorrectFormat(-1,"[ERROR] 로또 번호는 공백없이 콤마(,)로 구분해서 입력해주세요."),
-    numberOutOfRange(-2,"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    numberIncorrectLength(-3,"[ERROR] 로또 번호는 6자리를 입력해주셔야 합니다."),
-    numberIncorrect(-4,"[ERROR] 로또 번호는 정수로 입력해주세요.");
+    moneyOutOfRange(-1, "[ERROR] 구입 금액은 정수여야 합니다."),
+    moneyInvalidFormat(-2, "[ERROR] 구입 금액은 1000원 단위의 양의 정수여야 합니다."),
+    numberIncorrectFormat(-1, "[ERROR] 로또 번호는 공백없이 콤마(,)로 구분해서 입력해주세요."),
+    numberOutOfRange(-2, "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    numberIncorrectLength(-3, "[ERROR] 로또 번호는 6자리를 입력해주셔야 합니다."),
+    numberIncorrect(-4, "[ERROR] 로또 번호는 정수로 입력해주세요.");
     private final int code;
     private final String message;
 
---
+++ src/main/java/Money/Winning.java
@@ -0,0 +1,2 @@
+package Money;public enum Winning {
+}

e0c4089c9dbd6420dc6eaa162424cc4f2e2402ed
potatomoong <star901210@hanmail.net>
2023-11-07 02:25:04 +0900

    Feat: 입력 받은 당첨번호를 정수 배열로 변환하는 기능 구현

--- src/main/java/lotto/domain/BallNumber.java
+++ src/main/java/lotto/domain/BallNumber.java
@@ -9,6 +9,16 @@ public class BallNumber {
         return winningBall.split(",");
     }
 
+    public static int[] transWinningBall(String[] winningBallNumber) {
+        int[] winningNumber = new int[winningBallNumber.length];
+
+        for (int i = 0; i < winningBallNumber.length; i++) {
+            winningNumber[i] = Integer.parseInt(winningBallNumber[i]);
+        }
+
+        return winningNumber;
+    }
+
     public static int inputBonusBall() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusBall = Console.readLine();

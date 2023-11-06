ca8aa7bfd383e5564eecc3b29a6d3e8a9e71d1cb
PARKHWANYEONG <welcome0828@naver.com>
2023-11-07 05:48:43 +0900

    test(domain): checkMatchingNumbers 메서드에 대한 테스트 코드 추가
    - @CsvSource을 사용해 각 케이스마다 로또와 당첨번호를 비교했을때 matchingCount가 일치하는지 검증했습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,17 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +31,26 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또와 당첨번호를 비교해 맞힌 개수를 반환하는지 검증한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,2,3,4,5,6:6",
+            "1,2,3,4,5,7:5",
+            "1,2,3,4,5,8:5",
+            "1,2,3,4,7,8:4",
+            "1,2,3,7,8,9:3"
+    },delimiter = ':')
+    void checkMatchingNumbersTest(String inputWinningNumbers,int expectedMatchingCount) {
+        // 로또 생성
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+
+        // @CsvSource로 당첨번호를 각 케이스 별로 입력받는다.
+        List<Integer> winningNumbers = Arrays.stream(inputWinningNumbers.split(","))
+                .map(Integer::parseInt)
+                .toList();
+
+        // checkMatchingNumbers 메서드로 당첨 번호와 일치하는 개수를 구해 검증
+        int matchingCount = lotto.checkMatchingNumbers(winningNumbers);
+        assertThat(matchingCount).isEqualTo(expectedMatchingCount);
+    }
 }

1ce0cfbdbdc54fc0b2d2470a60300ee941729f85
ohhyeonn <ohhyeon120@gmail.com>
2023-11-06 16:59:58 +0900

    feat : add test 당첨번호 6개숫자 테스트

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -25,6 +25,14 @@ class ApplicationTest extends NsTest {
     }
 
 
+    @Test
+    void 당첨번호_6개숫자_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000" , "1,2,3,4,5,6,7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
 
     @Test
     void 당첨번호_공백문자_테스트() {

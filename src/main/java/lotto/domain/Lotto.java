aa2f7f628cb8617f7493abd658ad6aa344f39eaa
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-06 16:33:45 +0900

    feat: 오름차순으로 정렬된 로또 번호를 저장하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,17 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        List<Integer> sortedNumbers = sortNumbers(numbers);
+        this.numbers = sortedNumbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +21,13 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

ec8fc5ccc75ee15e949e2f29e4d181ab2330ed98
Hoo-sung <gntjd135@gmail.com>
2023-11-06 22:27:34 +0900

    test(UtilTest):  수익률 포매팅하여 형식에 맞게 반환하는 테스트 작성

--- src/test/java/lotto/util/UtilTest.java
+++ src/test/java/lotto/util/UtilTest.java
@@ -23,4 +23,17 @@ class UtilTest {
             Assertions.assertThat(Util.calculateYield(before, after)).isEqualTo(expected);
         }
     }
+
+    @Nested
+    class FormatFloat{
+        @ParameterizedTest
+        @CsvSource(value = {
+                "84.13: 1: 84.1",
+                "817.47: 1:817.5",
+                "123023.78: 1: 123,023.8"
+        },delimiter = ':')
+        void 형식에_맞추어_소수_첫쨰자리에서_반환하는지_테스트(float target, int precision, String expected){
+            Assertions.assertThat(Util.makeFloatFormattedYield(target, precision)).isEqualTo(expected);
+        }
+    }
 }

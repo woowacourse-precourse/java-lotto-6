ae40ebac81b8c7b922c60da45eee0aa7b604b8ed
gonixxxx <akdntm946@gmail.com>
2023-11-07 04:01:33 +0900

    feat: add output view to print lotto statistics

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,17 +1,40 @@
 package lotto.view;
 
-import lotto.model.Lotto;
+import java.text.DecimalFormat;
+import java.util.Map;
 import lotto.model.Lottos;
 import lotto.model.Money;
+import lotto.model.Rank;
+import lotto.model.User;
 
 public class OutputView {
 
     private static final String SHOW_LOTTO = "\n%d개를 구매했습니다.\n";
+    private static final String STAT_MESSAGE = "\n당첨 통계\n---";
+    private static final String STAT_RESULT = "%d개 일치 (%s원) - %d개\n";
+    private static final String STAT_RESULT_BONUS = "%d개 일치, 보너스 볼 일치 (%s)원 - %d개\n";
+    private static final String EARNING = "총 수익률은 %.1f입니다.";
+    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat("###,###");
 
     public void showLottos(Lottos lottos, Money money) {
         System.out.printf(SHOW_LOTTO, money.calNumberOfLotto());
         lottos.showLottos();
         printLine();
+    }
+
+    public void showLottoStatistics(Rank rank, Lottos lottos, User user) {
+        Map<Rank, Integer> rankResult = lottos.saveRankResult(user);
+        rankResult.remove(Rank.ECT);
+        System.out.println(STAT_MESSAGE);
+        rankResult.forEach((key, value) -> {
+            if (rank == Rank.FIVE_BONUS) {
+                System.out.printf(STAT_RESULT_BONUS, rank.getMatchingNumber(),
+                        MONEY_FORMAT.format(rank.getPrize()), rankResult.get(rank));
+            } else {
+                System.out.printf(STAT_RESULT, rank.getMatchingNumber(),
+                        MONEY_FORMAT.format(rank.getPrize()), rankResult.get(rank));
+            }
+        });
     }
 
     public void printLine() {

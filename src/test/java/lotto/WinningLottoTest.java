cb065085e3cabffb895a74b917a92d04babbdb58
sunwooiljoon <wnsdl1659@naver.com>
2023-11-06 16:55:26 +0900

    test : WinningLottoTest 수정
    생성자 변경에 따른 메소드 호출 수정

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -11,14 +11,14 @@ public class WinningLottoTest {
     @Test
     @DisplayName("보너스의 번호가 45를 넘어가면 에러 발생")
     void createWinningLottoByOverBonusNumber() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1,2,3,4,5,6), 46))
+        assertThatThrownBy(() -> new WinningLotto(List.of(1,2,3,4,5,6)).setBonusNumber(46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스의 번호가 로또번호랑 중복되면 에러")
     void createWinningLottoByDuplicatedBonusNumber() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1,2,3,4,5,6), 5))
+        assertThatThrownBy(() -> new WinningLotto(List.of(1,2,3,4,5,6)).setBonusNumber(5))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

6086e540c4d721e584ea765c9033dcea8346336a
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-06 22:28:37 +0900

    feat(LottoMachine): generate list of winning lotto
    - generate unique and random lottos
    - add winning lottos in candWinningLotto list

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,5 +1,24 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGenerator {
     // Randoms.를 사용해서 서로다른 6개의 로또를 생성한다
+
+    public void lottoGenerateWinningLotto(int gameAmount) {
+        List<Lotto> candWinningLotto = new ArrayList<>;
+        for (int i= 0; i < gameAmount; i++) {
+            candWinningLotto.add(generateLottoNumber());
+        }
+    }
+
+    public Lotto generateLottoNumber() {
+        List<Integer> uniqueRandomNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(uniqueRandomNumber);
+    }
+
 }

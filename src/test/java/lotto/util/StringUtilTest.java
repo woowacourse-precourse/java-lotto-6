550acb1001c8d79fede03e53df8d12d64d3d795e
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 11:16:07 +0900

    test: 구분자로 문자열을 숫자 리스트를 변환하는지, 변환할 수 없으면 예외가 발생하는지 테스트

--- src/test/java/lotto/util/StringUtilTest.java
+++ src/test/java/lotto/util/StringUtilTest.java
@@ -1,9 +1,12 @@
 package lotto.util;
 
+import static lotto.enums.Delimiter.COMMA;
 import static lotto.enums.ErrorMassage.INVALID_NUMBER;
+import static lotto.enums.ErrorMassage.NOT_SEPARATE_BY_DELIMITER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class StringUtilTest {
@@ -28,5 +31,31 @@ class StringUtilTest {
 
         // then
         assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void 구분자로_문자열을_숫자_리스트로_변환한다() {
+        // given
+        String input = "1" + COMMA.getValue() + "2" + COMMA.getValue() + "3"
+                + COMMA.getValue() + "4" + COMMA.getValue() + "5" + COMMA.getValue() + "6";
+
+        // when
+        List<Integer> result = StringUtil.convertToIntListByDelimiter(input);
+
+        // then
+        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(result).hasSize(6);
+    }
+
+    @Test
+    void 구분자로_문자열을_숫자_리스트로_변환할_수_없을_경우_예외가_발생한다() {
+        // given
+        String delimiter = ".";
+        String input = "1" + delimiter + "2" + delimiter + "3" + delimiter + "4" + delimiter + "5" + delimiter + "6";
+
+        // when & then
+        assertThatThrownBy(() -> StringUtil.convertToIntListByDelimiter(input))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_SEPARATE_BY_DELIMITER.getMassage());
     }
 }

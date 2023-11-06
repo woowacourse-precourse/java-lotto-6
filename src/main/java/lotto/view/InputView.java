025dd1d2efcb0db1e8e3a163a26dacd4fb920fd3
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 02:15:19 +0900

    feat(InputView): 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,18 +2,50 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class InputView {
     public static int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
 
-        validateInput(input);
+        validatePurchaseAmountInput(input);
 
         return Integer.parseInt(input);
     }
-    
-    private static void validateInput(String input) {
+
+    public static List<Integer> getWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+
+        validateWinningNumbersInput(input);
+
+        return null;
+    }
+
+    private static void validatePurchaseAmountInput(String input) {
         if (isNotPositiveInteger(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateWinningNumbersInput(String input) {
+        String[] numbers = input.split(",");
+        int lastIndex = numbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            String numberStr = numbers[i].trim();
+
+            if (!isNotPositiveInteger(numberStr)) {
+                throw new IllegalArgumentException();
+            }
+
+            if (i < lastIndex && numberStr.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        if (numbers[lastIndex].isEmpty()) {
             throw new IllegalArgumentException();
         }
     }

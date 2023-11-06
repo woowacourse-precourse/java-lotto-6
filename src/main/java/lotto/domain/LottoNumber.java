c70c15a70ecfd92b972cc8c465f1f3e2fe4f80a7
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:08:32 +0900

    feat: LottoNumber 클래스 정적 팩토리 메소드 구현 및 검증 로직 리팩토링

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -5,15 +5,19 @@ public class LottoNumber {
     private Integer lottoNumber;
 
     public LottoNumber(Integer lottoNumber) {
-        validateLottoNumber(lottoNumber);
         this.lottoNumber = lottoNumber;
     }
 
-    private void validateLottoNumber(Integer lottoNumber) {
+    public static LottoNumber from(Integer lottoNumber) {
+        validateLottoNumber(lottoNumber);
+        return new LottoNumber(lottoNumber);
+    }
+
+    private static void validateLottoNumber(Integer lottoNumber) {
         validateRange(lottoNumber);
     }
 
-    private void validateRange(Integer lottoNumber) {
+    private static void validateRange(Integer lottoNumber) {
         if (lottoNumber < 1 || lottoNumber > 45) {
             throw new IllegalArgumentException("[ERROR] 각각의 당첨 번호는 1~45 범위의 정수이어야 합니다.");
         }

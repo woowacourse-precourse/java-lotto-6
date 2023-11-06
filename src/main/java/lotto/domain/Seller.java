df775700f08fefea2d6fa7b7949b742769c2c95e
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-11-06 21:18:24 +0900

    feat(domain): 금액에 따른 다수 로또 생성 기능 구현 및 테스트 코드 작성

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -1,8 +1,29 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seller {
+    private static final Integer NUMBER_COUNT = 6;
     private static final Integer UNIT = 1000;
+    private static final Integer MINIMUM_RANGE = 1;
+    private static final Integer MAXIMUM_RANGE = 45;
+
     public static Integer numberOfLotto(Integer price) {
         return price / UNIT;
+    }
+
+    public static List<Lotto> makeLottos(Integer numberOfLotto) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (Integer count = 0; count < numberOfLotto; count++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MINIMUM_RANGE, MAXIMUM_RANGE, NUMBER_COUNT);
+
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
     }
 }
--- src/test/java/lotto/domain/SellerTest.java
+++ src/test/java/lotto/domain/SellerTest.java
@@ -10,4 +10,10 @@ public class SellerTest {
         assertThat(Seller.numberOfLotto(12000)).isEqualTo(12);
     }
 
+    @Test
+    void 로또_개수_입력_시_로또_객체의_리스트_리턴() {
+        Integer numberOfLotto = 8;
+
+        assertThat(Seller.makeLottos(numberOfLotto).size()).isEqualTo(8);
+    }
 }

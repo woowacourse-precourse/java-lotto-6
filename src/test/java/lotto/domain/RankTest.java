5dd26d0b4c6d7d6524b104ab6ac491c576032863
gosu <q63530@naver.com>
2023-11-06 22:00:43 +0900

    test: rank 테스트 수정

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,30 +1,29 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class RankTest {
 
     @DisplayName("조건에 맞는 RANK를 생성함")
-    @Test
-    void createRankTest() {
-        List<Rank> ranks = new ArrayList<>();
-        ranks.add(Rank.from(2, true));
-        ranks.add(Rank.from(3, true));
-        ranks.add(Rank.from(4, true));
-        ranks.add(Rank.from(5, false));
-        ranks.add(Rank.from(5, true));
-        ranks.add(Rank.from(6, true));
+    @ParameterizedTest
+    @MethodSource("generate")
+    void createRankTest(Rank createRank, Rank rank) {
+        Assertions.assertEquals(rank, createRank);
+    }
 
-        int index = 0;
-
-        for (Rank rank : Rank.values()) {
-            assertEquals(rank, ranks.get(index));
-            index++;
-        }
+    static Stream<Arguments> generate() {
+        return Stream.of(
+                Arguments.of(Rank.from(2, true), Rank.BLANK),
+                Arguments.of(Rank.from(3, true), Rank.FIFTH),
+                Arguments.of(Rank.from(4, true), Rank.FOURTH),
+                Arguments.of(Rank.from(5, false), Rank.THIRD),
+                Arguments.of(Rank.from(5, true), Rank.SECOND),
+                Arguments.of(Rank.from(6, true), Rank.FIRST)
+        );
     }
 }

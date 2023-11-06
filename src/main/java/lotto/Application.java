ec2fcc5e2a07153d33740b236ef8c0c2c2c5ade7
Jeongyounghyeon <snorlax1106@naver.com>
2023-11-06 17:44:47 +0900

    feat: main 로직 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,48 @@
 package lotto;
 
+
+import java.util.List;
+import lotto.controller.InputController;
+import lotto.controller.OutputController;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
+import lotto.service.LottoService;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount;
+        List<Lotto> lottos;
+        WinningLotto winningLotto;
+
+        purchaseAmount = InputController.purchaseAmount();
+        lottos = buyLottos(purchaseAmount);
+        winningLotto = setWinningLotto();
+        OutputController.winningStatistics(winningLotto, lottos, purchaseAmount);
+    }
+
+    public static List<Lotto> buyLottos(Integer purchaseAmount) {
+        List<Lotto> lottos;
+
+        lottos = LottoService.purchaseLotto(purchaseAmount);
+        System.out.println();
+        OutputController.purchaseLottos(lottos);
+        System.out.println();
+
+        return lottos;
+    }
+
+    public static WinningLotto setWinningLotto() {
+        WinningLotto winningLotto;
+        List<Integer> winningNumbers;
+        int bonusNumber;
+
+        winningNumbers = InputController.winningNumbers();
+        System.out.println();
+        bonusNumber = InputController.bonusNumber(winningNumbers);
+        System.out.println();
+        winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        return winningLotto;
     }
 }

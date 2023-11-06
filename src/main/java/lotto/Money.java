49c34ed6c2bc8edbd57ee2f3fb3418363405a368
이병수 <qudtn0315@naver.com>
2023-11-06 16:14:51 +0900

    chore (LottoGame) : enum문을 불러와 코드 넘버링 변경

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,12 +1,12 @@
 package lotto;
 
+import lotto.EnumList.ConstantErrorMessage;
+
 public class Money {
 
     private int money = 0;
     private static final int ONE_THOUSAND_WON = 1000;
     private static final int ZERO = 0;
-    private static final String ERROR_NUMBER_MESSAGE = "[ERROR] 숫자를 입력하세요.";
-    private static final String INSERT_WON_MESSAGE = "[ERROR] 1,000원 단위로 입력하세요.";
 
     public Money(String money) {
         this.money = validate(money);
@@ -24,7 +24,7 @@ public class Money {
 
     private void validateMoney(String money) {
         if(!money.matches("[0-9]+")) {
-            throw new IllegalArgumentException(ERROR_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(ConstantErrorMessage.ERROR_NUMBER.errorMessage());
         }
     }
 
@@ -32,7 +32,7 @@ public class Money {
         int realMoney = Integer.parseInt(money);
 
         if(realMoney % ONE_THOUSAND_WON != ZERO) {
-            throw new IllegalArgumentException(INSERT_WON_MESSAGE);
+            throw new IllegalArgumentException(ConstantErrorMessage.ERROR_INSERT_WON.errorMessage());
         }
 
         return realMoney;

f03ad939d5310d5f14290b1390b849d16c777a20
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-06 23:20:07 +0900

    feat: JackpotNumber에 대한 예외처리 기능 추가

---
+++ src/main/java/lotto/utils/JackpotNumberException.java
@@ -0,0 +1,44 @@
+package lotto.utils;
+
+import java.util.List;
+import java.util.Arrays;
+
+import lotto.domain.JackpotNumber;
+
+public class JackpotNumberException {
+    static final String NUMBER_IN_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    static final String NUMBER_DUPLICATED = "[ERROR] 로또 번호는 모두 중복되지 않은 수여야 합니다.";
+    static final String NUMBER_SIX_VALUES = "[ERROR] 로또 번호는 6개여야 합니다.";
+
+    public static void isJackpotValid(JackpotNumber jackpotNumber) {
+        isJackpotSixValue(jackpotNumber);
+        isJackpotDuplicated(jackpotNumber);
+        isJackpotRange(jackpotNumber);
+    }
+    public static void isJackpotSixValue(JackpotNumber jackpotNumber) {
+        List<Integer> numbers = jackpotNumber.getJackpot().getNumbers();
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(NUMBER_SIX_VALUES);
+        }
+    }
+    public static void isJackpotDuplicated(JackpotNumber jackpotNumber) {
+        List<Integer> numbers = jackpotNumber.getJackpot().getNumbers();
+        if (numbers.stream()
+                .distinct()
+                .count() != numbers.size()) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATED);
+        }
+    }
+    public static void isJackpotRange(JackpotNumber jackpotNumber) {
+        for (int number: jackpotNumber.getJackpot().getNumbers()) {
+            inRange(number);
+        }
+    }
+    private static void inRange(int number) {
+        if (1 > number || number > 45) {
+            throw new IllegalArgumentException(NUMBER_IN_RANGE);
+        }
+    }
+
+
+}

46989b48643da279f7170e33bd9352f903892eec
donghyun <kdhdh001@naver.com>
2023-11-07 01:14:30 +0900

    refactor: PurchaseAmountValidator 클래스 수정

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -4,8 +4,7 @@ public class Constants {
     public static final int LOTTO_TICKET_PRICE = 1000;
 
     public static final String ERROR = "[ERROR] ";
-    public static final String NON_NUMERIC_MSG = ERROR + "구입금액은 숫자로 입력해야 합니다.";
-    public static final String NON_POSITIVE_MSG = ERROR + "구입금액은 양수여야 합니다.";
+    public static final String NOT_POSITIVE_INTEGER_MSG = ERROR + "구입금액은 양수로 입력해야 합니다.";
     public static final String NOT_DIVISIBLE_MSG = ERROR + "구입금액이 로또 가격인 1000원으로 나누어 떨어져야 합니다.";
 
 }
--- src/main/java/lotto/validation/PurchaseAmountValidator.java
+++ src/main/java/lotto/validation/PurchaseAmountValidator.java
@@ -4,15 +4,9 @@ import lotto.utils.Constants;
 
 public class PurchaseAmountValidator {
 
-    public static void validateIsNumeric(String str) {
-        if (!str.matches("-?\\d+(\\.\\d+)?")) {
-            throw new IllegalArgumentException(Constants.NON_NUMERIC_MSG);
-        }
-    }
-
-    public static void validateIsPositive(int purchaseAmount) {
-        if (purchaseAmount <= 0) {
-            throw new IllegalArgumentException(Constants.NON_POSITIVE_MSG);
+    public static void validateIsPositiveInteger(String str) {
+        if (!str.matches("\\d+")) {
+            throw new IllegalArgumentException(Constants.NOT_POSITIVE_INTEGER_MSG);
         }
     }
 

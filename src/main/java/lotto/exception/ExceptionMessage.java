267987aa11fe4df976f52bbf93405e9c845952de
jhsong76 <20190976@sungshin.ac.kr>
2023-11-06 15:57:41 +0900

    feat: 예외 상황 [ERROR] 메세지 출력 클래스

---
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -0,0 +1,19 @@
+package lotto.exception;
+
+public class ExceptionMessage {
+    private static final String NUMBER_ALREADY_EXIST = "[ERROR] 중복된 숫자를 입력하셨습니다.";
+    private static final String INPUT_TYPE_ERROR = "[ERROR] 숫자만 입력해주세요.";
+    private static final String EXCEEDED_NUMBER = "[ERROR] 숫자는 6개만 입력 가능합니다.";
+
+    public static void existException() {
+        System.out.println(NUMBER_ALREADY_EXIST);
+    }
+
+    public static void typeExcetpion() {
+        System.out.println(INPUT_TYPE_ERROR);
+    }
+
+    public static void sizeException() {
+        System.out.println(EXCEEDED_NUMBER);
+    }
+}

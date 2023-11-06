3bcf9e1fea8925086bd3894965c3f8a29924b053
euniiiii <hee9920@naver.com>
2023-11-06 13:32:27 +0900

    feat: 당첨 번호 입력시 예외처리 기능 구현

--- src/main/java/lotto/domain/PrintMessage.java
+++ src/main/java/lotto/domain/PrintMessage.java
@@ -3,4 +3,7 @@ package lotto.domain;
 public class PrintMessage {
     public static final String ONLY_DIGIT = "[ERROR] 숫자를 입력해주세요.";
     public static final String ONLY_THOUSAND_UNIT = "[ERROR] 1000원 단위로 입력해주세요.";
+    public static final String ONLY_SPLIT_COMMA = "[ERROR] 번호를 쉼표(,)로 나누어야 합니다.";
+    public static final String BETWEEN_ONE_AND_FORTYFIVE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String NOT_DUPLICATION = "[ERROR] 중복되지 않는 번호로 입력해주세요.";
 }
--- src/main/java/lotto/utils/InputException.java
+++ src/main/java/lotto/utils/InputException.java
@@ -2,6 +2,10 @@ package lotto.utils;
 
 import lotto.domain.PrintMessage;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputException {
 
     public static void isDigit(String input) {
@@ -15,6 +19,22 @@ public class InputException {
     public static void isThousandUnit(int num) {
         if (num % 1000 != 0)
             throw new IllegalArgumentException(PrintMessage.ONLY_THOUSAND_UNIT);
+    }
+
+    public static void isContainComma(String input) {
+        if (!input.contains(","))
+            throw new IllegalArgumentException(PrintMessage.ONLY_SPLIT_COMMA);
+    }
+
+    public static void isBetweenOneAndFourtyfive(int num) {
+        if (num < 1 || num > 45)
+            throw new IllegalArgumentException(PrintMessage.BETWEEN_ONE_AND_FORTYFIVE);
+    }
+
+    public static void isDuplication(List<Integer> list) {
+        Set<Integer> set = new HashSet<>(list);
+        if (list.size() != set.size())
+            throw new IllegalArgumentException(PrintMessage.NOT_DUPLICATION);
     }
 
 }

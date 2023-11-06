115e97cfb9c3725421a9750bba71b878627b6df7
SJ70 <seojae70@gmail.com>
2023-11-06 16:44:14 +0900

    Refactor & Test: 상점이 번호는 반환하지 않고 구매 가능 수량만 반환

--- src/main/java/lotto/domain/shop/LottoShop.java
+++ src/main/java/lotto/domain/shop/LottoShop.java
@@ -1,25 +1,12 @@
 package lotto.domain.shop;
 
-import java.util.List;
-import lotto.domain.lotto.LottoNumbersGenerator;
-import lotto.dto.LottoNumbersDTO;
-import lotto.domain.lotto.strategy.PickNumbersStrategy;
-import lotto.domain.lotto.strategy.PickRandomNumbersStrategy;
-
 public class LottoShop {
-
-    private final PickNumbersStrategy pickNumbersStrategy = new PickRandomNumbersStrategy();
-    private final LottoNumbersGenerator lottoGenerator = new LottoNumbersGenerator(pickNumbersStrategy);
 
     private final int LOTTO_PRICE = ShopConfig.LOTTO_PRICE.getValue();
 
     public int countPurchasableAmount(int cash) {
         validateMaxPurchaseLimit(cash);
         return cash / LOTTO_PRICE;
-    }
-
-    public List<LottoNumbersDTO> publishNumbers(int count) {
-        return lottoGenerator.generateByCount(count);
     }
 
     private void validateMaxPurchaseLimit(int cash) {

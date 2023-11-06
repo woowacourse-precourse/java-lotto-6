31dd876652bea804c8e5b9968ab529c7a5892749
JIHYE <jdh324@naver.com>
2023-11-07 03:51:38 +0900

    refactor: 상수 클래스 작성
    로또 게임에서 사용하는 상수들을 모아놓는 상수 클래스를 따로 모아서 작성했습니다.

---
+++ src/main/java/lotto/LottoConstants.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public class LottoConstants {
+
+    public static final int LOTTO_TICKET_PRICE = 1000;
+    public static final int ZERO = 0;
+    public static final int LOTTO_NUMBERS = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+}

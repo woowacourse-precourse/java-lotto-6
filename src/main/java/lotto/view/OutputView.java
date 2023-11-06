2245f7302ac18470c0e7a22734175ad7bbdd9033
ChanUi <hcu55@naver.com>
2023-11-06 22:07:00 +0900

    feat: 로또 구입금액 입력 문구 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String PRINT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void printLottoPurchaseAmountInputMessage() {
+        System.out.println(PRINT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
+    }
+}

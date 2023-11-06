dfeaca20f7c72f49470e2787a4b2c498f9e3eb44
je488 <pje488@naver.com>
2023-11-06 14:38:57 +0900

    feat: 로또 구입 금액 입력받고 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static void lottoMoneyInput() {
+        String userInput = Console.readLine();
+        int lottoMoney = Integer.parseInt(userInput);
+        if (lottoMoney % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 로또 구입 금액 입력받고 예외처리
+        while(true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                lottoMoneyInput();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }

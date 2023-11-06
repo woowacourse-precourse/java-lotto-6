d4adcb6e8b9c3659a4b1c06f03a8aa82f51065be
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-11-06 21:05:47 +0900

    feat(Application) : 시작 메서드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.Controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller.LottoStart();
     }
 }

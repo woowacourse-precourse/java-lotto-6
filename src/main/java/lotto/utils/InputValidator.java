191b27903d276736fa8f4fae5f076c6d702d4632
seondays <agseon9@gmail.com>
2023-11-06 13:14:56 +0900

    feat(InputValidator): 사용자에게 당첨 번호 입력받기 예외처리 구현

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -1,5 +1,7 @@
 package lotto.utils;
 
+import java.util.List;
+
 public class InputValidator {
     private static final int MIN_COST = 1000;
     private static final int MAX_COST = 100000;
@@ -9,6 +11,14 @@ public class InputValidator {
         validateInputInt(cost);
         validateCostOutOfSize(cost);
         validateCostInUnit(cost);
+    }
+
+    public void validateWinningNumberInputView(String number) {
+        List<String> numberList = StringParser.splitString(number);
+        validateInputBlank(number);
+        validateWinningNumberSplitSize(numberList);
+        validateWinningNumberDuplication(numberList);
+        validateWinningNumberEach(numberList);
     }
 
     private void validateInputBlank(String input) {
@@ -34,6 +44,33 @@ public class InputValidator {
         int intCost = Integer.parseInt(cost);
         if (intCost % MIN_COST != 0) {
             throw new IllegalArgumentException("[Error] 구매 금액은 1000원 단위로만 가능합니다");
+        }
+    }
+
+    private void validateWinningNumberSplitSize(List<String> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[Error] 쉼표를 기준으로 당첨 번호 6개를 입력해 주세요");
+        }
+    }
+
+    private void validateWinningNumberOutOfSize(String winningNumber) {
+        int winningNumberToInt = StringParser.parseStringToInt(winningNumber);
+        if (winningNumberToInt < 1 || winningNumberToInt > 45) {
+            throw new IllegalArgumentException("[Error] 당첨 번호는 1부터 45까지의 숫자만 가능합니다");
+        }
+    }
+
+    private void validateWinningNumberDuplication(List<String> winningNumbers) {
+        List<String> distinctWinningNumbers = winningNumbers.stream().distinct().toList();
+        if (winningNumbers.size() != distinctWinningNumbers.size()) {
+            throw new IllegalArgumentException("[Error] 당첨 번호는 중복되어선 안됩니다");
+        }
+    }
+
+    private void validateWinningNumberEach(List<String> winningNumbers) {
+        for(String number : winningNumbers) {
+            validateInputInt(number);
+            validateWinningNumberOutOfSize(number);
         }
     }
 }

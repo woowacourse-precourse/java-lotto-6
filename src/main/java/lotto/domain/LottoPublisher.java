83964c892b6c2ab71caa6482909750d8c65fb6a4
Annjieun <dkslwdms2121@naver.com>
2023-11-06 16:11:42 +0900

    feat(LottoPublisher): 검증 로직을 Validation 클래스로 이동

--- src/main/java/lotto/domain/LottoPublisher.java
+++ src/main/java/lotto/domain/LottoPublisher.java
@@ -2,15 +2,13 @@ package lotto.domain;
 
 import static lotto.constant.LottoNumber.PURCHASE_UNIT;
 import static lotto.constant.LottoNumber.ZERO;
-import static lotto.constant.message.ErrorMessage.NON_INTEGER_MONEY;
-import static lotto.constant.message.ErrorMessage.NOT_PURCHASE_UNIT;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.util.Util;
 
-public class LottoPublisher {
+public class LottoPublisher extends Validation {
     private List<Lotto> userLottos;
     private int lottoAmount;
     private int money;
@@ -25,20 +23,6 @@ public class LottoPublisher {
         checkDivideByThousand(money);
 
         this.money = Integer.parseInt(money);
-    }
-
-    private void checkValidInteger(String money) {
-        try {
-            Integer.parseInt(money);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NON_INTEGER_MONEY.getMessage());
-        }
-    }
-
-    private void checkDivideByThousand(String money) {
-        if (Integer.parseInt(money) % PURCHASE_UNIT.getNumber() != ZERO.getNumber()) {
-            throw new IllegalArgumentException(NOT_PURCHASE_UNIT.getMessage());
-        }
     }
 
     private void createLottos() {

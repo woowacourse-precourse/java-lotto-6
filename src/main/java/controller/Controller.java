cf78150c462d59784f1fc365659c4894b81cc805
Minju Ji <hinote444@gmail.com>
2023-11-07 02:58:26 +0900

    feat(outputview, controller): 출력 메시지 기능 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,19 +7,24 @@ import domain.Lotto;
 import service.LottoGame;
 import utils.Utils;
 import view.InputView;
+import view.OutputView;
 
 public class Controller {
     private final LottoGame lottoGame = new LottoGame();
 
     private void setupWinningNumbers() {
+        OutputView.printInputWinningNumbers();
         String lottoNumbers = InputView.inputWinningNumbers();
         String[] separatedWinningNumbers = Utils.splitInputByComma(lottoNumbers);
         List<Integer> winningNumbers = Utils.convertToIntegerList(separatedWinningNumbers);
+
+        OutputView.printInputBonusNumber();
         int bonusNumber = InputView.inputBonusNumber();
         new WinningNumbers(winningNumbers, bonusNumber);
     }
 
     private void setupLottoMachine() {
+        OutputView.printInputSpend();
         int spend = InputView.inputSpend();
         int lottoTicketCount = spend / 1000;
         List<Integer> lottoNumbers = Utils.generateLottoNumbers();
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class OutputView {
+    public static void printInputSpend() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printInputWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printInputBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+}

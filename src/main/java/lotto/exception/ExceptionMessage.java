a16c210c1e2b93e407a4b245fb59cd2532ef0326
SungWoo <sungwoo166@gmail.com>
2023-11-06 13:15:40 +0900

    feat: Add error messages for Lotto game

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -5,7 +5,11 @@ public enum ExceptionMessage {
     RANDOM_NUMBER_GENERATION_ERROR("Error while generating Random Numbers: "),
     INVALID_LENGTH_RANDOM_NUMBER("Random Number must be %d long"),
     INVALID_PURCHASE_AMOUNT("Purchase amount must be in increments of 1000"),
-    INVALID_RANGE_OF_BONUS_NUMBER("Purchase amount must be in increments of 1000"),
+    INVALID_RANGE_OF_BONUS_NUMBER("Bonus number must be between 1~45"),
+    INVALID_INPUT_NOT_INTEGER_ERROR("Enter only Integer"),
+    INVALID_NUMBER_RANGE("Lotto Number must be between 1~45"),
+    EMPTY_INPUT("Winning number is empty"),
+    INVALID_WINNING_NUMBER_LENGTH("Winning number must have 6 number"),
     INVALID_DUPLICATE_NUMBER("Duplicate in Lotto Numbers");
 
 

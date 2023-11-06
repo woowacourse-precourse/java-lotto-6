1d5f793a2b9b070b1cdd8878c6488d2835b33dae
sangdol <priceelees@gmail.com>
2023-11-06 22:18:51 +0900

    refactor(LottoResult): 통계량을 문자열 형태로 제공하던 기능 제거 및 각 결과의 갯수 리스트를 반환하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,38 +1,12 @@
 package lotto.domain;
 
-import static lotto.domain.constant.DomainConstant.NEW_LINE;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import lotto.domain.money.Money;
 
 public record LottoResult(Map<LottoPrizes, Long> result) {
-    private static final String LEFT_PARENTHESIS = "(";
-    private static final String RIGHT_PARENTHESIS = ")";
-    private static final String SPACE = " ";
-    private static final String DASH_WITH_SPACE = " - ";
-    private static final String COUNT_TO_KOREAN = "개";
-
-
-    public String getStatistics() {
-        return Arrays.stream(LottoPrizes.values())
-                .map(this::displayPrizes)
-                .collect(Collectors.joining(NEW_LINE));
-    }
-
-    private String displayPrizes(final LottoPrizes lottoPrizes) {
-        final long winCount = result.get(lottoPrizes);
-        return lottoPrizes.getTerm()
-                + SPACE
-                + LEFT_PARENTHESIS
-                + lottoPrizes.winningAmountToString()
-                + RIGHT_PARENTHESIS
-                + DASH_WITH_SPACE
-                + winCount
-                + COUNT_TO_KOREAN;
-    }
-
     public Money getTotalRevenue() {
         return Arrays.stream(LottoPrizes.values())
                 .map(this::getRevenue)
@@ -43,5 +17,9 @@ public record LottoResult(Map<LottoPrizes, Long> result) {
         final long winCount = result.get(lottoPrizes);
         final Money winAmount = lottoPrizes.getWinningAmount();
         return winAmount.multiplyByCount(winCount);
+    }
+
+    public List<Long> getWinCountOfEachPrize() {
+        return new ArrayList<>(result.values());
     }
 }

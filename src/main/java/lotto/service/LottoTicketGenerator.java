cde729ad20a55c4d1ce7099ff478493fb12d6163
nhn1030 <shgusska12@gmail.com>
2023-11-07 03:27:24 +0900

    feat: generateLottoTicket 메서드 정의

--- src/main/java/lotto/service/LottoTicketGenerator.java
+++ src/main/java/lotto/service/LottoTicketGenerator.java
@@ -1,5 +1,19 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoTicketGenerator {
+    // 구매한 금액 기반 로또 티켓을 생성
+    public List<List<Integer>> generateLottoTicket(int purchaseAmount) {
+        int totalTicketCount = purchaseAmount / 1000;
+        List<List<Integer>> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < totalTicketCount; i++) {
+            List<Integer> lottoNumbers = generateLottoNumbers();
+            lottoTickets.add(lottoNumbers);
+        }
+        return lottoTickets;
+    }
 }

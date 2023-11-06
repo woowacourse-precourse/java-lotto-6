a6a30507911b0cf75e842e2f1ce16dd180c430d2
현 <sohyun95277@gmail.com>
2023-11-06 14:48:59 +0900

    feat(Customer): 구매한 로또들의 결과 생성 기능
    checkWinningResult: WinningNumbers 객체의 checkPrize 메서드를 통해
    모든 로또의 당첨 결과 확인

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -2,8 +2,12 @@ package lotto.domain;
 
 import static lotto.constant.message.ErrorMessage.NOT_MONEY;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import lotto.constant.Prize;
 
 public class Customer {
     private List<Lotto> lotteryTicket;
@@ -18,6 +22,21 @@ public class Customer {
         if (money == 0) {
             throw new IllegalArgumentException(NOT_MONEY.getMessage());
         }
+    }
+
+    public Map<Prize, Integer> checkWinningResult(WinningNumbers winningNumbers) {
+        Map<Prize, Integer> result = initResult();
+        lotteryTicket.stream()
+                .map(winningNumbers::checkPrize)
+                .forEach(prize -> result.put(prize, result.get(prize) + 1));
+        return result;
+    }
+
+    private Map<Prize, Integer> initResult() {
+        Map<Prize, Integer> result = new LinkedHashMap<>();
+        Arrays.stream(Prize.values())
+                .forEach(prize -> result.put(prize, 0));
+        return result;
     }
 
     public int getMoney() {

754680093f38b3820475dee21e15bd9832c8dc69
koomchang <leahpar0401@gmail.com>
2023-11-06 21:10:51 +0900

    로또 결과를 담은 클래스 생성

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,2 +1,22 @@
-package lotto.model;public class LottoResult {
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResult {
+    private final List<Lotto> lottos;
+
+    public LottoResult(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public Map<Rank, Integer> checkResult(WinningLotto winningLotto) {
+        Map<Rank, Integer> result = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            Rank rank = Rank.getRankByLotto(lotto, winningLotto);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+        return result;
+    }
 }

0242ee64a57ddde52b8d69c88e652bf9e13f847b
ghsyn <swanshawn715@gmail.com>
2023-11-07 02:00:00 +0900

    Feat: added function to remove duplicate numbers and sort

--- src/main/java/lotto/NumberPicker.java
+++ src/main/java/lotto/NumberPicker.java
@@ -1,15 +1,25 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberPicker {
+    private List<Integer> numberGroup = new ArrayList<>();
     private final int START_RANGE = 1;
     private final int END_RANGE = 45;
     private final int COUNT = 6;
 
     private List<Integer> getRandomNum() {
-        return Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, COUNT);
+        while (numberGroup.size() != 6) {
+            numberGroup = Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, COUNT);
+            numberGroup.stream().distinct().collect(Collectors.toList());
+        }
+        Collections.sort(numberGroup);
+        return numberGroup;
+
     }
 }

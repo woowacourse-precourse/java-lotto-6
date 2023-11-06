5957ebe8ddb5aaf1d71eb8f2460fd49ac0fbd220
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 19:14:09 +0900

    feat(LottoConstance) : 당첨 상수 추가

--- src/main/java/lotto/LottoConstance.java
+++ src/main/java/lotto/LottoConstance.java
@@ -5,8 +5,11 @@ public enum LottoConstance {
     MAX_NUMBER_LOTTO_RANGE(45),
     LOTTO_SIZE(6),
     MIN_PAYMENT(1000),
-    MAX_PAYMENT(100000);
-
+    MAX_PAYMENT(100000),
+    FIRST_WINING_COUNT(6),
+    SECOND_AND_THIRD_WINING_COUNT(5),
+    FOURTH_WINING_COUNT(4),
+    FIFTY_WINING_COUNT(3);
     private final int number;
 
     LottoConstance(int number) {

8466be0811b956e0c5204e7ef34368556b9cda69
hoyeon <highoption@naver.com>
2023-11-06 21:09:57 +0900

    feat(Money) : 입력 받은 돈이 숫자 인지 처리할 isInteger 메서드 생성
    입력 받은 금액은 숫자여야 하기 때문에 검증 해줄 isInteger 생성

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -4,7 +4,19 @@ public class Money {
     private final String money;
 
     public Money(String money) {
+        validateMoney(money);
         this.money = money;
     }
+
+    private static void validateMoney(String money) {
+        isInteger(money);
+    }
+
+    private static void isInteger(String money) {
+        if (!money.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+    }
+
 
 }

9e21b485e731018aa86efa19aeee4393860bb895
SeungGwa123 <skwan123@naver.com>
2023-11-07 04:19:27 +0900

    Fix: Update askMoney
    로또 구매 금액을 장수가 아닌 원래 금액으로 다시 설정

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -4,15 +4,25 @@ import Input.Input;
 import Output.Output;
 import lotto.BuyLotto;
 
+import java.util.List;
+
 public class Game {
     private static Input input = new Input();
     private static Output output = new Output();
     private static BuyLotto buylotto = new BuyLotto();
     private static int lotto_number;
+    private static List<Integer> answer_number;
+    private static int bonus_number;
 
     public void askMoney() {
         lotto_number = input.lottoBuyMoney();
         output.buyNumberPaperAns(lotto_number);
         buylotto.buyAllLotto(lotto_number);
+        lotto_number *= 1000;
+    }
+
+    public void askNumber() {
+        answer_number = input.answerNumber();
+        bonus_number = input.bonusNumber();
     }
 }

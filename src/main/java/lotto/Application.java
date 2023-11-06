095a4b2fb2851b7f779d0136b243cda3159ac442
SunHyeok <cghhld@naver.com>
2023-11-06 16:28:45 +0900

    feat: 프로그램 실행 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.view.LottoView;
+import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoView view = new LottoView();
+
+        LottoController controller = new LottoController(view);
+
+        controller.run();
     }
 }
+

a747e0bd2710a0bb80c1f7131192df4587143b45
Junobee25 <wnsdhqo@naver.com>
2023-11-07 00:31:59 +0900

    refactor(OutputView): lottoNumbersResult 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,14 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class OutputView {
-    public static void lottoNumbersResult(List<List<Integer>> lotto) {
-
+    public static void lottoNumbersResult(List<Integer> lotto) {
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+        for (Integer number : lotto) {
+            joiner.add(number.toString());
+        }
+        System.out.println(joiner.toString());
     }
 }

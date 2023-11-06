e783ee520ed4f2018975943261d1b579c1e86123
muyaaho <skanthsu715@gmail.com>
2023-11-06 10:37:54 +0900

    feat(lotto): 당첨 번호 입력을 List<String>으로 변경하는 기능 추가

--- src/main/java/lotto/InputValidate.java
+++ src/main/java/lotto/InputValidate.java
@@ -2,18 +2,28 @@ package lotto;
 
 import static java.lang.Integer.parseInt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputValidate {
-    public boolean isThereGap(String input) {
+    public String isThereGap(String input) {
         try {
             parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다. 프로그램이 종료됩니다.");
         }
-        return true;
+        return input;
     }
 
     public int changeInt(String s){
         return parseInt(s);
+    }
+
+    public List<String> changeList(String input){
+        String[] splitNums = input.split(",");
+        return Arrays.stream(splitNums).collect(Collectors.toList());
     }
 
 
--- src/test/java/lotto/InputValidateTest.java
+++ src/test/java/lotto/InputValidateTest.java
@@ -3,21 +3,31 @@ package lotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InputValidateTest {
+    private InputValidate inputValidate;
+    @BeforeEach
+    void setUp(){
+        inputValidate = new InputValidate();
+    }
     @Test
     void 보너스_숫자의_갯수가_1개인지_확인(){
-        InputValidate inputValidate = new InputValidate();
         assertThrows(IllegalArgumentException.class, () -> inputValidate.isThereGap(" 1 2"));
         assertThrows(IllegalArgumentException.class, () -> inputValidate.isThereGap("1,2,3"));
         assertThrows(IllegalArgumentException.class, () -> inputValidate.isThereGap(""));
-        assertEquals(true, inputValidate.isThereGap("1"));
+        assertEquals("1", inputValidate.isThereGap("1"));
     }
 
     @Test
     void String이_int로_리턴되는지_확인(){
-        InputValidate inputValidate = new InputValidate();
         assertEquals(8000, inputValidate.changeInt("8000"));
+    }
+
+    @Test
+    void 여섯개의개의_숫자가_List로_변환되는지_확인(){
+        assertEquals(List.of("1","2","3","4","5","6"), inputValidate.changeList("1,2,3,4,5,6"));
     }
 }

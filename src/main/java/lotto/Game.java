83ff144821de5dc21e2c23c49aea2cfd46af5594
hongkikii <hyangmeeh@icloud.com>
2023-11-06 22:13:55 +0900

    refactor: 로또 구매 가격 로컬 변수에서 인스턴스 변수로 변경

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -4,12 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
 
+    private Integer buyingPrice;
+
     public void run() {
         saveBuyingPrice();
+        giveLotto();
     }
 
     private void saveBuyingPrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        String buyingPrice = Console.readLine();
+        String readLine = Console.readLine();
+        buyingPrice = Integer.parseInt(readLine);
+    }
+
+    private void giveLotto() {
+
     }
 }

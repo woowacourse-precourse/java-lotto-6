29e8846483e3c7880a31e528fccc2217c7bb34e4
gonixxxx <akdntm946@gmail.com>
2023-11-06 18:16:14 +0900

    refactor(OutputView): update showLottos

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,13 +6,16 @@ import lotto.model.Money;
 
 public class OutputView {
 
-    private static final String SHOW_LOTTO = "%d개를 구매했습니다.\n";
+    private static final String SHOW_LOTTO = "\n%d개를 구매했습니다.\n";
 
     public void showLottos(Lottos lottos, Money money) {
         System.out.printf(SHOW_LOTTO, money.calNumberOfLotto());
-        for (Lotto lotto : lottos.getLottos()) {
-            lotto.showLotto();
-        }
+        lottos.showLottos();
+        printLine();
+    }
+
+    public void printLine() {
+        System.out.println();
     }
 
 }

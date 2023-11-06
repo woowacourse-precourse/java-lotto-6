2622713cb03819c6be638988802cef402fe81f1e
metishonora <metishonora@icloud.com>
2023-11-06 17:46:02 +0900

    test: 당첨 번호 입력 테스트 추가

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -28,5 +29,24 @@ class LottoControllerTest {
         assertThatThrownBy(inputView::requestInteger)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("구입 금액은 정수로 주어져야 합니다.");
+
+        Console.close();
+    }
+
+    @DisplayName("숫자가 아닌 로또 번호")
+    @Test
+    void requestWinningLotto() {
+        InputView inputView = InputView.getInstance();
+        String input = "1,4,2,10,6,42j";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        assertThatThrownBy(inputView::requestIntegers)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호는 정수로 주어져야 합니다.");
+
+        Console.close();
     }
 }

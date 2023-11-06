3064dfca2267af72f05b402b9a017feb32aea575
Ukj0ng <xb253q@gmail.com>
2023-11-06 19:34:04 +0900

    feat(LottoService): Lotto와 Lottos를 생성하는 기능 구현
    구현한 기능:
    - Lotto를 생성
    - 생성한 Lotto들을 Lotto의 element로 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,2 +1,30 @@
-package lotto.service;public class LottoService {
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+import lotto.model.LottoCondition;
+import lotto.model.Lottos;
+
+public class LottoService {
+    private static final int MIN_NUMBER = LottoCondition.MIN_NUMBER.getNumber();
+    private static final int MAX_NUMBER = LottoCondition.MAX_NUMBER.getNumber();
+    private static final int COUNT = LottoCondition.COUNT.getNumber();
+
+    public Lottos generateLottos(int lottoCount) {
+        Lottos lottos = new Lottos();
+        for(int i = 0; i < lottoCount; i++) {
+            Lotto lotto = generateLotto();
+            lottos.addLotto(lotto);
+        }
+        return lottos;
+    }
+
+    private Lotto generateLotto() {
+        return new Lotto(
+            Randoms.pickUniqueNumbersInRange(
+                MIN_NUMBER,
+                MAX_NUMBER,
+                COUNT)
+        );
+    }
 }

458eb4961b47f333e60216747123c6cb1ccab85f
SeungGwa123 <skwan123@naver.com>
2023-11-07 05:07:26 +0900

    Feat: Add calculateRevenue
    수익률 출력하는 함수 작성

--- src/main/java/Money/Money.java
+++ src/main/java/Money/Money.java
@@ -1,2 +1,17 @@
-package Money;public class Money {
+package Money;
+
+import lotto.Lotto;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Money {
+    public void calculateRevenue(int lottoBuy, long correctMoney) {
+        double revenue = (correctMoney / lottoBuy) * 100;
+        revenue = Math.round(revenue * 100.0) / 100.0;
+        DecimalFormat formatter = new DecimalFormat("#,###.###");
+        String totalRevenue = formatter.format(revenue);
+        System.out.println("총 수익률은 "+totalRevenue+"%입니다.");
+    }
 }

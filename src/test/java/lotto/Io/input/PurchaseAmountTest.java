bb6f781259f4154adc1ba2745230d2919180ec36
chan-won <clw8679@naver.com>
2023-11-06 23:48:57 +0900

    test: Lotto 구입 금액 입력 기능 테스트

---
+++ src/test/java/lotto/Io/input/PurchaseAmountTest.java
@@ -0,0 +1,36 @@
+package lotto.Io.input;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.validation.InputValidator;
+import org.junit.jupiter.api.Test;
+
+public class PurchaseAmountTest {
+    @Test
+    void _1000원_단위로_나누어_떨어지지_않는_경우_예외가_발생한다() {
+        int purchaseAmount = 11500;
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(purchaseAmount))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void _100000원이_초과인_경우_구매시_예외가_발생한다() {
+        int purchaseAmount = 100001;
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(purchaseAmount))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void _1000원_미만인_경우_구매시_예외가_발생한다() {
+        int purchaseAmount = 999;
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(purchaseAmount))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_아닌_경우_예외가_발생한다() {
+        String input = "money";
+        assertThatThrownBy(() -> InputValidator.validateInteger(input))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}

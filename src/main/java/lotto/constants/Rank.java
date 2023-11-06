79429a152a0a790702168bc08b4ac8e972e1d953
sangchu <didi1484@gmail.com>
2023-11-06 18:17:40 +0900

    feat: Enum 메시지 추가

--- src/main/java/lotto/constants/Rank.java
+++ src/main/java/lotto/constants/Rank.java
@@ -1,22 +1,28 @@
 package lotto.constants;
 
 public enum Rank {
-    FIRST(2000000000, "6개 일치 (2,000,000,000원) - "),
-    SECOND(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(1500000, "5개 일치 (1,500,000원) - "),
-    FOURTH(50000, "4개 일치 (50,000원) - "),
-    FIFTH(5000, "3개 일치 (5,000원) - ");
+    FIRST(2000000000, 6, "6개 일치 (2,000,000,000원) - "),
+    SECOND(30000000, 5, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(1500000, 5, "5개 일치 (1,500,000원) - "),
+    FOURTH(50000, 4, "4개 일치 (50,000원) - "),
+    FIFTH(5000, 3, "3개 일치 (5,000원) - ");
 
     private final String message;
     private final int prize;
+    private final int count;
 
-    Rank(int prize, String Message) {
+    Rank(int prize, int count, String Message) {
         this.prize = prize;
         this.message = Message;
+        this.count = count;
     }
 
     public int getPrize() {
         return prize;
+    }
+
+    public int getCount() {
+        return count;
     }
 
     public String getMessage() {

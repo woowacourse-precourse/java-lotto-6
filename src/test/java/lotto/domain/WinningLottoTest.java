b2198f0f732c885f32a365801f4e7e9697ea3d66
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-06 16:55:35 +0900

    test: 당첨 로또 테스트 코드 작성

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,4 +1,47 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class WinningLottoTest {
+
+    @DisplayName("당첨 로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverSize() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 8))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 5), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호가 1보다 작으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByUnderRange() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(0, 1, 2, 3, 4, 5), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호가 45보다 크면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverRange() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(41, 42, 43, 44, 45, 46), 40))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 로또 번호에 있는 번호면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

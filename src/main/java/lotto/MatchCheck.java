8c33c2725216282d73a02b23240cf11db90b5eb7
cousim46 <wjdghldns24@naver.com>
2023-11-06 22:59:19 +0900

    fix : 당첨 금액 데이터 타입 변경

--- src/main/java/lotto/MatchCheck.java
+++ src/main/java/lotto/MatchCheck.java
@@ -4,14 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 public class MatchCheck {
-    private final static Long MATCHES_3 = 5000L;
-    private final static Long MATCHES_4 = 50_000L;
-    private final static Long MATCHES_5 = 1_500_000L;
-    private final static Long MATCHES_5_PLUS_BONUS = 30_000_000L;
-    private final static Long MATCHES_6 = 2_000_000_000L;
+    private final static Integer MATCHES_3 = 5000;
+    private final static Integer MATCHES_4 = 50_000;
+    private final static Integer MATCHES_5 = 1_500_000;
+    private final static Integer MATCHES_5_PLUS_BONUS = 30_000_000;
+    private final static Integer MATCHES_6 = 2_000_000_000;
     private final static int MATCH_PLUS = 1;
 
-    public static void win(Map<Long, Integer> resultMap, int count) {
+    public static void win(Map<Integer, Integer> resultMap, int count) {
         switch (count) {
             case 3 -> resultMap.put(MATCHES_3, resultMap.get(MATCHES_3) + MATCH_PLUS);
             case 4 -> resultMap.put(MATCHES_4, resultMap.get(MATCHES_4) + MATCH_PLUS);

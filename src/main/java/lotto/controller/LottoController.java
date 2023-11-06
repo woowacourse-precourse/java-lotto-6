03d9ea572691fa07d7bb2b882dd05b25b10edfc4
UDADDY <gustmd5715@gmail.com>
2023-11-06 15:49:40 +0900

    feat: 컨트롤러에 로직 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,26 @@
 package lotto.controller;
 
-public class LottoController {
-    public void start(){
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
+import java.util.List;
+import java.util.random.RandomGenerator;
+
+public class LottoController {
+    public void start() {
+        Player player = new Player(InputView.inputCash());
+        OutputView.printAttempt(player.getCash());
+        player.lottoDraw();
+        OutputView.printLotto(player.getLottos());
+
+        Lotto winningLotto = InputView.inputWinningLotto();
+        Bonus bonus = InputView.inputBonus();
+
+        List<LottoResult> lottoResults = Comparator.compare(player.getLottos(), winningLotto, bonus);
+        OutputLottoResult outputLottoResult = new OutputLottoResult(lottoResults);
+
+        OutputView.printStat(outputLottoResult);
+        OutputView.printRateOfReturn(Calculator.calculateRateOfReturn(outputLottoResult.getTotalReturn(), player.getCash()));
     }
 }

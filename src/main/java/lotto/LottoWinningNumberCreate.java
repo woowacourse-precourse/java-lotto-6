ba9dd65d55d2e0cf27ea70afd20170d9c09d11a0
dukcode <duk9741@gmail.com>
2023-11-06 15:35:21 +0900

    feat: 로또 당첨 번호 생성 클래스 구현
    - 로또 번호 갯수 검증 로직 추가
    - 로또 번호 범위 검증 로직 추가

---
+++ src/main/java/lotto/LottoWinningNumberCreate.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoWinningNumberCreate {
+
+    private List<Integer> numbers;
+
+    public LottoWinningNumberCreate(List<Integer> numbers) {
+        this.numbers = new ArrayList<>();
+
+        validateNumbersSize(numbers);
+
+        for (int i = 0; i < numbers.size(); ++i) {
+            int number = numbers.get(i);
+            validNumber(number);
+            this.numbers.add(number);
+        }
+        this.numbers = numbers;
+    }
+
+    private static void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != LottoConstant.LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("당첨 번호는 " + LottoConstant.LOTTO_NUMBER_COUNT + "개여야 합니다.");
+        }
+    }
+
+    private void validNumber(int number) {
+        if (number < LottoConstant.LOTTO_START_NUMBER || number > LottoConstant.LOTTO_END_NUMBER) {
+            throw new IllegalArgumentException(
+                    "당첨 번호는 " + LottoConstant.LOTTO_START_NUMBER + " 이상,"
+                            + LottoConstant.LOTTO_END_NUMBER + " 이하여야 합니다.");
+        }
+    }
+}

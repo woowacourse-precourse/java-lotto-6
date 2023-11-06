c0083942f26169e3c610c2fad3ef701d335d3201
doka <posmane5@gmail.com>
2023-11-06 19:02:34 +0900

    feat(UI) : "view) 발행된 로또 출력" 기능을 담당하는 UI클래스에 출력 기능 추가
    - displayLottoTicketsHistory :
    로또 정보가 담긴 객체를 받아서 사용자가 보도록 정보를 출력하는 기능의 메소드
    
    - displayFormatMessage : 저장된 포멧팅 문자열 상수를 출력하는 메소드

--- src/main/java/lotto/view/UI.java
+++ src/main/java/lotto/view/UI.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.view.constant.Instructions;
 
 public class UI {
@@ -10,9 +12,23 @@ public class UI {
         String lottoPurchaseRequest  = Console.readLine();
         return lottoPurchaseRequest;
     }
-
     public static void displayMessage(final Instructions instructions) {
         System.out.println(instructions.getPrompts());
     }
 
+    public static void displayFormatMessage(final Instructions instructions, Object... args) {
+        System.out.printf(instructions.getPrompts(), args);
+    }
+
+
+    public static void displayLottoTicketsHistory(List<Lotto> LottoTickets) {
+        //로또구매 내역 객체인 List<Lotto> 형태의 객체를 요구조건 대로 출력해야 된다.
+        //구매 갯수인 List의 크기 size()메소드 이용
+        //구매 내역은 리스트의 각 요소인 Lotto 객체 내부의 로또 리스트를 읽는다. -> get 메소드 필요하다
+        displayFormatMessage(Instructions.MESSAGE_OF_PURCHASE_COUNT, LottoTickets.size());
+        for(Lotto lotto : LottoTickets) {
+            System.out.println(lotto.getLottoNumbers());
+        }
+
+    }
 }

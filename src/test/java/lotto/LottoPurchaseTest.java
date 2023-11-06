f3bd678f17573a47e91e15526c8038e3f46da16c
dukcode <duk9741@gmail.com>
2023-11-06 14:46:42 +0900

    test: LottoPurchase 생성 관련 테스트 추가

---
+++ src/test/java/lotto/LottoPurchaseTest.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LottoPurchaseTest {
+
+    @Test
+    public void 로또_1장_금액으로_나누어지는_금액으로_LottoPurchase_생성이_가능하다() throws Exception {
+        // given
+        long purchaseAmount = LottoConstant.LOTTO_TICKET_PRICE * 8;
+
+        // when
+        // then
+        assertThatCode(() -> {
+            new LottoPurchase(purchaseAmount);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 로또_1장_금액으로_나누어지지_않는_금액으로_LottoPurchase_생성시_예외가_발생한다() throws Exception {
+        // given
+        long purchaseAmount = LottoConstant.LOTTO_TICKET_PRICE * 8 + LottoConstant.LOTTO_TICKET_PRICE / 2;
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new LottoPurchase(purchaseAmount);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}

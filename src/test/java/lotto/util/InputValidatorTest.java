cb4d489fde3116e2245e2aec3e211f490aa7bfdf
maark1106 <mark1106@naver.com>
2023-11-06 21:21:29 +0900

    test : 구매 금액이 정수 타입이 아닐 떄 오류 발생 테스트

--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -5,14 +5,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
-    @DisplayName("구매 금익이 공백일 때 오류 발생")
+    @DisplayName("구매 금액이 공백일 때 오류 발생")
     @Test
     void validateInputEmpty(){
         assertThatThrownBy(()->validatePurchaseAmount(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] : 입력 값은 공백일 수 없습니다");
     }
+
+    @DisplayName("구매 금액이 정수타입이 아닐 떄 오류 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc","123j","!!","천원","1000.1"})
+    void purchaseAmountNotIntegerTypeTest(String purchaseAmount){
+        assertThatThrownBy(()->validatePurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] : 입력 값은 정수 타입이어야 합니다");
+    }
+
 }

db9603de7ee5e03ec74359bac1241b0dab033c1c
JW0321 <digneotn123@gmail.com>
2023-11-06 18:00:25 +0900

    feat: 출력 문자열 상수 값 추가

---
+++ src/main/java/lotto/view/LottoView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class LottoView { //게임 시작 메세지 출력, 구입 금액, 당첨 번호, 보너스 번호 입력 요청, 당첨 통계 및 결과 메세지 출력, 에러 메세지 출력
+
+    private static final String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
+    private static final String LOTTO_PURCHASE_MESSAGE = "%d개를 구매했습니다.";
+    private static final String LOTTO_NUMBER_PROMPT = "당첨 번호를 입력해 주세요";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요";
+    private static final String LOTTO_STATISTICS_HEADER = "당첨 통계";
+    private static final String SEPARATOR = "---";
+    private static final String COUNT_PRIZE_FORMAT = "%d개 일치 (%s원) - %d개";
+    private static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+}

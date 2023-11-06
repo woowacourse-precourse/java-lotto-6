cb6cd3c68abbd2f6032a0c5d4575c9163839987e
LeeJJ07 <jkj9513@naver.com>
2023-11-06 11:20:16 +0900

    feat : 당첨된 복권 수량 및 총 수익률 출력 뷰 기능 구현
    당첨된 복권의 수량을 출력해주고 총 수익률 출력을 출력 형식에 맞춰서 구현

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -11,7 +11,7 @@ public class Output {
 	private final String THIRD_PRIZE_WINNER_MESSAGE = "5개 일치 (1,500,000원) - %d개\n";
 	private final String FOURTH_PRIZE_WINNER_MESSAGE = "4개 일치 (50,000원) - %d개\n";
 	private final String FIFTH_PRIZE_WINNER_MESSAGE = "3개 일치 (5,000원) - %d개\n";
-	private final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1lf%%입니다.";
+	private final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 	
 	
 	public void lottos(ArrayList<ArrayList<Integer>>lotto) {
@@ -20,5 +20,21 @@ public class Output {
 			System.out.println(lotto.get(i).toString());
 		}
 		System.out.println();
+	}
+	
+	public void statistics(int[]count, double profitRate) {
+		System.out.println(WINNING_STATISTICS_MESSAGE);
+		prizeWinner(count);
+		totalProfitRate(profitRate);
+	}
+	private void prizeWinner(int []count) {
+		System.out.printf(FIFTH_PRIZE_WINNER_MESSAGE, count[4]);
+		System.out.printf(FOURTH_PRIZE_WINNER_MESSAGE, count[3]);
+		System.out.printf(THIRD_PRIZE_WINNER_MESSAGE, count[2]);
+		System.out.printf(SECOND_PRIZE_WINNER_MESSAGE, count[1]);
+		System.out.printf(FIRST_PRIZE_WINNER_MESSAGE, count[0]);
+	}
+	private void totalProfitRate(double profitRate) {
+		System.out.printf(TOTAL_PROFIT_RATE_MESSAGE, profitRate);
 	}
 }

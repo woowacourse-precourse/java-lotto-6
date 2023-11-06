9849743c44e22db0cae95b984cb707f456e8c001
김민정 <mjeong@sookmyung.ac.kr>
2023-11-06 16:06:45 +0900

    feat: 당첨 통계를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.ResultCalculator;
 
 public class OutputView {
     public static void printIssuedLotto(List<Lotto> lottos) {
@@ -10,5 +11,13 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public static void printResult(ResultCalculator resultCalculator) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(resultCalculator);
+        System.out.printf("총 수익률은 %.2f%%입니다.", resultCalculator.getProfitRate());
     }
 }

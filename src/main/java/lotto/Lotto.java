2c2916abb63df8b0c62060c1642b93297acbc171
kseenyoung <kseenyoung@gmail.com>
2023-11-06 23:51:59 +0900

    feat: 로또 번호 출력 메서드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void printNumbers(){
+        System.out.println("[" + String.join(", ", (CharSequence) numbers) + "]");
+    }
 }

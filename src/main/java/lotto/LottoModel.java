6a6cb6a8ab298cd9237e32843cb01941748f749b
namaek_2 <science4588@gmail.com>
2023-11-06 15:06:06 +0900

    feat : 구입한 로또만큼 발행 기능 수정
    오름차순 정렬 출력 기능 추가

--- src/main/java/lotto/LottoModel.java
+++ src/main/java/lotto/LottoModel.java
@@ -1,11 +1,9 @@
 package lotto;
-import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Randoms; import java.util.ArrayList; import java.util.Collections;
 
-import java.util.ArrayList;
-import java.util.List;
 public class LottoModel {
     private int lottoPrice;
-    private List<Lotto> boughtLottos;
+    private ArrayList<Lotto> boughtLottos;
 
     public LottoModel() {
         lottoPrice = LottoControl.inputLottoPrice();
@@ -15,11 +13,12 @@ public class LottoModel {
         LottoView.printGeneratedLottos(boughtLottos);
     }
 
-    private List<Lotto> generateLottos(int lottoPrice) {
-        List<Lotto> generatedLottos = new ArrayList<>();
+    private ArrayList<Lotto> generateLottos(int lottoPrice) {
+        ArrayList<Lotto> generatedLottos = new ArrayList<>();
 
         for(int i = 0; i < lottoPrice / 1000; i++) {
             Lotto generatedLottoNumber = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(generatedLottoNumber.getNumbers());
             generatedLottos.add(generatedLottoNumber);
         }
 

ac901a70105b69bef857bf220bc52072ba8a2455
gusgh075 <gusgh075@naver.com>
2023-11-06 11:13:45 +0900

    FIXED(LottoGenerator) : 함수목록을 static으로 변경

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    public Lotto generateRandomLotto(){
+    public static Lotto generateRandomLotto(){
         List<Integer> randNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(randNumbers);
     }
-    public List<Lotto> generateLottos(int money){
+    public static List<Lotto> generateLottos(int money){
         List<Lotto> lottos=new ArrayList<>();
         int n=money/1000;
         for(int i=0;i<n;i++){

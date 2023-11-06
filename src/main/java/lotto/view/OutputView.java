af0b0a41aebbfbc59ab2c8c87289ed0bb1167957
EHCTH <lees90896@gmail.com>
2023-11-06 14:16:14 +0900

    controlMain And Lotto And Player 클래스에 대해 결과 값을 얻으면 출력하게끔 나눴음

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,44 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+import lotto.model.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public static void printBuyLotto(List<List<Integer>> mainLotto, int count) {
+        System.out.println(count + "개를 구매했습니다.");
+        for (List<Integer> lotto: mainLotto) {
+            System.out.println(lotto);
+        }
+        System.out.println();
+    }
+
+    public static void printNumbers(List<Integer> numbers) {
+        String output = numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        System.out.println(output);
+        System.out.println();
+    }
+
+    public static void printBonusNumber(String bonusNumber) {
+        System.out.println(bonusNumber);
+        System.out.println();
+    }
+
+    public static void printWinningResultPreFace() {
+        System.out.println("당첨통계\n---");
+    }
+
+    public static void printWinningResult(String sentence, int matchCount) {
+        System.out.println(sentence + " " + matchCount + "개");
+
+    }
+
+    public static void printWinningTotal(Player player) {
+        int price = player.getPrice();
+        long total = player.getTotal();
+        double divTotal = (double) (total - price) / price;
+        System.out.printf("총 수익률은 %.1f%%입니다.", (100 + divTotal * 100));
+    }
 }

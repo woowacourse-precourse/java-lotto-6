34250c903e9513ef2bcbfe0d9f3ef92c5ed4406e
ddolboghi <rhdfyd128@gmail.com>
2023-11-06 19:38:14 +0900

    refactor: LottoWinningRule 메서드 수정 및 추가

--- src/main/java/lotto/common/config/LottoWinningRule.java
+++ src/main/java/lotto/common/config/LottoWinningRule.java
@@ -1,14 +1,14 @@
 package lotto.common.config;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 public enum LottoWinningRule {
     RANK_ONE(1, 6, false, 2000000000),
     RANK_TWO(2, 5, true, 30000000),
     RANK_THREE(3, 5, false, 1500000),
     RANK_FOUR(4, 4, false, 50000),
-    RANK_FIVE(5, 3, false, 5000);
+    RANK_FIVE(5, 3, false, 5000),
+    UNRANKED(0, 0, false, 0);
 
     private final int rank;
     private final int matchNumber;
@@ -22,14 +22,19 @@ public enum LottoWinningRule {
         this.money = money;
     }
 
-    public static Optional<LottoWinningRule> getRank(int countMatch, boolean bonusMatch) {
+    public static LottoWinningRule getRank(int countMatch, boolean bonusMatch) {
         return Arrays.stream(values())
                 .filter(rule -> rule.matchNumber == countMatch && (countMatch != 5 || rule.bonusMatch == bonusMatch))
-                .findFirst();
+                .findFirst()
+                .orElse(LottoWinningRule.UNRANKED);
     }
 
     public int getRankValue() {
         return rank;
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
     }
 
     public int getMoney() {

20125a67bd212104e549e3a9ee2771921ecc336c
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 17:09:26 +0900

    feat: LottoResultCheckerTest getResult 테스트

---
+++ src/test/java/lotto/LottoResultCheckerTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoResultCheckerTest {
+    @DisplayName("로또 번호와 당첨 번호를 비교해 결과를 반환한다.")
+    @Test
+    public void getResult() throws Exception {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        LottoResultChecker lottoResultChecker = new LottoResultChecker(lotto, winningLotto);
+        Result result = lottoResultChecker.getResult();
+
+        assertThat(result).isEqualTo(Result.SECOND_PRIZE);
+    }
+}

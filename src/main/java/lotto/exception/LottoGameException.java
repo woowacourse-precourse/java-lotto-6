6a901b742458b8949f4121089316ea65b711c1fb
leesh <qwqw8019@naver.com>
2023-11-06 15:49:18 +0900

    feat: 콘솔 종료 로직 제거

--- src/main/java/lotto/exception/LottoGameException.java
+++ src/main/java/lotto/exception/LottoGameException.java
@@ -2,14 +2,11 @@ package lotto.exception;
 
 import static lotto.view.OutputView.notifyErrorMessage;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class LottoGameException extends IllegalArgumentException {
 
     private LottoGameException(ErrorMessage errorMessage) {
         super(errorMessage.getMessage());
         notifyErrorMessage(errorMessage.getMessage());
-        Console.close();
     }
 
     public static LottoGameException of(ErrorMessage errorMessage) {

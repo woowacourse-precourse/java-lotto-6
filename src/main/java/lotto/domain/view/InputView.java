5e8a16959241d8a8a47c9fd9a3aa23fa87204d45
transfer_kk <kseysh123@naver.com>
2023-11-06 19:28:30 +0900

    feat : Lotto를 구매할 돈을 입력받는 기능 추가

--- src/main/java/lotto/domain/view/InputView.java
+++ src/main/java/lotto/domain/view/InputView.java
@@ -1,2 +1,15 @@
-package lotto.domain.view;public class InputView {
+package lotto.domain.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.model.LottoPrice;
+
+public class InputView {
+
+    public int inputLottoPrice() {
+        System.out.println(OutputType.INPUT_PURCHASE_AMOUNT_MESSAGE.getValue());
+        String userInputPrice = Console.readLine();
+        return Integer.parseInt(userInputPrice);
+    }
+
+
 }

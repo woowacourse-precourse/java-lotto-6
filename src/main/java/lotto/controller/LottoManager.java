729ab58b07a8b701b579484e460d2bfd7dbc7e09
1000zoo <cjswldn99@gmail.com>
2023-11-07 02:55:26 +0900

    refactor(LottoManager): 생성자 변경
    - BonusNumber는 정답 번호에 포함되어있으면 안됨
    - 따라서 이를 검증해야하는데,
    - LottoManager가 LottoAnswer를 생성하면서 검증
    - LottoAnswer는 model로 정의했기에, Input과의 의존관계를 없애기 위해

--- src/main/java/lotto/controller/LottoManager.java
+++ src/main/java/lotto/controller/LottoManager.java
@@ -1,20 +1,69 @@
 package lotto.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import lotto.model.BonusNumber;
 import lotto.model.Lotto;
 import lotto.model.LottoAnswer;
 import lotto.model.Lottos;
+import lotto.model.PurchaseAmount;
 import lotto.model.Rank;
+import lotto.view.input.BonusNumberInput;
+import lotto.view.input.Input;
+import lotto.view.input.LottoAnswerInput;
 
 public class LottoManager {
+
+    private final static String DELIMITER = ",";
 
     private final Lottos lottos;
     private final LottoAnswer lottoAnswer;
 
-    public LottoManager(Lottos lottos, LottoAnswer lottoAnswer) {
-        this.lottos = lottos;
-        this.lottoAnswer = lottoAnswer;
+    public LottoManager(PurchaseAmount purchaseAmount) {
+        this.lottos = createLottos(purchaseAmount);
+        this.lottoAnswer = createLottoAnswer();
+    }
+
+    private Lottos createLottos(PurchaseAmount purchaseAmount) {
+        Lottos lottos = new Lottos(purchaseAmount);
+        return lottos;
+    }
+
+    private LottoAnswer createLottoAnswer() {
+        Lotto answer = readLotto();
+        BonusNumber bonusNumber = readBonusNumber(answer);
+        return new LottoAnswer(answer, bonusNumber);
+    }
+
+    private Lotto readLotto() {
+        Input input = new LottoAnswerInput();
+        String numbersInput = input.readLine();
+        List<Integer> numbers = Arrays.stream(numbersInput.split(DELIMITER))
+                .map(Integer::parseInt).toList();
+        return new Lotto(numbers);
+    }
+
+    private BonusNumber readBonusNumber(Lotto answer) {
+        Input input = new BonusNumberInput();
+        BonusNumber bonusNumber;
+        while (true) {
+            try {
+                String numberInput = input.readLine();
+                bonusNumber = new BonusNumber(Integer.parseInt(numberInput));
+                bonusNumberValidation(answer, bonusNumber);
+                break;
+            } catch (IllegalArgumentException ignore) {
+
+            }
+        }
+        return bonusNumber;
+    }
+
+    private void bonusNumberValidation(Lotto answer, BonusNumber bonusNumber) {
+        if (answer.isContainBonusNumber(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public List<Rank> calculateResults() {
--- src/test/java/lotto/controller/LottoManagerTest.java
+++ src/test/java/lotto/controller/LottoManagerTest.java
@@ -2,12 +2,9 @@ package lotto.controller;
 
 import static org.mockito.Mockito.mockStatic;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import lotto.model.BonusNumber;
-import lotto.model.Lotto;
-import lotto.model.LottoAnswer;
-import lotto.model.Lottos;
 import lotto.model.PurchaseAmount;
 import lotto.model.Rank;
 import org.junit.jupiter.api.AfterAll;
@@ -18,26 +15,29 @@ import org.mockito.MockedStatic;
 
 public class LottoManagerTest {
 
-    private static LottoManager lottoManager;
     private static MockedStatic<Randoms> randoms;
+    private static MockedStatic<Console> console;
 
     @BeforeAll
     private static void beforeAll() {
-        LottoAnswer lottoAnswer = new LottoAnswer(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new BonusNumber(7));
-        Lottos lottos = new Lottos(new PurchaseAmount(5));
-        lottoManager = new LottoManager(lottos, lottoAnswer);
         randoms = mockStatic(Randoms.class);
+        console = mockStatic(Console.class);
     }
 
     @AfterAll
     private static void afterAll() {
         randoms.close();
+        console.close();
     }
 
     @Test
     @DisplayName("등수 리스트가 잘 나오는 지 확인")
     void calculateResultsTest() {
         // given
+        console.when(Console::readLine)
+                .thenReturn("1,2,3,4,5,6")
+                .thenReturn("7");
+
         randoms.when(() -> Randoms.pickUniqueNumbersInRange(1, 45, 6))
                 .thenReturn(List.of(1, 2, 3, 4, 5, 6))  // 1등
                 .thenReturn(List.of(1, 2, 3, 4, 5, 7))  // 2등
@@ -45,10 +45,24 @@ public class LottoManagerTest {
                 .thenReturn(List.of(1, 2, 3, 4, 8, 9))  // 4등
                 .thenReturn(List.of(1, 2, 3, 7, 8, 9)); // 5등
 
+        PurchaseAmount purchaseAmount = new PurchaseAmount(5000);
+        LottoManager lottoManager = new LottoManager(purchaseAmount);
+
         // when
         List<Rank> ranks = lottoManager.calculateResults();
 
         //then
         System.out.println(ranks);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 이미 당첨번호에 포함되어 있으면 입력을 다시 받는다.")
+    void answerNumbersCantContainsBonusNumber() {
+        console.when(Console::readLine)
+                .thenReturn("1,2,3,4,5,6")
+                .thenReturn("6")
+                .thenReturn("7");
+        PurchaseAmount purchaseAmount = new PurchaseAmount(5);
+        LottoManager lottoManager = new LottoManager(purchaseAmount);
     }
 }

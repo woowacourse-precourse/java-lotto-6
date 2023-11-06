143811c50949df7fa77dc16c096a36a38e45cb3a
haiseong <wjdgotjd9908@gmail.com>
2023-11-07 03:23:05 +0900

    fix(LottoInputView): import 순서 변경

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,10 +1,10 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class LottoInputView {
     private static final String DELIMITER = ",";

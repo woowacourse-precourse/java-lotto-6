7b3a7c4e6a831f42035a6d0d0f45781ab1c909b3
styughjvbn <3338761@naver.com>
2023-11-06 18:45:54 +0900

    <feat> : 로또 번호 검증 구현 ------------------------------ 1. 1~45 범위 검증 구현 및 테스트 2. 중복 검증 구현 및 테스트

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -5,16 +5,34 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    protected final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        for (Integer number : numbers) {
+            validateNumberRange(number);
+        }
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져있어야합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.indexOf(numbers.get(i))!=i) {
+                throw new IllegalArgumentException("[ERROR] 중복된 숫자가 입력되었습니다.");
+            }
+        }
+    }
+
+    protected void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 

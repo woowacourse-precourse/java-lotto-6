69075524f5fe0adb293c4a9824fe9e4dcbb50d32
MinseokGo <rhalstjr1999@naver.com>
2023-11-06 23:40:30 +0900

    refactor($LottoController): 유효성 검증 로직 분리
    기존에 LottoController에서 수행하던 입력값 유효성 검증을 책임 분리하였습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,57 +1,53 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.Referee;
 import lotto.model.Lotto;
+import lotto.model.LottoManager;
+import lotto.model.Referee;
 import lotto.model.WinningLotto;
 import lotto.view.OutputView;
 
 public class LottoController {
-    private int money;
-    private final List<Lotto> lottos;
-    private WinningLotto winningLotto;
+    private final LottoManager lottoManager;
+    private final Validator validator;
     private final OutputView outputView;
     private final Referee referee;
 
     public LottoController() {
-        this.lottos = new ArrayList<>();
+        this.validator = new Validator();
+        this.lottoManager = inputPurchaseMoney();
         this.outputView = new OutputView();
         this.referee = new Referee();
     }
 
-    public void inputPurchaseMoney() {
+    public LottoManager inputPurchaseMoney() {
         String input;
         do {
             input = Console.readLine();
-        } while (validateMoneyInput(input));
-        money = Integer.parseInt(input);
-    }
-
-    public void createLottos() {
-        for (int i = 0; i < money / 1000; i++) {
-            List<Integer> numbers = generateLottoNumbers();
-            Lotto newLotto = new Lotto(numbers);
-            lottos.add(newLotto);
-        }
-        outputView.printCreateLottoNumbers(lottos);
+        } while (validator.validateMoneyInput(input));
+        final int money = Integer.parseInt(input);
+        final LottoManager lottoManager = new LottoManager(money);
+        lottoManager.getLottos()
+                .forEach(System.out::println);
+        return lottoManager;
     }
 
     public void createWinningLotto() {
         final List<Integer> winningNumbers = inputWinningLottoNumbers();
         final int bonusNumber = inputWinningLottoBonusNumber(winningNumbers);
-        this.winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        final WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        lottoManager.setWinningLotto(winningLotto);
     }
 
     public void getPrizeList() {
+        final List<Lotto> lottos = lottoManager.getLottos();
+        final WinningLotto winningLotto = lottoManager.getWinningLotto();
         referee.judge(lottos, winningLotto);
-        System.out.println(referee);
+
         double rateOfReturn = referee.getRateOfReturn(lottos.size() * 1000);
-        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
+        outputView.printPrizeResult(referee);
+        outputView.printResult(rateOfReturn);
     }
 
     private List<Integer> inputWinningLottoNumbers() {
@@ -62,115 +58,16 @@ public class LottoController {
             winningNumbers = splitInput.stream()
                     .map(Integer::parseInt)
                     .toList();
-        } while (validateWinningNumbers(winningNumbers));
-
+        } while (validator.validateWinningNumbers(winningNumbers));
         return winningNumbers;
     }
 
     private int inputWinningLottoBonusNumber(final List<Integer> winningNumbers) {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input;
-        int bonusNumber;
         do {
             input = Console.readLine();
-        } while (validateWinningBonusNumber(winningNumbers, input));
-        bonusNumber = Integer.parseInt(input);
-        return bonusNumber;
-    }
-
-    private boolean validateMoneyInput(final String input) {
-        try {
-            if (!input.matches("\\d+")) {
-                throw new IllegalArgumentException("[ERROR] 구입금액은 숫자이어야 합니다. 다시 입력하세요.");
-            }
-            final int money = Integer.parseInt(input);
-            if (money == 0) {
-                throw new IllegalArgumentException("[ERROR] 구입금액은 0원일 수 없습니다. 다시 입력하세요.");
-            }
-            if (money % 1000 != 0) {
-                throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위입니다. 다시 입력하세요.");
-            }
-            return false;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return true;
-        }
-    }
-
-    private List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    }
-
-    private boolean validateWinningNumbers(final List<Integer> numbers) {
-        try {
-            validateNumbersSize(numbers);
-            validateDuplicateNumber(numbers);
-            validateNumbersBoundary(numbers);
-            return false;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return true;
-        }
-    }
-
-    private void validateNumbersSize(final List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호의 개수는 6개이어야 합니다. 다시 입력하세요.");
-        }
-    }
-
-    private void validateDuplicateNumber(final List<Integer> numbers) {
-        final Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (numbers.size() != uniqueNumbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다. 다시 입력하세요.");
-        }
-    }
-
-    private void validateNumbersBoundary(final List<Integer> numbers) {
-        final boolean isBoundary = numbers.stream()
-                .anyMatch(number -> number < 1 || number > 45);
-        if (isBoundary) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이의 숫자입니다. 다시 입력하세요.");
-        }
-    }
-
-    private boolean validateWinningBonusNumber(final List<Integer> winningNumbers, final String input) {
-        try {
-            validateBlankInput(input);
-            validateNumberFormat(input);
-            validateDuplicateBonusNumber(winningNumbers, input);
-            validateBonusNumberBoundary(input);
-            return false;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return true;
-        }
-    }
-
-    private void validateBlankInput(final String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 공백이어선 안됩니다. 다시 입력하세요.");
-        }
-    }
-
-    private void validateNumberFormat(final String input) {
-        if (!input.matches("\\d+")) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자이어야 합니다. 다시 입력하세요.");
-        }
-    }
-
-    private void validateDuplicateBonusNumber(final List<Integer> winningNumbers, final String input) {
-        final int bonusNumber = Integer.parseInt(input);
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다. 다시 입력하세요.");
-        }
-    }
-
-    private void validateBonusNumberBoundary(final String input) {
-        final int bonusNumber = Integer.parseInt(input);
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            System.out.println(5);
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자입니다. 다시 입력하세요.");
-        }
+        } while (validator.validateWinningBonusNumber(winningNumbers, input));
+        return Integer.parseInt(input);
     }
 }

126de7473473efc3b5734b0c4ddc762b83beff73
SJ70 <seojae70@gmail.com>
2023-11-06 11:46:46 +0900

    Chore: 중복되는 값에 대한 접근을 상수로 활용

--- src/main/java/lotto/domain/lotto/LottoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -6,6 +6,10 @@ import lotto.domain.lotto.strategy.PickNumbersStrategy;
 
 public class LottoGenerator {
 
+    private final int MIN_NUMBER = LottoConfig.MIN_NUMBER.getValue();
+    private final int MAX_NUMBER = LottoConfig.MAX_NUMBER.getValue();
+    private final int NUMBERS_COUNT = LottoConfig.NUMBERS_COUNT.getValue();
+
     private final PickNumbersStrategy pickNumbersStrategy;
 
     public LottoGenerator(PickNumbersStrategy pickNumbersStrategy) {
@@ -19,10 +23,7 @@ public class LottoGenerator {
     }
 
     public Lotto generate() {
-        int minNumber = LottoConfig.MIN_NUMBER.getValue();
-        int maxNumber = LottoConfig.MAX_NUMBER.getValue();
-        int count = LottoConfig.NUMBERS_COUNT.getValue();
-        List<Integer> numbers = pickNumbersStrategy.pickNumbers(minNumber, maxNumber, count);
+        List<Integer> numbers = pickNumbersStrategy.pickNumbers(MIN_NUMBER, MAX_NUMBER, NUMBERS_COUNT);
         return new Lotto(numbers);
     }
 

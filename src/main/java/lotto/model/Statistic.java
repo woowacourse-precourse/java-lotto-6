5a5f1de4bf995fb7df484e8d8c6185eaf5edb832
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-06 20:04:46 +0900

    add : for문에서 사용하는 인자를 명시적으로 네이밍

--- src/main/java/lotto/model/Statistic.java
+++ src/main/java/lotto/model/Statistic.java
@@ -7,23 +7,27 @@ import java.util.Map;
 
 public class Statistic {
 
+    private static final Integer DEFAULT_COUNT = 0;
+    private static final Integer COUNT = 1;
+
     private Map<LottoRank, Integer> rank;
 
     private Statistic(Map<LottoRank, Integer> rank) {
         this.rank = rank;
     }
 
-    public static Statistic from(final List<Integer> matchedNumberCount, final List<Boolean> includeBonusNumber) {
-        return new Statistic(getResult(matchedNumberCount, includeBonusNumber));
+    public static Statistic from(final List<Integer> matchedNumberCount, final List<Boolean> containBonusNumber) {
+        return new Statistic(makeResult(matchedNumberCount, containBonusNumber));
     }
 
-    private static Map<LottoRank, Integer> getResult(final List<Integer> matchedNumberCount,
-                                                     final List<Boolean> includeBonusNumber) {
+    private static Map<LottoRank, Integer> makeResult(final List<Integer> matchedNumberCount,
+                                                      final List<Boolean> includeBonusNumber) {
         Map<LottoRank, Integer> playerRank = new HashMap<>();
 
-        for (int index = 0; index < matchedNumberCount.size(); index++) {
+        int lottoSize = matchedNumberCount.size();
+        for (int index = 0; index < lottoSize; index++) {
             LottoRank rank = determineRank(matchedNumberCount.get(index), includeBonusNumber.get(index));
-            playerRank.put(rank, playerRank.getOrDefault(rank, 0) + 1);
+            playerRank.put(rank, playerRank.getOrDefault(rank, DEFAULT_COUNT) + COUNT);
         }
 
         return playerRank;

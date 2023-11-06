a0b4d1cd7a890b2d6a5c53f0cead7d4d0162598e
shining013 <nevertheless113@naver.com>
2023-11-06 16:31:22 +0900

    feat: InputController setQuantity 메서드 생성
    구매 금액을 입력받아 로또 수량을 세팅함

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,21 +1,17 @@
 package lotto.controller;
 
 import lotto.Constants;
-
-import lotto.model.WinningNumValidator;
-import lotto.model.BonusNumValidator;
-import lotto.model.PurchaseNumValidator;
+import lotto.model.PurchaseAmountValidator;
 import lotto.view.InputView;
 
 public class InputController {
-        public static int setPurchaseNum() {
+        public static int setQuantity() {
                 try {
-                        PurchaseNumValidator validation = new PurchaseNumValidator(InputView.purchaseInput());
+                        PurchaseAmountValidator validation = new PurchaseAmountValidator(InputView.purchaseInput());
                         return Integer.parseInt(validation.PURCHASE_STRING);
                 } catch (IllegalArgumentException e) {
                         System.out.println(Constants.PURCHASE_WRONG_ERROR);
-                        return setPurchaseNum();
+                        return setQuantity();
                 }
         }
-
 }

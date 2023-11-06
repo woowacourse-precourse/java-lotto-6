d9dc170d7176c30a9dc9052432937c2503236de8
Hanjaemo <hjm2530@gmail.com>
2023-11-06 15:23:55 +0900

    test(LottoTest): Lotto 클래스의 contains 메서드 단위 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,7 +6,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -50,5 +49,25 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 %d이상 %d이하여야 한다."
                         .formatted(LottoConstants.MIN_LOTTO_NUMBER, LottoConstants.MAX_LOTTO_NUMBER));
+    }
+
+    @DisplayName("로또 번호가 특정 숫자를 포함할 경우 true를 반환한다.")
+    @Test
+    void contains_True() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when, then
+        Assertions.assertThat(lotto.contains(1)).isTrue();
+    }
+
+    @DisplayName("로또 번호가 특정 숫자를 포함하지 않을 경우 false를 반환한다.")
+    @Test
+    void contains_False() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when, then
+        Assertions.assertThat(lotto.contains(7)).isFalse();
     }
 }

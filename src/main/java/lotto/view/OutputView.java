c3c811d721cf5a427f169d5592f780b55b9e2560
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-07 03:43:53 +0900

    feat(OutputView): 주어진 dto로 알맞게 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,7 @@ import lotto.Dto.LottoResultDto;
 import lotto.Dto.MyLottosDto;
 import lotto.constants.messages.Notification;
 
+
 public class OutputView {
     public void askMoney() {
         System.out.println(Notification.BUDGET.getMessage());
@@ -17,11 +18,21 @@ public class OutputView {
     }
 
     public void askWinnerNumber() {
+        System.out.println(Notification.WINNER_LOTTO.getMessage());
     }
 
     public void askBonusNumber() {
+        System.out.println(Notification.BONUS_LOTTO.getMessage());
     }
 
     public void showResult(LottoResultDto lottoResultDto) {
+        System.out.println(Notification.RESULT_PRE.getMessage());
+        for (List<Integer> x : lottoResultDto.getResult()) {
+            System.out.printf(Notification.RESULT_MATCHED.getMessage(), x.get(0), x.get(2), x.get(3));
+            System.out.println();
+        }
+        System.out.printf(Notification.RESULT_PROFIT.getMessage(), lottoResultDto.getProfit());
+        System.out.println();
+
     }
 }

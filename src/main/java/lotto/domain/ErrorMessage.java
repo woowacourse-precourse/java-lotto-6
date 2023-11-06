ff18633046141abcb391c64dac8e9285315a244e
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 04:06:55 +0900

    feat(domain.ErrorMessage): 에러메시지를 모아놓은 enum을 생성
    이번 과제 요구사항 중에서 enum을 적용해봐라는 요구사항이 있어 에러메시지를 모아놓기 위한 ErrorMessage enum을 생성하였습니다.

---
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public enum ErrorMessage {
+    LOTTO_COMBINATION_HAVE_SIX_NUMBER("[ERROR] 로또 번호 조합은 6개의 숫자로 이루어져 있습니다."),
+    LOTTO_COMBINATION_DO_NOT_HAVE_DUPLICATES("[ERROR] 로또 번호 조합에는 중복된 숫자가 없어야 합니다."),
+    LOTTO_NUMBER_RANGE_IS_BETWEEN_ONE_AND_FORTYFIVE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}

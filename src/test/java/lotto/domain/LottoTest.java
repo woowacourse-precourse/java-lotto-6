678b0219b0e1fd6199c086f70ae56d0fd509e940
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 04:23:54 +0900

    test(domain.Lotto): 기능4. 로또를 발행하기 위해 입력된 각 숫자가 1 ~ 45 범위에 있는지 체크한다
    숫자리스트에 1~45 범위를 벗어난 숫자가 있을 때 예외가 발생하는지 확인하는 테스트 코드를 작성했습니다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static lotto.domain.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,7 +12,7 @@ class LottoTest {
 
     @Test
     @DisplayName("기능1,2 테스트 : 입력된 숫자조합에 이상이 없으면 로또가 하나 정상적으로 발행된다.")
-    void LottoGeneratorShouldGenerateLottoInstance(){
+    void createLottoByNormalArgument(){
         // given
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
 
@@ -21,15 +22,27 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("기능3 테스트 : validate 메서드는 로또를 발행하기 위해 입력된 숫자의 개수가 6개가 아니면 IllegalArgumentException이 발생시킨다.")
-    void validateShouldThrowIllegalArgumentExceptionWhenListSizeIsNotSix(){
+    @DisplayName("기능3 테스트 : 숫자 리스트에 들어있는 숫자의 개수가 6개가 아니면 IllegalArgumentException이 발생한다.")
+    void createLottoByOversize(){
         // given
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);
 
         // when, then
         assertThatThrownBy(() -> new Lotto(list))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.LOTTO_COMBINATION_HAVE_SIX_NUMBER.getErrorMessage());
+                .hasMessage(LOTTO_COMBINATION_HAVE_SIX_NUMBER.getErrorMessage());
+    }
+
+    @Test
+    @DisplayName("기능4 테스트 : 숫자 리스트에 1~45 범위에 있지 않은 숫자가 있다면 예외가 발생한다.")
+    void createLottoByWrongRangeNumber(){
+        // given
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 46);
+
+        // when, then
+        assertThatThrownBy(() -> new Lotto(list))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBER_RANGE_IS_BETWEEN_ONE_AND_FORTYFIVE.getErrorMessage());
     }
 
 

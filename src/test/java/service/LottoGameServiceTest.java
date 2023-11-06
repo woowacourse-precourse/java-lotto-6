86d8e19facb895d19cbd974ae05548b9fd75c166
yeahdy <yejine98@naver.com>
2023-11-07 01:25:57 +0900

    test(LottoGameServiceTest): 당첨번호 로또 리스트 테스트 코드 작성

--- src/test/java/service/LottoGameServiceTest.java
+++ src/test/java/service/LottoGameServiceTest.java
@@ -2,6 +2,8 @@ package service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import domain.Lotto;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +17,39 @@ public class LottoGameServiceTest {
         lottoGameService = new LottoGameService();
     }
 
+    @DisplayName("구매한 로또 당첨 번호 리스트는 null 이 아니다.")
+    @Test
+    void notNullLottoListTest() {
+        // given
+        int lottoCount = 15000;
+
+        // when
+        List<Lotto> lottoList = getLottoList(lottoCount);
+
+        //then
+        assertThat(lottoList).isNotNull();
+    }
+
+    @DisplayName("10개의 로또 구매를 구매할 경우 size 는 10이다.")
+    @Test
+    void purchasedLottoCountTest() {
+        // given
+        int lottoCount = 10;
+
+        // when
+        List<Lotto> lottoList = getLottoList(lottoCount);
+
+        //then
+        assertThat(lottoList.size()).isEqualTo(10);
+    }
+
+    private List<Lotto> getLottoList(int lottoCount){
+        return lottoGameService.getLottoList(lottoCount);
+    }
+
     @DisplayName("구입 금액이 1,000원 단위가 아닐 경우 false")
     @Test
-    void purchasedLottoTest() {
+    void purchaseLottoTest() {
         // given
         String purchasedAmount = "13500";
 

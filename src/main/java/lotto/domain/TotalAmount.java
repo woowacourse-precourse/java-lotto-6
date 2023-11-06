4b2ff784dff3a15982e39c3f25835e276f8d877b
haiseong <wjdgotjd9908@gmail.com>
2023-11-07 03:23:28 +0900

    feat(TotalAmount): 에러 메시지 추가

--- src/main/java/lotto/domain/TotalAmount.java
+++ src/main/java/lotto/domain/TotalAmount.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 public class TotalAmount {
     private static final long MINIMUM_AMOUNT = 0L;
+    private static final String OVERFLOW_ERROR_MESSAGE = "오버플로우가 발생하였습니다.";
 
     private final long totalAmount;
 
@@ -16,7 +17,7 @@ public class TotalAmount {
 
     private void validateOverflow(long money) {
         if (money < MINIMUM_AMOUNT) {
-            throw new IllegalStateException();
+            throw new IllegalStateException(OVERFLOW_ERROR_MESSAGE);
         }
     }
 

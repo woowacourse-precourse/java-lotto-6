a77938c98ce76f9ba95d305f6a2f3034caa1d243
kangyuseok <kingyuseok@naver.com>
2023-11-07 03:21:38 +0900

    Feat: 로또 구매에 관한 메서드 추가 구현
    1. 각 구매당 로또 랜덤 번호 생성 메서드
    2. 랜덤 번호가 생성된 로또를 LottoTickets에 넣어주는 메서드

--- src/main/java/lotto/service/Buyer.java
+++ src/main/java/lotto/service/Buyer.java
@@ -1,15 +1,41 @@
 package lotto.service;
 
-import static lotto.service.Validator.validateNumber;
+import static lotto.view.IO.viewLottoTickets;
 
-import java.util.PriorityQueue;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseAmount;
 
 public class Buyer {
     //로또 서비스 같은 기능
-    public static int buyLottoTickets(PurchaseAmount purchaseAmount){
+    private static Buyer buyer = new Buyer();
+    private final List<Lotto> lottoTickets;
+
+    private Buyer(){
+        lottoTickets = new ArrayList<>();
+    }
+
+    public static Buyer getInstance(){
+        return buyer;
+    }
+    public static int calculateLottoTickets(PurchaseAmount purchaseAmount){
         //금액을 쪼개서 몇개 로또를 살 수 있는지 리턴
         int lottoTicketCount = Integer.parseInt(purchaseAmount.getPurchaseAmount()) / 1000;
         return lottoTicketCount;
+    }
+
+    private List<Integer> generateLottoNumbers(){
+        List<Integer>lottoTicket = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoTicket;
+    }
+
+    public void buyLottoTickets(int lottoTicketCount){
+        for(int i=0;i<lottoTicketCount;i++){
+            Lotto lotto = new Lotto(generateLottoNumbers());
+            lottoTickets.add(lotto);
+        }
+        viewLottoTickets(lottoTickets);
     }
 }

2967724f7e2fa013e21ed9537f74f54cc53972cb
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 14:29:27 +0900

    remove: WinningLotto validate 메서드

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -7,27 +7,7 @@ public class WinningLotto {
     private final int bonus;
 
     public WinningLotto(List<Integer> numbers, int bonus) {
-        validate(numbers);
-        validateBonus(bonus);
         this.numbers = numbers;
         this.bonus = bonus;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-        if (numbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException();
-        }
-        if (numbers.stream().anyMatch(number -> number<1 || number>45)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateBonus(int bonus) {
-        if (bonus<1||bonus>45) {
-            throw new IllegalArgumentException();
-        }
     }
 }

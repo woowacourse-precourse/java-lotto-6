7bd83b7bdda2995c3e1cf45509ad10655e5846b4
pjaehyun <show6756@gmail.com>
2023-11-06 18:41:12 +0900

    feat: 추가 등수 별 메세지를 위한 LottoRank Enum

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public enum LottoRank {
+    FIRST("6개 일치 (2,000,000,000원)"),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD("5개 일치 (1,500,000원)"),
+    FOURTH("4개 일치 (50,000원)"),
+    FIFTH("3개 일치 (5,000원)");
+
+    private final String rankMessage;
+
+    LottoRank(String rankMessage) {
+        this.rankMessage = rankMessage;
+    }
+
+    public String getRankMessage() {
+        return rankMessage;
+    }
+
+}

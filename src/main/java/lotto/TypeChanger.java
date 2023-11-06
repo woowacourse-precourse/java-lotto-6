961aa4729ca81d2ab1bdb04cc8bb751dd701f4e1
K-KY <kyuyoungk@naver.com>
2023-11-07 01:22:53 +0900

    Refactor(TypeChanger) : 변수이름, 메소드 이름이 목적을 나타내도록 변경

--- src/main/java/lotto/TypeChanger.java
+++ src/main/java/lotto/TypeChanger.java
@@ -3,8 +3,8 @@ package lotto;
 import java.util.List;
 
 public class TypeChanger {
-    public static List<String> stringToList(String s) {
-        List<String> userNumber = List.of(s.split(","));
+    public static List<String> stringToListWithComma(String numbers) {
+        List<String> userNumber = List.of(numbers.split(","));
         return userNumber;
     }
 }
--- src/test/java/lotto/TypeChangerTest.java
+++ src/test/java/lotto/TypeChangerTest.java
@@ -8,6 +8,6 @@ import org.junit.jupiter.api.Test;
 public class TypeChangerTest {
     @Test
     void 쉼표를_기준으로_분리한다() {
-        assertThat(TypeChanger.stringToList("1,2,3,4,5,6")).isEqualTo(List.of("1","2","3","4","5","6"));
+        assertThat(TypeChanger.stringToListWithComma("1,2,3,4,5,6")).isEqualTo(List.of("1","2","3","4","5","6"));
     }
 }

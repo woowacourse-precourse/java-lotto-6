ca0515235b3727b1b66946f91c9ef78f5ffcd6f1
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 21:32:36 +0900

    feat: bonusNum 클래스 생성 및 기능 구현

--- src/main/java/lotto/model/BonusNumberValidation.java
+++ src/main/java/lotto/model/BonusNumberValidation.java
@@ -1,2 +1,33 @@
-package lotto.model;public class BonusNumberValidation {
+package lotto.model;
+
+import lotto.view.ExceptionMessage;
+
+public class BonusNumberValidation {
+    private final int bonusNumber;
+
+    public BonusNumberValidation(String number) {
+        int amountNum = validateNumber(number);
+        validateNatural(amountNum);
+        this.bonusNumber = amountNum;
+    }
+
+    private static int validateNumber(String amount) {
+        try{
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            ExceptionMessage.numberException();
+            throw new IllegalStateException();
+        }
+    }
+
+    private void validateNatural(int amount) {
+        if (amount <= 0) {
+            ExceptionMessage.naturalException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }

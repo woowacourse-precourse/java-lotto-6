7f8d3a9aedb2ac009ec3b84fb1ba56da06c09d35
sunghyun <shlim0287@naver.com>
2023-11-06 17:26:09 +0900

    refactor : 로또 정렬메서드 추가 및 검증 메서드 추출.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,23 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.validator.ValidateLotto;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        ValidateLotto.validateLotto(numbers);
+        sortLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
 
-    // TODO: 추가 기능 구현
+
+    private List<Integer> sortLottoNumbers(List<Integer> numbers){
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }

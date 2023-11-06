ea937ec1d29fa9a3c75c19f6cde23bcd1522ef86
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-11-06 10:12:01 +0900

    test (Lotto) : 테스트 케이스 추가
    - 로또 번호 개수가 6개 보다 작을 때, 예외를 던지는지 확인하는 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,11 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -19,10 +18,17 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 개수가 6개 보다 적으면 예외가 발생한다.")
+    @Test
+    void createLottoByLessSize() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또는 숫자 6개로 구성되어야 합니다");
+    }
 }

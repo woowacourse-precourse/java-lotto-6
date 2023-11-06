e0f30b020485abb10c34fc8408299d75279a8818
Infikei <hckhong@naver.com>
2023-11-07 03:32:41 +0900

    refactor: 당첨 번호 변수 이름을 복수형으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String inputAmount() {
@@ -12,8 +12,8 @@ public class InputView {
         return Console.readLine();
     }
 
-    public String inputWinningNumber() {
-        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+    public String inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
         return Console.readLine();
     }
 

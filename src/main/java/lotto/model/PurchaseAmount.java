9fd414fc59a17085d75a3dff1076493cca8e133b
insu <is23193160@gmail.com>
2023-11-06 13:33:53 +0900

    feat: 사용자가 1000보다 작은 구입 금액 입력 시 예외 처리하는 기능 추가

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -2,10 +2,22 @@ package lotto.model;
 
 public class PurchaseAmount {
     private static final int LOTTO_PRICE = 1000;
+    private static final String INPUT_LEAST_PRICE_ERROR_MESSAGE = "[ERROR] 최소 1000원 이상의 돈을 입력해주세요.";
     private final int purchaseAmount;
 
     public PurchaseAmount(int inputMoney) {
+        validate(inputMoney);
         this.purchaseAmount = inputMoney;
+    }
+
+    private void validate(int inputMoney) {
+        checkLeastLottoPrice(inputMoney);
+    }
+
+    private void checkLeastLottoPrice(int inputMoney) {
+        if (inputMoney < LOTTO_PRICE) {
+            throw new IllegalArgumentException(INPUT_LEAST_PRICE_ERROR_MESSAGE);
+        }
     }
 
     public int getMaxLottoCountForBudget() {

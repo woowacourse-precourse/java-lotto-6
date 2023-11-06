4526f309f07bface591977204241d78645a2b3e6
hYe807n <rlagkdus55@gmail.com>
2023-11-06 13:05:33 +0900

    Test: Bonus 번호 예외 처리(중복, 범위, 숫자 이외 문자) 에 대한 단위 테스트 구현

---
+++ src/test/java/lotto/model/BonusTest.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusTest {
+    private final List<Integer> answer = List.of(1, 2, 3, 4, 5, 6);
+
+    @DisplayName("Bonus 번호에 숫자 외의 문자가 입력되면 예외를 발생한다.")
+    @Test
+    void createBonusByNotNumber() {
+        assertThatThrownBy(() -> new Bonus("50a", answer))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Bonus 번호의 범위가 1부터 45가 아니라면 예외를 발생한다.")
+    @Test
+    void createBonusByOverRange() {
+        assertThatThrownBy(() -> new Bonus("50", answer))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Bonus 번호가 당첨 번호와 중복되면 예외를 발생한다.")
+    @Test
+    void createBonusByDuplicatedAnswer() {
+        assertThatThrownBy(() -> new Bonus("1", answer))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}

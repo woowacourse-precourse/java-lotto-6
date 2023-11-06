2f12d3f34a958f3091806f92da0d87cb220d59b9
jeongjihun <wlgns2234@naver.com>
2023-11-06 16:27:04 +0900

    refactor: 상수를 사용하도록 변경

--- src/main/java/lotto/view/output/OutputFormatter.java
+++ src/main/java/lotto/view/output/OutputFormatter.java
@@ -7,12 +7,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputFormatter {
+    private static final String FIVE_MATCH_WITH_BONUS_MESSAGE_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String MATCH_MESSAGE_FORMAT = "%d개 일치 (%s원) - %d개";
+    private static final String TOTAL_EARNINGS_MESSAGE = "총 수익률은 ";
+    private static final String EARNINGS_RATE_FORMAT = "%,.1f%%";
+    private static final String EARNINGS_SUFFIX_MESSAGE = "입니다.";
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String LOTTO_STATISTICS_MESSAGE = "당첨 통계";
     private static final String LEFT_SQUARE_BRACKET = "[";
     private static final String RIGHT_SQUARE_BRACKET = "]";
     private static final String NEW_LINE = "\n";
     private static final String SPACE = " ";
     private static final String COMMA = ",";
+    private static final String DASH = "-";
 
     private OutputFormatter() {
     }
@@ -34,12 +41,12 @@ public class OutputFormatter {
     }
 
     public static String makeTotalEarningsRate(double totalEarningsRate) {
-        String formattedEarningsRate = String.format("%,.1f%%", totalEarningsRate);
-        return "총 수익률은 " + formattedEarningsRate + "입니다.";
+        String formattedEarningsRate = String.format(EARNINGS_RATE_FORMAT, totalEarningsRate);
+        return TOTAL_EARNINGS_MESSAGE + formattedEarningsRate + EARNINGS_SUFFIX_MESSAGE;
     }
 
     public static String makeLottoResult(List<LottoResult> results) {
-        String result = "당첨 통계" + NEW_LINE + "---";
+        String result = LOTTO_STATISTICS_MESSAGE + NEW_LINE + DASH + DASH + DASH;
 
         String resultLines = Arrays.stream(LottoResult.values())
                 .map(r -> makeLottoResultLine(results, r))
@@ -61,21 +68,17 @@ public class OutputFormatter {
     }
 
     private static String makeFiveMatchWithBonus(Long count, LottoResult lottoResult) {
-        return lottoResult.getMatchingNumbers()
-                + "개 일치, 보너스 볼 일치 ("
-                + lottoResult.getPrizeAmount()
-                + "원) - "
-                + count
-                + "개";
+        return String.format(FIVE_MATCH_WITH_BONUS_MESSAGE_FORMAT,
+                lottoResult.getMatchingNumbers(),
+                lottoResult.getPrizeAmount(),
+                count);
     }
 
     private static String makeMatch(Long count, LottoResult lottoResult) {
-        return lottoResult.getMatchingNumbers()
-                + "개 일치 ("
-                + lottoResult.getPrizeAmount()
-                + "원) - "
-                + count
-                + "개";
+        return String.format(MATCH_MESSAGE_FORMAT,
+                lottoResult.getMatchingNumbers(),
+                lottoResult.getPrizeAmount(),
+                count);
     }
 
 }

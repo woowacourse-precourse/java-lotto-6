42ff24df8f9168621c601dd4a24a8cab52dd9baf
wda067 <mg6359@naver.com>
2023-11-06 16:31:20 +0900

    feat(OutputView): 당첨 통계를 출력하는 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
 
 public class OutputView {
     private static final String PURCHASE_QUANTITY_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String DIVIDING_LINE = "---";
 
     public void printPurchaseQuantity(int purchaseQuantity) {
         System.out.println();
@@ -14,5 +18,16 @@ public class OutputView {
         for (List<Integer> lottoNumber : lottoNumbers) {
             System.out.println(lottoNumber);
         }
+    }
+
+    public void printWinningStatistics(EnumMap<LottoRank, Integer> lottoRakingMap) {
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(DIVIDING_LINE);
+
+        Arrays.stream(LottoRank.values())
+                .filter(rank -> rank != LottoRank.LAST_PLACE)
+                .forEach(rank ->
+                        System.out.println(rank.getOutputMessage()
+                                + lottoRakingMap.get(rank)));
     }
 }

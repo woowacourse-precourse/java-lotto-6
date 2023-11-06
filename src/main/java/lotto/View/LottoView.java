9be6ebbac20981e6fd1efdc281b5acdeda37847b
park_yeongyu <sjjs7771@naver.com>
2023-11-06 22:51:25 +0900

    feat(LottoView) : 몇개 구매했는지 출력하는 메세지 작성

--- src/main/java/lotto/View/LottoView.java
+++ src/main/java/lotto/View/LottoView.java
@@ -4,4 +4,8 @@ public class LottoView {
     public void printInputPrice(){
         System.out.println("구입 금액을 입력해 주세요.");
     }
+
+    public void printNumOfLotto(int number){
+        System.out.println(number+"개를 구매했습니다.");
+    }
 }

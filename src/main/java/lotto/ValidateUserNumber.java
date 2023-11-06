45ac0d796e0f480d614e8bab7786c072392b1616
K-KY <kyuyoungk@naver.com>
2023-11-07 08:09:40 +0900

    Refactor(ValidateUserNumber) : 범위 검증방식 변경, 모든 메서드 파라미터 타입 변경
    숫자와 범위를 한번에 검증하는 메서드 삭제
    
    범위 검증 방식을 스트림으로 1보다 작거나 45보다 큰 값이 있으면 false를 리턴하도록 변경

--- src/main/java/lotto/ValidateUserNumber.java
+++ src/main/java/lotto/ValidateUserNumber.java
@@ -3,26 +3,27 @@ package lotto;
 import java.util.List;
 
 public class ValidateUserNumber {
-    public static void isLengthSix(List<String> numbers) {
+    public static void isLengthSix(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력 해주세요");
         }
     }
 
-    public static void isValidNumber(List<String> numbers) {
-        numbers.forEach(number -> {
-            ValidateNumber.isNumber(number);
-            isInRange(Integer.parseInt(number));
-        });
-    }
-
-    public static void isInRange(Integer number) {
-        if (number <= 0 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1부터 45까지 입니다.");
+    public static void isInRange(List<Integer> numbers) {
+        if (check(numbers)) {
+            return;
         }
+        throw new IllegalArgumentException("[ERROR] 숫자는 1부터 45까지 입력 가능합니다.");
     }
 
-    public static void isDuplicate(List<String> numbers) {
+    private static boolean check(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(number -> number > 45 || number < 1)
+                .findFirst()
+                .isEmpty();
+    }
+
+    public static void isDuplicate(List<Integer> numbers) {
         int check = (int) numbers.stream().distinct().count();
         if (numbers.size() != check) {
             throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력 해주세요.");

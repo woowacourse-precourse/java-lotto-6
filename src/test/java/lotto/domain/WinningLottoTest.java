be7f6e7c49326a08597d754d8fc19dab64a04056
LeeJieon <dlwldjs7544@naver.com>
2023-11-07 02:07:20 +0900

    test: 입력된 당첨 번호에 대한 테스트 추가
    - 정상적인 당첨 번호가 입력된 테스트
    - 입력 값이 숫자나 쉼표가 아닌 경우에 대한 테스트

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningLottoTest {
+    @Test
+    @DisplayName("정상적인 당첨 번호 입력")
+    void inputCorrectWinningLotto() {
+        // Given
+        String inputWinningLotto = "1,2,3,4,5,6";
+
+        // When
+        WinningLotto winningLotto = new WinningLotto(inputWinningLotto);
+        Lotto lotto = winningLotto.getLotto();
+
+        // Then
+        assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    @DisplayName("입력 값에 숫자나 쉼표 이외의 문자가 들어올 경우 예외가 발생한다.")
+    void inputNotNumberOrComma() {
+        // Given
+        String inputWinningLotto = "1,2,3,a,4,5";
+
+        // When & Then
+        assertThatThrownBy(() -> new WinningLotto(inputWinningLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

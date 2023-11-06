6d14cd8b6e690f4654e9ec9aabb7b8bf8f955921
muyaaho <skanthsu715@gmail.com>
2023-11-06 13:23:11 +0900

    feat(lotto): 수익률 계산하는 기능 추가

--- src/main/java/lotto/LottoCalculator.java
+++ src/main/java/lotto/LottoCalculator.java
@@ -8,4 +8,9 @@ public class LottoCalculator {
     public int checkMatch(List<Integer> nums1, List<Integer> nums2){
         return (int)nums1.stream().filter(o -> nums2.stream().anyMatch(Predicate.isEqual(o))).count();
     }
+
+    public double roi(double investment, double current){
+        double result = investment/current * 100;
+        return Math.round(result*100)/100.0;
+    }
 }
--- src/test/java/lotto/LottoCalculatorTest.java
+++ src/test/java/lotto/LottoCalculatorTest.java
@@ -3,15 +3,27 @@ package lotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LottoCalculatorTest {
+    LottoCalculator lottoCalculator;
+    @BeforeEach
+    void setUp(){
+        lottoCalculator = new LottoCalculator();
+    }
+
     @Test
     void 일치하는_번호_갯수_확인(){
-        LottoCalculator lottoCalculator = new LottoCalculator();
         assertEquals(6, lottoCalculator.checkMatch(List.of(1,2,3,4,5,6), List.of(1,2,3,4,5,6)));
         assertEquals(5, lottoCalculator.checkMatch(List.of(11,2,3,4,5,6), List.of(1,2,3,4,5,6)));
         assertEquals(1, lottoCalculator.checkMatch(List.of(11,12,13,14,15,6), List.of(1,2,3,4,5,6)));
         assertEquals(3, lottoCalculator.checkMatch(List.of(11,2,13,4,15,6), List.of(1,2,3,4,5,6)));
+    }
+
+    @Test
+    void 수익률_계산_확인(){
+        assertEquals(62.5, lottoCalculator.roi(5000, 8000));
+        assertEquals(285.71, lottoCalculator.roi(20000, 7000));
     }
 }

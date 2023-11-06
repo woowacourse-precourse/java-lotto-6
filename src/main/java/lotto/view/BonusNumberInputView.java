83e09f3fcaa6d95e886d2e64fff92d189ced6031
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 08:06:30 +0900

    feat(BonusNumberInputView) : 보너스 숫자 입력 View 단 수정
    - 예외처리는 모델에서 받을 예정

--- src/main/java/lotto/view/BonusNumberInputView.java
+++ src/main/java/lotto/view/BonusNumberInputView.java
@@ -5,18 +5,11 @@ import lotto.util.StringToIntegerConverter;
 
 public class BonusNumberInputView extends InputView {
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private BonusNumberException bonusNumberException = new BonusNumberException();
-    private StringToIntegerConverter stringToIntegerConverter = new StringToIntegerConverter();
 
-    public int getBonusNumber(){
+
+    public String getBonusNumber(){
         System.out.println(BONUS_NUMBER_MESSAGE);
-        String input = getInputValue();
-        bonusNumberException.isNumber(input);
-        bonusNumberException.isNumberOnlyOne(input);
-        bonusNumberException.isBlank(input);
-        int bonusNum = stringToIntegerConverter.stringToInteger(input);
-        bonusNumberException.isRightRange(bonusNum);
-
+        String bonusNum = getInputValue();
         return bonusNum;
     }
 }

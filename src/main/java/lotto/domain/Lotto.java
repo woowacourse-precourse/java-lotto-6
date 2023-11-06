b01a08d4f83b3cb75dd6e4ab9e73bb4e057a0112
ykmxxi <ykmxxi97@gmail.com>
2023-11-06 16:42:08 +0900

    refactor(Lotto): 로또 번호 검증을 클래스로 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,11 +4,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-public class Lotto {
+import lotto.validator.LottoNumbersValidator;
 
-    private static final String LENGTH_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
-    private static final String DUPLICATION_ERROR_MESSAGE = "[ERROR] 로또 번호에 중복된 숫자가 존재할 수 없습니다.";
-    private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1 ~ 45 숫자만 가능합니다.";
+public class Lotto {
 
     private final List<Integer> numbers;
 
@@ -18,34 +16,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateNumbersSize(numbers);
-        validateDuplicationInNumbers(numbers);
-        validateNumberRange(numbers);
-    }
-
-    private void validateNumbersSize(final List<Integer> numbers) {
-        if (LottoNumbersRule.isLottoNumbersSizeMismatched(numbers)) {
-            throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
-        }
-    }
-
-    private void validateDuplicationInNumbers(final List<Integer> numbers) {
-        if (hasDuplication(numbers)) {
-            throw new IllegalArgumentException(DUPLICATION_ERROR_MESSAGE);
-        }
-    }
-
-    private boolean hasDuplication(final List<Integer> numbers) {
-        return LottoNumbersRule.isLottoNumbersSizeMismatched(numbers.stream()
-                .distinct()
-                .toList());
-    }
-
-    private void validateNumberRange(final List<Integer> numbers) {
-        if (numbers.stream()
-                .anyMatch(LottoNumbersRule::hasOutOfRangeNumber)) {
-            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
-        }
+        LottoNumbersValidator.validate(numbers);
     }
 
     public int compare(final Lotto lotto) {
---
+++ src/test/java/lotto/validator/LottoNumbersValidatorTest.java
@@ -0,0 +1,77 @@
+package lotto.validator;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LottoNumbersValidatorTest {
+
+    @ParameterizedTest
+    @MethodSource("provideDataForValidateNumbersSize")
+    @DisplayName("로또 번호 예외 처리: 6개의 숫자가 아닌 경우")
+    void givenList_whenValidateNumbersSize_thenThrowException(List<Integer> numbers) {
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumbersValidator.validate(numbers))
+                .withMessageStartingWith("[ERROR]")
+                .withMessageContaining("로또 번호는 6개여야 합니다.");
+    }
+
+    private static Stream<Arguments> provideDataForValidateNumbersSize() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7))
+        );
+    }
+
+    @Test
+    @DisplayName("로또 번호 예외 처리: 중복된 숫자가 존재하는 경우")
+    void givenNumbers_whenValidateDuplicationInNumbers_thenThrowException() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 5);
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumbersValidator.validate(numbers))
+                .withMessageStartingWith("[ERROR]")
+                .withMessageContaining("로또 번호에 중복된 숫자가 존재할 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "입력값 : {0}")
+    @MethodSource("provideDataForOutOfRangeNumbers")
+    @DisplayName("로또 번호 예외 처리: 1 ~ 45 범위를 벗어난 경우")
+    void givenOutOfRangeNumbers_whenValidateNumberRange_thenThrowException(List<Integer> numbers) {
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumbersValidator.validate(numbers))
+                .withMessageStartingWith("[ERROR]")
+                .withMessageContaining("로또 번호는 1 ~ 45 숫자만 가능합니다.");
+    }
+
+    private static Stream<Arguments> provideDataForOutOfRangeNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(0, 1, 2, 3, 4, 45)),
+                Arguments.of(List.of(1, 2, 3, 4, 45, 46))
+        );
+    }
+
+    @Test
+    @DisplayName("로또 번호 예외 처리 통과")
+    void givenNumbers_whenValidate_thenNoException() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when & then
+        assertThatNoException()
+                .isThrownBy(() -> LottoNumbersValidator.validate(numbers));
+    }
+
+}

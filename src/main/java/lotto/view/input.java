368813f2541e4c0f90119e8b5adf3d9017e6b9af
gahyeon <1000ji1@naver.com>
2023-11-06 16:07:11 +0900

    Feat : 당첨 번호 입력 받기

--- src/main/java/lotto/view/input.java
+++ src/main/java/lotto/view/input.java
@@ -4,13 +4,20 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class input {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
-    public static String read(){
+    public static String read() {
         return readLine();
     }
 
-    public static String purchaseAmount(){
+    public static String purchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         return read();
     }
+
+    public static String winningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
+        return read();
+    }
+
 }

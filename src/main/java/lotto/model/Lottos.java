3ce480d1ba1adf5b823f984aa03ff6ccb1b862ec
4444ki <qkrwogh423@gmail.com>
2023-11-06 15:08:51 +0900

    ## feat, refactor : Lottos 기능 추가 및 삭제 필요한 기능을 추가하고 다른 클래스에서 기능 할 함수를 제거함

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,81 +1,29 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import lotto.util.Quicksort;
 
+import lotto.util.LottoNumberGenerate;
 
 public class Lottos {
     private final List<Lotto> lottos;
     private final List<Integer> winningCounts;
 
     public Lottos(int tryNumber) {
-        this.lottos = createLottos(tryNumber);
+        this.lottos = LottoNumberGenerate.generateLottos(tryNumber);
         this.winningCounts = new ArrayList<>();
     }
 
-    public List<Lotto> getLottos(){
+    public List<Lotto> getLottos() {
         return this.lottos;
     }
 
-    public List<Integer> getWinningCounts(){
+    public List<Integer> getWinningCounts() {
         return this.winningCounts;
-    }
-    private List<Lotto> createLottos(int tryNumber) {
-        List<Lotto> Lottos = new ArrayList<>();
-        for (int i = 0; i < tryNumber; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Lottos.add(new Lotto(sortNumber(numbers)));
-        }
-        return Lottos;
-    }
-
-    private List<Integer> sortNumber(List<Integer> numbers) {
-        Quicksort quicksort = new Quicksort();
-        quicksort.sort(numbers);
-        return numbers;
-    }
-
-    public void checkRank(List<Integer> winning_numbers, Lotto lotto) {
-        int state = 0;
-        int count = 0;
-        for (int number : winning_numbers) {
-            if (lotto.getNumbers().contains(number)) {
-                count++;
-            }
-            if(state <6 && count == 6){
-                count = 7;
-                break;
-            }
-            state++;
-        }
-        addLottoRank(count);
     }
 
     public void addLottoRank(int count) {
-        if (count == 7) {
-            this.winningCounts.add(1);
-        } else if (count == 6) {
-            this.winningCounts.add(2);
-        } else if (count == 5) {
-            this.winningCounts.add(3);
-        } else if (count == 4) {
-            this.winningCounts.add(4);
-        } else if (count == 3) {
-            this.winningCounts.add(5);
-        }
-    }
-
-    public int getLottoMoney(){
-        int money = 0;
-        money += Collections.frequency(getWinningCounts(),5) * 5000;
-        money += Collections.frequency(getWinningCounts(),4) * 50000;
-        money += Collections.frequency(getWinningCounts(),3) * 1500000;
-        money += Collections.frequency(getWinningCounts(),2) * 30000000;
-        money += Collections.frequency(getWinningCounts(),1) * 200000000;
-        return money;
+        this.winningCounts.add(count);
     }
 
 }

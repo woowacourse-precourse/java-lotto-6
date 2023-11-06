587e5a6b620d03c210f12f36e19dbcf02ea6f95f
constant94-dev <constant94@naver.com>
2023-11-07 00:49:18 +0900

    feat(exception): 로또 구매 수와 발행 수 검증 예외 처리 메서드 구현

--- src/main/java/lotto/exception/ExceptionHandler.java
+++ src/main/java/lotto/exception/ExceptionHandler.java
@@ -3,9 +3,10 @@ package lotto.exception;
 public final class ExceptionHandler {
     public final static String NOT_DIVISIBLE = "[ERROR] 1,000원으로 나누어 떨어지지 않습니다.";
     public final static String OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    public final static String NO_LENGTH_OF_SIX = "[ERROR] 당첨 번호는 6개만 뽑아야 합니다.";
+    public final static String NO_LENGTH_OF_SIX = "[ERROR] 당첨 번호는 6개를 뽑아야 합니다.";
     public final static String NUMBER_IN_DUPLICATE = "[ERROR] 중복되지 않는 숫자 6개를 뽑아야 합니다.";
-    public final static String NO_LENGTH_OF_ONE = "[ERROR] 보너스 번호는 1개만 뽑아야 합니다.";
+    public final static String NO_LENGTH_OF_ONE = "[ERROR] 보너스 번호는 1개를 뽑아야 합니다.";
+    public final static String DIFFERENT_BUY_FROM_PUBLISH = "[ERROR] 구매한 로또 수와 발행한 로또 수가 다릅니다.";
 
     public static void notDivisibleBy1000() {
         throw new IllegalArgumentException(NOT_DIVISIBLE);
@@ -25,5 +26,9 @@ public final class ExceptionHandler {
 
     public static void numberInDuplicate() {
         throw new IllegalArgumentException(NUMBER_IN_DUPLICATE);
+    }
+
+    public static void differentBuyFromPublish() {
+        throw new IllegalStateException(DIFFERENT_BUY_FROM_PUBLISH);
     }
 }

f4e2c4ef6a6a0bd5bdf0e30ed24174cb5bdb76b0
JoonKyo <joonkyolee@gmail.com>
2023-11-06 21:11:20 +0900

    feat(ConvertInput): 구입 금액을 정수형으로 반환하는 메서드 추가

--- src/main/java/lotto/util/ConvertInput.java
+++ src/main/java/lotto/util/ConvertInput.java
@@ -1,2 +1,31 @@
-package lotto.util;public class ConvertInput {
+package lotto.util;
+
+import static lotto.exception.ErrorMessage.PURCHASE_AMOUNT_HAS_MORE_THAN_ONE_VALUE;
+import static lotto.exception.ErrorMessage.PURCHASE_AMOUNT_IS_NOT_NUMBER;
+import static lotto.constraint.CommonConstraint.DELIMITER;
+
+import lotto.exception.Exception;
+
+public class ConvertInput {
+    private ConvertInput() {
+    }
+
+    public static int makePlayerMoneyToInt(String input) {
+        try {
+            validateOneInput(input);
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw Exception.of(PURCHASE_AMOUNT_IS_NOT_NUMBER);
+        }
+    }
+
+    private static void validateOneInput(String input) {
+        if(!hasOnlyOneValue(input)){
+            throw Exception.of(PURCHASE_AMOUNT_HAS_MORE_THAN_ONE_VALUE);
+        }
+    }
+
+    private static boolean hasOnlyOneValue(String input){
+        return !input.contains(DELIMITER.getValue());
+    }
 }

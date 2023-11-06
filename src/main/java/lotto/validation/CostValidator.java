71ae0d54525194c6906bf775432c64520150d830
jiohh <dkswlgh99@naver.com>
2023-11-06 20:31:56 +0900

    feat(validation): 금액 관련 예외 기능 추가
    1. 금액이 숫자인지 검증
    2. 금액이 1000원 단위로 나눠지는지 검증
    3. 금액이 0인지 검증

--- src/main/java/lotto/validation/CostValidator.java
+++ src/main/java/lotto/validation/CostValidator.java
@@ -1,15 +1,44 @@
 package lotto.validation;
 
+import lotto.exception.CostUnitDivideException;
+import lotto.exception.CostZeroException;
+import lotto.exception.IncludeCharException;
 import lotto.exception.InputBlankException;
 
 public class CostValidator {
-    public static void validate(String readline) {
+    public static void validateReadLine(String readline) {
         checkBlank(readline);
+        checkDigit(readline);
+    }
+
+    public static void validateParsedCost(int cost) {
+        checkDivideUnit(cost);
+        checkZero(cost);
     }
 
     private static void checkBlank(String readline) {
-        if (readline.isBlank()) {
+        if (readline == null || readline.isBlank()) {
             throw InputBlankException.inputBlankException;
+        }
+    }
+
+    private static void checkDigit(String readLine) {
+        for (int i = 0; i < readLine.length(); i++) {
+            if (!Character.isDigit(readLine.charAt(i))) {
+                throw IncludeCharException.includeCharException;
+            }
+        }
+    }
+
+    private static void checkDivideUnit(int cost) {
+        if (cost % 1000 != 0) {
+            throw CostUnitDivideException.costUnitDivideException;
+        }
+    }
+
+    private static void checkZero(int cost) {
+        if (cost == 0) {
+            throw CostZeroException.costZeroException;
         }
     }
 }

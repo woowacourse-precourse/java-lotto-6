977753a071d793fa5d75086e4b2346cbee47dedc
jaegyeongkim <dongji_11@naver.com>
2023-11-06 12:27:50 +0900

    refactor: 에러 메시지 추가

--- src/main/java/utils/NumberValidation.java
+++ src/main/java/utils/NumberValidation.java
@@ -3,16 +3,17 @@ package utils;
 public class NumberValidation {
     private static final int MAX_NUMBER = 45;
     private static final int MIN_NUMBER = 1;
+    private static final String OUT_OF_RANGE = "[ERROR] 1~45 사이의 값을 입력해야 합니다. ";
 
     public static void isOverMaxNumber(int number) {
         if (number > MAX_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(OUT_OF_RANGE);
         }
     }
 
     public static void isUnderMinNumber(int number) {
         if (number < MIN_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(OUT_OF_RANGE);
         }
     }
 }

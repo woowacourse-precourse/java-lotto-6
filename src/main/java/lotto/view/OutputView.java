0acf0ac36dbf325f26193ae4e3cf48ad504b5693
MinseokGo <rhalstjr1999@naver.com>
2023-11-07 01:16:27 +0900

    refactor($OutputView): 싱글톤 패턴 적용
    OutputView에 싱글톤 패턴을 적용하였습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,15 @@ import lotto.model.Lotto;
 import lotto.utils.Constants;
 
 public class OutputView {
+    private static final OutputView outputView = new OutputView();
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        return outputView;
+    }
+
     public void printCreateLottoNumbers(final List<Lotto> lottos) {
         System.out.printf(Constants.PURCHASE_COUNT_MESSAGE, lottos.size());
         lottos.forEach(lotto -> System.out.println(lotto.toString()));

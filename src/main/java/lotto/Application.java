a08392a849fa583d7252f4d1a0132f5f49aa14fb
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 14:32:49 +0900

    feat: 구매가능한 로또 번호 등록하여 생성
    number개수로 입력받고 number개수 만큼의 랜덤 로또 세트 생성후 리턴

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+    }
+
+    public static List<Lotto> makeRandomLotto(int number){
+        List<Lotto> randomLottos = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            randomLottos.add(new Lotto(numbers));
+        }
+        return randomLottos;
     }
 }

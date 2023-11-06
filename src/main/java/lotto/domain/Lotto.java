b9c83c4270693dbd68986644f9550b3a727bfc1f
ay-eonii <naayen@naver.com>
2023-11-06 23:07:22 +0900

    feat(lotto): 로또 생성 및 오름차순 정렬

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,19 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final static int MIN_RANGE = 1;
+    private final static int MAX_RANGE = 45;
+    private final static int COUNT = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +23,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, COUNT);
+    }
 }

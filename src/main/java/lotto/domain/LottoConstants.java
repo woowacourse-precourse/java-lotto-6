b9d35dcb892fe630f4da4df22fba7571c41c9ca0
dmong <dt2980_hee@naver.com>
2023-11-06 13:54:19 +0900

    feat: LottoConstants 작성

---
+++ src/main/java/lotto/domain/LottoConstants.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public enum LottoConstants {
+    LOTTO_NUMBER_RANGE_START(1),
+    LOTTO_NUMBER_RANGE_END(45),
+    LOTTO_PRICE(1000),
+    LOTTO_LENGTH_CONSTRAINT(6);
+
+    private final int value;
+
+    LottoConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

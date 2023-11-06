0c4f381f1085d82df18d37ce6730d2f1683d107c
권장순 <jsoonworld@gmail.com>
2023-11-07 00:13:20 +0900

    feat( LottoResult#initializePrizeMap() ) : 당첨 번호 일치 개수별 상금 정보 맵 초기화 로직 구현.

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -8,10 +8,21 @@ public class LottoResult {
     // TODO : equals, hashcode 구현.
     private static final int MAX_MATCH_COUNT = 6;
     private Map<Integer, Integer> matchCounts = new HashMap<>();
+    private Map<Integer, Long> prizeMap = initializePrizeMap();
 
     public LottoResult() {
         for (int i = 0; i <= MAX_MATCH_COUNT; i++) {
             matchCounts.put(i, 0);
         }
     }
+
+    private Map<Integer, Long> initializePrizeMap() {
+        Map<Integer, Long> prizeMap = new HashMap<>();
+        prizeMap.put(3, 5_000L);
+        prizeMap.put(4, 50_000L);
+        prizeMap.put(5, 1_500_000L);
+        prizeMap.put(6, 2_000_000_000L);
+        return prizeMap;
+    }
+
 }

f3dac4d9ebf84f3a2a0a7609ee49be3f7f03612c
muyaaho <skanthsu715@gmail.com>
2023-11-06 16:32:36 +0900

    fix(lotto): 로또 금액 에러가 여러번 날 때 종료되는 현상 수정

--- src/main/java/lotto/AmountProcessing.java
+++ src/main/java/lotto/AmountProcessing.java
@@ -7,7 +7,7 @@ public class AmountProcessing {
 
     public boolean isDivided(int cost){
         if (cost%1000 > 0){
-            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해야 합니다. 다시 입력해주세요\n");
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해야 합니다. 다시 입력해주세요");
         }
         return true;
     }

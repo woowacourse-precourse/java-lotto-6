8da24e5f040bac4fdeb943e1b4ba215a00d50259
rakyun1 <dbskim234@naver.com>
2023-11-06 05:32:11 -0700

    Refactor(Application): add printResult() RankEnum을 활용하여 Result 클라스를 없애고 foreach로 RankEnum.values()를 순환하며 결과를 출력하였습니다. 또한 hasBonus()를 만들어 출력 요구사항을 지켰습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -35,26 +35,24 @@ public class Application {
 		System.out.println("보너스 번호를 입력하세요.");
 		final int bonus = validate.validateBonus(readLine(), winningNumbers);
 
-		List<Result> results = lottos.stream().map(lotto -> lotto.checkRank(winningNumbers, bonus)).toList();
-
-		int total = results.stream().mapToInt(Result::getPrize).sum();
-
+		List<RankEnum> results = lottos.stream().map(lotto -> lotto.checkRank(winningNumbers,bonus)).toList();
+		int total = results.stream().mapToInt(RankEnum::getPrize).sum();
 		double profitRate = ((double) (total * 100) / (count * 1000));
+		printResult(results, profitRate);
+	}
 
-		List<Result> first = results.stream().filter(result -> result.getRank() == RankEnum.FIRST).toList();
-		List<Result> second = results.stream().filter(result -> result.getRank() == RankEnum.SECOND).toList();
-		List<Result> third = results.stream().filter(result -> result.getRank() == RankEnum.THIRD).toList();
-		List<Result> forth = results.stream().filter(result -> result.getRank() == RankEnum.FORTH).toList();
-		List<Result> fifth = results.stream().filter(result -> result.getRank() == RankEnum.FIFTH).toList();
-
+	public static void printResult(List<RankEnum> results, double profitRate){
 		System.out.println();
 		System.out.println("당첨 통계");
 		System.out.println("---");
-		System.out.println("3개 일치 (5,000원) - " + fifth.size() + "개");
-		System.out.println("4개 일치 (50,000원) - " + forth.size() + "개");
-		System.out.println("5개 일치 (1,500,000원) - " + third.size() + "개");
-		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + second.size() + "개");
-		System.out.println("6개 일치 (2,000,000,000원) - " + first.size() + "개");
+		for (RankEnum rank : RankEnum.values()) {
+			if (rank == RankEnum.NONE) continue;
+			String message = "";
+			if (rank.hasBonus()) message = ", 보너스 볼 일치";
+			long countByRank = results.stream().filter(result -> result == rank).count();
+			String prize = rank.getFormattedPrize();
+			System.out.println(rank.getMatchCount() + "개 일치" + message + " (" + prize + "원) - " + countByRank + "개");
+		}
 		System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
 	}
 

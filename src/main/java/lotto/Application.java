fa9607c9dacd9b1241b7029bf82a9ffa9f1a4381
kseenyoung <kseenyoung@gmail.com>
2023-11-06 20:27:50 +0900

    feat: 사용자가 돈 입력 시 예외처리 in User class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startLotto();
     }
+
+    public static void startLotto(){
+        User user = new User();
+        Lotto lotto = new Lotto(getLottoNumbers());
+
+    }
+
+    private static int getMoney() {
+
+        return 1;
+    }
+
+    public static List<Integer> getLottoNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+        String[] input = Console.readLine().split(",");
+
+        return numbers;
+    }
+
+
+
 }
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,17 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
 
     private int money;
-    private int canGetTickets;
+    private int tickets;
 
-    User(int money){
-        this.money = money;
+    User(){
+        getMoney();
+    }
+
+    private void getMoney(){
         while(true){
             try{
-
+                this.money = Integer.parseInt(Console.readLine());
+                tickets = getTickets(money);
+                return;
             } catch (IllegalArgumentException e){
-
+                System.out.println("[ERROR] 구매 금액은 천원 단위로 입력해주세요.");
             }
         }
     }
@@ -19,7 +26,7 @@ public class User {
     /**
      * 천원단위로 나눠 떨어지지 않을 때 예외처리
      * */
-    private int canGetTickets(int money){
+    private int getTickets(int money){
         if(money % 1000 != 0){
             throw new IllegalArgumentException();
         }

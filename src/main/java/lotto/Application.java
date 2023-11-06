058977ec3b340959971febd46f1a27f8dde3fb37
Dcool <90443691+Leesiwon12171260@users.noreply.github.com>
2023-11-06 22:34:17 +0900

    로또 금액을 입력 받고 개수 만큼의 로또 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구매금액을 입력해 주세요.");
+        int buyMoney = Integer.parseInt(Console.readLine());
+        int count = buyMoney/1000;
+        System.out.println(count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            System.out.println(lotto);
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +13,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers == null || numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 정확히 6개여야 합니다.");
         }
+
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 있습니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+    public String toString() {
+        return numbers.toString();
     }
 
     // TODO: 추가 기능 구현

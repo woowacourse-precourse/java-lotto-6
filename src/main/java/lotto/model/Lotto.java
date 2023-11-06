9fa3959861d58eaa559d0c95b45500b0a9f4002e
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-07 01:04:58 +0900

    refactor: Lotto equals & hashCode method 추가, getNumber map() 인자 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,7 @@ import static lotto.util.Constants.CONFIG_NUMBERS_SIZE;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Lotto {
@@ -19,7 +20,7 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers.stream()
-            .map(number -> number.getNumber())
+            .map(Number::getNumber)
             .toList();
     }
 
@@ -47,5 +48,22 @@ public class Lotto {
         if (checkDuplicate.size() != CONFIG_NUMBERS_SIZE) {
             throw new IllegalArgumentException("중복된 수가 존재");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 }

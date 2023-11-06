9435a24aa9dd1bf66fe7b331b8629208b93627d5
HwangSeokJin <ghkd3703@gmail.com>
2023-11-06 22:33:15 +0900

    feat: 수익률을 출력하는 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class OutputView {
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
     private static final String LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
 
     public void printErrorMessage(String message) {
         System.out.println(String.format(ERROR_MESSAGE_FORMAT, message));
@@ -15,5 +16,9 @@ public class OutputView {
         for (String number : numbers) {
             System.out.println(number);
         }
+    }
+
+    public void printProfitRate(float profit) {
+        System.out.println(String.format(PROFIT_RATE_FORMAT, profit));
     }
 }

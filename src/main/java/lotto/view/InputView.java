6b26ac902f7d99c4fac9a3ce03ac79912859c7a0
sangeun-b <baek0007@algonquinlive.com>
2023-11-06 22:19:08 +0900

    feat: 로또 구입 금액 입력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static final String INPUT_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public String inputAmount() {
+        System.out.println(INPUT_AMOUNT);
+        String amount = Console.readLine();
+        return amount;
+    }
+}

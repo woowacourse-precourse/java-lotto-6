70d06d3a41a73d519457caa593509a492e0ba276
GS_song <20003204@sju.ac.kr>
2023-11-06 17:14:43 +0900

    refactor: 통계 출력 로직 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,11 +2,15 @@ package lotto.view;
 
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import lotto.model.Lotto;
 import lotto.model.LottoResult;
 
 public class OutputView {
+
+    private static final String STATISTIC_FORMAT = "%.1f";
+    private static final String DECIMAL_FORMAT = "###,###";
 
     public static void printUserLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
@@ -20,7 +24,7 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.println(makeResultsToString(lottoResults));
-        System.out.println("총 수익률은 " + gainStatistic + "%입니다.");
+        System.out.println("총 수익률은 " + String.format(STATISTIC_FORMAT, gainStatistic) + "%입니다.");
     }
 
     private static String changeLottoToString(List<Integer> numbers) {
@@ -29,20 +33,23 @@ public class OutputView {
                 .collect(Collectors.joining(", ", "[", "]"));
     }
 
-    private static String makeResultsToString(List<LottoResult> results) {
+    private static String makeResultsToString(List<LottoResult> userResults) {
         StringBuilder stringBuilder = new StringBuilder();
-        DecimalFormat decFormat = new DecimalFormat("###,###");
+        DecimalFormat decFormat = new DecimalFormat(DECIMAL_FORMAT);
 
-        for (LottoResult lottoResult : LottoResult.values()) {
-            long count = results.stream().filter(res -> res == lottoResult).count();
-            stringBuilder.append(lottoResult.getRule())
-                    .append(" (" + decFormat.format(lottoResult.getPrize()) + "원)")
-                    .append(" - ")
-                    .append(count)
-                    .append("개")
-                    .append("\n");
+        for (LottoResult lottoResultFormat : LottoResult.values()) {
+            stringBuilder.append(lottoResultFormat.getRule())
+                    .append(" (" + decFormat.format(lottoResultFormat.getPrize()) + "원) - ")
+                    .append(findEqualResultCount(userResults, lottoResultFormat))
+                    .append("개\n");
         }
 
         return stringBuilder.toString();
+    }
+
+    private static long findEqualResultCount(List<LottoResult> results, LottoResult lottoResult) {
+        return results.stream()
+                .filter(Objects::nonNull)
+                .filter(res -> res.equals(lottoResult)).count();
     }
 }

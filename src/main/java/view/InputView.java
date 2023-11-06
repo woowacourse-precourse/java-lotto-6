fb3fc90bcd774206068400e8284e4b6287632c58
suhyun <qkrtngus116@naver,com>
2023-11-06 11:43:07 +0900

    refactor(InputView): 당첨번호와 보너스번호를 입력받기 위한 메소드를 일부 수정하였습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -16,7 +16,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String enterAmount(){
         System.out.println(INPUT_AMOUNT_MESSAGE);
@@ -24,10 +24,12 @@ public class InputView {
     }
 
     public static String enterWinningNumbers(){
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
         return Console.readLine();
     }
 
     public static String enterBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }

12a7dfd36dcd9dd74c9eaa666e18ae1b7fef2e32
silverwest8 <silverwest8@gmail.com>
2023-11-07 03:50:36 +0900

    feat: add Message class

---
+++ src/main/java/lotto/Message.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Message {
+    public static final String RESULT_SHOW_MESSAGE = "당첨 통계";
+    public static final String DIVIDER = "---";
+
+    static final String PURCHASE_INPUT_PROMPT = "구입 금액을 입력해 주세요.";
+    static final String PURCHASE_CONFIRMATION_MESSAGE = "개를 구매했습니다.";
+    static final String WINNING_NUMBER_INPUT_PROMPT = "당첨 번호을 입력해 주세요.";
+    static final String BONUS_NUMBER_INPUT_PROMPT = "보너스 번호을 입력해 주세요.";
+
+}

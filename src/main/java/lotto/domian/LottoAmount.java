896689b2e07be28dc741a2e84b39b5a2ea931f42
km9404 <kmh940408@gmail.com>
2023-11-06 21:09:35 +0900

    feat (LottoAmount): 로또 구매 개수 계산 기능 구현

---
+++ src/main/java/lotto/domian/LottoAmount.java
@@ -0,0 +1,25 @@
+package lotto.domian;
+
+public class LottoAmount {
+    public long lottoAmount(long amount) {
+        validateAmount(amount);
+        return amount / 1000;
+    }
+
+    public void validateAmount(long amount) {
+        validateMinAmount(amount);
+        validateRemainderDivisionBy1000(amount);
+    }
+
+    private void validateMinAmount(long amount) {
+        if (amount < 1000) {
+            throw new IllegalArgumentException("최소 입력 금액은 1,000입니다.");
+        }
+    }
+
+    private void validateRemainderDivisionBy1000(long amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("입력 금액은 1,000원 단위로 입력이 필요합니다.");
+        }
+    }
+}

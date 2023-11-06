4fe0daa77a74a562f2360fab668124ba08872f1e
HSBODo <vinsulill@gmaile.com>
2023-11-06 19:49:59 +0900

    test : private 메서드 테스트 주석처리
    주석된 메서드
    - isDigit()
    - isThousandUnits()
    - inputWinningNumberSplit()
    - isWinningNumberDigit()
    - isRange()

--- src/test/java/lotto/service/LottoGameServiceTest.java
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -39,44 +39,6 @@ class LottoGameServiceTest {
     }
 
     @Test
-    @DisplayName("입력받은 값이 숫자인지 검증한다.")
-    void isDigit_정상케이스() {
-        String input = "14000";
-
-        Boolean result = lottoGameService.isDigit(input);
-
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    @DisplayName("입력받은 값이 숫자가 아닌지 검증한다.")
-    void isDigit_예외케이스() {
-        String input = "만사천원";
-
-        assertThatThrownBy(() -> lottoGameService.isDigit(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(LottoException.INPUT_NOT_DiGIT.getMessage());
-
-    }
-
-    @Test
-    @DisplayName("입력받은 값이 1,000원 단위 인지 검증한다.")
-    void isThousandUnits_정상케이스() {
-        String input = "14000";
-        boolean result = lottoGameService.isThousandUnits(input);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    @DisplayName("입력받은 값이 1,000원 단위 인지 검증한다.")
-    void isThousandUnits_예외케이스() {
-        String input = "14200";
-        assertThatThrownBy(() -> lottoGameService.isThousandUnits(input))
-              .isInstanceOf(IllegalArgumentException.class)
-              .hasMessageContaining(LottoException.INPUT_NOT_THOUSAND_UNITS.getMessage());
-    }
-
-    @Test
     @DisplayName("입력받은 구매금액을 검증한다.")
     void inputPurchaseAmountValidation_정상케이스() {
         String input = "14000";
@@ -92,44 +54,6 @@ class LottoGameServiceTest {
         boolean result = lottoGameService.inputPurchaseAmountValidation(input);
 
         assertThat(result).isFalse();
-    }
-
-    @Test
-    @DisplayName("입력받은 금액으로 로또 수량을 계산한다.")
-    void lottoQuantity() {
-        int lottoQuantity = lottoGameService.lottoQuantity(14000);
-        assertThat(lottoQuantity).isEqualTo(14);
-    }
-
-    @Test
-    @DisplayName("입력받은 값을 쉼표(,) 구분자로 나눈다.")
-    void inputWinningNumberSplit_정상케이스() {
-        String[] inputWinningNumberSplit = lottoGameService.inputWinningNumberSplit("1,2,3,4,5,6");
-        assertThat(inputWinningNumberSplit).hasSize(6);
-    }
-
-    @Test
-    @DisplayName("입력받은 값을 쉼표(,) 구분자로 나눈다.")
-    void inputWinningNumberSplit_예외케이스() {
-        assertThatThrownBy(() -> lottoGameService.inputWinningNumberSplit("123456"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(LottoException.INPUT_NOT_SPLIT.getMessage());
-    }
-
-    @Test
-    void isWinningNumberDigit_정상케이스() {
-        String[] inputWinningNumberSplit = {"1","2","3","4","5","6"};
-        boolean result = lottoGameService.isWinningNumberDigit(inputWinningNumberSplit);
-
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    void isWinningNumberDigit_예외케이스() {
-        String[] inputWinningNumberSplit = {"일","2","삼","4","오","6"};
-        assertThatThrownBy(() -> lottoGameService.isWinningNumberDigit(inputWinningNumberSplit))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(LottoException.INPUT_NOT_DiGIT.getMessage());
     }
 
     @Test
@@ -163,22 +87,101 @@ class LottoGameServiceTest {
         assertThat(result).isFalse();
     }
 
-
     @Test
-    @DisplayName("입력받은 보너스번호가가 1~45 범위인지 검증한다.")
-    void isRange_정상케이스() {
-        int input = 45;
-        boolean result = lottoGameService.isRange(input, 1, 45);
-
-        assertThat(result).isTrue();
+    @DisplayName("입력받은 금액으로 로또 수량을 계산한다.")
+    void lottoQuantity() {
+        int lottoQuantity = lottoGameService.lottoQuantity(14000);
+        assertThat(lottoQuantity).isEqualTo(14);
     }
 
-    @Test
-    @DisplayName("입력받은 보너스번호가가 1~45 범위인지 검증한다.")
-    void inputBonusNumberRangeValidation_예외케이스() {
-        int input = 46;
-        assertThatThrownBy(() -> lottoGameService.isRange(input,1,45))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(LottoException.INPUT_NOT_RANGE.getMessage());
-    }
+
+//    @Test
+//    @DisplayName("입력받은 값이 숫자인지 검증한다.")
+//    void isDigit_정상케이스() {
+//        String input = "14000";
+//
+//        Boolean result = lottoGameService.isDigit(input);
+//
+//        assertThat(result).isTrue();
+//    }
+//
+//    @Test
+//    @DisplayName("입력받은 값이 숫자가 아닌지 검증한다.")
+//    void isDigit_예외케이스() {
+//        String input = "만사천원";
+//
+//        assertThatThrownBy(() -> lottoGameService.isDigit(input))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(LottoException.INPUT_NOT_DiGIT.getMessage());
+//
+//    }
+//
+//    @Test
+//    @DisplayName("입력받은 값이 1,000원 단위 인지 검증한다.")
+//    void isThousandUnits_정상케이스() {
+//        String input = "14000";
+//        boolean result = lottoGameService.isThousandUnits(input);
+//        assertThat(result).isTrue();
+//    }
+//
+//    @Test
+//    @DisplayName("입력받은 값이 1,000원 단위 인지 검증한다.")
+//    void isThousandUnits_예외케이스() {
+//        String input = "14200";
+//        assertThatThrownBy(() -> lottoGameService.isThousandUnits(input))
+//              .isInstanceOf(IllegalArgumentException.class)
+//              .hasMessageContaining(LottoException.INPUT_NOT_THOUSAND_UNITS.getMessage());
+//    }
+
+
+//    @Test
+//    @DisplayName("입력받은 값을 쉼표(,) 구분자로 나눈다.")
+//    void inputWinningNumberSplit_정상케이스() {
+//        String[] inputWinningNumberSplit = lottoGameService.inputWinningNumberSplit("1,2,3,4,5,6");
+//        assertThat(inputWinningNumberSplit).hasSize(6);
+//    }
+//
+//    @Test
+//    @DisplayName("입력받은 값을 쉼표(,) 구분자로 나눈다.")
+//    void inputWinningNumberSplit_예외케이스() {
+//        assertThatThrownBy(() -> lottoGameService.inputWinningNumberSplit("123456"))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(LottoException.INPUT_NOT_SPLIT.getMessage());
+//    }
+
+//    @Test
+//    void isWinningNumberDigit_정상케이스() {
+//        String[] inputWinningNumberSplit = {"1","2","3","4","5","6"};
+//        boolean result = lottoGameService.isWinningNumberDigit(inputWinningNumberSplit);
+//
+//        assertThat(result).isTrue();
+//    }
+//
+//    @Test
+//    void isWinningNumberDigit_예외케이스() {
+//        String[] inputWinningNumberSplit = {"일","2","삼","4","오","6"};
+//        assertThatThrownBy(() -> lottoGameService.isWinningNumberDigit(inputWinningNumberSplit))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(LottoException.INPUT_NOT_DiGIT.getMessage());
+//    }
+
+
+
+//    @Test
+//    @DisplayName("입력받은 보너스번호가가 1~45 범위인지 검증한다.")
+//    void isRange_정상케이스() {
+//        int input = 45;
+//        boolean result = lottoGameService.isRange(input, 1, 45);
+//
+//        assertThat(result).isTrue();
+//    }
+//
+//    @Test
+//    @DisplayName("입력받은 보너스번호가가 1~45 범위인지 검증한다.")
+//    void inputBonusNumberRangeValidation_예외케이스() {
+//        int input = 46;
+//        assertThatThrownBy(() -> lottoGameService.isRange(input,1,45))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessageContaining(LottoException.INPUT_NOT_RANGE.getMessage());
+//    }
 }

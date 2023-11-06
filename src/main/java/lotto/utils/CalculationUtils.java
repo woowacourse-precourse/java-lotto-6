93842b5e8001d2dfb9d7eb0fe0473de066fe3fb8
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 02:24:28 +0900

    feat(utils.CalculationUtils): 기능30. 숫자가 특정 범위에 있는지 확인한다
    isNumberInRange 메서드는 목표숫자가 특정 범위에 있는지 확인하는 메서드 입니다. 로또 번호가 1 ~ 45 사이에 있는 값인지 확인할 때 사용했습니다.

--- src/main/java/lotto/utils/CalculationUtils.java
+++ src/main/java/lotto/utils/CalculationUtils.java
@@ -1,4 +1,9 @@
 package lotto.utils;
 
 public class CalculationUtils {
+
+    public static boolean isNumberInRange(int number, int start, int end) {
+        return number >= start && number <= end;
+    }
+
 }

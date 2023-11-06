1fa7cc0e4d1e16cd79cb8fb198210808e73cd6e5
zjacom <eephus625@icloud.com>
2023-11-07 02:49:36 +0900

    refactor(lotto): 중복된 코드 제거
    함수형 인터페이스를 사용해서 중복되는 코드 제거

--- src/main/java/lotto/InputValueController.java
+++ src/main/java/lotto/InputValueController.java
@@ -1,46 +1,32 @@
 package lotto;
 
+import java.util.function.Supplier;
 import view.InputView;
+import java.util.function.Consumer;
 
 public class InputValueController {
     InputView inputView = new InputView();
     InputValidate inputValidate = new InputValidate();
 
     public void inputPurchaseAmount() {
-        boolean isValidInput = false;
-
-        while (!isValidInput) {
-            String enteredPurchaseAmount = inputView.purchaseAmount();
-            try {
-                inputValidate.validatePurchaseAmount(enteredPurchaseAmount);
-                isValidInput = true;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        inputWithValidation(inputView::purchaseAmount, inputValidate::validatePurchaseAmount);
     }
 
     public void inputPrizeNumber() {
-        boolean isValidInput = false;
-
-        while (!isValidInput) {
-            String inputStr = inputView.prizeNumber();
-            try {
-                inputValidate.validatePrizeNumber(inputStr);
-                isValidInput = true;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        inputWithValidation(inputView::prizeNumber, inputValidate::validatePrizeNumber);
     }
 
     public void inputBonusNumber() {
+        inputWithValidation(inputView::bonusNumber, inputValidate::validateBonusNumber);
+    }
+
+    private void inputWithValidation(Supplier<String> inputSupplier, Consumer<String> validationFunction) {
         boolean isValidInput = false;
 
         while (!isValidInput) {
-            String bonusNumberTemp = inputView.bonusNumber();
             try {
-                inputValidate.validateBonusNumber(bonusNumberTemp);
+                String input = inputSupplier.get();
+                validationFunction.accept(input);
                 isValidInput = true;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());

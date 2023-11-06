6b2370493071b151a0b12b03bee19198eb8cb9a7
kwon <kckc0608@naver.com>
2023-11-06 22:15:45 +0900

    feat: 로또 구입 금액 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,4 +1,5 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static final String MESSAGE_FOR_PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
@@ -8,6 +9,10 @@ public class Application {
 
     private static int getPurchaseAmountWithInput() {
         System.out.println(MESSAGE_FOR_PURCHASE_AMOUNT_INPUT);
+        //while (true) {
+            String purchaseAmountInput = Console.readLine();
+        //}
         return 0;
     }
+
 }

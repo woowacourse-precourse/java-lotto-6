cfa0f78805aa8634727de932419f2bddc4120781
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-06 20:15:10 +0900

    feat: 생성된 로또를 오르차순으로 정렬 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import static java.util.Collections.sort;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +16,17 @@ public class LottoService {
         List<Lotto> purchasedLotto = new ArrayList<>();
         for (int i = 0; i < pickCount; i++) {
             List<Integer> purchasedOneLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            sort(purchasedOneLotto);
             Lotto lotto = new Lotto(purchasedOneLotto);
             purchasedLotto.add(lotto);
         }
         return purchasedLotto;
     }
 
-    public static PurchasedLottoDTO purchasedLottoToDTO(PurchasedLottoNumbers purchasedLottoNumbers){
+    public static PurchasedLottoDTO purchasedLottoToDTO(PurchasedLottoNumbers purchasedLottoNumbers) {
         int purchasedLottoCount = purchasedLottoNumbers.getPurchasedLotto().size();
         List<Lotto> purchasedLotto = new ArrayList<>();
-        for(int i = 0; i < purchasedLottoCount; i++){
+        for (int i = 0; i < purchasedLottoCount; i++) {
             purchasedLotto.add(purchasedLottoNumbers.getPurchasedLotto().get(i));
 
         }

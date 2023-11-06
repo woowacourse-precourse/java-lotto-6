9ff07ea3b68835e555b0a261a24a4face5aa93e0
Tiger <skdlghwns@gmail.com>
2023-11-06 18:25:44 +0900

    feat: 입력시 발생하는 예외 메시지 추가 - NUMBER_RANGE_ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의   숫자여야 합니다.") - PRICE_DEVIDE_ERROR_MESSAGE("[ERROR] 1000원 단위로 입력해야 합니다.") - PURE_INTEGER_ERROR_MESSAGE("[ERROR] 정수 입력이여야 합니다.")

--- src/main/java/lotto/enums/Messages.java
+++ src/main/java/lotto/enums/Messages.java
@@ -5,6 +5,9 @@ public enum Messages {
     INPUT_WINNUMER_MESSAGE("당첨 번호를 입력해 주세요."),
     INPUT_BONUSNUMER_MESSAGE("보너스 번호를 입력해 주세요."),
     LOTTERY_STATISTICS("당첨 통계"),
+    NUMBER_RANGE_ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    PRICE_DEVIDE_ERROR_MESSAGE("[ERROR] 1000원 단위로 입력해야 합니다."),
+    PURE_INTEGER_ERROR_MESSAGE("[ERROR] 정수 입력이여야 합니다."),
     ;
 
     private String message;

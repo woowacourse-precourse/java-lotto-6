e590c71e3fed3a169f257037b27dc68776bda205
현 <sohyun95277@gmail.com>
2023-11-06 14:59:45 +0900

    refactor(LottoCalculator): Customer 객체로 이전된 함수들 제거
    총 당첨 결과는 Customer 객체에서 진행, Calculator는 수익률만 계산

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -4,54 +4,14 @@ import static lotto.constant.LottoInformation.LOTTO_PRICE;
 
 import java.text.DecimalFormat;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import lotto.constant.Prize;
 
 public class LottoCalculator {
-    private final int SPECIAL_MATCH = 7;
-    private final Lotto winningNumber;
-    private final int bonusNumber;
-    private final Map<Prize, Integer> result = new LinkedHashMap<>();
+    private final Map<Prize, Integer> result;
 
-    public LottoCalculator(WinningNumbers winningNumbers) {
-        this.winningNumber = winningNumbers.getWinningNumber();
-        this.bonusNumber = winningNumbers.getBonusNumber();
-    }
-
-    public void makePrizeResult(List<Lotto> lottoTickets) {
-        initResult();
-        for (Lotto oneTicket : lottoTickets) {
-            int matchCount = countMatchNumbers(oneTicket);
-            Prize prize = Prize.getByMatch(matchCount);
-
-            if (prize != Prize.NONE) {
-                result.put(prize, result.get(prize) + 1);
-            }
-        }
-    }
-
-    public void initResult() {
-        result.put(Prize.FIFTH, 0);
-        result.put(Prize.FOURTH, 0);
-        result.put(Prize.THIRD, 0);
-        result.put(Prize.SECOND, 0);
-        result.put(Prize.FIRST, 0);
-        result.put(Prize.NONE, 0);
-    }
-
-    public int countMatchNumbers(Lotto lotto) {
-        int matchCount = 0;
-        for (Integer number : lotto.getNumbers()) {
-            if (winningNumber.getNumbers().contains(number)) {
-                matchCount++;
-            }
-            if (matchCount == 5 && isContainBonusNumber(lotto)) {
-                return SPECIAL_MATCH;
-            }
-        }
-        return matchCount;
+    public LottoCalculator(Map<Prize, Integer> result) {
+        this.result = result;
     }
 
     public String calculateProfitRate() {
@@ -80,10 +40,6 @@ public class LottoCalculator {
                 .mapToInt(Integer::intValue)
                 .sum();
         return count * LOTTO_PRICE;
-    }
-
-    private boolean isContainBonusNumber(Lotto lotto) {
-        return lotto.getNumbers().contains(bonusNumber);
     }
 
     public Map<Prize, Integer> getResult() {

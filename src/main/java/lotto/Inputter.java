7f3efd0f1b6123d1cfd724b29676766229208f43
Identityless <rlandwnd555@naver.com>
2023-11-07 00:24:32 +0900

    feat : 구입 금액 유효성 검사 기능 추가

--- src/main/java/lotto/Inputter.java
+++ src/main/java/lotto/Inputter.java
@@ -3,6 +3,8 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Inputter {
+
+    private static final int MONEY_UNIT = 1000;
     private static Inputter inputter;
 
     private Inputter() {}
@@ -23,6 +25,13 @@ public class Inputter {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요.");
         }
+        validatePurchase(won);
         return won;
+    }
+
+    private void validatePurchase(int won) {
+        if (won % MONEY_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] "+ MONEY_UNIT + "원 단위로 입력해주세요.");
+        }
     }
 }

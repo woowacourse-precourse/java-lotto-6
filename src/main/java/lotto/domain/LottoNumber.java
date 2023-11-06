328bcd37e48370c1a113b70add2d139c9f6088dc
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:10:14 +0900

    refector: 로또 숫자 길이 검증 메소드명 변경

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -14,10 +14,10 @@ public class LottoNumber {
     }
 
     private static void validateLottoNumber(Integer lottoNumber) {
-        validateRange(lottoNumber);
+        validateLottoNumberRange(lottoNumber);
     }
 
-    private static void validateRange(Integer lottoNumber) {
+    private static void validateLottoNumberRange(Integer lottoNumber) {
         if (lottoNumber < 1 || lottoNumber > 45) {
             throw new IllegalArgumentException("[ERROR] 각각의 당첨 번호는 1~45 범위의 정수이어야 합니다.");
         }

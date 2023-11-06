625c4d32750015ed2dbcc5a996aa19bacd2181e4
gahyeon <1000ji1@naver.com>
2023-11-06 20:25:48 +0900

    Feat : 발행한 로또 번호 출력 함수 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,21 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public static void printQuantity(int quantity) {
         System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumbers(List<Integer> numbers) {
+        System.out.print("[");
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+            if (i < numbers.size() - 1) {
+                System.out.print(", "); // 숫자 사이에 쉼표 추가
+            }
+        }
+        System.out.println("]");
     }
 }

b1a69f9129eb79567d1e0734c3da605f37cc04f5
stopmin <geemin0@naver.com>
2023-11-07 03:07:11 +0900

    feat: Lotto게임에 Player 도메인 추가
    이후, 비교하는 로직도 추가함

--- src/main/java/lotto/model/LottoGame.java
+++ src/main/java/lotto/model/LottoGame.java
@@ -1,15 +1,33 @@
 package lotto.model;
 
 import lotto.controller.InputController;
+import lotto.domain.Lotto;
 import lotto.domain.LottoBoard;
+import lotto.domain.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LottoGame {
     private LottoBoard lottoBoard;
+    private Player player;
     private InputController inputController = new InputController();
+    private int[] result = {0, 0, 0, 0, 0};
 
     public LottoGame() {
         inputController.startGame();
         lottoBoard = new LottoBoard(inputController.getMoney());
+
+        player = new Player(inputController.playerInput(), inputController.bonusInput());
+
+        for (int i = 0; i < lottoBoard.getTickets(); i++) {
+            checkLotto(lottoBoard.getOneLotto(i), player.getNumbers(), player.getBonus());
+        }
+    }
+
+    void checkLotto(Lotto lotto, List<Integer> numbers, Integer bonus) {
+        System.out.println(lotto);
     }
 
 }

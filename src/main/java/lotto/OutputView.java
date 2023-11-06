c11b7c1e6e0e8be2d3857b4f43d34bba26ff205a
SUJI-J <suzy852@naver.com>
2023-11-06 23:41:48 +0900

    feat: 보너스 숫자 입력 문구 출력

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -3,6 +3,7 @@ package lotto;
 import static lotto.Constants.REQUEST_BUY_MONEY;
 import static lotto.Constants.TICKET_COUNT;
 import static lotto.Constants.REQUEST_WINNING_NUMBERS;
+import static lotto.Constants.REQUEST_BONUS_NUMBERS;
 
 public class OutputView {
     public static void printRequestMoney() {
@@ -19,6 +20,10 @@ public class OutputView {
 
     public static void printWinningNumber() {
         System.out.println(REQUEST_WINNING_NUMBERS);
+    }
+
+    public static void printBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBERS);
     }
 
     public static void printBlankLine() {

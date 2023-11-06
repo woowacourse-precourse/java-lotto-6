0078e02ef53ac528080ba12f9ace250c2f5177a1
HungKungE <jonsun59@gmail.com>
2023-11-06 21:44:50 +0900

    [docs] 구현 목록 수정 및 추가

--- src/main/java/lotto/core/Lotto.java
+++ src/main/java/lotto/core/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
 import java.util.List;
+import static lotto.utils.Validator.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validate(numbers); // 길이 검사
+        hasDuplicateInNumbers(numbers); // 중복 검사
+        validateIsNumbersInRange(numbers); // 1~45 사이 검사
         this.numbers = numbers;
     }
 

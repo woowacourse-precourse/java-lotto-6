5f09a9399bb6f1381db228c3f93d7cb8f26b0552
aammddkkzxc <ideal0427@naver.com>
2023-11-06 19:11:53 +0900

    feat: 무작위 숫자 발행 기능 구현 및 클래스 이름 명확하게 수정

--- src/main/java/lotto/NumberGenerator.java
+++ src/main/java/lotto/NumberGenerator.java
@@ -1,7 +1,15 @@
 package lotto;
 
-public class NumberGenerator {
-    public void generate() {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
+public class NumberGenerator {
+    private static final int NUMBER_MIN_VALUE = 1;
+    private static final int NUMBER_MAX_VALUE = 45;
+    private static final int NUMBER_COUNT = 6;
+
+    public void generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(NUMBER_MIN_VALUE, NUMBER_MAX_VALUE,
+                NUMBER_COUNT);
     }
 }

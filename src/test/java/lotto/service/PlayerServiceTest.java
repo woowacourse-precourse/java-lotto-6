6ed9ed239cb710fbe6d8d866a79be9a6d85cd3a1
youngjunkim <kyj0703@konkuk.ac.kr>
2023-11-06 11:16:33 +0900

    test: 로또별 맞은 개수 확인 관련 테스트 코드 추가

--- src/test/java/lotto/service/PlayerServiceTest.java
+++ src/test/java/lotto/service/PlayerServiceTest.java
@@ -1,7 +1,13 @@
 package lotto.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import lotto.domain.CorrectCount;
+import lotto.domain.Lotto;
 import lotto.domain.Player;
-import org.assertj.core.api.Assertions;
+import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +18,7 @@ class PlayerServiceTest {
     Player player;
     PlayerService playerService;
     static LottoGenerator lottoGenerator;
+    static WinningNumberGenerator winningNumberGenerator;
     static WinningLottoCalculator winningLottoCalculator;
 
     @BeforeAll
@@ -34,6 +41,26 @@ class PlayerServiceTest {
         //when
         playerService.buy(lottoCount);
         //then
-        Assertions.assertThat(player.getNumberOfLotto()).isEqualTo(lottoCount);
+        assertThat(player.getNumberOfLotto()).isEqualTo(lottoCount);
+    }
+
+    @Test
+    @DisplayName("로또 숫자를 맞춘 개수를 정확하게 계산한다.")
+    void lottoCheckTest(){
+        //given
+        final Lotto lotto = new Lotto(List.of(1,3,5,7,9,10));
+        final List<Integer> WINNING_NUMBER = List.of(1,2,3,4,5,6);
+        final int BONUS_NUMBER = 10;
+        final int CORRECT_COUNT = 3;
+        final int CORRECT_BONUS_COUNT = 1;
+        WinningNumbers winningNumbers = new WinningNumbers(WINNING_NUMBER,BONUS_NUMBER);
+        //when
+        player.addLotto(lotto);
+        playerService.check(winningNumbers);
+        //then
+        for (CorrectCount correctLottoCount : player.getCorrectLottoCounts()) {
+            assertEquals(CORRECT_COUNT, correctLottoCount.getCorrectNumberCount(),"맞춘 개수가 동일해야 한다.");
+            assertEquals(CORRECT_BONUS_COUNT, correctLottoCount.getCorrectBonusNumberCount(),"보너스 개수가 동일해야 한다.");
+        }
     }
 }

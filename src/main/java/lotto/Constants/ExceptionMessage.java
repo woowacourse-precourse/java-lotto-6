355e8aba671109ec99a0cd05b676e6cc195e0012
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-07 00:50:36 +0900

    add: 애플리케이션 작동 확인

--- src/main/java/lotto/Constants/ExceptionMessage.java
+++ src/main/java/lotto/Constants/ExceptionMessage.java
@@ -1,21 +1,23 @@
 package lotto.Constants;
 
 public enum ExceptionMessage {
-    ERROR_MESSAGE ("[ERROR] "),
-    INPUT_EMPTY ("값을 입력 해주세요"),
-    INPUT_TYPE ("자연수만 입력 가능합니다"),
-    DIVISION_THOUSAND ("1000원 단위로 구매 가능합니다"),
-    INPUT_DUPLICATE ("중복된 번호가 있습니다"),
-    INTPUT_NUMBER_LENGTH ("1 ~ 45 사이의 번호만 입력 가능합니다"),
-    SIX_NUMBER ("6개의 당첨 번호를 입력해 주세요"),
-    ONE_NUMBER ("보너스 번호는 하나만 입력 가능합니다");
+    INPUT_EMPTY ("공백은 입력 불가능 합니다."),
+    INPUT_TYPE ("올바른 숫자를 입력해 주세요."),
+    INPUT_ZERO ("0보다 큰 숫자를 입력해 주세요."),
+    DIVISION_THOUSAND ("1000원 단위로 구매 가능합니다."),
+    INPUT_DUPLICATE ("중복된 번호가 있습니다."),
+    INTPUT_NUMBER_LENGTH ("1 ~ 45 사이의 번호만 입력 가능합니다."),
+    SIX_NUMBER ("6개의 당첨 번호를 입력해 주세요."),
+    NUMBER_OVERFLOW ("int의 범위를 벗어났습니다."),
+    IN_WINNING_NUMBERS ("당첨 번호에 포함되지 않는 번호를 입력해 주세요.");
 
+    private final static String ERROR_MESSAGE = "[ERROR] ";
     private final String exceptionMassage;
     ExceptionMessage(String exceptionMessage){
-        this.exceptionMassage = exceptionMessage;
+        this.exceptionMassage = ERROR_MESSAGE + exceptionMessage;
     }
 
     public IllegalArgumentException throwexception(){
-        return new IllegalArgumentException(ERROR_MESSAGE + exceptionMassage);
+        return new IllegalArgumentException(exceptionMassage);
     }
 }

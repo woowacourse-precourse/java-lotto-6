bc528505ccd9263afe06bd7de2080069cd8439d7
HSBODo <vinsulill@gmaile.com>
2023-11-06 20:13:29 +0900

    feat : 메인메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,33 +1,12 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import lotto.model.Lotto;
-
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import lotto.controller.LottoGameController;
 
 
 public class Application {
     public static void main(String[] args) {
-//        String inputPurchaseAmount = inputPurchaseAmount();
-//        int purchaseAmount = inputPurchaseAmountValidation(inputPurchaseAmount);
-//        int lottoQuantity = lottoQuantity(purchaseAmount);
-//
-//        List<Lotto> lottos = createLottos(lottoQuantity);
-//        purchaseLottoNumbersDisplay(lottos);
-//
-//        String inputWinningNumber = inputWinningNumber();
-//        String[] inputWinningNumberSplit = inputWinningNumberSplit(inputWinningNumber);
-//        List<Integer> winningNumber = inputWinningNumberValidation(inputWinningNumberSplit);
-//
-//        String inputBonusNumber = inputBonusNumber();
-//        int inputBonusNumberValidation = inputBonusNumberValidation(inputBonusNumber);
-//        int bonusNumber = inputBonusNumberRangeValidation(inputBonusNumberValidation);
-//
-//        lottoWinningResult(lottos,winningNumber,bonusNumber);
+        LottoGameController lottoGameController = new LottoGameController();
+        lottoGameController.gameStart();
     }
 
 

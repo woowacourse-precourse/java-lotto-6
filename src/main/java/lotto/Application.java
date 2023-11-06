c60615202740cf15aedb0f73265e14075dcf2540
kwon <kckc0608@naver.com>
2023-11-06 22:34:51 +0900

    feat: 로또 구매 금액 입력시 숫자 확인 예외 메세지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,13 @@
 package lotto;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static final String MESSAGE_FOR_PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
+
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+    private static final String ERROR_MESSAGE_FOR_PURCHASE_AMOUNT_INPUT = "구앱금액은 1000원 단위로 숫자를 입력해 주세요.";
+
     public static void main(String[] args) {
         int purchaseAmount = getPurchaseAmountWithInput();
     }
@@ -24,8 +29,12 @@ public class Application {
         try {
             int purchaseAmount = Integer.parseInt(purchaseAmountInput);
         } catch (NumberFormatException e) {
+            printErrorMessage(ERROR_MESSAGE_FOR_PURCHASE_AMOUNT_INPUT);
             throw new IllegalArgumentException();
         }
     }
 
+    private static void printErrorMessage(String errorMessage) {
+        System.out.println(ERROR_MESSAGE_PREFIX + errorMessage);
+    }
 }

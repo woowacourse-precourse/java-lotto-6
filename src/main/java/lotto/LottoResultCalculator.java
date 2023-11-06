b4546aa9d1e8da043112dd2fa2db7af581f8a0ec
Taehyoun Kim <kth9903@gmail.com>
2023-11-06 23:57:29 +0900

    feat(lottoresultcalculator): initialize prize mapping for lotto results

---
+++ src/main/java/lotto/LottoResultCalculator.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import lotto.Lotto;
+
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class LottoResultCalculator {
+
+    private static final Map<Integer, Integer> prizeMap = new HashMap<>();
+    private static final int BOUNUS_MATCH = 7;
+
+    static {
+        prizeMap.put(3, 5_000);
+        prizeMap.put(4, 50_000);
+        prizeMap.put(5, 1_500_000);
+        prizeMap.put(BOUNUS_MATCH, 30_000_000);
+        prizeMap.put(6, 2_000_000_000);
+    }
+
+
+}

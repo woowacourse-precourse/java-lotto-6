718ce406ed866d5a176bca494c0e8ef5d0b53468
sehee <tpgml701@naver.com>
2023-11-06 22:29:11 +0900

    test(WinningNumbers): 범위를 벗어난 로또 번호 검증 기능

--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -12,24 +12,32 @@ public class WinningNumberTest {
     @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createWinningNumbersByOverSize() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7),8))
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7), 8))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외를 발생한다.")
     @Test
     void createWinningNumbersByDuplicateBonus() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6),6))
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("당첨 번호에 중복된 번호가 있으면 예외가 발생한다.")
     @Test
     void createWinningNumbersByDuplicate() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5),6))
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-
+    @DisplayName("범위를 벗어난 범위 로또 번호")
+    @Test
+    void out_of_range() {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 51), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }

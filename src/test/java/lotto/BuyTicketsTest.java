b0ca0b67ce00975b3040b2abe3b415776916451f
AruJoy <wkrjsdl5632@gmail.com>
2023-11-06 15:00:49 +0900

    Test: BuyTickets클래스 테스트케이스 작성
     -입력값이 숫자인지 확인하고 아니면 Excption을 제대로 던지는지 확인한다.
     -입력값이 1000의 배수인지 확인하고 아니면 Excption을 제대로 던지는지 확인한다.
     -입력값만큼의 티켓을 정상적으로 구매하는지 확인한다.

--- src/test/java/lotto/BuyTicketsTest.java
+++ src/test/java/lotto/BuyTicketsTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuyTicketsTest {
@@ -16,15 +20,29 @@ class BuyTicketsTest {
 
     @Test
     @DisplayName("입력값은 숫자여야 한다.")
-    void ValidateInputIsIntegerTest() {
+    void validateInputIsIntegerTest() {
         String purchaseAmount = "String INPUT";
         Assertions.assertThrows(IllegalStateException.class, () -> new BuyTickets(purchaseAmount));
     }
 
     @Test
-    @DisplayName("입력값은 숫자여야 한다.")
-    void ValidateInputIsMultipleNumberOfThousand() {
+    @DisplayName("입력값은 1000의 배수여야 한다.")
+    void validateInputIsMultipleNumberOfThousand() {
         String purchaseAmount = "1200";
         Assertions.assertThrows(IllegalStateException.class, () -> new BuyTickets(purchaseAmount));
+    }
+
+    @Test
+    @DisplayName("입력한 만큼의 티켓이 구입되었는지 확인")
+    void testNumberOfTickets() {
+        Map<Long, Integer> ticketAmounts = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            ticketAmounts.put(Long.valueOf(i), pickNumberInRange(1, 99));
+        }
+        for (Long key : ticketAmounts.keySet()) {
+            String buyAmounts = String.valueOf(ticketAmounts.get(key)*1000);
+            BuyTickets buyTickets = new BuyTickets(buyAmounts);
+            assertThat(buyTickets.getLotteryNumbers().size()).isEqualTo(ticketAmounts.get(key));
+        }
     }
 }

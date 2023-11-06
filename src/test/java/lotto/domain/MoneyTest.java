cab9920a2a7c74572883436887b5825e5d7206ae
inpink <inpink@kakao.com>
2023-11-06 22:07:44 +0900

    test: 금액 정수값을 매개변수로 받아 Money 객체 생성하는 정적 팩터리 메서드

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -17,4 +20,17 @@ public class MoneyTest {
         // Then
         assertTrue(money.isSameAmount(5000));
     }
+
+    @Test
+    public void 금액_객체생성_정상정수값() {
+        // Given
+        int input = 5000;
+
+        // When
+        Money money = Money.create(input);
+
+        // Then
+        assertTrue(money.isSameAmount(5000));
+    }
+
 }

edc6d3bbbf58f387c82f48e7d5aa295ffccfa381
Kiltae <loverpleasestayo0@gmail.com>
2023-11-06 21:56:38 +0900

    feat: add final variable

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+    private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_LOTTO_WINNING = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    private static List<Integer> winningNumberList;
+
+}

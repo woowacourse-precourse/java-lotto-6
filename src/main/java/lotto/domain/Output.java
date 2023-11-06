b930e4c028660060d9c1552c8aee7a67b6c5ee46
woong2e <tlswodnd12345@naver.com>
2023-11-07 06:10:48 +0900

    feat: 구매한 로또, 당첨 통계, 수익률 출력 기능

--- src/main/java/lotto/domain/Output.java
+++ src/main/java/lotto/domain/Output.java
@@ -8,11 +8,18 @@ public class Output {
         System.out.println();
         System.out.printf("%d개를 구매했습니다.\n", lottos.size());
         for (Lotto lotto : lottos){
-            System.out.println(lotto);
+            System.out.println(lotto.getNumbers());
         }
     }
 
-    public static void printWinStats() {
+    public static void printWinStats(WinStats winstats) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(winstats.toString());
+    }
 
+    public static void printEarningsRate(Double rate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", rate);
     }
 }

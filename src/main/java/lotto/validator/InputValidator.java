a60ebb24f370f5aa9e5ac361c12fbe2271ed634d
hil679 <hil679922@gmail.com>
2023-11-06 13:39:39 +0900

    Feat(checkInteger): 입력이 정수인지 확인 기능 구현
    - isNotInteger: 정수아니면 true
    - checkInteger: 정수 아니면 예외 throw

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,11 +1,24 @@
 package lotto.validator;
 
 public class InputValidator {
-    private boolean isInteger(String price) {
-        return true;
+    private boolean isNotInteger(String number) {
+        try {
+            Integer.parseInt(number);
+            return false;
+        } catch (NumberFormatException ex) {
+            return true;
+        }
     }
 
-    private void checkInteger() {
-
+    public void checkInteger(String number) {
+        if(isNotInteger(number)) {
+            throw new IllegalArgumentException("[ERROR] 정수를 입력 해야 합니다.");
+        }
     }
+
+    //,로 구분됐는지
+
+    //, 제외 숫자만 입력에 있는지
+
+    // 중복이 있는지
 }

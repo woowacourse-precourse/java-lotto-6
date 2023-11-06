fce3b3c2f51367f7effa08e06906618f42299151
alenjb <alenjb@naver.com>
2023-11-07 06:45:03 +0900

    ✨feat(LottoResultChecker): 보너스 번호를 포함하는지 확인하는 기능 구현
    일치하는 번호들을 리스트로 만들었을 때 보너스 번호를 포함하는지 확인하는 기능을 구현함. 이로 인해 1등과 2등을 구분할 수 있게함.

--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -18,21 +18,27 @@ public class LottoResultChecker {
         this.bonusNumber = bonusNumber;
     }
 
-    public int countMatchNumbers(){
+    public double countMatchNumbers() {
         List<Integer> matchNumbers = winningNumbers.stream()
                 .filter(old -> userLottoNumbers.stream()
                         .anyMatch(Predicate.isEqual(old)))
                 .toList();
+        // 일치하는 개수가 6개인데 보너스 번호가 있는 경우 -> 2등
+        if(matchNumbers.size() == Rank.FIRST_PLACE.getMatchNumberCount() && hasBonusNumber(matchNumbers))
+            return Rank.SECOND_PLACE.getMatchNumberCount();
         return matchNumbers.size();
     }
-    
 
-    public String calculateReward(double matchNumberCount){
-        if(matchNumberCount == Rank.FIRST_PLACE.getMatchNumberCount()) return Rank.FIRST_PLACE.getReward();
-        if(matchNumberCount == Rank.SECOND_PLACE.getMatchNumberCount()) return Rank.SECOND_PLACE.getReward();
-        if(matchNumberCount == Rank.THIRD_PLACE.getMatchNumberCount()) return Rank.THIRD_PLACE.getReward();
-        if(matchNumberCount == Rank.FOURTH_PLACE.getMatchNumberCount()) return Rank.FOURTH_PLACE.getReward();
-        if(matchNumberCount == Rank.FIFTH_PLACE.getMatchNumberCount()) return Rank.FIFTH_PLACE.getReward();
+    public boolean hasBonusNumber(List<Integer> numbers) {
+        return numbers.contains(bonusNumber);
+    }
+
+    public String calculateReward(double matchNumberCount) {
+        if (matchNumberCount == Rank.FIRST_PLACE.getMatchNumberCount()) return Rank.FIRST_PLACE.getReward();
+        if (matchNumberCount == Rank.SECOND_PLACE.getMatchNumberCount()) return Rank.SECOND_PLACE.getReward();
+        if (matchNumberCount == Rank.THIRD_PLACE.getMatchNumberCount()) return Rank.THIRD_PLACE.getReward();
+        if (matchNumberCount == Rank.FOURTH_PLACE.getMatchNumberCount()) return Rank.FOURTH_PLACE.getReward();
+        if (matchNumberCount == Rank.FIFTH_PLACE.getMatchNumberCount()) return Rank.FIFTH_PLACE.getReward();
         return Rank.NONE.getReward(); // 매치되는 숫자 개수가 0인 경우
     }
 

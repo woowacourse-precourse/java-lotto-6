19b9a479317a30dab17c5b422f1aa99b25421aa9
ch1hyun <och2510@gmail.com>
2023-11-06 16:11:40 +0900

    feat: Add Bonus model

---
+++ src/main/java/lotto/model/Bonus.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+public class Bonus {
+    private final Integer bonusNumber;
+
+    public Bonus (Integer bonusNumber) {
+        validateRange(bonusNumber);
+
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateRange(Integer number) {
+        if (number < START_RANGE_OF_NUMBER || END_RANGE_OF_NUMBER < number) { // 상수 enum 추가
+            throw new IllegalArgumentException(); // 에러문 추가
+        }
+    }
+}

7d6d43d0bfbd979a9ed01395ca827abb14b1dcde
Mr.Kim <joajy98@gmail.com>
2023-11-06 23:20:36 +0900

    test: 로또 클래스에 대한 기능 테스트 추가 구현

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import static lotto.domain.Lotto.validateNumberSeparate;
 import static lotto.domain.Lotto.validateNumbersType;
+import static lotto.util.Constant.LOTTO_NUMBERS_COUNT;
+import static lotto.util.LottoGenerator.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
 
@@ -27,15 +30,15 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("로또 번호가 1 이상 45 이하가 아닐 경우에 대한 테스트")
-    void lottoNumberOutOfRangeTest() {
+    @DisplayName("로또 번호가 1 이상 45 이하가 아닐 경우에 대한 예외 테스트")
+    void lottoNumberOutOfRangeExceptionTest() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 47)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("로또 번호가 구분 문자로 제대로 분리되는지 확인하는 테스트")
-    void lottoNumberSeparateTest() {
+    @DisplayName("로또 번호가 구분 문자로 제대로 분리되는지 확인하는 예외 테스트")
+    void lottoNumberSeparateExceptionTest() {
         //given
         String input = "1,2,3,4,5?6";
         String[] numbers = input.split(",");
@@ -48,8 +51,8 @@ class LottoTest {
     }
 
     @Test
-    @DisplayName("로또 번호에 숫자 외의 값이 들어갔는지 확인하는 테스트")
-    void lottoNumberIsNotNumberTest(){
+    @DisplayName("로또 번호에 숫자 외의 값이 들어갔는지 확인하는 예외 테스트")
+    void lottoNumberIsNotNumberExceptionTest() {
         //given
         String input = "o,t,t,f,f,s";
         String[] numbers = input.split(",");
@@ -61,5 +64,28 @@ class LottoTest {
             assertThatThrownBy(() -> validateNumbersType(number))
                     .isInstanceOf(NumberFormatException.class);
         }
+    }
+
+    @Test
+    @DisplayName("로또 번호는 고정적으로 6개임을 확인하는 기능 테스트")
+    void lottoNumberCountTest(){
+        //given
+        Lotto lotto = generateRandomLotto();
+        //when
+
+        //then
+        assertEquals(lotto.getLotto().size(), LOTTO_NUMBERS_COUNT);
+    }
+
+    @Test
+    @DisplayName("로또 수량이 제대로 발행되는지 확인하는 기능 테스트")
+    void lottoAmountCountTest(){
+        //given
+        int purchaseAmount = 5;
+        List<Lotto> lottos = generateRandomLottos(purchaseAmount);
+        //when
+
+        //then
+        assertEquals(lottos.size(), purchaseAmount);
     }
 }

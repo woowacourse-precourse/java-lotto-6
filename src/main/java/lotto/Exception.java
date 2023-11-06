0d195dde9ae5f656105b251a20f9ebea2415c473
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 16:39:14 +0900

    feat(winningNumbers): 보너스 번호가 당첨 번호와 중복될 경우 예외 처리

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,13 +1,19 @@
 package lotto;
 
 public enum Exception {
+    // 구입 금액
     ONLY_DIVISIBLE_BY_THOUSAND("구입 금액은 1000원 단위로 입력해주세요."),
     ONLY_NUMERIC_INPUT_FOR_MONEY("구입금액은 숫자만 입력 가능합니다."),
+
+    // 당첨 번호
     INVALID_WINNING_NUMBERS_INPUT_TYPE("당첨 번호는 숫자만 가능합니다."),
-    INVALID_BONUS_NUMBER_INPUT_TYPE("보너스 번호는 숫자만 가능합니다."),
     WINNING_NUMBERS_SIZE_SHOULD_BE_SIX("당첨 번호는 6개여야 합니다."),
     INVALID_WINNING_NUMBER_RANGE("당첨 번호는 1과 45 사이에 있는 수만 가능합니다."),
     DUPLICATE_WINNING_NUMBER("당첨 번호는 중복될 수 없습니다."),
+
+    // 보너스 번호
+    INVALID_BONUS_NUMBER_INPUT_TYPE("보너스 번호는 숫자만 가능합니다."),
+    DUPLICATE_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     ;
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static lotto.Exception.DUPLICATE_BONUS_NUMBER;
 import static lotto.Exception.DUPLICATE_WINNING_NUMBER;
 import static lotto.Exception.INVALID_WINNING_NUMBER_RANGE;
 import static lotto.Exception.WINNING_NUMBERS_SIZE_SHOULD_BE_SIX;
@@ -11,13 +12,20 @@ public class WinningNumbers {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
+    private final int bonusNumber;
 
-    public WinningNumbers(List<Integer> numbers) {
+    public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+        // numbers
         validateNumberSize(numbers);
         numbers.forEach(this::validateNumberRange);
         validateNumberDuplication(numbers);
 
+        // bonusNumber
+        validateNumberRange(bonusNumber);
+        validateBonusNumberDuplication(numbers, bonusNumber);
+
         this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
     }
 
     private void validateNumberSize(List<Integer> numbers) {
@@ -39,6 +47,12 @@ public class WinningNumbers {
 
         if (numberSize != WINNING_NUMBERS_SIZE) {
             throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBER.getMessage());
+        }
+    }
+
+    private void validateBonusNumberDuplication(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER.getMessage());
         }
     }
 }

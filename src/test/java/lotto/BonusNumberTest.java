c82f39693ff6b111e69098d03d34192b7c3e54b8
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 19:15:11 +0900

    test: BonusNumber에 대한 테스트코드 생성

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -1,19 +1,32 @@
 package lotto;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
 import lotto.domain.BonusNumber;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BonusNumberTest {
     @Test
     public void 보너스_숫자가_범위를_벗어났을_때_테스트() {
         int bonusNumber = 500;
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new BonusNumber(bonusNumber));
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThrows(IllegalArgumentException.class, () -> new BonusNumber(bonusNumber, winningNumbers));
+    }
+
+    @Test
+    public void 보너스_숫자가_당첨번호와_중복되는_경우_테스트() {
+        int bonusNumber = 27;
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 27);
+        assertThrows(IllegalArgumentException.class, () -> new BonusNumber(bonusNumber, winningNumbers));
     }
 
     @Test
     public void 유효한_보너스_숫자_테스트() {
         int bonusNumber = 7;
-        Assertions.assertDoesNotThrow(() -> new BonusNumber(bonusNumber));
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertDoesNotThrow(() -> new BonusNumber(bonusNumber, winningNumbers));
     }
 }

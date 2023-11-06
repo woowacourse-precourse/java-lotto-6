e8291704bba816fa7b6028fe6e1f21cb43b28622
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 11:20:52 +0900

    feat(Bonus) : 보너스 클래스 추가

---
+++ src/main/java/lotto/domain/Bonus.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static lotto.ErrorMessage.BONUS_WRONG_RANGE_ERROR_MESSAGE;
+import static lotto.LottoConstance.MAX_NUMBER_LOTTO_RANGE;
+import static lotto.LottoConstance.MIN_NUMBER_LOTTO_RANGE;
+
+public class Bonus {
+
+    private final int number;
+
+    public Bonus(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (!isBetween1to45(number)) {
+            throwException(BONUS_WRONG_RANGE_ERROR_MESSAGE.get());
+        }
+    }
+
+    private boolean isBetween1to45(int number) {
+        return number >= MIN_NUMBER_LOTTO_RANGE.get() && number <= MAX_NUMBER_LOTTO_RANGE.get();
+    }
+
+    private void throwException(String errorMessage) {
+        throw new IllegalArgumentException(errorMessage);
+    }
+
+
+}

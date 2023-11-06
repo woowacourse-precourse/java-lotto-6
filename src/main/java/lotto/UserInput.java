0ec63ed82f02d783b9921db2562afe26fc0e9c53
K-KY <kyuyoungk@naver.com>
2023-11-07 08:40:11 +0900

    Refactor(UserInput) : 불필요한 메서드 삭제
    문자열을 숫자로 바꿀 때 TypeChanger의 메소드를 사용하도록 변경

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -3,18 +3,13 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
-
+    private static final TypeChanger typeChanger = TypeChanger.getTypeChanger();
 
     public int money() {
         String input = Console.readLine();
-        int money = isNumber(input);
+        int money = typeChanger.from(input);
         isValidMoney(money);
         return money;
-    }
-
-    private int isNumber(String input) {
-        ValidateNumber.isNumber(input);
-        return Integer.parseInt(input);
     }
 
     private void isValidMoney(int money) {

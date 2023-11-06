20e301a3dfb42dfe19b8686ec50decbe7131b0b7
kangyuseok <kingyuseok@naver.com>
2023-11-07 01:42:27 +0900

    Refactor: requestPerchaseAmount 함수 수정
    출력과 입력을 동시에 했지만 예외처리를 위해 출력만 기능하게 함

--- src/main/java/lotto/view/IO.java
+++ src/main/java/lotto/view/IO.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.view.constants.Front;
 
 public class IO {
-    public static String requestPerchaseAmount(Front message) {
+    public static void requestPerchaseAmount(Front message) {
         System.out.println(message.getMessage());
-        return Console.readLine();
     }
+
 }

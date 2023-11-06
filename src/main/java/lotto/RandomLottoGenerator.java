6169f22eaaae0fa5ed42456a395e0b51e9b6a5ff
dukcode <duk9741@gmail.com>
2023-11-06 14:53:55 +0900

    feat: 로또 생성 시 오름차순으로 정렬하도록 구현

--- src/main/java/lotto/RandomLottoGenerator.java
+++ src/main/java/lotto/RandomLottoGenerator.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomLottoGenerator implements LottoGenerator {
     @Override
     public Lotto generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         return new Lotto(numbers);
     }
 }

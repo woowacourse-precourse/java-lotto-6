1f6dc7652bf5cd9388c2ec4bc516a56e3a75fbae
정영록 <s9285s@naver.com>
2023-11-07 00:19:17 +0900

    feat : 당첨 번호와 로또 정덥 개수 비교 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,14 +1,19 @@
 package lotto;
 
+import lotto.LottoGameController.LottoGameController;
 import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -33,4 +38,18 @@ class LottoTest {
         assertThatThrownBy(()-> new Lotto(List.of(1,2,3,4,5,60)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호에 정답 번호 개수가 정확하지 않으면 예외가 발생한다.")
+    @Test
+    void validWinningNumberCount() {
+
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        HashSet<Integer> winningNumber = new HashSet<>();
+        for (int i = 1; i < 7; i++) {
+            winningNumber.add(i);
+        }
+
+        assertThat(lotto.countWinningNumber(winningNumber)).isEqualTo(6);
+    }
+
 }

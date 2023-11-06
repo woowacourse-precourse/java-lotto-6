af4b9b21e2fa14257df63b3b28dbc6a21f2ba856
pumpk <cong2738@gmail.com>
2023-11-06 15:07:44 +0900

    reafactor: 유저 로또 관리 클래스

---
+++ src/main/java/lotto/UserLotto.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class UserLotto {
+    private final List<Lotto> user;
+
+    public UserLotto(int ticketNum) {
+        this.user = setUser(ticketNum);
+    }
+
+    private List<Lotto> setUser(int ticketNum) {
+        System.out.println(String.format("%d개를 구매했습니다.", ticketNum));
+        List<Lotto> res = new ArrayList<>();
+        for (int index = 0; index < ticketNum; index++) {
+            res.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return res;
+    }
+
+    public List<Lotto> getLotto() {
+        return user;
+    }
+
+    public void showUser() {
+        for (Lotto l : user) {
+            System.out.println(l.getNumbers());
+        }
+    }
+}

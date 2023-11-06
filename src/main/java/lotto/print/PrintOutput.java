4528027a54d00b1127b566f062d6a93ec7585a83
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 14:17:42 +0900

    feat: 구입로또 개수 출력
    입력된 개수를 출력하기 위함

--- src/main/java/lotto/print/PrintOutput.java
+++ src/main/java/lotto/print/PrintOutput.java
@@ -4,4 +4,7 @@ public class PrintOutput {
     public void printMoneyQuestion(){
         System.out.println("구입금액을 입력해 주세요.");
     }
+    public void printPurchasedLottoNumber(int num){
+        System.out.println(num+"개를 구매했습니다.");
+    }
 }

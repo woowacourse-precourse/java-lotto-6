513c494a1cf69b718aceeea9f62cc3fff051eeee
JAY-Winter <rubat0@naver.com>
2023-11-06 15:38:41 +0900

    feat: Bonus Number Model

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public class BonusNumber {
+    private final int number;
+
+    public BonusNumber(String inputNumber) {
+        validate(inputNumber);
+        this.number = parseNumberToInt(inputNumber);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    private int parseNumberToInt(String inputNumber) {
+        return Integer.parseInt(inputNumber);
+    }
+
+    private static void validate(String inputNumber) {
+        int number = Integer.parseInt(inputNumber);
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 숫자는 1에서 45 사이여야 합니다.");
+        }
+    }
+}

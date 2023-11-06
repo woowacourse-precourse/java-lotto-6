91db226b7cca72c02bdd967b04d9396a91e6d689
JW0321 <digneotn123@gmail.com>
2023-11-06 18:03:52 +0900

    feat: 구매 금액 입력 기능 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoView { //게임 시작 메세지 출력, 구입 금액, 당첨 번호, 보너스 번호 입력 요청, 당첨 통계 및 결과 메세지 출력, 에러 메세지 출력
 
     private static final String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
@@ -10,5 +12,10 @@ public class LottoView { //게임 시작 메세지 출력, 구입 금액, 당첨
     private static final String SEPARATOR = "---";
     private static final String COUNT_PRIZE_FORMAT = "%d개 일치 (%s원) - %d개";
     private static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    public static String readPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_PROMPT);
+        return Console.readLine();
+    }
 
 }

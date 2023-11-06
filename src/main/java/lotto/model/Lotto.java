0ed7c314171cd8877f25dace5c2827cdeafb8d61
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 18:41:53 +0900

    feat: lotto 클래스 추가 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,10 +3,11 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
 
+    private final List<Integer> numbers;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverlap(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +17,19 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateOverlap(List<Integer> numbers) {
+        long uniqueNumbersCount = numbers.stream().distinct().count();
+        if (uniqueNumbersCount < numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

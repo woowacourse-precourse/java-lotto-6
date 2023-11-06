589d39b647d52e47abdad2d6b0b322caa9ac2588
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:34:28 +0900

    feat: PlayerLotto 클래스 생성자 추가

--- src/main/java/lotto/domain/PlayerLotto.java
+++ src/main/java/lotto/domain/PlayerLotto.java
@@ -5,4 +5,8 @@ public class PlayerLotto {
     private Lotto lottoNubmers;
     private BonusNumber bonusNumber;
 
+    public PlayerLotto(Lotto lottoNubmers, BonusNumber bonusNumber) {
+        this.lottoNubmers = lottoNubmers;
+        this.bonusNumber = bonusNumber;
+    }
 }

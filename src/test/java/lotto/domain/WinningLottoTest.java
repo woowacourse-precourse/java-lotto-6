629d6439181626b788eb0fbc5ab6cc30908f6f91
maark1106 <mark1106@naver.com>
2023-11-06 20:15:14 +0900

    test : 보너스 숫자 범위 벗어날 때 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -19,4 +19,13 @@ class WinningLottoTest {
                 .hasMessageContaining("[ERROR] : 보너스 숫자는 당첨 번호와 중복될 수 없습니다");
     }
 
+    @DisplayName("보너스 숫자 범위가 1~45 벗어나면 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    public void outOfBoundsBonusNumberTest(int bonusNumber) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] : 숫자가 1에서 45 사이의 범위를 벗어납니다.");
+    }
 }

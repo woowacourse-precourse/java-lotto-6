c7a8f119ef4d016fd5f0f24de9d14a4376ee961d
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 03:47:45 +0900

    test(OutputView): 당첨 통계 출력 기능 테스트 코드 구현

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,4 +1,50 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.view;
+
+import lotto.domain.Cash;
+import lotto.domain.Profit;
+import lotto.domain.Rank;
+import lotto.domain.WinningYield;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 class OutputViewTest {
-  
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void test() {
+        // given
+        List<Rank> ranks = new ArrayList<>();
+        ranks.add(Rank.FIFTH);
+        ranks.add(Rank.FOURTH);
+        ranks.add(Rank.FIFTH);
+        Cash cash = new Cash(8000);
+        Profit profit = new Profit();
+        profit.calculateAmount(ranks);
+        WinningYield winningYield = new WinningYield(cash, profit);
+
+        // when
+        OutputView.displayWinningDetails(ranks, winningYield);
+
+        // then
+        String capturedOutput = outputStreamCaptor.toString().trim();
+        Assertions.assertThat(capturedOutput).isEqualTo("당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 2개\n" +
+                "4개 일치 (50,000원) - 1개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n" +
+                "총 수익률은 750.0%입니다.");
+    }
 }

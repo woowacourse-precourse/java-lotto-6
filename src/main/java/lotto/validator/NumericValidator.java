98251af0d387320ff015c82838502f7d97c606f2
LEE-sh1673 <lsh901673@gmail.com>
2023-11-07 00:35:01 +0900

    refactor: 예외 상황 문구 추가

--- src/main/java/lotto/validator/NumericValidator.java
+++ src/main/java/lotto/validator/NumericValidator.java
@@ -4,11 +4,14 @@ import java.util.regex.Pattern;
 
 public class NumericValidator {
 
+    private static final String INVALID_NUMBER_FORMAT
+            = "[ERROR] 숫자 형식이 올바르지 않습니다.";
+
     private static final Pattern NUMERIC_PATTERN = Pattern.compile("\\d+");
 
     public static void validate(final String stringNumber) {
         if (isNullOrEmpty(stringNumber) || !isNumeric(stringNumber)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT);
         }
     }
 

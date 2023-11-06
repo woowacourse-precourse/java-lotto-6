407394dd2fd063e0edbdbcf03e50f1b4d5b4b3e5
dmong <dt2980_hee@naver.com>
2023-11-06 13:55:16 +0900

    feat: InputView에 사용자 입력 받기, 입력 관련 메시지 출력 로직 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,26 @@
 package lotto.view;
 
+import static lotto.view.MessageConstants.INPUT_MESSAGE_FOR_BONUS_NUMBER;
+import static lotto.view.MessageConstants.INPUT_MESSAGE_FOR_BUY_AMOUNT;
+import static lotto.view.MessageConstants.INPUT_MESSAGE_FOR_WINNING_NUMBERS;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String getUserInput() {
+        return Console.readLine();
+    }
+
+    public void printBuyAmountInput() {
+        System.out.println(INPUT_MESSAGE_FOR_BUY_AMOUNT);
+    }
+
+    public void printWinningNumbersInput() {
+        System.out.println(INPUT_MESSAGE_FOR_WINNING_NUMBERS);
+    }
+
+    public void printBonusNumberInput() {
+        System.out.println(INPUT_MESSAGE_FOR_BONUS_NUMBER);
+    }
 }

c7a81dedb38658f11547449ed0e28a185bd9f337
unknown <hakie2kim@gmail.com>
2023-11-06 21:03:47 +0900

    refactor(src/main/java/lotto/): replace the existing description using instance variables

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,21 +1,35 @@
 package lotto;
 
+import java.text.DecimalFormat;
+
 public enum Rank {
-    FIRST(1, 2_000_000_000L, "6개 일치 (2,000,000,000원)"),
-    SECOND(2, 30_000_000L, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(3, 1_500_000L, "5개 일치 (1,500,000원)"),
-    FOURTH(4, 50_000L, "4개 일치 (50,000원)"),
-    FIFTH(5, 5_000L, "3개 일치 (5,000원)"),
-    OTHERS(0, 0L, ""); // 나머지 등수
+    FIRST(1, 2_000_000_000L, 6, false),
+    SECOND(2, 30_000_000L, 5, true),
+    THIRD(3, 1_500_000L, 5, false),
+    FOURTH(4, 50_000L, 4, false),
+    FIFTH(5, 5_000L, 3, false),
+    OTHERS(0, 0L, 0, false); // 나머지 등수
 
     private final int number;
     private final long prizeAmount;
+    private final int numberOfMatching;
+    private final boolean isBonusNumberMatching;
     private final String description;
 
-    private Rank(int rank, long prizeAmount, String description) {
+    private Rank(int rank, long prizeAmount, int numberOfMatching, boolean isBonusNumberMatching) {
         this.number = rank;
         this.prizeAmount = prizeAmount;
-        this.description = description;
+        this.numberOfMatching = numberOfMatching;
+
+        String bonusMatchingDescription = "";
+        this.isBonusNumberMatching = isBonusNumberMatching;
+        if (isBonusNumberMatching) {
+            bonusMatchingDescription = ", 보너스 볼 일치";
+        }
+
+        String commaSeparatedPrizeAmount = new DecimalFormat("#,###").format(prizeAmount);
+
+        this.description = String.format("%d개 일치%s (%s원)", numberOfMatching, bonusMatchingDescription, commaSeparatedPrizeAmount);
     }
 
     public static Rank of(int number) {
@@ -25,7 +39,7 @@ public enum Rank {
 
     private static void validate(int number) {
         if (number < 0 || number > 5) {
-            throw new IllegalArgumentException("[ERROR] 순위는 1등부터 4등이어야 합니다.");
+            throw new IllegalArgumentException("순위는 1등부터 4등이어야 합니다.");
         }
     }
 

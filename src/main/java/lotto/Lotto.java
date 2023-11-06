eca5a245346431a983390fd56fee4ca5ab2a77cd
nill <wwaosldk@gmail.com>
2023-11-06 18:23:25 +0900

    feat: lotto number validate check

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> deduplicationNumbers = new HashSet<>(numbers);
+
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        if (deduplicationNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (numbers.stream().allMatch(v -> v >= 1 && v <= 45) != true) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
 }

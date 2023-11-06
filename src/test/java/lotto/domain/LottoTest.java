632249e6d1896048d6a6d1c96b5b063960c83ce4
yubin <dbqls2821@gmail.com>
2023-11-06 19:07:31 +0900

    test: Lotto 생성시 여섯자리 숫자를 입력하지 않은 경우 예외를 던지는 테스트 코드 추가

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoTest {
+
+    @Test
+    void 여섯자리_숫자를_입력하지_않은_경우_예외를_던진다() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DomainException.ERROR.getMessage());
+    }
+}

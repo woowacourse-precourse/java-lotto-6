2700538622d8aea016cd81d40d1257e557929da5
King's meow <mingee641@gmail.com>
2023-11-06 17:29:57 +0900

    refactor: 예외처리를 위한 입력 메서드 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,18 +2,32 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-
+import lotto.model.User;
+import lotto.service.LottoService;
 import lotto.view.PrintView;
 
 
 public class LottoController {
+
+    LottoService lottoService;
+
+
+    public LottoController() {
+        this.lottoService = new LottoService();
+    }
+
+
 
 
     public int getInputMoney() {
         while (true) {
             PrintView.inputMoneyView();
             String inputMoney = Console.readLine();
-
+            try {
+                return lottoService.initMoneyToCount(inputMoney);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
@@ -21,7 +35,11 @@ public class LottoController {
         while (true) {
             PrintView.inputWinningNumbersView();
             String inputWinningNumbers = Console.readLine();
-
+            try {
+                return lottoService.initWinningNumbers(inputWinningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
@@ -29,7 +47,11 @@ public class LottoController {
         while (true) {
             PrintView.inputBonusNumberView();
             String inputBonusNumber = Console.readLine();
-
+            try {
+                return lottoService.initBonusNumber(inputBonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 

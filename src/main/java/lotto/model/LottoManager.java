841b0b9bd4c1b08e6b851cfef074ad0efd3fe707
Handwoong <handwoong@gmail.com>
2023-11-06 17:15:58 +0900

    feat(lotto-manager): 금액을 전달받아 금액에 맞게 로또 생성 구현

---
+++ src/main/java/lotto/model/LottoManager.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoManager {
+    public static final int AMOUNT_UNIT = 1000;
+    private final LottoMachine lottoMachine;
+
+    public LottoManager(final LottoMachine lottoMachine) {
+        this.lottoMachine = lottoMachine;
+    }
+
+    public List<Lotto> createLottoTickets(final int amount) {
+        final int purchaseQuantity = amount / AMOUNT_UNIT;
+        return lottoMachine.createLotto(purchaseQuantity);
+    }
+}

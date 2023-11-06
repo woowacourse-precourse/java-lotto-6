5e1df00b9ee9ef9b5ba1f3cc52bb625d5f0244c9
Jokyo0612 <jgh010612@gmail.com>
2023-11-06 17:32:45 +0900

    feat : 초기입력 값이 0이하일 경우 예외처리 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,18 +5,26 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 class money_for_lotto{
-    public static String inputNumber;
+    public static int realNumber;
 
     public static int realMoney(String inputNumber){
         moneyIsNumber(inputNumber);
+        realNumber = Integer.parseInt(inputNumber);
+        moneyIsOver0(realNumber);
         return Integer.parseInt(inputNumber);
     }
 
-    public static void  moneyIsNumber(String inputNumber){
+    public static void moneyIsNumber(String inputNumber){
         try{
             Integer.parseInt(inputNumber);
         }catch(NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
+        }
+    }
+
+    public static void moneyIsOver0(int realNumber){
+        if(realNumber<1){
+            throw new IllegalArgumentException("[ERROR] 0보다 큰 수를 입력하세요");
         }
     }
 

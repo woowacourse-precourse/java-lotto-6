bca47f3dca53a531b608e0c912b9dd458a431f8f
JW0321 <digneotn123@gmail.com>
2023-11-06 23:44:33 +0900

    test: 보너스 번호 설정 및 검증 테스트

--- src/test/java/lotto/model/LottoMachineTest.java
+++ src/test/java/lotto/model/LottoMachineTest.java
@@ -6,28 +6,66 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoMachineTest {
 
-    @DisplayName("구매 횟수만큼 로또 번호 조합 발행")
+    private LottoMachine lottoMachine;
+
+    @BeforeEach
+    void setUp(){
+        lottoMachine = new LottoMachine();
+    }
+
+    @DisplayName("구매 횟수만큼 로또 번호 조합 발행 기능 테스트")
     @Test
     void createLottoByPurchaseCount() {
         int purchaseCount = 5;
-        LottoMachine lottoMachine = new LottoMachine();
 
         ArrayList<Lotto> lottoTickets = lottoMachine.runLottoMachine(purchaseCount);
 
         assertThat(lottoTickets.size()).isEqualTo(purchaseCount);
     }
 
-    @DisplayName("당첨 번호 설정")
+    @DisplayName("당첨 번호 설정 기능 테스트")
     @Test
     void testSetWinningNumbers() {
-        LottoMachine lottoMachine = new LottoMachine();
         lottoMachine.setWinningNumbers(List.of(1,2,3,4,5,6));
 
         assertThat(lottoMachine.getWinningNumbers()).isEqualTo(List.of(1,2,3,4,5,6));
+    }
+
+    @DisplayName("보너스 번호 설정 기능 테스트")
+    @Test
+    void testSetBonusNumber() {
+        int bonusNumber = 10;
+
+        lottoMachine.setBonusNumber(bonusNumber);
+
+        assertThat(lottoMachine.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+    @DisplayName("보너스 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByOutRange() {
+        int bonusNumber = 80;
+
+        assertThatThrownBy(() -> lottoMachine.setBonusNumber(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createBonusByDuplicatedNumber() {
+        int bonusNumber = 5;
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        lottoMachine.setWinningNumbers(winningNumbers);
+
+        assertThatThrownBy(() -> lottoMachine.setBonusNumber(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }

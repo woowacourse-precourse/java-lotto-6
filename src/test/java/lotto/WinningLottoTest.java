784da4d16197dad3e1ff9bf20af9b4266149e5c9
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-06 19:03:49 +0900

    test(WinningLottoTest): 당첨번호 생성 테스트의 오류 메시지 변경

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -11,14 +11,14 @@ import org.junit.jupiter.api.Test;
 
 public class WinningLottoTest {
 
-    @DisplayName("당첨번호가 숫자와 쉼표로만 구성되어 있는지 테스트")
+    @DisplayName("당첨번호가 올바르게 생성되는지 테스트")
     @Test
-    void validateWinningLotto1() {
+    void createWinningLotto() {
         String invalidInput = "1,2,3,4,5, 30";
 
         Assertions.assertThatThrownBy(() -> new WinningLotto(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 숫자와 쉼표로만 당첨 번호를 입력해 주세요.");
+                .hasMessage("[ERROR] 예시처럼 입력해주세요. 예시) 1,2,3,4,5,6");
     }
 
     @DisplayName("당첨번호가 쉼표로 끝나지 않는지 테스트")
@@ -33,20 +33,11 @@ public class WinningLottoTest {
 
     @DisplayName("당첨번호에 보너스 번호가 있으면 예외 테스트")
     @Test
-    void compare() {
+    void contain() {
         WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6");
 
         Assertions.assertThatThrownBy(() -> winningLotto.contain(new Bonus("5")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다.");
-    }
-
-    @DisplayName("당첨번호에 쉼표가 있는지 테스트")
-    @Test
-    void compare2() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6");
-
-        Assertions.assertThatThrownBy(() -> winningLotto.contain(new Bonus("5")))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }

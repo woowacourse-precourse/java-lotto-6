edcfa839a6c7ede0e71ba6b4d79f2e65482fd07e
jimmy0006 <jinmin645@gmail.com>
2023-11-06 18:03:10 +0900

    feat:게임의 진행을 위해 game을 생성하고 진행하는 로직 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구매금액을 입력해 주세요.");
+        String buyMoney = readLine();
+        System.out.println();
+        Game game = new Game(Integer.parseInt(buyMoney));
+        ArrayList<Integer> test = new ArrayList<>();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String numbers = readLine();
+        String[] split = numbers.split(",");
+        for (String s:split) {
+            test.add(Integer.valueOf(s));
+        }
+        System.out.println();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = readLine();
+        System.out.println();
+        game.printResult(test,Integer.parseInt(bonusNumber));
     }
 }

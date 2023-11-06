4933c4f83806bffcf23d62778c807041fc1bafa7
shining013 <nevertheless113@naver.com>
2023-11-06 18:57:55 +0900

    feat: generateLotto method 생성

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -15,6 +18,17 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
+
+    private List<List<Integer>> generateLotto(int quantity) {
+        List<List<Integer>> lottos = new ArrayList<>();
+
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(randomNumbers);
+        }
+        return lottos;
+    }
+
 
     // TODO: 추가 기능 구현
 }

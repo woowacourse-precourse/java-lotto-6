db050190bc91c36dcdb2e5a95bfb939e391b8e9b
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 19:10:37 +0900

    refactor: calculateTotalPrize() 리팩토링 calculateTotalPrize()에서 잘못된 값을 반환하여 올바르게 리팩토링하였음

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import java.util.Map;
+
 public class Calculator {
-    public static int calculateTotalPrize() {
+    public static int calculateTotalPrize(Map<Rank, Integer> rankCount) {
         int totalPrize = 0;
         for (Rank rank : Rank.values()) {
-            totalPrize += rank.getPrize() * rank.getCountOfWin();
+            totalPrize += rank.getPrize() * rankCount.getOrDefault(rank, 0);
         }
         return totalPrize;
     }
@@ -12,5 +14,4 @@ public class Calculator {
     public static double calculateEarningRate(int totalPrize, Money money) {
         return ((double) totalPrize / money.getMoney()) * 100;
     }
-
 }

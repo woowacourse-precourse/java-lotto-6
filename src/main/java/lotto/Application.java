adf039413079940f1cb710ce51ff4337676a4d6d
Chan Mi <rkdcksal02@gmail.com>
2023-11-07 01:29:22 +0900

    Application 클래스 - 프로그램의 진입점을 설정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+import lotto.domain.WinningResult;
+import lotto.service.LottoMachine;
+import lotto.view.InputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(new LottoMachine(), new InputView());
+
+        int purchaseAmount = lottoController.inputPurchaseAmount();
+        List<Lotto> lottoTickets = lottoController.generateLottoTickets(purchaseAmount);
+
+        List<Integer> winningNumbers = lottoController.inputWinningNumbers();
+        int bonusNumber = lottoController.inputBonusNumber();
+
+        WinningResult winningResult = lottoController.calculateWinningResult(lottoTickets, winningNumbers, bonusNumber);
+
+        lottoController.showResult(lottoTickets, winningResult);
     }
 }

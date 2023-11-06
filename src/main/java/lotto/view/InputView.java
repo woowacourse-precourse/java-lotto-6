5300d52704b6b99995467a8322262cdb4d4178e4
현 <sohyun95277@gmail.com>
2023-11-06 23:45:57 +0900

    fix(InputView): 에러 발생 시 재입력을 받도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,14 +3,35 @@ package lotto.view;
 import static lotto.constant.message.ErrorMessage.BLANK_LINE;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.util.Converter;
 
 public class InputView {
-    public String requestInputValue(String message) {
-        System.out.println(message);
-        String input = Console.readLine();
 
-        validateEmptyLine(input);
-        return input;
+    public int requestNumberInput(String message) {
+        while (true) {
+            try {
+                System.out.println(message);
+                String input = Console.readLine();
+                validateEmptyLine(input);
+                return Converter.convertInteger(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> requestListInput(String message) {
+        while (true) {
+            try {
+                System.out.println(message);
+                String input = Console.readLine();
+                validateEmptyLine(input);
+                return Converter.convertIntegerList(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private void validateEmptyLine(String input) {
@@ -18,5 +39,6 @@ public class InputView {
             throw new IllegalArgumentException(BLANK_LINE.getMessage());
         }
     }
+
 
 }

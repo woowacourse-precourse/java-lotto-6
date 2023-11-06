0345e703bf25ae34df19e5a6292b220ecec044b0
Yerinnnn <shinyerin0216@gmail.com>
2023-11-07 00:19:52 +0900

    Feat: 로또 번호와 사용자가 입력한 당첨 번호를 비교한 후 등수를 매김

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private LottoRank rank;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -16,5 +17,35 @@ public class Lotto {
         }
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public LottoRank getRank() {
+        return rank;
+    }
+
+    public void setRank(LottoRank rank) {
+        this.rank = rank;
+    }
+
     // TODO: 추가 기능 구현
+
+    public enum LottoRank {
+
+        FIRST, SECOND, THIRD, FOURTH, FIFTH, LOOSE
+
+
+//        FIRST("6개 일치 (2,000,000,000원) - "),
+//        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+//        THIRD("5개 일치 (1,500,000원) - "),
+//        FOURTH("4개 일치 (50,000원) - "),
+//        FIFTH("3개 일치 (5,000원) - "),
+//        LOOSE("");
+//        final private String rank;
+//
+//        LottoRank(String rank) {
+//            this.rank = rank;
+//        }
+    }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,28 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 import lotto.util.Validator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Collections.sort;
+import static lotto.Lotto.LottoRank.*;
 
 public class LottoController {
+
+    private List<Lotto> lotto = new ArrayList<>();
+    private Lotto winLotto;
+    private int bonusNumber;
+
+    public int first;
+    public int second;
+    public int third;
+    public int fourth;
+    public int fifth;
 
     public void startLotto() {
         OutputView.printGetPurchasePriceMessage();
@@ -19,7 +32,9 @@ public class LottoController {
         OutputView.printLottoNumMessage(lottoNum);
 
         for(int i = 0; i < lottoNum ; i++) {
-            OutputView.printLottoNumber(getLottoNumber());
+            List<Integer> lottoNumber = getLottoNumber();
+            OutputView.printLottoNumber(lottoNumber);
+            lotto.add(new Lotto(lottoNumber));
         }
 
         System.out.println();
@@ -28,7 +43,24 @@ public class LottoController {
         getWinLottoNumber();
 
         OutputView.printBonusNumberMessage();
-        getBonusNumber();
+        bonusNumber = getBonusNumber();
+
+        for (int i = 0; i < lottoNum; i++) {
+            List<Integer> lottoNumbers = lotto.get(i).getNumbers();
+            List<Integer> winNumbers = winLotto.getNumbers();
+            int sameNumberCount = compareLotto(lottoNumbers, winNumbers);
+//            System.out.println("다시 한 번!!! 갯수는!!! " + sameNumberCount);
+//            System.out.println("어떤 로또라고??? " + lotto.get(i));
+            setRank(lotto.get(i), sameNumberCount);
+//            System.out.println("그렇다면... 이 로또의 등수는!!! " + lotto.get(i).getRank());
+        }
+
+        /**
+         * 로또 배열
+         * 그 중에 한 로또를 뽑는 걸 반복하는 함수
+         * ture인 숫자의 갯수를 카운트
+         * 뽑은 로또의 숫자들을 하나씩 win로또 배열에 비교하는 함수 -> true 리턴
+         */
 
 
     }
@@ -44,27 +76,25 @@ public class LottoController {
         return lottoNumber;
     }
 
-    public List<Integer> getWinLottoNumber() {
+    public void getWinLottoNumber() {
         List<Integer> winNumber = InputView.inputWinNumber();
 
         try {
-            Validator.winNumberNum(winNumber);
+            winLotto = new Lotto(winNumber);
         } catch (IllegalArgumentException e) {
             System.out.println("당첨 번호를 6개 입력해주세요.");
             getWinLottoNumber();
         }
 
-        try {
-            Validator.winNumberDuplication(winNumber);
-        } catch (IllegalArgumentException e) {
-            System.out.println("당첨 번호를 중복 없이 입력해주세요.");
-            getWinLottoNumber();
-        }
-
-        return winNumber;
+//        try {
+//            Validator.winNumberDuplication(winNumber);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("당첨 번호를 중복 없이 입력해주세요.");
+//            getWinLottoNumber();
+//        }
     }
 
-    public void getBonusNumber() {
+    public int getBonusNumber() {
         int bonusNumber = InputView.inputBonusNumber();
 
         try {
@@ -72,6 +102,72 @@ public class LottoController {
         } catch (IllegalArgumentException e) {
             System.out.println("1개만 입력하세요.");
             getLottoNumber();
+        }
+
+        return bonusNumber;
+    }
+
+    public int compareLotto(List<Integer> lottoNumber, List<Integer> winNumber) {
+
+        int count = 0;
+
+        for(int i = 0; i < lottoNumber.size(); i++) {
+            if (winNumber.contains(lottoNumber.get(i))) {
+                count += 1;
+            }
+        }
+
+//        System.out.println(lottoNumber + " 인 로또의 같은 번호 갯수는: " + count);
+
+        return count;
+
+//        for (int i = 0; i < lottoNumber.size() < i++) {
+//            winNumber.contains(lottoNumber.get(i));
+//        }
+//        lottoNumber.forEach(num -> winNumber.contains(num));
+
+    }
+
+    public boolean compareBonusNumber(Lotto lotto, int bonusNumber) {
+
+        List<Integer> lottoNumber = lotto.getNumbers();
+
+        if (lottoNumber.contains(bonusNumber)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void setRank(Lotto lotto, int sameNumberCount) {
+//        System.out.println("갯수가 잘 들어갔나??? " + sameNumberCount);
+        switch (sameNumberCount) {
+            case 6:
+                lotto.setRank(FIRST);
+                break;
+            case 5:
+//                System.out.println("switch가 잘 되나??? " + sameNumberCount);
+                if (compareBonusNumber(lotto, bonusNumber)) {
+                    lotto.setRank(SECOND);
+                    break;
+                }
+                lotto.setRank(THIRD);
+                break;
+            case 4:
+                lotto.setRank(THIRD);
+                break;
+            case 3:
+                lotto.setRank(FOURTH);
+                break;
+            case 2:
+                lotto.setRank(FIFTH);
+                break;
+            case 1:
+                lotto.setRank(LOOSE);
+                break;
+            case 0:
+                lotto.setRank(LOOSE);
+                break;
         }
     }
 }

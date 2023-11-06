f101a13717650569443638aad7ec54989b699a16
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-06 15:13:32 +0900

    feat: 보너스 번호 null값 또는 정수 외값 입력시 예외처리

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -3,10 +3,25 @@ package lotto.domain;
 import lotto.utils.Parser;
 
 public class BonusNumber {
-    int bonusNumber;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+    private static final String OUT_RANGE_ERROR_MESSAGE = "[ERROR] 범위를 벗어난 숫자 입니다.";
+    private static final String NULL_INPUT_ERROR_MESSAGE = "[ERROR] 금액을 정확히 입력해 주세요.";
 
-    public BonusNumber(String userInput){
+    private int bonusNumber;
+
+    public BonusNumber(String userInput) {
         this.bonusNumber = Parser.parseStringToInt(userInput);
+    }
+
+    void validInRangeNumber() {
+        if (bonusNumber< MIN_RANGE || bonusNumber > MAX_RANGE) {
+            throw new IllegalArgumentException(OUT_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    void validateNullValue(String value) {
+        if (value.isBlank()) throw new IllegalArgumentException(NULL_INPUT_ERROR_MESSAGE);
     }
 
     public int getBonusNumber() {
--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -4,12 +4,13 @@ import lotto.domain.PurchaseAmount;
 
 public class Parser {
     static final int cost = 1000;
+    private static final String NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해 주세요.";
     public static int parseStringToInt(String stringValue) {
         try {
             int intValue = Integer.parseInt(stringValue);
             return intValue;
         } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 정수값을 입력해 주세요.");
+            throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
         }
     }
 

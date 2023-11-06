b0cb1cbe9553000c60466fe257e87b3761dac149
34suuuuu <psh7492@gmail.com>
2023-11-06 12:22:58 +0900

    feat: 로또 번호 범위 관련 예외 처리 기능 추가

--- src/main/java/lotto/exception/LottoNumRangeException.java
+++ src/main/java/lotto/exception/LottoNumRangeException.java
@@ -1,2 +1,10 @@
-package lotto.exception;public class LottoNumRangeException {
+package lotto.exception;
+
+public class LottoNumRangeException  extends  IllegalArgumentException{
+    private static final String ERROR_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    public LottoNumRangeException(){
+        super(ERROR_MESSAGE);
+    }
 }
+

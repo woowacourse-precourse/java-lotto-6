d405818deea9b7ddb0dfdcf5572b02c0b2157af5
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-06 13:09:48 +0900

    feat(Rank): 로또 등수별 정보관리를 위한 enum을 이용한 기능 추가
    각 등수별 상금, 일치하는 개수등 고유 정보를 엄격하게 관리하기 위하여enum으로 클래스를 만들었습니다.

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,32 @@
+package lotto;
+
+public enum Rank {
+
+    FIRST(6, 2000000000L, "2,000,000,000"),
+    SECOND(5, 30000000L, "30,000,000"),
+    THIRD(5, 1500000L, "1,500,000"),
+    FOURTH(4, 50000L, "50,000"),
+    FIFTH(3, 5000L, "5,000");
+
+    private final int count;
+    private final long prizeMoney;
+    private final String printablePrizeMoney;
+
+    private Rank(int count, long prizeMoney, String printablePrizeMoney) {
+        this.count = count;
+        this.prizeMoney = prizeMoney;
+        this.printablePrizeMoney = printablePrizeMoney;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public String getPrintablePrizeMoney() {
+        return printablePrizeMoney;
+    }
+}

984ae4c3520461fdf7e4b6d2e046babeb4605303
jongmee <ra123221@gmail.com>
2023-11-06 23:01:50 +0900

    feat(OutputView): 로또 당첨 결과 출력하기

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,16 +4,26 @@ import java.util.List;
 import lotto.domain.IssuedLottos;
 import lotto.utility.io.Writer;
 import lotto.utility.vo.LottoResponse;
+import lotto.utility.vo.WinningResponse;
 
 public class OutputView {
     private static final String NUMBER_OF_LOTTOS_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_MESSAGE = "\n당첨 통계\n---";
+
     public void informIssuedLottos(List<LottoResponse> lottos) {
+        Writer.println(" ");
         Writer.print(lottos.size());
         Writer.println(NUMBER_OF_LOTTOS_MESSAGE);
-        Writer.println(" ");
 
         lottos.stream()
             .map(LottoResponse::createMessage)
+            .forEach(Writer::println);
+    }
+
+    public void informWinningResult(WinningResponse winningResponse) {
+        Writer.println(WINNING_MESSAGE);
+        winningResponse.createdMessage()
+            .stream()
             .forEach(Writer::println);
     }
 }

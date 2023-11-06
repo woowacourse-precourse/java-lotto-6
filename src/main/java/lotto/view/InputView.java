78d1b13a8a88e699a808472d618acd9dae41f36c
Kimhyunjin4455 <dp4455@naver.com>
2023-11-06 18:24:43 +0900

    feat: 입력 문구 출력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class InputView {
+    public static void requestPurchaseAmountMessage() {
+        System.out.println("구매금액을 입력해 주세요.");
+    }
+    public static void requestWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public static void requestBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class OutputView {
+    public static void purchaseLottoNumber(){
+        System.out.println("{}개를 구매했습니다.");
+    }
+    public static void lottoPrinter(){
+        // 랜덤값 이용해 로또 출력(배열 형식)
+    }
+
+    public static void winningStatistics(){
+        // 당첨 통계 + 총 수익률 출력
+    }
+}

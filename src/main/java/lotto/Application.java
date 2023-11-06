a0893d642f4fdf233aeea3778d0ad5189eb0125c
Seo Young, Ha <59078585+82Hasheng@users.noreply.github.com>
2023-11-07 05:18:46 +0900

    feat: 발행한 로또 개수 계산 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,5 +8,9 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = Integer.parseInt(Console.readLine());
         /* 추가할 것 : 입력한 구입금액이 1000원으로 나누어 떨어지지 않을 경우 예외처리한다. */
+
+        System.out.print("발행한 로또 수량 : ");
+        int lottoTickets = purchaseAmount / 1000;
+        System.out.println(lottoTickets);
     }
 }

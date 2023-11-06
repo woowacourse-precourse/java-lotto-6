4f31003c67222a2cfcfbd307883b22b6b8dd4a13
sunghun <nokdonwithkj211@gmail.com>
2023-11-06 10:18:58 +0900

    refactor(LottoApplication): Register() 는 특정 타입 객체를 반환하는 것으로 수정

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,33 +2,36 @@ package lotto;
 
 import lotto.controller.RegisterLottoController;
 import lotto.controller.RegisterUserMoneyController;
-import lotto.io.ConsoleReader;
-import lotto.io.ConsoleWriter;
+import lotto.domain.Lotto;
+import lotto.domain.UserMoney;
 import lotto.repository.LottoRepository;
 import lotto.view.LottoScreen;
 
 public class LottoApplication {
     private final LottoScreen lottoScreen;
+    private final LottoRepository lottoRepository;
 
-    public LottoApplication(LottoScreen lottoScreen) {
+    public LottoApplication(LottoScreen lottoScreen, LottoRepository repository) {
         this.lottoScreen = lottoScreen;
+        this.lottoRepository = repository;
     }
 
     void run() {
         // COntroller를 호출한다
         // 각각의 Controller 내에서 할일을 진행한다 (데이터의 교환을목적으로 진행한다
         // 단 핵심 로직은 Service 단에서 실행하는 방향으로
-        registerUserMoney();
-        registerUserLotto();
+        UserMoney userMoney = registerUserMoney();
+        Lotto lotto = registerUserLotto();
+
 
     }
 
-    private void registerUserMoney() {
-        new RegisterUserMoneyController(new LottoRepository(), lottoScreen).process();
+    private UserMoney registerUserMoney() {
+        return new RegisterUserMoneyController(lottoRepository, lottoScreen).process();
     }
 
-    private void registerUserLotto() {
-        new RegisterLottoController(lottoScreen).process();
+    private Lotto registerUserLotto() {
+        return new RegisterLottoController(lottoScreen).process();
     }
 
 

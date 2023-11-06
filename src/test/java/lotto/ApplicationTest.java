15a8f04dd8b636b97c76568e1625e0d9bb5f8099
ohhyeonn <ohhyeon120@gmail.com>
2023-11-06 18:04:38 +0900

    feat : add test 보너스번호 숫자 체크

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -23,6 +23,16 @@ class ApplicationTest extends NsTest {
         Prize.FOUR.setCount(0);
         Prize.THREE.setCount(0);
     }
+
+
+    @Test
+    void 보너스번호_숫자_체크(){
+        assertSimpleTest(() -> {
+            runException("1000" , "1,2,3,4,5,6" , "a");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+
+    }
     @Test
     void 보너스번호_중복_체크(){
         assertSimpleTest(() -> {

f8d38c0dcdab6eddc1b8d805685405648223e0d3
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 16:09:00 +0900

    test: 보너스 번호 테스트 코드 추가

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class BonusNumberTest {
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호가 1부터 45사이의 숫자가 아니라면 예외 발생")
+    @ValueSource(ints = {66})
+    void bonusNumberOutOfRange(int bonusNumber) {
+        Assertions.assertThatThrownBy(() -> new BonusNumber().validate(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

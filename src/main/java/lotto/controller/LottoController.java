13b166408ec7daa46d3dd2c6def8de29b60ef245
jhsseonn <hs125bt@gmail.com>
2023-11-06 20:34:26 +0900

    feat: 사용자가 구입한 로또 장 수 출력하기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,18 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.service.LottoService;
 import lotto.view.LottoView;
 
 public class LottoController {
 
     private LottoView lottoView = new LottoView();
+    private LottoService lottoService = new LottoService();
 
     public void runLotto(){
         String lottoPurchase = lottoView.inputLottoPurchase();
+        int lottoPurchaseReturn = lottoService.getLottoCount(lottoPurchase);
+        lottoView.printLottoPurchase(lottoPurchaseReturn);
+
     }
 }
--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,11 +1,16 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.common.LottoFinalConsts;
 
-public class LottoView {
+public class LottoView implements LottoFinalConsts {
 
     public String inputLottoPurchase(){
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_LOTTO_PURCHASE);
         return Console.readLine();
+    }
+
+    public void printLottoPurchase(int lottoPurchase){
+        System.out.printf("%d%s", lottoPurchase, PRINT_LOTTO_PURCHASE_COUNT);
     }
 }

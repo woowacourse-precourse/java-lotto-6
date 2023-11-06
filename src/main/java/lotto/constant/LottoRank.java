2a44873cf12fafdc98fa5b2e43fdd0d3a533cdb4
wns312 <wns312@gmail.com>
2023-11-06 15:44:22 +0900

    refactor(LottoRank): 일치 개수를 통한 랭크 조회 메소드 로직 변경
    변경사항
    - filter 방식을 사용하도록 함
    - 이에 따라 보너스 번호 정보를 Rank에 추가
    - 보너스 번호와 당첨 번호 필터링 적용

--- src/main/java/lotto/constant/LottoRank.java
+++ src/main/java/lotto/constant/LottoRank.java
@@ -5,21 +5,21 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public enum LottoRank {
-    SIX(2_000_000_000L, 6),
-    FIVE_WITH_BONUS(30_000_000L, 5),
-    FIVE_WITHOUT_BONUS(1_500_000L, 5),
-    FOUR(50_000L, 4),
-    THREE(5_000L, 3),
-    NO_PRIZE(0L, 0);
-
-    private static final int MATCH_COUNT_FIVE = 5;
+    SIX(2_000_000_000L, 6, false),
+    FIVE_WITH_BONUS(30_000_000L, 5, true),
+    FIVE_WITHOUT_BONUS(1_500_000L, 5, false),
+    FOUR(50_000L, 4, false),
+    THREE(5_000L, 3, false),
+    NO_PRIZE(0L, 0, false);
 
     private final long winnings;
     private final int matchCount;
+    private final boolean isBonusMatched;
 
-    LottoRank(long winnings, int matchCount) {
+    LottoRank(long winnings, int matchCount, boolean isBonusMatched) {
         this.winnings = winnings;
         this.matchCount = matchCount;
+        this.isBonusMatched = isBonusMatched;
     }
 
     public long getWinnings() {
@@ -28,27 +28,16 @@ public enum LottoRank {
 
     private boolean hasSameMatchCount(int matchCount) { return this.matchCount == matchCount; }
 
-    private static boolean hasFiveMatchCount(int matchCount) { return MATCH_COUNT_FIVE == matchCount; }
+    private boolean isBonusMatched(boolean isBonusMatched) { return this.isBonusMatched == isBonusMatched; }
 
     public static boolean isWinningRank(LottoRank lottoRank) { return lottoRank != NO_PRIZE; }
 
     public static List<LottoRank> getWinningRanks() { return Arrays.stream(values()).filter(LottoRank::isWinningRank).toList(); }
 
-    private static LottoRank getByBonusMatchWhenMatchCountIsFive(boolean isBonusMatched) {
-        if (isBonusMatched) {
-            return FIVE_WITH_BONUS;
-        }
-
-        return FIVE_WITHOUT_BONUS;
-    }
-
     public static LottoRank getRankByMatches(int matchCount, boolean isBonusMatched) {
-        if (hasFiveMatchCount(matchCount)) {
-            return getByBonusMatchWhenMatchCountIsFive(isBonusMatched);
-        }
-
         return Stream.of(values())
-                .filter(value -> value.hasSameMatchCount(matchCount))
+                .filter(rank -> rank.isBonusMatched(isBonusMatched))
+                .filter(rank -> rank.hasSameMatchCount(matchCount))
                 .findAny()
                 .orElse(NO_PRIZE);
     }

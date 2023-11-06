2da12925cbac3df714ea0ca2fdcd9e43f6155415
happyhun <younghun.ai@gmail.com>
2023-11-06 19:13:19 +0900

    feat: 로또 게임을 진행할 클래스 생성

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class LottoGame {
+
+    public void start() {
+        // TODO: 로또 구입 금액 입력
+        // TODO: 발행한 로또 수량 및 번호 출력
+        // TODO: 당첨 번호 입력
+        // TODO: 보너스 번호 입력
+        // TODO: 당첨 내역 출력
+        // TODO: 수익률 출력
+    }
+}

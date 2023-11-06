eac51bd38df0184d56c197c6164f3d408609e93d
aeyongdodam <kikititi0510@gmail.com>
2023-11-07 04:35:06 +0900

    feat(Rank): Enum 타입 Rank 생성

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
+
+    private final int correctNumbers;
+    private final int winnings;
+
+    Rank(int correctNumbers, int winnings) {
+        this.correctNumbers = correctNumbers;
+        this.winnings = winnings;
+    }
+
+    public int getCorrectNumbers() {
+        return correctNumbers;
+    }
+
+    public int getWinnings() {
+        return winnings;
+    }
+
+    public static Rank valueOf(int correctNumbers, boolean isBonus) {
+        if (correctNumbers == 6) {
+            return FIRST;
+        }
+        if (correctNumbers == 5 && isBonus) {
+            return SECOND;
+        }
+        if (correctNumbers == 5) {
+            return THIRD;
+        }
+        if (correctNumbers == 4) {
+            return FOURTH;
+        }
+        if (correctNumbers == 3) {
+            return FIFTH;
+        }
+        return NONE;
+    }
+}

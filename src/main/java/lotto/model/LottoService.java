a8e46a674b525a13f19870a833210de8d3093c35
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-07 01:42:39 +0900

    refactor(LottoService): 메소드 책임 분리 및 최적화

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -2,11 +2,14 @@ package lotto.model;
 
 import lotto.config.LottoMessage;
 import lotto.config.Rank;
+import lotto.dto.request.PurchaseAmountDto;
 import lotto.dto.request.WinningNumbersDto;
 import lotto.dto.response.LottoResultsDto;
 import lotto.dto.response.LottoTicketsDto;
+import lotto.util.NumberGenerator;
 import lotto.util.ProfitCalculator;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,68 +23,66 @@ public class LottoService {
         return new LottoService();
     }
 
-    public LottoResultsDto calculateResults(LottoTicketsDto lottoTicketsDto, WinningNumbersDto winningNumbersDto, Money spentMoney) {
-        Lottos lottoTickets = lottoTicketsDto.getLottoTickets();
-        List<Lotto> lottos = lottoTickets.getLottoTickets();
-
-        Map<Rank, Long> lottoStream = lottos.stream()
-                .map(lotto -> determineRank(lotto, winningNumbersDto.getNumbers(), winningNumbersDto.getBonus()))
-                .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
-
+    // 로또 결과 계산과 관련된 주요 로직을 처리하는 메소드
+    public LottoResultsDto calculateResults(LottoTicketsDto lottoTicketsDto, WinningNumbersDto winningNumbersDto) {
+        Map<Rank, Long> lottoStream = countWinningRanks(lottoTicketsDto, winningNumbersDto);
         LottoResults lottoResults = LottoResults.from(lottoStream);
-
         String formattedResults = formatResults(lottoResults);
-        float profitRate = calculateProfitRate(spentMoney, lottoResults);
+        float profitRate = calculateProfitRate(lottoTicketsDto, lottoResults);
 
-        return new LottoResultsDto(formattedResults, profitRate);
+        return LottoResultsDto.of(formattedResults, profitRate);
     }
 
-    private Rank determineRank(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
-        long matchCount = lotto.getNumbers().stream()
-                .filter(winningNumbers::contains)
-                .count();
-        boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
+    // 로또 등수별로 몇 개의 당첨이 있는지 계산하는 메소드
+    private static Map<Rank, Long> countWinningRanks(LottoTicketsDto lottoTicketsDto,
+                                                     WinningNumbersDto winningNumbersDto) {
+        return lottoTicketsDto.getLottoTickets().getLottoTickets().stream()
+                .map(lotto -> determineRank(lotto, winningNumbersDto.getNumbers(), winningNumbersDto.getBonus()))
+                .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
+    }
 
+    // 당첨 등수를 결정하는 메소드
+    private static Rank determineRank(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
+        long matchCount = countMatches(lotto, winningNumbers);
+        boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
         return Rank.valueOf(matchCount, bonusMatch);
     }
 
-    private String formatResults(LottoResults lottoResults) {
-        StringBuilder builder = new StringBuilder();
-
-        for (Rank rank : Rank.values()) {
-            if (rank != Rank.NONE) {
-                builder.append(
-                        getFormattedResultLine(
-                                rank,
-                                lottoResults.getResults().getOrDefault(rank, 0L)
-                        ));
-            }
-        }
-
-        return builder.toString();
+    // 로또 번호와 당첨 번호를 비교하여 일치하는 번호의 개수를 반환하는 메소드
+    private static long countMatches(Lotto lotto, List<Integer> winningNumbers) {
+        return lotto.getNumbers().stream()
+                .filter(winningNumbers::contains)
+                .count();
     }
 
-    private String getFormattedResultLine(Rank rank, Long count) {
+    private static String formatResults(LottoResults lottoResults) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank != Rank.NONE)
+                .map(rank -> formatResultLine(rank, lottoResults.getResults().getOrDefault(rank, 0L)))
+                .collect(Collectors.joining());
+    }
+
+    private static String formatResultLine(Rank rank, Long count) {
         String rankMessage = getRankMessage(rank);
         String matchResultMessage = getMatchResultMessage(rank, count);
 
         return rankMessage + matchResultMessage;
     }
 
-    private String getMatchResultMessage(Rank rank, Long count) {
+    private static String getMatchResultMessage(Rank rank, Long count) {
         String prizeMoney = rank.getPrizeMoneyFormatted();
         return LottoMessage.MATCH_RESULT.getFormattedMessage(prizeMoney, count);
     }
 
-    private String getRankMessage(Rank rank) {
+    private static String getRankMessage(Rank rank) {
         if (rank == Rank.SECOND) {
             return LottoMessage.MATCH_COUNT_WITH_BONUS.getFormattedMessage(rank.getMatchCount());
         }
         return LottoMessage.MATCH_COUNT.getFormattedMessage(rank.getMatchCount());
     }
 
-    private float calculateProfitRate(Money money, LottoResults lottoResults) {
-        int totalCost = money.getAmount();
+    private static float calculateProfitRate(LottoTicketsDto lottoTicketsDto, LottoResults lottoResults) {
+        int totalCost = lottoTicketsDto.getLottoCost();
         long totalPrize = lottoResults.calculateTotalPrize();
         return ProfitCalculator.calculateLottoReturnRate(totalCost, totalPrize);
     }

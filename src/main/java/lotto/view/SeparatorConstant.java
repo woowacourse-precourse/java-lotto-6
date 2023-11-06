1561f3bd44a9c67c1fae0bff67abb930b226f44f
yeseong31 <yeseong31@naver.com>
2023-11-06 22:54:58 +0900

    feat(SeparatorConstant): 금액 출력 포맷 상수 등록

--- src/main/java/lotto/view/SeparatorConstant.java
+++ src/main/java/lotto/view/SeparatorConstant.java
@@ -5,6 +5,7 @@ public class SeparatorConstant {
     public static final String RESULT_SUMMARY_SEPARATOR = "---";
     public static final String WINNING_NUMBERS_SEPARATOR = ",";
     public static final String LOTTO_NUMBERS_SEPARATOR = ", ";
+    public static final String DECIMAL_FORMAT = "###,###";
 
     private SeparatorConstant() {
     }

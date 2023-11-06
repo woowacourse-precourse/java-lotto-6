582276b0183a28460a19371905b1d2cc95d99e6d
박준영 <wnsdud6969@naver.com>
2023-11-07 02:20:32 +0900

    feat(Money): add Money class
    Add Money claas to manage money
    - add field money
    - add constructor

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+public class Money {
+
+    private static final long LOTTO_PRICE = 1000;
+    private long money;
+
+    public Money(long money) {
+        this.money = money;
+    }
+}

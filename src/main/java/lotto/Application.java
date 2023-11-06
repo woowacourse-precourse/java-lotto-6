e5e1ed28acef3399fbdd45516f07fa8e21a05a0f
Jiyoung Choi <12201817@inha.edu>
2023-11-06 18:41:47 +0900

    refactor(Application) : 의존관계 주입 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,16 +3,10 @@ package lotto;
 import lotto.controller.LottoStore;
 import lotto.model.WinningStatistics;
 import lotto.util.RandomLottoMachine;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        WinningStatistics winningStatistics = new WinningStatistics();
-        RandomLottoMachine randomLottoMachine = new RandomLottoMachine();
-        LottoStore lottoStore = new LottoStore(inputView, outputView, randomLottoMachine, winningStatistics);
+        LottoStore lottoStore = new LottoStore(new RandomLottoMachine(), new WinningStatistics());
         lottoStore.visit();
     }
 }

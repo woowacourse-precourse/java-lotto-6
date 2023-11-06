d13ea88cf7ae91d9b0416f59a8b438f9a6011223
shkisme <shkisme0130@gmail.com>
2023-11-06 20:32:40 +0900

    feat: Lotto 객체 생성 기능 추가
    로또 생성 시 다음의 유효성 검사를 시행하는 코드 작성
    - 로또 숫자 크기 유효성 체크
    - 로또 숫자 범위 유효성 체크
    - 로또 숫자 중복 체크

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,48 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.generator.NumberGenerator;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+        this.numbers = numbers.stream()
+                .sorted()
+                .toList();
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public static Lotto generateLotto(NumberGenerator numberGenerator) {
+        return new Lotto(numberGenerator.generate());
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 크기는 6개 입니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LOTTO_MIN_NUMBER || LOTTO_MAX_NUMBER < number) {
+                throw new IllegalArgumentException("로또 숫자의 범위는 1~45입니다.");
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 숫자는 중복될 수 없습니다.");
+        }
+    }
 }

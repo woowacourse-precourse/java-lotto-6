04901e1c477f354a0344da605ff618eb2b6ac7ff
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-07 05:42:55 +0900

    test : LotteryIssueServiceTest 테스트 코드 작성

---
+++ src/test/java/lotto/domain/purchase/issue/LotteryIssueServiceTest.java
@@ -0,0 +1,37 @@
+package lotto.domain.purchase.issue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lotto.domain.Lottos;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LotteryIssueServiceTest {
+
+    private LotteryIssueService lotteryIssueService;
+
+    @BeforeEach
+    void setUp() {
+        lotteryIssueService = new LotteryIssueService();
+    }
+
+    @AfterEach
+    void tearDown() {
+        lotteryIssueService = null;
+    }
+
+    @Test
+    @DisplayName("티켓 개수만큼 로또를 발행합니다.")
+    void 티켓개수만큼_로또발행() {
+        // GIVEN
+        Integer lottoTicketCount = 3;
+
+        // WHEN
+        Lottos lottos = lotteryIssueService.issueLottoAmountOf(lottoTicketCount);
+
+        // THEN
+        assertEquals(lottos.getSize(), lottoTicketCount);
+    }
+}

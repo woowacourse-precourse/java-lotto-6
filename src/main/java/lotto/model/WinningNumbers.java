c206fc2aaf3ff66034110db303923298ccd34bab
SunHyeok <cghhld@naver.com>
2023-11-06 14:58:58 +0900

    refactor: 외부에서 받을수 있도록 수정

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -8,9 +8,24 @@ public class WinningNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
-    public WinningNumbers(String winningNumbers, int bonusNumber) {
-        this.winningNumbers = parseWinningNumbers(winningNumbers);
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningNumbers parseWinningNumbers(String winningNumbersStr) {
+        String[] winningNumbersArray = winningNumbersStr.split(",");
+        List<Integer> winningNumbers = Arrays.stream(winningNumbersArray)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        if (winningNumbers.size() != 7) {
+            throw new IllegalArgumentException("당첨 번호와 보너스 번호를 모두 입력하세요.");
+        }
+
+        int bonusNumber = winningNumbers.remove(winningNumbers.size() - 1);
+
+        return new WinningNumbers(winningNumbers, bonusNumber);
     }
 
     public List<Integer> getWinningNumbers() {
@@ -20,11 +35,6 @@ public class WinningNumbers {
     public int getBonusNumber() {
         return bonusNumber;
     }
-
-    private List<Integer> parseWinningNumbers(String winningNumbers) {
-        List<String> numbers = Arrays.asList(winningNumbers.split(","));
-        return numbers.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
 }
+
+

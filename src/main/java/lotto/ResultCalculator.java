76b7c3a7230bcc8312550ae0530ecac72ad3b93a
Park Eunhyeong <silbroeh@gmail.com>
2023-11-06 12:09:37 +0900

    feat : ResultCalculator클래스 - compare()메소드 구현

--- src/main/java/lotto/ResultCalculator.java
+++ src/main/java/lotto/ResultCalculator.java
@@ -7,10 +7,31 @@ import java.util.Map;
 public class ResultCalculator {
     private final Map<Lotto, PrizeGrade> result;
 
-    public ResultCalculator(Lottos lottos, Lotto winningLotto) {
+    public ResultCalculator(Lottos lottos, Lotto winningLotto, Bonus bonus) {
         Map<Lotto, PrizeGrade> result = new HashMap<>();
         lottos.getTickets().stream()
-                .forEach(lotto -> result.put(lotto, calculatePrizeGrade(lotto, winningLotto)));
+                .forEach(lotto -> result.put(lotto, compare(lotto, winningLotto, bonus)));
         this.result = result;
+    }
+    private PrizeGrade compare(Lotto lotto, Lotto winningLotto, Bonus bonus) {
+        int matches = countMatchingNumbers(lotto.getNumbers(), winningLotto.getNumbers());
+        boolean bonusMatch = matchWithBonus(lotto.getNumbers(), bonus.getNumber());
+
+        if (matches == PrizeGrade.FIRST.getMatchCount()) {
+            return PrizeGrade.FIRST;
+        }
+        if (matches == PrizeGrade.SECOND.getMatchCount() && bonusMatch) {
+            return PrizeGrade.SECOND;
+        }
+        if (matches == PrizeGrade.THIRD.getMatchCount()) {
+            return PrizeGrade.THIRD;
+        }
+        if (matches == PrizeGrade.FOURTH.getMatchCount()) {
+            return PrizeGrade.FOURTH;
+        }
+        if (matches == PrizeGrade.FIFTH.getMatchCount()) {
+            return PrizeGrade.FIFTH;
+        }
+        return PrizeGrade.NO_PRIZE;
     }
 }

59ecafb2c777f8314954f04002f051b4c3abbc93
sangdol <priceelees@gmail.com>
2023-11-06 22:32:08 +0900

    refactor(LottoPrizes): View의 수정으로 인해 사용하지 않게된 기능 제거

--- src/main/java/lotto/domain/LottoPrizes.java
+++ src/main/java/lotto/domain/LottoPrizes.java
@@ -7,30 +7,19 @@ import java.util.Map;
 import lotto.domain.money.Money;
 
 public enum LottoPrizes {
-    THREE_NUMBERS_MATCHED("3개 일치", new Money(5_000L)),
-    FOUR_NUMBERS_MATCHED("4개 일치", new Money(50_000L)),
-    FIVE_NUMBER_MATCHED("5개 일치", new Money(1_500_000L)),
-    FIVE_NUMBER_AND_BONUS_MATCHED("5개 일치, 보너스 볼 일치", new Money(30_000_000L)),
-    SIX_NUMBER_MATCHED("6개 일치", new Money(2_000_000_000L));
-
-    private final String term;
+    THREE_NUMBERS_MATCHED(new Money(5_000L)),
+    FOUR_NUMBERS_MATCHED(new Money(50_000L)),
+    FIVE_NUMBER_MATCHED(new Money(1_500_000L)),
+    FIVE_NUMBER_AND_BONUS_MATCHED(new Money(30_000_000L)),
+    SIX_NUMBER_MATCHED(new Money(2_000_000_000L));
     private final Money winningAmount;
 
-    LottoPrizes(String term, Money winningAmount) {
-        this.term = term;
+    LottoPrizes(Money winningAmount) {
         this.winningAmount = winningAmount;
-    }
-
-    public String getTerm() {
-        return term;
     }
 
     public Money getWinningAmount() {
         return winningAmount;
-    }
-
-    public String winningAmountToString() {
-        return winningAmount.toString();
     }
 
     public static Map<LottoPrizes, Long> getPrizesCountMap() {

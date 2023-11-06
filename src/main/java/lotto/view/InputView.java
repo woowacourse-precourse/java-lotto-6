57ef52c4442e42170aaef2936c2bf84b79a46dae
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 14:32:20 +0900

    feat(inputView): 당첨 번호 입력 시 쉼표와 숫자, 공백만으로 구성되지 않은지 검증
    - Exception에 INVALID_WINNING_NUMBERS_INPUT_TYPE 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.Exception.INVALID_WINNING_NUMBERS_INPUT_TYPE;
 import static lotto.Exception.ONLY_NUMERIC_INPUT_FOR_MONEY;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class InputView {
     private final String WINNING_NUMBERS_DELIMITER = ",";
@@ -17,9 +19,10 @@ public class InputView {
     }
 
     public List<Integer> inputWinningNumbers() {
-        String input = removeSpace(readLine());
-        String[] numbers = splitWinningNumbers(input);
+        String input = readLine();
+        validateWinningNumbersInput(input);
 
+        String[] numbers = splitWinningNumbers(removeSpace(input));
         return convertToIntegers(numbers);
     }
 
@@ -35,5 +38,12 @@ public class InputView {
         return Arrays.stream(numbers)
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    // 입력값에서 숫자와 쉼표(,)를 제외한 다른 문자가 있는지 검사
+    private void validateWinningNumbersInput(String input) {
+        if (!Pattern.matches("^[0-9,\\s]+$", input)) {
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBERS_INPUT_TYPE.getMessage());
+        }
     }
 }

65c6a5743841fcddb2bb748ba9343adfa438e0af
Sanghee0820 <lsh277604@gmail.com>
2023-11-06 19:21:06 +0900

    Feat: add Class LottoDraw

---
+++ src/main/java/lotto/Domain/LottoDraw.java
@@ -0,0 +1,10 @@
+package lotto.Domain;
+
+
+public class LottoDraw {
+    private final WinningLotto winningLotto;
+
+    public LottoDraw(WinningLotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+}

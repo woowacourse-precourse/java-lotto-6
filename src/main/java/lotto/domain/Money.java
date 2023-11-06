92a8faec680bc72d229525eb1a977f07701a6b48
Subin <rla0010101@gmail.com>
2023-11-07 00:27:15 +0900

    Feat(Test): 구입 금액 예외 처리 로직 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,9 +1,40 @@
 package lotto.domain;
 
+import static lotto.util.Instruction.ERROR_INPUT_MONEY;
+import static lotto.util.Instruction.ERROR_MULTIPLE_MONEY;
+
 public class Money {
+    private static final int MINIMUM_AMOUNT = 1000;
     private final int money;
 
     public Money(final String money) {
+        isValidMoney(money);
         this.money = Integer.parseInt(money);
+    }
+
+    private void isValidMoney(final String money) {
+        checkValidInt(money);
+        checkMoreThanMinimumAmount(Integer.parseInt(money));
+        checkMultiple(Integer.parseInt(money));
+    }
+
+    private void checkValidInt(final String money) {
+        try {
+            Integer.parseInt(money);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ERROR_INPUT_MONEY.getMessage());
+        }
+    }
+
+    private void checkMoreThanMinimumAmount(final int money) {
+        if (money < MINIMUM_AMOUNT) {
+            throw new IllegalArgumentException(ERROR_INPUT_MONEY.getMessage());
+        }
+    }
+
+    private void checkMultiple(final int money) {
+        if (money % MINIMUM_AMOUNT != 0) {
+            throw new IllegalArgumentException(ERROR_MULTIPLE_MONEY.getMessage());
+        }
     }
 }
--- src/main/java/lotto/util/Instruction.java
+++ src/main/java/lotto/util/Instruction.java
@@ -1,7 +1,9 @@
 package lotto.util;
 
 public enum Instruction {
-    PRINT_INPUT_MONEY("구입금액을 입력해 주세요.");
+    PRINT_INPUT_MONEY("구입금액을 입력해 주세요."),
+    ERROR_INPUT_MONEY("[ERROR] 구입 금액으로 1000 이상의 수를 입력해 주세요."),
+    ERROR_MULTIPLE_MONEY("[ERROR] 구입 금액으로 1000의 배수를 입력해 주세요.");
 
     private final String message;
 

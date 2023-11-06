cfca01a6257ce42b11d85c5fcb71fcabb4d08834
Handwoong <handwoong@gmail.com>
2023-11-06 20:37:30 +0900

    test(lotto-buyer): 수익률 계산 테스트 추가

--- src/test/java/lotto/model/LottoBuyerTest.java
+++ src/test/java/lotto/model/LottoBuyerTest.java
@@ -42,9 +42,45 @@ class LottoBuyerTest {
         final LottoResult lottoResult = new LottoResult(winningNumbers, 45, lottoManager);
 
         // when
-        final Map<LottoGrade, Integer> result = lottoBuyer.calculateLottoResult(lottoResult);
+        final Map<LottoGrade, Integer> result = lottoBuyer.calculateResult(lottoResult);
 
         // then
         assertThat(result.values()).containsExactly(0, 0, 0, 0, 8);
+    }
+
+    @Test
+    @DisplayName("수익률을 계산한다.")
+    void returnOnInvestment() throws Exception {
+        // given
+        final int amount = 8000;
+        final LottoBuyer lottoBuyer = new LottoBuyer(amount, lottoManager);
+
+        final List<Integer> winningNumbers = List.of(2, 3, 4, 5, 6, 7);
+        final LottoResult lottoResult = new LottoResult(winningNumbers, 1, lottoManager);
+
+        // when
+        lottoBuyer.calculateResult(lottoResult);
+        final double returnOnInvestment = lottoBuyer.returnOnInvestment();
+
+        // then
+        assertThat(returnOnInvestment).isEqualTo(3_000_000);
+    }
+
+    @Test
+    @DisplayName("당첨 수익이 0원이라면 수익률은 0이다.")
+    void returnOnInvestmentZero() throws Exception {
+        // given
+        final int amount = 8000;
+        final LottoBuyer lottoBuyer = new LottoBuyer(amount, lottoManager);
+
+        final List<Integer> winningNumbers = List.of(10, 11, 12, 13, 14, 15);
+        final LottoResult lottoResult = new LottoResult(winningNumbers, 45, lottoManager);
+
+        // when
+        lottoBuyer.calculateResult(lottoResult);
+        final double returnOnInvestment = lottoBuyer.returnOnInvestment();
+
+        // then
+        assertThat(returnOnInvestment).isEqualTo(0);
     }
 }

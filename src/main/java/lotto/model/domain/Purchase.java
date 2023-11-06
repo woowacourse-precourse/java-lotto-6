3b5aa4e8b99f4a3e48e692b9c30bc361eecf56a5
D0ri123 <lsh81550@gmail.com>
2023-11-06 13:03:07 +0900

    feat(Purchase): 구입 금액 검증 로직 추가 - 3차 구현
    구입 금액 입력에 대한 검증 로직을 추가했습니다.
    예외가 발생해도 게임이 진행될 수 있도록 InputValidator 인터페이스 메서드를 구현했습니다.

--- src/main/java/lotto/model/domain/Purchase.java
+++ src/main/java/lotto/model/domain/Purchase.java
@@ -1,21 +1,42 @@
 package lotto.model.domain;
 
-public class Purchase {
-    private final int price;
-    private final int ticketCount;
+import lotto.util.InputValidator;
 
-    public Purchase(int price) {
-        this.price = price;
-        //TODO: price 유효성 체크하기
-        this.ticketCount = price / 1000;
+public class Purchase implements InputValidator {
+    private static final int PURCHASE_PRICE_UNIT = 1000;
+    private Integer price;
+    private Integer ticketCount;
+
+    @Override
+    public boolean getInputUntilValid() {
+        return (price == null || ticketCount == null);
     }
 
-    public int getTicketCount() {
+    public void setOrderInfo(String order) {
+        this.price = validateIntegerInput(order);
+        this.ticketCount = validatePriceInThousandUnit(price);
+    }
+
+    private int validateIntegerInput(String price) {
+        try {
+            return Integer.parseInt(price);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자로만 입력해주세요. 예시) 1000 or 2000 ..");
+        }
+    }
+
+    private int validatePriceInThousandUnit(int price) {
+        if(price % PURCHASE_PRICE_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 " + PURCHASE_PRICE_UNIT + "단위로 입력해주세요.");
+        }
+        return price/PURCHASE_PRICE_UNIT;
+    }
+
+    public Integer getTicketCount() {
         return ticketCount;
     }
 
-    public int getPrice() {
+    public Integer getPrice() {
         return price;
     }
-
 }

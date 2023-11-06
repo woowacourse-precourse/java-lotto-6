ba2071b9840b3e6069754b24f4b5cbed0af242d9
SeungGwa123 <skwan123@naver.com>
2023-11-06 21:00:16 +0900

    Feat: Add outOfRangeMoney
    구입 금액이 0이하의 입력을 받을 경우 에러 메세지를 출력하는 함수 작성

--- src/main/java/Except/Except.java
+++ src/main/java/Except/Except.java
@@ -1,2 +1,23 @@
-package Except;public class Except {
+package Except;
+
+import Input.Input;
+import Message.Message;
+
+public class Except {
+    private static Message outOfRange = Message.moneyOutOfRange;
+    private static Message invalidFormat = Message.moneyInvalidFormat;
+    private static final String INTEGER_REGEX = "^-?\\d+$";
+    public int outOfRangeMoney(String money){
+        int result = outOfRange.getCode();
+        Input input = new Input();
+        try{
+            if(!money.matches(INTEGER_REGEX)){
+                throw new IllegalArgumentException();
+            }
+            result = Integer.parseInt(money);
+        }catch(IllegalArgumentException iae){
+            System.out.println(outOfRange.getMessage());
+        }
+        return result;
+    }
 }

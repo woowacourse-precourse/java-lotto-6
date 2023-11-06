67a520e1e681290d1af4650c2d3113be8137df61
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 12:50:29 +0900

    test(lottoMachine): 수량에 맞게 로또 리스트가 생성되는지 테스트
    - 수량 결정 메서드 private로 변경에 따른 구입 금액에 맞게 수량이 테스트되는지 삭제

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -2,18 +2,22 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
     private LottoMachine lottoMachine = new LottoMachine(new LottoNumberGenerator());
 
     @Test
-    void 구입_금액에_맞게_로또_수량이_결정되는가() {
+    void 로또_수량에_맞게_로또_리스트가_생성되는가() {
+        // given
         int lottoPrice = 1_000;
         int lottoQuantity = 10;
 
-        assertThat(
-                lottoMachine.calculateLottoQuantity(lottoPrice * lottoQuantity))
-                .isEqualTo(lottoQuantity);
+        // when
+        List<Lotto> actual = lottoMachine.buyLottos(lottoPrice * lottoQuantity);
+
+        // then
+        assertThat(actual).hasSize(lottoQuantity);
     }
 }

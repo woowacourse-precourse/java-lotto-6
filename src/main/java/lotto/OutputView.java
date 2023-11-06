de92d11aff597f328a98eab439ef337678067ada
yooyoungpark <yoongtopiaa@gmail.com>
2023-11-06 16:08:53 +0900

    refactor : 용어 변경에 따른 리팩토링 (purchasenumber -> lotto)

---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OutputView {
+
+    public static void printPurchaseCount(int purchaseCount) {
+        System.out.println();
+        System.out.println(purchaseCount + "개를 구매했습니다.");
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            List<LottoNumber> lottoNumbers = lotto.getNumbers();
+            List<Integer> numbers = new ArrayList<>();
+            for (LottoNumber lottoNumber : lottoNumbers) {
+                numbers.add(lottoNumber.getNumber());
+            }
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
+    }
+}

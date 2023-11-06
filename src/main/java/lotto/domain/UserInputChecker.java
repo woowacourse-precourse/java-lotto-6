4cc833d20acd7fbb340001f6970a2c1591e55247
HYOAN <aspqll@naver.com>
2023-11-06 22:48:06 +0900

    feat(lotto): 구입금액이 1000원 단위인지 확인하는 예외처리 기능 구현

--- src/main/java/lotto/domain/UserInputChecker.java
+++ src/main/java/lotto/domain/UserInputChecker.java
@@ -1,2 +1,28 @@
-package lotto.domain;public class UserInputChecker {
+package lotto.domain;
+
+public class UserInputChecker {
+    public UserInputChecker() {
+    }
+    public int checkPurchaseAmount(String inputPurchaseAmount) {
+        int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
+        try {
+            checkInputUnit(purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return purchaseAmount;
+    }
+    public int checkInputUnit(int purchaseAmount) { //구입금액 단위 확인
+        if (purchaseAmount % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로만 입력 가능합나다.");
+        return purchaseAmount;
+    }
+    public void checkInt(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+        }
+    }
+
 }

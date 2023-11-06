deff74317c1aef392e367e9dd77656ac929c0fde
SeungGwa123 <skwan123@naver.com>
2023-11-07 02:13:17 +0900

    Fix: Fix buyAllLotto
    buyAllLotto의 자료형과 출력을 하는 기능 추가

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import Output.Output;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -8,19 +9,22 @@ import java.util.List;
 
 public class BuyLotto {
     private static List<Lotto> lotto = new ArrayList<>();
+    private static Output output = new Output();
 
-    public List<Lotto> buyAllLotto(int money){
-        for(int i=0;i<money;i++){
+    public void buyAllLotto(int money) {
+        for (int i = 0; i < money; i++) {
             Lotto paper = new Lotto(pickNumber());
             lotto.add(paper);
         }
-        return lotto;
+        output.lottoAllPaperPrint(lotto);
     }
-    public List<Integer> pickNumber(){
+
+    public List<Integer> pickNumber() {
         List<Integer> paper = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         Collections.sort(paper);
         return paper;
     }
+
     public boolean checkSameNumber(List<Integer> paper, int number) {
         boolean result = true;
         for (int i = 0; i < paper.size(); i++) {

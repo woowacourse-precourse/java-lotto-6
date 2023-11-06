bfe43078f6d2f8e3ac5daa142d63b3ed8e0882d6
yang <hjyang@epience.com>
2023-11-06 10:53:46 +0900

    🎯feat(validate/InputValidation) : 중복 번호 입력 validation 기능 구현

--- src/main/java/lotto/validate/InputValidation.java
+++ src/main/java/lotto/validate/InputValidation.java
@@ -1,12 +1,14 @@
 package lotto.validate;
 
+import java.util.List;
 import java.util.Objects;
 
 public class InputValidation {
     private final String NUMBER_REG_EXP = "^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$";
     private final String ONLY_1_45_NUMBER = "1-45 사이의 숫자만 입력하세요.";
     private final String ONLY_DIVIDED_THOUSAND = "금액은 1000단위로 입력하세요.";
-    // 구입금액 validation
+
+    /* 구입금액 validation*/
 
     // 0-45 숫자 이외의 값이 있는지
     public void isNumber(String number) {
@@ -29,6 +31,16 @@ public class InputValidation {
 
     // 숫자 + 스페이스
     public boolean numberWhiteSpace(String number) {
-        return !number.replaceAll(" ","").equals(number);
+        return !number.replaceAll("\\s","").equals(number);
+    }
+
+    /* 로또 번호 validation*/
+    // 1-45 숫자 외 값, 공백은 공통으로 사용
+
+    // 중복된 숫자 검사
+    public void isDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("중복된 번호를 입력할 수 없습니다.");
+        }
     }
 }

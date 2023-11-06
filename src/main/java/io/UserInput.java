70256c70472a8863da3ffff8eaa38560226e615b
yewonniy <ae6652@naver.com>
2023-11-06 14:50:20 +0900

    feat : 사용자 input 기능 구현

---
+++ src/main/java/io/UserInput.java
@@ -0,0 +1,25 @@
+package io;
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.List;
+
+public class UserInput {
+
+    // 금액 입력받기
+    static String userMoney() {
+
+        return Console.readLine();
+    }
+
+    // 당첨 번호
+    static String winningLotteryNum() {
+
+        return Console.readLine();
+    }
+
+    // 보너스 번호
+    static String bonusNum() {
+
+        return Console.readLine();
+    }
+}

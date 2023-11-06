ed0d9c083d13dae0ac3003b1679b211fc3b6af7d
최진원 <etudent39@gmail.com>
2023-11-06 21:35:03 +0900

    feat : 구매 개수 반환 기능 추가

--- src/main/java/lotto/BuyAmount.java
+++ src/main/java/lotto/BuyAmount.java
@@ -27,4 +27,8 @@ public class BuyAmount {
     public Integer getBuyAmount() {
         return this.buyAmount;
     }
+
+    public Integer getBuyCount() {
+        return this.buyAmount / 1000;
+    }
 }

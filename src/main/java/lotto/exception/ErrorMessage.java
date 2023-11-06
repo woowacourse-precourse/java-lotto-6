19e0e5457d75d2d39b0ab5bc2ab30012113a49b8
xogus3492 <wkdxogus3492@gmail.com>
2023-11-06 15:31:27 +0900

    Refactor: 예외 알림 문자열 공통 부분 추출
    유지보수성이 증가함

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,16 +3,16 @@ package lotto.exception;
 public enum ErrorMessage {
 
     // Validator
-    PURCHASE_AMOUNT_UNDER_THOUSAND("[ERROR] 구입 금액은 1000원 이상이어야 합니다."),
-    INDIVISIBLE_PURCHASE_AMOUNT("[ERROR] 구입 금액은 1000원으로 나누어떨어져야 합니다."),
-    NOT_EMPTY_OR_BLANK("[ERROR] 공백이 존재하거나 값이 비어있으면 안됩니다."),
-    NOT_EXIST_CHARACTER("[ERROR] 문자가 존재하면 안됩니다."),
-    DUPLICATE_WITH_WIN_NUMBER("[ERROR] 당첨 숫자와 중복되지 않아야 합니다."),
+    PURCHASE_AMOUNT_UNDER_THOUSAND("구입 금액은 1000원 이상이어야 합니다."),
+    INDIVISIBLE_PURCHASE_AMOUNT("구입 금액은 1000원으로 나누어떨어져야 합니다."),
+    NOT_EMPTY_OR_BLANK("공백이 존재하거나 값이 비어있으면 안됩니다."),
+    NOT_EXIST_CHARACTER("문자가 존재하면 안됩니다."),
+    DUPLICATE_WITH_WIN_NUMBER("당첨 숫자와 중복되지 않아야 합니다."),
 
     // Lotto
-    INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호의 갯수는 6개여야 합니다."),
-    INVALID_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    INVALID_LOTTO_NUMBER_COUNT("로또 번호의 갯수는 6개여야 합니다."),
+    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복되지 않아야 합니다.");
 
     private final String message;
 
@@ -21,6 +21,6 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return message;
+        return "[ERROR] " + message;
     }
 }

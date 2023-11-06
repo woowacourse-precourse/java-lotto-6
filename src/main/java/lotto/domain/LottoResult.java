bf4e2c4b0266869b60826b552f8673262086f932
mumpuris <hst990124@naver.com>
2023-11-06 21:48:35 +0900

    기본 Enum 클래스 작성

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public enum LottoResult {
+    NONE(0, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    FIVE_WITH_BONUS(5, 30000000),
+    SIX(6, 2000000000);
+
+    private final int matchCount;
+    private final int price;
+
+    LottoResult(int matchCount, int price) {
+        this.matchCount = matchCount;
+        this.price = price;
+    }
+}

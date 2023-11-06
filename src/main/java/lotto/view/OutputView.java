bc6b8648b2a454bc279fb55299f80bd4f39030b2
sangdol <priceelees@gmail.com>
2023-11-06 22:18:15 +0900

    refactor(OutputView): OutputView가 도메인의 '변화하는 부분'만 출력하도록 수정 및 request 메서드 병합

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,26 @@
 package lotto.view;
 
+import static lotto.view.constant.ViewConstant.OutputViewConstant.AMOUNT;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.BONUS_NUMBER_MESSAGE;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.EARNING_RATE_MESSAGE;
-import static lotto.view.constant.ViewConstant.OutputViewConstant.HUNDRED;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.FIVE_AND_BONUS_MATCHED;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.FIVE_MATCHED;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.FOUR_MATCHED;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.NEW_LINE;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.PURCHASE_AMOUNT_MESSAGE;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.PURCHASE_MESSAGE;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.SIX_MATCHED;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.START_INDEX;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.STATUS;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.THREE_DASH;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.THREE_MATCHED;
+import static lotto.view.constant.ViewConstant.OutputViewConstant.WINNING_NUMBER;
 import static lotto.view.constant.ViewConstant.OutputViewConstant.WINNING_NUMBER_MESSAGE;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class OutputView {
 
     public void printErrorMessage(final String message) {
@@ -20,15 +32,15 @@ public class OutputView {
         System.out.println();
     }
 
-    public void requestAmount() {
-        System.out.println(PURCHASE_AMOUNT_MESSAGE);
-    }
-
-    public void requestWinningNumber() {
-        System.out.println(WINNING_NUMBER_MESSAGE);
-    }
-
-    public void requestBonusNumber() {
+    public void request(String order) {
+        if (AMOUNT.equals(order)) {
+            System.out.println(PURCHASE_AMOUNT_MESSAGE);
+            return;
+        }
+        if (WINNING_NUMBER.equals(order)) {
+            System.out.println(WINNING_NUMBER_MESSAGE);
+            return;
+        }
         System.out.println(BONUS_NUMBER_MESSAGE);
     }
 
@@ -38,15 +50,31 @@ public class OutputView {
         printNewLine();
     }
 
-    public void printStatistics(final String statistics) {
+    public void printStatistics(final List<Long> winCounts) {
         System.out.println(STATUS);
         System.out.println(THREE_DASH);
-        System.out.println(statistics);
+        System.out.println(supplyStatistics(winCounts));
     }
 
-    public void printEarningRate(final long purchaseAmount, final long revenue) {
-        final double earningRate = ((double)revenue / purchaseAmount) * HUNDRED;
-
+    public void printEarningRate(final double earningRate) {
         System.out.printf(EARNING_RATE_MESSAGE + NEW_LINE, earningRate);
+    }
+
+    public String supplyStatistics(List<Long> winCounts) {
+        List<String> prizeMessages = List.of(
+                THREE_MATCHED,
+                FOUR_MATCHED,
+                FIVE_MATCHED,
+                FIVE_AND_BONUS_MATCHED,
+                SIX_MATCHED
+        );
+
+        return IntStream.range(START_INDEX, winCounts.size())
+                .mapToObj(index -> {
+                    String prize = prizeMessages.get(index);
+                    long winCount = winCounts.get(index);
+                    return String.format(prize, winCount);
+                })
+                .collect(Collectors.joining(NEW_LINE));
     }
 }

2d8a5d3dcf5f18fc81b49ed686551e9148f5055b
10Hyuna <kimha105@naver.com>
2023-11-07 02:22:27 +0900

    add: Random number issuance logic sorted in ascending order

--- src/main/java/lotto/controller/ProviderRandomValue.java
+++ src/main/java/lotto/controller/ProviderRandomValue.java
@@ -1,4 +1,20 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProviderRandomValue {
+
+    public ProviderRandomValue() {
+    }
+
+    public List<Integer> provideLotto() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
 }

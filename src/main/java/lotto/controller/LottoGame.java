823253989fc6a1db38292e3b8785a41ef3bdab70
pokycookie <0cookieboy0@gmail.com>
2023-11-06 14:31:32 +0900

    feat: LottoGame Controller 생성
    - 필요한 사용자 입력을 입력받음
    - 입력 예외사항 발생 시 에러 메시지를 출력 후, 해당 시점부터 다시 입력받음
    - Lotto 개수만큼 생성
    - LottoResult를 계산하여 필요한 당첨 결과 내용 출력

---
+++ src/main/java/lotto/controller/LottoGame.java
@@ -0,0 +1,85 @@
+package lotto.controller;
+
+import lotto.config.WinningPrize;
+import lotto.domain.*;
+import lotto.util.Parser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+    private BuyingPrice buyingPrice;
+    private final List<Lotto> lottos = new ArrayList<>();
+    private final WinningNumber winning = new WinningNumber();
+    private final LottoResult result = new LottoResult();
+
+    public void run() {
+        inputBuyingPrice();
+        createLotto();
+        inputWinningNumber();
+        inputBonusNumber();
+        printResult();
+    }
+
+    private void inputBuyingPrice() {
+        try {
+            int price = Parser.parseIntOrThrow(InputView.inputBuyingPrice());
+            buyingPrice = new BuyingPrice(price);
+            OutputView.newLine();
+            OutputView.printBuyingCount(buyingPrice.getTryCount());
+        } catch (IllegalArgumentException error) {
+            OutputView.printErrorMessage(error);
+            inputBuyingPrice();
+        }
+    }
+
+    private void createLotto() {
+        int tryCount = buyingPrice.getTryCount();
+        while (tryCount-- > 0) {
+            List<Integer> numbers = LottoNumberGenerator.generate();
+            lottos.add(new Lotto(numbers));
+            OutputView.printEachLotto(numbers);
+        }
+        OutputView.newLine();
+    }
+
+    private void inputWinningNumber() {
+        try {
+            List<Integer> numbers = Parser.parseIntListOrThrow(InputView.inputWinningNumber());
+            winning.setNumbers(new Lotto(numbers));
+            OutputView.newLine();
+        } catch (IllegalArgumentException error) {
+            OutputView.printErrorMessage(error);
+            inputWinningNumber();
+        }
+    }
+
+    private void inputBonusNumber() {
+        try {
+            int bonus = Parser.parseIntOrThrow(InputView.inputBonusNumber());
+            winning.setBonus(bonus);
+            OutputView.newLine();
+        } catch (IllegalArgumentException error) {
+            OutputView.printErrorMessage(error);
+            inputBonusNumber();
+        }
+    }
+
+    private void makeResult() {
+        for (Lotto lotto : lottos) {
+            WinningPrize prize = winning.compareWithLotto(lotto);
+            result.apply(prize);
+        }
+    }
+
+    private void printResult() {
+        makeResult();
+        OutputView.printResultTitle();
+        for (WinningPrize prize : WinningPrize.values()) {
+            OutputView.printEachResult(prize, result.getEachResult(prize));
+        }
+        OutputView.printProfitResult(result.getFinalProfit());
+    }
+}

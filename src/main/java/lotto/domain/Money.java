5c0e58cc7ba0e855a62472c1b480d16f409a0278
박준영 <wnsdud6969@naver.com>
2023-11-07 02:38:05 +0900

    feat(Money): add new features
    Add new features to Money
    - add error message header
    - add error message that not purchasable money
    - add error message that not divisible
    - add error message that negative money
    - add zero constant
    - validate money : purchasable, divisible by lotto price, negative

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,10 +2,53 @@ package lotto.domain;
 
 public class Money {
 
+    private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
+    private static final String ERROR_NOT_PURCHASABLE = "불가능한 금액입니다.";
+    private static final String ERROR_NOT_DIVISIBLE = "천원 단위로 입력해주세요.";
+    private static final String ERROR_NEGATIVE_MONEY = "금액은 음수가 될 수 없습니다.";
     private static final long LOTTO_PRICE = 1000;
+    private static final long ZERO = 0;
+
     private long money;
 
     public Money(long money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(long money) {
+        validateNegativePurchaseMoney(money);
+        validateMinimumPurchaseMoney(money);
+        validateDivisibleByLottoPrice(money);
+    }
+
+    private void validateMinimumPurchaseMoney(long money) {
+        if (notPurchasable(money)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + ERROR_NOT_PURCHASABLE);
+        }
+    }
+
+    private void validateDivisibleByLottoPrice(long money) {
+        if (notDivisibleByLottoPrice(money)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + ERROR_NOT_DIVISIBLE);
+        }
+    }
+
+    private void validateNegativePurchaseMoney(long money) {
+        if (isNegative(money)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + ERROR_NEGATIVE_MONEY);
+        }
+    }
+
+    private boolean notPurchasable(long money) {
+        return (money < LOTTO_PRICE) && (money > ZERO);
+    }
+
+    private boolean notDivisibleByLottoPrice(long money) {
+        return (money % LOTTO_PRICE) != ZERO;
+    }
+
+    private boolean isNegative(long money) {
+        return money < ZERO;
     }
 }

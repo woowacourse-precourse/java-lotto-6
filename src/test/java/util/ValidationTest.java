36155efef875374e3c83febd24c28fb13206aca5
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-06 19:06:06 +0900

    <test> :  <테스트 코드 중 이상하게 테스트 하던 부분 수정>

--- src/test/java/util/ValidationTest.java
+++ src/test/java/util/ValidationTest.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -42,49 +44,50 @@ public class ValidationTest {
 
     @Test
     void 당첨_번호가_숫자가_아닌_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckMoneyInput("1,2,0,s,5,6"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckWinningNumber("1,2,0,s,5,6"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
 
     @Test
     void 당첨_번호가_범위_내에_없는_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckMoneyInput("46,0,1,3,4,5"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckWinningNumber("46,0,1,3,4,5"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
     @Test
     void  당첨_번호에_빈칸이_있는_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckMoneyInput("1,3 ,4,5,6,7"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckWinningNumber("1,3 ,4,5,6,7"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
 
     @Test
     void 보너스_번호의_길이가_올바르지_않은_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckWinningNumber("12"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckBonusNumber("123", Arrays.asList(1,3,5,7,8,9)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
 
     @Test
     void 보너스_번호가_숫자가_아닌_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckMoneyInput("s1"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckBonusNumber("s1", Arrays.asList(1,3,5,7,8,9)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
 
     @Test
     void 보너스_번호가_범위_내에_없는_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckMoneyInput("462"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckBonusNumber("46", Arrays.asList(1,3,5,7,8,9)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
 
     @Test
     void  보너스_번호에_빈칸이_있는_경우(){
-        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckMoneyInput("3 3"))
+        assertSimpleTest(() -> assertThatThrownBy(() -> validate.CheckBonusNumber("3 3", Arrays.asList(1,3,5,7,8,9)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE));
     }
+   
 }

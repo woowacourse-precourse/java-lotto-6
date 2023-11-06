23e42ea486be522819e2e5d0cb915376842ab119
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-06 20:46:04 +0900

    feat: 구매 내역 출력 메시지 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,15 @@
 package lotto.view;
 
-public class OutputView {
+import java.util.List;
 
+public class OutputView {
+	
+	public static void printCountOfLotto(int count) {
+		System.out.printf("%d개를 구매했습니다.\n", count);
+	}
+
+	public static void printNumbersOfLotto(List<Integer> numbers) {
+		System.out.println(numbers);
+	}
+	
 }

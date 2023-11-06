c897e647a5a43f3fbe5f16128bb461f53c5399be
Jeongwooyoung1026 <dndud1026@naver.com>
2023-11-06 23:16:02 +0900

    docs : 구현 기능 목록 순서 수정

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -10,6 +10,12 @@ public class LottoView {
         return price;
     }
 
+    // 로또 당첨 번호 입력 받기
+    public static Integer inputWinningNumber() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int price = Integer.parseInt(Console.readLine());
+        return price;
+    }
 
 
 }

12583ff42defc5bfe3e12ab3d3d7c0e9db34b434
le2sky <leehaneul990623@gmail.com>
2023-11-07 05:34:29 +0900

    feat : 재입력 기능 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,11 +1,12 @@
 package lotto;
 
-import lotto.domain.common.Money;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoFactory;
 import lotto.domain.lotto.LottoMachine;
 import lotto.domain.lotto.LottoNumber;
 import lotto.domain.player.Player;
+
+import java.util.function.Supplier;
 
 class Controller {
 
@@ -21,27 +22,45 @@ class Controller {
         printLottoResult(player);
     }
 
-    private Player createPlayer() {
-        return Player.from(requestPlayerMoney(), lottoMachine);
+    private void printLottoResult(Player player) {
+        Lotto answerLotto = createAnswerLotto();
+        LottoNumber bonus = createBonus();
+
+        handlePrintRaffleResultError(player, answerLotto, bonus);
     }
 
-    private void printLottoResult(Player player) {
-        Lotto answerLotto = requestAnswerLotto();
-        LottoNumber bonus = requestBonus();
+    private void handlePrintRaffleResultError(Player player, Lotto answerLotto, LottoNumber bonus) {
+        try {
+            printLottoResult(player, answerLotto, bonus);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            printLottoResult(player);
+        }
+    }
 
+    private void printLottoResult(Player player, Lotto answerLotto, LottoNumber bonus) {
         OutputView.printStatistics(player.showStatistics(answerLotto, bonus));
         OutputView.printEarningRate(player.showEarningRate(answerLotto, bonus));
     }
 
-    private Money requestPlayerMoney() {
-        return Mapper.mapToMoney(InputView.readPlayerMoney());
+    private Player createPlayer() {
+        return handleSupplierError(() -> Player.from(Mapper.mapToMoney(InputView.readPlayerMoney()), lottoMachine));
     }
 
-    private Lotto requestAnswerLotto() {
-        return Mapper.mapToLotto(InputView.readAnswerLotto());
+    private Lotto createAnswerLotto() {
+        return handleSupplierError(() -> Mapper.mapToLotto(InputView.readAnswerLotto()));
     }
 
-    private LottoNumber requestBonus() {
-        return Mapper.mapToLottoNumber(InputView.readBonus());
+    private LottoNumber createBonus() {
+        return handleSupplierError(() -> Mapper.mapToLottoNumber(InputView.readBonus()));
+    }
+
+    private <T> T handleSupplierError(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return handleSupplierError(supplier);
+        }
     }
 }

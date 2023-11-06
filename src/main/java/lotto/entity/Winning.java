4d32802c35e9e77fea9fd04488509207b2169719
seonghoo1217 <seonghoo1217@naver.com>
2023-11-07 01:58:15 +0900

    refactor: Winning bonus number 분리

--- src/main/java/lotto/entity/Winning.java
+++ src/main/java/lotto/entity/Winning.java
@@ -15,26 +15,24 @@ import static lotto.validation.ValidationForm.*;
 public class Winning{
     private final List<Integer> winningNumbers;
 
-    public Winning (String inputWinningNumbers,String winningBonusNumber){
-        this.winningNumbers = winningNumbersConvertToList(inputWinningNumbers, winningBonusNumber);
+    public Winning (String inputWinningNumbers){
+        this.winningNumbers = winningNumbersConvertToList(inputWinningNumbers);
     }
 
-    private List<Integer> winningNumbersConvertToList(String inputWinningNumbers,String winningBonusNumber){
+    private List<Integer> winningNumbersConvertToList(String inputWinningNumbers){
         List<String> winningNumberList = Arrays.asList(inputWinningNumbers.split(DELIMITER));
-        validate(inputWinningNumbers,winningBonusNumber,winningNumberList);
+        validate(inputWinningNumbers, winningNumberList);
         List<Integer> winningNumbers = new ArrayList<>(winningNumberList.stream().map(Integer::parseInt).toList());
-        winningNumbers.add(Integer.parseInt(winningBonusNumber));
         return winningNumbers;
     }
-    static void validate(String winningNumbers,String winningBonusNumber,List<String> winningNumberList){
+    static void validate(String winningNumbers, List<String> winningNumberList){
         verifyFormatForInputValue(WINNINGS,winningNumbers);
         winningNumberList.forEach(number->verifyFormatForInputValue(WINNING,number));
-        verifyFormatForInputValue(WINNING,winningBonusNumber);
     }
 
     public List<Integer> getWinningNumbers() {
-        return Collections.unmodifiableList((List<Integer>)FiledMapper.getFieldValue(
-                        this, MethodProperty.WINNING_NUMBERS)
+        return Collections.unmodifiableList(
+                (List<Integer>)FiledMapper.getFieldValue(this, MethodProperty.WINNING_NUMBERS)
         );
     }
 }
--- src/test/java/lotto/entity/WinningTest.java
+++ src/test/java/lotto/entity/WinningTest.java
@@ -16,23 +16,8 @@ public class WinningTest {
     @ValueSource(strings = {"1,2,3,,4,5,6","1,2,3,4,5,6,","1,2,3,4,5","1,2,3,사,5,6","","1,2,3, 4,5,6","1,2,3,4,5,46"})
     void Winning_생성_WinningNumbers_예외_테스트(String winningNumbers){
         //given
-        String bonusNumber = "7";
-
         assertThatThrownBy(()->{
-                new Winning(winningNumbers,bonusNumber);
-            }
-                ).isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(errorPrefix);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"0","46","한글","","2 1","4,"})
-    void Winning_생성_BonusNumber_예외_테스트(String bonusNumber){
-        //given
-        String winningNumbers = "1,2,3,4,5,6";
-
-        assertThatThrownBy(()->{
-                new Winning(winningNumbers,bonusNumber);
+                new Winning(winningNumbers);
             }
                 ).isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining(errorPrefix);
@@ -41,11 +26,10 @@ public class WinningTest {
     @Test
     void Winning_생성_유효한_값_테스트(){
         //given
-        String winningNumbersString = "1,2,3,4,5,6";
-        String bonusNumber = "7";
-        List<Integer> targetNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        String inputWinningNumbers = "1,2,3,4,5,6";
+        List<Integer> targetNumbers = List.of(1, 2, 3, 4, 5, 6);
         //when
-        Winning winning = new Winning(winningNumbersString, bonusNumber);
+        Winning winning = new Winning(inputWinningNumbers);
         List<Integer> winningNumbers = winning.getWinningNumbers();
 
         //then
@@ -55,11 +39,9 @@ public class WinningTest {
     @Test
     void Winning_생성_반환_값_리스트_추가_에러_테스트(){
         //given
-        String winningNumbersString = "1,2,3,4,5,6";
-        String bonusNumber = "7";
-
+        String inputWinningNumber = "1,2,3,4,5,6";
         //when
-        Winning winning = new Winning(winningNumbersString, bonusNumber);
+        Winning winning = new Winning(inputWinningNumber);
         List<Integer> winningNumbers = winning.getWinningNumbers();
         //then
         assertThatThrownBy(()->{

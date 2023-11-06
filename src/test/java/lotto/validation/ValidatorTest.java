9f8a9f45ee5eae8011a93ecbbfa1fee11be20bb7
DaeYoungK <hanguk830@naver.com>
2023-11-07 01:03:34 +0900

    test(Validator): 검증기가 제대로 동작하는지 테스트

---
+++ src/test/java/lotto/validation/ValidatorTest.java
@@ -0,0 +1,38 @@
+package lotto.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    Validator validator = new Validator();
+
+    @DisplayName("String 타입 변수를 Integer 로 변환하고, 실패하면 예외를 발생시킨다.")
+    @ValueSource(strings = {"Q", "DY", "type"})
+    @ParameterizedTest
+    void convertTypeAndExceptionTest(String value) {
+        Assertions.assertThatThrownBy(() -> validator.checkType(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 금액이 천원으로 나누어 떨어지지 않으면 예외를 발생시킨다.")
+    @ValueSource(ints = {5001, 4444, 500})
+    @ParameterizedTest
+    void numberRemainExceptionTest(int value) {
+        Assertions.assertThatThrownBy(() -> validator.checkRemain(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1~45 사이가 아니라면 예외를 발생시킨다.")
+    @ValueSource(ints = {0, 46, 500})
+    @ParameterizedTest
+    void numberRangeExceptionTest(int value) {
+        Assertions.assertThatThrownBy(() -> validator.checkRange(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

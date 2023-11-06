67a75152cb282efc2e40fdd42694fc51e10159f7
SongJwans <yhh5916@naver.com>
2023-11-06 22:56:42 +0900

    test: Money 예외처리 테스트 추가

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @DisplayName("로또 구입 금액이 0 이면 예외가 발생한다.")
+    @Test
+    void createMoneyIsZero() {
+        assertThatThrownBy(() -> new Money(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액이 1000으로 나누어지지 않으면 예외가 발생한다.")
+    @Test
+    void createMoneyNotDivide() {
+        assertThatThrownBy(() -> new Money(10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

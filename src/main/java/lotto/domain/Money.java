3190727ecf0f1da61039342150bba60cda393137
Han <yulmam1@naver.com>
2023-11-06 15:00:33 +0900

    feat(Money) : 1000원으로 나눠 떨어지지 않는 경우 예외처리 하는 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,6 +5,12 @@ public class Money {
     int money;
 
     public Money(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(int money) {
+        if(0 != money%1000)
+            throw new IllegalArgumentException();
     }
 }

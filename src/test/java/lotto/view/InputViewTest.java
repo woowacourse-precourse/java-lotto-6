83737141f13f1e2c075f927d449782ac2b2f4328
JiEung2 <https://github.com/JiEung2>
2023-11-06 21:16:54 +0900

    Test: 입력된 당첨 번호 문자열이 패턴에 맞지 않는 경우 예외 처리 테스트
    입력된 당첨 번호 문자열이 패턴에 맞지 않는 경우 패턴에 맞게 입력하라는 에러 메시지 출력 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -10,43 +10,69 @@ import org.junit.jupiter.api.Test;
 public class InputViewTest {
 
     @Test
-    void 입력된_로또_구입_금액이_숫자가_아닐_때(){
+    void 입력된_로또_구입_금액이_숫자가_아닐_때() {
         //given
         String buyPrice = "천원";
         //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new BuyPriceValidator(buyPrice));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new BuyPriceValidator(buyPrice));
         //then
         assertThat(e.getMessage()).isEqualTo("[ERROR] 로또 구입 금액은 숫자를 입력해야 합니다.");
     }
 
     @Test
-    void 입력된_로또_구입_금액이_1000으로_나누어_떨어지지_않을_때(){
+    void 입력된_로또_구입_금액이_1000으로_나누어_떨어지지_않을_때() {
         //given
         String buyPrice = "12500";
         //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new BuyPriceValidator(buyPrice));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new BuyPriceValidator(buyPrice));
         //then
         assertThat(e.getMessage()).isEqualTo("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야 합니다.");
     }
 
     @Test
-    void 입력된_당첨_번호에_중복이_있을_때(){
+    void 입력된_당첨_번호에_중복이_있을_때() {
         //given
         String winningNumber = "1,2,3,4,5,5";
         //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new WinningNumberValidator(winningNumber));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumberValidator(winningNumber));
         //then
         assertThat(e.getMessage()).isEqualTo("[ERROR] 각각 다른 숫자를 입력해야 합니다.");
     }
 
     @Test
-    void 입력된_당첨_번호가_숫자가_아닌_경우(){
+    void 입력된_당첨_번호가_숫자가_아닌_경우() {
         //given
         String winningNumber = "일이삼사오육";
         //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new WinningNumberValidator(winningNumber));
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumberValidator(winningNumber));
         //then
         assertThat(e.getMessage()).isEqualTo("[ERROR] 당첨 번호는 숫자를 입력해야 합니다.");
+    }
+
+    @Test
+    void 입력된_당첨_번호가_쉼표로_시작할_때() {
+        //given
+        String winningNumber = ",1,2,3,4,5,6";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumberValidator(winningNumber));
+        //then
+        assertThat(e.getMessage()).isEqualTo("[ERROR] 당첨 번호는 '1,2'의 형태로 입력해야 합니다.(시작과 끝은 모두 숫자)");
+    }
+
+    @Test
+    void 입력된_당첨_번호가_쉼표로_끝날_때() {
+        //given
+        String winningNumber = "1,2,3,4,5,6,";
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumberValidator(winningNumber));
+        //then
+        assertThat(e.getMessage()).isEqualTo("[ERROR] 당첨 번호는 '1,2'의 형태로 입력해야 합니다.(시작과 끝은 모두 숫자)");
     }
 
 }

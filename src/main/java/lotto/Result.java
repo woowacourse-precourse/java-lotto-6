289a7e6abbc52fe77017fc4e02b033ab2ae3b5ba
yeondori <yeonseo.jung0@gmail.com>
2023-11-07 01:04:55 +0900

    refactor: Result 순서 변경

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public enum Result {
-    FIRST_PRIZE(6, 2_000_000_000),
-    SECOND_PRIZE(5, 30_000_000),
-    THIRD_PRIZE(5, 1_500_000),
-    FOURTH_PRIZE(4, 50_000),
+    NO_PRIZE(2, 0),
     FIFTH_PRIZE(3, 5_000),
-    NO_PRIZE(2, 0);
+    FOURTH_PRIZE(4, 50_000),
+    THIRD_PRIZE(5, 1_500_000),
+    SECOND_PRIZE(5, 30_000_000),
+    FIRST_PRIZE(6, 2_000_000_000);
+
     private final int matchingNumbers;
     private final long prize;
 

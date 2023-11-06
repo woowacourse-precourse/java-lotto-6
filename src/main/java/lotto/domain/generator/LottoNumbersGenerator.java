8df9964ad277c901305ca3ac5419e521c44ebd17
dd <12202340@inha.edu>
2023-11-06 15:48:24 +0900

    refactor: LottoNumbersGenerator에서 로또 넘버를 정렬하도록 수정

--- src/main/java/lotto/domain/generator/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/generator/LottoNumbersGenerator.java
@@ -2,6 +2,7 @@ package lotto.domain.generator;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoNumbersGenerator {
     private static final int MIN = 1;
@@ -9,6 +10,7 @@ public class LottoNumbersGenerator {
     private static final int COUNT = 6;
 
     public static List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(MIN, MAX, COUNT);
+        return Randoms.pickUniqueNumbersInRange(MIN, MAX, COUNT)
+                .stream().sorted().collect(Collectors.toList());
     }
 }

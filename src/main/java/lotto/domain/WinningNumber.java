2f490c138bc5ea6aaa8b7ba19e4e992188f0931c
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-06 13:22:50 +0900

    feat: 법위 외 숫자 입력시 예외처리 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -6,11 +6,27 @@ import java.util.stream.Collectors;
 
 public class WinningNumber {
     private List<Integer> winningNumber;
+    private static final String DELIMITER = ",";
+    private static final String OUT_RANGE_ERROR_MESSAGE = "[ERROR] 범위를 벗어난 숫자 입니다.";
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
 
     public WinningNumber(String userValue) {
         this.winningNumber = Arrays
-                .stream(userValue.split(","))
+                .stream(userValue.split(DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+
+    }
+
+    void validInRangeNumber(){
+        for(int lottoNumber : winningNumber){
+            if(lottoNumber < MIN_RANGE || lottoNumber > MAX_RANGE){
+                throw new IllegalArgumentException(OUT_RANGE_ERROR_MESSAGE);
+            }
+        }
+    }
+    public List<Integer> getWinningNumber(){
+        return winningNumber;
     }
 }

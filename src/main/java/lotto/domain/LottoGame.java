d9aa4165a9b85c39d08590fda4d269a9de2ed35b
woong2e <tlswodnd12345@naver.com>
2023-11-06 21:34:06 +0900

    feat: 로또 게임 플레이중 로또 사는 기능 구현

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,2 +1,31 @@
-package lotto.domain;public class LottoGame {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoGame {
+    public final static int LOTTO_PRICE = 1000;
+
+    private LottoGame() {
+    }
+
+    public static void play() {
+        int purchaseMoney = Input.askHowManyLottoPurchase();
+        List<Lotto> lottos = buyLotto(purchaseMoney/LOTTO_PRICE);
+        Output.printLottos(lottos);
+        User user = new User(lottos);
+    }
+
+    private static List<Lotto> buyLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
 }

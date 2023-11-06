e83a02dbe58fb3f77fbfa974f67e0e2b21f8a5a8
JW0321 <digneotn123@gmail.com>
2023-11-07 00:19:11 +0900

    feat: 당첨 내역 출력 기능 수정
    2등의 경우 출력 문자열이 다르므로 Enum을 사용하여 해당하는 문자열이 출력되도록 변경

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -9,15 +9,47 @@ import lotto.util.ErrorMessage;
 
 public class LottoView { //게임 시작 메세지 출력, 구입 금액, 당첨 번호, 보너스 번호 입력 요청, 당첨 통계 및 결과 메세지 출력, 에러 메세지 출력
 
-    private static final DecimalFormat decimalFormat = new DecimalFormat("#,###");
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,###");
     private static final String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_MESSAGE = "%d개를 구매했습니다.";
     private static final String LOTTO_NUMBER_PROMPT = "당첨 번호를 입력해 주세요";
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요";
     private static final String LOTTO_STATISTICS_HEADER = "당첨 통계";
     private static final String SEPARATOR = "---";
-    private static final String COUNT_PRIZE_FORMAT = "%d개 일치 (%s원) - %d개";
+    private static final String COUNT_PRIZE_FORMAT = "%s (%s원) - %d개";
     private static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    private enum Ranks {
+        FIFTH(5, "3개 일치"),
+        FOURTH(4, "4개 일치"),
+        THIRD(3, "5개 일치"),
+        SECOND(2, "5개 일치, 보너스 볼 일치"),
+        FIRST(1, "6개 일치");
+
+        private final int rank;
+        private final String rankMessage;
+
+        Ranks(int rank, String rankMessage) {
+            this.rank = rank;
+            this.rankMessage = rankMessage;
+        }
+        private int getRank() {
+            return rank;
+        }
+
+        private String getRankMessage() {
+            return rankMessage;
+        }
+
+        private static String getRankMessageByRank(int rank) {
+            for (Ranks ranking : values()) {
+                if(ranking.getRank() == rank) {
+                    return ranking.getRankMessage();
+                }
+            }
+            return null;
+        }
+    }
 
 
     public static String readPurchaseAmount() {
@@ -45,10 +77,11 @@ public class LottoView { //게임 시작 메세지 출력, 구입 금액, 당첨
         System.out.println(SEPARATOR);
 
         lottoStatistics.forEach(statisticsEntity -> {
-            int matchCount = statisticsEntity.get("matchCount");
-            String prizeAmount = decimalFormat.format(statisticsEntity.get("prizeAmount"));
+            int rank = statisticsEntity.get("rank");
+            String rankMessage = Ranks.getRankMessageByRank(rank);
+            String prizeAmount = DECIMAL_FORMAT.format(statisticsEntity.get("prizeAmount"));
             int winningCount = statisticsEntity.get("winningCount");
-            System.out.println(String.format(COUNT_PRIZE_FORMAT, matchCount, prizeAmount, winningCount));
+            System.out.println(String.format(COUNT_PRIZE_FORMAT, rankMessage, prizeAmount, winningCount));
         });
     }
 

ecfdd00aa0b2e4d1d03942e353967584522c570b
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-06 09:11:25 +0900

    feat: 어플리케이션 진입점과 게임 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.service.LottoGeneratorService;
+import lotto.service.LottoNumberGeneratorService;
+import lotto.service.LottoRankCalculatorService;
+import lotto.service.LottoStoreService;
+import lotto.view.InputReader;
+import lotto.view.InputView;
+import lotto.view.MessageResolver;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new InputView(new InputReader());
+        OutputView outputView = new OutputView(new MessageResolver());
+        LottoRankCalculatorService lottoRankCalculatorService = new LottoRankCalculatorService();
+        LottoNumberGeneratorService lottoNumberGeneratorService = new LottoNumberGeneratorService();
+        LottoGeneratorService lottoGeneratorService = new LottoGeneratorService(lottoNumberGeneratorService);
+        LottoStoreService lottoStoreService = new LottoStoreService(lottoGeneratorService);
+
+        LottoGameController lottoGameController = new LottoGameController(inputView, outputView, lottoStoreService,
+                lottoRankCalculatorService);
+
+        lottoGameController.run();
     }
 }

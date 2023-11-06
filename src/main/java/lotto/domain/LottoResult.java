1e83baff9d65dc1992df8e88563153d0b8e24d7f
Jae-Young98 <ygy0102@gmail.com>
2023-11-07 00:09:53 +0900

    feat(LottoResult): 당첨된 등수 횟수를 저장하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -3,10 +3,21 @@ package lotto.domain;
 import lotto.domain.enums.LottoRank;
 
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
     private final Map<LottoRank, Integer> lottoResult = new EnumMap<>(LottoRank.class);
+
+    public LottoResult(List<LottoRank> ranks) {
+        putRankCount(ranks);
+    }
+
+    private void putRankCount(List<LottoRank> ranks) {
+        for (LottoRank rank : ranks) {
+            lottoResult.put(rank, lottoResult.getOrDefault(rank, 0) + 1);
+        }
+    }
 
     public Map<LottoRank, Integer> getLottoResult() {
         return lottoResult;

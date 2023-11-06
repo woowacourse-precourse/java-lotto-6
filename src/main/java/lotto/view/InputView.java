afe977ecaa942ac00ac76349fa6fda1a21e17293
ks0927 <brian_als@naver.com>
2023-11-06 16:30:59 +0900

    feat(InputView): 보너스볼을 입력 받는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
@@ -13,6 +14,11 @@ public class InputView {
 
     public String inputWinningNumber() {
         System.out.println(INPUT_WINNING_LOTTO_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputBonusBall() {
+        System.out.println(INPUT_BONUS_BALL_MESSAGE);
         return Console.readLine();
     }
 }

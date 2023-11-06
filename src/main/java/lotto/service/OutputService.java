bb2cc39ee41275baa89360a15c3e505346e40343
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-06 16:36:58 +0900

    feat: OutputService에 빈 줄 출력 로직 추가

--- src/main/java/lotto/service/OutputService.java
+++ src/main/java/lotto/service/OutputService.java
@@ -6,9 +6,10 @@ import lotto.domain.MatchResult;
 import java.util.List;
 import java.util.Map;
 
-import static lotto.domain.MatchResult.*;
+import static lotto.domain.MatchResult.NONE;
 import static lotto.domain.constant.StringConstant.*;
 
+@SuppressWarnings("SameParameterValue")
 public class OutputService {
     public static void printExpenseInputForm() {
         System.out.println(EXPENSE_INPUT_FORM_MESSAGE);
@@ -16,6 +17,8 @@ public class OutputService {
 
 
     public static void printPurchaseResult(int ticketNum, List<Lotto> randomLottos) {
+        printBlankLines(1);
+
         printTicketNum(ticketNum);
         printRandomLottos(randomLottos);
     }
@@ -25,21 +28,29 @@ public class OutputService {
     }
 
     private static void printRandomLottos(List<Lotto> randomLottos) {
-        randomLottos.forEach(System.out::println);
+        randomLottos.forEach(lotto ->
+                System.out.println(lotto.getNumbers())
+        );
     }
 
 
     public static void printWinningNumInputForm() {
+        printBlankLines(1);
+
         System.out.println(WINNING_NUM_INPUT_FORM_MESSAGE);
     }
 
 
     public static void printBonusNumInputForm() {
+        printBlankLines(1);
+
         System.out.println(BONUS_NUM_INPUT_FORM_MESSAGE);
     }
 
 
     public static void printResult(Map<MatchResult, Integer> result, double margin) {
+        printBlankLines(1);
+
         System.out.println(RESULT_PRINT_MESSAGE);
         printLottoResult(result);
         printMargin(margin);
@@ -50,11 +61,18 @@ public class OutputService {
             String message = matchResult.getMessage();
             int num = result.get(matchResult);
 
-            System.out.println(message + num + MATCH_RESULT_UNIT_STRING);
+            if (!matchResult.equals(NONE))
+                System.out.println(message + num + MATCH_RESULT_UNIT_STRING);
         });
     }
 
     private static void printMargin(double margin) {
         System.out.println(MARGIN_MESSAGE_FRONT + margin + MARGIN_MESSAGE_REAR);
+    }
+
+
+    private static void printBlankLines(int line) {
+        for (int i = 0; i < line; i++)
+            System.out.println();
     }
 }

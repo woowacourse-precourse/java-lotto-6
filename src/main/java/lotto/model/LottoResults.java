ddcf70b19a1ada6aafed2057da9ad8827652d815
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-06 23:00:33 +0900

    feat(LottoResults): 로또 결과 관련 책임 객체 생성

---
+++ src/main/java/lotto/model/LottoResults.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+import lotto.config.Rank;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoResults {
+
+    private final Map<Rank, Long> results;
+
+    private LottoResults(Map<Rank, Long> results) {
+        this.results = Map.copyOf(results);
+    }
+
+    public static LottoResults from(Map<Rank, Long> results) {
+        return new LottoResults(results);
+    }
+
+    public Map<Rank, Long> getResults() {
+        return results;
+    }
+
+    public long calculateTotalPrize() {
+        return results.entrySet().stream()
+                .filter(entry -> entry.getKey() != Rank.NONE)
+                .mapToLong(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
+                .sum();
+    }
+}

2d68cecc58e833e7c268b3c7093b83af9110f144
voyager003 <miraclex000@gmail.com>
2023-11-06 21:36:01 +0900

    feat(OutputView): add method
    ADD printEarningRate() method
    - method: print earning rate

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -55,4 +55,8 @@ public class OutputView {
             }
         }
     }
+
+    public static void printEarningRate(double earningRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", earningRate);
+    }
 }

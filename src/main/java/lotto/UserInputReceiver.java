5386111c794e7d5e281c3897e91bedddd16dc392
hoyun06 <hoyun06@gmail.com>
2023-11-06 13:37:42 +0900

    feat: UserInputReceiver 클래스 구현

---
+++ src/main/java/lotto/UserInputReceiver.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInputReceiver {
+
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static String receivePurchaseAmount() {
+        return getUserInput(PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public static String receiveWinningNumber() {
+        return getUserInput(WINNING_NUMBER_MESSAGE);
+    }
+
+    public static String receiveBonusNumber() {
+        return getUserInput(BONUS_NUMBER_MESSAGE);
+    }
+
+    private static String getUserInput(String message) {
+        System.out.println(message);
+        return Console.readLine();
+    }
+
+}

a919d3e4f2536e4bd878acab896962c866609486
KWY <wodrk1001@gmail.com>
2023-11-07 00:40:38 +0900

    [feat] 로또 게임 application 내 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoModel;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
 public class Application {
+    private static final LottoOutputView lottoOutputView = new LottoOutputView();
+    private static final LottoInputView lottoInputView = new LottoInputView();
+    private static final LottoModel lottoModel = new LottoModel();
+    private static final LottoController lottoController = new LottoController(lottoInputView, lottoOutputView, lottoModel);
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        lottoController.startGame();
     }
 }

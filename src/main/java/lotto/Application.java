240f717391f177467fe96583013712c205ea8979
oyeong011 <oyeong011@gmail.com>
2023-11-06 18:20:13 +0900

    fork : 우테코문제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int amountOfLotto = Integer.parseInt(br.readLine());
+        String[] winningNumber = br.readLine().split(",");
+        String bonusNumber = br.readLine();
+
+
+        System.out.println("amountOfLotto " + amountOfLotto);
+        System.out.println("winningNumber " + winningNumber);
+        System.out.println("bonusNumber " + bonusNumber);
     }
 }

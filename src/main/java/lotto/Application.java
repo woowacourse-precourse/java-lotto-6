bf7c0cd6b6e22eb3bac0990e89ea7d712192a541
grax <grax@192.168.35.119>
2023-11-07 03:52:20 +0900

    feat(lotto): add codes to connect with controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,10 +2,13 @@ package lotto;
 
 import java.util.List;
 
+import lotto.controller.GameController;
+
 
 public class Application {
     public static void main(String[] args) {
-    	
+    	GameController controller = new GameController();
+    	controller.playLotto();
         
     }
 }

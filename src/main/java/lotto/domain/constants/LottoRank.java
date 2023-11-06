a779a537751e2c00624e8d7366c006ad34bbbad8
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 18:21:02 +0900

    feat: 로또 당첨 결과에 쓰일 enum 추가

---
+++ src/main/java/lotto/domain/constants/LottoRank.java
@@ -0,0 +1,33 @@
+package lotto.domain.constants;
+
+public enum LottoRank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(6, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    LOSE(2, false, 0),
+    ;
+
+    private final int count;
+    private final boolean bonus;
+    private final int winningAmount;
+
+    LottoRank(int count, boolean bonus, int winningAmount) {
+        this.count = count;
+        this.bonus = bonus;
+        this.winningAmount = winningAmount;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    public int getWinningAmount() {
+        return winningAmount;
+    }
+}

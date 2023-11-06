ff8200afe20609ddc831401735461922c4a006e0
chansoo <chansoo38@gmail.com>
2023-11-06 16:55:25 +0900

    <Refactor> Validator - 각 예외상황에 대한 메세지를 던지도록 수정한다.

--- src/main/java/lotto/Domain/Validator/UserValidator.java
+++ src/main/java/lotto/Domain/Validator/UserValidator.java
@@ -4,17 +4,26 @@ import static lotto.Util.Constants.LOTTO_PRICE_UNIT;
 
 import java.util.List;
 import lotto.Domain.Lotto;
+import lotto.Util.ExceptionMessageFormatter;
+import lotto.Util.LottoGameException;
 
 public class UserValidator {
+    private UserValidator() {
+
+    }
+
     public static void doValidate(int money, List<Lotto> lottos) {
         if (!isMoneyOverLottoPrice(money)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(
+                    ExceptionMessageFormatter.makeMessage(LottoGameException.LOWER_THAN_LOTTO_PRICE.getMessage()));
         }
         if (!isMoneyDividedByLottoPriceUnit(money)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(
+                    ExceptionMessageFormatter.makeMessage(LottoGameException.NOT_DIVIDED_BY_LOTTO_UNIT.getMessage()));
         }
         if (!isLottoCountSameUnitOfMoney(money, lottos)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionMessageFormatter.makeMessage(
+                    LottoGameException.NOT_COINCIDE_LOTTO_WITH_MONEY.getMessage()));
         }
     }
 

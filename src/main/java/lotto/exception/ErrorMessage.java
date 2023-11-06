0d3409a60864f27a461dabc65ed70a5c31fa158b
JoonKyo <joonkyolee@gmail.com>
2023-11-06 17:08:07 +0900

    feat(ErrorMessage): 초기 입력에 대한 에러 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,2 +1,17 @@
-package lotto.exception;public enum ErrorMessage {
+package lotto.exception;
+
+public enum ErrorMessage {
+    INPUT_IS_NULL("null 값이 입력 되었습니다."),
+    INPUT_IS_EMPTY("빈 값이 입력 되었습니다.");
+
+    private static final String ERROR_HEAD = "[ERROR] ";
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return ERROR_HEAD + errorMessage;
+    }
 }

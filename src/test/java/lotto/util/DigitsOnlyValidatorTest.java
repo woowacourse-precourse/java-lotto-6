704eba422ceb5b5585016079b2e34b45619a813c
soochan <tncksdl05@gmail.com>
2023-11-06 15:33:56 +0900

    test(Validator): validator util 테스트 추가

---
+++ src/test/java/lotto/util/DigitsOnlyValidatorTest.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DigitsOnlyValidatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "123abc", "123한글", "12.34", "123 ", " 123", "1,000"})
+    void validateDigitsOnlyToFail(String input) {
+        assertThatThrownBy(() -> DigitsOnlyValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "5000", "45"})
+    void validateDigitsOnlyToSuccess(String input) {
+        assertDoesNotThrow(() -> DigitsOnlyValidator.validate(input));
+    }
+}
---
+++ src/test/java/lotto/util/DigitsWithCommaSeparatedValidatorTest.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DigitsWithCommaSeparatedValidatorTest {
+    @ParameterizedTest
+    @ValueSource(strings = {",", "123,", ",123", " ", "", "12a,3", "23 ,45"})
+    void validateDigitsWithCommaSeparatedToFail(String input) {
+        assertThatThrownBy(() -> DigitsWithCommaSeparatedValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "23"})
+    void validateDigitsWithCommaSeparatedToSuccess(String input) {
+        assertDoesNotThrow(() -> DigitsWithCommaSeparatedValidator.validate(input));
+    }
+}

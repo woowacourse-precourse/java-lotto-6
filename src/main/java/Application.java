cec5dc5e1ad7910783a86c6f735ac901554d79ea
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-06 23:58:01 +0900

    feat(Lotto): 중복 검사 기능 추가
    - feat(Lotto): 중복 검사 기능을 추가하고 LottoTest 작동 확인
    - refactor(Application): Application을 lotto 패키지 밖으로 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,3 @@
-package lotto;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +19,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

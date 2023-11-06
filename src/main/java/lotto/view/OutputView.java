af2b500fff4f0e790a5ceae04ec1f559b6ca8973
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 15:54:25 +0900

    feat : 발급된 로또들을 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,18 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.List;
+
 public class OutputView {
 
     public void printPurchaseCount(int purchaseCount) {
         System.out.printf("%s개를 구매했습니다.\n", purchaseCount);
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

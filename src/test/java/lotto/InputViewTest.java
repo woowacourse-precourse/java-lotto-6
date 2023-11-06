4e76664b7985323e24292a50c3e9cca05fdb676b
JongMinCh0i <wonderland314592@gmail.com>
2023-11-06 21:26:01 +0900

    test(InputView): 문자열 입력 테스트를 활용한 테스트 코드 재작성

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -3,62 +3,79 @@ package lotto;
 import lotto.view.InputView;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("로또 금액 입력에 대해서 검증한다.")
+@DisplayName("사용자의 입력에 대해서 검증한다.")
 public class InputViewTest {
 
-    private static InputView getInputView() {
-        InputView inputView = new InputView();
-        return inputView;
+    private final NsTestExtend nsTestExtend;
+
+    private InputViewTest() {
+        this.nsTestExtend = new NsTestExtend();
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1000", "2000", "3000", "4000", "10000", "100000"})
-    @DisplayName("양의 정수만 입력받을 수 있다.")
-    void validateInteger_positiveInteger_notThrowAnyException(String strings) {
-        Assertions.assertThatCode(() -> getInputView().validateInteger(strings))
-                .doesNotThrowAnyException();
+    private InputView getInputView() {
+        return new InputView();
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"-", "ㄱ", "가나", "피자"})
-    @DisplayName("문자 입력값은 예외가 발생한다.")
-    void validateInteger_invalidCharacter_exceptionThrown(String strings) {
-        Assertions.assertThatThrownBy(() -> getInputView().validateInteger(strings))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    @DisplayName("구입할 로또 금액를 검증한다.")
+    @Nested
+    public class ValidationLottoPrice {
+        @ParameterizedTest
+        @ValueSource(strings = {"1000", "2000", "3000", "4000", "10000", "100000"})
+        @DisplayName("양의 정수만 입력받을 수 있다.")
+        void validateInteger_positiveInteger_notThrowAnyException(String strings) {
+            nsTestExtend.inputTestValue(strings);
+            Assertions.assertThatCode(() -> getInputView().askPrice())
+                    .doesNotThrowAnyException();
+        }
 
-    @ParameterizedTest
-    @ValueSource(strings = {" ", "", "80 00", "10 000", "10000 ", "\t", "\n"})
-    @DisplayName("공백은 예외가 발생한다.")
-    void validateBlankAndEmptyInteger_blank_empty_exceptionThrown(String strings) {
-        Assertions.assertThatThrownBy(() -> getInputView().validateBlankAndEmptyInteger(strings))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1.1", "1.2", "1.3", "1.000", "2.0000"})
-    @DisplayName("실수 값은 예외가 발생한다.")
-    void validateInteger_floating_point_number_exceptionThrown(String strings) {
-        Assertions.assertThatThrownBy(() -> getInputView().validateInteger(strings))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @ParameterizedTest
+        @ValueSource(strings = {"-", "ㄱ", "가나", "피자"})
+        @DisplayName("문자 입력값은 예외가 발생한다.")
+        void validateInteger_invalidCharacter_exceptionThrown(String strings) {
+            nsTestExtend.inputTestValue(strings);
+            Assertions.assertThatThrownBy(() -> getInputView().askPrice())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"/", ".", ",", "$", "#"})
-    @DisplayName("특수문자 입력값은 예외가 발생한다.")
-    void validateInteger_special_character_exceptionThrown(String strings) {
-        Assertions.assertThatThrownBy(() -> getInputView().validateInteger(strings))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @ParameterizedTest
+        @ValueSource(strings = {" ", "80 00", "10 000", "10000 ", "\t", "\n"})
+        @DisplayName("공백은 예외가 발생한다.")
+        void validateBlankAndEmptyInteger_blank_empty_exceptionThrown(String strings) {
+            nsTestExtend.inputTestValue(strings);
+            Assertions.assertThatThrownBy(() -> getInputView().askPrice())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @ParameterizedTest
-    @ValueSource(ints = {-1, -100, -123, -1000, -10000, 0})
-    @DisplayName("0 이하의 입력값은 예외가 발생한다.")
-    void validateNegativeInteger_negative_integer_exceptionThrown(int ints) {
-        Assertions.assertThatThrownBy(() -> getInputView().validateNegativeIntegerAndZero(ints))
-                .isInstanceOf(IllegalArgumentException.class);
+        @ParameterizedTest
+        @ValueSource(strings = {"1.1", "1.2", "1.3", "1.000", "2.0000"})
+        @DisplayName("실수 값은 예외가 발생한다.")
+        void validateInteger_floating_point_number_exceptionThrown(String strings) {
+            nsTestExtend.inputTestValue(strings);
+            Assertions.assertThatThrownBy(() -> getInputView().askPrice())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"/", ".", ",", "$", "#"})
+        @DisplayName("특수문자 입력값은 예외가 발생한다.")
+        void validateInteger_special_character_exceptionThrown(String strings) {
+            nsTestExtend.inputTestValue(strings);
+            Assertions.assertThatThrownBy(() -> getInputView().askPrice())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {-1, -100, -123, -1000, -10000, 0})
+        @DisplayName("0 이하의 입력값은 예외가 발생한다.")
+        void validateNegativeInteger_negative_integer_exceptionThrown(int ints) {
+            nsTestExtend.inputTestValue(ints);
+            Assertions.assertThatThrownBy(() -> getInputView().askPrice())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }

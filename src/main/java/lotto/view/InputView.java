1b7049a916bed6e85096ffd2441ee1058551c663
acycoco <pink10257@naver.com>
2023-11-06 21:02:35 +0900

    feat(InputView): 당첨번호와 보너스 번호 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private final String PAID_AMOUNT_REQUEST_PROMPT = "구입금액을 입력해 주세요.";
+    private final String WINNING_NUMBERS_REQUEST_PROMPT = "당첨 번호를 입력해 주세요.";
+    private final String BONUS_NUMBER_REQUEST_PROMPT = "보너스 번호를 입력해 주세요.";
 
     private final OutputView outputView;
 
@@ -14,6 +16,14 @@ public class InputView {
 
     public void requestPaidAmount() {
         request(PAID_AMOUNT_REQUEST_PROMPT);
+    }
+
+    public void requestWinningNumbers() {
+        request(WINNING_NUMBERS_REQUEST_PROMPT);
+    }
+
+    public void requestBonusNumber() {
+        request(BONUS_NUMBER_REQUEST_PROMPT);
     }
 
     private void request(String requestPrompt) {

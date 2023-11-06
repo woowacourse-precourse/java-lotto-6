d2407315a500e27504d62d1239aae055498b42ac
JisooPyo <audwl326@gmail.com>
2023-11-07 00:39:24 +0900

    feat(Lotto): 당첨 번호는 6개여야 한다.

--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -2,7 +2,8 @@ package lotto;
 
 public enum ErrorCode {
 	NOT_COMPOSED_OF_NUMBERS("[ERROR] 숫자로 이루어져 있지 않습니다."),
-	NOT_DIVISIBLE_BY_THOUSAND("[ERROR] 1000으로 나누어 떨어져야 합니다.") ;
+	NOT_DIVISIBLE_BY_THOUSAND("[ERROR] 1000으로 나누어 떨어져야 합니다."),
+	ELEMENTS_SHOULD_BE_SIX("[ERROR] 번호는 6개여야 합니다.");
 	private final String errorMessage;
 
 	ErrorCode(String errorMessage) {
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,18 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+	private void validate(List<Integer> numbers) throws IllegalArgumentException{
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException(ErrorCode.ELEMENTS_SHOULD_BE_SIX.getErrorMessage());
+		}
+	}
 
-    // TODO: 추가 기능 구현
+	// TODO: 추가 기능 구현
 }

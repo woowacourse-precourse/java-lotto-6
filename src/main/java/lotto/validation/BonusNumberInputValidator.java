84ff95bf39e042b825831f30ed11ebe10e50d034
acs0209 <annjae4676@naver.com>
2023-11-06 20:50:31 +0900

    feat: 당첨 번호 입력 검증 기능 추가

--- src/main/java/lotto/validation/BonusNumberInputValidator.java
+++ src/main/java/lotto/validation/BonusNumberInputValidator.java
@@ -1,6 +1,11 @@
 package lotto.validation;
 
+import static lotto.validation.constant.BonusNumber.NUMERIC_FORMAT_MESSAGE;
 import static lotto.validation.constant.BonusNumber.DUPLICATION_BONUS_NUMBER;
+import static lotto.validation.constant.BonusNumber.LOTTO_MAX_NUMBER;
+import static lotto.validation.constant.BonusNumber.LOTTO_MIN_NUMBER;
+import static lotto.validation.constant.BonusNumber.NUMBER_RANGE_MESSAGE;
+import static lotto.validation.constant.BonusNumber.SPLIT_MESSAGE;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,8 +21,35 @@ public class BonusNumberInputValidator {
     }
 
     private List<Integer> stringToCollection(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(SPLIT_MESSAGE.getMessage()))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    public void validateInputNumericFormat(String input) {
+        if (isNotNumericFormat(input)) {
+            throw new IllegalArgumentException(NUMERIC_FORMAT_MESSAGE.getMessage());
+        }
+    }
+
+    private boolean isNotNumericFormat(String input) {
+        return !input.chars().allMatch(Character::isDigit);
+    }
+
+    public void validateNumberRange(String input) {
+        int number = Integer.parseInt(input);
+        isValidNumberRange(number);
+    }
+
+    private void isValidNumberRange(int number) {
+        if (number < LOTTO_MIN_NUMBER.getNumber() || number > LOTTO_MAX_NUMBER.getNumber()) {
+            throw new IllegalArgumentException(NUMBER_RANGE_MESSAGE.getMessage());
+        }
+    }
+
+    public void validateBonusNumberInput(String winningNumbers, String input) {
+        validateInputNumericFormat(input);
+        validateNumberRange(input);
+        validateBonusNumberDuplication(winningNumbers, input);
     }
 }
--- src/main/java/lotto/validation/constant/BonusNumber.java
+++ src/main/java/lotto/validation/constant/BonusNumber.java
@@ -2,15 +2,28 @@ package lotto.validation.constant;
 
 public enum BonusNumber {
 
-    DUPLICATION_BONUS_NUMBER("[ERROR] 보너스 번호와 당첨 번호는 중복 될 수 없습니다.");
+    DUPLICATION_BONUS_NUMBER("[ERROR] 보너스 번호와 당첨 번호는 중복 될 수 없습니다."),
+    NUMBER_RANGE_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NUMERIC_FORMAT_MESSAGE("[ERROR] 숫자만 입력할 수 있습니다."),
+    SPLIT_MESSAGE(","),
+    LOTTO_MIN_NUMBER(1),
+    LOTTO_MAX_NUMBER(45);
 
     private String message;
+    private int number;
 
     BonusNumber(final String message) {
         this.message = message;
     }
+    BonusNumber(final int number) {
+        this.number = number;
+    }
 
     public String getMessage() {
         return message;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }

4656ddfa33a049e5e2b3dfa8a19719e4f678f16a
JiEung2 <https://github.com/JiEung2>
2023-11-06 23:11:24 +0900

    Feat: insertWinningNumber, convertToList, convertToIntList 기능 구현
    - 당첨 번호를 입력받아 Intger 리스트로 반환하는 insertWinningNumber 메서드 구현
    - 문자열을 리스트로 변환시켜주는 convertToList 메서드 구현
    - String 리스트를 Integer 리스트로 변환시켜주는 convertToIntList 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.model.WinningNumberValidator;
 
 public class InputView {
 
@@ -11,5 +13,22 @@ public class InputView {
 
     public static int convertToInt(String buyPrice) {
         return Integer.parseInt(buyPrice);
+    }
+
+    public List<Integer> insertWinningNumber() {
+        String winningNumber = Console.readLine();
+        new WinningNumberValidator(winningNumber);
+        return convertToList(winningNumber);
+    }
+
+    private List<Integer> convertToList(String winningNumber) {
+        List<String> numbers = List.of(winningNumber.split(","));
+        return convertToIntList(numbers);
+    }
+
+    private List<Integer> convertToIntList(List<String> numbers) {
+        return numbers.stream()
+                .map(Integer::parseInt)
+                .toList();
     }
 }

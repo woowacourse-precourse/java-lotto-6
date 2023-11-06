3c8ad55fb755fa994d735041a8dc8d3d6503707b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-07 03:39:02 +0900

    test : 로또 개수 처리 테스트 코드 작성

---
+++ src/test/java/lotto/domain/purchase/lottocount/LotteryCountServiceTest.java
@@ -0,0 +1,34 @@
+package lotto.domain.purchase.lottocount;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LotteryCountServiceTest {
+    private LotteryCountService lotteryCountService;
+
+    @BeforeEach
+    void setUp() {
+        lotteryCountService = new LotteryCountService();
+    }
+
+    @AfterEach
+    void tearDown() {
+        lotteryCountService = null;
+    }
+
+    @ParameterizedTest
+    @DisplayName("정책에 따라, 금액을 입력하면 티켓 개수를 반환합니다.")
+    @ValueSource(strings = {"1000"})
+    void 천원단위_1이상_정수_금액(String cashInput) {
+        // WHEN
+        Integer lottoCount = lotteryCountService.getLottoCount(cashInput);
+
+        // THEN
+        assertEquals(1, lottoCount);
+    }
+}

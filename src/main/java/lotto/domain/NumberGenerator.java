e98c2436ee97e93d94f6bd5a129cd48a26e90cf3
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 23:03:41 +0900

    refactor: createRandomLottoNumbers() 메소드 분리

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.utils.Constants;
@@ -10,14 +11,14 @@ public class NumberGenerator {
     }
 
     public List<Integer> createRandomLottoNumbers() {
-        List<Integer> numbers = generateUniqueRandomNumbers();
+        List<Integer> numbers = createRandomNumbersInRange(
+                Constants.LOTTO_MIN_NUMBER, Constants.LOTTO_MAX_NUMBER, Constants.LOTTO_NUMBER_COUNTS);
         sortNumbers(numbers);
         return numbers;
     }
 
-    private List<Integer> generateUniqueRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(
-                Constants.LOTTO_MIN_NUMBER, Constants.LOTTO_MAX_NUMBER, Constants.LOTTO_NUMBER_COUNTS);
+    private List<Integer> createRandomNumbersInRange(int min, int max, int count) {
+        return new ArrayList<>(Randoms.pickUniqueNumbersInRange(min, max, count));
     }
 
     private void sortNumbers(List<Integer> numbers) {

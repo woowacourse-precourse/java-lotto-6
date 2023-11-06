4fa8fb683063e08b2996cf28468e6980c6180bfe
prao <pch5521@gmail.com>
2023-11-06 18:20:11 +0900

    refactor(LottoException): 필요없는 구문 제거(Console.close())

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -1,15 +1,12 @@
 package lotto.exception;
 
 
-import camp.nextstep.edu.missionutils.Console;
-
-public class LottoException extends IllegalArgumentException{
+public class LottoException extends IllegalArgumentException {
     private LottoException(ErrorMessage errorMessage) {
         super(errorMessage.getMessage());
     }
 
     public static LottoException of(ErrorMessage errorMessage) {
-        Console.close();
         return new LottoException(errorMessage);
     }
 }

f1bf47208f1355c66a29812102511db85f778529
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 04:15:35 +0900

    feat : 당첨번호 입력받는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,9 @@
 package lotto;
 
 import java.util.List;
-
 public class Lotto {
+    private static final int NUM_OF_LOTTO =6;
+    private static final String NUMBER_SIZE_ERROR = "[ERROR] 당첨 번호는 "+NUM_OF_LOTTO+"개 입력 가능합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +12,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != NUM_OF_LOTTO) {
+            throw new IllegalArgumentException(NUMBER_SIZE_ERROR);
         }
     }
 
     // TODO: 추가 기능 구현
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,19 +1,42 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     private static final String INPUT_LOTTO_AMOUNT ="구입금액을 입력해 주세요.";
     private static final String TYPE_ERROR ="숫자가 아닌 값을 입력하셨습니다.";
-
+    private static final String INPUT_WINNING_NUMBERS ="당첨 번호를 입력해주세요.";
+    private static List<Integer> winningNumbers;
     public static int inputPlayerAmount(){
         System.out.println(INPUT_LOTTO_AMOUNT);
         return convertToInt(Console.readLine());
     }
 
-    public static int convertToInt(String inputAmount)
-    {
+    public static int convertToInt(String inputAmount) {
         return Integer.parseInt(inputAmount);
     }
+
+    public static List<Integer> inputWinningNumbers(){
+        System.out.println(INPUT_WINNING_NUMBERS);
+        return  convertToList(Console.readLine());
+    }
+
+    private static List<Integer> convertToList(String inputNumbers){
+        String [] result= inputNumbers.split(",");
+        winningNumbers= new ArrayList<>();
+        for(int i=0; i<result.length; i++){
+            winningNumbers.add(Integer.parseInt(result[i]));
+        }
+        return winningNumbers;
+
+
+    }
+
+
+
 
 
 }

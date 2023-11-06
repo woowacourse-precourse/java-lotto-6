228d000a35c850a064f3aa616e3954240e87a8fa
gimdongha <d11210920@gmail.com>
2023-11-06 15:44:15 +0900

    test(LottoTest) : 테스트케이스 추가
    - 로또 번호에 범위에 벗어난 수가 있을때 예외를 발생시키는지 검증하는 테스트 메소드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -12,6 +12,7 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
+
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -23,6 +24,10 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위 이상의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 66)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

655acfac2963b619824ffbaa23572d8f5d66647c
JAY-Winter <rubat0@naver.com>
2023-11-06 14:19:43 +0900

    test: InputNumber Test

--- src/test/java/lotto/InputNumberTest.java
+++ src/test/java/lotto/InputNumberTest.java
@@ -1,4 +1,37 @@
+package lotto;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class InputNumberTest {
-  
+
+    @Test
+    void 당첨번호를_아무것도_입력하지_않았습니다() {
+        String inputNumber = "";
+        assertThrows(IllegalArgumentException.class,
+            () -> new InputNumber(inputNumber));
+    }
+
+    @Test
+    void 당첨번호는_6자리_숫자여야_합니다() {
+        String inputNumber = "1,2,3,4,5,6,7,8,9,10";
+        assertThrows(IllegalArgumentException.class,
+            () -> new InputNumber(inputNumber)
+        );
+    }
+
+    @Test
+    void 당첨번호는_숫자로만_구성되어야_합니다() {
+        String inputNumber = "1,2,3,ㄱ,ㄴ,ㄷ";
+        assertThrows(IllegalArgumentException.class,
+            () -> new InputNumber(inputNumber));
+    }
+
+    @Test
+    void 당첨번호는_유니크한_숫자로만_구성되어야_합니다() {
+        String inputNumber = "1,2,3,1,2,3";
+        assertThrows(IllegalArgumentException.class,
+            () -> new InputNumber(inputNumber));
+    }
 }

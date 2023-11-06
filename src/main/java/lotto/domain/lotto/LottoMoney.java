0cc51b3b16a10f381a3c7356594fd46a8997d5b6
JuSeong1130 <jsmoon1130@naver.com>
2023-11-06 21:30:30 +0900

    refactor(LottoMoney) : 매직 넘버 제거 후 상수화

--- src/main/java/lotto/domain/lotto/LottoMoney.java
+++ src/main/java/lotto/domain/lotto/LottoMoney.java
@@ -2,24 +2,27 @@ package lotto.domain.lotto;
 
 public class LottoMoney {
 
+    public static final int PERCENT = 100;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int ZERO = 0;
+    private static String PRICE_EXCEPTION_MESSAGE = String.format("[Error] Money는 %d원 단위어야 합니다", LOTTO_PRICE);
     private int money;
-
     public LottoMoney(int money) {
         validate(money);
         this.money = money;
     }
 
     private void validate(int money) {
-        if (money == 0 || money % 1000 != 0) {
-            throw new IllegalArgumentException("error");
+        if (money == ZERO || money % LOTTO_PRICE != ZERO) {
+            throw new IllegalArgumentException(PRICE_EXCEPTION_MESSAGE);
         }
     }
 
     public int calculatePurchasableCount() {
-        return money / 1000;
+        return money / LOTTO_PRICE;
     }
 
     public float calculateProfitRate(float winningMoney) {
-        return (winningMoney / money) * 100;
+        return (winningMoney / money) * PERCENT;
     }
 }

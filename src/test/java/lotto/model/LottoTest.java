3b862562d4af8decd6c3d5c5fb7c76f8dfda04c2
hoyeon <highoption@naver.com>
2023-11-06 22:59:56 +0900

    Test(LottoTest) : 정상 작동 케이스 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,11 +1,12 @@
-package lotto;
+package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +24,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("정상 작동 케이스")
+    void createCorrectCase() {
+        List<Integer> numbers = Arrays.asList(1, 5, 9, 4, 13, 6);
+        assertThatCode(() -> new Lotto(numbers))
+                .doesNotThrowAnyException();
+    }
+
 }

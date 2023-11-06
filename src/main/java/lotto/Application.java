af3ef1cf921efdf6a0f4649c14dce076368768f7
jwp345 <jwp135@naver.com>
2023-11-07 02:54:33 +0900

    feat(Display): 당첨 번호와 보너스 번호 입력
    유효한 int 범위의 수를 입력하지 않을 시 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         Display.readPrice();
+        Display.readWinningNumbers();
+        Display.readBonusNumber();
     }
 }
--- src/main/java/lotto/Display.java
+++ src/main/java/lotto/Display.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Display {
@@ -13,6 +17,27 @@ public class Display {
             }
             System.out.println(price / 1000 + "개를 구매했습니다.");
             return price;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static List<Integer> readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = readLine();
+        try {
+            String[] tokens = input.split(",");
+            return Arrays.stream(tokens).map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            return Integer.parseInt(readLine());
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }

b7beeca7e5b6d152442c4fb6ddb5db3d33edbc2f
hyeonjilee <skylar12200@gmail.com>
2023-11-06 14:31:08 +0900

    refactor(WinningNumbersDtoValidator) : 중복, 로또 갯수 검증 책임 Lotto 객체로 위임

--- src/main/java/lotto/util/validator/WinningNumbersDtoValidator.java
+++ src/main/java/lotto/util/validator/WinningNumbersDtoValidator.java
@@ -9,14 +9,10 @@ import java.util.Set;
 import lotto.dto.WinningNumbersDto;
 
 public class WinningNumbersDtoValidator implements Validator {
-    public static final int WINNING_NUMBERS_COUNT = 6;
-
     @Override
     public void validate(Object value) {
         validateNull((String) value);
         validateSeperator((String) value);
-        validateSize(convertToList((String) value));
-        validateDuplicates(convertToList((String) value));
     }
 
     private void validateNull(String winningNumbers) {
@@ -60,30 +56,6 @@ public class WinningNumbersDtoValidator implements Validator {
         if (winningNumbers.endsWith(WINNING_NUMBERS_SEPARATOR.getSeperator())) {
             throw new IllegalArgumentException("당첨 번호 입력 형식에 맞지 않습니다.");
         }
-    }
-
-    private void validateSize(List<String> winningNumbers) {
-        if (!isValideSize(winningNumbers)) {
-            throw new IllegalArgumentException("당첨 번호는 " + WINNING_NUMBERS_COUNT + "개가 입력되어야 합니다.");
-        }
-    }
-
-    private boolean isValideSize(List<String> winningNumbers) {
-        return winningNumbers.size() == WINNING_NUMBERS_COUNT;
-    }
-
-    private void validateDuplicates(List<String> winningNumbers) {
-        if (hasDuplicates(winningNumbers)) {
-            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다.");
-        }
-    }
-
-    private boolean hasDuplicates(List<String> winningNumbers) {
-        return new HashSet<>(winningNumbers).size() != winningNumbers.size();
-    }
-
-    private List<String> convertToList(String winningNumbers) {
-        return Arrays.asList(winningNumbers.split(WINNING_NUMBERS_SEPARATOR.getSeperator()));
     }
 
     @Override

c1b334900d02116de11e1449d75da3e4347655a1
gusgh075 <gusgh075@naver.com>
2023-11-06 10:47:09 +0900

    FEATURES(LottoView) : 우승번호 입력 기능 구현

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +13,15 @@ public class LottoView {
         int money = Integer.parseInt(Console.readLine());
         return money;
     }
-
+    public Lotto inputWinningLotto(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] sNumbers = Console.readLine().split(",");
+        List<Integer> winningLottoNumber=new ArrayList<>();
+        for(int i=0;i<sNumbers.length;i++){
+            winningLottoNumber.add(Integer.parseInt(sNumbers[i]));
+        }
+        return new Lotto(winningLottoNumber);
+    }
     public void showLottos(LottoRepository lottoRepository){
         List<Lotto> lottos = lottoRepository.getLottos();
         lottos.forEach(lotto->System.out.println(Arrays.toString(lotto.getNumbers().toArray())));

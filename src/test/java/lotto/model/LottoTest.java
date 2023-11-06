45bb78c400f5fa5f04f6462ca96d319d3b5e7718
jalddak <jalddak98@gmail.com>
2023-11-06 18:22:41 +0900

    test - Lotto: compareLotto() >> 1등 로또와 비교하는 메서드 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -6,8 +6,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -50,5 +54,53 @@ class LottoTest {
         List<Integer> result = lotto.getNumbers();
         //then
         assertThat(result).isEqualTo(expect);
+    }
+
+    @DisplayName("1등 로또와 비교해서 얼마나 맞았는지 체크")
+    @ParameterizedTest
+    @MethodSource({"provideLottoAndFirstPrizeAndExpect_1", "provideLottoAndFirstPrizeAndExpect_2",
+            "provideLottoAndFirstPrizeAndExpect_3"})
+    void 테스트_compareLotto(Lotto myLotto, Lotto firstPrizeLotto, int expect) {
+        //when
+        int result = myLotto.compareLotto(firstPrizeLotto);
+        //then
+        assertThat(result).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> provideLottoAndFirstPrizeAndExpect_1() {
+        Lotto firstPrizeLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(Arrays.asList(1, 8, 2, 6, 5, 7)));
+        lottos.add(new Lotto(Arrays.asList(1, 8, 2, 9, 5, 7)));
+        lottos.add(new Lotto(Arrays.asList(1, 8, 43, 9, 5, 7)));
+        return Stream.of(
+                Arguments.of(lottos.get(0), firstPrizeLotto, 4),
+                Arguments.of(lottos.get(1), firstPrizeLotto, 3),
+                Arguments.of(lottos.get(2), firstPrizeLotto, 2)
+        );
+    }
+
+    private static Stream<Arguments> provideLottoAndFirstPrizeAndExpect_2() {
+        Lotto firstPrizeLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(Arrays.asList(1, 4, 2, 6, 5, 3)));
+        lottos.add(new Lotto(Arrays.asList(1, 4, 2, 6, 5, 7)));
+        return Stream.of(
+                Arguments.of(lottos.get(0), firstPrizeLotto, 6),
+                Arguments.of(lottos.get(1), firstPrizeLotto, 6),
+                Arguments.of(lottos.get(2), firstPrizeLotto, 5)
+        );
+    }
+
+    private static Stream<Arguments> provideLottoAndFirstPrizeAndExpect_3() {
+        Lotto firstPrizeLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(Arrays.asList(12, 8, 43, 9, 5, 7)));
+        lottos.add(new Lotto(Arrays.asList(12, 8, 43, 9, 23, 7)));
+        return Stream.of(
+                Arguments.of(lottos.get(0), firstPrizeLotto, 1),
+                Arguments.of(lottos.get(1), firstPrizeLotto, 0)
+        );
     }
 }

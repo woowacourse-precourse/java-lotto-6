fe4d80162a74064e53e9ea6487c202ec92feaf56
Junobee25 <wnsdhqo@naver.com>
2023-11-07 04:45:24 +0900

    refactor(WinningLevel): Enum count 변수 추가

--- src/main/java/lotto/configuration/WinningLevel.java
+++ src/main/java/lotto/configuration/WinningLevel.java
@@ -1,17 +1,19 @@
 package lotto.configuration;
 
 public enum WinningLevel {
-    FIRST_PLACE(2000000000L,"6개 일치 (2,000,000,000원)"),
-    SECOND_PLACE(30000000L,"5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD_PLACE(1500000L,"5개 일치 (1,500,000원)"),
-    FOURTH_PLACE(50000L,"4개 일치 (50,000원)"),
-    FIFTH_PLACE(5000L,"3개 일치 (5,000원)"),
-    FALSE_PLACE(0L,"0개 일치 (0원)");
+    FIRST_PLACE(6,2000000000L,"6개 일치 (2,000,000,000원)"),
+    SECOND_PLACE(5,30000000L,"5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD_PLACE(5,1500000L,"5개 일치 (1,500,000원)"),
+    FOURTH_PLACE(4,50000L,"4개 일치 (50,000원)"),
+    FIFTH_PLACE(3,5000L,"3개 일치 (5,000원)"),
+    FALSE_PLACE(0,0L,"0개 일치 (0원)");
 
-    private final Long reward;
+    private final int count;
+    private final long reward;
     private final String result;
 
-    WinningLevel(Long reward, String result) {
+    WinningLevel(int count, long reward, String result) {
+        this.count = count;
         this.reward = reward;
         this.result = result;
     }

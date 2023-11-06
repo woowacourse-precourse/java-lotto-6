b3360c537140dfee9de615d93bf59a1519e4763a
soozzang <soozzang@inha.edu>
2023-11-06 19:38:58 +0900

    Feat(LottoService): lottos에 lotto 포장하는 메서드 생성

---
+++ src/main/java/lotto/model/service/LottoService.java
@@ -0,0 +1,18 @@
+package lotto.model.service;
+
+import lotto.model.domain.*;
+import lotto.util.LottoGenerator;
+
+public class LottoService {
+
+    public Lottos makeLottoAndLottos(User user) {
+        Lottos lottos = new Lottos();
+
+        for (int i = 0; i < user.getHavingLottosCount(); i++) {
+            Lotto lotto = new Lotto(LottoGenerator.generateRandomNumList());
+            lottos.addNewLotto(lotto);
+        }
+
+        return lottos;
+    }
+}

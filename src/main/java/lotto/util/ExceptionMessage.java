11f1d5c70722df5e2ef96f455361316969160427
noyes5 <noyes5@naver.com>
2023-11-07 00:43:18 +0900

    fix: Exception 메시지 사용자 기준으로 변경

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -4,7 +4,7 @@ import static lotto.util.Constants.LOTTO_ONE_GAME_PRICE;
 
 public enum ExceptionMessage {
     INVALID_LOTTO_SIZE("로또 번호는 6개로 구성되어야 합니다."),
-    INVALID_INPUT_NUMERIC("입력값은 숫자만 들어올 수 있습니다."),
+    INVALID_INPUT_NUMERIC("입력값은 숫자만 입력 할 수 있습니다."),
     INVALID_UNIT_OF_BUDGET(String.format("금액은 %d원 단위로 입력해야 합니다.", LOTTO_ONE_GAME_PRICE));
 
     public static final String BASE_MESSAGE = "[ERROR] %s";

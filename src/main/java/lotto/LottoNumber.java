4de068ed91891efd99874331cdca4259e41bf4d6
shin5774 <babo5774@naver.com>
2023-11-07 02:21:32 +0900

    Feat: 로또 번호 클래스 구현
    - 로또번호를 의미하는 LottoNumber클래스
    - 해당 클래스는 생성시 로또범위의 검증을 하기에 해당 클래스의 변수는 로또 번호임을 입증함

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public class LottoNumber {
+    private static final int NUMBER_LOW_BOUND = 1;
+    private static final int NUMBER_HIGH_BOUND = 45;
+    private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
+    private static final String OUT_OF_RANGE_MESSAGE = "숫자가 아닌 값이 입력되었습니다.";
+
+    private final int lottoNumber;
+
+
+    public LottoNumber(long number) {
+        validate(number);
+        lottoNumber = (int) number;
+    }
+
+    private void validate(long nubmer) {
+        if (isOutOfRange(nubmer)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + OUT_OF_RANGE_MESSAGE);
+        }
+    }
+
+    private boolean isOutOfRange(long number) {
+        return number < NUMBER_LOW_BOUND || number > NUMBER_HIGH_BOUND;
+    }
+}

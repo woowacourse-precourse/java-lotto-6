8b17fc805741baefbcdb081b0d795f396061964e
Infikei <hckhong@naver.com>
2023-11-07 01:30:58 +0900

    refactor: Amount 생성자 수정
    - Amount 생성자가 정수가 아닌 문자열을 입력받도록 수정
    - 입력받은 문자열을 정수로 변환하는 메서드 추가
    - 입력받은 문자열이 정수 형식이 아닌 경우 예외 발생

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,17 +1,27 @@
 package lotto.domain;
 
 public class Amount {
-    private static final String NOT_DIVIDED_BY_PRICE_EXCEPTION_MESSAGE = "구입금액은 1000원 단위로 나누어떨어져야 합니다.";
+    private static final String NOT_INTEGER_EXCEPTION_MESSAGE = "구입금액은 정수여야 합니다.";
     private static final String NOT_POSITIVE_EXCEPTION_MESSAGE = "구입금액은 양수여야 합니다.";
+    private static final String NOT_DIVIDED_BY_PRICE_EXCEPTION_MESSAGE = "구입금액은 1000원 단위로 나누어떨어져야 합니다.";
     private static final int LOTTO_PRICE = 1000;
 
     private final int amount;
     private final int numberOfLottos;
 
-    public Amount(int amount) {
-        validate(amount);
-        this.amount = amount;
-        this.numberOfLottos = calculateNumberOfLottos(amount);
+    public Amount(String amount) {
+        amount = amount.strip();
+        this.amount = convertToInteger(amount);
+        validate(this.amount);
+        this.numberOfLottos = calculateNumberOfLottos(this.amount);
+    }
+
+    private int convertToInteger(String amount) {
+        try {
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INTEGER_EXCEPTION_MESSAGE);
+        }
     }
 
     private void validate(final int amount) {

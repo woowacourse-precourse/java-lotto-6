6fd2ae316568638a64382fc13e3cd3f768b62b65
이정우 <acg6138@naver.com>
2023-11-06 15:26:17 +0900

    feat(NumberGenerator): 숫자를 생성하는 클래스 구현

---
+++ src/main/java/lotto/NumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class NumberGenerator {
+
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 45;
+    private static final int NUMBER_SIZE = 6;
+
+    public static List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, NUMBER_SIZE);
+    }
+
+}

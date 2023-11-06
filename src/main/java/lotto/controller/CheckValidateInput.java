caafcaa333128b96b4bee909fa05d19a9543dbcd
LeeJJ07 <jkj9513@naver.com>
2023-11-06 15:02:08 +0900

    feat : 입력된 금액이 유효한 입력인지 판별하는 기능 구현
    입력된 금액이 1000원단위인지 확인하고 숫자만 입력이 된지 확인하는 기능 구현

---
+++ src/main/java/lotto/controller/CheckValidateInput.java
@@ -0,0 +1,30 @@
+package lotto.controller;
+
+public class CheckValidateInput {
+	
+	public int money(String money) {
+		
+		if(!isInteger(money))
+			throw new IllegalArgumentException();
+		if(!isUnits1000(Integer.parseInt(money)))
+			throw new IllegalArgumentException();
+
+		return Integer.parseInt(money);
+	}
+
+	private boolean isInteger(String money) {
+		try {
+			Integer.parseInt(money);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
+	
+	private boolean isUnits1000(int money) {
+		return (money%1000) == 0;	
+	}
+	
+	
+
+}

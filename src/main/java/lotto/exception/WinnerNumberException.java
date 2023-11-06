7cde6ff79423e080c01c3ad51b9ecb0ef9eae27e
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 02:43:19 +0900

    feat(WinnerNumberException) : 당첨 번호 예외처리 코드 골격 작성 - isNumber() - isComma() - isBlank() - isRightRange() - isSize() - isDuplicate()

---
+++ src/main/java/lotto/exception/WinnerNumberException.java
@@ -0,0 +1,24 @@
+package lotto.exception;
+
+import java.util.List;
+
+public class WinnerNumberException {
+
+    public void isNumber(String winnerNum) {
+    }
+
+    public void isBlank(List<Integer> winnerNum) {
+    }
+
+    public void isRightRange(List<Integer> input) {
+    }
+
+    public void isSize(List<Integer> input) {
+    }
+
+    public void isDuplicate(List<Integer> input) {
+    }
+
+    public void isComma(String input) {
+    }
+}

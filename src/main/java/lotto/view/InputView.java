904fbfe8507b59c46a5c944c7bc909c9f90d270a
sinminseok12 <sinminseok12@naver.com>
2023-11-07 03:41:47 +0900

    refactor(InputView): 보너스 번호 입력 문구 앞 공백 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,7 +10,7 @@ import static lotto.utils.Parser.parseToIntegers;
 public class InputView {
     private static final String INPUT_BUY_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WIN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "\n+보너스 번호를 입력해 주세요.";
 
     public static Integer inputPurchaseAmount() {
         System.out.println(INPUT_BUY_MONEY_MESSAGE);

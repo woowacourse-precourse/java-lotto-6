4a5991c0d4e28d7d555cfb82d487fe2fbbeeb76a
jooda00 <dragon2000a@gmail.com>
2023-11-06 13:46:18 +0900

    feat(Lotto): 로또 번호 중복 시 예외 처리 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -16,6 +16,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (numbers.stream().count() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static Lotto makeLotto() {

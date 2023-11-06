c49f353e70f556c5a1af1bae0a0f89a45da43f39
DevFrog92 <yongsu1481@gmail.com>
2023-11-06 14:25:48 +0900

    feat: implement MoneyInputReader class

---
+++ src/main/java/lotto/util/reader/MoneyInputReader.java
@@ -0,0 +1,38 @@
+package lotto.util.reader;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.math.BigDecimal;
+import lotto.util.Logger;
+import lotto.util.RegxPattern;
+
+public class MoneyInputReader implements InputReader {
+        @Override
+        public String readLine() {
+                String money;
+
+                while (true) {
+                        try {
+                                Logger.info("구입금액을 입력해 주세요.");
+                                money = Console.readLine();
+                                validate(money);
+                                break;
+                        } catch (IllegalArgumentException e) {
+                                Logger.error(e.getMessage());
+                        }
+                }
+
+                return money;
+        }
+
+        private void validate(String money) {
+                if (!money.matches(RegxPattern.ONLY_DIGIT)) {
+                        throw new IllegalArgumentException("구입금액은 1000원 이상의 숫자만 입력할 수 있습니다.");
+                }
+
+                BigDecimal target = new BigDecimal(money);
+                if (target.equals(BigDecimal.ZERO) ||
+                        !target.remainder(BigDecimal.valueOf(1000)).equals(BigDecimal.ZERO)) {
+                        throw new IllegalArgumentException("구입금액은 1000원 단위로 입력할 수 있습니다.");
+                }
+        }
+}

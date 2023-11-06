499985a17f3592dc4fe65ee406bec0f5d3ee20fc
Daseul <daseull.rep@gmail.com>
2023-11-06 18:19:22 +0900

    feat(InputView): 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,14 @@
 package lotto.view;
 
 import static lotto.validator.Validator.validateNumeric;
-import static lotto.constant.ViewConstant.ASK_PAYMENT_MESSAGE;
+import static lotto.validator.Validator.validateWinningNumbersInput;
+import static lotto.view.ViewConstant.ASK_PAYMENT_MESSAGE;
+import static lotto.view.ViewConstant.ASK_WINNING_NUMBER_MESSAGE;
+import static lotto.view.ViewConstant.WINNING_NUMBER_DELIMITER;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
@@ -13,6 +18,20 @@ public class InputView {
         String payment = Console.readLine();
         validateNumeric(payment);
         return Integer.parseInt(payment);
+    }
+
+    public static List<Integer> readWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBER_MESSAGE);
+
+        String numbers = Console.readLine();
+        validateWinningNumbersInput(numbers);
+        return toIntegerList(numbers);
+    }
+
+    private static List<Integer> toIntegerList(String numbers) {
+        return Arrays.stream(numbers.split(WINNING_NUMBER_DELIMITER.value()))
+                .map(Integer::parseInt)
+                .toList();
     }
 
 }
--- src/main/java/lotto/view/ViewConstant.java
+++ src/main/java/lotto/view/ViewConstant.java
@@ -3,6 +3,8 @@ package lotto.view;
 public enum ViewConstant {
 
     ASK_PAYMENT_MESSAGE("구입금액을 입력해 주세요."),
+    ASK_WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    WINNING_NUMBER_DELIMITER(","),
     NUMBER_PATTERN("\\d+"),
     WINNING_NUMBER_PATTERN("([\\d]+[,])*[\\d]");
 

672c554160d7f7cfaad6d139294041712c396bb8
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 08:38:19 +0900

    test(BonusNumberExceptionTest) : 보너스 숫자 예외 처리 테스트 수정 - list 인자 추가

--- src/test/java/lotto/exception/BonusNumberExceptionTest.java
+++ src/test/java/lotto/exception/BonusNumberExceptionTest.java
@@ -9,12 +9,13 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BonusNumberExceptionTest {
+    private final List<Integer> list = Arrays.asList(1,2,3,4,5,6);
 
     @Test
     @DisplayName("보너스 숫자로 숫자가 아닌 다른 것이 들어온 경우 예외 처리")
     void isNumberTest(){
         String input = "a";
-        assertThatThrownBy(() -> new BonusNumberException().isNumber(input)).isInstanceOf(
+        assertThatThrownBy(() -> new BonusNumberException(input,list)).isInstanceOf(
                 IllegalArgumentException.class);
     }
 
@@ -22,7 +23,7 @@ class BonusNumberExceptionTest {
     @DisplayName("보너스 숫자로 들어온 것이 1개가 아닐 경우")
     void isNumberOnlyOneTest(){
         String input = "1,2";
-        assertThatThrownBy(() -> new BonusNumberException().isNumberOnlyOne(input)).isInstanceOf(
+        assertThatThrownBy(() -> new BonusNumberException(input, list)).isInstanceOf(
                 IllegalArgumentException.class
         );
     }
@@ -30,8 +31,8 @@ class BonusNumberExceptionTest {
     @Test
     @DisplayName("보너스 숫자로 들어온 것이 1 미만 45 초과일 경우")
     void isRightRangeTest(){
-        String input = "48";
-        assertThatThrownBy(() -> new BonusNumberException().isRightRange(input)).isInstanceOf(
+        String input = "0";
+        assertThatThrownBy(() -> new BonusNumberException(input,list)).isInstanceOf(
                 IllegalArgumentException.class
         );
     }
@@ -40,7 +41,7 @@ class BonusNumberExceptionTest {
     @DisplayName("보너스 숫자로 들어온 것이 공백일 경우")
     void isBlankTest(){
         String input = " ";
-        assertThatThrownBy(() -> new BonusNumberException().isBlank(input)).isInstanceOf(
+        assertThatThrownBy(() -> new BonusNumberException(input,list)).isInstanceOf(
                 IllegalArgumentException.class
         );
     }
@@ -49,8 +50,7 @@ class BonusNumberExceptionTest {
     @DisplayName("보너스 숫자로 들어온 것이 당첨 숫자와 중복일 경우")
     void isDuplicateWithWinnerNumTest(){
         String bonusNum = "3";
-        List<Integer> winnerNum = Arrays.asList(1,2,3);
-        assertThatThrownBy(() -> new BonusNumberException().isDuplicateWithWinnerNum(bonusNum, winnerNum)).isInstanceOf(
+        assertThatThrownBy(() -> new BonusNumberException(bonusNum, list)).isInstanceOf(
                 IllegalArgumentException.class
         );
     }

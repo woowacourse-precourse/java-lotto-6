8ceb0695fc69979f26cbebd881bd4abd4d773518
IM-GYURI <kgulr0517@gmail.com>
2023-11-07 02:36:05 +0900

    style: match로 통일, 횟수는 number 대신 count로 수정

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -6,37 +6,37 @@ import java.util.function.BiPredicate;
 public enum Prize {
     EMPTY(0
             , 0
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber < 3),
+            , (matchLottoCount, containBonusNumber) -> matchLottoCount < 3),
     FIFTH(5_000,
             3,
-            (matchedLottoNumber, containBonusNumber) -> matchedLottoNumber == 3),
+            (matchLottoCount, containBonusNumber) -> matchLottoCount == 3),
     FOURTH(50_000,
             4,
-            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
+            (matchLottoCount, containBonusNumber) -> matchLottoCount == 4),
     THIRD(1_500_000,
             5,
-            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
+            (matchLottoCount, containBonusNumber) -> matchLottoCount == 5 && !containBonusNumber),
     SECOND(30_000_000,
             5,
-            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
+            (matchLottoCount, containBonusNumber) -> matchLottoCount == 5 && containBonusNumber),
     FIRST(2_000_000_000,
             6,
-            (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
+            (matchLottoCount, containBonusNumber) -> matchLottoCount == 6);
 
 
     private final int money;
-    private final int matchedLottoNumber;
+    private final int matchLottoCount;
     private final BiPredicate<Integer, Boolean> isMatch;
 
-    Prize(final int money, final int matchedLottoNumber, final BiPredicate<Integer, Boolean> isMatch) {
+    Prize(final int money, final int matchLottoCount, final BiPredicate<Integer, Boolean> isMatch) {
         this.money = money;
-        this.matchedLottoNumber = matchedLottoNumber;
+        this.matchLottoCount = matchLottoCount;
         this.isMatch = isMatch;
     }
 
-    public static Prize getPrize(final int matchLottoNumber, final boolean containBonusNumber) {
+    public static Prize getPrize(final int matchLottoCount, final boolean containBonusNumber) {
         return Arrays.stream(Prize.values())
-                .filter(prize -> prize.isMatch.test(matchLottoNumber, containBonusNumber))
+                .filter(prize -> prize.isMatch.test(matchLottoCount, containBonusNumber))
                 .findAny()
                 .orElse(EMPTY);
     }
@@ -44,7 +44,7 @@ public enum Prize {
         return money;
     }
 
-    public int getMatchLottoNumber() {
-        return matchedLottoNumber;
+    public int getMatchLottoCount() {
+        return matchLottoCount;
     }
 }

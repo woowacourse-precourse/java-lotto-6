d1213898857908088b15a6bdf6c0cd4ed4fe68d4
yuseonjun <seonjun0906@gmail.com>
2023-11-06 18:32:43 +0900

    test(BonusNumberTest): isMatch 메서드는 보너스 번호가 구매자 로또 번호에 포함되어 있으면 true를 반환하는 테스트 구에

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,5 +17,14 @@ class BonusNumberTest {
     void BonusNumber_Constructor_ThrowsException_For_NumberOutOfRange(int number) {
         assertThatThrownBy(() -> new BonusNumber(number))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("isMatch 메서드는 보너스 번호가 구매자 로또 번호에 포함되어 있으면 true를 반환한다.")
+    void isMatch_Method_Return_BonusNumber_Contains_BuyerLotto_True() {
+        List<Integer> buyerLotto = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(6);
+
+        assertTrue(bonusNumber.isMatch(buyerLotto));
     }
 }

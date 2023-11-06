4696a210ebeb183e195280c6e38295a486b3f1e5
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-06 14:02:10 +0900

    feature : 당첨 번호 입력 검증 및 테스트 코드 추가
    검증 내용
    - ',' 기준으로 6개의 문자가 입력되는지 확인
    - 6개의 문자가 숫자가 맞는지 확인
    - 숫자가 1~45 범위까지의 수인지 확인
    - 중복된 숫자가 없는지 확인

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.stream.Collectors.toList;
 
@@ -18,37 +20,50 @@ public class InputView {
     }
 
     private static void checkPurchaseAmountInput(String input) {
-        InputValidation.checkConvertNumber(input);
+        InputValidation.checkPurchaseAmountConvertNumber(input);
         InputValidation.checkDivideThousand(input);
         InputValidation.checkMaxRangeNumber(input);
     }
 
-    public static List<Integer> readWinningNumberList() {
+    public static List<Integer> readWinningNumbers() {
         String input = Console.readLine();
+        checkWinningNumbersInput(input);
 
-        // validation 추가 예정
-        
-        
         String[] split = input.split(",");
         return Arrays.stream(split).map(Integer::parseInt)
                 .collect(toList());
+    }
+
+    private static void checkWinningNumbersInput(String input) {
+        InputValidation.checkNumberSixCount(input);
+        InputValidation.checkWinningNumbersConvertNumber(input);
+        InputValidation.checkWinningNumbersRange(input);
+        InputValidation.checkWinningNumbersDuplication(input);
     }
 
     public static int readBonusNumber() {
         String input = Console.readLine();
 
         // validation 추가 예정
+        //InputValidation.checkBonusConvertNumber(input);
+
 
         return Integer.parseInt(input);
     }
 
     static class InputValidation {
-        private static void checkConvertNumber(String input) {
-            for (int i=0; i < input.length(); i++) {
-                char checkData = input.charAt(i);
-                if (checkData < '0' || checkData > '9') {
-                    throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력 가능합니다.");
-                }
+        private static boolean isInteger(String input) {
+            try {
+                Integer.parseInt(input);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        private static void checkPurchaseAmountConvertNumber(String input) {
+            if (!isInteger(input)) {
+                throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력 가능합니다.");
             }
         }
 
@@ -67,6 +82,61 @@ public class InputView {
                 throw new IllegalArgumentException("[ERROR] 구매 금액은 100,000원을 넘을 수 없습니다. (100장 까지 구매 가능)");
             }
         }
+
+        private static void checkNumberSixCount(String input) {
+            String[] checkData = input.split(",");
+            if (checkData.length != 6) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 ',' 기준으로 6자리를 입력해야 합니다.");
+            }
+        }
+
+        private static void checkWinningNumbersConvertNumber(String input) {
+            String[] checkNumbers = input.split(",");
+            for (String checkNumber : checkNumbers) {
+                if (!isInteger(checkNumber)) {
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 입력 가능합니다.");
+                }
+            }
+        }
+
+        private static void checkWinningNumbersRange(String input) {
+            String[] checkDatas = input.split(",");
+            for (String checkNumber : checkDatas) {
+                if (!isWinningNumberRange(checkNumber)) {
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45까지 가능합니다.");
+                }
+            }
+        }
+
+        private static boolean isWinningNumberRange(String input) {
+            int checkNumber = Integer.parseInt(input);
+
+            return checkNumber >= 1 && checkNumber <= 45;
+        }
+
+        private static void checkWinningNumbersDuplication(String input) {
+            String[] checkDatas = input.split(",");
+            if (!isDuplicate(checkDatas)) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+            }
+        }
+
+        private static boolean isDuplicate(String[] checkDatas) {
+            Set<Integer> set = new HashSet<>();
+            for (String checkData : checkDatas) {
+                set.add(Integer.parseInt(checkData));
+            }
+
+            return set.size() == 6;
+        }
+
+        /*private static void checkBonusConvertNumber(String input) {
+            if (!isInteger(input)) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
+            }
+        }*/
+
+
 
 
     }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -65,8 +65,6 @@ class InputViewTest {
         assertThatThrownBy(InputView::readPurchaseAmount).isInstanceOf(IllegalArgumentException.class);
     }
 
-
-
     @Test
     void 당첨번호_입력_테스트() {
         String inputPurchaseAmount = "1,2,3,4,5,6";
@@ -75,7 +73,7 @@ class InputViewTest {
 
         String[] split = inputPurchaseAmount.split(",");
         List<Integer> inputDataNumbers = Arrays.stream(split).map(Integer::parseInt).toList();
-        List<Integer> testWinningNumbers = InputView.readWinningNumberList();
+        List<Integer> testWinningNumbers = InputView.readWinningNumbers();
 
         assertThat(testWinningNumbers.get(0)).isEqualTo(inputDataNumbers.get(0));
         assertThat(testWinningNumbers.get(1)).isEqualTo(inputDataNumbers.get(1));
@@ -86,11 +84,106 @@ class InputViewTest {
     }
 
     @Test
+    void 당첨번호_6자리입력_예외테스트1() {
+        String inputPurchaseAmount = "1,2,3,4,5";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_6자리입력_예외테스트2() {
+        String inputPurchaseAmount = "1,2,3,4,5,7,300";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_문자입력_예외테스트() {
+        String inputPurchaseAmount = "1,2,3,4,5,a";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_공백입력_예외테스트() {
+        String inputPurchaseAmount = "1,,3,4,5,6";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_공백입력_예외테스트2() {
+        String inputPurchaseAmount = "1,3,4,5,6,";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_범위_예외테스트1() {
+        String inputPurchaseAmount = "1,46,3,4,5,6";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_범위_예외테스트2() {
+        String inputPurchaseAmount = "-1,2,3,4,5,6";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_범위_예외테스트3() {
+        String inputPurchaseAmount = "0,2,3,4,5,6";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨번호_중복_예외테스트() {
+        String inputPurchaseAmount = "1,1,3,4,5,6";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(InputView::readWinningNumbers).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 보너스번호_입력_테스트() {
         String inputPurchaseAmount = "7";
         InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
         System.setIn(inputStream);
 
+        //List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
         assertThat(InputView.readBonusNumber()).isEqualTo(Integer.parseInt(inputPurchaseAmount));
     }
+
+    /*@Test
+    void 보너스번호_문자입력_테스트() {
+        String inputPurchaseAmount = "a";
+        InputStream inputStream = new ByteArrayInputStream(inputPurchaseAmount.getBytes());
+        System.setIn(inputStream);
+
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        assertThatThrownBy(() -> InputView.readBonusNumber(winningNumbers)).isInstanceOf(IllegalArgumentException.class);
+    }*/
+
 }

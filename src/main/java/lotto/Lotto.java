2d746ffde1e39883f7884c08176e69d80d6ab491
jsyeo97 <jsyeo97@naver.com>
2023-11-06 15:50:31 +0900

    feat: 중복되지 않는 1~45까지의 6개 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicated(numbers);
+        validateRange(numbers);
+    }
+
+    private static void validateRange(List<Integer> numbers) {
+        if(numbers.stream().filter(num -> {
+            return num < 1 || num > 45;
+        }).findFirst().isPresent()) {
+            String errorMsg = "[ERROR] 로또의 당첨 번호는 1~45 사이의 숫자여야 합니다.";
+            throw new IllegalArgumentException(errorMsg);
+        }
+    }
+
+    private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            String errorMsg = "[ERROR] 로또의 당첨 번호는 6개의 숫자여야 합니다.";
+            throw new IllegalArgumentException(errorMsg);
+        }
+    }
+
+    private static void validateDuplicated(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            String errorMsg = "[ERROR] 로또의 당첨 번호는 중복되지 않아야 합니다.";
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+
 }

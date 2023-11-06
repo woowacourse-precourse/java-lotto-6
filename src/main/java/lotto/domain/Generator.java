1d233d8f0f23a69d77aef8ec967345d02bb21a4e
senzoo-oh <osj3418@gmail.com>
2023-11-06 21:01:08 +0900

    feat(lotto): implement a feature of getting amount of money from user

--- src/main/java/lotto/domain/Generator.java
+++ src/main/java/lotto/domain/Generator.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Generator {
     //상수(static final) 또는 클래스 변수
     private final int LOTTERY_COST = 1000;
@@ -15,9 +17,36 @@ public class Generator {
     //생성자
 
     //메서드
-    public int getAmountOfMoney(int money) {
-        // 1000으로 나누어 떨어지지 않으면 예외를 던짐
-        return numberOfLottery = money / LOTTERY_COST;
+    public int getAmountOfMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        while(true) {
+            try {
+                int money = isCorrectFormat();
+                numberOfLottery = isDivisible(money);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return numberOfLottery;
+    }
+
+    private int isDivisible(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력해주세요.");
+        }
+        if (money % LOTTERY_COST != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+        return money % LOTTERY_COST;
+    }
+
+    private int isCorrectFormat() {
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+        }
     }
 
     public void createLotto(int numberOfLottery) {

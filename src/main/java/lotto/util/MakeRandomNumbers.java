562fc1d6893702ff8031fa9d6e3818c074a5870a
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-07 04:31:20 +0900

    refactor: 로또 번호 정렬 방법 수정
    기존의 방법은 테스트에서 오류가 나 stream을 이용해
    정렬하는 방법으로 수정

--- src/main/java/lotto/util/MakeRandomNumbers.java
+++ src/main/java/lotto/util/MakeRandomNumbers.java
@@ -2,13 +2,15 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MakeRandomNumbers {
     public static List<Integer> getRandomNumbers(){
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        randomNumbers.sort(Comparator.naturalOrder());
+        randomNumbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
         return randomNumbers;
     }
 }

43933e66df2d2702ce0db3c5abf57dcb7eaeda25
HJLee <goodforx0147@naver.com>
2023-11-06 17:58:01 +0900

    test: add winning lotto test

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -62,6 +62,38 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 당첨로또_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,a", "8");
+            assertThat(output().contains(ERROR_MESSAGE));
+        });
+    }
+
+    @Test
+    void 당첨로또_개수_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6,7", "8");
+            assertThat(output().contains(ERROR_MESSAGE));
+        });
+    }
+
+    @Test
+    void 당첨로또_범위_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,100", "8");
+            assertThat(output().contains(ERROR_MESSAGE));
+        });
+    }
+
+    @Test
+    void 당첨로또_중복_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,5", "8");
+            assertThat(output().contains(ERROR_MESSAGE));
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});

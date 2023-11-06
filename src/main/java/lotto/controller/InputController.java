f060938a419d35af42966ac08df92ea488a3c095
shining013 <nevertheless113@naver.com>
2023-11-06 18:14:01 +0900

    refactor: quantity 모델이 로또 수량 계산하여 리턴

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -2,13 +2,16 @@ package lotto.controller;
 
 import lotto.Constants;
 import lotto.model.PurchaseAmountValidator;
+import lotto.model.Quantity;
 import lotto.view.InputView;
 
 public class InputController {
         public static int setQuantity() {
                 try {
                         PurchaseAmountValidator validation = new PurchaseAmountValidator(InputView.purchaseInput());
-                        return Integer.parseInt(validation.PURCHASE_STRING);
+                        int purchaseAmount = Integer.parseInt(validation.PURCHASE_STRING);
+                        Quantity quantity = new Quantity(purchaseAmount);
+                        return quantity.quantityNum;
                 } catch (IllegalArgumentException e) {
                         System.out.println(Constants.PURCHASE_WRONG_ERROR);
                         return setQuantity();

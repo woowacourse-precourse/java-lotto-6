15ac9a4d9b51bcfd30af47dabedb7fe00b150fd7
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 15:44:47 +0900

    feat: 수익률을 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -36,4 +36,8 @@ public class OutputView {
             }
         }
     }
+
+    public static void printEarningRate(double earningRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", earningRate);
+    }
 }

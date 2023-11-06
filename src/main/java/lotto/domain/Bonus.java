b29f9545db2b01ceb69c3d02bc8d06275e842823
Infikei <hckhong@naver.com>
2023-11-07 04:29:03 +0900

    feat: Bonus 클래스에 검증 메서드 추가

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Bonus {
     private static final String NOT_INTEGER_EXCEPTION_MESSAGE = "로또 번호는 정수여야 합니다.";
     private static final String OUT_OF_RANGE_EXCEPTION_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
@@ -7,17 +9,37 @@ public class Bonus {
     private static final int LOTTO_RANGE_BEGIN_NUMBER = 1;
     private static final int LOTTO_RANGE_END_NUMBER = 45;
 
-    private final int bonusNumber;
+    private final int number;
 
-    public Bonus(String bonusNumber, Lotto winningLotto) {
-        this.bonusNumber = convertToInteger(bonusNumber);
+    public Bonus(Lotto winningLotto, String number) {
+        this.number = convertToInteger(number);
+        validate(winningLotto, this.number);
     }
 
-    private int convertToInteger(String bonusNumber) {
+    private int convertToInteger(String number) {
         try {
-            return Integer.parseInt(bonusNumber);
+            return Integer.parseInt(number);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_INTEGER_EXCEPTION_MESSAGE);
         }
+    }
+
+    private void validate(Lotto winningLotto, int number) {
+        if (!isInLottoRange(number)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION_MESSAGE);
+        } else if (isContainedInNumbers(winningLotto.getNumbers(), number)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBERS_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isInLottoRange(int number) {
+        if (number < LOTTO_RANGE_BEGIN_NUMBER || number > LOTTO_RANGE_END_NUMBER) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isContainedInNumbers(List<Integer> numbers, int number) {
+        return numbers.contains(number);
     }
 }

4c3ef92384d1d25d6a1e517afee0b1bbaf4551e3
kimdoha <kkhhjj3393@gmail.com>
2023-11-06 19:53:21 +0900

    feat(WinningLotto): 입력 받은 당첨 번호와 보너스 번호를 저장

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+
+
+    private final Integer bonusNumber;
+
+
+    public WinningLotto(final List<Integer> numbers, final Integer bonusNumber) {
+        super(numbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningLottoNumbers() {
+        return this.getNumbers();
+    }
+
+    public Integer getBonusNumber() {
+        return this.bonusNumber;
+    }
+}

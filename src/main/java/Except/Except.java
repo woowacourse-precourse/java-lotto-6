99f8154c3ed0cbca0a628ffb666df18255a88c12
SeungGwa123 <skwan123@naver.com>
2023-11-07 02:04:19 +0900

    Fix: Fix incorrectFormatMoney
    정상 작동해야하는 경우에 return값이 제대로 오지않는 부분 수정

--- src/main/java/Except/Except.java
+++ src/main/java/Except/Except.java
@@ -7,28 +7,30 @@ public class Except {
     private static Message outOfRange = Message.moneyOutOfRange;
     private static Message invalidFormat = Message.moneyInvalidFormat;
     private static final String INTEGER_REGEX = "^-?\\d+$";
-    public int outOfRangeMoney(String money){
+
+    public int outOfRangeMoney(String money) {
         int result = outOfRange.getCode();
         Input input = new Input();
-        try{
-            if(!money.matches(INTEGER_REGEX)){
+        try {
+            if (!money.matches(INTEGER_REGEX)) {
                 throw new IllegalArgumentException();
             }
             result = Integer.parseInt(money);
             result = incorrectFormatMoney(result);
-        }catch(IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             System.out.println(outOfRange.getMessage());
         }
         return result;
     }
 
-    public int incorrectFormatMoney(int money){
+    public int incorrectFormatMoney(int money) {
         int result = invalidFormat.getCode();
-        try{
-            if(money<0||money%1000!=0){
+        try {
+            if (money < 0 || money % 1000 != 0) {
                 throw new IllegalArgumentException();
             }
-        }catch(IllegalArgumentException iae){
+            result = money;
+        } catch (IllegalArgumentException iae) {
             System.out.println(invalidFormat.getMessage());
         }
         return result;
---
+++ src/main/java/Game/Game.java
@@ -0,0 +1,2 @@
+package Game;public class Game {
+}

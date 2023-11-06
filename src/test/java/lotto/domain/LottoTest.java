5c8ddbb5d8666b665eb88002d507781ca456e1e4
Juhyorim <joohr1234@gmail.com>
2023-11-06 21:19:35 +0900

    test: Lotto 문자열 반환, 숫자 하나 비교, 숫자리스트 비교 테스트 추가
    로또번호 문자열 반환, 로또번호와 숫자 하나 비교, 로또번호와 숫자리스트 비교 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,11 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -19,10 +18,43 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또번호와 숫자들을 비교하여 포함된 숫자 개수 확인")
+    @Test
+    void lottoNumberCompareWithNumbers() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+
+        //then
+        assertThat(lotto.numberMatch(list)).isEqualTo(6);
+    }
+
+    @DisplayName("로또번호에 숫자가 포함되는지 확인")
+    @Test
+    void lottoNumberCompareWithNumber() {
+        //given
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(list);
+
+        //then
+        for (Integer number : list) {
+            assertThat(lotto.isMatch(number)).isTrue();
+        }
+        assertThat(lotto.isMatch(7)).isFalse();
+    }
+
+    @DisplayName("로또번호 문자열 반환")
+    @Test
+    void lottoNumberReturn() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+
+        //then
+        assertThat(lotto.getNumbers()).isEqualTo("1, 2, 3, 4, 5, 6");
+    }
 }

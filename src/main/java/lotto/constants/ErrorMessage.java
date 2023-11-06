03dd24657ad2033a09658d5c49dfcd0fdc025dea
GaBaljaintheroom <junsu1222@naver.com>
2023-11-06 15:51:12 +0900

    refactor(ErrorMessage): 구입 금액 에러 메시지 수정

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -2,8 +2,8 @@ package lotto.constants;
 
 public enum ErrorMessage {
     ERROR_TAG("[ERROR] "),
-    NOT_NUMERIC_ERROR("숫자를 입력해 주세요"),
-    NOT_THOUSAND_UNIT_ERROR("1000단위로 입력을 해주세요"),
+    NOT_NUMERIC_ERROR("숫자 입력이 아닙니다."),
+    NOT_THOUSAND_UNIT_ERROR("로또 구입 금액이 1000단위가 아닙니다."),
     LOTTO_NUMBER_SIZE_ERROR("로또의 개수가 6이 아닙니다."),
     LOTTO_NUMBER_RANGE_ERROR("로또 숫자 범위가 1 부터 45가 아닙니다."),
     LOTTO_NUMBER_DUPLICATE_ERROR("로또 숫자가 중복되어 있습니다."),
@@ -17,7 +17,7 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return ERROR_TAG.message + message;
+        return ERROR_TAG.message + message + "\n";
     }
 
     @Override

931ff2a0bb77dd53a203fb6c12b3b518aaca2831
jiwoo <zbqmgldjfh@gmail.com>
2023-11-06 10:11:58 +0900

    feat(InputValidator): InputValidator 구현

--- src/main/java/lotto/view/InputViewImpl.java
+++ src/main/java/lotto/view/InputViewImpl.java
@@ -8,26 +8,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.util.InputValidator.isNumeric;
+import static lotto.util.InputValidator.isValidWinningNumbers;
 
 public class InputViewImpl implements InputView {
 
     @Override
     public Money readUserPurchaseMoney() {
-        String purchaseMoney = readLine();
-        return new Money(Long.parseLong(purchaseMoney));
+        String purchaseMoney = readLine().trim();
+        if(isNumeric(purchaseMoney)) {
+            return new Money(Integer.parseInt(purchaseMoney));
+        }
+
+        throw new IllegalArgumentException("구입 금액은 숫자로 입력해주세요.");
     }
 
     @Override
-    public List<LottoNumber> readWinnerLottoNumbers() {
-        return Arrays.stream(readLine().trim().split(","))
+    public List<LottoNumber> readWinnerLottoNumbers() throws IllegalArgumentException {
+        List<LottoNumber> lottoNumbers = Arrays.stream(readLine().trim().split(","))
                 .map(Integer::parseInt)
                 .map(LottoNumber::new)
                 .collect(Collectors.toList());
+
+        if(!isValidWinningNumbers(lottoNumbers)) {
+            throw new IllegalArgumentException("당첨 번호는 6개의 중복없는 숫자로 이루어져야 합니다.");
+        }
+
+        return lottoNumbers;
     }
 
     @Override
     public LottoNumber readBonusNumber() {
-        int bonusNumber = Integer.parseInt(readLine().trim());
-        return new LottoNumber(bonusNumber);
+        String input = readLine().trim();
+        if(isNumeric(input)) {
+            return new LottoNumber(Integer.parseInt(input));
+        }
+
+        throw new IllegalArgumentException("보너스 번호는 숫자로 입력해주세요.");
     }
 }

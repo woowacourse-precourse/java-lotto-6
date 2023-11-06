0eec1ea880a468d12352a41c0d4d8b21a51c8163
shin5774 <babo5774@naver.com>
2023-11-07 03:37:30 +0900

    Refactor: Validator 범위 확인 메서드 제거
    - 해당 메서드는 LottoNumber에 포함되어졌기에 코드의 중복을 없애고자 제거

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,12 +1,9 @@
 package lotto.util;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
 public enum Validator {
     INSTANCE;
-    private static final int NUMBER_LOW_BOUND = 1;
-    private static final int NUMBER_HIGH_BOUND = 45;
 
     private static final Pattern NUMBER = Pattern.compile("[0-9]+");
     private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
@@ -20,19 +17,5 @@ public enum Validator {
 
     private boolean isNotConsistOfNumber(String value) {
         return !NUMBER.matcher(value).matches();
-    }
-
-    public void validateOutOfRange(List<Integer> numbers) {
-        List<Integer> outOfRangeNumbers = numbers.stream()
-                .filter(this::isOutOfRange)
-                .toList();
-
-        if (!outOfRangeNumbers.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public boolean isOutOfRange(long number) {
-        return number < NUMBER_LOW_BOUND || number > NUMBER_HIGH_BOUND;
     }
 }

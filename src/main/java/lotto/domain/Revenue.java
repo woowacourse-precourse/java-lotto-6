fbbcb8993d31205a73fdfe0ad5655cb8dddf1ba0
eunzinn <dhr2176@gmail.com>
2023-11-07 01:06:16 +0900

    수익률 계산 메소드 추가

--- src/main/java/lotto/domain/Revenue.java
+++ src/main/java/lotto/domain/Revenue.java
@@ -1,4 +1,30 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Revenue {
+    public static void rateOfRevenue() {
+        //수익률 출력(소수점 둘째 자리에서 반올림)
+    }
+    public static Prize checkPrize(int matchingNumber){
+        if(matchingNumber == 3) {
+            return Prize.FIFTH_PLACE;
+        }
+        else if(matchingNumber == 4) {
+            return Prize.FOURTH_PLACE;
+        }
+        else if(matchingNumber == 6) {
+            return Prize.FIRST_PLACE;
+        }
+        return Prize.NO_PLACE;
+    }
+
+    public static Prize checkBonusPrize(List<Integer> winningNumber, int bonusNumber){
+        for(int num : winningNumber){
+            if(num == bonusNumber){
+                return Prize.THIRD_PLACE;
+            }
+        }
+        return Prize.SECOND_PLACE;
+    }
 }

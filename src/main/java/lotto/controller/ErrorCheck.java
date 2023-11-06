069119d9f7d064d840c828b40cb1839913d6317b
EHCTH <lees90896@gmail.com>
2023-11-06 14:06:44 +0900

    예외 처리 담당

--- src/main/java/lotto/controller/ErrorCheck.java
+++ src/main/java/lotto/controller/ErrorCheck.java
@@ -1,2 +1,52 @@
-package lotto.controller;public class ErrorCheck {
+package lotto.controller;
+import lotto.view.ErrorMessage;
+
+import java.util.List;
+public class ErrorCheck {
+    public static void priceErrorCheck(String price) throws IllegalArgumentException {
+        int priceInt = Integer.parseInt(price);
+        if (priceInt % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.notPriceMessage());
+        }
+    }
+
+    public static void stringChangeChar(String[] choiceLottoNum) throws IllegalArgumentException {
+        for (String s : choiceLottoNum) {
+            isDigitErrorCheck(s);
+        }
+    }
+
+    public static void isDigitErrorCheck(String s) throws IllegalArgumentException {
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException(ErrorMessage.notIntegerMessage());
+            }
+        }
+    }
+
+    public static void isDuplicationErrorcheck(String choice, List<Integer> numbers) throws IllegalArgumentException {
+        Integer bonusNum = Integer.parseInt(choice);
+        if (numbers.contains(bonusNum)) {
+            throw new IllegalArgumentException(ErrorMessage.isDuplication());
+        }
+
+    }
+
+    public static void isvalidateMaxAndMinNum(String choice) {
+        Integer bonusNum = Integer.parseInt(choice);
+        if (1 > bonusNum || bonusNum > 45) {
+            throw new IllegalArgumentException(ErrorMessage.isMaxAndMinValue());
+        }
+    }
+
+    public static void isSpaceValue(String choice) {
+        if (choice.equals("")) {
+            throw new IllegalArgumentException(ErrorMessage.isSpace());
+        }
+    }
+    public static void stringArrChangeString(String[] choiceLottoNum) throws IllegalArgumentException {
+        for (String s : choiceLottoNum) {
+            isSpaceValue(s);
+        }
+    }
 }

498c944f1ceeca0e50501bb00cc5ad9c4dd11b8a
Finn <wjstodsss@naver.com>
2023-11-06 20:11:53 +0900

    test - divide 메서드 테스트

--- src/test/java/lotto/model/UtilitiesTest.java
+++ src/test/java/lotto/model/UtilitiesTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import org.junit.jupiter.api.Test;
 
+import static lotto.model.Utilities.divide;
 import static lotto.model.Utilities.inputIntegerParsing;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +13,17 @@ class UtilitiesTest {
         int expected = 8000;
 
         int result = inputIntegerParsing(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDivideWithValidValues() {
+        double dividend = 8000;
+        double divisor = 1000;
+        double expected = 8;
+
+        double result = divide(dividend, divisor);
 
         assertEquals(expected, result);
     }

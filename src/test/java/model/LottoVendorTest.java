767daa5b49f29b2df3a0da9674155e578f2e2359
jaegyeongkim <dongji_11@naver.com>
2023-11-06 12:21:26 +0900

    test: 로또 발행 기능 테스트

--- src/test/java/model/LottoVendorTest.java
+++ src/test/java/model/LottoVendorTest.java
@@ -1,8 +1,8 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +13,31 @@ public class LottoVendorTest {
     void createLottoVendorByInvalidPurchaseAmount() {
         assertThatThrownBy(() -> new LottoVendor("23"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 하나 생성 테스트")
+    @Test
+    void createOneLottoByLottoVendor(){
+        assertThat(new LottoVendor("1000").generateRandomLotto().getClass())
+                .isEqualTo(Lotto.class);
+    }
+
+    @DisplayName("로또 여러개 생성 테스트")
+    @Test
+    void createManyLottoByLottoVendor(){
+        assertThat(new LottoVendor("4000").createLottos(4).size())
+                .isEqualTo(4);
+    }
+
+    @DisplayName("로또 저장소에 로또 발행 테스트")
+    @Test
+    void createLottoRepositoryByLottoVendor(){
+        assertThat(new LottoVendor("40000").publishLotto("40000").getClass())
+                .isEqualTo(LottoRepository.class);
+    }
+    @DisplayName("올바른 로또 개수만큼 로또가 발행 되었는지 테스트")
+    @Test
+    void createLottoRepositoryByValidLottoAmount(){
+        assertThat(new LottoVendor("40000").publishLotto("40000").getSize())
+                .isEqualTo(40);
     }
 }

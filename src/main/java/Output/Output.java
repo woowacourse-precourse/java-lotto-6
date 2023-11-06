359fda47992ffe952f9db11f607bfa8ad260a558
SeungGwa123 <skwan123@naver.com>
2023-11-07 03:45:54 +0900

    Style: Update answerNumberAsk & bonusNumberAsk & lottoAllPaperPrint
    한줄 공백을 주는 부분을 수정

--- src/main/java/Output/Output.java
+++ src/main/java/Output/Output.java
@@ -18,10 +18,12 @@ public class Output {
     }
 
     public void answerNumberAsk() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public void bonusNumberAsk() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
@@ -48,6 +50,5 @@ public class Output {
             String paper = lottoBuyPaperPrint(lotto.get(i).getLotto());
             System.out.println(paper);
         }
-        System.out.println();
     }
 }

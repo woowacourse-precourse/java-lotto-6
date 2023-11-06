7579c81e5e7a4ae243ba725934799c87b762f90d
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 18:24:24 +0900

    feat(WinningPrize): 상금 정보 관리 객체 및 매칭 개수에 따른 상금 반환 기능 구현

--- src/main/java/lotto/domain/WinningPrize.java
+++ src/main/java/lotto/domain/WinningPrize.java
@@ -1,17 +1,39 @@
 package lotto.domain;
 
-public enum Rank {
-    THREE(3, 5_000),
-    FOUR(4, 50_000),
-    FIVE(5, 1_500_000),
-    FIVE_WITH_BONUS(5, 3_000_000),
-    SIX(6, 20_000_000);
+public enum WinningPrize {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
 
-    private final int match;
-    private final int price;
+    private final Integer matchingNumbers;
+    private final Integer prizeAmount;
 
-    Rank(int match, int price) {
-        this.match = match;
-        this.price = price;
+    WinningPrize(Integer matchingNumbers, Integer prizeAmount) {
+        this.matchingNumbers = matchingNumbers;
+        this.prizeAmount = prizeAmount;
+    }
+
+    public static Integer calculatePrize(Integer matchingNumbers, boolean hasBonusNumber) {
+        if (validateMatchingNumbers(matchingNumbers)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (matchingNumbers == 5 && hasBonusNumber) {
+            return SECOND.prizeAmount;
+        }
+
+        for (WinningPrize prize : WinningPrize.values()) {
+            if (prize.matchingNumbers == matchingNumbers && hasBonusNumber) {
+                return prize.prizeAmount;
+            }
+        }
+
+        return 0;
+    }
+
+    private static boolean validateMatchingNumbers(Integer matchingNumbers) {
+        return matchingNumbers < 3 || matchingNumbers > 6;
     }
 }

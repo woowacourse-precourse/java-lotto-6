5d686131db134e15007ca85934baa66947a9c463
naahnoey <naahyin20@sookmyung.ac.kr>
2023-11-06 19:20:01 +0900

    feat: 구매금액 예외 처리 - 숫자가 아닌 경우, 1000원 단위가 아닌 경우

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,6 +10,24 @@ public class Application {
     static int askPurchaseAmount() {
         System.out.println("구매금액을 입력해 주세요.");
         String input = Console.readLine();
-        return Integer.parseInt(input);
+        return validate(input);
+    }
+
+    static int validate(String input) {
+        int purchaseAmount = 0;
+        try {
+            purchaseAmount = Integer.parseInt(input);
+            if (purchaseAmount % 1000 != 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 구매금액으로 숫자 외의 값을 입력했습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 구매금액이 1,000원으로 나누어 떨어지지 않습니다.");
+        } finally {
+            askPurchaseAmount();
+        }
+
+        return purchaseAmount;
     }
 }

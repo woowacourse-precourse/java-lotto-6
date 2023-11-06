4528812c63ab8f24e1a57f2cb9ba4a1083f6833f
Chenowoo <cjsgusdn14@naver.com>
2023-11-06 22:49:55 +0900

    feat(Number): 숫자 관련 enum 클래스 기능 구현

--- src/main/java/lotto/constant/NumberUtil.java
+++ src/main/java/lotto/constant/NumberUtil.java
@@ -1,4 +1,24 @@
 package lotto.constant;
 
 public enum NumberUtil {
+
+    LOTTO_PRICE(1000),
+    START_INDEX(1),
+    END_INDEX(45),
+    LOTTO_COUNT_LIMIT(6),
+    BONUS_KEY_FIVE(5),
+    DEFAULT_VALUE(0),
+    PROFIT_PERCENT(100),
+    MAX_NUMBER(2100000000),
+    MIN_NUMBER(0);
+
+    private final int number;
+
+    NumberUtil(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }

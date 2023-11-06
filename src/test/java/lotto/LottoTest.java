ef6dc0b6b964ab1e1f65253119d5eb09a46baad2
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 15:52:44 +0900

    test : 로또번호가 오름차순으로 정렬되었는지 검증하는 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,8 +4,10 @@ import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,5 +27,16 @@ class LottoTest {
                 .hasMessage("[ERROR] 로또 번호가 중복되어서는 안됩니다.");
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("생성된 로또 객체의 로또 번호가 오름차순으로 정렬된다.")
+    @Test
+    void createLotto() {
+        // given
+        List<Integer> numbers = Arrays.asList(3, 4, 1, 6, 2, 5);
+
+        // when
+        Lotto lotto = new Lotto(numbers);
+
+        // then
+        assertThat(lotto.getNumbers()).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
 }

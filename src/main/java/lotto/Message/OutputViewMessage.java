60276f928bd97846f7f467f625e04a6175b954e9
sami355-24 <ddung355@gmail.com>
2023-11-06 15:40:41 +0900

    refactor(Message): 출력 문구 수정

--- src/main/java/lotto/Message/OutputViewMessage.java
+++ src/main/java/lotto/Message/OutputViewMessage.java
@@ -3,11 +3,8 @@ package lotto.Message;
 public enum OutputViewMessage {
     PURCHASED_LOTTO_COUNT("\n%d를 구매하셨습니다."),
     WINNING_STATS("\n당첨 통계\n---"),
-    RESULT_5TH_PRIZE("3개 일치 (5,000원) - %d개"),
-    RESULT_4TH_PRIZE("4개 일치 (50,000원) - %d개"),
-    RESULT_3RD_PRIZE("5개 일치 (1,500,000원) - %d개"),
-    RESULT_2ND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    RESULT_1ST_PRIZE("6개 일치 (2,000,000,000원) - %d개"),
+    RESULT_WITHOUT_BONUS("%d개 일치 (%d원) - %d개"),
+    RESULT_WITH_BONUS("%d개 일치, 보너스 볼 일치 (%d원) - %d개"),
     EARNING_RATE("총 수익률은 %f입니다.");
 
     private String message;
@@ -20,8 +17,8 @@ public enum OutputViewMessage {
         return message;
     }
 
-    public String getMessage(int number) {
-        return String.format(message, number);
+    public String getMessage(int numberCount, int award, int prizeCount) {
+        return String.format(message, numberCount, award, prizeCount);
     }
 
     public String getMessage(float number) {

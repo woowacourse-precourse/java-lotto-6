bb2b3b9a05eb2f5a0970b20e887144ec0fed493d
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 20:44:06 +0900

    feat: Application 로또 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import lotto.ui.InputManager;
+import lotto.ui.OutputManager;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputManager inputManager = new InputManager();
+        OutputManager outputManager = new OutputManager();
+
+        LottoGenerator lottoGenerator = LottoGenerator.from(inputManager.requestAmount());
+        List<Lotto> lottos = lottoGenerator.generate();
+        outputManager.printLottos(lottos);
+
+        List<Integer> numbers = inputManager.requestNumbers();
+        WinningLotto winningLotto = new WinningLotto(numbers, inputManager.requestBonus(numbers));
+        LottoResultChecker lottoResultChecker = new LottoResultChecker(winningLotto);
+        List<Result> results = lottoResultChecker.getResults(lottos);
+
+        outputManager.printResults(results);
     }
 }

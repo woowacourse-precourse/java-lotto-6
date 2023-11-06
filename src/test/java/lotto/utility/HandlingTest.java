94ccd3dd2071a609241842c61fc3f0013d84e52f
SeongIl <biuld1234@gmail.com>
2023-11-06 22:57:18 +0900

    test(HandlingTest): add 입력 마지막에 쉼표가 있을 때, 예외 처리

--- src/test/java/lotto/utility/HandlingTest.java
+++ src/test/java/lotto/utility/HandlingTest.java
@@ -2,6 +2,7 @@ package lotto.utility;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -11,13 +12,23 @@ class HandlingTest {
 
     @DisplayName("입력된 로또를 쉼표로 구분하여 문자열 리스트로 분리하기")
     @Test
-    void split() {
-
+    void splitLottoNumber() {
         // given
         String lotto = "1,2,3,4,5,6";
 
         // when, then
         assertThat(Handling.split(lotto)).isEqualTo(List.of("1", "2", "3", "4", "5", "6"));
+    }
+
+    @DisplayName("입력 마지막에 쉼표가 있을 때, 예외 처리")
+    @Test
+    void splitLottoNumberByLastComma(){
+        // given
+        String lotto = "1,2,3,4,5,6,";
+
+        // when, then
+        assertThatThrownBy(()->Handling.split(lotto))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }

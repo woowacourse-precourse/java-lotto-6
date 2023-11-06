9f92987dab3a8b0b3d516c9f8d9054d505143c50
minkyole <7719s@nave.com>
2023-11-06 19:35:50 +0900

    test(WinningLottoTest.java) : 보너스번호가 null인 경우 테스트 구현
    - 보너스번호가 null인 경우 테스트 구현완료

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -6,13 +6,22 @@ import java.util.List;
 import lotto.model.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class WinningLottoTest {
     @DisplayName("보너스 번호에 범위(1 ~ 45) 밖의 숫자가 있으면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
-    void createLottoByOverSize(int bonusNumber) {
+    void createWinningLottoByBonusNumberOverRange(Integer bonusNumber) {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 null인 경우 예외가 발생한다..")
+    @ParameterizedTest
+    @NullSource
+    void createWinningLottoByNullOrEmptyBonusNumber(Integer bonusNumber) {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }

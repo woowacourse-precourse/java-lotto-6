ce0de0b1572bb20b6a664de5cd7e118684aa3633
twinson37 <twinson37@hanyang.ac.kr>
2023-11-06 19:18:42 +0900

    Test: 로또 구매 금액 검증 로직 테스트 수정

--- src/test/java/lotto/LottoGameControllerTest.java
+++ src/test/java/lotto/LottoGameControllerTest.java
@@ -1,16 +1,13 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.StringTokenizer;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoGameControllerTest {
@@ -28,8 +25,9 @@ class LottoGameControllerTest {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tk = new StringTokenizer(br.readLine());
+        int money = Integer.parseInt(tk.nextToken());
 
-        assertThatThrownBy(() -> controller.validateMoneyInput(tk.nextToken()))
+        assertThatThrownBy(() -> new User(money))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

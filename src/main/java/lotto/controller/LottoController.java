77d785a92094949d167682b80381822de2c2f030
yu-teacher <yusm1231@gmail.com>
2023-11-07 03:20:56 +0900

    refactor : 검증로직 추가 및 네이밍 변경
    클래스의 형태가 아닌 일반 클래스의 형태로 들어오는 것에 대한 검증 로직을 가지고 그 뒤 service로 보내주었습니다. 네이밍을 변경하여 가독성을 올렸습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,21 @@
 package lotto.controller;
 
-import lotto.Service.LottoService;
+import static lotto.etc.ErrorConstant.LOW_THOUSAND_ERROR;
+import static lotto.etc.ErrorConstant.NOT_DIVIDE_THOUSAND_ERROR;
+import static lotto.etc.SystemConstant.THREE_ZERO;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
 import lotto.dto.BonusRequestDTO;
-import lotto.dto.LottoRequestDTO;
-import lotto.dto.LottoResponseDTO;
+import lotto.dto.CountScoreRequestDTO;
+import lotto.dto.CountScoreResponseDTO;
+import lotto.etc.Validate;
+import lotto.Service.LottoService;
 
 public class LottoController {
-    private static LottoController instance;
+    private static LottoController instance = new LottoController();
     private LottoService lottoService;
 
     private LottoController() {
@@ -16,33 +23,45 @@ public class LottoController {
     }
 
     public static LottoController getInstance() {
-        if (instance == null) {
-            instance = new LottoController();
-        }
         return instance;
     }
-
-    public Lottos lottoSell(String money){
-        return LottoService
-                .getInstance()
-                .lottoSell(money);
+    public List<Lotto> setLottoBudget(String money) {
+        int time = validate(money);
+        return lottoService.buyLotto(time);
     }
 
-    public Lotto createUserLottoNumber(String numbers){
-        return LottoService
-                .getInstance()
-                .createLotto(numbers);
+    public Lotto createUserLottoNumber(String numbers) {
+        List<Integer> userNumbers = parseList(numbers);
+        return lottoService.createLotto(userNumbers);
     }
-    public int checkBonusNumber(BonusRequestDTO bonusRequestDTO){
-        return LottoService
-                .getInstance()
-                .checkBonus(bonusRequestDTO);
+    public int createBonusNumber(BonusRequestDTO bonusRequestDTO) {
+
+        return lottoService
+                .createBonusNumber(bonusRequestDTO);
     }
 
-    public LottoResponseDTO CountScore(LottoRequestDTO lottoRequestDTO){
-        return LottoService
-                .getInstance()
-                .countScore(lottoRequestDTO);
+    public CountScoreResponseDTO countScore(CountScoreRequestDTO countScoreRequestDTO) {
+        return lottoService
+                .countScore(countScoreRequestDTO);
+    }
+    private List<Integer> parseList(String numbers) {
+        List<Integer> lottoNumbers = Arrays
+                .stream(numbers.split(","))
+                .map(Validate::validateNumber)
+                .collect(Collectors.toList());
+        return lottoNumbers;
+    }
+    private int validate(String time) {
+        if (time.length() <= 3) {
+            throw new IllegalArgumentException(LOW_THOUSAND_ERROR.toString());
+        }
+
+        String lastThreeChars = time.substring(time.length() - 3);
+        if (!lastThreeChars.equals(THREE_ZERO.toString())) {
+            throw new IllegalArgumentException(NOT_DIVIDE_THOUSAND_ERROR.toString());
+        }
+
+        return Integer.parseInt(time) / 1000;
     }
 
 }

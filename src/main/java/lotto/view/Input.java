4762bea0c8dbb70a57374020d3cd16b1d257ddf2
KimSehoon <shggm2000@gmail.com>
2023-11-07 02:50:58 +0900

    feat: getBonusNumber method 구현
    - 보너스 번호를 입력 받는 getBonusNumber method 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -19,4 +19,8 @@ public class Input {
                 .map(Integer::parseInt)
                 .toList();
     }
+
+    public int getBonusNumber(){
+        return Integer.parseInt(Console.readLine());
+    }
 }

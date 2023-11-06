0d0365d5c2eb0763997e67e823747980111b7abc
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-06 22:36:25 +0900

    test(WinningResultTest) : 당첨 번호/보너스 번호 일치 여부로 순위 계산

--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -8,6 +8,31 @@ import static org.assertj.core.api.Assertions.*;
 
 class WinningResultTest {
 
+    @DisplayName("당첨 번호와 일치하는 숫자의 갯수와 보너스 번호 일치 여부로 순위를 계산한다.")
+    @Test
+    void winRank() {
+        // given
+        WinningResult winningResult1 = new WinningResult();
+        WinningResult winningResult2 = new WinningResult();
+        WinningResult winningResult3 = new WinningResult();
+        WinningResult winningResult4 = new WinningResult();
+        WinningResult winningResult5 = new WinningResult();
+
+        // when
+        winningResult1.winRank(6, false);
+        winningResult2.winRank(5, true);
+        winningResult3.winRank(5, false);
+        winningResult4.winRank(4, false);
+        winningResult5.winRank(3, false);
+
+        // then
+        assertThat(winningResult1.getRank1()).isEqualTo(1);
+        assertThat(winningResult2.getRank2()).isEqualTo(1);
+        assertThat(winningResult3.getRank3()).isEqualTo(1);
+        assertThat(winningResult4.getRank4()).isEqualTo(1);
+        assertThat(winningResult5.getRank5()).isEqualTo(1);
+    }
+
     @DisplayName("당첨금과 구입 금액으로부터 수익률을 계산한다.")
     @Test
     void calculateEarningsRate() {

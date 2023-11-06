f79609d2e0820eae2cfe93f7f3436593944270a9
dukcode <duk9741@gmail.com>
2023-11-06 16:09:51 +0900

    refactor: 로또 당첨 enum 클래스 멤버 변경 및 클래스명 변경

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -1,17 +1,17 @@
 package lotto;
 
-public enum LottoPrizeRank {
-    FIRST(2_000_000_000L, 6, 0),
-    SECOND(30_000_000L, 5, 1),
-    THIRD(1_500_000L, 5, 0),
-    FOURTH(50_000L, 4, 0),
-    FIFTH(5_000L, 3, 0);
+public enum LottoPrize {
+    FIRST_PRIZE(2_000_000_000L, 6, 0),
+    SECOND_PRIZE(30_000_000L, 5, 1),
+    THIRD_PRIZE(1_500_000L, 5, 0),
+    FOURTH_PRIZE(50_000L, 4, 0),
+    FIFTH_PRIZE(5_000L, 3, 0);
 
     private final long winningAmount;
     private final int numNumberMatches;
     private final int numBonusNumberMatches;
 
-    LottoPrizeRank(long winningAmount, int numNumberMatches, int numBonusNumberMatches) {
+    LottoPrize(long winningAmount, int numNumberMatches, int numBonusNumberMatches) {
         this.winningAmount = winningAmount;
         this.numNumberMatches = numNumberMatches;
         this.numBonusNumberMatches = numBonusNumberMatches;

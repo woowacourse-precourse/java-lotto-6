cf04e1935c5dab40ab991fc62e43ac290e06e10b
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-06 17:57:48 +0900

    feat : 로또 구매 수량 계산에 대한 테스트 작성

--- src/test/java/lotto/LottoPlayerTest.java
+++ src/test/java/lotto/LottoPlayerTest.java
@@ -4,11 +4,27 @@ import lotto.Model.LottoPlayer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoPlayerTest {
 
 
+
+    @ParameterizedTest
+    @CsvSource({
+            "1000, 1",
+            "5000, 5",
+            "99000, 99",
+            "990000, 990"
+    })
+    public void orderLottos_CalculatesQuantityCorrectly(Integer cache,Integer answerQuantity) {
+        LottoPlayer lottoPlayer = new LottoPlayer(cache);
+
+        int purchaseQuantity = lottoPlayer.orderLottos();
+
+        Assertions.assertThat(purchaseQuantity).isEqualTo(answerQuantity);
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {0,-1, -100, -9802})
@@ -25,4 +41,6 @@ public class LottoPlayerTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력 금액은 1000원 단위로 입력되어야 합니다.");;
     }
+
+
 }

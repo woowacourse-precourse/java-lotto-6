58ee7d26f426e4c06e0f33a89ae59844d640252f
jhsseonn <hs125bt@gmail.com>
2023-11-06 19:20:48 +0900

    feat: 로또 등수 Enum 생성

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
-public enum LottoRank {
+import lotto.common.LottoFinalConsts;
+
+public enum LottoRank implements LottoFinalConsts{
+
+    FIRST(LOTTO_FIRST, LOTTO_FIRST_RETURN),
+    SECOND(LOTTO_SECOND, LOTTO_SECOND_RETURN),
+    THIRD(LOTTO_THIRD, LOTTO_THIRD_RETURN),
+    FOURTH(LOTTO_FOURTH, LOTTO_FOURTH_RETURN),
+    FIFTH(LOTTO_FIFTH, LOTTO_FIFTH_RETURN);
+
+    private String sameCount;
+    private int lottoReturn;
+
+    private LottoRank(String sameCount, int lottoReturn){
+        this.sameCount = sameCount;
+        this.lottoReturn = lottoReturn;
+    }
 }

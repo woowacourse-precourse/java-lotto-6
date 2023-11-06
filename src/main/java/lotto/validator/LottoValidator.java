d537177bc6149d0e2092c0ac43aa6625d4844c0c
junsoo <choix712@umn.edu>
2023-11-06 20:32:11 +0900

    feat: 발행한 로또 번호 오름 차순 유효성 검사

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoValidator{
 
@@ -9,7 +10,7 @@ public class LottoValidator{
     public static void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
-
+        validateSort(numbers);
     }
 
     private static void validateSize(List<Integer> numbers) {
@@ -19,13 +20,19 @@ public class LottoValidator{
     }
 
     private static void validateDuplicate(List<Integer> numbers){
-        boolean duplicateExist = numbers.stream()
-                .count() != LOTTO_SIZE;
+       if (numbers.stream().count() != LOTTO_SIZE) {
+           throw new IllegalArgumentException("[ERROR] 로또 번호에 중복 숫자가 존재합니다.");
+       }
+    }
+    private static void validateContainsComma(List<Integer> numbers) {
 
-        if (duplicateExist) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복 숫자가 존재합니다.");
-        }
     }
 
-
+    private static void validateSort(List<Integer> numbers) {
+        boolean IsSort = IntStream.range(0, LOTTO_SIZE)
+                .allMatch(i -> numbers.get(i) < numbers.get(i + 1));
+        if (!IsSort) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 오름 차순으로 정렬되어야 합니다.");
+        }
+    }
 }

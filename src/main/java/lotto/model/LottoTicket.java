3c4229729868a03369d7b484c8b39c3268c2a0d2
LimGayoung <ygy920@gmail.com>
2023-11-06 17:42:24 +0900

    feat: LottoTicket 생성

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoTicket {
+
+    private final List<Integer> lottoNumbers;
+
+    public LottoTicket(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validate(){
+        
+    }
+
+    private void checkLottoNumbersWithinTheRange(){
+
+    }
+    private void checkLottoNumbersCount(){
+
+    }
+    private void checkLottoNumbersDuplicate(){
+
+    }
+
+}

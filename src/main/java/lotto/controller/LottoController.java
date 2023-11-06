e8010014afcd7a7f39c15f3195512cbedbd89e5c
jinny-l <jinny.l.dev@gmail.com>
2023-11-06 14:47:27 +0900

    ✨feat: 비즈니스 로직 흐름에 맞게 `Controller` 및 프로그램 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,41 @@
 package lotto.controller;
 
 import lotto.Lotto;
+import lotto.domain.LottoMachine;
 import lotto.domain.LottoNumber;
+import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
 import lotto.domain.WinningLotto;
+import lotto.util.random.RandomNumberGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     public void run() {
+        Money money = readMoney();
+        Lottos lottos = issueLottos(money);
+        OutputView.printPurchasedLottos(lottos.tickets());
+
         WinningLotto winningLotto = readWinningLotto();
+
+        LottoResult lottoResult = new LottoResult(winningLotto, lottos);
+        printResultAndProfitRate(money, lottoResult);
+    }
+
+    private Money readMoney() {
+        try {
+            return new Money(InputView.readMoney());
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return readMoney();
+        }
+    }
+
+    private static Lottos issueLottos(Money money) {
+        LottoMachine lottoMachine = new LottoMachine(new RandomNumberGenerator());
+        return lottoMachine.issue(money);
     }
 
     private WinningLotto readWinningLotto() {
@@ -21,6 +48,7 @@ public class LottoController {
         try {
             return new WinningLotto(lotto, bonusNumber);
         } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
             return createWinningLotto(lotto, readBonusNumber());
         }
     }
@@ -29,6 +57,7 @@ public class LottoController {
         try {
             return new Lotto(InputView.readLottoNumbers());
         } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
             return readLottoNumbers();
         }
     }
@@ -37,7 +66,15 @@ public class LottoController {
         try {
             return new LottoNumber(InputView.readBonusNumber());
         } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
             return readBonusNumber();
         }
+    }
+
+    private static void printResultAndProfitRate(Money money, LottoResult lottoResult) {
+        OutputView.printResults(lottoResult.getResults());
+
+        double profitRate = money.getProfitRates(lottoResult.getTotalPrize());
+        OutputView.printProfitRate(profitRate);
     }
 }

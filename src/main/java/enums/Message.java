590f1775a754e03dee6fff4b121e533bd62c3ce9
daneng4 <daneng4@naver.com>
2023-11-06 19:34:18 +0900

    feat: 개행 상수 문자열 추가

--- src/main/java/enums/Message.java
+++ src/main/java/enums/Message.java
@@ -6,7 +6,8 @@ public enum Message {
     REQUIRE_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
     REQUIRE_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS("당첨 통계"),
-    PUBLISHED_LOTTO("개를 구매했습니다.");
+    PUBLISHED_LOTTO("개를 구매했습니다."),
+    NEW_LINE("\n");
     private final String message;
     Message(String message) {
         this.message = message;

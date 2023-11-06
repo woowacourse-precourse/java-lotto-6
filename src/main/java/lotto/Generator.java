c1726a4050dae22a66e8789ac766fbbefafb3b2b
muyaaho <skanthsu715@gmail.com>
2023-11-06 11:05:20 +0900

    feat(lotto): 중복되지 않는 1~45 사이 숫자를 생성하는 기능 추가

--- src/main/java/lotto/Generator.java
+++ src/main/java/lotto/Generator.java
@@ -1,9 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class Generator {
     public int generateRandomNumber(){
         return Randoms.pickNumberInRange(0, 45);
+    }
+
+    public List<Integer> generate6Nums(){
+        return Randoms.pickUniqueNumbersInRange(0, 45, 6);
     }
 }
--- src/test/java/lotto/GeneratorTest.java
+++ src/test/java/lotto/GeneratorTest.java
@@ -2,6 +2,9 @@ package lotto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class GeneratorTest {
@@ -11,5 +14,16 @@ public class GeneratorTest {
         int generatedNum = generator.generateRandomNumber();
         boolean output = (generatedNum >= 0 && generatedNum <= 45);
         assertEquals(true, output);
+    }
+
+    @Test
+    @RepeatedTest(100)
+    void generator로_생성된_숫자가_6개인지_확인(){
+        Generator generator = new Generator();
+        assertEquals(6, generator.generate6Nums().size());
+
+        List<Integer> generateNums = generator.generate6Nums();
+        System.out.println(generateNums);
+        assertEquals(true, (generateNums.size() == new HashSet<>(generateNums).size()));
     }
 }

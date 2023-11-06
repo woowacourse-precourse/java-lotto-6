4d63f8fc1115ee65fdf60b432293654a85e97f70
2020006026 <jms0538@hanyang.ac.kr>
2023-11-06 16:03:36 +0900

    docs: Modify the order of feature list

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -3,8 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoView {
-    private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주새요";
-    private static final String NOT_NUMMBER_ERROR_MESSAGE = "[ERROR] 구입금액은 숫자를 입력해야 합니다.";
+    private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 구입금액은 숫자를 입력해야 합니다.";
 
     public LottoView() {}
 
@@ -17,7 +17,7 @@ public class LottoView {
                 money = Integer.parseInt(Console.readLine());
                 break;
             } catch (IllegalArgumentException e) {
-                System.out.println(NOT_NUMMBER_ERROR_MESSAGE);
+                System.out.println(NOT_NUMBER_ERROR_MESSAGE);
             }
         }
 

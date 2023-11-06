7ab66339c2427a165f4a0b81939719e8d5ce9aaa
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 15:24:58 +0900

    feat: 보너스번호 질문 출력
    보너스 번호 입력을 받기 위함

--- src/main/java/lotto/print/PrintOutput.java
+++ src/main/java/lotto/print/PrintOutput.java
@@ -27,4 +27,8 @@ public class PrintOutput {
     public void printLottoNumberQuestion(){
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public void printBonusNumberQuestion(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }

44160b2207da68c78939a6dba3ca3f1aa0b6e086
jinsu4755 <parkjinsu4755@gmail.com>
2023-11-06 19:44:51 +0900

    feat: 로또 기능 추가
    - 로또는 중복 숫자로 생성될 수 없으므로 이를 검증하는 함수 작성
    
    - 로또는 1~45 까지 숫자만 사용할 수 있으므로 이를 검증하는 함수 작성
    
    - 로또 스스로 특정한 번호를 가지고 있는지 확인하는 함수 작성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,58 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Lotto {
+
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        validateDuplicate(numbers);
+        this.numbers = numbers.stream()
+                .peek(this::validateNumber)
+                .toList();
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumber(int number) {
+        if (!containsLottoNumberRange(number)) {
+            throw new IllegalArgumentException(String.format("유효한 로또 번호가 아닙니다 : %d", number));
+        }
+    }
+
+    private boolean containsLottoNumberRange(int number) {
+        return IntStream.rangeClosed(START_NUMBER, END_NUMBER)
+                .boxed()
+                .toList()
+                .contains(number);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> duplicateNumbers = findDuplicates(numbers);
+        if (!duplicateNumbers.isEmpty()) {
+            throw new IllegalArgumentException(String.format("%s 에 중복값이 있습니다: %s", numbers, duplicateNumbers));
+        }
+    }
+
+    private Set<Integer> findDuplicates(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(number -> Collections.frequency(numbers, number) > 1)
+                .collect(Collectors.toSet());
+    }
+
+    public boolean has(int number) {
+        return this.numbers.contains(number);
+    }
+}

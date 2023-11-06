95f54e49ba8e1f9b63b1d71ba8e4944be5cd85d9
SeungGwa123 <skwan123@naver.com>
2023-11-07 06:48:01 +0900

    Fix: Update validate & Fix pickNumberDuplicate
    pickNumberDuplicate에서 정렬하는 코드 삭제
    validate에서는 중복 체크하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,11 +15,12 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-
+        if(pickNumberDuplicate(numbers)){
+            throw new IllegalArgumentException();
+        }
     }
     public boolean pickNumberDuplicate(List<Integer> numbers){
         boolean result = false;
-        Collections.sort(numbers);
         for(int i=1;i<numbers.size();i++){
             if(numbers.get(i)==numbers.get(i-1)){
                 result = true;

b41a1f6f0b45c8cc101b19843f46b51e1383f964
elyudwo <dudqk9696@naver.com>
2023-11-06 16:52:48 +0900

    [Test] : 로또 발급, 반환 테스트 수정
    PurchasedLotto 객체 정적 팩토리 메소드 사용하도록 수정

--- src/test/java/lotto/domain/PurchasedLottoTest.java
+++ src/test/java/lotto/domain/PurchasedLottoTest.java
@@ -4,7 +4,6 @@ import lotto.utils.numbergenerators.RandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,17 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PurchasedLottoTest {
 
 
-    @DisplayName("구입 금액에 맞춰 로또를 발급한다.")
+    @DisplayName("구입 금액에 맞춰 로또를 발급하고 발급된 로또를 반환한다.")
     @Test
     void issueLotto() {
         // given
-        PurchasedLotto purchasedLotto = new PurchasedLotto();
-        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        PurchasedLotto purchasedLotto = PurchasedLotto.of(3000, RandomNumberGenerator.getInstance());
 
-        // when
-        List<Lotto> lottos = purchasedLotto.issueLotto(3000, randomNumberGenerator);
-
-        // then
-        assertThat(lottos.size()).isEqualTo(3);
+        // when, then
+        assertThat(purchasedLotto.getPurchasedLotto().size()).isEqualTo(3);
     }
 }

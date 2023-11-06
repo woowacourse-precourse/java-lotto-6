34a468356d064d1a18449abf525a51b3af3795a6
seonghun <sdzx0719@naver.com>
2023-11-06 14:21:29 +0900

    fix(style): docs readme 내용 추가 및 수정

--- src/main/java/lotto/service/Exceptions.java
+++ src/main/java/lotto/service/Exceptions.java
@@ -6,4 +6,11 @@ public class Exceptions {
             throw new IllegalArgumentException("[ERROR] 금액은 1000원단위로 가능합니다.");
         }
     }
+
+    public void isInvalidNumberOfLotteries(String input){
+        String[] lotteries  = input.split(",");
+        if(lotteries.length != 6){
+            throw new IllegalArgumentException("로또 번호는 6개를 입력해주셔야 합니다.");
+        }
+    }
 }

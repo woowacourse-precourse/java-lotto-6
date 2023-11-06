f7661b10276267ecbe298249ae6fc9c891842033
King's meow <mingee641@gmail.com>
2023-11-06 17:12:11 +0900

    feat: 입력을 위해 메세지 출력하는 PrintView 클래스 추가

---
+++ src/main/java/lotto/view/PrintView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+
+public class PrintView {
+
+    public static void inputMoneyView() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void inputWinningNumbersView() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void inputBonusNumberView() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+
+
+}

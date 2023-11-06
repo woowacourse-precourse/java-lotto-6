205cea2630887984a5938909819879281fdeabdd
sami355-24 <ddung355@gmail.com>
2023-11-06 18:07:08 +0900

    refactor(feat): 누락되어 있던 리터럴 넘버 수정

--- src/main/java/lotto/CommonValidation/CommonValidation.java
+++ src/main/java/lotto/CommonValidation/CommonValidation.java
@@ -1,15 +1,17 @@
 package lotto.CommonValidation;
 
+import static lotto.Dictionary.LottoDictionary.*;
 import static lotto.Message.ExceptionMessage.CommonValidationErrorMessage.*;
 import static lotto.Util.Util.ConvertStringToInteger;
 
+import lotto.Dictionary.LottoDictionary;
 import lotto.Exception.CommonValidationException;
 
 public class CommonValidation {
 
 
-    public static void isValueBetween1And45(Integer value) {
-        if (value < 1 || value > 45) {
+    public static void isValidLottoNumber(Integer value) {
+        if (value < LOTTO_MIN_NUMBER.getValue() || value > LOTTO_MAX_NUMBER.getValue()) {
             throw new CommonValidationException(RANGE_ERROR_MESSAGE.getMessage(value));
         }
     }

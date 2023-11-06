5ace62f07b31cad0d1a343ff567028afb7413d1c
jiwoo <zbqmgldjfh@gmail.com>
2023-11-06 10:24:44 +0900

    refactor(LottoController): 메서드로 로직 분리 리팩토링

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -20,29 +20,67 @@ public class LottoController {
     }
 
     public void run() {
-        try {
-            outputView.writeLottoPurchaseMoney();
-            Money purchaseMoney = inputView.readUserPurchaseMoney();
-            int quantity = purchaseMoney.quantityAvailableForPurchase(1000);
-            outputView.writeBlank();
+        int lottoQuantity = getLottoQuantity();
 
-            List<Lotto> lottos = RandomLottoFactory.generateLottos(quantity);
-            outputView.writeGeneratedLotto(lottos);
-            outputView.writeBlank();
+        List<Lotto> lottos = generateRandomLottos(lottoQuantity);
 
+        List<LottoNumber> lottoWinnerNumbers = getLottoWinnerNumbers();
+
+        LottoNumber lottoBonusNumber = getLottoBonusNumber();
+
+        // TODO : 로또 결과 계산
+
+        outputView.writeResult();
+    }
+
+    private List<Lotto> generateRandomLottos(int lottoQuantity) {
+        List<Lotto> lottos = RandomLottoFactory.generateLottos(lottoQuantity);
+        outputView.writeGeneratedLotto(lottos);
+        outputView.writeBlank();
+        return lottos;
+    }
+
+    private Money getMoney() {
+        outputView.writeLottoPurchaseMoney();
+        return inputView.readUserPurchaseMoney();
+    }
+
+    private int getLottoQuantity() {
+        try{
+            Money purchaseMoney = getMoney();
+            int availableForPurchase = purchaseMoney.quantityAvailableForPurchase(1000);
+            outputView.writeBlank();
+            return availableForPurchase;
+        } catch (IllegalArgumentException e) {
+            outputView.writeWithErrorMessage(e.getMessage());
+            outputView.writeBlank();
+            return getLottoQuantity();
+        }
+    }
+
+    private List<LottoNumber> getLottoWinnerNumbers() {
+        try{
             outputView.writeLottoWinningNumbers();
-            List<LottoNumber> lottoNumbers = inputView.readWinnerLottoNumbers();
+            List<LottoNumber> winnerLottoNumbers = inputView.readWinnerLottoNumbers();
             outputView.writeBlank();
+            return winnerLottoNumbers;
+        } catch (IllegalArgumentException e) {
+            outputView.writeWithErrorMessage(e.getMessage());
+            outputView.writeBlank();
+            return getLottoWinnerNumbers();
+        }
+    }
 
+    private LottoNumber getLottoBonusNumber() {
+        try{
             outputView.writeLottoBonusNumber();
             LottoNumber bonusNumber = inputView.readBonusNumber();
             outputView.writeBlank();
-
-            // TODO : 로또 결과 계산
-
-            outputView.writeResult();
+            return bonusNumber;
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            outputView.writeWithErrorMessage(e.getMessage());
+            outputView.writeBlank();
+            return getLottoBonusNumber();
         }
     }
 }

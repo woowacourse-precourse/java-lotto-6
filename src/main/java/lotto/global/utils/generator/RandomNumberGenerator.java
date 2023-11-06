b73bc1397103e2411a596e65c9f44df599687c15
Junho Lee <ghzm888@gmail.com>
2023-11-06 20:56:58 +0900

    refactor : 7자리에서 6자리로 랜덤 생성하도록 수정

--- src/main/java/lotto/global/utils/generator/RandomNumberGenerator.java
+++ src/main/java/lotto/global/utils/generator/RandomNumberGenerator.java
@@ -2,7 +2,6 @@ package lotto.global.utils.generator;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator implements NumberGenerator {

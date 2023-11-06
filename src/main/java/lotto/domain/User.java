08910c3c681d5af4d2e12be36d95ac439c5b9574
woong2e <tlswodnd12345@naver.com>
2023-11-06 21:36:48 +0900

    feat: 로또게임을 하는 사람이 산 로또를 보유하는 기능

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,2 +1,11 @@
-package lotto.domain;public class User {
+package lotto.domain;
+
+import java.util.List;
+
+public class User {
+    private final List<Lotto> lottos;
+
+    public User(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
 }

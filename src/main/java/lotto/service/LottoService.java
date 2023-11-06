73a36821ecf74dbf9ae8432ad522720476383fab
emost22 <emoney96@naver.com>
2023-11-06 11:23:26 +0900

    Feat: Add getUserLottos

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,8 +1,10 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.data.Lotto;
 import lotto.data.UserLotto;
 import lotto.validator.MoneyValidator;
 
@@ -17,6 +19,7 @@ public class LottoService {
 
     public void start() {
         getSpendMoney();
+        getUserLottos(lottoCnt);
     }
 
     private void getSpendMoney() {
@@ -25,5 +28,22 @@ public class LottoService {
         MoneyValidator.validate(spend);
         spendMoney = Integer.parseInt(spend);
         lottoCnt = spendMoney / 1000;
+    }
+
+    private void getUserLottos(int cnt) {
+        System.out.println("\n" + cnt + "개를 구매했습니다.");
+        while (cnt-- > 0) {
+            userLottos.add(new UserLotto(getRandomLotto()));
+        }
+
+        userLottos.forEach(userLotto -> {
+            System.out.println(userLotto.toString());
+        });
+        System.out.println();
+    }
+
+    private Lotto getRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
     }
 }

daf91690bd26f3c2b66c42378f8a3a6d153bc75d
706com <ehdgusjdbswl@gmail.com>
2023-11-06 19:40:20 +0900

    feat : 로또 생성기 도메인객체 구현

--- src/main/java/lotto/domain/LotteryGenerator.java
+++ src/main/java/lotto/domain/LotteryGenerator.java
@@ -1,11 +1,31 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LotteryGenerator {
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 45;
     private static final int NUMBER_SIZE = 6;
 
-    private boolean validateMoney(int money){
+    private Lotto lotto;
 
+    private void generateLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        do {
+            int pickNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+            if (!numbers.contains(pickNumber)) {
+                numbers.add(pickNumber);
+            }
+        } while (numbers.size() != NUMBER_SIZE);
+//        System.out.println(numbers.toString());
+        this.lotto = new Lotto(numbers);
     }
+
+    public Lotto getLotto() {
+        generateLotto();
+        return lotto;
+    }
+
 }

09b4818ca2fcb2fdd98fe4525e7532f43f21b580
jang010505 <jang010505@inu.ac.kr>
2023-11-07 06:40:16 +0900

    feat: InputView 객체 생성 및 작성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,21 @@
+package lotto.view;
+
+import static lotto.constant.MessageConstant.BONUS_NUMBER_INPUT_MESSAGE;
+import static lotto.constant.MessageConstant.LOTTO_NUMBER_INPUT_MESSAGE;
+import static lotto.constant.MessageConstant.LOTTO_PRICE_INPUT_OUTPUT_MESSAGE;
+
+public class InputView {
+    public static void inputLottoPriceMessage() {
+        System.out.println(LOTTO_PRICE_INPUT_OUTPUT_MESSAGE.getMessage());
+    }
+
+    public static void inputLottoNumber() {
+        System.out.println();
+        System.out.println(LOTTO_NUMBER_INPUT_MESSAGE.getMessage());
+    }
+
+    public static void inputBonusNumber() {
+        System.out.println();
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE.getMessage());
+    }
+}

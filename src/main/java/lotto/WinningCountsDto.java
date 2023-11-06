12e56b1113524e953a0d2e05657e8c6a40df3dec
acycoco <pink10257@naver.com>
2023-11-07 03:11:43 +0900

    refactor(WinningRank): 등수 구하는 로직 수정 - 3등인지를 직접확인하지 않고, 표현식을 필드에 추가하여 확인하도록 수정 -

--- src/main/java/lotto/WinningCountsDto.java
+++ src/main/java/lotto/WinningCountsDto.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 public class WinningCountsDto {
     private final Integer countOfMatchingNumbers;
+    private final boolean requiresBonus;
     private final long prize;
     private final Integer winningCount;
 
-    private WinningCountsDto(Integer countOfMatchingNumbers, long prize, Integer winningCount) {
+    private WinningCountsDto(Integer countOfMatchingNumbers, boolean requiresBonus, long prize, Integer winningCount) {
         this.countOfMatchingNumbers = countOfMatchingNumbers;
+        this.requiresBonus = requiresBonus;
         this.prize = prize;
         this.winningCount = winningCount;
     }
@@ -19,15 +21,20 @@ public class WinningCountsDto {
                 .filter(winningRank -> winningRank != WinningRank.NO_WIN)
                 .map(winningRank -> {
                     int matchingNumbers = winningRank.getCountOfMatchingNumbers();
+                    boolean requiresBonus = winningRank.isRequiresBonus();
                     long prize = winningRank.getPrize();
                     int count = winningCounts.getCount(winningRank);
-                    return new WinningCountsDto(matchingNumbers, prize, count);
+                    return new WinningCountsDto(matchingNumbers, requiresBonus, prize, count);
                 })
                 .toList();
     }
 
     public Integer getCountOfMatchingNumbers() {
         return countOfMatchingNumbers;
+    }
+
+    public boolean isRequiresBonus() {
+        return requiresBonus;
     }
 
     public long getPrize() {
--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -1,45 +1,51 @@
 package lotto;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum WinningRank {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NO_WIN(0, 0);
+    NO_WIN(0, false, 0, (count, bonus) -> count <= 0),
+    FIFTH(3, false, 5_000, (count, bonus) -> count == 3),
+    FOURTH(4, false, 50_000, (count, bonus) -> count == 4),
+    THIRD(5, false, 1_500_000, (count, bonus) -> count == 5 && !bonus),
+    SECOND(5, true, 30_000_000, (count, bonus) -> count == 5 && bonus),
+    FIRST(6, false, 2_000_000_000, (count, bonus) -> count == 6);
 
     private final int countOfMatchingNumbers;
+    private final boolean requiresBonus;
     private final long prize;
+    private final BiFunction<Integer, Boolean, Boolean> conditionExpress;
 
-    WinningRank(int countOfMatchingNumbers, long prize) {
+    WinningRank(int countOfMatchingNumbers, boolean requiresBonus, long prize, BiFunction<Integer, Boolean, Boolean> conditionExpress) {
         this.countOfMatchingNumbers = countOfMatchingNumbers;
+        this.requiresBonus = requiresBonus;
         this.prize = prize;
+        this.conditionExpress = conditionExpress;
     }
 
     public int getCountOfMatchingNumbers() {
         return countOfMatchingNumbers;
     }
 
+    public boolean isRequiresBonus() {
+        return requiresBonus;
+    }
+
     public long getPrize() {
         return prize;
     }
 
-    public static WinningRank from(int countOfMatchingNumbers, boolean isBonusMatched) {
-        if (THIRD.matches(countOfMatchingNumbers) && !isBonusMatched) {
-            return THIRD;
-        }
-
+    public static WinningRank from(int count, boolean bonus) {
         return Arrays.stream(values())
-                .filter(winningRank -> winningRank.matches(countOfMatchingNumbers))
+                .filter(winningRank -> winningRank.isRank(count, bonus))
                 .findFirst()
                 .orElse(NO_WIN);
     }
 
-    private boolean matches(int countOfMatchingNumbers) {
-        return this.countOfMatchingNumbers == countOfMatchingNumbers;
+    private boolean isRank(int countOfMatchingNumbers, boolean isBonusMatched) {
+        return conditionExpress.apply(countOfMatchingNumbers, isBonusMatched);
     }
+
 }
 

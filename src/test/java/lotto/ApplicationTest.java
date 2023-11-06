0634f573fe4e13760042fb852782070dc5892001
bbbig5360 <bbbig5360>
2023-11-07 00:23:30 +0900

    로또 구매시 문자 포함 에러 발생 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -49,8 +50,10 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
-            runException("1000j");
-            assertThat(output()).contains(ERROR_MESSAGE);
+            assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1000j"))
+                    .isInstanceOf(IllegalArgumentException.class));
+        assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 

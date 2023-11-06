6bf26c1e03b6cf70f342986162206fbc6bccc56c
cheolhyeon <tghy4@naver.com>
2023-11-06 18:44:08 +0900

    refactor : 외부로부터 값을 할당받아서 초기화하기 위해서 코드 구조 개선

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,25 +1,40 @@
 package user;
 
-public class User {
-    //?? ??? ??????, ??? ??? ??? ?? ???
+import java.util.Arrays;
 
-    private final int lottoTicketPrice;
-    private final int bonusNumber;
-    private final String[] winningLottoNumber;
+public class User {
+    private int lottoTicketPrice;
+    private int bonusNumber;
+    private int[] winningLottoNumber;
+    private static final int DIVISION_NUM_FOR_TICKET = 1000;
 
     public User(int lottoTicketPrice, int bonusNumber, String[] winningLottoNumber) {
         this.lottoTicketPrice = lottoTicketPrice;
         this.bonusNumber = bonusNumber;
-        this.winningLottoNumber = winningLottoNumber;
+        setWinningNumbers(winningLottoNumber);
     }
+
+    public void setLottoTicketPrice(int lottoTicketPrice) {
+        this.lottoTicketPrice = lottoTicketPrice;
+    }
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+    public void setWinningNumbers(String[] winningLottoNumber) {
+        this.winningLottoNumber = Arrays.stream(winningLottoNumber)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
     public int inputLottoTicketPrice() {
-        return lottoTicketPrice / 1000;
+        return lottoTicketPrice / DIVISION_NUM_FOR_TICKET;
     }
+
     public int inputBonusNumber() {
         return bonusNumber;
     }
-    public String[] getInputWinningLottoNumber() {
+
+    public int[] getInputWinningLottoNumber() {
         return winningLottoNumber;
     }
-
 }

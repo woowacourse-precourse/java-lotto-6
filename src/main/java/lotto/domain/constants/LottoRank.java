bf1cd2f99353a55a906a1f19b69a617ef5713923
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 21:14:40 +0900

    fix: LottoRank 버그 수정 SECOND의 count가 6이 아니라 5이면서 bonus가 true일 때이므로 버그를 수정하였습니다.

--- src/main/java/lotto/domain/constants/LottoRank.java
+++ src/main/java/lotto/domain/constants/LottoRank.java
@@ -2,7 +2,7 @@ package lotto.domain.constants;
 
 public enum LottoRank {
     FIRST(6, false, 2_000_000_000),
-    SECOND(6, true, 30_000_000),
+    SECOND(5, true, 30_000_000),
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),

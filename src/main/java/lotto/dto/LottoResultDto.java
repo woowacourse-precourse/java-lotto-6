8fd995afb9be76c7a6b6bf36eb664f22a8b92789
seminchoi <choicco89@gmail.com>
2023-11-06 13:05:16 +0900

    refactor(dto.LottoResultDto) : 생성자에서 UI에 필요한 데이터만 가지도록 가공

--- src/main/java/lotto/dto/LottoResultDto.java
+++ src/main/java/lotto/dto/LottoResultDto.java
@@ -1,8 +1,12 @@
 package lotto.dto;
 
 import lotto.constant.LottoRank;
-
 import java.util.Map;
 
 public record LottoResultDto(double profitRate, Map<LottoRank, Integer> result) {
+    public LottoResultDto(double profitRate, Map<LottoRank, Integer> result) {
+        this.profitRate = profitRate;
+        result.remove(LottoRank.OUT_RANK);
+        this.result = result;
+    }
 }

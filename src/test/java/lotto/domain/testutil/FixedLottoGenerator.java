c531eee5d309b504e40b934075bc78ff107f45ec
mainfn <_@mainfn.dev>
2023-11-07 00:27:41 +0900

    docs: `FixedLottoGenerator` 기능 설명 추가

--- src/test/java/lotto/domain/testutil/FixedLottoGenerator.java
+++ src/test/java/lotto/domain/testutil/FixedLottoGenerator.java
@@ -4,14 +4,23 @@ import java.util.List;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoGenerator;
 
+/**
+ * 테스트 시, 고정 번호로 로또를 생성하기 위한 LottoGenerator
+ */
 public final class FixedLottoGenerator implements LottoGenerator {
 
+    /**
+     * 객체 생성 시, 고정으로 저장해둘 로또 번호
+     */
     private final List<Integer> numbers;
 
     public FixedLottoGenerator(final List<Integer> numbers) {
         this.numbers = numbers;
     }
 
+    /**
+     * 고정 로또 번호를 그대로 사용하여 Lotto 객체 생성
+     */
     @Override
     public Lotto generate() {
         return new Lotto(numbers);

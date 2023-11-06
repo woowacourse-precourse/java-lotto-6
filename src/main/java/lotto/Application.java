2752cbaaac5d25274ec499c286d2f17598b78e32
DahyeY <dhuniv@naver.com>
2023-11-06 18:43:51 +0900

    feat: 구입 금액만큼 로또 발행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,10 +10,18 @@ public class Application {
     }
 }
 
-class User{
+class User {
     public List<List<Integer>> tickets;
-    private List<Integer> generateTicket(){
+
+    private List<Integer> generateTicket() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
+    }
+
+    private void purchaseTicket(int price) {
+        final int AMOUNT = price / 1000;
+        for (int i = 0; i < AMOUNT; i++) {
+            this.tickets.add(generateTicket());
+        }
     }
 }

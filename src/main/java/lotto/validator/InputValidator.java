b0dbaa4a5f6278e46c2e11548b58dccb3ac74a6a
jongheonleee <qwefghnm1212@gmail.com>
2023-11-06 16:33:06 +0900

    [refactor] InputView 클래스에서 검증 메서드 InputViewValidator로 옮기기

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,4 +1,60 @@
 package lotto.validator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static lotto.util.CharacterUnits.COMMA;
+
 public class InputValidator {
+
+    private static final Pattern pattern = Pattern.compile("[ !@#$%^&*().?\":{}|<>]");
+
+    public static void validateNumber(String input) {
+        for (char token : input.toCharArray()) {
+            isNumberToken(token);
+        }
+    }
+
+    private static void isNumberToken(Character token) {
+        if (!(Character.isDigit(token))) {
+            throw new IllegalArgumentException("[ERROR] 입력값에 숫자가 아닌 값이 포함되어 있습니다.");
+        }
+    }
+
+    public static void validateBlank(String input) {
+        if (input.isBlank() || input == null) {
+            throw new IllegalStateException("[ERROR] 입력값을 입력하지 않았습니다.");
+        }
+
+    }
+
+    public static void validateInputNumbersFormat(List<String> numbers) {
+        validateSpecialSign(numbers);
+        validateNumbers(numbers);
+    }
+
+    private static void validateNumbers(List<String> numbers) {
+        for (String number : numbers) {
+            validateNumber(number);
+        }
+    }
+
+    private static void validateSpecialSign(List<String> numbers) {
+        for (String number : numbers) {
+            isSpecialSignToken(number);
+        }
+    }
+
+    private static void isSpecialSignToken(String number) {
+        if (pattern.matcher(number)
+                .find()) {
+            throw new IllegalArgumentException("[ERROR] 입력값에 구분자인,(쉼표) 이 외의 특수문자를 허용하지 않습니다.");
+        }
+
+    }
+
+
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,133 +3,60 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Cash;
 import lotto.domain.WinnerLotto;
+import lotto.validator.InputValidator;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static lotto.util.CharacterUnits.COMMA;
 
 public class InputView {
 
     public Cash InputCash() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = getInput();
-        validateBlank(input);
-        validateNumber(input);
-        Integer cash = parseStrToInt(input);
-        validateAvailableCash(cash);
+        InputValidator.validateNumber(input);
+        Integer cash = Integer.parseInt(input);
         return new Cash(cash);
     }
 
-    private void validateNumber(String input) {
-        for (char token : input.toCharArray()) {
-            if (!Character.isDigit(token)) {
-                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값을 입력할 수 없습니다. 숫자를 입력해주세요");
-            }
-        }
-    }
-
-    private void validateAvailableCash(Integer input) {
-        if (input % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지는 금액만 입력가능합니다");
-        }
-    }
-
-    private void validateBlank(String input) {
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 공백을 입력할 수 없습니다.");
-        }
-    }
-
-    private Integer parseStrToInt(String number) {
-        return Integer.parseInt(number);
-    }
-
-    public WinnerLotto inputWinnerLotto() {
-        List<Integer> winnerNumbers = inputWinnerNumbers();
-        Integer bonusNumber = inputBonusNumber();
-        return new WinnerLotto(winnerNumbers, bonusNumber);
-    }
 
     public List<Integer> inputWinnerNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = getInput();
-        validateBlank(input);
-        List<String> inputWinnerNumbers = Arrays.stream(
-                        input.split(","))
-                        .toList();
-        validateCountOfWinnerNumbers(inputWinnerNumbers);
-        validateNumbers(inputWinnerNumbers);
-        List<Integer> winnerNumbers = convertStrToInt(inputWinnerNumbers);
-        validateDuplicatedNumber(winnerNumbers);
-        validateNumbersRange(winnerNumbers);
-        System.out.println();
-        return winnerNumbers;
+        List<String> splitInput = splitByComma(input);
+        InputValidator.validateInputNumbersFormat(splitInput);
+        return splitInput.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     public Integer inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = getInput();
-        validateBlank(input);
-        validateNumber(input);
-        Integer number = parseStrToInt(input);
-        validateNumberRange(number);
+        InputValidator.validateNumber(input);
         System.out.println();
-        return number;
-    }
-
-    private void validateCountOfWinnerNumbers(List<String> inputWinnerNumbers) {
-        if (inputWinnerNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자로 구성 되야합니다");
-        }
-    }
-
-    private void validateNumbers(List<String> inputWinnerNumbers) {
-        for (String inputWinnerNumber : inputWinnerNumbers) {
-            validateNumber(inputWinnerNumber);
-        }
-    }
-
-    private void validateDuplicatedNumber(List<Integer> winnerNumbers) {
-        if (winnerNumbers.stream()
-                .collect(Collectors.toSet())
-                .size() != winnerNumbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 중복된 숫자를 입력할 수 없습니다");
-        }
-    }
-
-    private void validateNumberRange(Integer number) {
-        if (!(1 <= number && number <= 45)) {
-            throw new IllegalArgumentException("[ERROR] 1~45를 벗어난 숫자를 입력할 수 없습니다");
-        }
-    }
-
-    private void validateNumbersRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            validateNumberRange(number);
-        }
+        return Integer.parseInt(input);
     }
 
 
-    private List<Integer> convertStrToInt(List<String> inputWinnerNumbers) {
-        return inputWinnerNumbers.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+
+    private List<String> splitByComma(String input) {
+        return Arrays.stream(input
+                        .split(COMMA.getUnit()))
+                        .toList();
     }
 
 
-    //
 
-    private String getInput() throws IllegalStateException {
-       String input = Console.readLine();
-       validateNoLineFound(input);
-       return input;
+
+    private String getInput() {
+        String input = Console.readLine();
+        InputValidator.validateBlank(input);
+        return input;
     }
 
-    private void validateNoLineFound(String input) throws IllegalStateException {
-        if (input.isBlank() || input == null) {
-            throw new IllegalStateException("[ERROR] 입력값을 입력하지 않았습니다.");
-        }
-    }
 
     public void close() {
         Console.close();

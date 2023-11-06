6ddd36bb1aaa83857055fcf43bb5c6d3155a552c
shining013 <nevertheless113@naver.com>
2023-11-06 18:41:22 +0900

    feat: printQuantity 메서드 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+import lotto.Constants;
+public class OutputView {
+    public static void printQuantity(int quantity) {
+        System.out.println(quantity+Constants.QUANTITY_RESULT);
+    }
+}

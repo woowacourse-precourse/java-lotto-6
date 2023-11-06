4f0ba970d95ec2af8c6792cd2fcc7083d54fd7c5
nature1216 <annekim1216@naver.com>
2023-11-06 16:42:08 +0900

    style: 당첨 번호 입력 메소드명 변경 당첨 번호 입력 메소드명 변경(getLottoInput)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,5 @@ import lotto.contorller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.getAmountInput();
     }
 }
--- src/main/java/lotto/contorller/LottoController.java
+++ src/main/java/lotto/contorller/LottoController.java
@@ -6,7 +6,7 @@ import lotto.service.LottoService;
 
 public class LottoController {
     private final LottoService lottoService = new LottoService();
-    public void getAmountInput() {
+    public void getLottoInput() {
         String amountInput = Console.readLine();
         Lotto lotto = lottoService.stringToLotto(amountInput);
     }

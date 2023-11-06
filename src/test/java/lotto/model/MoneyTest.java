0785c3118d9cf42511bb42813ea7aa46c74ae22a
hoyeon <highoption@naver.com>
2023-11-06 22:26:25 +0900

    Test(MoneyTest) : 입력 받은 돈이 1000원 단위인지 테스트
    - String으로 돈을 입력 받는다
    - 받은 돈이 1000원 단위가 아니라면 예외 발생

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -12,6 +12,7 @@ public class MoneyTest {
     @DisplayName("Money 객체가 재대로 생성 되는지 테스트")
     void createMoney() {
         String input = "3000";
+
         Money money = new Money(input);
         assertThat(money).isNotNull();
     }
@@ -20,8 +21,19 @@ public class MoneyTest {
     @DisplayName("구입 금액이 숫자가 아니라면 예외 발생")
     void inputIsOnlyInteger() {
         String input = "hoyeon";
+
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Money(input))
                 .withMessage("[ERROR] 구입 금액은 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("구입 금액은 1000원 단위가 아니라면 예외 발생")
+    void inputIsPaperMoney() {
+        String money = "2500";
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Money(money))
+                .withMessage("[ERROR] 구입 금액은 1,000원 단위로 이루어져야 합니다.");
     }
 }

e52eef2ed566b9bb278784f70362eb0455ff614c
donglee99 <jhnj741@naver.com>
2023-11-06 22:29:51 +0900

    feat : 로또 생성 및 당첨 로또 입력 구현

--- src/main/java/lotto/util/NumberUtil.java
+++ src/main/java/lotto/util/NumberUtil.java
@@ -11,4 +11,12 @@ public class NumberUtil {
     public static final List<Integer> numberGenerator(int min, int max, int size) {
         return Randoms.pickUniqueNumbersInRange(min, max, size);
     }
+
+    public static final void validateNumberRange(int min, int max, List<Integer> numbers) {
+        if (numbers.stream()
+                .filter(number -> number >= min && number <= max)
+                .count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }

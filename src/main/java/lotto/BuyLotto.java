74e9f0a4fe4d4f04b047edcd63d1094ea5e6b835
SeungGwa123 <skwan123@naver.com>
2023-11-06 21:42:32 +0900

    Feat: Add pickNumber
    로또 한장을 뽑는 함수 작성

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -1,11 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BuyLotto {
     private static List<Lotto> lotto = new ArrayList<>();
 
+    public List<Integer> pickNumber(){
+        List<Integer> paper = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(paper);
+        return paper;
+    }
     public boolean checkSameNumber(List<Integer> paper, int number) {
         boolean result = true;
         for (int i = 0; i < paper.size(); i++) {

116044bf1ffab7960955db287cc3c3ffd335ded1
sinminseok12 <sinminseok12@naver.com>
2023-11-06 20:41:03 +0900

    test(LottoTest): 당첨 로또와 비교해 등수를 확인하는 기능 테스트 구현

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -3,10 +3,14 @@ package lotto.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -31,5 +35,25 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @ParameterizedTest
+    @MethodSource(value = "provideLottoTestData")
+    void 당첨_로또와_비교해_등수를_구한다(Lotto lotto, Ranking result) {
+        //given
+        WinLotto winLotto = new WinLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new LottoNumber(7));
+        //when
+        Ranking ranking = lotto.compareWinLotto(winLotto);
+        //then
+        assertThat(ranking).isEqualTo(result);
+    }
+
+    static List<Arguments> provideLottoTestData() {
+        return Arrays.asList(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), Ranking.FIRST),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)), Ranking.SECOND),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)), Ranking.THIRD),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 8, 9)), Ranking.FOURTH),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 8, 9, 10)), Ranking.FIFTH),
+                Arguments.of(new Lotto(List.of(1, 2, 10, 11, 12, 13)), Ranking.NONE)
+        );
+    }
 }

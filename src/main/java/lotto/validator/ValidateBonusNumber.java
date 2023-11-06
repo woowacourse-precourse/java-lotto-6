7a79b1954e8b06057acebece1161a9fff72f751c
sunghyun <shlim0287@naver.com>
2023-11-06 22:31:49 +0900

    refactor : 보너스 번호 검증 클래스에 번호 범위 판단하는 메서드 추가.

--- src/main/java/lotto/validator/ValidateBonusNumber.java
+++ src/main/java/lotto/validator/ValidateBonusNumber.java
@@ -1,9 +1,15 @@
 package lotto.validator;
 
+import java.util.StringTokenizer;
+
 public class ValidateBonusNumber {
+    
+    static final int START_INCLUSIVE=1;
+    static final int END_INCLUSIVE=45;
     public static void validateBonusNumber(String buyerInput){
         validateEmpty(buyerInput);
         validateConvertable(buyerInput);
+        validateNumberRange(buyerInput);
     }
 
     private static void validateConvertable(String buyerInput) {
@@ -16,5 +22,22 @@ public class ValidateBonusNumber {
         if(buyerInput.isBlank()){
             throw new IllegalArgumentException("[ERROR] 값을 입력해주세요.");
         }
+    }
+
+    private static void validateNumberRange(String buyerInput){
+        int bonusNumber = Integer.parseInt(buyerInput);
+        if(isOutOfRange(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1~45 입니다.");
+        }
+
+    }
+    private static boolean isOutOfRange(int bonusNumber){
+        if(bonusNumber<START_INCLUSIVE){
+            return true;
+        }
+        if(bonusNumber>END_INCLUSIVE){
+            return true;
+        }
+        return false;
     }
 }

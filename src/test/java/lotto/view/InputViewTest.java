a8febcd49851d7ad89f6404cfa4f162f9b404a01
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 01:01:33 +0900

    test(InputView): 구입 금액 입력 로직 테스트 코드 구현

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class InputViewTest {
-  
+    @BeforeEach
+    void resetBuffer() {
+        Console.close();
+    }
+
+    @Test
+    @DisplayName("구입금액을 입력받는다.")
+    void getPurchaseAmount() {
+        setInput("8000");
+        InputView.getPurchaseAmount();
+    }
+
+    @Test
+    @DisplayName("구입금액이 정수가 아니면 예외를 발생한다.")
+    void getPurchaseAmountWithNotInteger() {
+        setInput("팔천원");
+        assertThatThrownBy(() -> InputView.getPurchaseAmount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입금액이 정수가 아니면 예외를 발생한다.")
+    void getPurchaseAmountWithNotPositiveInteger() {
+        setInput("-2000");
+        assertThatThrownBy(() -> InputView.getPurchaseAmount())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void setInput(String input) {
+        final byte[] buf = input.getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
 }

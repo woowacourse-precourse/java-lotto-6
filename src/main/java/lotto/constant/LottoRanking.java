3d3a5cf5a148dea761787cbab48e0cbc8a54587e
ikjo39 <chi4321@naver.com>
2023-11-06 23:37:13 +0900

    refactor: enum 내 사용하지 않은 상수 삭제
    - isBonusNumberCorrect 필드 삭제

--- src/main/java/lotto/constant/LottoRanking.java
+++ src/main/java/lotto/constant/LottoRanking.java
@@ -1,19 +1,17 @@
 package lotto.constant;
 
 public enum LottoRanking {
-    FIRST(6, false, 2000000000L),
-    SECOND(5, true, 30000000L),
-    THIRD(5, false, 1500000L),
-    FOURTH(4, false, 50000L),
-    FIFTH(3, false, 5000L);
+    FIRST(6, 2000000000L),
+    SECOND(5, 30000000L),
+    THIRD(5, 1500000L),
+    FOURTH(4, 50000L),
+    FIFTH(3, 5000L);
 
     private final int sameCount;
-    private final boolean isBonusCorrect;
     private final long prize;
 
-    LottoRanking(int sameCount, boolean isBonusNumberCorrect, long prize) {
+    LottoRanking(int sameCount, long prize) {
         this.sameCount = sameCount;
-        this.isBonusCorrect = isBonusNumberCorrect;
         this.prize = prize;
     }
 
@@ -23,9 +21,5 @@ public enum LottoRanking {
 
     public long getPrize() {
         return prize;
-    }
-
-    public boolean isBonusCorrect() {
-        return isBonusCorrect;
     }
 }

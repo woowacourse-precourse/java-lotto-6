ca8a997dbd3eda8672f846136ef722fc5eed5b85
이정우 <acg6138@naver.com>
2023-11-06 13:48:16 +0900

    feat(LottoNumber): 로또 숫자 객체 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,15 +1,16 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     public static final int MAX_NUMBER_OF_NUMBERS = 6;
-    private final List<Integer> numbers;
+    private final List<LottoNumber> lottoNumbers = new ArrayList<>();
 
     public Lotto(List<Integer> numbers) {
         validateNumbersSize(numbers);
         validateDuplicateNumbers(numbers);
-        this.numbers = numbers;
+        createLottoNumbers(numbers);
     }
 
     private void validateNumbersSize(List<Integer> numbers) {
@@ -23,6 +24,12 @@ public class Lotto {
                 .distinct()
                 .count();
         if (count != MAX_NUMBER_OF_NUMBERS) throw new IllegalArgumentException();
+    }
+
+    private void createLottoNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            this.lottoNumbers.add(new LottoNumber(number));
+        }
     }
 
 }
---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public class LottoNumber {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}

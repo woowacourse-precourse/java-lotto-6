d1620087ff5303b346b4751960f0ee956f9eff48
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 23:53:18 +0900

    feat(WinningRanks): 당첨 내역을 계산하는 기능 구현

--- src/main/java/lotto/domain/WinningRanks.java
+++ src/main/java/lotto/domain/WinningRanks.java
@@ -1,5 +1,30 @@
 package lotto.domain;
 
-public class WinningDetails {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningRanks {
+    private final List<Lotto> lottos;
+    private final WinningNumbers winningNumbers;
+    private List<Rank> ranks;
+
+    public WinningRanks(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+    }
+
+    public void calculateRanks() {
+        List<Rank> ranks = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            Integer matchingNumbers = lotto.getMatchingNumbers(winningNumbers.getWinningNumber().getNumbers(), winningNumbers.getBonusNumber().getNumber());
+            boolean isBonusNumberMatch = lotto.isMatchingWithBonusNumber(winningNumbers.getBonusNumber().getNumber());
+            Rank rank = Rank.getRankByMatchingNumbersAndBonus(matchingNumbers, isBonusNumberMatch);
+            ranks.add(rank);
+        }
+        this.ranks = ranks;
+    }
+
+    public List<Rank> getRanks() {
+        return ranks;
+    }
 }

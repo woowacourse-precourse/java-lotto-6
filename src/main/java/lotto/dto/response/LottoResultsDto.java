5f0244af70b878815900ebcb845aad078c12085c
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-06 23:01:05 +0900

    refactor(LottoResultsDto): 비즈니스 로직 책임 이전

--- src/main/java/lotto/dto/response/LottoResultsDto.java
+++ src/main/java/lotto/dto/response/LottoResultsDto.java
@@ -1,52 +1,22 @@
 package lotto.dto.response;
 
-import lotto.config.LottoMessage;
-import lotto.config.Rank;
-
-import java.util.Map;
+import lotto.model.LottoResults;
 
 public class LottoResultsDto {
 
-    private final Map<Rank, Long> results;
+    private final String resultsText;
+    private final float profitRate;
 
-    private LottoResultsDto(Map<Rank, Long> results) {
-        this.results = results;
+    public LottoResultsDto(String resultsText, float profitRate) {
+        this.resultsText = resultsText;
+        this.profitRate = profitRate;
     }
 
-    public static LottoResultsDto of(Map<Rank, Long> results) {
-        return new LottoResultsDto(results);
+    public String getResults() {
+        return resultsText;
     }
 
-    public String formatResults() {
-        StringBuilder builder = new StringBuilder(LottoMessage.WINNING_STATISTICS_HEADER.getMessage());
-
-        for (Rank rank : Rank.values()) {
-            if (rank != Rank.NONE) {
-                builder.append(getFormattedResultLine(rank, results.getOrDefault(rank, 0L)));
-            }
-        }
-
-        return builder.toString();
-    }
-
-    public long calculateTotalPrize() {
-        return results.entrySet().stream()
-                .filter(entry -> entry.getKey() != Rank.NONE)
-                .mapToLong(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
-                .sum();
-    }
-
-    private String getFormattedResultLine(Rank rank, Long count) {
-        String prizeMoney = rank.getPrizeMoneyFormatted();
-        String rankMessage = getRankMessage(rank);
-
-        return String.format(LottoMessage.MATCH_RESULT.getMessage(), rankMessage, prizeMoney, count);
-    }
-
-    private String getRankMessage(Rank rank) {
-        if (rank == Rank.SECOND) {
-            return LottoMessage.MATCH_COUNT_WITH_BONUS.getFormattedMessage(rank.getMatchCount());
-        }
-        return LottoMessage.MATCH_COUNT.getFormattedMessage(rank.getMatchCount());
+    public float getProfitRate() {
+        return profitRate;
     }
 }

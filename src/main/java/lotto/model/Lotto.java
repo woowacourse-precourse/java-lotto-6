d8181b158d60ed1fc16f64f032ec6edb0ca8d7fb
고동환\eurdr <eurdream98@naver.com>
2023-11-06 15:08:52 +0900

    feature($lotto.Lotto): Add generateRandomLotto method
    - 로또 번호 6개를 무작위로 뽑아주는 기능

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,6 @@
-package lotto;
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
@@ -16,5 +18,9 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static Lotto generateRandomLotto(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
 }

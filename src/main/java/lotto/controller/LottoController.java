950e1a83801e774364951b6c3d30c6bcaee0e198
Seyeon <wjdtpdus828@naver.com>
2023-11-07 00:12:47 +0900

    modify(LottoController): startLotto 메소드 분할

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,12 +12,24 @@ public class LottoController {
     private PurchaseController purchaseController = new PurchaseController();
     private LottoView lottoView = new LottoView();
     private InitLottoController initLottoController = new InitLottoController();
+    private WinningNumberController winningNumberController = new WinningNumberController();
 
     private int ticketNumber;
     private List<Lotto> lotts  = new ArrayList<>();
     public void startLotto(){
+        purchaseLotto();
+        inputWinningNumber();
+    }
+
+    public void purchaseLotto(){
         ticketNumber = purchaseController.runPurchaseAmount();
         lottoView.outputPurchaseAmountView(ticketNumber);
         lotts = initLottoController.runInitLotto(ticketNumber);
+        lottoView.printPurchaseLotto(lotts);
+    }
+
+    public void inputWinningNumber(){
+
+        Lotto winningNumber = winningNumberController.runWinningNumber();
     }
 }

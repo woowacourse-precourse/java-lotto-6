5c0b7f73d54beb56d6371c8cf5956310a4d07d50
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-07 07:17:30 +0900

    test(domain): 6개 초과 당첨 번호 예외 처리 실패

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumberTest {
+    @DisplayName("당첨 번호의 개수가 6개 초과하면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOverSize() {
+        // given
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        int bonusNumber = 8;
+        // when
+
+        // then
+        assertThatThrownBy(() -> new WinningNumber(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호의 개수가 6개가 아닙니다.");
+    }
+}

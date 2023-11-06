5ec5655ff3330c9b10adae642a79aebffab6e1be
jihyuk <kwlgur0416@gmail.com>
2023-11-06 17:36:13 +0900

    feat : 당첨 번호와 보너스 번호 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,15 +4,43 @@ import static lotto.exception.LottoErrorCode.BLANK_INPUT;
 import static lotto.exception.LottoErrorCode.NOT_INTEGER_INPUT;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import lotto.exception.LottoException;
 
 public class InputView {
 
     private final static String LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요";
+    private final static String LOTTO_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    private static final String LOTTO_WINNING_NUMBER_DELIMITER = ",";
 
     public static int inputLottoPurchaseAmount() {
         System.out.println(LOTTO_PURCHASE_AMOUNT_MESSAGE);
 
+        return getIntegerInput();
+    }
+
+    public static List<Integer> inputLottoWinningNumbers() {
+        System.out.println(LOTTO_WINNING_NUMBER_MESSAGE);
+
+        return getWinningNumbers();
+    }
+
+    private static List<Integer> getWinningNumbers() {
+        return Arrays.stream(getInput().split(LOTTO_WINNING_NUMBER_DELIMITER))
+            .map(InputView::convertToInteger)
+            .toList();
+    }
+
+    public static int inputLottoBonusNumber() {
+        System.out.println(LOTTO_BONUS_NUMBER_MESSAGE);
+
+        return getIntegerInput();
+    }
+
+    private static int getIntegerInput() {
         return convertToInteger(getInput());
     }
 
@@ -25,7 +53,7 @@ public class InputView {
     }
 
     private static String getInput() {
-        final String input = Console.readLine();
+        final String input = Console.readLine().replaceAll(" ", "");
         validateNotBlank(input);
         return input;
     }

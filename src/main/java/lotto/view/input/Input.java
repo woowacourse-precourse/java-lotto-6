4a4cdeb37aef52dd230b83acb156077f81905e01
1000zoo <cjswldn99@gmail.com>
2023-11-07 02:33:22 +0900

    style(Input): 불필요한 상수 제거

--- src/main/java/lotto/view/input/Input.java
+++ src/main/java/lotto/view/input/Input.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public abstract class Input {
 
-    private final static String INITIALIZE = "";
-
     public String readLine() {
         String input;
 

0c5ef90860c3d08460679cb2a3400a3770cfb49e
DahyeY <dhuniv@naver.com>
2023-11-07 03:31:54 +0900

    feat: 로또 게임 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -13,14 +14,15 @@ public class Application {
 }
 
 class User {
-    public List<List<Integer>> tickets;
+    public List<List<Integer>> tickets= new ArrayList<>();
     public String price;
     private static final int TICKET_PRICE = 1000;
-    public final int AMOUNT = Integer.parseInt(this.price) / TICKET_PRICE;
+    public final int AMOUNT;
 
     public User(String price) {
         validate(price);
         this.price = price;
+        AMOUNT = Integer.parseInt(this.price) / TICKET_PRICE;
         purchaseTicket();
     }
 
@@ -44,7 +46,7 @@ class User {
 }
 
 class SingleGame {
-    public List<Integer> numbers;
+    public List<Integer> numbers = new ArrayList<>();;
     public boolean bonus;
     public int rank;
     public int result;
@@ -54,6 +56,8 @@ class SingleGame {
     public static final int THIRD_PROFIT = 1500000;
     public static final int FOURTH_PROFIT = 50000;
     public static final int FIFTH_PROFIT = 5000;
+    private int bonusNumber;
+
     public void inputLottoNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] inputNumbers;
@@ -65,29 +69,30 @@ class SingleGame {
 
     public void inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        this.numbers.add(Integer.parseInt(Console.readLine()));
+        this.bonusNumber = Integer.parseInt(Console.readLine());
     }
 
     public void compareNumbers(List<Integer> user, List<Integer> lotto) {
         this.rank = compareLotto(user, lotto);
-        this.bonus = compareBonus(user, lotto);
+        this.bonus = compareBonus(user, this.bonusNumber);
     }
 
     public int compareLotto(List<Integer> user, List<Integer> lotto) {
         int match = 0;
-        List<Integer> subLotto = lotto.subList(0, 6);
         for (Integer userNumber : user) {
-            if (subLotto.contains(userNumber)) {
+            if (lotto.contains(userNumber)) {
                 match++;
             }
         }
         return match;
     }
 
-    public boolean compareBonus(List<Integer> user, List<Integer> lotto) {
-        return user.contains(lotto.get(6));
+    public boolean compareBonus(List<Integer> user, int bonus) {
+        return user.contains(bonus);
     }
     public void computeResult(int rank){
+        this.result = 0;
+        this.profit = 0;
         if(rank==6){
             this.result = 1;
             this.profit = FIRST_PROFIT;
@@ -109,9 +114,11 @@ class SingleGame {
             this.profit = FIFTH_PROFIT;
         }
     }
-    public void run(List<Integer> user) {
+    public void setGame(){
         inputLottoNumbers();
         inputBonusNumber();
+    }
+    public void run(List<Integer> user) {
         Lotto lotto = new Lotto(this.numbers);
         compareNumbers(user, lotto.getNumbers());
         computeResult(this.rank);
@@ -149,24 +156,25 @@ class Game{
     public void run() {
         System.out.println("구입금액을 입력해 주세요.");
         setInputPrice();
-        User user = new User(inputPrice);
+        User user = new User(this.inputPrice);
         System.out.printf("\n%d개를 구매했습니다.\n", user.AMOUNT);
         for(int i=0;i<user.AMOUNT;i++){
             System.out.println(user.tickets.get(i));
         }
         SingleGame singleGame = new SingleGame();
+        singleGame.setGame();
         for(int i =0;i<user.AMOUNT; i++){
             singleGame.run(user.tickets.get(i));
             countPrize(singleGame.result);
             totalProfit += singleGame.profit;
         }
         System.out.println("당첨 통계\n---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", this.firstPrize);
-        System.out.printf("4개 일치 (50,000원) - %d개\n", this.firstPrize);
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", this.firstPrize);
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", this.firstPrize);
+        System.out.printf("3개 일치 (5,000원) - %d개\n", this.fifthPrize);
+        System.out.printf("4개 일치 (50,000원) - %d개\n", this.fourthPrize);
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", this.thirdPrize);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", this.secondPrize);
         System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", this.firstPrize);
-        float profitRate = totalProfit/Integer.parseInt(user.price);
+        float profitRate = totalProfit/Integer.parseInt(user.price)*100;
         System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
     }
 }

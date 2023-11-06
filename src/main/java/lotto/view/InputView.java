58c44cbf43259e4d6d9bb82bc24d5910789c072a
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 01:01:50 +0900

    feat(InputView): 구입 금액 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,32 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+
+        validateInput(input);
+
+        return Integer.parseInt(input);
+    }
+
+    private static void validateInput(String input) {
+        if (isNotPositiveInteger(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static int toInteger(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private static boolean isNotPositiveInteger(String input) {
+        try {
+            return toInteger(input) <= 0;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
 }

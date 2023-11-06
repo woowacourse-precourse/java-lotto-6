ef36a96573b5989560bea6d5fc75150ca7123a23
yonggwonk <gwonluck@dgu.ac.kr>
2023-11-06 20:38:13 +0900

    feat: Fix Application.java
    LottoGame의 run 메서드를 실행하여 게임을 시작하는 코드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.run();
     }
 }

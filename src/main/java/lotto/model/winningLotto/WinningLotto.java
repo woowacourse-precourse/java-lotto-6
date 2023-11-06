c4cdad003bf951fc2ae6e3c62275432bbdcfc794
dltjdn <hana40004@naver.com>
2023-11-07 04:14:46 +0900

    feat(WinningLotto): 로또 번호 비교해서 당첨 정보 생성하는 기능
    * 당첨 정보와 로또 번호를 비교하여 번호 일치 개수와 보너스 일치 여부를 구한다.

--- src/main/java/lotto/model/winningLotto/WinningLotto.java
+++ src/main/java/lotto/model/winningLotto/WinningLotto.java
@@ -1,22 +1,46 @@
 package lotto.model.winningLotto;
 
 
+import java.util.List;
 import lotto.model.lotto.Lotto;
+import lotto.model.lotto.LottoDTO;
+import lotto.model.lottoresult.WinningInfo;
 
 public class WinningLotto {
     private final Lotto winningNumber;
     private final int bonusNumber;
 
-    private WinningLotto(Lotto winningNumber, int bonusNumber){
+    private WinningLotto(Lotto winningNumber, int bonusNumber) {
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
     }
 
-    public static WinningLotto of(Lotto winningNumber, int bonusNumber){
+    public static WinningLotto of(Lotto winningNumber, int bonusNumber) {
         return new WinningLotto(winningNumber, bonusNumber);
     }
 
-    public WinningLottoDTO toWinningLottoDTO(){
+    public WinningLottoDTO toWinningLottoDTO() {
         return WinningLottoDTO.of(winningNumber, bonusNumber);
     }
+
+    public WinningInfo compare(LottoDTO lottoDTO) {
+        List<Integer> lottoNumbers = lottoDTO.numbers();
+        List<Integer> winningNumbers = winningNumber.toLottoDTO().numbers();
+
+        long matchedCount = countMatchedNumbers(lottoNumbers, winningNumbers);
+        boolean isBonusMatched = isBonusNumberMatched(lottoNumbers);
+
+        return WinningInfo.of((int) matchedCount, isBonusMatched);
+    }
+
+    private static long countMatchedNumbers(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+        return lottoNumbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    private boolean isBonusNumberMatched(List<Integer> lottoNumbers) {
+        return lottoNumbers.contains(bonusNumber);
+    }
+
 }

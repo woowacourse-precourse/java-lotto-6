91b1b4a8f6b04b8242be7c9ac2f7c011ce6f6691
yonggwonk <gwonluck@dgu.ac.kr>
2023-11-06 21:29:54 +0900

    test: Update Lottotest.java
    로또 번호 범위 테스트, 맞춘 숫자 개수에 대한 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +25,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 부터 45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 45, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 번호와 당첨 번호를 비교하여 맞춘 숫자 개수가 정확히 계산되는지 확인한다.")
+    @Test
+    void calculateMatchingNumbers() {
+        Lotto winningLotto = new Lotto(List.of(10, 20, 30, 40, 41, 42));
+        Lotto userLotto = new Lotto(List.of(10, 20, 30, 40, 41, 45));
+        GameResult gameResult = new GameResult(List.of(userLotto), winningLotto, 70);
+
+        int matchingNumbers = gameResult.countMatchingNumbers(userLotto, winningLotto);
+
+        assertThat(matchingNumbers).isEqualTo(5);
+    }
 }

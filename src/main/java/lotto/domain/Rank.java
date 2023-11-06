8aca317023f9ba76a440f2a173afec4417bc81bf
PARK9898 <wjddjs21@naver.com>
2023-11-06 17:56:09 +0900

    "feat: enum으로 로또 등수 처리"

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5,30_000_000),
+    THIRD(5,1_500_000),
+    FOURTH(4,50_000),
+    FIFTH(3,5_000),
+    ZERO(0,0)
+    ;
+
+    private final int sameNumberCount;
+    private final int prizeMoney;
+
+    Rank(int sameNumberCount, int prizeMoney) {
+        this.sameNumberCount = sameNumberCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public Rank decideRank(int sameNumberCount, boolean isContainBonusNumber) {
+        if(isContainBonusNumber && sameNumberCount == 5) {
+            return SECOND;
+        }
+        return Arrays.stream(Rank.values())
+                .filter(rank -> isCountMatch(sameNumberCount) && rank != SECOND)
+                .findAny()
+                .orElse(ZERO);
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    private boolean isCountMatch(int sameNumberCount) {
+        return this.sameNumberCount == sameNumberCount;
+    }
+}

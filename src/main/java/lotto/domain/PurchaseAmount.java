23e5c8f1185e575e8b0a6f891a823b0fac9cfe52
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:58:58 +0900

    feat: 구입 금액이 정수인지 검증하는 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -7,4 +7,11 @@ public class PurchaseAmount {
     public PurchaseAmount(Integer purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
     }
+
+    private static void validatePurchaseAmountDigit(String purchaseAmount) {
+        if (purchaseAmount.chars()
+                .anyMatch(ch -> !Character.isDigit(ch))) {
+            throw new IllegalArgumentException("구입 금액은 정수이어야 합니다.");
+        }
+    }
 }

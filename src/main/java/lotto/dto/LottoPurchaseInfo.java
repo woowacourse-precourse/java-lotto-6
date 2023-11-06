f2424d4db9dcad07972ff58f15e3bbea5802f6a8
alscjf1329 <alscjf1329@gmail.com>
2023-11-06 19:08:56 +0900

    refactor(LottoPurchaseInfo): public 제한자에서 private으로 변경
    public으로 할시 접근되기에 할당하려고 시도할 수 있기에 private으로 변경했습니다.

--- src/main/java/lotto/dto/LottoPurchaseInfo.java
+++ src/main/java/lotto/dto/LottoPurchaseInfo.java
@@ -1,17 +1,13 @@
 package lotto.dto;
 
-import static lotto.message.ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS;
-
 import java.math.BigDecimal;
 
 public class LottoPurchaseInfo {
-    public final BigDecimal purchaseAmount;
-    public final BigDecimal lottoPrice;
-    public final BigDecimal numberOfLottoPurchased;
+    private final BigDecimal purchaseAmount;
+    private final BigDecimal lottoPrice;
+    private final BigDecimal numberOfLottoPurchased;
 
     public LottoPurchaseInfo(BigDecimal purchaseAmount, BigDecimal lottoPrice) {
-        validateMultiplesOf(purchaseAmount, lottoPrice);
-
         this.purchaseAmount = purchaseAmount;
         this.lottoPrice = lottoPrice;
         numberOfLottoPurchased = purchaseAmount.divide(lottoPrice, BigDecimal.ROUND_DOWN);
@@ -21,5 +17,13 @@ public class LottoPurchaseInfo {
         if (num.remainder(factor).compareTo(BigDecimal.ZERO) != 0) {
             throw new IllegalArgumentException(AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS.getMessage());
         }
+    }
+
+    public BigDecimal getLottoPrice() {
+        return lottoPrice;
+    }
+
+    public BigDecimal getNumberOfLottoPurchased() {
+        return numberOfLottoPurchased;
     }
 }
--- src/test/java/lotto/LottoPurchaseInfoTest.java
+++ src/test/java/lotto/LottoPurchaseInfoTest.java
@@ -11,7 +11,7 @@ public class LottoPurchaseInfoTest {
     @Test
     @DisplayName("구매 금액에 맞게 lotto 개수를 생성한다.")
     void createNumberOfLottoPurchasedByPurchasedAmount() {
-        assertThat(new LottoPurchaseInfo(new BigDecimal(321000), Lotto.PRICE).numberOfLottoPurchased
+        assertThat(new LottoPurchaseInfo(new BigDecimal(321000), Lotto.PRICE).getNumberOfLottoPurchased()
                 .compareTo(BigDecimal.valueOf(321)))
                 .isEqualTo(0);
     }

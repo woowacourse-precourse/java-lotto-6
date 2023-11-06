4e6cadd1eea87eff924428ced19bb7f212f62592
tackyu97 <tack970913@gmail.com>
2023-11-06 20:56:46 +0900

    feat: 로또 검증 기능 추가
    중복 확인 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,7 +15,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        HashSet<Integer> nonDuplicate = new HashSet<>(numbers);
+        if (nonDuplicate.size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    // TODO: 추가 기능 구현
+
 }

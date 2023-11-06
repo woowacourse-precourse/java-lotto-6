73ec29a849d819b36cc4b37808e75fc8a19a964c
seona-moon <fhflwhwl5@gmail.com>
2023-11-06 12:29:54 +0900

    test(LottoTest): Lotto 클래스에서 발생할 수 있는 예외 설계

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,25 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    @DisplayName("로또 번호가 잘 생성되는지 확인한다.")
+    @Test
+    void createBasicLotto() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));;
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -23,5 +35,46 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    //TODO 비슷한 테스트케이스, List값만 바꿔도 될 수 있도록 공부해서 수정
+    @DisplayName("로또 번호에 숫자가 아닌 문자가 포함되어 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByContainLetter() {
+        /**
+         * List.of(1,2,3,4,5,-1)
+         * List.of(1,2,3,4,5,a)
+         * List.of(1,2,3,4,5,0.1)
+         * List.of(1,2,3,4,5,*)
+         */
+        assertThatThrownBy(() -> new Lotto(List.of())).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1~45 사이의 값이 아니면 예외가 발생한다. ")
+    @Test
+    void createLottoByInvalidNumber() {
+        /**
+         * List.of(1,2,3,4,5,55)
+         * List.of(1,2,3,4,5,0)
+         * List.of(1,2,3,4,5,100)
+         */
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 Empty 값이 있으면 예외가 발생한다. ")
+    @Test
+    void createLottoByEmptyValue() {
+        //이 경우, 애초에 공백 문자가 들어갈 수 없는 구조라 예외 발생이 어려움. 기능명세서 수정.
+        String input = "1,2,3,4,,6";
+        List<Integer> lottoNumbers = Arrays.stream(input.split(","))
+            .map(String::trim)
+            .filter(s -> !s.isEmpty())
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

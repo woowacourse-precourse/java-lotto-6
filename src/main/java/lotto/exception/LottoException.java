ff3fa4418bdceb32c39ff45068832a748bd10e99
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 08:28:52 +0900

    feat(LottoException) : 로또 예외처리 기능 수정 생성자로 한번에 묶어서 전달하도록 수정

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -12,6 +12,13 @@ public class LottoException {
     private static final String IS_RIGHT_RANGE_MESSAGE = "로또 번호는 1부터 45 사이의 숫자로만 구성되어야 합니다.";
     private static final String IS_NOT_ASCENDING_MESSAGE = "로또 번호가 오름차순이 아닙니다";
 
+    public LottoException(List<Integer> input){
+        isSize(input);
+        isDuplicate(input);
+        isAscending(input);
+        isRightRange(input);
+    }
+
     public void isDuplicate(List<Integer> input) {
         if(input.size() != input.stream().distinct().count()){
             throw new IllegalArgumentException(IS_DUPLICATE_MESSAGE);

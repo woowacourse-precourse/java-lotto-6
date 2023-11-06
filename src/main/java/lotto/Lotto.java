bded1f65a386a14b6be3a06baa94f5a5014806c2
Elliot-KJW <eidechse4@hanmail.net>
2023-11-06 18:24:57 +0900

    feat(Lotto): 로또 발행 기능 추가
    Lotto에 매개변수 없는 생성자를 실행시키면 자동으로 6개의 숫자가 담긴 리스트가 생성된다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import validation.IntegerValidator;
+import validation.ListValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,10 +15,22 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Lotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Const.LOTTO_BEGIN, Const.LOTTO_END, Const.LOTTO_NUMBERS);
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != Const.LOTTO_NUMBERS) {
+            throw new IllegalArgumentException("[ERROR] : 입력 숫자는 " + Const.LOTTO_NUMBERS + "개여야 합니다.");
         }
+
+        for(int number : numbers) {
+            IntegerValidator.checkRange(number, Const.LOTTO_BEGIN, Const.LOTTO_END);
+        }
+
+        ListValidator.checkDuplicate(numbers);
     }
 
     // TODO: 추가 기능 구현

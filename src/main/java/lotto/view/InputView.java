abc06c7b27afef15fe63e71eb1b830c0f95a0fff
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-06 23:41:30 +0900

    feat(InputView): 입력 뷰 단순 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readMoney() {
+        return Console.readLine();
+    }
+
+    public String readWinnerNumber() {
+        return Console.readLine();
+    }
+
+    public String readBonusNumber() {
+        return Console.readLine();
+    }
+}

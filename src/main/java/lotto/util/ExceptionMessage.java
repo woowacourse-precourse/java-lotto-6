9f2c699a6d96a7479a082197e3735e18c7e56945
kylim99 <zxzx05050@naver.com>
2023-11-06 12:42:30 +0900

    feat: 에러 매시지를 구현

---
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -0,0 +1,23 @@
+package lotto.util;
+
+public enum ExceptionMessage {
+
+    INVALID_NOT_NATURAL_NUMBER("자연수(1000원 단위)만 입력 가능합니다."),
+    INVALID_INPUT_LENGTH("6개의 숫자를 입력해주세요."),
+    INVALID_INPUT_RANGE("1 ~ 45 사이 숫자만 입력이 가능합니다."),
+    INVALID_INPUT_DUPLICATE("중복된 숫자는 허용하지 않습니다.")
+    ;
+
+
+
+    public static final String BASE_MESSAGE = "[ERROR] %s";
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = String.format(BASE_MESSAGE, message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

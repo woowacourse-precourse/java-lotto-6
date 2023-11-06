26f50794369637b778880386fab88fbd806767ae
sojungpp <sophie2458@naver.com>
2023-11-07 02:20:53 +0900

    feat: 구입 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        getPurchasePrice();
+
+    }
+
+    private static void getPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            Integer money = Integer.parseInt(Console.readLine());
+            validPrice(money);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 구입금액은 숫자여야 합니다.");
+            getPurchasePrice();
+        }
+    }
+
+    private static void validPrice(Integer money) {
+        if (money % 1000 != 0) {
+            System.out.println("[ERROR] 로또 1개의 가격은 1,000원입니다. 올바른 구입금액을 입력하셔야 합니다.");
+            getPurchasePrice();
+        }
     }
 }

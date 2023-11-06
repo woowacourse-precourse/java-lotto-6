7307a4be969e6739e9f227207a0e5071d67661cf
박정현 <ros28081@naver.com>
2023-11-06 21:04:18 +0900

    feat(Application): 로또 구매 금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     private static final int LOTTO_PRICE = 1_000;
     private static final int LOTTO_SIZE = 6;
     private static int purchasedPrice;
 
     public static void main(String[] args) {
+        readPurchasedPrice();
 
+    }
+
+    public static void readPurchasedPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        purchasedPrice = Integer.parseInt(Console.readLine());
     }
 
     public static int getPurchasedPrice() {

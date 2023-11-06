cc178cf8a91d16661dfc1fbba01be8a6becf861d
doka <posmane5@gmail.com>
2023-11-06 18:51:53 +0900

    feat(Instructions) : "view) 발행된 로또 출력" 기능의 상수값 생성
    - MESSAGE_OF_PURCHASE_COUNT : enum에 구입한 로또 티켓 갯수를 출력하는 메시지 저장

--- src/main/java/lotto/view/constant/Instructions.java
+++ src/main/java/lotto/view/constant/Instructions.java
@@ -1,7 +1,9 @@
 package lotto.view.constant;
 
 public enum Instructions {
-    MESSAGE_OF_ENTERING_LOTTO_AMOUNT("구입금액을 입력해 주세요.");
+    MESSAGE_OF_ENTERING_LOTTO_AMOUNT("구입금액을 입력해 주세요."),
+
+    MESSAGE_OF_PURCHASE_COUNT("\n%d개를 구매했습니다.\n");
 
     private final String prompts;
 

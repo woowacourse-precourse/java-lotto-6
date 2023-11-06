09327dca28405beb27f8aeb6d6cb6ef0574b4d05
DooDuZ <sin9158@naver.com>
2023-11-07 02:01:42 +0900

    fix: 로또 번호 검증 메서드 수정
    리턴타입 boolean -> void 변경

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -9,22 +9,19 @@ public class LottoNumberValidator extends Validator{
     private final static int LOTTO_NUMBER_COUNT = 6;
     private final static int MIN_LOTTO_NUMBER = 1;
     private final static int MAX_LOTTO_NUMBER = 45;
-    public static boolean hasDuplicateNumbers(List<Integer> numbers){
+    public static void hasDuplicateNumbers(List<Integer> numbers){
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
 
         if (uniqueNumbers.size()!=LOTTO_NUMBER_COUNT){
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER_MESSAGE.getMessage());
         }
-
-        return true;
     }
 
-    public static boolean validateLottoNumbersRange(List<Integer> numbers){
+    public static void validateLottoNumbersRange(List<Integer> numbers){
         for (Integer number : numbers){
             if(!validateNumberRange(number, MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)){
                 throw new IllegalArgumentException(ErrorMessage.LOTTONUMBER_OUT_OF_RANGE_MESSAGE.getMessage());
             }
         }
-        return true;
     }
 }

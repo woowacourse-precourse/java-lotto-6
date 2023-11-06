07f7270aab7de090c5878d3c1ef0ea3add8a0343
yubin <dbqls2821@gmail.com>
2023-11-06 18:02:22 +0900

    refactor: BonusNumber record 클래스의 메서드 초기화블록 내부로 이동

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,17 +1,9 @@
 package lotto.domain;
 
-record BonusNumber(Integer bonus, Lotto lotto) {
+record BonusNumber(int bonus, Lotto lotto) {
     BonusNumber {
-        validateRange();
-        validateDuplication();
-    }
-
-    void validateRange() {
         if(bonus < Constraint.MIN.getValue() || bonus > Constraint.MAX.getValue())
             throw new IllegalArgumentException(DomainException.BONUS_RANGE.getMessage());
-    }
-
-    void validateDuplication() {
         if(lotto.contains(bonus))
             throw new IllegalArgumentException(DomainException.BONUS_DUPLICATION.getMessage());
     }

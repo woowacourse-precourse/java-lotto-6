2e58436aacb75a451da91315b6fcddb799bdd0ed
kkw2758 <dtc3229@gmail.com>
2023-11-07 00:12:56 +0900

    Feat(Grade) : 점수에 따라 당첨 금액을 매핑하는 기능 추가

--- src/main/java/constants/Grade.java
+++ src/main/java/constants/Grade.java
@@ -4,20 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Grade {
-    FIRST(6),
-    SECOND(5.5),
-    THIRD(5),
-    FOURTH(4),
-    FIFTH(3);
+    FIRST(6, 2000000000),
+    SECOND(5.5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
 
     private final double point;
-
-    Grade(double point) {
+    private final int prizeMoney;
+    Grade(double point,int prizeMoney) {
         this.point = point;
+        this.prizeMoney = prizeMoney;
+
     }
 
     public double getPoint() {
         return point;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
     }
 
     public static List<Grade> getAllGrades() {

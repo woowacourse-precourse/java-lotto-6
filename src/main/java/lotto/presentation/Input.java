0dcec0f5d430532eeddc7d2814da3bf790c5db3e
olive-su <1466su@gmail.com>
2023-11-07 00:11:46 +0900

    feat: 입력 모듈 구현

---
+++ src/main/java/lotto/presentation/Input.java
@@ -0,0 +1,27 @@
+package lotto.presentation;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Input {
+    final private String INPUT_PURCHASE_PRICE_COMMAND = "구입금액을 입력해 주세요.";
+    final private String INPUT_WINNING_NUMBER_COMMAND = "당첨 번호를 입력해 주세요.";
+    final private String INPUT_BONUS_NUMBER_COMMAND = "보너스 번호를 입력해 주세요.";
+
+    public String inputPurchasePrice(){
+        System.out.println(INPUT_PURCHASE_PRICE_COMMAND);
+        String purchasePrice = readLine();
+        return purchasePrice;
+    }
+
+    public String[] inputWinningNumber(){
+        System.out.println(INPUT_WINNING_NUMBER_COMMAND);
+        String[] winningNumber = readLine().split(",");
+        return winningNumber;
+    }
+
+    public String inputBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER_COMMAND);
+        String bonusNumber = readLine();
+        return bonusNumber;
+    }
+}

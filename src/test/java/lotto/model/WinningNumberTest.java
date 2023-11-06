da2731789d8b1f7217827c4263668d683021a2a1
sehee <tpgml701@naver.com>
2023-11-06 21:35:28 +0900

    test: WinningNumbers 테스트 케이스 추가

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -1,17 +1,37 @@
 package lotto.model;
 
+import lotto.dto.Lotto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class WinningNumberTest {
 
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
-    public void testObjectCreation() {
-        WinningNumbers winningNumber1 = new WinningNumbers(List.of(1,2,3,4,5,6),7);
-        WinningNumbers winningNumber2 = new WinningNumbers(List.of(1,2,3,4,5,6),7);
-        Assertions.assertEquals(winningNumber1, winningNumber2);
+    void createWinningNumbersByOverSize() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7),8))
+                .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외를 발생한다.")
+    @Test
+    void createWinningNumbersByDuplicateBonus() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6),6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 번호가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByDuplicate() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5),6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 
 }

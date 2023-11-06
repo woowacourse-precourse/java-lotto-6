089b733473fc6a11e5bc319f9593667800680c95
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-06 14:11:00 +0900

    refactor: (InputView) 로또 번호 출력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,29 +9,34 @@ public class InputView {
 
     public String InputBudget(){
         //문장 출력
-        System.out.println(ProcessMessage.PLEASE_INPUT_BUDGET.getMessage());
+        System.out.print(ProcessMessage.PLEASE_INPUT_BUDGET.getMessage());
         String budget = Console.readLine();
-        Console.close();
         //유효성 검사
         inputValidator.validateInputBudgetIsNumber(budget);
         return budget;
     }
 
     public String InputWinningNumbers(){
-        System.out.println(ProcessMessage.PLEASE_INPUT_WINNING_NUMBERS.getMessage());
+        System.out.print(ProcessMessage.PLEASE_INPUT_WINNING_NUMBERS.getMessage());
         String winningNumbers = Console.readLine();
-        Console.close();
         //유효성 검사
         inputValidator.validateInputWinningNumbers(winningNumbers);
         return winningNumbers;
     }
 
     public String InputBonusNumber(){
-        System.out.println(ProcessMessage.PLEASE_INPUT_BONUS_NUMBER.getMessage());
+        System.out.print(ProcessMessage.PLEASE_INPUT_BONUS_NUMBER.getMessage());
         String bonusNumber = Console.readLine();
         Console.close();
         //유효성 검사
         inputValidator.validateInputBonusNumber(bonusNumber);
         return bonusNumber;
+    }
+
+    public void displayLottos(int lottosSize, String lottosForDisplay){
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format(ProcessMessage.PURCHASE_LOTTO.getMessage(),lottosSize));
+        sb.append(lottosForDisplay);
+        System.out.print(sb);
     }
 }

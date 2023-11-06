85495e66fef7f00aa47383c1bb356a95d7b2c55c
mmalfoy <stpmalfoy@gmail.com>
2023-11-06 19:35:06 +0900

    feat(Application) : 미션-로또 기능 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.LottoModel;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoModel lottoModel = new LottoModel();
+        lottoModel.run();
     }
 }

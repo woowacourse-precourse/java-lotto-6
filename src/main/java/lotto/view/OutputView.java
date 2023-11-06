a663ba8f40e4932f478b22b07d784e1f95b50c64
jongheonleee <qwefghnm1212@gmail.com>
2023-11-06 19:24:22 +0900

    [refactor] OutputView 클래스 StringBuilder를 활용해서 출력문 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -13,50 +13,87 @@ import static lotto.util.PatternUnits.PATTERN_FOR_DECIMAL_FORMAT;
 public class OutputView {
 
     private final DecimalFormat formatter;
+    private final StringBuilder outputBuilder;
     public OutputView() {
         formatter = new DecimalFormat(PATTERN_FOR_DECIMAL_FORMAT.getPattern());
+        outputBuilder = new StringBuilder();
     }
 
     public void printPurchasedLottos(Lottos lottos) {
-        System.out.print(ENTER.getUnit());
-        System.out.printf("%d개를 구매했습니다.", lottos.getCount());
-        System.out.print(ENTER.getUnit());
+        initOutputBuilder();
+        generatePurchasedLottosOutputBuilder(lottos);
+        printOutputBuilder();
+
+    }
+
+    private void generatePurchasedLottosOutputBuilder(Lottos lottos) {
+        outputBuilder.append(ENTER.getUnit())
+                .append(String.format("%d개를 구매했습니다.", lottos.getCount()))
+                .append(ENTER.getUnit());
+
         for (Lotto lotto : lottos.getLottos()) {
-            System.out.println(lotto.toString());
+            outputBuilder.append(lotto.toString())
+                    .append(ENTER.getUnit());
         }
-        System.out.println();
     }
 
     public void printStaticResult(Prizes prizes, Cash cash) {
-        System.out.println("당첨 통계");
-        System.out.print(LINE.getUnit());
+        initOutputBuilder();
+        generateStaticResultOutputBuilder(prizes);
+        generateTotalBenefitOutputBuilder(prizes, cash);
+        printOutputBuilder();
+    }
+
+    private void generateStaticResultOutputBuilder(Prizes prizes) {
+        outputBuilder.append("당첨 통계")
+                .append(LINE.getUnit());
         for (Prize prize : Prize.values()) {
-            if (prize == Prize.LAST_PLACE) continue;
-            if (!prize.isBonusNumber()) {
-                System.out.printf("%d개 일치 (%s원) - %d개",
-                        prize.getCountOfMatchedNumber(),
-                        formatter.format(prize.getReward()),
-                        prizes.countPrize(prize));
-            } else if (prize.isBonusNumber()){
-                System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개",
-                        prize.getCountOfMatchedNumber(),
-                        formatter.format(prize.getReward()),
-                        prizes.countPrize(prize));
-            }
-            System.out.println();
+            generateRankingResultOutputBuilder(prize, prizes.countPrize(prize));
+            outputBuilder.append(ENTER.getUnit());
+        }
+    }
+
+    private void generateRankingResultOutputBuilder(Prize prize, Integer countOfPrize) {
+        if (prize.isBonusNumber()) {
+            generateSecondPlaceResultOutputBuilder(prize, countOfPrize);
+        } else if (!prize.isBonusNumber() && prize != Prize.LAST_PLACE) {
+            generateOtherPlaceResultOutputBuilder(prize, countOfPrize);
         }
 
+    }
 
-        String prefix = String.format("총 수익률은 %.1f", prizes.getRoundedTotalBenefit(
-                cash.getDepositAmount()));
+    private void generateSecondPlaceResultOutputBuilder(Prize prize, Integer countOfPrize) {
+        outputBuilder.append(String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개",
+                prize.getCountOfMatchedNumber(),
+                formatter.format(prize.getReward()),
+                countOfPrize));
+    }
 
-        System.out.println(prefix + "%입니다.");
+    private void generateOtherPlaceResultOutputBuilder(Prize prize, Integer countOfPrize) {
+        outputBuilder.append(String.format("%d개 일치 (%s원) - %d개",
+                prize.getCountOfMatchedNumber(),
+                formatter.format(prize.getReward()),
+                countOfPrize));
+    }
+
+    private void generateTotalBenefitOutputBuilder(Prizes prizes, Cash cash) {
+        outputBuilder.append(String.format("총 수익률은 %.1f", prizes.getRoundedTotalBenefit(cash.getDepositAmount())));
+        outputBuilder.append("%입니다.");
     }
 
     public void printErrorMessage(Exception e) {
-        System.out.println(e.getMessage());
-        System.out.println();
+        initOutputBuilder();
+        outputBuilder.append(e.getMessage())
+                     .append(ENTER.getUnit());
+        printOutputBuilder();
+    }
+
+    private void printOutputBuilder() {
+        System.out.print(outputBuilder);
     }
 
 
+    private void initOutputBuilder() {
+        outputBuilder.setLength(0);
+    }
 }

22be98f907f84bacc88234dc04d427cc0f56cf31
jisu-om <lifyji@naver.com>
2023-11-06 20:29:45 +0900

    test: BonusNumberTest 리팩토링(@ParameterizedTest 적용)

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,69 +1,33 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BonusNumberTest {
-    String input;
-
     @DisplayName("보너스넘버는 1이상 45 이하의 숫자여야 한다.")
     @Test
     void createBonusNumber() {
         // given
-        input = "1";
+        String input = "1";
 
         // when
         BonusNumber bonusNumber = BonusNumber.create(input);
 
         // then
         assertThat(bonusNumber).isNotNull();
+        assertThat(bonusNumber.getNumber()).isBetween(1, 45);
+        assertThat(bonusNumber.getNumber()).isEqualTo(1);
     }
 
-    @DisplayName("보너스넘버가 공백이면 예외 발생한다.")
-    @Test
-    void cannotCreateBonusNumber_blank() {
-        // given
-        input = "";
-
-        // when, then
-        assertThatThrownBy(() -> BonusNumber.create(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스넘버가 숫자가 아니면 예외 발생한다.")
-    @Test
-    void cannotCreateBonusNumber_notNumeric() {
-        // given
-        input = "ㅈㄷ";
-
-        // when, then
-        assertThatThrownBy(() -> BonusNumber.create(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스넘버가 1이상 45이하가 아니면 예외 발생한다.")
-    @Test
-    void cannotCreateBonusNumber_invalidRange1() {
-        // given
-        input = "47";
-
-        // when, then
-        assertThatThrownBy(() -> BonusNumber.create(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스넘버가 1이상 45이하가 아니면 예외 발생한다.")
-    @Test
-    void cannotCreateBonusNumber_invalidRange2() {
-        // given
-        input = "0";
-
-        // when, then
-        assertThatThrownBy(() -> BonusNumber.create(input))
+    @ParameterizedTest(name = "[{index}] 보너스 넘버가 ''{0}'' 이면 예외 발생한다.")
+    @ValueSource(strings = {"", "we", "47", "0", "-1"})
+    //when, then
+    void cannotCreateBonusNumber(String element) {
+        assertThatThrownBy(() -> BonusNumber.create(element))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

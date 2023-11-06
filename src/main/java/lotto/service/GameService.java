fb13c908d86a4c935b05b66dd7ee853f113f156c
minsub <chrkb1569@naver.com>
2023-11-06 17:34:33 +0900

    feat : GameService method 추가 및 접근제어자 변경

--- src/main/java/lotto/service/GameService.java
+++ src/main/java/lotto/service/GameService.java
@@ -1,21 +1,46 @@
 package lotto.service;
 
-import lotto.model.Lotto;
+import lotto.dto.LottoDto;
+import lotto.model.Game;
+import lotto.util.LottoNumberFactory;
+import lotto.util.NumberFactory;
 
 import java.util.List;
 
+import static lotto.util.LottoResult.*;
+
 public class GameService {
-    public int getMatchedLottoNumber(Lotto myLotto, List<Integer> prizeNumber) {
-        int matchedCount = 0;
+    private final NumberFactory numberFactory;
 
-        for(int lottoNumber : myLotto.getNumbers()) {
-            if(prizeNumber.contains(lottoNumber)) matchedCount++;
-        }
-
-        return matchedCount;
+    public GameService(LottoNumberFactory lottoNumberFactory) {
+        this.numberFactory = lottoNumberFactory;
     }
 
-    public boolean getBonusResult(Lotto myLotto, int bonusNumber) {
-        return myLotto.getNumbers().contains(bonusNumber);
+    public void purchaseLotto(Game game) {
+        int lottoQuantity = game.getLottoQuantity();
+
+        for(int lotto = 0; lotto < lottoQuantity; lotto++) {
+            List<Integer> numbers = numberFactory.getNumbers();
+            game.buyLotto(numbers);
+        }
+    }
+
+    public void getLottoResult(Game game, List<Integer> winnerNumber, int bonusNumber) {
+        List<LottoDto> lottoNumbers = game.getLottoNumbers();
+
+        for(LottoDto dto : lottoNumbers) {
+            long matchedNumber = getMatchedLottoNumber(dto.getLottoNumber(), winnerNumber);
+            boolean bonusResult = getBonusResult(dto.getLottoNumber(), bonusNumber);
+            int grade = getGrade(matchedNumber, bonusResult);
+            game.winTheGame(grade);
+        }
+    }
+
+    private long getMatchedLottoNumber(List<Integer> myLotto, List<Integer> winnerNumber) {
+        return myLotto.stream().filter(winnerNumber::contains).count();
+    }
+
+    private boolean getBonusResult(List<Integer> myLotto, int bonusNumber) {
+        return myLotto.contains(bonusNumber);
     }
 }

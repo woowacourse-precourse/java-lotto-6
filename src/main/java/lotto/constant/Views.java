58c12cb7e4b14c2fdc7b456ac1de8662dbb55ae8
Seyeon <wjdtpdus828@naver.com>
2023-11-06 18:12:46 +0900

    feat(Views): 출력 메세지에 대한 enum 클래스 Views 생성

--- src/main/java/lotto/constant/Views.java
+++ src/main/java/lotto/constant/Views.java
@@ -1,4 +1,15 @@
 package lotto.constant;
 
 public enum Views {
+    PURCHASE_AMOUT_MESSAGE("구입금액을 입력해 주세요.");
+
+    private final String message;
+
+    Views(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }

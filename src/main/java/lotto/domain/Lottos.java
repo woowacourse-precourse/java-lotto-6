e139841df9bb686312f0fc04c8b71ac5e9e0706b
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-06 23:38:27 +0900

    feat(Lottos): dto로 변환시키는 기능 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,18 +1,27 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import lotto.Dto.MyLottosDto;
 
 public class Lottos {
     private final List<Lotto> Lottos = new ArrayList<>();
 
-    public void add(Lotto lotto){
+    public void add(Lotto lotto) {
         Lottos.add(lotto);
     }
-    public int size(){
+
+    public int size() {
         return Lottos.size();
     }
 
-
+    public MyLottosDto toDto() {
+        List<List<Integer>> result = new ArrayList<>();
+        for (Lotto lotto : Lottos) {
+            List<Integer> tmp = new ArrayList<>();
+            lotto.stamp(tmp);
+            result.add(tmp);
+        }
+        return new MyLottosDto(result);
+    }
 }

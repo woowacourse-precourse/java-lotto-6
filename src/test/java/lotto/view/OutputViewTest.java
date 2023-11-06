38bdc4917c8f4a42d89b508a51aef544906a2d29
82user <zxcvc9999@gmail.com>
2023-11-07 00:49:12 +0900

    test: print issuing lottery ticket

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,14 +1,18 @@
 package lotto.view;
 
+import lotto.domain.Buyer;
+import lotto.domain.Lotto;
 import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 class OutputViewTest {
 
     OutputView outputView = new OutputView();
+    LottoService lottoService = new LottoService();
 
     @Test
     @DisplayName("구입 금액 입력 안내")
@@ -20,7 +24,8 @@ class OutputViewTest {
     @Test
     @DisplayName("구매 완료 및 발행 내역 안내")
     void outputIssuingLotteryTicket() {
-        System.setIn(new ByteArrayInputStream("14000".getBytes()));
-        outputView.outputIssuingLotteryTicket();
+        System.setIn(new ByteArrayInputStream("5000".getBytes()));
+        Buyer buyer = lottoService.issueLotteryTicketAll();
+        outputView.outputIssuingLotteryTicket(buyer.getLottos(), lottoService.getTicketCount());
     }
 }

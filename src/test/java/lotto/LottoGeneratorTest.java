6801bcd5c60eaef6518df3e95af567a66681a59d
msung99 <msung6924@naver.com>
2023-11-06 13:03:36 +0900

    test: 유저 로또 생성 전략으로 만들어진 Lottos 의 크기가 getCountOfLottoLines 의 반환값과 동일한지 테스트

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.List;
+import lotto.domain.Budget;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.Lottos;
+import lotto.domain.strategy.UserLottoGenerateStrategy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoGeneratorTest {
+    private LottoGenerator lottoGenerator;
+    private Budget budget;
+    private Lottos lottos;
+
+    @BeforeEach
+    void setUp(){
+        lottoGenerator = new LottoGenerator();
+        budget = Budget.from("3000");
+    }
+
+    @DisplayName("유저 로또 생성 전략으로 만들어진 Lottos 의 크기는 getCountOfLottoLines() 의 반환 결과값과 같아야한다.")
+    @Test
+    void createLottoByUserLottoStrategy(){
+        lottoGenerator.setLottoGenerateStrategy(new UserLottoGenerateStrategy());
+        lottos = lottoGenerator.generateLottosByBudget(budget);
+        Assertions.assertThat(lottos.getLottos().size()).isEqualTo(budget.getCountOfLottoLines());
+    }
+}

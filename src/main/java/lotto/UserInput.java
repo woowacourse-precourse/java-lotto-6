75dac37e1c69eb435b9bce5dd23a847fb0d3b768
jjaegii <hn06038@gmail.com>
2023-11-07 04:52:50 +0900

    feat: 당첨 번호 입력받기

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInput {
     public static int getPurchaseAmount() {
@@ -19,6 +21,21 @@ public class UserInput {
     private static void validatePurchaseAmount(int amount) {
         if (amount <= 0 || amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위의 양수여야 합니다.");
+        }
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        while (true) {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            String input = Console.readLine();
+            String[] numberStrings = input.split(",");
+            List<Integer> numbers = new ArrayList<>();
+            for (String numberString : numberStrings) {
+                numberString = numberString.trim();
+                int number = Integer.parseInt(numberString);
+                numbers.add(number);
+            }
+            return numbers;
         }
     }
 }

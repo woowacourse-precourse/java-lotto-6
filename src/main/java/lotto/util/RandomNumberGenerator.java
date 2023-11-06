2efe75f8357964750f331596fb75bf7ebc161ce6
Haeun Jeong <gkdms4248@gmail.com>
2023-11-07 01:21:24 +0900

    fix: random 생성 복사 후 sort 로 변경 (UnsupportedOperationException)

--- src/main/java/lotto/util/RandomNumberGenerator.java
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -5,12 +5,15 @@ import static lotto.domain.LottoRule.MINIMUM;
 import static lotto.domain.LottoRule.SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumberGenerator implements Generator {
 
     @Override
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(MINIMUM.getValue(), MAXIMUM.getValue(), SIZE.getValue());
+        List<Integer> randomNumber = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(MINIMUM.getValue(), MAXIMUM.getValue(), SIZE.getValue()));
+        return randomNumber;
     }
 }

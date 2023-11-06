318ba150a4fbc4dfe36a95ea6f53c696f4e535bd
elyudwo <dudqk9696@naver.com>
2023-11-06 11:14:24 +0900

    [Feat] : 입력 정답 로또 번호 String -> List<Integer> 변환 기능 구현

--- src/main/java/lotto/utils/StringUtil.java
+++ src/main/java/lotto/utils/StringUtil.java
@@ -1,9 +1,24 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringUtil {
 
     public static int stringToInt(String inputValue) {
         InputValidator.validateAmountNotNumber(inputValue);
         return Integer.parseInt(inputValue);
+    }
+
+    public static List<Integer> StringToList(String inputValue) {
+        InputValidator.validateAnswerNumberIsNumber(inputValue);
+        return parseAnswerNumberToIntegerList(inputValue);
+    }
+
+    private static List<Integer> parseAnswerNumberToIntegerList(String inputValue) {
+        return Arrays.stream(inputValue.split(","))
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt)
+                .toList();
     }
 }

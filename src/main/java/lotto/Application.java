8fd73cfe2518859df77f0b48a51fa5f378d08998
hong <kevin0928@naver.com>
2023-11-06 14:58:52 +0900

    feat : LottoController의 생성자를 통해 InputView 의존성 주입
    - 필드 주입이 아닌 생성자 주입으로 의존성을 주입해 객체가 필요로 하는 의존성을 알 수 있게 함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        LottoController lottoController = new LottoController(inputView);
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,11 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoController {
+    private final InputView inputView;
+
+    public LottoController(InputView inputView) {
+        this.inputView = inputView;
+    }
 }

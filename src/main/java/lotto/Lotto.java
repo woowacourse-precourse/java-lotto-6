b21d52647f47e0239bf3ea4186ab3f268c7bddaa
peeaaccee <wlgp8958@naver.com>
2023-11-06 16:22:45 +0900

    feat(Lotto): 로또 번호가 1~45의 범위인지 확인하는 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Lotto {
     private static final String VALIDATE_COUNT_MASSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String VALIDATE_RANGE_MASSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,6 +14,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -21,5 +23,12 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int a : numbers) {
+            if (a < 1 || a > 45) {
+                throw new IllegalArgumentException(VALIDATE_RANGE_MASSAGE);
+            }
+        }
+    }
+
 }

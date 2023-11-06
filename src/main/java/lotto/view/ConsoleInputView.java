7fd5abe427211d3c68125a259669da670c12bba5
yoonion <yoonion97@gmail.com>
2023-11-06 23:17:36 +0900

    feat: 사용자에게 보너스 번호를 입력받는 메서드 구현

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -6,6 +6,7 @@ public class ConsoleInputView {
 
     private static final String BUY_LOTTO_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String USER_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String USER_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String inputBuyLottoAmount() {
         System.out.println(BUY_LOTTO_AMOUNT_MESSAGE);
@@ -14,6 +15,11 @@ public class ConsoleInputView {
 
     public String inputUserLottoNumbers() {
         System.out.println(USER_LOTTO_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputUserLottoBonusNumber() {
+        System.out.println(USER_LOTTO_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }

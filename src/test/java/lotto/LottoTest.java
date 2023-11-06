c4ef3da0f492444ba68007762e8692ce06b06c2e
Reddyong <d0209hong@naver.com>
2023-11-06 16:36:18 +0900

    test : validateLottoNumBetween()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -42,4 +42,11 @@ class LottoTest {
         assertThatThrownBy(() -> InputView.validateNum(8500))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호는 1부터 45 사이의 수가 아니면 예외가 발생한다.")
+    @Test
+    void validateLottoNumBetween() {
+        assertThatThrownBy(() -> InputView.validateLottoNum("1,2,3,4,5,48"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

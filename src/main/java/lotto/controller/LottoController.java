c00958f1aa051c847dec25d5dd92804665f5ed8c
yhcho0405 <yhcho0405@kakao.com>
2023-11-06 19:45:04 +0900

    refactor: Controller 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,12 +12,21 @@ public class LottoController {
     }
 
     public void runLottoGame() {
+        buyLottoTickets();
+        setWinningNumbers();
+        printResults();
+    }
+
+    private void buyLottoTickets() {
         // 구입 금액 입력
         OutputView.printRequestPurchaseAmount();
         InputView.readLine();
 
         // 로또 구매 결과 출력
         OutputView.printResponsePurchaseAmount(1);
+    }
+
+    private void setWinningNumbers() {
 
         // 당첨 번호 입력
         OutputView.printRequestWinningNumber();
@@ -26,10 +35,12 @@ public class LottoController {
         // 보너스 번호 입력
         OutputView.printRequestBonusNumber();
         InputView.readLine();
+    }
+
+    private void printResults() {
 
         // 당첨 통계 출력
         OutputView.printResponseWinningStats();
         OutputView.printResponseProfitRate();
     }
-
 }

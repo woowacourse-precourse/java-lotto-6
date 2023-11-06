628d5cf67af35fdd6a92f7c682f864499d5a75b2
이병수 <qudtn0315@naver.com>
2023-11-06 16:11:38 +0900

    refactor (BonusNumber) : Enum문을 통해 코드 개선

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -1,31 +1,32 @@
 package lotto;
 
 import java.util.List;
+import lotto.EnumList.ConstantErrorMessage;
 
 public class BonusNumber {
     private static int number = 0;
 
     public BonusNumber(List<Integer> lottoNumber, String bonus) {
         checkNumber(bonus);
-        duplicateNumber(lottoNumber,bonus);
+        duplicateNumber(lottoNumber, bonus);
         number = Integer.parseInt(bonus);
     }
 
     private void duplicateNumber(List<Integer> lottoNumber, String bonus) {
         int bonusNumber = Integer.parseInt(bonus);
         boolean isPresent = lottoNumber.stream()
-                .anyMatch(number->number==bonusNumber);
+                .anyMatch(number -> number == bonusNumber);
 
-        if(isPresent) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBER_DUPLICATE);
+        if (isPresent) {
+            throw new IllegalArgumentException(ConstantErrorMessage.ERROR_LOTTO_NUMBER_DUPLICATE.errorMessage());
         }
     }
 
     private void checkNumber(String bonus) {
         boolean isNumber = bonus.chars().allMatch(Character::isDigit);
 
-        if(!isNumber) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_NUMBER);
+        if (!isNumber) {
+            throw new IllegalArgumentException(ConstantErrorMessage.ERROR_NUMBER.errorMessage());
         }
     }
 

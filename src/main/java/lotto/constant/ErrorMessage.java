2a45e62cabd9ed58cc0df0a0948c6796df9a50c9
Gyeongho Yang <chchch1213@naver.com>
2023-11-06 18:56:58 +0900

    feat(const): 에러 메세지 접두어 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,6 +3,7 @@ package lotto.constant;
 import static lotto.constant.Config.PRICE_PER_LOTTO;
 
 public class ErrorMessage {
+    public static final String PREFIX = "[ERROR] ";
     public static final String PRICE_NOT_NUMERIC = "금액은 숫자 형식 이어야 합니다.";
     public static final String PRICE_NEGATIVE = "금액은 0 또는 양수 이어야 합니다.";
     public static final String PRICE_NOT_DIVIDED = String.format("금액은 %d으로 나누어 떨어져야 합니다.", PRICE_PER_LOTTO);

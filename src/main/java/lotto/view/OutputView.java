a9688c6886d985570254278e4758cf7c6d50a8d1
82user <zxcvc9999@gmail.com>
2023-11-06 22:57:52 +0900

    feat: print purchase price

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,18 @@ package lotto.view;
 
 public class OutputView {
 
-    public void outputPurchaseAmount() {
+    private static final String INPUT_PURCHASE_PRICE_SENTENCE = "구입금액을 입력해 주세요.";
+    private static final String OUTPUT_PURCHASE_COUNT_SENTENCE = "개를 구매했습니다.";
+    private static final String INPUT_WINNING_NUMBERS_SENTENCE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_SENTENCE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTICS = "당첨 통계" + "\n" + "---";
 
+    InputView inputView = new InputView();
+
+    public void outputPurchasePrice() {
+        System.out.println(INPUT_PURCHASE_PRICE_SENTENCE);
+        System.out.println(inputView.inputPurchasePrice());
+        System.out.println();
     }
 
     public void outputIssuingLotteryTicket() {

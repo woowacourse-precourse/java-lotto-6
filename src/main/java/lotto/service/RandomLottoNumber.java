2870c28089eb5dc85738667875efa7e120d4c2f1
ehddbs4521 <ehddbs452100@naver.com>
2023-11-06 23:17:45 +0900

    feat: 구매 갯수만큼 로또 갯수 반환

--- src/main/java/lotto/service/RandomLottoNumber.java
+++ src/main/java/lotto/service/RandomLottoNumber.java
@@ -1,14 +1,27 @@
 package lotto.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 
 public class RandomLottoNumber {
 
     private static final int MIN=1;
     private static final int MAX=1;
     private static final int COUNT=1;
-    private List<Integer> generateRandomLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(MIN, MAX, COUNT);
+    private Lotto generateRandomLottoNumbers() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(MIN, MAX, COUNT));
+    }
+
+    private List<Lotto> generateTotalLottoNumbers(int count) {
+        List<Lotto> totalLotoNumber = new ArrayList<>();
+
+        while (totalLotoNumber.size() < count) {
+            totalLotoNumber.add(generateRandomLottoNumbers());
+        }
+
+        return totalLotoNumber;
+
     }
 }

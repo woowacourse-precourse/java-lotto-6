0e25c2ce1c674edbd96b73739789c16e674c0249
sunghyun <shlim0287@naver.com>
2023-11-06 19:13:37 +0900

    refactor : 로또리스트 초기화 코드 추가.

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,11 +1,13 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
     private List<Lotto> lottos;
 
     public void addLotto (Lotto lotto){
+        lottos=new ArrayList<>();
         lottos.add(lotto);
     }
 

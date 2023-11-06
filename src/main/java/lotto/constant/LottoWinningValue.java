a1c8a75b2cea904a4f141975c3f5dc9cf0170fd7
YuJeongYun <yunyj99@naver.com>
2023-11-07 02:12:22 +0900

    refactor: 로또 당첨 상수 클래스명 변경

--- src/main/java/lotto/constant/LottoWinningValue.java
+++ src/main/java/lotto/constant/LottoWinningValue.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public enum LottoWinning {
+public enum LottoWinningValue {
     FIRST_PLACE(1, 6, 0, "6개 일치 (2,000,000,000원)"),
     SECOND_PLACE(2, 5, 1, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
     THIRD_PLACE(3, 5, 0, "5개 일치 (1,500,000원)"),
@@ -16,7 +16,7 @@ public enum LottoWinning {
     private int bonusNumberCnt;
     private String winnings;
 
-    LottoWinning(int rank, int winningNumberCnt, int bonusNumberCnt, String winnings) {
+    LottoWinningValue(int rank, int winningNumberCnt, int bonusNumberCnt, String winnings) {
         this.rank = rank;
         this.winningNumberCnt = winningNumberCnt;
         this.bonusNumberCnt = bonusNumberCnt;
@@ -25,10 +25,10 @@ public enum LottoWinning {
 
     // 모든 상수에 대한 맵을 BY_RANK라는 private static final 맵으로 미리 만들어 놓음
     // 이 맵은 rank를 키로 하고 해당 열거형 상수를 값으로 가지고 있다
-    private static final Map<Integer, LottoWinning> BY_RANK =
-            Stream.of(values()).collect(Collectors.toMap(LottoWinning::getRank, e -> e));
+    private static final Map<Integer, LottoWinningValue> BY_RANK =
+            Stream.of(values()).collect(Collectors.toMap(LottoWinningValue::getRank, e -> e));
 
-    public static LottoWinning valueOfRank(int rank) {
+    public static LottoWinningValue valueOfRank(int rank) {
         return BY_RANK.get(rank);
     }
 

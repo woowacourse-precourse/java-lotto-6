f1cea241324fadf845752fe0cd4f4a57a5cbbd83
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-11-06 22:31:11 +0900

    FEAT / modify sort

--- src/main/java/lotto/util/RandomValues.java
+++ src/main/java/lotto/util/RandomValues.java
@@ -1,10 +1,15 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomValues {
     public static List<Integer> getRandomValues() {
-        return Randoms.pickUniqueNumbersInRange(Constant.MIN_LOTTO_VALUE, Constant.MAX_LOTTO_VALUE, Constant.LOTTO_NUMBER);
+        List<Integer> result = Randoms.pickUniqueNumbersInRange(Constant.MIN_LOTTO_VALUE, Constant.MAX_LOTTO_VALUE,
+                Constant.LOTTO_NUMBER);
+        Collections.sort(result);
+
+        return result;
     }
 }

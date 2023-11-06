174a8c37aed9c80089a413f84cbf1b1906932fef
jang010505 <jang010505@inu.ac.kr>
2023-11-07 03:56:48 +0900

    feat: createLottoNumber 함수 코드 작성

--- src/main/java/lotto/utils/RandomUtil.java
+++ src/main/java/lotto/utils/RandomUtil.java
@@ -1,9 +1,18 @@
 package lotto.utils;
 
+import static lotto.constant.SystemConstant.NUMBER_COUNT;
+import static lotto.constant.SystemConstant.NUMBER_MAX;
+import static lotto.constant.SystemConstant.NUMBER_MIN;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomUtil {
     public static List<Integer> createLottoNumber() {
-        return null;
+        List<Integer> numbers =  Randoms.pickUniqueNumbersInRange(NUMBER_MIN, NUMBER_MAX, NUMBER_COUNT);
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }

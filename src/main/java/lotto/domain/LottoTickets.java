fff7b00a7801a1897e061a82504f22e9c51f45d1
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-06 11:34:59 +0900

    refactor(LottoTickets) : 메서드를 기능별로 분리

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,37 +1,38 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoTickets {
     private static final int STANDARD_OF_DIVIDE = 1000;
-    private final List<Lotto> lottoTickets;
+    private final List<Lotto> tickets;
 
-    private LottoTickets(List<Lotto> lottoTickets) {
-        this.lottoTickets = lottoTickets;
+    private LottoTickets(List<Lotto> tickets) {
+        this.tickets = tickets;
     }
 
     public static LottoTickets of(int purchasePrice, NumberGenerator lottoNumberGenerator) {
         int purchaseCount = calculatePurchaseCount(purchasePrice);
-        List<Lotto> lottos = IntStream.range(0, purchaseCount)
-                .mapToObj(i -> new Lotto(lottoNumberGenerator.generate()))
-                .collect(Collectors.toList());
-        return new LottoTickets(lottos);
+        return new LottoTickets(createPurchasedLottos(purchaseCount, lottoNumberGenerator));
     }
 
     private static int calculatePurchaseCount(int purchasePrice) {
         return purchasePrice / STANDARD_OF_DIVIDE;
     }
 
-    public String getLottoTickets() {
-        StringBuilder lottoPrinter = new StringBuilder();
-        lottoTickets.stream()
-                .forEach(lotto -> lottoPrinter.append(lotto).append("\n"));
-        return lottoPrinter.toString();
+    private static List<Lotto> createPurchasedLottos(int purchaseCount, NumberGenerator lottoNumberGenerator) {
+        return IntStream.range(0, purchaseCount)
+                .mapToObj(i -> new Lotto(lottoNumberGenerator.generate()))
+                .collect(Collectors.toList());
     }
 
-    public int size() {
-        return lottoTickets.size();
+    public List<Lotto> getLottoTickets() {
+        return Collections.unmodifiableList(tickets);
+    }
+
+    public int getTicketCount() {
+        return tickets.size();
     }
 }

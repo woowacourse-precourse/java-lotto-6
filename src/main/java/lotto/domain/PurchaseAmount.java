b54fd03d12e75d969e9ccb6a374bd173242c4449
Hanjaemo <hjm2530@gmail.com>
2023-11-06 23:27:26 +0900

    refactor(PurchaseAmount): 검증 순서 변경

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -11,21 +11,21 @@ public class PurchaseAmount {
     private int amount;
 
     public PurchaseAmount(int amount) {
-        validateUnit(amount);
         validateRange(amount);
+        validateUnit(amount);
         this.amount = amount;
-    }
-
-    private void validateUnit(int amount) {
-        if (amount % LottoConstants.LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.getMessage());
-        }
     }
 
     private void validateRange(int amount) {
         if (amount < MIN_PURCHASE_AMOUNT || amount > MAX_PURCHASE_AMOUNT) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_RANGE.getMessage()
                     .formatted(MIN_PURCHASE_AMOUNT, MAX_PURCHASE_AMOUNT));
+        }
+    }
+
+    private void validateUnit(int amount) {
+        if (amount % LottoConstants.LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.getMessage());
         }
     }
 

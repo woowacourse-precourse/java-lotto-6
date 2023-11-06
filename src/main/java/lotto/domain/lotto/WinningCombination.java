6e0b01658390010393f8c390f4e5a60d5fcd9a2e
minisyu <iswater.kim@gmail.com>
2023-11-06 11:10:01 +0900

    refactor: WinningCombination의 유효성 검사 기능 변경, 당첨 순위를 나타내는 기능 변경

--- src/main/java/lotto/domain/lotto/WinningCombination.java
+++ src/main/java/lotto/domain/lotto/WinningCombination.java
@@ -1,41 +1,38 @@
 package lotto.domain.lotto;
 
+import java.util.List;
+import lotto.domain.validator.LottoValidator;
+
 public class WinningCombination {
     private final Lotto winningNumbers;
     private final BonusNumber bonusNumber;
 
     private WinningCombination(Lotto winningNumbers, BonusNumber bonusNumber) {
-        verifyBonusIsValid(winningNumbers, bonusNumber);
+        LottoValidator.verifyBonusIsNotContainWinningNumbers(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    public static WinningCombination from(Lotto numbers, BonusNumber bonusNumber) {
+    public static WinningCombination of(Lotto numbers, BonusNumber bonusNumber) {
         return new WinningCombination(numbers, bonusNumber);
     }
 
-    private void verifyBonusIsValid(Lotto winningNumbers, BonusNumber bonusNumber) {
-        if (hasBonusNumber(winningNumbers, bonusNumber)) {
-            throw new IllegalArgumentException("입력한 당첨번호와 동일한 숫자를 입력하면 안 됩니다.");
-        }
-    }
-
-    public LottoRank getLottoRankOf(Lotto purchasedLotto) {
-        int matchCount = matchNumbers(purchasedLotto);
+    public LottoRank getLottoRankFrom(Lotto purchasedLotto) {
+        int matchCount = countMatchingNumbers(purchasedLotto);
         boolean hasBonusNumber = hasBonusNumber(winningNumbers, bonusNumber);
         return LottoRank.of(matchCount, hasBonusNumber);
     }
 
-    private int matchNumbers(Lotto purchasedLotto) {
-        int matchCount = 0;
-        if (winningNumbers.contains(purchasedLotto)) {
-            matchCount++;
-        }
-        return matchCount;
+    private int countMatchingNumbers(Lotto purchasedLotto) {
+        return (int) purchasedLotto.getLottoNumbers()
+                .stream()
+                .filter(winningNumbers.getLottoNumbers()::contains)
+                .count();
     }
 
     private boolean hasBonusNumber(Lotto winningNumbers, BonusNumber bonusNumber) {
-        return bonusNumber.isMatching(winningNumbers);
+        List<Integer> lottoNumbers = winningNumbers.getLottoNumbers();
+        return lottoNumbers.contains(bonusNumber.getBonusNumber());
     }
 }
 

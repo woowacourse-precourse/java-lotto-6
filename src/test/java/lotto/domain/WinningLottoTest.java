a08bb2caecf31e2bdfeb68de9875fe9a88c39b9c
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 16:04:39 +0900

    refactor: 당첨 로또 테스트 코드 삭제 삭제된 테스트는 input에 대한 테스트이므로 적합하지 않아 코드를 제거했습니다.

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,24 +1,5 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class WinningLottoTest {
 
-    @ParameterizedTest
-    @DisplayName("당첨 로또 번호를 쉼표로 구분지어 받아 리스트로 반환")
-    @ValueSource(strings = {"1,2,3,4,5,6"})
-    void inputLottoCommaToList(String inputLotto) {
-        List<Integer> winningLotto = Arrays.stream(inputLotto.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .toList();
-
-        Assertions.assertThat(winningLotto.size()).isEqualTo(6);
-    }
 }

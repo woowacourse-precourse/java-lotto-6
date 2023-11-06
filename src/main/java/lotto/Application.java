2953fd6cf9d8d08f3fd9dcaf51a27efb8d61ae37
박현수 <pm6567@naver.com>
2023-11-06 18:28:25 +0900

    feat : 출력추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
     }
 }

8b8c37900533533cbb3d4b522546e6209f770af8
Infikei <hckhong@naver.com>
2023-11-07 03:25:23 +0900

    feat: 구입한 로또의 발행 개수 출력 메서드 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,14 +1,20 @@
 package lotto.controller;
 
 import lotto.domain.Amount;
+import lotto.domain.LottosPurchased;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class GameController {
     private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
     private Amount amount;
+    private LottosPurchased lottosPurchased;
 
     public void playGame() {
         amount = repeatInputAmount();
+        lottosPurchased = new LottosPurchased(amount.getAmount());
+        outputView.printNumberOfLottosPurchased(lottosPurchased.getNumberOfLottos());
     }
 
     private Amount repeatInputAmount() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,4 +3,8 @@ package lotto.view;
 public class OutputView {
     private static final String NUMBER_OF_LOTTOS_PURCHASED_PRINT_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_PRINT_MESSAGE = "당첨 통계\n---";
+
+    public void printNumberOfLottosPurchased(int numberOfLottos) {
+        System.out.println(numberOfLottos + NUMBER_OF_LOTTOS_PURCHASED_PRINT_MESSAGE);
+    }
 }

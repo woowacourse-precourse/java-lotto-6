2f22531f8f159302a0d1d1e5d781ab74f73e797b
gusdn0108 <gusdn6671@naver.com>
2023-11-06 16:41:59 +0900

    feat(Result): Result클래스 만들기

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public enum Result {
+
+    THREE_MATCH(5000,3,false),
+    FOUR_MATCH(50000,4,false),
+    FIVE_MATCH(1500000,5,false),
+    FIVE_AND_BONUS_MATCH(30000000,5,true),
+    SIX_MATCH(2000000000,6,false),
+    NOTHING(0,0,false);
+
+    private final int winnings;
+    private final int matchCnt;
+    private final boolean bonus;
+
+    Result(int winnings, int matchCnt, boolean bonus) {
+        this.winnings = winnings;
+        this.matchCnt = matchCnt;
+        this.bonus = bonus;
+    }
+}

5629413a23f9d63ab198c96e1a15c08b84b3a0e7
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 20:21:36 +0900

    test: 로또 번호의 포함여부를 확인하는 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static lotto.enums.ErrorMassage.*;
 import static lotto.fixture.LottoFixture.lottoNumberFixtures;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -24,5 +25,31 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(lottoNumberFixtures(List.of(1, 1, 2, 3, 4, 5))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(DUPLICATE_LOTTO_NUMBER.getMassage());
+    }
+
+    @Test
+    void 로또에_전달받은_로또_번호가_포함되어_있으면_true를_반환한다() {
+        // given
+        Lotto lotto = new Lotto(lottoNumberFixtures(List.of(1, 2, 3, 4, 5, 6)));
+        LottoNumber lottoNumber = LottoNumber.from(1);
+
+        // when
+        boolean result = lotto.contains(lottoNumber);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 로또에_전달받은_로또_번호가_포함되어_있지_않으면_false를_반환한다() {
+        // given
+        Lotto lotto = new Lotto(lottoNumberFixtures(List.of(1, 2, 3, 4, 5, 6)));
+        LottoNumber lottoNumber = LottoNumber.from(7);
+
+        // when
+        boolean result = lotto.contains(lottoNumber);
+
+        // then
+        assertThat(result).isFalse();
     }
 }

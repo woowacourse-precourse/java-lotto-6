e04ba7b4b85b4b072295253a16bb70d62b66cefe
jooda00 <dragon2000a@gmail.com>
2023-11-06 10:13:24 +0900

    test(PlayerTest): 개수만큼 로또 생성 테스트

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -14,12 +14,19 @@ public class PlayerTest {
         player = new Player(3000);
     }
 
+    @DisplayName("개수만큼 로또를 생성한다.")
+    @Test
+    void createLottoByCount() {
+        int count = 3;
+
+        assertThat(count).isEqualTo(player.generateLotteries().size());
+    }
+
     @DisplayName("구매 금액을 로또 개수로 변환햔다.")
     @Test
     void convertAmountToLottoCount() {
-       int count = 3;
+        int count = 3;
 
-       assertThat(count).isEqualTo(player.convertToUnit());
+        assertThat(count).isEqualTo(player.convertToUnit());
     }
-
 }

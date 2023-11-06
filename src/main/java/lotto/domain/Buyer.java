737fda80b5f3369781a08219b211ab4d64527e12
euniiiii <hee9920@naver.com>
2023-11-06 15:33:23 +0900

    feat: 로또 번호 생성 기능 구현

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,2 +1,36 @@
-package lotto.domain;public class Buyer {
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Buyer {
+    private static final int LOTTO_PRICE = 1000;
+
+    private final int purchaseAmount;
+    private final List<Lotto> lottoList = new ArrayList<>();
+
+    public Buyer(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+        purchaseLotto();
+    }
+
+    private void purchaseLotto() {
+        for (int i = 0; i < purchaseAmount / LOTTO_PRICE; i++) {
+            generateLotto();
+        }
+    }
+
+    private void generateLotto() {
+        List<Integer> numbers = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(numbers);
+        lottoList.add(new Lotto(numbers));
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
 }

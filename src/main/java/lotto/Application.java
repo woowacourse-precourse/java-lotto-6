71f6bee8fbf011855d561b9d9fed6093e1bbffa3
HYUNJUN\sajun <sajun28@naver.com>
2023-11-07 02:00:23 +0900

    Refactor: "결과 계산 클래스 분리"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,6 @@ import lotto.model.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.io.ObjectOutput;
 import java.util.*;
 
 public class Application {
@@ -17,7 +16,8 @@ public class Application {
     private static final OutputView outputView = new OutputView();
     private static final RandomLotto random = new RandomLotto();
     private static final Intersection intersection = new Intersection();
-    private static final HashMap<Integer, Integer> result = new HashMap<>();
+    private static HashMap<Integer, Integer> result = new HashMap<>();
+
 
     public static void purchaseCount(String purchasingAmount) {
         try {
@@ -75,36 +75,9 @@ public class Application {
         for (int i = 0; i < 6; i++) {
             result.put(i,0);
         }
+        LottoResult lottoResult = new LottoResult(result);
         for (int i = 0; i<intersectionCount.size(); i++) {
-            lottoResult(intersectionCount.get(i), intersectionCheck.get(i));
-        }
-    }
-
-    public static void lottoResult(int count, int choice) {
-        if (count == 3) {
-            int num = result.get(4);
-            num++;
-            result.put(4,num);
-        }
-        if (count == 4) {
-            int num = result.get(3);
-            num++;
-            result.put(3,num);
-        }
-        if (count == 5 && choice == 1) {
-            int num = result.get(1);
-            num++;
-            result.put(1,num);
-        }
-        if (count == 5 && choice == 0) {
-            int num = result.get(2);
-            num++;
-            result.put(2,num);
-        }
-        if (count == 6) {
-            int num = result.get(0);
-            num++;
-            result.put(0,num);
+           result = LottoResult.duplicateResult(intersectionCount.get(i), intersectionCheck.get(i));
         }
     }
 

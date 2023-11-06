5da9bc1b76206d479ecb86e85e401eeed1119635
cmong0516 <cmong0516@gmail.com>
2023-11-06 16:35:09 +0900

    feat(Lotto): validateRange, validateDuplicated , lottoNumbersToString

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,54 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lotto.constans.ErrorMessage;
+import lotto.constans.GameMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumbersRange(numbers);
+        validateDuplicated(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_LENGTH_EXCEPTION.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_OVER_RANGE_EXCEPTION.getMessage());
+            }
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (Collections.frequency(numbers, number) > 1) {
+                throw new IllegalArgumentException(ErrorMessage.NUMBER_DUPLICATED_EXCEPTION.getMessage());
+            }
+        }
+    }
+
+    public String lottoNumbersToString() {
+        return GameMessage.LOTTO_NUMBER_PREFIX.getMessage() +
+                numbers.stream().map(Objects::toString)
+                        .collect(Collectors.joining(", ")) + GameMessage.LOTTO_NUMBER_SUFFIX.getMessage();
+    }
 }

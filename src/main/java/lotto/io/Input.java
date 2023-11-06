f42630920ee636de9a3c0114cfded195b32bf43c
cloudchamb3r <jizon0123@protonmail.com>
2023-11-06 22:02:04 +0900

    feat. add input helpers

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -1,9 +1,37 @@
 package lotto.io;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lotto.exceptions.ReadIntegerException;
 
 public class Input {
     public static String promptLine(Object message) {
         Output.println(message);
         return Console.readLine();
+    }
+
+    public static int readLineAsInt() {
+        String line = Console.readLine();
+        try {
+            return Integer.parseInt(line);
+        }
+        catch (NumberFormatException e) {
+            throw new ReadIntegerException();
+        }
+    }
+
+    public static List<Integer> readLineAsIntegerList(String sep) {
+        String line = Console.readLine();
+        List<String> splited = Arrays.stream(line.split(sep, -1)).toList();
+        List<Integer> ret = new ArrayList<>();
+        try {
+            for (var sp : splited) {
+                ret.add(Integer.parseInt(sp));
+            }
+        } catch (NumberFormatException e) {
+            throw new ReadIntegerException();
+        }
+        return ret;
     }
 }

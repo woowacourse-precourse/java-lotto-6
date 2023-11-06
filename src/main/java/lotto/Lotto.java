9c6100ae9be676ff58c6557db099eadf149856f0
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-06 16:27:59 +0900

    feat: create & printLotto 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,46 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Lotto {
-    private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	private final List<Integer> numbers;
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    // TODO: 추가 기능 구현
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void Create(int lottocount) {
+
+		List<List<Integer>> lottos = new ArrayList<>();
+
+		for (int i = 0; i < lottocount; i++) {
+
+			List<Integer> lottonumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+			lottos.add(lottonumbers);
+		}
+
+		for (int i = 0; i < lottocount; i++) {
+
+			System.out.println(lottos.get(i));
+
+		}
+	}
+
+	public static void printLotto(int lottocount) {
+
+		System.out.println(lottocount + "개를 구매했습니다.");
+
+	}
 }

b1927d6b5c4280015b570f3d3393f50955c73e07
Hyo Gyeong <cdkrcd8@gmail.com>
2023-11-06 21:14:54 +0900

    implement: lotto object 1. create constructor 1-1. get List<Integer> one lotto as parameter 2. validation 2-1. published lotto length 2-2. lotto number in range between 1 to 45 2-3. if lotto number duplicated

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,50 @@
 package lotto;
 
+import static lotto.Enum.ErrorMessage.DUPLICATED_ERROR;
+import static lotto.Enum.ErrorMessage.LENGHT_ERROR;
+import static lotto.Enum.ErrorMessage.NUMBER_RANGE_ERROR;
+import static lotto.Enum.Number.SIX;
+import static lotto.Enum.Number.MAX_NUMBER;
+import static lotto.Enum.Number.MIN_NUMBER;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLength(numbers);
+        validateDuplicated(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != SIX.getNumber()) {
+            throw new IllegalArgumentException(LENGHT_ERROR.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_NUMBER.getNumber() || number > MAX_NUMBER.getNumber()) {
+                throw new IllegalArgumentException(NUMBER_RANGE_ERROR.getMessage());
+            }
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATED_ERROR.getMessage());
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

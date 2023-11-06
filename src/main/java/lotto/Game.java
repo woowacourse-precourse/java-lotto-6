bd8617e50d302af3d64252bdac1d5e4e70e27152
Dong <dyeongkim98@gmail.com>
2023-11-06 20:36:16 +0900

    feat(Game): Add 입력 개수만큼 번호 제작기능 구현
    makeNumberTicket 메서드 추가
    - 입력받은 Integer만큼 makeNumber() 함수 실행
    번호를 담는 List형태 numberTickets에 담아서 목표 totalTicketNumber만큼 List에 담기면 while문 탈출

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -4,8 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Game {
+    private List<List<Integer>> numberTickets;
+    private List<Integer> numbers;
+    public void makeNumberTicket(Integer totalTicketNumber){
+        while (totalTicketNumber > numberTickets.size()){
+            makeNumber();
+            numberTickets.add(makeNumber());
+        }
+    }
     private List<Integer> makeNumber(){
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
     }
 }

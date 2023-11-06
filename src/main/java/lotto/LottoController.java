ce07baf9b8ad8efa8c1f4072d89305562a925741
SUJI-J <suzy852@naver.com>
2023-11-06 23:06:48 +0900

    feat: 당첨 번호 입력받기 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -4,16 +4,21 @@ import lotto.InputView;
 
 import static lotto.OutputView.printTicketCount;
 import static lotto.OutputView.printLottoList;
+import static lotto.OutputView.printBlankLine;
 
 public class LottoController {
 
     public void start() {
         Money money = inputLottoMoney();
         int ticketCount = money.getTicketCount();
+        printBlankLine();
         printTicketCount(ticketCount);
 
         Lottos lottos = getLottos(money);
         printLottoList(lottos);
+        printBlankLine();
+
+        LottoWin lottoWin = getWinningLotto();
     }
 
     private Money inputLottoMoney() {
@@ -24,6 +29,11 @@ public class LottoController {
     private Lottos getLottos(Money money) {
         GenerateLotto generateLotto = new GenerateLotto();
         return new Lottos(generateLotto.lottoGroup((money.getTicketCount())));
+    }
+
+    private LottoWin getWinningLotto() {
+        InputView.getWinningNumber();
+
     }
 }
 

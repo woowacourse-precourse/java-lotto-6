376dca019aaf059b8635ab0bb4221c9c51cd916a
SungWoo <sungwoo166@gmail.com>
2023-11-06 13:24:51 +0900

    refactor(Rank): Extend Rank enum with boolean
    - to determine bonus number

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -3,19 +3,20 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    NONE(0, 0, false);
 
     private final int matchCount;
     private final int prizeMoney;
-
-    Rank(int matchCount, int prizeMoney) {
+    private final boolean bonusMatched;
+    Rank(int matchCount, int prizeMoney, boolean bonusMatched) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.bonusMatched = bonusMatched;
     }
 
     public int getMatchCount() {

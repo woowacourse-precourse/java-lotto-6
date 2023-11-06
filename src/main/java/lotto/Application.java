27043e134f7f5084a68d73ccb0b142c35b9cd3a4
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-07 02:25:59 +0900

    feat(Application): create 6 lotto numbers(1~45)
    - pick 6 unique numbers from 1 to 45 for random
    - create a Lotto type object
    - allocate numbers to Lotto type object
    - sort numbers in ascending order

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(numbers);
+        Collections.sort(numbers);
+
+       
     }
 }

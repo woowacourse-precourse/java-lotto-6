b39f1f17b29f5b85b740e2fa1e840d3e81c85beb
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-11-06 19:54:04 +0900

    refactor: 로또 번호용 메서드를 사용하도록 수정

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -1,18 +1,22 @@
 package lotto.domain;
 
+import static lotto.constant.DomainConstant.LOTTO_SIZE;
 import static lotto.constant.DomainConstant.MAX_LOTTO_NUMBER;
 import static lotto.constant.DomainConstant.MIN_LOTTO_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public interface NumberGenerator {
-    int generate();
 
-    class RandomNumberGenerator implements NumberGenerator{
+    List<Integer> generateNumbers();
+
+    class RandomNumberGenerator implements NumberGenerator {
 
         @Override
-        public int generate() {
-            return Randoms.pickNumberInRange(MIN_LOTTO_NUMBER.getValue(), MAX_LOTTO_NUMBER.getValue());
+        public List<Integer> generateNumbers() {
+            return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.getValue(), MAX_LOTTO_NUMBER.getValue(),
+                LOTTO_SIZE.getValue());
         }
     }
 }

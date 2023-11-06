3402db75670712dbbcede9bd03a678a7ef152dd7
NAM GIBEOM <rlqja0523@naver.com>
2023-11-07 00:45:59 +0900

    feat: 수익률을 계산하는 기능 구현

--- src/main/java/lotto/dto/WinningResult.java
+++ src/main/java/lotto/dto/WinningResult.java
@@ -1,6 +1,7 @@
 package lotto.dto;
 
 import java.util.Map;
+import lotto.domain.LottoAmount;
 import lotto.enums.Prize;
 
 public record WinningResult(Map<Prize, Integer> prizes) {
@@ -8,5 +9,16 @@ public record WinningResult(Map<Prize, Integer> prizes) {
 
     public int countOf(final Prize prize) {
         return prizes.getOrDefault(prize, ZERO);
+    }
+
+    public double calculateYield(final LottoAmount amount) {
+        return (double) this.totalWinningMoney() / amount.amount() * 100;
+    }
+
+    private int totalWinningMoney() {
+        return prizes.keySet().stream()
+                .filter(prize -> prize != Prize.NONE)
+                .mapToInt(prize -> prize.getWinningMoney() * prizes.get(prize))
+                .sum();
     }
 }

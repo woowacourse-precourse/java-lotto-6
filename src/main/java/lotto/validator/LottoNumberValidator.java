f0f87b92a9fd03ff1c1e754e85da48cb037065aa
FL <refrain2k@gmail.com>
2023-11-06 18:03:54 +0900

    docs: 체크리스트 업데이트

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -3,7 +3,6 @@ package lotto.validator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 import static lotto.validator.ErrorMessage.*;
 

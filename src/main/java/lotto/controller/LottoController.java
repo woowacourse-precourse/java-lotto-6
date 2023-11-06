5d6f0d547b6df3ab16018b17b4e58474f2ceb733
Seyeon <wjdtpdus828@naver.com>
2023-11-06 19:43:40 +0900

    feat(LottoController): 구매 가격을 가져오고 예외처리를 진행하는 메소드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,18 +2,46 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Parser;
+import lotto.service.validator.PurchaseAmount;
 import lotto.view.LottoView;
 
 public class LottoController {
 
     private LottoView lottoView = new LottoView();
-    private int purchaseAmount;
+    private PurchaseAmount purchaseAmount = new PurchaseAmount();
+    private Parser parser = new Parser();
+    private int purchaseAmountNum;
     public void startLotto(){
-        purchaseAmount = getPurchaseAmount();
+        purchaseAmountNum = processPurchaseAmount();
     }
 
-    public int getPurchaseAmount(){
+    public int processPurchaseAmount(){
+        String purchaseNum;
+        while(true){
+            try{
+                purchaseNum = getPurchaseAmount();
+                validatePurchaseAmount(purchaseNum);
+                break;
+            }catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return parser.stringToInt(purchaseNum);
+    }
+
+    public String getPurchaseAmount(){
+        String purchaseNum;
+
         lottoView.purchaseAmountView();
-        return Parser.stringToInt(Console.readLine());
+        purchaseNum = Console.readLine();
+
+        return purchaseNum;
+    }
+
+    public void validatePurchaseAmount(String input){
+        purchaseAmount.validateNotInteger(input);
+        purchaseAmount.validateUnderThousand(input);
+        purchaseAmount.validateNotRemainZero(input);
     }
 }

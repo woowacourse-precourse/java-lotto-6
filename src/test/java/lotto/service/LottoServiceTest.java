5f69e4c717c2cfecd3f2ca62c519d1701b675c32
김효준 <khyojun1104@gmail.com>
2023-11-07 00:05:55 +0900

    test(lotto): 테스트 케이스 추가 및 현재 로또 티켓이 당첨 번호를 가지고 있나? 테스트  로직 일부 수정

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -3,6 +3,7 @@ package lotto.service;
 import static lotto.constant.NumberConstant.SAME_COUNT_FIVE;
 
 import java.util.List;
+import lotto.domain.LottoTicket;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,16 +16,19 @@ class LottoServiceTest {
 
     @Test
     @DisplayName("현재 로또 티켓이 당첨 번호를 가지고 있나?")
-    void nowLottoTicketContainWinNumberTest(){
+    void nowLottoTicketHasWinNumbersTest(){
         //given
         LottoTicket lottoTicket = new LottoTicket(List.of(1,2,3,4,5,6),0,false);
-        int lottoWinNumber = 3;
+        LottoTicket lottoTicketSecond = new LottoTicket(List.of(3,4,5,6,7,8),0,false);
+        List<Integer> winNumbers = List.of(1,2,3,4,5,6);
 
         //when
-        nowLottoTicketContainWinNumber(lottoTicket, lottoWinNumber);
+        nowLottoTicketHasWinNumber(lottoTicket, winNumbers);
+        nowLottoTicketHasWinNumber(lottoTicketSecond,winNumbers);
 
         //then
-        Assertions.assertThat(lottoTicket.getSameCount()).isEqualTo(1);
+        Assertions.assertThat(lottoTicket.getSameCount()).isEqualTo(6);
+        Assertions.assertThat(lottoTicketSecond.getSameCount()).isEqualTo(4);
     }
 
 
@@ -33,13 +37,16 @@ class LottoServiceTest {
     void nowLottoTicketHasBonusNumber(){
         //given
         LottoTicket lottoTicket = new LottoTicket(List.of(1,2,3,4,5,6),5,false);
+        LottoTicket lottoTicketSecond = new LottoTicket(List.of(1,2,4,5,6,7),5,false);
         int bonusNumber = 3;
 
         //when
         nowLottoTicketHasBonusNumber(lottoTicket, bonusNumber);
+        nowLottoTicketHasBonusNumber(lottoTicketSecond, bonusNumber);
 
         //then
         Assertions.assertThat(lottoTicket.isBonus()).isEqualTo(true);
+        Assertions.assertThat(lottoTicketSecond.isBonus()).isEqualTo(false);
     }
 
 
@@ -77,6 +84,13 @@ class LottoServiceTest {
             this.bonus = true;
         }
 
+    }
+
+    private void nowLottoTicketHasWinNumber(LottoTicket lottoTicket,
+        List<Integer> lottoWinNumbers) {
+        for (Integer lottoWinNumber : lottoWinNumbers) {
+            nowLottoTicketContainWinNumber(lottoTicket, lottoWinNumber);
+        }
     }
 
     private void nowLottoTicketContainWinNumber(LottoTicket lottoTicket,

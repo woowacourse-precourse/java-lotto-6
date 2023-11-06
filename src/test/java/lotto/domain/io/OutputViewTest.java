d4dab5eeddbc7b7718e5d16618aed58badbebdae
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-07 05:43:20 +0900

    test : OutputViewTest 테스트 코드 작성

---
+++ src/test/java/lotto/domain/io/OutputViewTest.java
@@ -0,0 +1,48 @@
+package lotto.domain.io;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    @DisplayName("생성한 로또들을 포맷에 맞추어 출력합니다.")
+    public void 로또들_출력() throws Exception {
+        // GIVEN
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+        lottos.add(new Lotto(List.of(3, 5, 11, 16, 32, 38)));
+        Lottos lottosObj = new Lottos(lottos);
+        String lottosStatus = lottosObj.toString();
+
+        // WHEN
+        OutputView.printLottos(lottosStatus);
+
+        // THEN
+        assertEquals("[8, 21, 23, 41, 42, 43]\n"
+                + "[3, 5, 11, 16, 32, 38]", outputStreamCaptor.toString()
+                .trim());
+    }
+}

403a5b11ac5a700671cbdd693e78062d4ea080e3
voyager003 <miraclex000@gmail.com>
2023-11-06 14:36:09 +0900

    feat(LottoRanking): add new enum and method, test code
    ADD LottoRanking ENUM, determineRanking() method
    - enum: represent lotto match count
    - method: determine match rank and return lotto ranking
    - test: none match and match five bonus case

---
+++ src/main/java/lotto/model/LottoRanking.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import java.util.Arrays;
+
+public enum LottoRanking {
+    NONE_MATCH(0, 0, false),
+    MATCH_THREE(3, 5_000, false),
+    MATCH_FOUR(4, 50_000, false),
+    MATCH_FIVE(5, 1_500_000, false),
+    MATCH_FIVE_BONUS(5, 30_000_000, true),
+    MATCH_SIX(6, 2_000_000_000, false);
+
+    private final int matchCount;
+    private final int prize;
+    private final boolean hasBonusNumber;
+
+    LottoRanking(int matchCount, int prize, boolean hasBonusNumber) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.hasBonusNumber = hasBonusNumber;
+    }
+
+    public static LottoRanking determineRanking(int matchCount, boolean hasBonusNumber) {
+        return Arrays.stream(LottoRanking.values())
+                .filter(ranking -> ranking.matchCount == matchCount && ranking.hasBonusNumber == hasBonusNumber)
+                .findAny()
+                .orElse(NONE_MATCH);
+    }
+}
---
+++ src/test/java/lotto/model/LottoRankingTest.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoRankingTest {
+
+    @Test
+    @DisplayName("조건에 맞는 로또 랭킹을 반환하는지 확인한다.")
+    void determineRanking_isNoneMatch() {
+        /**
+         * given : 일치하는 번호(0)가 없고, 보너스 번호도 없는(false) 경우가 주어진다.
+         * when : 당첨 등수를 판단한다.
+         * then : 매칭된 번호와 보너스가 없으므로, 결과는 NONE_MATCH이다.
+         */
+        int matchCount = 0;
+        boolean hasBonusNumber = false;
+
+        LottoRanking lottoRanking = LottoRanking.determineRanking(matchCount, hasBonusNumber);
+        assertThat(lottoRanking).isEqualTo(LottoRanking.NONE_MATCH);
+    }
+
+    @Test
+    @DisplayName("조건에 맞는 로또 랭킹을 반환하는지 확인한다.")
+    void determineRanking_isMatchFiveBonus() {
+        /**
+         * given : 일치하는 번호(5)가 있고, 보너스 번호도 있는(true) 경우가 주어진다.
+         * when : 당첨 등수를 판단한다.
+         * then : 매칭된 번호는 5개이고, 보너스 번호가 있으므로, 결과는 MATCH_FIVE_BONUS이다.
+         */
+        int matchCount = 5;
+        boolean hasBonusNumber = true;
+
+        LottoRanking lottoRanking = LottoRanking.determineRanking(matchCount, hasBonusNumber);
+        assertThat(lottoRanking).isEqualTo(LottoRanking.MATCH_FIVE_BONUS);
+    }
+}

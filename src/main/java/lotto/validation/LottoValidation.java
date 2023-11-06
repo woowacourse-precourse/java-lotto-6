0dc8d3b9352014d77d5ff72181184c5a4f00ed57
jooda00 <dragon2000a@gmail.com>
2023-11-06 14:26:16 +0900

    refactor(LottoValidation): 숫자 상수 처리 및 메소드명 변경

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -1,21 +1,37 @@
 package lotto.validation;
 
 public class LottoValidation {
+    private static final int STANDARD_UNIT = 1000;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+
     public static void validateIsNumber(String input) {
         if (!input.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
         }
     }
 
-    public static void validateOverThousandUnit(int amount) {
-        if (amount < 1000) {
+    public static void validateOverStandardUnit(int amount) {
+        if (amount < STANDARD_UNIT) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 최소 1000원입니다.");
         }
     }
 
-    public static void validateIsThousandUnit(int amount) {
-        if (amount % 1000 != 0) {
+    public static void validateIsStandardUnit(int amount) {
+        if (amount % STANDARD_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+        }
+    }
+
+    public static void validateIsNumberBonus(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫쟈여야 합니다.");
+        }
+    }
+
+    public static void validateInRange(int input) {
+        if (input < START_NUMBER || input > END_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45사이의 숫자여야 합니다.");
         }
     }
 }

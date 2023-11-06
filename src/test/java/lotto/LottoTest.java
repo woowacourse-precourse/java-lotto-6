bd9a9f02ff0448437e4ff59bdc3042357e0d2583
minkyole <7719s@nave.com>
2023-11-06 16:33:26 +0900

    test(LottoTest.java) : 로또 번호 범위 밖의 숫자가 들어오는 테스트 구현
    - 로또 번호 범위(1~45) 밖의 숫자가 들어오면 예외발생하는지 테스트하는 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import java.util.ArrayList;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -20,6 +23,21 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 범위(1 ~ 45) 밖의 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0,2,3,4,5,6", "1,2,3,4,5,46"})
+    void createLottoByOutOfRange(String numbers) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        String[] parserNumber = numbers.split(",");
+
+        for (String currentNumber : parserNumber) {
+            lottoNumbers.add(Integer.parseInt(currentNumber));
+        }
+
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

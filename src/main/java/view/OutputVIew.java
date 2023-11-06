7e1998025117a0ddc1d954b7e8a722c9139ec655
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 19:56:59 +0900

    feat(OutputView.java): add printPurchasingResultMessage function 구입 결과 안내 메시지를 출력하는 기능을 구현하였습니다.

--- src/main/java/view/OutputVIew.java
+++ src/main/java/view/OutputVIew.java
@@ -3,8 +3,13 @@ package view;
 public class OutputVIew {
 
     static final String MONEY_INPUT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
+    static final String PURCHASING_RESULT_MESSAGE = "개를 구매했습니다.";
 
     void  printMoneyInputRequestMessage() {
         System.out.println(MONEY_INPUT_REQUEST_MESSAGE);
+    }
+
+    void printPurchasingResultMessage() {
+        System.out.println(PURCHASING_RESULT_MESSAGE);
     }
 }

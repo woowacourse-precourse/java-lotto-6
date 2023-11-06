f09e223b291e175a0519ce49e89172790145cb24
JAY-Winter <rubat0@naver.com>
2023-11-06 14:07:16 +0900

    test: input & create Amount

--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -1,4 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 class AmountTest {
-  
+
+    @Test
+    void 최소_구입_금액은_1000원이다() {
+        String amount = "500";
+        assertThrows(IllegalArgumentException.class,
+            () -> new Amount(amount)
+        );
+    }
+
+    @Test
+    void 구입_금액은_1000원_단위로_나누어_떨어져야_한다() {
+        String validAmount = "10000";
+        String invalidAmount = "1350";
+
+        assertDoesNotThrow(() ->
+            new Amount(validAmount)
+        );
+
+        assertThrows(IllegalArgumentException.class,
+            () -> new Amount(invalidAmount));
+    }
+
+    @Test
+    void 구입_금액은_숫자여야한다() {
+        String amount = "구입금액";
+        assertThrows(IllegalArgumentException.class,
+            () -> new Amount(amount)
+        );
+    }
 }

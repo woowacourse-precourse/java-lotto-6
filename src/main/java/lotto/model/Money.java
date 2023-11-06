37781e86525fa4d393087f6bda7336f416ca6a45
hoyeon <highoption@naver.com>
2023-11-06 21:12:17 +0900

    feat(Money) : isPaperMoney 메서드 생성
    - 입력 받은 돈은 1000원 단위로 이루어져야함
    - 받은 돈이 지폐인지 확인하는 isPaperMoney 생성

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -10,6 +10,7 @@ public class Money {
 
     private static void validateMoney(String money) {
         isInteger(money);
+        isPaperMoney(money);
     }
 
     private static void isInteger(String money) {
@@ -18,5 +19,10 @@ public class Money {
         }
     }
 
-
+    private static void isPaperMoney(String money) {
+        int realMoney = Integer.parseInt(money);
+        if (realMoney % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 이루어져야 합니다.");
+        }
+    }
 }

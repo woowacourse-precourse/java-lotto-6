44f120200a5422a485c5d3a8a0e81059a92ad4ae
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-06 22:58:59 +0900

    fix(LottoMessage): 중복되어 문자열을 포맷하는 문제 수정

--- src/main/java/lotto/config/LottoMessage.java
+++ src/main/java/lotto/config/LottoMessage.java
@@ -2,9 +2,9 @@ package lotto.config;
 
 public enum LottoMessage {
     ENTER_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    TICKET_PURCHASED_COUNT("%d개를 구매했습니다."),
-    ENTER_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    TICKET_PURCHASED_COUNT("\n%d개를 구매했습니다."),
+    ENTER_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    ENTER_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS_HEADER("당첨 통계\n---\n"),
     MATCH_COUNT("%d개 일치"),
     MATCH_COUNT_WITH_BONUS("%d개 일치, 보너스 볼 일치"),
@@ -13,8 +13,8 @@ public enum LottoMessage {
 
     private final String message;
 
-    LottoMessage(String message, Object... args) {
-        this.message = String.format(message, args);
+    LottoMessage(String message) {
+        this.message = message;
     }
 
     public String getMessage() {

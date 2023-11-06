e17a6f141df9edfa48e161751774ff880355b87b
kimdoha <kkhhjj3393@gmail.com>
2023-11-06 18:59:57 +0900

    feat(Lotto): 로또 번호 개수가 6개가 아닐 경우 오류 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,10 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+    private final Integer LOTTO_NUM_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,7 +13,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUM_SIZE) {
             throw new IllegalArgumentException();
         }
     }

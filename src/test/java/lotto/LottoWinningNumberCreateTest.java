c04b08413a32c8214541312eae54a7934da0f86f
dukcode <duk9741@gmail.com>
2023-11-06 15:39:28 +0900

    test: 당첨 번호 생성 클래스 테스트 구현
    - 당첨 번호 갯수 검증 테스트 추가
    - 당첨 번호 범위 검증 테스트 추가

---
+++ src/test/java/lotto/LottoWinningNumberCreateTest.java
@@ -0,0 +1,62 @@
+package lotto;
+
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LottoWinningNumberCreateTest {
+
+    @Test
+    public void 로또_번호로_LottoWinningNumberCreate_를_생성할_수_있다() throws Exception {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 45);
+
+        // when
+        // then
+        assertThatCode(() -> {
+            new LottoWinningNumberCreate(numbers);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 로또_번호_갯수가_다르면_LottoWinningNumberCreate_를_생성_시_예외가_발생한다() throws Exception {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new LottoWinningNumberCreate(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 범위_초과의_로또_번호로_LottoWinningNumberCreate_를_생성_시_예외가_발생한다() throws Exception {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, LottoConstant.LOTTO_END_NUMBER + 1);
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new LottoWinningNumberCreate(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 범위_미만의_로또_번호로_LottoWinningNumberCreate_를_생성_시_예외가_발생한다() throws Exception {
+        // given
+        List<Integer> numbers = List.of(LottoConstant.LOTTO_START_NUMBER - 1, 2, 3, 4, 5, 45);
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new LottoWinningNumberCreate(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}

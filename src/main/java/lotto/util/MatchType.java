2c26c6561b13314da7bee97ca2d3b5370a564994
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-06 14:49:13 +0900

    feat: MatchType enum 구현

--- src/main/java/lotto/util/MatchType.java
+++ src/main/java/lotto/util/MatchType.java
@@ -1,5 +1,41 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+
 public enum MatchType {
-    THREE, FOUR, FIVE, FIVE_BONUS, SIX
+    THREE(3, false, "5,000"),
+    FOUR(4, false, "50,000"),
+    FIVE(5, false, "1,500,000"),
+    FIVE_BONUS(5, true, "30,000,000"),
+    SIX(6, false, "2,000,000,000");
+
+    private final int sameNumbersCount;
+    private final boolean bonusNumberIncluded;
+    private final String prizeMoney;
+
+    MatchType(int sameNumbersCount, boolean bonusNumberIncluded, String prizeMoney) {
+        this.sameNumbersCount = sameNumbersCount;
+        this.bonusNumberIncluded = bonusNumberIncluded;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static Optional<MatchType> valueOf(int sameNumbersCount, boolean bonusNumberIncluded) {
+        return Arrays.stream(MatchType.values())
+                .filter(matchType -> matchType.sameNumbersCount == sameNumbersCount
+                        && matchType.bonusNumberIncluded == bonusNumberIncluded)
+                .findFirst();
+    }
+
+    public static void printWinningStatistics(Map<MatchType, Integer> winningStatistics) {
+        StringBuilder sb = new StringBuilder(UiVO.getWinningStatistics());
+        for (MatchType matchType : MatchType.values()) {
+            sb.append(String.format(UiVO.getMatchesFormat(),
+                    matchType.sameNumbersCount,
+                    matchType.prizeMoney,
+                    winningStatistics.getOrDefault(matchType, 0)));
+        }
+        System.out.println(sb);
+    }
 }

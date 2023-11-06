08ce43aa48e74c8786ca9828081a64156151f5dc
정용우 <jyw1341@gmail.com>
2023-11-06 12:58:36 +0900

    feat: 당첨 번호 입력 기능 추가

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -1,26 +1,60 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
 
     private static final String ERROR_BLANK = "[ERROR] 입력된 값이 없습니다.";
     private static final String ERROR_NOT_NUMBER = "[ERROR] 숫자를 입력해야합니다.";
     private static final String ERROR_NOT_MULTIPLE_1000 = "[ERROR] 1000단위로 나누어 떨어지는 숫자가 아닙니다.";
+    private static final String ERROR_INCORRECT_LENGTH = "[ERROR] 로또 번호는 6개의 숫자를 입력해야 합니다.";
+    private static final String ERROR_INCORRECT_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_DUPLICATED_NUMBER = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
 
-    public static void validateBlank(String input) {
-        if(input.isBlank()){
+    public static void validateIsNotBlank(String input) {
+        if (input.isBlank()) {
             throw new IllegalArgumentException(ERROR_BLANK);
         }
     }
 
-    public static void validateNumbers(String input) {
-        if(!input.matches("^[1-9]\\d*$")){
+    public static void validateIsNumbers(String input) {
+        if (!input.matches("^[1-9]\\d*$")) {
             throw new IllegalArgumentException(ERROR_NOT_NUMBER);
         }
     }
 
+    public static void validateIsNumbers(String[] input) {
+        for (String s : input) {
+            if (!s.matches("^[1-9]\\d*$")) {
+                throw new IllegalArgumentException(ERROR_NOT_NUMBER);
+            }
+        }
+    }
+
     public static void validateIsMultipleOf1000(String input) {
-        if(Integer.parseInt(input) % 1000 != 0) {
+        if (Integer.parseInt(input) % 1000 != 0) {
             throw new IllegalArgumentException(ERROR_NOT_MULTIPLE_1000);
+        }
+    }
+
+    public static void validateStringArrayLength(String[] array, int target) {
+        if (array.length != target) {
+            throw new IllegalArgumentException(ERROR_INCORRECT_LENGTH);
+        }
+    }
+
+    public static void validateNumberInRange(int number, int minimum, int maximum) {
+        if (number > maximum || number < minimum) {
+            throw new IllegalArgumentException(ERROR_INCORRECT_RANGE);
+        }
+    }
+
+    public static void validateIsUnique(List<String> array) {
+        Set<String> set = new HashSet<>(array);
+        if (set.size() < array.size()) {
+            throw new IllegalArgumentException(ERROR_DUPLICATED_NUMBER);
         }
     }
 }
--- src/main/java/lotto/LottoGameView.java
+++ src/main/java/lotto/LottoGameView.java
@@ -2,14 +2,18 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoGameView {
 
-    private static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
 
     public int inputPurchaseAmount() {
-        System.out.println(PURCHASE_AMOUNT_GUIDE);
+        System.out.println(INPUT_PURCHASE_AMOUNT);
         String input = Console.readLine();
         validatePurchaseAmount(input);
         return Integer.parseInt(input) / 1000;
@@ -17,8 +21,8 @@ public class LottoGameView {
 
     private void validatePurchaseAmount(String input) {
         String trimmed = input.trim();
-        InputValidator.validateBlank(trimmed);
-        InputValidator.validateNumbers(trimmed);
+        InputValidator.validateIsNotBlank(trimmed);
+        InputValidator.validateIsNumbers(trimmed);
         InputValidator.validateIsMultipleOf1000(trimmed);
     }
 
@@ -32,5 +36,30 @@ public class LottoGameView {
             System.out.println(lotto.toString());
         }
         System.out.println();
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        ArrayList<Integer> winningNumbers = new ArrayList<>();
+        System.out.println(INPUT_WINNING_NUMBERS);
+        String input = Console.readLine();
+        validateWinningNumbers(input);
+        String[] split = input.split(",");
+
+        for (String s : split) {
+            winningNumbers.add(Integer.parseInt(s));
+        }
+        return winningNumbers;
+    }
+
+    private void validateWinningNumbers(String input) {
+        InputValidator.validateIsNotBlank(input);
+        String[] split = input.split(",");
+        InputValidator.validateStringArrayLength(split, 6);
+        InputValidator.validateIsNumbers(split);
+        for (String s : split) {
+            InputValidator.validateNumberInRange(Integer.parseInt(s), 1, 45);
+        }
+
+        InputValidator.validateIsUnique(Arrays.asList(split));
     }
 }

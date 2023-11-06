6a705d725812842c6260cc955d82f1f76d852df1
Ojh <201833022@yiu.ac.kr>
2023-11-06 18:12:56 +0900

    feat: InputView 보너스 번호 입력 받기

--- src/main/java/lotto/View/InputViewImpl.java
+++ src/main/java/lotto/View/InputViewImpl.java
@@ -6,6 +6,7 @@ public class InputViewImpl implements InputView {
 
   private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
   private static final String LOTTERY_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+  private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
   @Override
   public String readPurchase() {
@@ -16,6 +17,12 @@ public class InputViewImpl implements InputView {
   @Override
   public String readLotteryNumber() {
     System.out.println(LOTTERY_NUMBER_PROMPT);
+    return Console.readLine();
+  }
+
+  @Override
+  public String readBonusNumber() {
+    System.out.println(BONUS_NUMBER_PROMPT);
     return Console.readLine();
   }
 }

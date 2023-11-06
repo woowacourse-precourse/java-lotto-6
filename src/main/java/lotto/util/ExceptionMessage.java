b14cb98f1048a6c851e457e9935f9923c78c20ba
moong2 <www.castlehi@gmail.com>
2023-11-07 01:48:02 +0900

    refactor: make input lotto number exception message to enum

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -3,6 +3,9 @@ package lotto.util;
 import static lotto.util.AmountOfLotto.LOTTO_PRICE;
 import static lotto.util.AmountOfLotto.MAXIMUM_PRICE;
 import static lotto.util.AmountOfLotto.MINIMUM_PRICE;
+import static lotto.util.LottoDetails.COUNT_OF_LOTTO_NUMBER;
+import static lotto.util.LottoDetails.MAXIMUM_OF_LOTTO_NUMBER;
+import static lotto.util.LottoDetails.MINIMUM_OF_LOTTO_NUMBER;
 
 public enum ExceptionMessage {
     INVALID_INPUT_TYPE("숫자 형식으로 입력해주세요."),
@@ -10,7 +13,13 @@ public enum ExceptionMessage {
     BIGGER_THAN_MAX(MAXIMUM_PRICE.getPrice() + "원 이하의 로또 구입 금액을 입력해주세요."),
     NOT_DIVIDE_BY_LOTTO_PRICE(LOTTO_PRICE.getPrice() + "원 단위의 금액을 입력해주세요."),
 
-    INVALID_LONG_TYPE("올바른 형식의 숫자를 입력해주세요.");
+    INVALID_LONG_TYPE("올바른 형식의 숫자를 입력해주세요."),
+
+    BLANK_LOTTO_NUMBER("공백이 아닌 문자를 입력해주세요."),
+    INVALID_NUMBER_OF_LOTTO_NUMBER(COUNT_OF_LOTTO_NUMBER.getDetails() + "개의 번호를 입력해주세요."),
+    SMALLER_THAN_MIN_LOTTO_NUMBER(MINIMUM_OF_LOTTO_NUMBER.getDetails() + " 이상의 번호를 입력해주세요."),
+    BIGGER_THAN_MAX_LOTTO_NUMBER(MAXIMUM_OF_LOTTO_NUMBER.getDetails() + " 이하의 번호를 입력해주세요"),
+    DUPLICATE_LOTTO_NUMBER("중복되지 않은 숫자를 입력해주세요.");
 
     private final String ERROR_PREFIX = "[ERROR] ";
     private final String message;

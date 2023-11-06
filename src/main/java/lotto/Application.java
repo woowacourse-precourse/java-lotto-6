e2430366ef6540815a9ec1ab1e0e2b45ff52cea6
JAY-Winter <rubat0@naver.com>
2023-11-06 15:15:53 +0900

    feat: 구매 - 로또 발행 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        Amount amount = new Amount(purchaseAmount);
+        System.out.println(amount.getAmount() + "개를 구매했습니다.");
+
+        LottoQuantity lottoQuantity = new LottoQuantity(amount.getAmount());
+        for (int i = 0; i < lottoQuantity.getQuantity(); i++) {
+            System.out.println(new Lotto(new RandomNumber().getRandomNumber()).getNumbers());
+        }
     }
 }

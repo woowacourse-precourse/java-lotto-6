7ed20c3b55b99dbf4949d05f281e3ea9bdcaeb18
Sanghee0820 <lsh277604@gmail.com>
2023-11-06 19:20:06 +0900

    Feat: Launch Lotto App

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.App.LottoApp;
+import lotto.App.LottoConsole;
+import lotto.App.LottoView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoApp lottoApp = new LottoApp(new LottoConsole(), new LottoView());
+        lottoApp.run();
     }
 }

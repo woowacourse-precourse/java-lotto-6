851e7ea6653eb98f0576f8b211c831a456651ed7
jooda00 <dragon2000a@gmail.com>
2023-11-06 09:59:22 +0900

    feat(Application): main에서 controller 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,15 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        lottoController.play();
+        boolean continuePlaying = true;
+        while (continuePlaying) {
+            try {
+                lottoController.play();
+                continuePlaying = false;
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
     }
 }

e10a321aa79ed11552c5b1db2a167691d9487b3f
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 17:32:45 +0900

    refactor: 로또 관련 에러메시지 수정 로또 번호를 생성하는 과정에서 예외 발생 시 다시 생성하였다는 점을 안내하기 위해 수정하였음

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -8,9 +8,9 @@ public enum ErrorMessage {
     INPUT_MONEY_NOT_INTEGER_ERROR("[ERROR] 정수만 입력 가능합니다."),
 
     // 로또 관련 에러
-    LOTTO_NUMBER_COUNT_INVALID_ERROR("[ERROR] 로또 번호는 6개의 숫자여야 합니다."),
-    LOTTO_NUMBER_RANGE_INVALID_ERROR("[ERROR] 로또 번호는 1부터 45 사이여야 합니다."),
-    LOTTO_NUMBER_DUPLICATION_ERROR("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    LOTTO_NUMBER_COUNT_INVALID_ERROR("[ERROR] 로또 번호가 6개가 아닌 경우가 발생하여, 로또 번호를 다시 생성합니다."),
+    LOTTO_NUMBER_RANGE_INVALID_ERROR("[ERROR] 로또 번호가 1부터 45 사이의 숫자가 아닌 경우가 발생하여, 로또 번호를 다시 생성합니다."),
+    LOTTO_NUMBER_DUPLICATION_ERROR("[ERROR] 로또 번호에 중복된 숫자가 포함된 경우가 발생하여, 로또 번호를 다시 생성합니다."),
 
     // 당첨번호 관련 에러
     WINNING_NUMBER_COUNT_INVALID_ERROR("[ERROR] 당첨 번호는 6개여야 합니다."),

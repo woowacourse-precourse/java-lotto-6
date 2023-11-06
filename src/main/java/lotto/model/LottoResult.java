60f9c5260edb368c8d10265660ed9f32cd972a0a
HYUNJUN\sajun <sajun28@naver.com>
2023-11-07 02:00:04 +0900

    Refactor: "결과 계산 클래스 분리"

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LottoResult {
+
+    private static HashMap<Integer, Integer> result = new HashMap<>();
+    public LottoResult(HashMap result) {
+        this.result = result;
+    }
+
+    public static HashMap duplicateResult(int count, int choice) {
+        if (count == 3) {
+            int num = result.get(4);
+            num++;
+            result.put(4,num);
+        }
+        if (count == 4) {
+            int num = result.get(3);
+            num++;
+            result.put(3,num);
+        }
+        if (count == 5 && choice == 1) {
+            int num = result.get(1);
+            num++;
+            result.put(1,num);
+        }
+        if (count == 5 && choice == 0) {
+            int num = result.get(2);
+            num++;
+            result.put(2,num);
+        }
+        if (count == 6) {
+            int num = result.get(0);
+            num++;
+            result.put(0,num);
+        }
+        return result;
+    }
+}

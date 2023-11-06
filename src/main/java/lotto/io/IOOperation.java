d5f3ada7deeaa0c70776a977b1b6fa7942e40db3
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-11-06 21:03:51 +0900

    feat(io): 당첨 번호 예외 처리 UI 기능 구현

--- src/main/java/lotto/io/IOOperation.java
+++ src/main/java/lotto/io/IOOperation.java
@@ -3,6 +3,10 @@ package lotto.io;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Checker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class IOOperation {
     public static Integer getPrice() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -23,5 +27,28 @@ public class IOOperation {
         return price;
     }
 
+    public static List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = new ArrayList<>();
 
+        while (true) {
+            try {
+                String input = Console.readLine();
+                Arrays.asList(input.split(","))
+                        .forEach((value) -> {
+                            winningNumbers.add(Integer.parseInt(value));
+                        });
+
+                Checker.winningSix(winningNumbers);
+                Checker.winningInRange(winningNumbers);
+                Checker.winningNotDuplicated(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+
+                continue;
+            }
+            break;
+        }
+        return winningNumbers;
+    }
 }

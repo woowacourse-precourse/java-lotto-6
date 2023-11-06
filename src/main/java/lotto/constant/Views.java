5b91be40a81d48a58d4114805e4361a0a8b6cefa
Seyeon <wjdtpdus828@naver.com>
2023-11-06 22:02:03 +0900

    add(Views): 로또 구매 출력 메세지 String 상수 추가

--- src/main/java/lotto/constant/Views.java
+++ src/main/java/lotto/constant/Views.java
@@ -1,7 +1,9 @@
 package lotto.constant;
 
 public enum Views {
-    PURCHASE_AMOUT_MESSAGE("구입금액을 입력해 주세요.");
+    INPUT_PURCHASE_AMOUT_MESSAGE("구입금액을 입력해 주세요 ."),
+    OUTPUT_PURCHASE_AMOUT_MESSAGE("개를 구매했습니다 .");
+
 
     private final String message;
 

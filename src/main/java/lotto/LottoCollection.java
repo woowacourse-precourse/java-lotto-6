19ef91fb558eacd41d5c6cbea58039c2a8fdbb4c
pjaehyun <show6756@gmail.com>
2023-11-06 18:40:04 +0900

    feat: 추가 로또 등수 저장 함수

--- src/main/java/lotto/LottoCollection.java
+++ src/main/java/lotto/LottoCollection.java
@@ -3,11 +3,15 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoCollection {
     public List<Lotto> lottoCollection;
     private final int numberOfLotto;
+
+    private final Map<Integer, Integer> rankingCount = new HashMap<>();
 
     public LottoCollection(int numberOfLotto) {
         this.numberOfLotto = numberOfLotto;
@@ -35,6 +39,24 @@ public class LottoCollection {
         for (Lotto lotto : lottoCollection) {
             matchingCount = lotto.countMatchingLottoNumbers(result);
             matchingBonusCount = lotto.hasBonusNumber(result);
+            setRank(matchingCount, matchingBonusCount);
         }
     }
+
+    private void setRank(int matchingCount, boolean matchingBonusCount) {
+        int rank = 0;
+        if (matchingCount == 3) {
+            rank = 5;
+        } else if (matchingCount == 4) {
+            rank = 4;
+        } else if (matchingCount == 5 && !matchingBonusCount) {
+            rank = 3;
+        } else if (matchingCount == 5) {
+            rank = 2;
+        } else if (matchingCount == 6) {
+            rank = 1;
+        }
+        rankingCount.put(rank, rankingCount.getOrDefault(rank, 0) + 1);
+    }
+
 }

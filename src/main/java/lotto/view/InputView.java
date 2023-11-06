a847cf8178fe1942812a0e41882844c8cb8a6aaf
suihub <qkdcrt@gmail.com>
2023-11-06 19:19:15 +0900

    <feat(InputView)> : "입력 메서드 3개 추가"
    - 예정되어 있던 입력 기능 3개 추가
    
    - 구입금액, 당첨 번호, 보너스 번호.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.util.MessageConstant.*;
+
 public class InputView {
+    public String inputMoney() {
+        System.out.println(MONEY_INPUT);
+        return Console.readLine();
+    }
+
+    public String inputWinningLotto() {
+        System.out.println(LOTTO_INPUT);
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        System.out.println(BONUS_INPUT);
+        return Console.readLine();
+    }
 }

feefe4faf6118b080a82f78934ad0e2b25982219
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 00:47:58 +0900

    test(LottoPurchaseAmountExceptionTest) : 로또 구입 금액 예외처리 테스트 코드 작성 - 구입 금액이 1,000으로 나누어 떨어지지 않을 때 - 입력으로 숫자가 들어오지 않을 때 - 입력으로 공백이 들어올 때

---
+++ src/test/java/lotto/exception/LottoPurchaseAmountExceptionTest.java
@@ -0,0 +1,47 @@
+package lotto.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoPurchaseAmountExceptionTest {
+
+    @Test
+    @DisplayName("구입 금액이 1,000으로 나누어 떨어지지 않으면 예외 처리")
+    void checkLottoPurchaseAmountDivideThousand(){
+        // given
+        int purchaseAmount = 2500;
+
+        //then
+        assertThatThrownBy(() -> new LottoPurchaseAmountException().isDivideThousand()).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력으로 숫자가 들어오지 않을 시 예외처리")
+    void checkIsNumber(){
+        //given
+        String input = "abc";
+
+        //then
+        assertThatThrownBy(() -> new LottoPurchaseAmountException().isNumber()).isInstanceOf(
+                IllegalArgumentException.class);
+
+    }
+
+    @Test
+    @DisplayName("입력으로 빈칸이 들어올 시 예외처리")
+    void checkIsBlack(){
+        //given
+        String input = " ";
+
+        //then
+        assertThatThrownBy(() -> new LottoPurchaseAmountException().isBlank()).isInstanceOf(
+                IllegalArgumentException.class);
+
+    }
+
+
+}

593d8b8c432824dec2bf1f50661f739c8faa069d
YongJOoOo <we072200@naver.com>
2023-11-06 14:27:03 +0900

    feat: 구매금액에 대한 잘못된 입력 검증하는 validate() 메소드 추가 구현

--- src/main/java/lotto/validator/LottoPriceValidator.java
+++ src/main/java/lotto/validator/LottoPriceValidator.java
@@ -9,6 +9,7 @@ public class LottoPriceValidator {
     public static void validate(String input) {
         validateLottoPriceNotNumber(input);
         validateLottoPriceNotDivided(input);
+        validateLottoPriceFirstZero(input);
     }
 
     private static void validateLottoPriceNotNumber(String input) throws IllegalArgumentException {
@@ -20,7 +21,13 @@ public class LottoPriceValidator {
     private static void validateLottoPriceNotDivided(String input) throws IllegalArgumentException {
         BigDecimal num = new BigDecimal(input);
         if (!num.remainder(BigDecimal.valueOf(LOTTO_ONE_PRICE)).equals(BigDecimal.ZERO)) {
-            throw new IllegalArgumentException("[ERROR} 구매 금액은 1000원 단위로 작성되어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 작성되어야 합니다.");
+        }
+    }
+
+    private static void validateLottoPriceFirstZero(String input) throws IllegalArgumentException {
+        if (input.charAt(0) == '0') {
+            throw new IllegalArgumentException("[ERROR] 0으로 시작하는 값을 입력하면 안됩니다.");
         }
     }
 

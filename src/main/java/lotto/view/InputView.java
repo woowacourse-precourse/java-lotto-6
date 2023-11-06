aee6e363daee18635304fd268d997bb65193eb54
akskflwn <akskflwn@gmail.com>
2023-11-06 15:08:42 +0900

    feat: 로또 구입 금액을 입력 받는 기능구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private final static String GET_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String NUMBER_REGEX = "^[0-9]+$";
+
+    public long getMoneyInput() {
+        System.out.println(GET_MONEY_MESSAGE);
+        String input = Console.readLine();
+        validate(input);
+        return Long.parseLong(input);
+    }
+
+    private void validate(String input) {
+        if(!input.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
 }

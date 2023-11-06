69ade1fb4dfbd94ceacb765e6f514b46b08632d2
U-keun Song <sukeun319@gmail.com>
2023-11-06 14:37:56 +0900

    Feat : InputBonusNumber 클래스 생성
    보너스 번호를 입력받는 클래스를 생성했다.

---
+++ src/main/java/lotto/model/InputBonusNumber.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import lotto.util.InputNumber;
+
+public class InputBonusNumber extends InputNumber {
+    InputBonusNumber(String input) {
+        super(input);
+    }
+
+    public static InputBonusNumber getInstance(String input) {
+        return new InputBonusNumber(input);
+    }
+
+    public void validate() {
+        if (notLottoNumber()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public Integer sendInputData() {
+        return this.number;
+    }
+
+    private boolean notLottoNumber() {
+        return this.number < 1 || this.number > 45;
+    }
+}

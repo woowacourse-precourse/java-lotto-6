73caf659f532a26982e98bc38855bc56eab041fe
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-06 10:22:25 +0900

    refactor: adjusted try-catch

--- src/main/java/lotto/domain/purchase/Purchase.java
+++ src/main/java/lotto/domain/purchase/Purchase.java
@@ -7,37 +7,23 @@ public class Purchase {
     private int paidPrice;
 
     public int makePurchase() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String purchasePrice = Console.readLine();
-        repeatPurchasing(purchasePrice);
-        System.out.println();
-        int lottoQuantity = paidPrice / 1000;
-        System.out.println(lottoQuantity + "개를 구매했습니다.");
-
-        return lottoQuantity;
-    }
-
-    public void repeatPurchasing(String purchasePrice) {
-
         while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String purchasePrice = Console.readLine();
             try {
-                validatePrice(purchasePrice);
-                break;
+                paidPrice = Integer.parseInt(purchasePrice);
+                validatePrice(paidPrice);
+                int lottoQuantity = paidPrice / 1000;
+                System.out.println(lottoQuantity + "개를 구매했습니다.");
+                return lottoQuantity;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
             }
         }
     }
 
-    public void validatePrice(String purchasePrice) {
-
-        paidPrice = Integer.parseInt(purchasePrice);
-
-        if (paidPrice % 1000 != 0) {
-            throw new IllegalArgumentException();
-        }
-
-        if (paidPrice == 0) {
+    public void validatePrice(int paidPrice) {
+        if (paidPrice % 1000 != 0 || paidPrice == 0) {
             throw new IllegalArgumentException();
         }
     }

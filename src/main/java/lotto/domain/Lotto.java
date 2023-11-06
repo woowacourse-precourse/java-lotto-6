a2b31d9beefc23d03f6e113bba7495322a3c4f9b
Infikei <hckhong@naver.com>
2023-11-06 19:57:54 +0900

    feat: 로또 번호로 주어진 정수의 범위 검증 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,10 +13,21 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (!isSixNumbers(numbers)) {
             throw new IllegalArgumentException();
+        } else if (!isInLottoRange(numbers)) {
+            throw new IllegalArgumentException();
         }
     }
 
     private boolean isSixNumbers(final List<Integer> numbers) {
         return numbers.size() == 6;
+    }
+
+    private boolean isInLottoRange(final List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                return false;
+            }
+        }
+        return true;
     }
 }

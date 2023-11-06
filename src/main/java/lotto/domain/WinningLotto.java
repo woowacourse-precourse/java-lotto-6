07a3b069a4f3c3ccdb48df934a77d38a5fdd5362
woong2e <tlswodnd12345@naver.com>
2023-11-07 02:38:34 +0900

    feat: 당첨 번호를 저장하고 구매한 로또와 비교하는 기능

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,2 +1,30 @@
-package lotto.domain;public class WinningLotto {
+package lotto.domain;
+
+public class WinningLotto   {
+    private static Lotto LOTTO;
+    private static int BONUS;
+
+    public WinningLotto(Lotto lotto, int bonus) {
+        this.LOTTO = lotto;
+        this.BONUS = bonus;
+    }
+
+    public static Rank compare(Lotto lotto) {
+        int countOfMatch = countMatchNumber(lotto);
+        boolean hasBonus = lotto.getNumbers().contains(BONUS);
+        for (Rank rank : Rank.values()) {
+            return rank.valueOf(countOfMatch, hasBonus);
+        }
+        return Rank.LOSING_TICKET;
+    }
+
+    private static int countMatchNumber(Lotto lotto) {
+        int count = 0;
+        for (int number : lotto.getNumbers()) {
+            if (LOTTO.getNumbers().contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }

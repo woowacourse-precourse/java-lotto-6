41c873e1df7ce50885924150ef7b114731df93ae
jooda00 <dragon2000a@gmail.com>
2023-11-06 20:07:23 +0900

    feat(LottoController): 당첨 번호 및 보너스 번호 입력 처리 메소드 생성
    - progressLotto()

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,16 +1,22 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lottos;
 import lotto.domain.Player;
 import lotto.validation.LottoValidation;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
     private Player player;
+    private Lottos lottos;
 
     public void play() {
         prepareLotto();
+        progressLotto();
     }
 
     private void prepareLotto() {
@@ -22,6 +28,17 @@ public class LottoController {
         catch (IllegalArgumentException e) {
             System.out.println(e);
             prepareLotto();
+        }
+    }
+
+    private void progressLotto() {
+        try {
+            List<Integer> winningNumber = getWinningNumber();
+            int bonusNumber = getBonusNumber();
+            lottos = new Lottos(winningNumber, bonusNumber, player);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+            progressLotto();
         }
     }
 
@@ -37,7 +54,35 @@ public class LottoController {
         }
     }
 
+    private List<Integer> getWinningNumber() {
+        InputView.inputWinningNumber();
+        String input = Console.readLine();
+        return convertStringToIntList(input);
+    }
+
+    private int getBonusNumber() {
+        try {
+            InputView.inputBonusNumber();
+            String input = Console.readLine();
+            LottoValidation.validateIsNumberBonus(input);
+            int number = convertStringToInt(input);
+            LottoValidation.validateInRange(number);
+            return number;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+            return getBonusNumber();
+        }
+    }
+
     private int convertStringToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private List<Integer> convertStringToIntList(String input) {
+        List<Integer> result = new ArrayList<>();
+        for (String num : input.split(",")) {
+            result.add(Integer.parseInt(num));
+        }
+        return result;
     }
 }

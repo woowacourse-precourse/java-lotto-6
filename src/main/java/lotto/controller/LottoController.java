1c1912805d8de9d73e55cfdbfb6fee1eb1f2839d
KJBig <kjgi73k@gmail.com>
2023-11-06 22:45:14 +0900

    feat : 당첨 번호 입력 기능 추가
    - 당청 번호 입력 메세지 출력 기능 구현.
    - 당첨 번호 입력 기능 구현.
    - 쉼표(,)를 기준으로 나누어 List<>로 바꾸는 기능 구현.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 
@@ -16,5 +18,21 @@ public class LottoController {
         String lottoPayAmount = Console.readLine();
         // TODO : 로또 구매 기능 인풋 Validate
         lottoService.processStart(Integer.parseInt(lottoPayAmount));
+
+        InputView.printJackpotNumberInputMessage();
+        String jackpotNumberInput = Console.readLine();
+        // TODO : 당첨번호 인풋 Validate
+
+        List<Integer> jackpotNumbers = converteToList(jackpotNumberInput);
+
+
+    }
+
+    private List<Integer> converteToList(String jackpotNumberInput) {
+        List<Integer> jackpotNumbers = new ArrayList<>();
+        for (String number : jackpotNumberInput.split(",")) {
+            jackpotNumbers.add(Integer.parseInt(number));
+        }
+        return jackpotNumbers;
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,8 +2,14 @@ package lotto.view;
 
 public class InputView {
     private static final String PAY_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String JACKPOT_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static void printPayAmountInputMessage() {
         System.out.println(PAY_AMOUNT_INPUT_MESSAGE);
+    }
+
+    public static void printJackpotNumberInputMessage() {
+        System.out.println();
+        System.out.println(JACKPOT_NUMBER_INPUT_MESSAGE);
     }
 }

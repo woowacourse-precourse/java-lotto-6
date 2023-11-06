d2e87710bfed3a5f13e4df8f2e62e3e1ebf5aa3d
park_yeongyu <sjjs7771@naver.com>
2023-11-06 22:43:53 +0900

    feat(User) : Lotto 번호를 지정하고 얻을 수 있는 함수 작성.

---
+++ src/main/java/lotto/Model/User.java
@@ -0,0 +1,17 @@
+package lotto.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private List<Integer> userLotto; //사용자의 로또 번호가 담길 리스트
+
+    public void setUserLotto(ArrayList<Integer> LottoNumbers) {
+        this.userLotto = LottoNumbers;
+    }
+
+    public List<Integer> getUserLotto() {
+        return userLotto;
+    }
+
+}

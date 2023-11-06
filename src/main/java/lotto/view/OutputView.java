6bf4b09461ddbb0ad9867422249bd26065323abe
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-06 13:46:55 +0900

    refactor: OutputMessage enum 적용하기

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,21 +1,17 @@
 package lotto.view;
 
+import static lotto.enums.OutputMessages.*;
 import static lotto.enums.Prize.*;
 
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.WinningDetails;
+import lotto.enums.OutputMessages;
 
 public class OutputView {
-    private static final String PURCHASE_LOTTO = "%d개를 구매했습니다.\n";
-    private static final String WINNING_STATISTICS = "당첨 통게";
-    private static final String DIVIDED_LINE = "---";
-    private static final String MATCH_NUMBERS = "%d개 일치 (%s원) - %d개\n";
-    private static final String MATCH_FIVE_NUMBERS_BONUS_NUMBERS = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
-    private static final String EARNING_RATE = "총 수익률은 %.1f%%입니다.";
 
     public static void purchaseLotto(int lottoCount){
-        System.out.printf(PURCHASE_LOTTO,lottoCount);
+        System.out.printf(PURCHASE_LOTTO.getMessage(),lottoCount);
     }
 
     public static void showEachLotto(List<Lotto> lottos) {
@@ -27,16 +23,16 @@ public class OutputView {
     public static void showWinningDetails(WinningDetails winningDetails){
         int[] ranks = winningDetails.getRank();
 
-        System.out.println(WINNING_STATISTICS);
-        System.out.println(DIVIDED_LINE);
-        System.out.printf(MATCH_NUMBERS, FIFTH_RANK.getMatchNumbers(), FIFTH_RANK.getMoneyToString(), ranks[FIFTH_RANK.getRank()]);
-        System.out.printf(MATCH_NUMBERS, FOURTH_RANK.getMatchNumbers(), FOURTH_RANK.getMoneyToString(), ranks[FOURTH_RANK.getRank()]);
-        System.out.printf(MATCH_NUMBERS, THIRD_RANK.getMatchNumbers(), THIRD_RANK.getMoneyToString(), ranks[THIRD_RANK.getRank()]);
-        System.out.printf(MATCH_FIVE_NUMBERS_BONUS_NUMBERS, SECOND_RANK.getMatchNumbers(), SECOND_RANK.getMoneyToString(), ranks[SECOND_RANK.getRank()]);
-        System.out.printf(MATCH_NUMBERS, FIRST_RANK.getMatchNumbers(), FIRST_RANK.getMoneyToString(), ranks[FIRST_RANK.getRank()]);
+        System.out.println(WINNING_STATISTICS.getMessage());
+        System.out.println(DIVIDED_LINE.getMessage());
+        System.out.printf(MATCH_NUMBERS.getMessage(), FIFTH_RANK.getMatchNumbers(), FIFTH_RANK.getMoneyToString(), ranks[FIFTH_RANK.getRank()]);
+        System.out.printf(MATCH_NUMBERS.getMessage(), FOURTH_RANK.getMatchNumbers(), FOURTH_RANK.getMoneyToString(), ranks[FOURTH_RANK.getRank()]);
+        System.out.printf(MATCH_NUMBERS.getMessage(), THIRD_RANK.getMatchNumbers(), THIRD_RANK.getMoneyToString(), ranks[THIRD_RANK.getRank()]);
+        System.out.printf(MATCH_FIVE_NUMBERS_BONUS_NUMBERS.getMessage(), SECOND_RANK.getMatchNumbers(), SECOND_RANK.getMoneyToString(), ranks[SECOND_RANK.getRank()]);
+        System.out.printf(MATCH_NUMBERS.getMessage(), FIRST_RANK.getMatchNumbers(), FIRST_RANK.getMoneyToString(), ranks[FIRST_RANK.getRank()]);
     }
 
     public static void earningRate(float earningRate){
-        System.out.printf(EARNING_RATE, earningRate);
+        System.out.printf(EARNING_RATE.getMessage(), earningRate);
     }
 }

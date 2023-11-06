d9f95c9f9171d7abff653beaf242d0da270031cd
haero77 <shineecard5@gmail.com>
2023-11-07 07:24:12 +0900

    test: 로또 구입 금액시 예외 케이스 테스트 추가

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,17 +1,26 @@
 package lotto.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import lotto.domain.LottoPurchaseAmount;
 import lotto.mock.FakePrinter;
 import lotto.mock.FakeReader;
 import lotto.view.printer.Printer;
 import lotto.view.reader.Reader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
+
+    private Printer printer;
+
+    @BeforeEach
+    void setUp() {
+        this.printer = new FakePrinter();
+    }
 
     @DisplayName("1000원 단위로 로또 구입 금액을 입력할 수 있다.")
     @ParameterizedTest
@@ -19,8 +28,6 @@ class InputViewTest {
     void inputLottoPurchaseAmount(String input) {
         // given
         Reader reader = new FakeReader(input);
-        Printer printer = new FakePrinter();
-
         InputView inputView = new InputView(reader, printer);
 
         // when
@@ -28,6 +35,32 @@ class InputViewTest {
 
         // then
         assertThat(result.getAmount()).isEqualTo(input);
+    }
+
+    @DisplayName("로또 구입 금액이 양수가 아닐 시 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "-1000"})
+    void inputLottoPurchaseAmount_exception1(String input) {
+        // given
+        Reader reader = new FakeReader(input);
+        InputView inputView = new InputView(reader, printer);
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(inputView::inputLottoPurchaseAmount);
+    }
+
+    @DisplayName("로또 구입 금액이 1000원 단위가 아닐 시 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "999", "1001", "333333"})
+    void inputLottoPurchaseAmount_exception2(String input) {
+        // given
+        Reader reader = new FakeReader(input);
+        InputView inputView = new InputView(reader, printer);
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(inputView::inputLottoPurchaseAmount);
     }
 
 }

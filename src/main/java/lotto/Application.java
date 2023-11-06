90bf3b020ac4d3b70461515196aa5726e49c3411
Yoon Jiho <zlhh3842@nate.com>
2023-11-06 20:36:31 +0900

    style : 코드 정렬

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,19 +9,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class Application {
-    enum LOTTO_PRIZE{
+    enum LOTTO_PRIZE {
         FIRST(1, 2000000000),
         SECOND(2, 30000000),
         THIRD(3, 1500000),
         FOURTH(4, 50000),
         FIFTH(5, 5000),
-        NONE(0,0);
+        NONE(0, 0);
 
         private final long prize;
         private final int rank;
 
         LOTTO_PRIZE(int rank, long prize) {
-            this. rank = rank;
+            this.rank = rank;
             this.prize = prize;
         }
 
@@ -30,9 +30,10 @@ public class Application {
         }
 
         int getRank() {
-            return this. rank;
+            return this.rank;
         }
     }
+
     private static final int LOTTO_PRICE = 1000;
 
     public static void main(String[] args) {
@@ -40,17 +41,16 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         int price = 0;
         boolean validInput = false;
-        while(!validInput) {
+        while (!validInput) {
             String input = Console.readLine();
             try {
                 price = Integer.parseInt(input);
-                if(price % LOTTO_PRICE != 0) {
+                if (price % LOTTO_PRICE != 0) {
                     System.out.println("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
                     continue;
                 }
                 validInput = true;
-            }
-            catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 System.out.println("[ERROR] 구입금액은 숫자를 입력하여야 합니다.");
             }
         }
@@ -58,14 +58,14 @@ public class Application {
         // 2. 로또 구매
         int count = price / LOTTO_PRICE;
         List<Lotto> purchasedLotto = new ArrayList<>();
-        for(int i=0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(numbers);
             purchasedLotto.add(lotto);
         }
 
         System.out.printf("\n%d개를 구매했습니다.\n", count);
-        for(Lotto lotto: purchasedLotto) {
+        for (Lotto lotto : purchasedLotto) {
             lotto.printLotto();
         }
 
@@ -76,22 +76,22 @@ public class Application {
         try {
             String[] seps = winNumInput.split(",");
             List<Integer> intList = new ArrayList<>();
-            for(String sep: seps){
+            for (String sep : seps) {
                 int num = Integer.parseInt(sep);
-                if(num > 45 || num < 0) {
+                if (num > 45 || num < 0) {
                     throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
                 }
-                if(intList.contains(num)) {
+                if (intList.contains(num)) {
                     throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
                 }
                 intList.add(num);
             }
             winNumbers = intList;
 
-            if(winNumbers.size() != 6) {
+            if (winNumbers.size() != 6) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다.");
             }
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 로또 번호 형식입니다.");
         }
 
@@ -101,10 +101,10 @@ public class Application {
         int bonus;
         try {
             bonus = Integer.parseInt(bonusInput);
-            if(bonus <= 0 || bonus > 45) {
+            if (bonus <= 0 || bonus > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 로또 번호 형식입니다.");
         }
 
@@ -112,14 +112,14 @@ public class Application {
         List<LOTTO_PRIZE> lottoPrizes = new ArrayList<>();
         int[] prizeLog = new int[6];
         Arrays.fill(prizeLog, 0);
-        for(Lotto lotto:purchasedLotto) {
+        for (Lotto lotto : purchasedLotto) {
             int matchCount = 0;
             boolean bonusMatch = false;
-            for(int lottoNum: lotto.getLotto()) {
-                if(winNumbers.contains(lottoNum)) {
+            for (int lottoNum : lotto.getLotto()) {
+                if (winNumbers.contains(lottoNum)) {
                     matchCount += 1;
                 }
-                if(winNumbers.contains(bonus)) {
+                if (winNumbers.contains(bonus)) {
                     bonusMatch = true;
                 }
             }
@@ -140,7 +140,7 @@ public class Application {
 
         // 6.총 수익률 출력
         long totalPrize = 0;
-        for(LOTTO_PRIZE info : lottoPrizes) {
+        for (LOTTO_PRIZE info : lottoPrizes) {
             totalPrize += info.getPrize();
         }
         double rateOfReturn = (double) totalPrize / price * 100;
@@ -149,19 +149,19 @@ public class Application {
     }
 
     private static LOTTO_PRIZE getPrizeRank(int matchCount, boolean bonusMatch) {
-        if(matchCount == 6) {
+        if (matchCount == 6) {
             return LOTTO_PRIZE.FIRST;
         }
-        if(matchCount == 5 && bonusMatch) {
+        if (matchCount == 5 && bonusMatch) {
             return LOTTO_PRIZE.SECOND;
         }
-        if(matchCount == 5) {
+        if (matchCount == 5) {
             return LOTTO_PRIZE.THIRD;
         }
-        if(matchCount == 4) {
+        if (matchCount == 4) {
             return LOTTO_PRIZE.FOURTH;
         }
-        if(matchCount == 3) {
+        if (matchCount == 3) {
             return LOTTO_PRIZE.FIFTH;
         }
         return LOTTO_PRIZE.NONE;

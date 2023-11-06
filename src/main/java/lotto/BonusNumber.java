2adab6ce2bcd25a2cd39a5566ed6ddb566937292
hoyun06 <hoyun06@gmail.com>
2023-11-06 13:37:30 +0900

    feat: BonusNumber 클래스 구현

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class BonusNumber {
+
+    private int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return this.bonusNumber;
+
+    }
+}

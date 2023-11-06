26321cb634402f3543f69b65c429eab3d63f452c
donghoonyeom <dua5410773@naver.com>
2023-11-07 01:15:31 +0900

    feat(lotto): LottoGenerator 클래스 추가
    로또 번호를 생성하고 로또 티켓을 발행하는 LottoGenerator 클래스를 추가했습니다. 이 클래스에는 `generateLotto()` 함수와 `generateLottos(int count)` 함수가 포함되어 있습니다. `generateLotto()` 함수는 중복되지 않는 6개의 로또 번호를 생성하고, `generateLottos(int count)` 함수는 지정된 개수만큼의 로또 티켓을 생성합니다.

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    public static final int LOTTO_PRICE = 1000;
+
+    public static Lotto generateLotto() {
+        try {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, 6);
+            return new Lotto(numbers);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("로또 번호 생성 중 오류가 발생했습니다: " + e.getMessage());
+        }
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("로또 개수는 1개 이상이어야 합니다.");
+        }
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(generateLotto());
+        }
+        return lottos;
+    }
+}

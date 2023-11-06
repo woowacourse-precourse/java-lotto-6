b6711dbdf76a2907da07db3aac11c17cd915a860
senzoo-oh <osj3418@gmail.com>
2023-11-06 23:21:57 +0900

    feat(lotto): implement a feature of getting winning number from user

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class WinningNumber {
     //상수(static final) 또는 클래스 변수
     private final List<Integer> winningNumbers;
@@ -10,11 +12,25 @@ public class WinningNumber {
     //인스턴스 변수
 
     //생성자
-    public WinningNumber(String winningNumbersInput) {
-        List<Integer> winningNumber = new ArrayList<>();
-        // String을 List<Integer>형태로 반환함
-        isDuplicate(winningNumber);
-        this.winningNumbers = winningNumber;
+    public WinningNumber() {
+        this.winningNumbers = getWinningNumber();
+    }
+
+    private List<Integer> getWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumber = readLine();
+
+        List<Integer> result = new ArrayList<>();
+        for (String s : winningNumber.split(",")) {
+            result.add(Integer.parseInt(s));
+        }
+
+        isDuplicate(result);
+        return result;
+    }
+
+    private void validateCount() {
+        // 입력받은 당첨번호의 개수 확인
     }
 
     private void isDuplicate(List<Integer> winningNumber) {

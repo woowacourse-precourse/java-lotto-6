9e1ec5349cadab28b2ffc00168404db1a7524a17
hYe807n <rlagkdus55@gmail.com>
2023-11-06 11:16:10 +0900

    Test: 로또 번호의 범위, 중복, 개수에 대한 단위 테스트 구현

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
 import lotto.model.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -16,13 +19,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호의 범위가 1부터 45가 아니라면 예외가 발생힌다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 50));
+        });
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    // 아래에 추가 테스트 작성 가능
 }

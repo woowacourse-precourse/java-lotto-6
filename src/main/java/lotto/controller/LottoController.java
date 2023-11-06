1be132e38c4a9a6ecab2a573a988f7a11d24a188
SJ70 <seojae70@gmail.com>
2023-11-06 21:56:19 +0900

    Feat: 구입 금액 입력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.cash.Cash;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,13 +9,25 @@ public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
 
+    private Cash purchaseCash;
+
     public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void run() {
+        inputPurchaseCash();
+    }
 
+    private void inputPurchaseCash() {
+        // 구입금액을 입력해 주세요.
+        try {
+            purchaseCash = new Cash(inputView.inputNumber());
+        } catch (IllegalArgumentException e) {
+            outputView.print(e.getMessage());
+            inputPurchaseCash();
+        }
     }
 
 }

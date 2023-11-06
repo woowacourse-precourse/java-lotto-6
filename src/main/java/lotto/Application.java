f284337575b80608e6423d68447ce0146b689965
박수빈 <jrkd2165@gmail.com>
2023-11-06 22:26:50 +0900

    :sparkles: feat: 당첨 번호 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.Arrays;
 import java.util.List;
+
 
 public class Application {
 
@@ -18,12 +20,22 @@ public class Application {
         //1000의 배수라면 (메서드 나누기)
         //구입한 로또의 개수 출력
         System.out.println(String.format("%d개를 구매했습니다.",purchaseAmount / 1000));
+        //로또 생성
         //구입한 로또의 번호 출력
         List<Lotto> lottos = Lotto.CreateLottos(purchaseAmount / 1000);
 
         //당첨 번호 입력 받기
+        // ,로 분리되지 않는 경우
+        // 문자만 입력한 경우
+        // 6개의 수를 입력하지 않은 경우
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<String> winningNumberOfString =List.of(Console.readLine().split(","));
+        for(int i=0;i<6;i++){
+            System.out.println(winningNumberOfString.get(i));
+        }
+
         //보너스 번호 입력 받기
-        //로또 생성
+
         //구매한 로또의 번호를 보며, 몇 등인지 체크
         //수익률 계산
         //당첨 통계 출력

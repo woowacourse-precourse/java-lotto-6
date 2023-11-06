cd3a88e06f8bd6549689ffa82aca36356e467231
minju <minju9711@naver.com>
2023-11-07 03:27:45 +0900

    [feat] 컴퓨터에서 랜덤으로 만들어준 로또 객체가 담긴 lottoContainer 리스트와 당첨번호가 담긴 WinningLotto 리스트의 정보를 생성하는 클래스

--- src/main/java/lotto/Factory/LottoContainer.java
+++ src/main/java/lotto/Factory/LottoContainer.java
@@ -1,4 +1,19 @@
 package lotto.Factory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoContainer {
+    private List<Lotto> lottoContainer = new ArrayList<>();
+    private List<Lotto> WinningLotto = new ArrayList<>();
+    public void MakeContainer(int count){
+        for(int i=0; i<count; i++){
+            lottoContainer.add(Lotto.createLotto());
+        }
+    }
+
+    public void MakeWinningLotto(List<Integer> WinningInput){
+        WinningLotto.add(Lotto.createLotto(WinningInput));
+    }
+
 }

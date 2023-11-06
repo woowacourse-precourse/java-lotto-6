100dbf1a3249184a0aef34d32700a19985a72495
hoyeon <highoption@naver.com>
2023-11-06 21:03:22 +0900

    feat(InputView) : 구매 금액을 입력 받는 inputMoney 메서드 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+}

7c8b75ce16325edbd19680a74fa358fff9b785c2
soozzang <soozzang@inha.edu>
2023-11-06 19:34:50 +0900

    Feat(UserService): 유저의 정보 수정해 리턴하는 메서드 생성

---
+++ src/main/java/lotto/model/service/UserService.java
@@ -0,0 +1,14 @@
+package lotto.model.service;
+
+import lotto.model.domain.Rank;
+import lotto.model.domain.User;
+import lotto.view.InputView;
+
+public class UserService {
+    InputView inputView = new InputView();
+
+    public User setUserInfo() {
+
+        return new User(inputView.readPurchaseMoney());
+    }
+}

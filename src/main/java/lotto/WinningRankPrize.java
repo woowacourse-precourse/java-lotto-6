0e1d13bf03ecc0f910e7e9dc9589c4493684e2a2
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-06 15:34:32 +0900

    feat(WinningRankPrize): 열거형 필드들 getter 함수들 구현

--- src/main/java/lotto/WinningRankPrize.java
+++ src/main/java/lotto/WinningRankPrize.java
@@ -8,13 +8,20 @@ public enum WinningRankPrize {
     SECOND("5개 일치, 보너스 볼 일치", "30,000,000원"),
     FIRST("6개 일치", "2,000,000,000원");
 
-
     private String matchCount;
     private String prizeAmount;
 
     WinningRankPrize(String matchCount, String prizeAmount) {
         this.matchCount = matchCount;
         this.prizeAmount = prizeAmount;
+    }
+
+    public String getMatchCount() {
+        return matchCount;
+    }
+
+    public String getPrizeAmount() {
+        return prizeAmount;
     }
 
 }

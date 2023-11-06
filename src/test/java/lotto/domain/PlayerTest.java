abbe93938f95326c863cafd6973684b15dc472a4
Jiwon Lee <jiwon718@users.noreply.github.com>
2023-11-06 17:31:35 +0900

    test: 로또 당첨 번호 예외 처리 테스트

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
     @DisplayName("로또 구매 금액이 숫자가 아니면 예외가 발생한다.")
@@ -17,6 +19,29 @@ class PlayerTest {
     @Test
     void inputLottoPurchaseAmountByDivided() {
         assertThatThrownBy(() -> new Player().setPurchaseAmount("100"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호의 개수가 6개가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7"})
+    void inputLottoWinningNumbersBySize(String input) {
+        assertThatThrownBy(() -> new Player().setWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호가 1~45 사이의 숫자가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0,1,2,3,4,5", "41,42,43,44,45,46"})
+    void inputLottoWinningNumbersByValue(String input) {
+        assertThatThrownBy(() -> new Player().setWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void inputLottoWinningNumbersByValue() {
+        assertThatThrownBy(() -> new Player().setWinningNumbers("1,2,3,4,5,5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

074fb2d0350e80d51053a363344cd62df8cbeaad
Kimhyunjin4455 <dp4455@naver.com>
2023-11-07 04:10:17 +0900

    refactor: 랜덤값 생성 위치 조정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,8 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.util.Validator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -12,15 +14,15 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.*;
 
 public class LottoController {
-//    public static final int MIN_NUM = 1;
-//    public static final int MAX_NUM = 45;
-//    public static final int LOTTO_NUMBER = 6;
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 45;
+    public static final int LOTTO_NUMBER = 6;
     public static void inputPurchaseAmount(){
         InputView.requestPurchaseAmountMessage();
         String amount = Console.readLine();
         System.out.println();
         inputAmountValidator(amount);
-        lottoCount(amount);
+        lottoReady(amount);
     }
 
     public static void inputAmountValidator(String amount){
@@ -43,19 +45,29 @@ public class LottoController {
         Validator.validateBonusNumChange(bonusNum);
     }
 
-    public static void lottoCount(String amount){
+    public static void lottoReady(String amount){
         int lottoCounts = (Integer.parseInt(amount)) / 1000;
-        createLottoNum(lottoCounts);
+        for(int i=0; i< lottoCounts; i++){
+            lottoRun(lottoCounts);
+        }
+//        createLottoNum(lottoCounts);
     }
 
-    public static void createLottoNum(Integer lottoCounts){ // 횟수 만큼 반복
-        for(int i = 0; i< lottoCounts; i++){
-            createlLottoLst();
-        }
+    public static void lottoRun(Integer lottoCounts){
+        List<Integer> numbers = pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, LOTTO_NUMBER);
+        Lotto lotto = new Lotto(numbers);
+        OutputView.lottoPrinter(numbers);
+        Lottos.createLotto(lottoCounts, lotto);
     }
-    public static void createlLottoLst(){ // 생성 후 리스트로
-        OutputView.lottoPrinter(); // 로또 리스트 출력
-    }
+
+//    public static void createLottoNum(Integer lottoCounts){ // 횟수 만큼 반복
+//        for(int i = 0; i< lottoCounts; i++){
+//            createlLottoLst();
+//        }
+//    }
+//    public static void createlLottoLst(){ // 생성 후 리스트로
+//        OutputView.lottoPrinter(); // 로또 리스트 출력
+//    }
 
 
 

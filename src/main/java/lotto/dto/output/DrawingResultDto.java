9d5b505de77b208ab683ff59e0baf7ab11a899f1
minisyu <iswater.kim@gmail.com>
2023-11-06 15:52:59 +0900

    feat: DrawingResultDto 생성

---
+++ src/main/java/lotto/dto/output/DrawingResultDto.java
@@ -0,0 +1,11 @@
+package lotto.dto.output;
+
+public record DrawingResultDto(
+        int firstMatchCount,
+        int secondMatchCount,
+        int thirdMatchCount,
+        int fourthMatchCount,
+        int fifthMatchCount,
+        double earningOfRate
+) {
+}

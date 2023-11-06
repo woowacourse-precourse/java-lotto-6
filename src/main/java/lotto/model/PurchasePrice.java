35879be0858e15c162de87abd4db914ba4506438
kkkkimtaehyeon <kth1927@naver.com>
2023-11-06 20:51:14 +0900

    feat : 정규표현식 수정

--- src/main/java/lotto/model/PurchasePrice.java
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -1,14 +1,11 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class PurchasePrice {
 
     private final int PIECE_OF_LOTTO;
     private static final int LOTTO_PRICE_EACH = 1000;
 
     public PurchasePrice(String priceFromUser){
-
         isNotDigits(priceFromUser);
         isUnder1000(priceFromUser);
         isNotDivisibleBy1000(priceFromUser);
@@ -18,22 +15,18 @@ public class PurchasePrice {
     public int getCountPieceOfLotto(){
         return this.PIECE_OF_LOTTO;
     }
+    private void isNotDigits(String purchasePrice) throws IllegalArgumentException{
+        if(!purchasePrice.matches("^[0-9]+$")){
+            throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요.");
+        }
+    }
 
     private void isUnder1000(String purchasePrice) throws IllegalArgumentException{
         if(Integer.parseInt(purchasePrice) < LOTTO_PRICE_EACH){
             throw new IllegalArgumentException("[ERROR] 로또 한장은 1000원입니다.");
         }
     }
-    private void isNotDigits(String purchasePrice) throws IllegalArgumentException{
-        try{
-            if(purchasePrice.matches("[^0-9]")){
-                throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요.");
-            }
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
 
-    }
     private void isNotDivisibleBy1000(String purchasePrice) throws IllegalArgumentException{
         if(Integer.parseInt(purchasePrice) % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");

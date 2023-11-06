1ff196893cfb4f171c801c40c05ede8dfd7d6735
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 00:36:13 +0900

    feat: add publish lotto function to user lotto model

--- src/main/java/lotto/model/UserLotto.java
+++ src/main/java/lotto/model/UserLotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class UserLotto {
 
@@ -9,5 +11,16 @@ public class UserLotto {
 
     private void moneyToLotto(int money) {
         this.lottoCount = money / 1000;
+    }
+
+    private Lotto publishLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return new Lotto(numbers);
     }
 }

a860d1a51a711b41bb19bf3008d7fa05bcb075ea
hil679 <hil679922@gmail.com>
2023-11-07 05:07:36 +0900

    Style: output mesesage 수정
    개행 수정

--- src/main/java/lotto/util/ProgramMessage.java
+++ src/main/java/lotto/util/ProgramMessage.java
@@ -2,12 +2,13 @@ package lotto.util;
 
 public enum ProgramMessage {
     MONEY_INPUT_GUIDE_MESSAGE("구입금액을 입력해 주세요."),
-    PURCHASE_LOTTO_NUM_MESSAGE("%d개를 구매했습니다.\n"),
-    WIN_LOTTO_NUMBER_INPUT_GUIDE_MESSAGE("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_INPUT_GUIDE_MESSAGE("보너스 번호를 입력해 주세요."),
-    WIN_RESULT_GUIDE_MESSAGE("당첨 통계\n---"),
-    WIN_RESULT("%d 개 일치 (%s원) - %d개\n"),
-    PROFIT_MESSAGE("총 수익률은 %.2f%입니다.");
+    PURCHASE_LOTTO_NUM_MESSAGE("\n%d개를 구매했습니다.\n"),
+    WIN_LOTTO_NUMBER_INPUT_GUIDE_MESSAGE("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT_GUIDE_MESSAGE("\n보너스 번호를 입력해 주세요."),
+    WIN_RESULT_GUIDE_MESSAGE("\n당첨 통계\n---"),
+    WIN_RESULT_WITHOUT_BONUS("%d개 일치 (%s원) - %d개\n"),
+    WIN_RESULT_WITH_BONUS("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n"),
+    PROFIT_MESSAGE("총 수익률은 %s%%입니다.");
 
     String message;
 

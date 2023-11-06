7ecd791f7ebdaf760e2a5db61fb6095ba5a4488a
wns312 <wns312@gmail.com>
2023-11-06 12:49:05 +0900

    test(LottoBonusNumber): LottoBonusNumber 단위테스트 추가

---
+++ src/test/java/lotto/domain/model/LottoBonusNumberTest.java
@@ -0,0 +1,37 @@
+package lotto.domain.model;
+
+import lotto.constant.IllegalArgumentExceptionType;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+class LottoBonusNumberTest {
+
+    @Test
+    void 번호_범위_검증_성공_테스트() {
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThatCode(() -> new LottoBonusNumber(1)).doesNotThrowAnyException();
+        softAssertions.assertThatCode(() -> new LottoBonusNumber(20)).doesNotThrowAnyException();
+        softAssertions.assertThatCode(() -> new LottoBonusNumber(45)).doesNotThrowAnyException();
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    void 번호_범위_검증_실패_테스트() {
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThatThrownBy(() -> new LottoBonusNumber(-1))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(IllegalArgumentExceptionType.LOTTO_RANGE_ERROR.getMessage());
+
+        softAssertions.assertThatThrownBy(() -> new LottoBonusNumber(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentExceptionType.LOTTO_RANGE_ERROR.getMessage());
+
+        softAssertions.assertThatThrownBy(() -> new LottoBonusNumber(46))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(IllegalArgumentExceptionType.LOTTO_RANGE_ERROR.getMessage());
+
+        softAssertions.assertAll();
+    }
+}

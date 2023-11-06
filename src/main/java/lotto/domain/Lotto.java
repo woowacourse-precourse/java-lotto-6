cd4b325b7ead7c0999ceeb4f48c05db01b060c94
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 10:02:58 +0900

    feat(Lotto) : 세 가지 검증 기능 추가
    1~45사이 숫자로 이루어져있는지?
    중복되는 숫자가 존재하는지?
    6개의 숫자로 이루어져있는지?

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,13 @@
 package lotto.domain;
 
+import static lotto.ErrorMessage.HAS_DUPLICATION_ERROR_MESSAGE;
+import static lotto.ErrorMessage.WRONG_RANGE_ERROR_MESSAGE;
+import static lotto.ErrorMessage.WRONG_SIZE_ERROR_MESSAGE;
+import static lotto.LottoConstance.LOTTO_SIZE;
+import static lotto.LottoConstance.MAX_NUMBER_LOTTO_RANGE;
+import static lotto.LottoConstance.MIN_NUMBER_LOTTO_RANGE;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,9 +19,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!isLottoSize(numbers)) {
+            throwException(WRONG_SIZE_ERROR_MESSAGE.get());
+        } else if (!isBetween1to45(numbers)) {
+            throwException(WRONG_RANGE_ERROR_MESSAGE.get());
+        } else if (hasDuplication(numbers)) {
+            throwException(HAS_DUPLICATION_ERROR_MESSAGE.get());
         }
+    }
+
+    private boolean isBetween1to45(List<Integer> numbers) {
+        return numbers.stream().allMatch(n -> n >= MIN_NUMBER_LOTTO_RANGE.get() && n <= MAX_NUMBER_LOTTO_RANGE.get());
+    }
+
+    private boolean hasDuplication(List<Integer> numbers) {
+        return numbers.size() > new HashSet<>(numbers).size();
+    }
+
+    private boolean isLottoSize(List<Integer> numbers) {
+        return numbers.size() == LOTTO_SIZE.get();
+    }
+
+    private void throwException(String errorMessage) {
+        throw new IllegalArgumentException(errorMessage);
     }
 
 }

19a4f1d6594ecdc22ec51e0778b5863d1b01a170
ehddbs4521 <ehddbs452100@naver.com>
2023-11-06 23:33:23 +0900

    feat: 입력받은 로또 번호들 오름차순으로 정렬

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers = sorted(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +19,7 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private List<Integer> sorted(List<Integer> numbers) {
+        return numbers.stream().sorted().collect(Collectors.toList());
+    }
 }

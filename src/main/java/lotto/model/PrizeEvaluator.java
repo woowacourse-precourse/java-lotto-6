b48105eac7a9da6e058082241aacc1ecf053aed8
Yehyeok Bang <qkddpgur318@gmail.com>
2023-11-06 13:24:02 +0900

    refactor(model): 번호 매칭 결과 처리 로직 분리
    Enum을 사용하여 매칭 결과를 정의합니다.
    - 매칭된 번호 수, 상품 금액, 결과 처리 행동

--- src/main/java/lotto/model/PrizeEvaluator.java
+++ src/main/java/lotto/model/PrizeEvaluator.java
@@ -1,15 +1,8 @@
 package lotto.model;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class PrizeEvaluator {
-    private static final int THREE_MATCH_PRIZE = 5_000;
-    private static final int FOUR_MATCH_PRIZE = 50_000;
-    private static final int FIVE_MATCH_PRIZE = 1_500_000;
-    private static final int FIVE_MATCH_WITH_BONUS_PRIZE = 30_000_000;
-    private static final int SIX_MATCH_PRIZE = 2_000_000_000;
-
     private int threeMatchCount = 0;
     private int fourMatchCount = 0;
     private int fiveMatchCount = 0;
@@ -23,7 +16,8 @@ public class PrizeEvaluator {
             int matchCount = countMatch(winningNumbers.getNumbers(), lotto.getNumbers());
             boolean isBonusMatch = lotto.getNumbers().contains(bonusNumber);
 
-            checkMatch(matchCount, isBonusMatch);
+            PrizeType prizeType = PrizeType.valueOf(matchCount, isBonusMatch);
+            prizeType.increasePrize(this, isBonusMatch);
         }
 
         int prizeMoney = calculatePrizeMoney();
@@ -37,59 +31,31 @@ public class PrizeEvaluator {
                 .count();
     }
 
-    private void checkMatch(int matchCount, boolean isBonusMatch) {
-        List<Runnable> prizeIncreaseActions = Arrays.asList(
-                this::noMatch,
-                this::oneMatch,
-                this::twoMatch,
-                this::increaseThreeMatchPrize,
-                this::increaseFourMatchPrize,
-                () -> increaseFiveMatchPrize(isBonusMatch),
-                this::increaseSixMatchPrize
-        );
-
-        prizeIncreaseActions.get(matchCount)
-                .run();
-    }
-
-    private void noMatch() {
-        // do nothing
-    }
-
-    private void oneMatch() {
-        // do nothing
-    }
-
-    private void twoMatch() {
-        // do nothing
-    }
-
-    private void increaseThreeMatchPrize() {
+    public void increaseThreeMatchPrize() {
         threeMatchCount++;
     }
 
-    private void increaseFourMatchPrize() {
+    public void increaseFourMatchPrize() {
         fourMatchCount++;
     }
 
-    private void increaseFiveMatchPrize(boolean isBonusMatch) {
-        if (isBonusMatch) {
-            fiveMatchWithBonusCount++;
-            return;
-        }
-
+    public void increaseFiveMatchPrize() {
         fiveMatchCount++;
     }
 
-    private void increaseSixMatchPrize() {
+    public void increaseFiveMatchWithBonusPrize() {
+        fiveMatchWithBonusCount++;
+    }
+
+    public void increaseSixMatchPrize() {
         sixMatchCount++;
     }
 
     private int calculatePrizeMoney() {
-       return (threeMatchCount * THREE_MATCH_PRIZE)
-                    + (fourMatchCount * FOUR_MATCH_PRIZE)
-                    + (fiveMatchCount * FIVE_MATCH_PRIZE)
-                    + (fiveMatchWithBonusCount * FIVE_MATCH_WITH_BONUS_PRIZE)
-                    + (sixMatchCount * SIX_MATCH_PRIZE);
+        return PrizeType.THREE_MATCH.getPrizeMoney() * threeMatchCount
+                + PrizeType.FOUR_MATCH.getPrizeMoney() * fourMatchCount
+                + PrizeType.FIVE_MATCH.getPrizeMoney() * fiveMatchCount
+                + PrizeType.FIVE_MATCH_WITH_BONUS.getPrizeMoney() * fiveMatchWithBonusCount
+                + PrizeType.SIX_MATCH.getPrizeMoney() * sixMatchCount;
     }
 }
---
+++ src/main/java/lotto/model/PrizeType.java
@@ -0,0 +1,46 @@
+package lotto.model;
+
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+
+public enum PrizeType {
+    NO_MATCH(0, 0, (prizeEvaluator, isBonusMatch) -> {}),
+    ONE_MATCH(1, 0, (prizeEvaluator, isBonusMatch) -> {}),
+    TWO_MATCH(2, 0, (prizeEvaluator, isBonusMatch) -> {}),
+    THREE_MATCH(3, 5_000, (prizeEvaluator, isBonusMatch) -> prizeEvaluator.increaseThreeMatchPrize()),
+    FOUR_MATCH(4, 50_000, (prizeEvaluator, isBonusMatch) -> prizeEvaluator.increaseFourMatchPrize()),
+    FIVE_MATCH(5, 1_500_000, (prizeEvaluator, isBonusMatch) -> prizeEvaluator.increaseFiveMatchPrize()),
+    FIVE_MATCH_WITH_BONUS(5, 30_000_000, (prizeEvaluator, isBonusMatch) -> prizeEvaluator.increaseFiveMatchWithBonusPrize()),
+    SIX_MATCH(6, 2_000_000_000, (prizeEvaluator, isBonusMatch) -> prizeEvaluator.increaseSixMatchPrize());
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final BiConsumer<PrizeEvaluator, Boolean> prizeIncreaseAction;
+
+    PrizeType(int matchCount, int prizeMoney, BiConsumer<PrizeEvaluator, Boolean> prizeIncreaseAction) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.prizeIncreaseAction = prizeIncreaseAction;
+    }
+
+    public void increasePrize(PrizeEvaluator prizeEvaluator, boolean isBonusMatch) {
+        if (prizeMoney > 0) {
+            prizeIncreaseAction.accept(prizeEvaluator, isBonusMatch);
+        }
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public static PrizeType valueOf(int matchCount, boolean isBonusMatch) {
+        if (matchCount == 5 && isBonusMatch) {
+            return FIVE_MATCH_WITH_BONUS;
+        }
+
+        return Arrays.stream(values())
+                .filter(type -> type.matchCount == matchCount)
+                .findFirst()
+                .orElse(NO_MATCH);
+    }
+}

9b060229d5f46b59769222831752514e09a092ce
junsoo <choix712@umn.edu>
2023-11-06 19:10:26 +0900

    feat: 발행한 로또 중복 체크 유효성 검사

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -8,12 +8,22 @@ public class LottoValidator{
 
     public static void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
 
     }
 
     private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또는 반드시 6개 숫자를 가져야 합니다.");
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers){
+        boolean duplicateExist = numbers.stream()
+                .count() != LOTTO_SIZE;
+
+        if (duplicateExist) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복 숫자가 존재합니다.");
         }
     }
 

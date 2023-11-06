7419d07363f169f92ea293f72b220893a64a5165
bandall <jsm5315@naver.com>
2023-11-06 16:02:19 +0900

    feat: WinningNumber 클래스 추가
    로또의 추첨번호(6개 번호+보너스 번호)를 저장하는 클래스 추가

---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final List<Integer> numbers;
+    private final Integer bonusNumber;
+
+    public WinningNumber(List<Integer> numbers, Integer bonusNumber) {
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
+}

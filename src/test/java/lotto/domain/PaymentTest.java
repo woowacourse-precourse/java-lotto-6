9cfa95df049be4cd70b1e0651ae7cd6ba418efb7
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 15:28:28 +0900

    test(PaymentTest) : 생성자에서 실행되는 검증 기능 테스트 작성
    천원으로 나누어떨어지는가?
    천원보다 작은 돈을 지불했나?
    십만원보다 큰 돈을 지불했는가?

---
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import static lotto.ErrorMessage.PAYMENT_LESS_THAN_1000;
+import static lotto.ErrorMessage.PAYMENT_MORE_THAN_100000;
+import static lotto.ErrorMessage.PAYMENT_NOT_DIVISIBLE_BY_1000;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PaymentTest {
+
+    @Test
+    @DisplayName("천원보다 적은 돈을 지불하면 예외가 발생한다")
+    void createPaymentByLessThan1000() {
+        assertThatThrownBy(() -> new Payment(500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PAYMENT_LESS_THAN_1000.get());
+    }
+
+    @Test
+    @DisplayName("십만원보다 큰 돈을 지불하면 예외가 발생한다")
+    void createPaymentByMoreThan100000() {
+        assertThatThrownBy(() -> new Payment(1500000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PAYMENT_MORE_THAN_100000.get());
+    }
+
+    @Test
+    @DisplayName("1000으로 안나누어 떨어지면 예외가 발생한다")
+    void createPaymentByCanNotDivision() {
+        assertThatThrownBy(() -> new Payment(34300))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PAYMENT_NOT_DIVISIBLE_BY_1000.get());
+    }
+}

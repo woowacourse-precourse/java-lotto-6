b044ef75df52b758600839c408293dbe2dba9816
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-06 20:32:42 +0900

    feature(lottogame): 사용자 로또 번호 생성 부분 코드 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -2,13 +2,17 @@ package lotto.controller;
 
 import lotto.domain.Bonus;
 import lotto.domain.Lotto;
+import lotto.domain.LottoService;
 import lotto.domain.Money;
 import lotto.view.InputView;
+
+import java.util.List;
 
 public class LottoGame {
     private Money money;
     private Lotto lotto; // 당첨 번호
     private Bonus bonus; // 보너스 번호
+    private List<Lotto> playerLotto;
 
     public void run(){
         start();
@@ -17,6 +21,7 @@ public class LottoGame {
 
     private void start(){
         money = InputView.LoopInputMoney();
+        playerLotto = LottoService.generatePlayerLotto(money);
         // 사용자 로또 출력
         lotto = InputView.LoopInputSixNumbers();
         bonus = InputView.LoopInputBonusNumber(lotto);

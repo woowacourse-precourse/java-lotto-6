a481d07d9b34b54d06c603873173f59a8c7920b1
pgd <rudeh1253@gmail.com>
2023-11-06 19:36:55 +0900

    feat: 입력 값 검증을 위한 Validator 클래스

---
+++ src/main/java/lotto/util/Validator.java
@@ -0,0 +1,105 @@
+package lotto.util;
+
+import lotto.configure.LottoConfiguration;
+import lotto.configure.ErrorMessages;
+
+import java.util.List;
+
+/**
+ * 값을 검증하는 클래스.
+ * 값이 올바르지 않을 경우 예외를 발생시킨다.
+ */
+public class Validator {
+
+    /**
+     * 주어진 문자열이 정수를 나타내는지 검증한다.
+     * " 123"과 같이 공백을 포함하는 경우에도 예외를 발생시킨다.
+     * @param toValidate 정수인지 검증할 문자열
+     * @throws IllegalArgumentException toValidate가 정수를 나타내는 문자열이 아닐 경우 발생한다.
+     */
+    public static void checkIfInteger(String toValidate) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(toValidate);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessages.NOT_AN_INTEGER.get());
+        }
+    }
+
+    /**
+     * 주어진 숫자가 0 이상의 숫자인지 검증한다.
+     * numToValidate >= 0인 경우 예외를 발생시킨다.
+     * @param numToValidate 검증할 정수.
+     * @throws IllegalArgumentException numToValidate >= 0인 경우 발생한다.
+     */
+    public static void checkIfNonNegative(int numToValidate) throws IllegalArgumentException {
+        if (numToValidate < 0) {
+            throw new IllegalArgumentException(ErrorMessages.PURCHASE_NUMBER_IS_NEGATIVE.get());
+        }
+    }
+
+    /**
+     * 주어진 숫자가 1000의 배수인지 검증한다.
+     * 다시 말해, numToValidate % 1000 == 0인지 검증한다.
+     * @param numToValidate 검증할 정수.
+     * @throws IllegalArgumentException numToValidate가 1000의 배수가 아닐 경우 발생한다.
+     */
+    public static void checkIfIsMultipleOfThousand(int numToValidate) throws IllegalArgumentException {
+        if (!isMultipleOfThousand(numToValidate)) {
+            throw new IllegalArgumentException(ErrorMessages.PURCHASE_NUMBER_IS_NOT_MULTIPLE_OF_1000.get());
+        }
+    }
+
+    private static boolean isMultipleOfThousand(int num) {
+        return num % 1000 == 0;
+    }
+
+    /**
+     * 당첨 번호가 총 6개인지 검증한다.
+     * winningNumbers의 사이즈가 정확히 6이 아닐 경우 예외가 발생한다.
+     * @param winningNumbers 당첨 번호가 담긴 리스트.
+     * @throws IllegalArgumentException winningNumbers.size() != 6일 경우 발생한다.
+     */
+    public static void checkIfWinningNumbersAreSix(List<Integer> winningNumbers) throws IllegalArgumentException {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessages.WINNING_NUMBERS_ARE_NOT_SIX.get());
+        }
+    }
+
+    /**
+     * 개별 당첨 번호가 적절한 범위에 포함되는지 검증한다.
+     * 당첨 번호의 최댓값과 최솟값은 각각 lotto.configure.LottoConfigure 클래스에 정의된 두 상수,
+     * LOTTO_NUMBER_MAX, LOTTO_NUMBER_MIN이다.
+     * @param winningNumbers 당첨 번호가 담긴 리스트.
+     * @throws IllegalArgumentException 단 하나의 번호라도 로또 번호 범위에 포함되지 않을 경우 발생한다.
+     */
+    public static void checkRangeOfEachWinningNumber(List<Integer> winningNumbers) throws IllegalArgumentException {
+        winningNumbers.forEach((n) ->
+                checkIfRangeOfNumberIsProper(n, ErrorMessages.WINNING_NUMBERS_NOT_IN_PROPER_RANGE.get()));
+    }
+
+    /**
+     * 보너스 번호가 적절한 범위에 포함되는지 검증한다.
+     * 보너스 번호의 최댓값과 최솟값은 각각 lotto.configure.LottoConfigure 클래스에 정의된 두 상수,
+     * LOTTO_NUMBER_MAX, LOTTO_NUMBER_MIN이다.
+     * @param bonusNumber 검증할 보너스 번호.
+     * @throws IllegalArgumentException 보너스 번호가 로또 번호 범위에 포함되지 않을 경우 발생한다.
+     */
+    public static void checkRangeOfBonusNumber(Integer bonusNumber) throws IllegalArgumentException {
+        checkIfRangeOfNumberIsProper(bonusNumber, ErrorMessages.BONUS_NUMBER_NOT_IN_PROPER_RANGE.get());
+    }
+
+    private static void checkIfRangeOfNumberIsProper(Integer winningNumber, String errorMessage)
+            throws IllegalArgumentException {
+        if (isNumberOverRange(winningNumber) || isNumberBelowRange(winningNumber)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private static boolean isNumberOverRange(Integer winningNumber) {
+        return winningNumber > LottoConfiguration.LOTTO_NUMBER_MAX;
+    }
+
+    private static boolean isNumberBelowRange(Integer winningNumber) {
+        return winningNumber < LottoConfiguration.LOTTO_NUMBER_MIN;
+    }
+}
---
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -0,0 +1,223 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import lotto.configure.ErrorMessages;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ValidatorTest {
+
+    @Test
+    void testCheckIfInteger_includeNonNumber() {
+        String[] testCases = {
+                "13 ",
+                " 256",
+                "78!",
+                "a95",
+                "9!3",
+                " 10 2"
+        };
+
+        for (String testCase : testCases) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkIfInteger(testCase))
+                    .withMessageContaining(ErrorMessages.NOT_AN_INTEGER.get());
+        }
+    }
+
+    @Test
+    void testCheckIfInteger_tryRealNumber() {
+        String[] testCases = {
+                "3.14",
+                "56.25",
+                "13.99"
+        };
+
+        for (String testCase : testCases) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkIfInteger(testCase))
+                    .withMessageContaining(ErrorMessages.NOT_AN_INTEGER.get());
+        }
+    }
+
+    @Test
+    void testCheckIfInteger_noExceptionCase_onlyIntegers() {
+        String[] testCases ={
+                "15",
+                "6",
+                "0",
+                "1",
+                "-13",
+                "-1",
+                "146135812",
+                "-156165824"
+        };
+
+        for (String testCase : testCases) {
+            assertThatNoException().isThrownBy(() -> Validator.checkIfInteger(testCase));
+        }
+    }
+
+    @Test
+    void testCheckIfNonNegative_includeNegativeIntegers() {
+        int[] testCases = {
+                -15,
+                -1,
+                -142658124
+        };
+
+        for (int testCase : testCases) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkIfNonNegative(testCase))
+                    .withMessageContaining(ErrorMessages.PURCHASE_NUMBER_IS_NEGATIVE.get());
+        }
+    }
+
+    @Test
+    void testCheckIfNonNegative_onlyNonNegativeIntegers_noException() {
+        int[] testCases = {
+                0,
+                1,
+                13515481
+        };
+
+        for (int testCase : testCases) {
+            assertThatNoException().isThrownBy(() -> Validator.checkIfNonNegative(testCase));
+        }
+    }
+
+    @Test
+    void testCheckIfMultipleOfThousand_notMultipleOfThousand() {
+        int[] testCases = {
+                -153, 56, 901,
+                1001, 1850137414
+        };
+
+        for (int testCase : testCases) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkIfIsMultipleOfThousand(testCase))
+                    .withMessageContaining(ErrorMessages.PURCHASE_NUMBER_IS_NOT_MULTIPLE_OF_1000.get());
+        }
+    }
+
+    @Test
+    void testCheckIfMultipleOfThousand_multipleOfThousand_noException() {
+        int[] testCases = {
+                -1000, 0, 1000, 1000000
+        };
+
+        for (int testCase : testCases) {
+            assertThatNoException().isThrownBy(() -> Validator.checkIfIsMultipleOfThousand(testCase));
+        }
+    }
+
+    @Test
+    void testCheckIfWinningNumbersAreSix_notSix() {
+        List<List<Integer>> testCases = Arrays.asList(
+                new ArrayList<>(),
+                Arrays.asList(1, 15),
+                Arrays.asList(3, 6, 9, 12, 15, 18, 24)
+        );
+
+        testCases.forEach((l) -> {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkIfWinningNumbersAreSix(l))
+                    .withMessageContaining(ErrorMessages.WINNING_NUMBERS_ARE_NOT_SIX.get());
+        });
+    }
+
+    @Test
+    void testCheckIfWinningNumbersAreSix_noException() {
+        List<Integer> testCase = Arrays.asList(3, 6, 9, 12, 15, 18);
+        assertThatNoException()
+                .isThrownBy(() -> Validator.checkIfWinningNumbersAreSix(testCase));
+    }
+
+    @Test
+    void testCheckRangeOfEachWinningNumber_oneViolation() {
+        List<List<Integer>> testCases = Arrays.asList(
+                Arrays.asList(0, 13, 25, 26, 38, 40),
+                Arrays.asList(1, 0, 25, 26, 27, 28),
+                Arrays.asList(3, 6, -25, 37, 40, 44),
+                Arrays.asList(25, 27, 29, 30, 31, 478)
+        );
+
+        testCases.forEach((l) -> {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkRangeOfEachWinningNumber(l))
+                    .withMessageContaining(ErrorMessages.WINNING_NUMBERS_NOT_IN_PROPER_RANGE.get());
+        });
+    }
+
+    @Test
+    void testCheckRangeOfEachWinningNumber_twoViolation() {
+        List<List<Integer>> testCases = Arrays.asList(
+                Arrays.asList(0, -1, 25, 26, 38, 40),
+                Arrays.asList(1, 0, 47, 26, 27, 28),
+                Arrays.asList(3, 6, -25, 0, 40, 44),
+                Arrays.asList(25, 27, 29, 30, 59, 478)
+        );
+
+        testCases.forEach((l) -> {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkRangeOfEachWinningNumber(l))
+                    .withMessageContaining(ErrorMessages.WINNING_NUMBERS_NOT_IN_PROPER_RANGE.get());
+        });
+    }
+
+    @Test
+    void testCheckRangeOfEachWinningNumber_sixViolation() {
+        List<List<Integer>> testCases = Arrays.asList(
+                Arrays.asList(-256, -25, 0, 48, 49, 70)
+        );
+
+        testCases.forEach((l) -> {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkRangeOfEachWinningNumber(l))
+                    .withMessageContaining(ErrorMessages.WINNING_NUMBERS_NOT_IN_PROPER_RANGE.get());
+        });
+    }
+
+    @Test
+    void testCheckRangeOfEachWinningNumber_noViolation_noException() {
+        List<List<Integer>> testCases = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5, 6),
+                Arrays.asList(1, 3, 5, 7, 9, 11),
+                Arrays.asList(1, 2, 3, 4, 5, 45)
+        );
+
+        testCases.forEach((l) -> {
+            assertThatNoException()
+                    .isThrownBy(() -> Validator.checkRangeOfEachWinningNumber(l));
+        });
+    }
+
+    @Test
+    void testCheckRangeOfBonusNumber_violate() {
+        Integer[] testCases = {
+                -125142, -1, 0, 46, 4774, 1412425
+        };
+
+        Arrays.asList(testCases).forEach(e -> {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> Validator.checkRangeOfBonusNumber(e))
+                    .withMessageContaining(ErrorMessages.BONUS_NUMBER_NOT_IN_PROPER_RANGE.get());
+        });
+    }
+
+    @Test
+    void testCheckRangeOfBonusNumber_noException() {
+        Integer[] testCases = {
+                1, 2, 33, 44, 45
+        };
+
+        Arrays.asList(testCases).forEach(e -> {
+            assertThatNoException().isThrownBy(() -> Validator.checkRangeOfBonusNumber(e));
+        });
+    }
+}

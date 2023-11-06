6d03c1f3814a06266b6a10666c36cd5ce3f83aa7
HYOAN <aspqll@naver.com>
2023-11-07 01:16:10 +0900

    feat(lotto): 구입금액과 당첨번호에 대한 예외처리 테스트코드 구현

--- src/test/java/lotto/domain/UserInputTest.java
+++ src/test/java/lotto/domain/UserInputTest.java
@@ -7,10 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputTest {
     private UserInput userInput;
+    private UserInputChecker userInputChecker;
 
     @BeforeEach
-     void setUp() {
+    void setUp() {
         userInput = new UserInput();
+        userInputChecker = new UserInputChecker();
     }
 
+    @Test
+    void 구입금액이_1000원_단위가_아닌_상황_예외처리() {
+        assertEquals(0, userInput.getUserPurchaseAmount(userInputChecker, "10010"));
+    }
+
+    @Test
+    void 숫자가_아닌_구입금액_예외처리() {
+        assertEquals(0, userInput.getUserPurchaseAmount(userInputChecker, "abc"));
+    }
+
+    @Test
+    void 중복된_당첨번호_예외처리() {
+        assertEquals(null, userInput.getUserWinningNumbers(userInputChecker, "1,2,3,4,5,5"));
+    }
+
+    @Test
+    void 숫자가_아닌_당첨번호_입력_예외처리() {
+        assertEquals(null, userInput.getUserWinningNumbers(userInputChecker, "1,2,3,4,5,a"));
+    }
+
+    @Test
+    void 잘못된_범위의_당첨번호_입력_예외처리() {
+        assertEquals(null, userInput.getUserWinningNumbers(userInputChecker, "1,2,3,4,5,46"));
+    }
 }

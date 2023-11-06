c9a0f821d11b8405fa010be52343b6fd9dc0aa72
HYOAN <aspqll@naver.com>
2023-11-07 01:14:49 +0900

    feat(lotto): 당첨번호 입력기능 구현, 구입금액과 당첨번호 예외처리 로직 구현

--- src/main/java/lotto/domain/UserInput.java
+++ src/main/java/lotto/domain/UserInput.java
@@ -2,6 +2,9 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInput {
     public UserInput() {
     }
@@ -14,12 +17,19 @@ public class UserInput {
 
     public int getUserPurchaseAmount(UserInputChecker checker, String inputPurchaseAmount) { // 구입금액
         int purchaseAmount = checker.checkPurchaseAmount(inputPurchaseAmount);
+        if (purchaseAmount == 0)
+            return 0;
         return purchaseAmount;
     }
 
-    public int getUserWinningNumbers(String inputWinningNumbers) { // 당첨번호
+    public List<Integer> getUserWinningNumbers(UserInputChecker checker, String inputWinningNumbers) { // 당첨번호
+        String[] winnigNumbers = inputWinningNumbers.split(",");
+        List<Integer> checkedNumbers = new ArrayList<>();
+        checkedNumbers =  checker.checkWinningNumbers(winnigNumbers);
+        if (checkedNumbers == null)
+            return null;
 
-        return 0;
+        return checkedNumbers;
     }
 
     public int getUserBonusNumber() { // 보너스번호

7fbecc5453bf10058a367ec2a8487cdc9014d30c
jae woo Jung <awfjol2008@naver.com>
2023-11-06 20:30:14 +0900

    feat: 사용자 보너스 번호 타입 변경

--- src/main/java/lotto/domain/player/PlayerLotto.java
+++ src/main/java/lotto/domain/player/PlayerLotto.java
@@ -1,15 +1,16 @@
 package lotto.domain.player;
 
-import lotto.domain.player.lotto.Lotto;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
 
 public class PlayerLotto {
 
     private Lotto lotto;
-    private BonusNumber bonusNumber;
+    private LottoNumber bonusNumber;
 
     private PurchaseAmount purchaseAmount;
 
-    public PlayerLotto(Lotto lotto, BonusNumber bonusNumber, PurchaseAmount purchaseAmount) {
+    public PlayerLotto(Lotto lotto, LottoNumber bonusNumber, PurchaseAmount purchaseAmount) {
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
         this.purchaseAmount = purchaseAmount;

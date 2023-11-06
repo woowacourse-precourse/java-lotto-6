512478ceeb2202515e032dc780950a95dcfbcf42
ddohyeong <dohyeong7026@gmail.com>
2023-11-06 16:11:16 +0900

    refactor(PurchaseAmountTest) : 메서드명 기능에 맞게 리팩토링

--- src/test/java/domain/PurchaseAmountTest.java
+++ src/test/java/domain/PurchaseAmountTest.java
@@ -10,8 +10,8 @@ class PurchaseAmountTest {
 
 	@DisplayName("로또 구매 금액이 정수형이 아니라면 예외 발생")
 	@ParameterizedTest
-	@ValueSource(strings = {"test","!@#","12@12"})
-	public void test(String input) {
+	@ValueSource(strings = {"test", "!@#", "12@12"," ",""})
+	public void LottoPurchaseAmountWithNonIntegerValue(String input) {
 		// when & then
 		assertThatThrownBy(() -> new PurchaseAmount(input))
 				.isInstanceOf(IllegalArgumentException.class);

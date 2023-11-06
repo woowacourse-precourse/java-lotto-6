2e6f63023c1084022a1a1a83164767c6aaf420d0
donghoonyeom <dua5410773@naver.com>
2023-11-07 01:20:13 +0900

    feat(utils): StringUtil 클래스 추가
    입력된 문자열을 정수 리스트로 변환하는 기능을 제공하는 StringUtil 클래스를 추가했습니다. `convertToIntegerList(String input, String delimiter)` 함수는 지정된 구분자(delimiter)를 기반으로 입력된 문자열(input)을 파싱하여 정수 리스트로 반환합니다.

---
+++ src/main/java/lotto/StringUtil.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringUtil {
+    public static List<Integer> convertToIntegerList(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}

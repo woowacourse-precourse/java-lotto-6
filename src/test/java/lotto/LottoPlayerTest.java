37d5ebeb3a1f5cfb053bd53f23471877c93182b2
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-06 16:49:47 +0900

    feat : 플레이어 생성시 금액이 0원인 경우에 대한 테스트 코드 추가

--- src/test/java/lotto/LottoPlayerTest.java
+++ src/test/java/lotto/LottoPlayerTest.java
@@ -1,5 +1,4 @@
 package lotto;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import lotto.Model.LottoPlayer;
 import org.assertj.core.api.Assertions;
@@ -8,6 +7,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoPlayerTest {
+
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,-1, -100, -9802})
+    public void 금액이_0원_이하인_경우_예외처리(Integer cache) {
+        Assertions.assertThatThrownBy(() -> new LottoPlayer(cache))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력 금액은 0원을 초과해야 합니다.");
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {3245, 100, 98200,37002})

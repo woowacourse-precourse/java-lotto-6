58ce6ff074f588a61f6041071f0f795b5695cfc2
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-06 20:50:12 +0900

    refactor(Validator): 중복되는 예외처리 메소드 삭제

--- src/main/java/lotto/util/validator/Validator.java
+++ src/main/java/lotto/util/validator/Validator.java
@@ -1,11 +1,10 @@
 package lotto.util.validator;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class Validator {
 
-	public static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
+	public static final Pattern NUMBER_PATTERN = Pattern.compile("^-?\\d+$");
 
 	private Validator() {
 		throw new AssertionError();
@@ -23,47 +22,7 @@ public class Validator {
 		}
 	}
 
-	public static void validateSeparatedTextLength(String[] separatedText, int length, String message) {
-		if (separatedText.length != length) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
 	private static boolean isEmpty(String str) {
 		return str == null || str.isBlank();
-	}
-
-	public static void validateNumbersSize(List<Integer> numbers, int size, String message) {
-		if (numbers.size() != size) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	public static void validateDuplicateNumber(List<Integer> numbers, String message) {
-		if (numbers.stream().distinct().count() != numbers.size()) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	public static void validateNumbersOutOfRange(List<Integer> numbers, int[] ranges, String message) {
-		if (numbers.stream().anyMatch(number -> isNumberOutOfRange(number, ranges))) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	public static void validateNumberOutOfRange(int number, int[] ranges, String message) {
-		if (isNumberOutOfRange(number, ranges)) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	public static void validateNumberInNumbers(List<Integer> numbers, int number, String message) {
-		if (numbers.contains(number)) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	private static boolean isNumberOutOfRange(int number, int[] ranges) {
-		return number > Math.max(ranges[0], ranges[1]) || number < Math.min(ranges[0], ranges[1]);
 	}
 }

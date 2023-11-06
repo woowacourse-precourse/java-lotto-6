3a841b71447587372228e3422c095b7a848607c2
Soonrang <clearsbyul30@naver.com>
2023-11-06 21:27:08 +0900

    feat: 로또 구입 개수 구하기

---
+++ src/main/java/lotto/model/Money.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+public class Money {
+
+    private final int money;
+    private final static int TICKET_PRICE = 1000;
+    private final static int MIN_PRICE = 1000;
+
+    public Money(int money) {
+        this.money = money;
+    }
+
+    public void validate(int money){
+        validateRange(money);
+        validateUnit(money);
+    }
+
+    public void validateRange(int money){
+        if(!(money >MIN_PRICE)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateUnit(int money){
+        if(!(money % 1000 == 0)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getTicket() {
+        return money / TICKET_PRICE;
+    }
+
+}

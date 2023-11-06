664c2e7c2d1a9b8cc12b0e2ab14650515285ce80
jinsu868 <mjinsu098@gmail.com>
2023-11-06 12:44:47 +0900

    fix: Application에서 LottoController에 LottoService 의존관계 주입하게 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.service.LottoService;
 import lotto.validator.InputValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -8,7 +9,7 @@ import lotto.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController(
-                new InputView(new InputValidator()), new OutputView());
+                new InputView(new InputValidator()), new OutputView(), new LottoService());
         lottoController.run();
     }
 }

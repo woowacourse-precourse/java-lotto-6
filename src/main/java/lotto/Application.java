f178c4d495a5a06cfa5e8aa4236abdce7574078a
KIVSH <kihongkim3@hotmail.com>
2023-11-06 11:50:50 +0900

    refactor: change structure Application class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,27 +4,57 @@ import java.util.List;
 import java.util.Map;
 
 public class Application {
+    public static int purchaseAmount;
+    public static int bonusNumber;
+    public static List<Lotto> lottos;
+    public static List<Integer> winNumbers;
+    public static Map<Rank, Integer> lottoResult;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        runPurchaseAmountPart();
+
+        runCalculateIssuePart();
+
+        runWinNumbersPart();
+
+        runBonusNumberPart();
+
+        runLottoResultPart();
+
+        runReturnRatePart();
+    }
+
+    public static void runPurchaseAmountPart() {
         final String purchaseAmountMessage = "구입금액을 입력해 주세요.";
         Output.message(purchaseAmountMessage);
+        purchaseAmount = Input.purchaseAmount();
+    }
 
-        int purchaseAmount = Input.purchaseAmount();
+    public static void runCalculateIssuePart() {
         int issueCount = Lotto.calculateIssueCount(purchaseAmount);
-        List<Lotto> lottos = Lotto.issueLotto(issueCount);
+        lottos = Lotto.issueLotto(issueCount);
         Output.issueLotto(issueCount, lottos);
+    }
 
+    public static void runWinNumbersPart() {
         final String winNumbersMessage = "\n당첨 번호를 입력해 주세요.";
         Output.message(winNumbersMessage);
-        List<Integer> winNumbers = Input.winNumbers();
+        winNumbers = Input.winNumbers();
+    }
 
+    public static void runBonusNumberPart() {
         final String bonusNumberMessage = "\n보너스 번호를 입력해 주세요.";
         Output.message(bonusNumberMessage);
-        int bonusNumber = Input.bonusNumber();
+        bonusNumber = Input.bonusNumber();
+    }
 
-        Map<Rank, Integer> lottoResult = Lotto.isWin(lottos, winNumbers, bonusNumber);
+    public static void runLottoResultPart() {
+        lottoResult = Lotto.isWin(lottos, winNumbers, bonusNumber);
         Output.winStatistics(lottoResult);
+    }
 
+    public static void runReturnRatePart() {
         double returnRate = Lotto.calculateReturnRate(lottoResult, purchaseAmount);
         Output.returnRate(returnRate);
     }

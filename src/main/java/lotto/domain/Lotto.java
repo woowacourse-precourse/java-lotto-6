9fe3a5df46f2a3ba2cd409d4aafc692d26a92cf2
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 04:15:36 +0900

    feat(domain.Lotto): 기능4. 로또를 발행하기 위해 입력된 각 숫자가 1 ~ 45 범위에 있는지 체크한다
    반복문을 추가하여 생성자의 매개변수로 입력된 리스트의 각 숫자가 1 ~ 45 범위에 있는지 체크하는 코드를 추가하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import static lotto.domain.ErrorMessage.*;
+import static lotto.utils.CalculationUtils.isNumberInRange;
+
 import java.util.List;
 
 public class Lotto {
@@ -12,7 +15,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_COMBINATION_HAVE_SIX_NUMBER.getErrorMessage());
+        }
+
+        for (Integer number : numbers) {
+            if (!isNumberInRange(number, 1, 45)) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_IS_BETWEEN_ONE_AND_FORTYFIVE.getErrorMessage());
+            }
         }
     }
 }

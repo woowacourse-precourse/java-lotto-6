12be52af8734ee7be616fdfd1f6021d8d1f940ae
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 08:28:10 +0900

    test(LottoTest) : 로또 도메인 테스트 코드 작성 - createLottoByOverSize() - createLottoByDuplicateNumber() - isRightRange() - isAscendingOrder()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,10 +22,24 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 숫자가 1 미만 45 초과 시 예외처리")
+    void isRightRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5))).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 숫자가 오름차순이 아니면 예외처리")
+    void isAscendingOrder(){
+        assertThatThrownBy(() -> new Lotto(List.of(1,3,4,2,5,6))).isInstanceOf(
+                IllegalStateException.class);
+    }
+
+
 }

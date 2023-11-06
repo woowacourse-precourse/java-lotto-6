7eb6abe7398f83b0eb536e59edc7e93393e0a841
rbgusdlza <rbgusdlza@gmail.com>
2023-11-07 04:02:14 +0900

    feat(LottoBonus): isNumberRangeValid 메서드 구현

--- src/main/java/lotto/domain/LottoBonus.java
+++ src/main/java/lotto/domain/LottoBonus.java
@@ -1,14 +1,21 @@
 package lotto.domain;
 
+import static lotto.exception.ErrorMessage.NUMBER_OUT_OF_RANGE_ERROR;
+
 public class LottoBonus {
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_END_NUMBER = 45;
     private final int number;
 
     public LottoBonus(int number) {
-        validate(number);
+        isNumberRangeValid(number);
         this.number = number;
     }
 
+    private void isNumberRangeValid(int number) {
+        if (!(number >= LOTTO_START_NUMBER && number <= LOTTO_END_NUMBER)) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE_ERROR);
+        }
+    }
     
 }

51e08cc4e09e043c82784cb71f42e371c5887ec1
yesjjin99 <yesjjin99@naver.com>
2023-11-06 17:51:51 +0900

    test: 로또 생성 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -31,5 +32,18 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(0, 46, 3, 4, 5, 6)))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(ErrorMessage.INVALID_NUMBER_RANGE);
+    }
+
+    @Test
+    @DisplayName("로또 번호 생성 성공")
+    void createLotto() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = new Lotto(numbers);
+
+        // then
+        assertThat(lotto).isNotNull();
     }
 }

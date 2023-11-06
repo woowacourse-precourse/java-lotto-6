e0797485296656945a0b6a14fb592a23ad071da8
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-07 04:28:25 +0900

    feat: 로또 결과 가져오고 출력하는 기능 추가
    로또 결과를 lottoResult에 저장하고
    lottoResult를 OutputView에 전달해 결과 출력
    getLottoTickets 메서드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import lotto.domain.Lotto;
 import lotto.util.MakeLotto;
+import lotto.util.LottoResult;
 import lotto.view.OutputView;
 
 import java.util.List;
@@ -10,17 +11,27 @@ public class LottoController {
 
     private InputController inputController = new InputController();
     private MakeLotto makeLotto = new MakeLotto();
+    private final int money;
+    private final int lottoTickets;
+    private final int bonusNum;
 
     public LottoController(){
-        int lottoTickets = getLottoTickets();
+        money = getMoney();
+        lottoTickets = getLottoTickets(money);
         List<Lotto> lottoList = makeLotto.makeLottoList(lottoTickets);
         List<Integer> winningNum = getWinningNum();
-        int bonusNum = getBounsNum();
+        bonusNum = getBounsNum();
+        LottoResult lottoResult = new LottoResult(lottoList, winningNum, bonusNum);
+        printResult(lottoResult);
     }
 
-    public int getLottoTickets(){
+    public int getMoney(){
         OutputView.inputBuyMessage();
         return inputController.inputMoney();
+    }
+
+    public int getLottoTickets(int money){
+        return money/1000;
     }
 
     public List<Integer> getWinningNum(){
@@ -33,5 +44,12 @@ public class LottoController {
         OutputView.printBlank();
         OutputView.inputBonusNumMessage();
         return inputController.inputBonusNum();
+    }
+
+    public void printResult(LottoResult lottoResult){
+        OutputView.printBlank();
+        OutputView.printResultBar();
+        OutputView.printResult(lottoResult.getLottoResult());
+        OutputView.printTotalRate(money, lottoResult.getTotalRate());
     }
 }

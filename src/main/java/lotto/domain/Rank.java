b6c6d9c3f53004b209a1a52aa818023c0064482f
woong2e <tlswodnd12345@naver.com>
2023-11-06 15:54:13 +0900

    feat: 로또등수 enum 생성

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    LOSING_TICKET(0, 0);
+
+    int countOfMatch;
+    int winningMoney;
+    Rank(int countOfMatch, int winningMoney) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+
+    public static Rank valueOf(int countOfMatch, boolean hasBonus) {
+        if (countOfMatch < 3) {
+            return LOSING_TICKET;
+        }
+
+        if (SECOND.matchCorrectingCount(countOfMatch) && hasBonus) {
+            return SECOND;
+
+        }
+
+        for (Rank rank : Rank.values()) {
+            if (rank.matchCorrectingCount(countOfMatch) && rank != SECOND) {
+                return rank;
+            }
+        }
+
+        throw new IllegalArgumentException(countOfMatch + "는 유효하지 않은 값입니다.");
+    }
+
+    private boolean matchCorrectingCount(int countOfMatch) {
+        return this.countOfMatch == countOfMatch;
+    }
+}

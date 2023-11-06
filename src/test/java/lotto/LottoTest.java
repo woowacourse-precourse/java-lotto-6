68fb22f2f0dd0038a9649bbc5974386365da35a5
hayannn <dlgkdis801@naver.com>
2023-11-07 00:01:50 +0900

    test(createLottoByCorrectRange): 로또 번호의 범위 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import java.util.stream.Collectors;
 import lotto.Domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +27,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45 범위에 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoByCorrectRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 67)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

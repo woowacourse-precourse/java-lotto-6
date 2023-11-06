6fadad538dd48ec0175ab45b86d789f6ff2d7451
cmong0516 <cmong0516@gmail.com>
2023-11-06 15:40:04 +0900

    feat(InputView): userInput() 입력값을 반환한다.

---
+++ src/main/java/lotto/io/InputView.java
@@ -0,0 +1,13 @@
+package lotto.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String userInput() {
+        return Console.readLine();
+    }
+}

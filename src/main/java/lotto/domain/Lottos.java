cb9c1d96919cafdabd65b2c4193ff19b850d967d
ParkDH0809 <znxn1237@gmail.com>
2023-11-07 01:13:10 +0900

    style: 메서드 이름 및 위치 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.constants.ErrorMessage;
 import lotto.constants.LottoRule;
+import lotto.domain.winningNumber.FinalWinningNumber;
 import lotto.service.LottoService;
 
 import java.util.*;
@@ -12,12 +13,12 @@ public class Lottos {
     HashMap<LottoRank, Integer> rankCount;
 
     public Lottos(String purchaseAmount) {
-        numberOfLottos = validatePurchaseAmount(purchaseAmount);
+        numberOfLottos = validate(purchaseAmount);
         initEachLotto();
         initRankCount();
     }
 
-    private int validatePurchaseAmount(String purchaseAmount) {
+    private int validate(String purchaseAmount) {
         validateNumber(purchaseAmount);
         return validateDivisibilityByLottoPrice(purchaseAmount);
     }
@@ -26,14 +27,14 @@ public class Lottos {
         try {
             Integer.parseInt(purchaseAmount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_IS_NOT_NUMBER.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.ERROR_INVALID_NUMBER.getMessage());
         }
     }
 
     private int validateDivisibilityByLottoPrice(String purchaseAmount) {
         int purchaseResult = Integer.parseInt(purchaseAmount);
         if(purchaseResult % LottoRule.LOTTO_PRICE.getNumber() != 0) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_CANT_DIVIDED_BY_LOTTO_PRICE.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.ERROR_INVALID_AMOUNT.getMessage());
         }
         return purchaseResult / LottoRule.LOTTO_PRICE.getNumber();
     }
@@ -41,6 +42,13 @@ public class Lottos {
     private void initEachLotto() {
         for(int i = 0; i < numberOfLottos; i++) {
             lottos.add(new Lotto(LottoService.generateRandomLottoNumber()));
+        }
+    }
+
+    private void initRankCount() {
+        rankCount = new LinkedHashMap<>();
+        for(LottoRank lottoRank : LottoRank.values()) {
+            rankCount.put(lottoRank, 0);
         }
     }
 
@@ -57,20 +65,12 @@ public class Lottos {
     }
 
     public HashMap<LottoRank, Integer> getLottosResult(FinalWinningNumber finalWinningNumber) {
-//        HashMap<LottoRank, Integer> rankCount = initRankCount();
         for (Lotto lotto : lottos) {
-            LottoRank lottoRank = lotto.compareLottoNumberWithFianlWinningNumber(finalWinningNumber);
+            LottoRank lottoRank = lotto.compareLottoNumberWithFinalWinningNumber(finalWinningNumber);
             rankCount.put(lottoRank, rankCount.get(lottoRank) + 1);
         }
         rankCount.remove(LottoRank.OUT_OF_RANK);
         return rankCount;
-    }
-
-    private void initRankCount() {
-        rankCount = new LinkedHashMap<>();
-        for(LottoRank lottoRank : LottoRank.values()) {
-            rankCount.put(lottoRank, 0);
-        }
     }
 
     public double calculateTotalRateOfReturn() {

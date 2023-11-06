c06e1fdcbb273763f13f5d67a23ed8151bddaa4f
dukcode <duk9741@gmail.com>
2023-11-06 16:11:32 +0900

    feat: 로또 당첨 enum 클래스 미당첨 추가

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -5,7 +5,8 @@ public enum LottoPrize {
     SECOND_PRIZE(30_000_000L, 5, 1),
     THIRD_PRIZE(1_500_000L, 5, 0),
     FOURTH_PRIZE(50_000L, 4, 0),
-    FIFTH_PRIZE(5_000L, 3, 0);
+    FIFTH_PRIZE(5_000L, 3, 0),
+    NONE(0L, 0, 0);
 
     private final long winningAmount;
     private final int numNumberMatches;

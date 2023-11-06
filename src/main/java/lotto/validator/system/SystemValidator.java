5f4bb70e283fa7fe8265ef42285c5f9094d9704a
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-06 17:04:34 +0900

    refactor: (SystemValidator) 부모 메소드 역할로 제한
    각 도메인별 validator는 SystemValidator를 상속받는다.

--- src/main/java/lotto/validator/system/SystemValidator.java
+++ src/main/java/lotto/validator/system/SystemValidator.java
@@ -1,9 +1,6 @@
 package lotto.validator.system;
 
 import lotto.domain.WinningLottoNumbers;
-import lotto.view.message.exception.BudgetInputErrorMessage;
-import lotto.view.message.exception.LottoGenerateErrorMessage;
-import lotto.view.message.exception.WinningCombinationInputErrorMessage;
 
 import java.util.HashSet;
 import java.util.List;
@@ -16,87 +13,34 @@ public class SystemValidator {
     private static final int LOTTO_SIZE =6;
     private static final int BUDGET_MINIMUM_UNIT = 1000;
 
-    public static void budgetValidator(int budget){
-        if(isNotDivisibleByThousand(budget)){
-            throw new IllegalArgumentException(BudgetInputErrorMessage.DIVISIBLE_BY_THOUSAND.getMessage());
-        }
-        if(isLessThanMinimumBudget(budget)){
-            throw new IllegalArgumentException(BudgetInputErrorMessage.LESS_THAN_1000.getMessage());
-        }
+    protected static boolean isNotDivisibleByThousand(int budget){
+        return budget % BUDGET_MINIMUM_UNIT != 0;
     }
 
-    private static boolean isNotDivisibleByThousand(int budget){
-        if(budget%BUDGET_MINIMUM_UNIT!=0){
-            return true;
-        }
-        return false;
+    protected static boolean isSizeWrong(List<Integer> numbers){
+        return numbers.size() != LOTTO_SIZE;
     }
 
-    public static void lottoValidator(List<Integer> numbers){
-        if(isSizeWrong(numbers)){
-            throw new IllegalArgumentException(LottoGenerateErrorMessage.INCORRECT_SIZE.getMessage());
-        }
-        if(isOutOfRangeLottoNumbers(numbers)){
-            throw new IllegalArgumentException(LottoGenerateErrorMessage.OUT_OF_RANGE_NUMBER.getMessage());
-        }
-        if(isDuplicate(numbers)){
-            throw new IllegalArgumentException(LottoGenerateErrorMessage.DUPLICATED_NUMBER.getMessage());
-        }
-    }
-
-    public static void winningNumbersValidator(List<Integer> numbers){
-        if(isSizeWrong(numbers)){
-            throw new IllegalArgumentException(WinningCombinationInputErrorMessage.INCORRECT_SIZE.getMessage());
-        }
-        if(isOutOfRangeLottoNumbers(numbers)){
-            throw new IllegalArgumentException(WinningCombinationInputErrorMessage.OUT_OF_RANGE_NUMBER.getMessage());
-        }
-        if(isDuplicate(numbers)){
-            throw new IllegalArgumentException(WinningCombinationInputErrorMessage.DUPLICATED_NUMBER.getMessage());
-        }
-    }
-    public static void winningCombinationValidator(WinningLottoNumbers winningLottoNumbers, int bonusNumber){
-        if(isOutOfRangeBonusNumber(bonusNumber)){
-            throw new IllegalArgumentException(WinningCombinationInputErrorMessage.OUT_OF_RANGE_NUMBER.getMessage());
-        }
-        if(isDuplicateBonusNumber(winningLottoNumbers,bonusNumber)){
-            throw new IllegalArgumentException(WinningCombinationInputErrorMessage.DUPLICATED_NUMBER.getMessage());
-        }
-    }
-
-    private static boolean isSizeWrong(List<Integer> numbers){
-        if(numbers.size()!=LOTTO_SIZE){
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isDuplicateBonusNumber(WinningLottoNumbers winningLottoNumbers,int bonusNumber){
+    protected static boolean isDuplicateBonusNumber(WinningLottoNumbers winningLottoNumbers,int bonusNumber){
         List<Integer> winningNumbers = winningLottoNumbers.getNumbers();
-        if(winningNumbers.contains(bonusNumber)){
-            return true;
-        }
-        return false;
+        return winningNumbers.contains(bonusNumber);
     }
 
-    private static boolean isOutOfRangeBonusNumber(int bonusNumber){
+    protected static boolean isOutOfRangeBonusNumber(int bonusNumber){
         return isOutOfRange(bonusNumber);
     }
 
-    private static boolean isLessThanMinimumBudget(int budget){
+    protected static boolean isLessThanMinimumBudget(int budget){
         return budget<MINIMUM_BUDGET;
     }
 
-    private static boolean isDuplicate(List<Integer> numbers){
+    protected static boolean isDuplicate(List<Integer> numbers){
         Set<Integer> set = new HashSet<>(numbers);
-        if (set.size() < numbers.size()) {
-            // 중복된 숫자가 있음
-            return true;
-        }
-        return false;
+        // 중복된 숫자가 있음
+        return set.size() < numbers.size();
     }
 
-    private static boolean isOutOfRangeLottoNumbers(List<Integer> numbers){
+    protected static boolean isOutOfRangeLottoNumbers(List<Integer> numbers){
         for(int lottoNumber : numbers){
             if(isOutOfRange(lottoNumber)){
                 return true;
@@ -105,7 +49,7 @@ public class SystemValidator {
         return false;
     }
 
-    private static boolean isOutOfRange(int number){
+    protected static boolean isOutOfRange(int number){
         return number > LOTTO_MAX_NUMBER || number < LOTTO_MIN_NUMBER;
     }
 }

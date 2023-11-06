7cc23a01b5b7f23003b735380a0f99216f2db66e
WooJJam <jaemin5548@naver.com>
2023-11-06 22:04:33 +0900

    [Test] 테스트를 위한 PrizeResult 모델 생성

--- src/main/java/lotto/model/PrizeResult.java
+++ src/main/java/lotto/model/PrizeResult.java
@@ -1,2 +1,17 @@
-package lotto.model;public class PrizeResult {
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrizeResult {
+
+    List<Result> ranking = new ArrayList<>();
+
+    public PrizeResult(List<Integer> lottoMatch, List<Boolean> hasBonusMatch) {
+
+    }
+
+    public List<Result> getRanking() {
+        return ranking;
+    }
 }

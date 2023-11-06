1de09e5134b2c8d83aa2b24a733ac517c1dd2298
JIHYE <jdh324@naver.com>
2023-11-07 05:28:57 +0900

    test: 보너스 번호가 로또 번호와 중복이 되는지 확인하는 메서드 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,11 +2,13 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -21,5 +23,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("보너스 번호가 로또 번호와 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void isBonusNumberDuplicate() {
+        // given
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        Lotto lotto = new Lotto(numbers);
+        int bonusNumber = 1;
+
+        // when & then
+        assertThatThrownBy(() -> lotto.isBonusNumberDuplicate(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

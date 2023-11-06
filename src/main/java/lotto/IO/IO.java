631e9653eb24f1831818226f8487befcb556bf1a
minju <minju9711@naver.com>
2023-11-07 03:25:11 +0900

    [feat] 사용자로부터 입력과 출력을 담당하는 클래스. 현재 구입금액과 당첨번호, 보너스 번호를 입력받는 것까지 구현

--- src/main/java/lotto/IO/IO.java
+++ src/main/java/lotto/IO/IO.java
@@ -1,4 +1,33 @@
 package lotto.IO;
 
-public class IO {
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Util.Validate;
+
+import java.util.List;
+
+public class IO { //여긴 무조건 정적호출로 해보자
+
+    public static String InputCash(){
+        System.out.println("구입금액을 입력해주세요");
+        String cash = Console.readLine();
+        Validate.checkInteger(cash);
+        return cash;
+    }
+    
+    public static void PrintCount(int count){
+        System.out.println(count+"개를 구매했습니다.");
+    }
+
+    public static String InputWinningLotto(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String InputBonus(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonus = Console.readLine();
+        return bonus;
+    }
+
 }
+

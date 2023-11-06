255b1a7ac5adec792835b44abe9dc8217fa4592a
Namyejun <yejun1148@gmail.com>
2023-11-06 17:28:18 +0900

    발행한 로또 객체를 담당할 Lotto 클래스 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,20 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +23,29 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        numbers.forEach(i -> set.add(i));
+
+        if (set.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private List<Integer> sort() {
+        List<Integer> sortedLotto =  numbers.stream().sorted((o1, o2) -> {
+            return o1 - o2;
+        }).toList();
+
+        return sortedLotto;
+    }
+
+    public String printString() {
+        return "[" + String.join(", ", makeStringNumbers(sort())) + "]";
+    }
+
+    private List<String> makeStringNumbers(List<Integer> numbers) {
+        return numbers.stream().map(String::valueOf).toList();
+    }
 }

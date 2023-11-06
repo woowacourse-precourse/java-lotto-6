146449b91d8050f6e276fbd1be5567046332a07c
moong2 <www.castlehi@gmail.com>
2023-11-07 01:47:35 +0900

    feat: implement input lotto number validator

--- src/main/java/lotto/model/validator/LottoNumberValidator.java
+++ src/main/java/lotto/model/validator/LottoNumberValidator.java
@@ -1,2 +1,13 @@
-package lotto.model.validator;public interface LottoNumberValidator {
+package lotto.model.validator;
+
+import java.util.List;
+
+public interface LottoNumberValidator {
+    void checkNumberOfLottoNumbers(List<String> lottoNumbers);
+
+    void checkBlankOfLottoNumbers(List<String> lottoNumbers);
+
+    void checkInRangeOfLottoNumbers(List<Integer> lottoNumbers);
+
+    void checkDuplicateLottoNumbers(List<Integer> lottoNumbers);
 }

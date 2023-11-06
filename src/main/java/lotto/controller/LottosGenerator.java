774baf1dfca06e52c15a8e23925c795d994bd9c1
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-06 14:17:42 +0900

    feat: (LottosGenerator) Lottos 생성 기능

---
+++ src/main/java/lotto/controller/LottosGenerator.java
@@ -0,0 +1,31 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottosGenerator {
+    private static final int LOTTO_MAX_NUMBER =45;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_PRICE = 1000;
+
+    public Lottos generateLottos(int budget){
+        Lottos lottos = new Lottos();
+        for(int i=0;i<countToGenerateLotto(budget);i++){
+            lottos.addLotto(generateLotto());
+        }
+        return lottos;
+    }
+
+    private int countToGenerateLotto(int budget){
+        return budget/LOTTO_PRICE;
+    }
+
+    private Lotto generateLotto(){
+        return new Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE));
+    }
+}

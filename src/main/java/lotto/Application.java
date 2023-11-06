7333029cb03fe2bfb4ece53b6a7dce4d3b6b4940
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2023-11-06 23:47:44 +0900

    feat : 구입금액, 당첨번호 입력, 테스트코드 작성(작동x)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int totalPrice = Integer.parseInt(readLine());
+
+        int[] winNumber  = new int[7];
+        String[] winNumberString = readLine().split(",");
+
+        for (int i = 0; i < 6; i++){
+            winNumber[i] = Integer.parseInt(winNumberString[i].trim());
+        }
+
+        int bonusNumber = Integer.parseInt(readLine());
+        winNumber[6] = bonusNumber;
+
+        System.out.println("당첨번호: "+ winNumber);
+
+
     }
 }

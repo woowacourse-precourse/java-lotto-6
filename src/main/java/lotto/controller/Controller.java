2f75ea42f552d32959a99f5a5437b9b9b927ba73
DongHyun <kimdh0315@naver.com>
2023-11-06 14:45:53 +0900

    refactor(main/controller/Controller) : refactor code by splitting codes in startGame() and matching code convention

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -9,35 +9,38 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Controller {
-    public static void startGame(){
+    public static void startGame() {
+        User user = initializeLottoes();
+        Winning winning = initializeWinning();
+
+        checkLottoResult(user, winning);
+        showLottoResult(user);
+    }
+
+    public static User initializeLottoes() {
         int money = getValidMoney();
         HashMap<Lotto, Rank> lottos = publishLotto(money);
-
         User user = new User(money, lottos);
         UserOutput.ConsolePrintBoughtResult(user);
 
-        List<Integer> winning_number = getValidWinningNumber();
-
-        int bonusNumber = getValidBonusNumber(winning_number);
-
-        Winning winning = new Winning(new Lotto(winning_number), bonusNumber);
-
-        checkLottoResult(user, winning);
-
-        showLottoResult(user);
-
+        return user;
     }
 
-    public static void showLottoResult(User user){
+    public static Winning initializeWinning() {
+        List<Integer> winning_number = getValidWinningNumber();
+        int bonusNumber = getValidBonusNumber(winning_number);
+
+        return new Winning(new Lotto(winning_number), bonusNumber);
+    }
+
+    public static void showLottoResult(User user) {
         List<Integer> result = StaticsService.getWinningStatics(user.getLottos());
-
         UserOutput.ConsolePrintStatistics(result);
-
         double winning_rate = StaticsService.calculateReturnRate(result, user.getNumber() * Constant.LOTTO_PRICE);
         UserOutput.ConsolePrintStatResult(winning_rate);
     }
 
-    public static int getValidBonusNumber(List<Integer> winning){
+    public static int getValidBonusNumber(List<Integer> winning) {
         while (true) {
             try {
                 String bonus = UserInput.ConsoleInputBonusNumber();
@@ -48,24 +51,24 @@ public class Controller {
         }
     }
 
-    public static List<Integer> getValidWinningNumber(){
+    public static List<Integer> getValidWinningNumber() {
         while (true) {
             try {
                 String number = UserInput.ConsoleInputWinningNumber();
                 return Convertor.convertNumber(number);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
 
-    public static int getValidMoney(){
+    public static int getValidMoney() {
         while (true) {
             try {
                 String money = UserInput.ConsoleInputBoughtMoney();
                 return Convertor.convertMoney(money);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(Message.ERROR_MONEY);
             }
         }
@@ -74,7 +77,7 @@ public class Controller {
     public static HashMap<Lotto, Rank> publishLotto(int money) {
         HashMap<Lotto, Rank> lottos = new HashMap<>();
 
-        for(int i = 0; i < money / Constant.LOTTO_PRICE; i++){
+        for (int i = 0; i < money / Constant.LOTTO_PRICE; i++) {
             Lotto lotto = new Lotto(LottoService.makeRandomLottoNumber());
             lottos.put(lotto, Rank.NONE);
         }
@@ -83,7 +86,7 @@ public class Controller {
     }
 
     public static void checkLottoResult(User user, Winning winning) {
-        for (Lotto lotto : user.getLottos().keySet()){
+        for (Lotto lotto : user.getLottos().keySet()) {
             Rank rank = LottoService.checkLottoRank(lotto, winning);
             user.setLottoResult(lotto, rank);
         }

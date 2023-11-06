9062fd8a7547f13d5913cc89eb5765701ccb784a
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-11-06 21:50:03 +0900

    feat : 유저의 입력을 받는 유틸 클래스

---
+++ src/main/java/lotto/util/InputUtil.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+    
+    public String getInput() {
+        return Console.readLine();
+    }
+}

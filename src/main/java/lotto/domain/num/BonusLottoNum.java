fd594dc0759005c71f56cf18e7eebf7a368d8ea5
dmdrk1414 <seungchan141414@gmail.com>
2023-11-06 20:54:50 +0900

    [Style(lotto)] 주석 삭제

--- src/main/java/lotto/domain/num/BonusLottoNum.java
+++ src/main/java/lotto/domain/num/BonusLottoNum.java
@@ -2,9 +2,6 @@ package lotto.domain.num;
 
 import lotto.utill.Utii;
 
-/**
- * 보너스 번호 클래스
- */
 public class BonusLottoNum {
     private Integer bonusNum;
 
@@ -12,12 +9,7 @@ public class BonusLottoNum {
         this.bonusNum = bonusNumber;
     }
 
-    /**
-     * 보너스 번호가 같은지 확인한다.
-     *
-     * @param num 같은지 확인하고 싶은 번호
-     * @return
-     */
+     
     public Boolean isSame(Integer num) {
         return Utii.isSameInt(bonusNum, num);
     }

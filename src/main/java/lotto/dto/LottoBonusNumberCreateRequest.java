5bdecf30d7969f33f89b0979e0eeb6acaf22bf24
dukcode <duk9741@gmail.com>
2023-11-06 15:50:36 +0900

    feat: 보너스 번호 생성 요청 클래스 구현

--- src/main/java/lotto/dto/LottoBonusNumberCreateRequest.java
+++ src/main/java/lotto/dto/LottoBonusNumberCreateRequest.java
@@ -1,4 +1,17 @@
 package lotto.dto;
 
 public class LottoBonusNumberCreateRequest {
+    private int bonusNumber;
+
+    public LottoBonusNumberCreateRequest(String bonusNumber) {
+        try {
+            this.bonusNumber = Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("보너스 번호는 숫자여야 합니다.");
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }

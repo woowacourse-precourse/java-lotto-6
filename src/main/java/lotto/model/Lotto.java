217b369c2bebec51e9e5d47796d5b2045c13aeff
LeeJJ07 <jkj9513@naver.com>
2023-11-06 13:24:13 +0900

    feat : 복권 객체 추가 구현
    하나의 복권 객체 구현. 유효한 복권 번호, 범위, 자리수인지 확인

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,36 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    	
+    	if(!isSixDigit(numbers))
+    		throw new IllegalArgumentException();
+    	if(!isNumbersRange1to45(numbers))
+    		throw new IllegalArgumentException();
+    	if(!isDuplication(numbers))
+    		throw new IllegalArgumentException();
     }
-
-    // TODO: 추가 기능 구현
+    
+    private boolean isSixDigit(List<Integer> numbers) {
+    	return numbers.size() == 6;
+    }
+    
+    private boolean isNumbersRange1to45(List<Integer> numbers) {
+    	
+    	for(int number : numbers) {
+    		if(number<1 || number >45)
+    			return false;
+    	}
+    	return true;
+    }
+    
+    private boolean isDuplication(List<Integer> numbers) {
+    	return sizeSet(numbers) == numbers.size();
+    }
+    
+    private int sizeSet(List<Integer> numbers) {
+    	Set <Integer> set = new HashSet<Integer>(numbers);
+    	return set.size();
+    }
+    
+    	
 }

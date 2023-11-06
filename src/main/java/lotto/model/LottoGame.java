9830c29b6ffa3e68ae8dfd07cff4949f593b7704
SunHyeok <cghhld@naver.com>
2023-11-06 21:41:53 +0900

    feat: UI로직 생성

---
+++ src/main/java/lotto/model/LottoGame.java
@@ -0,0 +1,9 @@
+package lotto.model;
+
+import lotto.view.LottoView;
+public class LottoGame {
+    public static void main(String[] args) {
+        LottoView lottoView = new LottoView();
+        lottoView.startNewGame();
+    }
+}
--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,30 +1,40 @@
 package lotto.view;
 
-import java.util.Arrays;
-import java.util.List;
-import camp.nextstep.edu.missionutils.Console;
-import java.util.Collections;
-import java.util.stream.Collectors;
+import lotto.Lotto;
 import lotto.model.WinningResult;
 import lotto.model.LottoNumberGenerator;
-import lotto.Lotto;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import camp.nextstep.edu.missionutils.Console;
 
 public class LottoView {
+    public void startNewGame() {
+        int purchaseAmount = getPurchaseAmount();
+        int numberOfTickets = purchaseAmount / Lotto.TICKET_PRICE;
+        List<List<Integer>> purchasedTickets = generatePurchasedTickets(numberOfTickets);
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber();
+
+        List<WinningResult> winningResults = calculateWinningResults(purchasedTickets, winningNumbers, bonusNumber);
+
+        printNumberOfPurchasedTickets(numberOfTickets);
+        printWinningResults(winningResults);
+    }
+
     public int getPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 
-    public void printNumberOfPurchasedTickets(int count) {
-        System.out.printf("%d개를 구매했습니다.%n", count);
-        if (count > 0) {
-            System.out.println("구매한 로또 번호는 다음과 같습니다:");
-            for (int i = 0; i < count; i++) {
-                List<Integer> lottoNumbers = LottoNumberGenerator.generateRandomLottoNumbers();
-                Collections.sort(lottoNumbers);
-                System.out.println(lottoNumbers);
-            }
+    public List<List<Integer>> generatePurchasedTickets(int count) {
+        List<List<Integer>> purchasedTickets = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumbers = LottoNumberGenerator.generateRandomLottoNumbers();
+            Collections.sort(lottoNumbers);
+            purchasedTickets.add(lottoNumbers);
         }
+        return purchasedTickets;
     }
 
     public List<Integer> getWinningNumbers() {
@@ -40,43 +50,80 @@ public class LottoView {
         return Integer.parseInt(Console.readLine());
     }
 
+    public List<WinningResult> calculateWinningResults(List<List<Integer>> purchasedTickets, List<Integer> winningNumbers, int bonusNumber) {
+        List<WinningResult> results = new ArrayList<>();
+        for (List<Integer> ticket : purchasedTickets) {
+            int matchingCount = calculateMatchingCount(ticket, winningNumbers);
+            boolean hasBonusNumber = ticket.contains(bonusNumber);
+
+            results.add(new WinningResult(matchingCount, hasBonusNumber));
+        }
+        return results;
+    }
+
+    private int calculateMatchingCount(List<Integer> ticket, List<Integer> winningNumbers) {
+        long matchingCount = ticket.stream()
+                .filter(winningNumbers::contains)
+                .count();
+        return (int) matchingCount;
+    }
+
+    public void printNumberOfPurchasedTickets(int count) {
+        System.out.printf("%d개를 구매했습니다.%n", count);
+        if (count > 0) {
+            System.out.println("구매한 로또 번호는 다음과 같습니다:");
+            for (List<Integer> ticket : generatePurchasedTickets(count)) {
+                System.out.println(ticket);
+            }
+        }
+    }
+
     public void printWinningResults(List<WinningResult> results) {
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        int[] matchCounts = new int[6];
+        int[] matchCounts = new int[7]; // 0부터 6까지의 인덱스를 가질 배열 생성
         for (WinningResult result : results) {
             int matchingCount = result.getMatchingCount();
-            if (matchingCount >= 3) {
-                matchCounts[matchingCount] += 1;
-            }
+            matchCounts[matchingCount]++; // 매칭 카운트에 따른 배열 인덱스 증가
         }
 
-        for (int i = 3; i <= 5; i++) {
-            System.out.printf("%d개 일치 (%,d원) - %d개%n", i, calculateWinningAmount(i), matchCounts[i]);
+        for (int i = 3; i <= 6; i++) {
+            int winningAmount = calculateWinningAmount(i, false);
+            System.out.printf("%d개 일치 (%,d원) - %d개%n", i, winningAmount, matchCounts[i]);
         }
 
-        int matchingCount = 5;
-        boolean hasBonusNumber = false;
-        System.out.printf("%d개 일치, 보너스 볼 일치 (%,d원) - %d개%n", matchingCount, calculateWinningAmount(matchingCount, hasBonusNumber), matchCounts[matchingCount]);
+        int bonusMatchingCount = matchCounts[5];
+        if (bonusMatchingCount > 0) {
+            int winningAmount = calculateWinningAmount(5, true);
+            System.out.printf("%d개 일치, 보너스 볼 일치 (%,d원) - %d개%n", 5, winningAmount, bonusMatchingCount);
+        }
+
         System.out.println("총 수익률은 " + calculateTotalProfitRate(results) + "%입니다.");
     }
 
-    private int calculateWinningAmount(int matchingCount) {
-        return switch (matchingCount) {
-            case 3 -> 5_000;
-            case 4 -> 50_000;
-            case 5 -> 1_500_000;
-            default -> 0;
-        };
+    private int calculateWinningAmount(int matchingCount, boolean hasBonusNumber) {
+        int winningAmount = 0;
+
+        if (matchingCount == 3) {
+            winningAmount = 5_000;
+        }
+        if (matchingCount == 4) {
+            winningAmount = 50_000;
+        }
+        if (matchingCount == 5 && hasBonusNumber) {
+            winningAmount = 30_000_000;
+        }
+        if (matchingCount == 5 && !hasBonusNumber) {
+            winningAmount = 1_500_000;
+        }
+        if (matchingCount == 6) {
+            winningAmount = 2_000_000_000;
+        }
+
+        return winningAmount;
     }
 
-    private int calculateWinningAmount(int matchingCount, boolean hasBonusNumber) {
-        if (matchingCount == 5 && hasBonusNumber) {
-            return 30_000_000;
-        }
-        return 0;
-    }
 
     private double calculateTotalProfitRate(List<WinningResult> results) {
         int totalWinningAmount = results.stream()
@@ -86,5 +133,4 @@ public class LottoView {
         int totalPurchasePrice = results.size() * Lotto.TICKET_PRICE;
         return (double) totalWinningAmount / totalPurchasePrice * 100;
     }
-
 }

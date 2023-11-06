093abf80b8d9a1915d92cb1b3f24a6f99a1b8e9d
qncj5224 <qncj5224@naver.com>
2023-11-06 22:35:35 +0900

    fix : 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.validation.Validator;
 import lotto.model.Lotto;
 import lotto.model.LottoGame;
 import lotto.model.LottoResult;
@@ -33,24 +34,28 @@ public class LottoController {
     return lottos;
   }
 
-  public static void run(){
+  public static void run() {
     int money = inputView.inputMoney();
     int calculateLottoCount = InputPresent.calculateLottoCount(money);
-
     OutputView.printLottoCountMessage(calculateLottoCount);
     List<List<Integer>> lottos = createLottos(calculateLottoCount);
     OutputView.printLottos(lottos);
-
     String winningNumber = inputView.inputWinningNumber();
     List<String> winningNumbers = InputProcessor.splitWinningNumbers(winningNumber);
     List<Integer> winningNumberSet = InputProcessor.convertToIntegerList(winningNumbers);
-
-
     int bonusNumber = inputView.inputBonusNumber();
-    BonusNumberValidator.isDuplicate(winningNumbers, bonusNumber);
 
+    List<Integer> rank = checkLottoResults(lottos, winningNumberSet, bonusNumber);
+    printLottoResults(money, rank);
+  }
+
+  public static List<Integer> checkLottoResults(List<List<Integer>> lottos, List<Integer> winningNumberSet, int bonusNumber) {
     List<Integer> rank = LottoGame.checkWinningStatus(lottos, winningNumberSet, bonusNumber);
-    OutputView.printSuccessResult();
+    return rank;
+  }
+
+  public static void printLottoResults(int money, List<Integer> rank) {
+    OutputView.printLottoCountMessage(rank.size());
     OutputView.printRanking(rank);
 
     int prize = LottoResult.lottoPrizeCalculator(rank);

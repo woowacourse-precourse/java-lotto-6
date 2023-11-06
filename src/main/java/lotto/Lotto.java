7b26113d6668cbd0b4c5019825084f22c138ae16
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-07 02:55:35 +0900

    feat : 중복된숫자 예외검증 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateOverlap(numbers);
         this.numbers = numbers;
+    }
+
+    private void validateOverlap(List<Integer> numbers) {
+        Set<Integer> overlapCheck = new HashSet<>();
+        if(overlapCheck.size()!=6){
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +28,7 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void setRandomNumbers(){
+        new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+    }
 }

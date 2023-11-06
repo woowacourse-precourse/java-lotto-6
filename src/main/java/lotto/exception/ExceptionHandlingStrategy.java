ebe3d595aa17153c6b2675a710cc577d960ed3ad
dukcode <duk9741@gmail.com>
2023-11-06 23:28:43 +0900

    refactor: ExceptionHandlingStrategy 함수형 인터페이스 선언 제거

--- src/main/java/lotto/exception/ExceptionHandlingStrategy.java
+++ src/main/java/lotto/exception/ExceptionHandlingStrategy.java
@@ -4,7 +4,6 @@ import java.util.function.BiConsumer;
 import lotto.service.LottoGameService;
 import lotto.view.LottoGameView;
 
-@FunctionalInterface
 public interface ExceptionHandlingStrategy {
     void apply(LottoGameView lottoGameView, LottoGameService lottoGameService,
                BiConsumer<LottoGameView, LottoGameService> inputBehavior);

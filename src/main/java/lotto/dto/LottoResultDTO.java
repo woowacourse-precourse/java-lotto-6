9e8c1fd5ca975b39bc4fe54d12a4093f67311694
howudong <tjdvy963@naver.com>
2023-11-06 22:25:38 +0900

    refactor : LottoResultDTO 가 DTO.Ouput을 상속하도록 변경

--- src/main/java/lotto/dto/LottoResultDTO.java
+++ src/main/java/lotto/dto/LottoResultDTO.java
@@ -4,5 +4,20 @@ import lotto.domain.LottoRankInfo;
 
 import java.util.Map;
 
-public record LottoResultDTO(String returnRatio, Map<LottoRankInfo, Integer> rank) {
+public class LottoResultDTO extends DTO.Output {
+    private final String returnRatio;
+    private final Map<LottoRankInfo, Integer> ranks;
+
+    public LottoResultDTO(String returnRatio, Map<LottoRankInfo, Integer> ranks) {
+        this.returnRatio = returnRatio;
+        this.ranks = ranks;
+    }
+
+    public String getReturnRatio() {
+        return returnRatio;
+    }
+
+    public Map<LottoRankInfo, Integer> getRanks() {
+        return ranks;
+    }
 }

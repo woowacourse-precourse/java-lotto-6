0610cae3d01ddca5e55588800f829d1dfcab9412
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-06 09:10:46 +0900

    fix: InputMessage 올바르게 배정

--- src/main/java/lotto/view/InputMessage.java
+++ src/main/java/lotto/view/InputMessage.java
@@ -2,9 +2,9 @@ package lotto.view;
 
 public enum InputMessage {
 
-    INPUT_LOTTO("로또 번호에 중복이 있습니다"),
-    INPUT_BONUS_NUMBER("로또 번호의 개수가 맞지 않습니다"),
-    INPUT_PURCHASE_AMOUNT("로또 번호의 범위가 맞지 않습니다"),
+    INPUT_LOTTO("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     ;
 
     private final String message;

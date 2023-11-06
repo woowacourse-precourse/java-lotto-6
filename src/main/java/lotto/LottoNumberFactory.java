3a81259ce412cca204f89562bc0df3856d1648f8
rakyun1 <dbskim234@naver.com>
2023-11-06 06:08:57 -0700

    Remove(LottoNumberFactory): Remove unnecessary imports.

--- src/main/java/lotto/LottoNumberFactory.java
+++ src/main/java/lotto/LottoNumberFactory.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class LottoNumberFactory {
 

c093d72a6791c0295445278dd06c89dd79ebfc16
Yehyeok Bang <qkddpgur318@gmail.com>
2023-11-06 10:32:42 +0900

    refactor(model): 당첨 계산 로직과 당첨금 계산 로직 분리
    당첨 번호와 보너스 번호가 일치하는지 확인합니다.
    위 정보를 기반으로 당첨금을 계산합니다.

--- src/main/java/lotto/model/PrizeEvaluator.java
+++ src/main/java/lotto/model/PrizeEvaluator.java
@@ -1,18 +1,14 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PrizeEvaluator {
-    private static final int NO_MATCH_PRIZE = 0;
     private static final int THREE_MATCH_PRIZE = 5_000;
     private static final int FOUR_MATCH_PRIZE = 50_000;
     private static final int FIVE_MATCH_PRIZE = 1_500_000;
     private static final int FIVE_MATCH_WITH_BONUS_PRIZE = 30_000_000;
     private static final int SIX_MATCH_PRIZE = 2_000_000_000;
-    private static final int THREE_MATCH_COUNT = 3;
-    private static final int FOUR_MATCH_COUNT = 4;
-    private static final int FIVE_MATCH_COUNT = 5;
-    private static final int SIX_MATCH_COUNT = 6;
 
     private int threeMatchCount = 0;
     private int fourMatchCount = 0;
@@ -23,13 +19,14 @@ public class PrizeEvaluator {
     public Prize evaluate(WinningNumbers winningNumbers, List<Lotto> lottos) {
         int bonusNumber = winningNumbers.getBonusNumber();
 
-        int prizeMoney = 0;
         for (Lotto lotto : lottos) {
             int matchCount = countMatch(winningNumbers.getNumbers(), lotto.getNumbers());
             boolean isBonusMatch = lotto.getNumbers().contains(bonusNumber);
 
-            prizeMoney += calculatePrize(matchCount, isBonusMatch);
+            checkMatch(matchCount, isBonusMatch);
         }
+
+        int prizeMoney = calculatePrizeMoney();
 
         return new Prize(prizeMoney, threeMatchCount, fourMatchCount, fiveMatchCount, fiveMatchWithBonusCount, sixMatchCount);
     }
@@ -40,27 +37,59 @@ public class PrizeEvaluator {
                 .count();
     }
 
-    private int calculatePrize(int matchCount, boolean isBonusMatch) {
-        if (matchCount == THREE_MATCH_COUNT) {
-            threeMatchCount++;
-            return THREE_MATCH_PRIZE;
-        }
-        if (matchCount == FOUR_MATCH_COUNT) {
-            fourMatchCount++;
-            return FOUR_MATCH_PRIZE;
-        }
-        if (matchCount == FIVE_MATCH_COUNT && isBonusMatch) {
+    private void checkMatch(int matchCount, boolean isBonusMatch) {
+        List<Runnable> prizeIncreaseActions = Arrays.asList(
+                this::noMatch,
+                this::oneMatch,
+                this::twoMatch,
+                this::increaseThreeMatchPrize,
+                this::increaseFourMatchPrize,
+                () -> increaseFiveMatchPrize(isBonusMatch),
+                this::increaseSixMatchPrize
+        );
+
+        prizeIncreaseActions.get(matchCount)
+                .run();
+    }
+
+    private void noMatch() {
+        // do nothing
+    }
+
+    private void oneMatch() {
+        // do nothing
+    }
+
+    private void twoMatch() {
+        // do nothing
+    }
+
+    private void increaseThreeMatchPrize() {
+        threeMatchCount++;
+    }
+
+    private void increaseFourMatchPrize() {
+        fourMatchCount++;
+    }
+
+    private void increaseFiveMatchPrize(boolean isBonusMatch) {
+        if (isBonusMatch) {
             fiveMatchWithBonusCount++;
-            return FIVE_MATCH_WITH_BONUS_PRIZE;
+            return;
         }
-        if (matchCount == FIVE_MATCH_COUNT) {
-            fiveMatchCount++;
-            return FIVE_MATCH_PRIZE;
-        }
-        if (matchCount == SIX_MATCH_COUNT) {
-            sixMatchCount++;
-            return SIX_MATCH_PRIZE;
-        }
-        return NO_MATCH_PRIZE;
+
+        fiveMatchCount++;
+    }
+
+    private void increaseSixMatchPrize() {
+        sixMatchCount++;
+    }
+
+    private int calculatePrizeMoney() {
+       return (threeMatchCount * THREE_MATCH_PRIZE)
+                    + (fourMatchCount * FOUR_MATCH_PRIZE)
+                    + (fiveMatchCount * FIVE_MATCH_PRIZE)
+                    + (fiveMatchWithBonusCount * FIVE_MATCH_WITH_BONUS_PRIZE)
+                    + (sixMatchCount * SIX_MATCH_PRIZE);
     }
 }

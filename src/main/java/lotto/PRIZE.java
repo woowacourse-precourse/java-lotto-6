483f4f03cf523d86b3446df834edb0dee611e093
jimmy0006 <jinmin645@gmail.com>
2023-11-06 17:48:28 +0900

    feat:PRIZE enum 수정

--- src/main/java/lotto/PRIZE.java
+++ src/main/java/lotto/PRIZE.java
@@ -1,19 +1,32 @@
 package lotto;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public enum PRIZE {
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FORTH(50000),
-    FIFTH(5000),
-    FAIL(0);
+    FIRST(0,2000000000),
+    SECOND(1, 30000000),
+    THIRD(2, 1500000),
+    FORTH(3, 50000),
+    FIFTH(4, 5000),
+    FAIL(5, 0);
 
     private final int value;
-    PRIZE(int value){
+    private final int score;
+    PRIZE(int score, int value){
         this.value = value;
+        this.score = score;
     }
 
     public int getValue(){
         return value;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public String toString(){
+        return NumberFormat.getNumberInstance(Locale.US).format(value)+"원";
     }
 }

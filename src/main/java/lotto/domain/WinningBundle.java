5d53dcd1e35ecbc4091dfd0b7d4c68cfec328c66
dbstn1325 <dbstn1325@naver.com>
2023-11-07 06:25:09 +0900

    feat(WinningBundle): 당첨 번호의 갯수가 총 6개가 아니면 IllegalArgumentException 예외처리 기능 구현

--- src/main/java/lotto/domain/WinningBundle.java
+++ src/main/java/lotto/domain/WinningBundle.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningBundle {
+    private static final String WINNING_NUMBER_MUST_TOTAL_SIX_MESSAGE = "[ERROR] 당첨 번호의 갯수는 총 6개여야 합니다.";
 
     private final List<WinningNumber> winningBundle;
 
@@ -11,7 +12,16 @@ public class WinningBundle {
     }
 
     public static WinningBundle from(List<WinningNumber> winningBundle) {
+        validateSize(winningBundle);
         return new WinningBundle(winningBundle);
     }
+
+    private static void validateSize(List<WinningNumber> winningBundle) {
+        if(winningBundle.size() != 6) {
+            throw new IllegalArgumentException(WINNING_NUMBER_MUST_TOTAL_SIX_MESSAGE);
+        }
+    }
+
+
 
 }

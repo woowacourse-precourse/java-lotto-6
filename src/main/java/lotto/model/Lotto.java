de1bf0ddaacd856304ac6f09927ee0ad4446ec56
sunwooiljoon <wnsdl1659@naver.com>
2023-11-06 21:02:10 +0900

    refactor : Lotto.java 수정
    상수 하드코딩 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -8,6 +8,11 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
+    private static final Integer maxNumber = 45;
+    private static final Integer minNumber = 1;
+    private static final Integer sizeOfNumbers = 6;
+
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -18,14 +23,14 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호의 개수는 6이어야 합니다.");
+        if (numbers.size() != sizeOfNumbers) {
+            throw new IllegalArgumentException(String.format("로또 번호의 개수는 %d이어야 합니다.", sizeOfNumbers));
         }
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("로또 번호는 서로 중복되면 안됩니다.");
         }
-        if (Collections.max(numbers) > 45 || Collections.min(numbers) < 1) {
-            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        if (Collections.max(numbers) > maxNumber || Collections.min(numbers) < minNumber) {
+            throw new IllegalArgumentException(String.format("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", minNumber, maxNumber));
         }
     }
 
@@ -34,7 +39,7 @@ public class Lotto {
     }
 
     public static Lotto generateLotto() {
-        List<Integer> list = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List<Integer> list = new ArrayList<>(Randoms.pickUniqueNumbersInRange(minNumber, maxNumber, sizeOfNumbers));
         Collections.sort(list);
 
         return new Lotto(list);

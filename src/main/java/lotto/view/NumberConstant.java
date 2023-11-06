996a2aebfee14e1fafa3836f165990acc650977f
yeseong31 <yeseong31@naver.com>
2023-11-06 13:43:54 +0900

    feat(NumberConstant): NumberConstant로 숫자 상수 정의

---
+++ src/main/java/lotto/view/NumberConstant.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class NumberConstant {
+    
+    public static final int ZERO = 0;
+    public static final int VALIDATE_LOTTO_SIZE = 6;
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    public static final int INCREASE_NUMBER = 1;
+    public static final int LOTTO_PRICE_UNIT = 1_000;
+    
+    private NumberConstant() {
+    }
+}

779ac87f5e28bc4e47625da016b87ea14d8930e0
haden <80521474+thdefn@users.noreply.github.com>
2023-11-06 20:17:31 +0900

    feat : 당첨 번호 입력 기능
    로또 당첨 번호와 보너스 번호를 입력하는 기능을 구현했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-import static lotto.ErrorCode.PURCHASE_AMOUNT_ERROR;
+import static lotto.ErrorCode.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -22,7 +22,66 @@ public class Application {
         }
 
         System.out.println(number + "개를 구매했습니다.");
-        issueLotto(number);
+        List<Lotto> lottoList = issueLotto(number);
+
+        boolean[] isExistNumber = new boolean[47];
+        Lotto winningLotto = drawLotto(isExistNumber);
+        int bonusNumber = drawBonus(isExistNumber);
+    }
+
+    private static Lotto drawLotto(boolean[] isExistNumber){
+        System.out.println("당첨 번호를 입력해주세요.");
+        Lotto winningLotto = null;
+        while (winningLotto == null) {
+            isExistNumber = new boolean[47];
+            try {
+                winningLotto = getWinningLotto(isExistNumber);
+            } catch (RuntimeException e) {
+                handleException(e);
+            }
+        }
+        return winningLotto;
+    }
+
+    private static int drawBonus(boolean[] isExistNumber){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = 0;
+        while (bonusNumber == 0) {
+            try {
+                bonusNumber = getBonusNumber(isExistNumber);
+            } catch (RuntimeException e) {
+                handleException(e);
+            }
+        }
+        return bonusNumber;
+    }
+
+    private static Lotto getWinningLotto(boolean[] isExistNumber) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] input = Console.readLine().split(",");
+        if (input.length != 6)
+            throw new IllegalArgumentException(LOTTERY_SIZE_ERROR.getMessage());
+        for (String s : input) {
+            int number = Integer.parseInt(s);
+            validateNumber(isExistNumber, number);
+            winningNumbers.add(number);
+            isExistNumber[number] = true;
+        }
+        return new Lotto(winningNumbers);
+    }
+
+    private static void validateNumber(boolean[] isExistNumber, int number) {
+        if (number < 1 || number > 45)
+            throw new IllegalArgumentException(LOTTERY_NUMBER_RANGE_ERROR.getMessage());
+        if (isExistNumber[number])
+            throw new IllegalArgumentException(LOTTERY_NUMBER_DUPLICATED_ERROR.getMessage());
+    }
+
+    private static int getBonusNumber(boolean[] isExistNumber) {
+        int number = Integer.parseInt(Console.readLine());
+        validateNumber(isExistNumber, number);
+        isExistNumber[number] = true;
+        return number;
     }
 
     private static List<Lotto> issueLotto(int number) {
--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -1,6 +1,9 @@
 package lotto;
 
 public enum ErrorCode {
+    LOTTERY_NUMBER_DUPLICATED_ERROR("로또 번호는 중복될 수 없습니다."),
+    LOTTERY_NUMBER_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    LOTTERY_SIZE_ERROR("당첨 번호는 6개의 숫자여야 합니다."),
     PURCHASE_AMOUNT_ERROR("구매 금액은 1000원 단위로 입력해 주세요.");
     private final String message;
 

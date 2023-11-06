a065fffff1347cf9c9f0725e1afa77cb88dd83fb
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 16:23:06 +0900

    feat: Result from 메서드 추가

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,19 +1,29 @@
 package lotto;
 
+import java.util.stream.Stream;
+
 public enum Result {
     FIRST_PRIZE(6, false) {int prize() {return 2_000_000_000;}},
     SECOND_PRIZE(5, true) {int prize() {return 30_000_000;}},
     THIRD_PRIZE(5, false) {int prize() {return 1_500_000;}},
     FOURTH_PRIZE(4, false) {int prize() {return 50_000;}},
     FIFTH_PRIZE(3, false) {int prize() {return 5_000;}},
-    NO_PRIZE(0, false) {int prize() {return 0;}};
+    NO_PRIZE(2, false) {int prize() {return 0;}};
 
     abstract int prize();
+
     private final int matchingNumbers;
     private final boolean bonus;
 
     Result(int matchingNumbers, boolean bonus) {
         this.matchingNumbers = matchingNumbers;
         this.bonus = bonus;
+    }
+
+    public static Result from(int matchingNumbers, boolean hasBonus) {
+        return Stream.of(values())
+                .filter(result -> result.matchingNumbers == matchingNumbers && result.bonus == hasBonus)
+                .findFirst()
+                .orElse(NO_PRIZE);
     }
 }

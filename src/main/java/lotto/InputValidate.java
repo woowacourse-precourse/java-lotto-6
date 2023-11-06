3a1ff1426a5a42159f1f4d1ff730e3c11a10af88
Leespect <wewqwew153@gmail.com>
2023-11-06 16:41:52 +0900

    Feat : 구입 금액 유효성 검사

---
+++ src/main/java/lotto/InputValidate.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import static lotto.constants.ConstantValues.*;
+import static lotto.constants.ErrorMessages.*;
+
+
+public class InputValidate {
+
+    public static void validateBuyMoney(String buyMoney) {
+        validateBuyMoneyIsEmpty(buyMoney);
+        validateBuyMoneyIsNotNumber(buyMoney);
+        int money = Integer.parseInt(buyMoney);
+        validateBuyMoneyIsMinus(money);
+        validateBuyMoneyIsNotThousand(money);
+    }
+
+    public static void validateBuyMoneyIsEmpty(String buyMoney) {
+        if (buyMoney.isEmpty()) {
+            throw new IllegalArgumentException(BUY_EMPTY_ERROR.getMessage());
+        }
+    }
+
+    public static void validateBuyMoneyIsNotNumber(String buyMoney) {
+        try {
+            Integer.parseInt(buyMoney);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BUY_CHAR_ERROR.getMessage());
+        }
+    }
+
+    public static void validateBuyMoneyIsMinus(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException(BUY_MINUS_ERROR.getMessage());
+        }
+    }
+
+    public static void validateBuyMoneyIsNotThousand(int money) {
+        if (money % LOTTO_COST.getValue() != 0) {
+            throw new IllegalArgumentException(BUY_CONDITION_ERROR.getMessage());
+        }
+    }
+
+}

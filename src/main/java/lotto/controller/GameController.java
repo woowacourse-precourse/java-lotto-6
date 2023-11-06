6a2977e92346ae314e33391b03feca7dfd8f464c
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 18:18:42 +0900

    feat: 로또 당첨 번호 승지 기능 구현

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,5 +1,49 @@
 package lotto.controller;
 
-public class GameController {
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
+import lotto.model.LottoMatchService;
+import lotto.model.LottoService;
+import lotto.model.Ranking;
+import lotto.view.ResultView;
 
+public class GameController {
+    private final LottoService lottoService = new LottoService();
+    private final LottoMatchService lottoMatchService = new LottoMatchService();
+    private final int ticketCount;
+    private final List<Lotto> lottoList;
+    private final List<Integer> winningNumbers;
+
+    public GameController() {
+        this.ticketCount = lottoService.calculateTicketCount();
+        this.lottoList = lottoService.createLottos(ticketCount);
+        this.winningNumbers = InputController.inputWinningNumbers();
+    }
+
+    public void run() {
+        try {
+            start();
+            displayWinners();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void start() {
+        ResultView.printTicketCount(ticketCount);
+        ResultView.printLottos(lottoList);
+    }
+
+    private void displayWinners() {
+        List<Ranking> rankings = lottoMatchService.checkWinningNumbers(lottoList, winningNumbers);
+        Map<Ranking, Long> resultMap = rankings.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        ResultView.printSuccessResult();
+        ResultView.printResults(resultMap);
+    }
 }

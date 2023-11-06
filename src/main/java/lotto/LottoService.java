a2079bff61735ca0276561d1055f8e4c1be3a7b5
JisooPyo <audwl326@gmail.com>
2023-11-07 00:08:20 +0900

    feat(LottoService): 입력받은 금액으로 몇 개의 로또를 샀는지 구한다.

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -4,11 +4,11 @@ public class LottoService {
 	InputValidator inputValidator = new InputValidator();
 	Long money;
 
-	public int getNumberOfLottoTickets(String cost) throws IllegalArgumentException {
+	public long getNumberOfLottoTickets(String cost) throws IllegalArgumentException {
 		inputValidator.isComposedOfNumbers(cost);
 
 		money = Long.parseLong(cost);
 		inputValidator.isMultiplesOfThousand(money);
-		return 0;
+		return money / 1000;
 	}
 }

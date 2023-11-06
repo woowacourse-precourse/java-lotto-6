8bdf2371c84894e933501e9ad5064c0e50c0182e
hayannn <dlgkdis801@naver.com>
2023-11-06 23:08:56 +0900

    feat(InputView, PlayerBuyingPayException): 구입 금액 입력의 예외 처리 기능 구현

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -9,4 +9,16 @@ public class InputView {
             if (buyingPrice.isEmpty()) {
                 throw new IllegalArgumentException("[ERROR] 구입 금액을 입력해주세요.");
             }
+            playerBuyingPayException.checkPlayerBuyingPay(buyingPrice);
+            return InputNumberParser.StringToInt(buyingPrice);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위의 숫자로 입력해야 합니다.");
+        }
+    }
+
+
+    public String playerLottoNumbersConsole() {
+        String lottoNumbers = Console.readLine();
+        return lottoNumbers;
+    }
 }

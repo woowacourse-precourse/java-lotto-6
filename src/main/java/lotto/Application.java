52a62d82bc50b27a1cd4f69f9abbc792e710804e
FutureCircuit <137764369+FutureCircuit@users.noreply.github.com>
2023-11-06 21:16:06 +0900

    feat: 구입금액 입력 기능 추가 로또 구입금액을 입력받고, 1000의 배수가 아닐 경우 예외 처리하는 코드를 추가하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+
+        try {
+            int num = Integer.parseInt(Console.readLine());
+            if(num % 1000 != 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+        catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] 구입금액은 1000의 배수여야 합니다.");
+        }
+
+
     }
 }

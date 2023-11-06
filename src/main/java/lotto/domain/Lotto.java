47429a625cbb6b1e3014692936523f01323a1d7e
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:19:34 +0900

    feat: Lotto 클래스 정적 팩토리 메서드 구현 및 검증 로직 반영

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,6 +10,15 @@ public class Lotto {
         this.numbers = new ArrayList<>(numbers);
     }
 
+    public static Lotto of(List<LottoNumber> numbers) {
+        validateLottoNumber(numbers);
+        return new Lotto(numbers);
+    }
+
+    private static void validateLottoNumber(List<LottoNumber> numbers) {
+        validateLottoNumbersLength(numbers);
+    }
+
     private static void validateLottoNumbersLength(List<LottoNumber> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호의 총 길이는 6이어야 합니다.");

bb857e1b67d5e97b37e842737a92836c09bef747
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 13:11:10 +0900

    feat: WinningLotto validate 메서드

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -7,7 +7,20 @@ public class WinningLotto {
     private final int bonus;
 
     public WinningLotto(List<Integer> numbers, int bonus) {
+        validate(numbers);
         this.numbers = numbers;
         this.bonus = bonus;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException();
+        }
+        if (numbers.stream().anyMatch(number -> number<1 || number>45)) {
+            throw new IllegalArgumentException();
+        }
     }
 }

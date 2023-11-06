b652a9a2420df7a5df20b1500e2f7f6973e30caf
sangeun-b <baek0007@algonquinlive.com>
2023-11-06 22:55:02 +0900

    feat: 구입 금액만큼 생성된 로또들을 리스트에 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,11 +1,21 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
-    
+
+    public List<Lotto> makeLottoList(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0; i<amount; i++) {
+            Lotto lotto = new Lotto(generateLotto());
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
     public List<Integer> generateLotto() {
         List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1,45,6);
         return lotto;

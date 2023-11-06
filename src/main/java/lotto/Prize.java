7f05463a8d61f8d12145779da826ad29078fed09
Hajuuu <hpt0127@naver.com>
2023-11-06 19:07:54 +0900

    feat(Prize): winningAmount 메서드 작성
    당첨 번호와 보너스 번호가 일치하는 개수에 따라 당첨 금액을 반환하는 winningAmount 메서드 작성

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,24 +1,38 @@
 package lotto;
 
 public enum Prize {
-    THREE(5000, "3개 일치 (5,000원)"),
-    FOUR(50000, "4개 일치 (50,000원)"),
-    FIVE(1500000, "5개 일치 (1,500,000원)"),
-    BONUS(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    SIX(2000000000, "6개 일치 (2,000,000,000원)");
+    THREE(3, 5000, "3개 일치 (5,000원)"),
+    FOUR(4, 50000, "4개 일치 (50,000원)"),
+    FIVE(5, 1500000, "5개 일치 (1,500,000원)"),
+    BONUS(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIX(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
-    private final int prize;
+    static final int LOTTO_COUNT = 6;
+    private final int count;
+    private final int amount;
 
     private final String information;
 
-    Prize(int prize, String information) {
-        this.prize = prize;
+    Prize(int count, int amount, String information) {
+        this.count = count;
+        this.amount = amount;
         this.information = information;
     }
 
-    public int getPrize() {
-        return prize;
+    public static int winningAmount(int matchWinningNumber, int matchBonusNumber) {
+        if (matchWinningNumber == Prize.BONUS.count) {
+            bonusNumber(matchBonusNumber);
+        }
+        for (Prize prize : Prize.values()) {
+            if (prize.count == matchWinningNumber) {
+                return prize.amount;
+            }
+        }
+        return 0;
     }
 
+    public static void bonusNumber(int matchBonusNumber) {
+
+    }
 }
 

e0b657ba0e3d559da9c2135241f45674698eb5cc
dmdrk1414 <seungchan141414@gmail.com>
2023-11-06 22:50:26 +0900

    [Style(lotto)] 주석 삭제

--- src/main/java/lotto/domain/user/User.java
+++ src/main/java/lotto/domain/user/User.java
@@ -6,9 +6,6 @@ import lotto.domain.lotto.LottoEnvelop;
 import lotto.domain.num.LottoTargetNumResults;
 import lotto.utill.Input;
 
-/**
- * 사용자 클래스
- */
 public class User {
     private LottoEnvelop lottoEnvelop;
     private Statistic statistic;
@@ -29,13 +26,7 @@ public class User {
         this.lottoTargetNumResults = lottoTargetNumResults;
     }
 
-    /**
-     * 돈을 지불한다.
-     *
-     * @return
-     */
     public Integer payMoney() {
-        // TODO: 11/6/23 예외 처리.
         useMoney = inputMoney();
 
         ValidateException.isMultipleOf1000(useMoney);
@@ -43,30 +34,15 @@ public class User {
         return useMoney;
     }
 
-    /**
-     * 로또를 받는다.
-     *
-     * @param lottoEnvelop 로또들의 모임
-     */
     public void takeLottoEnvelop(LottoEnvelop lottoEnvelop) {
         this.lottoEnvelop = lottoEnvelop;
     }
 
 
-    /**
-     * 로또를 보여준다.
-     *
-     * @return
-     */
     public StringBuilder showLottoEnvelop() {
         return lottoEnvelop.giveInformation();
     }
 
-    /**
-     * 로또결과의 당첨에 대한 통계를 한다.
-     *
-     * @return
-     */
     public StringBuilder showStatisticLottoResult(LottoTargetNumResults lottoTargetNumResults) {
         takeTargetNumResults(lottoTargetNumResults);
         statistic = Config.statistic(lottoEnvelop, lottoTargetNumResults);
@@ -74,11 +50,6 @@ public class User {
         return statistic.show();
     }
 
-    /**
-     * 로또결과의 당첨에 대한 총 수익률을 계산한다.
-     *
-     * @return
-     */
     public StringBuilder calculateTotalRate() {
         totalWinMoney = statistic.getTotalWinMoney();
 
@@ -88,6 +59,4 @@ public class User {
     private Integer inputMoney() {
         return Input.InputNumber();
     }
-
-
 }

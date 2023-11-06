037e60ed0376c30124284cee386da5d57a73c2ca
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-07 00:36:48 +0900

    feat(OutputView): 로또 구매 갯수와 구매한 로또 번호 출력 기능 추가
    docs: 로또 구매내역 출력 기능 추가 체크 완료

---
+++ src/main/java/lotto/Controller/LottoRaffle.java
@@ -0,0 +1,76 @@
+package lotto.Controller;
+
+import lotto.Domain.BonusNumber;
+import lotto.Domain.LottoPapers;
+import lotto.Domain.WinningCalculator;
+import lotto.Util.Converter;
+import lotto.Domain.Lotto;
+import lotto.Domain.LottoAmount;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
+public class LottoRaffle {
+    LottoAmount lottoAmount = new LottoAmount();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    Converter converter = new Converter();
+    WinningCalculator winningCalculator = new WinningCalculator();
+    Lotto lotto;
+    private BonusNumber bonusNumber;
+    private LottoPapers lottoPapers;
+    private int amount;
+
+    public void run(){
+        inputBuyAmount();
+        maikingLottoPapers();
+        outputLottoPapers();
+        inputWinningNumber();
+        inputBonusNumber();
+        outputWinningCount();
+        outputROI();
+    }
+    private void outputWinningCount(){
+        winningCalculator.calculateMatchNumbers(lottoPapers.getLottoPapers()
+                , lotto.getNumbers(), bonusNumber.getBonusNumber());
+        outputView.printWinningCount(winningCalculator.getWinningCount());
+    }
+
+    private void outputROI(){
+        outputView.printROI(winningCalculator.calculateROI(amount));
+    }
+    private void inputBuyAmount(){
+        try {
+            amount = lottoAmount.initLottoAmount(converter.convertAmount(inputView.printLottoAmount()));
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            inputBuyAmount();
+        }
+    }
+
+    private void maikingLottoPapers(){
+        this.lottoPapers = new LottoPapers(amount);
+    }
+
+    private void outputLottoPapers(){
+        outputView.printBuyMessage(amount, lottoPapers.getLottoPapers());
+    }
+
+    private void inputWinningNumber(){
+        try {
+            lotto = new Lotto(converter.convertLotto(inputView.printInputWinningNumber()));
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            inputWinningNumber();
+        }
+    }
+
+    private void inputBonusNumber(){
+        try {
+            bonusNumber = new BonusNumber(converter.convertBonusNumber(inputView.printInputBonusNumber())
+                    , lotto.getNumbers());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            inputBonusNumber();
+        }
+    }
+}

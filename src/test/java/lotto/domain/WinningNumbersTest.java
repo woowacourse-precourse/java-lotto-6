a08ce6cc91f49883685684335ad1f727dcabf5ed
nhlee98 <e5slnh9876@gmail.com>
2023-11-06 16:23:01 +0900

    test: 올바른 당첨 번호를 확인하는 테스트 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -27,5 +28,24 @@ public class WinningNumbersTest {
         assertThatThrownBy(() -> new WinningNumbers(winningLotto, bonusLottoNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호에 포함될 수 없습니다.");
+    }
+
+    static Stream<Arguments> provideValidWinningNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 7),
+                Arguments.of(List.of(45, 44, 43, 42, 41, 40), 39)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidWinningNumbers")
+    void 올바른_당첨_번호로_당첨_번호를_생성할_수_있다(List<Integer> winningNumbers, int bonusNumber) {
+        // given
+        Lotto winningLotto = new Lotto(winningNumbers);
+        LottoNumber bonusLottoNumber = LottoNumber.valueOf(bonusNumber);
+
+        // when & then
+        assertThatCode(() -> new WinningNumbers(winningLotto, bonusLottoNumber))
+                .doesNotThrowAnyException();
     }
 }

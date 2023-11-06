6d5a1c7d85ef0420d818039a88ee9ff2def5d44a
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-06 20:43:28 +0900

    feature(outputview): 게임 플레이어 로또 출력 함수 구현 - 매개변수로 로또 리스트를 받아 차례대로 출력한다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String PLAYER_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+    public static void printPlayerLotto(List<Lotto> playerLotto){
+        Integer playerLottoCount = playerLotto.size();
+        System.out.println(playerLottoCount + PLAYER_LOTTO_COUNT_MESSAGE);
+        for(Lotto lotto : playerLotto){
+            System.out.println(lotto.getNumbers());
+        }
+    }
+}

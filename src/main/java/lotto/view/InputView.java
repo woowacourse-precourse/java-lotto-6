1eca9a51430c4ec1b8674519f4f78bbc493dc463
5jeong <wjdgns4385@naver.com>
2023-11-07 00:37:27 +0900

    [Feat] 잘못된 값을 입력할때 다시 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.ConverterUtil;
+import lotto.validator.InputPurchaseAmountValidator;
+import lotto.validator.InputWinningNumberValidator;
 
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -9,23 +12,37 @@ public class InputView {
 
 
     public int inputPurchaseAmount() {
-        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
-        int purchaseAmount = Integer.parseInt(Console.readLine());
-        System.out.println();
-        return purchaseAmount;
+        while (true) {
+            try {
+                System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+                String purchaseAmount = Console.readLine();
+                InputPurchaseAmountValidator.validatePurchaseAmount(purchaseAmount);
+                System.out.println();
+                return ConverterUtil.convertStringToInt(purchaseAmount);
+            } catch (IllegalArgumentException e) {
+            }
+        }
     }
 
     public String inputWinningNumber() {
-        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
-        String winningNumber = Console.readLine();
-        System.out.println();
-        return winningNumber;
+        while (true) {
+            try {
+                System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+                String winningNumber = Console.readLine();
+                InputWinningNumberValidator.validateWinningNumber(winningNumber);
+                System.out.println();
+                return winningNumber;
+            } catch (IllegalArgumentException e) {
+            }
+        }
     }
 
     public int inputBonusNumber() {
+
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
-        int bonusNumber = Integer.parseInt(Console.readLine());
+        String bonusNumber = Console.readLine();
+        InputWinningNumberValidator.validateBonusNumber(bonusNumber);
         System.out.println();
-        return bonusNumber;
+        return ConverterUtil.convertStringToInt(bonusNumber);
     }
 }

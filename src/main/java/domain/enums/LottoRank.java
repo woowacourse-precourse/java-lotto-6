e899d7574627647f1b4bf38fb4a9c7ae33312dac
mango <esudatcom@gmail.com>
2023-11-06 23:01:05 +0900

    feat: 등수반환 LottoRank 구현

---
+++ src/main/java/domain/enums/LottoRank.java
@@ -0,0 +1,41 @@
+package domain.enums;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    NONE(0, 0, false);
+
+    private final int matchCount;
+    private final int prize;
+    private final boolean bonusMatch;
+
+    LottoRank(int matchCount, int prize, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public static LottoRank valueOf(int matchCount, boolean bonusMatch) {
+        for (LottoRank rank : values()) {
+            if (rank.matchCount == matchCount && (!rank.bonusMatch || bonusMatch)) {
+                return rank;
+            }
+        }
+        return NONE;
+    }
+
+    public boolean isBonusMatch(){
+        return bonusMatch;
+    }
+
+    public int getMatchCount(){
+        return matchCount;
+    }
+
+    public int getPrize(){
+        return prize;
+    }
+}

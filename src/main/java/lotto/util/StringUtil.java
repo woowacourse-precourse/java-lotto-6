41203e1464dce26fa2a39fa49f778f335323cb38
donghoonyeom <dua5410773@naver.com>
2023-11-07 04:29:08 +0900

    refactor(StringUtil): 메서드 분리 및 가독성 향상
    StringUtil 클래스의 `convertToIntegerList` 메서드에서 중복 코드를 제거하고 가독성을 높이기 위해 `splitInput`이라는 별도의 private 메서드로 기능을 분리하였습니다. 이로써 코드의 유지보수성을 높이고 가독성을 향상시켰습니다.

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -1,14 +1,19 @@
-package lotto;
+package lotto.util;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringUtil {
     public static List<Integer> convertToIntegerList(String input, String delimiter) {
-        return Arrays.stream(input.split(delimiter))
+        return splitInput(input, delimiter)
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private static Stream<String> splitInput(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter));
     }
 }

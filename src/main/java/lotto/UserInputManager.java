2b040efddad874f79c92e4b6a129246fb4cf6b5f
PINGPINGYEE <owb7994@nate.com>
2023-11-06 11:09:02 +0900

    사용자 input 관련 string에서 int로 형변환하는 기능 추가(Utils)

---
+++ src/main/java/lotto/UserInputManager.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class UserInputManager {
+    String purchasePriceInput = readLine();
+    int purchasePrice;
+
+
+}
---
+++ src/main/java/lotto/Utils.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Utils {
+    public static int setPriceType(String purchasePriceInput) {
+        int purchasePrice = 0;
+        for (int i = purchasePriceInput.length() - 1, numberOfDigit = 1; i >= 0; i--, numberOfDigit *= 10) {
+            purchasePrice += ((purchasePriceInput.charAt(i) - '0') * numberOfDigit);
+        }
+
+        return purchasePrice;
+    }
+}

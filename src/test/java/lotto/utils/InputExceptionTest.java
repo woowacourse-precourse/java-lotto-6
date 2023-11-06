f174f9ce5b658ceac91c88ec344dccda23b032c4
euniiiii <hee9920@naver.com>
2023-11-06 13:54:08 +0900

    test: 입력에 대한 번호의 개수가 맞는지 확인하는 예외처리 테스트 작성

--- src/test/java/lotto/utils/InputExceptionTest.java
+++ src/test/java/lotto/utils/InputExceptionTest.java
@@ -191,4 +191,71 @@ class InputExceptionTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(PrintMessage.NOT_DUPLICATION);
     }
+
+    @Test
+    @DisplayName("리스트의 길이를 확인하는 메서드 테스트")
+    void checkSizeTest() {
+        // given
+        List<List<Integer>> lists = new ArrayList<>(List.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(2, 6, 10, 14, 18, 22),
+                List.of(5, 10, 20, 30, 40, 45)
+        ));
+
+        // when
+        Throwable throwable1 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(0));
+        });
+        Throwable throwable2 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(1));
+        });
+        Throwable throwable3 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(2));
+        });
+
+        // then
+        assertThat(throwable1).doesNotThrowAnyException();
+        assertThat(throwable2).doesNotThrowAnyException();
+        assertThat(throwable3).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("리스트의 길이를 확인하는 메서드 예외처리 테스트")
+    void checkSizeExceptionTest() {
+        // given
+        List<List<Integer>> lists = new ArrayList<>(List.of(
+                List.of(1),
+                List.of(1, 2),
+                List.of(1, 2, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5, 6, 7)
+        ));
+
+        // when
+        Throwable throwable1 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(0));
+        });
+        Throwable throwable2 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(1));
+        });
+        Throwable throwable3 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(2));
+        });
+        Throwable throwable4 = catchThrowable(() -> {
+            InputException.checkSize(lists.get(3));
+        });
+
+        // then
+        assertThat(throwable1)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PrintMessage.ONLY_SIX_NUMBER);
+        assertThat(throwable2)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PrintMessage.ONLY_SIX_NUMBER);
+        assertThat(throwable3)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PrintMessage.ONLY_SIX_NUMBER);
+        assertThat(throwable4)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PrintMessage.ONLY_SIX_NUMBER);
+    }
 }

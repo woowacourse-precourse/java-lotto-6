513d293df8916cbface134ce4a403c30c906fa88
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-07 06:04:28 +0900

    refactor: 로또 가격 enum화 1. 로또 가격을 enum화 하여 타 클래스에서 접근 가능하게 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,16 +8,26 @@ import java.util.ArrayList;
 public class Lotto {
     private final List<Integer> numbers;
 
+    public enum LottoConstants{
+        LOTTO_PRICE(1000);
+        private final int price;
+        LottoConstants(int price){
+            this.price = price;
+        }
+        public int getPrice(){
+            return price;
+        }
+    }
+
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        isValidLottoNumbers(numbers);
         List<Integer> sortableNumbers = new ArrayList<>(numbers);
         Collections.sort(sortableNumbers);
         this.numbers = Collections.unmodifiableList(sortableNumbers);
     }
 
 
-
-    private void validate(List<Integer> numbers) {
+    private void isValidLottoNumbers(List<Integer> numbers) {
         if (numbers == null || numbers.size() != 6 || !numbers.stream().allMatch(num -> num >= 1 && num <= 45)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }

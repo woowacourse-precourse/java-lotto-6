8f6d1fad37c5df8cf0171f34c5ac6600180c4fb3
khabh1 <kshababe1@gmail.com>
2023-11-06 15:27:08 +0900

    test(LottoWinningResult): 등수별 로또 당첨 개수 메시지 생성 기능 테스트 추가

---
+++ src/main/java/lotto/domain/LottoWinningResult.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoWinningResult {
+
+    public static LottoWinningResult of(List<Lotto> lottos, WinningLotto winningLotto) {
+        return null;
+    }
+
+    public String getLottoRankMessages() {
+        return null;
+    }
+
+    public float calculateWinningRate() {
+        return 0;
+    }
+}
---
+++ src/test/java/lotto/domain/LottoWinningResultTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoWinningResultTest {
+
+    @DisplayName("로또 당첨 통계 올바르게 생성")
+    @Test
+    void generateLottoRankMessage() {
+        // Given
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(Lotto.from(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(Lotto.from(List.of(1, 2, 3, 4, 5, 7)));
+        WinningLotto winningLotto = WinningLotto.from(List.of(1, 2, 3, 4, 5, 6));
+        winningLotto.addBonusNumber(7);
+
+        // When
+        LottoWinningResult lottoWinningResult = LottoWinningResult.of(lottos, winningLotto);
+        String lottoRankMessage = lottoWinningResult.getLottoRankMessages();
+
+        // Then
+        assertThat(lottoRankMessage).contains("3개 일치 (5,000원) - 0개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                "6개 일치 (2,000,000,000원) - 1개");
+    }
+}

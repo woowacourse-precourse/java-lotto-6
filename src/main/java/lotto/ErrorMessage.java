41359cb7a8a5ba26cd1ef82bf99d6298082e14b4
이병수 <qudtn0315@naver.com>
2023-11-06 16:10:50 +0900

    chore (ErrorMessage) : enum문으로 정리하여 삭제

--- src/main/java/lotto/ErrorMessage.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public class ErrorMessage {
-    public static final String ERROR_LOTTO_NUMBER_SIZE = "[ERROR] 로또 번호 갯수가 6개가 아닙니다.";
-    public static final String ERROR_LOTTO_NUMBER_DUPLICATE = "[ERROR] 중복된 숫자가 발생했습니다.";
-    public static final String ERROR_NUMBER = "[ERROR] 숫자를 입력하세요.";
-    public static final String ERROR_INSERT_WON = "[ERROR] 1,000원 단위로 입력하세요.";
-    public static final String ERROR_NUMBER_SIZE_MESSAGE = "[ERROR] 1부터 45 사이의 숫자를 입력하세요.";
-}

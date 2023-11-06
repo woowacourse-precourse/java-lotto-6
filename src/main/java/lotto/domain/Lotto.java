4c8b7478967ad21c3897f67e8b37cc0d8c6b1d4f
khabh1 <kshababe1@gmail.com>
2023-11-06 10:05:25 +0900

    refactor(Lotto): 검증 기능에 예외 처리 추가
    예외 사항:
    1. 로또 번호 개수가 올바르지 않을 경우
    2. 로또에 중복된 숫자가 있을 경우
    3. 유효 범위를 벗어난 번호가 있을
    경우

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,12 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.StringJoiner;
+import lotto.config.LottoConfig;
+import lotto.exception.InvalidLottoNumberCountException;
+import lotto.exception.LottoNumberDuplicatedException;
+import lotto.exception.LottoNumberOutOfRangeException;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,13 +21,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LottoConfig.LOTTO_NUMBER_COUNT) {
+            throw new InvalidLottoNumberCountException(numbers.size());
         }
+        if (new HashSet<>(numbers).size() != LottoConfig.LOTTO_NUMBER_COUNT) {
+            throw new LottoNumberDuplicatedException();
+        }
+        numbers.forEach(this::validateNumber);
     }
 
     // TODO: 추가 기능 구현
     public void addedToResult(StringJoiner result) {
         result.add(numbers.toString());
+    }
+
+    private void validateNumber(int number) {
+        if (number < LottoConfig.MIN_LOTTO_NUMBER || number > LottoConfig.MAX_LOTTO_NUMBER) {
+            throw new LottoNumberOutOfRangeException(number);
+        }
     }
 }

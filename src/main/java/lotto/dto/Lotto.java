b79384fdfd898ae8f3e14d40c4718212e425fa06
sehee <tpgml701@naver.com>
2023-11-06 12:37:33 +0900

    feat: Lotto 중복 방지 구현

--- src/main/java/lotto/dto/Lotto.java
+++ src/main/java/lotto/dto/Lotto.java
@@ -1,20 +1,30 @@
 package lotto.dto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+    private final List<Integer> lottoNumbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> lottoNumbers) {
+        validateSize(lottoNumbers);
+        validateDuplicate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개가 필요합니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> lottoNumbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(lottoNumbers);
+        if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
 }

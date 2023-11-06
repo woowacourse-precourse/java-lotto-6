5287e0c49cc7b2dafedc22eb18527959f0725eca
SeungGwa123 <skwan123@naver.com>
2023-11-07 02:15:07 +0900

    Fix: Fix lottoBuyMoney
    lottoBuyMoney의 함수부분에 로또를 구매하도록 하였고 로또 갯수의 변수를 새로 생성

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -2,13 +2,16 @@ package Input;
 
 import Except.Except;
 import Output.Output;
+import lotto.BuyLotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
     private static int money;
+    private static int lotto_number;
     private static Output output = new Output();
     private static Except except = new Except();
+    private static BuyLotto buylotto = new BuyLotto();
 
     public void lottoBuyMoney() {
         do {
@@ -16,6 +19,8 @@ public class Input {
             String money_string = readLine();
             money = except.outOfRangeMoney(money_string);
         } while (money < 0);
-        output.buyNumberPaperAns(money / 1000);
+        lotto_number = money / 1000;
+        output.buyNumberPaperAns(lotto_number);
+        buylotto.buyAllLotto(lotto_number);
     }
 }

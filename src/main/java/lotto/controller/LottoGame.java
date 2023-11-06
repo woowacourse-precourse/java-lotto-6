c95df262bc81c926f8fc428a660e4000e0aad0b0
leesh <qwqw8019@naver.com>
2023-11-06 16:21:11 +0900

    feat: 로또 게임 진행 기능 구현

---
+++ src/main/java/lotto/controller/LottoGame.java
@@ -0,0 +1,17 @@
+package lotto.controller;
+
+import lotto.service.LottoGameService;
+
+public class LottoGame {
+    public static void run() {
+
+        LottoGameService.purchaseLotto();
+
+        LottoGameService.requestWinningLotto();
+
+        LottoGameService.figureOutLottoGame();
+
+        LottoGameService.calculateTotalRateOfReturn();
+        
+    }
+}

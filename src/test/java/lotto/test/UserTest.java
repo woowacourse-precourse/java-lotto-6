d7b694b11e5b4c52aded068742003091a84bcb1e
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-06 16:10:32 +0900

    Test: 입력 금액에 따른 예외 테스트

--- src/test/java/lotto/test/UserTest.java
+++ src/test/java/lotto/test/UserTest.java
@@ -1,6 +1,7 @@
 package lotto.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 
@@ -9,10 +10,20 @@ import org.junit.jupiter.api.Test;
 
 import lotto.domain.Lotto;
 import lotto.domain.User;
+import lotto.utils.BillException;
 import lotto.service.UserLottoService;
 
 
 public class UserTest {
+    @Test
+    @DisplayName("로또 구매 금액이 12100원이라면 IllegalArgumentException이 발생하여야한다")
+    void billToLottoTest() {
+        //given
+        int Bill = 12100;
+        //then
+        assertThatThrownBy(() -> BillException.isBillValid(Bill))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
     @Test
     @DisplayName("로또 구매 금액이 12000원이라면 생성된 로또의 수는 12개여야한다")
     void lottoCountTest() {

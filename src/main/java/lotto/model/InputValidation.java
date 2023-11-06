0f8e101c3663fa229dfc87ae8b30e94a8b3c8a15
gahyeon <1000ji1@naver.com>
2023-11-06 18:49:14 +0900

    Feat : 번호 입력 시 숫자 범위 예외처리 구현

--- src/main/java/lotto/model/InputValidation.java
+++ src/main/java/lotto/model/InputValidation.java
@@ -2,11 +2,20 @@ package lotto.model;
 
 public class InputValidation {
     private static final int DIVIDED_AMOUNT = 1000;
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 45;
     private static final String INVALID_AMOUNT_MESSAGE = "구입 금액은 1,000원 단위로 입력해야 합니다.";
+    private static final String INVALID_RANGE_MESSAGE = "1~45 사이의 숫자를 입력해야 합니다.";
 
     public static void validatePurchaseAmount(int amount) {
         if (amount % DIVIDED_AMOUNT != 0) {
             throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+    }
+
+    public static void validateNumberInRange(int number) {
+        if (number < START_RANGE || number > END_RANGE) {
+            throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
         }
     }
 }

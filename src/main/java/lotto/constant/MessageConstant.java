cc5e1f3109a8384201ff3c8b16e66628bab9f1de
jang010505 <jang010505@inu.ac.kr>
2023-11-07 07:30:57 +0900

    style: 오타 수정

--- src/main/java/lotto/constant/MessageConstant.java
+++ src/main/java/lotto/constant/MessageConstant.java
@@ -1,7 +1,7 @@
 package lotto.constant;
 
 public enum MessageConstant {
-    LOTTO_PRICE_INPUT_OUTPUT_MESSAGE("구입금액을 입력해 주세요."),
+    LOTTO_PRICE_INPUT_MESSAGE("구입금액을 입력해 주세요."),
     LOTTO_NUMBER_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요.");
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,11 @@ package lotto.view;
 
 import static lotto.constant.MessageConstant.BONUS_NUMBER_INPUT_MESSAGE;
 import static lotto.constant.MessageConstant.LOTTO_NUMBER_INPUT_MESSAGE;
-import static lotto.constant.MessageConstant.LOTTO_PRICE_INPUT_OUTPUT_MESSAGE;
+import static lotto.constant.MessageConstant.LOTTO_PRICE_INPUT_MESSAGE;
 
 public class InputView {
     public static void inputLottoPriceMessage() {
-        System.out.println(LOTTO_PRICE_INPUT_OUTPUT_MESSAGE.getMessage());
+        System.out.println(LOTTO_PRICE_INPUT_MESSAGE.getMessage());
     }
 
     public static void inputLottoNumber() {

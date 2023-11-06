2bef59500bf6cd929c36d5a98f4d802eed8c9a90
sunghyun <shlim0287@naver.com>
2023-11-06 15:59:19 +0900

    feat : 로또 구입금액 입력값 검증 메서드 추가.

---
+++ src/main/java/lotto/validator/ValidatePurchaseAmount.java
@@ -0,0 +1,43 @@
+package lotto.validator;
+
+public class ValidatePurchaseAmount {
+
+    static final int LOTTO_PRICE=1000;
+
+    public static void validatePurchaseAmount(String buyerInput){
+        validateEmpty(buyerInput);
+        validateConvertable(buyerInput);
+        validateDividableThousand(buyerInput);
+        validateSmallThanThousand(buyerInput);
+
+    }
+
+    private static void validateDividableThousand(String buyerInput) {
+        if(!canDivide(buyerInput)){
+            throw new IllegalArgumentException("[ERROR] 로또의 한장 가격은 1000원 입니다.");
+        }
+    }
+
+    private static boolean canDivide(String buyerInput){
+        return Integer.parseInt(buyerInput) % LOTTO_PRICE ==0;
+    }
+
+    private static void validateSmallThanThousand(String buyerInput) {
+        int purchaseAmount = Integer.parseInt(buyerInput);
+        if(purchaseAmount<LOTTO_PRICE){
+            throw new IllegalArgumentException("[ERROR] 로또의 한장 가격은 1000원 입니다.");
+        }
+    }
+
+    private static void validateConvertable(String buyerInput) {
+        if(!buyerInput.matches("-?\\d+")){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void validateEmpty(String buyerInput) {
+        if(buyerInput.isBlank()){
+            throw new IllegalArgumentException("[ERROR] 값을 입력해주세요.");
+        }
+    }
+}

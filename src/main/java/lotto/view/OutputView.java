3d59abd2b12d2cfc29e2b525f5fab5c1c126ca44
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-06 15:10:26 +0900

    fix: 배열 int형의 rank를 hashMap으로 변경됨에따라 발생되는 오류 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,10 +3,10 @@ package lotto.view;
 import static lotto.enums.OutputMessages.*;
 import static lotto.enums.Prize.*;
 
+import java.util.HashMap;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.WinningDetails;
-import lotto.enums.OutputMessages;
 
 public class OutputView {
 
@@ -21,15 +21,15 @@ public class OutputView {
     }
 
     public static void showWinningDetails(WinningDetails winningDetails){
-        int[] ranks = winningDetails.getRank();
+        HashMap<Integer,Integer> rank= winningDetails.getRank();
 
         System.out.println(WINNING_STATISTICS.getMessage());
         System.out.println(DIVIDED_LINE.getMessage());
-        System.out.printf(MATCH_NUMBERS.getMessage(), FIFTH_RANK.getMatchNumbers(), FIFTH_RANK.getMoneyToString(), ranks[FIFTH_RANK.getRank()]);
-        System.out.printf(MATCH_NUMBERS.getMessage(), FOURTH_RANK.getMatchNumbers(), FOURTH_RANK.getMoneyToString(), ranks[FOURTH_RANK.getRank()]);
-        System.out.printf(MATCH_NUMBERS.getMessage(), THIRD_RANK.getMatchNumbers(), THIRD_RANK.getMoneyToString(), ranks[THIRD_RANK.getRank()]);
-        System.out.printf(MATCH_FIVE_NUMBERS_BONUS_NUMBERS.getMessage(), SECOND_RANK.getMatchNumbers(), SECOND_RANK.getMoneyToString(), ranks[SECOND_RANK.getRank()]);
-        System.out.printf(MATCH_NUMBERS.getMessage(), FIRST_RANK.getMatchNumbers(), FIRST_RANK.getMoneyToString(), ranks[FIRST_RANK.getRank()]);
+        System.out.printf(MATCH_NUMBERS.getMessage(), FIFTH_RANK.getMatchNumbers(), FIFTH_RANK.getMoneyToString(), rank.get(FIFTH_RANK.getRank()));
+        System.out.printf(MATCH_NUMBERS.getMessage(), FOURTH_RANK.getMatchNumbers(), FOURTH_RANK.getMoneyToString(), rank.get(FOURTH_RANK.getRank()));
+        System.out.printf(MATCH_NUMBERS.getMessage(), THIRD_RANK.getMatchNumbers(), THIRD_RANK.getMoneyToString(), rank.get(THIRD_RANK.getRank()));
+        System.out.printf(MATCH_FIVE_NUMBERS_BONUS_NUMBERS.getMessage(), SECOND_RANK.getMatchNumbers(), SECOND_RANK.getMoneyToString(), rank.get(SECOND_RANK.getRank()));
+        System.out.printf(MATCH_NUMBERS.getMessage(), FIRST_RANK.getMatchNumbers(), FIRST_RANK.getMoneyToString(),rank.get(FIRST_RANK.getRank()));
     }
 
     public static void earningRate(float earningRate){

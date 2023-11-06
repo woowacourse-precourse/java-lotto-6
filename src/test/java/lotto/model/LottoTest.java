4c065bd14cef8f59ea344a97ce426a9c25b406a1
hYe807n <rlagkdus55@gmail.com>
2023-11-06 12:44:19 +0900

    Test: 당첨 숫자가 0~2 개인 경우 0 을 반환하는 단위 테스트 구현

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,17 +1,29 @@
 package lotto.model;
 
-import lotto.model.Lotto;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    static Stream<Arguments> provideAnswerLotto() {
+        return Stream.of(
+            Arguments.of(new Lotto(List.of(1, 7, 8, 9, 10, 11)), 1),
+            Arguments.of(new Lotto(List.of(1, 2, 8, 9, 10, 11)), 2),
+            Arguments.of(new Lotto(List.of(40, 7, 8, 9, 10, 11)), 3)
+        );
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -22,9 +34,8 @@ class LottoTest {
     @DisplayName("로또 번호의 범위가 1부터 45가 아니라면 예외가 발생힌다.")
     @Test
     void createLottoByOverRange() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Lotto(List.of(1, 2, 3, 4, 5, 50));
-        });
+        assertThatIllegalArgumentException().isThrownBy(() ->
+            new Lotto(List.of(1, 2, 3, 4, 5, 50)));
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -41,15 +52,24 @@ class LottoTest {
             .containsExactly(1, 2, 3, 4, 5, 6);
     }
 
-    @DisplayName("로또 번호에 보너스 번호가 포함되어 있으면 true를 반환한다.")
-    @Test
-    void lottoHasBonus() {
-        Assertions.assertTrue(new Lotto(List.of(1,2,3,4,5,6)).hasBonus(5));
+    @DisplayName("당첨 숫자가 {1} 라면 0 을 반환한다.")
+    @ParameterizedTest
+    @MethodSource("provideAnswerLotto")
+    void checkScore(Lotto inputAnswer) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.calculateScore(inputAnswer))
+            .isEqualTo(0);
     }
 
-    @DisplayName("로또 번호에 보너스 번호가 포함되어 있으면 false를 반환한다.")
+    @DisplayName("로또 번호에 보너스 번호가 포함되어 있으면 true 를 반환한다.")
+    @Test
+    void lottoHasBonus() {
+        Assertions.assertTrue(new Lotto(List.of(1, 2, 3, 4, 5, 6)).hasBonus(5));
+    }
+
+    @DisplayName("로또 번호에 보너스 번호가 포함되어 있으면 false 를 반환한다.")
     @Test
     void lottoHasNotBonus() {
-        Assertions.assertTrue(new Lotto(List.of(1,2,3,4,5,6)).hasBonus(7));
+        Assertions.assertFalse(new Lotto(List.of(1, 2, 3, 4, 5, 6)).hasBonus(7));
     }
 }

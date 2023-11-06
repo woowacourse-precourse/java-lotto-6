6243f0eadeb283f74a6672708deccd0e5c456d38
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 06:13:44 +0900

    feat(LottoException) : 로또 예외처리 뼈대 작성

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,18 @@
+package lotto.exception;
+
+import java.util.List;
+
+public class LottoException {
+
+    public void isDuplicate(List<Integer> input) {
+    }
+
+    public void isSize(List<Integer> input) {
+    }
+
+    public void isRightRange(List<Integer> input) {
+    }
+
+    public void isAscending(List<Integer> input) {
+    }
+}

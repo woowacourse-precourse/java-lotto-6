dcf4b4dc1a8617ebc5ce8cc2836056dee43b3c85
김승우 <tmd8633@naver.com>
2023-11-06 19:42:57 +0900

    Refactor : 결과 집계 기능 분리 -> Result 객체로

--- src/main/java/lotto/enums/LottoEnum.java
+++ src/main/java/lotto/enums/LottoEnum.java
@@ -9,77 +9,40 @@ import static lotto.LottoConst.LOTTO_PRICE;
 
 public enum LottoEnum {
 
-    W1(LOTTO_COUNT, 0,2000000000, 0),
-    W2(LOTTO_COUNT-1, 1,30000000, 0),
-    W3(LOTTO_COUNT-1, 0,1500000, 0),
-    W4(LOTTO_COUNT-2, 0,50000, 0),
-    W5(LOTTO_COUNT-3, 0,5000, 0);
+    W1(LOTTO_COUNT, 0,2000000000),
+    W2(LOTTO_COUNT-1, 1,30000000),
+    W3(LOTTO_COUNT-1, 0,1500000),
+    W4(LOTTO_COUNT-2, 0,50000),
+    W5(LOTTO_COUNT-3, 0,5000);
 
 
     private final int winCount;
     private final int bonusCount;
     private final int reward;
-    private int result;
 
 
-    LottoEnum(int winCount, int bonusCount, int reward, int result) {
+    LottoEnum(int winCount, int bonusCount, int reward) {
         this.winCount = winCount;
         this.bonusCount = bonusCount;
         this.reward = reward;
-        this.result = result;
     }
 
-    public int getResult() {
-        return result;
+    public int getWinCount() {
+        return winCount;
     }
 
-    public static void printResult(int amount) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        printEnums();
-        System.out.println("총 수익률은 " + getRewardPercent(amount) + "입니다.");
+    public int getReward() {
+        return reward;
     }
 
-    private static void printEnums() {
-        LottoEnum[] values = LottoEnum.values();
-        Arrays.sort(values, Comparator.reverseOrder());
-        for (LottoEnum value : values) {
-            System.out.println(getResultString(value));
-        }
-    }
-
-    private static String getResultString(LottoEnum value) {
-        DecimalFormat df = new DecimalFormat("###,###");
-        if (value.equals(W2)){
-            return value.winCount + "개 일치, 보너스 볼 일치 (" + df.format(value.reward) + "원) - " + value.result + "개";
-        }
-        return value.winCount + "개 일치 (" + df.format(value.reward) + "원) - " + value.result + "개";
-    }
-
-    private static String getRewardPercent(int amount) {
-        int total = getTotal();
-        double totalDouble = 100.0 * total / (amount * LOTTO_PRICE);
-        DecimalFormat df = new DecimalFormat("0.0%");
-        return df.format(totalDouble / 100.0);
-    }
-
-    private static int getTotal() {
-        int total = 0;
-        LottoEnum[] values = LottoEnum.values();
-        for (LottoEnum value : values) {
-            total += value.reward * value.result;
-        }
-        return total;
-    }
-
-    public static void getLottoEnum(int count, int bonusCount) {
+    public static LottoEnum getLottoEnum(int count, int bonusCount) {
         LottoEnum[] values = LottoEnum.values();
         for (LottoEnum value : values) {
             if (value.winCount == count && value.bonusCount == bonusCount) {
-                value.result++;
-                return;
+                return value;
             }
         }
+        return null;
     }
 
 

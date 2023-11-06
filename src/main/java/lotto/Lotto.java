bdc2fa03e9c9b14629491ccf35eb44fa9c170b57
JIHYE <jdh324@naver.com>
2023-11-07 03:54:16 +0900

    refactor: Lotto 클래스 리팩토링
    로또 번호를 오름차순으로 정렬하는 코드를 생성자에 추가했습니다. 그리고 값을 하드코딩하지 않기 위한 처리 및 toString 메서드를 override 했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,29 @@
 package lotto;
 
+import static lotto.LottoConstants.LOTTO_NUMBERS;
+
+import exception.LottoException;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_NUMBERS) {
+            throw new IllegalArgumentException(LottoException.NOT_LOTTO_NUMBERS);
         }
     }
 
-    // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

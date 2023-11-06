ea835d0b1e57d03bf997c0f14c0b9fb2668c6a89
박정현 <ros28081@naver.com>
2023-11-06 20:06:24 +0900

    feat(LottoRankManagement): 결과 출력 기능 추가

--- src/main/java/lotto/LottoRankManagement.java
+++ src/main/java/lotto/LottoRankManagement.java
@@ -17,4 +17,17 @@ public class LottoRankManagement {
     public void add(LottoRank rank) {
         rankCounts.put(rank, rankCounts.get(rank) + 1);
     }
+
+    public void printResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (LottoRank rank : LottoRank.values()) {
+            int count = rankCounts.get(rank);
+            if (rank != LottoRank.NONE && count > 0) {
+                System.out.printf("%d개 일치 %s (%,d원) - %d개\n",
+                        rank.getMatchCount(), rank == LottoRank.SECOND ? ", 보너스 볼 일치" : "",
+                        rank.getPrize(), count);
+            }
+        }
+    }
 }

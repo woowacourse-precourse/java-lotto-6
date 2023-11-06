6b879290ab6a08b554b9c03254becc413205432d
K-KY <kyuyoungk@naver.com>
2023-11-07 02:48:40 +0900

    Test(TypeChangerTest) : 타입 변경 테스트

--- src/test/java/lotto/TypeChangerTest.java
+++ src/test/java/lotto/TypeChangerTest.java
@@ -6,8 +6,20 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TypeChangerTest {
+    private final TypeChanger typeChanger = TypeChanger.getTypeChanger();
     @Test
     void 쉼표를_기준으로_분리한다() {
-        assertThat(TypeChanger.stringToListWithComma("1,2,3,4,5,6")).isEqualTo(List.of("1","2","3","4","5","6"));
+        assertThat(typeChanger.stringToListWithComma("1,2,3,4,5,6")).isEqualTo(List.of("1","2","3","4","5","6"));
+    }
+
+    @Test
+    void 문자열을_숫자로_바꾼다() {
+        assertThat(typeChanger.stringToInteger("1")).isInstanceOf(Integer.class);
+    }
+
+    @Test
+    void String리스트를_Integer리스트로() {
+        List<String> numbers = typeChanger.stringToListWithComma("1,2,3,4,5,6");
+        typeChanger.genericToInteger(numbers).forEach(number -> assertThat(number).isInstanceOf(Integer.class));
     }
 }

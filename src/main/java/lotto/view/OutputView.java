1462e4ffbc8ec529601aac1497155bc10b79fb60
kang jihyeong <her4386@gmail.com>
2023-11-06 23:50:01 +0900

    [feat] 발행한 로또 번호 출력 기능

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import java.util.List;
+
+public class OutputView {
+
+    public static void printLottoNumbers(List<Integer> lotto) {
+        System.out.println(lotto);
+    }
+
+}

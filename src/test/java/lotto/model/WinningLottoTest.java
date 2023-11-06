43325818688cb028156e4fb707d2c2d178277140
jihun <pjh2996@naver.com>
2023-11-06 14:45:15 +0900

    test : 로또 당첨 번호 입력시 유효성 검사 작동 테스트

---
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverSize() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 1~45범위를 넘은 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverRange() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 46, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

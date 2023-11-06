5fcc05b82a81a658d9214f2c257fb1afa14df176
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-06 18:16:07 +0900

    feat(LottoService, LottoController) : 로또 번호는 1~45 정수를 무작위로 생성하는 숫자  구현

---
+++ src/main/java/lotto/global/constant/LottoConstant.java
@@ -0,0 +1,8 @@
+package lotto.global.constant;
+
+public class LottoConstant {
+
+    public static final int LOTTO_NUMBER_RANGE_START = 1;
+    public static final int LOTTO_NUMBER_RANGE_END = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+}
---
+++ src/main/java/lotto/utils/LottoService.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.List;
+
+import static lotto.global.constant.LottoConstant.*;
+
+public class LottoService {
+
+    public Lotto generateUserLotto(){
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
+                LOTTO_NUMBER_RANGE_START, LOTTO_NUMBER_RANGE_END, LOTTO_NUMBER_COUNT);
+        return new Lotto(randomNumbers);
+    }
+}

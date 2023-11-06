3b3da7dd8bc1fca59e1a6935aab61896a9d2641e
HYOAN <aspqll@naver.com>
2023-11-07 01:21:56 +0900

    feat(lotto): 구입금액과 당첨번호에 대한 예외처리 테스트코드 구현

--- src/test/java/lotto/domain/UserInputCheckerTest.java
+++ src/test/java/lotto/domain/UserInputCheckerTest.java
@@ -3,10 +3,14 @@ package lotto.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputCheckerTest {
     private UserInputChecker userInputChecker;
+    private List<Integer> winningNumbersTest = Arrays.asList(1, 2, 3, 4, 6, 6);
 
     @BeforeEach
     void sutUp() {
@@ -14,13 +18,33 @@ class UserInputCheckerTest {
     }
 
     @Test
-    void 입력값_자료형_확인() {
-        assertThrows(IllegalArgumentException.class, () -> userInputChecker.checkInt("a"));
+    void 입력된_당첨번호_범위_예외처리_1() {
+        int invalidNumber = 46;
+        assertEquals(0, userInputChecker.checkNumberInRange(invalidNumber));
+
     }
 
     @Test
-    void 구입금액_확인() {
+    void 입력된_당첨번호_범위_예외처리_2() {
+        int invalidNumber = 0;
+        assertEquals(0, userInputChecker.checkNumberInRange(invalidNumber));
+
+    }
+
+    @Test
+    void 입력된_당첨번호_중복_예외처리() {
+        assertEquals(true, userInputChecker.hasDuplicateWinningNumbers(winningNumbersTest));
+    }
+
+    @Test
+    void 구입금액_단위_예외처리() {
         assertThrows(IllegalArgumentException.class, () -> userInputChecker.checkInputUnit(10010));
     }
+
+    @Test
+    void 입력값_자료형_예외처리() {
+        assertThrows(IllegalArgumentException.class, () -> userInputChecker.checkInt("a"));
+    }
+
 
 }

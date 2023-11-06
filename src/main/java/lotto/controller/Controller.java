92d7ef7af4965e0eee80fe7866ed63bd2a0743a6
1000zoo <cjswldn99@gmail.com>
2023-11-07 03:32:58 +0900

    feat(Controller): 전체 프로그램을 관리하는 controller

---
+++ src/main/java/lotto/controller/Controller.java
@@ -0,0 +1,54 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.model.LottoResult;
+import lotto.model.PurchaseAmount;
+import lotto.model.Rank;
+import lotto.view.input.AmountInput;
+import lotto.view.input.Input;
+
+public class Controller {
+
+    private final static String PROFITS_RATE_MESSAGE = "총 수익률은 %.1%입니다.";
+
+    private final PurchaseAmount purchaseAmount;
+    private final LottoManager lottoManager;
+    private final LottoResult lottoResult;
+
+    public Controller() {
+        purchaseAmount = new PurchaseAmount(readAmount());
+        lottoManager = new LottoManager(purchaseAmount);
+        lottoResult = new LottoResult();
+    }
+
+    public void start() {
+        saveResults();
+        printResults();
+        printProfitRate();
+    }
+
+    private double calculateProfitRate() {
+        return (double) lottoResult.getProfits() / purchaseAmount.purchaseAmount();
+    }
+
+    private void printProfitRate() {
+
+    }
+
+    private void printResults() {
+
+    }
+
+    private void saveResults() {
+        List<Rank> results = lottoManager.calculateResults();
+
+        for (Rank result : results) {
+            lottoResult.addOne(result);
+        }
+    }
+
+    private int readAmount() {
+        Input input = new AmountInput();
+        return Integer.parseInt(input.readLine());
+    }
+}

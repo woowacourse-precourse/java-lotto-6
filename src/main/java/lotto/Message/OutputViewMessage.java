9542b85d4c8825a294d55f2884cad3a90e049ae6
sami355-24 <ddung355@gmail.com>
2023-11-06 09:49:31 +0900

    refactor(Message): 출력하는 문구들을 관리하는 enum 클래스의 이름 수정

--- src/main/java/lotto/Message/OutputViewMessage.java
+++ src/main/java/lotto/Message/OutputViewMessage.java
@@ -3,11 +3,11 @@ package lotto.Message;
 public enum OutputViewMessage {
     PURCHASED_LOTTO_COUNT("\n%d를 구매하셨습니다."),
     WINNING_STATS("\n당첨 통계\n---"),
-    MATCH_5TH_PRIZE("3개 일치 (5,000원) - %d개"),
-    MATCH_4TH_PRIZE("4개 일치 (50,000원) - %d개"),
-    MATCH_3RD_PRIZE("5개 일치 (1,500,000원) - %d개"),
-    MATCH_2ND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    MATCH_1ST_PRIZE("6개 일치 (2,000,000,000원) - %d개"),
+    RESULT_5TH_PRIZE("3개 일치 (5,000원) - %d개"),
+    RESULT_4TH_PRIZE("4개 일치 (50,000원) - %d개"),
+    RESULT_3RD_PRIZE("5개 일치 (1,500,000원) - %d개"),
+    RESULT_2ND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    RESULT_1ST_PRIZE("6개 일치 (2,000,000,000원) - %d개"),
     EARNING_RATE("총 수익률은 %f입니다.");
 
     private String message;

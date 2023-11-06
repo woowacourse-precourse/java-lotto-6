e9e91874a89c0773cd2aebb673328b24540e37bd
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-11-06 15:32:40 +0900

    refactor : input 방식 변경

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,17 +3,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    Scanner scanner = new Scanner(System.in);
-
-  
 
     public int priceAmountInput() {
         int priceAmount;
 
         System.out.println("구입금액을 입력해 주세요.");
-        priceAmount = scanner.nextInt();
+        priceAmount = Integer.parseInt(Console.readLine());
         if (!priceAmountVaildation(priceAmount)) {
             throw new IllegalArgumentException("[ERROR] 입력값은 1000원 단위여야 합니다.");
         }
@@ -39,19 +37,28 @@ public class Input {
      */
     public Lotto winningNumberInput() {
         ArrayList<Integer> winningNumber = new ArrayList<Integer>();
-        StringTokenizer st = new StringTokenizer(scanner.nextLine());
-
+        
         System.out.println("당첨 번호를 입력해 주세요.");
+        StringTokenizer st = new StringTokenizer(Console.readLine(), ",");
         while (st.hasMoreTokens()) {
             int input = Integer.parseInt(st.nextToken());
             winningNumber.add(input);
         }
 
-        System.out.println("\n" + "보너스 번호를 입력해 주세요.");
-        int bonusNumber = scanner.nextInt();
-
+        
         return new Lotto(winningNumber);
     } 
+
+    /**
+     * 보너스 번호 입력 
+     * @return 보너스 번호
+     */
+    public int bonusNumberInput() {
+        System.out.println("\n" + "보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+
+        return bonusNumber;
+    }
 
 }
 

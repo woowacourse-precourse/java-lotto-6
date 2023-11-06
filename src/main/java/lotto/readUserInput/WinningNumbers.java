b410b6c66e24700da94afb8bd706f8e6853a5e3f
junyoun99 <juny2486@naver.com>
2023-11-06 20:15:19 +0900

    refactor: 당첨 번호 입력에 관한 유효성 검사 추가

--- src/main/java/lotto/readUserInput/WinningNumbers.java
+++ src/main/java/lotto/readUserInput/WinningNumbers.java
@@ -3,6 +3,7 @@ package lotto.readUserInput;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class WinningNumbers implements UserInput {
     public static List<Integer> numbers = new ArrayList<>();
@@ -13,7 +14,14 @@ public class WinningNumbers implements UserInput {
     }
 
     public String userInput() {
-        return Console.readLine();
+        String readWinningNumbers = Console.readLine();
+        try {
+            isInputContainComma(readWinningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또 번호는 공백 없이 6개의 숫자 사이에 콤마를 넣어야 합니다.");
+            readWinningNumbers = userInput();
+        }
+        return readWinningNumbers;
     }
 
     public String[] separateUserInput(String inputNumbers) {
@@ -25,8 +33,29 @@ public class WinningNumbers implements UserInput {
     }
 
     public void addWinningNumbers(String[] readNumbers) {
+        String[] readInputNumber = readNumbers;
         while(numbers.size() < 6) { // 상수로 표현 가능.
-            numbers.add(singleTypeConversion(readNumbers[numbers.size()]));
+            int singleNumber = singleTypeConversion(readInputNumber[numbers.size()]);
+            try {
+                isNumberInRange(singleNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                numbers.clear();
+                readInputNumber = separateUserInput(userInput());
+            }
+            numbers.add(singleNumber);
+        }
+    }
+
+    public void isInputContainComma(String userInput) {
+        if (!Pattern.matches("^\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}$", userInput)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isNumberInRange(int singleNumber) {
+        if (!((singleNumber >= 1) && (singleNumber <= 45))) {
+            throw new IllegalArgumentException();
         }
     }
 }

f7fba6d3bead79e086b7267d992941e1283b0ebc
shlihs95 <fhzptvh@gmail.com>
2023-11-06 21:56:17 +0900

    Update: 1. 게임 준비_입력 값을 관리할 InputView 생성 - InputView 클래스에서 값을 받는 메서드만 남기고, 형태를 변환하는 클래스는 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,29 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    Console console;
+    private static Console console;
 
-    public int readPurchasingAmount() {
-        String purchasingAmount = console.readLine();
-        return Integer.parseInt(purchasingAmount) / 1000;
-    }
-
-    public List<Integer> readWinningNumbers() {
-        List<Integer> winningNumbers = new ArrayList<>();
-        String readNumbers = console.readLine();
-        String[] splitNumbers = readNumbers.split(",");
-
-        for (String splitNumber : splitNumbers) {
-            int number = Integer.parseInt(splitNumber);
-            winningNumbers.add(number);
-        }
-
-        return winningNumbers;
-    }
-
-    public int readBonusNumber() {
-        String readNumber = console.readLine();
-        return Integer.parseInt(readNumber);
+    public String readInput() {
+        return console.readLine();
     }
 
 }

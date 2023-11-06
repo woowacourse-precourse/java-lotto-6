fca02811a06f3020788af32794cfa3e5e826a2c2
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 02:04:30 +0900

    feat(utils.StringUtils): 기능29. 문자열에서 특정 문자열이 몇 번 등작하는지 세어준다.
    countOccurrences 메서드는 테스트 코드를 작성할 때, 출력값 검증과 관련해서 문자열에서 특정 문자열이 몇 번 나왔는지 확인해야 하는 경우가 있어 기능구현 해주었습니다.

--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -1,4 +1,17 @@
 package lotto.utils;
 
 public class StringUtils {
+
+    public static int countOccurrences(String str, String target) {
+        int count = 0;
+        int idx = 0;
+
+        while ((idx = str.indexOf(target, idx)) != -1) {
+            idx += target.length();
+            count++;
+        }
+
+        return count;
+    }
+
 }

2e92b5819333ee8f2b65509538c7c965f35d2124
jang010505 <jang010505@inu.ac.kr>
2023-11-07 06:39:55 +0900

    feat: MessageConstant 객체 생성

---
+++ src/main/java/lotto/constant/MessageConstant.java
@@ -0,0 +1,17 @@
+package lotto.constant;
+
+public enum MessageConstant {
+    LOTTO_PRICE_INPUT_OUTPUT_MESSAGE("구입금액을 입력해 주세요."),
+    LOTTO_NUMBER_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    MessageConstant(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

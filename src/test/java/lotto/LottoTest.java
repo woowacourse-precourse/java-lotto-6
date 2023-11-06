fa6cc49f44402bfe74993b072bc0beac4b71eed1
Namyejun <yejun1148@gmail.com>
2023-11-06 17:36:08 +0900

    로또 정렬 기능 테스트 추가, 그런데 출제자의 Lotto 클래스에 대한 의도가 내가 이해한 거랑 다른 것 같다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -21,8 +22,14 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 오름차순으로 출력이 잘 되는지 확인한다.")
+    @Test
+    void printStringTest() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        Assertions.assertThat(new Lotto(List.of(6, 8, 1, 3, 5, 7)).printString())
+            .isEqualTo("[1, 3, 5, 6, 7, 8]");
+    }
 }

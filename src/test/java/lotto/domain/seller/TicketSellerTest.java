d38db0aabc5d7e1f24ee4e8d35d28e7bc1a3cbc1
DevFrog92 <yongsu1481@gmail.com>
2023-11-06 14:29:26 +0900

    test: implement TicketSeller unit test

---
+++ src/test/java/lotto/domain/seller/TicketSellerTest.java
@@ -0,0 +1,34 @@
+package lotto.domain.seller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lotto.domain.fake.FakeRandomNumber;
+import lotto.domain.generator.AutoTicketGenerator;
+import lotto.domain.generator.TicketGenerator;
+import lotto.domain.money.Money;
+import lotto.domain.ticket.Ticket;
+import lotto.util.RandomNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TicketSellerTest {
+        @DisplayName("성공: 주어진 금액으로 최대로 구매 가능한 티켓을 판매할 수 있다.")
+        @Test
+        void call_sellTo_will_return_Ticket() {
+                //given
+                List<Integer> lottoBalls = List.of(1, 2, 3, 4, 5, 6);
+                RandomNumber randomNumber = new FakeRandomNumber(lottoBalls);
+                TicketGenerator generator = new AutoTicketGenerator(randomNumber);
+                Seller seller = new TicketSeller(generator);
+                Money payment = new Money(BigDecimal.valueOf(6000));
+
+                //when
+                Ticket ticket = seller.sellTo(payment);
+
+                //then
+                assertThat(ticket).isNotNull();
+                assertThat(ticket.getNumOfLottoEntry()).isEqualTo(6);
+        }
+}

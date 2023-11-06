f374dc5597ca92a59e8eadac51604c9f165072fd
toychip <dlawnsgud427@naver.com>
2023-11-06 12:44:06 +0900

    feat: LottoController 뼈대 구성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+}

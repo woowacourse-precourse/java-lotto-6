b1dbeff77d07987788cfb208a0bce562a39115ec
Hanjaemo <hjm2530@gmail.com>
2023-11-06 16:20:36 +0900

    refactor(ProfitRateCalculatorTest): 테스트 설명 추가 및 마지막 개행 추가

--- src/test/java/lotto/domain/ProfitRateCalculatorTest.java
+++ src/test/java/lotto/domain/ProfitRateCalculatorTest.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,10 +7,10 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class ProfitRateCalculatorTest {
 
-    @DisplayName("")
+    @DisplayName("수익률을 계산하면 구입 금액을 총 상금으로 나눈 값을 반환한다.")
     @ParameterizedTest
     @CsvSource(value = {"8000, 5000, 62.5", "8000, 10000, 125.0", "7000, 1500000, 21428.6"})
-    void calculate(int purchaseAmount, long reward, double expected) {
+    void calculate_Success(int purchaseAmount, long reward, double expected) {
         // given
         ProfitRateCalculator profitRateCalculator = new ProfitRateCalculator();
 
@@ -20,7 +18,6 @@ class ProfitRateCalculatorTest {
         double profitRate = profitRateCalculator.calculate(purchaseAmount, reward);
 
         // then
-        Assertions.assertThat(profitRate).isEqualTo(expected);
+        Assertions.assertThat(Math.round(profitRate * 10) / 10.0).isEqualTo(expected);
     }
-
 }

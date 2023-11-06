bb07eb18ec9b7e2f1231752e51fb0305a55b14ca
kseenyoung <kseenyoung@gmail.com>
2023-11-06 23:43:24 +0900

    feat: 랜덤 로또 번호 생성

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -3,7 +3,9 @@ package lotto;
 public enum Message {
     ERROR_MESSAGE("[ERROR]"),
     ONE_HUNDRED_DIVIDE("1,000 단위로 입력해주세요."),
-    INPUT_VALUE_OVERHEAD("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INPUT_VALUE_OVERHEAD("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INPUT_MONEY("구입금액을 입력해 주세요."),
+    LOTTO_RANDOM_PRINT("개를 구매했습니다.");
 
     final String filed;
     Message(String filed){
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,23 +1,33 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
 
     private int money;
     private int ticketCount;
     private List<Lotto> lottos;
+    private final int LOTTO_NUMBER_COUNT = 6;
 
     User(){
         getMoney();
     }
 
     public void getRandomLotto(){
-
+        System.out.println("\n" + Message.LOTTO_RANDOM_PRINT);
+        for (int i = 0; i < ticketCount; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
     }
 
     private void getMoney(){
+        System.out.println(Message.INPUT_MONEY);
         while(true){
             try{
                 this.money = Integer.parseInt(Console.readLine());

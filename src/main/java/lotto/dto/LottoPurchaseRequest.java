38c58c6ce9e52ea31ea559642132fb681637cc03
dukcode <duk9741@gmail.com>
2023-11-06 14:28:18 +0900

    feat: LottoPurchaseRequest 구현
    - 구입 금액 숫자 아닐 시 예외 발생

--- src/main/java/lotto/dto/LottoPurchaseRequest.java
+++ src/main/java/lotto/dto/LottoPurchaseRequest.java
@@ -1,4 +1,18 @@
 package lotto.dto;
 
 public class LottoPurchaseRequest {
+
+    private int purchaseAmount;
+
+    public LottoPurchaseRequest(String purchaseAmount) {
+        try {
+            this.purchaseAmount = Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자여야 합니다.");
+        }
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
 }

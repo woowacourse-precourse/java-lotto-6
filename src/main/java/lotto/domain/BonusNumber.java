e771c3c22fc5caca70ee7c9597ef0916d4fabe81
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:35:09 +0900

    feat: BonusNumber 클래스 생성자 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -3,4 +3,8 @@ package lotto.domain;
 public class BonusNumber {
 
     private Integer bonusNumber;
+
+    public BonusNumber(Integer bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
 }

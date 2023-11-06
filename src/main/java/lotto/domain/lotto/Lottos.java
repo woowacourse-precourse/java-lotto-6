514ef32b7d83af70244dd51918cd74b12183a218
JuSeong1130 <jsmoon1130@naver.com>
2023-11-06 11:01:07 +0900

    feat(Lottos) : 당첨결과 계산하는 기능 구현

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -1,6 +1,10 @@
 package lotto.domain.lotto;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import lotto.domain.result.WinningRank;
+import lotto.domain.result.WinningResult;
 
 public class Lottos {
 
@@ -8,5 +12,14 @@ public class Lottos {
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public WinningResult calculateWinningResult(WinningLotto winningLotto) {
+        Map<WinningRank, Integer> winningResult = new EnumMap<>(WinningRank.class);
+        for (Lotto lotto : lottos) {
+            WinningRank winningRank = winningLotto.findWinningRank(lotto);
+            winningResult.put(winningRank, winningResult.getOrDefault(winningRank,0) + 1);
+        }
+        return new WinningResult(winningResult);
     }
 }

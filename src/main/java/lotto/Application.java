ce6a1d0db2063dc4c1996a1f24cedecf95e1672a
IM-GYURI <kgulr0517@gmail.com>
2023-11-07 03:28:20 +0900

    feat: [ERROR] 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,12 +3,14 @@ package lotto;
 import lotto.controller.MainController;
 
 public class Application {
+    private static final String ERROR = "[ERROR] ";
+
     public static void main(String[] args) {
         try {
             MainController mainController = new MainController();
             mainController.start();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(ERROR + e.getMessage());
         }
     }
 }

41e2bc0b9adf601744e1f70e2a18ea17e64f511e
CHAHANS <herodev0129@gmail.com>
2023-11-06 13:39:39 +0900

    refector(domain): Lotto의 중복값 확인 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -7,12 +8,24 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateChecker(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void duplicateChecker(List<Integer> numbers){
+        List<Integer> checkedList = new ArrayList<>();
+        for (int i = 0; i<numbers.size(); i++) {
+            if (checkedList.contains(numbers.get(i))) {
+                checkedList.add(numbers.get(i));
+            } else {
+                throw new IllegalArgumentException();
+            };
         }
     }
 

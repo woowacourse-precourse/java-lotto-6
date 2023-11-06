518c88a2358c0a33a22b6c4257497574e14dbc08
JW0321 <digneotn123@gmail.com>
2023-11-07 00:58:48 +0900

    feat: 구매 횟수 계산 기능 구현

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -3,17 +3,27 @@ package lotto.model;
 public class Purchase { //도메인 로직과 관련된 구입 금액 검증 및 저장, 구입 금액에 따른 구매 횟수 계산
 
     private final int purchaseAmount;
+    private final int purchaseNumber;
 
     public Purchase(String purchaseAmount) {
         int purchaseMoney = Integer.parseInt(purchaseAmount);
         validatePurchaseAmount(purchaseMoney);
         this.purchaseAmount = purchaseMoney;
+        this.purchaseNumber = purchaseMoney / 1000;
     }
 
     private void validatePurchaseAmount(int purchaseMoney) {
         if (purchaseMoney % 1000 != 0) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public int getPurchaseNumber() {
+        return purchaseNumber;
     }
 
 

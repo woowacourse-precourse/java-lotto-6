753db39f16cc85160e2adfe221263761b9663952
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-06 15:13:46 +0900

    test(SetCollectionTest) : Set Collection 학습 테스트 구현
    1. 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는지 구현함
    2. Set에 들어있는지 확인하고 그 값을 대조함

--- src/test/java/lotto/study/SetCollectionTest.java
+++ src/test/java/lotto/study/SetCollectionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -25,7 +26,7 @@ public class SetCollectionTest {
 
     @DisplayName("Set의 size()를 활용해 Set의 크기를 확인하는지")
     @Test
-    void confirmSetSize() {
+    void confirmSetSizeTest() {
         // given
         Integer expectedSetSize = 3;
 
@@ -36,8 +37,19 @@ public class SetCollectionTest {
     @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는지")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void contains(Integer number) {
+    void containsTest(Integer number) {
         // given & when & then
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는지")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueOrFalseTest(Integer number, Boolean expectedResult) {
+        // given
+        Boolean result = numbers.contains(number);
+
+        // when & then
+        assertThat(result).isEqualTo(expectedResult);
     }
 }

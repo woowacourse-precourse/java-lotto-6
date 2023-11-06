4b1be16ea70b7678e2774a4d4f560ab1d398db8a
Park Eunhyeong <silbroeh@gmail.com>
2023-11-06 14:00:37 +0900

    refactor : PrizeGrade클래스 - matchCount 필드 삭제

--- src/main/java/lotto/PrizeGrade.java
+++ src/main/java/lotto/PrizeGrade.java
@@ -1,23 +1,17 @@
 package lotto;
 
 public enum PrizeGrade {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000),
     NO_PRIZE(0);
 
-    private final int matchCount;
     private final int prizeMoney;
 
-    PrizeGrade(int matchCount, int prizeMoney) {
-        this.matchCount = matchCount;
+    PrizeGrade(int prizeMoney) {
         this.prizeMoney = prizeMoney;
-    }
-
-    public int getMatchCount() {
-        return this.matchCount;
     }
 
     public int getPrizeMoney() {

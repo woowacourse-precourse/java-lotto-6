8960113ab7ad377effce911266762b5ba8c40de6
JaeHunJang <wuddlaa@gmail.com>
2023-11-06 22:27:03 +0900

    feat(Constant): 상수 클래스 생성

---
+++ src/main/java/lotto/config/Constant.java
@@ -0,0 +1,8 @@
+package lotto.config;
+
+public class Constant {
+    public static String NUMBER_DELIMITER = ",";
+    public static String PATTERN_NUMBER = "^[\\d]*$";
+    public static String PATTERN_NUMBERS = "^[\\d" + NUMBER_DELIMITER + "]*$";
+
+}

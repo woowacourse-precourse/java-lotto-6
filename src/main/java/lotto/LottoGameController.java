221115ab31e1034b739c532504383e485ed73e03
wda067 <mg6359@naver.com>
2023-11-07 06:25:17 +0900

    refactor(LottoGameController): 입력 메서드에 검증 메서드 추가

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -4,35 +4,39 @@ import java.util.List;
 
 public class LottoGameController {
 
-    private int purchaseAmount;
+    private String purchaseAmount;
     private int purchaseCount;
     private String winningNumbers;
     private List<Integer> winningNumbersList;
-    private int bonusNumber;
+    private String bonusNumber;
 
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
     LottoGameService lottoGameService = new LottoGameService();
 
     public void playGame() {
-        inputLottoPurchaseAmount();
-        setLottoPurchaseCount();
-        printPurchaseQuantity();
-        generateLottoNumbers();
-        printPurchasedLottoNumbers();
-        setWinningNumbers();
-        setWinningNumbersList();
-        setBonusNumber();
-        updateWinningCount();
-        printWinningStatistics();
-        printProfitRatio();
+        try {
+            inputLottoPurchaseAmount();
+            setLottoPurchaseCount();
+            printPurchaseQuantity();
+            generateLottoNumbers();
+            printPurchasedLottoNumbers();
+            setWinningNumbers();
+            setBonusNumber();
+            updateWinningCount();
+            printWinningStatistics();
+            printProfitRatio();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    void inputLottoPurchaseAmount() {
-        purchaseAmount = Integer.parseInt(inputView.inputPurchaseAmount());
+    public void inputLottoPurchaseAmount() {
+        purchaseAmount = inputView.inputPurchaseAmount();
+        lottoGameService.validatePurchaseAmount(purchaseAmount);
     }
 
-    void setLottoPurchaseCount() {
+    public void setLottoPurchaseCount() {
         purchaseCount = lottoGameService.getLottoTicketCount(purchaseAmount);
     }
 
@@ -52,20 +56,21 @@ public class LottoGameController {
 
     public void setWinningNumbers() {
         winningNumbers = inputView.inputWinningNumbers();
-    }
-
-    public void setWinningNumbersList() {
+        lottoGameService.validateWinningNumbers(winningNumbers);
         winningNumbersList = lottoGameService.convertWinningNumbersToCollection(winningNumbers);
+        lottoGameService.validateWinningNumbersList(winningNumbersList);
     }
 
     public void setBonusNumber() {
-        bonusNumber = Integer.parseInt(inputView.inputBonusNumber());
+        bonusNumber = inputView.inputBonusNumber();
+        lottoGameService.validateBonusNumber(bonusNumber);
     }
 
     public void updateWinningCount() {
         for (List<Integer> purchasedLottoNumber : lottoGameService.getPurchasedLottoNumbers()) {
             lottoGameService.updateWinningCount(
-                    lottoGameService.determineWinningRank(purchasedLottoNumber, winningNumbersList, bonusNumber)
+                    lottoGameService.determineWinningRank(purchasedLottoNumber, winningNumbersList,
+                            Integer.parseInt(bonusNumber))
             );
         }
     }

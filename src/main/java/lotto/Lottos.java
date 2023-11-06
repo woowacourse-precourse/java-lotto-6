a0e85d360d98ddbf0c6ddd850a07a61ff0149e30
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-11-07 04:02:43 +0900

    feat(Lottos): 당첨된 로또 등수 판별 기능 추가
    Lottos 객체가 가지고 있는 구입한 로또가 몇 등인지 판별하여 Map<Integer(등수), (Integer(그 등수의 로또 개수)> 형태로 반환하는 메서드 countMatches(Lotto winningLotto, int bonusNumber)를 추가함.

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lottos {
-    private List<Lotto> lottos;
+    private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
@@ -17,5 +19,36 @@ public class Lottos {
         return lottos.get(index);
     }
 
+    public Map<Integer, Integer> countMatches(Lotto winningLotto, int bonusNumber) {
+        Map<Integer, Integer> rankCounts = new HashMap<>();
+        for (int i = 0; i < 6; i++) {
+            rankCounts.put(i, 0);
+        }
+
+        for (Lotto lotto : lottos) {
+            int rank = CountWinningRanks(lotto.countMatchWithWinningLotto(winningLotto));
+            if (rank == 2 && lotto.contains(bonusNumber)) {
+                rank = 3;
+            }
+            rankCounts.put(rank, rankCounts.get(rank) + 1);
+        }
+        return rankCounts;
+    }
+
+    private static int CountWinningRanks(int matchesCount) {
+        if (matchesCount == 6) {
+            return 1;
+        }
+        if (matchesCount == 5) {
+            return 2;
+        }
+        if (matchesCount == 4) {
+            return 4;
+        }
+        if (matchesCount == 3) {
+            return 5;
+        }
+        return 0;
+    }
 
 }

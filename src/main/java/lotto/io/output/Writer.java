3ae21e5d4a7aa37187821029e175744279fdd562
minisyu <iswater.kim@gmail.com>
2023-11-06 11:39:15 +0900

    refactor: Writer에 "[ERROR] " prefix 추가

--- src/main/java/lotto/io/output/Writer.java
+++ src/main/java/lotto/io/output/Writer.java
@@ -1,6 +1,8 @@
 package lotto.io.output;
 
 public class Writer implements StdWriter {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
     @Override
     public void writeLine(String message) {
         System.out.println(message);

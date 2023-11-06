5110819a013370a67d12ce552093b9a2c993846d
pgd <rudeh1253@gmail.com>
2023-11-06 18:29:51 +0900

    refactor: 에러 메시지 내용 변경
    구입 금액 검증에서만 사용할 수 있었던 에러 메시지를
    여러 상황에서 사용될 수 있도록 변경

--- src/main/java/lotto/configure/ErrorMessages.java
+++ src/main/java/lotto/configure/ErrorMessages.java
@@ -2,7 +2,7 @@ package lotto.configure;
 
 public enum ErrorMessages {
     ERROR_MESSAGE_COMMON_PREFIX("[ERROR]"),
-    PURCHASE_NUMBER_IS_NOT_AN_INTEGER(ERROR_MESSAGE_COMMON_PREFIX + " 구입 금액은 정수여야 합니다."),
+    NOT_AN_INTEGER(ERROR_MESSAGE_COMMON_PREFIX + " 정수를 입력하셔야 합니다."),
     PURCHASE_NUMBER_IS_NEGATIVE(ERROR_MESSAGE_COMMON_PREFIX + " 구입 금액은 0 이상이어야 합니다."),
     PURCHASE_NUMBER_IS_NOT_MULTIPLE_OF_1000(ERROR_MESSAGE_COMMON_PREFIX + " 구입 금액은 1,000으로 나누어 떨어져야 합니다."),
     WINNING_NUMBERS_ARE_NOT_SIX(ERROR_MESSAGE_COMMON_PREFIX + " 당첨 번호는 정확히 6개의 숫자로 이루어져야 합니다."),

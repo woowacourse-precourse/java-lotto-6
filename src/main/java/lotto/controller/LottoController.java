69ca2255dadaf1e26a777c96ef4c9b9e0e285011
elyudwo <dudqk9696@naver.com>
2023-11-07 00:08:42 +0900

    [Feat] : Controller 생성자 기능 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final WinningLottoStorage winningLottoStorage;
+    private PurchasedLotto purchasedLotto;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.winningLottoStorage = new WinningLottoStorage();
+    }
+}

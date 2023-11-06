9f5f583ce7d51467f631f2e960a75427f2b230e2
jongmin Lee <rjsqktod1@gmail.com>
2023-11-06 20:00:58 +0900

    feat(enum): enum 값의 개수 계산 기능 추가
    결과 출력을 위해 enum 값 개수 계산 후 반환
    
    Signed-off-by: jongmin Lee <rjsqktod1@gmail.com>

--- src/main/java/lotto/domain/enums/LottoPrize.java
+++ src/main/java/lotto/domain/enums/LottoPrize.java
@@ -1,19 +1,24 @@
 package lotto.domain.enums;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public enum LottoPrize {
-    FIRST(1, 2_000_000_000),
-    SECOND(2, 30_000_000),
-    THIRD(3, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(5, 5_000),
-    NONE(0, 0);
+    FIRST(1, 2_000_000_000, 6),
+    SECOND(2, 30_000_000, 5),
+    THIRD(3, 1_500_000, 5),
+    FOURTH(4, 50_000, 4),
+    FIFTH(5, 5_000, 3),
+    NONE(0, 0, 0);
 
     private final int rank;
     private final int prizeMoney;
+    private final int matchCount;
 
-    LottoPrize(int rank, int prizeMoney) {
+    LottoPrize(int rank, int prizeMoney, int matchCount) {
         this.rank = rank;
         this.prizeMoney = prizeMoney;
+        this.matchCount = matchCount;
     }
 
     public static LottoPrize valueOf(int matchCount, boolean bonusMatch) {
@@ -25,11 +30,14 @@ public enum LottoPrize {
         return NONE;
     }
 
-    @Override
-    public String toString() {
-        return "LottoPrize{" +
-                "rank=" + rank +
-                ", prizeMoney=" + prizeMoney +
-                '}';
+    public static Map<LottoPrize, Integer> lottoPrizeCount(List<LottoPrize> allLottoPrizes) {
+        List<LottoPrize> ranks = List.of(LottoPrize.FIFTH, LottoPrize.FOURTH, LottoPrize.THIRD, LottoPrize.SECOND, LottoPrize.FIRST);
+
+        Map<LottoPrize, Integer> lottoPrizeAndCount = new HashMap<>();
+        for (LottoPrize rank : ranks) {
+            int count = Collections.frequency(allLottoPrizes, rank);
+            lottoPrizeAndCount.put(rank, count);
+        }
+        return lottoPrizeAndCount;
     }
 }

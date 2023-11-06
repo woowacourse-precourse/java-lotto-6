757223603590363510a14c3f94c6cc9e8628b1e1
HoeSeong123 <ghltjd3@gmail.com>
2023-11-06 11:19:01 +0900

    test(LottoTest) : Lotto 클래스에 대한 테스트 코드 작성

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -26,5 +27,30 @@ class LottoTest {
     void createLottoByOutOfRangeNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 특정 숫자를 포함하고 있으면 참을 리턴한다.")
+    @Test
+    void createLottoContainsNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.contains(1)).isEqualTo(true);
+    }
+
+    @DisplayName("로또 번호가 특정 숫자를 포함하고 있지 않으면 거짓을 리턴한다.")
+    @Test
+    void createLottoNotContainsNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.contains(7)).isEqualTo(false);
+    }
+
+    @DisplayName("로또 두 개를 비교하여 몇 개의 숫자가 일치하는 지 확인한다.")
+    @Test
+    void createTwoLottoAndCompare() {
+        Lotto testLotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto testLotto2 = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+
+        assertThat(testLotto1.compareLotto(testLotto2)).isEqualTo(3);
     }
 }

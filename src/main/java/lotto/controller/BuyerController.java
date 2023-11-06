a781521fc5f0883f4da3cc42346751a927f8b5dd
oxdjww <oxdjww@gmail.com>
2023-11-07 04:23:24 +0900

    refactor : BuyerService에서 View에 대한 접근 제거(BuyerController로 이전)

--- src/main/java/lotto/controller/BuyerController.java
+++ src/main/java/lotto/controller/BuyerController.java
@@ -1,11 +1,22 @@
 package lotto.controller;
 
+import static lotto.view.constants.ViewMessage.GET_PURCHASE_AMOUNT_MESSAGE;
+
 import lotto.domain.Buyer;
+import lotto.exception.LottoException;
 import lotto.service.BuyerService;
+import lotto.view.OutputView;
 
 public class BuyerController {
     private final BuyerService buyerService = new BuyerService();
     public Buyer createBuyer() {
-        return buyerService.generateBuyer();
+        while (true) {
+            OutputView.printMessage(GET_PURCHASE_AMOUNT_MESSAGE);
+            try {
+                return buyerService.generateBuyer();
+            } catch (LottoException e) {
+                OutputView.printMessage(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/lotto/service/BuyerService.java
+++ src/main/java/lotto/service/BuyerService.java
@@ -1,23 +1,12 @@
 package lotto.service;
 
-import static lotto.view.constants.ViewMessage.GET_PURCHASE_AMOUNT_MESSAGE;
-
 import lotto.domain.Buyer;
-import lotto.exception.LottoException;
 import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class BuyerService {
 
     public Buyer generateBuyer() {
-        while (true) {
-            try {
-                OutputView.printMessage(GET_PURCHASE_AMOUNT_MESSAGE);
-                String inputAmount = InputView.readLine();
-                return Buyer.of(inputAmount);
-            } catch (LottoException e) {
-                OutputView.printMessage(e.getMessage());
-            }
-        }
+        String inputAmount = InputView.readLine();
+        return Buyer.of(inputAmount);
     }
 }

4ac7d284985c23b90192488da86919ae1567f54e
wonjun <wonjon0930@daum.net>
2023-11-06 20:21:51 +0900

    feat: add feature to check user input about winning lotto numbers contains delimiter(comma)

--- src/main/java/lotto/view/validator/WinningLottoNumbersInputValidator.java
+++ src/main/java/lotto/view/validator/WinningLottoNumbersInputValidator.java
@@ -1,5 +1,6 @@
 package lotto.view.validator;
 
+import lotto.view.constant.InputConstant;
 import lotto.view.exception.LottoInputException;
 import lotto.view.message.LottoInputExceptionMessage;
 
@@ -7,6 +8,7 @@ public class WinningLottoNumbersInputValidator {
 
     public void validate(String winningLottoNumbers){
         isNotEmpty(winningLottoNumbers);
+        containsDelimiter(winningLottoNumbers);
     }
 
     private void isNotEmpty(String winningLottoNumbers){
@@ -14,5 +16,13 @@ public class WinningLottoNumbersInputValidator {
             throw LottoInputException.of(LottoInputExceptionMessage.INPUT_IS_EMPTY);
         }
     }
+
+    private void containsDelimiter(String winningLottoNumbers){
+        if(!winningLottoNumbers.contains(InputConstant.WINNING_LOTTO_NUMBERS_DELIMITER)){
+            throw LottoInputException.of(LottoInputExceptionMessage.WINNING_LOTTO_NUMBERS_DOES_NOT_CONTAIN_DELIMITER);
+        }
+    }
+
+
 
 }

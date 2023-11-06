6d3eaf529b0759406790cb048356664a209da45b
star-candy <el0710@naver.com>
2023-11-06 17:55:31 +0900

    feat(lotto): 로또 당첨 번호 입력시 6자리를 넘거나 중복이 있을 경우 재입력

--- src/main/java/utility/UserInputException.java
+++ src/main/java/utility/UserInputException.java
@@ -1,11 +1,33 @@
 package utility;
 
+import java.util.HashSet;
+import java.util.List;
+import model.Transform;
+
 public class UserInputException {
 
-    public void lottoPriceInputValidate(int lottoPrice) {
-
+    public void lottoPriceValidate(int lottoPrice) {
         if (lottoPrice % 1000 != 0) {
             throw new IllegalArgumentException();
         }
     }
+
+    public void winningNumberValidate(String winningNumber) {
+        Transform transform = new Transform();
+        List<Integer> winningNumberList;
+        winningNumberList = transform.winningNumberToList(winningNumber);
+        duplicate(winningNumberList);
+        if(winningNumberList.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>(numbers);
+        if (set.size() < numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,16 +10,25 @@ public class InputView {
         while (input == 0) {
             try {
                 input = Integer.parseInt(Console.readLine());
-                userInputException.lottoPriceInputValidate(input);
+                userInputException.lottoPriceValidate(input);
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                System.out.println("[ERROR] 로또 구입시 천원 단위의 숫자가 입력되어야 합니다.");
             }
         }
         return Console.readLine();
     }
 
     public String lottoWinningNumberInput() {
-        return Console.readLine();
+        String input = null;
+        while (input == null) {
+            try {
+                input = Console.readLine();
+                userInputException.winningNumberValidate(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 로또 당첨 번호는 중복 없는 6자리 입니다.");
+            }
+        }
+        return input;
     }
 
     public String bonusNumberInput() {

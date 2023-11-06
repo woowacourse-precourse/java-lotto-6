8d66711e0bbfd869bac9eeea29de2b096651f11f
SEOWONLEEE <9952sun@jj.ac.kr>
2023-11-07 03:52:13 +0900

    refactor: 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController.start();
+        LottoController controller = new LottoController();
+        controller.run();
     }
 }

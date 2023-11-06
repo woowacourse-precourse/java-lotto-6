71c367e5eb665d7a49905678bdf43f106436c4c2
SeungGwa123 <skwan123@naver.com>
2023-11-06 21:10:20 +0900

    Fix: Fix Message.moneyOutOfRange
    erroe메세지 값이 문제가 있는것 같아서 수정

--- src/main/java/Message/Message.java
+++ src/main/java/Message/Message.java
@@ -1,7 +1,7 @@
 package Message;
 
 public enum Message {
-    moneyOutOfRange(-1,"[ERROR] 구입 금액은 0이상의 정수여야 합니다."),
+    moneyOutOfRange(-1,"[ERROR] 구입 금액은 정수여야 합니다."),
     moneyInvalidFormat(-2,"[ERROR] 구입 금액은 1000원 단위의 양의 정수여야 합니다."),
     numberOutOfRange(-1,"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     numberInvalidFormat(-2,"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");

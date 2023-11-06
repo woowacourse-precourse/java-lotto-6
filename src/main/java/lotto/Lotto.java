bcd248041ab96fc5f5c03e8016fb270a0976908a
Hooni <gkxogns21@gmail.com>
2023-11-06 19:45:08 +0900

    feat: make validateRange() and getNumbers()

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,6 +7,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +18,15 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
 }

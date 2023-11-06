8ba1e4b57d6ef62a1cb49d5c784f71a1fdf89c95
twinson37 <twinson37@hanyang.ac.kr>
2023-11-07 00:52:47 +0900

    Feat: 로또 게임 클래스 추가

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame extends Lotto{
+
+    public LottoGame(List<Integer> winningNumbers) {
+        super(winningNumbers);
+    }
+}

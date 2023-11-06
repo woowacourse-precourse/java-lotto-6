b57d9e198641a1abc17f110a75fc69bbebdad2cc
yeseong31 <yeseong31@naver.com>
2023-11-06 13:42:08 +0900

    refactor(Lotteries): LottoSystem에서 Lotteries로 이름 변경

--- src/main/java/lotto/domain/Lotteries.java
+++ src/main/java/lotto/domain/Lotteries.java
@@ -1,29 +1,30 @@
 package lotto.domain;
 
 import static lotto.utils.Randoms.pickUniqueNumbersInRange;
+import static lotto.view.NumberConstant.ZERO;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class LottoSystem {
-
+public class Lotteries {
+    
     private final List<Lotto> lotteries;
-
-    private LottoSystem(final List<Lotto> lotteries) {
+    
+    private Lotteries(final List<Lotto> lotteries) {
         this.lotteries = lotteries;
     }
-
-    public static LottoSystem of(int count) {
-        List<Lotto> result = createLotteries(count);
-        return new LottoSystem(result);
+    
+    public static Lotteries from(Payment payment) {
+        List<Lotto> result = createLotteries(payment);
+        return new Lotteries(result);
     }
-
-    private static List<Lotto> createLotteries(int count) {
-        return IntStream.range(0, count)
+    
+    private static List<Lotto> createLotteries(Payment payment) {
+        return IntStream.range(ZERO, payment.receiveIssuedLottoCount())
                 .mapToObj(index -> new Lotto(pickUniqueNumbersInRange()))
                 .toList();
     }
-
+    
     public List<String> receiveLotteriesInfo() {
         return lotteries.stream()
                 .map(Lotto::toString)
--- src/test/java/lotto/domain/LotteriesTest.java
+++ src/test/java/lotto/domain/LotteriesTest.java
@@ -10,34 +10,36 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class LottoSystemTest {
-
+class LotteriesTest {
+    
     @Nested
-    @DisplayName("[LottoSystem] 입력 테스트")
-    class LottoSystemSuccessTest {
-
+    @DisplayName("[Lotteries] 입력 테스트")
+    class LotteriesSuccessTest {
+        
         @DisplayName("로또 발행이 정상적으로 진행되는 경우를 확인")
         @ParameterizedTest
-        @ValueSource(ints = {1, 3, 6})
-        void create_로또_발행이_정상적으로_진행되는_꼉우를_확인(int count) {
+        @ValueSource(strings = {"1000", "3000", "6000"})
+        void create_로또_발행이_정상적으로_진행되는_경우를_확인(String amount) {
+            Payment payment = Payment.from(amount);
+            
             assertThatNoException().isThrownBy(()
-                    -> LottoSystem.of(count));
+                    -> Lotteries.from(payment));
         }
     }
-
+    
     @Nested
-    @DisplayName("[LottoSystem] 기능 테스트")
-    class LottoSystemUnitTest {
-
+    @DisplayName("[Lotteries] 기능 테스트")
+    class LotteriesUnitTest {
+        
         @Test
         @DisplayName("발행한 로또 리스트를 정상적으로 반환하는지 확인")
         void toString_발행한_로또_리스트를_정상적으로_반환하는지_확인() {
-            int count = 8;
-
-            LottoSystem lottoSystem = LottoSystem.of(count);
-            List<String> result = lottoSystem.receiveLotteriesInfo();
-
-            assertThat(result.size()).isEqualTo(count);
+            Payment payment = Payment.from("8000");
+            
+            Lotteries lotteries = Lotteries.from(payment);
+            List<String> result = lotteries.receiveLotteriesInfo();
+            
+            assertThat(result.size()).isEqualTo(8);
         }
     }
 }

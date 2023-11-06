96a16c969d7013ab2d716bd0f3c98500c80165d6
DooDuZ <sin9158@naver.com>
2023-11-07 02:41:04 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/game/LotteryGame.java
+++ src/main/java/lotto/game/LotteryGame.java
@@ -1,30 +1,33 @@
 package lotto.game;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.WinNumber;
 import lotto.io.InputHandler;
 import lotto.io.OutputHandler;
+import lotto.validator.LottoNumberValidator;
 import lotto.validator.PriceValidator;
-import lotto.validator.Validator;
 
 public class LotteryGame {
     private List<Lotto> lottoList;
+    private WinNumber winNumber;
     private InputHandler inputHandler = new InputHandler();
     private OutputHandler outputHandler = new OutputHandler();
     private final String ERROR = "[ERROR]";
-    private final String REUQEST_PRICE = "구입금액을 입력해 주세요.";
+    private final String REQUEST_PRICE = "구입금액을 입력해 주세요.";
+    private final String REQUEST_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public void run() {
 
     }
 
     private void init() {
-        outputHandler.print(REUQEST_PRICE);
-        int price = getPrice();
-        lottoList = getLottoList(price);
+        outputHandler.print(REQUEST_PRICE);
+        lottoList = getLottoList(getPrice());
+        outputHandler.print(REQUEST_NUMBERS);
+        winNumber = getWinNumber();
     }
 
     private int getPrice() {
@@ -52,5 +55,46 @@ public class LotteryGame {
             lottos[i] = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         }
         return Arrays.asList(lottos);
+    }
+
+    private WinNumber getWinNumber(){
+        while (true){
+            try{
+                List<Integer> numbers = getLottoNumbers();
+                return new WinNumber(numbers, getBonusNumber(numbers));
+            }catch (IllegalArgumentException e){
+                outputHandler.print(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> getLottoNumbers() {
+        while (true) {
+            try {
+                String[] inputNumbers = inputHandler.readLine().split(",");
+                Integer[] numbers = new Integer[inputNumbers.length];
+                for (String number : inputNumbers){
+                    LottoNumberValidator.isInteger(number);
+                }
+
+                return Arrays.asList(numbers);
+            } catch (IllegalArgumentException e) {
+                outputHandler.print(ERROR+e.getMessage());
+            }
+        }
+    }
+
+    public int getBonusNumber(List<Integer> numbers){
+        while (true) {
+            try {
+                String inputNumber = inputHandler.readLine();
+                LottoNumberValidator.isInteger(inputNumber);
+                int bonusNumber = Integer.parseInt(inputNumber);
+                LottoNumberValidator.validateDuplicatedNumber(numbers, bonusNumber);
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                outputHandler.print(ERROR+e.getMessage());
+            }
+        }
     }
 }

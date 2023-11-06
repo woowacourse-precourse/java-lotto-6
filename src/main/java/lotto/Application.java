631a95ddf486dbf665ceb79f05622d5fd1cde3fe
ghsyn <swanshawn715@gmail.com>
2023-11-07 00:55:13 +0900

    feat(Lotto) : 게임 초반 입출력 logic

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import static lotto.LottoGame.issuanceLotto;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int lottoCnt = 0;
+
+        Output.startMessage();
+        lottoCnt = issuanceLotto(Input.money());
+        Output.howManyLottoMessage(lottoCnt);
+
     }
 }

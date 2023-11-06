00369e164600765a4a0f1352b9e12ec346dcfcf9
kkw2758 <dtc3229@gmail.com>
2023-11-06 14:42:04 +0900

    Feat(InputView) : 사용자로 부터 받은 입력이 공백인지 검증하는 기능 추가

--- src/main/java/constants/ErrorMessage.java
+++ src/main/java/constants/ErrorMessage.java
@@ -7,7 +7,9 @@ public enum ErrorMessage {
     ANSWER_LOTTO_HAS_DUPLICATE_NUMBER_ERROR("당첨번호에 중복된 숫자가 포함되어 있습니다."),
     ANSWER_LOTTO_NOT_IN_RANGE_ERROR("당첨번호에 %d~%d 범위 외의 숫자가 포함되어 있습니다."),
     BONUS_NUMBER_RANGE_ERROR("보너스 숫자는 %d~%d 범위 외의 숫자가 될 수 없습니다."),
-    BONUS_NUMBER_IN_ANSWER_LOTTO_ERROR("당첨번호에 포함된 숫자는 보너스 번호가 될 수 없습니다.");
+    BONUS_NUMBER_IN_ANSWER_LOTTO_ERROR("당첨번호에 포함된 숫자는 보너스 번호가 될 수 없습니다."),
+    BLANK_INPUT_ERROR("공백이 입력되었습니다.");
+
 
     private final String message;
 
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,18 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import constants.ErrorMessage;
 
 public class InputView {
     public static String readLine() {
-        return Console.readLine();
+        String userInput = Console.readLine();
+        validateBlankInput(userInput);
+        return userInput;
+    }
+
+    private static void validateBlankInput(final String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.BLANK_INPUT_ERROR.getMessage());
+        }
     }
 }

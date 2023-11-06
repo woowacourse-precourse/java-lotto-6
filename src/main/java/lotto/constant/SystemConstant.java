171314b1b258d939711c4fa254dde4d72cfe23dd
jang010505 <jang010505@inu.ac.kr>
2023-11-07 06:22:43 +0900

    feat: InputValidator 객체 내부 코드 작성

--- src/main/java/lotto/constant/SystemConstant.java
+++ src/main/java/lotto/constant/SystemConstant.java
@@ -6,4 +6,7 @@ public class SystemConstant {
     public static final int NUMBER_COUNT = 6;
 
     public static final int MONEY_UNIT = 1000;
+
+    public static final String PRICE_PATTERN = "^[1-9]\\d*000$";
+    public static final String NUMBER_PATTERN = "\\b[1-9]|[1-3]\\d|4[0-5]\\b";
 }
--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,20 +1,50 @@
 package lotto.validator;
 
+import static lotto.constant.ErrorMessageConstant.LOTTO_NUMBER_INPUT_ERROR_MESSAGE;
+import static lotto.constant.ErrorMessageConstant.LOTTO_NUMBER_SIZE_ERROR_MESSAGE;
+import static lotto.constant.ErrorMessageConstant.LOTTO_PRICE_INPUT_ERROR_MESSAGE;
+import static lotto.constant.SystemConstant.NUMBER_PATTERN;
+import static lotto.constant.SystemConstant.PRICE_PATTERN;
+
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class InputValidator {
     public static int validateLottoPriceInput(String input) {
+        if (!Pattern.matches(PRICE_PATTERN, input)) {
+            throw new IllegalArgumentException(LOTTO_PRICE_INPUT_ERROR_MESSAGE.getErrorMessage());
+        }
 
-        return 0;
+        return Integer.parseInt(input);
     }
 
     public static List<Integer> validateLottoNumberInput(String input) {
+        List<String> lottoNumbers = List.of(input.split(","));
 
-        return null;
+        validateInputNumberCount(lottoNumbers);
+
+        return lottoNumbers.stream()
+                .map(InputValidator::validateNumber)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private static void validateInputNumberCount(List<String> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_SIZE_ERROR_MESSAGE.getErrorMessage());
+        }
     }
 
     public static int validateBonusNumberInput(String input) {
+        return validateNumber(input);
+    }
 
-        return 0;
+    private static int validateNumber(String input) {
+        if (!Pattern.matches(NUMBER_PATTERN, input)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_INPUT_ERROR_MESSAGE.getErrorMessage());
+        }
+
+        return Integer.parseInt(input);
     }
 }

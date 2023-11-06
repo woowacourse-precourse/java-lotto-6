33420f6470489daa3cf05c26832ea0e762d5bf6b
KimDongEon <gold3987@naver.com>
2023-11-06 23:47:34 +0900

    feat(Store) : 당첨 로또 입력
    당첨 로또 입력 구현
    - inputWinningLotto() : 당첨 번호와 보너스 번호를 입력 받아 당첨 로또 생성합니다.

--- src/main/java/controller/Store.java
+++ src/main/java/controller/Store.java
@@ -4,6 +4,7 @@ package controller;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import model.Lotto;
+import model.WinningLotto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ public class Store {
   public void start() {
     int money = inputMoney();
     List<Lotto> lottos = giveLotto(money);
+   
 
 
 
@@ -49,6 +51,21 @@ public class Store {
   public List<Integer> makeLottoNumber() {
     return Randoms.pickUniqueNumbersInRange(1, 45, 6);
   }
+
+  public WinningLotto inputWinningLotto() {
+    System.out.println("당첨 번호를 입력해 주세요");
+    String numbersInput = Console.readLine();
+    System.out.println("보너스 번호를 입력해 주세요");
+    int bonusNumber = Integer.parseInt(Console.readLine());
+
+    List<Integer> numbers = new ArrayList<>();
+    String[] numberInputSplit = numbersInput.split(",");
+    for(String number : numberInputSplit) {
+      numbers.add(Integer.parseInt(number));
+    }
+    return new WinningLotto(numbers, bonusNumber);
+  }
+
 
 
 

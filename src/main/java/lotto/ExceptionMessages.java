8f44be13da4d1ba5d3aa276561e32c1b83d13f22
정영록 <s9285s@naver.com>
2023-11-07 00:12:14 +0900

    feat : 보너스 번호가 정답 번호에 있을 때 예외 메시지 작성

--- src/main/java/lotto/ExceptionMessages.java
+++ src/main/java/lotto/ExceptionMessages.java
@@ -7,7 +7,8 @@ public enum ExceptionMessages {
     NOT_VALIDATED_WINNING_NUMBER_SIZE("서로 다른 당첨 번호 6개를 입력해주세요."),
     NOT_VALIDATED_PURCHASE_AMOUNT("금액은 1,000원 단위로 입력해주세요."),
     DUPLICATE_NUMBER_INPUT("서로 다른 숫자를 입력해주세요."),
-    NOT_VALIDATED_INPUT_TYPE("올바른 타입으로 입력해주세요.");
+    NOT_VALIDATED_INPUT_TYPE("올바른 타입으로 입력해주세요."),
+    BONUS_NUMBER_DUPLICATE("당첨 번호에 중복되는 숫자가 존재합니다.");
 
     private final String message;
 

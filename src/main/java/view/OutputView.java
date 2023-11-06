e61c7dce3825bb10357352373788fb2c89112a10
daneng4 <daneng4@naver.com>
2023-11-06 19:36:05 +0900

    feat: 개행 상수 문자열 출력, 내가 산 로또 전체 번호 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,16 +1,30 @@
 package view;
 
+import enums.Constants;
 import enums.ExceptionMessage;
 import enums.Message;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OutputView {
+    private int size = Integer.parseInt(Constants.LOTTO_NUMBER_SIZE.getConst());
 
     public void printRequirelottoCost(){
         System.out.println(Message.REQUIRE_PURCHASE_MONEY.getMessage());
     }
 
     public void printPublishedLotto(int lottoPaper){
-        System.out.printf("%d%s", lottoPaper, Message.PUBLISHED_LOTTO.getMessage());
+        System.out.printf("%d%s\n", lottoPaper, Message.PUBLISHED_LOTTO.getMessage());
+    }
+
+    public void printNewLine(){
+        System.out.print(Message.NEW_LINE.getMessage());
+    }
+
+    public void printMyLottoNumber(ArrayList<List<Integer>> myLottoNumber){
+        for (List<Integer> integers : myLottoNumber) {
+            System.out.println(integers);
+        }
     }
 
     public void printIlligalLottoCostMessage(){

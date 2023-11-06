14ac8ec4328ddd1ef484c3a605ff33d19f5473f6
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-06 15:37:34 +0900

    refactor: 구매 금액이 음수인지에 대한 검사는 domain 영역에서 하도록 변경

--- src/main/java/domain/validator/PriceValidator.java
+++ src/main/java/domain/validator/PriceValidator.java
@@ -3,16 +3,24 @@ package domain.validator;
 import util.ErrorMessage;
 import util.ConstantOfLotto;
 
-public class PriceUnitValidator {
+public class PriceValidator {
 
     private final int STARNDARD_UNIT = ConstantOfLotto.priceOfLotto;
 
     public void checkPriceUnit(int price) {
+        isPricePositive(price);
         isPriceInStandardUnit(price, STARNDARD_UNIT);
     }
     private void isPriceInStandardUnit(int price, int unit) {
         if(price % unit != 0) {
             System.out.println(ErrorMessage.PRICE_ISNT_IN_STANDARD_UNIT.getErrorMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isPricePositive(int price) {
+        if(price < 0) {
+            System.out.println(ErrorMessage.PRICE_IS_NEGATIVE.getErrorMessage());
             throw new IllegalArgumentException();
         }
     }
--- src/main/java/view/validator/PriceFormatValidator.java
+++ src/main/java/view/validator/PriceFormatValidator.java
@@ -7,7 +7,6 @@ public class PriceFormatValidator {
     public int checkPriceFormat(String input) {
         isPriceEmpty(input);
         int price = isPriceContainsOnlyNumber(input);
-        isPricePositive(price);
         return price;
     }
 
@@ -23,13 +22,6 @@ public class PriceFormatValidator {
             return Integer.parseInt(input);
         } catch(NumberFormatException e) {
             System.out.println(ErrorMessage.PRICE_CONTAIN_NON_NUMERIC_CHARACTER.getErrorMessage());
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void isPricePositive(int price) {
-        if(price < 0) {
-            System.out.println(ErrorMessage.PRICE_IS_NEGATIVE.getErrorMessage());
             throw new IllegalArgumentException();
         }
     }

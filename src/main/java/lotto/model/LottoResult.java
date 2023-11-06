5d03365eba135314dbe17b5d7e64100cb9f9284c
sunghyun <shlim0287@naver.com>
2023-11-06 23:20:48 +0900

    feat : 로또진행 결과를 가지고 있는 enum클래스 추가.

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+public enum LottoResult {
+    NONE(0,0),
+    FIFTH(3,5000),
+    FOURTH(4,50000),
+    THIRD(5,1500000),
+    SECOND(5,30000000),
+    FIRST(6,2000000000);
+
+    private final int matchCount;
+    private final int prize;
+
+    LottoResult(int matchCount,int prize){
+        this.matchCount=matchCount;
+        this.prize=prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}

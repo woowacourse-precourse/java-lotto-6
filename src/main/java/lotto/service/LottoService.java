2578337984e84b7716df19e2fb7ffde8473561de
Zeus6768 <akaps6768@gmail.com>
2023-11-06 22:11:04 +0900

    fix: 로또 생성 로직 오류 수정
    - Lotto는 비교할 수 없으므로 Lottos 생성 시 sorted() 사용 불가
    - createLotto에서 로또 번호 정렬

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,13 +1,15 @@
 package lotto.service;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
-import lotto.model.Lottos;
 import lotto.model.LottoPrize;
-import lotto.model.PurchaseAmount;
 import lotto.model.LottoStatistic;
+import lotto.model.Lottos;
+import lotto.model.PurchaseAmount;
 import lotto.model.WinningNumbers;
 
 public class LottoService {
@@ -16,7 +18,6 @@ public class LottoService {
         int lottoCount = amount.toInt() / PurchaseAmount.LOTTO_PRICE;
         return Lottos.from(Stream.generate(this::createLotto)
                 .limit(lottoCount)
-                .sorted()
                 .toList());
     }
 
@@ -30,6 +31,8 @@ public class LottoService {
     }
 
     private Lotto createLotto() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return new Lotto(numbers);
     }
 }

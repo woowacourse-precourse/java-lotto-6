ae9f7c7b8b2d8aabb84313956b81de094222d9f3
JoonKyo <joonkyolee@gmail.com>
2023-11-06 17:10:24 +0900

    feat(InputView): 플레이어에게 입력을 받는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,20 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.Exception;
+
+public class InputView {
+    private InputView() {
+    }
+
+    public static String readInput() {
+        try {
+            String input = Console.readLine();
+            InputValidator.validateInput(input);
+            return input;
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            return readInput();
+        }
+    }
 }

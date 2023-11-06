61c670f64db93ae65d5cfc79114d18711abdfbca
Infikei <hckhong@naver.com>
2023-11-06 18:40:28 +0900

    feat: InputView 클래스 생성 및 상수 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class InputView {
+    private static final String AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+}

8ff7982c028558bc3709d58524c510cce1bdc854
bbbig5360 <bbbig5360>
2023-11-07 02:58:10 +0900

    List.of() 사용시 로또의 값을 정렬하지 못하는 오류 발생(UnsupportedOperationException)으로 인한 코드 변경

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -2,9 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-
+import java.util.ArrayList;
 import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,14 +35,14 @@ class ApplicationTest extends NsTest {
                             "총 수익률은 62.5%입니다."
                     );
                 },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
+                new ArrayList<>(List.of(8, 21, 23, 41, 42, 43)),
+                new ArrayList<>(List.of(3, 5, 11, 16, 32, 38)),
+                new ArrayList<>(List.of(7, 11, 16, 35, 36, 44)),
+                new ArrayList<>(List.of(1, 8, 11, 31, 41, 42)),
+                new ArrayList<>(List.of(13, 14, 16, 38, 42, 45)),
+                new ArrayList<>(List.of(7, 11, 30, 40, 42, 43)),
+                new ArrayList<>(List.of(2, 13, 22, 32, 38, 45)),
+                new ArrayList<>(List.of(1, 3, 5, 14, 22, 45))
         );
     }
 
@@ -61,7 +60,7 @@ class ApplicationTest extends NsTest {
     void 로또_숫자_범위_예외_처리() {
         assertSimpleTest(() -> {
             assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1000","1,2,3,4,5,55"))
+                assertThatThrownBy(() -> runException("1000j","1,2,3,4,5,55"))
                     .isInstanceOf(IllegalArgumentException.class));
         assertThat(output()).contains(ERROR_MESSAGE);
         });

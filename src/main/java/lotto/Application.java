8a86af2bad6bd4a8e2c62d1d91342938d29c4060
kwon <kckc0608@naver.com>
2023-11-07 00:24:59 +0900

    feat: 보너스 번호와 로또 번호 중복 체크

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -20,6 +20,7 @@ public class Application {
     private static final String ERROR_MESSAGE_FOR_WINNING_NUMBER_DUPLICATE = "로또 번호는 중복되지 않은 숫자여야 합니다.";
     private static final String ERROR_MESSAGE_FOR_BONUS_NUMBER_TYPE = "보너스 번호는 숫자여야 합니다.";
     private static final String ERROR_MESSAGE_FOR_BONUS_NUMBER_RANGE = "보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE_FOR_BONUS_NUMBER_DUPLICATE = "보너스 번호는 로또 번호와 중복되지 않아야 합니다.";
 
     private static int purchaseAmount;
     private static int bonusNumber;
@@ -125,6 +126,11 @@ public class Application {
 
         if (bonusNumber < 1 || bonusNumber > 45) {
             printErrorMessage(ERROR_MESSAGE_FOR_BONUS_NUMBER_RANGE);
+            throw new IllegalArgumentException();
+        }
+
+        if (winningNumbers.contains(bonusNumber)) {
+            printErrorMessage(ERROR_MESSAGE_FOR_BONUS_NUMBER_DUPLICATE);
             throw new IllegalArgumentException();
         }
     }

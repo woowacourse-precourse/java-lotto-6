3b076bfdd6225eec6acb403fa60252751e760108
nayooniverse <kangna99@naver.com>
2023-11-06 18:47:30 +0900

    fix: 로또 구입 금액 입력 예외처리 InputView로 로직 이동, 구입 금액 변수명 purchaseAmount로 수정

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,7 +1,60 @@
 package View;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputView {
-    public void printInputMoneyMessage() {
+
+    public int askPurchaseAmount() {
+        printInputMoneyMessage();
+        String purchaseAmount = Console.readLine();
+        validatePurchaseAmount(purchaseAmount);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public void validatePurchaseAmount(String purchaseAmount) {
+        validateNotBlank(purchaseAmount);
+        validateInputIsNumeric(purchaseAmount);
+        validateInputInRange(purchaseAmount);
+        validateInputIsMultipleOfThousand(purchaseAmount);
+    }
+
+    private void validateNotBlank(String input) {
+        if (input == null || input.isBlank() || containsWhitespace(input)) {
+            throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
+        }
+    }
+
+    private boolean containsWhitespace(String input) {
+        Pattern pattern = Pattern.compile("\\s");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+    private void validateInputIsNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+    }
+
+    private void validateInputInRange(String input) {
+        int purchaseAmount = Integer.parseInt(input);
+        if (purchaseAmount < 0) {
+            throw new IllegalArgumentException("[ERROR] 0 이상의 숫자를 입력해 주세요.");
+        }
+    }
+
+    private void validateInputIsMultipleOfThousand(String input) {
+        int purchaseAmount = Integer.parseInt(input);
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000 단위로 입력해 주세요.");
+        }
+    }
+
+    private void printInputMoneyMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
@@ -10,7 +63,7 @@ public class InputView {
     }
 
     public void printInputBonusNumberMessage() {
-        System.out.println("보너스 볼을 입력해 주세요.");
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 
     public void printResultMessage() {
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,19 +8,7 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OuputView outputView = new OuputView();
-        inputView.printInputMoneyMessage();
-        int money;
-        try {
-            money = Integer.parseInt(Console.readLine());
-        } catch(NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
-        }
-        if(money < 0) {
-            throw new IllegalArgumentException("[ERROR] 0 이상의 숫자를 입력해 주세요.");
-        }
-        if(money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000 단위로 입력해 주세요.");
-        }
+        int money = inputView.askPurchaseAmount();
 
         Lottos lottos = new Lottos();
         lottos.buy(money);

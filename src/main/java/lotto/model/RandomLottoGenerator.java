8c10ac8fe11a6348b2fd5cb4856ecdf9865e1a00
GS_song <20003204@sju.ac.kr>
2023-11-06 15:01:09 +0900

    fix: Application 기능_테스트에서 랜덤한 리스트를 불변객체로 생성해서 생긴 오류 수정
    ApplicationTest에서 기능_테스트를 수행할 때 pickUniqueNumbersInRange의 랜덤한 값을 만들어 줄 때 List.of() 메서드를 사용하는데 불변 객체를 sort할 때 나타나는 오류 해결
    새로운 ArrayList 객체를 만들어 값을 복사해와서 해결

--- src/main/java/lotto/model/RandomLottoGenerator.java
+++ src/main/java/lotto/model/RandomLottoGenerator.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class RandomLottoGenerator {
 
     public Lotto generate() {
-        List<Integer> RandomNums = Randoms.pickUniqueNumbersInRange(Lotto.LOTTO_MIN_NUMBER, Lotto.LOTTO_MAX_NUMBER,
-                Lotto.LOTTO_SIZE);
+        List<Integer> RandomNums = new ArrayList<>(Randoms.pickUniqueNumbersInRange(Lotto.LOTTO_MIN_NUMBER, Lotto.LOTTO_MAX_NUMBER,
+                Lotto.LOTTO_SIZE));
         Collections.sort(RandomNums);
         return new Lotto(RandomNums);
     }

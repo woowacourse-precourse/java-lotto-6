7c14878ce12942e9a5fe45d048db8c73e0d81452
km9404 <kmh940408@gmail.com>
2023-11-07 03:10:40 +0900

    feat (LottoController): 구매한 로또 번호 출력

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,10 +1,16 @@
 package lotto.Controller;
 
+import lotto.domian.BoughtLotto;
 import lotto.domian.LottoAmount;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
+
+    private List<List<Integer>> boughtLottos = new ArrayList<>();
     public void run() {
         start();
     }
@@ -12,10 +18,23 @@ public class LottoController {
     private void start() {
         long lottoAmount = inputLottoAmount();
         OutputView.printLottoAmount(lottoAmount);
+
+        printBoughtLotto(lottoAmount);
     }
 
     private long inputLottoAmount() {
         LottoAmount lottoAmountCount = new LottoAmount(InputView.inputAmount());
         return lottoAmountCount.calculateLottoAmount();
+    }
+
+    private void printBoughtLotto(long lottoAmount) {
+        long count = lottoAmount;
+        while (count-- > 0) {
+            BoughtLotto boughtLotto = new BoughtLotto();
+            boughtLottos.add(boughtLotto.generateLottoNumbers());
+        }
+        for (List<Integer> lotto : boughtLottos) {
+            System.out.println(lotto);
+        }
     }
 }

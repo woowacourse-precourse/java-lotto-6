ae8118363267e592c36b1082e130fe528ce06c7a
JoonKyo <joonkyolee@gmail.com>
2023-11-06 23:03:23 +0900

    feat(CommonConstraint, LottoConstraint): 입력 값에 대한 공통 제약과 로또 제약 추가

--- src/main/java/lotto/constraint/CommonConstraint.java
+++ src/main/java/lotto/constraint/CommonConstraint.java
@@ -1,2 +1,15 @@
-package lotto.constraint;public class CommonConstraint {
+package lotto.constraint;
+
+public enum CommonConstraint {
+    DELIMITER(",");
+
+    private final String value;
+
+    CommonConstraint(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }

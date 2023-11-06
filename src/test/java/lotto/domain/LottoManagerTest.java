30b2b9bdcefb2dc470b6af20b9e459ac713a6a9e
SeongIl <biuld1234@gmail.com>
2023-11-06 14:13:24 +0900

    test: add 로또 당첨 결과 반환

--- src/test/java/lotto/domain/LottoManagerTest.java
+++ src/test/java/lotto/domain/LottoManagerTest.java
@@ -4,17 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.constants.Prize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoManagerTest {
 
     @DisplayName("로또 리스트를 오름차순으로 정렬")
     @Test
     void sortLottos() {
-
         // given
         List<Lotto> lottos = new ArrayList<>();
         lottos.add(new Lotto(List.of(6, 5, 4, 3, 2, 1)));
@@ -44,5 +42,28 @@ class LottoManagerTest {
         // then
         assertThat(countMatching).isEqualTo(6);
     }
+
+
+    @DisplayName("로또 당첨 결과 반환")
+    @Test
+    void checkWinning() {
+        // given
+        List<Lotto> userLottos = new ArrayList<>();
+        userLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        userLottos.add(new Lotto(List.of(1, 2, 3, 7, 8, 9)));
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6)); // 1등: 1, 5등: 1
+        Integer bonusNumber = 10;
+
+        // when
+        LottoResult lottoResult = LottoManager.checkWinning(userLottos, winningLotto, bonusNumber);
+
+        // then
+        assertThat(lottoResult.state.get(Prize.FIRST)).isEqualTo(1);
+        assertThat(lottoResult.state.get(Prize.FIFTH)).isEqualTo(1);
+
+    }
+
+
 
 }

c7296b08a7cf0ff6ffc0654562fa72669ccee548
JiEung2 <https://github.com/JiEung2>
2023-11-06 21:01:54 +0900

    Style: 필요없는 import 삭제

--- src/main/java/lotto/model/BuyPriceValidator.java
+++ src/main/java/lotto/model/BuyPriceValidator.java
@@ -1,9 +1,11 @@
 package lotto.model;
 
-import static lotto.utils.Constants.*;
+import static lotto.utils.Constants.BUY_PRICE_PATTERN;
+import static lotto.utils.Constants.BUY_PRICE_PATTERN_ERROR;
+import static lotto.utils.Constants.BUY_PRICE_UNIT_ERROR;
+import static lotto.utils.Constants.ERROR_MESSAGE;
 
 import java.util.regex.Matcher;
-import lotto.utils.Constants;
 import lotto.view.InputView;
 
 public class BuyPriceValidator {
@@ -14,20 +16,22 @@ public class BuyPriceValidator {
         validate();
     }
 
-    private void validate(){
+    private void validate() {
         isNumber();
         isCorrectUnit();
     }
 
-    public void isNumber(){
+    public void isNumber() {
         Matcher matcher = BUY_PRICE_PATTERN.matcher(buyPrice);
-        if(!matcher.find()){
+        if (!matcher.find()) {
             throw new IllegalArgumentException(ERROR_MESSAGE + BUY_PRICE_PATTERN_ERROR);
         }
     }
 
-    public void isCorrectUnit(){
+    public void isCorrectUnit() {
         int price = InputView.convertToInt(buyPrice);
-        if(price % 1000 != 0) throw new IllegalArgumentException(ERROR_MESSAGE + BUY_PRICE_UNIT_ERROR);
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + BUY_PRICE_UNIT_ERROR);
+        }
     }
 }

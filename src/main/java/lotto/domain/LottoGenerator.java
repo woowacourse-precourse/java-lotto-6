9554a751ee1eba111c2dcb201e6d1990281df53e
jihwankim128 <jihwan1535@nate.com>
2023-11-07 01:38:26 +0900

    feat(lotto): 지불한 금액만큼 로또를 발행

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,5 +1,29 @@
 package lotto.domain;
 
-public class LottoGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
+public class LottoGenerator {
+    List<Lotto> lottos = new ArrayList<>();
+    final int LOTTO_PRICE_UNIT = 1000;
+    final int CHANGE = 0;
+
+    public LottoGenerator(int payment) {
+        int lottoCount = validatePayment(payment);
+        for(int lotto = 0; lotto < lottoCount; lotto++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            this.lottos.add(new Lotto(numbers));
+        }
+    }
+
+    private int validatePayment(int payment) {
+        if(payment < LOTTO_PRICE_UNIT) {
+            throw new IllegalArgumentException("최소 결제 금액은 1,000원 입니다.");
+        }
+        if(payment % LOTTO_PRICE_UNIT != CHANGE) {
+            throw new IllegalArgumentException("1,000원 단위로 결제해주세요.");
+        }
+        return payment/LOTTO_PRICE_UNIT;
+    }
 }

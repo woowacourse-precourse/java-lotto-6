5c88b38eab5b9449c967c45f47e31af776b7cbc1
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-06 21:03:53 +0900

    🚀refector(domain/LottoGenerator) : 로또 생성 기능 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,33 +2,21 @@ package lotto.domain;
 
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import lotto.Lotto;
-import lotto.validate.InputValidation;
 
 /* 로또 번호 랜덤 생성기
 * */
 public class LottoGenerator {
-    Lotto lotto = new Lotto(new ArrayList<>(Arrays.asList(0,0,0,0,0,0)));
-    Purchase purchase;
-    LottoStorage lottoStorage;
-    public LottoGenerator() {
+    private int MINIMUM_LOTTO_NUMBER = 1;
+    private int MAX_LOTTO_NUMBER = 45;
+    private int LOTTO_COUNT = 6;
 
+    public Lotto generateNumber() {
+        return new Lotto(generateLotto());
     }
 
-    public List<Integer> generateNumber() {
-        lotto.setNumber(new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45,1)));
-        return lotto.getNumbers();
-    }
-
-    // 로또 구입 개수만큼 생성
-    public void gernatePurchaseLotto() {
-        int quantity = purchase.getQuantity();
-        while (quantity-- < 0) {
-            generateNumber();
-            lottoStorage.addLotto(generateNumber());
-        }
+    private List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_COUNT);
     }
 }

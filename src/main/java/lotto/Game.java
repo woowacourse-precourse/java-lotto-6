042e88f6038c37ae4c60ec329f904ce5809b77e1
hongkikii <hyangmeeh@icloud.com>
2023-11-06 22:29:38 +0900

    feat: 로또 발행 기능 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,10 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Game {
 
+    private List<Lotto> lottos;
     private Integer buyingPrice;
+
+    public Game() {
+        this.lottos = new ArrayList<>();
+        this.buyingPrice = 0;
+    }
 
     public void run() {
         saveBuyingPrice();
@@ -18,6 +28,19 @@ public class Game {
     }
 
     private void giveLotto() {
+        int count = buyingPrice / 1000;
+        System.out.println(count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++) {
+            List<Integer> uniqueNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            showLotto(uniqueNumbers);
+            lottos.add(new Lotto(uniqueNumbers));
+        }
+    }
 
+    private void showLotto(List<Integer> uniqueNumbers) {
+        String result = uniqueNumbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", ", "[", "]"));
+        System.out.println(result);
     }
 }

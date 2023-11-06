5ab87677be00a292931be78ebc55d6bf99af5a22
yubin <dbqls2821@gmail.com>
2023-11-06 18:06:15 +0900

    feat: 통계 output형식을 반환하는 메소드 로직 생성

--- src/main/java/lotto/domain/StatisticsConverter.java
+++ src/main/java/lotto/domain/StatisticsConverter.java
@@ -9,6 +9,24 @@ public final class StatisticsConverter implements OutputConverter<Statistics> {
 
     @Override
     public String convert(Statistics statistics) {
+        StringBuilder output = new StringBuilder();
+        Arrays.stream(Result.values())
+                .forEach(it -> {
+                    output.append(oneStatisticsToString(it, statistics));
+                });
+        return output.toString();
+    }
+
+    private String oneStatisticsToString(Result it, Statistics statistics) {
+        StringBuilder temp = new StringBuilder();
+
+        int count = statistics.count(it);
+
+        temp.append(it.getContent());
+        temp.append(String.format(Constant.WON.value, it.getReward()));
+        temp.append(String.format(Constant.NUM.value, count));
+
+        return temp.toString();
     }
 
     enum Constant{

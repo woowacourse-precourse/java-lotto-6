cc47c321e96c2758f8e4f9ad139636ecb2c612a1
oiler831 <qudwls831@gmail.com>
2023-11-06 23:59:50 +0900

    feat : AutoLottoNumber 클래스 생성
    자동으로 6자리의 로또 번호를 만들어주는 클래스
    List<Integer>로 낸다.

---
+++ src/main/java/lotto/AutoLottoNumber.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class AutoLottoNumber {
+    private static final Integer LOTTO_NUMBER_SIZE = 6;
+    private static final Integer LOTTO_MIN_NUMBER = 1;
+    private static final Integer LOTTO_MAX_NUMBER = 45;
+
+    public List<Integer> putOutNumber() {
+        List<Integer> autoNumber = new ArrayList<>();
+        addNumber(autoNumber);
+        sortNumber(autoNumber);
+        return autoNumber;
+    }
+
+    private void addNumber(List<Integer> numbers) {
+        while (numbers.size() < LOTTO_NUMBER_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+
+    private void sortNumber(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
+    }
+}

1cc98b21c515fd9e98cef3b090f3418ead5695d8
Mr.Kim <joajy98@gmail.com>
2023-11-06 19:29:00 +0900

    feat: 기존에 분산되어 중복 선언되던 상수를 모아놓은 클래스 구현

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+public class Constant {
+
+    public static final int LOTTO_MIN_MONEY_UNIT = 1000;
+    public static final int LOTTO_MIN_UNIT = 0;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final String NUMBER_REGEX = "^[0-9]+$";
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final int DIVIDER_FOR_COUNT_PURCHASE_QUANTITY = 1000;
+}

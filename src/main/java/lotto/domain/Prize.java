d9dcea751ad853fb513aa58a229b68e547b9ac23
pyounani <yndbsk9372@gmail.com>
2023-11-06 19:21:03 +0900

    feat: 맟춘 번호(Prize) 클래스 생성

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
+public enum Prize {
+    EMPTY(0
+            , 0
+            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber < 3),
+    FIFTH(5_000
+            , 3
+            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 3),
+    FOURTH(50_000
+            , 4
+            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
+    THIRD(1_500_000
+            , 5
+            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
+    SECOND(30_000_000
+            , 5
+            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
+    FIRST(2_000_000_000
+            , 6
+            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
+    
+    private final int money;
+    private final int matchLottoNumber;
+    private final BiPredicate<Integer, Boolean> isMatch;
+
+    Prize(final int money, final int matchLottoNumber, final BiPredicate<Integer, Boolean> isMatch) {
+        this.money = money;
+        this.matchLottoNumber = matchLottoNumber;
+        this.isMatch = isMatch;
+    }
+
+    public static Prize getPrize(final int matchLottoNumber, final boolean containBonusNumber) {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.isMatch.test(matchLottoNumber, containBonusNumber))
+                .findAny()
+                .orElse(EMPTY);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getMatchLottoNumber() {
+        return matchLottoNumber;
+    }
+}

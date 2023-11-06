91933c61b0418a2bf8b759a2814f4e1d6692fbf3
Junobee25 <wnsdhqo@naver.com>
2023-11-06 23:57:06 +0900

    refactor(PrintMessage): ENUM 요소 수정

--- src/main/java/lotto/configuration/PrintMessage.java
+++ src/main/java/lotto/configuration/PrintMessage.java
@@ -4,7 +4,11 @@ public enum PrintMessage {
     BUY_LOTTO_COUNT("개를 구매했습니다."),
     WINNING_STATISTICS("당첨 통계"),
     DASH("-"),
-    COUNT("개");
+    COUNT("개"),
+    OPENING_BRACKET("["),
+    CLOSING_BRACKET("]"),
+    REST(",");
+
 
     private final String message;
 

e3ad57b94e7d9a0c5eca221f97fc36f4aa0ae496
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-06 16:40:02 +0900

    feat: 에러 문구 출력

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -7,6 +7,7 @@ public enum ErrorMessage {
     PRICE_CONTAIN_NON_NUMERIC_CHARACTER("[ERROR] 구매 금액에 숫자 이외의 문자(공백 포함)가 입력되었습니다. 숫자만 입력해 주세요."),
     PRICE_IS_NEGATIVE("[ERROR] 구매 금액은 0 이상인 정수만 입력해 주세요."),
     WINNNING_NUMBER_DOESNT_ENTER("[ERROR] 당첨 번호가 입력되지 않았습니다. 금액을 다시 입력해 주세요"),
+    LOTTO_MUST_CONTAIN_SIX_NUMBERS("[ERROR] 로또 번호는 6개여야 합니다."),
     LOTTO_CONTAIN_DULPICATE_NUMBER("[ERROR] 로또 번호는 중복되지 않아야 합니다. 중복되지 않은 숫자를 입력해 주세요."),
     OUT_OF_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 

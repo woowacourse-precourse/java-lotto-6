50c2c330bf0757487b37577e05e6a376d9ae4d8c
Haeun Jeong <gkdms4248@gmail.com>
2023-11-06 17:20:06 +0900

    feat: 구입 수량 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,14 @@
 package lotto.view;
 
+import static lotto.view.OutputMessage.PURCHASE_RESULT;
+
 public class OutputView {
 
     public void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public void purchaseResult(int number) {
+        System.out.println(number + PURCHASE_RESULT.getMessage());
     }
 }

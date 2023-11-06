c8f9df45222729974d05655f7cf7d7b7803d04c7
kariskan <zkfzpf56@naver.com>
2023-11-06 12:34:50 +0900

    feat: 로또 결과와 통계 전체를 담당하는 LottoResultManager 작성

---
+++ src/main/java/lotto/domain/LottoResultManager.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Map;
+import lotto.global.constant.Winning;
+
+public class LottoResultManager {
+
+    private final LottoResults lottoResults;
+    private final LottoStatistics lottoStatistics;
+    private long totalRevenue;
+
+    public LottoResultManager(LottoResults lottoResults, LottoStatistics lottoStatistics) {
+        this.lottoResults = lottoResults;
+        this.lottoStatistics = lottoStatistics;
+    }
+
+    public void calculateResult(WinningLotto winningLotto) {
+        for (LottoResult lottoResult : lottoResults.getLottoResults()) {
+            lottoResult.calculateResult(winningLotto);
+            Winning key = lottoResult.getWinning();
+            lottoStatistics.getStatistics().put(key, lottoStatistics.getStatistics().getOrDefault(key, 0) + 1);
+            totalRevenue += lottoResult.getWinning().getRevenue();
+        }
+    }
+
+    public List<LottoResult> getLottoResults() {
+        return lottoResults.getLottoResults();
+    }
+
+    public Map<Winning, Integer> getStatistics() {
+        return lottoStatistics.getStatistics();
+    }
+
+    public long getTotalRevenue() {
+        return totalRevenue;
+    }
+}

a0604707e6395f9fa41cdad262262dcc283a38f7
yeseong31 <yeseong31@naver.com>
2023-11-06 13:50:09 +0900

    refactor(InputView): 입력 처리 로직 간소화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,19 +1,13 @@
 package lotto.view;
 
-import static java.lang.Integer.parseInt;
-import static lotto.utils.Console.readLine;
+import lotto.utils.Console;
 
 public class InputView {
-
-    public static int readNumber() {
-        try {
-            return parseInt(readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(e);
-        }
+    
+    private InputView() {
     }
-
-    public static String readWinningNumbers() {
-        return readLine();
+    
+    public static String readLine() {
+        return Console.readLine();
     }
 }

500d65fd7f0312208f4ef439a3450a75939fe69b
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-11-06 23:20:35 +0900

    feat: Lotto 정렬, 문자열 변환, 포함판별 기능 추가
    로또 내 숫자를 오름차순으로 정렬하는 기능,[]를 안에 숫자를 나열한 문자열을 만드는 기능, 로또 숫자 안에 매개변수로 받은 숫자가 포함되어 있는지 판별하는 기능을 추가함.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,18 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final String OPEN_SQUARE_BRACKETS = "[";
+    private static final String CLOSE_SQUARE_BRACKETS = "]";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        sort();
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +22,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void sort() {
+        Collections.sort(numbers);
+    }
+
+    public String toString() {
+        String result = numbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        return OPEN_SQUARE_BRACKETS + result + CLOSE_SQUARE_BRACKETS;
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
 }

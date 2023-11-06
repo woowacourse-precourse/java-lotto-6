3400f380f1c2ab1f0bb4696d61947126b31dcfbd
tsulocalize <tsulocalizing@gmail.com>
2023-11-07 08:53:03 +0900

    feat : LottoApp 및 playGame 기능 구현

---
+++ src/main/java/lotto/LottoApp.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import exception.BusinessException;
+import exception.ErrorCode;
+
+public class LottoApp {
+    private final int PRICE_LOTTO = 1000;
+    private final LottoPlayer player;
+    private final LottoAdmin admin;
+
+    public LottoApp(LottoPlayer player, LottoAdmin admin) {
+        this.player = player;
+        this.admin = admin;
+    }
+
+    public void playGame() {
+        int times = getPurchaseMoney() / PRICE_LOTTO;
+        player.generateLottos(times);
+        admin.checkLottos(player);
+        admin.payPrizeMoney(player);
+    }
+
+    private int getPurchaseMoney() {
+        System.out.println("구입금액을 입력해주세요.\n");
+        String input = Console.readLine();
+        try {
+            int money = Integer.parseInt(input);
+            if (money % PRICE_LOTTO != 0) throw new BusinessException(ErrorCode.MoneyInputError);
+            return money;
+        } catch (NumberFormatException ne) {
+            System.out.println("[ERROR]숫자를 입력해야 합니다.\n");
+        } catch (BusinessException be) {
+            System.out.println(be.getMessage());
+        }
+        return getPurchaseMoney();
+    }
+}

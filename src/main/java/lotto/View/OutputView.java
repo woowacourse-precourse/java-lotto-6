30d651fd3171bed2a6245bd473b1e4aa3d9e4de3
chansoo <chansoo38@gmail.com>
2023-11-07 00:55:15 +0900

    <Feat> OutputView - 로또 게임의 출력 기능을 구현한다.

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,50 +1,63 @@
 package lotto.View;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 import lotto.Domain.Lotto;
+import lotto.Domain.Rank;
 
 public class OutputView {
     private final String BUY_LOTTO_MESSAGE_FORMAT = "%d개를 구매했습니다.";
-    private final String HIT_NUMBER_FORMAT = "%d개 일치";
-    private final String REWARD_FORMAT = "(%,d원)";
     private final String WINNING_NUM_FORMAT = "%d개";
-    private final String REVENUE_MESSAGE_FORMAT = "총 수익률은 %.1f%입니다.";
+    private final String REVENUE_MESSAGE_FORMAT = "총 수익률은 %.1f%%입니다.";
 
     private final String WIN_RESULT_MESSAGE = "당첨 통계";
     private final String LINE_DIVIDE_SYMBOL = "---";
-    private final String BONUS_SAME_MESSAGE = "보너스 볼 일치";
-    private final String COMMAS = ",";
+    private final String LINE = "\n";
+    private final String DASH = " - ";
     private final String SPACE = " ";
-    private final String DASH = "-";
 
-    private static OutputView INSTANCE = new OutputView();
+    private static final OutputView INSTANCE = new OutputView();
 
     private OutputView() {
     }
 
-    public OutputView getInstance() {
+    public static OutputView getInstance() {
         return INSTANCE;
     }
 
+    public void showNewLine(){
+        System.out.println();
+    }
     public void showBuyLotto(List<Lotto> buyLotto) {
-        System.out.println(String.format(BUY_LOTTO_MESSAGE_FORMAT,buyLotto.size()));
-        for(Lotto lotto : buyLotto) {
+        System.out.println(String.format(BUY_LOTTO_MESSAGE_FORMAT, buyLotto.size()));
+        for (Lotto lotto : buyLotto) {
             System.out.println(lotto);
         }
     }
 
-    public void showWinningResult() {
-        System.out.println(makeWinningResultMessage());
+    public void showWinningResult(Map<Rank,Integer> winningResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append(WIN_RESULT_MESSAGE).append(LINE)
+                .append(LINE_DIVIDE_SYMBOL).append(LINE)
+                .append(makeWinningResultMessage(winningResult));
+        System.out.println(stringBuilder.toString());
     }
 
     public void showRevenue(double revenue) {
-        System.out.println(String.format(REWARD_FORMAT,revenue));
+        System.out.println(String.format(REVENUE_MESSAGE_FORMAT, revenue));
     }
-    private String makeWinningResultMessage() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(WIN_RESULT_MESSAGE).append("\n").append(LINE_DIVIDE_SYMBOL);
-        // 당첨 통계 메세지를 이어 작성
-        return stringBuilder.toString();
+
+    private String makeWinningResultMessage(Map<Rank, Integer> winningResult) {
+        StringJoiner stringJoiner = new StringJoiner(LINE);
+        Arrays.stream(Rank.values())
+                .sorted(Comparator.reverseOrder())
+                .filter(rank -> rank!=Rank.NOTHING)
+                .forEach(rank -> stringJoiner.add(rank.toString() + DASH + String.format(WINNING_NUM_FORMAT,winningResult.getOrDefault(rank,0))));
+        return stringJoiner.toString();
     }
 
 }

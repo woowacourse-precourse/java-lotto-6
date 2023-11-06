a29910adf1e6cdeee2b17c9b6130d772172a5d3c
jhsseonn <hs125bt@gmail.com>
2023-11-06 19:51:58 +0900

    feat: 입력받은 금액에 해당하는만큼의 로또 장 수 계산하기

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,20 @@
 package lotto.service;
 
+import lotto.exception.LottoException;
+import lotto.view.LottoView;
+
 public class LottoService {
+
+    private LottoException lottoException = new LottoException();
+
+    public int getLottoCount(String lottoPurchase) {
+        int lottoCount = 0;
+        if (lottoException.isValidLottoPurchase(lottoPurchase)) {
+            lottoCount = Integer.parseInt(lottoPurchase)/1000;
+        }
+        return lottoCount;
+    }
+
+
+
 }

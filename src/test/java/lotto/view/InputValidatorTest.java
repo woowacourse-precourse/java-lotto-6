31bcfb24ff71a7bfcf0af19bea04bc293b1deb4a
anneu <b.imnuj@gmail.com>
2023-11-06 23:40:46 +0900

    test: 사용자 입력 값이 없을 경우 예외 테스트 추가

--- src/test/java/lotto/view/InputValidatorTest.java
+++ src/test/java/lotto/view/InputValidatorTest.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,6 +10,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class InputValidatorTest {
 
     InputValidator inputValidator = new InputValidator();
+
+    @Test
+    @DisplayName("사용자 입력 값이 없을 경우 예외가 발생한다.")
+    void inputEmptyEx() {
+        assertThatThrownBy(() -> inputValidator.validateNotEmpty(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"a", "가", "!"})

da5b0ce7f08046240ac4104b3fe1044ccf7c3f91
dustjq1004 <dustjq1005@naver.com>
2023-11-06 13:42:33 +0900

    yeonsup/feat : Lotto 보너스 번호 확인 기능 toString 오버라이딩 추가 1. toString : 출력을 위해 사용

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -16,14 +18,23 @@ public class Lotto {
         }
     }
 
-    public int compare(List<Integer> winningLotto) {
+    public int compare(List<Integer> winingLotto) {
         int count = 0;
-        for (Integer winningNumber : winningLotto) {
-            if (numbers.contains(winningNumber)) {
+        for (Integer winingNumber : winingLotto) {
+            if (numbers.contains(winingNumber)) {
                 count++;
             }
         }
         return count;
+    }
+
+    public boolean containBonus(int bonus) {
+        return numbers.contains(bonus);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(numbers);
     }
 
     // TODO: 추가 기능 구현

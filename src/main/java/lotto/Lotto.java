1926be7a583b62b4a045119b9edee3dda1fbb50f
jimmy0006 <jinmin645@gmail.com>
2023-11-07 01:47:00 +0900

    feat:lotto 객체 생성 시 숫자들 오름차순으로 정리, 중복 확인, 에러 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
@@ -8,20 +10,29 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         inputRangeValidate(numbers);
-        this.numbers = numbers;
+        inputDuplicateValidate(numbers);
+        List<Integer> modifyableList = new ArrayList<>(numbers);
+        modifyableList.sort(Comparator.naturalOrder());
+        this.numbers = modifyableList;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("크기가 맞지 않습니다.");
         }
     }
 
     private void inputRangeValidate(List<Integer> numbers){
         for (int number:numbers) {
             if(number<1 || number>45){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("범위에 맞지 않는 입력값입니다.");
             }
+        }
+    }
+
+    private void inputDuplicateValidate(List<Integer> numbers){
+        if(numbers.size()!=numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("같은 숫자가 존재합니다.");
         }
     }
 

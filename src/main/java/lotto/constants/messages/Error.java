706d4b1ebd38b4b3248a55b5c3d306ed303502a3
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-06 23:41:57 +0900

    feat(Error): 에러 메시지 이넘 클래스 추가

--- src/main/java/lotto/constants/messages/Error.java
+++ src/main/java/lotto/constants/messages/Error.java
@@ -1,16 +1,24 @@
 package lotto.constants.messages;
 
-public enum Error {
-    ERORR("[ERROR]");
 
+public enum Error {
+
+    NOT_DIGIT("숫자만 입력해주세요."),
+    MONEY_TOO_LARGE("금액이 너무 큽니다."),
+    NOT_DIVIDABLE("1000원 단위로 입력해주세요."),
+    MONEY_TOO_SMALL("금액이 너무 적습니다."),
+    LOTTO_NUMBER_OUT_OF_RANGE("유효하지 않은 로또 번호입니다."),
+    LOTTO_NOT_SIX_NUMBER("여섯 개의 수를 입력해 주세요"),
+    LOTTO_DUPLICATED_NUMBER("중복된 수는 입력할 수 없습니다.");
 
     private final String message;
+    private final String PREFIX = "[ERROR] ";
 
     Error(String message) {
         this.message = message;
     }
 
     public String getMessage() {
-        return this.message;
+        return PREFIX + this.message;
     }
 }

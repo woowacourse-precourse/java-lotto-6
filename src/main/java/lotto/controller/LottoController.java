25d877ae58072bde6fd015cce4e9277428a61887
Aeraekun <lsch3374@naver.com>
2023-11-06 21:20:13 +0900

    feat: LottoController.java ################ 목업 추가 ################
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,4 +5,28 @@ public class LottoController {
     public void start() {
 
     }
+
+    private void getMoney() {
+
+    }
+
+    private void createLotto() {
+
+    }
+
+    private void getNumber() {
+
+    }
+
+    private void getBonusNumber() {
+
+    }
+
+    private void calculateWin() {
+
+    }
+
+    private void calculateRate() {
+
+    }
 }

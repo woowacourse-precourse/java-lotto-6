343a321c2e89785ef4ec2763cdc960fa078b515b
NAM GIBEOM <rlqja0523@naver.com>
2023-11-07 01:22:49 +0900

    feat: 발행한 로또 수량 및 번호를 출력하는 기능 구현

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,7 +1,19 @@
 package lotto.ui;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import lotto.domain.LottoTicket;
+import lotto.dto.WinningResult;
+import lotto.enums.Prize;
+
 public class OutputView {
     public void printError(String errorMessage) {
         System.out.println(errorMessage);
+    }
+
+    public void printLottoTicket(LottoTicket lottoTicket) {
+        System.out.printf("%d개를 구매했습니다.%n", lottoTicket.count());
+        lottoTicket.lottoTicket().forEach(System.out::println);
+        System.out.println();
     }
 }

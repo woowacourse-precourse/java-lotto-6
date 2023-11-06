fc45be9f339e1facef612d140064c9c0c741065c
kylim99 <zxzx05050@naver.com>
2023-11-06 20:24:16 +0900

    feat: 로또 생성시 숫자가 중복이면 예외를 처리하는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,7 +2,10 @@ package lotto.model;
 
 import static lotto.util.Constant.LOTTO_LENGTH;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.util.ExceptionMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,9 +16,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_LENGTH) {
-            throw new IllegalArgumentException();
+        if (validateLength(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT_LENGTH.getMessage());
         }
+        if (validateDuplicate(numbers)){
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT_DUPLICATE.getMessage());
+        }
+    }
+
+    private boolean validateDuplicate(List<Integer> numbers) {
+        return  numbers.stream()
+                .distinct()
+                .collect(Collectors.toList())
+                .size() != LOTTO_LENGTH;
+    }
+
+    private boolean validateLength(List<Integer> numbers) {
+        return numbers.size() != LOTTO_LENGTH;
     }
 
     public List<Integer> getNumbers(){

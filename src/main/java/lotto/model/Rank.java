7a166f9aecccfd9bab718efd1587f990d556efbb
gonixxxx <akdntm946@gmail.com>
2023-11-07 00:21:55 +0900

    refactor(Rank): add Functional Interface field (isMatch)
    use BiPredicate to check bonus number is in lotto

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,28 +1,34 @@
 package lotto.model;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public enum Rank {
-    SIX(6, 2000000000),
-    FIVE_BONUS(7, 30000000),
-    FIVE(5, 1500000),
-    FOUR(4, 50000),
-    THREE(3, 5000),
-    ECT(0, 0);
+    SIX(6, 2000000000, (matchingNumber, isBonusContain) -> matchingNumber == 6),
+    FIVE_BONUS(5, 30000000,  (matchingNumber, isBonusContain) -> matchingNumber == 5 && isBonusContain),
+    FIVE(5, 1500000,  (matchingNumber, isBonusContain) -> matchingNumber == 5 && isBonusContain),
+    FOUR(4, 50000,  (matchingNumber, isBonusContain) -> matchingNumber == 4),
+    THREE(3, 5000,  (matchingNumber, isBonusContain) -> matchingNumber == 3),
+    ECT(0, 0,  (matchingNumber, isBonusContain) -> matchingNumber < 3);
 
     private final int matchingNumber;
     private final int prize;
+    private final BiPredicate<Integer, Boolean> isMatch;
 
-    Rank(int matchingNumber, int prize) {
+    Rank(int matchingNumber, int prize, BiPredicate<Integer, Boolean> isMatch) {
         this.matchingNumber = matchingNumber;
         this.prize = prize;
+        this.isMatch = isMatch;
     }
 
-    public static Rank getPrize(int number) {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> rank.matchingNumber == number)
+    public static Rank getPrize(int number, boolean isBonusContain) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.isMatch.test(number, isBonusContain))
                 .findAny()
                 .orElse(ECT);
     }
 
+    public int getPrize() {
+        return prize;
+    }
 }

d166803aefd995f6e4222ee6ad08dab67751e5f0
HYUNJUN\sajun <sajun28@naver.com>
2023-11-06 21:37:49 +0900

    Feat: "로또 중복 확인 기능 추가"

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateCheck(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +16,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6자리 입니다.");
         }
+    }
+
+    private void duplicateCheck(List<Integer> numbers) {
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 중복입니다.");
+        }
+        System.out.println(numberSet);
     }
 
     // TODO: 추가 기능 구현

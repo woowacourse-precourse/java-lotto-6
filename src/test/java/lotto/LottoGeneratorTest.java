f7ab2f76700c1e2d427ecdae6e66b26057de8f70
nill <wwaosldk@gmail.com>
2023-11-07 04:59:29 +0900

    test: defuplication numbers generator

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGeneratorTest {
+
+    @DisplayName("생성한 번호 개수 확인")
+    @Test
+    void generateNumbersSizeTest() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer> deduplicationNumbers = lottoGenerator.generateDeduplicationNumbers();
+        assertThat(deduplicationNumbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("생성한 번호 중복 확인")
+    @Test
+    void generateDeduplicationTest() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer> numbers = lottoGenerator.generateDeduplicationNumbers();
+        Set<Integer> deduplicationChecker = new HashSet<>(numbers);
+        assertThat(deduplicationChecker.size()).isEqualTo(6);
+    }
+
+    @DisplayName("배열 오름차순 정렬 확인")
+    @Test
+    void listSortTest() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(2);
+
+        Collections.sort(numbers);
+        assertThat(numbers).isEqualTo(List.of(1, 2, 3, 4));
+    }
+}

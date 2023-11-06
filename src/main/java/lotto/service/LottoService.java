39ac0b60788aef0336efc6576f196f4ebf9a10e0
King's meow <mingee641@gmail.com>
2023-11-06 17:25:29 +0900

    feat: 랜덤로또번호생성을 위한 makeRandomLotto 메서드 및 정렬을 위한 sorting 메서드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,10 @@
 package lotto.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import lotto.model.Lotto;
+import lotto.util.RandomUtil;
 import lotto.util.Validator;
 
 
@@ -11,11 +14,24 @@ public class LottoService {
 
 
 
+
     public int initMoneyToCount(String inputMoney) {
         int money = Validator.isNumber(inputMoney);
         return Validator.isDivide(money);
     }
 
+    public List<Integer> sorting(List<Integer> randomUniqueNumbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(randomUniqueNumbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
+    }
+
+    public void makeRandomLotto(List<Lotto> lottoNumbers, int count) {
+        for (int i = 0; i < count; i++) {
+            List<Integer> randomNumbers = sorting(RandomUtil.getRandomUniqueNumbers());
+            lottoNumbers.add(new Lotto(randomNumbers));
+        }
+    }
 
     public List<Integer> initWinningNumbers(String inputWinningNumbers) {
         String[] inputNumbers = inputWinningNumbers.split(",");

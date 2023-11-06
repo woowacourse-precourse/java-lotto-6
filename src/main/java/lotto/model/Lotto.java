9437ec965052ee895bb38359152c4c2422c57b40
SCY <helenason@naver.com>
2023-11-07 01:41:21 +0900

    feat: 입력받은 당첨 번호의 범위가 1에서 45 사이가 아닌 경우 예외 처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -8,6 +8,8 @@ public class Lotto {
     // 패키지 변경은 가능!
 
     private static final int WINNING_NUMBER_COUNT = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
 
     private final List<Integer> numbers;
 
@@ -18,11 +20,24 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateNumbersCount(numbers);
+        validateNumbersRange(numbers);
     }
 
     private void validateNumbersCount(List<Integer> numbers) {
         if (numbers.size() != WINNING_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 총 6개입니다.");
         }
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (isOutOfLottoNumbersRange(number)) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1에서 45 사이의 숫자입니다.");
+            }
+        }
+    }
+
+    private boolean isOutOfLottoNumbersRange(int number) {
+        return number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER;
     }
 }

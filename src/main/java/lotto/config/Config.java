acde8901340e3a3ec08cabd586a615264705ea10
King's meow <mingee641@gmail.com>
2023-11-06 17:24:30 +0900

    feat: 상수들을 위한 Config 클래스 추가

---
+++ src/main/java/lotto/config/Config.java
@@ -0,0 +1,12 @@
+package lotto.config;
+
+public class Config {
+
+    public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final int PRICE_UNIT = 1000;
+    public static final int MIN_LOTTO = 1;
+    public static final int MAX_LOTTO = 45;
+
+    public static final int COUNT_LOTTO = 6;
+    public static final int RANK_LOTTO = 5;
+}

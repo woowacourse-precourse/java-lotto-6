690f0b2b54e3e5ebae8846d6a6dec91cfa2899b6
Haeun Jeong <gkdms4248@gmail.com>
2023-11-07 00:25:56 +0900

    fix: 에러메세지 내용 수정

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -5,13 +5,13 @@ public enum ErrorMessage {
     DUPLICATE_NUMBER("로또 숫자가 중복됩니다."),
     NOT_IN_RANGE("로또 숫자가 1-45 사이의 숫자가 아닙니다."),
     NOT_SORT("로또 숫자가 정렬되어 있지 않습니다."),
-    INPUT_NOT_NUMBER("[ERROR] 숫자만 입력해주세요."),
-    INPUT_INVALID_NUMBER("[ERROR] 숫자의 크기가 너무 큽니다."),
-    INPUT_NOT_THOUSAND_UNIT("[ERROR] 구입 금액을 1000원 단위로 입력해주세요."),
-    INPUT_NOT_IN_RANGE("[ERROR] 당첨 번호가 1-45 사이의 숫자가 아닙니다."),
-    INPUT_OUT_OF_SIZE("[ERROR] 당첨 번호의 개수가 6개가 아닙니다."),
-    INPUT_DUPLICATE_NUMBER("[ERROR] 당첨 번호가 중복됩니다."),
-    INPUT_BONUS_DUPLICATE("[ERROR] 보너스 번호가 당첨 번호가 중복됩니다."),
+    INPUT_NOT_NUMBER("[ERROR] 숫자만 입력해야 합니다."),
+    INPUT_INVALID_NUMBER("[ERROR] 숫자의 크기가 더 작아야 합니다."),
+    INPUT_NOT_THOUSAND_UNIT("[ERROR] 구입 금액은 1000원 단위여야 합니다."),
+    INPUT_NOT_IN_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INPUT_OUT_OF_SIZE("[ERROR] 로또 번호의 개수는 6개여야 합니다."),
+    INPUT_DUPLICATE_NUMBER("[ERROR] 로또 번호는 중복되지 않아야 합니다."),
+    INPUT_BONUS_DUPLICATE("[ERROR] 보너스 번호가 로또 번호와 중복되지 않아야 합니다."),
     ;
 
     private final String message;

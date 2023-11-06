436ce3559c7ab92ce728f8dea2d552b1043b7da9
sangchu <didi1484@gmail.com>
2023-11-06 16:49:21 +0900

    fix: sort 기능 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,7 @@ import static lotto.constants.ExceptionMessage.IS_DUPLICATED;
 import static lotto.constants.ExceptionMessage.IS_INSUFFICIENT;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,7 +12,9 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicated(numbers);
-        numbers.stream().sorted();
+        numbers = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
         this.numbers = numbers;
     }
 

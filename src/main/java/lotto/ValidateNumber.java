56ba762535a5bf01074dd1ebd5d52d40f888d397
K-KY <kyuyoungk@naver.com>
2023-11-07 08:57:02 +0900

    Refactor(ValidateNumber) : 리턴 타입 변경, 파라미터 이름 변경

--- src/main/java/lotto/ValidateNumber.java
+++ src/main/java/lotto/ValidateNumber.java
@@ -1,11 +1,9 @@
 package lotto;
 
-import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
-
 public class ValidateNumber {
-    public static ThrowingCallable isNumber(String s) {
-        if (s.matches("\\d+")) {
-            return null;
+    public static void isNumber(String number) {
+        if (number.matches("\\d+")) {
+            return;
         }
         throw new IllegalArgumentException("[ERROR] 숫자만 입력 해주세요.");
     }

c1b265f3636ce1bd7af455d3283daa0d589dc19b
HYOAN <aspqll@naver.com>
2023-11-06 21:19:31 +0900

    feat(lotto): 구입금액을 입력받는 기능 구현, 구입금액이 1000원 단위인지 확인하는 기능 구현

--- src/main/java/lotto/domain/UserInput.java
+++ src/main/java/lotto/domain/UserInput.java
@@ -17,14 +17,27 @@ public class UserInput {
     }
 
     public int getUserPurchaseAmount(String inputPurchaseAmount) {
+        int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
+        try {
+            checkPurchaseAmount(purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return purchaseAmount;
+    }
+
+    public int checkPurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로만 입력 가능합나다.");
+        return purchaseAmount;
+    }
+
+    public int getUserWinningNumbers(String inputWinningNumbers) { // ","마다 따로 저장하기, 각 숫자는 1~45의 숫자이다.
+
         return 0;
     }
 
-    public int getUserWinningNumbers(String inputWinningNumbers) {
-        return 0;
-    }
-
-    public int getUserBonusNumber() {
+    public int getUserBonusNumber() { // 1~45의 숫자이다.
         return 0;
     }
 

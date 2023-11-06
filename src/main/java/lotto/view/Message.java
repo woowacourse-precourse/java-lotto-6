47cb0a82364ce9a3cb7640ee7c5f2c8376ba6d0c
JayG-5 <jg55552758@gmail.com>
2023-11-06 19:25:02 +0900

    chore: view/message 필드 이름 변경

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -5,8 +5,8 @@ public enum Message {
     REQUEST_NUMBERS("당첨 번호를 입력해 주세요."),
     REQUEST_NUMBER_BONUS("보너스 번호를 입력해 주세요."),
 
-    RESPONSE_CART("%d개를 구매했습니다."),
-    RESPONSE_NUMBERS("[%s]"),
+    RESPONSE_TICKETS("%d개를 구매했습니다."),
+    RESPONSE_TICKET_NUMBERS("[%s]"),
     RESPONSE_FINAL_TITLE("당첨 통계\n---"),
     RESPONSE_MATCHED("%d개 일치%s ($s원) - %d개"),
     RESPONSE_PNL("총 수익률은 %.1f%%입니다.");

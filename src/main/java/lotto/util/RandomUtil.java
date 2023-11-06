1d9a89cc5b841bafc74ea1d6f9752d490c4cdad4
jae woo Jung <awfjol2008@naver.com>
2023-11-06 19:48:52 +0900

    feat: 랜덤한 로또 번호 리스트를 생성하는 기능 구현

--- src/main/java/lotto/util/RandomUtil.java
+++ src/main/java/lotto/util/RandomUtil.java
@@ -9,4 +9,8 @@ public class RandomUtil {
     private RandomUtil() {
 
     }
+
+    public static List<Integer> generateRandomLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }

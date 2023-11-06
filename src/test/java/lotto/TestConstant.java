eb42aeb6cfcfb5bb266ddc989130d9ec5042c0d4
seminchoi <choicco89@gmail.com>
2023-11-06 16:11:51 +0900

    test(TestConstant) : 로또 최소 구매 가능 개수 변수 제거

--- src/test/java/lotto/TestConstant.java
+++ src/test/java/lotto/TestConstant.java
@@ -1,12 +1,9 @@
 package lotto;
 
 import lotto.config.TestLottoConfig;
-import lotto.constant.LottoConstraint;
 import java.util.List;
 
 public class TestConstant {
     public static final TestLottoConfig testLottoConfig = new TestLottoConfig();
     public static final List<Integer> normalNumbers = List.of(1, 2, 3, 4, 5, 6);
-    public static final int minPaymentAmount
-            = LottoConstraint.PRICE_PER_LOTTO.getValue() * LottoConstraint.MIN_PURCHASE_QUANTITY.getValue();
 }

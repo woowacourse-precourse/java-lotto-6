e0b512ac9f9807f50ca73892df65576d609091d0
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 15:35:55 +0900

    fix: Result 보너스 상황 고려

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,18 +1,20 @@
 package lotto;
 
 public enum Result {
-    FIRST_PRIZE(6, 2_000_000_000),
-    SECOND_PRIZE(5, 30_000_000),
-    THIRD_PRIZE(5, 1_500_000),
-    FOURTH_PRIZE(4, 50_000),
-    FIFTH_PRIZE(3, 5_000),
-    NO_PRIZE(0, 0);
+    FIRST_PRIZE(6, false, 2_000_000_000),
+    SECOND_PRIZE(5, true,30_000_000),
+    THIRD_PRIZE(5, false,1_500_000),
+    FOURTH_PRIZE(4, false, 50_000),
+    FIFTH_PRIZE(3, false,5_000),
+    NO_PRIZE(0, false,0);
 
     private final int matchingNumbers;
     private final int prizeAmount;
+    private final boolean bonus;
 
-    Result(int matchingNumbers, int prizeAmount) {
+    Result(int matchingNumbers, boolean bonus, int prizeAmount) {
         this.matchingNumbers = matchingNumbers;
+        this.bonus = bonus;
         this.prizeAmount = prizeAmount;
     }
 

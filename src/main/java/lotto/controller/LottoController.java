22c60d8d9db0795fd1a5727df3afe5fbb6b83a34
Seyeon <wjdtpdus828@naver.com>
2023-11-06 18:13:39 +0900

    feat(LottoController): startLotto 메소드 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,14 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.LottoView;
+
 public class LottoController {
+
+    private LottoView lottoView = new LottoView();
+    public void startLotto(){
+        inputPayLotto();
+    }
+
+
 }

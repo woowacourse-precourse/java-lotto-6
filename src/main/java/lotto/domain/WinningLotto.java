5d5c165bee96f5693c0147155f99c44b9386d454
jang010505 <jang010505@inu.ac.kr>
2023-11-07 05:57:21 +0900

    feat: WinningLottoTest 객체 내부 코드 작성

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.constant.ErrorMessageConstant.DUPLICATE_NUMBER_ERROR_MESSAGE;
+
+import java.util.List;
+
 public class WinningLotto {
     private final Lotto lotto;
     private final int bonusNumber;
@@ -11,11 +15,27 @@ public class WinningLotto {
     }
 
     private void validate(Lotto lotto, int bonusNumber) {
-
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE.getErrorMessage());
+        }
     }
 
     public LottoResult matchLotto(Lotto playerLotto) {
+        int count = matchNumber(playerLotto.getNumbers());
+        boolean bonus = matchBonus(playerLotto.getNumbers(), count);
 
-        return null;
+        return LottoResult.valueOf(count, bonus);
+    }
+
+    private boolean matchBonus(List<Integer> playerLotto, int count) {
+        return playerLotto.contains(bonusNumber) && count == 5;
+    }
+
+    private int matchNumber(List<Integer> playerLotto) {
+        List<Integer> winningLotto = lotto.getNumbers();
+
+        return (int) winningLotto.stream()
+                .filter(playerLotto::contains)
+                .count();
     }
 }

769360f7b433ecf86f0bd7569c0b6e5c4e2ce7d6
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-07 07:08:28 +0900

    refactor: LottoRank 등수 판별식을 속성으로 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,43 +1,48 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum LottoRank {
-    FIFTH(3, false, 5_000L),
-    FOURTH(4, false, 50_000L),
-    THIRD(5, false, 1_500_000L),
-    SECOND(5, true, 30_000_000L),
-    FIRST(6, false, 2_000_000_000L),
-    NONE(0, false, 0L),
+    FIFTH(3, false, 5_000L, (sameNumberCount, bonusMatch) -> sameNumberCount == 3),
+    FOURTH(4, false, 50_000L, (sameNumberCount, bonusMatch) -> sameNumberCount == 4),
+    THIRD(5, false, 1_500_000L, (sameNumberCount, bonusMatch) -> sameNumberCount == 5 && !bonusMatch),
+    SECOND(5, true, 30_000_000L, (sameNumberCount, bonusMatch) -> sameNumberCount == 5 && bonusMatch),
+    FIRST(6, false, 2_000_000_000L, (sameNumberCount, bonusMatch) -> sameNumberCount == 6),
+    NONE(0, false, 0L, (sameNumberCount, bonusMatch) -> sameNumberCount <= 2),
     ;
 
     private final int matchingCount;
-    private final boolean bonusNumberMatch;
+    private final boolean bonusMustMatch;
     private final long prize;
+    private final BiFunction<Integer, Boolean, Boolean> winCondition;
 
-    LottoRank(int matchingCount, boolean bonusNumberMatch, long prize) {
+    LottoRank(int matchingCount, boolean bonusMustMatch, long prize,
+              BiFunction<Integer, Boolean, Boolean> winCondition) {
         this.matchingCount = matchingCount;
-        this.bonusNumberMatch = bonusNumberMatch;
+        this.bonusMustMatch = bonusMustMatch;
         this.prize = prize;
+        this.winCondition = winCondition;
     }
 
     public static LottoRank of(int matchingCount, boolean bonusNumberMatch) {
-        if (bonusNumberMatch && matchingCount == LottoRank.SECOND.getMatchingCount()) {
-            return SECOND;
-        }
 
         return Arrays.stream(values())
-                .filter(rank -> rank.matchingCount == matchingCount)
+                .filter(rank -> rank.conditionMatch(matchingCount, bonusNumberMatch))
                 .findFirst()
                 .orElse(NONE);
+    }
+
+    private boolean conditionMatch(int matchingCount, boolean bonusMatch) {
+        return winCondition.apply(matchingCount, bonusMatch);
     }
 
     public int getMatchingCount() {
         return matchingCount;
     }
 
-    public boolean getBonusNumberMatch() {
-        return bonusNumberMatch;
+    public boolean getBonusMustMatch() {
+        return bonusMustMatch;
     }
 
     public long getPrize() {

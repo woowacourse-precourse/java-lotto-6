f2484668292dfd84ee45be41e8b93c50ec202524
Gyeongho Yang <chchch1213@naver.com>
2023-11-06 18:58:21 +0900

    feat(view): 인자를 포함한 일반 메세지 출력과 에러 메세지 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import lotto.constant.ErrorMessage;
 import lotto.constant.GeneralMessage;
 
 public class OutputView {
     public static void printGeneralMessage(GeneralMessage generalMessage) {
         System.out.println(generalMessage.getMessage());
+    }
+
+    public static void printGeneralMessage(GeneralMessage generalMessage, Object... args) {
+        System.out.printf(generalMessage.getMessage() + "%n", args);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.print(ErrorMessage.PREFIX);
+        System.out.println(message);
     }
 }

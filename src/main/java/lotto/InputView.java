62fcc30002d4916a7ae91c3e34f7faca5087f3e2
yooyoungpark <yoongtopiaa@gmail.com>
2023-11-06 16:44:46 +0900

    feat : 입력 에러시 예외 메시지를 출력하고 재시도 한다.

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -11,39 +11,55 @@ public class InputView {
     }
 
     public static PurchasePrice inputPurchasePrice() {
-        System.out.println("구입금액을 입력해 주세요.");
         try {
+            System.out.println("구입금액을 입력해 주세요.");
             String userInput = getUserInput();
-            int userIntegerInput = Integer.parseInt(userInput);
-            return new PurchasePrice(userIntegerInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 정수로 입력해야 합니다.");
+            return new PurchasePrice(inputToNumber(userInput));
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputPurchasePrice();
         }
     }
 
     public static WinningNumber inputWinningNumber() {
-        System.out.println();
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String userInput = Console.readLine();
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (String userInputSeperated : userInput.split(",")) {
-            LottoNumber lottoNumber = toLottoNumber(userInputSeperated);
-            lottoNumbers.add(lottoNumber);
-        }
+        try {
+            System.out.println("\n당첨 번호를 입력해 주세요.");
+            String userInput = Console.readLine();
+            List<LottoNumber> lottoNumbers = new ArrayList<>();
+            for (String userInputSeperated : userInput.split(",")) {
+                LottoNumber lottoNumber = toLottoNumber(userInputSeperated);
+                lottoNumbers.add(lottoNumber);
+            }
 
-        System.out.println();
-        System.out.println("보너스 번호를 입력해주세요.");
-        String userInputBonusNumber = Console.readLine();
-        LottoNumber bonusLottoNumber = toLottoNumber(userInputBonusNumber);
-        return new WinningNumber(lottoNumbers, bonusLottoNumber);
+            LottoNumber bonusLottoNumber = inputBonusNumber();
+            return new WinningNumber(lottoNumbers, bonusLottoNumber);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputWinningNumber();
+        }
+    }
+
+    private static LottoNumber inputBonusNumber() {
+        try {
+            System.out.println("\n보너스 번호를 입력해주세요.");
+            String userInputBonusNumber = Console.readLine();
+            return toLottoNumber(userInputBonusNumber);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputBonusNumber();
+        }
     }
 
     private static LottoNumber toLottoNumber(String userInputNumber) {
+        int number = inputToNumber(userInputNumber);
+        return new LottoNumber(number);
+    }
+
+    private static int inputToNumber(String userInput) {
         try {
-            int number = Integer.parseInt(userInputNumber);
-            return new LottoNumber(number);
+            return Integer.parseInt(userInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
         }
     }
 }
--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -22,4 +22,8 @@ public class OutputView {
             System.out.println(numbers);
         }
     }
+
+    public static void printError(String errorMessage) {
+        System.out.println("[ERROR] " + errorMessage);
+    }
 }

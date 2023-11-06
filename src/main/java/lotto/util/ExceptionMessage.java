f4bb83ca9fa38ee57522cdf5eadad629563596cf
noyes5 <noyes5@naver.com>
2023-11-07 00:01:02 +0900

    refactor: Exception 메시지 리팩토링

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -3,8 +3,9 @@ package lotto.util;
 import static lotto.util.Constants.LOTTO_ONE_GAME_PRICE;
 
 public enum ExceptionMessage {
-    INVALID_UNIT_OF_BUDGET(String.format("금액은 %d원 단위로 입력해야 합니다.", LOTTO_ONE_GAME_PRICE)),
-    INVALID_INPUT_NUMERIC("입력값은 숫자만 들어올 수 있습니다.");
+    INVALID_LOTTO_SIZE("로또 번호는 6개로 구성되어야 합니다."),
+    INVALID_INPUT_NUMERIC("입력값은 숫자만 들어올 수 있습니다."),
+    INVALID_UNIT_OF_BUDGET(String.format("금액은 %d원 단위로 입력해야 합니다.", LOTTO_ONE_GAME_PRICE));
 
     public static final String BASE_MESSAGE = "[ERROR] %s";
     private final String message;

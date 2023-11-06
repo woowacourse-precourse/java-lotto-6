f12f864e69c8eddc9df0775fe2750e6d4ab6069e
SeungGwa123 <skwan123@naver.com>
2023-11-07 02:18:02 +0900

    Fix: Fix lottoBuyMoney
    lottoBuyMoney의 return자료형을 int로 변경하였고 Game쪽에서 출력하는 부분과 구매하는 부분을 실행하도록 여기서는 삭제

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -8,19 +8,16 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
     private static int money;
-    private static int lotto_number;
     private static Output output = new Output();
     private static Except except = new Except();
-    private static BuyLotto buylotto = new BuyLotto();
 
-    public void lottoBuyMoney() {
+
+    public int lottoBuyMoney() {
         do {
             output.buyMoneyAsk();
             String money_string = readLine();
             money = except.outOfRangeMoney(money_string);
         } while (money < 0);
-        lotto_number = money / 1000;
-        output.buyNumberPaperAns(lotto_number);
-        buylotto.buyAllLotto(lotto_number);
+        return money / 1000;
     }
 }

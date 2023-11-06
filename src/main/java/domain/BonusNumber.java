4fedc2e5ad7cb66a03b409b0c4c0fe36244b25aa
ddohyeong <dohyeong7026@gmail.com>
2023-11-06 20:48:39 +0900

    feat(BonusNumber) : 보너스 번호가 1~45 사이의 값인지 검증 기능 구현
    docs : 보너스 번호가 1~45 사이의 값인지 기능 완료 체크

--- src/main/java/domain/BonusNumber.java
+++ src/main/java/domain/BonusNumber.java
@@ -5,7 +5,10 @@ public class BonusNumber {
 
 	public BonusNumber(String bonusNumber) {
 		validateIsDigit(bonusNumber);
-		this.bonusNumber = convertStringToInt(bonusNumber);
+		int convertedBonusNumber = convertStringToInt(bonusNumber);
+		validateIsBetweenLottoRange(convertedBonusNumber);
+
+		this.bonusNumber = convertedBonusNumber;
 	}
 
 	public int getBonusNumber() {
@@ -18,6 +21,12 @@ public class BonusNumber {
 
 	private void validateIsDigit(String bonusNumber) {
 		if (!isDigit(bonusNumber)) {
+			throw new IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해주세요");
+		}
+	}
+
+	private void validateIsBetweenLottoRange(int bonusNumber) {
+		if (!(1 <= bonusNumber && bonusNumber <= 45)) {
 			throw new IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해주세요");
 		}
 	}
--- src/test/java/domain/BonusNumberTest.java
+++ src/test/java/domain/BonusNumberTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,6 +12,17 @@ class BonusNumberTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"a", "!", " ", ""})
 	public void checkBonusNumberIsDigit(String input) {
+		// when & then
+		assertThatThrownBy(() -> new BonusNumber(input))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("보너스 번호가 1~45 사이의 값인지 검증 아니라면 예외 발생")
+	@Test
+	public void checkOutOfRangeNumbers() {
+		// given
+		String input = "46";
+
 		// when & then
 		assertThatThrownBy(() -> new BonusNumber(input))
 				.isInstanceOf(IllegalArgumentException.class);

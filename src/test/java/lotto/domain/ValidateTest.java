0a31dfb0b23fd57745de4611fdade88643dc3888
hakro <leehr125@naver.com>
2023-11-07 06:20:01 +0900

    docs(README) : 예외처리 관련 확인 기능 추가

--- src/test/java/lotto/domain/ValidateTest.java
+++ src/test/java/lotto/domain/ValidateTest.java
@@ -7,24 +7,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidateTest {
+    final int ERROR_NUMBER = -1;
     Validate validate = new Validate();
 
+    // InputPrice
     @DisplayName("입력받은 값이 숫자가 아닌경우")
     @Test
     void validateInputPrice_예외_테스트_Not_Number() {
         final String INPUT = "삼천원";
-        final long RESULT = -1;
 
-        assertThat(validate.validateInputPrice(INPUT)).isEqualTo(RESULT);
+        assertThat(validate.validateInputPrice(INPUT)).isEqualTo(ERROR_NUMBER);
     }
 
     @DisplayName("입력 받은 값이 1000에 나누어 떨어지지 않는 경우")
     @Test
     void validateInputPrice_예외_테스트_Can_Not_Divide() {
         final String INPUT = "1300";
-        final long RESULT = -1;
 
-        assertThat(validate.validateInputPrice(INPUT)).isEqualTo(RESULT);
+        assertThat(validate.validateInputPrice(INPUT)).isEqualTo(ERROR_NUMBER);
     }
 
     @DisplayName("입력 받은 값이 정상일 경우")
@@ -34,5 +34,30 @@ public class ValidateTest {
         final long RESULT = 10000;
 
         assertThat(validate.validateInputPrice(INPUT)).isEqualTo(RESULT);
+    }
+
+    // WinnerNumbers
+    @DisplayName("입력 값에 문자가 들어왔을 경우")
+    @Test
+    void validateWinnerNumbers_예외_테스트_Not_Number() {
+        final String INPUT = "1,2,3,4,오,6";
+
+        assertThat(validate.validateWinnerNumbers(INPUT)).contains(ERROR_NUMBER);
+    }
+
+    @DisplayName("입력 값에 범위 밖의 숫자를 입력한 경우")
+    @Test
+    void validateWinnerNumbers_예외_테스트_Incorrect_Range_Number() {
+        final String INPUT = "1,2,3,46,5,6";
+
+        assertThat(validate.validateWinnerNumbers(INPUT)).contains(ERROR_NUMBER);
+    }
+
+    @DisplayName("입력 값에 중복된 숫자를 입력한 경우")
+    @Test
+    void validateWinnerNumbers_예외_테스트_Check_Duplicated() {
+        final String INPUT = "1,2,2,4,5,6";
+
+        assertThat(validate.validateWinnerNumbers(INPUT)).contains(ERROR_NUMBER);
     }
 }

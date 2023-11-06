7d364aa0117f0a9e21778bdbe91b38e674e08e6e
Han <yulmam1@naver.com>
2023-11-06 18:34:37 +0900

    feat(WinningNumbers) : 당첨 번호의 숫자 범위를 넘어갈 경우 예외처리 하는 기능 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -2,11 +2,25 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.util.Parser.stringToInt;
+import static lotto.util.Parser.stringToList;
+
 public class WinningNumbers {
     List<Integer> winningNumbers;
 
     public WinningNumbers(List<Integer> winningNumbers) {
+        validate(winningNumbers);
         this.winningNumbers = winningNumbers;
     }
 
+    private void validate(List<Integer> winningNumbers) {
+        validateNumberBoundary(winningNumbers);
+    }
+
+    private void validateNumberBoundary(List<Integer> winningNumbers) {
+        for(int num : winningNumbers) {
+            if(num<1 || num>45)
+                throw new IllegalArgumentException();
+        }
+    }
 }

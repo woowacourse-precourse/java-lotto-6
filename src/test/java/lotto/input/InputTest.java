3d4485bacc44665c116ddc715e3ddf471d92c41b
bamm2 <xxknu88@naver.com>
2023-11-06 18:15:54 +0900

    test(InputTest): 1,000원 단위로 나누어 떨어지지 않는 금액에 대한 테스트

--- src/test/java/lotto/input/InputTest.java
+++ src/test/java/lotto/input/InputTest.java
@@ -2,6 +2,7 @@ package lotto.input;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static lotto.exception.ExceptionMessage.DIFFERENT_FORMAT_EXCEPTION_MESSAGE;
 import static lotto.exception.ExceptionMessage.NOT_NUMBER_EXCEPTION_MESSAGE;
 import static lotto.exception.ExceptionMessage.NULL_POINTER_EXCEPTION_MESSAGE;
 
@@ -32,11 +33,12 @@ public class InputTest {
     @DisplayName("입력에 Null값이 들어가는 경우")
     public void Null_입력_예외(String input) {
         assertThatThrownBy(() -> inputValidator.validateNotNull(input))
-                .isInstanceOf(RuntimeException.class)
+                .isInstanceOf(LottoApplicationException.class)
                 .hasMessageContaining(NULL_POINTER_EXCEPTION_MESSAGE.getMessage());
     }
+
     @ParameterizedTest
-    @ValueSource(strings = {"1a2b3c,.2.,3 c"})
+    @ValueSource(strings = {"1a2b3c",".2.","3 c"})
     @DisplayName("입력에 숫자외 문자가 들어갈 경우")
     public void 숫자_외_문자_입력_테스트(String input) {
         assertThatThrownBy(() -> inputValidator.validateNumber(input))
@@ -44,7 +46,14 @@ public class InputTest {
                 .hasMessageContaining(NOT_NUMBER_EXCEPTION_MESSAGE.getMessage());
     }
 
-
+    @ParameterizedTest
+    @ValueSource(strings = {"1111","0","1023","100000001","999","111"})
+    @DisplayName("금액이 1,000원 단위가 아닌 입력시, 예외 테스트")
+    public void 금액_단위_테스트(String input) {
+        assertThatThrownBy(() -> inputValidator.validatePurchasePrice(input))
+                .isInstanceOf(LottoApplicationException.class)
+                .hasMessageContaining(DIFFERENT_FORMAT_EXCEPTION_MESSAGE.getMessage());
+    }
 
 
 }

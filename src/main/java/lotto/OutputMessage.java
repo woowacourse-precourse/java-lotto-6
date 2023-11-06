c3183214c00008db95ea6ac68443568aaa333906
sunwooiljoon <wnsdl1659@naver.com>
2023-11-06 15:08:20 +0900

    refactor : OutputMessage 수정
    금액 하드코딩 -> Prize enum을 이용하여 포맷 후 출력

--- src/main/java/lotto/OutputMessage.java
+++ src/main/java/lotto/OutputMessage.java
@@ -1,18 +1,19 @@
 package lotto;
 
+import java.text.DecimalFormat;
+
 public enum OutputMessage {
     REQUEST_MONEY_TO_BUY("구입금액을 입력해 주세요."),
     BUY_LOTTO_RESULT("%d개를 구매했습니다."),
     REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     LOTTO_GRADE_RESULT(
-            "3개 일치 (5,000원) - %d개\n"
-            + "4개 일치 (50,000원) - %d개\n"
-            + "5개 일치 (1,500,000원) - %d개\n"
-            + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
-            + "6개 일치 (2,000,000,000원) - %d개\n"),
-    RATE_OF_RETURN("총 수익률은 %s%%입니다.\n")
-    ;
+            "3개 일치 (" + getFormattedNumber(Prize.THREE_MATCH) + "원) - %d개\n"
+                    + "4개 일치 (" + getFormattedNumber(Prize.FOUR_MATCH) + "원) - %d개\n"
+                    + "5개 일치 (" + getFormattedNumber(Prize.FIVE_MATCH) + "원) - %d개\n"
+                    + "5개 일치, 보너스 볼 일치 (" + getFormattedNumber(Prize.FIVE_AND_BONUS_MATCH) + "원) - %d개\n"
+                    + "6개 일치 (" + getFormattedNumber(Prize.SIX_MATCH) + "원) - %d개\n"),
+    RATE_OF_RETURN("총 수익률은 %s%%입니다.\n");
 
     private final String message;
 
@@ -22,5 +23,10 @@ public enum OutputMessage {
 
     public final String getMessage() {
         return message;
+    }
+
+    // 숫자를 3자리 마다 ',' 찍어 주는 함수
+    private static String getFormattedNumber(Prize prize) {
+        return DecimalFormat.getInstance().format(prize.getPrice());
     }
 }

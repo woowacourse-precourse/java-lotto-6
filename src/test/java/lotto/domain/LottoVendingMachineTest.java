46f9df8e0375b001ed87702cf3a2f29a8aa6736f
junOTL <naqp99@gmail.com>
2023-11-06 19:13:39 +0900

    test(LottoVendingMachineTest): 1에서 45사이 무작위 값 반환 테스트

---
+++ src/test/java/lotto/domain/LottoVendingMachineTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LottoVendingMachineTest {
+    private LottoVendingMachine lottoVendingMachine;
+
+    @BeforeEach
+    void init() {
+        lottoVendingMachine = new LottoVendingMachine();
+    }
+
+    @Test
+    @DisplayName("1에서 45사이 무작위 값 반환 테스트")
+    void randomValueBetween1And45Test() {
+        List<Integer> randomNumbers = lottoVendingMachine.getRandomNumbers();
+
+        for (int randomNumber : randomNumbers) {
+            assertThat(randomNumber).isBetween(1, 45);
+        }
+    }
+}

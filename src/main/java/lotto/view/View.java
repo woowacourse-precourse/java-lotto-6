bea9a55fb338ba93b7514edfde8fbbbbef1195c1
ay-eonii <naayen@naver.com>
2023-11-06 23:32:04 +0900

    feat(view): 당첨 번호 입력

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,9 +1,16 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.view.constant.Exception;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.view.constant.Exception.COST_FORMAT;
+import static lotto.view.constant.Exception.LOTTO_FORMAT;
 
 public class View {
+    private final static String SPLIT_SYMBOL = ",";
     public void output(String message) {
         System.out.println(message);
     }
@@ -12,7 +19,29 @@ public class View {
         try {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(Exception.COST_FORMAT);
+            throw new IllegalArgumentException(COST_FORMAT);
+        }
+    }
+
+    public List<Integer> inputMatch() {
+        try {
+            String match = Console.readLine();
+            return formatInput(match);
+        } catch (IllegalArgumentException e) {
+            output(e.getMessage());
+            return inputMatch();
+        }
+    }
+
+    private List<Integer> formatInput(String match) {
+        String inputWithoutSpace = match.replaceAll("\\s+", "");
+        String[] matches = inputWithoutSpace.split(SPLIT_SYMBOL);
+        try {
+            return Arrays.stream(matches)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LOTTO_FORMAT);
         }
     }
 }
--- src/main/java/lotto/view/constant/Exception.java
+++ src/main/java/lotto/view/constant/Exception.java
@@ -3,4 +3,6 @@ package lotto.view.constant;
 public class Exception {
     public final static String COST_MOD = "[ERROR] 1000원 단위로 입력해주세요.";
     public final static String COST_FORMAT = "[ERROR] 20억 이하의 숫자를 입력해주세요.";
+    public final static String LOTTO_FORMAT = "[ERROR] 로또 번호는 숫자여야 합니다.";
+
 }

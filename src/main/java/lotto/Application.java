7b23c50ec24e2ae1001996f12e683d7fae8fbb17
SeungGwa123 <skwan123@naver.com>
2023-11-07 06:56:00 +0900

    Feat: Add main
    전체 게임을 실행시키는 코드 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import Game.Game;
+import Input.Input;
+
 public class Application {
     public static void main(String[] args) {
+        Game game = new Game();
+        game.run();
         // TODO: 프로그램 구현
     }
 }

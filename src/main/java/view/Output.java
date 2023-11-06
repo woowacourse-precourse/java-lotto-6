b6c3e6ba3c5594e92c9d432e49ce9add008481ba
zzawang <hobin0116@naver.com>
2023-11-06 18:32:15 +0900

    feat: 로또 당첨 내역 출력 기능 추가
    - 필요한 상수 정의
    - Rank (등수), 등수의 개수(count), 그리고 상금 금액(prizeAmount)을 받아서 출력해야 하는 메시지를 결정한다.
    - rank가 Rank.SECOND와 같은 경우, 두 번째 등수 상금 설명(SECOND_PRIZE_DESCRIPTION)을 출력한다.
    - 그 외의 경우 (Rank.FAIL이 아닌 경우) 기본 상금 설명(PRIZE_DESCRIPTION)을 출력한다.
    - 상금은 NumberFormat을 사용하여 표시한다.

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,15 +1,22 @@
 package view;
 
+import java.text.NumberFormat;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 import lotto.Lotto;
+import lotto.Rank;
 
 public class Output {
     private static final String PURCHASE_AMOUNT_MESSAGE = "%d개를 구매했습니다.";
     private static final String LOTTO_NUMBERS_SEPARATOR = "[%s]";
     private static final String LOTTO_NUMBERS_JOIN_SEPARATOR = ", ";
     private static final String LINE_BREAK = "\n";
+    private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String WINNING_STATISTICS_SEPARATOR = "---";
+    private static final String PRIZE_DESCRIPTION = "%d개 일치 (%s원) - %d개";
+    public static final String SECOND_PRIZE_DESCRIPTION = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
 
     public static void getLottoNumbers(List<Lotto> lottoList) {
         System.out.printf(LINE_BREAK + PURCHASE_AMOUNT_MESSAGE + LINE_BREAK, lottoList.size());
@@ -25,5 +32,25 @@ public class Output {
         return numbers.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(LOTTO_NUMBERS_JOIN_SEPARATOR));
+    }
+
+    public static void printLottoGameResult() {
+        System.out.println(LINE_BREAK + WINNING_STATISTICS_MESSAGE);
+        System.out.println(WINNING_STATISTICS_SEPARATOR);
+    }
+
+    public static void handlePrizeDescription(Rank rank, long rankCount, long prizeAmount) {
+
+        if (rank.equals(Rank.SECOND)) {
+            printPrizeDescription(rank, rankCount, prizeAmount, SECOND_PRIZE_DESCRIPTION);
+        } else if (!rank.equals(Rank.FAIL)) {
+            printPrizeDescription(rank, rankCount, prizeAmount, PRIZE_DESCRIPTION);
+        }
+    }
+
+    private static void printPrizeDescription(Rank rank, long rankCount, long prizeAmount, String description) {
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.KOREA);
+        System.out.printf(description + LINE_BREAK, rank.getMatchCount(),
+                numberFormat.format(prizeAmount), rankCount);
     }
 }

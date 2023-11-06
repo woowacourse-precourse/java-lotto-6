0975fc0cdb7f7b22517753227fe822895ab3c31d
kangyuseok <kingyuseok@naver.com>
2023-11-06 20:21:06 +0900

    Feat: 구입금액을 묻는 문구와 입력 기능 구현

--- src/main/java/lotto/view/IO.java
+++ src/main/java/lotto/view/IO.java
@@ -1,5 +1,11 @@
 package lotto.view;
 
-public class IO {
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.constants.Front;
 
+public class IO {
+    public static String requestPerchaseAmount(Front message) {
+        System.out.println(message.getMessage());
+        return Console.readLine();
+    }
 }

2853bb89912781cc816f903c0283ec8c958be68f
MinseokGo <rhalstjr1999@naver.com>
2023-11-06 23:44:04 +0900

    feat($OutputView): 출력 기능 추가
    당첨 내역과 수익률을 출력하는 기능을 추가하였습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,20 @@
 package lotto.view;
 
 import java.util.List;
+import lotto.model.Referee;
 import lotto.model.Lotto;
 
 public class OutputView {
     public void printCreateLottoNumbers(final List<Lotto> lottos) {
         System.out.printf("\n%d개를 구매했습니다.\n", lottos.size());
         lottos.forEach(lotto -> System.out.println(lotto.toString()));
+    }
+
+    public void printPrizeResult(Referee referee) {
+        System.out.println(referee);
+    }
+
+    public void printResult(final double rateOfReturn) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
     }
 }

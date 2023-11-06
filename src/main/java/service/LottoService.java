f5ae4a42b92e6c0eb74b482fd2d92196563789ba
MoonJaeGyeong <bblbblan69@gmail.com>
2023-11-06 21:46:49 +0900

    <feat> : <총 수익률을 얻는 메서드 추가>
    자기가 쓴 돈 대비 얻은 돈의 비율을 계산하는 메서드 추가

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -8,6 +8,7 @@ import view.constant.ConstantMoney;
 
 public class LottoService {
     private static int MINIMUM_NUMBER_OF_WINNINGS = 3;
+    private static int LOTTO_PRICE = 1000;
     public int[] revealLottery(Lottos lottos, LottoSystem lottoSystem){
         int[] winningResult = {0,0,0,0,0};
 
@@ -41,7 +42,17 @@ public class LottoService {
         }
         return 0;
     }
-
-   
-
+    public double getEarningRate(int[] lottoResult, Lottos lottos){
+        return getTotalWinningMoney(lottoResult) / getTotalMYMoney(lottos);
+    }
+    private double getTotalMYMoney(Lottos lottos){
+        return lottos.getLottosSize() * LOTTO_PRICE;
+    }
+    private double getTotalWinningMoney(int[] lottoResult){
+        double totalMyMoney = 0;
+        for(int i=0; i<lottoResult.length; i++){
+            totalMyMoney += lottoResult[i] * ConstantMoney.getLottoResultMoney(i);
+        }
+        return totalMyMoney;
+    }
 }
--- src/main/java/view/constant/ConstantMoney.java
+++ src/main/java/view/constant/ConstantMoney.java
@@ -2,14 +2,22 @@ package view.constant;
 
 public enum ConstantMoney {
 
-    THREE_MATCHES_MONEY(5000),
-    FOUR_MATCHES_MONEY(50000),
-    FIVE_MATCHES_MONEY(1500000),
-    FIVE_BONUS_MATCHES_MONEY(30000000),
-    SIX_MATCHES_MONEY(2000000000);
+    THREE_MATCHES_MONEY_INT(5000),
+    FOUR_MATCHES_MONEY_INT(50000),
+    FIVE_MATCHES_MONEY_INT(1500000),
+    FIVE_BONUS_MATCHES_MONEY_INT(30000000),
+    SIX_MATCHES_MONEY_INT(2000000000);
 
 
     private final int money;
+
+    private static final ConstantMoney[] LottoResultMoney = {
+            THREE_MATCHES_MONEY_INT, FOUR_MATCHES_MONEY_INT, FIVE_MATCHES_MONEY_INT, FIVE_BONUS_MATCHES_MONEY_INT,SIX_MATCHES_MONEY_INT
+    };
+
+    public static int getLottoResultMoney(int i) {
+        return LottoResultMoney[i].getMoney();
+    }
 
     ConstantMoney(int money){
         this.money = money;

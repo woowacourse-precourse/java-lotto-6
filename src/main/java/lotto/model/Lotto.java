cf2b00106b3fd5301f9157a1e0511f8693b9ed93
HSBODo <vinsulill@gmaile.com>
2023-11-06 16:33:36 +0900

    refactor : Lotto 클래스 메서드 추가 및 수정
    추가
    - lottoNumbersSort() 오름차순 정렬 메서드 추가
    수정
    - validate() 메서드 로또 자리수 검증과 중복체크 분리
    - 생성자를 통하여 인스턴스 생성 시 검증 및 자동 정렬

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringJoiner;
 
@@ -7,24 +9,36 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        numbers = new ArrayList<>(numbers);
         validate(numbers);
+        lottoNumbersSort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         long removeDuplicationNumbersSize = numbers.stream().distinct().count();
 
-        if (removeDuplicationNumbersSize != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호 6자리를 입력하세요.");
+        }
+
+        if(removeDuplicationNumbersSize != numbers.size()){
+            throw new IllegalArgumentException("로또 번호에 중복이 있습니다.");
         }
     }
 
+    // TODO: 추가 기능 구현
     public List<Integer> getNumbers (){
         return this.numbers;
     }
 
+    private void lottoNumbersSort(List<Integer> numbers){
+        Collections.sort(numbers);
+    }
+
     public void lottoNumberDisplay(){
         List<Integer> lottoNumbers = getNumbers();
+
         StringJoiner displayLottoNumber = new StringJoiner(", ","[","]");
         for(int i = 0 ; i < lottoNumbers.size() ; i ++){
             String number = String.valueOf(lottoNumbers.get(i));
@@ -32,6 +46,4 @@ public class Lotto {
         }
         System.out.println(displayLottoNumber);
     }
-
-    // TODO: 추가 기능 구현
 }

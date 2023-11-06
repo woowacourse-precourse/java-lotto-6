86cec9f492fccddbab864a07a32af42905ebc79d
msung99 <msung6924@naver.com>
2023-11-06 10:09:01 +0900

    test: WinningRank 에서 입력으로 주어진 맞춘 개수와 보너스 여부와 일치하는 것이 없다면 낫싱을 반환하는지 테스트

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -10,11 +10,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningLottoTest {
+    private Lotto lotto;
+    private Bonus bonus;
+
     @DisplayName("보너스 번호가 로또 당첨 번호와 중복되는 경우 예외가 발생한다.")
     @Test
-    void createWinningLotto(){
-        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
-        Bonus bonus = Bonus.from("1");
+    void createWithDuplicateBonusNumber(){
+        lotto = new Lotto(List.of(1,2,3,4,5,6));
+        bonus = Bonus.from("1");
 
         assertThatThrownBy(() -> WinningLotto.of(lotto, bonus))
                 .isInstanceOf(IllegalArgumentException.class);
---
+++ src/test/java/lotto/WinningRankTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import lotto.domain.WinningRank;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningRankTest {
+
+    @DisplayName("입력으로 주어진 맞춘 개수와 보너스 여부와 일치하는 것이 없다면 낫싱을 반환한다.")
+    @Test
+    void createWinningRankNothing(){
+        int match = 6;
+        boolean isBonus = true;
+        Assertions.assertThat(WinningRank.from(match, isBonus))
+                .isEqualTo(WinningRank.NOTHING);
+    }
+}

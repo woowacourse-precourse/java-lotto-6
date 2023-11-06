a1c936fab7b8ab8d94f581e9291b7596a705fb65
tsulocalize <tsulocalizing@gmail.com>
2023-11-07 08:24:41 +0900

    feat : checkLottos 기능 구현

--- src/main/java/lotto/LottoAdmin.java
+++ src/main/java/lotto/LottoAdmin.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoAdmin {
@@ -9,5 +10,44 @@ public class LottoAdmin {
     public LottoAdmin(List<Integer> winningNumbers, Integer bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public List<LottoRank> checkLottos(LottoPlayer player) {
+        List<LottoRank> ranks = new ArrayList<>();
+        for(Lotto lotto : player.getLottos()) {
+            ranks.add(getRank(lotto));
+        }
+        return ranks;
+    }
+
+    private LottoRank getRank(Lotto lotto) {
+        int correct = checkNumber(lotto);
+        boolean isBonus = checkBonus(lotto);
+
+        if (correct == 6) return LottoRank.FIRST;
+        if (correct == 5 && isBonus) return LottoRank.SECOND;
+        if (correct == 5) return LottoRank.THIRD;
+        if (correct == 4) return LottoRank.FOURTH;
+        if (correct == 3) return LottoRank.FIFTH;
+        return LottoRank.NONE;
+    }
+
+    private int checkNumber(Lotto lotto) {
+        int correct = 0;
+        for (Integer number : lotto.getNumbers()) {
+            if (winningNumbers.contains(number)) {
+                correct += 1;
+            }
+        }
+        return correct;
+    }
+
+    private boolean checkBonus(Lotto lotto) {
+        for (Integer number : lotto.getNumbers()) {
+            if (bonusNumber.equals(number)) {
+                return true;
+            }
+        }
+        return false;
     }
 }

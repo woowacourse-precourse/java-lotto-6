c4afc1e327e5b4f9d322401c2bef0bed18f088e6
LeeJJ07 <jkj9513@naver.com>
2023-11-07 00:36:35 +0900

    fix : 유효한 입력인지 확인하는 기능 수정
    에러메세지 추가 및 하드코딩 수정

--- src/main/java/lotto/controller/CheckValidateInput.java
+++ src/main/java/lotto/controller/CheckValidateInput.java
@@ -7,76 +7,87 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CheckValidateInput {
-	
-	
+
+	private final String NOT_NUMBER_MESSAGE = "숫자를 입력해야합니다.";
+	private final String NOT_UNITS_1000_MESSAGE = "1,000원 단위의 금액을 입력해야합니다.";
+	private final String NOT_IN_RANGE_MONEY_MESSAGE = "0보다 큰 값을 입력해야합니다.";
+	private final String NOT_IN_RANGE_BONUS_NUMBER_MESSAGE = "1~45 사이의 숫자를 입력해야합니다.";
+
+	private final String ERROR_MESSAGE_TAG = "[ERROR] ";
+	private final int MONEY_UNIT = 1000;
+	private final int ZERO = 0;
+	private final int MAX_VALUE = Integer.MAX_VALUE;
+	private final int MAX_LOTTO_NUMBER = 45;
+	private final String COMMA = ",";
+
 	public int money(String money) {
 
-		if (!isInteger(money))
-			throw new IllegalArgumentException();
+		if (!isNumber(money))
+			throw new IllegalArgumentException(ERROR_MESSAGE_TAG + NOT_NUMBER_MESSAGE);
 		if (!isUnits1000(Integer.parseInt(money)))
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(ERROR_MESSAGE_TAG + NOT_UNITS_1000_MESSAGE);
 		if (!inRangeMoney(Integer.parseInt(money)))
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(ERROR_MESSAGE_TAG + NOT_IN_RANGE_MONEY_MESSAGE);
 
 		return Integer.parseInt(money);
 	}
 
 	private boolean isUnits1000(int money) {
-		return (money % 1000) == 0;
+		return (money % MONEY_UNIT) == ZERO;
 	}
 
 	private boolean inRangeMoney(int money) {
-		return (money > 0) && (money < Integer.MAX_VALUE);
+		return (money > ZERO) && (money < MAX_VALUE);
 	}
 
 	public int bonusNumber(String bonusNumber) {
 
-		if (!isInteger(bonusNumber))
-			throw new IllegalArgumentException();
+		if (!isNumber(bonusNumber))
+			throw new IllegalArgumentException(ERROR_MESSAGE_TAG + NOT_NUMBER_MESSAGE);
 		if (!inRangeBonusNumber(Integer.parseInt(bonusNumber)))
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(NOT_IN_RANGE_BONUS_NUMBER_MESSAGE + NOT_IN_RANGE_MONEY_MESSAGE);
 
 		return Integer.parseInt(bonusNumber);
 	}
 
 	private boolean inRangeBonusNumber(int bonusNumber) {
-		return (bonusNumber > 0) && (bonusNumber <= 45);
+		return (bonusNumber > ZERO) && (bonusNumber <= MAX_LOTTO_NUMBER);
 	}
 
-	public List<Integer> winningNumber(String numbers){
-		
-		if(isNumbersInteger(separated(numbers)))
-			throw new IllegalArgumentException();
-		
-		List<Integer> winnings = Arrays.stream(separated(numbers))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-		
+	public List<Integer> winningNumber(String numbers) {
+
+		if (isNumbersInteger(separated(numbers)))
+			throw new IllegalArgumentException(ERROR_MESSAGE_TAG + NOT_NUMBER_MESSAGE);
+
+		List<Integer> winnings = Arrays
+				.stream(separated(numbers))
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
 		sort(winnings);
 		return winnings;
 	}
-	
+
 	private void sort(List<Integer> numbers) {
 		Collections.sort(numbers);
 	}
-	
+
 	private String[] separated(String numbers) {
-		return numbers.split(",");
+		return numbers.split(COMMA);
 	}
-	
+
 	private boolean isNumbersInteger(String[] numbers) {
-		for(String number : numbers) {
-			if(!isInteger(number))
+		for (String number : numbers) {
+			if (!isNumber(number))
 				return false;
 		}
 		return true;
 	}
-	
-	private boolean isInteger(String number) {
+
+	private boolean isNumber(String number) {
 		try {
 			Integer.parseInt(number);
 			return true;
-		} catch (NumberFormatException ex) {
+		} catch (NumberFormatException numberFormatException) {
 			return false;
 		}
 	}

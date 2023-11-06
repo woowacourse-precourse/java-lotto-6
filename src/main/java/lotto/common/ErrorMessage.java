8732504ab9597522efda1f42a37296830e7cbda6
kimdoha <kkhhjj3393@gmail.com>
2023-11-06 21:11:18 +0900

    feat(LottoStorage): 로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 오류 처리

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     LOTTO_NUM_RANGE_ERROR("[ERROR] 로또 번호의 숫자 범위는 1 ~ 45 까지입니다."),
     LOTTO_NUM_SIZE_ERROR("[ERROR] 로또 번호 개수는 6개입니다."),
-    LOTTO_NUM_DUPLICATION_ERROR("[ERROR] 로또 번호는 중복되지 않는 6자리 숫자입니다.");
+    LOTTO_NUM_DUPLICATION_ERROR("[ERROR] 로또 번호는 중복되지 않는 6자리 숫자입니다."),
+    LOTTO_PURCHASE_AMOUNT_INVALID_ERROR("[ERROR] 로또 구입 금액 단위는 1000원입니다.");
 
     private String message;
 

425bee49cba7fe43cad9ee7b56c1fcfca036dd42
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-06 23:57:53 +0900

    feat(LottoAmount): 로또의 가격을 정의하는 기능 추가
    docs: 로또가격 정의 기능 추가 체크 완료

--- src/main/java/lotto/Domain/LottoAmount.java
+++ src/main/java/lotto/Domain/LottoAmount.java
@@ -6,6 +6,11 @@ public class LottoAmount {
     private static final int LOTTO_PRICE = 1000;
     private static final int ZERO = 0;
 
+    public int initLottoAmount(int amount){
+        checkZero(amount);
+        return checkLottoPrice(amount);
+    }
+
     private void checkZero(int amount){
         if (amount <= ZERO){
             throw ExceptionMessage.INPUT_ZERO.throwexception();

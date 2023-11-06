0374c988266d72b8908de13f8f0d8fd8ce097f05
Infikei <hckhong@naver.com>
2023-11-07 04:20:14 +0900

    feat: 보너스 번호를 담당할 Bonus 클래스 생성

---
+++ src/main/java/lotto/domain/Bonus.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public class Bonus {
+    private static final String NOT_INTEGER_EXCEPTION_MESSAGE = "로또 번호는 정수여야 합니다.";
+    private static final String OUT_OF_RANGE_EXCEPTION_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String DUPLICATE_NUMBERS_EXCEPTION_MESSAGE = "로또 번호는 서로 다른 수여야 합니다.";
+    private static final int LOTTO_RANGE_BEGIN_NUMBER = 1;
+    private static final int LOTTO_RANGE_END_NUMBER = 45;
+
+    private final int bonusNumber;
+
+    public Bonus(String number, Lotto winningLotto) {
+        bonusNumber = 1;
+    }
+}

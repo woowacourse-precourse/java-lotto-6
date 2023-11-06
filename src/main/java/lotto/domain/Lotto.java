440d138affe86335f13c8a03f4f576567d82025f
kangyuseok <kingyuseok@naver.com>
2023-11-06 21:55:35 +0900

    Refactor: Lotto 클래스 패키지 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -12,9 +12,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
         }
     }
+
+
 
     // TODO: 추가 기능 구현
 }

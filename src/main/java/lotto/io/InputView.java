8cff76ec2936d16c8e85f400e0320184f619742e
youngkyunIm <yee0230@naver.com>
2023-11-06 23:07:21 +0900

    refactor : 상수 사용

--- src/main/java/lotto/io/InputView.java
+++ src/main/java/lotto/io/InputView.java
@@ -8,18 +8,24 @@ public class InputView {
 
     private final IoManager ioManager;
 
+    // 상수로 사용할 메시지를 추가합니다.
+    private static final String PROMPT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String ERROR_MESSAGE = "숫자를 입력해야 합니다.";
+
     public InputView(IoManager ioManager) {
         this.ioManager = ioManager;
     }
 
     public int inputMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PROMPT_MONEY);
         return stringToInt(ioManager.read());
     }
 
     public List<Integer> inputLotto() {
         displayEmptyLine();
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(PROMPT_WINNING_NUMBERS);
         String[] input = ioManager.read().split(",");
         List<Integer> lottoNumbers = new ArrayList<>();
         for (String str : input) {
@@ -30,7 +36,7 @@ public class InputView {
 
     public int inputBonus() {
         displayEmptyLine();
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(PROMPT_BONUS_NUMBER);
         return stringToInt(ioManager.read());
     }
 
@@ -38,12 +44,11 @@ public class InputView {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException numberFormatException) {
-            throw ExceptionManager.ERROR_MSG_PREFIX.createIllegalArgumentException("숫자를 입력해야 합니다.");
+            throw ExceptionManager.ERROR_MSG_PREFIX.createIllegalArgumentException(ERROR_MESSAGE);
         }
     }
 
     private void displayEmptyLine() {
         System.out.println();
     }
-
 }

4b38575630b85ce4a19491b5f6fcc751c8053cea
mksystem98 <mksystem98@gmail.com>
2023-11-06 21:58:03 +0900

    feat: 입력한 당첨 번호에 대해 1~45 이내의 숫자인지 검증하는 기능 추가
    WinningNumberValidator.validateAllNumbersInRange() 메서드는 숫자가 1~45 이내의 숫자인지 검증함

--- src/main/java/lotto/validation/WinningNumberValidator.java
+++ src/main/java/lotto/validation/WinningNumberValidator.java
@@ -10,6 +10,13 @@ public class WinningNumberValidator {
     private static final int MAXIMUM_WINNING_NUMBER = 45;
     private static final int WINNING_NUMBERS_COUNT = 6;
 
+    public static void validateWinningNumbers(String winningNumbers) {
+        Set<Integer> numbersSet = parseToIntSet(winningNumbers);
+        validateNoDuplicates(numbersSet);
+        validateSixNumbers(numbersSet);
+        validateAllNumbersInRange(numbersSet);
+    }
+
 
     private static Set<Integer> parseToIntSet(String winningNumbers) {
         try {
@@ -31,6 +38,14 @@ public class WinningNumberValidator {
     private static void validateSixNumbers(Set<Integer> numbers) {
         if (numbers.size() != WINNING_NUMBERS_COUNT) {
             throw new IllegalArgumentException("[ERROR] 정확히 6개의 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private static void validateAllNumbersInRange(Set<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MINIMUM_WINNING_NUMBER || number > MAXIMUM_WINNING_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 " + MINIMUM_WINNING_NUMBER + "에서 " + MAXIMUM_WINNING_NUMBER + " 사이어야 합니다: " + number);
+            }
         }
     }
 

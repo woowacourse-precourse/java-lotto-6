592e1affc3365fb11582e5927fa691d38db172e9
PARKHWANYEONG <welcome0828@naver.com>
2023-11-07 03:29:51 +0900

    test(validator): winningNumbers 메서드에 대한 테스트 코드 추가
    - 여러 유효한 입력 케이스에 대해 예외가 발생하지 않음을 확인
    - 잘못된 형식("1,2,3,4,5,6,7") 입력 시 예외 발생과 메시지 검증
    - 문자열이 포함된 경우("1,2,3,a,5,6") 예외 발생과 메시지 검증
    - 빈 칸이 포함된 경우("1,2,3,,5,6") 예외 발생과 메시지 검증
    - 범위 초과 입력 시("1,2,3,4,5,50") 예외 발생과 메시지 검증
    - 최소값 미만 입력 시("0,2,3,4,5,11") 예외 발생과 메시지 검증
    - 중복된 값 입력 시("1,2,3,4,1,5") 예외 발생과 메시지 검증

--- src/test/java/lotto/validator/ValidatorTest.java
+++ src/test/java/lotto/validator/ValidatorTest.java
@@ -37,4 +37,35 @@ class ValidatorTest {
                     .hasMessage(expectedErrorMessage);
         }
     }
+
+    @Nested
+    @DisplayName("Validator.winningNumbers 테스트")
+    class winningNumbersTests {
+        @DisplayName("당첨 번호가 유효할 경우 예외가 발생하지 않는다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,3,4,5,6","12,13,1,7,5,33","5,7,9,21,33,45","1,45,22,2,7,8"})
+        void 유효한_당첨번호_입력값_테스트(String inputWinningNumbers) {
+            String[] winningNumbers = inputWinningNumbers.split(",");
+            assertThatCode(() -> Validator.winningNumbers(winningNumbers))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("입력값에 따라 설정한 예외 메시지와 예외를 발생시킨다.")
+        @ParameterizedTest
+        @CsvSource(value = {
+                "1,2,3,4,5,6,7 : ,로 구분하여 총 6개의 숫자를 입력해주세요.",
+                "1,2,3,a,5,6 : 정수만 입력이 가능합니다.",
+                "1,2,3, ,5,6 : 정수만 입력이 가능합니다.",
+                "1,2,3,4,5,50 : 1~45 범위의 정수만 입력 가능합니다.",
+                "0,2,3,4,5,11 : 1~45 범위의 정수만 입력 가능합니다.",
+                "-7,2,3,4,5,11 : 1~45 범위의 정수만 입력 가능합니다.",
+                "1,2,3,4,1,5 : 중복된 값이 존재합니다.",
+        },delimiter = ':')
+        void 유효하지_않은_당첨번호_입력값_테스트(String inputWinningNumbers,String expectedErrorMessage) {
+            String[] winningNumbers = inputWinningNumbers.split(",");
+            assertThatThrownBy(() -> Validator.winningNumbers(winningNumbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(expectedErrorMessage);
+        }
+    }
 }

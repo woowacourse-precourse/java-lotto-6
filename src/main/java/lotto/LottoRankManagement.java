e3f5403a20c8471d4e9fea26a1e40679082618fc
박정현 <ros28081@naver.com>
2023-11-06 19:48:11 +0900

    feat(LottoRankManagement): 로또 당첨 통계 생성자 구현

--- src/main/java/lotto/LottoRankManagement.java
+++ src/main/java/lotto/LottoRankManagement.java
@@ -1,4 +1,16 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LottoRankManagement {
+
+    private Map<LottoRank, Integer> rankCounts;
+
+    public LottoRankManagement() {
+        rankCounts = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            rankCounts.put(rank, 0);
+        }
+    }
 }

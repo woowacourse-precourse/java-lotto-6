aad32da7dfa1784adc39011f2fb7f05fa854703e
chansoo <chansoo38@gmail.com>
2023-11-06 19:21:48 +0900

    <Feat> InputValidator - 사용자 입력에 대한 유효성을 검증하는 기능을 구현한다.

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,12 +1,14 @@
 package lotto.View;
 
 public class InputView {
+    private final InputValidator inputValidator;
     private final String BUY_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static InputView INSTANCE = new InputView();
+    private static final InputView INSTANCE = new InputView(InputValidator.getInstance());
 
-    private InputView() {
+    private InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
     }
 
     public static InputView getInstance() {

09e8548e4e9e18e135ec8791110a84644f54d8c2
kangyuseok <kingyuseok@naver.com>
2023-11-07 03:22:13 +0900

    Feat: 로또 번호 출력하는 메서드 구현

--- src/main/java/lotto/view/IO.java
+++ src/main/java/lotto/view/IO.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.view.constants.Front;
 
 public class IO {
@@ -10,5 +12,11 @@ public class IO {
 
     public static void checkLottoTickets(final int lottoTicketCount, Front message){
         System.out.println(lottoTicketCount+message.getMessage());
+    }
+
+    public static void viewLottoTickets(final List<Lotto>lottoTickets){
+        for (Lotto lotto : lottoTickets) {
+            System.out.println(lotto);
+        }
     }
 }

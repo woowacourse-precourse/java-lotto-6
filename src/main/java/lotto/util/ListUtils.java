9acf879fc729c4f57c3e522b780e24e1e8d2c353
Haeun Jeong <gkdms4248@gmail.com>
2023-11-07 00:28:21 +0900

    fix: stringToList sort 추가

--- src/main/java/lotto/util/ListUtils.java
+++ src/main/java/lotto/util/ListUtils.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public class ListUtils {
 
-    public List<Integer> stringToIntegerList(String inputString) {
+    public static final int stringToInt(final String number) {
+        return Integer.parseInt(number);
+    }
+
+    public static final List<Integer> stringToIntegerSortedList(final String inputString) {
         String[] splitString = inputString.split(",", -1);
-        return Arrays.stream(splitString).map(string -> Integer.parseInt(string)).toList();
+        return Arrays.stream(splitString).map(string -> Integer.parseInt(string)).sorted().toList();
     }
 }

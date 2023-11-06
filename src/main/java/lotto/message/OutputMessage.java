17ee6eb7a81203ed5489864a3bd1c81bed5bf922
alscjf1329 <alscjf1329@gmail.com>
2023-11-06 18:46:33 +0900

    feat(OutputMessage): printWinningStatistics 메소드 작성
    printWinningStatistics 당첨 통계를 출력하는 메소드를 작성했습니다.

--- src/main/java/lotto/message/OutputMessage.java
+++ src/main/java/lotto/message/OutputMessage.java
@@ -1,5 +1,7 @@
 package lotto.message;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import lotto.Lotto;
 
@@ -20,5 +22,17 @@ public enum OutputMessage {
     public static void printLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         lottos.forEach(System.out::println);
+    }
+
+    public static void printWinningStatistics(HashMap<LottoResult, BigDecimal> result, float profitRate) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("당첨 통계\n").append("---------\n");
+        sb.append(LottoResult.FIFTH.getMessage()).append(" - ").append(result.get(LottoResult.FIFTH)).append("개\n");
+        sb.append(LottoResult.FOURTH.getMessage()).append(" - ").append(result.get(LottoResult.FOURTH)).append("개\n");
+        sb.append(LottoResult.THIRD.getMessage()).append(" - ").append(result.get(LottoResult.THIRD)).append("개\n");
+        sb.append(LottoResult.SECOND.getMessage()).append(" - ").append(result.get(LottoResult.SECOND)).append("개\n");
+        sb.append(LottoResult.FIRST.getMessage()).append(" - ").append(result.get(LottoResult.FIRST)).append("개\n");
+        sb.append(String.format("총 수익률은 %.1f입니다.", profitRate));
+        System.out.println(sb);
     }
 }

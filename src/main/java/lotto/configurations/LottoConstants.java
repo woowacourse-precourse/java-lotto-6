aa515b1de594ad9a227a4aec3043fe38185d91f8
bandall <jsm5315@naver.com>
2023-11-06 16:36:43 +0900

    feat: Lotto의 정보가 있는 LottoConstants 추가
    Lotto의 정보(번호 개수, 번호 범위, 가격)을 저장하는 상수 클래스 추가

---
+++ src/main/java/lotto/configurations/LottoConstants.java
@@ -0,0 +1,8 @@
+package lotto.configurations;
+
+public class LottoConstants {
+    public static final int MAX_LOTTO_NUM = 45;
+    public static final int MIN_LOTTO_NUM = 1;
+    public static final int LOTTO_NUMBER_LENGTH = 6;
+    public static final int LOTTO_PRICE = 1000;
+}

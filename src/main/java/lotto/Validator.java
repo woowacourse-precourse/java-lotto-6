11cd56bd8b567455ff63ac640e1c0714bccba764
prefer52 <tjsgh151@naver.com>
2023-11-06 21:42:54 +0900

    feat(Validator): 공백문자를 포함하는지 검증 구현

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -2,15 +2,26 @@ package lotto;
 
 public class Validator {
     public Boolean validate(String input) {
-        if (isSpaceOrNULL(input))
+        if (isBlankOrNULL(input))
+            throw new IllegalArgumentException("[ERROR]");
+
+        if (containSpace(input))
             throw new IllegalArgumentException("[ERROR]");
 
         return true;
     }
 
     // 공백이거나 NULL인지 검증
-    private Boolean isSpaceOrNULL(String input) {
+    private Boolean isBlankOrNULL(String input) {
         if (input.equals("") || input == null)
+            return true;
+
+        return false;
+    }
+
+    // 공백문자를 포함하는지 검증
+    private Boolean containSpace(String input) {
+        if (input.contains(" "))
             return true;
 
         return false;

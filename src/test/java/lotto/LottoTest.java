2ce2aaf09abbd107386dca99833a8f6d1ed4d359
kchranger <kchranger@gmail.com>
2023-11-06 21:22:37 +0900

    test(result) : number of winner are more than 1, other is 0

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +25,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("당첨되지 않은 로또는 0개, 당첨된 로또는 1개 이상이다.")
+    @Test
+    void findLottoWinner(){
+        LottoResultWinners lottoResultWinners = new LottoResultWinners();
+        lottoResultWinners.add(new LottoResult(Rank.SIX_MATCH.getNumberOfMatch(),Rank.SIX_MATCH.getBonus()));
+        lottoResultWinners.add(new LottoResult(Rank.FIVE_MATCH_BONUS.getNumberOfMatch(),Rank.FIVE_MATCH_BONUS.getBonus()));
+        lottoResultWinners.add(new LottoResult(Rank.FIVE_MATCH_BONUS.getNumberOfMatch(),Rank.FIVE_MATCH_BONUS.getBonus()));
+        lottoResultWinners.add(new LottoResult(Rank.THREE_MATCH.getNumberOfMatch(),Rank.THREE_MATCH.getBonus()));
+
+        assertThat(lottoResultWinners.getNumberOfWinner(Rank.SIX_MATCH) == 1);
+        assertThat(lottoResultWinners.getNumberOfWinner(Rank.FIVE_MATCH) == 0);
+        assertThat(lottoResultWinners.getNumberOfWinner(Rank.FIVE_MATCH_BONUS) == 2);
+        assertThat(lottoResultWinners.getNumberOfWinner(Rank.THREE_MATCH) == 1);
+    }
 }

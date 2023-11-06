9f6e66f9517f9058fb4cf6064038bb6e33b208dc
김민정 <mjeong@sookmyung.ac.kr>
2023-11-06 12:35:40 +0900

    fix: 3등 당첨 기준 오타 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,7 +3,7 @@ package lotto.domain;
 public enum Rank {
     FIFTH(3, false, 5_000),
     FOURTH(4, false, 50_000),
-    THIRD(5, true, 1_500_000),
+    THIRD(5, false, 1_500_000),
     SECOND(5,true, 30_000_000),
     FIRST(6, false, 2_000_000_000),
     OUT_OF_RANKS(0, false, 0);

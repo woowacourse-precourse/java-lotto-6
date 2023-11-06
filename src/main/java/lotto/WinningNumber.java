17a65c1f4806cfff83a8b3d2baa0951744c4f8fd
Hajuuu <hpt0127@naver.com>
2023-11-06 17:47:17 +0900

    feat(WinningNumber): compareToLotto 메서드 작성
    입력한 당첨 번호, 보너스 번호와 로또 번호를 비교하여 일치하는 번호의 개수를 list에 저장하는 메서드 작성

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -8,9 +8,11 @@ import java.util.List;
 public class WinningNumber {
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-
     private List<Integer> winningNumbers;
     private int bonusNumber;
+
+    private List<Long> matchWinningNumber = new ArrayList<>();
+    private List<Long> matchBonusNumber = new ArrayList<>();
 
     public void inputWinningNumber() {
         System.out.println();
@@ -20,13 +22,23 @@ public class WinningNumber {
         bonusNumber = Integer.parseInt(readLine());
     }
 
-    public static List<Integer> toWinningNumberList(String numbers) {
+
+    public List<Integer> toWinningNumberList(String numbers) {
         String[] num = numbers.split(",");
         List<Integer> winningNumbers = new ArrayList<>();
         for (String number : num) {
             winningNumbers.add(Integer.parseInt(number));
         }
         return winningNumbers;
+    }
+
+    public void compareToLotto(List<Lotto> lottos) {
+        for (int i = 0; i < lottos.size(); i++) {
+            Lotto lotto = lottos.get(i);
+            matchWinningNumber.add(winningNumbers.stream().filter(w -> lotto.getNumbers().contains(w))
+                    .count());
+            matchBonusNumber.add(lotto.getNumbers().stream().filter(l -> l.equals(bonusNumber)).count());
+        }
     }
 
 

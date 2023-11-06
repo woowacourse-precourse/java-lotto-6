060369fdd99c42fdcca71756bbf68d400ced2ebd
bin <gimgyubin1225@gmail.com>
2023-11-06 18:33:01 +0900

    feat: 사용자 입력 기능 추가
    정수 1개 입력
    콤마로 구분하여 정수 여러개 입력

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,10 +1,25 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.validator.InputValidator;
 
 public class Input {
 
-    public static String receiveUserInput() {
-        return Console.readLine().trim();
+    public static Integer receiveOneNumber() {
+        String input = Console.readLine().trim();
+        InputValidator.isInteger(input);
+
+        return Integer.parseInt(input);
+    }
+
+    public static List<Integer> receiveNumbersSeparatedByCommas() {
+        String[] inputs = Console.readLine().trim().split(",");
+        InputValidator.isInteger(inputs);
+
+        return Arrays.stream(inputs).map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

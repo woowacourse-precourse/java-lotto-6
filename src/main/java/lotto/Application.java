e7f3ca84c29b9233082bc7fabf297eff340f1a38
DahyeY <dhuniv@naver.com>
2023-11-07 01:23:59 +0900

    style: 코드 포맷팅

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -15,16 +15,19 @@ class User {
     public List<List<Integer>> tickets;
     public String price;
     private static final int TICKET_PRICE = 1000;
+
     public User(String price) {
         validate(price);
         this.price = price;
     }
-    private void validate(String price){
-        if(Integer.parseInt(price) % TICKET_PRICE != 0){
+
+    private void validate(String price) {
+        if (Integer.parseInt(price) % TICKET_PRICE != 0) {
             throw new IllegalArgumentException();
         }
         this.price = price;
     }
+
     private List<Integer> generateTicket() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
@@ -43,33 +46,34 @@ class Game {
     public String inputPrice;
     public int rank;
 
-    public void inputLottoNumbers(){
+    public void inputLottoNumbers() {
         String[] inputNumbers;
         inputNumbers = Console.readLine().split(",");
-        for(String number : inputNumbers){
+        for (String number : inputNumbers) {
             this.numbers.add(Integer.parseInt(number));
         }
     }
 
-    public void inputBonusNumber(){
+    public void inputBonusNumber() {
         this.numbers.add(Integer.parseInt(Console.readLine()));
     }
 
-    public void compareNumbers(List<Integer> user, List<Integer> lotto){
+    public void compareNumbers(List<Integer> user, List<Integer> lotto) {
 
     }
-    public int compareLotto(List<Integer> user, List<Integer> lotto){
+
+    public int compareLotto(List<Integer> user, List<Integer> lotto) {
         this.rank = 0;
-        List<Integer> subLotto = lotto.subList(0,6);
-        for(Integer userNumber : user) {
-            if(subLotto.contains(userNumber)){
+        List<Integer> subLotto = lotto.subList(0, 6);
+        for (Integer userNumber : user) {
+            if (subLotto.contains(userNumber)) {
                 this.rank++;
             }
         }
         return rank;
     }
 
-    public boolean compareBonus(List<Integer> user, List<Integer>lotto){
+    public boolean compareBonus(List<Integer> user, List<Integer> lotto) {
         return user.contains(lotto.get(6));
     }
 }

3eb4ec835a8c55971c2b566b7c53c649768ea246
little6523 <little6523@naver.com>
2023-11-07 00:32:41 +0900

    feat : 로또 구입 금액 입력 기능 추가  - docs/README.md의 2번 기능 구현  - 1,000원 단위로 입력하지 않을 시 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static int inputPurchaseAmount() {
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+        while (purchaseAmount % 1000 != 0) {
+            purchaseAmount = isValidPurchaseAmount(purchaseAmount);
+        }
+        return purchaseAmount;
+    }
+    private static int isValidPurchaseAmount(int purchaseAmount) {
+        try {
+            if (purchaseAmount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액 단위를 1,000원(천원)단위로 입력하여 주세요!");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            int refixPurchaseAmount = Integer.parseInt(Console.readLine());
+            return refixPurchaseAmount;
+        }
+        return purchaseAmount;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = inputPurchaseAmount();
+
+        System.out.print("구입금액" + purchaseAmount);
     }
 }

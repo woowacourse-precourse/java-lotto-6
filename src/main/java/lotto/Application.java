96aad5db8e6e840202adea1b36672f656f5671eb
ioyxxnstream <ioyxxnstream@naver.com>
2023-11-06 13:06:33 +0900

    게임 기능 2 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.util.Arrays;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -19,7 +23,7 @@ public class Application {
          */
 
         // 입력 1. 로또 구입 금액 1000원 단위로 입력 받기
-        int purchaseAmount = Integer.parseInt(Console.readLine());
+        int purchaseAmount = Integer.parseInt(readLine());
 
         // -1000원으로 안 나누어지면 예외처리
         try {
@@ -31,8 +35,28 @@ public class Application {
         }
 
         // 게임 1. 구입금액에 따른 로또 구매 개수 구하기
+        System.out.println("구입금액을 입력해 주세요.");
         int lottoCount = purchaseAmount / 1000;
 
+        // 게임 2. 1~45까지의 중복 되지 않는 랜덤 숫자 6개를 구매 개수에 맞게 뽑기
+        // 모든 로또 티켓 저장된 리스트
+        List<Lotto> lottos = new ArrayList<>();
+
+        while(lottos.size() < lottoCount){
+            List<Integer> numbers = pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+
+        /* 게임기능2 확인
+        for(Lotto l:lottos){
+            List<Integer> intlotto = new ArrayList<Integer>(l.getter());
+            for(Integer i:intlotto){
+                System.out.println(i);
+            }
+            System.out.println("---");
+        }
+        */
 
 
         // 입력 2. 쉼표 기준으로 구분해서 당첨 번호 입력 받기
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,5 +19,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getter(){
+        return numbers;
+    }
 }

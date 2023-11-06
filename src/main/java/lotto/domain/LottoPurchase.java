76b44589c896cf04b87a8e39e63a9fb30d9c21b4
dukcode <duk9741@gmail.com>
2023-11-06 18:26:02 +0900

    refactor: 하드코딩 상수로 사용하도록 변경

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -19,7 +19,7 @@ public class LottoPurchase {
             throw new IllegalArgumentException("로또 구입 금액은 양수여야 합니다.");
         }
 
-        if (purchaseAmount % 1000 != 0) {
+        if (purchaseAmount % LottoConstant.LOTTO_TICKET_PRICE != 0) {
             throw new IllegalArgumentException("로또 구입 금액은 " +
                     LottoConstant.LOTTO_TICKET_PRICE +
                     "원 단위여야 합니다.");

e542ff164cff3e4d882f30262dd261320e80f1c8
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-07 06:35:53 +0900

    feat(service): 0원 이하 로또 구매 금액 예외 처리 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -20,7 +20,7 @@ public class LottoService {
     }
 
     private void validateMoney(int money) {
-        if(money % 1000 != 0) {
+        if(money < 1000 || money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 가능합니다.");
         }
     }

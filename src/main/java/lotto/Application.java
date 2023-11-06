4df3449f938f8c5c6ee356b22983a4f44fec67b6
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-06 22:18:40 +0900

    refactor(ConsoleInputAdapter, InputPort) : Console 메서드 리팩토링
     ConsoleInputAdapter : Console 기능을 가진 Adapter 구현
     InputPort : 입력하는 방법을 연결해주는 Port 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,11 @@
 package lotto;
 
+import lotto.adapter.ConsoleInputAdapter;
 import lotto.controller.LottoGameController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoGameController lottoGameController = new LottoGameController();
+        LottoGameController lottoGameController = new LottoGameController(new ConsoleInputAdapter());
         lottoGameController.playGame();
     }
 }
--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,11 +1,18 @@
 package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Console;
+import lotto.port.InputPort;
+
 public class LottoGameController {
+
+    private final InputPort inputPort;
+
+    public LottoGameController(InputPort inputPort) {
+        this.inputPort = inputPort;
+    }
 
     public void playGame() {
         System.out.println("구입금액을 입력해 주세요.");
-        String s = Console.readLine();
+        String s = inputPort.readLine();
     }
 
 }

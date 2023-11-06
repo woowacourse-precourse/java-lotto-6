9a06f844e7fddd754606093f2a30393bfc7fc0ec
Park Eunhyeong <silbroeh@gmail.com>
2023-11-06 22:57:48 +0900

    refactor : ResultCalculator클래스 - 나머지 메소드 삭제

--- src/main/java/lotto/ResultCalculator.java
+++ src/main/java/lotto/ResultCalculator.java
@@ -1,37 +1,7 @@
 package lotto;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ResultCalculator {
-    private final Map<Lotto, PrizeGrade> result;
-
-    public ResultCalculator(Lottos lottos, Lotto winningLotto, Bonus bonus) {
-        Map<Lotto, PrizeGrade> result = new HashMap<>();
-        lottos.getTickets().stream()
-                .forEach(lotto -> result.put(lotto, compare(lotto, winningLotto, bonus)));
-        this.result = result;
-    }
-    private PrizeGrade compare(Lotto lotto, Lotto winningLotto, Bonus bonus) {
-        int matchCount = countMatchingNumbers(lotto, winningLotto);
-        boolean bonusMatch = matchWithBonus(lotto, bonus);
-
-        int number = LOTTO_SIZE;
-        if (matchCount == number--) {
-            return PrizeGrade.FIRST;
-        }
-        if (matchCount == number-- && bonusMatch) {
-            return PrizeGrade.SECOND;
-        }
-        if (matchCount == number) {
-            return PrizeGrade.THIRD;
-        }
-        if (matchCount == number--) {
-            return PrizeGrade.FOURTH;
-        }
-        if (matchCount == number--) {
-            return PrizeGrade.FIFTH;
-        }
-        return PrizeGrade.NO_PRIZE;
-    }
+    private final Map<Lotto, PrizeGrade> prizeGradeDist;
 }

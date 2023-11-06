9b71154c4fd95f5ded2bf3d926cfbe5dc3e42ba7
Octoping <mooner925@gmail.com>
2023-11-06 21:59:04 +0900

    feat: Lotto 클래스의 중복 체크 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,17 +14,39 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     public LottoMatchResult compare(List<Integer> winningNumbers, int bonusNumber) {
         return new LottoMatchResult(
                 getMatchCount(winningNumbers),
                 isMatchBonus(bonusNumber)
         );
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> !isOnRange(number))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isOnRange(int number) {
+        return LOTTO_NUMBER_MIN <= number && number <= LOTTO_NUMBER_MAX;
     }
 
     private int getMatchCount(List<Integer> winningNumbers) {

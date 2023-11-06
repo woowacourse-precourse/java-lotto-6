5b1510cb0f2106eb430f5a0003b3cb8e52436c10
Tiger <skdlghwns@gmail.com>
2023-11-06 21:39:45 +0900

    test: LottoGenerator 테스트 추가 -입력 받은 개수만큼 로또 생성 확인

--- src/test/java/lotto/GeneratorTest.java
+++ src/test/java/lotto/GeneratorTest.java
@@ -1,2 +1,17 @@
-package lotto;public class GeneratorTest {
+package lotto;
+
+import lotto.generator.LottoGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+public class GeneratorTest {
+    LottoGenerator lottoGenerator = new LottoGenerator();
+
+    @DisplayName("입력 개수에 따라 같은 수의 로또 생성")
+    @Test
+    void buyLottos(){
+        assertThat(lottoGenerator.buyLottos(8).size()).isEqualTo(8);
+    }
 }

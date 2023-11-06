38ff662c4438dbb66524f37693429d8a6b8e1089
Roista <Rosita57@gmail.com>
2023-11-06 21:01:12 +0900

    feat: 당첨된 로또 번호를 입력받는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,8 +10,11 @@ public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
         System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
-        int lottoTotalCount = lottoBuyCount(money);
-        ArrayList<Lotto> list = buyLotto(lottoTotalCount);
+        int buyCount = lottoBuyCount(money);
+        ArrayList<Lotto> list = buyLotto(buyCount);
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winNumbers = Console.readLine();
+
     }
     static int lottoBuyCount(String money){
         try{
@@ -19,19 +22,29 @@ public class Application {
             if (lottoTotalCount % 1000 != 0){
                 throw new IllegalArgumentException("구매 금액을 1000원 단위로 작성해주세요.");
             }
-            return lottoTotalCount;
+            return lottoTotalCount/1000;
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력받은 값이 잘못되었습니다.");
         }
     }
 
-    static ArrayList<Lotto> buyLotto(int lottoTotalCount){
-        int buyCount = lottoTotalCount/1000;
+    static ArrayList<Lotto> buyLotto(int buyCount){
+        System.out.println(buyCount+"개를 구매했습니다.");
         ArrayList<Lotto> buyLottoNumbers = new ArrayList<>();
         for (int i=0;i<buyCount;i++){
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             buyLottoNumbers.add((new Lotto(numbers)));
         }
         return buyLottoNumbers;
+    }
+
+    static List<Integer> getWinningNumbers(String winNumbers){
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] getWinNumber = winNumbers.split(",");
+        for (String number : getWinNumber){
+            Integer winNumber = Integer.parseInt(number);
+            winningNumbers.add(winNumber);
+        }
+        return winningNumbers;
     }
 }

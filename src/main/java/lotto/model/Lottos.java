7fcb713bd042bebf903feb8e576605ed98a0c279
eom-tae-in <eti0728@daum.net>
2023-11-07 03:51:15 +0900

    refactor: 불필요해진 메서드 제거

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,9 +1,9 @@
 package lotto.model;
 
-import static lotto.model.LottoPrize.*;
-
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import lotto.util.NumbersGenerator;
 
@@ -27,45 +27,6 @@ public class Lottos {
         return LongStream.range(ZERO, lottoTicketCount)
                 .mapToObj(lotto -> Lotto.createWith(numbersGenerator))
                 .toList();
-    }
-
-    public int countFirstPrizeWinners(final List<Integer> winningNumbers) {
-        return (int) lottos.stream()
-                .filter(lotto -> lotto.isFirstPrizeWinner(winningNumbers))
-                .count();
-    }
-
-    public int countSecondPrizeWinners(final List<Integer> winningNumbers, final int bonusNumber) {
-        return (int) lottos.stream()
-                .filter(lotto -> lotto.isSecondPrizeWinner(winningNumbers, bonusNumber))
-                .count();
-    }
-
-
-    public int countThirdPrizeWinners(final List<Integer> winningNumbers) {
-        return (int) lottos.stream()
-                .filter(lotto -> lotto.isThirdPrizeWinner(winningNumbers))
-                .count();
-    }
-
-    public int countFourthPrizeWinners(final List<Integer> winningNumbers) {
-        return (int) lottos.stream()
-                .filter(lotto -> lotto.isFourthPrizeWinner(winningNumbers))
-                .count();
-    }
-
-    public int countFifthPrizeWinners(final List<Integer> winningNumbers) {
-        return (int) lottos.stream()
-                .filter(lotto -> lotto.isFifthPrizeWinner(winningNumbers))
-                .count();
-    }
-
-    public long calculateWinningTotalPrize(final List<Integer> winningNumbers, final int bonusNumber) {
-        return calculateWinningPrize(FIRST_PRIZE, countFirstPrizeWinners(winningNumbers)) +
-                calculateWinningPrize(SECOND_PRIZE, countSecondPrizeWinners(winningNumbers, bonusNumber)) +
-                calculateWinningPrize(THIRD_PRIZE, countThirdPrizeWinners(winningNumbers)) +
-                calculateWinningPrize(FOURTH_PRIZE, countFourthPrizeWinners(winningNumbers)) +
-                calculateWinningPrize(FIFTH_PRIZE, countFifthPrizeWinners(winningNumbers));
     }
 
     public List<Lotto> getLottos() {

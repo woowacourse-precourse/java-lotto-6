15ad9f4b3f16cc29f6b6ed22a503481430e6f3ae
SUJI-J <suzy852@naver.com>
2023-11-06 23:04:12 +0900

    refactor: 변수명 변경

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -2,7 +2,7 @@ package lotto;
 
 public class Constants {
     public static final String REQUEST_BUY_MONEY = "구입금액을 입력해 주세요.";
-    public static final String REQUEST_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String REQUEST_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
     public static final String TICKET_COUNT = "개를 구매했습니다.";
 }

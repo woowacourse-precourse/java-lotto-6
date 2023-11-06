cd5b0bdaf502344f36c141c64f97f6fb884160b2
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-06 16:37:58 +0900

    refactor: 알맞은 출력을 위한 MatchResult 필드 수정

--- src/main/java/lotto/domain/MatchResult.java
+++ src/main/java/lotto/domain/MatchResult.java
@@ -5,10 +5,10 @@ import static lotto.domain.constant.StringConstant.*;
 
 public enum MatchResult {
     NONE(NONE_PRIZE_MONEY, NONE_MATCH_RESULT_MESSAGE),
-    BONUS(BONUS_PRIZE_MONEY, BONUS_MATCH_RESULT_MESSAGE),
     THREE(THREE_PRIZE_MONEY, THREE_MATCH_RESULT_MESSAGE),
     FOUR(FOUR_PRIZE_MONEY, FOUR_MATCH_RESULT_MESSAGE),
     FIVE(FIVE_PRIZE_MONEY, FIVE_MATCH_RESULT_MESSAGE),
+    BONUS(BONUS_PRIZE_MONEY, BONUS_MATCH_RESULT_MESSAGE),
     SIX(SIX_PRIZE_MONEY, SIX__MATCH_RESULT_MESSAGE);
 
     private final long prizeMoney;
@@ -20,7 +20,10 @@ public enum MatchResult {
     }
 
     public static MatchResult fromCount(int count) {
-        if (count >= MIN_WIN_COUNT && count <= MAX_WIN_COUNT)
+        if (count == SIX_WIN_COUNT)
+            return values()[count - SIX_IDX_OFFSET];
+
+        if (count >= MIN_WIN_COUNT)
             return values()[count - COUNT_IDX_OFFSET];
 
         return NONE;

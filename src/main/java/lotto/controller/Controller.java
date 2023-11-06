6d42347d8e0c642814a15c2ba78b650772323c67
mina <qkralsdk991@naver.com>
2023-11-06 16:53:04 +0900

    Feat : 랜덤 로또 번호 산출 & 출력 기능 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,15 +3,19 @@ package lotto.controller;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Amount;
 import lotto.domain.Lotto;
+import lotto.utils.LottoNumRandomGenerator;
 import lotto.utils.Parser;
 import lotto.view.RequestMessage;
+import lotto.view.ResultMessage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
     private static final int TICKET_PRICE=1000;
 
+    LottoNumRandomGenerator lottoNumRandomGenerator = new LottoNumRandomGenerator();
     Parser parser = new Parser();
 
     public void run(){
@@ -20,8 +24,23 @@ public class Controller {
 
     private void startLotto(){
         Amount amount = requestPlayerAmountInput();
+
+        List<Lotto> purchaseLotto = purchaseLottery(amount);
+        ResultMessage.setPurchaseMessage(amount,purchaseLotto);
+
         Lotto winningNum = requestPlayerWinningInput();
         int bonusNum = requestPlayerBonusInput();
+    }
+
+    private List<Lotto> purchaseLottery(Amount amount){
+        List<Lotto> result = new ArrayList<>();
+
+        for(int count=0;count<amount.getCount();count++){
+            Lotto lotto = new Lotto(lottoNumRandomGenerator.setRandomNum());
+            result.add(lotto);
+        }
+
+        return result;
     }
 
     private Amount requestPlayerAmountInput(){
--- src/main/java/lotto/view/ResultMessage.java
+++ src/main/java/lotto/view/ResultMessage.java
@@ -1,4 +1,26 @@
 package lotto.view;
 
-public class PrintOutput {
+import lotto.domain.Amount;
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class ResultMessage {
+
+    private static final int LOTTO_SIZE=6;
+    private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+
+    public static void setPurchaseMessage(Amount amount, List<Lotto> lotto){
+        System.out.println();
+        System.out.println(amount.getCount() + PURCHASE_MESSAGE);
+        printPurchaseLottoNum(amount.getCount(),lotto);
+    }
+
+    private static void printPurchaseLottoNum(int purchaseAmount, List<Lotto> lotto) {
+        for (int count = 0; count < purchaseAmount; count++) {
+            System.out.println(lotto.get(count).getNumbers());
+        }
+
+        System.out.println();
+    }
 }

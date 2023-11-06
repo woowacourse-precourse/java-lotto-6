cb57bfb050249d74e72af38724c1645de9299a99
whereami2048 <whereami2048@gmail.com>
2023-11-07 02:25:58 +0900

    feat: 로또 구매 금액 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    private static final String ASK_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public int inputPurchaseAmount() {
+        System.out.println(ASK_PURCHASE_AMOUNT);
+        return Integer.parseInt(readLine());
+    }
+}

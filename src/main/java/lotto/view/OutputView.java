2faaae8710ec35080b0db24b857e36c9013dd7d4
hongseokhodev <hongseokhodev@gmail.com>
2023-11-06 11:58:05 +0900

    feat: 수익률을 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,8 +7,12 @@ public class OutputView {
         System.out.println("당첨 통계\n---");
     }
     public void printStats(String description, int amount, int number) {
-        DecimalFormat decimalFormat = new DecimalFormat("###.###");
-        String formattedAmount = decimalFormat.format(amount);
-        System.out.println(description + " (" + formattedAmount + "원) - " + number + "개");
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        System.out.println(description + " (" + decimalFormat.format(amount) + "원) - " + number + "개");
+    }
+
+    public void printRateOfReturn(double rateOfReturn) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println("총 수익률은 " + decimalFormat.format(rateOfReturn) + "%입니다.");
     }
 }

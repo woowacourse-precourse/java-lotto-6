21f70e9bc531503e90103708cabb7cb6254ae116
ehddbs4521 <ehddbs452100@naver.com>
2023-11-06 22:53:07 +0900

    feat: 입력한 보너스 번호가 1과 45 사이인지 검증

--- src/main/java/lotto/view/InputBonus.java
+++ src/main/java/lotto/view/InputBonus.java
@@ -1,19 +1,23 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.RangeException;
 
 import java.util.regex.Pattern;
 
 public class InputBonus {
 
     private static final String INPUT_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
-
+    private static final int MIN = 1;
+    private static final int MAX = 45;
 
     private int getBonus() {
         System.out.println(INPUT_BONUS_MESSAGE);
         String bonus = Console.readLine();
         System.out.println();
         validate(bonus);
+        validateRange(bonus);
+        return Integer.parseInt(bonus);
     }
 
     private void validate(String bonus) {
@@ -21,5 +25,9 @@ public class InputBonus {
             throw new IllegalArgumentException();
         }
     }
-
+    private void validateRange(String bonus) {
+        if (Integer.parseInt(bonus) < MIN || Integer.parseInt(bonus) > MAX) {
+            throw new RangeException();
+        }
+    }
 }

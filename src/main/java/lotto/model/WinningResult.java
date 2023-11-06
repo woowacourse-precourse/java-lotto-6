a3aaae918667dfab8b85df945d486323ff383d36
ldw <leehan02@hanyang.ac.kr>
2023-11-07 07:57:11 +0900

    fix(lotto): 당첨 등수를 판별하고 집계하는 책임을 가진 클래스에 번호 일치 개수와 등수를 맵핑하기 위한 자료구조 추가

--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -7,16 +7,24 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public final class WinningResult {
-
-    private final Map<Integer, Long> winningResult;
+    private final Map<Integer, String> rankTable;
+    private final Map<String, Long> winningResult;
 
     public WinningResult(List<Integer> drawResult) {
+        this.rankTable = new HashMap<>();
+        this.rankTable.put(Rule.FIRST_RANK.value(), "1st");
+        this.rankTable.put(Rule.SECOND_RANK.value(), "2nd");
+        this.rankTable.put(Rule.THIRD_RANK.value(), "3rd");
+        this.rankTable.put(Rule.FOURTH_RANK.value(), "4th");
+        this.rankTable.put(Rule.FIFTH_RANK.value(), "5th");
+
         winningResult = drawResult.stream()
                 .filter(number -> number >= Rule.MINIMUM_NUMBER_TO_WIN.value())
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                .map(rankTable::get)
+                .collect(Collectors.groupingBy(Function.identity(),  Collectors.counting()));
     }
 
-    public Map<Integer, Long> getWinningResult() {
+    public Map<String, Long> getWinningResult() {
         return new HashMap<>(winningResult);
     }
 
@@ -25,15 +33,4 @@ public final class WinningResult {
             return Rule.SECOND_RANK.value();
         return rank;
     }
-
-    /**
-     * 당첨 통계
-     * ---
-     * 3개 일치 (5,000원) - 1개
-     * 4개 일치 (50,000원) - 0개
-     * 5개 일치 (1,500,000원) - 0개
-     * 5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-     * 6개 일치 (2,000,000,000원) - 0개
-     * 총 수익률은 62.5%입니다.
-     */
 }

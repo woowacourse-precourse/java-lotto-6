0dd2630651f43f6a004e074eacffe23938fd3083
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-07 02:51:38 +0900

    feat: 수익률 계산해주는 기능 추가

---
+++ src/main/java/lotto/service/ProfitCalculator.java
@@ -0,0 +1,22 @@
+package lotto.service;
+
+import lotto.domain.Result;
+import lotto.domain.User;
+import lotto.domain.Rank;
+
+
+public class ProfitCalculator {
+    public static long calcProfitRate(User user, Result result) {
+        int totalProfit = calcTotalProfit(result);
+        long profitRate = totalProfit / (user.getAmount() * 1000) *100
+        return profitRate;
+    }
+    private static Integer calcTotalProfit(Result result) {
+        int totalProfit = 0;
+        for (Rank rank: result.getResults()) {
+            totalProfit += rank.getAwardMoney();
+        }
+        return totalProfit;
+    }
+
+}

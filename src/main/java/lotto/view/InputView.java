b115d42f9e6f93b5eeeccdbddf07795348859259
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-07 02:40:36 +0900

    fix: change int to string format
    int형 -> String형으로 변화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static int requestPrice() {
-        return Integer.parseInt(Console.readLine());
+    public static String requestPrice() {
+        return Console.readLine();
     }
 
     public static String requestWinningNumber() {

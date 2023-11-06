eb7c191b5930e1dd664599d73e2d783f7aa9481c
JiEung2 <https://github.com/JiEung2>
2023-11-06 22:45:19 +0900

    Add: 1과 45 상수 선언

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -3,6 +3,8 @@ package lotto.utils;
 import java.util.regex.Pattern;
 
 public class Constants {
+    public static final int MAX_NUMBER = 45;
+    public static final int MIN_NUMBER = 1;
     public static final String ERROR_MESSAGE = "[ERROR] ";
     public static final String BUY_PRICE_PATTERN_ERROR = "로또 구입 금액은 숫자를 입력해야 합니다.";
     public static final String BUY_PRICE_UNIT_ERROR = "로또 구입 금액은 1000원 단위로 입력해야 합니다.";

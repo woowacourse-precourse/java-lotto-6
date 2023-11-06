f9f80c39f8d9fb148f674af628895c87949192e1
Annjieun <dkslwdms2121@naver.com>
2023-11-06 14:00:55 +0900

    feat(Util): 1부터 45 사이의 숫자 중 랜덤으로 6개의 숫자를 만드는 generateRandomNumber() 구현

--- src/main/java/lotto/util/Util.java
+++ src/main/java/lotto/util/Util.java
@@ -1,14 +1,22 @@
 package lotto.util;
 
+import static lotto.constant.LottoNumber.LOTTO_NUMBER_LENGTH;
+import static lotto.constant.LottoNumber.MAXIMUM_LOTTO_NUMBER;
+import static lotto.constant.LottoNumber.MINIMUM_LOTTO_NUMBER;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Util {
-    public static double revertToDouble(String number) {
-        return Double.parseDouble(number);
-    }
-
     public static String readInput() {
         return Console.readLine();
+    }
+
+    public static List<Integer> generateRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER.getNumber(), MAXIMUM_LOTTO_NUMBER.getNumber(), LOTTO_NUMBER_LENGTH.getNumber());
     }
 
 }

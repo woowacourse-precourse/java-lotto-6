b5f36c5978ba8f11d5c497d5496c55e794023c68
a3ure <148525725+a3ure@users.noreply.github.com>
2023-11-06 20:56:43 +0900

    feat(유효검사): 당첨 번호 유효성 검사
    입력된 당첨 번호 값이 있는지, 값이 숫자인지, 1~45 사이의 값인지, 중복 없는 6개의 값인지 확인

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -1,7 +1,9 @@
 package lotto.validation;
 
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.constants.ExceptionMessages;
 
 public class InputValidation {
@@ -13,8 +15,27 @@ public class InputValidation {
         isDivisibleBy1000(input);
     }
 
+    public void validateWinningNumbersInput(List<String> input) {
+        Set<String> set = new HashSet<>();
+
+        isNumbersExist(input);
+        for (String item : input) {
+            isDigit(item);
+            isDuplicateNumber(set, item);
+            isWithinLottoRange(item);
+        }
+        isValidLottoNumbersLength(set);
+
+    }
+
     public void isExist(String input) {
-        if (input.isEmpty()) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessages.NONE_INPUT.getMessage());
+        }
+    }
+
+    public void isNumbersExist(List<String> input) {
+        if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(ExceptionMessages.NONE_INPUT.getMessage());
         }
     }
@@ -30,6 +51,27 @@ public class InputValidation {
     public void isDivisibleBy1000(String input) {
         if (Integer.parseInt(input) % 1000 != 0) {
             throw new IllegalArgumentException(ExceptionMessages.NON_MULTIPLE_OF_1000.getMessage());
+        }
+    }
+
+    public void isDuplicateNumber(Set<String> set, String input) {
+        if (!set.add(input)) {
+            throw new IllegalArgumentException(
+                ExceptionMessages.DUPLICATE_LOTTO_NUMBERS.getMessage());
+        }
+    }
+
+    public void isWithinLottoRange(String input) {
+        int inputNumber = Integer.parseInt(input);
+        if ((inputNumber < 1) || (inputNumber > 45)) {
+            throw new IllegalArgumentException(ExceptionMessages.OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    public void isValidLottoNumbersLength(Set<String> input) {
+        if (input.size() != 6) {
+            throw new IllegalArgumentException(
+                ExceptionMessages.INVALID_LOTTO_NUMBERS_LENGTH.getMessage());
         }
     }
 }

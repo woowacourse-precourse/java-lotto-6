2558dd3f422cddf87d63113defb3fd3e518b02b8
namhoon-kim97 <kskcmh5723@gmail.com>
2023-11-06 23:02:46 +0900

    test: 당첨번호 중복 테스트 구현

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.exception.DuplicateNumberException;
+import org.junit.jupiter.api.Test;
+
+class WinningNumberTest {
+    @Test
+    public void testCreateWinningNumberWithDuplicateNumber() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+
+        int bonusNumber = 3;
+
+        assertThrows(DuplicateNumberException.class, () -> {
+            new WinningNumber(numbers, bonusNumber);
+        });
+    }
+
+    @Test
+    public void testCreateWinningNumberWithoutDuplicateNumber() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        int bonusNumber = 4;
+
+        new WinningNumber(numbers, bonusNumber);
+    }
+
+
+}

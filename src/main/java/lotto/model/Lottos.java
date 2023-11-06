62e0469c875da0022ca4d2ae767aa60f9a528ce6
Ukj0ng <xb253q@gmail.com>
2023-11-06 19:31:51 +0900

    feat(Lottos): 로또 티켓들의 집합 구현
    구현한 기능:
    - 발행한 로또들의 컬렉션
    - Lotto를 추가하는 기능

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,2 +1,20 @@
-package lotto.model;public class Lottos {
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos() {
+        this.lottos = new ArrayList<>();
+    }
+
+    public void addLotto(Lotto lotto) {
+        this.lottos.add(lotto);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }

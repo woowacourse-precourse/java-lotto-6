d8aaa5660f680098a9fbab3240d953fbf9ae7362
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 16:59:09 +0900

    refactor(winningNumbersTest): 테스트 클래스에 Nested 어노테이션 적용
    - 변수 사용하여 테스트 상황을 가독성 좋게 수정
    - WinningNumbersTest 클래스 내부에 당첨번호 테스트를 위한 Nested 클래스 추가
    - WinningNumbers객체에 bonusNumber 추가에 따른 당첨번호 테스트에 bonusNumber Mock Value 추가

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -1,30 +1,42 @@
 package lotto;
 
+import static lotto.Exception.DUPLICATE_BONUS_NUMBER;
 import static lotto.Exception.DUPLICATE_WINNING_NUMBER;
 import static lotto.Exception.INVALID_WINNING_NUMBER_RANGE;
 import static lotto.Exception.WINNING_NUMBERS_SIZE_SHOULD_BE_SIX;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.util.List;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersTest {
-    @Test
-    void 당첨번호_리스트의_크기가_6이_아닌_경우_예외() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4)))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage(WINNING_NUMBERS_SIZE_SHOULD_BE_SIX.getMessage());
-    }
-    @Test
-    void 당첨번호에_1부터_45사이_범위에_있지_않는_경우_예외() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 55, 10)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(INVALID_WINNING_NUMBER_RANGE.getMessage());
-    }
-    @Test
-    void 당첨번호_리스트에_중복된_숫자가_있는_경우_예외() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 10, 10)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(DUPLICATE_WINNING_NUMBER.getMessage());
+    @Nested
+    class 당첨번호_테스트 {
+        @Test
+        void 당첨번호_리스트의_크기가_6이_아닌_경우_예외() {
+            assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4), any(Integer.class)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(WINNING_NUMBERS_SIZE_SHOULD_BE_SIX.getMessage());
+        }
+
+        @Test
+        void 당첨번호에_1부터_45사이_범위에_있지_않는_경우_예외() {
+            int outRangeNumbers = 50;
+
+            assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 10, outRangeNumbers), any(Integer.class)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INVALID_WINNING_NUMBER_RANGE.getMessage());
+        }
+
+        @Test
+        void 당첨번호_리스트에_중복된_숫자가_있는_경우_예외() {
+            int sameNumber = 10;
+
+            assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, sameNumber, sameNumber), any(Integer.class)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(DUPLICATE_WINNING_NUMBER.getMessage());
+        }
     }
 }

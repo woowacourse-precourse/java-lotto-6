0dd993ecad34b5ac5df9b15a386afb580513c5fd
jisu-om <lifyji@naver.com>
2023-11-06 20:57:29 +0900

    test: LottoPurchaseAmountTest 테스트코드 리팩토링

--- src/test/java/lotto/domain/LottoPurchaseAmountTest.java
+++ src/test/java/lotto/domain/LottoPurchaseAmountTest.java
@@ -1,51 +1,15 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoPurchaseAmountTest {
-    @DisplayName("구입금액은 1000으로 나눠떨어지지 않으면 예외가 발생한다.")
-    @Test
-    void cannotPurchaseWithInvalidPurchaseAmount_number() {
-        assertThatThrownBy(() -> LottoPurchaseAmount.create("10100"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("구입금액이 0이면 예외가 발생한다")
-    @Test
-    void cannotPurchaseWithInvalidPurchaseAmount_zero() {
-        assertThatThrownBy(() -> LottoPurchaseAmount.create("0"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("구입금액이 음수이면 예외가 발생한다")
-    @Test
-    void cannotPurchaseWithInvalidPurchaseAmount_negativeNumber() {
-        assertThatThrownBy(() -> LottoPurchaseAmount.create("-1000"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("구입금액이 문자열이면 예외가 발생한다.")
-    @Test
-    void cannotPurchaseWithInvalidPurchaseAmount_string() {
-        assertThatThrownBy(() -> LottoPurchaseAmount.create("rsef"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("구입금액이 null이면 예외가 발생한다.")
-    @Test
-    void cannotPurchaseWithInvalidPurchaseAmount_null() {
-        assertThatThrownBy(() -> LottoPurchaseAmount.create(""))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("구입금액이 공백이면 예외가 발생한다.")
-    @Test
-    void cannotPurchaseWithInvalidPurchaseAmount_blank() {
-        assertThatThrownBy(() -> LottoPurchaseAmount.create(" "))
+    @ParameterizedTest(name = "[{index}] 구입금액이 ''{0}''이면 LottoPurchaseAmountTest 생성 시 예외가 발생한다.")
+    @ValueSource(strings = {"10100", "0", "-1000", "abc", "", " "})
+    void cannotCreate(String element) {
+        assertThatThrownBy(() -> LottoPurchaseAmount.create(element))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/lotto/domain/LottoPurchaseManagerTest.java
+++ src/test/java/lotto/domain/LottoPurchaseManagerTest.java
@@ -4,8 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-
 public class LottoPurchaseManagerTest {
     @DisplayName("LottoPurchaseManager 생성 테스트")
     @Test

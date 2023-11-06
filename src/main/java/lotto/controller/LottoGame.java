9017ce893f26f255429d8d33e0e62c9c1937b3de
kangyuseok <kingyuseok@naver.com>
2023-11-07 01:51:08 +0900

    Feat: controller생성

---
+++ src/main/java/lotto/controller/LottoGame.java
@@ -0,0 +1,34 @@
+package lotto.controller;
+
+import static lotto.service.Buyer.buyLottoTickets;
+import static lotto.service.Validator.validateDividedBy1000Won;
+import static lotto.service.Validator.validateLess1000Won;
+import static lotto.service.Validator.validateNumber;
+import static lotto.view.IO.requestPerchaseAmount;
+import static lotto.view.constants.Front.REQUEST_PERCHASE_AMOUNT;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.PurchaseAmount;
+import lotto.view.IO;
+import lotto.view.constants.Front;
+
+public class LottoGame {
+    public static void lottoGameStart(){
+        requestPerchaseAmount(REQUEST_PERCHASE_AMOUNT);
+        PurchaseAmount purchaseAmount = inputPurchaseAmount();
+        int lottoTicketCount = buyLottoTickets(purchaseAmount);
+    }
+
+    private static PurchaseAmount inputPurchaseAmount(){
+        while(true){
+            String money = Console.readLine();
+            try{
+                PurchaseAmount purchaseAmount = new PurchaseAmount(money);
+                return purchaseAmount;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}

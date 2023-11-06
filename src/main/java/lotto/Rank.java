acceb0dd2993f3f3110db12af763ef99557fe167
alenjb <alenjb@naver.com>
2023-11-07 06:37:37 +0900

    🎨fix(Rank): 매치되는 숫자의 개수 필드 추가
    매치되는 숫자의 개수 필드를 추가하여 로또 결과를 처리하는 클래스에서 하드코딩된 값을 사용하지 않게 함.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,21 +1,29 @@
 package lotto;
 
 public enum Rank {
-    FIRST_PLACE("2,000,000,000원"),
-    SECOND_PLACE("30,000,000원"),
-    THIRD_PLACE("1,500,000원"),
-    FOURTH_PLACE("50,000원"),
-    FIFTH_PLACE("5,000원")
+    FIRST_PLACE("2,000,000,000원", 6),
+    SECOND_PLACE("30,000,000원", 5.5),
+    THIRD_PLACE("1,500,000원", 5),
+    FOURTH_PLACE("50,000원", 4),
+    FIFTH_PLACE("5,000원", 3),
+    NONE("0원", 0)
     ;
 
     private final String reward;
+    private final double matchNumberCount;
 
-    Rank(String reward) {
+
+    Rank(String reward, double matchNumberCount) {
         this.reward = reward;
+        this.matchNumberCount = matchNumberCount;
     }
 
     public String getReward() {
         return reward;
+    }
+
+    public double getMatchNumberCount() {
+        return matchNumberCount;
     }
 }
 

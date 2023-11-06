9101ed8e1231f19c689db66383af175af959b1ec
HYOAN <aspqll@naver.com>
2023-11-06 21:16:15 +0900

    feat(lotto): 사용자의 입력값이 숫자인지 확인하는 테스트 구현

--- src/test/java/lotto/domain/UserInputTest.java
+++ src/test/java/lotto/domain/UserInputTest.java
@@ -1,4 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserInputTest {
-  
+    private UserInput userInput;
+
+    @BeforeEach
+     void setUp() {
+        userInput = new UserInput();
+    }
+    @Test
+    void 숫자_입력_확인() {
+        assertThrows(IllegalArgumentException.class, () -> userInput.checkInt("abc"));
+    }
+
 }

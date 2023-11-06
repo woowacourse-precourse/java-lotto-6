a5c1c3b11c6cd56ae94758345dd1d3f07f53c8bc
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-06 16:46:31 +0900

    feat(lotto): 중복된 번호가 입력되면 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +19,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicatedNumber(List<Integer> numbers){
+        List<Integer> tmp = new ArrayList<>();
+        for (int num : numbers){
+            if (!tmp.contains(num)){
+                tmp.add(num);
+            }
+        }
+
+        if (tmp.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
+    }
 }

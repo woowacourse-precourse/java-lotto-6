04d0c12da5d93078aa8a482b0ef55bf7684996a1
Gyeongho Yang <chchch1213@naver.com>
2023-11-06 19:12:41 +0900

    feat(controller): 예외 처리와 함께 유효한 구입 금액을 받을 때 까지 요청 하는 기능 구현

--- src/main/java/lotto/controller/SlotMachine.java
+++ src/main/java/lotto/controller/SlotMachine.java
@@ -1,10 +1,29 @@
 package lotto.controller;
 
+import lotto.domain.BuyPrice;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import static lotto.constant.GeneralMessage.NOTIFY_BUY_COUNT;
 
 public class SlotMachine {
 
     public static void run() {
-        InputView.askBuyPrice();
+        BuyPrice buyPrice = requestValidBuyPrice();
+        OutputView.printGeneralMessage(NOTIFY_BUY_COUNT, buyPrice.getBuyCount());
+    }
+
+    private static BuyPrice requestValidBuyPrice() {
+        BuyPrice buyPrice = null;
+        boolean valid = false;
+        while (!valid) {
+            try {
+                buyPrice = BuyPrice.from(InputView.askBuyPrice());
+                valid = true;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+        return buyPrice;
     }
 }

e1461f411e58746f2f90fc777afaabc7eaf475ae
honeydanji <tjdwls3680@naver.com>
2023-11-07 00:06:21 +0900

    TEST : 입력 예외 처리에 대한 예상값 수정
    // IllegalArgumentException 발생 시 예외 메세지로 응답
    - 기존 : 예외의 발생 유무에 대해서만 테스트 진행
    - 수정 : 예외 발생은 물론 예외 메세지가 예상값과 실제값이 일치하는 지 테스트

--- src/test/java/lotto/utils/BonusNumberExceptionTest.java
+++ src/test/java/lotto/utils/BonusNumberExceptionTest.java
@@ -12,20 +12,26 @@ public class BonusNumberExceptionTest {
 
     @Test
     void 보너스_번호_범위_벗어난_입력 () {
-        assertThrows(IllegalArgumentException.class, () ->
-                bonusNumberException.outsideRange("46"), "[ERROR]입력을 초과했습니다.");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                bonusNumberException.outsideRange("46"));
+
+        assertEquals("[ERROR]입력 범위를 벗어났습니다.", exception.getMessage());
     }
 
     @Test
     void 보너스_번호_당첨_번호_중복 () {
-        assertThrows(IllegalArgumentException.class, () ->
-                bonusNumberException.duplicateNumber(List.of("1","2","3","4","5","6"),"5"), "[ERROR]입력이 중복되었습니다.");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                bonusNumberException.duplicateNumber(List.of("1","2","3","4","5","6"),"5"));
+
+        assertEquals("[ERROR]입력이 중복되었습니다.", exception.getMessage());
     }
 
     @Test
     void 보너스_번호_숫자가_아닌_경우 () {
-        assertThrows(IllegalArgumentException.class, () ->
-                bonusNumberException.notNumeric("a"), "[ERROR]숫자를 입력하세요.");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                bonusNumberException.notNumeric("a"));
+
+        assertEquals("[ERROR]숫자를 입력하세요.", exception.getMessage());
     }
 }
 /*
--- src/test/java/lotto/utils/PurchaseAmountExceptionTest.java
+++ src/test/java/lotto/utils/PurchaseAmountExceptionTest.java
@@ -9,13 +9,17 @@ public class PurchaseAmountExceptionTest{
 
     @Test
     void 숫자가_아닌_값을_입력한_경우() {
-        assertThrows(IllegalArgumentException.class, () ->
-                        purchaseAmountException.notNumeric("a"), "[ERROR]숫자를 입력하세요.");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                        purchaseAmountException.notNumeric("a"));
+
+        assertEquals("[ERROR]숫자를 입력하세요.", exception.getMessage());
     }
 
     @Test
     void 숫자가_1000_단위로_나누어_떨어지지_않을_경우 (){
-        assertThrows(IllegalArgumentException.class, () ->
-                purchaseAmountException.notDivisible("1001"), "[ERROR]1000으로 나누어 떨어지지 않습니다.");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                purchaseAmountException.notDivisible("1001"));
+
+        assertEquals("[ERROR]1000으로 나누어 떨어지지 않습니다.", exception.getMessage());
     }
 }

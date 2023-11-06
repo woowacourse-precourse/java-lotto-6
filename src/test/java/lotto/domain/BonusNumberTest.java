40bc5b05c9ac16892f03adcb33ba519a287b0e28
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 08:26:24 +0900

    test(BonusNumberTest) : 보너스 도메인 테스트 코드 작성

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,58 @@
+package lotto.domain;
+
+import lotto.exception.BonusNumberException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.IllformedLocaleException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusNumberTest {
+
+    private final List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+
+    @Test
+    @DisplayName("보너스 숫자로 숫자가 아닌 다른 것이 들어온 경우 예외처리")
+    void isNumberTest(){
+        assertThatThrownBy(() -> new BonusNumber("abc", list)).isInstanceOf(
+                IllformedLocaleException.class);
+    }
+
+    @Test
+    @DisplayName("보너스 숫자로 들어온 것이 1개가 아닐 경우")
+    void isNumberOnlyOneTest(){
+        assertThatThrownBy(() -> new BonusNumber("1,2", list)).isInstanceOf(
+                IllegalArgumentException.class
+        );
+    }
+
+    @Test
+    @DisplayName("보너스 숫자로 들어온 것이 1 미만 45 초과일 경우")
+    void isRightRangeTest(){
+        assertThatThrownBy(() -> new BonusNumber("1,2,3,4,5,60", list)).isInstanceOf(
+                IllegalArgumentException.class
+        );
+    }
+
+    @Test
+    @DisplayName("보너스 숫자로 들어온 것이 공백일 경우")
+    void isBlankTest(){
+        assertThatThrownBy(() -> new BonusNumber(" ", list)).isInstanceOf(
+                IllegalArgumentException.class
+        );
+    }
+
+    @Test
+    @DisplayName("보너스 숫자로 들어온 것이 당첨 숫자와 중복일 경우")
+    void isDuplicateWithWinnerNumTest(){
+        assertThatThrownBy(() -> new BonusNumber("1", list)).isInstanceOf(
+                IllegalArgumentException.class
+        );
+    }
+
+}

fb17140dea8f8f750358d52751822de6ae155e74
hoyun06 <hoyun06@gmail.com>
2023-11-06 13:58:57 +0900

    test: 당첨 번호 숫자 테스트 생성

--- src/test/java/lotto/validator/GlobalValidatorTest.java
+++ src/test/java/lotto/validator/GlobalValidatorTest.java
@@ -3,6 +3,8 @@ package lotto.validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static lotto.validator.GlobalValidator.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,5 +30,16 @@ class GlobalValidatorTest {
         // when, then
         assertThrows(IllegalArgumentException.class,
                 () -> validatePurchaseAmount(purchaseAmount));
+    }
+
+    @Test
+    @DisplayName("당첨 번호 숫자인지 테스트")
+    void winningNumberDigitTest() {
+        //given
+        List<String> winningNumber = List.of("1,2,3,4,a,6");
+
+        // when, then
+        assertThrows(IllegalArgumentException.class,
+                () -> validateWinningNumber(winningNumber));
     }
 }

55b9a0dc4b3e562de24b66bff288173c5b0d4d93
daneng4 <daneng4@naver.com>
2023-11-06 19:34:49 +0900

    feat: 로또 한장의 번호들을 생성하는 기능 추가

--- src/main/java/model/MyLotto.java
+++ src/main/java/model/MyLotto.java
@@ -1,8 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import util.NumberGenerator;
+
 public class MyLotto {
 
     int lottoPaper;
+    public NumberGenerator numberGenerator = new NumberGenerator();
+    ArrayList<List<Integer>> myLottoNumber = new ArrayList<>();
+
 
     public void buyLotto(int lottoCost){
         lottoPaper = lottoCost/1000;
@@ -10,6 +17,16 @@ public class MyLotto {
 
     public int getLottoPaper(){
         return lottoPaper;
+    }
+
+    public ArrayList<List<Integer>> getMyLottoNumber(){
+        return myLottoNumber;
+    }
+
+    public void createMyLottoNumber(int lottoPaper){
+        for(int i = 0; i<lottoPaper; i++){
+            myLottoNumber.add(numberGenerator.generate());
+        }
     }
 
 }

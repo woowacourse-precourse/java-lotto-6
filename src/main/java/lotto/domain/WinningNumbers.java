3aecbf235fc4f6679c0856465c1b345a0cb29beb
bokyung <bbookng@gmail.com>
2023-11-06 11:54:48 +0900

    feat : WinningNumbers 생성자 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,13 +1,21 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
-    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = winningNumbers;
+    public WinningNumbers(String winningNumbers, int bonusNumber) {
+        this.winningNumbers = splitWinningNumbersString(winningNumbers);
         this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> splitWinningNumbersString(String winningNumbers) {
+        return Arrays.stream(winningNumbers.split(","))
+                .map(number -> Integer.parseInt(number))
+                .collect(Collectors.toList());
     }
 }

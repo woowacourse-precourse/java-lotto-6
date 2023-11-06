0828b42124b16de6d8017d835a8a49b300d78edf
yonggwonk <gwonluck@dgu.ac.kr>
2023-11-06 20:31:10 +0900

    feat: Generate Lotto.java
    기존에 주어진 Lotto 클래스에 에러 관리 메서드, 오름차순으로 리스트를 반환하는 메서드를 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,46 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class Lotto {
+class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1 부터 45 사이여야 합니다.");
+            }
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbersInAscendingOrder() {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
+    }
+
+    public boolean containsNumber(int number) {
+        for (int lottoNumber : numbers) {
+            if (lottoNumber == number) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

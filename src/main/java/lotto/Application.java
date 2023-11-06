bdb56abe78eda1c3b351e12f8072305786a46911
박정현 <ros28081@naver.com>
2023-11-06 20:19:50 +0900

    feat(Application): static 변수 추가, 구매 가격 변수 getter 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
 public class Application {
+    private static final int LOTTO_PRICE = 1_000;
+    private static final int LOTTO_SIZE = 6;
+    private static int purchasedPrice;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public static int getPurchasedPrice() {
+        return purchasedPrice;
     }
 }

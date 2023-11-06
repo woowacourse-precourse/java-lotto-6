1eccf4d52cbc427109d69aa783489a7eeb2c412b
jihyuk <kwlgur0416@gmail.com>
2023-11-06 17:26:08 +0900

    test : 당첨 번호와 보너스 번호 입력 테스트 작성
    - 정상 로직 테스트
    - 숫자 예외 테스트
    - 빈값 예외 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -7,9 +7,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import java.util.List;
 import lotto.exception.LottoException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
@@ -44,6 +47,57 @@ class InputViewTest {
         command(input);
 
         assertThatThrownBy(InputView::inputLottoPurchaseAmount)
+            .isInstanceOf(LottoException.class)
+            .hasMessageContaining(BLANK_INPUT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "1, 2, 3, 4, 5, 6"})
+    void 당첨_번호를_정상적으로_입력받는다(final String input) {
+        command(input);
+
+        final List<Integer> winningNumber = InputView.inputLottoWinningNumbers();
+
+        assertThat(winningNumber).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,poby", "1,2,3,,5,6"})
+    void 당첨_번호에_숫자가_값이_포함되지_않는다면_예외를_던진다(final String input) {
+        command(input);
+
+        assertThatThrownBy(InputView::inputLottoPurchaseAmount)
+            .isInstanceOf(LottoException.class)
+            .hasMessageContaining(NOT_INTEGER_INPUT.getMessage());
+
+    }
+
+    @Test
+    void 보너스_번호를_정상적으로_입력받는다() {
+        final String input = "5";
+        command(input);
+
+        final int bonusNumber = InputView.inputLottoBonusNumber();
+
+        assertThat(bonusNumber).isEqualTo(5);
+    }
+
+    @Test
+    void 보너스_번호가_숫자가_아니라면_예외를_던진다() {
+        final String input = "poby";
+        command(input);
+
+        assertThatThrownBy(InputView::inputLottoBonusNumber)
+            .isInstanceOf(LottoException.class)
+            .hasMessageContaining(NOT_INTEGER_INPUT.getMessage());
+    }
+
+    @Test
+    void 보너스_번호가_빈값이라면_예외를_던진다() {
+        final String input = "\n";
+        command(input);
+
+        assertThatThrownBy(InputView::inputLottoBonusNumber)
             .isInstanceOf(LottoException.class)
             .hasMessageContaining(BLANK_INPUT.getMessage());
     }

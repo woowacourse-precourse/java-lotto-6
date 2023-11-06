c5427aa7795943516080618c249429f12ad3cd0a
i3kae <snoger1234@gmail.com>
2023-11-06 15:06:51 +0900

    feat : 메인, 당첨 통계 메시지 추가 1. 메인 동작에서 사용되는 메시지들 추가 2. 당첨 통계에서 사용되는 메시지들 추가

--- src/main/java/lotto/Messages.java
+++ src/main/java/lotto/Messages.java
@@ -14,4 +14,36 @@ public class Messages {
             return this.errorMessage;
         }
     }
+
+    public enum MainMessage{
+        INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+        WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+        BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+        WINNING_STATISTICS("당첨 통계\n---"),
+        NUMBER_OF_ITEMS_PURCHASE("%d개를 구매했습니다.");
+
+        private String mainMessage;
+        private MainMessage(String message){
+            this.mainMessage = message;
+        }
+        public String getMessage(){
+            return this.mainMessage;
+        }
+    }
+
+    public enum WinningStatistics{
+        WIN_3_MATCH("3개 일치 (5,000원) - %d개"),
+        WIN_4_MATCH("4개 일치 (50,000원) - %d개"),
+        WIN_5_MATCH("5개 일치 (1,500,000원) - %d개"),
+        WIN_5_BONUS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+        WIN_6_MATCH("6개 일치 (2,000,000,000원) - %d개");
+
+        private String winningStatisticsMessage;
+        private WinningStatistics(String message){
+            this.winningStatisticsMessage = message;
+        }
+        public String getMessage(){
+            return this.winningStatisticsMessage;
+        }
+    }
 }

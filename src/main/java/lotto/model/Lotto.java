c48c899d2b1ea0973535208869f46de5e069749a
chan-won <clw8679@naver.com>
2023-11-07 01:13:53 +0900

    feat: Lotto 번호가 중복일 경우 예외 처리 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -14,6 +14,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현

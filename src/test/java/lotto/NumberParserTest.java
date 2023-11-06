323c5f558fffbabadcabdcfe3a295cfffaed83f8
choiejae <ejae8319@gmail.com>
2023-11-06 17:38:13 +0900

    test(NumberParser): 구입 금액 입력 값 분석 테스트 작성
    - 공백이 있는 경우 공백을 다듬어 객체를 생성 하는가
    - 입력 값이 정수가 아닌 경우 예외를 발생 하는가
    - 입력 값이 빈칸일 경우 예외를 발생 하는가
    - 입력 값이 공백일 경우 예외를 발생 하는가
    - 입력 값이 null 일 경우 예외를 발생 하는가

---
+++ src/test/java/lotto/NumberParserTest.java
@@ -0,0 +1,74 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumberParserTest {
+
+    @Test
+    @DisplayName("앞, 뒤 공백이 있는 경우 공백을 다듬어 객체를 생성한다")
+    void removeTrim() {
+        //given
+        String input = "  1000   ";
+
+        //when
+        PurchaseAmount purchaseAmount = NumberParser.parsePurchaseAmount(input);
+
+        //then
+        assertEquals(purchaseAmount.getPurchaseAmount(), 1000);
+    }
+
+    @Test
+    @DisplayName("입력 값이 정수가 아니면 예외를 발생")
+    void throwExceptionIfInputNotInteger() {
+        // given
+        String input = "money";
+
+        // when
+        // then
+        assertThatThrownBy(() -> NumberParser.parsePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력 값이 빈칸이면 예외를 발생")
+    void throwExceptionIfInputIsEmpty() {
+        // given
+        String input = "";
+
+        // when
+        // then
+        assertThatThrownBy(() -> NumberParser.parsePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NumberParser.INPUT_BLANK_EXCEPTION);
+    }
+
+    @Test
+    @DisplayName("입력 값이 공백이면 예외를 발생")
+    void throwExceptionIfInputIsBlank() {
+        // given
+        String input = " ";
+
+        // when
+        // then
+        assertThatThrownBy(() -> NumberParser.parsePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NumberParser.INPUT_BLANK_EXCEPTION);
+    }
+
+    @Test
+    @DisplayName("입력 값이 null이면 예외를 발생")
+    void throwExceptionIfInputIsNull() {
+        // given
+        String input = null;
+
+        // when
+        // then
+        assertThatThrownBy(() -> NumberParser.parsePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NumberParser.INPUT_BLANK_EXCEPTION);
+    }
+}

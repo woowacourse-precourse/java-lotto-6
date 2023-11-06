eb7a8fdcea2d9bd317dc537259170b6dcf259f0a
unknown <gpfls981220@gmail.com>
2023-11-07 02:02:32 +0900

    feat: add output for number of purchases

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     void outputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+    
+    void outputNumberOfPurchases(int number) {
+        System.out.println(number + "개를 구매했습니다.");
+    }
 }

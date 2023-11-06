53d515e7e55c085fd136cf09a1d36fe9b324ac89
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 13:35:18 +0900

    feat(inputView): 당첨 번호를 입력받는 메서드 작성
    - 공백을 제거하는 메서드 작성
    - 구분자에 맞게 당첨 번호를 split하는 메서드 작성
    - split한 배열을 당첨번호 List로 변환하는 메서드 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,37 @@ package lotto.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.Exception.ONLY_NUMERIC_INPUT_FOR_MONEY;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+    private final String WINNING_NUMBERS_DELIMITER = ",";
     public int inputMoney() {
         try {
             return Integer.parseInt(readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ONLY_NUMERIC_INPUT_FOR_MONEY.getMessage());
         }
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        String input = removeSpace(readLine());
+        String[] numbers = splitWinningNumbers(input);
+
+        return convertToIntegers(numbers);
+    }
+
+    private String removeSpace(String input) {
+        return input.replace(" ", "");
+    }
+
+    private String[] splitWinningNumbers(String input) {
+        return input.split(WINNING_NUMBERS_DELIMITER);
+    }
+
+    private List<Integer> convertToIntegers(String[] numbers) {
+        return Arrays.stream(numbers)
+                .map(Integer::parseInt)
+                .toList();
     }
 }

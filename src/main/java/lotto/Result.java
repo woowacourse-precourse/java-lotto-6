f3ccae08b3ad1066c6fa59198b0ec69c3822c9ff
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 20:19:51 +0900

    fix: Result prize 메서드 접근 제어자 변경(public)

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -3,14 +3,20 @@ package lotto;
 import java.util.stream.Stream;
 
 public enum Result {
-    FIRST_PRIZE(6, false) {int prize() {return 2_000_000_000;}},
-    SECOND_PRIZE(5, true) {int prize() {return 30_000_000;}},
-    THIRD_PRIZE(5, false) {int prize() {return 1_500_000;}},
-    FOURTH_PRIZE(4, false) {int prize() {return 50_000;}},
-    FIFTH_PRIZE(3, false) {int prize() {return 5_000;}},
-    NO_PRIZE(2, false) {int prize() {return 0;}};
+    FIRST_PRIZE(6, false) {
+        public int prize() {return 2_000_000_000;}},
+    SECOND_PRIZE(5, true) {
+        public int prize() {return 30_000_000;}},
+    THIRD_PRIZE(5, false) {
+        public int prize() {return 1_500_000;}},
+    FOURTH_PRIZE(4, false) {
+        public int prize() {return 50_000;}},
+    FIFTH_PRIZE(3, false) {
+        public int prize() {return 5_000;}},
+    NO_PRIZE(2, false) {
+        public int prize() {return 0;}};
 
-    abstract int prize();
+    public abstract int prize();
 
     private final int matchingNumbers;
     private final boolean bonus;

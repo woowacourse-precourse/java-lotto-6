46c76c92d1ebe14bdb2a14ce28b4f66f5ff5365f
hyebin <xxico89@gmail.com>
2023-11-06 22:49:23 +0900

    [Feat] enum 생성자 요소에 prizeNum (한 로또 당 당첨번호가 몇개 겹치는지에 대한 개수) 추가 ->  105는 5개 겹침 + 보너스볼 (보너스를 100점으로 설정)

--- src/main/java/lotto/prizeStatus.java
+++ src/main/java/lotto/prizeStatus.java
@@ -1,14 +1,17 @@
 package lotto;
 
 public enum prizeStatus {
-    THREE(3, 5000), FOUR(4, 50000), FIVE(5, 1500000), FIVEBONUS(5, 30000000), SIX(6, 2000000000);
+    THREE(3, 5000, 3), FOUR(4, 50000, 4), FIVE(5, 1500000, 5),
+    FIVEBONUS(5, 30000000, 105), SIX(6, 2000000000, 6);
 
     private int num;
     private int price;
+    private int prizeNum;
 
-    prizeStatus(int num, int price) {
+    prizeStatus(int num, int price, int prizeNum) {
         this.num = num;
         this.price = price;
+        this.prizeNum = prizeNum;
     }
 
     public int getNum() {
@@ -18,4 +21,8 @@ public enum prizeStatus {
     public int getPrice() {
         return this.price;
     }
+
+    public int getPrizeNum() {return this.prizeNum;}
 }
+
+

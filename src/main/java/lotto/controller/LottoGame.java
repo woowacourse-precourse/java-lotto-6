023dac4eba94bbd47d4b86252f9033105387fbfc
donghoonyeom <dua5410773@naver.com>
2023-11-07 03:19:10 +0900

    refactor(Application): Application 클래스 분리 (3/3)
    LottoGame 클래스에서 게임 로직을 처리하고, Application 클래스는 단순한 실행 클래스로 사용하도록 변경했습니다.

---
+++ src/main/java/lotto/controller/LottoGame.java
@@ -0,0 +1,33 @@
+package lotto.controller;
+
+import java.util.HashSet;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoGenerator;
+import lotto.model.LottoResult;
+import lotto.model.LottoResultCalculator;
+import lotto.model.WinningNumbers;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGame {
+    public void run() {
+        int purchaseAmount = InputView.getPurchaseAmount();
+        List<Lotto> lottos = generateLottos(purchaseAmount);
+        List<Integer> winningNumbers = InputView.getWinningNumbers();
+        int bonusNumber = InputView.getBonusNumber(new HashSet<>(winningNumbers));
+
+        WinningNumbers winningNumbersObject = new WinningNumbers(winningNumbers, bonusNumber);
+        LottoResult lottoResult = LottoResultCalculator.calculateResult(lottos, winningNumbersObject);
+
+        OutputView.printLottoNumbers(lottos.size(), lottos);
+        OutputView.printResult(lottoResult);
+
+        double winningRate = lottoResult.calculateWinningRate(purchaseAmount);
+        OutputView.printWinningRate(winningRate);
+    }
+
+    private List<Lotto> generateLottos(int purchaseAmount) {
+        return LottoGenerator.generateLottos(purchaseAmount / LottoGenerator.LOTTO_PRICE);
+    }
+}

5d325a2a31873888ae23a866fba392feb590eed8
JaeHunJang <wuddlaa@gmail.com>
2023-11-06 17:01:37 +0900

    refactor(InputValidator): 함수 내에서 직접 예외처리하도록 수정

--- src/main/java/lotto/domain/InputValidator.java
+++ src/main/java/lotto/domain/InputValidator.java
@@ -1,30 +1,50 @@
 package lotto.domain;
 
+import lotto.config.ErrorMessage;
+
 import java.util.List;
 
 public class InputValidator {
 
-    public static boolean isNotNumeric(String number) {
-        return !number.matches("^[\\d]*$");
+
+    public static void validateEmpty(String number) {
+        if(number.isEmpty() || number.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY.getMessage());
+        }
+    }
+    public static void validateNumeric(String number) {
+        if(!number.matches("^[\\d]*$")) {
+            throw new IllegalArgumentException(ErrorMessage.CONTAIN_IMPROPER_LETTER.getMessage());
+        }
     }
 
-    public static boolean isNotRemainderZero(String number, int divider) {
-        return Integer.parseInt(number) % divider != 0;
+    public static void validateRemainderZero(String number, int divider) {
+        if(Integer.parseInt(number) % divider != 0) {
+            throw new IllegalArgumentException(ErrorMessage.EXIST_REMAINDER.getMessage());
+        }
     }
 
-    public static boolean isNotMatchNumbersCount(String numbers, String delimiter, int count) {
-        return numbers.split(delimiter).length != count;
+    public static void validateDelimiterFormat(String numbers, String delimiter, int count) {
+        if(numbers.split(delimiter).length != count) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT.getMessage());
+        }
     }
 
-    public static boolean isDuplicateNumber(List<String> numbers) {
-        return numbers.size() != numbers.stream().distinct().count();
+    public static void validateDuplicateNumber(List<String> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
     }
 
-    public static boolean isDuplicateNumber(List<Integer> numbers, int number) {
-        return numbers.contains(number);
+    public static void validateExistNumber(List<Integer> numbers, int number) {
+        if(numbers.contains(number)) {
+            throw new IllegalArgumentException(ErrorMessage.EXIST_NUMBER.getMessage());
+        }
     }
 
-    public static boolean isNumberNotInRange(String number, int start, int end) {
-        return Integer.parseInt(number) < start || Integer.parseInt(number) > end;
+    public static void validateNumberInRange(String number, int start, int end) {
+        if(Integer.parseInt(number) < start || Integer.parseInt(number) > end) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_RANGE_NUMBER.getMessage());
+        }
     }
 }

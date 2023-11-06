0f4bf50e49fe9becf571b2d0a3cdbe6ef314894f
toychip <dlawnsgud427@naver.com>
2023-11-06 12:46:47 +0900

    refactor: 외부에서 주입받아 사용할 수 있게 수정하여 static 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String read() {
+    public String read() {
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,39 +2,39 @@ package lotto.view;
 
 public class OutputView {
 
-    public static void askAmount() {
+    public void askAmount() {
         print("구입금액을 입력해 주세요.");
     }
 
-    public static void showTickets(int count) {
+    public void showTickets(int count) {
         print(count + "개를 구매했습니다.");
     }
 
-    public static void askWinningNum() {
+    public void askWinningNum() {
         print("당첨 번호를 입력해주세요.");
     }
 
-    public static void askBonusNum() {
+    public void askBonusNum() {
         print("보너스 번호를 입력해주세요.");
     }
 
-    public static void showResults() {
+    public void showResults() {
         print("당첨 통계\n---");
     }
 
-    public static void showYield(double yield) {
+    public void showYield(double yield) {
         print("총 수익률은 %.1f%%입니다.", yield);
     }
 
-    private static void print(String str) {
+    private void print(String str) {
         System.out.println(str);
     }
 
-    private static void print(String str, double yield) {
+    private void print(String str, double yield) {
         System.out.printf(str, yield);
     }
 
-    private static void print(int count, String str) {
+    private void print(int count, String str) {
         System.out.println(count + str);
     }
 }

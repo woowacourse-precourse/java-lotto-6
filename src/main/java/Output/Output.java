b2d8d83ea9e20b4dc3319bfb33e3230829c14539
SeungGwa123 <skwan123@naver.com>
2023-11-06 19:13:56 +0900

    Feat: Add buyNumberPaperAns
    복권 구매 수량을 알려주는 print문 작성

--- src/main/java/Output/Output.java
+++ src/main/java/Output/Output.java
@@ -4,4 +4,8 @@ public class Output {
     public void buyMoneyAsk(){
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public void buyNumberPaperAns(int number){
+        System.out.println(number+"개를 구매했습니다.");
+    }
 }

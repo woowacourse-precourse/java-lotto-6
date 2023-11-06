9a1db1938e33c1c990fd6a36608f6107a3a8d8ed
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-07 03:13:39 +0900

    fix: 오타 수정

--- src/main/java/lotto/service/ProfitCalculator.java
+++ src/main/java/lotto/service/ProfitCalculator.java
@@ -8,7 +8,7 @@ import lotto.domain.Rank;
 public class ProfitCalculator {
     public static long calcProfitRate(User user, Result result) {
         int totalProfit = calcTotalProfit(result);
-        long profitRate = totalProfit / (user.getAmount() * 1000) *100
+        long profitRate = totalProfit / (user.getAmount() * 1000) *100;
         return profitRate;
     }
     private static Integer calcTotalProfit(Result result) {

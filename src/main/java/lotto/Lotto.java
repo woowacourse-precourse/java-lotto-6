02f40881d87e146634952f34b50df522cc52eda2
bbbig5360 <bbbig5360>
2023-11-07 03:11:24 +0900

    로또 당첨번호 중복 체크 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,28 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateCheck(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void duplicateCheck(List<Integer> numbers){        
+        Set<Integer> lottoNumbers = new HashSet<>(numbers);
+        if(lottoNumbers.size() != numbers.size()){
+            System.out.println("[ERROR] 당첨 번호에 중복된 숫자는 허용하지 않습니다.");
             throw new IllegalArgumentException();
         }
     }

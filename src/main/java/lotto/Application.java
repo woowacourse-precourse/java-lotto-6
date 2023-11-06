cf923aaaf5320dee02351dafc82ae7c59556796c
황준혁 <hyug0819@gmail.com>
2023-11-07 01:26:34 +0900

    feat : Lotto 시작 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = LottoController.getInstance();
+        lottoController.start();
     }
 }

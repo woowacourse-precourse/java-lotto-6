644085cc40b487b643222aa7af394bff6b42f6a4
Tiger <skdlghwns@gmail.com>
2023-11-06 21:38:46 +0900

    feat: 로또 생성기능 추가 - generator/LottoGenerator를 활용하여 입력 개수 만큼 로또 생성

--- src/main/java/lotto/generator/LottoGenerator.java
+++ src/main/java/lotto/generator/LottoGenerator.java
@@ -1,4 +1,24 @@
 package lotto.generator;
 
+import lotto.model.Lotto;
+import lotto.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGenerator {
+    public LottoGenerator(){}
+
+    private static Lotto makeOneLotto(List<Integer> choiceNumbers){
+        Lotto lotto = new Lotto(choiceNumbers);
+        return lotto;
+    }
+
+    public static List<Lotto> buyLottos(int quantity){
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0; i<quantity; i++){
+            lottos.add(makeOneLotto(InputView.choiceNumbers()));
+        }
+        return lottos;
+    }
 }

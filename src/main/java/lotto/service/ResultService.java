d70a29258ce014d5fa0b32b6b6e7dd8536f58165
sangeun-b <baek0007@algonquinlive.com>
2023-11-07 00:03:01 +0900

    feat: 로또 번호 비교

---
+++ src/main/java/lotto/service/ResultService.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.ResultType;
+import lotto.domain.WinningNumber;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultService {
+
+    public Map<ResultType,Integer> initResult() {
+        Map<ResultType,Integer> result = new HashMap<>();
+        for(ResultType resultType:ResultType.values()) {
+            result.put(resultType,0);
+        }
+        return result;
+    }
+
+    public int compareLotto(Lotto lotto, WinningNumber winningNumber) {
+        int check = 0;
+        for(int i=0; i<lotto.getNumbers().size(); i++) {
+            if(winningNumber.getWinningNumber().contains(lotto.getNumbers().get(i))) {
+                check++;
+            }
+        }
+        return check;
+    }
+}

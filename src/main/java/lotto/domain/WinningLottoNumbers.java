634984d1c284397e18527d1e53f1f0a874b342b1
wonjun <wonjon0930@daum.net>
2023-11-06 22:46:17 +0900

    feat: add feature to check user input about winning lotto numbers are convertible to numeric type

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -2,15 +2,43 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.view.exception.LottoInputException;
+import lotto.view.message.LottoInputExceptionMessage;
 
 public class WinningLottoNumbers {
     private List<LottoNumber> winningLottoNumbers;
 
-    private WinningLottoNumbers(String winningLottoNumbers){
+    private WinningLottoNumbers(List<String> winningLottoNumbers) {
         this.winningLottoNumbers = new ArrayList<>();
     }
 
-    public static WinningLottoNumbers of(String winningLottoNumbers){
-        return new WinningLottoNumbers(winningLottoNumbers);
+    public static WinningLottoNumbers from(final List<String> numbers) {
+        validate(numbers);
+        return new WinningLottoNumbers(numbers);
+    }
+
+    private static void validate(final List<String> numbers) {
+        // 6개의 값이 숫자 형식인지 확인한다.
+        isEachInputNumericType(numbers);
+        // 중복이 되지 않는지 확인한다.
+        // LottoNumber -> 1 ~ 45 까지의 숫자인지를 책임진다.
+    }
+
+    private static void isEachInputNumericType(final List<String> numbers) {
+        for (String number : numbers) {
+            throwExceptionIfNotNumeric(number);
+        }
+    }
+
+    private static void throwExceptionIfNotNumeric(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw LottoInputException.of(LottoInputExceptionMessage.WINNING_LOTTO_NUMBER_IS_NOT_NUMERIC_TYPE);
+        }
+    }
+
+    private static void isEachInputNotDuplicate(final List<String> numbers) {
+
     }
 }

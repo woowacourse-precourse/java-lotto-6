472266e97419d6850336bf1cbb6198a9c3b5c3b9
sinyebin <dpqls0625@naver.com>
2023-11-06 22:39:56 +0900

    feat: 당첨번호 입력 (예외사항) 양수가 아닐 때

--- src/main/java/lotto/service/InputLotto.java
+++ src/main/java/lotto/service/InputLotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lotto.domain.Lotto;
+import lotto.view.ErrorMessage;
 import lotto.view.PrintMessage;
 
 public class InputLotto {
@@ -15,14 +16,29 @@ public class InputLotto {
     }
 
     public static List<Integer> setLotto() {
-        String input = Console.readLine();
-        return separateLottoNumber(input);
+        try {
+            String input = Console.readLine();
+            isNumeric(input);
+            return separateLottoNumber(input);
+        } catch (IllegalArgumentException e) {
+            ErrorMessage.printErrorMessage(e);
+            PrintMessage.printWinnigLotto();
+            return setLotto();
+        }
     }
+
 
     public static List<Integer> separateLottoNumber(String input) {
         List<Integer> separateNumbers = Stream.of(input.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
         return separateNumbers;
+    }
+
+    public static void isNumeric(String input) {
+        if (!input.matches("^\\d+(,\\d+)*$")) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUM_ERROR);
+        }
+
     }
 }

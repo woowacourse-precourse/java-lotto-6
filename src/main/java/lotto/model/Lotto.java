0000c9d9d944f2501abfbaeb1d76c2838012c8a0
4444ki <qkrwogh423@gmail.com>
2023-11-06 15:43:04 +0900

    ## refactor : validate 수정 작성한 validate를 통해 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.List;
+import static lotto.validator.LottoNumberValidator.validate_MIN_SIZE;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +12,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        validate_MIN_SIZE(numbers);
     }
 
     public List<Integer> getNumbers() {

cf29170b696622b42ab706294c2cb6718852e2cd
Chenowoo <cjsgusdn14@naver.com>
2023-11-06 19:13:43 +0900

    feat(BonusString): 보너스 정보가 담긴 enum 기능 구현

--- src/main/java/lotto/constant/BonusString.java
+++ src/main/java/lotto/constant/BonusString.java
@@ -1,4 +1,15 @@
 package lotto.constant;
 
 public enum BonusString {
+    BONUS("Bonus"),
+    NOBONUS("NoBonus");
+    private String bonus;
+
+    BonusString(String hasBonus) {
+        this.bonus = hasBonus;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
 }

7ce332c208a6153264f7f4bda6542b327ff52ec7
seokmyungham <hellotjraud@gmail.com>
2023-11-06 15:08:48 +0900

    feat(InputView): OutputView 클래스 삭제 및 InputView 입력 시 프롬프트 출력 추가
    기존 OutputView 클래스가 담당하던 기능을 역할에 맡게 InputView 클래스로 재배치 시켰습니다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,16 +3,22 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public String inputPurchaseAmount() {
+    public static String inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return Console.readLine();
     }
 
-    public String inputWinningNumbers() {
+    public static String inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
         return Console.readLine();
     }
 
-    public String inputBonusNumber() {
+    public static String inputBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }

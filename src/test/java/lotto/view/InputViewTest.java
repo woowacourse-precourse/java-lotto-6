589fab82832aa7dc290b18c2aa1dff7320a87a05
hyeonjilee <skylar12200@gmail.com>
2023-11-06 11:38:39 +0900

    test(InputViewTest) : 가독성을 위해 메소드 이름 수정

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -20,30 +20,30 @@ public class InputViewTest {
         Console.close();
     }
 
-    @DisplayName("공백을 입력하면 예외를 반환한다.")
+    @DisplayName("구입 금액에 공백을 입력하면 예외를 반환한다.")
     @ParameterizedTest
     @EmptySource
-    void check_null_blank(String empty) {
+    void check_purchase_null_blank(String empty) {
         NumberValidator numberValidator = new NumberValidator();
         assertThatThrownBy(() -> numberValidator.validate(empty))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("공백은 입력할 수 없습니다.");
     }
 
-    @DisplayName("숫자가 아닌 값을 입력하면 예외를 반환한다.")
+    @DisplayName("구입 금액에 숫자가 아닌 값을 입력하면 예외를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"a", "a1", "-2"})
-    void check_numeric(String rawPurchase) {
+    void check_purchase_numeric(String rawPurchase) {
         System.setIn(createUserInput(rawPurchase));
         assertThatThrownBy(InputView::inputPurchase)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자로 입력해야합니다.");
     }
 
-    @DisplayName("Integer 범위 이상의 금액을 입력할경우 예외를 반환한다.")
+    @DisplayName("구입 금액에 Integer 범위 이상의 금액을 입력할경우 예외를 반환한다.")
     @ParameterizedTest
     @ValueSource(longs = {(long)(Integer.MAX_VALUE) + 1, (long)(Integer.MIN_VALUE) - 1})
-    void check_range(long rawPurchase) {
+    void check_purchase_range(long rawPurchase) {
         System.setIn(createUserInput(rawPurchase + ""));
         assertThatThrownBy(InputView::inputPurchase)
                 .isInstanceOf(IllegalArgumentException.class)

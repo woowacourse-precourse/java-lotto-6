a48fdda52be7041ccd1c7ce02d20869f705955e7
brightfanta <brightfanta@gmail.com>
2023-11-06 21:50:27 +0900

    style(ConvertorTest): import문 사용 현황에 맞게 수정

--- src/test/java/lotto/domain/ConvertorTest.java
+++ src/test/java/lotto/domain/ConvertorTest.java
@@ -4,10 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConvertorTest {
 

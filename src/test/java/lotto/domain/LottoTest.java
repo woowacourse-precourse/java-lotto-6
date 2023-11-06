48dfaa62840e1c6cc7e7fb4c6fef950401eaef86
dukcode <duk9741@gmail.com>
2023-11-06 18:43:09 +0900

    test: 로또 당첨 번호 검증 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,26 +1,123 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
-    void createLottoByOverSize() {
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void createLottoByDuplicatedNumber() {
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    public void 숫자가_3개_일치_보너스_숫자_0개_일치하면_5등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 11, 12, 13));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 45);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.FIFTH_PRIZE);
+    }
+
+    @Test
+    public void 숫자가_3개_일치_보너스_숫자_1개_일치하면_5등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 11, 12, 13));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 4);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.FIFTH_PRIZE);
+    }
+
+    @Test
+    public void 숫자가_4개_일치_보너스_숫자_0개_일치하면_4등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 4, 12, 13));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 45);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.FOURTH_PRIZE);
+    }
+
+    @Test
+    public void 숫자가_4개_일치_보너스_숫자_1개_일치하면_4등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 4, 12, 13));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 5);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.FOURTH_PRIZE);
+    }
+
+    @Test
+    public void 숫자가_5개_일치_보너스_숫자_0개_일치하면_3등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 4, 5, 13));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 45);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.THIRD_PRIZE);
+    }
+
+    @Test
+    public void 숫자가_5개_일치_보너스_숫자_1개_일치하면_2등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 4, 5, 13));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 6);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.SECOND_PRIZE);
+    }
+
+    @Test
+    public void 숫자가_6개_일치_보너스_숫자_0개_일치하면_1등을_반환한다() throws Exception {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(List.of(1, 2, 3, 4, 5, 6));
+        LottoBonusNumber lottoBonusNumber = new LottoBonusNumber(lottoWinningNumber, 45);
+
+        // when
+        LottoPrize prize = lotto.prize(lottoWinningNumber, lottoBonusNumber);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.FIRST_PRIZE);
+    }
 }

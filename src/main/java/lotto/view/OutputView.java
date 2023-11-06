9a683d6a6e73805583e25ed81d115ada927963ec
constant94-dev <constant94@naver.com>
2023-11-07 00:55:21 +0900

    feat(view) 발행한 로또 번호 출력 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
-    public void printToPrizeDetails(){
+    public void printToPrizeDetails() {
+    }
+
+    public void printToEarningRate() {
 
     }
 
-    public void printToEarningRate(){
-
+    public void printToBuyLotto(List<List<Integer>> buyLottoNumbers, int buyCount) {
+        System.out.println("\n" + buyCount + "개를 구매했습니다.");
+        for (List<Integer> lottoNumbers : buyLottoNumbers) {
+            System.out.println(lottoNumbers);
+        }
     }
 }

c81740e22f79696450ed2f9e613f64a53bcfbc99
euniiiii <hee9920@naver.com>
2023-11-06 16:19:02 +0900

    feat: 구입한 로또의 당첨 등수를 저장하는 기능

--- src/main/java/lotto/domain/LottoOffice.java
+++ src/main/java/lotto/domain/LottoOffice.java
@@ -8,6 +8,33 @@ public class LottoOffice {
 
     private static final int COUNT_ZERO = 0;
     private static final int COUNT_ONE = 1;
+    private static final int LEAST_WINNING_COUNT = 3;
+    private static final double PERCENT = 100;
+
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+    private final Map<LottoRank, Integer> result = new HashMap<>();
+
+    public LottoOffice(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+        initSetting();
+    }
+
+    public Map<LottoRank, Integer> getResult(List<Lotto> lottoList) {
+        for (Lotto lotto : lottoList) {
+            int rightNumber = compareNumbers(lotto.getNumbers(), winningNumbers);
+            int rightBonus = compareBonusNumber(winningNumbers, bonusNumber);
+            saveResult(rightNumber, rightBonus);
+        }
+        return result;
+    }
+
+    private void saveResult(int rightNumber, int bonusNumber) {
+        if (rightNumber < LEAST_WINNING_COUNT) return;
+        LottoRank rank = LottoRank.findRank(rightNumber, bonusNumber);
+        result.put(rank, result.get(rank) + COUNT_ONE);
+    }
 
     private int compareNumbers(List<Integer> lottoNumber, List<Integer> winningNumbers) {
         int cnt = 0;
@@ -22,4 +49,9 @@ public class LottoOffice {
         return COUNT_ZERO;
     }
 
+    private void initSetting() {
+        for (LottoRank rank : LottoRank.values()) {
+            result.put(rank, 0);
+        }
+    }
 }
---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIFTH_PLACE(3, 0, 5000L, "5,000"),
+    FOURTH_PLACE(4, 0, 50000L, "50,000"),
+    THIRD_PLACE(5, 0, 1500000L, "1,500,000"),
+    SECOND_PLACE(5, 1, 30000000L, "30,000,000"),
+    FIRST_PLACE(6, 0,2000000000L, "2,000,000,000");
+
+    private final int rightCnt;
+    private final int bonusCnt;
+    private final long winningAmount;
+    private final String amountStr;
+
+    LottoRank(int rightCnt, int bonusCnt, long winningAmount, String amountStr) {
+        this.rightCnt = rightCnt;
+        this.bonusCnt = bonusCnt;
+        this.winningAmount = winningAmount;
+        this.amountStr = amountStr;
+    }
+
+    public static LottoRank findRank(int cnt, int bonus) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank.rightCnt == cnt && lottoRank.bonusCnt == bonus)
+                .findAny()
+                .orElseThrow();
+    }
+}

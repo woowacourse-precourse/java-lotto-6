dfaf02504a11915d6a9d44496abd42c7063aea7e
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 15:47:05 +0900

    refactor: Result 상금 부분 추상 메서드

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,28 +1,19 @@
 package lotto;
 
 public enum Result {
-    FIRST_PRIZE(6, false, 2_000_000_000),
-    SECOND_PRIZE(5, true,30_000_000),
-    THIRD_PRIZE(5, false,1_500_000),
-    FOURTH_PRIZE(4, false, 50_000),
-    FIFTH_PRIZE(3, false,5_000),
-    NO_PRIZE(0, false,0);
+    FIRST_PRIZE(6, false) {int prize() {return 2_000_000_000;}},
+    SECOND_PRIZE(5, true) {int prize() {return 30_000_000;}},
+    THIRD_PRIZE(5, false) {int prize() {return 1_500_000;}},
+    FOURTH_PRIZE(4, false) {int prize() {return 50_000;}},
+    FIFTH_PRIZE(3, false) {int prize() {return 5_000;}},
+    NO_PRIZE(0, false) {int prize() {return 0;}};
 
+    abstract int prize();
     private final int matchingNumbers;
-    private final int prizeAmount;
     private final boolean bonus;
 
-    Result(int matchingNumbers, boolean bonus, int prizeAmount) {
+    Result(int matchingNumbers, boolean bonus) {
         this.matchingNumbers = matchingNumbers;
         this.bonus = bonus;
-        this.prizeAmount = prizeAmount;
-    }
-
-    public int getMatchingNumbers() {
-        return matchingNumbers;
-    }
-
-    public int getPrizeAmount() {
-        return prizeAmount;
     }
 }

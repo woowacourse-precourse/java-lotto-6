69b982ca75a9dd0ab49421a91f234ba270026871
seona-moon <fhflwhwl5@gmail.com>
2023-11-06 15:09:54 +0900

    feat(LottoController): 로또 게임을 위한 기본적인 실행 구조 구현(흐름별 입출력 등)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import lotto.Controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
+        LottoController lottoController = new LottoController(new InputView(), new OutputView());
         lottoController.run();
     }
 }
--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,20 +1,45 @@
 package lotto.Controller;
 
-import java.util.function.IntBinaryOperator;
+import java.util.List;
+import lotto.model.LottoPurchaseManager;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoPurchaseManager lottoPurchaseManager = new LottoPurchaseManager();
+
+    public LottoController(InputView inputView,OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 
     public void run() {
         purchaseLotto();
         validateLotto();
     }
 
-    public void purchaseLotto() {
+    private void purchaseLotto() {
+        //구매할 로또 개수 받아와서 로또 객체 생성
+        outputView.promptForPurchaseAmount();
+        int purchaseAmount = inputView.readPurchaseAccount();
+        outputView.displayPurchaseAmount(purchaseAmount);
 
+
+        //생성한 로또 출력
+        outputView.displayPurchaseLotto();
     }
 
-    public void validateLotto() {
+    private void validateLotto() {
+        //사용자에게 당첨될 로또 번호 받아오기
+        outputView.promptForWinningNumbers();
+        List<Integer> winningNumbers = inputView.readWinningNumbers();
+        outputView.promptForBonusNumbers();
+        int bonusNumber = inputView.readBonusNumber();
 
+        //최종 결과 출력
+        outputView.displayWinningStatistics();
     }
 
 }

bf7ebe5ca32e0c92d0b8b607d84024fded9a6ed2
unknown <winternrabbit@gmail.com>
2023-11-06 18:04:58 +0900

    refactor: 변수명 간략화

--- src/main/java/lotto/Config.java
+++ src/main/java/lotto/Config.java
@@ -6,12 +6,14 @@ public class Config {
     final static int MIN_LOTTO_NUMBER = 1;
     final static int MAX_LOTTO_NUMBER = 45;
     final static int SIZE_OF_LOTTO = 6;
-    final static int INDEX_OF_MATCH_WINNING_NUMBER = 0;
-    final static int INDEX_OF_MATCH_BONUS_NUMBER = 1;
-    final static int FIRST_PRIZE_WINNING_NUMBER_MATCH = 6;
-    final static int SECOND_THIRD_PRIZE_WINNING_NUMBER_MATCH = 5;
-    final static int SECOND_PRIZE_BONUS_NUMBER_MATCH = 1;
-    final static int FOURTH_PRIZE_WINNING_NUMBER_MATCH = 4;
-    final static int FIFTH_PRIZE_WINNING_NUMBER_MATCH = 3;
+    final static int INDEX_WINNING_NUMBER = 0;
+    final static int INDEX_BONUS_NUMBER = 1;
+    final static int FIRST_PRIZE_WINNING_NUMBER = 6;
+    final static int SECOND_PRIZE_WINNING_NUMBER = 5;
+    final static int THIRD_PRIZE_WINNING_NUMBER = 5;
+    final static int SECOND_PRIZE_BONUS_NUMBER = 1;
+    final static int FOURTH_PRIZE_WINNING_NUMBER = 4;
+    final static int FIFTH_PRIZE_WINNING_NUMBER = 3;
     final static String FIRST_PRIZE_REWARD = "2,000,000,000";
+    final static String SECOND_PRIZE_REWARD = "30,000,000";
 }

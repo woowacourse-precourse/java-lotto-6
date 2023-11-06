d834f5f02e39af214c986992fc9c3cc515ef8dfd
gusgh075 <gusgh075@naver.com>
2023-11-06 10:39:16 +0900

    FEATURES(LottoView) : 구매비용 및 구매로또목록 출력

---
+++ src/main/java/lotto/LottoView.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class LottoView {
+
+    public int inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = Integer.parseInt(Console.readLine());
+        return money;
+    }
+
+    public void showLottos(LottoRepository lottoRepository){
+        List<Lotto> lottos = lottoRepository.getLottos();
+        lottos.forEach(lotto->System.out.println(lotto.getNumbers()));
+    }
+}

60bd0cd030f84e124a3109da013edec8cf8b46ee
Yeriimii <seojs0511@naver.com>
2023-11-06 13:12:14 +0900

    feat(InputView): 사용자로부터 당첨 번호를 쉼표(,)를 기준으로 입력받을 수 있다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,7 +2,13 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+    private static final String SEPARATOR = ",";
+
     private enum InputMessage {
         REQUEST_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
         REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
@@ -20,5 +26,15 @@ public class InputView {
         String userInputNumber = Console.readLine();
 
         return Integer.parseInt(userInputNumber);
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        System.out.println(InputMessage.REQUEST_WINNING_NUMBERS.message);
+        String userInputBeforeSplit = Console.readLine();
+        List<String> userInputNumbers = Arrays.asList(userInputBeforeSplit.split(SEPARATOR));
+
+        return userInputNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

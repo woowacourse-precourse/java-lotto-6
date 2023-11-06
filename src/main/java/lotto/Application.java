345c03a007e5e149c5784abd1422d402d2172f37
je488 <pje488@naver.com>
2023-11-06 15:11:43 +0900

    feat: 발행한 로또 수량 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,23 +3,32 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void lottoMoneyInput() {
-        String userInput = Console.readLine();
-        int lottoMoney = Integer.parseInt(userInput);
-        if (lottoMoney % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.");
+    static int lottoMoney = 0;
+    public static boolean lottoMoneyInput() {
+        try {
+            String userInput = Console.readLine();
+            lottoMoney = Integer.parseInt(userInput);
+            if (lottoMoney % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.");
+            }
+            return false;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
         }
     }
     public static void main(String[] args) {
         // 로또 구입 금액 입력받고 예외처리
-        while(true) {
-            try {
-                System.out.println("구입금액을 입력해 주세요.");
-                lottoMoneyInput();
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        boolean flag = true;
+        while(flag) {
+            System.out.println("구입금액을 입력해 주세요.");
+            flag = lottoMoneyInput();
         }
+        System.out.println();
+
+        // 발행한 로또 수량 출력
+        int lottoAmount = lottoMoney / 1000;
+        System.out.println(lottoAmount + "개를 구매했습니다.");
+
     }
 }

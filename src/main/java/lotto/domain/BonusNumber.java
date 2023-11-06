8b2457958330d071a8efcb6ce2b2e2c5026d806e
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 22:28:33 +0900

    refactor: BonusNumber 메소드 분리 및 리팩토링

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -5,22 +5,26 @@ import lotto.utils.Constants;
 import lotto.utils.ErrorMessage;
 
 public class BonusNumber {
-    private int bonusNumber;
+    private final int bonusNumber;
 
     public BonusNumber(int inputBonusNumber, List<Integer> winningNumbers) {
         validateBonusNumber(inputBonusNumber, winningNumbers);
         this.bonusNumber = inputBonusNumber;
     }
 
-    public void validateBonusNumber(int inputBonusNumber, List<Integer> winningNumbers) {
+    private void validateBonusNumber(int inputBonusNumber, List<Integer> winningNumbers) {
         validateBonusNumberRange(inputBonusNumber);
         validateBonusNumberDuplicationWithWinningNumbers(inputBonusNumber, winningNumbers);
     }
 
     private void validateBonusNumberRange(int inputBonusNumber) {
-        if (inputBonusNumber < Constants.LOTTO_MIN_NUMBER || inputBonusNumber > Constants.LOTTO_MAX_NUMBER) {
+        if (!isWithinRange(inputBonusNumber)) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_RANGE_INVALID_ERROR.getMessage());
         }
+    }
+
+    private boolean isWithinRange(int inputBonusNumber) {
+        return inputBonusNumber >= Constants.LOTTO_MIN_NUMBER && inputBonusNumber <= Constants.LOTTO_MAX_NUMBER;
     }
 
     private void validateBonusNumberDuplicationWithWinningNumbers(int inputBonusNumber, List<Integer> winningNumbers) {

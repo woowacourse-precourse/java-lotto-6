27394c31fc795255c7a0e1163986e4c136f1b709
Genius-PAK <pyj789123@gmail.com>
2023-11-06 19:52:34 +0900

    feat(LottoManage): 로또 데이터 전체를 관리하는 매니저 클래스 추가

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -1,38 +1,47 @@
 package lotto.model;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import lotto.utils.Message;
+import lotto.utils.GameConstants;
 
 public class LottoManager {
     private ArrayList<Lotto> lottos;
-    private Map<Integer, Integer> winningNumbers;
-    private WinningStatistics winningStatistics;
+    private WinningStatistics statistics;
+    private WinningNumbers winningNumbers;
 
     public LottoManager() {
         lottos = new ArrayList<>();
     }
 
-    public void issueLottos(int lottoCount) {
-        
+    public void issueLottos(int purchaseAmount) {
+        statistics = new WinningStatistics(purchaseAmount);
+
+        int lottoCount = purchaseAmount / GameConstants.UNIT_PURCHASE_PRICE;
+
+        while (lottoCount != 0) {
+            List<Integer> numbers = pickUniqueNumbersInRange(GameConstants.MIN_LOTTO_NUMBER,
+                    GameConstants.MAX_LOTTO_NUMBER, GameConstants.LOTTO_NUMBER_COUNT);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+            lottoCount--;
+        }
     }
 
-    public void setWinningNumbers(Map<Integer, Integer> winningNumbers) {
-        this.winningNumbers = winningNumbers;
+    public void setWinningNumbers(Map<Integer, Integer> sequencedNumbers, int bonusNumber) {
+        this.winningNumbers = new WinningNumbers(sequencedNumbers, bonusNumber);
     }
 
     public Iterator<Lotto> getLottoListIterator() {
         return lottos.iterator();
     }
 
-    public void isDuplicatedBonusNumber(int bonusNumber) {
-        if (winningNumbers.containsKey(bonusNumber)) {
-            throw new IllegalArgumentException(Message.DUPLICATED_NUMBER_EXCEPTION_MESSAGE);
-        }
-    }
-
-    public WinningStatistics getWinningStatistics() {
-        return winningStatistics;
+    public String getStatistics() {
+        Iterator<Lotto> lottoIterator = lottos.iterator();
+        statistics.calculateStatistics(winningNumbers, lottoIterator);
+        return statistics.toString();
     }
 }

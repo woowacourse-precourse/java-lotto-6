223f33383702e09e767eeff1418763a182b7f1d4
SeungGwa123 <skwan123@naver.com>
2023-11-07 06:28:10 +0900

    Fix: Fix calculateRevenue
    출력 형식 수정

--- src/main/java/Money/Money.java
+++ src/main/java/Money/Money.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class Money {
     public void calculateRevenue(int lottoBuy, long correctMoney) {
-        double revenue = (correctMoney / lottoBuy) * 100;
+        double revenue = ((double)correctMoney / (double)lottoBuy) * 100;
         revenue = Math.round(revenue * 100.0) / 100.0;
-        DecimalFormat formatter = new DecimalFormat("#,###.###");
+        DecimalFormat formatter = new DecimalFormat("#,##0.0#");
         String totalRevenue = formatter.format(revenue);
         System.out.println("총 수익률은 " + totalRevenue + "%입니다.");
     }

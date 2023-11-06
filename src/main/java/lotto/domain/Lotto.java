c2863127a2f2ac590cbf35109268fd821a87c751
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-06 14:48:21 +0900

    feat: Lotto 클래스 함수 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,19 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.util.LottoVO;
+import lotto.util.UiVO;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDistinctNumbers(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
@@ -18,7 +24,26 @@ public class Lotto {
         }
     }
 
+    private void validateDistinctNumbers(List<Integer> numbers) {
+
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+
+        if (distinctNumbers.size() != LottoVO.getLottoNumberCount()) {
+            throw new IllegalArgumentException(UiVO.getWinningNumberCountException());
+        }
+    }
+
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    public int countSameNumbers(List<Integer> winningNumbers) {
+        List<Integer> tmp = new ArrayList<>(this.numbers);
+        tmp.retainAll(winningNumbers);
+        return tmp.size();
+    }
+
+    public boolean containsBonusNumber(int bonusNumber) {
+        return this.numbers.contains(bonusNumber);
     }
 }

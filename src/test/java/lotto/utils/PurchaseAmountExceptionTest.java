ef5762bcb47ddfec610821f2f83ff6c94005b934
honeydanji <tjdwls3680@naver.com>
2023-11-06 14:57:38 +0900

    TEST : 구입금액에 대한 예외 처리 테스트 코드 작성
    // 주어진 상황에서 예외가 잘 발생하는 지 체크
    - 숫자가 아닌 경우
    - 천원 단위로 나누어 떨어지지 않는 경우

---
+++ src/test/java/lotto/utils/PurchaseAmountExceptionTest.java
@@ -0,0 +1,22 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PurchaseAmountExceptionTest{
+    PurchaseAmountException purchaseAmountException = new PurchaseAmountException();
+
+    @Test
+    void 숫자가_아닌_값을_입력한_경우() {
+        assertThrows(IllegalArgumentException.class, () ->
+                        purchaseAmountException.notNullNumeric("8"), "[ERROR]숫자를 입력하세요.");
+    }
+
+    @Test
+    void 숫자가_1000_단위로_나누어_떨어지지_않을_경우 (){
+        assertThrows(IllegalArgumentException.class, () ->
+                purchaseAmountException.notDivisible("1001"), "[ERROR]1000으로 나누어 떨어지지 않습니다.");
+    }
+
+}

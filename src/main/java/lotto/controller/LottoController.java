7fec386e54759dbac5df2a481948898e07190aff
Seyeon <wjdtpdus828@naver.com>
2023-11-06 22:57:52 +0900

    add(LottoController): Lotto 클래스 리스트를 저장하는 코드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,16 +1,23 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 import lotto.utils.Parser;
 import lotto.view.LottoView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
     private PurchaseController purchaseController = new PurchaseController();
     private LottoView lottoView = new LottoView();
+    private InitLottoController initLottoController = new InitLottoController();
 
     private int ticketNumber;
+    private List<Lotto> lotts  = new ArrayList<>();
     public void startLotto(){
         ticketNumber = purchaseController.runPurchaseAmount();
         lottoView.outputPurchaseAmountView(ticketNumber);
+        lotts = initLottoController.runInitLotto(ticketNumber);
     }
 }

a32d49dae08b158371049207cdab5a3aad181d0e
sunwooiljoon <wnsdl1659@naver.com>
2023-11-06 21:24:20 +0900

    test : MoneyToBuyTest 추가
    살 수 있는 로또 개수 테스트

--- src/test/java/lotto/model/MoneyToBuyTest.java
+++ src/test/java/lotto/model/MoneyToBuyTest.java
@@ -1,19 +1,21 @@
-package lotto;
+package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import lotto.model.MoneyToBuy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MoneyToBuyTest {
 
+    // Error case
     @Test
     @DisplayName("금액이 0이하 에러 발생1")
     void createMoneyToBuyByUnderValue_1() {
         assertThatThrownBy(() -> new MoneyToBuy(0))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
     @Test
     @DisplayName("금액이 0이하 에러 발생2")
     void createMoneyToBuyByUnderValue_2() {
@@ -33,5 +35,15 @@ public class MoneyToBuyTest {
     void createMoneyToBuyByWrongValue_2() {
         assertThatThrownBy(() -> new MoneyToBuy(123))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    // Normal case
+
+    @Test
+    @DisplayName("금액을 1000으로 나눈 값을 반환")
+    void getLottosNumberTest() {
+        MoneyToBuy moneyToBuy = new MoneyToBuy(12000);
+
+        assertThat(moneyToBuy.getLottosNumber()).isEqualTo(12);
     }
 }

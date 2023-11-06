324bce849bf0f75affbe209dac421ce5eadf68ea
김효준 <khyojun1104@gmail.com>
2023-11-07 00:05:07 +0900

    test(lotto): 현재 로또 티켓이 5개 (2,3 등인 상황) 에서 보너스 번호 처리 하는 법 테스트 추가!

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,55 +1,15 @@
 package lotto.service;
 
 import static lotto.constant.NumberConstant.SAME_COUNT_FIVE;
-import static lotto.constant.NumberConstant.ZERO;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import java.util.stream.Stream;
-import lotto.domain.LottoTicket;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoServiceTest {
 
-    static class LottoTicket{
-    private final List<Integer> numbers;
 
-    private int sameCount;
-
-    private boolean bonus;
-
-    public LottoTicket(List<Integer> numbers, int sameCount, boolean bonus) {
-        this.numbers = numbers;
-        this.sameCount = ZERO.getNumber();
-        this.bonus = bonus;
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    public void addSameCount() {
-        this.sameCount++;
-    }
-
-    public int getSameCount() {
-        return sameCount;
-    }
-
-    public boolean isBonus() {
-        return bonus;
-    }
-
-    public void hasBonus() {
-        this.bonus = true;
-    }
-
-    }
 
 
 
@@ -68,8 +28,56 @@ class LottoServiceTest {
     }
 
 
+    @Test
+    @DisplayName("현재 로또 티켓이 5개 (2,3 등인 상황) 에서 보너스 번호 처리 하는 법 테스트")
+    void nowLottoTicketHasBonusNumber(){
+        //given
+        LottoTicket lottoTicket = new LottoTicket(List.of(1,2,3,4,5,6),5,false);
+        int bonusNumber = 3;
+
+        //when
+        nowLottoTicketHasBonusNumber(lottoTicket, bonusNumber);
+
+        //then
+        Assertions.assertThat(lottoTicket.isBonus()).isEqualTo(true);
+    }
 
 
+
+    static class LottoTicket{
+        private final List<Integer> numbers;
+
+        private int sameCount;
+
+        private boolean bonus;
+
+        public LottoTicket(List<Integer> numbers, int sameCount, boolean bonus) {
+            this.numbers = numbers;
+            this.sameCount = sameCount;
+            this.bonus = bonus;
+        }
+
+        public List<Integer> getNumbers() {
+            return numbers;
+        }
+
+        public void addSameCount() {
+            this.sameCount++;
+        }
+
+        public int getSameCount() {
+            return sameCount;
+        }
+
+        public boolean isBonus() {
+            return bonus;
+        }
+
+        public void hasBonus() {
+            this.bonus = true;
+        }
+
+    }
 
     private void nowLottoTicketContainWinNumber(LottoTicket lottoTicket,
         Integer lottoWinNumber) {

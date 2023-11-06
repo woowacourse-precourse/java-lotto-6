019215b7233c8284436fa0ddd255fa790d85dd8d
bamm2 <xxknu88@naver.com>
2023-11-06 15:16:00 +0900

    "feat(InputValidator): 숫자 외 문자입력 예외처리 기능"

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import static lotto.exception.ExceptionMessage.NOT_NUMBER_EXCEPTION_MESSAGE;
 import static lotto.exception.ExceptionMessage.NULL_POINTER_EXCEPTION_MESSAGE;
 
 import lotto.exception.LottoApplicationException;
@@ -9,6 +10,12 @@ public class InputValidator {
     public void validateNotNull(String input){
         if(input == null || input.isEmpty() ){
             throw new LottoApplicationException(NULL_POINTER_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public void validateNumber(String input){
+        if(!input.chars().allMatch(Character::isDigit)){
+            throw new LottoApplicationException(NOT_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 

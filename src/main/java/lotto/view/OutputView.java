4ac0eae3230c17183d3fa45ebb552aafbd1b748f
SeoYoung <seoyoung7623@naver.com>
2023-11-07 01:38:06 +0900

    feat: 구매한 로또 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import lotto.model.Number;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OutputView {
+    public void printBoughtLotto(int count, List<Number> buyedLotto){
+        System.out.println(count + "개를 구매했습니다.");
+        for(Number number:buyedLotto){
+            System.out.println(number.getNumberList());
+        }
+    }
+
 }

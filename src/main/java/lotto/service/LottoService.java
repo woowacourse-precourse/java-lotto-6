6d5690262cc93eaceb89fd08789b4371609d42f3
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-06 23:57:20 +0900

    feat(LottoService) : 숫자가 아닌 값을 입력하는 경우 예외처리
     isRestOfLottoPurchaseAmountDivideBy1000NotZero -> PurchaseAmountNOTDivideBy1000 : 메서드 이름 변경
     PurchaseAmountNotDigit : 메서드 생성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,6 +5,7 @@ public class LottoService {
     private static final int LOTTO_TICKET_PRICE = 1000;
     private static final String FIRST_ERROR_MESSAGE = "[ERROR] ";
     private static final String PURCHASE_AMOUNT_DIVIDE_EXCEPTION_MESSAGE = "로또 구입 금액은 1,000원으로 나누어 떨어져야합니다.";
+    private static final String PURCHASE_AMOUNT_NOT_DIGIT_EXCEPTION_MESSAGE = "로또 구입 금액은 숫자여야합니다.";
 
     public int calculateNumberOfLottoTickets(String purchaseAmount) {
         validateLottoPurchaseAmount(purchaseAmount);
@@ -13,14 +14,27 @@ public class LottoService {
 
     public void validateLottoPurchaseAmount(String lottoPurchaseAmount) {
 
-        if (isRestOfLottoPurchaseAmountDivideBy1000NotZero(lottoPurchaseAmount)) {
+        if (PurchaseAmountNOTDivideBy1000(lottoPurchaseAmount)) {
             throw new IllegalArgumentException(FIRST_ERROR_MESSAGE + PURCHASE_AMOUNT_DIVIDE_EXCEPTION_MESSAGE);
+        }
+
+        if (PurchaseAmountNotDigit(lottoPurchaseAmount)) {
+            throw new IllegalArgumentException(FIRST_ERROR_MESSAGE + PURCHASE_AMOUNT_NOT_DIGIT_EXCEPTION_MESSAGE);
         }
     }
 
-    private boolean isRestOfLottoPurchaseAmountDivideBy1000NotZero(String lottoPurchaseAmount) {
+    private boolean PurchaseAmountNOTDivideBy1000(String lottoPurchaseAmount) {
         int amount = Integer.parseInt(lottoPurchaseAmount);
         return amount % LOTTO_TICKET_PRICE != 0;
+    }
+
+    private boolean PurchaseAmountNotDigit(String lottoPurchaseAmount) {
+        try {
+            Integer.parseInt(lottoPurchaseAmount);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
     }
 
 }

4dbd50c133cd5ffb5109a50cfb7358a6e68eb3b0
U-keun Song <sukeun319@gmail.com>
2023-11-07 06:34:55 +0900

    Refactor : ViewMessage 열거형 클래스 생성
    메시지를 모아두는 클래스를 생성하고, 그 클래스로부터 메시지를 받아서 사용하도록 코드를 수정했다.

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,18 +1,22 @@
 package lotto.view;
 
+import static lotto.view.ViewMessage.REQUEST_BONUS_NUMBER;
+import static lotto.view.ViewMessage.REQUEST_INPUT_MONEY;
+import static lotto.view.ViewMessage.REQUEST_WINNER_NUMBERS;
+
 public class LottoInputView implements View {
     LottoInputView() {
     }
 
     public void requestMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(REQUEST_INPUT_MONEY.getMessage());
     }
 
     public void requestWinnerNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(REQUEST_WINNER_NUMBERS.getMessage());
     }
 
     public void requestBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
+        System.out.println(REQUEST_BONUS_NUMBER.getMessage());
     }
 }
--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -1,11 +1,34 @@
 package lotto.view;
 
+import static lotto.view.ViewMessage.BOUGHT_LOTTO;
+import static lotto.view.ViewMessage.PROFIT_RATE;
+import static lotto.view.ViewMessage.WINNER_RESULT;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lotto.util.constants.Reward;
+
 public class LottoOutputView implements View {
     LottoOutputView() {
     }
 
-    public void printPublishedLottos(Integer count, String lottoList) {
-        System.out.printf("\n%d개를 구매했습니다.\n", count);
-        System.out.println(lottoList);
+    public void printPublishedLotto(Integer count, String lottoList) {
+        System.out.println(BOUGHT_LOTTO.getMessage(count, lottoList));
+    }
+
+    public void printWinningResult(Map<Integer, Integer> rankCount) {
+        StringBuilder print = new StringBuilder();
+        print.append(WINNER_RESULT.getMessage());
+        String result = Arrays.stream(Reward.values())
+                .map(reward -> reward.getMessage(rankCount.getOrDefault(reward.getRank(), 0)))
+                .collect(Collectors.joining("\n"));
+        print.append(result);
+        print.append("\n");
+        System.out.println(print);
+    }
+
+    public void printProfitRate(Double profitRate) {
+        System.out.print(PROFIT_RATE.getMessage(profitRate));
     }
 }

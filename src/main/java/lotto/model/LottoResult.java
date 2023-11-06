49ced7ce48a64bfca6d4df2288e457f009ae887a
heesane <tees3359@gmail.com>
2023-11-06 21:28:30 +0900

    fix : 멤버 변수 추가로 인한 생성자 수정

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -7,10 +7,20 @@ public class LottoResult {
     private Lottos lottos;
     private Lotto winningLotto;
     private Integer bonusNumber;
+    private Integer countOf3;
+    private Integer countOf4;
+    private Integer countOf5;
+    private Integer countOf5AndBonus;
+    private Integer countOf6;
     public LottoResult(Lottos lottos, Lotto winningLotto, Integer bonusNumber) {
         this.lottos = lottos;
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+        this.countOf3 = 0;
+        this.countOf4 = 0;
+        this.countOf5 = 0;
+        this.countOf5AndBonus = 0;
+        this.countOf6 = 0;
     }
     public void countingMatchCount() {
         for(LottoInfo lottoInfo : this.lottos.getLottos()) {

a06c2c5a141295647f665935b4db9e41d698ee93
현 <sohyun95277@gmail.com>
2023-11-06 21:51:21 +0900

    test(LottoTest): 단위 테스트 작성
    숫자가 포함되어 있다면 true를 반환하는 기능,
    다른 lotto와 비교하여 포함한 숫자의 개수를 반환하는 기능

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
 import static lotto.constant.message.ErrorMessage.INVALID_NUMBER_RANGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import lotto.domain.Lotto;
@@ -26,10 +29,30 @@ class LottoTest {
 
     @DisplayName("로또 번호에 범위를 넘어간 숫자가 있으면 예외 발생")
     @Test
-    void createOverRangeNumber() {
+    void createLottoOverRangeNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 66, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_NUMBER_RANGE.getMessage());
+    }
+
+    @DisplayName("숫자가 포함되어 있다면 true 반환")
+    @Test
+    void isContainNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int containNumber = 3;
+        int notContain = 33;
+
+        assertTrue(lotto.isContain(containNumber));
+        assertFalse(lotto.isContain(notContain));
+    }
+
+    @DisplayName("포함된 숫자의 개수가 3개일 경우 반환 테스트")
+    @Test
+    void matchCount() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto otherLotto = new Lotto(List.of(3, 4, 5, 23, 33, 42));
+
+        assertThat(lotto.matchCount(otherLotto)).isEqualTo(3);
     }
 
 }

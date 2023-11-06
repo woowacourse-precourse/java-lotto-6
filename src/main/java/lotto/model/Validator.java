3e3a4b746962d37937d574c804b80cff19065946
cookingTorch <jhw7181@naver.com>
2023-11-06 16:30:10 +0900

    refactor(Validator): consolidate duplicate validation logic
    Refactor the validation methods to eliminate duplicated code.
    Extract common validation checks into reusable private methods.
    This includes numeric checks and range validations which are now centralized
    to optimize the validation process and reduce redundancy.

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -4,57 +4,43 @@ public class Validator {
     private final String REGULAR_NUMBER = "\\d+";
 
     public void validateCost(String cost, Integer lottoPrice) {
-        isCostNumeric(cost);
-        isCostThousands(cost, lottoPrice);
-    }
-
-    private void isCostNumeric(String cost) {
-        if (!cost.matches(REGULAR_NUMBER)) {
+        if (notNumeric(cost)) {
+            throw new IllegalArgumentException();
+        }
+        if (notThousands(cost, lottoPrice)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void isCostThousands(String cost, Integer lottoPrice) {
-        if (Integer.parseInt(cost) % lottoPrice != 0) {
-            throw new IllegalArgumentException();
-        }
+    private boolean notThousands(String cost, Integer lottoPrice) {
+        return (Integer.parseInt(cost) % lottoPrice == 0);
     }
 
     public void validateAnswer(String number, Integer start, Integer end) {
-        isAnswerNumeric(number);
-        isAnswerInRange(number, start, end);
-    }
-
-    private void isAnswerNumeric(String number) {
-        if (!number.matches(REGULAR_NUMBER)) {
+        if (notNumeric(number)) {
+            throw new IllegalArgumentException();
+        }
+        if (outOfRange(number, start, end)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void isAnswerInRange(String bonus, Integer start, Integer end) {
-        Integer number = Integer.parseInt(bonus);
-
-        if (number <= start || end <= number) {
+    public void validateBonus(String number, Integer start, Integer end) {
+        if (notNumeric(number)) {
+            throw new IllegalArgumentException();
+        }
+        if (outOfRange(number, start, end)) {
             throw new IllegalArgumentException();
         }
     }
 
-    public void validateBonus(String bonus, Integer start, Integer end) {
-        isBonusNumeric(bonus);
-        isBonusInRange(bonus, start, end);
+    private boolean notNumeric(String number) {
+        return !number.matches(REGULAR_NUMBER);
     }
 
-    private void isBonusNumeric(String bonus) {
-        if (!bonus.matches(REGULAR_NUMBER)) {
-            throw new IllegalArgumentException();
-        }
-    }
+    private boolean outOfRange(String number, Integer start, Integer end) {
+        Integer num = Integer.parseInt(number);
 
-    private void isBonusInRange(String bonus, Integer start, Integer end) {
-        Integer number = Integer.parseInt(bonus);
-
-        if (number <= start || end <= number) {
-            throw new IllegalArgumentException();
-        }
+        return (num <= start || end <= num);
     }
 }

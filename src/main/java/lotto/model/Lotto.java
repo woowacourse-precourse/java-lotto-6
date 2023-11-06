681ce30ff0751762a32c64e76d2fd0aa9ba80947
hoyeon <highoption@naver.com>
2023-11-06 22:41:08 +0900

    feat(Lotto) : 로또 번호를 검증하는 isCorrectRange 메서드 생성
    - 로또 번호가 1 ~ 45 사이인지 검증하는 함수

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -13,13 +13,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        isCorrectRange(numbers);
         isCorrectSize(numbers);
     }
 
     private static void isCorrectSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 넘버는 6개여야 합니다.");
         }
     }
 
+    private static void isCorrectRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        });
+    }
 }

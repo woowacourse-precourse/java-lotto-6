3c4298355fcb2db6a123c217324b6cf31898f77f
yang <hjyang@epience.com>
2023-11-06 17:17:38 +0900

    🎯feat(domain/LottoStorage) : 로또 개수 validation 기능 구현

---
+++ src/main/java/lotto/domain/LottoStorage.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.Lotto;
+public class LottoStorage {
+    Purchase purchase;
+    List<Lotto> lottos;
+
+    public LottoStorage(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+    // 로또 List 갯수 validation
+    public void validateLottoQuantity() {
+        if (lottos.size() != purchase.getQuantity()) {
+            throw new IllegalArgumentException("구매한 로또의 개수가 일치하지 않습니다.");
+        }
+    }
+
+    public  void addLotto(List<Integer> lotto) {
+        lottos.add((Lotto) lotto);
+    }
+}

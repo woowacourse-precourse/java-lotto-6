428c18d1fbe24078c213b2070368b2598edcc052
King's meow <mingee641@gmail.com>
2023-11-06 17:27:50 +0900

    feat: 랜덤 생성된 로또번호 출력을 위한 printLottos() 메서드 추가

--- src/main/java/lotto/view/PrintView.java
+++ src/main/java/lotto/view/PrintView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.User;
 
 public class PrintView {
 
@@ -13,6 +16,29 @@ public class PrintView {
 
     public static void inputBonusNumberView() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printLottos(User user) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append(user.getCount()).append("개를 구매했습니다.\n");
+
+        for (Lotto lotto : user.getLottos()) {
+            List<Integer> numbers = lotto.getNumbers();
+            sb.append("[");
+
+            for (int i = 0; i < numbers.size(); i++) {
+                sb.append(numbers.get(i));
+
+                if (i < numbers.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+
+            sb.append("]\n");
+        }
+
+        System.out.println(sb.toString());
     }
 
 

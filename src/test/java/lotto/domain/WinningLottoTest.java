e8b2e421cc3ce50879686c2f7b13c890063fc70d
seokmyungham <hellotjraud@gmail.com>
2023-11-06 12:36:50 +0900

    test(WinningLottoTest): 보너스 번호 테스트 코드 리팩토링

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,22 +17,25 @@ class WinningLottoTest {
         lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
     }
 
+    @DisplayName("보너스 번호와 당첨 번호가 중복되면 예외를 발생한다.")
     @Test
-    void 보너스_번호와_당첨_번호가_중복될_경우_예외_발생() {
+    void duplicateBonusAndWinningNumbers() {
         assertThatThrownBy(() -> new WinningLotto(lottoNumbers, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("보너스 번호와 당첨 번호는 중복될 수 없습니다.");
     }
 
+    @DisplayName("보너스 번호와 당첨 번호가 서로 다르면 정상 생성한다.")
     @Test
-    void 보너스_번호와_당첨_번호가_서로_다를_경우_예외_미발생() {
+    void noDuplicateBonusAndWinningNumbers() {
         assertThatNoException()
                 .isThrownBy(() -> new WinningLotto(lottoNumbers, 7));
     }
 
+    @DisplayName("보너스 번호가 1부터 45사이의 숫자가 아니라면 예외를 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {-3, 0, 46})
-    void 보너스_번호가_1부터_45사이가_아니라면_예외_발생(int bonusNumber) {
+    void bonusNumberNotInRange1To45(int bonusNumber) {
         assertThatThrownBy(() -> new WinningLotto(lottoNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 번호는 1부터 45사이의 숫자여야 합니다.");

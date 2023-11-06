cd9ccab2dbb4ecca257d3522fa19c699127c8236
kangyuseok <kingyuseok@naver.com>
2023-11-07 02:17:13 +0900

    Feat: 구매액에 따라 구매 개수 구하는 메서드 구현

--- src/main/java/lotto/service/Buyer.java
+++ src/main/java/lotto/service/Buyer.java
@@ -9,6 +9,7 @@ public class Buyer {
     //로또 서비스 같은 기능
     public static int buyLottoTickets(PurchaseAmount purchaseAmount){
         //금액을 쪼개서 몇개 로또를 살 수 있는지 리턴
-        return 0;
+        int lottoTicketCount = Integer.parseInt(purchaseAmount.getPurchaseAmount()) / 1000;
+        return lottoTicketCount;
     }
 }

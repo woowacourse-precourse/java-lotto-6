e5c68f87dc6ce15666d6f08c278a890302079bb9
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-06 09:10:20 +0900

    feat: OutputView 작성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,26 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+
+public class OutputView {
+
+    private final MessageResolver messageResolver;
+
+    public OutputView(MessageResolver messageResolver) {
+        this.messageResolver = messageResolver;
+    }
+
+    public void showPurchasedLottosInformation(List<Lotto> lottos) {
+        System.out.println(messageResolver.resolvePurchasedInformationMessage(lottos));
+    }
+
+    public void showResults(LottoResult result) {
+        System.out.println(messageResolver.resolveResultMessage(result));
+    }
+
+    public void showProfitRate(double profitRate) {
+        System.out.println(messageResolver.resolveProfitRateMessage(profitRate));
+    }
+}

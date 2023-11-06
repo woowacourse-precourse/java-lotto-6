5991e37f499693ae0d243eab793976d63c148f77
JayG-5 <jg55552758@gmail.com>
2023-11-07 00:19:18 +0900

    feat: winning 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,20 @@
 package lotto;
 
 import lotto.controller.Game;
+import lotto.domain.Winning;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        final Game game = new Game();
+        List<Winning> winnings = new ArrayList<>();
+        winnings.add(new Winning(3, false, 5000));
+        winnings.add(new Winning(4, false, 50000));
+        winnings.add(new Winning(5, false, 1500000));
+        winnings.add(new Winning(5, true, 30000000));
+        winnings.add(new Winning(6, false, 2000000000));
+        final Game game = new Game(winnings);
         game.start();
     }
 }
---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import lotto.view.Message;
+import lotto.view.View;
+
+public class Winning {
+    private final int match;
+    private final boolean bonus;
+    private final int prize;
+
+    private int ticketCount = 0;
+
+    public Winning(int match,boolean bonus, int prize) {
+        this.match = match;
+        this.bonus = bonus;
+        this.prize = prize;
+    }
+
+    public void setTicketCount(int ticketCount) {
+        this.ticketCount = ticketCount;
+    }
+    public int calculatePrize() {
+        return ticketCount * prize;
+    }
+
+    public void printResult() {
+        String bonusText = "";
+        if (bonus) {
+            bonusText = Message.RESPONSE_BONUS_TEXT.getMessage();
+        }
+        View.printMessageWithArgs(
+                Message.RESPONSE_MATCHED,
+                match,
+                bonusText,
+                Message.RESPONSE_PRIZE_FORMATTER.getMessage(prize),
+                ticketCount
+        );
+    }
+}

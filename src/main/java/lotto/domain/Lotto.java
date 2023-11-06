de271ee78db44b3da29df6239516185a1a5eb5ae
UDADDY <gustmd5715@gmail.com>
2023-11-06 16:40:00 +0900

    fix: 로또 안에 중복된 숫자 validate 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,10 +16,21 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != 6)
+            return true;
+        return false;
     }
 }

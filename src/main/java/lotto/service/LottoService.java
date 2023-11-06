15f979dbf4bc61ef7d8f6dfde6fc593503e79736
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 05:59:01 +0900

    feat(service.LottoService): 기능10. 로또를 발행하기 위한 6자리 숫자 조합을 생성한다
    generateLottoCombination 메서드는 Lotto 인스턴스를 생성하기 위해서 6개의 숫자로 이루어진 리스트를 반환하는 메서드 입니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,18 @@
 package lotto.service;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.List;
+
 public class LottoService {
+
+
+
+
+    public List<Integer> generateLottoCombination() {
+        List<Integer> list = pickUniqueNumbersInRange(1, 45, 6);
+        list.sort(null);
+        return list;
+    }
+
 }

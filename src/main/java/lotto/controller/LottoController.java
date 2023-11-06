4c5457040ba39381898ea482cb0ca73f9b29f081
Seyeon <wjdtpdus828@naver.com>
2023-11-06 21:05:41 +0900

    modify(LottoController): 로또를 관리하는 controller 클래스로 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,46 +2,11 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Parser;
-import lotto.service.validator.PurchaseAmount;
 import lotto.view.LottoView;
 
 public class LottoController {
-
-    private LottoView lottoView = new LottoView();
-    private PurchaseAmount purchaseAmount = new PurchaseAmount();
-    private Parser parser = new Parser();
-    private int purchaseAmountNum;
+    private PurchaseController purchaseController = new PurchaseController();
     public void startLotto(){
-        purchaseAmountNum = processPurchaseAmount();
-    }
-
-    public int processPurchaseAmount(){
-        String purchaseNum;
-        while(true){
-            try{
-                purchaseNum = getPurchaseAmount();
-                validatePurchaseAmount(purchaseNum);
-                break;
-            }catch(IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        return parser.stringToInt(purchaseNum);
-    }
-
-    public String getPurchaseAmount(){
-        String purchaseNum;
-
-        lottoView.purchaseAmountView();
-        purchaseNum = Console.readLine();
-
-        return purchaseNum;
-    }
-
-    public void validatePurchaseAmount(String input){
-        purchaseAmount.validateNotInteger(input);
-        purchaseAmount.validateUnderThousand(input);
-        purchaseAmount.validateNotRemainZero(input);
+        purchaseController.processPurchaseAmount();
     }
 }

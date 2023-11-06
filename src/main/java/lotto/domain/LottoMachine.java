7b832b708fe0e969b3baec045c893a3c1a792c67
5jeong <wjdgns4385@naver.com>
2023-11-07 00:45:35 +0900

    [Refactor] 배열 생성 초기화 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.util.NumberGenerator;
@@ -9,13 +10,18 @@ public class LottoMachine implements NumberGenerator {
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 45;
     private static final int NUMBER_OF_RANDOM_NUMBERS = 6;
+
     @Override
     public List<Integer> generator() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, NUMBER_OF_RANDOM_NUMBERS);
+        List<Integer> lottoNumbers = new ArrayList<Integer>(
+                Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE,
+                        NUMBER_OF_RANDOM_NUMBERS));
         ascendingSortLottoNumbers(lottoNumbers);
         return lottoNumbers;
     }
-    private void ascendingSortLottoNumbers(List<Integer> lottoNumbers){
+
+    private void ascendingSortLottoNumbers(List<Integer> lottoNumbers) {
         Collections.sort(lottoNumbers);
+
     }
 }

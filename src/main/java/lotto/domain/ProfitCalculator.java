10905a131eed38615b9b645282d15930020944c0
Woongbae Yun <devbelly@naver.com>
2023-11-06 19:02:05 +0900

    feat : 구입 갯수가 0이라면 수익률을 계산하지 못하는 예외 기능 추가

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -3,13 +3,21 @@ package lotto.domain;
 public class ProfitCalculator {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final String ZERO_DIVISION_ERROR_MESSAGE = "[ERROR] 로또를 구매해야 수익률을 계산할 수 있습니다.";
 
     private ProfitCalculator() {
     }
 
     public static String calculate(int priceSum, int lottoCount) {
+        validateLottoCount(lottoCount);
         double profit = (double) priceSum / (lottoCount * LOTTO_PRICE) * 100;
 
         return String.format("%.1f", profit);
+    }
+
+    private static void validateLottoCount(int lottoCount) {
+        if (lottoCount == 0) {
+            throw new IllegalArgumentException(ZERO_DIVISION_ERROR_MESSAGE);
+        }
     }
 }

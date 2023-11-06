abf6be72b0b0bd7f81380471fb2b90aa2d7c49e4
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 16:26:20 +0900

    test : 당첨번호와 보너스 번호를 검증하는 테스트 작성

--- src/test/java/lotto/model/JackpotTest.java
+++ src/test/java/lotto/model/JackpotTest.java
@@ -24,4 +24,43 @@ class JackpotTest {
                 .extracting("winningNumbers", "bonusNumber")
                 .contains(Arrays.asList(1, 2, 3, 4, 5, 6), 45);
     }
+
+    @DisplayName("당첨 번호가 6개로 이루어져있지 않으면 Exception 발생한다.")
+    @Test
+    void validateWinningNumbersSize() {
+        // given
+        String inputWinningNumbers = "1,2,3,4,5,6,7";
+        String inputBonusNumber = "45";
+
+        // when // then
+        assertThatThrownBy(() -> new Jackpot(inputWinningNumbers, inputBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개로 이루어져야 합니다.");
+    }
+
+    @DisplayName("당첨 번호가 숫자로 이루어져있지 않으면 Exception 발생한다.")
+    @Test
+    void validateNumericByWinningNumbers() {
+        // given
+        String inputWinningNumbers = "1,2,3,4,5,a";
+        String inputBonusNumber = "45";
+
+        // when // then
+        assertThatThrownBy(() -> new Jackpot(inputWinningNumbers, inputBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 숫자로만 이루어져야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 숫자가 아니라면 Exception 발생한다.")
+    @Test
+    void validateNumericByBonusNumber() {
+        // given
+        String inputWinningNumbers = "1,2,3,4,5,6";
+        String inputBonusNumber = "a";
+
+        // when // then
+        assertThatThrownBy(() -> new Jackpot(inputWinningNumbers, inputBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 숫자로만 이루어져야 합니다.");
+    }
 }

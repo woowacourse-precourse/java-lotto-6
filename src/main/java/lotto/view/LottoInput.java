424b7b0b35b05636d50cc58a2222f772b7366cec
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-07 01:28:40 +0900

    feat(LottoInput) : 사용자에게서 당첨 번호와 보너스 번호 입력받는 기능 구현

--- src/main/java/lotto/view/LottoInput.java
+++ src/main/java/lotto/view/LottoInput.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.WinningLotto;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,13 @@ public class LottoInput {
     public int lottoPurchaseInput(){
         String lottoPurchasePrice = Console.readLine();
         return Integer.parseInt(lottoPurchasePrice);
+    }
+
+    public WinningLotto winningLottoInput(){
+        List<Integer> lottoNumbers = parseByComma(Console.readLine()).stream()
+                .mapToInt(Integer::parseInt).boxed().toList();
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        return new WinningLotto(lottoNumbers, bonusNumber);
     }
 
     private List<String> parseByComma(String input){

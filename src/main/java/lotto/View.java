2bec56d8e1d6b759365df0447086ae7524c4b9ad
twinson37 <twinson37@hanyang.ac.kr>
2023-11-07 03:33:44 +0900

    Feat: 통계 출력

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -33,4 +33,18 @@ public class View {
 
         System.out.println(asKBonusNumber);
     }
+
+    void printWinStat(LottoGame lottoGame, User user){
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        System.out.println("3개 일치 (5,000원) - "+lottoGame.getRank(WinningConditionPrize.FIFTH)+"개");
+        System.out.println("4개 일치 (50,000원) - "+lottoGame.getRank(WinningConditionPrize.FOURTH)+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+lottoGame.getRank(WinningConditionPrize.THIRD)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+lottoGame.getRank(WinningConditionPrize.SECOND)+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+lottoGame.getRank(WinningConditionPrize.FIRST)+"개");
+        double earn = (Math.round(user.getTotalWinningAmount()/user.getAmountLotto())*10)/100.0;
+        System.out.println("총 수익률은 "+earn+"%입니다.");
+    }
 }

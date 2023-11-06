f7dfb8b23e044da26cc50a3dc7777242a9186938
Seyeon <wjdtpdus828@naver.com>
2023-11-06 23:00:14 +0900

    add(Views): String 상수인 당첨번호 입력 메세지와 보너스 번호 입력 메세지 추가

--- src/main/java/lotto/constant/Views.java
+++ src/main/java/lotto/constant/Views.java
@@ -2,7 +2,9 @@ package lotto.constant;
 
 public enum Views {
     INPUT_PURCHASE_AMOUT_MESSAGE("구입금액을 입력해 주세요 ."),
-    OUTPUT_PURCHASE_AMOUT_MESSAGE("개를 구매했습니다 .");
+    OUTPUT_PURCHASE_AMOUT_MESSAGE("개를 구매했습니다 ."),
+    INPUT_WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
 
 
     private final String message;

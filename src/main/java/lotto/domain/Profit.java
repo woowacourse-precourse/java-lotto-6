f59b480c187ba772f71d562cee3ef0acfe41ab64
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 00:24:00 +0900

    feat(Profit): 수익 더하기 기능 삭제 및 수익 계산 로직 구현

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -1,26 +1,18 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Profit {
-    private static final int MINIMUM_PROFIT = 0;
     private Integer amount;
 
     Profit() {
         this.amount = 0;
     }
 
-    void add(Integer profit) {
-        validate(profit);
-        this.amount += profit;
-    }
-
-    private void validate(Integer profit) {
-        if (isNotPositiveProfit(profit)) {
-            throw new IllegalArgumentException();
+    void calculateAmount(List<Rank> ranks) {
+        for (Rank rank : ranks) {
+            this.amount += rank.getPrizeAmount();
         }
-    }
-
-    private boolean isNotPositiveProfit(Integer profit) {
-        return profit < MINIMUM_PROFIT;
     }
 
     public Integer getAmount() {

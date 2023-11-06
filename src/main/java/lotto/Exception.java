a016431138c93ff358ebd15f5947e86fded303dd
KIVSH <kihongkim3@hotmail.com>
2023-11-06 22:15:13 +0900

    feat: add overLimitCount method in Exception class

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -16,4 +16,10 @@ public class Exception {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력 가능합니다.\n");
         }
     }
+
+    public static void overLimitCount(int inputSize, int limitCount) throws IllegalArgumentException {
+        if (inputSize > limitCount) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개만 입력 가능합니다.\n");
+        }
+    }
 }

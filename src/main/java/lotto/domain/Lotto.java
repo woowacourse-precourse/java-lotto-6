9e7143a25847f1a8a5643a197e2f5832cdaffb56
kimdoha <kkhhjj3393@gmail.com>
2023-11-06 19:13:08 +0900

    feat(Lotto): 당첨 번호가 올바르지 않을 경우, 1-45 사이의 수가 아닌 경우 오류 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,11 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.common.ErrorMessage;
+import lotto.util.LottoNumberValidator;
 
 public class Lotto {
-    private final Integer LOTTO_NUM_SIZE = 6;
-
+    private static final Integer LOTTO_NUM_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,11 +13,25 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto from() {
+        List<Integer> numbers = null;
+        return new Lotto(numbers);
+    }
+
     private void validate(List<Integer> numbers) {
+        validateLottoNumSize(numbers);
+        validateLottoInvalidNumber(numbers);
+    }
+
+    private void validateLottoNumSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUM_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUM_SIZE_ERROR.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoInvalidNumber(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> !LottoNumberValidator.validate(number))) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUM_RANGE_ERROR.getMessage());
+        }
+    }
 }

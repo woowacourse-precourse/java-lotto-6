fb740509baa23251a3bd77e84bb9b9395518ba02
shin5774 <babo5774@naver.com>
2023-11-06 22:37:51 +0900

    Feat: 모든 로또의 숫자들 문자열을 가져오는 기능 구현
    - Lotto에서 구현한 해당 로또번호 문자열을 가져오는 기능을 Lottos에서 stream을 통해 List로 가져옴.

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -7,16 +7,21 @@ import java.util.List;
 public class Lottos {
     private final List<Lotto> lottos;
 
-    public Lottos(List<Lotto> lottos){
-        this.lottos=lottos;
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
     }
 
-    public long getLottoAmount(){
+    public long getLottoAmount() {
         return lottos.size();
     }
 
-    public List<Lotto> getLottoTickets(){
+    public List<Lotto> getLottoTickets() {
         return Collections.unmodifiableList(lottos);
     }
 
+    public List<String> getAllLottoNumbersMessage() {
+        return lottos.stream()
+                .map(Lotto::getNumbersMessage)
+                .toList();
+    }
 }

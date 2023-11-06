38892c4f1bc2e53fd925014a139cb3e03d44fb88
HurDong <gjehdals456@gmail.com>
2023-11-06 20:52:07 +0900

    feat : Controller

--- src/main/java/lotto/View/LottoController.java
+++ src/main/java/lotto/View/LottoController.java
@@ -10,23 +10,70 @@ import lotto.Controller.LottoView;
 import lotto.Model.Lotto;
 
 public class LottoController {
+    private int purchaseAmount;
     private List<Lotto> lottoTicketes;
     private List<Integer> winningNumbers;
     private int bonusNumber;
+    private int[] rankCounts = new int[5]; // 1~5등까지 저장
 
     public void startGame() {
         try {
             getPrice();
             getWinningNumbers();
             getBonusNumber();
+            calculateResults();
+            calculateProfit();
         } catch (IllegalArgumentException e) {
             LottoView.printErrorMessage(e.getMessage());
         }
     }
 
+    private void calculateProfit() {
+        long totalPrize = 0;
+        totalPrize += rankCounts[0] * LottoConfig.FIFTH_PRIZE.getPrizeMoney();
+        totalPrize += rankCounts[1] * LottoConfig.SECOND_PRIZE.getPrizeMoney();
+        totalPrize += rankCounts[2] * LottoConfig.THIRD_PRIZE.getPrizeMoney();
+        totalPrize += rankCounts[3] * LottoConfig.FOURTH_PRIZE.getPrizeMoney();
+        totalPrize += rankCounts[4] * LottoConfig.FIFTH_PRIZE.getPrizeMoney();
+        double profit = ((double) totalPrize / purchaseAmount) * 100;
+        LottoView.printProfit(profit);
+    }
+
+    private void calculateResults() {
+        for (Lotto lotto : lottoTicketes) {
+            int matchCount = countMatch(lotto);
+            if (matchCount == LottoConfig.FIRST_PRIZE.getValue())
+                rankCounts[0]++;
+            if (matchCount == LottoConfig.SECOND_PRIZE.getValue() && lotto.getNumbers().contains(bonusNumber))
+                rankCounts[1]++;
+            if (matchCount == LottoConfig.THIRD_PRIZE.getValue() && !lotto.getNumbers().contains(bonusNumber))
+                rankCounts[2]++;
+            if (matchCount == LottoConfig.FOURTH_PRIZE.getValue())
+                rankCounts[3]++;
+            if (matchCount == LottoConfig.FIFTH_PRIZE.getValue())
+                rankCounts[4]++;
+        }
+        printRank();
+    }
+
+    private void printRank() {
+        LottoView.printResult();
+        LottoView.printRank(rankCounts);
+    }
+
+    private int countMatch(Lotto lotto) {
+        int count = 0;
+        for (int number : lotto.getNumbers()) {
+            if (winningNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     private void getPrice() {
         try {
-            int purchaseAmount = LottoView.promptForPurchaseAmount();
+            purchaseAmount = LottoView.promptForPurchaseAmount();
             validateAmount(purchaseAmount);
             lottoTicketes = purchaseLottoTickets(purchaseAmount);
             LottoView.printLottoTickets(lottoTicketes);

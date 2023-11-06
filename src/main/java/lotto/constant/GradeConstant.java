864e1877cc63c819acb457b3484026fc3011360e
Flash-KIM142 <topqr123q@gmail.com>
2023-11-06 22:48:02 +0900

    refactor: 등급 enum 클래스의 불필요한 속성 삭제
    - 몇 개 일치했는지의 값 불필요

--- src/main/java/lotto/constant/GradeConstant.java
+++ src/main/java/lotto/constant/GradeConstant.java
@@ -2,23 +2,17 @@ package lotto.constant;
 
 public enum GradeConstant {
 
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    NOTHING(0, 0);
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000),
+    NOTHING(0);
 
-    private final int hit;
     private final int prize;
 
-    GradeConstant(int hit, int prize) {
-        this.hit = hit;
+    GradeConstant(int prize) {
         this.prize = prize;
-    }
-
-    public int hit() {
-        return hit;
     }
 
     public int prize() {

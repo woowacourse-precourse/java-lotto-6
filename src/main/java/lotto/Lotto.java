ba2c70c5f26b34b2bf54e0326d10ddcee3814f33
ãJunChangM <jsjs1209@g.skku.edu>
2023-11-06 16:45:43 +0900

    Feat: match lotto number

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,12 @@
 package lotto;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static lotto.OrderEnum.CorrespondType;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -19,12 +24,60 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
 
-    public void calLottoResult(List<List<Integer>> lottoNumbers, int bonus){
 
-
-
-
-
+    public void calLottoResult(List<List<Integer>> lottoNumbers, int bonus) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        ArrayList<Integer> lottoResult = matchNumbers(lottoNumbers, bonus);
+        printResult(lottoResult);
     }
 
+    private ArrayList<Integer> matchNumbers(List<List<Integer>> lottoNumbers, int bonus) {
+        ArrayList<Integer> lottoResult = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0));
+        for (List<Integer> lottoNumber : lottoNumbers) {
+            int matchNum = matchNumber(lottoNumber);
+            CorrespondType result = matchResult(matchNum, bonus);
+            lottoResult.set(result.ordinal(), lottoResult.get(result.ordinal()) + 1);
+        }
+        return lottoResult;
+    }
+
+    private int matchNumber(List<Integer> lottoNumber) {
+        int matchNum = 0;
+        for (Integer num : lottoNumber) {
+            if (numbers.contains(num)) {
+                matchNum += 1;
+            }
+        }
+        return matchNum;
+    }
+
+    private CorrespondType matchResult(int matchNum, int bonus) {
+        if (matchNum == 6) {
+            return CorrespondType.FIRST;
+        }
+
+        if (matchNum == 5 && numbers.contains(bonus)) {
+            return CorrespondType.SECOND;
+        }
+
+        if (matchNum == 5) {
+            return CorrespondType.THIRD;
+        }
+
+        if (matchNum == 4) {
+            return CorrespondType.FOURTH;
+        }
+
+        if (matchNum == 3) {
+            return CorrespondType.FIFTH;
+        }
+
+        return CorrespondType.ZERO;
+    }
+
+
+    private void printResult (ArrayList<Integer> lottoResult){
+
+    }
 }
---
+++ src/main/java/lotto/OrderEnum.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class OrderEnum {
+
+    enum CorrespondType {
+        ZERO, FIFTH, FOURTH, THIRD, SECOND, FIRST,
+    }
+}

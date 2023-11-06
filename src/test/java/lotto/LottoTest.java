afe3f3ecca3e09867a68973c3a21caee1a87803c
donghoonyeom <dua5410773@naver.com>
2023-11-07 02:55:38 +0900

    test(LottoTest): 새로운 유효성 검사 테스트 케이스 추가
    `LottoTest` 클래스에 새로운 테스트 케이스를 추가했습니다. 이 테스트 케이스에서는 로또 번호의 유효성을 검사하는 여러 상황을 다루고 있습니다. 새로운 테스트는 로또 번호의 개수가 6개 초과, 중복된 숫자, 1부터 45까지의 숫자가 아닌 경우, 6개 미만의 숫자, 0이나 음수가 포함된 경우, 특수문자가 포함된 경우를 테스트합니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,37 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1부터 45까지의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoWithInvalidNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void createLottoWithLessThanSixNumbers() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 0이나 음수가 포함되면 예외가 발생한다.")
+    @Test
+    void createLottoWithNegativeOrZeroNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 특수문자가 포함되면 예외가 발생한다.")
+    @Test
+    void createLottoWithSpecialCharacters() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, +3)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }

eef60ea3388e014296547ed3aaec85d6a7bc2d72
witwint <witwint1234@gmail.com>
2023-11-06 14:34:19 +0900

    feat: 당첨번호 dto 구현

---
+++ src/main/java/lotto/dto/requset/WinningBonusNumber.java
@@ -0,0 +1,24 @@
+package lotto.dto.requset;
+
+public class WinningBonusNumber {
+
+	String winningNumber;
+
+	String bonusNumber;
+
+	public String getWinningNumber() {
+		return winningNumber;
+	}
+
+	public void setWinningNumber(String winningNumber) {
+		this.winningNumber = winningNumber;
+	}
+
+	public String getBonusNumber() {
+		return bonusNumber;
+	}
+
+	public void setBonusNumber(String bonusNumber) {
+		this.bonusNumber = bonusNumber;
+	}
+}

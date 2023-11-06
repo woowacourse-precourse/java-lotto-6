9cfe05e13cbc02a14bd89b9907f61e6c40508443
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 16:12:41 +0900

    test(WinningYield): 수익류은 소수점 둘째 자리에서 반올림하여 제공하는 기능 테스트 코드 구현

--- src/test/java/lotto/domain/WinningYieldTest.java
+++ src/test/java/lotto/domain/WinningYieldTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class WinningYieldTest {
-  
+    @DisplayName("수익률은 소수점 둘째 자리에서 반올림한다.")
+    @Test
+    void getYield() {
+        Cash cash = new Cash(131000);
+        Profit profit = new Profit();
+
+        profit.add(55000);
+
+        WinningYield winningYield = new WinningYield(cash, profit);
+
+        assertThat(winningYield.getYield()).isEqualTo(42.0);
+    }
 }

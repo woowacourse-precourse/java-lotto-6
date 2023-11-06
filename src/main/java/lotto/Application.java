a27ffe9cd673921431e7066cfb6ae4953ebd5085
seaniiio <cnc4750@naver.com>
2023-11-06 13:47:34 +0900

    feat(User): 사용자로부터 로또 구입 금액을 입력받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.User.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User user = new User();
     }
 }
--- src/main/java/lotto/domain/User/User.java
+++ src/main/java/lotto/domain/User/User.java
@@ -1,5 +1,28 @@
 package lotto.domain.User;
 
+import lotto.domain.Lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.validator.InputValidator;
+
 public class User {
+    private int lottoCount;
+    private List<Lotto> lotties = new ArrayList<>();
+    InputValidator inputValidator = new InputValidator();
+
+    public User() {
+        inputLottoAmount();
+    }
+
+    private void inputLottoAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String lottoAmountInput = Console.readLine();
+        //validate
+        lottoCount = Integer.parseInt(lottoAmountInput) / 1000;
+    }
 
 }

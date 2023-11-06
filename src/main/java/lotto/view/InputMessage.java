11538d864c8ef3458abb3e3a44a8eccfe78d4b49
sehee <tpgml701@naver.com>
2023-11-06 23:22:54 +0900

    feat: InputMessage 입력 시 안내 메시지 출력 기능

---
+++ src/main/java/lotto/view/InputMessage.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public final class InputMessage {
+    final static String MONEY_INPUT_MESSAGE = "구매금액을 입력해주세요.";
+    public static void MoneyInputMessage() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+    }
+
+}

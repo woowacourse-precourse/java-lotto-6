b701a48249c245403800243151a84291698e9cf4
박정현 <ros28081@naver.com>
2023-11-06 19:23:18 +0900

    feat(Lotto): validate 기능 에러 메시지 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,8 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호 갯수는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
 }

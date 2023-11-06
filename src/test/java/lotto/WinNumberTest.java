277f30951883d21b8a36e5dc52b21f89d517c407
baksakcci <pshsh910@gmail.com>
2023-11-07 01:37:59 +0900

    test: `WinNumber` test 추가
    - "당첨 번호가 숫자가 아니면 예외가 발생한다."
    - "당첨 번호를 6개 입력하지 않으면 예외가 발생한다."
    - "당첨 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다."

---
+++ src/test/java/lotto/WinNumberTest.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import lotto.model.vo.WinNumber;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class WinNumberTest {
+
+    @DisplayName("당첨 번호가 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void should_throwException_when_isNotNumeric() {
+        Assertions.assertThatThrownBy(() -> WinNumber.of("1,2,3,4,5,a"))
+                .isInstanceOf(NumberFormatException.class);
+    }
+
+    @DisplayName("당첨 번호를 6개 입력하지 않으면 예외가 발생한다.")
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7"})
+    @ParameterizedTest
+    void should_throwException_when_hasNot6Numbers(String input) {
+        Assertions.assertThatThrownBy(() -> WinNumber.of(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("6개의 수를 입력해야 합니다.");
+    }
+
+    @DisplayName("당첨 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void should_throwException_when_isNotBetween1And45() {
+        Assertions.assertThatThrownBy(() -> WinNumber.of("1,2,3,4,46,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1부터 45 사이의 수를 입력해야 합니다.");
+    }
+}

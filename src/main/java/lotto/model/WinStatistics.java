fc492ee40a7045f170cf1f32cfa3c520dd1fba76
sinminseok12 <sinminseok12@naver.com>
2023-11-07 00:19:35 +0900

    feat(WinStatistics): 발행된 로또 등수를 기반으로 당첨 통계를 만드는 기능 구현.

--- src/main/java/lotto/model/WinStatistics.java
+++ src/main/java/lotto/model/WinStatistics.java
@@ -1,11 +1,27 @@
 package lotto.model;
 
 import java.util.EnumMap;
+import java.util.List;
 
 public class WinStatistics {
     private final EnumMap<Ranking, Integer> statistics;
 
-    public WinStatistics(EnumMap<Ranking, Integer> statistics) {
+    private WinStatistics(EnumMap<Ranking, Integer> statistics) {
         this.statistics = statistics;
     }
+
+    public static WinStatistics from(List<Ranking> rankings) {
+        return new WinStatistics(createStatistics(rankings));
+    }
+
+    private static EnumMap<Ranking, Integer> createStatistics(List<Ranking> rankings) {
+        return rankings.stream()
+                .collect(
+                        () -> new EnumMap<>(Ranking.class),
+                        (map, ranking) -> map.put(ranking, map.getOrDefault(ranking, 0) + 1),
+                        EnumMap::putAll
+                );
+    }
+
+
 }

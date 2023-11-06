2754412db4faf5f1449f1878d841d7dd8fc2352e
stopmin <geemin0@naver.com>
2023-11-07 02:11:21 +0900

    refactor: 로또 게임 도메인 분리
    input controller가 도메인 기능을 수행하고 있다고 판단하여, lotto game을 별도로 두어 분리시켰음

---
+++ src/main/java/lotto/model/LottoGame.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import lotto.domain.LottoBoard;
+
+public class LottoGame {
+    private LottoBoard lottoBoard;
+
+
+    public LottoGame(LottoBoard lottoBoard) {
+        this.lottoBoard = lottoBoard;
+    }
+}

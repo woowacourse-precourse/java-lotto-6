f0a574bc6bdeaa4a835d465538f58a2abb2d796a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 14:47:53 +0900

    test(inputView): 당첨 번호가 숫자가 아닌 경우 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -36,7 +36,7 @@ class InputViewTest {
         @Test
         void 당첨번호_입력시_숫자와_구분자_공백이_아닌_다른_문자가_포함된_경우_예외() {
             assertSimpleTest(() -> {
-                assertThatThrownBy(() -> runException("1,2,3.4. 8, 6"))
+                assertThatThrownBy(() -> runException("1.2.3.4.8.6"))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage(INVALID_WINNING_NUMBERS_INPUT_TYPE.getMessage());
             });
@@ -47,6 +47,15 @@ class InputViewTest {
             assertSimpleTest(() -> {
                 run("1,2,3,4,  5, 7");
                 assertThat(output()).contains("1", "2", "3", "4", "5", "7");
+            });
+        }
+
+        @Test
+        void 당첨번호가_숫자가_아닌_경우_예외() {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("1,2,3,4, a, 6"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(INVALID_WINNING_NUMBERS_INPUT_TYPE.getMessage());
             });
         }
 

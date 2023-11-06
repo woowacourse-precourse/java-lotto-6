99d7f797146fed77ab5e81cc0ed05ea14d779c33
muyaaho <skanthsu715@gmail.com>
2023-11-06 10:05:01 +0900

    refactor(lotto): 보너스 클래스 생성

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -3,15 +3,13 @@ package lotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class ValidationTest {
+public class BonusTest {
 
     @Test
     void 일에서_45사이의_숫자가_아닌지_확인(){
-        Validation validation = new Validation();
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> validation.isIn1to45(99));
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->new Bonus(99));
         assertEquals(exception.getMessage(), "보너스 숫자가 1에서 45사이의 수가 아닙니다.");
     }
 }

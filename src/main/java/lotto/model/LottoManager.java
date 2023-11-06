79a313381a425d8ed1162ab9707e8780b974d8ef
dltjdn <hana40004@naver.com>
2023-11-07 05:08:51 +0900

    feat(LottoManager): 당첨 결과 정보 가져와 lottoResult에 저장
    * lottoResult에 당첨 정보와 해당 정보의 갯수 저장

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -1,28 +1,42 @@
 package lotto.model;
 
+import java.util.HashMap;
 import java.util.List;
 import lotto.model.lotto.Lotto;
 import lotto.model.lotto.LottoDTO;
 import lotto.model.winningLotto.WinningLotto;
-import lotto.model.winningLotto.WinningLottoDTO;
 
 public class LottoManager {
     private Lottos lottos;
     private WinningLotto winningLotto;
 
+
     public void makeLottos(int numberOfLottos, LottoGenerator lottoGenerator) {
-         lottos = Lottos.of(numberOfLottos, lottoGenerator);
+        lottos = Lottos.of(numberOfLottos, lottoGenerator);
     }
 
     public void makeWinningLotto(Lotto lotto, int bonusNumber) {
         winningLotto = WinningLotto.of(lotto, bonusNumber);
     }
 
-    public List<List<Integer>> getPurchaseLottos(){
-        return lottos.toSortedLottoDTOs()
+    public List<List<Integer>> getPurchaseLottos() {
+        return lottos.toLottoDTOs()
                 .stream()
                 .map(LottoDTO::numbers)
                 .toList();
     }
+
+
+    public LottoResult calculateLottoResult() {
+        LottoResult lottoResult = LottoResult.of();
+        List<WinningInfo> winningInfos = lottos.toLottoDTOs()
+                .stream()
+                .map(lottoDTO -> winningLotto.compare(lottoDTO))
+                .toList();
+
+        winningInfos.forEach(lottoResult::addResult);
+        return lottoResult;
+    }
+
 
 }

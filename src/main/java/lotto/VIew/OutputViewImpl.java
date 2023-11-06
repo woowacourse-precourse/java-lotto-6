d5d1467d71a99ec15c820da67e6a1abcd657c2ea
sami355-24 <ddung355@gmail.com>
2023-11-06 15:33:25 +0900

    feat(View): 구입한 로또 오름차순으로 정렬해서 출력하는 기능 추가

--- src/main/java/lotto/VIew/OutputViewImpl.java
+++ src/main/java/lotto/VIew/OutputViewImpl.java
@@ -2,6 +2,7 @@ package lotto.VIew;
 
 import static lotto.Message.OutputViewMessage.PURCHASED_LOTTO_COUNT;
 
+import java.util.List;
 import lotto.Domain.LottoGroup;
 import lotto.Domain.LottoResult.LottoResult;
 
@@ -15,7 +16,11 @@ public class OutputViewImpl implements OutputView{
 
     @Override
     public void printOrderedLottos(LottoGroup lottoGroup) {
-
+        for (int order = 0; order < lottoGroup.findLottoNumbersSize(); order++) {
+            List<Integer> numbers = lottoGroup.findLottoByIndex(order).getNumbers();
+            numbers.sort(Integer::compareTo);
+            System.out.println(numbers);
+        }
     }
 
     @Override

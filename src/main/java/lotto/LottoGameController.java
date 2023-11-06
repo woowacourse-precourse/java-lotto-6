bda8a42fbba4bea2e844160a0bf394f373f2dfdd
twinson37 <twinson37@hanyang.ac.kr>
2023-11-06 19:48:12 +0900

    Feature: 로또 클래스 생성 로직 변경 및 사용자 입력 방식 변경

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -6,18 +6,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class LottoGameController {
 
-    private Lotto lotto;
+    private List<Lotto> lottos = new ArrayList<>();
     User user;
     View view = new View();
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-    public void start() throws IOException {
+    public void start(){
 
         setUser();
         setLotto();
@@ -26,32 +26,36 @@ public class LottoGameController {
     private void setLotto() {
 
         int lottoAmount = user.getAmountLotto();
-        List<List<Integer>> allLottos = makeLottoNumbers(lottoAmount);
-        
-        for (List<Integer> numbers: allLottos){
+//        List<List<Integer>> allLottos = makeLottoNumbers(lottoAmount);
+        view.printPurchasedLottoAmount(lottoAmount);
 
-            Lotto lotto = new Lotto(numbers);
+        for (int i = 0; i<lottoAmount;i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+
+            view.printPurchasedLottoNumbers(lottos.get(i));
+
         }
-
     }
 
-    private List<List<Integer>> makeLottoNumbers(int lottoAmount) {
+//    private List<List<Integer>> makeLottoNumbers(int lottoAmount) {
+//
+//        List<List<Integer>> allNumbers = new ArrayList<>();
+//
+//        for(int i = 0; i< lottoAmount;i++){
+//
+//            allNumbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+//        }
+//
+//        return allNumbers;
+//    }
 
-        List<List<Integer>> allNumbers = new ArrayList<>();
-
-        for(int i = 0; i< lottoAmount;i++){
-
-            allNumbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        }
-
-        return allNumbers;
-    }
-
-    private void setUser() throws IOException {
+    private void setUser() {
+        Scanner sc = new Scanner(System.in);
 
         view.printAskMoneyInput();
 
-        StringTokenizer tk = new StringTokenizer(br.readLine());
+        StringTokenizer tk = new StringTokenizer(sc.next());
         int inputMoney = Integer.parseInt(tk.nextToken());
 
         user = new User(inputMoney);

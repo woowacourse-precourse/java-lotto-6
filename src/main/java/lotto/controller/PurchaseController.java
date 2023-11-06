3c4834fc9c89117b86adced8b1c6499833e7571b
Seyeon <wjdtpdus828@naver.com>
2023-11-06 22:00:26 +0900

    feat(PurchaseController): LottoController로 return 하는 메소드 구현

--- src/main/java/lotto/controller/PurchaseController.java
+++ src/main/java/lotto/controller/PurchaseController.java
@@ -2,28 +2,34 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.PurchaseAmount;
-import lotto.utils.Parser;
 import lotto.view.LottoView;
 
 public class PurchaseController {
     private LottoView lottoView = new LottoView();
-    public void processPurchaseAmount(){
+
+    public int runPurchaseAmount(){
+        return processPurchaseAmount();
+    }
+    public int processPurchaseAmount(){
         String purchaseNum;
+        int amount;
         while(true){
             try{
                 purchaseNum = getPurchaseAmount();
                 PurchaseAmount purchaseAmount = new PurchaseAmount(purchaseNum);
+                amount = purchaseAmount.getTicketNum();
                 break;
             }catch(IllegalArgumentException e){
                 System.out.println(e.getMessage());
-                // e.printStackTrace();
             }
         }
+
+        return amount;
     }
     public String getPurchaseAmount(){
         String purchaseNum;
 
-        lottoView.purchaseAmountView();
+        lottoView.inputPurchaseAmountView();
         purchaseNum = Console.readLine();
 
         return purchaseNum;

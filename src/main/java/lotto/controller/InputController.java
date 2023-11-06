36609d812d0afa9336a22ebdf1b3b56007efff05
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 21:31:41 +0900

    feat: bonusNum 인풋 구현

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import java.util.List;
+import lotto.model.BonusNumberValidation;
 import lotto.model.PlayerLottoAmount;
 import lotto.view.InputView;
 
@@ -12,5 +13,10 @@ public class InputController {
 
     public static List<Integer> inputWinningNumbers() {
         return InputView.inputLottoWinningNum();
+    }
+
+    public static int inputBonusNum() {
+        BonusNumberValidation bonusNumberValidation = new BonusNumberValidation(InputView.inputBonusNumber());
+        return bonusNumberValidation.getBonusNumber();
     }
 }

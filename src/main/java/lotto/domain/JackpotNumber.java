e0d8d205e5bb467bd58be576283299707dc4bbc3
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-07 01:52:13 +0900

    refactor: 보너스 번호까지 추가

--- src/main/java/lotto/domain/JackpotNumber.java
+++ src/main/java/lotto/domain/JackpotNumber.java
@@ -2,11 +2,16 @@ package lotto.domain;
 
 public class JackpotNumber {
     private final Lotto jackpot;
+    private final Integer bonusNumber;
 
-    public JackpotNumber(Lotto jackpot) {
+    public JackpotNumber(Lotto jackpot, Integer bounusNumber) {
         this.jackpot = jackpot;
+        this.bonusNumber = bounusNumber;
     }
     public Lotto getJackpot() {
         return jackpot;
+    }
+    public Integer getBonusNumber() {
+        return bonusNumber;
     }
 }

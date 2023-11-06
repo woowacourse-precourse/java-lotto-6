7ab6db3ef2be6d363f3273900a8fe38fe16c0aa9
akskflwn <akskflwn@gmail.com>
2023-11-06 15:08:10 +0900

    feat: 발행한 로또 수량과 수량 만큼 랜덤으로 생성된 `Lotto` 를 출력 하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
+
+    private static final String PURCHASE_MESSAGE = "\n%d개를 구매했습니다.\n";
+
+    public void print(List<Lotto> lottoTickets) {
+        printCountOf(lottoTickets);
+        lottoTickets.forEach(System.out::println);
+    }
+
+    private void printCountOf(List<Lotto> lottoTickets) {
+        System.out.printf(PURCHASE_MESSAGE, lottoTickets.size());
+    }
 }

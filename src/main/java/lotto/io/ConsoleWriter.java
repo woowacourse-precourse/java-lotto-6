2a13d74f7233c444534e6c59480c08534b74dc7d
sunghun <nokdonwithkj211@gmail.com>
2023-11-06 15:33:51 +0900

    feat(Writer): 포맷에 맞게 출력하는 기능 추가

--- src/main/java/lotto/io/ConsoleWriter.java
+++ src/main/java/lotto/io/ConsoleWriter.java
@@ -2,7 +2,16 @@ package lotto.io;
 
 public class ConsoleWriter implements Writer {
     @Override
+    public void write(String contextWithOutLine) {
+        System.out.print(contextWithOutLine);
+    }
+
+    @Override
     public void writeLine(String sentence) {
         System.out.println(sentence);
+    }
+
+    public void writeFormat(String format, Object... args) {
+        System.out.printf(format, args);
     }
 }
--- src/main/java/lotto/io/Writer.java
+++ src/main/java/lotto/io/Writer.java
@@ -1,5 +1,7 @@
 package lotto.io;
 
 public interface Writer {
+    void write(String contextWithOutLine);
     void writeLine(String context);
+    void writeFormat(String format, Object... args);
 }

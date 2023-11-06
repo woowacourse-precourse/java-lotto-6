18fcd86fe6b6e1121376f65018c9c989d1ec08be
4444ki <qkrwogh423@gmail.com>
2023-11-06 15:09:21 +0900

    ## feat : 돈을 받아 드리는 객체 추가 돈을 받고 출력하는 객체 추가

---
+++ src/main/java/lotto/model/Money.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import lotto.vo.MoneyNumber;
+
+public class Money {
+    private final MoneyNumber moneyNumber;
+
+    public Money(String money){
+        this.moneyNumber = new MoneyNumber(money);
+    }
+
+    public int getTryNumber(){
+        return Integer.parseInt(moneyNumber.value()) / 1000;
+    }
+
+    public int getMoney(){
+        return Integer.parseInt(moneyNumber.value());
+    }
+
+
+}

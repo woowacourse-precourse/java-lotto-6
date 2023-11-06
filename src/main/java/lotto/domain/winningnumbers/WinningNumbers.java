0bb116c62f2162c4badc3a5d712965ca44a94562
chanhoim <cksgh201@naver.com>
2023-11-07 02:29:14 +0900

    refactor(winning numbers) 필드 이름 수정

--- src/main/java/lotto/domain/winningnumbers/WinningNumbers.java
+++ src/main/java/lotto/domain/winningnumbers/WinningNumbers.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class WinningNumbers {
 
-    private final Lotto lotto;
+    private final Lotto winningNumbers;
     private final BonusNumber bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        this.lotto = new Lotto(winningNumbers);
+        this.winningNumbers = new Lotto(winningNumbers);
         this.bonusNumber = new BonusNumber(winningNumbers,bonusNumber);
     }
 

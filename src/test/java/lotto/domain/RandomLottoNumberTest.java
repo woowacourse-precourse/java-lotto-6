388e39fde18bfe38f6b6c1a21cf3dfcb4fcfcf3b
WinterHana <haha920@naver.com>
2023-11-06 23:22:05 +0900

    test(RandomLottoNumber) : RandomLottoNumber에 대한 테스트 코드 추가
    - RandomLottoNumber의 단위 테스트를 위한 테스트 코드 추가
    - 로또 번호가 중복 없이 무작위로 생성되는지에 대한 테스트 코드 추가

---
+++ src/test/java/lotto/domain/RandomLottoNumberTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomLottoNumberTest {
+    RandomLottoNumber randomLottoNumber;
+    static final int LOTTO_NUMBER_SIZE = 6;
+
+    @BeforeEach
+    void initRandomLottoNumber() {
+        randomLottoNumber = new RandomLottoNumber();
+    }
+
+    @DisplayName("로또 번호가 중복 없이 무작위로 생성되는지 확인")
+    @Test
+    void createRandomLottoNumber() {
+        List<Integer> LottoNumber = randomLottoNumber.getNumbers();
+        Set<Integer> setLottoNumber = LottoNumber.stream().collect(Collectors.toSet());
+        assertThat(setLottoNumber.size()).isEqualTo(LOTTO_NUMBER_SIZE);
+    }
+}

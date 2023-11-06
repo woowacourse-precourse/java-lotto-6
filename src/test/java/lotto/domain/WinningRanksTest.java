529b09e7026d9dbcc6208f8e6a189fc1096798c4
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 23:47:20 +0900

    test(WinningRanks): 당첨 내역 계산 로직 테스트 코드 구현

--- src/test/java/lotto/domain/WinningRanksTest.java
+++ src/test/java/lotto/domain/WinningRanksTest.java
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-class WinningDetailsTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningRanksTest {
     @DisplayName("당첨 내역을 계산한다.")
     @Test
-    void calculateProfitAmount() {
+    void calculateRanks() {
+        // given
         List<Lotto> lottos = new ArrayList<>();
         Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 15));
         lottos.add(lotto1);
         lottos.add(lotto2);
 
@@ -20,8 +23,13 @@ class WinningDetailsTest {
         BonusNumber bonusNumber = new BonusNumber(10);
         WinningNumbers winningNumbers = new WinningNumbers(winningNumber, bonusNumber);
 
-        WinningDetails winningDetails = new WinningDetails(lottos, winningNumbers);
+        WinningRanks winningRanks = new WinningRanks(lottos, winningNumbers);
 
-        winningDetails.getDetails();
+        // when
+        winningRanks.calculateRanks();
+
+        // then
+        assertThat(winningRanks.getRanks().size()).isEqualTo(lottos.size());
+        assertThat(winningRanks.getRanks().get(1)).isEqualTo(Rank.FIFTH);
     }
 }

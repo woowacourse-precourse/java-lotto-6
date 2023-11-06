3f6583cb9c8a52d7b6b7c400b65cf21e3ee2cf04
최진원 <etudent39@gmail.com>
2023-11-06 21:05:01 +0900

    feat : 구매 금액 관련 클래스 구현

---
+++ src/main/java/lotto/BuyAmount.java
@@ -0,0 +1,30 @@
+package lotto;
+
+public class BuyAmount {
+    private final static Integer UNIT = 1000;
+    private final Integer buyAmount;
+
+    public BuyAmount(String buyAmount) {
+        validateInteger(buyAmount);
+        this.buyAmount = Integer.parseInt(buyAmount);
+        validateUnit(this.buyAmount);
+    }
+
+    private void validateInteger(String buyAmount) {
+        try {
+            Integer.parseInt(buyAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력하신 값은 정수로 변환할 수 없습니다.");
+        }
+    }
+
+    private void validateUnit(Integer buyAmount) {
+        if (buyAmount % UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액이 1,000원으로 나누어 떨어지지 않습니다");
+        }
+    }
+
+    public Integer getBuyAmount() {
+        return this.buyAmount;
+    }
+}

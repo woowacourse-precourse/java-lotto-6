fd603d0ff2dddf797082a6fc9a0fba2c51515da7
김민정 <mjeong@sookmyung.ac.kr>
2023-11-06 15:58:04 +0900

    style: 사용하지 않는 임포트 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,6 @@
 package lotto;
 
-import java.util.List;
-import lotto.domain.Lotto;
-import lotto.domain.LottoGenerator;
-import lotto.util.SortedRandomNumberGenerator;
-import lotto.view.InputView;
-
 public class Application {
-    LottoGenerator lottoGenerator = new LottoGenerator(new SortedRandomNumberGenerator());
     public static void main(String[] args) {
         LottoGame lottoGame = new LottoGame();
         lottoGame.play();

58e0afc1e2040fe9aac2e032c6e2a62020390bec
JW0321 <digneotn123@gmail.com>
2023-11-06 23:03:37 +0900

    test: 당첨 번호 설정 테스트

--- src/test/java/lotto/model/LottoMachineTest.java
+++ src/test/java/lotto/model/LottoMachineTest.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +19,15 @@ public class LottoMachineTest {
         ArrayList<Lotto> lottoTickets = lottoMachine.runLottoMachine(purchaseCount);
 
         assertThat(lottoTickets.size()).isEqualTo(purchaseCount);
+    }
+
+    @DisplayName("당첨 번호 설정")
+    @Test
+    void testSetWinningNumbers() {
+        LottoMachine lottoMachine = new LottoMachine();
+        lottoMachine.setWinningNumbers(List.of(1,2,3,4,5,6));
+
+        assertThat(lottoMachine.getWinningNumbers()).isEqualTo(List.of(1,2,3,4,5,6));
     }
 
 }

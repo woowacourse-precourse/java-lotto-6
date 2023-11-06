df5656509e8db8eed43a8be8f04fff475163dba3
metishonora <metishonora@icloud.com>
2023-11-06 15:42:55 +0900

    refactor: 출력 메시지 변경

--- src/main/java/lotto/util/ErrorCode.java
+++ src/main/java/lotto/util/ErrorCode.java
@@ -3,7 +3,7 @@ package lotto.util;
 public enum ErrorCode {
     PURCHASE_NOT_DIVISIBLE("구입 단위는 1000원이어야 합니다."),
     PURCHASE_NOT_POSITIVE("구입 금액은 양수여야 합니다."),
-    PURCHASE_NOT_INTEGER("구입 금액은 숫자로 주어져야 합니다."),
+    PURCHASE_NOT_INTEGER("구입 금액은 정수로 주어져야 합니다."),
     LOTTO_FORMAT("쉼표(,)로 구분한 6개의 숫자를 입력해야 합니다."),
     LOTTO_ILLEGAL_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     LOTTO_DUPLICATED("중복된 번호가 존재합니다."),
--- src/main/java/lotto/util/Message.java
+++ src/main/java/lotto/util/Message.java
@@ -2,9 +2,9 @@ package lotto.util;
 
 public enum Message {
     REQUEST_MONEY("구입금액을 입력해 주세요."),
-    RESULT_MONEY("%d개를 구입했습니다."),
-    REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    RESULT_MONEY("\n%d개를 구입했습니다."),
+    REQUEST_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.");
 
     private final String message;
 

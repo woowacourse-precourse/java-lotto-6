8529e3ca271647b15bd80fae3b137f6cec757ee0
706com <ehdgusjdbswl@gmail.com>
2023-11-06 23:16:25 +0900

    add : 최종 통계 출력메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import lotto.domain.Lotto;
 
@@ -19,7 +20,17 @@ public class OutputView {
         System.out.println(number.toString());
     }
 
-    public static void outputWinningStatisitcs(){
+    public static void outputWinningStatisitcs(HashMap<Integer,Integer> hash, double winningRate){
+        statisitcsView[] views = statisitcsView.values();
         System.out.println('\n'+OUTPUT_WINNING_STATISITCS);
+        System.out.println("---");
+        for(int i=3; i<=6; i++){
+            System.out.println(views[i-3].getMessage()+hash.get(i)+"개");
+            if(i==5){
+                System.out.println(views[4].getMessage()+hash.get(i+2)+"개");
+            }
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.",winningRate);
     }
+
 }

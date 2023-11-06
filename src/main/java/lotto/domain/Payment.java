f9d14ca7cdea032f8abdc6089d03452781ff5c6f
yeseong31 <yeseong31@naver.com>
2023-11-06 13:39:55 +0900

    refactor(Payment): Purchase에서 Payment로 이름 변경

---
+++ src/main/java/lotto/domain/Payment.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import static lotto.exception.ExceptionMessage.MINIMUM_PAYMENT_ERROR;
+import static lotto.exception.ExceptionMessage.PURCHASE_AMOUNT_ERROR;
+import static lotto.utils.Converter.convertToInt;
+import static lotto.view.NumberConstant.LOTTO_PRICE_UNIT;
+import static lotto.view.NumberConstant.ZERO;
+
+import lotto.exception.LottoGameException;
+
+public class Payment {
+    
+    private final Integer amount;
+    
+    private Payment(final String input) {
+        int amount = convertToInt(input);
+        validate(amount);
+        this.amount = amount;
+    }
+    
+    public static Payment from(final String amount) {
+        return new Payment(amount);
+    }
+    
+    public int receiveIssuedLottoCount() {
+        return amount / LOTTO_PRICE_UNIT;
+    }
+    
+    private void validate(final int amount) {
+        validateRange(amount);
+        validateCurrencyDivision(amount);
+    }
+    
+    private void validateCurrencyDivision(int amount) {
+        if (notValidateCurrencyDivision(amount)) {
+            throw LottoGameException.from(PURCHASE_AMOUNT_ERROR);
+        }
+    }
+    
+    private boolean notValidateCurrencyDivision(int amount) {
+        return amount % LOTTO_PRICE_UNIT != ZERO;
+    }
+    
+    private void validateRange(int amount) {
+        if (isSmallerThanMinimumPayment(amount)) {
+            throw LottoGameException.from(MINIMUM_PAYMENT_ERROR);
+        }
+    }
+    
+    private boolean isSmallerThanMinimumPayment(int amount) {
+        return amount < LOTTO_PRICE_UNIT;
+    }
+}
--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -10,46 +10,46 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class PurchaseTest {
-
+class PaymentTest {
+    
     @Nested
-    @DisplayName("[Purchase] 입력 테스트")
-    class PurchaseSuccessTest {
-
+    @DisplayName("[Payment] 입력 테스트")
+    class PaymentSuccessTest {
+        
         @DisplayName("로또 구입 금액 입력이 정상적인 경우를 확인")
         @ParameterizedTest
-        @ValueSource(ints = {1000, 2000, 6000})
-        void create_로또_구입_금액_입력이_정상적인_경우를_확인(int amount) {
+        @ValueSource(strings = {"1000", "2000", "6000"})
+        void create_로또_구입_금액_입력이_정상적인_경우를_확인(String amount) {
             assertThatNoException().isThrownBy(()
-                    -> Purchase.of(amount));
+                    -> Payment.from(amount));
         }
     }
-
+    
     @Nested
-    @DisplayName("[Purchase] 예외 테스트")
-    class PurchaseExceptionTest {
-
+    @DisplayName("[Payment] 예외 테스트")
+    class PaymentExceptionTest {
+        
         @DisplayName("로또 구입 금액 입력이 올바르지 않은 경우를 확인")
         @ParameterizedTest
-        @ValueSource(ints = {0, 1, 1001, 9999})
-        void exception_로또_구입_금액_입력이_올바르지_않은_경우를_확인(int amount) {
-            assertThatThrownBy(() -> Purchase.of(amount))
+        @ValueSource(strings = {"0", "1", "1001", "9999"})
+        void exception_로또_구입_금액_입력이_올바르지_않은_경우를_확인(String amount) {
+            assertThatThrownBy(() -> Payment.from(amount))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("[ERROR]");
         }
     }
-
+    
     @Nested
-    @DisplayName("[Purchase] 기능 테스트")
+    @DisplayName("[Payment] 기능 테스트")
     class PriceUnitTest {
-
+        
         @DisplayName("구입한 로또의 수 확인")
         @ParameterizedTest
         @CsvSource({"1000, 1", "2000, 2", "6000, 6"})
-        void count_구입한_로또의_수_확인(int amount, int count) {
-            Purchase purchase = Purchase.of(amount);
-
-            assertThat(purchase.receiveIssuedLottoCount())
+        void count_구입한_로또의_수_확인(String amount, int count) {
+            Payment payment = Payment.from(amount);
+            
+            assertThat(payment.receiveIssuedLottoCount())
                     .isEqualTo(count);
         }
     }

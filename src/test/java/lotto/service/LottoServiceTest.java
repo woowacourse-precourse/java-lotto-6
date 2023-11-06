e5da3ecac300ebdbf239beaa1d439d0a9aeb69f6
seminchoi <choicco89@gmail.com>
2023-11-06 16:09:39 +0900

    test(service.LottoServiceTest) : getLottoResult에 대한 테스트 로직 강화

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoServiceTest {
-    private static final LottoDto lottoDto = new LottoDto(TestConstant.normalNumbers);
-
     private final LottoService lottoService = TestConstant.testLottoConfig.lottoService();
 
     @DisplayName("구매 금액에 알맞은 로또 영수증을 반환한다.")
@@ -24,24 +22,24 @@ class LottoServiceTest {
         LottoReceiptDto lottoReceiptDto = lottoService.getLottoReceipt(payment);
 
         assertThat(lottoReceiptDto.purchaseCount()).isEqualTo(1);
-        assertThat(lottoReceiptDto.lottos()).contains(lottoDto);
+        assertThat(lottoReceiptDto.lottos()).contains(new LottoDto(TestConstant.normalNumbers));
     }
 
     @DisplayName("로또 결과 통계를 반환한다.")
     @Test
     void getLottoResult() {
-        int payment = TestConstant.minPaymentAmount;
+        long payment = LottoConstraint.PRICE_PER_LOTTO.getValue();
         lottoService.getLottoReceipt(payment);
         lottoService.drawLottoWithoutBonusNumber(TestConstant.normalNumbers);
         lottoService.generateWinningLotto(45);
 
         LottoResultDto lottoResultDto = lottoService.getLottoResult();
+        int firstRankCount = lottoResultDto.result().get(LottoRank.SIX_MATCH);
+        int secondRankCount = lottoResultDto.result().get(LottoRank.FIVE_AND_BONUS_MATCH);
+        double profitRate = lottoResultDto.profitRate();
 
-        //TODO : 변수 위치 이동
-        double sixMatchedProfitRate
-                = (double) LottoRank.SIX_MATCH.getPrizeMoney() / LottoConstraint.PRICE_PER_LOTTO.getValue();
-        assertThat(lottoResultDto.profitRate()).isEqualTo(sixMatchedProfitRate);
-        assertThat(lottoResultDto.result().get(LottoRank.SIX_MATCH))
-                .isEqualTo(LottoConstraint.MIN_PURCHASE_QUANTITY.getValue());
+        assertThat(firstRankCount).isEqualTo(1);
+        assertThat(secondRankCount).isEqualTo(0);
+        assertThat(profitRate).isEqualTo(200000000.0);
     }
 }

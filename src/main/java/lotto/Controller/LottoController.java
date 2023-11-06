1b39092eb692fccecc69ac2b57b825ad6637fe80
7ngenious <zxvs1218@gmail.com>
2023-11-07 00:13:02 +0900

    feat: Add LottoController Entire Function

---
+++ src/main/java/lotto/Controller/LottoController.java
@@ -0,0 +1,107 @@
+package lotto.Controller;
+
+import lotto.Domain.Lotto;
+import lotto.View.LottoView;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    private LottoView view;
+
+    public LottoController(LottoView view) {
+        this.view = view;
+    }
+
+    public void runGame() {
+        int ticketCount = view.getUserInput("로또 구입 금액을 입력하세요: ");
+        List<Lotto> purchasedTickets = generateTickets(ticketCount);
+
+        List<Integer> winningNumbers = getWinningNumbersFromUser();
+        List<String> results = calculatePrizes(purchasedTickets, winningNumbers);
+        view.displayResults(results);
+    }
+
+    private List<Lotto> generateTickets(int ticketCount) {
+        List<Lotto> purchasedTickets = new ArrayList<>();
+        for (int i = 0; i < ticketCount; i++) {
+            Lotto lotto = generateRandomLotto();
+            purchasedTickets.add(lotto);
+        }
+        return purchasedTickets;
+    }
+
+    private Lotto generateRandomLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(randomNumbers);
+    }
+
+    private List<Integer> getWinningNumbersFromUser() {
+        List<Integer> winningNumbers = new ArrayList<>();
+        try {
+            for (int i = 0; i < 7; i++) {
+                int number = view.getUserInput("당첨 번호 " + (i + 1) + "을 입력하세요: ");
+                if (i < 6) {
+                    if (number < 1 || number > 45) {
+                        view.displayError("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+                        i--;
+                    } else {
+                        winningNumbers.add(number);
+                    }
+                } else {
+                    if (number < 1 || number > 45) {
+                        view.displayError("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+                        i--;
+                    } else {
+                        winningNumbers.add(number);
+                    }
+                }
+            }
+        } catch (NumberFormatException e) {
+            view.displayError("올바른 숫자를 입력하세요.");
+        }
+        return winningNumbers;
+    }
+
+    private List<String> calculatePrizes(List<Lotto> purchasedTickets, List<Integer> winningNumbers) {
+        List<String> results = new ArrayList<>();
+        for (Lotto lotto : purchasedTickets) {
+            int matchingNumbers = countMatchingNumbers(lotto, winningNumbers);
+            boolean hasBonusNumber = lotto.getNumbers().contains(winningNumbers.get(6));
+            String result = calculatePrize(matchingNumbers, hasBonusNumber);
+            results.add(result);
+        }
+        return results;
+    }
+
+    private int countMatchingNumbers(Lotto lotto, List<Integer> winningNumbers) {
+        int count = 0;
+        for (int number : lotto.getNumbers()) {
+            if (winningNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private String calculatePrize(int matchingNumbers, boolean hasBonusNumber) {
+        String result;
+        if (matchingNumbers == 6) {
+            result = "1등: 6개 번호 일치 - 상금 2,000,000,000원";
+        } else if (matchingNumbers == 5 && hasBonusNumber) {
+            result = "2등: 5개 번호 + 보너스 번호 일치 - 상금 30,000,000원";
+        } else if (matchingNumbers == 5) {
+            result = "3등: 5개 번호 일치 - 상금 1,500,000원";
+        } else if (matchingNumbers == 4) {
+            result = "4등: 4개 번호 일치 - 상금 50,000원";
+        } else if (matchingNumbers == 3) {
+            result = "5등: 3개 번호 일치 - 상금 5,000원";
+        } else {
+            result = "꽝: 일치하는 번호 없음";
+        }
+        return result;
+    }
+
+}

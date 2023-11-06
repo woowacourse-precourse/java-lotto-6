23c6f2379b7eb566c8ed368e3125e0536fd8768c
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 22:30:12 +0900

    refactor: NumberGenerator 내 매직넘버를 상수로 대체

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -4,13 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lotto.utils.Constants;
 
 public class NumberGenerator {
     public NumberGenerator() {
     }
 
     public List<Integer> createRandomLottoNumbers() {
-        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
+                Constants.LOTTO_MIN_NUMBER, Constants.LOTTO_MAX_NUMBER, Constants.LOTTO_NUMBER_COUNTS));
         Collections.sort(numbers);
         return numbers;
     }

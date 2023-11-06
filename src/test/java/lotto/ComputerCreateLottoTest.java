89e5589d29aa6217b48a4f370e54c329b5b97acb
yu-teacher <yusm1231@gmail.com>
2023-11-07 03:22:03 +0900

    test : lottos 삭제 후 test 코드변경
    lottos를 삭제하고 코드가 변경되어서 다른 방식으로 검증을 각각 거쳤습니다.

--- src/test/java/lotto/ComputerCreateLottoTest.java
+++ src/test/java/lotto/ComputerCreateLottoTest.java
@@ -1,41 +1,60 @@
 package lotto;
 
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.controller.LottoController;
-import lotto.domain.Lottos;
-import lotto.view.LottoView;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ComputerCreateLottoTest {
 
-    @Test
-    void 금액_입력시_로또_번호_발행() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        LottoView lottoView = new LottoView();
+    private LottoController lottoController;
 
-        Method lottoPayMethod = LottoView
-                .class
-                .getDeclaredMethod("lottoPay", String.class);
-        lottoPayMethod.setAccessible(true);
-        lottoPayMethod.invoke(lottoView, "10000");
+    @BeforeEach
+    void after(){
+        lottoController = LottoController.getInstance();
+    }
+
+    @Test
+    void 금액_입력시_로또_번호_발행() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    List<Lotto> lottoTickets1 = lottoController.setLottoBudget("3000");
+                    List<Lotto> lottoTickets2 = new ArrayList<>();
+                    ArrayList<Integer> number1 = new ArrayList<>(Arrays.asList(8, 21, 23, 41, 42, 43));
+                    lottoTickets2.add(new Lotto(number1));
+                    boolean isEqual = listsHaveSameElements(
+                            lottoTickets1.get(0).getNumbers(),
+                            lottoTickets2.get(0).getNumbers()
+                    );
+                    assertThat(isEqual)
+                            .isEqualTo(true);
+                },
+                List.of(8, 21, 23, 41, 42, 43)
+        );
+
     }
     @Test
     void 정렬확인(){
         assertRandomUniqueNumbersInRangeTest(
-                () -> { // input 21 output 8 둘다 8 나오면 OK
-                    Lottos lottos = LottoController
-                            .getInstance()
-                            .lottoSell("1000");
-                    assertThat(lottos.getLottos().get(0).getNumbers())
-                            .isEqualTo(new Lottos().getLottos().get(0).getNumbers());
+                () -> {
+                    List<Lotto> lottoTickets = lottoController
+                            .setLottoBudget("1000");
+                    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(21, 8, 23, 41, 42, 43));
+                    Lotto lotto = new Lotto(numbers);
+                    boolean isEqual = lottoTickets.get(0).getNumbers().equals(lotto.getNumbers());
+                    assertThat(isEqual)
+                            .isEqualTo(true);
                 },
-                List.of(21, 8, 23, 41, 42, 43)
+                List.of(8, 21, 23, 41, 42, 43)
         );
     }
 
@@ -43,7 +62,7 @@ public class ComputerCreateLottoTest {
     void 숫자가_아닌_것_예외처리(){
         String numberStr = "ㅂㅈㄷ";
 
-        assertThatThrownBy(() -> new Lottos(numberStr))
+        assertThatThrownBy(() -> lottoController.setLottoBudget(numberStr))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -51,7 +70,7 @@ public class ComputerCreateLottoTest {
     void 천_미만의_수_예외처리(){
         String numberStr = "900";
 
-        assertThatThrownBy(() -> new Lottos(numberStr))
+        assertThatThrownBy(() -> lottoController.setLottoBudget(numberStr))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -59,7 +78,13 @@ public class ComputerCreateLottoTest {
     void 백원단위가_있을_때_예외처리(){
         String numberStr = "11900";
 
-        assertThatThrownBy(() -> new Lottos(numberStr))
+        assertThatThrownBy(() -> lottoController.setLottoBudget(numberStr))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    public boolean listsHaveSameElements(List<Integer> list1, List<Integer> list2) {
+        Set<Integer> set1 = new HashSet<>(list1);
+        Set<Integer> set2 = new HashSet<>(list2);
+        return set1.equals(set2);
     }
 }
--- src/test/java/lotto/UserCreateLottoTest.java
+++ src/test/java/lotto/UserCreateLottoTest.java
@@ -3,13 +3,11 @@ package lotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import lotto.Service.LottoService;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
-import lotto.dto.LottoResponseDTO;
+import lotto.dto.CountScoreResponseDTO;
+import lotto.Service.LottoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,58 +22,54 @@ public class UserCreateLottoTest {
 
     @Test
     public void 당첨숫자_갯수출력() throws Exception {
-        Lottos lottos = new Lottos(); // 8, 21, 23, 41, 42, 43
-        int[] intArray = {8, 21, 23, 35, 36, 37};
-        List<Integer> intList = Arrays.stream(intArray)
-                .boxed()
-                .collect(Collectors.toList());
-        Lotto lotto = new Lotto(intList);
-        int index = 0;
+        ArrayList<Integer> user = new ArrayList<>(Arrays.asList(21, 8, 23, 41, 42, 43));
+        ArrayList<Integer> ticket = new ArrayList<>(Arrays.asList(21, 8, 23, 31, 32, 33));
+        Lotto userLotto = new Lotto(user);
+        Lotto lottoTicket = new Lotto(ticket);
 
-        Method method = LottoService.class.getDeclaredMethod("getOverlappingNumber", Lotto.class, Lottos.class, int.class);
+        Method method = LottoService.class.getDeclaredMethod("calculateOverlappingNumber", Lotto.class, Lotto.class);
         method.setAccessible(true);
-        int overlappingNumber = (int) method.invoke(lottoService, lotto, lottos, index);
+        int overlappingNumber = (int) method.invoke(lottoService, userLotto, lottoTicket);
 
         assertEquals(3, overlappingNumber);
     }
 
     @Test
     public void 스코어_체크() throws Exception {
-        Lottos lottos = new Lottos(); // 8, 21, 23, 41, 42, 43
-        int index = 0;
-        LottoResponseDTO lottoResponseDTO = new LottoResponseDTO();
+        ArrayList<Integer> user = new ArrayList<>(Arrays.asList(21, 8, 23, 41, 42, 43));
+        Lotto userLotto = new Lotto(user);
+        CountScoreResponseDTO countScoreResponseDTO = new CountScoreResponseDTO();
         int overlappingNumber = 3;
         int bonus = 41;
 
         Method method = LottoService
                 .class
-                .getDeclaredMethod("checkScore",
-                        Lottos.class,
+                .getDeclaredMethod("calculateScore",
+                        Lotto.class,
                         int.class,
-                        LottoResponseDTO.class,
-                        int.class,
+                        CountScoreResponseDTO.class,
                         int.class);
         method.setAccessible(true);
 
-        method.invoke(lottoService, lottos, bonus, lottoResponseDTO, index, overlappingNumber);
+        method.invoke(lottoService, userLotto, bonus, countScoreResponseDTO, overlappingNumber);
 
-        assertEquals(1, lottoResponseDTO.getFifthPlace());
+        assertEquals(1, countScoreResponseDTO.getFifthPlace());
     }
 
     @Test
     public void 수익률_정산() throws Exception {
         int lottoLength = 1;
-        LottoResponseDTO lottoResponseDTO = new LottoResponseDTO();
-        lottoResponseDTO.plusFifthPlace();
+        CountScoreResponseDTO countScoreResponseDTO = new CountScoreResponseDTO();
+        countScoreResponseDTO.plusFifthPlace();
 
         Method method = LottoService
                 .class
                 .getDeclaredMethod("calculateRateOfReturn",
                         int.class,
-                        LottoResponseDTO.class);
+                        CountScoreResponseDTO.class);
         method.setAccessible(true);
 
-        double rateOfReturn = (double) method.invoke(lottoService, lottoLength, lottoResponseDTO);
+        double rateOfReturn = (double) method.invoke(lottoService, lottoLength, countScoreResponseDTO);
 
         assertEquals(500.0, rateOfReturn);
     }

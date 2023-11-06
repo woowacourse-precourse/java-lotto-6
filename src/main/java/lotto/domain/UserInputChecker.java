6cebf1b3e3ffbabff0b44f9ba7ee578f82ab6ffe
HYOAN <aspqll@naver.com>
2023-11-06 23:05:44 +0900

    feat(lotto): 입력된 구입금액이 숫자인지 확인하는 예외처리 기능 구현

--- src/main/java/lotto/domain/UserInputChecker.java
+++ src/main/java/lotto/domain/UserInputChecker.java
@@ -3,20 +3,33 @@ package lotto.domain;
 public class UserInputChecker {
     public UserInputChecker() {
     }
+
     public int checkPurchaseAmount(String inputPurchaseAmount) {
+        try { // 입력된 구입금액이 숫자인지 확인
+            checkInt(inputPurchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+
         int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
-        try {
+
+        try { // 입력된 구입금액이 1000원 단위인지 확인
             checkInputUnit(purchaseAmount);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
         return purchaseAmount;
     }
+
     public int checkInputUnit(int purchaseAmount) { //구입금액 단위 확인
         if (purchaseAmount % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로만 입력 가능합나다.");
         return purchaseAmount;
     }
+
+
     public void checkInt(String input) {
         try {
             Integer.parseInt(input);

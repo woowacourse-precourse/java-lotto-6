fba8c47ce3774e4418e19d1ecdf3761d33341864
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-06 20:28:54 +0900

    refactor: Lotto 클래스 리팩토링

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,7 +1,5 @@
 package lotto.model;
 
-import lotto.exception.Validation;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -9,33 +7,43 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
-    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_TICKET_SIZE = 6;
 
-    private final List<LottoNumber> lottoNumbers;
+    private final List<LottoNumber> lottoTickets;
 
     public Lotto(String lottoNumbers) {
-        this.lottoNumbers = parseLottoNumbers(lottoNumbers);
+        this.lottoTickets = parseLottoNumbers(lottoNumbers);
     }
 
-    public Lotto(List<LottoNumber> lottoNumbers) {
-        Validation.validateLottoSize(lottoNumbers);
-        Validation.validateLottoDuplication(lottoNumbers);
-        this.lottoNumbers = lottoNumbers;
-    }
-
-    public static int getLottoSize() {
-        return LOTTO_SIZE;
+    public Lotto(List<LottoNumber> lottoTickets) {
+        validateLottoSize(lottoTickets);
+        validateLottoDuplication(lottoTickets);
+        this.lottoTickets = lottoTickets;
     }
 
     public List<LottoNumber> getNumbers() {
-        return Collections.unmodifiableList(lottoNumbers);
+        return Collections.unmodifiableList(lottoTickets);
     }
 
-    public static List<LottoNumber> parseLottoNumbers(String lottoNumbers) {
+    public List<LottoNumber> parseLottoNumbers(String lottoNumbers) {
 
         return Arrays.stream(lottoNumbers.split(","))
                 .map(String::trim)
                 .map(number -> new LottoNumber(Integer.parseInt(number)))
                 .collect(Collectors.toList());
+    }
+
+    private void validateLottoSize(List<LottoNumber> lottoTickets) {
+        if (lottoTickets.size() != LOTTO_TICKET_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateLottoDuplication(List<LottoNumber> lottoTickets) {
+        long count = lottoTickets.stream().distinct().count();
+
+        if (count != lottoTickets.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다.");
+        }
     }
 }

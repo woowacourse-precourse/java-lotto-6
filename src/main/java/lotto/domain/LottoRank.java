0fffdb785ffc27a93aeb9abbe16f2e662cc69c8d
ParkDH0809 <znxn1237@gmail.com>
2023-11-06 19:11:41 +0900

    feat: 당첨 금액 반환 구현

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,19 +4,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum LottoRank {
-    OUT_OF_RANK("순위권 외", List.of()),
-    FIFTH_PLACE("3개 일치 (5,000원)", List.of(List.of(3, 0), List.of(3, 1))),
-    FOURTH_PLACE("4개 일치 (50,000원)", List.of(List.of(4, 0), List.of(4, 1))),
-    THIRD_PLACE("5개 일치 (1,500,000원)", List.of(List.of(5, 0))),
-    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원)", List.of(List.of(5, 1))),
-    FIRST_PLACE("6개 일치 (2,000,000,000원)", List.of(List.of(6, 0), List.of(6, 1)));
+    OUT_OF_RANK(0, "순위권 외", List.of()),
+    FIFTH_PLACE(5000, "3개 일치 (5,000원)", List.of(List.of(3, 0), List.of(3, 1))),
+    FOURTH_PLACE(50000, "4개 일치 (50,000원)", List.of(List.of(4, 0), List.of(4, 1))),
+    THIRD_PLACE(1500000, "5개 일치 (1,500,000원)", List.of(List.of(5, 0))),
+    SECOND_PLACE(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)", List.of(List.of(5, 1))),
+    FIRST_PLACE(2000000000, "6개 일치 (2,000,000,000원)", List.of(List.of(6, 0), List.of(6, 1)));
 
+    private final int value;
     private final String rankContent;
     private final List<List<Integer>> comparisonResult;
 
-    LottoRank(String rankContent, List<List<Integer>> comparisonResult) {
+    LottoRank(int value, String rankContent, List<List<Integer>> comparisonResult) {
+        this.value = value;
         this.rankContent = rankContent;
         this.comparisonResult = comparisonResult;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public String getRankContent() {

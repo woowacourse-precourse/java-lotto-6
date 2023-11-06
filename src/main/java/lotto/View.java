2c36b3b799ee007b92d61395803a2c46d8381153
ratatuwee <pkqwaszx@gmail.com>
2023-11-06 21:31:50 +0900

    feat: View 클래스 구현
    사용자의 입력 및 출력을 관리하며 입력에 대해서 예외를 확인하고 각 값들을 내부에서 사용하기 편하도록 자료형을 맞춰주게 구현

---
+++ src/main/java/lotto/View.java
@@ -0,0 +1,72 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class View {
+    private static final String LOTTO_PURCHASE_AMOUNT_INPUTMESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_WINNING_NUMBER_INPUTMESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String LOTTO_BONUS_NUMBER_INPUTMESSAGE = "보너스 번호를 입력해 주세요.";
+
+    private List<Integer> userWinningRanks;
+    private float profitSummary;
+    private int lottoPurchaseNum;
+    private final String LOTTO_PURCHASE_NUMBERS_OUTPUTMESSAGE = String.format("%d개를 구매했습니다.", lottoPurchaseNum);
+    private final String WINNING_STATISTICS_OUTPUTMESSAGE = String.format("""
+                당첨 통계
+                ---
+                3개 일치 (5,000원) - %d개
+                4개 일치 (50,000원) - %d개
+                5개 일치 (1,500,000원) - %d개
+                5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+                6개 일치 (2,000,000,000원) - %d개     
+            """, userWinningRanks.get(0), userWinningRanks.get(1), userWinningRanks.get(2), userWinningRanks.get(3), userWinningRanks.get(4));
+    private final String PROFIT_SUMMARY_OUTPUTMESSAGE = String.format("총 수익률은 %.1f%입니다.", profitSummary);
+
+    public void checklottoPurchaseAmount(String lottoPurchaseAmount){
+        try{ //int값인지 확인, int 범위 내 인지 확인
+            Integer.parseInt(lottoPurchaseAmount);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] int로 정의 될 수 없습니다.");
+        }
+        if(Integer.parseInt(lottoPurchaseAmount)<1){
+            throw new IllegalArgumentException("[ERROR] 음수의 값은 들어올 수 없습니다.");
+        }
+        if(Integer.parseInt(lottoPurchaseAmount)%1000!=0){
+            throw new IllegalArgumentException("[ERROR] 잔돈이 없어서 1000원 단위의 돈만 받을 수 있습니다.");
+        }
+    }
+
+    public void lottoPurchaseAmountInput(){
+        System.out.println(LOTTO_PURCHASE_AMOUNT_INPUTMESSAGE);
+        String lottoPurchaseAmount = readLine();
+        checklottoPurchaseAmount(lottoPurchaseAmount);
+        lottoPurchaseNum = Integer.parseInt(lottoPurchaseAmount);
+    }
+    
+    
+    public void setUserWinningRanks(List<Integer> userWinningRanks){
+        this.userWinningRanks = userWinningRanks;
+    }
+
+    public void setProfitSummary(float profitSummary){
+        this.profitSummary = profitSummary;
+    }
+
+
+    public void lottoPurchaseNumbersOutput(ArrayList<Lotto> userLottos){
+        System.out.println(LOTTO_PURCHASE_NUMBERS_OUTPUTMESSAGE);
+        for(Lotto lotto : userLottos){
+            System.out.println(lotto.getNumbers().toString());
+        }
+    }
+    
+    public void winningStatisticsOutput(){
+        System.out.println(WINNING_STATISTICS_OUTPUTMESSAGE);
+    }
+    
+    public void profitSummaryOutput(){
+        System.out.println(PROFIT_SUMMARY_OUTPUTMESSAGE);
+    }
+}

b15b61b56f1204157c59c469d8542d4000421e9e
Hajuuu <hpt0127@naver.com>
2023-11-06 18:09:16 +0900

    refactor(Prize): information 추가
    당첨 내역을 출력하는 information 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,16 +1,19 @@
 package lotto;
 
 public enum Prize {
-    THREE(5000),
-    FOUR(50000),
-    FIVE(1500000),
-    BONUS(30000000),
-    SIX(2000000000);
+    THREE(5000, "3개 일치 (5,000원)"),
+    FOUR(50000, "4개 일치 (50,000원)"),
+    FIVE(1500000, "5개 일치 (1,500,000원)"),
+    BONUS(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIX(2000000000, "6개 일치 (2,000,000,000원)");
 
     private final int prize;
 
-    Prize(int prize) {
+    private final String information;
+
+    Prize(int prize, String information) {
         this.prize = prize;
+        this.information = information;
     }
 
     public int getPrize() {

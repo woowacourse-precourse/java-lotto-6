3c16194ebad381c9721142b978f263f43e4267ba
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-06 16:03:35 +0900

    feat: 공백 제거 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,15 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String BLANK = " ";
+    private static final String EMPTY = "";
+
     public String inputPurchaseAmount() {
-        return Console.readLine();
+        return deleteBlank(Console.readLine());
     }
 
     public String inputWinningNumbers() {
-        return Console.readLine();
+        return deleteBlank(Console.readLine());
     }
 
     public String inputBonusNumbers() {
-        return Console.readLine();
+        return deleteBlank(Console.readLine());
+    }
+
+    private String deleteBlank(String input) {
+        return input.replaceAll(BLANK, EMPTY);
     }
 }

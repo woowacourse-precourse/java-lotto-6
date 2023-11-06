9218422c643b876e6ecd4973e139b1a84870c2ff
ahma0 <dsd932@naver.com>
2023-11-07 00:08:51 +0900

    refactor: Exception을 터트린 후 다시 입력받도록 수정

--- src/main/java/lotto/contoller/LottoController.java
+++ src/main/java/lotto/contoller/LottoController.java
@@ -14,22 +14,40 @@ public class LottoController {
     }
 
     private PurchaseAmount receivePurchaseAmount() {
-        lottoView.printInputPurchaseAmount();
-        String answer = Console.readLine();
-        return new PurchaseAmount(answer);
+        while (true) {
+            try {
+                lottoView.printInputPurchaseAmount();
+                String answer = Console.readLine();
+                return new PurchaseAmount(answer);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private WinningNumber receiveWinningNumber() {
-        lottoView.printInputWinningNumber();
-        String answer = Console.readLine();
-        return new WinningNumber(answer);
+        while (true) {
+            try {
+                lottoView.printInputWinningNumber();
+                String answer = Console.readLine();
+                return new WinningNumber(answer);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private WinningNumber receiveBonusNumber(WinningNumber winningNumber) {
-        lottoView.printInputBonusNumber();
-        String answer = Console.readLine();
-        winningNumber.setBonusNumber(answer);
-        return winningNumber;
+        while (true) {
+            try {
+                lottoView.printInputBonusNumber();
+                String answer = Console.readLine();
+                winningNumber.setBonusNumber(answer);
+                return winningNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 }

b30f4c6e1d00e814555b8f669e74f62ba19f1e7c
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-06 13:15:17 +0900

    refactor: LottoService 인터페이스 메서드 업데이트

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -8,41 +8,54 @@ import lotto.util.MatchType;
 public interface LottoService {
 
     /**
-     * 구입 금액 입력 받는 함수
-     * 1,000단위 예외 처리 필요
+     * 구입 금액 입력 받는 함수 1,000단위 예외 처리 필요
+     *
      * @return : 로또 구입 개수
      */
     int calculatePurchaseCount();
 
     /**
      * 랜덤 로또 값 생성해서 반환
+     *
      * @return : 새로운 Lotto 클래스
      */
     List<Lotto> generateLottos(int lottoCount);
 
     /**
-     * 사용자로부터 당첨 번호 입력 받고 반환
-     * 1~45 예외 처리 필요
+     * 사용자로부터 당첨 번호 입력 받고 반환 1~45 예외 처리 필요
+     *
      * @return : 당첨 번호
      */
     List<Integer> inputWinningNumbers();
 
     /**
-     * 사용자로부터 보너스 번호 입력 받고 반환
-     * 1~45 예외 처리 필요
+     * 사용자로부터 보너스 번호 입력 받고 반환 1~45 예외 처리 필요
+     *
      * @return : 보너스 번호
      */
-    int inputBonusNumber();
+    int inputBonusNumber(List<Integer> winningNumbers);
 
     /**
      * 당첨 통계 계산하기
+     *
      * @param lottoNumbers : 로또값 배열
      * @return : 당첨 통계
      */
-    Map<MatchType, Integer> calculateWinningStatistics(List<Integer>[] lottoNumbers);
+    Map<MatchType, Integer> calculateWinningStatistics(List<Lotto> lottoNumbers,
+                                                       List<Integer> winningNumbers,
+                                                       int bonusNumber);
+
+    /**
+     * 수익 계산하는 함수
+     *
+     * @param winningStatistics : 당첨 통계
+     * @return : 수익
+     */
+    int computeEarnings(Map<MatchType, Integer> winningStatistics);
 
     /**
      * 수익률 계산하기
+     *
      * @param earnings : 수익
      * @return : 수익률
      */

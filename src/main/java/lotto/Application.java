4f582a8bc9e83e429ad3602ddcea368af9be86d7
chaerlo127 <monynony0203@gmail.com>
2023-11-06 13:52:51 +0900

    feat: [1] 구입 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int cash = inputLottoPayCount();
+    }
+
+    private static int inputLottoPayCount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        try{
+            int cash = inputLottoCash();
+            checkNotThousandAndNegativeException(cash);
+            return cash;
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+            inputLottoPayCount();
+        }
+        return 0;
+    }
+
+    private static void checkNotThousandAndNegativeException(int count) {
+        if (count % 1000 != 0 || count < 0) throw new IllegalArgumentException("[ERROR] 천원 단위로 입력해주세요.");
+    }
+
+    private static int inputLottoCash() {
+        int count;
+        try{
+            count = Integer.parseInt(Console.readLine());
+            return count;
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 숫자 형식으로 입력해주세요.");
+        }
     }
 }

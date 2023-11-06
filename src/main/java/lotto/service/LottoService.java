348000f76beb62c79aeae62834d7805e1d30e55a
isyoo1213 <moethns@gmail.com>
2023-11-06 23:57:25 +0900

    feat: LottoService 로또 당첨 여부 계산 기능 추가 LottoService - calculateWinningResult() 추가 README.md - 기능 명세 구현 체크

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,15 +1,19 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.BonusNumber;
 import lotto.model.Lotto;
 import lotto.model.Lottos;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.constants.LottoConstants.*;
 
 public class LottoService {
+    private static final int SECOND_PRIZE_SAME_NUMBERS = 5;
 
     public Lottos createUserLottos(int amount) {
         Lottos lottos = new Lottos();
@@ -24,6 +28,20 @@ public class LottoService {
 
     public Lotto createAnswerLotto(List<Integer> lottoNumbers) {
         return new Lotto(lottoNumbers);
+    }
+
+    public List<Integer> calculateWinningResult(Lottos lottos, Lotto answerLotto, BonusNumber bonusNumber) {
+        int[] winningResult = new int[NUMBER_OF_LOTTO_NUMBERS + 2];
+        for (Lotto lotto : lottos.getLottos()) {
+            int sameNumber = lotto.calculateSameNumbers(answerLotto);
+            winningResult[sameNumber]++;
+            if (sameNumber == SECOND_PRIZE_SAME_NUMBERS && lotto.hasNumber(bonusNumber.getBonusNumber())) {
+                winningResult[sameNumber]--;
+                winningResult[NUMBER_OF_LOTTO_NUMBERS + 1]++;
+            }
+        }
+        return Arrays.stream(winningResult)
+                .boxed().collect(Collectors.toList());
     }
 
     public List<Integer> sortNumbersByAsc(List<Integer> integerList) {

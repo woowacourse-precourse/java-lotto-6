43c96ce15614cc977b9c5609d10e03d7368fea62
Donggeon Moon <nullsafer@naver.com>
2023-11-07 04:36:23 +0900

    feat: 당첨 번호 입력 프롬프트 출력

--- src/main/java/lotto/constant/Message.java
+++ src/main/java/lotto/constant/Message.java
@@ -2,7 +2,8 @@ package lotto.constant;
 
 public enum Message {
     PURCHASE_AMOUNT_PROMPT("구입금액을 입력해 주세요."),
-    PURCHASE_AMOUNT_MESSAGE("%d개를 구매했습니다.");
+    PURCHASE_AMOUNT_MESSAGE("%d개를 구매했습니다."),
+    WINNING_NUMBER_PROMPT("당첨 번호를 입력해 주세요.");
 
     private final String message;
 

471502213c58a9f9420127aae728bf5bf932a44b
shkisme <shkisme0130@gmail.com>
2023-11-06 21:27:05 +0900

    refactor: WinningLotto Enum의 필드 순서 변경

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 
 public enum WinningLotto {
     ZERO(0, 0),
-    ONE(0, 1),
-    TWO(0, 2),
-    THREE(5_000, 3),
-    FOUR(50_000, 4),
-    FIVE(1_500_000, 5),
-    BONUS(30_000_000, 5),
-    SIX(2_000_000_000, 6),
+    ONE(1, 0),
+    TWO(2, 0),
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    BONUS(5, 30_000_000),
+    SIX(6, 2_000_000_000),
     ;
 
-    private final int amounts;
     private final int matchCount;
+    private final int amounts;
 
-    WinningLotto(int amounts, int matchCount) {
-        this.amounts = amounts;
+    WinningLotto(int matchCount, int amounts) {
         this.matchCount = matchCount;
+        this.amounts = amounts;
     }
 
     public static WinningLotto from(int matchCount) {

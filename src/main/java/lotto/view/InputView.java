0123bc2c7cbe8a2433736a342b6a0bff0067a89a
seokmyungham <hellotjraud@gmail.com>
2023-11-06 17:08:35 +0900

    refactor(InputView): 유효성 검증과 파싱을 처리하는 InputValidator 메서드 호출
    InputValidator 정적 메서드를 호출하여 각 문자열 입력을 유효성 검증 및 파싱한 뒤 반환하도록 코드를 수정하였습니다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,24 +1,27 @@
 package lotto.view;
 
+import static lotto.view.InputValidator.*;
+
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static String inputPurchaseAmount() {
+    public static int inputPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
-        return Console.readLine();
+        return validateAndParseInput(Console.readLine());
     }
 
-    public static String inputWinningNumbers() {
+    public static ArrayList<Integer> inputWinningNumbers() {
         System.out.println(WINNING_NUMBERS_MESSAGE);
-        return Console.readLine();
+        return validateAndParseNumbersInput(Console.readLine());
     }
 
-    public static String inputBonusNumber() {
+    public static int inputBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
-        return Console.readLine();
+        return validateAndParseInput(Console.readLine());
     }
 }

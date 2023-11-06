d0af0076909d4f4a00ebebf1f7fba6323611e661
HYUNJUN\sajun <sajun28@naver.com>
2023-11-07 00:06:38 +0900

    Feat: "당첨번호 비교 기능 추가"

---
+++ src/main/java/lotto/model/Intersection.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+import org.w3c.dom.CDATASection;
+
+import java.util.*;
+
+public class Intersection {
+
+    public List<Integer> winNum(List<Integer> winNum, List<Set> lottoNum) {
+        HashSet<Integer> convert = new HashSet<>(winNum);
+        List<Integer> intersectionCount = new ArrayList<>();
+        for (Set intersection : lottoNum) {
+            intersection.retainAll(convert);
+            intersectionCount.add(intersection.size());
+        }
+        return intersectionCount;
+    }
+}

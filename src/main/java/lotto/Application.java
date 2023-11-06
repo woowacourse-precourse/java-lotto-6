22c22fa272288074a2b0fad595d00b6f59026501
yubin <dbqls2821@gmail.com>
2023-11-06 18:11:27 +0900

    fix: Application 클래스 내부 구현 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,19 @@
 package lotto;
 
 import lotto.domain.LottoServer;
+import lotto.domain.LottosConverter;
+import lotto.domain.StatisticsConverter;
 import lotto.infra.RandomLottoGenerator;
 import lotto.io.LottoInput;
 import lotto.io.LottoOutput;
 
 public class Application {
     public static void main(String[] args) {
-        LottoServer server = new LottoServer(new LottoInput(), new LottoOutput(), new RandomLottoGenerator());
+        LottoServer server = new LottoServer(
+                new LottoInput(),
+                new LottoOutput(new LottosConverter(), new StatisticsConverter()),
+                new RandomLottoGenerator()
+        );
         server.run();
     }
 }

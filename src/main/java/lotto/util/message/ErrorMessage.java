3dfc6aa860fcbe6a10e899d4967c5be17922805e
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-07 01:15:44 +0900

    refactor: 메세지 서식자 변경

--- src/main/java/lotto/util/message/ErrorMessage.java
+++ src/main/java/lotto/util/message/ErrorMessage.java
@@ -2,13 +2,13 @@ package lotto.util.message;
 
 public enum ErrorMessage implements MessageFormatter{
 
-	LOTTO_SIZE_ERROR("로또의 사이즈는 %s이여야 합니다."),
+	LOTTO_SIZE_ERROR("로또의 사이즈는 %d이여야 합니다."),
 	LOTTO_NUMBER_DUPLICATE_ERROR("로또 번호는 중복되지 말아야 합니다."),
 	LOTTO_BONUS_NUMBER_DUPLICATE_ERROR("당첨 번호와 보너스 번호는 중복되지 말아야 합니다."),
-	LOTTO_NUMBER_RANGE_ERROR("로또 번호는 %s부터 %s 사이의 숫자여야 합니다."),
+	LOTTO_NUMBER_RANGE_ERROR("로또 번호는 %d부터 %d 사이의 숫자여야 합니다."),
 	ZERO_OR_NEGATIVE_NUMBER_ERROR("0보다 큰 수여야 합니다."),
 	MONEY_UNIT_ERROR("돈의 단위는 %,d입니다."),
-	INPUT_ANSWER_LOTTO_NUMBER_ERROR(",로 구분하여 %s개의 숫자를 입력해야합니다."),
+	INPUT_ANSWER_LOTTO_NUMBER_ERROR(",로 구분하여 %d개의 숫자를 입력해야합니다."),
 	INPUT_LETTER_ERROR("문자가 아닌 숫자만 입력해야 합니다."),
 	INPUT_EMPTY_ERROR("값을 입력해야 합니다."), 
 	PREFIX("[ERROR] ");
--- src/main/java/lotto/util/message/ProgressMessage.java
+++ src/main/java/lotto/util/message/ProgressMessage.java
@@ -3,7 +3,7 @@ package lotto.util.message;
 public enum ProgressMessage implements MessageFormatter{
 
 	INPUT_MONEY("구입금액을 입력해 주세요."),
-	BUYED_LOTTO_NUMBER("%s개를 구매했습니다."),
+	BUYED_LOTTO_NUMBER("%d개를 구매했습니다."),
 	INPUT_ANSWER_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
 	INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 

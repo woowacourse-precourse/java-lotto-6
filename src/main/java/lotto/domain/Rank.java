716129fbfd7b95fc5688a80294170bd354302a39
hlmg <devhlmg@gmail.com>
2023-11-07 03:30:25 +0900

    refactor: 등수 계산 캐시 적용

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,24 +1,43 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.EnumMap;
-import java.util.Map.Entry;
+import java.util.Optional;
 
 public enum Rank {
+    NONE(0, false, 0L),
     FIFTH(3, false, 5_000L),
     FOURTH(4, false, 50_000L),
     THIRD(5, false, 1_500_000L),
     SECOND(5, true, 30_000_000L),
-    FIRST(6, false, 2_000_000_000L),
-    NONE(0, false, 0L);
+    FIRST(6, false, 2_000_000_000L);
 
     private final int matchCount;
     private final boolean hasBonus;
     private final long prize;
+    private static final Rank[] scoreCache = new Rank[20];
+
+    static {
+        Arrays.stream(Rank.values())
+                .forEach(rank -> {
+                    int score = getScore(rank.matchCount, rank.hasBonus);
+                    scoreCache[score] = rank;
+                    scoreCache[score + 1] = rank;
+                });
+    }
 
     Rank(int matchCount, boolean hasBonus, long prize) {
         this.matchCount = matchCount;
         this.hasBonus = hasBonus;
         this.prize = prize;
+    }
+
+    private static int getScore(int matchCount, boolean hasBonus) {
+        int score = matchCount * 2;
+        if (hasBonus) {
+            score++;
+        }
+        return score;
     }
 
     public static Rank of(Lotto lotto, WinningTicket winningTicket) {
@@ -28,30 +47,14 @@ public enum Rank {
     }
 
     private static Rank of(int matchCount, boolean hasBonusNumber) {
-        if (matchCount == 3) {
-            return Rank.FIFTH;
-        }
-        if (matchCount == 4) {
-            return Rank.FOURTH;
-        }
-        if (matchCount == 5 && !hasBonusNumber) {
-            return Rank.THIRD;
-        }
-        if (matchCount == 5) {
-            return Rank.SECOND;
-        }
-        if (matchCount == 6) {
-            return Rank.FIRST;
-        }
-        return NONE;
+        int score = getScore(matchCount, hasBonusNumber);
+        return Optional.ofNullable(scoreCache[score]).orElse(NONE);
     }
 
-    public static long getTotalPrize(EnumMap<Rank, Integer> rankToCount) {
-        long totalPrize = 0;
-        for (Entry<Rank, Integer> rankToCountEntry : rankToCount.entrySet()) {
-            totalPrize += rankToCountEntry.getKey().prize * rankToCountEntry.getValue();
-        }
-        return totalPrize;
+    public static long getTotalPrize(EnumMap<Rank, Integer> rankResult) {
+        return rankResult.entrySet().stream()
+                .mapToLong(entry -> entry.getKey().prize * entry.getValue())
+                .sum();
     }
 
     public int getMatchCount() {

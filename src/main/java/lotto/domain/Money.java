81d144414cab6fddc93fbed4a0685fcf65520859
gumgu <bienlee@naver.com>
2023-11-06 21:52:12 +0900

    [Refactor] 에러 메시지: enum으로 관리하지 않고 예외 발생하는 곳에서 직접 서술하도록 변경

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import lotto.ui.message.ErrorMessage;
-
 public class Money {
 
     private final Integer amount;
@@ -13,11 +11,11 @@ public class Money {
 
     private void validate(Integer amount) {
         if (amount < 1000) {
-            throw new IllegalArgumentException(ErrorMessage.LESS_THAN_THOUSAND.getComment());
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 이상만 입력 가능합니다.");
         }
 
         if (amount % 1000 != 0) {
-            throw new IllegalArgumentException(ErrorMessage.NON_THOUSAND_UNIT.getComment());
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로만 입력 가능합니다.");
         }
     }
 

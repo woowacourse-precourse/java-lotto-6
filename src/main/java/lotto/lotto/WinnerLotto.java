a63ccb8a37457668bd38de34fb6faf4976c6efa7
gimdongha <d11210920@gmail.com>
2023-11-06 15:57:38 +0900

    feat(WinnerLotto) : 로직 추가
    - 생성자에 보너스번호에 대한 검증 로직 추가

--- src/main/java/lotto/lotto/WinnerLotto.java
+++ src/main/java/lotto/lotto/WinnerLotto.java
@@ -1,5 +1,7 @@
 package lotto.lotto;
 
+import static lotto.constants.ErrorConstants.NOTINARRANGEERROR;
+
 public class WinnerLotto {
     private Lotto lotto;
     private int bonusNum;
@@ -7,9 +9,14 @@ public class WinnerLotto {
 
     public WinnerLotto(Lotto lotto, int bonusNum) {
         this.lotto = lotto;
+        validate(bonusNum);
         this.bonusNum = bonusNum;
     }
 
+    private void validate(int bonusNum){
+        if (bonusNum < 1 || bonusNum > 45)
+            throw new IllegalArgumentException(NOTINARRANGEERROR.toString());
+    }
     public Lotto getLotto() {
         return lotto;
     }

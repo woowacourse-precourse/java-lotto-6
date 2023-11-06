6bba5832f78236e6877c59eac2d7322d228f3c2d
jooda00 <dragon2000a@gmail.com>
2023-11-06 10:07:13 +0900

    test(PlayerTest): 구매 금액을 로또 개수로 변환 테스트

---
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player(3000);
+    }
+
+    @DisplayName("구매 금액을 로또 개수로 변환햔다.")
+    @Test
+    void convertAmountToLottoCount() {
+       int count = 3;
+
+       assertThat(count).isEqualTo(player.convertToUnit());
+    }
+
+}

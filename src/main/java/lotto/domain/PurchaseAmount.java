2fc0c0dffcd8d98580baeb42d9915ee76ff4027d
jae woo Jung <awfjol2008@naver.com>
2023-11-06 17:59:51 +0900

    feat: 구입 금액이 1,000원 단위인지 검증하는 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static java.lang.Integer.parseInt;
+
 public class PurchaseAmount {
 
     private Integer purchaseAmount;
@@ -12,6 +14,12 @@ public class PurchaseAmount {
         if (purchaseAmount.chars()
                 .anyMatch(ch -> !Character.isDigit(ch))) {
             throw new IllegalArgumentException("구입 금액은 정수이어야 합니다.");
+        }
+    }
+
+    private static void validatePurchaseUnit(String purchaseAmount) {
+        if (parseInt(purchaseAmount) % 1000 != 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위이어야 합니다.");
         }
     }
 }

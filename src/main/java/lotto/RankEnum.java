ed180cca7ed3d7c44523a78c0e7e4d3b49565741
rakyun1 <dbskim234@naver.com>
2023-11-06 05:26:11 -0700

    Refactor(RankEnum): add Functoins and prize 이넘에 대해 더 자세하게 공부하여 이넘이 더 활용도가 높겠다는 생각이 들어 이넘에 등수에 해당하는 값을 추가하고, matchCount와 prize를 변수로 두고, getter를 추가하였습니다. 그리고 화면 출력에 필요한 메소드들을 생성하였습니다.

--- src/main/java/lotto/RankEnum.java
+++ src/main/java/lotto/RankEnum.java
@@ -1,10 +1,39 @@
 package lotto;
 
 public enum RankEnum {
-	FIRST,
-	SECOND,
-	THIRD,
-	FORTH,
-	FIFTH,
-	NONE
+
+	NONE(0, 0),
+
+	FIFTH(3, 5_000),
+
+	FORTH(4, 50_000),
+
+	THIRD(5, 1_500_000),
+
+	SECOND(5, 30_000_000),
+	FIRST(6, 2_000_000_000);
+
+	private final int matchCount;
+	private final int prize;
+
+	RankEnum(int matchCount, int prize) {
+		this.matchCount = matchCount;
+		this.prize = prize;
+	}
+
+	public int getMatchCount() {
+		return matchCount;
+	}
+
+	public int getPrize() {
+		return prize;
+	}
+
+	public String getFormattedPrize() {
+		return String.format("%,d", prize);
+	}
+
+	public boolean hasBonus() {
+		return this == RankEnum.SECOND;
+	}
 }

9719821fa7c978dceb8a19f14521e48429eaea4e
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-06 20:11:35 +0900

    Refactor: 고침 출력문 main 함수로 이동
    확장 가능성을 고려하여 출력문을 main 함수로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User user = new User();
+        System.out.println("구입금액을 입력해 주세요.");
+        int volume = user.inputPurchasingVolume();
+        List<Lotto> allLotto = user.getLottoAsMuchAsVolume(volume);
+        for(Lotto lotto : allLotto){
+            System.out.println(lotto.getNumbers().toString());
+        }
+        MatchingMachine machine = new MatchingMachine();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumber = user.inputWinningNumbers();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNum = user.inputBonusNumber();
+        List<MatchingCount> matchingCounts = machine.countAllLottoMatchingNumbers(allLotto,winningNumber,bonusNum);
+        System.out.println(matchingCounts.toString());
+        Calculator calculator = new Calculator();
+        int sumPrize = calculator.getProfitMoney(matchingCounts);
+        System.out.println(sumPrize);
+        double profitPercentage = calculator.getProfitPercentage(sumPrize, volume);
+        System.out.println(profitPercentage);
     }
 }

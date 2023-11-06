643bf742a2c627275642dbca10a96dc5e58304c6
U-keun Song <sukeun319@gmail.com>
2023-11-06 15:41:00 +0900

    Feat : InputService 클래스 생성
    입력과 관련된 기능을 수행하는 클래스를 생성했다.

---
+++ src/main/java/lotto/service/InputService.java
@@ -0,0 +1,76 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.input.InputBonusNumber;
+import lotto.model.input.InputMoney;
+import lotto.model.input.InputWinnerNumbers;
+import lotto.model.Lotto;
+import lotto.model.LottoBonusPair;
+import lotto.view.LottoInputView;
+import lotto.view.View;
+
+public class InputService {
+    private final LottoInputView lottoInputView;
+
+    InputService(View lottoInputView) {
+        this.lottoInputView = (LottoInputView) lottoInputView;
+    }
+
+    public static InputService getInstance(View lottoInputView) {
+        return new InputService(lottoInputView);
+    }
+
+    public Integer getInputMoney() {
+        lottoInputView.requestMoney();
+        return getValidMoney().sendInputData();
+    }
+
+    public Lotto getInputWinnerNumbers() {
+        lottoInputView.requestWinnerNumbers();
+        return getValidNumbers().sendInputData();
+    }
+
+    public LottoBonusPair getLottoBonusPair(Lotto winnerNumber) {
+        lottoInputView.requestBonusNumber();
+        return getValidInputBonusNumber(winnerNumber);
+    }
+
+    private InputMoney getValidMoney() {
+        InputMoney inputMoney;
+        try {
+            String input = Console.readLine();
+            inputMoney = InputMoney.getInstance(input);
+        } catch (IllegalArgumentException e) {
+            return getValidMoney();
+        }
+
+        return inputMoney;
+    }
+
+    private InputWinnerNumbers getValidNumbers() {
+        InputWinnerNumbers inputWinnerNumbers;
+        try {
+            String input = Console.readLine();
+            inputWinnerNumbers = InputWinnerNumbers.getInstance(input);
+        } catch (IllegalArgumentException e) {
+            return getValidNumbers();
+        }
+
+        return inputWinnerNumbers;
+    }
+
+    private LottoBonusPair getValidInputBonusNumber(Lotto winnerNumber) {
+        InputBonusNumber inputBonusNumber;
+        LottoBonusPair lottoBonusPair;
+        try {
+            String input = Console.readLine();
+            inputBonusNumber = InputBonusNumber.getInstance(input);
+            lottoBonusPair = LottoBonusPair.getInstance(winnerNumber, inputBonusNumber.sendInputData());
+        } catch (IllegalArgumentException e) {
+            return getValidInputBonusNumber(winnerNumber);
+        }
+
+        Console.close();
+        return lottoBonusPair;
+    }
+}

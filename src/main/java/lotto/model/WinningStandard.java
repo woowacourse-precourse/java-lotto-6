d08f7856f078e9b7ea104db62fdf84c569a4de15
k9want <zndwlrnjr26@daum.net>
2023-11-07 00:33:52 +0900

    refactor: isMatchBonus 삭제

--- src/main/java/lotto/model/WinningStandard.java
+++ src/main/java/lotto/model/WinningStandard.java
@@ -3,21 +3,19 @@ package lotto.model;
 import java.util.List;
 
 public enum WinningStandard {
-    FIRST(2_000_000_000, 6, false),
-    SECOND(30_000_000, 5, true),
-    THIRD(1_500_000, 5, false),
-    FORTH(50_000, 4, false),
-    FIFTH(5_000, 3, false),
-    ETC(0, 0, false);
+    FIRST(2_000_000_000, 6),
+    SECOND(30_000_000, 5),
+    THIRD(1_500_000, 5),
+    FORTH(50_000, 4),
+    FIFTH(5_000, 3),
+    ETC(0, 0);
 
     private final int winningAmount;
     private final int matchCount;
-    private final boolean isMatchBonus;
 
-    WinningStandard(int winningAmount, int matchCount, boolean isMatchBonus) {
+    WinningStandard(int winningAmount, int matchCount) {
         this.winningAmount = winningAmount;
         this.matchCount = matchCount;
-        this.isMatchBonus = isMatchBonus;
     }
 
     public static WinningStandard checkMatchNumbers(Lotto lotto, WinningLotto winningLotto) {
@@ -47,10 +45,6 @@ public enum WinningStandard {
             }
         }
         return ETC;
-    }
-
-    public boolean isMatchBonus() {
-        return isMatchBonus;
     }
 
     public int getMatchCount() {

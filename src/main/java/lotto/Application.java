a240ebe66503319504d39b23e8a03b5855e10f4e
sinminseok12 <sinminseok12@naver.com>
2023-11-06 19:57:56 +0900

    feat(LottoController): 프로그램 흐름을 제어할 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
     }
 }

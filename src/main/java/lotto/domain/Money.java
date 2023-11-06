fc2045bcea58b4fff5fb6513cd64545d52c5c2b3
oyoungsun <oyoungsun1030@gmail.com>
2023-11-06 16:21:45 +0900

    fix(Money) : Error 내용 출력 에러 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -17,10 +17,12 @@ public class Money {
 
     private static void validate(int money) {
         if (isDividedByPrice(money)) {
-            throw new IllegalArgumentException(String.format("[ERROR] 로또 구입 금액은 %d원 단위입니다.", LottoConstants.PRICE));
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 구입 금액은 %d원 단위입니다.", LottoConstants.PRICE.getConstants()));
         }
         if (isInMinMoney(money)) {
-            throw new IllegalArgumentException(String.format("[ERROR] 로또 최소 구입 금액은 %d원 입니다.", LottoConstants.PRICE));
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 최소 구입 금액은 %d원 입니다.", LottoConstants.PRICE.getConstants()));
         }
     }
 

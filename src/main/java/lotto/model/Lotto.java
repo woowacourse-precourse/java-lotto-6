489dd60183a4fa9b112c3c5733749205dcfff7d7
hoyeon <highoption@naver.com>
2023-11-06 22:50:55 +0900

    feat(Lotto) : 로또 번호를 검증하는 isDuplicate 메서드 생성
    - 로또 번호는 중복이 있으면 안되기때문에 검증 메서드 생성

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         isCorrectRange(numbers);
         isCorrectSize(numbers);
+        isDuplicate(numbers);
     }
 
     private static void isCorrectSize(List<Integer> numbers) {
@@ -29,5 +31,15 @@ public class Lotto {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         });
+    }
+
+    private static void isDuplicate(List<Integer> numbers) {
+        List<Integer> duplicate = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (duplicate.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 있으면 안됩니다.");
+            }
+            duplicate.add(number);
+        }
     }
 }

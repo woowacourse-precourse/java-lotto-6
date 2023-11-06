bb2578e65dcc09355d2783bda35508534af98cf6
hil679 <hil679922@gmail.com>
2023-11-06 18:25:06 +0900

    Refactor(WinnerRank): WinnerRank enum 클래스에 일치 개수 추가

--- src/main/java/lotto/util/WinnerRank.java
+++ src/main/java/lotto/util/WinnerRank.java
@@ -1,24 +1,31 @@
 package lotto.util;
 
 public enum WinnerRank {
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE_WITHOUT_BOUNUS(5, 1500000),
-    SIX(6, 2000000000),
-    FIVE_WITH_BONUS(7, 30000000);
+    THREE(3, 5000, 3),
+    FOUR(4, 50000, 4),
+    FIVE_WITHOUT_BOUNUS(5, 1500000, 5),
+    SIX(6, 2000000000, 6),
+    FIVE_WITH_BONUS(7, 30000000, 5);
 
     int value;
     int prizeMoney;
+    int sameNumberCount;
 
-    private WinnerRank(int value, int prizeMoney) {
+    private WinnerRank(int value, int prizeMoney, int sameNumberCount) {
         this.value = value;
         this.prizeMoney = prizeMoney;
+        this.sameNumberCount = sameNumberCount;
     }
 
     public int getValue() {
         return value;
     }
+
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public int getSameNumberCount() {
+        return sameNumberCount;
     }
 }

9cb2e7f97b146142b4c1c40932628ce627609e46
heiler <threepebbles@naver.com>
2023-11-06 22:17:09 +0900

    Feat: 사용되지 않는 에러메세지 삭제 및 새로운 에러메세지 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,11 +1,11 @@
 package lotto.constant;
 
 public enum ErrorMessage {
-    NOT_POSITIVE_INTEGER("양의 정수가 아닙니다."),
-    NOT_DIVIDED_BY_1000("1000으로 나누어 떨어지는 수가 아닙니다."),
     SIZE_OF_LOTTO_IS_NOT_PROPER("번호의 개수가 6개가 아닙니다."),
     NUMBER_NOT_IN_RANGE("올바른 범위의 수가 아닙니다."),
-    HAS_DUPLICATED_NUMBER("중복 값이 존재합니다.");
+    HAS_DUPLICATED_NUMBER("중복 값이 존재합니다."),
+    NOT_PROPER_OBJECT_TYPE("올바른 객체 타입이 아닙니다."),
+    NOT_PROPER_LOTTO_TICKET_PRICE("올바른 로또 구입 금액이 아닙니다.");
 
     private static final String MESSAGE_HEADER = "[ERROR]";
     private static final String WHITE_SPACE = " ";

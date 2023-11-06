a7295903c4aef0698faca8f22d04e1e342e40789
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-06 14:33:06 +0900

    test(WiningLottoNumbers) : 당첨 갯수 알아내는 기능 테스트 작성

--- src/test/java/lotto/domain/WiningLottoNumbersTest.java
+++ src/test/java/lotto/domain/WiningLottoNumbersTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static lotto.ErrorMessage.BONUS_DUPLICATION_ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -18,5 +19,19 @@ public class WiningLottoNumbersTest {
         assertThatThrownBy(() -> new WiningLottoNumbers(lotto, bonus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(BONUS_DUPLICATION_ERROR_MESSAGE.get());
+    }
+
+    @Test
+    @DisplayName("당첨 갯수를 알아낸다.")
+    void testCountWining() {
+        Lotto winingLotto = new Lotto(List.of(5, 10, 15, 20, 25, 30));
+        Bonus winingBonus = new Bonus(22);
+        WiningLottoNumbers winingLottoNumbers = new WiningLottoNumbers(winingLotto, winingBonus);
+        Lotto lotto = new Lotto(List.of(5, 25, 11, 22, 33, 44));
+
+        int result = winingLottoNumbers.countWining(lotto);
+        int expected = 3;
+
+        assertThat(result).isEqualTo(expected);
     }
 }

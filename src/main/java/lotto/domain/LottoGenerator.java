ace5979be08458fb0c196e92e5687f48658bbb7a
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-06 18:40:27 +0900

    [FEAT] generateLottos 함수 구현
    사용자가 구매한 가격을 1000으로 나눈 값 : lottoCount.
    lottoCount만큼 로또를 만들어주는 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,20 +1,29 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoGenerator {
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
     private static final int LOTTO_SIZE = 6;
 
-    private List<Integer> generateLottoNumbers() {
+    private static List<Integer> generateLottoNumbers() {
         return sortLottoNumbers(Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_SIZE));
     }
 
-    private List<Integer> sortLottoNumbers(List<Integer> numbers) {
+    private static List<Integer> sortLottoNumbers(List<Integer> numbers) {
         Collections.sort(numbers);
         return numbers;
+    }
+
+    public static List<Lotto> generateLottos(int lottoCount) {
+        return IntStream.range(0, lottoCount)
+                .mapToObj(i -> new Lotto(generateLottoNumbers()))
+                .collect(Collectors.toList());
     }
 }

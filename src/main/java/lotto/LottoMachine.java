f8e114769896052c78c864f8f637f2cf1e106ab4
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 12:32:29 +0900

    feat(lottoMachine): 사용자가 입력한 금액에 맞는 로또를 발행하는 메서드 작성
    - LottoNumberGenerator으로부터 로또 숫자를 입력받아 새 로또를 발행하는 메서드
    - 로또 숫자 발행 시 오름차순 정렬
    - 금액에 맞는 수량만큼 로또를 발행하는 LottoMachine 메서드 작성

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,11 +1,30 @@
 package lotto;
 
 
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoMachine {
     private final int LOTTO_PRICE = 1_000;
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public LottoMachine(LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
 
     public int calculateLottoQuantity(int amount) {
         return amount / LOTTO_PRICE;
+    }
+
+    public Lotto buyLotto() {
+        return new Lotto(lottoNumberGenerator.generateLottoNumbers());
+    }
+
+    public List<Lotto> buyLottos(int amount) {
+        int quantity = calculateLottoQuantity(amount);
+
+        return IntStream.range(0, quantity)
+                .mapToObj(i -> buyLotto())
+                .toList();
     }
 }
--- src/main/java/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -1,10 +1,18 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
     public List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        sortLottoNumbers(lottoNumbers);
+
+        return lottoNumbers;
+    }
+
+    private void sortLottoNumbers(List<Integer> lottoNumbers) {
+        Collections.sort(lottoNumbers);
     }
 }

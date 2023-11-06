eeb8e7d5828a357ef7c22b52976c6f43948e1474
leesh <qwqw8019@naver.com>
2023-11-06 15:50:20 +0900

    feat: 당첨 정보 dto 생성 메소드 구현

--- src/main/java/lotto/domain/dto/WinningResult.java
+++ src/main/java/lotto/domain/dto/WinningResult.java
@@ -1,7 +1,14 @@
 package lotto.domain.dto;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import lotto.constant.Prize;
 
 public record WinningResult(LinkedHashMap<Prize, Integer> prizes) {
+    public static WinningResult of(Prize... prizes) {
+        WinningResult result = new WinningResult(new LinkedHashMap<>());
+        Arrays.stream(prizes)
+                .forEach(prize -> result.prizes.put(prize, 0));
+        return result;
+    }
 }

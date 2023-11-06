3a6a17d459564acc0c14e9be2afd2c0a2eaf9e1a
Infikei <hckhong@naver.com>
2023-11-06 22:22:45 +0900

    feat: 오류 발생에 대한 메시지 추가
    - 오류 메시지 상수로 선언

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,10 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final String WRONG_SIZE_ERROR_MESSAGE = "6개의 정수를 입력해주세요.";
+    private static final String OUT_OF_RANGE_ERROR_MESSAGE = "1 이상 45 이하의 정수를 입력해주세요.";
+    private static final String DUPLICATE_NUMBERS_ERROR_MESSAGE = "서로 다른 정수를 입력해주세요.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,11 +18,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (!isSixNumbers(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(WRONG_SIZE_ERROR_MESSAGE);
         } else if (!isInLottoRange(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
         } else if (hasDuplicateNumbers(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_NUMBERS_ERROR_MESSAGE);
         }
     }
 

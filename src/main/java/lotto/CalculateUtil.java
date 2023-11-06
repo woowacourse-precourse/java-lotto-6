c5c16279be11a3b29e480b0e58a91c141cad6efd
giljin <68088970+giljin@users.noreply.github.com>
2023-11-06 17:17:57 +0900

    feat(CalculateUtil): 소수 원하는 자리수 반올림

--- src/main/java/lotto/CalculateUtil.java
+++ src/main/java/lotto/CalculateUtil.java
@@ -2,9 +2,14 @@ package lotto;
 
 public class CalculateUtil {
     private static final int PERCENTAGE_MULTIPLE_VALUE = 100;
-
+    private static final int DEFAULT_POW_VALUE = 10;
     public static double calculatePercentage(double a, double b){
         return a / b * PERCENTAGE_MULTIPLE_VALUE;
     }
 
+    public static double round(double number, int decimalDigit){
+        decimalDigit--;
+        double digit = Math.pow(DEFAULT_POW_VALUE, decimalDigit);
+        return Math.round(number * digit) / digit;
+    }
 }

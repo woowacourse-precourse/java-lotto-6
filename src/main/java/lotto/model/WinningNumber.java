26b63ca964d660ee21522abb80164bc0cff00e0c
HongYeseul <ghd8119@naver.com>
2023-11-06 14:01:17 +0900

    :sparkles: feat(WinningNumber): 당첨 번호 입력 기능 추가 - "," 기준으로 당첨 번호 입력 된 값 split 하여 Lotto 객체로 만들기

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,4 +1,22 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WinningNumber {
+    Lotto answer;
+    public WinningNumber(String winningNumber) {
+        List<Integer> lotto
+                = Arrays.stream(winningNumber.split(","))
+                .map(Integer::parseInt)
+                .toList();
+        answer = new Lotto(lotto);
+    }
+
+    @Override
+    public boolean equals(Object expectedNumbers){
+        return answer.equals(expectedNumbers);
+    }
 }

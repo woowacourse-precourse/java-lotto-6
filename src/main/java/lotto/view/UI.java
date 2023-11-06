1244ee3f5888b0c31e26958743246f760f3b3b83
doka <posmane5@gmail.com>
2023-11-06 22:26:11 +0900

    feat(UI) : "view) 당첨 번호 입력" 기능을 담당하는 UI클래스에 입력 기능 추가
    - InputWinningNumbers : 입력 안내 메시지를 출력후 당첨 로또 번호를 입력받는 메소드

--- src/main/java/lotto/view/UI.java
+++ src/main/java/lotto/view/UI.java
@@ -9,9 +9,10 @@ public class UI {
 
     public static String sendLottoPurchaseRequest() {
         displayMessage(Instructions.MESSAGE_OF_ENTERING_LOTTO_AMOUNT);
-        String lottoPurchaseRequest  = Console.readLine();
+        String lottoPurchaseRequest = Console.readLine();
         return lottoPurchaseRequest;
     }
+
     public static void displayMessage(final Instructions instructions) {
         System.out.println(instructions.getPrompts());
     }
@@ -22,13 +23,15 @@ public class UI {
 
 
     public static void displayLottoTicketsHistory(List<Lotto> LottoTickets) {
-        //로또구매 내역 객체인 List<Lotto> 형태의 객체를 요구조건 대로 출력해야 된다.
-        //구매 갯수인 List의 크기 size()메소드 이용
-        //구매 내역은 리스트의 각 요소인 Lotto 객체 내부의 로또 리스트를 읽는다. -> get 메소드 필요하다
         displayFormatMessage(Instructions.MESSAGE_OF_PURCHASE_COUNT, LottoTickets.size());
-        for(Lotto lotto : LottoTickets) {
+        for (Lotto lotto : LottoTickets) {
             System.out.println(lotto.getLottoNumbers());
         }
+    }
 
+    public static String InputWinningNumbers() {
+        displayMessage(Instructions.MESSAGE_OF_ENTERING_LOTTO_AMOUNT);
+        String WinningNumbers = Console.readLine();
+        return WinningNumbers;
     }
 }

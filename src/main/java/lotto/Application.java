13b2ea2727ed65fac62f74c88c9aa8ced424fbb4
7ngenious <zxvs1218@gmail.com>
2023-11-07 00:13:40 +0900

    feat: Modified Application to run Program

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.Controller.LottoController;
+import lotto.View.LottoView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoView view = new LottoView();
+        LottoController controller = new LottoController(view);
+
+        controller.runGame();
     }
 }

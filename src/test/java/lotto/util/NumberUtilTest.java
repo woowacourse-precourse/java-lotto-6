de7277221f22631b00d155e40d3eecae8dbf9398
donglee99 <jhnj741@naver.com>
2023-11-06 22:30:02 +0900

    test : 로또 생성 및 당첨 로또 입력 테스트 코드

--- src/test/java/lotto/util/NumberUtilTest.java
+++ src/test/java/lotto/util/NumberUtilTest.java
@@ -3,9 +3,10 @@ package lotto.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 class NumberUtilTest {
@@ -15,5 +16,21 @@ class NumberUtilTest {
     void 랜덤_숫자_생성_성공_테스트() {
         List<Integer> numbers = NumberUtil.numberGenerator(1, 45, 6);
         assertThat(numbers.size()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("숫자 범위 검증 성공 케이스")
+    void 숫자_범위_검증_성공_테스트() {
+        List<Integer> numbers = Arrays.asList(1, 2, 6, 3, 22, 5);
+        assertThatCode(() -> NumberUtil.validateNumberRange(1, 45, numbers))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("숫자 범위 검증 실패 케이스")
+    void 숫자_범위_검증_실패_테스트() {
+        List<Integer> numbers = Arrays.asList(1, 2, 6, 60, 22, 5);
+        assertThatThrownBy(() -> NumberUtil.validateNumberRange(1, 45, numbers))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

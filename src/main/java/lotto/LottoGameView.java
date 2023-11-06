35dc7c82bc44dc189b7ed0c5ad91b401b30fde92
dukcode <duk9741@gmail.com>
2023-11-06 14:10:58 +0900

    feat: 로또 입출력 관련 인터페이스 추가

---
+++ src/main/java/lotto/LottoGameView.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.List;
+import lotto.dto.LottoBonusNumberCreateRequest;
+import lotto.dto.LottoPurchaseRequest;
+import lotto.dto.LottoWinningNumberCreateRequest;
+import lotto.dto.LottoWinningStatistics;
+
+public interface LottoGameView {
+    LottoPurchaseRequest inputPurchaseRequest();
+
+    void printPurchasedTickets(List<Lotto> purchasedLottoTickets);
+
+    LottoWinningNumberCreateRequest inputLottoWinningNumberCreateRequest();
+
+    LottoBonusNumberCreateRequest inputLottoBonusNumberCreateRequest();
+
+    void printWinningStatistics(LottoWinningStatistics lottoWinningStatistics);
+}

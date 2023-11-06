963ce1792efa33ece47b02a6e53878b5b563c823
JayG-5 <jg55552758@gmail.com>
2023-11-06 20:37:54 +0900

    feat: 문자열->리스트 파서, 유니크 확인 추가

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -3,6 +3,10 @@ package lotto.utils;
 import lotto.exception.LottoException;
 import lotto.exception.Message;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Parser {
 
     private static void validateWhiteSpace(String text){
@@ -18,5 +22,13 @@ public class Parser {
         }catch (NumberFormatException e){
             throw LottoException.of(Message.NUMBER_TYPE);
         }
+    }
+
+    public static List<Integer> parseStringToIntList(String text){
+        return Arrays.stream(text.split(",")).map(Parser::parseStringToInt).toList();
+    }
+
+    public static boolean isUnique(List<Integer> integerList){
+        return new HashSet<>(integerList).size() == integerList.size();
     }
 }

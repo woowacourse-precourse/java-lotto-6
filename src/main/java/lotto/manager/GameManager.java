18138df42a33d69df2ce310eb72ca26fd31fe801
문고은 <>
2023-11-07 04:10:58 +0900

    feat : 에러 상황 시 다시 입력 기능
    에러 상황 시 에러 메시지 출력 후 다시 입력 받는 기능 추가

--- src/main/java/lotto/manager/GameManager.java
+++ src/main/java/lotto/manager/GameManager.java
@@ -1,7 +1,9 @@
 package lotto.manager;
 
 import lotto.Lotto;
+import lotto.ProfitCalculator;
 import lotto.lottery.Bonus;
+import lotto.lottery.BuyingAmount;
 import lotto.lottery.Ticket;
 import lotto.lottery.Winning;
 
@@ -13,25 +15,37 @@ public class GameManager {
     public void game() {
         OutputManager outputManager = new OutputManager();
         InputManager inputManager = new InputManager();
+        ProfitCalculator profitCalculator = new ProfitCalculator();
 
         //구입 금액
         String buyingAmount = getBuyingAmount(inputManager, outputManager);
         List<List<Integer>> ticketNumbers = buyTicket(buyingAmount, outputManager);
-
+        //당첨 번호
         Winning winning = getWinningNumbers(inputManager, outputManager);
         List<Integer> winningNumbers = winning.getWinningNumbers();
 
-        Bonus bonus = getBonusNumber(inputManager, outputManager);
+        Bonus bonus = getBonusNumber(inputManager, outputManager, winningNumbers);
 
         Lotto lotto = new Lotto(winningNumbers);
 
-        Map<Integer, Integer> winningStatistics = lotto.winningStatistics(ticketNumbers);
+        Map<Integer, Integer> winningStatistics = lotto.winningStatistics(ticketNumbers, bonus.getBonus());
         outputManager.printWinningMessage(winningStatistics);
+
+        double profitRate = profitCalculator.calculateProfitRate(winningStatistics, buyingAmount);
+        outputManager.printProfitRate(profitRate);
     }
 
     private String getBuyingAmount(InputManager inputManager, OutputManager outputManager) {
         outputManager.printInputAmount();
-        return inputManager.getUserInput();
+        while (true) {
+            try {
+                String buyingAmountInput = inputManager.getUserInput();
+                BuyingAmount buyingAmount = new BuyingAmount(buyingAmountInput);
+                return buyingAmountInput;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private List<List<Integer>> buyTicket(String buyingAmount, OutputManager outputManager) {
@@ -39,20 +53,34 @@ public class GameManager {
         int ticketCount = ticket.getTicketCount();
         outputManager.printTicketCount(ticketCount);
         List<List<Integer>> ticketNumbers = ticket.getTicketNumbers();
+
         outputManager.printRandomNumber(ticketNumbers);
         return ticketNumbers;
     }
 
     private Winning getWinningNumbers(InputManager inputManager, OutputManager outputManager) {
         outputManager.printWinningNumbers();
-        String inputWinningNumbers = inputManager.getUserInput();
-        return new Winning(inputWinningNumbers);
+        while(true){
+            try {
+                String inputWinningNumbers = inputManager.getUserInput();
+                Winning winning = new Winning(inputWinningNumbers);
+                return winning;
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private Bonus getBonusNumber(InputManager inputManager, OutputManager outputManager) {
+    private Bonus getBonusNumber(InputManager inputManager, OutputManager outputManager, List<Integer> winningNumbers) {
         outputManager.printBonusNumber();
-        String bonusNumber = inputManager.getUserInput();
-        return new Bonus(bonusNumber);
+        while (true){
+            try{
+                String bonusNumber = inputManager.getUserInput();
+                Bonus bonus = new Bonus(bonusNumber, winningNumbers);
+                return bonus;
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
-
 }

dac4ade27eaa058d852bff6cb82421f087451967
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-06 17:20:25 +0900

    test: complete unit test for number generator

---
+++ src/test/java/lotto/LottoNumberGeneratorTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoNumberGeneratorTest {
+    private static final int START = 1;
+    private static final int END = 45;
+    private static final int SIZE = 6;
+
+    @DisplayName("숫자 생성기가 1~45 사이 정수 6개를 생성하는지 확인한다.")
+    @Test
+    public void createNumbers_test() {
+        // given
+        List<Integer> numbers = LottoNumberGenerator.createNumbers();
+
+        // when
+
+        // then
+        assertThat(numbers).hasSize(SIZE)
+                .allMatch(number -> number >= START && number <= END);
+    }
+}

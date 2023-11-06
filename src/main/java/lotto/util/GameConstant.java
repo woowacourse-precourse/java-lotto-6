5795bd3ce53a4671ab153be8e9e45cce74f634a8
minsub <chrkb1569@naver.com>
2023-11-06 17:32:09 +0900

    feat : 상수 목록 수정

--- src/main/java/lotto/util/GameConstant.java
+++ src/main/java/lotto/util/GameConstant.java
@@ -2,10 +2,10 @@ package lotto.util;
 
 // 게임 과정에서 사용하기 위한 상수들의 모음
 public enum GameConstant {
-    CONSTANT_LOTTO_PRICE(1_000), // 로또의 가격
     CONSTANT_LOTTO_MIN_NUMBER(1), // 로또 번호 최소값
     CONSTANT_LOTTO_MAX_NUMBER(45), // 로또 번호 최대값
-    CONSTANT_LOTTO_LENGTH(6) // 로또 번호 길이
+    CONSTANT_LOTTO_LENGTH(6), // 로또 번호 길이
+    CONSTANT_GRADE_LENGTH(5) // 총 등수
     ;
     private final int constant;
 

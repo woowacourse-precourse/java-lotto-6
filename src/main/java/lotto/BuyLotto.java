947469ede93a6d9b5057f1800bf673d01c997c9a
SeungGwa123 <skwan123@naver.com>
2023-11-06 21:34:11 +0900

    Feat: Add checkSameNumber
    로또를 구매할때 숫자 중복검사 해주는 함수 작성

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -1,2 +1,19 @@
-package lotto;public class BuyLotto {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuyLotto {
+    private static List<Lotto> lotto = new ArrayList<>();
+
+    public boolean checkSameNumber(List<Integer> paper, int number) {
+        boolean result = true;
+        for (int i = 0; i < paper.size(); i++) {
+            if (paper.get(i) == number) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }

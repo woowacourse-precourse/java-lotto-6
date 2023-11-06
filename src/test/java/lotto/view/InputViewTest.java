7129d3cb3c9590067e4ee4c83dc1d9685c2be728
Hugh <lee2317884@gmail.com>
2023-11-06 15:33:41 +0900

    test: 구입 금액 숫자가 아닌 경우 예외 처리 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.stream.Stream;
 import lotto.AppConfig;
 import lotto.view.reader.CustomReader;
@@ -17,12 +20,21 @@ class InputViewTest {
     private final InputView inputView = APP_CONFIG.inputView;
 
     @ParameterizedTest
+    @MethodSource("provideValuesForNotNumericException")
+    @DisplayName("숫자 이외의 값은 입력할 수 없다.")
+    void readPurchaseAmountTest(final String input) {
+        customReader.initInput(input);
+        assertThatThrownBy(inputView::readPurchaseAmount)
+                .isInstanceOf(NumberFormatException.class);
+    }
+
+    @ParameterizedTest
     @MethodSource("provideValuesForNormalInput")
     @DisplayName("로또 구입 금액 입력 테스트")
     void readPurchaseAmountTest(final String input, final int expected) {
         customReader.initInput(input);
         final int amount = inputView.readPurchaseAmount();
-        Assertions.assertThat(amount).isEqualTo(expected);
+        assertThat(amount).isEqualTo(expected);
     }
 
     private static Stream<Arguments> provideValuesForNormalInput() {
@@ -32,6 +44,16 @@ class InputViewTest {
                 Arguments.of("3333", 3333),
                 Arguments.of("0", 0),
                 Arguments.of("-1", -1)
+        );
+    }
+
+    private static Stream<Arguments> provideValuesForNotNumericException() {
+        return Stream.of(
+                Arguments.of("100-0"),
+                Arguments.of("20-00"),
+                Arguments.of(" er66-"),
+                Arguments.of(" "),
+                Arguments.of("")
         );
     }
 }

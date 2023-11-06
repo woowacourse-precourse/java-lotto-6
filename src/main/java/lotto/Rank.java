f11c78f1dc18f1dfd83d1805cad42420ae30c6db
donghoonyeom <dua5410773@naver.com>
2023-11-07 01:37:20 +0900

    feat(lotto): Rank 열거형 추가
    Rank 열거형은 로또 등수와 상금을 정의합니다. 각 등수에 해당하는 일치하는 번호의 개수와 상금이 정의되어 있습니다. `valueOf(int matchCount, boolean hasBonusNumber)` 메서드는 주어진 일치 번호 개수와 보너스 번호 여부에 따라 해당하는 Rank 값을 반환합니다. `calculateTotalPrize(Map<Rank, Integer> resultCount)` 메서드는 각 등수별 당첨 횟수를 받아 총 상금을 계산합니다.

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import java.util.Map;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Rank valueOf(int matchCount, boolean hasBonusNumber) {
+        if (matchCount == SECOND.matchCount && hasBonusNumber) {
+            return SECOND;
+        }
+        for (Rank rank : values()) {
+            if (matchCount == rank.matchCount) {
+                return rank;
+            }
+        }
+        return NONE;
+    }
+
+    public static int calculateTotalPrize(Map<Rank, Integer> resultCount) {
+        int totalPrize = 0;
+        for (Rank rank : Rank.values()) {
+            totalPrize += rank.getPrize() * resultCount.get(rank);
+        }
+        return totalPrize;
+    }
+}

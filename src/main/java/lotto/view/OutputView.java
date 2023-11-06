4c7950d6cc2bf31b9ef5dcb96afd34ff188f1900
hil679 <hil679922@gmail.com>
2023-11-06 17:50:07 +0900

    Feat(OutputView): 입력 가이드 메세지 기능 구현
    - "구입금액을 입력해 주세요." 문구 출력
    - "당첨 번호를 입력해 주세요." 문구 출력
    - "보너스 번호를 입력해 주세요." 문구 출력
    - "당첨 통계\n---" 문구 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class OutputView {
     public void printMoneyInputGuideMessage() {
-
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
     public void printLottoAmountMessage(int lottoAmount) {
@@ -18,15 +18,15 @@ public class OutputView {
     }
 
     public void printWinLottoNumbersInputGuide() {
-
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public void printBonusNumbersInputGuide() {
-        
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 
     public void printWinningResultGuideMessage() {
-
+        System.out.println("당첨 통계\n" + "---");
     }
 
     public void printWinResult(WinResult winResult) {

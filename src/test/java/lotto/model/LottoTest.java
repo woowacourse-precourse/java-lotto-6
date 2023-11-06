926ad278fdbb1ea78e3f1fe550772b04a31b156b
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-06 21:35:18 +0900

    test: 로또 생성 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,11 +1,11 @@
-package lotto;
+package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +24,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("우승 로또 번호 생성")
+    void winningLotto() {
+        List<Integer> winningNumber = List.of(1,2,3,4,5,6);
+        assertEquals(new Lotto(winningNumber).getNumbers(),winningNumber);
+    }
 }

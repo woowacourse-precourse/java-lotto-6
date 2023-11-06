e0844ed777b05a01f57b56efd1398409dc6daf66
cloudchamb3r <jizon0123@protonmail.com>
2023-11-06 16:13:01 +0900

    feat. now main invoke GameManager.start

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,11 @@
 package lotto;
 
+import lotto.game.GameManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        var m = new LottoMachine();
-        System.out.println("구입 금액을 입력해주세요.");
-        var lottos  = m.buyLottos();
-        m.goLotto(lottos);
+        LottoGame game = new LottoGame();
+        GameManager gameManager = new GameManager(game);
+        gameManager.start();
     }
 }

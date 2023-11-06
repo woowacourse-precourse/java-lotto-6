7b0c0e40ca5760a4f93ab8a6ff96843fedd55ff3
potatomoong <star901210@hanmail.net>
2023-11-07 01:32:24 +0900

    Feat: 당첨 번호 입력 받는 기능 구현

--- src/main/java/lotto/domain/BallNumber.java
+++ src/main/java/lotto/domain/BallNumber.java
@@ -1,4 +1,12 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BallNumber {
+    public static String[] inputWinningBall() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningBall = Console.readLine();
+        return winningBall.split(",");
+    }
+
 }

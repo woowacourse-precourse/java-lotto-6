a94f618f3fc232c4863ec480a51422ae0e9217f7
Infikei <hckhong@naver.com>
2023-11-07 03:10:50 +0900

    feat: OutputView 클래스 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,6 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String NUMBER_OF_LOTTOS_PURCHASED_PRINT_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS_PRINT_MESSAGE = "당첨 통계\n---";
+}

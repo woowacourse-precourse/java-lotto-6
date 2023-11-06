a45d2699187284b75cdf98a2cfffe65fc6007f6f
hyuna <monicajo0@naver.com>
2023-11-06 02:58:29 -0800

    style(Application): 띄어쓰기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,10 +6,10 @@ import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView  = new InputView();
+        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
-        LottoController lottoController = new LottoController(inputView,outputView);
+        LottoController lottoController = new LottoController(inputView, outputView);
         lottoController.start();
     }
 }

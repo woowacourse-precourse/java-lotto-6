f98f62fcb9b53195421f8c4709b3b035b1c67df2
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-06 23:40:02 +0900

    reafactor(LottoGenerator): 변수명, 로직 등 리펙터링 진행

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -5,38 +5,70 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+가import lotto.constants.messages.Error;
 
 public class LottoGenerator {
 
-    private final Integer price;
-    int n;
+    private Integer money;
+    private final int numberOfTickets;
 
-    public LottoGenerator(Integer price) {
-        validatePrice(price);
-        this.price = price;
-        this.n = price / 1000;
+
+    public LottoGenerator(String input) {
+        checkInputFormat(input);
+        int money = Integer.parseInt(input);
+        this.money = money;
+        this.numberOfTickets = money / 1000;
     }
+
 
     private List<Integer> generateLottoNumber() {
         Set<Integer> numbers = new HashSet<>();
-        while(numbers.size() < 6) {
+        while (numbers.size() < 6) {
             numbers.add(Randoms.pickNumberInRange(1, 45));
         }
         return new ArrayList<>(numbers.stream().toList());
     }
 
-    public Lottos wishMeLuck() {
+    public Lottos generateMyTickets() {
         Lottos lottos = new Lottos();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < numberOfTickets; i++) {
             lottos.add(new Lotto(generateLottoNumber()));
         }
         return lottos;
     }
 
-    private void validatePrice(Integer price) {
-        final int MAX = 1000000;
-        if (price > MAX) {
-            throw new IllegalArgumentException();
+
+    public void checkInputFormat(String input) {
+        isAllDigit(input);
+        isParsible(input);
+        isDividable(input);
+        isLessThanLowerBound(input);
+    }
+
+    private void isLessThanLowerBound(String input) {
+        if (Integer.parseInt(input) < 1000) {
+            throw new IllegalArgumentException(Error.MONEY_TOO_SMALL.getMessage());
+        }
+    }
+
+    private void isDividable(String input) {
+        if (Integer.parseInt(input) % 1000 > 0) {
+            throw new IllegalArgumentException(Error.NOT_DIVIDABLE.getMessage());
+        }
+
+    }
+
+    private void isParsible(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (final NumberFormatException e) {
+            throw new IllegalArgumentException(Error.MONEY_TOO_LARGE.getMessage());
+        }
+    }
+
+    private void isAllDigit(String input) {
+        if (!input.matches("[0-9]+")) {
+            throw new IllegalArgumentException(Error.NOT_DIGIT.getMessage());
         }
     }
 }

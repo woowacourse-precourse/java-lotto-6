edef9286b2ee7d2e33d362e5ecabb032edf9c8bf
Sunhee Kim <sunni372@icloud.com>
2023-11-07 01:22:06 +0900

    feat: 당첨 결과 생성 기능 구현
    일치하는 숫자 개수와 보너스 번호 포함 여부를 통해 당첨 결과를 생성합니다.

--- src/main/java/lotto/domain/Referee.java
+++ src/main/java/lotto/domain/Referee.java
@@ -1,14 +1,42 @@
 package lotto.domain;
 
+import java.util.HashMap;
 import java.util.List;
+import lotto.RankType;
 
 public class Referee {
+    private final HashMap<Integer, Integer> result;
+
+    public Referee() {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        for (int i = 1; i < 6; i++) {
+            result.put(i, 0);
+        }
+        this.result = result;
+    }
+
     public void compare(List<Lotto> lottos, Lotto winningLotto, int bonus) {
         Judgment judgment = new Judgment();
 
         for (Lotto lotto : lottos) {
             int correctCount = judgment.correctCount(lotto.getNumbers(), winningLotto.getNumbers());
             boolean hasBonus = judgment.hasBonus(lotto.getNumbers(), bonus);
+
+            if (correctCount > 2) {
+                setResult(findRank(correctCount, hasBonus));
+            }
         }
+    }
+
+    private void setResult(int rank) {
+        result.put(rank, result.get(rank) + 1);
+    }
+
+    private int findRank(int correctCount, boolean hasBonus) {
+        return RankType.findByCorrectCount(correctCount, hasBonus).getRank();
+    }
+
+    public HashMap<Integer, Integer> getResult() {
+        return result;
     }
 }

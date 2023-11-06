5d3b1036eaf40ada7a8c80840a6b07de66ac6814
Junobee25 <wnsdhqo@naver.com>
2023-11-07 02:35:14 +0900

    feat(OutputView): outputViewStatistics 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,18 @@
 package lotto.view;
 
 import lotto.configuration.InputMessage;
+import lotto.configuration.PrintMessage;
+import lotto.configuration.WinningLevel;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.StringJoiner;
 
 public class OutputView {
+
+    private final static String DASH = "-";
+    private final static String STATISTICS = "당첨 통계";
+
     public static void lottoNumbersResult(List<Integer> lotto) {
         StringJoiner joiner = new StringJoiner(", ", "[", "]");
         for (Integer number : lotto) {
@@ -23,5 +30,16 @@ public class OutputView {
         System.out.println();
         System.out.println(InputMessage.INPUT_BONUS_NUMBER.inputMessage());
 
+    }
+
+    public static void outputViewStatistics(HashMap<String, Long> scoreBoard) {
+        System.out.println();
+        System.out.println(STATISTICS);
+        System.out.println(DASH.repeat(3));
+        System.out.println(WinningLevel.FIFTH_PLACE.getResult() + " - " + scoreBoard.get("5th")/WinningLevel.FIFTH_PLACE.getReward() + "개");
+        System.out.println(WinningLevel.FOURTH_PLACE.getResult() + " - " + scoreBoard.get("4th")/WinningLevel.FOURTH_PLACE.getReward() + "개");
+        System.out.println(WinningLevel.THIRD_PLACE.getResult() + " - " + scoreBoard.get("3rd")/WinningLevel.THIRD_PLACE.getReward() + "개");
+        System.out.println(WinningLevel.SECOND_PLACE.getResult() + " - " + scoreBoard.get("2nd")/WinningLevel.SECOND_PLACE.getReward() + "개");
+        System.out.println(WinningLevel.FIRST_PLACE.getResult() + " - " + scoreBoard.get("1st")/WinningLevel.FIRST_PLACE.getReward() + "개");
     }
 }

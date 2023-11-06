908320082dfd4d051a9ff37cc78a8ce62cbf3c1c
ahma0 <dsd932@naver.com>
2023-11-07 05:07:42 +0900

    refactor: ExceptionMessage.java에 개행문자 추가

--- src/main/java/lotto/view/ExceptionMessage.java
+++ src/main/java/lotto/view/ExceptionMessage.java
@@ -2,11 +2,11 @@ package lotto.view;
 
 public class ExceptionMessage {
 
-    public static final String ONLY_SIZE_IS_SIX = "[ERROR] 숫자 6개를 입력해주세요.";
-    public static final String IS_NOT_DIGIT = "[ERROR] 숫자를 입력해주세요.";
-    public static final String NOT_MINUS_OR_ZERO = "[ERROR] 0이나 음수는 입력하실 수 없습니다.";
-    public static final String IS_NOT_BEING_DIVIDED = "[ERROR] 1,000원으로 나누어 떨어지지 않습니다.";
-    public static final String NOT_EMPTY_OR_BLANK = "[ERROR] 공백이나 빈 칸이 있습니다.";
-    public static final String NOT_DUPLICATE = "[ERROR] 중복된 숫자를 입력하면 안됩니다.";
+    public static final String ONLY_SIZE_IS_SIX = "\n[ERROR] 숫자 6개를 입력해주세요.\n";
+    public static final String IS_NOT_DIGIT = "\n[ERROR] 숫자를 입력해주세요.\n";
+    public static final String NOT_MINUS_OR_ZERO = "\n[ERROR] 0이나 음수는 입력하실 수 없습니다.\n";
+    public static final String IS_NOT_BEING_DIVIDED = "\n[ERROR] 1,000원으로 나누어 떨어지지 않습니다.\n";
+    public static final String NOT_EMPTY_OR_BLANK = "\n[ERROR] 공백이나 빈 칸이 있습니다.\n";
+    public static final String NOT_DUPLICATE = "\n[ERROR] 중복된 숫자를 입력하면 안됩니다.\n";
 
 }

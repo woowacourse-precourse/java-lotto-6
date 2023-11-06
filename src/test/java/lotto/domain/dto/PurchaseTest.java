51e4672ee4dc54397adb683c5c522b399eccaa33
yubin <dbqls2821@gmail.com>
2023-11-06 18:59:54 +0900

    test: purchase 내부 필드가 동일하다면 Hash 자료구조에서도 같은 객체로 인식하는 로직에 대한 테스트 코드 추가

--- src/test/java/lotto/domain/dto/PurchaseTest.java
+++ src/test/java/lotto/domain/dto/PurchaseTest.java
@@ -18,4 +18,17 @@ class PurchaseTest {
 
         assertEquals(purchaseA, purchaseB);
     }
+
+    @Test
+    void purchase_내부_필드가_동일하다면_Hash_자료구조에서도_객체로_인식한다() {
+        Set<Purchase> set = new HashSet<>();
+        Lottos lottos = new Lottos();
+        Purchase purchaseA = new Purchase(5, lottos);
+        Purchase purchaseB = new Purchase(5, lottos);
+
+        set.add(purchaseA);
+        set.add(purchaseB);
+
+        assertEquals(1, set.size());
+    }
 }

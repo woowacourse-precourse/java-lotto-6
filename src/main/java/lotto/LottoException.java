eb0283a86f521fea37c02a9f5dbd6797d859e9a9
HSBODo <vinsulill@gmaile.com>
2023-11-06 23:19:46 +0900

    feat : 중복 예외 메세지 추가

--- src/main/java/lotto/LottoException.java
+++ src/main/java/lotto/LottoException.java
@@ -4,7 +4,8 @@ public enum LottoException {
     INPUT_NOT_DiGIT("[ERROR] 숫자로 입력해 주세요."),
     INPUT_NOT_THOUSAND_UNITS("[ERROR] 구입금액을 1,000원 단위로 입력하세요."),
     INPUT_NOT_SPLIT("[ERROR] 쉼표(,)를 기준으로 6자리를 입력해주세요."),
-    INPUT_NOT_RANGE("[ERROR] 1~45 사이의 숫자를 입력해 주세요.")
+    INPUT_NOT_RANGE("[ERROR] 1~45 사이의 숫자를 입력해 주세요."),
+    INPUT_DUPLICATION_NUMBER("[ERROR] 숫자가 중복됩니다.")
     ;
 
 

acd1eda7e870144a9ecc5e6b32665266c3d02e85
HwangSeokJin <ghkd3703@gmail.com>
2023-11-06 19:49:49 +0900

    test: 다른 로또와 비교 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -18,9 +19,17 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 다른_로또와_비교_테스트() {
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        assertThat(lotto1.compare(lotto2, 10)).isEqualTo(Rank.THIRD);
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto1.compare(lotto3, 10)).isEqualTo(Rank.FIRST);
     }
 
     // 아래에 추가 테스트 작성 가능

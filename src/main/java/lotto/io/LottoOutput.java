2959e0d926cd5fda67b9c39a610fa2678116228c
yubin <dbqls2821@gmail.com>
2023-11-06 18:07:53 +0900

    feat: 출력을 담당하는 구현체 추가

--- src/main/java/lotto/io/LottoOutput.java
+++ src/main/java/lotto/io/LottoOutput.java
@@ -1,4 +1,51 @@
 package lotto.io;
 
+import lotto.domain.dto.Statistics;
+
+import java.util.List;
+
 public final class LottoOutput implements OutputPort{
+
+    private final OutputConverter<List<List<Integer>>> lottosConverter;
+    private final OutputConverter<Statistics> statisticsConverter;
+
+    public LottoOutput(OutputConverter<List<List<Integer>>> lottosConverter, OutputConverter<Statistics> statisticsConverter) {
+        this.lottosConverter = lottosConverter;
+        this.statisticsConverter = statisticsConverter;
+    }
+
+    @Override
+    public void printErrorMessage(String message) {
+
+    }
+
+    @Override
+    public void printInputPurchaseAmount() {
+
+    }
+
+    @Override
+    public void printNumberOfLottos(Integer numberOfLottos) {
+
+    }
+
+    @Override
+    public void printCreatedLottos(List<List<Integer>> lottos) {
+
+    }
+
+    @Override
+    public void printInputWinningNumbers() {
+
+    }
+
+    @Override
+    public void printInputBonusNumber() {
+
+    }
+
+    @Override
+    public void printStatistics(Statistics statistics) {
+
+    }
 }

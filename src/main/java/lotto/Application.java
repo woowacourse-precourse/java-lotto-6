13c63b0da8d25637139f3fd23797b9697a4dc2cc
kimwanyoung <dhks2869@gmail.com>
2023-11-06 13:06:32 +0900

    refactor : LottoNumberGenerator 선언 생략

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,13 +2,11 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.domain.LottoNumberGenerator;
-import lotto.domain.NumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        NumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
         try {
-            LottoController lottoController = new LottoController(lottoNumberGenerator);
+            LottoController lottoController = new LottoController(new LottoNumberGenerator());
             lottoController.run();
         } catch (IllegalArgumentException exception) {
             System.out.println("[ERROR] " + exception.getMessage());

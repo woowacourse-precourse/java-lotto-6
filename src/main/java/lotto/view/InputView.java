10122cd96b60560ac30c7288e3dd1be55dcca187
kiwkim <atthenoon@gmail.com>
2023-11-07 08:01:32 +0900

    feat($InputView): Update access control of methods
    - Changed access control of `readInputNumber` method from private to public.
    - Changed access control of `readInputTargetNumber` method from private to public.
    
    This change was made to allow external classes or components to access these methods.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,14 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    private int readInputNumber() {
+    public static int readInputNumber() {
         String amount = Console.readLine();
+        System.out.println(amount);
         return convertNumber(amount);
     }
 
-    private List<Integer> readInputTargetNumbers() {
+    public static List<Integer> readInputTargetNumbers() {
         String[] targetStrings = Console.readLine().split(",");
         List<Integer> targetNumbers = new ArrayList<>();
+        System.out.println(targetNumbers);
         for (String string : targetStrings) {
             targetNumbers.add(convertNumber(string));
         }
@@ -23,7 +25,12 @@ public class InputView {
     private static int convertNumber(String inStream) {
         try {
             int convertedNum = Integer.parseInt(inStream);
-            return convertedNum;
+            if (convertedNum > 0) {
+                return convertedNum;
+            }
+            else {
+                throw new IllegalArgumentException();
+            }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }

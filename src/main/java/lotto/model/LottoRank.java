a285c39380f88e453a5bc1b7ab7b6bc2f72280b6
metishonora <metishonora@icloud.com>
2023-11-07 01:48:26 +0900

    fix: 3위 당첨값 수정

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -5,7 +5,7 @@ public enum LottoRank {
     // 열거형 원소 순서가 변경되면 예측하지 못한 결과를 낼 수 있습니다.
     FIRST_PLACE(6, false, "6개 일치", 2_000_000_000),
     SECOND_PLACE(5, true, "5개 일치, 보너스 볼 일치", 30_000_000),
-    THIRD_PLACE(5, false, "5개 일치", 30_000_000),
+    THIRD_PLACE(5, false, "5개 일치", 1_500_000),
     FOURTH_PLACE(4, false, "4개 일치", 50_000),
     FIFTH_PLACE(3, false, "3개 일치", 5_000),
     NO_PRIZE(0, false, "꽝", 0);

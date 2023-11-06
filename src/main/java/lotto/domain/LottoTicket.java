39a477a4273c2b4e84bb8d747ce459c17ec4b90b
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-06 16:02:34 +0900

    refactor(LottoTickets) : LottoTicket으로 이름 변경 및 구입가격을 받던 int 타입을 PurchasePrice 타입으로 변경

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoTicket {
+    private static final int STANDARD_OF_DIVIDE = 1000;
+    private final List<Lotto> tickets;
+
+    private LottoTicket(List<Lotto> tickets) {
+        this.tickets = tickets;
+    }
+
+    public static LottoTicket of(PurchasePrice purchasePrice, NumberGenerator lottoNumberGenerator) {
+        int purchaseCount = calculateTicketCount(purchasePrice);
+        return new LottoTicket(pickAutoLottoEqualTo(purchaseCount, lottoNumberGenerator));
+    }
+
+    private static int calculateTicketCount(PurchasePrice purchasePrice) {
+        return purchasePrice.getPrice() / STANDARD_OF_DIVIDE;
+    }
+
+    private static List<Lotto> pickAutoLottoEqualTo(int purchaseCount, NumberGenerator lottoNumberGenerator) {
+        return IntStream.range(0, purchaseCount)
+                .mapToObj(lotto -> new Lotto(lottoNumberGenerator.generate()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Lotto> getLottoTicket() {
+        return Collections.unmodifiableList(tickets);
+    }
+
+    public int getLottoCount() {
+        return tickets.size();
+    }
+}

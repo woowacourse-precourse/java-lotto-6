6205070fcee6f6ed5cabe9c6b94c844dba93c8da
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-06 18:46:47 +0900

    [REFACTOR] InputMessage Enum 클래스 삭제
    enum클래스 삭제 후, InputView에 입력관련 메세지 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,24 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.message.InputMessage;
 
 public class InputView {
+    private static final String TOTAL_COST = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
     public static String totalCost() {
-        System.out.println(InputMessage.TOTAL_COST);
+        System.out.println(TOTAL_COST);
         return Console.readLine();
     }
 
     public static String winningNumber() {
-        System.out.println(InputMessage.WINNING_NUMBER);
+        System.out.println(WINNING_NUMBER);
         return Console.readLine();
     }
 
     public static String bonusNumber() {
-        System.out.println(InputMessage.BONUS_NUMBER);
+        System.out.println(BONUS_NUMBER);
         return Console.readLine();
     }
 }

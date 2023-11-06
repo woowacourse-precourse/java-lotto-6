2403bf2e5f9b7de7076542e09e9ee3b9abce05ca
맹순영 <symaeng98@naver.com>
2023-11-06 10:39:59 +0900

    feat: 수익률 계산 및 로또 결과 기능 구현

---
+++ src/main/java/lotto/LottoProgram.java
@@ -0,0 +1,63 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.PrintUtil;
+
+import java.util.List;
+
+public class LottoProgram {
+    private String purchaseAmountInput;
+
+    public List<Lotto> buyLottos() {
+        PrintUtil.printInputPurchaseMessage();
+        String purchaseAmountInput = Console.readLine();
+        validateInteger(purchaseAmountInput);
+        this.purchaseAmountInput = purchaseAmountInput;
+
+        LottoProvider lottoProvider = new LottoProvider();
+
+        List<Lotto> lottos = lottoProvider.buyLottos(purchaseAmountInput);
+
+        PrintUtil.printPurchaseSuccessMessage(lottos.size());
+        PrintUtil.printAllLottos(lottos);
+
+        return lottos;
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        PrintUtil.printInputWinningNumberMessage();
+
+        String winningNumberInput = Console.readLine();
+        WinningNumberProvider winningNumberProvider = new WinningNumberProvider();
+
+        return winningNumberProvider.getWinningNumber(winningNumberInput);
+    }
+
+    public int inputBonus(List<Integer> winningNumbers) {
+        PrintUtil.printInputBonusNumberMessage();
+
+        String bonusInput = Console.readLine();
+        BonusProvider bonusProvider = new BonusProvider();
+
+        return bonusProvider.getBonus(winningNumbers, bonusInput);
+    }
+
+    public void showLottoResult(WinningNumber winningNumber, List<Lotto> lottos) {
+        PrizeProvider prizeProvider = new PrizeProvider();
+
+        LottoResult lottoResult = prizeProvider.getLottoResult(lottos, winningNumber);
+        List<Integer> winningResult = lottoResult.getWinningResult();
+        int totalPrize = lottoResult.getTotalPrize();
+
+        PrintUtil.printWinningResultMessage(winningResult);
+
+        PrintUtil.printReturnRateMessage((totalPrize/Double.parseDouble(purchaseAmountInput))*100);
+    }
+
+    private void validateInteger(String input) {
+        // 숫자가 아닌 경우 예외 발생
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+    }
+}

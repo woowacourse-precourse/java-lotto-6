fe3934a5104f28d6456173092c121adc48d2d7e0
dmong <dt2980_hee@naver.com>
2023-11-06 13:53:11 +0900

    feat: NumbersGenerator, RandomNumbersGenerator 작성

---
+++ src/main/java/lotto/util/NumbersGenerator.java
@@ -0,0 +1,7 @@
+package lotto.util;
+
+import java.util.List;
+
+public interface NumbersGenerator {
+    public List<Integer> create();
+}
---
+++ src/main/java/lotto/util/RandomNumbersGenerator.java
@@ -0,0 +1,16 @@
+package lotto.util;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.domain.LottoConstants.LOTTO_LENGTH_CONSTRAINT;
+import static lotto.domain.LottoConstants.LOTTO_NUMBER_RANGE_END;
+import static lotto.domain.LottoConstants.LOTTO_NUMBER_RANGE_START;
+
+import java.util.List;
+
+public class RandomNumbersGenerator implements NumbersGenerator {
+
+    public List<Integer> create() {
+        return pickUniqueNumbersInRange(LOTTO_NUMBER_RANGE_START.getValue(),
+                LOTTO_NUMBER_RANGE_END.getValue(), LOTTO_LENGTH_CONSTRAINT.getValue());
+    }
+}

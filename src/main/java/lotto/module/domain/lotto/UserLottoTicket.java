6023ee07f31d04feddf317ce8b66af945c2af3ed
lkdcode <dlrlejr1@naver.com>
2023-11-07 03:10:41 +0900

    Add [UserLottoTicket] : 유저가 구매하는 로또 클래스 구현

---
+++ src/main/java/lotto/module/domain/lotto/UserLottoTicket.java
@@ -0,0 +1,19 @@
+package lotto.module.domain.lotto;
+
+import java.util.List;
+
+public class UserLottoTicket extends Lotto {
+
+    public UserLottoTicket(List<Integer> numbers) {
+        super(numbers);
+    }
+
+    public static UserLottoTicket newInstance(List<Integer> numbers) {
+        return new UserLottoTicket(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return super.getNumbers();
+    }
+
+}

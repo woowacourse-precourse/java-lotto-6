9b8496b2d880bcd48c9d547cb3b237d8ee2c79e2
delena0702 <delena0702@gmail.com>
2023-11-06 18:18:39 +0900

    feat: Lotto 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
+    static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,20 +16,49 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    public static List<Lotto> generateRandomLotto(int number) {
-        // TODO: 미구현
-        return null;
+    public static Lotto generateRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+    public static List<Lotto> generateRandomLottos(int number) {
+        List<Lotto> generatedLottos = new ArrayList<>();
+
+        for (int i = 0; i < number; i++) {
+            generatedLottos.add(generateRandomLotto());
+        }
+
+        return generatedLottos;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
 
-        // TODO: numbers 내부 중복 값 검증 필요
+        // 중복 값 확인
+        Set<Integer> check = new HashSet<>(numbers);
+        if (check.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public LottoMatchResult matchWinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        return null;
+        int matched = 0;
+        boolean hasBonus = false;
+
+        Set<Integer> check = new HashSet<>(winningNumbers);
+
+        for (int number : numbers) {
+            if (check.contains(number)) {
+                matched++;
+            }
+
+            if (number == bonusNumber) {
+                hasBonus = true;
+            }
+        }
+
+        return LottoMatchResult.fromMatchedCount(matched, hasBonus);
     }
 }

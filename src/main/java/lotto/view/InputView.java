0cc635eb6ef669c6ddece6bea1ea829a2ec0fd50
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-06 20:32:37 +0900

    feat: 입력 메시지 기능 추가
    구입금액, 당첨 번호, 보너스 번호 입력메시지

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,17 @@
 package lotto.view;
 
-public class InputView {
-
+public final class InputView {
+	
+	public static void inputAmount() {
+		System.out.println("구입금액을 입력해 주세요.");
+	}
+	
+	public static void inputWinningNumbers() {
+		System.out.println("당첨 번호를 입력해 주세요.");
+	}
+	
+	public static void inputBonusNumber() {
+		System.out.println("보너스 번호를 입력해 주세요.");
+	}
+	
 }

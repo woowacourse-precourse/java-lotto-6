a0f193b7369f3e24d20b4ba0f0ac9f764e572ab1
jalddak <jalddak98@gmail.com>
2023-11-06 15:22:17 +0900

    fix - 에러 메세지 출력 테스트: 변수 추가

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
+import static lotto.view.ErrorMessage.DUPLICATED_NUMBER;
+import static lotto.view.ErrorMessage.LESS_MONEY;
+import static lotto.view.ErrorMessage.NOT_DIVISIBLE_MONEY;
+import static lotto.view.ErrorMessage.NOT_EQUAL_NUM_OF_NUMBERS;
+import static lotto.view.ErrorMessage.NOT_INTEGER_LIST;
 import static lotto.view.ErrorMessage.NOT_NUMBER;
+import static lotto.view.ErrorMessage.OUT_OF_RANGE_LOTTO_NUMBERS;
 import static lotto.view.OutputView.printErrorMessage;
 import static lotto.view.OutputView.printLottoNumbers;
 import static lotto.view.OutputView.printNumOfTickets;
@@ -60,15 +66,28 @@ public class OutputViewTest {
         );
     }
 
-    @DisplayName("출력 (구입 금액 예외처리)")
-    @Test
-    void 출력테스트_printErrorMessage() {
+    @DisplayName("에러 메세지 출력 테스트")
+    @ParameterizedTest
+    @MethodSource("provideErrorMessage")
+    void 출력테스트_printErrorMessage(ErrorMessage errorMessage) {
         //given
-        String expect = "[ERROR] 유효한 숫자를 입력해야 합니다.\n";
+        String expect = errorMessage.getMessage() + "\n";
         //when
-        printErrorMessage(new IllegalArgumentException(NOT_NUMBER.getMessage()));
+        printErrorMessage(new IllegalArgumentException(errorMessage.getMessage()));
         //then
         assertThat(output.toString()).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> provideErrorMessage() {
+        return Stream.of(
+                Arguments.of(NOT_NUMBER),
+                Arguments.of(LESS_MONEY),
+                Arguments.of(NOT_DIVISIBLE_MONEY),
+                Arguments.of(NOT_INTEGER_LIST),
+                Arguments.of(NOT_EQUAL_NUM_OF_NUMBERS),
+                Arguments.of(OUT_OF_RANGE_LOTTO_NUMBERS),
+                Arguments.of(DUPLICATED_NUMBER)
+        );
     }
 
     @DisplayName("출력 (티켓 개수 출력)")

69d399313625b8f925e6ae8c1bbef6f0ee837b84
koomchang <leahpar0401@gmail.com>
2023-11-07 03:02:30 +0900

    feat: 보너스 번호는 당첨 번호와 중복되지 않는 검증 조건 추가

--- src/main/java/lotto/validator/BonusNumberInputValidator.java
+++ src/main/java/lotto/validator/BonusNumberInputValidator.java
@@ -1,14 +1,17 @@
 package lotto.validator;
 
+import java.util.List;
 import lotto.model.LottoNumber;
 
 public class BonusNumberInputValidator {
     private static final String INPUT_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
     private static final String INPUT_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 입력값이 1과 45 사이여야 합니다.";
+    private static final String INPUT_DUPLICATED = "[ERROR] 입력값이 당첨번호와 중복되지 않아야 합니다.";
 
-    public static void validate(String input) {
+    public static void validate(String input, List<Integer> winningLotto) {
         validateNumberIfNumeric(input);
         validateNumberRange(input);
+        validateDuplicateNumber(input, winningLotto);
     }
 
     private static void validateNumberIfNumeric(String input) {
@@ -24,5 +27,16 @@ public class BonusNumberInputValidator {
         if (!LottoNumber.isInRange(bonusNumber)) {
             throw new IllegalArgumentException(INPUT_NOT_BETWEEN_ONE_AND_FORTY_FIVE);
         }
+    }
+
+    private static void validateDuplicateNumber(String input, List<Integer> winningLotto) {
+        int bonusNumber = Integer.parseInt(input);
+        if (hasBonusNumber(bonusNumber, winningLotto)) {
+            throw new IllegalArgumentException(INPUT_DUPLICATED);
+        }
+    }
+
+    private static boolean hasBonusNumber(Integer bonusNumber, List<Integer> winningLotto) {
+        return winningLotto.contains(bonusNumber);
     }
 }

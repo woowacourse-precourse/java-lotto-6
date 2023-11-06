e924ac02caba05fdb8694fdbd7a1f275f2c1a25f
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 14:53:35 +0900

    fix: 구매가능한 로또 번호 등록하여 생성 -> 오름차순 정렬 추가
    오름차순으로 로또 번호 정렬

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -15,6 +16,7 @@ public class Application {
         List<Lotto> randomLottos = new ArrayList<>();
         for (int i = 0; i < number; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
             randomLottos.add(new Lotto(numbers));
         }
         return randomLottos;

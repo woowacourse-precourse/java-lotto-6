4baa87db476819c791832e3c99641c6f016a9c07
jinsu4755 <parkjinsu4755@gmail.com>
2023-11-07 05:16:31 +0900

    style: 사용하지 않는 import 제거

--- src/test/java/lotto/domain/LottoTicketDispenserTest.java
+++ src/test/java/lotto/domain/LottoTicketDispenserTest.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

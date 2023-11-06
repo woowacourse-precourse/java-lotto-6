7fb5799da08e8b93f0db69fceb057f97a30d7fbd
bamm2 <xxknu88@naver.com>
2023-11-06 16:02:08 +0900

    feat(InputView): 정상 입력이 들어올 때까지 입력받는 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,58 @@
 package lotto.view;
 
+import static lotto.constant.Constant.PURCHASE_PRICE_MESSAGE;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.LottoApplicationException;
+import lotto.validator.InputValidator;
+
 public class InputView {
 
+    private final InputValidator inputValidator;
+    private String validateInput;
+
     public InputView() {
+        inputValidator = new InputValidator();
     }
 
-    public String removeWhitespace(String input){
+    public String writePurchasePrice() {
+        print(PURCHASE_PRICE_MESSAGE);
+        validateInput();
+        return validateInput;
+    }
+
+    public void validateInput(){
+        while(!validate()){
+        }
+    }
+
+    public boolean validate() {
+        try {
+            validateInput = removeWhitespace(Console.readLine());
+            inputValidator.validateNotNull(validateInput);
+            inputValidator.validateNumber(validateInput);
+            return true;
+        } catch (LottoApplicationException e) {
+            e.getErrorMessage();
+            return false;
+        }
+    }
+
+    public String removeWhitespace(String input) {
         return Stream.of(input)
                 .flatMap(userInput -> userInput.chars()
                         .mapToObj(element -> (char) element)
                         .filter(element -> !Character.isWhitespace(element))
                         .map(String::valueOf))
                 .collect(Collectors.joining());
+    }
+
+
+    private void print(String message) {
+        System.out.println(message);
     }
 
 }

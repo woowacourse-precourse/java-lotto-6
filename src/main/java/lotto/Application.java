2275616eb960944f361c02cdf52fa03f211bdbb7
jimmy0006 <jinmin645@gmail.com>
2023-11-07 01:44:27 +0900

    feat:input을 검증하는 함수 Game 클래스로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,5 @@
 package lotto;
 
-import java.util.ArrayList;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -9,20 +7,43 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("구매금액을 입력해 주세요.");
         String buyMoney = readLine();
+        Game game;
+        while(true){
+            try{
+                game = new Game(buyMoney);
+                break;
+            }catch(IllegalArgumentException e){
+                System.out.printf("[ERROR] %s\n",e.getMessage());
+            }
+            buyMoney = readLine();
+        }
         System.out.println();
-        Game game = new Game(Integer.parseInt(buyMoney));
-        ArrayList<Integer> test = new ArrayList<>();
+
         System.out.println("당첨 번호를 입력해 주세요.");
         String numbers = readLine();
-        String[] split = numbers.split(",");
-        for (String s:split) {
-            test.add(Integer.valueOf(s));
+        while(true){
+            try{
+                game.setWinNumbers(numbers);
+                break;
+            }catch(IllegalArgumentException e){
+                System.out.printf("[ERROR] %s\n",e.getMessage());
+            }
+            numbers = readLine();
         }
         System.out.println();
 
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = readLine();
+        while(true){
+            try{
+                game.setBonusNumber(bonusNumber);
+                break;
+            }catch(IllegalArgumentException e){
+                System.out.printf("[ERROR] %s\n",e.getMessage());
+            }
+            bonusNumber = readLine();
+        }
         System.out.println();
-        game.printResult(test,Integer.parseInt(bonusNumber));
+        game.printResult();
     }
 }

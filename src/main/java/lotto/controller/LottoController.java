24800bcd6e4790c8c7ee229029351d05df754ba4
lusate <rayrayr@naver.com>
2023-11-06 17:10:54 +0900

    feat: controller 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,21 +3,27 @@ package lotto.controller;
 import static lotto.view.Input.inputAmount;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
 import lotto.model.LottoSetNumber;
 import lotto.model.PlayerPurchase;
+import lotto.model.Ranking;
+import lotto.model.Winner;
 import lotto.view.Input;
 import lotto.view.Output;
 
 public class LottoController {
+
+    private final int lotto_One_Price = 1000;
+    private final int percentage = 100;
 
     private static List<Lotto> lottoAllNumbers;
     private static List<Integer> lottoList;
 
     public void run() {
         start();
-
     }
 
     public void start() {
@@ -25,13 +31,10 @@ public class LottoController {
         int lottoCount = inputPlayerAmount(); // 구입 금액 입력 후 계산 -> 8개 구매
 
         Output.print_Lotto_Count(lottoCount); // 8개를 구매했습니다.
-        createLottoAllList(); // 로또 6개 번호
+        List<Lotto> lottoAllList = createLottoAllList(lottoCount);// 로또 6개 번호
 
-        Output.input_Winning_Number();
-        Input.input_BonusNumber();
-
-        Output.input_Bonus_Number();
-        Input.input_BonusNumber();
+        Winner winner = winner();
+        winning_Statistics(lottoAllList, winner, lottoCount);
     }
 
 
@@ -41,21 +44,65 @@ public class LottoController {
     }
 
     // 랜덤으로 뽑아낸 8개 로또 찍어냄.
-    private static List<Lotto> createLottoAllList() {
+    private List<Lotto> createLottoAllList(int lottoCount) {
         lottoAllNumbers = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < lottoCount; i++) {
             lottoAllNumbers.add(createLottoList()); // 랜덤 숫자 6개를 8번 넣음.
         }
         return lottoAllNumbers;
     }
 
-    private static Lotto createLottoList() {
-        LottoSetNumber lottoSetNumber = new LottoSetNumber();
-        lottoList = lottoSetNumber.setRandomNumbers();
-
+    private Lotto createLottoList() {
+        lottoList = LottoSetNumber.setRandomNumbers();
         System.out.println(lottoList);
         return new Lotto(lottoList);
     }
 
-    
+    public Winner winner() {
+        Output.input_Winning_Number(); // 당첨 번호 입력하세요.
+        List<Integer> numbers = new ArrayList<>(Input.input_Winning_Number());
+        Output.input_Bonus_Number(); // 보너스 번호 입력
+        int bonus = Input.input_BonusNumber();
+
+        return new Winner(new Lotto(numbers), bonus);
+    }
+
+
+    private void winning_Statistics(List<Lotto> lottoAllList, Winner winner, int amount) {
+        Map<Ranking, Integer> statistics = rankMap();
+        for (Lotto lotto : lottoAllList) {
+            Ranking ranking = winner.winnerMatch(lotto); // 로또 8개의 랭킹을 조회
+            statistics.put(ranking, statistics.get(ranking) + 1);
+        }
+
+        Output.statistics();
+
+        printRankingResult(statistics);
+        earningRate(statistics, amount);
+    }
+
+    private Map<Ranking, Integer> rankMap() {
+        Map<Ranking, Integer> result = new LinkedHashMap<>();
+        for (Ranking ranking : Ranking.values()) {
+            result.put(ranking, 0);
+        }
+        return result;
+    }
+
+    private void printRankingResult(Map<Ranking, Integer> result) {
+        for (int i = Ranking.values().length - 1; i >= 0; i--) {
+            Ranking.values()[i].printMessage(result.get(Ranking.values()[i]));
+        }
+
+    }
+
+    private void earningRate(Map<Ranking, Integer> statistics, int lottoCount) {
+        double earningRate = 0;
+        for (Ranking ranking : statistics.keySet()) {
+            earningRate += (double) statistics.get(ranking) * ranking.getWinningPrize() / (lottoCount * lotto_One_Price)
+                    * percentage;
+        }
+
+        Output.total_EarningRate(earningRate);
+    }
 }

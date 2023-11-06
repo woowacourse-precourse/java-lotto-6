9d1db08c0ff35b2548b80db57673c0d7514514cc
dd <12202340@inha.edu>
2023-11-06 15:51:35 +0900

    test(Money.java): 수익률 계산 테스트 코드 작성

--- src/test/java/lotto/PrizeResultTest.java
+++ src/test/java/lotto/PrizeResultTest.java
@@ -8,24 +8,36 @@ import lotto.domain.Lottos;
 import lotto.domain.Prize;
 import lotto.domain.PrizeResult;
 import lotto.domain.WinningLotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PrizeResultTest {
-    @DisplayName("결과 계산 테스트")
-    @Test
-    void calculatePrizeTest() {
+    PrizeResult prizeResult;
+
+    @BeforeEach
+    void initPrizeResult() {
         Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
         Lottos lottos = new Lottos(List.of(lotto1, lotto2));
         WinningLotto winningLotto = WinningLotto.of("1,2,3,4,5,7", "8");
-        PrizeResult prizeResult = new PrizeResult(lottos, winningLotto);
+        prizeResult = new PrizeResult(lottos, winningLotto);
+    }
 
+    @DisplayName("결과 계산 테스트")
+    @Test
+    void calculatePrizeTest() {
         assertThat(prizeResult.getResult().get(Prize.FIRST)).isEqualTo(0);
         assertThat(prizeResult.getResult().get(Prize.SECOND)).isEqualTo(0);
         assertThat(prizeResult.getResult().get(Prize.THIRD)).isEqualTo(1);
         assertThat(prizeResult.getResult().get(Prize.FOURTH)).isEqualTo(0);
         assertThat(prizeResult.getResult().get(Prize.FIFTH)).isEqualTo(0);
         assertThat(prizeResult.getResult().get(Prize.MISS)).isEqualTo(1);
+    }
+
+    @DisplayName("총 상금 계산 테스트")
+    @Test
+    void getPrizeMoneyTest() {
+        assertThat(prizeResult.getPrizeMoney()).isEqualTo(1500000);
     }
 }

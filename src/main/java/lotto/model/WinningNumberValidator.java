1d453f6ce32842ebd4bfb97674cfc2e81bdb5f39
JiEung2 <https://github.com/JiEung2>
2023-11-06 22:32:38 +0900

    Feat: isNumber, isCorrectRange 메서드 추가
    숫자가 입력되었는지 확인하는 isNumber 메서드와 숫자가 해당하는 범위에 맞게 들어왔는지 확인하는 isCorrectRange 메서드 추가

--- src/main/java/lotto/model/WinningNumberValidator.java
+++ src/main/java/lotto/model/WinningNumberValidator.java
@@ -1,9 +1,17 @@
 package lotto.model;
 
-import static lotto.utils.Constants.*;
+import static lotto.utils.Constants.ERROR_MESSAGE;
+import static lotto.utils.Constants.WINNING_NUMBER_DUPLICATE_ERROR;
+import static lotto.utils.Constants.WINNING_NUMBER_ERROR;
+import static lotto.utils.Constants.WINNING_NUMBER_PATTERN;
+import static lotto.utils.Constants.WINNING_NUMBER_RANGE;
+import static lotto.utils.Constants.WINNING_NUMBER_RANGE_ERROR;
+import static lotto.utils.Constants.WINNING_NUMBER_STRING_ERROR;
+import static lotto.utils.Constants.WINNING_NUMBER_STRING_PATTERN;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 
 public class WinningNumberValidator {
     String winningNumber;
@@ -16,8 +24,17 @@ public class WinningNumberValidator {
     }
 
     private void validate() {
+        isCorrectPattern();
         isNumber();
+        isCorrectRange();
         isDuplicate();
+    }
+
+    private void isCorrectPattern() {
+        Matcher matcher = WINNING_NUMBER_STRING_PATTERN.matcher(winningNumber);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NUMBER_STRING_ERROR);
+        }
     }
 
     private void isNumber() {
@@ -25,12 +42,21 @@ public class WinningNumberValidator {
                 .filter(numbers -> !WINNING_NUMBER_PATTERN.matcher(numbers).find())
                 .findAny()
                 .ifPresent(s -> {
-                        throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NUMBER_ERROR);
+                    throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NUMBER_ERROR);
                 });
     }
 
-    private void isDuplicate(){
-        if(numbers.stream().distinct().count() < numbers.size()) {
+    private void isCorrectRange() {
+        numbers.stream()
+                .filter(numbers -> !WINNING_NUMBER_RANGE.matcher(numbers).find())
+                .findAny()
+                .ifPresent(s -> {
+                    throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NUMBER_RANGE_ERROR);
+                });
+    }
+
+    private void isDuplicate() {
+        if (numbers.stream().distinct().count() < numbers.size()) {
             throw new IllegalArgumentException(ERROR_MESSAGE + WINNING_NUMBER_DUPLICATE_ERROR);
         }
     }

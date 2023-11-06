bf506f90de82269334d5d2f434feefd16e915f62
JoonKyo <joonkyolee@gmail.com>
2023-11-06 17:13:47 +0900

    test: 플레이어에게 입력받은 값이 null 또는 비어있는 값에 대한 예외 테스트 추가

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,2 +1,30 @@
-package lotto;public class InputViewTest {
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import lotto.view.InputValidator;
+
+public class InputViewTest {
+    private static final String NULL_VALUE = null;
+    private static final String EMPTY_VALUE = "";
+    private static final String SPACE_VALUE = " ";
+
+    @Test
+    @DisplayName("플레이어가 null 값을 입력한 경우 예외를 던진다.")
+    void nullValueTest() {
+        assertThatThrownBy(() -> InputValidator.validateInput(NULL_VALUE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("플레이어가 비어있는 값을 입력한 경우 예외를 던진다.")
+    void emptyValueTest() {
+        assertThatThrownBy(() -> InputValidator.validateInput(EMPTY_VALUE))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputValidator.validateInput(SPACE_VALUE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

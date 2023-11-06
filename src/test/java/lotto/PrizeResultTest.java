8d2a41351228295414c80f7c6d94ce827e9b715d
dd <12202340@inha.edu>
2023-11-06 20:41:09 +0900

    test(PrizeResult.java): PrizeResult 리팩토링에 따라 테스트 코드 리팩토링

--- src/test/java/lotto/PrizeResultTest.java
+++ src/test/java/lotto/PrizeResultTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
@@ -18,23 +19,28 @@ public class PrizeResultTest {
     @BeforeEach
     void initLottos() {
         Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto lotto2 = new Lotto(List.of(6,7,8,9,10,11));
+        Lotto lotto2 = new Lotto(List.of(6, 7, 8, 9, 10, 11));
         this.lottos = Lottos.createByInput(List.of(lotto1, lotto2));
+    }
+
+    List<String> initAnswer(int fifth, int fourth, int third, int second, int first) {
+        return List.of(
+                String.format("%s - %d개", Prize.FIFTH.getPrint(), fifth),
+                String.format("%s - %d개", Prize.FOURTH.getPrint(), fourth),
+                String.format("%s - %d개", Prize.THIRD.getPrint(), third),
+                String.format("%s - %d개", Prize.SECOND.getPrint(), second),
+                String.format("%s - %d개", Prize.FIRST.getPrint(), first)
+        );
     }
 
     @DisplayName("3개가 일치하는 경우 테스트")
     @Test
     void fifthTest() {
         WinningLotto winningLotto = WinningLotto.of("1,2,3,30,31,32", "33");
-
+        List<String> answers = initAnswer(1,0,0,0,0);
         PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
 
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIRST)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.SECOND)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.THIRD)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FOURTH)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIFTH)).isEqualTo(1);
-
+        assertThat(prizeResult.getResults()).isEqualTo(answers);
         assertThat(prizeResult.getPrizeMoney()).isEqualTo(5000);
     }
 
@@ -42,15 +48,10 @@ public class PrizeResultTest {
     @Test
     void fourthTest() {
         WinningLotto winningLotto = WinningLotto.of("1,2,3,4,30,31", "32");
-
+        List<String> answers = initAnswer(0,1,0,0,0);
         PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
 
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIRST)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.SECOND)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.THIRD)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FOURTH)).isEqualTo(1);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIFTH)).isEqualTo(0);
-
+        assertThat(prizeResult.getResults()).isEqualTo(answers);
         assertThat(prizeResult.getPrizeMoney()).isEqualTo(50000);
     }
 
@@ -58,15 +59,10 @@ public class PrizeResultTest {
     @Test
     void thirdTest() {
         WinningLotto winningLotto = WinningLotto.of("1,2,3,4,5,30", "31");
-
+        List<String> answers = initAnswer(0,0,1,0,0);
         PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
 
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIRST)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.SECOND)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.THIRD)).isEqualTo(1);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FOURTH)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIFTH)).isEqualTo(0);
-
+        assertThat(prizeResult.getResults()).isEqualTo(answers);
         assertThat(prizeResult.getPrizeMoney()).isEqualTo(1500000);
     }
 
@@ -74,15 +70,10 @@ public class PrizeResultTest {
     @Test
     void secondTest() {
         WinningLotto winningLotto = WinningLotto.of("1,2,3,4,5,30", "6");
-
+        List<String> answers = initAnswer(0,0,0,1,0);
         PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
 
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIRST)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.SECOND)).isEqualTo(1);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.THIRD)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FOURTH)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIFTH)).isEqualTo(0);
-
+        assertThat(prizeResult.getResults()).isEqualTo(answers);
         assertThat(prizeResult.getPrizeMoney()).isEqualTo(30000000);
     }
 
@@ -90,15 +81,10 @@ public class PrizeResultTest {
     @Test
     void firstTest() {
         WinningLotto winningLotto = WinningLotto.of("1,2,3,4,5,6", "30");
-
+        List<String> answers = initAnswer(0,0,0,0,1);
         PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
 
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIRST)).isEqualTo(1);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.SECOND)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.THIRD)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FOURTH)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIFTH)).isEqualTo(0);
-
+        assertThat(prizeResult.getResults()).isEqualTo(answers);
         assertThat(prizeResult.getPrizeMoney()).isEqualTo(2000000000);
     }
 
@@ -106,15 +92,10 @@ public class PrizeResultTest {
     @Test
     void prizeMoneyTest() {
         WinningLotto winningLotto = WinningLotto.of("1,2,3,7,8,9", "30");
-
+        List<String> answers = initAnswer(2,0,0,0,0);
         PrizeResult prizeResult = PrizeResult.of(lottos, winningLotto);
 
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIRST)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.SECOND)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.THIRD)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FOURTH)).isEqualTo(0);
-        assertThat(prizeResult.getPrizeCounts().get(Prize.FIFTH)).isEqualTo(2);
-
+        assertThat(prizeResult.getResults()).isEqualTo(answers);
         assertThat(prizeResult.getPrizeMoney()).isEqualTo(10000);
     }
 }

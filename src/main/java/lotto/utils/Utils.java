925057a7466e37b4f94fdc34612a65fffeb8796b
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-06 17:54:20 +0900

    feat: 문자열을 쉼표로 구분하여 리스트화 하는 기능 추가

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -2,6 +2,8 @@ package lotto.utils;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -13,5 +15,11 @@ public class Utils {
 
 	public static List<Integer> generateUniqueNumberListInRange(int startInclusive, int endInclusive, int count) {
 		return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+	}
+	
+	public static List<String> splitString(String input) {
+		return Stream.of(input.split(","))
+				.map(String::trim)
+				.collect(Collectors.toList());
 	}
 }

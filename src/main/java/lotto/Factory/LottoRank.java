37cc55e231289bce0bacefd9faea70f6c45f03aa
minju <minju9711@naver.com>
2023-11-07 03:32:51 +0900

    [feat] Lotto의 등급을 나타내는 enum 클래스.

--- src/main/java/lotto/Factory/LottoRank.java
+++ src/main/java/lotto/Factory/LottoRank.java
@@ -1,5 +1,22 @@
 package lotto.Factory;
 
-public enum LottoRank
-{
+public enum LottoRank {
+    FIRST(6, "1등", 2_000_000_000),
+    SECOND(5, "2등", 30_000_000),
+    THIRD(5, "3등", 1_500_000),
+    FOURTH(4, "4등", 50_000),
+    FIFTH(3, "5등", 5_000),
+    NONE(0, "꽝", 0);
+
+    private final int matchCount;
+    private final String rankName;
+    private final long prizeMoney;
+
+    LottoRank(int matchCount, String rankName, long prizeMoney) {
+        this.matchCount = matchCount;
+        this.rankName = rankName;
+        this.prizeMoney = prizeMoney;
+    }
+
+
 }

bdd951439b0cfbb34915c3954a9f8c900c54a880
styughjvbn <3338761@naver.com>
2023-11-06 16:03:53 +0900

    <chore> : import 수정 --------------------------------------- 1. 필요없는 import 제거

--- src/test/java/lotto/MyLottoTest.java
+++ src/test/java/lotto/MyLottoTest.java
@@ -1,11 +1,8 @@
 package lotto;
 
-import model.Lotto;
 import model.MyLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 

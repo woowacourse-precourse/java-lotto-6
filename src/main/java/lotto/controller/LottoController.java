ef8767df65c28f575af561b5f32340bf26f36154
jhva <junghun8158@naver.com>
2023-11-06 22:54:01 +0900

    :sparkles: feat(LottoController): createLottoGenerate 메서드를 통해 LottoDto 참조값 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,12 @@
 package lotto.controller;
 
+import static lotto.utils.LottoUtils.divisionLottoPrice;
+import static lotto.utils.LottoUtils.generateRandomNumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.dto.LottoDto;
+import lotto.model.Lotto;
 import lotto.view.InputView;
 import lotto.view.OutView;
 
@@ -15,5 +22,19 @@ public class LottoController {
 
     public void run() {
         int priceLotto = inputView.parseInputFromUserInteger();
+        int divisionLottoPrice = divisionLottoPrice(priceLotto);
+        List<LottoDto> generateLottoNumbersDto = createLottoGenerate(divisionLottoPrice);
+    }
+
+    public List<LottoDto> createLottoGenerate(int divisionLottoPrice) {
+        List<LottoDto> lottoDtos = new ArrayList<>();
+        System.out.println();
+        outView.lottoPriceView(divisionLottoPrice);
+        for (int i = 0; i < divisionLottoPrice; i++) {
+            Lotto lotto = new Lotto(generateRandomNumbers());
+            lottoDtos.add(new LottoDto(lotto.getLotto()));
+            outView.randomLottoOutView(lotto.getLotto());
+        }
+        return lottoDtos;
     }
 }

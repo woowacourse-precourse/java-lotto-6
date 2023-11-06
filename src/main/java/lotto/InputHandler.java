e555d35a364172278d734cf4ecdc9d050e3727c1
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2023-11-06 21:06:00 +0900

    [Feat] Added method for entering lotto purchase cost

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -1,4 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputHandler {
+    public Integer  readCost() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        while(true) {
+            try {
+                String input = Console.readLine();
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해야 합니다.");
+            }
+        }
+    }
+    
+    
+    
 }
--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,4 +1,18 @@
 package lotto;
 
+import java.util.List;
+
 public class Result {
+//    public List<Long> scores;
+//    public Long profit;
+//    public Result(List<Long> scores) {
+//        this.scores = scores;
+//        this.profit = calculateProfit(scores);
+//    }
+//    public Long getProfit() {
+//        return this.profit;
+//    }
+//    private Long calculateProfit(List<Long> scores) {
+//        
+//    }
 }

ca71a2a8a754a7234cbce173f03b092b91a8346d
aammddkkzxc <ideal0427@naver.com>
2023-11-06 19:51:33 +0900

    feat: 구입 금액에 따른 티켓 수량 계산 기능 구현

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -1,9 +1,15 @@
 package lotto;
 
 public class PurchaseMoney {
+    private static int LOTTO_TICKET_PRICE = 1000;
+
     private final int amount;
 
     public PurchaseMoney(int amount) {
         this.amount = amount;
+    }
+
+    public int getLottoTicketQuantity() {
+        return amount / LOTTO_TICKET_PRICE;
     }
 }

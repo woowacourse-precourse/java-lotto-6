b1159f2e83ae665b72af0a080b22df95fbcb8aeb
최진원 <etudent39@gmail.com>
2023-11-07 00:49:07 +0900

    feat : 로또를 구매하고 보여주는 기능 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,2 +1,41 @@
-package lotto;public class LottoController {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoController {
+    private final static Integer START = 0;
+    private Lottos lottos;
+    private BuyAmount buyAmount;
+
+    public LottoController() {
+        buyLotto();
+        showLotto();
+    }
+
+    private void buyLotto() {
+        Output.printBuyAmount();
+        buyAmount = new BuyAmount(Input.user());
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = START; i < buyAmount.getBuyCount(); i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+        this.lottos = new Lottos(lottos);
+
+    }
+
+    private void showLotto() {
+        Output.printBuyCount(buyAmount.getBuyCount());
+        for(Lotto lotto : this.lottos.getLottos()) {
+            List<Integer> lottoNumber = lotto.getLotto();
+            Output.printLottoNumber(Arrays
+                    .toString(lottoNumber
+                            .stream()
+                            .mapToInt(Integer::intValue)
+                            .toArray()));
+        }
+    }
 }

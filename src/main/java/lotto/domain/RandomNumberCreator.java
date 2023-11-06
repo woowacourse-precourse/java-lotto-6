122e16a03f577efbaf11b3420feed7f5f4099d94
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 22:28:51 +0900

    Remove: NumberCreator 삭제

--- src/main/java/lotto/domain/RandomNumberCreator.java
+++ src/main/java/lotto/domain/RandomNumberCreator.java
@@ -5,9 +5,8 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.constant.NumbersOption;
 
-public class RandomNumberCreator implements NumberCreator {
+public class RandomNumberCreator {
 
-	@Override
 	public List<Integer> create() {
 		return Randoms.pickUniqueNumbersInRange(NumbersOption.MIN, NumbersOption.MAX, NumbersOption.SIZE);
 	}

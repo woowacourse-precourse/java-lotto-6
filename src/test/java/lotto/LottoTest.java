e76195e17b12f17a2027ca1a5aa9872ed22c9f3b
muyaaho <skanthsu715@gmail.com>
2023-11-06 21:05:15 +0900

    test(lotto): Lotto Test 리턴되는 에러 메시지 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -26,25 +26,25 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7, 8)));
-        assertEquals(exception.getMessage(), "6개의 숫자가 아닙니다.");
+        assertEquals(exception.getMessage(), "[ERROR] 6개의 숫자가 아닙니다. 다시 입력해주세요.");
         Exception exception2 = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5)));
-        assertEquals(exception2.getMessage(), "6개의 숫자가 아닙니다.");
+        assertEquals(exception2.getMessage(), "[ERROR] 6개의 숫자가 아닙니다. 다시 입력해주세요.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5, 5)));
-        assertEquals(exception.getMessage(), "중복된 숫자가 있습니다.");
+        assertEquals(exception.getMessage(), "[ERROR] 중복된 숫자가 있습니다. 다시 입력해주세요.");
     }
 
     @DisplayName("1~45 사이가 아니라면 예외 처리한다.")
     @Test
     void creatLottoBy1to45(){
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5, 100)));
-        assertEquals(exception.getMessage(), "1에서 45사이의 수가 아닙니다.");
+        assertEquals(exception.getMessage(), "[ERROR] 로또 번호의 숫자 범위는 1~45사이 입니다. 다시 입력해주세요.");
         Exception exception2 = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(-1, 2, 3, 4, 5, 11)));
-        assertEquals(exception2.getMessage(), "1에서 45사이의 수가 아닙니다.");
+        assertEquals(exception2.getMessage(), "[ERROR] 로또 번호의 숫자 범위는 1~45사이 입니다. 다시 입력해주세요.");
     }
 
 }

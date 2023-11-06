6bd9660f66c1c64d394be1b1426c640c94a710b2
transfer_kk <kseysh123@naver.com>
2023-11-06 19:50:32 +0900

    refactor : return 위치 변경

--- src/main/java/lotto/domain/service/LottoCalculator.java
+++ src/main/java/lotto/domain/service/LottoCalculator.java
@@ -2,20 +2,20 @@ package lotto.domain.service;
 
 import lotto.domain.model.LottoPrice;
 import lotto.global.error.ErrorType;
+
 public class LottoCalculator {
     public int calculateNumberOfLotto(int userPurchasePrice) {
-        int lottoCount;
+        int lottoCount = 0;
 
         try {
             if (userPurchasePrice % LottoPrice.LOTTO_PRICE.getValue() != 0) {
                 throw new IllegalArgumentException(ErrorType.DIVIDABLE_1000_MESSAGE.getValue());
             }
             lottoCount = userPurchasePrice / LottoPrice.LOTTO_PRICE.getValue();
-            return lottoCount;
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-        return 0;
+        return lottoCount;
     }
 }

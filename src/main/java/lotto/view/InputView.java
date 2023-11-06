e9dc6b7c046d2922ef941c0cf0098b9b971bb7d9
Han <yulmam1@naver.com>
2023-11-06 14:53:03 +0900

    feat(InputView) : 사용자에게 구입할 금액을 물어보는 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.util.Parser.stringToInt;
+
+public class InputView {
+
+    public void requestMoney() {
+        System.out.println("구입금액을 입력해 주세요");
+    }
+}

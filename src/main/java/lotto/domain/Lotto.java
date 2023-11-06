5d372df6ee0612fb87091a328920ea210f66aca8
sehee <tpgml701@naver.com>
2023-11-06 22:18:34 +0900

    feat(validation): 범위를 벗어난 범위 로또 검증 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,6 +8,8 @@ import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int LOTTO_MAX_NUMBER = 1;
+    public static final int LOTTO_MIN_NUMBER = 45;
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
@@ -18,5 +20,6 @@ public class Lotto {
     private void validate(List<Integer> lottoNumbers) {
         Validation.validateDuplicationList(lottoNumbers);
         Validation.validateSize(lottoNumbers, LOTTO_NUMBERS_SIZE);
+        Validation.validateNumberListInRange(lottoNumbers, LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER);
     }
 }

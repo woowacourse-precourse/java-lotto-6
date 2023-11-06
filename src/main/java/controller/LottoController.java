69d489d3373c048f5704d20b7e44aad2d16f5a91
nayeon Lee <skdus4190@gmail.com>
2023-11-06 22:10:26 +0900

    Feat: LottoController.run() 작성
    애플리케이션의 흐름을 가지는 run() 메서드 작성

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,33 @@
+package controller;
+
+public class LottoController {
+
+    public void run(){
+        buyLotto();
+        createLotto();
+        createWinningNumbers();
+        compareLottoNumbers();
+        printResult();
+    }
+
+    private void buyLotto() {
+
+    }
+
+    private void createLotto() {
+
+    }
+
+    private void createWinningNumbers() {
+
+    }
+
+    private void compareLottoNumbers() {
+    }
+
+
+    private void printResult() {
+        
+    }
+
+}

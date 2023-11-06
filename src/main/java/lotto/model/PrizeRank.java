fa018bcceac2f9cf6c3c630bc55b3000298dbc53
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 17:16:47 +0900

    feat : 등수와 가격을 관리하는 enum 클래스 생성

---
+++ src/main/java/lotto/model/PrizeRank.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+public enum PrizeRank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000);
+
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int price;
+
+    PrizeRank(int matchCount, boolean bonusMatch, int price) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.price = price;
+    }
+}

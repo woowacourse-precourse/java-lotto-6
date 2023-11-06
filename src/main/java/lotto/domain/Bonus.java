2dd0bcf0b634de74c7abdbddca6906170084234a
prao <pch5521@gmail.com>
2023-11-06 19:39:07 +0900

    feat(Bonus): 보너스 객체에 중복 검사 후 중복이 없을 경우 생성하도록 로직 추가

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,16 +1,19 @@
 package lotto.domain;
 
+import java.util.List;
 import lotto.dto.BonusResponse;
+import lotto.validator.InputValidator;
 
 public class Bonus {
     private final int bonusNumber;
 
-    private Bonus(final int input) {
+    private Bonus(final List<Integer> answerNumbers, final int input) {
+        InputValidator.validateDuplicateBonusNumber(answerNumbers, input);
         this.bonusNumber = input;
     }
 
-    public static Bonus create(final int input) {
-        return new Bonus(input);
+    public static Bonus create(final List<Integer> answerNumbers, final int input) {
+        return new Bonus(answerNumbers, input);
     }
 
     public BonusResponse generateBonusResponse() {

b425a9cdf368681c45dfe744290f3ded2fcde74a
KIVSH <kihongkim3@hotmail.com>
2023-11-06 21:49:15 +0900

    feat: add notThousandWon method in Exception class

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,11 +1,19 @@
 package lotto;
 
 public class Exception {
-    public static void NotNumberException(String inputData) throws IllegalArgumentException {
+    public static void notNumber(String inputData) throws IllegalArgumentException {
         try {
             Integer.parseInt(inputData);
         } catch (NumberFormatException numberFormatException) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.\n");
+        }
+    }
+
+    public static void notThousandWon(String inputData) throws IllegalArgumentException {
+        int amount = Integer.parseInt(inputData);
+
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 입력 가능합니다.\n");
         }
     }
 }

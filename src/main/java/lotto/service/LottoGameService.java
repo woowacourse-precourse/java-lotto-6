3855e594b182d771d84863dbd9842828aa982407
HSBODo <vinsulill@gmaile.com>
2023-11-06 16:31:05 +0900

    refactor : Application 클래스 메서드 LottoGameService 클래스의 인스턴스 메서드로 위치 변경
    수정내용
    - createLotto()

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,4 +1,21 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGameService {
+
+    public List<Lotto> createLotto(int lottoQuantity){
+        List<Lotto> lottos= new ArrayList<>();
+
+        for(int quantity = 1; quantity <= lottoQuantity ; quantity++){
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
 }

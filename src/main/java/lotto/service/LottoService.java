37bb20e17b85c377c4f290005417c5aefdfa5cd7
jongmee <ra123221@gmail.com>
2023-11-06 22:42:18 +0900

    feat(LottoService): 발행받은 로또, 입력받은 당첨 번호와 보너스번호로 등수를 계산하고 당첨 응답을 생성하기

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,10 +5,15 @@ import static lotto.constants.Condition.MINIMUM_VALUE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.domain.BonusNumber;
 import lotto.domain.IssuedLottos;
 import lotto.domain.Lotto;
 import lotto.domain.PurchaseNumber;
+import lotto.domain.Rank;
+import lotto.utility.vo.BonusNumberRequest;
 import lotto.utility.vo.PurchaseAmountRequest;
+import lotto.utility.vo.WinningNumberRequest;
+import lotto.utility.vo.WinningResponse;
 
 public class LottoService {
     public IssuedLottos generateLottos(PurchaseAmountRequest purchaseAmount) {
@@ -24,5 +29,15 @@ public class LottoService {
         }
 
         return new IssuedLottos(lottoTickets);
+    }
+
+    public WinningResponse determineWinning(
+        IssuedLottos issuedLottos,
+        BonusNumberRequest bonusNumberRequest,
+        WinningNumberRequest winningNumberRequest) {
+        Lotto winningLotto = winningNumberRequest.convertToValidLotto();
+        BonusNumber bonusNumber = bonusNumberRequest.convertToValidBonusNumber();
+        List<Rank> ranks = issuedLottos.determineRanks(winningLotto, bonusNumber);
+        return new WinningResponse(ranks);
     }
 }

7f1564a058182fdb8b2f55fe54eea84ddcdde66f
HungKungE <jonsun59@gmail.com>
2023-11-06 21:49:21 +0900

    [utils/UserInput] 사용자 입력 클래스 일부 수정

--- src/main/java/lotto/utils/UserInput.java
+++ src/main/java/lotto/utils/UserInput.java
@@ -2,36 +2,70 @@ package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static lotto.utils.Validator.*;
 public class UserInput {
-    private String lottoPrice;
-    private String winningNumbers;
-    private String bonusNumber;
+    private int lottoPrice;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
 
     public UserInput(){
-        this.lottoPrice="";
-        this.winningNumbers="";
-        this.bonusNumber="";
+        this.lottoPrice=0;
+        this.winningNumbers= new ArrayList<>();
+        this.bonusNumber=0;
     }
 
     public void inputLottoPrice(){
         String newLottoPrice = Console.readLine();
         // TODO : 입력값 검증
         validateLottoPrice(newLottoPrice);
-        this.lottoPrice = newLottoPrice;
+        this.lottoPrice = Integer.parseInt(newLottoPrice);
     }
 
     public void inputWinningNumbers(){
-        String newWinningNumbers = Console.readLine();
-        // TODO : 입력값 검증
-        validateWinningNumbers(newWinningNumbers);
-        this.winningNumbers = newWinningNumbers;
+        String input = Console.readLine();
+        validateWinningNumbers(input);
+
+        this.winningNumbers = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();;
     }
 
     public void inputBonusNumber(){
         String newBonusNumber = Console.readLine();
-        // TODO : 입력값 검증
         validateBonusNumber(newBonusNumber);
-        this.bonusNumber = newBonusNumber;
+        this.bonusNumber = Integer.parseInt(newBonusNumber);
+    }
+
+    public int getLottoPrice() {
+        return lottoPrice;
+    }
+
+    public List<Integer> getWinningNumbers(){
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validateLottoPrice(String input){
+        validateNullOrEmpty(input);
+        validateIsNumber(input);
+    }
+
+    private void validateWinningNumbers(String input){
+        validateNullOrEmpty(input);
+        validateIsNumbers(input.split(","));
+    }
+
+
+    private void validateBonusNumber(String input){
+        validateNullOrEmpty(input);
+        validateIsNumber(input);
+        validateIsNumberInRange(Integer.parseInt(input));
     }
 }

1a450480d62ca6fd4ab007cbf2cf47d460665f41
km9404 <kmh940408@gmail.com>
2023-11-06 10:35:01 +0900

    feat (BoughtLotto): 구매 로또 번호 생성 기능 구현

---
+++ src/main/java/lotto/domian/BoughtLotto.java
@@ -0,0 +1,19 @@
+package lotto.domian;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BoughtLotto {
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+    private static final int PICK_LOTTO_COUNT = 6;
+
+    private List<Integer> numbers;
+    public List<Integer> generateLottoNumbers() {
+        numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, PICK_LOTTO_COUNT);
+        Collections.sort(numbers);
+        return numbers;
+    }
+}

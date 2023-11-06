121beb8b5387dcbf99bb054a17a6eab0dd382e36
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 19:59:06 +0900

    feat(OutputView.java): add printWinningNumberInputRequest function 사용자에게 당첨 번호 입력을 요청하는 기능을 구현하였습니다.

--- src/main/java/view/OutputVIew.java
+++ src/main/java/view/OutputVIew.java
@@ -4,6 +4,7 @@ public class OutputVIew {
 
     static final String MONEY_INPUT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
     static final String PURCHASING_RESULT_MESSAGE = "개를 구매했습니다.";
+    static final String WINNING_NUMBER_INPUT_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     void  printMoneyInputRequestMessage() {
         System.out.println(MONEY_INPUT_REQUEST_MESSAGE);
@@ -11,5 +12,9 @@ public class OutputVIew {
 
     void printPurchasingResultMessage() {
         System.out.println(PURCHASING_RESULT_MESSAGE);
+    }
+
+    void printWinningNumberInputRequestMessage() {
+        System.out.println(WINNING_NUMBER_INPUT_REQUEST_MESSAGE);
     }
 }

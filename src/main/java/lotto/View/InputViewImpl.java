92f4f34159833aa54537e3c3e833a1e6c99df5ca
Ojh <201833022@yiu.ac.kr>
2023-11-06 18:11:13 +0900

    feat: InputView 당첨 번호 입력 받기

--- src/main/java/lotto/View/InputViewImpl.java
+++ src/main/java/lotto/View/InputViewImpl.java
@@ -5,10 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputViewImpl implements InputView {
 
   private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
+  private static final String LOTTERY_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
 
   @Override
   public String readPurchase() {
     System.out.println(PURCHASE_AMOUNT_PROMPT);
+    return Console.readLine();
+  }
+
+  @Override
+  public String readLotteryNumber() {
+    System.out.println(LOTTERY_NUMBER_PROMPT);
     return Console.readLine();
   }
 }

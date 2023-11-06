62ba458b883ff2263de1d7d9bae9847cdf49ee00
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 02:49:52 +0900

    test(utils.CalculationUtils): 기능31. List에 중복된 숫자가 있는지 확인한다
    hasDuplicates 메서드는 매개변수로 입력한 List가 중복된 값을 가지고 있는지 확인하는 메서드 입니다.

--- src/main/java/lotto/utils/CalculationUtils.java
+++ src/main/java/lotto/utils/CalculationUtils.java
@@ -1,9 +1,18 @@
 package lotto.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CalculationUtils {
 
     public static boolean isNumberInRange(int number, int start, int end) {
         return number >= start && number <= end;
+    }
+
+    public static boolean hasDuplicates(List<Integer> list) {
+        Set<Integer> set = new HashSet<>(list);
+        return set.size() < list.size();
     }
 
 }

2b983150f8d744d370bb7de06e97526301f5d77c
cousim46 <wjdghldns24@naver.com>
2023-11-06 23:41:10 +0900

    test : 로또 구입 가격 / 1000에 대한 테스트 코드

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,6 +17,7 @@ class CustomerTest {
         assertThatThrownBy(() -> Customer.create(buyPrice)).isInstanceOf(
             NumberFormatException.class);
     }
+
     @DisplayName("입력한 금액이 1000원 미만일경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"900", "999"})
@@ -30,4 +33,19 @@ class CustomerTest {
         assertThatThrownBy(() -> Customer.create(buyPrice)).isInstanceOf(
             IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("입력한 금액이 숫자로만 이루어져 있고 1000원 이상 및 1000원 단위일 경우 소비자는 구입가격 / 1000 만큼 로또를 구입한다.")
+    void buyLotto() {
+        //given
+        String buyPrice = "10000";
+
+        //when
+        Customer customer = Customer.create(buyPrice);
+
+        //when
+        assertThat(customer.getBuyCount()).isEqualTo(10);
+
+    }
+
 }

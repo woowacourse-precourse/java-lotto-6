e624065d0da8bba2c514a81c4dd4c09ad8182bd2
JuSeong1130 <jsmoon1130@naver.com>
2023-11-06 21:05:10 +0900

    refactor(LottosTest) : WinningResult 테스트 메서드 분리

--- src/test/java/lotto/domain/lotto/LottosTest.java
+++ src/test/java/lotto/domain/lotto/LottosTest.java
@@ -5,48 +5,68 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import lotto.domain.result.WinningRank;
 import lotto.domain.result.WinningResult;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottosTest {
 
-    @Test
-    @DisplayName("WinningResult 구하는 기능 테스트")
-    void WinningResult_test() {
-        // given
-        Map<WinningRank, Integer> expectedWinningResult = new EnumMap<>(WinningRank.class);
-        expectedWinningResult.put(WinningRank.FIRST, 1);
-        expectedWinningResult.put(WinningRank.SECOND, 1);
-        expectedWinningResult.put(WinningRank.THIRD, 1);
-        expectedWinningResult.put(WinningRank.FOURTH, 1);
-        expectedWinningResult.put(WinningRank.FIFTH, 1);
-        expectedWinningResult.put(WinningRank.EMPTY, 0);
-        //when
-        WinningResult winningResult = getWinningResult();
+    @ParameterizedTest()
+    @MethodSource("argumentsExpectedWinningResult")
+    @DisplayName("당첨로또와 로또들을 비교하여 예상하는 expectedWinningResult가 생성되는지 테스트")
+    void 구매한로또와_당첨로또_비교하여_결과생성(Lottos lottos, WinningLotto winningLotto,
+            Map<WinningRank, Integer> expectedWinningResult) {
+        // when
+        WinningResult winningResult = lottos.calculateWinningResult(winningLotto);
 
         // then
         Assertions.assertThat(winningResult.getResult()).isEqualTo(expectedWinningResult);
     }
 
-    private static WinningResult getWinningResult() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        BonusNumber bonusNumber = new BonusNumber(7);
-        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+    private static Stream<Arguments> argumentsExpectedWinningResult() {
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new BonusNumber(7));
 
-        Lotto testLottoData1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto testLottoData2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
-        Lotto testLottoData3 = new Lotto(List.of(1, 2, 3, 4, 5, 8));
-        Lotto testLottoData4 = new Lotto(List.of(1, 2, 3, 4, 7, 8));
-        Lotto testLottoData5 = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        Lotto sixNumbersMatchedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto fiveNumbersAndBonusNumberMatchedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto fiveNumbersMatchedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        Lotto fourNumbersMatchedLotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+        Lotto threeNumbersMatchedLotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        Lotto nothingMatchedLotto = new Lotto(List.of(10, 11, 12, 13, 14, 15));
 
-        Lottos lottos = new Lottos(
-                List.of(testLottoData1, testLottoData2, testLottoData3, testLottoData4,
-                        testLottoData5));
-        WinningResult winningResult = lottos.calculateWinningResult(winningLotto);
-        return winningResult;
+        Lottos allWinningLottos = new Lottos(
+                List.of(sixNumbersMatchedLotto, fiveNumbersAndBonusNumberMatchedLotto,
+                        fiveNumbersMatchedLotto, fourNumbersMatchedLotto,
+                        threeNumbersMatchedLotto));
+
+        Lottos nothingWinningLottos = new Lottos(List.of(nothingMatchedLotto));
+
+        Map<WinningRank, Integer> expectedAllWinning = createExpectedWinningResult(1, 1, 1, 1,
+                1, 0);
+        Map<WinningRank, Integer> expectedNothingWinning = createExpectedWinningResult(0, 0, 0, 0,
+                0, 1);
+
+        return Stream.of(
+                Arguments.of(allWinningLottos, winningLotto, expectedAllWinning),
+                Arguments.of(nothingWinningLottos, winningLotto, expectedNothingWinning)
+        );
+    }
+
+    private static Map<WinningRank, Integer> createExpectedWinningResult(int first, int second,
+            int third, int fourth, int fifth, int empty) {
+        Map<WinningRank, Integer> expectedWinningResult = new EnumMap<>(WinningRank.class);
+        expectedWinningResult.put(WinningRank.FIRST, first);
+        expectedWinningResult.put(WinningRank.SECOND, second);
+        expectedWinningResult.put(WinningRank.THIRD, third);
+        expectedWinningResult.put(WinningRank.FOURTH, fourth);
+        expectedWinningResult.put(WinningRank.FIFTH, fifth);
+        expectedWinningResult.put(WinningRank.EMPTY, empty);
+        return expectedWinningResult;
     }
 }

23c43b7d4756217456a33b6ee9cc71e15df1e472
msung99 <msung6924@naver.com>
2023-11-06 12:34:08 +0900

    feat: 당첨 로또 생성 전략 WinningLottoGenerateStrategy 생성

--- src/main/java/lotto/domain/WinningLottoGenerateStrategy.java
+++ src/main/java/lotto/domain/WinningLottoGenerateStrategy.java
@@ -1,4 +1,12 @@
 package lotto.domain;
 
-public class WinningLottoGenerateStrategy {
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.LottoParser;
+
+public class WinningLottoGenerateStrategy implements LottoGenerateStrategy {
+    @Override
+    public Lotto generate(){
+        return new Lotto(LottoParser.parseWinningInputs(Console.readLine()));
+
+    }
 }

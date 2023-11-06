23ac597ec6eb4ce3e5ac34f2486f85657c5531eb
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 14:36:01 +0900

    test(inputView): 당첨번호에 공백이 있는 경우 제거하는지 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,10 +1,13 @@
 package lotto.view;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.Exception.INVALID_WINNING_NUMBERS_INPUT_TYPE;
 import static lotto.Exception.ONLY_NUMERIC_INPUT_FOR_MONEY;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +28,24 @@ class InputViewTest {
         protected void runMain() {
             InputView inputView = new InputView();
             inputView.inputMoney();
+        }
+    }
+
+    @Nested
+    class 당첨번호_입력_테스트 extends NsTest {
+        @Test
+        void 당첨번호에_공백이_있는_경우_제거() {
+            assertSimpleTest(() -> {
+                run("1,2,3,4,  5, 7");
+                assertThat(output()).contains("1", "2", "3", "4", "5", "7");
+            });
+        }
+
+        @Override
+        protected void runMain() {
+            InputView inputView = new InputView();
+            List<Integer> winningNumbers = inputView.inputWinningNumbers();
+            System.out.println(winningNumbers);
         }
     }
 }

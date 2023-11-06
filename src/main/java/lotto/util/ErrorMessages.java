c02ca4746363ab117b32a708ab72dfdf9b838584
pgd <rudeh1253@gmail.com>
2023-11-06 13:50:56 +0900

    feat: 에러 메시지 추가

--- src/main/java/lotto/util/ErrorMessages.java
+++ src/main/java/lotto/util/ErrorMessages.java
@@ -3,6 +3,8 @@ package lotto.util;
 import static lotto.util.Constants.ERROR_MESSAGE_COMMON_PREFIX;
 
 public enum ErrorMessages {
+    PURCHASE_NUMBER_IS_NOT_AN_INTEGER(ERROR_MESSAGE_COMMON_PREFIX + " 구입 금액은 정수여야 합니다."),
+    PURCHASE_NUMBER_IS_NEGATIVE(ERROR_MESSAGE_COMMON_PREFIX + " 구입 금액은 0 이상이어야 합니다."),
     PURCHASE_NUMBER_IS_NOT_MULTIPLE_OF_1000(ERROR_MESSAGE_COMMON_PREFIX + " 구입 금액은 1,000으로 나누어 떨어져야 합니다."),
     WINNING_NUMBERS_ARE_NOT_SIX(ERROR_MESSAGE_COMMON_PREFIX + " 당첨 번호는 정확히 6개의 숫자로 이루어져야 합니다."),
     WINNING_NUMBERS_CONTAINS_NON_NUMBER(ERROR_MESSAGE_COMMON_PREFIX + " 당첨 번호는 공백 없는 정수로 이루어져야 합니다."),

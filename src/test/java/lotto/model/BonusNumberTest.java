bb42b831cc6a9c1a1e235dbd2aaae270aa9fb446
jihun <pjh2996@naver.com>
2023-11-06 16:05:58 +0900

    test : 보너스 번호의 숫자 범위 유효성 검사 기능 테스트

---
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BonusNumberTest {
+
+    private final List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+
+    @DisplayName("보너스 번호에 1~45범위를 넘은 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"0", "46", "77"})
+    void createLottoByDuplicatedNumber(int input) {
+        assertThatThrownBy(() -> new BonusNumber(input, winningNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

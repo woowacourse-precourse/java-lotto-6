1d29c1dadb2ff3340e5f34cefa0c35ca55d5cd1f
CodingApe9 <pos06098@gmail.com>
2023-11-06 21:53:07 +0900

    test(*) : 변수를 상수로 변환

--- src/test/java/lotto/SoldLottoTest.java
+++ src/test/java/lotto/SoldLottoTest.java
@@ -10,13 +10,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("발행한 모든 로또 당첨 결과 확인 기능")
 public class SoldLottoTest {
+    private static final WinningLotto WINNING_LOTTO = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
     @ParameterizedTest
     @MethodSource("checkLottoResultsData")
     @DisplayName("구입한 전체 로또 결과를 학인한다.")
     void 구입로또_결과_정상처리(List<Lotto> lottoTickets, List<LottoRanking> lottoRankings) {
         SoldLotto soldLotto = new SoldLotto(lottoTickets);
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        List<LottoRanking> resultLottoRankings = soldLotto.checkLottoResults(winningLotto);
+        List<LottoRanking> resultLottoRankings = soldLotto.checkLottoResults(WINNING_LOTTO);
 
         Assertions.assertThat(resultLottoRankings).isEqualTo(lottoRankings);
     }
@@ -27,6 +28,16 @@ public class SoldLottoTest {
                         List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                                 new Lotto(List.of(1, 2, 3, 4, 5, 7))),
                         List.of(LottoRanking.FIRST_PRIZE,
+                                LottoRanking.SECOND_PRIZE)),
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 44, 45))),
+                        List.of(LottoRanking.FOURTH_PRIZE)),
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 35, 32)),
+                                new Lotto(List.of(1, 2, 3, 4, 5, 32)),
+                                new Lotto(List.of(1, 2, 3, 4, 5, 7))),
+                        List.of(LottoRanking.FOURTH_PRIZE,
+                                LottoRanking.THIRD_PRIZE,
                                 LottoRanking.SECOND_PRIZE))
         );
     }
--- src/test/java/lotto/ValidateBonusNumberTest.java
+++ src/test/java/lotto/ValidateBonusNumberTest.java
@@ -12,13 +12,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("보너스 번호 검증")
 public class ValidateBonusNumberTest {
-    private static final List<Integer> winningLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+    private static final List<Integer> WINNING_LOTTO_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
 
     @ParameterizedTest
     @ValueSource(ints = {7, 9, 22, 35, 45})
     @DisplayName("[정상처리] 보너스 번호를 검증합니다.")
     void 보너스번호_검증_정상처리() {
-        assertThatCode(() -> Validator.validateBonusNumber(7, winningLottoNumbers))
+        assertThatCode(() -> Validator.validateBonusNumber(7, WINNING_LOTTO_NUMBERS))
                 .doesNotThrowAnyException();
     }
 
@@ -27,7 +27,7 @@ public class ValidateBonusNumberTest {
     @DisplayName("[예외처리] 보너스 번호가 1~45가 아니면 예외가 발생한다.")
     void 보너스번호_검증_범위초과_예외처리(int bonusNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Validator.validateBonusNumber(bonusNumber, winningLottoNumbers))
+                .isThrownBy(() -> Validator.validateBonusNumber(bonusNumber, WINNING_LOTTO_NUMBERS))
                 .withMessageContaining(LottoException.LOTTO_NUMBER_OUT_OF_RANGE.getErrorMessage());
     }
 
@@ -36,7 +36,7 @@ public class ValidateBonusNumberTest {
     @DisplayName("[예외처리] 보너스 번호가 당첨번호에 존재하면 예외가 발생한다.")
     void 보너스번호_검증_중복_예외처리(int bonusNumber) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Validator.validateBonusNumber(bonusNumber, winningLottoNumbers))
+                .isThrownBy(() -> Validator.validateBonusNumber(bonusNumber, WINNING_LOTTO_NUMBERS))
                 .withMessageContaining(LottoException.BONUS_NUMBER_IN_WINNING_NUMBERS.getErrorMessage());
     }
 }

0515dc58019e1b7648508931f7b05e7178792509
jhsseonn <hs125bt@gmail.com>
2023-11-06 19:14:27 +0900

    feat: 로또 등수 책정에 필요한 상수 정의

--- src/main/java/lotto/common/LottoFinalConsts.java
+++ src/main/java/lotto/common/LottoFinalConsts.java
@@ -1,4 +1,16 @@
 package lotto.common;
 
 public class LottoFinalConsts {
+    public final String LOTTO_FIRST = "6";
+    public final String LOTTO_SECOND = "5+1";
+    public final String LOTTO_THIRD = "5";
+    public final String LOTTO_FOURTH = "4";
+    public final String LOTTO_FIFTH = "3";
+    public final int LOTTO_FIRST_RETURN = 2000000000;
+    public final int LOTTO_SECOND_RETURN = 50000000;
+    public final int LOTTO_THIRD_RETURN = 1500000;
+    public final int LOTTO_FOURTH_RETURN = 50000;
+    public final int LOTTO_FIFTH_RETURN = 5000;
+
+
 }

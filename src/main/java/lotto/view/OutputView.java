52a94c685d5203bafe356ec8ce9c8b4f8ec004fc
hil679 <hil679922@gmail.com>
2023-11-06 17:37:51 +0900

    Feat(OutputView): create OutputView skeleton code

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,39 @@
+package lotto.view;
+
+import lotto.domain.WinResult;
+
+import java.util.List;
+
+public class OutputView {
+    public void printMoneyInputGuideMessage() {
+
+    }
+
+    public void printLottoAmountMessage(int lottoAmount) {
+
+    }
+
+    public void printUserLottos(List<Integer> userLotto) {
+
+    }
+
+    public void printWinLottoNumbersInputGuide() {
+
+    }
+
+    public void printBonusNumbersInputGuide() {
+        
+    }
+
+    public void printWinningResultGuideMessage() {
+
+    }
+
+    public void printWinResult(WinResult winResult) {
+
+    }
+
+    public void printProfit(double profit) {
+
+    }
+}

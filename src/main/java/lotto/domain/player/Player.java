618bffd067e05d035103c3d5bc854f3c207fe38c
jae woo Jung <awfjol2008@naver.com>
2023-11-06 20:34:41 +0900

    refector: PlayerLotto 클래스명 Player로 변경

--- src/main/java/lotto/domain/player/Player.java
+++ src/main/java/lotto/domain/player/Player.java
@@ -3,14 +3,14 @@ package lotto.domain.player;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoNumber;
 
-public class PlayerLotto {
+public class Player {
 
     private Lotto lotto;
     private LottoNumber bonusNumber;
 
     private PurchaseAmount purchaseAmount;
 
-    public PlayerLotto(Lotto lotto, LottoNumber bonusNumber, PurchaseAmount purchaseAmount) {
+    public Player(Lotto lotto, LottoNumber bonusNumber, PurchaseAmount purchaseAmount) {
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
         this.purchaseAmount = purchaseAmount;

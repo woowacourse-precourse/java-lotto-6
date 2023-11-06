4d18eb451ed11fed3c8161b9dd4ae2b0ab814a80
Minkyeong Choi <sallyever@naver.com>
2023-11-06 15:52:36 -0600

    create enum

--- src/main/java/lotto/Judging.java
+++ src/main/java/lotto/Judging.java
@@ -1,6 +1,13 @@
 package lotto;
 
 public class Judging {
+    enum PrizeMoney{
+        THREE(3, 5000),
+        FOUR(4, 50000),
+        FIVE(5, 1500000),
+        BONUS(5, 30000000),
+        SIX(6, 2000000000)
+    }
     public Judging(){
 
     }

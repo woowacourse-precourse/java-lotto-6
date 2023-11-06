e4e791705ff8734aeccac2110c26bf8b828c5b0d
hlmg <devhlmg@gmail.com>
2023-11-06 21:04:32 +0900

    refactor: StringBuilder 사용해서 출력하게 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,7 @@
 package lotto.view;
 
 import java.math.BigDecimal;
-import java.util.EnumMap;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map.Entry;
 import lotto.domain.Lotto;
@@ -10,47 +10,56 @@ import lotto.domain.Rank;
 import lotto.domain.dto.Result;
 
 public class OutputView {
+    public static final String PURCHASED = "\n%d개를 구매했습니다.\n";
+    public static final String WINNING_RESULT = "\n당첨 통계\n---\n";
+    public static final String MATCH_COUNT = "개 일치";
+    public static final String HAS_BONUS = ", 보너스 볼 일치";
+    public static final String PRIZE_COUNT = " (%s원) - %d개\n";
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,###");
+    public static final String RATE_OF_RETURN = "총 수익률은 %s%%입니다.";
 
     public void printPurchasedLottos(List<Lotto> purchasedLottos) {
-        System.out.printf("\n%d개를 구매했습니다.\n", purchasedLottos.size());
+        StringBuilder message = new StringBuilder();
+        message.append(String.format(PURCHASED, purchasedLottos.size()));
         for (Lotto purchasedLotto : purchasedLottos) {
-            printPurchasedLotto(purchasedLotto);
+            appendPurchasedLotto(purchasedLotto, message);
         }
+        System.out.println(message);
     }
 
-    private void printPurchasedLotto(Lotto purchasedLotto) {
+    private void appendPurchasedLotto(Lotto purchasedLotto, StringBuilder message) {
         List<LottoNumber> numbers = purchasedLotto.getNumbers();
-        List<LottoNumber> list = numbers.stream()
+        List<LottoNumber> sortedNumbers = numbers.stream()
                 .sorted()
                 .toList();
 
-        System.out.println(list);
+        message.append(sortedNumbers).append("\n");
     }
 
     public void printWinningResult(Result winningResult) {
-        System.out.println("\n당첨 통계");
-        System.out.println("---");
-        EnumMap<Rank, Integer> rankToCount = winningResult.getRankToCount();
-        BigDecimal rateOfReturn = winningResult.getRateOfReturn();
+        StringBuilder message = new StringBuilder();
+        message.append(WINNING_RESULT);
+        appendStatistics(winningResult, message);
+        appendRateOfReturn(winningResult, message);
+        System.out.println(message);
+    }
 
-        StringBuilder sb = new StringBuilder();
-        for (Entry<Rank, Integer> rankIntegerEntry : rankToCount.entrySet()) {
-            int matchCount = rankIntegerEntry.getKey().getMatchCount();
-            sb.append(matchCount).append("개 일치");
+    private static void appendStatistics(Result winningResult, StringBuilder message) {
+        for (Entry<Rank, Integer> rankIntegerEntry : winningResult.getRankToCount().entrySet()) {
+            message.append(rankIntegerEntry.getKey().getMatchCount()).append(MATCH_COUNT);
 
-            boolean hasBonus = rankIntegerEntry.getKey().hasBonus();
-            if (hasBonus) {
-                sb.append(", 보너스 볼 일치");
+            if (rankIntegerEntry.getKey().hasBonus()) {
+                message.append(HAS_BONUS);
             }
+
             long prize = rankIntegerEntry.getKey().getPrize();
-            sb.append(" (").append(prize).append("원)");
-
             Integer value = rankIntegerEntry.getValue();
-            sb.append(" - ");
-            sb.append(value).append("개\n");
+            message.append(String.format(PRIZE_COUNT, DECIMAL_FORMAT.format(prize), value));
         }
+    }
 
-        sb.append("총 수익률은 ").append(rateOfReturn.toPlainString()).append("%입니다.");
-        System.out.println(sb);
+    private void appendRateOfReturn(Result winningResult, StringBuilder message) {
+        BigDecimal rateOfReturn = winningResult.getRateOfReturn();
+        message.append(String.format(RATE_OF_RETURN, rateOfReturn.toPlainString()));
     }
 }

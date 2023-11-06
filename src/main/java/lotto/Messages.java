2cff33f9445723f7d3d5707f4d728faf4d037c89
hackertaco <imsa1224@korea.ac.kr>
2023-11-06 19:52:11 +0900

    feat: add Request msg

--- src/main/java/lotto/Messages.java
+++ src/main/java/lotto/Messages.java
@@ -3,6 +3,7 @@ package lotto;
 public enum Messages {
     REQUEST_MONEY("구입금액을 입력해 주세요."),
     REQUEST_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
 
     ALARM_TRIAL("%d개를 구매했습니다.\n"),
     ;

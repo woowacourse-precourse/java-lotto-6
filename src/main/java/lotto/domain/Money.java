6bd4e41dd19587c94abba413100d2fb99f42657c
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-11-06 14:23:57 +0900

    feat: 구입 금액 단위가 맞는지 확인하느 기능 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class Money {
     private static final int MIN_POSITIVE_RANGE = 1;
+    private static final int SCALE = 1_000;
+    private static final int SCALE_STANDARD = 0;
 
     private final int amount;
 
@@ -16,6 +18,7 @@ public class Money {
 
     private void validate(final int amount) {
         validatePositive(amount);
+        validateScale(amount);
     }
 
     private void validatePositive(final int amount) {
@@ -26,5 +29,15 @@ public class Money {
 
     private boolean isNotPositive(final int amount) {
         return amount < MIN_POSITIVE_RANGE;
+    }
+
+    private void validateScale(final int amount) {
+        if (isNotMatchScale(amount)) {
+            throw new IllegalArgumentException("로또 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    private boolean isNotMatchScale(final int amount) {
+        return amount % SCALE != SCALE_STANDARD;
     }
 }
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,5 +15,21 @@ class MoneyTest {
     void moneyNotPositiveExceptionTest(final int amount) {
         // expected
         assertThrows(IllegalArgumentException.class, () -> Money.from(amount));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100, 1001, 1010, 1011, 1100, 1101, 1111})
+    @DisplayName("로또 구입 금액이 1,000원 단위가 아니라면 예외 발생")
+    void moneyNotMathScaleExceptionTest(final int amount) {
+        // expected
+        assertThrows(IllegalArgumentException.class, () -> Money.from(amount));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 15000, 3000})
+    @DisplayName("로또 구입 금액이 양수이고 1,000원 단위라면 정상 생성")
+    void moneyCreateTest(final int amount) {
+        // expected
+        assertDoesNotThrow(() -> Money.from(amount));
     }
 }

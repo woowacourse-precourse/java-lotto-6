ba9130e2f3221a281b4d26e688f574c69c74bc59
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-06 19:07:03 +0900

    Test: lottoTicket validate

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -1,2 +1,17 @@
-package lotto;public class LottoTicketTest {
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTicketTest {
+    @DisplayName("로또 구매 금액이 1000원 단위로 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoTicketBy() {
+        assertThatThrownBy(() -> new LottoTicket(12345))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

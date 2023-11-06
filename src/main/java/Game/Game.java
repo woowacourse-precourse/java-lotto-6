1218e8fea273738e4f5d40f4d8a30271ca6ecea9
SeungGwa123 <skwan123@naver.com>
2023-11-07 02:19:37 +0900

    Feat: Add askMoney
    구입 금액을 물어보고 로또를 구매하고 출력을 하는 함수 추가

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,2 +1,18 @@
-package Game;public class Game {
+package Game;
+
+import Input.Input;
+import Output.Output;
+import lotto.BuyLotto;
+
+public class Game {
+    private static Input input = new Input();
+    private static Output output = new Output();
+    private static BuyLotto buylotto = new BuyLotto();
+    private static int lotto_number;
+
+    public void askMoney() {
+        lotto_number = input.lottoBuyMoney();
+        output.buyNumberPaperAns(lotto_number);
+        buylotto.buyAllLotto(lotto_number);
+    }
 }

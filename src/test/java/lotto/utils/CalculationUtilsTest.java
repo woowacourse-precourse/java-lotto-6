ef1d2b61779e8310e584283ea16ec7a010a58795
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 02:50:43 +0900

    feat(utils.CalculationUtils): 기능31. List에 중복된 숫자가 있는지 확인한다
    List에 중복된 값이 있을 때와 없을 때로 나누어서 hasDuplicates가 정상동작하는지 확인하는 테스트 코드를 작성했습니다.

--- src/test/java/lotto/utils/CalculationUtilsTest.java
+++ src/test/java/lotto/utils/CalculationUtilsTest.java
@@ -1,10 +1,13 @@
 package lotto.utils;
 
+import static lotto.utils.CalculationUtils.hasDuplicates;
 import static lotto.utils.CalculationUtils.isNumberInRange;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class CalculationUtilsTest {
 
@@ -42,6 +45,32 @@ class CalculationUtilsTest {
             result = isNumberInRange(target, start, end);
             assertThat(result).isEqualTo(false);
         }
+    }
+
+    @Test
+    @DisplayName("기능31 테스트 : 리스트에 중복값이 없으면 hasDuplicates는 false를 반환한다.")
+    void hasDuplicatesShouldReturnFalseWhenListDoesNotHaveDuplicates() {
+        // given
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        boolean result = hasDuplicates(list);
+
+        // then
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("기능31 테스트 : 리스트에 중복값이 있으면 hasDuplicates는 true를 반환한다.")
+    void hasDuplicatesShouldReturnFalseWhenListHaveDuplicates() {
+        // given
+        List<Integer> list = List.of(1, 1, 3, 4, 5, 6);
+
+        // when
+        boolean result = hasDuplicates(list);
+
+        // then
+        assertThat(result).isEqualTo(true);
     }
 
 }

54076914ec1ed3a6977f8db18ed5173d6602354c
park_yeongyu <sjjs7771@naver.com>
2023-11-06 22:55:46 +0900

    feat(LottoView) : 구입금액(price) 를 지정하고 가져올 수 있는 함수 작성

--- src/main/java/lotto/View/LottoView.java
+++ src/main/java/lotto/View/LottoView.java
@@ -1,11 +1,23 @@
 package lotto.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoView {
-    public void printInputPrice(){
+    private int price;
+
+    public void printInputPrice() {
         System.out.println("구입 금액을 입력해 주세요.");
     }
 
-    public void printNumOfLotto(int number){
-        System.out.println(number+"개를 구매했습니다.");
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void printNumOfLotto(int number) {
+        System.out.println(number + "개를 구매했습니다.");
     }
 }

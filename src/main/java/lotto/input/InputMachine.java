15a7486dd05e6cf43a83e005f3954335e376ef4b
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 18:01:19 +0900

    Refactor: 입력만 받는 기능으로 수정
    검증 기능을 제거해 입력만 받는 기능으로 수정

--- src/main/java/lotto/input/InputMachine.java
+++ src/main/java/lotto/input/InputMachine.java
@@ -1,54 +1,32 @@
 package lotto.input;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.WinningNumberMaker;
-import lotto.validation.NumbersValidation;
 
 public class InputMachine {
 
-	private static final String SEPARATOR = ",";
 	private static final String PURCHASE_MESSAGE = "구매금액을 입력해 주세요.";
 	private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 	private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-	public static String inputPurchaseAmount() {
+	public String inputPurchaseAmount() {
 
 		System.out.println(PURCHASE_MESSAGE);
-		String purchaseAmount = Console.readLine();
-
-		NumbersValidation.validateOnlyNumber(purchaseAmount);
-
-		return purchaseAmount;
+		return Console.readLine();
 
 	}
 
-	public static String inputWinningNumber() {
+	public String inputWinningNumber() {
 
+		System.out.println();
 		System.out.println(WINNING_NUMBER_MESSAGE);
-		String winningNumber = Console.readLine();
-
-		validateSeparator(winningNumber);
-
-		return winningNumber;
+		return Console.readLine();
 	}
 
-	private static void validateSeparator(String inputNumber) {
+	public String inputBonusNumber() {
 
-		if (!inputNumber.contains(SEPARATOR)) {
-			throw new IllegalArgumentException("구분자(,)만을 이용해 나눠주세요");
-		}
-
-	}
-
-	public static String inputBonusNumber() {
-
+		System.out.println();
 		System.out.println(BONUS_NUMBER_MESSAGE);
-		String bonusNumber = Console.readLine();
-
-		NumbersValidation.validateOnlyNumber(bonusNumber);
-
-		return bonusNumber;
+		return Console.readLine();
 	}
-
 
 }

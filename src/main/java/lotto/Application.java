4d542902549f6f941af1bb4e03245a9ad7fd8574
Hooni07 <gkxogns21@gmail.com>
2023-11-06 14:39:55 +0900

    feat: add input & output method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -22,14 +22,27 @@ public class Application {
         // 구매 확인 및 리스트
         int lottoNum = buyLotto / 1000;
         System.out.println(lottoNum + "개를 구매했습니다.");
+        List<List<Integer>> listNumbers = new ArrayList<>();
+        for (int i = 0; i < lottoNum; i++) {
+            List<Integer> tmpNum = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            listNumbers.add(tmpNum);
+        }
 
         // 당첨번호 입력
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winNumber = new ArrayList<>();
+        String winNumber = Console.readLine();
+        // ","을 기준으로 하여 각 경주 참가자들을 Array에 임시 저장
+        String[] winNumberArray = winNumber.split(",");
+        for (int i = 0; i < winNumberArray.length; i++) {
+//            winNumber.add(convertInputToNumbers(winNumberArray[i]));
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNum = Integer.parseInt(Console.readLine());
 
 
         // 당첨 통계
-
-
-
 
 
     }

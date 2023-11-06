72049c290aca6ad2b093e178b4e27521b710d63f
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-07 00:12:12 +0900

    🎯feat(domain/WinningLottoNumber) : 당첨번호 convert 기능 함수 구현

---
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lotto.Lotto;
+import lotto.validate.InputValidation;
+
+public class WinningLottoNumbers {
+    private Lotto winningLotto;
+    private int bouns;
+    InputValidation inputValidation = new InputValidation();
+
+    public WinningLottoNumbers(String numbers, String bouns) {
+        this.winningLotto = convertWinningNumbers(numbers);
+        this.bouns = convertBonus(bouns);
+    }
+
+    // 담첨번호 String > List<Integer>
+    private Lotto convertWinningNumbers(String numbers) {
+        return new Lotto(Stream.of(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
+    }
+
+}

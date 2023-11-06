7c9e588012a36b6e3f22142c700ffc9f815695be
Genius-PAK <pyj789123@gmail.com>
2023-11-06 14:08:45 +0900

    chore(PurchaseAmountConverter): 구입 가능한 로또를 반환하는 대신 금액 자체를 반환하도록 변경

--- src/main/java/lotto/controller/converter/PurchaseAmountConverter.java
+++ src/main/java/lotto/controller/converter/PurchaseAmountConverter.java
@@ -1,11 +1,8 @@
-package lotto.controller;
-
-import lotto.GameConstants;
+package lotto.controller.converter;
 
 public class PurchaseAmountConverter implements Converter<Integer> {
     @Override
     public Integer convert(String purchaseAmount) {
-        int numericPurchaseAmount = Integer.parseInt(purchaseAmount);
-        return numericPurchaseAmount / GameConstants.UNIT_PURCHASE_PRICE;
+        return Integer.parseInt(purchaseAmount);
     }
 }

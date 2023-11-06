57e6f2bbcc1134464063996691bdf0b4ed36fc21
tam31 <wxogud3377@naver.com>
2023-11-06 14:02:58 +0900

    Feat(InputView) : 보너스 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,13 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.validation.InputValidation;
 
+import java.util.List;
+
 public class InputView {
 
     private static final String PURCHASE_AMOUNT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_LOTTO_WINNING_Bonus_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     InputValidation inputValidation = new InputValidation();
 
@@ -34,6 +37,19 @@ public class InputView {
             System.out.println(e.getMessage());
             System.out.println();
             return lottoWinningNumber();
+        }
+    }
+
+    public String lottoWinningBonus(List<Integer> lottoWinningNumbers) {
+        try{
+            System.out.println(INPUT_LOTTO_WINNING_Bonus_MESSAGE);
+            String bonusNumber = Console.readLine();
+            inputValidation.validationLottoWinningBonusNumber(bonusNumber, lottoWinningNumbers);
+            return bonusNumber;
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            System.out.println();
+            return lottoWinningBonus(lottoWinningNumbers);
         }
     }
 }

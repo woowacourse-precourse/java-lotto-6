5cac1a3c89a1032ac09240e3d1b21513fa1d8ef6
sehee <tpgml701@naver.com>
2023-11-06 23:25:44 +0900

    feat: 사용자에게 금액 입력 받는 기능

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,9 +1,24 @@
 package lotto.controller;
 
+import lotto.util.Utils;
+import lotto.view.InputMessage;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Controller {
 
     public void run() {
         // TODO: 11/6/23
+    }
+
+    private void BeforeLottoGame() {
+
+    }
+
+    private int getInputAmount() {
+        InputMessage.MoneyInputMessage();
+        String userInput = readLine();
+        return Utils.stringToInteger(userInput);
     }
 
 }

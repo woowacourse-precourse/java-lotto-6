f4e7359a94808139928caefe7fb13567745b96e8
rbgusdlza <rbgusdlza@gmail.com>
2023-11-07 02:14:51 +0900

    feat(Validator): makeWinningNumbers 메서드 구현

--- src/main/java/lotto/util/WinningNumbersMaker.java
+++ src/main/java/lotto/util/WinningNumbersMaker.java
@@ -10,15 +10,15 @@ public class WinningNumbersMaker {
     }
 
     public static Lotto makeWinningNumbers(String input) {
-        List<Integer> separatedInput = separateNumbers(input);
-        
+        List<Integer> separatedNumbers = separateNumbers(input);
+        return new Lotto(separatedNumbers);
     }
 
     public static List<Integer> separateNumbers(String input) {
         String[] separatedInput = input.split(",");
         List<Integer> separatedNumbers = new ArrayList<>();
-        for (int i = 0; i < separatedInput.length; i++) {
-            Integer number = Integer.parseInt(separatedInput[i]);
+        for (String inputNum : separatedInput) {
+            Integer number = Integer.parseInt(inputNum);
             separatedNumbers.add(number);
         }
         return separatedNumbers;

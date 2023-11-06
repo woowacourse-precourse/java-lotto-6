502b2157118625789c75eadfc88c41b2c487b2d3
alenjb <alenjb@naver.com>
2023-11-07 06:40:02 +0900

    ✨feat(LottoResultChecker): 상금을 계산하는 기능 구현 및 상금의 자료형 변경
    상금을 계산하는 기능을 구현하고 상금의 자료형을 int에서 String으로 변경함.

--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -7,13 +7,15 @@ public class LottoResultChecker {
 
     final NumberGenerator numberGenerator = new NumberGenerator();
     List<Integer> winningNumbers; // 당첨번호
+    int bonusNumber; // 당첨 보너스 번호
     List<Integer> userLottoNumbers; // 사용자가 구매한 번호
     int rank; // 등수
-    int reward; // 상금
+    String reward; // 상금
 
-    public LottoResultChecker(List<Integer> winningNumbers, List<Integer> userLottoNumbers) {
+    public LottoResultChecker(List<Integer> winningNumbers, List<Integer> userLottoNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.userLottoNumbers = userLottoNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public int countMatchNumbers(){
@@ -22,6 +24,16 @@ public class LottoResultChecker {
                         .anyMatch(Predicate.isEqual(old)))
                 .toList();
         return matchNumbers.size();
+    }
+    
+
+    public String calculateReward(double matchNumberCount){
+        if(matchNumberCount == Rank.FIRST_PLACE.getMatchNumberCount()) return Rank.FIRST_PLACE.getReward();
+        if(matchNumberCount == Rank.SECOND_PLACE.getMatchNumberCount()) return Rank.SECOND_PLACE.getReward();
+        if(matchNumberCount == Rank.THIRD_PLACE.getMatchNumberCount()) return Rank.THIRD_PLACE.getReward();
+        if(matchNumberCount == Rank.FOURTH_PLACE.getMatchNumberCount()) return Rank.FOURTH_PLACE.getReward();
+        if(matchNumberCount == Rank.FIFTH_PLACE.getMatchNumberCount()) return Rank.FIFTH_PLACE.getReward();
+        return Rank.NONE.getReward(); // 매치되는 숫자 개수가 0인 경우
     }
 
 }

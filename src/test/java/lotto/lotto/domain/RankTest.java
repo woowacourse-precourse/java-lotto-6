04a9d6dbf9bc3cb3d9e0c2274565ba069fbff657
gengminy <kls1998@naver.com>
2023-11-06 18:13:19 +0900

    test: 당첨 결과 테스트 작성

---
+++ src/test/java/lotto/lotto/domain/RankTest.java
@@ -0,0 +1,62 @@
+package lotto.lotto.domain;
+
+import lotto.domain.Draw;
+import lotto.domain.Rank;
+import lotto.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RankTest {
+    @DisplayName("1등 당첨 정보를 반환한다.")
+    @Test
+    void createFirst() {
+        Rank rank = Rank.valueOf(6, false);
+
+        Assertions.assertEquals(rank, Rank.FIRST);
+    }
+
+    @DisplayName("2등 당첨 정보를 반환한다.")
+    @Test
+    void createSecond() {
+        Rank rank = Rank.valueOf(5, true);
+
+        Assertions.assertEquals(rank, Rank.SECOND);
+    }
+
+    @DisplayName("3등 당첨 정보를 반환한다.")
+    @Test
+    void createThird() {
+        Rank rank = Rank.valueOf(5, false);
+
+        Assertions.assertEquals(rank, Rank.THIRD);
+    }
+
+    @DisplayName("4등 당첨 정보를 반환한다.")
+    @Test
+    void createFourth() {
+        Rank rank = Rank.valueOf(4, false);
+
+        Assertions.assertEquals(rank, Rank.FOURTH);
+    }
+
+    @DisplayName("5등 당첨 정보를 반환한다.")
+    @Test
+    void createFifth() {
+        Rank rank = Rank.valueOf(3, false);
+
+        Assertions.assertEquals(rank, Rank.FIFTH);
+    }
+
+    @DisplayName("낙첨 정보를 반환한다.")
+    @Test
+    void createMiss() {
+        Rank rank = Rank.valueOf(0, false);
+
+        Assertions.assertEquals(rank, Rank.MISS);
+    }
+}

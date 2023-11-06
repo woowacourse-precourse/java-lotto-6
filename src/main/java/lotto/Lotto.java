fd964a24f6ae8a99beb92ff23dfba1c24fea2a5a
kkw2758 <dtc3229@gmail.com>
2023-11-06 15:03:06 +0900

    Feat(Lotto) : 발행한 로또에 대한 예외 처리 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,58 @@
 package lotto;
 
+import constants.NumberType;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLotto(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateLotto(List<Integer> numbers) {
+        validateLength(numbers);
+        validateDuplicateMember(numbers);
+        validateAllNumberInRange(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (checkLength(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean checkLength(List<Integer> numbers) {
+        return numbers.size() != NumberType.LOTTO_LENGTH.getValue();
+    }
+
+    private void validateDuplicateMember(List<Integer> numbers) {
+        if (checkHasDuplicateMember(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkHasDuplicateMember(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+    }
+
+    private void validateAllNumberInRange(List<Integer> numbers) {
+        if (!checkAllNumberInRange(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkAllNumberInRange(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .filter(num -> checkNumberInRange(num))
+                .count() == numbers.size();
+    }
+
+    private boolean checkNumberInRange(int number) {
+        return number >= NumberType.MIN_LOTTO_NUMBER.getValue() && number <= NumberType.MAX_LOTTO_NUMBER.getValue();
+    }
 }

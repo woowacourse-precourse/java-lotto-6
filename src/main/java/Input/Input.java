0bbee71deef5ce7c047d91535796b455e3241d18
SeungGwa123 <skwan123@naver.com>
2023-11-06 21:18:56 +0900

    Feat: Update lottoBuyMoney
    output, except을 private static으로 바꾸고 몇장 샀는지 출력하도록 수정

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -1,14 +1,20 @@
 package Input;
 
+import Except.Except;
 import Output.Output;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
+    private static int money;
+    private static Output output = new Output();
+    private static Except except = new Except();
     public void lottoBuyMoney(){
-        Output output = new Output();
-        output.buyMoneyAsk();
-        String money_string = readLine();
-        int money = outOfRangeMoney(money);
+        do {
+            output.buyMoneyAsk();
+            String money_string = readLine();
+            money = except.outOfRangeMoney(money_string);
+        }while(money<0);
+        output.buyNumberPaperAns(money&1000);
     }
 }

248080189fef09b1cade9349f9c164d1ea17b440
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-06 20:47:12 +0900

    feat(Constants) : LOTTO_PRICE 변수 생성

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static final int MIN_RANGE_NUMBER = 1;
     public static final int MAX_RANGE_NUMBER = 45;
     public static final int DESIGNATED_LOTTO_LENGTH = 6;
+    public static final int LOTTO_PRICE = 1000;
 
     // ERROR Message
     public static final String ERROR = "[ERROR] ";

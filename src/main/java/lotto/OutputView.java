8bc008637ed1e7cecb7edcadb3ff8c48d6ebc110
unknown <gpfls981220@gmail.com>
2023-11-07 02:05:36 +0900

    feat: add output for entering winning numbers

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     void outputNumberOfPurchases(int number) {
         System.out.println(number + "개를 구매했습니다.");
     }
+    
+    void outputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }

139905985befb9db2c4237d5114a613a62630ba7
Yeriimii <seojs0511@naver.com>
2023-11-07 00:41:12 +0900

    feat(Lotto): 중복된 번호가 있는지 검증할 수 있다

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import lotto.system.validator.Validator;
+
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +9,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +20,7 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Validator.validateDuplicateNumber(numbers);
+    }
 }

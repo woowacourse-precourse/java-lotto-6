53d6daa824871b00b5fee762feb07f504f4c6abf
yshong1998 <yshong1998@gmail.com>
2023-11-07 04:54:07 +0900

    Refactor(ExceptionMessage): 예외 메세지 변경

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -1,13 +1,15 @@
 package lotto.message;
 
-import lotto.constant.ConstantValue;
+import static lotto.constant.ConstantValue.*;
 
 public class ExceptionMessage {
 
-    public static final String INPUT_OUT_OF_RANGE = "[ERROR] 범위에서 벗어난 값을 입력할 수 없습니다.";
-    public static final String INPUT_NOT_INTEGER = "[ERROR] 숫자 이외에는 입력할 수 없습니다.";
-    public static final String INPUT_NOT_DIVISIBLE = "[ERROR] " + ConstantValue.LOTTO_PRICE + "원의 배수만 입력할 수 있습니다.";
-    public static final String INPUT_NOT_POSITIVE = "[ERROR] 양수만 입력할 수 있습니다.";
-    public static final String INPUT_NOT_DISTINCT = "[ERROR] 중복된 값을 입력할 수 없습니다.";
-    public static final String NUMBER_SIZE_NOT_MATCH = "[ERROR] " + ConstantValue.LOTTO_NUMBERS_SIZE + " 개의 숫자를 입력해 주세요";
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String INPUT_OUT_OF_RANGE = ERROR_MESSAGE +
+            START_INCLUSIVE + " ~ " + END_INCLUSIVE +"의 범위에서 벗어난 값을 입력할 수 없습니다.";
+    public static final String INPUT_NOT_INTEGER = ERROR_MESSAGE + " 정수 이외에는 입력할 수 없습니다.";
+    public static final String INPUT_NOT_DIVISIBLE = ERROR_MESSAGE + LOTTO_PRICE + "원의 배수만 입력할 수 있습니다.";
+    public static final String INPUT_NOT_POSITIVE = ERROR_MESSAGE + " 양수만 입력할 수 있습니다.";
+    public static final String INPUT_NOT_DISTINCT = ERROR_MESSAGE + " 중복된 값을 입력할 수 없습니다.";
+    public static final String NUMBER_SIZE_NOT_MATCH = ERROR_MESSAGE + ",로 구분된 " + LOTTO_NUMBERS_SIZE + "개의 숫자를 입력해 주세요";
 }

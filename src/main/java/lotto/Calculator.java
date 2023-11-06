a2b04219b39d4f8f7bc62ab35f79cc8ae0298227
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-06 20:54:51 +0900

    Style: 고침 줄바꿈, 띄어쓰기

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 public class Calculator {
 
-    public int getProfitMoney(List<MatchingCount> lottoResults){
-        int sum =0;
-        for(MatchingCount lottoMatching : lottoResults ){
+    public int getProfitMoney(List<MatchingCount> lottoResults) {
+        int sum = 0;
+        for (MatchingCount lottoMatching : lottoResults) {
             int prize = lottoMatching.getPrizeMoney();
             sum += prize;
         }
         return sum;
     }
 
-    public double getProfitPercentage(int sumPrize, int lottoVolume){
-        int initialMoney = lottoVolume *1000;
-        double profitPer = (double) sumPrize/initialMoney * 100;;
+    public double getProfitPercentage(int sumPrize, int lottoVolume) {
+        int initialMoney = lottoVolume * 1000;
+        double profitPer = (double) sumPrize / initialMoney * 100;
         profitPer = Math.round(profitPer * 10d) / 10d;
         return profitPer;
     }

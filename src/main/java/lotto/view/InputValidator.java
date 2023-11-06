6915e9dece8093afb83af9431c320fccd57f3bea
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-07 01:17:02 +0900

    feat(InputValidator) : 유저에게 받은 입력에 대한 예외 처리 기능 구현

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -3,13 +3,46 @@ package lotto.view;
 import lotto.global.ResponseStatus;
 import lotto.global.error.ErrorMessage;
 
-import static lotto.global.constant.LottoConstant.LOTTO_PRICE;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static lotto.global.constant.LottoConstant.*;
 
 public class InputValidator {
 
     public static ResponseStatus purchasePriceCheck(int lottoPurchasePrice){
         if(lottoPurchasePrice < LOTTO_PRICE || lottoPurchasePrice % LOTTO_PRICE != 0){
             throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_PURCHASE_PRICE.getMessage());
+        }
+        return ResponseStatus.OK;
+    }
+
+    public static ResponseStatus isInputValid(String userInput){
+        if (userInput == null || userInput.equals(" ") || userInput.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_IS_INVALID.getMessage());
+        }
+        return ResponseStatus.OK;
+    }
+
+    public static ResponseStatus isInputDigit(String userInput){
+        isInputValid(userInput);
+        if(!Pattern.matches("^[1-9]*$",userInput)) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_IS_NOT_NUMBER.getMessage());
+        }
+        return ResponseStatus.OK;
+    }
+
+
+    public static <E> ResponseStatus lottoNumbersEmptyCheck(List<E> lottoNumbers){
+        if(lottoNumbers.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_IS_INVALID.getMessage());
+        }
+        return ResponseStatus.OK;
+    }
+
+    public static ResponseStatus isLottoInRange(int number){
+        if(number >= LOTTO_NUMBER_RANGE_START && number <= LOTTO_NUMBER_RANGE_END){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
         }
         return ResponseStatus.OK;
     }
--- src/main/java/lotto/view/LottoInput.java
+++ src/main/java/lotto/view/LottoInput.java
@@ -2,10 +2,21 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoInput {
 
     public int lottoPurchaseInput(){
         String lottoPurchasePrice = Console.readLine();
         return Integer.parseInt(lottoPurchasePrice);
+    }
+
+    private List<String> parseByComma(String input){
+        List<String> parsedInput = Arrays.stream(input.split(",")).map(String::trim).toList();
+        InputValidator.isListEmpty(parsedInput);
+        parsedInput.forEach(InputValidator::isInputValid);
+
+        return parsedInput;
     }
 }

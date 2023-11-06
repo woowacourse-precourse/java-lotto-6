bdd6bae489a15975c42915937ec583b4711ce00a
Ukj0ng <xb253q@gmail.com>
2023-11-06 18:57:13 +0900

    feat(LottoCondition): 로또의 조건 명세
    명세한 조건:
    - 로또 번호의 최소값
    - 로또 번호의 최대값
    - 로또 한 장에 들어가는 번호의 수
    - 로또 한 장의 가격

--- src/main/java/lotto/model/LottoCondition.java
+++ src/main/java/lotto/model/LottoCondition.java
@@ -1,2 +1,18 @@
-package lotto.model;public class LottoCondition {
+package lotto.model;
+
+public enum LottoCondition {
+    MIN_NUMBER(1),
+    MAX_NUMBER(45),
+    COUNT(6),
+    LOTTO_PRICE(1000);
+
+    private final int number;
+
+    LottoCondition(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
 }

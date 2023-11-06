e3b2964586f7b2e9a875bfeaff6097491ee4842f
NAM GIBEOM <rlqja0523@naver.com>
2023-11-06 11:41:58 +0900

    feat: 구분자로 문자열을 숫자 리스트를 변환하는 기능 추가

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -1,13 +1,30 @@
 package lotto.util;
 
+import static lotto.enums.Delimiter.COMMA;
 import static lotto.enums.ErrorMassage.INVALID_NUMBER;
+import static lotto.enums.ErrorMassage.NOT_SEPARATE_BY_DELIMITER;
+import static lotto.enums.LottoConfig.LOTTO_COUNT;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class StringUtil {
-    public static int convertToInt(String input) {
+    public static int convertToInt(final String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_NUMBER.getMassage());
         }
+    }
+
+    public static List<Integer> convertToIntListByDelimiter(final String input) {
+        String[] inputs = input.split(COMMA.getValue());
+        if (inputs.length != LOTTO_COUNT.getValue()) {
+            throw new IllegalArgumentException(NOT_SEPARATE_BY_DELIMITER.getMassage());
+        }
+
+        return Arrays.stream(inputs)
+                .map(StringUtil::convertToInt)
+                .toList();
     }
 }

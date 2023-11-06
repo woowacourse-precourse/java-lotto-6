3621bb7eec4ebddc37623aaf28994bdd8760baac
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 18:19:10 +0900

    feat: 로또 당첨 기능 클래스 구현

--- src/main/java/lotto/model/LottoMatchService.java
+++ src/main/java/lotto/model/LottoMatchService.java
@@ -1,2 +1,19 @@
-package lotto.model;public class LottoMatchService {
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoMatchService {
+    public List<Ranking> checkWinningNumbers(List<Lotto> lottos, List<Integer> winningNumbers) {
+        return lottos.stream()
+                .map(lotto -> findMatches(lotto, winningNumbers))
+                .collect(Collectors.toList());
+    }
+
+    private Ranking findMatches(Lotto lotto, List<Integer> winningNumbers) {
+        int matchCount = (int) lotto.getNumbers().stream()
+                .filter(winningNumbers::contains)
+                .count();
+        return Ranking.findByMatchCount(matchCount);
+    }
 }

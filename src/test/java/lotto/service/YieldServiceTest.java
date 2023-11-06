58ef9610b97b214a82fcbb37dc0d06d0d9643292
김효준 <khyojun1104@gmail.com>
2023-11-07 01:45:13 +0900

    test(lotto): 수익률 계산 로직 테스트 추가!

--- src/test/java/lotto/service/YieldServiceTest.java
+++ src/test/java/lotto/service/YieldServiceTest.java
@@ -2,11 +2,29 @@ package lotto.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class YieldServiceTest {
 
+    YieldService yieldService;
+
+    @BeforeEach
+    void init(){
+        yieldService=new YieldService();
+    }
+
     @Test
     void calculateYield() {
+        //given
+        Long result=5000L;
+        Long money=8000L;
+
+        //when
+        double calculateResult = yieldService.calculateYield(result, money);
+
+        //then
+        Assertions.assertThat(calculateResult).isEqualTo(62.5f);
     }
 }

012bed4cf15a6179c3a4390aa29bc979e27adad9
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-07 04:06:29 +0900

    [feat : outputView] 생성된 로또 번호 출력 메소드

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,23 @@
 package lotto.controller;
 
-import lotto.Lotto;
+import lotto.model.Lotto;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
-    List<Lotto> lottos = new ArrayList<>();
+    private static int amount;
+    private static Lotto winningNumbers;
+    private static int bonusNumber;
 
+    List<Lotto> lottos = new ArrayList<>();
+    //amount 입력 받아서 모델에서 validation 처리 -> amount 모델 생성
+    //winning 입력 받아서 모델에서 validation 처리 -> Lotto에서
+    //bonusNumber 입력 받아서 모델에서 validation 처리 -> bonusNum 모델 생성?
+
+    //amount 값만큼 랜덤 로또 생성, lottos에 저장
+
+    //각 로또가 몇개 맞았는지 확인, lottos, amount, winningNumbers, bonusNumber 등등 활용,
+    //모델 생성(로또 결과 모델)해서 각 로또가 몇개 맞혔는지 보너스는 맞혔는지 확인하고 map에 넣기
+    //수익률 출력 -> 당첨금/로또 구입금
 }
--- src/main/java/lotto/view/outputView.java
+++ src/main/java/lotto/view/outputView.java
@@ -1,7 +1,19 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.List;
+
 public class outputView {
     //발행된 로또 번호 출력
+    public static void IssuedLottoNumbers(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
     //당첨 내역 출력
+
+
     //총 수익률 출력
 }

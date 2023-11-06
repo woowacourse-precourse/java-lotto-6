2e1783b0ac8a3491ce8137af59f84f2db661de76
gunwoong <gwj0421@gmail.com>
2023-11-06 21:02:48 +0900

    test : View 검증 간략화

--- src/main/java/lotto/validation/ViewValidate.java
+++ src/main/java/lotto/validation/ViewValidate.java
@@ -1,26 +1,25 @@
 package lotto.validation;
 
 import lotto.config.ErrorMessage;
-import lotto.util.CustomStringUtil;
 
 public class ViewValidate {
     private ViewValidate() {
     }
 
     public static void validateNotSpaceOrEmpty(String content) {
-        if (CustomStringUtil.isSpaceOrNull(content)) {
+        if (content.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.SPACE_OR_EMPTY.getMessage());
         }
     }
 
     public static void validateNotNull(String content) {
-        if (CustomStringUtil.isNull(content)) {
+        if (content == null) {
             throw new NullPointerException(ErrorMessage.NULL.getMessage());
         }
     }
 
-    public static void validateConvertingString2Int(String contetnt) {
-        if (!CustomStringUtil.canConvertString2Int(contetnt)) {
+    public static void validateConvertingString2Int(String content) {
+        if (!content.matches("\\d+")) {
             throw new NumberFormatException(ErrorMessage.STRING_IS_NOT_INT.getMessage());
         }
     }

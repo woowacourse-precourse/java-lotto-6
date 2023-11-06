c2b49f413d1b226452076fc73c5ab65a1e5d01a1
le2sky <leehaneul990623@gmail.com>
2023-11-06 21:31:16 +0900

    refactor : print 메서드 추출

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 class OutputView {
 
-    private static final String PRINT_ISSUANCE_RESULT_FORMAT = "%n%d개를 구매했습니다.%n";
+    private static final String PRINT_ISSUANCE_RESULT_SIZE_FORMAT = "%n%d개를 구매했습니다.%n";
 
     public static void printIssuanceResult(List<List<Integer>> issuanceResult) {
-        System.out.format(PRINT_ISSUANCE_RESULT_FORMAT, issuanceResult.size());
+        printIssuanceResultSize(issuanceResult);
+        printIssuanceResultAll(issuanceResult);
+        printPadding();
+    }
 
+    private static void printIssuanceResultSize(List<List<Integer>> issuanceResult) {
+        System.out.format(PRINT_ISSUANCE_RESULT_SIZE_FORMAT, issuanceResult.size());
+    }
+
+    private static void printIssuanceResultAll(List<List<Integer>> issuanceResult) {
         issuanceResult.forEach(System.out::println);
+    }
+
+    private static void printPadding() {
+        System.out.println();
     }
 }

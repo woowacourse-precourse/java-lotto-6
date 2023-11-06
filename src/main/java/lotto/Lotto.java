58078439b0146235b03e3ed744790f5b851e13b5
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-11-06 11:13:24 +0900

    feat : 입력 메소드와 입력값 검증 처러 메소드

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,32 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int bonusNumber;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers, int bonusNumber) {
         validate(numbers);
+        duplicateCheck(numbers);
         this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력된 숫자가 6개가 아닙니다!.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicateCheck(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+
+        if (set.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
 }

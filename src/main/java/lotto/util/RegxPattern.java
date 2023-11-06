f52125016cd1f86f5f4c92c170e6ef0bb2019401
DevFrog92 <yongsu1481@gmail.com>
2023-11-06 14:26:47 +0900

    feat: implement RegxPattern util class

---
+++ src/main/java/lotto/util/RegxPattern.java
@@ -0,0 +1,7 @@
+package lotto.util;
+
+public class RegxPattern {
+        public static final String ONLY_DIGIT = "^\\d+$";
+        public static final String LOTTO_NUMBER_RANGE = "^(?:[1-9]|[1-3][0-9]|4[0-5])$";
+        public static final String RESULT_FORMAT = "^\\d+(,\\d+)*$";
+}

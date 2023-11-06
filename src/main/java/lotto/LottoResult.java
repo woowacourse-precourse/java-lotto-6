b01f1dec66dc958b9be61f3376fe675f81e83da9
aammddkkzxc <ideal0427@naver.com>
2023-11-06 22:56:09 +0900

    feat: 번호 비교 결과를 토대로 당첨 내역 판단 기능 구현

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoResult {
@@ -9,5 +10,32 @@ public class LottoResult {
     public LottoResult(List<Integer> counted, List<Boolean> checked) {
         this.counted = counted;
         this.checked = checked;
+    }
+
+    public List<Rank> getRanks() {
+        List<Rank> ranks = new ArrayList<>();
+        for (int i = 0; i < counted.size(); i++) {
+            ranks.add(getRank(counted.get(i), checked.get(i)));
+        }
+        return ranks;
+    }
+
+    private Rank getRank(int count, boolean check) {
+        if (count == Rank.THREE_MATCH.getCounted()) {
+            return Rank.THREE_MATCH;
+        }
+        if (count == Rank.FOUR_MATCH.getCounted()) {
+            return Rank.FOUR_MATCH;
+        }
+        if (count == Rank.FIVE_MATCH_AND_BONUS.getCounted() && Rank.FIVE_MATCH_AND_BONUS.getChecked()) {
+            return Rank.FIVE_MATCH_AND_BONUS;
+        }
+        if (count == Rank.FIVE_MATCH.getCounted()) {
+            return Rank.FIVE_MATCH;
+        }
+        if (count == Rank.SIX_MATCH.getCounted()) {
+            return Rank.SIX_MATCH;
+        }
+        return null;
     }
 }

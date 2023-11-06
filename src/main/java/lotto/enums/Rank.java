4c49ccb45a08539aa75d842210da30fcd2c0de94
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-06 17:07:32 +0900

    refactor(Rank) : 천자리 구분을 위해 prize 값에 언더스코어 추가

--- src/main/java/lotto/enums/Rank.java
+++ src/main/java/lotto/enums/Rank.java
@@ -7,11 +7,11 @@ import lotto.domain.WinningNumber;
 
 public enum Rank {
     MISS(0, "", 0),
-    FIFTH_PLACE(3, "3개 일치 (5,000원) - %d개\n", 5000),
-    FOURTH_PLACE(4, "4개 일치 (50,000원) - %d개\n", 50000),
-    THIRD_PLACE(5, "5개 일치 (1,500,000원) - %d개\n", 1500000),
-    SECOND_PLACE(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", 30000000),
-    FIRST_PLACE(6, "6개 일치 (2,000,000,000원) - %d개\n", 2000000000);
+    FIFTH_PLACE(3, "3개 일치 (5,000원) - %d개\n", 5_000),
+    FOURTH_PLACE(4, "4개 일치 (50,000원) - %d개\n", 50_000),
+    THIRD_PLACE(5, "5개 일치 (1,500,000원) - %d개\n", 1_500_000),
+    SECOND_PLACE(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", 30_000_000),
+    FIRST_PLACE(6, "6개 일치 (2,000,000,000원) - %d개\n", 2_000_000_000);
 
     private final int requiredMatches;
     private final String matchInfo;

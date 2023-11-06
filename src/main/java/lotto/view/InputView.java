7eee6f365df157139fdf9bfe9fd6cf2b56538773
Hyun Woo <hwsa10041@gmail.com>
2023-11-06 15:05:52 +0900

    feat : 보너스 번호를 입력받는 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,4 +12,9 @@ public class InputView {
         System.out.println("당첨 번호를 입력해 주세요.");
         return readLine();
     }
+
+    public String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return readLine();
+    }
 }

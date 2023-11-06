73cd800a0a3aaf8029e9bbe12f84edc472a8fb2b
MinseokGo <rhalstjr1999@naver.com>
2023-11-07 01:19:30 +0900

    refactor($Application): Referee 의존 주입
    DIP, OCP 위반을 방지하기 위해 팩토리 메서드 패턴을 적용하여 Referee를 LottoController에 의존 주입합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,17 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.model.Referee;
 import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
+        InputView inputView = InputView.getInstance();
         inputView.printPurchaseMoneyMessage();
 
-        LottoController lottoController = new LottoController();
+        Referee referee = new Referee();
+        LottoController lottoController = new LottoController(referee);
 
         inputView.printWinningNumbersMessage();
         lottoController.createWinningLotto();

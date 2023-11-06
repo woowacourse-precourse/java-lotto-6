122460b4f3f2bdf0838bc417f783b5a97d744cc7
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-07 02:38:16 +0900

    [feat:InputView] 로또 구입 금액 입력

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    List<Lotto> lottos = new ArrayList<>();
+
+}
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    //로또 구입 금액 입력
+    public static String purchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    //당첨 번호 6자리 입력
+    //보너스 번호 1자리 입력
+}

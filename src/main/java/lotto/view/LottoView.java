6f30f6b2afef07e7caf129c4e3c6aee5d23b7231
Seyeon <wjdtpdus828@naver.com>
2023-11-06 21:58:52 +0900

    feat(LottoView): 구매 개수를 출력하는 메소드 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -4,7 +4,12 @@ import lotto.constant.Views;
 
 public class LottoView {
 
-    public void purchaseAmountView(){
-        System.out.println(Views.PURCHASE_AMOUT_MESSAGE.getMessage());
+    public void inputPurchaseAmountView(){
+        System.out.println(Views.INPUT_PURCHASE_AMOUT_MESSAGE.getMessage());
+    }
+
+    public void outputPurchaseAmountView(int amount){
+        System.out.println();
+        System.out.println(amount + Views.OUTPUT_PURCHASE_AMOUT_MESSAGE.getMessage());
     }
 }

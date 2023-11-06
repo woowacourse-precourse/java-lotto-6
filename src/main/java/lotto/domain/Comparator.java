cedaf66e9a92a407937097fbdc900e8a7939ecd2
Dowon <3120kang@gmail.com>
2023-11-06 23:37:33 +0900

    Feat(Comparator): 구매한 로또번호와 당첨번호를 비교하는 기능 구현

--- src/main/java/lotto/domain/Comparator.java
+++ src/main/java/lotto/domain/Comparator.java
@@ -1,16 +1,18 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Comparator {
 
-    public int compare(List<Integer> lotto,List<Integer> lucky){
+    public int compare(List<Integer> lotto, List<Integer> lucky) {
+        List<Integer> result = lotto.stream()
+                .filter(n -> lucky.stream()
+                        .anyMatch(Predicate.isEqual(n)))
+                .collect(Collectors.toList());
 
-        return 0;
+        return result.size();
     }
 
-    public boolean compareBonus(List<Integer> lotto,int bonus){
-
-        return true;
-    }
 }
---
+++ src/test/java/lotto/domain/ComputerTest.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerTest {
+
+    Computer computer=new Computer(Arrays.asList(1,2,3,4,5,6),7);
+
+}

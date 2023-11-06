90f561485029b41bdc3b27c7dd591324dc1fa1b2
JW0321 <digneotn123@gmail.com>
2023-11-06 21:31:56 +0900

    test: 로또 번호 오름차순 정렬 테스트 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,8 +39,7 @@ class LottoTest {
     @DisplayName("로또 번호를 오름차순으로 정리한다.")
     @Test
     void testSortLottoAscending() {
-
-        Lotto inputLotto = new Lotto(List.of(42, 21, 39, 5, 16, 1));
+        Lotto inputLotto = new Lotto(new ArrayList<>(Arrays.asList(42, 21, 39, 5, 16, 1)));
         List<Integer> sortedNumbers = List.of(1, 5, 16, 21, 39, 42);
 
         assertThat(inputLotto.getNumbers()).isEqualTo(sortedNumbers);

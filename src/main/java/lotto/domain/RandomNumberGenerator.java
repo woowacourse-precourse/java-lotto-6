5dc4c81b944ed5d1eca1b03f97f430ea828e0427
waveofmymind <sjun0913@gmail.com>
2023-11-07 00:34:27 +0900

    fix: ApplicationTest의 기능 테스트가 모킹이 잘못되어 시간 초과가 되는 문제를 해결한다.

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -2,9 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static lotto.domain.NumberConstraints.*;
 
@@ -12,11 +10,7 @@ public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
     public List<Integer> generateNumber() {
-        Set<Integer> numbersSet = new HashSet<>();
-        while (numbersSet.size() < LOTTO_PER_NUMBER.value()) {
-            int number = Randoms.pickNumberInRange(LOTTO_MIN_NUMBER.value(), LOTTO_MAX_NUMBER.value());
-            numbersSet.add(number);
-        }
-        return numbersSet.stream().sorted().toList();
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER.value(), LOTTO_MAX_NUMBER.value(), LOTTO_PER_NUMBER.value());
+        return numbers.stream().sorted().toList();
     }
 }

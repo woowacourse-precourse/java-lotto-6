1f76007ee914abab7f5f56b0290604bba53d3458
minkyole <7719s@nave.com>
2023-11-06 18:10:39 +0900

    feat(Lotto.java) : 로또 번호의 개수가 6보다 작을 경우 예외메시지 출력 구현
    - 로또 번호의 개수가 6보다 작을 경우 예외메시지 출력 구현완료

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -21,7 +21,7 @@ public class Lotto {
 
     private void vaildateLengthOfNumber(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 로또번호를 입력해주세요");
         }
     }
 

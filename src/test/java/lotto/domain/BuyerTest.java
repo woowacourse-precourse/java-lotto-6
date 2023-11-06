7e857c56bc023864b16455cf71697e56f748cc31
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 18:35:41 +0900

    test: 구매 금액에 따른 로또 갯수 테스트 작성

---
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuyerTest {
+
+    private LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+
+    @DisplayName("구매금액에 따른 로또 갯수 확인")
+    @Test
+    void buyLottoSize() {
+        int amount = 2000;
+        int lottoCount = new Amount(amount).buyCount();
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(lottoNumberGenerator.pickUniqueNumbers()));
+        }
+
+        Buyer buyer = new Buyer(lottos);
+
+        Assertions.assertThat(buyer.lottoCount()).isEqual(2);
+    }
+}

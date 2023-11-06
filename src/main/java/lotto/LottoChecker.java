a451976cbe695906aa80fee830cde11265f62de0
watermelon3737 <pshrina1@naver.com>
2023-11-06 14:21:58 +0900

    feat(LottoChecker) : 로또 결과 당첨 금액 판별, 수익률 계산 메서드 구현

--- src/main/java/lotto/LottoChecker.java
+++ src/main/java/lotto/LottoChecker.java
@@ -1,10 +1,11 @@
 package lotto;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class LottoChecker {
 
@@ -12,6 +13,8 @@ public class LottoChecker {
     private final int BONUS_NUMBER;
     private List<Lotto> lottos = new ArrayList<>();
     private Map<Prize, Integer> result = new HashMap<>();
+    private long totalPrize;
+    private String profitRate;
 
     public LottoChecker(final List<Integer> winningNumbers, final int bonusNumber) {
         this.WINNING_NUMBERS = winningNumbers;
@@ -33,7 +36,7 @@ public class LottoChecker {
     }
 
     public void saveLottosResult() {
-        for (Lotto lotto : this.lottos) {
+        this.lottos.forEach(lotto -> {
             long matches = this.checkWinningNumber(lotto);
             boolean isContainBonus = false;
             if (matches == 5L) {
@@ -41,19 +44,23 @@ public class LottoChecker {
             }
             Prize prize = this.getPrize(matches, isContainBonus);
             this.result.put(prize, this.result.get(prize) + 1);
-        }
+        });
     }
 
     public long checkWinningNumber(final Lotto lotto) {
         List<Integer> lottoNumbers = lotto.getNumbers();
+
         long matches = lottoNumbers.stream()
                 .filter(this.WINNING_NUMBERS::contains).count();
+
         return matches;
     }
 
     public boolean checkContainBonusNumber(final Lotto lotto) {
         List<Integer> lottoNumbers = lotto.getNumbers();
+
         boolean isContainBonus = lottoNumbers.contains(this.BONUS_NUMBER);
+
         return isContainBonus;
     }
 
@@ -76,4 +83,32 @@ public class LottoChecker {
         return Prize.NONE;
     }
 
+    public Map<Prize, Integer> getResult() {
+        return this.result;
+    }
+
+    public void calculateTotalPrize() {
+        long totalPrize = this.result.keySet().stream().mapToLong(prize -> prize.money * this.result.get(prize)).sum();
+        this.totalPrize = totalPrize;
+    }
+
+    public void calculateProfitRate() {
+        if (this.totalPrize == 0) {
+            this.profitRate = "0.0";
+            return ;
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
+        double usedMoney = (long) lottos.size() * 1000;
+        double profitRate = (this.totalPrize / usedMoney) * 100;
+        this.profitRate = decimalFormat.format(profitRate);
+    }
+
+    public long getTotalPrize() {
+        return this.totalPrize;
+    }
+
+    public String getProfitRate() {
+        return this.profitRate;
+    }
 }

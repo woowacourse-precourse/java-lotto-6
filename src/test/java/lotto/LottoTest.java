bf250c48659ebf08454b8a749fc49bd44c53ead1
ikjo39 <chi4321@naver.com>
2023-11-07 01:22:11 +0900

    test: 로또 테스트 추가
    - 로또 번호 목록은 정렬되어 생성
    - 로또 번호 목록 내 해당 번호가 포함되는지 체크
    - 로또 간 같은 숫자의 개수를 계산

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static lotto.constant.ErrorMessage.DUPLICATED_LOTTO_NUMBERS;
 import static lotto.constant.ErrorMessage.INVALID_LOTTO_NUMBERS_AMOUNT;
 import static lotto.constant.ErrorMessage.NOT_IN_RANGE_LOTTO_NUMBER;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -10,12 +11,27 @@ import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
 
     @Nested
     @DisplayName("로또가 로또번호 목록을 받아 생성된다.")
     class createLotto {
+        @DisplayName("[성공] 로또 번호 목록은 정렬되어 생성된다.")
+        @Test
+        void createLottoUnsorted() {
+            // given
+            List<Integer> lottoNumbers = List.of(7, 6, 4, 1, 2, 3);
+            List<Integer> expected = lottoNumbers.stream().sorted().toList();
+
+            // when
+            Lotto result = Lotto.from(lottoNumbers);
+
+            // then
+            assertThat(result).hasFieldOrPropertyWithValue("numbers", expected);
+        }
 
         @DisplayName("[실패] 로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
         @Test
@@ -52,5 +68,34 @@ class LottoTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(NOT_IN_RANGE_LOTTO_NUMBER.toString());
         }
+    }
+
+    @DisplayName("로또 번호 목록 내 해당 번호가 포함되는지 체크한다.")
+    @CsvSource({"1,true", "45,false", "6,true"})
+    @ParameterizedTest
+    void isLottoContainsNumberTrue(int number, boolean expected) {
+        // given
+        Lotto lotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        boolean result = lotto.isLottoContainsNumber(number);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("로또 간 같은 숫자의 개수를 계산한다.")
+    @Test
+    void calculateSameNumbers() {
+        // given
+        Lotto givenLotto = Lotto.from(List.of(1, 4, 6, 8, 9, 10));
+        Lotto otherLotto = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+        int expected = 3;
+
+        // when
+        int result = givenLotto.calculateSameNumbers(otherLotto);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }

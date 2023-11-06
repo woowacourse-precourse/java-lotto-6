71a68e38d97a7cc8560e23ec14f931089a725b38
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 15:39:22 +0900

    feat(Profit): 수익 클래스 및 수익을 저장하고 계산하는 기능 구현

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -1,2 +1,24 @@
-package lotto.domain;public class Profit {
+package lotto.domain;
+
+public class Profit {
+    private Integer amount;
+
+    Profit() {
+        this.amount = 0;
+    }
+
+    void add(Integer profit) {
+        validate(profit);
+        this.amount += profit;
+    }
+
+    private void validate(Integer profit) {
+        if (profit < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
 }

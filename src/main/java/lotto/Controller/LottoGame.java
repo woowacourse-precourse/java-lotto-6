bb620940c8909af80f0af8d5715ce24e63c89cfa
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-06 19:05:59 +0900

    feat(LottoGame): Controller 추가

--- src/main/java/lotto/Controller/LottoGame.java
+++ src/main/java/lotto/Controller/LottoGame.java
@@ -1,9 +1,10 @@
 package lotto.Controller;
 
 import lotto.Model.Bonus;
-import lotto.Model.Lotto;
+import lotto.Model.Lottos;
 import lotto.Model.Price;
 import lotto.Model.RandomLotto;
+import lotto.Model.Rate;
 import lotto.Model.Result;
 import lotto.Model.WinningLotto;
 import lotto.View.InputView;
@@ -12,21 +13,34 @@ import lotto.View.OutputView;
 public class LottoGame {
 
     public void start() {
+        // price는 로또 구매 가격
         Price price = createPrice();
 
+        // randomLotto는 발행된 랜덤 로또 개수
         RandomLotto randomLotto = new RandomLotto(price);
+        Lottos lottos = new Lottos(randomLotto.getCount());
 
         OutputView.printLottoPurChaseMessage(randomLotto);
-        while (randomLotto.minusCount() > 0) {
-            Lotto lotto = randomLotto.createRandomLottos();
-            OutputView.printRandomLottoNumbers(lotto);
-        }
+        OutputView.printRandomLottoNumbers(lottos);
+
         // ------로또 구입 금액 받아서 랜덤로또번호 출력까지
 
         WinningLotto winningLotto = createWinningLotto();
-
         Bonus bonus = createBonusNumber(winningLotto);
 
+        Result result = new Result();
+        result.compare(lottos, winningLotto, bonus);
+
+        // Rate는 총 Rank의 합
+        Rate rate = new Rate(result);
+
+        //System.out.println(result.getRankAndCounting());
+
+        // 당첨 결과 출력
+        OutputView.printWinningResult(result);
+
+        // 수익률 결과 출력
+        OutputView.printWinningRate(rate.calculateWinningRate(price));
 
     }
 

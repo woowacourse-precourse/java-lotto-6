c838e28abb6badda68f5cfdcb897ffd2cafe45d1
Genius-PAK <pyj789123@gmail.com>
2023-11-06 18:37:44 +0900

    feat(RankEvaluatorTest): RankEvaluator 테스트 코드 추가

--- src/test/java/lotto/model/RankEvaluatorTest.java
+++ src/test/java/lotto/model/RankEvaluatorTest.java
@@ -32,12 +32,12 @@ public class RankEvaluatorTest {
 
     @Test
     void testGetRankOutOfRank() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 9, 41, 5, 17);
+        List<Integer> lottoNumbers = Arrays.asList(1, 3, 29, 41, 5, 17);
         Iterator<Integer> lottoIterator = lottoNumbers.iterator();
         WinningRank rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isEqualTo(WinningRank.OUT_OF_RANK);
 
-        lottoNumbers = Arrays.asList(1, 2, 10, 3, 6, 18);
+        lottoNumbers = Arrays.asList(1, 3, 10, 5, 6, 18);
         lottoIterator = lottoNumbers.iterator();
         rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isNotEqualTo(WinningRank.OUT_OF_RANK);
@@ -45,7 +45,7 @@ public class RankEvaluatorTest {
 
     @Test
     void testGetRankFifth() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 40, 50, 60);
+        List<Integer> lottoNumbers = Arrays.asList(1, 3, 10, 31, 29, 44);
         Iterator<Integer> lottoIterator = lottoNumbers.iterator();
         WinningRank rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isEqualTo(WinningRank.FIFTH);
@@ -53,7 +53,7 @@ public class RankEvaluatorTest {
 
     @Test
     void testGetRankFourth() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 50, 60);
+        List<Integer> lottoNumbers = Arrays.asList(1, 3, 10, 29, 31, 44);
         Iterator<Integer> lottoIterator = lottoNumbers.iterator();
         WinningRank rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isEqualTo(WinningRank.FOURTH);
@@ -61,7 +61,7 @@ public class RankEvaluatorTest {
 
     @Test
     void testGetRankThird() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 50, 4);
+        List<Integer> lottoNumbers = Arrays.asList(1, 3, 10, 29, 31, 6);
         Iterator<Integer> lottoIterator = lottoNumbers.iterator();
         WinningRank rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isEqualTo(WinningRank.THIRD);
@@ -69,15 +69,21 @@ public class RankEvaluatorTest {
 
     @Test
     void testGetRankSecond() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 4);
+        // 4 is the bonus number
+        List<Integer> lottoNumbers = Arrays.asList(1, 3, 10, 29, 4, 6);
         Iterator<Integer> lottoIterator = lottoNumbers.iterator();
         WinningRank rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isEqualTo(WinningRank.SECOND);
+
+        lottoNumbers = Arrays.asList(1, 3, 10, 29, 7, 6);
+        lottoIterator = lottoNumbers.iterator();
+        rank = rankEvaluator.getRank(lottoIterator);
+        Assertions.assertThat(rank).isNotEqualTo(WinningRank.SECOND);
     }
 
     @Test
     void testGetRankFirst() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> lottoNumbers = Arrays.asList(1, 3, 10, 29, 18, 6);
         Iterator<Integer> lottoIterator = lottoNumbers.iterator();
         WinningRank rank = rankEvaluator.getRank(lottoIterator);
         Assertions.assertThat(rank).isEqualTo(WinningRank.FIRST);

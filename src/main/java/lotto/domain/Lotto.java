2d8489973db06e16889434efcb9d096e397e5fc5
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-06 18:11:34 +0900

    [REFACTOR] Lotto.java 상수 선언
    6을 NUMBERS_SIZE 라는 상수로 선언해서 사용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,23 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
 public class Lotto {
+    private static final int NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != NUMBERS_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
+
 }

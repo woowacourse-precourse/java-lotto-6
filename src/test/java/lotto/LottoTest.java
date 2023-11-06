5e5a4cf68cf80ddae1004a018cdfd9813f282b27
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-06 17:41:19 +0900

    test: complete number matching function test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -18,10 +19,19 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력한 로또 번호와 발행한 로또 번호가 틀린지 체크하는 테스트.")
+    @Test
+    void isMatch_test() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+
+        // then
+        assertThat(lotto.isMatch(List.of(1, 2, 3, 4, 5, 6))).isTrue();
+    }
 }

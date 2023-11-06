63255bf8f22b1d7e21ff582e6d374e3cca77a198
kwon <kckc0608@naver.com>
2023-11-06 21:37:08 +0900

    feat: 구입 금액 입력 안내 문구 출력하기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
 public class Application {
+    private static final String MESSAGE_FOR_PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = getPurchaseAmountWithInput();
+    }
+
+    private static int getPurchaseAmountWithInput() {
+        System.out.println(MESSAGE_FOR_PURCHASE_AMOUNT_INPUT);
+        return 0;
     }
 }

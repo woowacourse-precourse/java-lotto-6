f3e3f10b4373e9ea74f386e12bb4b510dd06908b
donghoonyeom <dua5410773@naver.com>
2023-11-07 01:26:57 +0900

    feat(lotto): Lotto 클래스 업데이트
    Lotto 클래스의 로또 번호 유효성을 검사하는 방법을 개선했습니다. 변경된 `validate(List<Integer> numbers)` 메서드는 이제 로또 번호가 6개인지, 중복이 없는지, 그리고 1부터 45까지의 숫자인지를 확인합니다. 이로써 로또 번호의 유효성이 더 엄격하게 검사됩니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +14,23 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45까지의 숫자여야 합니다.");
+            }
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

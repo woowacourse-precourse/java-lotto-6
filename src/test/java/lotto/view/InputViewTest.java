5945214a2ed41c44e42c09c904a3d66bf77acc49
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 14:37:03 +0900

    test(inputView): 당첨 번호 입력시 숫자, 구분자, 공백으로만 이루어지지 않은 경우 예외 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -34,6 +34,15 @@ class InputViewTest {
     @Nested
     class 당첨번호_입력_테스트 extends NsTest {
         @Test
+        void 당첨번호_입력시_숫자와_구분자_공백이_아닌_다른_문자가_포함된_경우_예외() {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("1,2,3.4. 8, 6"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(INVALID_WINNING_NUMBERS_INPUT_TYPE.getMessage());
+            });
+        }
+
+        @Test
         void 당첨번호에_공백이_있는_경우_제거() {
             assertSimpleTest(() -> {
                 run("1,2,3,4,  5, 7");

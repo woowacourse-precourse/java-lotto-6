4de27309822978b1b439989fba051217a78427a8
HJLee <goodforx0147@naver.com>
2023-11-06 13:29:15 +0900

    feat: add money input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,5 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        int money = inputMoney();
+        System.out.println(money);
+    }
+
+    public static Integer inputMoney() {
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요");
+            String input = Console.readLine();
+            try {
+                int money = Integer.parseInt(input);
+                return money;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 구입 금액은 정수로만 이루어져야 합니다.");
+            }
+        }
     }
 }

d8e61c7283d17b7ddc2198f6dd7ba41d2902ffee
jenakxm <jenakxm@gmail.com>
2023-11-06 11:41:14 +0900

    feat: 로또 발행 및 당첨 번호 구현

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGame {
+    // 로또 발행
+    public int buyLotto(int amount) {
+        int draws = amount/1000;
+        System.out.println(draws+"개를 구매했습니다.");
+        return draws;
+    }
+
+    // 당첨 번호 추첨
+    public void drawLotto(){
+        // [8, 21, 23, 41, 42, 43]
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        Lotto lotto = new Lotto(numbers);
+        System.out.println(numbers);
+    }
+
+    // 당첨 번호 + 보너스 번호 입력
+    // 당첨 내역 출력
+    // 수익률 출력
+    // 예외 상황 출력
+}

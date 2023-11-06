cb9b55f6a9fa34c32d6394bce9e335e19765e4d5
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 12:49:53 +0900

    Feat: 보너스 번호를 숫자로 변환하는 기능 구현

--- src/main/java/lotto/domain/WinningNumberMaker.java
+++ src/main/java/lotto/domain/WinningNumberMaker.java
@@ -2,67 +2,33 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+
+import lotto.validation.NumbersValidation;
 
 public class WinningNumberMaker {
 
 	private static final String SEPARATOR = ",";
-	private static final int SIZE = 6;
-	// 숫자만 허용
-	private static final Pattern ONLY_NUMBER = Pattern.compile("^[\\d]*$");
-	// 1~45 사이의 숫자만 허용
-	private static final Pattern NUMBER_RANGE = Pattern.compile("^[1-9]$|^[0-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$");
-
 
 	public static List<Integer> make(String inputNumber) {
-
-		validateSeparator(inputNumber);
 
 		List<Integer> winningNumber = Arrays.stream(inputNumber.split(SEPARATOR))
 			.map(String::trim)
 			.distinct()
-			.peek(WinningNumberMaker::validateOnlyNumber)
-			.peek(WinningNumberMaker::validateRange)
+			.peek(NumbersValidation::validateOnlyNumber)
+			.peek(NumbersValidation::validateRange)
 			.map(Integer::parseInt)
 			.toList();
 
-		validateSize(winningNumber);
+		NumbersValidation.validateSize(winningNumber);
 
 		return winningNumber;
 
 	}
 
-	private static void validateSeparator(String inputNumber) {
-		if (!inputNumber.contains(SEPARATOR)) {
-			throw new IllegalArgumentException("구분자(,)만을 이용해 나눠주세요");
-		}
+	public static int makeBonusNumber(String inputBonusNumber) {
+		return Integer.parseInt(inputBonusNumber);
 	}
 
-	private static void validateOnlyNumber(String inputNumber) {
-		if (!isNumber(inputNumber)) {
-			throw new IllegalArgumentException("양수의 숫자만 입력해주세요");
-		}
-	}
 
-	private static boolean isNumber(String inputNumber) {
-		return ONLY_NUMBER.matcher(inputNumber).matches();
-	}
-
-	private static void validateRange(String inputNumber) {
-		if (!inRange(inputNumber)) {
-			throw new IllegalArgumentException("1~45 사이의 숫자만 입력해주세요");
-		}
-	}
-
-	private static boolean inRange(String inputNumber) {
-		return NUMBER_RANGE.matcher(inputNumber).matches();
-	}
-
-	private static void validateSize(List<Integer> winningNumber) {
-
-		if (winningNumber.size() != SIZE) {
-			throw new IllegalArgumentException("중복되지 않는 6개의 숫자만 입력해주세요");
-		}
-	}
 
 }

a36bb8f5e25242042181dcf7cdcefd549ec292d1
JongMinCh0i <wonderland314592@gmail.com>
2023-11-06 20:51:24 +0900

    refactor(InputView): 매직 리터럴 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,12 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private final static String ASK_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
-    private final static String ASK_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private final static String ASK_BONUS_WINNING_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String START_OF_SENTENCE_CHARACTER = "문장은 첫번째 문자를 숫자 외 허용하지 않습니다.";
-    private static final String END_OF_SENTENCE_CHARACTER = "문장은 마지막 문자를 숫자 외 허용하지 않습니다.";
-    private static final String USER_DEFAULT_DELIMITER = ",";
+    private static final String ASK_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_WINNING_NUMBER_MESSAGE = "[ERROR] 보너스 번호를 입력해 주세요.";
+    private static final String START_OF_SENTENCE_CHARACTER = "[ERROR] 문장은 첫번째 문자를 숫자 외 허용하지 않습니다.";
+    private static final String END_OF_SENTENCE_CHARACTER = "[ERROR] 문장은 마지막 문자를 숫자 외 허용하지 않습니다.";
+    public static final String PERMIT_ONLY_NUMBER = "[ERROR] 정수 외에는 입력받을 수 없습니다.";
+    public static final String NO_PERMIT_BLANK = "[ERROR] 공백은 입력될 수 없습니다.";
+    public static final String NO_PERMIT_INPUT_LESS_ZERO = "[ERROR] 0 이하의 값은 입력받을 수 없습니다.";
 
     public int askPrice() {
         printHowManyPurchase();
@@ -60,20 +62,20 @@ public class InputView {
         try {
             Integer.parseInt(input);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("[ERROR] 정수 외에는 입력받을 수 없습니다.");
+            throw new IllegalArgumentException(PERMIT_ONLY_NUMBER);
         }
         return Integer.parseInt(input);
     }
 
     private void validateBlankAndEmptyInteger(String input) {
         if (input.isBlank() || input.isEmpty() || input.contains(" ")) {
-            throw new IllegalArgumentException("[ERROR] 공백은 입력될 수 없습니다.");
+            throw new IllegalArgumentException(NO_PERMIT_BLANK);
         }
     }
 
     private int validateNegativeIntegerAndZero(int input) {
         if (input <= 0) {
-            throw new IllegalArgumentException("[ERROR] 0 이하의 값은 입력받을 수 없습니다.");
+            throw new IllegalArgumentException(NO_PERMIT_INPUT_LESS_ZERO);
         }
         return input;
     }

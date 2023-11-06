a49237fbfd58920eb0959e989e348ba39c35c306
jang010505 <jang010505@inu.ac.kr>
2023-11-07 06:07:15 +0900

    feat: InputValidator 객체 생성 및 함수 개형 작성

---
+++ src/main/java/lotto/validator/InputValidator.java
@@ -0,0 +1,20 @@
+package lotto.validator;
+
+import java.util.List;
+
+public class InputValidator {
+    public static int validateLottoPriceInput(String input) {
+
+        return 0;
+    }
+
+    public static List<Integer> validateLottoNumberInput(String input) {
+
+        return null;
+    }
+
+    public static int validateBonusNumberInput(String input) {
+
+        return 0;
+    }
+}

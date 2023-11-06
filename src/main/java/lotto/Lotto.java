3e504169e7b25f2a7ee5c24d7b2d6f0fc194bd44
이정우 <acg6138@naver.com>
2023-11-06 12:39:46 +0900

    feat(Lotto): 로또 숫자 개수와 중복되는 숫자에 대한 검증 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,26 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static final int MAX_NUMBER_OF_NUMBERS = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSize(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != MAX_NUMBER_OF_NUMBERS) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        long count = numbers.stream()
+                .distinct()
+                .count();
+        if (count != MAX_NUMBER_OF_NUMBERS) throw new IllegalArgumentException();
+    }
+
 }

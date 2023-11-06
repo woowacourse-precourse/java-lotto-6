31d414e8103151f43c2bbaf0f92d824073a52823
little6523 <little6523@naver.com>
2023-11-07 01:08:02 +0900

    fix : 구매한 로또 개수 출력 수정  - docs/README.md의 4번 기능 문구 수정  - purchaseAmount / 1000 -> purchaseAmount

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static int inputPurchaseAmount() {
@@ -24,13 +27,21 @@ public class Application {
     }
 
     private static void printPurchaseNumber(int purchaseAmount) {
-        System.out.println(purchaseAmount / 1000 + "개를 구매했습니다.");
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+    }
+
+    private static List<Integer> inputLottoNumbers(int purchaseNumber) {
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        System.out.println(lotto);
+        return lotto;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = inputPurchaseAmount();
-        printPurchaseNumber(purchaseAmount);
+        int purchaseNumber = purchaseAmount / 1000;
+        printPurchaseNumber(purchaseNumber);
+        List<Lotto> lottos = new ArrayList<>();
     }
 }

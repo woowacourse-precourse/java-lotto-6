1632a68902e7f1237a56c788d91b36a0e3c46845
yoonjuho <dbswn990@naver.com>
2023-11-06 18:43:40 +0900

    feat(StatisticsOffice): 당첨 로또 번호 6개와 보너스 번호가 중복될 시 예외가 발생한다.

--- src/main/java/lotto/model/StatisticsOffice.java
+++ src/main/java/lotto/model/StatisticsOffice.java
@@ -10,15 +10,20 @@ public class StatisticsOffice {
     private final LottoNumber bonusNumber;
 
     private StatisticsOffice(Lotto winningLotto, LottoNumber bonusNumber) {
+        validateDuplicate(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicate(Lotto winningLotto, LottoNumber bonusNumber){
+        if(winningLotto.hasNumber(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호 6개와 보너스 번호는 중복될 수 없습니다.");
+        }
     }
 
     public static StatisticsOffice registerWinningLotto(List<Integer> winningLotto, Integer bonusNumber) {
         return new StatisticsOffice(new Lotto(winningLotto), new LottoNumber(bonusNumber));
     }
-
-
 
 
 }
--- src/test/java/lotto/model/StatisticsOfficeTest.java
+++ src/test/java/lotto/model/StatisticsOfficeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StatisticsOfficeTest {
 
@@ -16,6 +17,15 @@ class StatisticsOfficeTest {
         Integer bonusNumber = Integer.valueOf(7);
         assertThat(StatisticsOffice.registerWinningLotto(winningLotto, bonusNumber))
                 .isInstanceOf(StatisticsOffice.class);
+    }
+
+    @DisplayName("당첨 로또 번호 6개와 보너스 번호가 중복될 시 예외가 발생한다.")
+    @Test
+    void validateDuplicate(){
+        List<Integer> winningLotto = List.of(1,2,3,4,5,6);
+        Integer bonusNumber = Integer.valueOf(3);
+        assertThatThrownBy(() -> StatisticsOffice.registerWinningLotto(winningLotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR]");
     }
 
 }

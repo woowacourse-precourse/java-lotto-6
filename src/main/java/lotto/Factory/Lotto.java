7e5e40f098b34302112fb4773dcaeb223295fa09
minju <minju9711@naver.com>
2023-11-07 03:26:01 +0900

    [feat] 하나의 lotto 번호 생성 시 사용할 클래스. 정적 팩토리 메서드를 사용했고 인자의 수에 따라 생성자를 구분함

--- src/main/java/lotto/Factory/Lotto.java
+++ src/main/java/lotto/Factory/Lotto.java
@@ -1,19 +1,45 @@
 package lotto.Factory;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    // 외부에서 Lotto 객체를 생성하는 것을 막기위해 private으로 변경
+    private Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
+    // 정적 팩토리 메서드, 사용자 입력 로또 생성시 사용
+    public static Lotto createLotto() {
+        List<Integer> numbers = createRandomNumbers();
+        return new Lotto(numbers);
+    }
+
+    // 테스트 및 정답로또 생성시 사용
+    public static Lotto createLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private static List<Integer> createRandomNumbers(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        while (numbers.size() < 6) {
+            putRandomNum(numbers);
         }
     }
-    // TODO: 추가 기능 구현
+
+    private void putRandomNum(List<Integer> numbers){
+        int randomNumber = Randoms.pickNumberInRange(1, 45);
+        if (!numbers.contains(randomNumber)) {
+            numbers.add(randomNumber);
+        }
+    }
+
 }
+

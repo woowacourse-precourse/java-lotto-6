e19f94b0cb02528d7648658b78ddb07b898fb221
Junobee25 <wnsdhqo@naver.com>
2023-11-07 00:33:12 +0900

    feat(LottoController): buyLotto 메서드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,12 +2,15 @@ package lotto.controller;
 
 import lotto.Money;
 import lotto.configuration.InputMessage;
+import lotto.configuration.PrintMessage;
 import lotto.service.LottoService;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     private final LottoService lottoService;
+    private static final Integer ONE_THOUSAND = 1000;
 
     public LottoController() {
         lottoService = new LottoService();
@@ -15,15 +18,20 @@ public class LottoController {
 
     public void startLotto() {
         Long money = inputPurchaseAmount();
+        buyLotto(money);
     }
 
     private Long inputPurchaseAmount() {
-        InputMessage.INPUT_PURCHASE_AMOUNT.inputMoneyMessage();
+        InputMessage.INPUT_PURCHASE_AMOUNT.inputMessage();
         Long myMoney = Long.parseLong(InputView.input());
         Money money = new Money(myMoney);
         return money.getMoney();
     }
 
-    private void buyLotto() {
+    private void buyLotto(Long money) {
+        PrintMessage.BUY_LOTTO_COUNT.printMessage(money/ONE_THOUSAND);
+        for (int i = 0; i < money/ONE_THOUSAND; i++) {
+            OutputView.lottoNumbersResult(lottoService.generateRandomLottoNumbers(money/ONE_THOUSAND).get(i));
+        }
     }
 }

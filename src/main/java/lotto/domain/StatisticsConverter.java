8711638acefb63a3cef2ee499094290df34dbf0d
yubin <dbqls2821@gmail.com>
2023-11-06 18:05:27 +0900

    feat: 통계를 output형식에 맞게 반환해주는 OutputConverter 구현체 추가

---
+++ src/main/java/lotto/domain/StatisticsConverter.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import lotto.domain.dto.Statistics;
+import lotto.io.OutputConverter;
+
+import java.util.Arrays;
+
+public final class StatisticsConverter implements OutputConverter<Statistics> {
+
+    @Override
+    public String convert(Statistics statistics) {
+    }
+
+}

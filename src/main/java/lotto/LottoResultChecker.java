6211451ff53f2fcece04c00bde2248302b3da716
alenjb <alenjb@naver.com>
2023-11-07 07:06:52 +0900

    ✨feat(LottoResultChecker): 수익률을 계산하는 기능 구현
    수익률을 구하고 소수점 둘째 자리에서 반올림하도록 구현함.

--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -5,12 +5,14 @@ import java.util.function.Predicate;
 
 public class LottoResultChecker {
 
+    public static final double ONE_LOTTO_PRICE = 1000;
     final NumberGenerator numberGenerator = new NumberGenerator();
     List<Integer> winningNumbers; // 당첨번호
     int bonusNumber; // 당첨 보너스 번호
     List<Integer> userLottoNumbers; // 사용자가 구매한 번호
     int rank; // 등수
-    String reward; // 상금
+    int reward; // 상금
+    String returnRatio; // 수익률
 
     public LottoResultChecker(List<Integer> winningNumbers, List<Integer> userLottoNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
@@ -33,13 +35,18 @@ public class LottoResultChecker {
         return numbers.contains(bonusNumber);
     }
 
-    public String calculateReward(double matchNumberCount) {
+    public int calculateReward(double matchNumberCount) {
         if (matchNumberCount == Rank.FIRST_PLACE.getMatchNumberCount()) return Rank.FIRST_PLACE.getReward();
         if (matchNumberCount == Rank.SECOND_PLACE.getMatchNumberCount()) return Rank.SECOND_PLACE.getReward();
         if (matchNumberCount == Rank.THIRD_PLACE.getMatchNumberCount()) return Rank.THIRD_PLACE.getReward();
         if (matchNumberCount == Rank.FOURTH_PLACE.getMatchNumberCount()) return Rank.FOURTH_PLACE.getReward();
         if (matchNumberCount == Rank.FIFTH_PLACE.getMatchNumberCount()) return Rank.FIFTH_PLACE.getReward();
         return Rank.NONE.getReward(); // 매치되는 숫자 개수가 0인 경우
+    }
+
+    public double calculateReturnRation(){
+        // 계산한 수익률을 소수점 둘째 자리에서 반올림하여 리턴
+        return Math.round((reward / ONE_LOTTO_PRICE) * 100.0) / 100.0;
     }
 
 }

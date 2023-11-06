6a09c76bb5652bfe36ff9acdfd0b81127a6349cf
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-06 16:35:15 +0900

    faet: Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+import lotto.util.MatchType;
+
 public class Application {
+
+    private static LottoController lottoController = new LottoController();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int purchaseAmount = lottoController.receivePurchaseAmount();
+        int lottoCount = lottoController.getPurchaseCount(purchaseAmount);
+        List<Lotto> lottos = lottoController.generateLottos(lottoCount);
+        List<Integer> winningNumbers = lottoController.receiveWinningNumbers();
+        int bonusNumber = lottoController.receiveBonusNumber(winningNumbers);
+        Map<MatchType, Integer> winningStatistics =
+                lottoController.getWinningStatistics(lottos, winningNumbers, bonusNumber);
+        int earnings = lottoController.getEarnings(winningStatistics);
+        double totalReturn = lottoController.getReturnRate(purchaseAmount, earnings);
     }
 }

d54ca0b734cf15b4ccfe750c175c18cd9858da9f
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 14:59:16 +0900

    feat: 로또 번호 출력
    로또 번호 출력 함수 추가

--- src/main/java/lotto/print/PrintOutput.java
+++ src/main/java/lotto/print/PrintOutput.java
@@ -1,10 +1,27 @@
 package lotto.print;
 
+import lotto.Lotto;
+
+import java.util.List;
+
 public class PrintOutput {
-    public void printMoneyQuestion(){
+    public void printMoneyQuestion() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-    public void printPurchasedLottoNumber(int num){
-        System.out.println(num+"개를 구매했습니다.");
+
+    public void printPurchasedLottoNumber(int num) {
+        System.out.println(num + "개를 구매했습니다.");
+    }
+
+    public void printRandomLottosNumbers(List<Lotto> lottos) {
+        for (int i = 0; i < lottos.size(); i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            for (int j = 0; j < 5; j++) {
+                sb.append(lottos.get(i).getNumbers().get(j)).append(", ");
+            }
+            sb.append(lottos.get(i).getNumbers().get(5)).append("]");
+            System.out.println(sb);
+        }
     }
 }

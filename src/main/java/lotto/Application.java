5dce70cc90026e79f9821613c7dc9c29d88a1af8
Seo Young, Ha <59078585+82Hasheng@users.noreply.github.com>
2023-11-07 05:08:33 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+        /* 추가할 것 : 입력한 구입금액이 1000원으로 나누어 떨어지지 않을 경우 예외처리한다. */
     }
 }

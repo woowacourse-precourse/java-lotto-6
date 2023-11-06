42e781d44af5468de267781ba168bb65533c6fb3
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-06 17:33:35 +0900

    test: Player의 수익률 계산 메서드 테스트 코드 작성

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,4 +22,14 @@ class PlayerTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("수익률을 계산할 수 있다.")
+    @Test
+    void calculateReteOfReturn() {
+        Player player = new Player(8000);
+        int totalPrizeMoney = 5000;
+
+        double rateOfReturn = player.calculateRateOfReturn(totalPrizeMoney);
+
+        Assertions.assertThat(rateOfReturn).isEqualTo(62.5);
+    }
 }

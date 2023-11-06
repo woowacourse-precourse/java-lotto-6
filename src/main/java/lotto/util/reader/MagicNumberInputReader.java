b02f011cd9edc0aa7cd473449ae7c4fe189e6f18
DevFrog92 <yongsu1481@gmail.com>
2023-11-06 14:26:22 +0900

    feat: implement MagicNumberInputReader class

---
+++ src/main/java/lotto/util/reader/MagicNumberInputReader.java
@@ -0,0 +1,31 @@
+package lotto.util.reader;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Logger;
+import lotto.util.RegxPattern;
+
+public class MagicNumberInputReader implements InputReader {
+        @Override
+        public String readLine() {
+                String magicNumber;
+                while (true) {
+                        try {
+                                Logger.info("\n보너스 번호를 입력해 주세요.");
+                                magicNumber = Console.readLine();
+                                validate(magicNumber);
+                                break;
+                        } catch (IllegalArgumentException e) {
+                                Logger.error(e.getMessage());
+                        }
+
+                }
+
+                return magicNumber;
+        }
+
+        private void validate(String numbers) {
+                if (!numbers.matches(RegxPattern.LOTTO_NUMBER_RANGE)) {
+                        throw new IllegalArgumentException("보너스 숫자는 1에서 45사이의 숫자만 입력가능합니다.");
+                }
+        }
+}

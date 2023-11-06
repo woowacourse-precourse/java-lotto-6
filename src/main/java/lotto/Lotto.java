8c0e8720e693af5e07b2ea8f3f9f2fdc4b107001
ratatuwee <pkqwaszx@gmail.com>
2023-11-06 19:47:58 +0900

    feat: Lotto 클래스
    lotto 객체 생성 시 입력값 예외 추가, 값의 범위가 올바른지

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,12 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        for(Integer number:numbers){
+            if(number<1 && number>45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 

f1eea36d71472f0ecac3264ae0b48b8ed9ecd199
chanhoim <cksgh201@naver.com>
2023-11-07 02:59:51 +0900

    test(winnning numbers): 당첨번호, 보너스 번호 비교 테스트 작성

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -2,8 +2,7 @@ package lotto;
 
 import static lotto.domain.winningnumbers.WinningNumbersErrorMessage.BONUS_NUMBER_DUPLICATION_ERROR;
 import static lotto.domain.winningnumbers.WinningNumbersErrorMessage.BONUS_NUMBER_INVALID_RANGE;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -47,6 +46,46 @@ public class WinningNumbersTest {
         //when, then
         assertThatCode(() -> new WinningNumbers(winningNumbers, bonusNumber))
                 .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6})
+    void 해당_번호가_당첨_번호에_속한다(int number) {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        //when, then
+        assertThat(winningNumbers.isInWinningNumbers(number)).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7,8,9,10,11,12})
+    void 해당_번호가_당첨_번호에_속하지_않는다(int number) {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        //when, then
+        assertThat(winningNumbers.isInWinningNumbers(number)).isEqualTo(false);
+    }
+
+    @Test
+    void 해당_번호가_보너스_번호와_일치한다() {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        int number = 7;
+
+        //when, then
+        assertThat(winningNumbers.isSameWithBonusNumber(number)).isEqualTo(true);
+    }
+
+    @Test
+    void 해당_번호가_보너스_번호와_일치하지_않는다() {
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        int number = 1;
+
+        //when, then
+        assertThat(winningNumbers.isSameWithBonusNumber(number)).isEqualTo(false);
     }
 
     private static Stream<Arguments> provideValidWinningNumbersAndBonusNumber() {

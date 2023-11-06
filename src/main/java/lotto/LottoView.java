4d1d20d34d1b1579c2bbf2aea0c2728869f01892
gusgh075 <gusgh075@naver.com>
2023-11-06 11:06:31 +0900

    FIXED(LottoView) : 함수목록 static으로 수정

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -13,12 +13,12 @@ public class LottoView {
             "5개 일치 (1,500,000원) - ",
             "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
             "6개 일치 (2,000,000,000원) - "};
-    public int inputMoney(){
+    public static int inputMoney(){
         System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
         return money;
     }
-    public Lotto inputWinningLotto(){
+    public static Lotto inputWinningLotto(){
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] sNumbers = Console.readLine().split(",");
         List<Integer> winningLottoNumber=new ArrayList<>();
@@ -27,15 +27,15 @@ public class LottoView {
         }
         return new Lotto(winningLottoNumber);
     }
-    public int inputBonusNumber(){
+    public static int inputBonusNumber(){
         System.out.println("보너스 번호를 입력해 주세요.\n");
         return Integer.parseInt(Console.readLine());
     }
-    public void showLottos(LottoRepository lottoRepository){
+    public static void showLottos(LottoRepository lottoRepository){
         List<Lotto> lottos = lottoRepository.getLottos();
         lottos.forEach(lotto->System.out.println(Arrays.toString(lotto.getNumbers().toArray())));
     }
-    public void showStatus(LottoRepository lottoRepository){
+    public static void showStatus(LottoRepository lottoRepository){
         int[] ranks = lottoRepository.getRanks();
         System.out.println("당첨 통계");
         System.out.println("---");

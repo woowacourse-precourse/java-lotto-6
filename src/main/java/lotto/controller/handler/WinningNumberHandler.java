59bec5ab1686a2a1ee1d7bdcdda0d2969ef2cf06
Genius-PAK <pyj789123@gmail.com>
2023-11-06 20:33:08 +0900

    chore(WinningNumberHandler): WinningNumber의 타입 변경에 따른 handler의 paramizterized 타입 변경

--- src/main/java/lotto/controller/handler/WinningNumberHandler.java
+++ src/main/java/lotto/controller/handler/WinningNumberHandler.java
@@ -1,14 +1,13 @@
 package lotto.controller.handler;
 
-import java.util.Map;
+import java.util.Set;
 import lotto.controller.converter.WinningNumberConverter;
 import lotto.controller.validator.WinningNumberValidator;
 
-public class WinningNumberHandler extends InputHandler<Map<Integer, Integer>> {
+public class WinningNumberHandler extends InputHandler<Set<Integer>> {
     public WinningNumberHandler(String winningNumbers) {
         this.input = winningNumbers;
         this.converter = new WinningNumberConverter();
         this.validator = new WinningNumberValidator();
     }
-
 }

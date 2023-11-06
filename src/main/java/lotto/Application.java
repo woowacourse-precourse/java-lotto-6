2b2dff701e20e90a7f96aab61d981759a725cf8c
ioyxxnstream <ioyxxnstream@naver.com>
2023-11-06 09:32:43 +0900

    입력기능 1 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        /*
+        // 로또 당첨 금액 ENUM
+        enum LottoAmount {
+            FIFTH(5000), FOURTH(50000), THIRD(1500000), SECOND(30000000), FIRST(2000000000);
+
+            private final int value;
+
+            LottoAmount(int value) { this.value = value; }
+
+            public int getValue() { return value; }
+        }
+         */
+
+        // 1. 로또 구입 금액 1000원 단위로 입력 받기
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+
+
+
+
+
+
+
+
+
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,13 +3,16 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    // 로또 숫자 리스트
     private final List<Integer> numbers;
 
+    // 로또 리스트 생성
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
+    // 숫자가 6개가 맞는지 유효성 체크, 없으면 예외발생
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();

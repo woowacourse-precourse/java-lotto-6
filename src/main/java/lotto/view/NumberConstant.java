02526a2cda04383ccb34796361fe4d1ccb0d67f8
yeseong31 <yeseong31@naver.com>
2023-11-06 13:50:26 +0900

    refactor(NumberConstant): 상수 추가

--- src/main/java/lotto/view/NumberConstant.java
+++ src/main/java/lotto/view/NumberConstant.java
@@ -4,6 +4,7 @@ public class NumberConstant {
     
     public static final int ZERO = 0;
     public static final int VALIDATE_LOTTO_SIZE = 6;
+    public static final int LAST_INDEX_FOR_INSERT_SEPARATOR = 5;
     public static final int MINIMUM_LOTTO_NUMBER = 1;
     public static final int MAXIMUM_LOTTO_NUMBER = 45;
     public static final int INCREASE_NUMBER = 1;

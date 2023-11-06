e778ddd33497173b4142cdb2215f762c16f7bd5d
sinminseok12 <sinminseok12@naver.com>
2023-11-07 00:50:12 +0900

    feat(LottoController): 당첨 통계를 구해 결과를 보여주는 흐름 설계

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,8 @@ import lotto.model.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
@@ -14,8 +16,9 @@ public class LottoController {
     }
 
     public void run() {
-        buyLottoTicket();
-        registerWinLotto();
+        LottoTicket lottoTicket = buyLottoTicket();
+        WinLotto winLotto = registerWinLotto();
+        calculateWinStatistics(lottoTicket, winLotto);
     }
 
     private LottoTicket buyLottoTicket() {
@@ -30,5 +33,11 @@ public class LottoController {
         Lotto lotto = new Lotto(inputView.inputWinNumbers());
         LottoNumber bonusNumber = new LottoNumber(inputView.inputBonusNumber());
         return new WinLotto(lotto, bonusNumber);
+    }
+
+    private void calculateWinStatistics(LottoTicket lottoTicket, WinLotto winLotto) {
+        List<Ranking> rankings = lottoTicket.checkRankings(winLotto);
+        WinStatistics statistics = WinStatistics.from(rankings);
+        outputView.printWinStatistics(statistics.calculateProfit(), statistics.getStatistics());
     }
 }

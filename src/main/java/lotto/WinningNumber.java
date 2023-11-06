6c0c0a1819af69f4518d66fece0dae6c37b1a6fb
parkheeddong <phj7214go@gmail.com>
2023-11-06 16:01:01 +0900

    feat: 당첨번호 및 보너스번호 입력에 대한 유효성 검증 및 예외처리 구현

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -19,12 +19,44 @@ public class WinningNumber {
         winningNumbers = Arrays.stream(input.split(","))
                 .map(s -> Integer.parseInt(s))
                 .collect(Collectors.toList());
+        validateWinningNumber();
     }
 
     static public void gettingBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         bonusNumber = Integer.parseInt(input);
+        validateBonusNumber();
     }
 
+    static public void validateWinningNumber() {
+        try {
+            for (int num: winningNumbers) {
+                if (num < 1 | num > 45) {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                }
+            }
+            Set<Integer> numSet = new HashSet<Integer>(winningNumbers);
+            if (numSet.size() != winningNumbers.size()) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되어서는 안 됩니다.");
+            }
+            if (winningNumbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개여야 합니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            gettingWinningNumber();
+        }
+    }
+
+    static public void validateBonusNumber() {
+        try {
+            if (bonusNumber < 1 | bonusNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            gettingBonusNumber();
+        }
+    }
 }

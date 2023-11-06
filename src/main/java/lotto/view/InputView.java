d4d7a6c1ac2cabcfc0201400faf773df89c7f797
82user <zxcvc9999@gmail.com>
2023-11-06 17:54:26 +0900

    feat: input bonus number

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,19 +2,34 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class InputView {
 
     private static final String INPUT_Purchase_Amount = "구입금액을 입력해 주세요.";
+
+    private StringTokenizer st;
 
     public int inputPurchaseAmount() {
         return Integer.parseInt(Console.readLine());
     }
 
-    public int inputWinningNumbers() {
+    public List<Integer> inputWinningNumbers() {
+        return stringToListInteger(Console.readLine());
+    }
+
+    public int inputBonusNumber() {
         return Integer.parseInt(Console.readLine());
     }
 
-    public void inputBonusNumber() {
-
+    private List<Integer> stringToListInteger(String input) {
+        st = new StringTokenizer(input, ",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        while(st.hasMoreTokens()) {
+            winningNumbers.add(Integer.valueOf(st.nextToken()));
+        }
+        return winningNumbers;
     }
 }

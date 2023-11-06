752ea4f55b42f77df3ca45c174f26540407a53ec
honeydanji <tjdwls3680@naver.com>
2023-11-06 15:02:34 +0900

    FEAT : 예외 처리 메시지 enum 으로 선언
    - 예외 처리는 크게 3가지로 나누어지지만 처리 메시지는 한가지 enum 으로 선언.
    - 예외 처리 관련해서 중복이 많아서 한개의 enum 으로 선언이 효율적이라 판단.

---
+++ src/main/java/lotto/utils/ExceptionMessage.java
@@ -0,0 +1,21 @@
+package lotto.utils;
+
+public enum ExceptionMessage {
+
+    NOT_NUMBER("[ERROR]숫자를 입력하세요."),
+    NON_DIVISIBLE_BY_1000("[ERROR]1000으로 나누어 떨어지지 않습니다."),
+    OUTSIDE_RANGE("[ERROR]입력 범위를 벗어났습니다."),
+    NON_COMMA("[ERROR]쉼표로 구분해주세요."),
+    EXCEEDS_LENGTH("[ERROR]입력을 초과했습니다."),
+    DUPLICATE_INPUT("[ERROR]입력이 중복되었습니다.");
+
+    private String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

b51658feea0c34ed9ccd410f134f0bd4a52efc74
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-07 07:01:03 +0900

    feat: 메시지 더 상세히 작성

--- src/main/java/lotto/Option/ErrorMessage.java
+++ src/main/java/lotto/Option/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
     NOT_NEGATIVE_NUMBER("음수는 입력할 수 없습니다."),
     NOT_ZERO("0원으로는 아무것도 구입할 수 없습니다."),
     NOT_DIVISION_1000("1000으로 나누어 떨어지는 금액을 입력하세요."),
-    NOT_NUMBER("숫자를 입력해주세요."),
+    NOT_NUMBER("10자리 이하의 숫자를 입력해주세요."),
     NOT_SIX_LENGTH("로또 번호는 6개여야 합니다.");
 
     private final String errorMessage;

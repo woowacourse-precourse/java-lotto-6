051e4057c0cece503cec374e9f1291fba9f80aa8
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 17:49:10 +0900

    Feat: 숫자 관련 상수를 모아서 관리하는 클래스 구현

--- src/main/java/lotto/domain/RandomNumberCreator.java
+++ src/main/java/lotto/domain/RandomNumberCreator.java
@@ -3,17 +3,13 @@ package lotto.domain;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.domain.NumberCreator;
+import lotto.constant.NumbersOption;
 
 public class RandomNumberCreator implements NumberCreator {
 
-	private static final int MIN_NUMBER = 1;
-	private static final int MAX_NUMBER = 45;
-	private static final int SIZE = 6;
-
 	@Override
 	public List<Integer> create() {
-		return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, SIZE);
+		return Randoms.pickUniqueNumbersInRange(NumbersOption.MIN, NumbersOption.MAX, NumbersOption.SIZE);
 	}
 
 }

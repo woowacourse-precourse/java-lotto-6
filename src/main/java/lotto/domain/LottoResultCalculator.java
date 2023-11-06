456be8543d97bed61d02fc59212b4ddad768e641
xogns4909 <xogns4909@naver.com>
2023-11-06 19:22:44 +0900

    refacotr : 결과 계산기 기본값 생성 로직  추가

--- src/main/java/lotto/domain/LottoResultCalculator.java
+++ src/main/java/lotto/domain/LottoResultCalculator.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -24,16 +25,31 @@ public class LottoResultCalculator {
 
     private static MatchResult calculateMatchResult(Lotto lotto, WinningNumbers winningNumbers) {
         int matchCount = MatchCalculator.calculateMatchCount(lotto, winningNumbers);
-        boolean  matchBonus = MatchCalculator.isBonusNumberMatched(lotto, winningNumbers);
+        boolean matchBonus = MatchCalculator.isBonusNumberMatched(lotto, winningNumbers);
         return new MatchResult(matchCount, matchBonus);
     }
 
     private static Map<LottoRank, Integer> aggregateRankCounts(List<MatchResult> matchResults) {
+        Map<LottoRank, Integer> countedRanks = countRanks(matchResults);
+        return initializeWithDefaults(countedRanks);
+    }
+
+    private static Map<LottoRank, Integer> countRanks(List<MatchResult> matchResults) {
         return matchResults.stream()
                 .map(MatchResult::getRank)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.groupingBy(Function.identity(),
                         Collectors.summingInt(rank -> 1)));
+    }
+
+    private static Map<LottoRank, Integer> initializeWithDefaults(
+            Map<LottoRank, Integer> countedRanks) {
+        Map<LottoRank, Integer> allRanksWithDefaults = new EnumMap<LottoRank, Integer>(
+                LottoRank.class);
+        for (LottoRank rank : LottoRank.values()) {
+            allRanksWithDefaults.put(rank, countedRanks.getOrDefault(rank, 0));
+        }
+        return allRanksWithDefaults;
     }
 }

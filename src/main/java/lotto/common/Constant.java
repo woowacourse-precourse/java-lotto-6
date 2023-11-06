78a3df934e9a31c6fa7159270578b53c49bd088b
kimdoha <kkhhjj3393@gmail.com>
2023-11-06 20:24:00 +0900

    feat(Constant): 로또 게임에 필요한 상수 정리

---
+++ src/main/java/lotto/common/Constant.java
@@ -0,0 +1,8 @@
+package lotto.common;
+
+public class Constant {
+    public static final Integer LOTTO_MIN_NUM = 1;
+    public static final Integer LOTTO_MAX_NUM = 45;
+    public static final Integer LOTTO_NUM_SIZE = 6;
+    public static final Integer LOTTO_TICKET_PRICE = 1_000;
+}

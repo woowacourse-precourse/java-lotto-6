71b3706edc55d406d79181cc99046fddbe2b240b
HungKungE <jonsun59@gmail.com>
2023-11-06 21:51:44 +0900

    [const] constNumber 추가

---
+++ src/main/java/lotto/constValue/ConstNumber.java
@@ -0,0 +1,8 @@
+package lotto.constValue;
+
+public class ConstNumber {
+    public static final int MIX_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_LENGTH = 6;
+    public static final int LOTTO_PRICE = 1000;
+}

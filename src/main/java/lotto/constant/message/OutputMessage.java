fb46e9d5e37058bba6fecc87f0ab40ef897fbe79
Annjieun <dkslwdms2121@naver.com>
2023-11-06 13:58:56 +0900

    refactor(OutputMessage): 출력 요구사항 충족을 위한 개행문자 추가

--- src/main/java/lotto/constant/message/OutputMessage.java
+++ src/main/java/lotto/constant/message/OutputMessage.java
@@ -1,10 +1,11 @@
 package lotto.constant.message;
 
 public enum OutputMessage {
-    PURCHASE("개를 구매했습니다."),
-    WINNING_STATUS("당첨 통계\n---"),
-    WINNING_RESULT("%d개 일치 (%,d원) - %d개"),
-    TOTAL_RETURN("총 수익률은 %.1f%%입니다.");
+    PURCHASE("\n%d개를 구매했습니다.\n"),
+    WINNING_STATUS("\n당첨 통계\n---"),
+    WINNING_RESULT("%d개 일치 (%,d원) - %d개\n"),
+    WINNING_RESULT_BALL("%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n"),
+    TOTAL_RETURN("총 수익률은 %,.1f%%입니다.");
 
     private final String message;
 

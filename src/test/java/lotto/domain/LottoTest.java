d4694d6457b8e89fa93015ae75169a6049bc0fc2
Yang-soeun <soeun8636@naver.com>
2023-11-06 21:02:03 +0900

    [test]: 보너스 번호가 당첨 번호와 중복되는지, 내 로또가 당첨번호와 몇개 일치하는지 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +25,39 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void outOfRangeException(){
+        //given //when //then
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 보너스 번호가 중복되는 확인한다.")
+    @Test
+    void checkContains(){
+        //given
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int bonusNumber = 1;
+
+        //when
+        boolean isContain = lotto.checkContains(bonusNumber);
+
+        //then
+        assertThat(isContain).isEqualTo(true);
+    }
+
+    @DisplayName("당첨 로또와 내가 산 로또가 몇개가 일치하는지 반환한다.")
+    @Test
+    void getSameNumberCoun(){
+        //given
+        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto myLotto = new Lotto(List.of(1,2,3,7,8,9));
+
+        //when
+        int sameNumberCount = winningLotto.getSameNumberCount(myLotto);
+
+        //then
+        assertThat(sameNumberCount).isEqualTo(3);
+    }
 }

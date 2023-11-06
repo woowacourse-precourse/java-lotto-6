fd06d6efa27a2375fd63f54fc1e93af6b5de21ff
jang010505 <jang010505@inu.ac.kr>
2023-11-07 03:56:35 +0900

    feat: SystemConstant 생성

---
+++ src/main/java/lotto/constant/SystemConstant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class SystemConstant {
+    public static final int NUMBER_MIN = 1;
+    public static final int NUMBER_MAX = 45;
+    public static final int NUMBER_COUNT = 6;
+}

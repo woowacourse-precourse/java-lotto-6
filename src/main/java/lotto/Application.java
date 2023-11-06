4ee970b3ee3c014ee02fd79d7dae04faf75356ef
sora0319 <sojeoung536985@gmail.com>
2023-11-06 15:01:22 +0900

    feat: make print method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,13 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+
 }
+
+

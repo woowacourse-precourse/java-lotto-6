f30713ecbeb7830bc814b0e5b5dcc520c6836a3c
hoyeon <highoption@naver.com>
2023-11-06 23:16:17 +0900

    feat(Store) : 로또를 만들어주는 makeLotto, makeLottoTickets 생성
    - 구한 로또 갯수를 바탕으로 로또를 만듬
    - 로또를 만들어서 LottoTicket으로 만듬

--- src/main/java/lotto/model/Store.java
+++ src/main/java/lotto/model/Store.java
@@ -1,9 +1,16 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Store {
+    private final LottoTickets lottoTickets;
     private final int lottoCount;
 
     public Store(int money) {
+        lottoTickets = makeLottoTickets();
         lottoCount = countLotto(money);
     }
 
@@ -11,4 +18,15 @@ public class Store {
         return money / 1000;
     }
 
+    public static List<Integer> makeLotto() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private LottoTickets makeLottoTickets() {
+        List<Lotto> lottoList = IntStream.range(0, lottoCount)
+                .mapToObj(i -> new Lotto(makeLotto()))
+                .collect(Collectors.toList());
+
+        return new LottoTickets(lottoList);
+    }
 }

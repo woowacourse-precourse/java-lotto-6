61d4e722e1af66b71420aa5af868c1650344104c
jaegyeongkim <dongji_11@naver.com>
2023-11-06 21:13:58 +0900

    feat: 당첨 번호와 보너스 번호 중복 검증 기능 구현

--- src/main/java/model/LottoWinningNumbers.java
+++ src/main/java/model/LottoWinningNumbers.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import utils.Converter;
 import utils.NumberValidation;
@@ -9,6 +10,7 @@ public class LottoWinningNumbers {
     private Lotto winningNumbers;
     private int bonusNumber;
     private static final String DELIMITER = ",";
+    private static final String DUPLICATION_BONUS_AND_WINNING = "[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.";
 
     public LottoWinningNumbers(String winningNumbers, String bonusNumber) {
         this.winningNumbers = stringToLotto(winningNumbers);
@@ -16,11 +18,25 @@ public class LottoWinningNumbers {
         this.bonusNumber = Converter.stringToInt(bonusNumber);
     }
 
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers.getNumbers();
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
     public void validationBonusNumber(int bonusNumber) {
         NumberValidation.isOverMaxNumber(bonusNumber);
         NumberValidation.isUnderMinNumber(bonusNumber);
+        duplicationBonusNumber(bonusNumber);
     }
 
+    public void duplicationBonusNumber(int bonusNumber){
+        if(winningNumbers.getNumbers().contains(bonusNumber)){
+            throw new IllegalArgumentException(DUPLICATION_BONUS_AND_WINNING);
+        }
+    }
     public String[] splitByDelimiter(String winningNumbers) {
         return winningNumbers.split(DELIMITER);
     }

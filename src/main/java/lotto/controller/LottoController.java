38760e4773cf27b506cc0fc0a6fc8969b7f743e7
tfer2442 <tfer2442@gmail.com>
2023-11-07 02:03:34 +0900

    refactor(LottoController): run 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,48 +1,60 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lotto.constant.LottoRank;
 import lotto.constant.PrintMessages;
-import lotto.model.Lotto;
 import lotto.model.LottoStore;
 import lotto.model.Lottos;
 import lotto.model.WinningNumber;
 import lotto.view.InputHandler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class LottoController {
     private static final String SEPARATOR = ",";
-    public void run() {
-        System.out.println(PrintMessages.INPUT_PRICE);
-        String purchasePrice = InputHandler.purchasePrice();
-        final Lottos lottos = LottoStore.publishLotto(Integer.parseInt(purchasePrice));
+    private String purchasePrice;
+    private Lottos lottos;
+    private WinningNumber winningNumber;
+    private List<String> beforeWinningNumbers;
+    private List<LottoRank> lottoRanks;
 
+    public void run() {
+        purchaseLotto();
+        inputWinningNumber();
+        inputBonusNumber();
+        checkLottoRank();
+
+        double rate = getRateOfReturn(lottoRanks, purchasePrice);
+    }
+
+    private void purchaseLotto() {
+        System.out.println(PrintMessages.INPUT_PRICE);
+        purchasePrice = InputHandler.purchasePrice();
+        lottos = LottoStore.publishLotto(Integer.parseInt(purchasePrice));
+    }
+
+    private void inputWinningNumber() {
         System.out.println(PrintMessages.INPUT_WINNING_NUMBER);
         String winningNumberInput = InputHandler.winningNumber();
-        final List<String> beforeWinningNumbers = Arrays.stream(winningNumberInput.split(SEPARATOR)).toList();
+        beforeWinningNumbers = Arrays.asList(winningNumberInput.split(SEPARATOR));
+    }
 
+    private void inputBonusNumber() {
         System.out.println(PrintMessages.INPUT_BONUS_NUMBER);
         String bonusNum = InputHandler.bonusNumber(beforeWinningNumbers);
-        final WinningNumber winningNumber = new WinningNumber(toIntegers(beforeWinningNumbers), Integer.parseInt(bonusNum));
+        winningNumber = new WinningNumber(toIntegers(beforeWinningNumbers), Integer.parseInt(bonusNum));
+    }
 
-        final List<LottoRank> lottoRanks = lottos.checkRank(winningNumber);
-        double rate = getRateOfReturn(lottoRanks, purchasePrice);
-        System.out.println(rate);
+    private void checkLottoRank() {
+        lottoRanks = lottos.checkRank(winningNumber);
     }
 
     private double getRateOfReturn(List<LottoRank> lottoRanks, String purchasePrice) {
-        int totalReward = 0;
-        double rateOfReturn;
+        int totalReward = lottoRanks.stream()
+                .mapToInt(LottoRank::getReward)
+                .sum();
 
-        for (LottoRank lottoRank : lottoRanks) {
-            totalReward += lottoRank.getReward();
-        }
-
-        rateOfReturn = totalReward / Double.parseDouble(purchasePrice);
-
-        return rateOfReturn;
+        return totalReward / Double.parseDouble(purchasePrice);
     }
 
     private List<Integer> toIntegers(List<String> beforeWinningNumbers) {

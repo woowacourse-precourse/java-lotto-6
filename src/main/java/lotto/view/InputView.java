9cf0b8cda6c3c9b996e4498fe99b0cde2e071eae
JongMinCh0i <wonderland314592@gmail.com>
2023-11-06 19:14:30 +0900

    feat(InputView): 입력된 로또 번호의 마지막 문자가 쉼표(,)인지 검증하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,12 +7,20 @@ public class InputView {
     private final static String ASK_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String ASK_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final static String ASK_BONUS_WINNING_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String END_OF_SENTENCE_CHARACTER = "문장의 마지막 문자는 컴마(',') 를 허용하지 않습니다.";
+    private static final String USER_DEFAULT_DELIMITER = ",";
 
     public int askPrice() {
         askHowManyPurchase();
         String input = Console.readLine();
         validateBlankAndEmptyInteger(input);
         return validateNegativeIntegerAndZero(validateInteger(input));
+    }
+
+    private void validateLastCharacter(String input) {
+        if (input.endsWith(USER_DEFAULT_DELIMITER)) {
+            throw new IllegalArgumentException(END_OF_SENTENCE_CHARACTER);
+        }
     }
 
     public void askWinningNumber() {

0d899a7f7d998c3251b988de99801a5d6c77e1d9
SeongHun <tjdgns5506@gmail.com>
2023-11-06 17:31:49 +0900

    refactor: 에러 메세지 수정

--- src/main/java/lotto/constant/Error.java
+++ src/main/java/lotto/constant/Error.java
@@ -1,9 +1,9 @@
 package lotto.constant;
 
 public class Error {
-    public static final String NOT_NUMBER = "[ERROR] 0이상의 숫자가 아닙니다. 다시 입력하세요.";
-    public static final String DO_NOT_DIVIDED_BY_1000 = "[ERROR] 1000으로 나누어 떨어지는 숫자가 아닙니다. 다시 입력하세요.";
-    public static final String PLEASE_ENTER_A_UNDUPLICATED_NUMBER_OF_6 = "[ERROR] 6개의 중복되지 않은 수를 입력해주세요.";
-    public static final String PLEASE_ENTER_ONLY_NUMBERS_FROM_1_TO_45 = "[ERROR] 1~45의 숫자만 입력해주세요.";
-    public static final String ALREADY_EXISTING_WINNING_NUMBER = "[ERROR] 당첨번호와 보너스번호는 같을 수 없습니다. 다시 입력하세요.";
+    public static final String NOT_NUMBER = "[ERROR] 0이상의 숫자를 입력해주세요.";
+    public static final String DO_NOT_DIVIDED_BY_1000 = "[ERROR] 1000으로 나누어 떨어지는 숫자를 입력해주세요.";
+    public static final String PLEASE_ENTER_A_UNDUPLICATED_NUMBER_OF_6 = "[ERROR] 중복되지 않는 6개의 숫자를 입력해주세요";
+    public static final String PLEASE_ENTER_ONLY_NUMBERS_FROM_1_TO_45 = "[ERROR] 1~45의 정수만 입력해주세요.";
+    public static final String ALREADY_EXISTING_WINNING_NUMBER = "[ERROR] 이미 당첨번호에 존재합니다. 당첨번호와 다른 수를 입력해주세요.";
 }

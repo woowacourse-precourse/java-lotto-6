e87c39290c16d56742bc6c4902934458be29ade0
Gyeongho Yang <chchch1213@naver.com>
2023-11-06 18:57:42 +0900

    feat(const): 구매 수량 결과 메세지 상수 추가

--- src/main/java/lotto/constant/GeneralMessage.java
+++ src/main/java/lotto/constant/GeneralMessage.java
@@ -1,7 +1,8 @@
 package lotto.constant;
 
 public enum GeneralMessage {
-    ASK_BUY_PRICE("구입금액을 입력해 주세요.");
+    ASK_BUY_PRICE("구입금액을 입력해 주세요."),
+    NOTIFY_BUY_COUNT("\n%d개를 구매했습니다.");
 
     private final String message;
 

5ac305f677ece066ad59af8e45a3252a398d27d3
alenjb <alenjb@naver.com>
2023-11-07 06:59:35 +0900

    🎨fix(Rank): reward의 자료형을 변경
    수익률 계산을 하기 위해 reward의 자료형을 문자열에서 정수형으로 변경함.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,24 +1,24 @@
 package lotto;
 
 public enum Rank {
-    FIRST_PLACE("2,000,000,000원", 6),
-    SECOND_PLACE("30,000,000원", 5.5),
-    THIRD_PLACE("1,500,000원", 5),
-    FOURTH_PLACE("50,000원", 4),
-    FIFTH_PLACE("5,000원", 3),
-    NONE("0원", 0)
+    FIRST_PLACE(2000000000, 6),
+    SECOND_PLACE(30000000, 5.5),
+    THIRD_PLACE(1500000, 5),
+    FOURTH_PLACE(50000, 4),
+    FIFTH_PLACE(5000, 3),
+    NONE(0, 0)
     ;
 
-    private final String reward;
+    private final int reward;
     private final double matchNumberCount;
 
 
-    Rank(String reward, double matchNumberCount) {
+    Rank(int reward, double matchNumberCount) {
         this.reward = reward;
         this.matchNumberCount = matchNumberCount;
     }
 
-    public String getReward() {
+    public int getReward() {
         return reward;
     }
 

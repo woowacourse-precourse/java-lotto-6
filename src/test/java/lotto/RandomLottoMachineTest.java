82fcc9ad250b9651effa941708e3505c5899955a
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 02:06:01 +0900

    test : 로또 생성 테스트

---
+++ src/test/java/lotto/RandomLottoMachineTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.model.lotto.Lotto;
+import lotto.model.lotto.LottoMachine;
+import lotto.model.lotto.RandomLottoMachine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomLottoMachineTest {
+    private LottoMachine lottoMachine;
+
+    @BeforeEach
+    void setUp(){
+        lottoMachine = new RandomLottoMachine();
+    }
+
+    @DisplayName("로또 생성 테스트")
+    @Test
+    void creaetLotto(){
+        assertThat(lottoMachine.generateLotto()).isInstanceOf(Lotto.class);
+    }
+}

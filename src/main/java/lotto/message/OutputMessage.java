0496c44f7512ab5a4dae9bd975be77998c70cde8
jdohun <wjdehgns97@naver.com>
2023-11-06 14:22:33 +0900

    Feat: 수익률 메시지 포맷 추가
    OutputMessage.java

--- src/main/java/lotto/message/OutputMessage.java
+++ src/main/java/lotto/message/OutputMessage.java
@@ -2,12 +2,17 @@ package lotto.message;
 
 public enum OutputMessage {
     OUTPUT_COUNT_OF_LOTTO_FORMAT("\n%d개를 구매했습니다."),
+
     WINNING_STATISTICS("\n당첨 통계\n---"),
     MATCHES_3_FORMAT("3개 일치 (5,000원) - %d개"),
     MATCHES_4_FORMAT("4개 일치 (50,000원) - %d개"),
     MATCHES_5_FORMAT("5개 일치 (1,500,000원) - %d개"),
     MATCHES_5_ADDITION_BONUS_NUMBER_FORMAT("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    MATCHES_6_FORMAT("6개 일치 (2,000,000,000원) - %d개");
+    MATCHES_6_FORMAT("6개 일치 (2,000,000,000원) - %d개"),
+
+    TOTAL_RETURN("총 수익률은 %.1f%입니다."),
+    ;
+
 
     private final String message;
 

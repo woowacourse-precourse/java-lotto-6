77812005266c29c7e4d90d7d3d4abed9923bc169
Jokyo0612 <jgh010612@gmail.com>
2023-11-06 15:31:01 +0900

    feat : 초기 입력금액을 정수로 변환하는 기능 구현 1번 기능에 대한 클래스 생성 정수 변환 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+class money_for_lotto{
+    public static String inputNumber;
+
+    public static int realMoney(String inputNumber){
+        return Integer.parseInt(inputNumber);
+    }
+
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputMoney = Console.readLine();
+        money_for_lotto money = new money_for_lotto();
+        money.realMoney(inputMoney);
     }
 }

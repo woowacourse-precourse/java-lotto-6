330b2156e84b851b8d784e6fc9bee9efae7f6ab0
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-06 23:17:07 +0900

    refactor: WinningNumbers 내 메소드 분리

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -18,23 +18,31 @@ public class WinningNumbers {
         validateWinningNumbersRange(winningNumbersIntegerList);
         validateWinningNumberDuplication(winningNumbersIntegerList);
         validateWinningNumbersCount(winningNumbersIntegerList);
-
     }
 
-    private void validateWinningNumbersRange(List<Integer> winningNumbersIntegerList) {
-        for (int number : winningNumbersIntegerList) {
-            if (number < Constants.LOTTO_MIN_NUMBER || number > Constants.LOTTO_MAX_NUMBER) {
-                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE_INVALID_ERROR.getMessage());
-            }
+    private void validateWinningNumbersRange(List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            validateNumberRange(number);
         }
     }
+
+    private void validateNumberRange(int number) {
+        if (number < Constants.LOTTO_MIN_NUMBER || number > Constants.LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE_INVALID_ERROR.getMessage());
+        }
+    }
+
 
     private void validateWinningNumberDuplication(List<Integer> winningNumbersIntegerList) {
         Set<Integer> numberSet = new HashSet<>();
         for (int number : winningNumbersIntegerList) {
-            if (!numberSet.add(number)) {
-                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_DUPLICATION_ERROR.getMessage());
-            }
+            validateDuplication(numberSet, number);
+        }
+    }
+
+    private void validateDuplication(Set<Integer> numberSet, int number) {
+        if (!numberSet.add(number)) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_DUPLICATION_ERROR.getMessage());
         }
     }
 
@@ -51,6 +59,4 @@ public class WinningNumbers {
             throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_COUNT_INVALID_ERROR.getMessage());
         }
     }
-
-
 }

c9c66f6f19c860a8d7071ceb6a35c3373ff3e9d9
seokmyungham <hellotjraud@gmail.com>
2023-11-06 12:31:23 +0900

    test(PurchaseAmountTest): 구입 금액 테스트 리팩토링

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,31 +1,35 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PurchaseAmountTest {
-
+    @DisplayName("구입 금액이 1원 미만이면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, -23})
-    void 구입_금액이_1원_이상이_아닐_경우_에러_발생(int money) {
-        Assertions.assertThatThrownBy(() -> new PurchaseAmount(money))
+    void purchaseAmountBelowOne(int money) {
+        assertThatThrownBy(() -> new PurchaseAmount(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("구입 금액은 1원 이상이어야 합니다.");
     }
 
+    @DisplayName("구입 금액이 1000원 단위가 아니면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1001, 999, 1, 1234, 42342})
-    void 구입_금액이_1000원_단위가_아닐_경우_에러_발생(int money) {
-        Assertions.assertThatThrownBy(() -> new PurchaseAmount(money))
+    @ValueSource(ints = {1001, 999, 1, 42342})
+    void purchaseAmountIsNotMultipleOfThousand(int money) {
+        assertThatThrownBy(() -> new PurchaseAmount(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("구입 금액이 1000원 단위이어야 합니다.");
     }
 
+    @DisplayName("구입 금액이 1원 이상이고 1000원 단위일 경우 정상 생성한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1000, 2000, 35000, 14000})
-    void 구입_금액이_1원_이상이고_1000원_단위일_경우_정상_생성(int money) {
-        Assertions.assertThatNoException()
+    @ValueSource(ints = {1000, 35000, 14000})
+    void purchaseAmountCreateSuccessfully(int money) {
+        assertThatNoException()
                 .isThrownBy(() -> new PurchaseAmount(money));
     }
 }

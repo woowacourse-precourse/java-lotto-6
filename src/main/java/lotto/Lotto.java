9221abbf6e0dd8b21e4d557010121ce49411806f
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 04:37:50 +0900

    feat : 당첨번호 입력기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,18 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 public class Lotto {
     private static final int NUM_OF_LOTTO =6;
     private static final String NUMBER_SIZE_ERROR = "[ERROR] 당첨 번호는 "+NUM_OF_LOTTO+"개 입력 가능합니다.";
+    private static final String OVERLAP_NUM_ERROR="[ERROR] 중복된 숫자가 존재합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkOverlap(numbers);
         this.numbers = numbers;
     }
 
@@ -18,5 +23,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void checkOverlap(List<Integer> numbers){
+        Set<Integer> overlapCheck = new HashSet<>();
+        for(int i=0; i<numbers.size(); i++)
+        {
+            overlapCheck.add(numbers.get(i));
+        }
+        if(overlapCheck.size()!=6){
+            throw new IllegalArgumentException(OVERLAP_NUM_ERROR);
+        }
+
+    }
 
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,6 +10,9 @@ public class InputView {
     private static final String TYPE_ERROR ="숫자가 아닌 값을 입력하셨습니다.";
     private static final String INPUT_WINNING_NUMBERS ="당첨 번호를 입력해주세요.";
     private static List<Integer> winningNumbers;
+    private static final String RANGE_ERROR="[ERROR] 옳지 않은 범위의 숫자를 입력하셨습니다.";
+    private static final int MAX_LOTTO_NUM=45;
+
     public static int inputPlayerAmount(){
         System.out.println(INPUT_LOTTO_AMOUNT);
         return convertToInt(Console.readLine());
@@ -28,11 +31,27 @@ public class InputView {
         String [] result= inputNumbers.split(",");
         winningNumbers= new ArrayList<>();
         for(int i=0; i<result.length; i++){
-            winningNumbers.add(Integer.parseInt(result[i]));
+            int num=checkNumber(result[i]);
+            checkRange(num);
+            winningNumbers.add(num);
         }
         return winningNumbers;
+    }
 
+    private static int checkNumber(String num){
+        try{
+            return Integer.parseInt(num);
+        }
+        catch(NumberFormatException e){
+            throw new IllegalArgumentException(TYPE_ERROR);
+        }
+    }
 
+    private static int checkRange(int num){
+        if(num<=0 || num>MAX_LOTTO_NUM){
+            throw new IllegalArgumentException(RANGE_ERROR);
+        }
+        return num;
     }
 
 

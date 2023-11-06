db6e9433f4c1cedb0f818bf17a5c280333a4ad2b
tam31 <wxogud3377@naver.com>
2023-11-06 22:21:19 +0900

    Feat(LottoPrize) : 로또 당첨금 long 상수 추가

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,24 +1,30 @@
 package lotto.domain;
 
 public enum LottoPrize {
-    Three_PRIZE(3, "5,000"),
-    FOUR_PRIZE(4,"50,000"),
-    FIVE_PRIZE(5,"1,500,000"),
-    FIVE_AND_BONUS_PRIZE(50,"30,000,000"),
-    SIX_PRIZE(6,"2,000,000,000");
+    Three_PRIZE(3, 5000,"5,000"),
+    FOUR_PRIZE(4,50000,"50,000"),
+    FIVE_PRIZE(5,1500000,"1,500,000"),
+    FIVE_AND_BONUS_PRIZE(50,30000000,"30,000,000"),
+    SIX_PRIZE(6,2000000000,"2,000,000,000");
 
     private final int count;
-    private final String prizeMoney;
-    LottoPrize(int count, String prizeMoney) {
+    private final long prizeMoney;
+    private final String money;
+    LottoPrize(int count, long prizeMoney, String money) {
         this.count = count;
         this.prizeMoney = prizeMoney;
+        this.money = money;
     }
 
     public int getCount() {
         return count;
     }
 
-    public String getPrizeMoney() {
+    public long getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getMoney() {
+        return money;
     }
 }

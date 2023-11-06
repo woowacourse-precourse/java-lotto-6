d2fd922da3c2d1eb7f7f9f963432dba771e289cf
Firedrago95 <yonghwa95@naver.com>
2023-11-06 13:34:41 +0900

    refactor(ParserTest) : 유효성 검사 상황별 테스트 메서드 분리

--- src/test/java/lotto/Util/ParserTest.java
+++ src/test/java/lotto/Util/ParserTest.java
@@ -2,6 +2,7 @@ package lotto.Util;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -10,14 +11,34 @@ import org.junit.jupiter.api.Test;
 public class ParserTest {
 
     @Test
-    @DisplayName("유효성 검사 예외발생 테스트")
+    @DisplayName("null 빈값 유효성 검사 테스트")
     public void exceptionTest() {
-        String[] invalidInputs = {null, "", "3,24,", "1,2,3,4,5,46" ,"1,1,2,3,4,5"};
+        String[] invalidInputs = {null, ""};
 
         for (String input : invalidInputs) {
             assertThatThrownBy(() -> Parser.convertStringToNumbers(input)).isInstanceOf(
-                IllegalArgumentException.class);
+                IllegalArgumentException.class).hasMessage(Parser.NULL_EMPTY_MESSAGE);
         }
+    }
+
+    @Test
+    @DisplayName("쉼표로 구분된 6개의 숫자 유효성 검사 테스트")
+    public void sixSizeTest() {
+        String[] invalidInputs = {"1,2,3,4,5", "1,2,3,", "일,이,삼,사,오,육"};
+
+        for (String input : invalidInputs) {
+            assertThatThrownBy(() -> Parser.convertStringToNumbers(input)).isInstanceOf(
+                IllegalArgumentException.class).hasMessage(Parser.INVALID_FORM_MESSAGE);
+        }
+    }
+
+    @Test
+    @DisplayName("중복 숫자 유효성검사 테스트")
+    public void duplicateTest() {
+        String duplicatedInput = "1,1,2,3,4,5";
+
+        assertThatThrownBy(() -> Parser.convertStringToNumbers(duplicatedInput)).isInstanceOf(
+        IllegalArgumentException.class).hasMessage(Parser.DUPLICATE_MESSAGE);
     }
 
     @Test

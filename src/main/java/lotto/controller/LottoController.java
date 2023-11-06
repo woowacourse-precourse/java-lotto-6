16fcc765de5323b3035ced4045ae043f9f7a9bfd
koomchang <leahpar0401@gmail.com>
2023-11-07 03:36:00 +0900

    refactor: controller에서 메소드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,10 +2,12 @@ package lotto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
 import lotto.model.LottoGenerator;
 import lotto.model.LottoNumber;
 import lotto.model.LottoResult;
+import lotto.model.Rank;
 import lotto.model.WinningLotto;
 import lotto.validator.BonusNumberInputValidator;
 import lotto.validator.MoneyInputValidator;
@@ -25,21 +27,32 @@ public class LottoController {
     public void run() {
         outputView.askForMoney();
         int money = inputView.inputMoney();
+
         int lottoCount = money / LottoNumber.PURCHASE_PRICE.getNumber();
         outputView.printLottoCount(lottoCount);
+
+        LottoResult lottoResult = new LottoResult(generateLotto(lottoCount));
+        outputView.askForWinningNumbers();
+        List<Integer> winningNumbers = inputView.inputWinningNumbers();
+
+        outputView.askForBonusNumber();
+        int bonusNumber = inputView.inputBonusNumber(winningNumbers);
+
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        Map<Rank, Integer> result = lottoResult.checkResult(winningLotto);
+        outputView.printWinningStatistics(result);
+        double returnRate = lottoResult.calculateReturnRate(result, money);
+        outputView.printTotalProfit(returnRate);
+    }
+
+    private List<Lotto> generateLotto(int lottoCount) {
         List<Lotto> lottoArrayList = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             Lotto generatedLotto = lottoGenerator.generate();
             lottoArrayList.add(generatedLotto);
             outputView.printLottoNumbers(generatedLotto.getNumbers());
         }
-        LottoResult lottoResult = new LottoResult(lottoArrayList);
-        outputView.askForWinningNumbers();
-        List<Integer> winningNumbers = inputView.inputWinningNumbers();
-        outputView.askForBonusNumber();
-        int bonusNumber = inputView.inputBonusNumber(winningNumbers);
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
-        outputView.printWinningStatistics(lottoResult.checkResult(winningLotto));
-        outputView.printTotalProfit(lottoResult.calculateReturnRate(lottoResult.checkResult(winningLotto), money));
+        return lottoArrayList;
     }
+
 }

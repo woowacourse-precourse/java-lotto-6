aef1bf033a01f4cadc10422ede8891782b536c7c
moong2 <www.castlehi@gmail.com>
2023-11-06 20:56:15 +0900

    test: change the name of method

--- src/test/java/lotto/model/validator/AmountOfLottoValidatorTest.java
+++ src/test/java/lotto/model/validator/AmountOfLottoValidatorTest.java
@@ -36,46 +36,48 @@ class AmountOfLottoValidatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"2147483648"})
+    @ValueSource(longs = {2147483648L})
     @DisplayName("Integer 자료형이 아닐 경우 IllegalArgumentException이 반환된다.")
-    void biggerThanIntegerMaxFailTest(String amountOfLotto) {
+    void biggerThanIntegerMaxFailTest(Long amountOfLotto) {
         // given
         // when & then
         assertThatIllegalArgumentException().isThrownBy(
-                () -> amountOfLottoValidator.checkType(amountOfLotto));
+                () -> amountOfLottoValidator.checkAmountWithinLottoPriceRange(amountOfLotto));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1000})
+    @ValueSource(longs = {1000})
     @DisplayName("양수 입력 시 어떠한 에러도 반환되지 않는다.")
-    void integerSuccessTest(int amountOfLotto) {
+    void integerSuccessTest(Long amountOfLotto) {
         // given
         // when & then
-        assertThatNoException().isThrownBy(() -> amountOfLottoValidator.checkRange(amountOfLotto));
+        assertThatNoException().isThrownBy(
+                () -> amountOfLottoValidator.checkAmountWithinLottoPriceRange(amountOfLotto));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, -1, -2, -3})
+    @ValueSource(longs = {0, -1, -2, -3})
     @DisplayName("양수가 아닌 값 입력 시 IllegalArgumentException이 반환된다.")
-    void zeroFailTest(int amountOfLotto) {
+    void zeroFailTest(Long amountOfLotto) {
         // given
         // when & then
-        assertThatIllegalArgumentException().isThrownBy(() -> amountOfLottoValidator.checkRange(amountOfLotto));
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> amountOfLottoValidator.checkAmountWithinLottoPriceRange(amountOfLotto));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1000, 10000, 2147483000})
+    @ValueSource(longs = {1000, 10000, 2147483000})
     @DisplayName("로또 금액 (현재 기준 " + "원) 으로 나누어 떨어질 시 어떠한 에러도 반환하지 않는다.")
-    void divideByLottoPrice(int amountOfLotto) {
+    void divideByLottoPrice(Long amountOfLotto) {
         // given
         // when & then
         assertThatNoException().isThrownBy(() -> amountOfLottoValidator.checkDivideByLottoPrice(amountOfLotto));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
+    @ValueSource(longs = {1, 2, 3, 4, 5, 6, 7, 8, 9})
     @DisplayName("로또 금액 (현재 기준 " + "원) 으로 나누어 떨어지지 않을 시 IllegalArgumentException이 반환된다.")
-    void notDivideByLottoPrice(int amountOfLotto) {
+    void notDivideByLottoPrice(Long amountOfLotto) {
         // given
         // when & then
         assertThatIllegalArgumentException().isThrownBy(

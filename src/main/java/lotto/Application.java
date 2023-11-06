e0f6ad5d23995a2371667842d6608f9b4e321958
stats-dev <hohun94@naver.com>
2023-11-06 15:44:29 +0900

    feat2: 로또 번호 자동 출력 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
-    public static int[] lottoNum(int num) {
+    public static int[] winLottoNum(int num) {
 
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] lottoNumsStr = readLine().split(",");
@@ -20,6 +25,12 @@ public class Application {
         }
         return lottoNums;
     }
+
+    public static List<Integer> lottoNums(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
+
 
     public static int bonusNum() {
         System.out.println("보너스 번호를 입력해 주세요.");
@@ -42,11 +53,29 @@ public class Application {
         return num;
     }
 
+    // 배열을 문자열로 변환
+    public static String arrToStr(List<Integer> lottos) {
+        StringBuilder builder = new StringBuilder("[");
+        for (int i = 0; i < lottos.size(); i++) {
+            builder.append(lottos.get(i));
+            if (i < lottos.size() - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int num = inputMoney();
-        lottoNum(num);
+        //lotto 번호 출력
+        for (int ticket = 1; ticket <= num; ticket++) {
+            List lottoNumbers = lottoNums();
+            System.out.println(arrToStr(lottoNumbers));
+        }
+        winLottoNum(num);
         bonusNum();
 
 

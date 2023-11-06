78c6d73ef52bb2c68698432f9cf4e18573cb74cf
siwol025 <cyk99115695@gmail.com>
2023-11-06 20:16:40 +0900

    feat(Lotto): 로또 번호가 1부터 45 사이의 숫자인지 검사하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import lotto.util.LottoConstants;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,14 +15,23 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
     private void validateDuplication(List<Integer> numbers) {
         if (hasDuplicate(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 중복할 수 없습니다.");
         }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> LottoConstants.isNumberInRange(number))
+                .findAny()
+                .ifPresent(number -> {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                });
     }
 
     private boolean hasDuplicate(List<Integer> numbers) {

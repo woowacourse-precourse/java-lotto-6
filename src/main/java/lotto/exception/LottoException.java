07521e9a6c47591bc0c40cd5e1d780130647a9b9
minseo  jo <liging12@naver.com>
2023-11-06 20:00:59 +0900

    fix: 예외에 "." 을 빼먹음

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -14,7 +14,7 @@ public class LottoException extends IllegalArgumentException {
         RANGE_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자입니다."),
         DUPLICATE_LOTTO_NUMBERS("1부터 45 사이의 중복되지 않는 숫자 6개를 입력해 주세요." + LINE.getMessage() +
                 "양식: \"1,7,13,24,42,45\""),
-        RANGE_BONUS_NUMBER("보너스 번호는 1부터 45 사이의 숫자입니다"),
+        RANGE_BONUS_NUMBER("보너스 번호는 1부터 45 사이의 숫자입니다."),
         LOTTO_AMOUNT("숫자(양수)를 입력해 주세요." + LINE.getMessage() +
                 "로또 한 장 금액은 " + LOTTO_PRICE.getValue() + "원 입니다." + LINE.getMessage() +
                 "개인이 구매 가능한 로또 금액은 최대 "  + MAX_LOTTO_PRICE_PER_USER.getValue() + "원 입니다."),

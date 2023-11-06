4e8f6aa61efee84ba3f63982becdc8963cbee014
HurDong <gjehdals456@gmail.com>
2023-11-06 18:43:58 +0900

    feat : Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.View.LottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.startGame();
     }
 }

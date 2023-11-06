dd3cbae6210b3b02ccd2e8a916f3aae505246a51
Haebin <joker7011@naver.com>
2023-11-07 06:24:52 +0900

    feat: `Console.close` 로직 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LottoMainController;
 
 public class Application {
     public static void main(String[] args) {
         LottoMainController.start();
+        Console.close();
     }
 }

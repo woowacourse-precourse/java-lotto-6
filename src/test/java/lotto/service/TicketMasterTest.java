dfdf6716a70c3b881e888c678f164d2ef8cfdc6f
CHAHANS <herodev0129@gmail.com>
2023-11-06 17:14:13 +0900

    test(TicketMaster): 보너스 번호 확인 기능 테스트 코드 추가

--- src/test/java/lotto/service/TicketMasterTest.java
+++ src/test/java/lotto/service/TicketMasterTest.java
@@ -1,7 +1,5 @@
 package lotto.service;
 
-
-import lotto.domain.Customer;
 import lotto.domain.Lotto;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,32 +16,28 @@ class TicketMasterTest {
     @BeforeEach
     void setUP() {
         this.tm = new TicketMaster();
-    }
-
-    @DisplayName("Customer의 coin/1000개의 로또를 생성한다.")
-    @Test
-    void sellLotto_TEST() {
-        Customer cs = new Customer(4000);
-        tm.makeTicket(cs);
-        assertThat(cs.getCustomerLotto().size()).isEqualTo(4);
+        tm.setWinningNum(new Lotto(List.of(1,2,11,22,33,44)));
     }
 
     @DisplayName("보너스 번호 입력시 당첨번호와 중복되면 예외처리")
     @Test
     void creatBonusNumDuplicateWinningNumbers() {
-        tm.setWinningNum(new Lotto(List.of(1,2,11,22,33,44)));
         assertThatThrownBy(()->tm.setBonusNum(2)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 당첨 갯수를 확인할 수 있어야 한다.")
+    @DisplayName("로또 당첨 갯수를세는 기능")
     @Test
     void checkLotto_TEST() {
         Lotto newLottoNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto winnerNumber = new Lotto(List.of(1,2,11,22,33,44));
-        tm.setWinningNum(winnerNumber);
-        tm.setBonusNum(3);
-
         int result = tm.checkLotto(newLottoNumber);
-        assertThat(result).isEqualTo(3);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @DisplayName("로또에 보너스 번호가 있는지 비교하는 기능")
+    @Test
+    void checkLottoBonusNum_TEST() {
+        Lotto newLottoNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        tm.setBonusNum(3);
+        assertThat(tm.checkLottoBonus(newLottoNumber)).isEqualTo(true);
     }
 }

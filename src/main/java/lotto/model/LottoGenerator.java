3f9266b73728e0b25c63910ddaf54efbacdd9b34
hyuna <monicajo0@naver.com>
2023-11-05 17:06:04 -0800

    feat(LottoGenerator): 로또 번호 생성하는 기능 구현

---
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoGenerator {
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(Constants.MIN_LOTTO_NUMBER,
+                Constants.MAX_LOTTO_NUMBER,
+                Constants.LOTTO_NUMBER_COUNT);
+    }
+}

8705205a7aa81287d27d6947790e50c6a4e23147
kyoo0115 <internetkyoomin@gmail.com>
2023-11-06 18:21:40 +0900

    feat: ranking enum 추가 기능 구현 및 수정

--- src/main/java/lotto/model/Ranking.java
+++ src/main/java/lotto/model/Ranking.java
@@ -1,24 +1,32 @@
 package lotto.model;
 
 public enum Ranking {
-    FIRST_PRIZE(6, 2000000000),
-    SECOND_PRIZE(5, 30000000),
-    THIRD_PRIZE(5, 1500000),
-    FOURTH_PRIZE(4, 50000),
-    FIFTH_PRIZE(3, 5000),
-    NO_PRIZE(0, 0);
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - "),
+    FOURTH(4, 50_000, "4개 일치 (50,000원) - "),
+    FIFTH(3, 5_000, "3개 일치 (5,000원) - "),
+    NO_PRIZE(0, 0, "");
 
     private final int matchCount;
     private final int prize;
+    private final String description;
 
-    Ranking(int matchCount, int prize) {
+    Ranking(int matchCount, int prize, String description) {
         this.matchCount = matchCount;
         this.prize = prize;
+        this.description = description;
     }
 
-    // Getter methods
     public int getMatchCount() { return matchCount; }
-    public int getPrize() { return prize; }
+    public String getDescription() { return description; }
 
-    // Additional methods for checking matches and determining prize
+    public static Ranking findByMatchCount(int matchCount) {
+        for (Ranking ranking : values()) {
+            if (ranking.getMatchCount() == matchCount) {
+                return ranking;
+            }
+        }
+        return NO_PRIZE;
+    }
 }

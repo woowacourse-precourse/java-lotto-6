799ad4c1da71b29df43394910ab333c3b3541e27
Sanghee0820 <lsh277604@gmail.com>
2023-11-06 17:40:27 +0900

    Feat: add WinningType Enum

--- src/main/java/lotto/Domain/WinningType.java
+++ src/main/java/lotto/Domain/WinningType.java
@@ -1,9 +1,33 @@
 package lotto.Domain;
 
+import java.text.DecimalFormat;
+
 public enum WinningType {
-    FIFTH_PLACE,
-    FORTH_PLACE,
-    THIRD_PLACE,
-    SECOND_PLACE,
-    FIRST_PLACE
+    FIRST_PLACE(6, 2000000000L),
+    SECOND_PLACE(5, 30000000L),
+    THIRD_PLACE(5, 1500000L),
+    FORTH_PLACE(4, 50000L),
+    FIFTH_PLACE(3, 5000L);
+
+
+    private int matchCnt;
+    private Long winningPrice;
+
+    WinningType(int matchCnt, Long winningPrice) {
+        this.matchCnt = matchCnt;
+        this.winningPrice = winningPrice;
+    }
+
+    public Long getWinningPrice() {
+        return winningPrice;
+    }
+
+    public int getMatchCnt() {
+        return matchCnt;
+    }
+    public String getWinningPricePrintType() {
+        DecimalFormat df = new DecimalFormat("###,###");
+        return df.format(getWinningPrice());
+    }
+
 }

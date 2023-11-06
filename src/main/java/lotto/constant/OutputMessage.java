fab1c9f48e245fb70c057fcf470ed2bcb969cfe9
slowcloud <sju9417@gmail.com>
2023-11-06 18:39:32 +0900

    fix: 수익률 출력 메시지 수정

--- src/main/java/lotto/constant/OutputMessage.java
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -10,6 +10,6 @@ public class OutputMessage {
     public static String GET_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static String GET_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static String LOTTO_RESULT_MESSAGE = "당첨 통계\n---";
-    public static String INCOME_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+    public static String INCOME_RATE_MESSAGE = "총 수익률은 %,.1f%%입니다.";
 
 }

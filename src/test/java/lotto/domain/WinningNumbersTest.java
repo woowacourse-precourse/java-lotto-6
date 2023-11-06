98334db04d79babf0f81a073514bbb014602f27d
Mr.Kim <joajy98@gmail.com>
2023-11-06 23:04:27 +0900

    test: 당첨 및 보너스 번호에 대한 예외 테스트 및 기능 테스트 구현

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.domain.WinningNumbers.*;
+import static lotto.service.LottoService.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WinningNumbersTest {
 
@@ -26,8 +28,8 @@ public class WinningNumbersTest {
     }
 
     @Test
-    @DisplayName("보너스 번호가 당첨 번호와 중복되는 경우에 대한 테스트")
-    void bonusNumberIsInWinningNumbers() {
+    @DisplayName("보너스 번호가 당첨 번호와 중복되는 경우에 대한 예외 테스트")
+    void bonusNumberAlreadyExistsExceptionTest() {
         //given
 
         //when
@@ -37,8 +39,8 @@ public class WinningNumbersTest {
     }
 
     @Test
-    @DisplayName("보너스 번호가 1 이상 45 이하가 아닐 경우에 대한 테스트")
-    void bonusNumberOutOfRangeTest() {
+    @DisplayName("보너스 번호가 1 이상 45 이하가 아닐 경우에 대한 예외 테스트")
+    void bonusNumberOutOfRangeExceptionTest() {
         //given
 
         //when
@@ -50,12 +52,31 @@ public class WinningNumbersTest {
     }
 
     @Test
-    @DisplayName("당첨 번호에 숫자 외의 값이 들어갔는지 확인하는 테스트")
-    void bonusNumberIsNotNumberTest(){
+    @DisplayName("당첨 번호에 숫자 외의 값이 들어갔는지 확인하는 예외 테스트")
+    void bonusNumberIsNotNumberExceptionTest() {
         //given
         String input = "1x";
 
+        //when
+
+        //then
         assertThatThrownBy(() -> WinningNumbers.validateNumbersType(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("보너스 번호를 포함한 당첨을 확인하는 기능 테스트")
+    void secondRankWithBonusNumberTest(){
+        //given
+        List<Integer> winningLotto = List.of(1,2,3,4,5,6);
+        List<Integer> userLotto = List.of(2,3,4,5,6,7);
+
+        //when
+        int bonusNumber = 7;
+        WinningNumbers winningNumbers = new WinningNumbers(new Lotto(winningLotto), bonusNumber);
+        matchLottoTicket(winningNumbers, new Lotto(userLotto));
+
+        //then
+        assertEquals(1, Rank.SECOND.getWinningAmount());
     }
 }

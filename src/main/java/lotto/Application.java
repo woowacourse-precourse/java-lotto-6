f07af40cd0da9fe6f6b7c2fb8bd52b2858467280
ChoiseU <csw980928@gmail.com>
2023-11-07 06:23:19 +0900

    refactor : 메인에 쓰지않는 부분 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,8 +6,6 @@ import lotto.view.LottoView;
 
 public class Application {
     private static LottoView lottoView = new LottoView();
-    private static LottoUtil lottoUtil = new LottoUtil();
-    private static String payment = null;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현

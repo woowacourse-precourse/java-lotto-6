b112840adbef2eaa7ab004cac3de19c544903980
kylim99 <zxzx05050@naver.com>
2023-11-06 20:54:52 +0900

    feat:  입력 받은 보너스 수가 범위 안에 있는지 유효성 검사 기능

--- src/main/java/lotto/util/validator/BonusValidator.java
+++ src/main/java/lotto/util/validator/BonusValidator.java
@@ -1,5 +1,8 @@
 package lotto.util.validator;
 
+import static lotto.util.Constant.END_NUMBER;
+import static lotto.util.Constant.START_NUMBER;
+
 import lotto.model.Lotto;
 import lotto.util.ExceptionMessage;
 
@@ -14,7 +17,16 @@ public class BonusValidator {
             System.out.println(ExceptionMessage.INVALID_BONUS_IS_CONTAIN_NUMBERS.getMessage());
             return false;
         }
+        if (validationRange(input)){
+            System.out.println(ExceptionMessage.INVALID_INPUT_RANGE.getMessage());
+            return false;
+        }
         return true;
+    }
+
+    private boolean validationRange(String input) {
+        int bonus = Integer.parseInt(input);
+        return START_NUMBER > bonus || bonus > END_NUMBER;
     }
 
     private boolean validationIsDuplicate(String input, Lotto winnerLottoOnlyNumbers) {

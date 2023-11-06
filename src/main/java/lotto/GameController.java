39bd733bf46364227aacb2b9df2845124906419c
namhoon-kim97 <kskcmh5723@gmail.com>
2023-11-06 22:21:27 +0900

    feat: 로또 구입금액 입력기능 구현

--- src/main/java/lotto/GameController.java
+++ src/main/java/lotto/GameController.java
@@ -2,14 +2,17 @@ package lotto;
 
 import java.util.List;
 import lotto.view.BonusNumberInputView;
+import lotto.view.LottoPurchaseInputView;
 import lotto.view.WinningNumbersInputView;
 
 public class GameController {
     public void init() {
         // 로또 구입 금액을 입력 받는다.
+        int purchaseAmount = LottoPurchaseInputView.getLottoPurchaseAmount();
+        // 당첨 번호를 입력 받는다.
         List<Integer> winningNumbers = WinningNumbersInputView.getWinningNumbers();
         // 보너스 번호를 입력 받는다.
         int bonusNumber = BonusNumberInputView.getBonusNumber();
-
+        
     }
 }
---
+++ src/main/java/lotto/view/LottoPurchaseInputView.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
+import lotto.exception.InvalidLottoPurchaseException;
+
+public class LottoPurchaseInputView {
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
+    private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static int getLottoPurchaseAmount() {
+        System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
+        String purchaseAmount = Console.readLine().trim();
+        System.out.println();
+        inputValidate(purchaseAmount);
+        validateLottoPurchaseAmount(purchaseAmount);
+        return Integer.parseInt(purchaseAmount);
+    }
+
+    private static void inputValidate(String inputWinningNumbers) {
+        if (!NUMBER_PATTERN.matcher(inputWinningNumbers).matches()) {
+            throw new InvalidLottoPurchaseException();
+        }
+    }
+
+    private static void validateLottoPurchaseAmount(String input) {
+        int inputValue = Integer.parseInt(input);
+        if (inputValue % 1000 != 0) {
+            throw new InvalidLottoPurchaseException();
+        }
+    }
+}

8f71d2696efdb7a2761448128dbae240c0ef5669
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-06 19:07:09 +0900

    feat: 당첨 케이스를 담은 Enum 클래스 생성

---
+++ src/main/java/lotto/WinnigCase.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public enum WinnigCase {
+    MATCH_THREE("3개 일치 (5,000원)"),
+    MATCH_FOUR("4개 일치 (50,000원)"),
+    MATCH_FIVE("5개 일치 (1,500,000원)"),
+    MATCH_FIVE_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    MATCH_SIX("6개 일치 (2,000,000,000원)");
+
+    private String contents;
+
+    private WinnigCase(String contents) {
+        this.contents = contents;
+    }
+
+    public String getContents() {
+        return this.contents;
+    }
+
+}
+

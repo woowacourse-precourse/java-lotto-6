ea1acc4d6c7ca494dd2fc2935c4035c9b5a8d315
DahyeY <dhuniv@naver.com>
2023-11-06 18:38:17 +0900

    feat: 랜덤 번호의 로또 티켓 발행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+class User{
+    public List<List<Integer>> tickets;
+    private List<Integer> generateTicket(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 }

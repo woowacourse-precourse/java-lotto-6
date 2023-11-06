deacff4f4a06f2465f8c4375fa7cdc7a049a72fa
Isaac Han <isaachan233@gmail.com>
2023-11-06 15:23:20 +0900

    refactor: 생성자 및 게터 메소드 생성

--- src/main/java/lotto/domain/TotalWinning.java
+++ src/main/java/lotto/domain/TotalWinning.java
@@ -3,17 +3,16 @@ package lotto.domain;
 import java.util.List;
 
 public class TotalWinning {
-    private final int amount;
+    private final int totalAmount;
+    private final double totalReturn;
 
-    public TotalWinning(List<Integer> ranking) {
-        int total = 0;
-        for (int rank : ranking) {
-            total += rank;
-        }
-        this.amount = total;
+    public TotalWinning(LottoPurchase lottoPurchase, WinningResult winningResult) {
+        this.totalAmount = winningResult.calculateTotalAmount();
+        this.totalReturn = (double) totalAmount / lottoPurchase.getAmount();
     }
 
-    public double getTotalReturn(LottoPurchase lottoPurchase) {
-        return ((double) this.amount / lottoPurchase.getAmount()) * 100;
+    public int getTotalAmount() {return totalAmount;}
+    public double getTotalReturn() {
+        return totalReturn;
     }
 }

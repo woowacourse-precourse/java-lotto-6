f4039cf60b68c06063bfd849b8b60c6c1c9769bc
chanhoim <cksgh201@naver.com>
2023-11-07 00:47:07 +0900

    test(lotto) 로또 번호 범위 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,9 +4,15 @@ import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static lotto.domain.lotto.LottoErrorMessage.INVALID_RANGE_ERROR;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -32,6 +38,17 @@ class LottoTest {
 
     // 아래에 추가 테스트 작성 가능
 
+    @DisplayName("로또 번호는 1에서 45사이의 값이어야 한다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidRangeNumbers")
+    void createInvalidRange(List<Integer> numbers) {
+
+        //when, then
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_RANGE_ERROR.getMessage());
+    }
+
     @Test
     void 유효_로또_번호_생성() {
         //given
@@ -54,5 +71,13 @@ class LottoTest {
 
         //then
         assertThat(lottoNumber).isEqualTo(clone);
+    }
+
+    private static Stream<Arguments> provideInvalidRangeNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(0,1,2,3,4,5)),
+                Arguments.of(List.of(41,42,43,44,45,46)),
+                Arguments.of(List.of(0,10,20,30,40,50))
+        );
     }
 }

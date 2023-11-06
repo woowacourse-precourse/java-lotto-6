06cab08242a0f742e6c41074b097a21616507138
twinson37 <twinson37@hanyang.ac.kr>
2023-11-07 00:52:11 +0900

    Feat: 당첨번호 입력 및 보너스 번호 입력 요구 출력

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -10,13 +10,27 @@ public class View {
 
     void printPurchasedLottoAmount(int number){
 
-        String askInputMoney= "개를 구매했습니다.";
+        String askInputMoney= "\n개를 구매했습니다.";
 
-        System.out.println("\n"+number+askInputMoney);
+        System.out.println(number+askInputMoney);
     }
 
     void printPurchasedLottoNumbers(Lotto lotto){
 
         System.out.println(lotto.getNumbers());
+    }
+    
+    void printAskWinningNumber(){
+        
+        String askWinningNumber = "\n당첨 번호를 입력해 주세요.";
+
+        System.out.println(askWinningNumber);
+    }
+
+    void printAskBonusNumber(){
+
+        String asKBonusNumber = "\n보너스 번호를 입력해 주세요.";
+
+        System.out.println(asKBonusNumber);
     }
 }

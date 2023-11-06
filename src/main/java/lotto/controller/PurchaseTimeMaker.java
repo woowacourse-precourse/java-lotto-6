8eb5029908d34e3c64294775fd932c22ed1de3db
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-06 17:58:35 +0900

    Feat: 로또 구매 개수를 구하는 기능 구현
    로또 구매 개수를 구하는 makePurchaseTime 메서드 구현
    해당 기능을 검사하는 기능 구현
    해당 기능의 에러 메세지 추가

---
+++ src/main/java/lotto/controller/PurchaseTimeMaker.java
@@ -0,0 +1,29 @@
+package lotto.controller;
+
+import lotto.output.ErrorPrinter;
+import lotto.validation.NumbersValidation;
+
+public class PurchaseTimeMaker {
+
+	private static final int LOTTO_PRICE = 1000;
+
+	public static int makePurchaseTime(String inputPurchaseAmount) {
+
+		int purchaseAmount = toIntPurchaseAmount(inputPurchaseAmount);
+		validatePurchaseUnit(purchaseAmount);
+
+		return purchaseAmount / LOTTO_PRICE;
+	}
+
+	private static int toIntPurchaseAmount(String inputPurchaseAmount) {
+		NumbersValidation.validateOnlyNumber(inputPurchaseAmount);
+		return Integer.parseInt(inputPurchaseAmount);
+	}
+
+	private static void validatePurchaseUnit(int purchaseAmount) {
+		if (purchaseAmount % LOTTO_PRICE != 0) {
+			ErrorPrinter.printPurchaseUnitError();
+			throw new IllegalArgumentException();
+		}
+	}
+}
--- src/main/java/lotto/output/ErrorPrinter.java
+++ src/main/java/lotto/output/ErrorPrinter.java
@@ -1,12 +1,16 @@
 package lotto.output;
 
+import lotto.constant.NumbersOption;
+
 public class ErrorPrinter {
 
 	private static final String ERROR_SYMBOL = "[ERROR]";
 	private static final String ONLY_NUMBER_MESSAGE = "숫자만 입력해주세요";
-	private static final String OUT_OF_RANGE_MESSAGE = "1~45 사이의 숫자만 입력해주세요.";
-	private static final String OUT_OF_SIZE_MESSAGE = "중복되지 않는 6개의 숫자만 입력해주세요.";
-	private static final String SEPARATOR_ERROR_MESSAGE = "구분자(,)만을 이용해 나눠주세요";
+	private static final String OUT_OF_RANGE_MESSAGE = NumbersOption.MIN + "~" + NumbersOption.MAX + "의 숫자만 입력해주세요.";
+	private static final String OUT_OF_SIZE_MESSAGE = "중복되지 않는 " + NumbersOption.SIZE + "개의 숫자만 입력해주세요.";
+	private static final String SEPARATOR_ERROR_MESSAGE = "구분자(" + NumbersOption.SEPARATOR + ")만을 이용해 나눠주세요";
+	private static final int LOTTO_PRIZE = 1000;
+	private static final String PURCHASE_UNIT_ERROR_MESSAGE = LOTTO_PRIZE + "원 단위로만 입력해주세요.";
 
 	public static void printOnlyNumberError() {
 		System.out.println(ERROR_SYMBOL + ONLY_NUMBER_MESSAGE);
@@ -22,6 +26,10 @@ public class ErrorPrinter {
 
 	public static void printSeparatorError() {
 		System.out.println(ERROR_SYMBOL + SEPARATOR_ERROR_MESSAGE);
+	}
+
+	public static void printPurchaseUnitError() {
+		System.out.println(ERROR_SYMBOL + PURCHASE_UNIT_ERROR_MESSAGE);
 	}
 
 }

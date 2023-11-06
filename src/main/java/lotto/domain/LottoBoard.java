c01dfed7bb84d821b57875895157fd746c8e5340
stopmin <geemin0@naver.com>
2023-11-07 01:51:06 +0900

    feat: 금액에 맞는 ticket 개수 return
    값 하드코딩 하지 않도록 상수 선언완료
    예외처리 구현해야함

--- src/main/java/lotto/domain/LottoBoard.java
+++ src/main/java/lotto/domain/LottoBoard.java
@@ -3,12 +3,23 @@ package lotto.domain;
 import java.util.HashMap;
 
 public class LottoBoard {
+    final static int PRICE = 1000;
     static int count = 0;
     private int tickets;
     private HashMap<Integer, Lotto> lottoBoard = new HashMap<>();
 
-    private LottoBoard(int tickets) {
-        this.tickets = tickets;
+    public LottoBoard(Integer money) {
+        validate(money);
+        this.tickets = moneyToTicket(money);
+        System.out.println(this.tickets);
+    }
+
+    private void validate(Integer money) {
+        // TODO: 금액 예외 처리
+    }
+
+    private int moneyToTicket(Integer money) {
+        return money / PRICE;
     }
 
     private void putLotto(Lotto lotto) {

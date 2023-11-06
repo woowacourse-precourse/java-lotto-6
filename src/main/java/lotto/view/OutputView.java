608bedc859e1be4b3697ea4ecaee427cf759208a
박준영 <wnsdud6969@naver.com>
2023-11-06 23:35:46 +0900

    feat(OutputView): add new features
    Add new features to OutputView
    - add number of lotto purchased message
    - print with String format
    - print number of lotto purchased

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,16 @@ package lotto.view;
 
 public class OutputView {
 
+    private static final String NUMBER_OF_LOTTO_PURCHASED_MESSAGE = "%d개를 구매했습니다.";
+
     private OutputView() {
     }
 
+    private static void printWithFormat(String message, Object... args) {
+        System.out.println(String.format(message, args));
+    }
+
+    private static void printNumberOfLottoPurchased(int numberOfLottoPurchased) {
+        printWithFormat(NUMBER_OF_LOTTO_PURCHASED_MESSAGE, numberOfLottoPurchased);
+    }
 }

952e6c40e92a63e0c76ebc62b0b10b3b40220be6
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-07 00:03:12 +0900

    feat(LottoPapers): 랜덤한 수 6개를 반환하는 기능 추가
    랜덤수 변환 메서드를 가지고있는 인터페이스 추가
    
    docs: 랜덤수 생성 기능 추가 체크 완료

--- src/main/java/lotto/Domain/LottoPapers.java
+++ src/main/java/lotto/Domain/LottoPapers.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class LottoPapers{
+public class LottoPapers implements RandomNumberGenerator{
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int LOTTO_NUMBERS = 6;
@@ -14,5 +14,10 @@ public class LottoPapers{
     public LottoPapers(int amount){
         List<Lotto> lottoPapers = new ArrayList<>();
         this.lottoPapers = lottoPapers;
+    }
+    
+    @Override
+    public List<Integer> generateRandomNumbers(){
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBERS);
     }
 }
---
+++ src/main/java/lotto/Domain/RandomNumberGenerator.java
@@ -0,0 +1,7 @@
+package lotto.Domain;
+
+import java.util.List;
+
+public interface RandomNumberGenerator {
+     List<Integer> generateRandomNumbers();
+}

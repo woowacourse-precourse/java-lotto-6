a7124a0bab4b8c58944882d238f463fa72ed8dde
gmu1026 <curiouscomponent@gmail.com>
2023-11-07 03:19:37 +0900

    feat: 로또 값 검증 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int NUMBERS_LENGTH = 6;
@@ -12,7 +14,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumbersLength(numbers);
+        validateDuplicateNumbers(numbers);
+    }
+
+    private void validateNumbersLength(List<Integer> numbers) {
         if (numbers.size() != NUMBERS_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> numbersToSet = new HashSet<>(numbers);
+        if (numbersToSet.size() != NUMBERS_LENGTH) {
             throw new IllegalArgumentException();
         }
     }

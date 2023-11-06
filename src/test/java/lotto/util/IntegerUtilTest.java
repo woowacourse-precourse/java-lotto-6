571113362192e6645f29edd8cbc60de8500e35ed
inpink <inpink@kakao.com>
2023-11-07 01:49:44 +0900

    test: 정수인지 판단하는 기능 테스트

--- src/test/java/lotto/util/IntegerUtilTest.java
+++ src/test/java/lotto/util/IntegerUtilTest.java
@@ -1,6 +1,49 @@
 package lotto.util;
 
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class IntegerUtilTest {
+
+    @Test
+    public void 정수면_true() {
+        // Given
+        String input = "2000000000";
+
+        // When
+        boolean result = IntegerUtil.isInteger(input);
+
+        // Then
+        assertTrue(result);
+    }
+
+    @Test
+    public void 정수아니면_false_정수범위초과() {
+        // Given
+        String input = "2200000000";
+
+        // When
+        boolean result = IntegerUtil.isInteger(input);
+
+        // Then
+        assertFalse(result);
+    }
+
+    @Test
+    public void 정수아니면_false_숫자아닌값() {
+        // Given
+        String input = "hi";
+
+        // When
+        boolean result = IntegerUtil.isInteger(input);
+
+        // Then
+        assertFalse(result);
+    }
+
 
 }

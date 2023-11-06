7335d1e6a361d6498499bff7b44c138c6a3565b5
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 02:14:47 +0900

    feat: add constructor to winning model

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -1,4 +1,17 @@
 package lotto.model;
 
 public enum Winning {
+
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FORTH(50000),
+    FIFTH(5000);
+
+
+    private final int money;
+
+    Winning(int money) {
+        this.money = money;
+    }
 }

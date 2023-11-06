0ac22c71858e1bb7282967ae7cfa34d33b9455c8
chanwoo yang <moomsss@naver.com>
2023-11-06 21:29:50 +0900

    feat(WinningLotto): 당첨 번호 입력값의 범위가 1~45 안에 들어오도록 구현

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -2,6 +2,8 @@ package lotto.validator;
 
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import lotto.constants.Constants;
 import lotto.utils.Utils;
@@ -42,6 +44,7 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해 주세요");
         }
         checkDuplicateWinningNumber(winningNumbers);
+        checkNumberRange(winningNumbers);
         return winningNumbers;
     }
     public void checkLastCommas(String stringNumber) throws IllegalArgumentException{
@@ -49,12 +52,17 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 맨 뒤에 쉼표를 쓰지 마세요.");
         }
     }
-    public void checkDuplicateWinningNumber(List<Integer> winningNumbers){
+    public void checkDuplicateWinningNumber(List<Integer> winningNumbers) throws IllegalArgumentException{
         long number = winningNumbers.stream().distinct().count();
         if(winningNumbers.size() != number){
             throw new IllegalArgumentException("[ERROR] 중복되는 숫자를 입력하지 마세요");
         }
 
+    }
+    public void checkNumberRange(List<Integer> winningNumbers) throws IllegalArgumentException{
+        if(Collections.max(winningNumbers) > Constants.MAX_LOTTO_NUMBER || Collections.min(winningNumbers )< Constants.ONE){
+             throw new IllegalArgumentException("[ERROR] 입력이 가능한 숫자의 범위는 1 ~ 45 입니다.");
+        }
     }
 
 

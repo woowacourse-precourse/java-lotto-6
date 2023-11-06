f27176abdd3ad37faed502f133fc2ff30b23485e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-06 15:52:28 +0900

    test(winningNumbers): 당첨번호 범위와 중복된 경우 예외 테스트

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static lotto.Exception.DUPLICATE_WINNING_NUMBER;
+import static lotto.Exception.INVALID_WINNING_NUMBER_RANGE;
 import static lotto.Exception.WINNING_NUMBERS_SIZE_SHOULD_BE_SIX;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,5 +14,17 @@ class WinningNumbersTest {
         assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4)))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(WINNING_NUMBERS_SIZE_SHOULD_BE_SIX.getMessage());
+    }
+    @Test
+    void 당첨번호에_1부터_45사이_범위에_있지_않는_경우_예외() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 55, 10)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_WINNING_NUMBER_RANGE.getMessage());
+    }
+    @Test
+    void 당첨번호_리스트에_중복된_숫자가_있는_경우_예외() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 10, 10)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATE_WINNING_NUMBER.getMessage());
     }
 }

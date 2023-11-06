5c204f7bcca5f47dd86733b0963b5a7550bf749a
Seo Young, Ha <59078585+82Hasheng@users.noreply.github.com>
2023-11-07 06:08:29 +0900

    feat: 로또 수량 및 번호 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -13,9 +13,8 @@ public class Application {
         int purchaseAmount = Integer.parseInt(Console.readLine());
         /* 추가할 것 : 입력한 구입금액이 1000원으로 나누어 떨어지지 않을 경우 예외처리한다. */
 
-        System.out.print("발행한 로또 수량 : ");
         int lottoTickets = purchaseAmount / 1000;
-        System.out.println(lottoTickets);
+        System.out.println(String.format("\n%d개를 구매했습니다.", lottoTickets));
 
         //로또 발행
         List[] allTickets = new List[lottoTickets];

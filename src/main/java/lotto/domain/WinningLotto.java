9109fc6087107d1a8d7b76d169fae6b20d55daa0
sujnkim <blackout3210@naver.com>
2023-11-06 13:17:38 +0900

    feat: 보너스 번호가 당첨 번호에 사용되었는지 validate하는 메서드 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,16 +1,26 @@
 package lotto.domain;
 
+import static lotto.exception.ErrorMessage.INVALID_BONUS_NUMBER_VALUE;
+
 public class WinningLotto {
 
     private final Lotto winningNumber;
     private final int bonusNumber;
 
     public WinningLotto(Lotto winningNumber, int bonusNumber) {
+        validateBonusNumber(winningNumber, bonusNumber);
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateBonusNumber(Lotto winningNumber, int bonusNumber){
+        if(winningNumber.isContainNumber(bonusNumber)){
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_VALUE.getMessage());
+        }
+    }
 
+
+    //TODO: 로또 1개에 대해 일치 개수 계산
 
 
 }
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -9,7 +9,8 @@ public enum ErrorMessage {
     INVALID_PAYMENT_FORM("[ERROR] 구입금액은 로또 개당 가격으로 나누어 떨어져야 합니다."),
     INVALID_WINNING_NUMBER_FORM("[ERROR] 당첨 번호는 쉼표(,)로 구분된 숫자 배열이어야 합니다."),
     WINNING_NUMBER_ENDS_WITH_DELIMITER("[ERROR] 당첨 번호는 쉼표(,)로 끝나지 않아야 합니다."),
-    INVALID_BONUS_NUMBER_FORM("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INVALID_BONUS_NUMBER_FORM("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_BONUS_NUMBER_VALUE("[ERROR] 보너스 번호는 당첨 번호에 사용되지 않은 숫자여야 합니다.");
 
     private final String message;
 

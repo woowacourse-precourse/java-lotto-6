1be6c3d9fd28612fe908c575e1f68d586a23e6b9
lkdcode <dlrlejr1@naver.com>
2023-11-07 03:10:53 +0900

    Add [UserLottoTicketGenerator] : 유저가 구매하는 로또 생성 클래스 구현

---
+++ src/main/java/lotto/module/domain/lotto/UserLottoTicketGenerator.java
@@ -0,0 +1,31 @@
+package lotto.module.domain.lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 구매한 로또 번호를 생성하는 클래스
+ */
+public class UserLottoTicketGenerator implements LottoNumberGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int SIZE = 6;
+
+    private UserLottoTicketGenerator() {
+    }
+
+    public static UserLottoTicketGenerator newInstance() {
+        return new UserLottoTicketGenerator();
+    }
+
+    @Override
+    public UserLottoTicket generateLottoNumber() {
+        final List<Integer> RANDOM_NUMBER = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, SIZE);
+        Collections.sort(RANDOM_NUMBER);
+
+        return UserLottoTicket.newInstance(RANDOM_NUMBER);
+    }
+
+}

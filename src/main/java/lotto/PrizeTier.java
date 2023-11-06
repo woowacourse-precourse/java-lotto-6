4da462e94285fdbe1613daf2298ef78e2f2ea5de
jjaegii <hn06038@gmail.com>
2023-11-07 06:21:17 +0900

    fix: enum 순서 및 이름 변경

--- src/main/java/lotto/PrizeTier.java
+++ src/main/java/lotto/PrizeTier.java
@@ -3,12 +3,12 @@ package lotto;
 import java.util.Arrays;
 
 public enum PrizeTier {
-    FIRST(6, false, 2_000_000, "1등"),
-    SECOND(5, true, 30_000_000, "2등"),
-    THIRD(5, false, 1_500_000, "3등"),
-    FOURTH(4, false, 50_000, "4등"),
-    FIFTH(3, false, 5_000, "5등"),
-    NONE(0, false, 0, "No win");
+    NONE(0, false, 0, "꽝"),
+    FIFTH(3, false, 5_000, "3개 일치"),
+    FOURTH(4, false, 50_000, "4개 일치"),
+    THIRD(5, false, 1_500_000, "5개 일치"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, false, 2_000_000_000, "6개 일치");
 
     private final int matchCount;
     private final boolean bonusMatch;

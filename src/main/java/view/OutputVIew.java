1c4d33cbde99f423a1f6ac2efdf18abd410c2207
2021110806 <2021110806@dgu.ac.kr>
2023-11-06 19:53:59 +0900

    feat(OutputView.java): add printMoneyInputRequestMessage funtion 사용자에게 구입 금액 입력을 요청하는 메시지를 출력하는 기능을 구현하였습니다.

---
+++ src/main/java/view/OutputVIew.java
@@ -0,0 +1,10 @@
+package view;
+
+public class OutputVIew {
+
+    static final String MONEY_INPUT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
+
+    void  printMoneyInputRequestMessage() {
+        System.out.println(MONEY_INPUT_REQUEST_MESSAGE);
+    }
+}

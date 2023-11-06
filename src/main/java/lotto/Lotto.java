c4d108f55e91424d923cd6cd5fe22e9c3c3b3309
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-06 23:32:17 +0900

    feat: 로또 번호 중복 검증 로직
    6개의 로또 번호 중 중복되는 번호가 있다면 IllegalArgumentException를 던진다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,29 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer num : numbers) {
+            if(!set.add(num)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }

24943eb2339403ab5d76e42ff11bddf962603995
jaegyeongkim <dongji_11@naver.com>
2023-11-06 12:18:53 +0900

    feat: 로또 발행 기능 구현

---
+++ src/main/java/model/LottoRepository.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+
+public class LottoRepository {
+    List<Lotto> lottos;
+
+    private LottoRepository(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static LottoRepository lottosOf(List<Lotto> lottos) {
+        return new LottoRepository(lottos);
+    }
+
+    public int getSize() {
+        return lottos.size();
+    }
+}
--- src/main/java/model/LottoVendor.java
+++ src/main/java/model/LottoVendor.java
@@ -1,10 +1,19 @@
 package model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import utils.Converter;
 
 public class LottoVendor {
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+    private static final int COUNT = 6;
     private static final int LOTTO_AMOUNT = 1000;
     private static final String CAN_NOT_DIVIDE = "[ERROR] 1000으로 나누어 떨어지지 않습니다.";
+
     public LottoVendor(String purchaseAmount) {
         validationDivide(Converter.stringToInt(purchaseAmount));
     }
@@ -13,5 +22,24 @@ public class LottoVendor {
         if (purchaseAmount % LOTTO_AMOUNT > 0) {
             throw new IllegalArgumentException(CAN_NOT_DIVIDE);
         }
+    }
+
+    public int divideByAmount(int purchaseAmount) {
+        return purchaseAmount / LOTTO_AMOUNT;
+    }
+
+    public LottoRepository publishLotto(String purchaseAmount) {
+        int lottoCount = divideByAmount(Converter.stringToInt(purchaseAmount));
+        return LottoRepository.lottosOf(createLottos(lottoCount));
+    }
+
+    public List<Lotto> createLottos(int lottoCount) {
+        return IntStream.range(0, lottoCount).mapToObj(i -> generateRandomLotto())
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public Lotto generateRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT);
+        return new Lotto(numbers);
     }
 }

8f1ee22b84ac4491261270be31a70d690d2f829d
yubin <dbqls2821@gmail.com>
2023-11-06 18:09:24 +0900

    feat: 요구사항에 맞는 로또 구매 금액을 계속해서 입력받는 메소드 추가

--- src/main/java/lotto/domain/LottoServer.java
+++ src/main/java/lotto/domain/LottoServer.java
@@ -1,19 +1,32 @@
 package lotto.domain;
 
+import lotto.domain.dto.Purchase;
+import lotto.domain.dto.Statistics;
 import lotto.io.InputPort;
 import lotto.io.OutputPort;
+
+import java.util.List;
 
 public final class LottoServer {
 
     private final InputPort inputPort;
     private final OutputPort outputPort;
+    private final LottoGenerator generator;
 
-    public LottoServer(InputPort inputPort, OutputPort outputPort) {
+    public LottoServer(InputPort inputPort, OutputPort outputPort, LottoGenerator generator) {
         this.inputPort = inputPort;
         this.outputPort = outputPort;
+        this.generator = generator;
     }
 
-    public void run() {
-
+    private Money receiveMoney() {
+        outputPort.printInputPurchaseAmount();
+        while (true) {
+            try {
+                return new Money(inputPort.receiveMoney());
+            } catch (IllegalArgumentException e) {
+                outputPort.printErrorMessage(e.getMessage());
+            }
+        }
     }
 }

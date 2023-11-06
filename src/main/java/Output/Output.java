958bbf3dccac516124530474834def9aafc1da52
SeungGwa123 <skwan123@naver.com>
2023-11-06 19:24:53 +0900

    Feat: Add answerNumberAsk
    당첨번호를 입력해달라고 요청하는 print문을 작성

--- src/main/java/Output/Output.java
+++ src/main/java/Output/Output.java
@@ -1,11 +1,15 @@
 package Output;
 
 public class Output {
-    public void buyMoneyAsk(){
+    public void buyMoneyAsk() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void buyNumberPaperAns(int number){
-        System.out.println(number+"개를 구매했습니다.");
+    public void buyNumberPaperAns(int number) {
+        System.out.println(number + "개를 구매했습니다.");
+    }
+
+    public void answerNumberAsk() {
+        System.out.println("당첨 번호를 입력해 주세요.");
     }
 }

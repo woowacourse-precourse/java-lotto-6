c87a03f120b5e9487334dbf0bd5bb1293220987b
shining013 <nevertheless113@naver.com>
2023-11-06 20:32:04 +0900

    refactor: generateRandomLotto 오름차순 정렬 구현

--- src/main/java/lotto/model/RandomLotto.java
+++ src/main/java/lotto/model/RandomLotto.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 public class RandomLotto {
         public List<List<Integer>> randomLotto = new ArrayList<List<Integer>>();
 
@@ -16,8 +17,9 @@ public class RandomLotto {
 
             for (int i = 0; i < quantity; i++) {
                 List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+                Collections.sort(randomNumbers);
                 randomLotto.add(randomNumbers);
             }
-        return randomLotto;
+            return randomLotto;
         }
 }

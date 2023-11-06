1a38850c556a14a4bf814fc91abbca99758664e9
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-06 19:05:38 +0900

    feat(InputView): 요구사항에 맞는 추가 출력 내용 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,12 +1,6 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.lang.reflect.WildcardType;
-import lotto.Model.Lotto;
-import lotto.Model.Price;
-import lotto.Model.PriceValidator;
-import lotto.Model.WinningLotto;
-//import lotto.Model.WinningLottoValidator;
 
 public class InputView {
 
@@ -16,8 +10,14 @@ public class InputView {
     }
 
     public static String inputWinningLotto() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
+    }
 
+    public static String inputBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
     }
 }

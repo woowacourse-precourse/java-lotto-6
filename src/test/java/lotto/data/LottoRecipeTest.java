2e674e8d4ffe124117aa458d37e4c4403b17ad6f
moong2 <www.castlehi@gmail.com>
2023-11-06 22:49:26 +0900

    test: change the example of the test

--- src/test/java/lotto/data/LottoRecipeTest.java
+++ src/test/java/lotto/data/LottoRecipeTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.model.data.LottoRecipe;
 import lotto.model.record.Money;
+import lotto.model.record.NumberOfLotto;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,14 +16,14 @@ class LottoRecipeTest {
     @CsvSource({
             "1000, 1",
             "10000, 10",
-            "2147483000", "2147483"
+            "2147483000, 2147483"
     })
     void calculateCountOfLottoTest(int money, int expectedCountOfLotto) {
         // given
         lottoRecipe = LottoRecipe.of(new Money(money));
 
         // when & then
-        assertThat(lottoRecipe.purchaseLotto()).isEqualTo(expectedCountOfLotto);
+        assertThat(lottoRecipe.purchaseLotto()).isEqualTo(new NumberOfLotto(expectedCountOfLotto));
     }
 
 }

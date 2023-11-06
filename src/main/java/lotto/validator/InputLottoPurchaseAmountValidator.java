37b087bdf4e3c7d5a1132e230383a201fcd624be
ChanUi <hcu55@naver.com>
2023-11-07 01:38:32 +0900

    fix: 로또 구입금액 입력 예외 처리 메세지 수정

--- src/main/java/lotto/validator/InputLottoPurchaseAmountValidator.java
+++ src/main/java/lotto/validator/InputLottoPurchaseAmountValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class InputLottoPurchaseAmountValidator {
     private static final String NON_INPUT_ERROR_MESSAGE = "[ERROR] 입력하지 않으셨습니다. 구입금액을 입력해주세요";
     private static final String NON_INTEGER_ERROR_MESSAGE = "[ERROR] 로또 구입금액은 숫자를 입력하셔야합니다.";
-    private static final String NON_DIVIDE_1000_ERROR_MESSAGE = "[ERROR] 로또 구입금액은 숫자를 입력하셔야합니다.";
+    private static final String NON_DIVIDE_1000_ERROR_MESSAGE = "[ERROR] 로또 구입금액은 1000원 단위입니다.";
     private static final String inputPattern = "[0-9]+";
 
     public void validateNonInputPurchaseAmount(String purchaseAmount) {

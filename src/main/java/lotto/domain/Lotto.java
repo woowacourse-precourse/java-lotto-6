1d4bee3339128e69b79668d1bf9ea5fc657ecd8d
박준영 <wnsdud6969@naver.com>
2023-11-07 01:07:09 +0900

    feat(Lotto): add new features
    Add new features to Lotto
    - add error message header
    - add error message that lotto size is invalid
    - add error message that lotto has repetition number
    - validate repetition of lotto number

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,9 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
+    private static final String ERROR_INVALID_SIZE = "로또 번호의 개수는 %d개입니다.";
+    private static final String ERROR_REPETITION = "로또 번호의 중복이 존재합니다.";
     private static final int LOTTO_SIZE = 6;
 
     private final List<LottoNumber> numbers;
@@ -17,10 +20,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+        validateLottoSize(numbers);
+        validateRepetitionOfLottoNumber(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (isInvalidSize(numbers)) {
+            throw new IllegalArgumentException(
+                ERROR_MESSAGE_HEADER + String.format(ERROR_INVALID_SIZE, LOTTO_SIZE));
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRepetitionOfLottoNumber(List<Integer> numbers) {
+        if (hasRepetition(numbers)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_HEADER + ERROR_REPETITION);
+        }
+    }
+
+    private boolean isInvalidSize(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
+
+    private boolean hasRepetition(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
 }

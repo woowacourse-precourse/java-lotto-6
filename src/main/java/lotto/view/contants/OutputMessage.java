8c6340e316becdf60c844768a2aafd9f2de07dd9
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 23:42:46 +0900

    refactor: 출력 메세지 수익률 코트 리팩토링 %s가 아닌 %.1f로 변경하여 소수점 1자리까지 출력하게 만들었습니다.

--- src/main/java/lotto/view/contants/OutputMessage.java
+++ src/main/java/lotto/view/contants/OutputMessage.java
@@ -12,7 +12,7 @@ public enum OutputMessage {
     BONUS("보너스 볼 일치"),
     AMOUNT("%s원"),
     COUNT("%s개"),
-    RATE_OF_RETURN("총 수익률은 %s%%입니다.");
+    RATE_OF_RETURN("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 

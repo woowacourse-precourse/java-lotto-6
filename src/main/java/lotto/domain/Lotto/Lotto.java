72711854bd04e02f22dc866405f095cf461317b9
seaniiio <cnc4750@naver.com>
2023-11-07 01:19:36 +0900

    feat(Lotto): 로또 숫자 중복 시 예외 발생

--- src/main/java/lotto/domain/Lotto/Lotto.java
+++ src/main/java/lotto/domain/Lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto.domain.Lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -10,6 +12,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -22,6 +25,14 @@ public class Lotto {
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    public void checkDuplicate(List<Integer> numbers) {
+        Set<Integer> numberWithoutDuplicate = new HashSet<>(numbers);
+        if (numbers.size() != numberWithoutDuplicate.size()) {
+            System.out.println("[ERROR] 로또의 숫자는 중복될 수 없습니다.");
+            throw new IllegalArgumentException();
+        }
     }
 
 }

1cb311b643896d03b67c69e0cc7ab241aeb23a44
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2023-11-07 02:11:26 +0900

    refactor: 첫번째 입력 예외처리 구현

--- src/main/java/lotto/view/inputView/LottoPurchaseInput.java
+++ src/main/java/lotto/view/inputView/LottoPurchaseInput.java
@@ -5,9 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 public class LottoPurchaseInput {
 
     public int requestPurchaseAmount() {
-        System.out.println("로또 구매 금액을 입력해주세요");
-        String input = Console.readLine();
-        return Integer.parseInt(input);
+        while (true) {
+            try {
+                System.out.println("로또 구매 금액을 입력해주세요");
+                String input = Console.readLine();
+                return validatePurchaseAmount(input);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR]올바른 숫자를 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int validatePurchaseAmount(String input) throws IllegalArgumentException {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("[ERROR]입력값이 비어있습니다. 다시 입력해주세요.");
+        }
+        int parsedAmount = Integer.parseInt(input);
+        if (parsedAmount <= 0) {
+            throw new IllegalArgumentException("[ERROR]구매 금액은 0원보다 커야 합니다. 다시 입력해주세요.");
+        }
+        if (parsedAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR]로또 구매 금액은 1000원 단위로 입력해야 합니다. 다시 입력해주세요.");
+        }
+        return parsedAmount;
     }
 }
-

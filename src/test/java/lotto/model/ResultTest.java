df3efc8f25c84d52e2e3fd738a0d92e6ec04437b
WooJJam <jaemin5548@naver.com>
2023-11-06 18:49:38 +0900

    [Test] 로또 번호와 보너스 번호 검증 테스트

--- src/test/java/lotto/model/ResultTest.java
+++ src/test/java/lotto/model/ResultTest.java
@@ -9,18 +9,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ResultTest {
 
+    List<List<Integer>> userLottoNumbers = List.of(List.of(1,2,3,4,5,6), List.of(2,3,4,5,6,7), List.of(3,4,5,6,10,12),List.of(4,5,6,7,20,30));
+
+    List<Integer> lottoWinningNumber = List.of(1,2,3,4,5,6);
+    int bonus = 7;
     @Test
     @DisplayName("사용자가 구입한 로또 번호와 정답 로또 번호를 검증한다")
-    void 로또_결과를_검증() {
-        List<List<Integer>> userLottoNumbers = List.of(List.of(1,2,3,4,5,6), List.of(2,3,4,5,6,7), List.of(3,4,5,6,10,12),List.of(4,5,6,7,20,30));
-
-        List<Integer> lottoWinningNumber = List.of(1,2,3,4,5,6);
-        int bonus = 7;
-
+    void 로또_번호와_정답_로또_번호를_검증() {
         Result result = new Result(lottoWinningNumber,userLottoNumbers,bonus);
 
-        assertEquals(result.getMatch(),List.of(6,5,4,3));
-        assertEquals(result.getHasBonus(),List.of(false,true,false,true));
+        assertEquals(result.getLottoMatch(),List.of(6,5,4,3));
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 일치하는지 검증한다.")
+    void 보너스_번호가_일치하는지_검증() {
+        Result result = new Result(lottoWinningNumber,userLottoNumbers,bonus);
+
+        assertEquals(result.getHasBonusMatch(),List.of(false,true,false,true));
     }
 
 }

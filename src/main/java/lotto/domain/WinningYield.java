d6f8808bf43c41d0e69083928c2d83873fac154e
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 16:13:35 +0900

    feat(WinningYield): 소수점 둘째 자리에서 반올림한 수익률을 반환하는 기능 구현

--- src/main/java/lotto/domain/WinningYield.java
+++ src/main/java/lotto/domain/WinningYield.java
@@ -1,2 +1,20 @@
-package lotto.domain;public class WinningYield {
+package lotto.domain;
+
+public class WinningYield {
+    private Cash cash;
+    private Profit profit;
+
+    public WinningYield(Cash cash, Profit profit) {
+        this.cash = cash;
+        this.profit = profit;
+    }
+
+    public Double getYield() {
+        Integer profitAmount = profit.getAmount();
+        Integer cashAmount = cash.getAmount();
+
+        double yield = (double) profitAmount / cashAmount * 100.0;
+
+        return Double.parseDouble(String.format("%.1f", yield));
+    }
 }

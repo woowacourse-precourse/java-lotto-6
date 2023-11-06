7596c32c20fda61cbb2084038fbc6a4d771cc0c2
prefer52 <tjsgh151@naver.com>
2023-11-06 20:37:47 +0900

    feat(UserInterface): 사용자 입력 및 문자열 출력 구현

---
+++ src/main/java/lotto/UserInterface.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInterface {
+    private String input;
+
+    public UserInterface() {
+        input = "";
+    }
+
+    // 로또 구입 금액, 당첨 번호 , 보너스 번호 입력
+    public String getUserInput() {
+        input = Console.readLine();
+        return input;
+    }
+
+    // 에러 문구, 발행한 로또 수량 및 번호, 당첨 내역, 수익률 출력
+    public void showText(String text) {
+        System.out.println(text);
+    }
+}

9875e8fa9c375711b104e3f2b973cbef913914a4
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-11-07 00:14:43 +0900

    feat: 로또 구입 기능, 로또 발행 기능 추가
    로또 구입 금액을 입력하는 기능, 금액에 맞춘 수대로 로또를 발행하는 기능, 발행된 로또 목록을 출력하는 기능을 추가함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int price = 1000;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 로또 구입 기능 입력 처리
+        PrintOutput.printNoticeMessage(NoticeMessage.BUY_MONEY);
+        int money = UserInput.getUserMoney();
+
+        // 로또 구입 수 계산
+        int lottoCount = money / price;
+
+        // 구입 수만큼 로또 발행
+        List<Lotto> lotto = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lotto.add(new Lotto(makeLottoNumbers()));
+        }
+        Lottos lottos = new Lottos(lotto);
+
+        // 구입 로또 출력 처리
+        PrintOutput.printCountOfLotto(lottoCount);
+        PrintOutput.printLottos(lottos);
+    }
+    public static List<Integer> makeLottoNumbers() {
+        return pickUniqueNumbersInRange(1, 45, 6);
     }
 }

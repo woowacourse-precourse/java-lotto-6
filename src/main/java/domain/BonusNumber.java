9b45d5684ef6c8b9bd28468b5ec5ba526984f639
ddohyeong <dohyeong7026@gmail.com>
2023-11-06 21:18:49 +0900

    feat(BonusNumber) : 당첨 로또 번호에 보너스 번호 중복 검증 기능 구현
    docs : 당첨 로또 번호에 보너스 번호 중복 검증 기능 완료 체크

--- src/main/java/domain/BonusNumber.java
+++ src/main/java/domain/BonusNumber.java
@@ -3,11 +3,11 @@ package domain;
 public class BonusNumber {
 	private final int bonusNumber;
 
-	public BonusNumber(String bonusNumber) {
+	public BonusNumber(String bonusNumber, Lotto lotto) {
 		validateIsDigit(bonusNumber);
 		int convertedBonusNumber = convertStringToInt(bonusNumber);
 		validateIsBetweenLottoRange(convertedBonusNumber);
-
+		validateWinningLottoNumberDuplicate(convertedBonusNumber, lotto);
 		this.bonusNumber = convertedBonusNumber;
 	}
 
@@ -34,6 +34,18 @@ public class BonusNumber {
 	private boolean isDigit(String bonusNumber) {
 		return bonusNumber.chars()
 				.allMatch(Character::isDigit);
+	}
+
+	public void validateWinningLottoNumberDuplicate(int bonusNumber, Lotto lotto) {
+		if (isWinningLottoNumberDuplicate(bonusNumber, lotto)) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호에 없는 번호를 입력해주세요");
+		}
+	}
+
+	private boolean isWinningLottoNumberDuplicate(int bonusNumber, Lotto lotto) {
+		return lotto.getNumbers()
+				.stream()
+				.noneMatch(number -> number == bonusNumber);
 	}
 
 }
--- src/test/java/domain/BonusNumberTest.java
+++ src/test/java/domain/BonusNumberTest.java
@@ -2,29 +2,44 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BonusNumberTest {
+	private static final Lotto winnigLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
 	@DisplayName("보너스 번호가 정수형인지 검증 아니라면 예외 발생")
 	@ParameterizedTest
 	@ValueSource(strings = {"a", "!", " ", ""})
 	public void checkBonusNumberIsDigit(String input) {
 		// when & then
-		assertThatThrownBy(() -> new BonusNumber(input))
+		assertThatThrownBy(() -> new BonusNumber(input, winnigLotto))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
-	@DisplayName("보너스 번호가 1~45 사이의 값인지 검증 아니라면 예외 발생")
+	@DisplayName("보너스 번호가 1~45 사이의 값인지 검증 아정니라면 예외 발생")
 	@Test
 	public void checkOutOfRangeNumbers() {
 		// given
 		String input = "46";
 
 		// when & then
-		assertThatThrownBy(() -> new BonusNumber(input))
+		assertThatThrownBy(() -> new BonusNumber(input, winnigLotto))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("보너스 번호가 당첨 로또 번호에 존재하면 예외 발생")
+	@Test
+	public void BonusNumberInWinningNumbers() {
+		//given
+		String input = "7";
+
+		// when & then
+		assertThatThrownBy(() -> new BonusNumber(input, winnigLotto))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 }

decca6e5b212197a825dd07283f9aff6352ab905
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-06 19:24:42 +0900

    feat(InputView) : 보너스 번호 입력 안내 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,4 +12,9 @@ public class InputView {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
+
+    public String readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }

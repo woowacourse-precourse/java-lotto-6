54bab6a0b8ae80a7612a1d650124daf89b554fbd
jdohun <wjdehgns97@naver.com>
2023-11-06 11:47:52 +0900

    Feat: 로또 당첨 통계를 내는 기능 구현 및 사용
    Lotteries.java 구현
    LottoController.java 사용

--- src/main/java/lotto/domain/model/Lotteries.java
+++ src/main/java/lotto/domain/model/Lotteries.java
@@ -1,7 +1,10 @@
 package lotto.domain.model;
 
+import lotto.constant.RankConstant;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Lotteries {
     List<Lotto> lotteries = new ArrayList<>();
@@ -20,7 +23,21 @@ public class Lotteries {
         }
     }
 
-    public void confirmWinning(WinningNumber winningNumber) {
+    public Map<String, Integer> produceStatistics(WinningNumber winningNumber, BonusNumber bonusNumber) {
+        Map<String, Integer> statics = Map.of(
+                RankConstant.FIRST, 0,
+                RankConstant.SECOND, 0,
+                RankConstant.THIRD, 0,
+                RankConstant.FOURTH, 0,
+                RankConstant.FIFTH, 0
+        );
 
+        for (Lotto lotto:lotteries) {
+            String result = lotto.calculateRank(winningNumber, bonusNumber);
+
+            if(!result.equals(RankConstant.NONE)) statics.put(result, statics.get(result) + 1);
+        }
+
+        return statics;
     }
 }

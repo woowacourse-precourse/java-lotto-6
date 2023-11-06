e84837cc6d69a668be65dd736488a6753d87c034
jhva <junghun8158@naver.com>
2023-11-06 21:47:43 +0900

    :recycle: refactor: 로또 상수 메세지 추가

--- src/main/java/lotto/constants/LottoConstants.java
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -5,4 +5,6 @@ public class LottoConstants {
     public static final int LOTTO_SIZE_MAX_LENGTH = 6;
     public static final int LOTTO_NUMBER_OVER_MAX = 45;
     public static final int LOTTO_MIN_LENGTH = 1;
+    public static final int LOTTO_DIVISION = 1000;
+    public static final int LOTTO_NOT_DIVISION = 0;
 }

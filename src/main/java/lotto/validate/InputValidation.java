eec881d249fc0138c626d01a332a8eecfbc9597d
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-07 00:19:12 +0900

    🎯feat(domain/InputValidation) : 중복 제거 후 길이 반환 함수 구현

--- src/main/java/lotto/validate/InputValidation.java
+++ src/main/java/lotto/validate/InputValidation.java
@@ -2,23 +2,16 @@ package lotto.validate;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class InputValidation {
     public static final int  THOUSAND = 1000;
     private final int  ZERO = 0;
     private final String NUMBER_REG_EXP = "^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^4{1}[0-5]{1}$";
-    private final String COMMA_REG_EXP = "^,+$";
     private final String ONLY_1_45_NUMBER = "[ERROR] 1-45 사이의 숫자만 입력하세요.";
     private final String ONLY_DIVIDED_THOUSAND = "[ERROR] 금액은 1000단위로 입력하세요.";
 
-    /* =================구입금액 validation=====================*/
-    public void validatePhurchaseAmount(String number) {
-        isNumber(number);
-        isDividedOneThousand(Integer.parseInt(number));
-        isNullOrBlank(number);
-        numberWhiteSpace(number); // boolean
-    }
-
+    /* =================보너스 금액 validation=====================*/
     // 0-45 숫자 이외의 값이 있는지
     public void isNumber(String number) {
         if (!number.matches(NUMBER_REG_EXP)) {
@@ -46,28 +39,21 @@ public class InputValidation {
     /* 당첨 번호 validation*/
     // 1-45 숫자 외 값, 공백은 공통으로 사용
 
-    // , 이외의 값
-    public boolean commaOnly(String numbers) {
-        return numbers.matches(COMMA_REG_EXP);
-    }
-    // 6자리 인지
-    public boolean isNumbersLength(List<Integer> numbers) {
-        return numbers.size() == 6;
+    // 6자리 숫자가 ,로 구분되어 있는지
+    public void isNumbersDivied6NumbersWithComma(String numbers) {
+        if (numbers.replaceAll(",", "").length() != 6) {
+            throw new IllegalArgumentException("[ERROR] ,로 구분된 6자리 숫자를 입력해 주세요.");
+        }
     }
     // 중복된 숫자 검사
     public void isDuplicate(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != numbers.size()) {
+        if (duplicateCount(numbers) < numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 중복된 번호를 입력할 수 없습니다.");
         }
     }
 
-    /* 보너스 번호 validation
-    *  당첨번호와 공통으로 사용 */
+    public int duplicateCount(List<Integer> numbers) {
+        return (int)numbers.stream().distinct().count();
+    }
 
-    // 당첨번호와의 중복 여부
-//    public void isDuplicateWinningNumber(String number) {
-//        if (lotto.contains(number)) {
-//            throw new IllegalArgumentException("[ERROR] 중복된 번호입니다.");
-//        }
-//    }
 }

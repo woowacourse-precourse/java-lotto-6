2dbd148dad6a41b9658d1c85b98d629444e69699
song-jae-wan <sjo9810@gmail.com>
2023-11-06 16:15:38 +0900

    refactor: WinningLotto 클래스 객체 생성 방식 변경

--- src/main/java/config/GameConfig.java
+++ src/main/java/config/GameConfig.java
@@ -1,11 +1,15 @@
 package config;
 
-import VO.UserLottoVO;
 import controller.GameController;
+
 import domain.WinningLotto;
+
 import service.LottoService;
+
 import view.GameView;
 import view.InputView;
+
+import VO.UserLottoVO;
 
 public class GameConfig {
 
@@ -13,6 +17,7 @@ public class GameConfig {
     private InputView inputView;
     private GameController gameController;
     private LottoService lottoService;
+    private WinningLotto winningLotto;
     private UserLottoVO userLottoVO;
     private GameView gameView;
 
@@ -20,46 +25,49 @@ public class GameConfig {
     }
 
     public static GameConfig getInstance() {
-        if(defaultGameConfig == null) {
+        if (defaultGameConfig == null) {
             defaultGameConfig = new GameConfig();
         }
         return defaultGameConfig;
     }
 
     public GameView gameView() {
-        if(gameView == null) {
+        if (gameView == null) {
             gameView = new GameView();
         }
         return gameView;
     }
 
     public UserLottoVO userLottoVO() {
-        if(userLottoVO == null) {
+        if (userLottoVO == null) {
             userLottoVO = new UserLottoVO();
         }
         return userLottoVO;
     }
 
     public WinningLotto winningLotto() {
-        return WinningLotto.getInstance();
+        if (winningLotto == null) {
+            winningLotto = new WinningLotto();
+        }
+        return winningLotto;
     }
 
     public LottoService lottoService() {
-        if(lottoService == null) {
-            lottoService = new LottoService(winningLotto(),userLottoVO());
+        if (lottoService == null) {
+            lottoService = new LottoService(winningLotto(), userLottoVO());
         }
         return lottoService;
     }
 
     public GameController gameController() {
-        if(gameController == null) {
-            gameController = new GameController(lottoService(),gameView());
+        if (gameController == null) {
+            gameController = new GameController(lottoService(), gameView());
         }
         return gameController;
     }
 
     public InputView inputView() {
-        if(inputView == null) {
+        if (inputView == null) {
             inputView = new InputView(gameController());
         }
         return inputView;
--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,24 +1,13 @@
 package domain;
 
-import config.ErrorMessage;
+import static config.ErrorMessage.*;
 
-public class WinningLotto { // 싱글톤 패턴으로 관리
+public class WinningLotto {
 
     private static final int FIVE_CORRECT = 5;
     private static final int BONUS_COUNT = 2;
-    private static WinningLotto defaultWinningLotto;
     private Lotto numbers;
     private Integer bonusNumber;
-
-    private WinningLotto() {
-    }
-
-    public static WinningLotto getInstance() {
-        if(defaultWinningLotto == null) {
-            defaultWinningLotto = new WinningLotto();
-        }
-        return defaultWinningLotto;
-    }
 
     public void setNumbers(Lotto lotto) {
         numbers = lotto;
@@ -30,14 +19,14 @@ public class WinningLotto { // 싱글톤 패턴으로 관리
     }
 
     private void validateBonusNumber(Integer bonusNumber) {
-        if(numbers.containsNumber(bonusNumber)) {
-            throw new IllegalArgumentException(ErrorMessage.BONUS.getMessage());
+        if (numbers.containsNumber(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS.getMessage());
         }
     }
 
     public int countWinNumber(Lotto lotto) {
         int count = this.numbers.compareLotto(lotto);
-        if(count == FIVE_CORRECT && lotto.containsNumber(bonusNumber)) {
+        if (count == FIVE_CORRECT && lotto.containsNumber(bonusNumber)) {
             count += BONUS_COUNT;
         }
         return count;

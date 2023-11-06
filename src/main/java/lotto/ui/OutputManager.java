87f1b0aa891b9b0bb4843ba7555b53875b5b3fd4
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 20:41:00 +0900

    fix: OutputManager 출력값 간 공백 추가

--- src/main/java/lotto/ui/OutputManager.java
+++ src/main/java/lotto/ui/OutputManager.java
@@ -9,6 +9,7 @@ import static lotto.Result.*;
 
 public class OutputManager {
     public void printLottos(List<Lotto> lottos) {
+        System.out.println();
         System.out.println(lottos.size() + "개를 구매했습니다.");
 
         lottos.stream().map(Lotto::getNumbers)
@@ -16,6 +17,8 @@ public class OutputManager {
     }
 
     public void printResults(List<Result> results) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
         printMatchAndPrize(results);
         printReturns(results);
     }
@@ -26,7 +29,7 @@ public class OutputManager {
 
         double returns = (double) totalPrize / totalAmount * 100;
 
-        System.out.println("총 수익률은 " + String.format("%.2f%%", returns) + "입니다.");
+        System.out.println("총 수익률은 " + String.format("%.1f%%", returns) + "입니다.");
     }
 
     public void printMatchAndPrize(List<Result> results) {
@@ -34,7 +37,8 @@ public class OutputManager {
         List<Result> prizes = List.of(FIFTH_PRIZE, FOURTH_PRIZE, THIRD_PRIZE, SECOND_PRIZE, FIRST_PRIZE);
 
         for (Result result : prizes) {
-            String match = result.getMatchingNumbers() + "개 일치";
+            String match = result.getMatchingNumbers() + "개 일치 ";
+            if (result==SECOND_PRIZE) match += ",  보너스 볼 일치 " ;
             String prize = "(" + String.format("%,d", result.prize()) + "원) - ";
             String matchingNum = results.stream().filter(r-> r==result).count() + "개";
 

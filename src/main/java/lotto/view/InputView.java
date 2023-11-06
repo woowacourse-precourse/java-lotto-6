95e2ac80f771d43e6b6ec7e9a538c2483c72ece2
ohjuyoungg <ejrm12@naver.com>
2023-11-07 02:59:15 +0900

    fix: 보너스 번호 입력과 당첨 번호 입력 중복 예외 처리 버그 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,38 +4,37 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.service.LottoService;
-import lotto.validation.InputValidation;
 
 public class InputView {
-
     public static int inputPurchaseAmount() {
         String input = inputNotBlank();
-        InputValidation.validateInputString(input);
+        Lotto.validateInputString(input);
         int purchaseInput = Integer.parseInt(input);
-        InputValidation.validateInputPurchaseAmount(purchaseInput);
+        Lotto.validateInputPurchaseAmount(purchaseInput);
         return LottoService.amountPurchaseDivide(purchaseInput);
     }
 
     public static Lotto inputWinningNum() {
         String input = inputNotBlank();
-        List<Integer> winNumberList = InputValidation.validateInputWinningNumber(input);
-        InputValidation.validateNoDuplicates(winNumberList);
-        InputValidation.validateNumberRange(winNumberList);
-        InputValidation.validateSixNumbers(winNumberList);
+        List<Integer> winNumberList = Lotto.validateInputWinningNumber(input); // 초기화
+        Lotto.validateNoDuplicates(winNumberList);
+        Lotto.validateNumberRange(winNumberList);
+        Lotto.validateSixNumbers(winNumberList);
         return new Lotto(winNumberList);
     }
 
     public static int inputBonusNum(Lotto winningNumbers) {
         String input = inputNotBlank();
-        InputValidation.validateInputString(input);
-        int bonusNumber = InputValidation.validateBonusNumberRange(winningNumbers, Integer.parseInt(input));
+        Lotto.validateInputString(input);
+        Lotto.validateBonusNumberRange(Integer.parseInt(input), winningNumbers);
+        int bonusNumber = Integer.parseInt(input);
 
         return bonusNumber;
     }
 
     private static String inputNotBlank() {
         String input = Console.readLine();
-        InputValidation.validateIsBlank(input);
+        Lotto.validateIsBlank(input);
         return input;
     }
 }

b945a9ac94bbf6350235b8bd7b34f2cb4549efae
SeongIl <biuld1234@gmail.com>
2023-11-06 23:04:03 +0900

    test(HandlingTest): add String 리스트를 Integer 리스트로 변환

--- src/test/java/lotto/utility/HandlingTest.java
+++ src/test/java/lotto/utility/HandlingTest.java
@@ -1,9 +1,9 @@
 package lotto.utility;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,13 +22,22 @@ class HandlingTest {
 
     @DisplayName("입력 마지막에 쉼표가 있을 때, 예외 처리")
     @Test
-    void splitLottoNumberByLastComma(){
+    void splitLottoNumberByLastComma() {
         // given
         String lotto = "1,2,3,4,5,6,";
 
         // when, then
-        assertThatThrownBy(()->Handling.split(lotto))
+        assertThatThrownBy(() -> Handling.split(lotto))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("String 리스트를 Integer 리스트로 변환")
+    @Test
+    void parseNumbers() {
+        // given
+        List<String> lotto = List.of("1", "2", "3", "4", "5", "6");
+
+        // when, then
+        assertThat(Handling.parseNumbers(lotto)).isEqualTo(List.of(1,2,3,4,5,6));
+    }
 }

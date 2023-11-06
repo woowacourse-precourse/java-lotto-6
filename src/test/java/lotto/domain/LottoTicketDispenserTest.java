93c16d3e639a309cd20864a6c98c18431dfccd91
jinsu4755 <parkjinsu4755@gmail.com>
2023-11-07 05:16:00 +0900

    test: 자동 번호 생성 로또 구매 기능 테스트
    - 로또 구매 가능 금액으로 로또 구매 테스트 작성
    
    - 로또 구매 불가 금액으로 구매시 IllegalArgumentException 발생 테스트 작성

---
+++ src/test/java/lotto/domain/LottoTicketDispenserTest.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LottoTicketDispenserTest {
+
+    private LottoTicketDispenser lottoTicketDispenser;
+
+    @BeforeEach
+    void setUp() {
+        this.lottoTicketDispenser = new LottoTicketDispenser(new AutoLottoGenerator());
+    }
+
+    @Test
+    void 로또를_구매_할_수_있는_금액으로_로또_티켓을_구매할_수_있다() {
+        // given
+        int cost = 3_000;
+        // when
+        PurchasedLottoTickets purchasedLottoTickets = lottoTicketDispenser.buyAutoCreatedTicket(cost);
+        // then
+        assertThat(purchasedLottoTickets.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 로또를_구매_할_수_없는_금액이_들어오면_예외가_발생한다() {
+        // given
+        int cost = 2_500;
+        // when
+        // then
+        assertThatThrownBy(() -> lottoTicketDispenser.buyAutoCreatedTicket(cost))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("로또 구입 금액은 %d 단위입니다.", Lotto.PRICE));
+    }
+}

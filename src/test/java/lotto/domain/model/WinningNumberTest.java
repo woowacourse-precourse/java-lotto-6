d5ac51b195fb1d64209ca8bfd5021109c9311927
jdohun <wjdehgns97@naver.com>
2023-11-06 11:18:55 +0900

    Feat: 보너스 번호가 당첨 번호에 존재하는지 검증하는 기능 테스트
    WinningNumberTest.java

--- src/test/java/lotto/domain/model/WinningNumberTest.java
+++ src/test/java/lotto/domain/model/WinningNumberTest.java
@@ -1,11 +1,17 @@
 package lotto.domain.model;
 
+import lotto.domain.validator.Validator;
 import lotto.message.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningNumberTest {
@@ -32,5 +38,26 @@ class WinningNumberTest {
         assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 65, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.ERROR_OUT_OF_RANGES.getMessage());
+    }
+
+    @DisplayName("입력된 보너스 번호가 당첨 번호에 존재하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void validateBonusNumberByDuplicateNumber(int bonusNumber) {
+        WinningNumber winningNumber = new WinningNumber(Arrays.asList(1,2,3,4,5,6));
+
+        assertThatThrownBy(() -> winningNumber.validateBonusNumber(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_CONTAIN_BONUS_NUMBER_IN_WINNING_NUMBER.getMessage());
+    }
+
+    @DisplayName("입력된 보너스 번호가 당첨 번호에 존재하지 않으면 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {7, 8, 9, 10, 11, 12})
+    void validateBonusNumberByNonDuplicateNumber(int bonusNumber) {
+        WinningNumber winningNumber = new WinningNumber(Arrays.asList(1,2,3,4,5,6));
+
+        assertThatCode(() -> winningNumber.validateBonusNumber(bonusNumber))
+                .doesNotThrowAnyException();
     }
 }

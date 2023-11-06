21790cc6f36396dda67318b28a9e7b41fc1b8186
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-06 23:47:32 +0900

    feat(InputView): 로또 구입 금액을 입력 및 검증하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,19 @@
 package lotto.controller;
 
+import lotto.domain.Money;
+import lotto.service.LottoService;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final LottoService lottoService = new LottoService();
 
     public void run() {
         outputView.printInputPurchaseAmountMessage();
+        Money money = inputView.readPurchaseAmount();
+        lottoService.buyLotto(money);
+
     }
 }

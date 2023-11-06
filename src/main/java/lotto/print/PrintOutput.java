449d09643ac6a733800e87806b7e68b27550c248
LeeJaeHong <0915jhlee@daum.net>
2023-11-06 15:39:27 +0900

    feat: 당첨 통계 및 수익률 출력
    계산된 당첨 로또와 수익률 출력하기 위함

--- src/main/java/lotto/print/PrintOutput.java
+++ src/main/java/lotto/print/PrintOutput.java
@@ -31,4 +31,14 @@ public class PrintOutput {
     public void printBonusNumberQuestion(){
         System.out.println("보너스 번호를 입력해 주세요.");
     }
+
+    public void printStats(int[] stats, double percent){
+        System.out.println("당첨 통계\n" + "---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n",stats[0]);
+        System.out.printf("4개 일치 (50,000원) - %d개\n", stats[1]);
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", stats[2]);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", stats[3]);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", stats[4]);
+        System.out.printf("총 수익률은 %.1f%입니다.\n", percent);
+    }
 }

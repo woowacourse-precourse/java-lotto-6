009a805092721be55b815fc9a71e1e743609c147
Gyeongho Yang <chchch1213@naver.com>
2023-11-06 11:02:51 +0900

    feat(domain): 입력 받은 로또 금액을 관리하는 도메인 BuyPrice Class 생성

---
+++ src/main/java/lotto/domain/BuyPrice.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.regex.Pattern;
+
+import static lotto.constant.Config.PRICE_PER_LOTTO;
+import static lotto.constant.ErrorMessage.*;
+
+public class BuyPrice {
+    private int price;
+
+    private BuyPrice(int price) {
+        validateNotNegative(price);
+        validateDividedBy(price);
+        this.price = price;
+    }
+
+    // Creating instance method for factory pattern
+    public static int from(String price) {
+        validateNumericString(price.trim());
+        return Integer.getInteger(price.trim());
+    }
+
+    private static void validateNumericString(String price) throws IllegalArgumentException {
+        if (!Pattern.compile("-?\\d+").matcher(price).matches()) {
+            throw new IllegalArgumentException(PRICE_NOT_NUMERIC);
+        }
+    }
+
+    private static void validateNotNegative(int price) throws IllegalArgumentException {
+        if (price < 0) {
+            throw new IllegalArgumentException(PRICE_NEGATIVE);
+        }
+    }
+
+    private static void validateDividedBy(int price) throws IllegalArgumentException {
+        if (price % PRICE_PER_LOTTO != 0) {
+            throw new IllegalArgumentException(PRICE_NOT_DIVIDED);
+        }
+    }
+}

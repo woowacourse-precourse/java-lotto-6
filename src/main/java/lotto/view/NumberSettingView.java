91e6aff4e3951c366dbfd0b0c390f4eef2b91214
hyerimmy <heyrim2010@naver.com>
2023-11-07 00:05:19 +0900

    refactor: 클래스명 수정 - LottoGameView.java -> NumberSettingView.java

---
+++ src/main/java/lotto/view/NumberSettingView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.constant.message.SettingMessage;
+
+public class NumberSettingView {
+    public static void printInputWinningNumbers(){
+        System.out.println(SettingMessage.INPUT_WINNING_NUMBERS);
+    }
+}

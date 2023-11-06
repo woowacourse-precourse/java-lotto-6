7523907750665aecc48798290f60361b969d8dc1
FL <refrain2k@gmail.com>
2023-11-06 17:09:34 +0900

    feat: 보너스번호를 검증하는 기능 구현

---
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -0,0 +1,28 @@
+package lotto.validator;
+
+import static lotto.validator.ErrorMessage.*;
+
+public class BonusNumberValidator {
+    private static final int MIN_BONUS_NUMBER = 1;
+    private static final int MAX_BONUS_NUMBER = 45;
+
+    public void validate(String input) {
+        isBonusNumberNumeric(input);
+        isBonusNumberInRange(input);
+    }
+
+    private void isBonusNumberInRange(String bonusNumber) {
+        int number = Integer.parseInt(bonusNumber);
+        if (number < MIN_BONUS_NUMBER || MAX_BONUS_NUMBER < number) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE);
+        }
+    }
+
+    private void isBonusNumberNumeric(String bonusNUmber) {
+        try {
+            Integer.parseInt(bonusNUmber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_IS_NOT_NUMERIC_MESSAGE);
+        }
+    }
+}

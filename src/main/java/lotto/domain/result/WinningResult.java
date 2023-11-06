687f083664d5da133140bcae8f5b1d9061c582e6
JuSeong1130 <jsmoon1130@naver.com>
2023-11-06 10:59:44 +0900

    feat(WinningResult) : equals, hascode 기능 추가

--- src/main/java/lotto/domain/result/WinningResult.java
+++ src/main/java/lotto/domain/result/WinningResult.java
@@ -3,6 +3,7 @@ package lotto.domain.result;
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
 import lotto.domain.lotto.LottoMoney;
 
 public class WinningResult {
@@ -13,6 +14,37 @@ public class WinningResult {
         this.winningResult = winningResult;
     }
 
+    public float calculateProfitRate(LottoMoney lottoMoney) {
+        float winningMoney = calculateWinningMoney();
+        return lottoMoney.calculateProfitRate(winningMoney);
+    }
+
+    private float calculateWinningMoney() {
+        float result = 0L;
+        for (WinningRank winningRank : winningResult.keySet()) {
+            if (winningResult.get(winningRank) >= 1) {
+                result += winningRank.getMoney();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        WinningResult that = (WinningResult) o;
+        return Objects.equals(winningResult, that.winningResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winningResult);
+    }
 
     //todo : 불변 테스트
     public Map<WinningRank, Integer> getResult() {

6f7bc9e2dbfe0e3384dbd868fb943eaeef1678cb
yeondori <yeonseo.jung0@gmail.com>
2023-11-07 00:38:18 +0900

    refactor: Result 추상 메서드 삭제 후 prize 필드 추가

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -4,25 +4,18 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public enum Result {
-    FIRST_PRIZE(6) {
-        public int prize() {return 2_000_000_000;}},
-    SECOND_PRIZE(5) {
-        public int prize() {return 30_000_000;}},
-    THIRD_PRIZE(5) {
-        public int prize() {return 1_500_000;}},
-    FOURTH_PRIZE(4) {
-        public int prize() {return 50_000;}},
-    FIFTH_PRIZE(3) {
-        public int prize() {return 5_000;}},
-    NO_PRIZE(2) {
-        public int prize() {return 0;}};
-
-    public abstract int prize();
-
+    FIRST_PRIZE(6, 2_000_000_000),
+    SECOND_PRIZE(5, 30_000_000),
+    THIRD_PRIZE(5, 1_500_000),
+    FOURTH_PRIZE(4, 50_000),
+    FIFTH_PRIZE(3, 5_000),
+    NO_PRIZE(2, 0);
     private final int matchingNumbers;
+    private final long prize;
 
-    Result(int matchingNumbers) {
+    Result(int matchingNumbers, long prize) {
         this.matchingNumbers = matchingNumbers;
+        this.prize =  prize;
     }
 
     public static Result from(int matchingNumbers, boolean bonus) {
@@ -36,5 +29,9 @@ public enum Result {
 
     public int getMatchingNumbers() {
         return matchingNumbers;
+    }
+
+    public long getPrize() {
+        return prize;
     }
 }

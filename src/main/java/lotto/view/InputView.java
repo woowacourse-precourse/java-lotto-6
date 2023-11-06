4c63252cbf34d20d5f70846f9c0ae7fc30aa20f9
olsohee <dlthgml0108@daum.net>
2023-11-06 10:52:08 +0900

    refactor: 메소드명, 변수명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public interface InputView {
 
-    String getPurchasePrice();
+    String askPurchasePrice();
 
-    String getWinningNumbers();
+    String askWinningNumbers();
 
-    String getBonusNumber();
+    String askBonusNumber();
 
     default void print(String printMessage) {
         System.out.println(printMessage);

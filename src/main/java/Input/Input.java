959f564cb3da629f549803bdbdedabae22a58894
SeungGwa123 <skwan123@naver.com>
2023-11-07 02:25:50 +0900

    Feat: Add answerNumber
    당첨번호를 입력하는 함수 작성
    중간에 입력값을 검사하는 부분은 앞으로 작성예정이라 변경이 생기면 수정할 예정

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -4,10 +4,15 @@ import Except.Except;
 import Output.Output;
 import lotto.BuyLotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Input {
     private static int money;
+    private static List<Integer> answer = new ArrayList<>();
+    private static int bonus = -1;
     private static Output output = new Output();
     private static Except except = new Except();
 
@@ -19,5 +24,14 @@ public class Input {
             money = except.outOfRangeMoney(money_string);
         } while (money < 0);
         return money / 1000;
+    }
+
+    public List<Integer> answerNumber() {
+        do {
+            output.answerNumberAsk();
+            String number_string = readLine();
+            answer = except.outOfRangeMoney(number_string);
+        } while (answer.size() < 1);
+        return answer;
     }
 }

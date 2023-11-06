b41b5806f2adaec8a4510f517e6a66c2f40ea995
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-11-06 16:34:24 +0900

    test : User 입력 및 초기화 테스트 코드 추가

---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 비용이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createPaymentByIndivisibleByThousand() {
+        assertThatThrownBy(() -> new User(1004))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액을 입력 시, 사용자의 금액, 구매한 로또 개수, 구매한 로또 번호들이 올바르게 초기화되었는지 확인.")
+    @Test
+    void testUserInit() {
+        int payment = 3000;
+        User user = new User(3000);
+        assertEquals(payment, user.payment());
+        assertEquals(payment / 1000, user.purchaseNumber());
+        assertNotNull(user.PurchasedLottoNumbers());
+        assertEquals(payment / 1000, user.purchasedLottoNumbersSize());
+    }
+}

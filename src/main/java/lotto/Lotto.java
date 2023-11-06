64732904cd5c5e031f3f77b72f8b9a48c0bab0ab
Zho <chlwlgh1011@naver.com>
2023-11-06 21:48:45 +0900

    feat: lotto 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers = makeLottoNumbers();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -16,5 +18,8 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> makeLottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }

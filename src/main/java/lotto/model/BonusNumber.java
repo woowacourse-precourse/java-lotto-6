5cc2157dd5fb5ed4303f24434dc5f830d28a791d
jihun <pjh2996@naver.com>
2023-11-06 16:11:33 +0900

    feat : 보너스 번호와 당첨 번호의 중복 검사 기능 구현

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static lotto.util.message.Digit.LOTTO_RANGE_END;
 import static lotto.util.message.Digit.LOTTO_RANGE_START;
+import static lotto.util.message.Error.MUST_BONUS_NO_DUP_WINNING;
 import static lotto.util.message.Error.MUST_LOTTO_RANGE;
 
 import java.util.List;
@@ -17,12 +18,19 @@ public class BonusNumber {
 
     private void validate(int bonus, List<Integer> winningNumber) {
         validateNumberRange(bonus);
+        validateWinningDuplicate(bonus, winningNumber);
     }
 
     private void validateNumberRange(int bonus) {
         if (!(LOTTO_RANGE_START.getNumber() <= bonus && bonus <= LOTTO_RANGE_END.getNumber())) {
             throw new IllegalArgumentException(
                     MUST_LOTTO_RANGE.getError(LOTTO_RANGE_START.getNumber(), LOTTO_RANGE_END.getNumber()));
+        }
+    }
+
+    private void validateWinningDuplicate(int bonus, List<Integer> winningNumber) {
+        if (winningNumber.contains(bonus)) {
+            throw new IllegalArgumentException(MUST_BONUS_NO_DUP_WINNING.getError());
         }
     }
 }
--- src/main/java/lotto/util/message/Error.java
+++ src/main/java/lotto/util/message/Error.java
@@ -6,7 +6,8 @@ public enum Error {
     MUST_MONEY_UNIT("[ERROR] 금액은 %d원 단위만 가능합니다."),
     MUST_LOTTO_SIZE("[ERROR] 로또 번호는 %d자리만 가능합니다."),
     MUST_LOTTO_NO_DUP("[ERROR] 로또 번호는 중복될 수 없습니다."),
-    MUST_LOTTO_RANGE("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.");
+    MUST_LOTTO_RANGE("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다."),
+    MUST_BONUS_NO_DUP_WINNING("[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
 
     private final String error;
 

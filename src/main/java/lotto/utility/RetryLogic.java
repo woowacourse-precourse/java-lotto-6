ad00da50f719ebf9ca92d4744b18d822047ec640
DaeYoungK <hanguk830@naver.com>
2023-11-06 23:03:55 +0900

    feat(RetryLogic): 재시도 횟수를 정해 예외 발생시 재시도 하는 기능 추가

---
+++ src/main/java/lotto/utility/RetryLogic.java
@@ -0,0 +1,21 @@
+package lotto.utility;
+
+public class RetryLogic {
+
+    private static final int MAX_RETRIES = 5;
+    private static final String TIME_OUT = "최대 시도 횟수가 초과되었습니다.";
+
+    public static void retryCount(Retry retry) {
+        for (int i = 0; i < MAX_RETRIES; i++) {
+            try {
+                retry.run();
+                break;
+            } catch (IllegalArgumentException e) {
+                if (i == MAX_RETRIES) {
+                    System.out.println(TIME_OUT);
+                    throw e;
+                }
+            }
+        }
+    }
+}

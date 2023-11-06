94009f76a4bd667345c0f2e3210d4e541c0fc7a0
Jokyo0612 <jgh010612@gmail.com>
2023-11-06 19:23:28 +0900

    feat : 구매한 로또 횟수만큼 로또를 출력하는 메소드 구현 pick_lotto를 통해 랜덤한 6자리 정수 리스트 추출 save_lotto를 통해 출력된 로또 저장 print_lotto를 통해 저장한 로또 값 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class money_for_lotto{
@@ -37,7 +39,27 @@ class money_for_lotto{
     public static int repeatTime(int realNumber){
         return realNumber/1000;
     }
+}
 
+class random_lotto{
+    static final List<List<Integer>> totalLotto = new ArrayList<>();
+
+    public static List<List<Integer>> print_lotto(int repeatTime){
+        save_lotto(repeatTime);
+        for(int i = 0; i<repeatTime; i++){
+            System.out.println(totalLotto.get(i));
+        }
+        return totalLotto;
+    }
+
+    public static void save_lotto(int repeatTime){
+        for(int i = 0; i<repeatTime; i++){
+            totalLotto.add(pick_lotto());
+        }
+    }
+    public static List<Integer> pick_lotto(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }
 
 public class Application {
@@ -47,6 +69,8 @@ public class Application {
         int realMoney = money_for_lotto.realMoney(inputMoney);
         int repeatTime = money_for_lotto.repeatTime(realMoney);
 
+        System.out.println("\n" + repeatTime + "개를 구매했습니다.");
+        List<List<Integer>> totalLotto = random_lotto.print_lotto(repeatTime);
 
     }
 }

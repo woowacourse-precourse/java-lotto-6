4893499cf1087bf659849033f59bf4eb62d376d5
ChoiseU <csw980928@gmail.com>
2023-11-06 16:42:29 +0900

    feat : 금액을 입력받고 숫자인지 확인하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.utils.LottoUtil;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoView lottoView = new LottoView();
+        LottoUtil lottoUtil = new LottoUtil();
+        boolean numberCheck = false;
+
+        lottoView.printBuyLottomMoneyMessage();
+        while (!numberCheck) {
+            String payment = lottoUtil.getUserInput();
+            numberCheck = lottoUtil.validateNumber(payment);
+        }
+
     }
 }

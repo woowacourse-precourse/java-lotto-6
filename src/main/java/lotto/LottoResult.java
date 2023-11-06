3943df3c7010e6eda389f9a938b344cfcea579c0
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-11-06 16:31:05 +0900

    refactor : 열거형 부분 수정 및 생성 메서드 구현

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,22 +1,54 @@
+package lotto;
 
 public enum LottoResult {
     
-    ZeroMatches(0, 0, "0"),
-    OneMatches(1, 0, "0"),
-    TwoMatches(2, 0 ,"0"),
-    ThreeMatches(3, 5000, "5000"),
-    FourMatches(4, 50000, "50.000"),
-    FiveMatches(5, 30000000, "30.000.000"),
-    SixMatches(6, 2000000000, "2.000.000.000");
+    ZeroMatches(0, 0, "0", false),
+    OneMatches(1, 0, "0", false),
+    TwoMatches(2, 0 ,"0", false),
+    ThreeMatches(3, 5000, "5000", false),
+    FourMatches(4, 50000, "50.000", false),
+    FiveMatches(5, 1500000, "1.500.000", false),
+    FiveBonusMatches(5, 30000000, "30.000.000", true),
+    SixMatches(6, 2000000000, "2.000.000.000", false);
 
-    private final int rank;
+    private final int match;
     private final int price;
     private final String stringPrice;
+    private final boolean matchBonus;
 
-    LottoResult(int rank, int price, String stringPrice) {
-        this.rank = rank;
+    LottoResult(int match, int price, String stringPrice, boolean matchBonus) {
+        this.match = match;
         this.price = price;
         this.stringPrice = stringPrice;
+        this.matchBonus = matchBonus;
+    }
+
+    public static LottoResult getResult(int matchCount, boolean bonusMatch) {
+        if (matchCount == 0) {
+            return ZeroMatches;
+        }
+        if (matchCount == 1) {
+            return OneMatches;
+        }
+        if (matchCount == 2) {
+            return TwoMatches;
+        }
+        if (matchCount == 3) {
+            return ThreeMatches;
+        }
+        if (matchCount == 4) {
+            return FourMatches;
+        }
+        if (matchCount == 5 && !bonusMatch) {
+            return FiveMatches;
+        }
+        if (matchCount == 5 && bonusMatch) {
+            return FiveBonusMatches;
+        }
+        if (matchCount == 6) {
+            return SixMatches;
+        }
+        throw new IllegalArgumentException("유효하지 않은 결과: " + matchCount);
     }
 
 }

1fcc8df16449152e43fbc862cc8e594afa351acf
minkyole <7719s@nave.com>
2023-11-06 18:03:27 +0900

    refactor(Lotto.java) : 예외처리함수 리펙토링
    - 예외처리함수 boolean값을 반환하는 것이 아닌 바로 throw하게 변경
    - Length 유효성검사 메소드분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Lotto {
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int MIN_LOTTO_NUMBER = 1;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,25 +14,30 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        vaildateLengthOfNumber(numbers);
+        validateDuplicatedNumber(numbers);
+        vaildateOutOfRangeNumber(numbers);
+    }
+
+    private void vaildateLengthOfNumber(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
-        if (validateDuplicatedNumber(numbers)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 가질 수 없습니다");
-        }
-        if (vaildateOutOfRangeNumber(numbers)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 지정된 범위안의 숫자만 가질 수 있습니다 범위 "
-                    + MIN_LOTTO_NUMBER + "~" + MAX_LOTTO_NUMBER);
-        }
-    }
-    private boolean validateDuplicatedNumber(List<Integer> numbers) {
-        return (numbers.stream()
-                .distinct()
-                .count() < numbers.size());
     }
 
-    private boolean vaildateOutOfRangeNumber(List<Integer> numbers) {
-      return (numbers.stream()
-                .anyMatch(num -> num < MIN_LOTTO_NUMBER || num > MAX_LOTTO_NUMBER));
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        if (numbers.stream()
+                .distinct()
+                .count() < numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 가질 수 없습니다");
+        }
+    }
+
+    private void vaildateOutOfRangeNumber(List<Integer> numbers) {
+      if (numbers.stream()
+                .anyMatch(num -> num < MIN_LOTTO_NUMBER || num > MAX_LOTTO_NUMBER)) {
+          throw new IllegalArgumentException("[ERROR] 로또 번호는 지정된 범위안의 숫자만 가질 수 있습니다 범위 "
+                  + MIN_LOTTO_NUMBER + "~" + MAX_LOTTO_NUMBER);
+      }
     }
 }

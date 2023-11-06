b13ae1f957856ec44393852119330a8ed41f64ec
mina <qkralsdk991@naver.com>
2023-11-06 17:03:11 +0900

    Test : purchaseLottery 함수 테스트

--- src/test/java/lotto/utils/LottoNumRandomGeneratorTest.java
+++ src/test/java/lotto/utils/LottoNumRandomGeneratorTest.java
@@ -1,5 +1,8 @@
 package lotto.utils;
 
+import lotto.domain.Amount;
+import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,5 +18,15 @@ public class LottoNumRandomGeneratorTest {
         for(int data:numbers){
             System.out.print(data+" ");
         }
+    }
+
+    @Test
+    public void 로또번호생성테스트(){
+        Amount amount=new Amount(10000,1000);
+        LottoNumRandomGenerator lottoNumRandomGenerator = new LottoNumRandomGenerator();
+
+        List<Lotto> result=lottoNumRandomGenerator.purchaseLottery(amount);
+
+        Assertions.assertThat(result.size()).isEqualTo(amount.getCount());
     }
 }

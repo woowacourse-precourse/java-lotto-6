4ea5c9d6761a4dd7286738ea7cb6997d7781cb20
maark1106 <mark1106@naver.com>
2023-11-06 18:37:25 +0900

    test : 로또 번호 범위가 1~45 벗어날 때 예외 처리하는 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,11 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +23,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 ~ 45 범위를 벗어나면 예외가 발생한다")
+    @Test
+    void validateCorrectRangeTest1(){
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1 ~ 45 범위를 벗어나면 예외가 발생한다")
+    @Test
+    void validateCorrectRangeTest2(){
+        assertThatThrownBy(() -> new Lotto(List.of(0,2,3,4,5,6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

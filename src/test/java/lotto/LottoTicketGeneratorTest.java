25d11b3a75d79f3a0637688f143ea2764f1e2493
nhn1030 <shgusska12@gmail.com>
2023-11-07 04:04:52 +0900

    test: LottoTicketGenerator 클래스 - generateLottoTicket 메서드가 purchaseAmount를 기반으로 동작하는 지 테스트 - generateLottoNumbers 메서드를 통해 생성된 난수가 로또 티켓별로 6개의 난수를 입력받는 지 테스트 - 불필요한 라이브러리 삭제

--- src/test/java/lotto/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/LottoTicketGeneratorTest.java
@@ -4,7 +4,6 @@ import java.util.List;
 import lotto.service.LottoTicketGenerator;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public class LottoTicketGeneratorTest {
 

0b6552edad06e263ca9b8af87631868a93363ed1
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-07 03:39:23 +0900

    feat(Constant): 상수 추가

--- src/main/java/lotto/constants/Constant.java
+++ src/main/java/lotto/constants/Constant.java
@@ -1,4 +1,20 @@
 package lotto.constants;
 
-public class Constant {
+public enum Constant {
+
+    MATCHED_SIX_PRICE(2000000000),
+    MATCHED_FIVE_ONE_BONUS_PRICE(30000000),
+    MATCHED_FIVE_PRICE(1500000),
+    MATCHED_FOUR_PRICE(50000),
+    MATCHED_THREE_PRICE(5000);
+
+    private final Integer constant;
+
+    Constant(Integer constant) {
+        this.constant = constant;
+    }
+
+    public Integer getConstant() {
+        return this.constant;
+    }
 }

3eb20232376d7a2dfc40334ff57a0f88e5053fdd
Taehyoun Kim <kth9903@gmail.com>
2023-11-06 23:27:58 +0900

    feat(lotto): implement main application logic for lotto game

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = InputHandler.getValidPurchaseAmount();
+        LottoGame game = new LottoGame(purchaseAmount);
+
+        // Print purchased lotto tickets
+        LottoTicketPrinter.printTickets(game.getLottos());
+
+        // Calculate results
+        LottoResultCalculator resultCalculator = new LottoResultCalculator();
+        Map<Integer, Integer> results = resultCalculator.calculateResults(game.getLottos(), game.getWinningNumbers(), game.getBonusNumber());
+
+        // Print results
+        LottoRateOfReturnCalculator.printResults(results);
+
+        // Calculate and print rate of return
+        double rateOfReturn = LottoRateOfReturnCalculator.calculateRateOfReturn(results, purchaseAmount);
+        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
     }
 }
+
+
+
+
+
+

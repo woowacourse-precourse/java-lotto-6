ce936a7498523b6af326800d15a54bf2d5b7624c
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-06 17:33:37 +0900

    feat(LottoController): 구입 금액 만큼의 로또를 생성한다.
    docs: LottoController 기능 목록 추가 및 체크

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.model.Lottos;
 import lotto.model.PurchaseAmount;
 import lotto.util.Parser;
 import lotto.view.InputView;
@@ -8,6 +9,8 @@ public class LottoController {
 
     public void run() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(readPurchaseAmount());
+
+        Lottos lottos = new Lottos(purchaseAmount);
     }
 
     private int readPurchaseAmount() {

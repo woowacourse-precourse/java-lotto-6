3adb7a0db1e2dccd593a09f52ccc310a2e41dda7
dmdrk1414 <seungchan141414@gmail.com>
2023-11-06 21:15:09 +0900

    [Refactor(lotto)] 상수 수정

--- src/main/java/lotto/domain/num/RanNumbers.java
+++ src/main/java/lotto/domain/num/RanNumbers.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class RanNumbers {
     // TODO: 11/6/23 상수 관리
-    private static final Integer START_INCLUSIVE = 1;
-    private static final Integer END_INCLUSIVE = 45;
-    private static final Integer TOTAL_COUNT_NUMBER = 6;
+    private static final Integer START_NUMBER = 1;
+    private static final Integer END_NUMBER = 45;
+    private static final Integer COUNT_TOTAL_NUMBER = 6;
 
     public static List<Integer> createLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, TOTAL_COUNT_NUMBER);
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, COUNT_TOTAL_NUMBER);
     }
 
 }

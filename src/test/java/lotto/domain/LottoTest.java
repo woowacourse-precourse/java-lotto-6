03178ea477d3f702159af88a57e5bc78ba2b6101
acs0209 <annjae4676@naver.com>
2023-11-06 19:53:12 +0900

    test: lotto 객체 생성 테스트 구현

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,11 @@
-package lotto;
+package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +24,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("숫자 합계 계산을 통해 Lotto 객체가 제대로 생성이 되었는지 테스트 한다.")
+    @Test
+    void testCreateLotto() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+
+        int lottoSum = lotto.getNumbers().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        int numberSum = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        assertThat(lottoSum).isEqualTo(numberSum);
+    }
 }

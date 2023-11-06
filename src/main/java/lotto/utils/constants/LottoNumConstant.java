76f85675e367ac0a29ff11be685867805e9d87fa
ohjuyoungg <ejrm12@naver.com>
2023-11-06 19:14:53 +0900

    feat: 0 enum 클래스에 상수로 추가 구현

--- src/main/java/lotto/utils/constants/LottoNumConstant.java
+++ src/main/java/lotto/utils/constants/LottoNumConstant.java
@@ -4,7 +4,8 @@ public enum LottoNumConstant {
     LOTTO_MIN_NUM(1),
     LOTTO_MAX_NUM(45),
     LOTTO_SIZE(6),
-    LOTTO_AMOUNT_PURCHASE(1000);
+    LOTTO_AMOUNT_PURCHASE(1000),
+    ZERO(0);
 
     private final int number;
 

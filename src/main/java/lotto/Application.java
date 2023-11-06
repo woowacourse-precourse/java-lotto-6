7c7202be2c96addfa7bd8be77b28468878325e38
haden <80521474+thdefn@users.noreply.github.com>
2023-11-06 14:07:19 +0900

    feat : 로또 발행 기능
    로또를 특정 횟수만큼 발급하는 기능을 구현했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static lotto.ErrorCode.PURCHASE_AMOUNT_ERROR;
 
@@ -8,14 +12,27 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입 금액을 입력해 주세요.");
-        int tryTime = 0;
-        while (tryTime == 0) {
+        int number = 0;
+        while (number == 0) {
             try {
-                tryTime = purchase();
+                number = purchase();
             } catch (RuntimeException e) {
                 handleException(e);
             }
         }
+
+        System.out.println(number + "개를 구매했습니다.");
+        issueLotto(number);
+    }
+
+    private static List<Lotto> issueLotto(int number) {
+        List<Lotto> lottoList = new ArrayList<>();
+        while (lottoList.size() < number) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottoList.add(lotto);
+            lotto.printNumbers();
+        }
+        return lottoList;
     }
 
     private static void handleException(RuntimeException e) {
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +19,19 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void printNumbers() {
+        sort();
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        numbers.forEach(number -> stringJoiner.add(number + ""));
+        System.out.println("[" + stringJoiner + "]");
+    }
+
+    private void sort() {
+        numbers.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+    }
 }

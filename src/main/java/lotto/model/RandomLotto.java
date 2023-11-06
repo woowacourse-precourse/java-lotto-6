613cd2518eff9ad7f26089d4b188edb37e3f01b5
shining013 <nevertheless113@naver.com>
2023-11-06 20:01:08 +0900

    feat: RandomLotto model class 생성

---
+++ src/main/java/lotto/model/RandomLotto.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+public class RandomLotto {
+        public List<List<Integer>> randomLotto = new ArrayList<List<Integer>>();
+
+        public RandomLotto(int quantity) {
+            this.randomLotto = generateRandomLotto(quantity);
+        }
+
+        public static List<List<Integer>> generateRandomLotto(int quantity) {
+        List<List<Integer>> randomLotto = new ArrayList<>();
+
+            for (int i = 0; i < quantity; i++) {
+                List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+                randomLotto.add(randomNumbers);
+            }
+        return randomLotto;
+        }
+}

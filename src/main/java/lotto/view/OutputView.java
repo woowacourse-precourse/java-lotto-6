63e93da1b2a33343c62922774d7bdc90bbdb5cbc
yoonjuho <dbswn990@naver.com>
2023-11-06 23:48:49 +0900

    feat(OutputView): 발행한 로또 수량 및 번호를 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,28 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+import lotto.model.LottoNumber;
+
+import java.util.List;
+
 public class OutputView {
     public void printException(String exceptionMessage){
         System.out.println(exceptionMessage);
+    }
+
+    public void printLottoTicket(List<Lotto> lottoTicket){
+        System.out.println(String.format("%d개를 구매했습니다.", lottoTicket.size()));
+        for(Lotto lotto : lottoTicket){
+            System.out.println(printLottoDetail(lotto));
+        }
+    }
+
+    private String printLottoDetail(Lotto lotto){
+        String lottoDetail = "[";
+        lottoDetail += String.join(", ", lotto.getLottoNumbers().stream()
+                .map(lottoNumber -> Integer.toString(lottoNumber.getNumber()))
+                .toList());
+        lottoDetail += "]";
+        return lottoDetail;
     }
 }

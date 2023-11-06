d319dfd5c3e0476deef7574934f8765cdaf9e4a7
mistarson <yeowuli2@naver.com>
2023-11-06 15:28:28 +0900

    Feat: 로또 구입 금액 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,15 @@
 package lotto;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
 
         int lottoPurchaseAmount = InputView.getLottoPurchaseAmount();
+        OutputView.printLottoPurchaseAmount(lottoPurchaseAmount);
+
+
 
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void printLottoPurchaseAmount(int lottoPurchaseAmount) {
+        System.out.println(lottoPurchaseAmount + "개를 구매했습니다.");
+    }
+}

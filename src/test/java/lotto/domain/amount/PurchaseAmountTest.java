8e8d1b3bb87159fde085fc67837138423733d364
gundorit <packdev937@gmail.com>
2023-11-06 18:27:11 +0900

    test (PurchaseAmount) : 로또 구매 시 구입 금액에서 1000원이 차감되는지 테스트 한다

--- src/test/java/lotto/domain/amount/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/amount/PurchaseAmountTest.java
@@ -1,20 +1,20 @@
 package lotto.domain.amount;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static lotto.util.ConstantUtils.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PurchaseAmountTest {
+    private final int INITIAL_PURCHASE_AMOUNT = 3000;
 
     private PurchaseAmount purchaseAmount;
 
     @BeforeEach
     void setup() {
-        purchaseAmount = PurchaseAmount.from(3000);
+        purchaseAmount = PurchaseAmount.from(INITIAL_PURCHASE_AMOUNT);
     }
 
     @DisplayName("금액이 1000원으로 나누어 떨어지지 않는다면 예외를 발생한다.")
@@ -29,5 +29,12 @@ class PurchaseAmountTest {
     void createAmountLessThanThousand() {
         assertThatThrownBy(() -> PurchaseAmount.from(0))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구매 시 구입 금액에서 1000원을 차감한다.")
+    @Test
+    void purchaseLottoDecreasesAmount() {
+        purchaseAmount = purchaseAmount.subtractLottoCost();
+        assertThat(purchaseAmount.getAmount()).isEqualTo(INITIAL_PURCHASE_AMOUNT - LOTTO_COST_CRITERION);
     }
 }

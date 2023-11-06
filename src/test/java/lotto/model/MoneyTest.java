a5d0ce65fdf98f9b37508ca0c293de408baa51f5
myeongju00 <j51677767@gmail.com>
2023-11-06 20:57:43 +0900

    [test] : 오류 메세지에 "[ERROR]"가 들어 있는지 추가 검증

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -5,22 +5,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
+    private static final String ERROR = "[ERROR]";
 
     @Test
     void 구입_금액이_숫자가_아닌_경우_예외_발생() {
         assertThatThrownBy(() -> new Money("가나다"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
     }
 
     @Test
     void 구입_금액이_1000원_단위가_아닌_경우_예외_발생() {
         assertThatThrownBy(() -> new Money("1500"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
     }
 
     @Test
     void 구입_금액이_0원인_경우_예외_발생() {
         assertThatThrownBy(() -> new Money("0"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
     }
 }

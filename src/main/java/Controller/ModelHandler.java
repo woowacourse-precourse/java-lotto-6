efc29ec885e0fbc9a708226834d56ee3f37c5dd4
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-07 00:04:29 +0900

    getMyLotto 구현

--- src/main/java/Controller/ModelHandler.java
+++ src/main/java/Controller/ModelHandler.java
@@ -1,11 +1,13 @@
 package Controller;
 
 import Model.Domain;
+import Model.Lotto;
 import Model.Service;
+import java.util.List;
 
 public class ModelHandler {
-    public Domain domain = Domain.getInstance();
-    public Service service = Service.getInstance();
+    private Domain domain = Domain.getInstance();
+    private Service service = Service.getInstance();
 
     private ModelHandler() {}
 
@@ -15,5 +17,9 @@ public class ModelHandler {
 
     public static ModelHandler getInstance() {
         return Singleton.INSTANCE;
+    }
+
+    public List<Lotto> getMyLotto() {
+        return domain.getMyLotto();
     }
 }

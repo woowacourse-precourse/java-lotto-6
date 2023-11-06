e9358628eb43b28715d9599e0af0de25f610c823
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-11-07 00:10:19 +0900

    feat(Ui) : 로또 구매 현황을 알려준다.

--- src/main/java/lotto/ui/Ui.java
+++ src/main/java/lotto/ui/Ui.java
@@ -1,10 +1,33 @@
 package lotto.ui;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.Lotto;
+
 public class Ui {
 
     public static final String PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String BUY_COMFIRM_MESSAGE = "%s개를 구매했습니다.";
 
     public void printPrice() {
         System.out.println(PRICE_INPUT_MESSAGE);
+    }
+
+    public void printLotto(List<Lotto> lottos) {
+        System.out.println(String.format(BUY_COMFIRM_MESSAGE, lottos.size()));
+        for (Lotto lotto : lottos) {
+            List<Integer> lottoNumbers = deepCopy(lotto.getNumbers());
+            Collections.sort(lottoNumbers);
+            System.out.println(lottoNumbers);
+        }
+    }
+
+    private <T> List<T> deepCopy(List<T> original) {
+        List<T> copy = new ArrayList<>();
+        for (T element : original) {
+            copy.add(element);
+        }
+        return copy;
     }
 }
--- src/test/java/lotto/ui/UiTest.java
+++ src/test/java/lotto/ui/UiTest.java
@@ -2,6 +2,8 @@ package lotto.ui;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
+import lotto.Lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,4 +31,15 @@ class UiTest {
         Assertions.assertThat(outputStreamCaptor.toString().trim()).isEqualTo(expectMessage);
     }
 
+    @Test
+    void 로또구매현황을_출력한다() {
+        final String expectMessage = "3개를 구매했습니다.\n[1, 2, 3, 4, 5, 6]\n[1, 2, 3, 4, 5, 7]\n[7, 11, 15, 22, 33, 44]";
+        Ui ui = new Ui();
+        ui.printLotto(List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)),
+                new Lotto(List.of(11, 22, 33, 44, 15, 7))
+        ));
+        Assertions.assertThat(outputStreamCaptor.toString().trim()).isEqualToIgnoringNewLines(expectMessage);
+    }
 }

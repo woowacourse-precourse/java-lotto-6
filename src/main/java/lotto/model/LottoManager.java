b2a84aadfb5e4e2e3cd654144c3ad3e410720d8d
Dreaming-J <kimjjong321@naver.com>
2023-11-06 19:24:58 +0900

    :sparkles: feat. 정답 로또 보관

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -8,8 +8,10 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoManager {
+    private static final String WRONG_TYPE_OF_LOTTO = "[ERROR] 당첨 로또만 올 수 있습니다.";
     private final Token token;
     private List<Lotto> lottos;
+    private AnswerLotto answerLotto;
 
     public LottoManager(int amount) {
         this.token = new Token(amount);
@@ -21,6 +23,14 @@ public class LottoManager {
             lottos.add(new Lotto(pickNumbers()));
             token.ticket();
         }
+    }
+
+    public boolean registerAnswerLotto(Lotto lotto) {
+        if (!lotto.isAnswerLotto()) {
+            return false;
+        }
+        this.answerLotto = (AnswerLotto) lotto;
+        return true;
     }
 
     @Override

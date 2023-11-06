4002fd30f7620f42fbbb55c7b8b771b24e48ac39
JiEung2 <https://github.com/JiEung2>
2023-11-06 20:52:58 +0900

    Add: Constants 메시지 추가
    당첨 번호가 숫자인지 확인하는 패턴과 숫자가 아닐 경우 에러 메시지 추가

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static final String BUY_PRICE_PATTERN_ERROR = "로또 구입 금액은 숫자를 입력해야 합니다.";
     public static final String BUY_PRICE_UNIT_ERROR = "로또 구입 금액은 1000원 단위로 입력해야 합니다.";
     public static final String WINNING_NUMBER_DUPLICATE_ERROR = "각각 다른 숫자를 입력해야 합니다.";
+    public static final String WINNING_NUMBER_ERROR = "당첨 번호는 숫자를 입력해야 합니다.";
     public static final Pattern BUY_PRICE_PATTERN = Pattern.compile("^[0-9]*$");
-
+    public static final Pattern WINNING_NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
 }

4c41971eea3a190ecfc4836fba10e07127bbe75c
hong <kevin0928@naver.com>
2023-11-06 17:22:57 +0900

    feat : "사용자가 구매한 로또 개수만큼 출력한다." 구현
    - Player의 전역변수인 ticket에 로또 개수를 할당한 뒤, OutputView에게 인자로 전달해 출력

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,15 +1,19 @@
 package lotto.domain;
 
 
+import lotto.view.OutputView;
+
 public class Player {
     private int money;
+    private int ticket;
 
     public Player(String input) {
         this.money = Integer.parseInt(input);
     }
 
     public void divideMoneyByThousand() {
-        int tickets = money / ConstantValue.THOUSAND;
-        System.out.println(tickets);
+        OutputView outputView = new OutputView();
+        ticket = money / ConstantValue.THOUSAND;
+        outputView.displayLottoNumbers(ticket);
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class OutputView {
+    public void displayLottoNumbers(int ticket) {
+        System.out.println(ticket + "개를 구매했습니다.");
+    }
+}

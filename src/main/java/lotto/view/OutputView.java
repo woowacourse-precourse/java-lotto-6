19e026286aaed98044946b5ece76f63430198830
shining013 <nevertheless113@naver.com>
2023-11-06 20:33:12 +0900

    feat: printRandomLotto method 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,21 @@
 package lotto.view;
 
 import lotto.Constants;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
     public static void printQuantity(int quantity) {
         System.out.println(quantity+Constants.QUANTITY_RESULT);
     }
+
+    public static void printRandomLotto(List<List<Integer>> randomLotto) {
+        for(List<Integer> numbers : randomLotto) {
+            String numberStr = numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+            System.out.println(numberStr);
+        }
+    }
+
 }

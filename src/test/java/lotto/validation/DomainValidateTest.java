1c20a4840ce9596ab7de2238e58225e24cba9302
gunwoong <gwj0421@gmail.com>
2023-11-06 21:36:41 +0900

    test : 주어진 보너스 번호가 정해진 개수를 채우지 못했을 경우 검증 테스트 추가

--- src/test/java/lotto/validation/DomainValidateTest.java
+++ src/test/java/lotto/validation/DomainValidateTest.java
@@ -45,4 +45,20 @@ class DomainValidateTest {
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
         assertThatCode(() -> DomainValidate.validateNumberOfLottoNumbers(lotto)).doesNotThrowAnyException();
     }
+
+    @Test
+    void testBonusNumberCnt() {
+        // given
+        List<Integer> bonus = new ArrayList<>();
+        for (int i = LOTTO_NUMBER_MIN.getNum(); i < LOTTO_NUMBER_MIN.getNum() + LOTTO_BONUS_NUMBER_CNT.getNum(); i++) {
+            bonus.add(i);
+        }
+        List<Integer> wrongBonus = new ArrayList<>(bonus);
+        wrongBonus.add(bonus.get(0));
+
+        // when & then
+        assertThatThrownBy(() -> DomainValidate.validateNumberOfBonus(wrongBonus))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
+        assertThatCode(() -> DomainValidate.validateNumberOfBonus(bonus)).doesNotThrowAnyException();
+    }
 }

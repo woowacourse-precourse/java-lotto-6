7cb1669ce3a26ad638638273d89520d2ed836dcc
yeondori <yeonseo.jung0@gmail.com>
2023-11-06 14:42:00 +0900

    feat: LottoGenerator 생성자, from 메서드

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,5 +1,18 @@
 package lotto;
 
 public class LottoGenerator {
+    private final int amount;
+
+    private LottoGenerator(int amount) {
+        this.amount = amount;
+    }
+
+    public static LottoGenerator from(int amount) {
+        if (amount%1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        return new LottoGenerator(amount);
+    }
+
 
 }

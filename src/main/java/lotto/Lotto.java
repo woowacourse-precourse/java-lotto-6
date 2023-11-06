f8b8ec95474c4deb443dfc93fd9beddf0e898b1a
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-06 18:22:02 +0900

    feat : 로또 번호에 대한 범위, 중복 예외 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,43 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static lotto.LottoDetailConstant.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateLottoNumber(numbers);
+        validateRangeLottoNumberLoop(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_STORE_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateLottoNumber(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != numbers.size())
+            throw new IllegalArgumentException();
+    }
+
+    private void validateRangeLottoNumberLoop(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateRangeLottoNumber(number);
+        }
+    }
+
+    private void validateRangeLottoNumber(int number) {
+        if (!(number >= LOTTO_START_NUMBER && number <= LOTTO_END_NUMBER))
+            throw new IllegalArgumentException();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

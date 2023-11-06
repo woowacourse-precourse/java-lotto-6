4affd1d73c551dd6a2c49cfc3476305d2d47f224
Annjieun <dkslwdms2121@naver.com>
2023-11-06 14:10:13 +0900

    feat(LottoController): 로또 게임을 진행하는 playLottoGame() 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,12 @@
 package lotto.controller;
 
 import java.util.List;
+import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.LottoPublisher;
+import lotto.domain.RankDeterminer;
+import lotto.domain.WinningNumber;
+import lotto.domain.WinningResult;
 import lotto.view.OutputView;
 
 public class LottoController {
@@ -15,6 +19,15 @@ public class LottoController {
     private void playLottoGame() {
         LottoPublisher lottoPublisher =  inputController.getPurchasePrice();
         printLottoInformation(lottoPublisher);
+
+        WinningNumber winningNumber = inputController.getWinningNumber();
+        BonusNumber bonusNumber = inputController.getBonusNumber(winningNumber.getWinningNumber());
+
+        Lotto winningLotto = winningNumber.createWinningLotto();
+        RankDeterminer lottoResult = new RankDeterminer(winningLotto, bonusNumber);
+        WinningResult winningResult = new WinningResult(lottoResult.determineAllRank(lottoPublisher.getUserLottos()), lottoPublisher.getMoney());
+
+        printWinningInformation(winningResult);
     }
 
     private void printLottoInformation(LottoPublisher lottoPublisher) {
@@ -27,5 +40,12 @@ public class LottoController {
             OutputView.printLottoNumbers(lotto.getNumbers());
         }
     }
+
+    private void printWinningInformation(WinningResult winningResult) {
+        OutputView.printWinningStatus();
+        OutputView.printWinningResult(winningResult.getWinningResult());
+        OutputView.printProfitRate(winningResult.getProfitRate());
+    }
+
 
 }

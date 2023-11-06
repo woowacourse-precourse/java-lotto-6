8e201fff337d393e845119357045e7e47713b8eb
kkkkimtaehyeon <kth1927@naver.com>
2023-11-06 13:16:23 +0900

    feat : 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OutputView {
     private static final String PURCHASED_LOTTO_AMOUNT = "개를 구매했습니다.";
+    private static final List<String> lottCountSet = List.of("3", "4", "5", "5wB", "6");
+    private static final String[] matched = {
+            "3개 일치 (5,000원) - ",
+                    "4개 일치 (50,000원) - ",
+                    "5개 일치 (1,500,000원) - ",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+                    "6개 일치 (2,000,000,000원) - "
+};
 
     public static void printPieceOfLottoAmount(int pieceOfLotto){
         System.out.print(pieceOfLotto);
@@ -14,7 +23,20 @@ public class OutputView {
         for(List<String> lotto : allLottos){
             System.out.println(lotto);
         }
-
     }
+
+    public static void printLottoStates(ArrayList<String> matchedLottoList){//모델이나 컨트롤러로 옮김
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for(String number : lottCountSet){
+            if(Collections.frequency(matchedLottoList,number) >= 0){
+                System.out.print(matched[lottCountSet.indexOf(number)]);
+                System.out.print(Collections.frequency(matchedLottoList,number));
+                System.out.println("개");
+            }
+        }
+    }
+
+
 
 }

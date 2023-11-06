d18d34c4c6325336121e802b7ee168715446842a
bbungjin <leejintwo12@naver.com>
2023-11-06 15:28:13 +0900

    getLotto 메소드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int lottoPur = Integer.parseInt(readLine());
+        int lottoNum = lottoPur / 1000;
+        // 예외처리
+
+        List<List<Integer>> lotto = new ArrayList<>();
+        getLotto(lotto, lottoNum);
+    }
+    public static List<List<Integer>> getLotto(List<List<Integer>> lotto, int lottoNum){
+        for(int i=0; i<lottoNum; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lotto.add(numbers);
+        }
+        return lotto;
     }
 }

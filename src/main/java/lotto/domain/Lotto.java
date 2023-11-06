6e755459d8ff484fb539ed1f9fe1a97d8a54471e
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-06 23:41:05 +0900

    refactor(Lotto): 로직 변경, 단순화 등 리펙터링 진행

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,9 +7,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lotto.constants.messages.Error;
 
 public class Lotto {
     private final List<Integer> numbers;
+
+    public Lotto() {
+        numbers = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0));
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -18,35 +23,30 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        try {
-            validateDuplicateNumber(numbers);
-            validateLottoSize(numbers);
-            validateNumberRange(numbers);
-            throw new IllegalArgumentException();
-        } catch (Exception e) {
-
-        }
+        validateDuplicatedNumber(numbers);
+        validateLottoSize(numbers);
+        validateNumberRange(numbers);
     }
 
     private void validateNumberRange(List<Integer> numbers) {
         List<Integer> a = numbers.stream().filter(h -> h < 0 || h > 45).toList();
         if (!a.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Error.LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
         }
     }
 
     private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Error.LOTTO_NOT_SIX_NUMBER.getMessage());
         }
     }
 
-    private void validateDuplicateNumber(List<Integer> numbers) {
+    private void validateDuplicatedNumber(List<Integer> numbers) {
         final Set<Integer> set = new HashSet<>();
 
         for (Integer yourInt : numbers) {
             if (!set.add(yourInt)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(Error.LOTTO_DUPLICATED_NUMBER.getMessage());
             }
         }
     }
@@ -61,38 +61,38 @@ public class Lotto {
         return matchedNumber;
     }
 
+    public void stamp(List<Integer> numbers) {
+        assert (numbers.isEmpty());
+        for (int i = 0; i < 6; i++) {
+            numbers.add(this.numbers.get(i));
+        }
+    }
 
-    private boolean validateToLotto(List<String> numbers) {
-        boolean isValid = true;
+
+    private void validateToLotto(List<String> numbers) {
         try {
             for (String number : numbers) {
                 Integer.parseInt(number);
             }
-            throw new IllegalArgumentException();
         } catch (Exception e) {
-            isValid = false;
+            throw new IllegalArgumentException();
 
         }
-        return isValid;
     }
 
     public Lotto toLotto(String number) {
         if (number == null) {
             throw new IllegalArgumentException();
         }
-        List<String> numbers = new ArrayList<String>(Arrays.asList(number.split(",")));
-        if (!validateToLotto(numbers)) {
-            return null;
-        }
-        Lotto result = null;
+        List<String> numbers = new ArrayList<>(Arrays.asList(number.split(",")));
+        validateToLotto(numbers);
+
         try {
-            result = new Lotto(numbers.stream()
+            return new Lotto(numbers.stream()
                     .map(Integer::valueOf)
                     .collect(Collectors.toList()));
-            throw new IllegalArgumentException();
         } catch (Exception e) {
-            result = null;
+            throw new IllegalArgumentException();
         }
-        return result;
     }
 }

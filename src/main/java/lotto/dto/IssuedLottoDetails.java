5005891f7ce544bf5cb778f157bb4a18ebfb3195
Hanjaemo <hjm2530@gmail.com>
2023-11-07 02:55:50 +0900

    refactor(IssuedLottoDetails): getLottosQuantity 반환 값 변경

--- src/main/java/lotto/dto/IssuedLottoDetails.java
+++ src/main/java/lotto/dto/IssuedLottoDetails.java
@@ -20,7 +20,7 @@ public class IssuedLottoDetails {
     }
 
     public int getLottosQuantity() {
-        return lottos.size();
+        return quantity;
     }
 
     public List<String> getLottos() {

712982d2dc6c00c3ab46b4b04242b51a819d17cc
nayeon Lee <skdus4190@gmail.com>
2023-11-07 03:34:36 +0900

    Feat: WinningNumbers & BonusNumber 객체 작성
    WinningNumbers는 당첨 번호, BonusNumber는 당첨 번호의 보너스 번호를 나타내는 객체이다

---
+++ src/main/java/domain/BonusNumber.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class BonusNumber {
+    private int bonusNumber;
+
+    public BonusNumber(int bonusNumber){
+        this.bonusNumber = bonusNumber;
+    }
+}
---
+++ src/main/java/domain/WinningNumbers.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class WinningNumbers {
+    private Lotto lotto;
+    private BonusNumber bonusNumber;
+
+    public WinningNumbers(Lotto lotto, BonusNumber bonusNumber){
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+}

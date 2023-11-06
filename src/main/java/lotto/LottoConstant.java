6e3546fff18fd06d9617025750bf84f65ad86713
dukcode <duk9741@gmail.com>
2023-11-06 15:11:47 +0900

    feat: 로또 관련 상수 추가

--- src/main/java/lotto/LottoConstant.java
+++ src/main/java/lotto/LottoConstant.java
@@ -6,5 +6,8 @@ public class LottoConstant {
     }
 
     public static final long LOTTO_TICKET_PRICE = 1000L;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_START_NUMBER = 1;
+    public static final int LOTTO_END_NUMBER = 45;
 
 }

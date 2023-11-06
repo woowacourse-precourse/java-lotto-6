11efb306e07d7aca465ce0ff6a2c8b84cce5b4ed
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 18:42:11 +0900

    feat: 구매 금액에 따른 로또 갯수 반환 기능 추가

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,5 +1,13 @@
 package lotto.domain;
 
 public class Buyer {
+    private Lottos lottos;
 
+    public Buyer(Lottos lottos) {
+        this.lottos = lottos;
+    }
+
+    public int lottoCount() {
+        return this.lottos.lottoCount();
+    }
 }
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,6 +1,15 @@
 package lotto.domain;
 
+import java.util.List;
 
 public class Lottos {
+    private final List<Lotto> lottos;
 
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public int lottoCount() {
+        return this.lottos.size();
+    }
 }

0cc8348d3fbc074e84ab534ea5e930589552610c
Hooni07 <gkxogns21@gmail.com>
2023-11-06 14:07:38 +0900

    feat: implement input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int buyLotto = Integer.parseInt(Console.readLine());
+
+
     }
 }

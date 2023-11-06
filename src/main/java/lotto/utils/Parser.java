a8e88a726b7dc35a2cb720aba0b17ed3bb962cb1
sinminseok12 <sinminseok12@naver.com>
2023-11-06 19:53:26 +0900

    feat(InputView): 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,8 +1,25 @@
 package lotto.utils;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
+    public static final String COMMA = ",";
+    public static final String COMMA_WITH_SPACES_REGEX = ",\\s+";
 
     public static Integer parseToInteger(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> parseToIntegers(String input) {
+        String strings = removeSpacesBetweenCommas(input);
+        List<String> numbers = List.of(strings.split(COMMA));
+        return numbers.stream()
+                .map(Parser::parseToInteger)
+                .collect(Collectors.toList());
+    }
+
+    public static String removeSpacesBetweenCommas(String input) {
+        return input.replaceAll(COMMA_WITH_SPACES_REGEX, COMMA);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,15 +2,26 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 import static lotto.utils.Parser.parseToInteger;
+import static lotto.utils.Parser.parseToIntegers;
 
 public class InputView {
     private static final String INPUT_BUY_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static Integer inputPurchaseAmount() {
         System.out.println(INPUT_BUY_MONEY_MESSAGE);
         String input = Console.readLine();
         //todo 구입 금액 검증
         return parseToInteger(input);
+    }
+
+    public static List<Integer> inputWinNumbers() {
+        System.out.println(INPUT_WIN_NUMBERS_MESSAGE);
+        String input = Console.readLine();
+        //todo 당첨 번호 검증 기능
+        return parseToIntegers(input);
     }
 }

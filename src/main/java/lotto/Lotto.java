af101e9db47f1454a432d4c3622bbd032b3ac2df
hoyun06 <hoyun06@gmail.com>
2023-11-06 13:36:20 +0900

    feat: Lotto 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,57 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static lotto.ErrorMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_LOTTO_SIZE.getMessage());
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        List<Integer> duplicateRemoved = numbers.stream()
+                .distinct()
+                .toList();
+        if (duplicateRemoved.size() != 6) {
+            throw new IllegalArgumentException(INVALID_LOTTO_INPUT.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+    public void print() {
+        System.out.print("[");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(numbers.get(i) + ", ");
+        }
+        System.out.println(numbers.get(5) + "]");
+    }
+
+    public int calculateNumberOfMatches(List<Integer> comparisonTarget) {
+        return numbers.stream()
+                .filter(comparisonTarget::contains)
+                .toList()
+                .size();
+    }
+
+    public boolean containsBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
 }

b1ba57eddd370836327bbb3e9b000184b9cc8cd5
koomchang <leahpar0401@gmail.com>
2023-11-06 19:51:26 +0900

    feat: 로또 검증의 구체적인 예외 메시지 출력

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -5,6 +5,10 @@ import lotto.model.LottoNumber;
 
 public class LottoValidator {
 
+    private static final String NUMBER_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 로또 번호는 1과 45 사이여야 합니다.";
+    private static final String NUMBER_NOT_SIX_NUMBERS = "[ERROR] 로또 번호는 6자리가 아닙니다.";
+    private static final String NUMBER_DUPLICATED = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
+
     public static void validate(List<Integer> numbers) {
         validateNumbersSize(numbers);
         validateDuplicatedNumber(numbers);
@@ -19,19 +23,19 @@ public class LottoValidator {
 
     private static void validateNumberRange(Integer number) {
         if (!LottoNumber.isInRange(number)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_NOT_BETWEEN_ONE_AND_FORTY_FIVE);
         }
     }
 
     private static void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != LottoNumber.COUNT.getNumber()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_NOT_SIX_NUMBERS);
         }
     }
 
     private static void validateDuplicatedNumber(List<Integer> numbers) {
         if (numbers.stream().distinct().count() != LottoNumber.COUNT.getNumber()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBER_DUPLICATED);
         }
     }
 }

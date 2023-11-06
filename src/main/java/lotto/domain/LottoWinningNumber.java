bf1d636cf4127e7918dd9e833696f0f4cfe29e7a
myhomeTop <epqlfepzk@naver.com>
2023-11-07 00:16:19 +0900

    fix: LottoWinningNumber 클래스의 함수명 변경

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -9,17 +9,17 @@ public class LottoWinningNumber {
     private final Lotto readWinningNumber;
 
     public LottoWinningNumber(String readWinningNumber) {
-        validateNumber(readWinningNumber);
-        this.readWinningNumber = lottoParsing(readWinningNumber);
+        validateNumberAndCommaFormat(readWinningNumber);
+        this.readWinningNumber = convertStringToLottoFormat(readWinningNumber);
     }
 
-    private void validateNumber(String readWinningNumber) {
+    private void validateNumberAndCommaFormat(String readWinningNumber) {
         if (!readWinningNumber.matches("([0-9]+,)*[0-9]+")) {
             throw new IllegalArgumentException("당첨 번호는 숫자와 콤마 형식이어야 합니다.");
         }
     }
 
-    private Lotto lottoParsing(String readWinningNumber) {
+    private Lotto convertStringToLottoFormat(String readWinningNumber) {
         List<Integer> lottoNumber = Arrays.stream(readWinningNumber.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)

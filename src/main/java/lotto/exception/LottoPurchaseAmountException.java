27938f2ac82ab2777cecbd40b8a2dcab68da0ae9
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 01:18:52 +0900

    feat(LottoPurchaseAmountException) : isDivideThousand() 구현 - 구입 금액이 1,000으로 나누어 떨어지지 않을 때 예외 처리 기능 구현

--- src/main/java/lotto/exception/LottoPurchaseAmountException.java
+++ src/main/java/lotto/exception/LottoPurchaseAmountException.java
@@ -1,13 +1,21 @@
 package lotto.exception;
 
+import lotto.util.Converter;
+
 public class LottoPurchaseAmountException {
+    private static final String CAN_DIVIDE_THOUSAND_MESSAGE = "구매 금액은 1,000으로 나누어 떨어져야 합니다.";
+    private Converter converter = new Converter();
 
-    public void isDivideThousand() {
+    public void isDivideThousand(String input) {
+        int purchaseAmount = converter.stringToInteger(input);
+        if(purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(CAN_DIVIDE_THOUSAND_MESSAGE);
+        }
     }
 
-    public void isNumber() {
+    public void isNumber(String input) {
     }
 
-    public void isBlank() {
+    public void isBlank(String input) {
     }
 }

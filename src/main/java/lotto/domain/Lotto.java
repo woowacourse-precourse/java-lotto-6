6aa7842072eb03e3f664843d1c1cecb36eecbf5b
edgar6bf <edgar6bf@gmail.com>
2023-11-06 10:55:07 +0900

    refactor: Lotto 객체 검증 조건 추가
    Lotto 객체의 생성 인자로 중복된 정수 리스트가 입력될 시 예외가 발생하는 검증 조건을 추가하였다.
    정수 리스트의 크기를 검사하는 로직에 사용되던 정수 6을 매직넘버를 피하기 위해 상수화하였다.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+
+    private static final int ALLOWED_NUMBER_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (!isAllowedNumberSize(numbers) || !isUniqueNumbers(numbers)) {
+            throw new IllegalArgumentException("유효하지 않은 로또 번호입니다.");
+        }
+    }
+
+    private boolean isAllowedNumberSize(List<Integer> numbers) {
+        return numbers.size() == ALLOWED_NUMBER_SIZE;
+    }
+
+    private boolean isUniqueNumbers(List<Integer> numbers) {
+        return Set.copyOf(numbers)
+                .size() == ALLOWED_NUMBER_SIZE;
+    }
+}

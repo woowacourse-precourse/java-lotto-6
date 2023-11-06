f95913e9cfb69601adc31b7645141acb8ed7fe23
dltjdn <hana40004@naver.com>
2023-11-07 04:51:46 +0900

    feat(LottoResultCalculator): 당첨 결과 추가 기능
    * 없는 WinningInfo라면 추가
    * 이미 존재하는 WinningInfo라면 count를 증가

--- src/main/java/lotto/model/LottoResultCounter.java
+++ src/main/java/lotto/model/LottoResultCounter.java
@@ -4,10 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LottoResultCounter {
-    private final Map<WinningInfo,Integer> resultCount;
+    private static final int ONE_COUNT = 1;
+    private final Map<WinningInfo, Integer> resultCount;
 
-    public LottoResultCounter(){
+    public LottoResultCounter() {
         this.resultCount = new HashMap<>();
+    }
+
+
+    public void addResult(WinningInfo winningInfo) {
+        if (resultCount.containsKey(winningInfo)) {
+            plusOneCount(winningInfo);
+            return;
+        }
+        resultCount.put(winningInfo, ONE_COUNT);
+    }
+
+    private void plusOneCount(WinningInfo winningInfo) {
+        Integer count = resultCount.get(winningInfo);
+        resultCount.put(winningInfo, count + ONE_COUNT);
     }
 
 }

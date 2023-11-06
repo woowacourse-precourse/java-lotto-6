5ca19e2d94f26af2d2926ced35df446e83ee7239
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-06 19:04:25 +0900

    feat: 출력 기능 구현
    OutputView, ConsoleOutputView 를 통해 콘솔 출력을 구현함
    
    - OutputView 를 통해 다형성을 확보함
    - 값을 입력 받을 때, 사용자에게 보여질 문자열을 출력하는 기능 구현
    - 게임 결과 및 게임이 진행된 다음 출력하는 기능은, 게임 구현 구 구현할 예정

---
+++ src/main/java/lotto/io/output/ConsoleOutputView.java
@@ -0,0 +1,39 @@
+package lotto.io.output;
+
+import static lotto.utils.constant.IOConstant.GET_BONUS_NUMBER;
+import static lotto.utils.constant.IOConstant.GET_LOTTO_MONEY;
+import static lotto.utils.constant.IOConstant.GET_WINNING_NUMBERS;
+
+public class ConsoleOutputView implements OutputView {
+
+
+    @Override
+    public void printGetLottoMoney() {
+        System.out.println(GET_LOTTO_MONEY.getValue());
+    }
+
+    @Override
+    public void printGetWinningNumbers() {
+        System.out.println(GET_WINNING_NUMBERS.getValue());
+    }
+
+    @Override
+    public void printGetBonusNumber() {
+        System.out.println(GET_BONUS_NUMBER.getValue());
+    }
+
+    @Override
+    public void printResult() {
+        // todo 결과 출력 형식 결정시 구현
+    }
+
+    @Override
+    public void printStatistics() {
+        // todo 통계 출력 형식 결정시 구현
+    }
+
+    @Override
+    public void printProfitRate() {
+        // todo 수익률 출력 형식 결정시 구현
+    }
+}
---
+++ src/main/java/lotto/io/output/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.io.output;
+
+public interface OutputView {
+
+    void printGetLottoMoney();
+    void printGetWinningNumbers();
+    void printGetBonusNumber();
+    void printResult();
+    void printStatistics();
+    void printProfitRate();
+}
+
+

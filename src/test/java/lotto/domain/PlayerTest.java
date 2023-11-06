cee2b0e5d9eae1af973ab4fadee942a323096fda
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-11-06 16:34:50 +0900

    test: 플레이어 테스트 코드 작성

--- src/test/java/lotto/domain/PlayerTest.java
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -1,4 +1,24 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class PlayerTest {
+
+    @DisplayName("로또 구입 금액이 1,000원 보다 작으면 예외가 발생한다.")
+    @Test
+    void createPlayerByUnderMinimumPurchaseAmount() {
+        assertThatThrownBy(() -> new Player(999))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액이 1,000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void createPlayerByNotDivide1000() {
+        assertThatThrownBy(() -> new Player(10100))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

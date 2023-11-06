ae7cbc92cbde28d045c6c717c836cb98c0d995e6
EHCTH <lees90896@gmail.com>
2023-11-06 14:13:31 +0900

    사용자값 당첨 갯수, 합계, 로또를 살 금액으로 나눴음

--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -1,2 +1,50 @@
-package lotto.model;public class Player {
+package lotto.model;
+
+import lotto.view.OutputView;
+
+public class Player {
+    private long total = 0;
+    private final int[] matchCount = new int[5];
+    private int price;
+    private int bonusNumber;
+    private int count;
+
+    public void updatePrice(int price) {
+        System.out.println(price);
+        this.price = price;
+        updateCount(price);
+    }
+    public void updateCount(int price) {
+        this.count = price / 1000;
+    }
+
+
+
+    public void updateMatchCountAndTotal(EnumRanking.Ranking ranking) {
+        if (ranking != EnumRanking.Ranking.LAST) {
+            long rankingPrice = EnumRanking.Ranking.getPrice(ranking);
+            this.matchCount[ranking.ordinal()] += 1;
+            this.total += rankingPrice;
+        }
+    }
+    public void updateBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+    public int getCount() {return this.count;}
+
+    public int getPrice() {
+        return price;
+    }
+    public int getBonusNumber() {
+        return this.bonusNumber;
+    }
+
+    public long getTotal() {
+        return this.total;
+    }
+
+    public int getMatchNum(int cnt) {
+        return this.matchCount[cnt];
+    }
+
 }

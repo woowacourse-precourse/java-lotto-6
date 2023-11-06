5e4970a87789309ab127097104e81c70a4606c20
박준영 <wnsdud6969@naver.com>
2023-11-07 00:18:38 +0900

    feat(LottoNumber): add new features
    Add new features to LottoNumber
    - add error message header
    - add error message that number is less than minimum of lotto number
    - add error message that number is bigger than maximum of lotto number
    - add minimum number of lotto number
    - add maximum number of lotto number
    - validate lotto number range

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,9 +2,46 @@ package lotto.domain;
 
 public class LottoNumber {
 
+    private static final String ERROR_MESSAGE_HEADER = "[ERROR] ";
+    private static final String ERROR_LESS_THAN_MINIMUM_NUMBER = "로또 번호의 최솟값은 %d입니다.";
+    private static final String ERROR_BIGGER_THAN_MAXIMUM_NUMBER = "로또 번호의 최댓값은 %d입니다.";
+
+    private static final int MINIMUM_NUMBER_OF_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER_OF_LOTTO_NUMBER = 45;
+
     private int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
+        validateLottoNumberRange(lottoNumber);
         this.lottoNumber = lottoNumber;
+    }
+
+    private void validateLottoNumberRange(int lottoNumber) {
+        validateBiggerThanMinimumNumberOfRange(lottoNumber);
+        validateLessThanMaximumNumberOfRange(lottoNumber);
+    }
+
+    private void validateBiggerThanMinimumNumberOfRange(int number) {
+        if (isLessThanMinimumNumber(number)) {
+            throw new IllegalArgumentException(
+                ERROR_MESSAGE_HEADER + String.format(ERROR_LESS_THAN_MINIMUM_NUMBER,
+                    MINIMUM_NUMBER_OF_LOTTO_NUMBER));
+        }
+    }
+
+    private void validateLessThanMaximumNumberOfRange(int number) {
+        if (isBiggerThanMaximumNumber(number)) {
+            throw new IllegalArgumentException(
+                ERROR_MESSAGE_HEADER + String.format(ERROR_BIGGER_THAN_MAXIMUM_NUMBER,
+                    MAXIMUM_NUMBER_OF_LOTTO_NUMBER));
+        }
+    }
+
+    private boolean isLessThanMinimumNumber(int number) {
+        return number < MINIMUM_NUMBER_OF_LOTTO_NUMBER;
+    }
+
+    private boolean isBiggerThanMaximumNumber(int number) {
+        return number > MAXIMUM_NUMBER_OF_LOTTO_NUMBER;
     }
 }

fa57a32aaf723f06391960370b8340fc5fd0a2d9
ldw <leehan02@hanyang.ac.kr>
2023-11-06 11:34:48 +0900

    fix(lotto): 당첨 번호와 일치하는 숫자가 몇 개인지 계산하는 기능과 계산 결과를 집계하는 기능 분리

--- src/main/java/lotto/model/Aggregator.java
+++ src/main/java/lotto/model/Aggregator.java
@@ -1,15 +1,35 @@
 package lotto.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Aggregator {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
 
-    public List<Integer> result(List<Lotto> lottos, List<Integer> winningNumbers) {
+    public Aggregator(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> result(List<Lotto> lottos) {
         return lottos.stream()
-                .map(lotto -> (int) lotto.getNumbers()
-                        .stream()
-                        .filter(winningNumbers::contains)
-                        .count())
+                .map(this::countMatch)
                 .toList();
+    }
+
+    private int countMatch(Lotto lotto) {
+        int count = (int) lotto.getNumbers()
+                .stream()
+                .filter(winningNumbers::contains)
+                .count();
+        if (count == 5 && isSecondRank(lotto))
+            return Rule.SECOND_RANK.value();
+        return count;
+    }
+
+    private boolean isSecondRank(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
     }
 }
--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -1,6 +1,5 @@
 package lotto.model;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -19,6 +18,8 @@ public class WinningResult {
     public Map<Integer, Long> getWinningResult() {
         return winningResult;
     }
+
+
 
     /**
      * 당첨 통계

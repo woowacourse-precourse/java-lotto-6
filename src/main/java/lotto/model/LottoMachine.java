eee66b2c8b2125ab6697dd5ee27d2aa9358e41f9
권장순 <jsoonworld@gmail.com>
2023-11-06 21:06:09 +0900

    feat(LottoValidationUtils#checkValidBonusNumber) : 보너스 번호의 검증을 담당하는 메서드 구현.

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoMachine {
-
+    // TODO : enum 으로 변경해서 중복 제거.
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
--- src/main/java/lotto/util/LottoValidationUtils.java
+++ src/main/java/lotto/util/LottoValidationUtils.java
@@ -1,4 +1,20 @@
 package lotto.util;
 
+import java.util.List;
+
 public class LottoValidationUtils {
+
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+
+    public static void checkValidBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+    
 }

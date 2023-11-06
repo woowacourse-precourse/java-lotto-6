b6f1fa8823c48182a1b38e20f7553b84426698e9
jongmee <ra123221@gmail.com>
2023-11-06 22:25:41 +0900

    test(BonusNumberTest): 보너스 번호가 로또 번호에 포함되었는지 확인하는 메서드 기능 테스트하기

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -5,6 +5,8 @@ import static lotto.constants.ErrorMessage.INVALID_PURCHASE_AMOUNT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,5 +19,18 @@ class BonusNumberTest {
             new BonusNumber(input);
         }).isInstanceOf(IllegalArgumentException.class)
             .hasMessage(INVALID_LOTTO_NUMBER_RANGE);
+    }
+
+    @Test
+    void 보너스번호가_로또번호에_포함되었는지_확인한다() {
+        // given
+        BonusNumber 보너스번호 = new BonusNumber(3);
+        Lotto 로또 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        boolean 포함여부 = 보너스번호.contained(로또);
+
+        // then
+        assertEquals(true, 포함여부);
     }
 }

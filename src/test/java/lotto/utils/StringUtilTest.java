dbeb951011351f75a255870a27f68aae37d21f2f
elyudwo <dudqk9696@naver.com>
2023-11-06 17:35:39 +0900

    [Test] : 총 수익률 문자열 변환 테스트 구현

--- src/test/java/lotto/utils/StringUtilTest.java
+++ src/test/java/lotto/utils/StringUtilTest.java
@@ -1,11 +1,13 @@
 package lotto.utils;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 class StringUtilTest {
 
+    @DisplayName("구입 금액 문자열을 int 값으로 변환한다.")
     @Test
     void stringToInt() {
         // given
@@ -16,5 +18,18 @@ class StringUtilTest {
 
         // then
         Assertions.assertThat(purchaseAmount).isEqualTo(5000);
+    }
+
+    @DisplayName("총 수익률 double 값을 문자열 형식으로 변환한다.")
+    @Test
+    void doubleToString() {
+        // given
+        double rateOfReturn = 62.5;
+
+        // when
+        String printReturnValue = StringUtil.doubleToString(rateOfReturn);
+
+        // then
+        Assertions.assertThat(printReturnValue).isEqualTo("총 수익률은 62.5%입니다.");
     }
 }

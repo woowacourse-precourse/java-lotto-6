26723b6611d780c670a222f1c50574d8ef009b8b
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-07 06:20:02 +0900

    feat(service.LottoService): 기능8. 지정된 개수만큼 로또를 발행하고 List 형태로 반환한다
    generateLottoList 메서드는 지정된 개수만큼 Lotto를 발행한 후에 List 형태로 반환하는 메서드 입니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,9 +4,20 @@ import lotto.domain.Lotto;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
+
+    public List<Lotto> generateLottoList(int count) {
+        List<Lotto> result = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            result.add(generateLotto());
+        }
+
+        return result;
+    }
 
     public Lotto generateLotto() {
         List<Integer> list = generateLottoCombination();

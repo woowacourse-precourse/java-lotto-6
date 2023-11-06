10b958e1a21383940051f251652ddd617256e092
rbgusdlza <rbgusdlza@gmail.com>
2023-11-06 22:50:10 +0900

    feat(Application): main 메서드 구현

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,21 +1,25 @@
 package lotto.controller;
 
 import static lotto.controller.UserInputMessage.*;
+import static lotto.util.ThousandSeparator.addThousandsSeparator;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoController {
     private static final int LOTTO_PRICE = 1000;
+    private final String SEPARATED_LOTTO_PRICE = addThousandsSeparator(LOTTO_PRICE);
+
 
     public LottoController() {
     }
 
     public void start() {
-
+        requestLottoPurchaseAmount();
     }
 
-    public void requestLottoPurchaseAmount() {
-        System.out.printf(REQUEST_LOTTO_PURCHASE_AMOUNT, LOTTO_PRICE);
-
+    public int requestLottoPurchaseAmount() {
+        System.out.printf(REQUEST_LOTTO_PURCHASE_AMOUNT, SEPARATED_LOTTO_PRICE);
+        String userInputAmount = Console.readLine();
+        
     }
 }
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class ErrorMessage {
-    public static final String INVALID_AMOUNT_FORMAT_ERROR = "[ERROR] 구입 금액은 %d원 단위로 이루어져야 합니다.";
+    public static final String INVALID_AMOUNT_FORMAT_ERROR = "[ERROR] 구입 금액은 %s원 단위로 이루어져야 합니다.";
     public static final String INVALID_COUNT_OF_WINNING_NUMBERS_ERROR = "[ERROR] 당첨 번호의 개수는 정확히 6개이어야 합니다.";
     public static final String INVALID_FORMAT_ERROR = "[ERROR] 잘못된 형식의 입력입니다.";
     public static final String NUMBER_OUT_OF_RANGE_ERROR = "[ERROR] 번호는 1이상 45이하의 수이어야 합니다.";

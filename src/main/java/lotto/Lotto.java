d1840505c66fbd5d8ccab0c6e4b4c6ddbc686dcb
choji <waf268@gmail.com>
2023-11-06 17:16:25 +0900

    feat : Lotto 클래스와 테스트 코드 작성
    Lotto 클래스의 생성자에서 메서드를 호출하여 예외 검증
    로또번호를 출력하는 메서드 구현
    Lotto 클래스의 public 메서드에 대해 테스트코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumber(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +18,28 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void checkDuplicate(List<Integer> numbers) {
+        Set<Integer> checkSet = new HashSet<>();
+        for (Integer number : numbers) {
+            if (checkSet.contains(number)) {
+                throw new IllegalArgumentException();
+            }
+            checkSet.add(number);
+        }
+    }
+
+    public void printNumbers() {
+        System.out.println(numbers);
     }
 
     // TODO: 추가 기능 구현
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,11 +3,15 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -21,6 +25,25 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 맞지 않는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByInvalidNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 번호 출력확인")
+    @Test
+    void printNumbersTest() {
+        Lotto lotto = new Lotto(List.of(1,3,5,7,9,11));
+        System.setOut(new PrintStream(outputStreamCaptor));
+        lotto.printNumbers();
+        String output = outputStreamCaptor.toString().trim();
+        String expectedOutput = "[1, 3, 5, 7, 9, 11]";
+        assertEquals(expectedOutput, output);
+        System.setOut(System.out);
     }
 
     // 아래에 추가 테스트 작성 가능

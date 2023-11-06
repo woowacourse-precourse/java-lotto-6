0d3046437c81528b28e99e6a18ff86f357fef383
yechop <channetattoo@gmail.com>
2023-11-06 18:08:02 +0900

    test(Money): money를 반환 테스트 구현

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,21 +1,42 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MoneyTest {
+
+    Money money;
+
+    @BeforeEach
+    void setUp() {
+        money = new Money(1000);
+    }
+
+    @Test
+    @DisplayName("money를 반환한다.")
+    void getMoneyTest() {
+        //given
+
+        //when
+        int temp = money.getMoney();
+
+        //then
+        assertThat(temp).isEqualTo(1000);
+    }
 
     @Test
     @DisplayName("money 나누기 1000원 한 값을 quantity로 반환한다.")
     void getQuantityTest() {
         //given
-        Money money = new Money(1000);
 
         //when
         int quantity = money.getQuantity();
 
         //then
-        Assertions.assertThat(quantity).isEqualTo(1);
+        assertThat(quantity).isEqualTo(1);
     }
 }

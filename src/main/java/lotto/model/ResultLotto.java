e374c36ae66396ab41da8296b13d61f2fa650775
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 02:50:56 +0900

    feat: add validate duplicate bonus num to result lotto

--- src/main/java/lotto/model/ResultLotto.java
+++ src/main/java/lotto/model/ResultLotto.java
@@ -10,12 +10,19 @@ public class ResultLotto {
     public ResultLotto(Lotto winningLotto, int bonusNum) {
         this.winningLotto = winningLotto;
         validateRange(bonusNum);
+        validateDuplicate(bonusNum);
         this.bonusNum = bonusNum;
     }
 
     private void validateRange(int bonusNum) {
         if (bonusNum < 0 || bonusNum > 45) {
             throw new IllegalArgumentException(LottoException.INVALID_RANGE.getExceptionMessage());
+        }
+    }
+
+    private void validateDuplicate(int bonusNum) {
+        if(winningLotto.isContainNum(bonusNum)) {
+            throw new IllegalArgumentException(LottoException.NOT_DUPLICATE.getExceptionMessage());
         }
     }
 }

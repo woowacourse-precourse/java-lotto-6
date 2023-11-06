8c49d89084095e89a18d8179ea639f9fd8ae2066
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-06 16:33:49 +0900

    refactor: 보너스 번호 로또 번호 포함 테스트 코드 리팩토링

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -3,9 +3,12 @@ package lotto.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class BonusNumberTest {
 
@@ -17,7 +20,8 @@ public class BonusNumberTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @ParameterizedTest(name = "로또 번호 : {1, 2, 3, 4, 5, 6}, 보너스 번호 : {1}")
+    @ParameterizedTest(name = "로또번호 : {0}, 보너스번호 : {1}")
+    @MethodSource("lottoNumberAndBonusNumber")
     @DisplayName("보너스 번호가 당첨 로또 번호에 포함된다면 예외 발생")
     void bonusNumberContains(List<Integer> lottoNumber, int bonus) {
         Lotto lotto = new Lotto(lottoNumber);
@@ -25,5 +29,11 @@ public class BonusNumberTest {
 
         Assertions.assertThatThrownBy(() -> bonusNumber.containsLotto(lotto))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> lottoNumberAndBonusNumber() {
+        return Stream.of(
+                Arguments.arguments(List.of(1, 2, 3, 4, 5, 6), 5)
+        );
     }
 }

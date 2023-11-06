7b5c7845f273ed331272ccaf0847b7ee973a759b
alscjf1329 <alscjf1329@gmail.com>
2023-11-06 18:59:14 +0900

    feat(Application): lotto 구매 진행
    lottoPurchaseManager에게 로또 구매를 진행했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.LottoPurchaseManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoPurchaseManager lottoPurchaseManager = new LottoPurchaseManager();
+        lottoPurchaseManager.runPurchase();
     }
 }

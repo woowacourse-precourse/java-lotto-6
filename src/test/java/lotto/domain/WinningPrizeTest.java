0723efbae4b111317e03ba5abcaa88587f208039
SungKyum Kim <sungkyum1@naver.com>
2023-11-06 18:23:23 +0900

    test(WinningPrize): 상금 반환을 관리하는 기능의 테스트 코드 구현

--- src/test/java/lotto/domain/WinningPrizeTest.java
+++ src/test/java/lotto/domain/WinningPrizeTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class WinningPrizeTest {
-  
+    @DisplayName("매칭된 숫자 개수에 맞는 상금을 반환.")
+    @Test
+    void calculatePrice() {
+        assertThat(WinningPrize.calculatePrize(6, true)).isEqualTo(2_000_000_000);
+    }
+
+    @DisplayName("매칭 개수가 5개이고 보너스 넘버가 포함되어 있을 때 2등 상금 반환.")
+    @Test
+    void calculatePriceByFiveWithBonusNumber() {
+        assertThat(WinningPrize.calculatePrize(5, true)).isEqualTo(30_000_000);
+    }
+
+    @DisplayName("매칭된 숫자 개수에 맞는 상금을 반환.")
+    @Test
+    void calculatePriceByOutOfRangeMatchingNumber() {
+        assertThatThrownBy(() -> WinningPrize.calculatePrize(2, false))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

4476e1d41d7849e555ab1b064c644381473a63c7
ksw13 <ksw48483@gmail.com>
2023-11-06 12:16:45 +0900

    feat: 보너스 번호 입력값 검증

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -1,10 +1,24 @@
 package lotto.model;
 
+import lotto.util.ExceptionMessage;
+import lotto.util.NumberConst;
+
 public class Bonus {
     private int bonus;
 
     public Bonus(int bonus) {
+        validate(bonus);
         this.bonus = bonus;
+    }
+
+    private void validate(int bonus) {
+        if (!isValidRange(bonus)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_RANGE.getMessage());
+        }
+    }
+
+    private boolean isValidRange(int bonus) {
+        return bonus >= NumberConst.MIN_RANGE && bonus <= NumberConst.MAX_RANGE;
     }
 
 }

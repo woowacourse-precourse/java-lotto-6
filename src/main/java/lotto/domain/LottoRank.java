d82c995babf34e43d4e01a4cca2fa0c051e63c73
jyajoo <jyajoo1020@gmail.com>
2023-11-06 20:50:36 +0900

    fix: String 타입으로 winningAmount 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum LottoRank {
-    ZERO(0, 0, (matchCount, isBonus) -> matchCount < 3),
-    THREE(3, 5000, (matchCount, isBonus) -> matchCount == 3),
-    FOUR(4, 50000, (matchCount, isBonus) -> matchCount == 4),
-    FIVE(5, 150000, (matchCount, isBonus) -> matchCount == 5 && !isBonus),
-    FIVE_BONUS(5, 30000000, (matchCount, isBonus) -> matchCount == 5 && isBonus),
-    SIX(6, 2000000000, (matchCount, isBonus) -> matchCount == 6);
+    ZERO(0, "0", (matchCount, isBonus) -> matchCount < 3),
+    THREE(3, "5,000", (matchCount, isBonus) -> matchCount == 3),
+    FOUR(4, "50,000", (matchCount, isBonus) -> matchCount == 4),
+    FIVE(5, "1,500,000", (matchCount, isBonus) -> matchCount == 5 && !isBonus),
+    FIVE_BONUS(5, "30,000,000", (matchCount, isBonus) -> matchCount == 5 && isBonus),
+    SIX(6, "2,000,000,000", (matchCount, isBonus) -> matchCount == 6);
 
     private final int count;
-    private final int winningAmount;
+    private final String winningAmount;
     private final BiPredicate<Integer, Boolean> checkWin;
 
-    LottoRank(int count, int winningAmount, BiPredicate<Integer, Boolean> checkWin) {
+    LottoRank(int count, String winningAmount, BiPredicate<Integer, Boolean> checkWin) {
         this.count = count;
         this.winningAmount = winningAmount;
         this.checkWin = checkWin;
@@ -27,5 +27,13 @@ public enum LottoRank {
                 .filter(lottoRank -> lottoRank.checkWin.test(matchCount, isBonus))
                 .findFirst()
                 .orElse(ZERO);
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getWinningAmount() {
+        return winningAmount;
     }
 }

a7612867ff6da150d701a4b4b9d5fb3cfa6d07b1
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 02:38:32 +0900

    feat: add constructor to result lotto model

--- src/main/java/lotto/model/ResultLotto.java
+++ src/main/java/lotto/model/ResultLotto.java
@@ -1,4 +1,12 @@
 package lotto.model;
 
 public class ResultLotto {
+
+    private final Lotto winningLotto;
+    private final int bonusNum;
+
+    public ResultLotto(Lotto winningLotto, int bonusNum) {
+        this.winningLotto = winningLotto;
+        this.bonusNum = bonusNum;
+    }
 }

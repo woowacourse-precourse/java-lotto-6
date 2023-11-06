c8fa757b1b22eb50d2ffcb7c4482dd379db1d349
BonSik <rnqhstlr2297@gmail.com>
2023-11-07 03:11:38 +0900

    feat: 구입한 로또 정보 메시지 출력 기능 구현

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -1,13 +1,28 @@
 package lotto.view;
 
+import java.util.Comparator;
+import java.util.List;
+
 public final class LottoOutputView {
     private final static String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
-
+    private final static String LOTTO_COUNT_MESSAGE = "\n%d개를 구매했습니다.";
     private LottoOutputView() {
     }
 
     public static void printPurchasePriceMessage() {
         System.out.println(PURCHASE_PRICE_MESSAGE);
+    }
+
+    public static void printLottoCount(int lottoCount) {
+        String lottCountMessage = String.format(LOTTO_COUNT_MESSAGE, lottoCount);
+        System.out.println(lottCountMessage);
+    }
+
+    public static void printLottoNumbers(List<List<Integer>> numbers) {
+        for (List<Integer> number : numbers) {
+            number.sort(Comparator.naturalOrder());
+            System.out.println(number);
+        }
     }
 
 }

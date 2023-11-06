3561d9b4fe7691cac79ce9107297ad1553b3912d
권장순 <jsoonworld@gmail.com>
2023-11-07 00:44:35 +0900

    feat( Application#main() ) : 로또 애플리케이션의 메인 클래스 및 실행 흐름 구현.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoMachine;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoMachine lottoMachine = new LottoMachine();
+        LottoView lottoView = new LottoView();
+        LottoController lottoController = new LottoController(lottoMachine, lottoView);
+
+        lottoController.play();
     }
 }

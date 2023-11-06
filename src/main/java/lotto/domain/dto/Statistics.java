5a8c790d3198271e5726b5f7ed7c0c104aaa0a86
yubin <dbqls2821@gmail.com>
2023-11-06 19:02:59 +0900

    test: statistics 내부 필드가 동일하다면 같은 객체로 인식하는 테스트코드 추가

--- src/main/java/lotto/domain/dto/Statistics.java
+++ src/main/java/lotto/domain/dto/Statistics.java
@@ -8,7 +8,6 @@ public record Statistics(
         List<Result> results,
         double rate
 ) {
-
     public int count(Result target) {
         return (int) results.stream().filter(it -> it == target).count();
     }
---
+++ src/test/java/lotto/domain/dto/StatisticsTest.java
@@ -0,0 +1,22 @@
+package lotto.domain.dto;
+
+import lotto.domain.Result;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsTest {
+
+    @Test
+    void statistics_내부_필드가_동일하다면_같은_객체로_인식한다() {
+        List<Result> results = List.of(Result.FIVE_MATCH, Result.SIX_MATCH);
+        Statistics statisticsA = new Statistics(results, 50.0);
+        Statistics statisticsB = new Statistics(results, 50.0);
+
+        assertEquals(statisticsA, statisticsB);
+    }
+}

e26201e52627a8d1bbdd7d21bc79f0c4bea0bc7f
Handwoong <handwoong@gmail.com>
2023-11-06 16:46:38 +0900

    test: 로또 번호 오름차순 정렬 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -8,20 +9,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+    @Test
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        final List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        final List<Integer> numbers = List.of(1, 2, 3, 4, 5, 5);
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 번호를 생성하면 오름차순으로 정렬한다.")
+    void createLottoBySortAsc() throws Exception {
+        // given
+        final List<Integer> numbers = List.of(6, 5, 3, 4, 2, 1);
+        final Lotto lotto = new Lotto(numbers);
+
+        // when
+        final List<Integer> result = lotto.getNumbers();
+
+        // then
+        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+    }
 }

cee6b5557b6ecee44ac48bc944630393021d966e
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-06 17:10:32 +0900

    feat: 구입한 로또 수량만큼 로또 번호를 추출하여 저장하는 기능 구현

--- src/main/java/lotto/ResultService.java
+++ src/main/java/lotto/ResultService.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultService {
     public static int calculateNumberOfLottoTickets(int purchaseAmount) {
         return purchaseAmount / InputService.AMOUNT_UNIT;
@@ -7,6 +10,14 @@ public class ResultService {
 
     public static void printNumberOfLottoTickets(int purchaseAmount) {
         System.out.printf("%d개를 구매했습니다.", purchaseAmount);
+    }
+
+    public static List<Lotto> getResultsOfLottoIssuance(int purchaseAmount) {
+        List<Lotto> resultsOfLottoIssuance = new ArrayList<>();
+        for (int i = 1; i <= purchaseAmount; i++) {
+            resultsOfLottoIssuance.add(Lotto.createRandomNumbersForLotto());
+        }
+        return resultsOfLottoIssuance;
     }
 
 

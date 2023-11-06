17a035ef6d960df1f8862f6250ef2bb292c3f6f7
sgo722 <sgo722@naver.com>
2023-11-07 04:49:33 +0900

    fix : 로또 번호와 당첨 번호 비교 기능

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,22 +1,20 @@
 package lotto.domain;
 
 public enum Rank {
-    NONE(0, 0, false, "불일치"),
-    FIFTH(3, 5000, true,"3개 일치 (5,000원)"),
-    FOURTH(4, 50000, true,"4개 일치 (50,000원)"),
-    THIRD(5, 1500000, true,"5개 일치 (1,500,000원)"),
-    SECOND(6, 30000000, true,"5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    FIRST(6, 2000000000, false,"6개 일치 (2,000,000,000원)");
+    NONE(0, 0,  "불일치"),
+    FIFTH(3, 5000, "3개 일치 (5,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    SECOND(6, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
     private final int count;
     private final int profit;
-    private final boolean checkBonusNumber;
     private final String message;
 
-    Rank(int count, int profit, boolean checkBonusNumber, String message) {
+    Rank(int count, int profit, String message) {
         this.count = count;
         this.profit = profit;
-        this.checkBonusNumber = checkBonusNumber;
         this.message = message;
     }
 
@@ -25,8 +23,14 @@ public enum Rank {
     }
 
     public static Rank valueOf(Integer count, boolean checkBonusNumber) {
+        if(count == 6 && checkBonusNumber){
+            return FIRST;
+        }
+        if(count == 6 && !checkBonusNumber){
+            return SECOND;
+        }
         for(Rank rank : Rank.values()){
-            if(rank.getCount() == count && rank.checkBonusNumber == checkBonusNumber){
+            if(rank.count == count){
                 return rank;
             }
         }

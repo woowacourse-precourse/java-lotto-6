a082e03f2ed36340b21591bc5ef4dd8ac78bb7f1
AJN <buzz781@gmail.com>
2023-11-06 20:49:54 +0900

    feat : 당첨금 추출 기능 구현

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -4,6 +4,7 @@ import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoService {
+	private static final int LOTTO_NUMBERS_COUNT = 6;
 	//1000단위의 정수당 한개의 Lotto 객체 리스트
 	public static List<Lotto> createObjectPerThousandUnits(int inputData){
 		int playerInputThousands = inputData/1000;
@@ -14,7 +15,29 @@ public class LottoService {
 		}
 		return lotto_list;
 	}
+	private static int getWinningCount(List<Integer> playerLottoNumber,List<Integer> winningNumbers) {
+	    int winningCount = 0;
+	    for(int i=0;i<LOTTO_NUMBERS_COUNT;i++) {
+	    	if(playerLottoNumber.get(i) == winningNumbers.get(i)) winningCount++;
+	    }
+		return winningCount;
+	}
+	private static boolean isBonusNumberIncluded(List<Integer> playerLottoNumber,int bonusNumber) {
+		boolean bonusMatch = false;
+		for(int playerNum : playerLottoNumber) {
+			if(playerNum == bonusNumber) return true;
+		}
+		return bonusMatch;
+	}
 	
-
+	public static long getTotalWinningAmount(List<Lotto> playerLottos,List<Integer> winningNumbers,int bonusNumber) {
+		boolean bonusMatch = false; int winningCount = 0; long totalWinningAmount = 0;
+		for(Lotto playerLotto : playerLottos) {
+			winningCount = getWinningCount(playerLotto.getNumbers(),winningNumbers);
+			bonusMatch = isBonusNumberIncluded(playerLotto.getNumbers(), bonusNumber);
+			totalWinningAmount += LottoPrize.valueOf(winningCount, bonusMatch).getWinnings();
+		}
+		return totalWinningAmount;
+	}
 
 }

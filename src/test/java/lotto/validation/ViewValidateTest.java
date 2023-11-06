79c86c1c4bd70f53cc99c97fc4185c7c1a61834d
gunwoong <gwj0421@gmail.com>
2023-11-06 20:33:56 +0900

    test : null 처리 검증 테스트 추가

--- src/test/java/lotto/validation/ViewValidateTest.java
+++ src/test/java/lotto/validation/ViewValidateTest.java
@@ -22,4 +22,16 @@ class ViewValidateTest {
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
         assertThatCode(() -> ViewValidate.validateNotSpaceOrEmpty(content)).doesNotThrowAnyException();
     }
+
+    @Test
+    void validateNull() {
+        // given
+        String nullContent = null;
+        String content = "123";
+
+        // when & then
+        assertThatThrownBy(() -> ViewValidate.validateNotNull(nullContent))
+                .isInstanceOf(NullPointerException.class).hasMessageContaining(ERROR_MESSAGE);
+        assertThatCode(() -> ViewValidate.validateNotNull(content)).doesNotThrowAnyException();
+    }
 }

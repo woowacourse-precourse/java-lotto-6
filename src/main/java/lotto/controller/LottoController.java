db6d6d1d8a56e31bffac322a9b1150cf26387c16
kuk329 <kuk329@cau.ac.kr>
2023-11-07 00:46:31 +0900

    feat: 로또 구입금액 입력값에 대한 예외처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,27 +9,56 @@ public class LottoController {
 
     private final int lottoPrice = 1000;
     private int purchaseQuantity;
+    private boolean checkInitPurchaseQuantity = true;
 
 
     public void start() {
-
-        initPurchaseQuantity(); // 구매 수량 초기화
+        while (checkInitPurchaseQuantity) {
+            initPurchaseQuantity(); // 구매 수량 초기화
+        }
         outputPurchaseQuantity();
     }
 
     public void initPurchaseQuantity() {
-        String input = InputView.inputAmountOfMoney();
-        calculateQuantity(input);
+        try {
+            String input = InputView.inputAmountOfMoney();
+            validateInputMoney(input);
+            calculateQuantity(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private void calculateQuantity(String input) { // 로또 구매 수량 계산.
-        int money = Integer.parseInt(input);
-        purchaseQuantity = money / lottoPrice;
+        try {
+            int money = Integer.parseInt(input);
+            validatePurchaseQuantity(money);
+            purchaseQuantity = money / lottoPrice;
+            checkInitPurchaseQuantity = false;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void outputPurchaseQuantity() {
         OutputView.printPurchaseQuantity(purchaseQuantity);
     }
 
+    public void validateInputMoney(String input) {
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.find()) {
+            checkInitPurchaseQuantity = true;
+            throw new IllegalArgumentException("[ERROR]숫자를 입력해 주세요.");
+        }
+    }
+
+    public void validatePurchaseQuantity(int money) {
+        if (money == 0 || money % 1000 != 0) {
+            checkInitPurchaseQuantity = true;
+            throw new IllegalArgumentException("[ERROR]금액은 1,000원 단위로 입력해 주세요.");
+        }
+    }
 
 }

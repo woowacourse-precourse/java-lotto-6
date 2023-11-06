217c2abb441749dc89096cfd0e694cb8257af82c
BlueArcticFox <jominhyeok0103@gmail.com>
2023-11-06 16:00:12 +0900

    chore:하드코딩 부분 상수 처리

--- src/main/java/lotto/constant/Message.java
+++ src/main/java/lotto/constant/Message.java
@@ -1,6 +1,7 @@
 package lotto.constant;
 
 public enum Message {
+    LINE_BREAKING("\n"),
     INPUT_MONEY("구입금액을 입력해 주세요."),
     PURCHASE_LOTTO("%d개를 구매했습니다."),
     INPUT_PRIZE_NUMBER("당첨 번호를 입력해 주세요."),
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,21 +8,21 @@ public class InputView {
     public static String inputMoney() {
         System.out.println(Message.INPUT_MONEY);
         String input = readLine();
-        System.out.println(input+"\n");
+        System.out.println(input + Message.LINE_BREAKING);
         return input;
     }
 
     public static String inputPrizeNumber() {
         System.out.println(Message.INPUT_PRIZE_NUMBER);
         String input = readLine();
-        System.out.println(input+"\n");
+        System.out.println(input + Message.LINE_BREAKING);
         return input;
     }
 
     public static String inputBonusNumber() {
         System.out.println(Message.INPUT_BONUS_NUMBER);
         String input = readLine();
-        System.out.println(input+"\n");
+        System.out.println(input + Message.LINE_BREAKING);
         return input;
     }
 }

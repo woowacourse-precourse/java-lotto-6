0567cfdcce128996e9c17fe17107b848478138cd
jongmin Lee <rjsqktod1@gmail.com>
2023-11-06 20:42:48 +0900

    feat(bonus): 보너스 번호에서 누락된 검증 로직 추가
    번호 범위 검증을 추가
    
    Signed-off-by: jongmin Lee <rjsqktod1@gmail.com>

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -6,18 +6,32 @@ import java.util.List;
 
 public class BonusNumber {
     private final int BONUS_SIZE = 1;
+    private static final int LOWER_BOUND_NUMBER = 1;
+    private static final int UPPER_BOUND_NUMBER = 45;
     private final int bonusNumber;
     private ValidateUtils validateUtils = new ValidateUtils();
 
     public BonusNumber(int bonusNumber, Lotto lotto) {
-        lotto.lottoContainNumber(bonusNumber);
+        validateRange(bonusNumber, LOWER_BOUND_NUMBER, UPPER_BOUND_NUMBER);
+        validateUniqueBonusNumber(lotto, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
     public static BonusNumber from(String inputString, Lotto lotto) {
         ValidateUtils.validateNumeric(inputString);
-
         return new BonusNumber(Integer.parseInt(inputString), lotto);
+    }
+
+    public void validateUniqueBonusNumber(Lotto lotto, int bonusNumber) {
+        if (lotto.lottoContainNumber(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+    }
+
+    public void validateRange(int number, int lowerBound, int upperBound) {
+        if (number < lowerBound || number > upperBound) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
     }
 
     public int getBonusNumber() {

f46ca3cf70d4020f3e861fb1da4707bc286cf6a5
Haeun Jeong <gkdms4248@gmail.com>
2023-11-07 00:29:25 +0900

    feat: 당첨 내역 출력기능 추가

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -2,12 +2,14 @@ package lotto.view;
 
 public enum OutputMessage {
     REQUEST_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    REQUEST_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-    REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    PURCHASE_RESULT("개를 구매했습니다."),
-    WINNING_STATISTICS("당첨 통계"),
+    PURCHASE_RESULT("\n%d개를 구매했습니다.\n"),
+    REQUEST_WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("\n당첨 통계"),
     SEPARATOR("---"),
-    TOTAL_RETURN("총 수익률은 f%%입니다.");
+    RESULT("%d개 일치 (%s원) - %d개\n"),
+    BONUS_RESULT("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n"),
+    TOTAL_RETURN("총 수익률은 %.2f%%입니다.\n");
 
     private final String message;
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,22 @@
 package lotto.view;
 
+import static lotto.domain.LottoCriteria.FIFTH_PLACE;
+import static lotto.domain.LottoCriteria.FIRST_PLACE;
+import static lotto.domain.LottoCriteria.FOURTH_PLACE;
+import static lotto.domain.LottoCriteria.SECOND_PLACE;
+import static lotto.domain.LottoCriteria.THIRD_PLACE;
+import static lotto.view.OutputMessage.BONUS_RESULT;
 import static lotto.view.OutputMessage.PURCHASE_RESULT;
+import static lotto.view.OutputMessage.RESULT;
 import static lotto.view.OutputMessage.SEPARATOR;
 import static lotto.view.OutputMessage.TOTAL_RETURN;
 import static lotto.view.OutputMessage.WINNING_STATISTICS;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.domain.LottoCriteria;
 
 public class OutputView {
 
@@ -11,16 +24,45 @@ public class OutputView {
         System.out.println(message);
     }
 
-    public void purchaseResult(int number) {
-        System.out.println(number + PURCHASE_RESULT.getMessage());
+    public void printPurchaseResult(int number) {
+        System.out.printf(PURCHASE_RESULT.getMessage(), number);
     }
 
-    public void winningStatistics() {
+    public void printWinningStatistics() {
         System.out.println(WINNING_STATISTICS.getMessage());
         System.out.println(SEPARATOR.getMessage());
     }
 
-    public void totalReturn(float totalReturn) {
+    public void printIssuedLottos(List<Lotto> issuedLottos) {
+        issuedLottos.stream().forEach(lotto -> System.out.println(ListToStringWithBracket(lotto.getNumbers())));
+    }
+
+    private String ListToStringWithBracket(List<Integer> lottoNumbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        String lotto = lottoNumbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+        stringBuilder.append(lotto);
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    public void printResult(Map<LottoCriteria, Long> result) {
+        System.out.printf(RESULT.getMessage(), FIFTH_PLACE.getCount(), String.format("%,d", FIFTH_PLACE.getAmount()),
+                result.get(FIFTH_PLACE));
+        System.out.printf(RESULT.getMessage(), FOURTH_PLACE.getCount(), String.format("%,d", FOURTH_PLACE.getAmount()),
+                result.get(FOURTH_PLACE));
+        System.out.printf(RESULT.getMessage(), THIRD_PLACE.getCount(), String.format("%,d", THIRD_PLACE.getAmount()),
+                result.get(THIRD_PLACE));
+        System.out.printf(BONUS_RESULT.getMessage(), SECOND_PLACE.getCount(),
+                String.format("%,d", SECOND_PLACE.getAmount()),
+                result.get(SECOND_PLACE));
+        System.out.printf(RESULT.getMessage(), FIRST_PLACE.getCount(), String.format("%,d", FIRST_PLACE.getAmount()),
+                result.get(FIRST_PLACE));
+    }
+
+    public void printTotalReturn(float totalReturn) {
         System.out.printf(TOTAL_RETURN.getMessage(), totalReturn);
     }
 }

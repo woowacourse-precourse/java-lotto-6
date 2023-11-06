93a6f5408e96270e02eb13f5b7efdfc649add1f2
leegwichan <cnddkscndgus@naver.com>
2023-11-06 22:00:40 +0900

    feat (Application) : private 기본 생성자 구현
    - static method 외에 사용될 곳이 없어 생성자를 private로 바꿈

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,10 @@ package lotto;
 import lotto.controller.LottoController;
 
 public class Application {
+
+    private Application() {
+    }
+
     public static void main(String[] args) {
         new LottoController().run();
     }

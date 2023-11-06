528298cc32b9bb7675471f83da4cf47f8efa35ee
AJN <buzz781@gmail.com>
2023-11-06 19:32:22 +0900

    fix : LottoPrize 로직 수정

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -1,18 +1,36 @@
 package lotto.model;
 
 public enum LottoPrize {
-	THREE_MATCHES(5000),    // 3개 번호가 일치할 때의 상금
-    FOUR_MATCHES(50000),    // 4개 번호가 일치할 때의 상금
-    FIVE_MATCHES(1500000),  // 5개 번호가 일치할 때의 상금
-    SIX_MATCHES(2000000000); // 6개 번호가 일치할 때의 상금
+	FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
 
-    private final int prizeAmount;
+    private final int matchCount;
+    private final long winnings;
 
-    LottoPrize(int prizeAmount) {
-        this.prizeAmount = prizeAmount;
+    LottoPrize(int matchCount, long winnings) {
+        this.matchCount = matchCount;
+        this.winnings = winnings;
     }
 
-    public int getPrizeAmount() {
-        return prizeAmount;
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public long getWinnings() {
+        return winnings;
+    }
+
+    public static LottoPrize valueOf(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6) return FIRST;
+        if (matchCount == 5 && bonusMatch) return SECOND;
+        if (matchCount == 5) return THIRD; 
+        if (matchCount == 4) return FOURTH;
+        if (matchCount == 3) return FIFTH;
+        
+        return NONE;
     }
 }

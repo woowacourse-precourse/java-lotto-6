33c0f624027969ad1a5e05eec041b2e9c04aafc1
wonjun <wonjon0930@daum.net>
2023-11-06 21:22:55 +0900

    feat: add feature to check user input about winning lotto numbers contains empty item

--- src/main/java/lotto/view/validator/WinningLottoNumbersInputValidator.java
+++ src/main/java/lotto/view/validator/WinningLottoNumbersInputValidator.java
@@ -1,5 +1,6 @@
 package lotto.view.validator;
 
+import java.util.Arrays;
 import lotto.domain.constant.LottoConstant;
 import lotto.view.constant.InputConstant;
 import lotto.view.exception.LottoInputException;
@@ -11,6 +12,7 @@ public class WinningLottoNumbersInputValidator {
         isNotEmpty(winningLottoNumbers);
         containsDelimiter(winningLottoNumbers);
         containsSixNumbers(winningLottoNumbers);
+        isEachNumberNotEmpty(winningLottoNumbers);
     }
 
     private void isNotEmpty(String winningLottoNumbers){
@@ -27,12 +29,24 @@ public class WinningLottoNumbersInputValidator {
 
     private void containsSixNumbers(String winningLottoNumbers){
         if(!dividedIntoSixNumbers(winningLottoNumbers)){
-            throw LottoInputException.of(LottoInputExceptionMessage.WINNING_LOTTO_NUMBERS_DOS_NOT_CONTAIN_SIX_NUMBERS);
+            throw LottoInputException.of(LottoInputExceptionMessage.WINNING_LOTTO_NUMBERS_DOES_NOT_CONTAIN_SIX_NUMBERS);
         }
     }
 
     private boolean dividedIntoSixNumbers(String winningLottoNumbers){
         return winningLottoNumbers.split(InputConstant.WINNING_LOTTO_NUMBERS_DELIMITER).length == LottoConstant.LOTTO_ITEM_COUNT.getValue();
+    }
+
+    private void isEachNumberNotEmpty(String winningLottoNumbers){
+        String[] numbers = winningLottoNumbers.split(InputConstant.WINNING_LOTTO_NUMBERS_DELIMITER);
+        if(isMoreThanOneNumberEmpty(numbers)){
+            throw LottoInputException.of(LottoInputExceptionMessage.WINNING_LOTTO_NUMBERS_CONTAINS_EMPTY_NUMBER);
+        }
+    }
+
+    private boolean isMoreThanOneNumberEmpty(String[] numbers){
+        return Arrays.stream(numbers)
+                .anyMatch(String::isEmpty);
     }
 
 

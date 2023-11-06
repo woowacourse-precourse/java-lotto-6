5e97a774c3504eeddf03dfdf848e49bf67ff5875
최진원 <etudent39@gmail.com>
2023-11-06 21:39:50 +0900

    test : 구매개수에 대한 테스트 코드 작성

--- src/test/java/lotto/BuyAmountTest.java
+++ src/test/java/lotto/BuyAmountTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BuyAmountTest {
     @DisplayName("입력한 구입 금액이 정수가 아니면 예외가 발생한다.")
@@ -27,5 +28,11 @@ public class BuyAmountTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("구입한 금액에 따라 구매 개수를 반환한다.")
+    @Test
+    void 구매_개수_테스트(){
+        assertThat(new BuyAmount("8000").getBuyCount())
+                .isEqualTo(8);
+    }
 
 }

fbc2558e25a2226cc3156078c9dcf2df66cc2c13
최진원 <etudent39@gmail.com>
2023-11-07 01:01:53 +0900

    test : 로또 번호 정렬

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,13 @@
 package lotto;
 
-import java.util.Collections;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Arrays;
 
-
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -31,5 +26,24 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 정렬되어 있는지 확인한다.")
+    @Test
+    void 로또_번호_정렬() {
+        // given
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(1);
+        numbers.add(4);
 
+        //when
+        Lotto lotto = new Lotto(numbers);
+
+        //then
+        for(int i = 0; i < 6; i++) {
+            assertThat(lotto.getLotto().get(i)).isEqualTo(i + 1);
+        }
+    }
 }

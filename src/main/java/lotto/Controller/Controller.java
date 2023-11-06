7bbe6e1cf258fc622d4b4b589611d0ece17546e6
minju <minju9711@naver.com>
2023-11-07 03:23:40 +0900

    [feat] 사용자로부터 로또와 보너스 번호, 당첨번호를 입력받고 유효성 검사까지 해주는 SettingLotto 함수 생성

--- src/main/java/lotto/Controller/Controller.java
+++ src/main/java/lotto/Controller/Controller.java
@@ -1,4 +1,48 @@
 package lotto.Controller;
 
+import lotto.Factory.LottoContainer;
+import lotto.IO.IO;
+import lotto.Util.Validate;
+import java.util.List;
+
 public class Controller {
+    private static LottoContainer lottoContainer;
+    private int count;
+    private int bonus;
+    public Controller(LottoContainer lottoContainer) {
+        this.lottoContainer = lottoContainer;
+    }
+
+    public void SettingLotto() { //사용자로부터 로또와 정답로또 입력받고 생성까지 끝내놓기
+        String WinningLotto;
+        List<Integer> WinningNum;
+
+        purchaseLotto();
+        lottoContainer.MakeContainer(count);
+        WinningLotto = IO.InputWinningLotto();
+        WinningNum = Validate.WinningLottoInput(WinningLotto);
+        lottoContainer.MakeWinningLotto(WinningNum);
+        settingBonus();
+    }
+
+    private void purchaseLotto() {
+        String cash = IO.InputCash();
+        Validate.CashIsInteger(cash);
+        count = Integer.parseInt(cash) / 1000;
+        IO.PrintCount(count);
+    }
+
+    private void settingBonus(){
+        String bonusStr = IO.InputBonus();
+        bonus = Integer.parseInt(bonusStr);
+        Validate.isDuplicateBonus(lottoContainer, bonus);
+    }
+
+    public void RunLotto(){ //사용자의 로또와 정답로또 비교 후 rank 매기기
+
+    }
+
+    public void PrintResult(){ //매겨진 랭크 출력
+
+    }
 }

3ea0f41735dc8527f45f33c8794a50e74e517f3b
Park Eunhyeong <silbroeh@gmail.com>
2023-11-06 18:56:02 +0900

    refactor : PrizeGrade클래스 - NO_PRIZE 필드 추가

--- src/main/java/lotto/PrizeGrade.java
+++ src/main/java/lotto/PrizeGrade.java
@@ -5,7 +5,8 @@ public enum PrizeGrade {
     SECOND(5, 30000000, EMPTY),
     THIRD(5, 1500000, ", 보너스 볼 일치"),
     FOURTH(4, 50000, EMPTY),
-    FIFTH(3, 5000, EMPTY);
+    FIFTH(3, 5000, EMPTY),
+    NO_PRIZE(0, 0, EMPTY);
 
     private final int matchCount;
     private final int prizeMoney;

f01789905566fee615ac694060228294e545460f
김민정 <mjeong@sookmyung.ac.kr>
2023-11-06 16:04:05 +0900

    refactor: LottoGame 클래스에서 함수 이름, 타입 수정

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,39 +3,52 @@ package lotto;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
+import lotto.domain.Money;
+import lotto.domain.ResultCalculator;
+import lotto.domain.WinningLotto;
 import lotto.util.SortedRandomNumberGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoGame {
     private final LottoGenerator lottoGenerator = new LottoGenerator(new SortedRandomNumberGenerator());
+    private final ResultCalculator resultCalculator = new ResultCalculator();
 
     public void play() {
-        List<Lotto> lottos = issueLotto();
-        Lotto winningNumber = getValidWinningLotto();
-        int bonusNumber = getValidBonusNumber(winningNumber);
+        Money money = getValidMoney();
+        List<Lotto> lottos = lottoGenerator.generateForAmount(money);
+        OutputView.printIssuedLotto(lottos);
+
+        WinningLotto winningLotto = getValidWinningLotto();
+        getValidBonusNumber(winningLotto);
+
+        resultCalculator.calculateWinningCounts(lottos, winningLotto);
+        resultCalculator.calculateProfitRate(money.getAmount());
+        OutputView.printResult(resultCalculator);
     }
 
-    private List<Lotto> issueLotto() {
+    private Money getValidMoney() {
         try {
-            return lottoGenerator.generateForAmount(InputView.getAmount());
+            return new Money(InputView.getMoney());
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR]" + e.getMessage());
-            return issueLotto();
+            return getValidMoney();
         }
     }
 
-    private Lotto getValidWinningLotto() {
+    private WinningLotto getValidWinningLotto() {
         try {
-            return WinningAndBonusNumberValidator.validateWinningNumber(InputView.getWinningNumber());
+            return new WinningLotto(InputView.getWinningNumber());
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR]" + e.getMessage());
             return getValidWinningLotto();
         }
     }
 
-    private int getValidBonusNumber(Lotto winningLotto) {
+    private int getValidBonusNumber(WinningLotto winningLotto) {
         try {
-            return WinningAndBonusNumberValidator.validateBonusNumber(winningLotto.getNumbers(), InputView.getBonusNumber());
+            winningLotto.setBonusNumber(InputView.getBonusNumber());
+            return winningLotto.getBonusNumber();
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR]" + e.getMessage());
             return getValidBonusNumber(winningLotto);

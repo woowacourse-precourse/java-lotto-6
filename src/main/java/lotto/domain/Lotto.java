bdc93bf5f9cff7f3c9d8af966dfe1d78033d4ecc
DooDuZ <sin9158@naver.com>
2023-11-07 03:36:29 +0900

    feat: Lotto 당첨 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
+import lotto.game.Rank;
 import lotto.validator.LottoNumberValidator;
 
 public class Lotto {
@@ -16,5 +18,33 @@ public class Lotto {
         LottoNumberValidator.validateLottoNumbersRange(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    public Rank getRank(WinNumber winNumber) {
+        int count = getSameNumberCount(winNumber.getNumbers());
+        boolean bonus = isBonus(winNumber.getBonusNumber(), count);
+        for (Rank rank : Rank.values()){
+            if(rank.getCount()==count && rank.isBonus()==bonus){
+                return rank;
+            }
+        }
+        return Rank.NONE;
+    }
+    private int getSameNumberCount(Set<Integer> winNumbers){
+        int count = 0;
+        for (Integer number : numbers) {
+            if (winNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isBonus(int bonusNumber, int count){
+        boolean bonus = false;
+        for (Integer number : numbers) {
+            if(number==bonusNumber && count==5){
+                bonus = true;
+            }
+        }
+        return bonus;
+    }
 }

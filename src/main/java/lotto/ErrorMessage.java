06d5abbc275ae540cc1c539833636c37956712e0
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-11-06 23:15:19 +0900

    feat: 출력 메시지, 에러 메시지 관리 Enum 클래스 추가
    출력 메시지 상수, 에러 메시지 상수를 관리할 Enum 클래스 3개를 추가함.
    안내 메시지: NoticeMessage
    당첨 통계 메시지: MatchCountMessage
    에러 메시지: ErrorMessage

---
+++ src/main/java/lotto/ErrorMessage.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public enum ErrorMessage {
+    ERROR_OF_NOT_NUMBER("[ERROR] 입력값은 정수형 숫자여야 합니다!"),
+    ERROR_OF_MONEY_UNDER_1K("[ERROR] 구입금액은 1,000원 이상의 값이어야 합니다!"),
+    ERROR_OF_MONEY_NOT_MULTIPLES("[ERROR] 구입금액은 1,000원 단위여야 합니다!"),
+    ERROR_OF_WINNIG_NUMBER_OVER_RANGE("[ERROR] 당첨 번호는 1~45 사이의 숫자여야 합니다!"),
+    ERROR_OF_WINNIG_NUMBER_NOT_SIX_DIGITS("[ERROR] 당첨 번호는 6자리 숫자여야 합니다!"),
+    ERROR_OF_WINNIG_NUMBER_SAME_NUMBER("[ERROR] 당첨 번호는 서로 다른 숫자여야 합니다!"),
+    ERROR_OF_BONUS_NUMBER_OVER_RANGE("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다!"),
+    ERROR_OF_BONUS_NOT_ONE_DIGIT("[ERROR] 보너스 번호는 1자리 숫자여야 합니다!");
+
+    private final String message;
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/lotto/NoticeMessage.java
@@ -0,0 +1,22 @@
+package lotto;
+
+public enum NoticeMessage {
+    BUY_MONEY("구입금액을 입력해 주세요."),
+    BUY_COUNT("개를 구매했습니다."),
+    WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    WINNING_RESULT("당첨 통계"),
+    STRAIGHT_LINE("---"),
+    RATE_OF_RETURN_HEAD("총 수익률은 "),
+    RATE_OF_RETURN_TAIL("%입니다.");
+
+    private final String message;
+
+    NoticeMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

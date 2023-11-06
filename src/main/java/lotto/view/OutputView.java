51054d5e672291fb61677687502a2dd5a7d41928
opixxx <opix0306@naver.com>
2023-11-06 17:55:47 +0900

    feat(OutputView): 당첨 결과를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
+import java.util.Map;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
+import lotto.domain.Rank;
 
 public class OutputView {
     public static void printLottoQuantity(Money money) {
@@ -11,5 +13,15 @@ public class OutputView {
     public static void printLottoList(Lottos lottos) {
         lottos.getLottos().forEach(System.out::println);
         System.out.println();
+    }
+
+    public static void printResult(Map<Rank, Integer> lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoResult.getOrDefault(Rank.FIFTH, 0));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", lottoResult.getOrDefault(Rank.FOURTH, 0));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", lottoResult.getOrDefault(Rank.THIRD, 0));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", lottoResult.getOrDefault(Rank.SECOND, 0));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", lottoResult.getOrDefault(Rank.FIRST, 0));
     }
 }

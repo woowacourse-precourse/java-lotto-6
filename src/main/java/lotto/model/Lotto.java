306dbb0a1809b8f26cdb284509f1122a9925c0aa
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-06 17:08:38 +0900

    feat: Lotto 클래스의 List<Integer> 입력값 검증 기능 추가
    - 숫자 6개 검증
    - 중복된 숫자 검증
    - ErrorMessage Enum에 예외 메시지 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.model;
 
+import static lotto.Constraints.SIZE;
+import static lotto.model.enums.ErrorMessage.DUPLICATED_NUMBER_MESSAGE;
+import static lotto.model.enums.ErrorMessage.INVALID_SIZE_MESSAGE;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +15,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSize(numbers);
+        validateNumbersDuplicate(numbers);
+    }
+
+    // 6자리 검증
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE.getValue()) {
+            throw new IllegalArgumentException(INVALID_SIZE_MESSAGE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 중복된 숫자 검증
+    private void validateNumbersDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != SIZE.getValue()) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBER_MESSAGE.getMessage());
+        }
+    }
 }
--- src/main/java/lotto/model/enums/ErrorMessage.java
+++ src/main/java/lotto/model/enums/ErrorMessage.java
@@ -3,6 +3,7 @@ package lotto.model.enums;
 import static lotto.Constraints.LOTTO_PRICE;
 import static lotto.Constraints.MAX_NUMBER;
 import static lotto.Constraints.MIN_NUMBER;
+import static lotto.Constraints.SIZE;
 
 public enum ErrorMessage {
     NULL_INPUT_MESSAGE("[ERROR] null은 입력하실 수 없습니다."),
@@ -13,7 +14,8 @@ public enum ErrorMessage {
     NOT_CORRECT_INPUT_MESSAGE("[ERROR] 1, 2, 3, 4, 5, 6과 같은 형식으로 정수를 입력해 주세요."),
     OUT_OF_RANGE_NUMBER_MESSAGE("[ERROR] 로또 번호는 " + MIN_NUMBER.getValue() + "부터 "
             + MAX_NUMBER.getValue() + "사이의 숫자여야 합니다."),
-    DUPLICATED_NUMBER_MESSAGE("[ERROR] 당첨 번호와 중복된 숫자입니다.");
+    DUPLICATED_NUMBER_MESSAGE("[ERROR] 중복된 숫자가 존재 합니다."),
+    INVALID_SIZE_MESSAGE("[ERROR] 로또 번호는 " + SIZE.getValue() + "개 여야 합니다.");
 
     private final String message;
 

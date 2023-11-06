b9d07edd8ceb341a8ff44a889fa72e2555bc8967
sinminseok12 <sinminseok12@naver.com>
2023-11-06 18:38:46 +0900

    feat(Lotto): 중복되는 숫자를 검증하는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,21 +1,32 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final Integer LOTTO_SIZE = 6;
     private static final String LOTTO_SIZE_ERROR = "[ERROR] 로또 번호 갯수는 6개여야 합니다.";
+    private static final String LOTTO_DUPLICATE_ERROR = "[ERROR] 중복되는 숫자는 허용하지 않습니다.";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(LOTTO_SIZE_ERROR);
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> setNumbers = new HashSet<>(numbers);
+        if (setNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(LOTTO_DUPLICATE_ERROR);
         }
     }
 

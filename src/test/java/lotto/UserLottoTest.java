b2862ec8148d2bfe9117409989768eaa30666254
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 01:27:22 +0900

    test: add check lotto bundle to user lotto test

--- src/test/java/lotto/UserLottoTest.java
+++ src/test/java/lotto/UserLottoTest.java
@@ -3,6 +3,8 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
+import java.util.List;
+import lotto.model.Lotto;
 import lotto.model.UserLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,9 +34,24 @@ public class UserLottoTest {
         int lottoCount = 3;
 
         //when
-        UserLotto userLotto = new UserLotto(3000);
+        UserLotto userLotto = new UserLotto(money);
 
         //then
         Assertions.assertThat(userLotto.getLottoCount()).isEqualTo(lottoCount);
+    }
+
+    @DisplayName("구입 금액에 따라 적절한 개수 구매")
+    @Test
+    void checkLottoBundle() {
+        //given
+        int money = 3000;
+        int lottoCount = 3;
+
+        //when
+        UserLotto userLotto = new UserLotto(money);
+
+        //then
+        List<Lotto> lottoBundle = userLotto.getLottoBundle();
+        Assertions.assertThat(lottoBundle.size()).isEqualTo(lottoCount);
     }
 }

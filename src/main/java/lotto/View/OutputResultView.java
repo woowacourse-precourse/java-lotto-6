ac3575a6ec2889be3500cbb572bdc87fe001ed03
ekeprl <ykeon26@naver.com>
2023-11-06 22:28:17 +0900

    Rule에 정리한 메시지,  stream을 이용해 결과를 비교하여 결과를 출력하는 OutputResultView 구성

---
+++ src/main/java/lotto/View/OutputResultView.java
@@ -0,0 +1,33 @@
+package lotto.View;
+
+import lotto.Model.Rate;
+import lotto.Model.Winning;
+import lotto.Model.WinningResult;
+import lotto.Util.Rule;
+
+import java.util.Arrays;
+
+public class OutputResultView {
+    private static final String NEWLINE = "\n";
+
+    public static void printResult(WinningResult winningResult, Rate rate) {
+        System.out.println(Rule.OUTPUTWINNING_MESSAGE);
+        Arrays.stream(Winning.values())
+                .filter(winning -> !winning.equals(Winning.EMPTY))
+                .forEach(winning -> System.out.println(getprintResultWinning(winning, winningResult)));
+        System.out.printf((Rule.OUTPUTWINNING_MESSAGE) + NEWLINE, rate.getRate());
+    }
+
+    private static String getprintResultWinning(Winning winning, WinningResult winningResult) {
+        if (winning == Winning.SECOND) {
+            return String.format(Rule.OUTPUTBONUSMATCH_MESSAGE
+                    , winning.getMatchLottoNumber()
+                    , String.format("%,d", winning.getMoney())
+                    , winningResult.getWinningCount(winning));
+        }
+        return String.format(Rule.OUTPUTBONUSMATCH_MESSAGE
+                , winning.getMatchLottoNumber()
+                , String.format("%,d", winning.getMoney())
+                , winningResult.getWinningCount(winning));
+    }
+}

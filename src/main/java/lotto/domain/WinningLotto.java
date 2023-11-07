8f3a327c2b6fc4c17c76f0530ec2fc7c234b89d6
Lee9Bin <imagen33@daum.net>
2023-11-08 01:52:19 +0900

    feat : 당첨번호와 보너스 번호를 가지는 객체 클래스 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.HashSet;
+
+public class WinningLotto {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+    private static final int WINNING_LOTTO_SIZE = 7;
+
+    private final Lotto winningLotto;
+    private final Integer bonusNumber;
+
+    public WinningLotto(List<Integer> winningLotto, Integer bonusNumber) {
+        this.winningLotto = new Lotto(winningLotto);
+
+        isDuplicateValidate(winningLotto, bonusNumber);
+        isNumberRangeValidate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void isDuplicateValidate(List<Integer> winningLotto, Integer bonusNumber) {
+        HashSet<Integer> numberSet = new HashSet<>(winningLotto);
+        numberSet.add(bonusNumber);
+        if (!(numberSet.size() == WINNING_LOTTO_SIZE)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
+        }
+    }
+
+    private void isNumberRangeValidate(int number) {
+        if (!(MIN_RANGE <= number && number <= MAX_RANGE)) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 범위를 벗어났습니다.");
+        }
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
+}

645333f556ba4a171192b67e80ff2e72bea8ab10
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 08:26:39 +0900

    feat(OutputView): 당첨 통계를 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import static lotto.model.LottoRanking.FIFTH;
+import static lotto.model.LottoRanking.FIRST;
+import static lotto.model.LottoRanking.FORTH;
+import static lotto.model.LottoRanking.SECOND;
+import static lotto.model.LottoRanking.THIRD;
+import static lotto.util.OutputViewMessage.FIFTH_PRINT_MESSAGE;
+import static lotto.util.OutputViewMessage.FIRST_PRINT_MESSAGE;
+import static lotto.util.OutputViewMessage.FORTH_PRINT_MESSAGE;
 import static lotto.util.OutputViewMessage.INPUT_BONUS_NUMBER;
 import static lotto.util.OutputViewMessage.INPUT_PURCHASE_PRICE;
 import static lotto.util.OutputViewMessage.INPUT_WINNING_NUMBERS;
 import static lotto.util.OutputViewMessage.PURCHASE_MESSAGE;
+import static lotto.util.OutputViewMessage.SECOND_PRINT_MESSAGE;
+import static lotto.util.OutputViewMessage.THIRD_PRINT_MESSAGE;
 import static lotto.util.OutputViewMessage.TOTAL_PROFIT_RATE;
 import static lotto.util.OutputViewMessage.WINNING_STATISTICS;
 
@@ -37,5 +47,13 @@ public class OutputView {
 
     public static void printLottoNumbers(List<List<Integer>> lottoNumbers) {
         lottoNumbers.stream().forEach(lottoNumber -> System.out.println(lottoNumber));
+    }
+
+    public static void printLottoStatistics() {
+        System.out.printf(FIFTH_PRINT_MESSAGE, FIFTH.getPrizeMoney(), FIFTH.getCount());
+        System.out.printf(FORTH_PRINT_MESSAGE, FORTH.getPrizeMoney(), FORTH.getCount());
+        System.out.printf(THIRD_PRINT_MESSAGE, THIRD.getPrizeMoney(), THIRD.getCount());
+        System.out.printf(SECOND_PRINT_MESSAGE, SECOND.getPrizeMoney(), SECOND.getCount());
+        System.out.printf(FIRST_PRINT_MESSAGE, FIRST.getPrizeMoney(), FIRST.getCount());
     }
 }

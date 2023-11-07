7a402af4d0440163617cea8346bcbc1b7b6dc288
jiu6525 <jiu6525@daum.net>
2023-11-07 18:01:13 +0900

    feat : 구매 개수를 출력하는 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,17 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.LottoConfig;
 import lotto.exception.LottoException;
 
 public class InputView {
 
     private final LottoException lottoException = new LottoException();
 
+    private final LottoConfig lottoConfig = new LottoConfig();
+
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String NEW_LINE = "\n";
 
     public int purchaseAmountInput() {
@@ -22,6 +26,13 @@ public class InputView {
             System.out.println(NEW_LINE);
             return Integer.parseInt(purchaseAmount);
         }
+    }
+
+    public int lottoAmount() {
+        int purchaseAmount = purchaseAmountInput();
+        int count = purchaseAmount / lottoConfig.TICKET_PRICE;
+        System.out.println(count + PURCHASE_MESSAGE);
+        return count;
     }
 
 }

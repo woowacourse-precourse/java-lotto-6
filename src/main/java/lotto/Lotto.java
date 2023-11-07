18a5ac937b1a3d229b8dfc56206a87aecefcf400
nayonsoso <panda0329@naver.com>
2023-11-08 00:39:18 +0900

    refactor: 로또 번호가 범위를 넘어갈 경우 발생하는 커스텀 예외 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,14 @@
 package lotto;
 
-import java.util.Collection;
+import constants.Constants;
+import exception.lottoException.DuplicateNumberException;
+import exception.lottoException.InvalidLengthException;
+import exception.lottoException.OutOfRangeException;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,8 +19,8 @@ public class Lotto {
     }
 
     private void validateLength(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또는 여섯개의 숫자로 구성되어야 합니다.");
+        if (numbers.size() != Constants.LOTTO_LENGTH) {
+            throw new InvalidLengthException();
         }
     }
 
@@ -24,20 +28,20 @@ public class Lotto {
         long distinctNumberCount = numbers.stream()
                 .distinct()
                 .count();
-        if(distinctNumberCount != numbers.size()){
-            throw new IllegalArgumentException("[ERROR] 로또에는 중복되는 숫자가 있을 수 없습니다.");
+        if (distinctNumberCount != numbers.size()) {
+            throw new DuplicateNumberException();
         }
     }
 
-    private void validateRange(List<Integer> numbers){
-        for(int number : numbers){
-            if(number<1 || number>45){
-                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1부터 45까지 입니다.");
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < Constants.LOTTO_RANGE_START || number > Constants.LOTTO_RANGE_END) {
+                throw new OutOfRangeException();
             }
         }
     }
 
-    private void sort(){
+    private void sort() {
         Collections.sort(numbers);
     }
 }

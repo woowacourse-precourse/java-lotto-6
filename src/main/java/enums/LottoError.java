e67860e0b0aae73492fd43e9040e2a617e148951
KwangHo <skh990427@naver.com>
2023-11-08 00:34:55 +0900

    refactor: 구매 금액 숫자이외값 예외 추가

--- src/main/java/enums/LottoError.java
+++ src/main/java/enums/LottoError.java
@@ -7,7 +7,8 @@ public enum LottoError {
     NUMBER_DUPLICATE_ERROR("로또 당첨 번호는 중복된 값이 입력될 수 없습니다."),
     LOTTO_BUY_MIN_MONEY_ERROR("로또를 구매하기 위한 최소 금액은 1,000원 입니다."),
     LOTTO_BUY_CORRECT_ERROR("로또를 구매하기 위해서는 1,000원의 배수로 입력해야합니다."),
-    LOTTO_BLANK_ERROR("로또 당첨 번호에 공백이 입력될 수 없습니다.");
+    LOTTO_BLANK_ERROR("로또 당첨 번호에 공백이 입력될 수 없습니다."),
+    LOTTO_BUY_FORMAT_ERROR("구매 금액은 숫자로만 이루어져야 합니다.");
 
     private final String ERROR_PREFIX = "[ERROR] ";
     private final String message;

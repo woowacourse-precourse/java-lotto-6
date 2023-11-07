c15930bc5d4fa21d1a1d2ed7ffec1d6ab50b7b70
hoyeon <highoption@naver.com>
2023-11-07 15:35:19 +0900

    test(WinningLottoTest) : 보너스 넘버의 범위 테스트
    - 보너스 넘버는 1부터 45사이여야 한다

---
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+
+    @Test
+    @DisplayName("보너스 넘버의 범위가 1부터 45사이인지 테스트")
+    void createWinningLottoByDuplicateBonusNumber() {
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        String bonus = "46";
+
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+}

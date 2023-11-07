8e686c4c2d382c70f04a34af4e61eec2102e688c
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-07 19:49:17 +0900

    test(GenerateLottoNumbersTest) : 로또 리스트 정렬 테스트 코드 작성

--- src/test/java/lotto/utils/GenerateLottoNumbersTest.java
+++ src/test/java/lotto/utils/GenerateLottoNumbersTest.java
@@ -3,7 +3,12 @@ package lotto.utils;
 import lotto.Constants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class GenerateLottoNumbersTest {
     private static final GenerateLottoNumbers generateLottoNumbers = new GenerateLottoNumbers();
 
@@ -16,5 +21,19 @@ public class GenerateLottoNumbersTest {
         int result = generateLottoNumbers.getBuyingQuantity(buyingPrice);
         // then
         assertThat(result).isEqualTo(buyingPrice / Constants.LOTTO_PRICE);
+    }
+
+    @DisplayName("로또 리스트 정렬 테스트")
+    @Test
+    public void sortedListTest() {
+        // given
+        List<Integer> unsortedList = List.of(10, 1, 6, 25, 3, 15);
+        List<Integer> sortedList = new ArrayList<>();
+        List<Integer> answer = List.of(1, 3, 6, 10, 15, 25);
+
+        // when
+        sortedList = generateLottoNumbers.sortedList(unsortedList);
+        // then
+        assertThat(sortedList).isEqualTo(answer);
     }
 }

0e1c1758de67be3f26cf7332bff02f72f1747738
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-08 01:35:15 +0900

    refactor : EnumMap 으로 받는 대신 RankCount 로 포장

--- src/main/java/lotto/model/strategy/MyLottoStrategy.java
+++ src/main/java/lotto/model/strategy/MyLottoStrategy.java
@@ -4,18 +4,19 @@ import java.util.EnumMap;
 import lotto.model.lotto.BonusNumber;
 import lotto.model.lotto.Lotto;
 import lotto.model.lotto.LottoTicket;
+import lotto.model.lotto.RankCount;
 import lotto.model.lotto.WinningLotto;
 import lotto.model.lotto.LottoRank;
 
 public class MyLottoStrategy implements LottoStrategy {
     @Override
-    public EnumMap<LottoRank, Integer> determineRankCounts(LottoTicket lottoTicket, WinningLotto winningLotto, BonusNumber bonusNumber) {
-        EnumMap<LottoRank, Integer> rankCounts = new EnumMap<>(LottoRank.class);
+    public RankCount determineRankCounts(LottoTicket lottoTicket, WinningLotto winningLotto, BonusNumber bonusNumber) {
+        RankCount rankCounts = new RankCount();
 
         lottoTicket.getLottoTicket()
                 .forEach(lotto -> {
                     LottoRank rank = determineLottoRank(lotto, winningLotto, bonusNumber);
-                    rankCounts.merge(rank, 1, Integer::sum);
+                    rankCounts.incrementCount(rank);
                 });
 
         return rankCounts;

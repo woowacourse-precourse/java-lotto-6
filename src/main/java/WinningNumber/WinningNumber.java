aa6bb858d9990768ff93f1d81ff3f3fafbfa4594
youngduck98 <tommykimyd98@gmail.com>
2023-11-08 01:55:03 +0900

    당첨 번호 class 구현

---
+++ src/main/java/WinningNumber/WinningNumber.java
@@ -0,0 +1,13 @@
+package WinningNumber;
+
+import java.util.List;
+
+public class WinningNumber {
+    private List<Integer> numbers;
+    private Integer bonusNum;
+
+    public WinningNumber(List<Integer> numbers, Integer bonusNum){
+        this.numbers = numbers;
+        this.bonusNum = bonusNum;
+    }
+}

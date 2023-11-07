900e7573efc257dfbbd0565d1503beb2e8fdeb42
koomchang <leahpar0401@gmail.com>
2023-11-07 16:30:35 +0900

    feat: 당첨 결과를 확인 하는 테스트

---
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -0,0 +1,42 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoResultTest {
+
+    private LottoResult lottoResult;
+    private WinningLotto winningLotto;
+    private Integer bonusNumber;
+
+    @BeforeEach
+    void setUp() {
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)), //2등
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)), //3등
+                new Lotto(List.of(1, 2, 3, 4, 25, 26)), // 4등
+                new Lotto(List.of(1, 2, 3, 26, 30, 45)), // 5등
+                new Lotto(List.of(1, 2, 3, 4, 5, 11)), //3등
+                new Lotto(List.of(8, 9, 10, 11, 12, 13))
+        );
+        bonusNumber = 7;
+        winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), bonusNumber);
+        lottoResult = new LottoResult(lottos);
+    }
+
+    @Test
+    @DisplayName("당첨 결과 확인")
+    void checkResult() {
+        Map<Rank, Integer> result = lottoResult.checkResult(winningLotto);
+        assertThat(result.get(Rank.FIRST)).isEqualTo(0);
+        assertThat(result.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(result.get(Rank.THIRD)).isEqualTo(2);
+        assertThat(result.get(Rank.FOURTH)).isEqualTo(1);
+        assertThat(result.get(Rank.FIFTH)).isEqualTo(1);
+    }
+}

b2df965679ab9b31887b3f4b64d9b632f07f24f4
yeondori <yeonseo.jung0@gmail.com>
2023-11-07 23:22:30 +0900

    feat: hasNumber 검증

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,10 +22,22 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
     @DisplayName("로또 번호가 1~45 사이에 없으면 예외가 발생한다.")
     @Test
     void createLottoByOutOfRangeNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("해당 번호가 로또에 있으면 true, 없으면 false를 반환한다.")
+    @Test
+    void hasNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int rightNumber = 1;
+        int wrongNumber = 45;
+
+        assertThat(lotto.hasNumber(rightNumber)).isEqualTo(true);
+        assertThat(lotto.hasNumber(wrongNumber)).isEqualTo(false);
     }
 }

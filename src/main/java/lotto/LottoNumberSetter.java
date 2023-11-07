f03a7a67085890e4a84c02f703377a6695915eaf
isakdevelop <daishi7462@naver.com>
2023-11-08 03:55:38 +0900

    refactor: 클래스 이름 변경 및 함수 분리

--- src/main/java/lotto/LottoNumberSetter.java
+++ src/main/java/lotto/LottoNumberSetter.java
@@ -15,12 +15,10 @@ public class LottoNumberSetter {
             try {
                 String[] tokens = input_Winning_Numbers();
                 winningNumbers.clear(); // 각 시도마다 당첨 번호 초기화
-
                 for (String token : tokens) {
                     int number = Integer.parseInt(token.trim());
                     winningNumbers.add(number);
                 }
-
                 Lotto lotto = new Lotto(winningNumbers);
                 break; // 정상 입력 시 루프 종료
             } catch (IllegalArgumentException e) {

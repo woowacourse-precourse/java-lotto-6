4db45fd6360f04a73d58faaad755c702b7477f17
Ho-Da-Dak <godqhr721@gmail.com>
2023-11-07 18:41:38 +0900

    feat(Application): 로또 프로그램 작동을 위한 기능 구현
    각 기능을 Controller 클래스를 통해 상호작용하게 만들어 두었습니다.
    이 Controller를 실행 시킴으로써 로또 프로그램을 실행합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import View.ConsoleInputView;
+import controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller lottoController = new Controller(new LottoMachine(), new ConsoleInputView());
+        lottoController.run();
     }
 }

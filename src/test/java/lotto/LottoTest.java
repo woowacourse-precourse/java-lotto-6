a2073d25990d12aa8d59ce3822b3da053d32acf9
koomchang <leahpar0401@gmail.com>
2023-11-07 18:07:44 +0900

    test: 로또 번호가 당첨 로또와 비교하여 몇 개의 숫자가 맞았는지 확인하는 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -32,4 +32,18 @@ class LottoTest {
         Lotto lotto = new Lotto(numbers);
         assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
+
+    @DisplayName("로또 번호가 당첨 로또와 비교 하여 몇 개의 숫자가 맞았는지 확인한다.")
+    @Test
+    void getMatchCount() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+        assertThat(lotto.getMatchCount(List.of(1, 2, 3, 4, 5, 6))).isEqualTo(6);
+        assertThat(lotto.getMatchCount(List.of(1, 2, 3, 4, 5, 7))).isEqualTo(5);
+        assertThat(lotto.getMatchCount(List.of(1, 2, 3, 4, 7, 8))).isEqualTo(4);
+        assertThat(lotto.getMatchCount(List.of(1, 2, 3, 7, 8, 9))).isEqualTo(3);
+        assertThat(lotto.getMatchCount(List.of(1, 2, 7, 8, 9, 10))).isEqualTo(2);
+        assertThat(lotto.getMatchCount(List.of(1, 7, 8, 9, 10, 11))).isEqualTo(1);
+        assertThat(lotto.getMatchCount(List.of(7, 8, 9, 10, 11, 12))).isEqualTo(0);
+    }
 }

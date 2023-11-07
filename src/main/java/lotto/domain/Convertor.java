aef681fa3d2efd4f4e8af62a4096b70e053c3a0c
sangchu <didi1484@gmail.com>
2023-11-07 11:04:49 +0900

    fix: 전역 메서드 선언

--- src/main/java/lotto/domain/Convertor.java
+++ src/main/java/lotto/domain/Convertor.java
@@ -8,14 +8,14 @@ import java.util.stream.Collectors;
 public class Convertor {
     private static final String DELIMITER = ",";
 
-    public List<Integer> convertToNumbers(String input) {
+    public static List<Integer> convertToNumbers(String input) {
         return stream(separate(input))
                 .map(number -> new Number(number.trim()))
                 .map(Number::getNumber)
                 .collect(Collectors.toList());
     }
 
-    private String[] separate(String input) {
+    private static String[] separate(String input) {
         return input.split(DELIMITER);
     }
 }

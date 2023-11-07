5430b2c80f4e30c42e7382306b9383de3e9314f9
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 08:48:56 +0900

    feat(ExceptionMessageType): int 범위 넘는 경우 메시지 삭제

--- src/main/java/lotto/enums/ExceptionMessageType.java
+++ src/main/java/lotto/enums/ExceptionMessageType.java
@@ -1,9 +1,8 @@
 package lotto.enums;
 
 public enum ExceptionMessageType {
-    NUMBER_NOT_INTEGER("문자를 제외한 정수만 입력 가능합니다."),
+    NUMBER_NOT_INTEGER("문자를 제외한 2147483647 이하 정수만 가능합니다."),
     PAYMENT_NOT_NATURAL_NUMBER("구입 금액은 0원보다 커야 합니다."),
-    PAYMENT_OUT_OF_INTEGER_RANGE("구입 금액은 21474836470 이하 숫자만 가능합니다."),
     PAYMENT_INVALID_UNIT("구입 금액은 1,000원 단위만 가능합니다."),
     INVALID_SEPARATOR_OR_NUMBER_COUNT("6개의 숫자를 쉼표(,)로 구분하여 입력하세요."),
     NUMBER_OUT_OF_RANGE("번호는 1부터 45 사이의 숫자여야 합니다."),

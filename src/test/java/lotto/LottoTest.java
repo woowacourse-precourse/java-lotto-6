0dc8788b166e96a7511ae3c18fcac548e9e37701
yunju-m <dbswnfnal@naver.com>
2023-11-07 21:00:46 +0900

    Refactor(LottoTest): ✅ 중복되는 배열을 변수로 선언하여 중복성 감소

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private static final List<Integer> ANSWER = List.of(1, 2, 3, 4, 5, 6);
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -18,7 +20,6 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -47,21 +48,21 @@ class LottoTest {
     @DisplayName("보너스 로또 번호가 숫자가 아니면 예외가 발생한다.")
     @Test
     void createBonusNumberLottoMoney() {
-        assertThatThrownBy(() -> new LuckeyLotto(List.of(1, 2, 3, 4, 5, 6), "7a"))
+        assertThatThrownBy(() -> new LuckeyLotto(ANSWER, "7a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createBonusLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new LuckeyLotto(List.of(1, 2, 3, 4, 5, 6), "5"))
+        assertThatThrownBy(() -> new LuckeyLotto(ANSWER, "5"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 로또 번호의 범위가 1부터 45사이가 아니면 예외가 발생한다.")
     @Test
     void createBonusLottoByOverRange() {
-        assertThatThrownBy(() -> new LuckeyLotto(List.of(1, 2, 3, 4, 5, 6), "50"))
+        assertThatThrownBy(() -> new LuckeyLotto(ANSWER, "50"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

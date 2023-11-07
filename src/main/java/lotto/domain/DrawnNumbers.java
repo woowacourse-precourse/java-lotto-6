c9fb6a5da56e34c287eb336a8552e3691ec22b5b
MingyeomKim <mingyum119@naver.com>
2023-11-07 14:12:22 +0900

    feat(DrawnNumbers): 1부터 45까지 범위의 숫자인지 검증
    1부터 45까지의 범위의 숫자인지 검사하는 로직을 작성한다.

--- src/main/java/lotto/domain/DrawnNumbers.java
+++ src/main/java/lotto/domain/DrawnNumbers.java
@@ -1,7 +1,12 @@
 package lotto.domain;
 
+import static lotto.global.constants.NumberType.MAX_LOTTO_NUMBER;
+import static lotto.global.constants.NumberType.MIN_LOTTO_NUMBER;
+
 import java.util.List;
 import lotto.dto.DrawnNumbersDto;
+import lotto.global.exception.ErrorMessage;
+import lotto.global.exception.LottoException;
 
 public class DrawnNumbers {
     private final List<Integer> winningNumbers;
@@ -9,8 +14,23 @@ public class DrawnNumbers {
     private final Integer bonusNumber;
 
     private DrawnNumbers(List<Integer> winningNumbers, Integer bonusNumber) {
+        validateWinningNumbers(winningNumbers);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateWinningNumbers(List<Integer> winningNumbers) {
+        if (isInvalidRange(winningNumbers)) {
+            throw LottoException.from(ErrorMessage.INVALID_RANGE_ERROR);
+        }
+    }
+
+    private boolean isInvalidRange(List<Integer> winningNumbers) {
+        return winningNumbers
+                .stream()
+                .allMatch(number ->
+                        number >= MIN_LOTTO_NUMBER.getValue() && number <= MAX_LOTTO_NUMBER.getValue()
+                );
     }
 
     public static DrawnNumbers from(DrawnNumbersDto drawnNumbersDto) {

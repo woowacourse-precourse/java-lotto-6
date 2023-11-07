25741a78a70ce21e0357b1b6e42e5bf0932f4797
dmong <dt2980_hee@naver.com>
2023-11-08 06:04:56 +0900

    feat: 당첨번호와 보너스 번호를 갖는 WinningInfo 클래스 작성

---
+++ src/main/java/lotto/domain/WinningInfo.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import static lotto.domain.LottoConstants.LOTTO_LENGTH_CONSTRAINT;
+import static lotto.view.ErrorConstants.DUPLICATED_BONUS_NUMBER_ERROR_MESSAGE;
+import static lotto.view.ErrorConstants.DUPLICATED_WINNING_NUMBERS_ERROR_MESSAGE;
+import static lotto.view.ErrorConstants.INVALID_LENGTH_ERROR_MESSAGE;
+
+import java.util.List;
+
+public class WinningInfo {
+    private final List<LottoNumber> winningNumbers;
+    private final LottoNumber bonusNumber;
+
+    private WinningInfo(List<Integer> numbers, int bonusNumber) {
+        validate(numbers, bonusNumber);
+        this.winningNumbers = numbers.stream()
+                .map(LottoNumber::create)
+                .toList();
+        this.bonusNumber = LottoNumber.create(bonusNumber);
+    }
+
+
+    public static WinningInfo create(List<Integer> numbers, int bonusNumber) {
+        return new WinningInfo(numbers, bonusNumber);
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        if (isNotValidLength(numbers)) {
+            throw new IllegalArgumentException(String.format(INVALID_LENGTH_ERROR_MESSAGE.getMessage(),
+                    LOTTO_LENGTH_CONSTRAINT.getValue()));
+        }
+        if (isWinningNumbersDuplicated(numbers)) {
+            throw new IllegalArgumentException(DUPLICATED_WINNING_NUMBERS_ERROR_MESSAGE.getMessage());
+        }
+        if (isBonusNumberDuplicated(numbers, bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATED_BONUS_NUMBER_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private boolean isNotValidLength(List<Integer> numbers) {
+        return numbers.size() != LOTTO_LENGTH_CONSTRAINT.getValue();
+    }
+
+    private boolean isWinningNumbersDuplicated(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
+    private boolean isBonusNumberDuplicated(List<Integer> numbers, int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+
+}

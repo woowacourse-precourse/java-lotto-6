e0e18fa72153def2fd66c5ef1ba05390e85ed61e
hgh1472 <hgh1472@naver.com>
2023-11-07 13:26:15 +0900

    feat : 등수에 해당하는 당첨 개수와 수익을 가지는 WinningRate 객체 추가

---
+++ src/main/java/lotto/model/WinningRate.java
@@ -0,0 +1,54 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.constant.Ranking;
+import lotto.constant.WinningMoney;
+import lotto.controller.WinningCalculator;
+
+public class WinningRate {
+    Map<Integer, Integer> winningCount = new HashMap<>();
+    int profit = 0;
+
+    WinningRate(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        winningCount.put(Ranking.FIRST.getRanking(), 0);
+        winningCount.put(Ranking.SECOND.getRanking(), 0);
+        winningCount.put(Ranking.THIRD.getRanking(), 0);
+        winningCount.put(Ranking.FOURTH.getRanking(), 0);
+        winningCount.put(Ranking.FIFTH.getRanking(), 0);
+        winningCount.put(Ranking.NOT_WIN.getRanking(), 0);
+        countWinning(lottos, winningNumbers.getWinningNumbers(), winningNumbers.getBonusNumber());
+        calculateProfit();
+    }
+
+    public Map<Integer, Integer> getWinningCount() {
+        return this.winningCount;
+    }
+
+    public int getProfit() {
+        return this.profit;
+    }
+
+    public static WinningRate makeWinningRate(List<Lotto> lottos, WinningNumbers winningNumbers) {
+        return new WinningRate(lottos, winningNumbers);
+    }
+
+    private void countWinning(List<Lotto> lottos, Lotto winningNumber, BonusNumber bonusNumber) {
+        WinningCalculator winningCalculator = new WinningCalculator();
+        int rank;
+        for (Lotto lotto : lottos) {
+            rank = winningCalculator.rankWinning(lotto.getLottoNumbers(), winningNumber.getLottoNumbers(),
+                    bonusNumber.getBonusNumber());
+            winningCount.put(rank, winningCount.get(rank) + 1);
+        }
+    }
+
+    private void calculateProfit() {
+        profit += winningCount.get(Ranking.FIRST.getRanking()) * WinningMoney.LOTTO_WIN.getWinningMoney();
+        profit += winningCount.get(Ranking.SECOND.getRanking()) * WinningMoney.SECOND_WIN.getWinningMoney();
+        profit += winningCount.get(Ranking.THIRD.getRanking()) * WinningMoney.THIRD_WIN.getWinningMoney();
+        profit += winningCount.get(Ranking.FOURTH.getRanking()) * WinningMoney.FOURTH_WIN.getWinningMoney();
+        profit += winningCount.get(Ranking.FIFTH.getRanking()) * WinningMoney.FIFTH_WIN.getWinningMoney();
+    }
+}

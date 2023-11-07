0c048fc14aa388c1a0092242cc156bf6d6c4a2c2
seaniiio <cnc4750@naver.com>
2023-11-08 01:54:47 +0900

    feat(InputValidator): 구입 금액이 음수이거나 0인 경우 예외 처리

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -7,12 +7,13 @@ public class InputValidator {
     Constants constants = new Constants();
 
     public void checkAmount(String amountInput) {
-        checkAmountNum(amountInput);
-        checkDividedNum(Integer.parseInt(amountInput));
+        checkAmountNumber(amountInput);
+        checkAmountPositiveNumber(Integer.parseInt(amountInput));
+        checkDividedNumber(Integer.parseInt(amountInput));
     }
 
     //숫자인지 확인
-    private void checkAmountNum(String amountInput) {
+    private void checkAmountNumber(String amountInput) {
         for (int i = 0; i < amountInput.length(); i++) {
             char c = amountInput.charAt(i);
             if (!Character.isDigit(c)) {
@@ -22,15 +23,19 @@ public class InputValidator {
         }
     }
 
+    //양수인지 확인
+    private void checkAmountPositiveNumber(int amountInput) {
+        if(amountInput <= 0) {
+            System.out.printf(constants.LOTTO_PRICE_NOT_POSITIVE_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
     //1000으로 나누어떨어지는지 확인
-    private void checkDividedNum(int amountInput) {
+    private void checkDividedNumber(int amountInput) {
         if(amountInput % 1000 != constants.ZERO_NUMBER) {
             System.out.printf(constants.LOTTO_PRICE_NOT_DIVIDED_ERROR);
             throw new IllegalArgumentException();
         }
     }
-
-
-
-
 }

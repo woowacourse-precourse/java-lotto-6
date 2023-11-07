316fbf952313473c292ad519c390e191b90f3751
KimDongEon <gold3987@naver.com>
2023-11-07 17:39:17 +0900

    test(WinningLottoTest) : 테스트 코드 작성
    WinningLottoTest :  WinningLotto 메소드 관련 테스트 코드 작성
    - createWinningLottoUnderNumber() : 당첨 번호가 1보다 작은게 있으면 예외가 발생합니다.
    - createWinningLottoOverNumber() : 당첨 번호가 45보다 큰게 있으면 예외가 발생합니다.
    - createWinningLottoByOverSize() : 당첨 번호 개수가 6개 초과면 예외가 발생합니다.
    - createWinningLottoByUnderSize() : 당첨 번호의 개수가 6개 미만이면 예외가 발생합니다.
    - validateWinningLottoNumberDuplicated() : 당첨 번호가 중복되면 예외가 발생합니다.
    - validateWinningLottoNumberBonusNumberDuplicated() : 당첨 번호와 보너스 번호가 중복되면 예외가 발생합니다.
    - sortWinningLottoNumbers() : 당첨 번호가 오름차순으로 정렬했는지 확인합니다.

--- src/test/java/model/WinningLottoTest.java
+++ src/test/java/model/WinningLottoTest.java
@@ -9,17 +9,58 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WinningLottoTest {
 
 
-  @DisplayName("로또 번호가 오름차순으로 정렬되는지 확인한다.")
+  @DisplayName("당첨 번호가 1보다 작은게 있으면 예외가 발생한다.")
   @Test
-  void sortNumbers() {
-    List<Integer> numbers = new ArrayList<>(List.of(10, 5, 40, 31, 21, 9));
-    WinningLotto winningLotto = new WinningLotto(numbers, 7);
-    Collections.sort(numbers);
-    assertEquals(numbers, winningLotto.getNumbers());
+  void createWinningLottoUnderNumber() {
+    assertThatThrownBy(() -> new WinningLotto(List.of(0, 2, 3, 4, 5, 6, 7), 8))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("당첨 번호가 45보다 큰게 있으면 예외가 발생한다.")
+  @Test
+  void createWinningLottoOverNumber() {
+    assertThatThrownBy(() -> new WinningLotto(List.of(0, 2, 3, 4, 5, 6, 46), 8))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("당첨 번호의 개수가 6개가 초과면 예외가 발생한다.")
+  @Test
+  void createWinningLottoByOverSize() {
+    assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 8))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("당첨 번호의 개수가 6개가 미만이면 예외가 발생한다.")
+  @Test
+  void createWinningLottoByUnderSize() {
+    assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5), 8))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("당첨 번호가 중복되면 예외가 발생한다.")
+  @Test
+  void validateWinningLottoNumberDuplicated() {
+    assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 7, 7), 8))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다.")
+  @Test
+  void validateWinningLottoNumberBonusNumberDuplicated() {
+    assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 6))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("당첨 번호가 오름차순으로 정렬되는지 확인한다.")
+  @Test
+  void sortWinningLottoNumbers() {
+    WinningLotto winningLotto = new WinningLotto(List.of(10, 5, 40, 31, 21, 9), 7);
+    assertEquals(winningLotto.getNumbers(), List.of(5,9,10,21,31,40));
   }
 }

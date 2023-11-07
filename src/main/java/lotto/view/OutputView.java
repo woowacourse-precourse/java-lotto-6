b8d64153e562e2a3b360669a44562f926cabb6e7
lee jihyeon <lejinhy@gmail.com>
2023-11-07 16:55:49 +0900

    feat: 랜덤 생성한 로또 번호를 출력 시 정렬

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,26 +1,42 @@
 package lotto.view;
 
+import lotto.constant.StatisticsMessage;
 import lotto.domain.Lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class OutputView {
-    private StringBuilder outputMessage = new StringBuilder();
+    private StringBuilder outputMessage;
     public void printPurchasedLotteries(List<Lotto> lotteries) {
-        outputMessage.append(lotteries.size()).append("개를 구매했습니다.").append("\n");
+        List<Integer> lottoTemp;
+        outputMessage = new StringBuilder();
+        outputMessage.append(lotteries.size()).append("개를 구매했습니다.\n");
 
         for (Lotto lotto : lotteries) {
-            outputMessage.append(lotto.getNumbers()).append("\n");
+            lottoTemp = lotto.getNumbers();
+            sortNumbers(lottoTemp);
+            outputMessage.append(lottoTemp).append("\n");
         }
 
         System.out.println(outputMessage);
     }
 
-    public void printWinningStatistics() {
+    public void printWinningStatistics(List<Integer> rank) {
+        System.out.println(StatisticsMessage.STATISTICS_INFO.getMessage());
 
+        List<StatisticsMessage> messages = List.of(StatisticsMessage.values());
+        for (int i = 4; i >= 0; i--) {
+            System.out.println(messages.get(i).getLottoStatisticsDescription(rank.get(i)));
+        }
     }
 
-    public void printProfitPercentage() {
+    public void printProfitPercentage(double profitPercentage) {
+        System.out.println("총 수익률은 " + String.format("%.1f", profitPercentage) + "%입니다.");
+    }
 
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers;
     }
 }

6f7519040c8d4fa81c617aedc2d78246e3fa2cbb
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 02:53:30 +0900

    refactor: modify ConstantMessage

--- src/main/java/lotto/view/constant/ConstantMessage.java
+++ src/main/java/lotto/view/constant/ConstantMessage.java
@@ -1,11 +1,11 @@
 package lotto.view.constant;
 
 public enum ConstantMessage {
-    PAY_MONEY("구입금액을 입력해 주세요."),
-    BUY_LOTTO("%d개를 구매했습니다."),
-    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    WINNING_STATICS("당첨 통계\n---"),
+    PAY_MONEY("구입금액을 입력해 주세요.\n"),
+    BUY_LOTTO("\n%d개를 구매했습니다.\n"),
+    WINNING_NUMBER("\n당첨 번호를 입력해 주세요.\n"),
+    BONUS_NUMBER("\n보너스 번호를 입력해 주세요.\n"),
+    WINNING_STATICS("\n당첨 통계\n---\n"),
     THREE_MATCH("3개 일치 (5,000원) - %d개"),
     FOUR_MATCH("4개 일치 (50,000원) - %d개"),
     FIVE_MATCH("5개 일치 (1,500,000원) - %d개"),

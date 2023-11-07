918b1b8daa1aaaff69170818c2bbf7621732093a
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 20:31:25 +0900

    feat(MakeLotto) : 구입 금액 받아서 로또 리스트 반환하는 기능 완성 - makeLottoList(int lottoCount)

---
+++ src/main/java/lotto/util/MakeLotto.java
@@ -0,0 +1,25 @@
+package lotto.util;
+
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MakeLotto {
+
+    public List<Lotto> makeLottoList(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        while(lottos.size() < lottoCount){
+            lottos.add(generateLotto());
+        }
+
+        return lottos;
+    }
+
+    private Lotto generateLotto(){
+        MakeRandomNumber makeRandomNumber = new MakeRandomNumber();
+        return new Lotto(makeRandomNumber.getRandomNumber());
+    }
+
+
+}

040e4ab0c355a4fd533e8b6a36ffa5952f1d4eb1
suhyun <qkrtngus116@naver,com>
2023-11-07 09:26:53 +0900

    feat(OutputView): 수익률과 당첨내역을 출력하기 위한 메소드를 작성하였습니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -17,6 +17,8 @@ import java.util.List;
  */
 public class OutputView {
     private static String OUTPUT_PURCHASE_AMOUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
+    private static final String SEPARATOR = "---";
 
     public static void printNumberOfLottosPurchased(int numberOfLottosPurchased){
         System.out.println(numberOfLottosPurchased + OUTPUT_PURCHASE_AMOUNT_MESSAGE);
@@ -26,5 +28,15 @@ public class OutputView {
         for(Lotto lotto : lottos){
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public static void printWinningStatisticsHeader() {
+        System.out.println(WINNING_STATISTICS_HEADER);
+        System.out.println(SEPARATOR);
+    }
+
+    public static void printWinningMessage(String winningMessage, String rateOfReturn){
+        System.out.println(winningMessage);
+        System.out.println(rateOfReturn);
     }
 }

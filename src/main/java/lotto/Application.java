513c63d1b8fa228ad00197d0ba44cd3113235971
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-08 03:29:41 +0900

    feat: add controller instance for running lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,13 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.service.LottoService;
+import lotto.view.InputValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController(new LottoService(), new InputView(), new OutputView());
+        LottoController lottoController = new LottoController(new LottoService(), new InputView(new InputValidator()), new OutputView());
         lottoController.run();
     }
 }

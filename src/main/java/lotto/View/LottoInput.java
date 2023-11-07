6a20fa0c0940eaf40c75fb8c7ae6f0b5c81a0c94
ppusda <ppusda1234@gmail.com>
2023-11-07 16:19:15 +0900

    fix: 입출력 정보 수정

--- src/main/java/lotto/View/LottoInput.java
+++ src/main/java/lotto/View/LottoInput.java
@@ -4,18 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 import System.Message;
 
 public class LottoInput {
+
     public String inputPurchaseAmount() {
         System.out.println(Message.INPUT_PURCHASE_MONEY.getMessage());
         return Console.readLine();
     }
 
     public String inputWinningNumber() {
-        System.out.println(Message.INPUT_WINNING_NUMBER.getMessage());
+        System.out.println(
+                Message.LINE_BREAK.getMessage() + Message.INPUT_WINNING_NUMBER.getMessage());
         return Console.readLine();
     }
 
     public String inputBonusNumber() {
-        System.out.println(Message.INPUT_BONUS_NUMBER.getMessage());
+        System.out.println(
+                Message.LINE_BREAK.getMessage() + Message.INPUT_BONUS_NUMBER.getMessage());
         return Console.readLine();
     }
 }

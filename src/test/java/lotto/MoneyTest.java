64017cca271138d8a3630044f53d10533355cf81
meluvrose <tanggu3910@gmail.com>
2023-11-07 20:10:27 +0900

    test: edit testcase

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.constant.Constant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,14 +10,14 @@ class MoneyTest {
     @DisplayName("숫자가 아닌 문자를 입력했다면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Money("1,000"))
+        assertThatThrownBy(() -> new Money("1a2b"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구매금액이 1000원 단위가 아니라면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Money("500"))
+        assertThatThrownBy(() -> new Money("1500"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

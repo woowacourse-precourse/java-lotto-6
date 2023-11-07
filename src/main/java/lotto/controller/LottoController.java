8ff48abbb1487dd82cadbb31d656b82e1b645003
chnbroa <chnbroa011@naver.com>
2023-11-07 17:26:41 +0900

    feat: 구입 금액을 입력 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,31 @@
 package lotto.controller;
 
+import lotto.model.LottoAmount;
 import lotto.view.InputView;
 
 public class LottoController {
     private final InputView inputView;
+    private LottoAmount lottoAmount;
 
     public LottoController() {
         inputView = new InputView();
+
     }
 
     public void start() {
-        inputView.inputAmount();
+        int lottoTicketCount = inputLottoAmount();
+
     }
+
+    public int inputLottoAmount() {
+        while (true) {
+            try {
+                lottoAmount = new LottoAmount(inputView.inputAmount());
+                return lottoAmount.getlottoTicketCount();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
---
+++ src/main/java/lotto/model/LottoAmount.java
@@ -0,0 +1,47 @@
+package lotto.model;
+
+import lotto.view.exception.InvalidDivision;
+import lotto.view.exception.InvalidNumberException;
+import lotto.view.exception.InvalidZeroException;
+
+public class LottoAmount {
+    private static final int ZERO = 0;
+    private static final int THOUSAND = 1000;
+    private final int amount;
+
+    public LottoAmount(String amount) {
+        int number = isInvalidNumber(amount);
+        isZeroInput(number);
+        isNotDivisible(number);
+        this.amount = number;
+    }
+
+    private int isInvalidNumber(String amount) {
+        try {
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            throw new InvalidNumberException();
+        }
+    }
+
+    private void isZeroInput(int amount) {
+        if (amount == ZERO) {
+            throw new InvalidZeroException();
+        }
+    }
+
+    private void isNotDivisible(int amount) {
+        if (amount % THOUSAND != ZERO) {
+            throw new InvalidDivision();
+        }
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getlottoTicketCount() {
+        return (amount / THOUSAND);
+    }
+}

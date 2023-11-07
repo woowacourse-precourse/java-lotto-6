e219ae792417b587d1c0e4a823206c7865af37f0
SungHoCha <tjdgh3725@gmail.com>
2023-11-08 06:35:10 +0900

    Feat "add LottoRank"
    로또당첨결과를 위한 enum객체 구현

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(6, "6개 일치 (2,000,000,000원) - ",2000000000),
+    SECOND(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
+    THIRD(5, "5개 일치 (1,500,000원) - ", 1500000),
+    FOURTH(4, "4개 일치 (50,000원) - ", 50000),
+    FIFTH(3, "3개 일치 (5,000원) - ", 5000),
+    LOSE(0, "", 0);
+
+    private final int count;
+    private final String coment;
+    private final int winningAmount;
+
+    LottoRank(int count, String coment, int winningAmount) {
+        this.count = count;
+        this.coment = coment;
+        this.winningAmount = winningAmount;
+    }
+
+    public static LottoRank from(int count, boolean hasBonus) {
+        if (count >= 0 && count < 3) {
+            return LOSE;
+        }
+        if (count == 5 && hasBonus) {
+            return SECOND;
+        }
+        return Arrays.stream(values())
+                .filter(lottoRank -> !lottoRank.equals(SECOND) && lottoRank.count == count)
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}

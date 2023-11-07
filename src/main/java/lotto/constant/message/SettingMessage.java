99ebc4721a0a1ae8725d66fb50f0975933b25f3b
hyerimmy <heyrim2010@naver.com>
2023-11-08 02:35:43 +0900

    fix: 출력문 오류 수정 - (전) %d개를 구매하였습니다. - (후) %d개를 구매했습니다.

--- src/main/java/lotto/constant/message/SettingMessage.java
+++ src/main/java/lotto/constant/message/SettingMessage.java
@@ -2,7 +2,7 @@ package lotto.constant.message;
 
 public class SettingMessage {
     public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String NOTICE_PURCHASED_TICKET_COUNT = "%d개를 구매하였습니다.";
+    public static final String NOTICE_PURCHASED_TICKET_COUNT = "%d개를 구매했습니다.";
     public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 

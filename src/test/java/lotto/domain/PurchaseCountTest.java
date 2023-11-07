79e73bcb1b4dac17133c7ad99d1332bf097e7edf
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2023-11-08 00:08:10 +0900

    test: PurchaseCountTest 코드 작성

--- src/test/java/lotto/domain/PurchaseCountTest.java
+++ src/test/java/lotto/domain/PurchaseCountTest.java
@@ -1,10 +1,30 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.domain.constants.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PurchaseCountTest {
 
+    @DisplayName("유효한 구매 금액으로 PurchaseCount 생성")
     @Test
-    void getValue() {
+    void createPurchaseCountWithValidPurchaseAmount() {
+        int purchaseAmount = 3000;
+        PurchaseCount purchaseCount = new PurchaseCount(purchaseAmount);
+
+        assertThat(purchaseCount.getValue()).isEqualTo(purchaseAmount / 1000);
+    }
+
+    @DisplayName("유효하지 않은 구매 금액으로 PurchaseCount 생성 시 예외 발생")
+    @Test
+    void createPurchaseCountWithInvalidPurchaseAmount() {
+        int purchaseAmount = 2500;
+
+        assertThatThrownBy(() -> new PurchaseCount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_PURCHASE_AMOUNT.getMessage());
     }
 }

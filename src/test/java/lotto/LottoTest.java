d903c9e60d6ae723eafbe2acb4db3e9d0c66fb78
ssuzyn <asj7090@gmail.com>
2023-11-07 23:21:21 +0900

    :white_check_mark: [test] : 로또 기능 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,27 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.util.Utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -24,5 +33,28 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호의 숫자가 1 ~ 45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 60)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 오름차순으로 정렬되어야 한다.")
+    @Test
+    void createLottoByUnorderedNumbers() {
+        //given
+        List<Integer> case1 = List.of(9, 1, 41, 15, 38, 24);
+
+        //when
+        Lotto result1 = new Lotto(case1);
+
+        List<Integer> naturalOrder = List.of(1, 9, 15, 24, 38, 41);
+        //then
+        assertThat(result1.toString()).contains(Utils.integerListToStringList(naturalOrder));
+    }
+
 }

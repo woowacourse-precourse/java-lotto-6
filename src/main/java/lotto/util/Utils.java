a319a4a3cf0d3e9f4ee9fc9825eece218873f473
Junobee25 <wnsdhqo@naver.com>
2023-11-07 16:51:59 +0900

    feat(Utils): convertToLottoIntegerList 예외 처리 메서드 구현

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -2,7 +2,14 @@ package lotto.util;
 
 import lotto.verification.Validation;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Utils {
+
+
+    private static final String COMMAS = ",";
 
     public static long stringToLong(String string) {
         Validation.verifyStringToLong(string);
@@ -12,6 +19,13 @@ public class Utils {
     public static long rangeInputMoney(long money){
         Validation.verifyRangeInputMoney(money);
         return money;
+    }
+
+    public static List<Integer> convertToLottoIntegerList(String userInput) {
+        Validation.verifyRangeWinningLottoNumber(userInput);
+        return Arrays.stream(userInput.split(COMMAS))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
 }

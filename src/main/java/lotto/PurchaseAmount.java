4c819128c6601acc5ec373f8ca437673636a8884
gyeommm <dkndob02@naver.com>
2023-11-07 15:46:46 +0900

    구입금액 입력 기능 및 관련 예외처리 구현

---
+++ src/main/java/lotto/PurchaseAmount.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class PurchaseAmount {
+    private static int number_of_lottoes;
+
+    private void print_inputAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    private void inputAmount(){
+        String input_amount = readLine();
+
+        int amount;
+        try {
+            amount = Integer.parseInt(input_amount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 숫자가 아닙니다. 1000원 단위의 자연수를 입력해주세요.");
+        }
+
+        if(amount < 0 || amount % 1000 != 0){
+            throw new IllegalArgumentException("구입금액은 1000원 단위의 자연수로 입력해야 합니다.");
+        }
+
+        number_of_lottoes = amount / 1000;
+    }
+
+    private void print_number_of_lottoes(){
+        System.out.println("\n" + number_of_lottoes + "개를 구매했습니다.");
+    }
+}

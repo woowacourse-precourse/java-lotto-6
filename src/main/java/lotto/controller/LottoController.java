a405cb3ac6ea6590b603ae8ca6505facb021eb86
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 16:17:36 +0900

    feat(LottoController): 스켈레톤 코드 작성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,30 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+public class LottoController {
+//    private LottoService lottoService = new LottoService();
+
+    /*
+     * run() {
+     *    구입 금액 입력 및 저장(); savePurchaseAmount();
+     *    랜덤 번호 생성 및 저장(); generateAndSaveRandomNumber();
+     *    당첨 번호 입력 및 저장(); saveWinningNumber();
+     *    보너스 번호 입력 및 저장(); saveBonusNumber();
+     *    내 번호와 당첨 번호 비교 로직 수행(); performNumberComparisonLogic();
+     *    당첨 통계 출력(); printWinningStatistics();
+     * }
+     * */
+
+    public void run() {
+
+    }
+
+    private void savePurchaseAmount() {
+
+    }
+
+    private void generateAndSaveRandomNumber() {}
+    private void saveWinningNumber() {}
+    private void saveBonusNumber() {}
+    private void performNumberComparisonLogic() {}
+    private void printWinningStatistics() {}
 }

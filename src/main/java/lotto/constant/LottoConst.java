05368890b95bc95180fb2f07034aecfb6b6b0461
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-11-08 00:17:18 +0900

    Style : 상수 선언

--- src/main/java/lotto/constant/LottoConst.java
+++ src/main/java/lotto/constant/LottoConst.java
@@ -2,6 +2,8 @@ package lotto.constant;
 
 public class LottoConst {
 
-    public static final String WRONG_NUMBER = "[ERROR] 잘못된 입력입니다.";
+    public static final String IS_DUPLICATE = "중복된 수를 입력하면 안됩니다.";
+    public static final String OVER_RANGE = "수의 범위는 1 ~ 45 입니다.";
+    public static final String IS_NOT_NUMBER ="정수를 입력해주세요";
 
 }

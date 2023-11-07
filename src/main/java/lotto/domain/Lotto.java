1e0f389938f0b5380a15e264117c3ed61f646161
Seol_JY <wlsdud5654@gmail.com>
2023-11-07 19:20:49 +0900

    feat(Lotto): lotto 번호에 대한 검증 기능 구현
    구현 사항:
    - 번호가 6개가 맞는지 검증
    - 번호 간 중복이 없는지 검증
    - 번호가 1~45 범위 이내인지 검증
    - 번호 정렬

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,26 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+
+    private static final String INVALID_SIZE_MESSAGE = "[ERROR] 로또 번호는 %d개여야 합니다.";
+    private static final String DUPLICATE_NUMBER_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다 .";
+    private static final String NUMBER_OUT_OF_RANGE_MESSAGE = "[ERROR] 로또 번호는 %d부터 %d까지어야 합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSizeExact(numbers);
+        validateNoDuplicate(numbers);
+        validateNumbersInRange(numbers);
+        
+        numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
     }
 
@@ -15,9 +28,24 @@ public class Lotto {
         return new Lotto(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSizeExact(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_COUNT) {
+            throw new IllegalArgumentException(String.format(INVALID_SIZE_MESSAGE, LOTTO_COUNT));
+        }
+    }
+
+    private void validateNoDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_COUNT) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_MESSAGE);
+        }
+    }
+
+    private void validateNumbersInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < START_LOTTO_NUMBER || number > END_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(
+                        String.format(NUMBER_OUT_OF_RANGE_MESSAGE, START_LOTTO_NUMBER, END_LOTTO_NUMBER));
+            }
         }
     }
 
--- src/main/java/strategy/QuickpickIssuanceStrategy.java
+++ src/main/java/strategy/QuickpickIssuanceStrategy.java
@@ -5,10 +5,13 @@ import java.util.List;
 import lotto.domain.Lotto;
 
 public class QuickpickIssuanceStrategy implements IssuanceStrategy {
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
 
     @Override
     public Lotto issue() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_LOTTO_NUMBER, END_LOTTO_NUMBER, LOTTO_COUNT);
         return Lotto.from(numbers);
     }
 }

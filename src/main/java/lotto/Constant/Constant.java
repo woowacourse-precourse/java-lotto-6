465b6edff3f62f7876f018196653308155c438a1
EveKristinLee <jylisa0612@naver.com>
2023-11-07 21:15:47 +0900

    feat : 당첨 번호 입력받기 - 6개인지 확인

--- src/main/java/lotto/Constant/Constant.java
+++ src/main/java/lotto/Constant/Constant.java
@@ -8,15 +8,18 @@ public class Constant {
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String NUMBER_PREFIX = "구입금액은 ";
     public static final String BONUS_PREFIX = "보너스 번호는 ";
+    public static final String USER_NUMBER_PREFIX = "당첨 번호는 ";
     public static final String ERROR_NOT_NUMBER_MESSAGE = "숫자여야 합니다.";
     public static final String ERROR_NOT_DIVIDE_MESSAGE = "1,000원으로 나누어 떨어져야 합니다.";
     public static final String ERROR_NOT_BIGGER_MESSAGE = "1,000원 이상이어야 합니다.";
     public static final String ERROR_EMPTY_MESSAGE = "빈칸일 수 없습니다.";
     public static final String ERROR_NOT_BOUNDARY_MESSAGE = "1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_NOT_COUNT_MESSAGE = "6개의 숫자여야 합니다.";
 
     public static final String NUMBER_PATTERN = "[0-9]+";
 
     public static final int LOTTO_DIVIDE_PRICE = 1000;
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_LENGTH = 6;
 }

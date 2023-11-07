d21911c7e24cb0c54fd747d39513208f4d6e06b2
ParkJinYong <mej1406@gmail.com>
2023-11-07 14:48:02 +0900

    feat: 로또 번호 숫자 범위 설정, 번호 발행

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +14,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 6개가 아닙니다.");
+        }
+        else if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 중 중복이 있습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static Lotto generate() {
+        List<Integer> numbers = pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
 }

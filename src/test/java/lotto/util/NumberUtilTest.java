ea9db985444cba602fa310aa7aa3e3f8e5bed616
Minyongggg <naminyong97@gmail.com>
2023-11-08 08:42:54 +0900

    test: 숫자 관련 유틸 테스트 수정

--- src/test/java/lotto/util/NumberUtilTest.java
+++ src/test/java/lotto/util/NumberUtilTest.java
@@ -3,27 +3,17 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class NumberUtilTest {
 
     @Test
     void calculateReturnRatio() {
-        Double initialValue = (double) 8000;
-        Double finalValue = (double) 5000;
+        Float initialValue = (float) 8000;
+        Float finalValue = (float) 5000;
 
-        Double returnRatio = NumberUtil.calculateReturnRatio(initialValue, finalValue);
-        Double expected = 0.625;
+        Float returnRatio = NumberUtil.calculateReturnRatio(initialValue, finalValue);
+        Float expected = 62.5f;
 
         assertThat(returnRatio).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"62.45:62.5", "62.449:62.4", "62.50:62.5", "62.511:62.5"}, delimiter = ':')
-    void roundToTwoDecimalPlaces(Double input, Double expected) {
-        Double converted = NumberUtil.roundToTwoDecimalPlaces(input);
-
-        assertThat(converted).isEqualTo(expected);
     }
 }

f37a78ef69e9dc26b291df220db5f4ff0bac6c51
Dreaming-J <kimjjong321@naver.com>
2023-11-07 16:37:34 +0900

    :sparkles: feat. 당첨번호 및 보너스번호 입력 기능

--- src/main/java/lotto/util/TypeConverter.java
+++ src/main/java/lotto/util/TypeConverter.java
@@ -1,7 +1,11 @@
 package lotto.util;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class TypeConverter {
-    private static final String WRONG_NUMBER_INPUT_MSG = "숫자만 입력해주세요.";
+    private static final String WRONG_NUMBER_INPUT_MSG = "숫자만 입력해주세요. 복수 입력 시 쉼표(,)로 구분해주세요.";
+    private static final String DELIMiTER = ",";
 
     public static int toInt(String input) {
         try {
@@ -9,5 +13,11 @@ public class TypeConverter {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(WRONG_NUMBER_INPUT_MSG);
         }
+    }
+
+    public static List<Integer> toList(String input) {
+        return Stream.of(input.split(DELIMiTER))
+                .map(TypeConverter::toInt)
+                .toList();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,17 +2,35 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.List;
+import lotto.model.AnswerLotto;
 import lotto.model.Token;
 import lotto.util.TypeConverter;
 
 public class InputView {
 
     private static final String READ_AMOUNT_MSG = "구입금액을 입력해 주세요.";
+    private static final String READ_WINNING_NO = "당첨 번호를 입력해 주세요.";
+    private static final String READ_BONUS_NO = "보너스 번호를 입력해 주세요.";
 
     public Token readAmount() {
         System.out.println(READ_AMOUNT_MSG);
         int amount = TypeConverter.toInt(readLine());
         System.out.println();
         return new Token(amount);
+    }
+
+    public AnswerLotto readAnswerLotto() {
+        return new AnswerLotto(readWinningNo(), readBonusNo());
+    }
+
+    private List<Integer> readWinningNo() {
+        System.out.println(READ_WINNING_NO);
+        return TypeConverter.toList(readLine());
+    }
+
+    private int readBonusNo() {
+        System.out.println(READ_BONUS_NO);
+        return TypeConverter.toInt(readLine());
     }
 }

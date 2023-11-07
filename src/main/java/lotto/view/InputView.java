c60a46d151ef30014094f8359b09c70e36334a1f
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-07 23:24:43 +0900

    refactor(InputView): 상수 처리 및 메소드 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,16 +2,21 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
+import lotto.exception.NonNumberException;
+import lotto.exception.lotto.LottoFormatException;
 
 public class InputView {
     private static final Pattern LOTTO_PATTERN = Pattern.compile("^-?\\d+(\\s*,\\s*-?\\d+)*$");
     private static final Pattern NUMBER = Pattern.compile("^-?\\d+$");
+    private static final String INPUT_PRICE_MSG = "구입금액을 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MSG = "당첨 번호를 입력해 주세요.";
 
     private InputView() {
     }
 
     public static String inputMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_PRICE_MSG);
         return readInput();
     }
 
@@ -19,33 +24,39 @@ public class InputView {
         printLottoNumbers();
         final String lottoNumbers = readInput();
         validateLottoNumbers(lottoNumbers);
+        printSpace();
         return lottoNumbers;
     }
 
     public static int inputBonusLottoNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER_MSG);
         final String bonusNumber = readInput();
         validateIsNumeric(bonusNumber);
+        printSpace();
         return Integer.parseInt(bonusNumber);
     }
 
     private static void validateIsNumeric(final String bonusNumber) {
         if (!NUMBER.matcher(bonusNumber).matches()) {
-            throw new IllegalArgumentException("숫자만 입력해 주세요.");
+            throw new NonNumberException();
         }
     }
 
     private static void printLottoNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(INPUT_WINNING_NUMBER_MSG);
     }
 
     private static void validateLottoNumbers(final String lottoNumbers) {
         if (!LOTTO_PATTERN.matcher(lottoNumbers).matches()) {
-            throw new IllegalArgumentException("[1,2,3,4,5,6] 같은 형식으로 숫자만 입력해주세요.");
+            throw new LottoFormatException();
         }
     }
 
     private static String readInput() {
         return Console.readLine();
+    }
+
+    private static void printSpace() {
+        System.out.println();
     }
 }

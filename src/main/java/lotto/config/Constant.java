22e2a294eec2fe0a3779152506712690a5b2944c
JaeHunJang <wuddlaa@gmail.com>
2023-11-07 23:50:20 +0900

    feat(LottoPrize): 로또당첨금 Enum 생성 및 상수 추가

--- src/main/java/lotto/config/Constant.java
+++ src/main/java/lotto/config/Constant.java
@@ -1,7 +1,12 @@
 package lotto.config;
 
 public class Constant {
+    public static String LOTTO_PRIZE_NAME = "MATCH_";
+    public static String LOTTO_PRIZE_BONUS = "BONUS";
+    public static String LOTTO_PRIZE_NON_BONUS = "";
+
     public static String NUMBER_DELIMITER = ",";
+
     public static String PATTERN_NUMBER = "^[\\d]*$";
     public static String PATTERN_NUMBERS = "^[\\d" + NUMBER_DELIMITER + "]*$";
 

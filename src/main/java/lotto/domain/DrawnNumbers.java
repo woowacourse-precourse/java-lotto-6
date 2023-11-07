7d9e6018ff73a4f9879d1d0d4d49bab8f4946c20
MingyeomKim <mingyum119@naver.com>
2023-11-07 14:30:59 +0900

    feat(DrawnNumbers): 중복된 숫자의 입력을 검증하는 로직 구현
    당첨 번호의 중복된 입력을 검증하는 로직을 작성한다.

--- src/main/java/lotto/domain/DrawnNumbers.java
+++ src/main/java/lotto/domain/DrawnNumbers.java
@@ -14,26 +14,49 @@ public class DrawnNumbers {
     private final Integer bonusNumber;
 
     private DrawnNumbers(List<Integer> winningNumbers, Integer bonusNumber) {
-        validateWinningNumbers(winningNumbers);
+        Validator.validateWinningNumbers(winningNumbers);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateWinningNumbers(List<Integer> winningNumbers) {
-        if (isInvalidRange(winningNumbers)) {
-            throw LottoException.from(ErrorMessage.INVALID_RANGE_ERROR);
-        }
-    }
-
-    private boolean isInvalidRange(List<Integer> winningNumbers) {
-        return winningNumbers
-                .stream()
-                .allMatch(number ->
-                        number >= MIN_LOTTO_NUMBER.getValue() && number <= MAX_LOTTO_NUMBER.getValue()
-                );
-    }
-
     public static DrawnNumbers from(DrawnNumbersDto drawnNumbersDto) {
         return new DrawnNumbers(drawnNumbersDto.getWinningNumbers(), drawnNumbersDto.getBonusNumber());
+    }
+
+    private static class Validator {
+        private static void validateWinningNumbers(List<Integer> winningNumbers) {
+            validateInvalidRange(winningNumbers);
+            validateDuplication(winningNumbers);
+        }
+
+        private static void validateDuplication(List<Integer> winningNumbers) {
+            if (isDuplicated(winningNumbers)) {
+                throw LottoException.from(ErrorMessage.DUPLICATED_NUMBER_ERROR);
+            }
+        }
+
+        private static boolean isDuplicated(List<Integer> winningNumbers) {
+            int uniqueSize = getUniqueSize(winningNumbers);
+            return uniqueSize != winningNumbers.size();
+        }
+
+        private static int getUniqueSize(List<Integer> winningNumbers) {
+            return (int) winningNumbers.stream()
+                    .distinct()
+                    .count();
+        }
+
+        private static void validateInvalidRange(List<Integer> winningNumbers) {
+            if (isInvalidRange(winningNumbers)) {
+                throw LottoException.from(ErrorMessage.INVALID_RANGE_ERROR);
+            }
+        }
+
+        private static boolean isInvalidRange(List<Integer> winningNumbers) {
+            return winningNumbers.stream()
+                    .allMatch(number ->
+                            number >= MIN_LOTTO_NUMBER.getValue() && number <= MAX_LOTTO_NUMBER.getValue()
+                    );
+        }
     }
 }
--- src/main/java/lotto/global/exception/ErrorMessage.java
+++ src/main/java/lotto/global/exception/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     NOT_NUMBER_ERROR("숫자를 입력해주세요."),
     INVALID_UNIT_ERROR("%s원 단위의 구입 금액을 입력해주세요."),
     INVALID_SEPARATOR_ERROR("올바르지 않은 구분자 입력입니다."),
-    INVALID_RANGE_ERROR("올바르지 않은 범위의 입력입니다.");
+    INVALID_RANGE_ERROR("올바르지 않은 범위의 입력입니다."),
+    DUPLICATED_NUMBER_ERROR("중복되지 않은 숫자를 입력해주세요.");
 
     private final String message;
 

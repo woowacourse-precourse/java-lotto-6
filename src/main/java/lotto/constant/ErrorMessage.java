9dbc1139a212f5fd6fa065284d6355754980c031
limzzum <chfhddl1234@naver.com>
2023-11-07 19:27:13 +0900

    :sparkles: feat : 에러 메시지 상수 정의

---
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,6 @@
+package lotto.constant;
+
+public class ErrorMessage {
+    public static final String INVALID_RANGE_LOTTO_NUM = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INVALID_DUPLICATE_LOTTO_NUM = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+}

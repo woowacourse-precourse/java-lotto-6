a8eece8f69ff5f62b28f9eefc44c1746d41d491d
PeterChoi <94peterchoi@gmail.com>
2023-11-07 22:14:09 +0900

    feat: 로또 구입 금액 입력받는 기능 구현 후 프로그램 제어 흐름 작성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+import lotto.domain.Price;
+import lotto.view.InputView;
+
+public class LottoController {
+
+    public void run() {
+        Price purchasePrice = InputView.getPurchasePrice();
+
+    }
+
+}

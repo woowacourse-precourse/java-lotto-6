6902c0f7c81877ffbdb321c95a778f22d4be46ac
Hayoon <hy97@sju.ac.kr>
2023-11-07 14:26:25 +0900

    feat(InputView): inputPurchaseAmount() 구입금액을 입력받는 메서드를 구현한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+
+    public int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+
+        String input = readLine();
+        return Integer.parseInt(input);
+    }
 }

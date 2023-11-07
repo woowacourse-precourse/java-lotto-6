996f9b1d492af45289cddf58184854939d5e0a5d
JUNG GYUWON <milestogo0905@naver.com>
2023-11-07 23:55:08 +0900

    refactor: 로또 결과 계산 test 코드 리펙터링

--- src/test/java/lotto/model/ResultTest.java
+++ src/test/java/lotto/model/ResultTest.java
@@ -3,10 +3,13 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Stream;
 import lotto.dto.LottoDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ResultTest {
 
@@ -19,31 +22,31 @@ public class ResultTest {
         bonusNumber = BonusNumber.create("3", winningNumber);
     }
 
-    @DisplayName("6개의 숫자가 다 일치하는 경우 1등이다.")
-    @Test
-    public void 여섯개가_다_일치하는_경우() {
+    @DisplayName("로또의 등수를 계산해준다.")
+    @ParameterizedTest
+    @MethodSource("lottoAndPrizeProvider")
+    public void 로또의_등수를_계산해준다(Integer[] purchasedLotto, Integer prize) {
         //given
-        Lotto lotto = new Lotto(List.of(new Integer[]{34, 45, 1, 26, 8, 29}));
+        Lotto lotto = new Lotto(List.of(purchasedLotto));
         LottoDto lottoDto = LottoDto.create(lotto);
 
         //when
         Result lottoResult = Result.calculate(lottoDto, winningNumber, bonusNumber);
 
         //then
-        assertThat(lottoResult.getPrize()).isEqualTo(1);
+        assertThat(lottoResult.getPrize()).isEqualTo(prize);
     }
 
-    @DisplayName("5개의 숫자가 일치하고 보너스 번호가 일치하는 경우 2등이다.")
-    @Test
-    public void 이등인_경우() {
-        //given
-        Lotto lotto = new Lotto(List.of(new Integer[]{34, 45, 1, 26, 8, 3}));
-        LottoDto lottoDto = LottoDto.create(lotto);
-
-        //when
-        Result lottoResult = Result.calculate(lottoDto, winningNumber, bonusNumber);
-
-        //then
-        assertThat(lottoResult.getPrize()).isEqualTo(2);
+    static Stream<Arguments> lottoAndPrizeProvider() {
+        return Stream.of(
+                Arguments.arguments(new Integer[]{34, 45, 1, 26, 8, 29}, 1), //1등
+                Arguments.arguments(new Integer[]{34, 45, 1, 26, 8, 3}, 2),  // 2등
+                Arguments.arguments(new Integer[]{34, 45, 1, 26, 8, 2}, 3),  // 3등
+                Arguments.arguments(new Integer[]{34, 45, 1, 26, 12, 3}, 4),  // 4등
+                Arguments.arguments(new Integer[]{34, 45, 1, 26, 15, 12}, 4),  //4등
+                Arguments.arguments(new Integer[]{34, 44, 2, 26, 9, 29}, 5),  //5등
+                Arguments.arguments(new Integer[]{34, 44, 2, 26, 3, 29}, 5),  //5등
+                Arguments.arguments(new Integer[]{33, 44, 2, 26, 3, 29}, 0)  //0등
+        );
     }
 }

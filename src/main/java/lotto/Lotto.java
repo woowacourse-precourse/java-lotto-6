9a68dd6070411e38b5b7d9a81856893e939c4ffd
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 00:30:31 +0900

    feat: 입력받은 로또번호 예외처리 - 숫자가 6개가 아닐때

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,7 +12,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 번호를 입력해주세요.");
         }
     }
 

f9746c5056442bf9fcd0ab094a0f33f6e18a88e8
limzzum <chfhddl1234@naver.com>
2023-11-07 20:47:48 +0900

    :sparkles: feat : 로또 머신 투입 금액에 대한 로또 발행 메서드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.constant.Message.LOTTO_PRICE;
+import static lotto.validation.LottoMachineValidation.validate_inputMoney;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,4 +16,20 @@ public class LottoMachine {
     public LottoMachine(){
         lottos = new ArrayList<>();
     }
+
+    public List<Lotto> issuanceLotto(String inputAmount) {
+        this.inputAmount = validate_inputMoney(inputAmount);
+        while (this.inputAmount > 0){
+            createLotto();
+        }
+        return lottos;
+    }
+
+    private void createLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(numbers);
+        lottos.add(lotto);
+        inputAmount -= LOTTO_PRICE;
+    }
+
 }

6643b6933eb9e722dc228818f4720c483a0d452c
Minyongggg <naminyong97@gmail.com>
2023-11-08 03:08:30 +0900

    feat: 정수 리스트를 문자열 리스트로 변환하는 기능 구현

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -1,4 +1,13 @@
 package lotto.util;
 
+import java.util.List;
+import java.util.Objects;
+
 public class StringUtil {
+
+    public static List<String> toStringList(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Objects::toString)
+                .toList();
+    }
 }

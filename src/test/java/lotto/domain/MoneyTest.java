08fd60dedb089d067c9f6f14559f9b569c8005a7
SeongUk52 <rkfka7849@gmail.com>
2023-11-07 19:45:13 +0900

    test(Monet): 수익률 테스트

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import lotto.system.LottoRankConstant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +21,22 @@ public class MoneyTest {
     void createMoneyByNotEnough() {
         assertThatThrownBy(() -> Money.from(0))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매 개수")
+    @Test
+    void countPurchased() {
+        assertThat(Money.from(7000).countPurchase())
+                .isEqualTo(7);
+    }
+
+    @DisplayName("총 수익률")
+    @Test
+    void totalProfit() {
+        assertThat(Money.from(2000).getProfitMarginAsFormat(WinningStats.of(PurchasedLottos.of(List.of(
+                        Lotto.of(List.of(1, 2, 3, 4, 11, 12)),
+                        Lotto.of(List.of(11, 12, 1, 2, 3, 4)))),
+                WinningNumbersWithBonus.of(Lotto.of(List.of(1, 2, 3, 4, 5, 6)), Ball.from(7)))))
+                .isEqualTo((double) (LottoRankConstant.FOURTH_PRIZE.getPrize() * 2) / 2000 * 100);
     }
 }

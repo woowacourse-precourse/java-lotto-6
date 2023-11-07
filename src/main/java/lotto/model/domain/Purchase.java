58c69c66f1bb6d64d5e471654e1d9671f7e606d4
D0ri123 <lsh81550@gmail.com>
2023-11-08 02:18:32 +0900

    refactor(Purchase): PurchaseValidator와 Purchase를 합침 - 4차 구현

--- src/main/java/lotto/model/domain/Purchase.java
+++ src/main/java/lotto/model/domain/Purchase.java
@@ -1,12 +1,31 @@
 package lotto.model.domain;
 
+import static lotto.util.Constants.PURCHASE_PRICE_UNIT;
+import static lotto.util.ExceptionMessage.PURCHASE_INVALID_TYPE;
+import static lotto.util.ExceptionMessage.PURCHASE_INVALID_UNIT;
+
 public class Purchase {
     private final Integer price;
     private final Integer ticketCount;
 
-    public Purchase(int price, int ticketCount) {
-        this.price = price;
-        this.ticketCount = ticketCount;
+    public Purchase(String priceInput) {
+        this.price = validateIntegerInput(priceInput);
+        this.ticketCount = validatePriceInThousandUnit(price);
+    }
+
+    private int validateIntegerInput(String price) {
+        try {
+            return Integer.parseInt(price);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PURCHASE_INVALID_TYPE.getMessage());
+        }
+    }
+
+    private int validatePriceInThousandUnit(int price) {
+        if(price % PURCHASE_PRICE_UNIT != 0) {
+            throw new IllegalArgumentException(PURCHASE_INVALID_UNIT.format(PURCHASE_PRICE_UNIT));
+        }
+        return price/PURCHASE_PRICE_UNIT;
     }
 
     public Integer getTicketCount() {
--- src/main/java/lotto/service/PurchaseService.java
+++ src/main/java/lotto/service/PurchaseService.java
@@ -1,16 +1,12 @@
 package lotto.service;
 
 import lotto.model.domain.Purchase;
-import lotto.model.domain.PurchaseValidator;
 
 public class PurchaseService {
-    private final PurchaseValidator validator = new PurchaseValidator();
 
     public Purchase getPurchaseIfValid(String input) {
         try {
-            int priceInteger = validator.validateIntegerInput(input);
-            int lottoCount = validator.validatePriceInThousandUnit(priceInteger);
-            return new Purchase(priceInteger, lottoCount);
+            return new Purchase(input);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }

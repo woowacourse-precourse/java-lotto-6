cec32f719111672497eb36f141a6e35b1fb1062b
anjdydhody <a01066444534@gmail.com>
2023-11-08 01:51:55 +0900

    feat: Add Bonus constructor and validate method

--- src/main/java/lotto/element/Bonus.java
+++ src/main/java/lotto/element/Bonus.java
@@ -1,4 +1,16 @@
 package lotto.element;
 
 public class Bonus {
+    private int number;
+
+    Bonus(int number, Lotto lotto) {
+        validate(number, lotto);
+        this.number = number;
+    }
+
+    void validate(int num, Lotto lotto) {
+        if (lotto.getLottoNumbers().contains(num)) {
+            throw new IllegalArgumentException("번호가 중복됩니다.");
+        }
+    }
 }

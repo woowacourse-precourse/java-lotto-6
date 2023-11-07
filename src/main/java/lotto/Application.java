92d04a2348b9a520fa55a77638479da3726f1eda
Yeop <syt05342@naver.com>
2023-11-07 22:44:11 +0900

    feat: 애플리케이션 실행 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService();
+
+        try {
+            LottoController lottoController = new LottoController(lottoService);
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }

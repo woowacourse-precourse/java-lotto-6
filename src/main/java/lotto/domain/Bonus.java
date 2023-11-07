e4208c7451ae9b559badcee478066d15051c30bf
jiohh <dkswlgh99@naver.com>
2023-11-07 23:35:06 +0900

    feat(domain): BonusNumber 자료구조 구현

---
+++ src/main/java/lotto/domain/Bonus.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import lotto.validation.BonusNumberValidator;
+
+public class Bonus {
+    private final int number;
+
+    public Bonus(Integer number, Lotto winningLotto) {
+        BonusNumberValidator.validateBonusNumber(number, winningLotto);
+        this.number = number;
+    }
+}

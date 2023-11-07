7c5ca09746b4407745259c52135d70cd8a0c679e
권장순 <jsoonworld@gmail.com>
2023-11-08 00:34:32 +0900

    feat( LottoMachine#generateTicket() ) : 숫자가 담긴 티켓을 생성하는 메서드를 구현했고 메서드 명을 정정함.

--- src/main/java/lotto/v3/model/LottoMachine.java
+++ src/main/java/lotto/v3/model/LottoMachine.java
@@ -3,16 +3,25 @@ package lotto.v3.model;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.v3.util.LottoConstants;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    public Lotto generateLotto() {
+    public Lotto generateLottoNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 LottoConstants.LOTTO_NUMBER_MIN.getValue(),
                 LottoConstants.LOTTO_NUMBER_MAX.getValue(),
                 LottoConstants.LOTTO_NUMBERS_SIZE.getValue()
         );
         return new Lotto(numbers);
+    }
+
+    public List<Lotto> generateTicket(int count) {
+        List<Lotto> ticket = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            ticket.add(generateLottoNumbers());
+        }
+        return ticket;
     }
 
 }

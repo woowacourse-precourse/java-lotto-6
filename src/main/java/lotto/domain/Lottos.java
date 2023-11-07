c40ffff0350319d1e4cee1d38a867af1b8b70a9c
jooda00 <dragon2000a@gmail.com>
2023-11-07 12:06:43 +0900

    feat(Lottos): Map 사용해서 당첨 등급과 개수 받아서 계산

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -9,6 +11,7 @@ public class Lottos {
     private final int bonusNumber;
     private Player player;
     private LottoResult lottoResult = LottoResult.NONE;
+    private Map<LottoResult, Integer> lottoResultWithCount = new HashMap<>();
     private int total = 0;
 
     public Lottos(List<Integer> winningNumber, int bonusNumber, Player player) {
@@ -17,7 +20,7 @@ public class Lottos {
         this.player = player;
     }
 
-    public int classifyLottoGrade() {
+    public void classifyLottoGrade() {
         for (int i = 0; i < player.getLotteries().size(); i++) {
             List<Integer> result = calculateMatchNumbers(i);
             int size = result.size();
@@ -25,15 +28,22 @@ public class Lottos {
                 size += 5;
             }
             this.lottoResult = LottoResult.initiateLottoResult(size);
-            System.out.println(size);
+            addCount(lottoResult);
+            total += LottoResult.calculateRate(lottoResultWithCount, lottoResult);
         }
-        return this.total;
+        System.out.println(total);
+    }
+
+    private void addCount(LottoResult lottoResult) {
+        lottoResultWithCount.put(lottoResult, lottoResultWithCount.getOrDefault(lottoResult, 0) + 1);
     }
 
     private List<Integer> calculateMatchNumbers(int index) {
         return winningNumber.stream()
                 .filter(num -> player.getLotteries()
-                .get(index).getNumbers().stream()
+                .get(index)
+                        .getNumbers()
+                        .stream()
                 .anyMatch(Predicate.isEqual(num)))
                 .collect(Collectors.toList());
     }

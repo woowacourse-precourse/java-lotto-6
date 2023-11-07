a6b5dffb89d4c385206f63c417030c115e9058f3
MeoungHum <emmabark84@gmail.com>
2023-11-07 10:54:23 +0900

    구현할 기능 목록

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,6 +15,4 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }

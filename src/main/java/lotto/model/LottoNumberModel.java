f862725b90ba69c1b3652715cbfb015f13a9e0ff
HSBODo <vinsulill@gmaile.com>
2023-11-07 15:55:48 +0900

    refactor : lottoNumberDisplay() 메서드 추가 및 lottoQuantity 변수 추가

--- src/main/java/lotto/model/LottoNumberModel.java
+++ src/main/java/lotto/model/LottoNumberModel.java
@@ -6,14 +6,20 @@ import java.util.List;
 
 public class LottoNumberModel {
     private List<Lotto> lotto;
+    private int lottoQuantity;
 
     public LottoNumberModel(List<Lotto> lotto) {
         this.lotto = lotto;
+        this.lottoQuantity = lotto.size();
     }
 
-    public List<Lotto> getLotto() {
-        return lotto;
+    public int getLottoQuantity() {
+        return lottoQuantity;
     }
 
-
+    public void lottoNumberDisplay(){
+        for (int quantity = 1 ; quantity <= lottoQuantity ; quantity++){
+            lotto.get(quantity-1).lottoNumberDisplay();
+        }
+    }
 }

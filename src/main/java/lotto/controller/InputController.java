3b853d50283865ef55ae7cd920d6e3582c8c5981
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 03:46:48 +0900

    feat(InputController) : 입력 관련 컨트롤러단 기능 구현 -getPurchase() -getWinnerNumber() -getBonusNumner()

---
+++ src/main/java/lotto/controller/InputController.java
@@ -0,0 +1,52 @@
+package lotto.controller;
+
+import lotto.domain.BonusNumber;
+import lotto.domain.LottoPurchaseAmount;
+import lotto.domain.WinnerNumber;
+import lotto.view.InputViewBonusNumber;
+import lotto.view.InputViewLottoPurchaseAmount;
+import lotto.view.InputViewWinnerNumber;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class InputController {
+
+    private LottoPurchaseAmount lottoPurchaseAmount;
+    private BonusNumber bonusNumber;
+    private WinnerNumber winnerNumber;
+
+    public int getPurchaseAmount() {
+        try {
+            String purchase = InputViewLottoPurchaseAmount.getPurchaseAmount();
+            lottoPurchaseAmount = new LottoPurchaseAmount(purchase);
+            return lottoPurchaseAmount.getLottoCount();
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getPurchaseAmount();
+        }
+    }
+
+    public List<Integer> getWinnerNumber(){
+        try{
+            List<Integer> winnerNum = InputViewWinnerNumber.getWinnerNumber();
+            winnerNumber = new WinnerNumber(winnerNum);
+            return winnerNumber.getWinnerNumber();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getWinnerNumber();
+        }
+    }
+
+    public int getBonusNumber(){
+        try{
+            String bonusNum = InputViewBonusNumber.getBonusNumber();
+            bonusNumber = new BonusNumber(bonusNum,winnerNumber.getWinnerNumber());
+            return bonusNumber.getBonusNumber();
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getBonusNumber();
+        }
+    }
+
+}

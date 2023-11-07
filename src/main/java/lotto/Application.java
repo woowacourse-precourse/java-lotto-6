c492eac2aa426e3a4a9796337db0a0cbfc0d9c4f
ljt528 <ljt528@naver.com>
2023-11-07 21:51:55 +0900

    Feat : 구입 금액 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구입 금액 입력
+        double money = 0;
+        System.out.println("구입금액을 입력해 주세요.");
+        while (true) {
+            try {
+                money = Integer.parseInt(Console.readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자만 입력해주세요.");
+            }
+        }
+        System.out.println();
     }
 }

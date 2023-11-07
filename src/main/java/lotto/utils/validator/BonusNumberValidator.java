d0884979f12c53495498e235a810396b7c1ba748
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 20:03:19 +0900

    feat(BonusNumberValidator): 입력 가능 최대 길이를 검증하는 기능

--- src/main/java/lotto/utils/validator/BonusNumberValidator.java
+++ src/main/java/lotto/utils/validator/BonusNumberValidator.java
@@ -4,13 +4,25 @@ import lotto.utils.message.WinningInformationExceptionMessage;
 
 public class BonusNumberValidator {
 
+    private static final int MAX_LENGTH = 2;
+    private static final int MIN_BONUS_NUMBER = 1;
+    private static final int MAX_BONUS_NUMBER = 45;
+
     public static void validate(String target) {
         validateBlank(target);
+        validateMaxLength(target);
     }
 
     private static void validateBlank(String target) {
         if (target == null || target.isBlank()) {
             throw new IllegalArgumentException(WinningInformationExceptionMessage.BLANK.getError());
+        }
+    }
+
+    private static void validateMaxLength(String target) {
+        if (target == null || target.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    WinningInformationExceptionMessage.EXCEED_BONUS_NUMBER_LENGTH.getError());
         }
     }
 }

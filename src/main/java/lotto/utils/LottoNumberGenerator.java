50e0d2e7e1deee97e3e64976a0bf945996874101
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-07 20:17:31 +0900

    refactor: LottoNumberGenerator getRandomNumber로직 정렬 로직 제거

--- src/main/java/lotto/utils/LottoNumberGenerator.java
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -3,14 +3,12 @@ package lotto.utils;
 import static lotto.enums.Constants.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
 
     public static String getRandomNumber(){
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_INCLUSIVE.getValue(), MAX_INCLUSIVE.getValue(), LOTTO_NUMBER_SIZE.getValue());
-        Collections.sort(lottoNumbers);
         return lottoNumbers.toString().replaceAll("[\\['\\]]","");
     }
 }

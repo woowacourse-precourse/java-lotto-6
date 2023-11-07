330e60c82c615c7c645014f4aba41a41f31b28b4
Minyongggg <naminyong97@gmail.com>
2023-11-08 03:34:01 +0900

    feat: 로또 구입 금액 값의 유효성을 검증하는 기능 구현

--- src/main/java/lotto/constant/ExceptionMessages.java
+++ src/main/java/lotto/constant/ExceptionMessages.java
@@ -1,9 +1,13 @@
 package lotto.constant;
 
+import static lotto.constant.GameOptions.LOTTO_PRICE;
+
 public enum ExceptionMessages {
     PURCHASE_AMOUNT_FORMAT_EXCEPTION_MESSAGE("구입금액은 양의 정수여야 합니다."),
     WINNING_NUMBER_FORMAT_EXCEPTION_MESSAGE("당첨 번호는 ,로 구분된 양의 정수의 배열이어야 합니다."),
-    BONUS_NUMBER_FORMAT_EXCEPTION_MESSAGE("보너스 번호는 양의 정수여야 합니다.");
+    BONUS_NUMBER_FORMAT_EXCEPTION_MESSAGE("보너스 번호는 양의 정수여야 합니다."),
+    MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE("로또 구입 금액은 " + LOTTO_PRICE + " 이상이어야 합니다."),
+    PURCHASE_AMOUNT_DIVISIBILITY_EXCEPTION_MESSAGE("로또 구입 금액은 " + LOTTO_PRICE + "로 나누어 떨어져야 합니다."),
 
 
     private String value;
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import static lotto.constant.ExceptionMessages.MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE;
+import static lotto.constant.GameOptions.LOTTO_PRICE;
+
 import java.util.List;
 
 public class Validator {
@@ -15,6 +18,23 @@ public class Validator {
             if (number < 1) {
                 throw new IllegalStateException();
             }
+        }
+    }
+
+    public static void validatePurchaseAmount(Integer purchaseAmount) {
+        validateOverMinimum(purchaseAmount);
+        validateDivisibility(purchaseAmount);
+    }
+
+    private static void validateDivisibility(Integer purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException(MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE.toString());
+        }
+    }
+
+    private static void validateOverMinimum(Integer purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE.getValue()) {
+            throw new IllegalArgumentException(MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE.toString());
         }
     }
 }

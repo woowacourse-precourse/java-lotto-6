fd418e6846ebd0765c5f83104cf2b7b46673a5a9
Zeus6768 <akaps6768@gmail.com>
2023-11-07 15:41:48 +0900

    fix: 로또 구매 한도 100만원 -> 10만원 변경

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -1,13 +1,17 @@
 package lotto.model;
 
+import static lotto.model.Lotto.LOTTO_PRICE;
+
 public class PurchaseAmount {
 
-    public static final int LOTTO_PRICE = 1_000;
-    private static final int MAX_AMOUNT = 1_000_000;
+    private static final int MAX_AMOUNT = 100_000;
     private static final int NO_REMAINDER = 0;
 
     private static final String INVALID_PURCHASE_AMOUNT =
-            LOTTO_PRICE + "원 단위의 숫자를 입력하세요. 최소" + LOTTO_PRICE + "원, 최대 10만 원을 구입할 수 있습니다.";
+            LOTTO_PRICE + "원 단위의 숫자를 입력하세요. "
+                    + "최소 " + LOTTO_PRICE + "원, "
+                    + "최대 " + MAX_AMOUNT + "원"
+                    + "을 구입할 수 있습니다.";
 
     private final int amount;
 

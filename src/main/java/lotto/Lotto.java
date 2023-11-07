6503bfc2223f8d2d0b78cfa183dbbc992e47e774
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-07 16:25:49 +0900

    chore: refine winning number input validation

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,36 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers = new ArrayList<>();
+    private static final String WRONG_NUMBER_FORMAT = "잘못된 형식입니다. 숫자 구분은 오직 ,(comma)로만 해주세요.";
+    private static final String NOT_SIX_NUMBERS = "6개의 숫자를 입력(생성)해주세요.";
+    private static final String DOUBLE_ELEMENT_EXCEPTION = "중복된 원소가 존재합니다.";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
+    public Lotto(String numberCommand) {
+        List<String> commaSeparatedNumbers = List.of(numberCommand.split(","));
+        List<Integer> shiftedNumbers = new ArrayList<>();
+        for (String numberLiteral: commaSeparatedNumbers) {
+            try {
+                shiftedNumbers.add(Integer.parseInt(numberLiteral));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(WRONG_NUMBER_FORMAT);
+            }
+        }
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_SIX_NUMBERS);
         }
         validateUnique(numbers);
     }
@@ -23,15 +39,15 @@ public class Lotto {
         Set<Integer> uniqueNumbers = new HashSet<>();
         for (int element : numbers) {
             if (!uniqueNumbers.add(element)) {
-                throw new IllegalArgumentException("중복된 원소가 존재합니다.");
+                throw new IllegalArgumentException(DOUBLE_ELEMENT_EXCEPTION);
             }
         }
     }
 
-    public int countMatch(List<Integer> targetNumbers) {
+    public int countMatch(Lotto targetNumbers) {
         int countNumberMatching = 0;
         for (int i = 0; i < numbers.size(); i++) {
-            if (targetNumbers.get(i) == numbers.get(i))
+            if (targetNumbers.numbers.get(i) == numbers.get(i))
                 countNumberMatching++;
         }
         return countNumberMatching;

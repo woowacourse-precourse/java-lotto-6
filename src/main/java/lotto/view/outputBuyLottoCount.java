225fbe719714ea0d09fd8b68ee05972994e4933e
의찬 <srja432130@gmail.com>
2023-11-07 19:45:23 +0900

    refactor: 파라미터를 객체로 수정

--- src/main/java/lotto/view/outputBuyLottoCount.java
+++ src/main/java/lotto/view/outputBuyLottoCount.java
@@ -1,9 +1,9 @@
 package lotto.view;
-
+import lotto.domain.Money;
 public class outputBuyLottoCount {
     private static final String OUTPUT_MESSAGE = "%d개를 구매했습니다.";
 
-    public static void printBuyLotto(int buyLottoCount) {
-        System.out.printf((OUTPUT_MESSAGE) + "%n", buyLottoCount);
+    public static void printBuyLotto(Money money) {
+        System.out.printf((OUTPUT_MESSAGE) + "%n", money.getTicket());
     }
 }

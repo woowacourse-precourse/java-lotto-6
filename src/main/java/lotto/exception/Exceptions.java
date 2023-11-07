033138e13cc642b4b4e76a3d1566984f3f944ee2
seonghun <sdzx0719@naver.com>
2023-11-07 21:19:08 +0900

    refactor(exception): 에러 메세지 수정

--- src/main/java/lotto/exception/Exceptions.java
+++ src/main/java/lotto/exception/Exceptions.java
@@ -10,7 +10,7 @@ public class Exceptions {
     public void isInvalidNumberOfLotteries(String input) {
         String[] lotteries = input.split(",");
         if (lotteries.length != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개를 입력해주셔야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개를 입력해주셔야 합니다.");
         }
     }
 
@@ -18,16 +18,16 @@ public class Exceptions {
         try {
             int bonusNumber = Integer.parseInt(bonus);
             if (bonusNumber <= 1 || bonusNumber >= 45) {
-                throw new IllegalArgumentException("보너스 번호의 값은 1~45까지 입력 가능합니다.");
+                throw new IllegalArgumentException("[ERROR] 보너스 번호의 값은 1~45까지 입력 가능합니다.");
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 숫자 형식입니다.");
         }
     }
 
     public void isInvalidNullNumber(String number) {
         if (number.contains(" ") || number.contains(",,")) {
-            throw new IllegalArgumentException("이름은 공백이거나 null값이 될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 이름은 공백이거나 null값이 될 수 없습니다.");
         }
     }
 }

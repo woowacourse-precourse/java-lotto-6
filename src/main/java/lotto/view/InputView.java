413e5b1aa43af1cd001223cf8fc8cae516cced37
SubinHeo0 <anmedi3623@naver.com>
2023-11-08 07:48:28 +0900

    feat: 입력뷰 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String receiveValue() {
+        String money = Console.readLine();
+        return money;
+    }
+
+}

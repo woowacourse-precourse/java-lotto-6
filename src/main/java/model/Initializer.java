8fce1b4830710287a651783d8962f03c0dc88312
nayonsoso <panda0329@naver.com>
2023-11-08 02:33:15 +0900

    feat: 당첨 로또 초기화 기능 구현

--- src/main/java/model/Initializer.java
+++ src/main/java/model/Initializer.java
@@ -2,14 +2,28 @@ package model;
 
 import constants.Constants;
 import exception.InvalidWinningLottoFormatException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import lotto.Lotto;
 
 public class Initializer {
+    public Lotto initWinningLotto(String userInput){
+        validateFormat(userInput);
+        List<Integer> winningNumbers = convertToList(userInput);
+        return new Lotto(winningNumbers);
+    }
 
     private void validateFormat(String userInput) {
         String regex = "^([0-9],){" + (Constants.LOTTO_LENGTH - 1) + "}[0-9]$";
         if (!Pattern.matches(userInput, regex)) {
             throw new InvalidWinningLottoFormatException();
         }
+    }
+
+    private List<Integer> convertToList(String userInput){
+        return Arrays.stream(userInput.split(Constants.DELIMITER))
+                .map(Integer::parseInt)
+                .toList();
     }
 }

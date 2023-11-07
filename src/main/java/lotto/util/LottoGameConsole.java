f2b2a7b738b610a8c40232b7bbd7f5e5ef87ac9e
ASak1104 <khw56184@gmail.com>
2023-11-08 08:25:41 +0900

    fix: 당첨 로또 입력 코드 수정

--- src/main/java/lotto/util/LottoGameConsole.java
+++ src/main/java/lotto/util/LottoGameConsole.java
@@ -1,12 +1,13 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lotto.constant.LottoNumber;
 import lotto.model.Lotto;
 
 public final class LottoGameConsole {
+    private static final String SPLIT_REGEX = ",";
     private static final int COST_UNIT = 1_000;
 
     private LottoGameConsole() {
@@ -23,14 +24,8 @@ public final class LottoGameConsole {
     }
 
     public static Lotto readTargetLotto() {
-        List<Integer> numbers = new ArrayList<>();
-
-        for (int i = 0; i < LottoNumber.COUNT; i++) {
-            String input = Console.readLine();
-            int number = Integer.parseInt(input);
-
-            numbers.add(number);
-        }
+        String input = Console.readLine();
+        List<Integer> numbers = convertToIntegerList(input);
 
         return new Lotto(numbers);
     }
@@ -48,6 +43,12 @@ public final class LottoGameConsole {
         if (cost < COST_UNIT || cost % COST_UNIT != 0) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static List<Integer> convertToIntegerList(String input) {
+        return Arrays.stream(input.split(SPLIT_REGEX))
+                .map(Integer::parseInt)
+                .toList();
     }
 
     private static void validateBonusNumber(int bonusNumber) {

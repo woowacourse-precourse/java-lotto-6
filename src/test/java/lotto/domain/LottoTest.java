e0a4c348f8be6e2f5a1009ce37c91229377cd458
Uniaut <kunwoo0927@gmail.com>
2023-11-07 18:10:21 +0900

    test(domain.Lotto): add test of Lotto
    LottoTest
        add test: lotto number is stored sorted

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -27,5 +28,13 @@ class LottoTest {
     void createLottoByNumberOutOfRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 정렬된 상태로 저장된다.")
+    @Test
+    void createLottoByUnsortedNumber() {
+        Lotto lotto = new Lotto(List.of(5, 4, 3, 2, 1, 6));
+        assertThat(lotto.getNumbers())
+                .containsExactly(1, 2, 3, 4, 5, 6);
     }
 }

17216f969ae4388472d3911b75ff7d5df60f8203
Minyongggg <naminyong97@gmail.com>
2023-11-08 02:20:47 +0900

    feat: 구입금액, 당첨 번호, 보너스 번호를 입력받는 기능 구현

--- src/main/java/lotto/ui/ConsoleScanner.java
+++ src/main/java/lotto/ui/ConsoleScanner.java
@@ -1,4 +1,18 @@
 package lotto.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class ConsoleScanner {
+
+    public static String scanPurchaseAmount() {
+        return Console.readLine();
+    }
+
+    public static String scanWinningNumber() {
+        return Console.readLine();
+    }
+
+    public static String scanBonusNumber() {
+        return Console.readLine();
+    }
 }

666267ee7cea2b2687beacbafc6ff8011af1e80d
hoyeon <highoption@naver.com>
2023-11-07 22:19:44 +0900

    style(Lotto) : 가독성을 위해 상수 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private final int MAX_LOTTO_SIZE = 6;
+    private final String ERROR_NOT_CORRECT_SIZE = "[ERROR] 로또 넘버는 6개여야 합니다.";
+    private final int START_LOTTO_NUMBER = 1;
+    private final int END_LOTTO_NUMBER = 45;
+    private final String ERROR_NOT_CORRECT_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private final String ERROR_NOT_DUPLICATE = "[ERROR] 로또 번호는 중복이 있으면 안됩니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -23,25 +29,25 @@ public class Lotto {
         isDuplicate(numbers);
     }
 
-    private static void isCorrectSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 넘버는 6개여야 합니다.");
+    private void isCorrectSize(List<Integer> numbers) {
+        if (numbers.size() != MAX_LOTTO_SIZE) {
+            throw new IllegalArgumentException(ERROR_NOT_CORRECT_SIZE);
         }
     }
 
-    private static void isCorrectRange(List<Integer> numbers) {
+    private void isCorrectRange(List<Integer> numbers) {
         numbers.forEach(number -> {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            if (number < START_LOTTO_NUMBER || number > END_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(ERROR_NOT_CORRECT_RANGE);
             }
         });
     }
 
-    private static void isDuplicate(List<Integer> numbers) {
+    private void isDuplicate(List<Integer> numbers) {
         List<Integer> duplicate = new ArrayList<>();
         for (Integer number : numbers) {
             if (duplicate.contains(number)) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 있으면 안됩니다.");
+                throw new IllegalArgumentException(ERROR_NOT_DUPLICATE);
             }
             duplicate.add(number);
         }

b810f9ec0500a9b0ab3c1aeb750bb9c390e28742
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 08:19:39 +0900

    feat: 에러 메세지를 빨간색으로 변경

---
+++ src/main/java/lotto/constant/TextColor.java
@@ -0,0 +1,6 @@
+package lotto.constant;
+
+public class TextColor {
+    public static final String ERROR = "\u001B[31m";
+    public static final String WHITE = "\u001B[0m";
+}
--- src/main/java/lotto/message/Message.java
+++ src/main/java/lotto/message/Message.java
@@ -1,11 +1,13 @@
 package lotto.message;
 
+import static lotto.constant.TextColor.WHITE;
+
 public class Message {
-    public static final String PROMPT_ENTERING_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String PROMPT_ENTERING_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
-    public static final String PROMPT_ENTERING_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
-    public static final String WINNING_STATISTICS = "\n당첨 통계";
-    public static final String GUIDE_PURCHASED_QUANTITY = "\n%d개를 구매했습니다.";
-    public static final String GUIDE_TOTAL_ROR = "총 수익률은 %.1f%%입니다.";
-    public static final String DIVIDER = "---";
+    public static final String PROMPT_ENTERING_PURCHASE_AMOUNT = WHITE + "구입금액을 입력해 주세요.";
+    public static final String PROMPT_ENTERING_WINNING_NUMBERS = WHITE + "\n당첨 번호를 입력해 주세요.";
+    public static final String PROMPT_ENTERING_BONUS_NUMBER = WHITE + "\n보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS = WHITE + "\n당첨 통계";
+    public static final String GUIDE_PURCHASED_QUANTITY = WHITE + "\n%d개를 구매했습니다.";
+    public static final String GUIDE_TOTAL_ROR = WHITE + "총 수익률은 %.1f%%입니다.";
+    public static final String DIVIDER = WHITE + "---";
 }

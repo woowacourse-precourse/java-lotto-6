b0443991d33108337c0d43a2745d18236147d292
hgh1472 <hgh1472@naver.com>
2023-11-08 02:11:58 +0900

    feat : 중복 번호 검사 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,32 +1,46 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.constant.ErrorMessage;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        sortAscending(numbers);
+        hasDuplicate(numbers);
+        numbers = sortAscending(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorMessage
-                    .NOT_SIX_NUMBER_ERROR_MESSAGE.getErrorMessage());
-        }
+    public static Lotto makeLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
     }
 
     public List<Integer> getLottoNumbers() {
         return numbers;
     }
 
-    private void sortAscending(List<Integer> numbers) {
-        Collections.sort(numbers);
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            System.out.println(ErrorMessage.NOT_SIX_NUMBER_ERROR_MESSAGE.getErrorMessage());
+            throw new IllegalArgumentException();
+        }
     }
 
+    private void hasDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() < numbers.size()) {
+            System.out.println(ErrorMessage.DUPLICATE_ERROR_MESSAGE.getErrorMessage());
+            throw new IllegalArgumentException();
+        }
+    }
 
+    private List<Integer> sortAscending(List<Integer> numbers) {
+        List<Integer> sortNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortNumbers);
+        return sortNumbers;
+    }
 }

69e5951a05decb7fbf7d9c52ecd71c4592d08b6c
권장순 <jsoonworld@gmail.com>
2023-11-07 21:59:38 +0900

    refactoring ( LottoPurchaseView ) : 클래스의 이름을 명확하게 명시 LottoView -> LottoPurchaseView.

--- src/main/java/lotto/v3/view/LottoPurchaseView.java
+++ src/main/java/lotto/v3/view/LottoPurchaseView.java
@@ -2,7 +2,7 @@ package lotto.v3.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class LottoView {
+public class LottoPurchaseView {
     public int requestPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return parseAmount(Console.readLine());

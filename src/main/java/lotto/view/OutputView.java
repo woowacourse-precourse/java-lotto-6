941625f08cf3a582154e3eb543e8755e143eb947
kcw0360 <kcw0360@naver.com>
2023-11-07 16:17:08 +0900

    feat: OutputView 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,21 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void printPurchaseAmountInputMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printWinningNumberInputMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printBonusNumberInputMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printResultMessage() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+}

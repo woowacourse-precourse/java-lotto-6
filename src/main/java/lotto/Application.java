f345c87ce71465dd3018aed21173939715250cc8
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-08 03:58:56 +0900

    feat : 어플리케이션에 Controller, View 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.outputView.LottoOutputView;
+import lotto.view.outputView.LottoResultOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        LottoOutputView lottoOutputView = new LottoOutputView();
+        LottoResultOutputView lottoResultOutputView = new LottoResultOutputView();
+
+        LottoController lottoController = new LottoController(inputView, lottoOutputView,lottoResultOutputView);
+        lottoController.run();
+
     }
 }

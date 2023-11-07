bca2c67a831f68a608259cd2e8574117fb685249
San Kim <rlatks1217@gmail.com>
2023-11-08 01:28:25 +0900

    docs: 3개 이상 조건 확인 제거

---
+++ src/test/java/lotto/PurchaseTest.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.dataObjects.Lotto;
+
+public class PurchaseTest {
+
+	@DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+	@Test
+	void createLottoByDuplicatedNumber() {
+		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+}

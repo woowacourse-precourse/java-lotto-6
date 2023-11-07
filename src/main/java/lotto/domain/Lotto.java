673122115023fa7f538efa4f9924b340260382fb
nylonmusk <2912rla@gmail.com>
2023-11-08 03:34:49 +0900

    refactor : class를 record로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,16 @@
 package lotto.domain;
 
-import java.util.Collections;
 import java.util.List;
 import lotto.validator.LottoNumberValidator;
 
-public class Lotto {
-    private final List<Integer> numbers;
+public record Lotto(List<Integer> numbers) {
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
         LottoNumberValidator.isValid(numbers);
+        this.numbers = List.copyOf(numbers);
     }
 
     public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(numbers);
+        return numbers;
     }
 }

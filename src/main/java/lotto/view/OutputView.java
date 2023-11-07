1ea5e0a75297dc32c0f41618715ce1e4f27b7ab8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-07 15:49:40 +0900

    feat(outputView): 당첨 통계를 출력하는 메서드 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,9 +2,14 @@ package lotto.view;
 
 import java.util.List;
 import lotto.Lotto;
+import lotto.Ranking;
+import lotto.WinningStatistics;
 
 public class OutputView {
     private static final String LOTTO_PURCHASE_QUANTITY = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS = "당첨 통계";
+    private static final String SEPARATOR_LINE = "---";
+
     public static void println(String message) {
         System.out.println(message);
     }
@@ -16,5 +21,22 @@ public class OutputView {
 
     public static void printLotto(Lotto lotto) {
         System.out.println(lotto.getNumbers());
+    }
+
+    public static void printLottoStatistics(WinningStatistics winningStatistics) {
+        println(WINNING_STATISTICS);
+        println(SEPARATOR_LINE);
+
+        winningStatistics.rankingNumber().forEach(OutputView::printRankingDetails);
+
+        printReturnRate(winningStatistics);
+    }
+
+    private static void printRankingDetails(Ranking ranking, int count) {
+        println(ranking.getCriteria() + " - " + count + "개");
+    }
+
+    private static void printReturnRate(WinningStatistics winningStatistics) {
+        println("총 수익률은 " + winningStatistics.getReturnRate() + "%입니다.");
     }
 }

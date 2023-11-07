773940fbb82378ce8d9a4d73c31a1e8b94eb0019
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-11-08 04:45:53 +0900

    feat: 당첨번호를 입력받고 그떄 발생할 수 있는 예외처리 기능 구현

--- src/main/java/lotto/IOController.java
+++ src/main/java/lotto/IOController.java
@@ -2,22 +2,31 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 
 public class IOController {
-    private int price;
-
     public IOController() {
-        inputPrice();
 
     }
 
-    private void inputPrice() {
-        System.out.println(InstructionMessage.INSTRUCTION_INPUT_PRICE.getMessageText());
+    int inputPrice() {
+        System.out.println(InstructionMessage.INPUT_PRICE.getMessageText());
         String inputStringPrice = Console.readLine();
-        isAvailablePrice(inputStringPrice);
+        return isAvailablePrice(inputStringPrice);
     }
 
-    private void isAvailablePrice(String inputStringPrice) {
+    public List<Integer> inputWinNumber() {
+        System.out.println(InstructionMessage.INPUT_NUMBER.getMessageText());
+        String stringInputWinNumber = Console.readLine();
+
+        return isAvailableWinNumber(stringInputWinNumber);
+    }
+
+    int isAvailablePrice(String inputStringPrice) {
         int intPrice;
 
         try {
@@ -29,7 +38,54 @@ public class IOController {
         if (intPrice % 1000 != 0) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_AMOUNT.getMessageText());
         }
+        return intPrice;
     }
 
+    private List<Integer> isAvailableWinNumber(String stringInputWinNumber) {
+        inputIsNull(stringInputWinNumber);
+        makeWinNumberList(stringInputWinNumber);
+
+        List<Integer> winNumberList = makeWinNumberList(stringInputWinNumber);
+
+        isNotSix(winNumberList);
+        isDuplicatedNumber(winNumberList);
+
+        return winNumberList;
+    }
+
+    private void isNotSix(List<Integer> winNumberList) {
+        if (winNumberList.size() != 6)
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_NOT_SIX.getMessageText());
+    }
+
+    private void isDuplicatedNumber(List<Integer> winNumberList) {
+        Set<Integer> checkNumer = new HashSet<>();
+        for (Integer number : winNumberList) {
+            if (!checkNumer.add(number)) {
+                throw new IllegalArgumentException(ExceptionMessage.INPUT_DUPLICATED_NUMBER.getMessageText());
+            }
+        }
+    }
+
+    private List<Integer> makeWinNumberList(String stringInputWinNumber) {
+        List<Integer> winNumberList = new LinkedList<>();
+
+        String[] numbers = stringInputWinNumber.split(",");
+        for (String number : numbers) {
+            try {
+                winNumberList.add(Integer.parseInt(number.trim()));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_FORMAT.getMessageText());
+            }
+        }
+
+        return winNumberList;
+    }
+
+    private void inputIsNull(String stringInputWinNumber) {
+        if (stringInputWinNumber == null || stringInputWinNumber.trim().isEmpty())
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_NULL.getMessageText());
+
+    }
 
 }

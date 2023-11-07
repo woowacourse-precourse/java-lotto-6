ac326e342e77e7a34e251be0d2e37854fcba7873
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-07 22:18:45 +0900

    ApplicationTest 예외_테스트 통과하도록 수정 잘못 입력 시 재입력 받도록 처리

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -6,19 +6,29 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Purchase {
 
     public List<List<Integer>> process(){
         System.out.println("구입금액을 입력해주세요.");
-        int money = Integer.parseInt(Console.readLine());
-
+        int money = 0;
+        while (true){
+            try{
+                money = Integer.parseInt(inputMoney());
+                break;
+            }catch (NumberFormatException exception){
+                System.out.println("[ERROR] 구입 금액은 숫자만 입력 가능합니다.");
+            }
+        }
         int lottoCount = checkLottoCount(money);
-
         return printLotto(lottoCount);
     }
 
-    public int checkLottoCount(int money){
+    public String inputMoney(){
+        return Console.readLine();
+    }
+    public int checkLottoCount(int money) throws NumberFormatException{
         int lottoCount = money / 1000;
         if(money % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 구입 금액을 확인해주세요. 1장당 1000원");
@@ -28,7 +38,6 @@ public class Purchase {
     }
 
     public List<List<Integer>> printLotto(int lottoCount){
-
         List<List<Integer>> lottoNumbers = new ArrayList<>();
         while (lottoCount > 0){
             List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);

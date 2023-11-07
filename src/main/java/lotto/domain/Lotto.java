38737562f460dd713661788b19f4effe2b7efe75
dmswl6310 <dmswl6310@gmail.com>
2023-11-07 11:24:13 +0900

    feat(domain): 로또를 구매수량만큼 각 6개의 숫자를 뽑아 발행

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
@@ -10,10 +11,18 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Lotto() {
+        this.numbers = makeSixLottoNum();
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private List<Integer> makeSixLottoNum() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/domain/LottoPurchaseInfo.java
+++ src/main/java/lotto/domain/LottoPurchaseInfo.java
@@ -1,11 +1,23 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoPurchaseInfo {
     int purchaseAmount;
     int purchaseLottoNum;
+    private final List<Lotto> lottos;
 
     public LottoPurchaseInfo(int purchaseAmount, int purchaseLottoNum) {
         this.purchaseAmount = purchaseAmount;
         this.purchaseLottoNum = purchaseLottoNum;
+        this.lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseLottoNum; i++) {
+            this.lottos.add(new Lotto());
+        }
+    }
+
+    int getPurchaseLottoNum() {
+        return purchaseLottoNum;
     }
 }

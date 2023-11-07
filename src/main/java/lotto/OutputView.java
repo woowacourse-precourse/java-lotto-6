312d8ed755b2126e66cfd8371154b3efde3018e4
HwangSeokJin <ghkd3703@gmail.com>
2023-11-07 18:15:50 +0900

    fix: 보너스 번호 입력 버그 수정

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
     private static final String LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
     private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
+    private static final String RESULT_FORMAT = "당첨 통계\n---%s";
 
     public void printErrorMessage(String message) {
         System.out.println(String.format(ERROR_MESSAGE_FORMAT, message));
@@ -16,6 +17,10 @@ public class OutputView {
         for (String number : numbers) {
             System.out.println(number);
         }
+    }
+
+    public void printResult(String result) {
+        System.out.println(String.format(RESULT_FORMAT, result));
     }
 
     public void printProfitRate(float profit) {

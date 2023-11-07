33211e78bd318e8a7fc2b4638d5d94af76ddd226
PARK9898 <wjddjs21@naver.com>
2023-11-07 17:01:25 +0900

    "refactor: 메서드 분리"

--- src/main/java/lotto/domain/LottosResult.java
+++ src/main/java/lotto/domain/LottosResult.java
@@ -7,11 +7,15 @@ public class LottosResult {
     public int calculateTotalPrize(Map<Rank, Integer> results) {
         int totalPrize = 0;
         for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
-            Rank rank = entry.getKey();
-            int count = entry.getValue();
-            int prize = rank.getPrizeMoney();
-            totalPrize += count * prize;
+            totalPrize += calculateOncePrize(entry);
         }
         return totalPrize;
+    }
+
+    private int calculateOncePrize(Map.Entry<Rank, Integer> entry) {
+        Rank rank = entry.getKey();
+        int count = entry.getValue();
+        int prize = rank.getPrizeMoney();
+        return count * prize;
     }
 }

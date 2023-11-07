643d4620629227350cae9379b4e45b1e1d0a8300
icebear0111 <wltjq0901@naver.com>
2023-11-07 22:46:34 +0900

    feat: getLotto method return 6 random numbers sorted

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+
+import static java.util.Collections.sort;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,6 +17,12 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<Integer> getLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        sort(lottoNumbers);
+        return lottoNumbers;
     }
 
     // TODO: 추가 기능 구현

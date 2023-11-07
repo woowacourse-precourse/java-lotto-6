4e2da5715f86611514cadb243b4fdf8750140dd6
ASak1104 <khw56184@gmail.com>
2023-11-08 05:35:36 +0900

    test: 로또 발급기 테스트 코드 추가

---
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+import lotto.model.Lotto;
+
+public final class LottoGenerator {
+    private LottoGenerator() {
+        // Don't let anyone instantiate this class.
+    }
+
+    public static Lotto generate() {
+        return null;
+    }
+}
---
+++ src/test/java/lotto/util/LottoGeneratorTest.java
@@ -0,0 +1,36 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import lotto.model.Lotto;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+    @DisplayName("로또를 생성할 때 예외가 발생하지 않는다.")
+    @Test
+    void generateNoException() {
+        // given
+
+        // when
+        ThrowingCallable target = () -> LottoGenerator.generate();
+
+        // then
+        assertThatNoException().isThrownBy(target);
+    }
+
+    @DisplayName("로또를 인스턴스를 생성할 수 있다.")
+    @Test
+    void generateLottoInstance() {
+        // given
+
+        // when
+        Object generatedObject = LottoGenerator.generate();
+
+        // then
+        assertThat(generatedObject).isInstanceOf(Lotto.class);
+    }
+}

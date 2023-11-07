379a9d31d31e442080ab0535ca4b1ae64a7eb448
pwrwpw <dlgustjr9912@gmail.com>
2023-11-07 09:43:53 +0900

    fix : 원래 제공되었던 클래스로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,22 @@
 package lotto.domain;
 
 import java.util.List;
-import lotto.utils.Utils;
 import lotto.utils.Validation;
 
-public record Lotto(List<Integer> numbers) {
+
+public class Lotto {
+    private final List<Integer> numbers;
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = Utils.sortListAscending(numbers);
+        this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        Validation.validateListNumberCount(numbers, 6);
-        Validation.validateListNumberInRange(numbers, 1, 45);
-        Validation.validateListNumberNotDuplicated(numbers);
+        Validation.validateLotto(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }

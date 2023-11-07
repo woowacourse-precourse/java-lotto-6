d155c70c326c4275e37602988e98ebbe6a20a07a
mingeun0507 <tqdlfkg@naver.com>
2023-11-08 02:05:01 +0900

    feat: LottoGame 도메인 객체 구현

---
+++ src/main/java/lotto/domain/game/LottoGame.java
@@ -0,0 +1,32 @@
+package lotto.domain.game;
+
+import lotto.domain.generator.LottoNumberGenerator;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.lotto.WinningNumber;
+
+import java.util.stream.IntStream;
+
+public class LottoGame {
+    private final Lottos lottos;
+    private final WinningNumber winningNumber;
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    private LottoGame(final Lottos lottos, final WinningNumber winningNumber, final LottoNumberGenerator lottoNumberGenerator) {
+        this.lottos = lottos;
+        this.winningNumber = winningNumber;
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    public static LottoGame of(final Money money, final WinningNumber winningNumber, final LottoNumberGenerator lottoNumberGenerator) {
+        final int lottoCount = money.countLotto();
+        final Lottos lottos = Lottos.from(
+                IntStream.range(0, lottoCount)
+                        .mapToObj(i -> lottoNumberGenerator.generateLottoNumbers())
+                        .map(Lotto::new)
+                        .toList()
+        );
+
+        return new LottoGame(lottos, winningNumber, lottoNumberGenerator);
+    }
+}

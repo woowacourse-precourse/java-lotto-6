8c082164bfff4c6e56d22c98992b6093b53d35dd
nayonsoso <panda0329@naver.com>
2023-11-08 05:16:11 +0900

    test: 순위 통계 추가시 통계 숫자 1 증가 테스트

--- src/test/java/model/RankStatisticsTest.java
+++ src/test/java/model/RankStatisticsTest.java
@@ -7,9 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RankStatisticsTest {
+
     @DisplayName("순위 통계에 아무것도 추가하지 않으면 통계의 숫자는 0이다.")
     @Test
-    void remainStatisticsNumberZeroWithoutAddingRank(){
+    void remainStatisticsNumberZeroWithoutAddingRank() {
         RankStatistics rankStatistics = new RankStatistics();
         int firstCountBefore = rankStatistics.getRankStatistics().get(LottoRank.FIRST);
         rankStatistics.add(LottoRank.FIRST);
@@ -17,4 +18,16 @@ class RankStatisticsTest {
         assertThat(firstCountBefore).isEqualTo(0);
         assertThat(firstCountBefore).isEqualTo(1);
     }
+
+    @DisplayName("순위 통계에 순위 추가하면 통계의 숫자가 하나 늘어난다.")
+    @Test
+    void increaseFirstRankStatisticsByAddingFirstRank() {
+        RankStatistics rankStatistics = new RankStatistics();
+        int firstCountBefore = rankStatistics.getRankStatistics().get(LottoRank.FIRST);
+        rankStatistics.add(LottoRank.FIRST);
+        int firstCountAfter = rankStatistics.getRankStatistics().get(LottoRank.FIRST);
+        assertThat(firstCountBefore).isEqualTo(0);
+        assertThat(firstCountBefore).isEqualTo(1);
+    }
+
 }

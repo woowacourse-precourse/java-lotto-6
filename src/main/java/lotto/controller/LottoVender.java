4e5a8576af72db818d7a549bf1b1a22827cbe3ac
valborgs <voll1212@naver.com>
2023-11-07 16:39:14 +0900

    refactor : view수정

---
+++ src/main/java/lotto/controller/LottoVender.java
@@ -0,0 +1,40 @@
+package lottovender;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoVender {
+    private final int LOTTO_PRICE = 1000;
+    private int inputPrice;
+    private int lottoCount;
+
+    public int getLottoCount() {
+        return lottoCount;
+    }
+
+    /*public void start(){
+        InputView inputview = new InputView();
+        inputPrice = inputview.inputLottoPrice();
+        payPrice(inputPrice);
+        getLotto(lottoCount);
+    }*/
+
+    public void payPrice(int inputPrice){
+        int buyLottoCount;
+        lottoCount = inputPrice / LOTTO_PRICE;
+    }
+
+    public List<Lotto> getLotto(int buyLottoCount){
+        List<Lotto> lottoticekts = new ArrayList<>();
+        for(int i = 0; i<buyLottoCount; i++){
+            //6개의 랜덤 로또 번호 생성해주는 numbers
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoticekts.add(new Lotto(numbers));
+        }
+        return lottoticekts;
+    }
+
+}

62241054bf7709321eac31edbf98ac50d377866e
codms85 <lce0805@naver.com>
2023-11-07 13:40:32 +0900

    get_num, get_bonus, compare_num

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import java.util.List;
+
 import lotto.Lotto;
 
 public class Application {
     public static void main(String[] args) {
 
     	System.out.println("구매금액을 입력해 주세요.");
-    	Lotto.make_lotto(Lotto.make_num());  	
+    	List<Integer>[] lotto = Lotto.make_lotto(Lotto.make_num());  	
+    
+    	Lotto.compare_num(lotto, Lotto.get_num(), Lotto.get_bonus());
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -30,15 +30,49 @@ public class Lotto {
     }
     
     
-    public static void make_lotto(int num) { //로또 번호 발
+    public static List<Integer>[] make_lotto(int num) { //로또 번호 받기
     	List<Integer>[] lottoArray = new List[num];
-    	
     	for(int i = 0; i<num; i++) {
     		lottoArray[i] = Randoms.pickUniqueNumbersInRange(1, 45, 6);
     		Collections.sort(lottoArray[i]);
     		System.out.println(lottoArray[i]);
     	}
-
+    	return lottoArray;
     }
+    
+    public static int[] get_num() { //사용자 당첨 번호 입력값 받기 
+    	System.out.println("당첨 번호를 입력해 주세요.");
+    	String n = Console.readLine();
+		String[] num = n.split(",");
+		if (num.length != 6) { //6개 이외의 값을 넣었을 경우 
+			throw new IllegalArgumentException();
+		}
+		int[] money = new int[6];
+		for (int i = 0; i < 6; i++) {
+			money[i] = Integer.parseInt(num[i]);
+		}
+		return money;
+    }  
+    
+    public static int get_bonus() { //사용자 보너스번호 입력값 받기 
+    	System.out.println("보너스 번호를 입력해 주세요.");
+    	String n = Console.readLine();
+		int bonus = Integer.parseInt(n);
+		return bonus;
+    }
+    
+    public static int[] compare_num(List<Integer>[] lotto, int[] num, int bonus) { //로또확인 
+    	int[] result = new int[lotto.length];
+    	for(int i=0; i<lotto.length; i++) {
+    		for(int j=0; j<num.length; j++) {
+    			if(lotto[i].contains(num[j])) {
+    				result[i] ++; 
+    			}
+    		}
+        	System.out.println(result[i]);
+    	}
+		return result;
+    }
+    
     
 }

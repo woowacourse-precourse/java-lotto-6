b5363af729763eda9499e4ab3fa2ee56d6b35644
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-08 03:00:08 +0900

    style(lotto): 코드 간소화

--- src/main/java/lotto/service/DeliverService.java
+++ src/main/java/lotto/service/DeliverService.java
@@ -1,6 +1,6 @@
 package lotto.service;
 
-import lotto.Lotto;
+import lotto.vo.Lotto;
 import lotto.domain.LottoGenerator;
 import lotto.view.Output;
 
@@ -10,9 +10,8 @@ import java.util.List;
 import static lotto.values.ExceptionMessage.NOT_WORK_LOTTO_GENERATOR;
 
 public class DeliverService {
-    List<Lotto> lottoPackage;
-    public void generateLotto(int num){
-        lottoPackage = new ArrayList<>();
+    public List<Lotto> generateLotto(int num){
+        List<Lotto> lottoPackage = new ArrayList<>();
         LottoGenerator generator = new LottoGenerator();
 
         int i=0;
@@ -29,9 +28,7 @@ public class DeliverService {
                 i++;
             }
         }
-    }
 
-    public List<Lotto> getLottoPackage(){
         return lottoPackage;
     }
 

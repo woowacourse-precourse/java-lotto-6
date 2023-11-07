398195194821a1ef7c4c3f9c18d2ac171d22b9ef
minjong kim <godminjong@naver.com>
2023-11-07 23:16:54 +0900

    refact: 실제 로또 결과 번호와 구매한 로또 번호의 명확한 구분을 위해 Wrapper class 생성 또한 실제 로또 번호에는 보너스 번호가 있기 때문에 별도 필드 추가함. (실제 로또 결과: LottoWinningNumber.class, 로또 티켓: LottoTicket.class)
    명시적으로 Lotto 결과 번호와 구매한 Lotto 번호를 구분하기 위해

---
+++ src/main/java/lotto/domain/model/LottoTicket.java
@@ -0,0 +1,11 @@
+package lotto.domain.model;
+
+import java.util.List;
+
+public class LottoTicket extends Lotto{
+
+    public LottoTicket(List<Integer> numbers) {
+        super(numbers);
+    }
+
+}

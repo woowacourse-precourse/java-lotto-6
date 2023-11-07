d7f49973844cd456b284f28ad42ab4de3c9215ef
acs0209 <annjae4676@naver.com>
2023-11-07 15:50:21 +0900

    refactor: 당첨액과 구입 금액 계산 기능 테스트 수정

--- src/test/java/lotto/domain/LottoProfitTest.java
+++ src/test/java/lotto/domain/LottoProfitTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class LottoProfitTest {
@@ -19,29 +20,31 @@ class LottoProfitTest {
         lottoPrize = LottoPrize.create();
     }
 
-    @Test
-    @DisplayName("로또 당첨 내역을 기반하여 총 당첨액을 올바르게 계산 하는지 테스트")
-    void testTotalWinningAmountEqualsExpectedValue() {
-        boolean hasBonusNumber = true;
-        int secondRankCount = SECOND_PRIZE_COUNT.getNumber();
-        int firstRankCount = FIRST_PRIZE_COUNT.getNumber();
-        lottoPrize.updateRankCount(secondRankCount, hasBonusNumber);
-        lottoPrize.updateRankCount(firstRankCount, hasBonusNumber);
-        LottoPurchase lottoPurchase = LottoPurchase.create(8);
-        lottoProfit.createLottoProfit(lottoPrize, lottoPurchase);
+    @Nested
+    @DisplayName("총 당첨액과 총 구입 금액이 올바르게 계산이 되는지 테스트")
+    class testTotalWinningsAndTotalPurchaseAmount {
+        @Test
+        @DisplayName("로또 당첨 내역에 기반하여 로또 총 당첨액이 예상값과 일치하는지 테스트")
+        void testTotalWinningAmountEqualsExpectedValue() {
+            boolean hasBonusNumber = true;
+            int secondRankCount = SECOND_PRIZE_COUNT.getNumber();
+            int firstRankCount = FIRST_PRIZE_COUNT.getNumber();
+            lottoPrize.updateRankCount(secondRankCount, hasBonusNumber);
+            lottoPrize.updateRankCount(firstRankCount, hasBonusNumber);
 
-        int totalWinningAmount = lottoProfit.createTotalWinningAmount(lottoPrize);
+            int totalWinningAmount = lottoProfit.createTotalWinningAmount(lottoPrize);
 
-        assertThat(totalWinningAmount).isEqualTo(2030000000);
-    }
+            assertThat(totalWinningAmount).isEqualTo(2030000000);
+        }
 
-    @Test
-    @DisplayName("로또 구매 개수에 기반하여 로또 총 구입 금액이 예상값과 일치하는지 테스트")
-    void testLottoPurchaseTotalAmountEqualsExpectedValue() {
-        LottoPurchase lottoPurchase = LottoPurchase.create(23);
+        @Test
+        @DisplayName("로또 구매 개수에 기반하여 로또 총 구입 금액이 예상값과 일치하는지 테스트")
+        void testLottoPurchaseTotalAmountEqualsExpectedValue() {
+            LottoPurchase lottoPurchase = LottoPurchase.create(23);
 
-        int totalPurchaseAmount = lottoProfit.createTotalPurchaseAmount(lottoPurchase);
+            int totalPurchaseAmount = lottoProfit.createTotalPurchaseAmount(lottoPurchase);
 
-        assertThat(totalPurchaseAmount).isEqualTo(23000);
+            assertThat(totalPurchaseAmount).isEqualTo(23000);
+        }
     }
 }

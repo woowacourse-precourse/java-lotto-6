102c0e1073caac897ddd2c3417860189c4abc10c
happyryan <tjdgks120813@gmail.com>
2023-11-07 13:21:29 +0900

    refactor(lotto): integrated ReturnCalculator and LottoResultCalculator into Analyst class
    Merged the functionality of ReturnCalculator and LottoResultCalculator into the Analyst class. The Analyst class now handles the calculation and printing of lotto results based on the provided lottos, winning numbers, and bonus numbers.

--- src/main/java/lotto/Analyst.java
+++ src/main/java/lotto/Analyst.java
@@ -1,2 +1,67 @@
-package lotto;public class Analyst {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Analyst {
+    Map<LottoMessage, Integer> lottoResult = new LinkedHashMap<>();
+    private final List<Lotto> lottos;
+    private final List<Integer> sixLottoNumber;
+    private final int bonusNumber;
+
+    public Analyst(List<Lotto> lottos, List<Integer> sixLottoNumber, int bonusNumber) {
+        this.lottos = lottos;
+        this.sixLottoNumber = sixLottoNumber;
+        this.bonusNumber = bonusNumber;
+
+        lottoResult.put(LottoMessage.THREE, 0);
+        lottoResult.put(LottoMessage.FOUR, 0);
+        lottoResult.put(LottoMessage.FIVE, 0);
+        lottoResult.put(LottoMessage.BONUS, 0);
+        lottoResult.put(LottoMessage.SIX, 0);
+    }
+
+    public void calculate() {
+        for (Lotto lotto : lottos) {
+            List<Integer> winningNumbers = new ArrayList<>(lotto.getSixLottoNumbers());
+
+            boolean isBonusNumber = winningNumbers.contains(bonusNumber);
+            winningNumbers.retainAll(sixLottoNumber);
+
+            rankCalculator(isBonusNumber, winningNumbers);
+        }
+    }
+
+    private void rankCalculator(boolean isBonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.size() == 3) {
+            lottoResult.put(LottoMessage.THREE, lottoResult.get(LottoMessage.THREE) + 1);
+        }
+        if (winningNumbers.size() == 4) {
+            lottoResult.put(LottoMessage.FOUR, lottoResult.get(LottoMessage.FOUR) + 1);
+        }
+        if (winningNumbers.size() == 5 && !isBonusNumber) {
+            lottoResult.put(LottoMessage.FIVE, lottoResult.get(LottoMessage.FIVE) + 1);
+        }
+        if (winningNumbers.size() == 5 && isBonusNumber) {
+            lottoResult.put(LottoMessage.BONUS, lottoResult.get(LottoMessage.BONUS) + 1);
+        }
+        if (winningNumbers.size() == 6) {
+            lottoResult.put(LottoMessage.SIX, lottoResult.get(LottoMessage.SIX) + 1);
+        }
+    }
+
+    public void printLottoResult(int userMoney) {
+        int totalWinningAmount = 0;
+        Output.printWinningStatistics();
+
+        for (Map.Entry<LottoMessage, Integer> entry : lottoResult.entrySet()) {
+            if (entry.getValue() > 0) {
+                totalWinningAmount += entry.getKey().winningAmount * entry.getValue();
+            }
+            System.out.println(entry.getKey().message + " - " + entry.getValue() + "개");
+        }
+        Output.printReturn(totalWinningAmount, userMoney);
+    }
 }

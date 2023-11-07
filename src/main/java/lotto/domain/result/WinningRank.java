1db1edc32a39c0c07b3cb204ba06842794381812
JuSeong1130 <jsmoon1130@naver.com>
2023-11-07 15:10:50 +0900

    refactor(WinningRank) : findWinningRank 함수형 인터페이스 이용 기능 리팩토링

--- src/main/java/lotto/domain/result/WinningRank.java
+++ src/main/java/lotto/domain/result/WinningRank.java
@@ -1,35 +1,33 @@
 package lotto.domain.result;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public enum WinningRank {
-    FIFTH(3, false, 5000),
-    FOURTH(4, false, 50000),
-    THIRD(5, false, 1500000),
-    SECOND(5, true, 30000000),
-    FIRST(6, false, 2000000000),
-    EMPTY(-1, false, 0);
+    FIFTH(3, false, 5000, (matchCount, bonusNumber) -> matchCount == 3),
+    FOURTH(4, false, 50000, (matchCount, bonusNumber) -> matchCount == 4),
+    THIRD(5, false, 1500000, (matchCount, bonusNumber) -> matchCount == 5 && !bonusNumber),
+    SECOND(5, true, 30000000, (matchCount, bonusNumber) -> matchCount == 5 && bonusNumber),
+    FIRST(6, false, 2000000000, (matchCount, bonusNumber) -> matchCount == 6),
+    EMPTY(-1, false, 0, (matchCount, bonusNumber) -> matchCount == -1);
 
-    private final int count;
+    private final int matchCount;
     private final boolean bonusNumber;
     private final int money;
 
-    WinningRank(int count, boolean bonusNumber, int money) {
-        this.count = count;
+    private final BiPredicate<Integer,Boolean> isMatch;
+
+    WinningRank(int matchCount, boolean bonusNumber, int money,
+            BiPredicate<Integer, Boolean> isMatch) {
+        this.matchCount = matchCount;
         this.bonusNumber = bonusNumber;
         this.money = money;
+        this.isMatch = isMatch;
     }
 
     public static WinningRank findWinningRank(int matchCount, boolean hasBonusNumber) {
-        if (matchCount == 5) {
-            return Arrays.stream(WinningRank.values())
-                    .filter(winningResult -> winningResult.count == matchCount
-                            && winningResult.bonusNumber == hasBonusNumber)
-                    .findAny()
-                    .orElse(EMPTY);
-        }
         return Arrays.stream(WinningRank.values())
-                .filter(winningResult -> winningResult.count == matchCount)
+                .filter(winningResult -> winningResult.isMatch.test(matchCount, hasBonusNumber))
                 .findAny()
                 .orElse(EMPTY);
     }
@@ -39,7 +37,7 @@ public enum WinningRank {
     }
 
     public int getCount() {
-        return count;
+        return matchCount;
     }
 
     public boolean isBonusNumber() {

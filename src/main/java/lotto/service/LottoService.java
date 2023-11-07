6e0b9b86726dbdcfa8f298a56e3bfc504743882a
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-11-07 22:23:53 +0900

    fix(LottoService) : 당첨 통계 오류 수정
     LottoService : calculateMatchingCounts 메서드 수정

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -130,13 +130,17 @@ public class LottoService {
         Map<Integer, Integer> matchingCounts = new HashMap<>();
         for (List<Integer> lottoNumbers : userLottoNumbers) {
             int count = 0;
+            boolean isBonusMatched = false;
             for (int number : lottoNumbers) {
                 if (winningNumbers.contains(number)) {
                     count++;
                 }
+                if (number == bonusNumber) {
+                    isBonusMatched = true;
+                }
             }
-            if (lottoNumbers.contains(bonusNumber)) {
-                count = count == 5 ? 6 : count;
+            if (count == 5 && isBonusMatched) {
+                count = 7;
             }
             matchingCounts.put(count, matchingCounts.getOrDefault(count, 0) + 1);
         }
@@ -149,8 +153,8 @@ public class LottoService {
         System.out.println("3개 일치 (5,000원) - " + matchingCounts.getOrDefault(3, 0) + "개");
         System.out.println("4개 일치 (50,000원) - " + matchingCounts.getOrDefault(4, 0) + "개");
         System.out.println("5개 일치 (1,500,000원) - " + matchingCounts.getOrDefault(5, 0) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matchingCounts.getOrDefault(6, 0) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + matchingCounts.getOrDefault(7, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matchingCounts.getOrDefault(7, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + matchingCounts.getOrDefault(6, 0) + "개");
     }
 
     public void printReturnRate(Map<Integer, Integer> matchingCounts, int lottoTicketsCount) {
@@ -163,8 +167,8 @@ public class LottoService {
         totalPrize += matchingCounts.getOrDefault(3, 0) * 5000;
         totalPrize += matchingCounts.getOrDefault(4, 0) * 50000;
         totalPrize += matchingCounts.getOrDefault(5, 0) * 1500000;
-        totalPrize += matchingCounts.getOrDefault(6, 0) * 30000000;
-        totalPrize += matchingCounts.getOrDefault(7, 0) * 2000000000;
+        totalPrize += matchingCounts.getOrDefault(7, 0) * 30000000;
+        totalPrize += matchingCounts.getOrDefault(6, 0) * 2000000000;
         double purchaseAmount = lottoTicketsCount * LOTTO_TICKET_PRICE;
         return totalPrize / purchaseAmount * 100;
     }

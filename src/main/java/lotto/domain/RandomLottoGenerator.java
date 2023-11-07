18352a62611988112821cebcb79c656562158f64
swhyeon98 <smreo75@gmail.com>
2023-11-07 18:32:42 +0900

    feat: 로또 랜덤 번호 생성 기능 추가

---
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomLottoGenerator {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_SIZE);
+    }
+}

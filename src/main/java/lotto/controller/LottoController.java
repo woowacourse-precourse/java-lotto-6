51cac8d4abaa9fc9bed0e98a5edb704f42d0a7dc
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-07 11:19:01 +0900

    feat(LottoController) : 로또 컨트롤러 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,49 @@
+package lotto.controller;
+
+import lotto.domain.BonusNumber;
+import lotto.domain.Lottery;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+import lotto.domain.NumberGenerator;
+import lotto.domain.Profit;
+import lotto.domain.PurchasePrice;
+import lotto.domain.WinningNumber;
+import lotto.handler.InputHandler;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private NumberGenerator lottoNumberGenerator;
+
+    public LottoController(NumberGenerator lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    public void run() {
+        Lottery lottery = prepareLottery();
+        startLottery(lottery);
+    }
+
+    private Lottery prepareLottery() {
+        OutputView.printPurchaseMessage();
+        PurchasePrice purchasePrice = InputHandler.receiveValidatedPurchasePrice();
+        LottoTicket lottoTicket = createLottoEqualTo(purchasePrice);
+        OutputView.printWinningNumberMessage();
+        WinningNumber winningNumber = InputHandler.receiveValidatedWinningNumber();
+        OutputView.printBonusNumberMessage();
+        BonusNumber bonusNumber = InputHandler.receiveValidatedBonusNumberAndNotExistFrom(winningNumber);
+        return new Lottery(winningNumber, bonusNumber, lottoTicket);
+    }
+
+    private LottoTicket createLottoEqualTo(PurchasePrice purchasePrice) {
+        LottoTicket lottoTicket = LottoTicket.of(purchasePrice, lottoNumberGenerator);
+        OutputView.printPurchaseCount(lottoTicket);
+        OutputView.printPurchaseLottoTickets(lottoTicket);
+        return lottoTicket;
+    }
+
+    private void startLottery(Lottery lottery) {
+        LottoResult lottoResult = lottery.createLottoResult();
+        OutputView.printWinningResult(lottoResult);
+        OutputView.printRateOfReturn(new Profit(lottoResult));
+    }
+}

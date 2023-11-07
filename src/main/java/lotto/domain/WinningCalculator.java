9eeecceeb858ea9dbbfd8f9bb30bf288d4f98cac
bin <gimgyubin1225@gmail.com>
2023-11-07 14:46:01 +0900

    feat: 당첨번호, 보너스 번호와 로또를 비교해 당첨결과를 계산하는 기능 추가

---
+++ src/main/java/lotto/domain/WinningCalculator.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WinningCalculator {
+
+    private List<Integer> winningNumbers;
+    private Integer bonusNumber;
+
+    public WinningCalculator(List<Integer> winningNumbers, Integer bonusNumber) {
+        validateDuplicateNumber(winningNumbers, bonusNumber);
+        validateNumberCount(winningNumbers);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public WinningResult calculate(List<Lotto> lottoes){
+        WinningResult winningResult = new WinningResult();
+        for(Lotto lotto: lottoes){
+            int numberOfMatches = lotto.getNumberOfMatches(winningNumbers);
+            winningResult.addResults(numberOfMatches, lotto.containsBonusNumber(bonusNumber));
+        }
+        return winningResult;
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != Lotto.LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers, Integer bonusNumber) {
+        Set<Integer> set = new HashSet<>(numbers);
+        set.add(bonusNumber);
+        if (set.size() != numbers.size() + 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

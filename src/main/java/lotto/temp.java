73c6dd93b499084dd43294fdd8e32f9637fdd23e
5jeong <wjdgns4385@naver.com>
2023-11-08 03:26:22 +0900

    [Refactor] 예외처리 출력 구조수정

---
+++ src/main/java/lotto/temp.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class temp {
+    public static void main(String[] args) {
+        double n = 0;
+        n = Math.round(n *100.0) /100.0;
+        System.out.println(n);
+    }
+}

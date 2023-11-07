f9cb9d48a21fb710d93ec4901079e4798df9d3d4
KDT live <dnjsxo0616@gmail.com>
2023-11-08 03:08:58 +0900

    feat(InputWinningNumberView): 당첨 번호를 유효성 검사 메서드 추가
    - validateInput 메서드로 정수로 변환할 수 있는 지 입력값 검증

--- src/main/java/lotto/view/InputWinningNumberView.java
+++ src/main/java/lotto/view/InputWinningNumberView.java
@@ -1,11 +1,22 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-public class InputWinningNumberView {
 
+import java.util.List;
+
+public class InputWinningNumberView {
     private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+
     private String inputWinningNumber(){
         System.out.println(INPUT_WINNING_NUMBERS);
         return Console.readLine();
+    }
+
+    private void validateInput(String input){
+        try{
+            Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 입력값이 유효한 숫자가 아닙니다.");
+        }
     }
 }

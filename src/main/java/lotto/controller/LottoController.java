bbf0ebaaa07a81979a8d7fa46d88ee34d571c5d5
chnbroa <chnbroa011@naver.com>
2023-11-07 17:39:20 +0900

    feat: 구매한 로또 개수 출력 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,19 +2,21 @@ package lotto.controller;
 
 import lotto.model.LottoAmount;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView;
     private LottoAmount lottoAmount;
+    private OutputView outputView;
 
     public LottoController() {
         inputView = new InputView();
-
+        outputView = new OutputView();
     }
 
     public void start() {
         int lottoTicketCount = inputLottoAmount();
-
+        outputView.printlottoTicketCount(lottoTicketCount);
     }
 
     public int inputLottoAmount() {
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+    public static final String PURCHASE_MSG = "개를 구매했습니다.";
+
+    public void printlottoTicketCount(int TicketCount) {
+        System.out.println(TicketCount + PURCHASE_MSG);
+
+    }
+}

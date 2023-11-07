918d0b3c911a932c320a2bbea3a8472e836d8e7f
sujnkim <blackout3210@naver.com>
2023-11-07 18:08:06 +0900

    refactor: outputview for문을 strem으로 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-    private static final String PRINT_LOTTO_AMOUNT_SENTENCE = "개를 구매했습니다.";
+    private static final String PRINT_LOTTO_AMOUNT_SENTENCE = "%d개를 구매했습니다.";
     private static final String PRINT_RESULT_START_SENTENCE = "당첨 통계";
     private static final String DEFAULT_WINNING_SENTENCE = "%d개 일치 (%s원) - %d개";
     private static final String BONUS_WINNING_SENTENCE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
@@ -24,10 +24,7 @@ public class OutputView {
     }
 
     private static void printLottosAmount(int amount) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(amount);
-        sb.append(PRINT_LOTTO_AMOUNT_SENTENCE);
-        System.out.println(sb);
+        System.out.println(String.format(PRINT_LOTTO_AMOUNT_SENTENCE, amount));
     }
 
     private static void printLottosNumber(List<String> lottos) {
@@ -45,26 +42,30 @@ public class OutputView {
     }
 
     private static void printRankCount(LottoResult result) {
-        List<Map.Entry<LottoRank, Integer>> sortedLottoResult = result.getSortedLottoResult();
-        for (Map.Entry<LottoRank, Integer> entry : sortedLottoResult) {
-            System.out.println(makeLottoResultSentence(entry));
-        }
+        result.getSortedLottoResult().stream()
+                .forEach(entry -> System.out.println(makeLottoResultSentence(entry)));
     }
 
     private static String makeLottoResultSentence(Map.Entry<LottoRank, Integer> entry) {
         LottoRank rank = entry.getKey();
 
         if (rank.isBonusBall()) {
-            return String.format(BONUS_WINNING_SENTENCE, rank.getMatchCount(), rank.getFormattedReward(), entry.getValue());
+            return String.format(BONUS_WINNING_SENTENCE,
+                    rank.getMatchCount(),
+                    rank.getFormattedReward(),
+                    entry.getValue());
         }
-        return String.format(DEFAULT_WINNING_SENTENCE, rank.getMatchCount(), rank.getFormattedReward(), entry.getValue());
+        return String.format(DEFAULT_WINNING_SENTENCE,
+                rank.getMatchCount(),
+                rank.getFormattedReward(),
+                entry.getValue());
     }
 
     public static void printReturnRate(String result) {
         System.out.println(String.format(RETURN_RATE_SENTENCE, result));
     }
 
-    public static void printErrorMessage(String errorMessage){
+    public static void printErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
     }
 }

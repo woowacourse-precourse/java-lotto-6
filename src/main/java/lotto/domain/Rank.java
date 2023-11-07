4ab481abf423a3669b6c95e0748ec414b6faed3a
seokmyungham <hellotjraud@gmail.com>
2023-11-07 12:22:11 +0900

    refactor(Rank): Rank 열거형 순서 변경
    Rank.value() 사용 시 순위가 오름차 순으로 정렬될 수 있도록 열거형의 순서를 변경하였습니다

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
+    FIFTH(3, 5000),
     FOURTH(4, 50000),
-    FIFTH(3, 5000);
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
 
     private final int count;
     private final int winningAmount;
@@ -28,6 +28,10 @@ public enum Rank {
         }
 
         return rank;
+    }
+
+    public int getCount() {
+        return count;
     }
 
     public int getWinningAmount() {

75b8392be3c48c3ecdb0339692a9e964b3b44d57
yunju-m <dbswnfnal@naver.com>
2023-11-07 15:41:55 +0900

    Feat(LuckeyLotto): ✨ 보너스 로또 번호 중복여부 판단하는 유효성 검사 구현

--- src/main/java/lotto/LuckeyLotto.java
+++ src/main/java/lotto/LuckeyLotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class LuckeyLotto {
     private static final String WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호의 타입은 숫자이어야 합니다.";
+    private static final String WRONG_SAME_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호는 중복없는 숫자이어야 합니다.";
     private int luckeyBonusNumber;
     private List<Integer> luckeyNumbers;
 
@@ -18,6 +19,8 @@ public class LuckeyLotto {
     private void validate(List<Integer> luckeyNumbers, String inputBonusNumber) {
         if (!inputBonusNumber.matches("\\d+")) {
             throw new IllegalArgumentException(WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE);
+        } else if (luckeyNumbers.contains(Integer.valueOf(inputBonusNumber))) {
+            throw new IllegalArgumentException(WRONG_SAME_LOTTO_BONUS_NUMBER_MESSAGE);
         }
     }
 }

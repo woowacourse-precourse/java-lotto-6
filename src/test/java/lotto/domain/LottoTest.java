23ddb6623782e310c6979196367b9ebc0d5b6652
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:57:41 +0900

    test(Lotto) : Lotto test add

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    void getNumbers() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}

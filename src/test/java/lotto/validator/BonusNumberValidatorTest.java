892e04023876ac41be605d52c4417da721ad4793
nylonmusk <2912rla@gmail.com>
2023-11-08 06:36:20 +0900

    test : BonusNumberValidator.validate 매개변수 변경에 따른 수정

--- src/test/java/lotto/validator/BonusNumberValidatorTest.java
+++ src/test/java/lotto/validator/BonusNumberValidatorTest.java
@@ -3,9 +3,10 @@ package lotto.validator;
 import lotto.TestConstant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import java.util.Arrays;
+
 
 class BonusNumberValidatorTest {
 
@@ -13,14 +14,17 @@ class BonusNumberValidatorTest {
     @Test
     void 유효한_보너스_번호_테스트() {
         int validBonusNumber = 7;
-        assertThat(BonusNumberValidator.validate(String.valueOf(validBonusNumber))).isEqualTo(validBonusNumber);
+        String winningNumbersInput = "1,2,3,4,5,6";
+        assertThat(BonusNumberValidator.validate(String.valueOf(validBonusNumber), winningNumbersInput))
+                .isEqualTo(validBonusNumber);
     }
 
     @DisplayName("잘못된 형식의 보너스 번호 입력 시 예외가 발생한다.")
     @Test
     void 잘못된_보너스_번호_형식_예외_발생() {
         String invalidBonusNumber = "abc";
-        assertThatThrownBy(() -> BonusNumberValidator.validate(invalidBonusNumber))
+        String winningNumbersInput = "1,2,3,4,5,6";
+        assertThatThrownBy(() -> BonusNumberValidator.validate(invalidBonusNumber, winningNumbersInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(TestConstant.ERROR_MESSAGE);
     }
@@ -29,7 +33,9 @@ class BonusNumberValidatorTest {
     @Test
     void 범위_벗어난_보너스_번호_예외_발생() {
         int outOfRangeBonusNumber = 46;
-        assertThatThrownBy(() -> BonusNumberValidator.validate(String.valueOf(outOfRangeBonusNumber)))
+        String winningNumbersInput = "1,2,3,4,5,6";
+        assertThatThrownBy(
+                () -> BonusNumberValidator.validate(String.valueOf(outOfRangeBonusNumber), winningNumbersInput))
                 .isInstanceOf(IndexOutOfBoundsException.class)
                 .hasMessageContaining(TestConstant.ERROR_MESSAGE);
     }
@@ -37,8 +43,10 @@ class BonusNumberValidatorTest {
     @DisplayName("당첨 번호 내에 이미 존재하는 보너스 번호 입력 시 예외가 발생한다.")
     @Test
     void 당첨_번호_중복_보너스_번호_예외_발생() {
-        int duplicateBonusNumber = 15;
-        assertThatThrownBy(() -> BonusNumberValidator.validate(duplicateBonusNumber, Arrays.asList(1, 3, 5, 7, 9, 15)))
+        int duplicateBonusNumber = 1;
+        String winningNumbersInput = "1,2,3,4,5,6";
+        assertThatThrownBy(
+                () -> BonusNumberValidator.validate(String.valueOf(duplicateBonusNumber), winningNumbersInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(TestConstant.ERROR_MESSAGE);
     }

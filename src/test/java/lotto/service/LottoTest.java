93bf79d97e2cc7c84df85fe9cd9b9fe1926b4636
김효준 <khyojun1104@gmail.com>
2023-11-07 22:55:20 +0900

    chore(lotto): 주어진 LottoTest 활용하여 테스트 할 수 있도록 변경!

--- src/test/java/lotto/service/LottoTest.java
+++ src/test/java/lotto/service/LottoTest.java
@@ -1,101 +1,38 @@
-package lotto.service;
+package lotto;
 
-import static lotto.constant.ErrorMessage.WINNUMBER_LENGTH;
-import static lotto.constant.ErrorMessage.WINNUMBER_NO_DUPLICATE_LENGTH;
-import static lotto.constant.ErrorMessage.WINNUMBER_RANGE;
-import static lotto.constant.NumberConstant.LAST_LOTTO_NUMBER;
-import static lotto.constant.NumberConstant.NUMBERS_LENGTH;
-import static lotto.constant.NumberConstant.START_LOTTO_NUMBER;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
+import lotto.service.Lotto;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
-    @ParameterizedTest
-    @MethodSource("generateRangeData")
-    @DisplayName("당첨 번호 범위 테스트! 1-45 사이")
-    void rangeCheckTest(List<Integer> rangeNumbers) {
-        Assertions.assertThatThrownBy(() -> rangeCheck(rangeNumbers))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(WINNUMBER_RANGE.getMessage());
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
-    static Stream<Arguments> generateRangeData() {
-        return Stream.of(
-            Arguments.of(Arrays.asList(1, 2, 3, 46, 47, 48)),
-            Arguments.of(Arrays.asList(-1, 0, 1, 2, 3, 4))
-        );
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @ParameterizedTest
-    @MethodSource("generateDuplicateData")
-    @DisplayName("당첨 번호 내부에 중복된 숫자가 있는지 테스트!")
-    void duplicateTest(List<Integer> duplicateNumbers) {
-        Assertions.assertThatThrownBy(() -> duplicate(duplicateNumbers))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(WINNUMBER_NO_DUPLICATE_LENGTH.getMessage());
-    }
+    // 아래에 추가 테스트 작성 가능
 
-    static Stream<Arguments> generateDuplicateData() {
-        return Stream.of(
-            Arguments.of(Arrays.asList(1, 2, 2, 3, 4, 5)),
-            Arguments.of(Arrays.asList(1, 2, 3, 3, 4, 5))
-        );
-    }
-
-
-    @ParameterizedTest
-    @MethodSource("generateLengthData")
-    @DisplayName("당첨 번호 내부에 중복된 숫자가 있는지 테스트!")
-    void lengthTest(List<Integer> lengthNumbers) {
-        Assertions.assertThatThrownBy(() -> lengthCheck(lengthNumbers))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessageContaining(WINNUMBER_LENGTH.getMessage());
-    }
-
-    static Stream<Arguments> generateLengthData() {
-        return Stream.of(
-            Arguments.of(Arrays.asList(1, 2, 2, 3, 4, 5, 7)),
-            Arguments.of(Arrays.asList(1, 2, 3, 3, 4))
-        );
-    }
-
-
-    private void rangeCheck(List<Integer> numbers) {
-        for (Integer winnerNumber : numbers) {
-            if (isNumberNotInRange(winnerNumber)) {
-                throw new IllegalArgumentException(WINNUMBER_RANGE.getMessage());
-            }
-        }
-    }
-
-    private boolean isNumberNotInRange(Integer winnerNumber) {
-        return winnerNumber < START_LOTTO_NUMBER.getNumber()
-            || winnerNumber > LAST_LOTTO_NUMBER.getNumber();
-    }
-
-    private void duplicate(List<Integer> numbers) {
-        Set<Integer> nonDuplicateLotto = new HashSet<>(numbers);
-        List<Integer> numberNonDuplicate = new ArrayList<>(nonDuplicateLotto);
-        if (numberNonDuplicate.size() != NUMBERS_LENGTH.getNumber()) {
-            throw new IllegalArgumentException(WINNUMBER_NO_DUPLICATE_LENGTH.getMessage());
-        }
-    }
-
-    private void lengthCheck(List<Integer> numbers) {
-        if (numbers.size() != NUMBERS_LENGTH.getNumber()) {
-            throw new IllegalArgumentException(WINNUMBER_LENGTH.getMessage());
-        }
+    @DisplayName("로또 번호에 1-45 범위 외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByRangeOverNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 46, 5)))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
 }

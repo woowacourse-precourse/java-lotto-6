852536b41dcae6baeb44fd918ef4bad8c1c2a9b6
shlihs95 <fhzptvh@gmail.com>
2023-11-08 04:30:37 +0900

    Fix: OutputView 출력 메시지 오탈자 수정, printPurchaseResult 메서드 파라미터 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,29 +11,28 @@ public class OutputView {
 
     private static final String INPUT_PURCHASE_MONEY = "구입급액을 입력해주세요.";
     private static final String PURCHASED_COUNT = "%n%d개를 구매했습니다.%n";
-    private static final String PURCHASED_NUMBER = "[%s%n]";
+    private static final String PURCHASED_NUMBER = "[%s]%n";
     private static final String INPUT_WIN_NUMBER = "%n당첨 번호를 입력해주세요.%n";
     private static final String INPUT_BONUS_NUMBER = "%n보너스 번호를 입력해 주세요.%n";
     private static final String WINNING_STATS = "%n당첨 통계%n";
     private static final String DIVISION_LINE = "---%n";
     private static final String CORRECTING_COUNT = "%s개 일치 (%s원) - %s개%n";
     private static final String CORRECTING_COUNT_WITH_BONUS = "%s개 일치, 보너스 볼 일치 (%s원) - %s개%n";
-    private static final String EARNINGS_RATIO = "총 수익률은 %.1f%n";
+    private static final String EARNINGS_RATIO = "총 수익률은 %.1f%입니다.%n";
     private static final String COUNT_WITH_BONUS = "5 + bonus";
 
     public void printInputPurchaseMoney() {
         System.out.println(INPUT_PURCHASE_MONEY);
     }
-    public void printPurchaseResult(int purchaseCount) {
+    public void printPurchaseResult(Lottos lottos, int purchaseCount) {
         System.out.printf(PURCHASED_COUNT, purchaseCount);
 
-        Lottos lottos = new Lottos();
         List<String> purchasedNumbers = lottos.printNumbers(purchaseCount);
 
         for (String numbers : purchasedNumbers) {
             System.out.printf(PURCHASED_NUMBER, numbers);
         }
-
+        System.out.println();
     }
 
     public void printInputWinningNumber() {

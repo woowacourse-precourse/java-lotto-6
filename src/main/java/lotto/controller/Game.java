d997f7fb5043bff1ef92f3e1e29ef397d7af9c0d
SCY <helenason@naver.com>
2023-11-08 00:55:48 +0900

    feat: 출력을 위한 view 생성 및 controller와 연결

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -2,13 +2,16 @@ package lotto.controller;
 
 import lotto.model.LottoGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Game {
 
     private InputView inputView;
+    private OutputView outputView;
 
     public Game() {
         this.inputView = new InputView();
+        this.outputView = new OutputView();
         init();
     }
 

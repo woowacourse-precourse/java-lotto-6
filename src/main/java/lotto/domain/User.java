d68b4f14f157105f2bc76d33739e63dd2862212e
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-07 19:34:14 +0900

    feat(User) : User 생성자 생성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,9 +1,17 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+
 // 로또 구매 및 저장, 구매 금액과 총 상금 관리
 public class User {
     private int buyingPrice;
     private int winningPrice;
     private List<UserLotto> lottos;
+
+    public User(int buyingPrice) {
+        lottos = new ArrayList<>();
+        this.buyingPrice = buyingPrice;
+        this.winningPrice = 0;
+    }
 }

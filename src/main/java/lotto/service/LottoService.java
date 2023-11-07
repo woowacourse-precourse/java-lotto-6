462adabf9c59cf96cf1733f670c9db116cc911cd
JGM <cnxw4570123@gmail.com>
2023-11-08 03:07:53 +0900

    feat(LottoService) : add parsing input to number method input을 받아 부호없는 정수 리스트로 반환하는 parseInputToNumberCandidates 메소드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,16 @@
 package lotto.service;
 
+import java.util.Arrays;
+import java.util.List;
+import lotto.constant.Message;
+
 public class LottoService {
+
+    public List<Integer> parseInputToNumberCandidates(String input) throws NumberFormatException{
+        return Arrays.stream(input.split(Message.COMMA))
+                .map(String::trim)
+                .map(Integer::parseUnsignedInt)
+                .toList();
+    }
+
 }

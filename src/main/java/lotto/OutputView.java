fdd70066198b458df36efae73907233a0bb8d825
Park Eunhyeong <silbroeh@gmail.com>
2023-11-07 17:51:06 +0900

    feat : OutputView클래스 - 메시지 상수 구현

---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class OutputView {
+    private static final String TICKET_NUMBER_MESSAGE = "%d개를 구매했습니다.";
+    private static final String LOTTO_NUMBERS_MESSAGE = "[%d, %d, %d, %d, %d, %d]";
+    private static final String RESULT_HEADLINE_MESSAGE = "당첨 통계\n---";
+    private static final String PRIZE_FREQ_MESSAGE = "%d개 일치 (%s원)%s - %d개";
+    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+}

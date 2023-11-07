37b092a7b8b7d12728030127d43f5eb003bdf2ca
D0ri123 <lsh81550@gmail.com>
2023-11-08 07:46:52 +0900

    test(BonusNumberService): BonusNumberService 테스트 로직 구현 - 4차 구현

--- src/test/java/lotto/service/BonusNumberServiceTest.java
+++ src/test/java/lotto/service/BonusNumberServiceTest.java
@@ -6,22 +6,36 @@ import lotto.model.Bonus;
 import lotto.model.WinningNumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BonusNumberServiceTest {
 
     BonusNumberService bonusNumberService;
+    WinningNumber winningNumber;
 
     @BeforeEach
     void setUp() {
         bonusNumberService = new BonusNumberService();
+        winningNumber = new WinningNumber("1,2,3,4,5,6");
     }
 
     @Test
-    void 옳은_값이_들어오지_않으면_null을_반환한다() {
-        WinningNumber winningNumber = new WinningNumber("1,2,3,4,5,6");
-
+    void 옳은_값이_들어오면_Bonus를_생성한다() {
         Bonus bonus = bonusNumberService.getBonusNumberIfValid(winningNumber, "7");
         assertEquals(bonus.getNumber(), 7);
+        assertNotEquals(bonus, null);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "5",
+        "48",
+        "sdf"
+    })
+    void 옳은_값이_들어오지_않으면_Bonus를_생성하지_않는다(String bonusNumber) {
+        Bonus bonus = bonusNumberService.getBonusNumberIfValid(winningNumber, bonusNumber);
+        assertNull(bonus);
     }
 
 }

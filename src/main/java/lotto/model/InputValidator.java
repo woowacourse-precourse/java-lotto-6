ef1fe65d425b3fe4a3a7ea7287c2b2c1074149bb
Dawin <dawin002@naver.com>
2023-11-07 21:58:36 +0900

    feat(InputValidator): 사용자 입력 검증 기능 추가
    사용자 입력값이 빈 문자열이 아닌지 검증:
    - void checkIsNotBlank(String input) throws IllegalArgumentException
    - 빈 문자열이라면 예외 발생 시킴

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -8,4 +8,10 @@ public class InputValidator {
             throw new IllegalArgumentException("정수가 아닌 값이 입력되었습니다.");
         }
     }
+
+    public void checkIsNotBlank(String input) throws IllegalArgumentException {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
+        }
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.model.InputValidator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
@@ -16,8 +15,8 @@ public class InputView {
 
     public int readPurchaseMoney() {
         String input = Console.readLine();
-        // validator.checkCanParseInt(input);
-        // validator.checkIsNotBlank(input);
+        validator.checkIsNotBlank(input);
+        validator.checkCanParseInt(input);
         return Integer.parseInt(input);
     }
 
@@ -26,7 +25,8 @@ public class InputView {
         List<Integer> numberList = new ArrayList<>();
         String[] splitInput = input.split(",");
         for (String singleInput : splitInput) {
-            // validator.checkCanParseInt(singleInput);
+            validator.checkIsNotBlank(singleInput);
+            validator.checkCanParseInt(singleInput);
             int number = Integer.parseInt(singleInput);
             numberList.add(number);
         }
@@ -35,8 +35,8 @@ public class InputView {
 
     public int readBonusNumber() {
         String input = Console.readLine();
-        // validator.checkCanParseInt(input);
-        // validator.checkIsNotBlank(input);
+        validator.checkIsNotBlank(input);
+        validator.checkCanParseInt(input);
         return Integer.parseInt(input);
     }
 }

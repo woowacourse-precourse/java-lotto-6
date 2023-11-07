f7c876573e498e3fe48b1defb46d5039ac83821b
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 01:33:56 +0900

    fix: 수익률 메세지에 float 소수점 조건 설정

--- src/main/java/lotto/message/Message.java
+++ src/main/java/lotto/message/Message.java
@@ -3,9 +3,9 @@ package lotto.message;
 public class Message {
     public static final String PROMPT_ENTERING_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String PROMPT_ENTERING_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
-    public static final String PROMPT_ENTERING_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public static final String WINNING_STATISTICS = "당첨 통계";
+    public static final String PROMPT_ENTERING_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS = "\n당첨 통계";
     public static final String GUIDE_PURCHASED_QUANTITY = "\n%d개를 구매하였습니다.";
-    public static final String GUIDE_TOTAL_ROR = "총 수익률은 %d%%입니다.";
+    public static final String GUIDE_TOTAL_ROR = "총 수익률은 %.1f%%입니다.";
     public static final String DIVIDER = "---";
 }

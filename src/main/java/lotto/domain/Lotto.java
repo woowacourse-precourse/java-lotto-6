bae39970c9b561c7d4b5194e3b961fa19d38daf4
LastCow9000 <sys19912002@hanmail.net>
2023-11-07 23:08:12 +0900

    refactor: 등수 계산하는 메서드 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
@@ -28,31 +29,53 @@ public class Lotto {
         return numbers.contains(number);
     }
 
-    //@todo: refactoring
     public int judge(Lotto winningLotto, BonusNumber bonusNumber) {
-        HashSet<Integer> judgementer = new HashSet<>();
-        judgementer.addAll(numbers);
-        judgementer.addAll(winningLotto.getSortedNumbers());
+        int size = deduplicateSize(winningLotto);
+        int sameCount = getSameCount(size);
 
-        int result = LOTTO_SIZE * 2 - judgementer.size();
+        if (sameCount == 5) {
+            return caculate2Or3Rank(bonusNumber);
+        }
 
-        if (result == 6) {
-            return 1;
-        }
-        if (result == 5) {
-            if (numbers.stream().anyMatch(bonusNumber::isEqual)) {
-                return 2;
-            }
-            return 3;
-        }
-        if (result == 4) {
-            return 4;
-        }
-        if (result == 3) {
-            return 5;
+        return calculateRank(sameCount);
+    }
+
+    private int deduplicateSize(Lotto winningLotto) {
+        HashSet<Integer> deduplicater = new HashSet<>();
+        deduplicater.addAll(numbers);
+        deduplicater.addAll(winningLotto.getSortedNumbers());
+
+        return deduplicater.size();
+    }
+
+    private int getSameCount(int size) {
+        return LOTTO_SIZE * 2 - size;
+    }
+
+    private int calculateRank(int sameCount) {
+        Map<Integer, Integer> rankByCount = Map.of(
+                6, 1,
+                4, 4,
+                3, 5
+        );
+
+        if (rankByCount.containsKey(sameCount)) {
+            return rankByCount.get(sameCount);
         }
 
         return -1;
+    }
+
+    private int caculate2Or3Rank(BonusNumber bonusNumber) {
+        if (isSecondRank(bonusNumber)) {
+            return 2;
+        }
+        return 3;
+    }
+
+    private boolean isSecondRank(BonusNumber bonusNumber) {
+        return numbers.stream()
+                .anyMatch(bonusNumber::isEqual);
     }
 
     private void validateSize(List<Integer> numbers) {

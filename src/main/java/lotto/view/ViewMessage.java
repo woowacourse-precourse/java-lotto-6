d0d8cc59ac70af1df79ba6af7195e2e3c531ec69
waveofmymind <sjun0913@gmail.com>
2023-11-07 13:41:40 +0900

    refactor: 출력 요구사항에 맞춰 메시지를 수정한다.

--- src/main/java/lotto/view/ViewMessage.java
+++ src/main/java/lotto/view/ViewMessage.java
@@ -12,7 +12,7 @@ enum ViewMessage {
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
 
     OUTPUT_PURCHASED_LOTTOS("개를 구매했습니다."),
-    OUTPUT_PROFIT_RATE("총 수익률은 %.2f%%입니다."),
+    OUTPUT_PROFIT_RATE("총 수익률은 %.1f%%입니다."),
     OUTPUT_WINNING_STATISTICS("당첨 통계");
 
     final private String message;
@@ -32,9 +32,9 @@ enum ViewMessage {
     public static String getWinningResultMessage(Rank rank, int count) {
         NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
         if (rank == Rank.SECOND) {
-            return rank.getMatchCount() + "개 일치, 보너스 볼 일치(" + formatter.format(rank.getWinningMoney()) + "원) - " + count + "개";
+            return rank.getMatchCount() + "개 일치, 보너스 볼 일치 (" + formatter.format(rank.getWinningMoney()) + "원) - " + count + "개";
         }
-        return rank.getMatchCount() + "개 일치(" + formatter.format(rank.getWinningMoney()) + "원) - " + count + "개";
+        return rank.getMatchCount() + "개 일치 (" + formatter.format(rank.getWinningMoney()) + "원) - " + count + "개";
     }
 
     public static String getProfitMessage(double profit) {

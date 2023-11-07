692beaf1f9752d81d1b98056629e4ecf08313670
Son JunHyung <kingjh1125@gmail.com>
2023-11-07 22:19:47 +0900

    style (RandomNumber): 상수 네이밍 변경
    로또 번호 수를 나타내는 상수의 네이밍을 변경하였습니다:
    before:
    - COUNT_NUMBER
    after:
    - SELECT_NUMBER

--- src/main/java/service/RandomNumber.java
+++ src/main/java/service/RandomNumber.java
@@ -10,10 +10,10 @@ import java.util.List;
 public class RandomNumber {
     private static int MIN_NUMBER = 1;
     private static int MAX_NUMBER = 45;
-    private static int COUNT_NUMBER = 6;
+    private static int SELECT_NUMBER = 6;
 
     public static Lotto getRandomNumber(){
-        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT_NUMBER));
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, SELECT_NUMBER));
         sort(numbers);
         return new Lotto(numbers);
     }

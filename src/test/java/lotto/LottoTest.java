1a796333756f1f11ced1681e77538fdb94f40f6f
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-07 17:48:33 +0900

    test(LottoTest): 1~45 숫자 범위를 테스트하는 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -16,6 +16,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호가 1~45가 아니면 예외 발생")
+    @Test
+    void createLottoByNumberRange(){
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {

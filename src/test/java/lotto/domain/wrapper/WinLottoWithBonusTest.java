0eb0ae9254ce3745141e165f09e87bb749f61322
noxknow <chris2769@naver.com>
2023-11-07 18:29:54 +0900

    test(WinLottoWithBonusTest) : 보너스 번호가 당첨 번호와 중복이라면 예외가 발생한다

--- src/test/java/lotto/domain/wrapper/WinLottoWithBonusTest.java
+++ src/test/java/lotto/domain/wrapper/WinLottoWithBonusTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static lotto.handler.ErrorHandler.DUPLICATE_NUMBER;
 import static lotto.handler.ErrorHandler.INCONVERTIBLE_TYPE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,6 +22,14 @@ public class WinLottoWithBonusTest {
         );
     }
 
+    private static Stream<Arguments> generateDuplicateNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 10, 20, 30, 40, 45), "1"),
+                Arguments.of(List.of(1, 3, 5, 6, 10, 11), "10"),
+                Arguments.of(List.of(1, 3, 5, 6, 25, 36), "25")
+        );
+    }
+
     @DisplayName("보너스 번호에 숫자로 변환될 수 없는 타입이 들어가면 예외가 발생한다.")
     @ParameterizedTest(name = "[{index}] input {1} " )
     @MethodSource("generateIncovertibleNumber")
@@ -28,5 +37,14 @@ public class WinLottoWithBonusTest {
         assertThatThrownBy(() -> WinLottoWithBonus.create(winningLotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INCONVERTIBLE_TYPE.getException().getMessage());
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복이라면 예외가 발생한다.")
+    @ParameterizedTest(name = "[{index}] input {0} {1} " )
+    @MethodSource("generateDuplicateNumber")
+    void createWinLottoWithBonusByDuplicate(List<Integer> winningLotto, String bonusNumber) {
+        assertThatThrownBy(() -> WinLottoWithBonus.create(winningLotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATE_NUMBER.getException().getMessage());
     }
 }

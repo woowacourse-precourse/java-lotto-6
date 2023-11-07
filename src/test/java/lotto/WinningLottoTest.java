3ea5a23b6769e5c6c9b22ba8383b652b008664d9
yeondori <yeonseo.jung0@gmail.com>
2023-11-08 00:05:58 +0900

    refactor: 생성자 호출 방식 -> of 메서드 사용 방식으로 변경

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -11,34 +11,35 @@ class WinningLottoTest {
     @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createWinningLottoByOverSize() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 45))
+        assertThatThrownBy(() -> WinningLotto.of(List.of(1, 2, 3, 4, 5, 6, 7), 45))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createWinningLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 5), 45))
+        assertThatThrownBy(() -> WinningLotto.of(List.of(1, 2, 3, 4, 5, 5), 45))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
     @DisplayName("당첨 번호가 1~45 사이에 없으면 예외가 발생한다.")
     @Test
     void createWinningLottoByOutOfRangeNumber() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 46), 45))
+        assertThatThrownBy(() -> WinningLotto.of(List.of(1, 2, 3, 4, 5, 46), 45))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 번호가 1~45 사이에 없으면 예외가 발생한다.")
     @Test
     void createWinningLottoByWrongBonusNumber() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 46))
+        assertThatThrownBy(() -> WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
     @Test
     void createWinningLottoByDuplicatedBonusNumber() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 6))
+        assertThatThrownBy(() -> WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

a5e25084060a7da03fd41674abcf16c646495a0a
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 15:41:00 +0900

    refactor : 금액 입력 테스트 DisplayName 수정

--- src/test/java/view/InputTest.java
+++ src/test/java/view/InputTest.java
@@ -7,34 +7,34 @@ import lotto.exception.input.NotIntegerException;
 import lotto.exception.input.NullInputException;
 import lotto.validator.InputValidator;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputTest {
-    @DisplayName("입력이 공백일 시 예외가 발생한다.")
+    @DisplayName("금액 입력이 공백일 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {""," "})
     void inputNull(String input) {
-        assertThatThrownBy(() -> InputValidator.validateInput(input))
+        assertThatThrownBy(() -> InputValidator.validateInputPrice(input))
                 .isInstanceOf(NullInputException.class);
     }
 
-    @DisplayName("입력이 숫자가 아닐 시 예외가 발생한다.")
+    @DisplayName("금액 입력이 숫자가 아닐 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"a","1,a","a,b","1,2,3,a"})
     void inputNotInteger(String input){
-        assertThatThrownBy(() -> InputValidator.validateInput(input))
+        assertThatThrownBy(() -> InputValidator.validateInputPrice(input))
                 .isInstanceOf(NotIntegerException.class);
     }
 
-    @DisplayName("입력에 공백이 포함될 시 예외가 발생한다.")
+    @DisplayName("금액 입력에 공백이 포함될 시 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1, 2,3","1 0,11", "1 0, 11","1 1"})
     void inputHasSpace(String input){
-        assertThatThrownBy(() -> InputValidator.validateInput(input))
+        assertThatThrownBy(() -> InputValidator.validateInputPrice(input))
                 .isInstanceOf(HasSpaceException.class);
     }
+
 
 
 }

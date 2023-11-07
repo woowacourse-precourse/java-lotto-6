04146bfea27073d46a87f1649da76729a8a3d4c3
ffolabear <ffolabear@gmail.com>
2023-11-08 03:37:42 +0900

    feat : 당첨 로또 정보를 입력하는 기능 구현

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -14,10 +14,17 @@ public class LottoService {
     public void startService(User buyer) {
         this.user = buyer;
         sellLotto(buyer);
+        generateWinningLotto();
     }
 
     private void sellLotto(User buyer) {
         buyer.enterDraw(inputUtil.inputMoney());
+    }
+
+    private void generateWinningLotto() {
+        List<Integer> winningNumbers = inputUtil.inputWinningNumbers();
+        this.winningLotto = new Lotto(winningNumbers);
+        this.bonusNumber = new LottoNumber(inputUtil.inputBonusNumber());
     }
 
 }

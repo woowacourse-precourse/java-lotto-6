3a5dace1624880213ac769b4650ee6fa6c5e9ade
박승찬 <seungchan141414@gmail.com>
2023-11-08 02:39:17 +0900

    [Test(lotto)]  랜덤 숫자 생성 범위 1~45인가 확인 Test

--- src/test/java/lotto/domain/number/RanNumbersTest.java
+++ src/test/java/lotto/domain/number/RanNumbersTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +28,26 @@ class RanNumbersTest {
             // than
             assertThat(size).isEqualTo(6);
         }
+    }
+
+    @DisplayName("랜덤 숫자 리스트의 의 숫자의 범위가 1~45인지 확인")
+    @Test
+    void createLottoNumbers_랜덤_숫자_범위_검증() {
+
+        for (int i = 0; i < 1000; i++) {
+            // given
+            List<Integer> numbers = RanNumbers.createLottoNumbers();
+
+            // than
+            assertThat(numbers)
+                    .allMatch((value) -> isRange(value));
+        }
+    }
+
+    private boolean isRange(Integer value) {
+        if (value >= 1 && value <= 45) {
+            return true;
+        }
+        return false;
     }
 }

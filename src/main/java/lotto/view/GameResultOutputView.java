bfef43e2305a006fc88e9a74505c252af9992984
BigBird <waujoxc@gmail.com>
2023-11-08 01:14:14 +0900

    feat(lotto): 발행한 로또 수량 및 번호를 오름차순 정렬 후 출력 기능 구현

--- src/main/java/lotto/view/GameResultOutputView.java
+++ src/main/java/lotto/view/GameResultOutputView.java
@@ -5,7 +5,6 @@ import lotto.model.Lotto;
 import lotto.model.LottoTicket;
 
 public class GameResultOutputView {
-
     private static final String PURCHASED_COUNT_MESSAGE = "개를 구매했습니다.";
 
     public static void showLottoTickets(LottoTicket lottoTicket) {

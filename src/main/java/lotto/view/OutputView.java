0eb14733cd5f89d2b130415145e78c3bca96618a
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 02:42:51 +0900

    feat(printLottoPurchaseCountMessage): 구입 개수 안내 메세지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,13 @@ package lotto.view;
 
 public class OutputView {
     private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
 
     public void printLottoPurchaseInputMessage() {
         System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
+    }
+
+    public void printLottoPurchaseCountMessage(int LottoPurchaseCount) {
+        System.out.println(LottoPurchaseCount + LOTTO_PURCHASE_COUNT_MESSAGE);
     }
 }

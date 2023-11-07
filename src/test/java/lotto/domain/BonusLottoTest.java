a5186b58d14e82aa39f4eefbe684bff0c7b4f126
lee jihyeon <lejinhy@gmail.com>
2023-11-07 19:05:37 +0900

    refactor(test): 사용하지 않는 import 삭제, 객체 생성 간단하게 수정

--- src/test/java/lotto/domain/BonusLottoTest.java
+++ src/test/java/lotto/domain/BonusLottoTest.java
@@ -1,11 +1,8 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusLottoTest {
@@ -13,15 +10,13 @@ class BonusLottoTest {
     @Test
     void 로또번호와_같은숫자() {
         assertThrows(IllegalArgumentException.class, () -> {
-            BonusLotto bonusLotto = new BonusLotto(1,
-                    new Lotto(List.of(1,2,3,4,5,6)));});
+            new BonusLotto(1, new Lotto(List.of(1,2,3,4,5,6)));});
     }
 
     @Test
     void 범위_넘는_숫자() {
         assertThrows(IllegalArgumentException.class, () -> {
-            BonusLotto bonusLotto = new BonusLotto(100,
-                    new Lotto(List.of(1,2,3,4,5,6)));});
+            new BonusLotto(100, new Lotto(List.of(1,2,3,4,5,6)));});
     }
 
 }

2170037ff92ce9e67776a4bcfb1cc5b0597a058b
yunju-m <dbswnfnal@naver.com>
2023-11-07 20:54:20 +0900

    Test(LottoTest): ☔️ 보너스 로또 번호 중복 여부 판단하는 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -51,4 +51,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("보너스 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createBonusLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new LuckeyLotto(List.of(1, 2, 3, 4, 5, 6), "5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

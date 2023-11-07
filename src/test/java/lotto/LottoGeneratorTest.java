8affc2449387eec6257ca7c48c536cf97eb57011
dokkisan <dokkisan.wang@gmail.com>
2023-11-07 18:58:25 +0900

    test(lotto-generator): 구매 금액 만큼 로또를 발행하는 기능 테스트 작성

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGeneratorTest {
+
+    private LottoGenerator lottoGenerator;
+
+    @BeforeEach
+    void init() {
+        lottoGenerator = new LottoGenerator();
+    }
+
+    @DisplayName("구매 수량에 따라 로또를 발행한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 15, 60, 100})
+    void buyLottoWith(int purchaseQuantity) {
+
+        List<Lotto> lottoTickets = lottoGenerator.buy(purchaseQuantity);
+
+        assertThat(lottoTickets).hasSize(purchaseQuantity);
+
+        assertThat(lottoTickets)
+                .doesNotContainNull()
+                .allMatch(lotto -> lotto.getNumbers().size() == LottoGenerator.NUMBER_COUNT);
+    }
+}

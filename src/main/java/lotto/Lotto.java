2fbc1842c684907b912e94262fe06407482132e2
kg9585 <kg9585@naver.com>
2023-11-07 16:10:27 +0900

    feat(Lotto): 중복 유효성 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,35 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    public static final String DUPLICATE_ERROR = "중복된 번호는 사용할 수 없습니다.";
+    public static final String SIZE_ERROR = "로또 번호는 6개만 가능합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicates(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SIZE_ERROR);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicates(List<Integer> numbers) {
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR);
+        }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
+    }
 }
+

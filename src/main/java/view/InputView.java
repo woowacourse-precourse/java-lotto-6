9567ed432d92d83b2c21d4f36636d811ced1d401
PJH <parkjh0858@gmail.com>
2023-11-07 17:17:27 +0900

    refactor: 티켓 매수에 따른 로또 생성 기능 추가, 로또 가격 음수 확인 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
@@ -8,13 +9,17 @@ import lotto.Lotto;
 public class InputView {
     private static final int START = 1;
     private static final int END = 45;
-    private static List<Integer> winnerLotto;
+    private static final int SIZE = 6;
+
+    private static List<Integer> winner;
+
     public static int inputNumber() {
         while (true) {
             try {
                 System.out.println("구입금액을 입력해 주세요.");
                 int count = convertToInt(Console.readLine());
                 countCheck(count);
+                negativeCheck(count);
 
                 return count;
             } catch (IllegalArgumentException exception) {
@@ -23,13 +28,30 @@ public class InputView {
         }
     }
 
+    public static List<Lotto> generateLotto(int count) {
+        int index = 0;
+        List<Lotto> lottos = new ArrayList<>();
+
+        while (index < count) {
+            try {
+                List<Integer> lotto = Randoms.pickUniqueNumbersInRange(START, END, SIZE);
+                lottos.add(new Lotto(lotto));
+                index += 1;
+            } catch (IllegalArgumentException exception) {
+                System.out.println("[ERROR] 로또 번호를 확인해 주세요.");
+            }
+        }
+
+        return lottos;
+    }
+
     public static Lotto inputWinnerLotto() {
         while (true) {
             try {
                 System.out.println("당첨 번호를 입력해 주세요.");
-                makeLottoList(Console.readLine());
+                winner = makeLottoList(Console.readLine());
 
-                return new Lotto(winnerLotto);
+                return new Lotto(winner);
            } catch (IllegalArgumentException exception) {
                 System.out.println(exception.getMessage());
             }
@@ -37,11 +59,13 @@ public class InputView {
     }
 
     public static int inputBonusNumber() {
+        System.out.println();
+
         while (true) {
             try {
                 System.out.println("보너스 번호를 입력해 주세요.");
                 int bonusNumber = convertToInt(Console.readLine());
-                duplicationCheck(bonusNumber);
+                duplicationCheck(winner, bonusNumber);
                 rangeCheck(bonusNumber);
 
                 return bonusNumber;
@@ -57,6 +81,12 @@ public class InputView {
         }
     }
 
+    public static void negativeCheck(int count) throws IllegalArgumentException {
+        if (count < 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구매 가격은 0이상의 정수로 입력해주세요.");
+        }
+    }
+
     public static int convertToInt(String number) {
         try {
             int count = Integer.parseInt(number);
@@ -66,8 +96,8 @@ public class InputView {
         }
     }
 
-    public static void duplicationCheck(int bonusNumber) throws IllegalArgumentException {
-        if (winnerLotto.contains(bonusNumber)) {
+    public static void duplicationCheck(List<Integer> lotto, int bonusNumber) throws IllegalArgumentException {
+        if (lotto.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호가 보너스 번호를 포함하고 있습니다.");
         }
     }
@@ -78,12 +108,14 @@ public class InputView {
         }
     }
 
-    public static void makeLottoList(String winningNumber) {
+    public static List<Integer> makeLottoList(String winningNumber) {
         String[] numbers = winningNumber.split(",");
-        winnerLotto = new ArrayList<>();
+        List<Integer> lotto = new ArrayList<>();
 
         for (String number : numbers) {
-            winnerLotto.add(convertToInt(number));
+            lotto.add(convertToInt(number));
         }
+
+        return lotto;
     }
 }

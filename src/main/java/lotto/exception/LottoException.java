28185dce44cad4f48edab7b4a8cae657b5d5057d
jiu6525 <jiu6525@daum.net>
2023-11-07 17:46:45 +0900

    feat : 구입 금액이 정상입력인지 판단하는 예외 메서드 작성

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,19 @@
+package lotto.exception;
+
+import lotto.LottoConfig;
+
+public class LottoException {
+
+    private final LottoConfig lottoConfig = new LottoConfig();
+    private static final int ZERO_PRICE = 0;
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String TICKET_ERROR_MESSAGE = "구입 금액이 잘못 되었습니다. 티켓 한장의 가격은 1000원입니다.";
+
+    private boolean purchaseAmountCheck(String purchaseAmount) {
+        int amount = Integer.parseInt(purchaseAmount);
+        if ((amount < lottoConfig.TICKET_PRICE) || ((amount % lottoConfig.TICKET_PRICE) != ZERO_PRICE)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + TICKET_ERROR_MESSAGE);
+        }
+        return true;
+    }
+}

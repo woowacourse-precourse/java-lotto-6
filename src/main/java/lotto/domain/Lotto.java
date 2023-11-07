48e24bfb700f60137f480e4eaf03f0d2737622df
KDT live <dnjsxo0616@gmail.com>
2023-11-08 00:32:57 +0900

    refactor(Lotto): 로또 번호 개수 상수 추가 및 validate() 메서드 이름 변경
    - validate() 메서드 validateSize()로 변경.
    - LOTTO_SIZE를 사용하여 로또 번호 개수 검증.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,18 +3,19 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호가 6개가 아닙니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
 }

b2db2d97c49d9a3c1223491dc2c5d84c5d1ec393
yubin <dbqls2821@gmail.com>
2023-11-07 22:54:09 +0900

    test: 로또 발행 테스트코드 추가

---
+++ src/test/java/lotto/domain/LottoPublisherTest.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import lotto.io.LottoInput;
+import lotto.io.LottoOutput;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoPublisherTest {
+
+    @Test
+    void 로또_발행_테스트() {
+        LottoPublisher publisher = new LottoPublisher(() -> List.of(1, 2, 3, 4, 5, 6), new LottoController(new LottoInput(() -> ""), new LottoOutput(new LottosConverter(), new StatisticsConverter())));
+        Money money = new Money(1000);
+        Lottos expected = new Lottos();
+        expected.addLotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Lottos actual = publisher.publish(money);
+
+        assertEquals(expected, actual);
+
+    }
+
+}

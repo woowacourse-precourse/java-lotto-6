69d038b7512689cf75084236eaadb7404cf37d1c
WinterHana <haha920@naver.com>
2023-11-07 16:51:24 +0900

    test : calculateReturnTest 추가
    - 수익률 계산에 대한 정상 테스트 코드 추가

--- src/test/java/lotto/domain/LottoResultCalculationTest.java
+++ src/test/java/lotto/domain/LottoResultCalculationTest.java
@@ -20,8 +20,8 @@ class LottoResultCalculationTest {
     @Test
     void InputMoneyException() {
         assertThatThrownBy(() -> {
-            lottoResultCalculation.caculateLottoTicket(16500);
-        }).isInstanceOf(IllegalArgumentException.class);
+            lottoResultCalculation.caculateLottoTicket(16500);})
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 결과에 대한 정상 테스트")
@@ -29,13 +29,23 @@ class LottoResultCalculationTest {
     void checkResultTest() {
         assertThat(lottoResultCalculation.checkResult
                 (List.of(20, 30, 24, 1, 3, 4)
-                , List.of(30, 24, 5, 8, 10, 20))).isEqualTo(3);
+                , List.of(30, 24, 5, 8, 10, 20)))
+                .isEqualTo(3);
     }
 
     @DisplayName("보너스 숫자에 대한 정상 테스트")
     @Test
     void checkBounsNumberTest() {
         assertThat(lottoResultCalculation.checkBounsNumber
-                (List.of(20, 30, 24, 1, 3, 4), 3)).isTrue();
+                (List.of(20, 30, 24, 1, 3, 4), 3))
+                .isTrue();
+    }
+
+    @DisplayName("수익률 계산에 대한 정상 테스트")
+    @Test
+    void calculateReturnTest() {
+        lottoResultCalculation.caculateLottoTicket(15000);
+        assertThat(lottoResultCalculation.calculateReturn(50000))
+                .isEqualTo(333.3);
     }
 }

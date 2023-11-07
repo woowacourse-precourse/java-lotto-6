2df43208a39e44d36c301ec92fe9b6debf5ecdf0
bin <gimgyubin1225@gmail.com>
2023-11-07 21:03:01 +0900

    feat: null값 체크 기능 추가

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,12 +1,18 @@
 package lotto.validator;
 
+import lotto.view.Output;
+
 public class InputValidator {
+
+    private static final String ERROR_MESSAGE_NULL = "값을 입력해주세요.";
+    private static final String ERROR_MESSAGE_NOT_INTEGER = "정수를 입력해주세요.";
 
     public static void isInteger(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.");
+            Output.printErrorMessage(ERROR_MESSAGE_NOT_INTEGER);
+            throw new IllegalArgumentException();
         }
     }
 
@@ -16,7 +22,25 @@ public class InputValidator {
                 Integer.parseInt(inputs[i]);
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.");
+            Output.printErrorMessage(ERROR_MESSAGE_NOT_INTEGER);
+            throw new IllegalArgumentException();
         }
+    }
+
+    public static void isNull(String input){
+        if(input == null) {
+            Output.printErrorMessage(ERROR_MESSAGE_NULL);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isNull(String[] inputs){
+        for (int i = 0; i < inputs.length; i++) {
+            if(inputs[i] == null) {
+                Output.printErrorMessage(ERROR_MESSAGE_NULL);
+                throw new IllegalArgumentException();
+            }
+        }
+
     }
 }

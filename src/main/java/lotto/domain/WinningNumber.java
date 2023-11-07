86c43ff35e8519764ede5bbb93d2f53879aae676
dbstn1325 <dbstn1325@naver.com>
2023-11-07 11:10:05 +0900

    feat(WinningNumber): 당첨 번호가 1~45의 숫자가 아니면 IllegalArgumentException 예외처리 기능 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
 
+import java.util.List;
+
 public class WinningNumber {
     private static final String WINNING_NUMBER_MUST_NUMERIC_MESSAGE = "[ERROR] 당첨 번호는 숫자여야 합니다.";
+    private static final String WINNING_NUMBER_RANGE_MESSAGE = "[ERROR] 당첨 번호는 1~45의 숫자여야 합니다.";
+    private static final int START_RANGE_VALUE = 1;
+    private static final int END_RANGE_VALUE = 45;
     private final int winningNumber;
 
     private WinningNumber(int winningNumber) {
+        validateRange(winningNumber);
         this.winningNumber = winningNumber;
     }
 
@@ -16,6 +22,16 @@ public class WinningNumber {
         } catch (NumberFormatException ex) {
             throw new IllegalArgumentException(WINNING_NUMBER_MUST_NUMERIC_MESSAGE);
         }
+    }
+
+    private static void validateRange(int number) {
+        if (isOverRange(number)) {
+            throw new IllegalArgumentException(WINNING_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    private static boolean isOverRange(int number) {
+        return number < START_RANGE_VALUE || number > END_RANGE_VALUE;
     }
 
     public int toInt() {

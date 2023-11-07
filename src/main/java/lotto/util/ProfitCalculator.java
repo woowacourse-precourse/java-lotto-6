9f99e4d3214892066f169dcd53f6533afefaae65
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-07 22:17:09 +0900

    fix(ProfitCalculator): 반올림 수정 및 'div by 0'관련 예외 처리 추가

--- src/main/java/lotto/util/ProfitCalculator.java
+++ src/main/java/lotto/util/ProfitCalculator.java
@@ -5,10 +5,13 @@ import java.math.RoundingMode;
 
 public class ProfitCalculator {
     private static final int PERCENT = 100;
-    private static final int SCALE = 1;
+    private static final int SCALE = 3;
     public static float calculateLottoReturnRate(int totalCost, long totalPrize) {
-        if (totalPrize == 0) {
+        if (totalPrize <= 0) {
             return 0f;
+        }
+        if (totalCost == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
         }
 
         BigDecimal prize = new BigDecimal(totalPrize);

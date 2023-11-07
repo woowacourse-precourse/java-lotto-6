11f9046250c4088af263ad2a091a191dd07d3081
깃wooqqq <sge0225@naver.com>
2023-11-08 05:29:47 +0900

    style: 필요없는 줄바꿈 삭제

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -6,11 +6,9 @@ import java.util.List;
 public class LottoGenerator {
     public List<Lotto> generateLottos(int ticket) {
         List<Lotto> lottos = new ArrayList<>();
-
         while (lottos.size() < ticket) {
             lottos.add(generateLotto());
         }
-
         return lottos;
     }
 

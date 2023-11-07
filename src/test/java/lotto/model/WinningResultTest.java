7b0a69c603485d5d67d5579c7a18c7fa339e3409
goodchoi <jangu3384@gmail.com>
2023-11-07 20:36:22 +0900

    test: 전체 당첨금액 계산 테스트 추가

--- src/test/java/lotto/model/WinningResultTest.java
+++ src/test/java/lotto/model/WinningResultTest.java
@@ -1,17 +1,26 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningResultTest {
+    WinningResult winningResult;
+
+    @BeforeEach
+    void setWinningResult() {
+        winningResult = new WinningResult();
+    }
 
     @DisplayName("등수의 갯수를 저장할 수 있고, 가져올 수 있다.")
     @Test
     void plusLottoPlace() {
         //given
-        WinningResult winningResult = new WinningResult();
         LottoPlace calculatedLottoPlace = LottoPlace.THIRD;
         winningResult.plusLottoPlace(calculatedLottoPlace);
 
@@ -19,5 +28,19 @@ class WinningResultTest {
         Integer placeCount = winningResult.getPlaceCount(calculatedLottoPlace);
 
         assertThat(placeCount).isEqualTo(1);
+    }
+
+
+    @DisplayName("전체 당첨금액을 계산할 수 있다.")
+    void caculateTotalRevenue() {
+        //given
+        winningResult.plusLottoPlace(LottoPlace.FIFTH);
+        winningResult.plusLottoPlace(LottoPlace.FIFTH);
+        winningResult.plusLottoPlace(LottoPlace.FOURTH);
+
+        Money totalRevenue = winningResult.calculateTotalRevenue();
+        Money expected = new Money("60000");
+
+        assertThat(totalRevenue).satisfies(totalRevenue::equals);
     }
 }

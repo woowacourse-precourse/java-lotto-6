97a8e6ebd4bb6f5163003b323652ec091edc73c4
KimSehoon <shggm2000@gmail.com>
2023-11-07 16:17:56 +0900

    refactor: compareLottoWithWinningNumbers method 분리

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -6,24 +6,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+import lotto.domain.Lotto;
+import lotto.domain.Ranking;
+import lotto.domain.WinningStatistics;
 import lotto.view.Input;
 
 public class GameManager {
     Input input = new Input();
 
-    private int getPurchaseQuantity(int purchaseAmount) {
-        validationPurchaseAmountInThousands(purchaseAmount);
-        return purchaseAmount / 1000;
-    }
-
-    private void validationPurchaseAmountInThousands(int purchaseAmount) {
-        if (purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException(DIVISIBLE_BY_1000.errorMessage());
-        }
-    }
-
     public List<Lotto> createLotto() {
-        int purchaseQuantity = input.getPurchaseAmount();
+        int purchaseQuantity = getPurchaseQuantity(input.getPurchaseAmount());
         List<Lotto> totalLotto = new ArrayList<>();
 
         IntStream.range(0, purchaseQuantity)
@@ -36,5 +28,42 @@ public class GameManager {
         return totalLotto;
     }
 
+    private int getPurchaseQuantity(int purchaseAmount) {
+        validationPurchaseAmountInThousands(purchaseAmount);
+        return purchaseAmount / 1000;
+    }
 
+    private void validationPurchaseAmountInThousands(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(DIVISIBLE_BY_1000.errorMessage());
+        }
+    }
+
+    public WinningStatistics compareLottoWithWinningNumbers(Lotto lotto, List<Integer> winningNumbers, int bonusNumber, WinningStatistics winningStatistics) {
+        int matchCount = lotto.getNumbers().stream().filter(winningNumbers::contains).toList().size();
+
+        updateWinningStatistics(lotto, matchCount, bonusNumber, winningStatistics);
+
+        return winningStatistics;
+    }
+
+    private void updateWinningStatistics(Lotto lotto, int matchCount, int bonusNumber, WinningStatistics winningStatistics) {
+        if (matchCount == 5) {
+            winningStatistics.incrementWinningStatus(compareLottoWithBonusNumber(lotto, bonusNumber));
+            return;
+        }
+
+        if (matchCount >= 3) {
+            winningStatistics.incrementWinningStatus(Ranking.findByMatchCount(matchCount));
+        }
+    }
+
+    private Ranking compareLottoWithBonusNumber(Lotto lotto, int bonusNumber) {
+        for (int number : lotto.getNumbers()) {
+            if (number == bonusNumber) {
+                return Ranking.FIVE_MATCHES_BONUS_MATCHES;
+            }
+        }
+        return Ranking.FIVE_MATCHES;
+    }
 }

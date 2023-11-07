a2e80b9d4e0f407402058caed6601511cee995ac
honi20 <1594cyh@gmail.com>
2023-11-08 01:19:25 +0900

    test: 로또 별 일치 여부 계산
    각 로또 별 당첨 번호와 일치 여부 계산

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +24,63 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("번호 일치 계산 결과 1등(6개)")
+    @Test
+    void compareAndResultFirst() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        Rank expectedRank = lotto.compareWithWinningNumbers(winningLotto);
+
+        assertThat(expectedRank).isEqualTo(Rank.FIRST);
+    }
+
+    @DisplayName("번호 일치 계산 결과 2등(5개+보너스)")
+    @Test
+    void compareAndResultSecond() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 6, 7));
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        Rank expectedRank = lotto.compareWithWinningNumbers(winningLotto);
+
+        assertThat(expectedRank).isEqualTo(Rank.SECOND);
+    }
+
+    @DisplayName("번호 일치 계산 결과 3등(5개)")
+    @Test
+    void compareAndResultThird() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 16));
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        Rank expectedRank = lotto.compareWithWinningNumbers(winningLotto);
+
+        assertThat(expectedRank).isEqualTo(Rank.THIRD);
+    }
+
+    @DisplayName("번호 일치 계산 결과 4등(4개)")
+    @Test
+    void compareAndResultFourth() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 7, 16));
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        Rank expectedRank = lotto.compareWithWinningNumbers(winningLotto);
+
+        assertThat(expectedRank).isEqualTo(Rank.FOURTH);
+    }
+
+    @DisplayName("번호 일치 계산 결과 5등(3개)")
+    @Test
+    void compareAndResultFifth() {
+        Lotto lotto = new Lotto(List.of(1, 12, 3, 14, 5, 16));
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        Rank expectedRank = lotto.compareWithWinningNumbers(winningLotto);
+
+        assertThat(expectedRank).isEqualTo(Rank.FIFTH);
+    }
+
+    @DisplayName("번호 일치 계산 결과 탈락")
+    @Test
+    void compareAndResultNone() {
+        Lotto lotto = new Lotto(List.of(1, 12, 13, 14, 15, 16));
+        List<Integer> winningLotto = List.of(1, 2, 3, 4, 5, 6, 7);
+        Rank expectedRank = lotto.compareWithWinningNumbers(winningLotto);
+
+        assertThat(expectedRank).isEqualTo(Rank.NONE);
+    }
 }

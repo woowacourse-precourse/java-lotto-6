2aaf5f62e3c923fc5232a0e6d0e7d2a2638f690f
yeop0740 <ssucgoo@gmail.com>
2023-11-08 00:18:20 +0900

    test: add test code for Lotto#getResult

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -89,4 +89,67 @@ class LottoTest {
         }
     }
 
+    @Nested
+    class getResult_메서드는 {
+
+        static List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        static int bonusNumber = 7;
+        static WinningLotto winningLotto;
+
+        @Test
+        void 동일한_숫자가_존재하지않으면_NONE을_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.NONE);
+        }
+
+        @Test
+        void 동일한_숫자가_1개_존재하면_NONE을_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.NONE);
+        }
+
+        @Test
+        void 동일한_숫자가_2개_존재하면_NONE를_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(11, 8, 9, 10, 5, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.NONE);
+        }
+
+        @Test
+        void 동일한_숫자가_3개_존재하면_FIFTH을_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(7, 4, 9, 10, 5, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.FIFTH);
+        }
+
+        @Test
+        void 동일한_숫자가_4개_존재하면_FOURTH를_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(3, 4, 9, 10, 5, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.FOURTH);
+        }
+
+        @Test
+        void 동일한_숫자가_5개_존재하면_THIRD를_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(3, 4, 2, 10, 5, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.THIRD);
+        }
+
+        @Test
+        void 동일한_숫자가_5개_존재하고_보너스_숫자를_포함하면_SECOND를_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(3, 4, 2, 7, 5, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.SECOND);
+        }
+        @Test
+        void 동일한_숫자가_6개_존재하면_FIRST을_리턴한다() {
+            winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+            Lotto lotto = new Lotto(List.of(3, 4, 2, 1, 5, 6));
+            Assertions.assertThat(lotto.getResult(winningLotto)).isEqualTo(Result.FIRST);
+        }
+    }
+
 }

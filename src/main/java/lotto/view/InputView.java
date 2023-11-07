63cfbb93f703aae91486eb549f195e5dcbcf9993
wda067 <mg6359@naver.com>
2023-11-08 00:31:42 +0900

    refactor(InputView): 변수를 enum에서 불러오도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,27 +1,27 @@
 package lotto.view;
 
+import static lotto.enums.Message.INPUT_BONUS_NUMBER_MESSAGE;
+import static lotto.enums.Message.INPUT_PURCHASE_AMOUNT_MESSAGE;
+import static lotto.enums.Message.INPUT_WINNING_NUMBERS_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     public String inputPurchaseAmount() {
-        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE.getMessage());
         return Console.readLine();
     }
 
     public String inputWinningNumbers() {
         System.out.println();
-        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE.getMessage());
         return Console.readLine();
     }
 
     public String inputBonusNumber() {
         System.out.println();
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE.getMessage());
         return Console.readLine();
     }
 }

b0c77ba3fcdb8ff77b1ca889da04c0122d748594
jeongjihun <wlgns2234@naver.com>
2023-11-08 00:51:39 +0900

    test: 테스트 메서드 이름 변경
    - 빈 결과값을 반환 한다는 의미로 EmptyResult를 메서드 명에 추가하였다.

--- src/test/java/lotto/model/LottoResultTest.java
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -17,4 +17,13 @@ class LottoResultTest {
 
         assertThat(result.isEmpty()).isTrue();
     }
+
+    @DisplayName("로또결과 생성 테스트 : 로또 당첨에 해당하지 않는다면 빈 값을 반환한다.")
+    @ParameterizedTest(name = "당첨번호와 일치개수가 {0}개라면 반환되는 당첨 결과는 없다.")
+    @ValueSource(ints = {0, 1, 2})
+    void createEmptyResult(int matchingNumbers) {
+        Optional<LottoResult> result = LottoResult.create(matchingNumbers, true);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
 }

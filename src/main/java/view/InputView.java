e23a486906ec31d760b19bfef99727114d91c67e
JunEunChae <celina03240@ewhain.net>
2023-11-07 19:38:01 +0900

    Feat: InputView 클래스 작성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,52 @@
+/**
+ * @Package_name   : view
+ * @Enum_name     : InputView
+ * <p>
+ * Create Date : 2023-11-07
+ * Create User : 정은채
+ */
 package view;
 
+import java.util.ArrayList;
+import utility.Utility;
+
 public class InputView {
+    /**
+     * Description : 로또구입금액 입력
+     *
+     * @Method : inputLottoBuy()
+     * @return : Integer
+     */
+    public Integer inputLottoBuy(){
+        Integer money = Integer.parseInt(Utility.campReadline());
+        return money;
+    }
+
+    /**
+     * Description : 당첨 번호 입력
+     *
+     * @Method : inputLottoNum()
+     * @return : ArrayList<Integer>
+     */
+    public ArrayList<Integer> inputLottoNum(){
+        String inputLottoNum = Utility.campReadline();
+        String[] LottoNums = inputLottoNum.split(",");
+        ArrayList<Integer> LottoNumbers = new ArrayList<>();
+        for (String lottoNum : LottoNums) {
+            LottoNumbers.add(Integer.valueOf(lottoNum));
+        }
+        return LottoNumbers;
+    }
+
+    /**
+     * Description : 보너스 번호 입력
+     *
+     * @Method : inputBonusNum()
+     * @return : Integer
+     */
+    public Integer inputBonusNum(){
+        Integer BonusNum = Integer.parseInt(Utility.campReadline());
+        return BonusNum;
+    }
+
 }

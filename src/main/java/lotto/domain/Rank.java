54b18c689c2dfbb7a8203cfd4133ecc6c23f7e1d
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-08 00:31:20 +0900

    🎯feat(domain/Rank) : Rank 필드 값 반환 함수 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,19 +1,34 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import lotto.Lotto;
+
 /* 랭킹 enum
-*  */
+ *  */
 public enum Rank {
     FIRST(6, 2_000_000_000),
     SECOND(5, 30_000_000), // bonus
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    FIFTH(3, 5_000),
+    NONE(0, 0); // 일치하는 번호가 없거나 기준에 미달할 때
 
-    private final int correct;
+
+    private final int matchCount;
     private final int prize;
 
-    Rank(final int correct, final int prize) {
-        this.correct = correct;
+    Rank(final int matchCount, final int prize) {
+        this.matchCount = matchCount;
         this.prize = prize;
     }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+    public int getPrize() {
+        return prize;
+    }
+
+
+
 }

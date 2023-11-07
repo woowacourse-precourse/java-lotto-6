3403ed2b399430aa36aaa55f85af61e4f3ab2ffb
mung96 <mung5079@gmail.com>
2023-11-07 10:11:29 +0900

    test: countTicket()의 테스트
    TDD를 위한 테스트 코드
    -입력받은 금액으로 1000원에 1장을 반환하는지 countTickets
    -1000원 단위가 아니면 에러메세지를 띄우는지 countTicketException()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +28,26 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력한 금액을 통해 로또를 몇장 살 수 있는지 반환한다.")
+    @Test
+    void countTickets(){
+        //given
+        Player player = new Player();
+        //when
+        assertThat(16).isEqualTo(player.countTickets(16000));
+        //then
+    }
+
+    @DisplayName("입력한 금액이 1000으로 나누어떨어지지 않으면 예외를 발생시킨다.")
+    @Test
+    void countTicketsException(){
+        //given
+        Player player = new Player();
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> player.countTickets(13500));
+
+        assertThat(e.getMessage()).isEqualTo("[ERROR]1,000원 단위로만 입력가능합니다.");
+    }
+
 }

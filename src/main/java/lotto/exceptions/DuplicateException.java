8aedfc810997fa0a35ef310a7b05363977aecd70
cloudchamb3r <jizon0123@protonmail.com>
2023-11-07 12:26:59 +0900

    fix. change exception messages to korean

--- src/main/java/lotto/exceptions/DuplicateException.java
+++ src/main/java/lotto/exceptions/DuplicateException.java
@@ -2,9 +2,9 @@ package lotto.exceptions;
 
 import lotto.constants.StringConstants;
 
-public class DuplicateException extends IllegalArgumentException{
+public class DuplicateException extends IllegalArgumentException {
     @Override
     public String getMessage() {
-        return String.format(StringConstants.ERROR_FORMAT_MESSAGE.toString(), "duplicated value is not allowed on here");
+        return String.format(StringConstants.ERROR_FORMAT_MESSAGE.toString(), "중복된 값은 허용되지 않습니다");
     }
 }

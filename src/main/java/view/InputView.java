20537d9601d0ead9733cf51c765f5a23a678c59d
nayonsoso <panda0329@naver.com>
2023-11-08 05:50:19 +0900

    feat: 사용자 입력 받는 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,10 @@
+package view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String getUserInput() {
+        return readLine();
+    }
+}

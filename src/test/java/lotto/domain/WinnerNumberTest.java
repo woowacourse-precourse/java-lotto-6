818515593af651f4d3202f843dfc992f6f01c31b
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 09:22:43 +0900

    test(WinnerNumberTest) : 당첨 번호 도메인 테스트 코드 작성 - 리스트 인 것만 남겨놓기

---
+++ src/test/java/lotto/domain/WinnerNumberTest.java
@@ -0,0 +1,54 @@
+package lotto.domain;
+
+import lotto.exception.WinnerNumberException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinnerNumberTest {
+
+    @Test
+    @DisplayName("공백 시 예외처리")
+    void isBlankTest(){
+        //given
+        List<Integer> input = Arrays.asList();
+        //then
+        assertThatThrownBy(() -> new WinnerNumber(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("수의 범위가 1 미만 45 초과 시 예외처리")
+    void isRightRangeTest(){
+        //given
+        List<Integer> input = Arrays.asList(0,30,8);
+        //then
+        assertThatThrownBy(() -> new WinnerNumber(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력 숫자가 6개가 아닐 시 예외처리")
+    void isSizeTest(){
+        //given
+        List<Integer> input = Arrays.asList(1,2,3,4,5,6,7);
+        //then
+        assertThatThrownBy(() -> new WinnerNumber(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력 숫자 내부에서 중복된 것이 있으면 예외처리")
+    void isDuplicateTest(){
+        //given
+        List<Integer> input = Arrays.asList(1,1,3);
+        //then
+        assertThatThrownBy(() -> new WinnerNumber(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+}

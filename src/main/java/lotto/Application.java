7b72ecb06d7f68d67407bdcc0b20495273559864
Subi Hwang <ghkdtnql@gmail.com>
2023-11-07 19:58:48 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
+import camp.nextstep.edu.missionutils.Randoms;
 
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Application {
+
+	static private int seedMoney;
+
+	public static void main(String[] args) {
+		inputSeedMoney();
+		pickRandomNumber();
+	}
+
+	private static void inputSeedMoney() {
+		System.out.println("구입금액을 입력해 주세요.");
+		seedMoney = Integer.parseInt(readLine());
+		if (seedMoney % 1000 != 0) {
+			throw new ArithmeticException("1,000원 단위로 입력해주세요.");
+		}
+		System.out.println();
+		System.out.println(seedMoney % 1000 + "개를 구매했습니다.");
+	}
+
+	private static void pickRandomNumber() {
+		List<Lotto> lottos = new ArrayList<>();
+		for (int i = 0; i < seedMoney / 1000; i++) {
+			lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+		}
+	}
+
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,9 @@
 package lotto;
 
-import static camp.nextstep.edu.missionutils.Console.*;
-
 import java.util.List;
 
 public class Lotto {
+    static private int seedMoney;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,16 +16,6 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    private void inputSeedMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
-        int seedMoney = Integer.parseInt(readLine());
-        if(seedMoney % 1000 != 0) {
-            throw new ArithmeticException("1,000원 단위로 입력해주세요.");
-        }
-        System.out.println(seedMoney%1000 + "개를 구매했습니다.");
-    }
-
 
 
 }

94787913fb8223ed6f8d3f377e2b310ba6a3e8d6
Minji Kim <minji1289@naver.com>
2023-11-08 03:16:10 +0900

    [feat] 사용자로부터 구매 금액 입력받기-유효성 검사

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,6 +2,34 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoGame {
+
+    public static void run() {
+        int purchaseAmount = getValidPurchaseAmount();
+    }
+
+    private static int getValidPurchaseAmount() {
+        int purchaseAmount = 0;
+
+        do {
+            int inputAmount = 0;
+            try {
+                inputAmount = getPurchaseAmountFromUser();
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효한 숫자를 입력해야 합니다.");
+                continue; // Restart the loop if there's an exception
+            }
+
+            if (inputAmount % 1000 != 0) {
+                System.out.println("[ERROR] 1,000원 단위로 입력해야 합니다.");
+            } else if (inputAmount == 0) {
+                System.out.println("[ERROR] 숫자를 입력해야 합니다.");
+            } else {
+                purchaseAmount = inputAmount;
+            }
+        } while (purchaseAmount == 0);
+
+        return purchaseAmount;
+    }
     private static int getPurchaseAmountFromUser() {
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());

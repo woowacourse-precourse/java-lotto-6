d33a7f8903d2e7ebeddc7498f8f1cbc0983cb347
donghoony <aru0504@naver.com>
2023-11-08 02:55:04 +0900

    fix(Rank): 상금이 잘못 기입되어 있던 점 수정
    3등 상금이 1_500_000이 아닌 15_000_000으로 기입돼 있었던 점을 수정한다

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public enum Rank {
     FIRST(6, 2_000_000_000L),
     SECOND(5, 30_000_000L),
-    THIRD(5, 15_000_000L),
+    THIRD(5, 1_500_000L),
     FOURTH(4, 50_000L),
     FIFTH(3, 5_000L),
     NONE(0, 0L);

36a4b6d740451d098025ff5bb13a133364609a6b
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:58:28 +0900

    test(UserPrice) : UserPrice test add

---
+++ src/test/java/lotto/domain/UserPriceTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserPriceTest {
+
+    @Test
+    void buyCount() {
+    }
+
+    @Test
+    void buy() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+}

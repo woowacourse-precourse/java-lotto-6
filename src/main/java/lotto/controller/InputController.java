fbd2a2e9a9c6bbf7f55f5aa268c0368944280eb3
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 03:57:37 +0900

    feat : 구입 금액 문자열 입력 코드 추가

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,7 +1,10 @@
 package lotto.controller;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputController {
 
 	public String scanLottoPurchaseMoneyString() {
+		return readLine();
 	}
 }

23b9c6246a97ddcda6b3df40eb35f2acd3479440
eora21 <eora21@naver.com>
2023-11-07 17:25:37 +0900

    feat: 🎸 추가 로또 티켓 구매 과정에서 구매 금액이 제한 금액을 넘어갔는지 판단하는 메서드 작성

--- src/main/java/domain/LottoTicketsPurchasingMachine.java
+++ src/main/java/domain/LottoTicketsPurchasingMachine.java
@@ -2,18 +2,28 @@ package domain;
 
 import exception.NotCorrectUnitAmountException;
 import exception.NotPositivePurchaseAmountException;
+import exception.OverMaxPurchaseAmountException;
 
 public class LottoTicketsPurchasingMachine {
+    private static final Amount MAX_PURCHASE_AMOUNT = new Amount(2_000_000_000);
     private static final Amount LOTTO_UNIT_AMOUNT = new Amount(1_000);
+
     public int purchaseOfLottoTickets(Amount purchaseAmount) {
-        ValidateZeroAmount(purchaseAmount);
+        validateZeroAmount(purchaseAmount);
+        validateMaxPurchaseAmount(purchaseAmount);
         validatePurchaseAmountWithUnitAmount(purchaseAmount);
-        return purchaseAmount.calculateQuotientByUnitAmount(LOTTO_UNIT_AMOUNT);
+        return (int) purchaseAmount.calculateQuotientByUnitAmount(LOTTO_UNIT_AMOUNT);
     }
 
-    private void ValidateZeroAmount(Amount purchaseAmount) {
+    private void validateZeroAmount(Amount purchaseAmount) {
         if (purchaseAmount.isZeroAmount()) {
             throw new NotPositivePurchaseAmountException();
+        }
+    }
+
+    private void validateMaxPurchaseAmount(Amount purchaseAmount) {
+        if (purchaseAmount.isOverLimitAmount(MAX_PURCHASE_AMOUNT)) {
+            throw new OverMaxPurchaseAmountException(MAX_PURCHASE_AMOUNT);
         }
     }
 

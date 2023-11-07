cc9f75fb7bb83c95116ef596254f0592a5aebe49
KangDeok <rkdejr2321@naver.com>
2023-11-07 22:45:05 +0900

    [feat] 사용자가 입력한 로또 번호 객체

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -1,2 +1,25 @@
-package lotto.domain;public class UserLotto {
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserLotto {
+
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public UserLotto(List<Integer> numbers, int bonusNumber) {
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }

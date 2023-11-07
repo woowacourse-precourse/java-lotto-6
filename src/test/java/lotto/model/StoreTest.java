ba23e08a890e01b64efde022e3e019303d04e228
hoyeon <highoption@naver.com>
2023-11-07 15:47:34 +0900

    test(Store) : 가독성을 위해 생성자 매개변수 변경이 동작하는지 테스트
    - 기존 String을 받는 생성자에서 Money 객체를 받게 변경

--- src/test/java/lotto/model/StoreTest.java
+++ src/test/java/lotto/model/StoreTest.java
@@ -11,7 +11,7 @@ class StoreTest {
     @Test
     @DisplayName("주어진 금액으로 올바른 로또 갯수를 사는지 테스트")
     void countLottoTest() {
-        int money = 5000;
+        Money money = new Money("5000");
         int expectedCount = 5;
 
         int actualCount = Store.countLotto(money);

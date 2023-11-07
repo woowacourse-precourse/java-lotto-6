8f1c52aef5182e2d3b929a7b7a08b9c5e717ce42
Choo <choo000407@naver.com>
2023-11-08 02:01:34 +0900

    feat(WinningRule): 등수 별 `당첨 금액과 당첨 조건을 제공`한다

---
+++ src/main/java/lotto/model/WinningRule.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+public enum WinningRule {
+    FIRST_PRIZE("6개 일치 (2,000,000,000원)", 2_000_000_000),
+    SECOND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    THIRD_PRIZE("5개 일치 (1,500,000원)", 1_500_000),
+    FOURTH_PRIZE("4개 일치 (50,000원)", 50_000),
+    FIFTH_PRIZE("3개 일치 (5,000원)", 5_000),
+    NO_PRIZE("꽝 (0원)", 0);
+
+    private final String description;
+    private final long prizeAmount;
+
+    WinningRule(String description, int prizeAmount) {
+        this.description = description;
+        this.prizeAmount = prizeAmount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getPrizeAmount() {
+        return prizeAmount;
+    }
+}

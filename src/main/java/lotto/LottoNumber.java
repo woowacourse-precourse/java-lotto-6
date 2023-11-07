0da4ca26f6c5a95453c3d8b8730539fd626db891
kg9585 <kg9585@naver.com>
2023-11-07 16:10:45 +0900

    feat(LottoNumber): 유효성 검증

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class LottoNumber {
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+    private static final String NUMBER_RANGE_ERROR = String.format("로또 숫자는 %d 이상 %d 이하의 숫자만 가능합니다.", MIN, MAX);
+    private int number;
+    public LottoNumber(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+    private void validateNumber(int number) {
+        if (number < MIN || number > MAX) {
+            throw new IllegalArgumentException(NUMBER_RANGE_ERROR);
+        }
+    }
+}

701b98524b653621b08bd5de9bd8072cfb75817b
morenow <jjang9877@gmail.com>
2023-11-08 04:53:37 +0900

    feat: Controller, View 객체 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,7 @@
+package lotto.controller;
+
+public class LottoController {
+
+    public void playLotto() {
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}

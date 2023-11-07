95fd0650399e028727e3c4dcf9fff22d7cede2c0
김정민 <jmk445123456@gmail.com>
2023-11-08 00:31:55 +0900

    feat : LottoTest.java, README.md

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+1. Lotto domain 기능
+2. Lotto controller 기능
+3. Lotto GameDecisionMachine 기능
+4. Lotto GameView 기능
+5. Lotto Test 코드 
+
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import lotto.Model.Lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 

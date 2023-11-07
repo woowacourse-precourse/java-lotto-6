32d350297ffd664ba8b554caa870722b33a561e5
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-07 19:18:04 +0900

    test(result): 적절한 수익률을 출력하는지 테스트 코드 구현

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -1,27 +1,31 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
-class LottoResultTest {
-    private static Lotto winningLotto;
-    private static Bonus bonus;
+class ResultTest {
+    private static final Integer PERCENTAGE = 100;
+    private static final Integer FIX_MONEY_FOR_TEST = 8000;
+    private static final Integer FIRST_PRIZE = LottoRanking.FIRST.getWinningMoney();
+    private static final Integer SECOND_PRIZE = LottoRanking.SECOND.getWinningMoney();
+    private static final Integer THIRD_PRIZE = LottoRanking.THIRD.getWinningMoney();
+    private static final Integer FORTH_PRIZE = LottoRanking.FORTH.getWinningMoney();
+    private static final Integer FIFTH_PRIZE = LottoRanking.FIFTH.getWinningMoney();
 
-    @BeforeAll
-    public static void initializer(){
+    private Lotto winningLotto;
+    private Bonus bonus;
+
+    @BeforeEach
+    void initializer(){
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
         winningLotto = new Lotto(numbers);
         bonus = new Bonus(numbers, 7);
@@ -29,24 +33,24 @@ class LottoResultTest {
 
     @ParameterizedTest
     @MethodSource("provideEachRankingLotto")
-    @DisplayName("적절한_결과_생성_확인_테스트")
-    void createCorrectResultTest(Lotto matchedLotto, LottoRanking ranking){
+    @DisplayName("수익률에 맞는 올바른 이익률을 출력해야 한다.")
+    void getProfitRate(Lotto matchedLotto, LottoRanking ranking) {
         // given
         List<Lotto> playerLotto = new ArrayList<>();
         playerLotto.add(matchedLotto);
-        Boolean isCorrectResult = true;
-        // when
         Map<LottoRanking, Integer> results = LottoResult.getResults(winningLotto,playerLotto,bonus);
-        for(Map.Entry<LottoRanking, Integer> entry: results.entrySet()){
-            LottoRanking lottoRanking = entry.getKey();
-            int count = entry.getValue();
-            if(count == 1 && !(lottoRanking.equals(ranking))){
-                isCorrectResult = false;
-                break;
-            }
+        Money money = new Money(FIX_MONEY_FOR_TEST);
+        Double operand = PERCENTAGE.doubleValue()*ranking.getWinningMoney();
+        String expectedProfitRate = "0.0";
+        if(!ranking.equals(LottoRanking.OTHER)){
+            expectedProfitRate = String.format("%.1f",operand/FIX_MONEY_FOR_TEST);
         }
+        // when
+        Result result = new Result(results, money);
+        String profitRate = String.format("%.1f",result.getProfitRate());
         // then
-        assertThat(isCorrectResult).isEqualTo(true);
+        System.out.println(profitRate);
+        assertThat(profitRate).isEqualTo(expectedProfitRate);
     }
 
     private static Stream<Arguments> provideEachRankingLotto(){

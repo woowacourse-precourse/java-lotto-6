120e37d52319a4d5aa9524d41bcc1ca1d7ced141
binary_ho <dfghcvb11@naver.com>
2023-11-07 22:50:32 +0900

    feat : Lotto를 수동으로 발급해주는 issueChooseNumbersLotto 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,22 +1,33 @@
 package lotto;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record Lotto(Set<LottoNumber> lottoNumbers) {
 
     private static final int LOTTO_NUMBERS_SIZE = 6;
 
-    public Lotto {
+
+    public Lotto(Set<LottoNumber> lottoNumbers) {
         validateLottoSize(lottoNumbers);
     }
 
-    public static Lotto issueLotto() {
+    public static Lotto issueAutoPickNumbersLotto() {
         Set<LottoNumber> lottoNumbers = new HashSet<>();
         while (lottoNumbers.size() < LOTTO_NUMBERS_SIZE) {
             LottoNumber randomLottoNumber = LottoNumber.createRandomLottoNumber();
             lottoNumbers.add(randomLottoNumber);
         }
+        return new Lotto(lottoNumbers);
+    }
+
+    public static Lotto issueChooseNumbersLotto(List<Integer> userChooseNumbers) {
+        Set<LottoNumber> lottoNumbers = userChooseNumbers.stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toSet());
+
         return new Lotto(lottoNumbers);
     }
 

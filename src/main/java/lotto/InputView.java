efad91b7c09eacfbd8e969f616fe5174ee83ddbc
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-07 15:40:29 +0900

    fix: 입력값을 Integer형으로 반환하도록 수정
    String형으로 입력된 값을 Integer형으로 반환하도록 수정한다.

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,6 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
     public static Integer inputPurchaseAmount() {
@@ -10,15 +14,21 @@ public class InputView {
         return Integer.parseInt(inputValue);
     }
 
-    public static String inputWinningNumbers() {
+    public static List<Integer> inputWinningNumbers() {
         System.out.println(MessageConstants.WINNING_NUMBERS_GUIDE);
-        String inputValue = Console.readLine();
-        return inputValue;
+        String[] inputValues = Console.readLine().split(",");
+        List<Integer> inputIntValues = parseStringToInteger(inputValues);
+        return inputIntValues;
     }
 
-    public static String inputBonusNumber() {
+    private static List<Integer> parseStringToInteger(String[] inputValues) {
+        List<Integer> inputIntValues = Arrays.stream(inputValues).map(v -> Integer.parseInt(v)).collect(Collectors.toList());
+        return inputIntValues;
+    }
+
+    public static Integer inputBonusNumber() {
         System.out.println(MessageConstants.BONUS_NUMBER_GUIDE);
-        String inputValue = Console.readLine();
+        Integer inputValue = Integer.parseInt(Console.readLine());
         return inputValue;
     }
 }

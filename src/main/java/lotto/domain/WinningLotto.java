39dc5ea35ba6f66bb958c1f058a3435ab5ae50dd
ppdoli123 <ppdoli123@naver.com>
2023-11-07 15:50:49 +0900

    feat(WinningLotto): 당첨번호에서 해당하는 숫자를 세는 메소드 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,18 +1,31 @@
 package lotto.domain;
 
+import lotto.Lotto;
+
 import java.util.List;
 
 public class WinningLotto {
-    private final LottoTicket winningNumbers;
-    private final LottoNumber bonusNumber;
+    private final Lotto winningNumbers;
+    private final Lotto bonusNumber;
 
     // 입력된 winningNumbers와 bonusNumber가 올바른지 검증 필요
-    public WinningLotto(LottoTicket winningNumbers, LottoNumber bonusNumber) {
+    public WinningLotto(Lotto winningNumbers, Lotto bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
+    private int getMatchCount(Lotto ticket) {
+        return (int) ticket.getNumbers().stream()
+                .filter(winningNumbers.getNumbers()::contains)
+                .count();
+    }
 
     // 당첨 결과 계산 로직 작성 필요
-    public LottoResult compare(List<LottoTicket> tickets) {
+    public LottoResult compare(List<Lotto> tickets) {
+        LottoResult result = new LottoResult();
+        for (Lotto ticket : tickets) {
+            int matchCount = getMatchCount(ticket);
+            result.add(LottoRank.valueOf(matchCount));
+        }
+        return result;
     }
 }

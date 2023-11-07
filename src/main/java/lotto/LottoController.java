9cefcada4ee796d6f800beea7fea197ab1192281
hyuuuun <pam4522@gmail.com>
2023-11-08 02:17:18 +0900

    feat(LottoController): 입력금액 입력 기능 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,2 +1,22 @@
-package lotto;public class LottoController {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoController {
+
+        public void run(){
+
+        }
+        public void inputLottoPrice(){
+                LottoView.mentionInputPrice();
+                String inputPrice = Console.readLine();
+        }
+        private void vaildateLottoPrice(String inputPrice){
+                if(inputPrice.isEmpty()){
+                        throw new IllegalArgumentException("구입금액을 입력해 주세요.");
+                }
+                if(!inputPrice.matches("[0-9]+")){
+                        throw new IllegalArgumentException("구입금액은 숫자만 입력 가능합니다.");
+                }
+        }
 }

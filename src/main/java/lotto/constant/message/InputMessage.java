a51a2c37f90c240997bd32f8b46d0b86024e144d
Yang woosung <ngxk1217@gmail.com>
2023-11-07 17:51:12 +0900

    MSG를 제거 InputMessage를 통하여 추론이 가능해서 제거 하였습니다.

--- src/main/java/lotto/constant/message/InputMessage.java
+++ src/main/java/lotto/constant/message/InputMessage.java
@@ -1,9 +1,9 @@
 package lotto.constant.message;
 
 public enum InputMessage {
-    PURCHASE_MSG("구입금액을 입력해 주세요.")
-    , WINNING_MSG("당첨 번호를 입력해 주세요")
-    , BONUS_MSG("보너스 번호를 입력해 주세요.");
+    PURCHASE("구입금액을 입력해 주세요.")
+    , WINNING("당첨 번호를 입력해 주세요")
+    , BONUS("보너스 번호를 입력해 주세요.");
 
     private final String message;
 

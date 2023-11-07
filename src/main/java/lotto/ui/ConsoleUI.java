8316fc4a0cd7cb46f3f0c1e0b6dad3416ce137d2
jjw0071 <jjw0071@naver.com>
2023-11-07 17:26:44 +0900

    feat(ConsoleUI): Validator 객체를 주입 받는 생성자 추가

--- src/main/java/lotto/ui/ConsoleUI.java
+++ src/main/java/lotto/ui/ConsoleUI.java
@@ -1,4 +1,13 @@
 package lotto.ui;
 
+import lotto.domain.Validator;
+
 public class ConsoleUI {
+    private final Validator validator;
+
+    public ConsoleUI(Validator validator) {
+        this.validator = validator;
+    }
+
+
 }

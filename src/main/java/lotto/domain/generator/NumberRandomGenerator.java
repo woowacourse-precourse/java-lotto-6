b387b77dd1e9d2eb27beee0600b2d0d6bb23d49d
Silver <leeeun5423@naver.com>
2023-11-08 03:10:04 +0900

    refactor : 하드코딩된 수 지우고 상수로 변경하기

--- src/main/java/lotto/domain/generator/NumberRandomGenerator.java
+++ src/main/java/lotto/domain/generator/NumberRandomGenerator.java
@@ -4,13 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class NumberRandomGenerator {
+
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+    private static final int NUMBER = 6;
+
     public List<Integer> generate(){
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        validate(numbers);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN, MAX, NUMBER);
         return numbers;
     }
 
-    public void validate(List<Integer> inputValue) {
-
-    }
 }

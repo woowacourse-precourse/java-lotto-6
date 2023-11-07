6e0211cc55a28b544619cb12f834c1bc2a1fbeae
minsub <chrkb1569@naver.com>
2023-11-08 01:11:56 +0900

    feat : GameConstant 내부에 상수 추가

--- src/main/java/lotto/util/GameConstant.java
+++ src/main/java/lotto/util/GameConstant.java
@@ -5,8 +5,10 @@ public enum GameConstant {
     CONSTANT_LOTTO_MIN_NUMBER(1), // 로또 번호 최소값
     CONSTANT_LOTTO_MAX_NUMBER(45), // 로또 번호 최대값
     CONSTANT_LOTTO_LENGTH(6), // 로또 번호 길이
-    CONSTANT_GRADE_LENGTH(5) // 총 등수
+    CONSTANT_GRADE_LENGTH(5), // 총 등수
+    CONSTANT_INITIAL_VALUE(0)
     ;
+
     private final int constant;
 
     GameConstant(int constant) {

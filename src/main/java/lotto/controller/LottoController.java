7761c60a0dd5eda7b44852d26f1406433c588b47
username <57055730+Jake-huen@users.noreply.github.com>
2023-11-07 09:43:26 +0900

    feat : 구매 금액 입력 및 검증

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,6 +12,7 @@ public class LottoController {
     private final OutputView outputView;
     private final LottoManager lottoManager;
     private final LottoList lottoList;
+    private static final int LOTTO_PRICE = 1000;
 
     public LottoController() {
         this.inputView = new InputView();
@@ -22,6 +23,15 @@ public class LottoController {
 
     public void startLottoGame() {
         outputView.askPurchaseAmount();
+        int purchaseAmount = inputView.getPurchaseAmountFromUser();
+        validatePurchaseAmount(purchaseAmount);
+    }
+
+    // 구입 금액은 1000원으로 나누어떨어져야 한다.
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("로또 구입 금액은 1000원 단위로 입력 받아야합니다.");
+        }
     }
 
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,9 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public int getPurchaseAmountFromUser() {
+        return Integer.parseInt(Console.readLine());
+    }
 }

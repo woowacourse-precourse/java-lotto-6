2bd936922879364fd7306b67ef62475f0b7dc234
Yeriimii <seojs0511@naver.com>
2023-11-07 23:46:30 +0900

    feat(LottoResultAnalyzer): 로또 용지의 당첨 번호 일치 개수에 따라 등수를 나눌 수 있다

--- src/main/java/lotto/model/LottoResultAnalyzer.java
+++ src/main/java/lotto/model/LottoResultAnalyzer.java
@@ -1,7 +1,10 @@
 package lotto.model;
 
+import lotto.model.dto.LottoResultDTO;
+
 import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoResultAnalyzer {
     private EnumMap<PrizeCategory, Integer> matchResults = new EnumMap<>(PrizeCategory.class);
@@ -20,5 +23,30 @@ public class LottoResultAnalyzer {
     public void writeResultToLottoPapers(List<LottoPaper> lottoPapers, List<Integer> winningNumbers) {
         lottoPapers.stream()
                 .forEach(paper -> writeResultToLottoPaper(paper, winningNumbers));
+    }
+
+    public void matchByLottoPaper(LottoPaper lottoPaper, int bonusNumber) {
+        for (PrizeCategory category : PrizeCategory.values()) {
+            if (lottoPaper.getMatchingCount() == category.MATCH_COUNT) {
+                updateMatchResults(category, lottoPaper, bonusNumber);
+                return;
+            }
+        }
+    }
+
+    private void updateMatchResults(PrizeCategory category, LottoPaper lottoPaper, int bonusNumber) {
+        if (category == PrizeCategory.FIVE_MATCH_NO_BONUS && lottoPaper.hasBonusNumber(bonusNumber)) {
+            category = PrizeCategory.FIVE_MATCH_WITH_BONUS;
+        }
+        matchResults.put(category, matchResults.getOrDefault(category, 0) + 1);
+    }
+
+    public void matchByLottoPapers(List<LottoPaper> lottoPapers, int bonusNumber) {
+        lottoPapers.stream()
+                .forEach(paper -> matchByLottoPaper(paper, bonusNumber));
+    }
+
+    public EnumMap<PrizeCategory, Integer> getMatchResults() {
+        return matchResults;
     }
 }

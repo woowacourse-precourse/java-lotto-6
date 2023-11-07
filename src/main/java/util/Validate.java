c02f6743e05ea2f789e651f2ef141c677c40aad2
icebear0111 <wltjq0901@naver.com>
2023-11-07 22:38:13 +0900

    feat: check if user input is valid

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -8,7 +8,17 @@ public class Validate {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.");
     }
 
-    public static void isValidNumber(List<Integer> numbers) {
+    public static void isValidLotto(List<Integer> numbers) {
+        if (numbers.size() != 6)
+            throw  new IllegalArgumentException("[ERROR] 로또는 총 6개의 숫자여야 합니다.");
+        for (int num : numbers) {
+            if (num < 1 || num > 45)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 
+    public static void isValidBonus(int number) {
+        if (number < 1 || number > 45)
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 }

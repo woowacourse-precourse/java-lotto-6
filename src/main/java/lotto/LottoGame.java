52c354b382b491ac90b961830c072924c3dbb4a8
cloudchamb3r <jizon0123@protonmail.com>
2023-11-07 14:16:15 +0900

    refactor. game loop

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,86 +1,66 @@
 package lotto;
 
-import static java.lang.Integer.parseInt;
-import static lotto.constants.IntegerConstants.LOTTO_NUMBERS_COUNT;
-import static lotto.constants.IntegerConstants.LOTTO_PRICE;
-import static lotto.constants.IntegerConstants.LOTTO_RANGE_END;
-import static lotto.constants.IntegerConstants.LOTTO_RANGE_START;
-import static lotto.constants.IntegerConstants.MATCH_FIVE_BONUS_REWARD;
-import static lotto.constants.IntegerConstants.MATCH_FIVE_REWARD;
-import static lotto.constants.IntegerConstants.MATCH_FOUR_REWARD;
-import static lotto.constants.IntegerConstants.MATCH_SIX_REWARD;
-import static lotto.constants.IntegerConstants.MATCH_THREE_REWARD;
-import static lotto.constants.StringConstants.BUY_FORMAT_MESSAGE;
-import static lotto.constants.StringConstants.INPUT_BONUS_MESSAGE;
-import static lotto.constants.StringConstants.INPUT_BUY_PRICE;
-import static lotto.constants.StringConstants.INPUT_WINNING_MESSAGE;
-import static lotto.constants.StringConstants.MATCH_RESULT_MESSAGE;
-import static lotto.constants.StringConstants.MATCH_RESULT_PROFIT_RATE;
-import static lotto.constants.StringConstants.MATCH_RESULT_WITH_BONUS_MESSAGE;
-import static lotto.constants.StringConstants.WINNING_RESULT_MESSAGE;
-import static lotto.constants.StringConstants.WINNING_RESULT_SEPARATOR;
-import static lotto.io.Input.promptLine;
-import static lotto.io.Output.printf;
-import static lotto.io.Output.println;
-
-import java.util.ArrayList;
-import java.util.List;
-import lotto.exceptions.InvalidNumberRangeException;
-import lotto.game.result.AfterLoopResult;
-import lotto.game.result.BeforeLoopResult;
+import lotto.domain.LottoBonus;
+import lotto.domain.LottoMatch;
+import lotto.domain.LottoPurchase;
+import lotto.domain.LottoWinning;
 import lotto.game.Game;
-import lotto.io.Input;
-import lotto.utils.RandomUtils;
+import lotto.game.result.GameLoopResult;
+import lotto.views.LottoBonusInputView;
+import lotto.views.LottoMatchResultView;
+import lotto.views.LottoPurchaseInputView;
+import lotto.views.LottoPurchaseResultView;
+import lotto.views.LottoWinningInputView;
 
 public class LottoGame implements Game {
-    LottoMatcher matcher;
 
-    @Override
-    public BeforeLoopResult beforeLoop()  {
-        int buyPrice = parseInt(promptLine(INPUT_BUY_PRICE));
-        if (buyPrice % LOTTO_PRICE != 0) {
-            throw new InvalidNumberRangeException();
+    public LottoPurchase inputPurchase() {
+        LottoPurchase purchase = null;
+        while (purchase == null) {
+            try {
+                purchase = new LottoPurchaseInputView().render();
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
-        int lottoCount = buyPrice / LOTTO_PRICE;
-        printf(BUY_FORMAT_MESSAGE, lottoCount);
-
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0 ; i < lottoCount ; i++) {
-            var lotto = new Lotto(RandomUtils.createSortedUniqueRandomListOf(LOTTO_RANGE_START, LOTTO_RANGE_END, LOTTO_NUMBERS_COUNT));
-            println(lotto.toString());
-            lottos.add(lotto);
-        }
-
-        matcher = new LottoMatcher(lottos);
-        return BeforeLoopResult.GAME_READY;
+        return purchase;
     }
 
+    public LottoWinning inputWinning() {
+        LottoWinning winning = null;
+        while (winning == null) {
+            try {
+                winning = new LottoWinningInputView().render();
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return winning;
+    }
+
+    public LottoBonus inputBonus(LottoWinning winning) {
+        LottoBonus bonus = null;
+        while (bonus == null) {
+            try {
+                bonus = new LottoBonusInputView(winning).render();
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return bonus;
+    }
 
     @Override
-    public AfterLoopResult afterLoop() {
-        println(INPUT_WINNING_MESSAGE);
-        var winnings = Input.readLineAsIntegerList(",");
+    public GameLoopResult gameLoop() {
+        LottoPurchase purchase = inputPurchase();
+        new LottoPurchaseResultView(purchase).render();
 
-        if (winnings.size() != 6) {
-            throw new IllegalStateException();
-        }
-        // TODO: 중복 처리
-        // TODO: 범위 처리
-        var bonus = parseInt(promptLine(INPUT_BONUS_MESSAGE));
+        LottoWinning winning = inputWinning();
+        LottoBonus bonus = inputBonus(winning);
 
-        println(WINNING_RESULT_MESSAGE);
-        println(WINNING_RESULT_SEPARATOR);
+        LottoMatch lottoMatch = new LottoMatch(purchase, winning, bonus);
+        new LottoMatchResultView(lottoMatch).render();
 
-
-        GroupedLottoMatchResult matchResults = matcher.matchAll(winnings, bonus);
-
-        printf(MATCH_RESULT_MESSAGE + "\n", 3, MATCH_THREE_REWARD, matchResults.countMatchResult(LottoMatchResult.MATCH_THREE));
-        printf(MATCH_RESULT_MESSAGE + "\n", 4, MATCH_FOUR_REWARD, matchResults.countMatchResult(LottoMatchResult.MATCH_FOUR));
-        printf(MATCH_RESULT_MESSAGE + "\n", 5, MATCH_FIVE_REWARD, matchResults.countMatchResult(LottoMatchResult.MATCH_FIVE));
-        printf(MATCH_RESULT_WITH_BONUS_MESSAGE + "\n", 5, MATCH_FIVE_BONUS_REWARD, matchResults.countMatchResult(LottoMatchResult.MATCH_FIVE_BONUS));
-        printf(MATCH_RESULT_MESSAGE + "\n", 6, MATCH_SIX_REWARD, matchResults.countMatchResult(LottoMatchResult.MATCH_SIX));
-
-        printf(MATCH_RESULT_PROFIT_RATE, matchResults.getProfitRate());
-        return AfterLoopResult.END_GAME;
+        return GameLoopResult.END_GAME;
     }
 }

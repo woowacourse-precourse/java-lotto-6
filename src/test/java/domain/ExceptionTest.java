b3a1cd5ac6cccdfd0ddf4b7b226307477bd50149
jacomyou1026 <jacomyou1026@naver.com>
2023-11-07 22:50:45 +0900

    test : domain테스트코드 고치기

--- src/test/java/domain/ExceptionTest.java
+++ src/test/java/domain/ExceptionTest.java
@@ -1,11 +1,14 @@
 package domain;
 
+import lotto.domin.Exception;
 import lotto.view.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.domin.Exception.NumberCheck;
+import static lotto.domin.Exception.overNumber;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ExceptionTest {
@@ -13,26 +16,26 @@ class ExceptionTest {
 
     @Test
     @DisplayName("숫자인지 체크")
-    void numberCheck() {
-        assertThatThrownBy(() -> Exception.NumberCheck("일이이"))
+    void numberCheckTest() {
+        assertThatThrownBy(() -> NumberCheck("일이이"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.INPUT_TYPE_ERROR.getExceptionValue());
     }
 
     @Test
     @DisplayName("1~45사이의 로또 숫자가 아닌 경우")
-    void overNumber() {
-        assertThatThrownBy(() ->Exception.overNumber(55))
+    void overNumberTest() {
+        assertThatThrownBy(() ->overNumber(55))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.NOT_NUMBER_RANGE.getExceptionValue());
     }
 
     @DisplayName("중복값 체크")
     @Test
-    void duplicationCheckBonus() {
+    void duplicationCheckBonusTest() {
         int bonus = 7;
         List<Integer> selectBonus = List.of(1, 2, 4, 5, 6, 7);
-        assertThatThrownBy(() ->Exception.duplicationCheckBonus(bonus,selectBonus))
+        assertThatThrownBy(() -> Exception.duplicationCheckBonus(bonus,selectBonus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.NOT_NUMBER_OVERLAP.getExceptionValue());
     }

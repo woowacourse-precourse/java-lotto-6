09d795ea606b63d4c1feaa6da6378fcdb7d55863
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-07 18:36:59 +0900

    [refactor : LottoController] public -> private 메소드 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -24,6 +24,7 @@ public class LottoController {
         getBonusNumber();
     }
 
+    //amount 값만큼 랜덤 로또 생성, lottos에 저장
     private List<Lotto> getLottos(){
         RandomNumbers randomNumbers = new RandomNumbers();
         for (int i = 0; i < amount; i++) {
@@ -33,13 +34,13 @@ public class LottoController {
     }
 
     //amount 입력 받아서 모델에서 validation 처리 -> amount 모델 생성
-    public void getLottoAmount() {
+    private void getLottoAmount() {
         LottoAmount lottoAmount = new LottoAmount(InputView.purchaseAmount());
         amount = lottoAmount.getAmount();
     }
 
     //winning 입력 받아서 모델에서 validation 처리 -> Lotto에서
-    public void getWinningLotto() {
+    private void getWinningLotto() {
         Lotto lotto = new Lotto(lottoStringtoIntegerList());
         winningLotto = lotto;
     }
@@ -50,12 +51,11 @@ public class LottoController {
     }
 
     //bonusNumber 입력 받아서 모델에서 validation 처리 -> bonusNum 모델 생성?
-    public void getBonusNumber(){
+    private void getBonusNumber(){
         BonusNumber bonus = new BonusNumber(InputView.bonusNumber(), winningLotto.getNumbers());
         bonusNumber = bonus.getBonusNumber();
     }
 
-    //amount 값만큼 랜덤 로또 생성, lottos에 저장
 
     //각 로또가 몇개 맞았는지 확인, lottos, amount, winningNumbers, bonusNumber 등등 활용,
     //모델 생성(로또 결과 모델)해서 각 로또가 몇개 맞혔는지 보너스는 맞혔는지 확인하고 map에 넣기

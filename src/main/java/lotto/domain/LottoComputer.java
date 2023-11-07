01ad2ec41a9bad9bf30ef67fd71b42695986bb0f
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-07 17:38:55 +0900

    reafactor(LottoComputer): 가독성을 높이기 위한 리펙터링 진행

--- src/main/java/lotto/domain/LottoComputer.java
+++ src/main/java/lotto/domain/LottoComputer.java
@@ -1,25 +1,28 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import lotto.Dto.LottoResultDto;
+import java.util.HashMap;
+import java.util.Map;
+import lotto.Dto.SingleResultDto;
+import lotto.Dto.TotalResultDto;
 import lotto.constants.Constant;
 import lotto.constants.messages.Error;
 
 public class LottoComputer {
+
 
     private Lotto winnerLotto;
     private Integer bonusNumber;
 
     private Lottos myLottos;
 
-    List<Integer> s = new ArrayList<>(List.of(
-            Constant.MATCHED_SIX_PRICE.getConstant(),
-            Constant.MATCHED_FIVE_ONE_BONUS_PRICE.getConstant(),
-            Constant.MATCHED_FIVE_PRICE.getConstant(),
-            Constant.MATCHED_FOUR_PRICE.getConstant(),
-            Constant.MATCHED_THREE_PRICE.getConstant()
-    ));
+    // matched / prize
+    Map<Integer, Integer> prizes = new HashMap<>() {{
+        put(3, Constant.MATCHED_THREE_PRICE.getConstant());
+        put(4, Constant.MATCHED_FOUR_PRICE.getConstant());
+        put(5, Constant.MATCHED_FIVE_PRICE.getConstant());
+        put(6, Constant.MATCHED_SIX_PRICE.getConstant());
+        put(7, Constant.MATCHED_FIVE_ONE_BONUS_PRICE.getConstant());
+    }};
 
     public void config(Lottos myLottos) {
         this.myLottos = myLottos;
@@ -35,38 +38,38 @@ public class LottoComputer {
     }
 
 
-    public LottoResultDto simulate() {
-        LottoResultDto lottoResultDto = new LottoResultDto();
-        List<List<Integer>> wonLottery = calculateWonLottery();
-        lottoResultDto.setResult(wonLottery);
-        lottoResultDto.setProfit(calculateProfit(wonLottery));
-        return lottoResultDto;
+    public TotalResultDto simulate() {
+        TotalResultDto totalResultDto = new TotalResultDto();
+
+        for (int won = 3; won <= 7; won++) {
+            totalResultDto.add(won, calculateWonLottery(won));
+        }
+        totalResultDto.setProfit(calculateProfit(totalResultDto));
+
+        return totalResultDto;
     }
 
 
-    private double calculateProfit(List<List<Integer>> wonLottery) {
-
+    private double calculateProfit(TotalResultDto totalWon) {
         int totalPrize = 0;
         int spentMoney = myLottos.size() * 1000;
-        for (int i = 0; i < wonLottery.size(); i++) {
-            totalPrize += wonLottery.get(i).get(3) * s.get(i);
+        for (int i = 3; i <= 7; i++) {
+            totalPrize += totalWon.getSingleResult(i).getWon() * totalWon.getSingleResult(i).getPrize();
         }
         return (double) totalPrize / spentMoney * 100;
     }
 
     // [match, bonus, prize, won]
-    private List<List<Integer>> calculateWonLottery() {
-        List<List<Integer>> result = new ArrayList<>();
-        int x = 0;
-        for (int i = 6; i >= 3; i--) {
-            List<Integer> tmp = new ArrayList<>();
-            tmp.add(i); // match
-            tmp.add(0); // bonus
-            tmp.add(s.get(x++)); // prize
-            tmp.add(myLottos.match(winnerLotto, i)); // won
-            result.add(tmp);
+    private SingleResultDto calculateWonLottery(int won) {
+        SingleResultDto singleResultDto = new SingleResultDto();
+
+        singleResultDto.setMatch(won);
+        singleResultDto.setWon(myLottos.match(winnerLotto, won));
+        singleResultDto.setPrize(this.prizes.get(won));
+        if (won == 7) {
+            singleResultDto.setMatch(5);
         }
-        return result;
+        return singleResultDto;
     }
 
 

494a9c76628a2279e3134c3125622b1ea7708a02
Jiwon Na <jiwonna52@gmail.com>
2023-11-07 23:34:27 +0900

    Refactor : 에러 메시지를 enum으로 변경

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -1,38 +1,22 @@
 package lotto.view;
 
-//TODO : 에러 메시지 작성
-public class ErrorMessage {
-    private final static String error = "[ERROR]";
+public enum ErrorMessage {
+    PriceUnderThousand(" 로또는 한 장에 천원입니다. 천 원 이상으로 입력해주세요."),
+    priceNotThousandRate(" 로또는 한 장에 천 원입니다. 천 원 단위로 입력해주세요."),
+    outOfBound(" 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    duplicateWinning(" 당첨 번호들은 중복되면 안 됩니다."),
+    duplicateBonus(" 당첨 번호와 보너스 번호가 중복되면 안 됩니다."),
+    toManyComma(" 반점은 숫자마다 하나씩만 넣어주세요."),
+    notEnoughLength(" 총 6개의 숫자를 입력하셔야 합니다(구분자는 ',')."),
+    notDigit(" 숫자로 입력하셔야 합니다.");
 
-    public static void outOfBoundExceptionMessage() {
-        System.out.println(error + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    private final String message;
+    private static String commonMessage = "[ERROR]";
+    ErrorMessage(String message) {
+        this.message = message;
     }
 
-    public static void notDigitExceptionMessage() {
-        System.out.println(error + " 숫자로 입력하셔야 합니다.");
-    }
-
-    public static void notEnoughLengthOfWinningNumbersExceptionMessage() {
-        System.out.println(error + " 총 6개의 숫자를 입력하셔야 합니다(구분자는 ',').");
-    }
-
-    public static void priceZeroException() {
-        System.out.println(error + " 로또는 한 장에 천원입니다. 천 원 이상으로 입력해주세요.");
-    }
-
-    public static void duplicatedBonusNumber() {
-        System.out.println(error + " 당첨 번호와 보너스 번호가 중복되면 안 됩니다.");
-    }
-
-    public static void duplicatedWinningNumbers() {
-        System.out.println(error + " 당첨 번호들은 중복되면 안 됩니다.");
-    }
-
-    public static void muchComma() {
-        System.out.println(error + " 반점은 숫자마다 하나씩 넣어주세요.");
-    }
-
-    public static void notThousandPrice() {
-        System.out.println(error + " 로또는 한 장에 천 원입니다. 천 원 단위로 입력해주세요.");
+    public String getErrorMessage() {
+        return commonMessage + message;
     }
 }

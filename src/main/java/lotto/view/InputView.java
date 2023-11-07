f33fbacb7983746c84d9a6152a7d57d82f7878f2
Hayoon <hy97@sju.ac.kr>
2023-11-07 15:01:04 +0900

    feat(InputView): 당첨 번호(inputWinningNumbers)와 보너스 번호(inputBonusNumber)를 입력받는 메서드를 구현한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
@@ -10,4 +13,21 @@ public class InputView {
         String input = readLine();
         return Integer.parseInt(input);
     }
+
+    public List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        String input = readLine();
+
+        return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).boxed().toList();
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        String input = readLine();
+        return Integer.parseInt(input);
+    }
+
+
 }

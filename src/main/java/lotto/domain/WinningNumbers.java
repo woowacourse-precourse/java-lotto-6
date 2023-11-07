01493930b726d45c3e761f7161a2154beaa47847
mummhy0811 <mummhy0811@inu.ac.kr>
2023-11-07 15:22:39 +0900

    Fix: 당첨 숫자 예외 처리 방법 수정

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,6 +4,7 @@ import lotto.util.Constant;
 import lotto.util.ErrorConstant;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
@@ -14,24 +15,27 @@ public class WinningNumbers {
     private Integer bonusNumber;
 
     public WinningNumbers(String winningNumbers){
-        this.winningNumbers = new ArrayList<>();
-        validate(winningNumbers);
-
+        List<Integer> validatedNumbers = validate(winningNumbers);
+        this.winningNumbers = new ArrayList<>(validatedNumbers);
     }
 
-    public void validate(String winningNumbers){
+    public List<Integer> validate(String winningNumbers){
         hasBlank(winningNumbers);
+        List<Integer> tmp = new ArrayList<>(6);
 
         StringTokenizer st = new StringTokenizer(winningNumbers, Constant.WINNING_NUMBER_SEPARATOR);
         while(st.hasMoreTokens()){
             String n = st.nextToken();
             isNumber(n);
-            this.winningNumbers.add(Integer.parseInt(n));
+            tmp.add(Integer.parseInt(n));
         }
-        hasSixNumbers();
-        hasValidRange();
-        isDuplicated();
+        hasSixNumbers(tmp);
+        tmp.forEach(this::hasValidRange);
+        isDuplicated(tmp);
+
+        return tmp;
     }
+
     public void hasBlank(String winningNumbers){
         if(winningNumbers.contains(" ")){
             throw new IllegalArgumentException(ErrorConstant.HAS_BLANK);
@@ -39,37 +43,27 @@ public class WinningNumbers {
     }
     private void isNumber(String n) {
         if(!Pattern.matches(Constant.REGEXP_PATTERN_NUMBER, n)){
-            winningNumbers.clear();
             throw new IllegalArgumentException(ErrorConstant.NOT_A_NUMBER);
         }
-
     }
-    public void hasSixNumbers(){
-        if(winningNumbers.size()!=6){
-            winningNumbers.clear();
+    public void hasSixNumbers(List<Integer> tmp){
+        if(tmp.size()!=6){
             throw new IllegalArgumentException(ErrorConstant.WINNING_NUMBER_SIZE);
         }
     }
-    public void hasValidRange(){
-        long invalidNumbers = winningNumbers.stream()
-                .filter(n -> n < 1 || n > 45)
-                .count();
-        System.out.println(invalidNumbers);
-        if(invalidNumbers!=0){
-            winningNumbers.clear();
-            throw new IllegalArgumentException(ErrorConstant.LOTTO_NUMBER_RANGE);
-        }
-
-    }
-    public void isDuplicated(){
-        List<Integer> distinctNumbers = winningNumbers.stream()
+    public void isDuplicated(List<Integer> tmp){
+        List<Integer> distinctNumbers = tmp.stream()
                 .distinct()
                 .collect(Collectors.toList());
 
-        if(distinctNumbers.size()!=winningNumbers.size()){
-            winningNumbers.clear();
+        if(distinctNumbers.size()!=tmp.size()){
             throw new IllegalArgumentException(ErrorConstant.DUPLICATE_NUMBER);
         }
-
     }
+    public void hasValidRange(Integer n){
+        if(n < 1 || n > 45){
+            throw new IllegalArgumentException(ErrorConstant.LOTTO_NUMBER_RANGE);
+        }
+    }
+    
 }

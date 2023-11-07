bba942592a1c0d20501c80cd18f59bfadd9bdd2f
scarlet0o0 <ghd7673@naver.com>
2023-11-07 16:25:33 +0900

    Feat : 로또 프로그램 완성

---
+++ src/main/java/lotto/domain/LottoProgram.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lotto.congin.LottoWinningAmount;
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.prizecalculator.LottoPrizeCalculator;
+import lotto.domain.prizecalculator.WinningNumbers;
+import lotto.domain.repository.UserLottoRepository;
+import lotto.dto.WinningStatisticsDto;
+
+public class LottoProgram {
+    private final LottoGenerator lottoGenerator;
+    private final LottoPrizeCalculator lottoPrizeCalculator;
+    private final UserLottoRepository userLottoRepository;
+    private LottoMoney lottoMoney;
+
+    public LottoProgram() {
+        this.lottoGenerator = new LottoGenerator();
+        this.lottoPrizeCalculator = new LottoPrizeCalculator();
+        this.userLottoRepository = new UserLottoRepository();
+    }
+
+    public List<Lotto> getLottoListByPrice(int price) {
+        this.lottoMoney = new LottoMoney(price);
+        List<Lotto> userLotto = lottoGenerator.createLottoList(lottoMoney);
+        return userLottoRepository.save(userLotto);
+    }
+
+    public WinningStatisticsDto getWinningNumber(List<Integer> number, Integer bonus) {
+        WinningNumbers winningNumbers = new WinningNumbers(number, bonus);
+        lottoPrizeCalculator.setLottoPrizeCalculator(winningNumbers);
+        List<Lotto> userLotto = userLottoRepository.getUserLotto();
+        Map<LottoWinningAmount, Integer> winningAmount = lottoPrizeCalculator.countWinningNumbers(userLotto);
+
+        int investmentProfit = 0;
+        for (Entry<LottoWinningAmount, Integer> lottoWinningAmountIntegerEntry : winningAmount.entrySet()) {
+            investmentProfit +=
+                    lottoWinningAmountIntegerEntry.getKey().getWinningAmount() * lottoWinningAmountIntegerEntry.getValue();
+        }
+        float lottoYield = lottoMoney.getLottoYield(investmentProfit);
+
+        return new WinningStatisticsDto(winningAmount, lottoYield);
+
+    }
+
+}

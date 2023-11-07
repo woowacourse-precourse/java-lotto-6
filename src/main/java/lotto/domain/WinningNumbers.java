5380908dac7dded4c6c2f4e1381203a20105cc31
twoosky <lhjae3@naver.com>
2023-11-08 07:33:22 +0900

    feat: 당첨 번호 생성 기능 구현

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.utils.IntegerConverter;
+
+public class WinningNumbers {
+    public static final String DELIMITER = ",";
+
+    private final Lotto winningLotto;
+
+    public WinningNumbers(String value) {
+        List<Integer> numbers = mapToLottoNumbers(value);
+        this.winningLotto = new Lotto(numbers);
+    }
+
+    public List<Integer> mapToLottoNumbers(String value) {
+        return Arrays.stream(value.split(DELIMITER))
+                .map(IntegerConverter::convert)
+                .toList();
+    }
+
+
+}

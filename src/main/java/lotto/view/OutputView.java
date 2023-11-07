6ba795390f5f5e254ad5b58fdc082c6ccc8cf087
Hayoon <hy97@sju.ac.kr>
2023-11-07 17:38:23 +0900

    refactor(OutputView): printLottoBenefitResult() 수익률 출력메서드 String.format %s 포맷 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,7 +12,7 @@ public class OutputView {
     }
 
     public void printLottoNumbers(List<Lotto> lottoList) {
-        for(Lotto lotto : lottoList) {
+        for (Lotto lotto : lottoList) {
             System.out.println(lotto.getNumbers());
         }
     }
@@ -22,13 +22,13 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.printf("3개 일치 (5,000원) - %d개\n", lottoRank.get(5));
-        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoRank.get(4));
-        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoRank.get(3));
-        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoRank.get(2));
-        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoRank.get(1));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", lottoRank.get(4));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", lottoRank.get(3));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", lottoRank.get(2));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", lottoRank.get(1));
     }
 
     public void printLottoBenefitResult(Referee referee) {
-        System.out.printf("총 수익률은 %.1f입니다.\n", referee.getLottoBenefit());
+        System.out.printf("총 수익률은 %.1f%s입니다.\n", referee.getLottoBenefit(), "%");
     }
 }

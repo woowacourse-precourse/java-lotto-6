59e29275ffca1b8bb7b9df61d17a8d67d906506f
Hayoon <hy97@sju.ac.kr>
2023-11-07 19:23:10 +0900

    feat(InputValidate): 로또 당첨 번호, 로또 보너스 번호 입력 형식 및 범위 검증

--- src/main/java/lotto/util/validate/InputValidate.java
+++ src/main/java/lotto/util/validate/InputValidate.java
@@ -17,29 +17,39 @@ public class InputValidate {
     }
 
     public static void validateThousandUnitInputFormat(int input) {
-        if(input % 1000 != 0) {
+        if (input % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
     }
 
     public static void validateUniqueInputLottoNumbers(List<Integer> lottoNumbers) {
         Set<Integer> checkingUniqueNumbers = new HashSet<>(lottoNumbers);
-        if(checkingUniqueNumbers.size() != 6) {
+        if (checkingUniqueNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 입력한 로또 당첨 번호 중 중복된 숫자가 존재합니다.");
         }
     }
 
     public static void validateRangeInputLottoNumbers(List<Integer> lottoNumbers) {
-        for(Integer number : lottoNumbers) {
-            if (number > 45 || number < 1) {
-                throw new IllegalArgumentException("[ERROR] 입력한 로또 당첨 번호는 1~45 사이의 숫자여야 합니다.");
+        for (Integer lottoNumber : lottoNumbers) {
+            if (isValidNumberRange(lottoNumber)) {
+                throw new IllegalArgumentException("[ERROR] 입력한 로또 당첨 번호는 1 ~ 45 사이의 숫자여야 합니다.");
             }
         }
     }
 
+    public static void validateRangeInputLottoBonusNumber(int bonusNumber) {
+        if (isValidNumberRange(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 입력한 로또 당첨 보너스 번호는 1 ~ 45 사이의 숫자여야 합니다.");
+        }
+    }
+
     public static void validateUniqueInputLottoBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
-        if(lottoNumbers.contains(bonusNumber)) {
+        if (lottoNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 입력한 보너스 번호는 로또 당첨 번호와 중복된 숫자가 존재합니다.");
         }
+    }
+
+    static public boolean isValidNumberRange(int number) {
+        return 1 <= number && number <= 45;
     }
 }

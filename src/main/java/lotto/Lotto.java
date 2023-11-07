52cf018a11acac0b52d63d8e3a437ad01ab98815
prefer52 <tjsgh151@naver.com>
2023-11-07 22:47:46 +0900

    feat(Lotto, UserLottos): 로또 번호 수열 반환기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,14 +6,11 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public String getLottoSequence() {
+        return numbers.toString();
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/UserLottos.java
+++ src/main/java/lotto/UserLottos.java
@@ -7,16 +7,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class UserLottos {
     private List<Lotto> lottos;
-    private int number;
+    private int size;
 
     public UserLottos(int amount) {
-        this.number = amount/1000;
+        this.size = amount/1000;
+        this.lottos = new ArrayList<>();
         createLottos(amount);
     }
 
     // 중복되지 않은 1~45사이 로또 번호 6개 랜덤 생성 및 정렬
     private void createLottos(int amount) {
-        for(int i = 0 ; i < amount ; i++) {
+        for(int i = 0 ; i < size ; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             if (numbers.size() != 6)
                 throw new IllegalArgumentException("[ERROR]");
@@ -31,7 +32,16 @@ public class UserLottos {
         }
     }
 
-    public int getNumber() {
-        return this.number;
+    public int getSize() {
+        return this.size;
+    }
+
+    public String getLottosString() {
+        String purhcasedLotto = "";
+        for (Lotto lotto : lottos) {
+            purhcasedLotto += lotto.getLottoSequence() + "\n";
+        }
+
+        return purhcasedLotto;
     }
 }

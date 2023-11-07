988d6f5bb9b750ee9b90791eaa209786a1fcfd94
donghyun <kdhdh001@naver.com>
2023-11-08 01:17:51 +0900

    feat: 로또 당첨 등급과 상금 관련 Enum 클래스 구현

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+public enum Prize {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prizeAmount;
+
+    Prize(int matchCount, int prizeAmount) {
+        this.matchCount = matchCount;
+        this.prizeAmount = prizeAmount;
+    }
+
+    public static Prize valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && matchBonus) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        return NONE;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
+}

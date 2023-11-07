f8170db348a6575462e9e9381c0af4d60201c1a1
Identityless <rlandwnd555@naver.com>
2023-11-07 21:22:32 +0900

    refactor : 필드 및 메서드 명 리팩토링
    - statistics -> statistic
    - getStatistics -> getStatistic

--- src/main/java/lotto/Info.java
+++ src/main/java/lotto/Info.java
@@ -2,21 +2,21 @@ package lotto;
 
 public class Info {
     private static final int NUM_OF_GRADES = 5;
-    private int[] statistics;
+    private int[] statistic;
     private int rewards;
 
     public Info() {
-        this.statistics = new int[NUM_OF_GRADES+1];
+        this.statistic = new int[NUM_OF_GRADES+1];
         this.rewards = 0;
     }
 
     public void addStatistic(Grade grade) {
-        statistics[grade.getGrade()]++;
+        statistic[grade.getGrade()]++;
         rewards += grade.price;
     }
 
-    public int[] getStatistics() {
-        return statistics;
+    public int[] getStatistic() {
+        return statistic;
     }
 
     public int getRewards() {

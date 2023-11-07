29195da260b4e43fcb6dc7ead23bbb5c3ccd946a
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-08 06:58:42 +0900

    feat: 반올림하는 자리수, 주석 추가

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -1,6 +1,7 @@
 package lotto;
 
 public class Constants {
-    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_PRICE = 1000;//로또 가격
     public static final String COMMA = ",";
+    public static final int DEMICAL_PLACE = 2;//반올림하는 자리수
 }

f60c1e7ecbeb41d8141932e500c31a90c96d3e3d
KimSangHyeok <tkdgur0906@naver.com>
2023-11-07 23:01:07 +0900

    feat : 구입금액 입력 문구 출력 기능 구현

---
+++ src/main/java/lotto/view/LottoView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class LottoView {
+    private static final String PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+
+    public static void printPurchasePrompt(){
+        System.out.println(PURCHASE_PRICE);
+    }
+}

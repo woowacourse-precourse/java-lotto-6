93b16f9a94128db753c254fb9fea66b7b9e91423
kimdoha <kkhhjj3393@gmail.com>
2023-11-07 23:46:06 +0900

    test(Lotto): 수동 당첨 번호가 1 ~ 45 사이의 수가 아니면 IllegalArgumentException 예외 발생

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
 
@@ -23,4 +24,31 @@ class LottoTest {
         Assertions.assertIterableEquals(numbers, lotto.getNumbers());
         Assertions.assertEquals(numbers.size(), lotto.getNumbers().size());
     }
+
+
+    @DisplayName("수동 당첨 번호가 1 ~ 45 사이의 수가 아니면 IllegalArgumentException 예외 발생")
+    @ParameterizedTest()
+    @MethodSource("getNumberLists")
+    void createLottoByInvalidNumber(List<Integer> numbers) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers));
+    }
+
+    private static Stream<Arguments> getNumberLists() {
+        return Stream.of(
+            Arguments.of(List.of(0, 1, 2, 3, 4, 5)),
+            Arguments.of(List.of(1, 2, 3, 4, 5, 46))
+        );
+    }
+
+    @DisplayName("자동 로또 번호 랜덤 생성 객체 테스트")
+    @Test
+    void createAutomaticLotto() {
+        // Arrange
+        Lotto lotto = new Lotto();
+
+        // Act
+
+        // Assert
+    }
+
 }

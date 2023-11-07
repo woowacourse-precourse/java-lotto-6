9356006ac624d95c80675b7a82f9e482cb2b1928
KDT live <dnjsxo0616@gmail.com>
2023-11-08 03:49:55 +0900

    feat(InputBonusNumber): 보너스 번호 생성 기능 추가
    - 보너스 번호의 유효성 검사하고 생성하는 inputBonusNumber 메서드 추가.

--- src/main/java/lotto/view/InputBonusNumber.java
+++ src/main/java/lotto/view/InputBonusNumber.java
@@ -5,6 +5,12 @@ public class InputBonusNumber {
 
     private static final String INPUT_BONUSNUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
+    public int inputBonusNumber(){
+        String input = inputBonusNumberView();
+        validateInput(input);
+        return Integer.parseInt(input);
+    }
+
     private String inputBonusNumberView(){
         System.out.println(INPUT_BONUSNUMBER_MESSAGE);
         return Console.readLine();

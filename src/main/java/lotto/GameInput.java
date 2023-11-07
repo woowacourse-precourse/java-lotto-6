10ea450e8c3f74c4609ec0c632bdd4f81b8d5df9
Sur0n <skyzephyr333@gmail.com>
2023-11-08 07:49:53 +0900

    fix : GameInput method splitUserNumbers splitNumbers

--- src/main/java/lotto/GameInput.java
+++ src/main/java/lotto/GameInput.java
@@ -18,7 +18,7 @@ public class GameInput {
 
     public static List<Integer> getUserHopeLottoNumbers() {
         String UserHopeLottoNumbers = Console.readLine();
-        List<Integer> numbersCopy = splitNumbers(UserHopeLottoNumbers);
+        List<Integer> numbersCopy = splitUserNumbers(UserHopeLottoNumbers);
         validate(numbersCopy);
         return numbersCopy;
 
@@ -26,15 +26,22 @@ public class GameInput {
     }
     //private static final String REGEXP_PATTERN_NUMBER = "^[\\d]";
 
+    private static List<Integer> splitUserNumbers(String Numbers){
+        List<String> ArgNumbers = Arrays.stream(Numbers.split(",")).toList();
+        //System.out.println(ArgNumbers);
+        List<Integer> numbersCopy = validateNumbersArgument(ArgNumbers);
+        return numbersCopy;
+    }
     private static List<Integer> splitNumbers(String Numbers){
-        List<String> numbers = Arrays.stream(Numbers.split(",")).toList();
-        List<Integer> numbersCopy = validateNumbersArgument(numbers);
+        List<String> ArgNumbers = Arrays.stream(Numbers.split(", ")).toList();
+        //System.out.println(ArgNumbers);
+        List<Integer> numbersCopy = validateNumbersArgument(ArgNumbers);
         return numbersCopy;
     }
 
     //ticketNumbers와 bonusnumber, numbers 체크
     //일치 및 수익률 계산
-    public int earnMoney( List<Integer> numbers, List<String> ticketNumbers, int bonusnumber ){
+    public static int earnMoney( List<Integer> numbers, List<String> ticketNumbers, int bonusnumber ){
         int firstplaced = 0;
         int secondplaced = 0;
         int thirdplaced = 0;
@@ -61,22 +68,32 @@ public class GameInput {
                 thirdplaced++;
             }
         }
-        System.out.printf(Messages.CHECK_MATCH_THREE+"%d개\n",fifthplaced);
-        System.out.printf(Messages.CHECK_MATCH_FOUR+"%d개\n",fourthplaced);
-        System.out.printf(Messages.CHECK_MATCH_FIVE+"%d개\n",thirdplaced);
-        System.out.printf(Messages.CHECK_MATCH_BONUS+"%d개\n",secondplaced);
-        System.out.printf(Messages.CHECK_MATCH_SIX+"%d개\n",firstplaced);
+        System.out.printf(Messages.CHECK_MATCH_THREE.getMessages()+"%d개\n",fifthplaced);
+        System.out.printf(Messages.CHECK_MATCH_FOUR.getMessages()+"%d개\n",fourthplaced);
+        System.out.printf(Messages.CHECK_MATCH_FIVE.getMessages()+"%d개\n",thirdplaced);
+        System.out.printf(Messages.CHECK_MATCH_BONUS.getMessages()+"%d개\n",secondplaced);
+        System.out.printf(Messages.CHECK_MATCH_SIX.getMessages()+"%d개\n",firstplaced);
         return fifthplaced*5000+fourthplaced*50000+thirdplaced*1500000+secondplaced*30000000+firstplaced*2000000000;
         //enum으로 리턴할까?
         //removeAll으로 사용해서
     }
 
     private static List<Integer> validateNumbersArgument(List<String> Numbers){
-        List<Integer> NumbersCopy = new ArrayList<>();
+        List<Integer> NumbersCopy = new ArrayList<Integer>();
+        //에러 생기는 이유 List<String> 받을때 [8,43]으로 작업됨
+//        System.out.println(Integer.parseInt(Numbers.get(0).replaceAll("[\\[\\]]","")));
+//        System.out.println(Integer.parseInt(Numbers.get(1)));
+//        System.out.println(Integer.parseInt(Numbers.get(2)));
+//        System.out.println(Integer.parseInt(Numbers.get(3)));
+//        System.out.println(Integer.parseInt(Numbers.get(4)));
+//        System.out.println(Integer.parseInt(Numbers.get(5)));
+        //NumberFormatException 발생 43] 형태로 인해서
         try{
             for( int i = 0 ; i < Numbers.size() ; i++ ) {
-                // 타입에러 어케잡지? 트라이캐치 없이
-                NumbersCopy.add(Integer.valueOf(Numbers.get(i)));
+
+                NumbersCopy.add(Integer.parseInt(Numbers.get(i).replaceAll("[\\[\\]]","")));
+                //정규식 제거
+
             }
         }catch (Exception e){
             throw new IllegalArgumentException();

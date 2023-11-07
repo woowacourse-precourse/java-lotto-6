1a36b4f0683569e2ddd6ffb1c755aa814ccafb07
jookbooin <3670lsh@naver.com>
2023-11-07 19:14:42 +0900

    test : 로또 입력 테스트 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.constant.StringConstant.COMMA_SPACE;
+import static lotto.message.ErrorMessage.EXIST_DUPLICATE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +28,20 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호를 입력 구분자로 연결지은 문자열 확인")
+    @Test
+    public void joinNumbersWithDelimiterTest() {
+
+        Lotto lotto = new Lotto(List.of(6, 1, 5, 2, 4, 3));
+        assertThat(lotto.joinNumbersWithDelimiter(COMMA_SPACE)).isEqualTo("1, 2, 3, 4, 5, 6");
+    }
+
+    @DisplayName("중복 요소 존재하는지 체크")
+    @Test
+    public void validateDuplicateTest(){
+
+        assertThatThrownBy(()->new Lotto(List.of(1,1,2,3,4,5))).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EXIST_DUPLICATE.getMessage());
+    }
 }

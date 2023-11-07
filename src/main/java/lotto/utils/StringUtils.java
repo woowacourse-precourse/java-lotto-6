2357d958484d37a804cbeee760e9989c5e3edfbb
dustjq1004 <dustjq1005@naver.com>
2023-11-08 01:39:08 +0900

    yeonsup/feat : StringUtils 스트링 리스트로 변환 메서드 추가

--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -1,7 +1,18 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringUtils {
     public static String formatCommaIntegerToString(int number) {
         return String.format("%,d", number);
+    }
+
+    public static List<Integer> asListByDelimiter(String winingNumbers, String delimiter) {
+        List<Integer> list = new ArrayList<>();
+        for (String str : winingNumbers.split(delimiter)) {
+            list.add(Integer.valueOf(str));
+        }
+        return list;
     }
 }

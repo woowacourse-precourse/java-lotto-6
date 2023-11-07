ec23caffed428580c5da32cc8e6535e0d4f2c704
Son JunHyung <kingjh1125@gmail.com>
2023-11-07 21:50:06 +0900

    style (ErrorMessage): 에러 메세지 변경
    숫자가 입력되지 않았을 경우 출력되는 에러 메세지를 변경하였습니다:
    before:
    - 로또 구매 금액은 숫자만 가능합니다.
    after:
    - 숫자로만 입력이 가능합니다.

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     LOTTO_COUNT_LIMIT("로또 번호의 개수는 6개여야 합니다."),
     CANT_DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다."),
     LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    ONLY_CAN_NUMBER("로또 구매 금액은 숫자만 가능합니다."),
+    ONLY_CAN_NUMBER("숫자로만 입력이 가능합니다."),
     CANT_DIVIDE_AMOUNT("로또 구매 금액은 1,000원 단위여야 합니다."),
     NOT_POSITIVE_AMOUNT("로또 구매 금액은 자연수여야 합니다."),
     PREFIX("[ERROR] ");

5ccc0fbab11f570b11dab91b0d506eb61eea67ec
changhyun9 <ckdgus1713@naver.com>
2023-11-07 22:05:58 +0900

    feat : add class about WinningLotto

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,2 +1,50 @@
-package lotto.domain;public class WinningLotto {
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class WinningLotto {
+    private Lotto winningNumber;
+    private Integer bonusNumber;
+
+    public WinningLotto(String winningNumberInput, String bonusNumberInput) {
+        List<Integer> winningNumberList = stringInputToIntList(winningNumberInput);
+        this.winningNumber = new Lotto(winningNumberList);
+        int bonusParseNumber = validateNumericInput(bonusNumberInput);
+        validateDuplication(winningNumberList, bonusParseNumber);
+        validateBonusRange(bonusParseNumber);
+        this.bonusNumber = bonusParseNumber;
+    }
+
+    private List<Integer> stringInputToIntList(String winningNumberInput) {
+        String[] split = winningNumberInput.split(",");
+        Integer[] integerSplit = Stream.of(split)
+                                 .mapToInt(Integer::parseInt)
+                                 .boxed()
+                                 .toArray(Integer[]::new);
+        return Arrays.asList(integerSplit);
+    }
+
+    private void validateDuplication(List<Integer> winningNumberList, int bonusNumber) {
+        if (winningNumberList.contains(bonusNumber)) {
+            throw new IllegalArgumentException("로또 번호와 보너스 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateBonusRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이의 번호만 입력 가능합니다.");
+        }
+    }
+
+    private int validateNumericInput(String bonusNumberInput) {
+        try {
+            int bonusParseNumber = Integer.parseInt(bonusNumberInput);
+            return bonusParseNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("로또 번호는 숫자만 입력이 가능합니다.");
+        }
+    }
+
 }
--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,2 +1,43 @@
-package lotto.domain;public class WinningLottoTest {
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+
+    @DisplayName("6개 이상의 숫자를 입력하면 예외 발생")
+    @Test
+    void enterLottoInputByOverSize() {
+        Assertions.assertThatThrownBy(() -> new WinningLotto("1,2,3,4,5,6,7", "8"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복된 숫자를 입력하면 예외 발생")
+    @Test
+    void enterLottoInputByDuplicatedNumber() {
+        Assertions.assertThatThrownBy(() -> new WinningLotto("1,2,3,4,5,5", "8"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 숫자가 이미 당첨 번호에 있는 경우 예외 발생")
+    @Test
+    void enterBonusNumberByDuplicatedNumber() {
+        Assertions.assertThatThrownBy(() -> new WinningLotto("1,2,3,4,5,6", "5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1 ~ 45 사이 이외의 번호가 입력되면 예외 발생")
+    @Test
+    void enterLottoNumberByOutOfRange() {
+        Assertions.assertThatThrownBy(() -> new WinningLotto("1,2,3,4,46,5", "8"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 이외의 타입을 입력하면 예외 발생")
+    @Test
+    void enterLottoNumberByOtherType() {
+        Assertions.assertThatThrownBy(() -> new WinningLotto("1,2,3,4,5,6", "1,"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

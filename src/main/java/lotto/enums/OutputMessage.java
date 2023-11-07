fabba3b8d2849ec7121ef1afd4251e9bb7616cf4
seona-moon <fhflwhwl5@gmail.com>
2023-11-07 21:51:23 +0900

    feat: 발생할 수 있는 출력 메시지를 모은 enum 구현

--- src/main/java/lotto/enums/OutputMessage.java
+++ src/main/java/lotto/enums/OutputMessage.java
@@ -1,10 +1,10 @@
-package lotto.Enums;
+package lotto.enums;
 
 public enum OutputMessage {
     PURCHASE_AMOUNT_PROMPT("구입금액을 입력해 주세요."),
-    WINNING_NUMBER_PROMPT("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_PROMPT("보너스 번호를 입력해 주세요."),
-    PURCHASE_NUMBER_OUTPUT("%d개를 구매했습니다."),
+    WINNING_NUMBER_PROMPT("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_PROMPT("\n보너스 번호를 입력해 주세요."),
+    PURCHASE_NUMBER_OUTPUT("\n%d개를 구매했습니다.\n"),
     WINNING_STATISTICS_OUTPUT("당첨 통계\n---\n");
 
     private final String message;

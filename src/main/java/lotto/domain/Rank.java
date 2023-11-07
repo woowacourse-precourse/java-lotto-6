6b405169a351c28a1cc0bc0d6d11784053ce7ed9
jiu6525 <jiu6525@daum.net>
2023-11-07 19:35:24 +0900

    feat : 매치된 숫자를 확인하는 기능 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -19,4 +19,8 @@ public enum Rank {
         this.winningMoney = winningMoney;
         this.message = message;
     }
+
+    public boolean matchCount(int countOfMatch) {
+        return this.countOfMatch == countOfMatch;
+    }
 }

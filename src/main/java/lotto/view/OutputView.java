c33f2146de464a6db120c78d891645423ea249db
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 02:43:56 +0900

    feat(printWinningNumbersInputMessage): 당첨번호 입력 메세지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 public class OutputView {
     private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public void printLottoPurchaseInputMessage() {
         System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
@@ -10,5 +11,9 @@ public class OutputView {
 
     public void printLottoPurchaseCountMessage(int LottoPurchaseCount) {
         System.out.println(LottoPurchaseCount + LOTTO_PURCHASE_COUNT_MESSAGE);
+    }
+
+    public void printWinningNumbersInputMessage() {
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
     }
 }

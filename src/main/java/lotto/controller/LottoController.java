6767737b53c51d69a77993e928d7d3ccdfa7da8b
kimdongyeon <do121712@naver.com>
2023-11-07 17:13:49 +0900

    fix: run() 메소드를 통해 게임 시작 가능 하게 단편화, DI를 해주고, Service 클래스에서 진행에 필요한 메소드들을 호출

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,21 +1,17 @@
 package lotto.controller;
 
-import lotto.domain.Player;
+import lotto.domain.Lottos;
 import lotto.service.LottoService;
+import lotto.validation.Validation;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
+    private final LottoService lottoService = new LottoService(new InputView(), new OutputView(), new Validation(), new Lottos());
 
-    private LottoService lottoService = new LottoService();
-
-    public void run(){
-
-    }
-
-    public void start(){
-
-    }
-
-    public void finish(){
-
+    public void run() {
+        lottoService.createPlayer();
+        lottoService.createWinningLotto();
+        lottoService.totalPercent();
     }
 }

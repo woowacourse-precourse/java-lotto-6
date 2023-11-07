a4c12fdff2db8cec902fd5e765fd24af87709f42
ekeprl <ykeon26@naver.com>
2023-11-07 23:29:40 +0900

    ENUM을 이용한 당첨확인 메서드구현(오류수정)

--- src/main/java/lotto/Model/Winning.java
+++ src/main/java/lotto/Model/Winning.java
@@ -11,22 +11,22 @@ import java.util.List;
         EMPTY(0
                 , 0
                 , (matchLottoNumber, containBonusNumber) -> matchLottoNumber < 3),
-        FIFTH(5,000
-                , 3
+        FIFTH(3
+                , 5_000
                 , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 3),
-        FOURTH(50,000
-                , 4
+        FOURTH(4
+                , 50_000
                 , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
-        THIRD(1,500,000
-                , 5
+        THIRD(5
+                , 1_500_000
                 , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
-        SECOND(30,000,000
-                , 5
+        SECOND(5
+                , 30_000_000
                 , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
-        FIRST(2,000,000,000
-                , 6
+        FIRST(6
+                , 2_000_000_000
                 , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
-    }
+
 
     private final int money;
     private final int matchLottoNumber;
@@ -36,13 +36,13 @@ import java.util.List;
         this.money = money;
         this.matchLottoNumber = matchLottoNumber;
         this.isMatch = isMatch;
-        }
+    }
     public static Winning getWinning(final int matchLottoNumber, final boolean containBonusNumber) {
             return Arrays.stream(Winning.values())
                     .filter(winning -> winning.isMatch.test(matchLottoNumber, containBonusNumber))
                     .findAny()
                     .orElse(EMPTY);
-        }
+    }
     public int getMoney() {
         return money;
     }
@@ -50,7 +50,4 @@ import java.util.List;
     public int getMatchLottoNumber() {
         return matchLottoNumber;
     }
-
-    }
-
-
+}

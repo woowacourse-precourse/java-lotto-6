fa8c14e195c11faa952f8c08f4b86a24d2af8c43
nove1080 <skwpqjq@gmail.com>
2023-11-07 23:35:03 +0900

    feat: 출력 메시지 상수에 추가 상수 선언

--- src/main/java/lotto/constants/OutputMessage.java
+++ src/main/java/lotto/constants/OutputMessage.java
@@ -8,6 +8,8 @@ public enum OutputMessage {
     RANK_3("5개 일치 (1,500,000원) - %d개"),
     RANK_4("4개 일치 (50,000원) - %d개"),
     RANK_5("3개 일치 (5,000원) - %d개"),
+    PROFIT_RATE_INTRO("당첨 통계"),
+    SEPARATOR("---"),
     PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;

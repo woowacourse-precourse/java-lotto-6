2351473ee3f46af8c3bf5469981cc32df61a02b9
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 18:17:31 +0900

    feat : 랜덤 넘버 만드는 기능 구현 - RandomNum 의 메서드 getRandomNumber() 오버라이딩

--- src/main/java/lotto/util/MakeRandomNumber.java
+++ src/main/java/lotto/util/MakeRandomNumber.java
@@ -1,11 +1,15 @@
 package lotto.util;
 
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+// 여기서 랜덤 넘버로 리스트 뽑아주고 로또에서 사용자 수 만큼 뽑아주는 기능 짜기
 public class MakeRandomNumber implements RandomNum {
     @Override
     public List<Integer> getRandomNumber() {
-        return null;
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 }

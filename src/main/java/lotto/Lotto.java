5811522e05493f63334680db4cb6ed9a3140ffba
dyjeong365 <dyjeong365@gmail.com>
2023-11-08 00:37:36 +0900

    feat: add validateCount()

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,12 @@ public class Lotto {
         if (numbers.size() != 6 || numbers.stream().distinct().count() != NUMBER_OF_LOTTO_NUMBERS) {
             throw new IllegalArgumentException("[ERROR] 중복되지 않는 " + NUMBER_OF_LOTTO_NUMBERS + "개의 숫자여야 합니다.");
         } else if (numbers.stream().anyMatch(i -> i < MIN_LOTTO_NUMBER || i > MAX_LOTTO_NUMBER)) {
+    private static void validateCount(final List<Integer> numbers, final int NUMBER_OF_LOTTO_NUMBERS) {
+        if (numbers.size() != NUMBER_OF_LOTTO_NUMBERS) {
+            throw new IllegalArgumentException("[ERROR] " + NUMBER_OF_LOTTO_NUMBERS + "개의 숫자여야 합니다.");
+        }
+    }
+
             throw new IllegalArgumentException("[ERROR] 로또 번호는 " + MIN_LOTTO_NUMBER + "부터 " + MAX_LOTTO_NUMBER + " 사이의 숫자여야 합니다.");
         }
     }

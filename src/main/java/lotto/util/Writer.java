8f44d6589eafe6acaa0dc5625ed351259250a13c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-07 18:03:38 +0900

    feat: 출력 기능
    당첨 번호를 입력해 주세요 기능 추가

--- src/main/java/lotto/util/Writer.java
+++ src/main/java/lotto/util/Writer.java
@@ -5,11 +5,17 @@ import lotto.domain.Lottos;
 public class Writer {
 
     private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+
     public static void purchaseAmount(){
         System.out.println(PURCHASE_AMOUNT);
     }
 
     public static void lottos(Lottos lottos) {
         System.out.println(lottos);
+    }
+
+    public static void winningNumbers() {
+        System.out.println(WINNING_NUMBERS);
     }
 }

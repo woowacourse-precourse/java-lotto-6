dd1fecb425feba0e93a58e46724ce70b3ce5b4fb
eora21 <eora21@naver.com>
2023-11-07 11:13:12 +0900

    test: 💍 추가 로또 매칭에서 사용됐던 테스트 로직들을 로또 티어로 병합

--- src/test/java/lotto/domain/LottoWinningTierTest.java
+++ src/test/java/lotto/domain/LottoWinningTierTest.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Optional;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoWinningTierTest {
     @Test
@@ -16,5 +23,33 @@ public class LottoWinningTierTest {
     @DisplayName("일치한 로또 숫자 개수가 6개를 초과하면 예외를 발생시킨다.")
     void calculateLottoWinningTierByOverCount() {
         assertThrows(IllegalArgumentException.class, () -> LottoWinningTier.calculateTier(7, false));
+    }
+
+    @ParameterizedTest
+    @DisplayName("일치한 로또 숫자 개수가 3 미만이면 빈 옵셔널 값을 반환한다.")
+    @ValueSource(ints = {0, 1, 2})
+    void calculateLottoWinningTierByLess3(int matchCount) {
+        Optional<LottoWinningTier> tierOptional = LottoWinningTier.calculateTier(matchCount, false);
+        assertThat(tierOptional).isEmpty();
+    }
+
+    @ParameterizedTest
+    @DisplayName("일치한 로또 숫자 개수가 3 이상이면 상태가 들어있는 옵셔널 값을 반환한다.")
+    @MethodSource("matchCountMatchBonusAndExpectTier")
+    void calculateLottoMatchStateByLessMinCount(int matchCount, boolean matchBonus, LottoWinningTier expectTier) {
+        Optional<LottoWinningTier> stateOptional = LottoWinningTier.calculateTier(matchCount, matchBonus);
+        assertThat(stateOptional).hasValue(expectTier);
+    }
+
+    public static Stream<Arguments> matchCountMatchBonusAndExpectTier() {
+        return Stream.of(
+                Arguments.of(6, false, LottoWinningTier.FIRST_TIER),
+                Arguments.of(5, true, LottoWinningTier.SECOND_TIER),
+                Arguments.of(5, false, LottoWinningTier.THIRD_TIER),
+                Arguments.of(4, true, LottoWinningTier.FOURTH_TIER),
+                Arguments.of(4, false, LottoWinningTier.FOURTH_TIER),
+                Arguments.of(3, true, LottoWinningTier.FIFTH_TIER),
+                Arguments.of(3, false, LottoWinningTier.FIFTH_TIER)
+        );
     }
 }

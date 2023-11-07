3fecb062897bfe8d6752e56a58f5997a6e5a76b1
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 21:30:40 +0900

    feat : 보너스 번호 View 에서 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private String INPUT_MONEY = "구입금액을 입력해 주세요.";
-    private final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    private final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
 
     public String inputPurchasePrice() {
@@ -15,6 +15,11 @@ public class InputView {
 
     public String inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBERS);
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 

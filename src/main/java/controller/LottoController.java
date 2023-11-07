dc46bc66ea41a22bb27e6473f55fec72f9c2c6f6
daneng4 <daneng4@naver.com>
2023-11-07 12:19:49 +0900

    remove: 메서드 호출 제거

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -8,16 +8,12 @@ public class LottoController {
     LottoService lottoService = new LottoService();
     public LottoNumber winningNumber = new LottoNumber();
 
-    private int lottoCost;
-
     public LottoController(){
         startLotto();
     }
 
     public void startLotto(){
-        lottoService.getLottoCost();
         lottoService.buyLotto();
-        lottoService.createLottoNumber();
     }
 
 }

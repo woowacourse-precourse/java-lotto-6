1abb36c5d032799a0358ea3f28cc426cc83b8a51
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-07 23:34:20 +0900

    refact(lottorank): rank 네이밍 간소화

--- src/main/java/lotto/utils/LottoRank.java
+++ src/main/java/lotto/utils/LottoRank.java
@@ -1,18 +1,18 @@
 package lotto.utils;
 
 public enum LottoRank {
-    FIFTH_PRIZE(3, 5000, "3개 일치 (5,000원) - "),
-    FOURTH_PRIZE(4, 50000, "4개 일치 (50,000원) - "),
-    THIRD_PRIZE(5, 1500000, "5개 일치 (1,500,000원) - "),
-    SECOND_PRIZE(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST_PRIZE(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
+    FIFTH(3, 5000, "3개 일치 (5,000원) - "),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
 
     private final int matchedNumber;
     private final long prize;
     private final String resultMessage;
 
-    LottoRank(int matchingNumbers, long prize, String resultMessage) {
-        this.matchedNumber = matchingNumbers;
+    LottoRank(int matchedNumbers, long prize, String resultMessage) {
+        this.matchedNumber = matchedNumbers;
         this.prize = prize;
         this.resultMessage = resultMessage;
     }

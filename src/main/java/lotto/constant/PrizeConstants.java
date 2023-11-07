03142666cd5ad419d9082b6f9f3e88e4f8d4dd09
DESKTOP-6F3759O\hyemin <gpals5987@gmail.com>
2023-11-08 03:33:19 +0900

    feat(PrizeConstants): 상금 관련 상수 추가
    로또 게임의 당첨 번호 일치 개수에 따른 상금을 정의한 상수를 PrizeConstants 클래스에 추가하였습니다.

---
+++ src/main/java/lotto/constant/PrizeConstants.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public class PrizeConstants {
+    public static final int MATCH_3_NUMBER = 3;
+    public static final int MATCH_4_NUMBER = 4;
+    public static final int MATCH_5_NUMBER = 5;
+    public static final int MATCH_6_NUMBER = 6;
+
+    public static final int MATCH_3_PRIZE = 5000;
+    public static final int MATCH_4_PRIZE = 50000;
+    public static final int MATCH_5_PRIZE = 1500000;
+    public static final int MATCH_5_BONUS_PRIZE = 30000000;
+    public static final int MATCH_6_PRIZE = 2000000000;
+
+
+
+
+}

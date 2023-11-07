4607ee7cb5b99825ddf86055d66e4af5a980789d
K-KY <kyuyoungk@naver.com>
2023-11-07 23:26:40 +0900

    Test(UserInputMoneyTest) : UserInput에서 사용하지 않는 메서드 테스트 제거

--- src/test/java/lotto/UserInputMoneyTest.java
+++ src/test/java/lotto/UserInputMoneyTest.java
@@ -22,20 +22,6 @@ public class UserInputMoneyTest {
     }
 
     @Test
-    void 금액이_천보다_작을_때() {
-        String input = "999";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-
-        UserInput userInput = new UserInput();
-        assertThatThrownBy(userInput::money)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("천보다 큰 금액을 입력해주세요.");
-        System.setIn(System.in);
-        Console.close();
-    }
-
-    @Test
     void 숫자가_아닐_때() {
         String input = "I000";
         ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
@@ -45,34 +31,6 @@ public class UserInputMoneyTest {
         assertThatThrownBy(userInput::money)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자만 입력 해주세요.");
-        System.setIn(System.in);
-        Console.close();
-    }
-
-    @Test
-    void 천원_단위가_아닐_때() {
-        String input = "1001";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-
-        UserInput userInput = new UserInput();
-        assertThatThrownBy(userInput::money)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("금액은 천원 단위로 입력 해주세요.");
-        System.setIn(System.in);
-        Console.close();
-    }
-
-    @Test
-    void 금액이_너무_클_때() {
-        String input = "3000000";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        System.setIn(inputStream);
-
-        UserInput userInput = new UserInput();
-        assertThatThrownBy(userInput::money)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("100만을 넘는 금액은 입력 할 수 없습니다.");
         System.setIn(System.in);
         Console.close();
     }

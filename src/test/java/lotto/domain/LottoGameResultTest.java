c205cda6ea9be36abd18b7f0a26d93989274ddc1
zeomzzz <jmlee2705@gmail.com>
2023-11-08 06:02:57 +0900

    test: calculateLottoResult 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoGameResultTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.enums.LottoRankEnum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGameResultTest {
+
+    @DisplayName("calculateLottoResult 메소드 : 로또 등수 반환 테스트")
+    @Test
+    void calculateLottoResult_rankResult() {
+        LottoGameResult lottoGameResult = new LottoGameResult();
+
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = new ArrayList<>(List.of(4, 5, 6, 7, 8, 9));
+        int bonusNumber = 1;
+        Winning winning = new Winning(winningNumbers, bonusNumber);
+
+        assertThat(lottoGameResult.calculateLottoResult(lotto, winning)).isEqualTo(LottoRankEnum.FIFTH);
+    }
+}

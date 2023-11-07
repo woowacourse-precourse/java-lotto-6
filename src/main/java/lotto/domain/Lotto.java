c1525e40d36e31f519a582d1edcbf0c4f2d7075e
jongmyeongbak <withpleasure@gmail.com>
2023-11-07 17:48:13 +0900

    feat(Lotto): 검증 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,14 @@
 package lotto.domain;
 
+import static lotto.domain.constants.LottoConstants.ERROR_LOTTO_NUMBER_RANGE;
+import static lotto.domain.constants.LottoConstants.LOTTO_MAX_NUMBER;
+import static lotto.domain.constants.LottoConstants.LOTTO_MIN_NUMBER;
+import static lotto.domain.constants.LottoConstants.LOTTO_NUMBER_COUNT;
+
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,9 +17,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSizeOfSix(numbers);
+        validateUniqueness(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateSizeOfSix(final List<Integer> numbers) {
+        if (numbers == null || numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 번호는 " + LOTTO_NUMBER_COUNT + "개여야 합니다.");
         }
+    }
+
+    private void validateUniqueness(final List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateRange(final List<Integer> numbers) {
+        if (numbers.stream().anyMatch(this::isNumberOutOfRange)) {
+            throw new IllegalArgumentException(ERROR_LOTTO_NUMBER_RANGE);
+        }
+    }
+
+    private boolean isNumberOutOfRange(int number) {
+        return number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER;
     }
 
     public String formatNumbers() {

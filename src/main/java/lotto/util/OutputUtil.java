310c74aedcf683cb294217af341c33a24d1d882a
jae woo Jung <awfjol2008@naver.com>
2023-11-08 03:53:53 +0900

    feat: 우승 로또 출력 포맷 변환 기능 구현

--- src/main/java/lotto/util/OutputUtil.java
+++ src/main/java/lotto/util/OutputUtil.java
@@ -1,8 +1,24 @@
 package lotto.util;
 
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
+
+import java.util.StringJoiner;
+
 public class OutputUtil {
 
     private OutputUtil() {
 
+    }
+
+    public static String lottoFormat(Lotto lotto) {
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+        lotto.getLottoNumbers()
+                .forEach(lottoNumber -> joinNumber(joiner, lottoNumber));
+        return joiner.toString();
+    }
+
+    private static StringJoiner joinNumber(StringJoiner joiner, LottoNumber lottoNumber) {
+        return joiner.add(String.valueOf(lottoNumber.getLottoNumber()));
     }
 }

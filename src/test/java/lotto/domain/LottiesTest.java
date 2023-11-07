81f3389b859e66695f8571c8f3b8eb24a650c5e9
gusdn0108 <gusdn6671@naver.com>
2023-11-07 13:14:22 +0900

    test(Lotties) : Lotto List 를 반환할 땐 unmodifiedList 를 반환

--- src/test/java/lotto/domain/LottiesTest.java
+++ src/test/java/lotto/domain/LottiesTest.java
@@ -1,20 +1,21 @@
 package lotto.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottiesTest {
-
+    @DisplayName("Lotto List 를 반환할 땐 unmodifiedList 를 반환")
     @Test
-    void getLotties() {
-    }
-
-    @Test
-    void getLottiesNumbers() {
-    }
-
-    @Test
-    void size() {
+    public void returnLottoListAsUnmodifiable(){
+        Lotties lotties = new Lotties(List.of(new Lotto(List.of(1,2,3,4,5,6))));
+        List<Lotto> valueLotto = lotties.getLotties();
+        assertThatThrownBy(
+                ()-> valueLotto.add(new Lotto(List.of(1,2,3,4,5,6)))
+        ).isInstanceOf(UnsupportedOperationException.class);
     }
 }

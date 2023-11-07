1e7a4e1c58659a03d056a7aa6941b4f4ff6334f8
tsulocalize <tsulocalizing@gmail.com>
2023-11-07 09:04:14 +0900

    feat : 로또 출력 기능 구현

--- src/main/java/lotto/LottoPlayer.java
+++ src/main/java/lotto/LottoPlayer.java
@@ -8,10 +8,23 @@ public class LottoPlayer {
     private int money = 0;
 
     public void generateLottos(int number) {
+        System.out.printf("%d개를 구매했습니다.", number);
         for (int i = 0; i < number; i++) {
-            Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+            Lotto lotto = new Lotto(List.of(1,2,3,4,5,6)); // naturalOrder 적용 필요
+            printLotto(lotto);
             lottos.add(lotto);
         }
+    }
+
+    private void printLotto(Lotto lotto) {
+        StringBuilder sb = new StringBuilder("[");
+        List<Integer> numbers = lotto.getNumbers();
+        for (Integer number : numbers) {
+            sb.append(number);
+            sb.append(", ");
+        }
+        sb.replace(sb.length()-2, sb.length(), "]");
+        System.out.println(sb);
     }
 
     public void removeLottos() {

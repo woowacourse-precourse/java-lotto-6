5bcece4cc2e3f2d291d5b98afe1dc616a6c2fcb1
haruday97 <starharu97@naver.com>
2023-11-07 17:34:12 +0900

    :white_check_mark: test : Lotto Test 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +24,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("범위를 벗어난 숫자를 가진경우 예외가 발생한다.")
+    @Test
+    void should_ThrowException_When_HasNumberOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(50, 51, 52, 53, 54, 55)))
+                .isInstanceOf(LottoException.class);
+    }
+
+    @DisplayName("불변객체를 조작하려는 경우 예외가 발생한다.")
+    @Test
+    void should_ThrowException_When_TryToManipulate() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> lotto.getNumbers().add(7))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
 }

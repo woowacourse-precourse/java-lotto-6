474387bff86769482a2d91c8d380670fc9d65237
Hayoon <hy97@sju.ac.kr>
2023-11-07 14:45:36 +0900

    feat(LottoService): Lotto 별 인스턴스를 생성하여 초기화한 값들을 initLottoList() 메서드를 활용해 List<Lotto>로 반환한다

---
+++ src/main/java/lotto/model/service/LottoService.java
@@ -0,0 +1,26 @@
+package lotto.model.service;
+
+import lotto.model.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+public class LottoService {
+
+    public List<Lotto> initLottoList(int purchaseAmount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for(int i = 0; i < purchaseAmount; i++) {
+            List<Integer> lottoNumbers = pickLottoNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            lottoList.add(lotto);
+        }
+
+        return lottoList;
+    }
+
+    public List<Integer> pickLottoNumbers() {
+        return pickUniqueNumbersInRange(1, 45, 6);
+    }
+}

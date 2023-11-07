e14cafb92aebbb34dbf1c4fa401ff6e5790cb29e
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-07 22:02:04 +0900

    feat: 어플리케이션 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-
+    	
+    	Controller controller = new Controller();
+    	
+    	controller.run();
+        
     }
 }

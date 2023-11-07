634e08ef6e71e1947e360615649ad6cb3ae4f91d
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 21:09:53 +0900

    feat(InputController) : 재입력 기능 구현

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,10 +1,42 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class InputController {
 
-    private final InputView inputView = new InputView();
+    private static final InputView inputView = new InputView();
 
+    public static int inputAmountUntilSuccess() {
+        while (true) {
+            try {
+                return inputView.inputPurchaseAmount();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static List<Integer> inputWinningNumbersUntilSuccess() {
+        while (true) {
+            try {
+                return inputView.inputLotto();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static int inputBonusNumberUntilSuccess() {
+        while (true) {
+            try {
+                return inputView.inputBonusNumbers();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
 }

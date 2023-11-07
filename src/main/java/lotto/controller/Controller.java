4c736059622587e47ca74e8ec2f917c1b4093786
Donghun Won <wonhun1225@gmail.com>
2023-11-07 19:15:58 +0900

    feat(controller) : 게임시작전 로직 구현

---
+++ src/main/java/lotto/controller/Controller.java
@@ -0,0 +1,42 @@
+package lotto.controller;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+import lotto.model.Answer;
+import lotto.model.Lotto;
+import lotto.model.LottoGame;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class Controller {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    LottoGame lottoGame = new LottoGame();
+    Answer answerNumber;
+    public void run() {
+        beforeLottoGame();
+        startLottoGame();
+        showGameResult();
+    }
+    private void beforeLottoGame() {
+        int lottoNum = inputView.moneyInput();
+        lottoGame.createLotto(lottoNum);
+        List<Lotto> lottoGames = lottoGame.getLottoGames();
+        outputView.showLottoNum(lottoNum,lottoGames);
+
+        List<Integer> answer =inputView.answerInput();
+        int bonus = inputView.bonusInput();
+        answerNumber = new Answer(answer,bonus);
+    }
+
+    private void startLottoGame(){
+
+    }
+
+    private  void showGameResult(){
+
+    }
+
+
+}

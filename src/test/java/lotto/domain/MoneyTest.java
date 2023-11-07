3b6cedbacaec56064533a67772c0fb829c9e04ac
IM-GYURI <kgulr0517@gmail.com>
2023-11-08 02:12:14 +0900

    test: MoneyTest 작성

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MoneyTest {
+    @ParameterizedTest
+    @ValueSource(ints = {10001, 1100, -1})
+    @DisplayName("올바르지 않은 금액")
+    void failed(int inputValue) {
+        assertThatThrownBy(() -> new Money(inputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구매한 로또 티켓 장수")
+    void getTicketTest() {
+        Money money = new Money(1000);
+        assertThat(money.getTicket()).isEqualTo(1);
+    }
+}

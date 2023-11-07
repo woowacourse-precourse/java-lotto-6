3e192d6656d7ff92fd229f54245012ddab8e85a1
JongMinCh0i <wonderland314592@gmail.com>
2023-11-07 10:30:20 +0900

    refactor(LottoRank): 등수 당 배정된 상금 추가

--- src/main/java/lotto/constants/LottoRank.java
+++ src/main/java/lotto/constants/LottoRank.java
@@ -3,21 +3,23 @@ package lotto.constants;
 import java.util.function.BiPredicate;
 
 public enum LottoRank {
-    FIRST((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 6),
-    SECOND((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && bonusWinningNumber),
-    THIRD((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && !bonusWinningNumber),
-    FOURTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 4),
-    FIFTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 3),
-    NONE((matchedNumbers, bonusWinningNumber) -> matchedNumbers < 3);
+    FIRST("2,000,000,000원", (matchedNumbers, bonusWinningNumber) -> matchedNumbers == 6),
+    SECOND("30,000,000원", (matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && bonusWinningNumber),
+    THIRD("1,500,000원", (matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && !bonusWinningNumber),
+    FOURTH("50,000원", (matchedNumbers, bonusWinningNumber) -> matchedNumbers == 4),
+    FIFTH("5,000원", (matchedNumbers, bonusWinningNumber) -> matchedNumbers == 3),
+    NONE("", (matchedNumbers, bonusWinningNumber) -> matchedNumbers < 3);
 
     private final BiPredicate<Integer, Boolean> picked;
+    private final String prizeMoney;
 
-    LottoRank(BiPredicate<Integer, Boolean> picked) {
+    LottoRank(String prizeMoney, BiPredicate<Integer, Boolean> picked) {
+        this.prizeMoney = prizeMoney;
         this.picked = picked;
     }
 
     public static LottoRank evaluateLottoRank(int matchedNumbers, boolean hasBonus) {
-        LottoRank lottoRank = null;
+        LottoRank lottoRank = NONE;
         for (LottoRank p : values()) {
             if (p.picked.test(matchedNumbers, hasBonus)) {
                 lottoRank = p;

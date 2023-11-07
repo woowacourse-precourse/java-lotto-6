b813cad7d4e8d21a9d4e42a028a6a1f948d4ba9a
Minyongggg <naminyong97@gmail.com>
2023-11-08 01:23:28 +0900

    fix: 콘솔 메시지 수정

--- src/main/java/lotto/constant/ConsoleMessages.java
+++ src/main/java/lotto/constant/ConsoleMessages.java
@@ -6,8 +6,8 @@ public enum ConsoleMessages {
     WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
     WINNING_STATS_MESSAGE("당첨 통계\n---"),
-    WINNING_AWARDS("%d개 일치 (%s원) - %d개"),
-    WINNING_BONUS_AWARDS("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    WINNING_AWARD("%d개 일치 (%s원) - %d개"),
+    WINNING_BONUS_AWARD("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
     TOTAL_PROFIT("총 수익률은 %f%입니다.");
 
     private String value;

37aea225398189082b10c4c3db4cc0597e3e0301
song-jae-wan <sjo9810@gmail.com>
2023-11-07 14:12:40 +0900

    Test: 당첨번호 예외 재시작 테스트

--- src/test/java/view/ViewTest.java
+++ src/test/java/view/ViewTest.java
@@ -14,7 +14,7 @@ public class ViewTest extends NsTest {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @DisplayName("잘못된 금액 입력시 예외 발생후 재시작")
+    @DisplayName("잘못된 금액 입력시 예외 발생후 금액 입력부터 재시작")
     @Test
     void inputWrongPayAndRestart() {
         assertSimpleTest(
@@ -24,6 +24,21 @@ public class ViewTest extends NsTest {
                             DEPOSIT.getMessage(),
                             ERROR_MESSAGE,
                             DEPOSIT.getMessage()
+                    );
+                }
+        );
+    }
+
+    @DisplayName("당첨번호 개수 미달 입력시 예외 발생후 당첨번호 입력부터 재시작")
+    @Test
+    void inputWrongNumberCountAndRestart() {
+        assertSimpleTest(
+                () -> {
+                    run("1000", "1,2,3,4,5", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            WIN_NUMBER.getMessage(),
+                            ERROR_MESSAGE,
+                            WIN_NUMBER.getMessage()
                     );
                 }
         );

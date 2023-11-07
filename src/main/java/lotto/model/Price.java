02c79913ab3828bcf4ed7cdfa5d3bf41e8a65e61
Hyso <hsk7953@gmail.com>
2023-11-07 21:57:48 +0900

    feat: Price객체 생성

---
+++ src/main/java/lotto/model/Price.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+public class Price {
+    private int price;
+
+    public Price(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}

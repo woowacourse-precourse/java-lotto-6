d4abd100541e64ba19869d8f56f482656176a1d7
Zho <chlwlgh1011@naver.com>
2023-11-07 14:15:01 +0900

    feat: enum 적용 PrintOutput구현

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,4 +1,23 @@
 package lotto.view;
-
 public class View {
+
+    public enum PrintOutput {
+        START("구입금액을 입력해 주세요."),
+        WINNINGNUMBER("당첨 번호를 입력해 주세요."),
+        BONUSNUMBER("보너스 번호를 입력해 주세요."),
+        STATISTICS("당첨 통계"),
+        ;
+        private final String label;
+
+        PrintOutput(String label) {
+            this.label = label;
+        }
+
+        public String label() {
+            return label;
+        }
+    }
+    public static void printStartMessage () {
+        System.out.println(PrintOutput.START.label);
+    }
 }

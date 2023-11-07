adba3f909bec59c5a2abe3e92296ec35a0528bb6
morenow <jjang9877@gmail.com>
2023-11-08 04:54:30 +0900

    feat: 돈을 입력받아 저장하는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,10 @@
 package lotto.controller;
 
+import lotto.domain.Money;
+
 public class LottoController {
 
     public void playLotto() {
+        Money money = Money.createMoney();
     }
 }
---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class Money {
+    private static final int UNIT_PRICE = 1000;
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값을 입력했습니다.";
+    private int money;
+
+    Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    public static Money createMoney() {
+        try {
+            return inputMoney();
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(INPUT_ERROR_MESSAGE);
+            return createMoney();
+        }
+    }
+
+    private static Money inputMoney() {
+        OutputView.printMessage(MONEY_INPUT_MESSAGE);
+        String input = InputView.readMessage();
+        int money;
+        try {
+            money = Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
+        }
+        return new Money(money);
+    }
+
+    private void validate(int money) {
+        if (money % UNIT_PRICE != 0) {
+            throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
+        }
+    }
+
+}

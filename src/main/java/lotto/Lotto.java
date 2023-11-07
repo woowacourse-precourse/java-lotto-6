444b610efda83350050f28da9d758758ef9426e0
seohokim <shk9946@gmail.com>
2023-11-07 15:32:26 +0900

    [STYLE] LOTTO_LENGTH 상수값 대체

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,20 +3,17 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
-    private static final int LOTTO_PROCE = 1000;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int MIN_LOTTO_NUMBER = 1;
+    private final List<Integer> numbers;    
+    private static final int LOTTO_LENGTH = 6;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
 
-    
 }

dd632f342d9682d5ae7a47f3340d17ed54e253b4
geunsu <geunsu23@naver.com>
2023-11-07 09:24:11 +0900

    add RandomNumbers for getting random lotto numbers

---
+++ src/main/java/lotto/model/RandomNumbers.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.model.Constants.*;
+
+public class RandomNumbers {
+
+    private final List<List<Integer>> randomLottoNumbers = new ArrayList<>();
+
+    public RandomNumbers(int lottoPapers) {
+        for (int i = 0; i < lottoPapers; i++) {
+            randomLottoNumbers.add(setRandomNumbers());
+        }
+    }
+
+    private List<Integer> setRandomNumbers(){
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,MAX_LOTTO_NUMBER,LOTTO_LENGTH);
+    }
+
+    public List<Integer> getEachLottoNumbers(int index) {
+        return randomLottoNumbers.get(index);
+    }
+
+}

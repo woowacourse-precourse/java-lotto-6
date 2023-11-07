5757e122cbaa1005502e479c655ed1bdba637d94
kkw2758 <dtc3229@gmail.com>
2023-11-07 17:41:48 +0900

    Feat(LottoController) : 로또 시뮬레이션을 수행하는 Controller 생성

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,34 +1,87 @@
 package controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import constants.NumberType;
+import constants.Grade;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.AnswerLotto;
+import lotto.BonusNumber;
 import lotto.Lotto;
-import lotto.Lottos;
 import lotto.Money;
+import lotto.PointCalculator;
+import lotto.PointResult;
+import utils.Parser;
 import utils.RandomUtils;
 import view.InputView;
+import view.OutputView;
 
 public class LottoController {
 
+    private List<Lotto> lottos;
+    private AnswerLotto answerLotto;
+    private BonusNumber bonusNumber;
+    private Money money;
+    private final PointCalculator pointCalculator = new PointCalculator();
 
     public void run() {
-        int lottoCount = getLottoCount(generateMoney());
-        System.out.println(lottoCount + "개를 구매했습니다.");
-        for (int i = 0; i < lottoCount; i++) {
-            Lotto lotto = generateLotto();
-            System.out.println(lotto.toString());
+        OutputView.printMoneyInputMessage();
+        money = generateMoney();
+        int lottoPurchaseCount = money.getLottoPurchaseCount();
+        OutputView.printLottoPurchaseCount(lottoPurchaseCount);
+        lottos = generateLottoWithCount(lottoPurchaseCount);
+
+        //당첨 번호 입력
+        OutputView.printAnswerLottoNumberInputMessage();
+        answerLotto = new AnswerLotto(Parser.stringToIntegerList(InputView.inputAnswerLotto()));
+        //보너스 번호 입력
+        OutputView.printBonusNumberInputMessage();
+        bonusNumber = new BonusNumber(Integer.parseInt(InputView.inputNaturalNumber()), answerLotto);
+
+        // 당첨 통계
+        OutputView.printLottoResultMessage();
+        // --- 출력
+        OutputView.printHorizontalRule();
+
+        PointResult pointResult = new PointResult(getLottoResult(lottos));
+
+        for (Grade grade : Grade.values()) {
+            OutputView.printResult(grade, pointResult.getCountByGrade(grade));
         }
+
+        OutputView.printEarningRate(pointResult.calculateEarningRate(money));
+    }
+
+
+    private List<Double> getLottoResult(List<Lotto> lottos) {
+        List<Double> lottoResult = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            lottoResult.add(pointCalculator.calculateTotalPoint(lotto, answerLotto, bonusNumber));
+        }
+        return lottoResult;
+    }
+
+    private List<Lotto> generateLottoWithCount(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(generateLotto());
+        }
+        return lottos;
     }
 
     private Lotto generateLotto() {
-        return new Lotto(RandomUtils.generateRandomLottoNumber());
+        List<Integer> randomLottoNumbers = RandomUtils.generateRandomLottoNumber().stream()
+                .sorted()
+                .collect(Collectors.toList());
+        Lotto lotto = new Lotto(randomLottoNumbers);
+        System.out.println(lotto.getGeneratedLottoString());
+        return lotto;
     }
 
     private Money generateMoney() {
-        return new Money(Integer.parseInt(InputView.readLine()));
+        return new Money(Integer.parseInt(InputView.inputNaturalNumber()));
     }
 
-    private int getLottoCount(Money money) {
-        return money.getLottoCount();
+    private int getLottoPurchaseCount() {
+        return generateMoney().getLottoPurchaseCount();
     }
 }

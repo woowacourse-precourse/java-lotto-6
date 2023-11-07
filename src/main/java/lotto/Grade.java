8f9feb649f61f6f4ceedcebe631968a2aa479a0c
Identityless <rlandwnd555@naver.com>
2023-11-07 18:20:52 +0900

    feat : 복권 등수 및 당첨 액수 관리 Enum 추가

---
+++ src/main/java/lotto/Grade.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum Grade {
+
+    FIRST_GRADE(1, 2000000000),
+    SECCOND_GRADE(2, 30000000),
+    THIRD_GRADE(3, 1500000),
+    FORTH_GRADE(4, 50000),
+    FIFTH_GRADE(5, 5000);
+
+    int grade, price;
+
+    Grade(int grade, int price) {
+        this.grade = grade;
+        this.price = price;
+    };
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}

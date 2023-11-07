5ff14a9602d5b6a195c4629ba87ce096c8ebbb2c
sung-yeop <jjabc9459@naver.com>
2023-11-07 17:03:10 +0900

    refactor(Rank): Rank 기능 추가
    - 맞춘 당첨 번호 개수와 보너스 번호 당첨 여부를 통해 Rank를 반환하는 getRank() 메서드 구현

--- src/main/java/lotto/controller/Rank.java
+++ src/main/java/lotto/controller/Rank.java
@@ -1,26 +1,31 @@
 package lotto.controller;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
-public enum Compare {
+public enum Rank {
     ONE(6, "6개 일치", 2000000000),
     TWO(5, "5개 일치, 보너스 볼 일치", 30000000),
-    THREE(4, "5개 일치", 1500000),
-    FOUR(3, "4개 일치", 50000),
-    FIVE(2, "3개 일치", 5000);
+    THREE(5, "5개 일치", 1500000),
+    FOUR(4, "4개 일치", 50000),
+    FIVE(3, "3개 일치", 5000),
+    NONE(-1, "당첨 X", 0);
 
     private int shot;
     private String reply;
     private int money;
 
-    Compare(int shot, String reply, int money) {
+    Rank(int shot, String reply, int money) {
         this.shot = shot;
         this.reply = reply;
         this.money = money;
     }
 
-    public static Compare getRank(int shot, boolean bonusShot) {
-        if()
+    public static Rank getRank(int shot, boolean bonusShot) {
+        if (shot == 5 && bonusShot) {
+            return Rank.TWO;
+        }
+
+        return Arrays.stream(Rank.values()).filter(Rank -> Rank.shot == shot)
+                .findAny().orElse(Rank.NONE);
     }
 }

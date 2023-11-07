2f2afd5dd48a742b72772f6415e54927804b41b9
Knight <kangrack333@gmail.com>
2023-11-07 21:21:22 +0900

    refactor(ChangeMoneyToLotto) : 비용 > 로또 변경 메서드 수정

--- src/main/java/lotto/domain/ChangeMoneyToLotto.java
+++ src/main/java/lotto/domain/ChangeMoneyToLotto.java
@@ -3,9 +3,10 @@ package lotto.domain;
 public class ChangeMoneyToLotto {
 
     private final static int DIVIDE_BY_1000_UNITS = 1000;
+    public static int purchaseMoney;
 
     public int changeMoneyToLotto(String purchaseAmount) {
-
-        return Integer.parseInt(purchaseAmount) / DIVIDE_BY_1000_UNITS;
+        purchaseMoney = Integer.parseInt(purchaseAmount);
+        return purchaseMoney / DIVIDE_BY_1000_UNITS;
     }
 }

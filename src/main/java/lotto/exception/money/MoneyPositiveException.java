3e220680bc4c8c4187b4c818fc437b61fcb212c8
uwoobeat <desktop3711@gmail.com>
2023-11-08 06:51:05 +0900

    refactor: 에러 메세지 캡슐화

--- src/main/java/lotto/exception/money/MoneyPositiveException.java
+++ src/main/java/lotto/exception/money/MoneyPositiveException.java
@@ -3,7 +3,7 @@ package lotto.exception.money;
 import lotto.exception.common.CustomArgumentException;
 
 public class MoneyPositiveException extends CustomArgumentException {
-    public static final String MESSAGE = "화폐는 0원 이상의 금액을 가져야 합니다.";
+    private static final String MESSAGE = "화폐는 0원 이상의 금액을 가져야 합니다.";
 
     public MoneyPositiveException() {
         super(MESSAGE);

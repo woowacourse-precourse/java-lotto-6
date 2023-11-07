78f144b8a450b8cdb6ecf6c5268a87aab1b816e5
EveKristinLee <jylisa0612@naver.com>
2023-11-07 19:13:15 +0900

    feat : 로또 금액 입력받기 - 1000으로 나뉘는지 확인

--- src/main/java/lotto/Constant/Constant.java
+++ src/main/java/lotto/Constant/Constant.java
@@ -2,10 +2,13 @@ package lotto.Constant;
 
 public class Constant {
 
-    public static final String ERROR_PREFIX = "[ERROR] ";
-    public static final String ERROR_NOT_NUMBER_MESSAGE = "구입금액은 숫자여야 합니다.";
-
     public static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
 
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_NOT_NUMBER_MESSAGE = "구입금액은 숫자여야 합니다.";
+    public static final String ERROR_NOT_DIVIDE_MESSAGE = "구입금액은 1,000원으로 나누어 떨어져야 합니다.";
+
     public static final String NUMBER_PATTERN = "[0-9]+";
+
+    public static final int LOTTO_DIVIDE_PRICE = 1000;
 }
--- src/main/java/lotto/Domain/Money.java
+++ src/main/java/lotto/Domain/Money.java
@@ -13,12 +13,20 @@ public class Money {
 
     private void validate(String value) {
         isNumber(value);
+        isDivide(value);
     }
 
     //숫자값인지 확인
     private void isNumber(String value) {
         if (!value.matches(Constant.NUMBER_PATTERN)) {
             throw new IllegalArgumentException(Constant.ERROR_PREFIX + Constant.ERROR_NOT_NUMBER_MESSAGE);
+        }
+    }
+
+    //1,000으로 나누어지는지 확인
+    private void isDivide(String value) {
+        if (Integer.parseInt(value) % Constant.LOTTO_DIVIDE_PRICE != 0) {
+            throw new IllegalArgumentException(Constant.ERROR_PREFIX + Constant.ERROR_NOT_DIVIDE_MESSAGE);
         }
     }
 

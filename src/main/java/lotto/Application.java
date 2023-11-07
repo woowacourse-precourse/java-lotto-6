1dadc5f76ef3be202eb2243478791cc2d597f1a8
Subi Hwang <ghkdtnql@gmail.com>
2023-11-08 00:19:23 +0900

    feat: 당첨 통계 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,7 +10,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
 
 	static List<Lotto> lottos = new ArrayList<>();
-	static int bonus;
+	static List<Integer> newLottos = new ArrayList<>();
+
+	static List<Integer> countResult = new ArrayList<>();
+	static int bonus, count;
 	static private int seedMoney;
 
 	public static void main(String[] args) {
@@ -19,6 +22,7 @@ public class Application {
 		printStatus();
 		pickLottoNumber();
 		pickBonusNumber();
+		printResult();
 	}
 
 	private static void inputSeedMoney() {
@@ -42,7 +46,6 @@ public class Application {
 	}
 
 	public static void pickLottoNumber() {
-		List<Integer> newLottos = new ArrayList<>();
 		System.out.println("담청 번호를 입력해 주세요.");
 		String input = readLine().trim();
 		for (String lottoNumber : input.split(",")) {
@@ -55,5 +58,63 @@ public class Application {
 		bonus = Integer.parseInt(readLine());
 	}
 
+	public static void printResult() {
+		getWinningNumbers();
+		getCountNumber();
+		System.out.println("당첨 통계");
+		System.out.println("---");
+		System.out.println("3개 일치 (5,000원) - " + countResult.get(0));
+		System.out.println("4개 일치 (50,000원) - " + countResult.get(1));
+		System.out.println("5개 일치 (1,500,000원) - " + countResult.get(2));
+		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countResult.get(3));
+		System.out.println("6개 일치 (2,000,000,000원) - " + countResult.get(4));
+		int finalResult = 0;
+		for (int i = 0; i < countResult.size(); i++) {
+			int profit = 0;
+			profit += countResult.get(i);
+			finalResult = (profit / seedMoney) * 100;
+		}
+		System.out.println("총 수익률은 " + finalResult + "입니다.");
+	}
+
+	public static void getWinningNumbers() {
+		for (int i = 0; i < lottos.size(); i++) {
+			count = 0;
+			for (int j = 0; j < newLottos.size(); j++) {
+				if (lottos.contains(newLottos.get(j))) {
+					count++;
+				}
+			}
+		}
+	}
+
+	public static void getCountNumber() {
+		if (count == 3) {
+			int result = 0;
+			result++;
+			countResult.add(result);
+		}
+		if (count == 4) {
+			int result = 0;
+			result++;
+			countResult.add(result);
+		}
+		if (count == 5) {
+			int result = 0;
+			result++;
+			if (lottos.contains(bonus)) {
+				countResult.add(result);
+			}
+			if (!lottos.contains(bonus)) {
+				countResult.add(result);
+			}
+		}
+		if (count == 6) {
+			int result = 0;
+			result++;
+			countResult.add(result);
+		}
+
+	}
 
 }

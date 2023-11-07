040d85f1165bb4435a3c5fa006f9225af0cc6dec
siwol025 <cyk99115695@gmail.com>
2023-11-07 15:43:34 +0900

    style(OutputView): 로또 추첨결과에 2등 결과 메시지 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,7 +11,13 @@ public class OutputView {
     private static final String WINNIG_STATISTICS_MESSAGE = "당첨 통계";
     private static final String BOUNDARY = "---";
     private static final String WINNIG_DETAILS_MESSAGE = "%d개 일치 (%s원) - %d개";
+    private static final String SECOND_PLACE_WINNING_DETAIL_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
     private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    public void showIssuedLottoResult(List<String> issuedLottos) {
+        showLottoAmount(issuedLottos.size());
+        showLottoNumbers(issuedLottos);
+    }
 
     public void showLottoAmount(int lottoAmount) {
         System.out.println(String.format(LOTTO_AMOUNT_MESSAGE, lottoAmount));
@@ -34,7 +40,6 @@ public class OutputView {
                 .forEach(System.out::println);
     }
 
-
     public void showProfitRate(double profitRate) {
         System.out.println(String.format(PROFIT_RATE_MESSAGE, profitRate));
     }
@@ -42,8 +47,17 @@ public class OutputView {
     private String formatString(Prize prize, Prizes prizes) {
         DecimalFormat formatter = new DecimalFormat("###,###");
         String formattedPrizeMoney = formatter.format(prize.getPrizeMoney());
+        String winningDetailMessage = selectWinningMessage(prize);
 
-        return String.format(WINNIG_DETAILS_MESSAGE, prize.getMatchingNumberCount(), formattedPrizeMoney,
+        return String.format(winningDetailMessage, prize.getMatchingNumberCount(), formattedPrizeMoney,
                 prizes.getPrizeAmount(prize));
+    }
+
+    private String selectWinningMessage(Prize prize) {
+        if (prize == Prize.SECOND_PLACE) {
+            return SECOND_PLACE_WINNING_DETAIL_MESSAGE;
+        }
+
+        return WINNIG_DETAILS_MESSAGE;
     }
 }

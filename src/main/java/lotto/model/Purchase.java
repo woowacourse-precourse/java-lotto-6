2fc59e4a4a14d47dd516380ba8befeae38e083da
JW0321 <digneotn123@gmail.com>
2023-11-08 00:46:05 +0900

    refactor: 생성자, 검증 부분 변경
    생성자를 기본 생성자로 바꾸고 setter 메소드의 매개변수를 정수형으로 변경하여 controller에서 형변환 책임을 통일성 있게 가지도록 함
    검증 부분을 세분화 하여 각 예외 유형 별로 메세지 추가

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -1,20 +1,23 @@
 package lotto.model;
 
+import static lotto.util.ErrorMessage.*;
 public class Purchase { //도메인 로직과 관련된 구입 금액 검증 및 저장, 구입 금액에 따른 구매 횟수 계산
 
-    private final int purchaseAmount;
-    private final int purchaseNumber;
+    private int purchaseAmount;
+    private int purchaseNumber;
 
-    public Purchase(String purchaseAmount) {
-        int purchaseMoney = Integer.parseInt(purchaseAmount);
+    public void setPurchase(int purchaseMoney) {
         validatePurchaseAmount(purchaseMoney);
         this.purchaseAmount = purchaseMoney;
         this.purchaseNumber = purchaseMoney / 1000;
     }
 
     private void validatePurchaseAmount(int purchaseMoney) {
-        if (purchaseMoney % 1000 != 0 || purchaseMoney == 0) {
-            throw new IllegalArgumentException();
+        if (purchaseMoney % 1000 != 0 ) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_PURCHASE_AMOUNT_CANT_DIVIDE_BY_THOUSAND);
+        }
+        if (purchaseMoney == 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_PURCHASE_AMOUNT_CANT_BE_ZERO);
         }
     }
 

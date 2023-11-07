9434313edc5f38cf0244c8b8cfb840bf1770d4f1
Hyso <hsk7953@gmail.com>
2023-11-07 19:53:11 +0900

    feat: 숫자를 체크, 1000원 단위를 확인하고 에러메시지를 던지는 메소드

--- src/main/java/lotto/util/InputException.java
+++ src/main/java/lotto/util/InputException.java
@@ -1,2 +1,21 @@
-package lotto.util;public class InputException {
+package lotto.util;
+
+public class InputException {
+    public static void validPrice(int money) {
+
+    }
+
+    private static void checkChar(String input) {
+        try{
+            Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] : 옳바른 숫자를 입력해주세요");
+        }
+    }
+
+    private static void checkMultipleOfThousand (int number){
+        if(number % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] : 구입 가격은 1000원 단위입니다.");
+        }
+    }
 }

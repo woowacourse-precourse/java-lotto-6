8b373caa23c3958b8aa66268656466561dc9cb26
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-07 19:00:41 +0900

    [fix : InputView] 당첨번호, 보너스 번호 문구 앞에 \n 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,20 +5,20 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     //로또 구입 금액 입력
-    public static String purchaseAmount(){
+    public static String purchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
     //당첨 번호 6자리 입력
-    public static String winningNumber(){
-        System.out.println("당첨 번호를 입력해 주세요.");
+    public static String winningNumber() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     //보너스 번호 1자리 입력
-    public static String bonusNumber(){
-        System.out.println("보너스 번호를 입력해 주세요.");
+    public static String bonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }

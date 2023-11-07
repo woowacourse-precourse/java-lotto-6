be960f817adbac4f81090d5730f451e052083d02
isyoo1213 <moethns@gmail.com>
2023-11-07 22:23:09 +0900

    feat: OutputView 로또 금액별 당첨내역 출력기능 추가 OutputView - printLottoWinningResult() 추가 README.md - 기능 명세 구현 체크

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,6 +12,14 @@ public class OutputView {
         }
     }
 
+    public void printLottoWinningResult(List<String> eachWinningResult) {
+        System.out.println("3개 일치 (5,000원) - " + eachWinningResult.get(0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + eachWinningResult.get(1) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + eachWinningResult.get(2) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + eachWinningResult.get(4) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + eachWinningResult.get(3) + "개");
+    }
+
     private void printNumbers(List<Integer> numbers) {
         System.out.print("[");
         for (int i = 0; i < numbers.size(); i++) {

5a11aca8e659cf5ad0f78c595bf3658c4a0c9676
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-07 22:06:19 +0900

    fix: 오타 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,16 +4,19 @@ import lotto.domain.Rank;
 import lotto.domain.Result;
 import lotto.domain.User;
 import lotto.domain.Lotto;
+
+import java.text.DecimalFormat;
+
 public class OutputView {
-    public static final String AMOUNT_OF_LOTTO = "개를 구매하였습니다.";
-    public static final String WINNING_STATISTICS = "당첨 통계";
+    public static final String AMOUNT_OF_LOTTO = "개를 구매했습니다.";
+    public static final String WINNING_STATISTICS = "\n당첨 통계";
     public static final String DIVIDER_LINE = "---";
     public static final String PROFIT_ANNOUNCEMENT = "총 수익률은 ";
     public static final String PROFIT_ANNOUNCEMENT_CLOSING = "%입니다.";
 
 
     public static void printUserLotto(User user) {
-        System.out.println(user.getAmount()+AMOUNT_OF_LOTTO);
+        System.out.println("\n"+user.getAmount()+AMOUNT_OF_LOTTO);
         for (Lotto lotto: user.getLottos()) {
             System.out.println(lotto.toString());
         }
@@ -22,6 +25,9 @@ public class OutputView {
         System.out.println(WINNING_STATISTICS);
         System.out.println(DIVIDER_LINE);
         for(Rank currentrank : Rank.values()) {
+            if (currentrank == Rank.NONE) {
+                continue;
+            }
             int count = result.getResults().stream()
                     .filter(rank -> rank == currentrank)
                     .mapToInt(rank -> 1)
@@ -29,11 +35,15 @@ public class OutputView {
             int awardMoney = currentrank.getAwardMoney();
             String message = currentrank.getMessage();
 
-            System.out.println(count + "개 일치 (" + awardMoney + "원) - " + message);
+            System.out.println(message + " ("+formatNumber(awardMoney)+"원) - "+ count+"개");
         }
 
     }
-    public static void printProfitRate(long value) {
+    private static String formatNumber(int number) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(number);
+    }
+    public static void printProfitRate(double value) {
         System.out.println(PROFIT_ANNOUNCEMENT+Math.round(value*100.0)/100.0+PROFIT_ANNOUNCEMENT_CLOSING);
     }
 }

87a868008cacdcafbfd04b4c2c5d85c22a1eaae4
정재웅 <jweidon0821@gmail.com>
2023-11-07 16:40:06 +0900

    feat(view): 보너스 번호 입력 함수 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static String askMoneyMessage = "구입금액을 입력해 주세요.";
     public static String askWinningNumbersMessage = "당첨 번호를 입력해 주세요.";
+    public static String askBonusNumberMessage = "보너스 번호를 입력해 주세요.";
 
     public static String inputMoney() {
         System.out.println(askMoneyMessage);
@@ -13,6 +14,11 @@ public class InputView {
 
     public static String inputWinningNumbers() {
         System.out.println(askWinningNumbersMessage);
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(askBonusNumberMessage);
         return Console.readLine();
     }
 }

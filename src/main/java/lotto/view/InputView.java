76a1abf9d7024c33c64b81b14881604b9c6afbbb
kg9585 <kg9585@naver.com>
2023-11-08 04:35:01 +0900

    refactor(InputView): 콘솔 입력 리팩토링

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,25 +1,42 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
-
     private InputView() {
     }
 
     public static String inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
-    public static String inputWinningNumbers() {
+    public static List<Integer> inputprizeNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return scanner.nextLine();
+        String input = Console.readLine();
+        return parseNumbers(input);
     }
 
-    public static String inputBonusBall() {
+    public static int inputBonusBall() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        return scanner.nextLine();
+        String input = Console.readLine();
+        return parseSingleNumber(input);
     }
+
+    private static List<Integer> parseNumbers(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static int parseSingleNumber(String input) {
+        return Integer.parseInt(input.trim());
+    }
+
+
 }

fde44f954d7181da9aef6a96e2cdb2b48bc015ff
yubin <dbqls2821@gmail.com>
2023-11-07 13:44:02 +0900

    test: 로또 결과를 계산하는 테스트코드 추가

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottosTest {
+
+    @Test
+    void 로또_결과를_계산한다() {
+        Lottos lottos = new Lottos();
+        lottos.addLotto(List.of(1, 2, 3, 4, 5, 6));
+        lottos.addLotto(List.of(1, 2, 3, 4, 5, 7));
+        lottos.addLotto(List.of(7, 8, 9, 10, 11, 12));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7, winningNumber);
+
+        List<Result> results = lottos.calculateResult(winningNumber, bonusNumber);
+
+        assertEquals(List.of(Result.SIX_MATCH, Result.FIVE_MATCH_WITH_BONUS), results);
+    }
+}

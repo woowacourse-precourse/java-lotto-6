8acec12337da3efe5d5181259a64d779f3836ba1
hoyeon <highoption@naver.com>
2023-11-07 22:24:29 +0900

    style(Money) : 가독성을 위해 상수 추가

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,6 +1,11 @@
 package lotto.model;
 
 public class Money {
+    private final String INTEGER_RANGE = "^[0-9]+$";
+    private final String ERROR_NOT_INTEGER = "[ERROR] 구입 금액은 숫자여야 합니다.";
+    private final int ONE_LOTTO_PRICE = 1000;
+    private final int ZERO = 0;
+    private final String ERROR_NOT_PAPER_MONEY = "[ERROR] 구입 금액은 1,000원 단위로 이루어져야 합니다.";
     private final String money;
 
     public Money(String money) {
@@ -12,21 +17,21 @@ public class Money {
         return money;
     }
 
-    private static void validateMoney(String money) {
+    private void validateMoney(String money) {
         isInteger(money);
         isPaperMoney(money);
     }
 
-    private static void isInteger(String money) {
-        if (!money.matches("^[0-9]+$")) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+    private void isInteger(String money) {
+        if (!money.matches(INTEGER_RANGE)) {
+            throw new IllegalArgumentException(ERROR_NOT_INTEGER);
         }
     }
 
-    private static void isPaperMoney(String money) {
+    private void isPaperMoney(String money) {
         int realMoney = Integer.parseInt(money);
-        if (realMoney % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 이루어져야 합니다.");
+        if (realMoney % ONE_LOTTO_PRICE != ZERO) {
+            throw new IllegalArgumentException(ERROR_NOT_PAPER_MONEY);
         }
     }
 }

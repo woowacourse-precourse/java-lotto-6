a4a95f9777e1df67f483d9793dacaa7e8df52ec3
Gyeonghun Min <riverpower6@naver.com>
2023-11-08 05:29:42 +0900

    Feat: 당첨 로또 도메인 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,10 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
+import lotto.exception.InputLottoNumException;
 
 public class Lotto {
+    private static int bonusNum;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +13,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        InputLottoNumException inputLottoNumException = new InputLottoNumException();
+        inputLottoNumException.validateLotto(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    public void setBonusNum(int bonusNum) {
+        this.bonusNum = bonusNum;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonusNum() {
+        return bonusNum;
+    }
 }

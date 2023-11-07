621dcd87900092e55d946c887cccfbcdaeabfd14
yubin <dbqls2821@gmail.com>
2023-11-07 13:24:14 +0900

    test: 로또 생성시 로또 사이즈가 0일 경우 예외를 반환하는 테스트코드 추가

--- src/test/java/lotto/domain/LottoFactoryTest.java
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoFactoryTest {
@@ -16,5 +17,15 @@ class LottoFactoryTest {
         Lottos createdLottos = factory.createLottos(size);
 
         assertEquals(size, createdLottos.getLottos().size());
+    }
+
+    @Test
+    void 갯수로_0이_들어오면_예외를_반환한다() {
+        LottoFactory factory = new LottoFactory(() -> List.of(1, 2, 3, 4, 5, 6));
+        int size = 0;
+
+        assertThatThrownBy(() -> factory.createLottos(size))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DomainException.ERROR.getMessage());
     }
 }

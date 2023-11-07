93d4a185175c8d7d0e13b1d8874845a3ba6a7443
slowcloud <sju9417@gmail.com>
2023-11-07 21:24:56 +0900

    style: Application 개행 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,7 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -16,4 +17,5 @@ public class Application {
         lottoController.play();
 
     }
+
 }

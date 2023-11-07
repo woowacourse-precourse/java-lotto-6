02982097ec6594b6cf84ae1abdeef6ce4edcaf92
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-08 06:22:59 +0900

    feat: 구입, Prize로의 변환에서 사용할 함수 구현

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,10 +1,20 @@
 package lotto.util;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lotto.model.number.LottoNumber;
-import lotto.model.number.LottoNumbers;
 
 public class Validator {
+    public static void validatePurchaseAmount(int amount) {
+        String regex = "^[1-9]\\d*000$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(String.valueOf(amount));
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 0이상의 정수를 입력해주세요.");
+        }
+    }
+
     /**
      * Description: Validate LottoNumbers<br> rule1: LottoNumbers must have 6 numbers<br> rule2: LottoNumbers must have
      * unique numbers<br>
@@ -15,11 +25,12 @@ public class Validator {
      */
     public static void validateLottoNumbers(List<LottoNumber> numbers) {
         if (isLotteryNumberSizeValid(numbers)) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         if (isLottoNumberDuplicate(numbers)) {
-            throw new IllegalArgumentException("로또 번호는 중복되지 않아야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
+        numbers.forEach(number -> validateLottoNumber(number.getNumber()));
     }
 
     /**
@@ -31,7 +42,19 @@ public class Validator {
      */
     public static void validateLottoNumber(int number) {
         if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    /**
+     * Description: Validate LottoNumbers<br> rule1: matchCount must be between 3 and 6<br>
+     *
+     * @param matchCount
+     * @throws IllegalArgumentException
+     */
+    public static void validateMatchCount(int matchCount) {
+        if (matchCount < 3 || matchCount > 6) {
+            throw new IllegalArgumentException("[ERROR] 3 미만, 6 초과의 숫자는 Prize가 될 수 없습니다.");
         }
     }
 

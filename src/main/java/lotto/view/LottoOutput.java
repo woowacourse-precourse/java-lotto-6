7ff3a806e32a220067af6f8574a5e2e42accb01a
mksystem98 <mksystem98@gmail.com>
2023-11-08 04:13:56 +0900

    feat: 수익률을 출력하는 기능 추가
    LottoOutput.printEarningsRate 메서드는 둘째 자리에서 반올림한 수익률을 0.0%포맷에 맞추어 출력하는 기능

--- src/main/java/lotto/view/LottoOutput.java
+++ src/main/java/lotto/view/LottoOutput.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -50,6 +51,14 @@ public class LottoOutput {
                 System.out.printf("%s - %d개\n", prizeDescription, count);
             }
         }
+
+
+    }
+
+    public void printEarningsRate(double earningsRate) {
+        DecimalFormat df = new DecimalFormat("0.0%");
+        String formattedEarningsRate = df.format(earningsRate / 100);
+        System.out.println("총 수익률은 " + formattedEarningsRate + "입니다.");
     }
 
 }

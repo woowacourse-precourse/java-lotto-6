86daa9f1f449053968d908b157a8febf53b76c4d
enble <enble_777@naver.com>
2023-11-07 23:38:32 +0900

    refactor: LottoController 메서드 분리
    run 메서드를 분리하였습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,23 +9,36 @@ import lotto.view.View;
 
 public class LottoController {
 
-    public void run() {
-        LottoService lottoService = new LottoService();
+    private final LottoService lottoService = new LottoService();
 
+    private int purchasePrice = 0;
+
+    public void run() {
+        initLotto();
+        initWinningLotto();
+        projectStatistics();
+    }
+
+    private void initLotto() {
         // 구입 금액 입력
-        int purchasePrice = validateInputUntilSuccess(View::requestPurchasePrice, Parser::parsePurchasePrice);
+        purchasePrice = validateInputUntilSuccess(View::requestPurchasePrice, Parser::parsePurchasePrice);
         // 로또 번호 생성
         lottoService.setLottos(purchasePrice);
         // 로또 번호 출력
         View.responseLottoNumbers(lottoService.getLottos(), lottoService.getLottoCount());
+    }
 
+    private void initWinningLotto() {
         // 당첨 번호 입력
         Lotto winningLotto = validateInputUntilSuccess(View::requestWinningNumber, Parser::parseLottoNumbers);
         // 보너스 번호 입력
         int bonusNumber = validateInputUntilSuccess(View::requestBonusNumber, Parser::parseBonusNumber);
-
-        // 당첨 통계 출력
+        // 당첨 번호, 보너스 번호 설정
         lottoService.setWinningLotto(winningLotto, bonusNumber);
+    }
+
+    private void projectStatistics() {
+        // 당첨 통계 출력
         WinningStatisticsService winningStatisticsService = new WinningStatisticsService(lottoService);
         winningStatisticsService.setWinningStatistics();
         View.responseWinningStatistics(winningStatisticsService.getWinningStatistics());

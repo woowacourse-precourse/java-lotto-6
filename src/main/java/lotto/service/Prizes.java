7e267f8d4a6e0486a6778c25f3067b99170dd072
KimHongHee <qaz101044@naver.com>
2023-11-08 00:48:03 +0900

    feature : add a features related to print a result of lotto.

--- src/main/java/lotto/service/Prizes.java
+++ src/main/java/lotto/service/Prizes.java
@@ -1,5 +1,29 @@
 package lotto.service;
 
+import java.util.Arrays;
 
 public class Prizes {
+
+    FIRST(6,2_000_000_000,"6개 일치 (2,000,000,000원) - "),
+
+    SECOND(5,30_000_000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+
+    THIRD(5,1_500_000,"5개 일치 (1,500,000원) - "),
+
+    FOURTH(4,50_000,"4개 일치 (50,000원) - "),
+
+    FIFTH(3,5_000,"3개 일치 (5,000원) - "),
+
+    MISS(0,0,"");
+
+    private int count_match_numbers;
+    private int count_prize_numbers;
+    private String prize_message;
+    Prizes(int count_match_numbers, int count_prize_numbers, String prize_message) {
+        this.count_match_numbers = count_match_numbers;
+        this.count_prize_numbers = count_prize_numbers;
+        this.prize_message = prize_message;
+    }
+
+
 }
--- src/main/java/lotto/view/Output_View.java
+++ src/main/java/lotto/view/Output_View.java
@@ -1,4 +1,20 @@
 package lotto.view;
 
 public class Output_View {
+    public static void print_ticket_counts(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void print_winning_stat_message(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public void print_winning_stat_prize(String prize_message,int prize_amount){
+        System.out.println(prize_message + prize_amount + "개" );
+    }
+
+    public static void print_earning_rate(double earning_rate){
+        System.out.println("총 수익률은 " + String.format("%,.1f",earning_rate) + "%입니다.");
+    }
 }

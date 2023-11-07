588b70b6c7f4bc97246d388a5710c1d5cd722689
jongheonleee <qwefghnm1212@gmail.com>
2023-11-07 21:25:03 +0900

    [test] 입력값 처리 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -22,7 +22,7 @@ class InputViewTest extends IOTest{
     private InputView inputView = new InputView();
 
 
-    @DisplayName("현금 생성 성공 테스트")
+    @DisplayName("현금 입력값 정상적으로 받아오는 경우")
     @ParameterizedTest()
     @ValueSource(strings = {"1000", "2000", "3000", "4000", "5000"})
     void createCashByInputSuccessTest(String input) {
@@ -58,7 +58,18 @@ class InputViewTest extends IOTest{
         );
     }
 
-    @DisplayName("입력한 당첨 번호에 숫자가 아닌 값이 있는 경우")
+    @DisplayName("당첨 번호를 정상적으로 받아오는 경우")
+    @ParameterizedTest()
+    @ValueSource(strings = {"1,2,3,4,5,6", "1,2,3,4,5,9", "1,2,3,4,5,11", "1,2,3,4,5,12",  "1,2,3,4,5,42"})
+    void createWinnerNumbersSuccessTest(String input) {
+        assertDoesNotThrow(() -> {
+            systemIn(input);
+            inputView.requestWinnerNumbers();
+        });
+    }
+
+
+    @DisplayName("입력한 당첨 번호에 숫자가 아닌 값이 있는 경우 예외 발생")
     @ParameterizedTest()
     @ValueSource(strings = {"1,2,3,4,5,z", "1,2,3,4,5,% ", "1,2,3,4,5,ㅎ ", "1,2,3,4,5,!",  "1,2,3,4,5,."})
     void createWinnerNumbersWithNotNumberExceptionTest(String input) {
@@ -70,7 +81,7 @@ class InputViewTest extends IOTest{
         );
     }
 
-    @DisplayName("당첨 번호 입력 형식을 준수하지 않은 경우")
+    @DisplayName("당첨 번호 입력 형식을 준수하지 않은 경우 예외 발생")
     @ParameterizedTest()
     @ValueSource(strings = {"1, 2, 3, 4, 5, 6", "1/2/3/4/5/6 ", "1-2-3-4-5-6", "1,2,3,4,5,6,", ",1,2,3,4,5,6"})
     void createWinnerNumbersWithWrongNumbersSizeExceptionTest(String input) {
@@ -84,7 +95,54 @@ class InputViewTest extends IOTest{
 
 
 
+    @DisplayName("당첨 번호 입력시 공백을 입력한 경우 예외 발생")
+    @ParameterizedTest()
+    @ValueSource(strings = {" "})
+    void createWinnerNumbersWithBlankExceptionTest(String input) {
+        assertThrows(IllegalStateException.class,
+                () -> {
+                    systemIn(input);
+                    inputView.requestWinnerNumbers();
+                }
+        );
+    }
 
+
+    @DisplayName("보너스 번호를 정상적으로 받아오는 경우")
+    @ParameterizedTest()
+    @ValueSource(strings = {"1", "12", "22", "38", "39"})
+    void createBonusNumberSuccessTest(String input) {
+        assertDoesNotThrow(() -> {
+            systemIn(input);
+            inputView.requestBonusNumber();
+        });
+    }
+
+
+
+    @DisplayName("입력한 보너스 번호에 숫자가 아닌 값이 있는 경우 예외 발생")
+    @ParameterizedTest()
+    @ValueSource(strings = {"1ㅋ", "12#", "..", "hello", "3 "})
+    void createBonusNumberWithNotNumberExceptionTest(String input) {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    systemIn(input);
+                    inputView.requestBonusNumber();
+                }
+        );
+    }
+
+    @DisplayName("보너스 번호를 입력시 공백을 입력한 경우 예외 발생")
+    @ParameterizedTest()
+    @ValueSource(strings = {" "})
+    void createBonusNumberWithBlankExceptionTest(String input) {
+        assertThrows(IllegalStateException.class,
+                () -> {
+                    systemIn(input);
+                    inputView.requestBonusNumber();
+                }
+        );
+    }
 
 
 

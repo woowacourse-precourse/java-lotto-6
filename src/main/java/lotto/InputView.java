541008ffed1d79e1a4d039b3723a2605cd428671
tmdduq500 <osy9907@gmail.com>
2023-11-07 11:19:17 +0900

    fix: 구입금액 입력기능, 당첨번호 입력기능, 보너스번호 입력기능 메소드명 수정

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -6,17 +6,17 @@ public class InputView {
     static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public void InputMoney() {
+    public void inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
         Console.readLine();
     }
 
-    public void InputWinningNumber() {
+    public void inputWinningNumber() {
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         Console.readLine();
     }
 
-    public void InputBonusNumber() {
+    public void inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         Console.readLine();
     }

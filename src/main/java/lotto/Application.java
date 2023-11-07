0bef57b81dd40fa3a2b53cee14bcfc81cc2cb405
seohokim <shk9946@gmail.com>
2023-11-07 20:49:27 +0900

    [FEAT] main 프로그램 실행  구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,13 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 로또 상점 생성
+        LottoShop lottoShop = new LottoShop();
+       
+        LottoResult lottoResult = new LottoResult(lottoShop);
+        lottoResult.getResult();  // 결과를 계산합니다.
+ 
+        // 결과 출력
+        lottoResult.printResult();
     }
 }

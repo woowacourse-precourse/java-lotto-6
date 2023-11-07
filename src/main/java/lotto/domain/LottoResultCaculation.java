8b310a4b9303885261aa66dbf49402145bd7ee2a
WinterHana <haha920@naver.com>
2023-11-07 14:34:53 +0900

    feat(LottoResultCalculation) : 로또 결과를 반환할 클래스 추가
    - 전체적인 로또 결과를 계산해줄 클래스 추가

---
+++ src/main/java/lotto/domain/LottoResultCaculation.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoResultCaculation {
+    public int caculateLottoTicket(int money) {
+        if(money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        int ticket = money / 1000;
+        return ticket;
+    }
+
+    public int checkResult(List<Integer> target, List<Integer> tryLotto) {
+        int result = (int) tryLotto.stream()
+                .filter(target::contains)
+                .count();
+
+        return result;
+    }
+
+    public boolean checkBounsNumber(List<Integer> target, int bouns) {
+        boolean result = target.contains(bouns);
+        return result;
+    }
+}

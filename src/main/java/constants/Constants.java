4b757e1b74c1f05b733dad7bc4fa0688a2a11e89
nayonsoso <panda0329@naver.com>
2023-11-07 21:39:41 +0900

    feat: 사용할 값 상수화

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Constants {
+    public static final int LOTTO_LENGTH = 6;
+    public static final int LOTTO_RANGE_START = 1;
+    public static final int LOTTO_RANGE_END = 45;
+}

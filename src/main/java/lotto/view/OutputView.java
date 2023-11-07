fbeb671c45a04a80e246b4526f05dfa7c662102f
hellomatia <shuaigejp80@gmail.com>
2023-11-07 16:50:34 +0900

    feat(view): 출력 기능 추가
    - 로또 구입금액 문구를 출력한다.
    - 로또 구입한 로또 개수를 출력한다.
    - 로또 당첨 번호 입력 문구를 출력한다.
    - 로또 보너스 입력 문구를 출력한다.
    - 로또 번호를 출력한다.
    - 로또 당첨 결과를 출력한다.
      - 순위별 당첨 개수를 출력한다.
      - 총 로또 수익률을 출력한다.
    - 에러 메시지를 출력한다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,116 @@
+package lotto.view;
+
+import lotto.dto.*;
+
+import java.util.List;
+
+import static lotto.constants.ViewConstants.*;
+import static lotto.view.ViewMessage.*;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static OutputView create() {
+        return new OutputView();
+    }
+
+    public void printInputLottoPurchaseAmount() {
+        println(INPUT_LOTTO_PURCHASE_AMOUNT.getMessage());
+    }
+
+    public void printInputWinningLotto() {
+        printNewLine();
+        println(INPUT_WINNING_LOTTO.getMessage());
+    }
+
+    public void printInputBonusNumber() {
+        printNewLine();
+        println(INPUT_BONUS_NUMBER.getMessage());
+    }
+
+    public void printLottoPurchaseCount(final LottoCountDTO lottoCountDTO) {
+        printNewLine();
+        println(String.format(OUTPUT_LOTTO_PURCHASE_COUNT.getMessage(), lottoCountDTO.lottoCount()));
+    }
+
+    public void printLottoNumbers(final LottoNumbersDTO lottoNumbersDTO) {
+        String lottoNumbersMessage = String.join(OUTPUT_LOTTO_NUMBERS_DELIMITER.getMessage(),
+                lottoNumbersDTO.numbers());
+        println(String.format(OUTPUT_LOTTO_NUMBERS.getMessage(), lottoNumbersMessage));
+    }
+
+    public void printLottoWinningStatistics(final LottoResultDTO lottoResultDTO) {
+        printNewLine();
+        println(OUTPUT_LOTTO_WINNING_STATISTICS.getMessage());
+        printThreeNumberMatch(lottoResultDTO.lottoWinningResultDTOs());
+        printFourNumberMatch(lottoResultDTO.lottoWinningResultDTOs());
+        printFiveNumberMatch(lottoResultDTO.lottoWinningResultDTOs());
+        printFiveNumberAndBonusNumberMatch(lottoResultDTO.lottoWinningResultDTOs());
+        printSixNumberMatch(lottoResultDTO.lottoWinningResultDTOs());
+        printLottoReturns(lottoResultDTO.lottoReturnsRateDTO());
+    }
+
+    private void printThreeNumberMatch(final List<LottoWinningResultDTO> lottoWinningResultDTOs) {
+        int count = (int) lottoWinningResultDTOs.stream()
+                .map(LottoWinningResultDTO::numberMatchCount)
+                .filter(numberMatchCount -> numberMatchCount == LOTTO_THREE_NUMBER_MATCH.getValue())
+                .count();
+        println(String.format(OUTPUT_LOTTO_THREE_NUMBER_MATCH.getMessage(), count));
+    }
+
+    private void printFourNumberMatch(final List<LottoWinningResultDTO> lottoWinningResultDTOs) {
+        int count = (int) lottoWinningResultDTOs.stream()
+                .map(LottoWinningResultDTO::numberMatchCount)
+                .filter(numberMatchCount -> numberMatchCount == LOTTO_FOUR_NUMBER_MATCH.getValue())
+                .count();
+        println(String.format(OUTPUT_LOTTO_FOUR_NUMBER_MATCH.getMessage(), count));
+    }
+
+    private void printFiveNumberMatch(final List<LottoWinningResultDTO> lottoWinningResultDTOs) {
+        int count = (int) lottoWinningResultDTOs.stream()
+                .filter(lottoWinningResultDTO ->
+                        lottoWinningResultDTO.numberMatchCount() == LOTTO_FIVE_NUMBER_MATCH.getValue()
+                                && !lottoWinningResultDTO.isBonusNumberMatch())
+                .count();
+        println(String.format(OUTPUT_LOTTO_FIVE_NUMBER_MATCH.getMessage(), count));
+    }
+
+    private void printFiveNumberAndBonusNumberMatch(final List<LottoWinningResultDTO> lottoWinningResultDTOs) {
+        int count = (int) lottoWinningResultDTOs.stream()
+                .filter(lottoWinningResultDTO ->
+                        lottoWinningResultDTO.numberMatchCount() == LOTTO_FIVE_NUMBER_MATCH.getValue()
+                                && lottoWinningResultDTO.isBonusNumberMatch())
+                .count();
+        println(String.format(OUTPUT_LOTTO_FIVE_NUMBER_BONUS_NUMBER_MATCH.getMessage(), count));
+    }
+
+    private void printSixNumberMatch(final List<LottoWinningResultDTO> lottoWinningResultDTOs) {
+        int count = (int) lottoWinningResultDTOs.stream()
+                .map(LottoWinningResultDTO::numberMatchCount)
+                .filter(numberMatchCount -> numberMatchCount == LOTTO_SIX_NUMBER_MATCH.getValue())
+                .count();
+        println(String.format(OUTPUT_LOTTO_SIX_NUMBER_MATCH.getMessage(), count));
+    }
+
+    private void printLottoReturns(final LottoReturnsRateDTO lottoReturnsRateDTO) {
+        print(String.format(OUTPUT_LOTTO_RETURNS.getMessage(), lottoReturnsRateDTO.lottoReturnsRate()));
+    }
+
+    public void printErrorMessage(final String message) {
+        println(message);
+    }
+
+    public static void print(final String Message) {
+        System.out.print(Message);
+    }
+
+    public static void println(final String Message) {
+        System.out.println(Message);
+    }
+
+    public static void printNewLine() {
+        System.out.println();
+    }
+}

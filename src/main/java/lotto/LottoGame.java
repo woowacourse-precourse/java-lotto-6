c79e6ebf2b813afd7c4373a3ff22997b9e9adedf
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-07 17:19:20 +0900

    Feat: Add getUserInputBonus and Create Bonus instance in LottoGame

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 public class LottoGame {
     public static void main(String[] args) {
+
+
         List<Integer> lottoNumbers = InputHandler.getUserInputlotto();
 
         Lotto lotto = new Lotto(lottoNumbers);
+
+        int bonusNumber = InputHandler.getUserInputBonus();
+
+        Bonus bonus = new Bonus(bonusNumber, lotto);
     }
 }

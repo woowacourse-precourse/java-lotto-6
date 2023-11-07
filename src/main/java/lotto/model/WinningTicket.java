3b06593faf39b0d7ff02a9211ed1e126f53255a0
wook <dong930112@gmail.com>
2023-11-07 18:58:46 +0900

    feat: 당첨로또 클래스 추가

--- src/main/java/lotto/model/WinningTicket.java
+++ src/main/java/lotto/model/WinningTicket.java
@@ -8,6 +8,16 @@ public class WinningTicket {
     private List<Integer> winningNumbers;
     private int bonusNumber;
 
+    public WinningTicket(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
 
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
 
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }

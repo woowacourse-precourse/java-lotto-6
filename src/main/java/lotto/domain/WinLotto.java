3ed93048bd991d04af46e5cd4969253fb67dbac7
wooyong99 <wulovesk@naver.com>
2023-11-07 15:57:14 +0900

    refactor : 당첨 번호 입력 메서드

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -1,13 +1,27 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinLotto {
     private static Lotto lotto;
     private static BounsNumber bounsNumber;
 
     public void createLotto() {
-        String input = Console.readLine();
+        lotto = new Lotto(inputWinNumber());
+        lotto.lottoByDuplicateNumber();
     }
 
+    private List<Integer> inputWinNumber() {
+        try {
+            return Arrays.stream(Console.readLine().split(","))
+                           .map(e -> Integer.parseInt(e))
+                           .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(",로 구분하여 입력해주세요.");
+        }
+
+    }
 }

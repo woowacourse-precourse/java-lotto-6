e1798340fed97c58d73c42b3185901a29ab4a7bc
Sanghwi Lee <sanghwi47@naver.com>
2023-11-07 13:41:03 +0900

    refactor(WinningNumbers) 유효성 검사 메소드들에 대해 SRP를 지키도록 세분화하였으며, 이를 통합적으로 실행할 수 있는 메소드를 따로 분리함. 검증의 경우 생성자호출을 통해 이루어지도록 코드를 개선함 chore() 메소드명 및 변수명 수정

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -6,62 +6,90 @@ import java.util.List;
 import java.util.Set;
 
 public class WinningNumbers {
-    private static final String ERROR_NULL_OR_EMPTY_WINNING_NUMBER = "당첨번호를 입력하세요.";
+    private static final String ERROR_NULL_WINNING_NUMBER = "당첨번호를 입력하세요.";
+    private static final String ERROR_EMPTY_WINNING_NUMBER = "6개의 번호가 입력되었는지 확인하세요.";
     private static final String ERROR_INVALID_INPUT_TYPE = "유효하지 않은 타입입니다. 숫자로 입력해주세요.";
     private static final String ERROR_LESS_OR_OVER_COUNT_OF_NUMBER = "당첨 번호는 6개로 구성해 주세요.";
     private static final String ERROR_INVALID_DUPLICATED_NUMBER = "서로 다른 번호를 입력해 주세요.";
     private static final String ERROR_INPUT_NUMBER_NOT_IN_RANGE = "1-45 사이의 값을 입력해 주세요.";
-    private Set<Integer> winningNumbers;
+    private final Set<Integer> winningNumbers;
 
     public WinningNumbers(String[] numbers) {
-        this.winningNumbers = parseAndValidateNumbers(numbers);
+        validateInput(numbers);
+        int[] parsedNumbers = parseNumbers(numbers);
+        validateWinningNumbers(parsedNumbers);
+        this.winningNumbers = convertArrayToSet(parsedNumbers);
     }
 
     public List<Integer> getWinningNumbers() {
         return new ArrayList<>(winningNumbers);
     }
 
-    private Set<Integer> parseAndValidateNumbers(String[] numbers) {
-        validateNotNullOrEmpty(numbers);
-        Set<Integer> checkingNumbers = new HashSet<>();
-        for (String number : numbers) {
-            int parsedNumber = parseAndValidateSingleNumber(number);
-            if (checkingNumbers.contains(parsedNumber)) {
-                throw new IllegalArgumentException(ERROR_INVALID_DUPLICATED_NUMBER);
-            }
-            checkingNumbers.add(parsedNumber);
+    private void validateInput(String[] numbers) {
+        validateNotNull(numbers);
+        validateNotEmptyInput(numbers);
+        validateNumbersCount(numbers);
+        validateNoDuplicates(numbers);
+    }
+
+    private void validateWinningNumbers(int[] numbers) {
+        for (int number : numbers) {
+            validateNumberInRange(number);
         }
-        if (checkingNumbers.size() != 6) {
+    }
+
+    private void validateNotNull(String[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException(ERROR_NULL_WINNING_NUMBER);
+        }
+    }
+
+    private void validateNotEmptyInput(String[] numbers) {
+        for (String number : numbers) {
+            if (number == null || number.trim().isEmpty()) {
+                throw new IllegalArgumentException(ERROR_EMPTY_WINNING_NUMBER);
+            }
+        }
+    }
+
+    private void validateNumbersCount(String[] numbers) {
+        if (numbers.length != 6) {
             throw new IllegalArgumentException(ERROR_LESS_OR_OVER_COUNT_OF_NUMBER);
         }
-        return checkingNumbers;
     }
 
-    private void validateNotNullOrEmpty(String[] numberStrings) {
-        if (numberStrings == null || numberStrings.length == 0) {
-            throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY_WINNING_NUMBER);
-        }
-        for (String numberStr : numberStrings) {
-            if (numberStr == null || numberStr.trim().isEmpty()) {
-                throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY_WINNING_NUMBER);
+    private void validateNoDuplicates(String[] numbers) {
+        Set<String> uniqueNumbers = new HashSet<>();
+        for (String number : numbers) {
+            if (!uniqueNumbers.add(number.trim())) {
+                throw new IllegalArgumentException(ERROR_INVALID_DUPLICATED_NUMBER);
             }
         }
     }
 
-    private int parseAndValidateSingleNumber(String numberStr) {
-        int parsedNumber;
-        try {
-            parsedNumber = Integer.parseInt(numberStr);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR_INVALID_INPUT_TYPE);
-        }
-        validateNumber(parsedNumber);
-        return parsedNumber;
+    private int[] parseNumbers(String[] numbers) {
+        return java.util.Arrays.stream(numbers)
+                .mapToInt(number -> {
+                    try {
+                        return Integer.parseInt(number);
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException(ERROR_INVALID_INPUT_TYPE);
+                    }
+                })
+                .toArray();
     }
 
-    private void validateNumber(int number) {
+    private void validateNumberInRange(int number) {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException(ERROR_INPUT_NUMBER_NOT_IN_RANGE);
         }
+    }
+
+    private Set<Integer> convertArrayToSet(int[] numbers) {
+        Set<Integer> winningNumbers = new HashSet<>();
+        for (int number : numbers) {
+            winningNumbers.add(number);
+        }
+        return winningNumbers;
     }
 }

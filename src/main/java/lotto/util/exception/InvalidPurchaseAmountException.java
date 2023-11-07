508fac9064366b05d0ac89e7c0f342989f10c0bf
Seyoung Goo <0917sy@gmail.com>
2023-11-08 05:28:30 +0900

    test: add Lotto class tests

--- src/main/java/lotto/util/exception/InvalidPurchaseAmountException.java
+++ src/main/java/lotto/util/exception/InvalidPurchaseAmountException.java
@@ -2,11 +2,7 @@ package lotto.util.exception;
 
 public class InvalidPurchaseAmountException extends IllegalArgumentException {
     private final static String message = "구입 금액은 로또 가격의 배수여야 합니다. 입력값: ";
-
-    public InvalidPurchaseAmountException() {
-        super(message);
-    }
-
+    
     public InvalidPurchaseAmountException(int input) {
         super(message + input);
     }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,12 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -25,4 +25,20 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfTheRangeNumbers() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자들이 같으면 같은 로또 번호이다.")
+    @Test
+    void isEqualNumbersIsEqual() {
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertEquals(lotto1, lotto2);
+        assertEquals(lotto1.hashCode(), lotto2.hashCode());
+    }
 }

aebb42bce8fcfb42dd9e9608b4bb7fdcd4e1ef61
jae woo Jung <awfjol2008@naver.com>
2023-11-08 02:23:52 +0900

    feat: InputUtil 클래스 쉼표를 기준으로 로또 번호 입력값을 구분하는 기능 구현

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,8 +1,17 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputUtil {
 
     private InputUtil() {
 
+    }
+
+    public static List<Integer> convertToList(String input) {
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
     }
 }

8b43f2aaab9fd5b4a52f71db0c5f56ff5166f9ab
SungHoCha <tjdgh3725@gmail.com>
2023-11-08 04:24:53 +0900

    FEAT - "add Lottos, LottoFactory"
    구매횟수만큼 Lotto생성하는 로직을 LottoFactory에 추가
    생성된 Lotto들을 일급컬렉션으로 관리하는 Lottos 객체 추가

---
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoFactory {
+
+    public static Lotto createLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+
+    public static Lottos createLottos(PriceAmount priceAmount) {
+        int purchaseNumber = priceAmount.getPurchaseNumber();
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseNumber; i++) {
+            lottos.add(LottoFactory.createLotto());
+        }
+        return new Lottos(lottos);
+    }
+}

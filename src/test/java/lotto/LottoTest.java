48e4a5435718d524609b96d58be30e955ad63b77
hyuuuun <pam4522@gmail.com>
2023-11-07 13:59:42 +0900

    test: 로또 범위가 1~45가 아닌 경우

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,6 +22,12 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @DisplayName("로또 번호가 1~45가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoInRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 
     // 아래에 추가 테스트 작성 가능

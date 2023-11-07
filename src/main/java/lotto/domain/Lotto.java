11703f184478c9da3c0f0cefb95c4824dd2d26c8
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 22:01:23 +0900

    feat(Lotto): 당첨 번호와 일치하는 개수 반환 함수 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
 import lotto.util.Validator;
 
@@ -9,6 +10,16 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public int countMatchingLottoNumbers(Lotto winnigLotto) {
+        return (int) numbers.stream()
+                .filter(winnigLotto::containsNumber)
+                .count();
+    }
+
+    public boolean containsNumber(int number) {
+        return numbers.contains(number);
     }
 
     private void validate(List<Integer> numbers) {

1bc2af0d0a20e5f3a40e91a789f03741cd9dd561
Yeriimii <seojs0511@naver.com>
2023-11-07 14:56:08 +0900

    feat(LottoPurchaseManager): 구입 수량만큼 로또 용지를 여러 장 발행할 수 있다

--- src/main/java/lotto/model/LottoPurchaseManager.java
+++ src/main/java/lotto/model/LottoPurchaseManager.java
@@ -2,6 +2,9 @@ package lotto.model;
 
 import lotto.system.util.LottoNumberGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoPurchaseManager {
     private static final int LOTTO_PRICE = 1000;
     private final LottoNumberGenerator lottoNumberGenerator;
@@ -12,5 +15,17 @@ public class LottoPurchaseManager {
 
     public int calculatePurchaseQuantity(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    public List<LottoPaper> purchaseLottoPapers(int quantity) {
+        List<LottoPaper> purchasedLottoPapers = new ArrayList<>(quantity);
+        for (int purchaseIndex = 0; purchaseIndex < quantity; purchaseIndex++) {
+            Lotto lotto = new Lotto(lottoNumberGenerator.generate());
+            LottoPaper lottoPaper = lotto.toLottoPaper();
+
+            purchasedLottoPapers.add(lottoPaper);
+        }
+
+        return purchasedLottoPapers;
     }
 }

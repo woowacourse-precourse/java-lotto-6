830fd5174b897a7fce0bf8d4292881645113485d
jiu6525 <jiu6525@daum.net>
2023-11-07 17:55:35 +0900

    feat : 사용자 입력이 숫자가 아닐경우 예외를 발생하는 메서드 생성

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -8,6 +8,7 @@ public class LottoException {
     private static final int ZERO_PRICE = 0;
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String TICKET_ERROR_MESSAGE = "구입 금액이 잘못 되었습니다. 티켓 한장의 가격은 1000원입니다.";
+    private static final String DIGIT_ERROR_MESSAGE = "는 숫자가 아닙니다. 숫자를 입력해 주세요.";
 
     private boolean purchaseAmountCheckSub(String purchaseAmount) {
         int amount = Integer.parseInt(purchaseAmount);
@@ -24,5 +25,15 @@ public class LottoException {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+
+    public boolean checkNumber(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_PREFIX + userInput + DIGIT_ERROR_MESSAGE);
+            return false;
+        }
+        return true;
     }
 }

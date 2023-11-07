6029ea7b5786425911015777f8406cf3360a2be3
yunju-m <dbswnfnal@naver.com>
2023-11-07 16:14:33 +0900

    Feat(LottoController): ✨ 당첨 개수 구하는 기능 구현 - 1
    1. 당첨번호 개수가 5개이면서 보너스 번호가 포함된 경우 보너스 당첨자 수 증가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -7,11 +7,15 @@ import lotto.LuckeyLotto;
 import view.OutputView;
 
 public class LottoController {
+    private static final int REWARD_SIZE = 5;
+    private static final int LOTTO_BONUS_NUMBER_INDEX = 5;
+
     LuckeyLotto luckeyLotto;
     LottoMoney lottoMoney;
 
     OutputView outputView = new OutputView();
     int lottoWinnerCount;
+    int[] lottoWinners = new int[REWARD_SIZE];
 
     public LottoController(LottoMoney lottoMoney, LuckeyLotto luckeyLotto) {
         this.lottoMoney = lottoMoney;
@@ -26,7 +30,7 @@ public class LottoController {
     private void decideLottoWinner(List<List<Integer>> lottoNumbers) {
         for (int lottoIndex = 0; lottoIndex < lottoMoney.getLottoTicket(); lottoIndex++) {
             lottoWinnerCount = getLottoWinnerCount(lottoNumbers.get(lottoIndex));
-            System.out.println(lottoIndex + " : " + lottoWinnerCount);
+            increaseLottoWinnerCount(lottoNumbers.get(lottoIndex), lottoWinnerCount);
         }
     }
 
@@ -38,5 +42,11 @@ public class LottoController {
             }
         }
         return lottoCount;
+    }
+
+    private void increaseLottoWinnerCount(List<Integer> lottoNumbers, int lottoWinnerCount) {
+        if (lottoWinnerCount == LOTTO_BONUS_NUMBER_INDEX && lottoNumbers.contains(luckeyLotto.getLuckeyBonusNumber())) {
+            lottoWinners[lottoWinnerCount - 2]++;
+        }
     }
 }

a4c1df2e04e06a86edbd3275a5e024ea19fe5dc0
sdsr <dlxodnr1212@gmail.com>
2023-11-07 20:26:43 +0900

    "feat(Application): add feature to call run"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Controller controller = new Controller();
+            controller.run();
+        } catch (Exception exception) {
+        }
     }
 }

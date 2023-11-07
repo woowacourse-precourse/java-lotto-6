9929606675ac07b196d76d4051f5b02d2d2257b9
CHAHANS <herodev0129@gmail.com>
2023-11-07 20:23:11 +0900

    refactor(view): 입력예외처리는 모두 inpuMaker이동.

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -12,29 +12,35 @@ public class LottoController {
     private OutputMaker outputMaker;
     private Customer cs;
     private Analyst analyst;
+    private boolean isvalid;
 
     LottoController() {
         this.ticketMaster = new TicketMaster();
         this.inputMaker = new InputMaker();
         this.outputMaker = new OutputMaker();
+        this.isvalid = false;
     }
 
     public void insertCoin() {
-        try {
-            System.out.println("구입금액을 입력해 주세요.");
-            String inputText = Console.readLine();
-            int coin = inputMaker.inputNum(inputText);
-            inputMaker.inputCoinValidate(coin);
-            this.cs = new Customer(coin);
-        } catch (IllegalArgumentException e) {
-            System.err.println("[ERROR] 유효한 숫자를 입력해주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
+        isvalid = false;
+        while (!isvalid) {
+            try {
+                String userInput = Console.readLine();
+                int coin = inputMaker.inputNum(userInput);
+                inputMaker.inputCoinValidate(coin);
+                isvalid = true;
+                this.cs = new Customer(coin);
+            } catch (IllegalArgumentException e) {
+                System.err.println("[ERROR] 금액은 1000단위 숫자로 다시 입력해주세요.");
+            }
         }
     }
 
     public void buyLotto() {
         int lottoSize = cs.getWallet();
-        System.out.println(lottoSize/1000+"개를 구매했습니다.");
-        while(cs.getWallet()!=0) {
+        System.out.println(lottoSize / 1000 + "개를 구매했습니다.");
+        while (cs.getWallet() != 0) {
             Lotto newLotto = ticketMaster.makeTicket();
             System.out.println(outputMaker.printLotto(newLotto));
             cs.pay(LottoRole.LOTTO_PRICE.getNumber());
@@ -44,13 +50,35 @@ public class LottoController {
 
     public void setWinner() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        Lotto winner = new Lotto(inputMaker.inputWinningNumber(Console.readLine()));
-        ticketMaster.setWinningNum(winner);
+        isvalid = false;
+        while (!isvalid) {
+            try {
+                String userInput = Console.readLine();
+                Lotto winner = new Lotto(inputMaker.inputWinningNumber(userInput));
+                ticketMaster.setWinningNum(winner);
+                isvalid = true;
+            } catch (IllegalArgumentException e) {
+                System.err.println("[ERROR] 다시 입력하세요.");
+            }
+        }
     }
 
     public void setBonus() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        ticketMaster.setBonusNum(inputMaker.inputNum(Console.readLine()));
+        isvalid = false;
+        while (!isvalid) {
+            try {
+                String userInput = Console.readLine();
+                int bonusNum = inputMaker.inputNum(userInput);
+                inputMaker.lottoNumRangeValidate(bonusNum);
+                isvalid = true;
+                ticketMaster.setBonusNum(bonusNum);
+            } catch (IllegalArgumentException e) {
+                System.err.println("[ERROR] 다시 입력하세요.");
+            } finally {
+                Console.close();
+            }
+        }
     }
 
     public void resultLotto() {
@@ -60,7 +88,7 @@ public class LottoController {
             Lotto thisLotto = cs.getCustomerLotto(i);
             int winningCount = ticketMaster.checkLotto(thisLotto);
             boolean bonusCount = ticketMaster.checkLottoBonus(thisLotto);
-            analyst.addLottoResult(winningCount,bonusCount);
+            analyst.addLottoResult(winningCount, bonusCount);
         }
         System.out.println(outputMaker.printResult(analyst.revenueLotto()));
     }
--- src/main/java/lotto/view/InputMaker.java
+++ src/main/java/lotto/view/InputMaker.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.domain.LottoRole;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +11,7 @@ public class InputMaker {
         try {
             return Integer.parseInt(userInput.trim());
         } catch (NumberFormatException e) {
-            System.err.println("[ERROR] 유효하지 않은 숫자입니다.");
-            return -1;
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 숫자입니다.");
         }
     }
 
@@ -20,11 +21,22 @@ public class InputMaker {
         }
     }
 
+    public void lottoNumRangeValidate(int num) {
+        if (num < LottoRole.LOTTO_MIN_NUMBER.getNumber() ||
+                num > LottoRole.LOTTO_MAX_NUMBER.getNumber()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는"+
+                    LottoRole.LOTTO_MIN_NUMBER.getNumber()+"부터 "+
+                    LottoRole.LOTTO_MAX_NUMBER.getNumber()+" 사이의 숫자여야 합니다.");
+        }
+    }
+
     public List<Integer> inputWinningNumber(String userInput) {
         List<Integer> result = new ArrayList<>();
         String[] inputNumber = userInput.split(",");
         for (int i = 0; i < inputNumber.length; i++) {
-            result.add(inputNum(inputNumber[i].trim()));
+            int nowNum = inputNum(inputNumber[i].trim());
+            lottoNumRangeValidate(nowNum);
+            result.add(nowNum);
         }
         return result;
     }

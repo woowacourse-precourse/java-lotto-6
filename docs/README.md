ffb315313d338714dcd5dd16446d195ec1ac5470
oyeong011 <oyeong011@gmail.com>
2023-11-07 09:17:37 +0900

    feat : 로또번호 입력 및 예외처리 메서드 구현

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,25 @@
-# 입력 받기 기능
-+ BufferedReader로 금액과 로또 번호 입력받기
+# 입력값 검증 받는 기능
+## 로또 금액받기
+### setAmountOfLotto
++ 로또 금액을 받고 유효할때까지 반복시키는 메서드
+### parseAmount
++ 로또 금액을 setAmountOfLotto에서 입력 유효성 검사를 위한 trycatch문작성
+### validateInput
++ 로또 금액 유효성 검사 1000으로 나눴을때 안나누어지면 예외처리
+
+### setWinningNumber
++ null인 winningNumber 배열을 가지고 반복문 시작
+
++ 사용자로부터 입력을 받아 validateWinningNumbers 메소드를 호출
+
+### validateWinningNumbers
++ 입력이 유효하지 않은 경우, 즉 6개의 숫자가 아니거나 숫자가 아닌 값이 포함된 경우 IllegalArgumentException을 발생.
+
+### setWinningNumber
++ try-catch 블록에서 이 예외를 잡아내고, 사용자에게 오류 메시지를 출력한 후 다시 입력을 요청합니다.
+
+### setBonusNumber
++ 보너스 숫자 입력받는 메서드로 isValidWinningNumbers로 검사하면서 예외발생시 다시 입력을 요청합니다.
+
+### isValidWinningNumbers
++ 입력값이 숫자가 아니거나 1미만 45이상이면 예외처리
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,22 +1,84 @@
 package lotto;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) throws IOException{
-        // TODO: 프로그램 구현
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int amountOfLotto = Integer.parseInt(br.readLine());
-        String[] winningNumber = br.readLine().split(",");
-        String bonusNumber = br.readLine();
+    public static void main(String[] args){
+        // TODO: 프로그램 구현 
+        int amountOfLotto = setAmountOfLotto(-1);
+        String[] winningNumber = setWinningNumber();
+        int bonusNumber = setBonusNumber(-1);
 
 
         System.out.println("amountOfLotto " + amountOfLotto);
         System.out.println("winningNumber " + winningNumber);
         System.out.println("bonusNumber " + bonusNumber);
     }
+
+    public static int setAmountOfLotto(int amountOfLotto){
+        while (amountOfLotto == -1) {
+            amountOfLotto = parseAmount(Console.readLine());
+        }
+        return amountOfLotto;
+    }
+    private static int parseAmount(String input) {
+        try {
+            int amount = Integer.parseInt(input);
+            validateInput(amount);
+            return amount;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
+        return -1; // 유효하지 않은 입력에 대해 -1 반환
+    }
+    public static void validateInput(int amountOfLotto){
+        if(amountOfLotto % 1000 != 0){
+            throw new IllegalArgumentException("로또는 1000원 단위로 적어야합니다.");
+        }
+    }
+    public static String[] setWinningNumber() {
+        String[] winningNumber = null;
+        while (winningNumber == null) {
+            try {
+                System.out.print("당첨 번호를 입력하세요 (쉼표로 구분된 6개의 숫자): ");
+                winningNumber = validateWinningNumbers(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                // 잘못된 입력에 대해 다시 입력을 요청
+            }
+        }
+        return winningNumber;
+    }
+    private static String[] validateWinningNumbers(String input) {
+        String[] numbers = input.split(",");
+        if (numbers.length != 6) {
+            throw new IllegalArgumentException("당첨 번호는 쉼표로 구분된 6개의 숫자여야 함");
+        }
+        for (String number : numbers) {
+            isValidWinningNumbers(number);
+        }
+        return numbers;
+    }
+    public static int setBonusNumber(int initialNum){
+        while (initialNum == -1) {
+            try {
+                initialNum = isValidWinningNumbers(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return initialNum;
+    }
+    private static int isValidWinningNumbers(String number) {
+        int num = 0;
+        try {
+            num = Integer.parseInt(number.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("모든 번호는 숫자여야 함");
+        }
+        if (num < 1 || num > 45) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이여야 함");
+        }
+        return num;
+    } 
 }

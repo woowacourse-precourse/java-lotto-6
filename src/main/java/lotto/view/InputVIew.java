31042e8b52f5657b975178be189303579995b1e8
scarlet0o0 <ghd7673@naver.com>
2023-11-07 16:25:54 +0900

    Feat : view 추가 구현 완료

--- src/main/java/lotto/view/InputVIew.java
+++ src/main/java/lotto/view/InputVIew.java
@@ -2,18 +2,31 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputVIew {
     private static final String COMMA = ",";
 
     public int promptLottoPurchaseAmount() {
-        return Integer.parseInt(Console.readLine());
+        int number = 0;
+        boolean isValidInput = false;
+
+        while (!isValidInput) {
+            try {
+                number = Integer.parseInt(Console.readLine());
+                isValidInput = true; // 유효한 입력을 받았을 때 반복문을 종료
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자만 입력해주십시오");
+            }
+        }
+        return number;
     }
 
-    public int[] promptLottoWinningNumbers() {
+    public List<Integer> promptLottoWinningNumbers() {
         return Arrays.stream(Console.readLine().split(COMMA))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     public int promptLottoBonusNumber() {
--- src/main/java/lotto/view/OutputVIew.java
+++ src/main/java/lotto/view/OutputVIew.java
@@ -1,5 +1,47 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.congin.LottoWinningAmount;
+import lotto.domain.Lotto;
+import lotto.dto.WinningStatisticsDto;
+
 public class OutputVIew {
+
+    public void printForPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printUserLottoList(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.\n",lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.printf("%s \n",lotto.toString());
+        }
+    }
+
+    public void printWinningNumbersPrompt() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printBonusNumberPrompt() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printWinningStatistics(WinningStatisticsDto winningStatisticsDto) {
+        System.out.println("당첨 통계\n" + "---");
+        LottoWinningAmount[] values = LottoWinningAmount.values();
+        for (LottoWinningAmount value : values) {
+            int n = 0;
+            if (winningStatisticsDto.lottoWinningAmount().get(value) != null) {
+                n = winningStatisticsDto.lottoWinningAmount().get(value);
+            }
+            String s = "";
+            if (value.getBonus()) {
+                s = ", 보너스 볼 일치";
+            }
+            System.out.printf("%d개 일치%s (%s원) - %d개\n",value.getWinningNumber(),s, String.format("%,d",value.getWinningAmount()),n);
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", winningStatisticsDto.yield());
+    }
+
 
 }

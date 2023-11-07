13cb33f2be18b17e14f1279ceb1292c70779485b
sung-yeop <jjabc9459@naver.com>
2023-11-07 20:02:59 +0900

    feat(LottoController): View와 Model을 연결하는 Controller 구현
    - View의 기능과 Controller의 기능을 사용하여 로직을 처리하는 Controller 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.Input;
+import lotto.model.LottoUtil;
+import lotto.model.RandomGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(new Input(), new LottoUtil(),
+                new InputView(), new OutputView(), new RandomGenerator());
+        lottoController.run();
     }
 }

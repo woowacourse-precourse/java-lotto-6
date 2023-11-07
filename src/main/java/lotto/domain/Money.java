00d12318be616ffa7e184e600727804e23019aa9
twoosky <lhjae3@naver.com>
2023-11-08 07:31:14 +0900

    feat: 로또 구입 금액 검증 기능 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import static lotto.utils.ErrorMessages.MONEY_NOT_MULTIPLE_UNIT;
+import static lotto.utils.ErrorMessages.MONEY_UNDER_MINIMUM;
+
+
+public class Money {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int MINIMUM_AMOUNT = 1000;
+
+    private final int money;
+
+    public Money(int money) {
+        validateMinimum(money);
+        validateUnit(money);
+
+        this.money = money;
+    }
+
+    private void validateUnit(int money) {
+        if (!isMultipleOfPrice(money)) {
+            throw new IllegalArgumentException(MONEY_NOT_MULTIPLE_UNIT);
+        }
+    }
+
+    private boolean isMultipleOfPrice(int money) {
+        return (money % LOTTO_PRICE) == 0;
+    }
+
+    private void validateMinimum(int money) {
+        if (money < MINIMUM_AMOUNT) {
+            throw new IllegalArgumentException(MONEY_UNDER_MINIMUM);
+        }
+    }
+}

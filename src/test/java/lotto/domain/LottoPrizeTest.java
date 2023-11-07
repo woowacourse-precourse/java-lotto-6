59e29f9484bbb98ad3f069c0bf759be5544c7181
jihwankim128 <jihwan1535@nate.com>
2023-11-07 12:23:06 +0900

    refactor: 상금 순위권 외 탈락의 경우도 테스트 완료

--- src/test/java/lotto/domain/LottoPrizeTest.java
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -4,38 +4,52 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("상급 테스트")
 class LottoPrizeTest {
-    @Test
-    void 상금_1등_테스트() {
-        assertThat(LottoPrize.valueOf(6, false).getPrizeRank()).isEqualTo(6);
-        assertThat(LottoPrize.valueOf(6, false).getPrizeMoney()).isEqualTo(2000000000);
-        assertThat(LottoPrize.valueOf(6, true).getPrizeRank()).isEqualTo(6);
-        assertThat(LottoPrize.valueOf(6, true).getPrizeMoney()).isEqualTo(2000000000);
+    @ParameterizedTest
+    @CsvSource({"6, true", "6, false",})
+    void 상금_1등_테스트(int matchCount, boolean matchBonus) {
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeRank()).isEqualTo(6);
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeMoney()).isEqualTo(2000000000);
     }
+
     @Test
     void 상금_2등_테스트() {
         assertThat(LottoPrize.valueOf(5, true).getPrizeRank()).isEqualTo(5);
         assertThat(LottoPrize.valueOf(5, true).getPrizeMoney()).isEqualTo(30000000);
     }
+
     @Test
     void 상금_3등_테스트() {
         assertThat(LottoPrize.valueOf(5, false).getPrizeRank()).isEqualTo(5);
         assertThat(LottoPrize.valueOf(5, false).getPrizeMoney()).isEqualTo(1500000);
     }
-    @Test
-    void 상금_4등_테스트() {
-        assertThat(LottoPrize.valueOf(4, false).getPrizeRank()).isEqualTo(4);
-        assertThat(LottoPrize.valueOf(4, false).getPrizeMoney()).isEqualTo(50000);
-        assertThat(LottoPrize.valueOf(4, true).getPrizeRank()).isEqualTo(4);
-        assertThat(LottoPrize.valueOf(4, true).getPrizeMoney()).isEqualTo(50000);
+
+    @ParameterizedTest
+    @CsvSource({"4, true", "4, false",})
+    void 상금_4등_테스트(int matchCount, boolean matchBonus) {
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeRank()).isEqualTo(4);
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeMoney()).isEqualTo(50000);
     }
-    @Test
-    void 상금_5등_테스트() {
-        assertThat(LottoPrize.valueOf(3, false).getPrizeRank()).isEqualTo(3);
-        assertThat(LottoPrize.valueOf(3, false).getPrizeMoney()).isEqualTo(5000);
-        assertThat(LottoPrize.valueOf(3, true).getPrizeRank()).isEqualTo(3);
-        assertThat(LottoPrize.valueOf(3, true).getPrizeMoney()).isEqualTo(5000);
+
+    @ParameterizedTest
+    @CsvSource({"3, true", "3, false",})
+    void 상금_5등_테스트(int matchCount, boolean matchBonus) {
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeRank()).isEqualTo(3);
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeMoney()).isEqualTo(5000);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, true", "1, true", "0, true",
+            "2, false", "1, false", "0, false"
+    })
+    void 상금_외_테스트(int matchCount, boolean matchBonus) {
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeRank()).isEqualTo(0);
+        assertThat(LottoPrize.valueOf(matchCount, matchBonus).getPrizeMoney()).isEqualTo(0);
+    }
+
 }

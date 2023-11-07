a34b0a61dcdd2c1fe3b2617e0a8be02890d9e3c8
donghyun <kdhdh001@naver.com>
2023-11-08 02:59:50 +0900

    refactor: Prize enum 클래서 메시지 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,19 +1,21 @@
 package lotto.domain;
 
 public enum Prize {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIFTH(3, 5_000, "3개 일치 (5,000원) - "),
+    FOURTH(4, 50_000, "4개 일치 (50,000원) - "),
+    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - "),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    NONE(0, 0, "");
 
     private final int matchCount;
     private final int prizeAmount;
+    private final String message;
 
-    Prize(int matchCount, int prizeAmount) {
+    Prize(int matchCount, int prizeAmount, String message) {
         this.matchCount = matchCount;
         this.prizeAmount = prizeAmount;
+        this.message = message;
     }
 
     public static Prize valueOf(int matchCount, boolean matchBonus) {
@@ -41,5 +43,9 @@ public enum Prize {
 
     public int getPrizeAmount() {
         return prizeAmount;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }

5e9c551c8fa290196d66ce7d27a33fc713cd93f8
Zho <chlwlgh1011@naver.com>
2023-11-07 14:15:09 +0900

    feat: enum 적용 PrintOutput구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,4 +1,12 @@
 package lotto.controller;
 
+import lotto.view.View;
+
 public class LottoGameController {
+    public LottoGameController () {
+        View.printStartMessage();
+    }
+
+    public void lottoGameFlow() {}
+
 }

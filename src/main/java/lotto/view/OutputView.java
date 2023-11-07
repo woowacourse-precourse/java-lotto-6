e1c7a1a8f183a7531e6b75d731193774e0de5df0
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 03:04:32 +0900

    feat(OutputView): 기본적인 것 출력 할 view단 구현 - printError() - printEnter()

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void printError(String error){
+        System.out.println(error);
+        printEnter();
+    }
+
+    public static void printEnter(){
+        System.out.println();
+    }
+}

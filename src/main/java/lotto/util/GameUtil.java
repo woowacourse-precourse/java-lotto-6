7f9c0e83febaaa0b34ea817de051e8bfe06cadc7
Jaewon <choijawon99@gmail.com>
2023-11-07 22:05:44 +0900

    feat(gameutil): 로또 생성 기능 구현

--- src/main/java/lotto/util/GameUtil.java
+++ src/main/java/lotto/util/GameUtil.java
@@ -1,7 +1,24 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.Collections;
+import java.util.List;
+
 public class GameUtil {
-    //로또 출력 형태 반환
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+    private static final int LOTTO_NUM = 6;
+
+    //로또 생성
+    public static Lotto createLotto(){
+        List<Integer> createdLotto = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM,MAX_LOTTO_NUM,LOTTO_NUM);
+
+        Collections.sort(createdLotto);
+
+        return new Lotto(createdLotto);
+    }
 
     //당첨 결과 반환
 

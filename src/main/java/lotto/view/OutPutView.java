67b5c32309b1596511aa53ebce476967f830ab5a
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-07 17:56:03 +0900

    feat(Lotto): 출력문 클래스 로또 구입 횟수와, 생성된 랜덤 로또들을 보여주는 기능을 구현 했습니다. 당첨통계 이후 3,4,5,5(보너스),6개 당첨중 담첨갯수를 알려주고, 수익률을 보여줍니다.

---
+++ src/main/java/lotto/view/OutPutView.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
+import lotto.domain.Rank;
+
+import java.util.HashMap;
+
+public class OutPutView {
+
+    public void outPutViewLottos(Lottos lottos, int userMoney) {
+        int lottoCount = lottos.numberLottos(userMoney);
+        System.out.println(lottoCount + "개를 구매했습니다.");
+
+        System.out.println(lottos);
+    }
+
+    public void outPutViewWinningStatistics(LottoResult lottoResult) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        HashMap<Rank, Integer> result = lottoResult.getResult();
+
+        System.out.println("3개 일치 (5,000원) - " + result.getOrDefault(Rank.MATCH_3, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.getOrDefault(Rank.MATCH_4, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.getOrDefault(Rank.MATCH_5, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.getOrDefault(Rank.MATCH_5_BONUS, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.getOrDefault(Rank.MATCH_6, 0) + "개");
+    }
+
+}

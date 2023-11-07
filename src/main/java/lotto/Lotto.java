4b9d71c24f94554bd9bb001aff22f5ce3be63e62
je488 <pje488@naver.com>
2023-11-07 20:05:40 +0900

    feat: 당첨 번호를 잘못 입력한 경우 예외처리하는 함수 및 완성된 당첨 번호를 반환하는 함수 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,42 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해 주세요.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력해 주세요.");
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> deduplicateNumber = new HashSet<>(numbers);
+        if (deduplicateNumber.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않은 숫자를 입력해 주세요.");
+        }
+
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

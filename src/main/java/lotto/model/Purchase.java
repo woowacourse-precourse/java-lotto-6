bcea8c064e7ce7915fe03adaaf56519d37eb2b85
HYUNJUN\sajun <sajun28@naver.com>
2023-11-07 23:49:00 +0900

    Refactor: "예외 발생 기능 분리"

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -8,13 +8,17 @@ public class Purchase {
     private final int purchasingAmount;
 
     public Purchase(String purchasingAmount) {
-        this.purchasingAmount = validateAndParseAmount(purchasingAmount);
+        validateNum(purchasingAmount);
+        this.purchasingAmount = validateUnit(purchasingAmount);
     }
 
-    private int validateAndParseAmount(String input) {
+    private void validateNum(String input) {
         if (!input.matches(INPUT_CONTENT_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
+    }
+
+    private int validateUnit(String input) {
         int amount = Integer.parseInt(input);
         if (amount % PURCHASING_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] " + PURCHASING_UNIT + "원 단위로 입력 가능합니다.");

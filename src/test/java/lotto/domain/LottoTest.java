82cac1d81ea549630c294fde49942d7e5c98f676
slowcloud <sju9417@gmail.com>
2023-11-07 16:39:56 +0900

    test: Lotto의 calculate 메소드를 검사하는 코드 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.constant.LottoRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -52,6 +53,98 @@ class LottoTest {
 
         }
 
+    }
+
+    @DisplayName("calculate 테스트")
+    @Nested
+    class calculate {
+
+        @DisplayName("3개 미만 당첨 검사")
+        @Test
+        void shouldReturnNOTHING() {
+
+            Lotto lotto = new Lotto(List.of(40, 41, 42, 43, 44, 45));
+            Result result = new OneToSevenResult();
+
+            LottoRank lottoRank = lotto.calculate(result);
+
+            assertEquals(lottoRank, LottoRank.NOTHING);
+
+        }
+
+        @DisplayName("3개 당첨 검사")
+        @Test
+        void shouldReturnTHREE() {
+
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 43, 44, 45));
+            Result result = new OneToSevenResult();
+
+            LottoRank lottoRank = lotto.calculate(result);
+
+            assertEquals(lottoRank, LottoRank.THREE);
+
+        }
+
+        @DisplayName("4개 당첨 검사")
+        @Test
+        void shouldReturnFOUR() {
+
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 44, 45));
+            Result result = new OneToSevenResult();
+
+            LottoRank lottoRank = lotto.calculate(result);
+
+            assertEquals(lottoRank, LottoRank.FOUR);
+
+        }
+
+        @DisplayName("5개 당첨 검사")
+        @Test
+        void shouldReturnFIVE() {
+
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 45));
+            Result result = new OneToSevenResult();
+
+            LottoRank lottoRank = lotto.calculate(result);
+
+            assertEquals(lottoRank, LottoRank.FIVE);
+
+        }
+
+        @DisplayName("5개, 보너스 당첨 검사")
+        @Test
+        void shouldReturnFIVE_BONUS() {
+
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+            Result result = new OneToSevenResult();
+
+            LottoRank lottoRank = lotto.calculate(result);
+
+            assertEquals(lottoRank, LottoRank.FIVE_BONUS);
+
+        }
+
+        @DisplayName("6개 당첨 검사")
+        @Test
+        void shouldReturnSIX() {
+
+            Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+            Result result = new OneToSevenResult();
+
+            LottoRank lottoRank = lotto.calculate(result);
+
+            assertEquals(lottoRank, LottoRank.SIX);
+
+        }
+
+    }
+
+}
+
+class OneToSevenResult extends Result {
+
+    public OneToSevenResult() {
+        super(WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6)), new BonusNumber(7));
     }
 
 }

a68a23be2fd2623d1889edb9f38698374285b81c
Minyongggg <naminyong97@gmail.com>
2023-11-08 03:49:41 +0900

    feat: 당첨 번호 값의 유효성을 검증하는 기능 구현

--- src/main/java/lotto/constant/ExceptionMessages.java
+++ src/main/java/lotto/constant/ExceptionMessages.java
@@ -1,5 +1,8 @@
 package lotto.constant;
 
+import static lotto.constant.GameOptions.LOTTO_MAX_NUMBER;
+import static lotto.constant.GameOptions.LOTTO_MIN_NUMBER;
+import static lotto.constant.GameOptions.LOTTO_NUMBER_COUNT;
 import static lotto.constant.GameOptions.LOTTO_PRICE;
 
 public enum ExceptionMessages {
@@ -8,6 +11,9 @@ public enum ExceptionMessages {
     BONUS_NUMBER_FORMAT_EXCEPTION_MESSAGE("보너스 번호는 양의 정수여야 합니다."),
     MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE("로또 구입 금액은 " + LOTTO_PRICE + " 이상이어야 합니다."),
     PURCHASE_AMOUNT_DIVISIBILITY_EXCEPTION_MESSAGE("로또 구입 금액은 " + LOTTO_PRICE + "로 나누어 떨어져야 합니다."),
+    WINNING_NUMBER_COUNT_EXCEPTION_MESSAGE("당첨 번호의 개수는 " + LOTTO_NUMBER_COUNT + "개여야 합니다."),
+    WINNING_NUMBER_RANGE_EXCEPTION_MESSAGE("당첨 번호는 " + LOTTO_MIN_NUMBER + " ~ " + LOTTO_MAX_NUMBER + " 사이의 값이어야 합니다."),
+    WINNING_NUMBER_DUPLICATION_EXCEPTION_MESSAGE("당첨 번호는 중복될 수 없습니다.");
 
 
     private String value;
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,9 +1,17 @@
 package lotto.util;
 
 import static lotto.constant.ExceptionMessages.MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE;
+import static lotto.constant.ExceptionMessages.WINNING_NUMBER_COUNT_EXCEPTION_MESSAGE;
+import static lotto.constant.ExceptionMessages.WINNING_NUMBER_DUPLICATION_EXCEPTION_MESSAGE;
+import static lotto.constant.ExceptionMessages.WINNING_NUMBER_RANGE_EXCEPTION_MESSAGE;
+import static lotto.constant.GameOptions.LOTTO_MAX_NUMBER;
+import static lotto.constant.GameOptions.LOTTO_MIN_NUMBER;
+import static lotto.constant.GameOptions.LOTTO_NUMBER_COUNT;
 import static lotto.constant.GameOptions.LOTTO_PRICE;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
@@ -35,6 +43,38 @@ public class Validator {
     private static void validateOverMinimum(Integer purchaseAmount) {
         if (purchaseAmount < LOTTO_PRICE.getValue()) {
             throw new IllegalArgumentException(MIN_PURCHASE_AMOUNT_EXCEPTION_MESSAGE.toString());
+        }
+    }
+
+    public static void validateWinningNumber(List<Integer> numbers) {
+        validateNumbersCount(numbers);
+        validateNumbersRange(numbers);
+        validateNumbersDuplication(numbers);
+    }
+
+    private static void validateNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT.getValue()) {
+            throw new IllegalArgumentException(WINNING_NUMBER_COUNT_EXCEPTION_MESSAGE.toString());
+        }
+    }
+
+    private static void validateNumbersRange(List<Integer> numbers) {
+        numbers.forEach(Validator::validateNumberRange);
+    }
+
+    private static void validateNumberRange(Integer number) {
+        if (number < LOTTO_MIN_NUMBER.getValue() || number > LOTTO_MAX_NUMBER.getValue()) {
+            throw new IllegalArgumentException(WINNING_NUMBER_RANGE_EXCEPTION_MESSAGE.toString());
+        }
+    }
+
+    private static void validateNumbersDuplication(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer number : numbers) {
+            if (set.contains(number)) {
+                throw new IllegalArgumentException(WINNING_NUMBER_DUPLICATION_EXCEPTION_MESSAGE.toString());
+            }
+            set.add(number);
         }
     }
 }

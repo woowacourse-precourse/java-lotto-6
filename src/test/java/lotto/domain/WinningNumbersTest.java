6a978bfa8f8262b0551857e32d35134075929d41
jiyoon <95969941+jjunjji@users.noreply.github.com>
2023-11-08 01:24:16 +0900

    test: 당첨 번호의 UI 로직 테스트 삭제

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -3,36 +3,37 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import lotto.exception.LottoExceptionMessage;
-import lotto.util.OutputTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class WinningNumbersTest extends OutputTest {
-    @DisplayName("당첨 번호가 중복되면 에러 메시지가 출력된다.")
+public class WinningNumbersTest {
+    @DisplayName("당첨 번호가 중복되면 null이 반환된다.")
     @Test
-    void getErrorMessageByNonNumbers() {
-        WinningNumbers.from(List.of(1, 1, 2, 3, 4, 5));
-        assertThat(output()).contains(
-                LottoExceptionMessage.WINNING_NUMBERS_MUST_BE_SIX_DIGITS.getMessage()
-        );
+    void getNullByDuplicateNumbers() {
+        WinningNumbers actual = WinningNumbers.from(List.of(1, 1, 2, 3, 4, 5));
+        assertThat(actual).isEqualTo(null);
     }
 
-    @DisplayName("당첨 번호가 6자리가 아니면 에러 메시지가 출력된다.")
+    @DisplayName("당첨 번호가 6자리가 아니면 null이 반환된다.")
     @Test
-    void getErrorMessageByFIVEDIGIT() {
-        WinningNumbers.from(List.of(1, 2, 3, 4, 5));
-        assertThat(output()).contains(
-                LottoExceptionMessage.WINNING_NUMBERS_MUST_BE_SIX_DIGITS.getMessage()
-        );
+    void getNullByFiveDigit() {
+        WinningNumbers actual = WinningNumbers.from(List.of(1, 2, 3, 4, 5));
+        assertThat(actual).isEqualTo(null);
     }
 
-    @DisplayName("당첨 번호가 1~45 사이 숫자가 아니면 에러 메시지가 출력된다.")
+    @DisplayName("당첨 번호가 1~45 사이 숫자가 아니면 null이 반환된다.")
     @Test
-    void getErrorMessageByNotBetweenStartAndEndInclusive() {
-        WinningNumbers.from(List.of(1, 2, 3, 4, 5, 46));
-        assertThat(output()).contains(
-                LottoExceptionMessage.WINNING_NUMBERS_MUST_BETWEEN_START_AND_END_INCLUSIVE.getMessage()
-        );
+    void getNullByNotBetweenStartAndEndInclusive() {
+        WinningNumbers actual = WinningNumbers.from(List.of(1, 2, 3, 4, 5, 46));
+        assertThat(actual).isEqualTo(null);
+    }
+
+    @DisplayName("당첨 번호가 1~45 사이 숫자이고, 중복되지 않는 6자리라면 해당 번호의 WinningNumbers가 생성된다.")
+    @Test
+    void getWinningNumbers() {
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+        WinningNumbers winningNumbers = WinningNumbers.from(expected);
+        List<Integer> actual = winningNumbers.getWinningNumbers();
+        assertThat(actual).isEqualTo(expected);
     }
 }

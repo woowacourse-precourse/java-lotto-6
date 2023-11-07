96004f09694328c13a6a0e742f32f016218cf1a5
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 04:08:47 +0900

    refactor(LottoController): 로또 티켓 인스턴스 변수 선언

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import lotto.domain.BonusNumber;
 import lotto.domain.LottoTickets;
+import lotto.domain.Result;
 import lotto.domain.TicketAmount;
 import lotto.domain.WinningNumber;
 import lotto.view.OutputView;
@@ -9,14 +10,18 @@ import lotto.view.OutputView;
 public class LottoController {
     private TicketAmount ticketAmount;
     private int ticketCount;
+    private LottoTickets lottoTickets;
     private WinningNumber winningNumber;
     private BonusNumber bonusNumber;
+    private Result result;
 
     public void run() {
         setTicketAmountAndCount();
         setLottoTickets();
         setWinningNumber();
         setBonusNumber();
+
+        calculateResult();
     }
 
     private void setTicketAmountAndCount() {
@@ -26,7 +31,7 @@ public class LottoController {
     }
 
     private void setLottoTickets() {
-        LottoTickets lottoTickets = new LottoTickets(ticketCount);
+        lottoTickets = new LottoTickets(ticketCount);
         OutputView.printLottoNumbers(ticketCount, lottoTickets);
     }
 
@@ -38,5 +43,10 @@ public class LottoController {
     private void setBonusNumber() {
         OutputView.printBonusNumberInputMessage();
         bonusNumber = InputController.setBonusNumber(winningNumber);
+    }
+
+    protected void calculateResult() {
+        OutputView.printWinningStatusMessage();
+        Result result = lottoTickets.calculateLottoRank(winningNumber, bonusNumber);
     }
 }

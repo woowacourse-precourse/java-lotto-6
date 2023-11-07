820e7da66f4f694cd9fce77640be0b2f413a70ea
little6523 <little6523@naver.com>
2023-11-08 03:07:39 +0900

    feat : 각 게임에서 당첨번호 및 보너스 번호와 몇 개 일치하는지 검사하는 기능 구현  - docs/README.md의 11번 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,50 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
 
     private static final int WIN_LOTTO_SIZE = 6;
+    private static final int LOTTO_RESULT_SIZE = 8;
+
+    private static Lotto winLotto;
+
+    private static int winBonusLottoNumber;
+    
+    private static List<Integer> correctCount = new ArrayList<>();
+    private static List<Integer> totalWinResult = new ArrayList<>();
+
+    enum lottoResult {
+        six(6, 0, 2_000_000_000, 0),
+        fiveAndBonus(5, 1, 30_000_000, 1),
+        five(5, 0, 1_500_000, 2),
+        four(4, 0, 50_000, 3),
+        three(3, 0, 5_000, 4),
+        two(2,0,0,5),
+        one(1,0,0,6),
+        nothing(0, 0, 0, 7);
+
+        private final int corretNumbers;
+        private final int bonusNumbers;
+        private final int winPrize;
+
+        private final int index;
+
+        lottoResult(int corretNumbers, int bonusNumbers, int winPrize, int index) {
+            this.corretNumbers = corretNumbers;
+            this.bonusNumbers = bonusNumbers;
+            this.winPrize = winPrize;
+            this.index = index;
+        }
+
+        public static lottoResult whatIsResult() {
+            return Arrays.stream(lottoResult.values())
+                    .filter(lottoResult -> lottoResult.corretNumbers == correctCount.get(0))
+                    .filter(lottoResult -> lottoResult.bonusNumbers == correctCount.get(1))
+                    .findFirst()
+                    .orElseThrow();
+        }
+    }
 
     private static int inputPurchaseAmount() {
         String userPurchaseAmount = Console.readLine();
@@ -51,7 +90,7 @@ public class Application {
 
     private static Lotto inputLottoNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(lottoNumbers);
+        //Collections.sort(lottoNumbers);
         Lotto myLotto = new Lotto(lottoNumbers);
         return myLotto;
     }
@@ -88,6 +127,37 @@ public class Application {
         return Integer.parseInt(Console.readLine());
     }
 
+    private static void whatIsMyResult(List<Lotto> myLottos, int purchaseNumber) {
+        lottoResult myResult;
+        for(int i = 0; i < purchaseNumber; i++) {
+            initializeCorrectCount();
+            howManyCorrectLottoNumber(myLottos.get(i)); // 몇 개 맞았는지 검사 correctcount에 저장
+            myResult = lottoResult.whatIsResult(); //six, five ... 당첨 결과
+            int index = myResult.index; // 당첨 결과에 해당하는 인덱스 six = 0, fiveandbonus = 1 ...
+            totalWinResult.set(index, totalWinResult.get(index) + 1); // 당첨 개수 카운트
+        }
+    }
+
+    private static void initializeCorrectCount() {
+        correctCount.set(0, 0);
+        correctCount.set(1, 0);
+    }
+
+    private static void howManyCorrectLottoNumber(Lotto myLotto) {
+        int count = 0;
+        List<Integer> winLottoNumber = winLotto.getLottoNumbers();
+        List<Integer> myLottoNumber = myLotto.getLottoNumbers();
+        for (int i = 0; i < WIN_LOTTO_SIZE; i++) {
+            if (myLottoNumber.contains(winLottoNumber.get(i))) {
+                count += 1;
+            }
+        }
+        correctCount.set(0, count);
+        if (count == 5 && myLottoNumber.contains(winBonusLottoNumber)) {
+            correctCount.set(1, 1);
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
@@ -100,11 +170,20 @@ public class Application {
         printMyLottos(myLottos, purchaseNumber);
 
         System.out.println("당첨 번호를 입력해 주세요.");
-        Lotto winLotto = inputWinLottoNumbers();
-        winLotto.printLottoNumbers();
+        winLotto = inputWinLottoNumbers();
         System.out.println();
 
         System.out.println("보너스 번호를 입력해 주세요.");
-        int winBonusNumber = inputBonusLottoNumber();
+        winBonusLottoNumber = inputBonusLottoNumber();
+        
+        // 로또 번호 맞은 개수 카운트하는 리스트 초기화
+        for (int i = 0; i < 2; i++) {
+            correctCount.add(0);
+        }
+        for (int i = 0; i < LOTTO_RESULT_SIZE; i++) {
+            totalWinResult.add(0);
+        }
+
+        whatIsMyResult(myLottos, purchaseNumber);
     }
 }

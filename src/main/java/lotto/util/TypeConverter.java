a7da3ad98964b465ce194305a4955982e3b4d1ac
Dreaming-J <kimjjong321@naver.com>
2023-11-07 20:36:26 +0900

    :pencil2: fix. TypeConverter 오타 수정

--- src/main/java/lotto/util/TypeConverter.java
+++ src/main/java/lotto/util/TypeConverter.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 
 public class TypeConverter {
     private static final String WRONG_NUMBER_INPUT_MSG = "숫자만 입력해주세요. 복수 입력 시 쉼표(,)로 구분해주세요.";
-    private static final String DELIMiTER = ",";
+    private static final String DELIMITER = ",";
 
     public static int toInt(String input) {
         try {
@@ -16,7 +16,7 @@ public class TypeConverter {
     }
 
     public static List<Integer> toList(String input) {
-        return Stream.of(input.split(DELIMiTER))
+        return Stream.of(input.split(DELIMITER))
                 .map(TypeConverter::toInt)
                 .toList();
     }

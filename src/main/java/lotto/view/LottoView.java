bce674c102374951009dbffbb6e581f07537cd59
swhyeon98 <smreo75@gmail.com>
2023-11-07 21:26:00 +0900

    feat: 당첨 통계를 출력하는 기능 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,8 +2,10 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -38,8 +40,22 @@ public class LottoView {
     }
 
     public int inputBonusNumber() {
-        String input = input("보너스 번호를 입력해 주세요.");
+        String input = input("\n보너스 번호를 입력해 주세요.");
         return Integer.parseInt(input);
+    }
+
+    public void printResult(Map<Rank, Long> results, double profitRate) {
+        System.out.println("\n당첨 통계\n---------");
+        printRankResult(results, Rank.FIFTH, "3개 일치 (5,000원) - %d개\n");
+        printRankResult(results, Rank.FOURTH, "4개 일치 (50,000원) - %d개\n");
+        printRankResult(results, Rank.THIRD, "5개 일치 (1,500,000원) - %d개\n");
+        printRankResult(results, Rank.SECOND, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n");
+        printRankResult(results, Rank.FIRST, "6개 일치 (2,000,000,000원) - %d개\n");
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+    }
+
+    private void printRankResult(Map<Rank, Long> results, Rank rank, String format) {
+        System.out.printf(format, results.getOrDefault(rank, 0L));
     }
 
     private void validateIsNumber(String input) {

50c738fcc733083f50ae41fc62eb2983cd0110c3
박정현 <ros28081@naver.com>
2023-11-07 18:25:38 +0900

    feat(LottoRankManagement): 당첨 결과 출력 포맷에 맞춰 수정
    - 3개 일치부터 출력되도록 수정
    - 보너스 볼 일치 구문 띄어쓰기 삭제

--- src/main/java/lotto/LottoRankManagement.java
+++ src/main/java/lotto/LottoRankManagement.java
@@ -23,10 +23,11 @@ public class LottoRankManagement {
     public void printResult() {
         System.out.println("당첨 통계");
         System.out.println("---");
-        for (LottoRank rank : LottoRank.values()) {
-            int count = rankCounts.get(rank);
-            if (rank != LottoRank.NONE && count > 0) {
-                System.out.printf("%d개 일치 %s (%,d원) - %d개\n",
+        for (int i = LottoRank.values().length - 1; i >= 0; i--) {
+            LottoRank rank = LottoRank.values()[i];
+            if (rank != LottoRank.NONE) {
+                int count = rankCounts.get(rank);
+                System.out.printf("%d개 일치%s (%,d원) - %d개\n",
                         rank.getMatchCount(), rank == LottoRank.SECOND ? ", 보너스 볼 일치" : "",
                         rank.getPrize(), count);
             }

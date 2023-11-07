f8debbfc1dda833837a19939d56485173433dda1
Hajuuu <hpt0127@naver.com>
2023-11-07 17:06:53 +0900

    refactor(Lotto): 메서드 수정 및 startLotto 메서드 추가
    Application에 있던 createLotto 메서드와 printLotto메서드 Lotto 클래스로 이동,
    로또 생성과 당첨 번호 입력, 당첨 번호 비교 메서드를 호출하는 startLotto 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -15,6 +17,7 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
     }
 
     // TODO: 추가 기능 구현
@@ -22,6 +25,31 @@ public class Lotto {
     public static List<Integer> randomNumber() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return randomNumbers;
+    }
+
+    public static void startLotto(int numberOfLotto) {
+        List<Lotto> lottos = createLotto(numberOfLotto);
+        printLotto(lottos);
+
+        WinningNumber winningNumber = new WinningNumber();
+        winningNumber.inputWinningNumber();
+        winningNumber.compareToLotto(lottos);
+    }
+
+    public static List<Lotto> createLotto(int numberOfLotto) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> lotto = Lotto.randomNumber();
+            Collections.sort(lotto);
+            lottos.add(new Lotto(lotto));
+        }
+        return lottos;
+    }
+
+    public static void printLotto(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 
     @Override

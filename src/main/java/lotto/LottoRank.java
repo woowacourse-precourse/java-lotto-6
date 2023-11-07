7ef46345d1c1cc767c289cc3620bef5095feff19
choiejae <ejae8319@gmail.com>
2023-11-07 20:47:52 +0900

    feat(LottoRank): 로또 랭크 기능 구현
    - 로또 결과의 정보를 저장한다
    - ofMatchNumber(): 일치 개수와 보너스 번호를 통해 적절한 LottoRank 를 반환한다

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchNumber;
+    private final long winningAmount;
+
+    LottoRank(int matchNumber, long winningAmount) {
+        this.matchNumber = matchNumber;
+        this.winningAmount = winningAmount;
+    }
+
+    public static LottoRank ofMatchNumber(final int matchNumber, boolean isBonus) {
+        LottoRank lottoRank = findMatchNumber(matchNumber);
+
+        if (lottoRank == SECOND) {
+            if (isBonus) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+        return lottoRank;
+    }
+
+    private static LottoRank findMatchNumber(final int matchNumber) {
+        return Arrays.stream(values())
+                .filter(lottoRank -> lottoRank.matchNumber == matchNumber)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
+    }
+
+    public long getWinningAmount() {
+        return winningAmount;
+    }
+}

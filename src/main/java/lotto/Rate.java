1f159f3695c1a277173699c9c1431e1bdcfe31b6
dragon0622 <iamdragon.dev@gmail.com>
2023-11-08 08:06:52 +0900

    fix(Lotto) : 클래스 내부 로직 수정

--- src/main/java/lotto/Rate.java
+++ src/main/java/lotto/Rate.java
@@ -2,15 +2,16 @@ package lotto;
 public class Rate {
 
     public static double calculateRate(){
-        double rate;
-        int price = 0;
-        int cnt = 0;
+        double rate=0;
+        double price = 0;
+        double cnt = 0;
 
         for (Rank rank : Rank.values()){
             price = rank.getPrice();
             cnt = rank.getCnt();
+            rate += price * cnt;
         }
-        rate = ((price * cnt) / (double)Input.money) * 100;
+        rate = (rate / (double)Input.money) * 100;
         return rate;
     }
 }

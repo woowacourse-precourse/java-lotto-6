ed5e0d1a50c402ce0b39e55255caea341026101a
jhva <junghun8158@naver.com>
2023-11-07 15:37:18 +0900

    :recycle: refactor: 수익률 상수 메시지 추가

--- src/main/java/lotto/constants/LottoConstants.java
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -7,7 +7,10 @@ public class LottoConstants {
     public static final int LOTTO_MIN_LENGTH = 1;
     public static final int LOTTO_DIVISION = 1000;
     public static final int LOTTO_NOT_DIVISION = 0;
+    public static final int START = 3;
+    public static final int END = 6;
     public static final String WINNING = "당첨";
     public static final String BONUS = "보너스";
     public static final String DIVIDER = "---";
+    public static final String PATTERN = "#.0%";
 }

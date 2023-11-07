263da4d81d0e2ee13472d34dd97be6c77738e0c5
sungmin306 <another0306@naver.com>
2023-11-08 05:09:55 +0900

    Feat: 일치하는 당첨 통계 출력 기능 HashMap을 세팅하는 setTotalRanking을 만들어서 static으로 객체 생성 전에 세팅하게 만든 후 saveTotalRanking을 통해 당첨된 개수를 map 에 저장 이떄 map에 key값은 Ranking value는 Integer로 만든다. values 메서드를 통해 MISS를 제외한 Ranking에 있는 것을 하나씩 출력하고 map에 key 값을 통해 당첨된 로또 개수를 가져온다.

--- src/main/java/view/Ranking.java
+++ src/main/java/view/Ranking.java
@@ -2,11 +2,11 @@ package view;
 
 public enum Ranking {
 
-    FIRST(6, 2_000_000_000, "6개 일치(2,000,000,000원 - "),
-    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "), // 2등
-    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - "), // 3등
-    FOURTH(4, 50_000, "4개 일치 (50,000원) - "), // 4등
     FIFTH(3, 5_000, "3개 일치 (5,000원) - "), // 5등
+    FOURTH(4, 50_000, "4개 일치 (50,000원) - "), // 4등
+    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - "), // 3등
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "), // 2등
+    FIRST(6, 2_000_000_000, "6개 일치(2,000,000,000원 - "), // 1등
     MISS(0, 0, "");
 
     private int matchingCount;
@@ -44,10 +44,15 @@ public enum Ranking {
     }
 
     public static Ranking value(int matchingCount, boolean bonusCount) {
-        if (matchingCount < 3) {
-            return MISS;
+        if ((matchingCount == SECOND.matchingCount) && (bonusCount == true)) {
+            return SECOND;
         }
-        return FIRST;
+        for (Ranking ranking : Ranking.values()) {
+            if (ranking.matchingCount == matchingCount) {
+                return ranking;
+            }
+        }
+        return MISS;
     }
 
 
--- src/main/java/view/WinningStatistics.java
+++ src/main/java/view/WinningStatistics.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.HashMap;
 import java.util.List;
 import lotto.Lotto;
 
@@ -8,6 +9,7 @@ public class WinningStatistics {
     private Ranking ranking;
     private float rateOfReturn;
     private int matchingCount;
+    private static HashMap<Ranking, Integer> totalRanking;
 
     public WinningStatistics(Lotto resultLotto, Lotto userLotto) { // Test 용 생성자
         setMatchingCount(resultLotto, userLotto);
@@ -25,10 +27,13 @@ public class WinningStatistics {
     }
 
     private void checkLotto(Lotto resultLotto, List<Lotto> lotties, int bonusNumber) {
+        setTotalRanking(ranking);
         for (int i = 0; i < lotties.size(); i++) {
             setMatchingCount(resultLotto, lotties.get(i));
-            this.ranking = ranking.value(matchingCount, checkBonusNumber(resultLotto, bonusNumber));
+            ranking = ranking.value(matchingCount, checkBonusNumber(resultLotto, bonusNumber));
+            saveTotalRanking(ranking);
         }
+        printTotalRanking();
     }
 
     private void setMatchingCount(Lotto resultLotto, Lotto userLotto) {
@@ -44,5 +49,29 @@ public class WinningStatistics {
                 .anyMatch(number -> number == bonusNumber);
     }
 
+    private static void setTotalRanking(Ranking ranking) {
+        totalRanking = new HashMap<>();
+        totalRanking.put(Ranking.FIRST, 0);
+        totalRanking.put(Ranking.SECOND, 0);
+        totalRanking.put(Ranking.THIRD, 0);
+        totalRanking.put(Ranking.FOURTH, 0);
+        totalRanking.put(Ranking.FIFTH, 0);
+        totalRanking.put(Ranking.MISS, 0);
 
+    }
+
+    private void saveTotalRanking(Ranking ranking) {
+        int currentCount = totalRanking.get(ranking);
+        totalRanking.put(ranking, currentCount + 1);
+    }
+
+    private void printTotalRanking() {
+        for (Ranking ranking : ranking.values()) {
+            if (ranking == Ranking.MISS) {
+                continue;
+            }
+            System.out.println(ranking.getWinningMessage() + totalRanking.get(ranking) + "개");
+        }
+
+    }
 }

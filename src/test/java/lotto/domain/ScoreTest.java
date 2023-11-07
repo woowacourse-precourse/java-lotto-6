b2c5d0f5d008570feb02b713e5b709d4b9700142
gusdn0108 <gusdn6671@naver.com>
2023-11-07 14:17:38 +0900

    test(Score) : Score 클래스 의 Calculate 테스트 로또 결과를 합산 하여 결과 생성

--- src/test/java/lotto/domain/ScoreTest.java
+++ src/test/java/lotto/domain/ScoreTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -34,6 +35,34 @@ class ScoreTest {
             expectedTotalProfit += result.getWinnings() * scoreMap.get(result);
         }
         assertEquals(expectedTotalProfit, totalProfit);
+    }
+    @DisplayName("Score 클래스 의 Calculate 테스트 로또 결과를 합산 하여 결과 생성")
+    @Test
+    public void testCalculate() {
+        List<Lotto> lottos = List.of(
+                // 6개 매치 7, 8, 9, 10, 11, 12
+                new Lotto(List.of(7, 8, 9, 10, 11, 12)),
+                // 5개 + 보너스 7, 8, 9, 10, 11, 12 /  보너스 : 6
+                new Lotto(List.of(6, 7, 8, 9, 11, 12)),
+                // 5개  7, 8, 9, 10, 11, 12
+                new Lotto(List.of(7, 8, 9, 10, 11, 13)),
+                // 4개 7, 8, 9, 10, 11, 12
+                new Lotto(List.of(7, 8, 9, 10, 17, 18)),
+                // 3개
+                new Lotto(List.of(7, 8, 9, 16, 17, 18))
+        );
+        WinningNumber winningNumber = new WinningNumber(new Lotto(List.of(7, 8, 9, 10, 11, 12)), 6);
+        Lotties lotties = new Lotties(lottos);
+
+        Map<Result, Integer> scoreMap = Score.calculate(winningNumber, lotties);
+
+        assertEquals(1, scoreMap.get(Result.SIX_MATCH));
+        assertEquals(1, scoreMap.get(Result.FIVE_AND_BONUS_MATCH));
+        assertEquals(1, scoreMap.get(Result.FIVE_MATCH));
+        assertEquals(1, scoreMap.get(Result.FOUR_MATCH));
+        assertEquals(1, scoreMap.get(Result.THREE_MATCH));
+
+
     }
 
 }

a8fc0dbacd1a474f74185b4d37cbf6cd9210049f
freekyr7529 <freekyr7529@gmail.com>
2023-11-07 21:50:35 +0900

    feat: Answer 클래스에 결과를 반환하는 몌서드 추가
    - 매개변수로 주어진 Answer 객체가 List에 있는 숫자를 포함하고 있는지 확인한 후 그 결과를 Result 객체로 반환하는 compareNumberToAnswer 메서드를 추가했다.
    - Answer 객체의 당첨 번호에 해당 숫자가 있는지 확인한 후 0 또는 1을 반환하는 searchHitNumbers 메서드를 추가했다.
    - Answer 객체의 보너스 번호가 해당 숫자와 일치하는지 확인한 후 0 또는 1을 반환하는 searchHitNumbers 메서드를 추가했다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -20,4 +20,30 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
     }
+
+    public Result compareNumberToAnswer(Answer answer) {
+        int hitResult = 0;
+        int bonusResult = 0;
+
+        for (int number : numbers) {
+            hitResult += searchHitNumbers(answer, number);
+            bonusResult += searchBonusNumber(answer, number);
+        }
+
+        return new Result(hitResult, bonusResult);
+    }
+
+    private int searchHitNumbers(Answer answer, int number) {
+        if (answer.isHitNumbersHaveThisNumber(number)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private int searchBonusNumber(Answer answer, int number) {
+        if (answer.isBonusNumberTheSameAsThis(number)) {
+            return 1;
+        }
+        return 0;
+    }
 }

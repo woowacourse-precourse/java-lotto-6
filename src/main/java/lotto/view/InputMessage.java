7854b09f7517fff0a6b6feeda4d9aff30f53a795
kseenyoung <kseenyoung@gmail.com>
2023-11-07 22:52:10 +0900

    add: 보너스 번호 입력 메시지 추가

--- src/main/java/lotto/view/InputMessage.java
+++ src/main/java/lotto/view/InputMessage.java
@@ -3,7 +3,8 @@ package lotto.view;
 public enum InputMessage {
 
     INPUT_MONEY("구입금액을 입력해 주세요."),
-    REQUEST_WIN_LOTTO("당첨 번호를 입력해 주세요.");
+    REQUEST_WIN_LOTTO("당첨 번호를 입력해 주세요."),
+    REQUEST_BONUS_LOTTO("보너스 번호를 입력해 주세요.");
 
 
     final String msg;

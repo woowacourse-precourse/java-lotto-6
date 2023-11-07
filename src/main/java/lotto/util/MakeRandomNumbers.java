4a1d2f1cf91bfbac2470462e530440f874aacb57
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-08 03:19:00 +0900

    refactor: 하드코딩 번호들 상수로 수정

--- src/main/java/lotto/util/MakeRandomNumbers.java
+++ src/main/java/lotto/util/MakeRandomNumbers.java
@@ -6,8 +6,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MakeRandomNumbers {
+
+    private static final int MIN_NUM = 1;
+    private static final int MAX_NUM = 45;
+    private static final int SIZE = 6;
+
     public static List<Integer> getRandomNumbers(){
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, SIZE);
         return randomNumbers.stream()
                 .sorted()
                 .collect(Collectors.toList());

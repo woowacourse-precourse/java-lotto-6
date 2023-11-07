a2a7bb11a5f068764bf93a882d467750de829283
oosedus <seayeon0729@gmail.com>
2023-11-08 06:02:24 +0900

    feat: 등수별 당첨 금액 계산 함수 추가

---
+++ src/main/java/lotto/PrizeCalculaor.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrizeCalculaor {
+    private final static int THREE_WINNING_PRIZE = 5000;
+    private final static int FOUR_WINNING_PRIZE = 50000;
+    private final static int FIVE_WINNING_PRIZE = 15000000;
+    private final static int FIVE_BONUS_WINNING_PRIZE = 30000000;
+    private final static int SIX_WINNING_PRIZE = 2000000000;
+
+    List<Integer> calculatePrize(List<Integer> countsOfWonLotto){
+        List<Integer> prizes = new ArrayList<>();
+        prizes.add(countsOfWonLotto.get(0)*THREE_WINNING_PRIZE);
+        prizes.add(countsOfWonLotto.get(1)*FOUR_WINNING_PRIZE);
+        prizes.add(countsOfWonLotto.get(2)*FIVE_WINNING_PRIZE);
+        prizes.add(countsOfWonLotto.get(3)*FIVE_BONUS_WINNING_PRIZE);
+        prizes.add(countsOfWonLotto.get(4)*SIX_WINNING_PRIZE);
+        return prizes;
+    }
+}

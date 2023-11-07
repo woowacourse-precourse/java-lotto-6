f98091b5232e0107994c7374c860347b81c22ff3
JooDukHan <bfinecpa1@naver.com>
2023-11-07 16:08:56 +0900

    feat(로또 상수): 로또 상수 구현

---
+++ src/main/java/lotto/Constants/LottoConstant.java
@@ -0,0 +1,15 @@
+package lotto.Constants;
+
+public class LottoConstant {
+
+    public static final Integer LOTTO_NUMBER_LENGTH = 6;
+
+    public static final Integer ONE = 1;
+    public static final Integer MIN_LOTTO_NUMBER = 1;
+    public static final Integer MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int INIT_COUNT = 0;
+    public static final int TWO_BILLION = 2_000_000_000;
+    public static final String DIGIT_REGEX = "[0-9]*";
+
+}

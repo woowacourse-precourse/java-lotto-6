cef92bae061a6ebcf8f1fc195501b91a6a9c2885
dj <fjzl12@naver.com>
2023-11-08 02:05:06 +0900

    refactor(PrizeNumberValidator): 당첨 번호 정렬 기능 추가

--- src/main/java/lotto/domain/PrizeNumbersValidator.java
+++ src/main/java/lotto/domain/PrizeNumbersValidator.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,7 @@ public class PrizeNumbersValidator {
         isNumber();
         isCorrectNumber();
         isDuplicated();
+        Collections.sort(this.numbers);
     }
 
     private void canSplitComma(String numbers) {

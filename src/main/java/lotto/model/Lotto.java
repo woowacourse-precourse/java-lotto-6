2d964418652ecfebf76c515bc0311bc8ff956260
jun <ggprgrkjh@naver.com>
2023-11-07 15:22:45 +0900

    refactor(lotto): 매직넘버 제거

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,6 +6,10 @@ import java.util.stream.Collectors;
 import lotto.view.InputView;
 
 public class Lotto {
+
+    private static final int NUMBER_SIZE = 6;
+    private static final String SIX_NUMBER_ERROR = "[ERROR] 번호가 6자리가 아닙니다.";
+    private static final String DISTINCT_NUMBER_ERROR = "[ERROR] 로또 번호에 중복된 숫자가 발생했습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,15 +19,15 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != NUMBER_SIZE) {
+            throw new IllegalArgumentException(SIX_NUMBER_ERROR);
         }
     }
 
     private void validateDuplicateNumber(List<Integer> numbers) {
         List<Integer> distinctNumber = numbers.stream().distinct().collect(Collectors.toList());
         if (numbers.size() != distinctNumber.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DISTINCT_NUMBER_ERROR);
         }
     }
 

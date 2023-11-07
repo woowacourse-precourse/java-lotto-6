830b2cdef204be2da8a45fcee0303ade6f7c6587
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-08 03:15:10 +0900

    feat: exception 발생 추가 모든 조건들을 통과하면 true 를 반환함으로써 무한 루프 입력을 종료할 수 있다.

--- src/main/java/lotto/utils/ValidationImpl.java
+++ src/main/java/lotto/utils/ValidationImpl.java
@@ -4,64 +4,91 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
+import lotto.exception.IllegalInputException;
 
 public class ValidationImpl implements Validation {
     private final String DELIMITER = ",";
     private final int NUMBER_START = 1;
     private final int NUMBER_END = 45;
     private final Pattern WINNING_NUMBER_PATTERN = Pattern.compile("[^,\\d]");
-    private final Pattern BONUS_NUMBER_PATTERN = Pattern.compile("\\D");
-    private final Pattern MONEY_PATTERN = Pattern.compile("\\D");
+    private final Pattern DIGITS_PATTERN = Pattern.compile("\\D");
 
     @Override
     public boolean isValidWinningNumbers(String input) {
-        if (hasSpecialCharExceptComma(input)) {
-            return false;
-        }
-        String[] strings = input.split(DELIMITER);
-        for (String string : strings) {
-            if (!existDigitBetweenComma(string)
-                    || !isBetween(Integer.parseInt(string))) {
-                return false;
+        try {
+            hasSpecialCharExceptCommaDigits(input);
+            String[] strings = input.split(DELIMITER);
+            for (String string : strings) {
+                existDigitBetweenComma(string);
+                isBetween(Integer.parseInt(string));
             }
+            isDistinctNumbers(input);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
         }
-        return isDistinctNumbers(input);
+        return true;
     }
 
-    private boolean hasSpecialCharExceptComma(String input) {
-        return WINNING_NUMBER_PATTERN.matcher(input).find();
+    private void hasSpecialCharExceptCommaDigits(String input) throws IllegalArgumentException {
+        if (WINNING_NUMBER_PATTERN.matcher(input).find()) {
+            throw new IllegalArgumentException("[ERROR] 오직 숫자와 콤마만 입력 가능합니다.");
+        }
     }
 
-    private boolean existDigitBetweenComma(String string) {
-        return string.length() >= 1;
+    private void existDigitBetweenComma(String stringDigit) throws IllegalArgumentException {
+        if (stringDigit.length() < 1) {
+            throw new IllegalArgumentException("[ERROR] 콤마 사이에 숫자를 입력해주세요.");
+        }
     }
 
-    private boolean isDistinctNumbers(String input) {
+    private void isDistinctNumbers(String input) throws IllegalArgumentException {
         String[] strings = input.split(",");
         Set<Integer> distinctNumbers = new HashSet<>();
         for (String string : strings) {
             distinctNumbers.add(Integer.parseInt(string));
         }
-        return distinctNumbers.size() >= strings.length;
+        if (distinctNumbers.size() < strings.length) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
     }
 
-    private boolean isBetween(int number) {
-        return number >= NUMBER_START && number <= NUMBER_END;
+    private void isBetween(int number) throws IllegalArgumentException {
+        if (number < NUMBER_START || number > NUMBER_END) {
+            throw new IllegalArgumentException("[ERROR] 번호의 범위는 1 ~ 45 사이 입니다.");
+        }
     }
 
     @Override
     public boolean isValidBonusNumber(String input, List<Integer> winningNumbers) {
-        return !BONUS_NUMBER_PATTERN.matcher(input).find()
-                && isBetween(Integer.parseInt(input))
-                && isDuplicatedInput(input, winningNumbers);
+        try {
+            hasAllCharExceptDigits(input);
+            isBetween(Integer.parseInt(input));
+            isDuplicatedInput(input, winningNumbers);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
-    private boolean isDuplicatedInput(String input, List<Integer> winningNumbers) {
-        return !winningNumbers.contains(Integer.parseInt(input));
+    private void hasAllCharExceptDigits(String input) throws IllegalArgumentException {
+        if (DIGITS_PATTERN.matcher(input).find()) {
+            throw new IllegalArgumentException("[ERROR] 오직 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private void isDuplicatedInput(String input, List<Integer> winningNumbers) throws IllegalArgumentException {
+        if (winningNumbers.contains(Integer.parseInt(input))) {
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자가 이미 있습니다.");
+        }
     }
 
     @Override
     public boolean isValidMoney(String input) {
-        return !MONEY_PATTERN.matcher(input).find();
+        try {
+            hasAllCharExceptDigits(input);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }

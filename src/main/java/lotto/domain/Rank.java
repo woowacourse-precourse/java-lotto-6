eaa505a494aa0e9c37a8a97275e9616b858ebb88
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 19:14:49 +0900

    refactor : 등수별 메시지 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,19 +1,20 @@
 package lotto.domain;
-
+import lotto.view.OutputView;
 public enum Rank {
 
-    FIRST(6, 2000000000),
-    SECOND(5,30000000),
-    THRID(5,1500000),
-    FOURTH(4,50000),
-    FIFTH(3,5000),
-    NOTHING(0,0);
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(5,30000000,"5개 일치 (30,000,000원) - "),
+    THRID(5,1500000, "5개 일치 (1,500,000원) - "),
+    FOURTH(4,50000,"4개 일치 (50,000원) - "),
+    FIFTH(3,5000, "3개 일치 (5,000원) - "),
+    NOTHING(0,0, "");
 
     private static final int MIN_MATCH_COUNT=3;
     private static final String ERROR_MESSAGE="[ERROR]";
 
     private int matchCount;
     private int winningMoney;
+    private String message;
 
     public static Rank valueOf(int matchNum, boolean matchBonus){
         if(matchNum<MIN_MATCH_COUNT){
@@ -33,10 +34,11 @@ public enum Rank {
         throw new IllegalArgumentException(ERROR_MESSAGE);
     }
 
-    Rank(int matchCount, int winningMoney)
+    Rank(int matchCount, int winningMoney, String message)
     {
         this.matchCount=matchCount;
         this.winningMoney= winningMoney;
+        this.message = message;
     }
 
     public int getMatchCount(){
@@ -49,6 +51,13 @@ public enum Rank {
 
     private boolean match(int countOfMatch){
         return matchCount ==countOfMatch;
+    }
+
+
+    public void printMessage(int count){
+        if(this !=NOTHING){
+            OutputView.printSuccessMessage(message, count);
+        }
     }
 
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,21 @@
 package lotto.view;
 
+import java.sql.SQLOutput;
+
 public class OutputView {
     public static final String TICKET_COUNT="개를 구매했습니다.";
+    public static final String WINNING_STATICS_MESSAGE="당첨 통계";
     public static void printTicketCnt(int count){
         System.out.println(count+TICKET_COUNT);
     }
+
+    public static void printSucecessResult(){
+        System.out.println(WINNING_STATICS_MESSAGE);
+        System.out.println("---");
+    }
+
+    public static void printSuccessMessage(String message, int numberOfMatch){
+        System.out.println(message + numberOfMatch +"개");
+    }
+
 }

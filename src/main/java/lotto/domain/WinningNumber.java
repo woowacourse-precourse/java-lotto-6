09cf380025777a2b162ac7bd3c724efd4185b61f
Minyongggg <naminyong97@gmail.com>
2023-11-08 02:35:50 +0900

    feat: WinningNumber 생성자 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,4 +1,11 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumber {
+    private List<Integer> numbers;
+
+    public WinningNumber(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
 }

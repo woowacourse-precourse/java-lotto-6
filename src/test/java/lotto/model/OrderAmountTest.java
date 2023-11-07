20822703203817959feb4d86679a84adc2b7c4a8
HongYeseul <ghd8119@naver.com>
2023-11-07 13:39:24 +0900

    :white_check_mark: test(OrderAmount): OrderAmount 생성자 내 입력값 검사로 달라진 부분 수정 : Integer -> String

--- src/test/java/lotto/model/OrderAmountTest.java
+++ src/test/java/lotto/model/OrderAmountTest.java
@@ -9,7 +9,7 @@ class OrderAmountTest {
     @Test
     @DisplayName("로또 금액을 정상적인 가격으로 입력")
     void orderNormalPrice(){
-        OrderAmount order = new OrderAmount(8000);
+        OrderAmount order = new OrderAmount("8000");
         int orderAmount = order.amountOfLotto();
         assertThat(orderAmount).isEqualTo(8);
     }
@@ -17,7 +17,7 @@ class OrderAmountTest {
     @Test
     @DisplayName("로또 수익률 계산")
     void calculateProfit(){
-        OrderAmount order = new OrderAmount(8000);
+        OrderAmount order = new OrderAmount("8000");
         LottoResults results = new LottoResults( 0, 0, 0, 0, 1);
         assertThat(order.calculateProfit(results)).isEqualTo(62.5);
     }

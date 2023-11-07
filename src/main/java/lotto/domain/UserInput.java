f5fa847b175636e762f9282e014d7a037c01d89f
HYOAN <aspqll@naver.com>
2023-11-08 01:41:46 +0900

    feat(lotto): 보너스번호 입력기능 구현, 필요없는 코드 삭제

--- src/main/java/lotto/domain/UserInput.java
+++ src/main/java/lotto/domain/UserInput.java
@@ -17,26 +17,19 @@ public class UserInput {
 
     public int getUserPurchaseAmount(UserInputChecker checker, String inputPurchaseAmount) { // 구입금액
         int purchaseAmount = checker.checkPurchaseAmount(inputPurchaseAmount);
-        if (purchaseAmount == 0)
-            return 0;
-        return purchaseAmount;
+        return purchaseAmount; // 잘못된 값이면 0
     }
 
     public List<Integer> getUserWinningNumbers(UserInputChecker checker, String inputWinningNumbers) { // 당첨번호
         List<Integer> checkedNumbers = new ArrayList<>();
         String[] winnigNumbers = inputWinningNumbers.split(",");
         checkedNumbers = checker.checkWinningNumbers(winnigNumbers);
-        if (checkedNumbers == null)
-            return null;
-
-        return checkedNumbers;
+        return checkedNumbers; // 잘못된 값이면 null
     }
 
     public int getUserBonusNumber(UserInputChecker checker, String inputBonusNumber) { // 보너스번호
-        checker.checkBonusNumber(inputBonusNumber);
-
-
-        return 0;
+        int bonusNumber = checker.checkBonusNumber(inputBonusNumber);
+        return bonusNumber; // 잘못된 값이면 0
     }
 
 }

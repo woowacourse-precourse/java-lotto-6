7ea927a04eff6a8ec220d78e195baa026b146df6
정찬영 <jcy0342@gmail.com>
2023-11-07 17:23:02 +0900

    test:  Consumer 객체 생성 중복 제거
    - BeforeEach로 각 테스트 별로 새로운 Consumer 객체로 초기화

--- src/test/java/lotto/domain/ConsumerTest.java
+++ src/test/java/lotto/domain/ConsumerTest.java
@@ -3,18 +3,22 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ConsumerTest {
+    Consumer consumer;
+
+    @BeforeEach
+    void setUp(){
+        consumer = new Consumer();
+    }
 
     @DisplayName("구매 금액을 저장한다.")
     @Test
     void savePurchaseAmount() {
-        //Given
-        Consumer consumer = new Consumer();
         //When
         consumer.setPurchaseAmount(5000);
         //Then
@@ -24,7 +28,6 @@ public class ConsumerTest {
     @DisplayName("구매 금액이 1000으로 나누어 떨어지지 않는다면 예외가 발생한다.")
     @Test
     void savePurchaseAmountByNotDivided() {
-        Consumer consumer = new Consumer();
         assertThatThrownBy(() -> consumer.setPurchaseAmount(5001))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구매 금액은 1000원 단위로 입력해주세요.");
@@ -34,7 +37,6 @@ public class ConsumerTest {
     @Test
     void saveLottos() {
         //Given
-        Consumer consumer = new Consumer();
         List<Lotto> lottos = List.of(
                 new Lotto(List.of(1,2,3,4,5,6)),
                 new Lotto(List.of(1,2,3,4,5,6))
@@ -49,7 +51,6 @@ public class ConsumerTest {
     @Test
     void saveLottosByMismatchCount() {
         //Given
-        Consumer consumer = new Consumer();
         consumer.setPurchaseAmount(2000);
 
         List<Lotto> lottos = List.of(

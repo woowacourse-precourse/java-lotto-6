2de8ad8ead9f6ef9b29a4d05a96a91832bcdd0b8
HYUNJUN\sajun <sajun28@naver.com>
2023-11-07 12:56:15 +0900

    Refactor: 'enum 수정"

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -37,26 +37,6 @@ public class LottoResult {
             money = moneyOne;
         }
     }
-    enum Transportation {
-        BUS(100) {
-            int fare(int distance){
-                return distance * BASIC_FARE;
-            }
-        },
-        TRAIN(150) {
-            int fare(int distance){
-                return distance * BASIC_FARE;
-            }
-        };
-        abstract int fare(int distance);
-        protected final int BASIC_FARE;
-        Transportation(int basicFare){
-            BASIC_FARE = basicFare;
-        }
-        public int getBasicFare(){
-            return BASIC_FARE;
-        }
-    }
 
     public static HashMap statistics(int count, int choice) {
         if (count == 3) {

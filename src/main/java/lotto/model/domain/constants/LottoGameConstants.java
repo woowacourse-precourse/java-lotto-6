c9cddbd1edd7b6f846ccd22c9068fe18de8e4763
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-07 19:09:06 +0900

    feat: 보너스 매치의 개수를 가지고있는 상수 추가

--- src/main/java/lotto/model/domain/constants/LottoGameConstants.java
+++ src/main/java/lotto/model/domain/constants/LottoGameConstants.java
@@ -5,7 +5,8 @@ public enum LottoGameConstants {
     MIN_NUMBER(1),
     MAX_NUMBER(45),
     NUMBERS_PER_LOTTO(6),
-    PRICE_PER_LOTTO(1000)
+    PRICE_PER_LOTTO(1000),
+    BONUS_COUNT(1) // 보너스를 포함하는 당첨의 개수
     ;
 
     private final int value;

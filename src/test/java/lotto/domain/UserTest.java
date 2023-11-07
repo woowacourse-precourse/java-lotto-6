9e42c36176f468170ebeb1ac7eb13fb63c344423
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-07 18:57:11 +0900

    test(UserTest) : 유저 로또 구매 테스트 코드 작성

--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.Constants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,5 +16,17 @@ public class UserTest {
         User user = new User(buyingPrice);
         // then
         assertThat(user.getBuyingPrice()).isEqualTo(buyingPrice);
+    }
+
+    @DisplayName("유저 로또 구매 테스트")
+    @Test
+    public void userSetLottoTest() {
+        // given
+        int buyingPrice = 3000;
+        User user = new User(buyingPrice);
+        // when
+        user.setLottos(user.getBuyingPrice);
+        // then
+        assertThat(user.getLottos().size()).isEqualTo(buyingPrice / Constants.LOTTO_PRICE);
     }
 }

735dbea4ef4804651af1aeeaf5eed29df0ef49c4
권장순 <jsoonworld@gmail.com>
2023-11-07 23:48:05 +0900

    refactoring( LottoPurchaseController#startPurchaseProcess() ) : 재시작 최종적인 controller에서 제어를 해야 함.

--- src/main/java/lotto/v3/controller/LottoPurchaseController.java
+++ src/main/java/lotto/v3/controller/LottoPurchaseController.java
@@ -3,7 +3,6 @@ package lotto.v3.controller;
 import lotto.v3.view.LottoPurchaseView;
 
 import static lotto.v3.model.LottoConstants.LOTTO_PRICE;
-import static lotto.v3.model.LottoConstants.MAX_RETRY_ATTEMPTS;
 
 public class LottoPurchaseController {
 
@@ -13,25 +12,12 @@ public class LottoPurchaseController {
         this.lottoPurchaseView = lottoPurchaseView;
     }
 
-    public void startPurchaseProcess() {
-        int retryCount = 0;
-        while (retryCount < MAX_RETRY_ATTEMPTS.getValue()) {
-            try {
-                int purchaseAmount = lottoPurchaseView.requestPurchaseAmount();
-                validatePurchaseAmount(purchaseAmount);
-                int numberOfLottoTickets = calculateLottoTicketsPurchasable(purchaseAmount);
-                lottoPurchaseView.displayNumberOfLottoTicketsPurchased(numberOfLottoTickets);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 잘못된 입력입니다. 다시 시도해주세요. (" + (retryCount + 1) + "/" + MAX_RETRY_ATTEMPTS.getValue() + ")");
-                retryCount++;
-            }
-        }
-        if (retryCount == MAX_RETRY_ATTEMPTS.getValue()) {
-            throw new IllegalArgumentException("입력 재시도 횟수를 초과했습니다.");
-        }
+    public void ㅣ() {
+        int purchaseAmount = lottoPurchaseView.requestPurchaseAmount();
+        validatePurchaseAmount(purchaseAmount);
+        int numberOfLottoTickets = calculateLottoTicketsPurchasable(purchaseAmount);
+        lottoPurchaseView.displayNumberOfLottoTicketsPurchased(numberOfLottoTickets);
     }
-
 
 
     private void validatePurchaseAmount(int amount) {

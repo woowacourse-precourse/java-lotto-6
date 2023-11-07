debf1a8ee040155a3cc912c5c23937a88a4d1b9c
yoojaeyoonGit <jaeyoon321@naver.com>
2023-11-08 00:57:47 +0900

    feat: 로또 번호 중복 생성 예외처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -12,6 +12,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -22,6 +23,15 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueItems = new HashSet<>();
+        List<Integer> duplicates = numbers.stream()
+                .filter(item -> !uniqueItems.add(item))
+                .toList();
+        if (duplicates.size() > 0) {
+            throw new InvalidDuplicateNumberException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
 
     public List<Integer> getNumbers() {
         return this.numbers;

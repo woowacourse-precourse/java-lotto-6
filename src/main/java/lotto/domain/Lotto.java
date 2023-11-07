90bc9ee11d737bdcaa8504dcaa2107e1e084c333
donghyun <kdhdh001@naver.com>
2023-11-08 00:19:26 +0900

    refactor: Lotto 클래스 유효성 검사 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,17 +1,32 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.utils.Constants;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersCount(numbers);
+        validateDistinctNumbers(numbers);
+        validateNumbersRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != Constants.LOTTO_NUM_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDistinctNumbers(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != Constants.LOTTO_NUM_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < Constants.LOTTO_NUM_MIN || number > Constants.LOTTO_NUM_MAX)) {
             throw new IllegalArgumentException();
         }
     }

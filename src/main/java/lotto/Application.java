2348e1b26be9bfc04f0d8540173d221574286e76
Doyoung <doodoo55@gachon.ac.kr>
2023-11-07 22:36:04 +0900

    docs: 기능 목록 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해주세요");
+        String inputMoney = readLine();
+        moneyValidate(inputMoney);
+    }
+
+    private static void moneyValidate(String inputMoney) {
+
     }
 }
---
+++ src/test/java/lotto/serviceTest/lottoServiceTest.java
@@ -0,0 +1,15 @@
+package lotto.serviceTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class lottoServiceTest {
+
+    @DisplayName("구입금액 입력을 요구하는 지시문 출력해야 함")
+    @Test
+    void printStartDirection() {
+
+    }
+
+
+}

93fffea9dc374963c4b1b4f5e6553a3fa8b82ae5
박승민 <tmddudals369@naver.com>
2023-11-08 01:59:12 +0900

    test(numberValidator): 번호가 양의 정수인지 판단하는 기능 테스트 추가

--- src/test/java/lotto/validation/NumberValidatorTest.java
+++ src/test/java/lotto/validation/NumberValidatorTest.java
@@ -28,4 +28,26 @@ class NumberValidatorTest {
         assertThatCode(() -> NumberValidator.validateNumber(target))
             .doesNotThrowAnyException();
     }
+
+    @Test
+    @DisplayName("번호가 빈 문자열로 이루어진 경우 예외를 발생시킨다.")
+    void throwExceptionNumberBlank() {
+        //given
+        String target = "";
+        //when //then
+        assertThatThrownBy(() -> NumberValidator.validateNumber(target))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 번호는 공백일 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("번호가 공백을 포함한 경우 예외를 발생시킨다.")
+    void throwExceptionNumberContainBlank() {
+        //given
+        String target = "1 ";
+        //when //then
+        assertThatThrownBy(() -> NumberValidator.validateNumber(target))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 번호는 공백이 포함될 수 없습니다.");
+    }
 }

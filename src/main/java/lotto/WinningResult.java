0c58f60a992fd3fda3fb844ecc82debc7a4b69d3
rhehdgus2 <kdgkdg0217@naver.com>
2023-11-07 17:13:28 +0900

    최종

---
+++ src/main/java/lotto/WinningResult.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public class WinningResult {
+
+    private final Lotto lotto;
+    private final int bonusball;
+
+    public WinningResult(Lotto lotto, int bonusball) {
+        this.lotto = lotto;
+        this.bonusball = bonusball;
+    }
+
+    public Ranking match(Lotto playerNumber) {
+        int countOfMatch = playerNumber.countMatch(lotto);
+        boolean bonusCheck = playerNumber.containNumber(bonusball);
+        return Ranking.valueOf(countOfMatch, bonusCheck);
+    }
+
+
+}

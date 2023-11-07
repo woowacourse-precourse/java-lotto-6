3b3b9ffd0d8d282b248d140fcf036a1d903b65d7
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 22:40:34 +0900

    feat: 로또 길이 에러 처리 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,22 @@
 package lotto.model;
 
+import java.util.List;
+
+import static lotto.model.Constants.INVALID_NUMBER_SIZE;
+
 public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            System.out.println(INVALID_NUMBER_SIZE);
+            throw new IllegalArgumentException();
+        }
+    }
 }
+

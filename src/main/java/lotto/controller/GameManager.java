b2d05d8a009925cb974e79e8122a3ca60e2ad985
linirini <2001yerin@naver.com>
2023-11-07 19:56:05 +0900

    feat : initiate game manager code

--- src/main/java/lotto/controller/GameManager.java
+++ src/main/java/lotto/controller/GameManager.java
@@ -1,26 +1,8 @@
 package lotto.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import lotto.domain.lotto.Lotto;
-import lotto.domain.lotto.Lottos;
-import lotto.domain.Price;
-import lotto.view.InputView;
-
 public class GameManager{
 
     public void run(){
-        Price price = InputView.inputPurchasePrice();
-        Lottos lottos = issueRandomLottosByPrice(price);
-    }
-
-    private Lottos issueRandomLottosByPrice(Price price){
-        int lottoCount = price.getLottoCount();
-        List<Lotto> lottos = new ArrayList<>();
-        for(int i=0;i<lottoCount;i++){
-            lottos.add(Lotto.issueRandomLotto());
-        }
-        return new Lottos(lottos);
     }
 
 }

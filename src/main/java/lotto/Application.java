ae3dd5d1ef3731e92ec46fa8d31bca423b5a553b
koreanair777 <jinuklee777@soongsil.ac.kr>
2023-11-08 00:38:50 +0900

    기본 메서드 세팅

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private static List<Integer> getNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private static String getString() {
+        return Console.readLine();
+    }
+
+    private static Integer getInteger() {
+        return Integer.valueOf(Console.readLine());
+    }
+
+
 }

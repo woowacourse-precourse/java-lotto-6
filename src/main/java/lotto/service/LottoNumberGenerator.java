9782f0a1e50f41c9b3e823969b9c9933f0e808ae
nhn1030 <shgusska12@gmail.com>
2023-11-07 12:15:49 +0900

    feat: generateLottoNumber 메서드 정의 - Set을 이용한 중복 검증 - ArrayList로 변환 후 리턴

--- src/main/java/lotto/service/LottoNumberGenerator.java
+++ src/main/java/lotto/service/LottoNumberGenerator.java
@@ -1,9 +1,20 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoNumberGenerator {
     public List<Integer> generateLottoNumber() {
+        Set<Integer> lottoNumbers = new HashSet<>();
 
+        while (lottoNumbers.size() < 6) {
+            int lottoNumber = Randoms.pickNumberInRange(1, 45);
+            lottoNumbers.add(lottoNumber);
+        }
+
+        return new ArrayList<>(lottoNumbers);
     }
 }

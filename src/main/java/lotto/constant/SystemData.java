17e4f0d1e340ff289a9c1708923e763148341189
hyerimmy <heyrim2010@naver.com>
2023-11-07 14:46:28 +0900

    chore: 당첨 1~5등 조건 ENUM 정의

--- src/main/java/lotto/constant/SystemData.java
+++ src/main/java/lotto/constant/SystemData.java
@@ -10,13 +10,4 @@ public class SystemData {
     public static final int MAX_IN_LOTTO_NUMBER = 45;
     public static final int NUMBER_OF_WINNING_NUMBER = 6;
     public static final int NUMBER_OF_BONUS_NUMBER = 1;
-
-    public static final List<WinningCondition> WINNIG_CONDITIONS = List.of(
-            new WinningCondition(3,false,5000),
-            new WinningCondition(4,false,50000),
-            new WinningCondition(5,false,1500000),
-            new WinningCondition(5,true,30000000),
-            new WinningCondition(6,false,2000000000)
-    );
 }
-
--- src/main/java/lotto/model/WinningCondition.java
+++ src/main/java/lotto/model/WinningCondition.java
@@ -1,11 +1,17 @@
 package lotto.model;
 
-public class WinningCondition {
-    public int winningNumberCount;
-    public boolean containBonusNumber;
-    public int winningAmount;
+public enum WinningCondition {
+    RANK_1(6, false, 2000000000),
+    RANK_2(5, true, 30000000),
+    RANK_3(5, false, 1500000),
+    RANK_4(4, false, 50000),
+    RANK_5(3, false, 5000);
 
-    public WinningCondition(int winningNumberCount, boolean containBonusNumber, int winningAmount){
+    private final int winningNumberCount;
+    private final boolean containBonusNumber;
+    private final int winningAmount;
+
+    WinningCondition(int winningNumberCount, boolean containBonusNumber, int winningAmount) {
         this.winningNumberCount = winningNumberCount;
         this.containBonusNumber = containBonusNumber;
         this.winningAmount = winningAmount;

c6eeb0e0463072e445e1c736ab7bb1dd48e49856
Lee9Bin <imagen33@daum.net>
2023-11-07 23:29:37 +0900

    feat : 구입금액을 입력 받는 기능 구현

---
+++ src/main/java/lotto/view/InputBuyAmountView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+public class InputBuyAmountView extends InputView{
+    private static final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public Integer getInputValue() {
+        System.out.println(INPUT_MESSAGE);
+        String result = inputValue();
+        System.out.println();
+        isDigitValidate(result);
+        beDividedThousandValidate(result);
+        isNotZeroValidate(result);
+        return Integer.parseInt(result);
+    }
+}

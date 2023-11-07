80f1920966f138cab81d851b9766282fa4a43ae1
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 13:09:29 +0900

    test: Lotto 도메인 클래스 4가지 테스트 성공

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,23 +6,39 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.constant.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외 발생")
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_COUNT_NOT_MATCHED);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외 발생")
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_DUPLICATED);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("범위 밖의 숫자가 로또 번호에 있으면 예외 발생")
+    void createLottoNotInRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 0, 1, 2, 45, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NOT_IN_RANGE);
+    }
+
+    @Test
+    @DisplayName("로또 번호의 숫자가 오름차순 정렬되지 않은 경우 예외 발생")
+    void createLottoNotSorted() {
+        assertThatThrownBy(() -> new Lotto(List.of(6, 5, 4, 3, 2, 1)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NOT_ASCENDING);
+    }
 }

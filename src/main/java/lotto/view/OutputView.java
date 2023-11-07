16e39d7defa69cdc344c41f7cf19b8d0ed9242bd
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:52:51 +0900

    feat(OutputView) : 필요한 메세지 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,41 @@
 package lotto.view;
 
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.ResultLotto;
 import lotto.system.LottoMessage;
 
 public class OutputView {
-    private OutputView() {}
+    private OutputView() {
+    }
+
+    public static void printMyLotto(long count) {
+        printMessage(count + "개를 구매했습니다.");
+    }
 
     public static void printMessage(String message) {
         System.out.println(message);
     }
 
+    public static void printLotto(List<Integer> lotto) {
+        printMessage(lotto.stream()
+                .sorted()
+                .toList()
+                .toString()
+        );
+    }
+
+    public static void printReward(List<ResultLotto> result) {
+        printMessage("당첨 통계");
+        printMessage("---");
+        printMessage("3개 일치 (5,000원) - " + Collections.frequency(result, ResultLotto.THREE) + "개");
+        printMessage("4개 일치 (50,000원) - " + Collections.frequency(result, ResultLotto.FOUR) + "개");
+        printMessage("5개 일치 (1,500,000원) - " + Collections.frequency(result, ResultLotto.FIVE) + "개");
+        printMessage("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Collections.frequency(result, ResultLotto.FIVE_BONUS) + "개");
+        printMessage("6개 일치 (2,000,000,000원) - " + Collections.frequency(result, ResultLotto.SIX) + "개");
+    }
+
+    public static void printYield(float yield) {
+        printMessage("총 수익률은 " + yield + "%입니다.");
+    }
 }

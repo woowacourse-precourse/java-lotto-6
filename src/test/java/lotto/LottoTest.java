3e2b85735bf16bbb10e4ef593f93d69b0aab2146
minjong kim <godminjong@naver.com>
2023-11-07 23:14:13 +0900

    test: compareTo 통한 로또 번호 일지 갯수 테스트 코드 작성
    두 로또번호를 Comparable 구현을 통해 getter 없이 잘 일치 횟수 반환하는지 테스트 코드 작성함.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import lotto.domain.model.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +26,31 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("compareTo method 로 일치하는 로또 갯수 테스트하기")
+    void compareToTest() throws Exception{
+        //given
+        Lotto lotto1 = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto2 = new Lotto(List.of(2,3,4,5,6,7));
+        //when
+        int matchCount1 = lotto1.compareTo(lotto2);
+        int matchCount2 = lotto2.compareTo(lotto1);
+        //then
+        assertThat(matchCount1).isEqualTo(5);
+        assertThat(matchCount2).isEqualTo(5);
+     }
+
+     @Test
+     @DisplayName("한번도 일치 하지 않을 경우 테스트")
+     void compareToNothingTest() throws Exception{
+         //given
+         Lotto lotto1 = new Lotto(List.of(1,2,3,4,5,6));
+         Lotto lotto2 = new Lotto(List.of(11,12,13,14,15,16));
+         //when
+         int matchCount1 = lotto1.compareTo(lotto2);
+         int matchCount2 = lotto2.compareTo(lotto1);
+         //then
+         assertThat(matchCount1).isEqualTo(0);
+         assertThat(matchCount2).isEqualTo(0);
+      }
 }

256be27f492de905d77f861939557c226a6bf8ca
limzzum <chfhddl1234@naver.com>
2023-11-08 00:27:43 +0900

    :sparkles: feat : 로또 머신 당첨 복권 생성 메서드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,9 +2,11 @@ package lotto.domain;
 
 import static lotto.constant.Message.LOTTO_PRICE;
 import static lotto.validation.LottoMachineValidation.validate_inputMoney;
+import static lotto.validation.LottoMachineValidation.validate_winningLotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoMachine {
@@ -30,6 +32,18 @@ public class LottoMachine {
         Lotto lotto = new Lotto(numbers);
         lottos.add(lotto);
         inputAmount -= LOTTO_PRICE;
+    }
+
+    public void createWinningLotto(String winningNums, String bonusNum){
+        validate_winningLotto(winningNums, bonusNum);
+        transToWinningLotto(winningNums, bonusNum);
+    }
+
+    private void transToWinningLotto(String nums, String bonusNum){
+        List<Integer> winningNums = Arrays.stream(nums.split(","))
+            .map(Integer::parseInt).toList();
+        this.winningLotto = new WinningLotto(winningNums, Integer.parseInt(bonusNum));
+
     }
 
 }

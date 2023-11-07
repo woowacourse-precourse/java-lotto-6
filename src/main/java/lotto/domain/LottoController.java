8c8df28c306cc5c1662e11292def345fada8bfbe
HYOAN <aspqll@naver.com>
2023-11-07 21:04:10 +0900

    feat(lotto): 로또 프로그램의 안내 메세지 enum 구현, 메세지 출력 메서드 구현

--- src/main/java/lotto/domain/LottoController.java
+++ src/main/java/lotto/domain/LottoController.java
@@ -3,8 +3,34 @@ package lotto.domain;
 public class LottoController {
     public LottoController() {
     }
-    public void playGame() {
 
+    public enum LottoMessage {
+        PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
+        PURCHASE_COUNT_MESSAGE("개를 구매했습니다."),
+        REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+        WINNING_STATS_HEADER("당첨 통계 ln ---");
+
+        private String message;
+
+        LottoMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public void playGame() {
+        int purchaseAmount = 0;
+
+
+    }
+
+    public void lottoPrint(LottoMessage lottoMessage, int purchaseAmount) {
+        if (purchaseAmount > 0)
+            System.out.print(purchaseAmount);
+        System.out.println(lottoMessage.getMessage());
     }
 
 }

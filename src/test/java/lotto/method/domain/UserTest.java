684f968fc670a0c300a3b9434cdc31b0c6325960
JP-company <wjsdj2008@gmail.com>
2023-11-07 20:53:50 +0900

    Test: [추가] 로또 당첨 번호, 보너스 번호 입력 기능 테스트

--- src/test/java/lotto/method/domain/UserTest.java
+++ src/test/java/lotto/method/domain/UserTest.java
@@ -27,4 +27,27 @@ public class UserTest {
         UserInterfaceLogic.setUserInput("8000q\n8111\n8000");
         assertThat(user.buyLotto()).size().isEqualTo(8);
     }
+
+    @Test
+    @DisplayName("로또 당첨 번호와 보너스 번호를 입력하면 해당 값을 가지는 LottoWinningNumber 객체를 반환한다.")
+    void 정상_당첨_로또번호_입력() {
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber();
+        lottoWinningNumber.setLotto(new Lotto(List.of(1,2,3,4,5,6)));
+        lottoWinningNumber.setBonusNumber(7);
+
+        UserInterfaceLogic.setUserInput("1,2,3,4,5,6\n7");
+        assertThat(user.createWinningLottoNumber()).isInstanceOf(LottoWinningNumber.class);
+    }
+
+    @Test
+    @DisplayName("잘못된 로또 당첨 번호나 보너스 번호를 입력하면 잘못 입력된 부분부터 다시 요청한다.")
+    void 비정상_당첨_로또번호_입력시_재요청() {
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber();
+        lottoWinningNumber.setLotto(new Lotto(List.of(1,2,3,4,5,6)));
+        lottoWinningNumber.setBonusNumber(7);
+
+        UserInterfaceLogic.setUserInput("1,2,3,4,5,5\n1,2,3,4,5,6f\n1,2,3,4,5,6" +  // 로또 당첨 번호 재입력
+                                        "\n6\n0\n7");                               // 보너스 번호 재입력
+        assertThat(user.createWinningLottoNumber()).isInstanceOf(LottoWinningNumber.class);
+    }
 }

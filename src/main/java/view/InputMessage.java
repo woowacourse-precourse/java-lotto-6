3c69f75e950414293de3a67ce84fd17726a2c4c8
win9 <krrr883@gmail.com>
2023-11-08 05:09:53 +0900

    [Fix]: Enum의 텍스트 내용을 출력하지 못하는 오류 수정
    getText를 이용하여 텍스트 출력

--- src/main/java/view/InputMessage.java
+++ src/main/java/view/InputMessage.java
@@ -8,19 +8,19 @@ import java.util.List;
 
 public class InputMessage {
     public static int printMoneyInputMessage() {
-        System.out.println(TextType.START);
+        System.out.println(TextType.START.getText());
         String input = Console.readLine();
         return Parser.convertNumberInput(input);
     }
 
     public static List<Integer> printWinNumberInputMessage() {
-        System.out.println(TextType.LOTTO_WIN_NUMBER);
+        System.out.println(TextType.LOTTO_WIN_NUMBER.getText());
         String input = Console.readLine();
         return Parser.convertWinningNumberInput(input);
     }
 
     public static int printBonusNumberInputMessage() {
-        System.out.println(TextType.LOTTO_BONUS);
+        System.out.println(TextType.LOTTO_BONUS.getText());
         String input = Console.readLine();
         return Parser.convertNumberInput(input);
     }

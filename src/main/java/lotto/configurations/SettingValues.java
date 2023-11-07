3fdc01a44646957ec240ead1d03c6527825b001b
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-08 02:28:07 +0900

    add : add values about fail

--- src/main/java/lotto/configurations/SettingValues.java
+++ src/main/java/lotto/configurations/SettingValues.java
@@ -5,8 +5,9 @@ public enum SettingValues {
     MIN_OF_RANGE(1),
     MAX_OF_RANGE(45),
     MIN_WINNING_RANKING(5),
-    NUMBER_SIZE(6),
+    SIZE_OF_LOTTO(6),
     PRICE_OF_LOTTO(1000),
+
     // values about rankings
     FIRST(1),
     POINT_OF_FIRST(6),
@@ -23,11 +24,9 @@ public enum SettingValues {
     FIFTH(5),
     POINT_OF_FIFTH(3),
     REWARD_OF_FIFTH(5000),
-
-    EXTRA_POINT(1),
-    CALCULATOR(8),
     FAIL(0),
-    ZERO(0);
+    REWARD_OF_FAIL(0),
+    POINT_OF_FAIL(0);
 
     private final int value;
 

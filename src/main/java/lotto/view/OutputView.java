00ee70ec227bf525d7574f5ca3de5933c68e4200
gusdn0108 <gusdn6671@naver.com>
2023-11-07 10:50:48 +0900

    feat(OutputView) : 당첨 통계 메시지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,17 @@
 package lotto.view;
 
+import static lotto.domain.Result.FIVE_AND_BONUS_MATCH;
+import static lotto.domain.Result.FIVE_MATCH;
+import static lotto.domain.Result.FOUR_MATCH;
+import static lotto.domain.Result.SIX_MATCH;
+import static lotto.domain.Result.THREE_MATCH;
+
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import lotto.domain.Result;
+import lotto.utils.ErrorMessage;
 import lotto.utils.Message;
 
 public class OutputView {
@@ -14,23 +25,28 @@ public class OutputView {
     public static void printBuyLottoCount(int count) {
         System.out.println();
         System.out.println(count + Message.PRINT_USER_BUY_LOTTO_COUNT.getMessage());
-        System.out.println();
     }
 
-    public static void pritntLottos(List<List<Integer>> lottos){
+    public static void printLotties(List<List<Integer>> lottos){
         for (List<Integer> lotto : lottos) {
             System.out.println(lotto);
         }
     }
 
-    public static void printEntertWinningNumber(){
+    public static void printEnterWinningNumber(){
         System.out.println();
         System.out.println(Message.PRINT_USER_ENTER_WINNING_NUMBER.getMessage());
     }
 
-    public static void printEnterBounsNumber(){
+    public static void printEnterBonusNumber(){
         System.out.println();
         System.out.println(Message.PRINT_USER_ENTER_BONUS_NUMBER.getMessage());
+    }
+
+    private static void printWinningStatistics() {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
     }
 
 

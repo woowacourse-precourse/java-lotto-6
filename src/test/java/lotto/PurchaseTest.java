29d1ce821551e5f431cb410d7e82798be8d0db7b
San Kim <rlatks1217@gmail.com>
2023-11-08 02:56:23 +0900

    test: 로또 금액 입력 테스트 생성

--- src/test/java/lotto/PurchaseTest.java
+++ src/test/java/lotto/PurchaseTest.java
@@ -2,19 +2,80 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import lotto.dataObjects.Lotto;
+import net.bytebuddy.asm.Advice;
 
-public class PurchaseTest {
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.dataObjects.Money;
+import lotto.utils.CommonValidator;
+import lotto.utils.CommonValidatorImpl;
 
-	@DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+public class PurchaseTest extends NsTest {
+
+	@DisplayName("빈 문자열을 입력하면 예외가 발생한다.")
 	@Test
-	void createLottoByDuplicatedNumber() {
-		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+	void emptyStringTest() {
+		CommonValidator validator = new CommonValidatorImpl();
+
+		assertThatThrownBy(() -> validator.validatePrice(""))
 			.isInstanceOf(IllegalArgumentException.class);
+		assertThat(output()).isEqualTo("[ERROR] 입력값이 필요합니다.");
+	}
+
+	@DisplayName("숫자를 입력하지 않으면 예외가 발생한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"\n", "\t", "-s", "우", "-1ab"})
+	void digitTest(String input) {
+		CommonValidator validator = new CommonValidatorImpl();
+
+		assertThatThrownBy(() -> validator.validatePrice(input))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThat(output()).isEqualTo("[ERROR] 숫자만 입력할 수 있습니다.");
+	}
+
+	@DisplayName("int 범위가 아닌 금액을 입력하면 예외가 발생한다.")
+	@Test
+	void rangeTest() {
+		CommonValidator validator = new CommonValidatorImpl();
+
+		assertThatThrownBy(() -> validator.validatePrice("-1242352346366236"))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThat(output()).isEqualTo("[ERROR] 범위를 초과한 금액입니다.");
+	}
+
+	@DisplayName("자연수를 입력하지 않으면 예외가 발생한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"0", "-1"})
+	void naturalTest(String input) {
+		CommonValidator validator = new CommonValidatorImpl();
+
+		assertThatThrownBy(() -> validator.validatePrice(input))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThat(output()).isEqualTo("[ERROR] 1보다 작은 숫자는 입력할 수 없습니다.");
+	}
+
+	@DisplayName("1000원보다 적은 금액을 입력하면 예외가 발생한다.")
+	@Test
+	void minimumTest() {
+		assertThatThrownBy(() -> new Money(999))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThat(output()).isEqualTo("[ERROR] 1000원 이상의 금액만 입력할 수 있습니다.");
+	}
+
+	@DisplayName("1000원으로 나누어 떨이지지 않는 금액을 입력하면 예외가 발생한다.")
+	@Test
+	void moneyUnitTest() {
+		assertThatThrownBy(() -> new Money(12345))
+			.isInstanceOf(IllegalArgumentException.class);
+		assertThat(output()).isEqualTo("[ERROR] 1000원 단위부터 입력할 수 있습니다.");
+	}
+
+	@Override
+	protected void runMain() {
+		//사용하지 않음
 	}
 }

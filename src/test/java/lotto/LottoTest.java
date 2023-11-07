b8db10d5cdcdb56692c5a69d8ef04a285566284f
Bisi3asi <kueeng8008@naver.com>
2023-11-08 01:47:23 +0900

    test : 로또 번호 1 미만 45 초과 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,21 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 미만, 45를 초과하는 경우 예외가 발생한다 1.")
+    @Test
+    void createLottoByLimitExcessNumber1() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 47, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1 미만, 45를 초과하는 경우 예외가 발생한다 2.")
+    @Test
+    void createLottoByLimitExcessNumber2() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 47, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

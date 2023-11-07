d8fcb5cbd3c281e12aa125bb71d3bff26bc030f4
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:43:19 +0900

    feat(BonusNumber) : 필요한 기능 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
+import lotto.view.InputView;
+
 public class BonusNumber {
+    private int bonusNumber;
+
+    public BonusNumber() {
+        bonusNumber = inputBonusNumber();
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private int inputBonusNumber() {
+        System.out.println();
+        return InputView.inputBonusNumbers();
+    }
 }

530262a27da713dfcbc33e48a2fc6e626af3890f
장세진 <jsejin95@gmail.com>
2023-11-08 02:11:55 +0900

    refactor: 구매금액 책임을 도메인으로 넘김

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,12 +1,10 @@
 package lotto.domain;
 
-import lotto.util.Validator;
-
 public class PurchaseAmount {
     private int money;
 
     public PurchaseAmount(int money) {
-        Validator.validatePurchaseAmount(money);
+        validate(money);
         this.money = money;
     }
 
@@ -16,6 +14,23 @@ public class PurchaseAmount {
 
     public int getMoney() {
         return money;
+    }
+    private void validate(int money) {
+        validateInputZero(money);
+        validatePurchaseAmount(money);
+    }
+    private void validatePurchaseAmount(int money) {
+        if (money % 1000 != 0) {
+            System.out.println("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputZero(int money) {
+        if (money == 0) {
+            System.out.println("[ERROR] 구입 금액은 0보다 커야 합니다.");
+            throw new IllegalArgumentException();
+        }
     }
 
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,6 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.util.Validator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,30 +10,34 @@ public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String ASK_WINNER_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private static final String ERROR_MESSAGE_ABOUT_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.";
+    private static final String ERROR_MESSAGE_ABOUT_PURCHASE_AMOUNT = "[ERROR] 올바른 값을 입력해주세요";
     private static final String ERROR_MESSAGE_ABOUT_DUPLICATED_LOTTO_NUMBERS = "[ERROR] 중복되지 않는 숫자를 입력해주세요.";
     private static final String ERROR_MESSAGE_ABOUT_WRONG_RANGED_LOTTO_NUMBERS = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String ERROR_MESSAGE_ABOUT_BLANK_INPUT = "[ERROR] 입력값이 없습니다.";
     private static final String COMMA = ",";
+    private static final String ZERO = "0";
     public static int inputPurchaseAmount() {
-        int money = 0;
+        String input = "";
         try {
             System.out.println(ASK_PURCHASE_AMOUNT);
-            money = Integer.parseInt(Console.readLine());
-            Validator.validatePurchaseAmount(money);
+            input = Console.readLine();
+            validateInputBlank(input);
+            validateInputNumber(input);
         } catch(IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE_ABOUT_PURCHASE_AMOUNT);
         }
-        return money;
+        return Integer.parseInt(input);
     }
 
-    private static void validateInputZero(String input) {
-        if (input == "0") {
+
+    private static void validateInputBlank(String input) {
+        if (input.replaceAll(" ", "").equals("")) {
             throw new IllegalArgumentException();
         }
     }
-    private static void validateInputBlank(String input) {
-        if (input.replaceAll(" ", "") == "") {
+
+    private static void validateInputNumber(String input) {
+        if (!input.matches("[0-9]+")) {
             throw new IllegalArgumentException();
         }
     }
@@ -44,7 +47,7 @@ public class InputView {
         try {
             System.out.println(ASK_WINNER_NUMBERS);
             input = Console.readLine();
-            Validator.validateBlank(input);
+            validateInputBlank(input);
         } catch(IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE_ABOUT_BLANK_INPUT);
         }
@@ -56,7 +59,7 @@ public class InputView {
         try {
             System.out.println(ASK_BONUS_NUMBER);
             input = Console.readLine().replaceAll(" ", "");
-            Validator.validateBlank(input);
+            validateInputBlank(input);
         } catch(IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE_ABOUT_BLANK_INPUT);
         }

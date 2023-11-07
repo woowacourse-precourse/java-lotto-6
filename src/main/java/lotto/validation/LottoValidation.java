51ac5cde66be754ad2fafaf50574b74990b1926e
limzzum <chfhddl1234@naver.com>
2023-11-08 00:30:31 +0900

    :hammer: refactor : public 으로 로또 숫자 검증 메서드 생성

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -10,13 +10,18 @@ import java.util.Set;
 
 public class LottoValidation {
 
-    public void validate_isNotDuplicate(List<Integer> nums){
+    public static void validate_lottoNum(List<Integer> nums){
+        validate_isNotDuplicate(nums);
+        validate_isLottoRangeNum(nums);
+    }
+
+    private static void validate_isNotDuplicate(List<Integer> nums){
         if(Set.copyOf(nums).size() != nums.size()){
             throw new IllegalArgumentException(INVALID_DUPLICATE_LOTTO_NUM);
         }
     }
 
-    public void validate_isLottoRangeNum(List<Integer> nums){
+    private static void validate_isLottoRangeNum(List<Integer> nums){
         for (Integer num : nums){
             if(num < LOTTO_NUM_RANGE_MIN || num > LOTTO_NUM_RANGE_MAX){
                 throw new IllegalArgumentException(INVALID_RANGE_LOTTO_NUM);

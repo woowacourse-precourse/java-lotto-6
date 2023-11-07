0169cc24d55ca67acc3001e975b002085a5b30f6
74059 <shyeone@naver.com>
2023-11-07 14:25:48 +0900

    style: 필요 없는 import 삭제

--- src/test/java/lotto/domain/InputPurchaseAmountTest.java
+++ src/test/java/lotto/domain/InputPurchaseAmountTest.java
@@ -3,10 +3,6 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputPurchaseAmountTest {

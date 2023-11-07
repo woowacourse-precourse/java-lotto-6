3d032960e19d592ed73949066e54109f20f8a085
Jaewon <choijawon99@gmail.com>
2023-11-07 21:01:49 +0900

    refactor(validator): 오류 메세지 enum 사용해서 처리

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -4,8 +4,6 @@ public class Validator {
     private final static int LOTTO_PRICE = 1000;
     private final static int LOTTO_PRICE_MIN = 0;
 
-    private final static String ERROR = "[ERROR] ";
-
     public void check_InputMoney(String input) throws IllegalArgumentException{
         check_Empty(input);
         check_NotNumber(input);
@@ -17,35 +15,35 @@ public class Validator {
         check_NotDividedByPrice(money);
     }
 
+    private void check_Empty(String input){
+        if(input.isEmpty()){
+            throw new IllegalArgumentException(ErrorMessage.IS_EMPTY.getMessage());
+        }
+    }
+
     private void check_NotNumber(String input){
         try{
             Integer.parseInt(input);
         }catch(NumberFormatException e){
-            throw new IllegalArgumentException(ERROR + "투입 금액은 숫자여야 합니다.");
-        }
-    }
-
-    private void check_Empty(String input){
-        if(input.isEmpty()){
-            throw new IllegalArgumentException(ERROR + "값을 입력해 주세요.");
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_NUMBER.getMessage());
         }
     }
 
     private void check_NotDividedByPrice(int money){
         if(money % LOTTO_PRICE != LOTTO_PRICE_MIN){
-            throw new IllegalArgumentException(ERROR + LOTTO_PRICE +"원 단위로 금액을 투입해 주세요!");
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_DIVIDED_BY_PRICE.getMessage());
         }
     }
 
     private void check_PositiveNumber(int money){
         if(money < LOTTO_PRICE_MIN){
-            throw new IllegalArgumentException(ERROR + "투입 금액은 음수가 될 수 없습니다.");
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_POSITIVE_NUMBER.getMessage());
         }
     }
 
     private void check_OverMinLottoPrice(int money){
         if(money < LOTTO_PRICE){
-            throw new IllegalArgumentException(ERROR + "로또를 구매할 돈이 부족합니다.");
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_OVER_MIN_PRICE.getMessage());
         }
     }
 

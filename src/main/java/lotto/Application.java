b1397ce10171b9990b158616872b5df9c312e07b
MisaSohee <sohee5951@naver.com>
2023-11-07 15:01:32 +0900

    feat(Application): 로또 구입 및 숫자 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,67 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
 public class Application {
+
+    private final static int LOTTO_NUMBER_COUNT = 6;
+    private final static int LOTTO_MIN_NUMBER = 1;
+    private final static int LOTTO_MAX_NUMBER = 45;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Lotto> lottos = buyLottos();
+        printLottos(lottos);
+
+    }
+
+    private static List<Lotto> buyLottos() {
+        List<Lotto> lottos = new ArrayList<>();
+        int lottoCount = getLottoCount();
+
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    private static int getLottoCount() {
+        while (true) {
+            int amount = getInputAmount();
+            if (amount % 1000 != 0) {
+                System.out.println("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
+                continue;
+            }
+            return amount / 1000;
+        }
+    }
+
+    private static int getInputAmount() {
+        while (true) {
+            System.out.println("로또 구입 금액을 입력하세요.");
+            String input = Console.readLine();
+            if (isNotNumeric(input)) {
+                System.out.println("[ERROR] 잘못된 입력입니다. 숫자를 입력해주세요.");
+                continue;
+            }
+            return Integer.parseInt(input);
+        }
+    }
+
+    private static boolean isNotNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+
+    private static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +14,12 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }

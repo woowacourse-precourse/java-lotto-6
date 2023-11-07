1514af2018288b84ea8f9385af5828d06464840c
rakyun1 <dbskim234@naver.com>
2023-11-07 16:44:36 -0700

    Refactor(Application) 로또 클라스와 랭크이넘 클라스의 함수 변화로 인해 어플리케이션에서도 함수 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,7 +12,7 @@ public class Application {
 
 		int count = -1;
 
-		while (count < 0){
+		while (count < 0) {
 			try {
 				String moneyInput = readLine();
 				count = validate.validateLottoPrice(moneyInput);
@@ -21,7 +21,7 @@ public class Application {
 			}
 		}
 
-		System.out.println(count+"개를 구매했습니다.");
+		System.out.println(count + "개를 구매했습니다.");
 
 		List<Lotto> lottos = LottoNumberFactory.makeLotto(count);
 		lottos.forEach(System.out::println);
@@ -35,20 +35,25 @@ public class Application {
 		System.out.println("보너스 번호를 입력하세요.");
 		final int bonus = validate.validateBonus(readLine(), winningNumbers);
 
-		List<RankEnum> results = lottos.stream().map(lotto -> lotto.checkRank(winningNumbers,bonus)).toList();
+		List<RankEnum> results = lottos.stream().map(lotto -> lotto.checkRank(winningNumbers, bonus)).toList();
 		int total = results.stream().mapToInt(RankEnum::getPrize).sum();
 		double profitRate = ((double) (total * 100) / (count * 1000));
-		printResult(results, profitRate);
-	}
 
-	public static void printResult(List<RankEnum> results, double profitRate){
 		System.out.println();
 		System.out.println("당첨 통계");
 		System.out.println("---");
+		printResult(results, profitRate);
+	}
+
+	public static void printResult(List<RankEnum> results, double profitRate) {
 		for (RankEnum rank : RankEnum.values()) {
-			if (rank == RankEnum.NONE) continue;
+			if (rank == RankEnum.NONE) {
+				continue;
+			}
 			String message = "";
-			if (rank.hasBonus()) message = ", 보너스 볼 일치";
+			if (rank.hasBonus()) {
+				message = ", 보너스 볼 일치";
+			}
 			long countByRank = results.stream().filter(result -> result == rank).count();
 			String prize = rank.getFormattedPrize();
 			System.out.println(rank.getMatchCount() + "개 일치" + message + " (" + prize + "원) - " + countByRank + "개");

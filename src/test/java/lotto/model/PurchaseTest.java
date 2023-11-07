c4f63a2fe7b3abd837b7d57247beec20d281e938
JW0321 <digneotn123@gmail.com>
2023-11-08 00:48:09 +0900

    test: 입력값 문자열에서 정수형로 변경
    입력 매개변수의 정수형 변경에 따라 문자열에서 정수형으로 변경

--- src/test/java/lotto/model/PurchaseTest.java
+++ src/test/java/lotto/model/PurchaseTest.java
@@ -3,37 +3,43 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PurchaseTest {
 
+    private Purchase purchase;
+    @BeforeEach
+    void setUp() {
+        purchase = new Purchase();
+    }
     @DisplayName("구매 금액이 1000으로 나누어 떨어지지 않을 경우 예외 발생")
     @Test
     void createPurchaseAmountNotDivisibleBy1000() {
-        String testPurchaseAmount = "128700";
+        int testPurchaseAmount = 128700;
 
-        assertThatThrownBy(() -> new Purchase(testPurchaseAmount))
+        assertThatThrownBy(() -> purchase.setPurchase(testPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구매 금액이 0일 경우 예외 발생")
     @Test
     void createPurchaseAmountByZero() {
-        String testPurchaseAmount = "0";
+        int testPurchaseAmount = 0;
 
-        assertThatThrownBy(() -> new Purchase(testPurchaseAmount))
+        assertThatThrownBy(() -> purchase.setPurchase(testPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구매 금액을 1000으로 나눈 숫자가 구매 횟수")
     @Test
     void creatPurchaseNumberByPurchaseAmount() {
-        String testPurchaseAmount = "50000";
+        int testPurchaseAmount = 50000;
         int testPurchaseNumber = 50;
 
-        Purchase testPurchase = new Purchase(testPurchaseAmount);
+        purchase.setPurchase(testPurchaseAmount);
 
-        assertThat(testPurchase.getPurchaseNumber()).isEqualTo(testPurchaseNumber);
+        assertThat(purchase.getPurchaseNumber()).isEqualTo(testPurchaseNumber);
     }
 }

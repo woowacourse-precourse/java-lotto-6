9b0b13cc3f71a9ce75ddac5156e2fcb1aa556d1d
becooq81 <becooq81@gmail.com>
2023-11-07 21:40:14 +0900

    refactor: remove unnecessary while loop

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,14 +4,11 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        while (true) {
-            try {
-                LottoController lottoController = new LottoController();
-                lottoController.run();
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 }

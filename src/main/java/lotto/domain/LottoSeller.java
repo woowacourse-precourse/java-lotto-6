f31700fd8a46de36f2aa61e4d397dd91cae72255
zeomzzz <jmlee2705@gmail.com>
2023-11-08 03:32:56 +0900

    feat: 여러 개의 로또 생성 기능 구현

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,12 +1,21 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.utils.Generator;
 
 public class LottoSeller {
 
-    public Lotto generateLotto(int start, int end, int count) {
-        List<Integer> numbers = Generator.generateNumbers(start, end, count);
+    public List<Lotto> generateLottos(int start, int end, int purchaseAmount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseAmount; i++) {
+            lottos.add(generateLotto(start, end, purchaseAmount));
+        }
+        return lottos;
+    }
+
+    public Lotto generateLotto(int start, int end, int purchaseAmount) {
+        List<Integer> numbers = Generator.generateNumbers(start, end, purchaseAmount);
         return new Lotto(numbers);
     }
 

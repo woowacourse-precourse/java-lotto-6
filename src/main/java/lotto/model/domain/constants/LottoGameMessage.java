c3e294ca6788d316f342914c92a439f1d4b0c481
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-07 22:16:57 +0900

    feat: 로또게임 메시지 상수 구현

---
+++ src/main/java/lotto/model/domain/constants/LottoGameMessage.java
@@ -0,0 +1,26 @@
+package lotto.model.domain.constants;
+
+public enum LottoGameMessage {
+    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    PURCHASE_COUNT("%d개를 구매했습니다."),
+    LOTTO_NUMBERS("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    WINNING_STATS("당첨 통계\n---"),
+    MATCH_COUNT("%d개 일치 (%,d원) - %d개"),
+    MATCH_BONUS_COUNT("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
+    TOTAL_PROFIT_RATE("총 수익률은 %.2f%%입니다.");
+
+    private final String message;
+
+    LottoGameMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String format(Object... args) {
+        return String.format(message, args);
+    }
+}

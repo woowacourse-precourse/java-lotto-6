c2fd33f6ed2e8c09824600709fd4629288206d45
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-08 03:22:22 +0900

    refactor: InputMessage enum 클래스 삭제 및 InputView에 상수 작성, 중복 코드 단일 메서드 추출 개선

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.function.Supplier;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.PurchaseAmount;
@@ -7,6 +8,9 @@ import lotto.domain.WinningNumbers;
 
 public class InputView {
 
+    private static final String INPUT_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private final InputReader inputReader;
 
     public InputView(InputReader inputReader) {
@@ -14,42 +18,36 @@ public class InputView {
     }
 
     public PurchaseAmount requestPurchaseAmount() {
-        while (true) {
-            try {
-                System.out.println(InputMessage.INPUT_PURCHASE_AMOUNT.getMessage());
-                return inputReader.readPurchaseAmount();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        return requestInputUntilPassItsValidation(INPUT_PURCHASE_AMOUNT_MESSAGE, inputReader::readPurchaseAmount);
     }
 
     public WinningNumbers requestWinningNumbers() {
         Lotto winningLotto = requestWinningLotto();
-        LottoNumber bonusNumber = requestBonusNumber();
-        return WinningNumbers.of(winningLotto, bonusNumber);
+        while (true) {
+            try {
+                return WinningNumbers.of(winningLotto, requestBonusNumber());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private Lotto requestWinningLotto() {
-        while (true) {
-            try {
-                System.out.println(InputMessage.INPUT_LOTTO.getMessage());
-                return inputReader.readLotto();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        return requestInputUntilPassItsValidation(INPUT_LOTTO_MESSAGE, inputReader::readLotto);
     }
 
     private LottoNumber requestBonusNumber() {
+        return requestInputUntilPassItsValidation(INPUT_BONUS_NUMBER_MESSAGE, inputReader::readBonusNumber);
+    }
+
+    private <T> T requestInputUntilPassItsValidation(String inputMessage, Supplier<T> inputSupplier) {
         while (true) {
             try {
-                System.out.println(InputMessage.INPUT_BONUS_NUMBER.getMessage());
-                return inputReader.readBonusNumber();
+                System.out.println(inputMessage);
+                return inputSupplier.get();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
-
 }

16966355874161f827b27bb6807ecca63df253c3
KimSehoon <shggm2000@gmail.com>
2023-11-08 00:57:23 +0900

    feat: validatePurchaseAmount, checkPurchaseAmountExceptionCategory method 구현
    - 예외 발생 시 에러 메시지 출력 및 입력을 다시 받는 validatePurchaseAmount method 구현
    - 발생한 예외 유형에 따라 다른 에러 메시지를 출력하는 checkPurchaseAmountExceptionCategory method 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import static lotto.message.ErrorMessage.DIVISIBLE_BY_1000;
+import static lotto.message.ErrorMessage.NUMBER_FORMAT_MONEY;
+
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.WinningStatistics;
@@ -13,14 +16,13 @@ public class Game {
     public void startGame() {
         WinningStatistics winningStatistics = new WinningStatistics();
 
-        Output.printPurchaseAmountMessage();
-        int purchaseAmount = input.getPurchaseAmount();
+        int purchaseAmount = validatePurchaseAmount();
         List<Lotto> totalLotto = gameManager.createLotto(purchaseAmount);
 
         Output.printPurchaseLottoQuantityMessage(totalLotto.size());
         Output.printTotalLotto(totalLotto);
-
         Output.printWinningNumberMessage();
+
         List<Integer> winningNumbers = input.getWinningNumbers();
 
         Output.printBonusNumberMessage();
@@ -32,5 +34,31 @@ public class Game {
 
         double profitPercentage = gameManager.calculateProfitPercentage(winningStatistics, purchaseAmount);
         Output.printProfitPercentage(profitPercentage);
+    }
+
+    private int validatePurchaseAmount() {
+        int purchaseAmount;
+
+        while (true) {
+            Output.printPurchaseAmountMessage();
+            try {
+                purchaseAmount = input.getPurchaseAmount();
+                break;
+            } catch (IllegalArgumentException e) {
+                checkPurchaseAmountExceptionCategory(e);
+            }
+        }
+
+        return purchaseAmount;
+    }
+
+    private void checkPurchaseAmountExceptionCategory(IllegalArgumentException e) {
+        if (e.getMessage().equals(NUMBER_FORMAT_MONEY.errorMessage())) {
+            System.out.println(NUMBER_FORMAT_MONEY.errorMessage());
+        }
+
+        if (e.getMessage().equals(DIVISIBLE_BY_1000.errorMessage())) {
+            System.out.println(DIVISIBLE_BY_1000.errorMessage());
+        }
     }
 }

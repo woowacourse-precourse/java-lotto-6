edd322452139acda97048acf439fdf8ffbe82805
황준혁 <hyug0819@gmail.com>
2023-11-08 01:56:06 +0900

    feat : Lotto 생성 구현

--- src/main/java/lotto/lotto/controller/LottoController.java
+++ src/main/java/lotto/lotto/controller/LottoController.java
@@ -1,8 +1,16 @@
 package lotto.lotto.controller;
 
+import static lotto.lotto.model.LottoConstant.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.lotto.model.Lotto;
+import lotto.lotto.view.LottoView;
 import lotto.user.controller.UserController;
+import lotto.util.convert.ConvertUtil;
+import lotto.util.validate.LottoValidate;
 
 public class LottoController {
     private LottoController(){ }
@@ -14,8 +22,25 @@ public class LottoController {
 
     public void start(){
         UserController userController = UserController.getInstance();
-        userController.purchaseInput();
+        int money = userController.purchaseInput();
+        makeLotto(money);
         List<Integer> numbers = userController.lottoNumberInput();
-        userController.lottoBonusNumberInput(numbers);
+        int bonus = userController.lottoBonusNumberInput(numbers);
+    }
+
+    public List<Lotto> makeLotto(int money){
+        int count = ConvertUtil.moneyToCount(money);
+        List<Lotto> lottos = new ArrayList<>();
+        try{
+            for(int i=0;i<count;i++){
+                Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, MAX_INPUT_NUMBER));
+                lottos.add(lotto);
+            }
+            LottoValidate.checkLottos(lottos);
+            LottoView.printUserLotto(lottos);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        return lottos;
     }
 }

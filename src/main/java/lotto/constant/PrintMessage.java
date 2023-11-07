f4cb57031b25449f490942e606487442843289a4
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-07 23:07:17 +0900

    feat: 로또 당첨 번호 입력 받는 기능
    로또 당첨 번호 입력 안내 문구 출력
    Console을 이용한 당첨 번호 입력

--- src/main/java/lotto/constant/PrintMessage.java
+++ src/main/java/lotto/constant/PrintMessage.java
@@ -3,4 +3,6 @@ package lotto.constant;
 public class PrintMessage {
 
     public static final String LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import static lotto.constant.PrintMessage.LOTTO_PURCHASE_AMOUNT;
+import static lotto.constant.PrintMessage.LOTTO_WINNING_NUMBERS;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,6 +9,11 @@ public class InputView {
 
     public static String readLottoPurchaseAmount() {
         System.out.println(LOTTO_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public static String readLottoWinningNumbers() {
+        System.out.println(LOTTO_WINNING_NUMBERS);
         return Console.readLine();
     }
 

2fdfe4b76f292ecdf39b7156338c90cc8fce4a6d
seonghoo1217 <seonghoo1217@naver.com>
2023-11-08 02:17:50 +0900

    refactor: 당첨번호, 보너스 번호 적중 로직 분리

--- src/main/java/lotto/core/policy/WinningCheckPolicy.java
+++ src/main/java/lotto/core/policy/WinningCheckPolicy.java
@@ -5,10 +5,15 @@ import java.util.List;
 public class WinningCheckPolicy implements WinningPolicy {
 
     @Override
-    public Integer winningCount(List<Integer> lotto, List<Integer> winning, Integer bonus) {
+    public Integer winningCount(List<Integer> lotto, List<Integer> winning) {
         return lotto.stream()
-                .filter(number -> winning.contains(number) || number.equals(bonus))
+                .filter(winning::contains)
                 .toList()
                 .size();
+    }
+
+    @Override
+    public Boolean hitBonus(List<Integer> lotto, Integer bonus) {
+        return lotto.contains(bonus);
     }
 }
--- src/main/java/lotto/core/policy/WinningPolicy.java
+++ src/main/java/lotto/core/policy/WinningPolicy.java
@@ -3,5 +3,7 @@ package lotto.core.policy;
 import java.util.List;
 
 public interface WinningPolicy {
-    Integer winningCount(List<Integer> lotto, List<Integer> winning, Integer bonus);
+    Integer winningCount(List<Integer> lotto, List<Integer> winning);
+
+    Boolean hitBonus(List<Integer> lotto, Integer bonus);
 }

dbf578af387e3ba17c534e1c9f1576ab7728b84f
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 04:13:25 +0900

    typo: 출력 메세지 수정

--- src/main/java/lotto/message/Message.java
+++ src/main/java/lotto/message/Message.java
@@ -5,7 +5,7 @@ public class Message {
     public static final String PROMPT_ENTERING_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
     public static final String PROMPT_ENTERING_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATISTICS = "\n당첨 통계";
-    public static final String GUIDE_PURCHASED_QUANTITY = "\n%d개를 구매하였습니다.";
+    public static final String GUIDE_PURCHASED_QUANTITY = "\n%d개를 구매했습니다.";
     public static final String GUIDE_TOTAL_ROR = "총 수익률은 %.1f%%입니다.";
     public static final String DIVIDER = "---";
 }

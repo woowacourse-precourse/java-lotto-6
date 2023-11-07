0c633f1cc6354f5054e4a07ebac6577dbf4146b7
권장순 <jsoonworld@gmail.com>
2023-11-08 01:25:14 +0900

    test( LottoTest ) : Lotto 클래스의 메서드를 검증하는 테스트 구현.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,20 @@
 package lotto;
 
-import lotto.v1.model.LottoV1;
+import lotto.v3.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new LottoV1(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -20,9 +22,32 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new LottoV1(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1부터 45 사이가 아니면 예외가 발생한다.")
+    @Test
+    void shouldThrowExceptionIfNumbersAreNotBetween1And45() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Lotto(Arrays.asList(0, 2, 3, 4, 5, 6)))
+                .withMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("유효한 로또 번호 리스트를 제공하면 로또 객체가 생성된다.")
+    @Test
+    void shouldCreateLottoWhenValidNumbersAreProvided() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers()).containsExactlyInAnyOrder(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("로또 번호는 생성 시 정렬되어야 한다.")
+    @Test
+    void shouldSortNumbersOnCreation() {
+        Lotto lotto = new Lotto(Arrays.asList(6, 5, 4, 3, 2, 1));
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+
+
 }

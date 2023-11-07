f824280db3531036bb52db37309cfca5a8611827
ch1hyun <och2510@gmail.com>
2023-11-07 14:04:32 +0900

    feat: Add single parameter constructor that receives only money amount

--- src/main/java/lotto/dto/MoneyDTO.java
+++ src/main/java/lotto/dto/MoneyDTO.java
@@ -4,6 +4,10 @@ public class MoneyDTO {
     private final Integer amount;
     private final Float returnRate;
 
+    public MoneyDTO(Integer amount) {
+        this(amount, 0F);
+    }
+
     public MoneyDTO(Integer amount, Float returnRate) {
         this.amount = amount;
         this.returnRate = returnRate;

7bcf1c74b3a8d3f44437c09f3d75e6742e1340d8
minisyu <iswater.kim@gmail.com>
2023-11-07 11:47:48 +0900

    refactor: MoneyTest에서 isEnough 테스트 코드 수정, equals & hashCode 테스트 코드 삭제

--- src/test/java/lotto/domain/money/MoneyTest.java
+++ src/test/java/lotto/domain/money/MoneyTest.java
@@ -1,6 +1,5 @@
 package lotto.domain.money;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -26,13 +25,13 @@ class MoneyTest {
     @DisplayName("로또 한 장 이상을 구매하기에 구입 금액이 부족하다.")
     @Test
     public void should_Return_False_When_If_Not_Enough_With_Money() {
-        // Given
+        // given
         Money money = Money.fromRemainingMoney(1_000);
 
-        // When
+        // when
         boolean notEnough = money.isNotEnough();
 
-        // Then
+        // then
         assertFalse(notEnough);
     }
 
@@ -40,40 +39,17 @@ class MoneyTest {
     @Test
     public void should_Return_True_When_If__Enough_With_Money() {
         // given
-        Money money = Money.fromInitialMoney(3_000);
+        int amount = 3_000;
+        Money money1 = Money.fromInitialMoney(amount);
+        Money money2 = Money.fromRemainingMoney(amount);
 
         // when
-        boolean enough = money.isEnough();
+        boolean isEnough1 = money1.isEnough();
+        boolean isEnough2 = money2.isEnough();
 
         // then
-        assertTrue(enough);
-    }
+        assertTrue(isEnough1);
+        assertTrue(isEnough2);
 
-    @DisplayName("두 Money 객체는 같은 금액을 가질 때 동등해야 한다.")
-    @Test
-    public void should_MoneyObjects_Be_Equal_When_They_Have_Same_Money() {
-        // given
-        // when
-        Money money1 = Money.fromInitialMoney(1000);
-        Money money2 = Money.fromInitialMoney(1000);
-        Money money3 = Money.fromInitialMoney(2000);
-
-        // then
-        assertEquals(money1, money2);
-        assertNotEquals(money1, money3);
-    }
-
-    @Test
-    @DisplayName("두 Money 객체의 해시 코드는 같은 금액을 가질 때 동일해야 한다.")
-    public void should_HashCode_Of_MoneyObjects_Be_Equal_When_They_Have_Same_Money() {
-        // given
-        // when
-        Money money1 = Money.fromInitialMoney(1000);
-        Money money2 = Money.fromInitialMoney(1000);
-        Money money3 = Money.fromInitialMoney(2000);
-
-        // then
-        assertEquals(money1.hashCode(), money2.hashCode());
-        assertNotEquals(money1.hashCode(), money3.hashCode());
     }
 }

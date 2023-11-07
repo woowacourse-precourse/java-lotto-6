de5b393e02079e4472ea38e950136dae41ebace7
박승민 <tmddudals369@naver.com>
2023-11-08 01:38:09 +0900

    feat(number): 번호를 다루는 객체의 동등성을 판단하기 위한 equals와 hashcode 구현

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Number {
     private int value;
 
@@ -16,5 +18,22 @@ public class Number {
         if (value < 1 || value > 45) {
             throw new IllegalArgumentException("[ERROR] 번호는 1이상 45이하의 수만 입력할 수 있습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number = (Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }

99f9836b577a82190e2d391fb535d9fa24d8ce26
Infikei <hckhong@naver.com>
2023-11-08 07:01:29 +0900

    refactor: OutputView에서 메시지에 대한 enum을 문자열 상수로 다시 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,27 +3,13 @@ package lotto.view;
 import lotto.domain.LottosPurchased;
 
 public class OutputView {
-    private enum PrintMessage {
-        NUMBER_OF_LOTTOS("개를 구매했습니다."),
-        WINNING_STATISTICS("당첨 통계\n---");
-
-        private final String message;
-
-        private PrintMessage(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public String toString() {
-            return message;
-        }
-    }
+    private static final String NUMBER_OF_LOTTOS_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS = "당첨 통계\n---";
 
     public void printNumberOfLottosPurchased(LottosPurchased lottosPurchased) {
         int numberOfLottos = lottosPurchased.getNumberOfLottos();
 
-        System.out.print(numberOfLottos);
-        System.out.println(PrintMessage.NUMBER_OF_LOTTOS);
+        System.out.println(numberOfLottos + NUMBER_OF_LOTTOS_MESSAGE);
     }
 
     public void printLottosPurchased(LottosPurchased lottosPurchased) {

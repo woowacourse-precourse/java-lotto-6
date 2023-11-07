2551917416da803cb4b7dbefbd01125beb55bf65
IM-GYURI <kgulr0517@gmail.com>
2023-11-08 02:11:54 +0900

    refactor: public 삭제

--- src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class LottoNumbersGeneratorTest {
+class LottoNumbersGeneratorTest {
 
     @Test
     void RandomNumberGenerator() {

ed27b5b6978203eccb6b30e16d5c5550e50b9140
kangaroo0 <ljw42871@gmail.com>
2023-11-07 19:40:59 +0900

    Feat: 로또 구입 금액 입력 받기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,27 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+
+    public LottoController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+
+    public void play() {
+
+        int lottoAmount = inputView.getPurchaseAmount();
+
+        System.out.println(lottoAmount);
+
+
+    }
+
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,17 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String GET_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해주세요.";
+
+    public int getPurchaseAmount() {
+
+        System.out.println(GET_PURCHASE_AMOUNT_MESSAGE);
+        String input = Console.readLine();
+
+        return Integer.parseInt(input);
+    }
+
 }

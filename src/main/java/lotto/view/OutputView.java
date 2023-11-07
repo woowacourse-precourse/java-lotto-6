6824a1cc98d703b5fe15bf494aa7b1a53512d4a9
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 03:30:57 +0900

    refactor(OutputView): 출력 메시지들을 enum으로 변경 및 형식 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,26 +7,48 @@ import lotto.domain.enums.LottoRank;
 import java.util.List;
 import java.util.Map;
 
+import static lotto.constant.OutputMessage.DIVIDE_LINE;
+import static lotto.constant.OutputMessage.EARNING_RATE_END_MESSAGE;
+import static lotto.constant.OutputMessage.EARNING_RATE_START_MESSAGE;
+import static lotto.constant.OutputMessage.NEW_LINE;
+import static lotto.constant.OutputMessage.PURCHASE_COUNT_MESSAGE;
+import static lotto.constant.OutputMessage.RESULT_INTRO;
+import static lotto.constant.OutputMessage.WIN_COUNT_MESSAGE;
+
 public class OutputView {
     public static void showPurchaseCount(int purchaseCount) {
-        System.out.println(purchaseCount + "개를 구매했습니다.");
+        System.out.println();
+        System.out.println(purchaseCount + PURCHASE_COUNT_MESSAGE.getMessage());
     }
 
     public static void showPurchaseLotto(List<Lotto> lottos) {
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
 
+    public static void showResultIntro() {
+        System.out.println();
+        System.out.println(RESULT_INTRO.getMessage());
+        System.out.println(DIVIDE_LINE.getMessage());
+    }
+
     public static void showResult(LottoResult lottoResult, long money) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (Map.Entry<LottoRank, Integer> entry : getLottoResultEntry(lottoResult)) {
             LottoRank rank = entry.getKey();
             int count = entry.getValue();
-            System.out.println(rank.getMessage() + count + "개");
+            stringBuilder.append(rank.getMessage())
+                    .append(count)
+                    .append(WIN_COUNT_MESSAGE.getMessage())
+                    .append(NEW_LINE.getMessage());
         }
 
-        System.out.println("총 수익률은 " + String.format("%,.1f", lottoResult.getEarningRate(money)) + "%입니다.");
+        stringBuilder.append(EARNING_RATE_START_MESSAGE.getMessage())
+                .append(String.format("%,.1f", lottoResult.getEarningRate(money)))
+                .append(EARNING_RATE_END_MESSAGE.getMessage());
+        System.out.println(stringBuilder);
     }
 
-    public static List<Map.Entry<LottoRank, Integer>> getLottoResultEntry(LottoResult lottoResult) {
+    private static List<Map.Entry<LottoRank, Integer>> getLottoResultEntry(LottoResult lottoResult) {
         return lottoResult.getLottoResult().entrySet()
                 .stream()
                 .filter(entry -> entry.getKey() != LottoRank.NONE)

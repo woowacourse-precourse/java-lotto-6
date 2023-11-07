96404ff26304fbcc06cba435420c75063f78f448
juns1s <juns1s@naver.com>
2023-11-07 21:06:35 +0900

    feat: 미당첨 정의

--- src/main/java/lotto/embedded/LottoResult.java
+++ src/main/java/lotto/embedded/LottoResult.java
@@ -1,13 +1,12 @@
 package lotto.embedded;
 
-import java.util.Optional;
-
 public enum LottoResult {
     FIRST(2000000000, 6, false, "6개 일치 (2,000,000,000원)"),
     SECOND(30000000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
     THIRD(1500000, 5, false, "5개 일치 (1,500,000원)"),
     FOURTH(50000, 4, false, "4개 일치 (50,000원)"),
     FIFTH(5000, 3, false, "3개 일치 (5,000원)"),
+    NONE(0, 2, false, "미 당첨"),
     ;
 
     private final long prize;
@@ -38,22 +37,26 @@ public enum LottoResult {
         return description;
     }
 
-    public static Optional<LottoResult> of(int matchCount, boolean isBonusMatch){
-        if(matchCount == FIRST.matchCount){
-            return Optional.of(LottoResult.FIRST);
+    public static LottoResult of(int matchCount, boolean isBonusMatch) {
+        if (matchCount == FIRST.matchCount) {
+            return LottoResult.FIRST;
         }
-        if(matchCount == SECOND.matchCount && isBonusMatch){
-            return Optional.of(LottoResult.SECOND);
+        if (matchCount == SECOND.matchCount && isBonusMatch) {
+            return LottoResult.SECOND;
         }
-        if(matchCount == THIRD.matchCount){
-            return Optional.of(LottoResult.THIRD);
+        if (matchCount == THIRD.matchCount) {
+            return LottoResult.THIRD;
         }
-        if(matchCount == FOURTH.matchCount){
-            return Optional.of(LottoResult.FOURTH);
+        if (matchCount == FOURTH.matchCount) {
+            return LottoResult.FOURTH;
         }
-        if(matchCount == FIFTH.matchCount){
-            return Optional.of(LottoResult.FIFTH);
+        if (matchCount == FIFTH.matchCount) {
+            return LottoResult.FIFTH;
         }
-        return Optional.empty();
+        return NONE;
+    }
+
+    public boolean isWin() {
+        return this != NONE;
     }
 }

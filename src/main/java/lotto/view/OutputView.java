88d579f9f594b9d9092bede989c86b07cbf32102
Yeriimii <seojs0511@naver.com>
2023-11-08 02:33:24 +0900

    feat(OutputView): 총 수익률을 출력할 수 있다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,16 @@
 package lotto.view;
 
 import lotto.model.LottoPaper;
+import lotto.model.dto.LottoResultDTO;
 
 import java.util.List;
 
 public class OutputView {
+
     private enum OutputMessage {
-        PURCHASE_RESULT("개를 구매했습니다.");
+        PURCHASE_RESULT("개를 구매했습니다."),
+        RETURN_RATE_HEADER("총 수익률은 "),
+        RETURN_RATE_FOOTER("%입니다.");
 
         private final String message;
 
@@ -24,5 +28,13 @@ public class OutputView {
             System.out.println(lottoPaper.getLottoNumbers());
         }
         System.out.printf("%n");
+    }
+
+    public void printReturnRate(LottoResultDTO resultDTO) {
+        System.out.printf(
+                "%s%.1f%s",
+                OutputMessage.RETURN_RATE_HEADER.message,
+                resultDTO.getTotalReturnRate(),
+                OutputMessage.RETURN_RATE_FOOTER.message);
     }
 }

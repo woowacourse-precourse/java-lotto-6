891e68d1c9f7de59b14557810729c36aaaa265e0
miro-22 <rlawlgus96@snu.ac.kr>
2023-11-08 03:01:08 +0900

    fix: instantiate generator class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,32 +1,10 @@
 package lotto;
 
-import java.util.List;
-import lotto.domain.BonusNumberGenerator;
-import lotto.domain.Lotto;
-import lotto.domain.LottoAmountGenerator;
-import lotto.domain.LottoGenerator;
-import lotto.domain.WinningNumbersGenerator;
-import lotto.io.OutputHandler;
-import lotto.util.Calculator;
+import lotto.domain.LottoRunner;
 
 public class Application {
-    static int amount;
-    static List<Lotto> lottos;
-    static Lotto winningNumbers;
-    static int bonusNumber;
-
-
     public static void main(String[] args) {
-        amount = LottoAmountGenerator.generateLottoAmount();
-        lottos = LottoGenerator.issueLotto(amount);
-
-        OutputHandler.printLottoAmount(amount);
-        OutputHandler.printLottos(lottos);
-
-        winningNumbers = WinningNumbersGenerator.generateWinningNumbers();
-        bonusNumber = BonusNumberGenerator.generateBonusNumber();
-
-        Calculator calculator = new Calculator(lottos);
-        calculator.printResult(winningNumbers, bonusNumber);
+        LottoRunner lottoRunner = new LottoRunner();
+        lottoRunner.run();
     }
 }
--- src/main/java/lotto/domain/LottoRunner.java
+++ src/main/java/lotto/domain/LottoRunner.java
@@ -1,2 +1,40 @@
-package lotto.domain;public class LottoRunner {
+package lotto.domain;
+
+import java.util.List;
+import lotto.domain.generator.BonusNumberGenerator;
+import lotto.domain.generator.LottoAmountGenerator;
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.generator.WinningNumbersGenerator;
+import lotto.io.OutputHandler;
+import lotto.util.Calculator;
+
+public class LottoRunner {
+    final LottoAmountGenerator lottoAmountGenerator;
+    final LottoGenerator lottoGenerator;
+    final WinningNumbersGenerator winningNumbersGenerator;
+    final BonusNumberGenerator bonusNumberGenerator;
+    List<Lotto> lottos;
+    Lotto winningNumbers;
+    int bonusNumber;
+
+    public LottoRunner() {
+        lottoAmountGenerator = new LottoAmountGenerator();
+        lottoGenerator = new LottoGenerator();
+        winningNumbersGenerator = new WinningNumbersGenerator();
+        bonusNumberGenerator = new BonusNumberGenerator();
+    }
+
+    public void run() {
+        int amount = lottoAmountGenerator.generate();
+        lottos = lottoGenerator.issueLotto(amount);
+
+        OutputHandler.printLottoAmount(amount);
+        OutputHandler.printLottos(lottos);
+
+        winningNumbers = winningNumbersGenerator.generate();
+        bonusNumber = bonusNumberGenerator.generate();
+
+        Calculator calculator = new Calculator(lottos);
+        calculator.printResult(winningNumbers, bonusNumber);
+    }
 }

fc94062b3baa5d171a1a024e51a1a1d34334b008
권장순 <jsoonworld@gmail.com>
2023-11-08 07:18:57 +0900

    feat( LottoResultController#showResults() ) : 당첨 결과를 제어하는 컨트롤러 및 메서드 구현.

---
+++ src/main/java/lotto/v3/controller/LottoResultController.java
@@ -0,0 +1,20 @@
+package lotto.v3.controller;
+
+import lotto.v3.model.LottoResult;
+import lotto.v3.view.LottoResultView;
+import java.util.List;
+import java.util.Set;
+
+public class LottoResultController {
+    private final LottoResultView view;
+
+    public LottoResultController(LottoResultView view) {
+        this.view = view;
+    }
+
+    public void showResults(List<Set<Integer>> purchasedNumbers, Set<Integer> winningNumbers, int bonusNumber) {
+        LottoResult result = new LottoResult(purchasedNumbers, winningNumbers, bonusNumber);
+        view.displayResults(result.getMatchCounts());
+    }
+}
+

5574304d651b7c9d77e4b4d104cce1d252af8bb3
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 00:52:39 +0900

    feat: 로또 번호 개수 검증 메소드 구현

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,6 +1,9 @@
 package lotto.util;
 
+import lotto.Manager;
 import lotto.message.ErrorMessage;
+
+import java.util.List;
 
 public class Validator {
     public static void validateDigit(String str) throws IllegalArgumentException {
@@ -15,6 +18,12 @@ public class Validator {
         int THOUSAND = 1000;
         if (amount % THOUSAND != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_AMOUNT);
+        }
+    }
+
+    public static void validateNumberOfLottoNumbers(List<Integer> lotto) {
+        if (lotto.size() != Manager.NUMBER_OF_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_LOTTO_NUMBER_COUNT);
         }
     }
 }

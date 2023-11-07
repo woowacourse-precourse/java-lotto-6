7d6814eab5bf8219cd15f56f513f7139e709a8cf
jeongjihun <wlgns2234@naver.com>
2023-11-07 17:06:28 +0900

    refactor: 클래스 이름 변경
    - LottoNumberRandomGenerator -> LottoNumberGenerator 으로 변경
    - 로또번호를 생성한다고 했을때 이미 랜덤으로 생성할 것이라는 의미가 담겨있다고 생각하여 Random을 제거하였다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,12 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.model.LottoNumberRandomGenerator;
+import lotto.model.LottoNumberGenerator;
 import lotto.model.NumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        NumberGenerator numberGenerator = new LottoNumberRandomGenerator();
+        NumberGenerator numberGenerator = new LottoNumberGenerator();
 
         LottoController lottoController = new LottoController(numberGenerator);
         lottoController.start();

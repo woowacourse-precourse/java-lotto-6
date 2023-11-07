46478e296aea9fc316fa175ff816f22402f1a438
JeongMin <ajhappy12@gamil.com>
2023-11-07 21:49:10 +0900

    feat: lotto에 대한 상수 값 config 생성

---
+++ src/main/java/config/LottoConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+public class LottoConfig {
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_MIN = 1;
+    public static final int LOTTO_MAX = 45;
+    public static final int LOTTO_PRICE = 1000;
+}

8856f97f76e024de19d77b98801471681e538dfb
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 03:53:09 +0900

    feat($LottoResult): 보너스 일치 여부 기능 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -3,10 +3,15 @@ package lotto.domain;
 import lotto.type.LottoPrize;
 public class LottoResult {
     private final Lotto winningNumbers;
+    private final int bonusNumber;
     private int countMatchingNumbers(Lotto lotto) {
         return winningNumbers.getNumbers().stream()
                 .filter(lotto.getNumbers()::contains)
                 .toList()
                 .size();
+    }
+
+    private boolean isMatchBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
     }
 }

8a54e60ec1828cb95b69be5702619a7d2aff02aa
HyunmoBae <tsi0520@naver.com>
2023-11-08 02:14:36 +0900

    feat(lotto) : add ErrorMessage, ErrorType

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -2,11 +2,26 @@ package lotto;
 
 public class Error {
     public enum ErrorType{
-
+        INVALID_AMOUNT,
+        INVALID_INPUT_TYPE,
+        INVALID_LOTTO_INPUT,
+        DUPLICATION
     }
 
     public static void errorMessage(ErrorType errorType) {
-
+        if (errorType.equals("INVALID_AMOUNT")) {
+            System.out.println("[ERROR] 입력값에러");
+            throw new IllegalArgumentException();
+        } else if (errorType.equals("INVALID_INPUT_TYPE")) {
+            System.out.println("[ERROR] 입력타입에러");
+            throw new IllegalArgumentException();
+        } else if (errorType.equals("INVALID_LOTTO_INPUT")) {
+            System.out.println("[ERROR] 로또 값 에러");
+            throw new IllegalArgumentException();
+        } else if (errorType.equals("DUPLICATION")) {
+            System.out.println("[ERROR] 중복 에러");
+            throw new IllegalArgumentException();
+        }
     }
 
 }
--- src/main/java/lotto/LottoTicketGenerator.java
+++ src/main/java/lotto/LottoTicketGenerator.java
@@ -1,16 +1,28 @@
-package lotto;
+package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.error.Error;
+import lotto.error.Error.ErrorType;
 
 public class LottoTicketGenerator {
     List<Integer> lottoNumber;
-    int numberOfTicket;
+    static int NUMBER_OF_TICKET;
 
-    public LottoTicketGenerator(int amount){
+    public LottoTicketGenerator(int amount) {
+        validate(amount);
+        NUMBER_OF_TICKET = amount / 1000;
     }
 
     public List<Integer> createLottoTicket() {
+        lottoNumber = Randoms.pickUniqueNumbersInRange(1,45,6);
         return lottoNumber;
+    }
+
+    private void validate(int amount) {
+        if (!(amount / 1000 == 0) || amount <= 0) {
+            Error.errorMessage(ErrorType.INVALID_AMOUNT);
+        }
     }
 
 }

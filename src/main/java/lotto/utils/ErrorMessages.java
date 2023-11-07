9c5e06106ecd5bb9bb582cc828426461932176ce
twoosky <lhjae3@naver.com>
2023-11-08 07:59:36 +0900

    feat: 에러메시지 상수화

---
+++ src/main/java/lotto/utils/ErrorMessages.java
@@ -0,0 +1,21 @@
+package lotto.utils;
+
+public class ErrorMessages {
+    public static final String LOTTO_OUT_OF_SIZE = "[ERROR] 로또 번호의 개수는 6개여야 합니다.";
+    public static final String LOTTO_NUMBER_OUT_OF_RANGE = "[ERROR] 로또 번호의 숫자 범위는 1~45입니다.";
+    public static final String LOTTO_NUMBER_DUPLICATION = "[ERROR] 로또 번호로 중복된 숫자를 선택할 수 없습니다.";
+
+    public static final String LOTTO_STORE_IS_NULL = "[ERROR] 로또 판매점이 존재하지 않습니다.";
+    public static final String MONEY_NOT_MULTIPLE_UNIT = "[ERROR] 구매 금액은 1000원 단위만 입력 가능합니다.";
+    public static final String MONEY_UNDER_MINIMUM = "[ERROR] 최소 1000원 이상 구매해야 합니다.";
+
+    public static final String BONUS_NUMBER_INVALID_TYPE = "[ERROR] 보너스 번호는 숫자만 입력 가능합니다.";
+    public static final String BONUS_NUMBER_OUT_OF_RANGE = "[ERROR] 보너스 번호의 숫자 범위는 1~45입니다.";
+    public static final String BONUS_NUMBER_DUPLICATE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+
+    public static final String WINNING_NUMBERS_INVALID_TYPE = "[ERROR] 당첨 번호는 숫자만 입력 가능합니다.";
+
+    public static final String LOTTO_RANK_NOT_MATCHED = "[ERROR] 해당되는 로또 순위가 없습니다.";
+
+    public static final String NOT_INTEGER_TYPE = "[ERROR] 숫자가 아닙니다.";
+}

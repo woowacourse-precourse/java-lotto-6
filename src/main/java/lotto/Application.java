405e8c11feef846daa7bf52cac1ad1728525cf8f
m5s3 <devmyoungsoo2@gmail.com>
2023-11-08 02:37:51 +0900

    refactor(Application): `LottoGameController` 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+import lotto.controller.LottoGameController;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.WinningPrize;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        LottoService lottoService = new LottoService();
+        OutputView outputView = new OutputView();
+
+        LottoGameController lottoGameController = new LottoGameController(inputView, outputView, lottoService);
+        lottoGameController.run();
     }
 }

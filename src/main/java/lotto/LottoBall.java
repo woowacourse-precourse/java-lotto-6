f1608af111939fe249998f5b325664400f7dd25d
donghoony <aru0504@naver.com>
2023-11-07 10:03:29 +0900

    feat(LottoBall): 생성 시 수의 범위를 검증하도록 구현
    구현에 대한 테스트 두 가지도 작성
     - 정상적으로 생성
     - 범위 외 수에 대해서 예외

--- src/main/java/lotto/LottoBall.java
+++ src/main/java/lotto/LottoBall.java
@@ -1,8 +1,32 @@
 package lotto;
 
+import lotto.exception.ArgumentRangeOutOfBoundsException;
+
 public class LottoBall {
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 45;
+
     private final int number;
+
     public LottoBall(int number) {
+        validateNumberRange(number);
         this.number = number;
+    }
+
+    private void validateNumberRange(int number) {
+        validateNumberLowerRange(number);
+        validateNumberUpperRange(number);
+    }
+
+    private void validateNumberLowerRange(int number) {
+        if (number < MIN_RANGE) {
+            throw new ArgumentRangeOutOfBoundsException();
+        }
+    }
+
+    private void validateNumberUpperRange(int number) {
+        if (number > MAX_RANGE) {
+            throw new ArgumentRangeOutOfBoundsException();
+        }
     }
 }
---
+++ src/test/java/lotto/LottoBallTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoBallTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 44, 45})
+    @DisplayName("범위 내의 수로 생성하는 경우 정상적으로 생성된다.")
+    public void validRangeTest(int number) {
+        assertDoesNotThrow(() -> new LottoBall(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46, 50, 100})
+    @DisplayName("범위를 벗어나게 생성하는 경우, 예외를 발생한다.")
+    public void invalidRangeTest(int number) {
+        assertThatThrownBy(() -> new LottoBall(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

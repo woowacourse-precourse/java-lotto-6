e480972932929a0d5d6f5fca152f70e6af1dc1c9
tangba484 <tangba484@naver.com>
2023-11-07 15:29:23 +0900

    Test: LottoAnswer 클래스 테스트 코드 추가
    - 당첨번호클래스 내에서의 BonusNumber 생성 테스트
    - hasBonusNumber 메서드의 테스트
    - BonusNumber가 중복인지 체크하는 테스트

--- src/test/java/lotto/domain/LottoAnswerTest.java
+++ src/test/java/lotto/domain/LottoAnswerTest.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoAnswerTest {
@@ -41,5 +44,31 @@ class LottoAnswerTest {
         assertThrows(IllegalArgumentException.class, () -> {
             LottoAnswer lottoAnswer = new LottoAnswer("1,2,3,4,5,99");
         });
+    }
+
+    @Test
+    @DisplayName("BonusNumber 의 생성 테스트")
+    void testGetBonusNumber() {
+        LottoAnswer lottoAnswer = new LottoAnswer("1,2,3,4,5,6", 7);
+        BonusNumber bonusNumber = lottoAnswer.getBonusNumber();
+        assertNotNull(bonusNumber);
+        assertEquals(7, bonusNumber.getNumber());
+    }
+
+    @Test
+    @DisplayName("BonusNumber 의 존재 유무 테스트")
+    void testHasBonusNumber() {
+        LottoAnswer lottoAnswer1 = new LottoAnswer("1,2,3,4,5,6", 7);
+        assertTrue(lottoAnswer1.hasBonusNumber());
+
+        LottoAnswer lottoAnswer2 = new LottoAnswer("1,2,3,4,5,6");
+        assertFalse(lottoAnswer2.hasBonusNumber());
+    }
+
+    @Test
+    @DisplayName("BonusNumber 가 중복될 경우 에러가 발생한다.")
+    void testIsUnique(){
+        assertThatThrownBy(() -> new LottoAnswer("1,2,3,4,5,6",6))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

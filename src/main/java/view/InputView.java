6fdeed609f8f6c0bf77bf5f82d0a2fee0b17145f
daneng4 <daneng4@naver.com>
2023-11-07 23:25:35 +0900

    feat: 당첨 숫자, 보너스 숫자 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,14 +1,15 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import util.Splitter;
 import validator.InputDataValidator;
 
 public class InputView {
 
     InputDataValidator dataValidator = new InputDataValidator();
     OutputView outputView = new OutputView();
-
-    private String inputData;
+    Splitter splitter = new Splitter();
+    String inputData;
 
     public int getLottoCostData(){
         while(true){
@@ -18,6 +19,34 @@ public class InputView {
                 break;
             } catch (IllegalArgumentException e){
                 outputView.printIlligalLottoCostMessage();
+            }
+        }
+        return Integer.parseInt(inputData);
+    }
+
+    public String[] getWinningNumber(){
+        String[] splitData;
+        while(true){
+            try{
+                inputData = Console.readLine();
+                splitData = splitter.commaSplitter(inputData);
+                dataValidator.validWinningNumberInput(splitData);
+                break;
+            }catch(IllegalArgumentException e){
+                outputView.printIlligalWinningNumberMessage();
+            }
+        }
+        return splitData;
+    }
+
+    public int getBonusNumber(){
+        while(true){
+            try{
+                inputData = Console.readLine();
+                dataValidator.validBonusNumberInput(inputData);
+                break;
+            }catch(IllegalArgumentException e){
+                outputView.printIlligalWinningNumberMessage();
             }
         }
         return Integer.parseInt(inputData);

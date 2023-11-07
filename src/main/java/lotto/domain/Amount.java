edec4b3575dc92fece7a1b334fbe16ffc193353e
bin <gimgyubin1225@gmail.com>
2023-11-07 21:13:36 +0900

    feat: Exception 발생시 에러 메세지 출력 기능 추가

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
+import lotto.view.Output;
+
 public class Amount {
 
+    private static final String ERROR_MESSAGE_NOT_DIVIDED = "로또 금액으로 나누어 떨어지는 금액이어야 합니다.";
     private int amount;
 
     public Amount(Integer amount) {
@@ -13,8 +16,10 @@ public class Amount {
         return amount;
     }
 
+
     private void validateDividedByLottoPrice(Integer amount) {
         if (amount % Lotto.PRICE != 0) {
+            Output.printErrorMessage(ERROR_MESSAGE_NOT_DIVIDED);
             throw new IllegalArgumentException();
         }
     }

04357a4a18ea7fff882e1ca45ab8039ddb55bc03
ghsyn <iamdragon.dev@gmail.com>
2023-11-07 15:01:17 +0900

    feat(Lotto) : Create method winningNumbers, bonusNumber

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,18 +1,24 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Input {
+
+    final static int LOTTO_WINNING_NUMBER_COUNT = 6;
     public static int money(){
         int money = Integer.parseInt(Console.readLine());
         //TODO: exception 추가 - money가 1000으로 나누어 떨어지지 않을때
         return money;
     }
 
-    public static void winningNumbers(){
-
+    public static List<Integer> winningNumbers(){
+        String winningNumbers = Console.readLine();
+        return LottoGame.MakeWinningNumbers(winningNumbers);
     }
 
-    public static void bonusNumber(){
-
+    public static int bonusNumber(){
+        return Integer.parseInt(Console.readLine());
     }
 }

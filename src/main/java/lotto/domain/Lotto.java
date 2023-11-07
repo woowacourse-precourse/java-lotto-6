c640263fad85d5e2f61e27ee83464123943adf7d
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-08 01:27:33 +0900

    feat: test를 위한 검증기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,6 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public List<Integer> getLottoNumbers() {

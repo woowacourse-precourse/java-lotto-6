6b039e9d6bb0240b8bbdd5e3da847f584e1061f8
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-07 15:16:10 +0900

    Feat: main 클래스에 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,13 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+
+        lottoController.inputPurchasedPrice();
+        lottoController.generateLottos();
+        lottoController.generateWinningCondition();
+        lottoController.inputBonusNumber();
+        lottoController.generateWinningResult();
+        lottoController.displayFinalResult();
     }
 }

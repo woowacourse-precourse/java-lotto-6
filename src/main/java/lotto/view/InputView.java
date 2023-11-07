de733ede585608ad4c3c57fd967bc384cf8427f9
iceprins <junghankyul@gmail.com>
2023-11-08 00:07:35 +0900

    refactor: 공백 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,6 @@
 package lotto.view;
 
 public class InputView {
-
     static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";

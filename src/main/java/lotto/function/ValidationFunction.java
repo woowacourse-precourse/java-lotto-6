4734831e8c4590985b23b307be51cd0b0dedcc5c
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-08 01:29:21 +0900

    refactor: enum 추가로 인한 코드 수정

--- src/main/java/lotto/function/ValidationFunction.java
+++ src/main/java/lotto/function/ValidationFunction.java
@@ -1,28 +1,37 @@
 package lotto.function;
 
 import lotto.constant.ErrorMessage;
+import lotto.constant.LottoNumber;
+import lotto.domain.Lotto;
 
 import java.util.stream.Stream;
 
 public class ValidationFunction {
 
     public int validateMoney(int money) {
-        if (money % 1000 != 0) {
+        if (money % LottoNumber.PURCHASE_CONDITION.getNumber() != 0) {
             throw new IllegalArgumentException(ErrorMessage.PURCHASE_WRONG);
         }
         return money;
     }
 
+    public void validateMoneyIsNumber(String money) {
+        String regex = "[0-9]+";
+        if(!money.matches(regex)) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_ONLY_NUMBER);
+        }
+    }
+
     public void validateWinningNumberSize(String[] winningNumbers) {
-        if (winningNumbers.length != 6) {
+        if (winningNumbers.length != LottoNumber.LENGTH_CONDITION.getNumber()) {
             throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_SIZE);
         }
     }
 
     public void validateWinningNumberRange(String[] winningNumbers) {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < LottoNumber.LENGTH_CONDITION.getNumber(); i++) {
             int compareNum = Integer.parseInt(winningNumbers[i]);
-            if (compareNum < 1 || compareNum > 45) {
+            if (compareNum < LottoNumber.FIRST_RANGE.getNumber() || compareNum > LottoNumber.LAST_RANGE.getNumber()) {
                 throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE);
             }
         }
@@ -36,8 +45,9 @@ public class ValidationFunction {
     }
 
     public void validateBonusNumber(int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
+        if (bonusNumber < LottoNumber.FIRST_RANGE.getNumber() || bonusNumber > LottoNumber.LAST_RANGE.getNumber()) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_RANGE);
         }
     }
+
 }

b956124b4ff0900e31c28b28d40263bf01a84704
LeeJaeHong <0915jhlee@daum.net>
2023-11-07 11:50:17 +0900

    comment: 중요로직 주석 추가
    "(줄수) 함수내용 (입력값) -> 리턴값" 으로 내용 추가

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constant.Prize;
 import lotto.input.InputParam;
 import lotto.print.PrintOutput;
 
@@ -10,6 +11,8 @@ import java.util.List;
 public class Game {
     static InputParam inputParam = new InputParam();
     static PrintOutput printOutput = new PrintOutput();
+
+    //(12줄) 전체 로직 실행 함수()
     public void game(){
         printOutput.printMoneyQuestion();
         int money = inputParam.inputMoney();
@@ -24,6 +27,8 @@ public class Game {
         double profit = calculateProfit(money, stats);
         printOutput.printStats(stats, profit);
     }
+
+    //(6줄) 랜덤로또번호 생성함수(개수) -> 랜덤 로또 리스트
     public List<Lotto> makeRandomLotto(int number){
         List<Lotto> randomLottos = new ArrayList<>();
         for (int i = 0; i < number; i++) {
@@ -33,6 +38,7 @@ public class Game {
         return randomLottos;
     }
 
+    //(5줄) 구매한 로또들과 입력로또 비교 함수(랜덤로또들, 입력번호, 보너스번호) -> 등수 배열
     public int[] compareRandomLottosAndInputLotto(List<Lotto> randomLottos, int[] inputNumbers, int bonusNumber){
         int[] results=new int[6];
         for (int i = 0; i < randomLottos.size(); i++) {
@@ -40,6 +46,7 @@ public class Game {
         }
         return results;
     }
+    //(10줄) 한 개의 랜덤로또와 입력값을 비교하는 함수(랜덤로또, 입력번호, 보너스번호) -> 등수
     public int compareOneLottoAndInputLotto(Lotto lotto, int[] inputNumbers, int bonusNumber){
         List<Integer> numbers = lotto.getNumbers();
         int cnt=0;
@@ -52,13 +59,14 @@ public class Game {
         if(cnt==3)result=5;
         return result;
     }
+    //(8줄) 수익률 계산함수(돈, 당첨수) -> 수익률
     public double calculateProfit(int money, int[] stats){
         int totalMoney=0;
-        totalMoney+=stats[1]*2000000000;
-        totalMoney+=stats[2]*30000000;
-        totalMoney+=stats[3]*1500000;
-        totalMoney+=stats[4]*50000;
-        totalMoney+=stats[5]*5000;
+        totalMoney+=stats[1]* Prize.MONEYOFFIRST.getMoney();
+        totalMoney+=stats[2]*Prize.MONEYOFSECOND.getMoney();
+        totalMoney+=stats[3]* Prize.MONEYOFTHIRD.getMoney();
+        totalMoney+=stats[4]*Prize.MONEYOFFOURTH.getMoney();
+        totalMoney+=stats[5]*Prize.MONEYOFFIFTH.getMoney();
         double profit=((double) totalMoney/(double) money)*100;
         return profit;
     }

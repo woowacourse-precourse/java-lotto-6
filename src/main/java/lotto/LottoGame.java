72302e681dce6d3acd2ee7ce2c6788ee6cbf6289
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-07 17:05:32 +0900

    Feat: Add getUserInputLotto and Create Lotto instance

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,2 +1,11 @@
-package lotto;public class LottoGame {
+package lotto;
+
+import java.util.List;
+
+public class LottoGame {
+    public static void main(String[] args) {
+        List<Integer> lottoNumbers = InputHandler.getUserInputlotto();
+
+        Lotto lotto = new Lotto(lottoNumbers);
+    }
 }

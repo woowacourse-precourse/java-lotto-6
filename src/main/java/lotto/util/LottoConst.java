e5fb82939b23591eae4b0a8da9cda73743000f7d
jsyeo97 <jsyeo97@naver.com>
2023-11-07 17:00:56 +0900

    refactor: 클래스의 역할에 따라 package 분리

---
+++ src/main/java/lotto/util/LottoConst.java
@@ -0,0 +1,8 @@
+package lotto.util;
+
+public abstract class LottoConst {
+    public static final int LOTTO_SIZE = 6;
+    public static final int FIRST_NUM = 1;
+    public static final int LAST_NUM = 45;
+    public static final int UNIT = 1000;
+}

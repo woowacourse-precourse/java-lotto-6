54d07282d86c3d1c3633bfcd20ed365f69b488a9
Chan Mi <rkdcksal02@gmail.com>
2023-11-07 20:08:36 +0900

    ResultView 클래스 - 결과를 출력하고 수익률을 계산

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -4,10 +4,20 @@ import lotto.domain.Lotto;
 import lotto.domain.Rank;
 import lotto.domain.WinningResult;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResultView {
+    // 결과 출력
+    public static void showResult(List<Lotto> lottoTickets, WinningResult winningResult) {
+        showPurchasedLottoTickets(lottoTickets);
+        showWinningResult(winningResult);
+        double earningRate = calculateEarningRate(winningResult.getTotalPrize(), lottoTickets.size());
+        showEarningRate(earningRate);
+    }
+
+    // 구매한 로또 티켓 목록 출력
     public static void showPurchasedLottoTickets(List<Lotto> lottoTickets) {
         System.out.println(lottoTickets.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottoTickets) {
@@ -15,8 +25,9 @@ public class ResultView {
         }
     }
 
+    // 당첨 결과 출력
     public static void showWinningResult(WinningResult winningResult) {
-        System.out.println("당첨 결과");
+        System.out.println("당첨 통계");
         System.out.println("---");
         List<Rank> sortedRanks = new ArrayList<>(List.of(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST));
         for (Rank rank : sortedRanks) {
@@ -24,21 +35,32 @@ public class ResultView {
         }
     }
 
+    // 각 등수별 당첨 결과 출력
     private static void showEachWinningResult(WinningResult winningResult, Rank rank) {
         int matchCount = rank.getMatchCount();
         int prize = rank.getPrize();
+        int count = winningResult.getCount(rank);
 
-        if (rank != Rank.SECOND) {
-            System.out.print(matchCount + "개 일치");
+        String resultMessage = matchCount + "개 일치";
+        if (rank == Rank.SECOND) {
+            resultMessage += ", 보너스 볼 일치";
         }
+        resultMessage += " (" + String.format("%,d원", prize) + ") - " + count + "개";
 
-        if (rank == Rank.FIFTH) {
-            System.out.print(" (보너스 볼 일치)");
-        }
-        System.out.println(" (" + String.format("%,d", prize) + "원) - " + winningResult.getCount(rank) + "개");
+        System.out.println(resultMessage);
     }
 
+    // 수익률 계산
+    private static double calculateEarningRate(int totalPrize, int ticketCount) {
+        if (ticketCount == 0) {
+            return 0.0;
+        }
+        return ((double) totalPrize / (ticketCount * 1000));
+    }
+
+    // 수익률 출력
     public static void showEarningRate(double earningRate) {
-        System.out.println("총 수익률은 " + String.format("%.1f", earningRate) + "%입니다.");
+        DecimalFormat df = new DecimalFormat("0.0%");
+        System.out.println("총 수익률은 " + df.format(earningRate) + "입니다.");
     }
 }

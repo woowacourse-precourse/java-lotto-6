2f683bed43f32170d24e240dd35e2f6aaac1b4dc
nayonsoso <panda0329@naver.com>
2023-11-08 03:00:42 +0900

    feat: 보너스 번호 초기화 기능 구현

--- src/main/java/model/Initializer.java
+++ src/main/java/model/Initializer.java
@@ -1,17 +1,29 @@
 package model;
 
 import constants.Constants;
+import exception.DuplicateBonusNumberException;
 import exception.InvalidWinningLottoFormatException;
+import exception.NonNumericBonusNumberException;
+import exception.OutOfLottoNumberRangeException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import lotto.Lotto;
 
 public class Initializer {
-    public Lotto initWinningLotto(String userInput){
+
+    public Lotto initWinningLotto(String userInput) {
         validateFormat(userInput);
         List<Integer> winningNumbers = convertToList(userInput);
         return new Lotto(winningNumbers);
+    }
+
+    public int initBonusNumber(String userInput, Lotto winningLotto) {
+        validateInt(userInput);
+        validateRange(userInput);
+        int bonusNum = Integer.parseInt(userInput);
+        validateNoDuplicates(bonusNum, winningLotto);
+        return bonusNum;
     }
 
     private void validateFormat(String userInput) {
@@ -21,9 +33,31 @@ public class Initializer {
         }
     }
 
-    private List<Integer> convertToList(String userInput){
+    private List<Integer> convertToList(String userInput) {
         return Arrays.stream(userInput.split(Constants.DELIMITER))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    private void validateInt(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new NonNumericBonusNumberException();
+        }
+    }
+
+    private void validateRange(String userInput) {
+        int bonusNum = Integer.parseInt(userInput);
+        if (bonusNum < Constants.LOTTO_RANGE_START || bonusNum > Constants.LOTTO_RANGE_END) {
+            throw new OutOfLottoNumberRangeException();
+        }
+    }
+
+    private void validateNoDuplicates(int bonusNum, Lotto winningLotto) {
+        boolean isDuplicate = winningLotto.getNumbers().contains(bonusNum);
+        if(isDuplicate){
+            throw new DuplicateBonusNumberException();
+        }
     }
 }

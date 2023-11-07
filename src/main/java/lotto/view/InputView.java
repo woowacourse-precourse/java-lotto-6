6e9c271b6f7b1d6bcc410fbaab789dfd80738cec
Yeop <syt05342@naver.com>
2023-11-07 16:10:23 +0900

    feat: 당첨 번호 입력 및 보너스 번호 입력 로직 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,13 +2,20 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import static lotto.exception.InputValidator.*;
-import static lotto.exception.MoneyValidator.validatePurchaseAmount;
+
+import static lotto.validator.InputValidator.*;
+import static lotto.validator.MoneyValidator.validatePurchaseAmount;
+import static lotto.validator.TokenizedInputValidator.*;
 
 public class InputView {
 
     public static final String USER_MONEY_INPUT_MSG = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_INPUT_MSG = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INPUT_MSG = "보너스 번호를 입력해 주세요.";
+    public static final String SEPARATOR = ",";
 
     public static long getLottoPayment() {
         printMessage(USER_MONEY_INPUT_MSG);
@@ -17,6 +24,34 @@ public class InputView {
         validateLottoPurchaseAmount(money);
         validatePurchaseAmount(Long.parseLong(money));
         return Long.parseLong(money);
+    }
+
+    public static List<Integer> getWinningNumber() {
+        printMessage(WINNING_NUMBER_INPUT_MSG);
+
+        String winningNumber = Console.readLine();
+        return convertStringToIntegerList(winningNumber);
+    }
+
+    public static int getBonusNumber() {
+        printMessage(BONUS_NUMBER_INPUT_MSG);
+
+        String bonusNumber = Console.readLine();
+        checkBonusNumber(bonusNumber);
+        return Integer.parseInt(bonusNumber);
+    }
+
+    public static List<Integer> convertStringToIntegerList(String winningNumber) {
+        check(winningNumber.split(SEPARATOR));
+
+        String[] numberStrings = winningNumber.split(SEPARATOR);
+        List<Integer> numberList = new ArrayList<>();
+
+        for (String numberStr : numberStrings) {
+            numberList.add(Integer.parseInt(numberStr.trim()));
+        }
+
+        return numberList;
     }
 
     public static void printMessage(String message) {

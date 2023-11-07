fb54262ad716321821b27f85fd6f8e9e54596777
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 16:42:58 +0900

    feat(BonusNumber): 스켈레톤 코드 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,2 +1,29 @@
-package lotto.domain;public class BonusNumber {
+package lotto.domain;
+
+import java.util.List;
+
+public class BonusNumber {
+
+    private final int bonusNumber;
+
+    public BonusNumber(String bonusNumber, List<Integer> lottoNumber) {
+        validate(bonusNumber, lottoNumber);
+        this.bonusNumber = Integer.parseInt(bonusNumber);
+    }
+
+    private void validate(String bonusNumber, List<Integer> lottoNumber) {
+        validateIsDigit(bonusNumber);
+        validateNumberRange(bonusNumber);
+        validateDuplication(bonusNumber, lottoNumber);
+    }
+
+    private void validateDuplication(String bonusNumber, List<Integer> lottoNumber) {
+    }
+
+    private void validateNumberRange(String bonusNumber) {
+    }
+
+    private void validateIsDigit(String bonusNumber) {
+        
+    }
 }

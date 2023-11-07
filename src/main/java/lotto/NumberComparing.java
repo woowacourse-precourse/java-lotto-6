692ee8d69f3c8c750e3bd473de299b052c49e81a
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-07 23:38:57 +0900

    feat : 추첨기 클래스 생성 및 생성자 구현

---
+++ src/main/java/lotto/NumberComparing.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import java.util.List;
+
+public class NumberComparing {
+    private final List<Integer> lottoNumbers;
+
+    public NumberComparing(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+
+}

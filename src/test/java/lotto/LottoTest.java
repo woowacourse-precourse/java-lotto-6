5711dded1743370f0447ae790e6dd3cdb82e4685
jwp345 <jwp135@naver.com>
2023-11-07 15:23:20 +0900

    feat(valid): 로또 유효성 검사
    유효성 검사:
    - 6개가 맞는지 검사
    - 중복된 숫자가 있는지 검사
    - 1~45 사이의 숫자인지 검사

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,24 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 개수가 6개가 안되면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 1미만이면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 0, 6, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 45보다 크면 예외가 발생한다.")
+    @Test
+    void createLottoByOverNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 5, 6, 47)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

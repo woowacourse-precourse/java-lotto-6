65a223ed05ca875ab1ecd59b9300fa3ee12bbd3f
박준영 <wnsdud6969@naver.com>
2023-11-07 17:49:34 +0900

    feat(OutputView): add new feature
    Add new feature to OutputView
    - print information of issued lottos
    - print new empty line

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lottos;
+
 public class OutputView {
 
     private static final String NUMBER_OF_LOTTO_PURCHASED_MESSAGE = "%d개를 구매했습니다.";
@@ -13,5 +16,21 @@ public class OutputView {
 
     private static void printNumberOfLottoPurchased(int numberOfLottoPurchased) {
         printWithFormat(NUMBER_OF_LOTTO_PURCHASED_MESSAGE, numberOfLottoPurchased);
+    }
+
+    public static void printIssuedLottosInformation(Lottos lottos) {
+        printNewEmptyLine();
+
+        List<List<Integer>> lottosNumbers = lottos.getLottosNumbers();
+        printNumberOfLottoPurchased(lottosNumbers.size());
+
+        for (List<Integer> lottoNumbers : lottosNumbers) {
+            System.out.println(lottoNumbers);
+        }
+        printNewEmptyLine();
+    }
+
+    private static void printNewEmptyLine() {
+        System.out.println();
     }
 }

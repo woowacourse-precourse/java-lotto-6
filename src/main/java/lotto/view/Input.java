52d2b217531fa3bd06cea097f258bb4b81afe7d0
anjdydhody <a01066444534@gmail.com>
2023-11-07 22:14:51 +0900

    feat: inputAmount method

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+    int inputAmount() {
+        int amount = Integer.parseInt(Console.readLine());
+        validateAmount(amount);
+    }
 }

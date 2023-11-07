8384f9c392cf7a8ce93707c8375edfd99f8149bd
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-07 21:55:46 +0900

    feat(lotto): 예외처리를 위한 메세지 추가

--- src/main/java/lotto/values/ExceptionMessage.java
+++ src/main/java/lotto/values/ExceptionMessage.java
@@ -1,16 +1,21 @@
 package lotto.values;
 
-public enum ErrorMessage {
+import static lotto.values.LottoInformation.MAX_VALUE;
+
+public enum ExceptionMessage {
     NOT_IN_NUMBER_RANGE("1번부터 45번까지만 입력할 수 있습니다."),
     NOT_LOTTO_TOTAL_NUMBER("로또는 6개의 숫자를 뽑아야합니다."),
     ONLY_ONE_FOR_BONUS_NUMBER("보너스 번호는 하나만 입력할 수 있습니다."),
     NOT_CORRECT_FORM("입력 양식에 맞지 않습니다."),
+    NOT_NULL("입력값이 없습니다."),
     NOT_NUMBER("숫자만 입력할 수 있습니다."),
     CAN_NOT_PURCHASE("1000원 단위로만 구매할 수 있습니다."),
-    TOO_MUCH_MONEY("로또는 최대 %d장까지만 구매할 수 있습니다.");
+    TOO_MUCH_MONEY("로또는 최대 "+MAX_VALUE.getValue()+"원까지 구매할 수 있습니다."),
+    ONLY_NUMBER("^[0-9]*$"),
+    ERROR("[ERROR] ");
     private final String message;
 
-    ErrorMessage(String message) {
+    ExceptionMessage(String message) {
         this.message = message;
     }
 
--- src/main/java/lotto/values/LottoInformation.java
+++ src/main/java/lotto/values/LottoInformation.java
@@ -3,6 +3,7 @@ package lotto.values;
 public enum LottoInformation {
 
     LOTTO_PRICE(1000),
+    MAX_VALUE(100000),
     START_LOTTO_NUMBER(1),
     LAST_LOTTO_NUMBER(45),
     TOTAL_LOTTO_NUMBER(6),

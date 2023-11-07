febde43de24e39b997d91ebc9b37f38b93d0e1c7
sudhdkso <waga1009@gmail.com>
2023-11-07 23:31:47 +0900

    로또 목록에 대한 클래스 추가

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos create(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        lottos.forEach(lotto -> sb.append(lotto.toString()).append("\n"));
+        return sb.toString();
+    }
+}

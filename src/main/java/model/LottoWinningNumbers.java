486fbcc9041a86596f451a04548175193660fecc
jaegyeongkim <dongji_11@naver.com>
2023-11-07 17:02:06 +0900

    refactor: 문자열 변환의 책임을 Converter로 이동

--- src/main/java/model/LottoWinningNumbers.java
+++ src/main/java/model/LottoWinningNumbers.java
@@ -1,19 +1,16 @@
 package model;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import utils.Converter;
 import utils.NumberValidation;
 
 public class LottoWinningNumbers {
     private Lotto winningNumbers;
     private int bonusNumber;
-    private static final String DELIMITER = ",";
     private static final String DUPLICATION_BONUS_AND_WINNING = "[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.";
 
-    public LottoWinningNumbers(String winningNumbers, String bonusNumber) {
-        this.winningNumbers = stringToLotto(winningNumbers);
+    public LottoWinningNumbers(Lotto winningNumbers, String bonusNumber) {
+        this.winningNumbers = winningNumbers;
         validationBonusNumber(Converter.stringToInt(bonusNumber));
         this.bonusNumber = Converter.stringToInt(bonusNumber);
     }
@@ -27,22 +24,14 @@ public class LottoWinningNumbers {
     }
 
     public void validationBonusNumber(int bonusNumber) {
-        NumberValidation.isOverMaxNumber(bonusNumber);
-        NumberValidation.isUnderMinNumber(bonusNumber);
-        duplicationBonusNumber(bonusNumber);
+          NumberValidation.isOverMaxNumber(bonusNumber);
+            NumberValidation.isUnderMinNumber(bonusNumber);
+            duplicationBonusNumber(bonusNumber);
     }
 
-    public void duplicationBonusNumber(int bonusNumber){
-        if(winningNumbers.getNumbers().contains(bonusNumber)){
+    public void duplicationBonusNumber(int bonusNumber) {
+        if (winningNumbers.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATION_BONUS_AND_WINNING);
         }
-    }
-    public String[] splitByDelimiter(String winningNumbers) {
-        return winningNumbers.split(DELIMITER);
-    }
-
-    public Lotto stringToLotto(String winningNumbers) {
-        return new Lotto(Arrays.stream(splitByDelimiter(winningNumbers)).map(Converter::stringToInt)
-                .collect(Collectors.toList()));
     }
 }
--- src/main/java/utils/Converter.java
+++ src/main/java/utils/Converter.java
@@ -1,8 +1,20 @@
 package utils;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import model.Lotto;
+
 public class Converter {
 
     public static int stringToInt(String input) {
         return Integer.parseInt(input);
+    }
+    public static String[] splitByDelimiter(String winningNumbers, String delimiter) {
+        return winningNumbers.split(delimiter);
+    }
+
+    public static Lotto stringToLotto(String winningNumbers, String delimiter) {
+        return new Lotto(Arrays.stream(splitByDelimiter(winningNumbers, delimiter)).map(Converter::stringToInt)
+                .collect(Collectors.toList()));
     }
 }

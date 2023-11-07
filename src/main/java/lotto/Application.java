ab0c3527994f443e3973c8a704dd907b6770088c
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 13:13:13 +0900

    feat(Application): 로또 구매 개수만큼 생성된 로또 객체 번호들 저장할 리스트 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,8 +11,9 @@ public class Application {
         int purchaseAmount;
         int purchaseCount = 0;
 
-        List<Integer> lottoNumbers;
-        List<Integer> eachLottoNumber;
+        List<Integer> lottoNumbers = null;
+        List<Integer> eachLottoNumber = null;
+        List<List<Integer>> lottos = null;
 
         System.out.println("구입금액을 입력해 주세요.");
 
@@ -31,13 +32,14 @@ public class Application {
 
 
         //로또 개수만큼 로또 객체의 로또 번호 출력
-        Process process = new Process();
+        Process startProcess = new Process();
         //로또 개수만큼 반복 => 나중에 개별 함수화 리팩토링!
         for(int i = 0; i < purchaseCount; i++){
-            lottoNumbers = process.generateLottoNumbers();
-            process.sortLottoNumbers(lottoNumbers);
+            lottoNumbers = startProcess.generateLottoNumbers();
+            startProcess.sortLottoNumbers(lottoNumbers);
             Lotto lotto = new Lotto(lottoNumbers);
             eachLottoNumber = lotto.getLottoNumbers();
+            lottos.add(eachLottoNumber);
             System.out.println(eachLottoNumber);
         }
 
@@ -76,11 +78,53 @@ public class Application {
 
         System.out.println();
 
+        ///
+        List<Integer> winRecordBoard = new ArrayList<>();
+        Integer firstPlace = Integer.valueOf(0);
+        Integer secondPlace = Integer.valueOf(0);
+        Integer thirdPlace = Integer.valueOf(0);
+        Integer fourthPlace = Integer.valueOf(0);
+        Integer fifthPlace = Integer.valueOf(0);
+
+        winRecordBoard.set(1, firstPlace);
+        winRecordBoard.set(2, secondPlace);
+        winRecordBoard.set(3, thirdPlace);
+        winRecordBoard.set(4, fourthPlace);
+        winRecordBoard.set(5, fifthPlace);
+
+        ///
+        Process middleProcess = new Process();
+        List<Object> result = middleProcess.compareLottos(eachLottoNumber, winningNumbers, bonusNumber);
+
+            int mainMatchNumber = (int) result.get(0);
+            boolean matchedBonusNumber = (boolean) result.get(1);
+
+            if (mainMatchNumber == 3) {
+                winRecordBoard.set(5, ++fifthPlace);
+            }
+            if (mainMatchNumber == 4) {
+                winRecordBoard.set(4, ++fourthPlace);
+            }
+            if (mainMatchNumber == 5 && !matchedBonusNumber) {
+                winRecordBoard.set(3, ++thirdPlace);
+            }
+
+            if (mainMatchNumber == 5 && matchedBonusNumber) {
+                winRecordBoard.set(2, ++secondPlace);
+            }
+
+            if (mainMatchNumber == 6) {
+                winRecordBoard.set(1, ++firstPlace);
+            }
 
 
         //당첨 통계 출력
         System.out.println("당첨 통계");
         System.out.println("---");
+
+
+
+
 
 
 

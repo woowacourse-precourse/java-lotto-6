e1077f35de79a861f22eedc5eef3cc67e93faacf
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-07 19:36:26 +0900

    feat: prize enum first commit
    prize enum first commit

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,67 @@
+package lotto.domain;
+
+public enum Prize {
+
+    NONE(0, 0, " "),
+    THREE_MATCH(3, 5000, "3개 일치 (5,000원)"),
+    FOUR_MATCH(4, 50000, "4개 일치 (50,000원)"),
+    FIVE_MATCH(5, 1500000, "5개 일치 (1,500,000원)"),
+    FIVE_BONUS_MATCH(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIX_MATCH(6, 2000000000, "6개 일치 (2,000,000,000원)");
+
+    private final int matches;
+    private final int prizeAmount;
+    private final String description;
+
+
+    Prize(int matches, int prizeAmount, String description) {
+        this.matches = matches;
+        this.prizeAmount = prizeAmount;
+        this.description = description;
+    }
+
+    public int getMatches() {
+        return matches;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public static Prize findMatchingPrize(int matches, boolean hasBonus) {
+        if (isLessThreeMatches(matches)) {
+            return NONE;
+        }
+        if (isFiveMatches(matches)) {
+            return BonusOrNot(hasBonus);
+        }
+        for (Prize prize : Prize.values()) {
+            if (isMatchCountEqual(prize, matches)) {
+                return prize;
+            }
+        }
+        return NONE;
+    }
+
+    public static boolean isLessThreeMatches(int matches) {
+        return matches < 3;
+    }
+
+    public static boolean isFiveMatches(int matches) {
+        return matches == 5;
+    }
+
+    public static boolean isMatchCountEqual(Prize prize, int matches) {
+        return prize.matches == matches;
+    }
+
+    public static Prize BonusOrNot(boolean hasBonus) {
+        if (hasBonus) {
+            return FIVE_BONUS_MATCH;
+        }
+        return FIVE_MATCH;
+    }
+}

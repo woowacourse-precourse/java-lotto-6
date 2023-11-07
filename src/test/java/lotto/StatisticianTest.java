66b256e781e67875da6cc4472d45173ad04cc408
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-07 13:46:01 +0900

    test(statistician): 로또 목록과 당첨 번호로부터 정상적으로 통계를 내는지 테스트

---
+++ src/test/java/lotto/StatisticianTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class StatisticianTest {
+    @Test
+    void 로또통계가_정상적으로_생성되는지_테스트() {
+        List<Lotto> lottoList = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7))
+        );
+
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        assertThat(new Statistician().makeLottoStatistics(lottoList, winningLotto))
+                .isEqualTo(new WinningStatistics(Map.of(Ranking.FIRST, 1, Ranking.SECOND, 1)));
+    }
+}

9abb70e3e9adb228a2e3f218e2c05e50c04ce1a4
Yeop <syt05342@naver.com>
2023-11-07 17:19:13 +0900

    test: 로또 생성 시 오름차순 정렬 테스트

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -4,7 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class LottoMachineTest {
@@ -17,7 +23,22 @@ class LottoMachineTest {
         long total_lotto_count = money / LOTTO_TICKET_PRICE;
 
         List<Lotto> lottoList = LottoMachine.make(money);
-        Assertions.assertThat(lottoList.size()).isEqualTo(total_lotto_count);
+        assertThat(lottoList.size()).isEqualTo(total_lotto_count);
+    }
+
+    @DisplayName("오름차순으로 정상 정렬 되는지 테스트")
+    @Test
+    void sortTest() {
+        long money = 10000;
+        List<Lotto> lottoList = LottoMachine.make(money);
+
+        for (Lotto lotto : lottoList) {
+            List<Integer> lottoNumbers = lotto.getNumbers();
+            List<Integer> sortedLottoNumbers = new ArrayList<>(lottoNumbers);
+            Collections.sort(sortedLottoNumbers);
+            assertThat(lottoNumbers).isEqualTo(sortedLottoNumbers);
+        }
+
     }
 
 

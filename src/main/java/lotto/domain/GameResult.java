ea153b877e6302da7dde959c39d0bc484a555f82
JoonKyo <joonkyolee@gmail.com>
2023-11-07 22:35:01 +0900

    docs, feat(GameResult): 로또 등수, 당첨금, 통계를 계산하는 메서드 추가

--- src/main/java/lotto/domain/GameResult.java
+++ src/main/java/lotto/domain/GameResult.java
@@ -1,2 +1,50 @@
-package lotto.domain;public class GameResult {
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameResult {
+    private static final int INITIAL_AMOUNT = 0;
+    private static final int DECIMAL_POINT = 2;
+    private static final int PERCENTAGE_CONVERSION = 100;
+    private final List<Lotto> playerLotto;
+    private final List<LottoRank> lottoResult;
+
+    private GameResult(List<Lotto> playerLotto) {
+        this.playerLotto = playerLotto;
+        lottoResult = new ArrayList<>();
+    }
+
+    public GameResult of(List<Lotto> playerLotto) {
+        return new GameResult(playerLotto);
+    }
+
+    public List<LottoRank> calculateRank() {
+        for (Lotto eachLotto : playerLotto) {
+            lottoResult.add(WinningLottoNumber.calculateLottoPrize(eachLotto));
+        }
+
+        return lottoResult;
+    }
+
+    public long calculateWinningMoney() {
+        long winningMoney = INITIAL_AMOUNT;
+
+        for (LottoRank eachRank : lottoResult) {
+            winningMoney += eachRank.getWinningAmount();
+        }
+
+        return winningMoney;
+    }
+
+    public String calculateYield(long winningMoney, int purchaseAmount) {
+        double profit = (double) winningMoney / purchaseAmount * PERCENTAGE_CONVERSION;
+
+        BigDecimal yield = new BigDecimal(profit)
+                .setScale(DECIMAL_POINT, RoundingMode.HALF_UP);
+
+        return yield.toString();
+    }
 }

191b5613be843f0a5b4e8bc51c5580c1660f01b1
junseoks-99 <angec5645@naver.com>
2023-11-07 22:09:27 +0900

    feat: 로또 5등 여부 확인 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String[] args) {
 
         List<Integer> list = List.of(1,2,3,4,5,6);
-        List<Integer> list2 = List.of(1,2,3,4,7,8);
+        List<Integer> list2 = List.of(1,2,3,7,8,9);
         Lotto lotto = new Lotto(list);
 
         Integer sameSount = lotto.getSameNumberCount(list2);

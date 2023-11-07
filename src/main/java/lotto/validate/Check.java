da7faf4bcf2a14946308d21a149e402be8195963
lynjun97 <lynjun97@naver.com>
2023-11-07 20:57:24 +0900

    Revert "Revert "Feat: 당첨당첨번호 검증 추""
    This reverts commit d6cfdf3e0cd9e41cfb870002872c16eb98992396.

---
+++ src/main/java/lotto/validate/Check.java
@@ -0,0 +1,31 @@
+package lotto.validate;
+
+import lotto.util.ErrorMessage;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Check {
+
+    private static final int Start_Number = 1;
+    private static final int Final_Number = 45;
+
+    public static void numberDuplicate(List<Integer> winningNumber, List<Integer> bonusNumber) {
+        if (winningNumber.contains(bonusNumber.get(0))) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
+    public static void bonusNumberCount(List<Integer> winningNumber) {
+        if (winningNumber.size() != 1) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_COUNT_ERROR.getMessage());
+        }
+    }
+
+    public static void rightRange(List<Integer> winningNumber) {
+        winningNumber.stream().filter(number -> number < Start_Number || number > Final_Number).forEach(number -> {
+            throw new IllegalArgumentException(ErrorMessage.RANGE_ERROR.getMessage());
+        });
+    }
+}

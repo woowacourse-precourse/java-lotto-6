541be27c746cbbf630418feaf290e51c86994644
DongHwi Kim <ken7905@ajou.ac.kr>
2023-11-07 16:19:43 +0900

    feat: create OutputView

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,21 @@
 package lotto.view;
 
+import lotto.Lotto;
+
+import java.util.List;
+
 public class OutputView {
+    private static final String NUMBER_OF_BUY_LOTTO = "%d개를 구매했습니다.";
+
+    public static void printNumberOfBuyLotto(int numberOfLottoBuy){
+        System.out.printf(NUMBER_OF_BUY_LOTTO,numberOfLottoBuy);
+    }
+
+    public static void printLottoNums(List<Lotto> lottos){
+        for(Lotto lotto : lottos){
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+
 }

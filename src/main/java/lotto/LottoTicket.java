86d67c66afc244b001eea58c4f1877d3964647ae
binary_ho <dfghcvb11@naver.com>
2023-11-07 23:01:33 +0900

    feat : 정해진 갯수만큼 로또가 적힌 LottoTicket을 발급하는 LottoTicket 객체 생성

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -3,11 +3,17 @@ package lotto;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public record LottoTicket(List<Lotto> lottos) {
+public class LottoTicket {
+
+    private final List<Lotto> lottos;
+
+    private LottoTicket(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
 
     public static LottoTicket issueLottoTicket(int lottoCount) {
         List<Lotto> lottoNumbers = IntStream.range(0, lottoCount)
-            .mapToObj(i -> Lotto.issueLotto())
+            .mapToObj(i -> Lotto.issueAutoPickNumbersLotto())
             .toList();
         return new LottoTicket(lottoNumbers);
     }

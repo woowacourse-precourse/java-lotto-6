b9b35b3c1fef8c29ad3d21b8b3c3121b7a6778a3
jae woo Jung <awfjol2008@naver.com>
2023-11-08 03:09:25 +0900

    feat: 사용자 로또 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.InputUtil;
+import lotto.view.validator.InputValidator;
+
+import java.util.List;
 
 import static java.lang.Integer.*;
 
@@ -12,5 +16,11 @@ public class InputView {
 
     protected Integer readPurchaseAmount() {
         return parseInt(Console.readLine());
+    }
+
+    protected List<Integer> readLottoNumbers() {
+        String input = Console.readLine();
+        InputValidator.validateLottoNumber(input);
+        return InputUtil.convertToList(input);
     }
 }

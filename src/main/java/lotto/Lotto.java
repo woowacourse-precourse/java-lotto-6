28e33728381ad62f006bc86839c817f835e3d520
gyeommm <dkndob02@naver.com>
2023-11-07 16:30:33 +0900

    로또 번호 범위 확인, 중복 확인 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,13 +13,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        // 로또 번호가 6개인지 확인
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 무조건 6개여야 합니다.");
+        }
+
+        // 로또 번호가 모두 [1, 45] 구간에 있는지 확인
+        for(Integer number : numbers){
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자여야 합니다.");
+            }
+        }
+
+        // 로또 번호가 중복되는지 확인
+        Set<Integer> unique_numbers = new HashSet<>();
+        for(Integer number : numbers){
+            if(unique_numbers.contains(number)){
+                throw new IllegalArgumentException("6개의 번호들은 서로 중복될 수 없습니다.");
+            }
+            unique_numbers.add(number);
         }
     }
 
-
-
     // TODO: 추가 기능 구현
+
 
 }

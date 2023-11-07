d919a18fc439223d439a1c7e81ef96b791b384db
IToriginal <gitshineit@gmail.com>
2023-11-07 22:39:00 +0900

    :sparkles: [feature] #2 로또 당첨 통계 출력에 사용할 enum 클래스 생성

--- src/main/java/lotto/util/rule/RankingRule.java
+++ src/main/java/lotto/util/rule/RankingRule.java
@@ -1,4 +1,27 @@
 package lotto.util.rule;
 
 public enum RankingRule {
+
+    RANK_FIRST("6개 일치", "2,000,000,000"),
+    RANK_SECOND("5개 일치, 보너스 볼 일치", "30,000,000"),
+    RANK_THIRD("5개 일치", "1,500,000"),
+    RANK_FOURTH("4개 일치", "50,000"),
+    RANK_FIFTH("3개 일치", "5,000");
+
+    private final String content;
+    private final String prize;
+
+    RankingRule(String content, String prize) {
+        this.content = content;
+        this.prize = prize;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+
 }

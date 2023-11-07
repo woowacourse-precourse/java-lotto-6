319de33e988187911924978b81ea18a53a63378e
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-11-07 20:27:04 +0900

    Feat: [Lotto 클래스] 당첨 로또 번호 기능 구현

--- src/main/java/lotto/controller/PlayLotto.java
+++ src/main/java/lotto/controller/PlayLotto.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseAmount;
 import lotto.domain.PurchaseLotto;
 import lotto.view.Input;
@@ -13,11 +14,13 @@ public class PlayLotto {
     Output output = new Output();
     PurchaseAmount purchaseAmount;
     PurchaseLotto purchaseLotto;
+    Lotto lotto;
 
     public void playLotto() {
         int purchaseLottoCount = getPurchaseLottoCount(input, output);
         int purchaseLottoAmount = getPurchaseLottoAmount();
         ArrayList<List<Integer>> purchaseLottoNumbers = getPurchaseLottoNumbers(output, purchaseLottoCount);
+        List<Integer> winningNumbers = getWinningNumbers(input);
     }
 
     private int getPurchaseLottoCount(Input input, Output output) {
@@ -49,5 +52,24 @@ public class PlayLotto {
         ArrayList<List<Integer>> purchaseLottoNumbers = purchaseLotto.getPurchaseLottoNumbers();
         output.printLottoNumbers(purchaseLottoNumbers);
         return purchaseLotto.getPurchaseLottoNumbers();
+    }
+
+    private List<Integer> getWinningNumbers(Input input) {
+        List<Integer> winningNumbers;
+        while (true) {
+            try {
+                winningNumbers = handleWinningNumbers(input);
+                lotto = new Lotto(winningNumbers);
+                return lotto.getNumbers();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> handleWinningNumbers(Input input) {
+        String winningNumberInput = input.winningNumber();
+        lotto = new Lotto(winningNumberInput);
+        return lotto.getNumbers();
     }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
+
+    private static final String NULL = "";
+    private static final String SPACE = " ";
+    private static final String COMMA = ",";
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int COUNT_LOTTO_NUMBER = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +17,100 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public Lotto(String winningNumberInput) {
+        winningNumberInput = removeSpaces(winningNumberInput);
+        List<String> winningNumbers = stringToList(winningNumberInput);
+        validateInput(winningNumbers);
+        validateDuplicate(winningNumbers);
+        this.numbers = stringToInt(winningNumbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private String removeSpaces(String winningNumbers) {
+        return winningNumbers.replace(SPACE, NULL);
+    }
+
+    private List stringToList(String inputNumbers) {
+        return Arrays.asList(inputNumbers.split(COMMA));
+    }
+
+    private void validateInput(List<String> winningNumbersInput) {
+        try {
+            for(String winningNumberInput : winningNumbersInput) {
+                checkNotNull(winningNumberInput);
+                checkInteger(winningNumberInput);
+            }
+        } catch (IllegalArgumentException e) {
+            throw e;
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkNotNull(String input) {
+        if(input.equals(NULL) || input.isEmpty())
+            ExceptionMessage.NULL_INPUT.throwException();
+    }
+
+    private void checkInteger(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                ExceptionMessage.INVALID_NUMBER.throwException();
+            }
+        }
+    }
+
+    private void validateDuplicate(List<String> winningNumbersInput) {
+        try {
+            checkDuplicates(winningNumbersInput);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    private void checkDuplicates(List<String> inputs) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (String input : inputs) {
+            if (uniqueNumbers.contains(input)) {
+                ExceptionMessage.LOTTO_NUMBER_DUPLICATES.throwException();
+            }
+        }
+    }
+
+    private List<Integer> stringToInt(List<String> winningNumbersInput) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for(String winningNumberInput : winningNumbersInput) {
+            int number = stringToInt(winningNumberInput);
+            winningNumbers.add(number);
+        }
+        return winningNumbers;
+    }
+
+    private int stringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private void validate(List<Integer> numbers) {
+        try {
+            checkSixNumbers(numbers);
+            checkInRange(numbers);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    private void checkSixNumbers(List<Integer> numbers) {
+        if (numbers.size() != COUNT_LOTTO_NUMBER) {
+            ExceptionMessage.INVALID_LOTTO_WINNING_SIZE.throwException();
+        }
+    }
+
+    private void checkInRange(List<Integer> numbers) {
+        int minNumber = Collections.min(numbers);
+        int maxNumber = Collections.max(numbers);
+        if (minNumber < START_LOTTO_NUMBER || END_LOTTO_NUMBER < maxNumber) {
+            ExceptionMessage.OUT_OF_RANGE.throwException();
+        }
+    }
 }

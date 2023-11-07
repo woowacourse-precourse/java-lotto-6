13d460d6e01ef0491df44c02c0b48eb1a8f2520c
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-07 12:55:24 +0900

    feat(LottoMachine): increase value of rankContainer when rank is given

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    //유저와 발행된 로또 사이의 같은 로또 수를 계산한다
 
     public static int calculateMatchLottoNumber(UserLotto userLotto, Lotto winningLotto) {
         int count = 0;
@@ -26,6 +25,8 @@ public class Calculator {
     public void match(UserLotto userLotto, WinningLottos winninglottos) {
         for (Lotto lotto : winninglottos.getWiningLottos()) {
             Rank rank = Rank.valueOf(calculateMatchLottoNumber(userLotto, lotto), lotto.isContain(userLotto.getBonusNumber()));
+            RankContainer rankContainer = new RankContainer();
+            rankContainer.incRank(rank);
         }
     }
 }

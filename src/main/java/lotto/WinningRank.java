47b0abba496f1237f97dc057e08e22e9bfdb3380
HeoManBo <iovesssr__@naver.com>
2023-11-07 23:37:26 +0900

    Refactor : 당첨 등수 Enum 클래스 수정

--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -1,30 +1,45 @@
 package lotto;
 
-public enum Winning {
-    JACKPOT(6, 2_000_000_000, false),
-    SECOND_WINNER(5, 30_000_000, true),
-    THIRD_WINNER(5, 1_500_000, false),
-    FOURTH_WINNER(4, 50_000, false),
-    FIFTH_WINNER(3, 5_000, false),
-    NO_LUCK(0, 0, false);
+import java.util.Arrays;
 
-    private final int result;
+public enum WinningRank {
+    JACKPOT(6, 2_000_000_000, false, "6개 일치 (2,000,000,000)원 - "),
+    SECOND_WINNER(5, 30_000_000, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD_WINNER(5, 1_500_000, false, "5개 일치 (1,500,000원) - "),
+    FOURTH_WINNER(4, 50_000, false, "4개 일치 (50,000원) - "),
+    FIFTH_WINNER(3, 5_000, false, "3개 일치 (5,000원) - "),
+    NO_LUCK(0, 0, false, null);
+
+    private final int count;
     private final int reward;
-    private boolean bonusNumberCorrect;
-    Winning(int result, int reward, boolean bonusNumberCorrect){
-        this.result = result;
+    private final boolean matchedBonusNumber;
+    private final String message;
+    WinningRank(int count, int reward, boolean matchedBonusNumber, String message){
+        this.count = count;
         this.reward = reward;
-        this.bonusNumberCorrect = bonusNumberCorrect
+        this.matchedBonusNumber = matchedBonusNumber;
+        this.message = message;
     }
 
-    public int getResult() {
-        return result;
+    public int getCount() {
+        return count;
     }
 
     public int getReward() {
         return reward;
     }
 
+    public boolean isMatchedBonusNumber() {
+        return matchedBonusNumber;
+    }
 
-    public static Winning find
+    public String getMessage() {
+        return message;
+    }
+
+    public static WinningRank findWinningRank(int matchCount, boolean bonusNumberCorrect){
+        return Arrays.stream(values()).filter(winningRank ->
+            winningRank.getCount() == matchCount && bonusNumberCorrect == winningRank.isMatchedBonusNumber()
+        ).findFirst().orElse(NO_LUCK);
+    }
 }

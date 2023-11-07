5968149dac66c157900222c8c0b6dca67c939bb6
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-11-08 00:38:57 +0900

    fix(LottoNumberMaker): 중복 검증 기능 제거
     pickUniqueNumbersInRange 함수 이름을 제대로 안읽어 봤음 ㅋㅋㅋ

--- src/test/java/lotto/LottoNumberMakerTest.java
+++ src/test/java/lotto/LottoNumberMakerTest.java
@@ -5,24 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoNumberMakerTest {
-
-    @DisplayName("getLottoNumbers 값 중복 검증 테스트")
-    @Test
-    void validateGetLottoNumbersByNotDuplicated() {
-
-        List<Integer> numbers = getLottoNumbers();
-
-        Set<Integer> notDuplicateNumbers = new HashSet<>(numbers);
-
-        assertThat(notDuplicateNumbers.size()).isEqualTo(numbers.size());
-    }
 
     @DisplayName("getLottoNumbers 값 정렬 검증 테스트")
     @Test

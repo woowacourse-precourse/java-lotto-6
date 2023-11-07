24483e64863d562b533bdb8f00184409fdc24672
박승민 <tmddudals369@naver.com>
2023-11-08 01:32:55 +0900

    feat(winningNumberValidator): 중복된 번호가 존재하는지 검증하는 기능 구현

--- src/main/java/lotto/validation/WinningNumberValidator.java
+++ src/main/java/lotto/validation/WinningNumberValidator.java
@@ -1,7 +1,12 @@
 package lotto.validation;
 
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.Number;
+
 public class WinningNumberValidator {
     private static final String MULTIPLE_WINNING_NUMBER_DELIMITER = ",";
+
     private WinningNumberValidator() {
     }
 
@@ -13,5 +18,16 @@ public class WinningNumberValidator {
         if (!winningNumbers.contains(MULTIPLE_WINNING_NUMBER_DELIMITER)) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표를 구분으로 입력해야 합니다.");
         }
+    }
+
+    public static void validateDuplicateWinningNumber(List<Number> winningNumbers) {
+        if (isDuplicate(winningNumbers)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복이 존재할 수 없습니다.");
+        }
+    }
+
+    private static boolean isDuplicate(List<Number> winningNumbers) {
+        return winningNumbers.stream()
+            .anyMatch(winningNumber -> Collections.frequency(winningNumbers, winningNumber) > 1);
     }
 }

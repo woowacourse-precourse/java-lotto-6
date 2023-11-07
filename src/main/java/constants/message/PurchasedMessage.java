9027f4a287d1ac8f98e1aa60b7b9b741142a9125
nayonsoso <panda0329@naver.com>
2023-11-08 05:51:49 +0900

    refactor: 상수 public화

--- src/main/java/constants/message/PurchasedMessage.java
+++ src/main/java/constants/message/PurchasedMessage.java
@@ -1,8 +1,8 @@
 package constants.message;
 
 public class PurchasedMessage {
-    private static final String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
-    private static final String PURCHASED_LOTTO_START = "[";
-    private static final String PURCHASED_LOTTO_DELIMITER = ",";
-    private static final String PURCHASED_LOTTO_END = "]";
+    public static final String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
+    public static final String PURCHASED_LOTTO_START = "[";
+    public static final String PURCHASED_LOTTO_DELIMITER = ",";
+    public static final String PURCHASED_LOTTO_END = "]";
 }
--- src/main/java/constants/message/StatisticsMessage.java
+++ src/main/java/constants/message/StatisticsMessage.java
@@ -1,8 +1,8 @@
 package constants.message;
 
 public class StatisticsMessage {
-    private static final String START = "당첨 통계";
-    private static final String START_LINE = "---";
-    private static final String CONTENT = "%d개 일치 (%s원) - %d개";
-    private static final String RESULT = "총 수익률은 %.2f%입니다.";
+    public static final String START = "당첨 통계";
+    public static final String START_LINE = "---";
+    public static final String CONTENT = "%d개 일치 (%s원) - %d개";
+    public static final String RESULT = "총 수익률은 %.2f%입니다.";
 }

79130684838f4ba1a053b62fa97028327a4b3996
sw801733 <sw801733@naver.com>
2023-11-07 21:49:52 +0900

    feat : LottoResult 당첨 번호와 비교 기능 구현
    - 당첨 번호와 로또 번호 비교를 위한 cmpLotto 메서드 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,9 +1,36 @@
 package lotto.domain;
 
-public class LottoResult {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-    public LottoResult() {
-        return;
+public class LottoResult {
+    private final int LottoPurchaseCnt;
+    private final List<Integer> numbers;
+    private final List<List<Integer>> RandomNumbers;
+    private final List<Integer> LottoResult;
+
+    public LottoResult(List<Integer> numbers, List<List<Integer>> RandomNumbers, int LottoPurchaseCnt) {
+        this.LottoPurchaseCnt =LottoPurchaseCnt;
+        this.numbers = numbers;
+        this.RandomNumbers = RandomNumbers;
+        this.LottoResult = cmpLotto();
+    }
+
+    private List<Integer> cmpLotto() {
+        List<Integer> LottoResult = new ArrayList<>();
+
+        for(int i = 0; i < LottoPurchaseCnt; i++) {
+            int result = 0;
+            for (int j = 0; j < 6; j++) {
+                if (Objects.equals(numbers.get(j), RandomNumbers.get(i).get(j))) {
+                    result++;
+                }
+            }
+            LottoResult.add(result);
+        }
+
+        return LottoResult;
     }
 
 }

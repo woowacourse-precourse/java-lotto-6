9736ce3983a873f26d6472adb61c2cc406a092cc
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2023-11-07 14:07:23 +0900

    test: 수익률 테스트 진행

---
+++ src/test/java/lotto/domain/ReturnsTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReturnsTest {
+
+    @Test
+    @DisplayName("수익률이 정상 출력됨을 확인한다.")
+    void print(){
+        // given
+        Wallet wallet = new Wallet(14000);
+        Tickets tickets = new Tickets(wallet);
+        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Bonus bonus = new Bonus(7);
+
+        // when
+        WinRecord winRecord = new WinRecord(lotto, bonus);
+        winRecord.inputWinRecord(tickets);
+
+        // then
+        Returns returns = new Returns(wallet, winRecord);
+        returns.calculate();
+        returns.print();
+    }
+
+}

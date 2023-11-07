e7467383a1d625f6a502ac8aa3537dbef4c7d04b
choiejae <ejae8319@gmail.com>
2023-11-07 10:51:43 +0900

    test(Lotto): 로또 클래스 테스트
    - 로또가 생성되는지 확인한다
    - 특정 숫자가 로또 내에 있는지 확인이 가능해야한다
    - 또 다른 로또와 비교하여 일치 개수 확인이 가능해야한다

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +27,48 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("중복되지 않는 6개의 숫자를 통해 로또를 만들 수 있다.")
+    @Test
+    void generateLotto() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = Lotto.of(numbers);
+
+        // then
+        assertThat(Optional.of(lotto)).isNotNull();
+    }
+
+    @DisplayName("로또 내에서 특정 숫자의 존재 유무를 확인할 수 있다.")
+    @Test
+    void checkNumberIsIncluded() {
+        // given
+        Lotto lotto = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        int containedNumber = 5;
+        int notContainedNumber = 7;
+
+        // then
+        assertTrue(lotto.contains(containedNumber));
+        assertFalse(lotto.contains(notContainedNumber));
+    }
+
+    @DisplayName("두 개의 로또를 비교하여 일치하는 개수를 확인할 수 있다.")
+    @Test
+    void matchTwoLotto() {
+        // given
+        Lotto lotto = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        Lotto otherLotto = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        Lotto anotherLotto = Lotto.of(List.of(1, 2, 3, 4, 5, 7));
+
+        // when
+        int otherLottoMatchCount = lotto.match(otherLotto);
+        int anotherLottoMatchCount = lotto.match(anotherLotto);
+
+        // then
+        assertEquals(6, otherLottoMatchCount);
+        assertEquals(5, anotherLottoMatchCount);
+    }
 }

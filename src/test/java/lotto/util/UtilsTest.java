c8ab3cd2db3ced8a69e7548f4a45faa52d222ec8
Haeun Jeong <gkdms4248@gmail.com>
2023-11-07 23:03:48 +0900

    test: 반올림 테스트 추가

--- src/test/java/lotto/util/UtilsTest.java
+++ src/test/java/lotto/util/UtilsTest.java
@@ -1,5 +1,6 @@
 package lotto.util;
 
+import static lotto.util.Utils.getRoundUpTwoDecimalPlace;
 import static lotto.util.Utils.stringToIntegerSortedList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,14 +9,13 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ListUtilsTest {
+public class UtilsTest {
 
     @DisplayName("string 을 List<Integer> 로 변환하는 기능")
     @Test
     public void stringToIntegerList() {
         // given
         String inputLotto = "1,2,3,4,5,6";
-        Utils listUtils = new Utils();
 
         // when
         List<Integer> lotto = stringToIntegerSortedList(inputLotto);
@@ -23,5 +23,19 @@ public class ListUtilsTest {
         // then
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertEquals(expected, lotto);
+    }
+
+    @DisplayName("소수점 둘째 자리에서 반올림 하는 기능")
+    @Test
+    public void roundUpTwoDecimalPlace() {
+        // given
+        float number = 62.52123f;
+
+        // when
+        float roundUpNumber = getRoundUpTwoDecimalPlace(number);
+
+        // then
+        float expected = 62.5f;
+        assertEquals(expected, roundUpNumber);
     }
 }

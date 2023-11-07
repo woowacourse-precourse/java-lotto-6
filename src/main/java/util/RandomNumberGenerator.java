abbf7709bd76f4303c1d549a5b92175c1829f3ce
zjacom <eephus625@icloud.com>
2023-11-07 22:53:09 +0900

    fix(lotto): 오름차순 버그 수정
    테스트를 통해 알게된 오름차순이 적용되지 않는 버그를 수정
    6자리 숫자를 정렬하는 것이므로 단순한 버블 정렬로 변경

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -1,56 +1,29 @@
 package util;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumberGenerator {
     public List<Integer> createNewRandomNumber() {
         List<Integer> randomNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        mergeSort(randomNumber);
+        bubbleSort(randomNumber);
         return randomNumber;
     }
 
-    private List<Integer> mergeSort(List<Integer> list) {
-        if (list.size() <= 1) {
-            return list;
-        }
-        Divide dividedIntoLeftAndRight = getLeftAndRight(list);
-        return merge(mergeSort(dividedIntoLeftAndRight.left()), mergeSort(dividedIntoLeftAndRight.right()));
-    }
-
-    private Divide getLeftAndRight(List<Integer> list) {
-        int mid = list.size() / 2;
-        List<Integer> left = new ArrayList<>(list.subList(0, mid));
-        List<Integer> right = new ArrayList<>(list.subList(mid, list.size()));
-        Divide dividedIntoLeftAndRight = new Divide(left, right);
-        return dividedIntoLeftAndRight;
-    }
-
-    private record Divide(List<Integer> left, List<Integer> right) {
-    }
-
-    private List<Integer> merge(List<Integer> left, List<Integer> right) {
-        List<Integer> merged = new ArrayList<>();
-        int leftIndex = 0, rightIndex = 0;
-
-        while (leftIndex < left.size() && rightIndex < right.size()) {
-            if (left.get(leftIndex) < right.get(rightIndex)) {
-                merged.add(left.get(leftIndex++));
-                continue;
+    private void bubbleSort(List<Integer> list) {
+        int randomNumberSize = list.size();
+        for (int i = 0; i < randomNumberSize - 1; i++) {
+            for (int j = 0; j < randomNumberSize - i - 1; j++) {
+                swapTempAndList(list, j);
             }
-            merged.add(right.get(rightIndex++));
         }
-
-        addRemainingValues(left, merged, leftIndex);
-        addRemainingValues(right, merged, rightIndex);
-
-        return merged;
     }
 
-    private void addRemainingValues(List<Integer> leftOrRight, List<Integer> merged, int index) {
-        while (index < leftOrRight.size()) {
-            merged.add(leftOrRight.get(index++));
+    private void swapTempAndList(List<Integer> list, int j) {
+        if (list.get(j) > list.get(j + 1)) {
+            int temp = list.get(j);
+            list.set(j, list.get(j + 1));
+            list.set(j + 1, temp);
         }
     }
 }

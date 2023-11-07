80388aadeaf32cbf4847acfa036f6c6b72116455
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 03:29:37 +0900

    feat(requestBonusNumber): 보너스 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String ERROR_PROMPT = "[ERROR] ";
+
     public int requestLottoPurchase() {
         int input;
         while (true) {
@@ -17,9 +18,30 @@ public class InputView {
         }
         return input;
     }
+
     private void validateLottoPurchase(int input) {
         if (input % 1000 != 0) {
             throw new IllegalArgumentException(ERROR_PROMPT + "1000원 단위로 입력해주세요.");
+        }
+    }
+
+    public int requestBonusNumber() {
+        int input;
+        while (true) {
+            try {
+                input = Integer.parseInt(Console.readLine());
+                validateBonusNumber(input);
+                break;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ERROR_PROMPT + "금액으로 숫자를 입력해주세요.");
+            }
+        }
+        return input;
+    }
+
+    private void validateBonusNumber(int input) {
+        if (input < 1 || 45 < input) {
+            throw new IllegalArgumentException(ERROR_PROMPT + "로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }

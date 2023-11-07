d5bb7ed90a65c51efe2f72afedb4677472395ce0
zangsu <zangsu_@naver.com>
2023-11-07 15:39:31 +0900

    refactor : remove AppicationConfig

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,21 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.config.ApplicationConfig;
 import lotto.controller.LottoController;
+import lotto.exception.exceptionhandler.ExceptionHandler;
+import lotto.exception.exceptionhandler.RetryExceptionHandler;
+import lotto.model.LottoStore;
+import lotto.model.domain.lotto.lottogenerator.LottoGenerator;
+import lotto.model.domain.lotto.lottogenerator.RandomLottoGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = ApplicationConfig.controller;
-        lottoController.run();
+        ExceptionHandler handler = new RetryExceptionHandler();
+        LottoGenerator generator = new RandomLottoGenerator();
+        LottoStore lottoStore = new LottoStore(generator);
+        LottoController controller = new LottoController(handler, lottoStore);
+
+        controller.run();
         Console.close();
     }
 }

8210a57e43b4ba627be362cc4985fed8af8ea983
jiwoo <zbqmgldjfh@gmail.com>
2023-11-07 17:20:02 +0900

    feat(WinningResult): 복권 당첨 결과를 담은 Result 클래스 구현

---
+++ src/main/java/lotto/dto/WinningResult.java
@@ -0,0 +1,28 @@
+package lotto.dto;
+
+import lotto.domain.Rank;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class WinningResult {
+
+    private final Map<Rank, Integer> ranks;
+
+    public WinningResult(Map<Rank, Integer> ranks) {
+        this.ranks = ranks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningResult winningResult = (WinningResult) o;
+        return Objects.equals(ranks, winningResult.ranks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ranks);
+    }
+}

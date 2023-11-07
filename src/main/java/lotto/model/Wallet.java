36de9972ef860c7989339f462d8b13bafd60117e
minkyole <7719s@nave.com>
2023-11-07 15:10:47 +0900

    feat(Wallet.java) : 구입금액에 null이 들어온 경우 예외처리 구현
    - 구입금액에 null이 들어온 경우 예외처리 구현 완료

--- src/main/java/lotto/model/Wallet.java
+++ src/main/java/lotto/model/Wallet.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
 public class Wallet {
+    private static final int MAXIMUM_PURCHASE_AMOUNT =  1000000000;
+
     private int money;
 
     public Wallet(String money) {
@@ -9,12 +11,20 @@ public class Wallet {
     }
 
     private void validateMoney(String money) {
+        validateNotNullMoney(money);
         validateNonNumericMoney(money);
     }
 
     private void validateNonNumericMoney(String money) {
         if (!money.matches("^[0-9]*")) {
             throw new IllegalArgumentException("[Error] 구입 금액에 숫자가 아닌 값이 들어왔습니다.");
+        }
+    }
+
+    private void validateNotNullMoney(String money) {
+        if (money == null) {
+            throw new IllegalArgumentException("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
+                    + MAXIMUM_PURCHASE_AMOUNT + "원.");
         }
     }
 }

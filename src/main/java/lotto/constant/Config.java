c1d05c65fa06697fd0e3b5323e0ed37a81eb6b77
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 20:38:43 +0900

    feat(const): 당첨 번호 입력 값 분리를 위한 분리 기호 상수 추가

--- src/main/java/lotto/constant/Config.java
+++ src/main/java/lotto/constant/Config.java
@@ -5,4 +5,6 @@ public class Config {
     public static final int LOTTO_COUNT = 6;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
+
+    public static final String LOTTO_DELIMITER = ",";
 }

f4b9958eee484538c007524a3c21d84eac3fa17c
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-07 23:32:41 +0900

    feat : 통합 테스트 위한 임시 로직 작성
    Application 에서 의도한 대로 , 동작 하는지 확인
    두가지 Application Test 모두 통과

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,26 @@
 package lotto;
 
-import lotto.controller.PaymentController;
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.*;
+import lotto.domain.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        PaymentController temp = new PaymentController();
-        temp.createPayment();
+        PaymentController paymentController = new PaymentController();
+        LottoController lottoController = new LottoController();
+        LottoPaperController lottoPaperController = new LottoPaperController();
+        LottoResultController lottoResultController = new LottoResultController();
+        PrizeController prizeController = new PrizeController();
+        StatisticController statisticController = new StatisticController();
+
+        Payment payment = paymentController.createPayment();
+        LottoPaper lottoPaper = lottoPaperController.createLottoPaperWithPayment(payment);
+        LottoDraw lottoDraw = lottoController.createLottoDraw();
+        LottoResult lottoResult = lottoResultController.createLottoResult(lottoPaper,lottoDraw);
+        Prize prize = prizeController.createPrizeWithLottoResult(lottoResult);
+        Statistic statistic = statisticController.createStatistic(payment,prize);
+
+        Console.close();
     }
 }
+

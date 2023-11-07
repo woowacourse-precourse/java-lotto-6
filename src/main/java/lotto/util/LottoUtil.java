41d1064b5ad7b24acd2aac8cfde63c90a1fca97a
PeterChoi <94peterchoi@gmail.com>
2023-11-08 00:07:44 +0900

    feat: 로또 번호 유효성 검사 로직 추가
    - 로또번호가 숫자값이 맞는지 확인 후 추가적으로 숫자의 범위까지 확인

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import static lotto.constant.LottoConsts.MAX_NUMBER;
+import static lotto.constant.LottoConsts.MIN_NUMBER;
+
 public class LottoUtil {
 
     public static int parseInteger(String input) {
@@ -8,6 +11,16 @@ public class LottoUtil {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자로 변환하는 도중 오류가 발생하였습니다.");
         }
+    }
+
+    public static int parseLottoNumber(String input) {
+        int lottoNumber = parseInteger(input);
+
+        if (lottoNumber < MIN_NUMBER || lottoNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d부터 %d사이의 값이어야 합니다.", MIN_NUMBER, MAX_NUMBER));
+        }
+
+        return lottoNumber;
     }
 
 }

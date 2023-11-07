7dfc9ce98965f2175ba7234b67f3e6b0d14f6b07
HyunmoBae <tsi0520@naver.com>
2023-11-08 03:05:36 +0900

    test(lotto) : add constructor, error test

---
+++ src/test/java/lotto/LottoTicketGeneratorTest.java
@@ -0,0 +1,46 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTicketGeneratorTest {
+    @DisplayName("로또 용지가 금액에 맞춰 정확히 나오는지 테스트한다.")
+    @Test
+    void NumberOfTicketTest1() {
+        LottoTicketGenerator lottoTicketGenerator = new LottoTicketGenerator(10000);
+        int result = lottoTicketGenerator.numberOfTicket;
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void NumberOfTicketTest2() {
+        LottoTicketGenerator lottoTicketGenerator = new LottoTicketGenerator(15000);
+        int result = lottoTicketGenerator.numberOfTicket;
+        assertThat(result).isEqualTo(15);
+    }
+
+    @DisplayName("금액이 1000으로 나누어 떨어지지않으면 예외가 발생한다.")
+    @Test
+    void invalidAmountErrorTest() {
+        assertThatThrownBy(() -> new LottoTicketGenerator(10050))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 음수일 경우 예외가 발생한다.")
+    @Test
+    void invalidAmountErrorTest2() {
+        assertThatThrownBy(() -> new LottoTicketGenerator(-1000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("금액이 0일 경우 예외가 발생한다.")
+    @Test
+    void invalidAmountErrorTest3() {
+        assertThatThrownBy(() -> new LottoTicketGenerator(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

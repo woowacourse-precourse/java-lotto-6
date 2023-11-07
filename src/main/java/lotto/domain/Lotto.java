61ba0efc1220764030daa1980c5a9cfdf5dd009b
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-07 11:20:04 +0900

    refactor: 불필요한 import 구문 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,7 +6,6 @@ import static lotto.utils.LottoNumberUtil.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import lotto.enums.ExceptionMessages;
 

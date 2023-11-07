e52f58114ffbd121a0f18eb8ff7b53f984b5adae
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-07 18:28:12 +0900

    fix: % -> / 로 수정

--- src/main/java/lotto/service/UserBillToAmount.java
+++ src/main/java/lotto/service/UserBillToAmount.java
@@ -4,7 +4,7 @@ import lotto.utils.BillException;
 public class UserBillToAmount {
     public static int billToAmount(int Bill) {
         BillException.isBillValid(Bill);
-        int amount = Bill % 1000;
+        int amount = Bill / 1000;
         return amount;
     }
 }

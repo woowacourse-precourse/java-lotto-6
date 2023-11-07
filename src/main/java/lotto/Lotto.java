17e11ccf0c97a9603ac9dc822c1d2e87f2f40a85
의찬 <srja432130@gmail.com>
2023-11-07 18:56:10 +0900

    refactor: 숫자를 상수로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,22 +8,27 @@ import lotto.exception.DuplicateLottoNumberException;
 
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+
     private List<Integer> numbers;
 
+    public Lotto(List<Integer> numbers) {
+
+    }
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new InvalidSizeLottoNumberException();
         }
     }
 
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
-        if (nonDuplicateNumbers.size() != 6) {
+        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
             throw new DuplicateLottoNumberException();
         }
     }

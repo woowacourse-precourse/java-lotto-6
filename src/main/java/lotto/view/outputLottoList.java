5c51e9a6b84dc5c108eb088fa476b24a63076111
의찬 <srja432130@gmail.com>
2023-11-07 19:38:55 +0900

    feat: 발행한 로또 번호를 콘솔에 출력하는 기능을 추가

---
+++ src/main/java/lotto/view/outputLottoList.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class outputLottoList {
+    public static void printLottoList(List<Lotto> lottos) {
+        lottos.forEach(System.out::println);
+    }
+}

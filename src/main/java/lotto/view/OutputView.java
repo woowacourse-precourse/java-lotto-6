315fe0d4400770d6eb86f9edb16cfca42747ec13
gmu1026 <curiouscomponent@gmail.com>
2023-11-07 17:26:03 +0900

    feat: 당첨 로또 생성 메시지 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
 public class OutputView {
-    private static final String LOTTO_PURCHASE_COMMENT = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public void printLottoPurchaseComment() {
-        System.out.println(LOTTO_PURCHASE_COMMENT);
+    public void printLottoPurchaseMessage() {
+        System.out.println(LOTTO_PURCHASE_MESSAGE);
     }
 
     public void printLineSeparator() {
@@ -13,5 +15,13 @@ public class OutputView {
 
     public void printLottoCountMessage(int lottoCount) {
         System.out.printf("%d개를 구매했습니다.%n", lottoCount);
+    }
+
+    public void printWinningNumberMessage() {
+        System.out.println(WINNING_LOTTO_MESSAGE);
+    }
+
+    public void printBonusNumberMessage() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
     }
 }

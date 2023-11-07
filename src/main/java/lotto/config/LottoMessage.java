e8b6a6836cd9d4431ef4345312d7af10803de9b3
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-07 20:24:37 +0900

    style(LottoMessage): 출력 스타일 변경

--- src/main/java/lotto/config/LottoMessage.java
+++ src/main/java/lotto/config/LottoMessage.java
@@ -5,11 +5,11 @@ public enum LottoMessage {
     TICKET_PURCHASED_COUNT("\n%d개를 구매했습니다."),
     ENTER_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
     ENTER_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS_HEADER("당첨 통계\n---\n"),
+    WINNING_STATISTICS_HEADER("\n당첨 통계\n---"),
     MATCH_COUNT("%d개 일치"),
     MATCH_COUNT_WITH_BONUS("%d개 일치, 보너스 볼 일치"),
     MATCH_RESULT(" (%s원) - %d개\n"),
-    TOTAL_PROFIT_RATE("총 수익률은 %.2f%%입니다.");
+    TOTAL_PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 

f54dc3b0bb455df745d8973cd77b304f34b23f40
k9want <zndwlrnjr26@daum.net>
2023-11-07 19:29:05 +0900

    test: WinningLotto 생성 성공 및 당첨번호와 보너스번호 확인

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -14,5 +15,22 @@ public class WinningLottoTest {
 
         assertThatThrownBy(() -> new WinningLotto(lotto, 6))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("WinningLotto 생성 성공 및 당첨번호와 보너스번호 확인")
+    @Test
+    void createWinningLotto() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int number = 7;
+
+        //when
+        WinningLotto winningLotto = new WinningLotto(lotto, number);
+        Lotto winningNumbers = winningLotto.getWinningNumbers();
+        Integer bonusNumber = winningLotto.getBonusNumber();
+
+        //then
+        assertThat(winningNumbers.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+        assertThat(bonusNumber).isEqualTo(number);
     }
 }

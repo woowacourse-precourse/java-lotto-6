3fce48b9ffa32af0268684d8337619070436c2f5
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 03:45:51 +0900

    feat(BonusNumber) : 보너스 숫자 받아오는 기능 추가 - getBonusNumber()

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,19 +1,25 @@
 package lotto.domain;
 
 import lotto.exception.BonusNumberException;
+import lotto.util.StringToIntegerConverter;
 
 import java.util.List;
 
 public class BonusNumber {
-    private final String bonusNumber;
+    private int bonusNum;
+    private StringToIntegerConverter stringToIntegerConverter = new StringToIntegerConverter();
 
     public BonusNumber(String bonusNumber, List<Integer> winnerNum) {
         validate(bonusNumber, winnerNum);
-        this.bonusNumber = bonusNumber;
+        this.bonusNum = stringToIntegerConverter.stringToInteger(bonusNumber);
     }
 
     private void validate(String bonusNumber, List<Integer> winnerNum) {
         BonusNumberException bonusNumberException = new BonusNumberException(bonusNumber, winnerNum);
+    }
+
+    public int getBonusNumber(){
+        return bonusNum;
     }
 
 }

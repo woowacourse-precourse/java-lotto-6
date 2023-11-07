c4ccc980627d872baee663b3205b10bfc59651da
Sur0n <skyzephyr333@gmail.com>
2023-11-08 00:56:52 +0900

    feat : GameOutput method

--- src/main/java/lotto/GameOutput.java
+++ src/main/java/lotto/GameOutput.java
@@ -1,4 +1,20 @@
 package lotto;
+import lotto.Messages;
 
 public class GameOutput {
+    private void printMessages(String messages){
+        System.out.println(messages);
+    }
+
+    public void printUserCashInterface() {
+        printMessages(Messages.ASK_LOTTO_MONNEY.getMessages());
+    }
+
+    public void printUserHopeLottoNumbersInterface() {
+        printMessages(Messages.ASK_WINNING_PRIZE_NUMBERS.getMessages());
+    }
+
+    public void getUserHopeBonusNumbersInterface() {
+        printMessages(Messages.ASK_BONUS_NUMBER.getMessages());
+    }
 }

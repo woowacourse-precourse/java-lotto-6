8badf4389074969c14bef131f55f21409ecead18
geunsu <geunsu23@naver.com>
2023-11-07 09:22:07 +0900

    add Constants for setting constants

---
+++ src/main/java/lotto/model/Constants.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_LENGTH = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final List<Integer> LOTTO_CASH= Arrays.asList(2000000000,30000000,1500000,50000,5000);
+}

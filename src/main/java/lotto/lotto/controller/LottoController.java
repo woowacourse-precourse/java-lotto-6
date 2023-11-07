9c158be71a4ea70a1bcfe4aac88f7e65cee70f7a
DDing77 <devseo2021@gmail.com>
2023-11-08 03:23:08 +0900

    feat(lotto): LottoController 클래스 구현

---
+++ src/main/java/lotto/lotto/controller/LottoController.java
@@ -0,0 +1,15 @@
+package lotto.lotto.controller;
+
+import lotto.lotto.dto.LottoDto;
+import lotto.lotto.service.LottoService;
+
+import java.util.List;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+}

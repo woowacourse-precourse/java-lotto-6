671136fe563701166772087bf584cc6a3b24a77b
haden <80521474+thdefn@users.noreply.github.com>
2023-11-07 10:27:51 +0900

    fix : List 타입 정렬에 대한 에러 해결
    순수한 List 타입은 unmodified 라서 sort 시 에러가 생기는 문제가 발생해 수정했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringJoiner;
 
@@ -22,19 +23,15 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
     public void printNumbers() {
-        sort();
         StringJoiner stringJoiner = new StringJoiner(", ");
-        numbers.forEach(number -> stringJoiner.add(number + ""));
+        getSortedNumber().forEach(number -> stringJoiner.add(number + ""));
         System.out.println("[" + stringJoiner + "]");
     }
 
-    private void sort() {
-        numbers.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        });
+    private List<Integer> getSortedNumber() {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 
     public LottoRank getRank(Lotto winningLotto, int bonusNumber) {

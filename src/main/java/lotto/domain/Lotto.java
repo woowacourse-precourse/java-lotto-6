73a0d7d83430c897966eb0e71eaf81fe91ec0861
sungjinlee <qdlsungls@gmail.com>
2023-11-07 21:06:44 +0900

    로또 당첨 확인 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import static lotto.domain.LottoPrize.*;
+
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,6 +11,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        System.out.println(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -17,4 +21,30 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    LottoPrize confirmWinning(Set<Integer> winningNumbers, int bonusNumber) {
+        int hits = 0;
+        boolean bonusHit = false;
+
+        for (int number : numbers) {
+            if (winningNumbers.contains(number)) {
+                hits += 1;
+            } else if (number == bonusNumber) {
+                bonusHit = true;
+            }
+        }
+
+        if (hits == THREE_MATCH.getMatches()) {
+            return THREE_MATCH;
+        } else if (hits == FOUR_MATCH.getMatches()) {
+            return FOUR_MATCH;
+        } else if (hits == FIVE_MATCH.getMatches()) {
+            if (bonusHit) {
+                return FIVE_MATCH_WITH_BONUS;
+            }
+            return FIVE_MATCH;
+        } else if (hits == SIX_MATCH.getMatches()) {
+            return SIX_MATCH;
+        }
+        return null;
+    }
 }

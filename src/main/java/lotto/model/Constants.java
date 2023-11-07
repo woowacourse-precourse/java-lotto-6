9d1a8f0738402f2aaf37850aa161b8c4dd60e65b
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 22:41:41 +0900

    feat: 에러 관련 상수 추가

--- src/main/java/lotto/model/Constants.java
+++ src/main/java/lotto/model/Constants.java
@@ -1,4 +1,6 @@
 package lotto.model;
 
 public class Constants {
+    public static final String INVALID_MONEY_FORMAT = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INVALID_NUMBER_SIZE = "[ERROR] 당첨 번호는 6개 입력 가능합니다.";
 }

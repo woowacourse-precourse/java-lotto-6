af963a3f1b3f9c4212f04b9fe87d14055ad59e90
sehee <tpgml701@naver.com>
2023-11-07 13:37:56 +0900

    feat(PrizeTable): 문제에서 요구하는 출력 순으로 상수 정렬

--- src/main/java/lotto/domain/PrizeTable.java
+++ src/main/java/lotto/domain/PrizeTable.java
@@ -1,14 +1,14 @@
 package lotto.domain;
 
 public enum PrizeTable {
-    FIRST_PRIZE(6, 2_000_000_000),
-    SECOND_PRIZE(5, 30_000_000),
-    THIRD_PRIZE(5, 1_500_000),
-    FOURTH_PRIZE(4, 50_000),
-    FIFTH_PRIZE(3, 5_000),
-    NO_RANK_ZERO(0, 0),
+    NO_RANK_TWO(0, 0),
     NO_RANK_ONE(0, 0),
-    NO_RANK_TWO(0, 0);
+    NO_RANK_ZERO(0, 0),
+    FIFTH_PRIZE(3, 5_000),
+    FOURTH_PRIZE(4, 50_000),
+    THIRD_PRIZE(5, 1_500_000),
+    SECOND_PRIZE(5, 30_000_000),
+    FIRST_PRIZE(6, 2_000_000_000);
 
 
     private final int matchingNumbers;

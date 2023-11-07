9c377b083cc981fafe16af0a104c3550b1842b9f
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 04:13:11 +0900

    feat(OutputView) : 출력기능 추가

---
+++ src/main/java/lotto/domain/view/OutputView.java
@@ -0,0 +1,33 @@
+package lotto.domain.view;
+
+import static lotto.ViewMessage.BONUS_PROMPT_MESSAGE;
+import static lotto.ViewMessage.PAYMENT_PROMPT_MESSAGE;
+import static lotto.ViewMessage.WINNING_NUMBERS_PROMPT_MESSAGE;
+import static lotto.ViewMessage.WINNING_RESULT_MESSAGE;
+
+public class OutputView {
+    public void paymentPrompt() {
+        System.out.println(PAYMENT_PROMPT_MESSAGE);
+    }
+
+    public void bonusPrompt() {
+        System.out.println(BONUS_PROMPT_MESSAGE);
+    }
+
+    public void winningNumberPrompt() {
+        System.out.println(WINNING_NUMBERS_PROMPT_MESSAGE);
+    }
+
+    public void printLottoTickets(String lottoNumbers) {
+        System.out.println(lottoNumbers);
+    }
+
+    public void printWinningResult(String winningResult) {
+        System.out.println(WINNING_RESULT_MESSAGE);
+        System.out.println(winningResult);
+    }
+
+    public void printRateOfRevenue(String rateOfRevenue) {
+        System.out.printf(rateOfRevenue);
+    }
+}

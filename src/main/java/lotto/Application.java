0bcd321b807a4efa59a8023c9acb5e497576f614
Dobby_Kim <kimdobby@g.skku.edu>
2023-11-07 23:18:13 +0900

    feat - Application : Application main 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,37 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UserInputUtil userInputUtil = new UserInputUtil();
+        LottoService lottoService = new LottoService();
+
+        // 구입 금액 요청
+        DisplayUtil.displayRequestPurchaseAmount();
+        int purchaseAmount = userInputUtil.readPurchaseAmount();
+
+        // 로또 발행 및 표시
+        lottoService.generateLottoTickets(purchaseAmount);
+        DisplayUtil.displayPurchasedLottoCount(purchaseAmount / UserInputUtil.PURCHASE_AMOUNT_UNIT);
+        DisplayUtil.displayTickets(lottoService.getLottoTickets());
+
+        // 당첨 번호 및 보너스 번호 입력 요청
+        DisplayUtil.displayRequestWinningNumbers();
+        List<Integer> winningNumbers = userInputUtil.readWinningNumbers();
+        DisplayUtil.displayRequestBonusNumber();
+        int bonusNumber = userInputUtil.readBonusNumber(winningNumbers);
+
+        // 결과 계산 및 표시
+        Map<Prize, Integer> prizeResults = lottoService.calculateResults(winningNumbers, bonusNumber);
+        DisplayUtil.displayResults(prizeResults);
+
+        // 수익률 계산 및 표시
+        int totalEarnings = lottoService.calculateTotalEarnings(prizeResults);
+        double yield = lottoService.calculateYield(purchaseAmount, totalEarnings);
+        DisplayUtil.displayYield(yield);
     }
 }
+

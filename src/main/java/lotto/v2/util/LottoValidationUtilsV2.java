a6a00ab494aaeb79b64ae3b87558f4f4a468161c
권장순 <jsoonworld@gmail.com>
2023-11-07 13:11:59 +0900

    feat( LottoValidationUtils#checkValidLottoNumbers() ) : 로또 번호 유효성 검사 구현.

--- src/main/java/lotto/v2/util/LottoValidationUtilsV2.java
+++ src/main/java/lotto/v2/util/LottoValidationUtilsV2.java
@@ -8,8 +8,13 @@ public class LottoValidationUtilsV2 {
     public static final int LOTTO_NUMBER_MAX = 45;
     public static final int LOTTO_NUMBERS_SIZE = 6;
 
-    public static void checkValidLottoNumbers(List<Integer> lottoNumbers) {
-
+    public static void checkValidLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + LOTTO_NUMBERS_SIZE + "개여야 합니다.");
+        }
+        if (numbers.stream().anyMatch(num -> num < LOTTO_NUMBER_MIN || num > LOTTO_NUMBER_MAX)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + LOTTO_NUMBER_MIN + "에서 " + LOTTO_NUMBER_MAX + " 사이의 숫자여야 합니다.");
+        }
     }
 
     public static void checkValidBonusNumber(int bonusNumber, List<Integer> winningNumbers) {

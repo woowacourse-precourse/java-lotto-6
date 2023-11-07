eb033c8723d97f60f4e8e518e572e1d1e13bebaf
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-11-08 05:01:45 +0900

    feat: 이전 문자열 개행 추가, 로또 당첨 내역, 수익률 출력 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -8,10 +8,11 @@ public class LottoView {
 
 
     public void inputMoneyView() {
-        System.out.println("구입 금액을 입력해 주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
     public void outputBuyLottoTicketsView(int ticketCount) {
+        System.out.println();
         System.out.println(ticketCount + "개를 구매했습니다.");
     }
 
@@ -21,11 +22,24 @@ public class LottoView {
     }
 
     public void inputWinnerNumbersView() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
     public void inputBonusNumberView() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+
+    public void outputWinningStatisticsView() {
+        System.out.println("\n당첨 통계\n---");
+    }
+
+    public void outputWinningDescription(String t) {
+        System.out.println(t);
+    }
+
+
+    public void outputRevenueView(double revenue) {
+        System.out.println("총 수익률은 " + String.format("%.1f", revenue) + "%입니다.");
     }
 
 }

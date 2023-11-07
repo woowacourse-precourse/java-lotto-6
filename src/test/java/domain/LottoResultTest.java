04cd053bbf6ea5b9c4cc20266e22026ad2f54fdb
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-07 22:41:54 +0900

    refactor: 필요 없는 import 삭제

--- src/test/java/domain/LottoResultTest.java
+++ src/test/java/domain/LottoResultTest.java
@@ -1,13 +1,8 @@
 package domain;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoResultTest {
 

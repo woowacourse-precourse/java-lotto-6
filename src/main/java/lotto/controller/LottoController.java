2fb83da51638b3d592e904bc82df57e112c5a5bd
gunwoong <gwj0421@gmail.com>
2023-11-07 23:04:57 +0900

    feat : 실행을 위한 컨트롤러 정의

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,16 +1,51 @@
 package lotto.controller;
 
-import lotto.domain.UserMoney;
-import lotto.view.InputView;
+import lotto.domain.LottoCompany;
+import lotto.domain.SiteUser;
+import lotto.domain.WinningStatistics;
+import lotto.util.MathUtil;
+import lotto.view.OutputView;
 
 public class LottoController {
-    private UserMoney userMoney;
+    private SiteUser siteUser;
+    private LottoCompany lottoCompany;
+    private WinningStatistics winningStatistics;
+
     public void startLotto() {
         inputMoney();
+        outputLottoCnt();
+
+        inputWinningNumberAndBonusNumber();
+        summaryStatistics();
+        outputStatistics();
     }
 
     private void inputMoney() {
-        int money = (int) InputView.INPUT_MONEY.activateView();
-        userMoney = UserMoney.createMoney(money);
+        this.siteUser = SiteUser.createSiteUser();
+        System.out.println();
     }
+
+    private void outputLottoCnt() {
+        OutputView.showLottoCnt(this.siteUser.getLottoCnt());
+        OutputView.showLotto(this.siteUser.getUserLotto().getLottoTickets());
+        System.out.println();
+    }
+
+    private void inputWinningNumberAndBonusNumber() {
+        this.lottoCompany = LottoCompany.createlottoCompany();
+        System.out.println();
+    }
+
+    private void summaryStatistics() {
+        this.winningStatistics = WinningStatistics.calculateWinningConditions(
+                this.siteUser.getUserLotto(), this.lottoCompany.getWinningNumbers(), this.lottoCompany.getBonusNumbers());
+
+    }
+
+    private void outputStatistics() {
+        OutputView.showLottoWinningDetails(this.winningStatistics.getStatistics());
+        OutputView.showRateOfReturn(MathUtil.formatPercentage(this.siteUser.getUserMoney().getMoney(),
+                MathUtil.getProfit(this.winningStatistics.getStatistics())));
+    }
+
 }

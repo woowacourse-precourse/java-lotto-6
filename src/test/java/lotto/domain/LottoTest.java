403e814dfc72fc2c076546d63f0dc36f97284136
Yeop <syt05342@naver.com>
2023-11-07 22:34:14 +0900

    test: Lotto 클래스 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,14 +1,16 @@
-package lotto;
+package lotto.domain;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    private final static String WRONG_FORMAT_MSG = "[ERROR] 1 ~ 45 사이의 숫자만 입력 가능합니다.";
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -25,4 +27,20 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 ~ 45 범위를 벗어나면 예외가 발생한다")
+    @Test
+    void createLottoOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(WRONG_FORMAT_MSG);
+    }
+
+    @DisplayName("로또가 해당 숫자를 가지고 있는지 테스트")
+    @Test
+    void hasMatchedNumber() {
+        int number = 3;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        boolean flag = lotto.hasMatchedNumber(number);
+        assertThat(flag).isEqualTo(true);
+    }
 }

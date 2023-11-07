d291782e43602a4958e14beb1333afd0018a9e2a
KDT live <dnjsxo0616@gmail.com>
2023-11-07 23:54:28 +0900

    feat(Price): 구매 금액 유효성 검사
    - 구매 금액이 0원이 아닌지 검사하는 validateZERO 메서드 추가.
    - 이를 위해 상수 ZERO(0)를 사용하여 검사.

--- src/main/java/lotto/domain/Price.java
+++ src/main/java/lotto/domain/Price.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 public class Price {
     private static final int A_TICKET_PRICE = 1000;
+    private static final int ZERO = 0;
 
     private int price;
 
@@ -11,5 +12,11 @@ public class Price {
 
     private int changeToPrice(int purchaseAmount){
         return purchaseAmount / A_TICKET_PRICE;
+    }
+
+    private void validateZERO(int purchaseAmount){
+        if(purchaseAmount == ZERO){
+            throw new IllegalArgumentException("금액이 0원입니다.");
+        }
     }
 }

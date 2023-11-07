56daceefb65f545a8383c9a3d076d46ae337bc3d
JooDukHan <bfinecpa1@naver.com>
2023-11-07 16:39:03 +0900

    style: 에러 메세지 변경

--- src/main/java/lotto/Constants/ErrorConstant.java
+++ src/main/java/lotto/Constants/ErrorConstant.java
@@ -6,7 +6,7 @@ public class ErrorConstant {
     public static final String ERROR_INPUT_VALID_VAULE = "[ERROR] 유효한 값을 입력 해주세요";
     public static final String ERROR_INPUT_TWO_BILLION_LESS = "[ERROR] 20억 이하로 입력 해주세요";
     public static final String ERROR_INPUT_UNIT_OF_THOUSAND = "[ERROR] 1000원 단위로 입력 해주세요";
-    public static final String ERROR_INPUT_DIGIT = "[ERROR] 숫자를 입력 해주세요";
+    public static final String ERROR_INPUT_DIGIT = "[ERROR] 양수인 숫자을 입력 해주세요";
     public static final String ERROR_NO_INPUT_BLANK = "[ERROR] 유효한 값을 입력 해주세요";
     public static final String ERROR_LOTTO_NUMBER_BETWEEN_MIN_AND_MAX = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다";
     public static final String ERROR_NO_INPUT_DUPLICATED = "[ERROR] 중복된 숫자를 입력할 수 없습니다.";

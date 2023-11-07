9c8db5a2f6afdc251561010c4f673f034cd8bd8c
ppdoli123 <ppdoli123@naver.com>
2023-11-07 16:22:28 +0900

    feat(LottoResult): 수익률 계산하여 반환하는 메소드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,18 +1,27 @@
 package lotto.view;
 
 import lotto.Lotto;
+import lotto.domain.LottoEnum;
 import lotto.domain.LottoResult;
 
 import java.util.List;
 
     // 발행된 로또 번호 출력 로직 작성 필요
-public class OutputView {
-    public void printTickets(List<Lotto> tickets) {
+    public class OutputView {
+        public static void printTickets(List<Lotto> tickets) {
+            System.out.println(tickets.size() + "개를 구매했습니다.");
+            for (Lotto ticket : tickets) {
+                System.out.println(ticket.getNumbers());
+            }
+        }
 
+        public static void printResult(LottoResult result, int purchaseAmount) {
+            System.out.println("당첨 통계");
+            System.out.println("---------");
+            for (LottoEnum.LottoRank rank : LottoEnum.LottoRank.values()) {
+                System.out.println(rank + " - " + result.getCount(rank) + "개");
+            }
+            System.out.println("총 수익률은 " + result.calculateEarningsRate(purchaseAmount) + "입니다.");
+        }
     }
 
-    // 당첨 통계 출력 로직 작성 필요
-    public void printResult(LottoResult result) {
-
-    }
-}

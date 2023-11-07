fccdbbd15c74a239fa82af236590eda8778e4799
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 03:52:37 +0900

    feat : 로또 구입 금앱 입력의 흐름을 관리하는 LottoPurchaseMoneyScanController 생성

--- src/main/java/lotto/controller/LottoPurchaseMoneyScanController.java
+++
@@ -1,9 +0,0 @@
-package lotto.controller;
-
-import lotto.domain.LottoPurchaseMoney;
-
-public class LottoPurchaseMoneyScanController {
-
-	public void scanLottoPurchaseMoney(LottoPurchaseMoney lottoPurchaseMoney) {
-	}
-}

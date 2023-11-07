28f45ad05f141941424b578099a10f6654e2e4d0
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-07 16:54:07 +0900

    Feat: Add getUserInputLotto in InputHandler

---
+++ src/main/java/lotto/InputHandler.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    public static List<Integer> getUserInputlotto() {
+        System.out.println("당첨 번호를 입력해 주세요");
+
+        String input = Console.readLine();
+
+        String[] inputData = input.split(",");
+        List<Integer> inputList = new ArrayList<>();
+
+        for (String data : inputData) {
+            try {
+                int number = Integer.parseInt(data.trim());
+                inputList.add(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
+            }
+        }
+        return inputList;
+    }
+}

3c824f521852f3b4d4323696bf99f1c11c5eb351
HSBODo <vinsulill@gmaile.com>
2023-11-08 00:40:53 +0900

    refactor : 메세지 상수로 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,23 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String INPUT_LOTTO_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public String inputPurchaseAmount(){
-        System.out.println("구입금액을 입력해 주세요.");
-        String inputPurchaseAmount = Console.readLine();
-        return inputPurchaseAmount;
+        System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
+        return Console.readLine();
     }
 
     public String inputWinningNumber(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String inputWinningNumber = Console.readLine();
-        return inputWinningNumber;
+        System.out.println(INPUT_LOTTO_WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
     }
 
     public String inputBonusNumber(){
-        System.out.println("");
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String inputBonusNumber = Console.readLine();
-        return inputBonusNumber;
+        System.out.println();
+        System.out.println(INPUT_LOTTO_BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
     }
 
 }

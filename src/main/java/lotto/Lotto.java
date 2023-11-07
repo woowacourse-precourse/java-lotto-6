a5177bb5002806f4ee29518abc0628f1bbb1f063
ztng123 <dpwl991102@gmail.com>
2023-11-08 02:46:31 +0900

    로또 번호 생성
    예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,7 +18,18 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException();
+        }
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != 6) {
+            throw new IllegalArgumentException();
+        }
 
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현

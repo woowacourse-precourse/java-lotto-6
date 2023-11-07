6ebbf171cf487c48e958cc90a20c4a6bc5007317
dj <fjzl12@naver.com>
2023-11-07 23:46:47 +0900

    style(RandomNumberGenerator): 하드 코딩 값 상수로 수정

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomNumberGenerator {
+
     static List<Integer> getNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(Constants.MIN_NUMBER, Constants.MAX_NUMBER, Constants.LOTTO_COUNT);
     }
 }

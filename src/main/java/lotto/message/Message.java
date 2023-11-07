cd77b207fd4fb511527d11d8b0c86ddc67a21231
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-07 23:39:05 +0900

    fix: price를 amount로 변경

--- src/main/java/lotto/message/Message.java
+++ src/main/java/lotto/message/Message.java
@@ -1,7 +1,7 @@
 package lotto.message;
 
 public class Message {
-    public static final String PROMPT_ENTERING_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    public static final String PROMPT_ENTERING_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String PROMPT_ENTERING_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String PROMPT_ENTERING_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATISTICS = "당첨 통계";

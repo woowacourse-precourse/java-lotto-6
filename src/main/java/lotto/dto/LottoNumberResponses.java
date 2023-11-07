2460763c1dfc99db041d4dc721e2051050c589be
Haebin <joker7011@naver.com>
2023-11-08 04:15:34 +0900

    fix: Dto 버그 수정

--- src/main/java/lotto/dto/LottoNumberResponses.java
+++ src/main/java/lotto/dto/LottoNumberResponses.java
@@ -1,9 +1,27 @@
 package lotto.dto;
 
+import lotto.domain.lottery.Buyer;
+import lotto.domain.lottery.Lotto;
+import lotto.domain.lottery.Lottos;
+
 import java.util.List;
 
 public record LottoNumberResponses(
         int purchaseCount,
         List<LottoNumberResponse> lottoResponses
 ) {
+    public static LottoNumberResponses build(
+            final Buyer buyer,
+            final Lottos lottos
+    ) {
+        int ticketCount = buyer.getTicketCount();
+
+        List<Lotto> lottoTickets = lottos.getLottoTickets();
+        List<LottoNumberResponse> lottoResponses =
+                lottoTickets.stream()
+                        .map(LottoNumberResponse::build)
+                        .toList();
+
+        return new LottoNumberResponses(ticketCount, lottoResponses);
+    }
 }

4fc1d92b745f6b4d6d6310ccd2e170e0f6661e40
Yehyeok Bang <qkddpgur318@gmail.com>
2023-11-07 17:57:29 +0900

    refactor(model): 당첨 번호 입력 시 공백 불가 안내 추가
    1,2,3,4,5,6 형태로만 가능합니다.

--- src/main/java/lotto/model/winning/WinningNumberValidator.java
+++ src/main/java/lotto/model/winning/WinningNumberValidator.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 
 public enum WinningNumberValidator {
     EMPTY_VALIDATOR(WinningNumberValidator::isNotEmpty, "[ERROR] 번호가 입력되지 않았습니다."),
-    NUMBER_FORMAT_VALIDATOR(WinningNumberValidator::isNumberFormat, "[ERROR] 숫자 또는 쉼표(,)로만 구성되어야 합니다."),
+    NUMBER_FORMAT_VALIDATOR(WinningNumberValidator::isNumberFormat, "[ERROR] 숫자 또는 쉼표(,)로만 구성되어야 합니다. (공백 불가)"),
     SIZE_VALIDATOR(WinningNumberValidator::isValidSize, "[ERROR] 로또 당첨 번호는 6개의 숫자여야 합니다."),
     NUMBER_RANGE_VALIDATOR(WinningNumberValidator::isWithinRange, "[ERROR] 로또 당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATION_VALIDATOR(WinningNumberValidator::isDistinct, "[ERROR] 로또 당첨 번호는 중복될 수 없습니다.");

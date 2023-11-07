0018873bd789d1750ee8cd736f2bf95ecd6f39b1
깃wooqqq <sge0225@naver.com>
2023-11-08 04:55:24 +0900

    feat: 구입 금액이 1 이상의 숫자인지 확인하는 기능 추가

--- src/main/java/lotto/view/InputMoneyView.java
+++ src/main/java/lotto/view/InputMoneyView.java
@@ -5,9 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
 
 public class InputMoneyView {
-    private static final String ERROR_MESSAGE = "[ERROR] ";
+    private static final int MIN_VALUE = 1;
+    private static final String ERROR_MESSAGE = "[ERROR] 1,000원 단위의 숫자만 입력할 수 있습니다.";
     private static final String REQUEST_INPUT_MONEY = "구입금액을 입력해 주세요.";
-    private static final String ERROR_NOT_NUMBER = "숫자 형태의 값만 입력할 수 있습니다.";
     private static final Pattern PATTERN = Pattern.compile("\\d+");
 
     public String getInputMoney() {
@@ -15,16 +15,28 @@ public class InputMoneyView {
         String input = Console.readLine();
         System.out.println();
         try {
+            validate(input);
             validateNumeric(input);
         } catch (IllegalArgumentException e) {
-            System.out.println(ERROR_MESSAGE + ERROR_NOT_NUMBER);
+            System.out.println(ERROR_MESSAGE);
             input = getInputMoney();
         }
         return input;
     }
 
+    private void validate(String input) {
+        int number = Integer.parseInt(input);
+        validateRange(number);
+    }
+
     private void validateNumeric(String input) {
         if (!PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(int number) {
+        if (number < MIN_VALUE) {
             throw new IllegalArgumentException();
         }
     }

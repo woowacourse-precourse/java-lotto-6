b79bb0e2a862bcda0a97e5bef83b60bbbb4bf9ba
muyaaho <skanthsu715@gmail.com>
2023-11-07 16:11:50 +0900

    refactor(lotto): LottoTest의 에러 메시지가 ErrorMessages enum 값을 사용하도록 변경

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.ErrorMessages.VALIDATE_1_TO_45;
+import static lotto.ErrorMessages.VALIDATE_6_NUMS;
+import static lotto.ErrorMessages.VALIDATE_DUPLICATE_LOTTO;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -22,25 +25,25 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7, 8)));
-        assertEquals(exception.getMessage(), "[ERROR] 6개의 숫자가 아닙니다. 다시 입력해주세요.");
+        assertEquals(exception.getMessage(), VALIDATE_6_NUMS.getMessage());
         Exception exception2 = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5)));
-        assertEquals(exception2.getMessage(), "[ERROR] 6개의 숫자가 아닙니다. 다시 입력해주세요.");
+        assertEquals(exception2.getMessage(), VALIDATE_6_NUMS.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5, 5)));
-        assertEquals(exception.getMessage(), "[ERROR] 중복된 숫자가 있습니다. 다시 입력해주세요.");
+        assertEquals(exception.getMessage(), VALIDATE_DUPLICATE_LOTTO.getMessage());
     }
 
     @DisplayName("1~45 사이가 아니라면 예외 처리한다.")
     @Test
     void creatLottoBy1to45(){
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(1, 2, 3, 4, 5, 100)));
-        assertEquals(exception.getMessage(), "[ERROR] 로또 번호의 숫자 범위는 1~45사이 입니다. 다시 입력해주세요.");
+        assertEquals(exception.getMessage(), VALIDATE_1_TO_45.getMessage());
         Exception exception2 = assertThrows(IllegalArgumentException.class, () -> new Lotto(List.of(-1, 2, 3, 4, 5, 11)));
-        assertEquals(exception2.getMessage(), "[ERROR] 로또 번호의 숫자 범위는 1~45사이 입니다. 다시 입력해주세요.");
+        assertEquals(exception2.getMessage(), VALIDATE_1_TO_45.getMessage());
     }
 
 }

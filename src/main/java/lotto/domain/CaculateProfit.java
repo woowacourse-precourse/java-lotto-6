b57e29454ec54be4ee14c86e86231a258e20ff28
sw801733 <sw801733@naver.com>
2023-11-07 22:15:59 +0900

    feat : CaculateProfit 클래스 개수에 따른 수익 계산 구현
    - 개수에 따른 수익 계산을 위한 calculateCntProfit 메서드 추가

--- src/main/java/lotto/domain/CaculateProfit.java
+++ src/main/java/lotto/domain/CaculateProfit.java
@@ -1,13 +1,49 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaculateProfit {
 
-    private final int cntProfit;
-    private final int bonusProfit;
 
-    private final int totalProfit;
+    private final int lottoPurchaseCnt;
+    private final List<Integer> cntProfit;
+    private final List<Integer> bonusProfit;
 
-    public CaculateProfit() {
-        return;
+    private final List<Integer> totalProfit;
+
+    private final List<Integer> lottoResult;
+
+    private final List<Boolean> bonusCheck;
+
+    public CaculateProfit(int lottoPurchaseCnt ,List<Integer> lottoResult, List<Boolean> bonusCheck) {
+        this.lottoPurchaseCnt = lottoPurchaseCnt;
+        this.lottoResult = lottoResult;
+        this.bonusCheck = bonusCheck;
+
+        this.cntProfit = calculateCntProfit();
+    }
+
+    private List<Integer> calculateCntProfit() {
+        List<Integer> cntProfit = new ArrayList<>();
+
+        for(int i = 0; i < lottoPurchaseCnt; i++) {
+            int cnt = lottoResult.get(i);
+            int profit = 0;
+
+            if (cnt == 3) {
+                profit = 5000;
+            } else if (cnt == 4) {
+                profit = 50000;
+            } else if (cnt == 5) {
+                profit = 15000000;
+            } else if (cnt == 6) {
+                profit = 2000000000;
+            }
+
+            cntProfit.add(profit);
+        }
+
+        return cntProfit;
     }
 }

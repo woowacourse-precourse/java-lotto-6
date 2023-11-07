2a0c93d19c8f9711cb4653efc5d4c034e1a0895b
hyuuuun <pam4522@gmail.com>
2023-11-08 02:10:45 +0900

    feat(LottoView): 출력 메소드 구현

---
+++ src/main/java/lotto/LottoView.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class LottoView {
+        public static void mentionInputPrice() {
+                System.out.println("구입금액을 입력해 주세요.");
+        }
+        public static void printLottoCount() {
+                System.out.println("개를 구매했습니다.");
+        }
+        public static void mentionInputWinningNumber() {
+                System.out.println("\n당첨 번호를 입력해 주세요.");
+        }
+        public static void mentionInputBonusNumber() {
+                System.out.println("\n보너스 번호를 입력해 주세요.");
+        }
+
+}

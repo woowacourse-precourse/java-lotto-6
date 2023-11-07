a8d12c2e7405e32bdf2719997063a961baba3b54
dbstn1325 <dbstn1325@naver.com>
2023-11-07 12:24:11 +0900

    feat(WinningBundle): 당첨 번호와 보너스 번호가 일치하면 IllegalArgumentException 예외처리 기능 구현

--- src/main/java/lotto/domain/WinningBundle.java
+++ src/main/java/lotto/domain/WinningBundle.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class WinningBundle {
     private static final String WINNING_NUMBER_MUST_TOTAL_SIX_MESSAGE = "[ERROR] 당첨 번호의 갯수는 총 6개여야 합니다.";
+    private static final String WINNING_BUNDLE_CONTAIN_BONUS_NUMBER_MESSAGE = "[ERROR] 당첨 번호와 보너스 번호는 일치할 수 없습니다.";
 
     private final List<WinningNumber> winningBundle;
 
@@ -18,7 +19,7 @@ public class WinningBundle {
     }
 
     private static void validateSize(List<WinningNumber> winningBundle) {
-        if(winningBundle.size() != 6) {
+        if (winningBundle.size() != 6) {
             throw new IllegalArgumentException(WINNING_NUMBER_MUST_TOTAL_SIX_MESSAGE);
         }
     }
@@ -28,4 +29,16 @@ public class WinningBundle {
                 .map(WinningNumber::toInt)
                 .collect(Collectors.toList());
     }
+
+    public void validateContainsBonusNumber(int bonusNumber) {
+        if (isContainsBonus(bonusNumber)) {
+            throw new IllegalArgumentException(WINNING_BUNDLE_CONTAIN_BONUS_NUMBER_MESSAGE);
+        }
+    }
+
+    private boolean isContainsBonus(int bonusNumber) {
+        return winningBundle.stream()
+                .anyMatch(winningNumber -> winningNumber.isEqualsBonusNumber(bonusNumber));
+    }
+
 }

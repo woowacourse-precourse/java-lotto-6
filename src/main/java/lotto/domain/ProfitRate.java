7d35a906855c1f7eeab5a4a3009a655a0b251dce
doha <dohaahi@naver.com>
2023-11-07 22:05:02 +0900

    feat (ProfitRate): 수익률 도메인 구현

---
+++ src/main/java/lotto/domain/ProfitRate.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class ProfitRate {
+    private final double profitRate;
+
+    public ProfitRate(double profitRate) {
+        this.profitRate = profitRate;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+}

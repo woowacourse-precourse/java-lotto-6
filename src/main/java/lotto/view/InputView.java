497a1160f277e1a6b08be9d3a68ac460a5866b45
jae woo Jung <awfjol2008@naver.com>
2023-11-08 02:14:52 +0900

    feat: 로또 구입 금액을 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static java.lang.Integer.*;
+
 public class InputView {
 
     private InputView() {
 
+    }
+
+    protected Integer readPurchaseAmount() {
+        return parseInt(Console.readLine());
     }
 }

c321a67b812ccb3d99041fd58953b09e92b0d1bf
Hanbee Lee <bee1162@naver.com>
2023-11-08 00:01:19 +0900

    feat: view 클래스 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,20 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String NUMBER_OF_LOTTOS_PURCHASED_MESSAGE = "%d개를 구매했습니다.";
+
+    public static void printNumberOfPurchasedLotto(int numberOfPurchasedLotto) {
+        System.out.printf(NUMBER_OF_LOTTOS_PURCHASED_MESSAGE,numberOfPurchasedLotto);
+        System.out.println();
+    }
+    public static void printLottosList(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+            System.out.println();
+        }
+    }
 }

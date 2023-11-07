38b79d860c93e6a9a5665e1754d4c4154c387ff4
choiejae <ejae8319@gmail.com>
2023-11-07 18:48:49 +0900

    test(BonusNumber): 보너스 숫자 테스트 작성
    - 숫자로 객체를 생성할 수 있는가
    - 로또 범위를 벗어나면 예외를 발생하는가

---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    @DisplayName("숫자 값으로 객체를 생성할 수 있다.")
+    @Test
+    void generateClass() {
+        //given
+        int input = 2;
+
+        //when
+        BonusNumber bonusNumber = BonusNumber.from(input);
+
+        //then
+        assertThat(bonusNumber).isNotNull();
+    }
+
+    @DisplayName("로또 숫자 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void throwExceptionInvalidInput() {
+        //given
+        int input = Lotto.END_INCLUSIVE + 1;
+
+        //when
+        //then
+        assertThatThrownBy(() -> BonusNumber.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Lotto.LOTTO_RANGE_EXCEPTION);
+    }
+}

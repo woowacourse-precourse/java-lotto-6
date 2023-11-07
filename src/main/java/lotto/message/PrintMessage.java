954c5064feacb6b25abcf6d1b0879a2b836ace7e
NahyeChoi <skgp0628@naver.com>
2023-11-07 23:40:27 +0900

    refactor(lotto):출력할 메세지 수정

--- src/main/java/lotto/message/PrintMessage.java
+++ src/main/java/lotto/message/PrintMessage.java
@@ -2,16 +2,16 @@ package lotto.message;
 
 public enum PrintMessage {
 	PURCHASE_LOTTO("구입금액을 입력해 주세요."),
-	INPUT_LOTTO_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-	INPUT_LOTTO_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+	INPUT_LOTTO_WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
+	INPUT_LOTTO_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
 	
 	PRINT_LOTTO("개를 구매했습니다."),
 	RESULT_LOTTO_HEADER("당첨 통계\n---"),
-	COMPARE_RESULT3("3개 일치 (5,000원) -"),
-	COMPARE_RESULT4("4개 일치 (50,000원) -"),
-	COMPARE_RESULT5("5개 일치 (1,500,000원) -"),
-	COMPARE_RESULT5_2("5개 일치, 보너스 볼 일치 (30,000,000원) -"),
-	COMPARE_RESULT6("6개 일치 (2,000,000,000원) -");
+	FIRST("6개 일치 (2,000,000,000원) -"),
+	SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) -"),
+	THIRD("5개 일치 (1,500,000원) -"),
+	FOURTH("4개 일치 (50,000원) -"),
+	FIFTH("3개 일치 (5,000원) -");
 	
 
 	private String message;

6ffa93e01439fc9a3526327a6eeebb935ba7497a
ged01 <ged01@hanyang.ac.kr>
2023-11-08 05:16:48 +0900

    사용자가 입력한 시행 횟수대로 로또 번호 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,15 @@
 package lotto;
 
 import java.lang.NumberFormatException;
+import java.util.List;
+import java.util.ArrayList;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 
 public class Application {
-    public boolean isInteger(String input){
+    public static boolean isInteger(String input){
         try{
             Integer.parseInt (input);
             return true;
@@ -15,21 +19,76 @@ public class Application {
             return false;
         }
     }
-    public void cashSanityCheck(int cashInput) {
+    public static void cashSanityCheck(int cashInput)  throws IllegalArgumentException {
         if ( (cashInput%10) != 0) {
             System.out.println("[ERROR] 구입 금액은 1,000의 배수여야 합니다.");
             throw new IllegalArgumentException();
         }
     }
-    public void numberCheck(int bonusNumber) throws IllegalArgumentException {
-        if ( !(1 <= bonusNumber && bonusNumber <= 45) ) {
+
+    public void numberCheck(int inputNumber) throws IllegalArgumentException {
+        if ( !(1 <= inputNumber && inputNumber <= 45) ) {
             System.out.println("[ERROR] 로또 번호는 1에서 45 사이의 정수여야 합니다.");
             throw new IllegalArgumentException();
         }
     }
 
+    public static void overlapCheck(List<Integer> numbers) throws IllegalArgumentException {
+        List<Integer> UserInput = new ArrayList<>();
+        for (Integer number : numbers) {
+            int digit = Integer.parseInt(String.valueOf(number));
+            if (!UserInput.contains(digit)) {
+                UserInput.add(digit);
+                continue;
+            }
+            System.out.println("[ERROR] 로또 번호는 6개의 중복되지 않는 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isRightCash(String input){
+        if (isInteger(input)) return false;
+        int cash = Integer.parseInt(input);
+        try{
+            cashSanityCheck(cash);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
+    public static boolean isRightLotto(List<Integer> numbers){
+        try{
+            overlapCheck(numbers);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String buying = "a";
+        ArrayList<Lotto> Lottos = new ArrayList<>();
+
+        while(!isRightCash(buying)){
+            System.out.println("구입금액을 입력해 주세요.");
+            buying = Console.readLine();
+        }
+
+        int PlayingTime = Integer.parseInt(buying) / 1000;
+        System.out.printf("%d개를 구매했습니다.\n", PlayingTime);
+
+        for (int i = 0; i < PlayingTime; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto eachLotto = new Lotto(numbers);
+            Lottos.add(eachLotto);
+        }
+        for (Lotto lotto : Lottos){
+            lotto.printNumbers();
+        }
+        
 
     }
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import java.util.List;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,6 +15,17 @@ public class Lotto {
             System.out.println("[ERROR] 당첨 번호는 6개여야 합니다.");
             throw new IllegalArgumentException();
         }
+    }
+
+    public void printNumbers(){
+        System.out.print("[");
+        for (int i = 0; i < this.numbers.size(); i++){
+            System.out.print(this.numbers.get(i));
+            if ( i != (this.numbers.size() - 1) ){
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]\n");
     }
 
     // TODO: 추가 기능 구현

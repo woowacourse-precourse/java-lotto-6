391235866554d0f533fcc690d717bce683bb366e
dmong <dt2980_hee@naver.com>
2023-11-08 06:01:50 +0900

    feat: 오름차순 출력을 위한 LottoNumber에 Comparable 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
     private final int number;
 
     private LottoNumber(int number) {
@@ -9,6 +10,11 @@ public class LottoNumber {
 
     public static LottoNumber create(int number) {
         return new LottoNumber(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber other) {
+        return Integer.compare(this.number, other.number);
     }
 
     public int getNumber() {

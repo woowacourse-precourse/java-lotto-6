a0ce89c62d37ae83bcdf783a6fc6a8907c7bd417
Finn <wjstodsss@naver.com>
2023-11-07 19:23:45 +0900

    refactor : 객체 생성 관련 Lotto.pickSixLottoNumber(),  Lotto.sortAscending()메서드 LottoFenerator로 이동

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -10,4 +10,17 @@ public class LottoGenerator {
     public LottoGenerator(int lottoCount) {
             Lotto ticket = new Lotto(sortAscending(pickSixLottoNumber()));
     }
+
+    private List<Integer> pickSixLottoNumber() {
+        final int MIN_LOTTO_NUMBER = 1;
+        final int MAX_LOTTO_NUMBER = 45;
+        final int LOTTO_NUMBER_COUNT = 6;
+        return pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
+    }
+
+    public List<Integer> sortAscending(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers;
+    }
+
 }

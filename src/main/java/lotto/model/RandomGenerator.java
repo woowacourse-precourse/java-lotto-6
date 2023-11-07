5730497f214b79aa4f4fb81029ef06984e4ba7f6
sung-yeop <jjabc9459@naver.com>
2023-11-07 20:00:11 +0900

    feat(RandomGenerator): 랜덤 값을 생성해주는 기능 구현
    - 랜덤 값 생성을 클래스를 통해서 진행할 수 있도록 함

--- src/main/java/lotto/model/RandomGenerator.java
+++ src/main/java/lotto/model/RandomGenerator.java
@@ -1,2 +1,17 @@
-package lotto.model;public class RandomGenerator {
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomGenerator {
+
+    public List<List<Integer>> getRandomNumbers(int count) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            result.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        }
+        return result;
+    }
 }

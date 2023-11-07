94289f089b20bc6edd0b06d54d051fe49d3bb713
Junobee25 <wnsdhqo@naver.com>
2023-11-07 23:00:38 +0900

    test(LottoServiceTest): 당첨 번호 내 로또 번호 일치 갯수 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,7 +1,12 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.util.Utils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,8 +28,29 @@ class LottoServiceTest {
 
     }
 
+    @DisplayName("당첨 로또와 내가 산 로또 번호 사이 일치하는 갯수 테스트")
     @Test
     void countMatchingWinningNumbers() {
+        //given
+        Lotto lotto1 = new Lotto(Utils.convertToLottoIntegerList("1,2,3,4,5,6"));
+        Lotto lotto2 = new Lotto(Utils.convertToLottoIntegerList("1,2,3,4,5,6"));
+        List<Integer> winLotto1 = lotto1.getLotto();
+        List<Integer> myLotto1 = lotto2.getLotto();
+        Lotto lotto3 = new Lotto(Utils.convertToLottoIntegerList("1,2,3,4,7,8"));
+        Lotto lotto4 = new Lotto(Utils.convertToLottoIntegerList("1,2,3,4,5,6"));
+        List<Integer> winLotto2 = lotto3.getLotto();
+        List<Integer> myLotto2 = lotto4.getLotto();
+        LottoService lottoService = new LottoService();
+
+        //when
+        int result1 = lottoService.countMatchingWinningNumbers(winLotto1,myLotto1);
+        int result2 = lottoService.countMatchingWinningNumbers(winLotto2,myLotto2);
+
+        //then
+        org.assertj.core.api.Assertions.assertThat(result1).isEqualTo(6);
+        org.assertj.core.api.Assertions.assertThat(result2).isEqualTo(4);
+
+
     }
 
     @Test

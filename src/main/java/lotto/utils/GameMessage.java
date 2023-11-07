0576c555ad6be21fd3c43e926ea239bd0573253a
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-07 20:19:34 +0900

    refactor: 보너스 번호 입력 메시지 수정 잘못 입력되어있어 수정하였음

--- src/main/java/lotto/utils/GameMessage.java
+++ src/main/java/lotto/utils/GameMessage.java
@@ -4,7 +4,7 @@ public enum GameMessage {
     ENTER_PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
     PURCHASED_TICKET_COUNT_MESSAGE("%d개를 구매했습니다."),
     ENTER_WINNING_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
-    ENTER_BONUS_NUMBER_MESSAGE("보너스를 입력하세요."),
+    ENTER_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS_MESSAGE("당첨 통계"),
     RANK_MATCH_5_MESSAGE("5개 일치, 보너스 볼 일치 (%,d원) - %d개%n"),
     RANK_NOT_MATCH_5_MESSAGE("%d개 일치 (%,d원) - %d개%n");

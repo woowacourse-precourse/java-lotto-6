f3446bb260318bfda3d3fd6248b1820417b5fc67
Finn <wjstodsss@naver.com>
2023-11-07 21:43:09 +0900

    feat : 입력된 문자열(당첨 번호) 저장 기능

---
+++ src/main/java/lotto/controllers/DongHangLottery.java
@@ -0,0 +1,10 @@
+package lotto.controllers;
+
+public class DongHangLottery {
+    private String inputLottoNumber;
+
+    public void initialize() {
+        inputLottoNumber = InputProcessor.readLine();
+    }
+
+}

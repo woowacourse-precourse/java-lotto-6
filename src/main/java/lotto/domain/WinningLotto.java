7274a4a634ad00a9ff1056250791b2c962950fbb
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:40:31 +0900

    feat(WinningLotto) : inputWinning 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,18 @@
 package lotto.domain;
 
+import lotto.view.InputView;
+
 public class WinningLotto {
+    private Lotto winningLotto;
+    public WinningLotto() {
+        inputWinningNumbers();
+    }
+
+
+
+    private void inputWinningNumbers() {
+        System.out.println();
+        winningLotto = new Lotto(InputView.inputLotto());
+    }
+
 }

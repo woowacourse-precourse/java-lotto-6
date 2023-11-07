d08a294878bbcb2df5615dd3773bb98ad64b1ca1
JeongMin <ajhappy12@gamil.com>
2023-11-07 22:57:26 +0900

    feat: Lotto checker class 구현

---
+++ src/main/java/lotto/LottoChecker.java
@@ -0,0 +1,53 @@
+package lotto;
+
+import config.LottoConfig;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LottoChecker {
+    private final HashSet<Integer> answerNumberSet;
+    private final int bonusNumber;
+
+    public LottoChecker(String answerNumberSetInput, String bonusNumberInput) {
+        this.answerNumberSet = parseLottoNumberSet(answerNumberSetInput);
+        this.bonusNumber = parseLottoNumber(bonusNumberInput);
+
+        if (answerNumberSet.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        }
+    }
+
+    private HashSet<Integer> parseLottoNumberSet(String lottoNumberInput) {
+        try {
+            List<Integer> lottoNumberList = Stream.of(lottoNumberInput.split(",")).map(this::parseLottoNumber).toList();
+            if (lottoNumberList.size() != LottoConfig.LOTTO_SIZE) {
+                throw new IllegalArgumentException(String.format("[ERROR] 당첨 번호는 %d개여야 합니다.", LottoConfig.LOTTO_SIZE));
+            }
+            HashSet<Integer> lottoNumberSet = new HashSet<>(lottoNumberList);
+            if (lottoNumberSet.size() != LottoConfig.LOTTO_SIZE) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
+            }
+            return lottoNumberSet;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 숫자로만 입력해야 합니다.");
+        }
+    }
+
+    private int parseLottoNumber(String numberString) {
+    	try {
+            int number = Integer.parseInt(numberString);
+            validateLottoNumber(number);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또의 번호는 숫자로만 입력해야 합니다.");
+        }
+    }
+
+    private void validateLottoNumber(int number) {
+        if (number < LottoConfig.LOTTO_MIN || number > LottoConfig.LOTTO_MAX) {
+            throw new IllegalArgumentException(String.format("[ERROR] 각각의 당첨 번호는 %d~%d 사이여야 합니다.", LottoConfig.LOTTO_MIN, LottoConfig.LOTTO_MAX));
+        }
+    }
+}

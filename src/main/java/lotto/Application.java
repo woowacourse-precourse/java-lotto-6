d7fabfd406179406e8df059035da6a4e5fdb0472
Hoony0321 <eogns0321@gmail.com>
2023-11-08 04:59:58 +0900

    feat : skeleton 코드 추가
    (cherry picked from commit 981f6939d3cc3eec1fb97a196b802670e35110c1)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.service.GameService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.run();
     }
 }
---
+++ src/main/java/lotto/controller/GameController.java
@@ -0,0 +1,25 @@
+package lotto.controller;
+
+import lotto.service.GameService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class GameController {
+    public void run() {
+        startGame();
+        playGame();
+        endGame();
+    }
+
+    private void startGame() {
+
+    }
+
+    private void playGame() {
+
+    }
+
+    private void endGame() {
+
+    }
+}

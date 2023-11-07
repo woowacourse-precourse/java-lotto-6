65e3fedf79da75cb240446e37e4735ff1e533da3
whereami2048 <whereami2048@gmail.com>
2023-11-07 13:42:20 +0900

    feat: 입력 검증 예외 발생 시 안내 멘트 상수 enum 구현

---
+++ src/main/java/lotto/exception/InputExceptionConstant.java
@@ -0,0 +1,12 @@
+package lotto.exception;
+
+public enum InputExceptionConstant {
+    OUT_OF_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45사이의 숫자이어야 합니다."),
+    NO_BLANK_IN_STRING("[ERROR] 입력 문자열에 공백이 포함되어서는 안됩니다."),
+    ONLY_NATURAL_NUMBER_IN_STRING_ARRAY("[ERROR] 당첨 숫자는 자연수로만 이루어져야 합니다.");
+
+
+    InputExceptionConstant(String errText) {
+
+    }
+}

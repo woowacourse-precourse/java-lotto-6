1c5bf606b29aec9cfe49648d0e6377ef77905944
SeokEui Hong <hongs7701@naver.com>
2023-11-07 18:49:47 +0900

    refactor: [#16] rainking의 필드 타입을 int로 변환
    profit 활용을 더 편하게 위에 _ 추가한 int 타입으로 변환

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,19 +1,18 @@
 package lotto;
 
 public enum Ranking {
-    FIRST_PRIZE(1,6,  "2,000,000,000원"),
-    SECOND_PRIZE(2, 5,  "30,000,000원"),
-    THIRD_PRIZE(3, 5,  "1,500,000원"),
-    FOURTH_PRIZE(4, 4,  "50,000원"),
-    FIFTH_PRIZE(5, 3,  "5,000원"),
-    FAILURE(0, 0, "0원");
+    FIRST_PRIZE(1,6,  2_000_000_000),
+    SECOND_PRIZE(2, 5,  30_000_000),
+    THIRD_PRIZE(3, 5,  1_500_000),
+    FOURTH_PRIZE(4, 4,  50_000),
+    FIFTH_PRIZE(5, 3,  5_000),
+    FAILURE(0, 0, 0);
 
     private final int rank;
     private final int sameNumberCount;
-    private final String profit;
+    private final int profit;
 
-
-    Ranking(int rank, int sameNumberCount, String profit){
+    Ranking(int rank, int sameNumberCount, int profit){
         this.rank = rank;
         this.sameNumberCount = sameNumberCount;
         this.profit = profit;
@@ -21,5 +20,13 @@ public enum Ranking {
 
     public int getSameNumberCount(){
         return sameNumberCount;
+    }
+
+    public int getRank(){
+        return rank;
+    }
+
+    public int getProfit(){
+        return profit;
     }
 }

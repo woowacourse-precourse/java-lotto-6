423fbfd9e9ec2797a19c5d7c5209261e386d2f2a
LeeJaeHong <0915jhlee@daum.net>
2023-11-07 12:56:15 +0900

    feat: 중복 숫자 에러 체크
    11, 11 처럼 중복으로 입력될경우 다시 입력

--- src/main/java/lotto/input/InputParam.java
+++ src/main/java/lotto/input/InputParam.java
@@ -1,12 +1,15 @@
 package lotto.input;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputParam {
     public int inputMoney(){
-        return Integer.parseInt(inputLoop());
+        return Integer.parseInt(inputMoneyLoop());
     }
-    public String inputLoop(){
+    public String inputMoneyLoop(){
         String money;
         while (true){
             money=readLine();
@@ -31,12 +34,32 @@ public class InputParam {
     }
 
     public int[] inputLottoNumbers(){
-        String[] split = readLine().split(",");
-        int[] inputNumbers=new int[split.length];
-        for (int i = 0; i < split.length; i++) {
-            inputNumbers[i] = Integer.parseInt(split[i]);
-        }
+        int[] inputNumbers = inputNumberLoop();
         return inputNumbers;
+    }
+
+    private  int[] inputNumberLoop() {
+        while (true){
+            String[] split = readLine().split(",");
+            int[] inputNumbers=new int[split.length];
+            for (int i = 0; i < split.length; i++) {
+                inputNumbers[i] = Integer.parseInt(split[i]);
+            }
+            try{
+                errorCheckInputNumber(inputNumbers);
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 중복문자가 입력되었습니다.");
+            }
+            return inputNumbers;
+        }
+    }
+
+    public void errorCheckInputNumber(int[] inputNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int inputNumber : inputNumbers) {
+            uniqueNumbers.add(inputNumber);
+        }
+        if(uniqueNumbers.size()!=6) throw new IllegalArgumentException();
     }
 
     public int inputBonusNumber(){

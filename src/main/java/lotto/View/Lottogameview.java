985eaf81bdb9dac38de267b4729a787fbfc35c81
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 03:01:31 +0900

    [ADD] 당첨 통계 출력

--- src/main/java/lotto/View/Lottogameview.java
+++ src/main/java/lotto/View/Lottogameview.java
@@ -20,4 +20,23 @@ public class Lottogameview {
     private void printLottoNumbers(List<Integer> numbers) {
         System.out.println(numbers);
     }
+
+    private String formatResultLine(LottoResult result, int[] winCounts) {
+        if (result == LottoResult.THIRD && winCounts[LottoResult.FOURTH.ordinal()] > 0) {
+            int bonusCount = winCounts[LottoResult.FOURTH.ordinal()];
+            return formatBonusResultLine(bonusCount, LottoResult.FOURTH.getPrize());
+        }
+
+        String formattedPrize = formatPrize(result.getPrize());
+
+        if (result == LottoResult.FOURTH) {
+            return result.getMatchingNumbers() + "개 일치, 보너스 볼 일치 (" + formattedPrize + "원) - " + winCounts[result.ordinal()] + "개";
+        }
+
+        return result.getMatchingNumbers() + "개 일치 (" + formattedPrize + "원) - " + winCounts[result.ordinal()] + "개";
+    }
+
+    private String formatBonusResultLine(int bonusCount, int prize) {
+        return "5개 일치, 보너스 볼 일치 (" + formatPrize(prize) + "원) - " + bonusCount + "개";
+    }
 }

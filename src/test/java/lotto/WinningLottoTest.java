33b0dead4555c12c46fbe0bb2c3a79312e50a804
gmu1026 <curiouscomponent@gmail.com>
2023-11-07 17:42:02 +0900

    test: 보너스 번호 중복 검증 테스트 추가

---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+    @DisplayName("보너스 번호가 당첨 로또와 중복되면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

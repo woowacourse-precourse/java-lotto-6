ce076080af84a4e42f6136fd86c96d5c8aa81d01
eora21 <eora21@naver.com>
2023-11-07 17:23:24 +0900

    refactor: 💡 수정 단위 금액 표현 시 Amount의 메서드를 사용하도록 변경

--- src/main/java/exception/NotCorrectUnitAmountException.java
+++ src/main/java/exception/NotCorrectUnitAmountException.java
@@ -4,8 +4,7 @@ import domain.Amount;
 import java.text.DecimalFormat;
 
 public class NotCorrectUnitAmountException extends CustomException {
-    private static final DecimalFormat decimalFormat = new DecimalFormat("###,###");
     public NotCorrectUnitAmountException(Amount unitAmount) {
-        super("구입 금액은 " + decimalFormat.format(unitAmount.getAmount()) + "원 단위로 나누어 떨어져야 합니다.");
+        super("구입 금액은 " + (unitAmount.getDecimalFormatAmount()) + "원 단위로 나누어 떨어져야 합니다.");
     }
 }

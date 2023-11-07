4bd296a5b98f8a260b4a01773f76ae57b2b23b95
ohjuyoungg <ejrm12@naver.com>
2023-11-08 00:34:49 +0900

    feat: 당첨 통계 계산하는 로또 서비스 1차 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,8 +2,8 @@ package lotto.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.utils.constants.LottoNumConstant;
-import lotto.view.OutputView;
 
 public class LottoService {
 
@@ -13,12 +13,26 @@ public class LottoService {
     }
 
     public static List<List<Integer>> generateLottoNumbers(int lottoNumber) {
-       List<List<Integer>> allLottoNumbers = new ArrayList<>();
+        List<List<Integer>> allLottoNumbers = new ArrayList<>();
         for (int index = 0; index < lottoNumber; index++) {
             List<Integer> lottoNumbers = LottoNumberGenerator.generateNum();
             allLottoNumbers.add(lottoNumbers);
         }
         return allLottoNumbers;
     }
-}
 
+    public static int countMatchingNumbers(Lotto purchasedLotto, Lotto winningNumbers) {
+        int count = 0;
+        List<Integer> purchasedNumbers = purchasedLotto.getNumbers();
+        List<Integer> winningNumbersList = winningNumbers.getNumbers();
+
+        for (Integer number : purchasedNumbers) {
+            if (winningNumbersList.contains(number)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}

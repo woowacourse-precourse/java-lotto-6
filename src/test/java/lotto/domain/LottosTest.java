c085815c8495cbce868b3840c37b11bd81a5b3b0
inpink <inpink@kakao.com>
2023-11-08 00:14:17 +0900

    test: 로또 리스트들 형식에 맞춰 한 줄에 하나씩 출력

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LottosTest {
@@ -17,6 +18,21 @@ public class LottosTest {
 
         // Then
         assertThat(result.size()).isEqualTo(8);
+    }
+
+    @Test
+    public void 로또리스트_형식에맞춰_출력() {
+        // Given
+        List<Lotto> lottoList = List.of(
+                Lotto.create("1,2,3,4,5,6"),
+                Lotto.create("11,22,33,34,35,45"));
+        Lottos lottos = Lottos.create(lottoList);
+
+        // When
+        String result = lottos.toString();
+
+        // Then
+        assertThat(result).isEqualTo("[1, 2, 3, 4, 5, 6]\n[11, 22, 33, 34, 35, 45]");
     }
 
 }

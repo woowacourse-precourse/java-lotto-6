59b82bb4f2c3a92da51b61b90267d5ede02877b2
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-07 20:43:56 +0900

    style: 오타 수정

--- src/main/java/util/LottoResultMessage.java
+++ src/main/java/util/LottoResultMessage.java
@@ -5,7 +5,7 @@ public enum LottoResultMessage {
     FORTH_RANK(4, "4개 일치 (50,000원) - "),
     THIRD_RANK(3, "5개 일치 (1,500,000원) - "),
     SECOND_RANK(2,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST_RANK(1,"6개 일치 (2000,000,000원) - ");
+    FIRST_RANK(1,"6개 일치 (2,000,000,000원) - ");
 
     private final int rank;
     private final String messageOFRank;

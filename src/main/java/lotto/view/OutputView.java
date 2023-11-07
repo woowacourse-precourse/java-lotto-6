2197dfef7f61524e35ee3e8f5b5ef8014afca7f7
jae woo Jung <awfjol2008@naver.com>
2023-11-08 01:52:49 +0900

    feat: 각 당첨 금액별 당첨된 로또는 몇 개인지 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -21,4 +21,62 @@ public class OutputView {
     protected void printReadBonusNumberMessage() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
+
+    // 전체 당첨 통계 출력
+    protected static void printWinningStatusMessage(List<PrizeResponse> prizeResponses, double profitRate) {
+        printWinningResultMessage();
+        printLineSeparator();
+        for (PrizeResponse prizeResponse : prizeResponses) {
+            printPrizeMessage(prizeResponse);
+        }
+        printProfitRateMessage(profitRate);
+    }
+
+    private static void printWinningResultMessage() {
+        System.out.println(PrizeMessage.WINNING_STATUS.getMessage());
+    }
+
+    private static void printLineSeparator() {
+        System.out.println(PrizeMessage.LINE_SEPARATOR.getMessage());
+    }
+
+    // 개별 당첨 통계 출력
+    private static void printPrizeMessage(PrizeResponse prizeResponse) {
+        printMatchingNumberCount(prizeResponse.getMatchingNumberCount());
+        if (isBonusCondition(prizeResponse.getPrize())) {
+            printBonusConditionMessage();
+        }
+        printPrizeMoneyMessage(prizeResponse.getPrizeMoney());
+        printWinningCountMessage(prizeResponse.getWinningCount());
+        printLineBreak();
+    }
+
+    private static void printMatchingNumberCount(int matchingNumberCount) {
+        System.out.printf(PrizeMessage.MATCHING_NUMBER_COUNT.getMessage(), matchingNumberCount);
+    }
+
+    private static boolean isBonusCondition(Prize prize) {
+        return Objects.equals(prize, Prize.FIVE_NUMBER_AND_BONUS_NUMBER_MATCH);
+    }
+
+    private static void printBonusConditionMessage() {
+        System.out.print(PrizeMessage.MATCHING_BONUS_NUMBER.getMessage());
+    }
+
+    private static void printPrizeMoneyMessage(int prizeMoney) {
+        DecimalFormat decimalFormat = new DecimalFormat(PrizeMessage.MONEY_FORMAT.getMessage());
+        System.out.printf(PrizeMessage.PRIZE_MONEY.getMessage(), decimalFormat.format(prizeMoney));
+    }
+
+    private static void printWinningCountMessage(int winningCount) {
+        System.out.printf(PrizeMessage.WINNING_COUNT.getMessage(), winningCount);
+    }
+
+    private static void printLineBreak() {
+        System.out.println();
+    }
+
+    protected static void printProfitRateMessage(double profitRate) {
+        System.out.printf(PrizeMessage.PROFIT_RATE.getMessage(), profitRate);
+    }
 }

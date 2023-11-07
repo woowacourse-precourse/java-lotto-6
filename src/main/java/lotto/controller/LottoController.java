02f1de05e271928c9f8182ecb647f6c244296fde
noteasymin <noteasymin@gmail.com>
2023-11-08 01:16:32 +0900

    fix: 예기치 못한 오류 테스트
    로또 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,18 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.service.LottoService;
 import lotto.view.LottoView;
+
+import java.util.List;
 
 public class LottoController {
     public static void playLotto() {
         Integer numberOfLotto = LottoView.getNumberOfLottoForPrice();
         System.out.println();
         System.out.println(numberOfLotto + "개를 구매했습니다.");
+
+        List<Lotto> lottoList = LottoService.generateLottoList(numberOfLotto);
 
     }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,12 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoService {
     public static Integer inputNumberOfLotto(String price) {
         return validatePrice(price) / 1000;
@@ -15,5 +22,19 @@ public class LottoService {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
         }
+    }
+
+    public static List<Lotto> generateLottoList(Integer numberOfLotto) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < numberOfLotto; i++) {
+            lottoList.add(new Lotto(generateLotto()));
+        }
+        return lottoList;
+    }
+
+    private static List<Integer> generateLotto() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(numbers);
+        return numbers;
     }
 }

162386deebc6a18408e328bd4689033216376650
pwrwpw <dlgustjr9912@gmail.com>
2023-11-07 11:01:41 +0900

    refactor : 입력 처리 방식 변경

--- src/main/java/lotto/utils/InputProcessor.java
+++ src/main/java/lotto/utils/InputProcessor.java
@@ -5,15 +5,29 @@ import java.util.List;
 import lotto.view.InputView;
 
 public class InputProcessor {
+
     private final InputView inputView;
 
     public InputProcessor(InputView inputView) {
         this.inputView = inputView;
     }
 
+    public int getUserInputPurchaseAmount() {
+        String input = inputView.inputPurchaseAmount();
+        Validation.validateStringToInteger(input);
+        return parseUserInput(input);
+    }
+    public int getUserInputBonusNumber() {
+        String input = inputView.inputBonusNumber();
+        Validation.validateStringToInteger(input);
+        return parseUserInput(input);
+    }
+
     public List<Integer> getUserInputWinningNumbers() {
+        return getUserInputWinningNumbers(inputView.inputWinningNumbers());
+    }
+    public List<Integer> getUserInputWinningNumbers(String userInput) {
         List<Integer> winningNumbers = new ArrayList<>();
-        String userInput = inputView.inputWinningNumbers();
 
         String[] numberStrings = userInput.split(",");
         for (String numberString : numberStrings) {
@@ -24,13 +38,7 @@ public class InputProcessor {
         return winningNumbers;
     }
 
-    public int getUserInputBonusNumber() {
-        String userInput = inputView.inputBonusNumber();
-        return Integer.parseInt(userInput.trim());
-    }
-
-    public int getUserInputPurchaseAmount() {
-        String userInput = inputView.inputPurchaseAmount();
+    public int parseUserInput(String userInput) {
         return Integer.parseInt(userInput.trim());
     }
 }

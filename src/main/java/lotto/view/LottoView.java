f2fb37e542c18a5cd4ee5a91a47b99bbbec4c8de
KimSangHyeok <tkdgur0906@naver.com>
2023-11-07 23:24:46 +0900

    feat : 당첨 내역을 출력하는 기능 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,6 +11,13 @@ public class LottoView {
     private static final String PURCHASE_COUNT = "개를 구매했습니다.";
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_RESULT = "당첨 통계";
+    private static final String THREE_MATCH = "3개 일치 (5000원) - ";
+    private static final String FOUR_MATCH = "4개 일치 (50000원) - ";
+    private static final String FIVE_MATCH = "5개 일치 (1500000원) - ";
+    private static final String FIVE_MATCH_WITH_BONUS = "5개 일치, 보너스 볼 일치 (30000000원) - ";
+    private static final String SIX_MATCH = "6개 일치 (2000000000원) - ";
+    private static final String QUANTITY = "개";
 
     public static void printPurchasePrompt() {
         System.out.println(PURCHASE_PRICE);
@@ -34,5 +42,15 @@ public class LottoView {
 
     public static void printBonusPrompt() {
         System.out.println(BONUS_NUMBER);
+    }
+
+    public static void printWinningResult(LottoResult lottoResult) {
+        System.out.println(WINNING_RESULT);
+        System.out.println("---");
+        System.out.println(THREE_MATCH + lottoResult.getThreeMatchCount() + QUANTITY);
+        System.out.println(FOUR_MATCH + lottoResult.getFourMatchCount() + QUANTITY);
+        System.out.println(FIVE_MATCH + lottoResult.getFiveMatchCount() + QUANTITY);
+        System.out.println(FIVE_MATCH_WITH_BONUS + lottoResult.getFiveMatchWithBonusCount() + QUANTITY);
+        System.out.println(SIX_MATCH + lottoResult.getSixMatchCount() + QUANTITY);
     }
 }

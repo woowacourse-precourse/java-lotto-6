5fb3e5c35c219add48a7958ee4b562afa8d93ecc
IM-GYURI <kgulr0517@gmail.com>
2023-11-08 01:48:50 +0900

    test: LottoTest 추가 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,16 @@
 package lotto;
 
+import java.util.stream.Stream;
+import lotto.domain.Lotto;
+import lotto.view.InputBonusNumberView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +29,36 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("보너스 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByDuplicatedNumbers() {
+        assertThatThrownBy(() -> InputBonusNumberView.validateBonusNumber(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 정렬이 되는지 확인한다.")
+    @Test
+    void sortedLottoNumber() {
+        List<Integer> numbers = List.of(6, 4, 5, 2, 1, 3);
+        List<Integer> answerNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto.toString()).isEqualTo(answerNumbers.toString());
+    }
+
+    @DisplayName("1 미만, 45 초과의 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("generateWrongRangeNumber")
+    void createLottoByWrongRangeNumber(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> generateWrongRangeNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 0, 4, 5, 6)),
+                Arguments.of(List.of(10, 11, 12, 45, 50, 23))
+        );
+    }
 }

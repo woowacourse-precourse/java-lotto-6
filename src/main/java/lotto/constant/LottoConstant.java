34d009af77183de47aed9d2ab31bae09ddb1b3a8
takealook97 <takealook97@naver.com>
2023-11-08 03:02:09 +0900

    refactor: LottoConstant 요소 추가
    로또게임 전반에 사용될 매직넘버들을 추가적으로 상수처리하였다.

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -4,7 +4,9 @@ public enum LottoConstant {
 	MIN_LOTTO_NUMBER(1),
 	MAX_LOTTO_NUMBER(45),
 	MIN_RANK(1),
-	MAX_RANK(5);
+	MAX_RANK(5),
+	PICK_COUNT(6),
+	LOTTO_PRICE(1000);
 
 	private final int value;
 

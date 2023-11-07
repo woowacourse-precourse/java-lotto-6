7ae6febe61831b1da5abecd6a1b7b3c2f0f28018
10Hyuna <kimha105@naver.com>
2023-11-07 23:50:32 +0900

    add: payment must be unit of thousand Exception

--- src/main/java/lotto/controller/Setting.java
+++ src/main/java/lotto/controller/Setting.java
@@ -18,16 +18,27 @@ public class Setting {
         output.printMessage(Announcer.PURCHASE_PAYMENT.getMessage());
         String payment = input.inputFromUser();
 
+        manageException(payment);
 
+        return Integer.parseInt(payment);
     }
 
     private void manageException(String payment) {
         isMinimumPayment(payment);
+        isUnitOfThousane(payment);
     }
 
     private void isMinimumPayment(String payment) {
         if(Integer.parseInt(payment) < 1000) {
             purchaseException.minimumPay();
+        }
+    }
+
+    private void isUnitOfThousane(String payment) {
+        int value = Integer.parseInt(payment) % 1000;
+
+        if(value != 0){
+            purchaseException.payThousand();
         }
     }
 }

0c74d5928ec8c37b32c013d863dd85f848bf28b2
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-07 10:37:19 +0900

    style: NumberUtil -> LottoNumberUtil 클래스 이름 변경

--- src/main/java/lotto/utils/LottoNumberUtil.java
+++ src/main/java/lotto/utils/LottoNumberUtil.java
@@ -1,10 +1,9 @@
 package lotto.utils;
 
-public class NumberUtil {
+public class LottoNumberUtil {
     public static String removeSpace(String number){
         return number.replaceAll(" ","");
     }
-    
     public static String[] splitComma(String number){
         return number.split(",");
     }

a273cc15a868d77cfcf6b5c360fa5f8bd8831f02
Jiwon Na <jiwonna52@gmail.com>
2023-11-07 21:21:44 +0900

    Refactor : Rank의 First를 다른 객체와 마찬가지로 전부 대문자로 변경

--- src/main/java/lotto/controller/result/Rank.java
+++ src/main/java/lotto/controller/result/Rank.java
@@ -5,7 +5,7 @@ public enum Rank {
     FOURTH(4, 4, 50000, "4개 일치 (50,000원) - "),
     THIRD(3, 5, 1500000, "5개 일치 (1,500,000원) - "),
     SECOND(2, 5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    First(1, 6, 2000000000, "6개 일치 (2,000,000,000원) - ");
+    FIRST(1, 6, 2000000000, "6개 일치 (2,000,000,000원) - ");
     private final int ranking;
     private final int correctNum;
     private final int prize;

be79e6802ea24e10233fc81545b081e619547591
c1oud-dev <gksmf0187@gmail.com>
2023-11-08 00:25:32 +0900

    feat : added lotto exception handling function

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +15,41 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            //throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if(numberSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 없어야 합니다.");
+        }
+        numbers.stream().forEach(this::validate_Lotto_Number);
+    }
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+    // TODO: 추가 기능 구현
+    public void Output_Number() {
+        System.out.println("[" + numbers.stream().map(String::valueOf).collect(Collectors.joining(", ")) + "]");
+    }
+
+    private  void validate_Lotto_Number(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public boolean contain_Bouns_Number(int bonus_number) {
+        return this.numbers.contains(bonus_number);
+    }
+    public int matchCount(Lotto lotto) {
+        int matchCount = 0;
+        for (Integer number : lotto.getNumbers()) {
+            if (this.numbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
 }
+

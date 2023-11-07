fadc101e0257f8146885eb8037e202b6d664947a
cyzlcyzl <cyzlcyzl09@gmail.com>
2023-11-07 18:40:23 +0900

    feat: 구입 금액 검증 기능

--- src/main/java/lotto/Amount.java
+++ src/main/java/lotto/Amount.java
@@ -1,2 +1,22 @@
-package lotto;public class Amount {
+package lotto;
+
+public class Amount {
+
+    private final static int UNIT = 1000;
+
+    private final int amount;
+
+    public Amount(int amount){
+        validateIsThousand(amount);
+        validateIsPositive(amount);
+        this.amount = amount;
+    }
+
+    private void validateIsThousand(int amount) {
+        if(amount % UNIT != 0) throw new IllegalArgumentException();
+    }
+
+    private void validateIsPositive(int amount){
+        if(amount <= 0) throw new IllegalArgumentException();
+    }
 }
--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -1,2 +1,30 @@
-package lotto;public class AmountTest {
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class AmountTest {
+
+    @DisplayName("입력값이 1000원 단위가 아닐시 오류가 발생한다.")
+    @Test
+    void inputNotThousand(){
+        assertThatThrownBy(() -> new Amount(1234)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력값이 0일시 오류가 발생한다.")
+    @Test
+    void inputZero(){
+        assertThatThrownBy(() -> new Amount(0)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력값이 음수일시 오류가 발생한다.")
+    @Test
+    void inputNegative(){
+        assertThatThrownBy(() -> new Amount(-1000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

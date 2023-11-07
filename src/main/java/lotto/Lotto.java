5e8f0aa247c02aaf0042c4521be853bfde2a3b5e
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2023-11-08 06:06:10 +0900

    feat: ranking 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,6 +20,19 @@ public class Lotto {
         return numbers;
     }
 
+    public int countMatch(List<Integer> winningNumber){
+        int result = 0;
+        for (int number : numbers){
+            if (winningNumber.contains(number)){
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public boolean containBonusNUmber(int bonusNumber){
+        return numbers.contains(bonusNumber);
+    }
 
 
     // TODO: 추가 기능 구현

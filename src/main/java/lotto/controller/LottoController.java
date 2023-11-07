1273e37993de76149209ce01c14e6a0a4979935c
김현재 <khjmr@naver.com>
2023-11-07 21:56:08 +0900

    feat(purchaseLottos): 로또를 구매한후 출력하는 로직 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,23 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoFactory;
+import lotto.model.Lottos;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    private final OutputView outputView = OutputView.getInstance();
+    private final InputView inputView = InputView.getInstance();
+    private final LottoFactory lottoFactory = new LottoFactory();
+
+    public Lottos purchaseLottos(int numberOfLotto) {
+        outputView.printOutputNumberOfPurchaseLotto(numberOfLotto);
+        Lottos lottos = lottoFactory.createLottos(numberOfLotto);
+        outputView.printLottos(lottos.toString());
+        return lottos;
+    }
+}

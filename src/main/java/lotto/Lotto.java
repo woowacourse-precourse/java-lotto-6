78d28615116bdc70bf60dc2eece24b0a9af0efc6
윤병호 <pdot715@gmail.com>
2023-11-08 08:17:12 +0900

    refactor(Lotto):validate 메소드 내 숫자 상수로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,7 +20,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(SIZE_CHECK_ERROR);
         }
         if (duplicateCheck(numbers)) {

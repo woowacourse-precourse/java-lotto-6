3065454eb587af08e764b53cc37175f346396efc
nayonsoso <panda0329@naver.com>
2023-11-08 05:40:23 +0900

    feat: 요청 메세지 상수화

---
+++ src/main/java/constants/RequestMessage.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class RequestMessage {
+    private static final String PAYMENT = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}

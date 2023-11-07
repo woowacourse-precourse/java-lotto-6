8dafebe85733e1d4dabe9c59111424bb45c94f1f
Sur0n <skyzephyr333@gmail.com>
2023-11-07 20:59:02 +0900

    feat : build setBonusNumber, buyLottoTickets

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,30 +19,54 @@ public class Lotto {
         validate(numbers);
         this.numbers = numbers;
     }
+
     //numbers의 리스트 검증부
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-    // 중복값 검증부 제작 필요
+
     //https://doing7.tistory.com/145 참고 set 혹은 stream.distinct()이용
-    private void validateDuplicate(List<Integer> numbers){
+    // 중복값 검증부 제작
+    private void validateDuplicate(List<Integer> numbers) {
+        //형변환
         Set<Integer> checkDuplicateNumbers = new HashSet<>(numbers);
+        // != 연산부분 포장해서 true값으로 작업할것?
         if (numbers.size() != checkDuplicateNumbers.size()) {
             throw new IllegalArgumentException();
         }
     }
+
     // TODO: 추가 기능 구현
-    public void setBonusNumber(int bonusNumber){
+    public void setBonusNumber(int bonusNumber) {
+        validateInRangebBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
-    public void buyLottoes(int moneys){
 
+    private void validateInRangebBonusNumber(int bonusNumber) {
+        // 포장필요 magicnumbers
+        if (bonusNumber < MagicNumbers.CHECK_NUMBER_ONE.getMagicNumbers()) {
+            throw new IllegalArgumentException();
+        }
+        if (MagicNumbers.CHECK_NUMBER_FOURTYFIVE.getMagicNumbers() < bonusNumber) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    private void pickRandomNumbers(int moneys){
-//        numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+    public int buyLottoTickets(int moneys) {
+        // != 연산부분 포장해서 true값으로 작업할것?
+        if (moneys % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        int tickets;
+        return tickets = moneys/1000;
+    }
+
+    //리팩토링 필요
+    public Set<Integer> pickRandoms() {
+        Set<Integer> pickRandomnumbers = new HashSet<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        return pickRandomnumbers;
 
     }
 

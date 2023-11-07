808881de8133ddd8f35a3302dcd2f5f4c4982911
Jun Hyeok Choi <chrisgold@naver.com>
2023-11-08 00:21:09 +0900

    refactor(method): rename 'input_money' to 'String_to_Int'

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,10 +7,10 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.print("구입금액을 입력해 주세요.\n");
         String input = Console.readLine();
-        int money = input_money(input);
+        int money = String_to_Int(input);
         System.out.print(money);
     }
-    private static int input_money(String str){      // Input을 String으로 받아서 Int로 반환
+    private static int String_to_Int(String str){      // Input을 String으로 받아서 Int로 반환
         try{
             return Integer.parseInt(str);
         }catch (NumberFormatException e){  // 변환 포멧 안맞으면 IllegalArgumentException 발생

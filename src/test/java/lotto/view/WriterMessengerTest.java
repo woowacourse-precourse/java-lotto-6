5323e9f546df9f1253d32df274a326352fccbc2d
unknown <y2kf9a0q@gmail.com>
2023-11-07 17:36:03 +0900

    feat(lotto): WriterMessengerTest.java - 문자열 출력 테스트

---
+++ src/test/java/lotto/view/WriterMessengerTest.java
@@ -0,0 +1,41 @@
+package lotto.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WriterMessengerTest {
+    private PrintStream standardOut;
+    private OutputStream captor;
+    private Messenger writerMessenger;
+
+    @BeforeEach
+    void setUp() {
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
+        writerMessenger = new WriterMessenger();
+    }
+
+    @Test
+    void print() {
+        String input = "message";
+        String expected = "message" + System.lineSeparator();
+
+        writerMessenger.print(input);
+
+        assertThat(captor.toString()).isEqualTo(expected);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+}

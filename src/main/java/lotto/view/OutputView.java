2ebbc8cf8afc16e1edc3564cd1853f2ab58fbb70
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 03:58:23 +0900

    feat($OutputView): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.type.LottoPrize;
 
 import java.util.List;
 
@@ -11,5 +12,24 @@ public class OutputView {
             System.out.println(userLotto.getNumbers());
         }
         System.out.println();
+    }
+
+    public void displayResults(List<LottoPrize> result) {
+        System.out.println("당첨 통계\n---");
+        for (LottoPrize prize : LottoPrize.values()) {
+            if (prize.equals(LottoPrize.NONE)) {
+                continue;
+            }
+            int num = result.stream()
+                    .filter(r -> prize.getResult().equals(r.getResult()))
+                    .toList()
+                    .size();
+            System.out.print(prize.getResult());
+            System.out.println(" - " + num + "개");
+        }
+    }
+
+    public void displayReturnRatio(double returnRatio) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", returnRatio);
     }
 }

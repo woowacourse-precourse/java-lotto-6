248e6342bd6bffe1fd8c1ff178f09204d4d14b85
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-07 17:48:35 +0900

    setInputPrice 구현

--- src/main/java/Controller/ModelHandler.java
+++ src/main/java/Controller/ModelHandler.java
@@ -10,6 +10,7 @@ public class ModelHandler {
     private final Domain DOMAIN = Domain.getInstance();
     private final Service SERVICE = Service.getInstance();
     private final InputView INPUT_VIEW = InputView.getInstance();
+    private final ExceptionHandler EXCEPTION = new ExceptionHandler();
 
     public List<Lotto> MY_LOTTO = DOMAIN.getMyLotto();
     public List<Integer> CORRECT_NUMBER_COUNT = DOMAIN.getCorrectNumberCount();
@@ -25,11 +26,14 @@ public class ModelHandler {
     }
 
     public void setInputPrice() {
-        DOMAIN.setPrice(INPUT_VIEW.inputPrice());
+         EXCEPTION.checkPriceUnit(INPUT_VIEW.inputPrice());
+         DOMAIN.setPrice(INPUT_VIEW.inputPrice());
     }
 
     public void setInputWinningNumber() {
-        DOMAIN.setLottoWinningNumber(SERVICE.makeLotto(INPUT_VIEW.inputWinningNumber()));
+        DOMAIN.setLottoWinningNumber(
+                SERVICE.makeLotto(INPUT_VIEW.inputWinningNumber())
+        );
     }
 
     public void setInputBonusNumber() {

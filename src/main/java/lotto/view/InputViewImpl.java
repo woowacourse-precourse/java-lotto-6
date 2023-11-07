33286d3c21f04ca6f6b7539b801941a5c3192b95
reddevilmidzy <midzy0228@gmail.com>
2023-11-07 18:00:00 +0900

    refactor: 입력받는 메서드들 중복 제거

--- src/main/java/lotto/view/InputViewImpl.java
+++ src/main/java/lotto/view/InputViewImpl.java
@@ -5,29 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputViewImpl implements InputView {
     @Override
     public String readPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String input = readLine();
-        System.out.println();
-        return input;
+        return readLine("구입금액을 입력해 주세요.");
     }
 
     @Override
     public String readWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String input = readLine();
-        System.out.println();
-        return input;
+        return readLine("당첨 번호를 입력해 주세요.");
     }
 
     @Override
     public String readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String input = readLine();
-        System.out.println();
-        return input;
+        return readLine("보너스 번호를 입력해 주세요.");
     }
 
-    protected String readLine() {
-        return Console.readLine();
+    protected String readLine(String message) {
+        System.out.println(message);
+        String input = Console.readLine();
+        System.out.println();
+        return input;
     }
 }

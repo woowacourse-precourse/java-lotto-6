7dd17bf917cf5747455c5746f734f96c77677b70
tmdduq500 <osy9907@gmail.com>
2023-11-07 10:54:25 +0900

    fix: 구입금액 입력메시지, 당첨번호 입력메시지, 보너스번호 입력메시지 변수 final로 재 선언

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,9 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
-    static String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    static String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void InputAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);

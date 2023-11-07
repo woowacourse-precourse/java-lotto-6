7e2b32f89da118d58831af94cbaff07359a1f997
JP-company <wjsdj2008@gmail.com>
2023-11-07 20:48:18 +0900

    Feat: [추가] 유저 객체 당첨 번호, 보너스 번호 입력 기능

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -12,9 +12,39 @@ public class User {
         try {
             String inputPayment = UIFunction.requestInput("구입 금액을 입력해 주세요.");
             int payment = Parser.toInteger(inputPayment);
+            return new Market().issueLotto(payment);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return buyLotto();
+        }
+    }
+
+    public LottoWinningNumber createWinningLottoNumber() {
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber();
+        createLotto(lottoWinningNumber);
+        createBonusNumber(lottoWinningNumber);
+        return lottoWinningNumber;
+    }
+
+    private void createLotto(LottoWinningNumber lottoWinningNumber) {
+        try {
+            String inputLottoNumbers = UIFunction.requestInput("당첨 번호를 입력해 주세요.");
+            List<Integer> numbers = Parser.toIntegerList(inputLottoNumbers);
+            lottoWinningNumber.setLotto(new Lotto(numbers));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            createLotto(lottoWinningNumber);
+        }
+    }
+
+    private void createBonusNumber(LottoWinningNumber lottoWinningNumber) {
+        try {
+            String inputBonusNumber = UIFunction.requestInput("보너스 번호를 입력해 주세요");
+            int bonusNumber = Parser.toInteger(inputBonusNumber);
+            lottoWinningNumber.setBonusNumber(bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            createBonusNumber(lottoWinningNumber);
         }
     }
 }

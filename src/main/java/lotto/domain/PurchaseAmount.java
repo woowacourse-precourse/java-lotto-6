734b0fb0f7521147c7848253d85f8abc448b4c48
whereami2048 <whereami2048@gmail.com>
2023-11-07 13:33:26 +0900

    feat: 로또 구매 금액 입력 예외처리(1000원으로 나누어지지 않는 경우) 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,9 +1,18 @@
 package lotto.domain;
 
+import lotto.constant.LottoConstant;
+
 public class PurchaseAmount {
     private final int purchaseAmount;
 
     public PurchaseAmount(int purchaseAmount) {
+        validateIsDivideByLottoPrice();
         this.purchaseAmount = purchaseAmount;
+    }
+
+    private void validateIsDivideByLottoPrice() {
+        if (purchaseAmount % LottoConstant.LOTTO_PRICE.value() == 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }

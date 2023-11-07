f42c504324b43448c6e15639288b4fdb8c0e4c57
dhl1924 <dhl1924@naver.com>
2023-11-08 01:00:20 +0900

    refactor(InputView): 구입금액 입력 메시지 상수 이름 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,13 +2,13 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 public class InputView {
-    private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_LOTTO_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String INVALID_NUMBER_FORMAT = "[ERROR] 숫자만 입력이 가능해요";
 
     public static int inputLottoAmount() {
-        System.out.println(INPUT_LOTTO_AMOUNT);
+        System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT);
         return convertToIntegerValue(Console.readLine());
     }
 

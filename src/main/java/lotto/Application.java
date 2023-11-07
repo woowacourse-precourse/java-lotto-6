5e723180ee28481a3e83a916addc8557c0a734ce
doublekm153 <doublekm153@gmail.com>
2023-11-08 07:39:41 +0900

    당첨 확인 기능(보너스 볼 x)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Application {
@@ -23,5 +25,19 @@ public class Application {
             .map(Integer::parseInt)
             .collect(Collectors.toList());
         WinningLotto winningLotto = new WinningLotto(winningNumbers);
+        
+        Map<Integer, Integer> matchCount = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            int count = lotto.countMatchedNumbers(winningLotto.getLotto());
+            matchCount.put(count, matchCount.getOrDefault(count, 0) + 1);
+        }
+        
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println("3개 일치 (5,000원)- " + matchCount.getOrDefault(3, 0) + "개");
+        System.out.println("4개 일치 (50,000원)- " + matchCount.getOrDefault(4, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원)- " + matchCount.getOrDefault(5, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원)- " + matchCount.getOrDefault(6, 0) + "개");
     }
 }
+
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,6 +16,14 @@ public class Lotto {
         }
     }
     
+//    Lotto 객체에 다른 Lotto 객체를 인자로 받아 일치하는 번호와 갯수를 반환하는 메서드 추가
+//    당첨 번호와 사용자의 번호가 얼마나 일치하는지 확인 가능
+    public int countMatchedNumbers(Lotto other) {
+        return (int) numbers.stream()
+            .filter(other.numbers::contains)
+            .count();
+    }
+    
     @Override
     public String toString() {
         return numbers.toString();

423bc1c3a32e88088d98b093ce0e46a7a182c775
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 06:18:29 +0900

    refactor(OutputView) : 메서드 인자 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,33 +1,43 @@
 package lotto.view;
 
 import static lotto.ViewMessage.BONUS_PROMPT_MESSAGE;
+import static lotto.ViewMessage.NUMBER_OF_PURCHASED_FORMAT;
 import static lotto.ViewMessage.PAYMENT_PROMPT_MESSAGE;
+import static lotto.ViewMessage.RATE_OF_REVENUE_FORMAT;
 import static lotto.ViewMessage.WINNING_NUMBERS_PROMPT_MESSAGE;
 import static lotto.ViewMessage.WINNING_RESULT_MESSAGE;
 
+import java.util.List;
+
 public class OutputView {
     public void paymentPrompt() {
-        System.out.println(PAYMENT_PROMPT_MESSAGE);
+        System.out.println(PAYMENT_PROMPT_MESSAGE.get());
     }
 
     public void bonusPrompt() {
-        System.out.println(BONUS_PROMPT_MESSAGE);
+        System.out.println(BONUS_PROMPT_MESSAGE.get());
     }
 
     public void winningNumberPrompt() {
-        System.out.println(WINNING_NUMBERS_PROMPT_MESSAGE);
+        System.out.println(WINNING_NUMBERS_PROMPT_MESSAGE.get());
+    }
+
+    public void printNumberOfPurchased(int numberOfLottoTickets) {
+        System.out.println(String.format(NUMBER_OF_PURCHASED_FORMAT.get(), numberOfLottoTickets));
     }
 
     public void printLottoTickets(String lottoNumbers) {
         System.out.println(lottoNumbers);
     }
 
-    public void printWinningResult(String winningResult) {
-        System.out.println(WINNING_RESULT_MESSAGE);
-        System.out.println(winningResult);
+    public void printWinningResult(List<String> winningResult) {
+        System.out.println(WINNING_RESULT_MESSAGE.get());
+        for (int i = 0; i < winningResult.size(); i++) {
+            System.out.println(winningResult.get(i));
+        }
     }
 
-    public void printRateOfRevenue(String rateOfRevenue) {
-        System.out.printf(rateOfRevenue);
+    public void printRateOfRevenue(double rateOfRevenue) {
+        System.out.println(String.format(RATE_OF_REVENUE_FORMAT.get(), rateOfRevenue));
     }
 }

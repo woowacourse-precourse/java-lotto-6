ca083b482ac9f5c941efdfe293d69799fb8c18f6
jmj005 <jmj00005@naver.com>
2023-11-07 20:06:47 +0900

    refactor: 컨벤션 적용

--- src/main/java/lotto/mapper/LottoDtoMapper.java
+++ src/main/java/lotto/mapper/LottoDtoMapper.java
@@ -4,7 +4,7 @@ import lotto.dto.LottoDto;
 import lotto.model.Lotto;
 
 public class LottoDtoMapper {
-    public static LottoDto from (Lotto lotto) {
+    public static LottoDto from(Lotto lotto) {
         return new LottoDto(lotto.getNumbers());
     }
 }

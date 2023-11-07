0fc4d02d47e35b84750f7648417ce522aa53ccc3
SuhyungK <dook4966@gmail.com>
2023-11-08 08:33:23 +0900

    fix(User): public 접근 제어자 추가로 패키지 밖에서 접근 안 되는 문제 해결

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -3,15 +3,22 @@ package lotto.domain;
 import java.util.List;
 
 public class User {
-    int payment;
-    List<List<Integer>> userLottos;
+    public int payment;
+    public final List<List<Integer>> userLottos;
     public User(String payment) {
         this.payment = InputHandler.convertStringToInteger(payment);
         userLottos = Lotto.publish(this.payment);
     }
 
-    public static User publishLotto(String payment) {
-        return new User(null);
-    }
+//    public static User publishLotto(String payment) {
+//        return new User(null);
+//    }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "payment=" + payment +
+                ", userLottos=" + userLottos +
+                '}';
+    }
 }

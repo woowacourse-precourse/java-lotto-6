1b64ccb0fafc05eb105876663bb21ea89e653f9d
bitesnail <rkwoal216@gmail.com>
2023-11-07 16:05:10 +0900

    feature: implementation of validation and compare function

---
+++ src/main/java/lotto/utils.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.List;
+
+public class utils {
+    public static <T> boolean hasDuplicates(List<T> list){
+        return list.stream().distinct().count() != list.size();
+    }
+    public static boolean hasInvalidValue(List<Integer> list, int min, int max){
+        return list.stream().anyMatch(e -> e < min || e > max);
+    }
+
+    public static int getCountOfMatch(List<Integer> list1, List<Integer> list2){
+        int count = 0;
+        for(int number : list1){
+            if(list2.contains(number)){
+                count++;
+            }
+        }
+        return count;
+    }
+}

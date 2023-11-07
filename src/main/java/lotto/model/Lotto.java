59a7c40a3fa146db1767e07d336d6acc449f0017
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-08 04:21:58 +0900

    refactor: Lotto 클래스 리팩토링

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,48 +1,63 @@
 package lotto.model;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
     private static final int LOTTO_TICKET_SIZE = 6;
 
-    private final List<LottoNumber> lottoTickets;
+    private final List<Integer> lottoNumbers;
 
-    public Lotto(String lottoNumbers) {
-        this.lottoTickets = parseLottoNumbers(lottoNumbers);
+    public Lotto(List<Integer> lottoNumbers) {
+        validateLottoSize(lottoNumbers);
+        validateLottoDuplication(lottoNumbers);
+        validateWinningNumbersRange(lottoNumbers);
+        Collections.sort(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    public Lotto(List<LottoNumber> lottoTickets) {
-        validateLottoSize(lottoTickets);
-        validateLottoDuplication(lottoTickets);
-        this.lottoTickets = lottoTickets;
+    public List<Integer> getLottoNumbers() {
+        return Collections.unmodifiableList(lottoNumbers);
     }
 
-    public List<LottoNumber> getNumbers() {
-        return Collections.unmodifiableList(lottoTickets);
+    public int countMatchingNumbers(Lotto winningNumbers) {
+        return (int) this.lottoNumbers.stream()
+                .filter(winningNumbers.lottoNumbers::contains)
+                .count();
     }
 
-    public List<LottoNumber> parseLottoNumbers(String lottoNumbers) {
-
-        return Arrays.stream(lottoNumbers.split(","))
-                .map(String::trim)
-                .map(number -> new LottoNumber(Integer.parseInt(number)))
-                .collect(Collectors.toList());
+    public boolean matchBonusNumber(int bonusNumber) {
+        return lottoNumbers.contains(bonusNumber);
     }
 
-    private void validateLottoSize(List<LottoNumber> lottoTickets) {
-        if (lottoTickets.size() != LOTTO_TICKET_SIZE) {
+    public LottoResult match(Lotto winningNumbers,
+                             int bonusNumber) {
+        return LottoResult.findResult(countMatchingNumbers(winningNumbers), matchBonusNumber(bonusNumber));
+    }
+
+    private void validateWinningNumbersRange(List<Integer> lottoNumbers) {
+        lottoNumbers.forEach(this::validateWinningNumberRange);
+    }
+
+    private void validateWinningNumberRange(int lottoNumber) {
+        if (lottoNumber < MINIMUM_LOTTO_NUMBER || lottoNumber > MAXIMUM_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateLottoSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_TICKET_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리의 숫자여야 합니다.");
         }
     }
 
-    private void validateLottoDuplication(List<LottoNumber> lottoTickets) {
-        long count = lottoTickets.stream().distinct().count();
+    private void validateLottoDuplication(List<Integer> lottoNumbers) {
+        long count = lottoNumbers.stream().distinct().count();
 
-        if (count != lottoTickets.size()) {
+        if (count != lottoNumbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다.");
         }
     }

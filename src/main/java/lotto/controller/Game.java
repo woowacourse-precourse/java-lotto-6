8213574b21a7174ec718cbd31657a9b9b74b4ceb
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 21:19:09 +0900

    refactor(Game): 구입 금액 입력, 구매 로또 목록 출력 기능 private 함수로 분리

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -10,25 +10,12 @@ import java.util.List;
 
 public class Game {
     public void start() {
-        int purchaseAmount = 0;
-        while (true) {
-            try {
-                purchaseAmount = InputView.getPurchaseAmount();
-                // 입력이 유효하면 계속 진행
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] " + e.getMessage());
-            }
-        }
-        Cash cash = new Cash(purchaseAmount);
-        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        LottoSeller lottoSeller = new LottoSeller(lottoNumberGenerator);
+        //
+        Cash cash = getCash();
 
-        Customer customer = new Customer(cash, lottoSeller);
+        List<Lotto> lottos = getLottos(cash);
 
-        customer.purchaseLottos();
-
-        OutputView.displayLottos(customer.getLottos());
+        OutputView.displayLottos(lottos);
 
         List<Integer> winningNumbers = new ArrayList<>();
         while (true) {
@@ -57,7 +44,7 @@ public class Game {
         BonusNumber bonusNumber = new BonusNumber(number);
 
         WinningNumbers winningNumbers1 = new WinningNumbers(winningNumber, bonusNumber);
-        WinningRanks winningRanks = new WinningRanks(customer.getLottos(), winningNumbers1);
+        WinningRanks winningRanks = new WinningRanks(lottos, winningNumbers1);
 
         winningRanks.calculateRanks();
 
@@ -67,5 +54,28 @@ public class Game {
         WinningYield winningYield = new WinningYield(cash, profit);
 
         OutputView.displayWinningDetails(winningRanks.getRanks(), winningYield);
+    }
+
+    private Cash getCash() {
+        int purchaseAmount = 0;
+        while (true) {
+            try {
+                purchaseAmount = InputView.getPurchaseAmount();
+                // 입력이 유효하면 계속 진행
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+        return new Cash(purchaseAmount);
+    }
+
+    private List<Lotto> getLottos(Cash cash) {
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        LottoSeller lottoSeller = new LottoSeller(lottoNumberGenerator);
+
+        Customer customer = new Customer(cash, lottoSeller);
+
+        return customer.purchaseLottos();
     }
 }

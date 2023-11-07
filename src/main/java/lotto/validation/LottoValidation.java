a5d01a9b090f9ad1d1ecb203b3c733738ff54dbf
82user <zxcvc9999@gmail.com>
2023-11-08 04:21:52 +0900

    feat: validate winning numbers and bonus number

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -1,8 +1,11 @@
 package lotto.validation;
 
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class LottoValidation {
+
+    private StringTokenizer st;
 
     public void validatePurchasePrice(String input) {
         validateIsDigit(input);
@@ -14,8 +17,50 @@ public class LottoValidation {
         validateCharacterLength(numbers);
     }
 
-    private static void validateCharacterLength(List<Integer> numbers) {
-        if(numbers.size() != 6) {
+    public void validateWinningNumbers(String input) {
+        String value = input.replace(" ", "");
+        validateSeparator(value);
+        validateNumberRange(value);
+        validateCharacterLength(value);
+    }
+
+    public void validateBonusNumber(String input, List<Integer> winningNumbers) {
+        validateIsDigit(input);
+        validateNumberRange(input);
+
+        int value = Integer.parseInt(input);
+        for (Integer winningNumber : winningNumbers) {
+            if (winningNumber == value) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안 됩니다.");
+            }
+        }
+    }
+
+    private void validateSeparator(String input) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 쉼표로 구분하여 입력해 주세요.");
+        }
+
+        boolean matches = input.matches("[0-9|,]+");
+        if (!matches) {
+            throw new IllegalArgumentException("[ERROR] 숫자와 쉼표(,)만 입력 가능합니다.");
+        }
+    }
+
+    private void validateCharacterLength(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 6자리여야 합니다.");
+        }
+    }
+
+    private void validateCharacterLength(String input) {
+        st = new StringTokenizer(input, ",");
+        int count = 0;
+        while (st.hasMoreTokens()) {
+            st.nextToken();
+            count++;
+        }
+        if (count != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 숫자는 6자리여야 합니다.");
         }
     }
@@ -23,6 +68,14 @@ public class LottoValidation {
     private void validateNumberRange(List<Integer> numbers) {
         for (Integer number : numbers) {
             findNumber(number);
+        }
+    }
+
+    private void validateNumberRange(String input) {
+        st = new StringTokenizer(input, ",");
+        while (st.hasMoreTokens()) {
+            int value = Integer.parseInt(st.nextToken());
+            findNumber(value);
         }
     }
 

efb55182e5af9c0bc663c3152de8764da723bfd3
Kiltae <loverpleasestayo0@gmail.com>
2023-11-07 17:27:09 +0900

    feat: profit calculation and final result controller

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -47,5 +47,39 @@ public class LottoMachine {
         prize = Service.setPrize(winNumber, bonusNumber);
     }
 
-   
+    private void showResult(List<Lotto> lottoList, Prize winningLotto, int amount) {
+        Map<Ranking, Integer> result = setResult();
+        Ranking rank;
+        OutputView.printSuccessResult();
+        for (int i = 0; i < lottoList.size(); i++) {
+            rank = winningLotto.match(lottoList.get(i));
+            result.put(rank, result.get(rank) + 1);
+        }
+        printResult(result);
+        printEarningRate(result, amount);
+    }
+
+    private void printResult(Map<Ranking, Integer> result) {
+        for (int i = Ranking.values().length - 1; i >= 0; i--) {
+            Ranking.values()[i].printMessage(result.get(Ranking.values()[i]));
+        }
+    }
+
+    private void printEarningRate(Map<Ranking, Integer> result, int lottoAmount) {
+        double EarningRate = 0;
+        for (Ranking rank : result.keySet()) {
+            EarningRate =
+                    EarningRate + ((double) (rank.getWinningAmount()) / (lottoAmount * TICKET_PRICE) * (result.get(
+                            rank)) * (PERCENTAGE));
+        }
+        OutputView.printRevenueRate(EarningRate);
+    }
+
+    private Map<Ranking, Integer> setResult() {
+        Map<Ranking, Integer> result = new LinkedHashMap<>();
+        for (Ranking rank : Ranking.values()) {
+            result.put(rank, 0);
+        }
+        return result;
+    }
 }

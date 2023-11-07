f27981ca98ea574786d3651f12cc6f1a925ee710
shlihs95 <fhzptvh@gmail.com>
2023-11-07 22:48:54 +0900

    Update: 보너스 번호 유효값 검증하는 매서드에서 비교하기 위한 당첨 번호 파라미터 형식을 문자열에서 List로 변경 후 테스트케이스 수정

--- src/test/java/lotto/validation/InputValidationTest.java
+++ src/test/java/lotto/validation/InputValidationTest.java
@@ -3,6 +3,9 @@ package lotto.validation;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputValidationTest {
@@ -48,7 +51,7 @@ class InputValidationTest {
     void validateBonusNumberTest_success() {
         //given
         String number = "45";
-        String numbers = "1,2,3,4,5,6";
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
 
         //when
         boolean result = inputValidation.validateBonusNumber(numbers, number);
@@ -59,7 +62,14 @@ class InputValidationTest {
 
     @Test
     void validateBonusNumberTest_exception() {
-        Assertions.assertThatThrownBy(() -> inputValidation.validateBonusNumber("1,2,3,4,5,6", "6"))
+        //given
+        List<Integer> numbers = Arrays.asList(2, 15, 19, 21, 33, 42);
+
+        //when
+        String bonusNumber = "15";
+
+        //then
+        Assertions.assertThatThrownBy(() -> inputValidation.validateBonusNumber(numbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

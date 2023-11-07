550b49e7d0c14d81ab17bd3fb54143fd23e538ee
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-08 01:36:43 +0900

    controller 틀 생성

---
+++ src/main/java/lotto/controller/Controller.java
@@ -0,0 +1,12 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class Controller {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void startGame() {
+    }
+}

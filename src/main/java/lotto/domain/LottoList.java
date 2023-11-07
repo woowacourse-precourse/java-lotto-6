5e419e519db3d18287b7263c33404a949f71f9a3
changhyun9 <ckdgus1713@naver.com>
2023-11-07 23:54:46 +0900

    feat : add class of LottoList

--- src/main/java/lotto/domain/LottoList.java
+++ src/main/java/lotto/domain/LottoList.java
@@ -1,2 +1,24 @@
-package lotto.domain;public class LottoList {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoList {
+    private final List<Lotto> lottoList;
+
+    public LottoList() {
+        lottoList = new ArrayList<>();
+    }
+
+    public void insertLotto(Lotto lotto) {
+        lottoList.add(lotto);
+    }
+
+    public int getSize() {
+        return lottoList.size();
+    }
+
+    public List<Lotto> getAllLotto() {
+        return lottoList;
+    }
 }

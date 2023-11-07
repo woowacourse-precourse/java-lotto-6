ead97411803aeafbbb0dffaf4ba7a707f2eca49e
yubin <dbqls2821@gmail.com>
2023-11-07 22:50:49 +0900

    test: 통계 발행 테스트코드 추가

---
+++ src/test/java/lotto/domain/StatisticsPublisherTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import lotto.domain.dto.Statistics;
+import lotto.io.LottoInput;
+import lotto.io.LottoOutput;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsPublisherTest {
+
+    @Test
+    void 통계_발행_테스트() {
+        StatisticsPublisher publisher = new StatisticsPublisher(new LottoController(new LottoInput(() -> ""), new LottoOutput(new LottosConverter(), new StatisticsConverter())));
+        Lottos generatedLottos = new Lottos();
+        generatedLottos.addLotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(7, winningNumber);
+        Money money = new Money(1000);
+
+        Statistics actual = publisher.publish(generatedLottos, winningNumber, bonusNumber, money);
+
+        assertEquals(new Statistics(List.of(Result.SIX_MATCH), 200000000), actual);
+    }
+}

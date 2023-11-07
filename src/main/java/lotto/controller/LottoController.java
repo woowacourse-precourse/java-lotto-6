ab90be47418d7827d8c3424af4fa8138633c84b8
JDUB9823 <jeong98231@gmail.com>
2023-11-07 17:31:41 +0900

    feat: 로또 번호 비교 기능구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -23,6 +23,25 @@ public class LottoController {
             OutputView.showPurchasedLotto(lotto.getLottoNumbers());
         }
 
-        inputController.getWinnerNumbersFromUser();
+        LottoFromUser lottoFromUser = inputController.getWinnerNumbersFromUser();
+        lottery(lottoPurchasingAmount, lottos, lottoFromUser);
+    }
+
+    public void lottery(LottoPurchasingAmount lottoPurchasingAmount, Lottos lottos, LottoFromUser lottoFromUser) {
+        getProfits(lottos, lottoFromUser);
+    }
+
+    public void getProfits(Lottos lottos, LottoFromUser lottoFromUser) {
+        for (Lotto lotto : lottos.getLottoList()) {
+            int numberMatches = compareLotto(lotto, lottoFromUser);
+            boolean bonusCheck = lotto.getLottoNumbers().contains(lottoFromUser.getBonusNumber());
+
+            System.out.println("맞춘 숫자 갯수 : " + numberMatches + " 보너스 숫자 맞춘 여부 : " + bonusCheck);
+        }
+    }
+
+    public int compareLotto(Lotto lotto, LottoFromUser lottoFromUser) {
+        return (int) lotto.getLottoNumbers().stream().filter(
+                number -> lottoFromUser.getLotto().getLottoNumbers().contains(number)).count();
     }
 }

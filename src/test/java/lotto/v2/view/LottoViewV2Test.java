10f832440130903212d5b538ac6ab4c45a76ec17
권장순 <jsoonworld@gmail.com>
2023-11-07 12:54:02 +0900

    refactoring : domain logic에서 UI 부분을 분리하고, Java Enum을 적용을 한 뒤 단위 테스트를 실행.

--- src/test/java/lotto/v2/view/LottoViewV2Test.java
+++ src/test/java/lotto/v2/view/LottoViewV2Test.java
@@ -2,10 +2,12 @@ package lotto.v2.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class LottoViewV2Test {
 
@@ -18,6 +20,18 @@ class LottoViewV2Test {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         lottoViewV2 = new LottoViewV2(console);
+    }
+
+    @Test
+    public void testInputMoney_ValidAmount() {
+        // Given
+        when(console.readLine()).thenReturn("3000");
+
+        // When
+        int amount = lottoViewV2.inputMoney();
+
+        // Then
+        assertThat(amount).isEqualTo(3000);
     }
 
 }

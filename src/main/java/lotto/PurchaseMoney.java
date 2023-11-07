d504852386a555e4d9adf27440f112c1aafdcf9f
aammddkkzxc <ideal0427@naver.com>
2023-11-07 17:08:06 +0900

    feat: 구입 금액 양의 정수 요효성 검사 기능 구현

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -1,12 +1,19 @@
 package lotto;
 
 public class PurchaseMoney {
-    private static int LOTTO_PRICE = 1000;
+    private static final int LOTTO_PRICE = 1000;
+    private static final String POSITIVE_NUMBER_REQUEST_MESSAGE = "구입 금액은 양의 정수여야 합니다.";
 
     private final int amount;
 
     public PurchaseMoney(int amount) {
         this.amount = amount;
+    }
+
+    private void validatePositiveNumber() {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(POSITIVE_NUMBER_REQUEST_MESSAGE);
+        }
     }
 
     public int getLottoQuantity() {

aa33927953a6191b4c1bf4d24e7589581485778a
juns1s <juns1s@naver.com>
2023-11-07 22:57:43 +0900

    comment: 주석 적용

--- src/main/java/lotto/message/LottoExceptionMessage.java
+++ src/main/java/lotto/message/LottoExceptionMessage.java
@@ -1,5 +1,8 @@
 package lotto.message;
 
+/**
+ * Lotto 객체 생성시 예외값 정의 enum
+ */
 public enum LottoExceptionMessage {
     INVALID_LOTTO_LENGTH("[ERROR] 로또 번호는 6자리여야 합니다."),
     INVALID_LOTTO_DUPLICATED("[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다.");

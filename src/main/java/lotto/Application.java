c141bf7b415b442e2c6972183f4bab66d2ef8f13
Choi Seunghun <0015chseang@gmail.com>
2023-11-07 15:48:07 +0900

    feat: 게임 실행 로직

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,7 @@ import lotto.UI.LottoUI;
 
 public class Application {
     public static void main(String[] args) {
-        LottoUI lottoUI = new LottoUI();
-        lottoUI.start();
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
     }
 }

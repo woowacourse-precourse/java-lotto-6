022698f7d25b2ac24557456a66282d777cd329a6
황준혁 <hyug0819@gmail.com>
2023-11-08 01:47:50 +0900

    refactor : LottoView 접근 제어자 및 로직 변경

--- src/main/java/lotto/lotto/view/LottoView.java
+++ src/main/java/lotto/lotto/view/LottoView.java
@@ -2,18 +2,21 @@ package lotto.lotto.view;
 
 import java.util.List;
 
+import lotto.lotto.model.Lotto;
+
 public class LottoView {
-    public static void printUserCount(int count){
+    private static void printUserCount(int count){
         System.out.println(count+"개를 구매했습니다.");
     }
 
-    public static void printUserLotto(List<List<Integer>> lottos){
-        for(List<Integer> lotto : lottos){
+    public static void printUserLotto(List<Lotto> lottos){
+        printUserCount(lottos.size());
+        for(Lotto lotto : lottos){
             printLotto(lotto);
         }
     }
 
-    public static void printLotto(List<Integer> lotto){
+    private static void printLotto(Lotto lotto){
         System.out.println(lotto.toString());
     }
 }

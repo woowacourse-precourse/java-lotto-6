8a36030c024144a85c528857cc0df036e74469d5
MyStraw <kikongdosa@gmail.com>
2023-11-08 07:03:45 +0900

    Feat: 추가 로또 당첨번호와 일치 판별
    랜덤으로 뽑힌 로또번호와 입력받은 당첨번호와 몇개가 일치하는지 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -15,8 +17,53 @@ public class Application {
 
         List<Lotto> lottoPapers = generateLottoPaper(howManyLotto);
 
+        System.out.println("\n" + howManyLotto + "개를 구매했습니다.");
         for (Lotto lotto : lottoPapers) {
             System.out.println(lotto.getNumbers());
+        }
+
+        System.out.println("\n" + "당첨 번호를 입력해 주세요.");
+        String[] winNumber_String = Console.readLine().trim().split(",");
+        List<Integer> winNumbers = new ArrayList<>();
+        for (String numberStr : winNumber_String){
+            winNumbers.add(Integer.parseInt(numberStr.trim()));
+        }
+
+        System.out.println("\n" + "보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine().trim());
+
+
+        for (Lotto lotto : lottoPapers){
+            int matchingCount = 0;
+            boolean hasBonusNumber = false;
+            for (Integer number : winNumbers){
+                if (lotto.getNumbers().contains(number)) {
+                    matchingCount++;
+                }
+            }
+
+            if (lotto.getNumbers().contains(bonusNumber)) {
+                hasBonusNumber = true;
+            }
+
+
+            if (matchingCount == 3){
+                System.out.println("3개 일치 (5,000원) - " +matchingCount +"개");
+            }
+            if (matchingCount == 4){
+                System.out.println("4개 일치 (50,000원) - " +matchingCount +"개");
+            }
+            if (matchingCount == 5){
+                System.out.println("5개 일치 (1,500,000원) - " +matchingCount +"개");
+            }
+
+            if (matchingCount == 5 && hasBonusNumber){
+                System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " +matchingCount +"개");
+            }
+
+            if (matchingCount == 6){
+                System.out.println("6개 일치 (2,000,000,000원) - " +matchingCount +"개");
+            }
         }
     }
 

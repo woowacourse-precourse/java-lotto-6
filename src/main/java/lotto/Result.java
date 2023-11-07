35f150f7da52c1e43df54b9fb194c1c56d9c5919
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-07 22:21:40 +0900

    Feat : 사용자의 총 수익률을 계산한다
    - getTotalPrize( )에서 총 당첨금을 계산
    - getTotalPrice( )에서 총 구입금을 계산

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -3,4 +3,19 @@ package lotto;
 import java.util.Map;
 
 public record Result(Map<Rank, Integer> result) {
+    public String calculateTotalRate() {
+        return String.format("%d / %d * 100", getTotalPrize(), getTotalPrice());
+    }
+
+    private Integer getTotalPrize() {
+        return result.entrySet().stream()
+                .map(entry -> entry.getKey().getPrize() * entry.getValue())
+                .reduce(0, Integer::sum);
+    }
+
+    private Integer getTotalPrice() {
+        return result.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum() * 1000;
+    }
 }

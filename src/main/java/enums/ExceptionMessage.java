06a66aa92d04030e3ca187776b4a433ae6ba169c
daneng4 <daneng4@naver.com>
2023-11-07 23:23:04 +0900

    feat: 에러 메시지 추가

--- src/main/java/enums/ExceptionMessage.java
+++ src/main/java/enums/ExceptionMessage.java
@@ -1,7 +1,8 @@
 package enums;
 
 public enum ExceptionMessage {
-    ILLIGAL_ARGUMENT_ERROR_MESSAGE("[ERROR] 잘못된 로또 금액 입니다.");
+    ILLIGAL_COST_ERROR_MESSAGE("[ERROR] 잘못된 로또 금액 입니다."),
+    ILLIGAL_WINNING_NUM_MESSAGE("[ERROR] 잘못된 당첨 번호 입니다.");
 
     private final String exceptMessage;
     ExceptionMessage(String message) {

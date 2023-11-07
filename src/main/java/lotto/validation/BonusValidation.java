e303faf2728a5b017d31c7c3db20b0b1095f0885
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:46:15 +0900

    feat(BonusValidation) : 검증 기능 구현

--- src/main/java/lotto/validation/BonusValidation.java
+++ src/main/java/lotto/validation/BonusValidation.java
@@ -1,9 +1,19 @@
 package lotto.validation;
 
+import lotto.system.Constants;
+import lotto.system.ErrorMessage;
+
 public class BonusValidation implements Validation {
 
     @Override
     public void check(String input) {
+        checkNumberOutOfRange(input);
+    }
 
+    private void checkNumberOutOfRange(String input) {
+        if (Integer.parseInt(input) < Constants.START_NUMBER.getConstants()
+                || Integer.parseInt(input) > Constants.END_NUMBER.getConstants()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE);
+        }
     }
 }

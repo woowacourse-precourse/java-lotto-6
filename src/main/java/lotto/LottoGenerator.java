da7a739f5c67453e2aba24167cfde06dde782417
keongmiiin99 <smartmina7@naver.com>
2023-11-08 02:53:54 +0900

    feat(LottoGenerator): 로또 번호 랜덤생성하는 클래스

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public static List<Integer> generatgeLottoNumber() {
+        List<Integer> lottoNumber = new ArrayList<>();
+        while (lottoNumber.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!lottoNumber.contains(randomNumber)) {
+                lottoNumber.add(randomNumber);
+            }
+        }
+        return lottoNumber;
+    }
+}

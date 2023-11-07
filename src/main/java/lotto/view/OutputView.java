696a3d7c055e3fa0699c3334290644b3731fbb85
ohjuyoungg <ejrm12@naver.com>
2023-11-08 00:38:57 +0900

    feat: 당첨 통계 출력 관련 메소드 1차 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,9 @@ package lotto.view;
 
 import java.util.List;
 import lotto.service.LottoService;
+import lotto.service.LottoWinningCalculator;
 import lotto.utils.constants.Comment;
+import lotto.utils.constants.LottoWinning;
 
 public class OutputView {
 
@@ -29,9 +31,13 @@ public class OutputView {
         System.out.println(Comment.INPUT_BONUS_NUMBER.getComment());
     }
 
-    public static void printLottoWinningStatistics() {
+    public static void printLottoWinningStatistics(List<Integer> lottoWinningCounts) {
         System.out.println();
-        System.out.println(Comment.INPUT_WINNING_STATISTICS.getComment());
         System.out.println(Comment.INPUT_THREE_DASH.getComment());
+        System.out.println(Comment.INPUT_WINNING_STATISTICS.getComment());
+    }
+
+    public static void printLottoTotalProfit(List<Integer> lottoWinningCounts, int purchaseAmount) {
+        LottoWinningCalculator.printTotalProfit(lottoWinningCounts, purchaseAmount);
     }
 }

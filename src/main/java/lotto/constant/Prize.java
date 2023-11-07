62361f603fcb34bb96e9b20394fbd7c375451b56
현 <sohyun95277@gmail.com>
2023-11-07 19:52:54 +0900

    fix(Prize): "," 제거

--- src/main/java/lotto/constant/Prize.java
+++ src/main/java/lotto/constant/Prize.java
@@ -1,11 +1,11 @@
 package lotto.constant;
 
 public enum Prize {
-    FIRST(6, "(2,000,000,000원)", 2000000000),
-    SECOND(7, "(30,000,000원)", 30000000),
-    THIRD(5, "(1,500,000원)", 1500000),
-    FOURTH(4, "(50,000원)", 50000),
     FIFTH(3, "(5,000원)", 5000),
+    FOURTH(4, "(50,000원)", 50000),
+    THIRD(5, "(1,500,000원)", 1500000),
+    SECOND(7, "(30,000,000원)", 30000000),
+    FIRST(6, "(2,000,000,000원)", 2000000000),
     NONE(0, "0", 0);
 
     private final int match;
@@ -35,6 +35,6 @@ public enum Prize {
         if (prize == SECOND) {
             return "5개 일치, 보너스 볼 일치 " + prize.panel + " - ";
         }
-        return prize.match + "개 일치, " + prize.panel + " - ";
+        return prize.match + "개 일치 " + prize.panel + " - ";
     }
 }

fc20295cbcfd681cdfc7471a2560318c57484440
gmelon <hsh1769@naver.com>
2023-11-07 23:12:06 +0900

    refactor: LottoPurchaseAmount의 최솟값 검증 지점을 생성자로 변경

--- src/main/java/lotto/domain/LottoPurchaseAmount.java
+++ src/main/java/lotto/domain/LottoPurchaseAmount.java
@@ -4,27 +4,27 @@ public class LottoPurchaseAmount {
     private final int value;
 
     LottoPurchaseAmount(int value) {
+        validateMinimumAmount(value);
+
         this.value = value;
+    }
+
+    private void validateMinimumAmount(int amount) {
+        if (amount == 0) {
+            throw new IllegalArgumentException("[ERROR] 최소 1개 이상의 로또를 구매해야 합니다.");
+        }
     }
 
     public static LottoPurchaseAmount from(int price) {
         validateDivisible(price);
 
         int amount = price / Lotto.PRICE;
-        validateMinimumAmount(amount);
-
         return new LottoPurchaseAmount(amount);
     }
 
     private static void validateDivisible(int price) {
         if (price % Lotto.PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구매는 1,000원 단위로만 가능합니다.");
-        }
-    }
-
-    private static void validateMinimumAmount(int amount) {
-        if (amount == 0) {
-            throw new IllegalArgumentException("[ERROR] 최소 1개 이상의 로또를 구매해야 합니다.");
         }
     }
 

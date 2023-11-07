3ff86725dd2b1c91bf61217e7f0e9089227b7937
HyunmoBae <tsi0520@naver.com>
2023-11-08 05:59:53 +0900

    feat(lotto) : modify skeleton code

--- src/main/java/lotto/ui/LottoResultDisplay.java
+++ src/main/java/lotto/ui/LottoResultDisplay.java
@@ -2,15 +2,4 @@ package lotto.ui;
 
 public class LottoResultDisplay {
 
-    public int compareLottoTicket() {
-        return 0;
-    }
-
-    public int lottoResult() {
-        return 0;
-    }
-
-    public float rateOfReturn() {
-        return 0;
-    }
 }

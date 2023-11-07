8c99b2d6902ee7f66a634b5c73e7294674ff581c
doha <dohaahi@naver.com>
2023-11-07 13:07:24 +0900

    refactor (WinningLotto): 검증 로직 이동 WinningLottoDto -> WinningLotto

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,8 +4,14 @@ import java.util.List;
 import lotto.validator.WinningLottoValidator;
 
 public class WinningLotto {
+    private final List<Integer> numbers;
 
     public WinningLotto(List<Integer> numbers) {
         WinningLottoValidator.validateWinningNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }

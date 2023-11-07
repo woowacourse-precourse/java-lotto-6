67f0cfff33a1c17c2bd6f79c96c4508bbe78580a
Sim-GyuBin <skb0606@gmail.com>
2023-11-07 13:53:12 +0900

    refactor: money 멤버 변수에 할당

--- src/main/java/lotto/interactor/Buyer.java
+++ src/main/java/lotto/interactor/Buyer.java
@@ -5,13 +5,14 @@ import lotto.interactor.validator.InputValidator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Buyer {
-    private static int money;
+    private int money;
 
     public int pay() {
         System.out.printf("구입금액을 입력해 주세요.");
         String moneyString = readLine();
         checkMoneyValid(moneyString);
-        return Integer.parseInt(moneyString);
+        this.money = Integer.parseInt(moneyString);
+        return this.money;
     }
 
     private void checkMoneyValid(String moneyString) {

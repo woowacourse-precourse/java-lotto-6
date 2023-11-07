31f597fec0cc64272914b25189367ed3b77c798f
doublekm153 <doublekm153@gmail.com>
2023-11-08 07:04:24 +0900

    Application.java 파일에서 실행 결과가 해시코드 -> 로또 번호 출력으로 변환.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,17 +4,20 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
-
+    
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
+    
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

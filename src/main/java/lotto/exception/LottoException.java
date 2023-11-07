ec1873c46fc31c8076c94681d460908879613e82
jiu6525 <jiu6525@daum.net>
2023-11-07 19:10:00 +0900

    feat : 보너스 숫자에 대한 중복확인 기능 추가

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -12,6 +12,8 @@ public class LottoException {
     private static final String DIGIT_ERROR_MESSAGE = "는 숫자가 아닙니다. 숫자를 입력해 주세요.";
     private static final String OUT_OF_RANGE_ERROR_MESSAGE = "로또 숫자 범위(1~45)를 벗어났습니다.";
     private static final String LENGTH_ERROR_MESSAGE = "입력이 잘못 되었습니다. 중복하지 않는 1~45까지의 숫자 6개를 입력해 주세요.";
+    private static final String BONUS_ERROR_MESSAGE = "당첨 숫자와 중복됩니다. 다시 입력해 주세요.";
+
 
     public boolean purchaseAmountCheckSub(String purchaseAmount) {
         int amount = Integer.parseInt(purchaseAmount);
@@ -68,6 +70,22 @@ public class LottoException {
     private boolean numberLengthCheckSub(List<Integer> winningNumbers) {
         if (winningNumbers.size() != lottoConfig.LOTTO_SIZE) {
             throw new IllegalArgumentException(ERROR_PREFIX + LENGTH_ERROR_MESSAGE);
+        }
+        return true;
+    }
+
+    public boolean duplicateBonusNumberCheckMain(List<Integer> winningNumber, int bonusNumber) {
+        try {
+            return duplicateBonusNumberCheckSub(winningNumber, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private boolean duplicateBonusNumberCheckSub(List<Integer> winningNumber, int bonusNumber) {
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + BONUS_ERROR_MESSAGE);
         }
         return true;
     }

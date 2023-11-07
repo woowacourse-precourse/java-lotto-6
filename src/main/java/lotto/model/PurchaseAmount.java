a5d6415c90e4886739783774cfc32d607b0614e3
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-08 02:28:40 +0900

    feat : create PurchaseAmount

---
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -0,0 +1,35 @@
+package lotto.model;
+
+import static lotto.configurations.ErrorMessages.*;
+import static lotto.configurations.SettingValues.*;
+
+public class PurchaseAmount {
+    Long paidMoney;
+
+    public PurchaseAmount(Long paidMoney) {
+        validate(paidMoney);
+        this.paidMoney = paidMoney;
+    }
+
+    public Integer getNumberOfTicket() {
+        return Math.toIntExact(paidMoney / PRICE_OF_LOTTO.get());
+    }
+
+    private void validate(Long paidMoney) {
+        validatePurchaseUnit(paidMoney);
+        validateIntegerRange(paidMoney);
+    }
+
+    private void validatePurchaseUnit(Long paidMoney) {
+        if (paidMoney.intValue() % PRICE_OF_LOTTO.get() != 0) {
+            throw new IllegalArgumentException(ERROR_HEADER.get() + INVALID_UNIT_MESSAGE.get());
+        }
+    }
+
+    private void validateIntegerRange(Long paidMoney) {
+        long numberOfTicket = paidMoney / PRICE_OF_LOTTO.get();
+        if (numberOfTicket > (long) Integer.MAX_VALUE) {
+            throw new IllegalArgumentException(ERROR_HEADER.get() + OUT_OF_RANGE_PURCHASE_MESSAGE.get());
+        }
+    }
+}

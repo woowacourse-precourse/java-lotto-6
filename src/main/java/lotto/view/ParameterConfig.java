9e41d4f05a13f02008c5b312705894891f288adc
howudong <tjdvy963@naver.com>
2023-11-07 21:52:02 +0900

    refactor : 새로운 config 추가

--- src/main/java/lotto/view/ParameterConfig.java
+++ src/main/java/lotto/view/ParameterConfig.java
@@ -7,5 +7,6 @@ public class ParameterConfig {
     public static final String BUY_PRICE = "buyPrice";
     public static final String WINNING_LOTTO_NUMBERS = "winningLottoNumbers";
     public static final String BONUS_NUMBER = "bonusNumber";
+    public static final String WINNING_LOTTO = "winningLotto";
     public static final String LOTTO_RESULT = "lottoResult";
 }

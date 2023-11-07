baea1b0ae47891265f63356f7f83913bcdfcc773
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-07 15:36:58 +0900

    refactor : 오타 수정

--- src/main/java/lotto/utils/constants/OutputConstants.java
+++ src/main/java/lotto/utils/constants/OutputConstants.java
@@ -10,7 +10,7 @@ public class OutputConstants {
     public static final String THIRD_PRIZE = "3개 일치 (5,000원) - ";
     public static final String FOURTH_PRIZE = "4개 일치 (50,000원) - ";
     public static final String SECOND_PRIZE = "5개 일치 (1,500,000원) - ";
-    public static final String SECOND_PRIZE_BONUS_NOT_CORRECT = "5개 일치, 보너스 일치 (30,000,000원) - ";
+    public static final String SECOND_PRIZE_BONUS_NOT_CORRECT = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
     public static final String FIRST_PRIZE_MONEY = "6개 일치 (2,000,000,000원) - ";
     public static final String EARN_RATE_MESSAGE = "총 수익률은 ";
     public static final String COUNT_UNIT = "개";

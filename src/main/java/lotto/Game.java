4971b888e08c1dfefdb10eae96065ada8c06d175
Jaewon <choijawon99@gmail.com>
2023-11-07 19:13:42 +0900

    feat(game): 투입금액 입력시 오류 예외처리

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import lotto.view.View;
+import lotto.util.Validator;
 
 
 public class Game {
@@ -10,22 +11,34 @@ public class Game {
 
     private int money;
     private View view;
+    private Validator validator;
 
     public Game(){
         view = new View();
+        validator = new Validator();
     }
 
     public void play(){
-        getMoneyFromUserInput();
+        getMoney();
     }
 
-    private void getMoneyFromUserInput(){
-        money = inputMoney();
+    private void getMoney(){
+        money = Integer.parseInt(inputMoney());
     }
 
-    private int inputMoney(){
+    private String inputMoney(){
         view.request_InputMoney();
-        return Integer.parseInt(Console.readLine());
+        return check_ValidationInputMoney(Console.readLine());
+    }
+
+    private String check_ValidationInputMoney(String input){
+        try{
+            validator.check_InputMoney(input);
+            return input;
+        }catch(IllegalArgumentException e){
+            view.print_Exception(e.getMessage());
+            return inputMoney();
+        }
     }
 
 }

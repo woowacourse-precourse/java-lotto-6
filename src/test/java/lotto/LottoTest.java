ee990120dcfdc060599bf486b42e4964fe2dc423
kymin <park48664866@gmail.com>
2023-11-07 14:45:11 +0900

    refactor: 테스트코드 기능별 클래스 분리
    테스트코드 기능별 클래스 분리:
    - BonusNumValidator에 대한 테스트코드 분리
    - PaymentValidator에 대한 테스트코드 분리
    - WinningNumValidator에 대한 테스트코드 분리

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.model.Lotto;
+import lotto.model.WinningNum;
 import lotto.validator.BonusNumValidator;
 import lotto.validator.PaymentValidator;
 import lotto.validator.WinningNumValidator;
@@ -24,66 +25,6 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스 번호가 정수가 아니면 예외가 발생한다.")
-    @Test
-    void createBonusNumberByCharacter() {
-        assertThatThrownBy(() -> BonusNumValidator.validate("보너스 번호"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("보너스 번호가 1에서 45사이의 수가 아니면 예외가 발생한다.")
-    @Test
-    void createBonusNumberOverRange() {
-        assertThatThrownBy(() -> BonusNumValidator.validate("51"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("부족한 가격이 입력되면 예외가 발생한다.")
-    @Test
-    void buyLottoAtNotEnoughPrice() {
-        assertThatThrownBy(() -> PaymentValidator.validate("500"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("거스름돈이 발생하면 예외가 발생한다.")
-    @Test
-    void existChange() {
-        assertThatThrownBy(() -> PaymentValidator.validate("2500"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("당첨 번호에 정수가 아닌 값이 있으면 예외가 발생한다.")
-    @Test
-    void createWinningNumberByCharacter() {
-        final String[] winningNum = { "1", "2", "3", "a", "5", "6" };
-        assertThatThrownBy(() -> WinningNumValidator.validate(winningNum))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createWinningNumberByOverSize() {
-        final String[] winningNum = { "1", "2", "3", "4", "5", "6", "7" };
-        assertThatThrownBy(() -> WinningNumValidator.validate(winningNum))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createWinningNumberByDuplicatedNumber() {
-        final String[] winningNum = { "1", "2", "3", "4", "5", "5" };
-        assertThatThrownBy(() -> WinningNumValidator.validate(winningNum))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("당첨 번호가 1에서 45사이의 수가 아니면 예외가 발생한다.")
-    @Test
-    void reateWinningNumberByDuplicatedNumber() {
-        final String[] winningNum = { "1", "2", "3", "4", "5", "52" };
-        assertThatThrownBy(() -> WinningNumValidator.validate(winningNum))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

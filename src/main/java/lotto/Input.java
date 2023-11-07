750ee8b66af72c5d71e56a73eca4b14cba6c1403
dmdwns2 <ssreki1@naver.com>
2023-11-07 21:48:06 +0900

    refactor: modify input & validate purchase amount

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,20 +2,23 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Input {
-    public int inputPurchaseAmount(){
-        String purchaseAmount = Console.readLine();
-        validatePurchaseAmount(purchaseAmount);
-        return Integer.parseInt(purchaseAmount);
-    }
+import java.util.NoSuchElementException;
 
-    private void validatePurchaseAmount(String purchaseAmount){
-        int temp;
-        if(purchaseAmount.isBlank()){
+public class Input {
+    public int inputPurchaseAmount() {
+        try {
+            String purchaseAmount = Console.readLine();
+            validatePurchaseAmount(purchaseAmount);
+            return Integer.parseInt(purchaseAmount);
+        } catch (NoSuchElementException e) {
             throw new IllegalArgumentException();
         }
-        try{
-           temp = Integer.parseInt(purchaseAmount);
+    }
+
+    private void validatePurchaseAmount(String purchaseAmount) {
+        int temp;
+        try {
+            temp = Integer.parseInt(purchaseAmount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }

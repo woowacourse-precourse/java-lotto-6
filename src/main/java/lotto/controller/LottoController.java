e6fd1c1f6c930114d88bd37b23047cf5efb85080
hyuna <monicajo0@naver.com>
2023-11-07 08:40:06 -0800

    refactor(LottoController): record로 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,14 +9,7 @@ import lotto.model.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-public class LottoController {
-    public final InputView inputView;
-    public final OutputView outputView;
-
-    public LottoController(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
+public record LottoController(InputView inputView, OutputView outputView) {
 
     public void start() {
         int purchasePrice = inputView.getPurchasePrice();

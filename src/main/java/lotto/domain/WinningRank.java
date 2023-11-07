91a375e8df77d1031ea8dadf280ba44ada3e9513
noyes5 <noyes5@naver.com>
2023-11-07 17:19:41 +0900

    refactor: WinnerRank 클래스명 WinningRank 리팩토링
    - 당첨이 되지 않은 NONE에 대한 값 추가

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -2,31 +2,32 @@ package lotto.domain;
 
 import java.util.Arrays;
 
-public enum WinnerRank {
+public enum WinningRank {
     FIRST(6, false, 2_000_000_000, "6개 일치"),
     SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
     THIRD(5, false, 1_500_000, "5개 일치"),
     FOURTH(4, false, 50_000, "4개 일치"),
-    FIFTH(3, false, 5_000, "3개 일치");
+    FIFTH(3, false, 5_000, "3개 일치"),
+    NONE(1, false, 0, "");
 
     private int match;
     private boolean hasBonusNumber;
     private int prize;
     private String display;
 
-    WinnerRank(int match, boolean hasBonusNumber, int prize, String display) {
+    WinningRank(int match, boolean hasBonusNumber, int prize, String display) {
         this.match = match;
         this.hasBonusNumber = hasBonusNumber;
         this.prize = prize;
         this.display = display;
     }
 
-    public static WinnerRank getRank(int match, boolean hasBonusNumber) {
-        return Arrays.stream(WinnerRank.values())
+    public static WinningRank getRank(int match, boolean hasBonusNumber) {
+        return Arrays.stream(WinningRank.values())
                 .filter(rank -> rank.match == match)
                 .filter(rank -> rank.hasBonusNumber == hasBonusNumber)
                 .findAny()
-                .orElse(null);
+                .orElse(NONE);
     }
 
     public int getPrize() {

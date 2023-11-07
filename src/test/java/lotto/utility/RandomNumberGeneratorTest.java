8710bd065579dce8083c5d1ede746b7e5d2e2d5b
Sung Hyobin <hellmir610@gmail.com>
2023-11-08 05:35:56 +0900

    test: Add test for function that generate random numbers with size of random number count configuration

---
+++ src/test/java/lotto/utility/RandomNumberGeneratorTest.java
@@ -0,0 +1,21 @@
+package lotto.utility;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.configuration.RandomNumberConfig.COUNT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomNumberGeneratorTest {
+    @DisplayName("설정된 개수만큼의 임의의 숫자를 생성한다.")
+    @Test
+    void test() {
+        // given, when
+        List<Integer> randomNumbers = RandomNumberGenerator.generateNotDuplicatedRandomNumbers();
+
+        // then
+        assertThat(randomNumbers).hasSize(COUNT);
+    }
+}

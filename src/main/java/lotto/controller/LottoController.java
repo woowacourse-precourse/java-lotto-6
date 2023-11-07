1c9d4f92e8f3ed3b10cae70e71cc9a39dc3786d5
Yeriimii <seojs0511@naver.com>
2023-11-08 03:42:43 +0900

    feat(LottoController): 구입 금액 입력 후 금액에 맞는 수량만큼 구입할 수 있다

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import lotto.model.LottoPaper;
+import lotto.model.LottoPurchaseManager;
+import lotto.model.dto.PurchaseResult;
+import lotto.system.util.LottoNumberGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoPurchaseManager lottoPurchaseManager = new LottoPurchaseManager(new LottoNumberGenerator());
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public PurchaseResult purchaseProcess() {
+        // 구입 금액 입력 받기
+        int purchaseAmount = inputView.inputPurchaseAmount();
+
+        // 구입 금액 계산
+        int purchaseQuantity = lottoPurchaseManager.calculatePurchaseQuantity(purchaseAmount);
+
+        // 로또 구입
+        List<LottoPaper> purchasedLottoPapers = lottoPurchaseManager.purchaseLottoPapers(purchaseQuantity);
+
+        // 구입한 용지 출력
+        outputView.printPurchaseResult(purchaseQuantity);
+        outputView.printPurchasedLottoPaper(purchasedLottoPapers);
+
+        return new PurchaseResult(purchaseAmount, purchasedLottoPapers);
+    }
+}

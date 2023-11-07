804e3ee2a4b4d10cc6333f691ff7d9f8eb6c8979
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 03:00:07 +0900

    [ADD] 로또 개수, 구매 로또 출력

--- src/main/java/lotto/View/Lottogameview.java
+++ src/main/java/lotto/View/Lottogameview.java
@@ -1,2 +1,17 @@
-package lotto.View;public class Lottogameview {
+package lotto.View;
+
+public class Lottogameview {
+  public void printPurchasedLottosCount(int numberOfLotto) {
+        System.out.println(numberOfLotto + "개를 구매했습니다.");
+    }
+
+    public void printPurchasedLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            printLottoNumbers(lotto.getNumbers());
+        }
+    }
+
+    private void printLottoNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
+    }
 }

2958c8ae776ddc0ba3052d1cecea1e38306bfb46
limzzum <chfhddl1234@naver.com>
2023-11-08 00:29:28 +0900

    :sparkles: feat : 로또 머신 당첨 복권 검증 메서드 추가

--- src/main/java/lotto/validation/LottoMachineValidation.java
+++ src/main/java/lotto/validation/LottoMachineValidation.java
@@ -5,21 +5,39 @@ import static lotto.constant.ErrorMessage.INVALID_NUMBER;
 import static lotto.constant.ErrorMessage.INVALID_PURCHASE_AMOUNT;
 import static lotto.constant.Message.LOTTO_PRICE;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoMachineValidation {
 
-    public void validate_inputMoney(String inputMoney){
+    public static int validate_inputMoney(String inputMoney){
         validate_isInputValue(inputMoney);
         validate_isPositiveNum(inputMoney);
         validate_isExactlyDividedByPrice(Integer.parseInt(inputMoney));
+        return Integer.parseInt(inputMoney);
     }
 
-    private void validate_isInputValue(String input){
+    public static void validate_winningLotto(String winningNums, String bonusNum){
+        validate_isWinningNums(winningNums);
+        validate_isInputValue(bonusNum);
+        validate_isPositiveNum(bonusNum);
+    }
+
+    private static void validate_isWinningNums(String input){
+        validate_isInputValue(input);
+        List<String> strings = Arrays.stream(input.split(",")).toList();
+        for (String string : strings) {
+            validate_isPositiveNum(string);
+        }
+    }
+
+    private static void validate_isInputValue(String input){
         if(input == null || input.isBlank()){
             throw new IllegalArgumentException(INVALID_IS_NOT_INPUT);
         }
     }
 
-    private void validate_isPositiveNum(String input){
+    private static void validate_isPositiveNum(String input){
         int num;
         try {
             num = Integer.parseInt(input);
@@ -32,7 +50,7 @@ public class LottoMachineValidation {
         }
     }
 
-    private void validate_isExactlyDividedByPrice(int input){
+    private static void validate_isExactlyDividedByPrice(int input){
         if(input % LOTTO_PRICE != 0){
             throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT);
         }

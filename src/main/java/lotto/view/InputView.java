0fc642f161f8a319cd48922dead558e051d85f52
watermelon3737 <pshrina1@naver.com>
2023-11-07 13:08:21 +0900

    feat(InputView) : 로또 게임 사용자의 입력이 없을 경우, 숫자가 아닐 경우 예외 처리

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,39 @@
+package lotto.view;
+
+public class InputView {
+    public long validateCustomerMoneyInput(final String moneyInput) {
+        if (moneyInput.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
+        }
+
+        moneyInput.chars().forEach(o -> {
+            if (!Character.isDigit(o)) {
+                throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+            }
+        });
+
+        return Long.parseLong(moneyInput);
+    }
+
+    public String[] validateWinningNumberInput(final String winningNumberInput) {
+        if (winningNumberInput.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
+        }
+
+        String[] splitByDelimiter = winningNumberInput.split(",");
+
+        return splitByDelimiter;
+    }
+
+    public int validateBonusNumberInput(final String bonusNumberInput) {
+        if (bonusNumberInput.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
+        }
+        if (bonusNumberInput.chars().anyMatch(c -> !Character.isDigit((char) c))) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+
+        int bonusNumber = Integer.parseInt(bonusNumberInput);
+        return bonusNumber;
+    }
+}

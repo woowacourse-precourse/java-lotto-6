d21ec52d448312c24ea0c74058c107001ab649a1
Sunhee Kim <sunni372@icloud.com>
2023-11-07 23:54:35 +0900

    test: 로또(Lotto) 테스트 구현
    기존 로또 테스트를 패키지 변경하고, 추가 기능들을 테스트했습니다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,9 +1,9 @@
-package lotto;
+package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +18,19 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호를 오름차순으로 출력한다.")
+    @Test
+    void printNaturalOrder() {
+    }
+
+    @DisplayName("로또 번호를 가져온다.")
+    @Test
+    void getNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 7));
+    }
 }

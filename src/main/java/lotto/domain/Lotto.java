d4afbdc528b89de6c5b573193a7143c97b07900f
leejuno <zhqkfxm0127@naver.com>
2023-11-08 00:56:03 +0900

    refactor: Lotto클래스의 메소드 일부를 LottoMachine클래스로 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,9 @@
 package lotto.domain;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 import java.util.List;
 
 public class Lotto {
-    private static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,12 +15,5 @@ public class Lotto {
         if (numbers.size()!=6) {
             throw new IllegalArgumentException();
         }
-    }
-    
-    public int readAmount() {
-        System.out.println(PURCHASE_AMOUNT_GUIDE);
-        String purchaseAmountText = readLine();
-        int purchaseAmount = Integer.parseInt(purchaseAmountText);
-        return purchaseAmount;
     }
 }

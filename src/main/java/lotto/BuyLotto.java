e31485c612e09881c863615e5da059284f9490c3
K-KY <kyuyoungk@naver.com>
2023-11-08 02:21:56 +0900

    Feat(BuyLotto) : 로또를 구매후 `LottoNumbers` 를 리턴

---
+++ src/main/java/lotto/BuyLotto.java
@@ -0,0 +1,22 @@
+package lotto;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.GenerateLottoNumber;
+
+public class BuyLotto {
+    private static List<List<Integer>> lottoNumbers;
+
+    public BuyLotto() {
+        lottoNumbers = new ArrayList<>();
+    }
+
+    public LottoNumbers lotto(int purchaseCount) {
+        for (int i = 0; i < purchaseCount; i++) {
+            List<Integer> lottoNumber =  GenerateLottoNumber.generate();
+            lottoNumbers.add(lottoNumber);
+        }
+        return new LottoNumbers(lottoNumbers);
+    }
+}

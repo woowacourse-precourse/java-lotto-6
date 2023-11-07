d8afff8255a9573e016daa711124da7a0b54dafa
jongmee <ra123221@gmail.com>
2023-11-07 14:58:19 +0900

    feat(Profit): 수익률 도메인에 대한 클래스 생성하기
    - WHY: 미션의 수익률 산출 방법에 따라 계산되도록 내부에 메서드를 정의하여 계산하였다.

---
+++ src/main/java/lotto/domain/Profit.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+import static lotto.constants.Condition.MONEY_UNIT;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import lotto.utility.vo.ProfitResponse;
+
+public class Profit {
+    private static final int SCALE = 2;
+
+    private final Double rate;
+
+    public Profit(List<Rank> ranks) {
+        this.rate = calculateProfitRate(ranks);
+    }
+
+    private Double calculateProfitRate(List<Rank> ranks) {
+        long prize = calculatePrize(ranks);
+        long paidAmount = calculatePaidAmount(ranks);
+        double ratio = (double) prize / paidAmount;
+        return toPercentage(ratio);
+    }
+
+    private long calculatePrize(List<Rank> ranks) {
+        return ranks.stream()
+            .mapToLong(rank -> rank.getPrice())
+            .sum();
+    }
+
+    private long calculatePaidAmount(List<Rank> ranks) {
+        return ranks.size() * MONEY_UNIT;
+    }
+
+    private double toPercentage(double ratio) {
+        BigDecimal bd = new BigDecimal(ratio * 100);
+        bd = bd.setScale(SCALE, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    public ProfitResponse convertResponse() {
+        return new ProfitResponse(rate);
+    }
+}

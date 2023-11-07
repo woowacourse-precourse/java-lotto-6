d856567ef80e33abe862c3dcb0211970072bc520
honee8583 <honee8583@naver.com>
2023-11-07 20:02:34 +0900

    feat(input): add function checking is input has non_numeric characters

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -3,6 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoService {
+    private static final int LIMIT_NUMBER = 45;
+    private static final int PURCHASE_STANDARD = 1000;
+
     public int inputPurchaseAmount() {
         while (true) {
             String input = Console.readLine();
@@ -15,4 +18,11 @@ public class LottoService {
             }
         }
     }
+
+    private void containsNonNumericCharacters(String input) {
+        if (!input.matches("[0-9]+")) {
+            throw new IllegalArgumentException(ErrorMessage.CONTAINS_NON_NUMERIC_CHARACTERS.getMessage());
+        }
+    }
+
 }

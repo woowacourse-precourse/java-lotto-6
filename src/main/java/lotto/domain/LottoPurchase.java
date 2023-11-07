d18cdca401953767fd08828c8bff4bac98f64889
sw801733 <sw801733@naver.com>
2023-11-07 21:26:50 +0900

    feat : LottoPurchase 클래스 생성

---
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class LottoPurchase {
+    private final int LottoPurchaseCnt;
+
+    public LottoPurchase(int LottoPurchaseCnt){
+        this.LottoPurchaseCnt = LottoPurchaseCnt;
+    }
+
+}

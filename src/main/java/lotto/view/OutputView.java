f885ea14dfca50005d80bff42e883a966cc74e83
ch1hyun <och2510@gmail.com>
2023-11-07 19:02:51 +0900

    feat: Add output view for handling result outputs

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,40 @@
+package lotto.view;
+
+import lotto.dto.MoneyDTO;
+import lotto.dto.WinningStatisticDTO;
+import lotto.enums.OutputMessage;
+import lotto.model.Lotto;
+
+public class OutputView {
+    public static void printLottoCountBought(Integer lottoCount) {
+        System.out.println(String.format(
+                OutputMessage.NUMBER_OF_PURCHASED_LOTTO.getMessage(),
+                lottoCount
+        ));
+    }
+
+    public static void printLottoNumbers(Lotto lotto) {
+        System.out.println(String.format(
+                OutputMessage.LOTTO_NUMBERS.getMessage(),
+                lotto
+        ));
+    }
+
+    public static void printResultOfLottery(WinningStatisticDTO winningStatisticDTO) {
+        System.out.println(String.format(
+                OutputMessage.RESULT_OF_LOTTERY.getMessage(),
+                winningStatisticDTO.getMatched3(),
+                winningStatisticDTO.getMatched4(),
+                winningStatisticDTO.getMatched5(),
+                winningStatisticDTO.getMatched5AndBonusNumber(),
+                winningStatisticDTO.getMatched6()
+        ));
+    }
+
+    public static void printReturnRate(MoneyDTO moneyDTO) {
+        System.out.println(String.format(
+                OutputMessage.RETURN_RATE.getMessage(),
+                moneyDTO.getReturnRate()
+        ));
+    }
+}

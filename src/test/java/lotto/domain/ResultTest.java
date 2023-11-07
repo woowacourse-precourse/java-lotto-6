4d09827f9aa1ecd0b7757b8589464347832be577
yubin <dbqls2821@gmail.com>
2023-11-07 14:28:58 +0900

    test: 로또 다섯개 일치인 경우 그에 맞는 객체를 반환하는 테스트코드 추가

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -10,4 +10,9 @@ class ResultTest {
     void 여섯개_일치인_경우_그에_맞는_객체를_반환한다() {
         assertEquals(Result.SIX_MATCH, Result.of(6, false));
     }
+
+    @Test
+    void 다섯개_일치인_경우_그에_맞는_객체를_반환한다() {
+        assertEquals(Result.FIVE_MATCH, Result.of(5, false));
+    }
 }

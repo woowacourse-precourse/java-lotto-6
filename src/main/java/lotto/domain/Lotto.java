555d7e8a709dab4eb254b08e31a1a6b8725dfad8
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-07 18:24:44 +0900

    feat(Lotto) : getNumbers 메서드 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -19,6 +19,9 @@ public class Lotto {
     public void setBonusNumber(int bonusNumber) {
         lottoException.validateBonusNumber(numbers, bonusNumber);
     }
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
 
 }
 

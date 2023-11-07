491cb6a68c244e5fe2f701949719e26ae5f3d8e6
ztng123 <dpwl991102@gmail.com>
2023-11-08 02:41:02 +0900

    로또 번호 생성
    오름차순 정렬

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +16,7 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
     }
 
     // TODO: 추가 기능 구현

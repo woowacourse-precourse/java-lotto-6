e7afc0e4aaa25d33dd69450b50d10cdede154273
NyaongKAI <yukihusiru18@gmail.com>
2023-11-08 07:50:02 +0900

    클래스 틀 만들기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,21 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {

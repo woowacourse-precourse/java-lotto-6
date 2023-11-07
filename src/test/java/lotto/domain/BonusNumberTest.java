4d0f2e74f1bd149869255974d2a095e6864060da
qncj5224 <qncj5224@naver.com>
2023-11-07 11:27:33 +0900

    feat: 입력 예외 처리 테스트 추가 - 보너스 번호와 당첨 번호가 중복일 때 확인

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.List;
 import lotto.validator.BonusNumberValidator;
 import lotto.validator.PurchaseAmountValidator;
 import org.junit.jupiter.api.DisplayName;
@@ -35,4 +36,15 @@ public class BonusNumberTest {
     assertThatThrownBy(() -> BonusNumberValidator.isEmptyString(input))
         .isInstanceOf(IllegalArgumentException.class);
   }
+
+  @DisplayName("보너스 번호와 당첨번호 중복 확인")
+  @Test
+  void isDuplicate() {
+    String input = "";
+    List<Integer> myList = List.of(1, 2, 3, 4, 5, 6);
+
+    assertThatThrownBy(() -> BonusNumberValidator.isDuplicate(myList, input))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
 }

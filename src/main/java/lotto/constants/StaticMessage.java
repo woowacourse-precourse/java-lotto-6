a84c5797827536b3da3e4f5ac14b365d59bf71e2
SuhyungK <dook4966@gmail.com>
2023-11-08 08:15:08 +0900

    feat(StaticMessage): 구매 금액 & 당첨 번호 입력에 대한 문구 추가

--- src/main/java/lotto/constants/StaticMessage.java
+++ src/main/java/lotto/constants/StaticMessage.java
@@ -1,15 +1,23 @@
 package lotto.constants;
 
 public enum StaticMessage {
+    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_PAYMENT("구입금액을 입력해 주세요."),
     PRINT_LOTTO_COUNT("개를 구매했습니다."),
     PRINT_RESULT_NOTICE("당첨 통계%n---");
 
     private final String message;
+
     StaticMessage(String message) {
         this.message = message;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    @Override
+    public String toString() {
+        return getMessage();
     }
 }

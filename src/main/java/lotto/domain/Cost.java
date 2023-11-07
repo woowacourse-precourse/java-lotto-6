55f0f3a785f702f76839a120e34269ec10bfc090
jiohh <dkswlgh99@naver.com>
2023-11-07 17:36:12 +0900

    feat(domain): cost자료구조 반복 비지니스 로직 추가

--- src/main/java/lotto/domain/Cost.java
+++ src/main/java/lotto/domain/Cost.java
@@ -1,13 +1,29 @@
 package lotto.domain;
 
 
+import static lotto.constants.constants.LOTTO_COST;
+
 import lotto.validation.CostValidator;
 
 public class Cost {
     private final int number;
+    private int count;
 
-    public Cost(int number) {
+    public Cost(Integer number) {
         CostValidator.validateParsedCost(number);
+        resetCount();
         this.number = number;
+    }
+
+    public boolean buyingLotto() {
+        if (count < (number / LOTTO_COST)) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+
+    public void resetCount() {
+        this.count = 0;
     }
 }

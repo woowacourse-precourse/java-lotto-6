e6f4e7fa9e551801451b9137eaff5005955c372d
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 22:49:41 +0900

    feat: controller 실행 구조

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,13 @@
 package lotto.controller;
 
+import java.util.List;
+
 public class LottoController {
+    public void run() {
+        startLotto();
+    }
+
+    private void startLotto() {
+    }
+
 }

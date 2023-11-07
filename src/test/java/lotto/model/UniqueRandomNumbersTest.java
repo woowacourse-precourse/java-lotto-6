fafdd9d3be34100321eb1d349295a31e3429b369
cozym <ddongyee99@daum.net>
2023-11-07 23:39:29 +0900

    test: 생성된 랜덤 값 범위 테스트 추가

--- src/test/java/lotto/model/UniqueRandomNumbersTest.java
+++ src/test/java/lotto/model/UniqueRandomNumbersTest.java
@@ -1,5 +1,16 @@
 package lotto.model;
 
-class UniqueRandomNumbersTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UniqueRandomNumbersTest {
+    final UniqueRandomNumbers numbers = new UniqueRandomNumbers();
+
+    @DisplayName("생성된 값이 1~45범위를 벗어나지 않는지 테스트")
+    @Test
+    void 생성값_범위테스트() {
+        assertThat(numbers.getNumbers().stream().allMatch(n -> 1 <= n && n <= 45)).isTrue();
+    }
 }

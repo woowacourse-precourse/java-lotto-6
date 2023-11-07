91d57d166c2455f8bb67e9c776c33d91ec9bd9e8
euniiiii <hee9920@naver.com>
2023-11-07 15:05:16 +0900

    test: 구입 로또들의 당첨 내역 기능 테스트 수정

--- src/test/java/lotto/domain/LottoOfficeTest.java
+++ src/test/java/lotto/domain/LottoOfficeTest.java
@@ -21,21 +21,21 @@ class LottoOfficeTest {
         // given
         LottoOffice lottoOffice = new LottoOffice(winningNumbers, bonusNumber);
         List<Lotto> lottoList = new ArrayList<>(List.of(
-                new Lotto(List.of(8, 21, 23, 41, 42, 43)),
-                new Lotto(List.of(3, 5, 11, 16, 32, 38)),
-                new Lotto(List.of(7, 11, 16, 35, 36, 44)),
-                new Lotto(List.of(1, 8, 11, 31, 41, 42)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)), // 3등
+                new Lotto(List.of(1, 2, 3, 4, 8, 9)), // 4등
+                new Lotto(List.of(1, 2, 3, 10, 11, 12)), // 5등
                 new Lotto(List.of(13, 14, 16, 38, 42, 45)),
                 new Lotto(List.of(7, 11, 30, 40, 42, 43)),
-                new Lotto(List.of(2, 13, 22, 32, 38, 45)),
-                new Lotto(List.of(1, 3, 5, 14, 22, 45))
+                new Lotto(List.of(1, 3, 7, 14, 22, 45))
         ));
 
         Map<LottoRank, Integer> expected = new HashMap<>() {{
-            put(LottoRank.FIRST_PLACE, 0);
-            put(LottoRank.SECOND_PLACE, 0);
-            put(LottoRank.THIRD_PLACE, 0);
-            put(LottoRank.FOURTH_PLACE, 0);
+            put(LottoRank.FIRST_PLACE, 1);
+            put(LottoRank.SECOND_PLACE, 1);
+            put(LottoRank.THIRD_PLACE, 1);
+            put(LottoRank.FOURTH_PLACE, 1);
             put(LottoRank.FIFTH_PLACE, 1);
         }};
 

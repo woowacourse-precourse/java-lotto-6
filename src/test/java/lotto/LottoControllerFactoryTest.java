fcc60199866390b6cbbd0159e85673a32c8f5f0b
nylonmusk <2912rla@gmail.com>
2023-11-08 04:54:14 +0900

    test : LottoControllerFactoryTest 생성

---
+++ src/test/java/lotto/LottoControllerFactoryTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.factory.LottoControllerFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import lotto.controller.LottoController;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LottoControllerFactoryTest {
+
+    @DisplayName("LottoControllerFactory가 LottoController를 올바르게 생성하는지 확인한다.")
+    @Test
+    void LottoControllerFactory가_LottoController를_올바르게_생성() {
+        LottoController controller = LottoControllerFactory.create();
+
+        assertNotNull(controller);
+    }
+}

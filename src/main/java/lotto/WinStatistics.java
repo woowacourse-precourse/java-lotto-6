3fe67c24c9dd2b7d73adc5ff7bd826021642f3d0
You <ytn1574@gmail.com>
2023-11-07 16:06:33 +0900

    feat: enum 적용 작업중
    공부중인 부분이므로 설계는 언제던지 변경될 수 있음

---
+++ src/main/java/lotto/WinStatistics.java
@@ -0,0 +1,30 @@
+package lotto;
+
+public enum WinStatistics {
+
+    MATCH_ZERO(0, 0L),
+    MATCH_ONE(1, 0L),
+    MATCH_TWO(2, 0L),
+    MATCH_THREE_RANK_FIFTH(3, 5000L),
+    MATCH_FOUR_RANK_FOURTH(4, 50000L),
+    MATCH_FIVE_NO_BONUS_RANK_THIRD(5, 1500000L),
+    MATCH_FIVE_YES_BONUS_RANK_SECOND(6, 30000000L),
+    MATCH_SIX_NO_RANK_FIRST(6, 2000000000L);
+
+    private int matchCount;
+    private long winMoney;
+
+    WinStatistics(int matchCount, long winMoney) {
+        this.matchCount = matchCount;
+        this.winMoney = winMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public long getWinMoney() {
+        return winMoney;
+    }
+
+}

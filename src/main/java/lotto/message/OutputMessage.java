3a765d0d6bc2d353391966208fcc1bdbc9e5f4de
juns1s <juns1s@naver.com>
2023-11-07 16:08:07 +0900

    feat: 출력 메시지 정의

---
+++ src/main/java/lotto/message/OutputMessage.java
@@ -0,0 +1,21 @@
+package lotto.message;
+
+public enum OutputMessage {
+    TOTAL_PRICE("구입금액을 입력해 주세요."),
+    LOTTO_COUNT("개를 구매했습니다."),
+    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    RESULT_SUMMARY("당첨 통계\n---"),
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}

c5fb1e9d84b9a95962247cd9ab42cd789607d287
mmalfoy <stpmalfoy@gmail.com>
2023-11-08 02:32:59 +0900

    test(InputUI) : 추출된 메소드 테스트 추가
    body : moneyEdit() 메서드

--- src/test/java/lotto/domain/InputUITest.java
+++ src/test/java/lotto/domain/InputUITest.java
@@ -1,11 +1,18 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import lotto.data.Rewards;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class InputUITest {
     private InputUI inputUI;
@@ -60,6 +67,22 @@ class InputUITest {
         assertThatThrownBy(() -> inputUI.checkValidPurchase(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(expect);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterProviderMoneyEdit")
+    void moneyEdit_상금액수_출력형식_변경_테스트(Rewards reward, String expect) {
+        assertThat(inputUI.moneyEdit(reward)).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> parameterProviderMoneyEdit() {
+        return Stream.of(
+                arguments(Rewards.FIRST, "(2,000,000,000원)"),
+                arguments(Rewards.SECOND, "(30,000,000원)"),
+                arguments(Rewards.THIRD, "(1,500,000원)"),
+                arguments(Rewards.FOURTH, "(50,000원)"),
+                arguments(Rewards.FIFTH, "(5,000원)")
+        );
     }
 
 

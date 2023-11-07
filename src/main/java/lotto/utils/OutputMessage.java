b0644a715ff92ad07144bf0e6722c9bf21b49238
JiEung2 <https://github.com/JiEung2>
2023-11-08 01:43:37 +0900

    Add: 대괄호 enum 추가

--- src/main/java/lotto/utils/OutputMessage.java
+++ src/main/java/lotto/utils/OutputMessage.java
@@ -11,7 +11,9 @@ public enum OutputMessage {
     FOUR_MATCH_MESSAGE("4개 일치 (50,000원)"),
     FIVE_MATCH_MESSAGE("5개 일치 (1,500,000원)"),
     BONUS_MATCH_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    SIX_MATCH_MESSAGE("6개 일치 (2,000,000,000원)");
+    SIX_MATCH_MESSAGE("6개 일치 (2,000,000,000원)"),
+    BRACKETS1("["),
+    BRACKETS2("]");
 
     private final String message;
 

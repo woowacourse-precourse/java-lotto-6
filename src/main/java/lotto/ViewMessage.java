209d77368c02bdcc0e7606823d5a726c16474527
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 06:17:49 +0900

    fix(ViewMessage) : 출력문 요구사항에 맞게 변경

--- src/main/java/lotto/ViewMessage.java
+++ src/main/java/lotto/ViewMessage.java
@@ -2,11 +2,11 @@ package lotto;
 
 public enum ViewMessage {
     PAYMENT_PROMPT_MESSAGE("구입금액을 입력해 주세요."),
-    NUMBER_OF_PURCHASED_FORMAT("%d개를 구매했습니다"),
+    NUMBER_OF_PURCHASED_FORMAT("%d개를 구매했습니다."),
     WINNING_NUMBERS_PROMPT_MESSAGE("당첨 번호를 입력해 주세요."),
     BONUS_PROMPT_MESSAGE("보너스 번호를 입력해 주세요."),
     WINNING_RESULT_MESSAGE("당첨 통계\n---"),
-    RATE_OF_REVENUE_FORMAT("총 수익률은 %.1f입니다.");
+    RATE_OF_REVENUE_FORMAT("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 

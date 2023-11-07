1e24ee5dc3d29466c2b7977023722d074697257a
Identityless <rlandwnd555@naver.com>
2023-11-07 21:24:35 +0900

    feat : 수익률 DTO 추가

---
+++ src/main/java/lotto/dto/ProfitRateDTO.java
@@ -0,0 +1,13 @@
+package lotto.dto;
+
+public class ProfitRateDTO {
+    double profitRate;
+
+    public ProfitRateDTO(double profitRate) {
+        this.profitRate = profitRate;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+}

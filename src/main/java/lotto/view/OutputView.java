250b84b510f029eeb3d80e59cdaaed8ac46c36ea
sw801733 <sw801733@naver.com>
2023-11-07 22:55:41 +0900

    feat : OutputView 클래스 생성
    - 출력을 위한 OutputView 클래스 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import lotto.domain.LottoPurchase;
+
+public class OutputView {
+
+    private final int lottoPurchase;
+
+    public OutputView(int lottoPurchase) {
+        this.lottoPurchase = lottoPurchase;
+    }
+
+
+
+}

aa1d39524efdea99b0d7fc045b6ab730d7b83f89
Junobee25 <wnsdhqo@naver.com>
2023-11-07 16:12:44 +0900

    style(Money): 불필요한 import 제거

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,13 +1,10 @@
 package lotto.domain;
 
-import lotto.verification.Validation;
-
 public class Money {
 
     private final long money;
 
     public Money(long money) {
-        Validation.verifyRangeInputMoney(money);
         this.money = money;
     }
 

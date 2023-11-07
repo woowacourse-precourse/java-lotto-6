22b63fd9dd4eec2a11f00b48b475ce0317a35eeb
ksw13 <ksw48483@gmail.com>
2023-11-07 19:14:25 +0900

    refactor: 출력 결과 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,12 +5,15 @@ import lotto.model.PlayerLotto;
 import lotto.model.PlayerLottos;
 import lotto.model.WinningRank;
 import lotto.model.WinningResult;
-import lotto.util.MessageConst;
 import lotto.util.NumberConst;
 
 public class OutputView {
 
     public static final String ISSUE_COUNT_MSG = "개를 구매했습니다.";
+    public static final String STATISTICS_MSG = "당첨 통계";
+    public static final String CONTOUR_MSG = "---";
+    public static final String WINNING_DETAILS_MSG = "%s (%s원) - %d개";
+    public static final String PROFIT_MSG = "총 수익률은 %s%%입니다.";
 
     public void printIssueCount(int money) {
         int issueCount = money / NumberConst.DENOMINATION;
@@ -27,16 +30,13 @@ public class OutputView {
 
     public void printWinningResult(WinningResult winningResult) {
         System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(STATISTICS_MSG);
+        System.out.println(CONTOUR_MSG);
         WinningRank[] winningRanks = WinningRank.values();
         for (int i = winningRanks.length - 2; i >= 0; i--) {
-            System.out.print(winningRanks[i].getMessage());
-            System.out.print(MessageConst.BLANK);
-            System.out.print("(" + rewardDecimalFormat(winningRanks[i].getReward()) + "원)");
-            System.out.print(" - ");
-            System.out.print(getWinningRankCount(winningResult, winningRanks[i]));
-            System.out.println("개");
+            System.out.println(String.format(WINNING_DETAILS_MSG, winningRanks[i].getMessage(),
+                    rewardDecimalFormat(winningRanks[i].getReward()),
+                    getWinningRankCount(winningResult, winningRanks[i])));
         }
     }
 
@@ -60,9 +60,7 @@ public class OutputView {
 
     public void printProfit(int money, double totalRevenue) {
         double profit = (totalRevenue / money) * 100;
-        System.out.print("총 수익률은 ");
-        System.out.print(profitDecimalFormat(profit));
-        System.out.println("%입니다.");
+        System.out.println(String.format(PROFIT_MSG, profitDecimalFormat(profit)));
     }
 
     public void printExceptionMessage(IllegalArgumentException exception) {

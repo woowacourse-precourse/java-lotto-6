f57d0d09af796a94819e50ed209ce5049cefe46d
WinterHana <haha920@naver.com>
2023-11-08 01:59:11 +0900

    feat(ResultController) : 최종 결과를 반환하는 기능 구현
    - getTotalResult()를 통해 최종 결과 반환

--- src/main/java/lotto/controller/ResultController.java
+++ src/main/java/lotto/controller/ResultController.java
@@ -1,11 +1,12 @@
 package lotto.controller;
 
 import lotto.domain.LottoResultCalculation;
+import lotto.type.ResultType;
+import org.mockito.internal.matchers.Null;
 
 import java.util.List;
 
 public class ResultController {
-
     LottoResultCalculation lottoResultCalculation;
 
     ResultController() {
@@ -18,7 +19,32 @@ public class ResultController {
     }
 
     public boolean getBonusResult(List<Integer> target, int bouns) {
-        boolean result = lottoResultCalculation.checkBounsNumber(target, bouns);
+        boolean result = lottoResultCalculation.checkBonusNumber(target, bouns);
         return result;
+    }
+
+    public ResultType getLottoResult(int sameNumber, boolean isBonus) {
+        if (sameNumber == 3) {
+            return ResultType.THIRD;
+        }
+        if (sameNumber == 4) {
+            return ResultType.FOURTH;
+        }
+        if (sameNumber == 5) {
+            if (isBonus) {
+                return ResultType.FIFTH_BONUS;
+            }
+        }
+        if (sameNumber == 6) {
+            return ResultType.ALL;
+        }
+        return null;
+    }
+
+    public ResultType getTotalResult(List<Integer> target, List<Integer> tryLotto, int bonus) {
+        int sameNumber = getLottoResult(target, tryLotto);
+        boolean isBonus = getBonusResult(target, bonus);
+        ResultType resultType = getLottoResult(sameNumber, isBonus);
+        return resultType;
     }
 }

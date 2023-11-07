cbfc5b94d5e9c8a14df949372512d9774a4e4894
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2023-11-07 14:55:26 +0900

    test: 당첨 내역 기능 구현중

---
+++ src/test/java/lotto/domain/WinRecordTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinRecordTest {
+    @Test
+    @DisplayName("당첨 내역이 정상출력됨을 확인한다.")
+    void print(){
+        Wallet wallet = new Wallet(14000);
+        Tickets tickets = new Tickets(wallet);
+        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        Bonus bonus = new Bonus(7);
+
+        WinRecord winRecord = new WinRecord(lotto, bonus);
+        winRecord.inputWinRecord(tickets);
+
+        winRecord.print();
+    }
+
+    @Test
+    @DisplayName("당첨합계를 구한다.")
+    void sumPrizeAmount(){
+
+    }
+}

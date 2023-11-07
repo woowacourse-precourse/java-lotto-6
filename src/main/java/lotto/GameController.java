d41e2715257ff575c4a253345af735b1ce44c0b8
Sur0n <skyzephyr333@gmail.com>
2023-11-08 01:07:34 +0900

    feat : build GameController base

--- src/main/java/lotto/GameController.java
+++ src/main/java/lotto/GameController.java
@@ -1,4 +1,18 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+import lotto.GameInput;
+import lotto.GameOutput;
 
 public class GameController {
+    private GameController(){
+
+        GameOutput.printUserCashInterface();
+        GameInput.getUserCash();
+        GameOutput.printUserHopeLottoNumbersInterface();
+        GameInput.getUserHopeLottoNumbers();
+        GameOutput.getUserHopeBonusNumbersInterface();
+        GameInput.getUserHopeBonusNumbers();
+        Console.close();
+    }
 }
--- src/main/java/lotto/GameOutput.java
+++ src/main/java/lotto/GameOutput.java
@@ -2,19 +2,19 @@ package lotto;
 import lotto.Messages;
 
 public class GameOutput {
-    private void printMessages(String messages){
+    private static void printMessages(String messages){
         System.out.println(messages);
     }
 
-    public void printUserCashInterface() {
+    public static void printUserCashInterface() {
         printMessages(Messages.ASK_LOTTO_MONNEY.getMessages());
     }
 
-    public void printUserHopeLottoNumbersInterface() {
+    public static void printUserHopeLottoNumbersInterface() {
         printMessages(Messages.ASK_WINNING_PRIZE_NUMBERS.getMessages());
     }
 
-    public void getUserHopeBonusNumbersInterface() {
+    public static void getUserHopeBonusNumbersInterface() {
         printMessages(Messages.ASK_BONUS_NUMBER.getMessages());
     }
 }

b9537e8b7b295529c61871bcd3bb458e7ddfd251
devJSH1004 <devJSH1004@gmail.com>
2023-11-08 05:13:08 +0900

    feat: validate 통한 예외 처리 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,20 +1,87 @@
 package lotto.view;
 
+import lotto.Validator;
+
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+
+    Validator validator = new Validator();
     public String inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return readLine();
     }
 
-    public String inputLottoNumber() {
+    public String inputAmount2() {
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            try {
+                String input = readLine();
+                int amount = Integer.parseInt(input);
+                validator.validateAmountUnit(amount);
+                return input;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("[ERROR] 1000원 단위의 금액을 입력해주세요.");
+            }
+            catch (IllegalStateException e) {
+                System.out.println("[ERROR] 1000원 단위의 금액을 입력해주세요.");
+            }
+        }
+    }
+
+    public String inputWinNumber() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return readLine();
     }
 
-    public String inputLottoNumberBonus() {
+    public String inputWinNumber2() {
+        while (true) {
+            System.out.println();
+            System.out.println("당첨 번호를 입력해 주세요.");
+            try {
+                String input = readLine();
+                String[] numbers = input.trim().split("\\s*,\\s*");
+                validator.validateWinNumberLength(numbers);
+                List<Integer> numberList = validator.validateWinNumberInt(numbers);
+                validator.validateWinNumberDuplicate(numberList);
+                return input;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 당첨번호는 쉼표(,)로 구분하고, 1 ~ 45 사이의 중복되지 않는 숫자를 입력해주세요. NUM");
+            } catch (IllegalStateException e) {
+                System.out.println("[ERROR] 당첨번호는 쉼표(,)로 구분하고, 1 ~ 45 사이의 중복되지 않는 숫자를 입력해주세요. STATE");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 당첨번호는 쉼표(,)로 구분하고, 1 ~ 45 사이의 중복되지 않는 숫자를 입력해주세요. ARG");
+            }
+        }
+    }
+
+    public String inputBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         return readLine();
+    }
+
+    public String inputBonusNumber2(List<Integer> numbers) {
+        while (true) {
+            System.out.println();
+            System.out.println("보너스 번호를 입력해 주세요.");
+            try {
+                String input = readLine();
+                int number = Integer.parseInt(input);
+                validator.validateNumberRange(number);
+                validator.validateBonusNumberDuplicate(numbers, number);
+                return input;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해주세요. NUM");
+            } catch (IllegalStateException e) {
+                System.out.println("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 당첨 번호와 중복되지 않는 숫자를 입력해주세요.");
+            }
+        }
     }
 }

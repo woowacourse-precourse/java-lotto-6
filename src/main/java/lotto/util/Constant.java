6dff7b496c7be9938e131a31f185380d6acdb020
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 06:59:41 +0900

    feat(Constant): 등수에 따른 출력 메시지 구성

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -5,4 +5,10 @@ public class Constant {
     public static final int LOTTO_MAX_RANGE = 45;
     public static final int LOTTO_NUMBERS_COUNT = 6;
     public static final int LOTTO_PER_PRICE = 1000;
+
+    public static final String FIRST_PRINT_MESSAGE = "6개 일치 (%d원) - %d개";
+    public static final String SECOND_PRINT_MESSAGE = "5개 일치, 보너스 볼 일치 (%d원) - %d개";
+    public static final String THIRD_PRINT_MESSAGE = "5개 일치 (%d원) - %d개";
+    public static final String FORTH_PRINT_MESSAGE = "4개 일치 (%d원) - %d개";
+    public static final String FIFTH_PRINT_MESSAGE = "3개 일치 (%d원) - %d개";
 }

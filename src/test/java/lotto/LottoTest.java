373785173565f6a5a046e81fecf06ea404d8dc27
ahma0 <dsd932@naver.com>
2023-11-07 21:22:26 +0900

    test: 테스트 항목 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.model.Lotto;
+import lotto.view.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,27 +14,31 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ONLY_SIZE_IS_SIX);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_DUPLICATE);
     }
 
     @DisplayName("로또 번호에 음수가 있으면 예외가 발생한다.")
     @Test
     void createLottoByMinusNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, -2, 3, 4, 5, 6)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_MINUS_OR_ZERO);
     }
 
     @DisplayName("로또 번호에 0이 있으면 예외가 발생한다.")
     @Test
     void createLottoByZero() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 0, 3, 4, 5, 6)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.NOT_MINUS_OR_ZERO);
     }
 }

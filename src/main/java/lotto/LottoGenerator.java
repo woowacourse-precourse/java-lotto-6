1c0886b3ce6eba7adb0cb9d7db93517d4742de63
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-07 19:31:53 +0900

    LottoGenerator 클래스 생성
    로또 번호를 생성하는 클래스 입니다.
     - OOP의 단일 책임 원칙을 지키기 위해서 Lotto 클래스는 로또 번호의 유효성 검사와 다른 로또와의 비교를 하는 역할만 부여하고, 로또 번호를 만드는 별도의 클래스를 생성했습니다.

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class LottoGenerator {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private static final int LOTTO_SIZE = 6;
+    
+}

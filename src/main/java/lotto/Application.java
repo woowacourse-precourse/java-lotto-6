b20a205cea26933a8a5cda1907465b888bfb3db2
Dawin <dawin002@naver.com>
2023-11-08 02:58:50 +0900

    feat(Application): 프로그램 실행문 추가
    프로그램 실행문 추가:
    - InputValidator 객체 생성
    - InputView 객체 생성
    - OutputView 객체 생성
    - LottoGameController 객체 생성
    - LottoGameController 객체의 run() 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.model.InputValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputValidator inputValidator = new InputValidator();
+        InputView inputView = new InputView(inputValidator);
+        OutputView outputView = new OutputView();
+
+        LottoGameController lottoGameController = new LottoGameController(inputView, outputView);
+        lottoGameController.run();
     }
 }

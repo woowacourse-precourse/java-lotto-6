152a70458a852b056777cee0318f28e7d8ff3898
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-07 16:43:09 +0900

    refactor(NonNumberException): 재사용성을 위한 메소드명 변경

--- src/main/java/lotto/exception/NonNumberException.java
+++ src/main/java/lotto/exception/NonNumberException.java
@@ -1,9 +1,9 @@
-package lotto.exception.money;
+package lotto.exception;
 
-public class MoneyNumberFormatException extends IllegalArgumentException {
+public class NonNumberException extends IllegalArgumentException {
     private static final String ONLY_NUMBER_ALLOWED_MESSAGE = "숫자만 입력해 주세요.";
 
-    public MoneyNumberFormatException() {
+    public NonNumberException() {
         super(ONLY_NUMBER_ALLOWED_MESSAGE);
     }
 }

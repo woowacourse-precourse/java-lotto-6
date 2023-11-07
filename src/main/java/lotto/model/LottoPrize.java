f122ce84971d99b6b9450578e1d3310ec7db3bf8
Ukj0ng <xb253q@gmail.com>
2023-11-07 18:17:11 +0900

    feat(LottoPrize): 로또 당첨의 카테고리 분류
    멤버 상수:
    - 일치하는 번호의 수
    - 상금 액수
    - 보너스 번호의 당첨 여부

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -1,17 +1,18 @@
 package lotto.model;
 
-public enum LottoPrizes {
+public enum LottoPrize {
     FIRST_PRIZE(6, 2000000000, false),
     SECOND_PRIZE(5, 30000000, true),
     THIRD_PRIZE(3, 1500000, false),
     FOURTH_PRIZE(4, 50000, false),
     FIFTH_PRIZE(5, 5000, false);
 
+
     private final int matchedNumbers;
     private final int prize;
     private final boolean bonus;
 
-    LottoPrizes(int matchedNumbers, int prize, boolean bonus) {
+    LottoPrize(int matchedNumbers, int prize, boolean bonus) {
         this.matchedNumbers = matchedNumbers;
         this.prize = prize;
         this.bonus = bonus;

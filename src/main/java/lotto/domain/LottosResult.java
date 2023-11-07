054c5d360515fda882b491d4c608ab38a9f83f5a
PARK9898 <wjddjs21@naver.com>
2023-11-08 01:31:54 +0900

    "refactor: Static 메서드로 변환"

--- src/main/java/lotto/domain/LottosResult.java
+++ src/main/java/lotto/domain/LottosResult.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class LottosResult {
 
-    public long calculateTotalPrize(Map<Rank, Integer> results) {
+    public static long calculateTotalPrize(Map<Rank, Integer> results) {
         int totalPrize = 0;
         for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
             totalPrize += calculateOncePrize(entry);
@@ -16,7 +16,7 @@ public class LottosResult {
         return totalPrize;
     }
 
-    private long calculateOncePrize(Map.Entry<Rank, Integer> entry) {
+    private static long calculateOncePrize(Map.Entry<Rank, Integer> entry) {
         Rank rank = entry.getKey();
         int count = entry.getValue();
         int prize = rank.getPrizeMoney();
@@ -24,14 +24,14 @@ public class LottosResult {
         return count * prize;
     }
 
-    public List<Integer> countEachRank(Map<Rank, Integer> results) {
+    public static List<Integer> countEachRank(Map<Rank, Integer> results) {
         return Arrays.stream(Rank.values())
                 .filter(rank -> !isZero(rank))
                 .map(rank -> results.getOrDefault(rank, 0))
                 .collect(Collectors.toList());
     }
 
-    private boolean isZero(Rank rank) {
+    private static boolean isZero(Rank rank) {
         return rank == Rank.ZERO;
     }
 }

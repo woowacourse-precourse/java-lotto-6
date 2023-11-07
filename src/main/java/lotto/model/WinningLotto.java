cd0db2fe83530d1a335b919120794cbbf7b23f3a
ChaeAg <dydkglel123@gmail.com>
2023-11-08 00:52:55 +0900

    refactor(WinningLotto): 가독성을 위한 리팩토링

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -7,7 +7,7 @@ public class WinningLotto {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final String DUPLICATE_WINNING_BONUS_NUMBER_ERROR = "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.";
-    private static final String NOT_NUMBER_BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 정수여야 합니다.";
+    private static final String NOT_INTEGER_BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 정수여야 합니다.";
     private static final String INVALID_BONUS_NUMBER_RANGE_ERROR = "[ERROR] 보너스 번호는 %d부터 %d 사이의 수여야 합니다.";
     private final Lotto winningLotto;
     private final int bonusNumber;
@@ -31,23 +31,25 @@ public class WinningLotto {
     }
 
     private static void validateNoDuplicateNumber(List<Integer> numbers, String bonusNumberStr) {
-        List<String> numbersStr = numbers.stream().map(number -> String.valueOf(number)).collect(Collectors.toList());
+        List<String> numbersStr = numbers.stream()
+                .map(number -> String.valueOf(number))
+                .collect(Collectors.toList());
         if (numbersStr.contains(bonusNumberStr)) {
             throw new IllegalArgumentException(DUPLICATE_WINNING_BONUS_NUMBER_ERROR);
         }
     }
 
     private int bonusNumberValidator(String bonusNumberStr) {
-        int bonusNumber = validateNotNumber(bonusNumberStr);
+        int bonusNumber = validateNotInteger(bonusNumberStr);
         validateInvalidLottoNumberRange(bonusNumber);
         return bonusNumber;
     }
 
-    private int validateNotNumber(String bonusNumberStr) {
+    private int validateNotInteger(String bonusNumberStr) {
         try {
             return Integer.parseInt(bonusNumberStr);
         } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException(NOT_NUMBER_BONUS_NUMBER_ERROR);
+            throw new IllegalArgumentException(NOT_INTEGER_BONUS_NUMBER_ERROR);
         }
     }
 

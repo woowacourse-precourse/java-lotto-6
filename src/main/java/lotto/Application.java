3aa6ee3afb1cf850c2b060ed32187fdacd8c4cdc
권장순 <jsoonworld@gmail.com>
2023-11-08 01:04:24 +0900

    test : main에서 구현한 기능 작동 확인.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,22 @@
 package lotto;
 
 
+import lotto.v3.controller.LottoGenerationController;
 import lotto.v3.controller.LottoPurchaseController;
+import lotto.v3.model.LottoMachine;
+import lotto.v3.view.LottoGenerateView;
 import lotto.v3.view.LottoPurchaseView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoPurchaseController controller = new LottoPurchaseController(new LottoPurchaseView());
-        controller.startPurchaseProcess();
+        LottoPurchaseView purchaseView = new LottoPurchaseView();
+        LottoPurchaseController purchaseController = new LottoPurchaseController(purchaseView);
+
+        LottoGenerateView generateView = new LottoGenerateView();
+        LottoMachine machine = new LottoMachine();
+        LottoGenerationController generationController = new LottoGenerationController(generateView, machine);
+
+        int numberOfTickets = purchaseController.startPurchaseProcess();
+        generationController.generateLottoTickets(numberOfTickets);
     }
 }

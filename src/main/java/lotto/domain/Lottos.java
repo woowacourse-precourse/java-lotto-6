1f14cc4a383ac121fd8dabe1065922576148d413
jongheonleee <qwefghnm1212@gmail.com>
2023-11-07 16:58:33 +0900

    [refacotr] Lottos 클래스에서 사용하지 않는 메서드 제거

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -19,11 +19,6 @@ public class Lottos {
         return new Lottos(lottoDummy);
     }
 
-    public Prizes createPrizes(final WinnerLotto winnerLotto) {
-        final List<Prize> prizes = compareAllLottoWithWinnerLotto(winnerLotto.getWinnerNumbers(),
-                                                                winnerLotto.getBonusNumber());
-        return new Prizes(prizes);
-    }
     private List<Prize> compareAllLottoWithWinnerLotto(final List<Integer> winnerNumbers, final Integer bonusNumber) {
         return lottos.stream()
                 .map(lotto -> lotto.compareWithWinnerLotto(winnerNumbers, bonusNumber))

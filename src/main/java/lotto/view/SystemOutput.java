707e468b5cbbf6887f9e5ffe8e7dbea729904dcb
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-07 15:00:45 +0900

    feat(LottoMachine): maek code that prints out result of rank

--- src/main/java/lotto/view/SystemOutput.java
+++ src/main/java/lotto/view/SystemOutput.java
@@ -3,6 +3,9 @@ package lotto.view;
 import lotto.Lotto;
 
 import java.util.List;
+
+import lotto.domain.Rank;
+import lotto.domain.RankContainer;
 import main.java.lotto.domain.WinningLottos;
 public class SystemOutput {
     // 게임 개수를 출력한다
@@ -13,7 +16,7 @@ public class SystemOutput {
     public static void printPurchaseAmount(int purchaseAmount) {
         String OUTPUT_MESSAGE = "%d개를 구매했습니다.";
 
-        System.out.printf((OUTPUT_MESSAGE) + "%n",(purchaseAmount/1000));
+        System.out.printf((OUTPUT_MESSAGE) + "%n", (purchaseAmount / 1000));
     }
 
     public static void printWiningLotto(WinningLottos winningLottos) {
@@ -22,6 +25,26 @@ public class SystemOutput {
             System.out.println(lotto);
         }
     }
+
+    public static void printRankResult(RankContainer rankContainer, double rate) {
+        System.out.println("당첨 통계" + "\n" + "---");
+        for (Rank rank : Rank.values()) {
+            if (!rank.equals(Rank.NONE)) {
+                System.out.println()
+            }
+        }
+        System.out.printf(("총 수익률은 %.1f%%입니다.") + "\n", rate);
+    }
+
+    public static String getRankContainerResult(Rank rank, RankContainer rankContainer) {
+        if (rank == Rank.SECOND) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개", rank.getCount(), String.format("%,d", rank.getAward()), rankContainer.getRankContainer(rank));
+        }
+
+        return String.format("%d개 일치 (%s원) - %d개", rank.getCount(), String.format("%,d", rank.getAward(), rankContainer.getRankContainer(rank)));
+    }
+
+
 
 
 }

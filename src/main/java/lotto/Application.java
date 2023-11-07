5bc37054ff39eaa12e8c794e909d743b13a9ed7e
KangDeok <rkdejr2321@naver.com>
2023-11-08 02:42:20 +0900

    [feat] 검증 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,14 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.UserLotto;
 import lotto.service.LottoService;
+import lotto.validation.InputValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
 
-    static LottoController lottoController = new LottoController(new LottoService(), new InputView(), new OutputView());
+    static LottoController lottoController = new LottoController(new LottoService(), new InputView(new InputValidator()), new OutputView());
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현

87f8044b63c26cff6f9d75ddb99339d44748aef6
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 16:54:56 +0900

    feat(LottoService): 스켈레톤 코드 작성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,2 +1,26 @@
-package lotto.service;public class LottoService {
+package lotto.service;
+
+import java.util.List;
+
+public class LottoService {
+
+    public List<Integer> reshapeWinningNumber(String inputNumbersWithComma) {
+
+    }
+
+    public int calculateAvailableNumberOfLotteryTickets(int spentAmount) {}
+
+    public List<List<Integer>> generateRandomLottoNums(int availableTicketsCount) {}
+
+    public int compareMyNumberWithWinningNumber(List<Integer> myLottoNumbers, List<Integer> winningNumber) {}
+
+    public boolean checkBonusPoint(List<Integer> myLottoNumber, int bonusNumber) {}
+
+    public int getCaseNum(int lottoWinCase) {}
+
+    public void increaseTotalIncomeTest(int lottoWinCase) {}
+
+    public String typeConvertIncomeRate(float incomeRate) {}
+
+    public String calculateIncomeRate(int totalIncomde, int spentAmount) {}
 }

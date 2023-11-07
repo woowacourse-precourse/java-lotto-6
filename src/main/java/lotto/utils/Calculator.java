9d29b6d01d633fb6888cac45973881a8c7e82c3c
sangjun <sangjun1389@ajou.ac.kr>
2023-11-07 15:59:33 +0900

    feat(Calculator): 연산을 진행할 클래스 생성

--- src/main/java/lotto/utils/Calculator.java
+++ src/main/java/lotto/utils/Calculator.java
@@ -1,2 +1,22 @@
-package lotto.utils;public class Calculator {
+package lotto.utils;
+
+import static lotto.utils.LottoEnum.*;
+
+import java.util.List;
+import lotto.domain.model.ResultTable;
+
+public class Calculator {
+
+    public static int getLottoCount(int amountMoney) {
+        return amountMoney / LOTTO_PRICE.getValue();
+    }
+
+    public static double getProfitRate(long totalPrice, long amountMoney) {
+        return 100 * (double) totalPrice / amountMoney;
+    }
+
+    public static long evaluateTotalPrice(List<ResultTable> statistics) {
+        return statistics.stream()
+                .mapToLong(ResultTable::getTotalPrize).sum();
+    }
 }

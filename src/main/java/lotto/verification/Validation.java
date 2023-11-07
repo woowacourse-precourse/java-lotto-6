5ef40f5c297e15bd320c2a1e0d27d4747792eecb
Junobee25 <wnsdhqo@naver.com>
2023-11-07 16:11:45 +0900

    refactor(Validation): VerificationViewRangeInputMoney 메서드 명 수정

--- src/main/java/lotto/verification/Validation.java
+++ src/main/java/lotto/verification/Validation.java
@@ -6,7 +6,7 @@ public class Validation {
 
     public static void verifyRangeInputMoney(long myMoney) {
         if (myMoney / 1000 < 1 || myMoney % 1000 != 0) {
-            VerificationView.VerificationViewInputMoney();
+            VerificationView.VerificationViewRangeInputMoney();
             throw new IllegalArgumentException();
         }
     }

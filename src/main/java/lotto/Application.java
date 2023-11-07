962fef594709e9b53374332e1bf9df126e28251b
ekeprl <ykeon26@naver.com>
2023-11-07 23:23:29 +0900

    try~catch를 이용한 실행, 오류발생시 catch를 이용해 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,11 @@ import lotto.Controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       try {
         LottoController lottoController = new LottoController();
         lottoController.run();
-    }
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+}
 }

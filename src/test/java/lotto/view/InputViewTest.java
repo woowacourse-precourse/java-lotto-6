927b7ae0dcfd15add3658359e0bbf8a5217cebd6
ikjo39 <chi4321@naver.com>
2023-11-08 01:42:29 +0900

    test: 입력값 검증 테스트 추가
    - 당첨번호목록
    - 보너스번호

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -9,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -50,7 +52,7 @@ class InputViewTest {
             System.setIn(createUserInput(input));
 
             // when  then
-            assertThatThrownBy(() -> inputView.getPurchaseAmount())
+            assertThatThrownBy(inputView::getPurchaseAmount)
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(BLANK_INPUT.toString());
         }
@@ -64,7 +66,7 @@ class InputViewTest {
             System.setIn(createUserInput(input));
 
             // when  then
-            assertThatThrownBy(() -> inputView.getPurchaseAmount())
+            assertThatThrownBy(inputView::getPurchaseAmount)
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(INPUT_NOT_DIGIT.toString());
         }
@@ -78,7 +80,135 @@ class InputViewTest {
             System.setIn(createUserInput(input));
 
             // when  then
-            assertThatThrownBy(() -> inputView.getPurchaseAmount())
+            assertThatThrownBy(inputView::getPurchaseAmount)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(EXCEEDED_MAXIMUM_NUMBER_FORMAT.toString());
+        }
+    }
+
+    @Nested
+    @DisplayName("당첨 번호를 입력 받는다.")
+    class getWinningNumbers {
+
+        @DisplayName("[성공] 입력값이 적절한 형식으로 변환된다.")
+        @Test
+        void getWinningNumbersGood() {
+            // given
+            InputView inputView = new InputView();
+            String input = "1,2,3,4,5,6";
+            List<Integer> expected = Arrays.stream(input.split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+            System.setIn(createUserInput(input));
+
+            // when
+            List<Integer> result = inputView.getWinningNumbers();
+
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
+
+        @DisplayName("[실패] 입력값이 공백이면 예외가 발생한다.")
+        @Test
+        void exception_Blank() {
+            // given
+            InputView inputView = new InputView();
+            String input = "1,  ,2,3,4,5,6";
+            System.setIn(createUserInput(input));
+
+            // when  then
+            assertThatThrownBy(inputView::getWinningNumbers)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(BLANK_INPUT.toString());
+        }
+
+        @DisplayName("[실패] 입력값이 숫자가 아니면 예외가 발생한다.")
+        @Test
+        void exception_Not_Digit_Input() {
+            // given
+            InputView inputView = new InputView();
+            String input = "1,2,3,4,5,a+01a";
+            System.setIn(createUserInput(input));
+
+            // when  then
+            assertThatThrownBy(inputView::getWinningNumbers)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INPUT_NOT_DIGIT.toString());
+        }
+
+        @DisplayName("[실패] 입력값이 지정된 형식의 최댓값보다 크면 예외가 발생한다.")
+        @Test
+        void exception_Exceeded_Maximum_Type_Value() {
+            // given
+            InputView inputView = new InputView();
+            String input = "1,2,3,4,5,9223372036854775808";
+            System.setIn(createUserInput(input));
+
+            // when  then
+            assertThatThrownBy(inputView::getWinningNumbers)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(EXCEEDED_MAXIMUM_NUMBER_FORMAT.toString());
+        }
+    }
+
+    @Nested
+    @DisplayName("보너스 번호를 입력 받는다.")
+    class getBonusNumber {
+
+        @DisplayName("[성공] 입력값이 적절한 형식으로 변환된다.")
+        @Test
+        void getPurchaseAmount() {
+            // given
+            InputView inputView = new InputView();
+            String input = "1";
+            int expected = Integer.parseInt(input);
+            System.setIn(createUserInput(input));
+
+            // when
+            int result = inputView.getBonusNumber();
+
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
+
+        @DisplayName("[실패] 입력값이 공백이면 예외가 발생한다.")
+        @Test
+        void exception_Blank() {
+            // given
+            InputView inputView = new InputView();
+            String input = "    ";
+            System.setIn(createUserInput(input));
+
+            // when  then
+            assertThatThrownBy(inputView::getBonusNumber)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(BLANK_INPUT.toString());
+        }
+
+        @DisplayName("[실패] 입력값이 숫자가 아니면 예외가 발생한다.")
+        @Test
+        void exception_Not_Digit_Input() {
+            // given
+            InputView inputView = new InputView();
+            String input = "+01a";
+            System.setIn(createUserInput(input));
+
+            // when  then
+            assertThatThrownBy(inputView::getBonusNumber)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INPUT_NOT_DIGIT.toString());
+        }
+
+        @DisplayName("[실패] 입력값이 지정된 형식의 최댓값보다 크면 예외가 발생한다.")
+        @Test
+        void exception_Exceeded_Maximum_Type_Value() {
+            // given
+            InputView inputView = new InputView();
+            String input = "9223372036854775808";
+            System.setIn(createUserInput(input));
+
+            // when  then
+            assertThatThrownBy(inputView::getBonusNumber)
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(EXCEEDED_MAXIMUM_NUMBER_FORMAT.toString());
         }

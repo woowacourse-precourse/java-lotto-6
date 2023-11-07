f6aed5dc4600312e30ba4af92fb8e44e5dcd3de0
pgd <rudeh1253@gmail.com>
2023-11-08 04:39:03 +0900

    feat: 출력에 관련된 기능 구현

---
+++ src/main/java/lotto/io/OutputAdaptor.java
@@ -0,0 +1,63 @@
+package lotto.io;
+
+import lotto.configure.DomainConfiguration;
+import lotto.domain.Lotto;
+import lotto.domain.Result;
+
+import java.util.Collections;
+import java.util.List;
+
+import static lotto.configure.OutputConfiguration.*;
+
+public class OutputAdaptor {
+
+    public static void displayMessageToPromptToInputPurchasePrice() {
+        Printer.print(MESSAGE_TO_PROMPT_TO_INPUT_PURCHASE_PRICE + LINE_BRAKING);
+    }
+
+    public static void displayLottoNumbers(List<Lotto> lottos) {
+        Printer.print(LINE_BRAKING + lottos.size() + NOTIFICATION_FORMAT_OF_NUM_OF_LOTTO + LINE_BRAKING);
+        lottos.forEach((lotto -> Printer.print(lottos.toString() + LINE_BRAKING)));
+    }
+
+    public static void displayMessageToPromptToInputWinningNumbers() {
+        Printer.print(LINE_BRAKING + MESSAGE_TO_PROMPT_TO_INPUT_WINNING_NUMBERS + LINE_BRAKING);
+    }
+
+    public static void displayMessageToPromptToInputBonusNumber() {
+        Printer.print(LINE_BRAKING + MESSAGE_TO_PROMPT_TO_INPUT_BONUS_NUMBER + LINE_BRAKING);
+    }
+
+    public static void displayResult(Result result) {
+        Printer.print(LINE_BRAKING + WINNING_SUM + LINE_BRAKING);
+        List<Integer> matches = DomainConfiguration.RANK_PER_NUM_OF_MATCHES.keySet().stream().sorted().toList();
+        for (Integer match : matches) {
+            Integer rank = DomainConfiguration.RANK_PER_NUM_OF_MATCHES.get(match);
+            if (rank.equals(DomainConfiguration.RANK_FOR_CHECK_BONUS_NUMBER)) {
+                int rewardWithoutBonusMatch = DomainConfiguration.REWARD_PER_RANK.get(rank + 1);
+                int rewardWithBonusMatch = DomainConfiguration.REWARD_PER_RANK.get(rank);
+                Printer.print(
+                        match + NUM_OF_MATCHES + " (" + formatReward(rewardWithoutBonusMatch)
+                        + WON + ") - " + result.getCount(rank + 1) + COUNT
+                );
+                Printer.print(
+                        match + NUM_OF_MATCHES + ", " + MATCH_BONUS_NUMBER
+                        + " (" + formatReward(rewardWithBonusMatch)
+                        + WON + ") - " + result.getCount(rank) + COUNT
+                );
+            }
+        }
+    }
+
+    private static String formatReward(int reward) {
+        String asString = String.valueOf(reward);
+        StringBuilder sb = new StringBuilder();
+        int i;
+        for (i = asString.length() - WHERE_TO_SEPARATOR_IN_MONEY; i >= 0; i -= WHERE_TO_SEPARATOR_IN_MONEY) {
+            sb.insert(0, asString.substring(i, i + WHERE_TO_SEPARATOR_IN_MONEY));
+            sb.insert(0, ",");
+        }
+        sb.insert(0, asString.substring(0, i + WHERE_TO_SEPARATOR_IN_MONEY));
+        return sb.toString();
+    }
+}
---
+++ src/main/java/lotto/io/Printer.java
@@ -0,0 +1,8 @@
+package lotto.io;
+
+public class Printer {
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+}

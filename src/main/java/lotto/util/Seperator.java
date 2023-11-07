5932feda92c7e019dc3f1648913f93881276f07a
hyeonjilee <skylar12200@gmail.com>
2023-11-07 14:22:11 +0900

    refactor(Seperator) : 구분자 상수 모음 클래스를 enum에서 일반 클래스로 수정

--- src/main/java/lotto/util/Seperator.java
+++ src/main/java/lotto/util/Seperator.java
@@ -3,26 +3,13 @@ package lotto.util;
 import java.util.Arrays;
 import java.util.List;
 
-public enum Seperator {
-    WINNING_NUMBERS_SEPARATOR(",", "쉼표");
+public class Seperator {
+    public static final String WINNING_NUMBERS_SEPARATOR = ",";
 
-    private final String seperator;
-    private final String name;
-
-    Seperator(String seperator, String name) {
-        this.seperator = seperator;
-        this.name = name;
-    }
-
-    public List<String> split(String input) {
+    public static List<String> split(String seperator, String input) {
         return Arrays.asList(input.split(seperator));
     }
 
-    public String getSeperator() {
-        return seperator;
-    }
-
-    public String getName() {
-        return name;
+    private Seperator() {
     }
 }
--- src/main/java/lotto/util/validator/WinningNumbersDtoValidator.java
+++ src/main/java/lotto/util/validator/WinningNumbersDtoValidator.java
@@ -2,13 +2,11 @@ package lotto.util.validator;
 
 import static lotto.util.Seperator.WINNING_NUMBERS_SEPARATOR;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import lotto.dto.WinningNumbersDto;
 
 public class WinningNumbersDtoValidator implements Validator {
+    public static final String WINNING_NUMBERS_IS_INVALID_FORMAT = "당첨 번호 입력 형식에 맞지 않습니다.";
+
     @Override
     public void validate(Object value) {
         validateNull((String) value);
@@ -29,32 +27,32 @@ public class WinningNumbersDtoValidator implements Validator {
     }
 
     private void validateRightSperator(String winningNumbers) {
-        if (!winningNumbers.contains(WINNING_NUMBERS_SEPARATOR.getSeperator())){
-            throw new IllegalArgumentException("당첨 번호 입력 형식에 맞지 않습니다.");
+        if (!winningNumbers.contains(WINNING_NUMBERS_SEPARATOR)){
+            throw new IllegalArgumentException(WINNING_NUMBERS_IS_INVALID_FORMAT);
         }
     }
 
     private void validateDoubleSeperator(String winningNumbers) {
         if (containsDoubleSeperator(winningNumbers)) {
-            throw new IllegalArgumentException("당첨 번호 입력 형식에 맞지 않습니다.");
+            throw new IllegalArgumentException(WINNING_NUMBERS_IS_INVALID_FORMAT);
         }
 
     }
 
     private boolean containsDoubleSeperator(String winningNumbers) {
-        String doubleSeperator = WINNING_NUMBERS_SEPARATOR.getSeperator().repeat(2);
+        String doubleSeperator = WINNING_NUMBERS_SEPARATOR.repeat(2);
         return winningNumbers.contains(doubleSeperator);
     }
 
     private void validateStartWord(String winningNumbers) {
-        if (winningNumbers.startsWith(WINNING_NUMBERS_SEPARATOR.getSeperator())) {
-            throw new IllegalArgumentException("당첨 번호 입력 형식에 맞지 않습니다.");
+        if (winningNumbers.startsWith(WINNING_NUMBERS_SEPARATOR)) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_IS_INVALID_FORMAT);
         }
     }
 
     private void validateEndWord(String winningNumbers) {
-        if (winningNumbers.endsWith(WINNING_NUMBERS_SEPARATOR.getSeperator())) {
-            throw new IllegalArgumentException("당첨 번호 입력 형식에 맞지 않습니다.");
+        if (winningNumbers.endsWith(WINNING_NUMBERS_SEPARATOR)) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_IS_INVALID_FORMAT);
         }
     }
 

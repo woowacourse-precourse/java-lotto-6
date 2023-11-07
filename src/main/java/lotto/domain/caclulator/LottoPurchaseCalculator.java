67b11e5604635d2d3052b66be941d9df0be422ee
xogns4909 <xogns4909@naver.com>
2023-11-07 11:18:12 +0900

    Refactor : 상수 변경 및 모듈화

--- src/main/java/lotto/domain/caclulator/LottoPurchaseCalculator.java
+++ src/main/java/lotto/domain/caclulator/LottoPurchaseCalculator.java
@@ -1,20 +1,31 @@
-package lotto.domain;
+package lotto.domain.caclulator;
+
+import lotto.domain.LottoConfig;
+import lotto.domain.model.Money;
 
 public class LottoPurchaseCalculator {
 
     public static int calculateLottoCount(int lottoPrice, Money money) {
 
         int amount = money.getAmount();
-        int zero = LottoConfig.ZERO.getValue();
 
-        if (lottoPrice <= zero) {
-            throw new IllegalArgumentException(LottoErrorMessages.INVALID_LOTTO_PRICE.getMessage());
-        }
-        if (amount % lottoPrice != zero) {
-            throw new IllegalArgumentException(
-                    LottoErrorMessages.NOT_DIVISIBLE_AMOUNT.getMessage());
-        }
+        validateLottoPrice(lottoPrice);
+        validateAmountDivisibility(amount, lottoPrice);
         return amount / lottoPrice;
+    }
+
+    private static void validateLottoPrice(int lottoPrice) {
+        if (lottoPrice <= LottoConfig.ZERO.getValue()) {
+            throw new IllegalArgumentException(
+                    LottoCalculatorErrorMessage.INVALID_LOTTO_PRICE.getMessage());
+        }
+    }
+
+    private static void validateAmountDivisibility(int amount, int lottoPrice) {
+        if (amount % lottoPrice != LottoConfig.ZERO.getValue()) {
+            throw new IllegalArgumentException(
+                    LottoCalculatorErrorMessage.NOT_DIVISIBLE_AMOUNT.getMessage());
+        }
     }
 
 }

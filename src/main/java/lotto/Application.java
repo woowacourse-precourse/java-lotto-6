98c4c327e51610c20d4fc11c9172d8750c49403b
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-08 07:54:16 +0900

    feat(lotto): 로또 게임 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+
+import lotto.controller.*;
+import lotto.domain.WinningChecker;
+import lotto.vo.BonusNumber;
+import lotto.vo.Lotto;
+import lotto.vo.WinningNumber;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        OrderController order = new OrderController();
+        int lottoNumber = order.getLottoNumber();
+
+        DeliverController deliver = new DeliverController(lottoNumber);
+        List<Lotto> lottoPackage = deliver.getLottoPackage();
+
+        SetWinningNumController setWinningNumController = new SetWinningNumController();
+        WinningNumber w = setWinningNumController.getWinningNumber();
+
+        SetBonusNumController setBonusNumController = new SetBonusNumController(w);
+        BonusNumber b = setBonusNumController.getBonusNumber();
+
+        WinningChecker checker = new WinningChecker(w,b, lottoPackage);
+        WinningController c = new WinningController(checker.getWinningResult(), order.getWastedMoney());
     }
 }

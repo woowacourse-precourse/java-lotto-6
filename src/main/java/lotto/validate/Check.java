12e49d92d4a77080fde8dea42247edb196ee45ed
lynjun97 <lynjun97@naver.com>
2023-11-07 20:59:38 +0900

    Refactor: 매개변수명 변경

--- src/main/java/lotto/validate/Check.java
+++ src/main/java/lotto/validate/Check.java
@@ -15,14 +15,14 @@ public class Check {
         }
     }
 
-    public static void bonusNumberCount(List<Integer> winningNumber) {
-        if (winningNumber.size() != 1) {
+    public static void bonusNumberCount(List<Integer> bonusNumber) {
+        if (bonusNumber.size() != 1) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_COUNT_ERROR.getMessage());
         }
     }
 
-    public static void rightRange(List<Integer> winningNumber) {
-        winningNumber.stream().filter(number -> number < Start_Number || number > Final_Number).forEach(number -> {
+    public static void rightRange(List<Integer> bonusNumber) {
+        bonusNumber.stream().filter(number -> number < Start_Number || number > Final_Number).forEach(number -> {
             throw new IllegalArgumentException(ErrorMessage.RANGE_ERROR.getMessage());
         });
     }

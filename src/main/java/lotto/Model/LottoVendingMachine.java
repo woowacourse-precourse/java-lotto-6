1d6142887a11f038e67f0bc4b7a8f609fce3a722
YejiGong <yejigong00@gmail.com>
2023-11-08 01:48:27 +0900

    Feat: 로또 판매 기계 구현

--- src/main/java/lotto/Model/LottoVendingMachine.java
+++ src/main/java/lotto/Model/LottoVendingMachine.java
@@ -1,4 +1,16 @@
 package lotto.Model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.Controller.LottoController;
+
 public class LottoVendingMachine {
+
+    public Lotto createValidLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(LottoMachine.LOTTO_NUMBER_MIN_RANGE,
+                LottoMachine.LOTTO_NUMBER_MAX_RANGE,
+                LottoController.LOTTO_NUMBER_AMOUNT);
+        return new Lotto(randomNumbers);
+    }
+
 }

727d40301b8e89d0536591624032aed374b49b01
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 20:38:06 +0900

    feat(const): 당첨 번호 및 보너스 번호 질문 메세지 추가

--- src/main/java/lotto/constant/GeneralMessage.java
+++ src/main/java/lotto/constant/GeneralMessage.java
@@ -2,7 +2,9 @@ package lotto.constant;
 
 public enum GeneralMessage {
     ASK_BUY_PRICE("구입금액을 입력해 주세요."),
-    NOTIFY_BUY_COUNT("%d개를 구매했습니다.");
+    NOTIFY_BUY_COUNT("%d개를 구매했습니다."),
+    ASK_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     private final String message;
 

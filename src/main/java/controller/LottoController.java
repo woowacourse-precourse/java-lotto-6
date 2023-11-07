cb303f4ddfd160865335718b61f48e9432ad8f98
daneng4 <daneng4@naver.com>
2023-11-07 23:21:59 +0900

    refactor: 메서드 호출 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,19 +1,43 @@
 package controller;
 
-import model.LottoNumber;
 import model.LottoService;
+import model.WinningNumber;
+import view.InputView;
+import view.OutputView;
 
 public class LottoController {
 
     LottoService lottoService = new LottoService();
-    public LottoNumber winningNumber = new LottoNumber();
+    WinningNumber winningNumber;
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
 
     public LottoController(){
         startLotto();
+        createWinningNumber();
+        matchLotto();
     }
 
     public void startLotto(){
-        lottoService.buyLotto();
+        // 컨트롤러에서 input, outputView로 요청하고
+        // model 로 넘겨주는 방식으로 리팩토링 할 것
+        outputView.printRequirelottoCost();
+        int lottoPaper = lottoService.buyLotto();
+        outputView.printNewLine();
+        outputView.printPublishedLotto(lottoPaper);
+        lottoService.getLottoNumber();
+    }
+
+    public void createWinningNumber(){
+        outputView.printRequireWinningNumber();
+        winningNumber = new WinningNumber(inputView.getWinningNumber());
+        outputView.printRequireBonusNumber();
+        winningNumber.setBonusNumber(inputView.getBonusNumber());
+    }
+
+    public void matchLotto(){
+        outputView.printMatchResult();
+
     }
 
 }

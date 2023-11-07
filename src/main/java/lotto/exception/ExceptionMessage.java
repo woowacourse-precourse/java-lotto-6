d30d474bf5daa2a5fa619a5002e2cc71bed7ee99
BonSik <rnqhstlr2297@gmail.com>
2023-11-07 20:59:01 +0900

    feat: 누락된 당첨 번호 중복 에러 메시지 추가

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     INVALID_PURCHASE_PRICE_UNIT("[ERROR] 로또 구입 금액은 1,000원 단위로 입력이 가능합니다."),
     INVALID_SIZE_LOTTO_NUMBER("[ERROR] 로또 번호의 개수는 6개만 가능합니다."),
     INVALID_RANGE_LOTTO_NUMBER("[ERROR] 로또 번호는 1~45까지 숫자만 가능합니다."),
-    DUPLICATION_LOTTO_NUMBER("[ERROR] 중복된 로또 번호는 사용할 수 없습니다.")
+    DUPLICATION_LOTTO_NUMBER("[ERROR] 중복된 로또 번호는 사용할 수 없습니다."),
+    DUPLICATION_BONUS_NUMBER("[ERROR] 로또 번호와 중복된 보너스 번호는 사용할 수 없습니다.")
     ;
     private String desc;
 

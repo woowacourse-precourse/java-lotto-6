59bf113d9a27b1682d38b7396e59d9618f1f5f54
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-07 17:55:10 +0900

    test: add test for string constructor

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -2,7 +2,6 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.Application;
-import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,23 +11,30 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest extends NsTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생.")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("입력한 로또 번호와 발행한 로또 번호가 틀린지 체크하는 테스트.")
+    @DisplayName("당첨 번호 생성 시 형식을 지키지 않으면 예외 발생.")
     @Test
-    void isMatch_test() {
+    void Lotto_constructor_test() {
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, 4, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 발행한 로또 번호 중 같은 번호 개수를 테스트.")
+    @Test
+    void countMatch_test() {
         assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6))
                 .countMatch(new Lotto(List.of(1, 2, 3, 4, 5, 6)))
         ).isEqualTo(6);

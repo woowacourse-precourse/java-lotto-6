07e70e644556321d9fb47557a4350a4a99bb769a
KimSangHyeok <tkdgur0906@naver.com>
2023-11-07 15:46:53 +0900

    refactor : 상수를 static 변수로 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -7,8 +7,11 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LottoGenerator {
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
     public Lotto generate() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, LOTTO_SIZE);
         lotto.sort(Comparator.naturalOrder());
         return new Lotto(lotto);
     }

519533dcc5ee7cc93c1bbc45610eb1ace90f8e57
acs0209 <annjae4676@naver.com>
2023-11-08 01:50:58 +0900

    feat: Lotto domain에 숫자 중복 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,31 @@
 package lotto.domain;
 
+import static lotto.validation.constant.WinningNumbersInputConstant.DUPLICATE_MESSAGE;
+import static lotto.validation.constant.WinningNumbersInputConstant.NUMBERS_COUNT_MESSAGE;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumberDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBERS_COUNT_MESSAGE.getMessage());
+        }
+    }
+
+    public void validateNumberDuplication(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (distinctNumbers.size() < numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_MESSAGE.getMessage());
         }
     }
 

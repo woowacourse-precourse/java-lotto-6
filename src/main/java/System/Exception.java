30bed88020b94f090ac194e2ac7efc0b8a81d90e
ppusda <ppusda1234@gmail.com>
2023-11-07 16:31:08 +0900

    feat: Exception 내용 추가

--- src/main/java/System/Exception.java
+++ src/main/java/System/Exception.java
@@ -1,6 +1,11 @@
 package System;
+
 public enum Exception {
-    LOTTO_PURCHASE_EXCEPTION("로또 구매 금액은 1,000원 단위로 입력해주세요.");
+    LOTTO_PURCHASE_EXCEPTION("로또 구매 금액은 1,000원 단위로 입력해주세요."),
+    RAFFLE_WRONG_INPUT_EXCEPTION("당첨 번호는 6개로 ,를 통해 구분하여 입력해주세요."),
+    RAFFLE_WRONG_NUMBER_EXCEPTION("당첨 번호는 1에서 45까지의 숫자 중 입력해주세요."),
+    RAFFLE_DUPLICATE_NUMBER_EXCEPTION("당첨 번호 중 중복 값이 없도록 입력해주세요."),
+    RAFFLE_DUPLICATE_BONUS_NUMBER_EXCEPTION("당첨 번호와 보너스 번호가 중복이 되지 않게 입력해주세요.");
 
     Exception(String message) {
         this.message = message;

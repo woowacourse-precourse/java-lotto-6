872211186ccc6c782722e53cb7a6276ac115e9d4
jiu6525 <jiu6525@daum.net>
2023-11-07 17:36:37 +0900

    feat : 당첨번호를 저장할 WinningLotto 레코드 생성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

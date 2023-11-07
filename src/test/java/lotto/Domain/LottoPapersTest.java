0679445ec7a88ef2d820855eae1606313b3ed635
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-07 17:44:33 +0900

    test(LottoPapersTest): @BeforeEach를 통해 사전 세팅 추가 후 기존 중복코드 삭제

--- src/test/java/lotto/Domain/LottoPapersTest.java
+++ src/test/java/lotto/Domain/LottoPapersTest.java
@@ -5,25 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoPapersTest {
 
+    private LottoPapers lottoPapers;
+    private static final int AMOUNT = 5;
+
+    @BeforeEach
+    void setLottoPapers(){
+        lottoPapers = new LottoPapers(AMOUNT);
+    }
+
     @Test
     @DisplayName("로또 티켓 수가 올바르게 생성되는지 확인")
     void shouldGenerateCorrectNumberOfLottoPapers() {
-        int amount = 5;
-        LottoPapers lottoPapers = new LottoPapers(amount);
-        assertEquals(amount, lottoPapers.getLottoPapers().size());
+        assertEquals(AMOUNT, lottoPapers.getLottoPapers().size());
     }
 
     @Test
     @DisplayName("로또 번호가 유효한 범위 내에 있는지 확인")
     void shouldGenerateValidLottoNumbers() {
-        int amount = 5;
-        LottoPapers lottoPapers = new LottoPapers(amount);
-
         for (Lotto lotto : lottoPapers.getLottoPapers()) {
             for (int number : lotto.getNumbers()) {
                 assertTrue(number >= 1 && number <= 45);
@@ -34,9 +38,6 @@ class LottoPapersTest {
     @Test
     @DisplayName("로또 번호가 올바른 갯수만큼 있는지 확인")
     void shouldGenerateUniqueAndCorrectNumberOfLottoNumbers() {
-        int amount = 5;
-        LottoPapers lottoPapers = new LottoPapers(amount);
-
         for (Lotto lotto : lottoPapers.getLottoPapers()) {
             assertEquals(6, lotto.getNumbers().size());
         }
@@ -45,9 +46,6 @@ class LottoPapersTest {
     @Test
     @DisplayName("로또 번호가 오름차순으로 정렬되어 있는지 확인")
     void shouldGenerateLottoNumbersInAscendingOrder() {
-        int amount = 5;
-        LottoPapers lottoPapers = new LottoPapers(amount);
-
         for (Lotto lotto : lottoPapers.getLottoPapers()) {
             List<Integer> sortedNumbers = new ArrayList<>(lotto.getNumbers());
             Collections.sort(sortedNumbers);

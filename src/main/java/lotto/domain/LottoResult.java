9023ef741d68cd3b5993a1563c0d36d61ba4bdd2
jihwankim128 <jihwan1535@nate.com>
2023-11-07 12:53:21 +0900

    feat(lotto): 당첨번호에 대한 각 로또번호가 몇 등을 했는지 계산하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,4 +1,33 @@
 package lotto.domain;
 
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
 public class LottoResult {
+    private final int payment;
+    private long totalPrizeMoney;
+    final Map<LottoPrize, Integer> prizeResult;
+
+    public LottoResult(int payment) {
+        this.payment = payment;
+        this.totalPrizeMoney = 0;
+        this.prizeResult = new EnumMap<LottoPrize, Integer>(LottoPrize.class);
+        for (LottoPrize prize : LottoPrize.values()) {
+            prizeResult.put(prize, 0);
+        }
+    }
+
+    public Map<LottoPrize, Integer> getPrizeResult() {
+        return prizeResult;
+    }
+
+    public void calculateWinningResult(WinningLotto winningLotto, List<Lotto> lottos) {
+        for(Lotto lotto : lottos) {
+            int matchCount = winningLotto.lottoNumbersMatch(lotto);
+            boolean matchBonus = winningLotto.lottoBonusNumberMatch(lotto);
+            LottoPrize prize = LottoPrize.valueOf(matchCount, matchBonus);
+            prizeResult.put(prize, prizeResult.get(prize) + 1);
+        }
+    }
 }

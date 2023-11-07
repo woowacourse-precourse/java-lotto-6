90ac947fb0d14ce9b357baaba8f7dcbbee503587
wda067 <mg6359@naver.com>
2023-11-08 05:00:27 +0900

    refactor(LottoGameServiceTest): 공복 추가

--- src/test/java/lotto/service/LottoGameServiceTest.java
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoGameServiceTest {
     LottoGameService lottoGameService;
+
     @BeforeEach
     void setUp() {
         lottoGameService = new LottoGameService();

3782c8984e875e31ef22543fe02981d9db29fae4
jiu6525 <jiu6525@daum.net>
2023-11-07 17:42:59 +0900

    feat : 구입 금액을 입력받는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String NEW_LINE = "\n";
+
+    public int purchaseAmountInput() {
+        String purchaseAmount;
+
+        do {
+            System.out.println(PURCHASE_AMOUNT_MESSAGE);
+            purchaseAmount = Console.readLine().trim();
+        } while (false);
+        {
+            System.out.println(NEW_LINE);
+            return Integer.parseInt(purchaseAmount);
+        }
+    }
 }

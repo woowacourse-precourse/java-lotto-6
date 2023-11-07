20bd948968d7c6bcf62862ecb3cfeb63f4100d6b
eunjin <12191804@inha.edu>
2023-11-07 23:22:15 +0900

    fix: 예외문 조건에 맞게 수정

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -11,33 +11,52 @@ public class InputView {
         System.out.println("구매 금액을 입력해주세요.");
         while (true) {
             try {
-                int amount = scanner.nextInt();
-                scanner.nextLine();
+                String input = scanner.nextLine();
+                int amount = Integer.parseInt(input);
+                if (amount <= 0) {
+                    throw new IllegalArgumentException("[ERROR] 입력 금액은 양수여야 합니다.");
+                }
                 if (amount % 1000 != 0) {
                     throw new IllegalArgumentException("[ERROR] 입력 금액은 1,000원 단위여야 합니다.");
                 }
                 return amount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자 형태의 금액을 입력해주세요.");
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
-                scanner.nextLine();
             }
         }
     }
 
+
     public static List<Integer> getWinningNumbers() {
-        System.out.println("\n당첨 번호 6개를 입력해주세요.");
-        String line = scanner.nextLine();
-        String[] parts = line.split(",");
-        List<Integer> winningNumbers = new ArrayList<>();
-        for (String part : parts) {
-            winningNumbers.add(Integer.parseInt(part));
+        while (true) {
+            System.out.println("\n당첨 번호를 입력해주세요.");
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            try {
+                List<Integer> winningNumbers = new ArrayList<>();
+                for (String part : parts) {
+                    int number = Integer.parseInt(part.trim());
+                    winningNumbers.add(number);
+                }
+                return winningNumbers;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 로또 번호는 정수여야 합니다.");
+            }
         }
-        return winningNumbers;
     }
 
     public static int getBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해주세요.");
-        return scanner.nextInt();
+        while(true){
+            System.out.println("\n보너스 번호를 입력해주세요.");
+            try {
+                int bonusNumber = Integer.parseInt(scanner.nextLine().trim());
+                return bonusNumber;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 정수여야 합니다.");
+            }
+        }
     }
 }
 

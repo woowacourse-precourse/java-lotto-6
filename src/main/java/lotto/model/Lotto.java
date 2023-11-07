df7ec329d955cd462e62ed6ddb954781a145f610
Yerinnnn <shinyerin0216@gmail.com>
2023-11-07 15:24:52 +0900

    Refactor: Rank와 관련된 코드를 Rank enum으로 옮김

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    // TODO: 추가 기능 구현
+}
---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+public enum Rank {
+
+    FIRST, SECOND, THIRD, FOURTH, FIFTH, LOOSE
+
+
+//        FIRST("6개 일치 (2,000,000,000원) - "),
+//        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+//        THIRD("5개 일치 (1,500,000원) - "),
+//        FOURTH("4개 일치 (50,000원) - "),
+//        FIFTH("3개 일치 (5,000원) - "),
+//        LOOSE("");
+//        final private String rank;
+//
+//        LottoRank(String rank) {
+//            this.rank = rank;
+//        }
+}

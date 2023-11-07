6679ce166f22f9ae4bc0f873245702fb1c410a65
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-07 12:58:44 +0900

    feat(UserBuyingException) : validIsNumber 메서드 생성

--- src/main/java/lotto/exception/UserBuyingException.java
+++ src/main/java/lotto/exception/UserBuyingException.java
@@ -1,8 +1,17 @@
 package lotto.exception;
 
+import lotto.Constants;
+
 public class UserBuyingException {
     public void validateBuyingException(String buyingPrice) {
         validIsNumber(buyingPrice);
         validPrice(buyingPrice);
+    }
+
+    public void validIsNumber(String pay) {
+        int buyingPrice = Integer.parseInt(pay);
+        if (buyingPrice % Constants.LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(Constants.ERROR + "구매는 1,000원 단위로 가능합니다");
+        }
     }
 }

cec931fadc2d3d2b953ba519282e92e65b2e55fd
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 03:16:07 +0900

    feat: add LottoConstant
    - RANGE_START
    - RANGE_END
    - LOTTO_SIZE

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -6,6 +6,9 @@ public enum LottoConstant {
     FOUR_WINNINGS(50000),
     FIVE_WINNINGS(1500000),
     FIVE_BONUS_WINNINGS(30000000),
+    RANGE_START(1),
+    RANGE_END(45),
+    LOTTO_SIZE(6),
     SIX_WINNINGS(2000000000);
 
     private final int number;
@@ -14,7 +17,7 @@ public enum LottoConstant {
         this.number = number;
     }
 
-    public int getLottoConstant() {
+    public int getNumber() {
         return number;
     }
 

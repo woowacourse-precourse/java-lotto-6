4caaa0a6e24bfad5f9a2c93278e5899e4b92ad11
sgo722 <sgo722@naver.com>
2023-11-07 19:53:24 +0900

    refactor(LottoController) : 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,9 +3,7 @@ package lotto.controller;
 import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-
 import java.util.LinkedHashMap;
-import java.util.List;
 
 public class LottoController {
 
@@ -14,6 +12,17 @@ public class LottoController {
     private static BonusNumber bonusNumber;
 
     public void start(){
+        inputPurchaseAmount();
+        purchaseLotto();
+        inputWinningNumber();
+        inputBonusNumber();
+        Result result = checkUserNumber();
+        printResult(result);
+    }
+
+
+
+    private void inputPurchaseAmount() {
         OutputView.printRequestPurchaseAmount();
         while(true) {
             try {
@@ -24,10 +33,14 @@ public class LottoController {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private void purchaseLotto() {
         OutputView.printLottoQuantity(user.getLottoQuantity());
         OutputView.printLottoNumber(user.getLottos());
+    }
 
-
+    private void inputWinningNumber() {
         OutputView.printRequestWinnerNumberMessage();
         while(true){
             try{
@@ -37,7 +50,9 @@ public class LottoController {
                 System.out.println(e.getMessage());
             }
         }
+    }
 
+    private void inputBonusNumber() {
         OutputView.printRequestBonusNumberMessage();
         while(true){
             try{
@@ -47,11 +62,17 @@ public class LottoController {
                 System.out.println(e.getMessage());
             }
         }
+    }
 
+    private Result checkUserNumber() {
         Result result = new Result(new LinkedHashMap<>());
         for(Lotto lotto : user.getLottos()) {
             result.compare(lotto.getNumbers(), winningNumber.getWinningNumber(), bonusNumber.getBonusNumber());
         }
+        return result;
+    }
+
+    private void printResult(Result result) {
         OutputView.printResult(result);
         OutputView.printProfitRate(result.getProfitRate(user.getPurchaseAmount()));
     }

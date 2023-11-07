e9632171b720f73d38f34632fd934e34cc289560
HYOAN <aspqll@naver.com>
2023-11-08 00:28:33 +0900

    feat(lotto): 입력된 구입금액만큼 로또티켓을 생성하는 기능 구현

--- src/main/java/lotto/domain/LottoTicketGenerator.java
+++ src/main/java/lotto/domain/LottoTicketGenerator.java
@@ -1,16 +1,43 @@
 package lotto.domain;
 
-import java.lang.reflect.Array;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.LottoTicket;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTicketGenerator {
+
     public LottoTicketGenerator() {
     }
-    public int calculateLottoTicketCount(int purchaseAmount) {
-        return 0;
+
+    public List<LottoTicket> generateLottoTicket(int purchaseAmount) { // 로또 생성
+
+        List<LottoTicket> purchasedLottoTickets = new ArrayList<>(); // 구매한 로또들
+        List<Integer> lottoNumbers = new ArrayList<>(); // 한 장의 로또에 들어있는 번호들
+        int ticketCount = calculateLottoTicketCount(purchaseAmount); // 구매한 로또 개수
+
+        for (int conut = 0; conut < ticketCount; conut++) {
+            lottoNumbers = generateLottoNumbers();
+            purchasedLottoTickets.add(new LottoTicket(lottoNumbers));
+        }
+        return purchasedLottoTickets;
     }
-    public List<Integer> generateLottoNumbers(int lottoTicketCount) {
-        return new ArrayList<>();
+
+    public List<Integer> generateLottoNumbers() { // 로또 번호 생성
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
+
+    public int calculateLottoTicketCount(int purchaseAmount) { // 생성할 로또 개수 계산
+
+        return purchaseAmount / 1000;
+    }
+
 }

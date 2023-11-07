4afe449a1bda169b7bd8c342f962d2289e0f5d0c
정재웅 <jweidon0821@gmail.com>
2023-11-08 02:30:09 +0900

    feat(view): 횟수 메세지 출력 OutputView 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,9 +4,14 @@ public class OutputView {
     public static final String askMoneyMessage = "구입금액을 입력해 주세요.";
     public static final String askWinningNumbersMessage = "당첨 번호를 입력해 주세요.";
     public static final String askBonusNumberMessage = "보너스 번호를 입력해 주세요.";
+    public static final String noticeTrialMessage = "개를 구매했습니다.";
 
     public static void printAskMoneyMessage() {
         System.out.println(askMoneyMessage);
+    }
+
+    public static void printNoticeTrialMessage(int trial) {
+        System.out.println(trial + noticeTrialMessage);
     }
 
     public static void printAskWinningNumbersMessage() {

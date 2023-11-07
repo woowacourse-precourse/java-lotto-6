99668bac02f97710c69fab4e51b410030d9d8ee4
donghoony <aru0504@naver.com>
2023-11-08 01:51:34 +0900

    refactor(LottoBall): 싱글톤 구현에 따른 equals, hashcode 불필요 줄 삭제

--- src/main/java/lotto/domain/LottoBall.java
+++ src/main/java/lotto/domain/LottoBall.java
@@ -39,23 +39,4 @@ public class LottoBall {
             throw new ArgumentRangeOutOfBoundsException();
         }
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        LottoBall lottoBall = (LottoBall) o;
-
-        return number == lottoBall.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return number;
-    }
 }

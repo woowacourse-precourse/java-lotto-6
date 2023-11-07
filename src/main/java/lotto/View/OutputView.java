c2c6333cecb3a930cfc4a036ae520985b265673d
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 18:40:34 +0900

    refactor: 발행된 랜덤로또를 출력하는 기능의 로직을 분리

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -6,24 +6,28 @@ import lotto.Model.Rank;
 import lotto.Model.Result;
 
 public class OutputView {
+    private static final String COUNTING_OF_PURCHASED_RANDOM_LOTTOS = "%d개를 구매했습니다.\n";
+    public static void printCounting(RandomLottos randomLottos) {
+        System.out.printf(COUNTING_OF_PURCHASED_RANDOM_LOTTOS, randomLottos.getCounting());
+    }
 
-    public static void printRandomLottosNumberAndCounting(RandomLottos lottos) {
-        System.out.println(lottos.getCounting() + "개를 구매했습니다.");
-        lottos.getRandomLottos().forEach(System.out::println);
+    public static void printNumbers(RandomLottos randomLottos) {
+        randomLottos.getRandomLottos().forEach(System.out::println);
     }
 
     public static void printWinningResult(Result result) {
-        Map<Rank, Integer> results = result.getRankAndCounting();
+        newLine();
         System.out.println("당첨 통계");
         System.out.println("---");
-        for (Rank rank : results.keySet()) {
-            if(rank.compareTo(Rank.NONE) == 0) continue;
-            System.out.println(Rank.getMessage(rank) + " - " + results.get(rank) + "개");
+        Map<Rank, Integer> winningResults = result.getWinningRankAndCounting();
+        for (Rank winningRank : winningResults.keySet()) {
+            if(isNot(winningRank)) continue;
+            System.out.println(Rank.getMessage(winningRank) + " - " + winningResults.get(winningRank) + "개");
         }
     }
 
     public static void printWinningRate(double winningRate) {
-        System.out.println("총 수익률은 " + winningRate+"%입니다.");
+        System.out.println("총 수익률은 " + winningRate + "%입니다.");
     }
 
     public static void newLine() {
@@ -32,6 +36,10 @@ public class OutputView {
 
     public static void printErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
+    }
+
+    private static boolean isNot(Rank winningRank) {
+        return winningRank.compareTo(Rank.NONE) == 0;
     }
 }
 

4f4c1e6d5e9a4a9bb141e5200e1ac75ae4a0ac29
kimdongyeon <do121712@naver.com>
2023-11-07 16:41:09 +0900

    feat : 로또 일치 갯수와 금액에 따라 가각 ENUM 타입 생성, 로또 일치 갯수를 통해 ENUM 타입 반환 메소드 생성

--- src/main/java/lotto/Statistic.java
+++ src/main/java/lotto/Statistic.java
@@ -3,32 +3,31 @@ package lotto;
 import java.util.Arrays;
 
 public enum Statistic {
-    CORRECT_THREE(3, 5000),
-    CORRECT_FOUR(4, 50_000),
-    CORRECT_FIVE(5, 1_500_000),
-    CORRECT_FIVE_WITH_BONUS(5, 3_000_000),
-    CORRECT_SIX(6, 2_000_000_000);
+    MATCH_THREE(3, 5_000),
+    MATCH_FOUR(4, 50_000),
+    MATCH_FIVE(5, 1_500_000),
+    MATCH_FIVE_WITH_BONUS(5, 3_000_000),
+    MATCH_SIX(6, 2_000_000_000);
 
-    private int correctCount;
+    private int matchCount;
     private int prize;
 
-    Statistic(int correctNum, int prize) {
-        this.correctCount = correctNum;
+    Statistic(int matchCount, int prize) {
+        this.matchCount = matchCount;
         this.prize = prize;
     }
 
-    public Statistic getResult(int correctCount){
+    public static Statistic getStatistic(int matchCount){
         return Arrays.stream(values())
-                .filter(s -> s.correctCount == correctCount)
+                .filter(s -> s.matchCount == matchCount)
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(() -> new IllegalArgumentException("3개 이상 일치 하지 않음"));
     }
 
-    public int getCorrectCount() {
-        return correctCount;
+    public int getMatchCount() {
+        return matchCount;
     }
     public int getPrize() {
         return prize;
     }
-
 }

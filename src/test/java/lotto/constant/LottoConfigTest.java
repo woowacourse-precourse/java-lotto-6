87c7c3f83dbef464badc672f093e29d2f7ac2b76
tfer2442 <tfer2442@gmail.com>
2023-11-08 02:10:09 +0900

    test(LottoConfigTest): Enum 값 테스트 구현

---
+++ src/test/java/lotto/constant/LottoConfigTest.java
@@ -0,0 +1,33 @@
+package lotto.constant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class LottoConfigTest {
+    @DisplayName("PURCHASE_PRICE 값 테스트")
+    @Test
+    public void testPurchasePriceValue() {
+        assertThat(LottoConfig.PURCHASE_PRICE.getValue()).isEqualTo(1000);
+    }
+
+    @DisplayName("MIN_LOTTO_NUMBER 값 테스트")
+    @Test
+    public void testMinLottoNumberValue() {
+        assertThat(LottoConfig.MIN_LOTTO_NUMBER.getValue()).isEqualTo(1);
+    }
+
+    @DisplayName("MAX_LOTTO_NUMBER 값 테스트")
+    @Test
+    public void testMaxLottoNumberValue() {
+        assertThat(LottoConfig.MAX_LOTTO_NUMBER.getValue()).isEqualTo(45);
+    }
+
+    @DisplayName("TOTAL_CHOICE_NUMBER 값 테스트")
+    @Test
+    public void testTotalChoiceNumberValue() {
+        assertThat(LottoConfig.TOTAL_CHOICE_NUMBER.getValue()).isEqualTo(6);
+    }
+}

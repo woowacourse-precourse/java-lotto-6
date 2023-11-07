1d9bf2f0afd80c749436337ff2cdac31515b0457
MyStraw <kikongdosa@gmail.com>
2023-11-08 05:46:37 +0900

    Feat: 추가 구입 금액에 따른 로또 발행
    구입금액을 입력받아 로또를 구입금액 만큼 생성하는 기능 구현. 게임의 첫 단계. 아직 오름차순 정렬은 안됨

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+
+        int howMuchPurchase = Integer.parseInt(Console.readLine());
+        int howManyLotto = howMuchPurchase / 1000;
+
+        List<Lotto> lottoPapers = generateLottoPaper(howManyLotto);
+
+        for (Lotto lotto : lottoPapers) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    private static List<Lotto> generateLottoPaper(int howManyLotto) {
+        List<Lotto> lottoPapers = new ArrayList<>();
+        for (int i = 0; i < howManyLotto; i++) {
+            lottoPapers.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return lottoPapers;
     }
 }

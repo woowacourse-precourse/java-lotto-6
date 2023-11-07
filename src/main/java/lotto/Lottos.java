691d074c9c8fd673f657b9a28fad512811da2381
JiEung2 <https://github.com/JiEung2>
2023-11-08 00:45:23 +0900

    Feat: checkBonusNumber, getWinningStatistics 구현
    보너스 번호를 맞췄는지 체크하는 메서드와 Lotto 당첨 기준과 얼마나 맞는지 확인하는 메서드 추가

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.model.WinningStatistics;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -15,4 +20,20 @@ public class Lottos {
                 .map(lotto -> lotto.checkNumbers(winningNumber))
                 .collect(Collectors.toList());
     }
+
+    public List<Boolean> checkBonusNumber(int bonusNumber) {
+        return lottos.stream()
+                .map(lotto -> lotto.checkBonusNumber(bonusNumber))
+                .collect(Collectors.toList());
+    }
+
+    public Map<WinningStatistics, Long> getWinningStatistics(List<Integer> winningNumber, int bonusNumber) {
+        List<Integer> matchingCounts = checkWinning(winningNumber);
+        List<Boolean> containBonusNumber = checkBonusNumber(bonusNumber);
+        Map<WinningStatistics, Integer> result = new HashMap<>();
+        return IntStream.range(0, matchingCounts.size())
+                .mapToObj(i -> WinningStatistics.getStatistic(matchingCounts.get(i), containBonusNumber.get(i)))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
 }

de6d03fa5a0701d909dc1c64f88cdb9b15d0de7e
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 15:56:47 +0900

    feat(Lotto): 로또 번호가 1~45 사이가 아닌 경우를 검증하는 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.util.Validator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +12,6 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        numbers.forEach(Validator::validateInputRange);
     }
-
-    // TODO: 추가 기능 구현
 }

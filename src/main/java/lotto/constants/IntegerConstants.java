0a6a5084981617d54e3a3cc5e09d61375760a64b
cloudchamb3r <jizon0123@protonmail.com>
2023-11-07 10:22:37 +0900

    feat. match constants

--- src/main/java/lotto/constants/IntegerConstants.java
+++ src/main/java/lotto/constants/IntegerConstants.java
@@ -5,6 +5,10 @@ public class IntegerConstants {
     public final static int LOTTO_RANGE_START = 1;
     public final static int LOTTO_RANGE_END = 45;
     public final static int LOTTO_NUMBERS_COUNT = 6;
+    public final static long MATCH_THREE = 3;
+    public final static long MATCH_FOUR = 4;
+    public final static long MATCH_FIVE = 5;
+    public final static long MATCH_SIX = 6;
     public final static long MATCH_THREE_REWARD = 5000;
     public final static long MATCH_FOUR_REWARD = 50000;
     public final static long MATCH_FIVE_REWARD = 1500000;

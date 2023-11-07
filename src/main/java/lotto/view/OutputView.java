52df2eb7016bcc9c5851d4a19ddf78fd6c9deb93
juns1s <juns1s@naver.com>
2023-11-07 16:09:04 +0900

    feat: OutputView 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,46 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.Map;
+import lotto.dto.ResultDto;
+import lotto.embedded.LottoResult;
+import lotto.message.OutputMessage;
+import lotto.model.Lotto;
+
+public class OutputView {
+
+    public void printTotalPriceAsk() {
+        System.out.println(OutputMessage.TOTAL_PRICE);
+    }
+
+    public void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + OutputMessage.LOTTO_COUNT.toString());
+    }
+
+    public void printLottoList(List<Lotto> lottoList) {
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto.toString());
+        }
+    }
+
+    public void printWinningNumberAsk() {
+        System.out.println(OutputMessage.WINNING_NUMBER);
+    }
+
+    public void printBonusNumberAsk() {
+        System.out.println(OutputMessage.BONUS_NUMBER);
+    }
+
+    public void printResult(ResultDto totalResult) {
+        LottoResult[] values = LottoResult.values();
+        Map<LottoResult, Integer> resultMap = totalResult.resultMap();
+
+        System.out.println(OutputMessage.RESULT_SUMMARY);
+
+        for (LottoResult result : values) {
+            System.out.println(result.getDescription() + " - " + resultMap.getOrDefault(result, 0) + "개");
+        }
+
+        System.out.println("총 수익률은 " + totalResult.yield() + "%입니다.");
+    }
+}

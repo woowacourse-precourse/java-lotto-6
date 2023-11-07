baf55372d0e0d477922a0a315bb1d6894e1e0ffd
ztng123 <dpwl991102@gmail.com>
2023-11-08 03:58:01 +0900

    사용자 입력
    당첨 번호 뽑기

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,2 +1,13 @@
-package lotto;public class WinningLotto {
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = new Lotto(winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
 }

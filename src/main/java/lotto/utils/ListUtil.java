de7debdfe3f8c3499024c7f63f32bac30f137081
mingtorr <mingtorr@nave.com>
2023-11-08 03:00:27 +0900

    fix: 유틸리티 메소드 파라미터Integer 로 수정
    List<Integer>로 변경
    public 선언으로 변경

--- src/main/java/lotto/utils/ListUtil.java
+++ src/main/java/lotto/utils/ListUtil.java
@@ -8,18 +8,18 @@ import java.util.Objects;
 import java.util.Set;
 
 public class ListUtil {
-    private static void checkDuplicateValue(List<Objects> numbers) {
-        Set<Objects> set = new HashSet<>(numbers);
+    public static void checkDuplicateValue(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
         if(numbers.size() != set.size()) {
             throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_DUPLICATED_VALUE.getData());
         }
     }
-    private static void checkLength(List<Objects> numbers) {
+    public static void checkLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_LENGTH.getData());
         }
     }
-    private static void checkRange(List<Integer> numbers) {
+    public static void checkRange(List<Integer> numbers) {
         for(int num : numbers) {
             if( num <0 || num >46)
                 throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_RANGE.getData());

c8f25c5068ffa7acb0248e35637f72da46065c2c
jaeyoungjang2 <lion85488@gmail.com>
2023-11-07 20:25:29 +0900

    feat: 서비스가 필요없어 보여서, controller에서 로직을 실행하도록 함

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,37 +1,35 @@
 package lotto.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lottery;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.Price;
 import lotto.domain.Ranks;
-import lotto.service.LottoService;
+import lotto.domain.LottoMachine;
 
 public class LottoController {
 
-    public List<Lotto> buyLottery(Price purchasePrice) {
-        LottoService lottoService = new LottoService();
-        return lottoService.buyLottery(purchasePrice);
+    private final LottoMachine lottoMachine;
+
+    public LottoController() {
+        this.lottoMachine = new LottoMachine();
     }
 
-    private void validatePurchasePriceRange(Integer purchasePrice) {
-        if (purchasePrice % 1_000 != 0) {
-            throw new IllegalArgumentException("로또는 1000원 단위로만 구매할 수 있습니다.");
+    public Lottery buyLottery(Price purchasePrice) {
+        List<Lotto> lottery = new ArrayList<>();
+        Integer numOfLotto = purchasePrice.numberLotteryAvailablePurchase();;
+
+        for (Integer i = 0; i < numOfLotto; i++) {
+            lottery.add(this.lottoMachine.buyLotto());
         }
+
+        return new Lottery(lottery);
     }
 
-    private void validatePurchasePriceType(String purchasePrice) {
-        try {
-            Integer.valueOf(purchasePrice);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("로또 금액은 숫자만 입력 가능합니다.");
-        }
-    }
-
-    public Ranks lottoResults(List<Lotto> lottery, List<LottoNumber> winningNumber, LottoNumber bonusNumber) {
-
-        LottoService lottoService = new LottoService();
-        return lottoService.lottoResults(lottery, winningNumber, bonusNumber);
+    public Ranks lottoResults(Lottery lottery, List<LottoNumber> winningNumber, LottoNumber bonusNumber) {
+        return new Ranks(lottery, winningNumber, bonusNumber);
     }
 
 

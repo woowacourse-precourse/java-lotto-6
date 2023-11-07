d7304332e5b228e3bac5e921442bcd7f374cd518
minisyu <iswater.kim@gmail.com>
2023-11-07 21:20:08 +0900

    style: LottoValidator 주석 제거

--- src/main/java/lotto/domain/validator/LottoValidator.java
+++ src/main/java/lotto/domain/validator/LottoValidator.java
@@ -9,14 +9,12 @@ public class LottoValidator {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
 
-    // 로또 번호가 6개인지 검증 -> Lotto 클래스
     public static void verifyNumbersSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBERS) {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
     }
 
-    // 6자리 숫자에 중복이 있는지 검증 -> Lotto 클래스
     public static void verifyNoDuplication(List<Integer> numbers) {
         int uniqueCount = getUniqueCount(numbers);
         if (uniqueCount != numbers.size()) {
@@ -28,7 +26,6 @@ public class LottoValidator {
         return (int) numbers.stream().distinct().count();
     }
 
-    // 로또 번호 범위 설정 -> Lotto 클래스
     public static void verifyLottoNumberRange(List<Integer> numbers) {
         if (isOutOfRange(numbers)) {
             throw new IllegalArgumentException("로또 번호는 1에서 45사이의 숫자만 가능합니다.");
@@ -39,14 +36,12 @@ public class LottoValidator {
         return numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
     }
 
-    // 로또 범위 설정 -> BonusNumber 클래스
     public static void verifyRange(int bonusNumber) {
         if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
             throw new IllegalArgumentException("보너스 번호는 1에서 45 사이에 있어야 합니다.");
         }
     }
 
-    // 보너스 번호가 당첨 번호와 중복되는지 검증 -> BonusNumber 클래스
     public static void verifyBonusIsNotContainWinningNumbers(Lotto winningNumbers, BonusNumber bonusNumber) {
         if (hasBonusNumber(winningNumbers, bonusNumber)) {
             throw new IllegalArgumentException("입력한 당첨번호와 동일한 숫자를 입력하면 안 됩니다.");

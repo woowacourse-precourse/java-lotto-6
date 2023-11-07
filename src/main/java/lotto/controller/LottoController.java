55ad8206d3d558aa9c4ac8d85c0a50920c7c4410
MinseokGo <rhalstjr1999@naver.com>
2023-11-07 16:12:04 +0900

    refactor($LottoController): 책임 분리 및 로직 수정
    기존에 객체 간의 결합도가 높은 문제를 객체 값 리턴을 통해 결합도를 낮추고 결과 도출에 대한 로직을 책임 분리하였습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -10,7 +10,6 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
-    private final LottoManager lottoManager;
     private final Validator validator;
     private final InputView inputView;
     private final OutputView outputView;
@@ -20,38 +19,18 @@ public class LottoController {
         this.validator = new Validator();
         this.inputView = InputView.getInstance();
         this.outputView = OutputView.getInstance();
-        this.lottoManager = inputPurchaseMoney();
         this.referee = referee;
     }
 
-    public LottoManager inputPurchaseMoney() {
+    public int inputPurchaseMoney() {
         String input;
         do {
             input = Console.readLine();
         } while (validator.validateMoneyInput(input));
-        final int money = Integer.parseInt(input);
-        final LottoManager lottoManager = new LottoManager(money);
-        outputView.printCreateLottoNumbers(lottoManager.getLottos());
-        return lottoManager;
+        return Integer.parseInt(input);
     }
 
-    public void createWinningLotto() {
-        final List<Integer> winningNumbers = inputWinningLottoNumbers();
-        final int bonusNumber = inputWinningLottoBonusNumber(winningNumbers);
-        final WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
-        lottoManager.setWinningLotto(winningLotto);
-    }
-
-    public void getLottoResult() {
-        final List<Lotto> lottos = lottoManager.getLottos();
-        final WinningLotto winningLotto = lottoManager.getWinningLotto();
-        referee.judge(lottos, winningLotto);
-
-        final double rateOfReturn = referee.getRateOfReturn(lottos.size() * 1000);
-        outputView.printResult(referee, rateOfReturn);
-    }
-
-    private List<Integer> inputWinningLottoNumbers() {
+    public List<Integer> inputWinningLottoNumbers() {
         List<Integer> winningNumbers;
         do {
             final String inputNumbers = Console.readLine().trim();
@@ -63,12 +42,18 @@ public class LottoController {
         return winningNumbers;
     }
 
-    private int inputWinningLottoBonusNumber(final List<Integer> winningNumbers) {
-        inputView.printWinningBonusNumberMessage();
+    public int inputWinningLottoBonusNumber(final List<Integer> winningNumbers) {
         String input;
         do {
             input = Console.readLine();
         } while (validator.validateWinningBonusNumber(winningNumbers, input));
         return Integer.parseInt(input);
+    }
+
+    public void getLottoResult(final List<Lotto> lottos, final WinningLotto winningLotto) {
+        referee.judge(lottos, winningLotto);
+
+        final double rateOfReturn = referee.getRateOfReturn(lottos.size() * 1000);
+        outputView.printResult(referee, rateOfReturn);
     }
 }

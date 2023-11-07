e38b52fc937b76974d289f3fb1a7c1b58e75c8dc
kitewater <97283971+kitewater@users.noreply.github.com>
2023-11-08 02:36:32 +0900

    feat: 입력받을때 예외 처리 기능 추가

--- src/main/java/validate/Validator.java
+++ src/main/java/validate/Validator.java
@@ -3,8 +3,8 @@ package validate;
 public class Validator {
     public enum ErrorMessage {
 
-        ERROR1("[ERROR] 숫자를 입력해주세요");
-
+        ERROR1("[ERROR] 숫자를 입력해주세요"),
+        ERROR2("[ERROR] 가격을 천 단위로 입력해주세요");
         private String message;
 
         ErrorMessage(String message) {
@@ -15,10 +15,35 @@ public class Validator {
     public int validatePrice(String price){
         int my_price;
         try {
-            my_price = Integer.parseInt(price);
-        }catch (Exception e){
+            checkPriceNull(price);
+            my_price = checkPriceInteger(price);
+            checkPrice1000(my_price);
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(price);
+    }
+    public void checkPriceNull(String price){
+        if (price == null){
+            System.out.println(ErrorMessage.ERROR1);
+            throw new NullPointerException();
+        }
+    }
+
+    public int checkPriceInteger(String price){
+        try{
+            return Integer.parseInt(price);
+        }catch (NumberFormatException e){
             System.out.println(ErrorMessage.ERROR1);
             throw new IllegalArgumentException();
         }
     }
+
+    public void checkPrice1000(int price){
+        if(price%1000 != 0 ){
+            System.out.println(ErrorMessage.ERROR2);
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

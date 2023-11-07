40922adf37586e792d325471d7344f9ec100e54c
inpink <inpink@kakao.com>
2023-11-08 00:15:34 +0900

    feat: 로또 리스트를 직접 받거나, 자동 로또 리스트를 생성하는 정적 팩터리 메서드

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos create(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+    public static Lottos createAutomatic(ThousandUnitMoney purchaseAmount, int oneLottoPrice) {
+        int count = purchaseAmount.divide(oneLottoPrice);
+
+        return new Lottos(Stream.generate(() -> Lotto.createRandomSorted())
+                .limit(count)
+                .collect(Collectors.toList()));
+    }
+
+}

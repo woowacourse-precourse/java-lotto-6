50b850fbd467e131d8882308790adbff004c0ef9
Roista <Rosita57@gmail.com>
2023-11-07 23:25:56 +0900

    feat: 구매한 로또 번호와 당첨된 로또 번호의 개수를 확인하는 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -28,5 +30,12 @@ class LottoTest {
     void createLottoByLessSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 당첨_번호와_일치하는_번호의_개수_확인() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winNumber = new ArrayList<>(List.of(1, 2, 3, 7, 8, 9));
+        int equalNumber = lotto.lottoNumbersContains(winNumber);
+        assertThat(equalNumber).isEqualTo(3);
     }
 }

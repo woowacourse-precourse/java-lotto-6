ad5deea61395fb7fb6f7e8f3124b9df7a369ffcb
GaBaljaintheroom <junsu1222@naver.com>
2023-11-07 21:16:15 +0900

    test(LottoTest): 당첨 로또 저장 기능 확인

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("당첨 번호 객체에 대해")
@@ -16,7 +17,7 @@ class LottoTest {
 
     private static final String error = ErrorMessage.ERROR_TAG.toString();
 
-    static Stream<List<Integer>> winningNumbersSource() {
+    static Stream<List<Integer>> winningNumbersErrorSource() {
         return Stream.of(List.of(0, 2, 3, 99, 5, 6), List.of(55, 56, 57, 58, 59, 60));
     }
 
@@ -42,10 +43,24 @@ class LottoTest {
 
     @DisplayName("로또 번호가 1 부터 45 사이의 숫자가 아니면 예외가 발생한다.")
     @ParameterizedTest
-    @MethodSource("winningNumbersSource")
-    void When_SaveWinningNumbers_Then_(List<Integer> value) {
+    @MethodSource("winningNumbersErrorSource")
+    void When_SaveWinningNumbers_Then_ThrowException(List<Integer> value) {
         assertThatThrownBy(() -> Lotto.from(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(error);
     }
+
+    @DisplayName("리스트 타입의 숫자들 객체에 저장할 수 있다")
+    @Test
+    void When_CreateLotto_Then_NotEqual() {
+        //given
+        Lotto lotto = Lotto.from(List.of(1,2,3,4,5,6));
+
+        //when
+        List<Integer> lotto1Numbers = lotto.getNumbers();
+
+        //then
+        assertThat(lotto1Numbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
 }

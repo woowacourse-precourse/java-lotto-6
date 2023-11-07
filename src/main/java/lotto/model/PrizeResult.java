4d8656f7a7e3f2dfe34054884369e7cdf0e1b137
WooJJam <jaemin5548@naver.com>
2023-11-07 15:22:11 +0900

    [Feat] 로또 mathing 계산 기능 구현

--- src/main/java/lotto/model/PrizeResult.java
+++ src/main/java/lotto/model/PrizeResult.java
@@ -1,17 +1,42 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrizeResult {
 
-    List<Result> ranking = new ArrayList<>();
+    List<Prize> ranking = new ArrayList<>();
 
     public PrizeResult(List<Integer> lottoMatch, List<Boolean> hasBonusMatch) {
-
+        calculatePrizeRanking(lottoMatch,hasBonusMatch);
     }
 
-    public List<Result> getRanking() {
+    private void calculatePrizeRanking(List<Integer> lottoMatch, List<Boolean> hasBonusMatch) {
+        for (int i=0; i<lottoMatch.size(); i++) {
+          ranking.add(setRankingList(lottoMatch,hasBonusMatch, i));
+        }
+    }
+
+    private Prize setRankingList(List<Integer> lottoMatch, List<Boolean> hasBonusMatch, int index) {
+        int matchingNumbers = lottoMatch.get(index);
+        boolean hasBonus = hasBonusMatch.get(index);
+
+        if(matchingNumbers == 6) {
+            return Prize.FIRST;
+        } else if(matchingNumbers == 5 && hasBonus) {
+            return Prize.SECOND;
+        } else if (matchingNumbers == 5) {
+            return Prize.THIRD;
+        } else if (matchingNumbers == 4) {
+            return Prize.FOURTH;
+        } else if (matchingNumbers == 3) {
+            return Prize.FIFTH;
+        }
+        return Prize.NONE;
+    }
+
+    public List<Prize> getRanking() {
         return ranking;
     }
 }

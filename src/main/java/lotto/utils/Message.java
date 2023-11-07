5abfd1f40f56c94905b6e8ee1a682ea7ff35a936
다운 <j_d_woon@naver.com>
2023-11-07 15:35:19 +0900

    feat: 메세지 추가 - 당첨번호, 보너스 번호 입력 메세지 추가

--- src/main/java/lotto/utils/Message.java
+++ src/main/java/lotto/utils/Message.java
@@ -2,5 +2,7 @@ package lotto.utils;
 
 public class Message {
     public static String PURCAHSE_AMOUNT_LOTTO = "구입금액을 입력해 주세요.";
+    public static String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
 }

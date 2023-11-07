4a546dc286bd320fac0b09348078ad924617b708
dong2ast <kdh01132006@naver.com>
2023-11-08 00:48:04 +0900

    ♻️ refactor: 결과 출력에서 이용할 수 있게 enum 순서 변경

--- src/main/java/lotto/Enum/Winning.java
+++ src/main/java/lotto/Enum/Winning.java
@@ -1,11 +1,11 @@
 package lotto.Enum;
 
 public enum Winning {
-    FIRST("6개 일치 ", 2000000000),
-    SECOND("5개 일치, 보너스 볼 일치 ", 30000000),
-    THIRD("5개 일치 ", 1500000),
+    FIFTH("3개 일치 ", 5000),
     FOURTH("4개 일치 ", 50000),
-    FIFTH("3개 일치 ", 5000)
+    THIRD("5개 일치 ", 1500000),
+    SECOND("5개 일치, 보너스 볼 일치 ", 30000000),
+    FIRST("6개 일치 ", 2000000000)
     ;
     private final String message;
     private final Integer amount;

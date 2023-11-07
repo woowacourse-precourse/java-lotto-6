6f854ea995ed45258a9da9841731375ce2c9b480
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-07 22:54:15 +0900

    refact(lotto): 유효성 검사 세분화
    - validateSize : 갯수 검사
    - validateUnque : 중복 검사
    - validateNumber : 범위 검사

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,23 +1,61 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import lotto.utils.Constants;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateUnique(numbers);
+        validateNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != Constants.LOTTO_COUNT.getValues()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateUnique(List<Integer> numbers){
+        int prev_length = numbers.size();
+        int post_length = (int)numbers.stream()
+                .distinct()
+                .count();
+        if(prev_length!=post_length) throw new IllegalArgumentException();
+    }
+
+    private void validateNumber(List<Integer> numbers){
+        for(int number:numbers){
+            if(number<Constants.MIN_LOTTO_NUMBER.getValues()) throw new IllegalArgumentException();
+            if(number>Constants.MAX_LOTTO_NUMBER.getValues()) throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+    public long getMatchedNumber(Lotto winningLotto){
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+        return numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    public int getBonusNumber(Lotto winningLotto){
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+        return numbers.stream()
+                .filter(num -> !winningNumbers.contains(num))
+                .findFirst()
+                .get();
+    }
+
+    private List<Integer> getNumbers(){
+        return numbers;
     }
 }

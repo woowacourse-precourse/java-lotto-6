a90874549feea2143be97db0569a4293ddce8650
yeseong31 <yeseong31@naver.com>
2023-11-07 11:04:28 +0900

    refactor: 상수 나열 순서 정렬

--- src/main/java/lotto/domain/constants/SeparatorConstant.java
+++ src/main/java/lotto/domain/constants/SeparatorConstant.java
@@ -1,11 +1,11 @@
 package lotto.domain.constants;
 
 public class SeparatorConstant {
-
+    
+    public static final String DECIMAL_FORMAT = "###,###";
+    public static final String LOTTO_NUMBERS_SEPARATOR = ", ";
     public static final String RESULT_SUMMARY_SEPARATOR = "---";
     public static final String WINNING_NUMBERS_SEPARATOR = ",";
-    public static final String LOTTO_NUMBERS_SEPARATOR = ", ";
-    public static final String DECIMAL_FORMAT = "###,###";
 
     private SeparatorConstant() {
     }

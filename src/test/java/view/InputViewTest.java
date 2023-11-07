e9f7a94e24f61507099dd06826ac6c75ccf172e5
Son JunHyung <kingjh1125@gmail.com>
2023-11-07 23:49:20 +0900

    test (AmountTest): 테스트 추가
    공통 입력 규칙에 대한 테스트를 추가하였습니다:
    - 숫자가 아닌 값이 입력된 경우

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,8 +1,29 @@
 package view;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 
-public class InputViewTest{
+public class InputViewTest extends NsTest{
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("입력값이 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createAmountNotNumber() {
+        assertSimpleTest(() -> {
+            runException("run");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 
 }

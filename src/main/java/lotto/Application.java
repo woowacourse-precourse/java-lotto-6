131284f6b6c8f6bd39add4cf6af043f000a8cc41
Jiihyun <cwlgusc@gmail.com>
2023-11-07 23:36:08 +0900

    feat(GameController & Application): 로또 프로그램 시작 흐름 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.domain.LottoNumberGenerator;
+import lotto.domain.LottoStore;
+import lotto.io.InputReader;
+import lotto.io.OutputWriter;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = getGameController();
+        gameController.start();
+    }
+
+    private static GameController getGameController() {
+        InputReader reader = new InputReader();
+        OutputWriter writer = new OutputWriter();
+        InputView inputView = new InputView(writer, reader);
+        OutputView outputView = new OutputView(writer);
+        LottoStore lottoStore = new LottoStore(new LottoNumberGenerator());
+        return new GameController(inputView, outputView, lottoStore);
     }
 }
--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,4 +1,31 @@
 package lotto.controller;
 
+import lotto.domain.*;
+import lotto.domain.dto.input.PurchaseLottoDto;
+import lotto.domain.dto.output.LottosDto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoStore lottoStore;
+
+    public GameController(InputView inputView, OutputView outputView, LottoStore lottoStore) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoStore = lottoStore;
+    }
+
+    public void start() {
+        PurchaseLottoDto purchaseAmount = inputView.getPurchaseAmount();
+        Money money = Money.from(purchaseAmount.amount());
+        Lottos lottos = lottoStore.purchaseLottoTickets(money);
+        LottosDto lottosDto = lottos.toLottosDto();
+        outputView.printPurchasedLotto(lottosDto);
+        Lotto winningNumbers = inputView.getWinningNumber();
+        LottoNumber bonusNumber = inputView.getBonusNumber();
+        DrawMachine drawMachine = new DrawMachine(winningNumbers, bonusNumber, lottosDto);
+        drawMachine.draw();
+    }
 }

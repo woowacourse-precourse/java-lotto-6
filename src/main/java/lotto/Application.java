e8618142acc63918beaa9c1653100ec9b40d6f16
IM-GYURI <kgulr0517@gmail.com>
2023-11-08 03:38:10 +0900

    refactor: try-catch문 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,14 +3,8 @@ package lotto;
 import lotto.controller.MainController;
 
 public class Application {
-    private static final String ERROR = "[ERROR] ";
-
     public static void main(String[] args) {
-        try {
-            MainController mainController = new MainController();
-            mainController.start();
-        } catch (Exception e) {
-            System.out.println(ERROR + e.getMessage());
-        }
+        MainController mainController = new MainController();
+        mainController.start();
     }
 }

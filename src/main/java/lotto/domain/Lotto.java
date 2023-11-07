06531afccfc64742980a02fd45ca7378bb72eb2e
nove1080 <skwpqjq@gmail.com>
2023-11-07 23:33:20 +0900

    feat: 로또 도메인에 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,7 +4,9 @@ import lotto.exception.InvalidLottoNumberException;
 
 import java.util.*;
 
-import static lotto.constants.ErrorMessage.DUPLICATE_LOTTO_NUMBER;
+import static lotto.constants.ErrorMessage.*;
+import static lotto.constants.Lotto.MAX_NUMBER;
+import static lotto.constants.Lotto.MIN_NUMBER;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,25 +19,37 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         sizeCheck(numbers);
+        rangeCheck(numbers);
         duplicateCheck(numbers);
     }
 
     private void sizeCheck(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_ENOUGH_SIZE.getLabeledErrorMessage());
         }
+    }
+
+    private void rangeCheck(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < MIN_NUMBER.getValue() || number > MAX_NUMBER.getValue()) {
+                throw new InvalidLottoNumberException(OUT_OF_LOTTO_NUMBER_RANGE.getLabeledErrorMessage());
+            }
+        });
     }
 
     private void duplicateCheck(List<Integer> numbers) {
         Set<Object> duplicateCheck = new HashSet<>();
         for (Integer number : numbers) {
             if (!duplicateCheck.add(number)) {
-                throw new InvalidLottoNumberException(DUPLICATE_LOTTO_NUMBER.toString());
+                throw new InvalidLottoNumberException(DUPLICATE_LOTTO_NUMBER.getLabeledErrorMessage());
             }
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
     @Override
     public String toString() {
         StringJoiner format = new StringJoiner(", ", "[", "]");

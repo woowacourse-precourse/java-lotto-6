023ebd09a09bc8dfeaad2cff1a353563361d326d
olsohee <dlthgml0108@daum.net>
2023-11-07 12:00:50 +0900

    fix(PurchasePrice): 구입 금액을 0원으로 입력했을 때 정상 동작하는 오류 수정

--- src/main/java/lotto/domain/lotto/userLotto/PurchasePrice.java
+++ src/main/java/lotto/domain/lotto/userLotto/PurchasePrice.java
@@ -4,7 +4,8 @@ import static lotto.domain.lotto.LottoCondition.PRICE;
 
 public class PurchasePrice {
 
-    private static final String PURCHASE_PRICE_ERROR_MESSAGE = "구입 금액은 " + PRICE.getValue() + "원 단위로 입력해야 합니다.";
+    private static final String PURCHASE_PRICE_INDIVISIBLE_ERROR_MESSAGE = "구입 금액은 " + PRICE.getValue() + "원 단위로 입력해야 합니다.";
+    private static final String PURCHASE_PRICE_ZERO_ERROR_MESSAGE = "구입 금액은 0원 이상이어야 합니다.";
     private final int purchasePrice;
 
     public PurchasePrice(int purchasePrice) {
@@ -13,8 +14,19 @@ public class PurchasePrice {
     }
 
     private void validate(int purchasePrice) {
+        validateDivisible(purchasePrice);
+        validateZero(purchasePrice);
+    }
+
+    private void validateDivisible(int purchasePrice) {
         if (purchasePrice % PRICE.getValue() != 0) {
-            throw new IllegalArgumentException(PURCHASE_PRICE_ERROR_MESSAGE);
+            throw new IllegalArgumentException(PURCHASE_PRICE_INDIVISIBLE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateZero(int purchasePrice) {
+        if(purchasePrice == 0) {
+            throw new IllegalArgumentException(PURCHASE_PRICE_ZERO_ERROR_MESSAGE);
         }
     }
 

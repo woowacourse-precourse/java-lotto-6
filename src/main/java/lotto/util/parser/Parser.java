61849aa28960449d10b1617c9bfc68bc5b7effff
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-08 08:45:27 +0900

    feat: String을 int로 변환하는 함수 추가 - 예외처리를 위한 함수

--- src/main/java/lotto/util/parser/Parser.java
+++ src/main/java/lotto/util/parser/Parser.java
@@ -1,5 +1,9 @@
 package lotto.util.parser;
 
+import static lotto.Constants.INTEGER_REGEX;
+import static lotto.exception.ExceptionMessages.NEGATIVE_NUMBER_INPUT_MESSAGE;
+import static lotto.exception.ExceptionMessages.ONLY_NUMBER_INPUT_MESSAGE;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,5 +15,19 @@ public class Parser {
         return Arrays.stream(args)
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    /**
+     * Description: Parse String to int
+     */
+    public static int parseStringToInt(String arg) {
+        //음수도 허용
+        if (!arg.matches(INTEGER_REGEX)) {
+            throw new IllegalArgumentException(ONLY_NUMBER_INPUT_MESSAGE);
+        }
+        if (Integer.parseInt(arg) < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_INPUT_MESSAGE);
+        }
+        return Integer.parseInt(arg);
     }
 }

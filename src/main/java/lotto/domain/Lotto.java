1a6507d7eb3c7b91fee675681c0c5e338547fa58
zzun73 <736300@naver.com>
2023-11-07 18:03:50 +0900

    refactor: 로또 번호 범위 및 오름차순 검증로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,11 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+
+import static lotto.constant.Constants.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,7 +17,23 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateLottoSize(numbers);
+        validateLottoNumberRange(numbers);
         validateDuplicateNumber(numbers);
+        validateLottoNumberAscending(numbers);
+    }
+
+    private static void validateLottoNumberAscending(List<Integer> numbers) {
+        List<Integer> ascendingNumbers = new ArrayList<>(numbers);
+        Collections.sort(ascendingNumbers);
+        if (!ascendingNumbers.equals(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateLottoNumberRange(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(i -> (i >= MIN_LOTTO_NUMBER && i <= MAX_LOTTO_NUMBER))) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static void validateDuplicateNumber(List<Integer> numbers) {
@@ -23,7 +43,7 @@ public class Lotto {
     }
 
     private static void validateLottoSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }

a8a419f4853c22d9edc052bc7e4cd20d17c58af6
kkw2758 <dtc3229@gmail.com>
2023-11-07 11:58:25 +0900

    Test(PointResultTest) : 총 수익을 계산하는 기능 테스트 작성

--- src/test/java/lotto/PointResultTest.java
+++ src/test/java/lotto/PointResultTest.java
@@ -23,4 +23,21 @@ class PointResultTest {
         assertThat(pointResult.getCountByGrade(Grade.FOURTH)).isEqualTo(1);
         assertThat(pointResult.getCountByGrade(Grade.FIFTH)).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("총 수익을 계산한다.")
+    void calculateTotalPrizeMoneyTest() {
+        //Given
+        List<Double> points = new ArrayList<>(List.of(1.0, 3.5, 4.0, 5.0, 5.5, 6.0, 6.0));
+        PointResult pointResult = new PointResult(points);
+
+        //When
+        int result = pointResult.calculateTotalPrizeMoney();
+        int expectedResult =
+                Grade.FIRST.getPrizeMoney() * 2 + Grade.SECOND.getPrizeMoney() + Grade.THIRD.getPrizeMoney()
+                        + Grade.FOURTH.getPrizeMoney();
+
+        //When & Then
+        assertThat(result).isEqualTo(expectedResult);
+    }
 }

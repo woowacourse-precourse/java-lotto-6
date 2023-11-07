1f45435d7886db569d89eb68695140a694d3f00c
sw801733 <sw801733@naver.com>
2023-11-07 22:06:07 +0900

    feat : CaculateProfit 클래스 추가

---
+++ src/main/java/lotto/domain/CaculateProfit.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class CaculateProfit {
+
+    private final int cntProfit;
+    private final int bonusProfit;
+
+    private final int totalProfit;
+
+    public CaculateProfit() {
+        return;
+    }
+}

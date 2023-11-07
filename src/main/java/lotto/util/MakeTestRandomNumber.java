2a3315820a5a86e3d22bb355b67fea2aada2c9eb
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 18:04:23 +0900

    feat : 랜덤 넘버 테스트 기능을 위한 코드 뼈대 작성 - RandomNum 의 메서드 오버라이딩

---
+++ src/main/java/lotto/util/MakeTestRandomNumber.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+import java.util.List;
+
+public class MakeTestRandomNumber implements RandomNum {
+    @Override
+    public List<Integer> getRandomNumber() {
+        return null;
+    }
+}

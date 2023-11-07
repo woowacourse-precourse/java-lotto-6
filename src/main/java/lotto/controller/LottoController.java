af4dbead648ec8431156de0bb5c427babf13a422
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:59:46 +0900

    feat(LottoController): 로또 구매 개수만큼 로또 생성하는 함수 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.LottoTickets;
 import lotto.domain.TicketAmount;
 import lotto.view.OutputView;
 
@@ -9,11 +10,16 @@ public class LottoController {
 
     public void run() {
         setTicketAmountAndCount();
+        setLottoTickets();
     }
 
     private void setTicketAmountAndCount() {
         OutputView.printMoneyInputMessage();
         ticketAmount = InputController.setTicketAmount();
         ticketCount = ticketAmount.calculateTicketAmount();
+    }
+
+    private void setLottoTickets() {
+        LottoTickets lottoTickets = new LottoTickets(ticketCount);
     }
 }

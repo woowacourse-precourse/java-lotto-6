cd1167dffa06394d45e95563d074ade8fac1ef37
yh-skku <112400744+yh-skku@users.noreply.github.com>
2023-11-07 21:12:03 +0900

    feat:숫자가 아닌 입력이 들어올 시 예외 기능 구현

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -10,6 +10,11 @@ public class Money {
 
         System.out.println("구입 금액을 입력해 주세요.");
         String input = Console.readLine();
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
         money = Integer.parseInt(input);
 
     }

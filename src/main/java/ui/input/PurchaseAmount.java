e697c224d2dea08416bd0acd6cf5d55c6631e51a
nohheeyeon <heeyeon1213@gmail.com>
2023-11-07 17:31:53 +0900

    refactor : 구입 금액 입력을 처리하는 클래스 분리

---
+++ src/main/java/ui/input/PurchaseAmount.java
@@ -0,0 +1,26 @@
+package ui.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class PurchaseAmount {
+    public static int getPurchaseAmount() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        while (true) {
+            try {
+                int amount = Integer.parseInt(Console.readLine());
+                validateAmount(amount);
+                return amount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자만 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void validateAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+}

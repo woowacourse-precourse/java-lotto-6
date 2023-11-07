8b2af253fb7203ad5cb1b623760558ae8265a1c0
HYOAN <aspqll@naver.com>
2023-11-08 01:31:59 +0900

    feat(lotto): 로또티켓의 번호들을 확인하는 기능 구현

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -8,4 +8,12 @@ public class LottoTicket {
     private List<Integer> numbers = new ArrayList<>();
     private int matchingCount;
     private boolean bonusNumberMatch = false;
+
+    public LottoTicket(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+    public void printNumbers() {
+        System.out.println(numbers);
+    }
+
 }

72f82e49465cf7959cb303abc48efcfecf0da623
realcold0 <realcold0@gmail.com>
2023-11-08 03:38:30 +0900

    feat(lotto) : Lotto에 입력할 숫자들을 검증하는 기능, 로또를 출력하는 기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateNumberInRange(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers){
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateNumberInRange(List<Integer> numbers){
+        numbers.stream().forEach(num ->{
+            if(1 > num && num > 45){
+                throw new IllegalArgumentException("1~45 숫자를 입력해주세요");
+            }
+        });
+    }
+
+    public void printLotto(){
+        System.out.println( numbers.stream().map(Objects::toString).collect(Collectors.joining(", ", "[", "]")));
+    }
+}

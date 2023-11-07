39f123de11db621b30412f7df6f6fec0be58250e
권장순 <jsoonworld@gmail.com>
2023-11-08 01:54:02 +0900

    feat( WinningNumber ) : view로 부터 입력을 받아 검증을 처리하는 model package에 속한 WinningNumber( 당첨번호 model) 구현.

---
+++ src/main/java/lotto/v3/model/WinningNumber.java
@@ -0,0 +1,33 @@
+package lotto.v3.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WinningNumber {
+    private final Set<Integer> numbers;
+
+    public WinningNumber(List<Integer> numbers) {
+        validateNumbers(numbers);
+        this.numbers = new HashSet<>(numbers);
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호를 입력해야 합니다.");
+        }
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (!numbers.stream().allMatch(number -> number >= 1 && number <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 값이어야 합니다.");
+        }
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public Set<Integer> getNumbers() {
+        return numbers;
+    }
+}

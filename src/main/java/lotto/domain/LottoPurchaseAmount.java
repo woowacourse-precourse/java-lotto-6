f1eb281ebaaa4614e3dfb4acd9a3baf8e1aadb42
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 20:23:50 +0900

    feat(LottoPurchaseAmount) : 구입 금액 받아서 로또 개수 계산하는 기능 구현 - getLottoCount(String input)

--- src/main/java/lotto/domain/LottoPurchaseAmount.java
+++ src/main/java/lotto/domain/LottoPurchaseAmount.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.exception.LottoPurchaseAmountException;
 
 public class LottoPurchaseAmount {
+    private static final int lottoPrice = 1000;
     private final String input;
 
     public LottoPurchaseAmount(String input) {
@@ -13,5 +14,11 @@ public class LottoPurchaseAmount {
 
     private void validate(String input) {
         LottoPurchaseAmountException lottoPurchaseAmountException = new LottoPurchaseAmountException(input);
+    }
+
+    public int getLottoCount(String input){
+        int count = Integer.parseInt(input);
+        int lottoCount = count / lottoPrice;
+        return lottoCount;
     }
 }

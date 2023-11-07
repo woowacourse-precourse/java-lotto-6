e8cd6a58bf1ec0f901a028f239eda87377d397c3
hoyeon <highoption@naver.com>
2023-11-07 15:38:36 +0900

    test(WinningLottoTest) : 보너스 넘버가 숫자인지 테스트
    - 보너스 넘버는 숫자만 받을 수 있다

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -10,6 +10,17 @@ import org.junit.jupiter.api.Test;
 public class WinningLottoTest {
 
     @Test
+    @DisplayName("보너스 넘버가 숫자가 아니라면 예외 발생")
+    void createWinningLottoByIntegerBonusNumber() {
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        String bonus = "hoyeon";
+
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 숫자여야 합니다.");
+    }
+
+    @Test
     @DisplayName("보너스 넘버의 범위가 1부터 45사이인지 테스트")
     void createWinningLottoByBonusNumberRange() {
         Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));

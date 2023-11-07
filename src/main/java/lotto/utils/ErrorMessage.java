990160bbb0853b319ba0dfdad86d06c886979ed4
gusdn0108 <gusdn6671@naver.com>
2023-11-07 15:26:31 +0900

    feat(ErrorMessage) : [ERROR] 당첨번호 숫자는 6자리입니다 에러 메세지 추가

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -9,7 +9,9 @@ public enum ErrorMessage {
     LOTTO_NUMBER_NOT_DUPLICATE("[ERROR] 로또 번호는 중복 될수 없습니다"),
     LOTTO_NUMBER_NOT_SIX_SIZE("[ERROR] 로또 번호는 6자리 입니다"),
     LOTTO_NUMBER_BONUS_NUMBER_NOT_DUPLICATE("[ERROR] 보너스는 로또 번호와 중복될 수 없습니다."),
-    BONUS_NUMBER_RANGE_ERROR("[ERROR] 보너스 숫자의 범위는 1-45까지 입니다 ");
+    BONUS_NUMBER_RANGE_ERROR("[ERROR] 보너스 숫자의 범위는 1 - 45까지 입니다 "),
+    USER_INSERT_WINNING_NUMBER_IN_RANGE("[ERROR] 당첨번호의 숫자 범위는 1 - 45 까지입니다"),
+    WINNING_NUMBER_SIZE_ERROR("[ERROR] 당첨번호 숫자는 6자리입니다");
 
 
     private final String message;

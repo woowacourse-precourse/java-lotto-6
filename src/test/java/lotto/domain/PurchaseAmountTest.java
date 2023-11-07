53986100d41b1f3d3c2637184deb5f3b66d48e6b
whereami0404 <whereami2048@gmail.com>
2023-11-07 14:09:20 +0900

    test(PurchaseAmount): 모델 검증 로직 테스트 코드 구현

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.util.InputValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PurchaseAmountTest {
+    @DisplayName("입력 받은 금액이 1000원으로 나누어떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void validateIsDivideByLottoPrice() {
+        //given
+        int inputPrice = 14001;
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            PurchaseAmount purchaseAmount = new PurchaseAmount(inputPrice);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}

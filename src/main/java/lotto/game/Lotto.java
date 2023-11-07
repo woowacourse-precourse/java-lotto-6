def0c2497d597254e9c9f4d9e8e34b21da722b5c
ASak1104 <khw56184@gmail.com>
2023-11-08 04:59:44 +0900

    feat: 중복 숫자 검증 기능 추가

---
+++ src/main/java/lotto/game/Lotto.java
@@ -0,0 +1,27 @@
+package lotto.game;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6 | hasDuplicatedNumber(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean hasDuplicatedNumber(List<Integer> numbers) {
+        List<Integer> uniqueNumbers = numbers.stream()
+                .distinct()
+                .toList();
+
+        return numbers.size() != uniqueNumbers.size();
+    }
+}

6e482de4a961d2e12d3029fe3fa6cd8fbfcdd00b
ratatuwee <pkqwaszx@gmail.com>
2023-11-07 20:18:25 +0900

    fix: View 중복 숫자 확인 예외 오류 수정

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -73,9 +73,9 @@ public class View {
             if(Integer.parseInt(winningnum)<1 && Integer.parseInt(winningnum)>45){
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
-//            if(Collection.frequency(Arrays.asList(winningNumber.split(",")), winningnum)){
-//                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-//            }
+            if (Collections.frequency(Arrays.asList(winningNumber.split(",")), winningnum) > 1) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+            }
         }
     }
 

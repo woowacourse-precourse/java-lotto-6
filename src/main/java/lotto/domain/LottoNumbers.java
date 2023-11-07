932ec291b845939943164531e5ec02036d36a813
Jung So Young <soyoung5611@gmail.com>
2023-11-07 23:21:44 +0900

    style: model->domain 이동

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -1,6 +1,7 @@
-package lotto.model;
+package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
 import java.util.ArrayList;
 import java.util.Collections;

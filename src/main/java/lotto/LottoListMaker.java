16f4946071998518c14e8163e79bc01c1e49d404
Leespect <wewqwew153@gmail.com>
2023-11-07 13:54:36 +0900

    Refactor : LottoListMaker에서 로또 갯수 구하는 메서드 삭제

--- src/main/java/lotto/LottoListMaker.java
+++ src/main/java/lotto/LottoListMaker.java
@@ -2,17 +2,11 @@ package lotto;
 
 import java.util.ArrayList;
 
-import static lotto.constants.ConstantValues.*;
-
 public class LottoListMaker {
 
-    public static int getLottoListSize(int money) {
-        return money / LOTTO_COST.getValue();
-    }
-
-    public static ArrayList<Lotto> makeLottoList(int money) {
+    public static ArrayList<Lotto> makeLottoList(int lottoCount) {
         ArrayList<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < getLottoListSize(money); i++) {
+        for (int i = 0; i < lottoCount; i++) {
             Lotto lotto = LottoMaker.makeLotto();
             lottoList.add(lotto);
         }

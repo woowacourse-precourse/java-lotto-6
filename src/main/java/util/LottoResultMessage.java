909fe1db15c79f1e81bcd83863333e0fc75992e8
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-07 20:35:33 +0900

    refactor: enum클래스 values()메서드 사용을 위해 상수 순서 변경

--- src/main/java/util/LottoResultMessage.java
+++ src/main/java/util/LottoResultMessage.java
@@ -1,11 +1,11 @@
 package util;
 
 public enum LottoResultMessage {
-    FIRST_RANK(1,"6개 일치 (2000,000,000원) - "),
-    SECOND_RANK(2,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD_RANK(3, "5개 일치 (1,500,000원) - "),
+    FIFTH_RANK(5, "3개 일치 (5,000원) - "),
     FORTH_RANK(4, "4개 일치 (50,000원) - "),
-    FIFTH_RANK(5, "3개 일치 (5,000원) - ");
+    THIRD_RANK(3, "5개 일치 (1,500,000원) - "),
+    SECOND_RANK(2,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST_RANK(1,"6개 일치 (2000,000,000원) - ");
 
     private final int rank;
     private final String messageOFRank;

e298fac315763bd9f75a264a8023efb9f0923168
KIM0212DH <godqhr803@naver.com>
2023-11-07 21:54:14 +0900

    로또의 중복 숫자 예외 처리 완료

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,22 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        if (new HashSet<Integer>(numbers).size() != 6) {
             throw new IllegalArgumentException();
         }
     }

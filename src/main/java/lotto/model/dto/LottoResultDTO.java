c53da10caa23e6c9b83369d9cd3dd018e3e375c8
Yeriimii <seojs0511@naver.com>
2023-11-07 21:06:16 +0900

    feat(LottoResultDTO): 총 수익률을 저장할 수 있다

--- src/main/java/lotto/model/dto/LottoResultDTO.java
+++ src/main/java/lotto/model/dto/LottoResultDTO.java
@@ -6,12 +6,18 @@ import java.util.EnumMap;
 
 public class LottoResultDTO {
     private final EnumMap<PrizeCategory, Integer> matchResults;
+    private final double totalReturnRate;
 
-    public LottoResultDTO(EnumMap<PrizeCategory, Integer> matchResults) {
+    public LottoResultDTO(EnumMap<PrizeCategory, Integer> matchResults, double totalReturnRate) {
         this.matchResults = matchResults;
+        this.totalReturnRate = totalReturnRate;
     }
 
     public int getMatchResult(PrizeCategory match) {
         return matchResults.getOrDefault(match, 0);
+    }
+
+    public double getTotalReturnRate() {
+        return this.totalReturnRate;
     }
 }

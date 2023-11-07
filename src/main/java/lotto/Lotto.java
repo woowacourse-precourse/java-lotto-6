c994f33e621bde343c07855ae0244220dafe643c
nayonsoso <panda0329@naver.com>
2023-11-07 20:53:53 +0900

    feat: 로또 번호가 범위를 넘어갈 경우 예외 발생 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,23 +6,32 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLengthIsSix(numbers);
+        validateLength(numbers);
         validateNoDuplicates(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validateLengthIsSix(List<Integer> numbers) {
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또는 여섯개의 숫자로 구성되어야 합니다.");
         }
     }
 
-    private void validateNoDuplicates(List<Integer> list) {
-        long distinctNumberCount = list.stream()
+    private void validateNoDuplicates(List<Integer> numbers) {
+        long distinctNumberCount = numbers.stream()
                 .distinct()
                 .count();
-        if(distinctNumberCount != list.size()){
+        if(distinctNumberCount != numbers.size()){
             throw new IllegalArgumentException("[ERROR] 로또에는 중복되는 숫자가 있을 수 없습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for(int number : numbers){
+            if(number<1 || number>45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1부터 45까지 입니다.");
+            }
         }
     }
 }

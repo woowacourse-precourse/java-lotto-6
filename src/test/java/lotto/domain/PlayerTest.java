ceaf5bdf906d7bf4f78b52fa83bea8799d334487
unknown <y2kf9a0q@gmail.com>
2023-11-08 06:06:24 +0900

    test(lotto): PlayerTest.java

---
+++ src/test/java/lotto/domain/PlayerTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class PlayerTest {
+    private Money money;
+    private Lottos lottos;
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        money = mock(Money.class);
+        lottos = mock(Lottos.class);
+        player = new Player(money, lottos);
+    }
+
+    @Test
+    void getMoney() {
+        assertThat(player.getMoney()).isEqualTo(money);
+    }
+
+    @Test
+    void getLottos() {
+        assertThat(player.getLottos()).isEqualTo(lottos);
+    }
+}

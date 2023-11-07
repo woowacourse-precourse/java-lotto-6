5661a0bfc408411a172f56acf30b2d94eaff9c51
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 15:04:16 +0900

    feat(Constants): 상수 클래스 생성

---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,8 @@
+package lotto.util;
+
+public class Constants {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_MIN_AMOUNT = 1000;
+}

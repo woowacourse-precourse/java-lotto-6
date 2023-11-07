834a08b36d17c99878d600df5695a2cc70985a60
Knight <kangrack333@gmail.com>
2023-11-07 21:19:52 +0900

    refactor(BonusNumberController) : 보너스 인자값 추가

--- src/main/java/lotto/controller/BonusNumberController.java
+++ src/main/java/lotto/controller/BonusNumberController.java
@@ -6,12 +6,14 @@ import lotto.view.Input;
 public class BonusNumberController {
 
     private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    Input bonusNumberInput = new BonusNumberInput();
-    WinningStatisticsController winningStatisticsController = new WinningStatisticsController();
+    private static final Input bonusNumberInput = new BonusNumberInput();
+    private static final WinningStatisticsController winningStatisticsController = new WinningStatisticsController();
+    public static int bonusNumber = 0;
 
     public void start() {
         System.out.println(ENTER_BONUS_NUMBER);
-        bonusNumberInput.readLine();
+        bonusNumber = Integer.parseInt(bonusNumberInput.readLine());
         winningStatisticsController.start();
     }
+
 }

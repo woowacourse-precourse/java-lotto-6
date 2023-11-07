51c02384a26a38ee275a6fb2fdcd511e4e22684b
ch1hyun <och2510@gmail.com>
2023-11-07 20:10:46 +0900

    feat: Add constant 10 and 100 and regular expression for float type

--- src/main/java/lotto/enums/Constant.java
+++ src/main/java/lotto/enums/Constant.java
@@ -7,10 +7,13 @@ public enum Constant {
     COUNT_OF_LOTTO_NUMBERS("6"),
     START_RANGE_OF_NUMBER("1"),
     END_RANGE_OF_NUMBER("45"),
+    NUMBER_USED_TO_ROUND_FIRST_DIGIT("10"),
+    NUMBER_USED_TO_MAKE_PERCENTAGE("100"),
     DELIMITER(",");
 
-    public static final Pattern PATTERN_NUMBER = Pattern.compile("(\\+|-)?[1-9][0-9]+");
-    public static final Pattern PATTERN_POSITIVE_NUMBER_STEP_1000 = Pattern.compile("(\\+?0)|(\\+?[1-9][0-9]*000)");
+    public static final Pattern PATTERN_NUMBER = Pattern.compile("(\\+|-)?[0-9]+");
+    public static final Pattern PATTERN_FLOAT = Pattern.compile("(\\+|-)?[0-9]+(\\.[0-9]+)?");
+    public static final Pattern PATTERN_POSITIVE_NUMBER_AND_1000_UNITS = Pattern.compile("(\\+?0)|(\\+?[1-9][0-9]*000)");
 
     private String content;
 
@@ -25,7 +28,13 @@ public enum Constant {
     public Integer getContentToInteger() {
         validateNumber();
 
-        return Integer.parseInt(content);
+        return Converter.stringToInteger(content);
+    }
+
+    public Float getContentToFloat() {
+        validateFloat();
+
+        return Converter.stringToFloat(content);
     }
 
     private void validateNumber() {
@@ -33,6 +42,14 @@ public enum Constant {
 
         if (numberMatcher.matches() == false) {
             throw new IllegalArgumentException(ErrorMessage.STRING_CANNOT_CONVERT_TO_INTEGER.getMessage()); // 에러문 추가
+        }
+    }
+
+    private void validateFloat() {
+        Matcher floatMatcher = PATTERN_FLOAT.matcher(content);
+
+        if (floatMatcher.matches() == false) {
+            throw new IllegalArgumentException(ErrorMessage.STRING_CANNOT_CONVERT_TO_FLOAT.getMessage());
         }
     }
 }

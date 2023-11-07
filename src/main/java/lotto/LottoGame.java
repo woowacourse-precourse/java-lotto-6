a67ffc1a2e35d27229e0c5fefcd85085a7b012e2
ghsyn <iamdragon.dev@gmail.com>
2023-11-07 13:43:00 +0900

    feat(Lotto) : Create method getLotto, getRandomNumbers

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,8 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
+
+    final static int START_NUMBER = 1;
+    final static int END_NUMBER = 45;
+    final static int COUNT_NUMBER = 6;
+    static int lottoCnt;
     public static int issuanceLotto(int money){
-        int lottoCnt = money/1000;
+        lottoCnt = money/1000;
         return lottoCnt;
+    }
+
+    public static List<List<Integer>> getLotto(){
+        List<List<Integer>> tickets = new ArrayList<>();
+        for (int i = 0; i < lottoCnt; i++) {
+            tickets.add(getRandomNumbers());
+        }
+        for(List<Integer> ticket : tickets){
+            System.out.println(ticket);
+        }
+        return tickets;
+    }
+
+
+
+    public static List<Integer> getRandomNumbers(){
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER,END_NUMBER,COUNT_NUMBER);
     }
 }

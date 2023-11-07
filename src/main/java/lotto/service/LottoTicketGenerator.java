761089b1034df098cbf0c2b32006095962b9e2c9
nhn1030 <shgusska12@gmail.com>
2023-11-07 12:41:09 +0900

    refactor: LottoNumberGenerator의 generateLottoNumber메서드를 사용하도록 수정

--- src/main/java/lotto/service/LottoTicketGenerator.java
+++ src/main/java/lotto/service/LottoTicketGenerator.java
@@ -1,19 +1,28 @@
 package lotto.service;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTicketGenerator {
-    // 구매한 금액 기반 로또 티켓을 생성
+    private final LottoNumberGenerator numberGenerator;
+
+    public LottoTicketGenerator(LottoNumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    public static LottoTicketGenerator create(LottoNumberGenerator numberGenerator) {
+        return new LottoTicketGenerator(numberGenerator);
+    }
+
     public List<List<Integer>> generateLottoTicket(int purchaseAmount) {
         int totalTicketCount = purchaseAmount / 1000;
         List<List<Integer>> lottoTickets = new ArrayList<>();
 
         for (int i = 0; i < totalTicketCount; i++) {
-            List<Integer> lottoNumbers = generateLottoNumbers();
+            List<Integer> lottoNumbers = numberGenerator.generateLottoNumber();
             lottoTickets.add(lottoNumbers);
         }
+
         return lottoTickets;
     }
 }

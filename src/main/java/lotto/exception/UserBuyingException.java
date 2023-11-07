3bc765ced49efd528ce27779a08269400c5eafe3
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-07 12:56:21 +0900

    feat(UserBuyingException) : validateBuyingException 메서드 생성

--- src/main/java/lotto/exception/UserBuyingException.java
+++ src/main/java/lotto/exception/UserBuyingException.java
@@ -1,6 +1,8 @@
 package lotto.exception;
 
 public class UserBuyingException {
-
-
+    public void validateBuyingException(String buyingPrice) {
+        validIsNumber(buyingPrice);
+        validPrice(buyingPrice);
+    }
 }

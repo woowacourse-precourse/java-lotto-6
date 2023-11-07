df5945b80330ec4518cc8f8837f79ff46ca77a97
ZZ3n <ndsrhkd@naver.com>
2023-11-07 20:20:01 +0900

    refactor(README): ExceptionHandler 삭제
    구현 사항 상 ExceptionHandler가 필요 없을 듯 하여 삭제하였다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,21 +1,14 @@
 package lotto;
 
 import dto.LottoMoney;
-import ui.ExceptionHandler;
 import ui.IOView;
 
 public class Application {
     public static void main(String[] args) {
-        setExceptionHandler();
 
         LottoMoney money = IOView.getMoney();
         LottoTickets lottoTickets = LotteryMachine.buy(money);
         IOView.printLottoNumbers(lottoTickets);
         // TODO: 프로그램 구현
-    }
-
-    private static void setExceptionHandler() {
-        ExceptionHandler exceptionHandler = new ExceptionHandler();
-        Thread.setDefaultUncaughtExceptionHandler(exceptionHandler);
     }
 }

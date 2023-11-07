0b2bb9fa2e7085350bfcfc235ae9df0199a8feaf
yunju-m <dbswnfnal@naver.com>
2023-11-07 20:05:46 +0900

    Refactor(LuckeyLotto): ♻️ 예외처리 메시지 Java Enum 적용

--- src/main/java/lotto/LuckeyLotto.java
+++ src/main/java/lotto/LuckeyLotto.java
@@ -3,9 +3,22 @@ package lotto;
 import java.util.List;
 
 public class LuckeyLotto {
-    private static final String WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호의 타입은 숫자이어야 합니다.";
-    private static final String WRONG_SAME_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호는 중복없는 숫자이어야 합니다.";
-    private static final String WRONG_RANGE_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호는 1부터 45사이의 숫자여야 합니다.";
+    private enum ErrorMessage {
+        WRONG_TYPE("[ERROR] 보너스 로또 번호의 타입은 숫자이어야 합니다."),
+        WRONG_SAME("[ERROR] 보너스 로또 번호는 중복없는 숫자이어야 합니다."),
+        WRONG_RANGE("[ERROR] 보너스 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+
+        private final String message;
+
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
     private static final int LOTTO_NUMBER_RANGE_START = 1;
     private static final int LOTTO_NUMBER_RANGE_END = 45;
     private int luckeyBonusNumber;
@@ -21,11 +34,11 @@ public class LuckeyLotto {
 
     private void validate(List<Integer> luckeyNumbers, String inputBonusNumber) {
         if (!inputBonusNumber.matches("\\d+")) {
-            throw new IllegalArgumentException(WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.WRONG_TYPE.getMessage());
         } else if (luckeyNumbers.contains(Integer.valueOf(inputBonusNumber))) {
-            throw new IllegalArgumentException(WRONG_SAME_LOTTO_BONUS_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.WRONG_SAME.getMessage());
         } else if (!isWithValidRange(Integer.valueOf(inputBonusNumber))) {
-            throw new IllegalArgumentException(WRONG_RANGE_LOTTO_BONUS_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(ErrorMessage.WRONG_RANGE.getMessage());
         }
     }
 

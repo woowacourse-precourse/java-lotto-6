37dcf67bde25dced51556ab8a81b9bf767e83b8f
wonjun <wonjon0930@daum.net>
2023-11-07 19:20:41 +0900

    feat: add feature to check user input about bonus number is convertible to numeric type

--- src/main/java/lotto/view/message/LottoInputExceptionMessage.java
+++ src/main/java/lotto/view/message/LottoInputExceptionMessage.java
@@ -25,7 +25,9 @@ public enum LottoInputExceptionMessage {
             OutputConstant.ERROR_WARNING + "당첨번호 입력값 중 중복된 값이 존재합니다." + OutputConstant.NEW_LINE),
     LOTTO_NUMBER_IS_NOT_IN_APPROPRIATE_RANGE(
             OutputConstant.ERROR_WARNING + "로또 번호는 " + LottoConstant.LOTTO_MIN_NUM.getValue() + "부터"
-                    + LottoConstant.LOTTO_MAX_NUM.getValue() + " 사이의 숫자여야 합니다." + OutputConstant.NEW_LINE);
+                    + LottoConstant.LOTTO_MAX_NUM.getValue() + " 사이의 숫자여야 합니다." + OutputConstant.NEW_LINE),
+    BONUS_NUMBER_IS_NOT_NUMERIC_TYPE(
+            OutputConstant.ERROR_WARNING + "보너스 숫자는 숫자 형식의 입력이어야 합니다." + OutputConstant.NEW_LINE);
 
     private final String message;
 
--- src/main/java/lotto/view/validator/BonusNumberValidator.java
+++ src/main/java/lotto/view/validator/BonusNumberValidator.java
@@ -6,11 +6,20 @@ import lotto.view.message.LottoInputExceptionMessage;
 public class BonusNumberValidator {
     public void validate(String userInput) {
         isNotEmpty(userInput);
+        isNotNumeric(userInput);
     }
 
     private void isNotEmpty(String userInput) {
         if (userInput.isEmpty()) {
             throw LottoInputException.of(LottoInputExceptionMessage.INPUT_IS_EMPTY);
+        }
+    }
+
+    private void isNotNumeric(String userInput) {
+        try {
+            Long.parseLong(userInput);
+        } catch (NumberFormatException e) {
+            throw LottoInputException.of(LottoInputExceptionMessage.BONUS_NUMBER_IS_NOT_NUMERIC_TYPE);
         }
     }
 }

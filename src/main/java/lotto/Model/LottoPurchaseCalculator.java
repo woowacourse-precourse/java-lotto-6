374de5fa2f1254d6d16e660b18259fbb3a3857e0
mksystem98 <mksystem98@gmail.com>
2023-11-08 04:17:32 +0900

    refactor: 로또 가격 상수 접근성 변경
    LottoController에서 사용할 수 있도록 LOTTO_PRICE 상수의 접근 제한자를 private에서 public으로 수정함.

--- src/main/java/lotto/Model/LottoPurchaseCalculator.java
+++ src/main/java/lotto/Model/LottoPurchaseCalculator.java
@@ -2,7 +2,7 @@ package lotto.Model;
 
 public class LottoPurchaseCalculator {
 
-    private static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_PRICE = 1000;
 
     public static int calculateNumberOfTickets(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;

0916e445d423b5d6ef15dfc28c7c4ee1303d0919
donghoony <aru0504@naver.com>
2023-11-08 03:28:52 +0900

    fix: 에러 메시지를 요구사항에 맞도록 수정 (Error -> ERROR)

--- src/main/java/lotto/exception/LottoIllegalArgumentException.java
+++ src/main/java/lotto/exception/LottoIllegalArgumentException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class LottoIllegalArgumentException extends IllegalArgumentException {
-    private static final String ERROR_PREFIX = "[Error] ";
+    private static final String ERROR_PREFIX = "[ERROR] ";
 
     public LottoIllegalArgumentException(String message) {
         super(ERROR_PREFIX + message);

1054d67f2279642d4faa610c2599405a61e47655
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-07 22:39:00 +0900

    로또 등수를 나타내는 LottoRank enum 추가
    로또 등수와 상금 금액을 관리하기 위한 LottoRank enum을 추가했습니다.
    각 등수는 일치하는 번호의 개수(matchCount)와 해당 등수의 상금(reward)을 필드로 가집니다.
    
    - FIRST: 6개 번호 일치, 2,000,000,000원
    - SECOND: 5개 번호 일치, 30,000,000원
    - THIRD: 4개 번호 일치, 1,500,000원
    - FOURTH: 3개 번호 일치, 50,000원
    - FIFTH: 2개 번호 일치, 5,000원
    - NONE: 일치하는 번호 없음, 0원
    
    이 enum은 로또 게임의 결과 처리 로직에서 사용될 예정입니다.

---
+++ src/test/java/lotto/LottoRank.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(4, 1_500_000),
+    FOURTH(3, 50_000),
+    FIFTH(2, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int reward;
+
+    LottoRank(int matchCount, int reward) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+    }
+}

9b2bf7c2053228926fcee8d898352610953ccbbf
DSRN0000 <dsrn0000@gmail.com>
2023-11-07 21:16:12 +0900

    feat(LottoPrice): 구또 구입에 대한 model 구현

--- src/main/java/model/LottoPrice.java
+++ src/main/java/model/LottoPrice.java
@@ -1,38 +1,34 @@
 package model;
 
-import error.DivideMoneyException;
-import error.ZeroMoneyException;
+import message.ErrorMessage;
 
-;
+public class LottoPrice {
+    private final int price;
 
-public class Money {
-    private static final int ZERO = 0;
-    private static final int THOUSAND = 1000;
-    private final int money;
-
-    public Money(int money) {
-        validate(money);
-        this.money = money;
+    public LottoPrice(int price) {
+        validate(price);
+        this.price = price;
     }
 
-    private void validate(int inputValue) {
-        validateZero(inputValue);
-        validateDivideMoney(inputValue);
+    private void validate(int price) {
+        validateZero(price);
+        validateDivideMoney(price);
     }
 
-    private void validateZero(int inputValue) {
-        if (inputValue == ZERO) {
-            throw new ZeroMoneyException();
+    private void validateZero(int price) {
+        if (price == 0) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_PRICE_ZERO_ERROR);
         }
     }
 
-    private void validateDivideMoney(int inputValue) {
-        if (inputValue % THOUSAND != ZERO) {
-            throw new DivideMoneyException();
+    private void validateDivideMoney(int price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_PRICE_DIVIDE_ERROR);
         }
     }
 
-    public int getTicket() {
-        return money / 1000;
+    public int lottoPaperCount() {
+        return price / 1000;
     }
+
 }

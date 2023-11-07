252b27221680a610493a8ae2d264f40f5070750e
jeongjihun <wlgns2234@naver.com>
2023-11-08 00:50:17 +0900

    test: 테스트 코드 추가
    - LottoResult의 당첨개수에 따라 결과를 생성하는 테스트 추가
    - 일치개수가 3개 이하일 때는 당첨결과가 존재하지 않는다.

---
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.Optional;
+
+class LottoResultTest {
+
+    @DisplayName("로또결과 생성 테스트 : 로또 당첨에 해당하지 않는다면 빈 값을 반환한다.")
+    @ParameterizedTest(name = "당첨번호와 일치개수가 {0}개라면 반환되는 당첨 결과는 없다.")
+    @ValueSource(ints = {0, 1, 2})
+    void create(int matchingNumbers) {
+        Optional<LottoResult> result = LottoResult.create(matchingNumbers, true);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+}

9d38014069d1e3ba9f3828082b97723e8b6228a6
JIHYE <jdh324@naver.com>
2023-11-07 19:15:59 +0900

    style: Application 에 있던 input 메서드들을 Input 클래스로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,64 +1,19 @@
 package lotto;
 
-import java.util.List;
 import java.util.Map;
 import view.Input;
 import view.Output;
 
 public class Application {
     public static void main(String[] args) {
-        int purchaseAmount = inputPurchaseAmount();
+        int purchaseAmount = Input.getPurchaseAmount();
         Lotteries lotteries = new Lotteries(purchaseAmount);
         Output.printIssuedLotto(lotteries.getLotteries());
-        Lotto winningNumbers = inputWinningNumbers();
-        int bonusNumber = inputBonusNumber(winningNumbers);
+        Lotto winningNumbers = Input.getWinningNumbers();
+        int bonusNumber = Input.getBonusNumber(winningNumbers);
         Map<WinningRanking, Integer> winningStatistics = lotteries.getWinningStatistics(winningNumbers, bonusNumber);
         Output.printWinningStatistics(winningStatistics);
         String rateOfReturn = Output.getRateOfReturn(winningStatistics, purchaseAmount);
         Output.printRateOfReturn(rateOfReturn);
-    }
-
-    private static int inputBonusNumber(Lotto winningNumbers) {
-        while (true) {
-            int bonusNumber;
-            try {
-                String input = Input.inputBonusNumber();
-                bonusNumber = LottoNumber.validateLottoNumber(input);
-                winningNumbers.isBonusNumberDuplicate(bonusNumber);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            return bonusNumber;
-        }
-    }
-
-    private static Lotto inputWinningNumbers() {
-        while (true) {
-            Lotto lotto;
-            try {
-                String input = Input.inputWinningNumbers();
-                List<Integer> winningNumbers = LottoNumber.validateLottoNumbers(input.split(","));
-                lotto = new Lotto(winningNumbers);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            return lotto;
-        }
-    }
-
-    private static int inputPurchaseAmount() {
-        while (true) {
-            LottoPurchaseAmount lottoPurchaseAmount;
-            try {
-                String input = Input.inputPurchaseAmount();
-                lottoPurchaseAmount = new LottoPurchaseAmount(input);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            return lottoPurchaseAmount.getPurchaseAmount();
-        }
     }
 }
--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -1,6 +1,10 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.Lotto;
+import lotto.LottoNumber;
+import lotto.LottoPurchaseAmount;
 
 public class Input {
 
@@ -8,17 +12,61 @@ public class Input {
     private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String inputPurchaseAmount() {
+    public static int getBonusNumber(Lotto winningNumbers) {
+        while (true) {
+            int bonusNumber;
+            try {
+                String input = inputBonusNumber();
+                bonusNumber = LottoNumber.validateLottoNumber(input);
+                winningNumbers.isBonusNumberDuplicate(bonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            return bonusNumber;
+        }
+    }
+
+    public static Lotto getWinningNumbers() {
+        while (true) {
+            Lotto lotto;
+            try {
+                String input = inputWinningNumbers();
+                List<Integer> winningNumbers = LottoNumber.validateLottoNumbers(input.split(","));
+                lotto = new Lotto(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            return lotto;
+        }
+    }
+
+    public static int getPurchaseAmount() {
+        while (true) {
+            LottoPurchaseAmount lottoPurchaseAmount;
+            try {
+                String input = inputPurchaseAmount();
+                lottoPurchaseAmount = new LottoPurchaseAmount(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            return lottoPurchaseAmount.getPurchaseAmount();
+        }
+    }
+
+    private static String inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         return Console.readLine();
     }
 
-    public static String inputWinningNumbers() {
+    private static String inputWinningNumbers() {
         System.out.println("\n" + INPUT_WINNING_NUMBERS);
         return Console.readLine();
     }
 
-    public static String inputBonusNumber() {
+    private static String inputBonusNumber() {
         System.out.println("\n" + INPUT_BONUS_NUMBER);
         return Console.readLine();
     }

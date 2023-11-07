6fda87f9a68c9930a66760c1663254a1caa7a890
choji <waf268@gmail.com>
2023-11-07 21:17:44 +0900

    fix : Application이 playLotto에 진입하도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
+        PlayLotto playLotto = new PlayLotto();
+        playLotto.play();
         // TODO: 프로그램 구현
     }
 }

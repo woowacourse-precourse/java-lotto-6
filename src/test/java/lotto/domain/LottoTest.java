6f88dca089b7771cfe1d249cfabf913d0d32a469
Kim JunHyun <jjag1015@naver.com>
2023-11-08 04:36:33 +0900

    test(Lotto): 로또 숫자 범위 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,8 +2,12 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,5 +27,16 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 숫자 범위가 1~45를 벗어나면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"0,1,2,3,4,5", "41,42,43,44,45,46"})
+    void creatLottoByOutOfScopeNumber(String value) {
+        List<Integer> list = Arrays.asList(value.split(","))
+                .stream()
+                .map(token -> Integer.valueOf(token))
+                .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> new Lotto(list))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

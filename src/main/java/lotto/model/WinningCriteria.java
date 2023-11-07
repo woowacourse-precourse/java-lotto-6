98b7accd87be837abb82042c1b87c66a60f0cb16
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 02:53:52 +0900

    feat(WinningCriteria): 로또 당첨 기준 Enum 생성

---
+++ src/main/java/lotto/model/WinningCriteria.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+public enum WinningCriteria {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+    private Integer matchingNumberCount;
+    private boolean matchBonusNumber;
+    private int prizeAmount;
+
+    WinningCriteria(int matchingNumberCount, int prizeAmount) {
+        this(matchingNumberCount, false, prizeAmount);
+    }
+
+    WinningCriteria(int matchingNumberCount, boolean matchBonusNumber, int prizeAmount) {
+        this.matchingNumberCount = matchingNumberCount;
+        this.matchBonusNumber = matchBonusNumber;
+        this.prizeAmount = prizeAmount;
+    }
+}

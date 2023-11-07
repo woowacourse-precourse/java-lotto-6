da226e2e8f24b5091551acd0d6ec682edfeb07f7
bitesnail <rkwoal216@gmail.com>
2023-11-07 18:25:39 +0900

    feat: adding manager

---
+++ src/main/java/lotto/GameManager.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import lotto.models.Machine;
+import lotto.models.User;
+import lotto.views.GameView;
+
+public class GameManager {
+    private User user;
+    private Machine machine;
+    private GameView gameView;
+
+    public GameManager(){
+        user = new User();
+        machine = new Machine();
+        gameView = new GameView();
+    }
+
+    public void play(){
+        gameView.purchaseAmountView();
+    }
+}

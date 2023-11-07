685e5c7b6384156192ae73204a96418f5c5e022d
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 03:00:30 +0900

    [ADD] 헤더 추가

--- src/main/java/lotto/View/Lottogameview.java
+++ src/main/java/lotto/View/Lottogameview.java
@@ -1,5 +1,11 @@
 package lotto.View;
 
+import lotto.model.Lotto;
+import lotto.model.LottoResult;
+
+import java.util.List;
+
+
 public class Lottogameview {
   public void printPurchasedLottosCount(int numberOfLotto) {
         System.out.println(numberOfLotto + "개를 구매했습니다.");

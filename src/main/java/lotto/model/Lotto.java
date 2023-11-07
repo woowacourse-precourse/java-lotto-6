a65ea4c4c61af2478bf7329a0af32ba97830a061
HyungUk Ryu <hw0603@naver.com>
2023-11-08 01:53:38 +0900

    feat(Lotto): Lotto의 데이터 유효성 검증 구현
    숫자는 총 6개, 각 숫자는 1~45 범위 이내

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,10 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!isSizeMatch(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 숫자의 개수는 6개여야 합니다.");
+        }
+        if (hasDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또에 중복된 숫자가 존재합니다.");
+        }
+        if (!inRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 범위에 맞지 않는 수가 존재합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private boolean isSizeMatch(List<Integer> numbers) {
+        return numbers.size() == LOTTO_NUMBER_SIZE;
+    }
+
+    private boolean hasDuplicateNumber(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
+
+    private boolean inRange(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .allMatch(number -> number >= LOTTO_MIN_NUMBER && number <= LOTTO_MAX_NUMBER);
+    }
 }

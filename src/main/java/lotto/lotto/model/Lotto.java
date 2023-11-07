5b27b7daa25c627b846d927fef3bf84198edbfde
황준혁 <hyug0819@gmail.com>
2023-11-08 01:55:45 +0900

    feat : Lotto Model 기능 추가

--- src/main/java/lotto/lotto/model/Lotto.java
+++ src/main/java/lotto/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.lotto.model;
 
+import java.util.Collections;
 import java.util.List;
+
+import lotto.util.error.CustomError;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,13 +11,29 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        sorting(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CustomError.INPUT_MAX_NUMBER.getError());
         }
     }
 
     // TODO: 추가 기능 구현
+    public String toString(){
+        return numbers.toString();
+    }
+
+    private void sorting(List<Integer> numbers){
+        Collections.sort(numbers);
+    }
+
+    public int length(){
+        return numbers.size();
+    }
+
+    public int get(int index){
+        return numbers.get(index);
+    }
 }

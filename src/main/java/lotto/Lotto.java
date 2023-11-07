f313cc029cddd2c525b8e9abb953e2771a74427e
daki <daki@pop-os.localdomain>
2023-11-07 21:09:04 +0900

    Feat(Lotto): 로또 번호 생성 및 비교를 하는 Lotto 구현
    * isMatch : 정수를 입력 받고, 존재하면 true 반환.
    * getMatchCount : Lotto를 입력받고, 현재 lottoNumber와 몇개가 겹치는지 반환.
    * validateLength : 길이가 로또의 고정된 길이인지 검증 및 에러 발생
    * validateDuplicate : 로또에 중복 번호가 있는지 검증 및 에러 발생

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,38 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    public static final int LOTTO_LENGTH = 6;
+    private final List<Integer> lottoNumbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> lottoNumbers) {
+        validateLength(lottoNumbers);
+        validateDuplicate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public boolean isMatch(Integer number) {
+        return lottoNumbers.contains(number);
+    }
+    public Integer getMatchCount(Lotto lotto) {
+        return (int) lottoNumbers.stream()
+                .filter(lotto::isMatch)
+                .count();
+    }
+
+    private void validateLength(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("로또의 숫자는 6개여야 합니다!");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> lottoNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
+        if(uniqueNumbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다!");
+        }
+    }
 }

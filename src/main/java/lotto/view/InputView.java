8dcca6ac8de2670aafc2372dd1e0ffa6c7a3752b
whereami2048 <whereami2048@gmail.com>
2023-11-07 11:09:07 +0900

    feat: 당첨번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,9 +2,6 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
@@ -12,6 +9,11 @@ public class InputView {
 
     public String inputPurchaseAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT);
+        return readLine();
+    }
+
+    public String inputWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBERS);
         return readLine();
     }
 }

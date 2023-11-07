8f73bdf02d9c40d7067d2b93f86ffa2d509f0dc6
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 03:42:02 +0900

    test: 보너스 숫자가 조건을 만족하는지 확인
    - 보너스 번호는 숫자로만 이루어집니다.
    - 보너스 번호는 1부터 45까지의 숫자입니다.
    - 보너스 번호는 1개 입니다.

--- src/main/java/validator/BonusNumberGeneratorValidator.java
+++ src/main/java/validator/BonusNumberGeneratorValidator.java
@@ -8,8 +8,8 @@ public class BonusNumberGeneratorValidator {
     }
 
     void vaildBonusNumberRange(int bonusNumber) {
-        if (bonusNumber > 9 || bonusNumber < 1) {
-            throw new IllegalArgumentException("[ERROR] 보너스 숫자에는 1 ~ 9까지의 숫자만 입력해주세요.");
+        if (bonusNumber > 45 || bonusNumber < 1) {
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자에는 1 ~ 45까지의 숫자만 입력해주세요.");
         }
     }
 

b791d5829a54696d39a91b0683de6a0befa88bcb
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 07:30:17 +0900

    feat(LottoResult): 로또 번호와 당첨 번호를 비교하여 일치하는 개수를 구한다.

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,5 +1,18 @@
 package lotto.model;
 
+import java.util.List;
+
 public class LottoResult {
-    
+
+    private int matchResult(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+        int match = 0;
+        for (int lottoNumber : lottoNumbers) {
+            if (winningNumbers.contains(lottoNumber)) {
+                match++;
+            }
+        }
+        return match;
+    }
+
+
 }

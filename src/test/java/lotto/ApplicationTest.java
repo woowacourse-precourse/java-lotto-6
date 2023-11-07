a01a14fa36769efab5442678084d2611f0d4ee94
nylonmusk <2912rla@gmail.com>
2023-11-08 04:57:03 +0900

    test : ERROR_MESSAGE에 대한 클래스 생성에 따른 수정

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -10,7 +10,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
     void 기능_테스트() {
@@ -50,7 +49,7 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("1000j");
-            assertThat(output()).contains(ERROR_MESSAGE);
+            assertThat(output()).contains(TestConstant.ERROR_MESSAGE);
         });
     }
 

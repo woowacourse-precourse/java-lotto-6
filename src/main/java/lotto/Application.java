4da5f51c37397ed2e67774f05c061e4f487d07e7
lss0809 <ippnss@gmail.com>
2023-11-07 21:16:00 +0900

    feat(Purchase): 구매 단계의 인풋을 받고 검사하는 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.domain.Purchase;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Purchase p = new Purchase();
+        int trials = p.purchasing();
     }
 }
---
+++ src/main/java/lotto/domain/Purchase.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Purchase {
+    private int money;
+    private int trials;
+    private final String PURCHASE_INPUT_TEXT = "구입금액을 입력해 주세요.";
+    private final String PURCHASE_DONE_TEXT = "개를 구매했습니다.";
+    private final String ERROR_NOT_INTEGER_TEXT = "[ERROR] 숫자를 입력해주세요.";
+    private final String ERROR_NOT_1k_TEXT = "[ERROR] 1,000원 단위로 입력해주세요.";
+
+    public int purchasing() {
+        while (true) {
+            try {
+                System.out.println(PURCHASE_INPUT_TEXT);
+                this.money = readNumber();
+                validate1k();
+                this.trials = money / 1_000;
+                System.out.println(purchaseDone());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return this.trials;
+    }
+
+    private int readNumber() {
+        try {
+            String money = Console.readLine();
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_NOT_INTEGER_TEXT);
+        }
+    }
+
+    private void validate1k() {
+        if (this.money % 1_000 != 0) {
+            throw new IllegalArgumentException(ERROR_NOT_1k_TEXT);
+        }
+    }
+
+    private String purchaseDone() {
+        return String.format("%,d%s", this.trials, PURCHASE_DONE_TEXT);
+    }
+}

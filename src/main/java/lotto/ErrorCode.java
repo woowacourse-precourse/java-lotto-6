6ac593b75bcc908079bc66f31616c82ae42b5bbd
DaehoSon <thseogh135@gmail.com>
2023-11-07 16:19:28 +0900

    feat: 로또 번호가 1~45를 벗어날 경우 예외 처리

--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -2,7 +2,8 @@ package lotto;
 
 public enum ErrorCode {
     INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호의 개수는 6개입니다.\n"),
-    INVALID_LOTTO_NUMBER_DUPLICATE("[ERROR] 로또 번호: 중복되면 안 됩니다.")
+    INVALID_LOTTO_NUMBER_DUPLICATE("[ERROR] 로또 번호: 중복되면 안 됩니다."),
+    INVALID_LOTTO_NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호: 1~45 숫자만 가능합니다.")
     ;
 
     private final String value;
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,9 @@ import java.util.Set;
 
 public class Lotto {
 
+    private final int MAX_COUNT = 6;
+    private final int MIN_NUMBER = 1;
+    private final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,12 +16,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != this.MAX_COUNT) {
             throw new IllegalArgumentException(ErrorCode.INVALID_LOTTO_NUMBER_COUNT.getValue());
         }
 
         if (Set.copyOf(numbers).size() != numbers.size()) {
             throw new IllegalArgumentException(ErrorCode.INVALID_LOTTO_NUMBER_DUPLICATE.getValue());
+        }
+
+        for (Integer i : numbers) {
+            if (i < MIN_NUMBER || i > MAX_NUMBER) {
+                throw new IllegalArgumentException(ErrorCode.INVALID_LOTTO_NUMBER_OUT_OF_RANGE.getValue());
+            }
         }
     }
 

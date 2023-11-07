65f9f5c60e2a089deab12edd9e7815df0c2a8cf5
davemins <davemins4125@naver.com>
2023-11-07 15:21:50 +0900

    feat: 게임 실행 기능 추가 GameController에 구입 금액 입력 기능을 추가하고 게임 실행 기능을 추가했습니다.

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,7 +1,19 @@
 package lotto.controller;
 
-public class GameController {
-    public void play() {
+import lotto.service.AmountCalculator;
+import lotto.view.InputView;
 
+public class GameController {
+    private String amount;
+    private int NumberOfLottoPurchased;
+
+    public void play() {
+        getAmount();
+    }
+
+    private void getAmount() {
+        amount = InputView.AMOUNT.getInput();
+        AmountCalculator amountCalculator = new AmountCalculator(Integer.parseInt(amount));
+        NumberOfLottoPurchased = amountCalculator.getNumberOfLottoPurchased();
     }
 }

bbee328253d6d744bfd7a94bd89f90f80d7a1f7b
junseoks-99 <angec5645@naver.com>
2023-11-08 02:23:04 +0900

    feat: 총 수익률 계산 후 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,20 +7,8 @@ import view.OutputView;
 public class Application {
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(1,2,3,4,5,6);
-        List<Integer> list2 = List.of(1,2,3,4,5,6);
-        Lotto lotto = new Lotto(list);
 
-        Integer sameSount = lotto.getSameNumberCount(list2);
-
-        if (sameSount == 6) {
-            RankRepository.increaseNumberOfWins(1);
-            RankRepository.increaseNumberOfWins(1);
-            RankRepository.increaseNumberOfWins(1);
-            RankRepository.increaseNumberOfWins(1);
-            RankRepository.increaseNumberOfWins(1);
-        }
-
-        OutputView.outputWinningStatistics();
+        RankRepository.increaseNumberOfWins(5);
+        System.out.println(RankRepository.getRateOfReturn(8000));
     }
 }
--- src/main/java/repository/RankRepository.java
+++ src/main/java/repository/RankRepository.java
@@ -1,6 +1,7 @@
 package repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RankRepository {
@@ -8,6 +9,8 @@ public class RankRepository {
     private static final Map<Integer, Integer> rankRepository = new HashMap<Integer, Integer>();
     private static final Integer DEFAULT_VALUE = 0;
     private static final Integer INCREMENT_VALUE = 1;
+    private static final Integer START_INDEX = 0;
+    private static final Integer END_INDEX = 5;
 
     public static void increaseNumberOfWins(Integer rank) {
         rankRepository.put(rank, rankRepository.getOrDefault(rank, DEFAULT_VALUE) + INCREMENT_VALUE);
@@ -15,5 +18,17 @@ public class RankRepository {
 
     public static int getWinningStatistics(int rank) {
         return rankRepository.getOrDefault(rank, DEFAULT_VALUE);
+    }
+
+    public static Float getRateOfReturn(int investmentCost) {
+        List<Integer> rankList = List.of(1, 2, 3, 4, 5);
+        List<Integer> winningsList = List.of(2000000000, 30000000, 1500000, 50000, 5000);
+        Long sum = 0L;
+        for (int listIndex = START_INDEX;listIndex<END_INDEX;listIndex++) {
+            Integer winningCount = getWinningStatistics(rankList.get(listIndex));
+            Integer winnings = winningsList.get(listIndex);
+            sum += (long) winningCount * winnings;
+        }
+        return sum / (float) investmentCost * 100;
     }
 }

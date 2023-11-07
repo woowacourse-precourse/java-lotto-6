6b11db63a09fb61804a54898f7f7f66e35b76fb7
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:58:16 +0900

    test(Score) : Score test add

---
+++ src/test/java/lotto/domain/ScoreTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ScoreTest {
+
+    @Test
+    void calculate() {
+    }
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void getTotalProfit() {
+    }
+}

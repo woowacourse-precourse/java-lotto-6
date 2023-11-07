ed05ea9fa3e65d9bd45ad9b7106c613ba5591baf
liarreez <liarreez@gmail.com>
2023-11-07 19:42:16 +0900

    fix : 수익률이 원하던 형태로 표현되지 않았던 문제 해결

--- src/main/java/lotto/model/ReturnCalculator.java
+++ src/main/java/lotto/model/ReturnCalculator.java
@@ -1,12 +1,12 @@
 package lotto.model;
 
 public class ReturnCalculator {
-    private static final int PERCENTAGE_MULTIPLIER = 1000;
+    private static final int PERCENTAGE_MULTIPLIER = 100;
     private static final double ROUNDING_PRECISION = 10.0;
 
     public double calculateRateOfReturn(double rateOfReturn) {
-        rateOfReturn = roundToPrecision(rateOfReturn);
         rateOfReturn *= PERCENTAGE_MULTIPLIER;
+        rateOfReturn = roundToPrecision(rateOfReturn);
         return rateOfReturn;
     }
 

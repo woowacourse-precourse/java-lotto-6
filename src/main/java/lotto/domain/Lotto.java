04da952c5d76a504a525111fcf25950603277d5e
ohhyeon2 <dhgus1235@gmail.com>
2023-11-07 14:03:09 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,6 @@
 package lotto.domain;
 
-import java.util.HashSet;
 import java.util.List;
-
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;

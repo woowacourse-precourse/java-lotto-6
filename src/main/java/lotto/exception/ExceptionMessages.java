3ba197b6ccee09a2a5e3a23c07c5d84e89430b69
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-08 08:44:37 +0900

    feat: 예외 메시지 추가

--- src/main/java/lotto/exception/ExceptionMessages.java
+++ src/main/java/lotto/exception/ExceptionMessages.java
@@ -9,4 +9,6 @@ public class ExceptionMessages {
     public static final String DUPLICATE_LOTTO_MESSAGE = "로또 번호는 중복되지 않아야 합니다.";
     public static final String OUT_OF_RANGE_LOTTO_NUMBER_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String INVALID_MATCH_COUNT_MESSAGE = "3 미만, 6 초과의 숫자는 Prize가 될 수 없습니다.";
+    public static final String ONLY_NUMBER_INPUT_MESSAGE = "숫자만 입력 가능합니다.";
+    public static final String NEGATIVE_NUMBER_INPUT_MESSAGE = "음수는 입력할 수 없습니다.";
 }

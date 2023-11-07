3fc23abc1f1b906f28c1a7b0136e5a1d0d5b421b
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-07 17:42:22 +0900

    refactor: BonusNumberTest 리팩토링 메소드명 수정 및 displayName 설정하였음

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import lotto.domain.BonusNumber;
 import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BonusNumberTest {
+    @DisplayName("보너스 숫자 범위 유효성 테스트")
     @Test
     public void 보너스_숫자가_범위를_벗어났을_때_테스트() {
         int bonusNumber = 500;
@@ -14,6 +16,7 @@ public class BonusNumberTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new BonusNumber(bonusNumber, winningNumbers));
     }
 
+    @DisplayName("보너스 숫자가 당첨번호와 중복되는 경우 테스트")
     @Test
     public void 보너스_숫자가_당첨번호와_중복되는_경우_테스트() {
         int bonusNumber = 27;
@@ -21,8 +24,9 @@ public class BonusNumberTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new BonusNumber(bonusNumber, winningNumbers));
     }
 
+    @DisplayName("유효한 보너스 숫자 테스트")
     @Test
-    public void 유효한_보너스_숫자_테스트() {
+    public void 유효한_보너스_숫자를_넣었을_경우_테스트() {
         int bonusNumber = 7;
         WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         Assertions.assertDoesNotThrow(() -> new BonusNumber(bonusNumber, winningNumbers));

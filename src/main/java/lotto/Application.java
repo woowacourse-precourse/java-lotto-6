d79bc3a6daf71b0bb63d77f2d12a6e6e12670ae1
Jun Hyeok Choi <chrisgold@naver.com>
2023-11-08 00:26:25 +0900

    feat(Application) : add new function 'input_money'

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,10 +5,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.print("구입금액을 입력해 주세요.\n");
-        String input = Console.readLine();
+        String input = input_money();
         int money = String_to_Int(input);
         System.out.print(money);
+    }
+    private static String input_money(){               // input을 String으로 받아 반환
+        System.out.print("구입금액을 입력해 주세요.\n");
+        String input = Console.readLine();
+        return input;
     }
     private static int String_to_Int(String str){      // Input을 String으로 받아서 Int로 반환
         try{

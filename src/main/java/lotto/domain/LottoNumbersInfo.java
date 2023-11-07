0e68b62e7ea167c1010ed1de652f7f784e574edf
alscjf1329 <alscjf1329@gmail.com>
2023-11-07 20:11:31 +0900

    feat(WinningNumbers): WinningNumbers 생성
    당첨 번호와 보너스 번호를 가지는 클래스이며 로또 리스트와 비교하여 결과를 계산해주는 getResultWith 메소드를 작성했습니다. 기존에 getResultWith 메소드는 LottoNumbersInfo에 있었습니다.

--- src/main/java/lotto/domain/LottoNumbersInfo.java
+++ src/main/java/lotto/domain/LottoNumbersInfo.java
@@ -15,32 +15,14 @@ public class LottoNumbersInfo {
     private final List<Lotto> lottos;
 
     public LottoNumbersInfo(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
-        LottoUtil.validateLottoNum(bonusNumber);
+
 
         this.bonusNumber = bonusNumber;
         this.winningNumbers = winningNumbers;
         this.lottos = lottos;
     }
 
-    public HashMap<LottoResult, BigDecimal> getResult() {
-        HashMap<LottoResult, BigDecimal> result = initResult();
-        for (Lotto lotto : lottos) {
-            int matchCount = lotto.getMatchCountWith(winningNumbers);
-            LottoResult lottoResult = LottoResult.of(matchCount, lotto.contains(bonusNumber));
-            BigDecimal count = result.get(lottoResult).add(BigDecimal.ONE);
-            result.put(lottoResult, count);
-        }
-        return result;
-    }
 
-    private HashMap<LottoResult, BigDecimal> initResult() {
-        HashMap<LottoResult, BigDecimal> result = new HashMap<>();
-        result.put(LottoResult.FIRST, BigDecimal.ZERO);
-        result.put(LottoResult.SECOND, BigDecimal.ZERO);
-        result.put(LottoResult.THIRD, BigDecimal.ZERO);
-        result.put(LottoResult.FOURTH, BigDecimal.ZERO);
-        result.put(LottoResult.FIFTH, BigDecimal.ZERO);
-        result.put(LottoResult.NONE, BigDecimal.ZERO);
-        return result;
-    }
+
+
 }
---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import lotto.message.LottoResult;
+
+public class WinningNumbers extends Lotto {
+    private final int bonusNumber;
+
+    public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+        super(numbers);
+        validateLottoNum(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+    public HashMap<LottoResult, BigDecimal> getResultWith(List<Lotto> lottos) {
+        HashMap<LottoResult, BigDecimal> result = initResult();
+        for (Lotto lotto : lottos) {
+            int matchCount = lotto.getMatchCountWith(getNumbers());
+            LottoResult lottoResult = LottoResult.of(matchCount, lotto.contains(bonusNumber));
+            BigDecimal count = result.get(lottoResult).add(BigDecimal.ONE);
+            result.put(lottoResult, count);
+        }
+        return result;
+    }
+
+    private HashMap<LottoResult, BigDecimal> initResult() {
+        HashMap<LottoResult, BigDecimal> result = new HashMap<>();
+        result.put(LottoResult.FIRST, BigDecimal.ZERO);
+        result.put(LottoResult.SECOND, BigDecimal.ZERO);
+        result.put(LottoResult.THIRD, BigDecimal.ZERO);
+        result.put(LottoResult.FOURTH, BigDecimal.ZERO);
+        result.put(LottoResult.FIFTH, BigDecimal.ZERO);
+        result.put(LottoResult.NONE, BigDecimal.ZERO);
+        return result;
+    }
+
+}

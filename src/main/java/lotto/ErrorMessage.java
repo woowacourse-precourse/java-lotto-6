54ee7db0a98394584bb0da6d3a8322f75d64e093
Dobby_Kim <kimdobby@g.skku.edu>
2023-11-07 23:12:15 +0900

    chore - ErrorMessage : 에러 메세지 단순 수정

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,9 +1,9 @@
 package lotto;
 
 
-public final class ErrorMessage{
+public final class ErrorMessage {
     public static final String INVALID_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 1000원 단위여야 합니다.";
-    public static final String INVALID_WINNING_NUMBERS_COUNT = "[ERROR] 당첨 번호는 6개여야 합니다.";
+    public static final String INVALID_WINNING_NUMBERS_COUNT = "[ERROR] 당첨 번호는 정수 6개여야 합니다.";
     public static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 정수여야 합니다.";
     public static final String DUPLICATE_LOTTO_NUMBERS = "[ERROR] 로또 번호는 중복될 수 없습니다.";
     public static final String INVALID_BONUS_NUMBER_RANGE = "[ERROR] 보너스 번호는 1과 45 사이의 숫자여야 합니다.";

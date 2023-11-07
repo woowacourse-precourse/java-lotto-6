a03abac1ed7dca34fc32d09f1441ebaf489dff36
jhyii <83736896+jhyii@users.noreply.github.com>
2023-11-08 07:47:53 +0900

    style : ApplicationTest 복구

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -12,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-	@SuppressWarnings("unchecked")
 	@Test
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(

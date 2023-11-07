f79b8885acf313cb78f9fdea5cb3cc9437948dfe
cozym <ddongyee99@daum.net>
2023-11-08 03:38:56 +0900

    refactor: 입력 번호 개수 검사 기능 추가

--- src/main/java/lotto/model/MyLottoNumbers.java
+++ src/main/java/lotto/model/MyLottoNumbers.java
@@ -7,11 +7,14 @@ import java.util.List;
 public class MyLottoNumbers {
     private static final int MIN_IN_RANGE = 1;
     private static final int MAX_IN_RANGE = 45;
+    private static final int EXACT_SIZE = 6;
     private List<Integer> myNumbers;
     private int bonusNumber;
 
     public MyLottoNumbers(String input) {
-        myNumbers = validateNumber(validateSplit(input));
+        List<Integer> beForeCheck;
+        beForeCheck = validateNumber(validateSplit(input));
+        myNumbers = validateSize(beForeCheck);
     }
 
     private List<String> validateSplit(String input) {
@@ -39,6 +42,13 @@ public class MyLottoNumbers {
             throw new IllegalArgumentException("[ERROR] 1~45의 숫자들만 입력 가능합니다.");
         }
         return number;
+    }
+
+    private List<Integer> validateSize(List<Integer> numbers) {
+        if (numbers.size() != EXACT_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자로 구성되어야 합니다.");
+        }
+        return numbers;
     }
 
     public List<Integer> getMyNumbers() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,11 +5,16 @@ import lotto.model.Lottos;
 
 public class OutputView {
 
-    public static void printLottos(Lottos lottos) {
+    public static void printEmptyLine() {
         System.out.println();
+    }
+
+    public static void printLottos(Lottos lottos) {
+        printEmptyLine();
         System.out.printf("%d개를 구매했습니다.\n",lottos.count());
         for (Lotto lotto : lottos.get()) {
             System.out.println(lotto.getSortedNumbers());
         }
+        printEmptyLine();
     }
 }

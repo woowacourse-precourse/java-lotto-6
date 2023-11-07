27d47a9aab21ca9673aaafe79c953358f6ce74d4
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:45:05 +0900

    refactor(ResultLotto) : Enum 네이밍 변경
    LottoResult -> ResultLotto

---
+++ src/main/java/lotto/domain/ResultLotto.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+public enum ResultLotto {
+
+    ZERO(0),
+    ONE(0),
+    TWO(0),
+    THREE(5_000),
+    FOUR(50_000),
+    FIVE(1_500_000),
+    FIVE_BONUS(30_000_000),
+    SIX(2_000_000_000);
+
+    private int result;
+
+    ResultLotto(int result) {
+        this.result = result;
+    }
+
+    public long getResult() {
+        return result;
+    }
+
+    public static ResultLotto getOrdinal(int ordinal) {
+        return ResultLotto.values()[ordinal];
+    }
+}

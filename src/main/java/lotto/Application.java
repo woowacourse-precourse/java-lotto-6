106a9b850000d0e1856e62c987adbdb8a2313f9a
kihwn <kihwn98@gmail.com>
2023-11-08 03:11:18 +0900

    feat: 1~45 사이의 랜덤 숫자 6개를 뽑는 메서드(makeRandomNumbers()) 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
     public static void main(String[] args) {
 
     }
@@ -22,4 +28,8 @@ public class Application {
         System.out.println("\n" + lottoInit.numberOfLotto + "개를 구매했습니다.");
         return lottoInit;
     }
+    private static List<Integer> makeRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_SIZE);
+    }
+
 }

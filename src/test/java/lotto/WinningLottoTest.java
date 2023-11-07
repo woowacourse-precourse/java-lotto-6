159fecb43bdf2855eacab817ba6a664e569f6081
Son JunHyung <kingjh1125@gmail.com>
2023-11-07 23:25:28 +0900

    test (WinningLottoTest): 테스트 추가
    보너스 입력에 대한 테스트를 추가하였습니다:
    - 숫자 범위에 맞지 않는 숫자가 입력될 경우
    - 당첨 번호와 중복될 경우

---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,62 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningLottoTest extends NsTest{
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6,7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 번호에 범위에 맞지 않는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNumberRange() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,50");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("보너스 숫자가 로또 번호의 범위에 맞지 있으면 예외가 발생한다.")
+    @Test
+    void createBonusByNumberRange() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "50");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("보너스 숫자가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createBonusByDuplicate() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+}

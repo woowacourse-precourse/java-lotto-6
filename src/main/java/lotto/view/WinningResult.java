3bcd96e74fe17ffdf6e09b2813848df8d7a361e1
jiyeon <[leeyeon0908@gmail.com]>
2023-11-08 00:31:20 +0900

    사용자와 당첨로또 번호를 비교하는 클래스

---
+++ src/main/java/lotto/view/WinningResult.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+public class WinningResult {
+
+
+    private final Lotto lotto;
+    private final int bonusball;
+
+    public WinningResult(Lotto lotto, int bonusball) {
+        this.lotto = lotto;
+        this.bonusball = bonusball;
+    }
+
+    public Ranking match(Lotto playerNumber) {
+        int countOfMatch = playerNumber.countMatch(lotto);
+        boolean bonusCheck = playerNumber.containNumber(bonusball);
+        return Ranking.valueOf(countOfMatch, bonusCheck);
+    }
+
+
+}

0ab2625eaa4dfc53941505e9fa367fe7e9532ad7
chaehuichan <eqholic1125@gmail.com>
2023-11-08 01:42:32 +0900

    feat: 로또 번호 유효성 검사 기능 구현

--- src/main/java/lotto/exception/ExceptionInfo.java
+++ src/main/java/lotto/exception/ExceptionInfo.java
@@ -5,7 +5,9 @@ public enum ExceptionInfo {
 	PURCHASE_PRICE_IS_NOT_RIGHT_PRICE("[ERROR] 구입 금액은 1000원 단위여야 합니다."),
 	LOTTO_NUMBERS_OVER_SIX("[ERROR] 로또 번호가 6개를 넘습니다."),
 	WINNING_NUMBERS_ARE_NOT_INTEGER("[ERROR] 당첨 번호는 정수여야 합니다."),
-	BONUS_NUMBER_IS_NOT_INTEGER("[ERROR] 보너스 번호는 정수여야 합니다.");
+	BONUS_NUMBER_IS_NOT_INTEGER("[ERROR] 보너스 번호는 정수여야 합니다."),
+	DUPLICATED_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
+	OUT_OF_RANGE_LOTTO_NUMBER("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.");
 
 	private final String message;
 
--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -2,9 +2,13 @@ package lotto.lotto;
 
 import lotto.exception.ExceptionInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+	private static final int LOTTO_START_NUMBER = 1;
+	private static final int LOTTO_END_NUMBER = 45;
+
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
@@ -13,8 +17,32 @@ public class Lotto {
 	}
 
 	private void validate(List<Integer> numbers) {
+		validateSize(numbers);
+
+		validateDuplicatedNumber(numbers);
+	}
+
+	private void validateSize(List<Integer> numbers) {
 		if (numbers.size() != 6) {
 			throw new IllegalArgumentException(ExceptionInfo.LOTTO_NUMBERS_OVER_SIX.getMessage());
+		}
+	}
+
+	private void validateDuplicatedNumber(List<Integer> numbers) {
+		List<Integer> validNumbers = new ArrayList<>();
+
+		for (Integer number : numbers) {
+			validateInRange(number);
+			if (validNumbers.contains(number)) {
+				throw new IllegalArgumentException(ExceptionInfo.DUPLICATED_LOTTO_NUMBER.getMessage());
+			}
+			validNumbers.add(number);
+		}
+	}
+
+	private void validateInRange(Integer number) {
+		if (number < LOTTO_START_NUMBER  || LOTTO_END_NUMBER < number) {
+			throw new IllegalArgumentException(ExceptionInfo.OUT_OF_RANGE_LOTTO_NUMBER.getMessage());
 		}
 	}
 

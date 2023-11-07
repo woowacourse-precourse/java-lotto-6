03fcda8b9a4058b0093c4d26e2cfcf09149c058c
yy8305 <yy8305@kakao.com>
2023-11-07 17:30:44 +0900

    refactor(ConsolePrinter): 코드 리팩토링

--- src/main/java/lotto/util/ConsolePrinter.java
+++ src/main/java/lotto/util/ConsolePrinter.java
@@ -5,26 +5,26 @@ import lotto.constants.PrintMessages;
 import lotto.domain.Lotto;
 
 public class ConsolePrinter {
-    private static void nullCheck(Object target) {
+    private static void validateNull(Object target) {
         Objects.requireNonNull(target);
     }
 
     public static void showMessage(PrintMessages message) {
-        nullCheck(message);
+        validateNull(message);
 
         System.out.print(message.getMessage());
     }
 
     public static void showMessageWithNumber(PrintMessages message, Integer number) {
-        nullCheck(message);
-        nullCheck(number);
+        validateNull(message);
+        validateNull(number);
 
         String showMessage = String.format(message.getMessage(), number);
         System.out.print(showMessage);
     }
 
     public static void showLottoNumbers(Lotto lotto) {
-        nullCheck(lotto);
+        validateNull(lotto);
 
         PrintMessages messageTemplate = PrintMessages.LOTTO_NUMBERS;
         String showMessage = messageTemplate.getMessageWithLotto(lotto);

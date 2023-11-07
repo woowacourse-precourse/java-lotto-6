c946ba99b676156bd25dd250df46ebc80c661645
PARK9898 <wjddjs21@naver.com>
2023-11-08 03:00:34 +0900

    "style: 불필요한 import 제거"

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

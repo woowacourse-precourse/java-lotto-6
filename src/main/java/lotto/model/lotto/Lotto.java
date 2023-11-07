7ffc8fbe2b22d771bcdc27241349686a870d4eb8
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 22:49:54 +0900

    feat : 로또 숫자 불러오는 기능

--- src/main/java/lotto/model/lotto/Lotto.java
+++ src/main/java/lotto/model/lotto/Lotto.java
@@ -1,13 +1,19 @@
 package lotto.model.lotto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.validator.LottoValidator;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         LottoValidator.validateLotto(numbers);
         this.numbers = numbers;
+    }
+    public List<Integer> getLotto(){
+        return Collections.unmodifiableList(numbers);
     }
 }

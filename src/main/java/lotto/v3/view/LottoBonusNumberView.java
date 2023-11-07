9932eb87cca75f67eb29ffc83b9d47e70d63002e
권장순 <jsoonworld@gmail.com>
2023-11-08 02:34:03 +0900

    feat( LottoBonusNumberView#parseBonusNumber() ) : 한 자리가 아니라 1 ~ 45의 수면 만족.

--- src/main/java/lotto/v3/view/LottoBonusNumberView.java
+++ src/main/java/lotto/v3/view/LottoBonusNumberView.java
@@ -19,6 +19,9 @@ public class LottoBonusNumberView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
         }
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
 
         return number;
     }

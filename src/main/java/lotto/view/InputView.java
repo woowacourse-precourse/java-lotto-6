502d72289a25ea9dfb2fc63efd28f6ae3367890c
Nbowow <nbo5548@naver.com>
2023-11-07 09:48:29 +0900

    Feat: inputView handling error 2023.11.07 당첨 번호 입력 오류시 재입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class InputView {
 
     private static List<Integer> winningNumberList;
+    private static List<Integer> winningnumber;
 
     public static String requestLottoBuyingPrice() {
         System.out.println("구매하실 로또 금액을 입력해주세요.");
@@ -15,15 +16,21 @@ public class InputView {
     }
 
     public static List<Integer> requestLottoWinningNumber() {
-        System.out.println("당첨 번호를 입력해주세요.");
-        return stringToList(Console.readLine());
+        try {
+            System.out.println("당첨 번호를 입력해주세요.");
+            winningnumber = stringToList(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 당첨 번호는 숫자여야 합니다.");
+            requestLottoWinningNumber();
+        }
+        return winningnumber;
     }
 
     private static List<Integer> stringToList(String winningNumber) {
         String[] result = winningNumber.split(",");
         winningNumberList = new ArrayList<>();
-        for (int i = 0; i < result.length; i++) {
-            winningNumberList.add(Integer.parseInt(result[i]));
+        for (String string : result) {
+            winningNumberList.add(Integer.parseInt(string));
         }
         return winningNumberList;
     }

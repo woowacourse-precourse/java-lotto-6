7b2393ba9dad1fc9c55e85d2d34ccccedd37a923
ChoiseU <csw980928@gmail.com>
2023-11-07 16:55:42 +0900

    feat : 통계 이넘 생성

--- src/main/java/lotto/utils/StatisticsMessage.java
+++ src/main/java/lotto/utils/StatisticsMessage.java
@@ -1,16 +1,17 @@
 package lotto.utils;
 
-public enum ExceptionMessage {
-    NUMBER("[ERROR] 숫자만 입력해주세요."),
-    THOUSAND("[ERROR] 천원 단위로 입력해주세요."),
-    WINNING_NUMBER("[ERROR] 숫자와 콤마만 이용하여 입력하세요."),
-    WINNING_NUMBER_TOTAL("[ERROR] 번호를 6개 입력해주세요."),
-    WINNING_NUMBER_RANGE("[ERROR] 번호는 1부터 45까지 입력해주세요."),
-    WINNING_NUMBER_DUPLICATE("[ERROR] 중복된 번호는 입력할 수 없습니다.");
+public enum StatisticsMessage {
+    STATISTICS("당첨 통계"),
+    LINE("---"),
+    THREE("3개 일치 (5,000원) - "),
+    FOUR("4개 일치 (50,000원) - "),
+    FIVE("5개 일치 (1,500,000원) - "),
+    FIVE_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX("6개 일치 (2,000,000,000원) - ");
 
     private String value;
 
-    ExceptionMessage(String value) {
+    StatisticsMessage(String value) {
         this.value = value;
     }
 

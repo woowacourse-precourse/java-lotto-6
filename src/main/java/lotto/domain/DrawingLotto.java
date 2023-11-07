a791aca610d8f4a3ee49ae978b208de8a3a38faf
Choijake <shameless8@naver.com>
2023-11-07 23:23:54 +0900

    Refactor : LottoService -> DrawingLotto 클래스 이름과 역할 변경

--- src/main/java/lotto/domain/DrawingLotto.java
+++ src/main/java/lotto/domain/DrawingLotto.java
@@ -5,23 +5,21 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 //TODO : 클래스 작성 컨벤션 확인하기
-class LottoService {
-    public LottoService() {
-        provideWinningNumber();
-    }
+class DrawingLotto {
+
 
     public void getResult() {
         //TODO : client의 속성에 접근하기 위해 당첨번호와 로또번호를 나타내기 위한 클래스를 만든다 -> 로또 시스템을 나타내는 도메인을 따로 뺀다
         //TODO : setwinningNumber메서드에 파라메터를 둬서 LottoService의 속성을 이용하지 않도록 한다
     }
 
-    public WinnigNumber createWinningNumber() {
+    public static WinnigNumber winnigLotto() {
         List<Integer> winningNumber = provideWinningNumber();
 
-        return WinnigNumber.of(Lotto.from(winningNumber), provideBonusNumber());
+        return WinnigNumber.is(Lotto.of(winningNumber), provideBonusNumber());
     }
 
-    private List<Integer> provideWinningNumber() {
+    private static List<Integer> provideWinningNumber() {
         String WinningNumberString = InputHandler.inputWinningNumber();
         List<Integer> inputNumber = Stream.of(WinningNumberString.split(",\\s*"))
                 .map(Integer::parseInt)
@@ -29,7 +27,7 @@ class LottoService {
         return inputNumber;
     }
 
-    private int provideBonusNumber() {
+    private static int provideBonusNumber() {
         int bonus = InputHandler.inputBonusNumber();
         return bonus;
     }

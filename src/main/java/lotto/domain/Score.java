2f31cba6b78d846304b6ba8ab950c61c8c109daf
gusdn0108 <gusdn6671@naver.com>
2023-11-07 11:38:49 +0900

    refactor(Score) : Controller (findResult)=> Score(calculate)

--- src/main/java/lotto/domain/Score.java
+++ src/main/java/lotto/domain/Score.java
@@ -1,9 +1,24 @@
 package lotto.domain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Score {
     Map<Result, Integer> score = new HashMap<>();
 
+
+    private Result calculate(Lotto lotto, WinningNumber winningNumber) {
+        List<Integer> numbers = lotto.getNumbers();
+        List<Integer> winningLottoNumbers = winningNumber.getLottoNumbers();
+        int bonusNumber = winningNumber.getBonusNumber();
+        int matchCount = 0;
+        for(int number : numbers) {
+            if(winningLottoNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        boolean isBonusMatched = numbers.contains(bonusNumber);
+        return Result.find(matchCount, isBonusMatched);
+    }
 }

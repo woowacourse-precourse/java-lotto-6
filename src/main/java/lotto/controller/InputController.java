549fa3d16053d9f58f99fd45e60ad0ef5c1065de
hoyeon <highoption@naver.com>
2023-11-07 18:37:14 +0900

    feat(InputController) : Store 객체를 만드는 makeStore 생성

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.model.Money;
+import lotto.model.Store;
 import lotto.view.InputView;
 
 public class InputController {
@@ -13,6 +14,16 @@ public class InputController {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return makeMoney();
+        }
+    }
+
+    private static Store makeStore(Money money) {
+        try {
+            Store store = new Store(money);
+            return store;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return makeStore(money);
         }
     }
 }

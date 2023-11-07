7c6d6c5f8d7c90a8c4b3ad85f81ef3d951513a10
devhyeon0 <devhyeon0@gmail.com>
2023-11-07 14:58:58 +0900

    feat: 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,28 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_LUCKY_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public int getAmount() {
+        System.out.println(INPUT_AMOUNT_MESSAGE);
+
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public String getLuckyNumber() {
+        System.out.println(INPUT_LUCKY_NUMBER_MESSAGE);
+
+        return Console.readLine();
+    }
+
+    public int getBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+
+        return Integer.parseInt(Console.readLine());
+    }
 }

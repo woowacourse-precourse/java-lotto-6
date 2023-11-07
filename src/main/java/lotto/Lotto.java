6a081fd6062f00ed319607702b98efb614758750
aammddkkzxc <ideal0427@naver.com>
2023-11-07 18:03:19 +0900

    refactor: Lotto유효성 검사 기능 관련 네이밍 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,38 +8,38 @@ import java.util.Set;
 public class Lotto {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
-    private static final String CHECK_LOTTO_SIZE_REQUEST_MESSAGE = "로또 번호는 총 6개여야 합니다.";
-    private static final String CHECK_LOTTO_NUMBER_RANGE_REQUEST_MESSAGE = "로또 번호는 1~45 사이의 숫자여야 합니다.";
-    private static final String CHECK_EACH_LOTTO_NUMBER_NOT_OVERLAP_REQUEST_MESSAGE = "로또 번호는 서로 중복되지 않아야 합니다.";
+    private static final String CHECK_SIZE_REQUEST_MESSAGE = "로또 번호는 총 6개여야 합니다.";
+    private static final String CHECK_NUMBER_RANGE_REQUEST_MESSAGE = "로또 번호는 1~45 사이의 숫자여야 합니다.";
+    private static final String CHECK_EACH_NUMBER_NOT_OVERLAP_REQUEST_MESSAGE = "로또 번호는 서로 중복되지 않아야 합니다.";
     private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLottoSize(numbers);
-        validateLottoNumberRange(numbers);
-        validateEachLottoNumberNotOverlap(numbers);
+        validateSize(numbers);
+        validateNumberRange(numbers);
+        validateEachNumberNotOverlap(numbers);
         this.numbers = numbers;
     }
 
-    private void validateLottoSize(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException(CHECK_LOTTO_SIZE_REQUEST_MESSAGE);
+            throw new IllegalArgumentException(CHECK_SIZE_REQUEST_MESSAGE);
         }
     }
 
-    private void validateLottoNumberRange(List<Integer> numbers) {
+    private void validateNumberRange(List<Integer> numbers) {
         for (Integer number : numbers) {
             if ((number < LOTTO_MIN_NUMBER) || (number > LOTTO_MAX_NUMBER)) {
-                throw new IllegalArgumentException(CHECK_LOTTO_NUMBER_RANGE_REQUEST_MESSAGE);
+                throw new IllegalArgumentException(CHECK_NUMBER_RANGE_REQUEST_MESSAGE);
             }
         }
     }
 
-    private void validateEachLottoNumberNotOverlap(List<Integer> numbers) {
+    private void validateEachNumberNotOverlap(List<Integer> numbers) {
         for (Integer number : numbers) {
             if (numbers.contains(number)) {
-                throw new IllegalArgumentException(CHECK_EACH_LOTTO_NUMBER_NOT_OVERLAP_REQUEST_MESSAGE);
+                throw new IllegalArgumentException(CHECK_EACH_NUMBER_NOT_OVERLAP_REQUEST_MESSAGE);
             }
         }
     }

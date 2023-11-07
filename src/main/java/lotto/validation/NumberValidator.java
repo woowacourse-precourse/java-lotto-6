6c2e88aa34fdc559485522f7ea6ad8ae85f86588
박승민 <tmddudals369@naver.com>
2023-11-08 01:48:54 +0900

    feat(numberValidator): 번호에 대한 예외 처리 기능 추가
    빈 문자열, 공백 문자 포함 예외 처리 기능 추가

--- src/main/java/lotto/validation/NumberValidator.java
+++ src/main/java/lotto/validation/NumberValidator.java
@@ -4,16 +4,31 @@ import java.util.regex.Pattern;
 
 public class NumberValidator {
     private static final String POSITIVE_DIGIT_REGEX = "^[1-9]+$";
+
     private NumberValidator() {
     }
 
     public static void validateNumber(String number) {
         isPositiveDigit(number);
+        isBlank(number);
+        isContainBlank(number);
     }
 
     private static void isPositiveDigit(String number) {
         if (!Pattern.matches(POSITIVE_DIGIT_REGEX, number)) {
             throw new IllegalArgumentException("[ERROR] 번호는 양의 정수만 입력할 수 있습니다.");
+        }
+    }
+
+    private static void isBlank(String winningNumber) {
+        if (winningNumber.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 번호는 공백일 수 없습니다.");
+        }
+    }
+
+    private static void isContainBlank(String winningNumber) {
+        if (winningNumber.contains(" ")) {
+            throw new IllegalArgumentException("[ERROR] 번호는 공백이 포함될 수 없습니다.");
         }
     }
 }

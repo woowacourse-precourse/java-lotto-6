ff290b5c0e0d7584b74dc7c387ddd26e1bffd216
KangDeok <rkdejr2321@naver.com>
2023-11-08 02:40:35 +0900

    [chore] 코드 컨벤션

--- src/main/java/lotto/domain/LottoConstant.java
+++ src/main/java/lotto/domain/LottoConstant.java
@@ -10,10 +10,11 @@ public class LottoConstant {
     public static final int THIRD_WINNER_HIT_COUNT = 5;
     public static final int FORTH_WINNER_HIT_COUNT = 4;
     public static final int FIFTH_WINNER_HIT_COUNT = 3;
-    public static final int PRICE_PER_LOTTO = 1000;
+    public static final int PRICE_PER_LOTTO = 1_000;
     public static final int FIRST_WINNER_AMOUNT = 2_000_000_000;
     public static final int SECOND_WINNER_AMOUNT = 30_000_000;
     public static final int THIRD_WINNER_AMOUNT = 1_500_00;
     public static final int FORTH_WINNER_AMOUNT = 50_000;
     public static final int FIFTH_WINNER_AMOUNT = 5_000;
+    public static final String LOTTO_NUMBER_SEPARATOR = ",";
 }

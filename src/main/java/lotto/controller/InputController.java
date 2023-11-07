82f6973fd8185756a65ed227b84d27d83b4f422c
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:42:01 +0900

    feat(InputController): 로또 구입 금액에 대한 예외처리 및 재입력을 받는 함수 추가

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,8 +1,20 @@
 package lotto.controller;
 
+import lotto.domain.TicketAmount;
 import lotto.util.Validator;
+import lotto.view.InputView;
 
 public class InputController {
+    public TicketAmount setTicketAmount() {
+        try {
+            String input = InputView.inputMoney();
+            validateInput(input);
+            return new TicketAmount(input);
+        } catch (IllegalArgumentException e) {
+            return setTicketAmount();
+        }
+    }
+
     private void validateInput(String input) {
         Validator.validateInputNotEmpty(input);
         Validator.validateIsParsableToInt(input);

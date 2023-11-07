41ef64fe0d6a4094a2222a56fd9876b0407fae3b
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-07 15:49:35 +0900

    fix: 문자열 가이드 상수 수정
    당청 번호 입력 가이드, 보너스 번호 입력 가이드, 당첨 통계 가이드 상수 앞에 개행 문자를 추가한다.

--- src/main/java/lotto/MessageConstants.java
+++ src/main/java/lotto/MessageConstants.java
@@ -2,10 +2,10 @@ package lotto;
 
 public final class MessageConstants {
     public static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
-    public static final String WINNING_NUMBERS_GUIDE = "당첨 번호를 입력해 주세요.";
-    public static final String BONUS_NUMBER_GUIDE = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_NUMBERS_GUIDE = "\n당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_GUIDE = "\n보너스 번호를 입력해 주세요.";
 
-    public static final String WINNING_GUIDE = "당첨 통계\n---";
+    public static final String WINNING_GUIDE = "\n당첨 통계\n---";
     public static final String THREE_MATCH = "3개 일치 (5,000원) - ";
     public static final String FOUR_MATCH = "4개 일치 (50,000원) - ";
     public static final String FIVE_MATCH = "5개 일치 (1,500,000원) - ";

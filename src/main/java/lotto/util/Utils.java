b2acafb8d8e14f57ee65c9ae6a13cb72298d31e8
sehee <tpgml701@naver.com>
2023-11-07 09:21:26 +0900

    feat(util): List를 String으로 바꾸는 기능

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -2,7 +2,9 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Utils {
 
@@ -12,6 +14,12 @@ public final class Utils {
 
     public static int stringToInteger(String string) {
         return Integer.parseInt(string);
+    }
+
+    public static List<Integer> stringToIntegerList(String[] strings) {
+        return Arrays.stream(strings)
+                .map(Integer::parseInt) // Convert strings to integers
+                .collect(Collectors.toList());
     }
 
 

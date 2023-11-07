f711156985bdffef38d8288ad295095202091ea3
Zho <chlwlgh1011@naver.com>
2023-11-07 20:48:50 +0900

    feat: 로또 구매 가격, 구매 가격에 따른 개수 출력하기

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,12 +1,20 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
 import lotto.view.View;
 
+import java.util.ArrayList;
+
 public class LottoGameController {
+    static ArrayList<ArrayList<Integer>> lottos;
+;
     public LottoGameController () {
         View.printStartMessage();
     }
 
-    public void lottoGameFlow() {}
-
+    public void lottoGameFlow() {
+        View.inputTotalCost();
+        View.printNumberOfLotto();
+        View.printlottos();
+    }
 }
--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,11 +1,20 @@
 package lotto.view;
-public class View {
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.LottoGet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class View {
+    static int totalCost;
+    static int numberOfgetTotalCost;
     public enum PrintOutput {
         START("구입금액을 입력해 주세요."),
         WINNINGNUMBER("당첨 번호를 입력해 주세요."),
         BONUSNUMBER("보너스 번호를 입력해 주세요."),
         STATISTICS("당첨 통계"),
+        NUMBEROFLOTTO("개를 구매했습니다."),
         ;
         private final String label;
 
@@ -19,5 +28,36 @@ public class View {
     }
     public static void printStartMessage () {
         System.out.println(PrintOutput.START.label);
+    }
+
+    public static void printNumberOfLotto () {
+        numberOfgetTotalCost = getTotalNumber();
+        System.out.println(numberOfgetTotalCost + PrintOutput.NUMBEROFLOTTO.label);
+    }
+
+    public static void printlottos() {
+        int[][] lottos = LottoGet.getLottos();
+        for(int i = 0; i < numberOfgetTotalCost; i++) {
+            System.out.println(Arrays.toString(lottos[i]));
+        }
+    }
+
+    public static void inputTotalCost () {
+         totalCost = Integer.parseInt(getInput());
+    }
+
+    public static int getTotalCost () {
+        return totalCost;
+    }
+
+    public static int getTotalNumber () {
+        if(totalCost % 10 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요");
+        }
+        return totalCost / 1000;
+    }
+
+    private static String getInput () {
+        return Console.readLine();
     }
 }

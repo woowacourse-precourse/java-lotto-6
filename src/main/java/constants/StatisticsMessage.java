c04869f1a30bfdd94c5a3117a8152274012545fb
nayonsoso <panda0329@naver.com>
2023-11-08 05:42:52 +0900

    feat: 통계 관련 메세지 상수화

---
+++ src/main/java/constants/StatisticsMessage.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class StatisticsMessage {
+    private static final String START = "당첨 통계";
+    private static final String START_LINE = "---";
+    private static final String CONTENT = "%d개 일치 (%s원) - %d개";
+    private static final String RESULT = "총 수익률은 %.2f%입니다.";
+}

92ca3bf358e1f77697abe69f4cf6499f5accff7b
yunseung <yseungun.21@gmail.com>
2023-11-08 02:38:17 +0900

    chore(ErrorMessage): 에러메시지 수정

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -4,5 +4,5 @@ public class ErrorMessage {
     private final static String SYMBOL = "[ERROR] ";
     public final static String INVALID_NUMBER_COUNT = SYMBOL + "입력된 번호가 6개가 아닙니다. 다시 입력하세요.";
     public final static String NOT_NUMBER = SYMBOL + "숫자가 아닌 것을 입력했습니다. 다시 입력하세요.";
-    public final static String NOT_DIVISIBLE_BY_1000 = SYMBOL + "1,000원으로 나누어 떨어져야 합니다. 다시 입력하세요.";
+    public final static String NOT_DIVISIBLE_BY_1000 = SYMBOL + "구입 금액은 1000원 단위여야 합니다. 다시 입력하세요.";
 }

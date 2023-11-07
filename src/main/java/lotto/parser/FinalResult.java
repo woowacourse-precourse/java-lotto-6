f55bd6cafcc103da2c3895fefcfa7ec3269b4bab
heesane <tees3359@gmail.com>
2023-11-07 15:47:19 +0900

    refactor(FinalResult) : 출력 부분 View로 이동

--- src/main/java/lotto/parser/FinalResult.java
+++ src/main/java/lotto/parser/FinalResult.java
@@ -1,37 +1,17 @@
 package lotto.parser;
 import lotto.model.LottoResult;
-import java.util.List;
-import static lotto.view.constant.LottoGradeMessage.*;
-import static lotto.view.constant.LottoMessage.SHOW_STATISTICS_YIELD;
-import static lotto.view.constant.LottoMessage.SHOW_STATISTICS_YIELD_SUFFIX;
-public class FinalResult {
-    private final Integer PERCENT = 100;
-    private final LottoResult lottoResult;
+public record FinalResult(LottoResult lottoResult) {
+
     public FinalResult(final LottoResult lottoResult) {
         this.lottoResult = lottoResult;
         sumOfResult();
     }
+
     private void sumOfResult() {
         this.lottoResult.countingMatchCount();
         this.lottoResult.matchingCount(3);
         this.lottoResult.matchingCount(4);
         this.lottoResult.matchingCount(5);
         this.lottoResult.matchingCount(6);
-    }
-    public void printResult() {
-        List<Integer> countList = this.lottoResult.getCountList();
-        System.out.println(THREE_GRADE.getComment(countList.get(0)));
-        System.out.println(FOUR_GRADE.getComment(countList.get(1)));
-        System.out.println(FIVE_GRADE.getComment(countList.get(2)));
-        System.out.println(FIVE_GRADE_AND_BONUS.getComment(countList.get(3)));
-        System.out.println(SIX_GRADE.getComment(countList.get(4)));
-    }
-    public void printProfitRate(Integer price) {
-        Integer sum = this.lottoResult.getCountList().get(0) * THREE_GRADE.getPrize()
-                + this.lottoResult.getCountList().get(1) * FOUR_GRADE.getPrize()
-                + this.lottoResult.getCountList().get(2) * FIVE_GRADE.getPrize()
-                + this.lottoResult.getCountList().get(3) * FIVE_GRADE_AND_BONUS.getPrize()
-                + this.lottoResult.getCountList().get(4) * SIX_GRADE.getPrize();
-        System.out.println(SHOW_STATISTICS_YIELD.getMessage() + (float)sum / price * PERCENT + SHOW_STATISTICS_YIELD_SUFFIX.getMessage());
     }
 }
--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -5,10 +5,21 @@ import lotto.model.Lottos;
 import lotto.parser.FinalResult;
 import lotto.view.constant.LottoMessage;
 
+import java.util.List;
+
+import static lotto.view.constant.LottoGradeMessage.*;
+import static lotto.view.constant.LottoGradeMessage.SIX_GRADE;
+import static lotto.view.constant.LottoMessage.SHOW_STATISTICS_YIELD;
+import static lotto.view.constant.LottoMessage.SHOW_STATISTICS_YIELD_SUFFIX;
+
 
 public class LottoView {
+    private static final Integer PERCENT = 100;
     public static void printConstantMessage(LottoMessage lottoMessage){
         System.out.println(lottoMessage.getMessage());
+    }
+    public static void printVariableMessage(String message){
+        System.out.println(message);
     }
     public static String requestInputPrice(){
         printConstantMessage(LottoMessage.REQUEST_PURCHASE_AMOUNT);
@@ -36,8 +47,25 @@ public class LottoView {
         printConstantMessage(LottoMessage.SHOW_STATISTICS_DELIMITER);
     }
     public static void printResultMessage(FinalResult finalResult, int price){
-        finalResult.printResult();
-        finalResult.printProfitRate(price);
+        printResult(finalResult);
+        printProfitRate(finalResult,price);
+    }
+    public static void printResult(FinalResult finalResult) {
+        List<Integer> countList = finalResult.lottoResult().getCountList();
+        printVariableMessage(THREE_GRADE.getComment(countList.get(0)));
+        printVariableMessage(FOUR_GRADE.getComment(countList.get(1)));
+        printVariableMessage(FIVE_GRADE.getComment(countList.get(2)));
+        printVariableMessage(FIVE_GRADE_AND_BONUS.getComment(countList.get(3)));
+        printVariableMessage(SIX_GRADE.getComment(countList.get(4)));
+    }
+    public static void printProfitRate(FinalResult finalResult, Integer price) {
+        List<Integer> countList = finalResult.lottoResult().getCountList();
+        int sum = countList.get(0) * THREE_GRADE.getPrize()
+                + countList.get(1) * FOUR_GRADE.getPrize()
+                + countList.get(2) * FIVE_GRADE.getPrize()
+                + countList.get(3) * FIVE_GRADE_AND_BONUS.getPrize()
+                + countList.get(4) * SIX_GRADE.getPrize();
+        System.out.println(SHOW_STATISTICS_YIELD.getMessage() + (float)sum / price * PERCENT + SHOW_STATISTICS_YIELD_SUFFIX.getMessage());
     }
     private static void printWhiteSpace() {
         System.out.println();

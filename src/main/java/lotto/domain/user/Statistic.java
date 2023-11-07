f10a2c113756584dac9e8f00e8fcebe25cb18abe
박승찬 <seungchan141414@gmail.com>
2023-11-07 20:25:25 +0900

    [Refactor(lotto)] enum의 상수 관리
    이 렇게 하는 것이 맞는가 모르겠다. enum은 싱글콘이라 모든 클래스가 공유를 하는? 구조라는

--- src/main/java/lotto/domain/user/Statistic.java
+++ src/main/java/lotto/domain/user/Statistic.java
@@ -6,36 +6,46 @@ import lotto.domain.lotto.LottoEnvelop;
 import lotto.domain.num.LottoTargetNumResults;
 import lotto.utill.Utii;
 
+import java.util.List;
+
 public class Statistic {
     // TODO: 11/6/23 상수 정리
-    private static final Integer MONEY_FIRST_PLACE = 2000000000;
-    private static final Integer MONEY_SECOND_PLACE = 30000000;
-    private static final Integer MONEY_THIRD_PLACE = 1500000;
-    private static final Integer MONEY_FOURTH_PLACE = 50000;
-    private static final Integer MONEY_FIFTH_PLACE = 5000;
     private static final Integer TARGET_COUNT_SIX = 6;
     private static final Integer TARGET_COUNT_FIVE = 5;
     private static final Integer TARGET_COUNT_FOUR = 4;
     private static final Integer TARGET_COUNT_THREE = 3;
-    private static final Integer ADD_COUNT = 1;
+    private static final String LOTTO_WIN_FIRST_PLACE = LottoRank.MONEY_FIRST_PLACE.getPlace();
+    private static final String LOTTO_WIN_SECOND_PLACE = LottoRank.MONEY_SECOND_PLACE.getPlace();
+    private static final String LOTTO_WIN_THIRD_PLACE = LottoRank.MONEY_THIRD_PLACE.getPlace();
+    private static final String LOTTO_WIN_FOURTH_PLACE = LottoRank.MONEY_FOURTH_PLACE.getPlace();
+    private static final String LOTTO_WIN_FIFTH_PLACE = LottoRank.MONEY_FIFTH_PLACE.getPlace();
     private LottoEnvelop lottoEnvelop;
     private Judgment judgment;
-    private Integer countOfFirstPlace;
-    private Integer countOfSecondPlace;
-    private Integer countOfThirdPlace;
-    private Integer countOfFourthPlace;
-    private Integer countOfFifthPlace;
     private Integer totalWinMoney;
+
+    private List<LottoRank> lottoRanks;
 
     public Statistic(LottoEnvelop lottoEnvelop, LottoTargetNumResults lottoTargetNumResults) {
         this.lottoEnvelop = lottoEnvelop;
         this.judgment = Config.judgment(lottoTargetNumResults);
-        this.countOfFirstPlace = 0;
-        this.countOfSecondPlace = 0;
-        this.countOfThirdPlace = 0;
-        this.countOfFourthPlace = 0;
-        this.countOfFifthPlace = 0;
         this.totalWinMoney = 0;
+        this.lottoRanks = List.of(LottoRank.MONEY_FIRST_PLACE, LottoRank.MONEY_SECOND_PLACE,
+                LottoRank.MONEY_THIRD_PLACE, LottoRank.MONEY_FOURTH_PLACE, LottoRank.MONEY_FIFTH_PLACE);
+    }
+
+    public StringBuilder show() {
+        StringBuilder resultString = new StringBuilder();
+
+        // TODO: 11/6/23 상수 정리
+        execution();
+
+        resultString.append("3개 일치 (5,000원) - " + getCount(LOTTO_WIN_FIFTH_PLACE) + "개\n");
+        resultString.append("4개 일치 (50,000원) - " + getCount(LOTTO_WIN_FOURTH_PLACE) + "개\n");
+        resultString.append("5개 일치 (1,500,000원) - " + getCount(LOTTO_WIN_THIRD_PLACE) + "개\n");
+        resultString.append("5개 일치, 보너스 볼 일치 (30,000,000원) - " + getCount(LOTTO_WIN_SECOND_PLACE) + "개\n");
+        resultString.append("6개 일치 (2,000,000,000원) - " + getCount(LOTTO_WIN_FIRST_PLACE) + "개\n");
+
+        return resultString;
     }
 
     private void execution() {
@@ -53,27 +63,12 @@ public class Statistic {
         }
     }
 
-    public StringBuilder show() {
-        StringBuilder resultString = new StringBuilder();
-
-        // TODO: 11/6/23 상수 정리
-        execution();
-        resultString.append("3개 일치 (5,000원) - " + countOfFifthPlace + "개\n");
-        resultString.append("4개 일치 (50,000원) - " + countOfFourthPlace + "개\n");
-        resultString.append("5개 일치 (1,500,000원) - " + countOfThirdPlace + "개\n");
-        resultString.append("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countOfSecondPlace + "개\n");
-        resultString.append("6개 일치 (2,000,000,000원) - " + countOfFirstPlace + "개\n");
-
-        return resultString;
-    }
-
     // TODO: 11/6/23 상수 정리
     private void calculateTotalWinMoney() {
-        totalWinMoney = totalWinMoney + (countOfFifthPlace * MONEY_FIFTH_PLACE);
-        totalWinMoney = totalWinMoney + (countOfFourthPlace * MONEY_FOURTH_PLACE);
-        totalWinMoney = totalWinMoney + (countOfThirdPlace * MONEY_THIRD_PLACE);
-        totalWinMoney = totalWinMoney + (countOfSecondPlace * MONEY_SECOND_PLACE);
-        totalWinMoney = totalWinMoney + (countOfFirstPlace * MONEY_FIRST_PLACE);
+
+        for (LottoRank lottoRank : lottoRanks) {
+            totalWinMoney = totalWinMoney + lottoRank.getWinMoney();
+        }
     }
 
     public Integer getTotalWinMoney() {
@@ -84,32 +79,50 @@ public class Statistic {
 
     private void isFirstPlace(Integer countSame) {
         if (isSameCount(countSame, TARGET_COUNT_SIX)) {
-            countOfFirstPlace = countOfFirstPlace + ADD_COUNT;
+            increaseCount(LOTTO_WIN_FIRST_PLACE);
         }
     }
 
     private void isSecondPlace(Integer countSame, Boolean isSameBonus) {
         if (isSameCount(countSame, TARGET_COUNT_FIVE) && isSameBonus) {
-            countOfSecondPlace = countOfSecondPlace + ADD_COUNT;
+            increaseCount(LOTTO_WIN_SECOND_PLACE);
         }
     }
 
     private void isThirdPlace(Integer countSame, Boolean isSameBonus) {
         if (isSameCount(countSame, TARGET_COUNT_FIVE) && !isSameBonus) {
-            countOfThirdPlace = countOfThirdPlace + ADD_COUNT;
+            increaseCount(LOTTO_WIN_THIRD_PLACE);
         }
     }
 
     private void isFourthPlace(Integer countSame) {
         if (isSameCount(countSame, TARGET_COUNT_FOUR)) {
-            countOfFourthPlace = countOfFourthPlace + ADD_COUNT;
+            increaseCount(LOTTO_WIN_FOURTH_PLACE);
         }
     }
 
+
     private void isFifthPlace(Integer countSame) {
         if (isSameCount(countSame, TARGET_COUNT_THREE)) {
-            countOfFifthPlace = countOfFifthPlace + ADD_COUNT;
+            increaseCount(LOTTO_WIN_FIFTH_PLACE);
         }
+    }
+
+    private void increaseCount(String place) {
+        for (LottoRank lottoRank : lottoRanks) {
+            if (lottoRank.getPlace().equals(place)) {
+                lottoRank.increaseCount();
+            }
+        }
+    }
+
+    private Integer getCount(String place) {
+        for (LottoRank lottoRank : lottoRanks) {
+            if (lottoRank.getPlace().equals(place)) {
+                return lottoRank.getCount();
+            }
+        }
+        return 0;
     }
 
     private boolean isSameCount(Integer countSame, Integer targetCount) {

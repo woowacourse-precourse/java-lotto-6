7cca115f530c1b9ee66258f62680a0f0e0290c91
ppusda <ppusda1234@gmail.com>
2023-11-07 10:22:34 +0900

    fix: 예외 처리 시 출력 방식 변경

--- src/main/java/System/Exception.java
+++ src/main/java/System/Exception.java
@@ -1,6 +1,5 @@
 package System;
 public enum Exception {
-    ERROR("[ERROR] "),
     LOTTO_PURCHASE_EXCEPTION("로또 구매 금액은 1,000원 단위로 입력해주세요.");
 
     Exception(String message) {
@@ -8,7 +7,7 @@ public enum Exception {
     }
 
     public String getMessage() {
-        return ERROR.getMessage() + message;
+        return "[ERROR] " + message;
     }
 
     private final String message;

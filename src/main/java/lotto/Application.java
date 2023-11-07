505de8e13607f96385f90481f41ce59fc43801d9
doublekm153 <doublekm153@gmail.com>
2023-11-08 07:03:06 +0900

    사용자로부터 로또 구매 금액을 입력받고 결과 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+        LottoMachine lottoMachine = new LottoMachine(purchaseAmount);
+        List<Lotto> lottos = lottoMachine.purchaseLottos();
+        
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }

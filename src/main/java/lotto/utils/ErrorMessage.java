6ab139f51c7c11946b96f6b8a6d7402ccce8ba9e
gusdn0108 <gusdn6671@naver.com>
2023-11-07 14:47:21 +0900

    feat(ErrorMessage) : [ERROR] 1000원 단위로 구입가능합니다 출력 형식 추가

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -4,11 +4,13 @@ public enum ErrorMessage {
     USER_INSERT_ONLY_NUMBER("[ERROR] 숫자만 입력 가능합니다"),
     USER_INSERT_WINNING_NUMBER_ONLY_NUMBER("[ERROR] 당첨번호는 숫자만 입력 가능합니다"),
     USER_INSERT_BONUS_ONLY_NUMBER("[ERROR] 숫자만 입력 가능합니다"),
+    USER_PRICE_UNIT_TO_ONE_THOUSAND("[ERROR] 1000원 단위로 구입가능합니다 "),
     LOTTO_NUMBER_SIZE_RANGE_ERROR("[ERROR] 로또 번호의 범위는 1-45 입니다"),
     LOTTO_NUMBER_NOT_DUPLICATE("[ERROR] 로또 번호는 중복 될수 없습니다"),
     LOTTO_NUMBER_NOT_SIX_SIZE("[ERROR] 로또 번호는 6자리 입니다"),
     LOTTO_NUMBER_BONUS_NUMBER_NOT_DUPLICATE("[ERROR] 보너스는 로또 번호와 중복될 수 없습니다."),
     BONUS_NUMBER_RANGE_ERROR("[ERROR] 보너스 숫자의 범위는 1-45까지 입니다 ");
+
 
     private final String message;
 

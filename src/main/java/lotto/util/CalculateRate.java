f59fbdf6fcc931591130b80097fb81fe42953f4c
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-08 05:20:39 +0900

    feat: 총 수익률 계산 클래스 추가

---
+++ src/main/java/lotto/util/CalculateRate.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class CalculateRate {
+
+    public static Double getTotalRate(int money, int totalReward){
+        double totalRate = ((double) totalReward / money) * 100;
+        return totalRate;
+    }
+}

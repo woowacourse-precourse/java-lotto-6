f84f7bea8c623275d598078aa184ad59c878367c
Kim JunHyun <jjag1015@naver.com>
2023-11-08 03:14:53 +0900

    feat(lotto): 입력 에러시 재입력 요구 기능 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -5,23 +5,26 @@ import lotto.domain.Calculator;
 import lotto.domain.Evaluator;
 import lotto.domain.Lotto;
 import lotto.domain.LottoFactory;
-import lotto.domain.BonusNumber;
 import lotto.domain.LottoTicket;
 import lotto.domain.Money;
 import lotto.domain.Prize;
+import lotto.exception.InvalidNumberScopeException;
 import lotto.view.ConsoleInput;
 import lotto.view.ConsoleOutput;
 
 public class LottoGame {
+    private static final Integer MAXIMUM_LOTTO_NUMBER = 45;
+    private static final Integer MINIMUM_LOTTO_NUMBER = 1;
+
     public static void run() {
         Money amount = getLottoAmount();
         Integer numberOfLotto = Calculator.getNumberOfLotto(amount.getMoney());
 
-        LottoTicket lottoTicket = LottoFactory.makeNewLottos(numberOfLotto);
+        LottoTicket lottoTicket = LottoFactory.makeNewLottoTicket(numberOfLotto);
         ConsoleOutput.printLottoTickets(lottoTicket);
 
         Lotto winningLotto = getLotto();
-        BonusNumber bonusNumber = getLottoBonusNumber();
+        Integer bonusNumber = getLottoBonusNumber();
 
         Evaluator evaluator = new Evaluator(winningLotto, bonusNumber);
         evaluator.judgeLottoTicket(lottoTicket);
@@ -34,19 +37,51 @@ public class LottoGame {
     }
 
     private static Money getLottoAmount() {
-        ConsoleOutput.askLottoAmount();
-        Integer amount = ConsoleInput.readLottoAmount();
-        return new Money(amount);
+        while (true) {
+            try {
+                ConsoleOutput.askLottoAmount();
+                Integer amount = ConsoleInput.readLottoAmount();
+                return new Money(amount);
+
+            } catch (IllegalArgumentException e) {
+                ConsoleOutput.printError(e);
+            }
+        }
     }
 
     private static Lotto getLotto() {
-        ConsoleOutput.askWinningLottoNumbers();
-        return ConsoleInput.readLottoNumbers();
+        while (true) {
+            try {
+                ConsoleOutput.askWinningLottoNumbers();
+                return ConsoleInput.readLottoNumbers();
+
+            } catch (IllegalArgumentException e) {
+                ConsoleOutput.printError(e);
+            }
+        }
     }
 
-    private static BonusNumber getLottoBonusNumber() {
-        ConsoleOutput.askLottoBonusNumber();
-        Integer bonusNumber = ConsoleInput.readLottoBonusNumber();
-        return new BonusNumber(bonusNumber);
+    private static Integer getLottoBonusNumber() {
+        while (true) {
+            try {
+                ConsoleOutput.askLottoBonusNumber();
+                Integer bonusNumber = ConsoleInput.readLottoBonusNumber();
+                checkNumberScope(bonusNumber);
+
+                return bonusNumber;
+
+            } catch (IllegalArgumentException e) {
+                ConsoleOutput.printError(e);
+            }
+        }
+    }
+
+    private static void checkNumberScope(Integer number) {
+        if (number < MINIMUM_LOTTO_NUMBER) {
+            throw new InvalidNumberScopeException();
+        }
+        if (number > MAXIMUM_LOTTO_NUMBER) {
+            throw new InvalidNumberScopeException();
+        }
     }
 }

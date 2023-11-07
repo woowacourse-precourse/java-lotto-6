ffd7d3c50264e7accd955cc5d157a507f572fa93
bbungjin <leejintwo12@naver.com>
2023-11-07 15:29:44 +0900

    winPice 메소드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -19,59 +19,77 @@ public class Application {
 
         List<List<Integer>> lotto = new ArrayList<>();
         getLotto(lotto, lottoNum);
-        printLotto(lotto,lottoNum);
+        printLotto(lotto, lottoNum);
 
         System.out.println("당첨 번호를 입려해 주세요.");
         String[] input = readLine().split(",");
         List<Integer> userLotto = new ArrayList<>();
-        for(String list : input){
+        for (String list : input) {
             userLotto.add(Integer.valueOf(list));
         }
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         //예외
         userLotto.add(Integer.valueOf(readLine()));
-
-        System.out.println(userLotto);
-        System.out.println(lotto);
-        System.out.println(lotto.get(0));
-        System.out.println(lotto.get(0).get(1));
         winLotto(lotto, userLotto, lottoNum);
     }
-    public static List<List<Integer>> getLotto(List<List<Integer>> lotto, int lottoNum){
-        for(int i=0; i<lottoNum; i++){
+
+    public static List<List<Integer>> getLotto(List<List<Integer>> lotto, int lottoNum) {
+        for (int i = 0; i < lottoNum; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lotto.add(numbers);
         }
         return lotto;
     }
-    public static void printLotto(List<List<Integer>> lotto, int lottoNum){
+
+    public static void printLotto(List<List<Integer>> lotto, int lottoNum) {
         System.out.println();
-        System.out.println(lottoNum+"개를 구매했습니다.");
-        for(int i=0; i<lottoNum; i++){
+        System.out.println(lottoNum + "개를 구매했습니다.");
+        for (int i = 0; i < lottoNum; i++) {
             Collections.sort(lotto.get(i));
             System.out.println(lotto.get(i));
         }
     }
-    public static void winLotto(List<List<Integer>> lotto,List<Integer> userLotto,int lottoNum){
+
+    public static void winLotto(List<List<Integer>> lotto, List<Integer> userLotto, int lottoNum) {
         List<Integer> reward = new ArrayList<>();
+        System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
-        int total;
-        for(int i=0; i<lottoNum; i++){
-            winPrice(lotto.get(i)); // indent depth 1
-            int idx=0;
-            for(int j=0; j<6; j++) {
+        int bonus = 0;
+        for (int i = 0; i < lottoNum; i++) {
+            int idx = 0;
+            for (int j = 0; j < 6; j++) { // indent depth 1
                 if (lotto.get(i).contains(userLotto.get(j))) { // indent depth 2
                     idx += 1;  // indent depth 3
                 }
             }
-            System.out.println(idx);
             reward.add(idx);
+            if (idx == 4) {
+                if (lotto.get(i).contains(userLotto.get(6))){
+                    // 보너스 번호 2등
+                    bonus+=1;
+                    reward.remove(i);
+                }
             }
-        System.out.println(reward);
+        }
+        winPrice(reward, lottoNum, bonus);
     }
-    public static void winPrice(List<Integer> lotto){
 
+    public static void winPrice(List<Integer> reward, int lottoNum, int bonus) {
+        int three = Collections.frequency(reward, 3);
+        int four = Collections.frequency(reward, 4);
+        int five = Collections.frequency(reward, 5);
+        int six = Collections.frequency(reward,6);
+
+        System.out.println("3개 일치 (5,000원) - "+three+"개");
+        System.out.println("4개 일치 (50,000원) - "+four+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+five+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+bonus+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+six+"개");
+
+        float pricePer = ((three*5000) + (four*50000) + (five*1500000) +
+                (bonus*30000000) + (six*2000000000)) / (float)(lottoNum*1000);
+        System.out.printf("총 수익률은 %.2f%%입니다.",pricePer);
     }
 }

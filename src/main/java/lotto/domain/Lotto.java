07c24b381ad3a1a377af2dbf4ad730211086e81f
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-07 13:19:56 +0900

    refactor(Lotto) : toString 오버라이딩 생성
    로또를 출력할때 toString을 사용하므로 다시 생성함

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -33,4 +33,9 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

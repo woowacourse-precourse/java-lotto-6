18edc80f6a02d0f39731b69dd6e008a4a3c4e322
34suuuuu <psh7492@gmail.com>
2023-11-07 09:06:39 +0900

    feat: 구입 금액이 1,000단위가 아닌 경우 예외 처리

--- src/main/java/lotto/exception/LottoMoneyDivideException.java
+++ src/main/java/lotto/exception/LottoMoneyDivideException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class LottoMoneyDivideException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "구입 금액이 1,000원 단위만 가능합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 구입 금액이 1,000원 단위만 가능합니다.";
 
     public LottoMoneyDivideException(){
         super(ERROR_MESSAGE);

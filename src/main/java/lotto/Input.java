08df811cde96a64b7e0f7355e9dd7e477b2831ee
dragon0622 <iamdragon.dev@gmail.com>
2023-11-07 21:49:41 +0900

    refactor(Lotto) : money 전역변수로 할당

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -7,9 +7,10 @@ import java.util.List;
 public class Input {
 
     static int bonus;
+    static int money;
     final static int LOTTO_WINNING_NUMBER_COUNT = 6;
     public static int money(){
-        int money = Integer.parseInt(Console.readLine());
+        money = Integer.parseInt(Console.readLine());
         //TODO: exception 추가 - money가 1000으로 나누어 떨어지지 않을때
         return money;
     }

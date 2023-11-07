ef500c2f414120c818c60ec59b0cb314ed46a12b
Park Eunhyeong <silbroeh@gmail.com>
2023-11-07 14:02:41 +0900

    feat : LottoGenerator클래스 - calculateBuyingCount()메소드 구현

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -9,12 +9,17 @@ public class LottoGenerator {
     private final List<Lotto> lottos;
 
     public LottoGenerator(long buyingPrice) {
-        long buyingCount = calculateBuyingCount(buyingPrice);
+        long buyingCount = calculateBuyingCount(buyingPrice, LOTTO_PRICE);
         this.lottos = generateLottos(buyingCount);
     }
 
     public List<Lotto> getLottos() {
         return this.lottos;
+    }
+
+    private long calculateBuyingCount(long buyingPrice, long unitPrice) {
+        validate(buyingPrice, unitPrice);
+        return buyingPrice / unitPrice;
     }
 
     private List<Lotto> generateLottos(long buyingCount) {
@@ -26,6 +31,6 @@ public class LottoGenerator {
     }
 
     private Lotto generateLotto() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+        return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER,MAX_NUMBER,LOTTO_SIZE));
     }
 }

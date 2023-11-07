745feea742d3da71c4dee3e910ec50ceccef28b3
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 16:07:03 +0900

    feat(Process): 수익률 계산 함수 구현 (enum자료형 iteration)

--- src/main/java/lotto/Process.java
+++ src/main/java/lotto/Process.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Process {
     private static final int LOTTO_NUMEBER_RANGE_START = 1;
@@ -41,7 +42,7 @@ public class Process {
 
     public List<Object> compareLottos(List<Integer> eachLottoNumber, List<Integer> winningNumbers, int bonusNumber){
         int mainNumberMatchCount = 0;
-        boolean matchBonusNumber = false;
+        boolean matchedBonusNumber = false;
         List<Object> result = new ArrayList<>();
 
 
@@ -50,15 +51,78 @@ public class Process {
                 mainNumberMatchCount++;
             }
             if(element == bonusNumber){
-                matchBonusNumber = true;
+                matchedBonusNumber = true;
             }
         }
 
         result.add(mainNumberMatchCount);
-        result.add(matchBonusNumber);
+        result.add(matchedBonusNumber);
 
         return result;
     }
+
+
+    //수익률 계산
+    public float calculateRateOfReturn(int purchaseAmount, Map<Integer, Integer> winRecordBoard){
+
+        int totalPrizeAmount = 0;
+        float rateOfReturn;
+
+        for(WinningRankPrize rank : WinningRankPrize.values()){
+            totalPrizeAmount += Integer.parseInt(rank.getPrizeAmount()) * winRecordBoard.get(rank.getRank());
+        }
+
+        rateOfReturn = (totalPrizeAmount / purchaseAmount);
+
+        return rateOfReturn;
+    }
+
+
+
+
+    //
+//    List<Integer> winRecordBoard = new ArrayList<>();
+//    Integer firstPlace = Integer.valueof(0);
+//    Integer secondPlace = Integer.valueof(0);
+//    Integer thirdPlace = Integer.valueof(0);
+//    Integer fourthPlace = Integer.valueof(0);
+//    Integer fifthPlace = Integer.valueof(0);
+//
+//    winRecordBoard.set(1, firstPlace);
+//    winRecordBoard.set(2, secondPlace);
+//    winRecordBoard.set(3, thirdPlace);
+//    winRecordBoard.set(4, fourthPlace);
+//    winRecordBoard.set(5, fifthPlace);
+//
+//    //
+//
+//    //각 로또 마다 compareLottos 리턴값 가지고!
+//    List<Object> result =
+//
+//    for(var element : result){
+//        int mainMatchNumber = element.get(0);
+//        boolean matchedBonusNumber = element.get(1);
+//
+//        if (mainMatchNumber == 3) {
+//            winRecordBoard.set(5, ++fifthPlace);
+//        }
+//        if (mainMatchNumber == 4) {
+//            winRecordBoard.set(4, ++fourthPlace);
+//        }
+//        if (mainMatchNumber == 5 && !matchedBonusNumber) {
+//            winRecordBoard.set(3, ++thirdPlace);
+//        }
+//
+//        if (mainMatchNumber == 5 && matchedBonusNumber) {
+//            winRecordBoard.set(2, ++secondPlace);
+//        }
+//
+//        if (mainMatchNumber == 6) {
+//            winRecordBoard.set(1, ++firstPlace);
+//        }
+//    }
+
+
 
 
 

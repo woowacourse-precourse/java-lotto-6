baa51176fb7bbe45cf26961cc7e4bd85705edf39
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-07 20:55:13 +0900

    feat: 구입 금액에 맞는 수량의 로또를 발행하는 로직 구현

--- src/main/java/lotto/model/NumberGenerator.java
+++ src/main/java/lotto/model/NumberGenerator.java
@@ -1,10 +1,11 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class NumberGenerator {
 
     public static List<Integer> getLottoNumber() {
-        return null;
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }

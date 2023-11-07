01029291ac7e397331a2d8530c1994a3089f40b7
Choijake <shameless8@naver.com>
2023-11-08 00:09:12 +0900

    Refactor : RandomNumber 클래스명 변경

--- src/main/java/lotto/Util/RandomNumber.java
+++ src/main/java/lotto/Util/RandomNumber.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class RandomNumberUtil {
+public class RandomNumber {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
     private static final int NumberQuantity = 6;
@@ -17,7 +17,7 @@ public class RandomNumberUtil {
     }
 
     //TODO : 재귀로 짜도 괜찮은지 확인
-    public static int generateRandomNumber() {
+    private static int generateRandomNumber() {
         List<Integer> numberList = new ArrayList<>();
         int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
         if (hasNumberDuplicatesInList(numberList, randomNumber)) {

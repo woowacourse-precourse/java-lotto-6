aa78643dbb2993fa08b63971f2b175440ab312c7
i3kae <snoger1234@gmail.com>
2023-11-07 19:12:09 +0900

    feat : 당첨 통계 출력 기능 구현

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -3,8 +3,15 @@ package lotto;
 import java.util.List;
 import lotto.Input.InputType;
 import lotto.Messages.MainMessage;
+import lotto.Messages.WinningStatistics;
 
 public class Print {
+    private static final int RANK_5 = 3;
+    private static final int RANK_4 = 4;
+    private static final int RANK_3 = 5;
+    private static final int RANK_2 = 7;
+    private static final int RANK_1 = 6;
+
     public static void printMessage(String printStr){
         System.out.println(printStr);
     }
@@ -29,5 +36,14 @@ public class Print {
         for (Lotto lotto : purchasedLotto){
             lotto.printLotto();
         }
+    }
+
+    public static void printWinningStatistics(Float rateOfReturn, int[] winningCount){
+        System.out.printf((WinningStatistics.WIN_3_MATCH.getMessage()) + "%n", winningCount[RANK_5]);
+        System.out.printf((WinningStatistics.WIN_4_MATCH.getMessage()) + "%n", winningCount[RANK_4]);
+        System.out.printf((WinningStatistics.WIN_5_MATCH.getMessage()) + "%n", winningCount[RANK_3]);
+        System.out.printf((WinningStatistics.WIN_5_BONUS_MATCH.getMessage()) + "%n", winningCount[RANK_2]);
+        System.out.printf((WinningStatistics.WIN_6_MATCH.getMessage()) + "%n", winningCount[RANK_1]);
+        System.out.printf((WinningStatistics.TOTAL_AMOUNT.getMessage()) + "%n", rateOfReturn);
     }
 }

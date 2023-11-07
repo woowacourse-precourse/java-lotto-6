447e85ac9ae2c632e1140649e5fb448de7e4e8b2
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 03:55:00 +0900

    feat($LottoResult): 당첨 결과 계산 기능

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,9 +1,38 @@
 package lotto.domain;
 
 import lotto.type.LottoPrize;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoResult {
+    public static final int SECOND_MATCHING_CORRECT_NUM = 5;
+    private final List<Lotto> purchasedLottos;
     private final Lotto winningNumbers;
     private final int bonusNumber;
+
+    public LottoResult(List<Lotto> purchasedLottos, Lotto winningNumbers, int bonusNumber) {
+        this.purchasedLottos = purchasedLottos;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<LottoPrize> calculateResults() {
+        return purchasedLottos.stream()
+                .map(this::matchLotto)
+                .collect(Collectors.toList());
+    }
+
+    public LottoPrize matchLotto(Lotto lotto) {
+        int matchedNumber = countMatchingNumbers(lotto);
+
+        if (matchedNumber == SECOND_MATCHING_CORRECT_NUM) {
+            return LottoPrize.valueOf(matchedNumber, isMatchBonusNumber(lotto));
+        }
+
+        return LottoPrize.valueOf(matchedNumber, false);
+    }
+
     private int countMatchingNumbers(Lotto lotto) {
         return winningNumbers.getNumbers().stream()
                 .filter(lotto.getNumbers()::contains)

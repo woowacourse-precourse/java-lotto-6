4a1b64ade85e5e82d2a7e51f6d315e7359037187
KimSehoon <shggm2000@gmail.com>
2023-11-07 14:40:21 +0900

    feat: WinningStatistics class 구현
    - 당첨 통계를 의미하는 winningStatus Map을 가지고 있는 WinningStatistics class 구현
    - 일치 횟수에 따라 winningStatus를 증가시키는 incrementWinningStatus method 구현

---
+++ src/main/java/lotto/WinningStatistics.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import static lotto.Ranking.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningStatistics {
+    Map<Ranking, Integer> winningStatus = new HashMap<>();
+
+    public WinningStatistics(Map<Ranking, Integer> winningStatus) {
+        winningStatus.put(THREE_MATCHES, 0);
+        winningStatus.put(FOUR_MATCHES, 0);
+        winningStatus.put(FIVE_MATCHES, 0);
+        winningStatus.put(FIVE_MATCHES_BOUNS_MATCHES, 0);
+        winningStatus.put(SIX_MATCHES, 0);
+    }
+
+    private void incrementWinningStatus(int matchCount) {
+        winningStatus.keySet().stream()
+                .filter(ranking -> ranking.getMatchCount() == matchCount)
+                .findAny()
+                .ifPresent(ranking -> winningStatus.replace(ranking, winningStatus.get(ranking) + 1));
+    }
+}

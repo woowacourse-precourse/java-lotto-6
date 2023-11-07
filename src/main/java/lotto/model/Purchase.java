9f148bf5faa88e6f321d527405fa17ec2b975606
김현재 <khjmr@naver.com>
2023-11-07 18:08:28 +0900

    feat(purchase): 구매 검증 로직 추가

---
+++ src/main/java/lotto/model/Purchase.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import lotto.view.ErrorConstants;
+
+public class Purchase {
+    private static final Integer MAX_PRICE = 100_000;
+    private Integer money;
+
+    public Purchase(Integer money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(Integer money) {
+        if (money % 1000 != 0 || money == 0 || money > MAX_PRICE) {
+            throw new IllegalArgumentException(ErrorConstants.ILLEGAL_MONEY_ERROR);
+        }
+    }
+}
--- src/main/java/lotto/view/ErrorConstants.java
+++ src/main/java/lotto/view/ErrorConstants.java
@@ -2,4 +2,5 @@ package lotto.view;
 
 public class ErrorConstants {
     public static final String IS_NOT_NUMBER_ERROR = "입력 값이 숫자가 아닙니다.";
+    public static final String ILLEGAL_MONEY_ERROR = "잘못된 금액을 입력하셨습니다.";
 }

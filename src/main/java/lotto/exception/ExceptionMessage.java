6bcd2e7c83aa44452ffc9fb4b98dd37a68200167
BonSik <rnqhstlr2297@gmail.com>
2023-11-08 00:42:01 +0900

    refactor: ExceptionMessage 공통 메시지 prefix 분리

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,14 +1,15 @@
 package lotto.exception;
 
 public enum ExceptionMessage {
-    INVALID_NUMERIC("[ERROR] 숫자만 입력이 가능합니다."),
-    INVALID_NUMBER_COMMA("[ERROR] 숫자와 콤마 구분자를 다시 확인해 주세요."),
-    INVALID_PURCHASE_PRICE_UNIT("[ERROR] 로또 구입 금액은 1,000원 단위로 입력이 가능합니다."),
-    INVALID_SIZE_LOTTO_NUMBER("[ERROR] 로또 번호의 개수는 6개만 가능합니다."),
-    INVALID_RANGE_LOTTO_NUMBER("[ERROR] 로또 번호는 1~45까지 숫자만 가능합니다."),
-    DUPLICATION_LOTTO_NUMBER("[ERROR] 중복된 로또 번호는 사용할 수 없습니다."),
-    DUPLICATION_BONUS_NUMBER("[ERROR] 로또 번호와 중복된 보너스 번호는 사용할 수 없습니다.")
+    INVALID_NUMERIC("숫자만 입력이 가능합니다."),
+    INVALID_NUMBER_COMMA("숫자와 콤마 구분자를 다시 확인해 주세요."),
+    INVALID_PURCHASE_PRICE_UNIT("로또 구입 금액은 1,000원 단위로 입력이 가능합니다."),
+    INVALID_SIZE_LOTTO_NUMBER("로또 번호의 개수는 6개만 가능합니다."),
+    INVALID_RANGE_LOTTO_NUMBER("로또 번호는 1~45까지 숫자만 가능합니다."),
+    DUPLICATION_LOTTO_NUMBER("중복된 로또 번호는 사용할 수 없습니다."),
+    DUPLICATION_BONUS_NUMBER("로또 번호와 중복된 보너스 번호는 사용할 수 없습니다.")
     ;
+    private final String prefix = "[ERROR] ";
     private String desc;
 
     ExceptionMessage(String description) {
@@ -16,7 +17,7 @@ public enum ExceptionMessage {
     }
 
     public String getDesc(){
-        return this.desc;
+        return prefix + this.desc;
     }
 
 }

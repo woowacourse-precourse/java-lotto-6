a008c8b06cc11f13296fd0d02756dd7cc9fcb1e4
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 03:40:16 +0900

    Move($Lotto): Lotto 클래스 파일 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -16,5 +16,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

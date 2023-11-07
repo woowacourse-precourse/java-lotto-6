e26e743c5c7a6dfde2e02589f9ca62e39ece6074
whereami2048 <whereami2048@gmail.com>
2023-11-07 19:48:19 +0900

    refactor: 범위 체크 기능 로직 리팩토링

--- src/main/java/lotto/util/ModelValidator.java
+++ src/main/java/lotto/util/ModelValidator.java
@@ -4,11 +4,7 @@ import lotto.exception.ModelExceptionConstant;
 
 public class ModelValidator {
     public static void validateOutOfRange(int number, int startNumber, int endNumber) {
-        if (number < startNumber) {
-            throw new IllegalArgumentException(ModelExceptionConstant.OUT_OF_LOTTO_NUMBER_RANGE.getText());
-        }
-
-        if (number > endNumber) {
+        if (number < startNumber || number > endNumber) {
             throw new IllegalArgumentException(ModelExceptionConstant.OUT_OF_LOTTO_NUMBER_RANGE.getText());
         }
     }

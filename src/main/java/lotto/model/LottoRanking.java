c74ceacc8a32d4b98ed59c206747dc2a47486a7b
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 06:20:05 +0900

    feat: 당첨 등수, 상금, 당첨 개수를 enum으로 생성

---
+++ src/main/java/lotto/model/LottoRanking.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+public enum LottoRanking {
+    FIRST(6, 2_000_000_000, 0),
+    SECOND(5, 30_000_000, 0),
+    THIRD(5, 1_500_000, 0),
+    FORTH(4, 50_000, 0),
+    FIFTH(3, 5_000, 0);
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final int count;
+
+    LottoRanking(int matchCount, int prizeMoney, int count) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.count = count;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public int count() {
+        return count;
+    }
+}

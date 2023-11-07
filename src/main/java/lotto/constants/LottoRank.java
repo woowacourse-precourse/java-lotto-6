abcacd212132502b33846ba7a7b3e458e716ad39
JongMinCh0i <wonderland314592@gmail.com>
2023-11-07 10:20:39 +0900

    refactor(LottoRank): 아무것도 당첨되지 않았을 때 NONE 추가

--- src/main/java/lotto/constants/LottoRank.java
+++ src/main/java/lotto/constants/LottoRank.java
@@ -7,7 +7,8 @@ public enum LottoRank {
     SECOND((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && bonusWinningNumber),
     THIRD((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && !bonusWinningNumber),
     FOURTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 4),
-    FIFTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 3);
+    FIFTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 3),
+    NONE((matchedNumbers, bonusWinningNumber) -> matchedNumbers < 3);
 
     private final BiPredicate<Integer, Boolean> picked;
 

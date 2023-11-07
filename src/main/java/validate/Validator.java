b7d22f66bb88cf6648d0d843fc2d844d838007d2
cheolhyeon <tghy4@naver.com>
2023-11-08 01:39:53 +0900

    refactor : 테스트 코드의 기능 테스트 메서드에서 List of 를 사용하기 때문에 그로 인한 코드 구조 개선

--- src/main/java/validate/Validator.java
+++ src/main/java/validate/Validator.java
@@ -3,7 +3,6 @@ package validate;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 public abstract class Validator {
@@ -15,15 +14,8 @@ public abstract class Validator {
         if (hasDuplicate(numbers)) {
             throw new IllegalStateException("[ERROR] 로또 번호의 숫자는 중복될 수 없습니다..");
         }
-
     }
-    public static int[] validateWinningNumber(String[] winningLottoNumber) {
-        int[] numbers = Arrays.stream(winningLottoNumber)
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        Arrays.sort(numbers);
-
+    public static int[] validateWinningNumber(int[] numbers) {
         Set<Integer> winningNumbers = new HashSet<>();
         for (int number : numbers) {
             boolean isAdded = winningNumbers.add(number);
@@ -33,10 +25,26 @@ public abstract class Validator {
         }
         return numbers;
     }
+    public static void validateSize(int[] numbers) {
+        if (numbers.length!= 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호의 숫자는 6자리 입니다.");
+        }
+    }
+    public static void validateBonusNumber(int userInputBonusNumber) {
+        if (userInputBonusNumber < 1 || userInputBonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45사이의 숫자만 입력 가능합니다.");
+        }
+    }
 
     public static void isInvalidPriceUnit(int userInputPrice) {
         if (userInputPrice % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+    public static void validateNumericInput(String input) {
+        // 정규 표현식을 사용하여 입력값이 숫자만 포함하는지 검사
+        if (!input.matches("\\d+")) {
+            throw new NumberFormatException("[ERROR] 구입 금액은 문자를 포함할 수 없습니다.");
         }
     }
 

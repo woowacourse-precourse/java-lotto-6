a65358031f42452dca53c9b722713fb317f2964c
권장순 <jsoonworld@gmail.com>
2023-11-07 13:44:28 +0900

    test( LottoValidationUtils#should_ThrowException_When_LottoNumbersSizeIsNotSix() ) : 로또 번호가 6개가 아니면 예외를 던진다.

---
+++ src/test/java/lotto/v2/util/LottoValidationUtilsV2Test.java
@@ -0,0 +1,25 @@
+package lotto.v2.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class LottoValidationUtilsV2Test {
+    @Test
+    @DisplayName("로또 번호가 6개가 아니면 예외를 던진다")
+    void should_ThrowException_When_LottoNumbersSizeIsNotSix() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5); // 5 numbers, not 6
+
+        // when & then
+        assertThatThrownBy(() -> LottoValidationUtilsV2.checkValidLottoNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+    
+}

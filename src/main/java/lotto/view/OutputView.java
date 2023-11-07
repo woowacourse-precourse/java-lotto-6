4c3a95a2d70e3c6433c00a6f35cc7209909c2750
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 03:59:31 +0900

    rename: move Lotto class to model package
    + modify import line

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,6 @@
 package lotto.view;
 
-import lotto.Lotto;
+import lotto.model.Lotto;
 import lotto.model.Customer;
 import lotto.view.constant.ConstantMessage;
 import lotto.view.constant.ErrorMessage;

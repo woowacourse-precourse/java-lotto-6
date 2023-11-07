57b4db751ab1dfea2fa4b802a3d5c3ff39f13bac
sujnkim <blackout3210@naver.com>
2023-11-07 15:22:42 +0900

    style: InputView 코드 자동정렬

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,17 +10,17 @@ public class InputView {
     private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static int getPaymentFromUserInput(){
+    public static int getPaymentFromUserInput() {
         System.out.println(ASK_PAYMENT);
         return Parser.parsePay(Console.readLine());
     }
 
-    public static List<Integer> getWinningNumbersFromUserInput(){
+    public static List<Integer> getWinningNumbersFromUserInput() {
         System.out.println(ASK_WINNING_NUMBERS);
         return Parser.parseWinningNumber(Console.readLine());
     }
 
-    public static int getBonusNumberFromUserInput(){
+    public static int getBonusNumberFromUserInput() {
         System.out.println(ASK_BONUS_NUMBER);
         return Parser.parseBonusNumber(Console.readLine());
     }

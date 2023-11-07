991f913716c5a0d7d2698c308279e767bd2988ea
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 16:39:44 +0900

    "refactor(Money): 스켈레톤 코드 추가
    입력값이 숫자인지 검증하는 메소드 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,12 +5,16 @@ public class Money {
 
     public Money(String spendAmount) {
         validate(spendAmount);
-        this.spendAmount = spendAmount;
+        this.spendAmount = Integer.parseInt(spendAmount);
     }
 
     private void validate(String spendAmount) {
+        validateIsDigit(spendAmount);
         validateDividedByThousand(spendAmount);
         validateRangeOfSpendAmount(spendAmount);
+    }
+
+    private void validateIsDigit(String spendAmount) {
     }
 
     private void validateRangeOfSpendAmount(String spendAmount) {

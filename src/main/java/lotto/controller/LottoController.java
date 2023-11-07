854f6aa7b64b42a44613f48720a64172f65bfe90
kangaroo0 <ljw42871@gmail.com>
2023-11-07 20:04:45 +0900

    로또 수량에 맞게 발행하기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,22 +1,28 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.domain.Lotto;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private InputView inputView;
     private OutputView outputView;
+    private LottoService lottoService;
 
     public LottoController() {
         inputView = new InputView();
         outputView = new OutputView();
+        lottoService = new LottoService();
     }
 
 
     public void play() {
 
         int lottoAmount = inputView.getPurchaseAmount();
+        List<Lotto> userPurchasedLotto =lottoService.generateMultipleLotto(lottoAmount);
+
 
         System.out.println(lottoAmount);
 

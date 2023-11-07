cabe89daa5e19f8e4b00f04a2042e22f6e6e5e58
leejuno <zhqkfxm0127@naver.com>
2023-11-07 23:39:12 +0900

    feat(lotto): 구입금액을 입력받는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import java.util.List;
 
 public class Lotto {
+    private static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,6 +18,11 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
+    
+    public int readAmount() {
+        System.out.println(PURCHASE_AMOUNT_GUIDE);
+        String purchaseAmountText = readLine();
+        int purchaseAmount = Integer.parseInt(purchaseAmountText);
+        return purchaseAmount;
+    }
 }

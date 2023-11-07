b418ceb01c35035c8e681899a9e5c790300d4473
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-07 21:36:08 +0900

    feat(LottoConsole) : created getWinningLotto method

--- src/main/java/lotto/LottoConsole.java
+++ src/main/java/lotto/LottoConsole.java
@@ -50,4 +50,23 @@ public class LottoConsole {
     }
     System.out.println();
   }
+
+  private void getWinningLotto() {
+    try {
+      System.out.println("당첨 번호를 입력해 주세요.");
+      List<Integer> numbers = new ArrayList<Integer>();
+      String inputWinningLotto = readLine();
+      try {
+        numbers = Arrays.stream(inputWinningLotto.split(","))
+            .map(number -> Integer.parseInt(number))
+            .toList();
+      } catch (NumberFormatException e) {
+        throw new IllegalArgumentException("[ERROR] 6개의 당첨 로또 번호를 입력 받습니다. 번호는 정수형태이며 쉼표(,)를 기준으로 구분해야 합니다.");
+      }
+      winningLotto = new Lotto(numbers);
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+      getWinningLotto();
+    }
+  }
 }

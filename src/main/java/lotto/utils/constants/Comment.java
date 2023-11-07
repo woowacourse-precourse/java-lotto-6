fd90cb25f5dd21cfe70489c5dacebfcc7052fff0
ohjuyoungg <ejrm12@naver.com>
2023-11-08 04:11:53 +0900

    feat: 코멘트 추가 enum 클래스에 구현

--- src/main/java/lotto/utils/constants/Comment.java
+++ src/main/java/lotto/utils/constants/Comment.java
@@ -6,7 +6,14 @@ public enum Comment {
     INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     INPUT_WINNING_STATISTICS("당첨 통계"),
-    INPUT_THREE_DASH("---");
+    INPUT_THREE_DASH("---"),
+    OUTPUT_FIFTH_PRIZE("3개 일치 (5,000원) - "),
+    OUTPUT_FOURTH_PRIZE("4개 일치 (50,000원) - "),
+    OUTPUT_THIRD_PRIZE("5개 일치 (1,500,000원) - "),
+    OUTPUT_SECOND_PRIZE("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    OUTPUT_FIRST_PRIZE("6개 일치 (2,000,000,000원) - "),
+    OUTPUT_TOTAL_RETURN("총 수익률은 "),
+    OUTPUT_COUNT("개");
 
     private final String comment;
 

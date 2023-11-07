43bcf12ea42982062b74d9309c8036e539d0f3e4
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-07 11:32:09 +0900

    feat: 로또 구매 금액 예외 처리

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -12,6 +12,13 @@ public class LottoTicket {
 
     private int calculate (int money) {
         int ticketCost = 1000;
+        calculationValidation(money);
         return money/ticketCost;
+    }
+
+    public void calculationValidation (int money) {
+        if(money%1000 != 0){
+            throw new IllegalArgumentException();
+        }
     }
 }

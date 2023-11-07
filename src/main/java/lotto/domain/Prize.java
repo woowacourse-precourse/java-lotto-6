7973eb1063d6c6c7ab37c7297a28720c8c15dfcf
dmong <dt2980_hee@naver.com>
2023-11-08 07:14:15 +0900

    feat: 로또와 당첨 번호 비교 결과를 Prize 타입으로 변환하는 valueOf 메소드 작성

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Prize {
     FIRST(6, false, 2000000000),
     SECOND(5, true, 30000000),
     THIRD(5, false, 1500000),
     FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000);
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
 
     private final int numberOfMatches;
     private final boolean bonusIncluded;
@@ -15,5 +18,28 @@ public enum Prize {
         this.numberOfMatches = numberOfMatches;
         this.bonusIncluded = bonusIncluded;
         this.prizeMoney = prizeMoney;
+    }
+
+    public static Prize valueOf(int numberOfMatches, boolean bonusIncluded) {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.isSamePrize(numberOfMatches, bonusIncluded))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private boolean isSamePrize(int numberOfMatches, boolean bonusMatched) {
+        return this.numberOfMatches == numberOfMatches && (!this.bonusIncluded || bonusMatched);
+    }
+
+    public int getNumberOfMatches() {
+        return numberOfMatches;
+    }
+
+    public boolean isBonusIncluded() {
+        return bonusIncluded;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
     }
 }

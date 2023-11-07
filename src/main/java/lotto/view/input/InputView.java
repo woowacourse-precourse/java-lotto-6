426efe43fbd0040b2b3fbaf24009c9808acd1b41
gmelon <hsh1769@naver.com>
2023-11-08 01:07:52 +0900

    feat: InputView에 입력을 위한 스크립트 출력 로직 구현

--- src/main/java/lotto/view/input/InputView.java
+++ src/main/java/lotto/view/input/InputView.java
@@ -7,15 +7,17 @@ public class InputView {
     }
 
     public static int readLottoPurchasePrice() {
-        Input input = Input.readLine();
+        System.out.println("구입금액을 입력해 주세요.");
 
+        Input input = Input.readLine();
         IntegerInput integerInput = new IntegerInput(input.trimmed());
         return integerInput.value();
     }
 
     public static List<Integer> readWinningLottoNumbers() {
-        Input input = Input.readLine();
+        System.out.println("당첨 번호를 입력해 주세요.");
 
+        Input input = Input.readLine();
         IntegerInputs integerInputs = new IntegerInputs(input.trimmed());
         return integerInputs.values();
     }

26c47f2889c29913083978fbcdb5e177a89e879f
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-11-07 21:59:10 +0900

    feat: 로또 게임 세팅 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,5 +1,15 @@
 package lotto.controller;
 
+import lotto.service.Service;
+
 public class Controller {
+	
+	Service service = new Service();
+
+	public void gameSet() {
+		service.initBuyer();
+		service.buyLotteries();
+		service.showBuyingResult();
+	}
 
 }

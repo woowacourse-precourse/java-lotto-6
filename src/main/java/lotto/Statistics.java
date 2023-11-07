8d96010fddfd349840b8911df3f9d6f52d0f55e6
dev.hyejihwang@gmail.com <dev.hyejihwang@gmail.com>
2023-11-07 21:16:05 +0900

    Statistics 수익률 계싼 추가

--- src/main/java/lotto/Statistics.java
+++ src/main/java/lotto/Statistics.java
@@ -8,6 +8,13 @@ public class Statistics {
     private final List<Integer> winningNumbers;
     private final Integer bonus;
 
+    int matching_3 = 0;
+    int matching_4 = 0;
+    int matching_5 = 0;
+    int matching_6 = 0;
+    int matching_bonus = 0;
+    int matchingBonus = 0;
+
     public Statistics(List<List<Integer>> lottoNumbers, List<Integer> winningNumbers, Integer bonus) {
         this.lottoNumbers = lottoNumbers;
         this.winningNumbers = winningNumbers;
@@ -15,19 +22,12 @@ public class Statistics {
     }
 
     public void compareNumber(){
-        int matching_3 = 0;
-        int matching_4 = 0;
-        int matching_5 = 0;
-        int matching_6 = 0;
-        int matching_bonus = 0;
-        int matchingBonus = 0;
-
         for (List<Integer> lottoNumber : lottoNumbers) {
             int matchingCount = 0;
             for (Integer number : lottoNumber) {
                 if(number == bonus){
                     matchingBonus++;
-                }else{ // else 사용하지 않기.
+                }else{// else 사용하지 않기.
                     for (Integer winningNumber : winningNumbers) {
                         if(number == winningNumber){
                             matchingCount++;
@@ -36,27 +36,34 @@ public class Statistics {
                     }
                 }
             }
+            countMatch(matchingCount, matchingBonus);
+        }
+        printResult();
+    }
 
-            if(matchingCount == 3){
-                matching_3++;
-            }
-
-            if(matchingCount == 4){
-                matching_4++;
-            }
-
-            if(matchingCount == 5 && matchingBonus == 0){
-                matching_5++;
-            }
-            if(matchingCount == 5 && matchingBonus == 1){
-                matching_bonus++;
-            }
-
-            if(matchingCount == 6){
-                matching_6++;
-            }
+    private void countMatch(int matchingCount, int matchingBonus) {
+        if(matchingCount == 3){
+            matching_3++;
         }
 
+        if(matchingCount == 4){
+            matching_4++;
+        }
+
+        if(matchingCount == 5 && matchingBonus == 0){
+            matching_5++;
+        }
+
+        if(matchingCount == 5 && matchingBonus == 1){
+            matching_bonus++;
+        }
+
+        if(matchingCount == 6){
+            matching_6++;
+        }
+    }
+
+    public void printResult(){
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.println("3개 일치 (5,000원) - " + matching_3 +"개");
@@ -65,7 +72,9 @@ public class Statistics {
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matching_bonus +"개");
         System.out.println("6개 일치 (2,000,000,000원) - " + matching_6 +"개");
 
-        System.out.println("총 수익률은 몇입니다.");
-    }
+       int revenue = (matching_3*5000)+(matching_4*50000)+(matching_5*1500000)+(matching_bonus*30000000)+(matching_6*2000000000);
 
+       int revenuePercent = revenue /(lottoNumbers.size()*1000)*100;
+        System.out.println("총 수익률은 "+revenuePercent+"%입니다.");
+    }
 }

011bce2106e005423156376e963eba7ab6b3c090
jun <ggprgrkjh@naver.com>
2023-11-08 02:13:26 +0900

    test(lottochecker): 로또 랭크 생성 함수 반환값 테스트 with mock

--- src/test/java/lotto/domain/LottoCheckerTest.java
+++ src/test/java/lotto/domain/LottoCheckerTest.java
@@ -6,6 +6,7 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import lotto.model.BonusNumber;
+import lotto.model.Game;
 import lotto.model.Lotto;
 import lotto.model.LottoChecker;
 import lotto.model.LottoRank;
@@ -14,13 +15,31 @@ import lotto.model.Lottos;
 import lotto.model.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class LottoCheckerTest {
 
-    @DisplayName("Enum결과 생성 함수 성공 테스트")
+    private Lottos lottos;
+    private WinningNumbers winningNumbers;
+    private BonusNumber bonusNumber;
+    private LottoChecker lottoChecker;
+
+
+    @DisplayName("Enum결과 생성 함수 반환값 테스트")
     @Test
     void createEnumResult_EqualResult_Success() {
 
@@ -42,5 +61,29 @@ public class LottoCheckerTest {
 
     }
 
+
+    @DisplayName("로또 랭크 생성 함수 반환값 테스트 with Mock")
+    @Test
+    void createLottoRanks_EqualResult_Success() {
+
+        //given
+        lottos = mock(Lottos.class);
+        winningNumbers = mock(WinningNumbers.class);
+        bonusNumber = mock(BonusNumber.class);
+        lottoChecker = new LottoChecker(winningNumbers, bonusNumber);
+        MockedStatic<LottoRank> lottoRank = mockStatic(LottoRank.class);
+        List<Lotto> mockLottos = Arrays.asList(mock(Lotto.class), mock(Lotto.class));
+
+        when(winningNumbers.compareWinningNumbers(anyList())).thenReturn(3L);
+        when(bonusNumber.compareBonusNumber(anyList())).thenReturn(true);
+        when(lottos.getLottos()).thenReturn(mockLottos);
+        lottoRank.when(() -> LottoRank.valueOf(3L, true)).thenReturn(LottoRank.FIFTH);
+        //when
+        List<LottoRank> result = lottoChecker.createLottoRanks(lottos);
+
+        //then
+        assertThat(result.contains(LottoRank.FIFTH)).isTrue();
+
+    }
 
 }

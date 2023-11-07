53358db42e2f4c6a4fff45f869103ff40fe5ca5c
yewonniy <ae6652@naver.com>
2023-11-07 17:30:06 +0900

    [refactor] : LottoGame 클래스 제거 후 해당 클래스에 있던 메서드들을 Lotto 클래스로 이동시킴

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,24 +1,19 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+import java.util.function.Predicate;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class Lotto {
+
     static final int lotteryCost = 1000;
     private final List<Integer> numbers;
+
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
-
-    static List<Integer> makeLottoNums() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    }
-
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
@@ -29,9 +24,26 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호엔 중복된 숫자가 있으면 안됩니다.");
     }
 
+    public int numOfMatchNum(List<Integer> winningNumbers) {
+        //연산
+        List<Integer> matchNumbers = this.numbers.stream().filter(n -> winningNumbers.stream()
+                .anyMatch(Predicate.isEqual(n))).toList();
+
+        return matchNumbers.size();
+    }
+
+    public boolean ifBonusNumEqual(int bonusNum) {
+        return this.numbers.contains(bonusNum);
+    }
+
     static int lottoAmount(int userMoneyAmount) {
         return userMoneyAmount / lotteryCost;
     }
 
-    // TODO: 추가 기능 구현
+    public void printLottoNumber() {
+        List<Integer> numbers = new ArrayList<>(this.numbers);
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
+
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,4 +26,48 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 당첨 번호와 몇 개가 겹치는지 반환한다.")
+    @Test
+    void 로또_번호_당첨_번호_중복_있음() {
+        Lotto lottoNumbers = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        List<Integer> winningNumbers = Arrays.asList(3,4,5,7,6,8);
+
+        int result = lottoNumbers.numOfMatchNum(winningNumbers);
+
+        Assertions.assertThat(result).isEqualTo(4);
+    }
+
+    @DisplayName("로또 번호가 당첨 번호와 몇 개가 겹치는지 반환한다.")
+    @Test
+    void 로또_번호_당첨_번호_중복_없음() {
+        Lotto lottoNumbers = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        List<Integer> winningNumbers = Arrays.asList(7,8,9,10,11,12);
+
+        int result = lottoNumbers.numOfMatchNum(winningNumbers);
+
+        Assertions.assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("보너스 번호가 있으면 true를 반환한다.")
+    @Test
+    void 로또번호에_보너스_번호_있음() {
+        Lotto lottoNumbers = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        int bonusNum = 5;
+
+        boolean result = lottoNumbers.ifBonusNumEqual(bonusNum);
+
+        Assertions.assertThat(result).isEqualTo(true);
+    }
+
+    @DisplayName("보너스 번호가 없으면 false 반환")
+    @Test
+    void 로또번호에_보너스_번호_없음() {
+        Lotto lottoNumbers = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        int bonusNum = 20;
+
+        boolean result = lottoNumbers.ifBonusNumEqual(bonusNum);
+
+        Assertions.assertThat(result).isEqualTo(false);
+    }
 }
+

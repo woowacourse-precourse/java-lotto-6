1fdf58de4c29025a9f782ceee56c3f864545e18b
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 01:22:35 +0900

    feat: bonus 번호가 1~45가 아닐 때 예외처리

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -8,5 +8,14 @@ public class Bonus {
     public void inputBonus() {
         String bonusChar = Console.readLine();
         bonus = Integer.parseInt(bonusChar);
+
+        validate(bonus);
+    }
+
+    private void validate(int bonus) {
+        //번호가 1~45가 아닐 때
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45이어야 합니다.");
+        }
     }
 }

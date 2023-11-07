e52c603e4e9281f827040ad89ef33b1e848aca6e
KimSehoon <shggm2000@gmail.com>
2023-11-07 15:37:00 +0900

    refactor: incrementWinningStatus method 수정

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -7,19 +7,20 @@ import java.util.Map;
 
 public class WinningStatistics {
     Map<Ranking, Integer> winningStatus = new HashMap<>();
+    Ranking ranking;
 
     public WinningStatistics() {
         winningStatus.put(THREE_MATCHES, 0);
         winningStatus.put(FOUR_MATCHES, 0);
         winningStatus.put(FIVE_MATCHES, 0);
-        winningStatus.put(FIVE_MATCHES_BOUNS_MATCHES, 0);
+        winningStatus.put(FIVE_MATCHES_BONUS_MATCHES, 0);
         winningStatus.put(SIX_MATCHES, 0);
     }
 
-    private void incrementWinningStatus(int matchCount) {
-        winningStatus.keySet().stream()
-                .filter(ranking -> ranking.getMatchCount() == matchCount)
-                .findAny()
-                .ifPresent(ranking -> winningStatus.replace(ranking, winningStatus.get(ranking) + 1));
+    public void incrementWinningStatus(int matchCount) {
+        winningStatus.computeIfPresent(
+                Ranking.findByMatchCount(matchCount),
+                (ranking, count) -> count + 1
+        );
     }
 }

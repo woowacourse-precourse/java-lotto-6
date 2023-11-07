f587195f4d8eedfca5c2075e38e3e59209203ede
lynjun97 <lynjun97@naver.com>
2023-11-07 20:47:48 +0900

    Feat: outputLottoTickets 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.Lotto;
 import java.util.List;
 import static lotto.controller.LottoController.*;
+import static lotto.view.OutView.outputLottoNumber;
 
 public class GameController {
     private List<Lotto> lotto;
@@ -10,10 +11,16 @@ public class GameController {
     public GameController() {
         //구매 금액 입력
         setLotto();
+        // 티켓 출력
+        outputLottoTickets();
     }
 
     private void setLotto() {
         int amount = setPurchaseAmount();
         lotto = setLottoNumber(amount);
+    }
+
+    private void outputLottoTickets() {
+        outputLottoNumber(lotto);
     }
 }

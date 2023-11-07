f3ecfc22c0120c50bd13528c2633be1406f9da98
peeaaccee <wlgp8958@naver.com>
2023-11-07 16:57:59 +0900

    feat(Grade): 로또 당첨 번호, 보너스 번호의 맞춘 갯수만큼의 금액을 enum화

---
+++ src/main/java/lotto/domain/Grade.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public enum Grade {
+    FIFTH("3", 3, 5_000),
+    FOURTH("4", 4, 50_000),
+    THIRD("5", 5, 1_500_000),
+    SECOND("5+B", 5, 30_000_000),
+    FIRST("6", 6, 2_000_000_000);
+
+    private final String rank;
+    private final int matchCount;
+    private final int prizeMoney;
+
+    Grade(String rank, int matchCount, int prizeMoney) {
+        this.rank = rank;
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+}

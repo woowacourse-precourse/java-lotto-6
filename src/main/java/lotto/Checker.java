294edff61e1dd94aeb47188bb55ed6fe8b5e89e2
seohokim <shk9946@gmail.com>
2023-11-07 20:47:34 +0900

    [FIX] error handlling 수정

--- src/main/java/lotto/Checker.java
+++ src/main/java/lotto/Checker.java
@@ -7,24 +7,34 @@ import java.util.Set;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 public class Checker {
+	private static final String ERROR_MESSAGE = "[ERROR]";
 	private static final int LOTTO_PRICE = 1000;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int LOTTO_NUMBER_LEGNTH = 6;
 	
 	public static int checkPayment(String str) {
-		int payment = Integer.parseInt(str);
-		if(payment <= 0 || payment % LOTTO_PRICE != 0) {
-			throw new IllegalArgumentException();
+		try {
+			int payment = Integer.parseInt(str);
+			if (payment <= 0 || payment % LOTTO_PRICE != 0) {
+				throw new IllegalArgumentException(ERROR_MESSAGE);
+			}
+			return payment / LOTTO_PRICE;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ERROR_MESSAGE);
 		}
-		return payment / LOTTO_PRICE;
 	}
 	
 	public static List<Integer> checkWinningInput(String str) {
 		List<String> obj = Arrays.asList(str.split(","));
 		List<Integer> intObj = obj.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
 		Set<Integer> numSet = new HashSet<>(intObj);
+		if(intObj.size() != LOTTO_NUMBER_LEGNTH){
+			throw new IllegalArgumentException(ERROR_MESSAGE);
+		}
+
 		if(numSet.size()!= intObj.size()){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(ERROR_MESSAGE);
         }
 		for (int e : intObj) {
 			if (e > MAX_LOTTO_NUMBER || e < MIN_LOTTO_NUMBER) {
@@ -33,19 +43,16 @@ public class Checker {
 		}
 		return intObj;
 	}
-
 	public static int checkBonusInput(List<Integer> list,String str) {
 		int bonusNumber = Integer.parseInt(str);
 		if(bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(ERROR_MESSAGE);
 		}
 		for (int e : list){
 			if (e == bonusNumber) {
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException(ERROR_MESSAGE);
 			}
 		}
 		return bonusNumber;
 	}
-
-
 }

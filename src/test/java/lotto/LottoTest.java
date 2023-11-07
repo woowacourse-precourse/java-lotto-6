b64af0d7856f479d3d20e76197a86532e8ba3286
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2023-11-07 18:04:57 +0900

    test: Lottotest케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +26,48 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 목록이 6개 미만인 경우 예외가 발생한다.")
+    @Test
+    void createLottoWithLessThanSixNumbers(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 범위를 벗어나는 숫자가 있는 경우 예외가 발생한다.")
+    @Test
+    void createLottoWithOutOfRangeNumbers() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 목록이 null인 경우 예외가 발생한다.")
+    @Test
+    void createLottoWithNullNumbers() {
+        assertThatThrownBy(() -> new Lotto(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 정상적으로 생성된다.")
+    @Test
+    void createValidLotto() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+        assertThat(lotto.getNumbers()).isEqualTo(numbers);
+    }
+
+    @DisplayName("로또 번호가 중복 없이 생성된다.")
+    @Test
+    void createLottoWithoutDuplicates() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+        assertThat(lotto.getNumbers()).doesNotHaveDuplicates();
+    }
+
+    @DisplayName("로또 번호가 주어진 범위 내에서 생성된다.")
+    @Test
+    void createLottoWithinRange() {
+        Lotto lotto = Lotto.generateRandomLotto();
+        List<Integer> numbers = lotto.getNumbers();
+        assertThat(numbers).allMatch(num -> num >= 1 && num <= 45);
+    }
 }

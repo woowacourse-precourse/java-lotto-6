fc7c216c61df8ee81f84407e4221c1928ed57a22
BOYOUNG-KANG <28287@naver.com>
2023-11-08 00:31:52 +0900

    refactor: stream으로 코드 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,20 +1,17 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lotto.constant.GameConstant;
 
 public class LottoGenerator {
     public List<Lotto> makeLotto(int count){
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(GameConstant.NUMBER_MIN,
-                    GameConstant.NUMBER_MAX, GameConstant.NUMBER_SIZE)
-                    .stream().sorted().collect(Collectors.toList());
-            lottos.add(new Lotto(lotto));
-        }
-        return lottos;
+        return IntStream.range(0, count)
+                .mapToObj(i -> new Lotto(Randoms.pickUniqueNumbersInRange(GameConstant.NUMBER_MIN,
+                                GameConstant.NUMBER_MAX, GameConstant.NUMBER_SIZE)
+                        .stream().sorted().collect(Collectors.toList())))
+                .collect(Collectors.toList());
     }
 }

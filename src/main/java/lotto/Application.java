3e8d2b6880e208d88492e4346537f4d8861efb05
giljin <68088970+giljin@users.noreply.github.com>
2023-11-08 00:00:40 +0900

    feat(Application): 로또 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,18 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoResultManager lottoResultManager = new LottoResultManager();
+        LottoTicketGenerator generator = new LottoTicketGenerator();
+        Payment payment = UserIO.scanPayment();
+
+        Lottos lottos = generator.generate(payment);
+        lottos.printLottos();
+
+        Lotto winningsLotto = UserIO.scanWinningsLotto();
+        BonusNumber bonusNumber = UserIO.scanBonusNumber(winningsLotto);
+        LottoWinningChecker lottoWinningChecker = new LottoWinningChecker(winningsLotto, bonusNumber);
+        lottos.checkLottos(lottoWinningChecker);
+
+        LottoResultManager lottoResultManager = lottoWinningChecker.getLottoResultManager();
+        lottoResultManager.printLottoResult();
     }
 }

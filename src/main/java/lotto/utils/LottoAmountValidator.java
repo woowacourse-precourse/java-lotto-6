7fea8cbff78994b16d5b654bbb8bc28907461735
Kiltae <loverpleasestayo0@gmail.com>
2023-11-07 15:17:24 +0900

    feat: add lotto number type validation

---
+++ src/main/java/lotto/utils/LottoAmountValidator.java
@@ -0,0 +1,29 @@
+package lotto.utils;
+
+import lotto.view.ExceptionMessage;
+
+public class LottoAmountValidator {
+    public final int amount;
+    private static final int LOTTO_MIN_AMOUNT = 1000;
+
+    public LottoAmountValidator(String amount) {
+        this.amount = validateNumber(amount);
+        validate();
+    }
+
+    public void validate() {
+        validateNatural(amount);
+        validateDivisible(amount);
+    }
+
+    private int validateNumber(String amount) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            ExceptionMessage.numberException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    
+}

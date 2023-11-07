dda9e1aa200702a95765c650488e03e8e8f0f889
kijen723 <kij2646@gmail.com>
2023-11-07 21:02:42 +0900

    feat: Lotto count message

--- src/main/java/lotto/GameMessage.java
+++ src/main/java/lotto/GameMessage.java
@@ -4,4 +4,8 @@ public class GameMessage {
     public static void priceInputRequestMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void lottoCountMessage(int lottoCount) {
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount);
+    }
 }

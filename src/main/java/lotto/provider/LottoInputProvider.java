2b45ac801b545c412d6c91514f81c3e3fdb73042
ljkhyeong <jolri24@gmail.com>
2023-11-07 23:10:45 +0900

    fix: InputProvider 예외처리 추가
    Integer.parseInt 예외처리 추가

--- src/main/java/lotto/provider/LottoInputProvider.java
+++ src/main/java/lotto/provider/LottoInputProvider.java
@@ -5,26 +5,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.common.ErrorType;
 import lotto.domain.Lotto;
 
 public class LottoInputProvider {
 
 	public int getInputPrice(String question) {
 		System.out.println(question);
-		return Integer.parseInt(Console.readLine());
+		try {
+			return Integer.parseInt(Console.readLine());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorType.INVALID_PURCHASE_ERROR.getErrorMessage());
+		}
 	}
 
 	public Lotto getInputWinningNumber(String question) {
 		System.out.println(question);
-		List<Integer> numbers = Arrays.stream(Console.readLine().split(","))
-			.map(num -> Integer.parseInt(num))
-			.collect(Collectors.toList());
+		List<Integer> numbers;
+
+		try {
+			numbers = Arrays.stream(Console.readLine().split(","))
+				.map(num -> Integer.parseInt(num))
+				.collect(Collectors.toList());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorType.INVALID_LOTTO_NUMBER.getErrorMessage());
+		}
 
 		return new Lotto(numbers);
 	}
 
 	public int getInputBonusNumber(String question) {
 		System.out.println(question);
-		return Integer.parseInt(Console.readLine());
+
+		try {
+			return Integer.parseInt(Console.readLine());
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ErrorType.INVALID_BONUS_NUMBER.getErrorMessage());
+		}
 	}
 }

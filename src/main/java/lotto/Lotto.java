e4994385fcc58306b9f55b9eb8275555801d3e33
sosojean <thwlsdl0989@gmail.com>
2023-11-07 17:50:56 +0900

    로또와 당첨번호 대조 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -21,6 +23,22 @@ public class Lotto {
 
     void printNumbers() {
         System.out.println(numbers);
+    }
+
+    Integer compare(HashSet<Integer> luckyNumbers, Integer bonusNumber) {
+        List<Integer> lottoNumbers = this.numbers;
+        int count = 0;
+        for (int lottoNumber : lottoNumbers) {
+            for (int luckynumber : luckyNumbers) {
+                if (lottoNumber == luckynumber) {
+                    count++;
+                }
+            }
+        }
+        if (count == 5 & lottoNumbers.contains(bonusNumber)) {
+            System.out.println("2!!!");
+        }
+        return count;
     }
 
 

1b00afed8c11bf7909722b296846fe0de7153bd8
kang jihyeong <her4386@gmail.com>
2023-11-07 13:59:20 +0900

    [feat] 로또 숫자 범위 설정

--- src/main/java/lotto/utils/LottoSystemUtils.java
+++ src/main/java/lotto/utils/LottoSystemUtils.java
@@ -6,4 +6,8 @@ public class LottoSystemUtils {
 
     public static final int lottoCount = 6;
 
+    public static final int lottoNumberMinArrange = 1;
+
+    public static final int lottoNumberMaxArrange = 45;
+
 }

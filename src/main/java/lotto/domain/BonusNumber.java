006c3cc45abbfe2e2fee90c67378533df079e025
GiJung <weeds2577@gmail.com>
2023-11-08 02:20:35 +0900

    feat(BonusNumber): 보너스 번호의 유효 숫자 범위 예외 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -6,14 +6,24 @@ import java.util.List;
 
 public class BonusNumber {
 
+    private static final Integer START_NUMBER = 1;
+    private static final Integer END_NUMBER = 45;
+
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber, List<Integer> winningLottoNumbers) {
-        validator(bonusNumber, winningLottoNumbers);
+        validateNumber(bonusNumber);
+        validatorDuplicate(bonusNumber, winningLottoNumbers);
         this.bonusNumber = bonusNumber;
     }
 
-    private void validator(int bonusNumber, List<Integer> winningLottoNumbers) {
+    private static void validateNumber(int number) {
+        if (number > END_NUMBER || number < START_NUMBER) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private void validatorDuplicate(int bonusNumber, List<Integer> winningLottoNumbers) {
         if (winningLottoNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(ErrorCode.DUPLICATE_NUMBER.getMessage());
         }

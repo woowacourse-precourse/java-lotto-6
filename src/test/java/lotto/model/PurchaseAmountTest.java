8fa245cddbc095619158b96e962a9470026146f3
insu <is23193160@gmail.com>
2023-11-07 21:08:35 +0900

    test: 1000원 미만의 금액을 입력 받았을 시 예외가 발생하는 테스트 추가

--- src/test/java/lotto/model/PurchaseAmountTest.java
+++ src/test/java/lotto/model/PurchaseAmountTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class PurchaseAmountTest {
     @Test
-    @DisplayName("PurchaseAmount가 잘 초기화 되는지 확인")
+    @DisplayName("PurchaseAmount가 잘 초기화 되는지 확인한다")
     void initPurchaseAmount() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(50000);
 
@@ -16,11 +16,19 @@ class PurchaseAmountTest {
     }
 
     @Test
-    @DisplayName("구입할 수 있는 최대 로또 갯수를 잘 반환하는지 확인")
+    @DisplayName("구입할 수 있는 최대 로또 갯수를 잘 반환하는지 확인한다")
     void getMaxLottoCountForBudget() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(50000);
 
         assertThat(purchaseAmount.getMaxLottoCountForBudget()).isEqualTo(50);
+    }
+
+    @Test
+    @DisplayName("1000원 미만의 금액을 입력받았을 시 예외가 발생한다")
+    void inputLessThanLeastMoney() {
+        assertThatThrownBy(() -> new PurchaseAmount(500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 최소 1000원 이상의 돈을 입력해주세요.");
     }
 
 }

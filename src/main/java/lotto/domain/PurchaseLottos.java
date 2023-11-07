92dba1a3fca8dfa1dea8c514182e41c615e1cd4d
HeoManBo <iovesssr__@naver.com>
2023-11-07 23:44:26 +0900

    Refactor : PurchaseLotto 클래스 리팩토링

--- src/main/java/lotto/domain/PurchaseLottos.java
+++ src/main/java/lotto/domain/PurchaseLottos.java
@@ -3,33 +3,44 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import static lotto.Constant.*;
+import static lotto.Message.NOT_VALID_MONEY;
 
-public class PurchaseLottos extends Amount{
+public class PurchaseLottos {
 
+    protected final int unitOfMoney = 1000;
     private List<Lotto> userLottos = new ArrayList<>();
+
     private final int lottoTicketCount;
 
     public PurchaseLottos(int amount) {
-        super(amount);
-        lottoTicketCount = this.amount / this.unitOfMoney;
+        validateNotHasRemainder(amount);
+        lottoTicketCount = amount / this.unitOfMoney;
         makeLotto();
-    }
-
-
-    private void makeLotto(){
-        for(int i=0; i<lottoTicketCount; i++){
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(minimumLottoNumber, maximumLottoNumber, NUMBER_COUNT);
-            userLottos.add(new Lotto(numbers));
-        }
     }
 
     public BuyLottoInfo getBuyLottoInfo(){
         List<List<Integer>> lottoNumber = userLottos.stream().map(Lotto::getLottoNumbers).toList();
         return new BuyLottoInfo(lottoNumber, lottoTicketCount);
     }
+
+    public List<List<Integer>> geBuyingLottoNumbers(){
+        return userLottos.stream().map(Lotto::getLottoNumbers).toList();
+    }
+
+    private void validateNotHasRemainder(int amount){
+        if(amount % unitOfMoney != 0) throw new IllegalArgumentException(NOT_VALID_MONEY);
+    }
+
+    private void makeLotto(){
+        for(int i=0; i<lottoTicketCount; i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(minimumLottoNumber, maximumLottoNumber, NUMBER_COUNT);
+            Collections.sort(numbers);
+            userLottos.add(new Lotto(numbers));
+        }
+    }
+
 }

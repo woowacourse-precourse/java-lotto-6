96e6926370aa99392978000a818122443a47cd6c
oiler831 <qudwls831@gmail.com>
2023-11-07 16:45:43 +0900

    fix: WinningNumber 보강
    List 입력에서 string 입력으로 변경하였고 그에 따른 메소드 변경 및 추가가 있다.

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,38 +1,57 @@
 package lotto;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class WinningNumber {
-    private static final Integer LOTTO_NUMBER_SIZE = 6;
-    private static final Integer LOTTO_MIN_NUMBER = 1;
-    private static final Integer LOTTO_MAX_NUMBER = 45;
+    public static final String SPLITOR = ",";
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
     private final List<Integer> winningNumbers;
 
-    public WinningNumber(List<Integer> numbers) {
+    public WinningNumber(String stringNumbers) {
+        validate(stringNumbers);
+        this.winningNumbers = Arrays.stream(stringNumbers.split(SPLITOR))
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private void validate(String stringNumbers) {
+        List<Integer> numbers = validateNumber(stringNumbers);
         validateSize(numbers);
         validateDuplication(numbers);
         validateRange(numbers);
-        this.winningNumbers = numbers;
+    }
+
+    private List<Integer> validateNumber(String stringNumbers) {
+        try{
+            return Arrays.stream(stringNumbers.split(SPLITOR))
+                    .map(Integer::parseInt)
+                    .toList();
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.");
+        }
+
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자여야 합니다");
         }
     }
 
     private void validateDuplication(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되지 않는 숫자여야 합니다");
         }
     }
 
     private void validateRange(List<Integer> numbers) {
-        if (numbers.stream().mapToInt(v -> v).min().orElse(0) < LOTTO_MIN_NUMBER) {
-            throw new IllegalArgumentException();
-        }
-        if (numbers.stream().mapToInt(v -> v).max().orElse(0) > LOTTO_MAX_NUMBER) {
-            throw new IllegalArgumentException();
+        if (numbers.stream().mapToInt(v -> v).min().orElse(0) < LOTTO_MIN_NUMBER||
+                numbers.stream().mapToInt(v -> v).max().orElse(0) > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다");
         }
     }
 }

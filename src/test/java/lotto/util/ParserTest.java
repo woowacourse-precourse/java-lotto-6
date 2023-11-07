587514b53bc1e39581d698bdc1b2d6468679872a
enble <enble_777@naver.com>
2023-11-07 23:41:43 +0900

    style: 작업중

--- src/test/java/lotto/util/ParserTest.java
+++ src/test/java/lotto/util/ParserTest.java
@@ -1,9 +1,7 @@
 package lotto.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

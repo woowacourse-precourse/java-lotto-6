929d9f72c985d7baaaf71239a4056fdaab9ef91f
seona-moon <fhflwhwl5@gmail.com>
2023-11-07 21:52:04 +0900

    feat: 수익률을 계산하기 위한 util 구현

--- src/main/java/lotto/util/ProfitCalculator.java
+++ src/main/java/lotto/util/ProfitCalculator.java
@@ -1,2 +1,15 @@
-package lotto.util;public class ProfitCalculator {
+package lotto.util;
+
+import java.text.DecimalFormat;
+
+public class ProfitCalculator {
+
+    public static double calculateProfitPercentage(double purchaseAmount, double winningAmount) {
+        double profitPercentage = (winningAmount / purchaseAmount) * 100.0;
+        return roundToTwoDecimalPlaces(profitPercentage);
+    }
+
+    private static double roundToTwoDecimalPlaces(double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
 }

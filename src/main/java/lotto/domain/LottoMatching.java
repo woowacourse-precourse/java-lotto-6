5d33ced84e2443eaa25bd4ad98239e17ee310271
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-11-07 12:18:28 +0900

    feat : 로또 번호 비교 기능 구현

---
+++ src/main/java/lotto/domain/LottoMatching.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.List;
+
+// LottoCompae
+// ### ☑️ 로또 번호 비교 기능
+// - 발행한 로또 번호와 당첨 번호가 일치하는 개수를 계산한다.
+
+
+public class LottoMatching {
+
+    private int matchingCount = 0;
+    
+    public void MatchingNumber(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            if (winningNumbers.contains(lottoNumbers.get(i))) {
+                matchingCount++;
+            }
+        }
+    }
+
+    public void checkBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
+        if (matchingCount == 5 && lottoNumbers.contains(bonusNumber)) {
+            matchingCount *= 0;
+        }
+    }
+
+    public int getMatchingCount() {
+        return matchingCount;
+    }
+}

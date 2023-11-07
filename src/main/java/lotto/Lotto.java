b3acf386c097f149c0133f60eccfbae183169988
ratatuwee <pkqwaszx@gmail.com>
2023-11-07 20:27:53 +0900

    feat: Lotto 클래스 중복 숫자 확인 추가
    로또 번호가 중복된 경우 예외 발생

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,6 +20,9 @@ public class Lotto {
             if(number<1 && number>45){
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+            if (Collections.frequency(numbers, number) > 1) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+            }
         }
     }
 

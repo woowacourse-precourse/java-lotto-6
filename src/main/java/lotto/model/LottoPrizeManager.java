3948c16bc3203d51c9ed899432650e577af0651a
Dawin <dawin002@naver.com>
2023-11-07 18:11:21 +0900

    feat(LottoPrizeManager): 로또 상금 계산 클래스 추가
    로또 등수 결과와 상금 결과 의존성 주입:
    - LottoPrizeManager()
    최종 상금 계산해 상금 결과에 추가:
    - void calculateTotalPrize()
    - 등수 결과와 Prize enum을 활용해 최종 상금 계산
    등수에 해당하는 상금을 반환
    - long getPrizeOfRank(int rank)
    - Prize enum을 순회하며 rank에 해당하는 prize 반환

---
+++ src/main/java/lotto/model/LottoPrizeManager.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+import lotto.model.enums.Prize;
+import lotto.model.enums.Rank;
+
+public class LottoPrizeManager {
+    private LottoRankResult rankResult;
+    private LottoPrizeResult prizeResult;
+
+    public LottoPrizeManager(LottoRankResult rankResult, LottoPrizeResult prizeResult) {
+        this.rankResult = rankResult;
+        this.prizeResult = prizeResult;
+    }
+
+    public void calculateTotalPrize() {
+        for (int rank = Rank.RANK1.getRank(); rank <= Rank.RANK5.getRank(); rank++) {
+            long prize = getPrizeOfRank(rank);
+            int rankCount = rankResult.getCount(rank);
+            prizeResult.addTotalPrize(prize * rankCount);
+        }
+    }
+
+    private long getPrizeOfRank(int rank) {
+        for (Prize prize : Prize.values()) {
+            if (prize.isMatch(rank)) {
+                return prize.getPrize();
+            }
+        }
+        return Prize.NO_PRIZE.getPrize();
+    }
+}

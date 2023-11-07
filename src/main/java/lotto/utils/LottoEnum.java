4f8c9bdc9f8d61f3f8ae86131da8dc17ba5cccbb
sangjun <sangjun1389@ajou.ac.kr>
2023-11-07 15:30:52 +0900

    feat(LottoEnum): 프로그램 실행 간 필요한 Enum 추가
    - 로또 가격, 번호 개수, 시작 숫자, 끝 숫자
    - 보너스 숫자 카운트, 보너스 숫자 사용 상태

--- src/main/java/lotto/utils/LottoEnum.java
+++ src/main/java/lotto/utils/LottoEnum.java
@@ -1,2 +1,20 @@
-package lotto.utils;public enum LottoEnum {
+package lotto.utils;
+
+public enum LottoEnum {
+    LOTTO_PRICE(1000),
+    LOTTO_COUNT(6),
+    LOTTO_START_NUMBER(1),
+    LOTTO_END_NUMBER(45),
+    LOTTO_BONUS_NUMBER_COUNT(7),
+    LOTTO_BONUS_USING_CONDITION(5);
+
+    private final int value;
+
+    LottoEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }

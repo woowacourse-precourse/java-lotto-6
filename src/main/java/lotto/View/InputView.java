26fb5e6a9da0e7ab89bb5337e47b344fe77794c2
ekeprl <ykeon26@naver.com>
2023-11-07 23:24:40 +0900

    구매, 당첨번호, 보너스번호 입력 총 3개로 이루어져있던것을 세분화시킴

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -3,17 +3,7 @@ package lotto.View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String BuyLotto() {
-        System.out.println("구매 금액을 입력하세요");
-        return Console.readLine();
-    }
-    public static String RightNumber() {
-        System.out.println("당첨번호를 입력해주세요.");
-        return Console.readLine();
-    }
-
-    public static String BonusNumber() {
-        System.out.println("보너스 번호를 입력해주세요.");
+    protected String inputValue() {
         return Console.readLine();
     }
 }

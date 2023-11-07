0ee3f8cd8ddaa824a008e1dc64bfed1da948fa00
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 17:35:15 +0900

    feat: add print lotto to output view

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
+
 public class OutputView {
 
     private final static String PRINT_BUY_AMOUNT = "%d개를 구매했습니다.\n";
+    
+
+    private void printLotto(List<Integer> numbers) {
+        String numbersLine = numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", ", "[", "]"));
+        System.out.println(numbersLine);
+    }
 
     private void printBuyAmount(int buyAmount) {
         System.out.printf(PRINT_BUY_AMOUNT, buyAmount);

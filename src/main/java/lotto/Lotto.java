5f2b8770df1115c5364dec7c80e4d9cd15c48db3
aammddkkzxc <ideal0427@naver.com>
2023-11-07 17:56:01 +0900

    feat: 로또 번호 범위 유효성 검사 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,19 +8,29 @@ import java.util.Set;
 public class Lotto {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
-    private static final String CHECK_LOTTO_SIZE_REQUEST_MESSAGE = "로또 번호는 총 6개 여야 합니다.";
+    private static final String CHECK_LOTTO_SIZE_REQUEST_MESSAGE = "로또 번호는 총 6개여야 합니다.";
+    private static final String CHECK_LOTTO_NUMBER_RANGE_REQUEST_MESSAGE = "로또 번호는 1~45 사이의 숫자여야 합니다.";
     private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
+        validateLottoNumberRange(numbers);
         this.numbers = numbers;
     }
 
     private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(CHECK_LOTTO_SIZE_REQUEST_MESSAGE);
+        }
+    }
+
+    private void validateLottoNumberRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if ((number < LOTTO_MIN_NUMBER) || (number > LOTTO_MAX_NUMBER)) {
+                throw new IllegalArgumentException(CHECK_LOTTO_NUMBER_RANGE_REQUEST_MESSAGE);
+            }
         }
     }
 

c18a61d1c731fa1cfe40b943524ea52737d9dba7
SeYoE <onetuks@naver.com>
2023-11-08 03:39:41 +0900

    Fix: 출력 메시지 수정

--- src/main/java/lotto/io/processor/OutputProcessor.java
+++ src/main/java/lotto/io/processor/OutputProcessor.java
@@ -1,18 +1,19 @@
 package lotto.io.processor;
 
+import java.text.DecimalFormat;
 import java.util.List;
-import lotto.model.WinningGrade;
+import lotto.domain.statics.WinningGrade;
 
 public class OutputProcessor {
 
     private static final String PURCHASE_PRICE_HINT = "구입 금액을 입력해 주세요.";
-    private static final String WINNING_NUMBERS_HINT = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_HINT = "보너스 번호를 입력해 주세요.";
-    private static final String WINNING_STATICS_HINT = ("당첨 통계\n---");
+    private static final String WINNING_NUMBERS_HINT = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_HINT = "\n보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATICS_HINT = "\n당첨 통계\n---";
 
-    private static final String LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
-    private static final String WINNING_STATICS_FORMAT = "%d개 일치 (%d원) - %d개";
-    private static final String WINNING_BONUS_STATICS_FORMAT = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
+    private static final String LOTTO_COUNT_FORMAT = "\n%d개를 구매했습니다.";
+    private static final String WINNING_STATICS_FORMAT = "%d개 일치 (%s원) - %d개";
+    private static final String WINNING_BONUS_STATICS_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
     private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
 
     public void outputErrorMessage(final Exception e) {
@@ -36,20 +37,22 @@ public class OutputProcessor {
     }
 
     public void outputLottoCount(final int count) {
-        String message = String.format(LOTTO_COUNT_FORMAT, count);
+        final String message = String.format(LOTTO_COUNT_FORMAT, count);
         System.out.println(message);
     }
 
     public void outputWinningStatics(final WinningGrade winningGrade, final int count) {
+        final DecimalFormat decimalFormat = new DecimalFormat("#,###");
+
         String message = String.format(WINNING_STATICS_FORMAT,
                 winningGrade.getMatchedCount(),
-                winningGrade.getWinningPrice(),
+                decimalFormat.format(winningGrade.getWinningPrice()),
                 count);
 
         if (winningGrade.isMatchedBonusNumber()) {
             message = String.format(WINNING_BONUS_STATICS_FORMAT,
                     winningGrade.getMatchedCount(),
-                    winningGrade.getWinningPrice(),
+                    decimalFormat.format(winningGrade.getWinningPrice()),
                     count);
         }
 
@@ -57,7 +60,7 @@ public class OutputProcessor {
     }
 
     public void outputProfitRate(final float profitRate) {
-        String message = String.format(PROFIT_RATE_FORMAT, profitRate);
+        final String message = String.format(PROFIT_RATE_FORMAT, profitRate);
         System.out.println(message);
     }
 

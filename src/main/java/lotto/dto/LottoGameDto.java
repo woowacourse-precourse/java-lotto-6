847581711382e0c495e85fb5429cae72a268922f
chs98412 <chs98412@gmail.com>
2023-11-07 10:52:02 +0900

    feat: 결과 반환

--- src/main/java/lotto/dto/LottoGameDto.java
+++ src/main/java/lotto/dto/LottoGameDto.java
@@ -1,5 +1,7 @@
 package lotto.dto;
 
+import java.lang.reflect.Array;
+
 public class LottoGameDto {
     public static class Response{
         private final Long lottoGameId;
@@ -10,6 +12,52 @@ public class LottoGameDto {
 
         public Long getLottoGameId() {
             return lottoGameId;
+        }
+    }
+    public static class Request{
+        private final Long lottoGameId;
+
+        public Request(Long lottoGameId) {
+            this.lottoGameId = lottoGameId;
+        }
+
+        public Long getLottoGameId() {
+            return lottoGameId;
+        }
+    }
+    public static class Result{
+        private final int three;
+        private final int four;
+        private final int five;
+        private final int fiveAndBonus;
+        private  final int six;
+
+        public Result(int three, int four, int five, int fiveAndBonus, int six) {
+            this.three = three;
+            this.four = four;
+            this.five = five;
+            this.fiveAndBonus = fiveAndBonus;
+            this.six = six;
+        }
+
+        public int getThree() {
+            return three;
+        }
+
+        public int getFour() {
+            return four;
+        }
+
+        public int getFive() {
+            return five;
+        }
+
+        public int getFiveAndBonus() {
+            return fiveAndBonus;
+        }
+
+        public int getSix() {
+            return six;
         }
     }
 

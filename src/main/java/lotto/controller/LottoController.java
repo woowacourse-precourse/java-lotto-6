22a4790cc133f280b1460dff4df6f24ca93737a9
KWY <wodrk1001@gmail.com>
2023-11-07 10:38:18 +0900

    [feat] 에러 발생 시 에러가 발생한 부분부터 다시 시작하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.domain.Lotto;
 import lotto.domain.LottoMatch;
 import lotto.dto.LottoMatchCountDto;
+import lotto.dto.LottoViewDto;
 import lotto.model.LottoModel;
 import lotto.validate.BonusNumberValidation;
 import lotto.validate.PurchaseAmountValidation;
@@ -24,24 +25,77 @@ public class LottoController {
     }
 
     public void startGame() {
+        LottoViewDto lottoViewDto = new LottoViewDto();
+        while (true) {
+            try {
+                playGame(lottoViewDto);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void playGame(final LottoViewDto lottoViewDto) {
+        if (!lottoViewDto.isPurchaseAmount()) inputPurchaseAmount(lottoViewDto);
+        if (!lottoViewDto.isLottos()) printPurchaseLottos(lottoViewDto);
+        if (!lottoViewDto.isWinningNumbers()) inputWinningNumber(lottoViewDto);
+        if (!lottoViewDto.isBonusNumber()) inputBonusNumber(lottoViewDto);
+        printLottoResult(lottoViewDto);
+        printLottoRevenue(lottoViewDto);
+    }
+
+    private void inputPurchaseAmount(final LottoViewDto lottoViewDto) {
         lottoOutputView.printUserInputPurchaseAmount();
         int purchaseAmount = lottoInputView.inputPurchaseAmount(new PurchaseAmountValidation());
 
+        lottoViewDto.setPurchaseAmountAndCount(purchaseAmount);
+    }
+
+    private void printPurchaseLottos(final LottoViewDto lottoViewDto) {
+        int purchaseAmount = lottoViewDto.getPurchaseAmount();
+        int purchaseAmountCount = lottoViewDto.getPurchaseAmountCount();
+
         lottoOutputView.printPurchaseAmountCount(purchaseAmount);
-        List<Lotto> lottos = lottoModel.getRandomLottos(purchaseAmount / 1000);
+        List<Lotto> lottos = lottoModel.getRandomLottos(purchaseAmountCount);
         lottoOutputView.printLottos(lottos);
 
+        lottoViewDto.setLottos(lottos);
+    }
+
+    private void inputWinningNumber(final LottoViewDto lottoViewDto) {
         lottoOutputView.printUserInputWinningNumber();
         List<Integer> winningNumbers = lottoInputView.inputWinningNumber(new WinningNumberValidation());
 
+        lottoViewDto.setWinningNumbers(winningNumbers);
+    }
+
+    private void inputBonusNumber(final LottoViewDto lottoViewDto) {
+        List<Integer> winningNumbers = lottoViewDto.getWinningNumbers();
+
         lottoOutputView.printUserInputBonusNumber();
         int bonusNumber = lottoInputView.inputBonusNumber(new BonusNumberValidation(), winningNumbers);
+
+        lottoViewDto.setBonusNumber(bonusNumber);
+    }
+
+    private void printLottoResult(final LottoViewDto lottoViewDto) {
+        List<Lotto> lottos = lottoViewDto.getLottos();
+        List<Integer> winningNumbers = lottoViewDto.getWinningNumbers();
+        int bonusNumber = lottoViewDto.getBonusNumber();
 
         List<LottoMatch> lottoMatches = lottoModel.getLottoMatches(lottos, winningNumbers, bonusNumber);
         LottoMatchCountDto lottoMatchCountDto = lottoModel.getLottoMatchCount(lottoMatches);
         lottoOutputView.printLottoResult(lottoMatchCountDto);
 
-        double lottoResultRevenue = lottoModel.calculateRevenue(lottoMatches, purchaseAmount / 1000);
+        lottoViewDto.setLottoMatches(lottoMatches);
+    }
+
+    private void printLottoRevenue(final LottoViewDto lottoViewDto) {
+        List<LottoMatch> lottoMatches = lottoViewDto.getLottoMatches();
+        int purchaseAmountCount = lottoViewDto.getPurchaseAmountCount();
+
+        double lottoResultRevenue = lottoModel.calculateRevenue(lottoMatches, purchaseAmountCount);
         lottoOutputView.printLottoRevenue(lottoResultRevenue);
     }
 }

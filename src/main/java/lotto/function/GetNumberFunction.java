eafaa381bdde7e861b7416c05e73c1f1be52b08d
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-08 01:23:47 +0900

    remove: test실패로 인한 정렬기능 삭제

--- src/main/java/lotto/function/GetNumberFunction.java
+++ src/main/java/lotto/function/GetNumberFunction.java
@@ -2,14 +2,11 @@ package lotto.function;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class GetNumberFunction {
     public List<Integer> getLottoNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(numbers);
         return numbers;
     }
 }

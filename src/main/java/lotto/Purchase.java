6aaaa510e2515ba1254b27eefff06ba89cbc4c88
noeyodeel <sko8044@gmail.com>
2023-11-07 17:53:39 +0900

    feat : 금액 입력 예외처리.
    - 금액이 1000으로 나누어 떨어지지 않을때의 예외 처리 구현함.
    - 메소드를 분리하여 담당하는 기능을 나누게함.

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -5,14 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class Purchase {
   public int purchaseAmount() {
     System.out.println("로또 구입 금액을 입력하세요.");
+    System.out.println(lotto_purchase() + "개를 구매했습니다.");
+    return lotto_purchase();
+  }
+
+  private int lotto_purchase() {
+
     int purchaseAmount = Integer.parseInt(Console.readLine());
+    exception_divide(purchaseAmount);
     int numberOfTickets = purchaseAmount / 1000;
     return numberOfTickets;
   }
 
   private void exception_divide(int p) {
-      if(p % 1000 != 0) {
-        throw new IllegalArgumentException("1000으로 나눠지지 않습니다.");
-      }
+    if (p % 1000 != 0) {
+      throw new IllegalArgumentException("1000으로 나눠지지 않습니다.");
     }
+  }
+
 }

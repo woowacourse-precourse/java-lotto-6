216d1560af24892a7d8b1aa2a2de1d93170ce6e3
jexnjeux <zieun.na@gmail.com>
2023-11-08 00:58:49 +0900

    fix: 로또번호, 보너스번호 입력 시, 공백 허용

--- src/main/java/lotto/domain/GameInput.java
+++ src/main/java/lotto/domain/GameInput.java
@@ -18,7 +18,7 @@ public class GameInput {
 
     public int readAndValidatePurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        String input = Console.readLine();
+        String input = Console.readLine().replace(" ", "");
         Validator.validatePurchaseAmount(input);
         return Integer.parseInt(input);
     }
@@ -29,9 +29,9 @@ public class GameInput {
 
     public List<Integer> readAndValidateNumbers() {
         System.out.println("로또 번호를 입력하세요. 번호는 쉼표(,)를 기준으로 구분합니다.");
-        String input = Console.readLine();
+        String input = Console.readLine().replace(" ", "");
         Validator.validateNumbers(input);
-        String[] inputStrings = input.trim().split(",");
+        String[] inputStrings = input.split(",");
         List<Integer> numbers = new ArrayList<>();
         for (String inputString : inputStrings) {
             numbers.add(Integer.parseInt(inputString));
@@ -45,13 +45,17 @@ public class GameInput {
 
     public int readAndValidateBonumNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        String input = Console.readLine();
+        String input = Console.readLine().replace(" ", "");
         Validator.validateLottoNumber(input);
         return Integer.parseInt(input);
     }
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    public int calculateLottoPurchase() {
+        return purchaseAmount / 1000;
     }
 
 

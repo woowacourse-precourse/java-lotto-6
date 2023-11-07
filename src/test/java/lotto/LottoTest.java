f1bc6be34033025fa30cdc3a07b6f4aea714ffc9
Hoo-sung <gntjd135@gmail.com>
2023-11-07 11:10:28 +0900

    test(LottoTest): 로또 숫자가 6개가 아닌 경우 테스트 코드 생성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,17 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.system.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -25,4 +32,27 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    @DisplayName("checkNumberCount 검증 테스트")
+    class CheckNumberCount {
+        @ParameterizedTest(name = "{0}가 입력되었을 때")
+        @MethodSource("parameterProvider")
+        void 유효한_개수가_입력되었는지_테스트(List<Integer> target) {
+            assertThatThrownBy(() -> new Lotto(target))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ExceptionMessage.COUNT_NOT_EQUAL);
+        }
+
+        private Stream<Arguments> parameterProvider() {
+            return Stream.of(
+                    Arguments.of(List.of()),
+                    Arguments.of(List.of(1)),
+                    Arguments.of(List.of(1,2)),
+                    Arguments.of(List.of(1,2,3)),
+                    Arguments.of(List.of(1,2,3,4,5)),
+                    Arguments.of(List.of(1,2,3,4,5,6,7))
+            );
+        }
+    }
 }

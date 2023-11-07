7776aed27b0e4889a8e08ee8386fc23f854ffdf9
icebear0111 <wltjq0901@naver.com>
2023-11-07 22:37:20 +0900

    feat: lotto numbers and bonus number input method

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import util.Validate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
@@ -18,5 +19,35 @@ public class User {
         }
 
         return price;
+    }
+
+    public List<Integer> winningNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while(numbers.size() < 6) {
+            int num = Integer.parseInt(Console.readLine());
+            if (!numbers.contains(num))
+                numbers.add(num);
+        }
+
+        try {
+            Validate.isValidLotto(numbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            winningNumbers();
+        }
+        return numbers;
+    }
+
+    public int bonusNumber() {
+        int bonusNumber = Integer.parseInt(Console.readLine());
+
+        try {
+            Validate.isValidBonus(bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            bonusNumber();
+        }
+
+        return bonusNumber;
     }
 }

14fcc72495d7aac452b5b7bec1f1c328964b0511
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-11-08 06:31:33 +0900

    feat: 수익률 둘째 자리에서 반올림 기능 추가

--- src/main/java/lotto/domain/Counter.java
+++ src/main/java/lotto/domain/Counter.java
@@ -7,7 +7,7 @@ public class Counter {
     }
 
     public double calculateProfitRate(int userMoney, long totalPrize) {
-        return (userMoney - totalPrize) / (double) userMoney * 100;
+        double profitRate = (userMoney - totalPrize) / (double) userMoney * 100;
+        return Math.round(profitRate * 10.0) / 10.0;
     }
-
 }

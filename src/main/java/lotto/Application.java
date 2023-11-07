366ee30731d5954ee364fbd9764b646efd4c789a
hellomatia <shuaigejp80@gmail.com>
2023-11-07 16:57:30 +0900

    feat(application): Application 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.domain.LottoGenerator;
+import lotto.domain.RandomLottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(initInputView(), initOutPutView(), initLottoGenerator());
+        controller.run();
+    }
+
+    private static InputView initInputView() {
+        return InputView.create();
+    }
+
+    private static OutputView initOutPutView() {
+        return OutputView.create();
+    }
+
+    private static LottoGenerator initLottoGenerator() {
+        return RandomLottoGenerator.create();
     }
 }

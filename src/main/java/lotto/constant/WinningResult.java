0387d83f510edf2a22535a6edf373fd9be89095e
Seyeon <wjdtpdus828@naver.com>
2023-11-07 17:33:45 +0900

    modify(WinningResult): 상수 수정

--- src/main/java/lotto/constant/WinningResult.java
+++ src/main/java/lotto/constant/WinningResult.java
@@ -2,11 +2,11 @@ package lotto.constant;
 
 public enum WinningResult {
 
-    FIRST("1등: 6개 번호 일치 / 2,000,000,000원"),
-    SECOND("2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원"),
-    THIRD("3등: 5개 번호 일치 / 1,500,000원"),
-    FORTH("4등: 4개 번호 일치 / 50,000원"),
-    FIFTH("5등: 3개 번호 일치 / 5,000원");
+    FIRST("6개 일치 (2,000,000,000원) - "),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD("5개 일치 (1,500,000원) - "),
+    FORTH("4개 일치 (50,000원) - "),
+    FIFTH("3개 일치 (5,000원) - ");
     private final String winningMessage;
     WinningResult(String winningMessage){
         this.winningMessage = winningMessage;

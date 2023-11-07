a01861c7c84f1879027e57c6f492dda0a0eda5ab
Kiltae <loverpleasestayo0@gmail.com>
2023-11-07 17:12:54 +0900

    feat: winning number duplicate validation

---
+++ src/main/java/lotto/utils/PrizeNumberValidator.java
@@ -0,0 +1,29 @@
+package lotto.utils;
+
+import lotto.view.ExceptionMessage;
+
+import java.util.*;
+
+public class PrizeNumberValidator {
+    private final List<Integer> numbers = new ArrayList<>();
+    private final int bonusNumber;
+
+    public PrizeNumberValidator(List<Integer> winNumber, int bonusNumber) {
+        validateSixNumbers(winNumber);
+        validateNumberRange(bonusNumber);
+        validateBonusNumber(winNumber, bonusNumber);
+        validateRange(winNumber);
+        validateDuplicatedNumber(winNumber);
+
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicatedNumber(List<Integer> winNumber) {
+        Set<Integer> duplicatedCheck = new HashSet<>(winNumber);
+        if (duplicatedCheck.size() != 6) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_DUPLICATED_ERROR);
+        }
+    }
+
+   
+}

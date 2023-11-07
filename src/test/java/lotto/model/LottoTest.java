25aad0efd2cabbd93076444e128a4f0901443d79
cozym <ddongyee99@daum.net>
2023-11-08 00:10:19 +0900

    test: 생성된 로또번호 개수 검사 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,6 +1,16 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoTest {
-
-
+    @DisplayName("로또 번호의 개수가 6개가 넘으면 예외처리")
+    @Test
+    void 로또번호_개수검사() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,6,7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

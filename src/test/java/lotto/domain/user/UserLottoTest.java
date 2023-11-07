2c2d700b8bd2ad1778e80984e2dc5af8342bbf16
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-07 11:04:19 +0900

    test: 순서 수정

--- src/test/java/lotto/domain/user/UserLottoTest.java
+++ src/test/java/lotto/domain/user/UserLottoTest.java
@@ -12,16 +12,16 @@ public class UserLottoTest {
     @DisplayName("UserLotto에 발행된 로또들이 저장되는지 확인한다.")
     @Test
     void createUserLotto() {
+        //given
         LottoCount lottoCount = new LottoCount(3000);
         UserLotto userLotto = new UserLotto(lottoCount);
         MockNumberGenerator mockNumberGenerator = new MockNumberGenerator();
-
-        Lotto lotto = new Lotto(List.of(1, 3, 5, 7, 9, 11));
 
         //when
         userLotto.create(mockNumberGenerator);
 
         //then
+        Lotto lotto = new Lotto(List.of(1, 3, 5, 7, 9, 11));
         assertThat(userLotto.getLottos().get(0)).usingRecursiveComparison().isEqualTo(lotto);
         assertThat(userLotto.getLottos().get(1)).usingRecursiveComparison().isEqualTo(lotto);
     }

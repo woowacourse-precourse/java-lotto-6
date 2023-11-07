2c4e339787d340bf574d88146e476971bb14ee35
KimSangHyeok <tkdgur0906@naver.com>
2023-11-07 23:19:44 +0900

    feat : 보너스 번호 입력 문구 출력 기능 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -9,6 +9,7 @@ public class LottoView {
     private static final String PURCHASE_PRICE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_COUNT = "개를 구매했습니다.";
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static void printPurchasePrompt() {
         System.out.println(PURCHASE_PRICE);
@@ -31,4 +32,7 @@ public class LottoView {
         System.out.println(WINNING_NUMBER);
     }
 
+    public static void printBonusPrompt() {
+        System.out.println(BONUS_NUMBER);
+    }
 }

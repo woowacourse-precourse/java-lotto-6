161ed263f7adecd95f00257b51cc8599d70808a8
ChanUi <hcu55@naver.com>
2023-11-08 06:00:29 +0900

    feat: 로또 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,18 @@
 package lotto.view;
 
+import lotto.domain.LottoGrade;
+
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String PRINT_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PRINT_COUNT_PURCHASE_LOTTO_MESSAGE = "개를 구매했습니다.";
     private static final String PRINT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String PRINT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTIC_MESSAGE = "당첨 통계";
+    private static final String DIVISION_LINE = "---";
+
 
     public void printLottoPurchaseAmountInputMessage() {
         System.out.println(PRINT_LOTTO_PURCHASE_AMOUNT_MESSAGE);
@@ -29,5 +35,19 @@ public class OutputView {
     public void printBonusNumberInputMessage() {
         System.out.println();
         System.out.println(PRINT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public void printWinningStatisticsMessage() {
+        System.out.println();
+        System.out.println(WINNING_STATISTIC_MESSAGE);
+        System.out.println(DIVISION_LINE);
+    }
+
+    public void printLottoStatistic(Map<LottoGrade, Integer> result) {
+        System.out.println("3개 일치 (5,000원) - " + result.get(LottoGrade.FIFTH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get(LottoGrade.FOURTH) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get(LottoGrade.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get(LottoGrade.SECOND) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.get(LottoGrade.FIRST) + "개");
     }
 }

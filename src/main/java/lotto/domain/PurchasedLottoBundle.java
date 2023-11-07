d05305fe6812b6cfd271f0570508c24bd57503ca
JaehongDev <println@kakao.com>
2023-11-07 20:43:50 +0900

    refactor: 정산 로직 도메인으로 이동

--- src/main/java/lotto/domain/PurchasedLottoBundle.java
+++ src/main/java/lotto/domain/PurchasedLottoBundle.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.util.EnumMap;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class PurchasedLottoBundle {
     private final List<Lotto> lottoBundle;
@@ -15,17 +16,21 @@ public class PurchasedLottoBundle {
     }
 
     public WinningResults checkWinningResults(AnswerLotto answerLotto) {
-        return new WinningResults(EnumSet.allOf(LottoRewardTable.class).stream()
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        result -> lottoBundle.stream()
-                                .map(answerLotto::match)
-                                .map(LottoMatchingTable::of)
-                                .filter(m -> Objects.equals(result.getLottoMatchingTable(), m))
-                                .count(),
+        return new WinningResults(EnumSet.allOf(LottoRewardTable.class)
+                .stream()
+                .collect(toMap(Function.identity(),
+                        lottoRewardTable -> calculateMatchCount(answerLotto, lottoRewardTable),
                         (prev, next) -> next,
                         () -> new EnumMap<>(LottoRewardTable.class)
                 )));
+    }
+
+    private long calculateMatchCount(AnswerLotto answerLotto, LottoRewardTable lottoRewardTable) {
+        return lottoBundle.stream()
+                .map(answerLotto::match)
+                .map(LottoMatchingTable::of)
+                .filter(lottoMatchingTable -> Objects.equals(lottoRewardTable.getLottoMatchingTable(), lottoMatchingTable))
+                .count();
     }
 
     public int getQuantity() {
@@ -33,8 +38,8 @@ public class PurchasedLottoBundle {
     }
 
     public <T> List<T> convert(Function<Lotto, T> function) {
-        return lottoBundle.stream().map(function)
-                .collect(Collectors.toList());
-
+        return lottoBundle.stream()
+                .map(function)
+                .toList();
     }
 }
--- src/main/java/lotto/event/PaymentSummaryEvent.java
+++ src/main/java/lotto/event/PaymentSummaryEvent.java
@@ -1,17 +1,9 @@
 package lotto.event;
 
-import java.util.EnumMap;
-import java.util.EnumSet;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import lotto.domain.LottoMatchingTable;
-import lotto.domain.LottoRewardTable;
 import lotto.domain.Money;
 import lotto.event.EventListener.ReturnEvent;
 import lotto.repository.LottoRepository;
 import lotto.state.WinningStatisticsState;
-import lotto.state.WinningStatisticsState.MessagePerCount;
 
 public record PaymentSummaryEvent(LottoRepository lottoRepository) implements ReturnEvent<WinningStatisticsState> {
     @Override
@@ -20,31 +12,11 @@ public record PaymentSummaryEvent(LottoRepository lottoRepository) implements Re
         final var answerLotto = lottoRepository.findAnswerLotto();
         final var lottoBundle = lottoRepository.findPurchasedLottoBundle();
 
-        final var matchResults = lottoBundle.checkResult(answerLotto);
-
-        final var joinResult = EnumSet.allOf(LottoRewardTable.class).stream()
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        result -> matchResults.stream().map(LottoMatchingTable::of)
-                                .filter(m -> Objects.equals(result.getLottoMatchingTable(), m))
-                                .count(),
-                        (prev, next) -> next,
-                        () -> new EnumMap<>(LottoRewardTable.class)
-                ));
-
-        final var profit = joinResult.entrySet()
-                .stream().mapToLong((lottoRewardTableLongEntry ->
-                        lottoRewardTableLongEntry.getKey().getReward() * lottoRewardTableLongEntry.getValue()))
-                .sum();
-
-        final var messages = joinResult.entrySet()
-                .stream().map(lottoRewardTableLongEntry -> new MessagePerCount(lottoRewardTableLongEntry.getKey().getMessage(),
-                        lottoRewardTableLongEntry.getValue()))
-                .toList();
+        final var winningResults = lottoBundle.checkWinningResults(answerLotto);
 
         return new WinningStatisticsState(
-                paymentPrice.calculateProfit(new Money(profit)),
-                messages
+                paymentPrice.calculateProfitMargin(new Money(winningResults.calculateProfit())),
+                winningResults.getWinningMessages()
         );
     }
 }

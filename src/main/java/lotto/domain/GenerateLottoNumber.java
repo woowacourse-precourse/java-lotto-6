d65115e2866bb2c32775f050d049872a30f1868f
K-KY <kyuyoungk@naver.com>
2023-11-08 02:13:21 +0900

    Refactor(GenerateLottoNumber) : 정렬후 리턴

--- src/main/java/lotto/domain/GenerateLottoNumber.java
+++ src/main/java/lotto/domain/GenerateLottoNumber.java
@@ -1,12 +1,17 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GenerateLottoNumber {
-    private static final List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    private static List<Integer> lottoNumber;
     public static List<Integer> generate() {
-        return lottoNumber;
+        lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> sortedNumbers = new ArrayList<>(lottoNumber);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 
 }

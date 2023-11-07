6b9def48292c0c4a0f27e2146e4791f63b56896d
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-11-08 05:46:32 +0900

    feat: 수익률 계산 기능 추가

--- src/main/java/lotto/domain/Counter.java
+++ src/main/java/lotto/domain/Counter.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Counter {
     public int generateLottoCount(int userMoney) {
         int lottoCount = userMoney / 1000;
         return lottoCount;
+    }
+
+    public double calculateProfitRate(int userMoney, long totalPrize) {
+        return (userMoney - totalPrize) / (double) userMoney * 100;
     }
 
 }

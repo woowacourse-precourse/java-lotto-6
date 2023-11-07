ec30d62191012536b4825b1e936287215b57c8e7
2uwls <2uwls1126@gmail.com>
2023-11-07 14:11:13 +0900

    feat: create method calculate quantity of lotto by the number of money

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해주세요:");
+        getQuantityOfLotto(Console.readLine());
     }
+
+    public static Integer convertMoneyFormat(String inputMoney) {
+        Integer money = Integer.valueOf(inputMoney);
+        return money;
+    }
+    public static void getQuantityOfLotto(String inputMoney) {
+        Integer money = convertMoneyFormat(inputMoney);
+        Integer quantityOfLotto = money/1000;
+        System.out.println(quantityOfLotto);
+    }
+
+    //수량 만큼 반복
+        // 로또 랜덤 생성
+        //랜덤 생성된 로또 정렬
+        //로또 객체 만들기
+        //로또 list에 삽입(로또 배열 만들어야함)
 }

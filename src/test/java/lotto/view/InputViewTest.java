4873802c33cac608016e801950c53a45b4b2a9f9
k9want <zndwlrnjr26@daum.net>
2023-11-07 23:08:14 +0900

    test: 형식에 맞지 않는 당첨 번호를 입력받을 때 예외 발생

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -16,4 +16,18 @@ class InputViewTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 숫자가 아닌 다른 값이 입력되었습니다.");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            ",",
+            "1,2,3,4,5,6,7,",
+            "1,2,3,4,5,6,",
+            "122,",
+            "a,s,2,3,4,5"})
+    @DisplayName("형식에 맞지 않는 당첨 번호를 입력받을 때 예외 발생")
+    void testValidateWinningNumbersWithRegex(String invalidInput) {
+        Assertions.assertThatThrownBy(() -> Validation.validateWinningNumbersWithRegex(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 구분 쉼표(,)를 기준으로 숫자 6개를 입력해야 합니다.");
+    }
 }

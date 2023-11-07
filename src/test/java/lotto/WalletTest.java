dfedd1d3acb13f2efbe1be3b112b685739560a63
minkyole <7719s@nave.com>
2023-11-07 15:25:36 +0900

    test(WalletTest.java) : 구입금액이 최대금액을 넘는 경우 예외 테스트 구현
    - 구입금액이 최대금액을 넘는 경우 예외 테스트 구현 완료

--- src/test/java/lotto/WalletTest.java
+++ src/test/java/lotto/WalletTest.java
@@ -3,11 +3,10 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.lang.reflect.Field;
-import java.util.List;
-import lotto.model.Lotto;
 import lotto.model.Wallet;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -28,7 +27,7 @@ public class WalletTest {
     @DisplayName("구입 금액에 숫자가 아닌 값이 있으면 예외가 발생 한다.")
     @ParameterizedTest
     @ValueSource(strings = {"숫자아님", "notNumber", "1섞어2MIX3"})
-    void createWinningLottoByNonNumericValueBonusNumber(String money) {
+    void createWalletByNonNumericValueMoney(String money) {
         assertThatThrownBy(() -> new Wallet(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 구입 금액에 숫자가 아닌 값이 들어왔습니다.");
@@ -37,10 +36,21 @@ public class WalletTest {
     @DisplayName("구입 금액이 null인 경우 예외가 발생한다.")
     @ParameterizedTest
     @NullSource
-    void createWinningLottoByNullOrEmptyBonusNumber(String money) {
+    void createWalletByNullMoney(String money) {
         assertThatThrownBy(() -> new Wallet(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
-                        + maximumPurchaseAmount + "원");
+                        + maximumPurchaseAmount + "원.");
+    }
+
+    @DisplayName("구입 금액이 최대금액을 넘는 경우 예외가 발생한다.")
+    @Test
+    void createWinningLottoByNullOrEmptyBonusNumber() {
+        Integer inputMoney = maximumPurchaseAmount + 1000;
+
+        assertThatThrownBy(() -> new Wallet(inputMoney.toString()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
+                        + maximumPurchaseAmount + "원.");
     }
 }

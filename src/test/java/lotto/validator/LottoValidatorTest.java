a2e6aaa91ea843c63154331e4dc09c28c2957171
koomchang <leahpar0401@gmail.com>
2023-11-07 16:01:43 +0900

    style: import 정리

--- src/test/java/lotto/validator/LottoValidatorTest.java
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -1,7 +1,6 @@
 package lotto.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;

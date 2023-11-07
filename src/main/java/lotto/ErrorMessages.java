21f82cbd55b214a9362aa420141a7721de12f2ad
ldw <leehan02@hanyang.ac.kr>
2023-11-08 08:23:47 +0900

    feat(lotto): 예외처리하면서 추가적으로 발견한 발생가능한 예외에 대한 에러메세지들을 상수로 추가 정의

--- src/main/java/lotto/ErrorMessages.java
+++ src/main/java/lotto/ErrorMessages.java
@@ -2,12 +2,13 @@ package lotto;
 
 public enum ErrorMessages {
     NO_CONTENT("[ERROR] 공백은 유효하지 않은 입력입니다."),
-    NOT_NUMBER("[ERROR] 숫자를 입력해주세요."),
+    NOT_NUMBER("[ERROR] 숫자를 입력해야 합니다."),
     NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다."),
     NOT_MATCHED_DIGITS_FOR_NUMBERS("[ERROR] 로또 번호는 6자리를 입력해야 합니다."),
     NOT_MATCHED_DIGITS_FOR_BONUS_NUMBER("[ERROR] 보너스 번호는 1자리를 입력해야 합니다."),
     NOT_ALLOW_REMAINDER("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다."),
-    TOO_LOW_BUDGET("[ERROR] 로또 구입 금액은 1,000원 이상이어야 합니다.");
+    TOO_LOW_BUDGET("[ERROR] 로또 구입 금액은 1,000원 이상이어야 합니다."),
+    NOT_ALLOW_OVERLAP("중복된 숫자가 있습니다.");
 
 
     private final String message;

344981ab7727232f84959f8df2cc907afd84d58c
eun-byeol <joeunbyeol98@gmail.com>
2023-11-07 23:39:15 +0900

    feat(InputValidator): 자연수 검증 기능 구현

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -1,6 +1,7 @@
 package lotto.utils;
 
 import static lotto.enums.ExceptionMessageType.NUMBER_NOT_INTEGER;
+import static lotto.enums.ExceptionMessageType.PAYMENT_NOT_NATURAL_NUMBER;
 
 public class InputValidator {
     public static void validateInteger(String number) {
@@ -8,6 +9,12 @@ public class InputValidator {
             Integer.parseInt(number);
         } catch(NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_NOT_INTEGER.getMessage());
+        }
+    }
+
+    public static void validateNaturalNumber(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException(PAYMENT_NOT_NATURAL_NUMBER.getMessage());
         }
     }
 }

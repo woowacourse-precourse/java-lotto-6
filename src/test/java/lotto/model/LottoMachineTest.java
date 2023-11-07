43c9e408a39e5cb0f10e37fe22a4faf4758aaa82
JW0321 <digneotn123@gmail.com>
2023-11-07 16:28:35 +0900

    test: 로또 당첨 판단 기능 테스트

--- src/test/java/lotto/model/LottoMachineTest.java
+++ src/test/java/lotto/model/LottoMachineTest.java
@@ -1,12 +1,13 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import lotto.model.LottoStatistics.PrizeInformation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -66,6 +67,22 @@ public class LottoMachineTest {
 
         assertThatThrownBy(() -> lottoMachine.setBonusNumber(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 당첨 번호와 일치하는 갯수에 따라 당첨 판단")
+    @Test
+    void test() {
+        int bonusNumber = 15;
+        List<Integer> winningNumbers = List.of(1, 7, 14, 21, 35, 41);
+        ArrayList<Lotto> testLotto = new ArrayList<>();
+        Lotto testLottoNumber = new Lotto(Arrays.asList(1,7,15,21,35,41));
+        testLotto.add(testLottoNumber);
+
+        lottoMachine.setWinningNumbers(winningNumbers);
+        lottoMachine.setBonusNumber(bonusNumber);
+        ArrayList<PrizeInformation> result = lottoMachine.determineLottoWinning(testLotto);
+
+        assertThat(result.get(0)).isEqualTo(PrizeInformation.SECOND_PRIZE);
     }
 
 }

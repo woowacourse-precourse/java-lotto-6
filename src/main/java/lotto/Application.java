f175a2ca418d95f937c2dd2bae085161bf6e061b
huhdy32 <mong3268@gmail.com>
2023-11-08 02:57:55 +0900

    refactor: 게임 실행 메소드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.InputController;
+import lotto.controller.LottoController;
+import lotto.logic.LottoLogic;
+import lotto.view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new LottoLogic(
+                new InputController(new ConsoleView()),
+                new LottoController()
+        ).run();
     }
 }

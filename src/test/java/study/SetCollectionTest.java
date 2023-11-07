cdf4b6ec4f1f4ce34dd13999461198885e1d808a
ASak1104 <khw56184@gmail.com>
2023-11-08 04:27:12 +0900

    test(study): 추가된 숫자 조회 테스트 추가

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetCollectionTest {
     private Set<Integer> numbers;
@@ -31,5 +33,18 @@ public class SetCollectionTest {
 
         // then
         assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "{0}을 조회한 결과는 true다.")
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("추가된 숫자 조회 테스트")
+    void testContainsInt(int existElement) {
+        // given
+
+        // when
+        boolean hasElement = numbers.contains(existElement);
+
+        // then
+        assertThat(hasElement).isTrue();
     }
 }

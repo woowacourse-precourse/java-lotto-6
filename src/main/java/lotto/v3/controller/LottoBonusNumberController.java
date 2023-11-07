020ff6ec87f6555e360e58e36151359e1dd89119
권장순 <jsoonworld@gmail.com>
2023-11-08 02:45:23 +0900

    feat( LottoBonusNumberController) : 보너스 번호를 관리하는 controller 구현.

---
+++ src/main/java/lotto/v3/controller/LottoBonusNumberController.java
@@ -0,0 +1,20 @@
+package lotto.v3.controller;
+
+import lotto.v3.model.LottoBonusNumber;
+import lotto.v3.model.LottoWinningNumber;
+import lotto.v3.view.LottoBonusNumberView;
+
+public class LottoBonusNumberController {
+    private final LottoBonusNumberView lottoBonusNumberView;
+    private final LottoWinningNumber winningNumber;
+
+    public LottoBonusNumberController(LottoBonusNumberView lottoBonusNumberView, LottoWinningNumber winningNumber) {
+        this.lottoBonusNumberView = lottoBonusNumberView;
+        this.winningNumber = winningNumber;
+    }
+
+    public LottoBonusNumber createBonusNumber() {
+        int bonusNumber = lottoBonusNumberView.requestBonusNumber();
+        return new LottoBonusNumber(bonusNumber, winningNumber.getNumbers());
+    }
+}

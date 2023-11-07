64aa7907a102b41a4caef83fdee90bbbe1d07645
mina <qkralsdk991@naver.com>
2023-11-07 16:59:53 +0900

    Test : Rank 기능 테스트 기능 구현

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,2 +1,18 @@
-package lotto.domain;public class RankTest {
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class RankTest {
+    @DisplayName("ENUM Rank를 결정하는 도메인 로직 테스트")
+    @Test
+    void decideRankTest() {
+        Rank rank;
+
+        rank = Rank.decideRank(5, true);
+        Assertions.assertThat(rank.getWinningMesaage()).isEqualTo("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
+    }
 }

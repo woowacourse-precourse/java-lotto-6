9f3e326f809fe897ecaebcc8d9da959a032f49f3
wonu606 <wonu606@gmail.com>
2023-11-08 06:29:51 +0900

    feat: 로또 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,21 +1,46 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = List.copyOf(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException(
+                    String.format("로또는 %d개의 숫자로 이루어져야 합니다. 입력값: %d", SIZE, numbers.size()));
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != SIZE) {
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException(
+                        String.format("로또 숫자의 범위는 %d에서 %d 사이여야 합니다.", MIN_NUMBER, MAX_NUMBER));
+            }
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

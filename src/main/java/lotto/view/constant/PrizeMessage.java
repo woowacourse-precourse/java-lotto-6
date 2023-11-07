19319f45740c2c95a3c4fb75c3d404de6676cbdf
jae woo Jung <awfjol2008@naver.com>
2023-11-08 01:46:28 +0900

    feat: PrizeMessage Enum 생성

---
+++ src/main/java/lotto/view/constant/PrizeMessage.java
@@ -0,0 +1,23 @@
+package lotto.view.constant;
+
+public enum PrizeMessage {
+
+    WINNING_STATUS("당첨 통계"),
+    LINE_SEPARATOR("---"),
+    MATCHING_NUMBER_COUNT("%d개 일치"),
+    MATCHING_BONUS_NUMBER(", 보너스 볼 일치"),
+    PRIZE_MONEY(" (%s원)"),
+    MONEY_FORMAT("#,###"),
+    WINNING_COUNT(" - %d개"),
+    PROFIT_RATE("총 수익률은 f%입니다.");
+
+    private final String message;
+
+    PrizeMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

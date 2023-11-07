513784e7bdd413eefdc647f0cbe05b630b3533fa
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-07 20:44:33 +0900

    feat(ErrorMessage): 로또 길이 관련 에러 메시지 추가

--- src/main/java/lotto/config/ErrorMessage.java
+++ src/main/java/lotto/config/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     NUMBER_RANGE_ERROR("[ERROR] 번호는 %d부터 %d 사이의 숫자여야 합니다."),
     DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
     MISSING_WINNING_NUMBERS("[ERROR] 당첨 번호를 입력해주세요."),
+    LOTTO_NUMBERS_SIZE_ERROR("[ERROR] 로또 번호는 %d개여야 합니다."),
     WINNING_NUMBERS_SIZE_ERROR("[ERROR] 당첨 번호는 %d개여야 합니다."),
     BONUS_NUMBER_DUPLICATION("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 %,d원 단위여야 합니다.");

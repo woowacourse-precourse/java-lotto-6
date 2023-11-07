9624d639f016f7e2de8639d7b5535880f6fa55b1
win9 <krrr883@gmail.com>
2023-11-07 22:47:05 +0900

    [Feat]: 사용자, 로또 도메인 추가
    사용자와 로또번호의 역할을 하는 클래스 추가

---
+++ src/main/java/domain/Lottos.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottoList = new ArrayList<>();
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+}

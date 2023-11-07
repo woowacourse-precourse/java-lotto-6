247c12b14acb45dd481cf7981d1f68521054bef4
eunjin <12191804@inha.edu>
2023-11-07 22:53:18 +0900

    style: 출력 메세지 및 주석 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,14 +4,11 @@ import java.util.List;
 
 public class Application {
     public void run() {
-        // 가정된 사용자 입력 처리
         int purchaseAmount = InputView.getPurchaseAmount();
 
-        // 로또 기계를 생성하고 로또를 구매합니다.
         LottoMachine lottoMachine = new LottoMachine();
         List<Lotto> lottos = lottoMachine.purchaseLottos(purchaseAmount);
 
-        // 구매한 로또 결과를 출력합니다.
         ResultView.printPurchasedLottos(lottos);
 
         Lotto winningLotto = null;
@@ -35,14 +32,12 @@ public class Application {
             }
         }
 
-        // 로또 결과를 계산합니다.
         LottoResult result = new LottoResult();
         for (Lotto lotto : lottos) {
             LottoRank rank = winning.checkRank(lotto);
             result.record(rank);
         }
 
-        // 결과를 출력합니다.
         ResultView.printResult(result, purchaseAmount);
     }
     public static void main(String[] args) {
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,7 +15,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 정확히 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != numbers.size()) {

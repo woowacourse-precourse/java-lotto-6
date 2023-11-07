bc128d083d3c189d28d2f5484741f88489b5b229
박준영 <wnsdud6969@naver.com>
2023-11-07 18:21:37 +0900

    feat(Ranking): add new features
    Add new features to Ranking
    - return instance of Ranking called of()
    - return whether it has same bonus number
    - return whether it has same match number count

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Ranking {
     FIRST(6, new Money(2000000000), false),
     SECOND(5, new Money(30000000), true),
@@ -8,13 +10,29 @@ public enum Ranking {
     FIFTH(3, new Money(5000), false),
     NOTHING(0, new Money(0), false);
 
-    private int sameNumberCount;
+    private int matchNumberCount;
     private Money prizeMoney;
     private boolean hasBonusNumber;
 
-    Ranking(int sameNumberCount, Money prizeMoney, boolean hasBonusNumber) {
-        this.sameNumberCount = sameNumberCount;
+    Ranking(int matchNumberCount, Money prizeMoney, boolean hasBonusNumber) {
+        this.matchNumberCount = matchNumberCount;
         this.prizeMoney = prizeMoney;
         this.hasBonusNumber = hasBonusNumber;
+    }
+
+    public static Ranking of(long sameNumberCount, boolean hasBonusNumber) {
+        return Arrays.stream(Ranking.values())
+            .filter(ranking -> ranking.isSameMatchNumberCount(sameNumberCount))
+            .filter(ranking -> ranking.isSameBonusNumber(hasBonusNumber))
+            .findFirst()
+            .orElse(NOTHING);
+    }
+
+    private boolean isSameBonusNumber(boolean hasBonusNumber) {
+        return this.hasBonusNumber == hasBonusNumber;
+    }
+
+    private boolean isSameMatchNumberCount(long matchNumberCount) {
+        return this.matchNumberCount == matchNumberCount;
     }
 }

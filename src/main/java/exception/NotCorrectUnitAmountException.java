922fea324e7f10b647ea54c4034249f8702e1806
eora21 <eora21@naver.com>
2023-11-07 15:39:55 +0900

    refactor: 💡 수정 구입 금액 예외에 Amount 적용

--- src/main/java/exception/NotCorrectUnitAmountException.java
+++ src/main/java/exception/NotCorrectUnitAmountException.java
@@ -1,10 +1,11 @@
 package exception;
 
+import domain.Amount;
 import java.text.DecimalFormat;
 
 public class NotCorrectUnitAmountException extends CustomException {
     private static final DecimalFormat decimalFormat = new DecimalFormat("###,###");
-    public NotCorrectUnitAmountException(int unitAmount) {
-        super("구입 금액은 " + decimalFormat.format(unitAmount) + "원 단위로 나누어 떨어져야 합니다.");
+    public NotCorrectUnitAmountException(Amount unitAmount) {
+        super("구입 금액은 " + decimalFormat.format(unitAmount.getAmount()) + "원 단위로 나누어 떨어져야 합니다.");
     }
 }

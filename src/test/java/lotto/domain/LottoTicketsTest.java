f50021aa2a86a8ef03816b8e77376599524bb1c9
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 19:43:13 +0900

    feat(LottoTicketsTest): 로또 갯수만큼 로또 티켓을 발행하는 기능 테스트 추가

---
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketsTest {
+    private static final int ticketAmount = 5;
+
+    @DisplayName("로또 갯수만큼 로또 티켓을 발행한다.")
+    @Test
+    void createLottoTickets() {
+        LottoTickets lottoTickets = new LottoTickets(ticketAmount);
+
+        assertEquals(lottoTickets.getLottoTickets().size(), ticketAmount);
+    }
+}

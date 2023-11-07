d677f47d75f95c86d452e55930d18f8e6e02370c
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-08 06:23:27 +0900

    feat: Lotto -> Prize 로 파싱하는 함수 추가

--- src/main/java/lotto/util/parser/LottoParser.java
+++ src/main/java/lotto/util/parser/LottoParser.java
@@ -2,6 +2,9 @@ package lotto.util.parser;
 
 import java.util.Arrays;
 import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.Prize;
+import lotto.model.WinningLotto;
 import lotto.model.number.LottoNumber;
 import lotto.model.number.LottoNumbers;
 
@@ -32,9 +35,22 @@ public class LottoParser extends Parser {
     }
 
     /**
-     * Description: Parse List&lt;Integer&gt; to Lotto
+     * Description: Parse List&lt;Integer&gt; to LottoNumbers
      */
-    public static LottoNumbers parseIntListToLotto(List<Integer> args) {
+    public static LottoNumbers parseIntListToLottoNumbers(List<Integer> args) {
         return new LottoNumbers(parseIntListToLottoNumberList(args));
+    }
+
+    /**
+     * Description: Parse Lotto to Prize
+     *
+     * @param lotto
+     * @param winningLotto
+     * @return
+     */
+    public static Prize parseLottoToPrize(Lotto lotto, WinningLotto winningLotto) {
+        int matchCount = winningLotto.getMatchCount(lotto);
+        boolean matchBonusNumber = winningLotto.isMatchBonusNumber(lotto);
+        return Prize.of(matchCount, matchBonusNumber);
     }
 }

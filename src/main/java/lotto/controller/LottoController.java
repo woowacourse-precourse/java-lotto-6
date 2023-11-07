cdda62787a6aacaaa692c8c5b433526b9f36edbc
PARK9898 <wjddjs21@naver.com>
2023-11-08 03:07:38 +0900

    "feat: 우승자 로또 get 메서드 구현"

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,36 @@
+package lotto.controller;
+
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoManager;
+import lotto.domain.LottoShop;
+import lotto.domain.LottosResult;
+import lotto.domain.PurchaseAmount;
+import lotto.domain.Rank;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void play() {
+
+    }
+    private List<Integer> getWinningLottoNumbers() {
+        try {
+            List<Integer> winningNumbers = inputView.inputWinningNumbers();
+            Lotto winningLotto = new Lotto(winningNumbers);
+            return winningNumbers;
+        } catch (IllegalArgumentException exception) {
+            outputView.printError(exception.getMessage());
+            return getWinningLottoNumbers();
+        }
+    }
+}

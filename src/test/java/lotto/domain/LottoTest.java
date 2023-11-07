6b7534def88972c863a9a720f76657d74915662a
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 22:21:18 +0900

    feat(LottoTest): 로또의 당첨 개수를 반환하는 기능 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
+    private static final List<Integer> WINNING_NUMBER_INPUT = List.of(1, 7, 13, 17, 28, 34);
+    private static final List<Integer> LOTTO_INPUT = List.of(1, 7, 19, 23, 38, 44);
+    private static final int WINNING_COUNT = 2;
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -25,7 +30,16 @@ class LottoTest {
     @DisplayName("로또 번호가 1~45사이의 값이 아니면 예외가 발생한다.")
     @Test
     void createLottoByOutOfRangeNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1,13,23,32,38,49)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 13, 23, 32, 38, 49)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또의 당첨 개수를 반환한다.")
+    @Test
+    void calculateLotto() {
+        Lotto winningNumber = new Lotto(WINNING_NUMBER_INPUT);
+        Lotto lotto = new Lotto(LOTTO_INPUT);
+
+        assertEquals(lotto.countMatchingLottoNumbers(winningNumber), WINNING_COUNT);
     }
 }

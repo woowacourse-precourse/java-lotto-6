315a43a52ae7aa853d13338fa9d5f1adcd6d21b2
dyeongkim <dyeongkim98@gmail.com>
2023-11-07 14:37:55 +0900

    Refactor(Game): Modify numbers를 Lotto 클래스로 관리
    Lotto 클래스를 통해 로또번호 관리

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -4,16 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Game {
-    private List<List<Integer>> numberTickets;
-    private List<Integer> numbers;
+    private List<Lotto> numberTickets;
     public void makeNumberTicket(Integer totalTicketNumber){
         while (totalTicketNumber > numberTickets.size()){
             makeNumber();
             numberTickets.add(makeNumber());
         }
     }
-    private List<Integer> makeNumber(){
-        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
+    private Lotto makeNumber(){
+        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        return lotto;
     }
 }

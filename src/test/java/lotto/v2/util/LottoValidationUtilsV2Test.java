abf1cd0beeaf9ef0c2a8fdb39ca42cca0ecacda6
권장순 <jsoonworld@gmail.com>
2023-11-07 13:55:28 +0900

    test( LottoValidationUtils#should_NotThrowException_When_BonusNumberIsValid() ) : 보너스 번호가 유효하고 당첨 번호와 중복되지 않을 때 예외를 던지지 않는다.

--- src/test/java/lotto/v2/util/LottoValidationUtilsV2Test.java
+++ src/test/java/lotto/v2/util/LottoValidationUtilsV2Test.java
@@ -14,7 +14,7 @@ class LottoValidationUtilsV2Test {
     @DisplayName("로또 번호가 6개가 아니면 예외를 던진다")
     void should_ThrowException_When_LottoNumbersSizeIsNotSix() {
         // given
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5); 
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
         // when & then
         assertThatThrownBy(() -> LottoValidationUtilsV2.checkValidLottoNumbers(numbers))
@@ -68,6 +68,17 @@ class LottoValidationUtilsV2Test {
         assertThatThrownBy(() -> LottoValidationUtilsV2.checkValidBonusNumber(bonusNumber, winningNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 유효하고 당첨 번호와 중복되지 않을 때 예외를 던지지 않는다")
+    void should_NotThrowException_When_BonusNumberIsValid() {
+        // given
+        int bonusNumber = 7; 
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // when & then
+        assertThatNoException().isThrownBy(() -> LottoValidationUtilsV2.checkValidBonusNumber(bonusNumber, winningNumbers));
     }
 
 }

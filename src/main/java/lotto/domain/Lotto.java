e7fae38c18ace351d194baf73ee0de11fc054c09
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 13:00:20 +0900

    feat(domain): Lotto 도메인 클래스 기능 및 검증 함수 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,61 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.constant.Config.*;
+import static lotto.constant.ErrorMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateCount(numbers);
+        validateNotDuplicated(numbers);
+        validateInRange(numbers);
+        validateAscending(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public static Lotto create() {
+        return new Lotto(
+                pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_COUNT)
+                        .stream()
+                        .sorted()
+                        .toList()
+        );
+    }
+
+    private void validateCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_COUNT) {
+            throw new IllegalArgumentException(LOTTO_COUNT_NOT_MATCHED);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNotDuplicated(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() < numbers.size()) {
+            throw new IllegalArgumentException(LOTTO_DUPLICATED);
+        }
+    }
+
+    private void validateInRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX)) {
+            throw new IllegalArgumentException(LOTTO_NOT_IN_RANGE);
+        }
+    }
+
+    private void validateAscending(List<Integer> numbers) {
+        if (IntStream.range(1, numbers.size())
+                .map(index -> numbers.get(index - 1).compareTo(numbers.get(index)))
+                .anyMatch(order -> order > 0)) {
+            throw new IllegalArgumentException(LOTTO_NOT_ASCENDING);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

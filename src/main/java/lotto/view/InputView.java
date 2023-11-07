b77b42318ce6fb6f10b60bab8e95ef443592ae05
whereami0404 <whereami2048@gmail.com>
2023-11-07 22:42:05 +0900

    fix: 당첨 번호 입력 기능 반환 타입 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,15 +6,16 @@ public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_SPLITTER = ",";
 
     public String inputPurchaseAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT);
         return readLine();
     }
 
-    public String inputWinningNumbers() {
+    public String[] inputWinningNumbers() {
         System.out.println(ASK_WINNING_NUMBERS);
-        return readLine();
+        return readLine().split(INPUT_SPLITTER);
     }
 
     public String inputBonusNumber() {

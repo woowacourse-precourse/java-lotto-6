8bb267920946e4d6d9f79b3c56a25022f9a83cff
Identityless <rlandwnd555@naver.com>
2023-11-07 20:28:34 +0900

    feat : Discriminator 클래스 추가

---
+++ src/main/java/lotto/Discriminator.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.List;
+
+public class Discriminator {
+    private List<Integer> correctNumbers;
+
+    public Discriminator(List<Integer> correctNumbers) {
+        this.correctNumbers = correctNumbers;
+    }
+}

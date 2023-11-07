d287e44129c2d6e9b37e2ab68133a1cf2e0cb291
mingtorr <mingtorr@nave.com>
2023-11-07 23:45:41 +0900

    feat: 오류 메시지 출력 기능 추가
    오류 메시지 출력 메소드 view 레이어에 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,11 @@ import lotto.constants.MsgConstants;
 
 public class OutputView {
 
+    public static void printErrorMessage(String err) {
+        System.out.println(err);
+    }
+
+
     public static void printEnterPurchaseAmount() {
         System.out.println(MsgConstants.PLEASE_ENTER_PURCHASE_AMOUNT.getData());
     }

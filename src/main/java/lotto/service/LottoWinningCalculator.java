04ace6a0af1cc4f01ec869d834cde4630aaa0d0a
ohjuyoungg <ejrm12@naver.com>
2023-11-08 04:13:07 +0900

    feat: 당첨통계 1차 구현 수정 후 2차 구현

--- src/main/java/lotto/service/LottoWinningCalculator.java
+++ src/main/java/lotto/service/LottoWinningCalculator.java
@@ -1,44 +1,56 @@
 package lotto.service;
 
-import static lotto.service.LottoService.countMatchingNumbers;
-
-import java.util.ArrayList;
+import java.text.DecimalFormat;
 import java.util.List;
 import lotto.domain.Lotto;
-import lotto.utils.constants.LottoWinning;
+import lotto.utils.constants.Comment;
 
 public class LottoWinningCalculator {
+    public static void calculatorLotto(List<Lotto> lottoNumbers, Lotto winningNumbers, int bonusNumber, int purchaseAmount) {
+        int countFifthPrize = 0;
+        int countFourthPrize = 0;
+        int countThirdPrize = 0;
+        int countSecondPrize = 0;
+        int countFirstPrize = 0;
 
-    public static List<Integer> lottoCalculator(List<Integer> winningNumbers, List<Lotto> purchasedLottos) {
-        List<Integer> counts = new ArrayList<>();
-        for (Lotto purchasedLotto : purchasedLottos) {
-            int matchingNumbers = countMatchingNumbers(purchasedLotto.getNumbers(), winningNumbers);
-            counts.add(matchingNumbers);
-        }
-        return counts;
-    }
+        for (int index = 0; index < purchaseAmount; index++) {
+            int matchingNumbers = calculateMatchingNumbers(lottoNumbers.get(index), winningNumbers);
+            boolean hasBonus = lottoNumbers.get(index).getNumbers().contains(bonusNumber);
 
-    public static void printTotalProfit(List<Integer> lottoWinningCounts, int purchaseAmount) {
-        int totalPrize = 0;
-
-        for (int i = 0; i < 6; i++) {
-            int finalI = i;
-            long matchingNumbers = lottoWinningCounts.stream().filter(count -> count == finalI).count();
-            if (matchingNumbers > 0) {
-                LottoWinning winning = LottoWinning.findByMatchValues(i);
-                String prize;
-                if (winning.getRank() == 2 && i == 5) {
-                    prize = "5개 일치, 보너스 볼 일치";
-                } else {
-                    prize = i + "개 일치";
-                }
-                System.out.println(
-                    prize + " (" + LottoWinning.formatCurrency(winning.getReward()) + "원) - " + matchingNumbers + "개");
-                totalPrize += winning.getReward() * (int) matchingNumbers;
+            if (matchingNumbers == 3) {
+                countFifthPrize++;
+            } else if (matchingNumbers == 4) {
+                countFourthPrize++;
+            } else if (matchingNumbers == 5 && hasBonus) {
+                countSecondPrize++;
+            } else if (matchingNumbers == 5) {
+                countThirdPrize++;
+            } else if (matchingNumbers == 6) {
+                countFirstPrize++;
             }
         }
 
-        double profitRate = ((double) (totalPrize - purchaseAmount) / purchaseAmount) * 100;
-        System.out.println("총 수익률은 " + String.format("%.1f", profitRate) + "%입니다.");
+        DecimalFormat format = new DecimalFormat("0.0");
+        double profitRate = ((calculateTotalPrize(countFifthPrize, countFourthPrize, countThirdPrize, countSecondPrize, countFirstPrize) - purchaseAmount) / purchaseAmount) * 100;
+
+        System.out.println(Comment.OUTPUT_FIFTH_PRIZE.getComment() + countFifthPrize + Comment.OUTPUT_COUNT.getComment());
+        System.out.println(Comment.OUTPUT_FOURTH_PRIZE.getComment() + countFourthPrize + Comment.OUTPUT_COUNT.getComment());
+        System.out.println(Comment.OUTPUT_THIRD_PRIZE.getComment() + countThirdPrize + Comment.OUTPUT_COUNT.getComment());
+        System.out.println(Comment.OUTPUT_SECOND_PRIZE.getComment() + countSecondPrize + Comment.OUTPUT_COUNT.getComment());
+        System.out.println(Comment.OUTPUT_FIRST_PRIZE.getComment() + countFirstPrize + Comment.OUTPUT_COUNT.getComment());
+        System.out.println(Comment.OUTPUT_TOTAL_RETURN.getComment() + format.format(profitRate) + "%입니다.");
+    }
+
+    private static int calculateMatchingNumbers(Lotto lotto, Lotto winningNumbers) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        List<Integer> winningNums = winningNumbers.getNumbers();
+
+        return (int) lottoNumbers.stream()
+            .filter(winningNums::contains)
+            .count();
+    }
+
+    private static double calculateTotalPrize(int countFifthPrize, int countFourthPrize, int countThirdPrize, int countSecondPrize, int countFirstPrize) {
+        return (countFifthPrize * 5000) + (countFourthPrize * 50000) + (countThirdPrize * 1500000) + (countSecondPrize * 30000000) + (countFirstPrize * 2000000000);
     }
 }

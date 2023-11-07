6ba62e37f7d627c55eb91d139db8d89c6340c9b8
acs0209 <annjae4676@naver.com>
2023-11-08 00:35:43 +0900

    feat: LottoController에서 컨트롤러 기능을 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,10 @@
 package lotto;
 
-import lotto.controller.LottoController;
+import lotto.controller.LottoMainController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.play();
+        LottoMainController lottoMainController = new LottoMainController();
+        lottoMainController.play();
     }
 }

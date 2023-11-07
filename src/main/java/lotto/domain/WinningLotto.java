63f63e5bb1ce7b2333822a57aff084ed74f5541d
Minsu Choi <cms102510@naver.com>
2023-11-08 01:33:56 +0900

    feat: 당첨 로또 번호를 나타내는 클래스 작성

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+
+// 당첨 로또 번호를 나타내는 클래스
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

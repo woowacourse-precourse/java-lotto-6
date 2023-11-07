458e7247c7eca0c4187abc43a904fea516539855
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 01:33:29 +0900

    feat: WinningCondition에 NOTHING enum 추가

--- src/main/java/lotto/constant/WinningCondition.java
+++ src/main/java/lotto/constant/WinningCondition.java
@@ -5,7 +5,8 @@ public enum WinningCondition {
     FIVE_MATCH_WITH_BONUS(PRIZE.FIVE_MATCH_WITH_BONUS, DESCRIPTION.FIVE_MATCH_WITH_BONUS),
     FIVE_MATCH(PRIZE.FIVE_MATCH, DESCRIPTION.FIVE_MATCH),
     FOUR_MATCH(PRIZE.FOUR_MATCH, DESCRIPTION.FOUR_MATCH),
-    THREE_MATCH(PRIZE.THREE_MATCH, DESCRIPTION.THREE_MATCH);
+    THREE_MATCH(PRIZE.THREE_MATCH, DESCRIPTION.THREE_MATCH),
+    NOTHING(PRIZE.NOTHING, DESCRIPTION.NOTHING);
 
     public static class PRIZE {
         public static final int SIX_MATCH = 20000000;
@@ -13,6 +14,7 @@ public enum WinningCondition {
         public static final int FIVE_MATCH = 1500000;
         public static final int FOUR_MATCH = 50000;
         public static final int THREE_MATCH = 5000;
+        public static final int NOTHING = 0;
     }
 
     public static class DESCRIPTION {
@@ -21,10 +23,11 @@ public enum WinningCondition {
         public static final String FIVE_MATCH = "5개 일치 (1,500,000원)";
         public static final String FOUR_MATCH = "4개 일치 (50,000원)";
         public static final String THREE_MATCH = "3개 일치 (5,000원)";
+        public static final String NOTHING = "0개 일치 (0원)";
     }
 
-    private int prize;
-    private String description;
+    private final int prize;
+    private final String description;
 
     WinningCondition(int prize, String description) {
         this.prize = prize;

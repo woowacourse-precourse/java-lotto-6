207901f40e301bf843679bcf3177a9faf85498e0
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 03:06:17 +0900

    feat: 당첨번호 중복 유효성을 판단하는 vaildWinnerNumberDuplication 함수 생성

--- src/main/java/validator/WinnerNumberGeneratorValidator.java
+++ src/main/java/validator/WinnerNumberGeneratorValidator.java
@@ -1,9 +1,11 @@
 package validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinnerNumberGeneratorValidator {
-    void vaildRandomNumberRange(List<Integer> winnerNumbers) {
+    void vaildWinnerNumberRange(List<Integer> winnerNumbers) {
         for (int winnerNumber : winnerNumbers) {
             if (winnerNumber < 1) {
                 throw new IllegalArgumentException("[ERROR] 당첨 번호 생성 시 최소값(1) 보다 작은 값이 있습니다.");
@@ -14,9 +16,16 @@ public class WinnerNumberGeneratorValidator {
         }
     }
 
-    void vaildRandomNumberSize(List<Integer> randomNumbers) {
+    void vaildWinnerNumberSize(List<Integer> randomNumbers) {
         if (randomNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 난수가 6개로 생성되지 않았습니다.");
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 6개로 생성되지 않았습니다.");
+        }
+    }
+
+    void vaildWinnerNumberDuplication(List<Integer> randomNumbers) {
+        Set<Integer> uniqueNumber = new HashSet<>(randomNumbers);
+        if (uniqueNumber.size() != randomNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호 중 중복된 값이 있습니다.");
         }
     }
 }

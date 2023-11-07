bddedb01f7e251cfa05da2d3d81a560b19cfc8e8
sudhdkso <waga1009@gmail.com>
2023-11-08 03:03:56 +0900

    refactor : 오타 수정

--- src/main/java/lotto/domain/enums/Rank.java
+++ src/main/java/lotto/domain/enums/Rank.java
@@ -12,7 +12,7 @@ public enum Rank {
     SECOND(5, 30_000_000, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     THIRD(5, 15_000_000, false, "5개 일치 (1,500,000원) - "),
     FOURTH(4, 50_000, false, "4개 일치 (50,000원) - "),
-    FIVETH(3, 5_000, false, "3개 일치 (5,000원) - "),
+    FIFTH(3, 5_000, false, "3개 일치 (5,000원) - "),
     EMPTY(0, 0, false, "낙첨");
 
     private final int matchCount;

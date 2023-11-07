20628a1f2d7d95f53c40649d038acb9372c221fa
Heojamong <wlaud0823@naver.com>
2023-11-07 18:56:43 +0900

    feat: 입력 받는 로또의 번호가 1 ~ 45 사이의 값인지 확인하는 메소드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,12 +10,21 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         duplicationValidate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 번호는 1부터 45 사이여야 합니다.");
+            }
         }
     }
 

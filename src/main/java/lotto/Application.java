cd2ae62c75605b775be3f1a8dc933dc50cafdfd1
gahee99 <u1879@sookmyung.ac.kr>
2023-11-08 07:43:31 +0900

    fix(Application): 로직 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,11 @@
 package lotto;
 
 import lotto.controller.LottoGameController;
+import lotto.util.LottoNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        LottoGameController game = new LottoGameController();
+        LottoGameController game = new LottoGameController(new LottoNumberGenerator());
         game.startGame();
     }
 }

1e81178ab8bb23218e3fc14cc07e0aa77ad2aa87
whereami0404 <whereami2048@gmail.com>
2023-11-08 04:16:21 +0900

    refactor: 입력 로직 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,64 +1,75 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import lotto.controller.LottoGameController;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotties;
-import lotto.domain.Lotto;
 import lotto.domain.PurchaseAmount;
 import lotto.domain.WinningNumbers;
+import lotto.exception.ModelExceptionConstant;
 import lotto.service.LottoGameService;
 import lotto.util.LottoGameUtil;
-import lotto.util.RandomNumberGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
-    static InputView inputView = new InputView();
+    private final static InputView inputView = new InputView();
+    private final static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        String input = inputView.inputPurchaseAmount();
-        PurchaseAmount purchaseAmount = convertPurchaseAmount(input);
 
-        Lotties lotties = setLotties(purchaseAmount.getLottoCount());
+        PurchaseAmount purchaseAmount = setPurchaseAmount();
 
-        String[] inputNumbers = inputView.inputWinningNumbers();
-        WinningNumbers winningNumbers = convertWinningNumbers(inputNumbers);
+        Lotties lotties = LottoGameUtil.setLotties(purchaseAmount.getLottoCount());
+        outputView.printLotties(lotties.getLottiesStatus(), purchaseAmount.getLottoCount());
 
-        input = inputView.inputBonusNumber();
-        BonusNumber bonusNumber = convertBonusNumber(input);
+        WinningNumbers winningNumbers = setWinningNumbers();
+
+        BonusNumber bonusNumber = setBonusNumber();
 
         LottoGameController controller = new LottoGameController(new LottoGameService());
         controller.run(purchaseAmount, winningNumbers, bonusNumber, lotties);
     }
 
-    private static PurchaseAmount convertPurchaseAmount(String input) {
-        int number = LottoGameUtil.StringToInt(input);
+    private static PurchaseAmount setPurchaseAmount() {
+        while (true) {
+            try {
+                String input = inputView.inputPurchaseAmount();
 
-        return new PurchaseAmount(number);
-    }
+                int number = LottoGameUtil.StringToInt(input);
 
-    private static BonusNumber convertBonusNumber(String input) {
-        int number = LottoGameUtil.StringToInt(input);
-
-        return new BonusNumber(number);
-    }
-
-    private static WinningNumbers convertWinningNumbers(String[] inputNumbers) {
-        List<Integer> numbers = LottoGameUtil.StringArrayToList(inputNumbers);
-
-        return new WinningNumbers(numbers);
-    }
-
-    private static Lotties setLotties(int count) {
-        List<Lotto> lottoSaver = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            List<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumbers();
-            Collections.sort(randomNumbers);
-            lottoSaver.add(new Lotto(randomNumbers));
+                return new PurchaseAmount(number);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ModelExceptionConstant.NOT_DIVIDE_BY_LOTTO_PRICE.getText());
+            }
         }
+    }
 
-        return new Lotties(lottoSaver);
+    private static WinningNumbers setWinningNumbers() {
+        while (true) {
+            try {
+                String[] inputArray = inputView.inputWinningNumbers();
+
+                List<Integer> numbers = LottoGameUtil.stringArrayToList(inputArray);
+
+                return new WinningNumbers(numbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ModelExceptionConstant.OUT_OF_WINNING_NUMBERS_SIZE.getText());
+            }
+        }
+    }
+
+    private static BonusNumber setBonusNumber() {
+        while (true) {
+            try {
+                String input = inputView.inputBonusNumber();
+
+                int number = LottoGameUtil.StringToInt(input);
+
+                return new BonusNumber(number);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ModelExceptionConstant.OUT_OF_LOTTO_NUMBER_RANGE.getText());
+            }
+        }
     }
 }
--- src/main/java/lotto/util/LottoGameUtil.java
+++ src/main/java/lotto/util/LottoGameUtil.java
@@ -1,15 +1,30 @@
 package lotto.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import lotto.domain.Lotties;
+import lotto.domain.Lotto;
 
 public class LottoGameUtil {
     public static int StringToInt(String number) {
         return Integer.parseInt(number);
     }
 
-    public static List<Integer> StringArrayToList(String[] numbers) {
+    public static List<Integer> stringArrayToList(String[] numbers) {
         return Arrays.stream(numbers).mapToInt(Integer::parseInt).boxed().toList();
     }
+    
+    public static Lotties setLotties(int count) {
+        List<Lotto> lottoSaver = new ArrayList<>();
 
+        for (int i = 0; i < count; i++) {
+            List<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumbers();
+            Collections.sort(randomNumbers);
+            lottoSaver.add(new Lotto(randomNumbers));
+        }
+
+        return new Lotties(lottoSaver);
+    }
 }

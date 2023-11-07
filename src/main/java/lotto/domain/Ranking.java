74e434408a0c37cd6c3b61e612882dce4e2f1e52
KimSehoon <shggm2000@gmail.com>
2023-11-07 15:36:37 +0900

    feat: findByMatchCount method 구현
    - 일치 횟수에 따른 Ranking 객체를 반환하는 findByMatchCount method 구현

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,10 +1,15 @@
 package lotto.domain;
 
+import static java.util.Arrays.stream;
+
+import java.util.Arrays;
+import java.util.Stack;
+
 public enum Ranking {
     THREE_MATCHES(3, "3개 일치 (5,000원) - ", 5_000),
     FOUR_MATCHES(4, "4개 일치 (50,000원) - ", 50_000),
     FIVE_MATCHES(5, "5개 일치 (1,500,000원) - ", 1_500_000),
-    FIVE_MATCHES_BOUNS_MATCHES(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30_000_000),
+    FIVE_MATCHES_BONUS_MATCHES(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30_000_000),
     SIX_MATCHES(6, "6개 일치 (2,000,000,000원) - ", 2_000_000_000);
 
     private final int matchCount;
@@ -27,5 +32,12 @@ public enum Ranking {
 
     public int getWinningAmount() {
         return winningAmount;
+    }
+
+    public static Ranking findByMatchCount(int matchCount) {
+        return Arrays.stream(values())
+                .filter(ranking -> ranking.getMatchCount() == matchCount)
+                .findAny()
+                .orElseThrow(IllegalStateException::new);
     }
 }

32dce0bc1babfa66c1a5687cc7c77b2c29d8b8e1
의찬 <srja432130@gmail.com>
2023-11-07 19:07:37 +0900

    feat: 당첨 번호들의 범위를 확인하는 메소드를 추가

--- src/main/java/lotto/view/inputWinningLottoView.java
+++ src/main/java/lotto/view/inputWinningLottoView.java
@@ -2,10 +2,12 @@ package lotto.view;
 
 import lotto.exception.InvalidLottoInputFormatException;
 
+import java.util.List;
 import java.util.regex.Pattern;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 public class inputWinningLottoView extends inputView {
-    private static final Pattern PATTERN = Pattern.compile("(\\d{1,2}[,]){5}\\d{1,2}");
+    private static final Pattern PATTERN = Pattern.compile("(\\d{1,2},){5}\\d{1,2}");
 
     public String getValue() {
         String result = inputValue();
@@ -15,11 +17,19 @@ public class inputWinningLottoView extends inputView {
 
     private void validate(String inputValue) {
         validateInput(inputValue);
+        validateRangeNumbers(inputValue);
     }
 
     private void validateInput(String inputValue) {
         if (!PATTERN.matcher(inputValue).matches()) {
             throw new InvalidLottoInputFormatException();
         }
+    }
+
+    private void validateRangeNumbers(String inputValue) {
+        List<Integer> numbers = Stream.of(inputValue.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        numbers.forEach(this::validateRange);
     }
 }

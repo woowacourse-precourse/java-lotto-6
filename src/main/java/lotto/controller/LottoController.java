f966b34afab5e171c7b0c7ebfebf23281f51f333
MingyeomKim <mingyum119@naver.com>
2023-11-07 13:40:20 +0900

    feat(LottoController): 당첨 번호와 보너스 번호를 통해 dto를 생성
    뷰를 호출하여 당첨 번호와 보너스 번호를 입력받고, dto의 from 메서드로 dto를 생성한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,26 @@
 package lotto.controller;
 
-import lotto.model.Lottos;
+import static lotto.view.View.requestBonusNumber;
+import static lotto.view.View.requestWinningNumbers;
+
+import lotto.domain.Lottos;
+import lotto.dto.DrawnNumbersDto;
 import lotto.view.View;
 
 public class LottoController {
     public void run() {
         issue();
+        draw();
     }
 
     private void issue() {
         int cost = Integer.parseInt(View.requestCost());
         Lottos lottos = Lottos.from(getCountFrom(cost));
         View.printLottos(lottos);
+    }
+
+    private void draw() {
+        DrawnNumbersDto dto = DrawnNumbersDto.of(requestWinningNumbers(), requestBonusNumber());
     }
 
     private int getCountFrom(int cost) {

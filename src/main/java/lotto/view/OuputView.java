5c0c8633d64132a4514c0209ddd919df34007d6d
goodchoi <jangu3384@gmail.com>
2023-11-08 04:23:49 +0900

    feat: 당첨 결과 출력 기능 구현

--- src/main/java/lotto/view/OuputView.java
+++ src/main/java/lotto/view/OuputView.java
@@ -1,15 +1,21 @@
 package lotto.view;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.List;
+import lotto.model.LottoPlace;
+import lotto.model.LottoStatus;
+import lotto.model.MarginRate;
+import lotto.model.Money;
+import lotto.model.WinningResult;
 import lotto.model.dto.LottoPaper;
+import lotto.model.dto.LottoResult;
 
 public class OuputView {
-    private static final String ERROR_HEAD = "[ERROR]";
-    private static final String LEFT_BOUND = "[";
-    private static final String RIGHT_BOUND = "]";
-    private static final String SEPERATOR = ",";
+    private static final String COMMA = ",";
     private static final String WHITE_SPACE = " ";
-    private static final String COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String COUNT = "개";
 
     public void printErrorMessage(String errorMessage) {
         String formattedErrorMessage = formatErrorMessage(errorMessage);
@@ -23,31 +29,82 @@ public class OuputView {
     }
 
     private void printLottoCount(int lottoSize) {
-        System.out.println(lottoSize + COUNT_MESSAGE);
+        System.out.println(lottoSize + COUNT + "를 구매했습니다.");
     }
 
     private void printLottoNumbers(List<LottoPaper> lottoPapers) {
         for (LottoPaper lottoPaper : lottoPapers) {
             System.out.println(foramtLottoNumbers(lottoPaper));
         }
+        printEmptyLine();
     }
 
     private String foramtLottoNumbers(LottoPaper lottoPaper) {
-        return LEFT_BOUND
-                + concatNumbers(lottoPaper)
-                + RIGHT_BOUND;
+        return "[" + concatNumbers(lottoPaper) + "]";
     }
 
     private String concatNumbers(LottoPaper lottoPaper) {
         return lottoPaper.getNumbers()
                 .stream()
                 .map(String::valueOf)
-                .reduce((e1, e2) -> e1 + SEPERATOR + WHITE_SPACE + e2)
+                .reduce((e1, e2) -> e1 + COMMA + WHITE_SPACE + e2)
                 .orElseThrow(() -> new IllegalStateException("로또 용지가 없습니다."));
     }
 
 
     private String formatErrorMessage(String errorMessage) {
-        return ERROR_HEAD + WHITE_SPACE + errorMessage;
+        return "[ERROR] " + errorMessage;
+    }
+
+    public void printLottoResult(LottoResult lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        printWinningResult(lottoResult.getWinngResult());
+        printMarginRate(lottoResult.getMarginRate());
+    }
+
+    private void printWinningResult(WinningResult winnigResult) {
+        Arrays.stream(LottoPlace.values())
+                .forEach(lottoPlace -> {
+                    Integer count = winnigResult.getCount(lottoPlace);
+                    printWinningResultDetail(lottoPlace, count);
+                });
+    }
+
+    private void printWinningResultDetail(LottoPlace lottoPlace, Integer count) {
+        LottoStatus lottoStatus = lottoPlace.getLottoStatus();
+        Money prize = lottoPlace.getPrizeAmount();
+
+        String detail = lottoStatus.getCorrectNumber()
+                + "개 일치"
+                + ifBounusRequired(lottoPlace.isBonusRequired())
+                + " ("
+                + formatDecimal(prize.getAmount())
+                + "원)"
+                + " - "
+                + count
+                + "개";
+
+        System.out.println(detail);
+    }
+
+    private String ifBounusRequired(boolean isBonusRequired) {
+        if (isBonusRequired) {
+            return ", 보너스 볼 일치";
+        }
+        return "";
+    }
+
+    private String formatDecimal(BigDecimal amount) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(amount);
+    }
+
+    private void printMarginRate(MarginRate marginRate) {
+        System.out.println("총 수익률은 " + marginRate.getMarginRate() + "%입니다.");
+    }
+
+    private void printEmptyLine() {
+        System.out.println();
     }
 }

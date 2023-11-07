7e05dadc243b435d5dd8947b574b47da353792cb
star-candy <el0710@naver.com>
2023-11-07 21:38:04 +0900

    refactor(lotto): 매직 넘버 상수화

--- src/main/java/model/Transform.java
+++ src/main/java/model/Transform.java
@@ -1,12 +1,17 @@
 package model;
 
+import static constants.ConstantNumbersManager.LOTTO_NUMBER_SIZE;
+import static constants.ConstantNumbersManager.MAXIMUM_LOTTO_NUMBER;
+import static constants.ConstantNumbersManager.MINIMUM_LOTTO_NUMBER;
+
 import camp.nextstep.edu.missionutils.Randoms;
+import constants.ConstantStringManager;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Transform {
     public List<Integer> winningNumberToList(String input) {
-        String[] parts = input.split(",");
+        String[] parts = input.split(ConstantStringManager.SPLIT_TARGET.getMessage());
         List<Integer> numbers = new ArrayList<>();
 
         for (String part : parts) {
@@ -20,7 +25,8 @@ public class Transform {
         List<Integer> randoms;
         while (true) {
             try {
-                randoms = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+                randoms = Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER,
+                        LOTTO_NUMBER_SIZE);
                 break;
             } catch (IllegalArgumentException e) {
                 // 유효한 로또 번호를 생성할 수 없는 경우 다시 시도

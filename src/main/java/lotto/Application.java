61c8340c64fb6724cc6142a506dd8b6010178f76
박정현 <ros28081@naver.com>
2023-11-07 16:42:00 +0900

    feat(Application): 구매 갯수에 따른 로또 생성 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static final int LOTTO_PRICE = 1_000;
@@ -8,7 +11,11 @@ public class Application {
     private static int purchasedPrice;
 
     public static void main(String[] args) {
+
         readPurchasedPrice();
+        List<Lotto> lottos = createLottos(purchasedPrice);
+        printLottoNumbers(lottos);
+
 
     }
 
@@ -19,5 +26,22 @@ public class Application {
 
     public static int getPurchasedPrice() {
         return purchasedPrice;
+    }
+
+    private static List<Lotto> createLottos(int purchasedPrice) {
+        int lottoCount = purchasedPrice / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_SIZE);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    private static void printLottoNumbers(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

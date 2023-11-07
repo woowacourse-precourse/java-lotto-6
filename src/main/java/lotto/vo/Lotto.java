8bcdd0df59b1d8db4d6c6ee2f910aca2204270d9
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-08 07:53:12 +0900

    feat(lotto): 로또 중복 검사 구현

--- src/main/java/lotto/vo/Lotto.java
+++ src/main/java/lotto/vo/Lotto.java
@@ -2,11 +2,14 @@ package lotto.vo;
 
 import java.util.List;
 
+import static lotto.values.ExceptionMessage.DUPLICATE_NUMBER;
+
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -17,6 +20,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    private void checkDuplication(List<Integer> numbers){
+        for(int i=0; i<numbers.size(); i++){
+            for(int j=i+1; j< numbers.size(); j++){
+                if(numbers.get(i) == numbers.get(j)) {
+                    throw new IllegalArgumentException(DUPLICATE_NUMBER.getMessage());
+                }
+            }
+        }
+    }
     public List<Integer> getNumbers() {
         return numbers;
     }

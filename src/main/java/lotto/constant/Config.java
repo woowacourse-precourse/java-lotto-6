a7d62a724981c1fa11b77fc96dc448d1dd429804
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 23:43:42 +0900

    feat(const): 당첨 금액 상수 추가

--- src/main/java/lotto/constant/Config.java
+++ src/main/java/lotto/constant/Config.java
@@ -6,5 +6,11 @@ public class Config {
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
 
+    public static final long REWARD_FIFTH = 5000;
+    public static final long REWARD_FOURTH = 50000;
+    public static final long REWARD_THIRD = 1500000;
+    public static final long REWARD_SECOND = 30000000;
+    public static final long REWARD_FIRST = 2000000000;
+
     public static final String LOTTO_DELIMITER = ",";
 }

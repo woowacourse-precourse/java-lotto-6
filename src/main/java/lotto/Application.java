e45c55904876a97da1cfed6f35c8965d43e6bbf8
MyStraw <kikongdosa@gmail.com>
2023-11-08 07:51:25 +0900

    Refactor: 수정 로또 당첨번호와 일치 판별 계수정렬
    Enum 클래스를 만들기 전이라 else if로 판별을 분리했다. 계수정렬로 배열을 만들어 각 해당개수 판별에 대입했다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -25,18 +25,20 @@ public class Application {
         System.out.println("\n" + "당첨 번호를 입력해 주세요.");
         String[] winNumber_String = Console.readLine().trim().split(",");
         List<Integer> winNumbers = new ArrayList<>();
-        for (String numberStr : winNumber_String){
+        for (String numberStr : winNumber_String) {
             winNumbers.add(Integer.parseInt(numberStr.trim()));
         }
 
         System.out.println("\n" + "보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine().trim());
 
+        int[] matchingCounts = new int[5];
 
-        for (Lotto lotto : lottoPapers){
+        for (Lotto lotto : lottoPapers) {
             int matchingCount = 0;
             boolean hasBonusNumber = false;
-            for (Integer number : winNumbers){
+
+            for (Integer number : winNumbers) {
                 if (lotto.getNumbers().contains(number)) {
                     matchingCount++;
                 }
@@ -46,26 +48,22 @@ public class Application {
                 hasBonusNumber = true;
             }
 
-
-            if (matchingCount == 3){
-                System.out.println("3개 일치 (5,000원) - " +matchingCount +"개");
-            }
-            if (matchingCount == 4){
-                System.out.println("4개 일치 (50,000원) - " +matchingCount +"개");
-            }
-            if (matchingCount == 5){
-                System.out.println("5개 일치 (1,500,000원) - " +matchingCount +"개");
-            }
-
-            if (matchingCount == 5 && hasBonusNumber){
-                System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " +matchingCount +"개");
-            }
-
-            if (matchingCount == 6){
-                System.out.println("6개 일치 (2,000,000,000원) - " +matchingCount +"개");
+            if (matchingCount == 6) {
+                matchingCounts[4]++;
+            } else if (matchingCount == 5 && hasBonusNumber) {
+                matchingCounts[3]++;
+            } else if (matchingCount >= 3) {
+                matchingCounts[matchingCount - 3]++;
             }
         }
+
+        System.out.println("3개 일치 (5,000원) - " + matchingCounts[0] + "개");
+        System.out.println("4개 일치 (50,000원) - " + matchingCounts[1] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + matchingCounts[2] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matchingCounts[3] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + matchingCounts[4] + "개");
     }
+
 
     private static List<Lotto> generateLottoPaper(int howManyLotto) {
         List<Lotto> lottoPapers = new ArrayList<>();

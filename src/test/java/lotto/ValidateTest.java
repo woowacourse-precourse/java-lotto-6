9debb8f53e755b0ec3564d7ccca222b0b5f4dad0
AJN <buzz781@gmail.com>
2023-11-07 11:31:50 +0900

    fix : 테스트코드 메소드 참조 버그 수정

--- src/test/java/lotto/ValidateTest.java
+++ src/test/java/lotto/ValidateTest.java
@@ -14,31 +14,31 @@ class ValidateTest {
 	@Test
     @DisplayName("입력 형식 검증")
     void validateFormatTest() {
-        assertDoesNotThrow(() -> Validate.validateFormat("1,2,3,4,5,6"));
-        assertThrows(IllegalArgumentException.class, () -> Validate.validateFormat("1, 2, 3, a, 5"));
+        assertDoesNotThrow(() -> Validate.checkFormat("1,2,3,4,5,6"));
+        assertThrows(IllegalArgumentException.class, () -> Validate.checkFormat("1, 2, 3, a, 5"));
     }
 
     @Test
     @DisplayName("숫자 범위 검증")
     void numberInRangeTest() {
-        assertDoesNotThrow(() -> Validate.numberInRange("1,2,3,4,5,6"));
-        assertThrows(IllegalArgumentException.class, () -> Validate.numberInRange("0,1,2,3,4,5"));
-        assertDoesNotThrow(() -> Validate.numberInRange(25));
-        assertThrows(IllegalArgumentException.class, () -> Validate.numberInRange(46));
+        assertDoesNotThrow(() -> Validate.checkValidSixNumberInput("1,2,3,4,5,6"));
+        assertThrows(IllegalArgumentException.class, () -> Validate.checkNumberInRange("0,1,2,3,4,5"));
+        assertDoesNotThrow(() -> Validate.checkSingleNumberInRange(25));
+        assertThrows(IllegalArgumentException.class, () -> Validate.checkSingleNumberInRange(46));
     }
 
     @Test
     @DisplayName("유니크 숫자 6개 입력 검증")
     void validSixNumberInputTest() {
-        assertDoesNotThrow(() -> Validate.validSixNumberInput("1,2,3,4,5,6"));
-        assertThrows(IllegalArgumentException.class, () -> Validate.validSixNumberInput("1,2,2,3,4,5"));
+        assertDoesNotThrow(() -> Validate.checkValidSixNumberInput("1,2,3,4,5,6"));
+        assertThrows(IllegalArgumentException.class, () -> Validate.checkValidSixNumberInput("1,2,2,3,4,5"));
     }
 
     @Test
     @DisplayName("숫자만 포함되었는지 검증")
     void numericInputTest() {
-        assertDoesNotThrow(() -> Validate.numericInput("123456"));
-        assertThrows(IllegalArgumentException.class, () -> Validate.numericInput("1234a6"));
+        assertDoesNotThrow(() -> Validate.checkNumericInput("123456"));
+        assertThrows(IllegalArgumentException.class, () -> Validate.checkNumericInput("1234a6"));
     }
     
     
@@ -46,6 +46,6 @@ class ValidateTest {
     @DisplayName("보너스번호와 당첨번호가 중복 오류 검증")
     void bonusNumberUniquenessTest() {
     	List<Integer> winningNumber = List.of(1,2,3,4,5,6);
-        assertThrows(IllegalArgumentException.class, () -> Validate.bonusNumberUniqueness(winningNumber, 6));
+        assertThrows(IllegalArgumentException.class, () -> Validate.checkBonusNumberUniqueness(winningNumber, 6));
     }
 }

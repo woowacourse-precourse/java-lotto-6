87e1a3594f0f89910ed1624ee2df585d114cbf8c
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 04:57:32 +0900

    feat($Validator): 1000원 미만으로 입력할 경우 예외처리 기능 구현

--- src/main/java/lotto/exception/Validator.java
+++ src/main/java/lotto/exception/Validator.java
@@ -9,4 +9,10 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
     }
+
+    private static void validatePurchaseAmountMinimumValue(int amountInteger) {
+        if (amountInteger < 1000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
+        }
+    }
 }

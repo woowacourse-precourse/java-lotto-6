7acce57dad97720ad95f1211eefde8a865789f44
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 01:51:03 +0900

    feat(ErrorMessage) : 입력에 최소한의 검증에 사용될 에러메세지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -9,7 +9,11 @@ public enum ErrorMessage {
     BONUS_WRONG_RANGE_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 1~45사의 숫자입니다."),
     PAYMENT_NOT_DIVISIBLE_BY_1000(ERROR.get() + "잔돈 없어요. 알맞게 내주세요."),
     PAYMENT_LESS_THAN_1000(ERROR.get() + "로또는 장당 천원 입니다."),
-    PAYMENT_MORE_THAN_100000(ERROR.get() + "로또는 한 번에 최대 10만원까지만 구매 가능합니다.");
+    PAYMENT_MORE_THAN_100000(ERROR.get() + "로또는 한 번에 최대 10만원까지만 구매 가능합니다."),
+    LOTTO_WRONG_INPUT_ERROR_MESSAGE(ERROR.get() + "로또 번호는 6개의 숫자를 ',' 기준으로 입력해주세요."),
+    BONUS_WRONG_INPUT_ERROR_MESSAGE(ERROR.get() + "보너스 번호는 1개의 숫자로 입력해주세요."),
+    PAYMENT_WRONG_INPUT_ERROR_MESSAGE(ERROR.get() + "지불할 금액은 숫자로 입력해주세요.");
+
 
     private final String message;
 

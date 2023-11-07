a950f08214d6c6a492dd88045a774033c5234d5d
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-08 02:24:34 +0900

    add : add functions - validateSize : validate size of numbers - validateDuplicateNumber : validate duplication - validateRange : validate range of each number - getValue : get value of numbers[index]

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.model;
 
+import static lotto.configurations.ErrorMessages.*;
+import static lotto.configurations.SettingValues.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -10,14 +14,35 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-    public List<Integer> getLottoNumbers(){
-        return numbers;
+    public Integer getValue(Integer index) {
+        return numbers.get(index);
     }
 
-    // TODO: 추가 기능 구현
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers.size());
+        List<Integer> confirmedNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            validateRange(number);
+            validateDuplicateNumber(confirmedNumbers, number);
+            confirmedNumbers.add(number);
+        }
+    }
+
+    private void validateSize(Integer size) {
+        if (size != SIZE_OF_LOTTO.get()) {
+            throw new IllegalArgumentException(ERROR_HEADER.get() + INVALID_SIZE_MESSAGE.get());
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numberList, Integer number) {
+        if (numberList.contains(number)) {
+            throw new IllegalArgumentException(ERROR_HEADER.get() + ALREADY_DRAWN_MESSAGE.get());
+        }
+    }
+
+    private void validateRange(Integer number) {
+        if (number < MIN_OF_RANGE.get() || number > MAX_OF_RANGE.get()) {
+            throw new IllegalArgumentException(ERROR_HEADER.get() + OUT_OF_RANGE_NUMBER_MESSAGE.get());
+        }
+    }
 }

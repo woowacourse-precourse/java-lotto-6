cd73da05f4a727f4e6f72734ada5c871799c4971
wonindev <sangwonparkdev@gmail.com>
2023-11-07 17:27:30 +0900

    Feat: 어디에도 종속되어 있지 않는 메소드 추가

---
+++ src/main/java/lotto/utils/CommonHelper.java
@@ -0,0 +1,27 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constants.LottoEnum;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CommonHelper {
+    public static int strToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static List<Integer> listSort(List<Integer> unSortedList) {
+        Collections.sort(unSortedList);
+        return unSortedList;
+    }
+
+
+    public static List<Integer> numGenerator() {
+        return Randoms.pickUniqueNumbersInRange(LottoEnum.LOTTO_MIN_NUMBER.getValue(), LottoEnum.LOTTO_MAX_NUMBER.getValue(), LottoEnum.LOTTO_SIZE.getValue());
+    }
+}

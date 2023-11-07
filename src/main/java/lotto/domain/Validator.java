46ebc04ee1f0e566d631db7c9cff2984d9f81dc6
jjw0071 <jjw0071@naver.com>
2023-11-07 21:39:41 +0900

    feat(Validator) : 예외 메세지 앞에 [ERROR] 부분을 추가해 형식이 조건에 맞도록 수정

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -9,19 +9,19 @@ public class Validator {
         try{
             Integer.parseInt(input);
         }catch(NumberFormatException e){
-            throw new IllegalArgumentException("정수를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.");
         }
     }
 
     public void validatePositiveInteger(int lottoPurchaseCost) {
         if(lottoPurchaseCost<1){
-            throw new IllegalArgumentException("양의 정수를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해야 합니다.");
         }
     }
 
     public void validateAmountInThousands(int lottoPurchaseCost) {
         if(lottoPurchaseCost%1000!=0){
-            throw new IllegalArgumentException("1000의 배수인 정수를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 1000의 배수인 정수를 입력해야 합니다.");
         }
     }
 

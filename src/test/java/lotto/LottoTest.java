490b31cd92b89ae14e35eb5458db829f8a3ef9e0
honee8583 <honee8583@naver.com>
2023-11-07 22:17:33 +0900

    test(lotto): add test code of lotto toString()

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -19,17 +19,23 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
     @DisplayName("로또 번호 가져오기.")
     @Test
     void getLottoNumbers() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         List<Integer> numbers = lotto.getNumbers();
         assertEquals(6, numbers.size());
+    }
+
+    @DisplayName("로또 번호 가져오기.")
+    @Test
+    void lottoToString() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        String content = "[1, 2, 3, 4, 5, 6]";
+        assertEquals(content, lotto.toString());
     }
 }

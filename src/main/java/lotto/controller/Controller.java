37e6e0eb724db14f05409931e117124ce25f88b0
seondays <agseon9@gmail.com>
2023-11-07 23:46:38 +0900

    feat(Controller): 로또 프로세스 진행 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,8 +1,13 @@
 package lotto.controller;
 
 import java.util.List;
+import java.util.Map;
 import lotto.model.BonusNumber;
+import lotto.model.LottoProfit;
+import lotto.model.LottoStore;
+import lotto.model.Lottos;
 import lotto.model.Money;
+import lotto.model.WinningNumberChecker;
 import lotto.utils.StringParser;
 import lotto.view.BonusNumberInputView;
 import lotto.view.InputView;
@@ -12,6 +17,42 @@ import lotto.view.WinningNumberInputView;
 
 public class Controller {
     private InputView inputView;
+    private Lottos lottos = new Lottos();
+    private Money money;
+    private Map<String, Integer> result;
+
+    public void start() {
+        processBuyingLotto();
+        processCalculateResult();
+        processProfit();
+    }
+
+    public void processBuyingLotto() {
+        money = getMoney();
+        OutputView.printLottoCount(money.getLottoCount());
+        LottoStore lottoStore = new LottoStore(money.getLottoCount());
+        lottos = lottoStore.buyLotto();
+        OutputView.printCommonString(lottos.toString());
+    }
+
+    public void processCalculateResult() {
+        List<Integer> winning = getWinningNumber();
+        BonusNumber bonus = getBonusNumber(winning);
+        result = getWinningResult(winning, bonus);
+        OutputView.printWinningResult(result);
+    }
+
+    public void processProfit() {
+        LottoProfit lottoProfit = new LottoProfit(result, money);
+        OutputView.printLottoProfit(lottoProfit.getResult());
+    }
+
+    private Map<String, Integer> getWinningResult(List<Integer> winning, BonusNumber bonus) {
+        WinningNumberChecker winningNumberChecker =
+                new WinningNumberChecker(winning, bonus.getBonus(), lottos);
+        winningNumberChecker.checkWinningRankResult();
+        return winningNumberChecker.getWinningRankResult();
+    }
 
     private Money getMoney() {
         OutputView.printGetMoneyMessage();

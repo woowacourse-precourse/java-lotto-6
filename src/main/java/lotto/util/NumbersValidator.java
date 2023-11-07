fd90ccbadb9f3a6ffdb2cd4748cbff9d7bb3c156
JW0321 <digneotn123@gmail.com>
2023-11-08 00:50:49 +0900

    refactor: 예외 유형별 메세지 추가

--- src/main/java/lotto/util/NumbersValidator.java
+++ src/main/java/lotto/util/NumbersValidator.java
@@ -1,5 +1,7 @@
 package lotto.util;
 
+import static lotto.util.ErrorMessage.*;
+
 import java.util.List;
 
 public class NumbersValidator { //검증 메소드들을 모아 놓은 클래스
@@ -13,20 +15,20 @@ public class NumbersValidator { //검증 메소드들을 모아 놓은 클래스
 
     public static void validate(List<Integer> numbers) { //갯수 체크
         if (numbers.size() != TOTAL_LOTTO_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_NUMBER_OF_LOTTO_ISNT_SIX);
         }
     }
 
     // TODO: 추가 기능 구현
     public static void checkDuplicate(List<Integer> numbers) { //중복 체크
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_CANT_BE_DUPLICATED);
         }
     }
 
     public static void checkElementsInRange(List<Integer> numbers) { //범위 체크
         if (numbers.stream().anyMatch(number -> number < RANDOM_NUMBER_MIN_VALUE || number > RANDOM_NUMBER_MAX_VALUE)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_NUMBER_OUT_OF_RANGE);
         }
     }
 

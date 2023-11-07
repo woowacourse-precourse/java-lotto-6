377150a7668def73979b19623834cd9db4482c50
yeonjae02 <0yeonjae2@naver.com>
2023-11-07 23:49:55 +0900

    feat : printResult() 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -262,8 +262,15 @@ public class Application {
 			return 1;
 		return 0;
 	}
+	
 	private void printResult() {
-		
+		System.out.println("\n당첨 통계");
+		System.out.println("---");
+		System.out.println("3개 일치 (5,000원) - " + rankCnt.get(4) + "개");
+		System.out.println("4개 일치 (50,000원) - " + rankCnt.get(3) + "개");
+		System.out.println("5개 일치 (1,500,000원) - " + rankCnt.get(2) + "개");
+		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + rankCnt.get(1) + "개");
+		System.out.println("6개 일치 (2,000,000,000) - " + rankCnt.get(0) + "개");
 	}
 	
 	private void printProfitPercent(){

00ec66ceff9c983cded6f6de5ce7b8c691aca210
WinterHana <haha920@naver.com>
2023-11-07 18:06:54 +0900

    refactor : bonus 오타 수정
    - 단어 오타 수정

---
+++ src/main/java/lotto/domain/BonusLottoNumber.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class BonusLottoNumber {
+    private int bonusNum;
+
+    public BonusLottoNumber(int bonusNum) {
+        this.bonusNum = bonusNum;
+    }
+
+    public int getBounsNum() {
+        return bonusNum;
+    }
+}

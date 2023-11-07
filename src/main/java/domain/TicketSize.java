581560d61ba3a8116e480783c7d4c8712d96e6f1
eora21 <eora21@naver.com>
2023-11-08 08:51:58 +0900

    feat: 🎸 (TicketSize) 추가 로또 티켓 발행 개수를 나타내는 클래스 작성

---
+++ src/main/java/domain/TicketSize.java
@@ -0,0 +1,22 @@
+package domain;
+
+import exception.NotPositiveLottoTicketsSizeException;
+
+public class TicketSize {
+    private final int ticketSize;
+
+    public TicketSize(int ticketSize) {
+        validateTicketsSize(ticketSize);
+        this.ticketSize = ticketSize;
+    }
+
+    private static void validateTicketsSize(int ticketsSize) {
+        if (ticketsSize <= 0) {
+            throw new NotPositiveLottoTicketsSizeException();
+        }
+    }
+
+    public int getTicketSize() {
+        return ticketSize;
+    }
+}

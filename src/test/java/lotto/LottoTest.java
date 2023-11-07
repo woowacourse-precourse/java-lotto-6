bf91bcf989e66226eaa6da1cb40515d3a661cd95
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-11-07 16:32:16 +0900

    test : Lotto 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -18,10 +18,15 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    
+    @DisplayName("로또 번호에 범위가 벗어나는 숫자가 있으면 에러가 발생한다.")
+    @Test
+    void createLottoByOutOfNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(31, 24, 43, 44, 94, 86)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

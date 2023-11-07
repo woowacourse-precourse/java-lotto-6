4df181d2d63b6ea3896d074d9b311cdd19671182
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-11-07 19:53:08 +0900

    refactor: 입력 검증 로직 클래스로 분리

--- src/main/java/lotto/ui/Input.java
+++ src/main/java/lotto/ui/Input.java
@@ -1,13 +1,9 @@
 package lotto.ui;
 
-import static lotto.global.ErrorMessage.INVALID_LOTTO_NUMBER_ERROR;
-import static lotto.global.ErrorMessage.INVALID_LOTTO_SIZE_ERROR;
 import static lotto.global.ErrorMessage.INVALID_NATURAL_NUMBER_ERROR;
-import static lotto.global.ErrorMessage.INVALID_PRICE_UNIT_ERROR;
-import static lotto.global.LottoInformation.LOTTO_BALLS_NUMBER;
-import static lotto.global.LottoInformation.LOTTO_MAX_NUMBER;
-import static lotto.global.LottoInformation.LOTTO_MIN_NUMBER;
-import static lotto.global.LottoInformation.LOTTO_PRICE_UNIT;
+import static lotto.global.InputValidation.validBonusNumber;
+import static lotto.global.InputValidation.validPurchasePrice;
+import static lotto.global.InputValidation.validUserNumbers;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
@@ -16,43 +12,10 @@ import java.util.stream.Collectors;
 
 public class Input {
     private static final String COMMA_SEPARATOR = ",";
-    private static final int ZERO = 0;
 
     public static int getPurchasePrice() {
         String userInput = Console.readLine();
         return validPurchasePrice(userInput);
-    }
-
-    private static int validPurchasePrice(String userInput) {
-        checkInteger(userInput);
-        int price = StringToInt(userInput);
-        checkNaturalNumber(price);
-        checkPurchaseAmount(price);
-        return price;
-    }
-
-    private static void checkInteger(String userInput) {
-        try {
-            StringToInt(userInput);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_NATURAL_NUMBER_ERROR.getMessage());
-        }
-    }
-
-    private static void checkNaturalNumber(int price) {
-        if (isSmallerOrSameThan(price, ZERO)) {
-            throw new IllegalArgumentException(INVALID_NATURAL_NUMBER_ERROR.getMessage());
-        }
-    }
-
-    private static void checkPurchaseAmount(int price) {
-        if (!isDividedBy(price, LOTTO_PRICE_UNIT)) {
-            throw new IllegalArgumentException(INVALID_PRICE_UNIT_ERROR.getMessage());
-        }
-    }
-
-    private static boolean isDividedBy(int number, int pivot) {
-        return number % pivot == ZERO;
     }
 
     public static List<Integer> getUserNumbers() {
@@ -72,56 +35,8 @@ public class Input {
         }
     }
 
-    private static void validUserNumbers(List<Integer> userNumbers) {
-        checkNumberSize(userNumbers);
-        for (Integer number : userNumbers) {
-            checkNaturalNumber(number);
-            checkInRange(number);
-        }
-    }
-
-    private static void checkNumberSize(List<Integer> userNumbers) {
-        if (userNumbers.size() != LOTTO_BALLS_NUMBER) {
-            throw new IllegalArgumentException(INVALID_LOTTO_SIZE_ERROR.getMessage());
-        }
-    }
-
-    private static void checkInRange(int number) {
-        if (isOutOfRange(number)) {
-            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_ERROR.getMessage());
-        }
-    }
-
-    private static boolean isOutOfRange(int number) {
-        return isSmallerThan(number, LOTTO_MIN_NUMBER) || isBiggerThan(number, LOTTO_MAX_NUMBER);
-    }
-
-    private static boolean isBiggerThan(int number, int pivot) {
-        return number > pivot;
-    }
-
-    private static boolean isSmallerThan(int number, int pivot) {
-        return number < pivot;
-    }
-
-    private static boolean isSmallerOrSameThan(int number, int pivot) {
-        return number <= pivot;
-    }
-
-
     public static int getBonusNumber() {
         String userInput = Console.readLine();
         return validBonusNumber(userInput);
-    }
-
-    private static int validBonusNumber(String userInput) {
-        checkInteger(userInput);
-        int bonusNumber = StringToInt(userInput);
-        checkInRange(bonusNumber);
-        return bonusNumber;
-    }
-
-    private static int StringToInt(String userInput) {
-        return Integer.parseInt(userInput);
     }
 }
--- src/test/java/lotto/ui/InputTest.java
+++ src/test/java/lotto/ui/InputTest.java
@@ -1,12 +1,9 @@
 package lotto.ui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,70 +15,6 @@ public class InputTest {
     @BeforeEach
     public void setUp() {
         input = new Input();
-    }
-
-    @Test
-    @DisplayName("정수가 아닌 수를 입력했을 때, 예외가 발생한다.")
-    public void testNotIntegerPurchasePrice() throws Exception {
-        //given
-        String inValidInput = "2001.1";
-
-        //when
-        Method method = Input.class.getDeclaredMethod("validPurchasePrice", String.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, inValidInput);
-        });
-    }
-
-    @Test
-    @DisplayName("0보다 작은 수를 입력했을 때, 예외가 발생한다.")
-    public void testLessThanZeroPurchasePrice() throws Exception {
-        //given
-        String inValidInput = "-1";
-
-        //when
-        Method method = Input.class.getDeclaredMethod("validPurchasePrice", String.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, inValidInput);
-        });
-    }
-
-    @Test
-    @DisplayName("로또 금액으로 나누어지지 않은 금액을 입력했을 때, 예외가 발생한다.")
-    public void testNotLottoPurchasePrice() throws Exception {
-        //given
-        String inValidInput = "100002";
-
-        //when
-        Method method = Input.class.getDeclaredMethod("validPurchasePrice", String.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, inValidInput);
-        });
-    }
-
-    @Test
-    @DisplayName("String 형태의 정수 값을 int 변환에 성공한다.")
-    public void testStringToInt() throws Exception {
-        //given
-        String validInput = "10000";
-        int expectedResult = 10000;
-
-        //when
-        Method method = Input.class.getDeclaredMethod("StringToInt", String.class);
-        method.setAccessible(true);
-        Object result = method.invoke(input, validInput);
-
-        //then
-        assertEquals(expectedResult, result);
     }
 
     @Test
@@ -97,118 +30,6 @@ public class InputTest {
         //then
         assertThrows(InvocationTargetException.class, () -> {
             method.invoke(input, invalidInput);
-        });
-    }
-
-    @Test
-    @DisplayName("기존 로또의 개수보다 많은 개수를 입력할 경우, 예외가 발생한다.")
-    public void testMoreThanLottoSize() throws Exception {
-        // given
-        List<Integer> invalidInput = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-
-        // when
-        Method method = Input.class.getDeclaredMethod("checkNumberSize", List.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidInput);
-        });
-    }
-
-    @Test
-    @DisplayName("기존 로또의 개수보다 적은 개수를 입력할 경우, 예외가 발생한다.")
-    public void testLessThanLottoSize() throws Exception {
-        // given
-        List<Integer> invalidInput = Arrays.asList(1, 2, 3, 4, 5);
-
-        // when
-        Method method = Input.class.getDeclaredMethod("checkNumberSize", List.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidInput);
-        });
-    }
-
-    @Test
-    @DisplayName("주어진 수가 로또 수 범위보다 클 경우, 예외가 발생한다.")
-    public void testBiggerOutOfRange() throws Exception {
-        // given
-        int invalidNumber = 46;
-
-        // when
-        Method method = Input.class.getDeclaredMethod("checkInRange", int.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidNumber);
-        });
-    }
-
-    @Test
-    @DisplayName("주어진 수가 로또 수 범위보다 작을 경우, 예외가 발생한다.")
-    public void testLessOutOfRange() throws Exception {
-        // given
-        int invalidNumber = 0;
-
-        // when
-        Method method = Input.class.getDeclaredMethod("checkInRange", int.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidNumber);
-        });
-    }
-
-    @Test
-    @DisplayName("보너스 수가 정수 형태가 아닐 경우, 예외가 발생한다.")
-    public void testBonusNotInteger() throws Exception {
-        // given
-        String invalidNumber = "43.4";
-
-        // when
-        Method method = Input.class.getDeclaredMethod("validBonusNumber", String.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidNumber);
-        });
-    }
-
-    @Test
-    @DisplayName("보너스 수가 로또 수 범위보다 클 경우, 예외가 발생한다.")
-    public void testBonusBiggerOutOfRange() throws Exception {
-        // given
-        String invalidNumber = "46";
-
-        // when
-        Method method = Input.class.getDeclaredMethod("validBonusNumber", String.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidNumber);
-        });
-    }
-
-    @Test
-    @DisplayName("보너스 수가 로또 수 범위보다 작을 경우, 예외가 발생한다.")
-    public void testBonusLessOutOfRange() throws Exception {
-        // given
-        String invalidNumber = "0";
-
-        // when
-        Method method = Input.class.getDeclaredMethod("validBonusNumber", String.class);
-        method.setAccessible(true);
-
-        //then
-        assertThrows(InvocationTargetException.class, () -> {
-            method.invoke(input, invalidNumber);
         });
     }
 }

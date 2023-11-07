a2d06ab7b5baadb7d9500c92ec49ed02680f495e
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-08 02:03:35 +0900

    feat(controller/LottoController) : 기능15. 로또 구입 금액을 입력 받는다
    recieveMoney는 사용자의 입력을 검증하고 가공하여 숫자로 돌려주는 메서드 입니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,12 @@
 package lotto.controller;
 
+import static lotto.utils.StringUtils.parseInt;
+import static lotto.view.InputMessage.HOW_MUCH_MONEY_FOR_LOTTO_PURCHASE;
+import static lotto.view.InputView.readInput;
+
 public class LottoController {
+    public int receiveMoney() {
+        String userInput = readInput(HOW_MUCH_MONEY_FOR_LOTTO_PURCHASE.getInputMessage());
+        return parseInt(userInput);
+    }
 }

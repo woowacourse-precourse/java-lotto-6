bb67e18179186ff7cec7d3f4c2c9eedda9df0ed2
becooq81 <becooq81@gmail.com>
2023-11-07 16:29:52 +0900

    refactor(LottosGenerator): replace literal with variable constants

--- src/main/java/lotto/domain/LottosGenerator.java
+++ src/main/java/lotto/domain/LottosGenerator.java
@@ -6,12 +6,13 @@ import java.util.stream.IntStream;
 
 public class LottosGenerator {
 
+    private static final int ZERO = 0;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
 
     public Lottos generateLottos(int numberOfLottos) {
-        List<Lotto> lottos = IntStream.range(0, numberOfLottos)
+        List<Lotto> lottos = IntStream.range(ZERO, numberOfLottos)
                 .mapToObj(i -> generateLotto())
                 .toList();
         return new Lottos(lottos);

695f7acba243b408998be012abbba848454e51e5
mingeun0507 <tqdlfkg@naver.com>
2023-11-08 01:07:24 +0900

    feat: 추가적인 에러 정의

--- src/main/java/lotto/exception/constants/ErrorConsts.java
+++ src/main/java/lotto/exception/constants/ErrorConsts.java
@@ -1,10 +1,11 @@
-package lotto.constants;
+package lotto.exception.constants;
 
 public enum ErrorConsts {
     LOTTO_NUMBER_OUT_OF_RANGE("로또 번호는 1 ~ 45 사이의 숫자만 가능합니다."),
     LOTTO_NUMBERS_DUPLICATED("로또 번호에 중복된 숫자가 있습니다."),
     LOTTO_NUMBERS_SIZE_NOT_MATCH("로또 번호는 6개만 가능합니다."),
     MONEY_NOT_DIVIDED_BY_LOTTO_PRICE("로또 구입 금액은 로또 가격의 배수여야 합니다."),
+    BONUS_NUMBER_DUPLICATED("보너스 번호는 로또 번호와 중복될 수 없습니다."),
 
     ;
 

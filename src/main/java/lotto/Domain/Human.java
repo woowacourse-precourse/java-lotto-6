7c40e048804e676c0f5f77e080122d5936780468
SangHee Lee <lsh277604@gmail.com>
2023-11-07 12:34:25 +0900

    Feat: getLottoLength() and toString Method

--- src/main/java/lotto/Domain/Human.java
+++ src/main/java/lotto/Domain/Human.java
@@ -1,28 +1,41 @@
 package lotto.Domain;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class Human {
+
+
     private List<Lotto> lottos = new ArrayList<>();
     private Map<WinningType, Integer> winningResult = new EnumMap<>(WinningType.class);
     private Long budget;
+
     public Human(Long budget) {
         this.budget = budget;
     }
 
-    public boolean buyLotto(){
-        if(budget > 1000){
+    public boolean buyLotto() {
+        if (budget > 1000) {
             budget -= 1000;
             return true;
         }
         return false;
     }
 
-    public void addLotto(List<Integer> lottoNumber){
+    public void addLotto(List<Integer> lottoNumber) {
         Lotto lotto = new Lotto(lottoNumber);
         lottos.add(lotto);
+    }
+
+    public int getLottosLength() {
+        return Array.getLength(lottos);
+    }
+
+    @Override
+    public String toString() {
+        return "개를 구매했습니다.";
     }
 }

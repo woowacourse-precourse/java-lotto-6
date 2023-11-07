211ebf239c5157f1c3814a727cff607322a20a8d
JongMinCh0i <wonderland314592@gmail.com>
2023-11-07 13:34:08 +0900

    style(LottoNumber): 클래스 이름 변경 (LottoValue -> LottoNumber)

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
-public class LottoValue {
+public class LottoNumber {
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_RANGE_IN_START_VALUE = 1;
     public static final int LOTTO_RANGE_IN_END_VALUE = 45;
 
-    private LottoValue() {
+    private LottoNumber() {
     }
 }

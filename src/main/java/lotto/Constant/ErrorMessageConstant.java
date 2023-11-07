53484e42479371788d82e6f5c313a02df1a472f2
mj.kim <alswo9853@gmail.com>
2023-11-07 15:02:08 +0900

    fix: 에러 출력 문구 수정

--- src/main/java/lotto/Constant/ErrorMessageConstant.java
+++ src/main/java/lotto/Constant/ErrorMessageConstant.java
@@ -8,7 +8,7 @@ public class ErrorMessageConstant {
 
     public static final String DUPLICATE = "[ERROR] 로또 번호와 중복된 숫자는 입력할 수 없습니다.";
 
-    public static final String CANNOT_INSERT_MINOUS = "[] 양을 넣을 수 없습니다.";
+    public static final String CANNOT_INSERT_MINOUS = "[ERROR] 양의 정수만 넣을 수 없습니다.";
 
     public static final String ONLY_DIVIDE_BY_1000 = "[ERROR] 1000원 단위로만 입력해주세요.";
 }

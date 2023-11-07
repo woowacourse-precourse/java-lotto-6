5d0054f7c6226df1756550680e6624008c603e81
정재웅 <jweidon0821@gmail.com>
2023-11-07 17:05:29 +0900

    refactor(view): 입력 요청 메세지 OutputView로 이동

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,22 +3,18 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String askMoneyMessage = "구입금액을 입력해 주세요.";
-    public static String askWinningNumbersMessage = "당첨 번호를 입력해 주세요.";
-    public static String askBonusNumberMessage = "보너스 번호를 입력해 주세요.";
-
     public static String inputMoney() {
-        System.out.println(askMoneyMessage);
+        OutputView.printAskMoneyMessage();
         return Console.readLine();
     }
 
     public static String inputWinningNumbers() {
-        System.out.println(askWinningNumbersMessage);
+        OutputView.printAskWinningNumbersMessage();
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
-        System.out.println(askBonusNumberMessage);
+        OutputView.printAskBonusNumberMessage();
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,19 @@
 package lotto.view;
 
 public class OutputView {
+    public static String askMoneyMessage = "구입금액을 입력해 주세요.";
+    public static String askWinningNumbersMessage = "당첨 번호를 입력해 주세요.";
+    public static String askBonusNumberMessage = "보너스 번호를 입력해 주세요.";
+
+    public static void printAskMoneyMessage() {
+        System.out.println(askMoneyMessage);
+    }
+
+    public static void printAskWinningNumbersMessage() {
+        System.out.println(askWinningNumbersMessage);
+    }
+
+    public static void printAskBonusNumberMessage() {
+        System.out.println(askBonusNumberMessage);
+    }
 }

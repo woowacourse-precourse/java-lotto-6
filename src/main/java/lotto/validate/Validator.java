d95c6264d7f727a94a5c8f324e0c1de95a59e6ee
KimSangHyeok <tkdgur0906@naver.com>
2023-11-08 00:11:52 +0900

    feat : 숫자가 1000의 배수인지 검증하는 기능 구현

--- src/main/java/lotto/validate/Validator.java
+++ src/main/java/lotto/validate/Validator.java
@@ -4,4 +4,8 @@ public class Validator {
     public static boolean isNumeric(String str) {
         return str.chars().allMatch(Character::isDigit);
     }
+
+    public static boolean isMultipleOf1000(int money) {
+        return money % 1000 == 0;
+    }
 }

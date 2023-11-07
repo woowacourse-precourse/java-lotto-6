821a7b8942f9f385543a91fe7423ad21ec1c857c
LeeJaeHoon <dlwogns3413@naver.com>
2023-11-08 00:32:59 +0900

    feat: Winning Result 뼈대 구축

--- src/main/java/lotto/command/Command.java
+++ src/main/java/lotto/command/Command.java
@@ -8,7 +8,8 @@ public enum Command {
     BUY_LOTTO,
     OUTPUT_USER_LOTTO,
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    OUTPUT_WINNING_RESULT;
 
     private String message;
     private Validator validator;
---
+++ src/main/java/lotto/controller/lotto/WinningResultController.java
@@ -0,0 +1,12 @@
+package lotto.controller.lotto;
+
+import java.util.List;
+import lotto.controller.Controller;
+import lotto.view.View;
+
+public class WinningResultController implements Controller {
+    @Override
+    public View process(List<String> input) {
+        return null;
+    }
+}

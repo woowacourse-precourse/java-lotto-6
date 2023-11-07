9decdb98285f5deb77285a02336ee7f42de58ed1
hoyeon <highoption@naver.com>
2023-11-07 15:16:43 +0900

    feat(WinningLottoValidator) : 당첨 넘버를 검증하는 isInteger 생성
    - 입력받은 당첨결과를 검증하기 위한 메서드 생성

---
+++ src/main/java/lotto/model/WinningLottoValidator.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+import java.util.List;
+import lotto.Utils;
+
+public class WinningLottoValidator {
+    public static void validateWinningLotto(String winningLotto) {
+        isInteger(winningLotto);
+    }
+
+    private static void isInteger(String winningLotto) {
+        List<String> winningNumbers = Utils.splitStringToList(winningLotto);
+
+        if (winningNumbers.stream().anyMatch(number -> !number.matches("^[0-9]+$"))) {
+            throw new IllegalArgumentException("[ERROR] 로또 넘버는 숫자여야 합니다.");
+        }
+    }
+}

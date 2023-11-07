0bebb892b517b6d11fed981f35d0a763bf5793a3
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-07 20:07:16 +0900

    feat: statement출력하는 프린터 클래스 구현

---
+++ src/main/java/lotto/controller/Printer.java
@@ -0,0 +1,25 @@
+package lotto.controller;
+
+import lotto.view.Statement;
+
+public class Printer {
+    public static void printPurchaseAmount() {
+        System.out.println(Statement.ASK_PURCHASE_AMOUNT);
+    }
+
+    public static void printTotalTickets() {
+        System.out.println(Statement.NOTIFY_TOTAL_LOTTO_TICKETS);
+    }
+
+    public static void printUserNumbers() {
+        System.out.println(Statement.ASK_JACKPOT_NUMBERS);
+    }
+
+    public static void printAskBonus() {
+        System.out.println(Statement.ASK_BONUS_NUMBER);
+    }
+
+    public static void printProfitRate() {
+        System.out.println(Statement.SHOW_PROFIT_RATE);
+    }
+}
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,9 +1,5 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.Console;
-import lotto.view.ExceptionMessage;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -11,60 +7,6 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
-    }
-
-    private List<Integer> InputNumbers() {
-        String[] input = Console.readLine().split(",");
-        List<Integer> inputNumbers = new ArrayList<>();
-
-        for (String str : input) {
-            inputNumbers.add(Integer.parseInt(str));
-        }
-
-        return inputNumbers;
-    }
-
-    public int bonusNumber() {
-        int number = Integer.parseInt(Console.readLine());
-        bonusNumberValidation(number);
-        rangeBonusValidation(number);
-        return number;
-    }
-
-    private void bonusNumberValidation(int number) {
-        Lotto lotto = new Lotto(numbers);
-
-        if(lotto.getNumbers().contains(number)) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_BONUS_NUMBER.getMessage());
-        }
-
-    }
-
-    private void sizeValidation(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            if(numbers.size() == 0){
-                throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_EMPTY.getMessage());
-            }
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_COUNT.getMessage());
-        }
-    }
-
-    private void rangeValidation(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_RANGE.getMessage());
-            }
-        }
-    }
-
-    private void rangeBonusValidation(int number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_RANGE.getMessage());
-        }
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
     }
 
 }

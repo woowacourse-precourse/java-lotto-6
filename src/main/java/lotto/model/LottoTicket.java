9550bd2a7bf9e36e5b6f7d75b53b97a885b4d310
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-07 20:24:15 +0900

    ref: LottoTicket 모델과 컨트롤러 분리

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -13,57 +13,9 @@ public class LottoTicket {
     private final int tickets;
     private final List<List<Integer>> ticketNumbers;
 
-    public LottoTicket() {
-        this.tickets = purchaseTicket();
-        this.ticketNumbers = purchaseTicketNumbers();
-    }
-
-    private List<List<Integer>> purchaseTicketNumbers() {
-        int ticket = purchaseTicket();
-        List<List<Integer>> purchaseTicketNumbers = new ArrayList<>();
-
-        for(int i = 0; i < ticket; i++) {
-            purchaseTicketNumbers.add(setTicketNumbers());
-        }
-
-        return purchaseTicketNumbers;
-    }
-
-    private List<Integer> setTicketNumbers () {
-        List<Integer> ticketNumbers = new ArrayList<>();
-
-        for(int i = 0; i < 6; i++){
-            int number = Randoms.pickNumberInRange(1,45);
-            validateDistinction(ticketNumbers, number);
-        }
-
-        Collections.sort(ticketNumbers);
-        return ticketNumbers;
-    }
-
-    private void validateDistinction (List<Integer> ticketNumbers, int randomNumber) {
-
-        if (!ticketNumbers.contains(randomNumber)) {
-            ticketNumbers.add(randomNumber);
-        }
-
-    }
-
-    private int purchaseTicket() {
-        int money = Integer.parseInt(Console.readLine());
-        return calculate(money);
-    }
-
-    private int calculate(int money) {
-        int ticketCost = GameEnvironment.TICKET.getValue();
-        calculationValidation(money);
-        return money/ticketCost;
-    }
-
-    private void calculationValidation(int money) {
-        if (money%1000 != 0){
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_PURCHASE_AMOUNT.getMessage());
-        }
+    public LottoTicket(int tickets, List<List<Integer>>ticketNumbers) {
+        this.tickets = tickets;
+        this.ticketNumbers = ticketNumbers;
     }
 
     public List<List<Integer>> getTicketNumbers() {

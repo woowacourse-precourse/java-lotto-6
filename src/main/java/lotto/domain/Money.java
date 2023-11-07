cb9f519f8771194c220dccde11e7bc20ff34c66e
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 16:32:24 +0900

    feat(Money): 스켈레톤 코드 작성

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,2 +1,22 @@
-package lotto.domain;public class Money {
+package lotto.domain;
+
+public class Money {
+    private final int spendAmount;
+
+    public Money(String spendAmount) {
+        validate(spendAmount);
+        this.spendAmount = spendAmount;
+    }
+
+    private void validate(String spendAmount) {
+        validateDividedByThousand(spendAmount);
+        validateRangeOfSpendAmount(spendAmount);
+    }
+
+    private void validateRangeOfSpendAmount(String spendAmount) {
+    }
+
+    private void validateDividedByThousand(String spendAmount) {
+        
+    }
 }

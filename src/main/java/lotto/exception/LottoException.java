35eadb410346954c2921378b401e49d546440b04
jiu6525 <jiu6525@daum.net>
2023-11-07 17:51:09 +0900

    feat : 사용자 입력이 비정상일경우 예외를 발생하는 메서드 작성

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -9,11 +9,20 @@ public class LottoException {
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String TICKET_ERROR_MESSAGE = "구입 금액이 잘못 되었습니다. 티켓 한장의 가격은 1000원입니다.";
 
-    private boolean purchaseAmountCheck(String purchaseAmount) {
+    private boolean purchaseAmountCheckSub(String purchaseAmount) {
         int amount = Integer.parseInt(purchaseAmount);
         if ((amount < lottoConfig.TICKET_PRICE) || ((amount % lottoConfig.TICKET_PRICE) != ZERO_PRICE)) {
             throw new IllegalArgumentException(ERROR_PREFIX + TICKET_ERROR_MESSAGE);
         }
         return true;
+    }
+
+    public boolean purchaseAmountCheckMain(String purchaseAmount) {
+        try {
+            return purchaseAmountCheckMain(purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }

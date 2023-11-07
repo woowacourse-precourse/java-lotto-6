7b010f259e80ea792aca27e072e88384aff25903
SunHyeok <cghhld@naver.com>
2023-11-07 11:51:42 +0900

    refactor: 오름차순 정렬

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -1,11 +1,13 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Collections;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoNumberGenerator {
     public static List<Integer> generateRandomLottoNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
 }

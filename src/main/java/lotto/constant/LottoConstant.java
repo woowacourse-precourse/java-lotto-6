c912545db2d77e3e4a0b17917261601164647e81
OhJongMin <jmoh7480@gmail.com>
2023-11-07 20:11:09 +0900

    feat(lotto): 로또 당첨 관련 상수 정의

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -1,20 +1,23 @@
 package lotto.constant;
 
 public enum LottoConstant {
-    NO_PRIZE_INDEX(-1),
-    FIRST_PLACE_INDEX(0),
-    SECOND_PLACE_INDEX(1),
-    THIRD_PLACE_INDEX(2),
-    FOURTH_PLACE_INDEX(3),
-    FIFTH_PLACE_INDEX(4);
+    NO_PRIZE(0, -1),
+    FIRST_PLACE(6, 0),
+    SECOND_PLACE(5, 1),
+    THIRD_PLACE(5, 2),
+    FOURTH_PLACE(4, 3),
+    FIFTH_PLACE(3, 4);
 
     private final int value;
+    private final int index;
 
-    LottoConstant(int value){
+    LottoConstant(int value, int index){
         this.value = value;
+        this.index = index;
     }
 
     public int getValue(){
         return this.value;
     }
+    public int getIndex() { return this.index; }
 }
--- src/main/java/lotto/model/Bank.java
+++ src/main/java/lotto/model/Bank.java
@@ -1,20 +1,29 @@
 package lotto.model;
 
+import static lotto.constant.LottoConstant.FIFTH_PLACE;
+import static lotto.constant.LottoConstant.FIRST_PLACE;
+import static lotto.constant.LottoConstant.FOURTH_PLACE;
+import static lotto.constant.LottoConstant.NO_PRIZE;
+import static lotto.constant.LottoConstant.SECOND_PLACE;
+import static lotto.constant.LottoConstant.THIRD_PLACE;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import lotto.constant.LottoConstant;
 import lotto.constant.NumberConstant;
 
 public class Bank {
+    private final int rankRange = 5;
+    private final double rateRange = 10.0;
+
     public List<Integer> getRanks(List<Lotto> lottos, List<Integer> userNumbers, int bonusNumber){
         int[] ranks = new int[5];
         for (Lotto lotto : lottos){
             List<Integer> lottoNumbers = lotto.getNumbers();
             int index = getRankIndex(lottoNumbers, userNumbers, bonusNumber);
-            if (index != LottoConstant.NO_PRIZE_INDEX.getValue()){
+            if (index != NO_PRIZE.getValue()){
                 ranks[index]++;
             }
         }
@@ -23,7 +32,7 @@ public class Bank {
 
     public double getFormatProfitRate(int money, int profit){
         double profitRate = getProfitRate(money, profit);
-        return Math.round(profitRate * 10) / 10.0;
+        return Math.round(profitRate * rateRange) / rateRange;
     }
     private int getRankIndex(List<Integer> lottoNumbers, List<Integer> userNumbers, int bonusNumber){
         int rightCount = getRightCount(lottoNumbers, userNumbers);
@@ -36,21 +45,21 @@ public class Bank {
     }
 
     private int getRank(int count, boolean bonus){
-        if (count == 3){
-            return LottoConstant.FIFTH_PLACE_INDEX.getValue();
+        if (count == FIFTH_PLACE.getValue()){
+            return FIFTH_PLACE.getIndex();
         }
-        if (count == 4){
-            return LottoConstant.FOURTH_PLACE_INDEX.getValue();
+        if (count == FOURTH_PLACE.getValue()){
+            return FOURTH_PLACE.getIndex();
         }
-        if (count == 5){
+        if (count == THIRD_PLACE.getValue()){
             if (bonus)
-                return LottoConstant.SECOND_PLACE_INDEX.getValue();
-            return LottoConstant.THIRD_PLACE_INDEX.getValue();
+                return SECOND_PLACE.getIndex();
+            return THIRD_PLACE.getIndex();
         }
-        if (count == 6){
-            return LottoConstant.FIRST_PLACE_INDEX.getValue();
+        if (count == FIRST_PLACE.getValue()){
+            return FIRST_PLACE.getIndex();
         }
-        return LottoConstant.NO_PRIZE_INDEX.getValue();
+        return NO_PRIZE.getIndex();
     }
 
     private int getRightCount(List<Integer> lottoNumbers, List<Integer> userNumbers){

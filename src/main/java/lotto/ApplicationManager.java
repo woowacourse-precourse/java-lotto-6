5200a45d8ec723831011ef7ed19807c7fc1bebcf
wonindev <sangwonparkdev@gmail.com>
2023-11-07 19:08:25 +0900

    Feat: 발행한 로또를 출력하는 곳까지 진행

--- src/main/java/lotto/ApplicationManager.java
+++ src/main/java/lotto/ApplicationManager.java
@@ -1,9 +1,13 @@
 package lotto;
 
 import lotto.UI.InputHandler;
+import lotto.UI.OutputHandler;
 import lotto.constants.LottoEnum;
+import lotto.domain.Lotto;
 import lotto.domain.LottoManager;
 import lotto.utils.CommonHelper;
+
+import java.util.List;
 
 public class ApplicationManager {
     public void start() {
@@ -11,5 +15,8 @@ public class ApplicationManager {
         int cost = CommonHelper.strToInt(inputHandler.inputCost());
         int ticketCnt = CommonHelper.divide(cost, LottoEnum.LOTTO_PRICE.getValue());
         LottoManager lottoManager = new LottoManager(ticketCnt);
+        List<Lotto> lottos = lottoManager.createLotto(ticketCnt);
+        OutputHandler outputHandler = new OutputHandler();
+        outputHandler.printLottos();
     }
 }

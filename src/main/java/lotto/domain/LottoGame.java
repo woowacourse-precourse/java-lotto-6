ea03dabaf54787bdf8858ad4934ab8637ef45df0
jexnjeux <zieun.na@gmail.com>
2023-11-08 01:24:08 +0900

    refactor: 당첨 등수 상수명 개선

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -9,11 +9,11 @@ public class LottoGame {
     private int bonusNumber;
     private int[] winner = new int[5];
 
-    private static final int MATCH_3_PRIZE = 5000;
-    private static final int MATCH_4_PRIZE = 50000;
-    private static final int MATCH_5_PRIZE = 1500000;
-    private static final int MATCH_5_WITH_BONUS_PRIZE = 30000000;
-    private static final int MATCH_6_PRIZE = 2000000000;
+    private static final int THREE_MATCH_PRIZE = 5000;
+    private static final int FOUR_MATCH_PRIZE = 50000;
+    private static final int FIVE_MATCH_PRIZE = 1500000;
+    private static final int FIVE_MATCH_WITH_BONUS_PRIZE = 30000000;
+    private static final int SIX_MATCH_PRIZE = 2000000000;
     private static final int TICKET_PRICE_PER_LOTTO = 1000;
 
     public LottoGame(List<Lotto> lottos) {
@@ -27,13 +27,13 @@ public class LottoGame {
 
     public void printLottoResult() {
         countMatchingLottos();
-        System.out.println("당첨 통계\n---\n");
+        System.out.println("\n당첨 통계\n---");
         System.out.printf("3개 일치 (5,000원) - %d개%n", winner[0]);
         System.out.printf("4개 일치 (50,000원) - %d개%n", winner[1]);
         System.out.printf("5개 일치 (1,500,000원) - %d개%n", winner[2]);
         System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", winner[3]);
         System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", winner[4]);
-        System.out.printf("총 수익률은 %.2f%%입니다.", calculateProfitRate());
+        System.out.printf("총 수익률은 %.1f%%입니다.", calculateProfitRate());
 
     }
 
@@ -67,8 +67,8 @@ public class LottoGame {
     }
 
     private int calculateTotalPrize() {
-        return winner[0] * MATCH_3_PRIZE + winner[1] * MATCH_4_PRIZE + winner[2] * MATCH_5_PRIZE
-                + winner[3] * MATCH_5_WITH_BONUS_PRIZE + winner[4] * MATCH_6_PRIZE;
+        return winner[0] * THREE_MATCH_PRIZE + winner[1] * FOUR_MATCH_PRIZE + winner[2] * FIVE_MATCH_PRIZE
+                + winner[3] * FIVE_MATCH_WITH_BONUS_PRIZE + winner[4] * SIX_MATCH_PRIZE;
     }
 
     private double calculateProfitRate() {

8f0507150168d01f049b2fe69d3ba240193d668e
jihwankim128 <jihwan1535@nate.com>
2023-11-08 01:04:38 +0900

    feat(lotto): 인수 에러 발생시 반환하는 리턴 메시지들을 상수로 저장

---
+++ src/main/java/lotto/util/IllegalArgumentMessage.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public final class IllegalArgumentMessage {
+    public static final String ERROR_DUPLICATION_MESSAGE = "[Error] 입력하신 번호와 중복된 번호가 존재합니다.";
+    public static final String ERROR_RANGE_MESSAGE = "[Error] 로또 번호는 범위는 1부터 45입니다.";
+    public static final String ERROR_LESS_THAN_MINIMUM_PRICE = "[Error] 최소 결제 금액은 1,000원 입니다.";
+    public static final String ERROR_NOT_PRICE_UNIT = "[Error] 1,000원 단위로 결제해주세요.";
+    public static final String ERROR_PICK_COUNT = "[Error] 로또 번호는 6개입니다.";
+}

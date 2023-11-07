e6cd49d395cd3ae18141b45a57c233ec112062fa
ohhyeon2 <dhgus1235@gmail.com>
2023-11-07 14:51:35 +0900

    feat: 수익률 output 생성

---
+++ src/main/java/lotto/view/ProfitRateOutPut.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class ProfitRateOutPut {
+    public static void profitRate(double profitRate) {
+        System.out.println("총 수익률은" + profitRate + "%입니다.");
+    }
+}

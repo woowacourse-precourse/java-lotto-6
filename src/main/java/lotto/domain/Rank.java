daaded3521b8602daf149df9eb4ed75cea089b5d
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 18:16:39 +0900

    refactor(Rank): 등수 상수 위치 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,21 +4,23 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public enum Rank {
-    FIRST(6, List.of(true, false), new BigDecimal(2_000_000_000L)),
-    SECOND(5, List.of(true), new BigDecimal(30_000_000L)),
-    THIRD(5, List.of(false), new BigDecimal(1_500_000L)),
-    FOURTH(4, List.of(true, false), new BigDecimal(50_000L)),
-    FIFTH(3, List.of(true, false), new BigDecimal(5_000L)),
-    NONE(0, List.of(true, false), new BigDecimal(0L));
+    FIFTH(3, List.of(true, false), new BigDecimal(5_000L), "3개 일치 (5,000원)"),
+    FOURTH(4, List.of(true, false), new BigDecimal(50_000L), "4개 일치 (50,000원)"),
+    THIRD(5, List.of(false), new BigDecimal(1_500_000L), "5개 일치 (1,500,000원)"),
+    SECOND(5, List.of(true), new BigDecimal(30_000_000L), "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, List.of(true, false), new BigDecimal(2_000_000_000L), "6개 일치 (2,000,000,000원)"),
+    NONE(0, List.of(true, false), new BigDecimal(0L), "없음");
 
     private final int matchCounts;
     private final List<Boolean> matchBonus;
-    private final BigDecimal price;
+    private final BigDecimal prize;
+    private final String prizeMessage;
 
-    Rank(int matchCounts, List<Boolean> matchBonus, BigDecimal price) {
+    Rank(int matchCounts, List<Boolean> matchBonus, BigDecimal prize, String prizeMessage) {
         this.matchCounts = matchCounts;
         this.matchBonus = matchBonus;
-        this.price = price;
+        this.prize = prize;
+        this.prizeMessage = prizeMessage;
     }
 
     public static Rank calculateRank(final int matchCounts, final boolean matchBonus) {
@@ -30,11 +32,11 @@ public enum Rank {
         return NONE;
     }
 
-    public int getMatchCounts() {
-        return matchCounts;
+    public BigDecimal getPrize() {
+        return prize;
     }
 
-    public BigDecimal getPrice() {
-        return price;
+    public String getPrizeMessage() {
+        return prizeMessage;
     }
 }

2c2ed421b366877066bd2254ff13d15d17505913
Identityless <rlandwnd555@naver.com>
2023-11-08 01:01:34 +0900

    feat : Bonus model 추가 (일급컬렉션의 역할 포함)

---
+++ src/main/java/lotto/Bonus.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class Bonus {
+    private final int bonus;
+
+    public Bonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}

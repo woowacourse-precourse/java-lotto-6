c77f2c388de30637acc48e2b4d0de726a74ed5a8
KimChanJin97 <cjkimhello97@gmail.com>
2023-11-07 17:07:41 +0900

    feat: implement Price validations

--- src/main/java/lotto/domain/consumer/Price.java
+++ src/main/java/lotto/domain/consumer/Price.java
@@ -1,8 +1,36 @@
 package lotto.domain.consumer;
 
+import lotto.exception.Price.PriceNot1KsException;
+import lotto.exception.Price.PriceNotInRangeException;
+import lotto.exception.Price.PriceNotPositiveIntegerException;
+
 public record Price(int value) {
 
-    public Price {
+    private static final String NUMERIC_REGEX = "^[0-9]*$";
 
+    public Price {
+        validatePriceNumeric(value);
+        validatePrice1Ks(value);
+        validatePriceNotInRange(value);
+    }
+
+    private void validatePriceNumeric(int value) {
+        String line = String.valueOf(value);
+
+        if (!line.matches(NUMERIC_REGEX)) {
+            throw new PriceNotPositiveIntegerException();
+        }
+    }
+
+    private void validatePrice1Ks(int value) {
+        if (value % 1000 != 0) {
+            throw new PriceNot1KsException();
+        }
+    }
+
+    private void validatePriceNotInRange(int value) {
+        if (value <= 0) {
+            throw new PriceNotInRangeException();
+        }
     }
 }

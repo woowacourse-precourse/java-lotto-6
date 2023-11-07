acfedd579e4f38413722bcd66f75cdb5dcbb5afa
Minjae-An <mj3242@naver.com>
2023-11-07 20:49:48 +0900

    feat : 로또 검증 기능 구성
    Lotto 생성자에서 로또 숫자의 개수와 중복 숫자 존재 여부,
    로또 숫자의 범위 유효성을 검증하도록 구현

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,19 +1,18 @@
 package lotto.domain.lotto;
 
+import static lotto.validator.InputValidator.*;
+
 import java.util.List;
+import lotto.validator.InputValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumberCount(numbers);
+        validateDuplicateNumber(numbers);
+        numbers.forEach(InputValidator::validateLottoNumberRange);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public List<Integer> getNumbers() {

572e5a3dfc4a6b7b1913829cc9c026fcf64b0908
jiyoon <95969941+jjunjji@users.noreply.github.com>
2023-11-07 19:28:29 +0900

    feat: 로또 게임을 실행한다.

---
+++ src/main/java/lotto/domain/LottoGame.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import lotto.util.AutoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGame {
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoResult lottoResult;
+    private PurchaseLottos purchaseLottos;
+
+    public LottoGame() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    public void start() {
+        PurchasePrice purchasePrice = inputPurchasePrice();
+        purchaseLottos = new PurchaseLottos(purchasePrice, new AutoGenerator());
+
+        outputView.printPurchaseLottos(purchaseLottos);
+
+        WinningNumbers winningNumbers = inputWinningNumbers();
+        BonusNumber bonusNumber = inputBonusNumber();
+
+        lottoResult = purchaseLottos.calculateLottoResult(winningNumbers, bonusNumber);
+
+        outputView.printLottoResult(lottoResult);
+        outputView.printRateOfReturn(lottoResult, purchasePrice);
+    }
+
+    private PurchasePrice inputPurchasePrice() {
+        return inputView.inputPurchasePrice();
+    }
+
+    private WinningNumbers inputWinningNumbers() {
+        return inputView.inputWinningNumbers();
+    }
+
+    private BonusNumber inputBonusNumber() {
+        return inputView.inputBonusNumber();
+    }
+}

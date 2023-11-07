e8a78434c3b9da89cf1e4b7a4092fd5dbcfb2e39
parkheeddong <phj7214go@gmail.com>
2023-11-07 14:40:40 +0900

    feat: main 메소드에서 사용자 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,11 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 로또 구입 금액 입력 받기
+        BuyingLotto.gettingInput();
+
+        // 당첨 번호와 보너스 번호 입력 받기
+        WinningNumber.gettingWinningNumber();
+        WinningNumber.gettingBonusNumber();
     }
 }

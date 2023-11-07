5088f88eaa01cd0e2cccbfa8ec240c48097ce45b
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:55:16 +0900

    feat(LottoController): 로또를 구매하는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,15 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoPurchase;
 import lotto.domain.Money;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private Money money;
+    private List<Lotto> lottos;
 
     public void start() {
         initialize();
@@ -15,6 +19,9 @@ public class LottoController {
         money = putMoney(inputMoney());
         int purchaseCount = money.getPurchaseCount();
         OutputView.showPurchaseCount(purchaseCount);
+
+        lottos = buyLotto(purchaseCount);
+        OutputView.showPurchaseLotto(lottos);
     }
 
     private Money putMoney(long money) {
@@ -33,5 +40,9 @@ public class LottoController {
             System.out.println(e.getMessage());
             return inputMoney();
         }
+    }
+
+    private List<Lotto> buyLotto(int purchaseCount) {
+        return LottoPurchase.buyLotto(purchaseCount);
     }
 }

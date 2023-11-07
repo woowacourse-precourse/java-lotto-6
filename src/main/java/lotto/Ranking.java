d3fda35a2370d3505bbcc00b3fdadac39a5272fe
kg9585 <kg9585@naver.com>
2023-11-07 18:37:58 +0900

    refactor(Ranking): 순위 구하는 기능 리팩토링

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -3,24 +3,27 @@ package lotto;
 import java.util.Arrays;
 
 public enum Ranking {
-    FIRST(2000000000, 6),
-    SECOND(30000000, 5),
-    THIRD(1500000, 5),
-    FOURTH(50000, 4),
-    FIFTH(5000, 3);
+    FIRST(2000000000, 6, false),
+    SECOND(30000000, 5,true),
+    THIRD(1500000, 5,false),
+    FOURTH(50000, 4,false),
+    FIFTH(5000, 3,false);
 
     private final int prize;
     private final int count;
+    private final boolean hasBonusNumber;
 
-    Ranking(int prize, int count) {
+    Ranking(int prize, int count,boolean hasBonusNumber) {
         this.prize = prize;
         this.count = count;
+        this.hasBonusNumber = hasBonusNumber;
     }
 
-    public static Ranking findRankingByCnt(int cnt) {
+    public static Ranking findRanking(int count, boolean hasBonusNumber) {
         return Arrays.stream(Ranking.values())
-                .filter(ranking -> ranking.count == cnt)
-                .findAny()
-                .orElseThrow(IllegalArgumentException::new);
+                .filter(ranking -> ranking.count == count)
+                .filter(ranking -> ranking.hasBonusNumber == hasBonusNumber)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No matching ranking found."));
     }
 }
--- src/main/java/lotto/prizeNumbers.java
+++ src/main/java/lotto/prizeNumbers.java
@@ -3,11 +3,13 @@ package lotto;
 public class prizeNumbers {
     private Lotto lotto;
     private BonusNumber bonusNumber;
+
     public prizeNumbers(Lotto lotto, BonusNumber bonusNumber) {
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
         validateDuplicateNumber(bonusNumber);
     }
+
     private void validateDuplicateNumber(BonusNumber bonusNumber) {
         lotto.checkForBonusNumber(bonusNumber);
     }
@@ -15,16 +17,6 @@ public class prizeNumbers {
     public Ranking calculatePrize(Lotto otherLottoNumbers) {
         int count = lotto.calculateSameCount(otherLottoNumbers);
         boolean hasBonus = otherLottoNumbers.hasBonusNumber(bonusNumber);
-
-        if (count == 6) {
-            return Ranking.FIRST;
-        }
-        if (count == 5 && hasBonus) {
-            return Ranking.SECOND;
-        }
-        if (count == 5) {
-            return Ranking.THIRD;
-        }
-            return Ranking.findRankingByCnt(count);
-        }
+        return Ranking.findRanking(count, hasBonus);
     }
+}

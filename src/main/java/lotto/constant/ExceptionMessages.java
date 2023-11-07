c48b5de521b3c04dd1a25317c7e447e0823f3ddd
Minyongggg <naminyong97@gmail.com>
2023-11-08 02:35:32 +0900

    feat: 문자열을 당첨 번호로 파싱하는 기능 구현

--- src/main/java/lotto/constant/ExceptionMessages.java
+++ src/main/java/lotto/constant/ExceptionMessages.java
@@ -1,7 +1,9 @@
 package lotto.constant;
 
 public enum ExceptionMessages {
-    PURCHASE_AMOUNT_FORMAT_EXCEPTION_MESSAGE("구입금액은 양의 정수여야 합니다.");
+    PURCHASE_AMOUNT_FORMAT_EXCEPTION_MESSAGE("구입금액은 양의 정수여야 합니다."),
+    WINNING_NUMBER_FORMAT_EXCEPTION_MESSAGE("당첨 번호는 ,로 구분된 양의 정수의 배열이어야 합니다.");
+
 
     private String value;
 
--- src/main/java/lotto/util/StringParser.java
+++ src/main/java/lotto/util/StringParser.java
@@ -1,8 +1,14 @@
 package lotto.util;
 
 import static lotto.constant.ExceptionMessages.PURCHASE_AMOUNT_FORMAT_EXCEPTION_MESSAGE;
+import static lotto.constant.ExceptionMessages.WINNING_NUMBER_FORMAT_EXCEPTION_MESSAGE;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.WinningNumber;
 
 public class StringParser {
+    String DELIMITER = ",";
 
     public Integer parsePurchaseAmount(String str) {
         try {
@@ -11,6 +17,18 @@ public class StringParser {
             return parsed;
         } catch (Exception e) {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_FORMAT_EXCEPTION_MESSAGE.toString());
+        }
+    }
+
+    public WinningNumber parseWinningNumber(String str) {
+        try {
+            List<Integer> parsed = Arrays.asList(str.split(DELIMITER)).stream()
+                    .map(Integer::parseInt)
+                    .toList();
+            Validator.validatePositiveIntegerList(parsed);
+            return new WinningNumber(parsed);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(WINNING_NUMBER_FORMAT_EXCEPTION_MESSAGE.toString());
         }
     }
 }

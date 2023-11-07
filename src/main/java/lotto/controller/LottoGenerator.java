7394eddc897c41604f790cd8fbb645491c2cc785
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-07 23:00:55 +0900

    refact(lottocontroller): service와 controller 분리

---
+++ src/main/java/lotto/controller/LottoGenerator.java
@@ -0,0 +1,17 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.utils.Constants;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoGenerator {
+    public static List<Integer> generateLotto(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Constants.MIN_LOTTO_NUMBER.getValues(),
+                Constants.MAX_LOTTO_NUMBER.getValues(),
+                Constants.LOTTO_COUNT.getValues());
+        return numbers.stream().sorted().toList();
+    }
+}

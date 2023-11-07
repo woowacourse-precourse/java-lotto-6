1c17bcc8634c84ea56abb9fcec9140f0e1fbcaa7
hoyeon <highoption@naver.com>
2023-11-07 20:34:09 +0900

    feat(OutputView) : printResult 메서드에 "당첨 통계" 추가
    - WinningResult에 결과값들을 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -18,6 +18,8 @@ public class OutputView {
     }
 
     public static void printResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
         WinningResult.showResult();
     }
 }

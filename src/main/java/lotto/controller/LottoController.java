ecb05d220157876a26ceaa919f295edd47b95469
noteasymin <noteasymin@gmail.com>
2023-11-07 23:54:30 +0900

    fix: 개행 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,6 +12,7 @@ import static lotto.service.LottoService.generateLottoList;
 public class LottoController {
     public static void playLotto() {
         Integer numberOfLotto = LottoView.getNumberOfLottoForPrice();
+        System.out.println();
         System.out.println(numberOfLotto + "개를 구매했습니다.");
 
         List<Lotto> lottoList = generateLottoList(numberOfLotto);

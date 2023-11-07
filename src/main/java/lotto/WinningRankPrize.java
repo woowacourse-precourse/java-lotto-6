d6071c7ba734b513468cee07392747dbb04231ba
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 16:03:16 +0900

    fix(WinningRankPrize): 수익률 계산을 사용하기 위해서, 객체의 생성자에 rank항목 추가 => 이후 winRecordBoard hashMap의 key값으로 쓸 수 있음

--- src/main/java/lotto/WinningRankPrize.java
+++ src/main/java/lotto/WinningRankPrize.java
@@ -2,16 +2,18 @@ package lotto;
 
 public enum WinningRankPrize {
 
-    FIFTH("3개 일치", "5,000"),
-    FOURTH("4개 일치", "50,000"),
-    THIRD("5개 일치", "1,500,000"),
-    SECOND("5개 일치, 보너스 볼 일치", "30,000,000"),
-    FIRST("6개 일치", "2,000,000,000");
+    FIFTH(5, "3개 일치", "5,000"),
+    FOURTH(4, "4개 일치", "50,000"),
+    THIRD(3, "5개 일치", "1,500,000"),
+    SECOND(2, "5개 일치, 보너스 볼 일치", "30,000,000"),
+    FIRST(1, "6개 일치", "2,000,000,000");
 
+    private int rank;
     private String matchCount;
     private String prizeAmount;
 
-    WinningRankPrize(String matchCount, String prizeAmount) {
+    WinningRankPrize(int rank, String matchCount, String prizeAmount) {
+        this.rank = rank;
         this.matchCount = matchCount;
         this.prizeAmount = prizeAmount;
     }

12e6cf7e1270caeb9db18a644bab10999638417e
ppusda <ppusda1234@gmail.com>
2023-11-08 03:14:01 +0900

    feat: 구입금액 정수 입력만 받도록 예외처리

--- src/main/java/lotto/Model/Util/LottoUtil.java
+++ src/main/java/lotto/Model/Util/LottoUtil.java
@@ -1,25 +1,39 @@
 package lotto.Model.Util;
 
 import System.Constant;
+import System.Exception;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
 import lotto.Model.Domain.Lotto;
 
 public class LottoUtil {
 
     public Lotto pickLottoNumber() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(
+        List<Integer> lottos = Randoms.pickUniqueNumbersInRange(
                 Constant.LOTTO_MIN_VALUE.getConstant(), Constant.LOTTO_MAX_VALUE.getConstant(),
                 Constant.LOTTO_SIZE.getConstant());
 
-        lotto.sort(Comparator.naturalOrder());
+        ArrayList<Integer> sortedLottos = new ArrayList<>(lottos);
+        sortedLottos.sort(Comparator.naturalOrder());
 
-        return new Lotto(lotto);
+        return new Lotto(sortedLottos);
     }
 
-    public boolean checkPurchaseAmount(int money) {
-        return money % Constant.LOTTO_PRICE.getConstant() == 0;
+    public void validatePurchaseAmount(String purchaseMoney) {
+        if(!Pattern.matches("\\d+", purchaseMoney)) {
+            throw new IllegalArgumentException(Exception.LOTTO_PURCHASE_EXCEPTION.getMessage());
+        }
+    }
+
+    public void checkPurchaseAmount(int money) {
+        if(money % Constant.LOTTO_PRICE.getConstant() != 0) {
+            throw new IllegalArgumentException(Exception.LOTTO_PURCHASE_EXCEPTION.getMessage());
+        }
     }
 
     public int calculateCount(int money) {

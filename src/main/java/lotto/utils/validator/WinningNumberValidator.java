d7364a2ff3604f529aa5d21e38c8f729acb90b00
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 16:06:23 +0900

    feat(WinningNumberValidator): 입력 가능 최대 길이를 검증하는 기능

--- src/main/java/lotto/utils/validator/WinningNumberValidator.java
+++ src/main/java/lotto/utils/validator/WinningNumberValidator.java
@@ -4,13 +4,22 @@ import lotto.utils.message.WinningInformationExceptionMessage;
 
 public class WinningNumberValidator {
 
+    private static final int MAX_LENGTH = 20;
+
     public static void validate(String input) {
         validateBlank(input);
+        validateSize(input);
     }
 
     private static void validateBlank(String target) {
         if (target == null || target.isBlank()) {
             throw new IllegalArgumentException(WinningInformationExceptionMessage.BLANK_WINNING_NUMBER.getError());
+        }
+    }
+
+    private static void validateSize(String target) {
+        if (target.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(WinningInformationExceptionMessage.EXCEED_LENGTH.getError());
         }
     }
 }

c06f907103260236ec4116bffed092b58b6b4999
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 00:16:14 +0900

    fix: 천원 단위 유효성 확인 함수 validPurchaseDivideUp1000 수정

--- src/main/java/validator/LottoPurchaseValidator.java
+++ src/main/java/validator/LottoPurchaseValidator.java
@@ -16,7 +16,7 @@ public class LottoPurchaseValidator {
     }
 
     void validPurchaseDivideUp1000(int purchaseAmount) {
-        if (purchaseAmount % 1000 == 0) {
+        if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1000원 단위여야 합니다.");
         }
     }

883cb45934f2e17cb5ca768393277aedc8309307
Concepty <khs1903b@gmail.com>
2023-11-07 13:54:06 +0900

    test(Lotto): Lotto 클래스 유닛 테스트 추가
    다음 기능의 유닛 테스트 추가
    - 로또 번호 전체 자동 생성이 기준을 만족하는지 테스트
    - 로또 번호 하나 생성이 기준을 만족하는지 테스트
    - 주어진 숫자들과 로또 오브젝트의 숫자들 중 몇 개가 일치하는지 반환하는 기능 테스트
    - 주어진 숫자가 로또 오브젝트의 숫자들에 포함되는지 확인하는 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,20 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +32,63 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 자동 생성시 1 ~ 45 범위의 서로 다른 6개의 번호를 생성한다.")
+    @RepeatedTest(10)
+    void checkGeneratedLottoNumbers() {
+        List<Integer> numbers = Lotto.generateLottoNumbers();
+        assertEquals(6, numbers.size());
+        assertEquals(6, new HashSet(numbers).size());
+        assertTrue(numbers.stream().allMatch(n -> n <= 45 && n >= 1));
+    }
+
+    @DisplayName("로또 번호 범위 숫자 생성시 1~45 범위의 정수를 생성한다.")
+    @RepeatedTest(10)
+    void checkGeneratedOneLottoNumber() {
+        Integer number = Lotto.generateOneLottoNumber();
+        assertTrue(number <= 45 && number >=1);
+    }
+
+    @DisplayName("Lotto의 numbers와 주어진 숫자들의 일치하는 숫자를 센다.")
+    @ParameterizedTest
+    @MethodSource("provideTestLotto")
+    void checkLottoMatch(List<Integer> testTickets, int expected) {
+        // TODO: refactor
+        Lotto testLotto = new Lotto(List.of(5, 10, 15, 20, 25, 30));
+
+        int actual = testLotto.countMatchingNumbers(testTickets);
+
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideTestLotto() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 1),
+                Arguments.of(List.of(23, 12, 22, 33, 14, 7), 0),
+                Arguments.of(List.of(30, 25, 20, 15, 5, 10), 6),
+                Arguments.of(List.of(1,23,5,10,2,45), 2)
+        );
+    }
+
+    @DisplayName("Lotto의 number에 해당 숫자가 포함되어 있는지 판단")
+    @ParameterizedTest
+    @MethodSource("provideTestNumber")
+    void checkNumberMatch(Integer testNumber, boolean expected) {
+        // TODO: refactor
+        Lotto testLotto = new Lotto(List.of(5, 10, 15, 20, 25, 30));
+
+        boolean actual = testLotto.numbersContain(testNumber);
+
+        assertEquals(expected, actual);
+    }
+
+
+    static Stream<Arguments> provideTestNumber() {
+        return Stream.of(
+                Arguments.of(1, false),
+                Arguments.of(2, false),
+                Arguments.of(5, true),
+                Arguments.of(10, true),
+                Arguments.of(34, false)
+        );
+    }
 }

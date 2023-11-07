43005b41730bbb31c29174e892a0572cbf5583e0
seona-moon <fhflwhwl5@gmail.com>
2023-11-07 10:07:55 +0900

    feat(LottoNumberGenerator): Lotto Generate 구현 및 테스트 통과

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -1,5 +1,10 @@
-package lotto.Util;
+package lotto.util;
 
-public class LottoNumberGenerator implements NumberGenerator{
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
+public class LottoNumberGenerator {
+    public static List<Integer> generateNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }

0bad33d82e66cbd1c6ecdd185af3ad5c0666a9b9
Jung Yunho <a01049048063@gmail.com>
2023-11-08 01:49:28 +0900

    feat(Ticket): 값이 없으면 예외처리

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -1,14 +1,21 @@
 package lotto.domain;
 
+import lotto.constant.ExceptionMessage;
+
 public class Ticket {
 
     private final Integer value;
 
-    public Ticket(Integer value) {
+    public Ticket(final Integer value) {
         this.value = value;
     }
 
     public Integer toValue() {
+        validateNullTicket(value);
         return value;
+    }
+
+    public void validateNullTicket(final Integer value) {
+        throw new IllegalStateException(ExceptionMessage.INVALID_NULL.toMessage());
     }
 }

486d02addf38aaf3ceeda17aaa369625bf7ac297
Dowon <3120kang@gmail.com>
2023-11-07 11:01:38 +0900

    Feat(ResultRepository): 순위로 당첨 횟수를 불러오는 기능 구현

--- src/main/java/lotto/domain/ResultRepository.java
+++ src/main/java/lotto/domain/ResultRepository.java
@@ -1,21 +1,24 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
 
 public class ResultRepository {
 
-    private Map<Rank,Integer> board=new EnumMap<>(Rank.class);
+    private static Map<Rank, Integer> board = new EnumMap<>(Rank.class);
 
-    public ResultRepository(){
-        board.put(Rank.FIRST,0);
-        board.put(Rank.SECOND,0);
-        board.put(Rank.THIRD,0);
-        board.put(Rank.FOURTH,0);
-        board.put(Rank.FIFTH,0);
+    public void init() {
+        Arrays.stream(Rank.values())
+                .forEach(rank -> board.put(rank, 0));
     }
 
-    public void save(Rank rank){
-        board.put(rank,board.get(rank)+1);
+    public void save(Rank rank) {
+        board.put(rank, board.get(rank) + 1);
     }
+
+    public Integer findWinningByRank(Rank rank) {
+        return board.get(rank);
+    }
+
 }

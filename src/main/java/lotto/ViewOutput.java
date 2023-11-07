6332f3b4ac53c5c1aa8cc239770d1bc3ccab85d0
GumaLee <rkdxhd97@naver.com>
2023-11-07 18:20:01 +0900

    Feat: 출력기능_로또목록

--- src/main/java/lotto/ViewOutput.java
+++ src/main/java/lotto/ViewOutput.java
@@ -1,5 +1,24 @@
 package lotto;
 
-public class ViewOutput {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ViewOutput {
+    private static StringBuilder stringBuilder = new StringBuilder();
+
+    public void showLottoList(List<Lotto> lottoList) {
+        for(Lotto lotto : lottoList) {
+            stringBuilder.append("[");
+
+            for(int index = 0; index < 6; index++) {
+                List<Integer> lottoNumbers = new ArrayList<>();
+                stringBuilder.append(lottoNumbers.get(index) + ", ");
+            }
+            stringBuilder.setLength(stringBuilder.length() - 2);
+            stringBuilder.append("]\n");
+        }
+
+        System.out.println(stringBuilder);
+        stringBuilder.setLength(0);
+    }
 }

d65737ad2ba806fe48fe179111fa9b3fb1c82449
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:57:15 +0900

    feat(LottoController): 당첨번호, 보너스번호를 입력받는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoPurchase;
 import lotto.domain.Money;
+import lotto.domain.WinningNumber;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private Money money;
+    private WinningNumber winningNumber;
     private List<Lotto> lottos;
 
     public void start() {
@@ -22,6 +24,8 @@ public class LottoController {
 
         lottos = buyLotto(purchaseCount);
         OutputView.showPurchaseLotto(lottos);
+
+        winningNumber = makeWinningNumber();
     }
 
     private Money putMoney(long money) {
@@ -44,5 +48,39 @@ public class LottoController {
 
     private List<Lotto> buyLotto(int purchaseCount) {
         return LottoPurchase.buyLotto(purchaseCount);
+    }
+
+    private WinningNumber makeWinningNumber() {
+        Lotto winningLotto = inputWinningNumber();
+        int bonusNumber = inputBonusNumber();
+
+        return getWinningNumber(winningLotto, bonusNumber);
+    }
+
+    private Lotto inputWinningNumber() {
+        try {
+            return new Lotto(InputView.inputWinningNumber());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinningNumber();
+        }
+    }
+
+    private int inputBonusNumber() {
+        try {
+            return InputView.inputBonusNumber();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputBonusNumber();
+        }
+    }
+
+    private WinningNumber getWinningNumber(Lotto winningLotto, int bonusNumber) {
+        try {
+            return new WinningNumber(winningLotto, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getWinningNumber(winningLotto, inputBonusNumber());
+        }
     }
 }

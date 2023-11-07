5a9ae8c74dc06036b6a7a0f5fdc94377543cc8c1
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-08 01:45:17 +0900

    refactor: 알맞은 데이터 형식으로 출력하기 위한 코드수정

--- src/main/java/lotto/constant/ProgressMessage.java
+++ src/main/java/lotto/constant/ProgressMessage.java
@@ -12,15 +12,15 @@ public final class ProgressMessage {
 
     public static final String OUTPUT_STATISTICS = "당첨 통계\n---";
 
-    public static final String THREE_WINNING = "3개 일치 (5,000원) - ";
+    public static final String THREE_WINNING = "3개 일치 (5,000원) - %d개\n";
 
-    public static final String FOUR_WINNING = "4개 일치 (50,000원) - ";
+    public static final String FOUR_WINNING = "4개 일치 (50,000원) - %d개\n";
 
-    public static final String FIVE_WINNING = "5개 일치 (1,500,000원) - ";
+    public static final String FIVE_WINNING = "5개 일치 (1,500,000원) - %d개\n";
 
-    public static final String BONUS_WINNING = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String BONUS_WINNING = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
 
-    public static final String SIX_WINNING = "6개 일치 (2,000,000,000원) - ";
+    public static final String SIX_WINNING = "6개 일치 (2,000,000,000원) - %d개\n";
 
-    public static final String RATE_OF_RETURN = "총 수익률은 ()입니다.";
+    public static final String RATE_OF_RETURN = "총 수익률은 %.1f%%입니다.";
 }

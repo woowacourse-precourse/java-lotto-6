f366e17cd3e7eb8a173e49fc215ed58df6b373db
Controllls <whdrms9459@naver.com>
2023-11-07 11:05:16 +0900

    refactor : 당첨금 enum 추가

--- src/main/java/lotto/domain/winningManager/RateOfReturn.java
+++ src/main/java/lotto/domain/winningManager/RateOfReturn.java
@@ -1,6 +1,7 @@
 package lotto.domain.winningManager;
 
 import lotto.constants.LottoConstants;
+import lotto.constants.LottoDrawConstants;
 
 import java.util.HashMap;
 
@@ -13,11 +14,11 @@ public class RateOfReturn {
         Integer rank3Count = ranks.get(3);
         Integer rank2Count = ranks.get(2);
         Integer rank1Count = ranks.get(1);
-        double totalReturn = rank1Count * 2000000000
-                + rank2Count * 30000000
-                + rank3Count * 1500000
-                + rank4Count * 50000
-                + rank5Count * 5000;
+        double totalReturn = rank1Count * LottoDrawConstants.FIRST_PRIZE_AMOUNT.getValue()
+                + rank2Count * LottoDrawConstants.SECOND_PRIZE_AMOUNT.getValue()
+                + rank3Count * LottoDrawConstants.THIRD_PRIZE_AMOUNT.getValue()
+                + rank4Count * LottoDrawConstants.FOURTH_PRIZE_AMOUNT.getValue()
+                + rank5Count * LottoDrawConstants.FIFTH_PRIZE_AMOUNT.getValue();
 
         return totalReturn / (lottoCount * LottoConstants.LOTTO_PRICE.getValue()) * 100;
     }

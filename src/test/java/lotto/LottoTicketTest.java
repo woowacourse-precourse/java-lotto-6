fca6a8e63a3b3c88e65ea9fe4342fff91349bfa5
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 22:51:24 +0900

    refactor : PurchasePrice 이름 Budget 으로 변경

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -2,7 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import lotto.model.PurchasePrice;
+import lotto.model.Budget;
 import lotto.model.lotto.LottoTicket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +11,7 @@ public class LottoTicketTest {
     @DisplayName("로또 수량은 금액/로또_금액 이어야 한다.")
     @Test
     void createLottoTicketTest(){
-        PurchasePrice price = new PurchasePrice(3000);
+        Budget price = new Budget(3000);
         LottoTicket lottoTicket = new LottoTicket(price);
         assertThat(lottoTicket.getLottoTicketSize()).isEqualTo(3);
     }

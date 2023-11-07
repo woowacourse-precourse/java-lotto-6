c9f150fc0f5ea33a7e1162e7f02cdc8495d5da1a
Sung Hyobin <hellmir610@gmail.com>
2023-11-08 08:38:49 +0900

    test: Remove forth test case of createLottoByProperNumbers in LottoTest class

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -38,19 +38,16 @@ class LottoTest {
         List<Integer> numbers1 = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> numbers2 = List.of(40, 41, 42, 43, 44, 45);
         List<Integer> numbers3 = List.of(1, 3, 16, 28, 37, 45);
-        List<Integer> numbers4 = List.of(2, 8, 10, 15, 22, 36);
 
         // when
         Lotto lotto1 = new Lotto(numbers1);
         Lotto lotto2 = new Lotto(numbers2);
         Lotto lotto3 = new Lotto(numbers3);
-        Lotto lotto4 = new Lotto(numbers4);
 
         // then
         assertThat(lotto1.getNumbers()).isEqualTo(numbers1);
         assertThat(lotto2.getNumbers()).isEqualTo(numbers2);
         assertThat(lotto3.getNumbers()).isEqualTo(numbers3);
-        assertThat(lotto4.getNumbers()).isEqualTo(numbers4);
     }
 
     @DisplayName("로또 번호가 1에서 45 사이의 값이 아니면 예외가 발생한다.")

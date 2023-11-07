db5d6e07b2c160de430b9929fc9a927a8aa643d9
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-08 02:26:51 +0900

    feat(controller/LottoController) : 기능16. 구입 금액을 제대로 입력했는지 검증한다 (기능17, 기능18) 한 번에 구현
    validateReceivedMoney 메서드는 다음 두가지 사항을 검증하고 검증에 통과하지 못한 경우 예외를 발생시키는 메서드 입니다.
    
    📌 검증내용
    기능17. 입력된 값이 숫자가 아니면 예외가 발생한다.
    기능18. 로또 구입 금액이 1000원 단위인지 체크한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,27 @@
 package lotto.controller;
 
+import static lotto.utils.CalculationUtils.isDivisible;
 import static lotto.utils.StringUtils.parseInt;
+import static lotto.view.ErrorMessage.RECEIVED_MONEY_NOT_MULTIPLE_OF_1000;
 import static lotto.view.InputMessage.HOW_MUCH_MONEY_FOR_LOTTO_PURCHASE;
 import static lotto.view.InputView.readInput;
 
 public class LottoController {
+    public static final int ONE_LOTTO_PRICE = 1000;
+
     public int receiveMoney() {
         String userInput = readInput(HOW_MUCH_MONEY_FOR_LOTTO_PURCHASE.getInputMessage());
+        validateReceivedMoney(userInput);
         return parseInt(userInput);
     }
+
+    private void validateReceivedMoney(String userInput) {
+        // 숫자 인지 검증
+        int number = parseInt(userInput);
+
+        if (!isDivisible(number, ONE_LOTTO_PRICE)) {
+            throw new IllegalArgumentException(RECEIVED_MONEY_NOT_MULTIPLE_OF_1000.getErrorMessage());
+        }
+    }
+
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import static lotto.domain.ErrorMessage.*;
+import static lotto.view.ErrorMessage.*;
 import static lotto.utils.CalculationUtils.hasDuplicates;
 import static lotto.utils.CalculationUtils.isNumberInRange;
 

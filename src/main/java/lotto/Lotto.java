88b7174e3a2b19941aed254bfca611a6ef86b2f1
nohheeyeon <heeyeon1213@gmail.com>
2023-11-07 17:01:35 +0900

    chore : 개별 로또 번호 세트 표현

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import java.util.List;
+
+import java.util.*;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6 || !isAllUnique(numbers) || !isWithinRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자 중 중복되지 않게 6개를 입력해야 합니다.");
+        }
+    }
+
+    private boolean isAllUnique(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        return numberSet.size() == numbers.size();
+    }
+
+    private boolean isWithinRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(number -> number >= 1 && number <= 45);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
+

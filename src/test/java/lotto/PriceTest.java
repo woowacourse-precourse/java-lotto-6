fd97b0cafb862c51d806be86decfde26bdd796d5
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 11:29:18 +0900

    refactor(PriceTest): 테스트의 행동을 명확하게 작성 및 중복 제거 * 중복 제거에 @BeforeEach 사용

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -4,15 +4,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import lotto.Model.Price;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PriceTest {
     private Price price;
 
-    @DisplayName("Price는 숫자로만 이루어져야 한다.")
+    @DisplayName("Price로 문자가 주어지면 예외 발생 테스트.")
     @Test
-    void isNumber1() {
+    void checkNumber1() {
         String input = "5000원";
 
         assertThatThrownBy(() -> new Price(input))
@@ -20,19 +21,9 @@ public class PriceTest {
                 .hasMessage("[ERROR] 공백이나 쉼표같은 특수기호 없이 숫자만을 입력하세요.(최대 구입금액은 20억입니다)");
     }
 
-    @DisplayName("Price가 20억~int형 타입 사이일 때 테스트")
+    @DisplayName("Price가 int타입 넘어가면 예외 발생 테스트")
     @Test
-    void isNumber2() {
-        String input = "2000000001";
-
-        assertThatThrownBy(() -> new Price(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 로또구입금액은 최대 20억입니다.");
-    }
-
-    @DisplayName("Price가 int타입 넘어갈 때 테스트")
-    @Test
-    void isNumber3() {
+    void checkNumber2() {
         String input = "4000000001";
 
         assertThatThrownBy(() -> new Price(input))
@@ -40,19 +31,19 @@ public class PriceTest {
                 .hasMessage("[ERROR] 공백이나 쉼표같은 특수기호 없이 숫자만을 입력하세요.(최대 구입금액은 20억입니다)");
     }
 
-    @DisplayName("Price가 1000원 단위로 떨어지지 않을 때")
+    @DisplayName("Price가 한 개의 로또 가격 단위로 떨어지지 않으면 예외 발생 테스트")
     @Test
-    void isDivideWithOneThousands() {
-        String input = "1500";
+    void checkDividingUnitOfOneLottoPrice() {
+        String input = "500";
 
         assertThatThrownBy(() -> new Price(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1000원 단위로 입력하세요.");
     }
 
-    @DisplayName("Price가 0원 이하일 때")
+    @DisplayName("Price가 0원이나 음수면 예외 발생하는 테스트")
     @Test
-    void isCorrectRange() {
+    void checkValidRange1() {
         String input = "0";
 
         assertThatThrownBy(() -> new Price(input))
@@ -60,19 +51,48 @@ public class PriceTest {
                 .hasMessage("[ERROR] 로또구입금액은 최소 1000원입니다.");
     }
 
-    @DisplayName("Price가 1000원 단위로 나눠지는지")
+    @DisplayName("Price가 0원이나 음수면 예외 발생하는 테스트")
     @Test
-    void divideWithOneThousands() {
-        price = new Price("5000");
+    void checkValidRange2() {
+        String input = "-1000";
 
-        assertThat(price.divideWithOneThousands()).isEqualTo(5);
+        assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또구입금액은 최소 1000원입니다.");
     }
 
-    @DisplayName("당첨금액/구매금액 * 100을 해서 수익률이 잘 생성되는지 테스트")
+    @DisplayName("Price가 1000원으로 떨어지나 로또 최대 구입 가격인 20억~int타입 사이면 예외 발생하는 테스트")
     @Test
-    void divideWith() {
-        price = new Price("8000");
+    void checkValidRange3() {
+        String input = "2000001000";
 
-        assertThat(price.divideWith(5000)).isEqualTo(62.5);
+        assertThatThrownBy(() -> new Price(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또구입금액은 최대 20억입니다.");
+    }
+
+    @BeforeEach
+    void set() {
+        price = new Price("8000");
+    }
+
+    @DisplayName("Price에 따라 구매한 로또 개수를 집계하는 테스트")
+    @Test
+    void countPurchasedLotto() {
+        int expectedCountingLottos = price.countPurchasedLotto();
+        int actualCountingLottos = 8;
+
+        assertThat(expectedCountingLottos).isEqualTo(actualCountingLottos);
+    }
+
+    @DisplayName("당첨금액에 따라 수익률이 만들어지는지 테스트")
+    @Test
+    void makeWinningRate() {
+        int totalWinningPrize = 5000;
+
+        double actualWinningRate = price.makeWinningRate(totalWinningPrize);
+        double expectedWinningRate = 62.5d;
+
+        assertThat(actualWinningRate).isEqualTo(expectedWinningRate);
     }
 }

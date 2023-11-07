26b56dbd669a9b08b09be5abc7f91c5f8833d844
Joohui <joohuii96@gmail.com>
2023-11-07 18:44:57 +0900

    style: 에러메시지에 [ERROR] 문구 추가

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -2,10 +2,10 @@ package lotto.constants;
 
 public enum ErrorMessage {
 
-    PURCHASE_AMOUNT_WRONG_UNIT_INPUT("1,000원 단위로 입력해 주세요."),
-    PURCHASE_AMOUNT_NOT_DIGIT("숫자만 입력 가능합니다."),
-    PURCHASE_AMOUNT_ZERO("0보다 큰 금액을 입력해 주세요."),
-    PURCHASE_AMOUNT_NULL_INPUT("값을 입력해 주세요.");
+    PURCHASE_AMOUNT_WRONG_UNIT_INPUT("[ERROR] 1,000원 단위로 입력해 주세요."),
+    PURCHASE_AMOUNT_NOT_DIGIT("[ERROR] 숫자만 입력 가능합니다."),
+    PURCHASE_AMOUNT_ZERO("[ERROR] 0보다 큰 금액을 입력해 주세요."),
+    PURCHASE_AMOUNT_NULL_INPUT("[ERROR] 값을 입력해 주세요.");
 
     private String message;
 

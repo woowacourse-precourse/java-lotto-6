2a7dc67ccd29dd3c99b282e397cb0e529aba65e2
Sanghwi Lee <sanghwi47@naver.com>
2023-11-08 00:18:37 +0900

    refactor(OutputView) Lotto 결과 출력 메소드의 구조 개선, 결과 출력 시 하드코딩 되어있던 코드들을 상수처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,10 +9,19 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String TICKET_COUNT_MESSAGE ="개를 구매했습니다.";
+    private static final String TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String SYSTEM_MESSAGE_WINNING_STATIC = "당첨 통계";
     private static final String EARNING_RATE = "총 수익률은 %.1f%%입니다.";
     private static final String SYSTEM_MESSAGE_DASH = "---";
+    private static final String RESULT_FORMAT = "%s (%,d원) - %d개";
+    private static final LottoPrize[] prizesInOrder = {
+            LottoPrize.THREE_MATCH,
+            LottoPrize.FOUR_MATCH,
+            LottoPrize.FIVE_MATCH,
+            LottoPrize.FIVE_AND_BONUS_MATCH,
+            LottoPrize.SIX_MATCH
+    };
+
     public static void displayTicket_Count(int ticketCount) {
         System.out.println();
         System.out.println(ticketCount + TICKET_COUNT_MESSAGE);
@@ -32,18 +41,15 @@ public class OutputView {
         System.out.println(SYSTEM_MESSAGE_WINNING_STATIC);
         System.out.println(SYSTEM_MESSAGE_DASH);
     }
-    public static void displayLottoResult(EnumMap<LottoPrize, Integer> winCount) {
-        LottoPrize[] prizesInOrder = {
-                LottoPrize.THREE_MATCH,
-                LottoPrize.FOUR_MATCH,
-                LottoPrize.FIVE_MATCH,
-                LottoPrize.FIVE_AND_BONUS_MATCH,
-                LottoPrize.SIX_MATCH
-        };
 
+    public static void displayLottoResult(EnumMap<LottoPrize, Integer> winCount) {
         for (LottoPrize prize : prizesInOrder) {
             int count = winCount.getOrDefault(prize, 0);
-            System.out.println(prize.getDescription() + " (" + String.format("%,d", prize.getPrize()) + "원) - " + count + "개");
+            String result = String.format(RESULT_FORMAT,
+                    prize.getDescription(),
+                    prize.getPrize(),
+                    count);
+            System.out.println(result);
         }
     }
 

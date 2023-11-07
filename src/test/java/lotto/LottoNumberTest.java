2c29c44cb6752aa4ed769ea9e62ad8c690fa2c23
K-KY <kyuyoungk@naver.com>
2023-11-08 08:39:19 +0900

    Test(LottoNumberTest) : 맞춘 번호 테스트

---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberTest {
+
+    @Test
+    void 맞춘_로또_번호() {
+        LottoNumber lottoNumber = new LottoNumber(List.of(1,2,3,4,5,6));
+        assertThat(lottoNumber.calculateScore(1)).isTrue();
+    }
+
+}

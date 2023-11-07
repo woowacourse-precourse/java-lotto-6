0aa5add4819530c5bb13fd3a4fed7cef448fd531
inpink <inpink@kakao.com>
2023-11-07 23:30:07 +0900

    refactor: Money끼리 더하기는 int값을 반환

--- src/main/java/lotto/domain/ThousandUnitMoney.java
+++ src/main/java/lotto/domain/ThousandUnitMoney.java
@@ -23,8 +23,12 @@ public class ThousandUnitMoney {
         return new ThousandUnitMoney(IntegerUtil.trimAndParseInt(input));
     }
 
-    public ThousandUnitMoney plus(ThousandUnitMoney money) {
-        return new ThousandUnitMoney(this.amount + money.amount);
+    public boolean isSameAmount(int amount) {
+        return this.amount == amount;
+    }
+
+    public int plus(ThousandUnitMoney money) {
+        return this.amount + money.amount;
     }
 
     public boolean isSameAmount(int amount) {

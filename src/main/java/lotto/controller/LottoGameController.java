78677921e3d81689e9e2a40b9742ddf5d94957f5
Sung Hyobin <hellmir610@gmail.com>
2023-11-08 03:54:38 +0900

    refactor: Delegate instruction of printing function from LottoGameController to PrintHandler

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -6,21 +6,16 @@ import lotto.service.LottoGameService;
 import lotto.service.LottoGameServiceImpl;
 import lotto.view.InputHandler;
 import lotto.view.PrintHandler;
-import lotto.view.Printer;
 
 public class LottoGameController {
     private static final LottoGameService lottoGameService = new LottoGameServiceImpl();
 
     public static void startLottoGame() {
         String purchaseAmount = InputHandler.getPurchaseInputMessage();
+
         LottoCount lottoCount = lottoGameService.computeLottoCount(purchaseAmount);
-
-        Printer.printLineBreak();
-
-        Printer.printLottoCount(lottoCount);
         Lottos lottos = lottoGameService.generateLottos(lottoCount);
-        PrintHandler.printLottos(lottos);
 
-        Printer.printLineBreak();
+        PrintHandler.printStartingPartOfLottoGame(lottoCount, lottos);
     }
 }
--- src/main/java/lotto/view/PrintHandler.java
+++ src/main/java/lotto/view/PrintHandler.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import lotto.domain.LottoCount;
 import lotto.domain.Lottos;
 
 public class PrintHandler {
-    public static void printLottos(Lottos lottos) {
+    public static void printStartingPartOfLottoGame(LottoCount lottoCount, Lottos lottos) {
+        Printer.printLineBreak();
+
+        Printer.printLottoCount(lottoCount);
+        printLottos(lottos);
+
+        Printer.printLineBreak();
+    }
+
+    private static void printLottos(Lottos lottos) {
         for (int i = 0; i < lottos.size(); i++) {
             Printer.printLotto(lottos.get(i));
         }

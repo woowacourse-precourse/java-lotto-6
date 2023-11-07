14107b3eea5519425a0790f6e83040a905d9dc04
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 01:38:31 +0900

    refactor: Lotto 클래스 패키지 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,7 @@ import controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-        
+
         lottoController.gameStart();
     }
 }

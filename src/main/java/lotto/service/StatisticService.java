ef977b468181861bc26c472df8fc974f2f106032
Ukj0ng <xb253q@gmail.com>
2023-11-07 18:42:38 +0900

    feat(StatisticService): 로또 당첨에 대한 통계를 계산하는 로직 구현
    구현한 기능:
    - 등수에 따른 당첨 횟수 계산
    - 구매한 금액과 당첨금을 비교해 수익률 계산

--- src/main/java/lotto/service/StatisticService.java
+++ src/main/java/lotto/service/StatisticService.java
@@ -1,2 +1,28 @@
-package lotto.service;public class StatisticService {
+package lotto.service;
+
+import java.util.Map;
+import lotto.model.Lotto;
+import lotto.model.LottoPrize;
+import lotto.model.LottoStatistic;
+
+public class StatisticService {
+
+    public LottoStatistic calculateLottoStatistics(Map<Lotto, LottoPrize> results) {
+        LottoStatistic lottoStatistic = new LottoStatistic();
+        results.values().forEach(prize -> {
+            if (prize != null) {
+                lottoStatistic.addStatistic(prize);
+            }
+        });
+        return lottoStatistic;
+    }
+
+    public double calculateYield(Map<Lotto, LottoPrize> results, int lottoPrice) {
+        double totalPrice = results.size() * lottoPrice;
+        double totalPrize = results.values().stream()
+                                    .filter(prize -> prize != null)
+                                    .mapToDouble(LottoPrize::getPrize)
+                                    .sum();
+        return Math.round(totalPrize / totalPrice * 10.0) / 10.0;
+    }
 }

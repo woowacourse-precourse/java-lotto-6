54b91992750b924704ffee3d3103aa1cba2192e1
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-07 18:57:28 +0900

    feat(LottoMachine): prints out validation error for wrong input
    - validate if money is divisible by thousand
    - validate if there is input of input
    - validate if string form is given in lotto number

--- src/main/java/lotto/view/SystemInput.java
+++ src/main/java/lotto/view/SystemInput.java
@@ -17,25 +17,69 @@ public class SystemInput {
 
     public static void validateNone(String purchase) {
         if (purchase.length() == 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력된 값이 없습니다. 다시 입력해주세요.");
         }
     }
 
     public static void validateDivision(String purchase) {
         int money = Integer.parseInt(purchase);
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 금액은 1000으로 나눠 떨어져야 합니다.");
         }
     }
 
     public static String readPickLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        String pickLotto = Console.readLine();
+        validatePickLotto(pickLotto);
+        return pickLotto;
     }
 
-    public static String readBonusNumber() {
+    public static void validatePickLotto(String pickLotto) {
+        validateCorrectInput(pickLotto);
+        validateString(pickLotto);
+        validateNone(pickLotto);
+    }
+
+    public static void validateCorrectInput(String pickLotto) {
+        char lastChar = pickLotto.charAt(pickLotto.length()-1);
+        char firstChar = pickLotto.charAt(0);
+
+        if(lastChar == ",".charAt(0)){
+            throw new IllegalArgumentException("[ERROR] 입력 값이 잘 못 되었습니다. 다시 입력해주세요!");
+        }
+        if (pickLotto.contains(",,") || firstChar == ",".charAt(0)){
+            throw new IllegalArgumentException("[ERROR] 입력 값이 잘 못 되었습니다. 다시 입력해주세요!");
+        }
+
+    }
+
+    public static void validateString(String pickLotto) {
+        String PATTERN = "^[0-9,]+$";
+        if (pickLotto.matches(PATTERN)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자 형식입니다. 다시 입력해주세요!");
+        }
+
+    }
+
+    public static int readBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
+        String pickBonus = Console.readLine();
+        validateBonus(pickBonus);
+        return Integer.parseInt(pickBonus);
+    }
+
+    public static void validateBonus(String pickBonus) {
+        validateBonusNum(pickBonus);
+        validateNone(pickBonus);
+
+    }
+
+    public static void validateBonusNum(String pickBonus) {
+        String PATTERN = "^[0-9,]+$";
+        if (pickBonus.matches(PATTERN)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자 형식입니다. 다시 입력해주세요!");
+        }
     }
 
 }

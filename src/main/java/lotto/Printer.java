98d14d4cab208b0a72d687e6e73324799f3767fb
Identityless <rlandwnd555@naver.com>
2023-11-07 17:53:48 +0900

    fix : countOfLotto 메서드에서 로또 개수를 출력할 때, 직접 금액을 받아 1000으로 나눠 출력하는 것이 아닌 갯수 자체를 받아서 출력하도록 로직 수정

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -1,7 +1,8 @@
 package lotto;
 
+import java.util.List;
+
 public class Printer {
-    private static final int MONEY_UNIT = 1000;
     private static Printer printer;
 
     private Printer() {}
@@ -13,7 +14,7 @@ public class Printer {
         return printer;
     }
 
-    public void countOfLotto(int won) {
-        System.out.println(won/MONEY_UNIT + "개를 구매했습니다.");
+    public void countOfLotto(int numOfLotto) {
+        System.out.println(numOfLotto + "개를 구매했습니다.");
     }
 }

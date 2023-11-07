42a63ff387d2af7b8a4e168ef73c1fa7b2a9ddbc
binary_ho <dfghcvb11@naver.com>
2023-11-07 17:26:55 +0900

    feat : LottoNumber를 Set으로 관리하기 위해 equals와 hashCode 구현

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Objects;
 
 public record LottoNumber(int number) {
 
@@ -20,5 +21,22 @@ public record LottoNumber(int number) {
         if (number < LOTTO_NUMBER_RANGE_START || LOTTO_NUMBER_RANGE_END < number) {
             throw new IllegalArgumentException();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }

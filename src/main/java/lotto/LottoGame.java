3c543f72d2ea9533968d81b1a3444e50052c53e4
i3kae <snoger1234@gmail.com>
2023-11-07 18:42:42 +0900

    feat : 로또 구매 기능 구현 완료 1. 당첨 번호와 보너스 번호에 따라 각 로또에서 맞는 갯수 계산 2. 맞는 갯수에 따라 등수를 계산해 당첨 금액을 계산 3. 당첨 금액을 필드 변수에 저장 4. 각 등수에 해당하는 상금에 대한 상수와 0에 대한 상수 추가 5. 등수별 상금을 관리하는 리스트 생성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lotto.Input.InputType;
@@ -8,11 +9,20 @@ import lotto.Input.InputType;
 public class LottoGame {
     private static final int FIRST = 0;
     private static final int LOTTO_PRICE = 1000;
+    private static final int ZERO = 0;
+    private static final int FIVE_RANK = 5000;
+    private static final int FOUR_RANK = 50000;
+    private static final int THREE_RANK = 1500000;
+    private static final int TWO_RANK = 30000000;
+    private static final int ONE_RANK = 2000000000;
     private Input lottoInput = new Input();
     private List<Lotto> purchasedLotto;
-
+    private final List<Integer> lottoAmountByRank;
+    private Integer winningAmount;
     public LottoGame() {
         purchasedLotto = new ArrayList<>();
+        winningAmount = ZERO;
+        lottoAmountByRank = Arrays.asList(ZERO, ZERO, ZERO, FIVE_RANK, FOUR_RANK, THREE_RANK, ONE_RANK, TWO_RANK);
     }
 
     public void gameStart(){
@@ -24,7 +34,7 @@ public class LottoGame {
         buyLotto(purchaseAmount.get(FIRST));
         winningNumber = lottoInput.input(InputType.WINNING_NUMBERS);
         bonusNumber = lottoInput.input(InputType.BONUS_NUMBER);
-
+        calcWinningAmount(winningNumber, bonusNumber.get(FIRST));
     }
 
     public void buyLotto(Integer purchaseAmount){
@@ -35,5 +45,11 @@ public class LottoGame {
             purchasedLotto.add(new Lotto(makedNumbers));
         }
         Print.printPurchasedLotto(purchasedLotto);
+    }
+
+    public void calcWinningAmount(List<Integer> winningNumber, Integer bonusNumber){
+        for (Lotto lotto : purchasedLotto){
+            winningAmount += lottoAmountByRank.get(lotto.calcMatchNumber(winningNumber, bonusNumber));
+        }
     }
 }

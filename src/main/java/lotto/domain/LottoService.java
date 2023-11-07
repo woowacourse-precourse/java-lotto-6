77d03590b2fee57338a5a78afa841c41220a5be8
Gukin Han <gukin.dev@gmail.com>
2023-11-07 18:07:06 +0900

    refactor(domain): 당첨 통계 계산을 위한 메서드 분리

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -3,41 +3,68 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoService {
-    int numOfFirstPlace = 0;
-    int numOfSecondPlace = 0;
-    int numOfThirdPlace = 0;
-    int numOfFourthPlace = 0;
-    int numOfFifthPlace = 0;
+    private int numOfFirstPlace = 0;
+    private int numOfSecondPlace = 0;
+    private int numOfThirdPlace = 0;
+    private int numOfFourthPlace = 0;
+    private int numOfFifthPlace = 0;
 
-    public void compare(List<Lotto> lottos, List<Integer> winnerNumbers, int bonusNumber) {
-        for (Lotto lotto : lottos) {
-            int matchCount = 0;
+    public void compare(List<Lotto> lottoBundle, List<Integer> winnerNumbers, int bonusNumber) {
+        for (Lotto lotto : lottoBundle) {
             List<Integer> list = lotto.getNumbers();
-            for (int winNum : winnerNumbers) {
-                if (list.contains(winNum)) {
-                    matchCount++;
-                }
-            }
-
-            if (matchCount == 6) {
-                numOfFirstPlace++;
-                continue;
-            } else if (matchCount == 5 && list.contains(bonusNumber)) {
-                numOfSecondPlace++;
-                continue;
-            } else if (matchCount == 5) {
-                numOfThirdPlace++;
-                continue;
-            } else if (matchCount == 4) {
-                numOfFourthPlace++;
-                continue;
-            } else if (matchCount == 3) {
-                numOfFifthPlace++;
-                continue;
-            }
-            continue;
+            int matchCount = getMatchCount(winnerNumbers, list);
+            checkIfWinFirstPlace(matchCount, list, bonusNumber);
         }
     }
+
+    private int getMatchCount(List<Integer> winnerNumbers, List<Integer> list) {
+        int matchCount = 0;
+        for (int winNum : winnerNumbers) {
+            if (list.contains(winNum)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+    public void checkIfWinFirstPlace(int matchCount, List<Integer> list, int bonusNumber) {
+        if (matchCount == 6) {
+            numOfFirstPlace++;
+            return;
+        }
+        checkIfWinSecondPlace(matchCount, list, bonusNumber);
+    }
+
+    public void checkIfWinSecondPlace(int matchCount, List<Integer> list, int bonusNumber) {
+        if (matchCount == 5 && list.contains(bonusNumber)) {
+            numOfSecondPlace++;
+            return;
+        }
+        checkIfWinThirdPlace(matchCount, list, bonusNumber);
+    }
+
+    public void checkIfWinThirdPlace(int matchCount, List<Integer> list, int bonusNumber) {
+        if (matchCount == 5) {
+            numOfThirdPlace++;
+            return;
+        }
+        checkIfWinFourthPlace(matchCount, list, bonusNumber);
+    }
+
+    public void checkIfWinFourthPlace(int matchCount, List<Integer> list, int bonusNumber) {
+        if (matchCount == 4) {
+            numOfFourthPlace++;
+            return;
+        }
+        getCheckIfWinFifthPlace(matchCount, list, bonusNumber);
+    }
+
+    private void getCheckIfWinFifthPlace(int matchCount, List<Integer> list, int bonusNumber) {
+        if (matchCount == 3) {
+            numOfFifthPlace++;
+        }
+    }
+
 
     public long getProfit() {
         long profit = 0;

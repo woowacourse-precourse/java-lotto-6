f1b2c6e4fda31b7e242bc839644131c44cd27a7b
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 01:40:49 +0900

    feat: 구입 금액이 1000으로 나누어떨어지지 않는 경우 예외처리

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -12,9 +12,14 @@ public class Controller {
         money = Integer.parseInt(moneyString);
 
         tryNum = money/1000;
-        
+
         return tryNum;
     }
 
- 
+    public void moneyValidate(int money) {
+        //1000으로 나누어떨어지지 않을 때
+        if (money%1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력해주세요.");
+        }
+    }
 }

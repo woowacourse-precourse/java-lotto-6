c142fc5b48e98eb6c258eb02cd469dfbe5412ca6
KimSangHyeok <tkdgur0906@naver.com>
2023-11-07 23:19:20 +0900

    feat : 당첨 번호 입력 문구 출력 기능 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class LottoView {
     private static final String PURCHASE_PRICE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_COUNT = "개를 구매했습니다.";
+    private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public static void printPurchasePrompt() {
         System.out.println(PURCHASE_PRICE);
@@ -25,4 +26,9 @@ public class LottoView {
                         .collect(Collectors.joining(", ")) + "]")
                 .forEach(System.out::println);
     }
+
+    public static void printWinningPrompt() {
+        System.out.println(WINNING_NUMBER);
+    }
+
 }

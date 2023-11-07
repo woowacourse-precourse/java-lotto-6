b7e0b743406fed0d2d19e3a964e5c6e65e019e6a
hoosa <janghoosa@gmail.com>
2023-11-08 01:03:22 +0900

    test: 불가능한 범위의 숫자 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호에 불가능한 범위의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByIllegalNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     // 아래에 추가 테스트 작성 가능
 }

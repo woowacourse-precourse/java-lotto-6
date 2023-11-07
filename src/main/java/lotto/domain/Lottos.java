411faefd2629387ee1ac40d200041040640f1532
jooda00 <dragon2000a@gmail.com>
2023-11-07 10:06:44 +0900

    feat(Lottos): 당첨 번호와 일치하는 번호 개수로 LottoResult 초기화 기능

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,15 +1,44 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private final List<Integer> winningNumber;
     private final int bonusNumber;
     private Player player;
+    private LottoResult lottoResult = LottoResult.NONE;
+    private int total = 0;
 
     public Lottos(List<Integer> winningNumber, int bonusNumber, Player player) {
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
         this.player = player;
+    }
+
+    public int classifyLottoGrade() {
+        for (int i = 0; i < player.getLotteries().size(); i++) {
+            List<Integer> result = calculateMatchNumbers(i);
+            int size = result.size();
+            if (size == 5 && isBonusNumberIncluded(i)) {
+                size += 5;
+            }
+            this.lottoResult = LottoResult.initiateLottoResult(size);
+            System.out.println(size);
+        }
+        return this.total;
+    }
+
+    private List<Integer> calculateMatchNumbers(int index) {
+        return winningNumber.stream()
+                .filter(num -> player.getLotteries()
+                .get(index).getNumbers().stream()
+                .anyMatch(Predicate.isEqual(num)))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isBonusNumberIncluded(int index) {
+        return player.getLotteries().get(index).getNumbers().contains(bonusNumber);
     }
 }

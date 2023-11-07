e0c2737bd05ee6fa1040aebd437c1cb3c88a10c1
Lee hwee <gee6121@naver.com>
2023-11-07 23:45:10 +0900

    Docs : README 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static class InputPrice {
+        final int money = 1000;
+        int count;
+        int total_price;
+
+        public void game_num() {
+            System.out.println("구입금액을 입력해 주세요.");
+            String price = Console.readLine();
+            total_price = Integer.parseInt(price);
+            count = total_price / money;
+            System.out.printf("%d개를 구매했습니다.", this.count);
+        }
+    }
+
+    public class
 }
+
+

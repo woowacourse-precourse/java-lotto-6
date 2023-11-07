9b39c644aa6a0305a08446a36fb25cf8734e6706
daheeParkk <qkrekgml7414@naver.com>
2023-11-07 22:32:30 +0900

    feat : 구입 금액과 당첨 금액으로 수익률을 계산하는 기능 구현

--- src/main/java/domain/UserLottos.java
+++ src/main/java/domain/UserLottos.java
@@ -1,16 +1,23 @@
 package domain;
 
+import java.text.DecimalFormat;
+
 public class UserLottos {
     private final Lottos lottos = new Lottos();
+    private final int amount;
 
     public UserLottos(int amount) {
-        int count = amount/1000;
-        buyLottos(count);
+        this.amount = amount;
+        buyLottos(amount/1000);
     }
 
     private void buyLottos(int count) {
         for (int i = 0; i < count; i++) {
             lottos.generate();
         }
+    }
+
+    public double calculateRateOfReturn(double winningAmount) {
+        return winningAmount/amount*100;
     }
 }
--- src/test/java/lotto/UserLottosTest.java
+++ src/test/java/lotto/UserLottosTest.java
@@ -1,2 +1,23 @@
-package lotto;public class UserLottosTest {
+package lotto;
+
+import domain.UserLottos;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.DecimalFormat;
+
+public class UserLottosTest {
+
+    @DisplayName("구입 금액과 당첨 금액으로 수익률을 계산하는 기능 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"8000:5000:62.5", "2000:50000:2500", "50000:50000:100"},delimiter = ':')
+    void calculateRateOfReturn(int purchaseAmount, int winningAmount, String result) {
+        UserLottos userLottos = new UserLottos(purchaseAmount);
+        double rateOfReturn = userLottos.calculateRateOfReturn(winningAmount);
+        DecimalFormat df = new DecimalFormat("#.#");
+
+        Assertions.assertThat(df.format(rateOfReturn)).isEqualTo(result);
+    }
 }

e734962cd1bb2c3a932cf08ee13e1ce86ede357a
hgh1472 <hgh1472@naver.com>
2023-11-08 04:17:02 +0900

    test : Lotto객체 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
-package lotto.domain;
+package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -11,7 +12,7 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> Lotto.makeLotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -19,15 +20,23 @@ class LottoTest {
     @Test
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> Lotto.makeLotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 숫자 범위에 있지 않으면 예외가 발생한다.")
     @Test
     void createNotInRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
+        assertThatThrownBy(() -> Lotto.makeLotto(List.of(1, 2, 3, 4, 5, 100)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Lotto 생성 테스트")
+    @Test
+    void makeLottoTest() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lottoTest = Lotto.makeLotto(numbers);
+        assertThat(numbers).isEqualTo(lottoTest.getLottoNumbers());
     }
     // 아래에 추가 테스트 작성 가능
 }

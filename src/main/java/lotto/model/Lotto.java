958e3f043b8753c13583d705f63f14792470d921
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 03:05:28 +0900

    [ADD] 로또 번호 체크

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +14,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != 6 || !isValidNumbers(numbers) || hasDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> seen = new HashSet<>();
+        for (int number : numbers) {
+            if (!seen.add(number)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidNumbers(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }

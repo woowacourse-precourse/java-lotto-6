646f368c7b07a9fef3ec084d46a0a8d8d0982442
Hajuuu <hpt0127@naver.com>
2023-11-07 20:13:27 +0900

    feat(WinningNumber): 예외 처리 메서드 작성
    당첨 번호의 개수가 6개가 아니거나 숫자의 범위가 1~45를 벗어날 경우 예외 발생

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -9,6 +9,8 @@ public class WinningNumber {
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
+    private static final String ERROR_NUMBER_RANGE = "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_NUMBER_SIZE = "[ERROR] 당첨 번호는 6개를 입력해야 합니다.";
     private static List<Long> matchWinningNumber = new ArrayList<>();
     private static List<Long> matchBonusNumber = new ArrayList<>();
 
@@ -18,17 +20,39 @@ public class WinningNumber {
 
     public void inputWinningNumber() {
         System.out.println();
-        System.out.println(WINNING_NUMBER);
-        winningNumbers = toWinningNumberList(readLine());
+        while (true) {
+            try {
+                System.out.println(WINNING_NUMBER);
+                winningNumbers = toWinningNumberList(readLine());
+                checkWinningNumber(winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         System.out.println();
     }
 
+
+    public void checkWinningNumber(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(ERROR_NUMBER_SIZE);
+        }
+
+        for (int i : winningNumbers) {
+            if (i < 1 || i > 45) {
+                throw new IllegalArgumentException(ERROR_NUMBER_RANGE);
+            }
+        }
+
+    }
 
     public void inputBonusNumber() {
         System.out.println(BONUS_NUMBER);
         bonusNumber = Integer.parseInt(readLine());
         System.out.println();
     }
+    
 
     public List<Integer> toWinningNumberList(String numbers) {
         String[] num = numbers.split(",");

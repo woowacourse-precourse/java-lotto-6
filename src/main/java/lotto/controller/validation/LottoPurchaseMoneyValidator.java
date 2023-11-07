f405260439b637d77772444666e01289c64d880c
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 04:31:05 +0900

    feat : 로또 구매 금액에 대한 유효성 검사 코드 추가

--- src/main/java/lotto/controller/validation/LottoPurchaseMoneyValidator.java
+++ src/main/java/lotto/controller/validation/LottoPurchaseMoneyValidator.java
@@ -1,7 +1,31 @@
 package lotto.controller.validation;
 
-public class LottoPurchaseMoneyValidator {
+import lotto.view.ErrorOutputView;
 
+public class LottoPurchaseMoneyValidator {
+	ErrorOutputView errorOutputView = new ErrorOutputView();
+	private static final Integer MONEY_UNIT = 1000;
+	private static final Integer WANTED_REMAINDER = 0;
 	public void checkValidatioOfLottoPurchaseMoney(String scannedPurchaseMoneyString) {
+		checkLottoPurchaseMoneyWithChar(scannedPurchaseMoneyString);
+	}
+
+	private void checkLottoPurchaseMoneyWithChar(String scannedPurchaseMoneyString) {
+		try{
+			Integer.parseInt(scannedPurchaseMoneyString);
+		}
+		catch(Exception exception){
+			errorOutputView.printErrorOfLottoPurchaseMoneyWithChar();
+			throw new IllegalArgumentException();
+		}
+		checkLottoPurchaseMoneyUnit(scannedPurchaseMoneyString);
+	}
+
+	private void checkLottoPurchaseMoneyUnit(String scannedPurchaseMoneyString) {
+		Integer remainder = Integer.parseInt(scannedPurchaseMoneyString) % MONEY_UNIT;
+		if (remainder == WANTED_REMAINDER) {
+			errorOutputView.printErrorOfLottoPurchaseMoneyUnit();
+			throw new IllegalArgumentException();
+		}
 	}
 }

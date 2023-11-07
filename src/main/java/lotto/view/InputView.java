1cfc3e12caf3ea563951241b17fd95cc18c07442
Lee Geun Woo <gamebdy@naver.com>
2023-11-08 02:04:16 +0900

    :sparkles: 변수명의 모호함에 따른 변수명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,29 +2,31 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-
+import static lotto.util.Phrase.InputPhrase.INPUT_COST;
+import static lotto.util.Phrase.InputPhrase.INPUT_ORIGINALNUMBER;
+import static lotto.util.Phrase.InputPhrase.INPUT_BONUS;
 
 public class InputView {
-    private enum InputPhrase{
-        INPUT_COST("구입금액을 입력해 주세요."),
-        INPUT_NUMBER("당첨 번호를 입력해 주세요."),
-        INPUT_BONUS("보너스 번호를 입력해 주세요.");
-        private final String message;
-
-        InputPhrase(String message){
-            this.message = message;
-        }
-    }
+//    private enum InputPhrase{
+//        INPUT_COST("구입금액을 입력해 주세요."),
+//        INPUT_NUMBER("당첨 번호를 입력해 주세요."),
+//        INPUT_BONUS("보너스 번호를 입력해 주세요.");
+//        private final String message;
+//
+//        InputPhrase(String message){
+//            this.message = message;
+//        }
+//    }
 
     public int inputCost(){
-        System.out.println(InputPhrase.INPUT_COST.message);
+        System.out.println(INPUT_COST.printMessage());
         String cost = Console.readLine();
         //exception
         return Integer.parseInt(cost);
     }
 
-    public List<Integer> inputNumber(){
-        System.out.println(InputPhrase.INPUT_NUMBER.message);
+    public List<Integer> inputOriginalNumber(){
+        System.out.println(INPUT_ORIGINALNUMBER.printMessage());
         String number = Console.readLine();
         //exception
         //return number;
@@ -33,7 +35,7 @@ public class InputView {
 
 
     public int inputBonus(){
-        System.out.println(InputPhrase.INPUT_BONUS.message);
+        System.out.println(INPUT_BONUS.printMessage());
         String bonus = Console.readLine();
         //exception
         //return bonus;

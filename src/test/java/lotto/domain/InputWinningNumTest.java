02aae4dace3262ff59110e3639ea00da02bf2c88
74059 <shyeone@naver.com>
2023-11-07 23:51:15 +0900

    test: 보너스 번호가 1~45 사이에 있는 수가 아닐 경우 예외

--- src/test/java/lotto/domain/InputWinningNumTest.java
+++ src/test/java/lotto/domain/InputWinningNumTest.java
@@ -7,11 +7,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputWinningNumTest {
     private static final String BONUS_NUM = "33i";
+    private static final int BONUS_NUM_RANGE_OVER = 666;
 
     @DisplayName("입력 받은 보너스 번호가 숫자가 아니라면 예외가 발생한다.")
     @Test
     void createBonusNumberByNOTInt() {
         assertThatThrownBy(() -> InputWinningNum.inputBonusNumNotInt(BONUS_NUM))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력 받은 보너스 번호가 1 ~ 45 사이에 없다면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByRangeOfNumber() {
+        assertThatThrownBy(() -> InputWinningNum.inputBonusNumNotRange(BONUS_NUM_RANGE_OVER))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

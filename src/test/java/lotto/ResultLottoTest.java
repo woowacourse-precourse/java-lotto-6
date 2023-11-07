975c9a52a6b667f5064c56648464341bc236f095
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 16:22:08 +0900

    test: add check when bonus num under range to result lotto test

---
+++ src/test/java/lotto/ResultLottoTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+
+import lotto.model.ResultLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ResultLottoTest {
+
+    @DisplayName("보너스 번호가 음수면 오류가 발생한다.")
+    @Test
+    void checkBonusNumUnderRange() {
+        assertThatThrownBy(() -> new ResultLotto(List.of(1, 2, 3, 4, 5), 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}

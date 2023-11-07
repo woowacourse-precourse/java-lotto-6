b5b74d90a3e5bbcac8351ab4c91493784e322239
delena0702 <delena0702@gmail.com>
2023-11-08 03:48:49 +0900

    feat: 오류 문구 출력하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,11 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         LottoSimulator simulator = new LottoSimulator();
-        simulator.run();
+        try {
+            simulator.run();
+        } catch (IllegalArgumentException exception) {
+            String message = exception.getMessage();
+            System.out.println("[ERROR] " + message);
+        }
     }
 }

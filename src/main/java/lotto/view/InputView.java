a0dbc4ef35567d48e4760e5f7dc33c95c44a7d13
cyPark95 <parkcy1007@gmail.com>
2023-11-07 21:31:26 +0900

    feat: 보너스 번호 입력 전에 개행 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -23,7 +23,7 @@ public class InputView {
     }
 
     public static String inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }

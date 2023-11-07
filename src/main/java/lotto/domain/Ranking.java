e84f63526e128f1feeef81885b5864c7e68557c9
gumgu <bienlee@naver.com>
2023-11-07 13:04:51 +0900

    [Fix] 당첨 번호와 보너스 번호 비교 오류 수정

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Ranking {
 
     FIRST(6, 0, 2000000000),
@@ -21,13 +23,28 @@ public enum Ranking {
         this.prizeAmount = prizeAmount;
     }
 
-    public static Ranking compareRanking(Integer matchNumbers, Integer bonus) {
-        for (Ranking ranking : values()) {
-            if (ranking.matchNumbers == matchNumbers && ranking.matchBonus == bonus) {
-                return ranking;
-            }
+    public static Ranking compareRanking(Integer matchNumbers, Integer matchBonus) {
+
+        boolean isBonusEffective = (matchNumbers == 5);
+
+        if (isBonusEffective) {
+            return matchNumbersAndBonus(matchNumbers, matchBonus);
         }
-        return Ranking.NON_RANKING;
+        return matchNumbers(matchNumbers);
+    }
+
+    private static Ranking matchNumbers(Integer matchNumbers) {
+        return Arrays.stream(values())
+                .filter(ranking -> ranking.matchNumbers.equals(matchNumbers))
+                .findFirst()
+                .orElse(NON_RANKING);
+    }
+
+    private static Ranking matchNumbersAndBonus(Integer matchNumbers, Integer matchBonus) {
+        return Arrays.stream(values())
+                .filter(ranking -> ranking.matchNumbers.equals(matchNumbers) && ranking.matchBonus.equals(matchBonus))
+                .findFirst()
+                .orElse(NON_RANKING);
     }
 
     public Integer getMatchNumbers() {

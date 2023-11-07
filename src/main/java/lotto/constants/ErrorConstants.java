d1b9bc86dbe7c132ac2abad075a241ec7a075a2f
mingtorr <mingtorr@nave.com>
2023-11-08 01:28:43 +0900

    feat: 사용자가 금액만큼의 로또 번호를 생성시 검증 기능 추가

--- src/main/java/lotto/constants/ErrorConstants.java
+++ src/main/java/lotto/constants/ErrorConstants.java
@@ -4,7 +4,10 @@ public enum ErrorConstants {
     PLEASE_ENTER_VALUE("값을 입력해 주세요."),
     ONLY_ENTER_NUMBERS("숫자만 입력하실 수 있습니다."),
     INVALID_RANGE_INPUT("%d 이상의 구매 금액을 입력해 주세요."),
-    INVALID_UNIT_INPUT("%d 원 단위의 구매 금액을 입력해 주세요.");
+    INVALID_UNIT_INPUT("%d 원 단위의 구매 금액을 입력해 주세요."),
+    ERROR_LOTTO_INVALID_LENGTH("로또 발행 중 에러발생. 6자리가 아닙니다."),
+    ERROR_LOTTO_INVALID_RANGE("로또 발행 중 에러발생. 유효하지 않은 범위의 숫자입니다."),
+    ERROR_LOTTO_DUPLICATED_VALUE("로또 발행 중 에러발생. 중복된 값이 존재합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String ErrorConstants;
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,11 @@
 
 package lotto.domain;
 
+import lotto.constants.ErrorConstants;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +16,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkLength(numbers);
+        checkRange(numbers);
+        checkDuplicateValue(numbers);
+    }
+
+    private void checkLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_LENGTH.getData());
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        for(int num : numbers) {
+            if( num <0 || num >46)
+                throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_RANGE.getData());
+        }
+    }
+
+    private void checkDuplicateValue(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if(numbers.size() != set.size()) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_DUPLICATED_VALUE.getData());
         }
     }
 

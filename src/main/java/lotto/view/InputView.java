600c3a6da3c49fd16d79b26923f3325a591afb1a
KangDeok <rkdejr2321@naver.com>
2023-11-07 22:41:04 +0900

    [feat] 사용자 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,18 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String inputLottoPrice() {
+        return readLine();
+    }
+
+    public String inputLottoNumbers() {
+        return readLine();
+    }
+
+    public String inputLottoBonusNumber() {
+        return readLine();
+    }
 }

d313449cf77fd1abcb61cb5510d45ebc51fabcf5
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 21:37:20 +0900

    feat(const): 당첨 통계 상수 메세지 추가

--- src/main/java/lotto/constant/GeneralMessage.java
+++ src/main/java/lotto/constant/GeneralMessage.java
@@ -4,7 +4,8 @@ public enum GeneralMessage {
     ASK_BUY_PRICE("구입금액을 입력해 주세요."),
     NOTIFY_BUY_COUNT("%d개를 구매했습니다."),
     ASK_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    NOTIFY_WINNING_STATISTICS("당첨 통계" + System.lineSeparator() + "---");
 
     private final String message;
 

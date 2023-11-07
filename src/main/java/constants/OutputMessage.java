0e71d83bfe4381ebe31efa31e8962ede4f1b1de8
kkw2758 <dtc3229@gmail.com>
2023-11-07 15:05:48 +0900

    Feat(OutputView) : 수익률 출력 기능 추가

--- src/main/java/constants/OutputMessage.java
+++ src/main/java/constants/OutputMessage.java
@@ -5,7 +5,8 @@ public enum OutputMessage {
     ANSWER_LOTTO_INPUT_MESSAGE("당첨 번호를 입력해주세요."),
     BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해주세요."),
     LOTTO_RESULT_MESSAGE("당첨 통계"),
-    LOTTO_PURCHASE_COUNT_MESSAGE("%d개를 구매했습니다.");
+    LOTTO_PURCHASE_COUNT_MESSAGE("%d개를 구매했습니다."),
+    EARNING_RATE_MESSAGE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 

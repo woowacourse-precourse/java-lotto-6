67ac2ef5b2e4b7457d19b1d3f7f9af18c386d36c
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-07 22:58:59 +0900

    feat(application): 로또 게임 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.controller.LottoService;
+import lotto.domain.Lottos;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController controller = new LottoController(new InputView(), new OutputView(), new LottoService());
+        controller.run();
     }
 }

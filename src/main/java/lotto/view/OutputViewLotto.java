4a77a3068f3f8db431df032d76279cd06a664557
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 21:55:26 +0900

    refactor(OutputViewLotto) : 메서드 이름 변경 - getLotto 에서 printLotto로 의미 있게 변경

--- src/main/java/lotto/view/OutputViewLotto.java
+++ src/main/java/lotto/view/OutputViewLotto.java
@@ -4,7 +4,7 @@ package lotto.view;
 import lotto.domain.Lottos;
 
 public class OutputViewLotto {
-    public static void getLotto(Lottos lottos){
+    public static void printLotto(Lottos lottos){
         lottos.getLottos().forEach(System.out::println);
         System.out.println();
     }

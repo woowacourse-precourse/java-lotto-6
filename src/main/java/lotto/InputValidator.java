1b468de13520789fa6f5339421549a52ae2b34ba
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-08 06:28:19 +0900

    Refactor(inputValidator) : 매직 넘버 상수화

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -1,6 +1,10 @@
 package lotto;
 
 public class InputValidator {
+    private static final Integer PRICE_UNIT = 1000;
+    private static final Integer PRICE_STANDARD = 1000;
+    private static final Integer REMAIN_ZERO = 0;
+
     public void validatePrice(int userInput) {
         validateAmount(userInput);
         validateUnit(userInput);
@@ -8,21 +12,21 @@ public class InputValidator {
 
     private void validateUnit(int userInput) {
         if (isNotRequiredUnit(userInput)) {
-            throw new IllegalArgumentException("1000원 단위 이외의 금액은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(PRICE_UNIT + "원 단위 이외의 금액은 입력할 수 없습니다.");
         }
     }
 
     private boolean isNotRequiredUnit(int userInput) {
-        return userInput % 1000 != 0;
+        return userInput % PRICE_UNIT != REMAIN_ZERO;
     }
 
     private void validateAmount(int userInput) {
         if (isLowerThanStandard(userInput)) {
-            throw new IllegalArgumentException("1000원 미만의 금액은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(PRICE_STANDARD + "원 미만의 금액은 입력할 수 없습니다.");
         }
     }
 
     private boolean isLowerThanStandard(int userInput) {
-        return userInput < 1000;
+        return userInput < PRICE_STANDARD;
     }
 }

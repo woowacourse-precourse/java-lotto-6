bb7b2071e5a101448ce4ebb2729ce2e22877d7a9
ohhyeon2 <dhgus1235@gmail.com>
2023-11-07 15:21:48 +0900

    refactor: 띄어쓰기 수정

--- src/main/java/lotto/view/ProfitRateOutPut.java
+++
@@ -1,7 +0,0 @@
-package lotto.view;
-
-public class ProfitRateOutPut {
-    public static void profitRate(double profitRate) {
-        System.out.println("총 수익률은" + profitRate + "%입니다.");
-    }
-}
---
+++ src/main/java/lotto/view/ProfitRateOutput.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class ProfitRateOutput {
+    public static void profitRate(double profitRate) {
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
+    }
+}

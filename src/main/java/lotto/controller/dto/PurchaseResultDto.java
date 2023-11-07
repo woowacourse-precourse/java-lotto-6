898b4a973e321b5c1686deac8e163018c8a7eebb
wwan13 <wwan13@naver.com>
2023-11-07 21:12:42 +0900

    feat : OutputView에 사용되는 Dto 구현

---
+++ src/main/java/lotto/controller/dto/PurchaseResultDto.java
@@ -0,0 +1,21 @@
+package lotto.controller.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.lotto.Lotto;
+
+public record PurchaseResultDto(
+    int amount,
+    List<List<Integer>> lottos
+) {
+
+    public static PurchaseResultDto from(List<Lotto> lottos) {
+        int amount = lottos.size();
+
+        List<List<Integer>> lottoNumbers = lottos.stream()
+            .map(Lotto::getNumbers)
+            .toList();
+
+        return new PurchaseResultDto(amount, lottoNumbers);
+    }
+}

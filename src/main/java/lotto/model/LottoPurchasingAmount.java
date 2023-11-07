9227f2665041e597a968f93a7e9c85d011f64d75
JDUB9823 <jeong98231@gmail.com>
2023-11-07 11:03:02 +0900

    feat: 1000으로 나누어 떨어지지 않는 구입 금액 예외처리

--- src/main/java/lotto/model/LottoPurchasingAmount.java
+++ src/main/java/lotto/model/LottoPurchasingAmount.java
@@ -11,7 +11,8 @@ public class LottoPurchasingAmount {
     public LottoPurchasingAmount(String moneyFromUserInput) {
         int moneyFromUser = validateNumber(moneyFromUserInput);
         validateMinimumPrice(moneyFromUser);
-        this.lottoAmount = moneyFromUser / LOTTO_MINIMUM_PRICE;
+        validateDivisible(moneyFromUser);
+        this.lottoAmount = parseInt(moneyFromUserInput) / LOTTO_MINIMUM_PRICE;
     }
 
     private int validateNumber(String money) {
@@ -26,6 +27,13 @@ public class LottoPurchasingAmount {
     private void validateMinimumPrice(int money) {
         if (money < LOTTO_MINIMUM_PRICE) {
             ErrorMessage.minimumLottoPriceException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivisible(int money) {
+        if (money % LOTTO_MINIMUM_PRICE != 0) {
+            ErrorMessage.indivisibleLottoPriceException();
             throw new IllegalArgumentException();
         }
     }
--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -4,11 +4,16 @@ public class ErrorMessage {
     private static final int MINIMUM_LOTTO_PRICE = 1000;
     private static final String NOT_A_NUMBER = "[ERROR] 숫자만 입력 가능합니다.";
     private static final String INVALID_MINIMUM_LOTTO_PRICE = "[ERROR] " + MINIMUM_LOTTO_PRICE + "원 이상의 금액만 입력 가능합니다.";
+    private static final String INDIVISIBLE_LOTTO_PRICE = "[ERROR] 금액은 " + MINIMUM_LOTTO_PRICE + "단위만 가능합니다.";
     public static void numberException() {
         System.out.println(NOT_A_NUMBER);
     }
 
     public static void minimumLottoPriceException() {
         System.out.println(INVALID_MINIMUM_LOTTO_PRICE);
+    }
+
+    public static void indivisibleLottoPriceException() {
+        System.out.println(INDIVISIBLE_LOTTO_PRICE);
     }
 }

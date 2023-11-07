aaf415458b9f704d0d0ef85d31b3a1c42e02fcd3
D0ri123 <lsh81550@gmail.com>
2023-11-08 02:15:14 +0900

    refactor(Bonus): BonusValidator와 Bonus를 합침 - 4차 구현
    Validator 서비스를 도메인에서 분리하여서 구현했습니다.
    이렇게 했을 때 Bonus 생성자에 올바르지 않은 값을
    바로 넣을 수 있다는 문제가 발생했습니다.
    그래서 이를 합치기로 했습니다.

--- src/main/java/lotto/model/domain/Bonus.java
+++ src/main/java/lotto/model/domain/Bonus.java
@@ -1,14 +1,44 @@
 package lotto.model.domain;
 
+import static lotto.util.Constants.MAX_NUMBER;
+import static lotto.util.Constants.MIN_NUMBER;
+import static lotto.util.ExceptionMessage.BONUS_ALREADY_CONTAIN_WINNING;
+import static lotto.util.ExceptionMessage.COMMON_INVALID_RANGE;
+import static lotto.util.ExceptionMessage.COMMON_INVALID_TYPE;
+
+import java.util.List;
+
 public class Bonus {
     private final Integer number;
 
-    public Bonus(int number) {
-        this.number = number;
+    public Bonus(List<Integer> numbers, String number) {
+        int bonusInteger = validateBonusIsNumeric(number);
+        validateNumberBetweenInRange(bonusInteger);
+        validateWinningNumbersContainBonusNumber(numbers, bonusInteger);
+        this.number = bonusInteger;
+    }
+
+    private void validateNumberBetweenInRange(int bonus) {
+        if (bonus < MIN_NUMBER || bonus > MAX_NUMBER) {
+            throw new IllegalArgumentException(COMMON_INVALID_RANGE.format(MIN_NUMBER, MAX_NUMBER));
+        }
+    }
+
+    private int validateBonusIsNumeric(String bonusNumber) {
+        try {
+            return Integer.parseInt(bonusNumber.trim());
+        } catch (Exception e) {
+            throw new IllegalArgumentException(COMMON_INVALID_TYPE.getMessage());
+        }
+    }
+
+    private void validateWinningNumbersContainBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_ALREADY_CONTAIN_WINNING.getMessage());
+        }
     }
 
     public int getNumber() {
         return number;
     }
-
 }
--- src/main/java/lotto/service/BonusNumberService.java
+++ src/main/java/lotto/service/BonusNumberService.java
@@ -1,18 +1,12 @@
 package lotto.service;
 
 import lotto.model.domain.Bonus;
-import lotto.model.domain.BonusValidator;
 import lotto.model.domain.WinningLotto;
 
 public class BonusNumberService {
-    private final BonusValidator validator = new BonusValidator();
-
     public Bonus getBonusNumberIfValid(WinningLotto winningLotto, String bonusNumber) {
         try {
-            int bonusInteger = validator.validateBonusIsNumeric(bonusNumber);
-            validator.validateNumberBetweenInRange(bonusInteger);
-            validator.validateWinningNumbersContainBonusNumber(winningLotto.getNumbers(), bonusInteger);
-            return new Bonus(bonusInteger);
+            return new Bonus(winningLotto.getNumbers(),bonusNumber);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }

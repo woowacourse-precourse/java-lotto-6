5941b99d61a3b4b7389dd273aa32f66373107759
ikjo39 <chi4321@naver.com>
2023-11-07 23:24:03 +0900

    refactor: 게산된 수익률 출력 문구 변환시 필요한 숫자형식 수정
    - 숫자형식 추가
    - 하드코딩 변환

--- src/main/java/lotto/dto/RateOfReturn.java
+++ src/main/java/lotto/dto/RateOfReturn.java
@@ -1,8 +1,12 @@
 package lotto.dto;
 
+import java.text.DecimalFormat;
+
 public record RateOfReturn(double value) {
+    private static final DecimalFormat decimalFormat = new DecimalFormat("#,###.0");
+
     @Override
     public String toString() {
-        return "총 수익률은 " + value + "%입니다.";
+        return String.format("총 수익률은 %s%%입니다.", decimalFormat.format(value));
     }
 }

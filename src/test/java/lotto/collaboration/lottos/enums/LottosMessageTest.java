e8e632130df300d9228f7996a30046fbe470e8fe
aka-nick <bechalrang@gmail.com>
2023-11-07 18:05:03 +0900

    추가(WinningLottoMessageTest) : 테스트 클래스
    WinningLottoMessageTest
    LottoMessageTest

---
+++ src/test/java/lotto/collaboration/lottos/enums/LottosMessageTest.java
@@ -0,0 +1,36 @@
+package lotto.collaboration.lottos.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class LottosMessageTest {
+
+    @Test
+    void get으로_메시지얻을수있다() {
+        String expected = "원 단위로 입력해 주세요";
+
+        String actual = LottosMessage.EXCEPTION_INDIVISIBLE.get();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void makeMessageForIndivisible로_단위불가분_경고메시지획득할수있다() {
+        String expected = "50원 단위로 입력해 주세요";
+
+        String actual = LottosMessage.makeMessageForIndivisible(50);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void makeMessageForOutOfRange_단위벗어남_경고메시지획득할수있다() {
+        String expected = "로또 구매 금액은 10원에서 500원까지만 가능합니다.";
+
+        String actual = LottosMessage.makeMessageForOutOfRange(10, 500);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}

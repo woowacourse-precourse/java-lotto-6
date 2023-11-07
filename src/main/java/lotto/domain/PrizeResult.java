58322ded93f443bd98608cc1c81f25a438735d0a
의찬 <srja432130@gmail.com>
2023-11-07 19:40:17 +0900

    feat: 로또 당첨 개수를 관리하는 클래스를 추가

---
+++ src/main/java/lotto/domain/PrizeResult.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class PrizeResult {
+    private Map<Prize, Integer> prizeResult;
+
+    public PrizeResult() {
+        prizeResult = new EnumMap<Prize, Integer>(Prize.class);
+        Arrays.stream(Prize.values())
+                .filter(prize -> !prize.equals(Prize.EMPTY))
+                .forEach(prize -> prizeResult.put(prize, 0));
+    }
+
+    public void updatePrizeCount(Prize prize) {
+        prizeResult.put(prize, prizeResult.get(prize) + 1);
+    }
+
+    public Integer getPrizeCount(Prize prize) {
+        return prizeResult.get(prize);
+    }
+}

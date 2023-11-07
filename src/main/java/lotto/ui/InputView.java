b4178d6a1e455e3f5a166b68b168d46f5230935e
dj <fjzl12@naver.com>
2023-11-07 23:45:51 +0900

    style(InputView): 공백 제거

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -4,13 +4,10 @@ import static lotto.domain.Message.INPUT_COST;
 import static lotto.domain.Message.INPUT_NUMBERS;
 import static lotto.domain.Message.INPUT_BONUS_NUMBERS;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class InputView {
     public static void readCost() {
         System.out.println(INPUT_COST.getMessage());
     }
-
 
     public static void readNumbers() {
         System.out.println(INPUT_NUMBERS.getMessage());

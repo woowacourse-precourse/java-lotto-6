d38c4a6b1c80367159bbc1e4adb0c2c197bf7cc4
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-08 02:57:19 +0900

    refactor: 로또 번호 생성 로직 수정 및 Lottos 객체 생성 기능 추가
    - Randoms의 pickUniqueNumbersInRange 메소드를 이용해 로또 번호 생성
    - AbstractImmutableList를 반환하므로 객체를 복사하여 정렬하는 기능 추가
    - 생성된 로또 번호를 바탕으로 Lottos 객체 생성

--- src/main/java/lotto/model/LottoNumbersGenerator.java
+++ src/main/java/lotto/model/LottoNumbersGenerator.java
@@ -8,39 +8,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumbersGenerator {
-    public List<Integer> generateLottoNumbers() {
-        List<Integer> randomNumbers = new ArrayList<>();
-        addNumbers(randomNumbers);
-        sortLottoNumbers(randomNumbers);
-        return randomNumbers;
+    protected List<Integer> generateLottoNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
+                MIN_NUMBER.getValue(),
+                MAX_NUMBER.getValue(),
+                SIZE.getValue()
+        );
+        List<Integer> sortedRandomNumbers = new ArrayList<>(randomNumbers);
+        Collections.sort(sortedRandomNumbers);
+        return sortedRandomNumbers;
     }
 
-    // List에 숫자 6개 추가
-    private void addNumbers(List<Integer> randomNumbers) {
-        while (randomNumbers.size() < SIZE.getValue()) {
-            int randomNumber = createRandomNonDuplicateNumber(randomNumbers);
-            randomNumbers.add(randomNumber);
-        }
-    }
-
-    // 랜덤 숫자 1개 생성
-    private int createRandomNumber() {
-        return Randoms.pickNumberInRange(MIN_NUMBER.getValue(), MAX_NUMBER.getValue());
-    }
-
-    // 중복되지 않은 숫자 생성
-    private int createRandomNonDuplicateNumber(List<Integer> randomNumbers) {
-        int randomNumber;
-        do {
-            randomNumber = createRandomNumber();
-        } while (randomNumbers.contains(randomNumber));
-        return randomNumber;
-    }
-
-    // List 정렬
-    private void sortLottoNumbers(List<Integer> randomNumbersList) {
-        Collections.sort(randomNumbersList);
+    public List<Lotto> generateLottos(int count) {
+        return IntStream.range(0, count).mapToObj(i -> new Lotto(generateLottoNumbers()))
+                .collect(Collectors.toList());
     }
 }

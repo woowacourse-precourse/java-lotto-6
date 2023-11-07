0eab8f69b2db1117807727b505b78bac60117ddf
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:02:14 +0900

    refactor(Message) : 요구사항에 맞는 메시지 수정

--- src/main/java/lotto/utils/Message.java
+++ src/main/java/lotto/utils/Message.java
@@ -2,11 +2,10 @@ package lotto.utils;
 
 public enum Message {
     PRINT_USER_INPUT_BUY_PRICE("구입금액을 입력해 주세요."),
-    PRINT_USER_BUY_LOTTO_COUNT("개를 구매했습니다"),
+    PRINT_USER_BUY_LOTTO_COUNT("개를 구매했습니다."),
     PRINT_USER_ENTER_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
     PRINT_USER_ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
-
-
+    
     private final String message;
 
     Message(String message) {

759b5cec30a424d594a27176297b02f60d097b0b
Chan Mi <rkdcksal02@gmail.com>
2023-11-07 20:09:41 +0900

    InputView 클래스 - 사용자 입력을 처리하고 에러 처리를 수행

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,17 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class InputView {
+
+    // 구입 금액을 입력받는 메서드
     public int inputPurchaseAmount() {
         while (true) {
             try {
-                System.out.print("구입 금액을 입력하세요 (1,000원 단위): ");
+                System.out.println("구입 금액을 입력하세요 (1,000원 단위): ");
                 int purchaseAmount = Integer.parseInt(Console.readLine());
 
                 if (purchaseAmount % 1000 != 0) {
@@ -18,17 +20,18 @@ public class InputView {
 
                 return purchaseAmount;
             } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 올바른 숫자 형식이 아닙니다.");
+                handleError("올바른 숫자 형식이 아닙니다.");
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] " + e.getMessage());
+                handleError(e.getMessage());
             }
         }
     }
 
+    // 당첨 번호를 입력받는 메서드
     public List<Integer> inputWinningNumbers() {
         while (true) {
             try {
-                System.out.print("당첨 번호를 입력하세요 (1부터 45까지의 숫자 중복없이 콤마(,)로 구분): ");
+                System.out.println("당첨 번호를 입력하세요 (1부터 45까지의 숫자 중복없이 콤마(,)로 구분): ");
                 String input = Console.readLine();
                 List<Integer> numbers = parseInputNumbers(input);
 
@@ -38,24 +41,36 @@ public class InputView {
 
                 return numbers;
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] " + e.getMessage());
+                handleError(e.getMessage());
             }
         }
     }
 
-    public int inputBonusNumber() {
+    // 보너스 볼을 입력받는 메서드
+    public int inputBonusNumber(List<Integer> winningNumbers) {
         while (true) {
             try {
-                System.out.print("보너스 볼을 입력하세요 (1부터 45까지의 숫자): ");
-                return Integer.parseInt(Console.readLine());
+                System.out.println("보너스 볼을 입력하세요 (1부터 45까지의 숫자): ");
+                int bonusNumber = Integer.parseInt(Console.readLine());
+
+                if (bonusNumber < 1 || bonusNumber > 45) {
+                    throw new IllegalArgumentException("보너스 볼은 1부터 45 사이의 숫자여야 합니다.");
+                }
+
+                if (winningNumbers.contains(bonusNumber)) {
+                    throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복되면 안됩니다.");
+                }
+
+                return bonusNumber;
             } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 올바른 숫자 형식이 아닙니다.");
+                handleError("올바른 숫자 형식이 아닙니다.");
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] " + e.getMessage());
+                handleError(e.getMessage());
             }
         }
     }
 
+    // 입력된 숫자 중 중복을 검사하는 메서드
     private List<Integer> parseInputNumbers(String input) {
         List<Integer> numbers = new ArrayList<>();
         try {
@@ -67,9 +82,22 @@ public class InputView {
                 }
                 numbers.add(number);
             }
+            if (hasDuplicates(numbers)) {
+                throw new IllegalArgumentException("당첨 번호는 중복되면 안됩니다.");
+            }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
+            handleError("올바른 숫자 형식이 아닙니다.");
         }
         return numbers;
+    }
+
+    // 중복된 숫자가 있는지 확인하는 메서드
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.size() != new ArrayList<>(new HashSet<>(numbers)).size();
+    }
+
+    // 에러를 처리하는 메서드
+    private void handleError(String errorMessage) {
+        System.out.println("[ERROR] " + errorMessage);
     }
 }

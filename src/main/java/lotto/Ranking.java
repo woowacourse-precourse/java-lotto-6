5a436a779b7a8633183c17ec3f5d9a8426299638
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-07 18:41:31 +0900

    fix(ranking): enumMap 사용으로 상수 선언 순서 변경 및 사용하지 않는 name 필드 제거

--- src/main/java/lotto/Ranking.java
+++ src/main/java/lotto/Ranking.java
@@ -1,19 +1,16 @@
 package lotto;
 
 public enum Ranking {
-    FIRST("1등", "6개 일치 (2,000,000,000원)", 2_000_000_000),
-    SECOND("2등", "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
-    THIRD("3등", "5개 일치 (1,500,000원)", 1_500_000),
-    FOURTH("4등", "4개 일치 (50,000원)", 50_000),
-    FIFTH("5등", "3개 일치 (5,000원)",5_000),
-    NONE("낙첨", "모든 번호 불일치",0);
+    FIFTH("3개 일치 (5,000원)", 5_000),
+    FOURTH("4개 일치 (50,000원)", 50_000),
+    THIRD("5개 일치 (1,500,000원)", 1_500_000),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000);
 
-    private final String rankingName;
     private final String criteria;
     private final int prize;
 
-    Ranking(String rankingName, String criteria, int prize) {
-        this.rankingName = rankingName;
+    Ranking(String criteria, int prize) {
         this.criteria = criteria;
         this.prize = prize;
     }
@@ -39,9 +36,6 @@ public enum Ranking {
         if (matchCount == 4) {
             return FOURTH;
         }
-        if (matchCount == 3) {
-            return FIFTH;
-        }
-        return NONE;
+        return FIFTH;
     }
 }

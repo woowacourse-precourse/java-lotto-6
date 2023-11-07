e68f290a80063f8e903f270d2d658bf6dbbe4221
KDT live <dnjsxo0616@gmail.com>
2023-11-07 23:17:06 +0900

    feat(InputPurchaseAmountView): 구입금액 입력 처리 메서드 추가
    - inputAmountView 메서드를 구현하여 구입금액을 입력 받고 반환
    - 입력값 유효성 검사 수행
    - 입력값을 정수로 파싱하여 반환

--- src/main/java/lotto/view/InputPurchaseAmountView.java
+++ src/main/java/lotto/view/InputPurchaseAmountView.java
@@ -4,6 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputPurchaseAmountView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
 
+    public int inputAmountView(){
+        String input = inputPurchaseAmount();
+        validateInput(input);
+        return Integer.parseInt(input);
+    }
+
     public String inputPurchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
         return Console.readLine();

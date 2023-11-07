9b711b4c853e3db429efab8a23e03410ae4b4e41
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-11-08 05:14:23 +0900

    feat: 로또 당첨 통계 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,12 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.WinningResult;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OutputView {
 
@@ -13,6 +17,14 @@ public class OutputView {
     public void printLottoList(List<Lotto> lottoList) {
         for (Lotto lotto : lottoList) {
             System.out.println(lotto);
+        }
+    }
+
+    public static void printWinningResult(Map<WinningResult, Integer> results) {
+        for (WinningResult winningResult : WinningResult.values()) {
+            Integer count = results.getOrDefault(winningResult, 0);
+            String message = String.format("%s - %d개", winningResult.getMessage(), count);
+            System.out.println(message);
         }
     }
 

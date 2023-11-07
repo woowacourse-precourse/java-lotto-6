8dd36fa77bd333c35415be1a795696310d41fef7
Choijake <shameless8@naver.com>
2023-11-08 01:44:06 +0900

    Feat : Constant 클래스 출력 메시지 추가

---
+++ src/main/java/lotto/view/constant/Constant.java
@@ -0,0 +1,32 @@
+package lotto.view.constant;
+
+public enum Constant {
+
+    // TODO 게터이외에 메시지를 이용하는 메서드 구현이 필요함
+
+    //입력 메시지
+    INPUT_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
+    INPUT_LOTTO_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+
+    //출력 메시지
+    FRONT_BRACKETS("["),
+    REAR_BRACKETS("]"),
+    COMMAS(","),
+    RESULT_HEAD(System.lineSeparator() + "당첨 통계" + "---------"),
+    BUYING_COUNT("%d개를 구매했습니다."),
+    WIN_STATISTICS_HEADER("당첨 통계\n---"),
+    STATISTICS("%d개 일치%s(%d원)- %d개" + System.lineSeparator()),
+    TAKE_BONUS(", 보너스 볼 일치"),
+    PROFIT("총 수익률은 %.2f입니다.");
+
+    private final String message;
+
+    Constant(String message) {
+        this.message = message;
+    }
+
+    String message() {
+        return message;
+    }
+}

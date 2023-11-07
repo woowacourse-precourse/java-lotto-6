2a8f6da3f8db054be9dfa2efa018a30bcd6da4e8
jihwankim128 <jihwan1535@nate.com>
2023-11-08 04:04:27 +0900

    refactor: ERROR MESSAGE 수정 Error -> ERROR

--- src/main/java/lotto/util/IllegalArgumentMessage.java
+++ src/main/java/lotto/util/IllegalArgumentMessage.java
@@ -1,12 +1,12 @@
 package lotto.util;
 
 public final class IllegalArgumentMessage {
-    public static final String ERROR_DUPLICATION_MESSAGE = "[Error] 입력하신 번호와 중복된 번호가 존재합니다.";
-    public static final String ERROR_RANGE_MESSAGE = "[Error] 로또 번호는 범위는 1부터 45입니다.";
-    public static final String ERROR_LESS_THAN_MINIMUM_PRICE = "[Error] 최소 결제 금액은 1,000원 입니다.";
-    public static final String ERROR_NOT_PRICE_UNIT = "[Error] 1,000원 단위로 결제해주세요.";
-    public static final String ERROR_PICK_COUNT = "[Error] 로또 번호는 6개입니다.";
-    public static final String ERROR_PAYMENT_INPUT = "[Error] 구입금액을 정수로 입력해주세요.";
-    public static final String ERROR_SPLIT_IDENTIFICATION = "[Error] ','를 구분자로 입력해주세요.";
-    public static final String ERROR_ONLY_NUMBERS = "[Error] 번호는 정수로만 구성이 되어야합니다.";
+    public static final String ERROR_DUPLICATION_MESSAGE = "[ERROR] 입력하신 번호와 중복된 번호가 존재합니다.";
+    public static final String ERROR_RANGE_MESSAGE = "[ERROR] 로또 번호는 범위는 1부터 45입니다.";
+    public static final String ERROR_LESS_THAN_MINIMUM_PRICE = "[ERROR] 최소 결제 금액은 1,000원 입니다.";
+    public static final String ERROR_NOT_PRICE_UNIT = "[ERROR] 1,000원 단위로 결제해주세요.";
+    public static final String ERROR_PICK_COUNT = "[ERROR] 로또 번호는 6개입니다.";
+    public static final String ERROR_PAYMENT_INPUT = "[ERROR] 구입금액을 정수로 입력해주세요.";
+    public static final String ERROR_SPLIT_IDENTIFICATION = "[ERROR] ','를 구분자로 입력해주세요.";
+    public static final String ERROR_ONLY_NUMBERS = "[ERROR] 번호는 정수로만 구성이 되어야합니다.";
 }

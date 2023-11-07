81b040213d0379d0052dd0bc021f43cb4d59d5ae
i3kae <snoger1234@gmail.com>
2023-11-07 19:02:04 +0900

    feat : 로또 번호 중복 검사 기능 추가 1. 로또 번호 리스트 내에 중복된 번호가 있을 경우 예외 처리하도록 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (numbers.stream().distinct().count() != numbers.size()){
+            throw new IllegalArgumentException();
+        }
     }
     public void printLotto(){
         Print.printLotto(numbers);

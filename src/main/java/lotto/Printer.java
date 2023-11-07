18c47fa20562023e6cf0b74b750909e5f701edfb
Identityless <rlandwnd555@naver.com>
2023-11-08 00:00:21 +0900

    feat : 당첨 통계 출력 기능 추가

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -27,7 +27,22 @@ public class Printer {
         System.out.println();
     }
 
+    public void printStatistic(Info info) {
+        StringBuilder sb = new StringBuilder();
+        int[] statistic = info.getStatistic();
+        sb.append("당첨 통계\n").append("---\n")
+                .append("3개 일치 (5,000원) - ").append(statistic[Grade.FIFTH_GRADE.getGrade()])
+                .append("\n").append("4개 일치 (50,000원) - ")
+                .append(statistic[Grade.FIFTH_GRADE.getGrade()]).append("\n")
+                .append("5개 일치 (1,500,000원) - ").append(statistic[Grade.FIFTH_GRADE.getGrade()])
+                .append("\n").append("5개 일치, 보너스 볼 일치 (30,000,000원) - ")
+                .append(statistic[Grade.FIFTH_GRADE.getGrade()]).append("\n")
+                .append("6개 일치 (2,000,000,000원) - ")
+                .append(statistic[Grade.FIFTH_GRADE.getGrade()]);
+        System.out.println(sb);
+    }
+
     public void profitRate(ProfitRateDTO profitRateDTO) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", profitRateDTO.getProfitRate());
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRateDTO.getProfitRate());
     }
 }

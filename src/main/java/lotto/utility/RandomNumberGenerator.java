e4e6b955678d4683d38e865acdf9cc4e716f4d7a
Sung Hyobin <hellmir610@gmail.com>
2023-11-08 08:05:03 +0900

    fix: Modify RandomNumberGenerator to return numbers in ascending order

--- src/main/java/lotto/utility/RandomNumberGenerator.java
+++ src/main/java/lotto/utility/RandomNumberGenerator.java
@@ -2,12 +2,15 @@ package lotto.utility;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 import static lotto.configuration.RandomNumberConfig.*;
 
 public class RandomNumberGenerator {
-    public static List<Integer> generateNotDuplicatedRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+    public static List<Integer> generateNotDuplicatedRandomNumbersOrderByAsc() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+        Collections.sort(randomNumbers);
+        return randomNumbers;
     }
 }

dfded610e73aae24049d6b7f41c80b9e95c81f92
lynjun97 <lynjun97@naver.com>
2023-11-07 20:49:36 +0900

    Feat: inputWinningNumbers 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -3,16 +3,20 @@ package lotto.controller;
 import lotto.Lotto;
 import java.util.List;
 import static lotto.controller.LottoController.*;
+import static lotto.controller.LottoController.lottoNumbers;
 import static lotto.view.OutView.outputLottoNumber;
 
 public class GameController {
     private List<Lotto> lotto;
+    private List<Integer> list;
 
     public GameController() {
         //구매 금액 입력
         setLotto();
         // 티켓 출력
         outputLottoTickets();
+        //당첨 번호 입력
+        inputWinningNumbers();
     }
 
     private void setLotto() {
@@ -22,5 +26,9 @@ public class GameController {
 
     private void outputLottoTickets() {
         outputLottoNumber(lotto);
+    }
+
+    private void inputWinningNumbers() {
+        list = lottoNumbers();
     }
 }

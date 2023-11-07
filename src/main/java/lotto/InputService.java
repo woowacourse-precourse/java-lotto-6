b4998b993310deeb8c6c2541adff1d49605d9680
changhyeonnam <hj04143@gmail.com>
2023-11-07 18:01:52 +0900

    feat(InputService) : 로또 구입 금액 입력 기능 구현

---
+++ src/main/java/lotto/InputService.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static constants.Constant.*;
+
+public class InputService {
+
+    public Integer inputPurchasePrice(){
+        System.out.println(PURCAHSE_PRICE_STRING);
+        return Integer.parseInt(Console.readLine());
+    }
+
+}

96ca33a12353f7ed740baf2911164ee0a4d2c36e
윤병호 <pdot715@gmail.com>
2023-11-08 08:22:41 +0900

    feat(Lotto): WinNumber 도메인 생성 및 값 검증 구현

---
+++ src/main/java/lotto/domain/WinNumbers.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+public class WinNumbers {
+    private static final String BONUS_NUMBER_DUPLICATE_ERROR = "[ERROR] 입력한 보너스 번호가 당첨 번호와 중복됩니다.";
+    private final Lotto winNumbers;
+    private final int bonusNumber;
+
+    public WinNumbers(Lotto winNumbers, int bonusNumber) {
+        validate(winNumbers, bonusNumber);
+        this.winNumbers = winNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void validate(Lotto numbers, int bonusNumber) {
+        if(numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR);
+        }
+    }
+
+    public Lotto getWinNumbers() {
+        return this.winNumbers;
+    }
+}

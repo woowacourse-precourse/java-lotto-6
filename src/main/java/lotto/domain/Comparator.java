503a936eab488b0652c7385a669b75dc568e4d0c
sangchu <didi1484@gmail.com>
2023-11-07 11:04:45 +0900

    fix: 전역 메서드 선언

--- src/main/java/lotto/domain/Comparator.java
+++ src/main/java/lotto/domain/Comparator.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Comparator {
-    private int countSameNumber(List<Integer> numbers, List<Integer> winningNumbers) {
+    private static int countSameNumber(List<Integer> numbers, List<Integer> winningNumbers) {
         return (int) numbers.stream()
                 .filter(winningNumbers::contains)
                 .count();
     }
 
-    private boolean checkBonusNumber(List<Integer> numbers, int bonusNumber) {
+    private static boolean checkBonusNumber(List<Integer> numbers, int bonusNumber) {
         return numbers.stream()
                 .anyMatch(number -> number == bonusNumber);
     }
 
-    public List<WinningCount> compare(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+    public static List<WinningCount> compare(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
         List<WinningCount> winningCounts = new ArrayList<>();
         for (Lotto lotto : lottos) {
             int count = countSameNumber(lotto.getNumbers(), winningNumbers);

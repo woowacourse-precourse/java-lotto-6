b21ceead1677ac91f815f31b56e2cc0d416f2127
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 17:13:26 +0900

    feat: add read bonus num to input view

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
 
     private final static String READ_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
     private final static String READ_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private final static String READ_BONUS_NUM = "보너스 번호를 입력해 주세요.";
 
     public int readPurchaseMoney() {
         System.out.println(READ_PURCHASE_MONEY);
@@ -27,6 +28,13 @@ public class InputView {
         return Arrays.stream(WinningNumbers)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public int readBonusNum() {
+        System.out.println(READ_BONUS_NUM);
+        String bonusNum = Console.readLine();
+        validateInteger(bonusNum);
+        return Integer.parseInt(bonusNum);
     }
 
     public void validateAllInteger(String[] strings) {

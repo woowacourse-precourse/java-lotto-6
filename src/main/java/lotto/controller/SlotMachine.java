6cd60a89752fc46118e98198280aca17db4d455c
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 23:48:19 +0900

    feat(controller): 메인 컨트롤러인 SlotMachine에서 RankMachine 생성 및 당첨 통계 요청 후 결과 출력

--- src/main/java/lotto/controller/SlotMachine.java
+++ src/main/java/lotto/controller/SlotMachine.java
@@ -1,24 +1,32 @@
 package lotto.controller;
 
-import lotto.domain.BuyPrice;
-import lotto.domain.Lotto;
-import lotto.domain.LottoNumber;
-import lotto.domain.Tickets;
+import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
 import java.util.function.Supplier;
+
+import static lotto.constant.Config.*;
+import static lotto.constant.WinningMessage.*;
 
 public class SlotMachine {
 
     public static void run() {
+        // 구입 금액 요청
         BuyPrice price = requestUntilValidated(() -> BuyPrice.from(InputView.askBuyPrice()));
         int count = price.getBuyCount();
         Tickets tickets = Tickets.generate(count);
         OutputView.printBuyLottos(count, tickets.toString());
 
-        Lotto winning = requestUntilValidated(() -> Lotto.from(InputView.askWinningNumbers()));
-        LottoNumber bonus = requestUntilValidated(() -> LottoNumber.from(InputView.askBonusNumber()));
+        // 당첨 번호 및 보너스 번호 요청
+        Lotto winningTicket = requestUntilValidated(() -> Lotto.from(InputView.askWinningNumbers()));
+        LottoNumber bonusNumber = requestUntilValidated(() -> LottoNumber.from(InputView.askBonusNumber()));
+
+        // 당첨 통계 계산 및 출력
+        RankMachine rankMachine = createRankMachine();
+        rankMachine.applyTickets(tickets, winningTicket, bonusNumber);
+        OutputView.printWinningStatistics(rankMachine.getWinningStatistics(), rankMachine.getRateOfReturn());
     }
 
     private static <T> T requestUntilValidated(Supplier<T> supplier) {
@@ -29,5 +37,20 @@ public class SlotMachine {
                 OutputView.printErrorMessage(e.getMessage());
             }
         }
+    }
+
+    private static RankMachine createRankMachine() {
+        return new RankMachine(List.of(
+                new Rank(RANK_FIFTH, REWARD_FIFTH, (ticket, winningTicket, bonusNumber) ->
+                        ticket.countMatched(winningTicket) == 3),
+                new Rank(RANK_FOURTH, REWARD_FOURTH, (ticket, winningTicket, bonusNumber) ->
+                        ticket.countMatched(winningTicket) == 4),
+                new Rank(RANK_THIRD, REWARD_THIRD, (ticket, winningTicket, bonusNumber) ->
+                        ticket.countMatched(winningTicket) == 5 && !ticket.hasNumber(bonusNumber)),
+                new Rank(RANK_SECOND, REWARD_SECOND, (ticket, winningTicket, bonusNumber) ->
+                        ticket.countMatched(winningTicket) == 5 && ticket.hasNumber(bonusNumber)),
+                new Rank(RANK_FIRST, REWARD_FIRST, (ticket, winningTicket, bonusNumber) ->
+                        ticket.countMatched(winningTicket) == 6)
+        ));
     }
 }

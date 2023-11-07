c9c13b23de76aed578923affa299c0d6c22935a4
dbstn1325 <dbstn1325@naver.com>
2023-11-07 22:54:58 +0900

    refactor: 에러 메시지를 ErrorMessage 인터페이스에서 역할을 책임지도록 개선 및 중복 코드 제거

--- src/main/java/lotto/constants/LottoErrorMessage.java
+++ src/main/java/lotto/constants/LottoErrorMessage.java
@@ -1,24 +1,18 @@
 package lotto.constants;
 
-public enum LottoErrorMessage {
+public enum LottoErrorMessage implements ErrorMessage {
     LENGTH_INVALID("발행한 로또 번호는 6자리 숫자여야 합니다."),
     RANGE_INVALID("발행한 로또 번호는 1~45의 숫자여야 합니다."),
-    DUPLICATE_INVALID("발행한 로또 번호는 중복이 있으면 안됩니다."),
-    ;
+    DUPLICATE_INVALID("발행한 로또 번호는 중복이 있으면 안됩니다.");
 
-    private static final String START_ERROR_MESSAGE = "[ERROR] ";
     private final String message;
 
     LottoErrorMessage(String message) {
         this.message = message;
     }
 
-    public String entireMessage() {
-        return (START_ERROR_MESSAGE + getMessage());
-    }
-
+    @Override
     public String getMessage() {
         return this.message;
     }
-
 }

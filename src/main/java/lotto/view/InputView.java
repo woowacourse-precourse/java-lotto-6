6186b25985ba2a60262a32e4d02bfc32ebe0f865
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-07 14:37:58 +0900

    Feat : InputView class 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,35 @@
 package lotto.view;
 
+import static lotto.util.InputUtil.toIntStringNumberParser;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.InvalidPurchaseCostException;
+
 
 
 public class InputView {
+    private final InvalidPurchaseCostException invalidPurchaseCostException = new InvalidPurchaseCostException();
 
+
+    public int inputPriceView() {
+        try {
+            String price = Console.readLine();
+            invalidPurchaseCostException.validatePurchaseCostException(price);
+            return toIntStringNumberParser(price);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 입력은 숫자만 가능합니다.");
+        }
+    }
+
+    public String inputLottoNumbersView() {
+        String lottoNumbers = Console.readLine();
+        return lottoNumbers;
+    }
+
+    public String inputBonusNumberView(){
+        String bonusNumber = Console.readLine();
+        return bonusNumber;
+    }
 
 
 }

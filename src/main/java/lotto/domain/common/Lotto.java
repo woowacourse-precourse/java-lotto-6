13510c81e4787f43fd19abc573bea87da0f1eba2
KimChanJin97 <cjkimhello97@gmail.com>
2023-11-07 17:22:19 +0900

    feat: implement Lotto validation

--- src/main/java/lotto/domain/common/Lotto.java
+++ src/main/java/lotto/domain/common/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain.common;
 
 
+import lotto.exception.InputView.InputLottoNotNumericException;
 import lotto.exception.Lotto.LottoNotSizeSixException;
 
 import java.util.List;
@@ -11,15 +12,24 @@ public record Lotto(List<Integer> numbers) {
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_SIZE = 6;
+    private static final String NUMERIC_REGEX = "^[0-9]*$";
+
 
 
     public Lotto {
         validateLottoSize(numbers);
+        validateLottoNumeric(numbers);
     }
 
     private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new LottoNotSizeSixException();
+        }
+    }
+
+    private void validateLottoNumeric(List<Integer> numbers) {
+        if (!numbers.stream().map(i -> String.valueOf(i)).allMatch(s -> s.matches(NUMERIC_REGEX))) {
+            throw new InputLottoNotNumericException();
         }
     }
 

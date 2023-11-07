20dcc3e41255aed2f10c4a1fc3af3a60913f2851
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 02:55:14 +0900

    feat(printLottosStatics): 로또 당첨 통계 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,23 @@
 package lotto.view;
 
+import lotto.model.WinningCriteria;
 import java.util.List;
 
 public class OutputView {
+    private static final String LINE_SPACE = "\n";
+    private static final String LINE_DIVIDER = "---";
     private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String LOTTO_STATISTICS = "당첨 통계" + LINE_SPACE + LINE_DIVIDER;
+    private static List<WinningCriteria> orderPrintRank = List.of(
+            WinningCriteria.FIFTH,
+            WinningCriteria.FOURTH,
+            WinningCriteria.THIRD,
+            WinningCriteria.SECOND,
+            WinningCriteria.FIRST
+    );
 
     public void printLottoPurchaseInputMessage() {
         System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
@@ -26,5 +37,19 @@ public class OutputView {
 
     public void printLottoPurchasedNumber(List<Integer> numbers) {
         System.out.println(numbers);
+    }
+
+    public void printLottosStatics() {
+        System.out.println(LOTTO_STATISTICS);
+        for (WinningCriteria currentRank : orderPrintRank) {
+            printLottoStatics();
+        }
+        printRateOfReturn();
+    }
+
+    private void printLottoStatics() {
+    }
+
+    private void printRateOfReturn() {
     }
 }

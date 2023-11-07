be1e2b6d068cb99faaf3412745134770414336df
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 01:46:18 +0900

    test: 결제 기능 테스트

---
+++ src/test/java/lotto/service/impl/PaymentImplTest.java
@@ -0,0 +1,29 @@
+package lotto.service.impl;
+
+import static lotto.config.LottoPrice.LOTTO_PRICE;
+
+import lotto.config.AppConfig;
+import lotto.service.Payment;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PaymentImplTest {
+    Payment payment;
+
+    @BeforeEach
+    void beforeEach() {
+        payment = new AppConfig().payment();
+    }
+
+    @Test
+    void 로또구매테스트() {
+        //given
+        int money = 10000;
+        int lottoPrice = LOTTO_PRICE.getPrice();
+        //when
+        int testResult = payment.pay(money, lottoPrice);
+        //then
+        Assertions.assertThat(testResult).isEqualTo(money/lottoPrice);
+    }
+}

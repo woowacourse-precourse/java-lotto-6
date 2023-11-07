3ab86f3d1c356bda5973f8d12d05d1ceed31a594
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-08 00:16:13 +0900

    refactor : LottoRank enum 업데이트
    - matchCount 속성 생성
    - 보너스 넘버와 일치하는지 여부를 나타내는 bonusMatch 속성

--- src/main/java/lotto/model/lotto/LottoRank.java
+++ src/main/java/lotto/model/lotto/LottoRank.java
@@ -1,22 +1,49 @@
-package lotto.utils;
+package lotto.model.lotto;
 
 public enum LottoRank {
-    RANK_NOTHING(6, 0, ""),
-    RANK5(5, 5_000, "3개 일치 (5,000원) - "),
-    RANK4(4, 50_000, "4개 일치 (50,000원) - "),
-    RANK3(3, 5_000, "5개 일치 (1,500,000원) - "),
-    RANK2(2, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    RANK1(1, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
+    FAIL(0, false, 0, ""),
+    RANK5(3, false, 5_000, "3개 일치 (5,000원) - "),
+    RANK4(4, false, 50_000, "4개 일치 (50,000원) - "),
+    RANK3(5, false, 5_000, "5개 일치 (1,500,000원) - "),
+    RANK2(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    RANK1(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
 
-    private int rank;
-    private int amount;
-    private String message;
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int amount;
+    private final String message;
 
-    LottoRank(int rank, int amount, String message) {
-        this.rank = rank;
+    LottoRank(int matchCount, boolean bonusMatch, int amount, String message) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
         this.amount = amount;
         this.message = message;
+    }
+
+    public static LottoRank valueOf(int matchCount, boolean matchBonus) {
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank.getMatchCount() == matchCount && rank.isBonusMatch() == matchBonus) {
+                return rank;
+            }
+        }
+        return FAIL;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getMsg() {
+        return message;
     }
 
 

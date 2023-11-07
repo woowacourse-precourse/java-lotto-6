276ce92077e7ce7398beccbdc7692fc68f031cc7
oiler831 <qudwls831@gmail.com>
2023-11-07 22:44:05 +0900

    fix: BonusNumber 수정
    int 입력에서 string 입력으로 변경하였고 그에 따른 메소드 변경 및 추가가 있다.

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -7,17 +7,27 @@ public class BonusNumber {
     private static final int LOTTO_MAX_NUMBER = 45;
     private static int bonus;
 
-    public BonusNumber(int number, List<Integer> winningNumbers) {
+    public BonusNumber(String stringNumber, List<Integer> winningNumbers) {
+        validate(stringNumber, winningNumbers);
+        bonus = Integer.parseInt(stringNumber);
+    }
+
+    private void validate(String stringNumber, List<Integer> winningNumbers) {
+        int number = validateNumber(stringNumber);
         validateRange(number);
         validateDuplication(number, winningNumbers);
-        bonus = number;
+    }
+
+    private int validateNumber(String stringNumber) {
+        try {
+            return Integer.parseInt(stringNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
     }
 
     private void validateRange(int number) {
-        if (number < LOTTO_MIN_NUMBER) {
-            throw new IllegalArgumentException();
-        }
-        if (number > LOTTO_MAX_NUMBER) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
             throw new IllegalArgumentException();
         }
     }

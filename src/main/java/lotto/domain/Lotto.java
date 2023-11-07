8f32763a938acf7cbb3053d024893044fa358dfa
dmong <dt2980_hee@naver.com>
2023-11-08 06:07:19 +0900

    refactor: validate의 매직 넘버 상수 처리 및 검증 메소드 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,10 +13,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (isNotValidLength(numbers)) {
+            throw new IllegalArgumentException(String.format(ErrorConstants.INVALID_LENGTH_ERROR_MESSAGE.getMessage(),
+                    LOTTO_LENGTH_CONSTRAINT.getValue()));
+        }
+        if (isDuplicatedNumberExist(numbers)) {
+            throw new IllegalArgumentException(ErrorConstants.DUPLICATED_LOTTO_NUMBERS_ERROR_MESSAGE.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+    private boolean isNotValidLength(List<Integer> numbers) {
+        return numbers.size() != LOTTO_LENGTH_CONSTRAINT.getValue();
+    }
+
+    private boolean isDuplicatedNumberExist(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
 }
--- src/main/java/lotto/view/ErrorConstants.java
+++ src/main/java/lotto/view/ErrorConstants.java
@@ -2,7 +2,12 @@ package lotto.view;
 
 public enum ErrorConstants {
     INDIVISIBLE_BUY_AMOUNT_ERROR_MESSAGE("[ERROR] 구입 금액은 %d의 배수여야 합니다."),
-    MINIMUM_BUY_AMOUNT_ERROR_MESSAGE("[ERROR] 구입할 로또 개수는 최소 %d개 이상이어야 합니다.");
+    MINIMUM_BUY_AMOUNT_ERROR_MESSAGE("[ERROR] 구입할 로또 개수는 최소 %d개 이상이어야 합니다."),
+    OUT_OF_RANGE_NUMBER_ERROR_MESSAGE("[ERROR] 로또 번호는 %d-%d 범위 내의 자연수여야 합니다."),
+    INVALID_LENGTH_ERROR_MESSAGE("[ERROR] 로또는 %d개의 숫자로 구성되어야 합니다."),
+    DUPLICATED_WINNING_NUMBERS_ERROR_MESSAGE("[ERROR] 당첨 번호에 중복된 숫자가 존재합니다."),
+    DUPLICATED_LOTTO_NUMBERS_ERROR_MESSAGE("[ERROR] 생성된 로또에 중복된 숫자가 존재합니다."),
+    DUPLICATED_BONUS_NUMBER_ERROR_MESSAGE("[ERROR] 보너스 번호는 우승 번호의 숫자와 중복될 수 없습니다.");
 
     private final String message;
 

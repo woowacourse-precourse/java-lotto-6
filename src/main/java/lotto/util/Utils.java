7bffb78711fadba991804d1694a2f85c7e820e9f
hyungsoolim <hyungsoolim0@gmail.com>
2023-11-08 02:48:38 +0900

    refactor: 랜덤 번호 생성 유틸 하드코딩값 제거
    하드코딩되어 있는 랜덤 로또 번호 생성 메서드의 파라미터값을 상수로 변경함.

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -5,12 +5,17 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Utils {
+    private static final int NUMBER_START_INCLUSIVE = 1;
+    private static final int NUMBER_END_INCLUSIVE = 45;
+    private static final int NUMBER_PICK_COUNT = 6;
+    
     public static int stringToInt(String string) {
         return Integer.parseInt(string.trim());
     }
 
     public static List<Integer> createSortedLottoNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(NUMBER_START_INCLUSIVE, NUMBER_END_INCLUSIVE,
+                                                                      NUMBER_PICK_COUNT);
         lottoNumbers.sort(Comparator.naturalOrder());
         return lottoNumbers;
     }

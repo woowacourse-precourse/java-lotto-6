a143711c021d2598a3c62b434e97b6d6ead16030
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-07 14:40:21 +0900

    feat(LottoMachine): add method that prints out lotto list in winning lotto

--- src/main/java/lotto/view/SystemOutput.java
+++ src/main/java/lotto/view/SystemOutput.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.Lotto;
+
+import java.util.List;
+import main.java.lotto.domain.WinningLottos;
 public class SystemOutput {
     // 게임 개수를 출력한다
     // 발행된 로또들을 출력한다
@@ -11,4 +15,13 @@ public class SystemOutput {
 
         System.out.printf((OUTPUT_MESSAGE) + "%n",(purchaseAmount/1000));
     }
+
+    public static void printWiningLotto(WinningLottos winningLottos) {
+        List<Lotto> winningLottoGroup = winningLottos.getWiningLottos();
+        for (Lotto lotto : winningLottoGroup) {
+            System.out.println(lotto);
+        }
+    }
+
+
 }

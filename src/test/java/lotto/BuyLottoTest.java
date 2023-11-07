91693f561c7c9545a70b8c69e17e93ffd7a4d885
K-KY <kyuyoungk@naver.com>
2023-11-08 06:44:21 +0900

    Test(BuyLottoTest) : 로또구매 테스트

---
+++ src/test/java/lotto/BuyLottoTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BuyLottoTest {
+
+    @Test
+    void 입력한_금액만큼_로또_구매() {
+        Money money = new Money(10000);
+        BuyLotto buyLotto = new BuyLotto();
+        assertThat(buyLotto.lotto(money)).isInstanceOf(LottoNumbers.class);
+    }
+}

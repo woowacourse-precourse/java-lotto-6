f04181d12cc96e1dde14d4682d6beaf5083923a9
twoosky <lhjae3@naver.com>
2023-11-08 07:34:53 +0900

    feat: 로또 번호 중복 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,36 @@
-package lotto;
+package lotto.domain;
+
+import static lotto.utils.ErrorMessages.LOTTO_NUMBER_DUPLICATION;
+import static lotto.utils.ErrorMessages.LOTTO_OUT_OF_SIZE;
 
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> lotto;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        validateDuplicate(numbers);
+        this.lotto = createLotto(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_OUT_OF_SIZE);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != getUniqueSize(numbers)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATION);
+        }
+    }
+
+    private long getUniqueSize(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count();
+    }
+
+
 }

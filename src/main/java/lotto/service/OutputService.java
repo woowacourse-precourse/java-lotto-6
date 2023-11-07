14c17b003cfe95ef7e00de312f6f2cdcf46203c1
besky <onlybesky@gmail.com>
2023-11-07 20:06:54 +0900

    feat: 로또 당첨 내역 및 수익률을 출력하는 기능 구현
    - ReturnResultController, OutputService에 해당 기능 구현

---
+++ src/main/java/lotto/service/OutputService.java
@@ -0,0 +1,38 @@
+package lotto.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lotto.constant.LottoPrize;
+import lotto.constant.PrintMessage;
+import lotto.model.Lotto;
+
+public class OutputService {
+    public List<String> boughtResult(List<Lotto> lottos) {
+        List<String> list = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            list.add(lotto.getNumbers().toString());
+        }
+        return list;
+    }
+
+    public List<String> winResult(Map<LottoPrize, Integer> result) {
+        List<String> list = new ArrayList<>();
+        for (LottoPrize lottoPrize : result.keySet()) {
+            list.add(winReturnFormat(lottoPrize.getMessage(), result.get(lottoPrize)));
+        }
+        return list;
+    }
+
+    private String winReturnFormat(String message, int number) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(message);
+        sb.append(number);
+        sb.append(PrintMessage.NUMBER_UNIT.getMessage());
+        return sb.toString();
+    }
+
+    public String earningRateToFormat(float earningRate) {
+        return String.format("%.1f", earningRate);
+    }
+}

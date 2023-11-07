02540a14efcee6d092baacbd38992de587e324e9
Sur0n <skyzephyr333@gmail.com>
2023-11-08 07:50:40 +0900

    fix : Enums Messages change

--- src/main/java/lotto/Enums.java
+++ src/main/java/lotto/Enums.java
@@ -1,16 +1,16 @@
 package lotto;
 
-enum Enums {
+enum Messages {
     ASK_LOTTO_MONNEY("구입금액을 입력해 주세요."),
-    SAY_HOWMANY_BOUGHT("%d개를 구매했습니다."),
+    SAY_HOWMANY_BOUGHT("개를 구매했습니다."),
     //구매내역 내역 그냥 SET으로 출력 때리면 될듯? []로 되어있으니까.
     ASK_WINNING_PRIZE_NUMBERS("당첨 번호를 입력해 주세요."),
     ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    CHECK_MATCH_THREE("3개 일치 (5,000원) - %d개"),
-    CHECK_MATCH_FOUR("4개 일치 (50,000원) - %d개"),
-    CHECK_MATCH_FIVE("5개 일치 (1,500,000원) - %d개"),
-    CHECK_MATCH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    CHECK_MATCH_SIX("6개 일치 (2,000,000,000) - %d개"),
+    CHECK_MATCH_THREE("3개 일치 (5,000원) - "),
+    CHECK_MATCH_FOUR("4개 일치 (50,000원) - "),
+    CHECK_MATCH_FIVE("5개 일치 (1,500,000원) - "),
+    CHECK_MATCH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    CHECK_MATCH_SIX("6개 일치 (2,000,000,000원) - "),
     CHECK_EARN_RATIO("총 수익률은 %f%입니다."),
     OUTPUT_ERROR("[ERROR] "),
     OUTPUT_ERROR_THOUSAND("1000원으로 나누어 떨어지게 자료를 입력하셔야 합니다."),
@@ -25,11 +25,11 @@ enum Enums {
     // print 출력값 포장할 것
     final private String PrintInterface;
 
-    String getInterface() {
+    String getMessages() {
         return PrintInterface;
     }
 
-    private Enums(String printInterface) {
+    private Messages(String printInterface) {
         this.PrintInterface = printInterface;
     }
 }
@@ -41,11 +41,11 @@ enum MagicNumbers {
     // print 출력값 포장할 것
     final private int PrintInterface;
 
-    int getInterface() {
+    public int getMagicNumbers() {
         return PrintInterface;
     }
 
-    private MagicNumbers(int printInterface) {
+    MagicNumbers(int printInterface) {
         this.PrintInterface = printInterface;
     }
 }

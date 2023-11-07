eb7188dbae1fdcd87ed29fa9e7744afd03678711
dsadara <chaehyun.forest@gmail.com>
2023-11-07 19:46:49 +0900

    feat: 금액 입력 안내문 출력

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    public void run() {
+        OutputView.printBuyLottoPromptMessage();
+    }
+
+}

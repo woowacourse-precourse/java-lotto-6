94c76e49d5580b21133061fa0e589f1b2193467a
LeeJaeHong <0915jhlee@daum.net>
2023-11-07 13:24:27 +0900

    commment: 테스트 메시지

--- src/test/java/lotto/print/PrintOutputTest.java
+++ src/test/java/lotto/print/PrintOutputTest.java
@@ -1,6 +1,7 @@
 package lotto.print;
 
 import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 
 class PrintOutputTest {
     PrintOutput printOutput = new PrintOutput();
+    @DisplayName("랜덤 로또 메시지 출력")
     @Test
     void printRandomLottosNumbersTest() {
         List<Lotto> lottos = new ArrayList<>();
@@ -20,13 +22,14 @@ class PrintOutputTest {
 
         printOutput.printRandomLottosNumbers(lottos);
     }
+    @DisplayName("통계 메시지 출력")
     @Test
     void printStats(){
         int[] stats={0, 1, 1, 1, 1, 1};
         double percent=100.4567;
         printOutput.printStats(stats, percent);
     }
-
+    @DisplayName("몇개 구입했는지 출력")
     @Test
     void printPurchasedLottoNumber(){
         printOutput.printPurchasedLottoNumber(10000);

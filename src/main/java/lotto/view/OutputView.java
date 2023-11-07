f0fdea0978bde1226b0b52b635d7d9fc2603ebfe
taehoon <xogns1514@gmail.com>
2023-11-07 17:46:08 +0900

    fix(OutputView): 당첨 통계 출력 기능
    - 당첨 통계를 순서대로 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,23 @@
 package lotto.view;
 
+import static lotto.model.Prize.SECOND;
+
+import java.text.DecimalFormat;
+import java.util.Map;
 import lotto.model.LottoPlayerNumber;
 import lotto.model.LottoPlayerNumbers;
+import lotto.model.Prize;
+import lotto.model.Statistics;
 
 public class OutputView {
 
     private enum OutputMessage {
-        NUMBER_OF_LOTTO_TICKETS("%d개를 구매했습니다.\n");
+        NUMBER_OF_LOTTO_TICKETS("%d개를 구매했습니다.\n"),
+        OUTPUT_STATISTICS("당첨 통계\n---"),
+        OUTPUT_MATCH_RESULT("%d개 일치 (%s원) - %d개\n"),
+        OUTPUT_SECOND_MATCH_RESULT("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n")
+        ;
+
 
         private final String message;
 
@@ -24,5 +35,28 @@ public class OutputView {
                 .stream()
                 .map(LottoPlayerNumber::getLottoPlayerNumber)
                 .forEach(System.out::println);
+    }
+
+    public static void outputStatistics(Statistics statistics) {
+
+        System.out.println(OutputMessage.OUTPUT_STATISTICS.message);
+        Map<Prize, Integer> result = statistics.getResult();
+        DecimalFormat formatter = new DecimalFormat("###,###");
+
+        for (Map.Entry<Prize, Integer> entry : result.entrySet()) {
+            if (entry.getKey() == Prize.NONE) continue;
+            if (entry.getKey() == SECOND) {
+                System.out.printf(OutputMessage.OUTPUT_SECOND_MATCH_RESULT.message,
+                        SECOND.getMatch(),
+                        formatter.format(SECOND.getPrize()),
+                        entry.getValue());
+                continue;
+            }
+
+            System.out.printf(OutputMessage.OUTPUT_MATCH_RESULT.message
+                    ,entry.getKey().getMatch(),
+                    formatter.format(entry.getKey().getPrize()),
+                    entry.getValue());
+        }
     }
 }

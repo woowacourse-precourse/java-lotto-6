fee4c6a8cd215c8b4a18f466ad38e554b4975666
toychip <dlawnsgud427@naver.com>
2023-11-07 11:13:39 +0900

    feat: 구입 로또의 등수와 개수를 갖는 LottoResult 추가

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<LottoRank, Integer> results;
+
+    public LottoResult(Map<LottoRank, Integer> results) {
+        this.results = results;
+    }
+
+    public static LottoResult determineWinnings(WinningLotto winningLotto, Lottos lottos) {
+        EnumMap<LottoRank, Integer> results = new EnumMap<>(LottoRank.class);
+        for (Lotto lotto : lottos.getLottos()) {
+            // 현재 로또가 몇등인지 구함
+            LottoRank rank = winningLotto.match(lotto);
+            results.merge(rank, 1, Integer::sum);
+        }
+        return new LottoResult(results);
+    }
+
+    public int getCount(LottoRank rank) {
+        return results.getOrDefault(rank, 0);
+    }
+
+
+}

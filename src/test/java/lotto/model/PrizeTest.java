b638e5283b6f9d9268611cdfcb37f49498546dbe
MinseokGo <rhalstjr1999@naver.com>
2023-11-07 21:22:02 +0900

    test($Prize): 4등 당첨을 판별한다.

--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +20,20 @@ class PrizeTest extends NsTest {
         System.out.println("fifth = " + fifth);
 
         assertThat(output()).contains("3개 일치 (5,000원)");
+    }
+
+    @DisplayName("4등 당첨을 판별한다.")
+    @Test
+    void isPrizeFourth() {
+        final int count = 4;
+        final boolean isBonus = false;
+        final Prize fourth = Arrays.stream(Prize.values())
+                .filter(prize -> prize.judge(count, isBonus))
+                .findFirst()
+                .orElse(Prize.NONE);
+        System.out.println("fourth = " + fourth);
+
+        assertThat(output()).contains("4개 일치 (50,000원)");
     }
 
     @Override

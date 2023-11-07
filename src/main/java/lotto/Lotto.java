417a0ffcc3b76ee61c65d174a1f7c2f6c7eab480
nayonsoso <panda0329@naver.com>
2023-11-07 20:47:06 +0900

    feat: 로또 번호의 갯수 불일치시 예외 발생 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,15 +6,13 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLengthIsSix(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLengthIsSix(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또는 여섯개의 숫자로 구성되어야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }

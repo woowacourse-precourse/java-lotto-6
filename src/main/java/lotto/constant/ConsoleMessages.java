2ac041b9b2904dfaab28c88241895aec2d352045
Minyongggg <naminyong97@gmail.com>
2023-11-08 00:51:52 +0900

    feat: 콘솔 메시지 enum 추가

---
+++ src/main/java/lotto/constant/ConsoleMessages.java
@@ -0,0 +1,22 @@
+package lotto.constant;
+
+public enum ConsoleMessages {
+    PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
+    LOTTO_PURCHASED_MESSAGE("%d개를 구매했습니다."),
+    WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+    WINNING_STATS_MESSAGE("당첨 통계\n---"),
+    WINNING_AWARDS("%d개 일치 (%s원) - %d개"),
+    WINNING_BONUS_AWARDS("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    TOTAL_PROFIT("총 수익률은 %f%입니다.");
+
+    private String value;
+
+    private ConsoleMessages(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

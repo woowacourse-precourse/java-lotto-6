28ff757753aa3440e9aa8e1f15c499b019ca2031
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:36:53 +0900

    feat(BonusNumber): 보너스 번호 자연수에 대한 검증 추가

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.util.Validator;
+
+public class BonusNumber {
+    private final int bonus;
+    public BonusNumber(WinningNumber winningNumber, String number) {
+        int bonusNumber = Integer.parseInt(number);
+        validateBonusNumber(winningNumber.getLotto().getNumbers(), bonusNumber);
+
+        this.bonus = bonusNumber;
+    }
+
+    private static void validateBonusNumber(List<Integer> lotto, int bonus) {
+        Validator.validateInputIsNatural(bonus);
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}

4152eba15a230eea5c274f84c4e99a2a1633334d
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-07 10:46:26 +0900

    feat(LottoOutput) : 기능 워딩 수정과 로또 번호를 한 줄 출력한다 기능 구현

--- src/main/java/lotto/view/LottoOutput.java
+++ src/main/java/lotto/view/LottoOutput.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class LottoOutput {
 
     public String printRandomLotto(List<Integer> lottoNumbers){
+        StringBuffer stringBuffer = new StringBuffer();
 
+        stringBuffer.append("[");
+        int lottoLength = lottoNumbers.size();
+        for(int i = 0; i < lottoLength - 1; i++){
+            stringBuffer.append(lottoNumbers.get(i)+", ");
+        }
+        stringBuffer.append(lottoNumbers.get(lottoLength - 1)+"]");
+        String returnValue = stringBuffer.toString();
+        System.out.println(returnValue);
+        return returnValue;
     }
 }

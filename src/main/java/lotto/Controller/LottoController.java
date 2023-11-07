cf8ccab2463076c3353acd62e1e224a303260ad2
Sanghee0820 <lsh277604@gmail.com>
2023-11-07 21:43:26 +0900

    Fix: generate WinningLotto -> LottoDraw

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lotto.Domain.Human;
 import lotto.Domain.Lotto;
+import lotto.Domain.LottoDraw;
 import lotto.Domain.WinningLotto;
 import lotto.RandomGenerator.LottoGenerator;
 
 public class LottoController {
     private Human human;
-    private WinningLotto winningLotto;
+    private LottoDraw lottoDraw;
 
     public void gameStart(String budgets) {
         // Exception Checking will be here
@@ -41,14 +42,14 @@ public class LottoController {
     public void raffleLotto(String normalNumbers, String bonusNumbers) {
         // Exception Checking will be here
         // If Exception occur -> return
-        winningLotto = new WinningLotto(
-                Arrays.stream(normalNumbers.split(","))
-                        .map(String::trim) // 부분 문자열의 앞뒤 공백 제거
-                        .map(Integer::parseInt) // 문자열을 정수로 변환
-                        .collect(Collectors.toList()),
-                Integer.valueOf(bonusNumbers)
+        lottoDraw = new LottoDraw(
+                new WinningLotto(
+                        Arrays.stream(normalNumbers.split(","))
+                                .map(String::trim) // 부분 문자열의 앞뒤 공백 제거
+                                .map(Integer::parseInt) // 문자열을 정수로 변환
+                                .collect(Collectors.toList()),
+                        Integer.valueOf(bonusNumbers))
         );
-        System.out.println(winningLotto);
         // return Successful
 
     }

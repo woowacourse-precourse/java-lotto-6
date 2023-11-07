06b8fc13d45768188aff9336c9503d2379a8ca61
Junobee25 <wnsdhqo@naver.com>
2023-11-07 18:02:14 +0900

    feat(VerificationView): verificationViewRangeInputBonusNumber 예외 처리 뷰 메서드 구현

--- src/main/java/lotto/view/VerificationView.java
+++ src/main/java/lotto/view/VerificationView.java
@@ -4,6 +4,7 @@ public class VerificationView {
 
     private final static String INPUT_MONEY_ERROR = "[ERROR] 구입 금액은 최소 1000원 이상, 1000원 단위의 금액입니다.";
     private final static String STRING_TO_LONG_ERROR = "[ERROR] 입력 값은 숫자로만 이루어져 있어야 합니다.";
+    private final static String INPUT_BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 1 부터 45 까지의 숫자여야만 합니다.";
     private final static String STRING_TO_INTEGER_ERROR = "[ERROR] 입력 값은 숫자로만 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1 부터 45 까지의 숫자로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_LENGTH_ERROR = "[ERROR] 로또 번호 6자리로 이루어져 있어야 합니다.";
@@ -11,7 +12,9 @@ public class VerificationView {
     public static void verificationViewRangeInputMoney() {
         System.out.println(INPUT_MONEY_ERROR);
     }
-
+    public static void verificationViewRangeInputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_ERROR);
+    }
     public static void verificationViewStringToLong() {
         System.out.println(STRING_TO_LONG_ERROR);
     }

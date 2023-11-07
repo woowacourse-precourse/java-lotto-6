77bfd86fea9a788c6f9f124fe936d766dd575ac9
kchranger <kchranger@gmail.com>
2023-11-07 23:44:47 +0900

    feature(exception) : do not allow duplicated number in lotto
    add error message about size check validation

---
+++ src/main/java/Constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package Constant;
+
+public class ErrorMessage {
+    public static final String ERROR_MSG_DUPLICATED_LOTTO_NUMBER = "[ERROR] 생성된 번호 중, 중복된 숫자가 있습니다.";
+    public static final String ERROR_MSG_NUMBER_OF_LOTTO_IS_6 = "[ERROR] 로또 숫자는 6자리 이어야 합니다.";
+
+}
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import static Constant.ErrorMessage.ERROR_MSG_DUPLICATED_LOTTO_NUMBER;
+import static Constant.ErrorMessage.ERROR_MSG_NUMBER_OF_LOTTO_IS_6;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -12,7 +15,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_MSG_NUMBER_OF_LOTTO_IS_6);
+        }
+        checkDupilcatedNumber(numbers);
+    }
+
+    public  void checkDupilcatedNumber(List<Integer> value) throws IllegalArgumentException{
+        List<Integer> testValue = new ArrayList<>(value);
+        for (Integer oneValue : value) {
+            testValue.remove(oneValue);
+            if (testValue.contains(oneValue)) {
+                throw new IllegalArgumentException(ERROR_MSG_DUPLICATED_LOTTO_NUMBER);
+            }
         }
     }
 

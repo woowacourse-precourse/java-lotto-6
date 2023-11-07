3b615c1303d2ae09ee1bcaef76577f30462c2389
kylim99 <zxzx05050@naver.com>
2023-11-07 21:32:01 +0900

    feat: WinnerLotto에 bonus validation추가구현

--- src/main/java/lotto/model/WinnerLotto.java
+++ src/main/java/lotto/model/WinnerLotto.java
@@ -1,14 +1,39 @@
 package lotto.model;
 
+import static lotto.util.Constant.END_NUMBER;
+import static lotto.util.Constant.START_NUMBER;
+
 import java.util.Arrays;
+import lotto.util.ExceptionMessage;
 
 public class WinnerLotto {
-    Lotto WinnerLotto;
+    Lotto winnerLotto;
     int bonus;
 
     public WinnerLotto(Lotto winnerLotto, int bonus) {
-        this.WinnerLotto = winnerLotto;
+        this.winnerLotto = winnerLotto;
+        validation(bonus);
         this.bonus = bonus;
+    }
+
+    private void validation(int bonus) {
+        validateRange(bonus);
+        validateDuplication(bonus);
+    }
+    private void validateRange(int bonus) {
+        if (isOutOfRange(bonus)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT_RANGE.getMessage());
+        }
+    }
+
+    private boolean isOutOfRange(int bonus) {
+        return START_NUMBER > bonus || END_NUMBER < bonus;
+    }
+
+    private void validateDuplication(int bonus) {
+        if (winnerLotto.contains(bonus)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_BONUS_IS_CONTAIN_NUMBERS.getMessage());
+        }
     }
 
     public Rank compare(Lotto lotto) {
@@ -23,7 +48,7 @@ public class WinnerLotto {
     private int getMatchCount(Lotto lotto) {
         return (int) lotto.getNumbers()
                 .stream()
-                .filter(WinnerLotto::contains)
+                .filter(winnerLotto::contains)
                 .count();
     }
 }

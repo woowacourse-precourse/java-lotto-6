65ecf8a7959d4393fd33a75eac62082c3a22520f
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-08 01:57:16 +0900

    test : 금액을 1000으로 나눈 시도횟수 구하기 테스트코드작성

--- src/test/java/lotto/domain/PlayerLottoAmountTest.java
+++ src/test/java/lotto/domain/PlayerLottoAmountTest.java
@@ -1,4 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PlayerLottoAmountTest {
-  
+    @Test
+    void 금액을_1000으로나눈_amount구하기() {
+        PlayerLottoAmount playerLottoAmount = new PlayerLottoAmount("5000");
+        int result = playerLottoAmount.calculateLottoCount();
+        Assertions.assertThat(result).isEqualTo(5);
+    }
 }

a8fbb557b6d53e523b34a651b35d00084c9a7665
Choijake <shameless8@naver.com>
2023-11-08 00:05:44 +0900

    Feat : InputHandler 입력 값 검증을 위한 클래스 추가

--- src/main/java/lotto/domain/InputHandler.java
+++ src/main/java/lotto/domain/InputHandler.java
@@ -3,8 +3,19 @@ package lotto.domain;
 import lotto.view.InputView;
 
 public class InputHandler {
+    //검증하는 부분 추가??
     public static String inputWinningNumber() {
         String inputNumber = InputView.inputWinningLottoNumber();
         return inputNumber;
+    }
+
+    public static int inputBonusNumber() {
+        int bonus = InputView.inputBonusNumber();
+        return bonus;
+
+    }
+
+    public static int inputAmountNumber() {
+        return InputView.inputAmount();
     }
 }

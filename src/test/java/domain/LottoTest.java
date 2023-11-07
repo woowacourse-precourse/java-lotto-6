ae2c423749625d09d054b8e96f563f61248c9353
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-07 18:46:31 +0900

    fix: 로또 클래스에서 정렬 메서드를 제거함에 따라 테스트 삭제

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -23,15 +22,6 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("로또 번호는 오름차순으로 정렬 된다.")
-    @Test
-    void lottoNumberSortByAscendingOrder() {
-        List<Integer> numbers = new ArrayList<>(List.of(6,4,3,2,1,5));
-        Lotto lotto = new Lotto(numbers);
-        Collections.sort(numbers);
-        assertEquals(lotto.getNumbers(),numbers);
     }
 
     @DisplayName("주어진 번호가 로또 번호에 포함되어 있는지 알려준다.")

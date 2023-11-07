380e60932819dba4b74130936e49f74dbf3eb3c4
sw801733 <sw801733@naver.com>
2023-11-07 22:59:44 +0900

    feat : OutputView 클래스 랜덤 로또 번호 생성 기능 추가
    - 구매 횟수에 맞게 생성한 로또 번호들을 출력하는 showRandomNumbers 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,22 @@
 package lotto.view;
 
+import java.util.List;
 import lotto.domain.LottoPurchase;
 
 public class OutputView {
 
     private final int lottoPurchase;
+    private final List<List<Integer>> randomNumbers;
 
-    public OutputView(int lottoPurchase) {
+    public OutputView(int lottoPurchase, List<List<Integer>> randomNumbers) {
         this.lottoPurchase = lottoPurchase;
+        this.randomNumbers = randomNumbers;
     }
 
-
+    public void showRandomNumbers() {
+        for(int i = 0; i < lottoPurchase; i++) {
+            System.out.println(randomNumbers.get(i));
+        }
+    }
 
 }

91d3baef5f8c7a38e82ee3db35ff5012ea95ab2d
wda067 <mg6359@naver.com>
2023-11-07 14:11:11 +0900

    refactor(Application): 패키지 변경으로 인한 import 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+
 public class Application {
     public static void main(String[] args) {
         LottoGameController lottoGameController = new LottoGameController();

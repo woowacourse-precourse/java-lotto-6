a99ce173f8778430e7c3f7f0e92638c46f726e5c
kimmk89 <kmk7347@naver.com>
2023-11-07 17:44:03 +0900

    [feat] 기능구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final static int PERMITTED_LOTTO_LENGTH = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validate2(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +20,19 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
+    private void validate2(List<Integer> numbers) {
+        if(hasDuplicate(numbers))
+        {
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!set.add(number)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

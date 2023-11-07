07f602f0c91a34181b1b46f4ac610804cfd20424
bamm2 <xxknu88@naver.com>
2023-11-08 00:44:51 +0900

    style(LottoApplicationException): 줄바꿈 제거

--- src/main/java/lotto/exception/LottoApplicationException.java
+++ src/main/java/lotto/exception/LottoApplicationException.java
@@ -3,7 +3,7 @@ package lotto.exception;
 public class LottoApplicationException extends RuntimeException {
 
     private ExceptionMessage exceptionMessage;
-    
+
     public LottoApplicationException(ExceptionMessage exceptionMessage) {
         super(exceptionMessage.getMessage());
         this.exceptionMessage = exceptionMessage;

07fcd0f02d4f8c3e3df682b82e1ab48530225ffa
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 00:01:48 +0900

    feat: 구매 금액 입력 안내 출력 함수

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import lotto.message.Message;
+
+public class Output {
+    public void printEnteringAmountPrompt() {
+        System.out.println(Message.PROMPT_ENTERING_PURCHASE_AMOUNT);
+    }
+}

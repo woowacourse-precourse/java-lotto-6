a8488e53e1ba4a2cd2a6d2efa1e4bfe6843f042a
JDUB9823 <jeong98231@gmail.com>
2023-11-07 10:55:39 +0900

    feat: 구입 금액 입력 문구 출력 기능구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,10 @@
+package lotto.controller;
+
+import lotto.model.LottoPurchasingAmount;
+import lotto.view.OutputView;
+
+public class LottoController {
+    public void initLotto() {
+        OutputView.askLottoPurchasingAmount();
+    }
+}

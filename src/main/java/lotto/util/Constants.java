802caa1fb559af1a615e6bb0925236758cc74982
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:14:16 +0900

    feat(Constants): 0 상수 추가

--- src/main/java/lotto/util/Constants.java
+++ src/main/java/lotto/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_MIN_AMOUNT = 1000;
+    public static final int ZERO = 0;
 }

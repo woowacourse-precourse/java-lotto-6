4accbb72e0701c88d329590c26e94585f14e6830
Sur0n <skyzephyr333@gmail.com>
2023-11-08 06:37:09 +0900

    feat : Build GameController

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
-
+import lotto.GameController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController GC = new GameController();
+
     }
 }
--- src/main/java/lotto/GameController.java
+++ src/main/java/lotto/GameController.java
@@ -1,18 +1,36 @@
 package lotto;
+
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.Lotto;
 import lotto.GameInput;
 import lotto.GameOutput;
 
 public class GameController {
-    private GameController(){
+
+    public GameController() {
 
         GameOutput.printUserCashInterface();
-        GameInput.getUserCash();
+        int moneys = GameInput.getUserCash();
+        //System.out.println(moneys);
         GameOutput.printUserHopeLottoNumbersInterface();
-        GameInput.getUserHopeLottoNumbers();
+        List<Integer> numbers = GameInput.getUserHopeLottoNumbers();
+        //System.out.println(numbers);
+        Lotto lottoGame = new Lotto(numbers);
+        int tickets = lottoGame.buyLottoTickets(moneys);
         GameOutput.getUserHopeBonusNumbersInterface();
-        GameInput.getUserHopeBonusNumbers();
+        int bonusNumber = GameInput.getUserHopeBonusNumbers();
+        lottoGame.setBonusNumber(bonusNumber);
+        //System.out.println(bonusNumber);
+
+        List<String> ticketNumbers = GameOutput.printBuyTickets( lottoGame , tickets );
+        int earnRatio = GameInput.earnMoney(numbers,ticketNumbers, bonusNumber)/moneys;
+        System.out.print("총 수익률은 %f입니다." , moneys);
+
+
+
         Console.close();
     }
+
+
 }

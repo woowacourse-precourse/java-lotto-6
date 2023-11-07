0b618e4ed6799018c137afd7359da9bff4c54717
JiEung2 <https://github.com/JiEung2>
2023-11-07 23:07:06 +0900

    Test: LottosTest에 있던 테스트 LottoTest로 이동

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import lotto.model.LottoGenerator;
+import lotto.utils.FixNumberGenerator;
+import lotto.utils.NumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +26,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    void 몇_개의_숫자가_일치하는지_테스트() {
+        //given
+        int buyPrice = 1000;
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        NumberGenerator numberGenerator = new FixNumberGenerator();
+        Lotto lotto = new Lotto(numberGenerator.generateNumber());
+        List<Integer> winningList = List.of(1, 2, 3, 4, 5, 45);
+        //when
+        int count = lotto.checkNumbers(winningList);
+        //then
+        assertThat(count).isEqualTo(5);
+    }
+
 }

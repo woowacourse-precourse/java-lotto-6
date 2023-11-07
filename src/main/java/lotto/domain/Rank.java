d5b9e1158949e8adadaf6e82d2582cac297daa48
PARK9898 <wjddjs21@naver.com>
2023-11-07 22:45:03 +0900

    "refactor: message 변수 추가"

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,20 +4,22 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    ZERO(0,0),
-    FIFTH(3,5_000),
-    FOURTH(4,50_000),
-    THIRD(5,1_500_000),
-    SECOND(5,30_000_000),
-    FIRST(6, 2_000_000_000)
+    ZERO(0,0, " "),
+    FIFTH(3,5_000, "3개 일치 (5,000원) - "),
+    FOURTH(4,50_000, "4개 일치 (50,000원) - "),
+    THIRD(5,1_500_000, "5개 일치 (1,500,000원) - "),
+    SECOND(5,30_000_000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - ")
     ;
 
     private final int sameNumberCount;
     private final int prizeMoney;
+    private final String message;
 
-    Rank(int sameNumberCount, int prizeMoney) {
+    Rank(int sameNumberCount, int prizeMoney, String message) {
         this.sameNumberCount = sameNumberCount;
         this.prizeMoney = prizeMoney;
+        this.message = message;
     }
 
     public static Rank decideRank(int sameNumberCount, boolean isContainBonusNumber) {
@@ -32,5 +34,9 @@ public enum Rank {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }

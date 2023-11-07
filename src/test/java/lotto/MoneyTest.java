5aae97e270a15cd8fb1d1213244d33b53d45fed0
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-07 17:34:03 +0900

    refactor: MoneyTest에 ParameterizedTest 적용

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -2,27 +2,23 @@ package lotto;
 
 import lotto.domain.Money;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class MoneyTest {
-    @Test
-    public void 로또_구매_단위_테스트() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(5500));
+    @DisplayName("로또 구매금액 유효성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {5500, 100_000_001, 100})
+    public void 로또_금액_유효성_테스트(int inputMoney) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(inputMoney));
     }
 
+    @DisplayName("로또 구매개수 테스트")
     @Test
-    public void 로또_최대금액_테스트() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(100_000_001));
-    }
-
-    @Test
-    public void 로또_최소금액_테스트() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Money(100));
-    }
-
-    @Test
-    public void 로또_구매개수_테스트() {
+    public void 로또_구매액과_구매개수가_일치하는지_테스트() {
         Money money = new Money(10000);
         Assertions.assertEquals(10, money.calculateTicketCount());
     }

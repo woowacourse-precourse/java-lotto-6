fd46e2fc0194fb5eba6353a8e5574573cb478547
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 00:06:03 +0900

    feat: 구매 금액이 천원 단위인지 확인하는 validPurchaseDivideUp1000 함수 생성

--- src/main/java/validator/LottoPurchaseValidator.java
+++ src/main/java/validator/LottoPurchaseValidator.java
@@ -6,4 +6,10 @@ public class LottoPurchaseValidator {
             throw new IllegalArgumentException("[ERROR] 로또 최소 구입금액은 1000원 입니다.");
         }
     }
+
+    void validPurchaseDivideUp1000(int purchaseAmount) {
+        if (purchaseAmount % 1000 == 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1000원 단위여야 합니다.");
+        }
+    }
 }

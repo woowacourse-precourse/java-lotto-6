edc4407a8933e0260e4fd333cdc9327fe2a5a9f6
bandall <jsm5315@naver.com>
2023-11-07 22:10:44 +0900

    fix: Lotto 생성자 검증 메서드 추가
    Lotto 객체 생성 시 로또 번호 범위 검증 메서드 추가

--- src/main/java/lotto/model/domain/Lotto.java
+++ src/main/java/lotto/model/domain/Lotto.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import lotto.configuration.ErrorMessage;
+import lotto.configuration.LottoConstants;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        isDuplicated(numbers);
+        isUnique(numbers);
+        isNumberInRange(numbers);
         this.numbers = numbers;
     }
 
@@ -21,10 +23,18 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    private void isDuplicated(List<Integer> numbers) {
+    private void isUnique(List<Integer> numbers) {
         HashSet<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
         if (nonDuplicateNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_DUPLICATED);
+        }
+    }
+
+    private void isNumberInRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LottoConstants.MIN_LOTTO_NUM || number > LottoConstants.MAX_LOTTO_NUM) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_NOT_IN_RANGE);
+            }
         }
     }
 

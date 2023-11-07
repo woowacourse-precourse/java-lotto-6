46caaeeee8161b816b66feb2bac7f0ab71d548d0
JP-company <wjsdj2008@gmail.com>
2023-11-07 20:38:43 +0900

    Refactor: [수정] 로또 금액 입력 기능을 User객체와 UIFunction 객체로 분리
    로또 금액 입력 기능에서
    1. 사용자의 입력을 받는 기능
    2. 로또 금액을 입력받고 검증 후 실패 시 다시 입력받는 기능
    을 2개의 객체로 분리했다.

---
+++ src/main/java/lotto/domain/User.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import lotto.function.Parser;
+import lotto.function.UIFunction;
+
+import java.util.List;
+
+public class User {
+    public User() {}
+
+    public List<Lotto> buyLotto() {
+        try {
+            String inputPayment = UIFunction.requestInput("구입 금액을 입력해 주세요.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return buyLotto();
+        }
+    }
+}

b922bcda7c445959a56402f93f9fd6ff12d2f7bc
ycis5013@naver.com <ycis5013@naver.com>
2023-11-08 01:00:52 +0900

    feat(Application) : 로또 구입 금액을 설정하는 함수 setPrice() 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+    static InputView inputView = InputView.getInstance();
+    static OutputView outputView = OutputView.getInstance();
+    static Validator validator = Validator.getInstance();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        inputView = InputView.getInstance();
+        outputView = OutputView.getInstance();
+        validator = Validator.getInstance();
+
+        int price = setPrice();
+
+    }
+
+    static int setPrice() {
+        boolean askAgain = true;
+        int price = 0;
+
+        while (askAgain) {
+            askAgain = false;
+            String priceInput = inputView.inputPrice();
+            try {
+                price = validator.validatePrice(priceInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                askAgain = true;
+            }
+        }
+        return price;
     }
 }

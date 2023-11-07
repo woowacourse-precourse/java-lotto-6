f584ceabb6661786d6c3a77622058e74e647d619
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 00:11:25 +0900

    feat: 구매 금액이 정수로 이루어졌는지 확인하는 vaildIntegerPurchasAmount 함수 생성

--- src/main/java/validator/LottoPurchaseValidator.java
+++ src/main/java/validator/LottoPurchaseValidator.java
@@ -12,4 +12,11 @@ public class LottoPurchaseValidator {
             throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1000원 단위여야 합니다.");
         }
     }
+
+    void vaildIntegerPurchasAmount(String input) {
+        String regex = "[+-]?\\d*(\\.\\d+)?";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.");
+        }
+    }
 }

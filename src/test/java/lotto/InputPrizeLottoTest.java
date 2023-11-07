91f1f52285f95c414dc704bad2b35990266773d5
Jaewon <choijawon99@gmail.com>
2023-11-08 00:17:58 +0900

    test(game): 당첨 로또 번호 입력 테스트 추가
    - 빈칸 입력
    - 입력 형태 오류
    - 입력 번호 개수
    - 입력 번호 범위 미만의 수
    - 입력 번호 범위 초과의 수
    - 입력 번호 중복

---
+++ src/test/java/lotto/InputPrizeLottoTest.java
@@ -0,0 +1,64 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputPrizeLottoTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 예외_테스트_빈칸_입력(){
+        assertSimpleTest(() -> {
+            runException("1000"," ");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_입력_형태_오류(){
+        assertSimpleTest(() -> {
+            runException("1000","1,2,3,4,5,");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_입력_번호_개수(){
+        assertSimpleTest(() -> {
+            runException("1000","1,2,3,4,5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_입력_번호_범위_작은수(){
+        assertSimpleTest(() -> {
+            runException("1000","-1,1,2,3,4,5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_입력_번호_범위_큰수(){
+        assertSimpleTest(() -> {
+            runException("1000","1,2,3,4,5,46");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_입력_번호_중복(){
+        assertSimpleTest(() -> {
+            runException("1000","1,2,3,4,5,5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}

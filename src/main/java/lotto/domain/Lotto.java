0ea5d9754e8b21288762f87ddc15cd0ac281ee4d
hyungsoolim <hyungsoolim0@gmail.com>
2023-11-08 05:28:22 +0900

    feat: 로또 구입시 번호 생성 유효성 검증 추가
    구입한 로또 갯수에 따라 랜덤 로또 번호 생성할 때 유효성 검증 및 예외처리 추가.
    예외 처리 목록
    - 중복된 숫자가 존재할 때
    - 1~45 범위에서 벗어난 숫자가 존재할 때
    - 생성된 숫자의 개수가 6개가 아닐 때

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.lotto.DuplicateNumberException;
+import lotto.exception.lotto.IllegalNumberCountException;
+import lotto.exception.lotto.OutOfRangeException;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,14 +13,35 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
         return numbers;
     }
+
+    private void validate(List<Integer> numbers) {
+        validateLottoNumberCount(numbers);
+        validateDuplicateNumber(numbers);
+        validateOutOfRangeNumber(numbers);
+    }
+
+    private void validateLottoNumberCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalNumberCountException();
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        long distinctCount = numbers.stream().distinct().count();
+        if (distinctCount < numbers.size()) {
+            throw new DuplicateNumberException();
+        }
+    }
+
+    private void validateOutOfRangeNumber(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < 1 || 45 < number) {
+                throw new OutOfRangeException();
+            }
+        });
+    }
+
 }

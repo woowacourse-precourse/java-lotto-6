339883add06b0fbc0a9c2dfe6e3be52fc29f1985
Bisi3asi <kueeng8008@naver.com>
2023-11-08 01:56:50 +0900

    feat(main) : add controller
    * 컨트롤러에 의한 실행 메소드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,10 @@
 package lotto;
 
 public class Application {
+    GameController gameController = new GameController();
+
     void boot(){
-        // todo : controller.start
+        gameController.start();
     }
 
     public static void main(String[] args) {

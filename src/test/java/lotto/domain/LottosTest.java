3488f430ad6959e21af15b02542a09d455f706da
sujnkim <blackout3210@naver.com>
2023-11-07 18:43:11 +0900

    test: LottosTest에 BeforeEach 추가

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,16 +9,21 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottosTest {
+    List<Lotto> lotto;
 
-    @DisplayName("모든 Lotto의 번호를 가져온다")
-    @Test
-    void getsAllLottoNumber() {
-        // given
-        List<Lotto> lotto = List.of(
+    @BeforeEach
+    void setUp() {
+        lotto = List.of(
                 new Lotto(List.of(43, 21, 23, 42, 41, 8)),
                 new Lotto(List.of(5, 3, 16, 11, 38, 32)),
                 new Lotto(List.of(22, 45, 5, 14, 1, 3))
         );
+    }
+
+    @DisplayName("[SUCCESS]Lottos에 포함된 모든 Lotto의 정렬된 번호를 가져온다")
+    @Test
+    void getsAllLottoNumber() {
+        // given
         Lottos lottos = new Lottos(lotto);
 
         // when
@@ -31,15 +37,10 @@ class LottosTest {
                         "[1, 3, 5, 14, 22, 45]"));
     }
 
-    @DisplayName("Lottos에 포함된 Lotto의 개수를 가져온다")
+    @DisplayName("[SUCCESS]Lottos에 포함된 Lotto의 개수를 가져온다")
     @Test
     void getLottosAmount() {
         // given
-        List<Lotto> lotto = List.of(
-                new Lotto(List.of(43, 21, 23, 42, 41, 8)),
-                new Lotto(List.of(5, 3, 16, 11, 38, 32)),
-                new Lotto(List.of(22, 45, 5, 14, 1, 3))
-        );
         Lottos lottos = new Lottos(lotto);
 
         // when
@@ -49,5 +50,4 @@ class LottosTest {
         assertThat(lottosAmount)
                 .isEqualTo(3);
     }
-
 }

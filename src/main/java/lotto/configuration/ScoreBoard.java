f34e9f02e9a54bb2eff8c319b52fe48349a289c0
Junobee25 <wnsdhqo@naver.com>
2023-11-07 13:46:58 +0900

    refactor(ScoreBoard): ScoreBoard key 값 수정

--- src/main/java/lotto/configuration/ScoreBoard.java
+++ src/main/java/lotto/configuration/ScoreBoard.java
@@ -1,24 +1,24 @@
 package lotto.configuration;
 
 public enum ScoreBoard {
-    FIRST(1, 0L),
-    SECOND(2, 0L),
-    THIRD(3, 0L),
-    FOURTH(4, 0L),
-    FIFTH(5, 0L),
-    SIXTH(6, 0L),
-    SEVENTH(7, 0L),
-    EIGHTH(8, 0L);
+    FIRST("1st", 0L),
+    SECOND("2nd", 0L),
+    THIRD("3rd", 0L),
+    FOURTH("4th", 0L),
+    FIFTH("5th", 0L),
+    SIXTH("6th", 0L),
+    SEVENTH("7th", 0L),
+    EIGHTH("8th", 0L);
 
-    private final long key;
+    private final String key;
     private final long value;
 
-    ScoreBoard(long key, long value) {
+    ScoreBoard(String key, long value) {
         this.key = key;
         this.value = value;
     }
 
-    public long getKey() {
+    public String getKey() {
         return key;
     }
 

83c08aa4e529cd8020f6de32b1abfef0a33298e9
swhyeon98 <smreo75@gmail.com>
2023-11-07 19:05:59 +0900

    feat: 로또 번호 출력 기능 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class LottoView {
@@ -19,9 +22,15 @@ public class LottoView {
         return Integer.parseInt(input);
     }
 
+    public void printLottos(List<Lotto> lottos) {
+        System.out.println();
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        lottos.forEach(lotto -> System.out.println(lotto));
+    }
+
     private void validateIsNumber(String input) {
         if (!NUMBER_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException("숫자만 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
         }
     }
 }

99ce1f0bfbacd2dccb942782503d4a65edf87022
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-08 03:23:07 +0900

    feat(domain/Utils): 입력 받은 문자열을 숫자 리스트로 만드는 기능 추가

--- src/main/java/domain/Utils.java
+++ src/main/java/domain/Utils.java
@@ -1,12 +1,29 @@
 package domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
 
     public static List<Integer> generateRandomNumbers(int startInclusive, int endInclusive, int count) {
         return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+    }
+
+    // 입력 받은 문자열을 숫자 배열로 변환
+    public static int[] stringToIntArray(String string) {
+        return Arrays.stream(string.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    // 입력 받은 문자열을 숫자 리스트로 변환
+    public static List<Integer> stringToIntegerList(String string) {
+        Validation.validateListParseInt(Arrays.asList(string.split(",")));
+        return Arrays.stream(stringToIntArray(string))
+                .boxed()
+                .collect(Collectors.toList());
     }
 
 }

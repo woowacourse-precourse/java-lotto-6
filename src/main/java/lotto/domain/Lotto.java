c2ab27a292cede73f31939ea1c70ffc43fb1d4f0
PeterChoi <94peterchoi@gmail.com>
2023-11-07 22:43:55 +0900

    feat: 로또 발행 시 유효성 검사 로직 보완
    - 6개의 번호 중 중복되는 번호가 없는지 확인 및 예외처리 메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.constant.LottoConsts.REQUIRED_COUNT;
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -11,8 +13,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != REQUIRED_COUNT) {
+            throw new IllegalArgumentException(String.format("[ERROR] 총 %d개의 번호가 입력되어야 합니다.", REQUIRED_COUNT));
+        }
+        if (numbers.stream().distinct().count() != REQUIRED_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 존재합니다.");
         }
     }
 

520a7089375811ce885e7396ed9437581f1be200
shining013 <nevertheless113@naver.com>
2023-11-07 18:11:37 +0900

    feat: 중복 입력 검증 메서드 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,19 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.model.Lotto;
 import lotto.view.OutputView;
 import lotto.model.Quantity;
+import lotto.model.RandomLotto;
+
+import lotto.view.OutputView;
 
 public class LottoController {
         private Lotto lotto;
+        private RandomLotto randomLotto;
         private int quantity;
+        private List<List<Integer>> randomNumbers;
+        private List<Integer> lottoNumbers;
 
         public LottoController() {
                 set();
@@ -16,10 +23,12 @@ public class LottoController {
 
         public void set() {
                 quantity = InputController.setQuantity();
+                randomNumbers = InputController.setRandomNumbers(quantity);
         }
 
         public void conduct() {
-
+                OutputView.printQuantity(quantity);
+                OutputView.printRandomLotto(randomNumbers);
         }
 
         public void showResult() {

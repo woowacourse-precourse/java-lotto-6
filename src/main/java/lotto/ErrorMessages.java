42e0b08b19e5ba5ddf53455c91a393050362e1b5
sleeg00 <slee000220@gmail.com>
2023-11-07 19:20:44 +0900

    Refector: 구매금액이 0일때 예외처리 Enum추가

--- src/main/java/lotto/ErrorMessages.java
+++ src/main/java/lotto/ErrorMessages.java
@@ -5,7 +5,8 @@ public enum ErrorMessages {
     INVALID_DIVIDE_NUMBER("[ERROR] 1,000으로 나누어 떨어지는 값을 입력해주세요."),
     INVALID_SIZE("[ERROR] 당첨 번호가 6개가 아닙니다."),
     INVALID_DUPLICATION("[ERROR] 중복된 수 발견."),
-    INVALID_RANGE("[ERROR] 로또번호는 1~45까지 입니다.");
+    INVALID_RANGE("[ERROR] 로또번호는 1~45까지 입니다."),
+    INVALID_ZERO("[ERROR] 0말고 다른값을 입력해주세요.");
 
     private String message;
 

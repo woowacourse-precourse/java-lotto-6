fb3a9ae8aa917f3654327c3dfecc89dabed4f1c0
hoyeon <highoption@naver.com>
2023-11-07 19:26:30 +0900

    refactor(WinningLotto) : 보너스 넘버와 당첨 넘버를 검증하는 메서드 생성
    - 보너스 넘버와 당첨 넘버의 중복이 있으면 안되기 때문에 생성

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,11 +1,22 @@
 package lotto.model;
 
 public class WinningLotto {
-    private final BonusNumber bonusNumber
+    private final BonusNumber bonusNumber;
     private final Lotto winningLotto;
 
     public WinningLotto(Lotto lotto, BonusNumber bonus) {
+        validateWinningLotto(lotto, bonus);
         winningLotto = lotto;
         bonusNumber = bonus;
+    }
+
+    public void validateWinningLotto(Lotto lotto, BonusNumber bonusNumber) {
+        isDuplicateWinningLotto(lotto, bonusNumber);
+    }
+
+    private void isDuplicateWinningLotto(Lotto lotto, BonusNumber bonusNumber) {
+        if (lotto.isContain(bonusNumber.getBonusNumber())) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
+        }
     }
 }

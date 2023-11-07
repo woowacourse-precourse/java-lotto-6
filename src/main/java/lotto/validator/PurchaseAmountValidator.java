d98e211c70fa9207a757875fa18a9b264e9b7320
doha <dohaahi@naver.com>
2023-11-08 01:59:44 +0900

    feat (PurchaseAmountValidator): 0일 경우 검증 메소드 추가

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -17,7 +17,14 @@ public class PurchaseAmountValidator {
     }
 
     public static void validatePurchaseAmount(final int amount) {
+        validatePurchaseAmountIsZero(amount);
         validatePurchaseAmountUnit(amount);
+    }
+
+    private static void validatePurchaseAmountIsZero(final int amount) {
+        if (0 == amount) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_IS_ZERO_MESSAGE.getMessage());
+        }
     }
 
     private static void validatePurchaseAmountUnit(final int amount) {

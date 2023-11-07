b35a911abee59719e4761fc9c664fab1acb3a72d
linirini <2001yerin@naver.com>
2023-11-07 19:57:27 +0900

    test : refactor Price into Amount

--- src/test/java/lotto/domain/AmountTest.java
+++ src/test/java/lotto/domain/AmountTest.java
@@ -15,30 +15,16 @@ class AmountTest{
     @DisplayName("구입 금액이 1000원 단위가 아니면 예외 처리한다.")
     @ParameterizedTest
     @ValueSource(ints = {100, 10100, 11200, 5100, 10})
-    void 구입_금액_단위_예외_처리(int price){
-        assertThatThrownBy(() -> new Price(price)).isInstanceOf(IllegalArgumentException.class)
+    void 구입_금액_단위_예외_처리(int amount){
+        assertThatThrownBy(() -> new Amount(amount)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionEnum.INVALID_PURCHASE_PRICE_UNIT.getMessage());
     }
 
     @DisplayName("구입 금액이 1000원 단위이다.")
     @ParameterizedTest
     @ValueSource(ints = {1000, 10000, 11000, 510000, 11000})
-    void 구입_금액_단위(int price){
-        assertThatNoException().isThrownBy(() -> new Price(price));
-    }
-
-    @DisplayName("구입 금액으로 발행할 로또 개수를 구한다.")
-    @Test
-    void 구입_금액으로_로또_개수_반환(){
-        //given
-        int purchasePrice = 101000;
-        Price price = new Price(purchasePrice);
-
-        //when
-        int lottoCount = price.getLottoCount();
-
-        //then
-        assertThat(lottoCount).isEqualTo(101);
+    void 구입_금액_단위(int amount){
+        assertThatNoException().isThrownBy(() -> new Amount(amount));
     }
 
 }

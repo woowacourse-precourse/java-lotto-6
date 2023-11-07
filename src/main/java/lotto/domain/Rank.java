be8258f3d1aea2319f998353ec15a7a56405fc11
Minsu Choi <cms102510@naver.com>
2023-11-08 02:01:41 +0900

    feat: 로또 당첨 번호를 나타내는 Enum

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+// 로또 당첨 번호를 나타내는 Enum
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}

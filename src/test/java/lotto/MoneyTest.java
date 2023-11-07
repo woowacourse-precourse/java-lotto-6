66d67c951a99dd2d3bf01dfa7351120daecf60bf
K-KY <kyuyoungk@naver.com>
2023-11-08 00:41:18 +0900

    Feat(MoneyTest) : `availableCount()` 테스트

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,21 @@ public class MoneyTest {
 
     @Test
     void 금액_검증_부족() {
-        assertThatThrownBy(() -> new Money(999)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 천보다 큰 금액을 입력해주세요.");
+        assertThatThrownBy(() -> new Money(999))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 천보다 큰 금액을 입력해주세요.");
     }
 
     @Test
     void 금액_검증_큼() {
-        assertThatThrownBy(() -> new Money(5000000)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 100만을 넘는 금액은 입력 할 수 없습니다.");
+        assertThatThrownBy(() -> new Money(5000000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 100만을 넘는 금액은 입력 할 수 없습니다.");
+    }
+
+    @Test
+    void 구매_가능한_로또() {
+        assertThat(new Money(10000).availableCount()).isEqualTo(10);
     }
 
 }

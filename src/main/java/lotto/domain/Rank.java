878edb01284ca2936c659cb15f0f8adfb9947e3b
elephantLikesApple <elephant890@naver.com>
2023-11-07 23:54:41 +0900

    fix(rank) : 로또 당첨 내역 계산 기능 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,36 +3,34 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    MISS(0, false, 0),
-    FIFTH(3, false, 5_000),
-    FOURTH(4, false, 50_000),
-    THIRD(5, false, 1_500_000),
-    SECOND(5, true, 30_000_000),
-    FIRST(6, false, 2_000_000_000);
+    MISS(0, 0),
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private final int matchCount;
-    private final boolean bonus;
     private final int prize;
 
-    Rank(int matchCount, boolean bonus, int prize) {
+    Rank(int matchCount, int prize) {
         this.matchCount = matchCount;
-        this.bonus = bonus;
         this.prize = prize;
     }
 
     public static Rank getRank(int matchCount, boolean matchBonus) {
+        if (matchCount == 5 && matchBonus) {
+            return SECOND;
+        }
+
         return Arrays.stream(values())
-                .filter(rank -> rank.getMatchCount() == matchCount && rank.isBonus() == matchBonus)
+                .filter(rank -> rank.getMatchCount() == matchCount)
                 .findAny()
                 .orElse(MISS);
     }
 
     public int getMatchCount() {
         return matchCount;
-    }
-
-    public boolean isBonus() {
-        return bonus;
     }
 
     public int getPrize() {

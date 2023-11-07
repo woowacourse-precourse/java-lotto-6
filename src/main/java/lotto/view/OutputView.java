aa264692c46cb5f510139cfd05a853d88d4cf5f3
ppdoli123 <ppdoli123@naver.com>
2023-11-07 16:49:22 +0900

    feat(OutputView): 당첨 통계를 출력하는 메소드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -15,6 +15,13 @@ import java.util.List;
             }
         }
 
-
+        public static void printResult(LottoResult result, int purchaseAmount) {
+            System.out.println("당첨 통계");
+            System.out.println("---------");
+            for (LottoEnum.LottoRank rank : LottoEnum.LottoRank.values()) {
+                System.out.println(rank + " - " + result.getCount(rank) + "개");
+            }
+            System.out.println("총 수익률은 " + result.calculateEarningsRate(purchaseAmount)*100 + "%입니다.");
+        }
     }
 

80d70b3243bdd88fe81f02ea0d47aca0d33f8d1c
ZZ3n <ndsrhkd@naver.com>
2023-11-07 20:34:59 +0900

    refactor(input): 에러 발생시 다시 받도록 수정
    에러 발생시 에러 내용을 출력하고 입력을 다시 받도록 수정하였다.

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -11,12 +11,18 @@ class InputView {
      * @return 1000 단위로 끊어진 로또 구매 금액.
      */
     public static LottoMoney getMoney() {
-        String input = Console.readLine();
-        try {
-            int money = Integer.parseInt(input);
-            return new LottoMoney(money);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 금액입니다.", e);
+        String input;
+
+        while (true) {
+            try {
+                input = Console.readLine();
+                int money = Integer.parseInt(input);
+                return new LottoMoney(money);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 1000원 단위로 입력해주세요.");
+            }
         }
     }
 }

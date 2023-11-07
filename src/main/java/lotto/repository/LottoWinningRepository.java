b9398cafd5e23c2b65c37518ecbb94c429e2d204
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-11-08 03:19:54 +0900

    feat: add lotto winning repository

--- src/main/java/lotto/repository/LottoWinningRepository.java
+++ src/main/java/lotto/repository/LottoWinningRepository.java
@@ -1,15 +1,27 @@
 package lotto.repository;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.LottoWinning;
 
 public class LottoWinningRepository {
     private static LottoWinning lottoWinning;
 
-    public void save(LottoWinning lottoWinning){
-        LottoWinningRepository.lottoWinning = lottoWinning;
+    public LottoWinningRepository(){
+        LottoWinningRepository.lottoWinning = new LottoWinning();
+    }
+
+    public void saveWinningNumbers(List<Integer> winningNumbers) {
+        getLottoWinning().setWinningNumbers(new Lotto(winningNumbers));
+    }
+
+    public void saveBonusNumber(int bonusNumber) {
+        getLottoWinning().setBonusNumber(bonusNumber);
     }
 
     public LottoWinning getLottoWinning(){
         return lottoWinning;
     }
+
+
 }

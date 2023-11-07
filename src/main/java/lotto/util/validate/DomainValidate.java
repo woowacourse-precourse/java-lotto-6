f58444dcd093f57b75e7869a881e1ba23e745885
Hayoon <hy97@sju.ac.kr>
2023-11-08 01:54:36 +0900

    fix(DomainInvalidate): validateOutOfRankingIndex() 랭킹 등수 값 수정 ('>' -> '>=')

--- src/main/java/lotto/util/validate/DomainValidate.java
+++ src/main/java/lotto/util/validate/DomainValidate.java
@@ -1,16 +1,18 @@
 package lotto.util.validate;
 
+import static lotto.util.constant.InvalidConstant.*;
+
 public class DomainValidate {
 
     public static void validateOutOfRankingIndex(int rank) {
-        if (rank > 6) {
-            throw new IllegalStateException("[ERROR] 잘못된 랭킹입니다. 랭킹은 1등부터 5등 사이여야 합니다.");
+        if (rank >= 6) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + OUT_OF_RANKING_INDEX);
         }
     }
 
     public static void validateDivideByZero(int denominator) {
         if (denominator <= 0) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 크기입니다. 0으로 나눌 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_DIVIDE_ZERO);
         }
     }
 }

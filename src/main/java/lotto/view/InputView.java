c3ac32fe7779855af59aa64bfefbe803cd3e96cc
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 03:38:36 +0900

    feat(requestWinningNumbers): 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String ERROR_PROMPT = "[ERROR] ";
+    private static final String COMMA = ",";
 
     public int requestLottoPurchase() {
         int input;
@@ -42,6 +46,29 @@ public class InputView {
     private void validateBonusNumber(int input) {
         if (input < 1 || 45 < input) {
             throw new IllegalArgumentException(ERROR_PROMPT + "로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public List<Integer> requestWinningNumbers() {
+        List<Integer> winningNumbers;
+        while (true) {
+            try {
+                winningNumbers = Arrays.stream(Console.readLine().split(COMMA))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                validateLottoNumbers(winningNumbers);
+                break;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ERROR_PROMPT + "로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+        return winningNumbers;
+    }
+
+    private void validateLottoNumbers(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateBonusNumber(number);
         }
     }
 }

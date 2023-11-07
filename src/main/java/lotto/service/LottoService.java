05042766d0a8bd5c490e9a3f5a2a2c576d834de5
Dowon <3120kang@gmail.com>
2023-11-08 03:12:44 +0900

    Feat(LottoService): 로또 사는 서비스 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,24 +1,21 @@
 package lotto.service;
 
-import lotto.domain.Customer;
-import lotto.domain.IssuingMachine;
-import lotto.domain.Lucky;
-import lotto.domain.ResultRepository;
+import lotto.domain.*;
 
 public class LottoService {
 
     private Customer customer;
-    private IssuingMachine issuingMachine;
     private Lucky lucky;
-    private ResultRepository resultRepository=new ResultRepository();
+    private ResultRepository resultRepository = new ResultRepository();
 
-    public LottoService(Customer customer, IssuingMachine issuingMachine, Lucky lucky){
-        this.customer=customer;
-        this.issuingMachine=issuingMachine;
-        this.lucky=lucky;
+    public LottoService(Customer customer, Lucky lucky) {
+        this.customer = customer;
+        this.lucky = lucky;
     }
 
-    public void start(){
+    public void buyLotto() {
         customer.pay();
     }
+
+
 }

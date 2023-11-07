79b20ffef043ded936829ac8f11bd3d29184ca0f
yuseok0215 <elwlahstmxjf@naver.com>
2023-11-07 20:33:30 +0900

    feat: LottoSeller/createLottoTickets 메서드에 대한 단위테스트 기능 추가
    LottoGenerator/generateLottoNumbers 자체는 검증이 이미 된 메서드이기 때문에 stream 으로 로또 수량만큼 잘 티켓이 만들어져서 나오는지에 대한 검증 테스트를 추가함.

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;

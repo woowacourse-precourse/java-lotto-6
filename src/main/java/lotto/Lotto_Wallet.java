dee31c66b35cb377cf2a7b753a7d52e2377f96b0
Jun Hyeok Choi <chrisgold@naver.com>
2023-11-08 01:12:29 +0900

    feat(Lotto_Wallet) : add new function 'setLottos'

--- src/main/java/lotto/Lotto_Wallet.java
+++ src/main/java/lotto/Lotto_Wallet.java
@@ -1,9 +1,10 @@
 package lotto;
-import lotto.Lotto;
 import java.util.List;
 
 public class Lotto_Wallet {
     private List<Lotto> Lottos;
 
-
+    public void setLottos(List<Lotto> lottos) { // 로토를 지갑에 넣기
+        Lottos = lottos;
+    }
 }

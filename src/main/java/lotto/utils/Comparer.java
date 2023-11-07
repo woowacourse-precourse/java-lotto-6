e14e06272b9374aa19e05ff9a465adf1ec4aa9c0
zeomzzz <jmlee2705@gmail.com>
2023-11-08 05:18:41 +0900

    feat: 로또와 당첨 번호 비교하는 기능 구현

--- src/main/java/lotto/utils/Comparer.java
+++ src/main/java/lotto/utils/Comparer.java
@@ -3,10 +3,15 @@ package lotto.utils;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.Winning;
 
 public class Comparer {
 
-    static int compareLottos(Lotto lotto1, Lotto lotto2) {
+    private int compareLottoToWinning(Lotto lotto, Winning winning) {
+        return Comparer.compareLottos(lotto, winning.getWinningLotto());
+    }
+
+    public static int compareLottos(Lotto lotto1, Lotto lotto2) {
         List<Integer> sameNumbers = new ArrayList<>(lotto1.getNumbers());
         sameNumbers.removeAll(lotto2.getNumbers());
         return sameNumbers.size();

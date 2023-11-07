bbdae1a60cd4ff98294b0df3582af94e5c6cbd6d
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:53:55 +0900

    feat(Constants) : Enum 필요한 상수 추가

--- src/main/java/lotto/system/Constants.java
+++ src/main/java/lotto/system/Constants.java
@@ -1,18 +1,19 @@
 package lotto.system;
 
 public enum Constants {
+    LOTTO_SIZE(6),
     MONEY_UNIT(1000),
     START_NUMBER(1),
-    END_NUMBER(45),
-    LOTTO_SIZE(6);
+    END_NUMBER(45);
 
-    private final int constants;
+    public int constants;
+
     Constants(int constants) {
         this.constants = constants;
     }
 
     public int getConstants() {
-        return this.constants;
+        return constants;
     }
 
 }

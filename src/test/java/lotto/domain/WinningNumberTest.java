0a1466aa2480420562f58c6e5a73bbb77a2b25cb
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:58:36 +0900

    test(WinningNumber) : WinningNumber test add

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WinningNumberTest {
+
+    @Test
+    void getLotto() {
+    }
+
+    @Test
+    void getBonusNumber() {
+    }
+
+    @Test
+    void getLottoNumbers() {
+    }
+}

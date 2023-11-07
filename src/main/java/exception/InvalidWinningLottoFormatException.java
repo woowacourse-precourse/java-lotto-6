05472baec145e10ef54b52bc376cc0c6ab433030
nayonsoso <panda0329@naver.com>
2023-11-08 02:14:34 +0900

    feat: 당첨 번호 형식이 올바르지 않을시 발생하는 커스텀 예외 구현

---
+++ src/main/java/exception/InvalidWinningLottoFormatException.java
@@ -0,0 +1,15 @@
+package exception;
+
+import constants.Constants;
+
+public class InvalidWinningLottoFormatException extends IllegalArgumentException{
+    private static final String message = String.format(
+            "당첨 번호 형식이 올바르지 않습니다. (올바른 형식 : 정수 %d개, 각 정수 사이에는 구분자(%s)가 위치)",
+            Constants.LOTTO_LENGTH,
+            Constants.DELIMITER
+            );
+
+    public InvalidWinningLottoFormatException() {
+        super(message);
+    }
+}

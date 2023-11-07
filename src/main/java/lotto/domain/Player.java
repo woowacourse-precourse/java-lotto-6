971be92b50a87d1b6b93afd077a093e46a9a81bb
kimdongyeon <do121712@naver.com>
2023-11-07 17:04:47 +0900

    feat : Play 객체를 생성 할 때 money에 대한 예외를 추가

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -3,12 +3,38 @@ package lotto.domain;
 public class Player {
     private int money;
     private int quantity;
+    private double prize;
 
-    public Player(int money) {
-        this.money = money;
-        this.quantity = money / 1000;
+    public Player(String money) {
+        int parsingMoney = checkMoney(money);
+        shortCheckMoney(parsingMoney);
+        this.money = parsingMoney;
+        this.quantity = parsingMoney / 1000;
+        this.prize = 0;
     }
+
+    private int checkMoney(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액을 올바르게 입력 하세요.");
+        }
+    }
+
+    private void shortCheckMoney(int money) {
+        if (money < 1000)
+            throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액을 입력해 주세요.");
+    }
+
     public int getQuantity() {
         return quantity;
+    }
+
+    public void sumPrize(int prize) {
+        this.prize = this.prize + prize;
+    }
+
+    public double getPercent() {
+        return 100.0 * prize / money;
     }
 }

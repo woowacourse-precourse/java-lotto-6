c9f889f57461aabf25d626110fe4d04a9922fbbd
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:00:09 +0900

    feat(Money): 투입금액에 관한 기능을 처리하는 객체 생성

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class Money {
+    private final long money;
+
+    public Money(long money) {
+        this.money = money;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+}

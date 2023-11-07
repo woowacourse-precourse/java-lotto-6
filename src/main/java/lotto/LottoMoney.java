ebb08e25c6db2855884785290f74e3dc56300e05
yunju-m <dbswnfnal@naver.com>
2023-11-07 14:23:21 +0900

    Feat(LottoMoney): ✨ 로또 구매 금액이 숫자인지 유효성 검사 기능 구현
    로또 구매 금액이 숫자가 아닌 경우 IlligalArgumentException 예외 처리
    
    로또 구매 금액 조건
    1. 로또 구매 금액은 숫자여야 한다.

--- src/main/java/lotto/LottoMoney.java
+++ src/main/java/lotto/LottoMoney.java
@@ -1,12 +1,20 @@
 package lotto;
 
 public class LottoMoney {
+    private static final String WRONG_TYPE_LOTTO_MONEY_MESSAGE = "[ERROR] 로또 구매금액의 타입은 숫자여야 합니다.";
     private double lottoMoney;
     private int lottoTiket;
 
     public LottoMoney(String inputMoney) {
+        validate(inputMoney);
         int lottoMoney = Integer.valueOf(inputMoney);
         this.lottoMoney = lottoMoney;
         this.lottoTiket = lottoMoney / 1000;
+    }
+
+    private void validate(String inputMoney) {
+        if (!inputMoney.matches("\\d+")) {
+            throw new IllegalArgumentException(WRONG_TYPE_LOTTO_MONEY_MESSAGE);
+        }
     }
 }

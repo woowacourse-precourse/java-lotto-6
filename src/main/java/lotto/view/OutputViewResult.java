f3bf7fbd0c4ab1697d1c513d007f5572d58ecf6c
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 21:59:49 +0900

    feat(OutputViewResult) : 출력 결과 outputView 기능 구현 - printResultStart() 구현

---
+++ src/main/java/lotto/view/OutputViewResult.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+public class OutputViewResult {
+    private static final String WIN_STATICS_MESSAGE = "당첨 통계";
+    private static final String SEPARATE = "---";
+
+    public static void printResultStart(){
+        System.out.println(WIN_STATICS_MESSAGE);
+        System.out.println(SEPARATE);
+    }
+
+    public static void printResultLotto(){
+
+    }
+
+    public static void printResultRate(){
+
+    }
+}

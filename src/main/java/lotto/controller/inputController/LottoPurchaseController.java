e25865766b05f2f70b8a13046ba0914c1b740161
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2023-11-07 13:48:02 +0900

    refactor: 첫번째 입력 유효성검사 가져오는 방식 수정

--- src/main/java/lotto/controller/inputController/LottoPurchaseController.java
+++ src/main/java/lotto/controller/inputController/LottoPurchaseController.java
@@ -1,14 +1,17 @@
 package lotto.controller.inputController;
+import lotto.model.validator.FirstInputValidator;
 import lotto.view.inputView.LottoPurchaseInput;
 
 public class LottoPurchaseController {
+
 
     private final LottoPurchaseInput inputView = new LottoPurchaseInput();
 
     public int getPurchaseAmount() {
         while (true) {
             try {
-                return inputView.requestPurchaseAmount();
+                String input = inputView.requestPurchaseAmount();
+                return FirstInputValidator.validatePurchaseAmount(input);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
--- src/main/java/lotto/view/inputView/LottoPurchaseInput.java
+++ src/main/java/lotto/view/inputView/LottoPurchaseInput.java
@@ -1,12 +1,10 @@
 package lotto.view.inputView;
 import camp.nextstep.edu.missionutils.Console;
-import lotto.model.validator.FirstInputValidator;
 
 public class LottoPurchaseInput {
 
-    public int requestPurchaseAmount() {
-        System.out.println("로또 구매 금액을 입력해주세요");
-        String input = Console.readLine();
-        return FirstInputValidator.validatePurchaseAmount(input);
+    public String requestPurchaseAmount() {
+        System.out.println("로또 구매 금액을 입력해주세요.");
+        return Console.readLine();
     }
 }

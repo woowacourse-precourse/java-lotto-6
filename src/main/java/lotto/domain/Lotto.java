cfb2df10293412a6cc51425a0bb29803d67e3687
leejuno <zhqkfxm0127@naver.com>
2023-11-07 23:29:45 +0900

    chore(lotto): domain 패키지 추가 및 Lotto 클래스 위치 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -11,7 +11,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size()!=6) {
             throw new IllegalArgumentException();
         }
     }

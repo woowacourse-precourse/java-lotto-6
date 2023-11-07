8e069cf45cdd504d98b3bcdec359e2e09195a11a
linirini <2001yerin@naver.com>
2023-11-08 03:19:42 +0900

    fix : add checking bonus number logic in determine winnings method

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -6,6 +6,8 @@ import lotto.domain.WinningResult;
 
 public class Lottos {
 
+    private static final int BONUS = 7;
+
     List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
@@ -25,10 +27,21 @@ public class Lottos {
         HashMap<Integer,Integer>winningResult = new HashMap<>();
         for (Lotto lotto : lottos) {
             int winningNumbersCount = lotto.countWinningNumbers(winningNumbers);
+            if(isWinningNumberCountFive(winningNumbersCount) && hasBonusNumber(lotto,bonus)){
+                winningNumbersCount = BONUS;
+            }
             int lottoCount = winningResult.getOrDefault(winningNumbersCount,0);
             winningResult.put(winningNumbersCount,lottoCount+1);
         }
         return new WinningResult(winningResult);
+    }
+
+    private boolean isWinningNumberCountFive(int winningNumberCount){
+        return winningNumberCount == 5;
+    }
+
+    private boolean hasBonusNumber(Lotto lotto, Bonus bonus){
+        return lotto.hasSameNumber(bonus.getBonusNumber());
     }
 
 }

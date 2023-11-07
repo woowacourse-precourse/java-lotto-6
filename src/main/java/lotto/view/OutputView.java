9030ee0a84e379069a3b9d2629fdf8e79a2c2d85
wook <dong930112@gmail.com>
2023-11-07 18:59:50 +0900

    feat: 구매한 로또정보를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,21 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public class OutputView {
 
+    private static final String PURCHASE_INFORMATION = "개를 구매했습니다.";
+
     //발행한 로또 수량 및 번호를 출력하는 기능
-    public void lottoTickets() {
+    public void lottoTickets(List<Lotto> lottos) {
+
+        System.out.printf("%d%s %n", lottos.size(), PURCHASE_INFORMATION);
+
+        for(int i=0; i<lottos.size(); i++) {
+            System.out.println(lottos.get(i).getNumbers());
+        }
+
 
     }
 

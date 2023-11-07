1e204dc42f7ecf3e60b375864964844ed4fb3149
Juri Oh <julioh0603@gmail.com>
2023-11-07 16:55:25 +0900

    로또 구입 금액 입력
    - 한장에 1000원 하는 로또를 몇개 구입할지 1000원 단위로 입력한다. (만약 1,10,100원 단위 입력 시 예외 발생)
    - playLotto 메소드에 실행할 메소드를 정리하고 Aplication 클래스에서 객체 생성 후 실행한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> numbers = new ArrayList<>();
+        List<Integer> randomNumber = new ArrayList<>();
+        Lotto lotto = new Lotto(numbers);
+        lotto.playLotto();
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    Message message = new Message();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -16,5 +20,36 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void playLotto() {
+        int money = 0;
+        int purchaseNumber = 0;
+        int bonusInput = 0;
+        List<List<Integer>> allNumbers = new ArrayList<>();
+        money = purchaseResult(purchaseNumber, money, allNumbers);
+    }
+
+    // 구입 금액 입력 받고 예외 처리한 뒤 리턴
+    public int purchaseResult(int purchaseNumber, int money, List<List<Integer>> allNumbers) {
+        message.purchaseMessage();
+        try {
+            int moneyInput = Integer.parseInt(Console.readLine());
+            money = validateOfMoney(moneyInput);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR]");
+            System.out.println();
+            return purchaseResult(0, 0, allNumbers); //예외 발생 시 기존 값 초기화 해줌
+        }
+
+        return money;
+    }
+
+    private int validateOfMoney(int moneyInput) {
+        //1000단위의 숫자만 입력 받음
+        if (moneyInput % 1000 != 0 || moneyInput < 1000) {
+            throw new IllegalArgumentException();
+        }
+
+        return moneyInput;
+    }
 }

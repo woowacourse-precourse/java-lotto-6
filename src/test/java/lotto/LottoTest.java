c22fbca22d63bb88188979e218bd0a1f9ccb80a0
dmdwns2 <ssreki1@naver.com>
2023-11-07 22:09:53 +0900

    test: add error message

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.Error.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -12,26 +13,30 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NUMBERS_SIZE.getErrMsg());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATE_NUMBERS.getErrMsg());
     }
 
     @DisplayName("로또 번호에 46이상의 숫자가있으면 예외가 발생한다.")
     @Test
     void createLottoByInvalidedNumber_01() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 98)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NUMBERS_VALUE.getErrMsg());
     }
     @DisplayName("로또 번호에 1이하의 숫자가있으면 예외가 발생한다.")
     @Test
     void createLottoByInvalidedNumber_02() {
         assertThatThrownBy(() -> new Lotto(List.of(-5, 2, 3, 4, 5, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NUMBERS_VALUE.getErrMsg());
     }
 }

8ad024c5ed759dc9bd2dbe3c9eea50cc5405a103
ParkJinYong <mej1406@gmail.com>
2023-11-07 16:28:50 +0900

    feat: 로또 구입 금액 입력, 로또 티켓 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,19 +7,31 @@ import java.util.stream.Collectors;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    static final int LOTTO_TICKET_PRICE = 1_000;
+
     public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(readLine());
+
+        int ticketCount = purchaseAmount / LOTTO_TICKET_PRICE;
+        Lotto[] tickets = new Lotto[ticketCount];
+
+        for (int i = 0; i < ticketCount; i++) {
+            tickets[i] = Lotto.generate();
+        }
+
+
         System.out.println("당첨 번호를 입력해 주세요.");
         String lottoNumbers = readLine();
 
         List<Integer> numbers = Arrays.stream(lottoNumbers.split(","))
-                                    .mapToInt(Integer::parseInt)
-                                    .boxed()
-                                    .collect(Collectors.toList());
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
 
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = readLine();
 
         Lotto win = new Lotto(numbers);
-
     }
 }

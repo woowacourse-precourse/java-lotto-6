1b081927d8f1594330309a8c824be26b3fdd4ca6
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-11-07 10:37:38 +0900

    style: Prize -> LottoRank 클래스 이름 변경

--- src/main/java/lotto/enums/LottoRank.java
+++ src/main/java/lotto/enums/LottoRank.java
@@ -2,22 +2,24 @@ package lotto.enums;
 
 import java.text.DecimalFormat;
 
-public enum Prize {
+public enum LottoRank {
 
-    FIRST_RANK(1,2_000_000_000,6),
-    SECOND_RANK(2,30_000_000,5),
-    THIRD_RANK(3,1_500_000,5),
-    FOURTH_RANK(4,50_000,4),
-    FIFTH_RANK(5,5_000,3);
+    FIRST_RANK(1,2_000_000_000,6,false),
+    SECOND_RANK(2,30_000_000,5,true),
+    THIRD_RANK(3,1_500_000,5,false),
+    FOURTH_RANK(4,50_000,4,false),
+    FIFTH_RANK(5,5_000,3,false);
 
     private final int rank;
     private final int money;
     private final int matchNumbers;
+    private boolean bonusMatch;
 
-    Prize(int rank, int money, int matchNumbers){
+    LottoRank(int rank, int money, int matchNumbers, boolean bonusMatch){
         this.rank = rank;
         this.money = money;
         this.matchNumbers = matchNumbers;
+        this.bonusMatch = bonusMatch;
     }
 
     public int getRank(){
@@ -30,6 +32,10 @@ public enum Prize {
 
     public int getMatchNumbers(){
         return matchNumbers;
+    }
+
+    public boolean isBonusMatch(){
+        return bonusMatch;
     }
 
     public String getMoneyToString(){

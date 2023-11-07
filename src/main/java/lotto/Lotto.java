c775e813c124568c5fefa3d362593a6a84a01eb4
JooDukHan <bfinecpa1@naver.com>
2023-11-07 15:16:06 +0900

    feat(로또): 당첨번호와 비교하여 몇 개 일치하는 확인하는 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import static lotto.Constants.LottoConstant.*;
+import static lotto.Constants.MessageConstant.COMMA_BLANK_DELIMITER;
+import static lotto.Constants.MessageConstant.LOTTO_NUMBER_MESSAGE;
+import static lotto.Constants.NumberOfLottoCorrect.*;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lotto.Constants.NumberOfLottoCorrect;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -23,6 +33,33 @@ public class Lotto {
             LOTTO_NUMBER_MESSAGE.formatted(numbers);
     }
 
+    public NumberOfLottoCorrect compareWinningNumber(WinningNumber winningNumber,
+        LottoNumber bonusNumber) {
+        int correctCount = (int) numbers.stream()
+            .filter(winningNumber::hasNumber)
+            .count();
+        boolean hasBonus = numbers.stream()
+            .anyMatch(bonusNumber::isSame);
+        if (hasBonus) {
+            correctCount++;
+        }
+        if (correctCount == THREE_MATCHES.getMatchCount()) {
+            return THREE_MATCHES;
+        }
+        if (correctCount == FOUR_MATCHES.getMatchCount()) {
+            return FOUR_MATCHES;
+        }
+        if (correctCount == FIVE_MATCHES.getMatchCount()) {
+            return FIVE_MATCHES;
+        }
+        if (correctCount == FIVE_BONUS_MATCHES.getMatchCount() && hasBonus) {
+            return FIVE_BONUS_MATCHES;
+        }
+        if (correctCount == SIX_MATCHES.getMatchCount()) {
+            return SIX_MATCHES;
+        }
+        return TWO_LESS_MATCHES;
+    }
 
     private void validateSize(int numbers) {
         if (numbers != LOTTO_NUMBER_LENGTH) {

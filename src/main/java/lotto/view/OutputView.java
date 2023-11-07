bb07d6c94e08ad561dbc6b259beb6681d7057fdb
Dreaming-J <kimjjong321@naver.com>
2023-11-07 19:56:55 +0900

    :sparkles: feat. 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,15 @@
 package lotto.view;
 
 import java.util.List;
+import lotto.model.EarningRate;
 import lotto.model.Score;
 
 public class OutputView {
 
     private static final String PRINT_LOTTOS_COUNT = "개를 구매했습니다.";
+    private static final String PRINT_WINNING_STATS = "당첨 통계\n---";
+    private static final String PREFIX_PRINT_EARNING_RATE = "총 수익률은 ";
+    private static final String POSTFIX_PRINT_EARNING_RATE = "입니다.";
 
     public void printTicketingLottos(int count, String lottos) {
         System.out.println(count + PRINT_LOTTOS_COUNT);
@@ -13,7 +17,11 @@ public class OutputView {
     }
 
     public void printScore(List<Score> scores) {
-        System.out.println("당첨 통계\n" + "---");
+        System.out.println(PRINT_WINNING_STATS);
         System.out.println(Score.stats(scores));
+    }
+
+    public void printEarningRate(EarningRate earningRate) {
+        System.out.println(PREFIX_PRINT_EARNING_RATE + earningRate + POSTFIX_PRINT_EARNING_RATE);
     }
 }

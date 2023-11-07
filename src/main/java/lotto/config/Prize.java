4c6e674e7379a48a127c02fcff7e318342a205db
jae woo Jung <awfjol2008@naver.com>
2023-11-08 01:44:50 +0900

    feat: Prize Enum 생성

--- src/main/java/lotto/config/Prize.java
+++ src/main/java/lotto/config/Prize.java
@@ -2,10 +2,11 @@ package lotto.config;
 
 public enum Prize {
 
+    NONE(0, 0),
     THREE_NUMBER_MATCH(3, 5000),
     FOUR_NUMBER_MATCH(4, 50000),
     FIVE_NUMBER_MATCH(5, 1500000),
-    FIVE_NUMBER_AND_BONUS_NUMBER_MATCH(6, 30000000),
+    FIVE_NUMBER_AND_BONUS_NUMBER_MATCH(5, 30000000),
     SIX_NUMBER_MATCH(6, 2000000000);
 
     private final int matchingNumberCount;

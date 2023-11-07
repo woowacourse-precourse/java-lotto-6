39a2e4e413de9a722fb97265bf43674a148af8bc
jihyuk <kwlgur0416@gmail.com>
2023-11-07 17:07:15 +0900

    feat : 수익률 계산 출력 기능 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,8 +6,8 @@ import lotto.model.LottoTickets;
 public class OutputView {
 
     private static final String LOTTO_TICKETS_COUNT_MESSAGE = "%d개를 구매했습니다.";
-
     private static final String LOTTO_TICKETS_RESULT_MESSAGE = "당첨 통계\n---";
+    private static final String LOTTO_TICKETS_EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public static void printLottoTickets(final LottoTickets tickets) {
         final String ticketCountPrintFormat = String.format(LOTTO_TICKETS_COUNT_MESSAGE,
@@ -19,6 +19,12 @@ public class OutputView {
     public static void printLottoResult(final LottoResult result) {
         System.out.println(LOTTO_TICKETS_RESULT_MESSAGE);
         System.out.println(result.toOutputString());
+    }
+
+    public static void printEarningRate(final double earningRate) {
+        final String earningRateMessage = String.format(LOTTO_TICKETS_EARNING_RATE_MESSAGE,
+            earningRate);
+        System.out.println(earningRateMessage);
     }
 
 }

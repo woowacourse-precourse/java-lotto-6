9e77e486e23bea08d2f263bf58a52059e04a3f0f
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:42:14 +0900

    feat(ErrorMessage) : 필요한 메세지 추가

--- src/main/java/lotto/system/ErrorMessage.java
+++ src/main/java/lotto/system/ErrorMessage.java
@@ -2,10 +2,24 @@ package lotto.system;
 
 public class ErrorMessage {
 
-    private ErrorMessage() {}
-    public static final String IS_NOT_DIVISIBLE = "[ERROR] 금액이 " + Constants.MONEY_UNIT + " 으로 떨어지지 않는 값을 입력했습니다.";
-    public static final String IS_NOT_NUMERIC = "[ERROR] 숫자가 아닌 입력 입니다.";
-    public static final String OUT_OF_RANGE = "[ERROR] 입력 범위가 너무 큽니다.";
-    public static final String IS_NOT_POSITIVE = "[ERROR] 입력 값은 음수 입니다.";
+    public static final String NOT_DIVISIBLE =
+            "[ERROR] 금액이 " + Constants.MONEY_UNIT.getConstants() + "으로 떨어지지 않는 값을 입력했습니다.";
+    public static final String NOT_NUMERIC = "[ERROR] 숫자가 아닌 값을 입력 했습니다.";
+    public static final String AMOUNT_OUT_OF_RANGE = "[ERROR] 입력 범위가 넘어간 값을 입력 했습니다.";
+    public static final String NOT_POSITIVE = "[ERROR] 음수인 값을 입력 했습니다.";
+    public static final String LOTTO_NUMBER_OUT_OF_RANGE =
+            "[ERROR] 입력한 로또의 값이 "
+                    + Constants.START_NUMBER.getConstants()
+                    + "이상 " + Constants.END_NUMBER.getConstants()
+                    + "이하가 아닌 값을 입력 했습니다.";
+    public static final String EACH_NOT_NUMERIC = "[ERROR] 입력한 로또의 값들 중에 숫자가 아닌 값이 있습니다.";
+    public static final String LOTTO_IS_OVER_SIZE =
+            "[ERROR] 로또의 길이가 " + Constants.LOTTO_SIZE.getConstants() + " 이 아닙니다.";
+    public static final String NUM_DUPLICATION = "[ERROR] 로또는 중복된 값이 들어 갈 수 없습니다.";
+    public static final String ALREADY_EXIST = "[ERROR] 이미 당첨 번호에서 입력 한 값입니다.";
+
+    private ErrorMessage() {
+    }
+
 
 }

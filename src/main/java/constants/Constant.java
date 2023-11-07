4b7fb153c599f697c5ad8cdd64733c68a95aef9c
changhyeonnam <hj04143@gmail.com>
2023-11-07 18:03:30 +0900

    add(Constant) : 하드코딩 방지 위한 클래스 추가

---
+++ src/main/java/constants/Constant.java
@@ -0,0 +1,21 @@
+package constants;
+
+public class Constant {
+
+    public static final String PURCAHSE_PRICE_STRING = "구입금액을 입력해 주세요";
+    public static final String PURCAHSED_STRING = "개를 구매했습니다.";
+    public static final String LOTTO_NUMBER_STRING = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_STRING = "보너스 번호를 입력해 주세오.";
+    public static final String SEP_COMMA_STRING = ",";
+    public static final String RESULT_STRING = "당첨 통계\n---";
+
+    public static final String HIT_THREE_STRING = "3개 일치 (5,000원) - ";
+    public static final String HIT_FOUR__STRING= "4개 일치 (50,000원) - ";
+    public static final String HIT_FIVE_STRING = "5개 일치 (1,500,000원) - ";
+    public static final String HIT_FIVE_BOUNUS_STRING = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String HIT_SIX_STRING = "6개 일치 (2,000,000,000원) - ";
+    public static final String COUNT_STRING = "개";
+    public static final String TOTAL_PROFIT_STRING_BEGIN= "총 수익률은 ";
+    public static final String TOTAL_PROFIT_STRING_END= "입니다.";
+
+}

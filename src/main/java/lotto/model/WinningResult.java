8658a709840d668b093e75aa4a95811698b38c7b
hoyeon <highoption@naver.com>
2023-11-07 20:41:01 +0900

    feat(WinningResult) : 상수 위치 변경 및 price 필드 추가

--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -4,22 +4,24 @@ import java.util.Arrays;
 
 public enum WinningResult {
 
-    FIRST("6개 일치 (2,000,000,000원) - %d개", 0, 6, false),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", 0, 5, true),
-    THIRD("5개 일치 (1,500,000원) - %d개", 0, 5, false),
-    FOURTH("4개 일치 (50,000원) - %d개", 0, 4, false),
-    FIFTH("3개 일치 (5,000원) - %d개", 0, 3, false);
+    FIFTH("3개 일치 (5,000원) - %d개", 0, 3, false, 5000),
+    FOURTH("4개 일치 (50,000원) - %d개", 0, 4, false, 50000),
+    THIRD("5개 일치 (1,500,000원) - %d개", 0, 5, false, 1500000),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", 0, 5, true, 30000000),
+    FIRST("6개 일치 (2,000,000,000원) - %d개", 0, 6, false, 2000000000);
 
     private final String message;
     private int countResult;
     private final int matchCount;
     private final boolean matchBonus;
+    private final int price;
 
-    WinningResult(String message, int countResult, int matchCount, boolean matchBonus) {
+    WinningResult(String message, int countResult, int matchCount, boolean matchBonus, int price) {
         this.message = message;
         this.countResult = countResult;
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
+        this.price = price;
     }
 
     public static void countLottoResult(int correctCnt, boolean correctBonus) {
@@ -34,4 +36,5 @@ public enum WinningResult {
             System.out.println(singleMessage);
         }
     }
+
 }

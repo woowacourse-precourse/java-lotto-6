b7b2e8fb04a7414768d6b98f1d5d1c01fbed9aa0
doka <posmane5@gmail.com>
2023-11-07 22:02:41 +0900

    feat(Instructions) : view의 보너스 번호 입력 유도 메시지 MESSAGE_OF_ENTERING_BONUS_NUMBERS 추가

--- src/main/java/lotto/view/constant/Instructions.java
+++ src/main/java/lotto/view/constant/Instructions.java
@@ -4,7 +4,8 @@ public enum Instructions {
     MESSAGE_OF_ENTERING_LOTTO_AMOUNT("구입금액을 입력해 주세요."),
 
     MESSAGE_OF_PURCHASE_COUNT("\n%d개를 구매했습니다.\n"),
-    MESSAGE_OF_ENTERING_WINNING_NUMBERS("당첨 번호를 입력해 주세요.");
+    MESSAGE_OF_ENTERING_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    MESSAGE_OF_ENTERING_BONUS_NUMBERS("보너스 번호를 입력해 주세요.");
 
     private final String prompts;
 

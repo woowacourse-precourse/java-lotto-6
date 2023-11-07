69186a0145f35863dafa999e215be8b0a212c6b7
kcw0360 <kcw0360@naver.com>
2023-11-08 00:14:54 +0900

    Refactor: LottoResult에서 checkResult 메소드 로직 및 변수 수정

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,70 +1,25 @@
 package lotto.model;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class LottoResult {
 
-    private int first;
-    private int second;
-    private int third;
-    private int fourth;
-    private int fifth;
-    private int totalPrizeMoney;
+    private HashMap<LottoRankings, Integer> lottoResult;
 
-    public LottoResult(int resetNumber) {
-        this.first = resetNumber;
-        this.second = resetNumber;
-        this.third = resetNumber;
-        this.fourth = resetNumber;
-        this.fifth = resetNumber;
-    }
-
-    public void checkResult(List<Integer> winningMatchResult, List<Boolean> bonusMatchResult) {
-        for (int i = 0; i < winningMatchResult.size(); i++) {
-            if (winningMatchResult.get(i) == 6) {
-                first++;
-                totalPrizeMoney += LottoRankings.FIRST.getWinningAmount();
+    public void checkResult(List<LottoRankings> results) {
+        HashMap<LottoRankings, Integer> hashMap = new HashMap<>();
+        for (LottoRankings result : results) {
+            if (hashMap.containsKey(result)) {
+                hashMap.put(result, hashMap.get(result) + 1);
+                continue;
             }
-            if (winningMatchResult.get(i) == 5 && bonusMatchResult.get(i)) {
-                second++;
-                totalPrizeMoney += LottoRankings.SECOND.getWinningAmount();
-            }
-            if (winningMatchResult.get(i) == 5 && !bonusMatchResult.get(i)) {
-                third++;
-                totalPrizeMoney += LottoRankings.THIRD.getWinningAmount();
-            }
-            if (winningMatchResult.get(i) == 4) {
-                fourth++;
-                totalPrizeMoney += LottoRankings.FOURTH.getWinningAmount();
-            }
-            if (winningMatchResult.get(i) == 3) {
-                fifth++;
-                totalPrizeMoney += LottoRankings.FIFTH.getWinningAmount();
-            }
+            hashMap.put(result, 1);
         }
+        this.lottoResult = hashMap;
     }
 
-    public int getFirst() {
-        return first;
-    }
-
-    public int getSecond() {
-        return second;
-    }
-
-    public int getThird() {
-        return third;
-    }
-
-    public int getFourth() {
-        return fourth;
-    }
-
-    public int getFifth() {
-        return fifth;
-    }
-
-    public int getTotalPrizeMoney() {
-        return totalPrizeMoney;
+    public HashMap<LottoRankings, Integer> getLottoResult() {
+        return lottoResult;
     }
 }

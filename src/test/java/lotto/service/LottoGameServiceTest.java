07849b0d468bfa4d5516ec809efbd98a616b7af4
wda067 <mg6359@naver.com>
2023-11-08 03:09:46 +0900

    test(LottoGameService): 당첨 번호와 보너스 번호 검증 테스트 추가

--- src/test/java/lotto/service/LottoGameServiceTest.java
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -42,4 +42,55 @@ class LottoGameServiceTest {
         assertThatThrownBy(() -> lottoGameService.validatePurchaseAmount(lottoPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호가 쉼표로 구분된 숫자 6개가 아닐 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4", "1,2,3,4,5,6,", "1"})
+    void createNotLottoFormat(String lottoNumbers) {
+        assertThatThrownBy(() -> lottoGameService.validateInputNumbers(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 1~45사이의 번호가 아닌 숫자를 포함할 경우 예외가 발생한다.")
+    @Test
+    void createLottoNumbersByWrongRange() {
+        List<Integer> lottoNumbers = List.of(0, 1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> lottoGameService.validateWinningNumbers(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 중복된 숫자를 포함할 경우 예외가 발생한다.")
+    @Test
+    void ContainDuplicateDigits() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 5);
+        assertThatThrownBy(() -> lottoGameService.validateWinningNumbers(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 숫자가 아닌 문자일 때 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", " ", "25c"})
+    void createBonusNumberNonDigit(String bonusNumber) {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> lottoGameService.validateBonusNumber(bonusNumber, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1~45사이의 숫자가 아닐 때 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46"})
+    void createBonusNumberByWrongRange(String bonusNumber) {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> lottoGameService.validateBonusNumber(bonusNumber, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호에 포함될 때 예외가 발생한다.")
+    @Test
+    void createBonusNumberByWrongRange() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        String bonusNumber = String.valueOf(lottoNumbers.get(0));
+        assertThatThrownBy(() -> lottoGameService.validateBonusNumber(bonusNumber, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

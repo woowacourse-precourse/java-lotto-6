b643f5995a80fcce9df5aacb429538d90f8c5805
oiler831 <qudwls831@gmail.com>
2023-11-07 15:15:00 +0900

    feat : InputView 클래스 생성
    구입금액, 당첨 번호, 보너스 번호를 입력받는 클래스이다.

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String ENTER_CASH_IN_HAND = "구입금액을 입력해 주세요.";
+    private static final String ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public String readCash() {
+        System.out.println(ENTER_CASH_IN_HAND);
+        return Console.readLine();
+    }
+
+    public String readWinningNumber() {
+        System.out.println(ENTER_WINNING_NUMBER);
+        return Console.readLine();
+    }
+
+    public String readBonusNumber() {
+        System.out.println(ENTER_BONUS_NUMBER);
+        return Console.readLine();
+    }
+
+}

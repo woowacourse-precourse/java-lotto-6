3154aa4803e8449c96c8a3b11615ada6c58847c1
zeomzzz <jmlee2705@gmail.com>
2023-11-08 06:05:49 +0900

    feat: 여러 개의 로또의 결과를 반환하는 기능 구현

--- src/main/java/lotto/domain/LottoGameResult.java
+++ src/main/java/lotto/domain/LottoGameResult.java
@@ -1,9 +1,20 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.enums.LottoRankEnum;
 import lotto.utils.Comparer;
 
 public class LottoGameResult {
+
+    List<LottoRankEnum> lottoRanks;
+
+    public void calculateLottoRanks(List<Lotto> lottos, Winning winning) {
+        lottoRanks = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            lottoRanks.add(calculateLottoResult(lotto, winning));
+        }
+    }
 
     public LottoRankEnum calculateLottoResult(Lotto lotto, Winning winning) {
         int sameCount = Comparer.compareLottoToWinning(lotto, winning);

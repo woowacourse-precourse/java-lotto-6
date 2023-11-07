b5c1fcc96911ec3fe2baaa737306469cb3dfe05a
gusdn0108 <gusdn6671@naver.com>
2023-11-07 14:52:13 +0900

    test(WinningNumber) : 당첨 번호 검증하기

--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -1,20 +1,24 @@
 package lotto.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumberTest {
-
+    @DisplayName("당첨 번호 검증하기 ")
     @Test
-    void getLotto() {
-    }
+    void validWinningNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
 
-    @Test
-    void getBonusNumber() {
-    }
+        WinningNumber winningNumber = new WinningNumber(lotto, bonusNumber);
 
-    @Test
-    void getLottoNumbers() {
+        assertAll(
+                () -> assertEquals(lotto, winningNumber.getLotto()),
+                () -> assertEquals(bonusNumber, winningNumber.getBonusNumber())
+        );
     }
 }

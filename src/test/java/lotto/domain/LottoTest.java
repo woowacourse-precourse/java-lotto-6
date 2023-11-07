de4550a24a6c0c4f7ad48bf351b1eb52de4ca3df
GiJung <weeds2577@gmail.com>
2023-11-08 03:26:09 +0900

    test(LottoTest): 당첨 번호의 개수가 6개가 아닌 경우 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,7 +4,6 @@ import lotto.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -22,12 +21,12 @@ public class LottoTest {
     @Test
     void 로또_번호가_오름차순으로_정렬되는지_테스트() {
 
-        List<Integer> lottoNumbers = new ArrayList<>(List.of(6, 5, 4, 3, 2, 1));
+        List<Integer> lottoNumbers = new ArrayList<>(List.of(6, 4, 5, 1, 2, 3));
+        List<Integer> sortedLottoNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
 
         Lotto lotto = new Lotto(lottoNumbers);
-        Collections.sort(lottoNumbers);
 
-        assertThat(lotto.getLotto()).isEqualTo(lottoNumbers);
+        assertThat(lotto.getLotto()).isEqualTo(sortedLottoNumbers);
     }
 
     @Test
@@ -35,6 +34,13 @@ public class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 1, 13, 15, 18, 43)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorCode.DUPLICATE_NUMBER.getMessage());
+    }
+
+    @Test
+    void 당첨_번호가_6개가_아닌_경우() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 1, 13, 15, 18, 43, 45)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorCode.INVALID_LOTTO_SIZE.getMessage());
     }
 
 }

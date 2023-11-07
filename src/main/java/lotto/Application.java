eda8b69605ddd4fad63a2fa1ef13a00c515c2a1f
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-11-08 07:12:04 +0900

    feat: Application(main) 개발

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.LottoResult;
+import lotto.domain.User;
+import lotto.domain.WinningNumber;
+import lotto.service.LottoService;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        LottoService lottoService = new LottoService(new User(), new WinningNumber(), new LottoResult());
+        LottoView lottoView = new LottoView();
+        LottoController lottoController = new LottoController(lottoService, lottoView);
+        lottoController.inputMoney();
+        lottoController.getLottoTicketsInfo();
+        lottoController.setLottoWinningNumbers();
+        lottoController.getNumbersMatchesResult();
+        lottoController.printRevenue();
     }
 }

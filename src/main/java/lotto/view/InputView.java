b84f8dbb2da731078b125cb3271aef00195916f7
sujnkim <blackout3210@naver.com>
2023-11-07 15:10:50 +0900

    refactor: parser실행위치를 controller에서 inputview로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,24 +1,27 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.Parser;
+
+import java.util.List;
 
 public class InputView {
     private static final String ASK_PAYMENT = "구입금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String getPaymentFromUserInput(){
+    public static int getPaymentFromUserInput(){
         System.out.println(ASK_PAYMENT);
-        return Console.readLine();
+        return Parser.parsePay(Console.readLine());
     }
 
-    public static String getWinningNumbersFromUserInput(){
+    public static List<Integer> getWinningNumbersFromUserInput(){
         System.out.println(ASK_WINNING_NUMBERS);
-        return Console.readLine();
+        return Parser.parseWinningNumber(Console.readLine());
     }
 
-    public static String getBonusNumberFromUserInput(){
+    public static int getBonusNumberFromUserInput(){
         System.out.println(ASK_BONUS_NUMBER);
-        return Console.readLine();
+        return Parser.parseBonusNumber(Console.readLine());
     }
 }

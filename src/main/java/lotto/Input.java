5016f07e1e8d193d7dfa622469562b1324cf490f
ghsyn <swanshawn715@gmail.com>
2023-11-07 18:14:47 +0900

    Feat: implement of lottery number input and exceptions handling

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,6 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Input {
     public static int getPrice() {
         int purchase = 0;
@@ -18,5 +22,28 @@ public class Input {
         }
 
         return purchase;
+    }
+
+    public static List<Integer> getLotteryNums() {
+        List<Integer> lotteryList = new ArrayList<>();
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                String lotteryInput = Console.readLine();
+                List<String> lotteryString = List.of(lotteryInput.split(","));
+                lotteryString = lotteryString.stream().distinct().collect(Collectors.toList());
+
+                lotteryList.clear();
+                for (String number : lotteryString) {
+                    lotteryList.add(Integer.parseInt(number));
+                }
+
+                validInput = Lotto.validate(lotteryList);
+            } catch (NumberFormatException e) {
+                new IllegalArgumentExceptionHandler("로또 번호는 정수여야 합니다.");
+            }
+        }
+        return lotteryList;
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,10 +10,22 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public static boolean validate(List<Integer> numbers) {
+        try {
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException("로또 번호는 중복되지 않는 6자리여야 합니다.");
+            }
+
+            for (int number : numbers) {
+                if (number > 45 || number < 1) {
+                    throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이여야 합니다.");
+                }
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            new IllegalArgumentExceptionHandler(e.getMessage());
         }
+        return false;
     }
 
     // TODO: 추가 기능 구현

e059018db8ca007944677e257d54239370d3910e
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-07 10:05:33 +0900

    feat : LottoRank 를 결정 하는 기능 구현
    LottoRank Domain , MatchResult Domain 생성
    MatchResult 를 통해 , LottRank enum 에서 해당값 결정
    이에 따른 , 테스트 구현

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+public enum LottoRank {
+    FIRST(6, false),
+    SECOND(5, true),
+    THIRD(5, false),
+    FOURTH(4, false),
+    FIFTH(3, false),
+    NONE(0, false);
+
+    private final int matchCount;
+    private final boolean matchBonus;
+
+    LottoRank(int matchCount, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.matchBonus = bonusMatch;
+    }
+    public static LottoRank determineRankByMatchResult(MatchResult matchResult) {
+        int matchCount = matchResult.matchCount();
+        boolean matchBonus = matchResult.matchBonus();
+        if (matchCount!=5 && matchBonus == true){
+            matchCount+=1;
+            matchBonus=false;
+        }
+        for (LottoRank rank : values()) {
+            if (rank.matchCount == matchCount && (!rank.matchBonus || matchBonus)) {
+                return rank;
+            }
+        }
+        return NONE;
+    }
+}

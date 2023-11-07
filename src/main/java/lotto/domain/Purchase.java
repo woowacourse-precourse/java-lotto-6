2d386c81cd11c5dab44525854d3a157b744ed34f
potatomoong <star901210@hanmail.net>
2023-11-07 17:19:01 +0900

    Feat: 구입 금액 1000원단위 입력 확인하는 기능 추가

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -7,6 +7,17 @@ public class Purchase {
 
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        int purchaseQuantity = Integer.parseInt(Console.readLine());
+
+        checkInputMoney(purchaseQuantity);
+
+        return purchaseQuantity;
+    }
+
+    private static void checkInputMoney(int purchaseQuantity) {
+        if (purchaseQuantity % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1000원 단위로 입력 가능합니다.");
+        }
+
     }
 }

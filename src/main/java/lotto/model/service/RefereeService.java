77ac850a6c414cedbb8192d9437ee1ade2652017
Hayoon <hy97@sju.ac.kr>
2023-11-07 21:22:40 +0900

    refactor(RefereeService): 로또 결과 계산 메서드 추출로 분리 getRank(), getHitNumberCount(), isHitBonusNumber()

--- src/main/java/lotto/model/service/RefereeService.java
+++ src/main/java/lotto/model/service/RefereeService.java
@@ -5,40 +5,58 @@ import lotto.model.domain.Referee;
 
 import java.util.List;
 
+import static lotto.model.domain.Rank.*;
+import static lotto.util.constant.Constant.INITIALIZE_NUMBER;
+import static lotto.util.constant.Constant.PER_THOUSAND;
+
 public class RefereeService {
 
-    public Referee calculateLottoResult(Referee referee, List<Lotto> lottoList) {
+    public void calculateLottoResult(Referee referee, List<Lotto> lottoList) {
         List<Integer> winningNumbers = referee.getWinningNumbers();
         int bonusNumber = referee.getBonusNumber();
 
         for (Lotto lotto : lottoList) {
-            int hitNumberCount = 0;
+            int hitNumberCount = INITIALIZE_NUMBER;
             boolean isHitBonusNumber = false;
-            int rank = 0;
+            int rank = INITIALIZE_NUMBER;
             List<Integer> numbers = lotto.getNumbers();
-            for (int i = 0; i < 6; i++) {
-                if (winningNumbers.contains(numbers.get(i))) {
-                    hitNumberCount++;
-                }
-            }
-            if (numbers.contains(bonusNumber)) {
-                isHitBonusNumber = true;
-            }
-            if (hitNumberCount == 6) {
-                rank = 1;
-            } else if (hitNumberCount == 5 && isHitBonusNumber) {
-                rank = 2;
-            } else if (hitNumberCount == 5) {
-                rank = 3;
-            } else if (hitNumberCount == 4) {
-                rank = 4;
-            } else if (hitNumberCount == 3) {
-                rank = 5;
-            }
+            hitNumberCount = getHitNumberCount(winningNumbers, hitNumberCount, numbers);
+            isHitBonusNumber = isHitBonusNumber(bonusNumber, isHitBonusNumber, numbers);
+            rank = getRank(hitNumberCount, isHitBonusNumber, rank);
 
             referee.increaseCountForRank(rank);
-            referee.calculateLottoBenefit(lottoList.size() * 1000);
+            referee.calculateLottoBenefit(lottoList.size() * PER_THOUSAND);
         }
-        return referee;
+    }
+
+    private int getRank(int hitNumberCount, boolean isHitBonusNumber, int rank) {
+        if (hitNumberCount == FIRST.getHitCount()) {
+            rank = FIRST.getRank();
+        } else if (hitNumberCount == SECOND.getHitCount() && isHitBonusNumber) {
+            rank = SECOND.getRank();
+        } else if (hitNumberCount == THIRD.getHitCount()) {
+            rank = THIRD.getRank();
+        } else if (hitNumberCount == FOURTH.getHitCount()) {
+            rank = FOURTH.getRank();
+        } else if (hitNumberCount == FIFTH.getHitCount()) {
+            rank = FIFTH.getRank();
+        }
+        return rank;
+    }
+
+    public boolean isHitBonusNumber(int bonusNumber, boolean isHitBonusNumber, List<Integer> numbers) {
+        if (numbers.contains(bonusNumber)) {
+            isHitBonusNumber = true;
+        }
+        return isHitBonusNumber;
+    }
+
+    public int getHitNumberCount(List<Integer> winningNumbers, int hitNumberCount, List<Integer> numbers) {
+        for (int i = 0; i < 6; i++) {
+            if (winningNumbers.contains(numbers.get(i))) {
+                hitNumberCount++;
+            }
+        }
+        return hitNumberCount;
     }
 }

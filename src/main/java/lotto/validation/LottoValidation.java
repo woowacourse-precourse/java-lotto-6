eba82085d92b5090061ccf2dd328101f4053e420
limzzum <chfhddl1234@naver.com>
2023-11-07 19:29:12 +0900

    :sparkles: feat : 로또 숫자 범위 체크 메서드 추가

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -1,6 +1,9 @@
 package lotto.validation;
 
 import static lotto.constant.ErrorMessage.INVALID_DUPLICATE_LOTTO_NUM;
+import static lotto.constant.ErrorMessage.INVALID_RANGE_LOTTO_NUM;
+import static lotto.constant.Message.LOTTO_NUM_RANGE_MAX;
+import static lotto.constant.Message.LOTTO_NUM_RANGE_MIN;
 
 import java.util.List;
 import java.util.Set;
@@ -12,4 +15,13 @@ public class LottoValidation {
             throw new IllegalArgumentException(INVALID_DUPLICATE_LOTTO_NUM);
         }
     }
+
+    public void validate_isLottoRangeNum(List<Integer> nums){
+        for (Integer num : nums){
+            if(num < LOTTO_NUM_RANGE_MIN || num > LOTTO_NUM_RANGE_MAX){
+                throw new IllegalArgumentException(INVALID_RANGE_LOTTO_NUM);
+            }
+        }
+    }
+
 }

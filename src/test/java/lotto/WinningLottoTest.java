c74d8b524f529cfecad188d76226c250c3263cdc
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 14:06:16 +0900

    refactor: 오류메시지 수정

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,43 +1,45 @@
 package lotto;
 
+import static lotto.Model.WinningLotto.ERROR_END_LOTTO_NUMBER_DELEMETER;
+import static lotto.Model.WinningLotto.ERROR_INVALID_BONUS;
+import static lotto.Model.WinningLotto.ERROR_NOT_LOTTO_FORMAT;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import lotto.Model.Bonus;
-import lotto.Model.Lotto;
 import lotto.Model.WinningLotto;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningLottoTest {
 
-    @DisplayName("당첨번호가 올바르게 생성되는지 테스트")
+    @DisplayName("숫자와 쉼표로 구성 되지 않은 로또번호는 예외 발생하는 테스트")
     @Test
     void createWinningLotto() {
         String invalidInput = "1,2,3,4,5, 30";
 
-        Assertions.assertThatThrownBy(() -> new WinningLotto(invalidInput))
+        assertThatThrownBy(() -> new WinningLotto(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 예시처럼 입력해주세요. 예시) 1,2,3,4,5,6");
+                .hasMessage(ERROR_NOT_LOTTO_FORMAT);
     }
 
-    @DisplayName("당첨번호가 쉼표로 끝나지 않는지 테스트")
+    @DisplayName("당첨번호가 쉼표로 끝나면 예외 발생하는 테스트")
     @Test
-    void validateWinningLotto2() {
+    void createWinningLotto2() {
         String invalidInput = "1,2,3,4,5,30,";
 
-        Assertions.assertThatThrownBy(() -> new WinningLotto(invalidInput))
+        assertThatThrownBy(() -> new WinningLotto(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 쉼표로 끝나는 당첨번호는 입력할 수 없습니다.");
+                .hasMessage(ERROR_END_LOTTO_NUMBER_DELEMETER);
     }
 
-    @DisplayName("당첨번호에 보너스 번호가 있으면 예외 테스트")
+    @DisplayName("당첨번호에 보너스 번호가 있으면 예외 발생하는 테스트")
     @Test
     void contain() {
         WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6");
+        Bonus bonus = new Bonus("5");
 
-        Assertions.assertThatThrownBy(() -> winningLotto.contain(new Bonus("5")))
+        assertThatThrownBy(() -> winningLotto.contain(bonus))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다.");
+                .hasMessage(ERROR_INVALID_BONUS);
     }
 }

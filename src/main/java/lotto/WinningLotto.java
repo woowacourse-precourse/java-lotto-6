5b0f6bb3065b2a262e0af870cb51162d3115f646
choiejae <ejae8319@gmail.com>
2023-11-07 21:19:17 +0900

    feat(WinningLotto): 당첨 번호 로또 기능 구현
    - bonusNumber 가 당첨번호에 포함되어 있으면 예외처리 된다
    - 당첨 번호를 통해 결과를 반환한다

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,31 @@
+package lotto;
+
+public class WinningLotto {
+
+    public static final String CONTAINED_EXCEPTION = "보너스 번호가 당첨 번호에 포함되어 있습니다";
+    private final Lotto winningNumbers;
+    private final BonusNumber number;
+
+    public WinningLotto(final Lotto winningNumbers, BonusNumber number) {
+        validateWinningLotto(winningNumbers, number);
+        this.winningNumbers = winningNumbers;
+        this.number = number;
+    }
+
+    public static WinningLotto of(final Lotto winningNumbers, BonusNumber number) {
+        return new WinningLotto(winningNumbers, number);
+    }
+
+    private void validateWinningLotto(final Lotto winningNumbers, final BonusNumber number) {
+        if (winningNumbers.contains(number.bonusNumber())) {
+            throw new IllegalArgumentException(CONTAINED_EXCEPTION);
+        }
+    }
+
+    public LottoRank match(Lotto lotto) {
+        int matchNumber = lotto.match(winningNumbers);
+        boolean isBonus = lotto.contains(number.bonusNumber());
+
+        return LottoRank.ofMatchNumber(matchNumber, isBonus);
+    }
+}

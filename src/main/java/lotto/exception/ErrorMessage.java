a79ffc67021d1a8b368de6853f64307d8c325280
SungKyum Kim <sungkyum1@naver.com>
2023-11-08 00:06:40 +0900

    feat(ErrorMessage): 에러 메시지를 Enum 으로 관리

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,8 +1,8 @@
-package lotto.util;
+package lotto.exception;
 
 public enum ErrorMessage {
-    INVALID_AMOUNT("구입 금액은 양의 정수만 입력 가능합니다."),
-    NON_INTEGER("입력값은 정수가 아닙니다."),
+    INVALID_AMOUNT("구입 금액은 1000원 단위만 입력 가능합니다."),
+    NON_INTEGER("입력값이 정수가 아닙니다."),
     CONSECUTIVE_COMMAS("콤마가 연속으로 입력되었습니다."),
     LAST_COMMA("입력의 마지막이 콤마입니다."),
     INVALID_INTEGER_COUNT("정수는 6개만 입력 가능합니다."),

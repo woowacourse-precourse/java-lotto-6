27e945d0d36652cc7880ff957e0b0527ce4a9ffb
doha <dohaahi@naver.com>
2023-11-07 23:47:03 +0900

    refactor (PurchaseAmount): 공백 제거

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -4,14 +4,12 @@ import lotto.validator.PurchaseAmountValidator;
 
 public class PurchaseAmount {
     public static final int PURCHASE_AMOUNT_UNIT = 1_000;
-
     private final int amount;
 
     public PurchaseAmount(int amount) {
         PurchaseAmountValidator.validatePurchaseAmount(amount);
         this.amount = amount;
     }
-
 
     public int purchaseQuantityLottos() {
         return amount / PURCHASE_AMOUNT_UNIT;

7dee279f3880809e20bc8743cd57ae4e600248f7
황준혁 <hyug0819@gmail.com>
2023-11-08 00:04:12 +0900

    feat : 사용자 Input String -> int[] 변환

--- src/main/java/lotto/util/convert/ConvertUtil.java
+++ src/main/java/lotto/util/convert/ConvertUtil.java
@@ -1,6 +1,10 @@
 package lotto.util.convert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lotto.util.validate.CommonValidate;
+import lotto.util.validate.UserValidate;
 
 public class ConvertUtil {
 
@@ -12,5 +16,32 @@ public class ConvertUtil {
             throw new IllegalArgumentException(e);
         }
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> stringToIntArr(String input){
+        List<Integer> numbers;
+        try{
+            CommonValidate.checkBlank(input);
+            numbers = splitString(input);
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(e);
+        }
+        return numbers;
+    }
+
+    private static List<Integer> splitString(String input){
+        String[] temp = input.split(",");
+        List<Integer> numbers = new ArrayList<>(temp.length);
+        try{
+            for(String number : temp){
+                CommonValidate.checkIsNumber(number);
+                int n = Integer.parseInt(number);
+                UserValidate.checkNumberRange(n);
+                numbers.add(n);
+            }
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(e);
+        }
+        return numbers;
     }
 }

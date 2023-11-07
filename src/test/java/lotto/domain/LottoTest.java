8332b14658237cb599343dff37c54b7225127829
mina <qkralsdk991@naver.com>
2023-11-07 16:55:42 +0900

    Test : Lotto 예외 테스트 기능 구현

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,7 @@
-package lotto;
+package lotto.domain;
 
 import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,10 +20,46 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 범위가 1~45를 벗어난다.")
+    @Test
+    void createLottoByOverRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("45");
+    }
+
+    @DisplayName("보너스 번호의 범위가 1~45를 벗어난다.")
+    @Test
+    void inputBonusByOverRange() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 46;
+
+        assertThatThrownBy(() -> winningNum.validateBonus(winningNum, bonus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("45");
+    }
+
+    @DisplayName("보너스 번호와 당첨 번호 중 중복된 값이 존재한다.")
+    @Test
+    void inputBonusByDuplicatedNum() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 6;
+
+        assertThatThrownBy(() -> winningNum.validateBonus(winningNum, bonus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복");
+    }
+
+    @DisplayName("당첨 번호와 로또 번호를 비교하는 도메인 로직 테스트.")
+    @Test
+    void compareWinningWithLotto() {
+        Lotto winningNum = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+
+        Assertions.assertThat(lotto.compareLottoWithWinning(winningNum)).isEqualTo(4);
+    }
 }

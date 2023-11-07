259fa688d86eb1c4713ff068599557873142a724
ztng123 <dpwl991102@gmail.com>
2023-11-08 03:59:10 +0900

    사용자 입력
    당첨 번호 예외 처리

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -10,4 +10,10 @@ public class WinningLotto {
         this.winningNumbers = new Lotto(winningNumbers);
         this.bonusNumber = bonusNumber;
     }
+
+    private void validateBonusNumber() {
+        if (bonusNumber < 1 || bonusNumber > 45 || winningNumbers.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

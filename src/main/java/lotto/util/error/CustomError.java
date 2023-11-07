77bb5fa32110526b7da67a8ef3692adc457bd112
황준혁 <hyug0819@gmail.com>
2023-11-08 00:03:23 +0900

    feat : 커스텀 에러 추가

--- src/main/java/lotto/util/error/CustomError.java
+++ src/main/java/lotto/util/error/CustomError.java
@@ -1,9 +1,12 @@
 package lotto.util.error;
 
 public enum CustomError {
-    INPUT_RANGE_ERROR("입력 범위는 1부터 35까지입니다."),
     INPUT_IS_NOT_NUMBER("입력된 값이 숫자가 아닙니다."),
-    INPUT_CONTAIN_BLANK("입력에 빈칸이 포함되어 있습니다.")
+    INPUT_CONTAIN_BLANK("입력에 빈칸이 포함되어 있습니다."),
+    INPUT_IS_NOT_THOUSAND("1000원 단위로 입력하세요."),
+    INPUT_MAX_NUMBER("숫자 6개를 입력하세요."),
+    INPUT_NUMBER_RANGE("숫자의 입력 범위는 1부터 45까지입니다."),
+    NUMBER_DUPLICATE("중복된 숫자가 있습니다.")
     ;
 
     private final String description;

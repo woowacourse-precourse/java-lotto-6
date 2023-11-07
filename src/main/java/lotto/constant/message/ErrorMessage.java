9771007979a694f4f014604aa03e35844e0410a9
현 <sohyun95277@gmail.com>
2023-11-07 11:57:03 +0900

    refactor: 사용하지 않는 상수 삭제
    하나의 클래스 내부에서만 사용하는 DELIMITER 삭제

--- src/main/java/lotto/constant/message/ErrorMessage.java
+++ src/main/java/lotto/constant/message/ErrorMessage.java
@@ -1,6 +1,5 @@
 package lotto.constant.message;
 
-import static lotto.constant.LottoInformation.DELIMITER;
 import static lotto.constant.LottoInformation.LOTTO_PRICE;
 import static lotto.constant.LottoInformation.MAX_NUMBER;
 import static lotto.constant.LottoInformation.MIN_NUMBER;
@@ -13,8 +12,7 @@ public enum ErrorMessage {
     DUPLICATE_BONUS("당첨 번호와 중복될 수 없습니다."),
     INVALID_NUMBER_RANGE("로또 번호는 " + MIN_NUMBER + "부터 " + MAX_NUMBER + " 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBER("중복된 숫자가 존재합니다."),
-    NOT_MONEY("주머니를 뒤적였지만 돈이 없습니다.."),
-    INVALID_COUNT(NUMBER_COUNT + "개의 숫자를 구분자(" + DELIMITER + ")와 함께 입력해주세요.");
+    INVALID_COUNT(NUMBER_COUNT + "개의 숫자를 구분자(,)와 함께 입력해주세요.");
 
     private final String message;
 

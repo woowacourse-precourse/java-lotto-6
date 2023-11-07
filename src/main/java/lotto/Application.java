23bef9a8ced6e9d76d0d71ce71022cc18b257a93
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-08 08:43:12 +0900

    feat(main): 메인 메서드 작성
    해치웠나?!..
    메인 메서드 작성 완료
    일단 8줄 안에 끝나지긴 하네요 ^^

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+import java.util.List;
+
+import static lotto.controller.LottoController.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+
+        // 로또 구매 금액 입력
+        int totalPurchaseMoney = lottoController.receiveMoneyUntilPass();
+
+        // 입력 금액 만큼 로또 구매
+        int purchaseCount = totalPurchaseMoney / ONE_LOTTO_PRICE;
+        List<Lotto> lottoList = lottoController.generateLottoList(purchaseCount);
+
+        // 구매 결과 출력
+        lottoController.showPurchaseResult(lottoList, totalPurchaseMoney);
+
+        // 정답 로또 번호 입력
+        Lotto answer = lottoController.registerWinningLottoCombinationUntilPass();
+
+        // 보너스 숫자 입력
+        int bonusNumber = lottoController.registerBonusNumberUntilPass();
+
+        // 통계 결과 출력
+        lottoController.showStatisticsResult(lottoList,answer,bonusNumber);
     }
+
+
 }

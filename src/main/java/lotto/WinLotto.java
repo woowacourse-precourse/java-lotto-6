edcc15ce35a6ed5bc11382525f2400f80e019992
prefer52 <tjsgh151@naver.com>
2023-11-08 00:22:23 +0900

    feat(WinLotto): 당첨 번호 저장 로또 클래스 생성

---
+++ src/main/java/lotto/WinLotto.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinLotto {
+    private Lotto lotto;
+    private int bonusNumber;
+
+    public WinLotto(List<String> winNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String winNumber : winNumbers) {
+            numbers.add(Integer.parseInt(winNumber));
+        }
+
+        lotto = new Lotto(numbers);
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+}

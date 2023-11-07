1392c1bff04369aa138ee46ea574bbb6d524b321
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-08 02:23:00 +0900

    fix : fix to calculate winningPoint using Lotto instance

--- src/main/java/lotto/service/LottoScoringService.java
+++ src/main/java/lotto/service/LottoScoringService.java
@@ -2,53 +2,70 @@ package lotto.service;
 
 import static lotto.configurations.SettingValues.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import lotto.model.UserLotto;
-import lotto.model.WinningLotto;
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
+import lotto.model.UserLottos;
+import lotto.model.WinningNumbers;
+
 
 public class LottoScoringService {
-    WinningLotto winningLotto;
-    List<Integer> userLottoRankings;
+    private static class SingletonLottoScoringService {
+        private static final LottoScoringService LOTTO_SCORING_SERVICE = new LottoScoringService();
+    }
 
-    LottoScoringService(WinningLotto winningLotto) {
-        this.winningLotto = winningLotto;
+    public static LottoScoringService getInstance() {
+        return SingletonLottoScoringService.LOTTO_SCORING_SERVICE;
     }
-    public void scoreUserLottos(List<UserLotto> userLottoList){
-        userLottoRankings = new ArrayList<>();
-        for(UserLotto userLotto : userLottoList){
-            Integer ranking = scoreALotto(userLotto);
-            //validate(ranking);
-            userLottoRankings.add(ranking);
+
+    private LottoScoringService() {
+    }
+
+    public Integer[] rankUserLottos(WinningNumbers winningNumbers, BonusNumber bonusNumber, UserLottos userLottos) {
+        Integer[] numberOfRankings = {0, 0, 0, 0, 0, 0};
+
+        userLottos.resetIndex();
+        while (userLottos.isRemainUserLotto()) {
+            Lotto userLotto = userLottos.getNextUserLotto();
+
+            Integer ranking = scoreALotto(winningNumbers, bonusNumber, userLotto);
+
+            numberOfRankings[ranking]++;
         }
+        return numberOfRankings;
     }
-    private Integer scoreALotto(UserLotto userLotto) {
+
+    private Integer scoreALotto(WinningNumbers winningNumbers, BonusNumber bonusNumber, Lotto lotto) {
         Integer ranking;
 
-        List<Integer> userNumbers = userLotto.getLottoNumbers();
-
-        Integer winningPoint = calculatePoint(userNumbers);
-        Boolean bonusPoint = checkBonusPoint(userNumbers);
-
-        // validate winning point
-        // validate bonus point
+        Integer winningPoint = calculatePoint(winningNumbers, lotto);
+        Boolean bonusPoint = checkBonusPoint(bonusNumber, lotto);
 
         ranking = rank(winningPoint, bonusPoint);
+
         return ranking;
     }
 
-    private Integer calculatePoint(List<Integer> userNumbers) {
+    private Integer calculatePoint(WinningNumbers winningNumbers, Lotto lotto) {
         Integer winningPoint = 0;
-        for (Integer number : userNumbers) {
-            if (winningLotto.contains(number)) {
+
+        for (int i = 0; i < SIZE_OF_LOTTO.get(); i++) {
+            Integer number = lotto.getValue(i);
+            if (winningNumbers.contains(number)) {
                 winningPoint++;
             }
         }
+
         return winningPoint;
     }
 
-    private Boolean checkBonusPoint(List<Integer> userNumbers) {
-        return userNumbers.contains(winningLotto.getBonusNumber());
+    private Boolean checkBonusPoint(BonusNumber bonusNumber, Lotto lotto) {
+        for (int i = 0; i < SIZE_OF_LOTTO.get(); i++) {
+            Integer number = lotto.getValue(i);
+            if (bonusNumber.equals(number)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private Integer rank(Integer winningPoint, Boolean bonusPoint) {

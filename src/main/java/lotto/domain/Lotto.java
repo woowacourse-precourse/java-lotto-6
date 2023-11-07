cf5e232c9dfa405b1c049bbbe1290cff2f2d929b
KDT live <dnjsxo0616@gmail.com>
2023-11-08 00:26:43 +0900

    feat(Lotto): validate 에러 메시지 추가
    - "[ERROR] 로또 번호가 6개가 아닙니다." 메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -12,7 +12,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 6개가 아닙니다.");
         }
     }
 

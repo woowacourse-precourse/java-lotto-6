38ac544c4eaeb10a38567d35c597a9c02c2111da
zjacom <eephus625@icloud.com>
2023-11-07 22:51:36 +0900

    test(lotto): RandomNumberGeneratorTest 작성
    생성된 랜덤번호의 오름차순 여부를 확인하는 테스트 추가

--- src/test/java/lotto/RandomNumberGeneratorTest.java
+++ src/test/java/lotto/RandomNumberGeneratorTest.java
@@ -2,10 +2,12 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import util.RandomNumberGenerator;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.RandomNumberGenerator;
 
 class RandomNumberGeneratorTest {
     RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
@@ -16,18 +18,27 @@ class RandomNumberGeneratorTest {
         randomNumbers = randomNumberGenerator.createNewRandomNumber();
     }
 
+    @DisplayName("생성된 랜덤 번호의 크기가 6인지 확인하는 테스트")
     @Test
-    void 생성된_로또의_길이_테스트() {
+    void createRandomNumberHasCorrectSize() {
         assertThat(randomNumbers).hasSize(6);
     }
 
+    @DisplayName("생성된 랜덤 번호의 각 번호가 1에서 45사이에 있는지 확인하는 테스트")
     @Test
-    void 생성된_로또번호_범위_테스트() {
+    void createRandomNumberHasCorrectRange() {
         assertThat(randomNumbers).allMatch(value -> value >= 1 && value <= 45);
     }
 
+    @DisplayName("생성된 랜덤 번호가 중복되지 않는지 확인하는 테스트")
     @Test
-    void 생성된_로또번호_중복_확인_테스트() {
+    void createRandomNumberWithOutDuplicated() {
         assertThat(randomNumbers).doesNotHaveDuplicates();
+    }
+
+    @DisplayName("생성된 랜덤 번호가 오름차순으로 정렬됐는지 확인하는 테스트")
+    @Test
+    void createRandomNumberByAscending() {
+        assertThat(randomNumbers).isSortedAccordingTo(Integer::compareTo);
     }
 }

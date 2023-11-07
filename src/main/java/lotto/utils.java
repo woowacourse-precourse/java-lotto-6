55ad106421c50e8408a24b406189d6f390642996
bitesnail <rkwoal216@gmail.com>
2023-11-07 18:23:30 +0900

    feat: implementation function for validation

--- src/main/java/lotto/utils.java
+++ src/main/java/lotto/utils.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import lotto.configs.Constant;
 
 public class utils {
     public static <T> boolean hasDuplicates(List<T> list){
@@ -8,6 +9,14 @@ public class utils {
     }
     public static boolean hasInvalidValue(List<Integer> list, int min, int max){
         return list.stream().anyMatch(e -> e < min || e > max);
+    }
+    public static boolean isInteger(String number){
+        try{
+            Integer.parseInt(number);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
     }
 
     public static int getCountOfMatch(List<Integer> list1, List<Integer> list2){
@@ -18,5 +27,9 @@ public class utils {
             }
         }
         return count;
+    }
+
+    public static String createErrorMessage(String message){
+        return String.join(Constant.BLANK, Constant.ERROR_MESSAGE, message);
     }
 }

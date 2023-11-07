00f6e881c9874a715b724c7d31f5df3552802b68
jhon3242 <rldotnr2@gmail.com>
2023-11-07 14:39:59 +0900

    docs : 확장 가능성 수정

--- src/main/java/lotto/domain/LottoOption.java
+++ src/main/java/lotto/domain/LottoOption.java
@@ -5,7 +5,7 @@ public class LottoOption {
     public static final String LOTTO_NUMBER_DELIMITER = ",";
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_START_INCLUSIVE = 1;
-    public static final int LOTTO_END_INCLUSIVE = 50;
+    public static final int LOTTO_END_INCLUSIVE = 45;
 
     private LottoOption() {
         // 불필요한 인스턴스 생성 방지

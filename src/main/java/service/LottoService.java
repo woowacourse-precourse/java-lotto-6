4e190a933befcfac1bd4cbf09e384b0757ba2e40
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-11-07 18:34:31 +0900

    LottoService - comparator 구현

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,9 +1,30 @@
 package service;
 
+import domain.Lotto;
+import domain.WinningLotto;
+import support.LottoComparator;
+import support.RandomLotto;
+import support.Rate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
 
-    public void exectue() {
-        //todo
+    private static final RandomLotto randomLotto = new RandomLotto();
+
+    private List<Rate> rate = new ArrayList<>();
+    public void execute(WinningLotto winningLotto, int lottoTimes) {
+
+        Lotto winning = new Lotto(winningLotto.getWinning());
+        LottoComparator lottoComparator = new LottoComparator(winning, winningLotto.getBonus());
+
+        for (int i = 0; i < lottoTimes; i++) {
+            Lotto lotto = new Lotto(randomLotto.getRandomLotto());
+            rate.add(lottoComparator.compareLotto(lotto));
+        }
+
+
     }
 
 }

a2c0ba5a98000057ad276ed284dbd3030c160031
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-07 23:16:55 +0900

    refactor(WinningNumber): 보너스 중복 예외처리 분리

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.exception.lotto.BonusDuplicateException;
+
 public record WinningNumber(Lotto lotto, BonusNumber bonusNumber) {
     public WinningNumber {
         validateDuplicate(lotto, bonusNumber);
@@ -7,7 +9,7 @@ public record WinningNumber(Lotto lotto, BonusNumber bonusNumber) {
 
     private void validateDuplicate(final Lotto lotto, final BonusNumber bonusNumber) {
         if (lotto.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 로또와 다른 번호로 입력해주세요.");
+            throw new BonusDuplicateException();
         }
     }
 }

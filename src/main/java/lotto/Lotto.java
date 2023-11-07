d7661c242a4190d6672de058a046dfe6af01b618
Identityless <rlandwnd555@naver.com>
2023-11-08 00:49:30 +0900

    fix : 입력 값 검증 책임 Lotto Model로 이전

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import static lotto.CommonUnits.MAX_LOTTO_NUMBER;
+import static lotto.CommonUnits.MIN_LOTTO_NUMBER;
+import static lotto.CommonUnits.NUMBERS_OF_LOTTO;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,13 +15,26 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        isDup(numbers);
+        numbers.stream().forEach(this::validateLottoNumber);
+    }
+
+    private void validateLottoNumber(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + MIN_LOTTO_NUMBER +
+                "부터 " + MAX_LOTTO_NUMBER + " 사이의 숫자여야 합니다.");
         }
     }
 
-    public List<Integer> getNumbers() {
+    private List<Integer> isDup(List<Integer> numbers) {
+        if (numbers.stream().collect(Collectors.toSet()).size() != NUMBERS_OF_LOTTO) {
+            throw new IllegalArgumentException("[ERROR] 올바른 갯수의 숫자를 입력해주세요.");
+        }
         return numbers;
     }
 }

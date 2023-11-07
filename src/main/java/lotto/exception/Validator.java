a53b47e95d2cd5cc2193943a4061980b66d2671a
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 05:01:05 +0900

    feat($Validator): 보너스번호가 당첨 번호의 존재할 경우 예외처리 기능 구현

--- src/main/java/lotto/exception/Validator.java
+++ src/main/java/lotto/exception/Validator.java
@@ -47,4 +47,10 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력 불가능합니다.");
         }
     }
+
+    private static void validateNoDuplicateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
 }

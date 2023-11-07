e7a2617d727384b0a23d802ec5bf8ddd4102ed75
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 02:39:00 +0900

    feat(printLottoPurchaseInputMessage): 구입금액 입력 메세지 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void printLottoPurchaseInputMessage() {
+        System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
+    }
+}

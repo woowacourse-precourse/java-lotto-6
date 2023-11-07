42ffe1be9d21e80623dd1b22c9597e0098678bb0
potatomoong <star901210@hanmail.net>
2023-11-08 01:28:03 +0900

    Feat: 당첨결과를 확인하기 위한 뼈대 구현

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Result {
+    private final List<Lotto> purchasedLotto;
+    private final int bonusBall;
+    public Result(List<Lotto> purchasedLotto, List<Integer> winningNumber, int bonusBall) {
+        this.purchasedLotto = purchasedLotto;
+        this.bonusBall = bonusBall;
+    }
+    public int[] checkWinning() {
+        int[] result = new int[6];
+        for (Lotto lotto : purchasedLotto) {
+            boolean hasBonusBall = lotto.getNumbers().contains(bonusBall);
+        }
+        return result;
+    }
 }

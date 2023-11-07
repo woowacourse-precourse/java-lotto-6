3ab54d10ec59b69e4f5cc776f58b8444b2891b26
Han <yulmam1@naver.com>
2023-11-08 00:36:22 +0900

    feat(LottoResult) : 총 수익률을 계산하는 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -7,10 +7,21 @@ public class LottoResult {
     private final Map<Rank, Integer> results = new HashMap<>();
 
     public void addResult(Rank rank){
-        results.put(rank, results.getOrDefault(rank, 0) + 1);
+        results.put(rank, results.getOrDefault(rank, -1) + 1);
     }
 
     public int getResult(Rank rank) {
         return results.get(rank);
+    }
+
+    public long getRateOfReturn(){
+        long sumPrize = 0;
+        long lottoCount = 0;
+        for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
+            sumPrize += entry.getKey().getPrize() * entry.getValue();
+            lottoCount++;
+        }
+
+        return sumPrize/lottoCount;
     }
 }

f113f29638814c6a1d79e501ce8063f2eb886c08
noteasymin <noteasymin@gmail.com>
2023-11-07 15:16:46 +0900

    refator: 당첨 번호 입력 시 중복 숫자 체크

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,7 +19,14 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
         } else if (!numbers.stream().allMatch(n -> n >= 1 && n <= 45)) {
             throw new IllegalArgumentException("[ERROR] 1 ~ 45 까지의 숫자를 입력해주세요.");
+        } else if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다. 다시 입력해주세요.");
         }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
     }
 
     @Override

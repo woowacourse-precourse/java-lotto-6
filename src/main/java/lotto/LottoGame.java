6d4df0d741f67b4999fecf75bee7da5815db3199
i3kae <snoger1234@gmail.com>
2023-11-07 17:16:47 +0900

    feat : 메인 객체 구현, 로또 구매 기능 완성 1. 로또 진행을 담당할 메인 객체 LottoGame 구현 2. 구매 금액 입력 후 구매 금액만큼 로또 구매 후 저장

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Input.InputType;
+
+public class LottoGame {
+    private static final int FIRST = 0;
+    private Input lottoInput = new Input();
+    private List<Integer> purchaseAmount;
+    private List<Lotto> purchasedLotto = new ArrayList<>();
+
+    public LottoGame() {
+    }
+
+    public void gameStart(){
+        purchaseAmount = lottoInput.input(InputType.PURCHASE_AMOUNT);
+        buyLotto(purchaseAmount.get(FIRST));
+    }
+
+    public void buyLotto(Integer purchaseAmount){
+        Print.printPurchase(purchaseAmount);
+        for (int i = 0; i < purchaseAmount % 1000; i++){
+            purchasedLotto.add(new Lotto(Utils.makeRandomNumbers(1, 45, 6)));
+        }
+        Print.printPurchasedLotto(purchasedLotto);
+    }
+}

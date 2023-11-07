272adc4709333f465d5c58aacf67999c4d48f088
qncj5224 <qncj5224@naver.com>
2023-11-07 11:29:03 +0900

    feat : 로또 번호 범위 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1 ~ 45 외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByValues() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,46)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }

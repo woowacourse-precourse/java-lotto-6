8eee233fc917a72821f4884474a4dd01b124e96d
yang <hc1489@naver.com>
2023-11-07 19:29:31 +0900

    feat: 당첨 번호와 보너스 번호 입력받아 저장 및 예외처리

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -8,9 +8,15 @@ import java.util.List;
 public class LottoController {
 
     static final String INPUT_PROMPT_AMOUNT_TO_PAY = "구입금액을 입력해 주세요.";
+    static final String PROMPT_LOTTO_COUNT = "8개를 구매했습니다.";
+    static final String INPUT_PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    static final String INPUT_PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     static final int LOTTO_PRICE = 1000;
-    private int lottoTicketCount;
+
+    private int lottoCount;
     private List<Lotto> issuedLottos;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
 
     public LottoController() {
         issuedLottos = new ArrayList<>();
@@ -21,7 +27,24 @@ public class LottoController {
 
         System.out.println(INPUT_PROMPT_AMOUNT_TO_PAY);
         totalAmountToPay = Integer.parseInt(Console.readLine());
-        this.lottoTicketCount = calculateLottoTicketCount(totalAmountToPay);
+        this.lottoCount = calculateLottoTicketCount(totalAmountToPay);
+    }
+
+    private void inputWinningNumbers() {
+        System.out.println(INPUT_PROMPT_WINNING_NUMBERS);
+        String winningNumbersInput = Console.readLine();
+        String[] winningNumbers = winningNumbersInput.split(",");
+
+        validate(winningNumbers);
+
+        for (String number : winningNumbers) {
+            this.winningNumbers.add(Integer.parseInt(number));
+        }
+    }
+
+    private void inputBonusNumber() {
+        System.out.println(INPUT_PROMPT_BONUS_NUMBER);
+        this.bonusNumber = Integer.parseInt(Console.readLine());
     }
 
     private int calculateLottoTicketCount(int totalAmountToPay) {
@@ -33,7 +56,7 @@ public class LottoController {
     }
 
     private void createLotto() {
-        for (int i = 0; i < lottoTicketCount; i++) {
+        for (int i = 0; i < lottoCount; i++) {
             List<Integer> lottoNumbers = generateLottoNumbers();
             Lotto lotto = new Lotto(lottoNumbers);
 
@@ -42,10 +65,42 @@ public class LottoController {
     }
 
     private void displayIssuedLottos() {
+        System.out.println(PROMPT_LOTTO_COUNT);
+
         for(Lotto lotto: this.issuedLottos) {
             lotto.displayLottoNumbers();
         }
     }
 
+    private void validate(String[] numbers) {
+        validateInputCount(numbers);
+        validateInputIsNumbers(numbers);
+        validateInputNumberRange(numbers);
+    }
+
+    private void validateInputCount(String[] numbers) {
+        if (numbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 형식에 맞춰 입력해주세요.");
+        }
+    }
+
+    private void validateInputIsNumbers(String[] numbers) {
+        for (String numberStr : numbers) {
+            try {
+                int number = Integer.parseInt(numberStr);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
+    }
+
+    private void validateInputNumberRange(String[] numbers) {
+        for (String numberStr : numbers) {
+            int number = Integer.parseInt(numberStr);
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 1 ~ 45 사이의 숫자만 입력하세요.");
+            }
+        }
+    }
 
 }

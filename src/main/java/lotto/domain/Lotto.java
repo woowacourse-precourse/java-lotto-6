eb75520bb253369312d76bc4adad9224e6e07bcc
ppdoli123 <ppdoli123@naver.com>
2023-11-07 18:40:43 +0900

    feat : 1~45 까지의 숫자만 입력 가능한 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -18,6 +18,9 @@ public class Lotto {
         }
         Set<Integer> uniqueNumbers = new HashSet<>();
         for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
             if (!uniqueNumbers.add(number)) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
             }

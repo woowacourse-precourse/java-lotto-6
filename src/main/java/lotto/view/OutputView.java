3b2f3b4847eff1cc728e10136ebb8856dfa7d9e8
twoosky <lhjae3@naver.com>
2023-11-08 08:02:21 +0900

    feat: 발행받은 로또 번호 출력 뷰 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+import java.text.DecimalFormat;
+import lotto.domain.LottoRank;
+import lotto.dto.LottoResultDto;
+import lotto.dto.LottosDto;
+
+public class OutputView {
+    private static final DecimalFormat decimalFormat = new DecimalFormat("###,###");
+    private static final String OUTPUT_BUY_QUANTITY_FORMAT = "%n%d개를 구매했습니다.%n";
+    private static final String OUTPUT_LOTTO_RESULT_FORMAT = "%s개 일치 (%,d원) - %d개%n";
+    private static final String OUTPUT_BONUS_LOTTO_RESULT_FORMAT = "%s개 일치, 보너스 볼 일치 (%,d원) - %d개%n";
+    private static final String OUTPUT_TOTAL_RETURN = "총 수익률은 %,.1f%%입니다.%n";
+    private static final String OUTPUT_STATISTICS_WINNING = "%n당첨 통계%n";
+    private static final String OUTPUT_STATISTICS_WINNING_DIVIDER = "---";
+
+
+    public static void printQuantity(int quantity) {
+        System.out.printf(OUTPUT_BUY_QUANTITY_FORMAT, quantity);
+    }
+
+    public static void printLottos(LottosDto dto) {
+        dto.getLotto().forEach(System.out::println);
+    }
+
+
+}

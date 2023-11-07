f3c568dc9e3e1de591095719f40597047377c345
dmong <dt2980_hee@naver.com>
2023-11-08 06:02:29 +0900

    feat: LottoNumber가 범위 이외의 값을 갖는지 검증 및 예외처리 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,15 +1,32 @@
 package lotto.domain;
 
-public class LottoNumber {
+import static lotto.domain.LottoConstants.LOTTO_NUMBER_RANGE_END;
+import static lotto.domain.LottoConstants.LOTTO_NUMBER_RANGE_START;
+import static lotto.view.ErrorConstants.OUT_OF_RANGE_NUMBER_ERROR_MESSAGE;
+
 public class LottoNumber implements Comparable<LottoNumber> {
     private final int number;
 
     private LottoNumber(int number) {
+        validate(number);
         this.number = number;
     }
 
     public static LottoNumber create(int number) {
         return new LottoNumber(number);
+    }
+
+    private boolean isOutOfRange(int number) {
+        return number < LOTTO_NUMBER_RANGE_START.getValue() || number > LOTTO_NUMBER_RANGE_END.getValue();
+    }
+
+    private void validate(int number) {
+        if (isOutOfRange(number)) {
+            throw new IllegalArgumentException(
+                    String.format(OUT_OF_RANGE_NUMBER_ERROR_MESSAGE.getMessage(),
+                            LOTTO_NUMBER_RANGE_START.getValue(),
+                            LOTTO_NUMBER_RANGE_END.getValue()));
+        }
     }
 
     @Override

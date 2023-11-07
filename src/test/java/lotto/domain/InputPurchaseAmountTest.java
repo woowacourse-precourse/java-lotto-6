36c5c33e7f53541a259ac924cda109827de104dc
74059 <shyeone@naver.com>
2023-11-07 14:30:49 +0900

    style: 코드 자동 정렬

--- src/test/java/lotto/domain/InputPurchaseAmountTest.java
+++ src/test/java/lotto/domain/InputPurchaseAmountTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class InputPurchaseAmountTest {
     private static final int NEGATIVE_PURCHASE = -1000;
     private static final int NOT_DIVIDE_1000 = 100500;
+
     @DisplayName("입력 받은 로또 구입 가격이 int형 이지만 양수가 아니라면 예외가 발생한다.")
     @Test
     void createPurchaseByNegative() {

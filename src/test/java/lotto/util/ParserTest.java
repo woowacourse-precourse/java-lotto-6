71ad67330009f529b51bdeb47338ef667e98a52a
Hyso <hsk7953@gmail.com>
2023-11-07 21:19:05 +0900

    test: 주석처리

--- src/test/java/lotto/util/ParserTest.java
+++ src/test/java/lotto/util/ParserTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
     Parser parser = new Parser();
-    @Test
-    @DisplayName("입력한 금액에 단위표시가 있을 때 테스트")
-    public void testParserInputWithComma(){
-        String input = "1,000";
-        int expected = 1000;
-        int result = parser.lottoPrice(input);
-        assertEquals(expected, result);
-    }
+//    @Test
+//    @DisplayName("입력한 금액에 단위표시가 있을 때 테스트")
+//    public void testParserInputWithComma(){
+//        String input = "1,000";
+//        int expected = 1000;
+//        int result = parser.lottoPrice(input);
+//        assertEquals(expected, result);
+//    }
 
     @Test
     @DisplayName("정상적으로 입력했을 때 테스트")
@@ -24,7 +24,5 @@ public class ParserTest {
         int result = parser.lottoPrice(input);
         assertEquals(expected, result);
     }
-
-
 
 }

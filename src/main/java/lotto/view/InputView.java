638974d52120a626c170b945d20837e3e1b2c5d1
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 15:33:32 +0900

    refactor : 사용하지 않는 변수 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private final String SPACE = " ";
+
 
     public String inputPurchasePrice() {
         System.out.println(INPUT_MONEY);

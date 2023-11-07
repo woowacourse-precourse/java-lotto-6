3813b4893f2b7eddc1d82994f29d2bcd61bb9c89
34suuuuu <psh7492@gmail.com>
2023-11-07 09:06:03 +0900

    feat: 당첨번호에 보너스 번호가 포함된 경우 예외 처리

--- src/main/java/lotto/domain/lotto/LottoAmount.java
+++ src/main/java/lotto/domain/lotto/LottoAmount.java
@@ -2,18 +2,16 @@ package lotto.domain.lotto;
 
 import lotto.exception.LottoMoneyDivideException;
 import lotto.exception.LottoMoneyLessException;
+import lotto.exception.LottoNumNotNumException;
 
 public class LottoMoney {
     private static final int LOTTO_MONEY_DIVIDE = 1000;
     private static int lottoMoney;
 
-    private LottoMoney(int lottoMoney) {
+    public LottoMoney(String lottoMoneyString) {
+        int lottoMoney = validateNumber(lottoMoneyString);
         validate(lottoMoney);
         this.lottoMoney = lottoMoney;
-    }
-
-    public int getLottoMoney(){
-        return lottoMoney;
     }
 
     public int changeToLottoCnt(){
@@ -21,11 +19,22 @@ public class LottoMoney {
     }
 
     private static void validate(int lottoMoney) {
+        if (lottoMoney <= 0) {
+            throw new IllegalArgumentException();
+        }
         if (lottoMoney < LOTTO_MONEY_DIVIDE) {
             throw new LottoMoneyLessException();
         }
         if (lottoMoney % LOTTO_MONEY_DIVIDE != 0) {
             throw new LottoMoneyDivideException();
+        }
+    }
+
+    private static int validateNumber(String lottoMoneyString) {
+        try {
+            return Integer.parseInt(lottoMoneyString);
+        } catch (NumberFormatException e) {
+            throw new LottoNumNotNumException();
         }
     }
 }
--- src/main/java/lotto/exception/BonusNumDuplicateException.java
+++ src/main/java/lotto/exception/BonusNumDuplicateException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class BonusNumDuplicateException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "당첨 번호에 보너스 번호가 포함되어 있습니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 당첨 번호에 보너스 번호가 포함되어 있습니다.";
 
     public BonusNumDuplicateException(){
         super(ERROR_MESSAGE);

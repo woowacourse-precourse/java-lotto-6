11222df269af608f1e840f43d907ec2327636cb0
InGyu Moon <ansdlsrb1121@naver.com>
2023-11-08 00:33:40 +0900

    feat: Lotto의 validate에 범위 검증 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,12 +13,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkSize(numbers);
+        checkUnique(numbers);
+        checkRange(numbers);
+    }
+
+    private static void checkSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또번호가 6개가 아닙니다.");
         }
+    }
+
+    private static void checkUnique(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또에 중복된 숫자가 존재하면 안됩니다.");
+        }
+    }
+
+    private static void checkRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("숫자는 1에서 45 사이어야 합니다: " + number);
+            }
         }
     }
 

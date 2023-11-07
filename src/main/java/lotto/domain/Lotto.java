64b71f8d3006a03c031a1a69d67416171f057fa8
SuhyungK <dook4966@gmail.com>
2023-11-08 02:48:34 +0900

    feat(Lotto): 구입 금액으로 구매할 수 있는 로또의 개수 구하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constants.LottoConfig;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static lotto.constants.LottoConfig.*;
 
 public class Lotto {
     private final List<Integer> numbers;
+
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -17,4 +24,9 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    
+    private static int getLottoCount (int payment) {
+        return payment / LottoConfig.UNIT_PRICE.getValue();
+    }
+
 }

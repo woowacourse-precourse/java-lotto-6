2f0d726fc08b37f6b9335045f44d0452aefbdc57
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 02:42:20 +0900

    [ADD] 사용자에게 로또 티켓의 구매 금액을 입력하고 유효성 검사

--- src/main/java/lotto/control/Lottogame.java
+++ src/main/java/lotto/control/Lottogame.java
@@ -20,4 +20,20 @@ package lotto.control;public class Lottogame {
         validatePurchaseAmount(purchaseAmount);
         return purchaseAmount;
     }
+
+    private int readPurchaseAmount() {
+        int purchaseAmount = 0;
+        while (purchaseAmount < 1000) {
+            purchaseAmount = readIntWithMessage("[ERROR] 로또 구입 금액은 1,000원 이상이어야 합니다.\n로또 구입 금액을 입력해 주세요.");
+        }
+        return purchaseAmount;
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 이상이어야 합니다.");
+        }
+    }
+
+    
 }

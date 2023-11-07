f00895139dcbf84720f2a997fe63a9bfe5dc56cf
whereami0404 <whereami2048@gmail.com>
2023-11-07 14:29:08 +0900

    refactor: 모델 검증 예외 메세지 상수 추가

--- src/main/java/lotto/exception/ModelExceptionConstant.java
+++ src/main/java/lotto/exception/ModelExceptionConstant.java
@@ -3,7 +3,8 @@ package lotto.exception;
 import org.junit.jupiter.api.Test;
 
 public enum ModelExceptionConstant {
-    OUT_OF_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45사이의 숫자이어야 합니다.");
+    OUT_OF_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45사이의 숫자이어야 합니다."),
+    NOT_DIVIDE_BY_LOTTO_PRICE("[ERROR] 로또 구입 금액이 로또 1장의 가격으로 나누어 떨어지지 않습니다.");
 
     private final String text;
     ModelExceptionConstant(String text) {

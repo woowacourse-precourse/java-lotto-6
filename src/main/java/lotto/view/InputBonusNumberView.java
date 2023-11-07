49016523678a31551759413365e13ed62c07b7df
깃wooqqq <sge0225@naver.com>
2023-11-08 03:59:02 +0900

    feat: 보너스 번호에 공백 예외처리 및 String 입력값 통합 예외처리 메서드 추가

--- src/main/java/lotto/view/InputBonusNumberView.java
+++ src/main/java/lotto/view/InputBonusNumberView.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class InputBonusNumberView {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
-    private static final String SEPARATOR = ",";
+    private static final String BLANK = " ";
     private static final String ERROR_MESSAGE = "[ERROR] ";
     private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String ERROR_NOT_VALIDATE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
@@ -19,7 +19,7 @@ public class InputBonusNumberView {
         String input = Console.readLine();
         System.out.println();
         try {
-            validateNumeric(input);
+            validate(input);
             int bonus = Integer.parseInt(input);
             validateRangeNumber(bonus);
             validateDuplicate(winningNumbers, bonus);
@@ -30,8 +30,19 @@ public class InputBonusNumberView {
         }
     }
 
+    private void validate(String input) {
+        validateNumeric(input);
+        validateBlank(input);
+    }
+
     private void validateNumeric(String input) {
         if (!PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateBlank(String input) {
+        if (input.contains(BLANK)) {
             throw new IllegalArgumentException();
         }
     }

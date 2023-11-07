7494a21686e6ca2409d776084288f02f8c6e2546
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 20:43:19 +0900

    feat(Game): 게임 세팅, 게임 실행 기능 구현

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,2 +1,71 @@
-package lotto.controller;public class Game {
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.util.LottoNumberGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    public void start() {
+        int purchaseAmount = 0;
+        while (true) {
+            try {
+                purchaseAmount = InputView.getPurchaseAmount();
+                // 입력이 유효하면 계속 진행
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+        Cash cash = new Cash(purchaseAmount);
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        LottoSeller lottoSeller = new LottoSeller(lottoNumberGenerator);
+
+        Customer customer = new Customer(cash, lottoSeller);
+
+        customer.purchaseLottos();
+
+        OutputView.displayLottos(customer.getLottos());
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        while (true) {
+            try {
+                winningNumbers = InputView.getWinningNumbers();
+                // 입력이 유효하면 계속 진행
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+
+        Integer number = 0;
+
+        while (true) {
+            try {
+                number = InputView.getBonusNumber();
+                // 입력이 유효하면 계속 진행
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+
+        WinningNumber winningNumber = new WinningNumber(winningNumbers);
+        BonusNumber bonusNumber = new BonusNumber(number);
+
+        WinningNumbers winningNumbers1 = new WinningNumbers(winningNumber, bonusNumber);
+        WinningRanks winningRanks = new WinningRanks(customer.getLottos(), winningNumbers1);
+
+        winningRanks.calculateRanks();
+
+        Profit profit = new Profit();
+        profit.calculateAmount(winningRanks.getRanks());
+
+        WinningYield winningYield = new WinningYield(cash, profit);
+
+        OutputView.displayWinningDetails(winningRanks.getRanks(), winningYield);
+    }
 }

aa9be1d21b15605500a4d30964a3b67445256c99
JaeHunJang <wuddlaa@gmail.com>
2023-11-08 00:30:50 +0900

    refactor(InputValidator): 멤버변수 참조하도록 메소드 수정

--- src/main/java/lotto/util/validator/InputValidator.java
+++ src/main/java/lotto/util/validator/InputValidator.java
@@ -6,12 +6,12 @@ public class InputValidator {
     final String input;
 
     public InputValidator(String input) {
-        validateEmpty();
+        validateEmpty(input);
         this.input = input;
     }
 
-    private void validateEmpty() {
-        if(input.isEmpty() || input.isBlank()) {
+    private void validateEmpty(String input) {
+        if (input.isEmpty() || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.EMPTY.getMessage());
         }
     }

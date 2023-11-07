73f87462de4e5ae0ccd766f8531d2b6cfc0d3915
윤병호 <pdot715@gmail.com>
2023-11-08 08:28:54 +0900

    refactor(Lotto):ProfitTest 좀 더 가독성 좋게 수정

--- src/test/java/lotto/ProfitTest.java
+++ src/test/java/lotto/ProfitTest.java
@@ -11,7 +11,8 @@ class ProfitTest {
     @DisplayName("수익률을 정확하게 계산하는지, 소수점 둘째자리에서 반올림 하는지 확인")
     @Test
     void createProfit() {
-        assertThat(new Profit(new Money(8000), 5000).getProfit()).isEqualTo(62.5);
+        assertThat(new Profit(new Money(8000), 5000).getProfit())
+                .isEqualTo(62.5);
     }
 
 }

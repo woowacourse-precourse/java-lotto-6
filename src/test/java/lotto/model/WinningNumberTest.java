a0858403b9d6d0b512038500ef69ac311e55a9fc
jihun <pjh2996@naver.com>
2023-11-07 20:19:26 +0900

    style : 클래스 이름에 맞게 테스트명 변경 WinningLottoTest->WinningNumberTest

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class WinningLottoTest {
+class WinningNumberTest {
 
     @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test

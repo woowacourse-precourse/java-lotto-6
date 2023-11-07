7b47b92d4190cd1a13e4be790b8a606e43a27742
changhyun9 <ckdgus1713@naver.com>
2023-11-07 21:41:24 +0900

    feat : add validate number range

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,43 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    public static final int MAX_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateRange(numbers);
+        validateDuplication(numbers);
+        validateSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != MAX_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개이어야 합니다.");
         }
+    }
+
+    private void validateDuplication(List<Integer> numbers){
+        Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+        if (nonDuplicatedNumbers.size() != MAX_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 중복돼서는 안됩니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이 여야 합니다.");
+            }
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,7 @@
-package lotto;
+package lotto.domain;
 
 import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +25,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("1 ~ 45 범위를 벗어나는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRange(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
     // 아래에 추가 테스트 작성 가능
 }

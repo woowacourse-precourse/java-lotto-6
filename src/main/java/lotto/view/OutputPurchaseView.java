fa800a6a8fe9d228ab0aca0353980f9484d3530f
realcold0 <realcold0@gmail.com>
2023-11-08 03:31:47 +0900

    feat(lotto) : 구매한 Lotto의 갯수를 와 구매한 로또들의 숫자들을 보여주는 OutputPurchaseView 기능 생성

---
+++ src/main/java/lotto/view/OutputPurchaseView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import lotto.domain.Lottos;
+
+/**
+ * 구매한 Lotto의 갯수를 보여주는 view
+ */
+public class OutputPurchaseView {
+    /**
+     * 구매한 로또의 갯수와 로또들의 수를 보여준다.
+     * @param amount 구매한 Lotto 갯수
+     * @param lottos 구매한 Lotto들이 들어있는 Lottos
+     */
+    public void printPurchaseLotto(int amount, Lottos lottos){
+        System.out.println(amount + "개를 구매했습니다.");
+        lottos.printLottos();
+    }
+}

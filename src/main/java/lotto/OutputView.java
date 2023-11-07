a4f3f041864d277e84bedaa46aac440813405cd7
oosedus <seayeon0729@gmail.com>
2023-11-08 04:49:47 +0900

    feat: 당첨 내역 출력 함수 추가

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -13,4 +13,14 @@ public class OutputView {
             System.out.println(numbers);
         }
     }
+
+    void printCountOfWonLotto(List<Integer> counts){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(String.format("3개 일치 (5,000원) - %d개", counts.get(0)));
+        System.out.println(String.format("4개 일치 (50,000원) - %d개", counts.get(1)));
+        System.out.println(String.format("5개 일치 (1,500,000원) - %d개", counts.get(2)));
+        System.out.println(String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", counts.get(3)));
+        System.out.println(String.format("6개 일치 (2,000,000,000원) - %d개", counts.get(4)));
+    }
 }

3745328127747a3c36915de97397415ec4de2c7d
Jinseo Jeong <js020315@gmail.com>
2023-11-07 19:21:55 +0900

    [FEAT] LottoNums 객체

---
+++ src/main/java/lotto/Model/LottoNums.java
@@ -0,0 +1,28 @@
+package lotto.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNums {
+    private static final int NUMBER_COUNT = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private static List<Integer> lottoNumberList;
+
+    public LottoNums() {
+
+    }
+
+    public static List<Integer> setRandomNums() {
+        List<Integer> lottoPurchased = new ArrayList<>(lottoNumberList);
+
+        lottoNumberList = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT);
+        Collections.sort(lottoPurchased);
+
+        return lottoPurchased;
+    }
+}

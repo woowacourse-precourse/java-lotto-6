3b59fe725a003d3daa6b1db1a531c2a229a468f9
ASak1104 <khw56184@gmail.com>
2023-11-08 05:47:11 +0900

    feat: 로또 발급 기능 추가

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -1,13 +1,40 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import lotto.model.Lotto;
 
 public final class LottoGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int NUMBER_COUNT = 6;
+
     private LottoGenerator() {
         // Don't let anyone instantiate this class.
     }
 
     public static Lotto generate() {
-        return null;
+        List<Integer> numbers = generateUniqueNumbers(NUMBER_COUNT);
+
+        return new Lotto(numbers);
+    }
+
+    private static List<Integer> generateUniqueNumbers(int count) {
+        Set<Integer> numbers = new HashSet<>();
+
+        while (numbers.size() < count) {
+            int number = generateNumber();
+
+            numbers.add(number);
+        }
+
+        return numbers.stream()
+                .toList();
+    }
+
+    private static int generateNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }

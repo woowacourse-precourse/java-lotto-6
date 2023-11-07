15d8312357a5642568b59e3ada9a4d89a57ae02a
yunju-m <dbswnfnal@naver.com>
2023-11-07 14:13:01 +0900

    Feat(GameController): ✨ 로또 구매 금액 객체 선언 및 반복 기능 구현
    입력한 로또 구매 금액 객체 선언 및 유효성 검사 호출
    유효한 데이터가 아닌 경우 IllegalArgumentException 예외 처리 후 반복

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,13 +1,28 @@
 package controller;
 
+import lotto.LottoMoney;
 import view.InputView;
 
 public class GameController {
+    LottoMoney lottoMoney;
+
     InputView inputView = new InputView();
+    boolean restart = true;
 
     public void startGame() {
-        String lottoMoney = inputView.enterLottoMoney();
-        System.out.println(lottoMoney);
+        saveLottoMoney();
+    }
+
+    private void saveLottoMoney() {
+        while (restart) {
+            try {
+                String inputMoney = inputView.enterLottoMoney();
+                lottoMoney = new LottoMoney(inputMoney);
+                restart = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 }

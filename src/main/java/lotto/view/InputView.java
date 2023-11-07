8edfd5a8df161224ba92604b23a01952744f2396
nylonmusk <2912rla@gmail.com>
2023-11-07 23:58:32 +0900

    refactor : try catch 적용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.LottoNumberValidator;
+import lotto.validator.PurchaseAmountValidator;
 
 public class InputView {
 
@@ -10,18 +13,41 @@ public class InputView {
         this.outputView = new OutputView();
     }
 
-    public int readPurchaseAmount() {
-        outputView.printRequestPurchaseAmount();
-        return Integer.parseInt(Console.readLine());
+    public int getPurchaseAmount() {
+        try {
+            outputView.printRequestPurchaseAmount();
+            String purchaseAmount = getInput();
+            return PurchaseAmountValidator.validate(purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return getPurchaseAmount();
+        }
     }
 
-    public String readWinningNumbers() {
-        outputView.printRequestWinningNumbers();
+    public String getWinningNumbers() {
+        try {
+            outputView.printRequestWinningNumbers();
+            String winningNumbers = getInput();
+            LottoNumberValidator.isValid(LottoNumberValidator.convertStringToList(winningNumbers));
+            return winningNumbers;
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return getWinningNumbers();
+        }
+    }
+
+    public int getBonusNumber() {
+        try {
+            outputView.printRequestBonusNumber();
+            String bonusNumber = Console.readLine();
+            return BonusNumberValidator.validate(bonusNumber);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return getBonusNumber();
+        }
+    }
+
+    private String getInput() {
         return Console.readLine();
-    }
-
-    public int readBonusNumber() {
-        outputView.printRequestBonusNumber();
-        return Integer.parseInt(Console.readLine());
     }
 }

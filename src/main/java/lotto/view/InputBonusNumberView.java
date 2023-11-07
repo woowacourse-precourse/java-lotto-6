0449eb7c8f29bf927606c9aca8de8fe07cf78786
깃wooqqq <sge0225@naver.com>
2023-11-08 03:34:02 +0900

    style: 에러 메시지 상수명 변경 및 하나의 메시지로 통일

--- src/main/java/lotto/view/InputBonusNumberView.java
+++ src/main/java/lotto/view/InputBonusNumberView.java
@@ -10,8 +10,7 @@ public class InputBonusNumberView {
     private static final String SEPARATOR = ",";
     private static final String ERROR_MESSAGE = "[ERROR] ";
     private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private static final String ERROR_NOT_NUMBER = "숫자 형태의 값만 입력할 수 있습니다.";
-    private static final String ERROR_NOT_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_NOT_VALIDATE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final Pattern PATTERN = Pattern.compile("\\d+");
 
     public Integer getBonusNumber() {
@@ -21,7 +20,7 @@ public class InputBonusNumberView {
         try {
             validateNumeric(input);
         } catch (IllegalArgumentException e) {
-            System.out.println(ERROR_MESSAGE + ERROR_NOT_NUMBER);
+            System.out.println(ERROR_MESSAGE + ERROR_NOT_VALIDATE);
             return getBonusNumber();
         }
         return Integer.parseInt(input);

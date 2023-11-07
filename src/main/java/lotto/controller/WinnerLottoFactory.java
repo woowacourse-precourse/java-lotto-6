112500cedc5d7dbed418f0eba3cfb41822668272
hunko <rhwlgns4386@naver.com>
2023-11-07 15:17:42 +0900

    refactor: 정답로또 생성로직 위임

---
+++ src/main/java/lotto/controller/WinnerLottoFactory.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+import lotto.controller.numbercandidatestring.NumberCandidateString;
+import lotto.controller.numbercandidatestring.NumberCandidateStrings;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.lotto.WinnerLotto;
+
+public class WinnerLottoFactory {
+
+    public  WinnerLotto generate(NumberCandidateStrings winningNumber, NumberCandidateString bonus) {
+        Lotto lotto = new Lotto(winningNumber.toLottoNumberList());
+
+        return new WinnerLotto(lotto, new LottoNumber(bonus.getNumber()));
+    }
+}

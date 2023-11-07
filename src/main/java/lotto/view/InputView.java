8a03ac6e2e70412dedd515fa75a5182af0ea70e3
정재웅 <jweidon0821@gmail.com>
2023-11-07 16:20:28 +0900

    feat(view): 금액 입력 함수 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,11 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String askMoney = "구입금액을 입력해 주세요.";
     public static String inputMoney() {
+        System.out.println(askMoney);
+        return Console.readLine();
     }
 }

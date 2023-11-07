68a175c7b9c60e3b17ccdc3c2da93db03cffa0ed
정재웅 <jweidon0821@gmail.com>
2023-11-08 04:20:21 +0900

    refactor: 랜덤 숫자 뽑기 Numbers 클래스로 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,10 +3,12 @@ package lotto.controller;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
 import lotto.domain.Money;
+import lotto.domain.Numbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
     private static ArrayList<Lotto> myLottos = new ArrayList<>();
@@ -22,8 +24,9 @@ public class LottoController {
     public void buyLottos(int trial) {
         OutputView.printNoticeTrialMessage(trial);
         do {
-            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-            myLottos.add(lotto);
+            List<Integer> numbers = Numbers.makeRandomNumbers();
+            myLottos.add(new Lotto(numbers));
         } while (myLottos.size() < trial);
+        System.out.println(myLottos);
     }
 }
---
+++ src/main/java/lotto/domain/Numbers.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Numbers {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int NUMBER_COUNTER = 6;
+
+    public static List<Integer> makeRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNTER);
+    }
+}

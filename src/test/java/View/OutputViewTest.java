993520c08e391f64a937babd8a471e499d02116a
nayeon Lee <skdus4190@gmail.com>
2023-11-08 05:16:15 +0900

    Feat: 수익률 계산 test
    당첨금과 구매비용을 비교해 수익률을 제대로 계산하는지 테스트 하는 코드 작성

--- src/test/java/View/OutputViewTest.java
+++ src/test/java/View/OutputViewTest.java
@@ -5,6 +5,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import domain.Amount;
+import domain.Rank;
 import java.util.List;
 import lotto.Application;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +26,21 @@ class OutputViewTest extends NsTest {
                 },
                 List.of(23, 21, 8, 41, 42, 43),
                 List.of(3, 11, 5, 38, 32, 16)
+        );
+    }
+
+    @DisplayName("수익률이 잘 계된 되었는지 확인")
+    @Test
+    void checkCalculateRateOfReturn() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    runException("2000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "총 수익률은 2750.0%입니다."
+                    );
+                },
+                List.of(1,2,3,9,10,11),
+                List.of(1,2,3,4,10,11)
         );
     }
 

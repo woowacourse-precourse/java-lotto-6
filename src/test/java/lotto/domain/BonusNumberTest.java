adaf0c2789bca3f68a3363bb972f0f96685cb923
bbbbooo <qlqlzbxkzl@naver.com>
2023-11-07 17:08:48 +0900

    test(BonusNumberTest) : 보너스 번호에 1부터 45 사이의 값 입력 시, 예외처리하는지 테스트 구현

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -17,4 +17,13 @@ class BonusNumberTest {
         assertThatThrownBy(() -> new BonusNumber(input))
                 .isInstanceOf(InvalidInputException.class);
     }
+
+    @DisplayName("보너스 번호에 1부터 45 사이의 값 입력 시, 예외처리하는지")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46", "300"})
+    void isCorrectNumberRangeTest(String input) {
+        // given & when & then
+        assertThatThrownBy(() -> new BonusNumber(input))
+                .isInstanceOf(InvalidInputException.class);
+    }
 }

11038f43cbad7e5a6937337f3a584b3ee42cac48
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-07 22:59:26 +0900

    refact(application): 에러 메세지 수정

--- src/main/java/lotto/utils/ErrorCode.java
+++ src/main/java/lotto/utils/ErrorCode.java
@@ -1,9 +1,9 @@
 package lotto.utils;
 
 public enum ErrorCode {
-    INVALID_NUMBERS("로또 번호는 "+Constants.MIN_LOTTO_NUMBER+"부터 "+Constants.MAX_LOTTO_NUMBER+" 사이의 숫자여야 합니다."),
-    TOOMANY_NUMBERS(Constants.LOTTO_COUNT +"개의 번호를 입력해주세요."),
-    INVALID_MONEY(Constants.PRICE_UNIT+"원 단위의 금액을 입력해주세요."),
+    INVALID_NUMBERS("로또 번호는 "+Constants.MIN_LOTTO_NUMBER.getValues()+"부터 "+Constants.MAX_LOTTO_NUMBER.getValues()+" 사이의 숫자여야 합니다."),
+    TOOMANY_NUMBERS(Constants.LOTTO_COUNT.getValues() +"개의 번호를 입력해주세요."),
+    INVALID_MONEY(Constants.PRICE_UNIT.getValues()+"원 단위의 금액을 입력해주세요."),
     INPUT_IS_NULL("공백을 입력할 수 없습니다.");
 
     private final String message;
@@ -13,6 +13,6 @@ public enum ErrorCode {
     }
 
     public String getMessage() {
-        return "[ERROR]"+message;
+        return "[ERROR] "+message;
     }
 }

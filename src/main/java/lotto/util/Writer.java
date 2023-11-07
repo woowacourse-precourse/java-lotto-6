07e93ec340b1cbfd3cb9ed879031504dd9862bd1
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-07 17:51:29 +0900

    feat: 출력 기능
    구입금액을 입력해 주세요 출력 기능 추가

---
+++ src/main/java/lotto/util/Writer.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class Writer {
+
+    private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static void purchaseAmount(){
+        System.out.println(PURCHASE_AMOUNT);
+    }
+}

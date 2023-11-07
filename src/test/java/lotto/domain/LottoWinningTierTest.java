bb515be85d0eb0656a52ca64b44464aad9bf6587
eora21 <eora21@naver.com>
2023-11-07 14:12:21 +0900

    test: 💍 추가 맞춘 로또 숫자가 6개인데 보너스까지 맞췄을 경우 예외를 발생시키는 테스트 작성

--- src/test/java/lotto/domain/LottoWinningTierTest.java
+++ src/test/java/lotto/domain/LottoWinningTierTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import domain.LottoWinningTier;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -23,6 +24,12 @@ public class LottoWinningTierTest {
     @DisplayName("일치한 로또 숫자 개수가 6개를 초과하면 예외를 발생시킨다.")
     void calculateLottoWinningTierByOverCount() {
         assertThrows(IllegalArgumentException.class, () -> LottoWinningTier.calculateTier(7, false));
+    }
+
+    @Test
+    @DisplayName("일치한 로또 숫자 개수가 6개이며 보너스 번호까지 맞췄다면 예외를 발생시킨다.")
+    void calculateLottoWinningTierByMaxCountPlusBonus() {
+        assertThrows(IllegalArgumentException.class, () -> LottoWinningTier.calculateTier(6, true));
     }
 
     @ParameterizedTest

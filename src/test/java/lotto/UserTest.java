286e7c18df48c72edc3526bec192e70f5b639643
gonixxxx <akdntm946@gmail.com>
2023-11-08 04:34:20 +0900

    test: rename class
    BonusNumberTest -> UserTest

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class BonusNumberTest {
+public class UserTest {
 
     private Lotto lotto;
 

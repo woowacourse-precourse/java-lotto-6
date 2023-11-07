cc0666dee776b38f3032926f29b0807c4561888f
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 16:23:05 +0900

    feat(LottoTickets): 로또 개수만큼 로또 티켓을 생성하는 클래스 추가
    - 로또 티켓 개수만큼 랜덤 로또 티켓을 생성해주는 LottoTikets 클래스 추가

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.util.Constants;
+
+public class LottoTickets {
+    private final List<Lotto> lottoTickets;
+    public LottoTickets(int ticketAmount) {
+        this.lottoTickets = createLottoTickets(ticketAmount);
+    }
+
+    private static List<Lotto> createLottoTickets(int ticketAmount) {
+        List<Lotto> tickets = new ArrayList<>();
+        IntStream.range(0, ticketAmount).forEach(i ->
+            tickets.add(new Lotto(generateRandomNumbers())));
+        return tickets;
+    }
+
+    private static List<Integer> generateRandomNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(Constants.MIN_NUMBER,
+                Constants.MAX_NUMBER, Constants.LOTTO_SIZE);
+        Collections.sort(randomNumbers);
+        return randomNumbers;
+    }
+}

2dac5b86cc10d6293ff7b30269f454356b5d6316
freekyr7529 <freekyr7529@gmail.com>
2023-11-07 22:04:34 +0900

    feat: 생성된 로또 번호들과 정답을 비교하는 로직 생성
    - Tickets 클래스 내부에 Answer 객체를 매개변수로 받아 각 로또 번호와 정답을 비교한 후 EnumMap 형식으로 결과를 취합한 뒤 이를 반환하는 compareTicketsToAnswer 메서드를 추가했다.
    - EnumMap을 초기화하는 initEnumMap 메서드를 추가했다.
    - Map을 순회하는 circuitEnum 메서드를 추가했다.
    - Result 객체에서 결과를 받아 이를 EnumMap에 반영하는 compareResultToCriterion 메서드를 추가했다.

--- src/main/java/lotto/domain/Tickets.java
+++ src/main/java/lotto/domain/Tickets.java
@@ -2,7 +2,9 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
+import lotto.enums.Reward;
 
 public class Tickets {
     private final List<Lotto> tickets;
@@ -17,5 +19,37 @@ public class Tickets {
 
     public List<Lotto> getTickets() {
         return Collections.unmodifiableList(tickets);
+    }
+
+    public TotalResult compareTicketsToAnswer(Answer answer) {
+        EnumMap<Reward, Integer> totalResult = new EnumMap<>(Reward.class);
+        initEnumMap(totalResult);
+        for (Lotto ticket : tickets) {
+            Result result = ticket.compareNumberToAnswer(answer);
+            circuitEnum(totalResult, result);
+        }
+        return new TotalResult(totalResult);
+    }
+
+    private void initEnumMap(EnumMap<Reward, Integer> result) {
+        for (Reward reward : Reward.values()) {
+            result.put(reward, 0);
+        }
+    }
+
+    private void circuitEnum(EnumMap<Reward, Integer> totalResult, Result result) {
+        for (Reward reward : totalResult.keySet()) {
+            compareResultToCriterion(totalResult, reward, result);
+        }
+    }
+
+    private void compareResultToCriterion(EnumMap<Reward, Integer> totalResult, Reward reward, Result result) {
+        int hitResult = result.getHitResult();
+        int bonusResult = result.getBonusResult();
+
+        if (reward.compareHitCnt(hitResult) && reward.compareBonusCnt(bonusResult)) {
+            int preValue = totalResult.get(reward);
+            totalResult.put(reward, preValue + 1);
+        }
     }
 }

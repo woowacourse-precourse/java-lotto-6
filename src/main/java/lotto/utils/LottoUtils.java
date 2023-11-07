0b7e7952af9a30450fbb490136d812e26c9359a6
jookbooin <3670lsh@naver.com>
2023-11-07 19:04:14 +0900

    feat : LottoUtils 추가

---
+++ src/main/java/lotto/utils/LottoUtils.java
@@ -0,0 +1,29 @@
+package lotto.utils;
+
+import lotto.constant.StringConstant;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.validator.GlobalValidator.validateNumber;
+
+public class LottoUtils {
+
+    public static List<String> splitStringToList(StringConstant constant, String input) {
+        String[] splitWithDelimiter = splitStringByDelimiter(constant, input);
+        return Arrays.asList(splitWithDelimiter);
+    }
+
+    public static String[] splitStringByDelimiter(StringConstant constant, String input) {
+        return input.split(constant.value());
+    }
+
+    public static int convertStringToInteger(String input){
+        return Integer.parseInt(input);
+    }
+
+    public static int validateAndConvertStringToInteger(String number) {
+        validateNumber(number);
+        return convertStringToInteger(number);
+    }
+}

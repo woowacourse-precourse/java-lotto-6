f152235decf7774b9af721b2d50e56fc47337d94
Choijake <shameless8@naver.com>
2023-11-08 00:06:08 +0900

    Refactor : InputView 메서드 명 간략화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,36 +4,35 @@ import static lotto.view.constant.ConstantMessage.INPUT_AMOUNT_MESSAGE;
 import static lotto.view.constant.ConstantMessage.INPUT_BONUS_NUMBER_MESSAGE;
 import static lotto.view.constant.ConstantMessage.INPUT_LOTTO_NUMBER_MESSAGE;
 
-
 import camp.nextstep.edu.missionutils.Console;
 import lotto.view.constant.ConstantMessage;
 
 public class InputView {
-    public static int inputAmountView(){
+    public static int inputAmount() {
         printConstantMessage(INPUT_AMOUNT_MESSAGE);
 
         return Integer.parseInt(Console.readLine());
     }
 
-    public static void inputBonusNumberView(){
+    public static int inputBonusNumber() {
         printConstantMessage(INPUT_BONUS_NUMBER_MESSAGE);
         printNewLine();
-        Console.readLine();
+        return Integer.parseInt(Console.readLine());
     }
 
-    public static void inputLottoNumberView(){
+    public static String inputWinningLottoNumber() {
         printConstantMessage(INPUT_LOTTO_NUMBER_MESSAGE);
         printNewLine();
-        Console.readLine();
+        return Console.readLine();
     }
 
-    private static void printConstantMessage(ConstantMessage message){
+    private static void printConstantMessage(ConstantMessage message) {
         System.out.print(message);
         printNewLine();
         Console.readLine();
     }
 
-    private static void printNewLine(){
+    private static void printNewLine() {
         System.out.print("\n");
     }
 }

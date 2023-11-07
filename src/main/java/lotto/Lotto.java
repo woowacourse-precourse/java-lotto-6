9c65ea1407dec6305bda75dd01fd9f802a5cc608
jagarchi <relong1026@naver.com>
2023-11-07 19:11:19 +0900

    로또 구입금액 입력받기

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,14 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    private int money;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
+
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
@@ -16,5 +19,12 @@ public class Lotto {
         }
     }
 
+    private void getMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        money = Integer.parseInt(Console.readLine());
+    }
+
+
     // TODO: 추가 기능 구현
 }
+

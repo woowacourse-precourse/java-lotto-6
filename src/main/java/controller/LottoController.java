86ecf6b3c35a84b4c52eff20a4e0182bc790e253
nayeon Lee <skdus4190@gmail.com>
2023-11-07 22:22:43 +0900

    Refactor: run() 내용 메소드 분할
    전체 진행을 단계별로 나눠서 함수화

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -15,27 +15,35 @@ import service.UserInputService;
 public class LottoController {
 
     public void run() {
-        //buyLotto
+        Amount amount = buyLotto();
+        Lottos lottos = createLottos(amount);
+        WinningNumbers winningNumbers = createWinningNumbers();
+        Rank rank = CompareLottoServcie.rank(amount, lottos, winningNumbers);
+        OuputView.lottosResult(amount, rank);
+    }
+
+    private Amount buyLotto() {
         InputView.buyMessage();
         Amount amount = MakeObjectService.amount(UserInputService.amount());
         OuputView.buyMessage(amount);
 
-        //createLottos
+        return amount;
+    }
+
+    private Lottos createLottos(Amount amount) {
         Lottos lottos = MakeObjectService.lottos(amount);
         OuputView.lottosNumbers(lottos);
 
-        //createWinningNumbers
+        return lottos;
+    }
+
+    private WinningNumbers createWinningNumbers() {
         InputView.winningNumbersMessage();
         Lotto lotto = UserInputService.lotto();
         InputView.bonusNumberMessage();
         BonusNumber bonusNumber = UserInputService.bonusNumber(lotto);
-        WinningNumbers winningNumbers = MakeObjectService.winningNumbers(lotto, bonusNumber);
 
-        //compareLottoNumbers
-        Rank rank = CompareLottoServcie.rank(amount, lottos, winningNumbers);
-
-        //printResult
-        OuputView.lottosResult(amount, rank);
+        return  MakeObjectService.winningNumbers(lotto, bonusNumber);
     }
 
 }

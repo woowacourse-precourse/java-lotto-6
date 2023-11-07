41d998e8ff4c9f495fd45795767778f1cf800823
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-11-08 03:48:26 +0900

    feat: 구매 금액 만큼 로또 생성 및 처리 기능 개발

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,20 @@
 package lotto.controller;
 
+import java.util.List;
 import java.util.function.Supplier;
 import lotto.model.Amount;
+import lotto.model.Lotto;
+import lotto.model.LottoNumbersGenerator;
+import lotto.model.Lottos;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
+    Lottos lottos;
+
     public void run() {
         Amount amount = getValidInput(() -> new Amount(InputView.requestAmount()));
+        createLottos(amount);
     }
 
     // IllegalArgumentException 발생 시 해당 메소드 반복
@@ -18,5 +26,18 @@ public class LottoController {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    // 입력 받은 금액에 해당하는 로또 생성
+    private void createLottos(Amount amount) {
+        List<Lotto> autoLottos = generateAutoLottos(amount);
+        lottos = new Lottos(autoLottos);
+        OutputView.printLottos(lottos);
+    }
+
+    private List<Lotto> generateAutoLottos(Amount amount) {
+        int count = amount.calculateCount();
+        OutputView.printCount(count);
+        return new LottoNumbersGenerator().generateLottos(count);
     }
 }

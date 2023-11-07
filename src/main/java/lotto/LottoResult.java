5e3fdc4154e09d8324dfe66f9b05ab34cca76acc
pjaehyun <show6756@gmail.com>
2023-11-07 20:15:46 +0900

    feat: 수정 입력값을 main에서 다 변환하여 전달받는 형태로 변경

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,26 +1,16 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class LottoResult {
 
     private final List<Integer> resultNumbers;
     private final int bonusNumber;
 
-    public LottoResult(String resultNumbers, String bonusNumber) {
-        this.resultNumbers = convertToIntegerList(resultNumbers);
-        this.bonusNumber = Integer.parseInt(bonusNumber);
-    }
-
-    private List<Integer> convertToIntegerList(String resultNumbers) {
-        String[] numbers = resultNumbers.split(",");
-        List<Integer> list = new ArrayList<>();
-
-        for (String number : numbers) {
-            list.add(Integer.parseInt(number.trim()));
-        }
-        return list;
+    public LottoResult(List<Integer> resultNumbers, int bonusNumber) {
+        this.resultNumbers = resultNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public int containResultNumbers(int number) {

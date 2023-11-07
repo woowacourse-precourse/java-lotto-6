6259b526778dcce99af0af56caebd2912e664f0e
zzun73 <736300@naver.com>
2023-11-08 00:56:47 +0900

    refactor: 사용자 입력 에러 메시지 상수화

--- src/main/java/lotto/exception/UserInputValidator.java
+++ src/main/java/lotto/exception/UserInputValidator.java
@@ -9,6 +9,13 @@ import java.util.Set;
 import static lotto.constant.Constants.*;
 
 public class UserInputValidator {
+    private static final String DUPLICATE_WINNING_NUMBER_AND_BONUS_NUMBER_MESSAGE = "[ERROR] 당첨 로또 번호와 보너스 번호는 서로 달라야 합니다.";
+    private static final String DUPLICATE_LOTTO_NUMBER_MESSAGE = "[ERROR] 당첨 로또 번호는 중복되면 안됩니다.";
+    private static final String INVALID_WINNING_NUMBER_DELIMITER_MESSAGE = "[ERROR] 당첨 번호는 ,로 구분해야 합니다.";
+    private static final String INVALID_LOTTO_NUMBER_RANGE_MESSAGE = "[ERROR] 당첨 번호는 1부터 45까지의 정수여야 합니다.";
+    private static final String NON_NUMERIC_INPUT_MESSAGE = "[ERROR] 입력 값은 숫자여야 합니다.";
+    private static final String NEGATIVE_PURCHASE_AMOUNT_MESSAGE = "[ERROR] 로또 구입 금액은 양수여야 합니다.";
+    private static final String INVALID_PURCHASE_UNIT_MESSAGE = "[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.";
 
     public static void validatePurchasePrice(String purchasePrice) {
         isNumber(purchasePrice);
@@ -34,26 +41,26 @@ public class UserInputValidator {
 
     private static void isValidDuplicateBonusNumber(Lotto winningLotto, int bonusNumber) {
         if (winningLotto.contain(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 당첨 로또 번호와 보너스 번호는 서로 달라야 합니다.");
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBER_AND_BONUS_NUMBER_MESSAGE);
         }
     }
 
     private static void isValidDuplicateWinningLotto(List<String> splitLotto) {
         Set<String> setLotto = new HashSet<>(splitLotto);
         if (setLotto.size() != splitLotto.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 로또 번호는 중복되면 안됩니다.");
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_MESSAGE);
         }
     }
 
     public static void isValidLottoSize(int splitSize) {
         if (splitSize != LOTTO_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 ,로 구분해야 합니다.");
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_DELIMITER_MESSAGE);
         }
     }
 
     public static void isValidLottoRange(int lottoNumber) {
         if (MIN_LOTTO_NUMBER > lottoNumber || MAX_LOTTO_NUMBER < lottoNumber) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45까지의 정수여야 합니다.");
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_RANGE_MESSAGE);
         }
     }
 
@@ -61,19 +68,19 @@ public class UserInputValidator {
         try {
             Integer.parseInt(userInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력 값은 숫자여야 합니다.");
+            throw new IllegalArgumentException(NON_NUMERIC_INPUT_MESSAGE);
         }
     }
 
     public static void isValidPurchaseRange(int userInput) {
         if (userInput <= 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 양수여야 합니다.");
+            throw new IllegalArgumentException(NEGATIVE_PURCHASE_AMOUNT_MESSAGE);
         }
     }
 
     public static void isValidPurchaseUnit(int userInput) {
         if (userInput % STANDARD_LOTTO_AMOUNT_UNIT != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.");
+            throw new IllegalArgumentException(INVALID_PURCHASE_UNIT_MESSAGE);
         }
     }
 }

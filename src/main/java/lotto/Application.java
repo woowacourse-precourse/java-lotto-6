ca0c74ce33f5ea1ca2842bcfd367bc0cbe784e79
muyaaho <skanthsu715@gmail.com>
2023-11-07 16:03:12 +0900

    refactor(lotto): 의미없는 주석 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,7 +7,6 @@ import lotto.View.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Process process = new Process(new InputView(), new OutputView());
         process.drawLotto(new Exceptable());
 

b5620897a7402d094a91fbbd2c9c9a82ad89e265
Lee9Bin <imagen33@daum.net>
2023-11-08 01:07:16 +0900

    feat : 구매 금액에 따른 로또 생성 기능 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    public List<Lotto> generateLottoGroup(int createLottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        while (lottos.size() < createLottoCount) {
+            lottos.add(generateLotto());
+        }
+
+        return lottos;
+    }
+
+    public Lotto generateLotto() {
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+        return new Lotto(lottoNumbersGenerator.generateLottoNumbers());
+    }
+}

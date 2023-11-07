4edad19d97f112483f7b663dca6c4ce9800f9271
anjdydhody <a01066444534@gmail.com>
2023-11-07 23:18:30 +0900

    feat: Add printWinnings method, toString method in WinningDetails

--- src/main/java/lotto/element/WinningDetails.java
+++ src/main/java/lotto/element/WinningDetails.java
@@ -1,19 +1,22 @@
 package lotto.element;
 
 public enum WinningDetails {
-    THREE(0, 3, 5_000),
-    FOUR(1, 4, 50_000),
-    FIVE(2, 5, 1_500_000),
-    BONUS(3, 5, 3_000_000),
-    SIX(4, 6, 2_000_000_000);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    BONUS( 5, 3_000_000),
+    SIX(6, 2_000_000_000);
 
-    int index;
-    int matchingNumbers;
-    int prize;
+    private final int matchingNumbers;
+    private final int prize;
 
-    WinningDetails(int index, int matchingNumbers, int prize) {
-        this.index = index;
+    WinningDetails(int matchingNumbers, int prize) {
         this.matchingNumbers = matchingNumbers;
         this.prize = prize;
+    }
+
+    @Override
+    public String toString() {
+        return matchingNumbers + "개 일치 (" + prize + ") ";
     }
 }
--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.element.Lotto;
+import lotto.element.WinningDetails;
 import java.util.*;
 
 public class Output {
@@ -8,6 +9,13 @@ public class Output {
         System.out.printf("\n%d개를 구매했습니다.\n", lottoes.size());
         for (Lotto lotto : lottoes) {
             System.out.println(lotto);
+        }
+    }
+
+    void printWinnings(int[] winCount) {
+        for (WinningDetails wd : WinningDetails.values()) {
+            System.out.print(wd);
+            System.out.printf("- %d개\n", winCount[wd.ordinal()]);
         }
     }
 }

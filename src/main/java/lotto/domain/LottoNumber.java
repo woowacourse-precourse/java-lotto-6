8ddf9d12116525124cdd485ee7ba4631353ec371
JongMinCh0i <wonderland314592@gmail.com>
2023-11-07 14:20:07 +0900

    refactor(LottoNumber): 로또 번호에 대한 검증 기능을 LottoNumber 에게 할당

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,10 +1,25 @@
 package lotto.domain;
 
 public class LottoNumber {
-    public static final int LOTTO_SIZE = 6;
+
+    public static final String OUT_OF_LOTTO_VALUE_RANGE = "[ERROR] 로또 입력 값은 1이상 45이하의 값만 가능합니다.";
     public static final int LOTTO_RANGE_IN_START_VALUE = 1;
     public static final int LOTTO_RANGE_IN_END_VALUE = 45;
 
-    private LottoNumber() {
+    private final Integer LottoNumber;
+
+    public LottoNumber(Integer lottoNumber) {
+        validationInRange(lottoNumber);
+        LottoNumber = lottoNumber;
+    }
+
+    private boolean range(Integer number) {
+        return LOTTO_RANGE_IN_START_VALUE <= number && number <= LOTTO_RANGE_IN_END_VALUE;
+    }
+
+    private void validationInRange(Integer number) {
+        if (range(number)) {
+            throw new IllegalArgumentException(OUT_OF_LOTTO_VALUE_RANGE);
+        }
     }
 }

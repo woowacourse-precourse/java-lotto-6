53d3b259029db70cc948340d549b7423ce64c0e1
dabeann <dabin4078@daum.net>
2023-11-07 21:20:00 +0900

    feat: lotto result

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -28,27 +28,57 @@ public class Application {
     // 로또 구매 금액 입력, 당첨 번호 입력, 보너스 번호 입력 등의 메소드 구현
     private static int inputPurchaseAmount() {
         System.out.println("구매 금액을 입력해 주세요.");
-        int purchaseAmount = Integer.parseInt(Console.readLine());
-        if (purchaseAmount <= 0 || purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("유효하지 않은 구매 금액입니다.");
+        try {
+            int purchaseAmount = Integer.parseInt(Console.readLine());
+            if (purchaseAmount <= 0 || purchaseAmount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위의 양수여야 합니다.");
+            }
+            return purchaseAmount;
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자 형식의 데이터를 입력해 주세요.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        return purchaseAmount;
+        // 유효하지 않을 경우 재입력
+        return inputPurchaseAmount();
     }
 
     private static List<Integer> inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String[] winningNumberStrings = Console.readLine().split(",");
-        if (winningNumberStrings.length != 6) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        try {
+            String[] winningNumberStrings = Console.readLine().split(",");
+            if (winningNumberStrings.length != 6) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            }
+            List<Integer> winningNumbers = Arrays.stream(winningNumberStrings)
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            new Lotto(winningNumbers); // 유효성 검사를 위해 Lotto 객체 생성
+            return winningNumbers;
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자 형식의 데이터를 입력해 주세요.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        return Arrays.stream(winningNumberStrings)
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        // 유효하지 않을 경우 재입력
+        return inputWinningNumbers();
     }
 
     private static int inputBonusNumber() {
-        System.out.println("보너스 볼을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine().trim());
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            int bonusNumber = Integer.parseInt(Console.readLine().trim());
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            return bonusNumber;
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자 형식의 데이터를 입력해 주세요.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        // 유효하지 않을 경우 재입력
+        return inputBonusNumber();
     }
 }
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -19,18 +19,60 @@ public class LottoGame {
     }
 
     public void printLottoTickets() {
-        // 구현: 발행한 로또 티켓을 출력합니다.
+        System.out.println(lottoTickets.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottoTickets) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 
     public void checkWinning(List<Integer> winningNumbers, int bonusNumber) {
-        // 구현: 각 로또 티켓 별로 당첨 확인을 합니다.
+        for (Lotto lotto : lottoTickets) {
+            int matchCount = 0;
+            boolean bonusMatch = false;
+            for (int number : lotto.getNumbers()) {
+                if (winningNumbers.contains(number)) {
+                    matchCount++;
+                } else if (number == bonusNumber) {
+                    bonusMatch = true;
+                }
+            }
+            LottoRank rank = getRank(matchCount, bonusMatch);
+            statistics.recordWinningRank(rank);
+        }
+    }
+
+    private LottoRank getRank(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6) return LottoRank.FIRST;
+        if (matchCount == 5 && bonusMatch) return LottoRank.SECOND;
+        if (matchCount == 5) return LottoRank.THIRD;
+        if (matchCount == 4) return LottoRank.FOURTH;
+        if (matchCount == 3) return LottoRank.FIFTH;
+        return LottoRank.NONE;
     }
 
     public void printStatistics() {
-        // 구현: 당첨 통계를 출력합니다.
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        // LottoRank 열거형의 값을 역순으로 가져오기 위해 배열을 뒤집습니다.
+        LottoRank[] ranks = LottoRank.values();
+        for (int i = ranks.length - 1; i >= 0; i--) {
+            LottoRank rank = ranks[i];
+            if (rank != LottoRank.NONE) { // NONE은 제외하고 출력
+                // 보너스 볼 일치 여부에 따른 문자열 조합
+                String bonusMatchString = (rank == LottoRank.SECOND) ? ", 보너스 볼 일치" : "";
+                // 숫자에 콤마를 포함시켜 문자열 형식화
+                String prizeString = String.format("%,d", rank.getPrize());
+                int count = statistics.getCount(rank);
+                System.out.println(rank.getMatchCount() + "개 일치" + bonusMatchString + " (" + prizeString + "원) - " + count + "개");
+            }
+        }
     }
 
+
+
     public void calculateProfitRate(int purchaseAmount) {
-        // 구현: 수익률을 계산하고 출력합니다.
+        long totalPrize = statistics.getTotalPrize();
+        double profitRate = ((double) totalPrize / purchaseAmount) * 100;
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }

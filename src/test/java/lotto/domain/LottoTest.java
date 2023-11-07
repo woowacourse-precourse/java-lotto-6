97da5f28826b98c761d1dde02d199febc5e340ef
sinyebin <dpqls0625@naver.com>
2023-11-08 01:38:59 +0900

    test: 발행한 로또번호 출력

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -23,4 +23,12 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호 범위넘어가면 에러가 발생한다.")
+    @Test
+    void 범위넘어가면_에러발생() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
--- src/test/java/lotto/service/InputLottoAmountTest.java
+++ src/test/java/lotto/service/InputLottoAmountTest.java
@@ -66,4 +66,5 @@ class InputLottoAmountTest {
             InputLottoAmount.checkTicket(amount);
         }).doesNotThrowAnyException();
     }
+    
 }

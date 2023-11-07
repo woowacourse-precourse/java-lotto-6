3eedd7bd55db61a2e8a48798acf0d4db4a732c43
SSUHYUNKIM <yellosuhyun@naver.com>
2023-11-07 15:58:32 +0900

    feat: 게임 시작 및 로또 구입 금액을 입력 받기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,19 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoController {
+
+    private final InputView inputView;
+
+    public LottoController(InputView inputView) {
+        this.inputView = inputView;
+    }
+    public void run() {
+        purchaseLotto();
+    }
+
+    private void purchaseLotto() {
+        String money = inputView.getLottoPurchaseAmount();
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,12 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.message.InputMessage;
+
 public class InputView {
+    public String getLottoPurchaseAmount() {
+        System.out.println(InputMessage.INPUT_PURCHASE_AMOUNT.getMessage());
+
+        return Console.readLine();
+    }
 }

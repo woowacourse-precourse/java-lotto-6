a2434855158f5b31131e00aff7897d242f53c1bb
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-11-07 20:48:17 +0900

    fix: 출력 형식에 맞게 message수정

--- src/main/java/lotto/constant/OutputMessage.java
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -1,8 +1,8 @@
 package lotto.constant;
 
 public enum OutputMessage {
-    NUMBER_OF_LOTTO_BOUGHT("%d개를 구매하셨습니다."),
-    PROFIT_RATE("총 수익률은 %.1f%입니다.");
+    NUMBER_OF_LOTTO_BOUGHT("%d개를 구매했습니다."),
+    PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 

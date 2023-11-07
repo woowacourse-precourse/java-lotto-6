b284c7f1255f864500b4d2dc1f699ad54cbbe315
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:20:38 +0900

    feat(InputView): 금액 입력 함수 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private InputView() {
+    }
+
+    public static String inputMoney() {
+        return Console.readLine();
+    }
+}

96c88c0aa92d9a09f6babc2318aaec844a459040
honee8583 <honee8583@naver.com>
2023-11-07 22:08:44 +0900

    feat(error): add error message
    add error message NUMBERS_SIZE_IS_NOT_6 to use when lotto numbers size is not 6.

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto;
 public enum ErrorMessage {
     NUMBER_OVER_LIMIT("[ERROR] 숫자는 45를 넘기면 안됩니다."),
     PURCHASE_NOT_DIVIDE_BY_1000("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다."),
-    CONTAINS_NON_NUMERIC_CHARACTERS("[ERROR] 숫자만을 입력해야 합니다.");
+    CONTAINS_NON_NUMERIC_CHARACTERS("[ERROR] 숫자만을 입력해야 합니다."),
+    NUMBERS_SIZE_IS_NOT_6("[ERROR] 로또는 6개의 숫자로 이루어저야 합니다.");
 
     private String message;
 

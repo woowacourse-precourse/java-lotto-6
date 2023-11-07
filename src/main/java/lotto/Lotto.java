4fbe505b4861eead7c8983b2e032f02d5a2088b6
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-07 20:37:24 +0900

    refactor: 유효성 검사 CheckValid로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,17 +11,12 @@ public class Lotto {
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
-		validate(numbers);
+		
+		CheckValid.numbers(numbers);
+		
 		this.numbers = numbers;
 	}
-
-	private void validate(List<Integer> numbers) {
-
-		if (numbers.size() != 6) {
-			throw new IllegalArgumentException();
-		}
-	}
-
+	
 	private List<Integer> getNumbers() {
 		return numbers;
 	}

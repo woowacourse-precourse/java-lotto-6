c285ffa6f789029db4b1d4f64ca2065185262974
dj <fjzl12@naver.com>
2023-11-08 02:04:18 +0900

    feat(OutputView): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -4,6 +4,9 @@ import lotto.Lotto;
 
 import java.util.List;
 
+import lotto.domain.Constants;
+import lotto.domain.ResultMessage;
+
 import static lotto.domain.Message.PURCHASE_COMMENT;
 
 public class OutputView {
@@ -15,4 +18,11 @@ public class OutputView {
         System.out.println();
     }
 
+    public static void printStatistics(List<Integer> prizes, float profitPercent) {
+        System.out.println(Constants.RESULT);
+        for (ResultMessage result : ResultMessage.values()) {
+            System.out.printf(result.getMessage(), prizes.get(result.ordinal()));
+        }
+        System.out.printf(Constants.PROFIT, profitPercent);
+    }
 }

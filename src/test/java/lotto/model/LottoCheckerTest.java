7ef04fa9d2e32c0fd422741bdf93f4abce188f60
jihyuk <kwlgur0416@gmail.com>
2023-11-07 18:06:17 +0900

    test : LottoRank의 LESS_THREE_MATCH 반환 테스트

--- src/test/java/lotto/model/LottoCheckerTest.java
+++ src/test/java/lotto/model/LottoCheckerTest.java
@@ -3,7 +3,10 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoCheckerTest {
 
@@ -14,8 +17,26 @@ class LottoCheckerTest {
 
         final LottoRank lottoRank = lottoChecker.checkTicket(ticket);
 
-        assertThat(lottoRank.getOutputString(1))
-            .contains("5개 일치, 보너스 볼 일치 (30,000,000원) - 1개");
+        assertThat(lottoRank).isEqualTo(LottoRank.FIVE_MATCH_WITH_BONUS);
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateTicketNumbers")
+    void 맞춘_개수가_3개보다_작다면_LESS_THREE_MATCH를_반환한다(List<Integer> ticketNumbers) {
+        final LottoChecker lottoChecker = new LottoChecker(List.of(1, 2, 3, 4, 5, 6), 7);
+        final Lotto ticket = new Lotto(ticketNumbers);
+
+        final LottoRank lottoRank = lottoChecker.checkTicket(ticket);
+
+        assertThat(lottoRank).isEqualTo(LottoRank.LESS_THREE_MATCH);
+    }
+
+    private static Stream<List<Integer>> generateTicketNumbers() {
+        return Stream.of(
+            List.of(7, 8, 9, 10, 11, 12),
+            List.of(1, 8, 9, 10, 11, 12),
+            List.of(1, 2, 9, 10, 11, 12)
+        );
     }
 
 }

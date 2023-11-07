6aa5cc93b8c6338bb1a410011ac9b28785342557
zzawang <hobin0116@naver.com>
2023-11-07 15:46:43 +0900

    refactor: 상수 이름 수정
    - 더 올바른 의미로 상수 이름을 수정하였다.

--- src/main/java/view/InputException.java
+++ src/main/java/view/InputException.java
@@ -10,10 +10,10 @@ public class InputException {
     private static final int PURCHASE_AMOUNT_UNIT = 1000;
     private static final int ZERO = 0;
     private static final String IS_VALID_AMOUNT_MESSAGE = "로또 구입 금액은 1,000원 단위로 입력해야 합니다.";
-    private static final String IS_NUMBER_IN_RANGE_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String NOT_NUMBER_IN_RANGE_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final int WINNING_NUMBER_LENGTH = 6;
-    private static final String HAS_SIX_NUMBERS_MESSAGE = "당첨 번호는 6개를 입력해야 합니다.";
-    private static final String HAS_NO_DUPLICATE_NUMBERS_MESSAGE = "번호 중 중복되는 번호가 있습니다.";
+    private static final String NOT_SIX_NUMBERS_MESSAGE = "6개의 번호를 입력해야 합니다.";
+    private static final String HAS_DUPLICATE_NUMBERS_MESSAGE = "번호 중 중복되는 번호가 있습니다.";
     private static final String IS_IN_WINNING_NUMBERS_MESSAGE = "보너스 번호는 당첨 번호와 다른 수 이어야 합니다.";
 
     public static void canBeConvertedToInteger(String userInput) {
@@ -34,13 +34,13 @@ public class InputException {
     public static void isNumberInRange(String userInput) {
         int winningNum = Integer.parseInt(userInput);
         if (winningNum < 1 || winningNum > 45) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + IS_NUMBER_IN_RANGE_MESSAGE);
+            throw new IllegalArgumentException(ERROR_MESSAGE + NOT_NUMBER_IN_RANGE_MESSAGE);
         }
     }
 
     public static void hasSixNumbers(String[] userInputs) {
         if (userInputs.length != WINNING_NUMBER_LENGTH) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + HAS_SIX_NUMBERS_MESSAGE);
+            throw new IllegalArgumentException(ERROR_MESSAGE + NOT_SIX_NUMBERS_MESSAGE);
         }
     }
 
@@ -51,7 +51,7 @@ public class InputException {
                 .filter(winningNum -> !winningNums.add(winningNum))
                 .findAny()
                 .ifPresent(pNum -> {
-                    throw new IllegalArgumentException(ERROR_MESSAGE + HAS_NO_DUPLICATE_NUMBERS_MESSAGE);
+                    throw new IllegalArgumentException(ERROR_MESSAGE + HAS_DUPLICATE_NUMBERS_MESSAGE);
                 });
     }
 

ddeaa129f525207902b32c099db9b19df6820619
ikjo39 <chi4321@naver.com>
2023-11-08 00:04:27 +0900

    test: LottoRanking 테스트 추가
    - LottoRanking 값들을 키, 0을 값으로 갖는 map이 생성

--- src/test/java/lotto/constant/LottoRankingTest.java
+++ src/test/java/lotto/constant/LottoRankingTest.java
@@ -5,9 +5,10 @@ import static lotto.constant.LottoRanking.FIRST;
 import static lotto.constant.LottoRanking.FOURTH;
 import static lotto.constant.LottoRanking.SECOND;
 import static lotto.constant.LottoRanking.THIRD;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
 import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,22 +17,43 @@ import org.junit.jupiter.params.provider.MethodSource;
 class LottoRankingTest {
 
     @DisplayName("정답 횟수와 보너스번호일치 여부를 이용해서 로또 순위를 판별한다.")
-    @MethodSource("generateData")
+    @MethodSource("generateLottoAndRanking")
     @ParameterizedTest
     void convertLottoIntoLottoRanking(int count, boolean isBonusNumberCorrect, LottoRanking expected) {
         // when
         LottoRanking result = LottoRanking.convertLottoIntoLottoRanking(count, isBonusNumberCorrect);
         // then
-        Assertions.assertThat(result).isEqualTo(expected);
+        assertThat(result).isEqualTo(expected);
     }
 
-    static Stream<Arguments> generateData() {
+    static Stream<Arguments> generateLottoAndRanking() {
         return Stream.of(
                 Arguments.of(6, false, FIRST),
                 Arguments.of(5, true, SECOND),
                 Arguments.of(5, false, THIRD),
                 Arguments.of(4, false, FOURTH),
                 Arguments.of(3, false, FIFTH)
+        );
+    }
+
+    @DisplayName("로또 순위들을 키, 0을 value로 갖는 map이 생성된다.")
+    @MethodSource("generateKeyAndValue")
+    @ParameterizedTest
+    void convertValuesIntoCounters(LottoRanking key, int value) {
+        // when
+        Map<LottoRanking, Integer> result = LottoRanking.convertValuesIntoCounters();
+
+        // then
+        assertThat(result).containsEntry(key, value);
+    }
+
+    static Stream<Arguments> generateKeyAndValue() {
+        return Stream.of(
+                Arguments.of(FIRST, 0),
+                Arguments.of(SECOND, 0),
+                Arguments.of(THIRD, 0),
+                Arguments.of(FOURTH, 0),
+                Arguments.of(FIFTH, 0)
         );
     }
 }

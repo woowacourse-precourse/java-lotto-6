8b8bd0d94c491416f314d66122d84968b4df94ab
isakdevelop <daishi7462@naver.com>
2023-11-08 03:46:24 +0900

    feat: Enum 클래스를 활용한 로또 당첨 정보

--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -1,35 +1,51 @@
 package lotto;
 
-public enum LottoRank {
-    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "), // 1등
-    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "), // 2등
-    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - "), // 3등
-    FOURTH(4, 50_000, "4개 일치 (50,000원) - "), // 4등
-    FIFTH(3, 5_000, "3개 일치 (5,000원) - "), // 5등
-    MISS(0, 0, ""); // 꽝
+public enum WinningRank {
+    NON(0, 0, ""),
+    FIFTH(3, 5000, "3개 일치 (5,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
-    private final int matchCount;
-    private final int prize;
-    private final String description;
+
+    private int matchCount;
+    private int prize;
+    private String description;
+    private int bonusPrize;
 
     WinningRank(int matchCount, int prize, String description) {
         this.matchCount = matchCount;
         this.prize = prize;
         this.description = description;
+        this.bonusPrize = prize;
+    }
+
+    public int calculatePrize(int count, boolean isBonusNumberMatch) {
+        if (matchCount == 5 && count == 5) {
+            if (isBonusNumberMatch) {
+                return bonusPrize;
+            }
+        }
+        return prize * count;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public int calculatePrize(int count) {
-        if (matchCount == 5) {
-            if (count == 5) {
-                return prize;
-            } else if (count == 6) {
-                return 0; // 보너스 번호는 2등에 해당하므로, 6개 일치 시 당첨금 없음
+    public static WinningRank getWinningRank(int commonNumbersCount, boolean isBonusNumberMatch) {
+        for (WinningRank rank : values()) {
+            if (rank.matchCount == commonNumbersCount) {
+                if (rank.matchCount == 5) {
+                    if (isBonusNumberMatch && rank == SECOND) {
+                        return rank;
+                    }
+                } else {
+                    return rank;
+                }
             }
         }
-        return prize * count;
+        return NON;
     }
 }

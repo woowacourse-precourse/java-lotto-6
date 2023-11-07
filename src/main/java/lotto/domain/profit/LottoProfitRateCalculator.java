a2b4629b684d1f7edb4fb64f70796efd66db7532
haero77 <shineecard5@gmail.com>
2023-11-08 02:22:29 +0900

    fix: 반올림 기준에 맞춰 수익률을 계산하도록 수정

--- src/main/java/lotto/domain/profit/LottoProfitRateCalculator.java
+++ src/main/java/lotto/domain/profit/LottoProfitRateCalculator.java
@@ -3,18 +3,19 @@ package lotto.domain.profit;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import lotto.domain.LottoPurchaseAmount;
-import lotto.domain.result.LottoGameResult;
 import lotto.domain.result.TotalPrizeAmount;
 
 public class LottoProfitRateCalculator {
 
-    public LottoProfitRate calculate(LottoPurchaseAmount purchaseAmount, LottoGameResult gameResult) {
-        return new LottoProfitRate(applyProfitRateFormula(gameResult.calculateTotalPrizeAmount(), purchaseAmount));
+    public static final int PROFIT_RATE_SCALE = 3;
+
+    public LottoProfitRate calculate(LottoPurchaseAmount purchaseAmount, TotalPrizeAmount totalPrizeAmount) {
+        return new LottoProfitRate(applyProfitRateFormula(totalPrizeAmount, purchaseAmount));
     }
 
     private BigDecimal applyProfitRateFormula(TotalPrizeAmount totalPrizeAmount, LottoPurchaseAmount purchaseAmount) {
         return totalPrizeAmount.getAmount()
-                .divide(purchaseAmount.getAmount(), RoundingMode.HALF_UP)
+                .divide(purchaseAmount.getAmount(), PROFIT_RATE_SCALE, RoundingMode.HALF_UP)
                 .multiply(BigDecimal.valueOf(100L));
     }
 
--- src/test/java/lotto/domain/profit/LottoProfitRateCalculatorTest.java
+++ src/test/java/lotto/domain/profit/LottoProfitRateCalculatorTest.java
@@ -3,37 +3,27 @@ package lotto.domain.profit;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
-import java.util.Map;
 import lotto.domain.LottoPurchaseAmount;
-import lotto.domain.prize.LottoPrizeType;
-import lotto.domain.result.LottoGameResult;
+import lotto.domain.result.TotalPrizeAmount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoProfitRateCalculatorTest {
 
-    @DisplayName("구입 금액과 당첨 내역을 바탕으로 수익률을 계산할 수 있다.")
+    @DisplayName("구입 금액과 당첨 금액을 바탕으로 수익금을 계산할 수 있다.")
     @Test
     void calculate() {
         // given
-        LottoPurchaseAmount purchaseAmount = new LottoPurchaseAmount(BigDecimal.valueOf(5000L));
-        LottoGameResult gameResult = new LottoGameResult(
-                Map.of(
-                        LottoPrizeType.FIRST, 0,
-                        LottoPrizeType.SECOND, 0,
-                        LottoPrizeType.THIRD, 0,
-                        LottoPrizeType.FOURTH, 1,
-                        LottoPrizeType.FIFTH, 2
-                )
-        );
-
         LottoProfitRateCalculator calculator = new LottoProfitRateCalculator();
+        LottoPurchaseAmount purchaseAmount = new LottoPurchaseAmount(new BigDecimal(8000));
+        TotalPrizeAmount prizeAmount = new TotalPrizeAmount(new BigDecimal(5000));
 
         // when
-        LottoProfitRate result = calculator.calculate(purchaseAmount, gameResult);
+        LottoProfitRate profitRate = calculator.calculate(purchaseAmount, prizeAmount);
 
         // then
-        assertThat(result.getRate()).isEqualTo(BigDecimal.valueOf(1200L));
+        assertThat(profitRate.getRate().doubleValue())
+                .isEqualTo(62.5);
     }
 
 }

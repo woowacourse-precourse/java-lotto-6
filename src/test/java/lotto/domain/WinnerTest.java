f005f67a77b9dd1e30a0734eb26e9d6169bc9a53
tam31 <wxogud3377@naver.com>
2023-11-07 15:44:01 +0900

    Test(WinnerTest) : 로또 당첨번호와 사용자의 로또 번호 중 동일한 개수 테스트

--- src/test/java/lotto/domain/WinnerTest.java
+++ src/test/java/lotto/domain/WinnerTest.java
@@ -37,4 +37,40 @@ class WinnerTest {
         //then 결과값 비교
         Assertions.assertThat(check).isTrue();
     }
+
+    @DisplayName("로또 당첨번호와 사용자의 로또 번호 중 동일한 개수 구하기")
+    @Test
+    public void 당첨번호_사용자번호_동일한_개수() throws Exception {
+        //given (주어진 값)
+        Lotto sameUserLottoNumberSix = new Lotto(new ArrayList<>(List.of(1, 6, 8, 25, 33, 45)));
+        Lotto sameUserLottoNumberFiveAndBonus = new Lotto(new ArrayList<>(List.of(1, 6, 8, 25, 33, 5)));
+        Lotto sameUserLottoNumberFive = new Lotto(new ArrayList<>(List.of(1, 6, 8, 25, 33, 44)));
+        Lotto sameUserLottoNumberFour = new Lotto(new ArrayList<>(List.of(1, 6, 8, 25, 31, 43)));
+        Lotto sameUserLottoNumberThree = new Lotto(new ArrayList<>(List.of(1, 6, 8, 18, 22, 40)));
+        Lotto sameUserLottoNumberTwo = new Lotto(new ArrayList<>(List.of(1, 6, 7, 16, 21, 32)));
+        Lotto sameUserLottoNumberOne = new Lotto(new ArrayList<>(List.of(1, 2, 3, 5, 11, 42)));
+        Lotto sameUserLottoNumberZero = new Lotto(new ArrayList<>(List.of(2, 3, 4, 5, 11, 42)));
+
+        //when (기능 작동)
+        Method method = winner.getClass().getDeclaredMethod("checkUserLottoNumberCount", Lotto.class);
+        method.setAccessible(true);
+        int six = (Integer) method.invoke(winner, sameUserLottoNumberSix);
+        int fiveAndBonus = (Integer) method.invoke(winner, sameUserLottoNumberFiveAndBonus);
+        int five = (Integer) method.invoke(winner, sameUserLottoNumberFive);
+        int four = (Integer) method.invoke(winner, sameUserLottoNumberFour);
+        int three = (Integer) method.invoke(winner, sameUserLottoNumberThree);
+        int two = (Integer) method.invoke(winner, sameUserLottoNumberTwo);
+        int one = (Integer) method.invoke(winner, sameUserLottoNumberOne);
+        int zero = (Integer) method.invoke(winner, sameUserLottoNumberZero);
+
+        //then (기능 작동 후 결과)
+        Assertions.assertThat(six).isEqualTo(6);
+        Assertions.assertThat(fiveAndBonus).isEqualTo(50);
+        Assertions.assertThat(five).isEqualTo(5);
+        Assertions.assertThat(four).isEqualTo(4);
+        Assertions.assertThat(three).isEqualTo(3);
+        Assertions.assertThat(two).isEqualTo(2);
+        Assertions.assertThat(one).isEqualTo(1);
+        Assertions.assertThat(zero).isEqualTo(0);
+    }
 }

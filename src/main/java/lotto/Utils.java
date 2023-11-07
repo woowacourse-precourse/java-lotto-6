bf5cf3a1450f07df0978b150276e7314960e5a79
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 18:52:21 +0900

    feat(Utils) : Formatting 기능 추가

--- src/main/java/lotto/Utils.java
+++ src/main/java/lotto/Utils.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class Utils {
@@ -11,6 +12,12 @@ public class Utils {
 
     public static float calculateYield(float after, float before) {
         return (after / before) * 100;
+    }
+
+    public static String makeFloatFormatted(float value, int precision)  {
+        DecimalFormat formatter = new DecimalFormat("###,##0.0");
+        String rounded = String.format("%." + precision + "f", value);
+        return formatter.format(Float.parseFloat(rounded));
     }
 
 }

35675092bdcfb8b36ba0c53b8d95f11ae64f50e9
fly-like-you <qkrwnsgh71w@gmail.com>
2023-11-07 17:18:57 +0900

    feat(validation): 추가 로또를 살 가격 입력 받기 기능
    로또 구매에 입력받은 금액에 대한 검증 조건을 추가하였습니다.
    가격의 검증 조건
    - 숫자 이외의 값이 올 수 없다(NumberCondition)
    - 로또 가격으로 나누어 떨어지는 경우는 없다(MoneyDivisionCondition)

---
+++ src/main/java/lotto/validation/money/MoneyDivisionCondition.java
@@ -0,0 +1,21 @@
+package lotto.validation.money;
+
+import lotto.domain.Money;
+import lotto.validation.ValidationCondition;
+
+public class MoneyDivisionCondition extends ValidationCondition {
+
+    private Money lottoPrice;
+
+    public MoneyDivisionCondition(Money lottoPrice) {
+        this.lottoPrice = lottoPrice;
+    }
+
+    @Override
+    public void isSatisfiedBy(String money) {
+        Money wons = Money.wons(Integer.parseInt(money));
+        if (!wons.calculateChange(lottoPrice).equals(Money.ZERO)) {
+            throw new IllegalArgumentException("잔돈이 남습니다.");
+        }
+    }
+}

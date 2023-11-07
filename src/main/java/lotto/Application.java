ff1a27e55e44f3022dc390b663d2b1e460aea6fd
Junho Choi <tig9659@ajou.ac.kr>
2023-11-08 03:43:17 +0900

    feat: 로또 구입 금액, 당첨 번호, 보너스 번호 설정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,108 @@
 package lotto;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static int amount;
+    private static int bonusNum;
+    private static Lotto answer;
 
     public static void main(String[] args) {
+        getAmount();
+
+        getAnswer();
+
+        getBonusNum();
+
         printResult();
     }
 
-    public static void printResult() {
+    private static void printResult(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
         for (WinningResult result : WinningResult.values()) {
             System.out.println(result.getPhrase() + " - " + result.getCount() + "개");
         }
     }
 
+    private static List<Integer> generateRandomNumbers(){
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < LOTTO_NUMBER_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER);
+            numbers.add(randomNumber);
+        }
+        return new ArrayList<>(numbers);
+    }
+
+    private static void getAmount(){
+        while (true) {
+            try{
+                System.out.println("구입금액을 입력해 주세요.");
+                String amountInput = Console.readLine();
+                validateAmount(amountInput);
+                break;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void validateAmount(String amountInput) {
+        if (!amountInput.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+        amount = Integer.parseInt(amountInput);
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    private static void getBonusNum(){
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String bonusNumInput = Console.readLine();
+                validateBonusNum(bonusNumInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void validateBonusNum(String bonusNumInput) {
+        if (!bonusNumInput.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+        bonusNum = Integer.parseInt(bonusNumInput);
+        if (bonusNum < LOTTO_START_NUMBER || bonusNum > LOTTO_END_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 정수로 입력해야 합니다.");
+        }
+    }
+
+    private static void getAnswer(){
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String answerInput = Console.readLine();
+                List<Integer> answerNums = Arrays.stream(answerInput.split(","))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                answer = new Lotto(answerNums);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            }catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,14 +7,20 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        isDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void isDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호를 입력할 수 없습니다.");
+        }
+    }
 }

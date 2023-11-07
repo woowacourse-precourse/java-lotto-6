3110c1203b1c23883cea05e0a08738e154dcff2c
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 21:39:00 +0900

    refactor : 로또번호 세팅 기능 수정

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -2,18 +2,20 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static lotto.view.Constants.*;
 
 public class LottoNumbers {
-
+    private static List<Integer> lottoNumberList;
 
     public List<Integer> setLottoNumbers() {
-        List<Integer> LottoNums = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, NUM_OF_LOTTO);
-        Collections.sort(LottoNums);
-        return LottoNums;
+        lottoNumberList = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, NUM_OF_LOTTO);
+        List<Integer> lottoNums = new ArrayList<>(lottoNumberList);
+        Collections.sort(lottoNums);
+        return lottoNums;
     }
 
 }

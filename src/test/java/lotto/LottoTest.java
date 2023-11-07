dd12f322940096d1b65f3d414f54f29fc2321d7a
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 21:35:41 +0900

    test: 로또 번호를 담당하는 Lotto 도메인의 구현 함수들이 정상 동작하는지 테스트 성공

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,14 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static lotto.constant.ErrorMessage.*;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @Test
@@ -28,8 +29,11 @@ class LottoTest {
 
     @Test
     @DisplayName("범위 밖의 숫자가 로또 번호에 있으면 예외 발생")
-    void createLottoNotInRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(-1, 0, 1, 2, 45, 46)))
+    void createLottoOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NOT_IN_RANGE);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(LOTTO_NOT_IN_RANGE);
     }
@@ -40,5 +44,28 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(6, 5, 4, 3, 2, 1)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(LOTTO_NOT_ASCENDING);
+    }
+
+    @Test
+    @DisplayName("티켓에 로또 번호가 존재 하는지")
+    void hasNumber() {
+        Lotto ticket = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(ticket.hasNumber(new LottoNumber(4))).isEqualTo(true);
+        assertThat(ticket.hasNumber(new LottoNumber(7))).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 자료형 변환이 되는지")
+    void from() {
+        assertThatCode(() -> Lotto.from("1,2,3,4,5,6")).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("로또 번호들이 정상 출력 되는지")
+    void stringify() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThat(new Lotto(numbers).toString())
+                .contains(numbers.stream().map(String::valueOf).toList())
+                .contains("[", "]");
     }
 }

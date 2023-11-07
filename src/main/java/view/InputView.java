110d16963a024c949b61e8af232231de11bbf000
eora21 <eora21@naver.com>
2023-11-08 08:47:48 +0900

    feat: 🎸 (InputView) 추가 입력을 위한 인터페이스 작성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.List;
+
+public interface InputView {
+    int enterPurchaseAmount();
+
+    List<Integer> enterWinningLottoNumbers();
+
+    int enterBonusLottoNumber();
+}

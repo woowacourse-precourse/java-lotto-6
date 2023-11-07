2ddb9c3f21ca5a29e84699136779815d2890801f
zeomzzz <jmlee2705@gmail.com>
2023-11-08 06:03:48 +0900

    feat: 로또 하나의 결과를 반환하는 기능 구현

--- src/main/java/lotto/domain/LottoGameResult.java
+++ src/main/java/lotto/domain/LottoGameResult.java
@@ -1,9 +1,23 @@
 package lotto.domain;
 
+import lotto.enums.LottoRankEnum;
+import lotto.utils.Comparer;
+
 public class LottoGameResult {
 
-//    private int countSameNumber(Lotto lotto, Winning winning) {
-//        compare(lotto, winning.getWinningLotto());
-//    }
+    public LottoRankEnum calculateLottoResult(Lotto lotto, Winning winning) {
+        int sameCount = Comparer.compareLottoToWinning(lotto, winning);
+        boolean containsBonus = Comparer.containsBonus(lotto, winning);
+
+        for (LottoRankEnum lottoRank : LottoRankEnum.values()) {
+            if (lottoRank.getSameCount() == sameCount) {
+                if (lottoRank.isContainsBonus() == containsBonus) {
+                    return lottoRank;
+                }
+                return lottoRank;
+            }
+        }
+        return LottoRankEnum.NOTHING;
+    }
 
 }

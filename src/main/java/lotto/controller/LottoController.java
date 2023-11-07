7afa6b6160be2b6a32b6e4797768319c334ec249
jhva <junghun8158@naver.com>
2023-11-07 13:33:52 +0900

    :recycle: refactor: 보너스 번호 중복체크 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import static lotto.constants.LottoConstants.BONUS;
+import static lotto.constants.LottoConstants.WINNING;
 import static lotto.utils.LottoUtils.divisionLottoPrice;
 import static lotto.utils.LottoUtils.generateRandomNumbers;
 
@@ -7,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lotto.dto.LottoDto;
 import lotto.model.Lotto;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutView;
 
@@ -14,16 +17,26 @@ public class LottoController {
 
     private final InputView inputView;
     private final OutView outView;
+    private final LottoService lottoService;
 
     public LottoController() {
         this.inputView = new InputView();
         this.outView = new OutView();
+        this.lottoService = new LottoService();
     }
 
     public void run() {
         int priceLotto = inputView.parseInputFromUserInteger();
         int divisionLottoPrice = divisionLottoPrice(priceLotto);
         List<LottoDto> generateLottoNumbersDto = createLottoGenerate(divisionLottoPrice);
+        outView.lottoCommonNumberView(WINNING);
+        List<Integer> lottoWinningNumbers = lottoService.createWinningNumbers(inputView.commonInput());
+        outView.lottoCommonNumberView(BONUS);
+        int lottoBonusNumber = lottoService.createBonusNumber(generateLottoNumbersDto, inputView.commonInput());
+        lottoService.findWinners(lottoWinningNumbers, lottoBonusNumber, generateLottoNumbersDto);
+
+        outView.winnersMsg(generateLottoNumbersDto, lottoWinningNumbers, lottoBonusNumber);
+
     }
 
     public List<LottoDto> createLottoGenerate(int divisionLottoPrice) {
@@ -32,7 +45,7 @@ public class LottoController {
         outView.lottoPriceView(divisionLottoPrice);
         for (int i = 0; i < divisionLottoPrice; i++) {
             Lotto lotto = new Lotto(generateRandomNumbers());
-            lottoDtos.add(new LottoDto(lotto.getLotto()));
+            lottoDtos.add(new LottoDto(lotto.getLotto(), lotto));
             outView.randomLottoOutView(lotto.getLotto());
         }
         return lottoDtos;
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import lotto.dto.LottoDto;
 import lotto.exception.ErrorMsg;
 import lotto.exception.UserInputException;
 
@@ -26,7 +27,21 @@ public class LottoService {
         }
     }
 
-    public int createBonusNumber(String commonInput) {
+    public int createBonusNumber(List<LottoDto> lottoDtos, String commonInput) {
+        for (LottoDto lottoDto : lottoDtos) {
+            List<Integer> lottoNumbers = lottoDto.lottoRandomNumbers();
+            validateBonusNumberCheck(lottoNumbers, commonInput);
+        }
         return Integer.parseInt(commonInput);
+    }
+
+    public void validateBonusNumberCheck(List<Integer> lottoNumbers, String commonInput) {
+        if (lottoNumbers.contains(commonInput)) {
+            throw new UserInputException(ErrorMsg.ERROR_LOTTO_DUPLICATES_NUMBERS.getMsg());
+        }
+    }
+
+    public void findWinners(List<Integer> lottoWinningNumbers, int lottoBonusNumber,
+            List<LottoDto> generateLottoNumbersDto) {
     }
 }

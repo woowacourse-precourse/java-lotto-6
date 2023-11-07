ad545299ed7d2fbf6fbe510c1bd190244fb35795
k-kmw <kimmw2_@naver.com>
2023-11-08 05:59:48 +0900

    test: lotto 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -18,10 +20,44 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호와 결과를 비교하여 요구 사항에 맞게 반환한다.")
+    @Test
+    void compareLottoNumWithResultNum() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Result sixExpected = lotto.match(List.of(1, 2, 3, 4, 5, 6), 7);
+        assertThat(sixExpected).isEqualTo(Result.SIX);
+
+        Result fiveBonusExpected = lotto.match(List.of(1, 2, 3, 4, 5, 13), 6);
+        assertThat(fiveBonusExpected).isEqualTo(Result.FIVE_BONUS);
+
+        Result fiveExpected = lotto.match(List.of(1, 2, 3, 4, 5, 13), 7);
+        assertThat(fiveExpected).isEqualTo(Result.FIVE);
+
+        Result fourExpected = lotto.match(List.of(1, 2, 3, 4, 9, 13), 6);
+        assertThat(fourExpected).isEqualTo(Result.FOUR);
+
+        Result threeExpected = lotto.match(List.of(1, 2, 3, 8, 9, 13), 6);
+        assertThat(threeExpected).isEqualTo(Result.THREE);
+
+        Result loseExpected2 = lotto.match(List.of(1, 2, 7, 8, 9, 13), 6);
+        assertThat(loseExpected2).isEqualTo(Result.LOSE);
+
+        Result loseExpected1 = lotto.match(List.of(1, 40, 7, 8, 9, 13), 6);
+        assertThat(loseExpected1).isEqualTo(Result.LOSE);
+
+        Result loseExpected0 = lotto.match(List.of(41, 40, 7, 8, 9, 13), 6);
+        assertThat(loseExpected0).isEqualTo(Result.LOSE);
+    }
 }

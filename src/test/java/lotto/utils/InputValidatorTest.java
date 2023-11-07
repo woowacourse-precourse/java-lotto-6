85b013cc258b221db598fd92bb492f018c6252f4
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 02:15:10 +0900

    test(InputValidatorTest): exception 메시지 출력 테스트

--- src/test/java/lotto/utils/InputValidatorTest.java
+++ src/test/java/lotto/utils/InputValidatorTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static lotto.constant.ExceptionMessage.INVALID_BLANK_EXCEPTION;
+import static lotto.constant.ExceptionMessage.INVALID_INPUT_NUMBER_EXCEPTION;
+import static lotto.constant.ExceptionMessage.INVALID_NUMBER_EXCEPTION;
+import static lotto.constant.Global.DELIMITER;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
@@ -13,7 +17,7 @@ public class InputValidatorTest {
     void wrongInputMoney(String money) {
         assertThatThrownBy(() -> InputValidator.validateInputMoney(money))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자만 입력 가능합니다.");
+                .hasMessageContaining(INVALID_NUMBER_EXCEPTION.getMessage());
     }
 
     @DisplayName("당첨 번호에 공백이 있다면 예외가 발생한다.")
@@ -26,7 +30,7 @@ public class InputValidatorTest {
         // when, then
         assertThatThrownBy(() -> InputValidator.validateInputWinningNumber(inputWinningNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("공백은 입력될 수 없습니다.");
+                .hasMessageContaining(INVALID_BLANK_EXCEPTION.getMessage());
     }
 
     @DisplayName("당첨 번호에 숫자가 아닌 다른 게 있다면 예외가 발생한다.")
@@ -34,12 +38,12 @@ public class InputValidatorTest {
     @ValueSource(strings = {"1,2, 3,4,5,6", "1,2,삼,4,5,6", "1,two,3,4,5,6"})
     void isNotDigitInputWinningNumber(String input) {
         // given
-        String[] inputWinningNumber = input.split(",");
+        String[] inputWinningNumber = input.split(DELIMITER.getValue());
 
         // when, then
         assertThatThrownBy(() -> InputValidator.validateInputWinningNumber(inputWinningNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("1~45 사이의 숫자만 입력 가능합니다.");
+                .hasMessageContaining(INVALID_INPUT_NUMBER_EXCEPTION.getMessage());
     }
 
     @DisplayName("보너스 번호에 공백이 입력되면 예외가 발생한다.")
@@ -48,7 +52,7 @@ public class InputValidatorTest {
     void isBlankBonusNumber(String bonusNumber) {
         assertThatThrownBy(() -> InputValidator.validateBonusNumber(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("공백은 입력될 수 없습니다.");
+                .hasMessageContaining(INVALID_BLANK_EXCEPTION.getMessage());
     }
 
     @DisplayName("보너스 번호가 숫자가 아니면 예외가 발생한다.")
@@ -57,6 +61,6 @@ public class InputValidatorTest {
     void isNotDigitBonusNumber(String bonusNumber) {
         assertThatThrownBy(() -> InputValidator.validateBonusNumber(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("1~45 사이의 숫자만 입력 가능합니다.");
+                .hasMessageContaining(INVALID_NUMBER_EXCEPTION.getMessage());
     }
 }

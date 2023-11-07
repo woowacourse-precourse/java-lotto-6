47cf334bf39f9ab33bd1afff6fdb41b228498027
권장순 <jsoonworld@gmail.com>
2023-11-08 01:18:39 +0900

    test ( LottoMachineTest#shouldGenerateTheRequestedNumberOfLottoTickets() ) : LottoMachine은 요청된 개수만큼 로또 티켓을 생성하는 테스트 검증.

--- src/test/java/lotto/v3/model/LottoMachineTest.java
+++ src/test/java/lotto/v3/model/LottoMachineTest.java
@@ -4,7 +4,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static lotto.v3.util.LottoConstants.LOTTO_NUMBERS_SIZE;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 
 class LottoMachineTest {
     @DisplayName("LottoMachine은 중복되지 않는 번호로 로또를 생성해야 한다.")
@@ -13,8 +17,25 @@ class LottoMachineTest {
         LottoMachine machine = new LottoMachine();
         Lotto lotto = machine.generateLottoNumbers();
 
-        Assertions.assertThat(lotto.getNumbers()).hasSize(LOTTO_NUMBERS_SIZE.getValue())
+        assertThat(lotto.getNumbers()).hasSize(LOTTO_NUMBERS_SIZE.getValue())
                 .doesNotHaveDuplicates();
+    }
+
+    @DisplayName("LottoMachine은 요청된 개수만큼 로또 티켓을 생성해야 한다.")
+    @Test
+    void shouldGenerateTheRequestedNumberOfLottoTickets() {
+        LottoMachine machine = new LottoMachine();
+        int requestedCount = 5; // 테스트용으로 요청할 로또 티켓의 수
+
+        assertThatNoException().isThrownBy(() -> machine.generateTickets(requestedCount));
+
+        List<Lotto> tickets = machine.generateTickets(requestedCount);
+
+        assertThat(tickets).hasSize(requestedCount);
+        for (Lotto ticket : tickets) {
+            assertThat(ticket.getNumbers()).hasSize(LOTTO_NUMBERS_SIZE.getValue())
+                    .doesNotHaveDuplicates();
+        }
     }
 
 }

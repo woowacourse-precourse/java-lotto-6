a87da7fe132bf9c72f071a51cd5db821618d0dad
Jang DaeYoung <789456jang@naver.com>
2023-11-07 17:20:31 +0900

    test: BonusNumber 테스트 작성

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -2,20 +2,51 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BonusNumberTest {
 
-    @DisplayName("로또 번호가 정해진 범위를 넘어서면 예외가 발생한다.")
+    @DisplayName("보너스 번호를 정상적으로 입력했을 때 보너스 넘버 객체가 반환된다.")
     @Test
-    void createLottoByOverRange() {
-        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 46, 5)))
+    void bonusNumberCreateTest() {
+        Integer number = 5;
+
+        BonusNumber bonusNumber = BonusNumber.of(number);
+
+        assertThat(bonusNumber)
+                .usingRecursiveComparison()
+                .isEqualTo(BonusNumber.of(number));
+    }
+
+    @DisplayName("보너스 번호가 정해진 범위를 넘어서면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46, 100})
+    void createBonusLottoNumberByOverRange() {
+        assertThatThrownBy(() -> BonusNumber.of(0))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 0, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 넘버가 List안에 들어있으면 true를 반환")
+    @Test
+    void containsIn() {
+        BonusNumber bonusNumber = BonusNumber.of(5);
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6);
+
+        assertThat(bonusNumber.containsIn(lotto)).isTrue();
+    }
+
+    @DisplayName("보너스 넘버가 List안에 없으면 flase 반환")
+    @Test
+    void NotContainsIn() {
+        BonusNumber bonusNumber = BonusNumber.of(5);
+        List<Integer> lotto = List.of(1, 2, 3, 4, 9, 6);
+
+        assertThat(bonusNumber.containsIn(lotto)).isFalse();
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,13 +1,26 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    @DisplayName("로또의 번호를 정상적으로 입력했을 때 로또 객체가 반환된다.")
+    @Test
+    void lottoCreateTest() {
+        List<Integer> expectedNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        Lotto lotto = Lotto.of(expectedNumbers);
+
+        assertThat(lotto.getNumbers()).containsExactlyElementsOf(expectedNumbers);
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {

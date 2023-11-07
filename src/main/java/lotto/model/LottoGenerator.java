c7dc2323c4c1c940c14e951770afbf7edc52f68e
NahyeChoi <skgp0628@naver.com>
2023-11-07 18:48:42 +0900

    feat(lotto): 로또 생성

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,14 +1,20 @@
 package lotto.model;
 
+import java.util.ArrayList;
+
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.view.OutputView;
+import lotto.model.Lotto;
 
-public class RandomLottoGenerator {
-	static final int MIN_NUMBER = 1;
-	static final int MAX_NUMBER = 45;
-	static final int LOTTO_SIZE = 6;
+public class LottoGenerator { // 로또 생성기
+	private final int MIN_NUMBER = 1;
+	private final int MAX_NUMBER = 45;
+	private final int LOTTO_SIZE = 6;
 	
-	public List<Integer> lottoNumber(){
-		return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE);
+	private List<Integer> lotto;
+	
+	public List<Integer> randomNumbers(){
+		return lotto = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE);
 	}
 }

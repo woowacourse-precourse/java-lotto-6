a2b1b3761dc5d94c0001c5ce516abc7ad1318427
koreanair777 <jinuklee777@soongsil.ac.kr>
2023-11-08 00:49:10 +0900

    로또의 구매 개수만큼 로또 객체 생성 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -10,7 +11,17 @@ public class Application {
         System.out.println("구입 금액을 입력해 주세요.");
         int buyMoney = getInteger();
         int number = moneyToLottoNumber(buyMoney);
+        System.out.println(number + "개를 구매했습니다");
+        List<Lotto> lottoList = new ArrayList<>();
+        lottoToList(number, lottoList);
         List<Integer> numbers = getNumbers();
+    }
+
+    private static void lottoToList(int number, List<Lotto> lottoList) {
+        for (int i = 0; i < number; i++) {
+            Lotto tmp = new Lotto(getNumbers());
+            lottoList.add(tmp);
+        }
     }
 
     private static List<Integer> getNumbers() {

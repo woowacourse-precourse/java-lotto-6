7cae6c22d788ad68b14d79330333a524dc669c43
gusdn0108 <gusdn6671@naver.com>
2023-11-07 13:22:22 +0900

    test(LottoFactory) : 로또 번호가 1-45 사이의 숫자 이고 6자리인 로또 생성 검증하기

--- src/test/java/lotto/domain/LottoFactoryTest.java
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -2,19 +2,19 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoFactoryTest {
-
+    @DisplayName("로또 번호가 1-45 사이의 숫자 이고 6자리인 로또 생성 검증하기")
     @Test
-    void generateLotto() {
-    }
-
-    @Test
-    void testGenerateLotto() {
-    }
-
-    @Test
-    void generateLotties() {
+    public void testGenerateLotto(){
+        Lotto lotto = LottoFactory.generateLotto();
+        List<Integer> numbers = lotto.getNumbers();
+        assertEquals(6, numbers.size());
+        for (int number : numbers) {
+            assertTrue(number >= 1 && number <= 45);
+        }
     }
 }

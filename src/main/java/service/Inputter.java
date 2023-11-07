ceccd66abb46dced204f8c23dbf3f09148a350b5
Picka <marmong@naver.com>
2023-11-08 00:22:07 +0900

    feat: add inputBonusNumber and inputWinningNumbers in Inputter

--- src/main/java/service/Inputter.java
+++ src/main/java/service/Inputter.java
@@ -4,7 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Inputter {
     public String inputPurchaseAmount() {
-        return Console.readLine();
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
+
+    public String inputWinningNumbers() {
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
+
+    public String inputBonusNumber() {
+        String input = Console.readLine();
+        Console.close();
+        return input;
     }
 
 }

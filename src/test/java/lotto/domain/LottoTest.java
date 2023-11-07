4c5ad52fb1e497ff2d2585bb943b40161957d9c1
siwol025 <cyk99115695@gmail.com>
2023-11-07 21:42:57 +0900

    test(LottoTest): 로또 번호의 개수 유효성을 검사하는 기능 테스트

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.domain.lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoTest {
+    @DisplayName("정해진 로또 번호의 개수를 벗어난 값일때 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3,7})
+    void validateSizeRangeThrowExceptionTest(int size) {
+        List<Integer> numbers = IntStream.rangeClosed(1, size)
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정해진 로또 번호의 개수와 같은 값일때 예외처리 테스트")
+    @Test
+    void validateSizeRangeDoesNotThrowExceptionTest() {
+        int size = 6;
+        List<Integer> numbers = IntStream.rangeClosed(1, size)
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertThatCode(() -> new Lotto(numbers))
+                .doesNotThrowAnyException();
+    }
+}

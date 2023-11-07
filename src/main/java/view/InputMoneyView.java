39fbdf7fe85da93cf3443f4f9725721809352b3b
Seongjun Ahn <dksckd0804@naver.com>
2023-11-08 01:07:06 +0900

    refactor : refactored getValue return type from Money to int

--- src/main/java/view/InputMoneyView.java
+++ src/main/java/view/InputMoneyView.java
@@ -9,13 +9,13 @@ public class InputMoneyView {
     private static final String MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final int ZERO = 0;
 
-    public Money getValue() {
+    public int getValue() {
         System.out.println(MONEY_MESSAGE);
         String money = Console.readLine();
 
         validate(money);
 
-        return new Money(Integer.parseInt(money));
+        return Integer.parseInt(money);
     }
 
     public void validate(String inputValue) {

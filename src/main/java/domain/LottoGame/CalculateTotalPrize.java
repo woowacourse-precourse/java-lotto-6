dfa719b48ce753f3b302b58cfe094767162161a8
nohheeyeon <heeyeon1213@gmail.com>
2023-11-07 16:57:50 +0900

    refactor : 총 상금을 계산하는 클래스 분리

---
+++ src/main/java/domain/LottoGame/CalculateTotalPrize.java
@@ -0,0 +1,15 @@
+package domain.LottoGame;
+
+import java.util.Map;
+
+public class CalculateTotalPrize {
+    public static double calculateTotalPrize(Map<Integer, Long> matchCounts) {
+        double totalPrize = 0;
+        totalPrize += matchCounts.getOrDefault(6, 0L) * 2_000_000_000;
+        totalPrize += matchCounts.getOrDefault(5, 0L) * 30_000_000;
+        totalPrize += matchCounts.getOrDefault(4, 0L) * 50_000;
+        totalPrize += matchCounts.getOrDefault(3, 0L) * 5_000;
+        return totalPrize;
+    }
+}
+

a1cb1e8ed4edef3edfb6164eadbcc43442bb65cf
Youth <boysoeng@naver.com>
2023-11-08 03:24:30 +0900

    feat(Lotto): 로또 숫자 중복 예외 발생 기능
    - Lotto 생성시 6개의 숫자에 중복이 있다면 예외 발생

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,22 +1,32 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.constant.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_SIZE_ERROR.getErrorMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (numbers.size() != distinctNumbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_DUPLICATE_ERROR.getErrorMessage());
+        }
+    }
 
     @Override
     public String toString() {

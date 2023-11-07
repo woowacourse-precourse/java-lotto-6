cc0caf7de74b7e620feba6c5dd54ca42efb18167
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:46:36 +0900

    feat(OutputView): 로또 추첨 결과를 출력하는 기능 추가
    - 등수별 당첨 갯수, 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+import lotto.domain.enums.LottoRank;
 
 public class OutputView {
     public static void showPurchaseCount(int purchaseCount) {
@@ -10,5 +13,22 @@ public class OutputView {
 
     public static void showPurchaseLotto(List<Lotto> lottos) {
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public static void showResult(LottoResult lottoResult, long money) {
+        for (Map.Entry<LottoRank, Integer> entry : getLottoResultEntry(lottoResult)) {
+            LottoRank rank = entry.getKey();
+            int count = entry.getValue();
+            System.out.println(rank.getMessage() + count + "개");
+        }
+
+        System.out.println("총 수익률은 " + String.format("%,.1f", lottoResult.getEarningRate(money)) + "%입니다.");
+    }
+
+    public static List<Map.Entry<LottoRank, Integer>> getLottoResultEntry(LottoResult lottoResult) {
+        return lottoResult.getLottoResult().entrySet()
+                .stream()
+                .filter(entry -> entry.getKey() != LottoRank.NONE)
+                .toList();
     }
 }

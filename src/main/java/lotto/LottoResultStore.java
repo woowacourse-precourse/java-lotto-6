2d8dc162b1911e7ff92df3c1afc5873449603039
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-07 21:58:07 +0900

    refactor : 당첨 금액을 상수로 만들어 구현

--- src/main/java/lotto/LottoResultStore.java
+++ src/main/java/lotto/LottoResultStore.java
@@ -3,10 +3,13 @@ package lotto;
 import java.util.HashMap;
 import java.util.Map;
 
-import static lotto.LottoWinningStatus.*;
-
 public class LottoResultStore {
     private final Map<Integer, Integer> lottoResultStore = new HashMap<>();
+    private static final int FIFTH_AWARD_REWARD_MONEY = 5_000;
+    private static final int FOURTH_AWARD_REWARD_MONEY = 50_000;
+    private static final int THIRD_AWARD_REWARD_MONEY = 1_500_000;
+    private static final int SECOND_AWARD_REWARD_MONEY = 30_000_000;
+    private static final int FIRST_AWARD_REWARD_MONEY = 2_000_000_000;
 
     public void resetLottoResultStore() {
         for (LottoWinningStatus status : LottoWinningStatus.values()) {
@@ -16,18 +19,19 @@ public class LottoResultStore {
 
     public void checkLottoRank(int winningNumberMatch, int bonusNumberMatch) {
         if (winningNumberMatch == 3)
-            lottoResultStore.put(FIFTH_AWARD.getRewardMoney(), lottoResultStore.get(FIFTH_AWARD.getRewardMoney()) + 1);
+            lottoResultStore.put(FIFTH_AWARD_REWARD_MONEY, lottoResultStore.get(FIFTH_AWARD_REWARD_MONEY) + 1);
         if (winningNumberMatch == 4)
-            lottoResultStore.put(FOURTH_AWARD.getRewardMoney(), lottoResultStore.get(FOURTH_AWARD.getRewardMoney()) + 1);
+            lottoResultStore.put(FOURTH_AWARD_REWARD_MONEY, lottoResultStore.get(FOURTH_AWARD_REWARD_MONEY) + 1);
         if (winningNumberMatch == 5)
-            lottoResultStore.put(THIRD_AWARD.getRewardMoney(), lottoResultStore.get(THIRD_AWARD.getRewardMoney()) + 1);
+            lottoResultStore.put(THIRD_AWARD_REWARD_MONEY, lottoResultStore.get(THIRD_AWARD_REWARD_MONEY) + 1);
         if (winningNumberMatch == 5 && bonusNumberMatch == 1)
-            lottoResultStore.put(SECOND_AWARD.getRewardMoney(), lottoResultStore.get(SECOND_AWARD.getRewardMoney()) + 1);
+            lottoResultStore.put(SECOND_AWARD_REWARD_MONEY, lottoResultStore.get(SECOND_AWARD_REWARD_MONEY) + 1);
         if (winningNumberMatch == 6)
-            lottoResultStore.put(FIRST_AWARD.getRewardMoney(), lottoResultStore.get(FIRST_AWARD.getRewardMoney()) + 1);
+            lottoResultStore.put(FIRST_AWARD_REWARD_MONEY, lottoResultStore.get(FIRST_AWARD_REWARD_MONEY) + 1);
     }
 
     public Map<Integer, Integer> getLottoResultStore() {
         return lottoResultStore;
     }
 }
+

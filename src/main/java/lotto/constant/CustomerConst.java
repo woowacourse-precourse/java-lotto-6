f877de56376788ac964330ac863bd10145942bbd
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-11-07 23:47:26 +0900

    Feat : CustomerConst 구현

---
+++ src/main/java/lotto/constant/CustomerConst.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public class CustomerConst {
+
+    public static final String IS_NOT_NUMBER ="숫자를 입력해주세요";
+    public static final String INDIVISIBLE ="1000 단위로 입력해주세요";
+    public static final String IS_NOT_POSITIVE ="양수를 입력해주세요";
+
+
+}

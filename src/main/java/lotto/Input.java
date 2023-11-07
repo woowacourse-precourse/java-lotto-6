d83621ce65c684614b7ebf25b63e924c3aaedd25
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-07 17:20:25 +0900

    style: 클래스 분리 및 명확한 코드를 위한 메서드 이름 변경

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+	public static int calculateLottoCount(String strUserMoney) {
+
+		int userMoney = Integer.parseInt(strUserMoney);
+
+		int lottoCount = (userMoney / 1000);
+
+		return lottoCount;
+
+	}
+
+	public static String numsInput() {
+
+		System.out.println();
+		System.out.println("당첨 번호를 입력해주세요.");
+
+		return Console.readLine();
+	}
+
+	public static String bonusInput() {
+
+		System.out.println();
+		System.out.println("보너스 번호를 입력해 주세요.");
+
+		return Console.readLine();
+	}
+
+	public static String moneyInput() {
+
+		System.out.println("구입금액을 입력해 주세요.");
+
+		return Console.readLine();
+	}
+
+}
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,62 +2,27 @@ package lotto;
 
 import java.util.List;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class LottoGame {
 
 	public static void run() {
 
-		String usermoney = moneyInput();
+		String usermoney = Input.moneyInput();
 
-		int lottoCount = processNumsInput(usermoney);
+		int lottoCount = Input.calculateLottoCount(usermoney);
 
 		printLottocount(lottoCount);
 
-		List<List<Integer>> lottos = Lotto.Create(lottoCount);
+		List<List<Integer>> lottos = Lotto.create(lottoCount);
 
 		printLottoNum(lottos);
 
-		String userNums = numsInput();
-		String bonusUserNum = bonusInput();
+		String userNums = Input.numsInput();
+		String bonusUserNum = Input.bonusInput();
 
 		List<WinningRank> rankingList = CheckNum.lotto(lottos, userNums, bonusUserNum);
 
 		WinningRank.printResult(rankingList);
 
-	}
-
-	private static int processNumsInput(String strUserMoney) {
-
-		int userMoney = Integer.parseInt(strUserMoney);
-
-		int lottocount = (userMoney / 1000);
-
-		return lottocount;
-
-	}
-
-	private static String numsInput() {
-
-		System.out.println();
-		System.out.println("당첨 번호를 입력해주세요.");
-
-		return Console.readLine();
-	}
-
-	private static String bonusInput() {
-
-		System.out.println();
-		System.out.println("보너스 번호를 입력해 주세요.");
-
-		return Console.readLine();
-	}
-
-	private static String moneyInput() {
-
-		System.out.println("구입금액을 입력해 주세요.");
-
-		return Console.readLine();
 	}
 
 	private static void printLottocount(int lottoCount) {

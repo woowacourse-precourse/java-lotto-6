c78bf3c37feb69e001164c2fef8b1c8232869ef6
win9 <krrr883@gmail.com>
2023-11-08 04:03:10 +0900

    [Feat]: 금액입력 검증메소드 구현
    금액 검증 후 정수로 변환하도록 구현

--- src/main/java/view/InputMessage.java
+++ src/main/java/view/InputMessage.java
@@ -1,9 +1,13 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
 import type.TextType;
+import utils.Parser;
 
 public class InputMessage {
-    public static void startInput(){
+    public static int printMoneyInputMessage() {
         System.out.println(TextType.START);
+        String input = Console.readLine();
+        return Parser.convertNumberInput(input);
     }
 }

6de5eafd6b41f620f2b6d4a39c2e34c9f5513cbf
jhva <junghun8158@naver.com>
2023-11-07 20:03:36 +0900

    :recycle: refactor: 로또 구매 재입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,12 +2,20 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.constants.LottoMsg;
+import lotto.exception.ErrorMsg;
 
 public class InputView {
 
     public int parseInputFromUserInteger() {
         System.out.println(LottoMsg.LOTTO_BUY_PRICE.getMsg());
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                int price = Integer.parseInt(commonInput());
+                return price;
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorMsg.ERROR_INPUT_NOT_NUMBER.getMsg());
+            }
+        }
     }
 
     public String commonInput() {

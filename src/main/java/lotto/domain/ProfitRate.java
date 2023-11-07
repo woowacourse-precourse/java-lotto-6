52db25df8e261f64c02d8ac6dca17fde40c541fc
hil679 <hil679922@gmail.com>
2023-11-08 00:43:57 +0900

    Fix(ProfitRate): 테스트 중 큰 숫자에 대한 정밀도 한계 문제 해결
    float을 double로 변경

--- src/main/java/lotto/domain/ProfitRate.java
+++ src/main/java/lotto/domain/ProfitRate.java
@@ -2,18 +2,21 @@ package lotto.domain;
 
 import lotto.util.WinnerRank;
 
+import java.math.BigDecimal;
+
 public class ProfitRate {
     private final static float HUNDRED_FOR_PERCENT = 100.0f;
 
     public String calculateProfitRate(PurchasePrice purchasePrice, WinResult winResult) {
-        float moneyPrize = sumWinMoney(winResult);
-        float profitRate = (moneyPrize / purchasePrice.getPrice()) * HUNDRED_FOR_PERCENT;
+        double moneyPrize = sumWinMoney(winResult);
+        double profitRate = (moneyPrize / purchasePrice.getPrice()) * HUNDRED_FOR_PERCENT;
         return roundUpProfit(profitRate);
     }
 
-    private String roundUpProfit(float profitRate) {
+    private String roundUpProfit(double profitRate) {
         profitRate = Math.round(profitRate * HUNDRED_FOR_PERCENT) / HUNDRED_FOR_PERCENT;
-        return String.valueOf(profitRate);
+
+        return String.valueOf(new BigDecimal(profitRate));
     }
 
     private float sumWinMoney(WinResult winResult) {

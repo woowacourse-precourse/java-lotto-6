4e66218468b5998e25214015f0bfda9f28164779
linirini <2001yerin@naver.com>
2023-11-07 21:16:34 +0900

    refactor : fix printing lotto numbers to string

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,37 +2,19 @@ package lotto.view;
 
 import static lotto.util.OutputEnum.PUBLISHED_LOTTO_COUNT_OUTPUT;
 
-import java.util.List;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.Lottos;
 
 public class OutputView{
 
-    private static final String OPEN_BRACKET = "[";
-    private static final String CLOSE_BRACKET = "]";
-    private static final String SEPERATOR = ", ";
-
     public void printPublishedLottoCount(int lottoCount){
-        System.out.println(lottoCount+PUBLISHED_LOTTO_COUNT_OUTPUT.message());
+        System.out.println(lottoCount + PUBLISHED_LOTTO_COUNT_OUTPUT.message());
     }
 
     public void printPublishedLottos(Lottos lottos){
-        for(Lotto lotto:lottos.getLottos()){
-            printPublishedLotto(lotto);
+        for (Lotto lotto : lottos.getLottos()){
+            System.out.println(lotto.getNumbers().toString());
         }
-    }
-
-    private void printPublishedLotto(Lotto lotto){
-        StringBuilder output = new StringBuilder(OPEN_BRACKET);
-        List<Integer> numbers = lotto.getNumbers();
-        for (int i=0; i<numbers.size();i++){
-            output.append(numbers.get(i));
-            if(i!=numbers.size()-1){
-                output.append(SEPERATOR);
-            }
-        }
-        output.append(CLOSE_BRACKET);
-        System.out.println(output.toString());
     }
 
     public void printWinningResult(){
@@ -42,4 +24,5 @@ public class OutputView{
     public void printEarningRate(){
 
     }
+
 }

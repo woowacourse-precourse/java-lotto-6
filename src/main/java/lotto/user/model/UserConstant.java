cb18846d1738ef81c249a54bc64e4777722377ad
황준혁 <hyug0819@gmail.com>
2023-11-08 00:02:00 +0900

    feat : 로또 번호 입력 상수

---
+++ src/main/java/lotto/user/model/UserConstant.java
@@ -0,0 +1,8 @@
+package lotto.user.model;
+
+public class UserConstant {
+    public static final int MAX_INPUT_NUMBER = 6;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int THOUSAND = 1000;
+}

700e5607e57306f4b5d7ae13aa4cfedecaca7769
anjdydhody <a01066444534@gmail.com>
2023-11-07 22:10:21 +0900

    feat: Add WinningDetails enum

--- src/main/java/lotto/element/WinningDetails.java
+++ src/main/java/lotto/element/WinningDetails.java
@@ -1,4 +1,19 @@
 package lotto.element;
 
 public enum WinningDetails {
+    THREE(0, 3, 5_000),
+    FOUR(1, 4, 50_000),
+    FIVE(2, 5, 1_500_000),
+    BONUS(3, 5, 3_000_000),
+    SIX(4, 6, 2_000_000_000);
+
+    int index;
+    int matchingNumbers;
+    int prize;
+
+    WinningDetails(int index, int matchingNumbers, int prize) {
+        this.index = index;
+        this.matchingNumbers = matchingNumbers;
+        this.prize = prize;
+    }
 }

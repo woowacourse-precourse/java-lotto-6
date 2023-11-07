76a30a0f23cad6cdaec25300b5735949bc140528
kariskan <zkfzpf56@naver.com>
2023-11-07 15:43:29 +0900

    refactor: 소수점 자리까지 표시하지 귀한 BigDecimal 사용

--- src/main/java/lotto/global/view/OutputView.java
+++ src/main/java/lotto/global/view/OutputView.java
@@ -1,6 +1,9 @@
 package lotto.global.view;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.Map;
 import lotto.domain.LottoResult;
 import lotto.domain.LottoResultManager;
 import lotto.global.constant.ConsoleType;
@@ -19,17 +22,14 @@ public class OutputView {
 
     public static void outputStatistics(double rateOfReturn, LottoResultManager lottoResultManager) {
         System.out.println(ConsoleType.OUTPUT_STATISTICS.getComment());
-        System.out.println(ConsoleType.THREE_MATCH.getComment(
-                lottoResultManager.getStatistics().getStatistics().get(WinningType.FIFTH)));
-        System.out.println(ConsoleType.FOUR_MATCH.getComment(
-                lottoResultManager.getStatistics().getStatistics().get(WinningType.FOURTH)));
-        System.out.println(ConsoleType.FIVE_MATCH.getComment(
-                lottoResultManager.getStatistics().getStatistics().get(WinningType.THIRD)));
-        System.out.println(ConsoleType.FIVE_AND_BONUS_MATCH.getComment(
-                lottoResultManager.getStatistics().getStatistics().get(WinningType.SECOND)));
-        System.out.println(ConsoleType.ALL_MATCH.getComment(
-                lottoResultManager.getStatistics().getStatistics().get(WinningType.FIRST)));
-        System.out.println(ConsoleType.OUTPUT_RATE_OF_RETURN.getComment(rateOfReturn + PERCENT));
+        Map<WinningType, Integer> statistics = lottoResultManager.getStatistics().getStatistics();
+        System.out.println(ConsoleType.THREE_MATCH.getComment(statistics.get(WinningType.FIFTH)));
+        System.out.println(ConsoleType.FOUR_MATCH.getComment(statistics.get(WinningType.FOURTH)));
+        System.out.println(ConsoleType.FIVE_MATCH.getComment(statistics.get(WinningType.THIRD)));
+        System.out.println(ConsoleType.FIVE_AND_BONUS_MATCH.getComment(statistics.get(WinningType.SECOND)));
+        System.out.println(ConsoleType.ALL_MATCH.getComment(statistics.get(WinningType.FIRST)));
+        BigDecimal decimalRateOfReturn = new BigDecimal(rateOfReturn).setScale(1, RoundingMode.HALF_UP);
+        System.out.println(ConsoleType.OUTPUT_RATE_OF_RETURN.getComment(decimalRateOfReturn.toPlainString() + PERCENT));
     }
 
     public static void outputLottoNumbers(List<LottoResult> lottoResults) {
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Comparator;
 import java.util.List;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
@@ -23,5 +25,51 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("[1, 45]범위의 중복없는 6개의 정수가 생성되어야 한다.")
+    @Test
+    void should_generateValidNumbers_when_generateLottoNumbers() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto.getNumbers())
+                .isSortedAccordingTo(Comparator.naturalOrder());
+        assertThat(lotto.getNumbers())
+                .containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("중복있는 숫자가 들어가면 실패한다.")
+    @Test
+    void should_fail_when_isDuplicateNumbers() {
+        List<Integer> numbers = List.of(1, 1, 2, 3, 4, 5);
+
+        assertThatThrownBy(
+                () -> new Lotto(numbers));
+    }
+
+    @DisplayName("1미만의 숫자가 들어가면 실패한다.")
+    @Test
+    void should_fail_when_hasLessThan1() {
+        List<Integer> numbers = List.of(-1, 1, 2, 3, 4, 5);
+
+        assertThatThrownBy(
+                () -> new Lotto(numbers));
+    }
+
+    @DisplayName("45초과의 숫자가 들어가면 실패한다.")
+    @Test
+    void should_fail_when_hasGreaterThan45() {
+        List<Integer> numbers = List.of(46, 1, 2, 3, 4, 5);
+
+        assertThatThrownBy(
+                () -> new Lotto(numbers));
+    }
+
+    @DisplayName("6자리가 아니면 실패한다.")
+    @Test
+    void should_fail_when_lengthIsNot6() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        assertThatThrownBy(
+                () -> new Lotto(numbers));
+    }
 }

d624ecb86eaa153c4146024bdfd0f09d52ac25ab
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-11-07 23:04:38 +0900

    fix : convert immutable list to mutable list

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -10,8 +10,10 @@ public class LottoGenerator {
     public static List<Lotto> generateLotteries(int lottoAmount) {
         List<Lotto> lotteries = new ArrayList<>();
         while (lotteries.size() < lottoAmount) {
-            List<Integer> lotteryNumbers = Randoms.pickUniqueNumbersInRange(Unit.LOTTERY_MINIMUM_NUMBER.getUnit(),
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(Unit.LOTTERY_MINIMUM_NUMBER.getUnit(),
                     Unit.LOTTERY_MAXIMUM_NUMBER.getUnit(), Unit.LOTTERY_NUMBER_COUNT.getUnit());
+            List<Integer> lotteryNumbers = new ArrayList<>();
+            lotteryNumbers.addAll(randomNumbers);
             lotteryNumbers.sort(Comparator.naturalOrder());
             lotteries.add(new Lotto(lotteryNumbers));
         }

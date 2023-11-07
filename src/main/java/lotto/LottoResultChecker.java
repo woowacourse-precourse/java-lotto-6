ea707234b0b459f949da36c49b85f45f5dcbebe1
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-08 00:58:09 +0900

    feat($LottoResultChecker): 로또 결과를 체크하는 메서드 구현
    로또 결과를 집계하는 메서드 구현

--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class LottoResultChecker {
-    public void checkLotto(Object lotto, Object drawnNumbers) {
+    HashMap<LottoRank, Integer> lottoResult;
+
+    public LottoResultChecker() {
+        this.lottoResult = new HashMap<>();
+        for (LottoRank lottoRank : LottoRank.values()) {
+            this.lottoResult.put(lottoRank, 0);
+        }
+    }
+
+    public void checkLotto(List<Lotto> lottos, WinningLotto drawnNumbers) {
+        for (Lotto lotto : lottos) {
+            LottoRank rank = LottoRank.getRank(lotto, drawnNumbers);
+            if (null != rank) {
+                Integer count = this.lottoResult.get(rank);
+                this.lottoResult.put(rank, count + 1);
+            }
+        }
     }
 
     public void printTotalPrize() {

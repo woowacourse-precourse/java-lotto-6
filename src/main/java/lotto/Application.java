9cbea00937fb16e765a1938a64efa35b8fbe6419
sojungpp <sophie2458@naver.com>
2023-11-07 14:23:32 +0900

    refactor: controller, view 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,28 +1,15 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        getPurchasePrice();
-
-    }
-
-    private static void getPurchasePrice() {
-        System.out.println("구입금액을 입력해 주세요.");
-        try {
-            Integer money = Integer.parseInt(Console.readLine());
-            validPrice(money);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 구입금액은 숫자여야 합니다.");
-            getPurchasePrice();
-        }
-    }
-
-    private static void validPrice(Integer money) {
-        if (money % 1000 != 0) {
-            System.out.println("[ERROR] 로또 1개의 가격은 1,000원입니다. 올바른 구입금액을 입력하셔야 합니다.");
-            getPurchasePrice();
-        }
+        LottoController lottoController = new LottoController(
+                new InputView(),
+                new OutputView()
+        );
+        lottoController.start();
     }
 }
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public int getPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = 0;
+        try {
+            int input = Integer.parseInt(Console.readLine());
+            validPrice(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 구입금액은 숫자여야 합니다.");
+            getPurchasePrice();
+        }
+        return money;
+    }
+
+    private void validPrice(int money) {
+        if (money % 1000 != 0) {
+            System.out.println("[ERROR] 로또 1개의 가격은 1,000원입니다. 올바른 구입금액을 입력하셔야 합니다.");
+            getPurchasePrice();
+        }
+    }
+}

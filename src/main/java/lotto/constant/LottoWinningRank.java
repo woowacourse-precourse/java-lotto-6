fbbb538e496a2a3219d8799762cd3b8a71473191
edgar6bf <edgar6bf@gmail.com>
2023-11-08 00:45:59 +0900

    fix: LottoWinningRank 잘못된 로또 등수 판단 로직 수정
    [3, true]와 같은 경우는 보너스 번호 일치 여부와 상관없이 번호 개수만 일치하면 상관하면 등수(3등)를 반환해야 한다. 하지만 기존 작성된 로직은 일치하는 번호 개수와 보너스 번호 일치 여부를 초반에 모두 검사하므로 올바른 등수를 판단하지 못한다. 따라서 이 문제를 해결할 수 있도록 로직을 수정하였다.

--- src/main/java/lotto/constant/LottoWinningRank.java
+++ src/main/java/lotto/constant/LottoWinningRank.java
@@ -7,7 +7,8 @@ public enum LottoWinningRank {
     SECOND(5, true, 30000000),
     THIRD(5, false, 1500000),
     FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000);
+    FIFTH(3, false, 5000),
+    NO_LUCK(0, false, 0);
 
     private final int matchingNumberCount;
     private final boolean isBonusNumberMatched;
@@ -25,10 +26,22 @@ public enum LottoWinningRank {
 
     public static LottoWinningRank checkWinningResult(int matchingNumberCount, boolean isBonusNumberMatched) {
         return Arrays.stream(LottoWinningRank.values())
-                .filter(lottoWinningRank -> lottoWinningRank.matchingNumberCount == matchingNumberCount
-                        && lottoWinningRank.isBonusNumberMatched == isBonusNumberMatched)
+                .filter(lottoWinningRank -> lottoWinningRank.matchingNumberCount == matchingNumberCount)
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("일치하는 당첨 기준이 존재하지 않습니다."));
+                .map(filteringRank -> checkBonusNumberMatched(filteringRank, isBonusNumberMatched))
+                .orElse(NO_LUCK);
+    }
+
+    private static LottoWinningRank checkBonusNumberMatched(LottoWinningRank rank, boolean isBonusNumberMatched) {
+        if (rank != SECOND && rank != THIRD) {
+            return rank;
+        }
+
+        if (isBonusNumberMatched) {
+            return SECOND;
+        }
+
+        return THIRD;
     }
 
     public int getMatchingNumberCount() {

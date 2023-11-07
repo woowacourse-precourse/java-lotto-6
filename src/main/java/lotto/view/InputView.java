87dc02e9c2c7403fafac4ed7887831ee61477a76
freemoon99 <soloist132@gmail.com>
2023-11-07 20:49:02 +0900

    feat: 구매 금액 입력 유효성 검사 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static String getInputNumber(){
         String money = Console.readLine();
+        ValidationView.isDivided(money);
         return money;
     }
 }
--- src/main/java/lotto/view/ValidationView.java
+++ src/main/java/lotto/view/ValidationView.java
@@ -1,11 +1,18 @@
 package lotto.view;
 
 public class ValidationView {
+    private static final int LOTTO_PRICE = 1000;
     public static int isNumber(String input) throws IllegalArgumentException{
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 입력한 값이 숫자가 아닙니다.");
+        }
+    }
+    public static void isDivided(String input){
+        int money = Integer.parseInt(input);
+        if ((money % LOTTO_PRICE) != 0){
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 구매할 수 있습니다.");
         }
     }
 }

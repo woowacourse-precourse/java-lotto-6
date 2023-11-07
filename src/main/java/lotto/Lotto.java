b1bcb8f899277f14a4ce9ac19efc853f0cdbdbb0
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 02:09:24 +0900

    feat: '중복 숫자 입력시 얘외 발생 및 equal함수, hash함수 작성'

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,42 @@
-package domain;
+package lotto;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        replicated(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return numbers.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this){
+            return true;
+        }
+        Lotto other = (Lotto) obj;
+        return other.numbers == this.numbers;
+    }
+
+    private void replicated(List<Integer> numbers){
+        Set<Integer> set = numbers.stream()
+                .collect(Collectors.toSet());
+        if(set.size() != 6){
             throw new IllegalArgumentException();
         }
     }

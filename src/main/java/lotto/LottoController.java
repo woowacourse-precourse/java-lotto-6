14ccdbec6a1d249408c9649980d6ea6793898707
Ji <zeegeon@naver.com>
2023-11-07 22:33:41 +0900

    feat: generate lotto random number

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,9 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static java.util.Collections.sort;
 import static lotto.Util.*;
 
 public class LottoController {
@@ -13,7 +16,6 @@ public class LottoController {
 
     private List<Integer> winningNumbers;
     private int bonusNumber;
-
 
     public void playLotto() {
         start();
@@ -26,24 +28,28 @@ public class LottoController {
     private void start() {
         System.out.println("구입금액을 입력해 주세요.");
         this.totalLottoCnt = checkTotalPrice(checkNumberValidate(readLine()));
+        System.out.println();
     }
 
     private void generateLottoNumber() {
         List<Lotto> newLotto = new ArrayList<>();
-        
-        // 로또 횟수만큼 반복
+
+        System.out.println(totalLottoCnt + "개를 구매했습니다.");
+        // 구매한 로또 수만큼 반복
         for(int i=0; i<totalLottoCnt; ++i) {
-            // 6자리 랜덤 변수 생성
-            List<Integer> numbers = new ArrayList<>();
-
-            //생성한 변수 Lotto 객체에 주입
-            Lotto a = new Lotto(numbers);
-
-            // newLotto 배열에 추가
-            lottos.add(a);
+            // 6자리 랜덤 변수 생성 후 생성한 변수 Lotto 객체에 주입
+            Lotto gotcha = new Lotto(generateRandomNumbers());
+            gotcha.printLotto();
+            newLotto.add(gotcha);
         }
-
+        System.out.println();
         this.lottos = newLotto;
+    }
+
+    private List<Integer> generateRandomNumbers() {
+        List<Integer> randonValue = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        sort(randonValue);
+        return randonValue;
     }
 
     private void middle() {
@@ -58,5 +64,13 @@ public class LottoController {
     }
 
     private void printEnd() {
+        System.out.println("당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 1개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n" +
+                "총 수익률은 62.5%입니다.");
     }
 }

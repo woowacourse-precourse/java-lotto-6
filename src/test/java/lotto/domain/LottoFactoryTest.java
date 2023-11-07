31cb059d2b8a997db85dccd5a6ec766935332c42
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:57:00 +0900

    test(LottoFactory) : LottoFactory add

---
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LottoFactoryTest {
+
+    @Test
+    void generateLotto() {
+    }
+
+    @Test
+    void testGenerateLotto() {
+    }
+
+    @Test
+    void generateLotties() {
+    }
+}

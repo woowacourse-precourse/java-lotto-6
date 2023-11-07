b6612cfe0fb4f26afac178c88857d2220650df4b
김민정 <mjeong@sookmyung.ac.kr>
2023-11-07 22:18:18 +0900

    refactor: 예외 메세지 열 길이 감축

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,20 +1,14 @@
 package lotto.domain;
 
 public class Money {
+    public static final String INSUFFICIENT_AMOUNT_MESSAGE = "로또 1장의 가격은 1,000원입니다.";
+    public static final String NOT_DIVISIBLE_MESSAGE = "1,000원으로 나누어 떨어지지 않습니다.";
     private static final int LOTTO_PRICE = 1000;
-    public static final String INSUFFICIENT_AMOUNT_MESSAGE = String.format("로또 1장의 가격은 %,d원입니다.", LOTTO_PRICE);
-    public static final String NOT_DIVISIBLE_MESSAGE = String.format("%,d원으로 나누어 떨어지지 않습니다.", LOTTO_PRICE);
     private final int amount;
 
     public Money(int amount) {
         validate(amount);
         this.amount = amount;
-    }
-
-    public int getLottoPrice() { return LOTTO_PRICE; }
-
-    public int getAmount() {
-        return amount;
     }
 
     private void validate(int amount) {
@@ -24,5 +18,11 @@ public class Money {
         if (amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(NOT_DIVISIBLE_MESSAGE);
         }
+    }
+
+    public int getLottoPrice() { return LOTTO_PRICE; }
+
+    public int getAmount() {
+        return amount;
     }
 }

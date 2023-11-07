cac0143e6dc2bab59c8087161373624383b24193
Hayoon <hy97@sju.ac.kr>
2023-11-08 01:39:37 +0900

    refactor(LottoService): initLottoList() 로직 for문 -> stream lambda로 리팩토링

--- src/main/java/lotto/model/service/LottoService.java
+++ src/main/java/lotto/model/service/LottoService.java
@@ -5,6 +5,7 @@ import lotto.model.domain.Lotto;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 import static lotto.util.constant.Constant.*;
@@ -12,14 +13,9 @@ import static lotto.util.constant.Constant.*;
 public class LottoService {
 
     public List<Lotto> initLottoList(int purchaseAmount) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < purchaseAmount; i++) {
-            List<Integer> lottoNumbers = pickLottoNumbers();
-            Lotto lotto = new Lotto(lottoNumbers);
-            lottoList.add(lotto);
-        }
-
-        return lottoList;
+        return IntStream.range(0, purchaseAmount)
+                .mapToObj(e -> new Lotto(pickLottoNumbers()))
+                .toList();
     }
 
     public List<Integer> pickLottoNumbers() {

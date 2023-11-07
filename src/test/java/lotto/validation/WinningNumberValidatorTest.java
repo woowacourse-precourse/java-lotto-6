5bd1720f005f5d5209c1d7ab309e2d0dc4f4870e
박승민 <tmddudals369@naver.com>
2023-11-07 20:50:40 +0900

    test(winningNumberValidator): 당첨 번호가 여러 개인지 검증하는 기능 테스트

---
+++ src/test/java/lotto/validation/WinningNumberValidatorTest.java
@@ -0,0 +1,33 @@
+package lotto.validation;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumberValidatorTest {
+    
+    @Test
+    @DisplayName("당첨 번호에 쉼표가 포함할 경우 예외를 발생시키지 않는다.")
+    void notThrowExceptionWinningNumbersContainLestDelimiter() {
+        //given
+        String winningNumbers = "1,2,3,4,5,6";
+        //when //then
+        assertThatCode(() -> WinningNumberValidator.validateWinningNumbers(winningNumbers))
+            .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("당첨 번호에 쉼표가 포함되지 않을 경우 예외를 발생시킨다.")
+    void throwExceptionWinningNumbersNotContainLestDelimiter() {
+        //given
+        String winningNumbers = "1";
+        //when //then
+        assertThatThrownBy(() -> WinningNumberValidator.validateWinningNumbers(winningNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 당첨 번호는 쉼표를 구분으로 입력해야 합니다.");
+    }
+}

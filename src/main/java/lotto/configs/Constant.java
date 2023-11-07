8ce45468f5bdf3300f16de9d9802249559fc01b0
bitesnail <rkwoal216@gmail.com>
2023-11-07 16:04:32 +0900

    feature: adding Constant values

---
+++ src/main/java/lotto/configs/Constant.java
@@ -0,0 +1,21 @@
+package lotto.configs;
+
+public class Constant {
+    public static final int LOTTO_PRICE = 1_000;
+    public static final int LOTTO_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_FIFTH_PRIZE = 5_000;
+    public static final int LOTTO_FIFTH_COUNT = 3;
+    public static final int LOTTO_FOURTH_PRIZE = 50_000;
+    public static final int LOTTO_FOURTH_COUNT = 4;
+    public static final int LOTTO_THIRD_PRIZE = 1_500_000;
+    public static final int LOTTO_THIRD_COUNT = 5;
+    public static final int LOTTO_SECOND_PRIZE = 30_000_000;
+    public static final int LOTTO_SECOND_COUNT = 6;
+    public static final int LOTTO_FIRST_PRIZE = 2_000_000_000;
+    public static final int LOTTO_FIRST_COUNT = 6;
+    public static final int LOTTO_NONE_PRIZE = 0;
+    public static final int LOTTO_NONE_COUNT = 0;
+    public static final String LOTTO_FORMAT = "[%d, %d, %d, %d, %d, %d]";
+}

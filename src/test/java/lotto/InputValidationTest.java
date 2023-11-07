a4523c6846eb558104a354716f32207d690ad21d
freemoon99 <soloist132@gmail.com>
2023-11-07 21:37:41 +0900

    test: 유효성 검사 테스트 추가
    - 콤마 중복 입력
    - 당첨 번호 개수
    - 로또 번호 범위
    - 로또 번호 중복

--- src/test/java/lotto/InputValidationTest.java
+++ src/test/java/lotto/InputValidationTest.java
@@ -2,6 +2,8 @@ package lotto;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.view.ValidationView;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,25 @@ public class InputValidationTest {
         assertThrows(IllegalArgumentException.class, () -> {
             ValidationView.isDivided("1500");
         });
+    }
+    @Test
+    void 입력_콤마_테스트(){
+        String[] arr = new String[]{"1", "", "3"};
+        assertThrows(IllegalArgumentException.class, () -> ValidationView.checkConsecutiveCommas(arr));
+    }
+    @Test
+    void 당첨_번호_개수_테스트(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        assertThrows(IllegalArgumentException.class, () -> ValidationView.checkNumberSize(numbers));
+    }
+    @Test
+    void 로또_번호_범위_테스트(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 50);
+        assertThrows(IllegalArgumentException.class, () -> ValidationView.checkInRange(numbers));
+    }
+    @Test
+    void 로또_번호_중복_테스트(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+        assertThrows(IllegalArgumentException.class, () -> ValidationView.checkDuplicateNumbers(numbers));
     }
 }

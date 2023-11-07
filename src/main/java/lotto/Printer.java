e3240b72bdcde24b66cfdf72a8372121802b6cff
Identityless <rlandwnd555@naver.com>
2023-11-07 17:34:37 +0900

    feat : Printer 클래스 추가 및 싱글톤 관리를 위한 코드 추가

---
+++ src/main/java/lotto/Printer.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class Printer {
+    private static Printer printer;
+
+    private Printer() {}
+
+    public static Printer getPrinter() {
+        if (printer == null) {
+            printer = new Printer();
+        }
+        return printer;
+    }
+
+}

a6beeefbc7a475cac9be7378faf282a187d1c20a
minjong kim <godminjong@naver.com>
2023-11-07 23:01:53 +0900

    refact: 공통 사용 되는 문자열 상수로 추출함.
    콘솔 화면에 표시되는 공통 문자열 인터페이스에 상수로 정의해두었다.

---
+++ src/main/java/lotto/view/ViewConstant.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public interface ViewConstant {
+    String PRICE_MESSAGE = "구입 금액을 입력해 주세요.";
+    String PURCHASE_COMPLETED_MESSAGE = "개를 구매했습니다.";
+    String LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    String WINNING_STATISTIC_MESSAGE = "\n당첨 통계\n---";
+
+}

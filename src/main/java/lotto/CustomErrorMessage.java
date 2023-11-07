f51218d211bd0202eed58bdbea4b07d521f1a603
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-07 20:27:09 +0900

    test($LottoMachineTest): 로또 발행기 테스트 구현
    입력에 따라 예상되는 테스트 코드 구현

--- src/main/java/lotto/CustomErrorMessage.java
+++ src/main/java/lotto/CustomErrorMessage.java
@@ -7,5 +7,5 @@ public class CustomErrorMessage {
     public static final String DUPLICATED_BONUS_NUMBER = String.join(" ", ERROR_PREFIX, "기존 6개 숫자와 중복됩니다.");
     public static final String WRONG_NUMBER_LENGTH = String.join(" ", ERROR_PREFIX, "기본 숫자의 개수가 잘못되었습니다.");
     public static final String DUPLICATED_NUMBERS = String.join(" ", ERROR_PREFIX, "중복된 숫자가 존재합니다.");
-    ;
+    public static final String CHANGE = String.join(" ", ERROR_PREFIX, "잔돈이 존재합니다.");
 }

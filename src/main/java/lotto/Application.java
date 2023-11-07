181d3aa77d09bb4d7a630b4a112e4d6f460a6f34
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-11-07 21:59:03 +0900

    23/11/07 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,53 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력 메소드 실행
+        int purchaseMoney = purchaseMoney();
+        List<Integer> winNumbers = winNumber();
+        int addNum = addNum();
+
+        System.out.println(purchaseMoney);
+        System.out.println(winNumbers);
+        System.out.println(addNum);
+
     }
+
+    // 로또 구입 금액 입력
+    public static int purchaseMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int input = Integer.parseInt(readLine());
+
+        return input;
+    }
+
+    // 당첨 번호 입력
+    public static List<Integer> winNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = readLine();
+
+        List<Integer> numbers = new ArrayList<>();
+        List<String> strNumbers = Arrays.asList(input.split(","));
+
+        for(int i = 0; i < strNumbers.size(); i++){
+            numbers.add(Integer.parseInt(strNumbers.get(i)));
+        }
+
+        return numbers;
+    }
+
+    // 보너스 번호 입력
+    public static int addNum(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int input = Integer.parseInt(readLine());
+
+        return input;
+    }
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +20,19 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public List<Integer> createRandomNumber(){
+        // 3개의 숫자가 담길 때까지 반복한다
+        // 숫자는 어떤 수든 상관없다
+        // 주어진 횟수만큼 실행할 것이다.
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int number = Randoms.pickNumberInRange(0, 9);
+            numbers.add(number);
+        }
+        return numbers;
+    }
 }

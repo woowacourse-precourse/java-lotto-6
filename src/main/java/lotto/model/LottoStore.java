0b890b62dc7bef0a28bff76933b0ed60bc372278
Cycrypto <jh01love@naver.com>
2023-11-07 18:38:36 +0900

    feat: 상금 및 수익률 계산 메서드 추가

--- src/main/java/lotto/model/LottoStore.java
+++ src/main/java/lotto/model/LottoStore.java
@@ -1,10 +1,31 @@
 package lotto.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import lotto.constant.Ranking;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.constant.WinningMoney;
 
 public class LottoStore {
+    private static final int LOTTO_COST = 1000;
+    private int lottoSales;
+    private BigDecimal totalWinningMoney;
+
+    public void setLottoSales(int lottoSales) {
+        this.lottoSales = lottoSales;
+    }
+
+    public void setTotalWinningMoney(List<Integer> lottoRankings) {
+        Stream<BigDecimal> winningAmounts = IntStream.range(1, lottoRankings.size())
+                .mapToObj(i -> BigDecimal.valueOf(lottoRankings.get(i))
+                        .multiply(BigDecimal.valueOf(WinningMoney.values()[i-1].getMoney())));
+
+        this.setTotalWinningMoney(winningAmounts.reduce(BigDecimal.ZERO, BigDecimal::add));
+    }
+
     public List<Integer> rankLottos(List<Lotto> customerLotto, Lotto winningLotto, int bonusNumber) {
         List<Integer> lottoRank = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0));
         for (Lotto customer : customerLotto) {
@@ -19,8 +40,8 @@ public class LottoStore {
 
     private int compareLotto(List<Integer> customerLotto, List<Integer> winningLotto, int bonusNumber) {
         int matchCount = 0;
-        for (int i = 0; i < customerLotto.size(); i++) {
-            if (winningLotto.contains(customerLotto.get(i))) {
+        for (Integer lotto : customerLotto) {
+            if (winningLotto.contains(lotto)) {
                 matchCount++;
             }
         }
@@ -47,7 +68,22 @@ public class LottoStore {
         return flag;
     }
 
+    public BigDecimal calculateRateOfReturn() {
+        BigDecimal intDivisor = BigDecimal.valueOf((long) this.lottoSales * LOTTO_COST);
+        return this.totalWinningMoney.divide(intDivisor, 3, RoundingMode.DOWN)
+                .multiply(BigDecimal.valueOf(100)).stripTrailingZeros();
+    }
+
+
     private boolean isBonusInNumbers(List<Integer> numbers, int bonusNumber) {
         return numbers.contains(bonusNumber);
+    }
+
+    public BigDecimal getTotalWinningMoney() {
+        return totalWinningMoney;
+    }
+
+    public void setTotalWinningMoney(BigDecimal totalWinningMoney) {
+        this.totalWinningMoney = totalWinningMoney;
     }
 }

95b3053e8ff12bd61a9a4f67d16655727d75f03a
Chaeros <chpark6851@koreatech.ac.kr>
2023-11-07 17:24:01 +0900

    feat: 로또 번호 6개를 랜덤 생성하는 메서드 작성
    랜덤으로 로또 번호 6개를 List 형식으로 생성하고, 그에 필요한 상수를
    작성하였음

---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class Constant {
+    final public static int LOTTO_START_NUMBER = 1;
+    final public static int LOTTO_END_NUMBER = 45;
+    final public static int LOTTO_TOTAL_NUMBERS =6;
+}
---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,12 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.constant.Constant;
+
+public class LottoService {
+
+    List<Integer> generateLotto(){
+        return Randoms.pickUniqueNumbersInRange(Constant.LOTTO_START_NUMBER, Constant.LOTTO_END_NUMBER, Constant.LOTTO_TOTAL_NUMBERS);
+    }
+}

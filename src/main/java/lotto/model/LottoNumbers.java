31920d7d403be3325e3773ef6001a8721d19eb37
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 00:10:19 +0900

    feat: 구입한 로또 수량 만큼 로또를 생성한다.

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -9,10 +9,20 @@ import java.util.stream.Collectors;
 public class LottoNumbers {
     private final List<List<Integer>> lottoNumbers = new ArrayList<>();
 
+    public LottoNumbers(int lottoPurchaseCount) {
+        createLottoNumbers(lottoPurchaseCount);
+    }
+
     private List<Integer> sortedLottoNumbers() {
         return generateLottoNumbers().stream()
                 .sorted()
                 .collect(Collectors.toList());
+    }
+
+    private void createLottoNumbers(int lottoPurchaseCount) {
+        do {
+            lottoNumbers.add(sortedLottoNumbers());
+        } while (lottoNumbers.size() < lottoPurchaseCount);
     }
 
 }

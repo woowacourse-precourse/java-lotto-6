ffac16aa446ec7e6e6880cd592772639260fa075
권장순 <jsoonworld@gmail.com>
2023-11-07 14:17:44 +0900

    feat ( LottoRank ) : 로또 당첨 등수를 나타내는Enum 구현.

---
+++ src/main/java/lotto/v2/util/LottoRank.java
@@ -0,0 +1,48 @@
+package lotto.v2.util;
+
+public enum LottoRank {
+
+    FIRST(6, 2_000_000_000L, false),
+    SECOND(5, 30_000_000L, true), // 보너스 번호 일치 가정
+    THIRD(5, 1_500_000L, false),
+    FOURTH(4, 50_000L, false),
+    FIFTH(3, 5_000L, false);;
+
+    private final int matchCount;
+    private final long prizeMoney;
+    private final boolean bonusMatch;
+
+    LottoRank(int matchCount, long prizeMoney, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+
+    public static LottoRank valueOf(int matchCount, boolean bonusMatch) {
+        if (matchCount == SECOND.matchCount && bonusMatch) {
+            return SECOND;
+        }
+        for (LottoRank rank : values()) {
+            if (rank.matchCount == matchCount && !rank.bonusMatch) {
+                return rank;
+            }
+        }
+        return null; // or throw an exception if match count is not a valid rank
+    }
+}
+
+
+
+}

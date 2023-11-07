84f55c4d9dad2848b4ca46bdfcc5909fae543fd0
davemins <davemins4125@naver.com>
2023-11-07 14:24:58 +0900

    refactor: 패키지 구조 변경 영역에 따라 패키지 구조를 변경하여 Lotto 클래스를 이동하였습니다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,8 +1,7 @@
-package lotto;
+package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;

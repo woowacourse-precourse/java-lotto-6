4f04304f993ce232f95badc32b2f4901e8850d73
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-07 20:30:40 +0900

    refactor(Lotto): 쓰지 않는 import문을 모두 제거했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,6 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.Lotto;
-import lotto.domain.Lottos;
-import lotto.domain.NumberGenerator;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {

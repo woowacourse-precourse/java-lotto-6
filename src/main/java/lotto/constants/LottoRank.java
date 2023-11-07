5c47c617a1918205cd423811472e6b1afc90f7b4
JongMinCh0i <wonderland314592@gmail.com>
2023-11-07 10:16:20 +0900

    feat(LottoRank): 로또의 등수를 계산하는 기능 추가

---
+++ src/main/java/lotto/constants/LottoRank.java
@@ -0,0 +1,28 @@
+package lotto.constants;
+
+import java.util.function.BiPredicate;
+
+public enum LottoRank {
+    FIRST((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 6),
+    SECOND((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && bonusWinningNumber),
+    THIRD((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 5 && !bonusWinningNumber),
+    FOURTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 4),
+    FIFTH((matchedNumbers, bonusWinningNumber) -> matchedNumbers == 3);
+
+    private final BiPredicate<Integer, Boolean> picked;
+
+    LottoRank(BiPredicate<Integer, Boolean> picked) {
+        this.picked = picked;
+    }
+
+    public static LottoRank evaluateLottoRank(int matchedNumbers, boolean hasBonus) {
+        LottoRank lottoRank = null;
+        for (LottoRank p : values()) {
+            if (p.picked.test(matchedNumbers, hasBonus)) {
+                lottoRank = p;
+                break;
+            }
+        }
+        return lottoRank;
+    }
+}

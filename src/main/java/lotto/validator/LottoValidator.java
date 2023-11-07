cf13672c5985689031aac9822720208c34ef706e
koomchang <leahpar0401@gmail.com>
2023-11-07 16:00:52 +0900

    test: 로또 번호에 대한 입력값 검증 테스트

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -6,7 +6,7 @@ import lotto.model.LottoNumber;
 public class LottoValidator implements BasicValidator<List<Integer>> {
 
     private static final String NUMBER_NOT_BETWEEN_ONE_AND_FORTY_FIVE = "[ERROR] 로또 번호는 1과 45 사이여야 합니다.";
-    private static final String NUMBER_NOT_SIX_NUMBERS = "[ERROR] 로또 번호는 6자리가 아닙니다.";
+    private static final String NUMBER_NOT_SIX_NUMBERS = "[ERROR] 로또 번호는 6자리 이어야 합니다.";
     private static final String NUMBER_DUPLICATED = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
 
     @Override

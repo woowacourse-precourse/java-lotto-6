ad113f3e16bbc51136d328d9fe4ff894c6ff6e17
WooJJam <jaemin5548@naver.com>
2023-11-07 15:21:15 +0900

    [Refactor] Prize enum 필드 변경

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -8,35 +8,11 @@ public enum Prize {
     FIFTH(3, 5_000,0),
     NONE(0, 0,0);
 
-    private final int match;
-    private final int price;
-    private int count;
-    private final boolean hasBonus;
-
     Prize(int match, int price, int count) {
         this(match, price, count, false);
     }
 
     Prize(int match, int price, int count, boolean hasBonus) {
-        this.match = match;
-        this.price = price;
-        this.count = count;
-        this.hasBonus = hasBonus;
     }
 
-    public void addCount() {
-        count++;
-    }
-
-    public int getMatch() {
-        return match;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public boolean isHasBonus() {
-        return hasBonus;
-    }
 }

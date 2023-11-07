3192f8419b2731bd8db6263678ce7f32cee985cd
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-07 23:23:09 +0900

    refactor(LottoBundle): 메소드 수정 및 테스트코드 추가

--- src/main/java/lotto/domain/LottoOrder.java
+++ src/main/java/lotto/domain/LottoOrder.java
@@ -4,12 +4,16 @@ public class LottoOrder {
     private final Money money;
     private final LottoBundle lottos;
 
-    public LottoOrder(final Money money, final LottoBundle lottos) {
+    private LottoOrder(final Money money, final LottoBundle lottos) {
         this.money = money;
         this.lottos = lottos;
     }
 
-    public int getBuyQuantity(final int price) {
+    public static LottoOrder of(final Money money, final LottoBundle lottos) {
+        return new LottoOrder(money, lottos);
+    }
+
+    public int getOrderQuantity(final int price) {
         return money.calculateLottoQuantity(price);
     }
 

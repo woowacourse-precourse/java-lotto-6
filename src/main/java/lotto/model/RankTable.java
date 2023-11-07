4dde6e74fcb61965b71c6c009a1e209d3357c3eb
taewan625 <taewan1997625@gmail.com>
2023-11-07 22:28:10 +0900

    Feat: Add RankTable

---
+++ src/main/java/lotto/model/RankTable.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+public enum RankTable {
+    THREE( 3, false, 5_000),
+    FOUR(4, false, 50_000),
+    FIVE(5, false,  1_500_000),
+    BONUS(5, true,  30_000_000),
+    SIX(6, false, 2_000_000_000);
+
+    private int matchNumber;
+    private boolean bonus;
+    private int money;
+
+    RankTable(int matchNumber, boolean bonus, int money) {
+        this.matchNumber = matchNumber;
+        this.bonus = bonus;
+        this.money = money;
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
+    }
+
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}

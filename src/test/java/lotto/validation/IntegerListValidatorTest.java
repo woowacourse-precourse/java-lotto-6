d1ae4709c769dacaa5dbe2dd8f0150a1a0360982
inpink <inpink@kakao.com>
2023-11-08 00:50:06 +0900

    test: 정수리스트가 주어진 정수와 사이즈가 같지 않으면 예외처리

--- src/test/java/lotto/validation/IntegerListValidatorTest.java
+++ src/test/java/lotto/validation/IntegerListValidatorTest.java
@@ -31,4 +31,29 @@ public class IntegerListValidatorTest {
 
     }
 
+    @Test
+    public void 정수리스트_사이즈가_다르면_예외처리() {
+        // Given
+        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int invalidSize = 7;
+
+        // When && Then
+        assertThatThrownBy(() -> IntegerListValidator.validateSize(validNumbers, invalidSize))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("정해진 개수의 숫자를 입력하세요.");
+    }
+
+    @Test
+    public void 정수리스트_사이즈가_같으면_정상통과() {
+        // Given
+        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int validSize = 6;
+
+        // When
+        IntegerListValidator.validateSize(validNumbers, validSize);
+
+        // Then
+
+    }
+
 }

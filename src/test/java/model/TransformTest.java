1a75bd26ca076db419918f6a662b57224aedd171
star-candy <el0710@naver.com>
2023-11-07 12:18:41 +0900

    feat(lotto): String input을 list로 전환하는지에 대한 테스트

--- src/test/java/model/TransformTest.java
+++ src/test/java/model/TransformTest.java
@@ -1,16 +1,21 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TransformTest {
 
     @Test
-    void generateRandomListTest() {
-    }
-
-    @Test
-    void fullLottoValueTest() {
+    void 입력값_리스트로_분할_테스트() {
+        //given
+        String input = "1,2,3,4,5";
+        List<Integer> result;
+        List<Integer> expectedResult = List.of(1, 2, 3, 4, 5);
+        //when
+        Transform transform = new Transform();
+        result = transform.winningNumberToList(input);
+        //then
+        assertThat(result).isEqualTo(expectedResult);
     }
 }

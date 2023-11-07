ae04c73aefb9aaf57fe6fe7f2f34f15eb310e4c3
SunHyeok <cghhld@naver.com>
2023-11-07 15:12:10 +0900

    refactor: 중복 에러 추가

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -22,9 +24,7 @@ public class InputValidator {
         while (true) {
             try {
                 int bonusNumber = Integer.parseInt(Console.readLine());
-                if (bonusNumber < 1 || bonusNumber > 45) {
-                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-                }
+                validateBonusNumber(bonusNumber);
                 return bonusNumber;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -45,11 +45,24 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
 
+        Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
+
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+
         for (int number : lottoNumbers) {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
     }
+
+    private static void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
+
 

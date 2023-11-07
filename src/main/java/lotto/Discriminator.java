e9de0c2a1b23cd7708b7451b96173e6a40868027
Identityless <rlandwnd555@naver.com>
2023-11-07 20:50:23 +0900

    feat : 복권 판독 기능 추가

--- src/main/java/lotto/Discriminator.java
+++ src/main/java/lotto/Discriminator.java
@@ -4,8 +4,22 @@ import java.util.List;
 
 public class Discriminator {
     private List<Integer> correctNumbers;
+    private int bonus;
 
-    public Discriminator(List<Integer> correctNumbers) {
+    public Discriminator(List<Integer> correctNumbers, int bonus) {
         this.correctNumbers = correctNumbers;
+        this.bonus = bonus;
+    }
+
+    public Grade discriminate(List<Integer> lotto) {
+        int cnt = 0;
+
+        for (int n : lotto) {
+            if (correctNumbers.contains(cnt)) {
+                cnt++;
+            }
+        }
+
+        return GradeUtil.gradeMapper(cnt, lotto.contains(bonus));
     }
 }

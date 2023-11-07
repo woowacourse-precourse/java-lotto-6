f2f750666ef808517cce22f6cee9fa8e3d615fbb
choheejin <hjcho9510@gmail.com>
2023-11-08 00:54:31 +0900

    test(LottoMachine): Test correct comparison of bonus number and purchased lotto tickets

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -4,6 +4,7 @@ import lotto.model.Lotto;
 import lotto.model.LottoMachine;
 import lotto.model.Lottos;
 import lotto.model.WinningLotto;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,21 +15,33 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LottoMachineTest {
-    private final List<Integer> HUMAN_LOTTO = List.of(1, 4, 5, 8, 9, 10);
-    private final List<Integer> WINNING_LOTTO = List.of(1, 2, 3, 4, 5, 6);
-    private final int BONUS = 7;
-    private final int COUNT = 1;
+    private static Lottos lottos;
+    private static WinningLotto winningLotto;
+    private static LottoMachine lottoMachine;
+    private static final List<Integer> HUMAN_LOTTO = List.of(1, 4, 5, 7, 9, 10);
+    private static final List<Integer> WINNING_LOTTO = List.of(1, 2, 3, 4, 5, 6);
+    private static final int BONUS = 7;
+    private static final int COUNT = 1;
+
+    @BeforeAll
+    static void setup() {
+        lottos = mock(Lottos.class);
+        when(lottos.getLottos()).thenReturn(List.of(new Lotto(HUMAN_LOTTO)));
+
+        winningLotto = new WinningLotto(WINNING_LOTTO, BONUS);
+
+        lottoMachine = new LottoMachine(lottos, winningLotto, COUNT);
+    }
 
     @DisplayName("당첨 로또와 구매한 로또의 차이를 제대로 비교하는가")
     @Test
     void lottoMachine_로또번호_비교() {
-        Lottos lottos = mock(Lottos.class);
-        when(lottos.getLottos()).thenReturn(List.of(new Lotto(HUMAN_LOTTO)));
-
-        WinningLotto winningLotto = new WinningLotto(WINNING_LOTTO, BONUS);
-
-        LottoMachine lottoMachine = new LottoMachine(lottos, winningLotto, COUNT);
-
         assertThat(lottoMachine.compareLotto(lottos.getLottos().get(0))).isEqualTo(3);
+    }
+
+    @DisplayName("보너스 번호와 구매한 로또의 차이를 제대로 비교하는가?")
+    @Test
+    void lottoMachine_보너스번호_비교() {
+        assertThat(lottoMachine.compareBonus(lottos.getLottos().get(0))).isEqualTo(true);
     }
 }

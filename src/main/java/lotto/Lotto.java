e4d5d79d16d2fdcf77f55f7fe648fd8c6a09a256
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-11-08 06:46:07 +0900

    feat: 보너스볼을 포함했는지 판단하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public boolean hasBonusBall(int bonusNumber){
+        return numbers.contains(bonusNumber);
+    }
 }

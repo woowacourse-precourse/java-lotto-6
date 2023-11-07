8ec5c4837702eaa926f8eebee45a8419bff35e0d
daheeParkk <qkrekgml7414@naver.com>
2023-11-08 01:10:49 +0900

    feat : output 기능 구현

--- src/main/java/lotto/message/OutputMessage.java
+++ src/main/java/lotto/message/OutputMessage.java
@@ -1,2 +1,26 @@
-package lotto.message;public class OutputView {
+package lotto.message;
+
+public enum OutputMessage {
+    NUMBER_OF_PURCHASES("%d개를 구매했습니다."),
+    WINNING_STATISTICS("당첨 통계\n---"),
+    FIRST_PLACE("3개 일치 (5,000원) - %d개"),
+    SECOND_PLACE("4개 일치 (50,000원) - %d개"),
+    THIRD_PLACE("5개 일치 (1,500,000원) - %d개"),
+    FOURTH_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIFTH_PLACE("6개 일치 (2,000,000,000원) - %d개"),
+    RATE_OF_RETURN("총 수익률은 %d%입니다.");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessage(int number) {
+        return String.format(message, number);
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,44 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import static lotto.message.OutputMessage.*;
+
+public class OutputView {
+
+    public void outputNumberOfPurchases(int numberOfPurchases) {
+        System.out.println(NUMBER_OF_PURCHASES.getMessage(numberOfPurchases));
+    }
+
+    public void outputLotto(Lotto lotto) {
+        System.out.println(lotto.toString());
+    }
+
+    public void outputWinningStatistics() {
+        System.out.println(WINNING_STATISTICS.getMessage());
+    }
+
+    public void outputFirstPlace(int count) {
+        System.out.print(FIRST_PLACE.getMessage(count));
+    }
+
+    public void outputSecondPlace(int count) {
+        System.out.print(SECOND_PLACE.getMessage(count));
+    }
+
+    public void outputThirdPlace(int count) {
+        System.out.print(THIRD_PLACE.getMessage(count));
+    }
+
+    public void outputFourthPlace(int count) {
+        System.out.print(FOURTH_PLACE.getMessage(count));
+    }
+
+    public void outputFifthPlace(int count) {
+        System.out.print(FIFTH_PLACE.getMessage(count));
+    }
+
+    public void outputRateOfReturn(int rateOfReturn) {
+        System.out.print(RATE_OF_RETURN.getMessage(rateOfReturn));
+    }
 }

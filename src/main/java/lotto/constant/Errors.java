e3640f3fe77837552f8ca0a487f644175ef58c12
Seyeon <wjdtpdus828@naver.com>
2023-11-07 21:46:03 +0900

    add(Errors): 보너스 숫자에 관련된 예외 처리 String 상수 추가

--- src/main/java/lotto/constant/Errors.java
+++ src/main/java/lotto/constant/Errors.java
@@ -8,7 +8,9 @@ public enum Errors {
     WINNING_NUMBER_WRONG_SIZE_MESSAGE("[ERROR] 로또 번호의 개수는 6개만 가능합니다."),
     WINNIG_NUMBER_NOT_INTEGER("[ERROR] 로또 번호에 문자가 존재합니다."),
     WINNING_NUMBER_WRONG_RANGE_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    WINNIG_NUMBER_DUPLICATE_MESSAGE("[ERROR] 로또 번호에 중복된 수가 존재합니다.");
+    WINNIG_NUMBER_DUPLICATE_MESSAGE("[ERROR] 로또 번호에 중복된 수가 존재합니다."),
+    BONUS_NUMBER_NOT_INTEGER_MESSAGE("[ERROR] 보너스 번호는 정수만 가능합니다."),
+    BONUS_NUMBER_DUPLICATE_MESSAGE("[ERROR] 당첨 번호에 보너스 번호와 중복된 수가 존재합니다.");
 
     private final String errorMessage;
     Errors(String errorMessage){

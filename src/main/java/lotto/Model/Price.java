7b74c80f3d64395ce23e5cc6cd16de22b908d0da
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 11:27:57 +0900

    refactor(Price): 매직넘버 상수화, 함수 분리

--- src/main/java/lotto/Model/Price.java
+++ src/main/java/lotto/Model/Price.java
@@ -2,55 +2,71 @@ package lotto.Model;
 
 import static lotto.Utils.toInt;
 
-import lotto.View.InputView;
-
 public class Price {
+    private static final int ZERO = 0;
+    private static final int ONE_HUNDRED_PERCENT = 100;
+    private static final int UNIT_OF_ONE_LOTTO_PRICE = 1000;
+    private static final int MAX_LOTTO_PURCHASE_PRICE = 2000000000;
+
+    private static final String ERROR_NOT_NUMBER =
+            "[ERROR] 공백이나 쉼표같은 특수기호 없이 숫자만을 입력하세요.(최대 구입금액은 20억입니다)";
+    private static final String ERROR_UNIT_PRICE_OF_PURCHASED_LOTTO = "[ERROR] 1000원 단위로 입력하세요.";
+    private static final String ERROR_NOT_PURCHASE_LOTTO = "[ERROR] 로또구입금액은 최소 1000원입니다.";
+    private static final String ERROR_OVER_MAX_PRICE_OF_LOTTO_FOR_PURCHASE = "[ERROR] 로또구입금액은 최대 20억입니다.";
+
     int purchasePrice;
 
     public Price(String input) {
+        
         validate(input);
         this.purchasePrice = toInt(input);
     }
 
-    public int divideWithOneThousands() {
-        return purchasePrice / 1000;
+    public int countPurchasedLotto() {
+        return purchasePrice / UNIT_OF_ONE_LOTTO_PRICE;
     }
 
-    public double divideWith(int totalPrize) {
-        if (totalPrize == 0) {
-            return 0d;
+    public double makeWinningRate(int totalWinningPrize) {
+        if (isZero(totalWinningPrize)) {
+            return ZERO;
         }
-        return (double)totalPrize / purchasePrice * 100;
+        return dividePurchasePriceWith(totalWinningPrize) * ONE_HUNDRED_PERCENT;
     }
 
-    private void validate(String input) {
-        int validInput = isNumber(input);
-        isDivideWithOneThousands(validInput);
-        isCorrectRange(validInput);
+    private void validate(String input) throws IllegalArgumentException {
+        int numberInput = checkNumber(input);
+        checkDividingUnitOfOneLottoPrice(numberInput);
+        checkValidRange(numberInput);
     }
 
-    private int isNumber(String input) {
+    private int checkNumber(String input) {
         try {
-            int priceInput = toInt(input);
-            if (priceInput > 2000000000) {
-                throw new IllegalArgumentException("[ERROR] 로또구입금액은 최대 20억입니다.");
-            }
-            return priceInput;
+            return toInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 공백이나 쉼표같은 특수기호 없이 숫자만을 입력하세요.(최대 구입금액은 20억입니다)");
-
+            throw new IllegalArgumentException(ERROR_NOT_NUMBER);
         }
     }
 
-    private void isDivideWithOneThousands(int validInput) throws IllegalArgumentException {
-        if (validInput % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하세요.");
+    private void checkDividingUnitOfOneLottoPrice(int validInput) {
+        if (validInput % UNIT_OF_ONE_LOTTO_PRICE != ZERO) {
+            throw new IllegalArgumentException(ERROR_UNIT_PRICE_OF_PURCHASED_LOTTO);
         }
     }
 
-    private void isCorrectRange(int validInput) {
-        if (validInput <= 0) {
-            throw new IllegalArgumentException("[ERROR] 로또구입금액은 최소 1000원입니다.");
+    private void checkValidRange(int validInput) {
+        if (validInput <= UNIT_OF_ONE_LOTTO_PRICE) {
+            throw new IllegalArgumentException(ERROR_NOT_PURCHASE_LOTTO);
         }
+        if (validInput > MAX_LOTTO_PURCHASE_PRICE) {
+            throw new IllegalArgumentException(ERROR_OVER_MAX_PRICE_OF_LOTTO_FOR_PURCHASE);
+        }
+    }
+
+    private boolean isZero(int totalPrize) {
+        return totalPrize == ZERO;
+    }
+
+    private double dividePurchasePriceWith(int totalPrize) {
+        return (double)totalPrize / purchasePrice;
     }
 }

a5f82d5258ec4ef29bce44400c95c8553b179f08
EveKristinLee <jylisa0612@naver.com>
2023-11-07 21:15:39 +0900

    feat : 당첨 번호 입력받기 - 6개인지 확인

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,20 +1,29 @@
 package lotto.Domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.Constant.Constant;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(String inputNumbers) {
+        List<Integer> numbers = validate(inputNumbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    private List<Integer> validate(String inputNumbers) {
+        List<Integer> validateNumbers = new ArrayList<>();
+        lottoSizeValidate(inputNumbers);
+        return validateNumbers;
     }
 
-    // TODO: 추가 기능 구현
+    //입력값이 6개인지 확인
+    private void lottoSizeValidate(String inputNumbers) {
+        String[] splitNumbers = inputNumbers.split(",");
+        if (splitNumbers.length != Constant.LOTTO_LENGTH) {
+            throw new IllegalArgumentException(
+                    Constant.ERROR_PREFIX + Constant.USER_NUMBER_PREFIX + Constant.ERROR_NOT_COUNT_MESSAGE);
+        }
+    }
 }

8a513a0a23fcbb33fbec969081abd24cec0e5386
siyeonSon <siyeon0209@naver.com>
2023-11-07 11:58:22 +0900

    feat(WinningNumbers): 당첨 번호가 1부터 45 사이 숫자가 아닐 경우 예외를 발생시킨다

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,13 +1,38 @@
 package lotto.model;
 
+import static lotto.util.message.ErrorMessages.WINNING_NUMBERS_RANGE_EXCEPTION;
+
 import java.util.List;
 
 public class WinningNumbers {
+    private static final Integer LOTTO_MIN_NUMBER = 1;
+    private static final Integer LOTTO_MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
     private final Integer bonusNumber;
 
     public WinningNumbers(List<Integer> numbers, Integer bonusNumber) {
+        validate(numbers, bonusNumber);
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, Integer bonusNumber) {
+        validateNumbersRange(numbers);
+        validateNumberRange(bonusNumber);
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        numbers.forEach(this::validateNumberRange);
+    }
+
+    private void validateNumberRange(Integer number) {
+        if (!isValidRange(number)) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_RANGE_EXCEPTION);
+        }
+    }
+
+    private boolean isValidRange(Integer number) {
+        return LOTTO_MIN_NUMBER <= number && number <= LOTTO_MAX_NUMBER;
     }
 }
--- src/main/java/lotto/util/message/ErrorMessages.java
+++ src/main/java/lotto/util/message/ErrorMessages.java
@@ -4,4 +4,5 @@ public class ErrorMessages {
     public static final String NUMBER_FORMAT_EXCEPTION = "[ERROR] 정수만 입력 가능합니다.";
     public static final String BLANK_EXCEPTION = "[ERROR] 공백 문자는 입력할 수 없습니다.";
     public static final String USER_MONEY_UNIT_EXCEPTION = "[ERROR] 구입 금액은 1000원 단위입니다.";
+    public static final String WINNING_NUMBERS_RANGE_EXCEPTION = "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
 }

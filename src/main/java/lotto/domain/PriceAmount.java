debfcb2cf2833804473f8789011ffc08f8efdb88
SungHoCha <tjdgh3725@gmail.com>
2023-11-08 04:24:06 +0900

    FEAT - "add PurchaseNumber logic in PriceAmount"
    PriceAmount 객체에 PurchaseNumber(구매횟수)구하는 로직 추가

--- src/main/java/lotto/domain/PriceAmount.java
+++ src/main/java/lotto/domain/PriceAmount.java
@@ -3,13 +3,13 @@ package lotto.domain;
 import org.assertj.core.api.ThrowableAssert;
 
 public class PriceAmount {
-    private int price;
+    private int priceAmount;
 
     public PriceAmount(int priceNumber) {
         if (!is1000Unit(priceNumber) || !isGreaterThanZero(priceNumber)) {
             throw new IllegalArgumentException("[ERROR] 금액은 1000단위로 입력해주세요.");
         }
-        this.price = priceNumber;
+        this.priceAmount = priceNumber;
     }
 
     private static boolean isGreaterThanZero(int priceNumber) {
@@ -18,5 +18,9 @@ public class PriceAmount {
 
     private static boolean is1000Unit(int priceNumber) {
         return priceNumber % 1000 == 0;
+    }
+
+    public int getPurchaseNumber() {
+        return priceAmount / 1000;
     }
 }
--- src/test/java/lotto/domain/PriceAmountTest.java
+++ src/test/java/lotto/domain/PriceAmountTest.java
@@ -11,4 +11,12 @@ public class PriceAmountTest {
     void 구매금액이_1000원단위가_아니면_에러발생() {
         assertThatThrownBy(() -> new PriceAmount(8001)).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 구매금액이_8000원이면_구매횟수는_8이어야한다() {
+        PriceAmount priceAmount = new PriceAmount(8000);
+        int purchaseNumber = priceAmount.getPurchaseNumber();
+        assertThat(purchaseNumber).isEqualTo(8);
+
+    }
 }

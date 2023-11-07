bdc213865671f1305404a868ca77bf1aaaa7c70a
minisyu <iswater.kim@gmail.com>
2023-11-07 23:37:57 +0900

    feat: OutputView에서 사용하는 상수 클래스 ViewConstants 생성

---
+++ src/main/java/lotto/util/ViewConstants.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+public class ViewConstants {
+    public static final String DELIMITER = ", ";
+    public static final String PREFIX = "[";
+    public static final String SUFFIX = "]";
+    public static final String TEMPLATE = """
+             
+                    당첨 통계
+                    ---
+                    3개 일치 (5,000원) - %d개
+                    4개 일치 (50,000원) - %d개
+                    5개 일치 (1,500,000원) - %d개
+                    5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+                    6개 일치 (2,000,000,000원) - %d개
+                    총 수익률은 %s%%입니다.
+            """.replaceAll("( ){2,}", "");
+
+    public static final String NUMBER_FORMAT_PATTERN = "###,###.0";
+    public static final double ZERO_PROFIT_RATE = 0.0;
+    public static final String ZERO_PROFIT_RATE_STRING = "0.0";
+}

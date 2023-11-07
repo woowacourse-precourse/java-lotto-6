45b05535b0699f0b1a0cfc4a7b3162803ea7ba2b
Hayoon <hy97@sju.ac.kr>
2023-11-07 15:43:09 +0900

    feat(LottoService): 로또 번호 랜덤 생성 후 오름차순 정렬(Collections.sort) 적용

--- src/main/java/lotto/model/service/LottoService.java
+++ src/main/java/lotto/model/service/LottoService.java
@@ -3,6 +3,7 @@ package lotto.model.service;
 import lotto.model.domain.Lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
@@ -11,7 +12,7 @@ public class LottoService {
 
     public List<Lotto> initLottoList(int purchaseAmount) {
         List<Lotto> lottoList = new ArrayList<>();
-        for(int i = 0; i < purchaseAmount; i++) {
+        for (int i = 0; i < purchaseAmount; i++) {
             List<Integer> lottoNumbers = pickLottoNumbers();
             Lotto lotto = new Lotto(lottoNumbers);
             lottoList.add(lotto);
@@ -21,6 +22,9 @@ public class LottoService {
     }
 
     public List<Integer> pickLottoNumbers() {
-        return pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> pickUniqueNumbersInRange = pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(pickUniqueNumbersInRange);
+
+        return pickUniqueNumbersInRange;
     }
 }

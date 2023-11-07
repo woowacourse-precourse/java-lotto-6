dc882b4a8837f2631845e8af38b39834934c1357
choiejae <ejae8319@gmail.com>
2023-11-07 21:17:27 +0900

    test(WinningLotto): 당첨 번호 로또 기능 테스트 작성
    - autoLotto 와 bonusNumber 가 있으면 WinningLotto 를 생성할 수 있어야 한다
    - bonusNumber 가 당첨번호에 포함되어 있으면 예외를 발생한다
    - winningLotto 에서 autoLotto 들과의 비교를 통해 결과를 반환할 수 있어야 한다

---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,53 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("winningNumbers 와 bonusNumber 로 객체를 생성할 수 있다.")
+    @Test
+    void generateClass() {
+        //given
+        Lotto winningNumbers = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = BonusNumber.from(7);
+
+        //when
+        WinningLotto winningLotto = WinningLotto.of(winningNumbers, bonusNumber);
+
+        //then
+        assertThat(winningLotto).isNotNull();
+    }
+
+    @DisplayName("winningNumbers 에 bonusNumber 가 포함되어 있으면 예외를 발생한다.")
+    @Test
+    void throwExceptionContainedBonusNumber() {
+        //given
+        Lotto winningNumbers = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = BonusNumber.from(1);
+
+        //when
+        //then
+        assertThatThrownBy(() -> WinningLotto.of(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(WinningLotto.CONTAINED_EXCEPTION);
+    }
+
+    @DisplayName("winningLotto 를 만들면 lotto 와 비교해 결과를 가져올 수 있다.")
+    @Test
+    void matchWinningLottoAndLotto() {
+        //given
+        Lotto winningNumbers = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = BonusNumber.from(1);
+
+        //when
+        //then
+        assertThatThrownBy(() -> WinningLotto.of(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(WinningLotto.CONTAINED_EXCEPTION);
+    }
+}

e4354e0d16e53d6ed6bb09f506135f3b2af50e40
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-07 14:27:00 +0900

    feat(LottoMachine): make method that prints the amount of winning lotto that has to be generated

--- src/main/java/lotto/view/SystemOutput.java
+++ src/main/java/lotto/view/SystemOutput.java
@@ -5,4 +5,10 @@ public class SystemOutput {
     // 발행된 로또들을 출력한다
     // rank에 계산된 값을 출력한다
     // rate 값을 출력한다
+
+    public static void printPurchaseAmount(int purchaseAmount) {
+        String OUTPUT_MESSAGE = "%d개를 구매했습니다.";
+
+        System.out.printf((OUTPUT_MESSAGE) + "%n",(purchaseAmount/1000));
+    }
 }

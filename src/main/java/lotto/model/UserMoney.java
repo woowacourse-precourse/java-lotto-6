066d8a86986b4d5dae8c1ae4d7ff7256a660e8bf
siyeonSon <siyeon0209@naver.com>
2023-11-07 11:06:05 +0900

    feat(UserMoney): 로또 구입 금액이 1000원 단위로 나누어 떨어지지 않을 경우 예외를 발생시킨다

--- src/main/java/lotto/model/UserMoney.java
+++ src/main/java/lotto/model/UserMoney.java
@@ -1,9 +1,18 @@
 package lotto.model;
 
+import static lotto.util.message.ErrorMessages.USER_MONEY_UNIT_EXCEPTION;
+
 public class UserMoney {
     int amount;
 
     public UserMoney(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(USER_MONEY_UNIT_EXCEPTION);
+        }
     }
 }
--- src/main/java/lotto/util/message/ErrorMessages.java
+++ src/main/java/lotto/util/message/ErrorMessages.java
@@ -3,4 +3,5 @@ package lotto.util.message;
 public class ErrorMessages {
     public static final String NUMBER_FORMAT_EXCEPTION = "[ERROR] 정수만 입력 가능합니다.";
     public static final String BLANK_EXCEPTION = "[ERROR] 공백 문자는 입력할 수 없습니다.";
+    public static final String USER_MONEY_UNIT_EXCEPTION = "[ERROR] 구입 금액은 1000원 단위입니다.";
 }

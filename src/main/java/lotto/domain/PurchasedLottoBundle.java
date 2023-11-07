1fb9a5a4bcabecd36b071c2dff6ef2da347e1bcb
JaehongDev <println@kakao.com>
2023-11-07 18:43:55 +0900

    refactor: PurchasedLottoBundle record -> 클래스로 변경 후 수량 및 타입 변경 기능 구현

--- src/main/java/lotto/domain/PurchasedLottoBundle.java
+++ src/main/java/lotto/domain/PurchasedLottoBundle.java
@@ -1,10 +1,40 @@
 package lotto.domain;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public record PurchasedLottoBundle(List<Lotto> value) {
-    public List<MatchResult> checkResult(AnswerLotto answerLotto) {
-        return value.stream().map(answerLotto::match)
-                .toList();
+public class PurchasedLottoBundle {
+    private final List<Lotto> lottoBundle;
+
+    public PurchasedLottoBundle(List<Lotto> lottoBundle) {
+        this.lottoBundle = lottoBundle;
+    }
+
+    public WinningResults checkWinningResults(AnswerLotto answerLotto) {
+        return new WinningResults(EnumSet.allOf(LottoRewardTable.class).stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        result -> lottoBundle.stream()
+                                .map(answerLotto::match)
+                                .map(LottoMatchingTable::of)
+                                .filter(m -> Objects.equals(result.getLottoMatchingTable(), m))
+                                .count(),
+                        (prev, next) -> next,
+                        () -> new EnumMap<>(LottoRewardTable.class)
+                )));
+    }
+
+    public int getQuantity() {
+        return lottoBundle.size();
+    }
+
+    public <T> List<T> convert(Function<Lotto, T> function) {
+        return lottoBundle.stream().map(function)
+                .collect(Collectors.toList());
+
     }
 }

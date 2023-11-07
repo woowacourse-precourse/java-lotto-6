0304101681e71b6705d0332f9363ec2a2852dccf
ljkhyeong <jolri24@gmail.com>
2023-11-07 17:37:05 +0900

    feat: LottoResult 클래스 추가
    로또 결과 상태정보를 갖기 위한 클래스

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,2 +1,37 @@
-package lotto.domain;public class LottoResult {
+package lotto.domain;
+
+public class LottoResult {
+
+	private final int matchCount;
+	private final boolean matchBonusNumber;
+	private final WinningRank winningRank;
+
+	public LottoResult(int matchCount, boolean matchBonusNumber) {
+		this.matchCount = matchCount;
+		this.matchBonusNumber = matchBonusNumber;
+		this.winningRank = rank(matchCount, matchBonusNumber);
+	}
+
+	public int getMatchCount() {
+		return matchCount;
+	}
+
+	public boolean isMatchBonusNumber() {
+		return matchBonusNumber;
+	}
+
+	private WinningRank rank(int matchCount, boolean matchBonusNumber) {
+		if (matchCount == 0)
+			return WinningRank.FAIL;
+		if (matchCount == 3)
+			return WinningRank.FIFTH;
+		if (matchCount == 4)
+			return WinningRank.FOURTH;
+		if (matchCount == 5 && !matchBonusNumber)
+			return WinningRank.THIRD;
+		if (matchCount == 5 && matchBonusNumber)
+			return WinningRank.SECOND;
+		if (matchCount == 6)
+			return WinningRank.FIRST;
+	}
 }

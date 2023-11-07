4eaaa364df349c9a1ea1fe11344deb2894a5aac1
깃wooqqq <sge0225@naver.com>
2023-11-08 04:47:14 +0900

    style: 필요없는 import 삭제

--- src/main/java/lotto/view/InputWinningNumberView.java
+++ src/main/java/lotto/view/InputWinningNumberView.java
@@ -2,7 +2,6 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.nio.channels.FileLockInterruptionException;
 import java.util.*;
 import java.util.regex.Pattern;
 

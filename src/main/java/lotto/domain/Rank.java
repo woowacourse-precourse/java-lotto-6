4b1133263c748f44a687359c1059ec0e48a5c4be
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 15:42:26 +0900

    feat: 일치번호 개수와 당첨금액을 정의하는 enum 추가

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,55 @@
+package lotto.domain;
+
+public enum Rank {
+
+    FIRST(6, 2000000000),
+    SECOND(5,30000000),
+    THRID(5,1500000),
+    FOURTH(4,50000),
+    FIFTH(3,5000),
+    NOTHING(0,0);
+
+    private static final int MIN_MATCH_COUNT=3;
+    private static final String ERROR_MESSAGE="[ERROR]";
+
+    private int matchCount;
+    private int winningMoney;
+
+    private static Rank valueOf(int matchNum, boolean matchBonus){
+        if(matchNum<MIN_MATCH_COUNT){
+            return NOTHING;
+        }
+
+        if(SECOND.match(matchNum) && matchBonus){
+            return SECOND;
+        }
+
+        for(Rank rank : values()){
+            if(rank.match(matchNum)){
+                return rank;
+            }
+        }
+
+        throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
+
+    Rank(int matchCount, int winningMoney)
+    {
+        this.matchCount=matchCount;
+        this.winningMoney= winningMoney;
+    }
+
+    public int getMatchCount(){
+        return matchCount;
+    }
+
+    public int getWinningMoney(){
+        return winningMoney;
+    }
+
+    private boolean match(int countOfMatch){
+        return matchCount ==countOfMatch;
+    }
+
+
+}

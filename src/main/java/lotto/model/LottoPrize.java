cc32c5e1b35e5d20e53c059935f387704d1d0d83
Sanghwi Lee <sanghwi47@naver.com>
2023-11-08 00:14:21 +0900

    refactor(LottoPrize) 메소드 오버라이딩을 통해 중복된 상수를 설정하던 코드를 개선, switch/case 구문의 삭제 chore() 메소드 및 상수 순서 정렬, 코드정렬

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -1,29 +1,54 @@
 package lotto.model;
 
 public enum LottoPrize {
-    THREE_MATCH(3, 5_000),
-    FOUR_MATCH(4, 50_000),
-    FIVE_MATCH(5, 1_500_000),
-    FIVE_AND_BONUS_MATCH(-1, 30_000_000),
-    SIX_MATCH(6, 2_000_000_000);
+    THREE_MATCH(3, 5_000) {
+        @Override
+        public String getDescription() {
+            return "3개 일치";
+        }
+    },
+    FOUR_MATCH(4, 50_000) {
+        @Override
+        public String getDescription() {
+            return "4개 일치";
+        }
+    },
+    FIVE_MATCH(5, 1_500_000) {
+        @Override
+        public String getDescription() {
+            return "5개 일치";
+        }
+    },
+    FIVE_AND_BONUS_MATCH(-1, 30_000_000) {
+        @Override
+        public String getDescription() {
+            return "5개 일치, 보너스 볼 일치";
+        }
+    },
+    SIX_MATCH(6, 2_000_000_000) {
+        @Override
+        public String getDescription() {
+            return "6개 일치";
+        }
+    };
 
     private final int matchCount;
     private final long prize;
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public long getPrize() {
+        return prize;
+    }
 
     LottoPrize(int matchCount, long prize) {
         this.matchCount = matchCount;
         this.prize = prize;
     }
 
-    public String getDescription() {
-        return switch (this) {
-            case THREE_MATCH -> "3개 일치";
-            case FOUR_MATCH -> "4개 일치";
-            case FIVE_MATCH -> "5개 일치";
-            case FIVE_AND_BONUS_MATCH -> "5개 일치, 보너스 볼 일치";
-            case SIX_MATCH -> "6개 일치";
-        };
-    }
+    public abstract String getDescription();
 
     public static LottoPrize valueOf(int matchCount) {
         for (LottoPrize prize : values()) {
@@ -32,14 +57,5 @@ public enum LottoPrize {
             }
         }
         return null;
-    }
-
-
-    public int getMatchCount() {
-        return matchCount;
-    }
-
-    public long getPrize() {
-        return prize;
     }
 }

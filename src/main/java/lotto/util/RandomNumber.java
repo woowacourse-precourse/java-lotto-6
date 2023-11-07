4d750478354e53247581ab753c802f287ef45ba8
jihun <pjh2996@naver.com>
2023-11-07 13:48:53 +0900

    fix : UnsupportedOperationException 오류 해결
    - 랜덤 숫자 생성 시 List에 new ArrayList() 할당 후에 랜덤 값 저장

--- src/main/java/lotto/util/RandomNumber.java
+++ src/main/java/lotto/util/RandomNumber.java
@@ -5,14 +5,16 @@ import static lotto.util.message.Digit.LOTTO_RANGE_START;
 import static lotto.util.message.Digit.LOTTO_SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumber {
 
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_START.getNumber(), LOTTO_RANGE_END.getNumber(),
-                LOTTO_SIZE.getNumber()
-        );
+        List<Integer> randomNumber = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_START.getNumber(), LOTTO_RANGE_END.getNumber(),
+                        LOTTO_SIZE.getNumber()));
+        return randomNumber;
     }
-
+    
 }

1925b7ea016669bf412ea3b793b872eb35497afd
다운 <j_d_woon@naver.com>
2023-11-07 15:32:47 +0900

    feat: 당첨번호 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 
+import static lotto.utils.Message.INPUT_WINNING_NUMBER;
 import static lotto.utils.Message.PURCAHSE_AMOUNT_LOTTO;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -8,6 +9,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public String buyLotto() {
         System.out.println(PURCAHSE_AMOUNT_LOTTO);
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
         return Console.readLine();
     }
 

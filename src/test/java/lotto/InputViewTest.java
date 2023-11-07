72449500b62d565abdd2b5835ae1b07c3c084630
watermelon3737 <pshrina1@naver.com>
2023-11-08 02:07:24 +0900

    test : inputView 변경에 따른 테스트 변경

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -5,6 +5,8 @@ import lotto.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 public class InputViewTest {
@@ -20,8 +22,8 @@ public class InputViewTest {
 
         //then
         assertThatThrownBy(() -> inputView.validateCustomerMoneyInput(moneyInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.EMPTY.errorMessage)
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage(ErrorMessage.NOT_DIGIT_MONEY.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
     }
 
@@ -38,17 +40,17 @@ public class InputViewTest {
 
         //then
         assertThatThrownBy(() -> inputView.validateCustomerMoneyInput(englishInMoney))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_MONEY.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
 
         assertThatThrownBy(() -> inputView.validateCustomerMoneyInput(koreanInMoney))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_MONEY.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
 
         assertThatThrownBy(() -> inputView.validateCustomerMoneyInput(specialSignInMoney))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_MONEY.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
     }
@@ -79,24 +81,8 @@ public class InputViewTest {
 
         //then
         assertThatThrownBy(() -> inputView.validateWinningNumberInput(winningNumberInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.EMPTY.errorMessage)
-                .hasMessageContaining(ERROR_PHRASES);
-    }
-
-    @DisplayName("당첨 번호 입력 값이 \',\'로 구분되어 있지 않을 경우")
-    @Test
-    void validateDelimiterWinningNumberInput() {
-        //given
-        InputView inputView = new InputView();
-
-        //when
-        String winningNumberInput = "123456";
-
-        //then
-        assertThatThrownBy(() -> inputView.validateWinningNumberInput(winningNumberInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.DELIMITER.errorMessage)
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage(ErrorMessage.NOT_DIGIT_WINNING_NUM.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
     }
 
@@ -110,8 +96,8 @@ public class InputViewTest {
         String winningNumbersInput = "1,2,3,4,5,a";
 
         //then
-        assertThatThrownBy(() -> inputView.validateEachWinningNumberInput(winningNumbersInput))
-                .isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> inputView.validateWinningNumberInput(winningNumbersInput))
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_WINNING_NUM.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
     }
@@ -124,11 +110,11 @@ public class InputViewTest {
 
         //when
         String winningNumberInput = "1,2,3,4,5,6";
-        String[] winningNumber = inputView.validateWinningNumberInput(winningNumberInput);
+        List<Integer> winningNumber= inputView.validateWinningNumberInput(winningNumberInput);
 
         //then
-        String[] expectedWinningNumber = {"1", "2", "3", "4", "5", "6"};
-        assertThat(winningNumber.length).isEqualTo(6);
+        List<Integer> expectedWinningNumber = List.of(1, 2, 3, 4, 5, 6);
+        assertThat(winningNumber.size()).isEqualTo(6);
         assertThat(winningNumber).isEqualTo(expectedWinningNumber);
     }
 
@@ -143,8 +129,8 @@ public class InputViewTest {
 
         //then
         assertThatThrownBy(() -> inputView.validateBonusNumberInput(bonusNumberInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.EMPTY.errorMessage)
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage(ErrorMessage.NOT_DIGIT_BONUS_NUM.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
     }
 
@@ -161,17 +147,17 @@ public class InputViewTest {
 
         //then
         assertThatThrownBy(() -> inputView.validateBonusNumberInput(englishInBonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_BONUS_NUM.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
 
         assertThatThrownBy(() -> inputView.validateBonusNumberInput(koreanInBonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_BONUS_NUM.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
 
         assertThatThrownBy(() -> inputView.validateBonusNumberInput(specialSignInBonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.NOT_DIGIT_BONUS_NUM.errorMessage)
                 .hasMessageContaining(ERROR_PHRASES);
     }

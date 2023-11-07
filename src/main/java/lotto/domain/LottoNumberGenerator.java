417a1edcf9adac1dd28f6ea2b3089d0a5a1e8070
KDT live <dnjsxo0616@gmail.com>
2023-11-08 01:44:11 +0900

    feat(LottoNumberGenerator): 로또 번호 생성 클래스 추가
    - MIN_NUMBER에서 MAX_NUMBER 범위 내에서 중복되지 않는 COUNT_NUMBER 개의 로또 번호를 생성하는 createLottoNumber() 메서드 추가.

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoNumberGenerator {
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+    private static final int COUNT_NUMBER = 6;
+    private List<Integer> createLottoNumber(){
+       return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT_NUMBER);
+    }
+}

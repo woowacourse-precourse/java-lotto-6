95fdcec0ccdfb0999783b6cfcc73cd9edf56ae09
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-07 14:17:02 +0900

    feat(winningStatistics): 구입금액을 받아 수익률을 계산하는 메서드 생성

--- src/main/java/lotto/WinningStatistics.java
+++ src/main/java/lotto/WinningStatistics.java
@@ -3,4 +3,9 @@ package lotto;
 import java.util.Map;
 
 public record WinningStatistics(Map<Ranking, Integer> rankingNumber) {
+    public double getReturnRage(int amount) {
+        return rankingNumber.entrySet().stream()
+                .mapToDouble(entry -> entry.getKey().getPrize() * entry.getValue())
+                .sum() / amount * 100;
+    }
 }

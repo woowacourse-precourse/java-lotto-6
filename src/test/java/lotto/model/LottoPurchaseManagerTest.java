28d5c7c40e3291cbfec96e9f3e528d0413376389
Yeriimii <seojs0511@naver.com>
2023-11-07 14:55:22 +0900

    test(LottoPurchaseManagerTest): 구입 수량만큼 로또 용지 확인 테스트 작성

--- src/test/java/lotto/model/LottoPurchaseManagerTest.java
+++ src/test/java/lotto/model/LottoPurchaseManagerTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoPurchaseManagerTest {
@@ -20,5 +23,27 @@ class LottoPurchaseManagerTest {
 
         // then
         assertEquals(expectedPurchaseQuantity, calculatePurchaseQuantity);
+    }
+
+    @DisplayName("구입한 로또 용지의 로또 번호 확인")
+    @ParameterizedTest(name = "새로운 {0}개의 로또 용지 번호 확인")
+    @CsvSource(value = {"25000", "50000"})
+    public void 구입한_로또_용지의_로또_번호_확인(int purchaseQuantity) {
+        // given
+        LottoPurchaseManager lottoPurchaseManager = new LottoPurchaseManager(new LottoNumberGenerator());
+        int minLottoNumber = 1;
+        int maxLottoNumber = 45;
+
+        // when
+        List<LottoPaper> purchasedLottoPapers = lottoPurchaseManager.purchaseLottoPapers(purchaseQuantity);
+
+        // then
+        assertAll(
+                "구입한 로또 용지 개수 확인과 로또 번호가 1부터 45임을 확인",
+                () -> assertEquals(purchaseQuantity, purchasedLottoPapers.size()),
+                () -> assertThat(purchasedLottoPapers)
+                        .flatExtracting(LottoPaper::getLottoNumbers)
+                        .allMatch(number -> number >= minLottoNumber && number <= maxLottoNumber)
+        );
     }
 }

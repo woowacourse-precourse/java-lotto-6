44294e06238ea9d1c25d5954201ffda454ccbc3b
psionic0324 <psionic0324@naver.com>
2023-11-08 00:45:02 +0900

    refactor: 출력문 printf에서 format형식으로 변경
    코드 가독성 측면에서 format을 사용하는것이 더 좋을것 같아서 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,12 +4,16 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.model.Lotto;
+import lotto.model.Prize;
 
 public class OutputView {
 
+    private static final String PRIZE_FORMAT = "%d개 일치 (%s) - %d개\n";
+    private static final String PRIZE_BONUS_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+
     public void printUserLotto(List<Lotto> lottos) {
         System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
-        for(Lotto lotto : lottos) {
+        for (Lotto lotto : lottos) {
             System.out.println(
                     lotto.getLotto().stream()
                             .sorted(Comparator.naturalOrder())
@@ -18,14 +22,14 @@ public class OutputView {
         }
     }
 
-    public void printWinningStatistics(List<Integer> prizes, double TotalReturn) {
+    public void printWinningStatistics(List<Integer> prizes, double totalReturn) {
         System.out.println("\n당첨 통계");
         System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) - %d개", prizes.get(4)).println();
-        System.out.printf("4개 일치 (50,000원) - %d개", prizes.get(3)).println();
-        System.out.printf("5개 일치 (1,500,000원) - %d개", prizes.get(2)).println();
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", prizes.get(1)).println();
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개", prizes.get(0)).println();
-        System.out.printf("총 수익률은 %.1f%%입니다.", TotalReturn).println();
+        System.out.format(PRIZE_FORMAT, Prize.FIFTH.getMatchNum(), "5,000원", prizes.get(4));
+        System.out.format(PRIZE_FORMAT, Prize.FOURTH.getMatchNum(), "50,000원", prizes.get(3));
+        System.out.format(PRIZE_FORMAT, Prize.THIRD.getMatchNum(), "1,500,000원", prizes.get(2));
+        System.out.format(PRIZE_BONUS_FORMAT, Prize.SECOND.getMatchNum(), "30,000,000원", prizes.get(1));
+        System.out.format(PRIZE_FORMAT, Prize.FIRST.getMatchNum(), "2,000,000,000원", prizes.get(0));
+        System.out.format("총 수익률은 %.1f%%입니다.", totalReturn);
     }
 }

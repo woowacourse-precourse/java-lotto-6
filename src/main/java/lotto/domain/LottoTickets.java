467f87e3ba3c9fbdd9e76f829942cb70198f40e5
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-07 21:33:32 +0900

    feat(LottoTickets) : 각 로또들의 LottoRank를 구하는 기능

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoTickets {
 
@@ -13,6 +15,16 @@ public class LottoTickets {
         for (int i = 0; i < numberOfLottoTickets; i++) {
             buyLottoTickets();
         }
+    }
+
+    public List<LottoRank> getWiningRanks(WinningLottoNumbers winningLottoNumbers) {
+        return IntStream.range(0, numberOfLottoTickets)
+                .mapToObj(i -> getWiningRank(winningLottoNumbers, i))
+                .collect(Collectors.toList());
+    }
+
+    private LottoRank getWiningRank(WinningLottoNumbers winningLottoNumbers, int index) {
+        return LottoRank.get(winningLottoNumbers, lottoTickets.get(index));
     }
 
     private void buyLottoTickets() {

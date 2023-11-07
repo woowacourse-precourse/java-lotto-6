fbb1471a7b8892ae7c1b3b007b6618e26c02d81f
1027bestlife <1027bestlife@gmail.com>
2023-11-08 04:17:08 +0900

    feat: 구매 수량 출력 기능, 오름 차순 정렬 기능 추가
    - 입력 받은 구입 금액에 맞는 수량 대로 한 줄씩 오름 차순 으로 정렬 하여 출력 하는 메서드 추가
    - 메인 메서드 내용 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,13 @@
 package lotto;
 
+import static lotto.QuantityDisplayManager.printPurchaseQuantity;
 import static lotto.UserInputHandler.getLottoPurchaseAmount;
 import static lotto.UserInputHandler.requestPurchaseAmount;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,5 +16,10 @@ public class Application {
         int purchaseAmount = getLottoPurchaseAmount();
         int purchaseQuantity = purchaseAmount / 1000;
         System.out.println(purchaseQuantity+"개를 구매했습니다.");
+        List<List<Integer>> numbers = printPurchaseQuantity(purchaseQuantity);
+        int i = 0;
+        for(i = 0 ; i < purchaseQuantity ; i++){
+            System.out.println(numbers.get(i));
+        }
     }
 }
--- src/main/java/lotto/QuantityDisplayManager.java
+++ src/main/java/lotto/QuantityDisplayManager.java
@@ -1,5 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class QuantityDisplayManager {
+    public static List<List<Integer>> printPurchaseQuantity(int purchaseQuantity) {
+        List<List<Integer>> numbers = new ArrayList<List<Integer>>();
+        for (int i = 0; i < purchaseQuantity; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(randomNumbers);
+            numbers.add(randomNumbers);
+        }
+        return numbers;
+    }
 
 }

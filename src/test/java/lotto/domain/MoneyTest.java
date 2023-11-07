b63c518d7aa7e85e6ca30967cdaaaa78d336bb38
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 15:22:39 +0900

    refactor(MoneyTest): 테스트 코드 파라미터 타입 변환
    입력 함수는 String으로 입력을 받기 때문에 Money를 생성할때에도
    String으로 입력 받게 된다.

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class MoneyTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1500, 1200, 3300, 7777})
+    @ValueSource(strings = {"1500", "1200", "3300", "7777"})
     @DisplayName("로또 구입 금액은 1,000으로 나누어 떨어지지 않으면 예외 처리")
-    void lottoSpentMoneydividedBy1000(int inputMoney) {
+    void lottoSpentMoneydividedBy1000(String inputMoney) {
         Assertions.assertThatThrownBy(() -> new Money(inputMoney))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]")
@@ -18,12 +18,12 @@ public class MoneyTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 500, 110000, 200000})
+    @ValueSource(strings = {"0", "500", "110000", "200000"})
     @DisplayName("로또 구입 금액은 1,000보다 작거나 100,000보다 크면 예외 처리")
-    void lottoSpentMoneyRange(int inputMoney) {
+    void lottoSpentMoneyRange(String inputMoney) {
         Assertions.assertThatThrownBy(() -> new Money(inputMoney))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]")
-                .hasMessageContaining("구입 금액은 1,000원이상 부터 10만원 까지 가능합니다.");
+                .hasMessageContaining("구입 금액은 1,000원이상 부터 10만원 까지 가능합니.");
     }
 }

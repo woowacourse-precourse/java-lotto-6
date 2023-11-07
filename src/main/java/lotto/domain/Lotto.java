ff0ac2a7abd4a4bda8788491b32e5867cb287124
ming-taro <hera002008@naver.com>
2023-11-08 02:33:10 +0900

    feat: Lotto객체 내에서 당첨 번호 일치 개수 계산

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,6 +16,19 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public int calcMatchNumber(List<Integer> numbersToCompare) {
+        Set<Integer> winningNumbers = new HashSet<>(numbersToCompare);
+        int matchCount = 0;
+
+        for(int number: numbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount += 1;
+            }
+        }
+
+        return matchCount;
     }
 
     public List<Integer> getNumbers() {

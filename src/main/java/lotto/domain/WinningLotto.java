2bdd6f0deda29fb0e990e76e76dbeaf69d48de11
limzzum <chfhddl1234@naver.com>
2023-11-08 00:32:28 +0900

    :sparkles: feat : 당첨 복권 생성 시 검증 메서드 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.validation.LottoValidation.validate_lottoNum;
+
 import java.util.List;
 
 public class WinningLotto extends Lotto{
@@ -9,5 +11,12 @@ public class WinningLotto extends Lotto{
     public WinningLotto(List<Integer> numbers, int bonusNum) {
         super(numbers);
         this.bonusNum = bonusNum;
+        validateWinningLottoNums(numbers, bonusNum);
     }
+
+    private void validateWinningLottoNums(List<Integer> numbers, int bonusNum) {
+        numbers.add(bonusNum);
+        validate_lottoNum(numbers);
+    }
+
 }

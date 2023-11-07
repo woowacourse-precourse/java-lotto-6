2181f6c4ddca68b3e2f63aa5f474a7fcb933740e
dbstn1325 <dbstn1325@naver.com>
2023-11-07 12:32:43 +0900

    test(BonusNumberTest): 보너스 번호가 1~45의 숫자가 아니면 IllegalArgumentException 예외처리 테스트 검증

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,9 +11,18 @@ public class BonusNumberTest {
     @ParameterizedTest
     @ValueSource(strings = {"one", "two", "three", "yoon", "su"})
     void 보너스_번호가_숫자가_아니면_IllegalArgumentException_예외처리_검증(String bonusNumberInput) {
-        assertThatThrownBy(() ->BonusNumber.from(bonusNumberInput))
+        assertThatThrownBy(() -> BonusNumber.from(bonusNumberInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 숫자여야 합니다.");
-
     }
+
+    @DisplayName("보너스 번호가 1~45의 숫자가 아니면 IllegalArgum햐entException로 예외처리하는지 검증")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46", "47", "250", "100000"})
+    void 보너스_번호가_숫자_IllegalArgumentException_예외처리_검증(String bonusNumberInput) {
+        assertThatThrownBy(() -> BonusNumber.from(bonusNumberInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1~45의 숫자여야 합니다.");
+    }
+
 }

eea950b36e01a3480e674264ea4e57c10802e1fd
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 03:05:06 +0900

    refactor : static으로 바꾸어주기

--- src/main/java/lotto/util/StringToListConverter.java
+++ src/main/java/lotto/util/StringToListConverter.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringToListConverter {
-    public List<Integer> stringToList(String input) {
+    public static List<Integer> stringToList(String input) {
         List<Integer> userNum = new ArrayList<>();
         for(String s : input.split(",")){
             int number = Integer.parseInt(s);
--- src/main/java/lotto/view/InputViewWinnerNumber.java
+++ src/main/java/lotto/view/InputViewWinnerNumber.java
@@ -7,13 +7,12 @@ import java.util.List;
 
 public class InputViewWinnerNumber extends InputView {
     private static final String WINNER_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private StringToListConverter stringToListConverter = new StringToListConverter();
 
-    public List<Integer> getWinnerNumber() {
+    public static List<Integer> getWinnerNumber() {
         System.out.println(WINNER_NUMBER_MESSAGE);
         String input = getInputValue();
         new WinnerNumberException(input);
-        List<Integer> winnerNum = stringToListConverter.stringToList(input);
+        List<Integer> winnerNum = StringToListConverter.stringToList(input);
         return winnerNum;
     }
 }

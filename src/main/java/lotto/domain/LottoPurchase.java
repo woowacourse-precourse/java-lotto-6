f77857e51945dbbd0776f8e1297f065205467453
sw801733 <sw801733@naver.com>
2023-11-07 21:36:21 +0900

    feat : generateRandomNumbers 기능 구현
    - LottoPurchase의 generateRandomNumbers 기능 구현

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -1,17 +1,32 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoPurchase {
     private final int LottoPurchaseCnt;
+    private final List<List<Integer>> RandomNumbers;
 
     public LottoPurchase(int LottoPurchaseCnt){
         validate(LottoPurchaseCnt);
         this.LottoPurchaseCnt = LottoPurchaseCnt;
+        this.RandomNumbers = generateRandomNumbers();
     }
 
     private void validate(int LottoPurchaseCnt) {
         return;
+    }
+
+    private List<List<Integer>> generateRandomNumbers() {
+        List<List<Integer>> RandomNumbers = new ArrayList<>();
+
+        for(int i = 0; i < this.LottoPurchaseCnt; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            RandomNumbers.add(numbers);
+        }
+        return RandomNumbers;
+
     }
 
 }

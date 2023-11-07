fd656bffbf6c47f6785f3166fbd608055daad686
whereami0404 <whereami2048@gmail.com>
2023-11-08 03:36:03 +0900

    refactor: 입력 메소드 로직 예외 처리 기능 포함으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,9 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import lotto.exception.InputExceptionConstant;
+import lotto.util.InputValidator;
+
 public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
@@ -9,17 +12,44 @@ public class InputView {
     private static final String INPUT_SPLITTER = ",";
 
     public String inputPurchaseAmount() {
-        System.out.println(ASK_PURCHASE_AMOUNT);
-        return readLine();
+        while (true) {
+            try {
+                System.out.println(ASK_PURCHASE_AMOUNT);
+                String input = readLine();
+                InputValidator.validatePurchaseNumber(input);
+
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(InputExceptionConstant.ONLY_NATURAL_NUMBER_IN_STRING);
+            }
+        }
     }
 
     public String[] inputWinningNumbers() {
-        System.out.println(ASK_WINNING_NUMBERS);
-        return readLine().split(INPUT_SPLITTER);
+        while (true) {
+            try {
+                System.out.println(ASK_WINNING_NUMBERS);
+                String[] inputNumbers = readLine().split(INPUT_SPLITTER);
+                InputValidator.isNaturalNumbers(inputNumbers);
+
+                return inputNumbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(InputExceptionConstant.ONLY_NATURAL_NUMBER_IN_STRING_ARRAY.getText());
+            }
+        }
     }
 
     public String inputBonusNumber() {
-        System.out.println(ASK_BONUS_NUMBER);
-        return readLine();
+        while (true) {
+            try {
+                System.out.println(ASK_BONUS_NUMBER);
+                String input = readLine();
+                InputValidator.isOneNaturalNumber(input);
+
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(InputExceptionConstant.ONLY_NATURAL_NUMBER_IN_STRING.getText());
+            }
+        }
     }
 }

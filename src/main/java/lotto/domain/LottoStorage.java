13099d8f3d94afa6d2f9e05e65becc972794da2a
kimdoha <kkhhjj3393@gmail.com>
2023-11-07 18:44:57 +0900

    feat(LottoStorage): 로또 당첨 내역 및 수익률 출력 구현

--- src/main/java/lotto/domain/LottoStorage.java
+++ src/main/java/lotto/domain/LottoStorage.java
@@ -3,9 +3,10 @@ package lotto.domain;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import lotto.domain.dto.LottoNumberCompareResult;
-import lotto.domain.dto.LottoRankResult;
+import lotto.util.LottoGamePrinter;
+import lotto.util.LottoProfitCalculator;
+import lotto.util.TextBuilder;
 
 public class LottoStorage {
 
@@ -13,28 +14,52 @@ public class LottoStorage {
     private final WinningLotto winningLotto;
 
 
-    public LottoStorage(AutomaticLottoStorage automaticLottoStorage, WinningLotto winningLotto) {
+    private LottoStorage(AutomaticLottoStorage automaticLottoStorage, WinningLotto winningLotto) {
         this.automaticLottoStorage = automaticLottoStorage;
         this.winningLotto = winningLotto;
     }
 
-    public List<LottoRankResult> finalAllLottoRanks(List<LottoNumberCompareResult> compareResults) {
-        return compareResults.stream()
-                .map(LottoRank::findLottoRank)
-                .filter(Objects::nonNull)
-                .map(LottoRankResult::from)
-                .collect(Collectors.toList());
+    public static LottoStorage of(AutomaticLottoStorage automaticLottoStorage, WinningLotto winningLotto) {
+        return new LottoStorage(automaticLottoStorage, winningLotto);
     }
 
-    public List<LottoNumberCompareResult> compareAllAutomaticLottoWithWinningNumbers() {
+    public void showAllLottoRankResult(List<LottoRank> lottoRanks) {
+        TextBuilder textBuilder = TextBuilder
+                .fromString("당첨 통계").appendLineSeparator()
+                .append("---").appendLineSeparator();
+
+        for(LottoRank lottoRank : LottoRank.values()) {
+            textBuilder.appendInteger(lottoRank.getMatchingCount()).append("개 일치");
+
+            if(lottoRank.isBonusIncluded()) {
+                textBuilder.append(", 보너스 볼 일치");
+            }
+
+            textBuilder.append(" (").appendPriceAmount(lottoRank.getPrizeAmount()).append(")");
+
+            int count = (int) lottoRanks.stream().filter(lottoRank::equals).count();
+            textBuilder.append(" - ").appendInteger(count).append("개").appendLineSeparator();
+        }
+
+        LottoGamePrinter.println(textBuilder.build());
+    }
+
+    public void showLottoProfitRate(List<LottoRank> lottoRanks) {
+        Double profit = LottoProfitCalculator.calculate(lottoRanks, automaticLottoStorage.getTotalPrice());
+
+        TextBuilder textBuilder = TextBuilder.fromString("총 수익률은 ").appendDouble(profit).append("%입니다.").appendLineSeparator();
+        LottoGamePrinter.println(textBuilder.build());
+    }
+
+    public List<LottoRank> compareAllAutomaticLottoWithWinningNumbers() {
 
         return automaticLottoStorage.getAutomaticLottos().stream()
                 .map(automaticLotto -> compareWithWinningNumbers(automaticLotto.getNumbers()))
-                .collect(Collectors.toList());
+                .map(LottoRank::findLottoRank)
+                .filter(Objects::nonNull).toList();
     }
 
-    // util 라이브러리로 리팩토링
-    public LottoNumberCompareResult compareWithWinningNumbers(List<Integer> automaticLottoNumbers) {
+    private LottoNumberCompareResult compareWithWinningNumbers(List<Integer> automaticLottoNumbers) {
 
         int matchingCount = (int) automaticLottoNumbers.stream().filter(winningLotto::isWinningNumber).count();
         boolean bonusIncluded = false;

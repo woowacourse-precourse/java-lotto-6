5cd8ef48432d7d80a4fae0c97ca55a8866626460
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 22:41:26 +0900

    feat: OutputView 출력 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,13 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.model.Constants.INVALID_MONEY_FORMAT;
+
 public class OutputView {
     private static final OutputView instance = new OutputView();
 
@@ -8,5 +16,21 @@ public class OutputView {
     }
 
     private OutputView() {
+    }
+
+    public void printBuyLottoCount(int total, List<Lotto> lottoList){
+        System.out.println();
+        System.out.println(total + "개를 구매했습니다.");
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void printGradeResult(String gradeComment, int count){
+        System.out.println(gradeComment + count + "개");
+    }
+
+    public void printRateOfReturn(String rateOfReturn){
+        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }

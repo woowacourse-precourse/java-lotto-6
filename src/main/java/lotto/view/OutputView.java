eb7d50388708e31619012d24838a69ebdec0a974
wda067 <mg6359@naver.com>
2023-11-08 00:31:57 +0900

    refactor(OutputView): 변수를 enum에서 불러오도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,21 +1,21 @@
 package lotto.view;
 
+import static lotto.enums.Message.OUTPUT_COUNT_UNIT;
+import static lotto.enums.Message.OUTPUT_DIVIDING_LINE;
+import static lotto.enums.Message.OUTPUT_PROFIT_RATIO_MESSAGE;
+import static lotto.enums.Message.OUTPUT_PURCHASE_QUANTITY_MESSAGE;
+import static lotto.enums.Message.OUTPUT_WINNING_STATISTICS_MESSAGE;
+
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
-import lotto.LottoRank;
+import lotto.enums.LottoRank;
 
 public class OutputView {
-    private static final String PURCHASE_QUANTITY_MESSAGE = "개를 구매했습니다.";
-    private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
-    private static final String DIVIDING_LINE = "---";
-    private static final String COUNT_UNIT = "개";
-
-    private static final String PROFIT_RATIO_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public void printPurchaseQuantity(int purchaseQuantity) {
         System.out.println();
-        System.out.println(purchaseQuantity + PURCHASE_QUANTITY_MESSAGE);
+        System.out.println(purchaseQuantity + OUTPUT_PURCHASE_QUANTITY_MESSAGE.getMessage());
     }
 
     public void printPurchasedLottoNumbers(List<List<Integer>> lottoNumbers) {
@@ -25,17 +25,17 @@ public class OutputView {
     }
 
     public void printWinningStatistics(EnumMap<LottoRank, Integer> lottoRakingMap) {
-        System.out.println(WINNING_STATISTICS_MESSAGE);
-        System.out.println(DIVIDING_LINE);
+        System.out.println(OUTPUT_WINNING_STATISTICS_MESSAGE.getMessage());
+        System.out.println(OUTPUT_DIVIDING_LINE.getMessage());
 
         Arrays.stream(LottoRank.values())
                 .filter(rank -> rank != LottoRank.LAST_PLACE)
                 .forEach(rank ->
                         System.out.println(rank.getOutputMessage()
-                                + lottoRakingMap.get(rank) + COUNT_UNIT));
+                                + lottoRakingMap.get(rank) + OUTPUT_COUNT_UNIT.getMessage()));
     }
 
     public void printProfitRatio(double profit) {
-        System.out.printf(PROFIT_RATIO_MESSAGE, profit);
+        System.out.printf(OUTPUT_PROFIT_RATIO_MESSAGE.getMessage(), profit);
     }
 }

2f4186df5a16dfb15ca956e8424e6eef57562220
whereami2048 <whereami2048@gmail.com>
2023-11-07 20:27:27 +0900

    test: 구매 가능 로또 개수 계산 기능 테스트 코드 추가

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.util.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +16,18 @@ class PurchaseAmountTest {
         //then
         assertThatThrownBy(() -> {
             PurchaseAmount purchaseAmount = new PurchaseAmount(inputPrice);
-        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 로또 구입 금액이 로또 1장의 가격으로 나누어 떨어지지 않습니다.");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 구입 금액이 로또 1장의 가격으로 나누어 떨어지지 않습니다.");
+    }
+
+    @DisplayName("입력 받은 금액에 따른 구매 가능 로또 개수 계산 기능 검증")
+    @Test
+    void getLottoCount() {
+        //given
+        int inputPrice = 14000;
+        int lottoCount = new PurchaseAmount(inputPrice).getLottoCount();
+        //when
+        assertThat(lottoCount).isEqualTo(14);
+        //then
     }
 }

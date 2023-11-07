8013d5c65c132bb8c2034a803c5792ddbdeb276a
mainfn <_@mainfn.dev>
2023-11-08 00:32:01 +0900

    docs: 설명 첨부

--- src/main/java/numbers/SizedNumbersGenerator.java
+++ src/main/java/numbers/SizedNumbersGenerator.java
@@ -3,6 +3,9 @@ package numbers;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
+/**
+ * 정해진 크기의 unique한 랜덤 숫자를 생성하는 배열 생성기
+ */
 public final class SizedNumbersGenerator {
     private final int startInclusive;
     private final int endInclusive;

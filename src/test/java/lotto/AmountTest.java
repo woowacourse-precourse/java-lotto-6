f697afa1ca67b408205c4af8ce5027483faf87d7
Son JunHyung <kingjh1125@gmail.com>
2023-11-07 23:02:28 +0900

    test (AmountTest): 테스트 추가
    로또 구입 금액에 대한 테스트를 추가하였습니다:
    - 로또 구입 금액을 입력하지 않는 경우
    - 로또 구입 금액이 공백일 경우
    - 로또 구입 금액이 숫자가 아닌 경우

--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -8,22 +8,49 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AmountTest extends NsTest {
-
     private static final String ERROR_MESSAGE = "[ERROR]";
-    @DisplayName("로또 구입 금액이 0 이하면 예외가 발생한다.")
-    @Test
-    void createAmountByNegative() {
-        assertSimpleTest(() -> {
-            runException("-1");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
-    }
 
     @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
     @Test
     void createAmountByDivideNumber() {
         assertSimpleTest(() -> {
             runException("100");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 구입 금액을 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void createAmountByNull() {
+        assertSimpleTest(() -> {
+            runException(System.lineSeparator());
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 구입 금액이 공백이면 예외가 발생한다.")
+    @Test
+    void createAmountByBlank() {
+        assertSimpleTest(() -> {
+            runException(" ");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 구입 금액이 숫자가 아닌 값이면 예외가 발생한다.")
+    @Test
+    void createAmountByCharacter() {
+        assertSimpleTest(() -> {
+            runException("a");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 구입 금액이 0 이하면 예외가 발생한다.")
+    @Test
+    void createAmountByNegative() {
+        assertSimpleTest(() -> {
+            runException("-1");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }

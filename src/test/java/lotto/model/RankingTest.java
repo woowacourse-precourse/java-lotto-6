c79a0ca697c58f6ee1ee6bf30e421b6435412342
Kiltae <loverpleasestayo0@gmail.com>
2023-11-07 17:23:58 +0900

    test: matching 5 + bonus number

--- src/test/java/lotto/model/RankingTest.java
+++ src/test/java/lotto/model/RankingTest.java
@@ -17,5 +17,15 @@ public class RankingTest {
         assertThat(prize.match(playerInputNumber)).isEqualTo(FIRST);
     }
 
+    @DisplayName("5개의 숫자와 보너스 숫자를 맞춰서 SECOND가 나오는지 확인한다.")
+    @Test
+    void correctSecondPrize() {
+        Lotto playerInputNumber = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Prize prize = new Prize(lotto, 7);
+        assertThat(prize.match(playerInputNumber)).isEqualTo(SECOND);
+    }
+
+
 
 }

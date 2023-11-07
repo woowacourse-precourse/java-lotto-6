bfd8adbf64e456d1656b041d4ffff8e628a55a66
siwol025 <cyk99115695@gmail.com>
2023-11-07 19:58:44 +0900

    refactor(LottoFactory): 를 생성할때 로또 번호를 정렬해서 생성하도록 기능 수정

--- src/main/java/lotto/domain/lotto/LottoFactory.java
+++ src/main/java/lotto/domain/lotto/LottoFactory.java
@@ -1,5 +1,6 @@
 package lotto.domain.lotto;
 
+import java.util.Collections;
 import java.util.List;
 import lotto.domain.NumberGenerator;
 import lotto.domain.lotto.Lotto;
@@ -7,6 +8,7 @@ import lotto.domain.lotto.Lotto;
 public class LottoFactory {
     public static Lotto createLotto() {
         final List<Integer> numbers = NumberGenerator.generateRandomNumbers();
+        Collections.sort(numbers);
 
         return new Lotto(numbers);
     }

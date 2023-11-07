911860127fa98822fe2c7c1b26bdba7fa8573aa7
bbbang105 <2018111366@dgu.ac.kr>
2023-11-08 02:49:55 +0900

    feat : 특수 기호 enum 클래스 생성

--- src/main/java/lotto/constant/Sign.java
+++ src/main/java/lotto/constant/Sign.java
@@ -1,4 +1,19 @@
 package lotto.constant;
 
-public class Sign {
+public enum Sign {
+    DIVIDER("---"),
+    SEPARATOR(" - "),
+    LINE_BREAKER("\n"),
+    COMMA(","),
+    PERCENT("%");
+
+    private final String sign;
+
+    Sign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getSign() {
+        return sign;
+    }
 }

23dadd27283fba27281d70bcd2b38d0bb25fe101
권장순 <jsoonworld@gmail.com>
2023-11-08 00:30:53 +0900

    feat( Lotto ) : validate 메서드에 예외처리를 추가하고 필요한 중복 검증 메서드와 숫자를 반환하는 메서드를 구현함.

--- src/main/java/lotto/v3/model/Lotto.java
+++ src/main/java/lotto/v3/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.v3.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -12,8 +13,25 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 반드시 6개이어야 합니다.");
         }
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+
+        Collections.sort(numbers);
     }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 
 }

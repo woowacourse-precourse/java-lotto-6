c35b447396c964a7cda8b51dd4bd5b1bbb22dcd5
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-07 16:30:25 +0900

    test(remove) : 접근 제어자 제거

--- src/test/java/lotto/dto/LottoDtoTest.java
+++ src/test/java/lotto/dto/LottoDtoTest.java
@@ -16,7 +16,7 @@ class LottoDtoTest {
 
     @DisplayName("로또 여러 개에 대한 리스트를 dto 리스트 형태로 변환한다.")
     @Test
-    public void toDto_test() {
+    void toDto_test() {
         // given
         List<Lotto> lotteries = Arrays.asList(
                 new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
@@ -37,7 +37,7 @@ class LottoDtoTest {
 
     @DisplayName("단일 로또의 DTO에 대해 새로운 숫자를 추가하려고 시도할 경우 예외가 발생한다.")
     @Test
-    public void unmodifiable_list_test() {
+    void unmodifiable_list_test() {
         // given
         Lotteries lotteries = Lotteries.createLotteries(new TicketCount(1), new LottoNumberGenerator() {
             @Override

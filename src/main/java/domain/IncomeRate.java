b9f0425c238cc9b47c987dff4dad3c4bf7c6634a
Seongjun Ahn <dksckd0804@naver.com>
2023-11-07 22:54:08 +0900

    fix : fixed calcIncome

--- src/main/java/domain/IncomeRate.java
+++ src/main/java/domain/IncomeRate.java
@@ -4,18 +4,20 @@ public class IncomeRate {
     private final double rate;
 
     public IncomeRate(Money money, LottoResult lottoResult) {
-        this.rate =(calcIncome(lottoResult) * 0.1) / money.buyLottoTicket();
+        this.rate = (calcIncome(lottoResult) / (double) money.getMoney()) * 100;
     }
+
 
     public long calcIncome(LottoResult lottoResult) {
         long result = 0;
 
         for (Rank rank : Rank.values()) {
-            result += lottoResult.getEachRankResult(rank) * rank.getPrize();
+            result += (long) lottoResult.getEachRankResult(rank) * rank.getPrize();
         }
 
         return result;
     }
+
 
     public double getRate() {
         return rate;

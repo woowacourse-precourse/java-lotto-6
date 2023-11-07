4702ba85ba79ace7d78cc6d2ccb84fddb3800686
dj <fjzl12@naver.com>
2023-11-08 02:00:51 +0900

    refactor(Constants): 당첨 금액 및 결과 수익률 상수 추가

--- src/main/java/lotto/domain/Constants.java
+++ src/main/java/lotto/domain/Constants.java
@@ -7,4 +7,12 @@ public class Constants {
     public static final int MIN_COST = 1000;
     public static final int COST_UNIT = 1000;
     public static final int ZERO = 0;
+    public static final int HUNDRED = 100;
+    public static final long FIFTH_PRIZE = 5000;
+    public static final long FOURTH_PRIZE = 50000;
+    public static final long THIRD_PRIZE = 1500000;
+    public static final long SECOND_PRIZE = 30000000;
+    public static final long FIRST_PRIZE = 2000000000;
+    public static final String PROFIT = "총 수익률은 %.1f%%입니다.\n";
+    public static final String RESULT = "당첨 통계\n---";
 }

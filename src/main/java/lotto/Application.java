9032c32d253042a0f0d4bcb47d6918fc4eaa3c68
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-07 18:00:01 +0900

    feat(Lotto): 미션 - 로또 실행 메인메서드에서 LottoController만 불러와서 start 메소드를 실행하면 미션 - 로또가 실행됩니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final LottoController lottoController = new LottoController();
+
+        lottoController.lottoStart();
     }
 }

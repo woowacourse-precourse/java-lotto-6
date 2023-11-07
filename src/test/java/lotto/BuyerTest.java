9be0231a7ddb8887edc595d16840c89f5a319309
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-07 21:49:10 +0900

    Test : BuyerTest 코드 추가

---
+++ src/test/java/lotto/BuyerTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BuyerTest {
+    @Test
+    @DisplayName("::번호가 3개가 일치하여 5등 객체를 반환합니다.")
+    public void When_MatchThreeNumbers_Then_RankIsFiFTH() {
+        //Given
+        Lotto buyerNumbers = Lotto.create(List.of(1, 2, 3, 4, 5, 6));
+
+        Lotto randomLotto = Lotto.create(List.of(1, 2, 3, 7, 8, 9));
+
+        Buyer buyer = Buyer.create(buyerNumbers, 7);
+
+        //When
+        Rank rank = buyer.calculateComparingResult(randomLotto);
+
+        //Then
+        assertThat(rank).isEqualTo(Rank.FIFTH);
+    }
+}

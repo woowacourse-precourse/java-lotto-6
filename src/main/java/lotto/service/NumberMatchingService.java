323b118ea75f199cc8796c54a1aa340d3e1deb84
Sanghwi Lee <sanghwi47@naver.com>
2023-11-08 00:09:00 +0900

    refactor(NumberMatchingService) SRP를 만족하지 않던 기존 메소드들의 기능분리 chore() 일부 메소드 및 매개변수의 네이밍 수정

--- src/main/java/lotto/service/NumberMatchingService.java
+++ src/main/java/lotto/service/NumberMatchingService.java
@@ -5,53 +5,59 @@ import lotto.model.Lotto;
 import lotto.model.LottoPrize;
 import lotto.model.WinningNumbers;
 
-import java.util.*;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class NumberMatchingService {
 
-    public EnumMap<LottoPrize, Integer> calculateResults(List<List<Integer>> allYourNums, List<Integer> winningNumbers, int bonusNumber) {
-        EnumMap<LottoPrize, Integer> winCount = new EnumMap<>(LottoPrize.class);
-        Set<Integer> winningNumbersSet = new HashSet<>(winningNumbers);
+    public EnumMap<LottoPrize, Integer> calculateWinCounts(
+            List<Lotto> lottos, WinningNumbers winningNumbers, BonusNumber bonusNumber) {
 
-        for (List<Integer> yourNumbers : allYourNums) {
-            LottoPrize prize = checkWinning(yourNumbers, winningNumbersSet, bonusNumber);
+        Set<Integer> winningNumber = new HashSet<>(winningNumbers.getWinningNumbers());
+        EnumMap<LottoPrize, Integer> winCounts = initializeWinCount();
+
+        for (Lotto lotto : lottos) {
+            int matchCount = getMatchCount(lotto.getNumbers(), winningNumber);
+            boolean bonusMatch = isBonusMatch(lotto.getNumbers(), bonusNumber.getBonusNumber(), matchCount);
+            LottoPrize prize = determinePrize(matchCount, bonusMatch);
             if (prize != null) {
-                winCount.merge(prize, 1, Integer::sum);
+                winCounts.merge(prize, 1, Integer::sum);
             }
         }
-
-        return winCount;
+        return winCounts;
     }
 
-    private LottoPrize checkWinning(List<Integer> yourNumbers, Set<Integer> winningNumbersSet, int bonusNumber) {
+    private int getMatchCount(List<Integer> singleGameLottoNumbers, Set<Integer> winningNumbers) {
         int matchCount = 0;
-        for (int number : yourNumbers) {
-            if (winningNumbersSet.contains(number)) {
+        for (int number : singleGameLottoNumbers) {
+            if (winningNumbers.contains(number)) {
                 matchCount++;
             }
         }
+        return matchCount;
+    }
 
-        boolean bonusMatch = yourNumbers.contains(bonusNumber) && matchCount == 5;
-        return determinePrize(matchCount, bonusMatch);
+    private boolean isBonusMatch(List<Integer> singleGameLottoNumbers, int bonusNumber, int matchCount) {
+        return singleGameLottoNumbers.contains(bonusNumber) && matchCount == 5;
     }
 
     private LottoPrize determinePrize(int matchCount, boolean bonusMatch) {
+        if (matchCount < 3) {
+            return null;
+        }
         if (bonusMatch) {
             return LottoPrize.FIVE_AND_BONUS_MATCH;
         }
         return LottoPrize.valueOf(matchCount);
     }
 
-    public EnumMap<LottoPrize, Integer> calculateWinCounts(List<Lotto> lottoNumberList, WinningNumbers winningNumbers, BonusNumber bonusNumber) {
-        List<List<Integer>> lottoNumbersList = convertToNumberList(lottoNumberList);
-        return calculateResults(lottoNumbersList, winningNumbers.getWinningNumbers(), bonusNumber.getBonusNumber());
-    }
-
-    private List<List<Integer>> convertToNumberList(List<Lotto> lottos) {
-        List<List<Integer>> lottoNumbersList = new ArrayList<>();
-        for (Lotto lotto : lottos) {
-            lottoNumbersList.add(lotto.getNumbers());
+    private EnumMap<LottoPrize, Integer> initializeWinCount() {
+        EnumMap<LottoPrize, Integer> winCount = new EnumMap<>(LottoPrize.class);
+        for (LottoPrize prize : LottoPrize.values()) {
+            winCount.put(prize, 0);
         }
-        return lottoNumbersList;
+        return winCount;
     }
 }

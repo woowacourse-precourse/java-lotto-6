7aeb5b6273de468da1f91f095f8f51cdbe1898c0
Haebin <joker7011@naver.com>
2023-11-08 01:14:45 +0900

    test: [Domain Layer] `PrizeTest` Init

---
+++ src/test/java/lotto/domain/prize/PrizeTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.prize;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("[Domain Layer] Prize")
+class PrizeTest {
+
+    @Nested
+    @DisplayName("[of] 정적 팩토리 메소드 / 생성자 테스트")
+    class parseStringToInt {
+
+        /**
+         * Prize 객체 생성자 예외처리
+         * 1. 로또 객체 예외처리 -> Lotto 객체 생성자 위임
+         * 2. bonusNumberInput ->
+         */
+
+        @Test
+        @DisplayName("[]")
+        void tdd() {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+}

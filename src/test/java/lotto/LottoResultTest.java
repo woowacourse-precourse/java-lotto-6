07f900a4c7136340068a5bac5f1af48ad75582f8
gonixxxx <akdntm946@gmail.com>
2023-11-08 07:25:08 +0900

    test: add test to check earning rate

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.LottoResult;
 import lotto.model.Lottos;
+import lotto.model.Rank;
 import lotto.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,19 +19,25 @@ public class LottoResultTest {
     @BeforeEach
     void createLottoResult() {
         List<Lotto> newLotto = List.of(
-                new Lotto(List.of(3, 5, 11, 16, 32, 38)),
-                new Lotto(List.of(7, 11, 16, 35, 36, 44)));
+                new Lotto(List.of(4, 19, 26, 28, 43, 44)),
+                new Lotto(List.of(14, 15, 22, 23, 32, 36)),
+                new Lotto(List.of(4, 17, 26, 31, 37, 43)),
+                new Lotto(List.of(2, 6, 11, 26, 33, 34)),
+                new Lotto(List.of(10, 20, 23, 24, 35, 41)));
 
         Lottos lottos = new Lottos(newLotto);
-        User user = new User(new Lotto(List.of(3, 5, 11, 2, 7, 1)), "4");
+        User user = new User(new Lotto("4,19,26,28,43,10"), "44");
         this.lottoResult = new LottoResult(lottos, user);
     }
 
     @DisplayName("총 수익률을 계산한다.")
     @Test
     void calculateEarningRate() {
-        int cost = 2000;
-        assertThat(lottoResult.calEarningRate(cost)).isEqualTo(250.0);
+        int cost = 5000;
+        int earnings = lottoResult.findResult(Rank.FIVE_BONUS) * Rank.FIVE_BONUS.getPrize()
+                + lottoResult.findResult(Rank.THREE) * Rank.THREE.getPrize();
+        double earningRate = (earnings / (double) cost) * 100;
+        assertThat(lottoResult.calEarningRate(cost)).isEqualTo(earningRate);
     }
 
 }

c5f5d0c0a65850bf3d5b0c2826909d109a69bc1b
Donghun Won <wonhun1225@gmail.com>
2023-11-07 19:48:18 +0900

    feat(model) : answer 클래스 생성

---
+++ src/main/java/lotto/model/Answer.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+    private final List<Integer> answerNumber;
+    private final int bonuseNumber;
+
+    public Answer(List<Integer> answer,int bonus){
+        this.answerNumber = answer;
+        this.bonuseNumber = bonus;
+    }
+
+}

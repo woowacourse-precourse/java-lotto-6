1b53cbc95aab122cd33495931ae9f09764fa813f
seonghoo1217 <seonghoo1217@naver.com>
2023-11-07 18:21:18 +0900

    feat: Lotto Reflection Getter 추가 및 에러 타입 지정

--- src/main/java/lotto/entity/Lotto.java
+++ src/main/java/lotto/entity/Lotto.java
@@ -1,6 +1,12 @@
 package lotto.entity;
 
+import lotto.entity.mapper.FiledMapper;
+import lotto.property.MethodProperty;
+
+import java.util.Collections;
 import java.util.List;
+
+import static lotto.property.ErrorProperty.LOTTO_SIZE_IS_NOT_STANDARD;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +18,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_SIZE_IS_NOT_STANDARD.toString());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(
+                (List<Integer>) FiledMapper.getFieldValue(this, MethodProperty.LOTTO_NUMBERS)
+        );
+    }
 }
--- src/main/java/lotto/property/MethodProperty.java
+++ src/main/java/lotto/property/MethodProperty.java
@@ -4,4 +4,5 @@ public class MethodProperty {
     public static final String PURCHASE_COST = "purchaseCost";
     public static final String WINNING_NUMBERS = "winningNumbers";
     public static final String BONUS_NUMBER = "bonusNumber";
+    public static final String LOTTO_NUMBERS = "numbers";
 }

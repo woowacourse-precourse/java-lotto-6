065e2cc56b307fa42065d478c072b857f29c478b
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 14:55:28 +0900

    refactor: 보너스번호 검증하는 기능 삭제

--- src/main/java/lotto/Model/WinningLotto.java
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -1,6 +1,5 @@
 package lotto.Model;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,7 +9,6 @@ public class WinningLotto {
             String.format("[ERROR] 숫자와 쉼표(%s)만을 이용해서 입력해주세요. 예시) 1,2,3,4,5,6", LOTTO_NUMBER_DELIMETER);
     public static final String ERROR_END_LOTTO_NUMBER_DELEMETER =
             String.format("[ERROR] 마지막은 %s가 올 수 없습니다. 예시) 1,2,3,4,5,6", LOTTO_NUMBER_DELIMETER);
-    public static final String ERROR_INVALID_BONUS = "[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다.";
 
     private final Lotto winningLotto;
 
@@ -22,14 +20,6 @@ public class WinningLotto {
                     .collect(Collectors.toList()));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_NOT_LOTTO_FORMAT);
-        }
-    }
-
-    public void contain(Bonus bonus) throws IllegalArgumentException {
-        int bonusNumber = bonus.getBonusNumber();
-        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
-        if (winningLottoNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(ERROR_INVALID_BONUS);
         }
     }
 

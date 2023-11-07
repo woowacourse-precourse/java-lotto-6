fe5d482227c08b2e2f08cf3e0f9a0fd0a60b1105
MyStraw <kikongdosa@gmail.com>
2023-11-08 05:59:28 +0900

    Feat: 추가 Lotto 클래스에 LottoTest 통과할 코드 작성
    LottoTest로 미리 만들어둔 몇개의 테스트 코드를 통과할수 있는 예외처리 코드를 Lotto 클래스에 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,7 +14,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개여야 해요.");
+        }
+        Set<Integer> notDuplicatedNumbers = new HashSet<>(numbers);
+        if (notDuplicatedNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복되면 안돼요.");
+        }
+        for (int number : numbers){
+            if (number <1 || number >45){
+                throw new IllegalArgumentException("로또 번호는 1과 45 사이의 값이어야 해요.");
+            }
         }
     }
 

1cd1a1dbc27bde9e571db9d5dc83f0cbf3a84648
Dowon <3120kang@gmail.com>
2023-11-08 01:30:03 +0900

    Feat(OutputView): 로또 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,19 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.stream.Collectors;
+
 public class OutputView {
 
     public void printCount(int count) {
         System.out.printf("%d개를 구매했습니다.\n", count);
+    }
+
+    public void printLotto(Lotto lotto) {
+        String numbers = lotto.getNumbers().stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        System.out.println("[" + numbers + "]");
     }
 }

40060cf579f7b743f2ece8839a794e62615e2585
yang <hjyang@epience.com>
2023-11-07 13:26:18 +0900

    🎯feat(domain/Lottos) : rank(맞은숫자)와 매칭하는 함수 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.Lotto;
 public class Lottos {
     Purchase purchase;
@@ -18,5 +20,16 @@ public class Lottos {
 
     public  void addLotto(List<Integer> lotto) {
         lottos.add((Lotto) lotto);
+    }
+
+    // 각 Lotto를 순회하며 Rank enum과 매칭
+    public void matchRank(Lotto lotto) {
+    }
+    public Map<Rank, Long> getResults(WinningLottoNumbers winningLottoNumbers) {
+        return lottos.stream()
+                .collect(Collectors.groupingBy(
+                        lotto -> Rank.valueOf(lotto.matchNumber(winningLottoNumbers)),
+                        Collectors.counting()
+                ));
     }
 }

8518585ecb6d2ffd2b95a1e577ec6cbf8f870337
nylonmusk <2912rla@gmail.com>
2023-11-08 04:07:21 +0900

    refactor : private constructor 생성

--- src/main/java/lotto/domain/WinningNumbersFactory.java
+++ src/main/java/lotto/domain/WinningNumbersFactory.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class WinningNumbersFactory {
 
+    private WinningNumbersFactory() {}
+
     public static WinningNumbers createWinningNumbers(String winningNumbersInput, int bonusNumber) {
         return new WinningNumbers(winningNumbersInput, bonusNumber);
     }

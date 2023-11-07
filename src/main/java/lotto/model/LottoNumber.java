27060785f7e24cb3302b5100dabac1ebf199edc9
sinminseok12 <sinminseok12@naver.com>
2023-11-07 18:04:14 +0900

    chore(LottoNumber): 자바 컨벤션에 맞춰 공백 수정

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -5,7 +5,7 @@ import lotto.utils.ErrorMessage;
 import java.util.Objects;
 
 
-public class LottoNumber{
+public class LottoNumber {
     public static final Integer MIN_LOTTO_NUMBER_RANGE = 1;
     public static final Integer MAX_LOTTO_NUMBER_RANGE = 45;
     private static final String LOTTO_NUMBER_RANGE_ERROR = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";

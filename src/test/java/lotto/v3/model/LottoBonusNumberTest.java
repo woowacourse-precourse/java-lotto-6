c58b7d0d7f0ad2ab1378d4b35e93011415481aac
권장순 <jsoonworld@gmail.com>
2023-11-08 02:52:09 +0900

    test( LottoBonusNumberTest#testBonusNumberWithinRange() ) : 보너스 번호가 1부터 45 사이 값인지 검증.

---
+++ src/test/java/lotto/v3/model/LottoBonusNumberTest.java
@@ -0,0 +1,25 @@
+package lotto.v3.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoBonusNumberTest {
+    @Test
+    @DisplayName("보너스 번호가 1부터 45 사이 값인지 검증")
+    void testBonusNumberWithinRange() {
+        // Given
+        int validBonusNumber = 45;
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+
+        // When & Then
+        assertThatCode(() -> new LottoBonusNumber(validBonusNumber, winningNumbers))
+                .doesNotThrowAnyException();
+    }
+
+}

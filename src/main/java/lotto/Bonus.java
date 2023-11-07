17026b1110deae2b9d56f4de533612bfcf26005b
Identityless <rlandwnd555@naver.com>
2023-11-08 01:03:02 +0900

    fix : 입력된 보너스 숫자 유효성 검증 책임 부여

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -1,13 +1,24 @@
 package lotto;
 
+import static lotto.CommonUnits.MAX_LOTTO_NUMBER;
+import static lotto.CommonUnits.MIN_LOTTO_NUMBER;
+
 public class Bonus {
     private final int bonus;
 
     public Bonus(int bonus) {
+        validate(bonus);
         this.bonus = bonus;
     }
 
     public int getBonus() {
         return bonus;
+    }
+
+    private void validate(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + MIN_LOTTO_NUMBER +
+                "부터 " + MAX_LOTTO_NUMBER + " 사이의 숫자여야 합니다.");
+        }
     }
 }

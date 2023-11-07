372a53d32e6d0d423f9f081c2ff805a316d99c52
gusdn0108 <gusdn6671@naver.com>
2023-11-07 12:56:46 +0900

    test(Lotties) : LotteisTest add

---
+++ src/test/java/lotto/domain/LottiesTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LottiesTest {
+
+    @Test
+    void getLotties() {
+    }
+
+    @Test
+    void getLottiesNumbers() {
+    }
+
+    @Test
+    void size() {
+    }
+}

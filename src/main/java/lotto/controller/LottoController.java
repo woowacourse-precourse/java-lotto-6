99199da33ca6e57c05840785ab1f7d2e8357623c
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 18:21:05 +0900

    feat: add purchase lotto to lotto controller

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,28 @@
 package lotto.controller;
 
+import lotto.model.UserLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private UserLotto userLotto;
 
     public LottoController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+    }
+
+    public void purchaseLotto() {
+        while (true) {
+            try {
+                int purchaseMoney = inputView.readPurchaseMoney();
+                userLotto = new UserLotto(purchaseMoney);
+                outputView.printLottoBundle(userLotto.getLottoBundle());
+                break;
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
     }
 }

b29db0d388d27517f3e1122ad324197b8801a37b
devhoya97 <diajbh123@gmail.com>
2023-11-07 18:20:31 +0900

    test(service): 랜덤 숫자를 기반으로 생성하는 로또의 수가 많아 가독성을 해치므로 두 개로 줄임

--- src/test/java/lotto/service/VendingMachineTest.java
+++ src/test/java/lotto/service/VendingMachineTest.java
@@ -15,9 +15,9 @@ class VendingMachineTest extends NsTest {
 
     @DisplayName("구입 금액에 따라 구입한 개수만큼 로또가 랜덤 번호를 기반으로 생성된다.")
     @Test
-    void buyLottos() {
+    void getLottos() {
         // given
-        PurchaseAmount purchaseAmount = new PurchaseAmount(8000);
+        PurchaseAmount purchaseAmount = new PurchaseAmount(2000);
         VendingMachine vendingMachine = new VendingMachine(purchaseAmount);
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
@@ -25,23 +25,11 @@ class VendingMachineTest extends NsTest {
                     System.out.println(lottos.toString());
                     assertThat(output()).contains(
                             "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]"
+                            "[3, 5, 11, 16, 32, 38]"
                     );
                 },
                 List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
+                List.of(3, 5, 11, 16, 32, 38)
         );
     }
 

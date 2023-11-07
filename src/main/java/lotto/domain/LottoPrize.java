2aa05875626e6a2e904c5ff5333080b6a887c4e0
tam31 <wxogud3377@naver.com>
2023-11-07 18:02:57 +0900

    Refactor(LottoPrize) : 사용하지 않는 변수 제거

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,19 +1,17 @@
 package lotto.domain;
 
 public enum LottoPrize {
-    Three_PRIZE(3, 5000,"5,000"),
-    FOUR_PRIZE(4,50000,"50,000"),
-    FIVE_PRIZE(5,1500000,"1,500,000"),
-    FIVE_AND_BONUS_PRIZE(50,30000000,"30,000,000"),
-    SIX_PRIZE(6,2000000000,"2,000,000,000");
+    Three_PRIZE(3, 5000),
+    FOUR_PRIZE(4,50000),
+    FIVE_PRIZE(5,1500000),
+    FIVE_AND_BONUS_PRIZE(50,30000000),
+    SIX_PRIZE(6,2000000000);
 
     private final int count;
     private final long prizeMoney;
-    private final String money;
-    LottoPrize(int count, long prizeMoney, String money) {
+    LottoPrize(int count, long prizeMoney) {
         this.count = count;
         this.prizeMoney = prizeMoney;
-        this.money = money;
     }
 
     public int getCount() {
@@ -22,10 +20,6 @@ public enum LottoPrize {
 
     public long getPrizeMoney() {
         return prizeMoney;
-    }
-
-    public String getMoney() {
-        return money;
     }
 
     public static Long findPrizeMoney(int number) {

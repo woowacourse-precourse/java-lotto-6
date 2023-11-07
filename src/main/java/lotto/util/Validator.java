c4f5463778d698f3f4ee0b5f5bf4286453ac60cc
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-07 16:50:05 +0900

    [feat : validator] 입력한 숫자가 범위를 초과하는 경우

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -4,32 +4,48 @@ import java.util.List;
 
 public class Validator {
     //1,000으로 나누어 떨어지지 않는 경우
-    public static void notSeparatedBy1000(int amount){
-        if(amount %1000 != 0){
+    public static void notSeparatedBy1000(int amount) {
+        if (amount % 1000 != 0) {
             throw new IllegalArgumentException("구입 금액에 1,000보다 작은 단위가 있습니다.");
         }
     }
 
     //숫자가 아닌 경우
-    public static void isDigit(String number){
-        try{
+    public static void isDigit(String number) {
+        try {
             Integer.parseInt(number);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력하신 보너스 번호는 숫자가 아닙니다.");
         }
     }
 
     //숫자가 아닌 경우-리스트 버전
-    public static void isDigit(List<String> numbers){
-        try{
-            for(String number:numbers){
+    public static void isDigit(List<String> numbers) {
+        try {
+            for (String number : numbers) {
                 Integer.parseInt(number);
             }
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("로또 번호 중 숫자가 아닌 입력이 있습니다.");
         }
     }
+
     //1 ~ 45 범위를 초과하는 경우
+    public static void outOfRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("입력하신 보너스 번호가 범위(1 ~ 45)를 초과합니다.");
+        }
+    }
+
     //1 ~ 45 범위를 초과하는 경우-리스트 버전
-    //중복된 숫자가 포함된 경우 - 리스트
+    public static void outOfRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("입력하신 로또 번호 중 범위(1 ~ 45)를 초과하는 입력이 있습니다.");
+            }
+        }
+    }
+
+    //중복된 숫자가 포함된 경우 - 리스트, 보너스
+
 }

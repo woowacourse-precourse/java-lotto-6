62f94e1183be63c810aafd0ae4417b6657fc5734
20191777 <20191777@o365.hanbat.ac.kr>
2023-11-08 00:01:47 +0900

    TEST: 테스트코드 수정 - 잘못된 테스트코드 제거

--- src/test/java/test/InputTest.java
+++ src/test/java/test/InputTest.java
@@ -13,15 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputTest extends NsTest {
-    @DisplayName("구입 금액 입력한다.")
-    @Test
-    void createpriceoflottotobuy() {
-        Buyer buyer = new Buyer();
-        run("1000");
-        assertThat(buyer.getPrice()).isEqualTo(1000);
-
-    }
-
     @Override
     protected void runMain() {}
 }

a1ff0546fde1fd0bd6ae35e4cdc990f026b25ad9
howudong <tjdvy963@naver.com>
2023-11-07 21:52:29 +0900

    feat : MainController에 controller 연결 및 ControllerConfig 구성

--- src/main/java/lotto/controller/ControllerConfig.java
+++ src/main/java/lotto/controller/ControllerConfig.java
@@ -4,7 +4,10 @@ import lotto.domain.AutoLottoCreateModel;
 import lotto.domain.LottoMachine;
 import lotto.service.LottoBuyService;
 import lotto.view.inputview.LottoBuyInputView;
+import lotto.view.inputview.WinningLottoInputView;
 import lotto.view.outputview.LottoBuyOutputView;
+import lotto.view.outputview.LottoResultOutputView;
+import lotto.view.outputview.WinningLottoOutputView;
 
 public class ControllerConfig {
     private ControllerConfig() {
@@ -13,5 +16,13 @@ public class ControllerConfig {
     public static Controller createLottoBuyController() {
         return new LottoBuyController(new LottoBuyInputView(), new LottoBuyOutputView(),
                 new LottoBuyService(new LottoMachine(new AutoLottoCreateModel())));
+    }
+
+    public static Controller createWinningLottoController() {
+        return new WinningLottoController(new WinningLottoInputView(), new WinningLottoOutputView());
+    }
+
+    public static Controller createLottoResultController() {
+        return new LottoResultController(new LottoResultOutputView());
     }
 }
--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -7,16 +7,29 @@ import java.util.Map;
 
 public final class MainController {
     private final Map<String, Controller> controllers = Map.ofEntries(
-            Map.entry("lottoBuy", ControllerConfig.createLottoBuyController())
+            Map.entry("lottoBuy", ControllerConfig.createLottoBuyController()),
+            Map.entry("winningLotto", ControllerConfig.createWinningLottoController()),
+            Map.entry("lottoResult", ControllerConfig.createLottoResultController())
     );
 
     public void run() {
         Map<String, DTO.Input> inputs = new HashMap<>();
         Map<String, DTO.Output> outputs = new HashMap<>();
-        getLottoBuy(inputs, outputs);
+
+        processBuyLotto(inputs, outputs);
+        processWinningLotto(inputs, outputs);
+        processLottoResult(inputs, outputs);
     }
 
-    private void getLottoBuy(Map<String, DTO.Input> inputs, Map<String, DTO.Output> outputs) {
+    private void processBuyLotto(Map<String, DTO.Input> inputs, Map<String, DTO.Output> outputs) {
         controllers.get("lottoBuy").process(inputs, outputs);
+    }
+
+    private void processWinningLotto(Map<String, DTO.Input> inputs, Map<String, DTO.Output> outputs) {
+        controllers.get("winningLotto").process(inputs, outputs);
+    }
+
+    private void processLottoResult(Map<String, DTO.Input> inputs, Map<String, DTO.Output> outputs) {
+        controllers.get("lottoResult").process(inputs, outputs);
     }
 }

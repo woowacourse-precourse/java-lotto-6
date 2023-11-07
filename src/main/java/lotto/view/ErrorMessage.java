9c513cdcc3e5fc11b155557badd48404fbfb88a1
Junobee25 <wnsdhqo@naver.com>
2023-11-07 20:44:02 +0900

    refactor(ErrorMessage): 에러메세지 내용 수정

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -6,7 +6,7 @@ public class ErrorMessage {
     private final static String STRING_TO_LONG_ERROR = "[ERROR] 구입 금액 입력 값은 숫자로만 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_DUPLICATE_ERROR = "[ERROR] 당첨 번호는 중복 되면 안됩니다.";
     private final static String INPUT_BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 1 부터 45 까지의 숫자여야만 합니다.";
-    private final static String STRING_TO_INTEGER_ERROR = "[ERROR] 입력 값은 숫자로만 이루어져 있어야 합니다.";
+    private final static String STRING_TO_INTEGER_ERROR = "[ERROR] 보너스 번호는 숫자로만 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1 부터 45 까지의 숫자로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_LENGTH_ERROR = "[ERROR] 로또 번호 6자리로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_INTEGER_ERROR = "[ERROR] 문자열의 요소는 숫자 패턴이여야 합니다.";

cfb024b10831cc83a1f3a39cc3a701429396926d
huhdy32 <mong3268@gmail.com>
2023-11-08 02:56:13 +0900

    test: 테이스트 케이스 수정

--- src/test/java/lotto/LogicTest.java
+++ src/test/java/lotto/LogicTest.java
@@ -1,9 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.controller.InputController;
 import lotto.controller.LottoController;
 import lotto.logic.LottoLogic;
-import lotto.model.Lotto;
 import lotto.view.ConsoleView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,27 +14,27 @@ public class LogicTest extends NsTest {
 
     @Override
     protected void runMain() {
-        new LottoLogic(new ConsoleView(), new LottoController()).run();
+        new LottoLogic(new InputController(new ConsoleView()), new LottoController()).run();
     }
 
     @Test
     @DisplayName("지불금액 1000원 단위 에러처리")
     void provideIllegalInteger() {
         runException("5001");
-        assertThat(output()).contains("[ERROR] 지불 금액은 1000원 단위여야 합니다.");
+        assertThat(output()).contains("[ERROR] 지불 금액은 1000원 단위의 양수여야 합니다.");
     }
 
     @Test
     @DisplayName("지불금액 음수 에러처리")
     void provideNegativeInteger() {
         runException("-5000");
-        assertThat(output()).contains("[ERROR] 지불 금액은 0 보다 커야 합니다.");
+        assertThat(output()).contains("[ERROR] 지불 금액은 1000원 단위의 양수여야 합니다.");
     }
 
     @Test
     @DisplayName("지불금액 문자 에러처리")
     void provideCharacter() {
         runException("5jfe");
-        assertThat(output()).contains("[ERROR] 지불 금액은 정수여야 합니다.");
+        assertThat(output()).contains("[ERROR] 입력은 숫자만으로 이루어져야 합니다.");
     }
 }

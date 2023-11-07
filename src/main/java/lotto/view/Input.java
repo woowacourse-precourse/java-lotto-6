ab8abd4bfcbfce691f9b613b7c64fb868041483f
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 00:53:04 +0900

    feat: 로또 번호 입력 메소드 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,9 +1,11 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Converter;
 
-import static lotto.util.Validator.validateAmountInThousands;
-import static lotto.util.Validator.validateDigit;
+import java.util.List;
+
+import static lotto.util.Validator.*;
 
 public class Input {
     public int readTotalPurchasedAmount() {
@@ -12,5 +14,12 @@ public class Input {
         int amount = Integer.parseInt(amountStr);
         validateAmountInThousands(amount);
         return amount;
+    }
+
+    public List<Integer> readWinningNumbers() {
+        String numberStr = Console.readLine();
+        List<Integer> winningNumbers = Converter.stringToIntegerList(numberStr, ",");
+        validateNumberOfLottoNumbers(winningNumbers);
+        return winningNumbers;
     }
 }

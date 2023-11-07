4f53ee242b62068a4a5c55633e019d456f0c307a
ChanUi <hcu55@naver.com>
2023-11-08 04:53:27 +0900

    test: 당첨 통계 업데이트 테스트 코드 구현

---
+++ src/test/java/lotto/domain/LottoStatisticTest.java
@@ -0,0 +1,46 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class LottoStatisticTest {
+
+    private LottoStatistic lottoStatistic;
+
+    private final List<Lotto> issueLottos = new ArrayList<>();
+
+    @BeforeEach
+    void 초기_값_세팅() {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        issueLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));    // 1등
+        issueLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));    // 2등
+        issueLottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 8)));    // 3등
+        issueLottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));   // 5등
+        issueLottos.add(new Lotto(List.of(1, 2, 6, 8, 9, 10)));   // 5등
+        lottoStatistic = new LottoStatistic(issueLottos, winningLotto);
+    }
+
+    @Test
+    @DisplayName("당첨 통계 업데이트 테스트")
+    void 당첨_통계_업데이트_테스트() {
+        Map<LottoGrade, Integer> result = lottoStatistic.getResult();
+
+        Integer first = result.get(LottoGrade.FIRST);
+        Integer second = result.get(LottoGrade.SECOND);
+        Integer third = result.get(LottoGrade.THIRD);
+        Integer fourth = result.get(LottoGrade.FOURTH);
+        Integer fifth = result.get(LottoGrade.FIFTH);
+
+        Assertions.assertThat(first).isEqualTo(1);
+        Assertions.assertThat(second).isEqualTo(1);
+        Assertions.assertThat(third).isEqualTo(1);
+        Assertions.assertThat(fourth).isEqualTo(0);
+        Assertions.assertThat(fifth).isEqualTo(2);
+    }
+}

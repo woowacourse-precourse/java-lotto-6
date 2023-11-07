0d6799c848a53e033df1942e39a068e365121ee9
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 12:59:44 +0900

    feat(const): Lotto 도메인에 필요한 상수 추가

--- src/main/java/lotto/constant/Config.java
+++ src/main/java/lotto/constant/Config.java
@@ -2,4 +2,7 @@ package lotto.constant;
 
 public class Config {
     public static final int PRICE_PER_LOTTO = 1000;
+    public static final int LOTTO_COUNT = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
 }

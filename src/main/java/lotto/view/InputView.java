5da6972d47bc33becdec2c505dccec8a56e5730b
Hayoon <hy97@sju.ac.kr>
2023-11-07 19:22:52 +0900

    feat(InputValidate): 로또 당첨 번호, 로또 보너스 번호 입력 형식 및 범위 검증

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,31 +1,46 @@
 package lotto.view;
 
+import lotto.util.validate.InputValidate;
+
 import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.util.validate.InputValidate.*;
 
 public class InputView {
 
     public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-
         String input = readLine();
-        return Integer.parseInt(input);
+
+        int parseIntInputValue = validateInputFormat(input);
+        validateThousandUnitInputFormat(parseIntInputValue);
+
+        return Integer.parseInt(input) / 1000;
     }
 
     public List<Integer> inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-
         String input = readLine();
 
-        return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).boxed().toList();
+        List<Integer> winningNumbers = Arrays.stream(input.split(",")).
+                mapToInt(Integer::parseInt).boxed().toList();
+
+        validateRangeInputLottoNumbers(winningNumbers);
+        validateUniqueInputLottoNumbers(winningNumbers);
+
+        return winningNumbers;
     }
 
-    public int inputBonusNumber() {
+    public int inputBonusNumber(List<Integer> winningNumbers) {
         System.out.println("보너스 번호를 입력해 주세요.");
-
         String input = readLine();
+
+        int bonusNumber = validateInputFormat(input);
+        validateRangeInputLottoBonusNumber(bonusNumber);
+        validateUniqueInputLottoBonusNumber(winningNumbers, bonusNumber);
+
         return Integer.parseInt(input);
     }
 

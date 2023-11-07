ad8e94e7e19b9086839a0858e20e130441bb3e45
Infikei <hckhong@naver.com>
2023-11-08 03:48:56 +0900

    refactor: InputView의 메시지에 대해 enum 적용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,24 +3,35 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_INPUT_MESSAGE = "\n보너스 번호를 입력해 주세요.";
-    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private enum InputMessage {
+        AMOUNT("구입금액을 입력해 주세요."),
+        WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+        BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+
+        private final String message;
+
+        private InputMessage(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return message;
+        }
+    }
 
     public String inputAmount() {
-        System.out.println(AMOUNT_INPUT_MESSAGE);
+        System.out.println(InputMessage.AMOUNT);
         return Console.readLine();
     }
 
     public String inputWinningNumbers() {
-        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
+        System.out.println(InputMessage.WINNING_NUMBERS);
         return Console.readLine();
     }
 
     public String inputBonusNumber() {
-        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        System.out.println(InputMessage.BONUS_NUMBER);
         return Console.readLine();
     }
 }

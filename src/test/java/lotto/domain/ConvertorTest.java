af09cbf9a16e6c8352c3b49248692ab971b91f9a
sangchu <didi1484@gmail.com>
2023-11-07 11:09:02 +0900

    fix: test 코드 수정

--- src/test/java/lotto/domain/ConvertorTest.java
+++ src/test/java/lotto/domain/ConvertorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,8 +15,7 @@ class ConvertorTest {
     @DisplayName("1-45 사이의 숫자면 리스트 생성")
     @ValueSource(strings = "1,3,18,45,27,15")
     void convertToIntegersSuccess(String input) {
-        setUp();
-        List<Integer> numbers = convertor.convertToNumbers(input);
+        List<Integer> numbers = Convertor.convertToNumbers(input);
         assertThat(numbers).contains(1, 3, 18, 45, 27, 15);
     }
 
@@ -25,13 +23,7 @@ class ConvertorTest {
     @DisplayName("1-45 이외의 숫자, 문자 입력시 예외 발생")
     @ValueSource(strings = {"1,3,18,45,27,46", "$,3,18,45,27,15", "1,3,18,45,27, "})
     void convertToIntegersFail(String input) {
-        setUp();
-        assertThatThrownBy(() -> convertor.convertToNumbers(input))
+        assertThatThrownBy(() -> Convertor.convertToNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @BeforeEach
-    void setUp() {
-        convertor = new Convertor();
     }
 }

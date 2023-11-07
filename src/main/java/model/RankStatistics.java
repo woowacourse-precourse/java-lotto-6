25021060188792e42923bcae7dd7cff6bb74a85a
nayonsoso <panda0329@naver.com>
2023-11-08 04:57:42 +0900

    feat: 등수 통계 추가하는 기능 구현

--- src/main/java/model/RankStatistics.java
+++ src/main/java/model/RankStatistics.java
@@ -6,4 +6,9 @@ public class RankStatistics {
 
     private EnumMap<LottoRank, Integer> rankStatistics;
 
+    public void add(LottoRank rank) {
+        int rankCount = rankStatistics.getOrDefault(rank, 0);
+        rankStatistics.put(rank, rankCount + 1);
+    }
+
 }

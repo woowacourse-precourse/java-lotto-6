1669162444fb6b68c515ca8fa8883206b22f9b09
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-07 16:02:47 +0900

    feat: 수익률 계산 기능 구현

---
+++ src/main/java/lotto/services/EarningRateCalculator.java
@@ -0,0 +1,36 @@
+package lotto.services;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.EnumMap;
+import java.util.Map;
+import lotto.enums.LottoPrize;
+
+public class EarningRateCalculator {
+    public static BigDecimal calculate(EnumMap<LottoPrize, Integer> prizes, int purchaseMoney) {
+        BigDecimal totalWinnings = calculateTotalWinnings(prizes);
+        BigDecimal earningRate = calculateEarningRate(totalWinnings, purchaseMoney);
+        return earningRate.setScale(1, RoundingMode.HALF_UP);
+    }
+
+    private static BigDecimal calculateTotalWinnings(EnumMap<LottoPrize, Integer> prizes) {
+        BigDecimal totalWinnings = BigDecimal.ZERO;
+
+        for (Map.Entry<LottoPrize, Integer> entry : prizes.entrySet()) {
+            LottoPrize prize = entry.getKey();
+            int winningCount = entry.getValue();
+            double prizeAmount = prize.getPrizeAmount();
+
+            if (winningCount > 0) {
+                BigDecimal prizeTotal = BigDecimal.valueOf(prizeAmount).multiply(BigDecimal.valueOf(winningCount));
+                totalWinnings = totalWinnings.add(prizeTotal);
+            }
+        }
+        return totalWinnings;
+    }
+
+    private static BigDecimal calculateEarningRate(BigDecimal totalWinnings, int purchaseMoney) {
+        return totalWinnings.divide(BigDecimal.valueOf(purchaseMoney), 3, RoundingMode.DOWN)
+                .multiply(BigDecimal.valueOf(100));
+    }
+}

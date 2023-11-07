91d4c1322bb479a0207b1564cdb67f1f5aa82bc0
sgo722 <sgo722@naver.com>
2023-11-07 20:57:02 +0900

    fix : 수익률 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -42,7 +42,6 @@ public class OutputView {
     }
 
     public static void printProfitRate(double profitRate){
-        String formatProfitRate = String.format("%.1f", profitRate);
-        System.out.println("총 수익률은 " + formatProfitRate + "%입니다.");
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
     }
 }

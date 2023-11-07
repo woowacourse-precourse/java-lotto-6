01207c0b952d801115da3840f026e6e4a8e520e0
Junobee25 <wnsdhqo@naver.com>
2023-11-07 18:22:09 +0900

    feat(Lotto): duplicate 예외 처리 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,15 +2,16 @@ package lotto.domain;
 
 import lotto.view.VerificationView;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -22,6 +23,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void duplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int lottoNumber : numbers) {
+            if(!uniqueNumbers.add(lottoNumber)) {
+                VerificationView.verificationViewDuplicateLottoNumber();
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public List<Integer> getLotto() {
         return this.numbers;
     }

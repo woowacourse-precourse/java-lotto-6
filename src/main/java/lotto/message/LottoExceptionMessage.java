74ed6e00e8c44945674b41fa247ba1c8ae2c52e9
juns1s <juns1s@naver.com>
2023-11-07 22:36:11 +0900

    comment: 불필요한 코드 수정

--- src/main/java/lotto/message/LottoExceptionMessage.java
+++ src/main/java/lotto/message/LottoExceptionMessage.java
@@ -2,8 +2,7 @@ package lotto.message;
 
 public enum LottoExceptionMessage {
     INVALID_LOTTO_LENGTH("[ERROR] 로또 번호는 6자리여야 합니다."),
-    INVALID_LOTTO_DUPLICATED("[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다.")
-    ;
+    INVALID_LOTTO_DUPLICATED("[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다.");
 
     private final String message;
 

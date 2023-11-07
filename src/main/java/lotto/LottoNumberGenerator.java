0c8cbeece4ce9665051c60f52fea84349d4cb79e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-07 18:24:36 +0900

    fix(lottoNumberGenerator): immutable한 리스트 정렬 코드 수정

--- src/main/java/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -1,18 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
     public List<Integer> generateLottoNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        sortLottoNumbers(lottoNumbers);
-
-        return lottoNumbers;
+        return sortLottoNumbers(lottoNumbers);
     }
 
-    private void sortLottoNumbers(List<Integer> lottoNumbers) {
-        Collections.sort(lottoNumbers);
+    private List<Integer> sortLottoNumbers(List<Integer> lottoNumbers) {
+        return lottoNumbers.stream()
+                .sorted()
+                .toList();
     }
 }

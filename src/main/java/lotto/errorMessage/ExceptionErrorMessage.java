51955584146d983574e376fbb02a54637fc491f2
lusate <rayrayr@naver.com>
2023-11-07 21:43:53 +0900

    feat: 로또 번호 예외 상수 추가

--- src/main/java/lotto/errorMessage/ExceptionErrorMessage.java
+++ src/main/java/lotto/errorMessage/ExceptionErrorMessage.java
@@ -6,5 +6,6 @@ public class ExceptionErrorMessage {
     public static final String LOTTO_DUPLICATE = "[ERROR] 숫자가 중복되었습니다";
     public static final String LOTTO_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String INPUT_ONLY_NUMBER = "[ERROR] 로또 구입 금액은 숫자만 입력해야 합니다";
+    public static final String INPUT_ONLY_NUMBER_LOTTO = "[ERROR] 로또 번호는 숫자만 입력해야 합니다";
     public static final String INPUT_ONLY_NUMBER_BONUS = "[ERROR] 보너스 번호는 숫자만 입력해야 합니다";
 }

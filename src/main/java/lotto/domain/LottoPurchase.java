8f1d07f9e0c2fc3fa491af668d14b4890b3ccabe
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 02:16:08 +0900

    refactor(LottoPurchase): buyLotto 메소드 매개변수를 int로 변경

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -1,39 +1,19 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class LottoPurchase {
-    public static List<Lotto> buyLotto(long money) {
-        validateMoney(money);
+    public static List<Lotto> buyLotto(int purchaseCount) {
         List<Lotto> lottos = new ArrayList<>();
 
-        long purchaseCount = money / 1000;
         while (lottos.size() != purchaseCount) {
             lottos.add(makeLotto());
         }
 
         return lottos;
-    }
-
-    private static void validateMoney(long money) {
-        if (isLackMoney(money)) {
-            throw new IllegalArgumentException("금액은 1,000원 이상 투입 되어야 합니다.");
-        }
-
-        if (isWrongMoney(money)) {
-            throw new IllegalArgumentException("금액은 1,000원 단위로 투입 되어야 합니다.");
-        }
-    }
-
-    private static boolean isLackMoney(long money) {
-        return money < 1000;
-    }
-
-    private static boolean isWrongMoney(long money) {
-        return money % 1000 != 0;
     }
 
     private static Lotto makeLotto() {

7678ea18c634ffef2e83bd03c14a8761558c0c0f
acycoco <pink10257@naver.com>
2023-11-07 22:47:57 +0900

    feat(Application): 게임이 실행되도록 하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView(outputView);
+        InputProcessor inputProcessor = new InputProcessor();
+        LottoController lottoController = new LottoController(outputView, inputView, inputProcessor);
+        lottoController.start();
     }
 }

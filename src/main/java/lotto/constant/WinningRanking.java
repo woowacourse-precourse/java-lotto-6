230d70668d4cadf4e50fa9c2fff05d2af489811c
limzzum <chfhddl1234@naver.com>
2023-11-08 02:02:18 +0900

    :sparkles: feat : 당첨 기준 enum 에 랭킹 추가

--- src/main/java/lotto/constant/WinningRanking.java
+++ src/main/java/lotto/constant/WinningRanking.java
@@ -2,18 +2,31 @@ package lotto.constant;
 
 public enum WinningRanking {
 
-    ONE(6, 2_000_000_000),
-    TWO(5, 3_000_000_0),
-    THREE(5, 1_500_000),
-    FOUR(4, 50000),
-    FIVE(3, 5000);
+    RANK1(1,6, 2_000_000_000),
+    RANK2(2,5, 3_000_000_0),
+    RANK3(3,5, 1_500_000),
+    RANK4(4, 4, 50000),
+    RANK5(5, 3, 5000);
 
+    private final int ranking;
     private final int matchNumCnt;
     private final int winnings;
 
-    WinningRanking(int matchNumCnt, int winnings){
+    WinningRanking(int ranking, int matchNumCnt, int winnings){
+        this.ranking = ranking;
         this.matchNumCnt = matchNumCnt;
         this.winnings = winnings;
     }
 
+    public int getMatchNumCnt(){
+        return matchNumCnt;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public int getWinnings() {
+        return winnings;
+    }
 }

d6002178e9ef4b78e95c3307eb3454a908d58540
Lee Geun Woo <gamebdy@naver.com>
2023-11-08 04:36:26 +0900

    :sparkles: OutputView 에 추첨문구와 통계문구 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,27 +1,22 @@
 package lotto.view;
 
-
+import lotto.model.MyLottoNumber;
+import lotto.model.MyLottoNumbers;
 
 import static lotto.util.Phrase.OutputPhrase.BUY_HOWMUCH;
 
 public class OutputView {
-
-//    private enum OutputPhrase {
-//        BUY_HOWMUCH("%d개를 구매했습니다.\n");
-//        private final String message;
-//
-//        OutputPhrase(String message) {
-//            this.message = message;
-//        }
-//
-//    }
 
 
     public void printNumberOfLotto(int NumberOfLotto) {
         System.out.printf(BUY_HOWMUCH.printMessage(), NumberOfLotto);
     }
 
-    public void printWinningNumber() {
-        //비교 이후 구현
+    public void printMyNumbers(MyLottoNumbers myLottoNumbers) {
+        for(MyLottoNumber myLottoNumber : myLottoNumbers.getMyLottoNumbers()){
+            System.out.println((myLottoNumber.toString()));
+        }
+
+
     }
 }

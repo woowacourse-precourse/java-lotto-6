a7b16b394fb8aae3de82bbc448199b046b1da32d
minjong kim <godminjong@naver.com>
2023-11-07 23:32:38 +0900

    feat: 당첨 번호와 보너스 번호에 대한 검증 기능 추가
    당첨 금액 입력과 보너스 번호입력에 대한 유일성, 6개의 입력, 숫자, 1~45 사이의 숫자임을 보장하는 검증 기능 추가함.

--- src/main/java/lotto/validate/InputValidator.java
+++ src/main/java/lotto/validate/InputValidator.java
@@ -25,6 +25,61 @@ public class InputValidator {
         }
     }
 
+
+    public Integer validateBonusNumber(String number){
+        try{
+            Integer integer = validateCorrectIntegerFormat(number);
+            validateNonNegativeInteger(integer);
+            validateUniqueBonus(integer);
+            return integer;
+        }catch (Exception e){
+            System.out.println(ERROR_PREFIX + e.getMessage());
+            return inputBonusNumber();
+        }
+    }
+
+
+    public String validateWinningInput(String lottos){
+        try {
+            lottos = lottos.replaceAll(" ", "");
+            totalValidateForWinningInput(lottos);
+            return lottos;
+        }
+        catch (Exception e){
+            System.out.println(ERROR_PREFIX+e.getMessage());
+            return inputNumberOfWinningNumbers();
+        }
+    }
+    private void totalValidateForWinningInput(String lottos) {
+        String[] split = validateNumberOfLotto(lottos);
+        validateListNumberFormat(split);
+        validateListRange(split);
+        splitToList(split);
+        validateUniqueList();
+    }
+
+    private void splitToList(String[] split) {
+        this.list= Arrays.stream(split)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private void validateListNumberFormat(String[] split) {
+        try{
+            Arrays.stream(split)
+                    .map(Integer::parseInt)
+                    .toList();
+        }catch (Exception e){
+            throw new IllegalArgumentException("'숫자' 그리고 ',' 만을 입력해주세요");
+        }
+    }
+    private static String[] validateNumberOfLotto(String lottos) {
+        String[] split = lottos.split(",");
+        if (split.length != 6){
+            throw new IllegalArgumentException("잘못된 입력입니다. ',' 로 구분하여 정확히 6개 숫자를 입력해주세요.");
+        }
+        return split;
+    }
     private void validateDivideByThousand(Long longNumber){
         if (longNumber % 1000 != 0L){
             throw new IllegalArgumentException("1,000 단위의 구매 가격을 입력해주세요.");
@@ -41,10 +96,49 @@ public class InputValidator {
 
 
 
+    private Integer validateCorrectIntegerFormat(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 숫자를 입력해주세요");
+        }
+    }
+    private void validateUniqueList() {
+        int distinctCount = (int) list.stream()
+                .distinct()
+                .count();
+        if (distinctCount != list.size()) {
+            throw new IllegalStateException("로또 번호가 중복되었습니다. 중복되지 않은 숫자들로 다시 입력해주세요.");
+        }
+
+    }
+
+    private void validateNonNegativeInteger(Integer number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("0 이상의 숫자를 입력해주세요.");
+        }
+    }
+
 
     private void validateNonNegativeLong(Long number) {
         if (number < 0) {
             throw new IllegalArgumentException("0 이상의 숫자를 입력해주세요.");
+        }
+    }
+    private void validateListRange(String[] split) {
+        Arrays.stream(split)
+                .map(Integer::parseInt)
+                .forEach(this::validateCorrectLottoNumber);
+    }
+    private void validateCorrectLottoNumber(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("로또 번호는 1에서 45까지만 허용합니다.");
+        }
+    }
+
+    private void validateUniqueBonus(int bonus) {
+        if (list.contains(bonus)) {
+            throw new IllegalArgumentException("보너스 번호는 로또 번호와 겹칠 수 없습니다 다시 입력해세요.");
         }
     }
 

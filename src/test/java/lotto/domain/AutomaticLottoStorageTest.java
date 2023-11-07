a2acbb9ddd60a5c4cfc2418aae17ba916080eb18
kimdoha <kkhhjj3393@gmail.com>
2023-11-08 03:51:20 +0900

    test(AutomaticLottoStorage): 발행한 로또 수량 및 로또 번호 출력 테스트

--- src/test/java/lotto/domain/AutomaticLottoStorageTest.java
+++ src/test/java/lotto/domain/AutomaticLottoStorageTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.common.Constant;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class AutomaticLottoStorageTest {
+class AutomaticLottoStorageTest extends NsTest {
 
     @DisplayName("자동 로또 저장소 객체 생성 테스트")
     @ParameterizedTest
@@ -20,7 +21,7 @@ class AutomaticLottoStorageTest {
 
         // Assert
         Assertions.assertEquals(
-                totalPrice / Constant.LOTTO_TICKET_PRICE,
+                automaticLottoStorage.getTicketCount(),
                 automaticLottoStorage.getAutomaticLottos().size());
     }
 
@@ -32,13 +33,19 @@ class AutomaticLottoStorageTest {
                 () -> AutomaticLottoStorage.from(totalPrice));
     }
 
-//    @Test
-//    void showAutomaticLottosResult() {
-//
-//    }
-//
-//    @Test
-//    void getTicketCount() {
-//
-//    }
+    @DisplayName("발행한 로또 수량 및 로또 번호 출력 테스트")
+    @Test
+    void showAutomaticLottosResult() {
+        // Assert
+        assertAll(() -> {
+            run();
+            Assertions.assertTrue(output().contains("8개를 구매했습니다."));
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        AutomaticLottoStorage automaticLottoStorage = AutomaticLottoStorage.from(8000);
+        automaticLottoStorage.showAutomaticLottosResult();
+    }
 }

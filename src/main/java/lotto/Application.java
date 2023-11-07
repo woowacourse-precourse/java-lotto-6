6cad19733d4a8aca83842df8788c13bbce399ee2
sujnkim <blackout3210@naver.com>
2023-11-07 15:12:09 +0900

    feat: Application 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import lotto.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        GameController.start();
+        GameController gameController = new GameController();
+        gameController.start();
     }
 }

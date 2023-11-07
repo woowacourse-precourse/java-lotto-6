9708a1e98eb8105ecb98785f8ee2334fdaa9e00c
hyungjin <hyungzin0309@naver.com>
2023-11-07 20:25:52 +0900

    feat: 메세지 작성

---
+++ src/main/java/lotto/message/Message.java
@@ -0,0 +1,16 @@
+package lotto.message;
+
+public class Message {
+
+    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    public static final String BUY_NUM = "\n%d개를 구매했습니다.\n";
+    public static final String WIN_STATISTICS = "\n당첨 통계";
+    public static final String DIVIDING_LINE = "---";
+    public static final String YIELD_RATE = "총 수익률은 %.1f%%입니다.\n";
+
+    private Message() {
+    }
+
+}

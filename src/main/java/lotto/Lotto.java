593f46900d99197a7be35ea0d85f2a21f696169f
yev <yevops@gmail.com>
2023-11-07 18:05:40 +0900

    상수 변수 추가 및 final로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,23 @@
 package lotto;
 
+import static lotto.constants.*;
+
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+  private final List<Integer> numbers;
+
+  public Lotto(List<Integer> numbers) {
+    validate(numbers);
+    this.numbers = numbers;
+  }
+
+  private void validate(List<Integer> numbers) {
+    if (numbers.size() != COUNT_NUMBER) {
+      throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
     }
+  }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    // TODO: 추가 기능 구현
+  // TODO: 추가 기능 구현
 }

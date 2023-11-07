ff4184da3335c4d0362546e9f1debe56a1c9d994
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-11-08 01:30:29 +0900

    Feat: 입력값들을 입력 받고 LottoGame을 생성하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.ErrorMessage;
 import lotto.domain.Amount;
-import lotto.domain.Number;
-import lotto.domain.WinningLotto;
+import lotto.domain.number.LottoNumber;
+import lotto.domain.number.LottoNumbers;
+import lotto.domain.lotto.WinningLotto;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,41 +13,64 @@ import java.util.List;
 public class InputView {
 
     public static final String NUMBERS_DELIMITER = ",";
+    public static final String NEW_LINE = "\n";
+
+    private InputView() {
+    }
 
     public static Amount inputAmount() {
         try {
-            System.out.println("구입금액을 입력해 주세요.\n");
-            int amount = Integer.parseInt(Console.readLine());
+            System.out.println("구입금액을 입력해 주세요.");
+            int amount = toInteger(Console.readLine());
+
             return new Amount(amount);
-        } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NUMBER.message());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputAmount();
         }
     }
 
     public static WinningLotto inputWinningLotto() {
-        try {
-            List<Number> numbers = inputWinningNumbers();
-            Number bonus = inputBonusNumber();
-            return new WinningLotto(numbers, bonus);
+        LottoNumbers numbers = inputWinningNumbers();
+        LottoNumber bonus = inputBonusNumber();
 
-        } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NUMBER.message());
+        return new WinningLotto(numbers, bonus);
+    }
+
+    private static LottoNumbers inputWinningNumbers() {
+        try {
+            System.out.println(NEW_LINE + "당첨 번호를 입력해 주세요.");
+            String read = Console.readLine();
+            List<LottoNumber> lottoNumbers = Arrays.stream(read.split(NUMBERS_DELIMITER))
+                    .map(String::trim)
+                    .map(InputView::toInteger)
+                    .map(LottoNumber::from)
+                    .toList();
+
+            return new LottoNumbers(lottoNumbers);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputWinningNumbers();
         }
     }
 
-    private static List<Number> inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.\n");
-        String numbers = Console.readLine();
-        return Arrays.stream(numbers.split(NUMBERS_DELIMITER))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .map(Number::from)
-                .toList();
+    private static LottoNumber inputBonusNumber() {
+        try {
+            System.out.println(NEW_LINE + "보너스 번호를 입력해 주세요.");
+            int bonus = toInteger(Console.readLine());
+
+            return LottoNumber.from(bonus);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputBonusNumber();
+        }
     }
 
-    private static Number inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.\n");
-        String bonus = Console.readLine();
-        return Number.from(Integer.parseInt(bonus));
+    private static int toInteger(String read) {
+        try {
+            return Integer.parseInt(read);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_NUMBER.message());
+        }
     }
 }

e9f35b1e7c13e313f0097f32697b4cb0288d9ee5
dmdwns2 <ssreki1@naver.com>
2023-11-07 22:45:17 +0900

    feat: create input winning numbers

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,19 +2,38 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
+import static java.util.Arrays.stream;
 import static lotto.Error.*;
 
 public class Input {
-    public int inputPurchaseAmount() {
+    public String input() {
         try {
-            String purchaseAmount = Console.readLine();
-            validatePurchaseAmount(purchaseAmount);
-            return Integer.parseInt(purchaseAmount);
+            return Console.readLine();
         } catch (NoSuchElementException e) {
             throw new IllegalArgumentException(INPUT_EMPTY.getErrMsg());
         }
+    }
+
+    public int inputPurchaseAmount() {
+        String purchaseAmount = input();
+        validatePurchaseAmount(purchaseAmount);
+        return Integer.parseInt(purchaseAmount);
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        String inputString = input();
+        List<Integer> winningNumbers = stream(inputString.split(","))
+                .map(Integer::valueOf)
+                .toList();
+        validateWinningNumbersSize(winningNumbers);
+        validateWinningNumbersVal(winningNumbers);
+        validateDuplication(winningNumbers);
+        return winningNumbers;
     }
 
     private void validatePurchaseAmount(String purchaseAmount) {
@@ -26,6 +45,29 @@ public class Input {
         }
         if ((temp / 1000) != 0) {
             throw new IllegalArgumentException(UNDIVIDED_PURCHASE_AMOUNT.getErrMsg());
+        }
+    }
+
+    private void validateWinningNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(INVALID_NUMBERS_SIZE.getErrMsg());
+        }
+    }
+
+    private void validateWinningNumbersVal(List<Integer> numbers) {
+        for (int i = 0; i < 6; i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45) {
+                throw new IllegalArgumentException(INVALID_NUMBERS_VALUE.getErrMsg());
+            }
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> temp = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!temp.add(number)) {
+                throw new IllegalArgumentException(DUPLICATE_NUMBERS.getErrMsg());
+            }
         }
     }
 }

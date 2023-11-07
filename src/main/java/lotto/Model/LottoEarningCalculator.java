2db839524040c774563b10945b1eeae28663d80f
mksystem98 <mksystem98@gmail.com>
2023-11-08 04:12:34 +0900

    feat: 수익률을 소수점 둘째 자리에서 반올림하는 기능 추가
    - return earningRateBD;에서 return earningRateBD.setScale(2, RoundingMode.HALF_UP);로 수정하여 소수점 둘째 자리에서 반올림하도록 수정함

--- src/main/java/lotto/Model/LottoEarningCalculator.java
+++ src/main/java/lotto/Model/LottoEarningCalculator.java
@@ -5,16 +5,13 @@ import java.math.RoundingMode;
 
 public class LottoEarningCalculator {
     public static BigDecimal calculateEarningsRate(long totalPrizeMoney, int numberOfTickets, int lottoPrice) {
-        // 총 상금을 BigDecimal로 변환
-        BigDecimal totalPrizeMoneyBD = new BigDecimal(totalPrizeMoney);
 
-        // 투자한 금액 계산
+        BigDecimal totalPrizeMoneyBD = new BigDecimal(totalPrizeMoney);
         BigDecimal investedMoneyBD = new BigDecimal((long)numberOfTickets * lottoPrice);
 
-        // 수익률 계산 (총 상금 / 투자한 금액)
         BigDecimal earningRateBD = totalPrizeMoneyBD.divide(investedMoneyBD, 4, RoundingMode.HALF_UP)
                 .multiply(BigDecimal.valueOf(100));
 
-        return earningRateBD;
+        return earningRateBD.setScale(2, RoundingMode.HALF_UP);
     }
 }

2378df79b38bf9bc4e4119413c1693ddccb3b52b
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 21:25:07 +0900

    refactor: 테스트에도 오류메시지 사용하기 위해 제어자를 public으로 변경

--- src/main/java/lotto/Model/Price.java
+++ src/main/java/lotto/Model/Price.java
@@ -1,18 +1,22 @@
 package lotto.Model;
 
+import static lotto.Constants.DEFAULT;
+import static lotto.Constants.MAX_LOTTO_PURCHASE_PRICE;
+import static lotto.Constants.MAX_LOTTO_PURCHASE_PRICE_TO_STRING;
+import static lotto.Constants.ONE_HUNDRED_PERCENT;
+import static lotto.Constants.TENTHS;
+import static lotto.Constants.UNIT_OF_ONE_LOTTO_PRICE;
 import static lotto.Utils.toInt;
 
 public class Price {
-    private static final int ZERO = 0;
-    private static final int ONE_HUNDRED_PERCENT = 100;
-    private static final int UNIT_OF_ONE_LOTTO_PRICE = 1000;
-    private static final int MAX_LOTTO_PURCHASE_PRICE = 2000000000;
-
-    private static final String ERROR_NOT_NUMBER =
-            "[ERROR] 공백이나 쉼표같은 특수기호 없이 숫자만을 입력하세요.(최대 구입금액은 20억입니다)";
-    private static final String ERROR_UNIT_PRICE_OF_PURCHASED_LOTTO = "[ERROR] 1000원 단위로 입력하세요.";
-    private static final String ERROR_NOT_PURCHASE_LOTTO = "[ERROR] 로또구입금액은 최소 1000원입니다.";
-    private static final String ERROR_OVER_MAX_PRICE_OF_LOTTO_FOR_PURCHASE = "[ERROR] 로또구입금액은 최대 20억입니다.";
+    public static final String ERROR_NOT_NUMBER =
+            String.format("[ERROR] 숫자만을 입력하세요.(최대 구입금액은 %s입니다)", MAX_LOTTO_PURCHASE_PRICE_TO_STRING);
+    public static final String ERROR_UNIT_PRICE_OF_PURCHASED_LOTTO =
+            String.format("[ERROR] %d원 단위로 입력하세요.", UNIT_OF_ONE_LOTTO_PRICE);
+    public static final String ERROR_NOT_PURCHASE_LOTTO =
+            String.format("[ERROR] 로또구입금액은 최소 %d원입니다.", UNIT_OF_ONE_LOTTO_PRICE);
+    public static final String ERROR_OVER_MAX_PRICE_OF_LOTTO_FOR_PURCHASE =
+            String.format("[ERROR] 로또구입금액은 최대 %s입니다.", MAX_LOTTO_PURCHASE_PRICE_TO_STRING);
 
     int purchasePrice;
 
@@ -27,9 +31,9 @@ public class Price {
 
     public double makeWinningRate(int totalWinningPrize) {
         if (isZero(totalWinningPrize)) {
-            return ZERO;
+            return DEFAULT;
         }
-        return dividePurchasePriceWith(totalWinningPrize) * ONE_HUNDRED_PERCENT;
+        return (double) Math.round(dividePurchasePriceWith(totalWinningPrize) * ONE_HUNDRED_PERCENT * TENTHS) / TENTHS;
     }
 
     private void validate(String input) throws IllegalArgumentException {
@@ -47,13 +51,13 @@ public class Price {
     }
 
     private void validateDividingUnitOfOneLottoPrice(int validInput) {
-        if (validInput % UNIT_OF_ONE_LOTTO_PRICE != ZERO) {
+        if (validInput % UNIT_OF_ONE_LOTTO_PRICE != DEFAULT) {
             throw new IllegalArgumentException(ERROR_UNIT_PRICE_OF_PURCHASED_LOTTO);
         }
     }
 
     private void validateNumberRange(int validInput) {
-        if (validInput <= UNIT_OF_ONE_LOTTO_PRICE) {
+        if (validInput < UNIT_OF_ONE_LOTTO_PRICE) {
             throw new IllegalArgumentException(ERROR_NOT_PURCHASE_LOTTO);
         }
         if (validInput > MAX_LOTTO_PURCHASE_PRICE) {
@@ -62,10 +66,10 @@ public class Price {
     }
 
     private boolean isZero(int totalPrize) {
-        return totalPrize == ZERO;
+        return totalPrize == DEFAULT;
     }
 
     private double dividePurchasePriceWith(int totalPrize) {
-        return (double)totalPrize / purchasePrice;
+        return (double) totalPrize / purchasePrice;
     }
 }

80f473a86bd6a66ff678522bbbc39ac11d283cbd
jae woo Jung <awfjol2008@naver.com>
2023-11-07 13:44:40 +0900

    feat: 당첨 번호 입력을 요구하는 시스템 메시지 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -13,4 +13,8 @@ public class OutputView {
     protected void printPurchaseLottoCountMessage(int purchaseLottoCount) {
         System.out.printf("%d개를 구매했습니다.%n", purchaseLottoCount);
     }
+
+    protected void printReadLottoNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }

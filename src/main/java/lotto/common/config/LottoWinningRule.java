de437be7254c5fc7badd5a23bff78d1d60ac9299
ddolboghi <rhdfyd128@gmail.com>
2023-11-07 15:05:10 +0900

    feat: LottoWinningRule에 information 항목 추가

--- src/main/java/lotto/common/config/LottoWinningRule.java
+++ src/main/java/lotto/common/config/LottoWinningRule.java
@@ -3,23 +3,25 @@ package lotto.common.config;
 import java.util.Arrays;
 
 public enum LottoWinningRule {
-    RANK_ONE(1, 6, false, 2000000000),
-    RANK_TWO(2, 5, true, 30000000),
-    RANK_THREE(3, 5, false, 1500000),
-    RANK_FOUR(4, 4, false, 50000),
-    RANK_FIVE(5, 3, false, 5000),
-    UNRANKED(0, 0, false, 0);
+    RANK_ONE(1, 6, false, 2000000000, "6개 일치 (2,000,000,000원)"),
+    RANK_TWO(2, 5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    RANK_THREE(3, 5, false, 1500000, "5개 일치 (1,500,000원)"),
+    RANK_FOUR(4, 4, false, 50000, "4개 일치 (50,000원)"),
+    RANK_FIVE(5, 3, false, 5000, "3개 일치 (5,000원)"),
+    UNRANKED(0, 0, false, 0, "일치하는 볼이 없습니다.");
 
     private final int rank;
     private final int matchNumber;
     private final boolean bonusMatch;
     private final int money;
+    private final String information;
 
-    LottoWinningRule(int rank, int matchNumber, boolean bonusMatch, int money) {
+    LottoWinningRule(int rank, int matchNumber, boolean bonusMatch, int money, String information) {
         this.rank = rank;
         this.matchNumber = matchNumber;
         this.bonusMatch = bonusMatch;
         this.money = money;
+        this.information = information;
     }
 
     public static LottoWinningRule getRank(int countMatch, boolean bonusMatch) {
@@ -39,5 +41,9 @@ public enum LottoWinningRule {
 
     public int getMoney() {
         return money;
+    }
+
+    public String getInformation() {
+        return information;
     }
 }

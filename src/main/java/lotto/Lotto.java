f709e3f86f08a95465669c3c9c59abdfadec4d27
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-11-07 21:58:00 +0900

    feat ($Lotto) : Lotto 서로다른 숫자인지 검증 함수 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +20,10 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers){
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (numbers.size() != distinctNumbers.size()){
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
 }

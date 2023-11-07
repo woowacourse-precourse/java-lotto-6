1c7a2a477ea188a23ad2d035272c7988cbde4c60
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-11-07 20:00:04 +0900

    Feat: controller로 PlayLotto 생성

---
+++ src/main/java/lotto/controller/PlayLotto.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+import lotto.view.Input;
+import lotto.view.Output;
+
+public class PlayLotto {
+    Input input = new Input();
+    Output output = new Output();
+
+    public void playLotto() {
+
+    }
+}

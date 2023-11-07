520b572ea25f06b6bc7103057e2e32a54c16eba3
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-07 21:16:42 +0900

    feat(WinningNumber): 보너스 번호 범위 검증 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -3,14 +3,17 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.List;
 import lotto.util.ErrorMessage;
+import lotto.util.Validator;
 
 public class WinningNumber {
     private static final String SEPARATOR = ",";
     private final Lotto lotto;
     private final int bonusNumber;
 
-    public WinningNumber(String lottoNumbers, String bonus) {
+    public WinningNumber(String lottoNumbers, int bonus) {
         validateLotto(lottoNumbers);
+        List<Integer> numbers = stringToIntList(lottoNumbers);
+        validateBonusNumber(numbers, bonus);
     }
 
     private static void validateLotto(String numbers) {
@@ -21,5 +24,14 @@ public class WinningNumber {
             ErrorMessage.printNotNumericNumberErrorMessage();
             throw new IllegalArgumentException();
         }
+    }
+
+    private static void validateBonusNumber(List<Integer> lotto, int bonus) {
+        Validator.validateInputRange(bonus);
+    }
+
+    private static List<Integer> stringToIntList(String numbers) {
+        return Arrays.stream(numbers.split(SEPARATOR))
+                .map(Integer::parseInt).toList();
     }
 }

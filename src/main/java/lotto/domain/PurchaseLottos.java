9accbf40ebc99bf65a29bfeef19220295dba7304
GiJung <weeds2577@gmail.com>
2023-11-08 00:24:50 +0900

    feat(PurchaseLottos): 구매한 로또들을 관리하는 객체 생성

---
+++ src/main/java/lotto/domain/PurchaseLottos.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import lotto.exception.ErrorCode;
+
+import java.util.List;
+
+public class PurchaseLottos {
+
+    private final List<Lotto> purchaseLottos;
+
+    public PurchaseLottos(List<Lotto> purchaseLottos) {
+        validatorSize(purchaseLottos);
+        this.purchaseLottos = purchaseLottos;
+    }
+
+    private void validatorSize(List<Lotto> purchaseLottos) {
+        if (purchaseLottos.isEmpty()) {
+            throw new IllegalArgumentException(ErrorCode.NOT_FOUND_PURCHASE_LOTTO.getMessage());
+        }
+    }
+}

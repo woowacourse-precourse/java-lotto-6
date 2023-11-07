f84a7a59a8a8b3f8939785cfd5b4852a60ade491
jhva <junghun8158@naver.com>
2023-11-07 15:36:03 +0900

    :sparkles: feat(LottoDto): 보너스,로또 번호 일치 기능

--- src/main/java/lotto/dto/LottoDto.java
+++ src/main/java/lotto/dto/LottoDto.java
@@ -1,7 +1,27 @@
 package lotto.dto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import lotto.model.Lotto;
 
-public record LottoDto(List<Integer> lottoRandomNumbers) {
+public record LottoDto(List<Integer> lottoRandomNumbers, Lotto lotto) {
 
+    public Map<Integer, Integer> matchCount(List<Integer> lottoWinnerNumbers,
+            int bonus) {
+        int count = lotto.matchLottoCount(lottoWinnerNumbers);
+        int bonusCount = isContainsCount(bonus);
+        Map<Integer, Integer> countMap = new HashMap<>();
+        countMap.put(count, bonusCount);
+        return countMap;
+    }
+
+    public int isContainsCount(int bonus) {
+        int count = 0;
+        boolean isContains = lotto.isBonusMatch(bonus);
+        if (isContains) {
+            count++;
+        }
+        return count;
+    }
 }

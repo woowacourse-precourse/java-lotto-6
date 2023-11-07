ab2535bf1b3da51bd803662af33c04d570ec710b
dyjeong365 <dyjeong365@gmail.com>
2023-11-07 21:52:46 +0900

    feat: add getTickets()

--- src/main/java/service/LottoGame.java
+++ src/main/java/service/LottoGame.java
@@ -1,11 +1,33 @@
 package service;
 
 import camp.nextstep.edu.missionutils.Console;
+import util.LottoNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGame {
+    final int TICKET_PRICE = 1000;
+    final int MIN_LOTTO_NUMBER = 1;
+    final int MAX_LOTTO_NUMBER = 45;
+    final int NUMBER_OF_LOTTO_NUMBERS = 6;
 
     public void start() {
         int ticketPurchaseAmount = getTicketPurchaseAmount();
+        int ticketQuantity = ticketPurchaseAmount / TICKET_PRICE;
+
+        List<List<Integer>> tickets = getTickets(ticketQuantity);
+    }
+
+    private List<List<Integer>> getTickets(int ticketQuantity) {
+        List<List<Integer>> tickets = new ArrayList<>();
+
+        for (int i = 0; i < ticketQuantity; i++) {
+            tickets.add(LottoNumberGenerator.generate(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUMBER_OF_LOTTO_NUMBERS));
+        }
+
+        System.out.println();
+        return tickets;
     }
 
     private static int getTicketPurchaseAmount() {

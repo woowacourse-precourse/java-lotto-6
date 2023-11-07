f30f0b653ad8742969779f173630f2ecef6f07cf
윤병호 <pdot715@gmail.com>
2023-11-08 08:12:34 +0900

    feat(Lotto):사용자 번호 입력시 중복하는 번호가 있으면 오류 발생

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +13,25 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력한 번호가 6자리가 아닙니다.");
+        } else if (duplicateCheck(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 입력한 번호 중 중복하는 번호가 있습니다.");
         }
     }
+
+    private boolean duplicateCheck(List<Integer> numbers) {
+        List<Integer> tempNumbers = new ArrayList<>();
+        for(int i = 0; i < numbers.size(); i++) {
+            if(tempNumbers.contains(numbers.get(i))) {
+                tempNumbers.add(numbers.get(i));
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
     // TODO: 추가 기능 구현
 }

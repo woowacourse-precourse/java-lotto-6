2bc3bf11c84487b7c0f2048ade647ff19c0f3deb
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 22:38:54 +0900

    feat: InputView 함수 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,12 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.model.Constants.INVALID_MONEY_FORMAT;
+
 public class InputView {
     private static final InputView instance = new InputView();
 
@@ -8,5 +15,47 @@ public class InputView {
     }
 
     private InputView() {
+    }
+
+    public Integer getPurchaseAmount() throws NumberFormatException{
+        System.out.println();
+        System.out.println("구입금액을 입력해 주세요.");
+
+        int result;
+
+        try{
+            result = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e){
+            System.out.println(INVALID_MONEY_FORMAT);
+            throw new IllegalArgumentException();
+        }
+
+        return result;
+    }
+
+    public List<Integer> getInputNumbers(){
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        return stringToIntegerList(Console.readLine());
+    }
+
+    public Integer getInputBonusNumber(){
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private List<Integer> stringToIntegerList(String string){
+        List<Integer> inputList = new ArrayList<>();
+
+        String[] result = string.split(",");
+
+        for (String s : result) {
+            inputList.add(Integer.parseInt(s));
+        }
+
+        return inputList;
     }
 }

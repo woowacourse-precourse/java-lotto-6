16ff73b53c35424cc56d5c0b9639c8c8d7eb3b80
inwoo <korean5078@gmail.com>
2023-11-07 14:25:31 +0900

    fix(InputVIew): 로또 구매 금액 입력 시 예외 처리 후 다시 입력받게 수정 1. 입력한 값을 확인하고 예외 처리하는 validate 함수 작성 2. 숫자 이외의 값이 와도 예외 처리 하도록 수정 3. inputPrice 함수에서 입력 값이 예외 발생 시 처리하고 다시 작성하게 하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,6 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         InputView inputView = new InputView();
-        try {
-            inputView.inputPrice();
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR]");
-            inputView.inputPrice();
-        }
+        inputView.inputPrice();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,31 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public int inputPrice() throws IllegalArgumentException {
-        int price = Integer.parseInt(Console.readLine());
+    public int inputPrice() {
+        String input = Console.readLine();
 
-        if (price % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액이 1000원으로 나누어 떨어지지 않는다.");
+        while (!validate(input)) {
+            input = Console.readLine();
         }
-        return Integer.parseInt(Console.readLine());
+        return Integer.parseInt(input);
+    }
+
+    private boolean validate(String input) {
+        try {
+            int price = Integer.parseInt(input);
+            checkDividedByThousand(price);
+            return true;
+        } catch (NumberFormatException ne) {
+            System.out.println("[ERROR] 숫자를 입력하세요.");
+        } catch (IllegalArgumentException ie) {
+            System.out.println("[ERROR] 금액은 1000원 단위여야 합니다.");
+        }
+        return false;
+    }
+
+    private void checkDividedByThousand(int price) throws IllegalArgumentException {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }

d788b88db537fb3404e1806d904b5638c80bda22
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 18:53:43 +0900

    feat(Constants) : Formatted 기준 상수 추가
    몇 자리수에서 반올림 하는지, 상수로 표현 합니다.

--- src/main/java/lotto/system/Constants.java
+++ src/main/java/lotto/system/Constants.java
@@ -4,7 +4,8 @@ public enum Constants {
     LOTTO_SIZE(6),
     MONEY_UNIT(1000),
     START_NUMBER(1),
-    END_NUMBER(45);
+    END_NUMBER(45),
+    YIELD_PRECISION(1);
 
     public int constants;
 

50142fdfe95de80546036e0c346a78a25bf4348b
mina <qkralsdk991@naver.com>
2023-11-07 16:36:28 +0900

    Refactor : 에러 문구 변경

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class Parser {
 
     private static String INPUT_STYLE_ERROR_INT = "[ERROR] 공백을 포함하지 않는 한 개의 숫자를 입력해야 합니다.";
-    private static String INPUT_STYLE_ERROR_STRING = "[ERROR] 공백을 포함하지 않는 여섯 개의 숫자를 입력해야 합니다.";
+    private static String INPUT_STYLE_ERROR_STRING = "[ERROR] 공백을 포함하지 않는 숫자를 입력해야 합니다.";
 
     public List<Integer> translatePlayerInputStringToInt(String playerInputString){
         try {

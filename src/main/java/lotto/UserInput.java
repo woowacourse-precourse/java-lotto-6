be474a916c98948861906bc6502e90185086e168
K-KY <kyuyoungk@naver.com>
2023-11-07 23:25:48 +0900

    Refactor(UserInput) : 금액 검증메서드 제거

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -9,14 +9,7 @@ public class UserInput {
     public int money() {
         String input = Console.readLine();
         int money = typeChanger.from(input);
-        isValidMoney(money);
         return money;
-    }
-
-    private void isValidMoney(int money) {
-        ValidateMoney.isOverThousand(money);
-        ValidateMoney.isUnitFollowed(money);
-        ValidateMoney.isMoneyInRange(money);
     }
 
     public List<Integer> lottoNumber() {

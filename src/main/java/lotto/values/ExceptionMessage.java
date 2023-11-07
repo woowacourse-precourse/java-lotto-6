af1651ed02242ce0e3fee124bcc8571136b2ed20
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-11-08 02:00:24 +0900

    feat(lotto): 예외처리 추가

--- src/main/java/lotto/values/ExceptionMessage.java
+++ src/main/java/lotto/values/ExceptionMessage.java
@@ -9,10 +9,12 @@ public enum ExceptionMessage {
     ONLY_ONE_FOR_BONUS_NUMBER("보너스 번호는 하나만 입력할 수 있습니다."),
     NOT_CORRECT_FORM("입력 양식에 맞지 않습니다."),
     NOT_NULL("입력값이 없습니다."),
+    DUPLICATE_NUMBER("중복된 수를 뽑을 수 없습니다."),
     NOT_NUMBER("숫자만 입력할 수 있습니다."),
     CAN_NOT_PURCHASE("1000원 단위로만 구매할 수 있습니다."),
     TOO_MUCH_MONEY("로또는 최대 "+MAX_VALUE.getValue()+"원까지 구매할 수 있습니다."),
     ONLY_NUMBER("^[0-9]*$"),
+    PARTITION(","),
     ERROR("[ERROR] ");
     private final String message;
 

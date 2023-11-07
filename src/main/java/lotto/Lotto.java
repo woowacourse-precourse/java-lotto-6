d553fb7c941183abd97866fd50f46c48fd6ed769
isakdevelop <daishi7462@naver.com>
2023-11-08 03:40:35 +0900

    refactor: Lotto 클래스 내의 validate 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.lottoUI.ERRORUI;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,13 +12,32 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+
+        WinningStatistics winningStatistics = new WinningStatistics(Collections.singletonList(this));
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERRORUI.InputSixNumberError());
+        }
+
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ERRORUI.CategoryOfValueError());
+            }
+        }
+
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(ERRORUI.SameNumberExistError());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

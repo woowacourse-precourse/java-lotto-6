ab558062f825bf27e3100a792708b06976070f55
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-07 15:40:16 +0900

    feat(validator): 유효성 검증 클래스 작성

--- src/main/java/lotto/validator/PriceValidator.java
+++ src/main/java/lotto/validator/PriceValidator.java
@@ -1,18 +1,18 @@
 package lotto.validator;
 
-import lotto.ErrorMessages;
+import lotto.utils.ErrorCode;
 
 public class PriceValidator {
         public void checkBlank(String input){
-                if(input.isEmpty()) throw new IllegalArgumentException(ErrorMessages.INVALID_MONEY.getMessage());
+                if(input.isEmpty()) throw new IllegalArgumentException(ErrorCode.INVALID_MONEY.getMessage());
         }
 
         public void checkNumber(String input){
-                if(input.matches("[^0-9]")) throw new IllegalArgumentException(ErrorMessages.INVALID_MONEY.getMessage());
+                if(input.matches("[^0-9]*")) throw new IllegalArgumentException(ErrorCode.INVALID_MONEY.getMessage());
         }
 
         public void checkWon(String input){
                 long money = Long.parseLong(input);
-                if(money%1000!=0) throw new IllegalArgumentException(ErrorMessages.INVALID_MONEY.getMessage());
+                if(money%1000!=0) throw new IllegalArgumentException(ErrorCode.INVALID_MONEY.getMessage());
         }
 }

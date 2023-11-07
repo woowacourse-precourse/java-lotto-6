074babfebaab3c242108697ab7688359f80417d8
jeli01 <jeli01@naver.com>
2023-11-07 20:53:13 +0900

    style: change study test code public to default
    match the access modifier with other tests
    for uniformity.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -24,21 +24,21 @@ public class SetTest {
 
     @Test
     @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습 테스트")
-    public void size_메소드_학습_테스트() {
+    void size_메소드_학습_테스트() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 @ValueSource를 이용해 확인하는 학습 테스트")
-    public void contains_메소드_학습_테스트_by_ValueSouce(int input) {
+    void contains_메소드_학습_테스트_by_ValueSouce(int input) {
         Assertions.assertThat(numbers.contains(input)).isTrue();
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "4:false"}, delimiter = ':')
     @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 @CsvSource를 이용해 확인하는 학습 테스트")
-    public void contains_메소드_학습_테스트_by_CsvSource(int input, boolean expected) {
+    void contains_메소드_학습_테스트_by_CsvSource(int input, boolean expected) {
         Assertions.assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class StringTest {
     @Test
     @DisplayName("\"1,2\"을 , 로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트")
-    public void split_일반적인_경우() {
+    void split_일반적인_경우() {
         String input = "1,2";
         String[] split = input.split(",");
         Assertions.assertThat(split).containsExactly("1", "2");
@@ -15,7 +15,7 @@ public class StringTest {
 
     @Test
     @DisplayName("\"1\"을 , 로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트")
-    public void split_한_원소만_있을_경우() {
+    void split_한_원소만_있을_경우() {
         String input = "1";
         String[] split = input.split(",");
         Assertions.assertThat(split).containsExactly("1");
@@ -23,7 +23,7 @@ public class StringTest {
 
     @Test
     @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 () 을 제거하고 \"1,2\"를 반환하는지에 대한 학습 테스트.")
-    public void split_괄호_제거_후() {
+    void split_괄호_제거_후() {
         String input = "(1,2)";
         String purityInput = input.substring(1, input.length() - 1);
         String[] split = purityInput.split(",");
@@ -33,7 +33,7 @@ public class StringTest {
     @Test
     @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는지에 대한 학습 테스트\n"
             + "테스트를 구현한다.")
-    public void charAt_각_문자를_가져오기() {
+    void charAt_각_문자를_가져오기() {
         String input = "abc";
         int idx = 1;
         char idxOneValue = input.charAt(idx);
@@ -43,7 +43,7 @@ public class StringTest {
     @Test
     @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면\n"
             + "StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트")
-    public void charAt_위치_값을_벗어나면_예외_발생() {
+    void charAt_위치_값을_벗어나면_예외_발생() {
         String input = "abc";
         int idx = input.length();
         Assertions.assertThatThrownBy(() -> {

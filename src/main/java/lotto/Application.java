2249e032724e3c6e3e4a37fb54327adde4c0d52e
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 13:20:50 +0900

    fix(Application): lottos List<List<Integer>> 초기화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import javax.lang.model.type.ArrayType;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +14,14 @@ public class Application {
 
         List<Integer> lottoNumbers = null;
         List<Integer> eachLottoNumber = null;
-        List<List<Integer>> lottos = null;
+        List<List<Integer>> lottos = new ArrayList<>();
 
         System.out.println("구입금액을 입력해 주세요.");
 
         UserInput userInput = new UserInput();
         Process lottoCount = new Process();
 
-        while(UserInput.repeatPurchase) {
+        while (UserInput.repeatPurchase) {
             purchaseAmount = userInput.getPurchaseAmount();
             userInput.validatePurchaseAmount(purchaseAmount);
             purchaseCount = lottoCount.countLottos(purchaseAmount);
@@ -30,11 +31,10 @@ public class Application {
         System.out.printf("%d개를 구매했습니다.%n", purchaseCount); //
 
 
-
         //로또 개수만큼 로또 객체의 로또 번호 출력
         Process startProcess = new Process();
         //로또 개수만큼 반복 => 나중에 개별 함수화 리팩토링!
-        for(int i = 0; i < purchaseCount; i++){
+        for (int i = 0; i < purchaseCount; i++) {
             lottoNumbers = startProcess.generateLottoNumbers();
             startProcess.sortLottoNumbers(lottoNumbers);
             Lotto lotto = new Lotto(lottoNumbers);
@@ -46,9 +46,6 @@ public class Application {
         System.out.println();
 
 
-
-
-
         //당첨 번호 입력
         List<Integer> winningNumbers = null;
 
@@ -56,7 +53,7 @@ public class Application {
 
         UserInput userInputWinningNumber = new UserInput();
 
-        while(UserInput.repeatWinningNumber) {
+        while (UserInput.repeatWinningNumber) {
             winningNumbers = userInputWinningNumber.getWinningNumbers();
             userInputWinningNumber.sortWinningNumbers(winningNumbers);
             userInputWinningNumber.hasDuplicates(winningNumbers);
@@ -71,12 +68,13 @@ public class Application {
 
         UserInput userInputBonusNumber = new UserInput();
 
-        while(UserInput.repeatBonusNumber){
+        while (UserInput.repeatBonusNumber) {
             bonusNumber = userInputBonusNumber.getBonusNumber();
             userInputBonusNumber.overlapWithNumber(winningNumbers, bonusNumber);
         }
 
         System.out.println();
+
 
         ///
         List<Integer> winRecordBoard = new ArrayList<>();
@@ -93,8 +91,11 @@ public class Application {
         winRecordBoard.set(5, fifthPlace);
 
         ///
-        Process middleProcess = new Process();
-        List<Object> result = middleProcess.compareLottos(eachLottoNumber, winningNumbers, bonusNumber);
+
+        // lottos에서 각 로또 번호와 당첨번호&보너스 번호를 비교하고, winRecordBoard에 기록하기
+        for (var lottoNumber : lottos) {
+            Process middleProcess = new Process();
+            List<Object> result = middleProcess.compareLottos(lottoNumber, winningNumbers, bonusNumber);
 
             int mainMatchNumber = (int) result.get(0);
             boolean matchedBonusNumber = (boolean) result.get(1);
@@ -117,19 +118,14 @@ public class Application {
                 winRecordBoard.set(1, ++firstPlace);
             }
 
+        }
 
         //당첨 통계 출력
         System.out.println("당첨 통계");
         System.out.println("---");
 
 
-
-
-
-
-
         //수익률 출력
-
 
 
     }

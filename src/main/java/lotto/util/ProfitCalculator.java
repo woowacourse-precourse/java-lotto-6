70e057a89b47474841c7e1d582b22509c94aeaf3
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-07 20:32:35 +0900

    fix(ProfitCalculator): 소수점 둘째자리에서 반올림되도록 수정

--- src/main/java/lotto/util/ProfitCalculator.java
+++ src/main/java/lotto/util/ProfitCalculator.java
@@ -1,11 +1,20 @@
 package lotto.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class ProfitCalculator {
     private static final int PERCENT = 100;
+    private static final int SCALE = 1;
     public static float calculateLottoReturnRate(int totalCost, long totalPrize) {
         if (totalPrize == 0) {
             return 0f;
         }
-        return (totalPrize / (float) totalCost) * PERCENT;
+
+        BigDecimal prize = new BigDecimal(totalPrize);
+        BigDecimal cost = new BigDecimal(totalCost);
+        BigDecimal returnRate = prize.divide(cost, SCALE, RoundingMode.HALF_UP).multiply(new BigDecimal(PERCENT));
+
+        return returnRate.floatValue();
     }
 }

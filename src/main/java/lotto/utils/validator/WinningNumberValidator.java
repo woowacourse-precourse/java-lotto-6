402ddf39f8acbbb7beeb99e2eda63eec27ebafff
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 16:07:30 +0900

    feat(WinningNumberValidator): 첫 문자에 콤마 입력을 검증하는 기능

--- src/main/java/lotto/utils/validator/WinningNumberValidator.java
+++ src/main/java/lotto/utils/validator/WinningNumberValidator.java
@@ -5,10 +5,12 @@ import lotto.utils.message.WinningInformationExceptionMessage;
 public class WinningNumberValidator {
 
     private static final int MAX_LENGTH = 20;
+    private static final char COMMA = ',';
 
     public static void validate(String input) {
         validateBlank(input);
         validateSize(input);
+        validateFirstCharacterIsComma(input);
     }
 
     private static void validateBlank(String target) {
@@ -20,6 +22,12 @@ public class WinningNumberValidator {
     private static void validateSize(String target) {
         if (target.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(WinningInformationExceptionMessage.EXCEED_LENGTH.getError());
+        }
+    }
+
+    private static void validateFirstCharacterIsComma(String target) {
+        if (target.charAt(0) == COMMA) {
+            throw new IllegalArgumentException(WinningInformationExceptionMessage.FIRST_CHARACTER_COMMA.getError());
         }
     }
 }

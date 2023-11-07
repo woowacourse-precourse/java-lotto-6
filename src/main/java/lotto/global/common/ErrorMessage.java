11a92ebe3be69bbc4136795f0ab25087765977cc
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-07 22:28:15 +0900

    Feat(ErrorMessage): 에러메세지 추가

--- src/main/java/lotto/global/common/ErrorMessage.java
+++ src/main/java/lotto/global/common/ErrorMessage.java
@@ -7,7 +7,8 @@ public enum ErrorMessage {
     RANGE_1_45("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다."),
     HUNDRED_THOUSAND_OR_UNDER("[ERROR] 10만원 이하로 구입할 수 있습니다."),
     SIX_NUMBERS("[ERROR] 6개의 숫자만 입력해 주세요."),
-    DUPLICATE("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+    DUPLICATE("[ERROR] 로또 번호는 중복이 없어야 합니다."),
+    DUPLICATE_BONUS("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 
     private String message;
 

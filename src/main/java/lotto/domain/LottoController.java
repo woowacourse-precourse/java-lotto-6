7c0e88bf02346519cba22b0c119e0111681acf3d
HYOAN <aspqll@naver.com>
2023-11-08 01:28:10 +0900

    feat(lotto): 당첨번호를 입력받고 lotto에 저장하는 기능 구현, 사용자가 잘못된 값 입력 시 해당 부분을 다시 실행하는 기능 구현

--- src/main/java/lotto/domain/LottoController.java
+++ src/main/java/lotto/domain/LottoController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
-    private Lotto lotto;
+    private static Lotto lotto;
     private LottoTicketGenerator lottoTicketGenerator;
     private UserInput userInput;
     private UserInputChecker userInputChecker;
@@ -22,6 +22,7 @@ public class LottoController {
         PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
         PURCHASE_COUNT_MESSAGE("개를 구매했습니다."),
         REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+        REQUEST_BOUNS_NUMBERS("보너스 번호를 입력해 주세요."),
         WINNING_STATS_HEADER("당첨 통계 ln ---");
 
         private String message;
@@ -41,19 +42,36 @@ public class LottoController {
 
         userLottoTickets = lottoTicketGenerator.generateLottoTicket(purchaseAmount);
         printTickets(userLottoTickets); // 구매한 로또들 번호 출력
+
+        lotto = new Lotto(handleWinningNumbers()); // 당첨번호 입력, lotto 객체에 저장
+    }
+
+    public List<Integer> handleWinningNumbers() {
+        List<Integer> winningNumbers = new ArrayList<>();
+        do {
+            System.out.println(LottoMessage.REQUEST_WINNING_NUMBERS.getMessage()); // 당첨번호 입력 안내문구 출력
+            String input = "";
+            input = userInput.input();
+            winningNumbers = userInput.getUserWinningNumbers(userInputChecker, input);
+        } while (winningNumbers == null);
+
+        return winningNumbers;
     }
 
     public int handlePurchaseAmount() {
-        int ticketCount = 0;
-        String input = "";
+        int purchaseAmount = 0;
+        do {
+            int ticketCount = 0;
+            String input = "";
 
-        System.out.println(LottoMessage.PURCHASE_AMOUNT_MESSAGE.getMessage()); // 구입금액 입력 안내문구 출력
-        input = userInput.input();
+            System.out.println(LottoMessage.PURCHASE_AMOUNT_MESSAGE.getMessage()); // 구입금액 입력 안내문구 출력
+            input = userInput.input();
 
-        int purchaseAmount = userInput.getUserPurchaseAmount(userInputChecker, input);
-        ticketCount = purchaseAmount / 1000;
-        System.out.println(ticketCount + LottoMessage.PURCHASE_COUNT_MESSAGE.getMessage()); // 구매한 로또 개수 출력
+            purchaseAmount = userInput.getUserPurchaseAmount(userInputChecker, input);
+            ticketCount = purchaseAmount / 1000;
+            System.out.println(ticketCount + LottoMessage.PURCHASE_COUNT_MESSAGE.getMessage()); // 구매한 로또 개수 출력
 
+        } while (purchaseAmount == 0);
         return purchaseAmount;
     }
 

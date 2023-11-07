7072adbaf60d153a663c1a4ba2f589513e5c310d
MyStraw <kikongdosa@gmail.com>
2023-11-08 06:20:34 +0900

    Feat: 추가 오름차순 버블정렬 로직 추가
    로또번호 랜덤으로 뽑되, 오름차순으로 정렬되게끔 버블정렬 사용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,7 +10,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+        bubbleSort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -20,8 +22,8 @@ public class Lotto {
         if (notDuplicatedNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException("로또 번호는 중복되면 안돼요.");
         }
-        for (int number : numbers){
-            if (number <1 || number >45){
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("로또 번호는 1과 45 사이의 값이어야 해요.");
             }
         }
@@ -30,6 +32,24 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
+
+    private void bubbleSort(List<Integer> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    swap(list, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private void swap(List<Integer> list, int index1, int index2) {
+        int temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
+
 
     // TODO: 추가 기능 구현
 }

cee112e3e08c1942c0fe6544a48bc5fca75117b3
hongkikii <hyangmeeh@icloud.com>
2023-11-08 03:11:57 +0900

    fix: 당첨 번호 입력 시 예외 처리

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -3,6 +3,8 @@ package lotto;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Winning {
@@ -23,27 +25,41 @@ public class Winning {
     }
 
     public void saveNumbers(String readLine) {
-        validateSaveNumbers(readLine);
-        String[] split = readLine.split(",");
-        List<Integer> numbers = Arrays.stream(split)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        this.numbers = this.numbers;
-    }
-
-    private void validateSaveNumbers(String readLine) {
-        String noEmptyReadLine = removeEmpty(readLine);
-
-    }
-
-    private String removeEmpty(String readLine) {
-        return readLine.replaceAll("\\s", "");
+        List<Integer> numbers = validateNumbers(readLine);
+        this.numbers = numbers;
     }
 
     public void saveBonusNumber(String readLine) {
         validateBonusNumber(readLine);
         this.bonusNumber = bonusNumber;
+    }
+
+    private List<Integer> validateNumbers(String readLine) {
+        String noEmptyReadLine = removeEmpty(readLine);
+        checkOtherCharacter(noEmptyReadLine);
+        List<Integer> numbers = createNumbers(noEmptyReadLine);
+        numbers.forEach(this::checkBoundary);
+        return numbers;
+    }
+
+    private List<Integer> createNumbers(String noEmptyReadLine) {
+        String[] split = noEmptyReadLine.split(",");
+        return Arrays.stream(split)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private void checkOtherCharacter(String noEmptyReadLine) {
+        String regex = "^[0-9,]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(noEmptyReadLine);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String removeEmpty(String readLine) {
+        return readLine.replaceAll("\\s", "");
     }
 
     private void validateBonusNumber(String readLine) {

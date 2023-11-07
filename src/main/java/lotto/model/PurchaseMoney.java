5551d00fcedb51f697b90441d73f60a2a417a4a7
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-07 11:22:13 +0900

    feat: 최소 구매 금액 검증 method 추가

--- src/main/java/lotto/model/PurchaseMoney.java
+++ src/main/java/lotto/model/PurchaseMoney.java
@@ -1,12 +1,14 @@
 package lotto.model;
 
 public class PurchaseMoney {
+
+  private static final int MIN_PRICE = 1000;
   private static final int DIVIDE_MONEY = 1000;
 
   private int purchaseMoney;
 
   public PurchaseMoney(int purchaseMoney) {
-    validateAmount(purchaseMoney);
+    validate(purchaseMoney);
     this.purchaseMoney = purchaseMoney;
   }
 
@@ -14,9 +16,24 @@ public class PurchaseMoney {
     return purchaseMoney / DIVIDE_MONEY;
   }
 
+  public int getPurchaseMoney() {
+    return purchaseMoney;
+  }
+
+  private void validate(int purchaseMoney) {
+    validateMinPrice(purchaseMoney);
+    validateAmount(purchaseMoney);
+  }
+
+  private void validateMinPrice(int purchaseMoney) {
+    if (purchaseMoney < MIN_PRICE) {
+      throw new IllegalArgumentException("최소 구입 금액은 1,000원 이상입니다.");
+    }
+  }
+
   private void validateAmount(int purchaseMoney) {
     if (isDivisible(purchaseMoney)) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException("구입 금액은 1,000원 단위입니다.");
     }
   }
 

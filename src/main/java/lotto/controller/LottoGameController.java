c30ee149c4d4299001662e97fe85e432a2ec0755
jookbooin <3670lsh@naver.com>
2023-11-07 18:46:48 +0900

    feat : 로또 구매 금액 입력 기능 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,12 +1,10 @@
 package lotto.controller;
 
+import lotto.model.PurchaseCost;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.List;
-
-import static lotto.message.GameMessage.*;
-import static lotto.message.LottoMessage.LOTTO_COUNT;
+import static lotto.message.GameMessage.ASK_FOR_PURCHASE_COST;
 
 public class LottoGameController {
 
@@ -19,35 +17,45 @@ public class LottoGameController {
     }
 
     public void start() {
-        int purchaseCost = inputLottoPurchaseCost();
-        printLottoNumbers(purchaseCost);
-        inputWinningNumbers();
-        inputBonusNumbers();
-        printWinningStatistics();
+        PurchaseCost purchaseCost = inputLottoPurchaseCost();
+//        printLottoNumbers();
+//        inputWinningNumbers();
+//        inputBonusNumbers();
+//        printWinningStatistics();
     }
 
-    private int inputLottoPurchaseCost() {
+    private PurchaseCost inputLottoPurchaseCost() {
+
+        while (true) {
+            try {
+                String money = inputPurchaseCost();
+                return new PurchaseCost(money);
+            } catch (IllegalArgumentException e) {
+                output.printErrorMessage(e);
+            }
+        }
+    }
+
+    private String inputPurchaseCost() {
         output.printMessage(ASK_FOR_PURCHASE_COST);
-        return 8;
+        return input.purchaseCost();
     }
-
-    private void printLottoNumbers(int purchaseCost) {
-        output.printLottoCount(LOTTO_COUNT,purchaseCost);
-    }
-
-    private List<Integer> inputWinningNumbers() {
-        output.printMessage(ASK_FOR_WINNING_NUMBER);
-        return null;
-    }
-
-    private int inputBonusNumbers() {
-        output.printMessage(ASK_FOR_BONUS_NUMBER);
-        return 0;
-    }
-
-    private void printWinningStatistics() {
-        output.printMessage(WINNING_STATISTICS);
-    }
+//
+//
+//
+//    private List<Integer> inputWinningNumbers() {
+//        output.printMessage(ASK_FOR_WINNING_NUMBER);
+//        return null;
+//    }
+//
+//    private int inputBonusNumbers() {
+//        output.printMessage(ASK_FOR_BONUS_NUMBER);
+//        return 0;
+//    }
+//
+//    private void printWinningStatistics() {
+//        output.printMessage(WINNING_STATISTICS);
+//    }
 
 
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.validator.GlobalValidator.validateBlank;
+import static lotto.validator.GlobalValidator.validateNumber;
+
 public class InputView {
 
+    public String purchaseCost(){
+        String input = Console.readLine();
+        validateBlank(input);
+        validateNumber(input);
+        return input;
+    }
 }

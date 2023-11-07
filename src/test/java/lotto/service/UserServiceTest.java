9c86eec2f25f8ef5cb162ae7c1e031998c694635
eunjungL <dldmswjd322@gmail.com>
2023-11-07 23:58:35 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/lotto/service/UserServiceTest.java
+++ src/test/java/lotto/service/UserServiceTest.java
@@ -3,13 +3,11 @@ package lotto.service;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserServiceTest {

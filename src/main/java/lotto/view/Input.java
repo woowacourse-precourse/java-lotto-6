ca06f81055e067640b84fe8e8e2a52be59f02ca4
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 00:18:39 +0900

    feat: 입력 기능 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,4 +1,21 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+    public String enterAmount() {
+        return Console.readLine();
+    }
+
+    public String enterWiningNumber() {
+        return Console.readLine();
+    }
+
+    public String enterBonusNumber() {
+        return Console.readLine();
+    }
+
+    public void inputClose() {
+        Console.close();
+    }
 }

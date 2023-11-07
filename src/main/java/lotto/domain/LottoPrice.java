d34a187d588b8d28b9bac4f02e635da41caf96ea
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-11-08 02:59:14 +0900

    feat: 당첨 내역에 변수 추가 및 등수를 통한 반환 기능 구현

--- src/main/java/lotto/domain/LottoPrice.java
+++ src/main/java/lotto/domain/LottoPrice.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoPrice {
-    NONE(0, false, 0),
-    FIFTH(3, false, 5000),
-    FOURTH(4, false, 50000),
-    THIRD(5, false, 1500000),
-    SECOND(5, true, 30000000),
-    FIRST(6, false, 2000000000);
+    NONE(0, false, 0, "", 0),
+    FIFTH(3, false, 5000, "(5,000원)", 5),
+    FOURTH(4, false, 50000, "(50,000원)", 4),
+    THIRD(5, false, 1500000, "(1,500,000원)", 3),
+    SECOND(5, true, 30000000, "(30,000,000원)", 2),
+    FIRST(6, false, 2000000000, "(2,000,000,000원)", 1);
 
     private final int matchesNumber;
 
@@ -16,10 +16,15 @@ public enum LottoPrice {
 
     private final int priceMoney;
 
-    LottoPrice(int matchesNum, boolean bonusNumber, int priceMoney) {
+    private final String priceMoneyMessage;
+    private final int prize;
+
+    LottoPrice(int matchesNum, boolean bonusNumber, int priceMoney, String priceMoneyMessage, int prize) {
         this.matchesNumber = matchesNum;
         this.bonusNumber = bonusNumber;
         this.priceMoney = priceMoney;
+        this.priceMoneyMessage = priceMoneyMessage;
+        this.prize = prize;
     }
 
     public static LottoPrice getLottoPriceByMatchesNumber(int matchesNumber, boolean bonusNumber) {
@@ -31,11 +36,26 @@ public enum LottoPrice {
 
     }
 
+    public static LottoPrice getLottoPriceByPrize(int prize){
+        return Arrays.stream(LottoPrice.values())
+                .filter(lottoPrice -> lottoPrice.getPrize() == prize)
+                .findFirst()
+                .get();
+    }
+
     public int getMatchesNumber() {
         return matchesNumber;
     }
 
     public int getPriceMoney() {
         return priceMoney;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getPriceMoneyMessage() {
+        return priceMoneyMessage;
     }
 }

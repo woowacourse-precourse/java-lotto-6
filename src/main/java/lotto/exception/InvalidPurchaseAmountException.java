c44f23dc41855e5779307e03440cd04e75ca75cc
chan-won <clw8679@naver.com>
2023-11-08 02:43:46 +0900

    fix : 오류 메시지 수정

--- src/main/java/lotto/exception/InvalidPurchaseAmountException.java
+++ src/main/java/lotto/exception/InvalidPurchaseAmountException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class InvalidPurchaseAmountException extends RuntimeException {
-    private static final String MESSAGE = "[ERROR] 구입 금액은 1000원 이상이어야 합니다.";
+    private static final String MESSAGE = "[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.";
 
     public InvalidPurchaseAmountException() {
         super(MESSAGE);

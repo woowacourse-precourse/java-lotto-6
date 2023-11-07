67d16f5839b88a5eaeb1b886b6f275e383bc0e44
깃wooqqq <sge0225@naver.com>
2023-11-08 00:45:53 +0900

    feat: 입력한 당첨 번호가 로또 가능 범위인지 확인하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputView {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private static final String REQUEST_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
@@ -41,6 +43,12 @@ public class InputView {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateRangeNumber(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException();
         }
     }

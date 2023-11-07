a415f365d0d4c85355fff2eafd531fb62c984422
박수빈 <jrkd2165@gmail.com>
2023-11-07 23:05:26 +0900

    :sparkles: feat: 당첨 번호와 구입한 로또들이 몇 등인지 판단하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.*;
+import lotto.domain.enums.Rank;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 public class Application {
@@ -13,25 +13,29 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         long purchaseAmount = Integer.parseInt(Console.readLine());
         //1000의 배수 인지 판단 -> 아니라면 에러 후 종료 (정상, 예외 테스트 추가)
-        if ( purchaseAmount % 1000 != 0 ){
+        if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000의 배수여야 합니다.");
         }
 
         //1000의 배수라면 (메서드 나누기)
         //구입한 로또의 개수 출력
-        System.out.println(String.format("%d개를 구매했습니다.",purchaseAmount / 1000));
+        System.out.println(String.format("%d개를 구매했습니다.", purchaseAmount / 1000));
         //로또 생성
         //구입한 로또의 번호 출력
-        List<Lotto> lottos = Lotto.CreateLottos(purchaseAmount / 1000);
+        List<Lotto> lottos = Lotto.createLottos(purchaseAmount / 1000);
 
         //당첨 번호 입력 받기
         // ,로 분리되지 않는 경우
         // 문자만 입력한 경우
         // 6개의 수를 입력하지 않은 경우
+
         System.out.println("당첨 번호를 입력해 주세요.");
-        List<String> winningNumberOfString =List.of(Console.readLine().split(","));
-        for(int i=0;i<6;i++){
+        List<String> winningNumberOfString = List.of(Console.readLine().split(","));
+        // 그냥 아예 String으로 받으면 for문써서 int배열로 바꿔주는 걸 메서드로 빼야겠다....
+        List<Integer> winningNumber = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
             System.out.println(winningNumberOfString.get(i));
+            winningNumber.add(Integer.parseInt(winningNumberOfString.get(i)));
         }
 
         //보너스 번호 입력 받기
@@ -40,9 +44,21 @@ public class Application {
         System.out.println(bonusNumber);
 
         //구매한 로또의 번호를 보며, 몇 등인지 체크
+        //여기서 처음에 반복문을 써서. 로또 하나 보너스번호 당첨번호 넘기고 enum을 받자
+        //6인경우 미당첨 enum
+        //enum으로 rankMap 만들기
+        Map<Rank, Integer> lottoRanks = new HashMap<>();
+        //초기화 해줘야하나?
+        for (int i = 0; i < lottos.size(); i++) {
+            Rank rank = Lotto.judgeLottoRank(winningNumber, lottos.get(i), bonusNumber);
+            lottoRanks.put(rank, lottoRanks.get(rank) + 1);
+        }
+
+        //rankMap 가지고 와서 번 돈 계산
+
         //수익률 계산
         //당첨 통계 출력
-        //수익률 출력
+        //수익률 출력 (원금 - 수익금) / 원금 * 100인듯
 
     }
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.enums.Rank;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,18 +14,23 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     @Override
     public String toString() {
         return numbers.toString();
     }
 
+    //테스트코드에서 써보기
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    static List<Lotto> CreateLottos(long numberOfLotto) {
+    static List<Lotto> createLottos(long numberOfLotto) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < numberOfLotto; i++) {
             Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
@@ -32,6 +38,45 @@ public class Lotto {
             lottos.add(lotto);
         }
         return lottos;
+    }
 
+    static int countWinningNumberInLotto(List<Integer> winningNumber, Lotto lotto){
+        int count = 0;
+        //만약 당첨번호가 로또 안에 있다면 + 1
+        for (int i = 0; i < 6; i++) {
+            if (checkLottoNumberInWinningNumber(winningNumber, lotto.getNumbers().get(i))) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    //메서드 분리
+    static Rank judgeLottoRank(List<Integer> winningNumber, Lotto lotto, int bonusNumber) {
+        int count = countWinningNumberInLotto(winningNumber, lotto);
+        if (count >= 0 && count < 3) {
+            return Rank.NOT;
+        }
+        if (count == 3) {
+            return Rank.FIFTH;
+        }
+        if (count == 4) {
+            return Rank.FOURTH;
+        }
+        if (count == 5) {
+            if (isBonusNumberInLotto(lotto, bonusNumber)) {
+                return Rank.SECOND;
+            }
+            return Rank.THIRD;
+        }
+        return Rank.FIRST;
+    }
+
+    static boolean checkLottoNumberInWinningNumber(List<Integer> winningNumber, int numberInLotto) {
+        return winningNumber.contains(numberInLotto);
+    }
+
+    static boolean isBonusNumberInLotto(Lotto lotto, int bonusNumber) {
+        return lotto.getNumbers().contains(bonusNumber);
     }
 }

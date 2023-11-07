2962740c4fa7e27bfab41bd709b4be0ae910ba69
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-07 21:31:19 +0900

    feat(Lotto) : update validate method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,15 @@ public class Lotto {
 
   private void validate(List<Integer> numbers) {
     if (numbers.size() != 6) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 구성됩니다.");
+    }
+    for(int i=0; i<numbers.size(); i++) {
+      for(int j=i+1; j<numbers.size(); j++) {
+        if (numbers.get(i) == numbers.get(j))
+          throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 다른 6개의 숫자로 구성됩니다.");
+      }
+      if(numbers.get(i) < 1 || 45 < numbers.get(i))
+        throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 정수이어야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }

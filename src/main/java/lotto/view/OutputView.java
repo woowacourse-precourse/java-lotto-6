d0486a389813162f7cc78ddcb3614b71e6d0732d
jooda00 <dragon2000a@gmail.com>
2023-11-07 23:52:53 +0900

    refactor(OutputView): 메소드 static으로 만들기

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,21 +4,21 @@ import lotto.domain.Lotto;
 import lotto.domain.Player;
 
 public class OutputView {
-    public static void printAmount(int amount) {
+    public static final void printAmount(int amount) {
         System.out.println("\n" + amount + "개를 구매했습니다.");
     }
 
-    public static void printLotteries(Player player) {
+    public static final void printLotteries(Player player) {
         for (Lotto lotto : player.generateLotteries()) {
             System.out.println(lotto.getNumbers());
         }
     }
 
-    public static void printResult() {
+    public static final void printResult() {
         System.out.println("\n당첨 통계\n---");
     }
 
-    public static void printTotalRate(double rate) {
+    public static final void printTotalRate(double rate) {
         System.out.println("총 수익률은 " + rate + "%입니다.");
     }
 }

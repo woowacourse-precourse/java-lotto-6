a731fd676487ca9650e2b6e2516356f3318ed324
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-07 22:27:47 +0900

    feat : validate 수정
    숫자 중복 확인 코드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.HashSet;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,6 +13,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        if (new HashSet<Integer>(numbers).size() != 6) {
             throw new IllegalArgumentException();
         }
     }

faf7c1eafcf0b826f3015145baa36654f29ded4c
juhwan5 <lavistrike770@gmail.com>
2023-11-08 03:16:29 +0900

    Feat : 중복 숫자 검사 기능
    로또 / 당첨 번호 6개가 중복되지 않는지 검사하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,12 +2,14 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        redundancyCheck(numbers);
         this.numbers = numbers;
         Collections.sort(this.numbers);
     }
@@ -18,7 +20,19 @@ public class Lotto {
         }
     }
 
+    private void redundancyCheck(List<Integer> numbers){
+        List<Integer> check = numbers.stream().distinct().collect(Collectors.toList());
+        try {
+            validate(check);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자는 모두 달라야 합니다.");
+        }
+    }
+
     public List<Integer> getNumbers(){
         return numbers;
     }
+}
+
+enum LottoRank{
 }

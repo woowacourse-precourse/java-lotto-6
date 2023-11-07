4fac0f5c18a61d1554964f6640d6451e6d63a4da
dragon0622 <iamdragon.dev@gmail.com>
2023-11-07 21:34:11 +0900

    refactor(Lotto) : 메소드 위치 정리

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -8,14 +8,14 @@ public class Output {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void howManyLottoMessage(int countLotto){
-        System.out.println(countLotto + "개를 구매했습니다.");
-    }
-
     public static void checkLottoNumberMessage(List<List<Integer>> tickets){
         for(List<Integer> ticket : tickets){
             System.out.println(ticket);
         }
+    }
+
+    public static void howManyLottoMessage(int countLotto){
+        System.out.println("\n" + countLotto + "개를 구매했습니다.");
     }
 
     public static void winningNumberGuide(){
@@ -23,12 +23,12 @@ public class Output {
     }
 
     public static void bonusNumberGuide(){
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n" + "보너스 번호를 입력해 주세요.");
     }
 
     public static void resultLotto(){
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.println("당첨 통계");
+        System.out.println("\n" + "당첨 통계");
         System.out.println("---");
         for (Rank rank : Rank.values()){
             System.out.println(rank.getMessage() + " (" + decimalFormat.format(rank.getPrice()) + "원) - " + rank.getCnt() + "개") ;
@@ -36,6 +36,8 @@ public class Output {
     }
 
     public static void RateOfReturn(){
-
+        double rate = Rate.calculateRate();
+        rate = Math.round(rate * 100.0) / 100.0;
+        System.out.println("총 수익률은 " + rate + "입니다.");
     }
 }

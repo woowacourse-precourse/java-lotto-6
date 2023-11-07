2b8845424c54a66ab2c935fe8637cb7caf3cb1d5
준써크 <kjs8960@inha.edu>
2023-11-08 03:20:41 +0900

    feat: 로또 등수 클래스 생성

---
+++ src/main/java/lotto/constant/LottoRanking.java
@@ -0,0 +1,24 @@
+package lotto.constant;
+
+public enum LottoRanking {
+    FIFTH_RANK(5000L, "3개 일치 (5,000원)", 3, JudgeBonus.BONUS_NO_MATTER),
+    FOURTH_RANK(50000L, "4개 일치 (50,000원)", 4, JudgeBonus.BONUS_NO_MATTER),
+    THIRD_RANK(1500000L, "5개 일치 (1,500,000원)", 5, JudgeBonus.BONUS_UNEQUAL),
+    SECOND_RANK(30000000L, "5개 일치, 보너스 볼 일치 (30,000,000원)", 5, JudgeBonus.BONUS_EQUAL),
+    FIRST_RANK(2000000000L, "6개 일치 (2,000,000,000원)", 6, JudgeBonus.BONUS_NO_MATTER)
+    ;
+
+    private final Long price;
+    private final String message;
+    private final Integer matchNumber;
+    private final JudgeBonus judgeBonus;
+    private Integer count;
+
+    LottoRanking(final Long price, final String message, final Integer matchNumber, final JudgeBonus judgeBonus) {
+        this.price = price;
+        this.message = message;
+        this.matchNumber = matchNumber;
+        this.judgeBonus = judgeBonus;
+        this.count = 0;
+    }
+}

35d59d1d877e73afbc2a5ddfbc30662bf33afa33
donghyun <kdhdh001@naver.com>
2023-11-07 22:20:29 +0900

    feat: 보너스 번호 유효성 검증 구현

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -12,7 +12,9 @@ public class Constants {
     public static final String EMPTY_INPUT_MSG = ERROR + "당첨 번호를 입력해주세요.";
     public static final String MISSING_COMMA_MSG = ERROR + "당첨 번호는 쉼표(,)로 구분하여 입력해주세요.";
     public static final String INVALID_NUM_COUNT_MSG = ERROR + "당첨 번호는 6개를 입력해야 합니다.";
-    public static final String INVALID_NUM_RANGE_MSG = ERROR + "당첨 번호와 보너스 번호는 1부터 45까지의 정수를 입력해야 합니다.";
+    public static final String INVALID_NUM_RANGE_MSG = ERROR + "당첨 번호는 1부터 45까지의 정수를 쉼표를 띄우지 않고 입력해야 합니다.";
     public static final String DUPLICATE_NUM_MSG = ERROR + "중복된 번호가 있습니다. 각 번호는 서로 다른 숫자여야 합니다.";
+    public static final String INVALID_BONUS_NUMBER_MSG = ERROR + "보너스 번호는 1부터 45 사이의 정수여야 합니다.";
+    public static final String DUPLICATE_BONUS_NUMBER_MSG = ERROR + "보너스 번호가 당첨 번호와 중복됩니다.";
 
 }
---
+++ src/main/java/lotto/validation/BonusNumberInputValidator.java
@@ -0,0 +1,31 @@
+package lotto.validation;
+
+import java.util.List;
+import lotto.utils.Constants;
+
+public class BonusNumberInputValidator {
+    public static void validate(String input, List<Integer> winningNumbers) {
+        validateIsNumeric(input);
+        int bonusNumber = Integer.parseInt(input);
+        validateIsInRange(bonusNumber);
+        validateIsNotDuplicate(bonusNumber, winningNumbers);
+    }
+
+    private static void validateIsNumeric(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException(Constants.INVALID_BONUS_NUMBER_MSG);
+        }
+    }
+
+    private static void validateIsInRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(Constants.INVALID_BONUS_NUMBER_MSG);
+        }
+    }
+
+    private static void validateIsNotDuplicate(int number, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException(Constants.DUPLICATE_BONUS_NUMBER_MSG);
+        }
+    }
+}

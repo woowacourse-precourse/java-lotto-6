0519bd0504324ee762a0b4d8fca5d6744081c667
nylonmusk <2912rla@gmail.com>
2023-11-08 06:35:48 +0900

    refactor : 보너스 번호를 입력 받음과 동시에 기존 번호들과 비교하는 로직으로 변경

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -1,37 +1,47 @@
 package lotto.validator;
 
 import java.util.List;
-
+import lotto.constant.LottoNumber;
 import lotto.constant.message.ErrorMessage;
 
 public class BonusNumberValidator {
 
-    private BonusNumberValidator() {}
-
-    public static int validate(String input) {
-        try {
-            int bonusNumber = Integer.parseInt(input.trim());
-            validateRange(bonusNumber);
-            return bonusNumber;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_AMOUNT.getMessage());
-        }
+    private BonusNumberValidator() {
     }
 
-    public static int validate(int bonusNumber, List<Integer> winningNumbers) {
-        validateRange(bonusNumber);
-        validateBonusNumberNotInLottoNumbers(bonusNumber, winningNumbers);
+    public static int validate(String bonusNumberInput, String winningNumbersInput) {
+        checkIfNumeric(bonusNumberInput.trim());
+        int bonusNumber = parseBonusNumberToInt(bonusNumberInput);
+        checkRange(bonusNumber);
+        checkIfBonusNumberInWinningNumbers(bonusNumber, winningNumbersInput);
         return bonusNumber;
     }
 
-    private static void validateRange(int number) {
-        if (LottoNumberValidator.isNumberOutOfRange(number)) {
-            throw new IndexOutOfBoundsException(ErrorMessage.INVALID_RANGE.getMessage());
+    private static int parseBonusNumberToInt(String bonusNumberInput) {
+        try {
+            return Integer.parseInt(bonusNumberInput.trim());
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(ErrorMessage.INVALID_RANGE.getMessage(
+                    LottoNumber.MIN_INCLUSIVE.getNumber(), LottoNumber.MAX_INCLUSIVE.getNumber()));
         }
     }
 
-    private static void validateBonusNumberNotInLottoNumbers(int bonusNumber, List<Integer> numbers) {
-        if (numbers.contains(bonusNumber)) {
+    private static void checkIfNumeric(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT.getMessage());
+        }
+    }
+
+    private static void checkRange(int number) {
+        if (LottoNumberValidator.isNumberOutOfRange(number)) {
+            throw new IndexOutOfBoundsException(ErrorMessage.INVALID_RANGE.getMessage(
+                    LottoNumber.MIN_INCLUSIVE.getNumber(), LottoNumber.MAX_INCLUSIVE.getNumber()));
+        }
+    }
+
+    private static void checkIfBonusNumberInWinningNumbers(int bonusNumber, String winningNumbersInput) {
+        List<Integer> winningNumbers = LottoNumberValidator.convertStringToList(winningNumbersInput);
+        if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
         }
     }

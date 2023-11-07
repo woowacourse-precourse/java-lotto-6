8614b2de02303497e447ea726b7bb31a478bc5bb
Kim JunHyun <jjag1015@naver.com>
2023-11-08 03:12:34 +0900

    style(LottoFactory): 로또 숫자 정렬 스트림 API 활용

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -2,23 +2,28 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoFactory {
     public static Lotto makeNewLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(numbers);
+
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
 
         return new Lotto(numbers);
     }
 
-    public static LottoTicket makeNewLottos(Integer count) {
-        List<Lotto> lottos = new ArrayList<>();
+    public static LottoTicket makeNewLottoTicket(Integer count) {
+
+        List<Lotto> lottoData = new ArrayList<>();
+
         for (int i = 0; i < count; i++) {
-            lottos.add(makeNewLotto());
+            lottoData.add(makeNewLotto());
         }
-        return new LottoTicket(lottos);
+
+        return new LottoTicket(lottoData);
     }
 }

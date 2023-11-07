cd252ebdbf7bdea24f959241b47d72fcc647fc2e
ttalkisyrup <ttalkisyrup@gmail.com>
2023-11-08 00:16:18 +0900

    feat: 로또 번호 생성 메소드 구현

--- src/main/java/lotto/Manager.java
+++ src/main/java/lotto/Manager.java
@@ -1,14 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.view.Input;
 import lotto.view.Output;
+
+import java.util.List;
 
 public class Manager {
     private final Input input = new Input();
     private final Output output = new Output();
+    public static final int NUMBER_OF_LOTTO_NUMBER = 6;
 
     public void getAmountToPurchase() {
         output.printEnteringAmountPrompt();
         int purchasedAmount = input.readTotalPurchasedAmount();
+    }
+
+    public Lotto generateLottoTicket() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, NUMBER_OF_LOTTO_NUMBER);
+        return new Lotto(numbers);
     }
 }

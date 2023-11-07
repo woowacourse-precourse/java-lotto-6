85bd2da8f5810db7bfa9379150e7cbc9e80cf5f8
Dawin <dawin002@naver.com>
2023-11-07 17:32:56 +0900

    refactor(Rank): 로또 등수 검사용 enum 클래스 수정
    클래스 이름 변경: LottoRank -> Rank
    등수가 없는 enum 추가: NO_RANK(-1, 0)

--- src/main/java/lotto/model/enums/Rank.java
+++ src/main/java/lotto/model/enums/Rank.java
@@ -1,21 +1,22 @@
 package lotto.model.enums;
 
-public enum LottoRank {
+public enum Rank {
     RANK1(1, 6),
     RANK2(2, 5, true),
     RANK3(3, 5, false),
     RANK4(4, 4),
-    RANK5(5, 3);
+    RANK5(5, 3),
+    NO_RANK(-1, 0);
 
     private final int rank;
     private final int matchingNumbers;
     private final boolean bonusMatch;
 
-    LottoRank(int rank, int matchingNumbers) {
+    Rank(int rank, int matchingNumbers) {
         this(rank, matchingNumbers, false);
     }
 
-    LottoRank(int rank, int matchingNumbers, boolean bonusMatch) {
+    Rank(int rank, int matchingNumbers, boolean bonusMatch) {
         this.rank = rank;
         this.matchingNumbers = matchingNumbers;
         this.bonusMatch = bonusMatch;

60e591694940aae4a7cf280d4a7d12e0d0d151f5
hsyoodev <hsyoodev@gmail.com>
2023-11-07 23:39:42 +0900

    feat(lotto): 로또 번호는 중복되지 않는 6개의 숫자를 가진다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int NUMBERS_SIZE = 6;
 
     private final List<Integer> numbers;
 
@@ -12,10 +15,30 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (isNotSizeSix(numbers) || isDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException(Message.INVALID_NUMBERS.getMessage());
         }
     }
 
     // TODO: 추가 기능 구현
+    private boolean isNotSizeSix(List<Integer> numbers) {
+        return numbers.size() != NUMBERS_SIZE;
+    }
+
+    private boolean isDuplicateNumbers(List<Integer> numbers) {
+        return Set.copyOf(numbers).size() != NUMBERS_SIZE;
+    }
+
+    enum Message {
+        INVALID_NUMBERS("로또는 중복되지 않는 6개의 숫자만 가능합니다.");
+        private final String message;
+
+        Message(String message) {
+            this.message = String.format("[Error] %s", message);
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }

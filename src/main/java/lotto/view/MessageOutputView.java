4bab3ddfcda2efc232e50f6dd311ea6dfa17d1c1
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 04:06:39 +0900

    refactor : view 패키지에 맞게 MessageOutputView 로 객체 이름 변경

--- src/main/java/lotto/view/MessageOutputView.java
+++ src/main/java/lotto/view/MessageOutputView.java
@@ -1,6 +1,6 @@
 package lotto.view;
 
-public class MessageOutputController {
+public class MessageOutputView {
 
 	public void printScanningLottoPurchaseMoneyMessage() {
 		System.out.println("구입금액을 입력해 주세요.");

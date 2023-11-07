1abfb21a33bc2754bc02bb3464fa2d69fc85abc8
Jaewon-pro <jaewonlee.pro@gmail.com>
2023-11-07 13:28:35 +0900

    feat(lotto): add lottery validation & lotto save

--- src/main/java/lotto/Lottery.java
+++ src/main/java/lotto/Lottery.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.exception.LottoErrorCode;
+
 public record Lottery(
         Lotto lotto,
         Integer bonusNumber
 ) {
+    public Lottery {
+        validateAllUnique(lotto, bonusNumber);
+    }
+
+    private void validateAllUnique(Lotto lotto, Integer bonusNumber) {
+        if (lotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LottoErrorCode.DUPLICATED_NUMBER.getMessage());
+        }
+    }
+
 }

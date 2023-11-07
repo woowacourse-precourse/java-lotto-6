c56db2bcb43495de097632242dc195e02cf6c688
realcold0 <realcold0@gmail.com>
2023-11-08 03:02:08 +0900

    chore(lotto) :  클래스 및 메서드 주석 추가

--- src/main/java/lotto/view/InputPurchaseView.java
+++ src/main/java/lotto/view/InputPurchaseView.java
@@ -2,7 +2,14 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * 구매할 금액을 입력받는 화면을 출력하는 View
+ */
 public class InputPurchaseView {
+    /**
+     * 구매할 금액을 입력하라고 출력하고 입력받는 메서드
+     * @return 입력받은 String
+     */
     public String printPurchasePrice(){
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();

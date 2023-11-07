006358784b03336c7c32e31393afd3b39a2f0f9b
JISUmaker <knightmaker666@gmail.com>
2023-11-07 20:20:40 +0900

    feat(controller): 로또 생성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -5,12 +5,16 @@ import lotto.controller.StatisticsController;
 import lotto.domain.Lotto;
 import lotto.domain.Player;
 
+import java.util.ArrayList;
+
 public class LottoGame {
     private final LottoMakingController gameSetting = new LottoMakingController();
     private final StatisticsController gameResult = new StatisticsController();
 
     public void start() {
         gameSetting.createLottoCount();
+        ArrayList<Lotto> lottos = gameSetting.createLottos();
+
     }
 
 }
--- src/main/java/lotto/controller/LottoMakingController.java
+++ src/main/java/lotto/controller/LottoMakingController.java
@@ -1,15 +1,22 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 import lotto.domain.Player;
 import lotto.view.ErrorMessage;
 import lotto.view.LottoView;
 
+import java.util.ArrayList;
+
 public class LottoMakingController {
     private static final LottoView view = new LottoView();
     private final int MONEY_UNIT = 1000;
+    private final int LOTTO_COUNT = 6;
     private int money;
+    private int lottoCount;
+
     public void createLottoCount() {
-        while(true) {
+        while (true) {
             try {
                 String input = view.inputMoney();
                 validateIsNumber(input);
@@ -28,7 +35,18 @@ public class LottoMakingController {
     }
 
     private void validateMoneyUnit(int money) {
-        if(money % MONEY_UNIT != 0)
+        if (money % MONEY_UNIT != 0)
             throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_THOUSAND_MESSAGE.getValue());
+        lottoCount = money / MONEY_UNIT;
     }
+
+    public ArrayList<Lotto> createLottos() {
+        ArrayList<Lotto> lottos = new ArrayList<Lotto>();
+        for (int i = 0; i < lottoCount; i++)
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        view.outputLottos(lottos, lottos.size());
+
+        return lottos;
+    }
+
 }

cb87c91e4bfd298475f00d65231b5c8e98ca5a0f
Sung Hyobin <hellmir610@gmail.com>
2023-11-07 23:40:39 +0900

    test: Add and modify tests to consider multiple of lotto price

--- src/test/java/lotto/exception/ExceptionHandlerTest.java
+++ src/test/java/lotto/exception/ExceptionHandlerTest.java
@@ -7,12 +7,12 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ExceptionHandlerTest {
-    @DisplayName("양의 정수 값을 입력하면 ExceptionStatus의 NOT_OCCURED 상태를 반환한다.")
+    @DisplayName("1,000원 단위의 양의 정수 값을 입력하면 ExceptionStatus의 NOT_OCCURED 상태를 반환한다.")
     @ParameterizedTest
-    @CsvSource({"1", "9", "10", "100", "1000", "10000"})
-    void handleMoneyFormatExceptionFromPositiveInteger(String purchaseAmount) {
+    @CsvSource({"1000", "3000", "10000", "600000", "7000000"})
+    void handleMoneyFormatExceptionFromMultipleOfLottoPricePositiveInteger(String purchaseAmount) {
         // given, when
-        ExceptionStatus exceptionStatus = ExceptionHandler.handleMoneyFormatException(purchaseAmount);
+        ExceptionStatus exceptionStatus = ExceptionHandler.handleMoneyInputException(purchaseAmount);
 
         // then
         assertThat(exceptionStatus.isOccurred()).isFalse();
@@ -20,10 +20,21 @@ class ExceptionHandlerTest {
 
     @DisplayName("양의 정수가 아닌 값을 입력하면 ExceptionStatus의 OCCURED 상태를 반환한다.")
     @ParameterizedTest
-    @CsvSource({"0", "가나다라마바사", "abcdef", "12ab가나", "ab12가나", "-1"})
+    @CsvSource({"0", "-1", "가나다라마바사", "abcdef", "12ab가나", "ab12가나"})
     void handleMoneyFormatExceptionFromNotPositiveInteger(String purchaseAmount) {
         // given, when
-        ExceptionStatus exceptionStatus = ExceptionHandler.handleMoneyFormatException(purchaseAmount);
+        ExceptionStatus exceptionStatus = ExceptionHandler.handleMoneyInputException(purchaseAmount);
+
+        // then
+        assertThat(exceptionStatus.isOccurred()).isTrue();
+    }
+
+    @DisplayName("1,000원 단위가 아닌 양의 정수 값을 입력하면 ExceptionStatus의 OCCURED 상태를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"1", "50", "999", "1001", "4999", "50001"})
+    void handleMoneyFormatExceptionFromNotMultipleOfLottoPricePositiveInteger(String purchaseAmount) {
+        // given, when
+        ExceptionStatus exceptionStatus = ExceptionHandler.handleMoneyInputException(purchaseAmount);
 
         // then
         assertThat(exceptionStatus.isOccurred()).isTrue();
--- src/test/java/lotto/validation/InputFormatValidatorTest.java
+++ src/test/java/lotto/validation/InputFormatValidatorTest.java
@@ -5,26 +5,36 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static lotto.message.ErrorMessage.ERROR_MESSAGE_HEAD;
-import static lotto.message.ErrorMessage.INVALID_MONEY_FORMAT_EXCEPTION;
+import static lotto.message.ErrorMessage.INVALID_MONEY_INPUT_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class InputFormatValidatorTest {
-    @DisplayName("양의 정수 값을 입력하면 IllegalArgumentException이 발생하지 않는다.")
+    @DisplayName("1,000원 단위의 양의 정수 값을 입력하면 IllegalArgumentException이 발생하지 않는다.")
     @ParameterizedTest
-    @CsvSource({"1", "9", "10", "100", "1000", "10000"})
-    void validateMoneyFormatFromPositiveInteger(String input) {
+    @CsvSource({"1000", "3000", "10000", "600000", "7000000"})
+    void validateMoneyFormatFromMultipleOfLottoPricePositiveInteger(String input) {
         // given, when, then
         InputFormatValidator.validateMoneyFormat(input);
     }
 
     @DisplayName("양의 정수가 아닌 값을 입력하면 IllegalArgumentException이 발생한다.")
     @ParameterizedTest
-    @CsvSource({"0", "가나다라마바사", "abcdef", "12ab가나", "ab12가나", "-1"})
+    @CsvSource({"0", "-1", "가나다라마바사", "abcdef", "12ab가나", "ab12가나"})
     void validateMoneyFormatFromNotPositiveInteger(String input) {
         // given, when, then
         assertThatThrownBy(() -> InputFormatValidator.validateMoneyFormat(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_MONEY_FORMAT_EXCEPTION);
+                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_MONEY_INPUT_EXCEPTION);
+    }
+
+    @DisplayName("1,000원 단위가 아닌 양의 정수 값을 입력하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource({"0", "가나다라마바사", "abcdef", "12ab가나", "ab12가나", "-1"})
+    void validateMoneyFormatFromNotMultipleOfLottoPricePositiveInteger(String input) {
+        // given, when, then
+        assertThatThrownBy(() -> InputFormatValidator.validateMoneyFormat(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE_HEAD + INVALID_MONEY_INPUT_EXCEPTION);
     }
 }

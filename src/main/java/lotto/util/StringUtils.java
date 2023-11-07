936821e4d83ed1d8faa334979e40b4860e907489
gmu1026 <curiouscomponent@gmail.com>
2023-11-07 17:25:17 +0900

    feat: 문자열 정수형 리스트 변환 기능 구현

--- src/main/java/lotto/util/StringUtils.java
+++ src/main/java/lotto/util/StringUtils.java
@@ -1,9 +1,12 @@
 package lotto.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class StringUtils {
     private static final Pattern NUMBER_ALLOWED_PATTERN = Pattern.compile("\\D");
+    private static final String NUMBER_SEPARATOR = ",";
 
     private StringUtils() {
 
@@ -15,5 +18,16 @@ public class StringUtils {
         }
 
         return true;
+    }
+
+    public static List<Integer> convertStringToList(String target) {
+        List<Integer> result = new ArrayList<>();
+
+        String[] numbers = target.split(NUMBER_SEPARATOR);
+        for (String number : numbers) {
+            result.add(Integer.valueOf(number));
+        }
+
+        return result;
     }
 }

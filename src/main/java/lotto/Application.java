e9be36225b3548af30f06659472d284d8cfd8b5f
chansoo <chansoo38@gmail.com>
2023-11-07 11:40:53 +0900

    <Feat> Application - 로또 게임의 main 함수를 작성한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.Controller.Controller;
+import lotto.Service.LottoService;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
 public class Application {
+    static Controller controller = new Controller(InputView.getInstance(), OutputView.getInstance(), new LottoService());
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        controller.run();
     }
 }

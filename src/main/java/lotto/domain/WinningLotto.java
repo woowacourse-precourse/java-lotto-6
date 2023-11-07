8d32680385e2d9cd2c33612f930563179a23d938
kimdongyeon <do121712@naver.com>
2023-11-07 23:57:40 +0900

    fix: 초기 Map 설정을 iterator을 통해 코드 간결화

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -17,46 +17,41 @@ public class WinningLotto {
 
     public Map reset() {
         Map<Statistic, Integer> reset = new HashMap<>();
-        reset.put(Statistic.MATCH_THREE, 0);
-        reset.put(Statistic.MATCH_FOUR, 0);
-        reset.put(Statistic.MATCH_FIVE, 0);
-        reset.put(Statistic.MATCH_FIVE_WITH_BONUS, 0);
-        reset.put(Statistic.MATCH_SIX, 0);
+        for (Statistic stat : Statistic.values()) {
+            reset.put(stat, 0);
+        }
         return reset;
     }
 
-    public void accordCheck(Lottos lottos, Player player) {
+    public void calculatePrize(Lottos lottos, Player player) {
         for (int i = 0; i < lottos.getLotto().size(); i++) {
-
-            int count = countCheck(lottos, i);
-
+            int count = containCheck(lottos, i);
             if (count < 3) continue;
-            Statistic statistic = Statistic.getStatistic(count);
-
-            if (Statistic.MATCH_THREE == statistic)
-                match.put(Statistic.MATCH_THREE, match.getOrDefault(Statistic.MATCH_THREE, 0) + 1);
-            if (Statistic.MATCH_FOUR == statistic)
-                match.put(Statistic.MATCH_FOUR, match.getOrDefault(Statistic.MATCH_FOUR, 0) + 1);
-            if (Statistic.MATCH_FIVE == statistic && !(lottos.getLotto().get(i).getNumbers().contains(bonus)))
-                match.put(Statistic.MATCH_FIVE, match.getOrDefault(Statistic.MATCH_FIVE, 0) + 1);
-            if (Statistic.MATCH_FIVE == statistic && (lottos.getLotto().get(i).getNumbers().contains(bonus))) {
-                match.put(Statistic.MATCH_FIVE_WITH_BONUS, match.getOrDefault(Statistic.MATCH_FIVE_WITH_BONUS, 0) + 1);
-                statistic = Statistic.MATCH_FIVE_WITH_BONUS;
-            }
-            if (Statistic.MATCH_SIX == statistic)
-                match.put(Statistic.MATCH_SIX, match.getOrDefault(Statistic.MATCH_SIX, 0) + 1);
-
+            Statistic statistic = countStatistic(lottos, count, i);
             player.sumPrize(statistic.getPrize());
         }
     }
 
-    public int countCheck(Lottos lottos, int index) {
+    public int containCheck(Lottos lottos, int index) {
         int count = 0;
         for (Integer number : numbers) {
             if ((lottos.getLotto().get(index).getNumbers()).contains(number))
                 count++;
         }
         return count;
+    }
+
+    public Statistic countStatistic(Lottos lottos, int count ,int index){
+        Statistic statistic = Statistic.getStatistic(count);
+        if (Statistic.MATCH_THREE == statistic) match.put(Statistic.MATCH_THREE, match.getOrDefault(Statistic.MATCH_THREE, 0) + 1);
+        if (Statistic.MATCH_FOUR == statistic) match.put(Statistic.MATCH_FOUR, match.getOrDefault(Statistic.MATCH_FOUR, 0) + 1);
+        if (Statistic.MATCH_FIVE == statistic && !(lottos.getLotto().get(index).getNumbers().contains(bonus))) match.put(Statistic.MATCH_FIVE, match.getOrDefault(Statistic.MATCH_FIVE, 0) + 1);
+        if (Statistic.MATCH_FIVE == statistic && (lottos.getLotto().get(index).getNumbers().contains(bonus))) {
+            match.put(Statistic.MATCH_FIVE_WITH_BONUS, match.getOrDefault(Statistic.MATCH_FIVE_WITH_BONUS, 0) + 1);
+            statistic = Statistic.MATCH_FIVE_WITH_BONUS;
+        }
+        if (Statistic.MATCH_SIX == statistic) match.put(Statistic.MATCH_SIX, match.getOrDefault(Statistic.MATCH_SIX, 0) + 1);
+        return statistic;
     }
 
     public Map<Statistic, Integer> getMatch() {

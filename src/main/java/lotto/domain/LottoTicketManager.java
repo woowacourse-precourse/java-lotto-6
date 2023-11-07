e7de50a4e71a1a178d2f6e7190fb4a0b1417cdda
bamm2 <xxknu88@naver.com>
2023-11-07 20:20:06 +0900

    feat(LottoTicketManager): 로또 번호 얻기 , 보너스 번호 검증 후 인스턴스 변수에 저장

--- src/main/java/lotto/domain/LottoTicketManager.java
+++ src/main/java/lotto/domain/LottoTicketManager.java
@@ -1,8 +1,17 @@
 package lotto.domain;
 
-import lotto.validator.LottoTicketValidator;
+import static lotto.constant.ConstantInteger.LOTTO_MAX_NUMBER;
+import static lotto.constant.ConstantInteger.LOTTO_MIN_NUMBER;
+import static lotto.exception.ExceptionMessage.DUPLICATED_BONUS_NUMBER_MESSAGE;
+import static lotto.exception.ExceptionMessage.NOT_IN_RANGE_NUMBER_INPUT_MESSAGE;
+
+import java.util.List;
 
 public class LottoTicketManager {
+
+    private List<Lotto> lottoTickets;
+    private Lotto luckyNumbers;
+    private Integer bonusNumber;
 
     public LottoTicketManager(){
     }
@@ -15,4 +24,37 @@ public class LottoTicketManager {
         return Integer.parseInt(price);
     }
 
+    public void getUserLottoTickets(List<Lotto> lottoTickets){
+        this.lottoTickets=lottoTickets;
+    }
+
+    public void getLuckyNumber(Lotto luckyNumbers){
+        this.luckyNumbers=luckyNumbers;
+    }
+
+    public void getBonusNumber(Integer bonusNumber){
+        validate(bonusNumber);
+        this.bonusNumber=bonusNumber;
+    }
+
+    private void validate(Integer bonusNumber) {
+        validateNumberRange(bonusNumber);
+        validateDuplicateBonusNumberInLuckyNumber(bonusNumber);
+    }
+
+    private void validateNumberRange(Integer bonusNumber){
+        if(!isInRangeNumber(bonusNumber)){
+            throw new IllegalArgumentException(NOT_IN_RANGE_NUMBER_INPUT_MESSAGE.getMessage());
+        }
+    }
+
+    private boolean isInRangeNumber(Integer number){
+        return LOTTO_MIN_NUMBER <= number && number <= LOTTO_MAX_NUMBER;
+    }
+
+    private void validateDuplicateBonusNumberInLuckyNumber(Integer bonusNumber){
+        if(luckyNumbers.getNumbers().contains(bonusNumber)){
+            throw new IllegalArgumentException(DUPLICATED_BONUS_NUMBER_MESSAGE.getMessage());
+        }
+    }
 }

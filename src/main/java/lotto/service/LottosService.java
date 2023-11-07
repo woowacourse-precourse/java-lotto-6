3f2db84598b5c9e7ac2073d8f34e21d7d63bd33f
acs0209 <annjae4676@naver.com>
2023-11-07 18:10:38 +0900

    fix: 기능 테스트 오류 수정(정렬 이슈)

--- src/main/java/lotto/service/LottosService.java
+++ src/main/java/lotto/service/LottosService.java
@@ -5,6 +5,7 @@ import static lotto.domain.constant.LottoConstant.MIN_NUMBER;
 import static lotto.domain.constant.LottoConstant.NUMBER_COUNT;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.domain.Lotto;
@@ -22,17 +23,22 @@ public class LottosService {
     public Lottos createLottos(LottoPurchase lottoPurchase) {
         int purchaseCount = 0;
         while (lottoPurchase.isRunning(purchaseCount)) {
-            Lotto lotto = Lotto.create(createRandomNumbers());
+            Lotto lotto = Lotto.create(createOrderedRandomNumbers());
             lottos.save(lotto);
             purchaseCount++;
         }
         return lottos;
     }
 
+    private List<Integer> createOrderedRandomNumbers() {
+        List<Integer> randomNumbers = createRandomNumbers();
+        List<Integer> sortedNumbers = new ArrayList<>(randomNumbers);
+        Collections.sort(sortedNumbers);
+        return randomNumbers;
+    }
+
     private List<Integer> createRandomNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getNumber(), MAX_NUMBER.getNumber(),
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getNumber(), MAX_NUMBER.getNumber(),
                 NUMBER_COUNT.getNumber());
-        Collections.sort(numbers);
-        return numbers;
     }
 }

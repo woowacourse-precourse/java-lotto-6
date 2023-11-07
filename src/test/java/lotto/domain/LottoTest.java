29896b2ab12be339a6fa1269fa467adf6e93a591
cyPark95 <parkcy1007@gmail.com>
2023-11-07 09:47:20 +0900

    refactor: LottoTest 코드 수정
    - 통일성 있게 메서드 레벨 DisplayName 어노테이션 제거하고, 가독성을 위해 메서드명을 한글로 변경
    - given, when, then 명시
    - 에러 메시지 검증 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,24 +1,36 @@
 package lotto.domain;
 
+import static common.ErrorCode.LOTTO_NUMBERS_INVALID_SIZE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("로또 번호 검증")
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+
     @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+    void 로또_번호_6개_이상_예외() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBERS_INVALID_SIZE.getMessage());
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void createLottoByDuplicatedNumber() {
+    void 로또_번호_중복된_숫자_예외() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 5);
+
+        // when
+        // then
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

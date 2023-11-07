8693ee17ac05f95c5a181052d8d1f686ef1a14c6
davemins <davemins4125@naver.com>
2023-11-07 12:08:21 +0900

    style: InputView에 enum 적용 InputView에 있는 중복되는 코드들에 enum을 사용하여 재사용성과 가독성을 높였습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,23 +2,19 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView {
-    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+public enum InputView {
+    AMOUNT("구입금액을 입력해 주세요."),
+    LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
+    BONUMS_NUMBER("보너스 번호를 입력해 주세요.");
 
-    public static String inputAmount() {
-        System.out.println(INPUT_AMOUNT_MESSAGE);
-        return Console.readLine();
+    private final String message;
+
+    InputView(String message) {
+        this.message = message;
     }
 
-    public static String inputLottoNumber() {
-        System.out.println(INPUT_LOTTO_NUMBER_MESSAGE);
-        return Console.readLine();
-    }
-
-    public static String inputBonusNumber() {
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    public String getInput() {
+        System.out.println(message);
         return Console.readLine();
     }
 }

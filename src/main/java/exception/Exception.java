3a58812f2f7eb9921a440be1a39e8ea5e2692eb3
2021110806 <2021110806@dgu.ac.kr>
2023-11-07 16:09:56 +0900

    feat(Exception.java): add winningNumber exception raising functions winningLotto class와 관련한 exception을 발생시키는 기능들을 추가하였습니다.

--- src/main/java/exception/Exception.java
+++ src/main/java/exception/Exception.java
@@ -13,4 +13,21 @@ public class Exception {
     void raiseMoneyNotDevidedByThousandException() {
         throw new IllegalArgumentException(ExceptionMessage.MONEY_NOT_BEING_DEVIDED_BY_THOUSAND_MESSAGE.getMessage());
     }
+
+    void raiseInvalidWinningNumberArgumentException() {
+        throw new IllegalArgumentException(ExceptionMessage.INVALID_WINNING_NUMBER_ARGUMENT_MESSAGE.getMessage());
+    }
+
+    void raiseInvalidWinningNumberRangeException() {
+        throw new IllegalArgumentException(ExceptionMessage.INVALID_WINNING_NUMBER_RANGE_MESSAGE.getMessage());
+    }
+
+    void raiseInvalidWinningNumberInputFormatException() {
+        throw new IllegalArgumentException(ExceptionMessage.INVALID_WINNING_NUMBER_INPUT_FORMAT_MESSAGE.getMessage());
+    }
+
+    void raiseInvalidWinningNumberDuplicationException() {
+        throw new IllegalArgumentException(ExceptionMessage.INVALID_WINNING_NUMBER_DUPLICATION_MESSAGE.getMessage());
+    }
+
 }

901cb31d5183867f96a3f37a768912089d6ac3fa
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-07 16:35:44 +0900

    [feat : Validator] 입력한 로또 번호 중 숫자가 아닌 입력이 있는 경우

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,5 +1,7 @@
 package lotto.util;
 
+import java.util.List;
+
 public class Validator {
     //1,000으로 나누어 떨어지지 않는 경우
     public static void notSeparatedBy1000(int amount){
@@ -18,6 +20,15 @@ public class Validator {
     }
 
     //숫자가 아닌 경우-리스트 버전
+    public static void isDigit(List<String> numbers){
+        try{
+            for(String number:numbers){
+                Integer.parseInt(number);
+            }
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("로또 번호 중 숫자가 아닌 입력이 있습니다.");
+        }
+    }
     //1 ~ 45 범위를 초과하는 경우
     //1 ~ 45 범위를 초과하는 경우-리스트 버전
     //중복된 숫자가 포함된 경우 - 리스트

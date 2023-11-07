d52cecb18ab571a600c2f8d318a0f20129d86f0f
BOYOUNG-KANG <28287@naver.com>
2023-11-07 22:53:36 +0900

    refactor: 변수 이름 수정

--- src/main/java/lotto/domain/Reward.java
+++ src/main/java/lotto/domain/Reward.java
@@ -5,27 +5,33 @@ import lotto.constant.GameConstant;
 
 public enum Reward {
 
-    FIRST_REWARD(luckyNumberCount -> luckyNumberCount == 6, bonusNumberCount -> bonusNumberCount == 0,1, GameConstant.FIRST_REWARD),
-    SECOND_REWARD(luckyNumberCount -> luckyNumberCount == 5, bonusNumberCount -> bonusNumberCount == 1,2, GameConstant.SECOND_REWARD),
-    THIRD_REWARD(luckyNumberCount -> luckyNumberCount == 5, bonusNumberCount -> bonusNumberCount == 0,3, GameConstant.THIRD_REWARD),
-    FOURTH_REWARD(luckyNumberCount -> luckyNumberCount == 4, bonusNumberCount -> bonusNumberCount == 0,4, GameConstant.FOURTH_REWARD),
-    FIFTH_REWARD(luckyNumberCount -> luckyNumberCount == 3, bonusNumberCount -> bonusNumberCount == 0,5, GameConstant.FIFTH_REWARD),
-    NO_REWARD(luckyNumberCount -> luckyNumberCount < 3, bonusNumberCount -> bonusNumberCount >= 0 ,0, 0);
-    private IntPredicate luckyNumberCount;
-    private IntPredicate bonusNumberCount;
+    FIRST_REWARD(matchCount -> matchCount == 6, bonusCount -> bonusCount == 0,
+            1, GameConstant.FIRST_REWARD),
+    SECOND_REWARD(matchCount -> matchCount == 5, bonusCount -> bonusCount == 1,
+            2, GameConstant.SECOND_REWARD),
+    THIRD_REWARD(matchCount -> matchCount == 5, bonusCount -> bonusCount == 0,
+            3, GameConstant.THIRD_REWARD),
+    FOURTH_REWARD(matchCount -> matchCount == 4, bonusCount -> bonusCount == 0,
+            4, GameConstant.FOURTH_REWARD),
+    FIFTH_REWARD(matchCount -> matchCount == 3, bonusCount -> bonusCount == 0,
+            5, GameConstant.FIFTH_REWARD),
+    NO_REWARD(matchCount -> matchCount < 3, bonusCount -> bonusCount >= 0 ,
+            0, 0);
+    private IntPredicate matchCount;
+    private IntPredicate bonusCount;
     private int rank;
     private int money;
 
-    Reward(IntPredicate luckyNumberCount, IntPredicate bonusNumberCount, int rank, int money) {
-        this.luckyNumberCount = luckyNumberCount;
-        this.bonusNumberCount = bonusNumberCount;
+    Reward(IntPredicate matchCount, IntPredicate bonusCount, int rank, int money) {
+        this.matchCount = matchCount;
+        this.bonusCount = bonusCount;
         this.rank = rank;
         this.money = money;
     }
 
-    public static Reward computer(int luckyNumberCount, int bonusNumberCount){
+    public static Reward computer(int matchCount, int bonusCount){
         for (Reward reward : values()){
-            if (reward.luckyNumberCount.test(luckyNumberCount) && reward.bonusNumberCount.test(bonusNumberCount)){
+            if (reward.matchCount.test(matchCount) && reward.bonusCount.test(bonusCount)){
                 return reward;
             }
         }

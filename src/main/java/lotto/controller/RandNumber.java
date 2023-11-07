a9c761d007bbfc612a3832ac5e8e32c8a2fd7de3
Hyso <hsk7953@gmail.com>
2023-11-08 03:34:22 +0900

    feat:중복 되지 않는 숫자 6개 생성

--- src/main/java/lotto/controller/RandNumber.java
+++ src/main/java/lotto/controller/RandNumber.java
@@ -2,11 +2,40 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RandNumber {
-    public static List<Integer> randomLottoNumber(){
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+//    public static List<Integer> randomLottoNumber(){
+//        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+//        return numbers;
+//    }
+
+    public static List<Integer> makeLottoNumber() {
+        List<Integer> numbers = new ArrayList<>();
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < 6) {
+            int randomNumber = generateRandomNumber();
+            if (isUniqueNumber(uniqueNumbers, randomNumber)) {
+                addUniqueNumber(numbers, uniqueNumbers, randomNumber);
+            }
+        }
         return numbers;
+    }
+
+    private static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(1,45);
+    }
+
+    private static boolean isUniqueNumber(Set<Integer> uniqueNumbers, int number){
+        return !uniqueNumbers.contains(number);
+    }
+
+    private static void addUniqueNumber(List<Integer> numbers, Set<Integer> uniqueNumbers, int number){
+        uniqueNumbers.add(number);
+        numbers.add(number);
     }
 }

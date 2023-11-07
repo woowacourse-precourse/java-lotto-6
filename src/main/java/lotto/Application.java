61e60c7e0812c5f00dfb683596cf909d6ea9ab93
nylonmusk <2912rla@gmail.com>
2023-11-08 04:05:14 +0900

    refactor : 팩토리 패턴으로 객체 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,30 +1,11 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.RandomLottoNumberGenerator;
-import lotto.domain.LottoResultCalculator;
-import lotto.domain.ProfitCalculator;
-import lotto.service.LottoService;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.factory.LottoControllerFactory;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        RandomLottoNumberGenerator randomLottoNumberGenerator = new RandomLottoNumberGenerator();
-        LottoService lottoService = new LottoService(randomLottoNumberGenerator);
-        LottoResultCalculator resultCalculator = new LottoResultCalculator();
-        ProfitCalculator profitCalculator = new ProfitCalculator();
-
-        LottoController lottoController = new LottoController(
-                inputView,
-                outputView,
-                lottoService,
-                resultCalculator,
-                profitCalculator
-        );
-
+        LottoController lottoController = LottoControllerFactory.create();
         lottoController.playLotto();
     }
 }
---
+++ src/main/java/lotto/factory/LottoControllerFactory.java
@@ -0,0 +1,31 @@
+package lotto.factory;
+
+import lotto.controller.LottoController;
+import lotto.domain.numbergenerator.RandomLottoNumberGenerator;
+import lotto.domain.calculator.LottoResultCalculator;
+import lotto.domain.calculator.ProfitCalculator;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoControllerFactory {
+
+    private LottoControllerFactory() {}
+
+    public static LottoController create() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RandomLottoNumberGenerator randomLottoNumberGenerator = new RandomLottoNumberGenerator();
+        LottoService lottoService = new LottoService(randomLottoNumberGenerator);
+        LottoResultCalculator resultCalculator = new LottoResultCalculator();
+        ProfitCalculator profitCalculator = new ProfitCalculator();
+
+        return new LottoController(
+                inputView,
+                outputView,
+                lottoService,
+                resultCalculator,
+                profitCalculator
+        );
+    }
+}

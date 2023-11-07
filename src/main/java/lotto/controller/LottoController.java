ca875cbf04e296dfdc71f502c974f4b1df1d7e6f
정재웅 <jweidon0821@gmail.com>
2023-11-08 01:19:47 +0900

    feat(controller): 금액을 입력 받고 횟수로 반환하기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,16 @@
 package lotto.controller;
 
+import lotto.domain.Money;
+import lotto.view.InputView;
+
 public class LottoController {
     public void play() {
 
     }
 
     public void start() {
-
+        int inputMoney = InputView.inputMoney();
+        Money money = new Money(inputMoney);
+        int trial = money.trial();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String inputMoney() {
+    public static int inputMoney() {
         OutputView.printAskMoneyMessage();
-        return Console.readLine();
+        return Integer.parseInt(Console.readLine());
     }
 
     public static String inputWinningNumbers() {

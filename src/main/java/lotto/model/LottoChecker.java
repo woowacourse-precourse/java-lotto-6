0e4df3b103d02192622af12bc9ee1172ee891975
jihyuk <kwlgur0416@gmail.com>
2023-11-08 03:31:12 +0900

    feat : 보너스 번호 중복 및 범위 검증 기능 구현

--- src/main/java/lotto/model/LottoChecker.java
+++ src/main/java/lotto/model/LottoChecker.java
@@ -1,15 +1,40 @@
 package lotto.model;
 
+import static lotto.Constants.Constants.MAX_RANDOM_NUMBER;
+import static lotto.Constants.Constants.MIN_RANDOM_NUMBER;
+import static lotto.exception.ErrorCode.BONUS_NUMBER_DUPLICATE;
+import static lotto.exception.ErrorCode.BONUS_NUMBER_RANGE;
+
 import java.util.List;
+import lotto.exception.BonusNumberException;
 
 public class LottoChecker {
 
     private final Lotto winningLotto;
     private final int bonus;
 
-    public LottoChecker(Lotto winningLotto, int bonus) {
+    public LottoChecker(List<Integer> winningNumbers, int bonus) {
+        Lotto winningLotto = new Lotto(winningNumbers);
+        validateBonusNumber(bonus);
         this.winningLotto = winningLotto;
         this.bonus = bonus;
+    }
+
+    private void validateBonusNumber(final int bonus) {
+        validateBonusNumberRange(bonus);
+        validateBonusNumberDuplicate(bonus);
+    }
+
+    private void validateBonusNumberRange(final int number) {
+        if (number < MIN_RANDOM_NUMBER || number > MAX_RANDOM_NUMBER) {
+            throw new BonusNumberException(BONUS_NUMBER_RANGE);
+        }
+    }
+
+    private void validateBonusNumberDuplicate(final int number) {
+        if (winningLotto.contains(number)) {
+            throw new BonusNumberException(BONUS_NUMBER_DUPLICATE);
+        }
     }
 
     public LottoResult checkLottos(final List<Lotto> lottos) {

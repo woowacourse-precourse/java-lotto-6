92ec8f466077502056d2e7979d0ff05c22ad32a3
sw801733 <sw801733@naver.com>
2023-11-07 21:28:26 +0900

    feat : LottoPurchase validate 기능 추가

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -1,10 +1,17 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoPurchase {
     private final int LottoPurchaseCnt;
 
     public LottoPurchase(int LottoPurchaseCnt){
+        validate(LottoPurchaseCnt);
         this.LottoPurchaseCnt = LottoPurchaseCnt;
+    }
+
+    private void validate(int LottoPurchaseCnt) {
+        return;
     }
 
 }

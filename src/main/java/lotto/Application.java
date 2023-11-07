05043d33be133b5d8f64a9aca349ad97088fcf37
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 19:56:40 +0900

    feat: 로또 게임을 진행하는 컨트롤러 함수 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.controller.LottoController;
 import lotto.domain.LottoAmount;
 import lotto.domain.LottoNumbers;
 import lotto.view.InputView;
@@ -8,14 +9,8 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-//        LottoAmount playerLottoAmount = new LottoAmount(InputView.inputPlayerAmount());
-//
-//        int lottoNum=playerLottoAmount.calcLottoNum();
-//        System.out.println(lottoNum);
-
-        LottoNumbers lottoNumbers = new LottoNumbers();
-        List<Integer> result= lottoNumbers.setLottoNumbers();
-        System.out.println(result);
+        LottoController lottoController = new LottoController();
+        lottoController.play();
 
     }
 }

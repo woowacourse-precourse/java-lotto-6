408ac34285b82dd1d5a7b49c1d7d728c919568a9
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-08 08:38:49 +0900

    feat: 예외처리 메시지를 위한 메시지 보관 클래스 생성

--- src/main/java/lotto/ExceptionMessages.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public class ExceptionMessages {
-    public static final String INVALID_PURCHASE_AMOUNT_MESSAGE = "[ERROR] 0이상의 정수를 입력해주세요.";
-    public static final String INVALID_LOTTO_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
-    public static final String DUPLICATE_LOTTO_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
-    public static final String OUT_OF_RANGE_LOTTO_NUMBER_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    public static final String INVALID_MATCH_COUNT_MESSAGE = "[ERROR] 3 미만, 6 초과의 숫자는 Prize가 될 수 없습니다.";
-}
---
+++ src/main/java/lotto/exception/ExceptionMessages.java
@@ -0,0 +1,12 @@
+package lotto.exception;
+
+import static lotto.Constants.LOTTO_PRICE;
+
+public class ExceptionMessages {
+    public static final String INVALID_PURCHASE_AMOUNT_MESSAGE =
+            LOTTO_PRICE + " 이상의 " + LOTTO_PRICE + " 단위 정수를 입력해주세요.";
+    public static final String INVALID_LOTTO_SIZE_MESSAGE = "로또 번호는 6개여야 합니다.";
+    public static final String DUPLICATE_LOTTO_MESSAGE = "로또 번호는 중복되지 않아야 합니다.";
+    public static final String OUT_OF_RANGE_LOTTO_NUMBER_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INVALID_MATCH_COUNT_MESSAGE = "3 미만, 6 초과의 숫자는 Prize가 될 수 없습니다.";
+}

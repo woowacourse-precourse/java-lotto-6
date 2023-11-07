ac05e281e0eac1d1ae3d69a3b9818f110c8038b2
ilot12 <rlaehdwn60@gmail.com>
2023-11-07 18:15:31 +0900

    feat: Lotto클래스 정렬 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,12 +10,17 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        arrangeNumbers();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 각각의 로또는 6개의 수여야 합니다.");
         }
+    }
+    
+    private void arrangeNumbers() {
+        Collections.sort(numbers);
     }
     
     public List<Integer> getNumbers() {

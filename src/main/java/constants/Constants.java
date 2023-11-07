8ff5ee2c7afc67ab10cc543db441a9f9ed8ee3fb
nayonsoso <panda0329@naver.com>
2023-11-08 02:13:09 +0900

    feat: 구분자 쉼표를 상수화

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final int LOTTO_RANGE_START = 1;
     public static final int LOTTO_RANGE_END = 45;
     public static final int LOTTO_PRICE = 1000;
+    public static final String DELIMITER = ",";
 }

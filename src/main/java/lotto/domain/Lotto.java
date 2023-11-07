f7f1d215eb46dfbaa29f16cb0ee7d56c1db975ed
Lee9Bin <imagen33@daum.net>
2023-11-08 00:14:39 +0900

    feat : 1부터 45 사이에 숫자가 아닌 경우 예외처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,15 +4,27 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
 
     public Lotto(List<Integer> numbers) {
         sizeValidate(numbers);
         this.numbers = numbers;
     }
 
+
+
     private void sizeValidate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 문자를 입력했습니다.");
+        }
+    }
+
+    private void isNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (!(MIN_RANGE <= number && number <= MAX_RANGE)) {
+                throw new IllegalArgumentException("[ERROR] 숫자가 범위를 벗어났습니다.");
+            }
         }
     }
 }

712ba89c60c887df6b22976b5a4c4dc6a66b95cc
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 09:31:37 +0900

    feat(WinnerNumberInputView) : 당첨 숫자 입력 뷰 단 구현

--- src/main/java/lotto/view/WinnerNumberInputView.java
+++ src/main/java/lotto/view/WinnerNumberInputView.java
@@ -8,19 +8,13 @@ import java.util.List;
 
 public class WinnerNumberInputView extends InputView {
     private static final String WINNER_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private WinnerNumberException winnerNumberException = new WinnerNumberException();
     private StringToListConverter stringToListConverter = new StringToListConverter();
 
-    public List<Integer> getWinnerNumber(){
+    public List<Integer> getWinnerNumber() {
         System.out.println(WINNER_NUMBER_MESSAGE);
         String input = getInputValue();
-        winnerNumberException.isNumber(input);
-        winnerNumberException.isComma(input);
+        new WinnerNumberException(input);
         List<Integer> winnerNum = stringToListConverter.stringToList(input);
-        winnerNumberException.isBlank(winnerNum);
-        winnerNumberException.isSize(winnerNum);
-        winnerNumberException.isRightRange(winnerNum);
-        winnerNumberException.isDuplicate(winnerNum);
         return winnerNum;
     }
 }

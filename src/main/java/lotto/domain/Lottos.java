96ef113cd67548170fb3cdd635da74d113e0a1c5
Minyongggg <naminyong97@gmail.com>
2023-11-08 07:52:31 +0900

    feat: 종합 당첨 통계를 구하는 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import lotto.constant.Rewards;
 
 public class Lottos {
     private List<Lotto> lottos = new ArrayList<>();
@@ -19,5 +22,23 @@ public class Lottos {
             totalReward += lotto.checkReward(winningNumber, bonus).getValue();
         }
         return totalReward;
+    }
+
+    public Map<Rewards, Integer> getTotalStats(WinningNumber winningNumber, Integer bonus) {
+        HashMap<Rewards, Integer> stats = new HashMap<>();
+        stats.put(Rewards.LOSE, 0);
+        stats.put(Rewards.FIFTH, 0);
+        stats.put(Rewards.FOURTH, 0);
+        stats.put(Rewards.THIRD, 0);
+        stats.put(Rewards.SECOND, 0);
+        stats.put(Rewards.FIRST, 0);
+
+        for (Lotto lotto : lottos) {
+            Rewards key = lotto.checkReward(winningNumber, bonus);
+            Integer before = stats.get(key);
+            stats.put(key, before + 1);
+        }
+
+        return stats;
     }
 }

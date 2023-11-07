d772875c8c68b5355dbf0b7e3195472e5db84406
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 21:08:43 +0900

    fix(UserInput - getPurchaseAmount(), validatePurchaseAmount()): 예외 발생 이후 다시 유저 입력을 받을 수 있도록 try-catch문으로 구현

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -11,25 +11,40 @@ public class UserInput {
     public static boolean repeatBonusNumber = true;
 
     //구매 금액 입력
-    public int getPurchaseAmount() {
-        int purchaseAmount = Integer.parseInt(Console.readLine());
+//    public int getPurchaseAmount() {
+//        int purchaseAmount = Integer.parseInt(Console.readLine());
+//
+//        return purchaseAmount;
+//    }
 
+
+    public int getPurchaseAmount() {
+        int purchaseAmount;
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                purchaseAmount = Integer.parseInt(Console.readLine());
+                validatePurchaseAmount(purchaseAmount);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
         return purchaseAmount;
     }
 
-    //후에 검증기능과, purchaseCount 리턴하는 기능 별도의 함수로 분리 리팩토링 예정!
-    public void validatePurchaseAmount(int purchaseAmount) {
-//        int purchaseCount;
 
-        if (purchaseAmount % 1000 == 0) {
-            repeatPurchase = false;
-//            purchaseCount = purchaseAmount / 1000;
-            return;
+
+    //후에 검증기능과, purchaseCount 리턴하는 기능 별도의 함수로 분리 리팩토링 예정!
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력해 주세요.");
         }
 
-        throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요");
+        return;
     }
-
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -90,17 +105,15 @@ public class UserInput {
 
 
     //보너스 번호 중복 확인 => while문 입력값 다시 받기
-    public void overlapWithNumber(List<Integer> winningNumbers, int bonusNumber){
+    public void overlapWithNumber(List<Integer> winningNumbers, int bonusNumber) {
 
-        for(var winningNumber : winningNumbers){
-            if(winningNumber == bonusNumber){
+        for (var winningNumber : winningNumbers) {
+            if (winningNumber == bonusNumber) {
                 throw new IllegalArgumentException("당첨 번호와 다른 숫자를 입력해주세요.");
             }
         }
         repeatBonusNumber = false;
     }
-
-
 
 
     //나중에 main() 아래에서 각 입력값별로 메소드들 묶어서 하나의 메소드화 할꺼임 & while문으로 제대로된 값 입력될때까지 다시 물어보기
@@ -110,8 +123,6 @@ public class UserInput {
 //
 //            validatePurchaseAmount(purchaseAmount);
 //    }
-
-
 
 
 }

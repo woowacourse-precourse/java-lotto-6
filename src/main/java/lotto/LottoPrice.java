929f0eb21550ece443d1be29ffbb9d890952d63b
hyuuuun <pam4522@gmail.com>
2023-11-07 21:15:09 +0900

    feat(BonusNumber): 보너스 번호 범위 지정

--- src/main/java/lotto/LottoPrice.java
+++ src/main/java/lotto/LottoPrice.java
@@ -1,18 +1,21 @@
 package lotto;
 
 public class LottoPrice {
-        private static final int LOTTO_PRICE = 1000;
         private final int payMoney;
         private int lottoCount;
         public LottoPrice(int payMoney) {
                 validatePrice(payMoney);
                 this.payMoney = payMoney;
-                lottoCount = payMoney / LOTTO_PRICE;
+                getLottoCount(payMoney);
         }
         private void validatePrice(int payMoney) {
-                if(payMoney%LOTTO_PRICE!=0){
+                if(payMoney%Constants.LOTTO_PRICE!=0){
                         throw new IllegalArgumentException();
                 }
+        }
+        private int getLottoCount(int payMoney) {
+                lottoCount = payMoney / Constants.LOTTO_PRICE;
+                return lottoCount;
         }
 
 

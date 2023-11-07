c62fac6709a8fe2e70f01242cc69db744ab75180
dj <fjzl12@naver.com>
2023-11-08 02:10:29 +0900

    feat(Statistics): 통계 분석 생성자 구현

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,11 +1,38 @@
 package lotto.domain;
 
+import lotto.Lotto;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import static lotto.domain.Constants.ZERO;
 
 public class Statistics {
-    // 각 일치 개수에 대하여 list
     private List<Integer> prizes;
-    public void calculate() {
+    private List<Lotto> myLotto;
+    private List<Integer> prizeNumbers;
+    private int bonusNumber;
+    private float profitPercent;
+    private int cost;
+    private long totalProfit;
+
+    public Statistics(List<Lotto> myLotto, List<Integer> prizeNumbers, int bonusNumber, int cost) {
+        prizes = Arrays.asList(ZERO, ZERO, ZERO, ZERO, ZERO);
+        this.myLotto = myLotto;
+        this.prizeNumbers = prizeNumbers;
+        this.bonusNumber = bonusNumber;
+        this.cost = cost;
+        calculate();
+        computeProfit();
+    }
+
+    public List<Integer> getPrizes() {
+        return prizes;
+    }
+
+    public float getProfitPercent() {
+        return profitPercent;
+    }
 
     }
 

ed0d3d1d03e66d2b74c34c4de44091b7246c3169
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 16:16:59 +0900

    refactor: 당첨결과를 구하는 함수 로직 분리 및 매직넘버 상수화

--- src/main/java/lotto/Model/Result.java
+++ src/main/java/lotto/Model/Result.java
@@ -1,41 +1,46 @@
 package lotto.Model;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Result {
-    private Map<Rank, Integer> rankAndCounting;
+    private static final int DEFAULT = 0;
+
+    private Map<Rank, Integer> winningRankAndCounting;
 
     public Result() {
-        this.rankAndCounting = new LinkedHashMap<>();
+        this.winningRankAndCounting = new LinkedHashMap<>();
         for (Rank rank : Rank.values()) {
-            rankAndCounting.put(rank, 0);
+            winningRankAndCounting.put(rank, DEFAULT);
         }
     }
 
-    public void compare(Lottos lottos, WinningLotto winningLotto, Bonus bonus) {
-        for (Lotto lotto : lottos.getLottos()) {
-            Rank rank = Rank.calculateRank(lotto, winningLotto, bonus);
-
-            rankAndCounting.put(rank, rankAndCounting.get(rank) + 1);
+    public void calculateWinningResult(RandomLottos randomLottos, WinningLotto winningLotto, Bonus bonus) {
+        for (Lotto randomLotto : randomLottos.getRandomLottos()) {
+            Rank rank = Rank.calculateWinningRank(randomLotto, winningLotto, bonus);
+            winningRankAndCounting.put(rank, winningRankAndCounting.get(rank) + 1);
         }
     }
 
-    public Map<Rank, Integer> getRankAndCounting() {
-        return rankAndCounting;
+    public Map<Rank, Integer> getWinningRankAndCounting() {
+        return winningRankAndCounting;
     }
 
-    public int calculateTotalSum() {
-        int sum = 0;
-        for (Rank rank : rankAndCounting.keySet()) {
-            if (rankAndCounting.get(rank) != 0) {
-                sum += Rank.getPrize(rank) * rankAndCounting.get(rank);
+    public int calculateTotalWinningPrize() {
+        int winningPrize = DEFAULT;
+        for (Rank rank : winningRankAndCounting.keySet()) {
+            if (hasWinningCounts(getWinningCountsFrom(rank))) {
+                winningPrize += Rank.getPrize(rank) * getWinningCountsFrom(rank);
             }
         }
-        return sum;
+        return winningPrize;
+    }
+
+    private int getWinningCountsFrom(Rank rank) {
+        return winningRankAndCounting.get(rank);
+    }
+
+    private boolean hasWinningCounts(int winningCounts) {
+        return winningCounts != DEFAULT;
     }
 }

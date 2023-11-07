8fb89ae8a17105369a17e588b1466dd214e893ca
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 17:25:45 +0900

    feat: add print buy amount to output view

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,12 @@
 package lotto.view;
 
 public class OutputView {
+
+    private final static String PRINT_BUY_AMOUNT = "%d개를 구매했습니다.\n";
+
+    private void printBuyAmount(int buyAmount) {
+        System.out.printf(PRINT_BUY_AMOUNT, buyAmount);
+    }
+
+
 }

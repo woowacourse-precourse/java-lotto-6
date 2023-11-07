91584ecfb384256df8a27a17addb39977a736776
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-08 01:55:54 +0900

    fix : 1000으로 나누어떨어지는지 검사 버그수정

--- src/main/java/lotto/domain/PlayerLottoAmount.java
+++ src/main/java/lotto/domain/PlayerLottoAmount.java
@@ -4,30 +4,34 @@ import lotto.view.ExceptionMessage;
 
 public class PlayerLottoAmount {
 
-    public PlayerLottoAmount(String amount){
+    private static final int LOTTO_MIN_AMOUNT = 1000;
+    private final int amount;
+
+    public PlayerLottoAmount(String amount) {
         int playerPrice = validateNumber(amount);
         validateNumberDivide(playerPrice);
-        }
+        this.amount = playerPrice;
+    }
 
     private static void validateNumberDivide(int playerPrice) {
-        if(playerPrice!=1000){
+        if (playerPrice % 1000 != 0) {
             ExceptionMessage.numberNotDivideException();
             throw new IllegalArgumentException();
         }
 
-}
+    }
 
-    private static int validateNumber(String amount) throws IllegalArgumentException{
-        try{
+    private static int validateNumber(String amount) throws IllegalArgumentException {
+        try {
             return Integer.parseInt(amount);
-        }
-        catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             ExceptionMessage.numberException();
             throw new IllegalArgumentException();
         }
-        finally{
 
-        }
+    }
 
+    public int calculateLottoCount() {
+        return amount / LOTTO_MIN_AMOUNT;
     }
 }

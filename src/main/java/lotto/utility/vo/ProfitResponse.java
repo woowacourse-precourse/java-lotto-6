e6760b3578b8accccb1536cfb95fe710e94c9ce5
jongmee <ra123221@gmail.com>
2023-11-07 15:00:43 +0900

    feat(ProfitResponse): 수익률 출력 메세지를 생성하는 VO 클래스 생성하기
    - WHY: 정수부분과 소수부분에 출력 형식이 따로 존재하므로 나누어서 처리하였다.

---
+++ src/main/java/lotto/utility/vo/ProfitResponse.java
@@ -0,0 +1,18 @@
+package lotto.utility.vo;
+
+public class ProfitResponse {
+    private static final String OUTPUT_FORM = "총 수익률은 %,d%s%%입니다.";
+
+    private final int integerPart;
+    private final double decimalPart;
+
+    public ProfitResponse(double value) {
+        this.integerPart = (int)value;
+        this.decimalPart = value % 1;
+    }
+
+    public String createMessage() {
+        String decimalFrom = String.format("%.1f", decimalPart).replace("0.", ".");
+        return String.format(OUTPUT_FORM, integerPart, decimalFrom);
+    }
+}

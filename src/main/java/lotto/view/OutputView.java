faeb567d1262799b1bb5500b963d38b45d03d301
Lee Geun Woo <gamebdy@naver.com>
2023-11-08 02:08:01 +0900

    :sparkles: OutputView ~ 로또 갯수 구하기 까지 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,21 +1,24 @@
 package lotto.view;
 
 
+
+import static lotto.util.Phrase.OutputPhrase.BUY_HOWMUCH;
+
 public class OutputView {
 
-    private enum OutputPhrase {
-        BUY_HOWMUCH("%d개를 구매했습니다.\n");
-        private final String message;
+//    private enum OutputPhrase {
+//        BUY_HOWMUCH("%d개를 구매했습니다.\n");
+//        private final String message;
+//
+//        OutputPhrase(String message) {
+//            this.message = message;
+//        }
+//
+//    }
 
-        OutputPhrase(String message) {
-            this.message = message;
-        }
 
-    }
-
-
-    public void printTicketNumber(int ticketNumber) {
-        System.out.printf(OutputPhrase.BUY_HOWMUCH.message, ticketNumber);
+    public void printNumberOfLotto(int NumberOfLotto) {
+        System.out.printf(BUY_HOWMUCH.printMessage(), NumberOfLotto);
     }
 
     public void printWinningNumber() {

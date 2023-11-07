845a72f68d3147c9efaa27c2e1e0cfc55c004e63
jexnjeux <zieun.na@gmail.com>
2023-11-08 02:29:46 +0900

    fix: 구매한 로또 출력되도록 toString() 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -31,4 +31,17 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < numbers.size(); i++) {
+            sb.append(numbers.get(i));
+            if (i < numbers.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }

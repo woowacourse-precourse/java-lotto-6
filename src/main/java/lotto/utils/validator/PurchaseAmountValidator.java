e69ee60d0cf4e233f17d0f2754f462833977bf40
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 14:43:41 +0900

    feat(PurchaseAmountValidator): 숫자가 아닌 문자 포함 여부를 검증하는 기능

--- src/main/java/lotto/utils/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/utils/validator/PurchaseAmountValidator.java
@@ -10,6 +10,7 @@ public class PurchaseAmountValidator {
     public static void validate(String input) {
         validateBlank(input);
         validateMaxLength(input);
+        validateNumeric(input);
     }
 
     private static void validateBlank(String input) {
@@ -21,6 +22,14 @@ public class PurchaseAmountValidator {
     private static void validateMaxLength(String input) {
         if (input == null || input.length() > String.valueOf(LOTTO_COUNTS_MAX_LIMIT * LOTTO_PRICE_UNIT).length()) {
             throw new IllegalArgumentException(PurchaseAmountExceptionMessage.EXCEED_LENGTH.getError());
+        }
+    }
+
+    private static void validateNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PurchaseAmountExceptionMessage.NOT_NUMERIC.getError(), e);
         }
     }
 }

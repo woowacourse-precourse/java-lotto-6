e359029f7f518c422a82dc83d2759759dbecfc2c
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 13:06:23 +0900

    feat(InputView): 당첨 번호 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 public class InputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요.";
     private final Reader reader;
 
     public InputView(Reader reader) {
@@ -11,6 +12,11 @@ public class InputView {
 
     public String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+        return reader.readLine();
+    }
+
+    public String readWinningNumbers() {
+        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
         return reader.readLine();
     }
 }

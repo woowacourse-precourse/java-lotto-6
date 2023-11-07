486a40c65fe55bdcb3667e1b7f1dd54976980c97
Nara Park <skfk6352@naver.com>
2023-11-08 03:46:12 +0900

    <test>(LottoResult): 당첨 여부와 수익률이 올바르게 구현되는지 검증하는 테스트 메서드 구현

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,8 +1,13 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import lotto.constant.PriceConstant;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
 
@@ -11,7 +16,29 @@ class LottoResultTest {
 
     @BeforeEach
     void init() {
+        ParticularNumberGenerator particularNumberGenerator = new ParticularNumberGenerator(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 28));
+        BonusNumber bonusNumber = BonusNumber.of("6", winningNumbers);
 
+        lotteries = Lotteries.of(1, particularNumberGenerator);
+        winningLotto = WinningLotto.of(winningNumbers,bonusNumber);
     }
 
+    @DisplayName("2등 당첨을 확인한다.")
+    @Test
+    void checkSecondPrize() {
+        LottoResult result = LottoResult.from();
+        result.countWinningCase(lotteries.getLotteries(), winningLotto);
+
+        assertThat(result.getResultMap().get(PriceConstant.SECOND_PLACE.getLabel())).isEqualTo(1);
+    }
+
+    @DisplayName("수익률을 계산한다.")
+    @Test
+    void checkEarningRate() {
+        LottoResult result = LottoResult.from();
+        result.countWinningCase(lotteries.getLotteries(), winningLotto);
+        double earningRate = result.calculateEarningRate(1000);
+        assertThat(earningRate).isEqualTo(3_000_000);
+    }
 }

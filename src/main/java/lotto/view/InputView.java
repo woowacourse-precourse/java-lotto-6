80265e960648624546c4ea61dba36fca5d928aa7
정재웅 <jweidon0821@gmail.com>
2023-11-07 16:31:34 +0900

    feat(view): 당첨 번호  입력 함수 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,16 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String askMoney = "구입금액을 입력해 주세요.";
+    public static String askMoneyMessage = "구입금액을 입력해 주세요.";
+    public static String askWinningNumbersMessage = "당첨 번호를 입력해 주세요.";
+
     public static String inputMoney() {
-        System.out.println(askMoney);
+        System.out.println(askMoneyMessage);
+        return Console.readLine();
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println(askWinningNumbersMessage);
         return Console.readLine();
     }
 }

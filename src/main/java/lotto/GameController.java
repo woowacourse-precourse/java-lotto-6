e9dac4afe31b1d1130843f82828b419ee9cf5031
34suuuuu <psh7492@gmail.com>
2023-11-07 09:58:43 +0900

    feat: 구입금액을 로또로 바꾸는 메서드 추가

--- src/main/java/lotto/GameController.java
+++ src/main/java/lotto/GameController.java
@@ -1,2 +1,42 @@
-package lotto;public class GameControllere {
+package lotto;
+
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoAmount;
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.lotto.WinningLotto;
+import lotto.domain.rank.Rank;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import static lotto.view.OutputView.printTicketCnt;
+
+public class GameController {
+
+    private static final int TICKET_PRICE = 1000;
+    private static final int PERCENTAGE = 100;
+
+    private static LottoAmount lottoAmount;
+    private static List<Integer> lotto = new ArrayList<>();
+    private static List<Lotto> lottoList;
+    private static WinningLotto winningResult;
+
+    public GameController() {
+    }
+
+    public void start() {
+        int ticketCnt = moneyToCnt();
+        printTicketCnt(ticketCnt);
+
+    }
+
+    public int moneyToCnt(){
+        lottoAmount = new LottoAmount(InputView.getInputAmount());
+        return lottoAmount.amountChangeToLottoCnt();
+    }
 }

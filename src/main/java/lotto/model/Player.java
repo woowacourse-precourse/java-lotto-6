14ca3c22f380fa334c11476acf36d70516ea90f4
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-11-08 00:08:35 +0900

    test/player: test를 위한 getter함수 추가, player의 단위별 test 작성

--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -27,4 +27,8 @@ public class Player {
     public int getTotalWinningAccount() {
         return totalWinningAccount;
     }
+
+    public int getPaymentAccount() {
+        return paymentAccount;
+    }
 }

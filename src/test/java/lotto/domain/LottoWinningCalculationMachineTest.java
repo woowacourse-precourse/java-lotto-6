3f27ea1e0d6e1a7794129e35f05fb8c6a07a3ee8
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 00:57:46 +0900

    Test : 당첨 번호와 일치 개수 반환 기능 테스트
    로또 하나와 당첨 번호 주어졌을 때 일치 개수 반환하는 기능 테스트
    Lotto 객체 하나 만들기 위해 List 필요한데 @MethodSource를 사용해
    List를 테스트의 파라미터로 받아 테스트
    실제 당첨 번호는 winningNumber로 미리 생성해 둠

---
+++ src/test/java/lotto/domain/LottoWinningCalculationMachineTest.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LottoWinningCalculationMachineTest {
+
+    LottoWinningCalculationMachine calculationMachine = new LottoWinningCalculationMachine();
+
+    Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+    WinningNumber winningNumber = new WinningNumber(lotto, 7);
+
+    static Stream<Arguments> generateLottoData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(Arrays.asList(8, 2, 3, 4, 15, 28), 3),
+                Arguments.of(Arrays.asList(6, 5, 4, 3, 2, 1), 6),
+                Arguments.of(Arrays.asList(8, 9, 40, 30, 15, 28), 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateLottoData")
+    @DisplayName("당첨 번호와 일치 개수 테스트")
+    void 당첨번호_일치_개수_테스트(List<Integer> lotto, int expectedCount) {
+        Lotto oneLotto = new Lotto(lotto);
+        int actualCount = calculationMachine.calculateWinningNumber(oneLotto, winningNumber);
+        Assertions.assertThat(actualCount).isEqualTo(expectedCount);
+    }
+}

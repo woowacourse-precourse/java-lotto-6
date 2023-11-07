9f10a6a796fc8d5cb9084126d782697ae1c70ab9
aka-nick <bechalrang@gmail.com>
2023-11-07 15:00:23 +0900

    추가(PlayerLottoTest) : PlayerLotto의 테스트 클래스
    PlayerLotto의 테스트 클래스를 추가함
    
    PlayerLottoTest
    PlayerLotto

--- src/main/java/lotto/collaboration/lottos/dto/PlayerLotto.java
+++ src/main/java/lotto/collaboration/lottos/dto/PlayerLotto.java
@@ -10,7 +10,32 @@ public class PlayerLotto {
     private final List<Integer> numbers;
 
     public PlayerLotto(final Lotto lotto) {
+        validate(lotto);
         this.numbers = lotto.stream().sorted().toList();
+    }
+
+    private void validate(Lotto lotto) {
+        occurExceptionIfIsNull(lotto);
+        occurExceptionIfNotSix(lotto);
+        occurExceptionIfOutOfRange(lotto);
+    }
+
+    private static void occurExceptionIfIsNull(Lotto lotto) {
+        if (lotto == null) {
+            throw new IllegalArgumentException("초기화 되지 않은 로또 객체.");
+        }
+    }
+
+    private static void occurExceptionIfNotSix(Lotto lotto) {
+        if (lotto.stream().count() != 6) {
+            throw new IllegalArgumentException("번호는 총 6개여야 합니다.");
+        }
+    }
+
+    private static void occurExceptionIfOutOfRange(Lotto lotto) {
+        if (lotto.stream().anyMatch(number -> number < 1 || 45 < number)) {
+            throw new IllegalArgumentException("번호는 1부터 45까지의 숫자 중에서 선택할 수 있습니다.");
+        }
     }
 
     public Stream<Integer> stream() {

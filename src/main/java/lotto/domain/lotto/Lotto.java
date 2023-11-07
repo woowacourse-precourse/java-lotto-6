9ae0bc070ea9b2635c7244907fc114de13aee13d
34suuuuu <psh7492@gmail.com>
2023-11-07 09:48:24 +0900

    feat: 로또 번호들 사이에 중복 확인하는 메서드 추가

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -12,19 +12,27 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-//        if (numbers.size() != 6) {
-//            throw new IllegalArgumentException();
-//        }
         if (numbers.size() != 6) {
             throw new LottoSizeException();
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> hashNumbers = new HashSet<>();
+        for(Integer number : numbers) {
+            hashNumbers.add(number);
+        }
+        if (hashNumbers.size() != numbers.size()) {
+            throw new LottoNumDuplicateException();
+        }
+    }
+
     public boolean containBonusNum(int num) {
         return numbers.contains(num);
     }

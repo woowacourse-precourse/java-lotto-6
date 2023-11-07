491152fbd3aa11df8e9599806b098da0b7ea9dc8
liarreez <liarreez@gmail.com>
2023-11-08 00:00:28 +0900

    style : 사용하지 않는 import문 제거

--- src/test/java/lotto/model/ClientTest.java
+++ src/test/java/lotto/model/ClientTest.java
@@ -3,7 +3,6 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import lotto.model.constans.WinningRank;
 import org.junit.jupiter.api.DisplayName;

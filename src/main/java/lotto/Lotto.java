ce1f4c2a45b2ed2fb69a58a075800a6dd3bc79a3
GyungA <GyungA@users.noreply.github.com>
2023-11-08 02:50:22 +0900

    chore: 예외 메세지의 [ERROR]를 상수로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
     public static final Integer NUMBER_LENGTH = 6;
     public static final Integer NUMBER_MIN = 1;
     public static final Integer NUMBER_MAX = 45;
+    public static final String ERROR_NOTICE = "[ERROR]";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -24,13 +25,13 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != NUMBER_LENGTH) {
-            throw new IllegalArgumentException(String.format("로또 번호는 %d개여야 합니다.", NUMBER_LENGTH));
+            throw new IllegalArgumentException(String.format("%s 로또 번호는 %d개여야 합니다.", ERROR_NOTICE, NUMBER_LENGTH));
         }
     }
 
     private void validateSingleNumberRange(Integer number) {
         if ((number < NUMBER_MIN) || (NUMBER_MAX < number)) {
-            throw new IllegalArgumentException(String.format("로또 번호 %d가 주어진 범위를 벗어났습니다.", number));
+            throw new IllegalArgumentException(String.format("%s 로또 번호 %d가 주어진 범위를 벗어났습니다.", ERROR_NOTICE, number));
         }
     }
     private void validateNumbersRange(List<Integer> numbers) {
@@ -41,14 +42,14 @@ public class Lotto {
 
     private void validateBonusNumberDuplicate(Integer number) {
         if (numbers.contains(number)) {
-            throw new IllegalArgumentException(String.format("로또 번호 %d가 중복으로 포함되었습니다.", number));
+            throw new IllegalArgumentException(String.format("%s 로또 번호 %d가 중복으로 포함되었습니다.", ERROR_NOTICE, number));
         }
     }
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>();
         for (Integer n : numbers) {
             if (!uniqueNumbers.add(n)) {
-                throw new IllegalArgumentException(String.format("로또 번호 %d가 중복으로 포함되었습니다.", n));
+                throw new IllegalArgumentException(String.format("%s 로또 번호 %d가 중복으로 포함되었습니다.", ERROR_NOTICE, n));
             }
         }
     }

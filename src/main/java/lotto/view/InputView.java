92ba26fa46b280348e18d8a7e4a7517134531f50
daheeParkk <qkrekgml7414@naver.com>
2023-11-07 23:50:37 +0900

    feat : 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,23 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.message.InputMessage.*;
+
 public class InputView {
 
+    public String inputLottoAmount() {
+        System.out.println(LOTTO_AMOUNT.getMessage());
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber() {
+        System.out.println(WINNING_NUMBER.getMessage());
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        System.out.println(BONUS_NUMBER.getMessage());
+        return Console.readLine();
+    }
 }

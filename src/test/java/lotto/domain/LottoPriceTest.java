1f88c4cae9987cb36da5d849aad627656722410f
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-11-08 03:02:59 +0900

    test: 당첨 내역 기능 구현에 따른 테스트 구현

--- src/test/java/lotto/domain/LottoPriceTest.java
+++ src/test/java/lotto/domain/LottoPriceTest.java
@@ -1,8 +1,9 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoPriceTest {
 
@@ -20,8 +21,25 @@ class LottoPriceTest {
         LottoPrice lottoPriceSecond = LottoPrice.getLottoPriceByMatchesNumber(matchesNumber, bonusNumberTrue);
 
         //then
-        Assertions.assertThat(lottoPriceThird).isEqualTo(LottoPrice.THIRD);
-        Assertions.assertThat(lottoPriceSecond).isEqualTo(LottoPrice.SECOND);
+        assertThat(lottoPriceThird).isEqualTo(LottoPrice.THIRD);
+        assertThat(lottoPriceSecond).isEqualTo(LottoPrice.SECOND);
+
+    }
+
+    @Test
+    @DisplayName("등수에 따른 당첨 내역 반환 - 성공")
+    public void get_lotto_price_by_prize() {
+        //given
+        int prizeSecond = 2;
+        int prizeFourth = 4;
+
+        //when
+        LottoPrice lottoPriceSecond = LottoPrice.getLottoPriceByPrize(prizeSecond);
+        LottoPrice lottoPriceFourth = LottoPrice.getLottoPriceByPrize(prizeFourth);
+
+        //then
+        assertThat(lottoPriceSecond).isEqualTo(LottoPrice.SECOND);
+        assertThat(lottoPriceFourth).isEqualTo(LottoPrice.FOURTH);
 
     }
 }

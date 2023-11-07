d1b50fd0bf2bd9596e612ccf15727b6b7147f480
Sun! <87172228+GangSSun@users.noreply.github.com>
2023-11-07 20:37:20 +0900

    feat : LottoGame.java - 로또 번호 생성, 로또 구매 메서드 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,6 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGame {
     private static final int LOTTO_PRICE = 1000;
@@ -16,5 +19,20 @@ public class LottoGame {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
         }
         return purchaseAmount;
+    }
+
+    private static List<Lotto> purchaseLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(generateRandomLottoNumbers()));
+        }
+        return lottos;
+    }
+
+    private static List<Integer> generateRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBERS_COUNT)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }

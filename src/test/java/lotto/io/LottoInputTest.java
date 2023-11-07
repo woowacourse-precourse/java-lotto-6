64fbecc952e7a4ca7375552dc5de6901828935a4
yubin <dbqls2821@gmail.com>
2023-11-07 15:32:39 +0900

    test: 당첨번호를 입력받는 테스트코드 추가

--- src/test/java/lotto/io/LottoInputTest.java
+++ src/test/java/lotto/io/LottoInputTest.java
@@ -2,6 +2,8 @@ package lotto.io;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +23,13 @@ class LottoInputTest {
         assertThatThrownBy(lottoInput::receiveMoney)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(IoException.ERROR.getMessage());
+    }
+
+    @Test
+    void 당첨번호를_입력받는다() {
+        LottoInput lottoInput = new LottoInput(() -> "1,2,3,4,5,6");
+
+        assertEquals(List.of(1,2,3,4,5,6), lottoInput.receiveLottos());
     }
 
 }

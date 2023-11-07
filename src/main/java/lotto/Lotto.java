b8d2318dcf48308ad8d687e15d49949181134e97
uwoobeat <desktop3711@gmail.com>
2023-11-07 19:01:31 +0900

    feat: 로또를 생성할 수 있다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static final int MIN_BOUND = 1;
+    public static final int MAX_BOUND = 45;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,24 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!isValid(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public boolean isValid(List<Integer> numbers) {
+        return hasProperLength(numbers) && hasProperRange(numbers) && hasDistinctNumbers(numbers);
+    }
+
+    private boolean hasProperLength(List<Integer> numbers) {
+        return numbers.size() == LOTTO_NUMBERS_SIZE;
+    }
+
+    private boolean hasProperRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(number -> number >= MIN_BOUND && number <= MAX_BOUND);
+    }
+
+    private boolean hasDistinctNumbers(List<Integer> numbers) {
+        return numbers.stream().distinct().count() == 6;
+    }
 }

1d7e1d18a6cffb3932ceff8a5597eea205047835
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 02:59:38 +0900

    feat: 당첨번호를 입력받는 LottoWinningNumberInputView 클래스 구현

--- src/main/java/view/LottoWinningNumberInputView.java
+++ src/main/java/view/LottoWinningNumberInputView.java
@@ -1,4 +1,18 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoWinningNumberInputView {
+    public List<Integer> readLottoWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winnnigLottoNumber = Console.readLine();
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String winningNumber: winnnigLottoNumber.split(",")) {
+            winningNumbers.add(Integer.parseInt(winningNumber));
+        }
+        return winningNumbers;
+    }
 }

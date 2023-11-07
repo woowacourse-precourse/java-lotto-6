96b495f29e8d79896527287ead833faad2b9ae20
LEE-sh1673 <lsh901673@gmail.com>
2023-11-07 15:35:58 +0900

    feat: 로또 게임에 대한 controller 구현

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,62 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoGame;
+import lotto.model.LottoGenerator;
+import lotto.model.LottoPublisher;
+import lotto.model.Money;
+import lotto.model.ProfitRate;
+import lotto.model.RandomLottoGenerator;
+import lotto.model.WinningDetails;
+import lotto.model.WinningNumber;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    public void run() {
+        try {
+            process();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void process() {
+        final Money money = readPurchaseAmount();
+        final List<Lotto> lottoTickets = publishLotto(money);
+        OutputView.printLottoTickets(lottoTickets);
+
+        final LottoGame lottoGame = new LottoGame(createWinningNumber(), lottoTickets);
+        final WinningDetails winningDetails = lottoGame.play();
+        OutputView.printWinningDetails(winningDetails);
+
+        final ProfitRate profitRate = new ProfitRate(money, winningDetails.sumUpWinningAmount());
+        OutputView.printProfitRate(profitRate);
+    }
+
+    private Money readPurchaseAmount() {
+        return Money.of(InputView.readPurChaseAmount());
+    }
+
+    private List<Lotto> publishLotto(final Money money) {
+        final LottoPublisher publisher = createLottoPublisher();
+        return publisher.publish(money);
+    }
+
+    private LottoPublisher createLottoPublisher() {
+        return new LottoPublisher(createLottoGenerator());
+    }
+
+    private LottoGenerator createLottoGenerator() {
+        return new RandomLottoGenerator();
+    }
+
+    private WinningNumber createWinningNumber() {
+        return new WinningNumber(
+                InputView.readWinningNumbers(),
+                InputView.readBonusNumber()
+        );
+    }
+}

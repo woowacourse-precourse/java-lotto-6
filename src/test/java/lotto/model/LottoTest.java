7131c6fb397e7ae73c8fe4b928242dcb6189c2af
k9want <zndwlrnjr26@daum.net>
2023-11-07 18:40:44 +0900

    test: 로또 번호가 1부터 45사이에 숫자가 아닌 경우 예외 발생

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -22,5 +22,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1부터 45사이에 숫자가 아닌 경우 예외가 발생하다")
+    @Test
+    public void createLottoByRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

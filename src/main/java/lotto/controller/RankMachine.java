52baf87f0b2daaad6fb2a59010631903e838d26b
Gyeongho Yang <chchch1213@naver.com>
2023-11-08 08:11:45 +0900

    feat(controller): 수익률 문장을 만들어주는 함수 구현

--- src/main/java/lotto/controller/RankMachine.java
+++ src/main/java/lotto/controller/RankMachine.java
@@ -5,20 +5,25 @@ import lotto.domain.LottoNumber;
 import lotto.domain.Rank;
 import lotto.domain.Tickets;
 
-import java.text.NumberFormat;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static lotto.constant.Config.PRICE_PER_LOTTO;
+import static lotto.constant.GeneralMessage.NOTIFY_RATE_OF_RETURN;
+
 public class RankMachine {
     private final List<Rank> ranks;
-    private List<Integer> counts;
+    private final List<Integer> counts;
+    private int applyCount;
 
     public RankMachine(List<Rank> ranks) {
         this.ranks = ranks;
         counts = new ArrayList<>(Collections.nCopies(ranks.size(), 0));
+        applyCount = 0;
     }
 
     public void applyTicket(Lotto ticket, Lotto winningTicket, LottoNumber bonusNumber) {
@@ -27,14 +32,15 @@ public class RankMachine {
                 counts.set(i, counts.get(i) + 1);
             }
         }
+        applyCount++;
     }
 
     public void applyTickets(Tickets tickets, Lotto winningTicket, LottoNumber bonusNumber) {
         tickets.lottos().forEach(ticket -> applyTicket(ticket, winningTicket, bonusNumber));
     }
 
-    public String getWinningStatistics() {
-        NumberFormat formatter = NumberFormat.getNumberInstance();
+    public String getRankStatistics() {
+        DecimalFormat formatter = new DecimalFormat("#,###");
         return IntStream
                 .range(0, ranks.size())
                 .mapToObj(index -> String.format(
@@ -46,7 +52,15 @@ public class RankMachine {
     }
 
     public String getRateOfReturn() {
-        // TODO: 수익률 구현
-        return "";
+        DecimalFormat formatter = new DecimalFormat("#,###.0");
+        long amountWinning = IntStream
+                .range(0, ranks.size())
+                .mapToLong(index -> ranks.get(index).reward() * counts.get(index))
+                .sum();
+        double rate = (double) amountWinning / (applyCount * PRICE_PER_LOTTO);
+        return String.format(
+                NOTIFY_RATE_OF_RETURN.getMessage(),
+                formatter.format(Math.round(rate * 1000) / 10.0).concat("%")
+        );
     }
 }

54008adc6e0ae1040b9a092bb244b91f00d0c5f6
lynjun97 <lynjun97@naver.com>
2023-11-07 20:54:30 +0900

    Feat: inputBonusNumber 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -2,13 +2,14 @@ package lotto.controller;
 
 import lotto.Lotto;
 import java.util.List;
+
 import static lotto.controller.LottoController.*;
-import static lotto.controller.LottoController.lottoNumbers;
 import static lotto.view.OutView.outputLottoNumber;
 
 public class GameController {
     private List<Lotto> lotto;
     private List<Integer> list;
+    private int bonusNumber;
 
     public GameController() {
         //구매 금액 입력
@@ -17,6 +18,8 @@ public class GameController {
         outputLottoTickets();
         //당첨 번호 입력
         inputWinningNumbers();
+        //보너스 번호 입력
+        inputBonusNumber();
     }
 
     private void setLotto() {
@@ -30,5 +33,9 @@ public class GameController {
 
     private void inputWinningNumbers() {
         list = lottoNumbers();
+    }
+
+    private void inputBonusNumber() {
+        bonusNumber = setBonusNumber(list);
     }
 }

8903424cfde0d875f9ffbc3efc094ae703ea4625
WinterHana <haha920@naver.com>
2023-11-07 18:40:18 +0900

    feat : LottoGetter 관런 인터페이스 및 클래스 추가
    - 로또 정보를 가저오는 것에 대한 인터페이스와 클래스 추가

---
+++ src/main/java/lotto/controller/LottoGetter.java
@@ -0,0 +1,52 @@
+package lotto.controller;
+
+import lotto.domain.BonusLottoNumber;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResultCalculation;
+import lotto.domain.RandomLottoNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGetter implements LottoGetterInterface {
+    LottoResultCalculation lottoResultCalculation;
+    RandomLottoNumber randomLottoNumber;
+    List<Lotto> randomLottoResult;
+
+    public LottoGetter() {
+        lottoResultCalculation = new LottoResultCalculation();
+        randomLottoNumber = new RandomLottoNumber();
+        randomLottoResult = new ArrayList<>();
+    }
+
+    @Override
+    public int getLottoTicket(int money) {
+        return lottoResultCalculation.caculateLottoTicket(money);
+    }
+
+    @Override
+    public List<Lotto> getLottoNumber(int tickets) {
+        randomLottoResult.clear();
+        for (int i = 0; i < tickets; i++) {
+            randomLottoNumber.initRandomNumber();
+            Lotto lottoNumbers = new Lotto(randomLottoNumber.getNumbers());
+            this.randomLottoResult.add(lottoNumbers);
+        }
+        return randomLottoResult;
+    }
+
+    @Override
+    public BonusLottoNumber getBounsNumber(int bonus) {
+        return new BonusLottoNumber(bonus);
+    }
+
+    @Override
+    public String getLottoString() {
+        StringBuffer stringResult = new StringBuffer();
+        for (Lotto lotto : randomLottoResult) {
+            stringResult.append(lotto.getNumbers().toString());
+            stringResult.append("\n");
+        }
+        return stringResult.toString();
+    }
+}

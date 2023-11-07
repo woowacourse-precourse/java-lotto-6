373d6230a9e484af6f2345451e576bb65d3370ee
BH kang <rkdqudgjs1127@gmail.com>
2023-11-07 16:51:43 +0900

    #12 feat: 당첨 번호 예외처리

--- src/main/java/lotto/model/CorrectNum.java
+++ src/main/java/lotto/model/CorrectNum.java
@@ -1,16 +1,44 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CorrectNum {
-    public ArrayList<Integer> correctNum = new ArrayList<>();
+    public List<Integer> correctNum = new ArrayList<>();
 
     public CorrectNum(String s) {
         String correctString[] =correctStringArray(s);
+        validateCorrectString(correctString);
+        correctStringtoNum(correctString);
     }
 
-    public String[] correctStringArray(String s){
+    private String[] correctStringArray(String s){
         String correctString[] = s.split(",");
         return correctString;
+    }
+    private void correctStringtoNum(String[] correctString){
+
+        for(int i = 0 ; i<correctString.length; i++){
+            correctNum.add(Integer.parseInt(correctString[i]));
+        }
+    }
+    private void validateCorrectString(String[] correctString){
+        for(int i = 0 ; i<correctString.length; i++){
+            String s = correctString[i];
+            validateCorrectStringNum(s);
+            validateCorrectStringRange(s);
+        }
+    }
+
+    private void validateCorrectStringRange(String s) {
+        if(Integer.parseInt(s)>=1 && Integer.parseInt(s)<=45){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateCorrectStringNum(String s) {
+        if (!s.matches("\\d+")) {
+            throw new IllegalArgumentException();
+        }
     }
 }

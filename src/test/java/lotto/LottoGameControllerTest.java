2831a2a27287410250486555924aa5fbc1c85842
jsyeo97 <jsyeo97@naver.com>
2023-11-07 17:44:17 +0900

    test: 보너스 번호 유효성 검사 단위 테스트 추가

--- src/test/java/lotto/LottoGameControllerTest.java
+++ src/test/java/lotto/LottoGameControllerTest.java
@@ -3,15 +3,19 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 
 import lotto.core.LottoGameController;
+import lotto.preprocessor.BonusNumPreprocessor;
 import lotto.preprocessor.PurchasePreprocessor;
 import lotto.preprocessor.WinningNumsPreprocessor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class LottoGameControllerTest {
 
     PurchasePreprocessor purchasePreprocessor = new PurchasePreprocessor();
     WinningNumsPreprocessor winningNumsPreprocessor = new WinningNumsPreprocessor();
+    BonusNumPreprocessor bonusNumPreprocessor = new BonusNumPreprocessor();
 
     @DisplayName("로또 구매 금액이 조건에 맞지 않으면 true를 반환한다.")
     @Test
@@ -48,9 +52,30 @@ class LottoGameControllerTest {
 
     @DisplayName("로또 당첨 번호가 유효하다면 flase를 반환한다.")
     @Test
-    private void isValidWinningNumsTest() {
+    void isValidWinningNumsTest() {
         // 정상 입력
         assertThat(winningNumsPreprocessor.isInvalid("1,2,3,4,5,6")).isFalse();
         assertThat(winningNumsPreprocessor.isInvalid("1, 2, 3, 4, 5, 6")).isFalse();
+    }
+
+    @DisplayName("보너스 번호가 유효하지 않다면 true를 반환한다.")
+    @Test
+    void isInvalidBonusNumsTest() {
+        // init
+        bonusNumPreprocessor.setWinningNumbers(List.of(1,2,3,4,5,6));
+
+        // 중복 테스트
+        assertThat(bonusNumPreprocessor.isInvalid("1")).isTrue();
+        assertThat(bonusNumPreprocessor.isInvalid("6")).isTrue();
+
+        // 숫자 테스트
+        assertThat(bonusNumPreprocessor.isInvalid("1, 2")).isTrue();
+
+        // 범위 테스트
+        assertThat(bonusNumPreprocessor.isInvalid("0")).isTrue();
+        assertThat(bonusNumPreprocessor.isInvalid("46")).isTrue();
+
+        // 정상 입력
+        assertThat(bonusNumPreprocessor.isInvalid("7")).isFalse();
     }
 }

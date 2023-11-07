7fc40aa3a1e8256b0cc0f64a2c8583d7c973e335
FL <refrain2k@gmail.com>
2023-11-07 18:25:02 +0900

    feat: 보너스번호가 당첨번호와 중복될때 표시되는 메시지 추가

--- src/main/java/lotto/validator/ErrorMessage.java
+++ src/main/java/lotto/validator/ErrorMessage.java
@@ -8,6 +8,7 @@ public class ErrorMessage {
 
     public static final String BONUS_NUMBER_IS_NOT_NUMERIC_MESSAGE = "[ERROR] 입력된 보너스 번호가 숫자가 아닙니다.";
     public static final String BONUS_NUMBER_NOT_IN_RANGE_MESSAGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String BONUS_NUMBER_IS_DUPLICATED_BY_LOTTO_NUMBER = "[ERROR] 보너스 번호가 당첨번호와 중복됩니다.";
 
     public static final String PURCHASE_AMOUNT_IS_NOT_NUMERIC_MESSAGE = "[ERROR] 입력된 보너스 번호가 숫자가 아닙니다.";
     public static final String PURCHASE_AMOUNT_IS_NOT_JUSTIFIED_MESSAGE = "[ERROR] 구매 금액이 올바르지 않습니다.";

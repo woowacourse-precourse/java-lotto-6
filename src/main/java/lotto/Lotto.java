4f5525112a1c148ca7537b4013ec512ffd154b8e
Bisi3asi <kueeng8008@naver.com>
2023-11-08 01:48:00 +0900

    feat(lotto) : 비유효 로또 번호 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,9 +12,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 여섯 개의 숫자여야 합니다.");
+        }
+        if (numbers.stream().anyMatch(i -> i < 1 || i > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (numbers.stream().distinct().count() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다");
         }
     }
-
-    // TODO: 추가 기능 구현
 }

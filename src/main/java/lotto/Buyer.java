9a49b507cd34f33e4a4d65baf6cd6efc2a2f2a5a
keongmiiin99 <smartmina7@naver.com>
2023-11-08 01:57:51 +0900

    feat(Buyer): 사용자 정보를 저장하는 클래스

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,2 +1,17 @@
-package lotto;public class Buyer {
+package lotto;
+
+public class Buyer {
+    private int amount;
+
+    public Buyer(){
+        String amountInput = BuyerInfoInput.inputPurchaseAmount();
+        validateAmount(amountInput);
+        this.amount = Integer.parseInt(amountInput);
+    }
+
+    public void validateAmount(String amount){
+        Validator.isValidAmount(amount);
+        Validator.isAmountDivisibleBy1000(amount);
+    }
+
 }

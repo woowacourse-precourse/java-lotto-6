a5eada075ff2edcbb806bbee1fa43d3dc796d14d
gomudayya <gomudayya@gmail.com>
2023-11-08 02:08:35 +0900

    Style : 불필요한 주석 및 임포트문 제거

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,24 +2,13 @@ package lotto.view;
 
 import lotto.Lotto;
 import lotto.LottoGameResult;
-import lotto.constant.ConsoleMessage;
-import lotto.constant.LottoRank;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static lotto.constant.ConsoleMessage.LOTTO_PROFIT_RATE;
 import static lotto.constant.ConsoleMessage.LOTTO_PURCHASE_COMPLETED;
 
 public class OutputView {
-
-//                                    "3개 일치 (5,000원) - 1개",
-//                                    "4개 일치 (50,000원) - 0개",
-//                                    "5개 일치 (1,500,000원) - 0개",
-//                                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-//                                    "6개 일치 (2,000,000,000원) - 0개",
-//                                    "총 수익률은 62.5%입니다.";
 
     public void printPurchasedLottos(List<Lotto> lottos) {
         System.out.printf((LOTTO_PURCHASE_COMPLETED), lottos.size());

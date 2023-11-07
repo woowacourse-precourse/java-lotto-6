0807ed1eaecd570e373d3a40a81f3ac1b280666d
S2Hyeon <tmdgus908@naver.com>
2023-11-08 06:26:03 +0900

    :sparkles: feat : 최종 결과 출력 구현

--- src/main/java/lotto/Constant/Constant.java
+++ src/main/java/lotto/Constant/Constant.java
@@ -1,9 +1,12 @@
 package lotto.Constant;
 
+import java.text.DecimalFormat;
+
 public class Constant {
 
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String NUMBER_PATTERN = "[0-9]+";
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,###");
     public static final int LOTTO_PRICE = 1000;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
--- src/main/java/lotto/View/OutputPrinter.java
+++ src/main/java/lotto/View/OutputPrinter.java
@@ -1,6 +1,12 @@
 package lotto.View;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import lotto.Constant.Constant;
 import lotto.Domain.Lotto;
+import lotto.Domain.LottoReward;
 import lotto.Domain.TotalLotto;
 
 public class OutputPrinter {
@@ -12,5 +18,37 @@ public class OutputPrinter {
             System.out.println(lotto.getNumbers());
         }
         System.out.println();
+    }
+
+    public void printResult(Map<LottoReward, Integer> resultStatistic, String yield) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        List<LottoReward> lottoRewards = Arrays.asList(LottoReward.values());
+        Collections.reverse(lottoRewards);
+        for (LottoReward lottoReward : lottoRewards) {
+            printStatistic(lottoReward, resultStatistic);
+        }
+        System.out.printf("총 수익률은 %s%%입니다.", yield);
+    }
+
+    public void printStatistic(LottoReward lottoReward, Map<LottoReward, Integer> resultStatistic) {
+        if (lottoReward.equals(LottoReward.SECOND_PLACE)) {
+            printBonusStatistic(lottoReward, resultStatistic);
+            return;
+        } else if (lottoReward.equals(LottoReward.DEFAULT)) {
+            return;
+        }
+
+        System.out.printf("%d개 일치 (%s원) - %d개\n",
+                lottoReward.getEqualCount(),
+                Constant.DECIMAL_FORMAT.format(lottoReward.getReward()),
+                resultStatistic.get(lottoReward));
+    }
+
+    public void printBonusStatistic(LottoReward lottoReward, Map<LottoReward, Integer> resultStatistic) {
+        System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n",
+                lottoReward.getEqualCount(),
+                Constant.DECIMAL_FORMAT.format(lottoReward.getReward()),
+                resultStatistic.get(lottoReward));
     }
 }

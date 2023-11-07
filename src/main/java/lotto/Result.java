f5b7676f1fa65656214ffb86133f90cf42428d0e
yeop0740 <ssucgoo@gmail.com>
2023-11-07 20:32:30 +0900

    feat: add Enum type for result

---
+++ src/main/java/lotto/Result.java
@@ -0,0 +1,27 @@
+package lotto;
+
+public enum Result {
+    FIRST(2_000_000_000, "6개 일치 (2,000,000,000원) - %d개\n"),
+    SECOND(30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    THIRD(1_500_000, "5개 일치 (1,500,000원) - %d개\n"),
+    FOURTH(50_000, "4개 일치 (50,000원) - %d개\n"),
+    FIFTH(5_000, "3개 일치 (5,000원) - %d개\n"),
+    NONE(0, null);
+
+    private final int price;
+    private final String message;
+
+    Result(int price, String message) {
+        this.price = price;
+        this.message = message;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}

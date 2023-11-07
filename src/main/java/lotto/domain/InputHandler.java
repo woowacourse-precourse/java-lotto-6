94a798068115218ecfd10b4017b3f7c9afff4cb1
Choijake <shameless8@naver.com>
2023-11-07 09:20:16 +0900

    Refactor : InputHandler 클래스 생성 및 에 당첨 번호 입력 메서드 이동

---
+++ src/main/java/lotto/domain/InputHandler.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+import lotto.view.InputView;
+
+public class InputHandler {
+    public static String inputWinningNumber() {
+        String inputNumber = InputView.inputWinningLottoNumber();
+        return inputNumber;
+    }
+}

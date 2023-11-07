e421b476d46339145f7e0a2e5f614f83ba1e1c7b
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-07 22:52:15 +0900

    feat : 예외 발생시 계속 입력 받는 기능
    - 금액
    - 당첨번호
    - 보너스 번호

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,31 +1,62 @@
 package lotto.controller;
 
-import lotto.model.PurchasePrice;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Budget;
+import lotto.model.lotto.BonusNumber;
+import lotto.model.lotto.WinningLotto;
 import lotto.validator.InputValidator;
 import lotto.view.InputView;
 
 public class LottoController {
     private final InputView inputView;
-    public LottoController(InputView inputView){
+
+    public LottoController(InputView inputView) {
         this.inputView = inputView;
     }
 
-    private PurchasePrice getPurchasePrice(){
-        while(true){
+    private Budget getPurchasePrice() {
+        while (true) {
             try {
                 String inputPrice = inputView.inputPurchasePrice();
-                InputValidator.validateInput(inputPrice);
-                return new PurchasePrice(Integer.parseInt(inputPrice));
-            } catch (Exception e){
+                InputValidator.validateInputPrice(inputPrice);
+                return new Budget(Integer.parseInt(inputPrice));
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public static void main(String[] args) {
-        InputView inputView = new InputView();
-        LottoController lottoController = new LottoController(inputView);
-        lottoController.getPurchasePrice();
+    private WinningLotto getWinningLotto() {
+        while (true) {
+            try {
+                String inputWinningNumber = inputView.inputWinningNumbers();
+                List<Integer> numbers = parseListFromNumbers(inputWinningNumber);
+                return new WinningLotto(numbers);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private BonusNumber getBonusNumber(WinningLotto winningLotto) {
+        while (true) {
+            try {
+                String inputBonusNumber = inputView.inputBonusNumber();
+                InputValidator.validateBonusNumber(inputBonusNumber);
+                return new BonusNumber(Integer.parseInt(inputBonusNumber), winningLotto);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+
+            }
+        }
+    }
+
+    private List<Integer> parseListFromNumbers(String inputNumbers) {
+        return Arrays.stream(inputNumbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
 }

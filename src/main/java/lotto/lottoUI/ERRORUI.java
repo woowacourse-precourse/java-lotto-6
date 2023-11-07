0e84f01c4803303ad9697ca998525db7f7a1e2de
isakdevelop <daishi7462@naver.com>
2023-11-08 03:56:33 +0900

    feat 에러에 대한 UI

--- src/main/java/lotto/lottoUI/ERRORUI.java
+++ src/main/java/lotto/lottoUI/ERRORUI.java
@@ -1,2 +1,31 @@
-package lotto.lottoUI;public class ERRORUI {
+package lotto.lottoUI;
+
+public class ERRORUI {
+    public static void NumberFormatException() {
+        System.out.println("[ERROR] 숫자를 입력하세요.");
+    }
+
+    public static String InputDecimalPointNumberError() {
+        return "[ERROR] 정수를 입력하세요.";
+    }
+
+    public static String InputNegativeError() {
+        return "[ERROR] 양의 정수를 입력하세요.";
+    }
+
+    public static String MultipleOfThousandError() {
+        return "[ERROR] 입력한 금액은 1000으로 나누어 떨어져야 합니다.";
+    }
+
+    public static String InputSixNumberError() {
+        return "[ERROR] 6개의 숫자를 입력하세요.";
+    }
+
+    public static String CategoryOfValueError() {
+        return "[ERROR] 1부터 45 사이의 숫자를 입력하세요.";
+    }
+
+    public static String SameNumberExistError() {
+        return "[ERROR] 중복된 숫자를 제외하고 입력해주세요.";
+    }
 }

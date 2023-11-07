269eac7d9d6dc687d92d005b47813b505fb8c0c0
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 05:06:58 +0900

    feat : enum인 LottoMoney 코드 구현 - getLotto() 메서드 작성

---
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+public enum LottoMoney {
+    ZERO(0,0,false),
+    FIFTH(3,5000,false),
+    FOURTH(4,50000,false),
+    THIRD(5,1500000,false),
+    SECOND(5,30000000, true),
+    FIRST(6,2000000000,false);
+
+
+    private final int matchNumber;
+    private final int money;
+    private boolean matchBonus;
+
+    LottoMoney(int matchNumber, int money, boolean matchBonus) {
+        this.matchNumber = matchNumber;
+        this.money = money;
+        this.matchBonus = matchBonus;
+    }
+
+    public static LottoMoney getLotto(int number, boolean bonus){
+        for(LottoMoney lottoMoney : LottoMoney.values()){
+            if(lottoMoney.matchNumber == number && lottoMoney.matchBonus == bonus){
+                return lottoMoney;
+            }
+        }
+        return ZERO;
+    }
+}

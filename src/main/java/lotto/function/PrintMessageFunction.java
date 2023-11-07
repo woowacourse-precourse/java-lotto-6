a6ef0809db5010cc6a5109f9eaa33a224ed9d691
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-11-08 01:48:14 +0900

    refactor: 출력 코드수정 및 정렬기능 추가
    알맞은 데이터 형식으로 받아오기 위해 코드 수정 및
    test실패로 인해 정렬기능 수정.

--- src/main/java/lotto/function/PrintMessageFunction.java
+++ src/main/java/lotto/function/PrintMessageFunction.java
@@ -2,6 +2,8 @@ package lotto.function;
 
 import lotto.constant.ProgressMessage;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PrintMessageFunction {
@@ -23,20 +25,36 @@ public class PrintMessageFunction {
     }
 
     public void printLottoNumbers(List<Integer> lottoNumbers) {
-        System.out.println(lottoNumbers);
-
+        List<Integer> sortLottoNums = new ArrayList<>(lottoNumbers);
+        Collections.sort(sortLottoNums);
+        System.out.println(sortLottoNums);
     }
 
-    public void printWinningResult() {
+    public void printWinningStat() {
         System.out.println(ProgressMessage.OUTPUT_STATISTICS);
-        System.out.println(ProgressMessage.THREE_WINNING);
-        System.out.println(ProgressMessage.FOUR_WINNING);
-        System.out.println(ProgressMessage.FIVE_WINNING);
-        System.out.println(ProgressMessage.BONUS_WINNING);
-        System.out.println(ProgressMessage.SIX_WINNING);
     }
 
-    public void printRateOfReturn() {
-        System.out.println(ProgressMessage.RATE_OF_RETURN);
+    public void printThreeWinningResult(int winningCount) {
+        System.out.printf(ProgressMessage.THREE_WINNING, winningCount);
+    }
+
+    public void printFourWinningResult(int winningCount) {
+        System.out.printf(ProgressMessage.FOUR_WINNING, winningCount);
+    }
+
+    public void printFiveWinningResult(int winningCount) {
+        System.out.printf(ProgressMessage.FIVE_WINNING, winningCount);
+    }
+
+    public void printSixWinningResult(int winningCount) {
+        System.out.printf(ProgressMessage.SIX_WINNING, winningCount);
+    }
+
+    public void printBonusWinningResult(int winningCount) {
+        System.out.printf(ProgressMessage.BONUS_WINNING, winningCount);
+    }
+
+    public void printRateOfReturn(double rate) {
+        System.out.printf(ProgressMessage.RATE_OF_RETURN, rate);
     }
 }

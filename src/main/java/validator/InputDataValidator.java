8fa7a3bdf459aff8d2f1ca9589eafac11de68fd6
daneng4 <daneng4@naver.com>
2023-11-07 23:25:06 +0900

    feat: 당첨 숫자와 보너스 숫자 검사 기능 추가

--- src/main/java/validator/InputDataValidator.java
+++ src/main/java/validator/InputDataValidator.java
@@ -7,9 +7,24 @@ import java.util.regex.Pattern;
 public class InputDataValidator {
 
     public void validLottoCostInput(String data){
+        validIntegerInput(data);
+        isDivideBy1000(data);
+    }
+
+    public void validWinningNumberInput(String[] data) {
+        validWinningNumberSize(data);
+        for (String singleWinningNum : data) {
+            validIntegerInput(singleWinningNum);
+        }
+    }
+
+    public void validBonusNumberInput(String data){
+        validIntegerInput(data);
+    }
+
+    public void validIntegerInput(String data){
         isNull(data);
         isNumeric(data);
-        isDivideBy1000(data);
     }
 
     public void isNull(String data){
@@ -25,6 +40,13 @@ public class InputDataValidator {
     }
     public void isDivideBy1000(String data){
         if(Integer.parseInt(data) % 1000 != 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validWinningNumberSize(String[] data){
+        int size = Integer.parseInt(Constants.WINNING_NUMBER_SIZE.getConst());
+        if(data.length != size){
             throw new IllegalArgumentException();
         }
     }

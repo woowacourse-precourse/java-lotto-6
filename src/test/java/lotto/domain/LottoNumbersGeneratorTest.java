bbc358df45f77f4d14869f15dd404413361fe12e
IM-GYURI <kgulr0517@gmail.com>
2023-11-08 03:40:32 +0900

    feat: @DisplayName 추가

--- src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumbersGeneratorTest.java
@@ -1,12 +1,11 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoNumbersGeneratorTest {
-
+    @DisplayName("로또 번호 생성 시 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void RandomNumberGenerator() {
         LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();

035d455bf106d5881eecf50830e4f8fec79cc1be
SuhyungK <dook4966@gmail.com>
2023-11-08 02:53:28 +0900

    feat(Application): main 함수에서 사용자의 입력값 받는 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.constants.ErrorMessage;
+import lotto.domain.InputHandler;
+import lotto.domain.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        start();
+    }
+
+    public static void start() {
+        User user = InputHandler.askPayment();
     }
 }

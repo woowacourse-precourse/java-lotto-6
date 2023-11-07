f36bb7b04f3ecfe88029aa1a95b196dd2dc05695
seokmyungham <hellotjraud@gmail.com>
2023-11-07 14:26:43 +0900

    test(PurchaseAmountTest): 구입 금액 검증 조건 변경에 따른 테스트 수정

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PurchaseAmountTest {
-    @DisplayName("구입 금액이 1원 미만이면 예외가 발생한다.")
+    @DisplayName("구입 금액이 1000원 미만이면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(ints = {0, -23})
+    @ValueSource(ints = {0, -2300, 999})
     void purchaseAmountBelowOne(int money) {
         assertThatThrownBy(() -> new PurchaseAmount(money))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("구입 금액은 1원 이상이어야 합니다.");
+                .hasMessage("구입 금액은 1000원 이상이어야 합니다.");
     }
 
     @DisplayName("구입 금액이 1000원 단위가 아니면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1001, 999, 1, 42342})
+    @ValueSource(ints = {1001, 42342})
     void purchaseAmountIsNotMultipleOfThousand(int money) {
         assertThatThrownBy(() -> new PurchaseAmount(money))
                 .isInstanceOf(IllegalArgumentException.class)

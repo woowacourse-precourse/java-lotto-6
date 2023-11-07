c89867d2eb937f538084bf6bbdc8bd4c218a2302
HSBODo <vinsulill@gmaile.com>
2023-11-07 15:40:16 +0900

    refactor : inputPurchaseAmount() 메서드 역할 분리
    수정내용
    - createLotto() 추가
    - lottoNumbersDisplayModelAndView() 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -12,15 +12,11 @@ import java.util.Map;
 
 public class LottoGameController {
 
-
-    private String inputLottoPurchaseAmount;
     private int lottoPurchaseAmount;
-    private int lottoQuantity;
-    private List<Lotto> lotto;
-    private String inputLottoWinningNumber;
-    private List<Integer> winningNumber;
-    private String inputBonusNumber;
     private int bonusNumber;
+    private int lottoQuantity;
+    private List<Integer> winningNumber;
+    private List<Lotto> lotto;
 
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
@@ -28,40 +24,50 @@ public class LottoGameController {
 
     public void gameStart(){
         inputPurchaseAmount();
+        createLotto();
+        lottoNumbersDisplayModelAndView();
         inputWinningNumber();
         inputBonusNumber();
-        lottoWinningResult();
+        lottoWinningResultModelAndView();
     }
 
     private void inputPurchaseAmount(){
         boolean isPurchaseAmountValidation = false;
+        String inputLottoPurchaseAmount = "";
 
         while (!isPurchaseAmountValidation){
             inputLottoPurchaseAmount = inputView.inputPurchaseAmount();
             isPurchaseAmountValidation = lottoGameService.inputPurchaseAmountValidation(inputLottoPurchaseAmount);
         }
-        lottoPurchaseAmount = Integer.parseInt(inputLottoPurchaseAmount);
 
+        lottoPurchaseAmount = Integer.parseInt(inputLottoPurchaseAmount);
+    }
+
+    private void createLotto(){
         lottoQuantity = lottoGameService.lottoQuantity(lottoPurchaseAmount);
         lotto = lottoGameService.createLotto(lottoQuantity);
+    }
 
+    private void lottoNumbersDisplayModelAndView(){
         LottoNumberModel lottoNumberModel = createLottoNumberModel(lotto);
-
         outputView.purchaseLottoNumbersDisplay(lottoNumberModel);
     }
 
     private void inputWinningNumber(){
         boolean isWinningNumberValidation = false;
+        String inputLottoWinningNumber = "";
 
         while (!isWinningNumberValidation){
             inputLottoWinningNumber = inputView.inputWinningNumber();
             isWinningNumberValidation = lottoGameService.inputWinningNumberValidation(inputLottoWinningNumber);
         }
+
         winningNumber = lottoGameService.createWinningNumber(inputLottoWinningNumber);
     }
 
     private void inputBonusNumber(){
         boolean isBonusNumberValidation = false;
+        String inputBonusNumber = "";
 
         while (!isBonusNumberValidation){
             inputBonusNumber = inputView.inputBonusNumber();
@@ -70,13 +76,13 @@ public class LottoGameController {
         bonusNumber = Integer.parseInt(inputBonusNumber);
     }
 
-    private void lottoWinningResult(){
+    private void lottoWinningResultModelAndView(){
         LottoResultModel lottoResultModel = lottoGameService.lottoWinningResultCalculation(lotto, winningNumber, bonusNumber);
         outputView.lottoResultDisplay(lottoResultModel);
     }
 
     private LottoNumberModel createLottoNumberModel(List<Lotto> lotto){
-        LottoNumberModel lottoNumberModel =new LottoNumberModel(lotto);
+        LottoNumberModel lottoNumberModel = new LottoNumberModel(lotto);
         return lottoNumberModel;
     }
 

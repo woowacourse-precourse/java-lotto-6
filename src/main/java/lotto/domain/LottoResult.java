20cce6a26225f47ea0e6e1d67f4c84639e2ac9ad
tam31 <wxogud3377@naver.com>
2023-11-07 14:12:13 +0900

    Refactor(LottoResult) : 사용하지 않는 메서드 제거

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -7,6 +7,7 @@ public class LottoResult {
     private Map<Integer, Integer> winningCount = new LinkedHashMap<>();
     private long totalWinningMoney;
     private double profitMargin;
+
     public LottoResult() {
         addPrizeCount();
         this.totalWinningMoney = 0l;
@@ -24,22 +25,18 @@ public class LottoResult {
         return winningCount;
     }
 
-    public long getTotalWinningMoney() {
-        return totalWinningMoney;
-    }
-
     public double getProfitMargin() {
         return profitMargin;
     }
 
     public void addWinningCount(int number) {
-        if(checkLottoWinner(number)){
+        if (checkLottoWinner(number)) {
             winningCount.put(number, winningCount.get(number) + 1);
         }
     }
 
     public void addTotalWinningMoney(int number) {
-        if(checkLottoWinner(number)){
+        if (checkLottoWinner(number)) {
             totalWinningMoney += LottoPrize.findPrizeMoney(number);
         }
     }
@@ -49,6 +46,6 @@ public class LottoResult {
     }
 
     public void calculateProfitMargin(int money) {
-        this.profitMargin = ((double)this.totalWinningMoney/money)*100;
+        this.profitMargin = ((double) this.totalWinningMoney / money) * 100;
     }
 }

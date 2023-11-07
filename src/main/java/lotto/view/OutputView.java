9d08a28f78058df662bb01eb5087d06da07c5186
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 16:30:43 +0900

    feat(OutputView): 발행한 로또 수량 및 번호를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,7 +5,14 @@ import lotto.dto.LottoDto;
 import lotto.dto.PurchaseLottoDto;
 
 public class OutputView {
+    private static final String PURCHASED_MESSAGE = "%d개를 구매했습니다.%n";
     private static final String PURCHASE_LOTTO_FORM_DELIMITER = " \n";
+
+    public void printPurchaseLotto(PurchaseLottoDto purchaseLottoDto) {
+        System.out.printf(PURCHASED_MESSAGE, purchaseLottoDto.size());
+        String purchaseLottoResultForm = makePurchaseLottoResultForm(purchaseLottoDto);
+        System.out.println(purchaseLottoResultForm);
+    }
 
     private String makePurchaseLottoResultForm(PurchaseLottoDto purchaseLottoDto) {
         List<String> purchaseLottoResults = purchaseLottoDto.getTotalPurchaseLotto()

d96345d17776a20a52e4b980b8fc03f2d8c98f9b
pjaehyun <show6756@gmail.com>
2023-11-07 16:36:09 +0900

    feat: 추가 LottoErrorMessageConstants 클래스 추가 및 에러 메세지 상수 추가

---
+++ src/main/java/constants/LottoErrorMessageConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class LottoErrorMessageConstants {
+    public static final String ERROR_MESSAGE_NON_NUMERIC_VALUE = "[ERROR] 입력된 값에 숫자가 아닌 값이 포함되어 있습니다.";
+
+    public static final String ERROR_MESSAGE_INVALID_LOTTO_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    public static final String ERROR_MESSAGE_INVALID_LOTTO_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 입니다.";
+
+    public static final String ERROR_MESSAGE_ZERO_NOT_ALLOWED = "[ERROR] 입력된 값에 0이 포함되어 있습니다.";
+}

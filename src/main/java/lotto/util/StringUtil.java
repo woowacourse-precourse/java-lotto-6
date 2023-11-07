373143b709865e1bb7a937670ef935dcb2a5eca6
donghoonyeom <dua5410773@naver.com>
2023-11-08 03:01:40 +0900

    chore(util): StringUtil 클래스 수정
    입력 문자열을 정수 리스트로 변환하는 StringUtil 클래스를 개선했습니다.

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -3,17 +3,12 @@ package lotto.util;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StringUtil {
     public static List<Integer> convertToIntegerList(String input, String delimiter) {
-        return splitInput(input, delimiter)
+        return Arrays.stream(input.split(delimiter))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-    }
-
-    private static Stream<String> splitInput(String input, String delimiter) {
-        return Arrays.stream(input.split(delimiter));
     }
 }

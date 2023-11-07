9f71ce136fb649de33fd429766ca66e7c5265ee6
Picka <marmong@naver.com>
2023-11-07 21:56:40 +0900

    feat: 금액 입력 받는 기능

---
+++ src/main/java/service/Inputter.java
@@ -0,0 +1,10 @@
+package service;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Inputter {
+    public String inputPurchaseAmount() {
+        return Console.readLine();
+    }
+
+}

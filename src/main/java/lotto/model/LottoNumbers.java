483bcac541de2f3497013694ce7a93cf443ccaa7
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 00:05:57 +0900

    refactor(LottoNumbers): 로또 번호를 오름차순으로 정렬한다.

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -1,10 +1,18 @@
 package lotto.model;
 
+import static lotto.util.LottoNumbersGenerator.generateLottoNumbers;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoNumbers {
     private final List<List<Integer>> lottoNumbers = new ArrayList<>();
 
+    private List<Integer> sortedLottoNumbers() {
+        return generateLottoNumbers().stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -1,10 +1,10 @@
 package lotto.util;
 
 public class ErrorMessage {
-    public static final String ERROR_LOTTO_SIZE = "번호는 6개여야 합니다.";
-    public static final String ERROR_LOTTO_DUPLICATE = "중복된 숫자가 존재합니다.";
-    public static final String ERROR_LOTTO_OUT_OF_RANGE = "1 ~ 45 사이의 숫자만 가능합니다.";
-    public static final String ERROR_NUMERIC_TYPE = "숫자를 입력해주세요.";
-    public static final String ERROR_MINIMUM_PRICE = "1,000원 이상의 금액을 입력해주세요.";
-    public static final String ERROR_NOT_DIVIDE_LOTTO_PRICE = "1,000원 단위의 금액을 입력해주세요.";
+    public static final String ERROR_LOTTO_SIZE = "[ERROR] 번호는 6개여야 합니다.";
+    public static final String ERROR_LOTTO_DUPLICATE = "[ERROR] 중복된 숫자가 존재합니다.";
+    public static final String ERROR_LOTTO_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_NUMERIC_TYPE = "[ERROR] 숫자를 입력해주세요.";
+    public static final String ERROR_MINIMUM_PRICE = "[ERROR] 1,000원 이상의 금액을 입력해주세요.";
+    public static final String ERROR_NOT_DIVIDE_LOTTO_PRICE = "[ERROR] 1,000원 단위의 금액을 입력해주세요.";
 }

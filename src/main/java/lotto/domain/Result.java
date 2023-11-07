3f9f5245e7c6df89266f5f8e1a088d50cc40d1c9
potatomoong <star901210@hanmail.net>
2023-11-08 01:41:24 +0900

    Feat: 일치하는 번호가 몇개인지 파악하는 기능 구현

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -5,15 +5,31 @@ import java.util.List;
 public class Result {
     private final List<Lotto> purchasedLotto;
     private final int bonusBall;
+    private final List<Integer> winningNumber;
+
     public Result(List<Lotto> purchasedLotto, List<Integer> winningNumber, int bonusBall) {
         this.purchasedLotto = purchasedLotto;
         this.bonusBall = bonusBall;
+        this.winningNumber = winningNumber;
     }
+
     public int[] checkWinning() {
         int[] result = new int[6];
         for (Lotto lotto : purchasedLotto) {
+            int matchCount = countMatchNumber(lotto);
             boolean hasBonusBall = lotto.getNumbers().contains(bonusBall);
         }
         return result;
     }
+
+    private int countMatchNumber(Lotto lotto) {
+        int matchCount = 0;
+        for (int number : lotto.getNumbers()) {
+            if (winningNumber.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
 }

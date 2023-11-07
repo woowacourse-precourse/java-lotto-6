98616e72b09aaca7cfb76adf906ada23c9fd88d1
devhyeon0 <devhyeon0@gmail.com>
2023-11-07 21:17:19 +0900

    feat: 로또 번호 발행 및 수량 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,25 +2,27 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static lotto.view.OutputView.*;
+
 public class InputView {
 
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_LUCKY_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public int getAmount() {
+    public static void getAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
 
-        return Integer.parseInt(Console.readLine());
+        printAmountLottoNumbers(Integer.parseInt(Console.readLine()));
     }
 
-    public String getLuckyNumber() {
+    public static String getLuckyNumber() {
         System.out.println(INPUT_LUCKY_NUMBER_MESSAGE);
 
         return Console.readLine();
     }
 
-    public int getBonusNumber() {
+    public static int getBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
 
         return Integer.parseInt(Console.readLine());

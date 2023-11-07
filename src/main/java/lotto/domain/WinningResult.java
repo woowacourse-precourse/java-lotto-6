f05aa2a0d3821811f7fb3c0cd485704f6616da48
김영준 <rladudwns164@gmail.com>
2023-11-07 22:02:51 +0900

    feat : 당첨 결과 저장 및 관리
    - 당첨 등수와 당첨 횟수를 계산 기능 추가.
    - 당첨된 등수와 관련된 등수 당첨 횟수 조작

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import lotto.constants.LottoConstants;
+import lotto.constants.LottoReward;
+
+public class WinningResult {
+    private final EnumMap<LottoReward, Integer> winningResult;
+
+    private WinningResult(EnumMap<LottoReward, Integer> winningResult) {
+        this.winningResult = winningResult;
+    }
+
+    public static WinningResult create() {
+        EnumMap<LottoReward, Integer> winningResult = new EnumMap<>(LottoReward.class);
+        Arrays.stream(LottoReward.values())
+                .forEach(winningReward -> winningResult.put(winningReward, 0));
+        return new WinningResult(winningResult);
+    }
+
+    public void calWinningResult(WinningLotto winningLotto, Lottos lottos) {
+        lottos.getLottos().stream()
+                .map(lotto -> LottoReward.getWinningReward(lotto.getMatchLottoCount(winningLotto),
+                        lotto.isContain(winningLotto.getBonusNumber().getBonusNumber())))
+                .forEach(this::updateWinningCount);
+    }
+
+    private void updateWinningCount(LottoReward winningReward) {
+        winningResult.put(winningReward,
+                winningResult.get(winningReward) + LottoConstants.PRIZE_COUNT_INCREMENT.getNumber());
+    }
+
+    public Integer getWinningCount(LottoReward winningReward) {
+        return winningResult.get(winningReward);
+    }
+}

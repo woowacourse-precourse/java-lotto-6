edcf384cbaef078d132a2ce2e8bfb4ada69537b2
SungKyum Kim <sungkyum1@naver.com>
2023-11-08 01:13:21 +0900

    refactor(OutputView): 당첨 통계 출력 함수를 private 함수 분리로 개선

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -13,37 +13,61 @@ import java.util.List;
 public class OutputView {
     public static void displayLottos(List<Lotto> lottos) {
         System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
-
-        for (Lotto lotto : lottos) {
-            System.out.println(lotto.getNumbers());
-        }
+        printLottos(lottos);
     }
 
     public static void displayWinningDetails(List<Rank> ranks, WinningYield winningYield) {
         System.out.println("\n당첨 통계");
         System.out.println("---");
+        printRankDetail(ranks);
+        printYield(winningYield);
+    }
 
-        List<Rank> reversedRanks = new ArrayList<>(Arrays.asList(Rank.values()));
-        Collections.reverse(reversedRanks);
-
+    private static void printRankDetail(List<Rank> ranks) {
+        // 출력 형식 요구사항을 위한 정렬
+        List<Rank> reversedRanks = sortRank();
 
         for (Rank rank : reversedRanks) {
-            long count = ranks.stream().filter(r -> r == rank).count();
-            int matchingNumbers = rank.getMatchingNumbers();
-
-            if (rank.name().equals("SECOND")) {
-                String prizeAmountFormatted = NumberFormat.getIntegerInstance().format(rank.getPrizeAmount());
-                System.out.println(matchingNumbers + "개 일치, 보너스 볼 일치 (" + prizeAmountFormatted + "원) - " + count + "개");
+            if (rank == Rank.NONE) {
                 continue;
             }
-
-            if (!rank.name().equals("NONE")) {
-                String prizeAmountFormatted = NumberFormat.getIntegerInstance().format(rank.getPrizeAmount());
-                System.out.println(matchingNumbers + "개 일치 (" + prizeAmountFormatted + "원) - " + count + "개");
-            }
+            long count = countMatches(ranks, rank);
+            System.out.println(rankMessage(rank, count));
         }
+    }
 
-        double yield = winningYield.getYield();
-        System.out.println("총 수익률은 " + yield + "%입니다.");
+    private static long countMatches(List<Rank> ranks, Rank rank) {
+        return ranks.stream().filter(r -> r == rank).count();
+    }
+
+    private static String formatPrizeAmount(Rank rank) {
+        return NumberFormat.getIntegerInstance().format(rank.getPrizeAmount());
+    }
+
+    private static String rankMessage(Rank rank, long count) {
+        if (isSecondRank(rank)) {
+            return rank.getMatchingNumbers() + "개 일치, 보너스 볼 일치 (" + formatPrizeAmount(rank) + "원) - " + count + "개";
+        }
+        return rank.getMatchingNumbers() + "개 일치 (" + formatPrizeAmount(rank) + "원) - " + count + "개";
+    }
+
+    private static boolean isSecondRank(Rank rank) {
+        return rank == Rank.SECOND;
+    }
+
+    private static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    private static void printYield(WinningYield winningYield) {
+        System.out.println("총 수익률은 " + winningYield.getYield() + "%입니다.");
+    }
+
+    private static List<Rank> sortRank() {
+        List<Rank> reversedRanks = new ArrayList<>(Arrays.asList(Rank.values()));
+        Collections.reverse(reversedRanks);
+        return reversedRanks;
     }
 }

55c216c3b5e9e227b4e24ed28e7bdb277565a492
Junobee25 <wnsdhqo@naver.com>
2023-11-07 17:52:59 +0900

    feat(VerificationView): verificationViewStringToInteger 예외 처리 뷰 메서드 구현

--- src/main/java/lotto/view/VerificationView.java
+++ src/main/java/lotto/view/VerificationView.java
@@ -4,6 +4,7 @@ public class VerificationView {
 
     private final static String INPUT_MONEY_ERROR = "[ERROR] 구입 금액은 최소 1000원 이상, 1000원 단위의 금액입니다.";
     private final static String STRING_TO_LONG_ERROR = "[ERROR] 입력 값은 숫자로만 이루어져 있어야 합니다.";
+    private final static String STRING_TO_INTEGER_ERROR = "[ERROR] 입력 값은 숫자로만 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1 부터 45 까지의 숫자로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_LENGTH_ERROR = "[ERROR] 로또 번호 6자리로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_INTEGER_ERROR = "[ERROR] 문자열의 요소는 숫자 패턴이여야 합니다.";
@@ -14,6 +15,11 @@ public class VerificationView {
     public static void verificationViewStringToLong() {
         System.out.println(STRING_TO_LONG_ERROR);
     }
+
+    public static void verificationViewStringToInteger() {
+        System.out.println(STRING_TO_INTEGER_ERROR);
+    }
+
 
     public static void verificationViewRangeLottoNumber() {
         System.out.println(LOTTO_NUMBER_RANGE_ERROR);

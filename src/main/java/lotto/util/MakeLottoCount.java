7d2fba499f8629f7b6c9f89466199de28dddeee1
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 08:11:30 +0900

    feat(MakeLottoCount) : 로또 갯수 반환하는 기능 분리 구현

---
+++ src/main/java/lotto/util/MakeLottoCount.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+public class MakeLottoCount {
+    private static final int lottoPrice = 1000;
+
+    public static int getLottoCount(int purchaseCount){
+        int lottoCount = purchaseCount / lottoPrice;
+        return lottoCount;
+    }
+}

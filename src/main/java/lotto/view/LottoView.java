6b5adc2d9332a43974287a73c7f733d9cddaebc9
swhyeon98 <smreo75@gmail.com>
2023-11-07 18:51:09 +0900

    feat: 숫자 검증 로직 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,7 +2,11 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class LottoView {
+
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
 
     public String input(String message) {
         System.out.println(message);
@@ -11,6 +15,13 @@ public class LottoView {
 
     public int inputPurchaseAmount() {
         String input = input("구입금액을 입력해 주세요.");
+        validateIsNumber(input);
         return Integer.parseInt(input);
+    }
+
+    private void validateIsNumber(String input) {
+        if (!NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
     }
 }

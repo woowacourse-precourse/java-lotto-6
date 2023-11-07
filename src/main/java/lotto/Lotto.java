2c3dfab170c8b51a9b9384e25dd89a6b33e4b008
kihwn <kihwn98@gmail.com>
2023-11-08 03:49:39 +0900

    feat: Lotto 클래스 내에 로또 용지 당 당첨번호와 일치하는 숫자의 개수와 보너스 번호와 일치하는지 확인하는 메서드(compareWinningNumbers)생성 및 일치하는 개수와 보너스 숫자 일치 여부를 묶어주기 위한 Score 클래스 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,9 +17,23 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-
     public void printLottoPaper(Lotto lotto) {
         System.out.println(lotto.numbers);
+    }
+    public Score compareWinningNumbers(WinningNumberSet winningNumberSets) {
+        int countMatch = 0;
+        boolean checkBonus = false;
+        for (String winningNumber : winningNumberSets.getWinningNumbers()) {
+            if (numbers.contains(Integer.parseInt(winningNumber))) {
+                countMatch++;
+            }
+        }
+        if (numbers.contains(winningNumberSets.getBonusNumber())) {
+            checkBonus = true;
+        }
+
+        return new Score(countMatch, checkBonus);
+
     }
 
 }

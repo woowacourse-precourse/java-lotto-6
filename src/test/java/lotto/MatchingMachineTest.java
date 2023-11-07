9218f46d3056e72a3614efeb7c2933a7a4ce0704
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-07 21:49:46 +0900

    Refactor: 추가 자주 쓰이는 리스트 상수로 변경

--- src/test/java/lotto/MatchingMachineTest.java
+++ src/test/java/lotto/MatchingMachineTest.java
@@ -4,18 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
-import java.sql.SQLOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MatchingMachineTest {
 
+    private final Lotto BASIC_LOTTO = new Lotto(Arrays.asList(1,2,3,4,5,6));
     private MatchingMachine matchingMachine;
 
     @BeforeEach
@@ -26,18 +25,16 @@ class MatchingMachineTest {
     @DisplayName("일치하는 로또 번호 개수 찾아오기")
     @Test
     void matchLottoNumberCountTest(){
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
         Lotto winningLotto = new Lotto(Arrays.asList(5,4,8,7,2,9));
-        int count = matchingMachine.countSingleLottoMatchingNumbers(lotto,winningLotto);
+        int count = matchingMachine.countSingleLottoMatchingNumbers(BASIC_LOTTO,winningLotto);
         assertThat(count).isEqualTo(3);
     }
 
     @DisplayName("보너스 숫자 체크 확인")
     @Test
     void checkBonusNumberTest(){
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
-        boolean hasBonus1 = matchingMachine.checkBonusNumber(lotto, 5);
-        boolean hasBonus2 = matchingMachine.checkBonusNumber(lotto, 7);
+        boolean hasBonus1 = matchingMachine.checkBonusNumber(BASIC_LOTTO, 5);
+        boolean hasBonus2 = matchingMachine.checkBonusNumber(BASIC_LOTTO, 7);
         assertThat(hasBonus1).isEqualTo(true);
         assertThat(hasBonus2).isEqualTo(false);
     }
@@ -46,14 +43,12 @@ class MatchingMachineTest {
     @Test
     void countAllLottoNumberTest(){
         List<Lotto> lottos = new ArrayList<>();
-        for(int i =1; i<7; i++){
-            Lotto lotto = new Lotto(Arrays.asList(i, i+1, i+2, i+3, i+4, i+5));
-            lottos.add(lotto);
-        }
+        lottos.add(BASIC_LOTTO);
+        lottos.add(new Lotto(Arrays.asList(7,8,9,10,11,12)));
         Lotto winningLotto = new Lotto(Arrays.asList(1,2,3,7,8,9));
-        List<MatchingCount> list =matchingMachine.countAllLottoMatchingNumbers(lottos, winningLotto, 10);
+        List<MatchingCount> list = matchingMachine.countAllLottoMatchingNumbers(lottos, winningLotto, 10);
         assertThat(list.get(0)).isEqualTo(MatchingCount.THREE);
-        assertThat(list.get(2)).isEqualTo(MatchingCount.THREE);
+        assertThat(list.get(1)).isEqualTo(MatchingCount.THREE);
         Lotto winningLotto2 = new Lotto(Arrays.asList(1,2,3,4,5,10));
         List<MatchingCount> list2 =matchingMachine.countAllLottoMatchingNumbers(lottos, winningLotto2, 6);
         assertThat(list2.get(0)).isEqualTo(MatchingCount.FIVE_BONUS);

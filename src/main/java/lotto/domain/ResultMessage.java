768239e8eede5f4bfd914eb0142e691780623d5f
dj <fjzl12@naver.com>
2023-11-08 02:06:41 +0900

    feat(ResultMessage): 일치한 로또에 대한 통계 출력 문구 작성 및 기능 구현

---
+++ src/main/java/lotto/domain/ResultMessage.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+public enum ResultMessage {
+    THREE_MATCH("3개 일치 (5,000원) - %d개\n"),
+    FOUR_MATCH("4개 일치 (50,000원) - %d개\n"),
+    FIVE_MATCH("5개 일치 (1,500,000원) - %d개\n"),
+    BONUS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    SIX_MATCH("6개 일치 (2,000,000,000원) - %d개\n");
+    private String message;
+    ResultMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}

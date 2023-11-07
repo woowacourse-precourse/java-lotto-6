5a27ee85f6c3f4030eabb80e0adead1b9b5a203a
Hajin-Lee0406 <glgkwls1@naver.com>
2023-11-07 22:43:00 +0900

    feat: enum 관련 상수 추가

--- src/main/java/lotto/model/Constants.java
+++ src/main/java/lotto/model/Constants.java
@@ -1,6 +1,11 @@
 package lotto.model;
 
 public class Constants {
+    public static final String RESULT_GRADE_FIRST = "6개 일치 (2,000,000,000원) - ";
+    public static final String RESULT_GRADE_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String RESULT_GRADE_THIRD = "5개 일치 (1,500,000원) - ";
+    public static final String RESULT_GRADE_FOURTH = "4개 일치 (50,000원) - ";
+    public static final String RESULT_GRADE_FIFTH = "3개 일치 (5,000원) - ";
     public static final String INVALID_MONEY_FORMAT = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String INVALID_NUMBER_SIZE = "[ERROR] 당첨 번호는 6개 입력 가능합니다.";
 }

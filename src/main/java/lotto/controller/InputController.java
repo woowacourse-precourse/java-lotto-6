2792022ada5588b3c619a137e009ded224419a48
hoyeon <highoption@naver.com>
2023-11-07 19:34:30 +0900

    feat(InputController) : makeWinningLotto 메서드 생성
    - 당첨 번호와 보너스 번호를 입력 받아 객체로 생성
    - 두 객체를 바탕으로 WinningLotto 생성

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,9 +1,16 @@
 package lotto.controller;
 
+import static lotto.Utils.convertIntegerList;
+import static lotto.view.InputView.inputBonusNumber;
+import static lotto.view.InputView.inputMoney;
+import static lotto.view.InputView.inputWinningNumber;
+
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
 import lotto.model.Money;
 import lotto.model.Store;
 import lotto.model.User;
-import lotto.view.InputView;
+import lotto.model.WinningLotto;
 
 public class InputController {
     public static User makeUser() {
@@ -14,7 +21,7 @@ public class InputController {
 
     private static Money makeMoney() {
         try {
-            Money money = new Money(InputView.inputMoney());
+            Money money = new Money(inputMoney());
             return money;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -29,6 +36,36 @@ public class InputController {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return makeStore(money);
+        }
+    }
+
+    public static WinningLotto makeWinningLotto(Lotto winningNumbers) {
+        try {
+            WinningLotto winningLotto = new WinningLotto(winningNumbers, makeBonusNumber());
+            return winningLotto;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return makeWinningLotto(winningNumbers);
+        }
+    }
+
+    public static Lotto makeWinningNumbers() {
+        try {
+            Lotto winningNumbers = new Lotto(convertIntegerList(inputWinningNumber()));
+            return winningNumbers;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return makeWinningNumbers();
+        }
+    }
+
+    private static BonusNumber makeBonusNumber() {
+        try {
+            BonusNumber bonusNumber = new BonusNumber(inputBonusNumber());
+            return bonusNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return makeBonusNumber();
         }
     }
 }

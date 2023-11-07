8b9eb52321a9084e0cadc7e197a83b6ec1081513
ASak1104 <khw56184@gmail.com>
2023-11-08 05:15:54 +0900

    test: 로또 숫자가 정렬 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -18,10 +20,23 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호는 정렬되어 출력한다.")
+    @Test
+    void createLottoByDescendingOrder() {
+        // given
+        List<Integer> numbers = List.of(6, 3, 4, 1, 2, 5);
+
+        // when
+        Lotto lotto = new Lotto(numbers);
+
+        // then
+        String actual = lotto.toString();
+        String expected = "[1, 2, 3, 4, 5, 6]";
+
+        assertThat(actual).isEqualTo(expected);
+    }
 }

8921a5f31ba63107b8b0bc9c5c673c509d681f50
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-07 15:43:21 +0900

    feat : 로또 게임 실행 부분 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Amount amount = new Amount();
+        Draw draw = new Draw();
+        Print print = new Print();
+
+        int money = amount.enterAmount();
+        List<Lotto> lottos = draw.issueLottos(money);
+        print.printPurchasedLottos(lottos);
+
+        Lotto winningLotto = draw.enterWinningLottoNums();
+        int bonusNum = draw.enterBonusNum(winningLotto);
+
+        Map<Rank, Integer> drawResult = draw.draw(winningLotto, bonusNum, lottos);
+        print.printDrawResultStats(money, drawResult);
     }
 }

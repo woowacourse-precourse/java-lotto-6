9fdb85928a5f9e2ed43a00ac73adb1d4a89066a1
dmswl6310 <dmswl6310@gmail.com>
2023-11-08 00:55:15 +0900

    feat(WinningLotto): 일치한 번호의 갯수와 보너스 번호를 계산

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,8 +6,22 @@ public class WinningLotto {
     private final List<Integer> numbers;
     private final int bonusNum;
 
-    public WinningLotto(List<Integer> integers, int bonusNum) {
-        this.numbers = integers;
+    public WinningLotto(List<Integer> numbers, int bonusNum) {
+        this.numbers = numbers;
         this.bonusNum = bonusNum;
+    }
+
+    public int countMatchedNum(List<Integer> numbers) {
+        int count = 0;
+        for (int number : numbers) {
+            if (this.numbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isMatchedBonus(List<Integer> numbers) {
+        return numbers.contains(bonusNum);
     }
 }

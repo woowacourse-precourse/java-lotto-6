cbb6f4188932e28323336f4147bcf57d16f7a414
Chan Mi <rkdcksal02@gmail.com>
2023-11-07 20:06:48 +0900

    WinningNumbers 클래스 - 당첨 번호와 보너스 번호 유효성 검사와 순위 계산

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -6,12 +6,15 @@ public class WinningNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
+    // 생성자
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        // 입력값의 유효성을 검사하는 validate 메서드 호출
         validate(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
+    // 입력값의 유효성을 검사하는 메서드
     private void validate(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.size() != 6 || !isInRange(winningNumbers, 1, 45) || !isInRange(bonusNumber, 1, 45)) {
             throw new IllegalArgumentException("당첨 번호와 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
@@ -21,10 +24,12 @@ public class WinningNumbers {
         }
     }
 
+    // 숫자의 범위를 검사하는 메서드
     private boolean isInRange(int number, int min, int max) {
         return number >= min && number <= max;
     }
 
+    // 여러 숫자의 범위를 검사하는 메서드
     private boolean isInRange(List<Integer> numbers, int min, int max) {
         for (int number : numbers) {
             if (!isInRange(number, min, max)) {
@@ -34,6 +39,7 @@ public class WinningNumbers {
         return true;
     }
 
+    // 로또 번호와 당첨 번호를 비교하여 Rank(등수)를 계산하는 메서드
     public Rank calculateRank(Lotto lotto) {
         List<Integer> lottoNumbers = lotto.getNumbers();
         long matchCount = lottoNumbers.stream()

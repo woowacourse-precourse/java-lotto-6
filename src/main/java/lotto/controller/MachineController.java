31e58186831541f809fb91796088a265e76e44cf
choheejin <hjcho9510@gmail.com>
2023-11-07 21:37:31 +0900

    refactor(MachineController): Change the return types of functions

--- src/main/java/lotto/controller/MachineController.java
+++ src/main/java/lotto/controller/MachineController.java
@@ -1,8 +1,6 @@
 package lotto.controller;
 
 import lotto.model.*;
-import lotto.utils.LottoCompare;
-import lotto.utils.Random;
 import lotto.view.OutputView;
 import lotto.view.PurchaseView;
 import lotto.view.WinningLottoView;
@@ -10,28 +8,24 @@ import lotto.view.WinningLottoView;
 import java.util.*;
 
 public class MachineController {
-    private Human human;
-    private WinningLotto winningLotto;
-    private Lottos lottos;
-
     public void execution() {
-        initAmount();
-        lottos = new Lottos(human.getCount());
+        Human human = initAmount();
+        Lottos lottos = new Lottos(human.getCount());
         OutputView.displayPurchaseList(lottos);
-        initWinningLotto();
+        WinningLotto winningLotto = initWinningLotto();
         LottoMachine lottoMachine = new LottoMachine(lottos, winningLotto, human.getCount());
         OutputView.displayLottoCompareResult(lottoMachine.getLottoCompares());
         OutputView.displayProfit(lottoMachine.calcProfit(human.getMoney()));
     }
 
-    public void initAmount() {
+    public Human initAmount() {
         int money = PurchaseView.lotto();
-        human = new Human(money);
+        return new Human(money);
     }
 
-    public void initWinningLotto() {
+    public WinningLotto initWinningLotto() {
         List<Integer> lottos = WinningLottoView.publishLotto();
         int bonus = WinningLottoView.publishBonus(lottos);
-        winningLotto = new WinningLotto(lottos, bonus);
+        return new WinningLotto(lottos, bonus);
     }
 }

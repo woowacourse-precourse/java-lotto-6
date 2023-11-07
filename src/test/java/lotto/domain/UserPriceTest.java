349294875d9c62c92d600a774cf966344ce4dcb4
gusdn0108 <gusdn6671@naver.com>
2023-11-07 14:22:38 +0900

    test(UserPrice) : 구매 금액을 생성을 검증한다.

--- src/test/java/lotto/domain/UserPriceTest.java
+++ src/test/java/lotto/domain/UserPriceTest.java
@@ -1,20 +1,21 @@
 package lotto.domain;
 
+import static lotto.controller.Controller.LOTTO_PRICE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserPriceTest {
-
+    @DisplayName("구매 금액을 생성을 검증한다.")
     @Test
-    void buyCount() {
+    void userPrice(){
+        int price = LOTTO_PRICE * 3;
+        UserPrice userPrice = new UserPrice(price);
+        assertThat(userPrice.getPrice()).isEqualTo(price);
     }
 
-    @Test
-    void buy() {
-    }
 
-    @Test
-    void getPrice() {
-    }
 }

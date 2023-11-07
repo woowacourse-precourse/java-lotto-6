ed950837bc8bc0ad9c74bbddb077d15d606072fa
siwol025 <cyk99115695@gmail.com>
2023-11-07 18:08:50 +0900

    feat(LottoSeller): 받은 금액만큼 로또를 생성해주는 기능 구현

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -10,6 +10,12 @@ public class LottoSeller {
         return LottosAmount;
     }
 
+    public Lottos getLottos(int money) {
+        int numberOfLottos = calculateNumberOfLottos(money);
+
+        return Lottos.createLottos(numberOfLottos);
+    }
+
     public void validatePurchaseAmount(int money) {
         if (money % Lotto_Price != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");

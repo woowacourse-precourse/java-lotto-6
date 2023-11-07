1bba8d9cfcafd7d3f699979eba7c816ed62df61e
hunko <rhwlgns4386@naver.com>
2023-11-07 15:32:44 +0900

    chore: 리펙토링에따른 생성자 주입 코드 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,13 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.domain.ProfitCalculator;
+import lotto.controller.WinnerLottoFactory;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoController controller = new LottoController();
+        LottoController controller = new LottoController(new ProfitCalculator(),new WinnerLottoFactory());
         controller.run();
     }
 }

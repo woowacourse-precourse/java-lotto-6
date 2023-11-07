7081398eab4b6df3d2c135569abab99b4684b163
GS_song <20003204@sju.ac.kr>
2023-11-07 18:25:46 +0900

    fix: Lotto 범위 에러 수정

--- src/main/java/lotto/model/ErrorMessage.java
+++ src/main/java/lotto/model/ErrorMessage.java
@@ -9,7 +9,7 @@ public enum ErrorMessage {
 
     INPUT_POSITIVE_ERROR("[ERROR] 양의 정수만 입력해주세요."),
     BONUS_DUPLICATE_ERROR("[ERROR] 보너스 숫자는 당첨 번호에 포함되지 않은 숫자를 선택해야 합니다."),
-    LOTTO_RANGE_ERROR("[ERROR] 로또 숫자는 " + LOTTO_MIN_NUMBER + "이상" + LOTTO_MAX_NUMBER + "이하인 값만 입력하세요"),
+    LOTTO_RANGE_ERROR("[ERROR] 로또 숫자는 " + LOTTO_MIN_NUMBER + "이상 " + LOTTO_MAX_NUMBER + "이하인 값만 입력하세요"),
     LOTTO_WRONG_SIZE_ERROR("[ERROR] " + LOTTO_SIZE + "개의 숫자를 입력하세요."),
     LOTTO_DUPLICATE_ERROR("[ERROR] 중복하지 않는 숫자만 입력해주세요."),
     MONEY_UNIT_ERROR("[ERROR] 금액은 " + CURRENT_UNIT + "으로 나누어 떨어져야 합니다.");
--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,9 +1,10 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public enum LottoResult {
-    FIFTH(3, 0,"3개 일치", 5000),
+    FIFTH(3, 0, "3개 일치", 5000),
     FOURTH(4, 0, "4개 일치", 50000),
     THIRD(5, 0, "5개 일치", 1500000),
     SECOND(5, 1, "5개 일치, 보너스 볼 일치", 30000000),
@@ -31,32 +32,47 @@ public enum LottoResult {
     }
 
     public static LottoResult compareLottoNums(List<Integer> LottoNums, WinningLotto winningLotto) {
-        int equalLottoCnt = countEqualLotto(LottoNums, winningLotto);
-        int equalBonusCnt = countEqualBonus(LottoNums, winningLotto);
+        int equalLottoCnt = countEqualLotto(LottoNums, winningLotto.getLottoNums());
+        int equalBonusCnt = countEqualBonus(LottoNums, winningLotto.getBonus());
         return findCorrectLottoResult(equalLottoCnt, equalBonusCnt);
     }
 
-    private static int countEqualBonus(List<Integer> LottoNums, WinningLotto winningLotto) {
+    private static int countEqualBonus(List<Integer> LottoNums, int bonus) {
         return (int) LottoNums.stream()
-                .filter(userLottoNum -> userLottoNum.equals(winningLotto.getBonus()))
+                .filter(userLottoNum -> userLottoNum.equals(bonus))
                 .count();
     }
 
-    private static int countEqualLotto(List<Integer> LottoNums, WinningLotto winningLotto) {
+    private static int countEqualLotto(List<Integer> LottoNums, List<Integer> winningLottoNums) {
         return (int) LottoNums.stream()
-                .filter(winningLotto.getLottoNums()::contains)
+                .filter(winningLottoNums::contains)
                 .count();
     }
 
     private static LottoResult findCorrectLottoResult(int equalLottoCnt, int equalBonusCnt) {
         LottoResult findLottoResult = null;
-
-        for (LottoResult lottoResult : LottoResult.values()) {
-            if (lottoResult == LottoResult.DEFAULT) { continue; }
-            if (lottoResult.winningCnt != equalLottoCnt) { continue; }
-            if (lottoResult.winningCnt == 5 && lottoResult.bonusCnt != equalBonusCnt) { continue; }
+        List<LottoResult> lottoResults = getLottoResultExceptDefault();
+        for (LottoResult lottoResult : lottoResults) {
+            if (isDifferent(lottoResult.winningCnt, equalLottoCnt) || checkSecondGrade(equalBonusCnt, lottoResult)) {
+                continue;
+            }
             findLottoResult = lottoResult;
         }
         return findLottoResult;
+    }
+
+    private static List<LottoResult> getLottoResultExceptDefault() {
+        return Arrays.stream(LottoResult.values())
+                .filter(result -> result != LottoResult.DEFAULT)
+                .toList();
+    }
+
+    private static boolean checkSecondGrade(int equalBonusCnt, LottoResult lottoResult) {
+        return lottoResult.winningCnt == LottoResult.SECOND.winningCnt
+                && isDifferent(lottoResult.bonusCnt, equalBonusCnt);
+    }
+
+    private static boolean isDifferent(int a, int b) {
+        return a != b;
     }
 }

24ba7c581ab1a136f8333f57354e94a7174c116c
mmalfoy <stpmalfoy@gmail.com>
2023-11-08 01:54:26 +0900

    refactor(OutputUI) : 패키지분리 /lotto/domain to /lotto/UI

---
+++ src/test/java/lotto/UI/OutputUITest.java
@@ -0,0 +1,39 @@
+package lotto.UI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
+import lotto.data.Rewards;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+
+class OutputUITest {
+    private OutputUI outputUI;
+
+    @BeforeEach
+    void setUp() {
+        outputUI = new OutputUI();
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameterProviderMoneyEdit")
+    void moneyEdit_상금액수_출력형식_변경_테스트(Rewards reward, String expect) {
+        assertThat(outputUI.moneyEdit(reward)).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> parameterProviderMoneyEdit() {
+        return Stream.of(
+                arguments(Rewards.FIRST, "(2,000,000,000원)"),
+                arguments(Rewards.SECOND, "(30,000,000원)"),
+                arguments(Rewards.THIRD, "(1,500,000원)"),
+                arguments(Rewards.FOURTH, "(50,000원)"),
+                arguments(Rewards.FIFTH, "(5,000원)")
+        );
+    }
+}

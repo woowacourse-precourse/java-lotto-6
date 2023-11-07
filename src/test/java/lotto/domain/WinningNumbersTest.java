d9bd10aae1eb5df9a32af92acb1ff373179bd8fb
jun <ggprgrkjh@naver.com>
2023-11-08 03:32:36 +0900

    test(winningnumbers): winningNumbers 로또 번호와 비교 테스트

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,4 +1,43 @@
 package lotto.domain;
 
+import java.util.List;
+import lotto.model.WinningNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
 public class WinningNumbersTest {
+
+    @DisplayName("winningNumbers 로또번호와 비교 정상 작동 확인 테스트")
+    @Test
+    void compareWinningNumbers_EqualCount_Success() {
+
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        long result = winningNumbers.compareWinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(6);
+
+    }
+
+    @DisplayName("winningNumbers 로또번호와 비교 실패 테스트")
+    @Test
+    void compareWinningNumbers_DifferentCount_Success() {
+
+        //given
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        long result = winningNumbers.compareWinningNumbers(List.of(1, 2, 3, 4, 5, 7));
+        //then
+        assertThat(result).isNotNull();
+        assertThat(result).isNotEqualTo(6);
+
+    }
+
+
 }

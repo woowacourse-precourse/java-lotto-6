ccc5773556365d02fa385ebbda41a5afca5f0612
coli-geonwoo <kkwoo001021@naver.com>
2023-11-07 21:31:00 +0900

    Style : 입력 메시지 수정

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -5,8 +5,8 @@ import static lotto.view.Constants.NUM_OF_LOTTO;
 
 public class Message {
     public static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해주세요.";
-    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해주세요.";
+    public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     public static final String TICKET_COUNT = "개를 구매했습니다.";
     public static final String WINNING_STATICS_MESSAGE = "당첨 통계";
 

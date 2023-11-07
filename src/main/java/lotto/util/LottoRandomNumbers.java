0a34b8a075170470b936ab2b8919e7c1b6b2bcf0
GaBaljaintheroom <junsu1222@naver.com>
2023-11-07 11:54:43 +0900

    refactor(LottoRandomNumbers): LottoRule 상수 적용

--- src/main/java/lotto/util/LottoRandomNumbers.java
+++ src/main/java/lotto/util/LottoRandomNumbers.java
@@ -1,19 +1,16 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constants.LottoRule;
 
 import java.util.List;
 
 public final class LottoRandomNumbers {
 
-    private static final int START_NUMBER = 1;
-    private static final int END_NUMBER = 45;
-    private static final int RANDOM_COUNT = 6;
-
     private LottoRandomNumbers() {
     }
 
     public static List<Integer> getRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, RANDOM_COUNT);
+        return Randoms.pickUniqueNumbersInRange(LottoRule.START_NUMBER, LottoRule.END_NUMBER, LottoRule.NUMBER_MAX_SIZE.intValue());
     }
 }

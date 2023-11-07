c75a1cef1b66dd64f096e305d553ae468a013a08
jhva <junghun8158@naver.com>
2023-11-07 23:17:10 +0900

    :recycle: refactor: 로또 상수 메시지 추가

--- src/main/java/lotto/constants/LottoConstants.java
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -7,11 +7,13 @@ public class LottoConstants {
     public static final int LOTTO_MIN_LENGTH = 1;
     public static final int LOTTO_DIVISION = 1000;
     public static final int LOTTO_NOT_DIVISION = 0;
+    public static final int LOTTO_NOT_MATCH_COUNT = 0;
     public static int START = 3;
     public static final int END = 7;
     public static final int FORMMATER_COUNT_BONUS = 5;
     public static final int FORMMATER_COUNT_SIX = 6;
     public static final int FORMMATER_COUNT_SEVEN = 7;
+    public static final int LOTTO_MULTIPLE_100 = 100;
     public static final String WINNING = "당첨";
     public static final String BONUS = "보너스";
     public static final String DIVIDER = "---";

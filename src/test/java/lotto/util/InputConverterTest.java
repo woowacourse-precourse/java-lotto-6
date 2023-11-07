1e430cdc498c86c00569d3a15caa44581e9b2f35
siwol025 <cyk99115695@gmail.com>
2023-11-07 22:13:57 +0900

    test(InputConverterTest): 잘못된 형식의 로또 번호가 입력됐을 때 예외처리 테스트

---
+++ src/test/java/lotto/util/InputConverterTest.java
@@ -0,0 +1,20 @@
+package lotto.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputConverterTest {
+
+    @DisplayName("잘못된 형식의 로또 번호가 입력됐을 때 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {",1,2,3,4,5,6", "1,2,3,4,5,6,", ",1,2,3,4,5,6,", "1,,2,3,4,5,6" , "1,2, ,3,4,5,6", "1,2,3,4,,6", "1, , ,4"})
+    void convertToList(String input) {
+        Assertions.assertThatThrownBy(() -> InputConverter.convertToList(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

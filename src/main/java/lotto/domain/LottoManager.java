4524930c677eecc37fa2cfabe162e49f6b342c99
siwol025 <cyk99115695@gmail.com>
2023-11-07 16:15:38 +0900

    feat(LottoManager): 당첨 로또를 생성하는 기능 구현

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -13,4 +13,8 @@ public class LottoManager {
     public static LottoManager from(WinningDetails winningDetails) {
         return new LottoManager(winningDetails);
     }
+
+    public void createWinningLotto(List<Integer> numbers, int bonusNumber) {
+        winningLotto = WinningLotto.of(numbers, bonusNumber);
+    }
 }

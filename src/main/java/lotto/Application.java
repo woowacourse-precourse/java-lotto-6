89b53aeb400d89ff5d54615a1c4923f6283b9d62
ppdoli123 <ppdoli123@naver.com>
2023-11-07 17:08:36 +0900

    feat(Application): start 메소드 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+import static lotto.domain.WinningLotto.convertToIntegerList;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.start();
     }
 }
+

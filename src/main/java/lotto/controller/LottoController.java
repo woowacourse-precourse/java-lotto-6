825368f17a8e76c33479a2dcf4028c884a05bbfc
donghyun <kdhdh001@naver.com>
2023-11-08 03:32:34 +0900

    refactor: LottoController run 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,28 +3,59 @@ package lotto.controller;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
+import lotto.domain.Prize;
+import lotto.domain.PrizeResult;
+import lotto.domain.WinningLotto;
+import lotto.utils.Constants;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
-    public void run() {
-        int purchaseAmount = InputView.inputPurchaseAmount();
+    private List<Lotto> lottos;
+    private int lottoCount;
+    private int purchaseAmount;
 
-        int lottoCount = purchaseAmount / 1000;
-        List<Lotto> lottos = LottoGenerator.createLottos(lottoCount);
+    public void run() {
+        purchaseLotto();
+        checkWinning();
+    }
+
+    private void purchaseLotto() {
+        purchaseAmount = InputView.inputPurchaseAmount();
+
+        lottoCount = purchaseAmount / Constants.LOTTO_TICKET_PRICE;
+        lottos = LottoGenerator.createLottos(lottoCount);
 
         OutputView.printNewLine();
         OutputView.printTicketCount(lottoCount);
+        printLottoNumbers();
+    }
 
+    private void printLottoNumbers() {
         for (Lotto lotto : lottos) {
             OutputView.printLottoNumbers(lotto);
         }
-
         OutputView.printNewLine();
-        List<Integer> winningNumbers = InputView.inputWinningNumbers();
+    }
 
+    private void checkWinning() {
+        List<Integer> winningNumbers = InputView.inputWinningNumbers();
         OutputView.printNewLine();
         int bonusNumber = InputView.inputBonusNumber(winningNumbers);
+        OutputView.printNewLine();
 
+        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), bonusNumber);
+        PrizeResult prizeResult = getPrizeResult(winningLotto);
+
+        OutputView.printPrizeResult(prizeResult, purchaseAmount);
+    }
+
+    private PrizeResult getPrizeResult(WinningLotto winningLotto) {
+        PrizeResult prizeResult = new PrizeResult();
+        for (Lotto lotto : lottos) {
+            Prize prize = winningLotto.match(lotto);
+            prizeResult.addPrize(prize);
+        }
+        return prizeResult;
     }
 }

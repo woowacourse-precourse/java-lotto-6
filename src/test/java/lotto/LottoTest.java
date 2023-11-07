2c0369e5d92731b5ee262b0a9ce4a9f3d0cc7c45
honee8583 <honee8583@naver.com>
2023-11-07 22:15:40 +0900

    test(lotto): add test code of getting numbers of lotto

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,4 +25,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 가져오기.")
+    @Test
+    void getLottoNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> numbers = lotto.getNumbers();
+        assertEquals(6, numbers.size());
+    }
 }

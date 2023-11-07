57b701bad0c85a32199279ea76e5509dba7a75bc
Hoo-sung <gntjd135@gmail.com>
2023-11-07 14:40:27 +0900

    fix(WinningLottoTest): 테스트 정상 작동하도록 수정

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
@@ -8,22 +7,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.*;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class WinningLottoTest {
-
-    OutputStream out = new ByteArrayOutputStream();
-    InputStream in = System.in;
-
-    @BeforeEach
-    private final void init() {
-        System.setOut(new PrintStream(out));
-    }
 
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     @Nested
@@ -31,19 +20,19 @@ class WinningLottoTest {
     class Grade {
         @ParameterizedTest
         @MethodSource("parameterProvider")
-        void 정답_로또로_다른_로또를_채점하면_같은값_개수를_반환하는지_테스트(String source, Lotto target, int expected) {
-            ByteArrayInputStream in = new ByteArrayInputStream(source.getBytes());
-            System.setIn(in);
-            WinningLotto winningLotto = new WinningLotto();
+        void 정답_로또로_다른_로또를_채점하면_같은값_개수를_반환하는지_테스트(Lotto source, Lotto target, int expected) {
+
+
+            WinningLotto winningLotto = new WinningLotto(source);
 
             assertThat(winningLotto.grade(target)).isEqualTo(expected);
         }
 
         private Stream<Arguments> parameterProvider() {
             return Stream.of(
-                    Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), 6),
-                    Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)), 3),
-                    Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12)), 0)
+                    Arguments.of(new Lotto(Arrays.asList(1,2,3,4,5,6)), new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), 6),
+                    Arguments.of(new Lotto(Arrays.asList(1,2,3,4,5,6)), new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)), 3),
+                    Arguments.of(new Lotto(Arrays.asList(1,2,3,4,5,6)), new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12)), 0)
             );
         }
     }

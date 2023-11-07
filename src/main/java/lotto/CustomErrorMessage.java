be8742cbb2e3d92b311d99ab912605e525e1ae4e
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-07 17:34:26 +0900

    feat($WinningLotto): 당첨 번호 로또 구현
    기존 로또에 더하여 보너스번호가 추가된 번호의 스켈레톤 코드 구현

--- src/main/java/lotto/CustomErrorMessage.java
+++ src/main/java/lotto/CustomErrorMessage.java
@@ -4,6 +4,7 @@ public class CustomErrorMessage {
     private static final String ERROR_PREFIX = "[ERROR]";
     public static final String PRICE_VALID = String.join(" ", ERROR_PREFIX, "금액은 양수여야 합니다.");
     public static final String NUMBER_FORMAT = String.join(" ", ERROR_PREFIX, "입력값은 숫자여야 합니다.");
+    public static final String DUPLICATED_BONUS_NUMBER = String.join(" ", ERROR_PREFIX, "기존 6개 숫자와 중복됩니다.");
     public static final String WRONG_NUMBER_LENGTH = String.join(" ", ERROR_PREFIX, "기본 숫자의 개수가 잘못되었습니다.");
     ;
 }
---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public class WinningLotto {
+    private Lotto lotto;
+    private int bonusNumber;
+
+    public WinningLotto(Lotto lotto, Integer bonusNumber) {
+        this.lotto = lotto;
+        validate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Integer bonusNumber) {
+        if (this.lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(CustomErrorMessage.DUPLICATED_BONUS_NUMBER);
+        }
+    }
+}

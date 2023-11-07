5a14773a1d86adcb9abe42afbefb5aa5586b7b8a
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-07 21:25:04 +0900

    refactor: 보너스 번호에 대한 예외 처리를 DTO에서 검증하도록 리팩토링

--- src/main/java/lotto/domain/LottoWinningBonusNumber.java
+++ src/main/java/lotto/domain/LottoWinningBonusNumber.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.validatior.InputWinningBonusNumberValidator.validateBonusNumberRange;
+import static lotto.validatior.InputWinningBonusNumberValidator.validateOnlyPositiveNumberBonus;
+import static lotto.validatior.InputWinningBonusNumberValidator.validateWinningBonusNumberAlreadyExists;
+
 import java.util.List;
 
 public class LottoWinningBonusNumber {
@@ -13,29 +17,11 @@ public class LottoWinningBonusNumber {
         this.winningBonusNumber = winningBonusNumber;
     }
 
-    public int getWinningBonusNumber() {
-        return winningBonusNumber;
-    }
-
-    private void validateOnlyPositiveNumberBonus(String numbersString) {
-        if (!numbersString.matches("\\d+")) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자로만 입력되어야 합니다.");
-        }
-    }
-
     private int convertToInteger(String winningBonusNumberString) {
         return Integer.parseInt(winningBonusNumberString);
     }
 
-    private void validateBonusNumberRange(int winningBonusNumber) {
-        if (winningBonusNumber < 1 || winningBonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private void validateWinningBonusNumberAlreadyExists(int winningBonusNumber, List<Integer> winningNumbers) {
-        if (winningNumbers.contains(winningBonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 이미 당첨 번호에 존재합니다.");
-        }
+    public int getWinningBonusNumber() {
+        return winningBonusNumber;
     }
 }

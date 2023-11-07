9e7f1f5954b7fdc5f499e777f15944c0be71aeb4
kitewater <97283971+kitewater@users.noreply.github.com>
2023-11-08 01:24:15 +0900

    feat: view 기능 1차 작성
    출력이 확정된 텍스트들을 우선 작성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,13 @@
+package view;
+
+public class InputView {
+    public void printAskPrice(){
+        System.out.printf("구입금액을 입력해 주세요.");
+    }
+    public void printAskLottoNumbers(){
+        System.out.printf("당첨 번호를 입력해 주세요.");
+    }
+    public void printAskBonusNumber(){
+        System.out.printf("보너스 번호를 입력해 주세요.");
+    }
+}
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,14 @@
+package view;
+
+public class OutputView {
+    public void printLottoCount(int count){
+        System.out.printf("%d개를 구매했습니다.", count);
+    }
+    public void printLottoNumbers(){
+        //로또 번호 출력하기
+    }
+    public void printResult(){
+        System.out.printf("당첨 통계\n");
+        System.out.printf("---\n");
+    }
+}

29d04b689f4b8232f8ae7eb00c410b757d86eb51
rakyun1 <dbskim234@naver.com>
2023-11-07 16:37:10 -0700

    Feat(RankEnum.class): add getRankByMatchCount() count(로또가 맞은 개수)를 인자로 받아 필드에 있는 matchCount와 비교후에 해당하는 Rank를 리턴합니다. Lotto 클라스 안에서 checkRank메소드가 길이가 15가 넘어가게 되어서 어떻게 하면 줄일 수 있을까 고민하다 RankEnum에서 count에 해당하는 값을 주면 좋을 것 같아서 코드를 수정하였습니다.

--- src/main/java/lotto/RankEnum.java
+++ src/main/java/lotto/RankEnum.java
@@ -3,18 +3,15 @@ package lotto;
 public enum RankEnum {
 
 	NONE(0, 0),
-
 	FIFTH(3, 5_000),
-
 	FORTH(4, 50_000),
-
 	THIRD(5, 1_500_000),
-
 	SECOND(5, 30_000_000),
 	FIRST(6, 2_000_000_000);
 
 	private final int matchCount;
 	private final int prize;
+
 
 	RankEnum(int matchCount, int prize) {
 		this.matchCount = matchCount;
@@ -29,11 +26,31 @@ public enum RankEnum {
 		return prize;
 	}
 
+	/**
+	 * 요구사항에 명시 되어 있는 출력 값을 지키기 위해 format()을 사용하여
+	 * prize를 천 단위 마다 ,로 구분하여 리턴합니다.
+	 * @return
+	 */
 	public String getFormattedPrize() {
 		return String.format("%,d", prize);
 	}
 
 	public boolean hasBonus() {
 		return this == RankEnum.SECOND;
+	}
+
+	/**
+	 * 로또가 맞은 개수(count)를 인자로 받고, RankEnum.values로 모든 상수와 count 값을 비교 후에
+	 * 해당 개수에 맞는 등급을 리턴합니다.
+	 * @param count
+	 * @return 매치 횟수에 해당하는 RankEnum. 매치 횟수에 해당하는 RankEnum이 없을 경우 RankEnum.NONE을 반환합니다.
+	 */
+	public static RankEnum getRankByMatchCount(int count) {
+		for (RankEnum rank : RankEnum.values()) {
+			if (rank.matchCount == count) {
+				return rank;
+			}
+		}
+		return RankEnum.NONE;
 	}
 }

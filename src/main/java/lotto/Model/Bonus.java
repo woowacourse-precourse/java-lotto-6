4546f975afb0db47a8deb63db1f092a714a77146
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 14:39:13 +0900

    refactor: 오류메시지 상수화

--- src/main/java/lotto/Model/Bonus.java
+++ src/main/java/lotto/Model/Bonus.java
@@ -1,8 +1,13 @@
 package lotto.Model;
 
+import static lotto.Model.Lotto.MAX_LOTTO_NUMBER;
+import static lotto.Model.Lotto.MIN_LOTTO_NUMBER;
 import static lotto.Utils.toInt;
 
 public class Bonus {
+    public static final String ERROR_NOT_BONUS_NUMBER_FORMAT =
+            String.format("[ERROR] 보너스 번호는 %d~%d사이의 숫자로만 입력해야 합니다.", MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+
     int bonusNumber;
 
     public Bonus(String input) {
@@ -14,23 +19,23 @@ public class Bonus {
         return bonusNumber;
     }
 
-    private void validate(String input) {
-        int validInput = isNumber(input);
-        isCorrectRange(validInput);
+    private void validate(String input) throws IllegalArgumentException {
+        int numberInput = validateNumber(input);
+        validateNumberRange(numberInput);
 
     }
 
-    private int isNumber(String input) {
+    private int validateNumber(String input) {
         try {
             return toInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45사이의 숫자로만 입력해야 합니다.");
+            throw new IllegalArgumentException(ERROR_NOT_BONUS_NUMBER_FORMAT);
         }
     }
 
-    private void isCorrectRange(int validInput) {
-        if (validInput < 1 || validInput > 45) {
-            throw new IllegalArgumentException(("[ERROR] 보너스 번호는 1~45사이의 숫자여야 합니다."));
+    private void validateNumberRange(int numberInput) {
+        if (numberInput < MIN_LOTTO_NUMBER || numberInput > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ERROR_NOT_BONUS_NUMBER_FORMAT);
         }
     }
 }

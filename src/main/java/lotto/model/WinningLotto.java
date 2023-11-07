a31da254cb671c73dc233be14225b8ebf9bacca1
NahyeChoi <skgp0628@naver.com>
2023-11-07 23:45:51 +0900

    feat(lotto): 당첨 로또 번호 관리

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,32 +1,21 @@
 package lotto.model;
 
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.model.Lotto;
 
 public class WinningLotto { // 당첨 번호 + 보너스 넘버
 	
-	private final Lotto winningLotto;
+	private final Lotto winningNumber;
 	private final int bonusNumber;
 	
-	public WinningLotto() { //Lotto winningLotto, int bonusNumber
-		this.winningLotto = getWinningNumber();
-		this.bonusNumber = getBonusNumber();
+	public WinningLotto(Lotto winningNumber, int bonusNumber) { 
+		this.winningNumber = winningNumber;
+		this.bonusNumber = bonusNumber;
 	}
 	
-	OutputView outputView = new OutputView();
-	InputView inputView = new InputView();
-	
-	public Lotto getWinningNumber() { //당첨 번호 입력
-		outputView.printWinningNumbers();
-		WinningNumbers winningNumbers =  new WinningNumbers(inputView.winningNumbers());
-		Lotto winningLottoNumbers = new Lotto(winningNumbers.getWinningNumbers());
-		return winningLottoNumbers;
+	public Rating matchLotto(Lotto lotto) { //당첨 매치
+		int matchCount = lotto.match(winningNumber);
+		boolean isContainBonus = lotto.containNumber(bonusNumber);
+		
+		return Rating.rating(matchCount, isContainBonus);
 	}
-	
-	public int getBonusNumber() { //보너스 번호 입력
-		outputView.printBonusNumber();
-		BonusNumber bonusNumber = new BonusNumber(inputView.bonusNumber());
-		return bonusNumber.getBonusNumber();
-	}
-	
 }

22fbcbdfbfcda2f05bb3e4bf937d018bf88fb477
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-07 17:05:05 +0900

    feat: 로또 수익률 계산 로직
    로또 수익률을 계산하는 로직을 작성했다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class LottoGame {
     private final List<Lotto> lottos = new ArrayList<>();
-    private int firstPlaceCount, secondPlaceCount, thirdPlaceCount, fourthPlaceCount, fifthPlaceCount;
+    private int firstPlaceCount = 0, secondPlaceCount = 0, thirdPlaceCount = 0, fourthPlaceCount = 0, fifthPlaceCount = 0;
 
     public void run() {
         Integer purchaseAmount = InputView.inputPurchaseAmount();
@@ -17,27 +17,38 @@ public class LottoGame {
 
         checkLottoWinningRank(winningNumbers, bonusNumber);
         printRank();
+
+        incomeRate(purchaseAmount);
+    }
+
+    private void incomeRate(Integer purchaseAmount) {
+        Double sum = (double) (firstPlaceCount * 2000000000 + secondPlaceCount * 30000000 + thirdPlaceCount * 1500000 + fourthPlaceCount * 50000 + fifthPlaceCount * 5000);
+        Double per = ((sum - purchaseAmount) / purchaseAmount) * 100.;
+        per = 100 + per;
+        per = Math.round(per * 10) / 10.;
+
+        System.out.println("총 수익률은 " + per + "%입니다.");
     }
 
     private void checkLottoWinningRank(List<Integer> winningNumbers, Integer bonusNumber) {
         for (Lotto lotto : lottos) {
             Rank rank = lotto.checkLottoWinningRank(winningNumbers, bonusNumber);
-            if(rank == Rank.FIRST_PLACE) {
+            if (rank == Rank.FIRST_PLACE) {
                 firstPlaceCount += 1;
-            } else if(rank == Rank.SECOND_PLACE) {
+            } else if (rank == Rank.SECOND_PLACE) {
                 secondPlaceCount += 1;
-            } else if(rank == Rank.THIRD_PLACE) {
+            } else if (rank == Rank.THIRD_PLACE) {
                 thirdPlaceCount += 1;
-            } else if(rank == Rank.FOURTH_PLACE) {
+            } else if (rank == Rank.FOURTH_PLACE) {
                 fourthPlaceCount += 1;
-            } else if(rank == Rank.FIFTH_PLACE) {
+            } else if (rank == Rank.FIFTH_PLACE) {
                 fifthPlaceCount += 1;
             }
         }
     }
 
     private void createLottos(Integer purchaseAmount) {
-        System.out.println("\n" + purchaseAmount /1000 + "개를 구매했습니다.");
+        System.out.println("\n" + purchaseAmount / 1000 + "개를 구매했습니다.");
         for (int i = 0; i < purchaseAmount / 1000; i++) {
             Lotto lotto = new Lotto(LottoNumberGenerator.run());
             lottos.add(lotto);

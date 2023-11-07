2e1e86eadabd55deb9eb0706ab0998b1e6a0c66d
Lee9Bin <imagen33@daum.net>
2023-11-07 23:26:11 +0900

    feat : 문자가 있는 경우 예외처리 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,17 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputView {
     protected String inputValue() {
         return Console.readLine();
+    }
+
+    protected void isDigitValidate(String result) {
+        Pattern PATTERN = Pattern.compile("^[0-9]+$");
+        if (!PATTERN.matcher(result).matches()) {
+            throw new IllegalArgumentException("[ERROR] 금액을 잘못 입력했습니다.");
+        }
     }
 }

04b8c3cc72fb7651bcf9b8acde3de9145b5b5a76
KimChanJin97 <cjkimhello97@gmail.com>
2023-11-07 17:23:28 +0900

    feat: implement Lotto validations

--- src/main/java/lotto/domain/common/Lotto.java
+++ src/main/java/lotto/domain/common/Lotto.java
@@ -2,9 +2,13 @@ package lotto.domain.common;
 
 
 import lotto.exception.InputView.InputLottoNotNumericException;
+import lotto.exception.Lotto.LottoDuplicationException;
+import lotto.exception.Lotto.LottoNotInRangeException;
 import lotto.exception.Lotto.LottoNotSizeSixException;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public record Lotto(List<Integer> numbers) {
@@ -15,10 +19,11 @@ public record Lotto(List<Integer> numbers) {
     private static final String NUMERIC_REGEX = "^[0-9]*$";
 
 
-
     public Lotto {
         validateLottoSize(numbers);
         validateLottoNumeric(numbers);
+        validateLottoRange(numbers);
+        validateLottoDuplication(numbers);
     }
 
     private void validateLottoSize(List<Integer> numbers) {
@@ -33,5 +38,20 @@ public record Lotto(List<Integer> numbers) {
         }
     }
 
+    private void validateLottoRange(List<Integer> numbers) {
+        for (int i : numbers) {
+            if (i < LOTTO_MIN_NUMBER || i > LOTTO_MAX_NUMBER) {
+                throw new LottoNotInRangeException();
+            }
+        }
+    }
 
+    private void validateLottoDuplication(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new LottoDuplicationException();
+            }
+        }
+    }
 }

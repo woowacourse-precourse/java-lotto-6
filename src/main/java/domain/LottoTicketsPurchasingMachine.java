ceea7dec89c2769b4f2e2a980a0fbec9876fdd60
eora21 <eora21@naver.com>
2023-11-07 15:47:03 +0900

    refactor: 💡 수정 로또 금액 결제에 Amount 적용, 테스트코드 통과 가능하도록 변경

--- src/main/java/domain/LottoTicketsPurchasingMachine.java
+++ src/main/java/domain/LottoTicketsPurchasingMachine.java
@@ -1,25 +1,29 @@
 package domain;
 
-import exception.NotPositivePurchaseAmountException;
 import exception.NotCorrectUnitAmountException;
+import exception.NotPositivePurchaseAmountException;
 
 public class LottoTicketsPurchasingMachine {
-    private static final int LOTTO_UNIT_AMOUNT = 1_000;
-    public int purchaseOfLottoTickets(int purchaseAmount) {
-        validateAmount(purchaseAmount);
+    private static final Amount LOTTO_UNIT_AMOUNT = new Amount(1_000);
+    public int purchaseOfLottoTickets(Amount purchaseAmount) {
+        ValidateZeroAmount(purchaseAmount);
         validatePurchaseAmountWithUnitAmount(purchaseAmount);
-        return purchaseAmount / LOTTO_UNIT_AMOUNT;
+        return purchaseAmount.calculateQuotientByUnitAmount(LOTTO_UNIT_AMOUNT);
     }
 
-    private void validateAmount(int purchaseAmount) {
-        if (purchaseAmount <= 0) {
+    private void ValidateZeroAmount(Amount purchaseAmount) {
+        if (purchaseAmount.isZeroAmount()) {
             throw new NotPositivePurchaseAmountException();
         }
     }
 
-    private void validatePurchaseAmountWithUnitAmount(int purchaseAmount) {
-        if (purchaseAmount % LOTTO_UNIT_AMOUNT != 0) {
-            throw new NotCorrectUnitAmountException(LOTTO_UNIT_AMOUNT);
+    private void validatePurchaseAmountWithUnitAmount(Amount purchaseAmount) {
+        Amount modAmount = purchaseAmount.calculateModAmountByUnitAmount(LOTTO_UNIT_AMOUNT);
+
+        if (modAmount.isZeroAmount()) {
+            return;
         }
+
+        throw new NotCorrectUnitAmountException(LOTTO_UNIT_AMOUNT);
     }
 }

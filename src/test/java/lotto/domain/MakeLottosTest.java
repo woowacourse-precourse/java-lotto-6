64319a138c9b988f7881505e654088a3ebdea4ff
74059 <shyeone@naver.com>
2023-11-07 10:00:10 +0900

    test: 로또 하나 당 6개의 숫자가 들어감

--- src/test/java/lotto/domain/MakeLottosTest.java
+++ src/test/java/lotto/domain/MakeLottosTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class MakeLottosTest {
     private int PURCHACE_NUM = 7;
@@ -16,5 +17,12 @@ public class MakeLottosTest {
     void createRandomLottoNums() {
         List<List<Integer>> makeRandomNums = new MakeLottos().makeLotto(PURCHACE_NUM);
         assertThat(makeRandomNums.size()).isEqualTo(PURCHACE_NUM);
+    }
+
+    @DisplayName("로또 하나 당 6개의 숫자가 들어있다.")
+    @Test
+    void createRandomLottoTotal6() {
+        List<List<Integer>> makeRandomNums = new MakeLottos().makeLotto(PURCHACE_NUM);
+        IntStream.range(0, PURCHACE_NUM).forEach(i -> assertThat(makeRandomNums.get(i).size()).isEqualTo(LOTTO_NUM));
     }
 }

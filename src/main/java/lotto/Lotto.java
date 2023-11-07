153ef9b61277ac50bf133543236ab33e372b5893
seohokim <shk9946@gmail.com>
2023-11-07 14:40:08 +0900

    [FEAT] 로또 가격, 번호 범위 상수 정의

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    private static final int LOTTO_PROCE = 1000;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_NUMBER = 1;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -16,5 +18,5 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    
 }

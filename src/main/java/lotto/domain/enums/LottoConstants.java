e6c49d30e76ceecfe12b6a5dc8c69710ad4b32ce
sudhdkso <waga1009@gmail.com>
2023-11-08 03:00:33 +0900

    feat: 보너스 번호 상수 추가

--- src/main/java/lotto/domain/enums/LottoConstants.java
+++ src/main/java/lotto/domain/enums/LottoConstants.java
@@ -4,4 +4,6 @@ public class LottoConstants {
     public static final int MIN_LOTTO_NUM = 1;
     public static final int MAX_LOTTO_NUM = 45;
     public static final int LOTTO_SIZE = 6;
+
+    public static final int CHECK_BONUS_COUNT = 5;
 }

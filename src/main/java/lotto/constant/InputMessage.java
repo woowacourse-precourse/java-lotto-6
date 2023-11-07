3888e324be54040e23921b0792466e96256b80a0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 02:48:48 +0900

    refactor: amount를 money로 작명 변경
    직관적인 이름으로 변수명 및 메서드명 변경

--- src/main/java/lotto/constant/InputMessage.java
+++ src/main/java/lotto/constant/InputMessage.java
@@ -2,7 +2,7 @@ package lotto.constant;
 
 public class InputMessage {
 
-    public static final String LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
     public static final String LOTTO_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,15 @@
 package lotto.view;
 
 import static lotto.constant.InputMessage.BONUS_NUMBER;
-import static lotto.constant.InputMessage.LOTTO_PURCHASE_AMOUNT;
+import static lotto.constant.InputMessage.LOTTO_PURCHASE_MONEY;
 import static lotto.constant.InputMessage.LOTTO_WINNING_NUMBERS;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String readLottoPurchaseAmount() {
-        System.out.println(LOTTO_PURCHASE_AMOUNT);
+    public static String readLottoPurchaseMoney() {
+        System.out.println(LOTTO_PURCHASE_MONEY);
         return Console.readLine();
     }
 

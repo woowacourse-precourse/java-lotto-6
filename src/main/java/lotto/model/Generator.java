5886338c541f926cec36c41ab7e4e407574cc8d2
jun <ggprgrkjh@naver.com>
2023-11-07 15:15:39 +0900

    refactor(generator): 매직넘버 제거

--- src/main/java/lotto/model/Generator.java
+++ src/main/java/lotto/model/Generator.java
@@ -7,9 +7,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Generator {
+
+    private static final int RANDOM_START_NUMBER = 1;
+    private static final int RANDOM_END_NUMBER = 45;
+    private static final int RANDOM_PICK_SIZE = 6;
+
+
     public static List<Integer> createNumbers() {
 
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).stream().distinct().sorted()
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER,
+                        RANDOM_PICK_SIZE).stream().distinct().sorted()
                 .collect(Collectors.toList());
 
         return numbers;

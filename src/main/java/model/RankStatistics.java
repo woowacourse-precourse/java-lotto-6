655fd091803540131f2a4a8e52e8edb4f3d1c57c
nayonsoso <panda0329@naver.com>
2023-11-08 04:58:07 +0900

    feat: 등수 통계 조회하는 기능 구현

--- src/main/java/model/RankStatistics.java
+++ src/main/java/model/RankStatistics.java
@@ -6,6 +6,10 @@ public class RankStatistics {
 
     private EnumMap<LottoRank, Integer> rankStatistics;
 
+    public EnumMap<LottoRank, Integer> getRankStatistics() {
+        return this.rankStatistics;
+    }
+
     public void add(LottoRank rank) {
         int rankCount = rankStatistics.getOrDefault(rank, 0);
         rankStatistics.put(rank, rankCount + 1);

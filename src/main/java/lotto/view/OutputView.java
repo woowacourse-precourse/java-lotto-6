efac826c9abe6bd886b8991c859888c7871df3a4
Hayoon <hy97@sju.ac.kr>
2023-11-07 14:48:16 +0900

    feat(OutputView): List<Lotto>의 원소별 로또번호를 출력하는 printLottoNumbers() 메서드를 구현한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,18 @@
 package lotto.view;
 
+import lotto.model.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
 
     public void printLottoAmount(int num) {
         System.out.printf("%d개를 구매했습니다.%n", num);
     }
 
+    public void printLottoNumbers(List<Lotto> lottoList) {
+        for(Lotto lotto : lottoList) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
 }

ce88ac1a97d5578bab35c3441b5506a4af8023b7
의찬 <srja432130@gmail.com>
2023-11-07 19:03:17 +0900

    feat: 로또 번호 범위를 확인하는 메소드를 추가

--- src/main/java/lotto/view/inputView.java
+++ src/main/java/lotto/view/inputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.InvalidRangeLottoNumberException;
 
 public class inputView {
+    private static final int MIN_RANGE_NUMBER = 1;
+    private static final int MAX_RANGE_NUMBER = 45;
 
     public String inputValue() {
         return Console.readLine();
+    }
+
+    private void validateRange(int number) {
+        if (!(MIN_RANGE_NUMBER <= number && number <= MAX_RANGE_NUMBER)) {
+            throw new InvalidRangeLottoNumberException();
+        }
     }
 }

cd39131fd921e82f5d77b3445e4c66adf984bca1
Lee hwee <gee6121@naver.com>
2023-11-08 03:15:40 +0900

    Feat : 사용자 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,19 +11,30 @@ public class Application {
         final int money = 1000;
         int count;
         int total_price;
+        String price = Console.readLine();
 
         public void game_num() {
             System.out.println("구입금액을 입력해 주세요.");
             String price = Console.readLine();
             total_price = Integer.parseInt(price);
-            count = total_price / money;
-            System.out.printf("%d개를 구매했습니다.", this.count);
+
+            while (true) {
+                try {
+                    count = total_price / money;
+                    System.out.printf("%d개를 구매했습니다.", this.count);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("[ERROR] 숫자를 입력해 주세요.");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("[ERROR] 1,000원 이상의 금액을 입력해 주세요.");
+                }
+                return;
+            }
         }
     }
 
-    public class {
-
-    }
 }
+
+
 
 

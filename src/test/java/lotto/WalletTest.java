9b1339dcea3c11f5534bf52ea369972d67b1a050
minkyole <7719s@nave.com>
2023-11-07 15:56:32 +0900

    test(WalletTest.java) : 구입금액이 정상적으로 들어온 경우 테스트 구현
    - 구입금액이 정상적으로 들어온 경우 테스트 구현완료

--- src/test/java/lotto/WalletTest.java
+++ src/test/java/lotto/WalletTest.java
@@ -1,13 +1,17 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.lang.reflect.Field;
+import lotto.model.Lotto;
 import lotto.model.Wallet;
+import lotto.model.WinningLotto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -33,10 +37,10 @@ public class WalletTest {
                 .hasMessageContaining("[Error] 구입 금액에 숫자가 아닌 값이 들어왔습니다.");
     }
 
-    @DisplayName("구입 금액이 null인 경우 예외가 발생한다.")
+    @DisplayName("구입 금액이 null이거나 비어있는 경우 예외가 발생한다.")
     @ParameterizedTest
-    @NullSource
-    void createWalletByNullMoney(String money) {
+    @NullAndEmptySource
+    void createWalletByNullOrEmptyMoney(String money) {
         assertThatThrownBy(() -> new Wallet(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
@@ -62,5 +66,18 @@ public class WalletTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
                         + maximumPurchaseAmount + "원.");
+    }
+
+    @DisplayName("구입 금액이 정상적으로 들어온 경우.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "45000", "999999000"})
+    void createWinningLottoByNormal(String money) throws NoSuchFieldException, IllegalAccessException {
+        Wallet wallet = new Wallet(money);
+
+        Field privateMoney = Wallet.class.getDeclaredField("money");
+        privateMoney.setAccessible(true);
+        int walletPrivateMoney = (int) privateMoney.get(wallet);
+
+        assertEquals(walletPrivateMoney, Integer.parseInt(money));
     }
 }

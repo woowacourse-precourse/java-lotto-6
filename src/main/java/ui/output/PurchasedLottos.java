9b80eea5644eb9ff10b0db59f4b9e7d768705315
nohheeyeon <heeyeon1213@gmail.com>
2023-11-07 19:06:52 +0900

    feat : 로또 번호 오름차순으로 출력

--- src/main/java/ui/output/PurchasedLottos.java
+++ src/main/java/ui/output/PurchasedLottos.java
@@ -4,13 +4,14 @@ import domain.LottoTicket.LottoTicket;
 import lotto.Lotto;
 
 import java.util.List;
-
+import java.util.Collections;
 public class PurchasedLottos {
     public static void printPurchasedLottos(List<LottoTicket> lottoTickets) {
         System.out.println();
         System.out.println(lottoTickets.size() + "개를 구매했습니다.");
         for (LottoTicket ticket : lottoTickets) {
             for (Lotto lotto : ticket.getLottoNumbers()) {
+                Collections.sort(lotto.getNumbers());
                 System.out.println(lotto.getNumbers());
             }
         }

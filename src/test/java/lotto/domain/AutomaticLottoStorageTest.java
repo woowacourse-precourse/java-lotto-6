34b6c06c039d2dcc7a0461b0ee8ca37af0a29d73
kimdoha <kkhhjj3393@gmail.com>
2023-11-08 03:28:57 +0900

    test(AutomaticLottoStorage): 자동 로또 저장소 객체 생성 테스트 작성

--- src/test/java/lotto/domain/AutomaticLottoStorageTest.java
+++ src/test/java/lotto/domain/AutomaticLottoStorageTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import lotto.common.Constant;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,16 +11,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class AutomaticLottoStorageTest {
 
-//    @DisplayName("자동 로또 저장소 객체 생성 테스트")
-//    @Test
-//    void createAutomaticLottoStorage() {
-//        AutomaticLottoStorage.from();
-//    }
+    @DisplayName("자동 로또 저장소 객체 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = { 1000, 2000, 3000, 4000, 5000 })
+    void createAutomaticLottoStorage(Integer totalPrice) {
+        // Arrange & Act
+        AutomaticLottoStorage automaticLottoStorage = AutomaticLottoStorage.from(totalPrice);
+
+        // Assert
+        Assertions.assertEquals(
+                totalPrice / Constant.LOTTO_TICKET_PRICE,
+                automaticLottoStorage.getAutomaticLottos().size());
+    }
 
     @DisplayName("로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 IllegalArgumentException 발생")
     @ParameterizedTest
-    @ValueSource(ints = { 500, 1500, 10500, 10050, 10005 })
-    void createAutomaticLottoStorage(Integer totalPrice) {
+    @ValueSource(ints = {500, 1500, 10500, 10050, 10005})
+    void checkInvalidTotalPrice(Integer totalPrice) {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> AutomaticLottoStorage.from(totalPrice));
     }

d637c537b37e3563b08acd7b5fcbf98404f36e2e
PARK9898 <wjddjs21@naver.com>
2023-11-08 01:32:38 +0900

    "refactor: 메서드명 변경"

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,20 +8,20 @@ public class InputView {
 
     private static final String STRING_DELIMITER = ",";
 
-    public int getPurchaseAmount() {
+    public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
 
         return StringHandler.stringToInt(input);
     }
 
-    public List<Integer> getWinningNumbers() {
+    public List<Integer> inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         return StringHandler.separatedWith(input, STRING_DELIMITER);
     }
 
-    public int getBonusNumber() {
+    public int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         return StringHandler.stringToInt(input);

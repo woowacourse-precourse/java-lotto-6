74ec1191dd9b195e322df03aec15004d9a016954
yunseung <yseungun.21@gmail.com>
2023-11-08 02:46:04 +0900

    chore(ErrorMessage): 'NOT_POSITIVE_NUMBER' 에러메시지 내용 수정

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -3,6 +3,6 @@ package constant;
 public class ErrorMessage {
     private final static String SYMBOL = "[ERROR] ";
     public final static String INVALID_NUMBER_COUNT = SYMBOL + "입력된 번호가 6개가 아닙니다. 다시 입력하세요.";
-    public final static String NOT_NUMBER = SYMBOL + "숫자가 아닌 것을 입력했습니다. 다시 입력하세요.";
+    public final static String NOT_POSITIVE_NUMBER = SYMBOL + "양의 정수만 입력 가능합니다. 다시 입력하세요.";
     public final static String NOT_DIVISIBLE_BY_1000 = SYMBOL + "구입 금액은 1000원 단위여야 합니다. 다시 입력하세요.";
 }

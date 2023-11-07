65fc7d88944c6d459cc4c9aee6c1eb31c4d36192
yunju-m <dbswnfnal@naver.com>
2023-11-07 15:38:28 +0900

    Feat(LuckeyLotto): ✨ 보너스 로또 번호가 숫자 타입인지 확인하는 유효성 검사

--- src/main/java/lotto/LuckeyLotto.java
+++ src/main/java/lotto/LuckeyLotto.java
@@ -3,13 +3,21 @@ package lotto;
 import java.util.List;
 
 public class LuckeyLotto {
+    private static final String WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호의 타입은 숫자이어야 합니다.";
     private int luckeyBonusNumber;
     private List<Integer> luckeyNumbers;
 
     public LuckeyLotto(List<Integer> luckeyNumbers, String inputBonusNumber) {
+        validate(luckeyNumbers, inputBonusNumber);
         int luckeyBonusNumber = Integer.valueOf(inputBonusNumber);
         luckeyNumbers.add(luckeyBonusNumber);
         this.luckeyNumbers = luckeyNumbers;
         this.luckeyBonusNumber = luckeyBonusNumber;
+    }
+
+    private void validate(List<Integer> luckeyNumbers, String inputBonusNumber) {
+        if (!inputBonusNumber.matches("\\d+")) {
+            throw new IllegalArgumentException(WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE);
+        }
     }
 }

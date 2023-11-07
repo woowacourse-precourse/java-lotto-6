8d0f40dff0454e088b01ffced227a9cad4e5234f
qncj5224 <qncj5224@naver.com>
2023-11-07 15:30:59 +0900

    refactor : 메시지 상수로 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,10 +6,10 @@ import lotto.model.Ranking;
 
 public class OutputView {
   private final static String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-  private final static String INPUT_WINNINGNUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-  private final static String INPUT_BOUNSNUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+  private final static String INPUT_WINNINGNUMBER_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+  private final static String INPUT_BOUNSNUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
   private final static String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
-  private static InputProcessor inputProcessor = new InputProcessor();
+  private final static String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---";
 
   public static void printInputMoneyMessage() {
     System.out.println(INPUT_MONEY_MESSAGE);
@@ -50,8 +50,7 @@ public class OutputView {
   }
 
   public static void printSuccessResult() {
-    System.out.println("당첨 통계");
-    System.out.println("---");
+    System.out.print(WINNING_STATISTICS_MESSAGE);
   }
 
   public static void printRanking(List<Integer> rank) {

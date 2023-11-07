e064608341fd8ea2488b8ec9e016d889f29a49dd
황중섭 <jshwang9709@gmail.com>
2023-11-07 20:56:36 +0900

    comment(controller): handler 부분 주석 추가

--- src/main/java/lotto/controller/handler/WinningHandler.java
+++ src/main/java/lotto/controller/handler/WinningHandler.java
@@ -16,6 +16,11 @@ public class WinningHandler {
     private Lotto main;
     private Bonus bonus;
 
+    /**
+     * 당첨 번호 6자리를 입력받았을 때 예외가 발생하진 않는지 검사한다.
+     * <p>만약 예외가 발생하면 해당 예외와 관련된 문구를 출력한 후 "당첨 번호를 입력해 주세요." 안내메시지부터 다시 출력한다.
+     * @param message 안내 메시지
+     */
     public void processWinningOngoing(String message) {
         String input;
         do {
@@ -24,6 +29,12 @@ public class WinningHandler {
         } while (hasExceptionWinningOngoing(input));
     }
 
+    /**
+     * 보너스 번호 1자리를 입력받고 이를 토대로 당첨 번호+보너스 번호를 생성하는 일련의 과정을 진행하는 동안 예외가 발생하진 않는지 검사한다.
+     * <p>만약 예외가 발생하면 해당 예외와 관련된 문구를 출력한 후 "보너스 번호를 입력해 주세요." 안내메시지부터 다시 출력한다.
+     * @param message 안내 메시지
+     * @param service 서비스 로직 객체
+     */
     public void processWinningComplete(String message, LottoService service) {
         String input;
         do {

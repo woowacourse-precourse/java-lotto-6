9ee4eb874e37a699f489c51fa2ebe589d0ed56c3
jongheonleee <qwefghnm1212@gmail.com>
2023-11-07 19:58:15 +0900

    [test] 테스트 수정

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("입력 처리")
@@ -41,6 +43,7 @@ class InputViewTest extends IOTest{
                     inputView.requestCash();
                 }
         );
+
     }
 
     @DisplayName("현금 입력시 공백을 입력한 경우 예외 발생")
@@ -54,53 +57,6 @@ class InputViewTest extends IOTest{
                 }
         );
     }
-
-    @DisplayName("당첨 번호 생성 성공 테스트")
-    @ParameterizedTest()
-    @ValueSource(strings = {"1,2,3,4,5,6", "11,2,17,41,5,16", "1,2,33,4,42,6"})
-    void createWinnerNumbersByInputSuccessTest(String inputWinnerNumbers) {
-        assertDoesNotThrow(() -> {
-            systemIn(inputWinnerNumbers);
-            inputView.inputWinnerNumbers();
-        });
-    }
-
-    @DisplayName("당첨 번호 입력시 입력 형식을 준수하지 않은 경우 예외 발생")
-    @ParameterizedTest()
-    @ValueSource(strings = {"1, 2, 3, 4, 5, 6", "1,,2,3,4,5,6", "11, 2,17,41,5,16", "1,2,ㅁ,4,42,6"})
-    void createWinnerNumbersByWrongInputExceptionTest(String inputWrongWinnerNumbers) {
-        assertThrows(IllegalArgumentException.class,
-                () -> {
-                    systemIn(inputWrongWinnerNumbers);
-                    inputView.inputWinnerNumbers();
-                }
-        );
-    }
-
-    @DisplayName("당첨 번호 입력시 공백을 입력한 경우 예외 발생")
-    @ParameterizedTest()
-    @ValueSource(strings = {" "})
-    void createWinnerNumbersByBlankInputExceptionTest(String inputWrongWinnerNumbers) {
-        assertThrows(IllegalStateException.class,
-                () -> {
-                    systemIn(inputWrongWinnerNumbers);
-                    inputView.inputWinnerNumbers();
-                }
-        );
-    }
-
-    @DisplayName("당첨 번호 입력시 1~45 이외의 값 입력시 예외 발생")
-    @ParameterizedTest()
-    @ValueSource(strings = {"1,2,3,4,5,100", "0,2,17,41,5,16", "1,2,33,4,42,49"})
-    void createWinnerNumbersByNumberOverRangeInputExceptionTest(String inputWrongWinnerNumbers) {
-        assertThrows(IllegalArgumentException.class,
-                () -> {
-                    systemIn(inputWrongWinnerNumbers);
-                    inputView.inputWinnerNumbers();
-                }
-        );
-    }
-
 
 
 

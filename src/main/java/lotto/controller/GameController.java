7822982eee78dacd0d77f25bc042c4038bac8038
ohjuyoungg <ejrm12@naver.com>
2023-11-08 02:38:22 +0900

    refactor: static 변수 삭제 후 코드 리팩토링

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -10,44 +10,30 @@ import lotto.view.OutputView;
 
 public class GameController {
 
-    private static int purchaseAmount;
-    private static int bonusNumber;
-    private static Lotto winningNumbers;
-    private static List<List<Integer>> allLottoNumbers;
-
-    public void play() {
+    public static int play() {
         OutputView.printGameStartMessage();
-        purchaseAmount = InputView.inputPurchaseAmount();
-        lottoPurchaseNum(purchaseAmount);
+        int purchaseAmount = InputView.inputPurchaseAmount();
+        return lottoPurchaseNum(purchaseAmount);
     }
 
-    private void lottoPurchaseNum(int lottoNum) {
-        allLottoNumbers = LottoService.generateLottoNumbers(lottoNum);
-        OutputView.printLottoPurchaseAmount(lottoNum);
-        lottoPrintNum(allLottoNumbers);
-    }
-
-
-    public static void lottoPrintNum(List<List<Integer>> lottoAllNumbers) {
-        OutputView.printLottoNumber(lottoAllNumbers);
+    private static int lottoPurchaseNum(int purchaseAmount) {
+        List<Lotto> lottoNum = LottoService.generateLottoNumbers(purchaseAmount);
+        OutputView.printLottoPurchaseAmount(purchaseAmount);
+        OutputView.printLottoNumber(lottoNum);
         lottoInputWinningNum();
+        return purchaseAmount;
     }
 
     public static void lottoInputWinningNum() {
-        winningNumbers = InputView.inputWinningNum();
+        Lotto winningNumbers = InputView.inputWinningNum();
         lottoPrintBonusNumber(winningNumbers);
     }
 
-    public static void lottoPrintBonusNumber(Lotto winningNumbers) {
+    public static int lottoPrintBonusNumber(Lotto winningNumbers) {
         OutputView.printLottoBonusNumber();
-        bonusNumber = InputView.inputBonusNum(winningNumbers);
-        List<Integer> lottoWinningCounts = lottoWinCalculator();
-        OutputView.printLottoWinningStatistics(lottoWinningCounts);
-        OutputView.printLottoTotalProfit(lottoWinningCounts, purchaseAmount);
-    }
-
-    public static List<Integer> lottoWinCalculator() {
-        return LottoWinningCalculator.lottoCalculator(winningNumbers, allLottoNumbers);
+        int bonusNumber = InputView.inputBonusNum(winningNumbers);
+        OutputView.printLottoWinningStatistics();
+        return bonusNumber;
     }
 }
 

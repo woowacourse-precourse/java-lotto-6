3480aa72f6bcca3beb5b44572e4723631d4a5ad6
Leespect <wewqwew153@gmail.com>
2023-11-07 14:28:03 +0900

    Refacotr : LottoListMaker에서 ArrayList가 아닌 List로 형 변환

--- src/main/java/lotto/LottoListMaker.java
+++ src/main/java/lotto/LottoListMaker.java
@@ -1,11 +1,12 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LottoListMaker {
 
-    public static ArrayList<Lotto> makeLottoList(int lottoCount) {
-        ArrayList<Lotto> lottoList = new ArrayList<>();
+    public static List<Lotto> makeLottoList(int lottoCount) {
+        List<Lotto> lottoList = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             Lotto lotto = LottoMaker.makeLotto();
             lottoList.add(lotto);

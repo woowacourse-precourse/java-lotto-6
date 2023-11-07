7658db00e7926a6df35961990408439963a96803
sw801733 <sw801733@naver.com>
2023-11-07 22:03:27 +0900

    feat : LottoResult 클래스 보너스 번호 확인 기능 추가
    - 보너스 번호 일치 여부 확인 메서드 bonusNumberCheck 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -5,25 +5,32 @@ import java.util.List;
 import java.util.Objects;
 
 public class LottoResult {
-    private final int LottoPurchaseCnt;
+    private final int lottoPurchaseCnt;
     private final List<Integer> numbers;
-    private final List<List<Integer>> RandomNumbers;
+    private final List<List<Integer>> randomNumbers;
     private final List<Integer> LottoResult;
 
-    public LottoResult(List<Integer> numbers, List<List<Integer>> RandomNumbers, int LottoPurchaseCnt) {
-        this.LottoPurchaseCnt =LottoPurchaseCnt;
+    private final List<Boolean> bonusCheck;
+
+    private final int bounsNumber;
+
+    public LottoResult(List<Integer> numbers, List<List<Integer>> randomNumbers, int lottoPurchaseCnt, int bonusNumber) {
+        this.lottoPurchaseCnt = lottoPurchaseCnt;
         this.numbers = numbers;
-        this.RandomNumbers = RandomNumbers;
+        this.randomNumbers = randomNumbers;
+        this.bounsNumber = bonusNumber;
+
         this.LottoResult = cmpLotto();
+        this.bonusCheck = bounusNumberCheck();
     }
 
     private List<Integer> cmpLotto() {
         List<Integer> LottoResult = new ArrayList<>();
 
-        for(int i = 0; i < LottoPurchaseCnt; i++) {
+        for(int i = 0; i < lottoPurchaseCnt; i++) {
             int result = 0;
             for (int j = 0; j < 6; j++) {
-                if (Objects.equals(numbers.get(j), RandomNumbers.get(i).get(j))) {
+                if (Objects.equals(numbers.get(j), randomNumbers.get(i).get(j))) {
                     result++;
                 }
             }
@@ -31,6 +38,22 @@ public class LottoResult {
         }
 
         return LottoResult;
+    }
+
+    private List<Boolean> bounusNumberCheck() {
+        List<Boolean> bonusCheck = new ArrayList<>();
+
+        for(int i = 0; i < lottoPurchaseCnt; i++) {
+            boolean result = false;
+            for (int j = 0; j < 6; j++) {
+                if (Objects.equals(bounsNumber, randomNumbers.get(i).get(j))) {
+                    result = true;
+                }
+            }
+            bonusCheck.add(result);
+        }
+
+        return bonusCheck;
     }
 
 }

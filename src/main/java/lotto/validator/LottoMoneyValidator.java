b1b4e3df78e9aeefb99e8a9c28f590ae055d2873
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-07 20:49:29 +0900

    refactor: 에러메세지가 화면에 출력되도록 구현

--- src/main/java/lotto/validator/LottoMoneyValidator.java
+++ src/main/java/lotto/validator/LottoMoneyValidator.java
@@ -11,18 +11,21 @@ public class LottoMoneyValidator {
 
     private static void isNumeric(String lottoMoneyInput) {
         if (!lottoMoneyInput.matches("^[0-9]*$")) {
-            throw new IllegalArgumentException(ErrorMessages.NOT_NUMBER.getMessage());
+            System.out.println(ErrorMessages.NOT_NUMBER.getMessage());
+            throw new NumberFormatException(ErrorMessages.NOT_NUMBER.getMessage());
         }
     }
 
     private static void isOverThousands(String lottoMoneyInput) {
         if (Integer.parseInt(lottoMoneyInput) < 1000) {
+            System.out.println(ErrorMessages.NOT_ENOUGH_MONEY.getMessage());
             throw new IllegalArgumentException(ErrorMessages.NOT_ENOUGH_MONEY.getMessage());
         }
     }
 
     private static void isThousands(String lottoMoneyInput) {
         if (Integer.parseInt(lottoMoneyInput) % 1000 != 0) {
+            System.out.println(ErrorMessages.NOT_THOUSAND.getMessage());
             throw new IllegalArgumentException(ErrorMessages.NOT_THOUSAND.getMessage());
         }
     }

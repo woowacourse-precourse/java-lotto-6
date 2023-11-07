b1fc720490d9091340b4c7b187868423507945d1
cloudchamb3r <jizon0123@protonmail.com>
2023-11-07 12:26:09 +0900

    chore. remove comments

--- src/main/java/lotto/game/Game.java
+++ src/main/java/lotto/game/Game.java
@@ -8,27 +8,14 @@ public interface Game {
     default void awake() {
     }
 
-    /**
-     * executed by {@link GameManager} before each game loop
-     *
-     * @return : true if start gameLoop()
-     */
     default BeforeLoopResult beforeLoop() {
         return BeforeLoopResult.GAME_READY;
     }
 
-    /**
-     * @return : true if continue loop
-     */
     default GameLoopResult gameLoop() {
         return GameLoopResult.END_GAME;
     }
 
-    /**
-     * executed by {@link GameManager} after each game loop
-     *
-     * @return : true if restart
-     */
     default AfterLoopResult afterLoop() {
         return AfterLoopResult.END_GAME;
     }

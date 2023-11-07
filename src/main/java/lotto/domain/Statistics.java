0b7e97dfd1b526d658a2f26d7d34ae0fc07de72a
dj <fjzl12@naver.com>
2023-11-07 21:06:31 +0900

    feat(Statistics): 로또 당첨 결과 분석 스켈레톤 코드 작성

---
+++ src/main/java/lotto/domain/Statistics.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Statistics {
+    // 각 일치 개수에 대하여 list
+    private List<Integer> prizes;
+    public void calculate() {
+
+    }
+
+}

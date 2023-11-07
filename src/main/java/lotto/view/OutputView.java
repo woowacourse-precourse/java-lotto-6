122dbf45d61755e55e154b7928dca9ff00681480
이재현 <fanta4715@naver.com>
2023-11-08 00:52:43 +0900

    feat: OutputView 수정
    EnumMap으로 구현사항이 변경되어서 적용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,35 +1,40 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
 import lotto.model.Lotto;
-import lotto.model.Statistic;
+import lotto.model.Rank;
 
 public class OutputView {
-    private static final String PURCHASED_LOTTIES_MENT = "%d개를 구매했습니다.";
-    private static final String WINNING_STATISTICS_MENT = "%d개 일치 (%d원) - %d개";
+    private static final String PURCHASED_LOTTIES_MENT = "%d개를 구매했습니다.\n";
+    private static final String WINNING_STATISTICS_MENT = "%s (%s원) - %d개\n";
+    private static final String PROFITABILITY_MENT = "총 수익률은 %.1f%%입니다.\n";
+
     public void printPurchasedLotties(List<Lotto> lotties) {
         System.out.printf(PURCHASED_LOTTIES_MENT, lotties.size());
         for (int i = 0; i < lotties.size(); i++) {
-            System.out.println(lotties.get(i));
+            System.out.println(lotties.get(i).toString());
         }
     }
 
-    public void printWinningStatistics(List<Statistic> statistics) {
+    public void printWinningStatistics(EnumMap<Rank, Integer> enumMap) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        int statisticLength = statistics.size();
-        for (int i = 0; i < statisticLength; i++) {
-            Statistic statistic = statistics.get(i);
-            int correctNumberCount = statistic.getCorrectCount();
-            int winning = statistic.getWinning();
-            int matchLottoCount = statistic.getMatchLottoCount();
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        for (Rank rank : Rank.values()) {
+            String correctNumberCount = rank.getDescription();
+            int winning = rank.getPrize();
+            int matchLottoCount = enumMap.getOrDefault(rank, 0);
+            String winningWithComma = decimalFormat.format(winning);
             System.out.printf(WINNING_STATISTICS_MENT, correctNumberCount
-            ,winning, matchLottoCount);
+                    , winningWithComma, matchLottoCount);
         }
 
     }
 
     public void printProfitability(double profitabliity) {
-        System.out.println(String.format(".1f",profitabliity));
+        System.out.println(String.format(PROFITABILITY_MENT, profitabliity));
     }
 }

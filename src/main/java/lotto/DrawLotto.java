22772cbad11387faea854063ee8064861d014c73
Leegeonwoo7 <workgeon07@naver.com>
2023-11-08 00:11:48 +0900

    보너스 번호 입력구현

--- src/main/java/lotto/DrawLotto.java
+++ src/main/java/lotto/DrawLotto.java
@@ -8,7 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class DrawLotto {
 
-	public List<Integer> setNumbers() {
+	public List<Integer> getNumbers() {
 		List<Integer> winnigNumbers = new ArrayList<>();
 		System.out.println("당첨 번호를 입력해 주세요.");
     	StringTokenizer st = new StringTokenizer(Console.readLine(), ",");
@@ -17,4 +17,12 @@ public class DrawLotto {
     	}
     	return winnigNumbers;
     }
+	
+	public List<Integer> getBonusNumber(){
+		System.out.println("보너스 번호를 입력해 주세요.");
+		List<Integer> bonusNumber = new ArrayList<>();
+		int bonusNum = Integer.parseInt(Console.readLine());
+		bonusNumber.add(bonusNum);
+		return bonusNumber;
+	}
 }

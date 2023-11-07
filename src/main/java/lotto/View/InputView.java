32246e327acf31ebc756acb363c2b9ddaafce25e
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 20:56:05 +0900

    refactor: 입력메시지 상수화

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -3,21 +3,24 @@ package lotto.View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String INPUT_PRICE_FOR_PURCHASING_LOTTO = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "구입금액을 입력해 주세요.";
 
-    public static String inputPriceForLotto() {
-        System.out.println("구입금액을 입력해 주세요.");
+    public static String inputPriceForPurchasingLotto() {
+        System.out.println(INPUT_PRICE_FOR_PURCHASING_LOTTO);
         return Console.readLine();
     }
 
     public static String inputWinningLotto() {
-        System.out.println();
-        System.out.println("당첨 번호를 입력해 주세요.");
+        OutputView.newLine();
+        System.out.println(INPUT_WINNING_LOTTO);
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
-        System.out.println();
-        System.out.println("보너스 번호를 입력해 주세요.");
+        OutputView.newLine();
+        System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 }

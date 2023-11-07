09e3be943ad4edd81a5f67c8818ea5bf448e2718
Dawin <dawin002@naver.com>
2023-11-08 02:58:38 +0900

    feat(OutputView): 로또 결과 출력 기능 추가
    로또 결과 출력 기능 추가:
    - void printLottoResult(List<Integer> rankResult, double rateOfReturn)
    - 당첨 통계와 수익률을 양식에 맞게 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.model.Lotto;
+import lotto.model.enums.Rank;
 
 import java.util.List;
 
@@ -34,7 +35,7 @@ public class OutputView {
             stringBuilder.append("]");
             stringBuilder.append("\n");
         }
-        System.out.println(stringBuilder.toString());
+        System.out.println(stringBuilder);
     }
 
     private String makeLottoString(Lotto lotto) {
@@ -44,6 +45,19 @@ public class OutputView {
                 .map(Object::toString)
                 .toList();
         return String.join(", ", numbers);
+    }
+
+    public void printLottoResult(List<Integer> rankResult, double rateOfReturn) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("당첨 통계\n");
+        stringBuilder.append("---\n");
+        stringBuilder.append("3개 일치 (5,000원) - " + rankResult.get(Rank.RANK5.getRank() - 1) + "개\n");
+        stringBuilder.append("4개 일치 (50,000원) - " + rankResult.get(Rank.RANK4.getRank() - 1) + "개\n");
+        stringBuilder.append("5개 일치 (1,500,000원) - " + rankResult.get(Rank.RANK3.getRank() - 1) + "개\n");
+        stringBuilder.append("5개 일치, 보너스 볼 일치 (30,000,000원) - " + rankResult.get(Rank.RANK2.getRank() - 1) + "개\n");
+        stringBuilder.append("6개 일치 (2,000,000,000원) - " + rankResult.get(Rank.RANK1.getRank() - 1) + "개\n");
+        stringBuilder.append("총 수익률은 " + String.format("%.1f",rateOfReturn) + "%입니다.");
+        System.out.println(stringBuilder);
     }
 
     public void printBlankLine() {

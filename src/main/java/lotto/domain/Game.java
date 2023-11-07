5140ee49bc0f523f76cd63794eab7a12a9e12cff
bumstead-bumstead <dldyghks951@gmail.com>
2023-11-08 02:36:39 +0900

    refactor: Game.winningNumbers 이름을 변경하고, 생성자에서 직접 Lotto 객체로 입력받도록 수정

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -5,19 +5,20 @@ import java.util.List;
 //너무 넓은 의미를 갖고 있는 단어인 것 같다. 더 로또 계산과 관련된 로직을 포함하는 클래스라는 의미를 나타내는 이름이 어떤지?... LottoMachine??..
 public class Game {
     private List<Lotto> lottos;
-    private Lotto winningNumbers;
+    private Lotto winningLotto;
     private Integer bonusNumber;
 
-    public Game(List<Integer> winningNumbers,
+    public Game(Lotto winningLotto,
                 Integer bonusNumber,
                 List<Lotto> lottos) {
-        validate(winningNumbers, bonusNumber);
-        this.winningNumbers = new Lotto(winningNumbers);
+        validate(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
         this.lottos = lottos;
     }
 
-    private void validate(List<Integer> winningNumbers, Integer bonusNumber) {
+    private void validate(Lotto winningLotto, Integer bonusNumber) {
+        List<Integer> winningNumbers = winningLotto.getLottoNumbers();
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("당첨 번호가 보너스 번호를 포함하고 있습니다.");
         }
@@ -25,7 +26,7 @@ public class Game {
 
     public List<Rank> calculateRanks() {
         return lottos.stream()
-                .map(lotto -> lotto.calculateRank(winningNumbers.getLottoNumbers(), bonusNumber))
+                .map(lotto -> lotto.calculateRank(winningLotto.getLottoNumbers(), bonusNumber))
                 .toList();
     }
 }

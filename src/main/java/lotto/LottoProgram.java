abfbe742b8ad60361445156a7009ec148699f25e
맹순영 <symaeng98@naver.com>
2023-11-07 10:06:47 +0900

    fix: provider를 final 변수로 관리하도록 변경

--- src/main/java/lotto/LottoProgram.java
+++ src/main/java/lotto/LottoProgram.java
@@ -4,25 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.data.Lotto;
 import lotto.data.LottoResult;
 import lotto.data.WinningNumber;
-import lotto.error.Error;
 import lotto.provider.BonusProvider;
 import lotto.provider.LottoProvider;
 import lotto.provider.PrizeProvider;
 import lotto.provider.WinningNumberProvider;
+import lotto.utils.InputUtil;
 import lotto.utils.PrintUtil;
 
 import java.util.List;
 
 public class LottoProgram {
     private String purchaseAmountInput;
+    private final InputUtil inputUtil = new InputUtil();
+    private final LottoProvider lottoProvider = new LottoProvider();
+    private final WinningNumberProvider winningNumberProvider = new WinningNumberProvider();
+    private final BonusProvider bonusProvider = new BonusProvider();
+    private final PrizeProvider prizeProvider = new PrizeProvider();
 
     public List<Lotto> buyLottos() {
         PrintUtil.printInputPurchaseMessage();
-        String purchaseAmountInput = Console.readLine();
-        validateInteger(purchaseAmountInput);
+        String purchaseAmountInput = inputUtil.inputPurchaseAmount();
         this.purchaseAmountInput = purchaseAmountInput;
-
-        LottoProvider lottoProvider = new LottoProvider();
 
         List<Lotto> lottos = lottoProvider.buyLottos(purchaseAmountInput);
 
@@ -36,7 +38,6 @@ public class LottoProgram {
         PrintUtil.printInputWinningNumberMessage();
 
         String winningNumberInput = Console.readLine();
-        WinningNumberProvider winningNumberProvider = new WinningNumberProvider();
 
         return winningNumberProvider.getWinningNumber(winningNumberInput);
     }
@@ -45,14 +46,11 @@ public class LottoProgram {
         PrintUtil.printInputBonusNumberMessage();
 
         String bonusInput = Console.readLine();
-        BonusProvider bonusProvider = new BonusProvider();
 
         return bonusProvider.getBonus(winningNumbers, bonusInput);
     }
 
     public void showLottoResult(WinningNumber winningNumber, List<Lotto> lottos) {
-        PrizeProvider prizeProvider = new PrizeProvider();
-
         LottoResult lottoResult = prizeProvider.getLottoResult(lottos, winningNumber);
         List<Integer> winningResult = lottoResult.getWinningResult();
         int totalPrize = lottoResult.getTotalPrize();
@@ -60,12 +58,5 @@ public class LottoProgram {
         PrintUtil.printWinningResultMessage(winningResult);
 
         PrintUtil.printReturnRateMessage((totalPrize/Double.parseDouble(purchaseAmountInput))*100);
-    }
-
-    private void validateInteger(String input) {
-        // 숫자가 아닌 경우 예외 발생
-        if (!input.matches("\\d+")) {
-            throw new IllegalArgumentException(Error.LOTTO_PURCHASE_AMOUNT_ERROR.getMessage());
-        }
     }
 }

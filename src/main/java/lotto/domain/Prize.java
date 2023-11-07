86b1a09df29a8611fb4cd4a1dd271651c3321c46
Daseul <daseull.rep@gmail.com>
2023-11-07 12:07:18 +0900

    feat(Prize): 1등~5등 조건 상수 구현

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Prize {
+    FIFTH(5, 3, false),
+    FOURTH(4, 4, false),
+    THIRD(3, 5, false),
+    SECOND(2, 5, true),
+    FIRST(1, 6, false);
+
+    private final int rank;
+    private final int mainMatchCount;
+    private final boolean isMatchBonus;
+
+    Prize(int rank, int mainMatchCount, boolean isMatchBonus) {
+        this.rank = rank;
+        this.mainMatchCount = mainMatchCount;
+        this.isMatchBonus = isMatchBonus;
+    }
+
+    public static Prize findFrom(int mainMatchCount, boolean isMatchBonus) {
+        return Arrays.stream(Prize.values()).filter(prize -> prize.equals(mainMatchCount, isMatchBonus))
+                .findAny().orElseThrow();
+    }
+
+    private boolean equals(int mainMatchCount, boolean isMatchBonus) {
+        return this.mainMatchCount == mainMatchCount && this.isMatchBonus == isMatchBonus;
+    }
+
+    public int rank() {
+        return this.rank;
+    }
+
+    public int mainMatchCount() {
+        return this.mainMatchCount;
+    }
+
+    public boolean isMatchBonus() {
+        return this.isMatchBonus;
+    }
+
+}

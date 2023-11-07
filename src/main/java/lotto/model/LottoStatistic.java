243183ca13b3d3adaff321d0154f3197dd4fbae4
Ukj0ng <xb253q@gmail.com>
2023-11-07 18:37:22 +0900

    feat(LottoStatistic): 로또 당첨 통계 정보를 담당하는 클래스
    멤버 변수:
    - 등수와 해당 등수의 당첨 횟수를 담는 컬렉션
    
    구현한 기능:
    - 등수에 따라 당첨 횟수 증가

--- src/main/java/lotto/model/LottoStatistic.java
+++ src/main/java/lotto/model/LottoStatistic.java
@@ -1,2 +1,27 @@
-package lotto.model;public class LottoStatistic {
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoStatistic {
+    private final Map<LottoPrize, Integer> lottoStatistic;
+
+    public LottoStatistic() {
+        this.lottoStatistic = new HashMap<>();
+    }
+
+    public LottoStatistic(LottoStatistic lottoStatistic) {
+        this.lottoStatistic = lottoStatistic.getLottoStatistic();
+    }
+
+    public Map<LottoPrize, Integer> getLottoStatistic() {
+        return lottoStatistic;
+    }
+
+    public void addStatistic(LottoPrize prize) {
+        lottoStatistic.put(
+            prize,
+            lottoStatistic.getOrDefault(prize, 0) + 1
+        );
+    }
 }

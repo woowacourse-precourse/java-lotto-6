e26adbacd3c37ac43699b4ecf52fbe8060148695
YejiGong <yejigong00@gmail.com>
2023-11-08 01:49:36 +0900

    Feat: 통합뷰 구현

--- src/main/java/lotto/View/View.java
+++ src/main/java/lotto/View/View.java
@@ -1,2 +1,54 @@
-package lotto.View;public class View {
+package lotto.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import lotto.Global.LottoResult;
+import lotto.Global.PrintPhrase;
+
+public class View {
+    private InputView inputView;
+    private OutputView outputView;
+
+    public View(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public int getLottoBuyAmout() {
+        outputView.printPhrase(PrintPhrase.PURCHASE_INPUT.getPhrase());
+        return inputView.SingleNumberInput();
+    }
+
+    public void putLottoSellResult(List<String> lottos) {
+        outputView.printPhrase(LottoResult.PURCHASE_RESULT.getResultState(lottos.size()));
+        outputView.printMultiplePhrase(lottos);
+    }
+
+    public List<Integer> getWinningLottoNumber(int length) {
+        outputView.printPhrase(PrintPhrase.WINNER_NUMBER_INPUT.getPhrase());
+        return inputView.NumberListSplitInput(length);
+    }
+
+    public int getBonusLottoNumber() {
+        outputView.printPhrase(PrintPhrase.BONUS_NUMBER_INPUT.getPhrase());
+        return inputView.SingleNumberInput();
+    }
+
+    public void putLottoResult(HashMap<String, Integer> result) {
+        outputView.printPhrase(PrintPhrase.WINNING_INFORMATION_OUTPUT.getPhrase());
+        outputView.printMultiplePhrase(getLottoResultPhrase(result));
+    }
+
+    private List<String> getLottoResultPhrase(HashMap<String, Integer> lottoResult) {
+        List<String> result = new ArrayList<>();
+        List<LottoResult> resultType = List.of(LottoResult.FIFTH_RESULT, LottoResult.FOURTH_RESULT,
+                LottoResult.THIRD_RESULT, LottoResult.SECOND_RESULT, LottoResult.FIRST_RESULT,
+                LottoResult.RATE_OF_RETURN_RESULT);
+        for (LottoResult type : resultType) {
+            result.add(type.getResultState(lottoResult.get(type.getName())));
+        }
+        return result;
+    }
+
 }

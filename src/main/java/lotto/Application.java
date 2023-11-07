5b842115282c71ddc36f929cd609980e31475c62
PARK9898 <wjddjs21@naver.com>
2023-11-08 03:11:35 +0900

    "feat: 로또 Application 구현"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
     public static void main(String[] args) {
+        LottoController lottoController = new LottoController(inputView, outputView);
+        lottoController.play();
         // TODO: 프로그램 구현
     }
 }

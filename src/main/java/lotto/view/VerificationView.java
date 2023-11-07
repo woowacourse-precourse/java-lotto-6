aa03019a7ef644ea24aeed7dca2f28c253bfe469
Junobee25 <wnsdhqo@naver.com>
2023-11-07 16:39:13 +0900

    feat(VerificationView): verificationViewRangeLottoNumber 메서드 구현

--- src/main/java/lotto/view/VerificationView.java
+++ src/main/java/lotto/view/VerificationView.java
@@ -4,12 +4,17 @@ public class VerificationView {
 
     private final static String INPUT_MONEY_ERROR = "[ERROR] 구입 금액은 최소 1000원 이상, 1000원 단위의 금액입니다.";
     private final static String STRING_TO_LONG_ERROR = "[ERROR] 입력 값은 숫자로만 이루어져 있어야 합니다.";
+    private final static String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1 부터 45 까지의 숫자로 이루어져 있어야 합니다.";
 
-    public static void VerificationViewRangeInputMoney() {
+    public static void verificationViewRangeInputMoney() {
         System.out.println(INPUT_MONEY_ERROR);
     }
 
-    public static void VerificationViewStringToLong() {
+    public static void verificationViewStringToLong() {
         System.out.println(STRING_TO_LONG_ERROR);
+    }
+
+    public static void verificationViewRangeLottoNumber() {
+        System.out.println(LOTTO_NUMBER_RANGE_ERROR);
     }
 }

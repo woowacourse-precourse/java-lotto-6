f943356164a2ea40f25b95254a6f27d4eaf41c1f
박정현 <ros28081@naver.com>
2023-11-07 17:04:20 +0900

    feat(Lotto): 중복 번호 검증 예외 처리 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,12 +7,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        valideteDuplicatedNumbers(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 갯수는 6개여야 합니다.");
+        }
+    }
+
+    private void valideteDuplicatedNumbers(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 

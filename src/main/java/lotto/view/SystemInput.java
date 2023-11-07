c9dfdf41af06c33946010fc964a30838116c4081
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-07 22:29:11 +0900

    refactor(LottoMachine): use invariant variable

--- src/main/java/lotto/view/SystemInput.java
+++ src/main/java/lotto/view/SystemInput.java
@@ -4,8 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.validator.Validator;
 
 public class SystemInput {
+
+    private static final String PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_INPUT = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_INPUT = "보너스 번호를 입력해 주세요.";
+
+    private static final String BLANK_ERROR = "입력된 값이 없습니다. 다시 입력해주세요.";
+    private static final String DIVISION_ERROR = "금액은 1000으로 나눠 떨어져야 합니다.";
+    private static final String DO_IT_AGAIN = "입력 값이 잘 못 되었습니다. 다시 입력해주세요!";
+    private static final String LOTTO_NUMBER_INPUT_REQUIRED = "로또 번호는 숫자 형식입니다. 다시 입력해주세요!";
+    private static final String BONUS_NUMBER_INPUT_REQUIRED = "보너스 번호는 숫자 형식입니다. 다시 입력해주세요!";
+
+    private static final String COMMA_SEPERATOR = ",";
+    private static final String WRONG_SEPERATOR = ",,";
+
+    private static final String REGULAR_EXPRESSION = ".*\\d.*";
+
+    private static final Integer THOUSANDS = 1000;
+
+
     public static int readMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PURCHASE_AMOUNT_INPUT);
         String purchaseAmount = Console.readLine();
         try {
             validateMoney(purchaseAmount);
@@ -25,19 +44,19 @@ public class SystemInput {
 
     public static void validateNone(String purchase) {
         if (purchase.length() == 0) {
-            throw new IllegalArgumentException("입력된 값이 없습니다. 다시 입력해주세요.");
+            throw new IllegalArgumentException(BLANK_ERROR);
         }
     }
 
     public static void validateDivision(String purchase) {
         int money = Integer.parseInt(purchase);
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("금액은 1000으로 나눠 떨어져야 합니다.");
+        if (money % THOUSANDS != 0) {
+            throw new IllegalArgumentException(DIVISION_ERROR);
         }
     }
 
     public static String readPickLotto() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(LOTTO_INPUT);
         String pickLotto = Console.readLine();
         try {
         validatePickLotto(pickLotto);}
@@ -60,25 +79,25 @@ public class SystemInput {
         char lastChar = pickLotto.charAt(pickLotto.length()-1);
         char firstChar = pickLotto.charAt(0);
 
-        if(lastChar == ",".charAt(0)){
-            throw new IllegalArgumentException("입력 값이 잘 못 되었습니다. 다시 입력해주세요!");
+        if(lastChar == COMMA_SEPERATOR.charAt(0)){
+            throw new IllegalArgumentException(DO_IT_AGAIN);
         }
-        if (pickLotto.contains(",,") || firstChar == ",".charAt(0)){
-            throw new IllegalArgumentException("입력 값이 잘 못 되었습니다. 다시 입력해주세요!");
+        if (pickLotto.contains(WRONG_SEPERATOR) || firstChar == COMMA_SEPERATOR.charAt(0)){
+            throw new IllegalArgumentException(DO_IT_AGAIN);
         }
 
     }
 
     public static void validateString(String pickLotto) {
-        String PATTERN = ".*\\d.*";
+        String PATTERN = REGULAR_EXPRESSION;
         if (!pickLotto.matches(PATTERN)) {
-            throw new IllegalArgumentException("로또 번호는 숫자 형식입니다. 다시 입력해주세요!");
+            throw new IllegalArgumentException(LOTTO_NUMBER_INPUT_REQUIRED);
         }
 
     }
 
     public static int readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(BONUS_INPUT);
         String pickBonus = Console.readLine();
         try {
             validateBonus(pickBonus);
@@ -97,9 +116,9 @@ public class SystemInput {
     }
 
     public static void validateBonusNum(String pickBonus) {
-        String PATTERN = ".*\\d.*";
+        String PATTERN = REGULAR_EXPRESSION;
         if (!pickBonus.matches(PATTERN)) {
-            throw new IllegalArgumentException("보너스 번호는 숫자 형식입니다. 다시 입력해주세요!");
+            throw new IllegalArgumentException(BONUS_NUMBER_INPUT_REQUIRED);
         }
     }
 
--- src/main/java/lotto/view/SystemOutput.java
+++ src/main/java/lotto/view/SystemOutput.java
@@ -9,10 +9,17 @@ import lotto.domain.RankContainer;
 import main.java.lotto.domain.WinningLottos;
 public class SystemOutput {
 
+    private static final String AMOUNT_OF_PURCHASE = "%d개를 구매했습니다.";
+    private static final String NEWLINE = "%n";
+    private static final String WINNING_STATISTIC = "당첨 통계";
+    private static final String NEWLINE_TWO = "\n";
+    private static final String LINE_SEPERATOR = "---";
+    private static final String TOTAL_RETURN = "총 수익률은 %.1f%%입니다.";
+    private static final String CORRESPOND_BONUS = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String CORRESPOND = "%d개 일치 (%s원) - %d개";
+    private static final String COMMA_DECIMAL = "%,d";
     public static void printPurchaseAmount(int purchaseAmount) {
-        String OUTPUT_MESSAGE = "%d개를 구매했습니다.";
-
-        System.out.printf((OUTPUT_MESSAGE) + "%n", (purchaseAmount / 1000));
+        System.out.printf((AMOUNT_OF_PURCHASE) + NEWLINE, (purchaseAmount / 1000));
     }
 
     public static void printWiningLotto(WinningLottos winningLottos) {
@@ -23,21 +30,21 @@ public class SystemOutput {
     }
 
     public static void printRankResult(RankContainer rankContainer, double rate) {
-        System.out.println("당첨 통계" + "\n" + "---");
+        System.out.println(WINNING_STATISTIC + NEWLINE_TWO + LINE_SEPERATOR);
         for (Rank rank : Rank.values()) {
             if (!rank.equals(Rank.NONE)) {
                 System.out.println(getRankContainerResult(rank, rankContainer));
             }
         }
-        System.out.printf(("총 수익률은 %.1f%%입니다.") + "\n", rate);
+        System.out.printf((TOTAL_RETURN) + NEWLINE_TWO, rate);
     }
 
     public static String getRankContainerResult(Rank rank, RankContainer rankContainer) {
         if (rank == Rank.SECOND) {
-            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개", rank.getCount(), String.format("%,d", rank.getAward()), rankContainer.getRankContainer(rank));
+            return String.format(CORRESPOND_BONUS, rank.getCount(), String.format(COMMA_DECIMAL, rank.getAward()), rankContainer.getRankContainer(rank));
         }
 
-        return String.format("%d개 일치 (%s원) - %d개", rank.getCount(), String.format("%,d", rank.getAward()), rankContainer.getRankContainer(rank));
+        return String.format(CORRESPOND, rank.getCount(), String.format(COMMA_DECIMAL, rank.getAward()), rankContainer.getRankContainer(rank));
     }
 
 

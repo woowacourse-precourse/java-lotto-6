b05670eb8e19e2d686c32b404e27797770afecb6
seona-moon <fhflwhwl5@gmail.com>
2023-11-07 21:54:21 +0900

    feat : 예외 처리 로직 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -19,34 +19,18 @@ public class InputView {
     }
 
     public int readPurchaseAmount() {
-        try {
-            int purchaseAmount = Integer.parseInt(readLine().trim());
-            accountValidator(purchaseAmount);
-            return purchaseAmount;
-        } catch (NumberFormatException e) {
-            System.out.println(NUMBER_FORMAT_ERROR.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return 0;
+        int purchaseAmount = Integer.parseInt(readLine().trim());
+        accountValidator(purchaseAmount);
+        return purchaseAmount;
     }
 
     public List<Integer> readWinningNumbers() {
-
-        try {
-            String numbers = readLine().trim();
-            inputValidator.validateEmptyValue(numbers);
-            List<Integer> winningNumbers = Arrays.stream(numbers.split(NUMBER_DELIMITER))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-            numbersValidator(winningNumbers);
-            return winningNumbers;
-        } catch (NumberFormatException e) {
-            System.out.println(NUMBER_FORMAT_ERROR.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return List.of(0);
+        String numbers = readLine().trim();
+        inputValidator.validateEmptyValue(numbers);
+        List<Integer> winningNumbers = Arrays.stream(numbers.split(NUMBER_DELIMITER))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+        return winningNumbers;
     }
 
     public int readBonusNumber() {
@@ -56,18 +40,10 @@ public class InputView {
     }
 
     public void accountValidator(int purchaseAmount) throws IllegalArgumentException {
+        //해당 내용을 LottoPurchaseManager로 옮기는게 나으려나...? ㅠㅠ
         inputValidator.validateNegativeNum(purchaseAmount);
         inputValidator.validateLessAccount(purchaseAmount);
         inputValidator.validatePurchaseAmountUnit(purchaseAmount);
     }
 
-    /**
-     * 사용자에게 중복되지 않는 숫자 6개로 구성된 당첨 번호를 입력받는다. Exception 공백이 들어온 경우 예외처리한다. Exception ,를 제외한 문자가 들어온
-     * 경우 예외처리한다. Exception ,으로 입력이 시작하거나 끝나는 경우 예외처리한다. Exception ,,가 두번 연속으로 사용된 경우 예외처리한다.
-     * Exception 입력된 숫자 리스트에 중복이 포함된 경우 예외처리한다. Exception 입력된 숫자가 6개가 아니면 예외처리한다. Exception 입력된 숫자가
-     * 1부터 45 사이의 값이 아니면 예외처리한다
-     */
-    public void numbersValidator(List<Integer> numbers) {
-
-    }
 }

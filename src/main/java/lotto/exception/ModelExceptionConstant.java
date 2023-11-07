67f72a7af5e6ce00ca3b2fca07a46537ff9aa843
whereami0404 <whereami2048@gmail.com>
2023-11-07 19:34:39 +0900

    refactor: 입력 숫자 범위 예외 메세지 변경

--- src/main/java/lotto/exception/ModelExceptionConstant.java
+++ src/main/java/lotto/exception/ModelExceptionConstant.java
@@ -1,12 +1,9 @@
 package lotto.exception;
 
-import org.junit.jupiter.api.Test;
-
 public enum ModelExceptionConstant {
     OUT_OF_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45사이의 숫자이어야 합니다."),
-    NOT_DIVIDE_BY_LOTTO_PRICE("[ERROR] 로또 구입 금액이 로또 1장의 가격으로 나누어 떨어지지 않습니다."),
-    UNDER_LOTTO_START_NUMBER("[ERROR] 당첨 번호가 1보다 낮을 수 없습니다."),
-    UPPER_LOTTO_END_NUMBER("[ERROR] 당첨 번호가 45보다 높을 수 없습니다.");
+    NOT_DIVIDE_BY_LOTTO_PRICE("[ERROR] 로또 구입 금액이 로또 1장의 가격으로 나누어 떨어지지 않습니다.");
+
 
     private final String text;
     ModelExceptionConstant(String text) {
--- src/main/java/lotto/util/ModelValidator.java
+++ src/main/java/lotto/util/ModelValidator.java
@@ -5,11 +5,11 @@ import lotto.exception.ModelExceptionConstant;
 public class ModelValidator {
     public static void validateOutOfRange(int number, int startNumber, int endNumber) {
         if (number < startNumber) {
-            throw new IllegalArgumentException(ModelExceptionConstant.UNDER_LOTTO_START_NUMBER.getText());
+            throw new IllegalArgumentException(ModelExceptionConstant.OUT_OF_LOTTO_NUMBER_RANGE.getText());
         }
 
         if (number > endNumber) {
-            throw new IllegalArgumentException(ModelExceptionConstant.UPPER_LOTTO_END_NUMBER.getText());
+            throw new IllegalArgumentException(ModelExceptionConstant.OUT_OF_LOTTO_NUMBER_RANGE.getText());
         }
     }
 }

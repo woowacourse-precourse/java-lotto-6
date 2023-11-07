14d20f55d7c8100d120477283920061f32956a61
박승민 <tmddudals369@naver.com>
2023-11-08 04:15:07 +0900

    feat(view): inputView와 outputView의 기능 조합을 위한 view 객체 구현

---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import lotto.domain.Amount;
+
+public class View {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public View(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+}

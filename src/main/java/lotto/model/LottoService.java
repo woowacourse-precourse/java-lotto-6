789f666e3051e5f35da84384332efb82df4a4ae8
AJN <buzz781@gmail.com>
2023-11-07 11:41:44 +0900

    refactor : 구글자바컨벤션 가이드에 따른 코드 리팩토링

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -1,21 +1,29 @@
 package lotto.model;
-import java.util.*;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoService {
-	private static final int LOTTO_NUMBERS_COUNT = 6;
+    private static final int LOTTO_NUMBERS_COUNT = 6;
+    private static final int LOTTO_PRICE_UNIT = 1000;
+    private static final int LOTTO_NUMBER_MAX = 45;
+
 	public static List<Lotto> createObjectPerThousandUnits(int inputData){
-		int playerInputThousands = inputData/1000;
-		List<Lotto> lotto_list = new ArrayList<Lotto>();
+		int playerInputThousands = inputData/LOTTO_PRICE_UNIT;
+		List<Lotto> lottoList = new ArrayList<Lotto>();
 		for(int i=1;i<=playerInputThousands;i++) {
-			Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-			lotto_list.add(lotto);
+			Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, LOTTO_NUMBER_MAX, LOTTO_NUMBERS_COUNT));
+			lottoList.add(lotto);
 		}
-		return lotto_list;
+		return lottoList;
 	}
 	private static int getWinningCount(List<Integer> playerLottoNumber,List<Integer> winningNumbers) {
-		int winningCount = 0;
+		int winningCount =0;
 	    for (int number : playerLottoNumber) {
 	        if (winningNumbers.contains(number)) {
 	            winningCount++;
@@ -23,19 +31,15 @@ public class LottoService {
 	    }
 	    return winningCount;
 	}
-	private static boolean isBonusNumberIncluded(List<Integer> playerLottoNumber,int bonusNumber) {
-		boolean bonusMatch = false;
-		for(int playerNum : playerLottoNumber) {
-			if(playerNum == bonusNumber) return true;
-		}
-		return bonusMatch;
-	}
+    private static boolean isBonusNumberMatched(List<Integer> playerLottoNumbers, int bonusNumber) {
+        return playerLottoNumbers.contains(bonusNumber);
+    }
 	
 	public static long getTotalWinningAmount(List<Lotto> playerLottos,List<Integer> winningNumbers,int bonusNumber) {
 		long totalWinningAmount = 0;
 		for(Lotto playerLotto : playerLottos) {
 			int winningCount = getWinningCount(playerLotto.getNumbers(),winningNumbers);
-			boolean bonusMatch = isBonusNumberIncluded(playerLotto.getNumbers(), bonusNumber);
+			boolean bonusMatch = isBonusNumberMatched(playerLotto.getNumbers(), bonusNumber);
 			totalWinningAmount += LottoPrize.valueOf(winningCount, bonusMatch).getWinnings();
 		}
 		return totalWinningAmount;
@@ -44,14 +48,14 @@ public class LottoService {
 		Map<String,Integer> rankCount = new HashMap<>(); 
 		for(Lotto playerLotto : playerLottos) {
 			int winningCount = getWinningCount(playerLotto.getNumbers(), winningNumbers);
-			boolean bonusMatch = isBonusNumberIncluded(playerLotto.getNumbers(), bonusNumber);
+			boolean bonusMatch = isBonusNumberMatched(playerLotto.getNumbers(), bonusNumber);
 			String rank = LottoPrize.valueOf(winningCount,bonusMatch).name();
 			rankCount.put(rank, rankCount.getOrDefault(rank, 0) + 1);
 		}
 		return rankCount;
 	}
 	public static double calculateReturnOnInvestment(long totalWinningAmount,List<Lotto> playerLottos) {
-		double investment = playerLottos.size() * 1000.0;
+		double investment = playerLottos.size() * (double) LOTTO_PRICE_UNIT;
 		return (totalWinningAmount / investment) * 100;
 	}
 }

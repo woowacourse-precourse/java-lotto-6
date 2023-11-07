569b019f11ede331cab53b9f4ec0282184ecb993
ldw <leehan02@hanyang.ac.kr>
2023-11-08 07:58:15 +0900

    feat(lotto): 로또와 관련된 규칙들을 상수로 Enum 클래스에 정의

--- src/main/java/lotto/model/Rule.java
+++ src/main/java/lotto/model/Rule.java
@@ -7,7 +7,12 @@ public enum Rule {
     LOTTO_NUMBERS_DIGITS(6),
     BONUS_NUMBER_DIGITS(1),
     LOTTO_PRICE(1000),
-    MINIMUM_NUMBER_TO_WIN(3);
+    MINIMUM_NUMBER_TO_WIN(3),
+    FIFTH_RANK(3),
+    FOURTH_RANK(4),
+    THIRD_RANK(5),
+    SECOND_RANK(20),
+    FIRST_RANK(6);
 
     private final int value;
 

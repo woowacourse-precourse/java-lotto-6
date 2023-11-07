60669a15ae709f2d6a5b319b1a6204b0165979bd
watermelon3737 <pshrina1@naver.com>
2023-11-07 13:58:10 +0900

    test : 파라미터 자료구조 수정 및 WinnigNumberGenerator 기능 적용

--- src/test/java/lotto/WinningNumbersGeneratorTest.java
+++ src/test/java/lotto/WinningNumbersGeneratorTest.java
@@ -10,22 +10,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class WinningNumbersGeneratorTest {
 
-    @DisplayName("당첨 번호 \',\'으로 나누기")
-    @Test
-    void dividedByDelimiter() {
-        //given
-        WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
-
-        //when
-        String winningNumbersInput = "1,2,3,4,5,6";
-        String [] expectedWinningNumbers = {"1", "2", "3", "4", "5", "6"};
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
-
-        //then
-        assertThat(winningNumbersGenerator.getInputDividedByDelimiter()).isEqualTo(expectedWinningNumbers);
-        assertThat(winningNumbersGenerator.getInputDividedByDelimiter().length).
-                isEqualTo(expectedWinningNumbers.length);
-    }
 
     @DisplayName("당첨 번호의 갯수가 6미만일 경우 예외 처리")
     @Test
@@ -34,13 +18,14 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "1,2,3,4,5";
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
+        String[] winningNumbersInput = {"1", "2", "3", "4", "5"};
+        winningNumbersGenerator.setWinningNumberInput(winningNumbersInput);
 
         //then
-        assertThatThrownBy(winningNumbersGenerator::validateInputDividedByDelimiter)
+        assertThatThrownBy(winningNumbersGenerator::validateEachWinningNumberInput)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 6개의 번호가 필요합니다.");
+                .hasMessage("[ERROR] 6개의 번호가 필요합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("당첨 번호의 갯수가 6초과일 경우 예외 처리")
@@ -50,13 +35,14 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "1,2,3,4,5,6,7";
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
+        String[] winningNumbersInput = {"1", "2", "3", "4", "5", "6", "7"};
+        winningNumbersGenerator.setWinningNumberInput(winningNumbersInput);
 
         //then
-        assertThatThrownBy(winningNumbersGenerator::validateInputDividedByDelimiter)
+        assertThatThrownBy(winningNumbersGenerator::validateEachWinningNumberInput)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 6개의 번호가 필요합니다.");
+                .hasMessage("[ERROR] 6개의 번호가 필요합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("당첨 번호에 중복된 값이 존재할 경우 예외 처리")
@@ -66,13 +52,14 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "1,2,3,4,5,5";
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
+        String[] winningNumbersInput = {"1", "2", "3", "4", "5", "5"};
+        winningNumbersGenerator.setWinningNumberInput(winningNumbersInput);
 
         //then
-        assertThatThrownBy(winningNumbersGenerator::validateInputDividedByDelimiter)
+        assertThatThrownBy(winningNumbersGenerator::validateEachWinningNumberInput)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 중복된 번호가 있습니다.");
+                .hasMessage("[ERROR] 중복된 번호가 있습니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("당첨 번호가 숫자가 아닌 경우 예외 처리")
@@ -82,13 +69,14 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "1,a,2,3,4,5";
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
+        String[] winningNumbersInput = {"1", "2", "3", "4", "5", "a"};
+        winningNumbersGenerator.setWinningNumberInput(winningNumbersInput);
 
         //then
         assertThatThrownBy(winningNumbersGenerator::validateDividedInput)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 당첨 번호는 숫자여야 합니다.");
+                .hasMessage("[ERROR] 당첨 번호는 숫자여야 합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("당첨 번호가 범위를 초과한 경우 예외 처리")
@@ -98,13 +86,14 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "46";
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
+        String[] winningNumbersInput = {"1", "2", "3", "4", "5", "46"};
+        winningNumbersGenerator.setWinningNumberInput(winningNumbersInput);
 
         //then
         assertThatThrownBy(winningNumbersGenerator::validateDividedInput)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
+                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("당첨 번호가 범위에 미달일 경우 예외 처리")
@@ -114,13 +103,14 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "0";
-        winningNumbersGenerator.dividedInputByDelimiter(winningNumbersInput);
+        String[] winningNumbersInput = {"0", "1", "2", "3", "4", "5"};
+        winningNumbersGenerator.setWinningNumberInput(winningNumbersInput);
 
         //then
         assertThatThrownBy(winningNumbersGenerator::validateDividedInput)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
+                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("당첨 번호 생성 후 저장")
@@ -130,7 +120,7 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String numbers = "1,2,6,7,43,21";
+        String[] numbers = {"1", "2", "6", "7", "43", "21"};
         winningNumbersGenerator.generateWinningNumbers(numbers);
         List<Integer> expectedWinningNumbers = List.of(1, 2, 6, 7, 43, 21);
 
@@ -140,21 +130,6 @@ class WinningNumbersGeneratorTest {
         assertThat(winningNumbers).containsAnyElementsOf(expectedWinningNumbers);
     }
 
-    @DisplayName("보너스 번호가 숫자가 아닐 경우 예외 처리")
-    @Test
-    void bonusNumberNotDigit() {
-        //given
-        WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
-
-        //when
-        String bonusNumberInput = "1a";
-
-        //then
-        assertThatThrownBy(() -> winningNumbersGenerator.validateBonusNumberInput(bonusNumberInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 보너스 번호는 숫자여야 합니다.");
-    }
-
     @DisplayName("보너스 번호가 범위를 초과할 경우 예외 처리")
     @Test
     void bonusNumberOverRange() {
@@ -162,17 +137,17 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String numbers = "1,2,6,7,43,21";
+        String[] numbers = {"1", "2", "6", "7", "43", "21"};
         winningNumbersGenerator.generateWinningNumbers(numbers);
 
-        String bonusNumberInput = "46";
-        winningNumbersGenerator.validateBonusNumberInput(bonusNumberInput);
+        int bonusNumberInput = 46;
         winningNumbersGenerator.saveBonusNumber(bonusNumberInput);
 
         //then
         assertThatThrownBy(winningNumbersGenerator::validateBonusNumber)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
+                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("보너스 번호가 범위 미달일 경우 예외 처리")
@@ -182,17 +157,17 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String numbers = "1,2,6,7,43,21";
+        String[] numbers = {"1", "2", "6", "7", "43", "21"};
         winningNumbersGenerator.generateWinningNumbers(numbers);
 
-        String bonusNumberInput = "0";
-        winningNumbersGenerator.validateBonusNumberInput(bonusNumberInput);
+        int bonusNumberInput = 0;
+
         winningNumbersGenerator.saveBonusNumber(bonusNumberInput);
 
         //then
         assertThatThrownBy(winningNumbersGenerator::validateBonusNumber)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 번호는 1부터 45사이의 숫자여야 합니다.");
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("보너스 번호가 당첨 번호에 존재할 경우 예외 처리")
@@ -202,18 +177,18 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String winningNumbersInput = "1,2,6,7,43,21";
-        winningNumbersGenerator.generateWinningNumbers(winningNumbersInput);
+        String[] numbers = {"1", "2", "6", "7", "43", "21"};
+        winningNumbersGenerator.generateWinningNumbers(numbers);
 
-        String bonusNumberInput = "43";
+        int bonusNumberInput = 43;
 
-        winningNumbersGenerator.validateBonusNumberInput(bonusNumberInput);
         winningNumbersGenerator.saveBonusNumber(bonusNumberInput);
 
         //then
         assertThatThrownBy(winningNumbersGenerator::validateBonusNumber)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 당첨 번호 중 보너스 번호가 존재합니다.");
+                .hasMessage("[ERROR] 당첨 번호 중 보너스 번호가 존재합니다.")
+                .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("보너스 번호 생성 후 저장")
@@ -223,14 +198,13 @@ class WinningNumbersGeneratorTest {
         WinningNumbersGenerator winningNumbersGenerator = new WinningNumbersGenerator();
 
         //when
-        String numbers = "1,2,6,7,43,21";
+        String[] numbers = {"1", "2", "6", "7", "43", "21"};
         winningNumbersGenerator.generateWinningNumbers(numbers);
 
-        String bonusNumberInput = "45";
-        winningNumbersGenerator.generateBonusNumber(bonusNumberInput);
-        int expectedBonusNumber = Integer.parseInt(bonusNumberInput);
+        int bonusNumber = 45;
+        winningNumbersGenerator.generateBonusNumber(bonusNumber);
 
         //then
-        assertThat(winningNumbersGenerator.getBonusNumber()).isEqualTo(expectedBonusNumber);
+        assertThat(winningNumbersGenerator.getBonusNumber()).isEqualTo(bonusNumber);
     }
 }

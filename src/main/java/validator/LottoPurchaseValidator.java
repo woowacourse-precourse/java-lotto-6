cb91ee2c6510e7e0f2c29847f957edfc2f92e590
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 07:50:20 +0900

    refactor: LottoPurchaseValidator 상수 설정

--- src/main/java/validator/LottoPurchaseValidator.java
+++ src/main/java/validator/LottoPurchaseValidator.java
@@ -1,6 +1,10 @@
 package validator;
 
 public class LottoPurchaseValidator {
+    public static final String INVALID_MINIMUM_PURCHASE_AMOUNT = "[ERROR] 로또 최소 구입금액은 1000원 입니다.";
+    public static final String INVALID_PURCHASE_AMOUNT_DIVIDE_UP_1000 = "[ERROR] 로또 구입금액은 1000원 단위여야 합니다.";
+    public static final String INVALID_INTEGER_PURCHASE_AMOUNT = "[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.";
+
     public int validPurchaseAmount(String purchaseAmount) {
         vaildIntegerPurchaseAmount(purchaseAmount);
         int purchaseAmountPrice = Integer.parseInt(purchaseAmount);
@@ -11,19 +15,19 @@ public class LottoPurchaseValidator {
 
     void validMinimumPurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < 1000) {
-            throw new IllegalArgumentException("[ERROR] 로또 최소 구입금액은 1000원 입니다.");
+            throw new IllegalArgumentException(INVALID_MINIMUM_PURCHASE_AMOUNT);
         }
     }
 
     void validPurchaseDivideUp1000(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1000원 단위여야 합니다.");
+            throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_DIVIDE_UP_1000);
         }
     }
 
     void vaildIntegerPurchaseAmount(String input) {
         if (!input.matches("^[0-9]+$")) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.");
+            throw new IllegalArgumentException(INVALID_INTEGER_PURCHASE_AMOUNT);
         }
     }
 }

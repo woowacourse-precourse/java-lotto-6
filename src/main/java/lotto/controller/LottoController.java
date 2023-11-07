e08da9b5a5932635c632099b2ab57a877c116d88
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:56:51 +0900

    feat(LottoController): 구입 금액 입력 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,17 @@
 package lotto.controller;
 
+import lotto.domain.TicketAmount;
 import lotto.view.OutputView;
 
 public class LottoController {
+    private TicketAmount ticketAmount;
+    
     public void run() {
         setTicketAmountAndCount();
     }
 
     private void setTicketAmountAndCount() {
         OutputView.printMoneyInputMessage();
+        ticketAmount = InputController.setTicketAmount();
     }
 }

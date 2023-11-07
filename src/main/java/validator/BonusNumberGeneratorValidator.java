6764e4802ed56139c97623a9e255c6b384bbdb89
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 03:33:01 +0900

    feat: 보너스 번호가 숫자인지 확인하는 vaildBonusNumberNonNumeric 함수 생성

--- src/main/java/validator/BonusNumberGeneratorValidator.java
+++ src/main/java/validator/BonusNumberGeneratorValidator.java
@@ -6,4 +6,11 @@ public class BonusNumberGeneratorValidator {
             throw new IllegalArgumentException("[ERROR] 보너스 숫자에는 1 ~ 9까지의 숫자만 입력해주세요.");
         }
     }
+
+    void vaildBonusNumberNonNumeric(String input) {
+        String regex = "[+-]?\\d*(\\.\\d+)?";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.");
+        }
+    }
 }

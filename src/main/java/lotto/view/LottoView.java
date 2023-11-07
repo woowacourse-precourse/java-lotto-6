a0ac3c0546e5611d4e3518e198eb60c673cf3b75
KimSangHyeok <tkdgur0906@naver.com>
2023-11-07 23:05:16 +0900

    feat : 로또를 몇개 구입했는지 출력하는 기능 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,8 +2,14 @@ package lotto.view;
 
 public class LottoView {
     private static final String PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_COUNT = "개를 구매했습니다.";
 
-    public static void printPurchasePrompt(){
+    public static void printPurchasePrompt() {
         System.out.println(PURCHASE_PRICE);
     }
+
+    public static void printPurchaseCount(int purchaseCount) {
+        System.out.println(purchaseCount + PURCHASE_COUNT);
+    }
+
 }

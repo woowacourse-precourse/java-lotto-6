79f358f7fbc2533a95966a170169e38aabc4c820
82user <zxcvc9999@gmail.com>
2023-11-07 17:57:53 +0900

    refactor: change parameter and change double to bigdecimal

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,9 +1,14 @@
 package lotto.view;
 
 import lotto.domain.Buyer;
+import lotto.domain.Prize;
 import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class OutputViewTest {
 
@@ -17,17 +22,34 @@ class OutputViewTest {
     }
 
     @Test
-    @DisplayName("구입 금액 출력")
-    void printPurchasePrice() {
-        int purchasePrice = 14000;
-        outputView.printPurchasePrice(purchasePrice);
-    }
-
-    @Test
     @DisplayName("구매 완료 및 발행 내역 안내")
     void printIssuingLotteryTicket() {
         int purchasePrice = 5000;
         Buyer buyer = lottoService.issueLotteryTicketAll(purchasePrice);
         outputView.printIssuingLotteryTicket(buyer.getLottos(), lottoService.getLotteryTicketCount(purchasePrice));
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력 안내")
+    void printInputWinningNumbersSentence() {
+        outputView.printInputWinningNumbersSentence();
+    }
+
+    @Test
+    @DisplayName("보너스 번호 입력 안내")
+    void printBonusNumberSentence() {
+        outputView.printBonusNumberSentence();
+    }
+
+    @Test
+    @DisplayName("당첨 내역 안내")
+    void printResult() {
+        List<Prize> prizes = new ArrayList<>();
+        prizes.add(Prize.FIFTH_PLACE);
+        prizes.add(Prize.SECOND_PLACE);
+
+        List<Integer> prizeCount = Arrays.asList(0, 0, 1, 0, 0, 1);
+
+        outputView.printResult(prizes, prizeCount);
     }
 }

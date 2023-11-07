abe13b51fae9463c99cda110a05c5cf392140143
권장순 <jsoonworld@gmail.com>
2023-11-08 02:13:57 +0900

    test( LottoWinningNumberTest#createLottoWinningNumberSuccessfully() ) : 당첨 번호가 올바르게 생성되는지 테스트.

---
+++ src/test/java/lotto/v3/model/LottoWinningNumberTest.java
@@ -0,0 +1,25 @@
+package lotto.v3.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoWinningNumberTest {
+    @Test
+    @DisplayName("당첨 번호가 올바르게 생성되는지 테스트")
+    void createLottoWinningNumberSuccessfully() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        LottoWinningNumber lottoWinningNumber = new LottoWinningNumber(winningNumbers);
+
+        assertThat(lottoWinningNumber.getNumbers())
+                .hasSize(6)
+                .isEqualTo(new HashSet<>(winningNumbers));
+    }
+
+}

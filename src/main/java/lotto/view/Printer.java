66b0dc2444c6fd50da6ce884f52c2b3cddb6b452
grax <grax@192.168.35.119>
2023-11-07 18:19:49 +0900

    feat(lotto): create printLottoTickets() method

--- src/main/java/lotto/view/Printer.java
+++ src/main/java/lotto/view/Printer.java
@@ -1,9 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+
 public class Printer {
 
 	public void printDirection(String direction) {
 		System.out.print(direction);
+	}
+	
+	public void printLottoTickets(int ticketsQty, List<List<Integer>> tickets) {
+		System.out.println("\n" + ticketsQty + "개를 구매했습니다.");
+		for (List<Integer> numbers : tickets) {
+			System.out.println(numbers);
+		}
 	}
 	
 }

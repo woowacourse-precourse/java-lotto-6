ac1b6be2f5bf31ba326c69aaef2da4742fce7210
SungKyum Kim <sungkyum1@naver.com>
2023-11-07 21:18:02 +0900

    refactor(Customer): getter 를 사용하지 않고 바로 로또를 반환하도록 수정

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -5,18 +5,13 @@ import java.util.List;
 public class Customer {
     private final Cash cash;
     private final LottoSeller lottoSeller;
-    private List<Lotto> lottos;
 
     public Customer(Cash cash, LottoSeller lottoSeller) {
         this.cash = cash;
         this.lottoSeller = lottoSeller;
     }
 
-    public void purchaseLottos() {
-        this.lottos = lottoSeller.sellLottos(cash);
-    }
-
-    public List<Lotto> getLottos() {
-        return lottos;
+    public List<Lotto> purchaseLottos() {
+        return lottoSeller.sellLottos(cash);
     }
 }

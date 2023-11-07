c5bc381ad013ea5222877f5f720658bd6feec904
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 14:05:47 +0900

    refactor: 오류메시지 상수화

--- src/main/java/lotto/Model/WinningLotto.java
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -1,30 +1,35 @@
 package lotto.Model;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WinningLotto {
-    private Lotto winningLotto;
+    private static final String LOTTO_NUMBER_DELIMETER = ",";
+    public static final String ERROR_NOT_LOTTO_FORMAT =
+            String.format("[ERROR] 숫자와 쉼표(%s)만을 이용해서 입력해주세요. 예시) 1,2,3,4,5,6", LOTTO_NUMBER_DELIMETER);
+    public static final String ERROR_END_LOTTO_NUMBER_DELEMETER =
+            String.format("[ERROR] 마지막은 %s가 올 수 없습니다. 예시) 1,2,3,4,5,6", LOTTO_NUMBER_DELIMETER);
+    public static final String ERROR_INVALID_BONUS = "[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다.";
+
+    private final Lotto winningLotto;
 
     public WinningLotto(String input) {
         validate(input);
         try {
-            this.winningLotto = new Lotto(Stream.of(input.split(","))
+            this.winningLotto = new Lotto(Stream.of(input.split(LOTTO_NUMBER_DELIMETER))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList()));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 예시처럼 입력해주세요. 예시) 1,2,3,4,5,6");
+            throw new IllegalArgumentException(ERROR_NOT_LOTTO_FORMAT);
         }
-
     }
 
-    public void contain(Bonus bonus) {
+    public void contain(Bonus bonus) throws IllegalArgumentException {
         int bonusNumber = bonus.getBonusNumber();
         List<Integer> winningLottoNumbers = winningLotto.getNumbers();
         if (winningLottoNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호에 포함되면 안됩니다.");
+            throw new IllegalArgumentException(ERROR_INVALID_BONUS);
         }
     }
 
@@ -38,8 +43,8 @@ public class WinningLotto {
     }
 
     private void validate(String input) {
-        if (input.endsWith(",")) {
-            throw new IllegalArgumentException("[ERROR] 쉼표로 끝나는 당첨번호는 입력할 수 없습니다.");
+        if (input.endsWith(LOTTO_NUMBER_DELIMETER)) {
+            throw new IllegalArgumentException(ERROR_END_LOTTO_NUMBER_DELEMETER);
         }
     }
 }

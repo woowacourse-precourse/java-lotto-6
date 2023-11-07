54f3ede920cd8cbca51889316efea46f292203d9
gonixxxx <akdntm946@gmail.com>
2023-11-07 23:26:41 +0900

    refactor(Lotto): delete unused method

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -35,10 +35,6 @@ public class Lotto {
         InputValidator.checkBonusNumber(numbers, bonus);
     }
 
-    public void showLotto() {
-        System.out.println(numbers);
-    }
-
     public int countMatchingNumber(Lotto userLotto) {
         return (int) numbers.stream().filter(userLotto::isContain).count();
     }

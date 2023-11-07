fb80a11bba41ef0c146776fe54e53d11ab4be49c
yunju-m <dbswnfnal@naver.com>
2023-11-07 14:27:11 +0900

    Feat(LottoMoney): ✨ 로또 구매 금액이 1,000단위인지 유효성 검사 기능 구현
    로또 구매 금액이 숫자가 아닌 경우 IlligalArgumentException 예외 처리
    
    로또 구매 금액 조건
    1. 로또 구매 금액은 숫자여야 한다.
    2. 로또 구매 금액은 1,000단위여야 한다.

--- src/main/java/lotto/LottoMoney.java
+++ src/main/java/lotto/LottoMoney.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class LottoMoney {
     private static final String WRONG_TYPE_LOTTO_MONEY_MESSAGE = "[ERROR] 로또 구매금액의 타입은 숫자여야 합니다.";
+    private static final String WRONG_LOTTO_MONEY_MESSAGE = "[ERROR] 로또 구매금액은 1000단위 숫자여야 합니다.";
     private double lottoMoney;
     private int lottoTiket;
 
@@ -15,6 +16,8 @@ public class LottoMoney {
     private void validate(String inputMoney) {
         if (!inputMoney.matches("\\d+")) {
             throw new IllegalArgumentException(WRONG_TYPE_LOTTO_MONEY_MESSAGE);
+        } else if (Integer.valueOf(inputMoney) % 1000 != 0) {
+            throw new IllegalArgumentException(WRONG_LOTTO_MONEY_MESSAGE);
         }
     }
 }

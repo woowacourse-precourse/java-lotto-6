27d449315b2381e5c952bcad145b952d1ac087d3
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:59:28 +0900

    feat(Lotto) : 필요한 연산 기능 추가
    contain, compareWithAnotherLotto, printLottoInfo, formatIntegerListToString

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.function.Predicate;
+import lotto.Utils;
+import lotto.validation.LottoValidation;
+import lotto.view.OutputView;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,11 +14,28 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public boolean contains(int number) {
+        return numbers.contains(number);
     }
 
-    // TODO: 추가 기능 구현
+    public int compareWithAnotherLotto(Lotto another) {
+        return another.compareWithNumberList(this.numbers);
+    } // 당첨 갯수 확인 로또끼리 확인
+
+    public int compareWithNumberList(List<Integer> anotherNumbers) {
+        return (int) numbers.stream()
+                .filter(number -> anotherNumbers.stream()
+                .anyMatch(Predicate.isEqual(number)))
+                .count();
+    }
+
+    public void printLottoInfo() {
+        OutputView.printLotto(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        String Lotto = Utils.formatIntegerListToString(numbers);
+        new LottoValidation().check(Lotto);
+    }
+
 }

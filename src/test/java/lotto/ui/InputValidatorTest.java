234623e9916665f67e874942edbcfcd4c4584aaf
lzlkolo <lzlko1209@gmail.com>
2023-11-07 18:47:30 +0900

    refactor: move test package

--- src/test/java/lotto/ui/InputValidatorTest.java
+++ src/test/java/lotto/ui/InputValidatorTest.java
@@ -1,6 +1,5 @@
-package lotto;
+package lotto.ui;
 
-import lotto.ui.InputValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;

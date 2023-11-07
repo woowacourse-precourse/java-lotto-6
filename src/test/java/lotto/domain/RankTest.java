0cefd4f7ca40a9741df7515937e93cb89f74941a
anneu <b.imnuj@gmail.com>
2023-11-07 16:59:22 +0900

    test: 매칭 개수와 보너스 매칭 여부에 따른 당첨 순위를 구하는 기능 테스트 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,22 +1,29 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RankTest {
 
-    @Test
-    void find() {
-        Rank rank1 = Rank.find(5, false);
-        Rank rank2 = Rank.find(5, true);
-        Rank rank3 = Rank.find(6, false);
-        Rank rank4 = Rank.find(6, true);
-        Rank rank5 = Rank.find(0, true);
-        assertThat(rank1).isEqualTo(Rank.THREE);
-        assertThat(rank2).isEqualTo(Rank.TWO);
-        assertThat(rank3).isEqualTo(Rank.ONE);
-        assertThat(rank4).isEqualTo(Rank.ONE);
-        assertThat(rank5).isEqualTo(Rank.NONE);
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0, true, NONE",
+            "0, false, NONE",
+            "3, true, FIVE",
+            "3, false, FIVE",
+            "4, true, FOUR",
+            "4, false, FOUR",
+            "5, false, THREE",
+            "5, true, TWO",
+            "6, true, ONE",
+            "6, false, ONE",
+    })
+    @DisplayName("매칭 개수와 보너스 매칭 여부에 따른 당첨 순위를 구한다.")
+    void findRank(int matchCount, boolean hasBonus, Rank expected) {
+        Rank rank = Rank.find(matchCount, hasBonus);
+        assertThat(rank).isEqualTo(expected);
     }
 }

7b60106249d6a2f49b4b1afafce93332fee38ec4
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-08 00:49:15 +0900

    test(lottoValidator): Lotto를 테스트하는 것이 아닌 lottoValidator의 메서드를 검증하도록 수정

--- src/test/java/lotto/validator/LottoValidatorTest.java
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -2,17 +2,16 @@ package lotto.validator;
 
 import static lotto.exception.Exception.DUPLICATE_WINNING_NUMBER;
 import static lotto.exception.Exception.WINNING_NUMBERS_SIZE_SHOULD_BE_SIX;
+import static lotto.validator.LottoValidator.validate;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 
 import java.util.List;
-import lotto.domain.WinningLotto;
 import org.junit.jupiter.api.Test;
 
 class LottoValidatorTest {
     @Test
     void 당첨번호_리스트의_크기가_6이_아닌_경우_예외() {
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4), any(Integer.class)))
+        assertThatThrownBy(() -> validate(List.of(1, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(WINNING_NUMBERS_SIZE_SHOULD_BE_SIX.getMessage());
     }
@@ -21,7 +20,7 @@ class LottoValidatorTest {
     void 당첨번호_리스트에_중복된_숫자가_있는_경우_예외() {
         int sameNumber = 10;
 
-        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, sameNumber, sameNumber), any(Integer.class)))
+        assertThatThrownBy(() -> validate(List.of(1, 2, 3, 4, sameNumber, sameNumber)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(DUPLICATE_WINNING_NUMBER.getMessage());
     }

07f135df4ecef250b488dffaddad4f47d7ebcea4
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-07 12:46:11 +0900

    feat: 로또에 쓰이는 여러 메세지를 상수로 선언
    로또에 쓰이는 여러 메세지를 상수로 선언한다.

---
+++ src/main/java/lotto/MessageConstants.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public final class MessageConstants {
+    public static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBERS_GUIDE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_GUIDE = "보너스 번호를 입력해 주세요.";
+
+    public static final String WINNING_GUIDE = "당첨 통계\n---";
+    public static final String THREE_MATCH = "3개 일치 (5,000원) - ";
+    public static final String FOUR_MATCH = "4개 일치 (50,000원) - ";
+    public static final String FIVE_MATCH = "5개 일치 (1,500,000원) - ";
+    public static final String FIVE_BONUS_MATCH = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String SIX_MATCH = "6개 일치 (2,000,000,000원) - ";
+}

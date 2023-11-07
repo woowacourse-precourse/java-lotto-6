f41b8bef3dd77d9ffa0ab2e4802e2dddc1bdd83a
Kiltae <loverpleasestayo0@gmail.com>
2023-11-07 17:25:45 +0900

    test: matching 3

--- src/test/java/lotto/model/RankingTest.java
+++ src/test/java/lotto/model/RankingTest.java
@@ -44,6 +44,15 @@ public class RankingTest {
         assertThat(prize.match(playerInputNumber)).isEqualTo(FOURTH);
     }
 
+    @DisplayName("3개의 숫자를 맞춰서 FIFTH가 나오는지 확인한다.")
+    @Test
+    void correctFifthPrize() {
+        Lotto playerInputNumber = new Lotto(List.of(1, 2, 3, 9, 10, 8));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Prize prize = new Prize(lotto, 7);
+        assertThat(prize.match(playerInputNumber)).isEqualTo(FIFTH);
+    }
+
 
 
 

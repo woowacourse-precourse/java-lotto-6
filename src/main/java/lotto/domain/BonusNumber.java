595d88dae55779b53eddd5da6f2a2498d472c97d
JaehongDev <println@kakao.com>
2023-11-07 15:35:18 +0900

    refactor: 보너스 번호가 1과 45사이의 숫자로 구성하도록 변경

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,5 +1,11 @@
 package lotto.domain;
 
+import lotto.domain.exception.DomainExceptionCode;
+
 public record BonusNumber(int value) {
+
+    public BonusNumber {
+        DomainExceptionCode.BONUS_NUMBER_MUST_BE_BETWEEN_SIZE.dynamicInvokeBy(() -> value < 1 || value > 45);
+    }
 
 }

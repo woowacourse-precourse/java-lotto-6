b041e73dc2660330eda309dac71ed5cc18b4b2d2
Jaewon <choijawon99@gmail.com>
2023-11-07 23:41:27 +0900

    feat(view): 당첨 번호 입력 문구 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class View {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
-    private static final String INPUT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_WIN_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBERS = "보너스 번호를 입력해 주세요.";
 
     private static final String OUTPUT_PURCHASED_LOTTO_NUMBERS = "개를 구매했습니다.";
@@ -15,8 +15,8 @@ public class View {
         System.out.println(INPUT_MONEY);
     }
 
-    public void request_InputLottoNumbers(){
-        System.out.println(INPUT_LOTTO_NUMBERS);
+    public void request_InputWinLottoNumbers(){
+        System.out.println(INPUT_WIN_LOTTO_NUMBERS);
     }
 
     public void request_InputBonusNumbers(){

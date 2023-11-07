6662cd07f76df9faeacc3e18b4a06ded8ac7c197
SungKyum Kim <sungkyum1@naver.com>
2023-11-08 03:44:56 +0900

    remove(ErrorMessage): 불필요한 메시지 삭제

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,8 +2,6 @@ package lotto.exception;
 
 public enum ErrorMessage {
     INVALID_AMOUNT("구입 금액은 1000원 단위만 입력 가능합니다."),
-    NON_INTEGER("입력값이 정수가 아닙니다."),
-    CONSECUTIVE_COMMAS("콤마가 연속으로 입력되었습니다."),
     LAST_COMMA("입력의 마지막이 콤마입니다."),
     INVALID_INTEGER_COUNT("정수는 6개만 입력 가능합니다."),
     NON_POSITIVE_INTEGER("양수의 정수만 입력 가능합니다."),

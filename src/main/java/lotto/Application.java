30288df43b4da536925271ba4869a9bd000eb38b
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2023-11-08 03:03:49 +0900

    moneyInput 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int numberOfLotto = 0;
+        int money = 0;
+
+        System.out.println("구입 금액을 입력해 주세요.");
+        money = moneyInput(money);
+        System.out.println();
+        System.out.println("");
     }
+
+    public static int moneyInput(int money){
+        try {
+            money = Integer.parseInt(Console.readLine());
+        }catch(IllegalArgumentException e){
+            System.out.println("[ERROR] 1000원 단위로 입력하여야 합니다.");
+        }
+        if((money%1000)!=0) throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하여야 합니다. ");
+        return money;
+    }
+
 }

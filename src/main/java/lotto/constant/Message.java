839873bb9fa52700a7685386c1965c4907d1b109
JGM <cnxw4570123@gmail.com>
2023-11-07 21:06:08 +0900

    fix : add ERROR_messages

--- src/main/java/lotto/constant/Message.java
+++ src/main/java/lotto/constant/Message.java
@@ -12,12 +12,14 @@ public final class Message {
     public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static final String ERROR_TEMPLATE = "[ERROR] %s%s\n";
-    public static final String ERROR_MSG_NOT_A_NUMBER = "(은)는 숫자여야 합니다.";
+    public static final String ERROR_NOT_A_NUMBER = "(은)는 숫자여야 합니다.";
 
-    public static final String ERROR_MSG_NOT_IN_THOUSANDS = "(은)는 1000원 단위여야 합니다.";
-    public static final String ERROR_MSG_OUT_OF_RANGE = "(은)는 1부터 45 사이의 수여야 합니다.";
-    public static final String ERROR_MSG_LENGTH_NOT_MATCH = "(은)는 6자리여야 합니다.";
-    public static final String ERROR_MSG_DUPLICATED_NUMBER = "(은)는 중복될 수 없습니다.";
+    public static final String ERROR_NOT_IN_THOUSANDS = "(은)는 1000원 단위여야 합니다.";
+    public static final String ERROR_MONEY_OUT_OF_RANGE = "(은)는 0이상의 숫자여야 합니다.";
+    public static final String ERROR_NUMBER_OUT_OF_RANGE = "(은)는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_NOT_SIX_WINNING_NUMBERS = "(은)는 6자리 숫자여야 합니다.";
+    public static final String ERROR_NOT_ONE_NUMBER = "(은)는 1자리 숫자여야 합니다.";
+    public static final String ERROR_DUPLICATED_NUMBER = "(은)는 중복될 수 없습니다.";
 
     public static final String RESULT_MESSAGE =
             """

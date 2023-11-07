c6ce9c67bf6261bfca574bbebc845efa5538011c
gusdn0108 <gusdn6671@naver.com>
2023-11-07 11:47:08 +0900

    refactor(Score) : 여러장 에 대한 로또 calculate

--- src/main/java/lotto/domain/Score.java
+++ src/main/java/lotto/domain/Score.java
@@ -5,10 +5,21 @@ import java.util.List;
 import java.util.Map;
 
 public class Score {
-    Map<Result, Integer> score = new HashMap<>();
+    private final Map<Result, Integer> score;
+    public Score(WinningNumber winningNumber , Lotties lotties) {
+        this.score = calculate(winningNumber,lotties);
+    }
 
-
-    private Result calculate(Lotto lotto, WinningNumber winningNumber) {
+    public static Map<Result,Integer> calculate(WinningNumber winningNumber, Lotties lotties) {
+        Map<Result,Integer> score = new HashMap<>();
+        for (Lotto lotto : lotties.getLotties()) {
+            Result result = calculate(winningNumber, lotto);
+            int resultCnt = score.getOrDefault(result, 0);
+            score.put(result, resultCnt + 1);
+        }
+        return score;
+    }
+    private static Result calculate(WinningNumber winningNumber, Lotto lotto ) {
         List<Integer> numbers = lotto.getNumbers();
         List<Integer> winningLottoNumbers = winningNumber.getLottoNumbers();
         int bonusNumber = winningNumber.getBonusNumber();

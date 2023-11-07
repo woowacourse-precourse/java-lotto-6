2544ca7158112b2de4b19cbe155952c5c590005b
seona-moon <fhflwhwl5@gmail.com>
2023-11-07 21:50:34 +0900

    feat: 발생할 수 있는 예외 메시지를 모은 enum 구현

--- src/main/java/lotto/enums/LottoRank.java
+++ src/main/java/lotto/enums/LottoRank.java
@@ -1,9 +1,20 @@
-package lotto.Enums;
+package lotto.enums;
 
 public enum LottoRank {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIVETH
+    FIRST_PRIZE(2000000000),
+    SECOND_PRIZE(30000000),
+    THIRD_PRIZE(1500000),
+    FOURTH_PRIZE(50000),
+    FIFTH_PRIZE(5000),
+    NO_PRIZE(0);
+
+    private final int prizeAmount;
+
+    LottoRank(int prizeAmount) {
+        this.prizeAmount = prizeAmount;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
 }

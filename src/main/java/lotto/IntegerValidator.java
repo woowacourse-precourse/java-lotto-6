67c3db2cf2ba161e66f6e2199516f935f48acee4
kchranger <kchranger@gmail.com>
2023-11-07 23:00:55 +0900

    fix(bug) : correct method of checking int max value
    - Before : inspect string number as dictionary order
    - After : inspect string number as character value

--- src/main/java/lotto/IntegerValidator.java
+++ src/main/java/lotto/IntegerValidator.java
@@ -2,15 +2,11 @@ package lotto;
 
 import static Constant.GuideMessagePiece.EMPTY_VALUE;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class IntegerValidator {
 
     private static final String ERROR_MSG_INPUT_ZERO_OR_POSITIVE_INTEGER = "[ERROR] 0 혹은 양의 정수만 입력해 주세요.";
     private static final String ERROR_MSG_INPUT_NOT_EMPTY_VALUE = "[ERROR] 하나 이상의 값을 입력해야 합니다.";
-    private static final String ERROR_MSG_INPUT_INTEGER_RANGE_OVER = "[ERROR] 2147483647원 이하의 금액을 입력해 주세요.";
+    private static final String ERROR_MSG_INPUT_UNDER_INTEGER_RANGE = "[ERROR] 2147483647 이하의 숫자를 입력해 주세요.";
 
     public static void checkEmptyValue(String value) throws IllegalArgumentException{
         if(EMPTY_VALUE.equals(value)){
@@ -28,17 +24,30 @@ public class IntegerValidator {
     }
 
     public static void checkIntegerMaxValue(String value) throws IllegalArgumentException{
-        String intMaxValue = EMPTY_VALUE+Integer.MAX_VALUE;
-        List<String> maxValues = new ArrayList<String>();
-        maxValues.add(value);
-        maxValues.add(intMaxValue);
-        Collections.sort(maxValues);
-        if(intMaxValue.equals(maxValues.get(0))){
-            if(intMaxValue.equals(maxValues.get(1))){
-                return;
-            }
-            throw new IllegalArgumentException(ERROR_MSG_INPUT_INTEGER_RANGE_OVER);
+        String maxValue = EMPTY_VALUE+Integer.MAX_VALUE;
+        if(isFirstBiggerThanSecond(value,maxValue)){
+            throw new IllegalArgumentException(ERROR_MSG_INPUT_UNDER_INTEGER_RANGE);
         }
+        return;
+    }
+
+    private static Boolean isFirstBiggerThanSecond(String value1, String value2){
+        if (value1.length() > value2.length()) {
+            return true;
+        }
+        if (value1.length() < value2.length()) {
+            return false;
+        }
+
+        for (Integer index = 0; index <= value1.length(); index++) {
+            if(value1.charAt(index) > value2.charAt(index)){
+                return true;
+            }
+            if(value1.charAt(index) < value2.charAt(index)){
+                return false;
+            }
+        }
+        return false;
     }
 
     public static void checkWinnigNumbersValue(String value)throws IllegalArgumentException{

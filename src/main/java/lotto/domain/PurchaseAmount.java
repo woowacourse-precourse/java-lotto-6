41ca6682da1f46e1f1d1a36c2898539d72ce7e3f
whereami2048 <whereami2048@gmail.com>
2023-11-07 20:23:37 +0900

    feat: 구입 가능 로또 개수 계산 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -4,16 +4,20 @@ import lotto.constant.LottoConstant;
 import lotto.exception.ModelExceptionConstant;
 
 public class PurchaseAmount {
-    private final int purchaseAmount;
+    private final int amount;
 
     public PurchaseAmount(int purchaseAmount) {
         validateIsDivideByLottoPrice(purchaseAmount);
-        this.purchaseAmount = purchaseAmount;
+        this.amount = purchaseAmount;
     }
 
     private void validateIsDivideByLottoPrice(int purchaseAmount) {
         if (purchaseAmount % LottoConstant.LOTTO_PRICE.value() != 0) {
             throw new IllegalArgumentException(ModelExceptionConstant.NOT_DIVIDE_BY_LOTTO_PRICE.getText());
         }
+    }
+
+    public int getLottoCount() {
+        return this.amount / LottoConstant.LOTTO_PRICE.value();
     }
 }

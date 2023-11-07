9770046ccfbdfa19967bd8085b0680b29515b1a3
KDT live <dnjsxo0616@gmail.com>
2023-11-08 03:13:25 +0900

    feat(InputWinningNumberView): 당첨 번호를 쉼표 기준으로 구분하고 파싱하여 정수 리스트로 반환 메서드 추가
    - 사용자로부터 입력받은 당첨 번호 문자열을 파싱하여 정수 리스트로 반환.
    - 입력된 문자열에서 쉼표(,)로 구분된 숫자들을 추출하여 리스트 만듬.

--- src/main/java/lotto/view/InputWinningNumberView.java
+++ src/main/java/lotto/view/InputWinningNumberView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputWinningNumberView {
@@ -18,5 +19,11 @@ public class InputWinningNumberView {
         }catch (NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 입력값이 유효한 숫자가 아닙니다.");
         }
+    }
+
+    private List<Integer> createWinningNumbers(String input){
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
     }
 }

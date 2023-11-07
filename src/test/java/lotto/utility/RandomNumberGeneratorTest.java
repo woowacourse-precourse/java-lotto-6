651c4374229c3c5754d1e27c9a05f08b10739de7
Sung Hyobin <hellmir610@gmail.com>
2023-11-08 08:06:18 +0900

    test: Modify RandomNumberGeneratorTest to validate ascending order of random numbers

--- src/test/java/lotto/utility/RandomNumberGeneratorTest.java
+++ src/test/java/lotto/utility/RandomNumberGeneratorTest.java
@@ -5,21 +5,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static lotto.configuration.RandomNumberConfig.COUNT;
+import static lotto.configuration.RandomNumberConfig.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomNumberGeneratorTest {
-    @DisplayName("설정된 개수와 범위만큼의 임의의 숫자를 생성한다.")
+    @DisplayName("설정된 개수와 범위만큼의 임의의 숫자를 오름차순으로 생성한다.")
     @Test
     void test() {
         // given, when
-        List<Integer> randomNumbers = RandomNumberGenerator.generateNotDuplicatedRandomNumbers();
+        List<Integer> randomNumbers = RandomNumberGenerator.generateNotDuplicatedRandomNumbersOrderByAsc();
 
         // then
         assertThat(randomNumbers).hasSize(COUNT);
-        
+
         for (int randomNumber : randomNumbers) {
             assertThat(randomNumber).isBetween(START_INCLUSIVE, END_INCLUSIVE);
+        }
+
+        for (int i = 0; i < randomNumbers.size() - 1; i++) {
+            assertThat(randomNumbers.get(i)).isLessThan(randomNumbers.get(i + 1));
         }
     }
 }

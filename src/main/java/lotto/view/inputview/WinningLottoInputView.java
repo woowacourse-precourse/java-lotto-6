b971462de8c120e963bc8c8d71c85e64ce25b0bd
howudong <tjdvy963@naver.com>
2023-11-07 21:49:22 +0900

    refactor : 해당 파라미터의 입력이 null 일때만 특정 함수를 수행 할 수 있도록 변경

--- src/main/java/lotto/view/inputview/WinningLottoInputView.java
+++ src/main/java/lotto/view/inputview/WinningLottoInputView.java
@@ -10,30 +10,25 @@ import lotto.view.ParameterConfig;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 public final class WinningLottoInputView implements InputView {
 
     private static final String DELIMITER = ",";
-    private final Map<String, Consumer<WinningLottoDTO>> methods = Map.ofEntries(
-            Map.entry(ParameterConfig.WINNING_LOTTO_NUMBERS, this::inputLottoNumbers),
-            Map.entry(ParameterConfig.BONUS_NUMBER, this::inputBonusNumber)
-    );
 
     @Override
     public void read(Map<String, ? super DTO.Input> parameter) {
-        parameter.forEach(this::runMethod);
-    }
-
-    private void runMethod(String key, Object value) {
-        WinningLottoDTO dto = (WinningLottoDTO) value;
-
-        if (methods.containsKey(key)) {
-            methods.get(key).accept(dto);
+        WinningLottoDTO input = (WinningLottoDTO) parameter.get(ParameterConfig.WINNING_LOTTO);
+        if (input.getLotto() == null) {
+            inputLottoNumbers(input);
+            return;
         }
+        input.setBonus(inputBonusNumber(input));
     }
 
     private void inputLottoNumbers(WinningLottoDTO dto) {
+        if (dto.getLotto() != null) {
+            return;
+        }
         String input = Console.readLine();
 
         for (String split : input.split(DELIMITER)) {
@@ -51,9 +46,12 @@ public final class WinningLottoInputView implements InputView {
         return new Lotto(numbers);
     }
 
-    private void inputBonusNumber(WinningLottoDTO dto) {
+    private Integer inputBonusNumber(WinningLottoDTO dto) {
+        if (dto.getBonus() != null) {
+            return dto.getBonus();
+        }
         String input = Console.readLine();
         InputValidator.validateIsNumber(input);
-        dto.setBonus(Integer.parseInt(input));
+        return Integer.parseInt(input);
     }
 }

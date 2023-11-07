fc41cad8c14c5d7d1bf791d4d74c9a5ea0f46c0d
swhyeon98 <smreo75@gmail.com>
2023-11-07 21:01:31 +0900

    feat: 당첨 번호와 구매 번호를 비교하여 당첨 등급을 계산하는 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,8 +2,12 @@ package lotto.service;
 
 import lotto.domain.Lotto;
 import lotto.domain.RandomLottoGenerator;
+import lotto.domain.Rank;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -22,5 +26,29 @@ public class LottoService {
         return IntStream.range(0, lottoCount)
                 .mapToObj(i -> new Lotto(lottoGenerator.generate()))
                 .collect(Collectors.toList());
+    }
+
+    public Map<Rank, Long> calculateResults(Lotto winningLotto, List<Lotto> purchasedLottos, int bonusNumber) {
+        Map<Rank, Long> results = new HashMap<>();
+        for (Lotto purchasedLotto : purchasedLottos) {
+            Rank rank = getRank(winningLotto, purchasedLotto, bonusNumber);
+            if (rank != Rank.NO_LUCK) {
+                results.put(rank, results.getOrDefault(rank, 0L) + 1);
+            }
+        }
+        return results;
+    }
+
+    private Rank getRank(Lotto winningLotto, Lotto purchasedLotto, int bonusNumber) {
+        boolean isBonusMatched = purchasedLotto.getNumbers().contains(bonusNumber);
+        int matchCount = countMatchingNumbers(winningLotto, purchasedLotto);
+        return Rank.checkWinningStatus(matchCount, isBonusMatched);
+    }
+
+    private int countMatchingNumbers(Lotto winningLotto, Lotto purchasedLotto) {
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+        return (int) purchasedLotto.getNumbers().stream()
+                .filter(winningNumbers::contains)
+                .count();
     }
 }

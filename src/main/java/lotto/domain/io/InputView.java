489bb3142c4f07f116194a88294fe6de5477d9e2
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-07 18:48:03 +0900

    feat : 보너스 번호 안내 뷰

--- src/main/java/lotto/domain/io/InputView.java
+++ src/main/java/lotto/domain/io/InputView.java
@@ -1,11 +1,15 @@
 package lotto.domain.io;
 
 public class InputView {
-    public static final void getCash() {
+    public static void getCash() {
         System.out.println(InputViewFormat.GUIDE_CASH.getState());
     }
 
     public static void getLottoAnswer() {
         System.out.println(InputViewFormat.GUIDE_LOTTO_ANSWER.getState());
+    }
+
+    public static void getBonusAnswer() {
+        System.out.println(InputViewFormat.GUIDE_BONUS_ANSWER.getState());
     }
 }

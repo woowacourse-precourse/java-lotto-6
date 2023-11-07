7e9a97ef9601ba378a9e3c1c9784d74fdf5470c4
Terry <t3rryahn.dev@gmail.com>
2023-11-08 00:56:36 +0900

    feat: 숫자 생성과 정렬 관련 기능 추가
    NumberGenerator 클래스 구현
    NumberGeneratorTest 테스트 클래스 구현
    테스트 내용
    1.NotNull
    2.6개의 숫자인가?
    3.중복이 없는가?
    4.1 ~ 45 범위인가?
    5.오름차순 정렬인가?
    
    > 테스트 통과

--- src/main/java/lotto/model/NumberGenerator.java
+++ src/main/java/lotto/model/NumberGenerator.java
@@ -2,16 +2,16 @@ package lotto.model;
 
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class NumberGenerator {
-    private List<Integer> lottoNumbers = new ArrayList<>();
 
     public List<Integer> getNumber() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+
         lottoNumbers = pickUniqueNumbersInRange(1, 45, 6);
         Collections.sort(lottoNumbers);
 

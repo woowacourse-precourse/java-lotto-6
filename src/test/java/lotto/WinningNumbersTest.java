03cb2addaf14af1a5cea4bba82155a6b7cd4cd4e
rlwnduu <rlwnduu@gmail.com>
2023-11-07 18:41:41 +0900

    test: 보너스 번호 중복 테스트 추가

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,24 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WinningNumbersTest {
 
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
     @Test
-    void draw() {
-        List<Lotto> ticket = new ArrayList<>(List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                new Lotto(List.of(2, 3, 4, 5, 6, 7)),
-                new Lotto(List.of(2, 3, 4, 5, 6, 8)),
-                new Lotto(List.of(3, 4, 5, 6, 7, 8)),
-                new Lotto(List.of(4, 5, 6, 7, 8, 9)),
-                new Lotto(List.of(5, 6, 7, 8, 9, 10)),
-                new Lotto(List.of(6, 7, 8, 9, 10, 11))));
-
-        WinningNumbers winningNumbers = new WinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
-
-        Map<Grade, Integer> winners = winningNumbers.draw(ticket);
-
-        for (Grade grade : winners.keySet()) {
-            Integer count = winners.get(grade);
-            System.out.println(grade + ": " + count);
-        }
+    void 보너스_번호_중복_예외() {
+        Assertions.assertThatThrownBy(() -> new WinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 6))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

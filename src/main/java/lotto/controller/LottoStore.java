9491a0fa6af5156449a68cea71de47123656a91d
Hanbee Lee <bee1162@naver.com>
2023-11-08 00:01:51 +0900

    feat: controller 클래스 작성

--- src/main/java/lotto/controller/LottoStore.java
+++ src/main/java/lotto/controller/LottoStore.java
@@ -1,5 +1,64 @@
-package lotto.domain;
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoStore {
-    
+    private static int price;
+    private static List<Lotto> lottos;
+    private static WinningNumber winningNumbers;
+    private static BonusNumber bonusNumber;
+
+    public void lottoGameStart() {
+        purchaseLotto();
+        lottoProcess();
+    }
+
+    private void lottoProcess() {
+        winningNumbers = setWinningNumbers();
+        bonusNumber = setBonusNumbers(winningNumbers);
+    }
+
+    private void purchaseLotto() {
+        LottoTotalPrice lottoTotalPrice1 = setPrice();
+        int numberOfPurchasedLotto = lottoTotalPrice1.getNumberOfPurchasedLotto();
+        price = lottoTotalPrice1.getPrice();
+
+        PurchasedLotto purchasedLotto1 = setPurchasedLottos(numberOfPurchasedLotto);
+        lottos = purchasedLotto1.getPurchasedLottos();
+
+        printPurchaseInformation(numberOfPurchasedLotto, lottos);
+    }
+
+    private PurchasedLotto setPurchasedLottos(int numberOfLottoPurchased) {
+        return new PurchasedLotto(numberOfLottoPurchased);
+    }
+
+    private LottoTotalPrice setPrice() {
+        return new LottoTotalPrice(InputView.inputLottoPrice());
+    };
+
+    private void printPurchaseInformation(int numberOfPurchasedLotto, List<Lotto> lottos) {
+        OutputView.printNumberOfPurchasedLotto(numberOfPurchasedLotto);
+        OutputView.printLottosList(lottos);
+    }
+    private WinningNumber setWinningNumbers() {
+        return new WinningNumber(InputView.inputWinningNumber());
+    }
+
+    private BonusNumber setBonusNumbers(WinningNumber winningNumbers) {
+        return new BonusNumber(InputView.inputBonusNumber(), winningNumbers);
+    }
+
+    private static void getWinningNumbers() {
+    }
+    private void getBonusNumber() {
+
+    }
+    private void printWinningStatistics() {
+
+    }
 }

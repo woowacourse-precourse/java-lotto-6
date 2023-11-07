052bf33b270862e327320796b5ca88c180efb2fd
MisaSohee <sohee5951@naver.com>
2023-11-07 21:15:29 +0900

    feat(WinningResult): 당첨 통계 출력 기능 추가

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -10,4 +10,15 @@ public class WinningResult {
         Rank rank = Rank.valueOf(matchCount, matchBonus);
         result.put(rank, result.getOrDefault(rank, 0) + 1);
     }
+
+    public void printResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.MISS) continue;
+            System.out.println(rank.getCountOfMatch() + "개 일치" + rank.getBonus() +
+                    "(" + rank.getWinningMoney() + "원)- " +
+                    result.getOrDefault(rank, 0) + "개");
+        }
+    }
 }

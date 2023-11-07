d97faf7cf60c210c72c2cdcc91aa1e83047126c5
Knight <kangrack333@gmail.com>
2023-11-07 21:20:44 +0900

    refactor(startController) : 불필요한 코드 삭제

--- src/main/java/lotto/controller/startController.java
+++ src/main/java/lotto/controller/startController.java
@@ -5,10 +5,5 @@ public class startController {
     public void start() {
         LottoPurchaseController lottoPurchaseController = new LottoPurchaseController();
         lottoPurchaseController.start();
-
     }
-
-//    public static void main(String[] args) {
-//        System.out.println(PrizeMoney.getByOrdinal(3).getPrizeMoneyValue());
-//    }
 }

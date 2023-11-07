9b3c86152657c4ebc44457014f51e0d960725504
kang jihyeong <her4386@gmail.com>
2023-11-07 15:39:29 +0900

    [refactor] 에러 메시지 변수명 변경

--- src/main/java/lotto/utils/ErrorPhrase.java
+++ src/main/java/lotto/utils/ErrorPhrase.java
@@ -2,8 +2,8 @@ package lotto.utils;
 
 public class ErrorPhrase {
 
-    public static final String MONEY_IS_NOT_DIGIT_ERROR_PHRASE = "구입 금액은 숫자만 가능합니다";
+    public static final String ERROR_PHRASE_MONEY_IS_NOT_DIGIT = "구입 금액은 숫자만 가능합니다";
 
-    public static final String MONEY_IS_WRONG_UNIT_ERROR_PHRASE = "구입 금액은 1000원 단위만 가능합니다";
+    public static final String ERROR_PHRASE_MONEY_IS_WRONG_UNIT = "구입 금액은 1000원 단위만 가능합니다";
 
 }

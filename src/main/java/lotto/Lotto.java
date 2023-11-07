1a1e72fa436f57c959a1d70170bbbd3848bbc609
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-07 22:32:35 +0900

    feat : 로또 번호에 대한 예외 문구 메시지 상수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,6 +6,9 @@ import java.util.List;
 import static lotto.LottoDetailNumberConstant.*;
 
 public class Lotto {
+    private static final String LOTTO_NUMBER_STORE_SIZE_EXCEPTION_MESSAGE = "[ERROR] 로또 번호의 개수는 6개이어야 합니다.";
+    private static final String LOTTO_NUMBER_DUPLICATED_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 중복되지 않는 숫자 이어야 합니다.";
+    private static final String LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 1 ~ 45의 자연수 이어야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,13 +20,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_STORE_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_NUMBER_STORE_SIZE_EXCEPTION_MESSAGE);
         }
     }
 
     private void validateDuplicateLottoNumber(List<Integer> numbers) {
         if (new HashSet<>(numbers).size() != numbers.size())
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATED_EXCEPTION_MESSAGE);
     }
 
     private void validateRangeLottoNumberLoop(List<Integer> numbers) {
@@ -34,7 +37,7 @@ public class Lotto {
 
     private void validateRangeLottoNumber(int number) {
         if (!(number >= LOTTO_START_NUMBER && number <= LOTTO_END_NUMBER))
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE);
     }
 
     public List<Integer> getNumbers() {

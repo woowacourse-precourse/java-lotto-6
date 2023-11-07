57b91d6ac838a1370a8d2c297a489a49e82e1195
devJSH1004 <devJSH1004@gmail.com>
2023-11-08 05:11:56 +0900

    feat: 범위와 중복 값에 대한 validate 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,16 +6,34 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateNumber(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateNumber(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
+    private void validateDuplicate(List<Integer> numbers) {
+        if (!(numbers.size() == numbers.stream().distinct().count())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

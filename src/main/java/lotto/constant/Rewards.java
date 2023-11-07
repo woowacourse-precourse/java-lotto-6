5fbd6c310e1a051fb1bd127d41a97c69c05b36b9
Minyongggg <naminyong97@gmail.com>
2023-11-08 06:49:48 +0900

    feat: Rewards enum에 당첨 결과를 확인하는 메서드 구현

--- src/main/java/lotto/constant/Rewards.java
+++ src/main/java/lotto/constant/Rewards.java
@@ -1,17 +1,29 @@
 package lotto.constant;
 
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
 public enum Rewards {
-    LOSE(0),
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000);
+    FIRST(2_000_000_000, (matchCount, hasBonus) -> matchCount == 6),
+    SECOND(30_000_000, (matchCount, hasBonus) -> matchCount == 5 && hasBonus),
+    THIRD(1_500_000, (matchCount, hasBonus) -> matchCount == 5 && !hasBonus),
+    FOURTH(50_000, (matchCount, hasBonus) -> matchCount == 4),
+    FIFTH(5_000, (matchCount, hasBonus) -> matchCount == 3),
+    LOSE(0, (matchCount, hasBonus) -> matchCount < 3);
 
-    private int value;
+    private final int value;
+    private final BiPredicate<Integer, Boolean> condition;
 
-    private Rewards(int value) {
+    private Rewards(int value, BiPredicate<Integer, Boolean> condition) {
         this.value = value;
+        this.condition = condition;
+    }
+
+    public static Rewards of(Integer matchCount, Boolean hasBonus) {
+        return Arrays.stream(Rewards.values())
+                .filter(reward -> reward.condition.test(matchCount, hasBonus))
+                .findAny()
+                .orElse(LOSE);
     }
 
     public int getValue() {

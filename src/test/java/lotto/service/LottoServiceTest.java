b545e19bf99a5c65c82721b890282ed9793f928b
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-07 14:30:27 +0900

    test: 로또 출력하는 테스트 코드 수정

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,9 +1,9 @@
 package lotto.service;
 
 import static lotto.service.LottoService.lottoGenerator;
+import static lotto.service.LottoService.purchasedLottoToDTO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.Lotto;
@@ -34,24 +34,22 @@ public class LottoServiceTest {
     @DisplayName("생성된 로또가 출력되는지 검사한다.")
     public void 로또_출력() throws Exception {
         //given
-        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto lotto2 = new Lotto(List.of(10, 11, 12, 13, 14, 15));
-        Lotto lotto3 = new Lotto(List.of(40, 41, 42, 43, 44, 45));
-        List<Lotto> lottos = new ArrayList<>();
-        lottos.add(lotto1);
-        lottos.add(lotto2);
-        lottos.add(lotto3);
+        List<Lotto> lottos = lottoGenerator(3000);
+        PurchasedLottoNumbers purchasedLottoNumbers = new PurchasedLottoNumbers(lottos);
 
         //when
-        PurchasedLottoNumbers purchasedLottoNumbers = new PurchasedLottoNumbers(lottos);
-        PurchasedLottoDTO purchasedLottoDTO = LottoService.purchasedLottoToDTO(purchasedLottoNumbers);
+        PurchasedLottoDTO purchasedLottoDTO = purchasedLottoToDTO();
 
-        //then
         String lottoDTO1 = Arrays.toString(purchasedLottoDTO.getPurchasedLotto().get(0).getNumbers().toArray());
         String lottoDTO2 = Arrays.toString(purchasedLottoDTO.getPurchasedLotto().get(1).getNumbers().toArray());
         String lottoDTO3 = Arrays.toString(purchasedLottoDTO.getPurchasedLotto().get(2).getNumbers().toArray());
-        Assertions.assertEquals("[1, 2, 3, 4, 5, 6]", lottoDTO1);
-        Assertions.assertEquals("[10, 11, 12, 13, 14, 15]", lottoDTO2);
-        Assertions.assertEquals("[40, 41, 42, 43, 44, 45]", lottoDTO3);
+
+        //then
+        Assertions.assertEquals(
+                Arrays.toString(purchasedLottoNumbers.getPurchasedLotto().get(0).getNumbers().toArray()), lottoDTO1);
+        Assertions.assertEquals(
+                Arrays.toString(purchasedLottoNumbers.getPurchasedLotto().get(1).getNumbers().toArray()), lottoDTO2);
+        Assertions.assertEquals(
+                Arrays.toString(purchasedLottoNumbers.getPurchasedLotto().get(2).getNumbers().toArray()), lottoDTO3);
     }
 }

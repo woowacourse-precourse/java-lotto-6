e3b1573ba8f3efc1799ac7930e62955945f570ab
kihwn <kihwn98@gmail.com>
2023-11-08 04:31:59 +0900

    feat: Lotto 클래스의 당첨 번호와 로또 용지 내의 숫자를 비교하여 일치 개수와 보너스 번호 일치 여부를 확인하는 메서드(compareWinningNumbers)를 테스트하기 위한 메서드(checkMatchNumber()) 생성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,12 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +26,24 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호와 당첨 번호가 일치한 개수에 해당하는 값이 return 되어야 한다 ")
+    @Test
+    void checkMatchNumber() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        WinningNumberSet winningNumberSet = new WinningNumberSet(
+                new String[]{"1", "2", "3", "4", "5", "6"}, 7);
+        int expectedCountMatch = 6;
+        boolean expectedCheckBonus = false;
+        int notExpectedCountMath = 1;
+        boolean notExpectedCheckBonus = true;
+
+        Score score = lotto.compareWinningNumbers(winningNumberSet);
+
+        assertEquals(expectedCountMatch, score.getCountMatch());
+        assertEquals(expectedCheckBonus, score.isCheckBonus());
+        assertNotEquals(notExpectedCountMath, score.getCountMatch());
+        assertNotEquals(notExpectedCheckBonus, score.isCheckBonus());
+    }
+
+
 }

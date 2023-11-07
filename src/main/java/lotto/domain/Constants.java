c3a5884f9e0ae6bd6c53b21b09d71dea4cc67edd
dj <fjzl12@naver.com>
2023-11-07 23:44:33 +0900

    feat(Constants): 상수 클래스 구현

---
+++ src/main/java/lotto/domain/Constants.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class Constants {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_COUNT = 6;
+    public static final int MIN_COST = 1000;
+    public static final int COST_UNIT = 1000;
+    public static final int ZERO = 0;
+}

1c10d69cdc617de79808d84f8bcf837cd30f77c2
jun <ggprgrkjh@naver.com>
2023-11-08 00:57:42 +0900

    refactor(lottocheck): lottoChecker 테스트 given when then 이용해 정리

--- src/test/java/lotto/domain/LottoCheckerTest.java
+++ src/test/java/lotto/domain/LottoCheckerTest.java
@@ -12,6 +12,7 @@ import lotto.model.LottoRank;
 import lotto.model.LottoRankInfo;
 import lotto.model.Lottos;
 import lotto.model.WinningNumbers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,22 +20,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoCheckerTest {
 
-
+    @DisplayName("Enum결과 생성 함수 성공 테스트")
     @Test
     void createEnumResult_EqualResult_Success() {
+
+        //given
         List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
         Lottos lottos = new Lottos(lottoList);
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 8);
         int bonusNumber = 7;
-        LottoChecker lottoChecker = new LottoChecker(new WinningNumbers(winningNumbers), new BonusNumber(bonusNumber));
+        lottoList.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
 
+        //when
+        LottoChecker lottoChecker = new LottoChecker(new WinningNumbers(winningNumbers), new BonusNumber(bonusNumber));
         LottoRankInfo lottoRankInfo = lottoChecker.createResult(lottos);
         Map<LottoRank, Integer> lottoRank = lottoRankInfo.getLottoRankInfo();
+
+        //then
         assertThat(lottoRank.get(LottoRank.THIRD)).isEqualTo(1);
         assertThat(lottoRank.size()).isEqualTo(6);
 
-
     }
+
 
 }
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Game;
 import lotto.model.Lotto;
 import lotto.model.Lottos;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

8e760dedd0daeb5f45921a0b0e19e556918b5d11
bamm2 <xxknu88@naver.com>
2023-11-08 01:05:44 +0900

    feat(ConstantInteger): 숫자형 상수 모음 클래스

---
+++ src/main/java/lotto/constant/ConstantInteger.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public class ConstantInteger {
+
+    public static final Integer ZERO = 0;
+    public static final Integer ADD = 1;
+    public static final Integer UNIT = 1_000;
+    public static final Integer MAX_PRICE = 2_100_000_000;
+    public static final Integer MAX_LENGTH = 11;
+    public static final Integer NUMBER_COUNT = 6;
+    public static final Integer LOTTO_MIN_NUMBER = 1;
+    public static final Integer LOTTO_MAX_NUMBER = 45;
+    public static final Integer PERCENT = 100;
+
+    private ConstantInteger() {
+    }
+
+}

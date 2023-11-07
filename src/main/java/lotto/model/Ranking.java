27fd07ea112bdb1902b69617272c9e824af177be
huhdy32 <mong3268@gmail.com>
2023-11-07 19:02:42 +0900

    fix: 배열 비교 에러 수정

--- src/main/java/lotto/model/Ranking.java
+++ src/main/java/lotto/model/Ranking.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public enum Ranking {
     FIRST(new int[] {6, 0}, 2_000_000_000),
     SECOND(new int[] {5, 1}, 30_000_000),
-    THIRD(new int[] {5, 0}, 1_5000_000),
+    THIRD(new int[] {5, 0}, 15_000_000),
     FORTH(new int[] {4, 0}, 50_000),
     FIFTH(new int[] {3, 0}, 5_000),
     EMPTY(null, 0);
@@ -21,9 +21,12 @@ public enum Ranking {
 
     public static Ranking getRank(int[] score) {
         return Arrays.stream(Ranking.values())
-                .filter(rank -> rank.equals(score))
+                .filter(rank -> Arrays.equals(rank.getScore(), score))
                 .findFirst()
                 .orElse(EMPTY);
+    }
+    private int[] getScore(){
+        return score;
     }
     public int getReward() {
         return reward;

3ccd8886463836f395c9d76935e77f6b5472155c
Minjae-An <mj3242@naver.com>
2023-11-07 20:52:30 +0900

    feat : 로또 결과 정렬 기능 구현
    로또 결과를 출력시 당첨 기준의 필요 당첨 로또 숫자 수에
    따라 정렬된 형태가 되도록, 정렬 기능 구현

--- src/main/java/lotto/domain/lotto/LottoManager.java
+++ src/main/java/lotto/domain/lotto/LottoManager.java
@@ -7,9 +7,12 @@ import static lotto.config.GameConfig.NUMBER_OF_LOTTO_NUMBERS;
 import static lotto.config.GameConfig.START_INCLUSIVE;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.domain.lotto_policy.FixedLottoWinningPolicy;
 import lotto.domain.lotto_prize.FixedLottoPrizeStandard;
@@ -22,37 +25,41 @@ public class LottoManager {
     private Integer purchaseAmount;
     private Integer purchaseQuantity;
 
-    private Integer getNumberOfLotteries(Integer purchaseAmount){
+    private Integer getNumberOfLotteries(Integer purchaseAmount) {
         return purchaseQuantity = purchaseAmount / LOTTO_NUMBER_UNIT;
     }
 
-    private List<Integer> generateLottoNumbers(){
+    private List<Integer> generateLottoNumbers() {
         return pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, NUMBER_OF_LOTTO_NUMBERS);
     }
 
-    private void setPurchaseAmount(Integer purchaseAmount){
+    private void setPurchaseAmount(Integer purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
     }
 
-    public void generateLotteries(Integer purchaseAmount){
+    public void generateLotteries(Integer purchaseAmount) {
         setPurchaseAmount(purchaseAmount);
 
         Integer numberOfLotteries = getNumberOfLotteries(purchaseAmount);
 
-        lotteries=new ArrayList<>(numberOfLotteries);
+        lotteries = new ArrayList<>(numberOfLotteries);
         IntStream.range(0, numberOfLotteries)
                 .forEach(i -> lotteries.add(new Lotto(generateLottoNumbers())));
     }
 
-    public void setWinningNumbers(List<Integer> winningNumbers){
-        this.winningNumbers=winningNumbers;
+    public void setWinningNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
     }
 
     public void setFixedLottoWinningPolicy(FixedLottoWinningPolicy fixedLottoWinningPolicy) {
         this.fixedLottoWinningPolicy = fixedLottoWinningPolicy;
     }
 
-    public List<List<Integer>> getLotteriesNumbers(){
+    public List<List<Integer>> getLotteriesNumbers() {
         return lotteries.stream()
                 .map(Lotto::getNumbers)
                 .toList();
@@ -62,7 +69,14 @@ public class LottoManager {
         return purchaseQuantity;
     }
 
-    public Map<FixedLottoPrizeStandard, Integer> getLottoWinningResults(){
+    private Map<FixedLottoPrizeStandard, Integer> sortLottoWinningResult(
+            Map<FixedLottoPrizeStandard, Integer> lottoWinningResults) {
+        return lottoWinningResults.entrySet().stream()
+                .sorted(Comparator.comparingLong(winningResult -> winningResult.getKey().getMatchCount()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+
+    public Map<FixedLottoPrizeStandard, Integer> getLottoWinningResults() {
         Map<FixedLottoPrizeStandard, Integer> lottoWinningResults = new HashMap<>();
         fixedLottoWinningPolicy.getFixedLottoPrizeStandards()
                 .forEach(prizeStandard -> lottoWinningResults.put(prizeStandard, 0));
@@ -72,18 +86,19 @@ public class LottoManager {
                     .getWinningResult(lotto.getNumbers(), winningNumbers);
 
             winningResult.forEach(prizeStandard ->
-                    lottoWinningResults.put(prizeStandard, lottoWinningResults.get(prizeStandard)+1));
+                    lottoWinningResults.put(prizeStandard, lottoWinningResults.get(prizeStandard) + 1));
         });
 
-        return lottoWinningResults;
+        return sortLottoWinningResult(lottoWinningResults);
     }
 
-    public Double getTotalReturnRate(){
+
+    public Double getTotalReturnRate() {
         Map<FixedLottoPrizeStandard, Integer> lottoWinningResults = getLottoWinningResults();
-        Integer totalReturn = lottoWinningResults.values().stream()
-                .mapToInt(Integer::intValue)
+        Integer totalReturn = lottoWinningResults.entrySet().stream()
+                .mapToInt(winningResult -> winningResult.getKey().getPrize() * winningResult.getValue())
                 .sum();
 
-        return 1.0 * totalReturn / purchaseAmount;
+        return (1.0 * totalReturn / purchaseAmount) * 100;
     }
 }

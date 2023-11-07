0e00951e12204a1c713786efd3b1b3e57bbfa2ff
김라연 <rayeon@gimlayeon-ui-noteubug.local>
2023-11-08 04:06:47 +0900

    feat:로또 구입 금액을 입력받았을 때 발생가능한 예외 처리기능 구현

--- src/main/java/lotto/IOController.java
+++ src/main/java/lotto/IOController.java
@@ -2,14 +2,34 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+
 public class IOController {
     private int price;
 
     public IOController() {
         inputPrice();
+
     }
 
     private void inputPrice() {
-        price = Integer.parseInt(Console.readLine());
+        System.out.println(InstructionMessage.INSTRUCTION_INPUT_PRICE.getMessageText());
+        String inputStringPrice = Console.readLine();
+        isAvailablePrice(inputStringPrice);
     }
+
+    private void isAvailablePrice(String inputStringPrice) {
+        int intPrice;
+
+        try {
+            intPrice = Integer.parseInt(inputStringPrice);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_NUMBER_FORMAT.getMessageText());
+        }
+
+        if (intPrice % 1000 != 0) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_AMOUNT.getMessageText());
+        }
+    }
+
+
 }

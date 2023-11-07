b2f920bf77647db8e1d0667dae9c9a9c44ef6e33
acs0209 <annjae4676@naver.com>
2023-11-07 15:25:17 +0900

    feat: 총 당첨액 계산 기능 구현

---
+++ src/main/java/lotto/domain/LottoProfit.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import static lotto.domain.constant.LottoProfitConstant.FIFTH_PRIZE_AMOUNT;
+import static lotto.domain.constant.LottoProfitConstant.FIRST_PRIZE_AMOUNT;
+import static lotto.domain.constant.LottoProfitConstant.FOURTH_PRIZE_AMOUNT;
+import static lotto.domain.constant.LottoProfitConstant.SECOND_PRIZE_AMOUNT;
+import static lotto.domain.constant.LottoProfitConstant.THIRD_PRIZE_AMOUNT;
+
+import lotto.domain.wrapper.ProfitRate;
+
+public class LottoProfit {
+
+    private final ProfitRate profitRate;
+
+    private LottoProfit() {
+        this.profitRate = ProfitRate.create(0.0);
+    }
+
+    public static LottoProfit create() {
+        return new LottoProfit();
+    }
+
+    public void createLottoProfit(LottoPrize lottoPrize, LottoPurchase lottoPurchase) {
+        int totalWinningAmount = createTotalWinningAmount(lottoPrize);
+    }
+
+    public int createTotalWinningAmount(LottoPrize lottoPrize) {
+        int totalWinningAmount = 0;
+        totalWinningAmount += lottoPrize.getFiveRankCount() * FIFTH_PRIZE_AMOUNT.getNumber();
+        totalWinningAmount += lottoPrize.getFourRankCount() * FOURTH_PRIZE_AMOUNT.getNumber();
+        totalWinningAmount += lottoPrize.getThirdRankCount() * THIRD_PRIZE_AMOUNT.getNumber();
+        totalWinningAmount += lottoPrize.getSecondRankCount() * SECOND_PRIZE_AMOUNT.getNumber();
+        totalWinningAmount += lottoPrize.getFirstRankCount() * FIRST_PRIZE_AMOUNT.getNumber();
+        return totalWinningAmount;
+    }
+}

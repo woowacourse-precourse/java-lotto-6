96b7feffa15d976b550fcb30ade36ae58ca24439
Lee Geun Woo <gamebdy@naver.com>
2023-11-08 04:36:03 +0900

    :sparkles: Bonus 구현

---
+++ src/main/java/lotto/model/Bonus.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class Bonus {
+
+    private final int bonusNumber;
+
+    public Bonus(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

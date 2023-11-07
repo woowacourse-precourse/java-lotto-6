834e03f8bff25493b678a1eb4a60795d7c882834
seohokim <shk9946@gmail.com>
2023-11-07 16:55:52 +0900

    [REFACTOR] LottoShop 리펙토링

--- src/main/java/lotto/LottoShop.java
+++ src/main/java/lotto/LottoShop.java
@@ -5,43 +5,58 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class LottoShop {
-	private static final int LOTTO_PROCE = 1000;
+	private static final int LOTTO_PRICE = 1000;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int MIN_LOTTO_NUMBER = 1;
 	private static final int LOTTO_LENGTH = 6;
+	
 	private ArrayList<Lotto> lottos = new ArrayList<>();
 	private final List<Integer> winningNumbers;
 	private final int bonusNumber;
-	public static int buyLotto(){
+
+	public LottoShop() {
+		int numberOfLotto = buyLotto();
+		this.winningNumbers = decideWinningNumbers();
+		this.bonusNumber = decideBonusNumber();
+		generateLotto(numberOfLotto);
+		Console.close();
+	}
+	private int buyLotto(){
 		System.out.println("구입금액을 입력해 주세요.");
 		String payment = Console.readLine();
 		int numberOfLotto = Checker.checkPayment(payment);
-		Console.close();
 		return numberOfLotto;
 	}
 
-	public List<Integer> decideWinningNumbers() {
+	private List<Integer> decideWinningNumbers() {
 		System.out.println("당첨 번호를 입력해 주세요.");
 		String winningInput = Console.readLine();
-		List<Integer> winningNumbers = Chekcer.checkWinningInput(winningInput);
-		this.winningNumbers = winningNumbers;
-		return this.winningNumbers;
+		return Checker.checkWinningInput(winningInput);
 	}
 
-	public int decideBonusNumber() {
+	private int decideBonusNumber() {
 		System.out.println("보너스 번호를 입력해 주세요.");
 		String bonusInput = Console.readLine();
-		int bonusNumber = Checker.checkBonusInput(bonusInput);
-		this.bonusNumber = bonusNumber;
-		return this.bonusNumber;
+		return Checker.checkBonusInput(bonusInput);
 	}
 	
-	private ArrayList<Lotto> generateLotto(int numberOfLotto){
-		for (i = 0; i < numberOfLotto; i++) {
+	private void generateLotto(int numberOfLotto){
+		for (int i = 0; i < numberOfLotto; i++) {
 			List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_LENGTH);
 			Lotto lotto = new Lotto(numbers);
 			this.lottos.add(lotto);
 		}
+	}
+
+	public List<Integer> getWinningNumbers() {
+		return this.winningNumbers;
+	}
+
+	public ArrayList<Lotto> getLottos() {
 		return this.lottos;
+	}
+
+	public int getBonusNumber() {
+		return this.bonusNumber;
 	}
 }

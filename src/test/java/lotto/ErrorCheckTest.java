3d881b419137c541bfa72c9b6ee81da190323f5e
kihwn <kihwn98@gmail.com>
2023-11-08 04:36:34 +0900

    feat: ErrorCheck 클래스 중 validateWinningNumbersDataType 메서드를 테스트하기 위한 메서드인 checkErrorOccurWinningNumbersDataAlphabet()와 checkErrorOccurWinningNumbersData() 생성

--- src/test/java/lotto/ErrorCheckTest.java
+++ src/test/java/lotto/ErrorCheckTest.java
@@ -17,5 +17,21 @@ class ErrorCheckTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("당첨 번호로 숫자 이외의 값(알파벳)을 입력하면 에러가 발생해야 한다.")
+    @Test
+    void checkErrorOccurWinningNumbersDataAlphabet() {
+        String[] invalidWinningNumbers = {"1", "2", "3", "a", "5", "6"};
+        assertThatThrownBy(() -> ErrorCheck.validateWinningNumbersDataType(invalidWinningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호로 숫자 이외의 값을 입력하면 에러가 발생해야 한다.")
+    @Test
+    void checkErrorOccurWinningNumbersData() {
+        String[] invalidWinningNumbers = {"1", "2", "3", ".", "5", "6"};
+        assertThatThrownBy(() -> ErrorCheck.validateWinningNumbersDataType(invalidWinningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
 }

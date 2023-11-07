2f6faf254958f16590d72f6b4b6e90bf8c83635e
mini1115 <kmini9611@naver.com>
2023-11-07 16:57:01 +0900

    feat : 사용자로부터 당첨 로또 번호를 입력받는 기능 추가

--- src/main/java/lotto/PlayerInput.java
+++ src/main/java/lotto/PlayerInput.java
@@ -2,11 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerInput {
     //금액 입력받기
     public int InputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
         return money;
+    }
+
+    public List<Integer> LottoNumber() {
+        List<Integer> winNumber = new ArrayList<>();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        for (String number : input.split(",")) {
+            winNumber.add(Integer.parseInt(number));
+        }
+        return winNumber;
     }
 }

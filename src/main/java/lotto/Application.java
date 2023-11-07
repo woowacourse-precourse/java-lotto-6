1e7bdbdf3679a87c5e5a8cdca2138314bd5dee14
moo <p_caso@naver.com>
2023-11-07 20:41:12 +0900

    Application 클래스의 기본 구조 LottoMachine 클래스의 기본 구조 작성 todo : Lotto 클래스: 하나의 로또를 관리합니다. Lotto 클래스는 기본적으로 주어진 Lotto 클래스를 활용하고, 필요한 로짐을 추가합니다.
    WinningLotto 클래스: 당첨 번호와 보너스 번호를 관리합니다. 입력받는 문자열로부터 당첨 번호와 보너스 번호를 생성합니다.
    
      LottoResult 클래스: 로또 결과를 관리합니다. 당첨 번호와 로또들을 비교하여 결과를 계산합니다.
    
      ResultView 클래스: 결과를 출력하는 뷰 역할을 합니다. 요구사항에 따라 결과를 출력하는 메서드를 구현합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,18 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구입 금액 입력 및 로또 발행
+        int purchaseAmount = 0; // 구입 금액 입력 로직 추가
+        int manualLottoCount = 0; // 수동으로 구매할 로또 개수 입력 로직 추가
+        LottoMachine lottoMachine = new LottoMachine(purchaseAmount, manualLottoCount);
+
+        // 당첨 번호 및 보너스 번호 입력
+        String winningNumbersInput = ""; // 당첨 번호 입력 로직 추가
+        int bonusNumber = 0; // 보너스 번호 입력 로직 추가
+        WinningLotto winningLotto = WinningLotto.of(winningNumbersInput, bonusNumber);
+
+        // 로또 결과 출력
+        LottoResult lottoResult = lottoMachine.calculateResult(winningLotto);
+        ResultView.printResult(lottoMachine, lottoResult);
     }
 }
---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_SIZE = 6;
+
+    private final List<Lotto> lottos;
+
+    public LottoMachine(int purchaseAmount, int manualLottoCount) {
+        // TODO: 구입 금액과 수동 로또 개수에 따라 lottos 초기화
+    }
+
+    // TODO: 나머지 구현
+}

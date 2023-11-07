154c67ced381d6e8543c5ce0886bf19d795e3838
Junobee25 <wnsdhqo@naver.com>
2023-11-07 16:38:48 +0900

    feat(Validation): verifyRangeWinningLottoNumber 메서드 구현

--- src/main/java/lotto/verification/Validation.java
+++ src/main/java/lotto/verification/Validation.java
@@ -4,17 +4,31 @@ import lotto.view.VerificationView;
 
 public class Validation {
 
+    private static final String COMMAS = "-";
+
     public static void verifyRangeInputMoney(long myMoney) {
         if (myMoney / 1000 < 1 || myMoney % 1000 != 0) {
-            VerificationView.VerificationViewRangeInputMoney();
+            VerificationView.verificationViewRangeInputMoney();
             throw new IllegalArgumentException();
         }
     }
 
     public static void verifyStringToLong(String string) {
         if (!string.chars().allMatch(Character::isDigit)) {
-            VerificationView.VerificationViewStringToLong();
+            VerificationView.verificationViewStringToLong();
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void verifyRangeWinningLottoNumber(String string) {
+        String[] lottoNumbers = string.split(COMMAS);
+
+        for(String lottoNumber : lottoNumbers) {
+            int number = Integer.parseInt(lottoNumber);
+            if (number < 1 || number > 45) {
+                VerificationView.verificationViewRangeLottoNumber();
+                throw new IllegalArgumentException();
+            }
         }
     }
 

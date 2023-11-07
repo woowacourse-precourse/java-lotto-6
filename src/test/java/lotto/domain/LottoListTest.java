90506733fbee5760f853ef6361fb71ae60732401
changhyun9 <ckdgus1713@naver.com>
2023-11-07 23:53:51 +0900

    test : add testing code about LottoList

--- src/test/java/lotto/domain/LottoListTest.java
+++ src/test/java/lotto/domain/LottoListTest.java
@@ -1,2 +1,27 @@
-package lotto.domain;public class LottoListTest {
+package lotto.domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoListTest {
+    @DisplayName("생성된 로또 번호를 삽입")
+    @Test
+    void insertLottoInList() {
+        LottoList lottoList = new LottoList();
+        lottoList.insertLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        Assertions.assertThat(lottoList.getSize()).isEqualTo(1);
+    }
+
+    @DisplayName("모든 로또를 반환")
+    @Test
+    void returnAllLotto() {
+        LottoList lottoList = new LottoList();
+        lottoList.insertLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottoList.insertLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottoList.insertLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        List<Lotto> allLotto = lottoList.getAllLotto();
+        Assertions.assertThat(allLotto.size()).isEqualTo(3);
+    }
 }

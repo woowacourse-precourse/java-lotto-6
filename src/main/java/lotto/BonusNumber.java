212b3c9fbe95c572b355a9d9ae7f6aa9a98ec1a3
kg9585 <kg9585@naver.com>
2023-11-07 17:01:10 +0900

    feat(BonusNumber): 보너스 숫자 생성

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import java.util.Collection;
+import java.util.List;
+
+public class BonusNumber extends LottoNumber{
+    public BonusNumber(int number) {
+        super(number);
+    }
+}

3f094058aa50426228fca4713450e7bc80415f8c
PARK9898 <wjddjs21@naver.com>
2023-11-07 21:24:07 +0900

    "faet: 보너스 번호 입력 받는 메서드 구현"

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,10 +2,13 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import javax.sound.midi.Soundbank;
 import lotto.util.StringHandler;
 
 public class InputView {
+
     private static final String STRING_DELIMITER = ",";
+
     public int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
@@ -13,11 +16,16 @@ public class InputView {
         return StringHandler.stringToInt(input);
     }
 
-
     public List<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         return StringHandler.separatedWith(input, STRING_DELIMITER);
+    }
+
+    public int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return StringHandler.stringToInt(input);
     }
 
 }

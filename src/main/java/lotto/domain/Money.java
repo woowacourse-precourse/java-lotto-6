6deb2f476d29e4db9190130da19136a2bdc0d942
becooq81 <becooq81@gmail.com>
2023-11-07 16:30:50 +0900

    style(Money): add space for better readability

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -6,7 +6,9 @@ import lotto.exception.IndivisibleByUnitException;
 public class Money {
     private static final int UNIT = 1000;
     private static final int ZERO = 0;
+
     private final int money;
+
     public Money(int money) {
         validateMoney(money);
         this.money = money;
@@ -24,11 +26,13 @@ public class Money {
         validateNotZero(money);
         validateDivisibleByUnit(money);
     }
+
     private void validateDivisibleByUnit(int money) {
         if (money % UNIT != 0) {
             throw new IndivisibleByUnitException();
         }
     }
+
     private void validateNotZero(int money) {
         if (money == ZERO) {
             throw new ArgumentIsZeroException();

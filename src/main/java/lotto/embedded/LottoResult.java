a4f00c4738728f1ff7e80d129f7b3b758b3fdcb2
juns1s <juns1s@naver.com>
2023-11-07 16:09:16 +0900

    feat: LottoResult 정의

---
+++ src/main/java/lotto/embedded/LottoResult.java
@@ -0,0 +1,59 @@
+package lotto.embedded;
+
+import java.util.Optional;
+
+public enum LottoResult {
+    FIRST(2000000000, 6, false, "6개 일치 (2,000,000,000원)"),
+    SECOND(30000000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(1500000, 5, false, "5개 일치 (1,500,000원)"),
+    FOURTH(50000, 4, false, "4개 일치 (50,000원)"),
+    FIFTH(5000, 3, false, "3개 일치 (5,000원)"),
+    ;
+
+    private final int prize;
+    private final int matchCount;
+    private final boolean isBonusMatch;
+    private final String description;
+
+    LottoResult(int prize, int matchCount, boolean isBonusMatch, String description) {
+        this.prize = prize;
+        this.matchCount = matchCount;
+        this.isBonusMatch = isBonusMatch;
+        this.description = description;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return isBonusMatch;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Optional<LottoResult> of(int matchCount, boolean isBonusMatch){
+        if(matchCount == FIRST.matchCount){
+            return Optional.of(LottoResult.FIRST);
+        }
+        if(matchCount == SECOND.matchCount && isBonusMatch){
+            return Optional.of(LottoResult.SECOND);
+        }
+        if(matchCount == THIRD.matchCount){
+            return Optional.of(LottoResult.THIRD);
+        }
+        if(matchCount == FOURTH.matchCount){
+            return Optional.of(LottoResult.FOURTH);
+        }
+        if(matchCount == FIFTH.matchCount){
+            return Optional.of(LottoResult.FIFTH);
+        }
+        return Optional.empty();
+    }
+}

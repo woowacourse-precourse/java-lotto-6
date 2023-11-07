bda51896d91cebcc48527e8085eb7d65ad4313ae
zion0425 <zion0425@naver.com>
2023-11-07 23:42:34 +0900

    test(InputMoneyTest): 구매 금액 입력 예외 테스트

---
+++ src/test/java/lotto/input/InputMoneyTest.java
@@ -0,0 +1,48 @@
+package lotto.input;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static lotto.input.validator.InputMoneyValidator.getInstance;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputMoneyTest {
+    @DisplayName("사용자가 빈 문자열을 입력하면 예외가 발생한다.")
+    @Test
+    public void createInputBlank() {
+        assertThatThrownBy(() -> getInstance().validate(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 특수문자를 입력하면 예외가 발생한다.")
+    @Test
+    public void createInputSpecialCharacter() {
+        assertThatThrownBy(() -> getInstance().validate(";"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 알파벳을 입력하면 예외가 발생한다.")
+    @Test
+    public void createInputAlphabet() {
+        assertThatThrownBy(() -> getInstance().validate("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자가 올바른 값을 입력하면 입력 값이 그대로 반환된다.")
+    @Test
+    void createInputSuccess() {
+        String input = "1000";
+        String result = "";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        InputMoney inputMoney = new InputMoney();
+
+        System.setIn(in);
+        result = inputMoney.getUserInput();
+
+        Assertions.assertEquals(input, result);
+    }
+}

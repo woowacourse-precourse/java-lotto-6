a24ef85184c98b0d8ef9195502bbd73da714dd1e
eunjin3481 <eunjin3481@gmail.com>
2023-11-08 05:45:52 +0900

    feat($Lotto): 예외 처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +14,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자여야 합니다.");
+        }
+        if(numbers.stream().distinct().count() != LOTTO_COUNT){
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력 할 수 없습니다.");
+        }
+        for(int number : numbers) {
+            if(number >= LOTTO_START_NUMBER && number <= LOTTO_END_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 1~45 까지의 수를 입력해주세요");
+            }
         }
     }
 

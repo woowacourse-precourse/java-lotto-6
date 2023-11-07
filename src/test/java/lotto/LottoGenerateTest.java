e46effa8c767a1be82ba555bdd3fe393d5e77e81
unknown <winternrabbit@gmail.com>
2023-11-07 21:45:56 +0900

    test: 지정한 숫자만큼 로또 생성하여 저장하는 로직 수정에 따른 테스트 코드 수정

--- src/test/java/lotto/LottoGenerateTest.java
+++ src/test/java/lotto/LottoGenerateTest.java
@@ -10,15 +10,15 @@ class LottoGenerateTest {
     @DisplayName("요청 횟수만큼 로또 번호 묶음 생성되는지 확인")
     @Test
     void checkSize() {
-        LottoGenerate lottoGenerate = new LottoGenerate(3);
-        assertThat(lottoGenerate.getRandomLottoNumbers().size()).isEqualTo(3);
+        LottoGenerate lottoGenerate = new LottoGenerate();
+        assertThat(lottoGenerate.createLottoRepeat(3).size()).isEqualTo(3);
     }
 
     @DisplayName("로또 번호 오름차순으로 출력되는지 확인")
     @Test
     void checkNumberSortedASC() {
-        LottoGenerate lottoGenerate = new LottoGenerate(1);
-        List<Integer> result = lottoGenerate.getRandomLottoNumbers().get(0);
+        LottoGenerate lottoGenerate = new LottoGenerate();
+        List<Integer> result = lottoGenerate.createLottoRepeat(1).get(0).getLotto();
         List<Integer> compare = result.stream().sorted().toList();
 
         assertThat(result).isEqualTo(compare);

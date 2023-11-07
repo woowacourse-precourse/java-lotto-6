42df615d4f5b28c6ce7f3636bc8a991e9dd7797f
Jehun Yoo <percyu98@gmail.com>
2023-11-07 12:13:18 +0900

    feat(Application): play lotto game

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.config.Config;
+import lotto.game.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Config config = new Config();
+        Game game = config.getLottoGame();
+        game.play();
     }
 }

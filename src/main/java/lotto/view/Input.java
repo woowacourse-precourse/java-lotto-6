1311ed1fab3663acac9a4693451fe38d7e4f2b44
anjdydhody <a01066444534@gmail.com>
2023-11-07 22:34:28 +0900

    Add inputNumbers method

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 
 public class Input {
+
+    Output output;
     int inputAmount() {
         int amount = Integer.parseInt(Console.readLine());
         validateAmount(amount);
@@ -14,4 +17,19 @@ public class Input {
             throw new IllegalArgumentException("1000원 단위로 입력해주세요");
         }
     }
+
+    List<Integer> inputNumbers() {
+        List<Integer> lotto;
+        while (true) {
+            try {
+                lotto = splitNumbers(Console.readLine());
+                break;
+            } catch (IllegalArgumentException e) {
+                output.printError(e);
+            }
+        }
+        return lotto;
+    }
+
+
 }

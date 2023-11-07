f99b1f6536973c0181a3cb2f54b0a4f59c8a267a
win9 <krrr883@gmail.com>
2023-11-08 05:15:40 +0900

    [Fix]: 형식과 다른 출력 수정
    온점 추가

--- src/main/java/type/TextType.java
+++ src/main/java/type/TextType.java
@@ -2,10 +2,10 @@ package type;
 
 public enum TextType {
     START("구입금액을 입력해 주세요."),
-    LOTTO_WIN_NUMBER("당첨 번호를 입력해 주세요"),
+    LOTTO_WIN_NUMBER("당첨 번호를 입력해 주세요."),
     PRICE_TO_NUMBER("개를 구매했습니다."),
     LOTTO_BONUS("보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS("당첨 통계.");
+    WINNING_STATISTICS("당첨 통계"),
 
     private final String text;
 

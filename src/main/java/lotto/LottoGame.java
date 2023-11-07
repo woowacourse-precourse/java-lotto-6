6e5f562797b97ba2c3a0769902c01b162e5376d3
choji <waf268@gmail.com>
2023-11-07 22:00:49 +0900

    add : LottoGame이 로또 당첨여부를 확인하도록 구현
    로또 번호를 당첨번호와 확인하여 enum에 count하도록 메서드 구현
    해당 메서드의 테스트코드 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,14 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
+import java.util.*;
 
 public class LottoGame {
     private final int lottoStartNumber = 1;
     private final int lottoEndNumber = 45;
     private final int lottoLength = 6;
     private Lotto winningLotto;
-    private int BonusNumber;
+    private int bonusNumber;
+    private Map<Integer, LottoPrize> numberToPrize;
+
+    public LottoGame() {
+        numberToPrize = new HashMap<>();
+        numberToPrize.put(7, LottoPrize.SECOND);
+        numberToPrize.put(6, LottoPrize.FIRST);
+        numberToPrize.put(5, LottoPrize.THIRD);
+        numberToPrize.put(4, LottoPrize.FOURTH);
+        numberToPrize.put(3, LottoPrize.FIFTH);
+    }
 
     public Lotto getLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(lottoStartNumber, lottoEndNumber, lottoLength);
@@ -17,19 +27,51 @@ public class LottoGame {
         return lotto;
     }
 
+    public void checkLotto(Lotto lotto) {
+        int equalcount = countEqual(lotto);
+        boolean bonusCheck = checkBounsNumber(lotto);
+        if (equalcount == 5) {
+            if (bonusCheck) {
+                equalcount += 2;
+            }
+        }
+        if (equalcount >= 3) {
+            numberToPrize.get(equalcount).increaseCount();
+        }
+    }
+
+    private int countEqual(Lotto baseLotto) {
+        int duplicateCount = 0;
+        List<Integer> winningList = this.winningLotto.getNumbers();
+        List<Integer> baseList = baseLotto.getNumbers();
+        Set<Integer> checkSet = new HashSet<>();
+        for (Integer winningNumber : winningList) {
+            checkSet.add(winningNumber);
+        }
+        for (Integer baseNumber : baseList) {
+            if (checkSet.contains(baseNumber)) {
+                duplicateCount++;
+            }
+        }
+        return duplicateCount;
+    }
+
+    private boolean checkBounsNumber(Lotto baseLotto) {
+        if (baseLotto.getNumbers().contains(this.bonusNumber)) {
+            return true;
+        }
+        return false;
+    }
+
     public void setWinningLotto(List<Integer> numbers) {
         this.winningLotto = new Lotto(numbers);
     }
 
     public void setBonusNumber(int number) {
-        this.BonusNumber = number;
+        this.bonusNumber = number;
     }
 
     public Lotto getWinningLotto() {
         return this.winningLotto;
-    }
-
-    public int getBonusNumber() {
-        return this.BonusNumber;
     }
 }
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -3,17 +3,32 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LottoGameTest {
     LottoGame lottoGame;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         lottoGame = new LottoGame();
     }
+
     @Test
-    void getLottoTest(){
+    void getLottoTest() {
         Lotto lotto = lottoGame.getLotto();
         assertNotNull(lotto);
     }
+
+    @Test
+    void checkLottoTest() {
+        lottoGame.setWinningLotto(List.of(1, 3, 5, 7, 9, 11));
+        lottoGame.setBonusNumber(13);
+        assertEquals(0, LottoPrize.SECOND.getCount());
+        lottoGame.checkLotto(new Lotto(List.of(1, 3, 5, 7, 9, 13)));
+        assertEquals(1, LottoPrize.SECOND.getCount());
+    }
+
 }

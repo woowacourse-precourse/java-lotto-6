2d090706df0373e337b69c2841f2b3306357c1a3
박승민 <tmddudals369@naver.com>
2023-11-07 20:23:24 +0900

    feat(winningNumberValidator): 당첨 번호가 여러 개인지 검증하는 기능 구현

---
+++ src/main/java/lotto/validation/WinningNumberValidator.java
@@ -0,0 +1,17 @@
+package lotto.validation;
+
+public class WinningNumberValidator {
+    private static final String MULTIPLE_WINNING_NUMBER_DELIMITER = ",";
+    private WinningNumberValidator() {
+    }
+
+    public static void validateWinningNumbers(String winningNumbers) {
+        isMultiple(winningNumbers);
+    }
+
+    private static void isMultiple(String winningNumbers) {
+        if (!winningNumbers.contains(MULTIPLE_WINNING_NUMBER_DELIMITER)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표를 구분으로 입력해야 합니다.");
+        }
+    }
+}

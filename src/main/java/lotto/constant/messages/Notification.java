74c48d565af7f2e497bc67d3c24a606a970ead9d
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-07 20:16:10 +0900

    reafactor(Notification): WINNER->WINNING 이름 변경

--- src/main/java/lotto/constant/messages/Notification.java
+++ src/main/java/lotto/constant/messages/Notification.java
@@ -1,9 +1,9 @@
-package lotto.constants.messages;
+package lotto.constant.messages;
 
 public enum Notification {
     BUDGET("구입금액을 입력해 주세요."),
     PURCHASED("%d개를 구매했습니다."),
-    WINNER_LOTTO("당첨 번호를 입력해 주세요."),
+    WINNING_LOTTO("당첨 번호를 입력해 주세요."),
     BONUS_LOTTO("보너스 번호를 입력해 주세요."),
     RESULT_PRE("당첨 통계\n"
             + "---"),

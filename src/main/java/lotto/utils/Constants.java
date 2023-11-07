f4aa03b7cf908521cb65713253158e38c651c285
snaag <snaag.dev@gmail.com>
2023-11-08 06:36:45 +0900

    feat: (Constants) Constants 클래스 기능 구현

---
+++ src/main/java/lotto/utils/Constants.java
@@ -0,0 +1,11 @@
+package lotto.utils;
+
+public class Constants {
+    public static int LOTTO_PRICE = 1_000;
+    public static int LOTTO_DRAW_NUMBER_COUNT = 6;
+    public static int LOTTO_DRAW_BONUS_NUMBER_COUNT = 1;
+
+    public static int LOTTO_UPPER_BOUND = 45;
+    public static int LOTTO_UNDER_BOUND = 1;
+    public static String SEPERATOR = ",";
+}

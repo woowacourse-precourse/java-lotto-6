1fe484afca3c2f5ae0e86d22a357c685efabb5db
donghoonyeom <dua5410773@naver.com>
2023-11-08 02:53:13 +0900

    feat(Validator): BonusNumberValidator 클래스를 추가
    보너스 번호의 입력 유효성을 검사하는 BonusNumberValidator 클래스를 추가하였습니다. 입력된 번호가 1부터 45 사이의 숫자인지, 중복된 번호인지, 그리고 숫자 형식인지 검사하여 유효성을 확인합니다.

---
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -0,0 +1,31 @@
+package lotto.validator;
+
+import java.util.Set;
+
+public class BonusNumberValidator {
+    public static int validateBonusNumber(String input, Set<Integer> winningNumbers) {
+        int bonusNumber = validateNumericInput(input);
+        validateNumberRange(bonusNumber);
+        validateDuplicateNumber(bonusNumber, winningNumbers);
+        return bonusNumber;
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력해 주세요.");
+        }
+    }
+
+    private static void validateDuplicateNumber(int number, Set<Integer> winningNumbers) {
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 이미 당첨 번호로 선택된 숫자입니다.");
+        }
+    }
+
+    private static int validateNumericInput(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+        }
+        return Integer.parseInt(input);
+    }
+}

749bc7b56d918399b797e481fba8efa6cc5a48ee
nhn1030 <shgusska12@gmail.com>
2023-11-08 03:00:18 +0900

    test: WinningChecker 클래스 구현을 위한 테스트 코드 작성

--- src/test/java/lotto/WinningCheckerTest.java
+++ src/test/java/lotto/WinningCheckerTest.java
@@ -1,4 +1,27 @@
 package lotto;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.WinningChecker;
+import org.junit.jupiter.api.Test;
+
 public class WinningCheckerTest {
+    @Test
+    void testWinningChecker() {
+        WinningChecker winningChecker = new WinningChecker();
+
+        List<Integer> winningNumbers = Arrays.asList(3, 7, 12, 19, 24, 30);
+        int bonusNumber = 5;
+
+        List<Integer> userTicket1 = Arrays.asList(3, 7, 12, 19, 24, 30);
+        List<Integer> userTicket2 = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        List<List<Integer>> userTickets = Arrays.asList(userTicket1, userTicket2);
+
+        List<Integer> result = winningChecker.checkWinning(userTickets, winningNumbers, bonusNumber);
+
+        assertEquals(Arrays.asList(1, 0), result);
+    }
 }

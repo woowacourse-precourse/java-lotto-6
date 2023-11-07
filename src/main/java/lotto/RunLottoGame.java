763d767348261ae65f5853ee8dfbc76d949c8196
박채연 <parkchaeyeon@bagchaeyeon-ui-MacBookAir.local>
2023-11-07 21:43:55 +0900

    refactor: RunLottoGame 클래스의 start() 메서드 분해
    start() 메서드가 너무 많은 책임을 지고 있어 코드를 작업 단계별로 분리

--- src/main/java/lotto/RunLottoGame.java
+++ src/main/java/lotto/RunLottoGame.java
@@ -3,13 +3,22 @@ package lotto;
 import java.util.List;
 
 public class RunLottoGame {
+    private int lottoQuantity;
+
     public void start() {
+        prepareLotto();
+        publishLotto();
+    }
+
+    public void prepareLotto() {
         InputManager inputManager = new InputManager();
         int moneyInput = inputManager.inputMoney();
 
         LottoQuantityManager lottoquantitymanager = new LottoQuantityManager();
-        int lottoQuantity = lottoquantitymanager.calculateLottoQuantity(moneyInput);
+        lottoQuantity = lottoquantitymanager.calculateLottoQuantity(moneyInput);
+    }
 
+    public void publishLotto() {
         LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
         List<List<Integer>> allRandomLottoNumbers = lottoNumberGenerator.generateLottoNumbers(lottoQuantity);
 

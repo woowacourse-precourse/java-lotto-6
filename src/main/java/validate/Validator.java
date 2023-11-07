2025c59a33ec640d3504ef035c96c4b8c47a607c
kitewater <97283971+kitewater@users.noreply.github.com>
2023-11-08 02:16:26 +0900

    feat: 오류메시지 enum으로 추가
    오류 메시지들을 정리하는 enum 클래스 추가

--- src/main/java/validate/Validator.java
+++ src/main/java/validate/Validator.java
@@ -1,7 +1,24 @@
 package validate;
 
 public class Validator {
+    public enum ErrorMessage {
+
+        ERROR1("[ERROR] 숫자를 입력해주세요");
+
+        private String message;
+
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+    }
+
     public int validatePrice(String price){
-        if(p)
+        int my_price;
+        try {
+            my_price = Integer.parseInt(price);
+        }catch (Exception e){
+            System.out.println(ErrorMessage.ERROR1);
+            throw new IllegalArgumentException();
+        }
     }
 }

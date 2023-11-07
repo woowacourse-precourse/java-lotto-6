d1750aa8b1e5248033b626dd90c7540908595249
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 14:44:09 +0900

    feat(PurchaseAmountValidator): 구매 금액 범위를 검증하는 기능

--- src/main/java/lotto/utils/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/utils/validator/PurchaseAmountValidator.java
@@ -4,6 +4,7 @@ import lotto.utils.message.PurchaseAmountExceptionMessage;
 
 public class PurchaseAmountValidator {
 
+    private static final long LOTTO_COUNTS_MIN_LIMIT = 1L;
     private static final long LOTTO_COUNTS_MAX_LIMIT = 100_000L;
     private static final long LOTTO_PRICE_UNIT = 1000L;
 
@@ -11,6 +12,7 @@ public class PurchaseAmountValidator {
         validateBlank(input);
         validateMaxLength(input);
         validateNumeric(input);
+        validateRange(input);
     }
 
     private static void validateBlank(String input) {
@@ -30,6 +32,15 @@ public class PurchaseAmountValidator {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(PurchaseAmountExceptionMessage.NOT_NUMERIC.getError(), e);
+        }
+    }
+
+    private static void validateRange(String input) {
+        int purchaseAmount = Integer.parseInt(input);
+
+        if (purchaseAmount < LOTTO_COUNTS_MIN_LIMIT * LOTTO_PRICE_UNIT ||
+                purchaseAmount > LOTTO_COUNTS_MAX_LIMIT * LOTTO_PRICE_UNIT) {
+            throw new IllegalArgumentException(PurchaseAmountExceptionMessage.OUT_OF_AMOUNT_RANGE.getError());
         }
     }
 }

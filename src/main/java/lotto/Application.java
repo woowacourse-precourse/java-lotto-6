f435d4fbb32990a1713fc17545c4e0f37ced5472
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-07 16:13:25 +0900

    refactor: 메서드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.*;
+import java.util.Map;
+import java.util.HashMap;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -17,20 +19,29 @@ public class Application {
     public static void main(String[] args) {
 
         final int LOTTO_PRICE = 1000;
+        int parseMoney = 0;
 
-        String money = inputBuyMoney();
+        while (true) {
+            try {
+                String money = inputBuyMoney();
 
-        System.out.println();
+                System.out.println();
 
-        validateEmpty(money);
+                validateEmpty(money);
 
-        validateNumberFormat(money);
+                validateStartNumber(money);
 
-        int parseMoney = parseInteger(money);
+                parseMoney = validateNumberFormat(money);
 
-        validateStartNumber(money);
-
-        validateUnit(parseMoney);
+                validateUnit(parseMoney);
+                break;
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println(INCORRECT_NUMBER_FORMAT.getErrorMessage());
+                continue;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                continue;
+            }
+        }
 
         int buyLottoCount = buyLottoTicket(parseMoney, LOTTO_PRICE);
 
@@ -40,40 +51,56 @@ public class Application {
 
         printLotto(lotties);
 
-        System.out.println();
-
-        String winNumber = inputWinNumber();
+        List<Integer> parseWinNumbers;
 
         System.out.println();
 
-        validateEmpty(winNumber);
+        while (true) {
+            try {
+                String winNumber = inputWinNumber();
 
-        validateWinNumberFormat(winNumber);
+                System.out.println();
 
-        int[] winNumbers = parseIntArray(winNumber);
+                validateEmpty(winNumber);
 
-        List<Integer> parseWinNumbers = parseIntList(winNumbers);
+                validateWinNumberFormat(winNumber);
 
-        validateSixNumber(parseWinNumbers);
+                int[] winNumbers = parseIntArray(winNumber);
 
-        String bonusNumber = inputBonusNumber();
+                parseWinNumbers = parseIntList(winNumbers);
 
-        validateEmpty(bonusNumber);
+                validateSixNumber(parseWinNumbers);
+                break;
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println(INCORRECT_NUMBER_FORMAT.getErrorMessage());
+                continue;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                continue;
+            }
 
-        validateNumberFormat(bonusNumber);
+        }
 
-        int parseBounusNumber = parseInteger(bonusNumber);
+        int parseBonusNumber = 0;
 
-        validateNumberRange(parseBounusNumber);
+        while (true) {
+            String bonusNumber = inputBonusNumber();
 
-        validateDuplicateNumber(parseWinNumbers, parseBounusNumber);
+            validateEmpty(bonusNumber);
 
-        getWinNumberCount(lotties, parseWinNumbers, parseBounusNumber, parseMoney);
-    }
+            validateNumberFormat(bonusNumber);
 
-    public static List<Integer> makeLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
+            parseBonusNumber = parseInteger(bonusNumber);
+
+            validateNumberRange(parseBonusNumber);
+
+            validateDuplicateNumber(parseWinNumbers, parseBonusNumber);
+            break;
+        }
+
+        Map<String, Integer> result = getResultWinCount(lotties, parseWinNumbers, parseBonusNumber, parseMoney);
+        int earnMoney = getEarnMoney(result);
+        double rateOfReturn = getRateOfReturn(earnMoney, parseMoney);
+        printResult(result, rateOfReturn);
     }
 
     public static void printConstantMessage(ConstantMessage ConstantMessage) {
@@ -85,30 +112,27 @@ public class Application {
         return Console.readLine();
     }
 
-    //try catch로 묶기
     public static void validateEmpty(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException(EMPTY.getErrorMessage());
+            System.out.println(EMPTY.getErrorMessage());
+            throw new IllegalArgumentException();
         }
+    }
+
+    public static int validateNumberFormat(String input) {
+        return parseInteger(input);
     }
 
     public static int parseInteger(String input) {
         return Integer.parseInt(input);
     }
 
-    public static void validateNumberFormat(String input) {
-        try {
-            parseInteger(input);
-        } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException(INCORRECT_NUMBER_FORMAT.getErrorMessage());
-        }
-    }
-
     public static void validateStartNumber(String input) {
         final int START_NUMBER = 0;
         final char CHECK_CHARACTER = '0';
         if (input.charAt(START_NUMBER) == CHECK_CHARACTER) {
-            throw new IllegalArgumentException(START_LETTER_ZERO.getErrorMessage());
+            System.out.println(START_LETTER_ZERO.getErrorMessage());
+            throw new IllegalArgumentException();
         }
     }
 
@@ -116,7 +140,8 @@ public class Application {
         final int UNIT = 1000;
         final int REST_NUMBER = 0;
         if (input % UNIT != REST_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
+            System.out.println(INCORRECT_THOUSAND_UNIT.getErrorMessage());
+            throw new IllegalArgumentException();
         }
     }
 
@@ -127,6 +152,11 @@ public class Application {
     public static void printLottoTicketCount(int lottoTicketCount) {
         System.out.print(lottoTicketCount);
         printConstantMessage(BUY_LOTTO_TICKET);
+    }
+
+    public static List<Integer> makeLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 
     public static List<Lotto> collectLotto(int lottoTicketCount) {
@@ -154,7 +184,8 @@ public class Application {
         final int LAST_NUMBER = winNumber.length() - 1;
         final char CHECK_CHARACTER = ',';
         if (winNumber.charAt(START_NUMBER) == CHECK_CHARACTER || winNumber.charAt(LAST_NUMBER) == CHECK_CHARACTER) {
-            throw new IllegalArgumentException(INCORRECT_WIN_NUMBER_FORMAT.getErrorMessage());
+            System.out.println(INCORRECT_WIN_NUMBER_FORMAT.getErrorMessage());
+            throw new IllegalArgumentException();
         }
     }
 
@@ -173,7 +204,8 @@ public class Application {
     public static void validateSixNumber(List<Integer> parseWinNumbers) {
         final int TARGET_NUMBER_COUNT = 6;
         if (parseWinNumbers.size() != TARGET_NUMBER_COUNT) {
-            throw new IllegalArgumentException(NOT_SIX_NUMBER.getErrorMessage());
+            System.out.println(NOT_SIX_NUMBER.getErrorMessage());
+            throw new IllegalArgumentException();
         }
     }
 
@@ -186,67 +218,32 @@ public class Application {
         final int MIN_RAGNE = 1;
         final int MAX_RANGE = 45;
         if (parseBounusNumber < MIN_RAGNE || parseBounusNumber > MAX_RANGE) {
-            throw new IllegalArgumentException(NOT_IN_INPUT_RANGE.getErrorMessage());
+            System.out.println(NOT_IN_INPUT_RANGE.getErrorMessage());
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validateDuplicateNumber(List<Integer> parseWinNumbers, int parseBounusNumber) {
         if (parseWinNumbers.contains(parseBounusNumber)) {
-            throw new IllegalArgumentException(DUPLICATE_NUMBER.getErrorMessage());
+            System.out.println(DUPLICATE_NUMBER.getErrorMessage());
+            throw new IllegalArgumentException();
         }
     }
 
-    public static void getWinNumberCount(List<Lotto> lotties, List<Integer> parseWinNumbers, int parseBonusNumber,
+    public static Map<String, Integer> getResultWinCount(List<Lotto> lotties, List<Integer> parseWinNumbers,
+            int parseBonusNumber,
             int parseMoney) {
-        int threeCorrect = 0;
-        int fourCorrect = 0;
-        int fiveCorrect = 0;
-        int fiveBonusCorrect = 0;
-        int sixCorrect = 0;
-        int earnMoney = 0;
+
         boolean hasBonusNumber = false;
+        Map<String, Integer> result = new HashMap<>();
         for (int i = 0; i < lotties.size(); i++) {
             int correctCount = 0;
             Lotto lotto = lotties.get(i);
             correctCount = checkWinNumber(lotto, correctCount, parseWinNumbers);
             hasBonusNumber = checkBonusNumber(lotto, parseBonusNumber);
-            if (correctCount == 3) {
-                threeCorrect++;
-                earnMoney += 5000;
-            }
-
-            if (correctCount == 4) {
-                fourCorrect++;
-                earnMoney += 50000;
-            }
-
-            if (correctCount == 5) {
-                fiveCorrect++;
-                earnMoney += 1500000;
-            }
-
-            if (correctCount == 5 && hasBonusNumber) {
-                fiveBonusCorrect++;
-                earnMoney += 30000000;
-            }
-
-            if (correctCount == 6) {
-                sixCorrect++;
-                earnMoney += 2000000000;
-            }
+            result = saveCount(result, correctCount, hasBonusNumber);
         }
-
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + threeCorrect + "개");
-        System.out.println("4개 일치 (50,000원) - " + fourCorrect + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + fiveCorrect + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveBonusCorrect + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + sixCorrect + "개");
-
-        double parseEarnMoney = (double) earnMoney;
-        double rateOfReturn = parseEarnMoney / parseMoney * 100;
-        System.out.println("총 수익률은 " + String.format("%.1f", rateOfReturn) + "%입니다.");
+        return result;
 
     }
 
@@ -267,9 +264,101 @@ public class Application {
         return false;
     }
 
-    public static void getResult(int correctCount, boolean hasBonusNumber, int earnMoney, int threeCorrect,
-            int fourCorrect, int fiveCorrect, int fiveBonusCorrect, int sixCorrect) {
+    public static Map<String, Integer> saveCount(Map<String, Integer> result, int correctCount,
+            boolean hasBonusNumber) {
 
+        if (correctCount == 3) {
+            result.put("three", result.getOrDefault(result.get("three"), 0) + 1);
+        }
+
+        if (correctCount == 4) {
+            result.put("four", result.getOrDefault(result.get("four"), 0) + 1);
+        }
+
+        if (correctCount == 5) {
+            result.put("five", result.getOrDefault(result.get("five"), 0) + 1);
+        }
+
+        if (correctCount == 5 && hasBonusNumber) {
+            result.put("fiveBonus", result.getOrDefault(result.get("fiveBonus"), 0) + 1);
+        }
+
+        if (correctCount == 6) {
+            result.put("six", result.getOrDefault(result.get("six"), 0) + 1);
+        }
+        return result;
+    }
+
+    public static int getEarnMoney(Map<String, Integer> result) {
+        final int THREE_MONEY = 5000;
+        final int FOUR_MONEY = 5000;
+        final int FIVE_MONEY = 1500000;
+        final int FIVE_BONUS_MONEY = 30000000;
+        final int SIX_MONEY = 2000000000;
+        int earnMoney = 0;
+        for (String key : result.keySet()) {
+            if (key.equals("three")) {
+                earnMoney += (result.get(key) * THREE_MONEY);
+            }
+            if (key.equals("four")) {
+                earnMoney += (result.get(key) * FOUR_MONEY);
+            }
+            if (key.equals("five")) {
+                earnMoney += (result.get(key) * FIVE_MONEY);
+            }
+            if (key.equals("fiveBonus")) {
+                earnMoney += (result.get(key) * FIVE_BONUS_MONEY);
+            }
+            if (key.equals("six")) {
+                earnMoney += (result.get(key) * SIX_MONEY);
+            }
+        }
+
+        return earnMoney;
+    }
+
+    public static void printWinCount(Map<String, Integer> result, String key) {
+        if (result.containsKey(key)) {
+            System.out.print(result.get(key));
+        }
+        if (!result.containsKey(key)) {
+            System.out.print(0);
+        }
+    }
+
+    public static double getRateOfReturn(int earnMoney, int parseMoney) {
+        double parseEarnMoney = (double) earnMoney;
+        double rateOfReturn = parseEarnMoney / parseMoney * 100;
+        return rateOfReturn;
+    }
+
+    public static void printResult(Map<String, Integer> result, double rateOfReturn) {
+        printConstantMessage(WINNING_STAT);
+        printConstantMessage(THREE_CORRECT);
+        printWinCount(result, "three");
+        printConstantMessage(COUNT);
+
+        printConstantMessage(FOUR_CORRECT);
+        printWinCount(result, "four");
+        printConstantMessage(COUNT);
+
+        printConstantMessage(FIVE_CORRECT);
+        printWinCount(result, "five");
+        printConstantMessage(COUNT);
+
+        printConstantMessage(FIVE_BONUS_CORRECT);
+        printWinCount(result, "fiveBonus");
+        printConstantMessage(COUNT);
+
+        printConstantMessage(SIX_CORRECT);
+        printWinCount(result, "six");
+        printConstantMessage(COUNT);
+        printRateOfReturn(rateOfReturn);
+    }
+
+    public static void printRateOfReturn(double rateOfReturn) {
+        final String ROUND_POSITION = "%.1f";
+        System.out.println("총 수익률은 " + String.format(ROUND_POSITION, rateOfReturn) + "%입니다.");
     }
 
 }

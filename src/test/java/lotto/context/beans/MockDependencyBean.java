7b2c8767c7462e123528b92d3588024fb6e91398
mr-silvester <126128590+mr-silvester@users.noreply.github.com>
2023-11-07 15:02:33 +0900

    test(SimpleBeanFactory): 테스트 코드 작성 및 검증 완료
    - 테스트 콘솔 로그 출력을 위한 프록시 클래스 작성
    - 테스트에 사용할 모의 Bean 클래스와 Config 클래스 추가

---
+++ src/test/java/lotto/context/beans/MockDependencyBean.java
@@ -0,0 +1,4 @@
+package lotto.context.beans;
+
+public class MockDependencyBean {
+}
--- src/test/java/lotto/context/container/implementation/annotationConfig/annotation/config/TestConfig.java
+++ src/test/java/lotto/context/container/implementation/annotationConfig/annotation/config/TestConfig.java
@@ -1,16 +1,20 @@
 package lotto.context.container.implementation.annotationConfig.annotation.config;
 
+import lotto.context.beans.MockBean;
+import lotto.context.beans.MockDependencyBean;
 import lotto.context.container.implementation.annotationConfig.annotation.bean.Bean;
 import lotto.context.container.implementation.annotationConfig.annotation.configuration.Configuration;
 
 @Configuration
 public class TestConfig {
     @Bean
-    public void test1() {
+    public MockDependencyBean mockDependencyBean() {
+        return new MockDependencyBean();
     }
 
     @Bean
-    public void test2() {
+    public MockBean mockBean(MockDependencyBean mockDependencyBean) {
+        return new MockBean(mockDependencyBean);
     }
 
     public void test3() {

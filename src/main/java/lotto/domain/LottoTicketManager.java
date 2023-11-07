cb93b2454993c12b843b698a9ef421a0d4c14bb2
bamm2 <xxknu88@naver.com>
2023-11-07 22:17:04 +0900

    feat(LottoTicketManager): 결과 출력을 위한 Map 형태로 변환

--- src/main/java/lotto/domain/LottoTicketManager.java
+++ src/main/java/lotto/domain/LottoTicketManager.java
@@ -5,23 +5,29 @@ import static lotto.constant.ConstantInteger.LOTTO_MIN_NUMBER;
 import static lotto.exception.ExceptionMessage.DUPLICATED_BONUS_NUMBER_MESSAGE;
 import static lotto.exception.ExceptionMessage.NOT_IN_RANGE_NUMBER_INPUT_MESSAGE;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoTicketManager {
 
+    private Integer price;
     private List<Lotto> lottoTickets;
     private Lotto luckyNumbers;
     private Integer bonusNumber;
+    private Map<Rank, Integer> resultMap;
 
     public LottoTicketManager(){
+        initMap();
     }
 
     public Integer countTickets(String price) {
         return convertPrice(price)/1000 ;
     }
 
-    private Integer convertPrice(String price){
-        return Integer.parseInt(price);
+    private Integer convertPrice(String price) {
+        this.price = Integer.parseInt(price);
+        return this.price;
     }
 
     public void getUserLottoTickets(List<Lotto> lottoTickets){
@@ -48,13 +54,41 @@ public class LottoTicketManager {
         }
     }
 
-    private boolean isInRangeNumber(Integer number){
+    private boolean isInRangeNumber(Integer number) {
         return LOTTO_MIN_NUMBER <= number && number <= LOTTO_MAX_NUMBER;
     }
 
-    private void validateDuplicateBonusNumberInLuckyNumber(Integer bonusNumber){
-        if(luckyNumbers.getNumbers().contains(bonusNumber)){
+    private void validateDuplicateBonusNumberInLuckyNumber(Integer bonusNumber) {
+        if (luckyNumbers.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATED_BONUS_NUMBER_MESSAGE.getMessage());
         }
     }
+
+    private void initMap() {
+        resultMap = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            resultMap.put(rank, 0);
+        }
+    }
+
+    public Map<Rank, Integer> makeResult() {
+        for (Lotto lotto : lottoTickets) {
+            int correctLuckyNumber = correctLuckyNumber(lotto);
+            boolean correctBonusNumber = correctBonusNumber(lotto);
+            Rank ranking = Rank.matching(correctLuckyNumber, correctBonusNumber);
+            resultMap.put(ranking, resultMap.getOrDefault(ranking, 0) + 1);
+        }
+        return resultMap;
+    }
+
+    public Integer correctLuckyNumber(Lotto lotto) {
+        return (int) lotto.getNumbers().stream().filter(luckyNumbers.getNumbers()::contains)
+                .count();
+    }
+
+    public boolean correctBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+
+
 }

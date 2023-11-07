28b704a0a1ee8e8880e51a9dccda9033526da18f
HYOAN <aspqll@naver.com>
2023-11-08 00:41:41 +0900

    feat(lotto): 구입금액 입력 기능 구현, 구매한 금액만큼 로또를 생성하고 구매한 로또들의 번호 출력 기능 구현

--- src/main/java/lotto/domain/LottoController.java
+++ src/main/java/lotto/domain/LottoController.java
@@ -1,7 +1,21 @@
 package lotto.domain;
 
+import lotto.Lotto;
+import lotto.LottoTicket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
+    private Lotto lotto;
+    private LottoTicketGenerator lottoTicketGenerator;
+    private UserInput userInput;
+    private UserInputChecker userInputChecker;
+
     public LottoController() {
+        lottoTicketGenerator = new LottoTicketGenerator();
+        userInput = new UserInput();
+        userInputChecker = new UserInputChecker();
     }
 
     public enum LottoMessage {
@@ -22,15 +36,30 @@ public class LottoController {
     }
 
     public void playGame() {
-        int purchaseAmount = 0;
+        List<LottoTicket> userLottoTickets = new ArrayList<>();
+        int purchaseAmount = handlePurchaseAmount();
 
-
+        userLottoTickets = lottoTicketGenerator.generateLottoTicket(purchaseAmount);
+        printTickets(userLottoTickets); // 구매한 로또들 번호 출력
     }
 
-    public void lottoPrint(LottoMessage lottoMessage, int purchaseAmount) {
-        if (purchaseAmount > 0)
-            System.out.print(purchaseAmount);
-        System.out.println(lottoMessage.getMessage());
+    public int handlePurchaseAmount() {
+        int ticketCount = 0;
+        String input = "";
+
+        System.out.println(LottoMessage.PURCHASE_AMOUNT_MESSAGE.getMessage()); // 구입금액 입력 안내문구 출력
+        input = userInput.input();
+
+        int purchaseAmount = userInput.getUserPurchaseAmount(userInputChecker, input);
+        ticketCount = purchaseAmount / 1000;
+        System.out.println(ticketCount + LottoMessage.PURCHASE_COUNT_MESSAGE.getMessage()); // 구매한 로또 개수 출력
+
+        return purchaseAmount;
     }
 
+    public void printTickets(List<LottoTicket> userLottoTickets) {
+        for (LottoTicket ticket : userLottoTickets) {
+            ticket.printNumbers();
+        }
+    }
 }

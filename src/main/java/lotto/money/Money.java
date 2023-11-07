34e421285614677987c26b1467e2af59975568a0
kimminsu <xjfcnfw3@naver.com>
2023-11-07 20:50:25 +0900

    refactor: 로또 금액에 가격 객체를 분리

--- src/main/java/lotto/money/Money.java
+++ src/main/java/lotto/money/Money.java
@@ -4,61 +4,51 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Money {
-    private static final int PERCENTAGE = 100;
+
     private static final String WRONG_MONEY = "로또는 1000원씩 구매해야 합니다.";
-    private static final String NOT_INTEGER = "금액은 정수로 입력하세요.";
     private static final String INVALID_RANGE_AMOUNT = "1000원 이상 입력하세요";
     private static final int LOTTO_PRICE = 1000;
     private static final int ROUNDING_DIGITS = 1;
 
-    private final Integer money;
+    private final Price money;
 
-    public Money(String amount) {
-        this.money = validate(amount);
+    public Money(String money) {
+        Price price = new Price(money);
+        validate(price);
+        this.money = price;
     }
 
-    private Integer validate(String amount) {
-        Integer money = checkIntegerAmount(amount);
-        checkMinimumAmount(money);
+    private void validate(Price money) {
+        checkMinimumPrice(money);
         checkCorrectUnit(money);
-        return money;
     }
 
-    private Integer checkIntegerAmount(String amount) {
-        try {
-            return Integer.valueOf(amount);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_INTEGER);
-        }
-    }
-
-    private void checkMinimumAmount(Integer amount) {
-        if (!isAmountBelowMinimum(amount)) {
+    private void checkMinimumPrice(Price price) {
+        if (!isAmountBelowMinimum(price)) {
             throw new IllegalArgumentException(INVALID_RANGE_AMOUNT);
         }
     }
 
-    private boolean isAmountBelowMinimum(Integer amount) {
-        return amount >= LOTTO_PRICE;
+    private boolean isAmountBelowMinimum(Price price) {
+        return price.isGreaterThanOrEqualTo(LOTTO_PRICE);
     }
 
-    private void checkCorrectUnit(Integer money) {
-        if (!isDivisibleByLottoPrice(money)) {
+    private void checkCorrectUnit(Price price) {
+        if (!isDivisibleByLottoPrice(price)) {
             throw new IllegalArgumentException(WRONG_MONEY);
         }
     }
 
-    private boolean isDivisibleByLottoPrice(Integer money) {
-        return money % LOTTO_PRICE == 0;
+    private boolean isDivisibleByLottoPrice(Price money) {
+        return money.isDivisible(LOTTO_PRICE);
     }
 
     public Integer getLottoCount() {
-        return money / LOTTO_PRICE;
+        return money.calculateQuotient(LOTTO_PRICE);
     }
 
-    public Double calculateProfit(Integer totalPrice) {
-        double profit = (double) totalPrice / money * PERCENTAGE;
-        BigDecimal bigDecimal = new BigDecimal(String.valueOf(profit));
-        return bigDecimal.setScale(ROUNDING_DIGITS, RoundingMode.HALF_UP).doubleValue();
+    public Double calculateProfit(Integer totalIncome) {
+        BigDecimal profit = money.calculateProfit(totalIncome);
+        return profit.setScale(ROUNDING_DIGITS, RoundingMode.HALF_UP).doubleValue();
     }
 }

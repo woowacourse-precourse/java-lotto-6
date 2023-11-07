79052d0ce32367dd15a6313949c94f67a4a79b0c
ay-eonii <naayen@naver.com>
2023-11-08 00:45:38 +0900

    refactor(controller): 메서드가 한가지 일만 하도록 분리

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -7,44 +7,53 @@ import lotto.domain.WinningLotto;
 import lotto.view.View;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static lotto.view.constant.Message.*;
 
 public class Controller {
-
-    private final static View view = new View();
+    private static final View view = new View();
     public void start() {
         view.output(INPUT_COST);
         Buyer buyer = view.inputAndValidateCost();
 
-        int count = buyer.getBuyCount();
-        view.output(String.format(OUTPUT_BUY, count));
+        List<Lotto> lottos = buyLottos(buyer);
 
-        List<Lotto> lottos = generateLottos(count);
-        for (Lotto lotto : lottos) {
-            view.output(lotto.toString());
-        }
-
-        view.output(INPUT_MATCH);
+        view.output(INPUT_WINNING_LOTTO);
         WinningLotto winningLotto = view.inputAndValidateWinningLotto();
 
         view.output(INPUT_BONUS);
         int bonus = view.inputAndValidateBonus(winningLotto);
 
-        view.output(WINNING_STATISTICS);
         Statistics statistics = Statistics.calculate(lottos, winningLotto, bonus);
-        view.output(statistics.toString());
-
-        view.output(String.format(REVENUE_RATE, statistics.calculateRevenueRate(buyer)));
+        displayStatistics(statistics);
+        displayRevenueRate(buyer, statistics);
     }
 
-    private Buyer getCost() {
-        try {
-            int cost = view.inputCost();
-            return new Buyer(cost);
-        } catch (IllegalArgumentException e) {
-            view.output(e.getMessage());
-            return getCost();
+    private List<Lotto> buyLottos(Buyer buyer) {
+        int count = buyer.getBuyCount();
+        List<Lotto> lottos = Stream.generate(() -> new Lotto(Lotto.generateLotto()))
+                .limit(count)
+                .collect(Collectors.toList());
+        displayLottos(lottos, count);
+        return lottos;
+    }
+
+    private void displayLottos(List<Lotto> lottos, int count) {
+        view.output(String.format(OUTPUT_BUY, count));
+        for (Lotto lotto : lottos) {
+            view.output(lotto.toString());
         }
+    }
+
+    private void displayStatistics(Statistics statistics) {
+        view.output(WINNING_STATISTICS);
+        view.output(statistics.toString());
+    }
+
+    private void displayRevenueRate(Buyer buyer, Statistics statistics) {
+        String revenueRate = statistics.calculateRevenueRate(buyer);
+        view.output(String.format(REVENUE_RATE, revenueRate));
     }
 }
--- src/main/java/lotto/view/constant/Message.java
+++ src/main/java/lotto/view/constant/Message.java
@@ -3,7 +3,7 @@ package lotto.view.constant;
 public class Message {
     public static final String INPUT_COST = "구입금액을 입력해 주세요.";
     public static final String OUTPUT_BUY = "\n%d개를 구매했습니다.";
-    public static final String INPUT_MATCH = "\n당첨 번호를 입력해 주세요.";
+    public static final String INPUT_WINNING_LOTTO = "\n당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS = "\n보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATISTICS = "\n당첨 통계\n---";
     public static final String REVENUE_RATE = "총 수익률은 %s%%입니다.";

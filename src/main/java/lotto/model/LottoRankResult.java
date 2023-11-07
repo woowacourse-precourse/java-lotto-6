840b34bd331c57dd576228e49375c646ca89e13f
Dawin <dawin002@naver.com>
2023-11-07 17:26:22 +0900

    refactor(LottoRankResult): 로또 등수 관리 기능 수정
    등수별 당첨 로또 개수 반환 방식 수정:
    - 변경전: getRankResult()로 전체 결과 리스트로 반환
    - 변경후: getCount(int rank)로 등수에 해당하는 로또 개수 반환
    상수 대신 Rank enum 사용

--- src/main/java/lotto/model/LottoRankResult.java
+++ src/main/java/lotto/model/LottoRankResult.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
+import lotto.model.enums.Prize;
+import lotto.model.enums.Rank;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoRankResult {
@@ -9,19 +11,19 @@ public class LottoRankResult {
 
     public LottoRankResult() {
         this.rankCount = new ArrayList<>();
-        for (int i = 0; i < 6 ; i++) {
+        for (int i = 0; i <= Rank.RANK5.getRank() ; i++) {
             rankCount.add(0);
         }
     }
 
     public void increaseRankCount(int rank) {
-        if (rank != -1) {
+        if (rank != Rank.NO_RANK.getRank()) {
             int count = rankCount.get(rank);
             rankCount.set(rank, count + 1);
         }
     }
 
-    public List<Integer> getRankResult() {
-        return Collections.unmodifiableList(rankCount);
+    public int getCount(int rank) {
+        return rankCount.get(rank);
     }
 }

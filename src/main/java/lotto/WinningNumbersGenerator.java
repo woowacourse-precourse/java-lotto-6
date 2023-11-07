3f0d4eea73d1d724764dba28b223bf307b49c4af
watermelon3737 <pshrina1@naver.com>
2023-11-07 13:57:00 +0900

    fix(WinningNumberGenerator) : input 기능 삭제 및 파라미터 적용

--- src/main/java/lotto/WinningNumbersGenerator.java
+++ src/main/java/lotto/WinningNumbersGenerator.java
@@ -9,29 +9,28 @@ public class WinningNumbersGenerator {
     private final int START_NUM = 1;
     private final int END_NUM = 45;
     private final int NUM_COUNT = 6;
-    private final String DELIMITER = ",";
 
     private List<Integer> winningNumbers;
     private int bonusNumber;
-    private String[] inputDividedByDelimiter;
+    private String[] winningNumberInput;
 
-    public void generateWinningNumbers(final String winningNumbersInput) {
-        this.dividedInputByDelimiter(winningNumbersInput);
-        this.validateInputDividedByDelimiter();
+    public void generateWinningNumbers(final String[] winningNumbersInput) {
+        this.setWinningNumberInput(winningNumbersInput);
+        this.validateEachWinningNumberInput();
         this.validateDividedInput();
         this.saveWinningNumbers();
     }
 
-    public void dividedInputByDelimiter(final String winningNumbersInput) {
-        this.inputDividedByDelimiter = winningNumbersInput.split(DELIMITER);
+    public void setWinningNumberInput(final String[] winningNumbersInput) {
+        this.winningNumberInput = winningNumbersInput;
     }
 
-    public void validateInputDividedByDelimiter() {
-        if (this.inputDividedByDelimiter.length != NUM_COUNT) {
+    public void validateEachWinningNumberInput() {
+        if (this.winningNumberInput.length != NUM_COUNT) {
             throw new IllegalArgumentException("[ERROR] 6개의 번호가 필요합니다.");
         }
 
-        final Set<String> noDuplicatedInput = Arrays.stream(this.inputDividedByDelimiter)
+        final Set<String> noDuplicatedInput = Arrays.stream(this.winningNumberInput)
                 .collect(Collectors.toSet());
         if (noDuplicatedInput.size() != NUM_COUNT) {
             throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
@@ -39,7 +38,7 @@ public class WinningNumbersGenerator {
     }
 
     public void validateDividedInput() {
-        for (String winningNumberInput : this.inputDividedByDelimiter) {
+        for (String winningNumberInput : this.winningNumberInput) {
             if (winningNumberInput.chars().anyMatch(c -> !Character.isDigit((char) c))) {
                 throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.");
             }
@@ -50,28 +49,19 @@ public class WinningNumbersGenerator {
         }
     }
 
-    public String[] getInputDividedByDelimiter() {
-        return inputDividedByDelimiter;
-    }
 
     public void saveWinningNumbers() {
-        this.winningNumbers = Arrays.stream(this.inputDividedByDelimiter).map(Integer::parseInt).toList();
+        this.winningNumbers = Arrays.stream(this.winningNumberInput).map(Integer::parseInt).toList();
     }
 
-    public void generateBonusNumber(final String bonusNumberInput) {
-        this.validateBonusNumberInput(bonusNumberInput);
+    public void generateBonusNumber(final int bonusNumberInput) {
         this.saveBonusNumber(bonusNumberInput);
         this.validateBonusNumber();
     }
 
-    public void validateBonusNumberInput(final String bonusNumberInput) {
-        if (bonusNumberInput.chars().anyMatch(c -> !Character.isDigit((char) c))) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
-        }
-    }
 
-    public void saveBonusNumber(final String bonusNumberInput) {
-        this.bonusNumber = Integer.parseInt(bonusNumberInput);
+    public void saveBonusNumber(final int bonusNumberInput) {
+        this.bonusNumber = bonusNumberInput;
     }
 
     public void validateBonusNumber() {

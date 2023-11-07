45a1e13a5b946f1c22ab3ce30ccdcb110d7d736f
jiu6525 <jiu6525@daum.net>
2023-11-07 20:09:04 +0900

    test : 수익률 계산 테스트

--- src/test/java/lotto/domain/LottoMatchTest.java
+++ src/test/java/lotto/domain/LottoMatchTest.java
@@ -1,15 +1,19 @@
 package lotto.domain;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import java.util.Map;
+import lotto.Application;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class LottoMatchTest {
+class LottoMatchTest extends NsTest {
 
     private LottoMatch lottoMatch;
 
@@ -38,6 +42,42 @@ class LottoMatchTest {
                 assertThat(k).isEqualTo(ranks[index]);
             }
         });
+    }
+
+    @Test
+    @DisplayName("당첨횟수에 따른 수익률 계산 테스트")
+    void earningRatesTest() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("6000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "6개를 구매했습니다.",
+                            "[11, 12, 13, 14, 15, 16]",
+                            "[1, 2, 3, 7, 8, 9]",
+                            "[1, 2, 3, 4, 8, 9]",
+                            "[1, 2, 3, 4, 5, 8]",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "[1, 2, 3, 4, 5, 6]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 1개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 1개",
+                            "총 수익률은 33859250.0%입니다."
+                    );
+                },
+                List.of(11, 12, 13, 14, 15, 16),
+                List.of(1, 2, 3, 7, 8, 9),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 4, 5, 8),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 6)
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 
 }

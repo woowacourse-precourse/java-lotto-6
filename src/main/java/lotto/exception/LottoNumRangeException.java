b799783c2bcc75ffea83160f7da1b5fc511a70f3
34suuuuu <psh7492@gmail.com>
2023-11-07 09:08:31 +0900

    feat: 로또 번호가 1-45 사이의 숫자가 아닌 경우 예외 처리

--- src/main/java/lotto/exception/LottoNumRangeException.java
+++ src/main/java/lotto/exception/LottoNumRangeException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class LottoNumRangeException  extends  IllegalArgumentException{
-    private static final String ERROR_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     public LottoNumRangeException(){
         super(ERROR_MESSAGE);

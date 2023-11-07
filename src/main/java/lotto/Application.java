21f501823e5639d50186eca4715fc19b47f4aba0
2uwls <2uwls1126@gmail.com>
2023-11-07 16:45:55 +0900

    feat: create lotto numbers according to the number of buying lotto tickets

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,27 +3,35 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    static ArrayList<Lotto> lottoTickets = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해주세요:");
-        getQuantityOfLotto(Console.readLine());
+        generateLottoTickets(Console.readLine());
     }
 
     public static Integer convertMoneyFormat(String inputMoney) {
         Integer money = Integer.valueOf(inputMoney);
         return money;
     }
-    public static void getQuantityOfLotto(String inputMoney) {
-        Integer money = convertMoneyFormat(inputMoney);
+    public static Integer getQuantityOfLotto(Integer money) {
         Integer quantityOfLotto = money/1000;
-        System.out.println(quantityOfLotto);
+        return quantityOfLotto;
+    }
+    public static void generateLottoTickets(String inputMoney) {
+        Integer money= convertMoneyFormat(inputMoney);
+        Integer quantityOfLotto = getQuantityOfLotto(money);
+        for (int i = 0; i<quantityOfLotto; i++) {
+            lottoTickets.add(generateLotto());
+        }
+    }
+    public static Lotto generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.sort(null);
+        return new Lotto(numbers);
     }
 
-    //수량 만큼 반복
-        // 로또 랜덤 생성
-        //랜덤 생성된 로또 정렬
-        //로또 객체 만들기
-        //로또 list에 삽입(로또 배열 만들어야함)
 }

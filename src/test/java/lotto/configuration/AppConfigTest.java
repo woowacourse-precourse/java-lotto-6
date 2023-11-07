925089a88cdfc150b21738a72a06750b70d3d6a2
inpink <inpink@kakao.com>
2023-11-08 03:05:03 +0900

    refactor: IoC 컨테이너에서 중복코드 검증 시 중복되는 코드를 제네릭과 BeforeEach로 줄임

--- src/test/java/lotto/configuration/AppConfigTest.java
+++ src/test/java/lotto/configuration/AppConfigTest.java
@@ -7,87 +7,51 @@ import lotto.repository.Repository;
 import lotto.service.LottoGameService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AppConfigTest {
 
-    Config config = AppConfig.getInstance();
+    private Config config;
+
+    @BeforeEach
+    void setUp() {
+        config = AppConfig.getInstance();
+    }
 
     @Test
     public void AppConfig가_싱글톤인지() {
-        // Given && When
-        Config config1 = AppConfig.getInstance();
-        Config config2 = AppConfig.getInstance();
-
-        // Then
-        assertThat(config1).isInstanceOf(Config.class);
-        assertThat(config1).isNotNull();
-        assertThat(config2).isNotNull();
-        assertThat(config1).isSameAs(config2);
+        assertSingleton(config);
     }
 
     @Test
     public void LottoGameController가_싱글톤인지() {
-        // Given && When
-        LottoGameController lottoGameController1 = config.lottoGameController();
-        LottoGameController lottoGameController2 = config.lottoGameController();
-
-        // Then
-        assertThat(lottoGameController1).isInstanceOf(LottoGameController.class);
-        assertThat(lottoGameController1).isNotNull();
-        assertThat(lottoGameController2).isNotNull();
-        assertThat(lottoGameController1).isSameAs(lottoGameController2);
+        assertSingleton(config.lottoGameController());
     }
 
     @Test
     public void LottoGameService가_싱글톤인지() {
-        // Given && When
-        LottoGameService lottoGameService1 = config.lottoGameService();
-        LottoGameService lottoGameService2 = config.lottoGameService();
-
-        // Then
-        assertThat(lottoGameService1).isInstanceOf(LottoGameService.class);
-        assertThat(lottoGameService1).isNotNull();
-        assertThat(lottoGameService2).isNotNull();
-        assertThat(lottoGameService1).isSameAs(lottoGameService2);
+        assertSingleton(config.lottoGameService());
     }
 
     @Test
     public void InputView가_싱글톤인지() {
-        // Given && When
-        InputView inputView1 = config.inputView();
-        InputView inputView2 = config.inputView();
-
-        // Then
-        assertThat(inputView1).isInstanceOf(InputView.class);
-        assertThat(inputView1).isNotNull();
-        assertThat(inputView2).isNotNull();
-        assertThat(inputView1).isSameAs(inputView2);
+        assertSingleton(config.inputView());
     }
 
     @Test
     public void OutputView가_싱글톤인지() {
-        // Given && When
-        OutputView outputView1 = config.outputView();
-        OutputView outputView2 = config.outputView();
-
-        // Then
-        assertThat(outputView1).isInstanceOf(OutputView.class);
-        assertThat(outputView1).isNotNull();
-        assertThat(outputView2).isNotNull();
-        assertThat(outputView1).isSameAs(outputView2);
+        assertSingleton(config.outputView());
     }
 
     @Test
     public void LottoGameRepository가_싱글톤인지() {
-        // Given && When
-        Repository repository1 = config.lottoGameRepository();
-        Repository repository2 = config.lottoGameRepository();
+        assertSingleton(config.lottoGameRepository());
+    }
 
-        // Then
-        assertThat(repository1).isInstanceOf(Repository.class);
-        assertThat(repository1).isNotNull();
-        assertThat(repository2).isNotNull();
-        assertThat(repository1).isSameAs(repository2);
+    private <T> void assertSingleton(T instance) {
+        assertThat(instance).isInstanceOf(instance.getClass());
+        assertThat(instance).isNotNull();
+        assertThat(instance).isSameAs(instance);
     }
 }

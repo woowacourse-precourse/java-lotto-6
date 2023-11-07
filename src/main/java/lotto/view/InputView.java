d99a3fc058a77e357e005421fd0351d326b4fcd3
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-07 22:04:26 +0900

    feat(InputView): InputView 구현 완료

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+
+    public String readMoney() {
+        return Console.readLine();
+    }
+
+    public String readWinningNumbers() {
+        return Console.readLine();
+    }
+
+    public String readBonusNumber() {
+        return Console.readLine();
+    }
+}

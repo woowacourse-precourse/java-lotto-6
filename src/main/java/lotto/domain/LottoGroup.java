0afcfe3b9c7a56d8504f735d3d3afb26fdcb48c6
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 13:39:59 +0900

    feat(domain): 로또를 개수만큼 생성하고 관리해주는 LottoGroup 도메인 생성

---
+++ src/main/java/lotto/domain/LottoGroup.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoGroup {
+    private List<Lotto> lottos;
+
+
+    public LottoGroup(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static LottoGroup create(int count) {
+        return new LottoGroup(IntStream.range(0, count).mapToObj(i -> Lotto.create()).toList());
+    }
+
+    @Override
+    public String toString() {
+        return lottos
+                .stream()
+                .map(Lotto::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+}

6d56fe6e00b8924f1960816ac699925657175fa5
unifolio0 <boho82ha@gmail.com>
2023-11-07 19:10:31 +0900

    test(BonusNumberTest) : 1~45사이를 벗어날 때 예외 처리 테스트 분리
    - 45보다 클 때와 1보다 작을 때 두 가지 경우로 분리

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -17,10 +17,18 @@ public class BonusNumberTest {
                 .hasMessage("[ERROR] 정수만 입력할 수 있습니다.\n");
     }
 
-    @DisplayName("1~45 이외의 숫자를 입력할 경우 예외가 발생한다.")
+    @DisplayName("45보다 큰 정수를 입력할 경우 예외가 발생한다.")
     @Test
-    void createInputBonusNumberOutOfRange() {
+    void createInputBonusNumberOverOfRange() {
         assertThatThrownBy(() -> new BonusNumber(46, List.of(1,2,3,4,5,6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1~45 사이의 정수만 입력할 수 있습니다.\n");
+    }
+
+    @DisplayName("1보다 작은 정수를 입력할 경우 예외가 발생한다.")
+    @Test
+    void createInputBonusNumberUnderOfRange() {
+        assertThatThrownBy(() -> new BonusNumber(0, List.of(1,2,3,4,5,6)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1~45 사이의 정수만 입력할 수 있습니다.\n");
     }
@@ -28,7 +36,6 @@ public class BonusNumberTest {
     @DisplayName("당첨 번호에 있는 숫자를 보너스 번호로 입력하면 예외가 발생한다")
     @Test
     void createResultNumbersByContainBonusNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new BonusNumber(1, List.of(1, 2, 3, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 당첨 번호에 없는 정수를 입력해야 합니다.\n");

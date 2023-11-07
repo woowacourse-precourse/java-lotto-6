61b6b9e8f97d129a849a59168b9dfd6328891b9e
HZion <hasion96@gmail.com>
2023-11-08 00:44:26 +0900

    입력한 돈 유효성검사

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 public class Application {
+    private int moneyValidate(){
+        int money;
+        while(true){
+           try{
+               System.out.println("구입금액을 입력해 주세요.");
+               money =Integer.valueOf(Console.readLine());
+               if ( (money % 1000) != 0) {
+                   throw new IllegalArgumentException("1,000원 단위로 입력하세요.");
+               }
+               break;
+           }catch (NumberFormatException e){
+               throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력입니다.");
+           }
+        }
+        return money;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int money;
+
+
+        money = moneyValidate();
+
     }
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,4 +17,9 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    //1.임의의 숫자 리스트를 private list에 입력
+    //  해당 매개변수가 list 타입이니 외부에서 돌려서 해당 숫자 입력
+    //2.해당 로또 번호 출력
+    // public Stirng printLotto() 해서 각 로또 번호 출력
+
 }

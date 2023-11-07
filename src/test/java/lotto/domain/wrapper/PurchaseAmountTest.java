503081c69bf3ed920212915ab00f436269fe3b02
devhoya97 <diajbh123@gmail.com>
2023-11-07 15:55:24 +0900

    test(domain): 최대 구입 금액 검사 테스트코드 추가 및 가독성을 위한 수정

--- src/test/java/lotto/domain/wrapper/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/wrapper/PurchaseAmountTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.utils.ErrorMessage;
+import lotto.utils.LottoConstantValue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,32 +22,52 @@ class PurchaseAmountTest {
 
     @DisplayName("구입 금액이 로또 가격으로 나누어 떨어지지 않을 때 예외를 발생시킨다.")
     @ParameterizedTest
-    @ValueSource(ints = {800, 1500, 8010})
+    @ValueSource(ints = {800, 1500})
     void createPurchaseAmountByChangeRemaining(int purchaseAmount) {
         assertThatThrownBy(() -> new PurchaseAmount(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.CHANGE_REMAINING.getWithPrefix());
     }
 
+    @DisplayName("로또를 최대 허용 개수보다 많이 살 수 있는 구입 금액을 받은 경우 예외를 발생시킨다.")
+    @Test
+    void createPurchaseAmountByTooMuchMoney() {
+        // given
+        int samplePurchaseAmount =
+                (LottoConstantValue.LOTTO_PRICE.get() * LottoConstantValue.MAX_NUMBER_OF_LOTTOS.get())
+                        + LottoConstantValue.LOTTO_PRICE.get();
+        // when, then
+        assertThatThrownBy(() -> new PurchaseAmount(samplePurchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.TOO_MUCH_PURCHASE_AMOUNT.getWithFormatAndPrefix(
+                        LottoConstantValue.MAX_NUMBER_OF_LOTTOS.get())
+                );
+    }
+
     @DisplayName("구입 금액으로 구입 가능한 로또 개수를 계산한다.")
     @Test
     void calculateAvailableLottoCount() {
         // given
-        PurchaseAmount purchaseAmount = new PurchaseAmount(8000);
+        int samplePurchaseAmount = LottoConstantValue.LOTTO_PRICE.get() * 8;
+        int expectedLottoCount = 8;
+        PurchaseAmount purchaseAmount = new PurchaseAmount(samplePurchaseAmount);
         // when
         int availableLottoCount = purchaseAmount.getAvailableLottoCount();
         // then
-        assertThat(availableLottoCount).isEqualTo(8);
+        assertThat(availableLottoCount).isEqualTo(expectedLottoCount);
     }
 
     @DisplayName("구입 금액과 상금 비율로 총 수익률을 계산한다.")
     @Test
     void calculateProfitRate() {
         // given
-        PurchaseAmount purchaseAmount = new PurchaseAmount(10000);
+        int samplePurchaseAmount = 10000;
+        long sampleProfit = 5000L;
+        double expectedProfitRate = 50.0;
+        PurchaseAmount purchaseAmount = new PurchaseAmount(samplePurchaseAmount);
         // when
-        double profitRate = purchaseAmount.calculateProfitRate(5000L);
+        double profitRate = purchaseAmount.calculateProfitRate(sampleProfit);
         // then
-        assertThat(profitRate).isEqualTo(50.0);
+        assertThat(profitRate).isEqualTo(expectedProfitRate);
     }
 }

03aa581f74166f3e724cf9d8b585d888d494e185
Jaewon <choijawon99@gmail.com>
2023-11-07 22:06:37 +0900

    feat(game): 구매한 로또 개수&로또 번호 출력 기능 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -2,16 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import lotto.util.GameUtil;
 import lotto.view.View;
 import lotto.util.Validator;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Game {
     private static final int LOTTO_PRICE = 1000;
 
-    private int money;
     private View view;
     private Validator validator;
+
+    private int money;
+    private List<Lotto> purchasedLotto;
+
 
     public Game(){
         view = new View();
@@ -20,6 +28,11 @@ public class Game {
 
     public void play(){
         getMoney();
+
+        view.print_PurchasedLottoNumbers(money/LOTTO_PRICE);
+        purchaseLotto(money/LOTTO_PRICE);
+        view.print_purchasedLotto(purchasedLotto);
+
     }
 
     private void getMoney(){
@@ -39,6 +52,17 @@ public class Game {
             view.print_Exception(e.getMessage());
             return inputMoney();
         }
+    }
+
+    private void purchaseLotto(int purchaseNumber){
+
+        List<Lotto> createdLotto= new ArrayList<>();
+
+        for(int i=0;i<purchaseNumber;i++){
+            createdLotto.add(GameUtil.createLotto());
+        }
+
+        purchasedLotto = createdLotto;
     }
 
 }

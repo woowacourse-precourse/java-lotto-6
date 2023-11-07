ac7ba16c4e55c569638eeffde28ffb49bc29c672
BH kang <rkdqudgjs1127@gmail.com>
2023-11-07 15:26:36 +0900

    #3 feat: 로또 구입 금액 입력 예외처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,13 +2,16 @@ package lotto.controller;
 
 import lotto.model.Lotto;
 import lotto.model.LottoList;
+import lotto.validate.Validate;
 import lotto.view.View;
 
 public class LottoController {
     View view = new View();
     LottoList lottoList = new LottoList();
+    Validate validate = new Validate();
 
     public void start(){
         String s = view.inputBuyingMoney();
+        validate.inputBuyingMoneyValidate(s);
     }
 }

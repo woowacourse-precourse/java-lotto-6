93f7311675f8e05f1003ebf9b54b01edab4757de
SunwuPark <sunwu5678@gmail.com>
2023-11-07 14:00:40 +0900

    UPDATE: Lotto에 distinct 체크 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,8 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        } else if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException();
         }
     }
 

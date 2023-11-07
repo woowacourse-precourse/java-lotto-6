864d912bb2c8683c2715e6781b5a13c08f352488
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 13:06:48 +0900

    feat(InputView): 보너스 번호 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,7 @@ public class InputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "\n보너스 번호를 입력해 주세요.";
     private final Reader reader;
 
     public InputView(Reader reader) {
@@ -17,6 +18,11 @@ public class InputView {
 
     public String readWinningNumbers() {
         System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        return reader.readLine();
+    }
+
+    public String readBonusNumber() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
         return reader.readLine();
     }
 }

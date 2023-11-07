74d08a0dfc3576611f55d0cb3f088fc6c58959f7
sudhdkso <waga1009@gmail.com>
2023-11-08 02:59:32 +0900

    feat: 랜덤 숫자 오름차순으로 정렬하도록 추가

--- src/main/java/lotto/util/RandomNumberGenerator.java
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -5,6 +5,7 @@ import static lotto.domain.enums.LottoConstants.MAX_LOTTO_NUM;
 import static lotto.domain.enums.LottoConstants.MIN_LOTTO_NUM;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator {
@@ -13,6 +14,8 @@ public class RandomNumberGenerator {
     }
 
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_SIZE);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_SIZE);
+        Collections.sort(randomNumbers);
+        return randomNumbers;
     }
 }

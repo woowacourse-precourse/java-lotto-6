8839c216692465bf5a97643f5d16429fa80bcdaf
김강년 <64246800+ekek54@users.noreply.github.com>
2023-11-07 20:39:57 +0900

    feat: 로또를 생성한다
    상세 기능
      - 로또는 6개의 숫자이다
      - 로또는 1~45의 숫자이다
      - 로또는 중복되지 않는 숫자이다
      - 로또는
    오름차순 정렬된다
    
    테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.domain.lotto.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +25,33 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 ~ 45 범위를 초과하면 예외가 발생한다.")
+    @Test
+    void createLottoByExcessNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("로또 번호가 정상적으로 생성된다.")
+    @Test
+    void createLottoSucceed() {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        //when
+        Lotto lotto = new Lotto(numbers);
+        //then
+        Assertions.assertThat(lotto.getNumbers()).isEqualTo(numbers);
+    }
+
+    @DisplayName("로또 번호가 오름차순 정렬되어 생성된다.")
+    @Test
+    void createLottoSorted() {
+        //given
+        List<Integer> numbers = List.of(6, 5, 4, 3, 2, 1);
+        //when
+        Lotto lotto = new Lotto(numbers);
+        //then
+        Assertions.assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
 }

53730124e15a869ffdd66f2f8c44bd2c143f93ff
5jeong <wjdgns4385@naver.com>
2023-11-08 03:26:45 +0900

    [Refactor] 필요없는 클래스 제거

--- src/main/java/lotto/temp.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public class temp {
-    public static void main(String[] args) {
-        double n = 0;
-        n = Math.round(n *100.0) /100.0;
-        System.out.println(n);
-    }
-}

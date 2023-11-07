a5ab1725aba69403541aa0e877af9db3170b19d1
yy8305 <yy8305@kakao.com>
2023-11-07 17:25:34 +0900

    test(ConsolePrinter): 로또 번호 출력 기능 테스트 코드 추가

--- src/test/java/lotto/util/ConsolePrinterTest.java
+++ src/test/java/lotto/util/ConsolePrinterTest.java
@@ -1,12 +1,16 @@
 package lotto.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import static org.junit.jupiter.params.provider.EnumSource.Mode.INCLUDE;
 
+import java.util.List;
 import lotto.TestDefault;
 import lotto.constants.PrintMessages;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -32,7 +36,7 @@ class ConsolePrinterTest extends TestDefault {
     @DisplayName("출력할 메시지가 null일 경우 예외 처리가 발생한다.")
     @ParameterizedTest
     @NullSource
-    void testShowGeneralMessageNullCheck(PrintMessages message) {
+    void testShowGeneralMessageNullExceptionCheck(PrintMessages message) {
         assertThatNullPointerException().isThrownBy(() -> {
             ConsolePrinter.showMessage(message);
         });
@@ -67,7 +71,7 @@ class ConsolePrinterTest extends TestDefault {
             "SECOND_PLACE_MESSAGE",
             "FIRST_PLACE_MESSAGE",
     })
-    void testShowMessageWithNumberNullCheck(PrintMessages message) {
+    void testShowMessageWithNumberNullExceptionCheck(PrintMessages message) {
         assertThatNullPointerException().isThrownBy(() -> {
             ConsolePrinter.showMessageWithNumber(message, null);
         });
@@ -76,9 +80,39 @@ class ConsolePrinterTest extends TestDefault {
     @DisplayName("한개의 숫자(int형)가 포함된 문자열 출력시 메시지가 null 일경우 예외처리가 발생한다.")
     @ParameterizedTest
     @NullSource
-    void testShowMessageWithNumberMessageNullCheck(PrintMessages message) {
+    void testShowMessageWithNumberMessageNullExceptionCheck(PrintMessages message) {
         assertThatNullPointerException().isThrownBy(() -> {
             ConsolePrinter.showMessageWithNumber(message, 1);
+        });
+    }
+
+    @DisplayName("로또 번호 6자리가 정상적으로 출력된다.")
+    @Test
+    void testShowLottoNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        ConsolePrinter.showLottoNumbers(lotto);
+
+        assertThat(getConsoleOuputMessage())
+                .isEqualTo(PrintMessages.LOTTO_NUMBERS.getMessageWithLotto(lotto));
+    }
+
+    @DisplayName("로또 번호 출력시 null을 전달할 경우 예외가 발생한다.")
+    @Test
+    void testShowLottoNumbersNullExceptionCheck() {
+        Lotto lotto = null;
+
+        assertThatNullPointerException().isThrownBy(() -> {
+            ConsolePrinter.showLottoNumbers(lotto);
+        });
+    }
+
+    @DisplayName("로또 번호 출력시 번호 개수가 다를 경우 예외가 발생한다.")
+    @Test
+    void testShowLottoNumbersLengthExceptionCheck() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Lotto lotto = new Lotto(List.of(1, 2, 3));
+            ConsolePrinter.showLottoNumbers(lotto);
         });
     }
 }

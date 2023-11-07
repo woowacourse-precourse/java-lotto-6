dc35e7ea6ef152a672268b9d719f89a4ec219971
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-07 17:39:38 +0900

    feat: 로또 번호가 중복이 있는지 검사한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import static lotto.util.ErrorMessage.ERROR_LOTTO_DUPLICATE;
+import static lotto.util.ErrorMessage.ERROR_LOTTO_SIZE;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +17,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_LOTTO_SIZE);
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicate = new HashSet<>(numbers);
+        if (nonDuplicate.size() != 6) {
+            throw new IllegalArgumentException(ERROR_LOTTO_DUPLICATE);
+        }
+    }
 }
---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,6 @@
+package lotto.util;
+
+public class ErrorMessage {
+    public static final String ERROR_LOTTO_SIZE = "번호는 6개여야 합니다.";
+    public static final String ERROR_LOTTO_DUPLICATE = "중복된 숫자가 존재합니다.";
+}

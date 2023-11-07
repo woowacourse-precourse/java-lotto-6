3d23addf3e682119c9e87c38bb9d68b421e0df74
jacomyou1026 <jacomyou1026@naver.com>
2023-11-07 22:30:58 +0900

    feat  : 로또 리스트 구하기

--- src/main/java/lotto/controller/Application.java
+++ src/main/java/lotto/controller/Application.java
@@ -1,8 +1,15 @@
 package lotto.controller;
 
+import lotto.domin.Lotto;
+import lotto.domin.LottoNumbers;
 import lotto.domin.PlayerLottoAmount;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.view.OutputView.PrintLottoNumbers;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,6 +18,12 @@ public class Application {
         playerLottoAmount = getPlayerLottoAmount(playerLottoAmount);
         int count = playerLottoAmount.calculateLottoCount();
         OutputView.printTicketCount(count);
+
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = null;
+            getLottoList(lottoList, lotto);
+        }
 
 
     }
@@ -24,5 +37,26 @@ public class Application {
             }
         }
     }
+
+    private static void getLottoList(List<Lotto> lottoList, Lotto lotto) {
+        while (true) {
+            try {
+                lotto = makeLotto();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        lottoList.add(lotto);
+    }
+
+    public static Lotto makeLotto() throws IllegalArgumentException {
+        List<Integer> numbers = new LottoNumbers().LottoNumbers();
+        PrintLottoNumbers(numbers);
+        return new Lotto(numbers);
+    }
+
+
 
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     public static final String TICKET_COUNT = "개를 구매했습니다.";
     public static void printTicketCount(int count) {
         System.out.println(count + TICKET_COUNT);
     }
+    public static void PrintLottoNumbers(List<Integer> lotto) {
+        System.out.println(lotto);
+    }
+
 
 }

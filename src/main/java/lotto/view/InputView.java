9845717a4371aa8d1de074d051805752cb630564
DongHwi Kim <ken7905@ajou.ac.kr>
2023-11-07 13:54:14 +0900

    feat: create inputview

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,25 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public String enterBuyLottoAmount(){
+        System.out.println(INPUT_AMOUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String enterPrizeLottoNum(){
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String enterBonusNum(){
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
 }

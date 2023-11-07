d8931c8798b8cefdcd8a94145328e214c2b1e086
KangDeok <rkdejr2321@naver.com>
2023-11-07 22:45:25 +0900

    [feat] 로또 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.UserLotto;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
+    static LottoController lottoController = new LottoController(new LottoService(), new InputView(), new OutputView(new StringBuilder()));
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        lottoController.run();
     }
 }

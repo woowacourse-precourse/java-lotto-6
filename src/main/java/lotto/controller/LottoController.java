aa5ceeebd72f13b4b9ca593fdfc37cbe192ac4bb
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-11-07 18:10:59 +0900

    feat: add contructor to lotto controller

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,14 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
 }

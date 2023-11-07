c7a0995732c4970a7f3c0a639e41d1ea0205ba39
twoosky <lhjae3@naver.com>
2023-11-08 08:01:34 +0900

    feat: 구매 금액 입력 뷰 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+import static lotto.utils.RepeatReader.read;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.IntegerConverter;
+
+public class InputView {
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "%n당첨 번호를 입력해 주세요.%n";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "%n보너스 번호를 입력해 주세요.%n";
+
+    public static int inputMoney() {
+        System.out.println(INPUT_MONEY_MESSAGE);
+        return read(() -> IntegerConverter.convert(Console.readLine()));
+    }
+
+
+}

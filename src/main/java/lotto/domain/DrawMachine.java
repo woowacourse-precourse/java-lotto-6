f10612d6421842f806ff80ad93d7f0879a58ab98
Jiihyun <cwlgusc@gmail.com>
2023-11-08 03:40:57 +0900

    feat(DrawMachine): 수익률 계산하기

--- src/main/java/lotto/domain/DrawMachine.java
+++ src/main/java/lotto/domain/DrawMachine.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import java.util.Map;
+
 import static lotto.domain.DrawResult.getResult;
+import static lotto.domain.LottoStore.LOTTO_PRICE;
 
 public class DrawMachine {
     private final WinningLottoNumbers winningLottoNumbers;
@@ -13,5 +16,23 @@ public class DrawMachine {
         int matchingCount = winningLottoNumbers.getMatchingCount(userLotto);
         boolean containBonusNumber = winningLottoNumbers.containBonusNumber(userLotto);
         return getResult(matchingCount, containBonusNumber);
+    }
+
+    private double getRateOfReturn(Map<DrawResult, Integer> statistic, int size) {
+        double spentAmount = size * LOTTO_PRICE;
+        double prizeAmount = getPrizeAmount(statistic);
+
+        double rateOfReturn = prizeAmount / spentAmount * 100;
+        return Math.round(rateOfReturn * 10.0) / 10.0;
+    }
+
+    private double getPrizeAmount(Map<DrawResult, Integer> statistic) {
+        double prizeAmount = 0;
+        for (Map.Entry<DrawResult, Integer> entry : statistic.entrySet()) {
+            DrawResult drawResult = entry.getKey();
+            Integer count = entry.getValue();
+            prizeAmount += drawResult.getPrizeAmount() * count;
+        }
+        return prizeAmount;
     }
 }

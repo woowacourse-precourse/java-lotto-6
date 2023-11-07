53ba26e540f0abf34c2f08b60d5d1b03ac441f7b
jacomyou1026 <jacomyou1026@naver.com>
2023-11-07 22:25:05 +0900

    feat  :getPlayerLottoAmount()를 통해 사용자 로또 금액 유효성 검사

--- src/main/java/lotto/controller/Application.java
+++ src/main/java/lotto/controller/Application.java
@@ -1,7 +1,25 @@
 package lotto.controller;
 
+import lotto.domin.PlayerLottoAmount;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayerLottoAmount playerLottoAmount = null;
+        playerLottoAmount = getPlayerLottoAmount(playerLottoAmount);
+
+
     }
+
+    private static PlayerLottoAmount getPlayerLottoAmount(PlayerLottoAmount playerLottoAmount) {
+        while (true) {
+            try {
+                return playerLottoAmount = new PlayerLottoAmount(InputView.inputPlayPay());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }

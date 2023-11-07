a90f5bfefe6942143c7e86e742b400c7948dab29
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-07 16:23:18 +0900

    feat(Lotto): 스켈레톤 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -7,14 +7,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        sortNumbers(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoNumberRange(numbers);
+        validateDuplicateNumber(numbers);
+        validateLottoNumbersSize(numbers);
+    }
+
+    private void validateLottoNumberRange(List<Integer> numbers) {
+
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+
+    }
+
+    private void validateLottoNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void sortNumbers(List<Integer> numbers) {}
+
 }

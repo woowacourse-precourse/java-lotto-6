5a7bbff9227cac80b36881c04702158732dacf78
KangDeok <rkdejr2321@naver.com>
2023-11-07 23:45:49 +0900

    [refactor] 매직 넘버 상수로 변환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -10,12 +10,24 @@ import lotto.domain.UserLotto;
 
 public class LottoService {
 
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final String LOTTO_NUMBER_SEPARATOR = ",";
+    private static final int FIRST_WINNER_HIT_COUNT = 6;
+    private static final int SECOND_WINNER_HIT_COUNT = 5;
+    private static final int THIRD_WINNER_HIT_COUNT = 5;
+    private static final int FORTH_WINNER_HIT_COUNT = 4;
+    private static final int FIFTH_WINNER_HIT_COUNT = 3;
+    private static final int PRICE_PER_LOTTO = 1000;
+
+
     public int calculateLottoSheet(int price) {
-        if (price % 1000 != 0) {
+        if (price % PRICE_PER_LOTTO != 0) {
             throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매해야 합니다. 다시 입력해주세요.");
         }
 
-        return price / 1000;
+        return price / PRICE_PER_LOTTO;
     }
 
     public List<Lotto> generateLotto(int lottoQuantity) {
@@ -53,23 +65,23 @@ public class LottoService {
     }
 
     private static LottoRank getLottoRank(long hitCount, boolean isBonusNumberContain) {
-        if (hitCount == 6) {
+        if (hitCount == FIRST_WINNER_HIT_COUNT) {
             return LottoRank.FIRST;
         }
 
-        if (hitCount == 5 && isBonusNumberContain) {
+        if (hitCount == SECOND_WINNER_HIT_COUNT && isBonusNumberContain) {
             return LottoRank.SECOND;
         }
 
-        if (hitCount == 5) {
+        if (hitCount == THIRD_WINNER_HIT_COUNT) {
             return LottoRank.THIRD;
         }
 
-        if (hitCount == 4) {
+        if (hitCount == FORTH_WINNER_HIT_COUNT) {
             return LottoRank.FOURTH;
         }
 
-        if (hitCount == 3) {
+        if (hitCount == FIFTH_WINNER_HIT_COUNT) {
             return LottoRank.FIFTH;
         }
 
@@ -77,7 +89,7 @@ public class LottoService {
     }
 
     public UserLotto purchaseLotto(String inputLottoNumbers, String inputBonusNumber) {
-        List<Integer> lottoNumbers = Arrays.stream(inputLottoNumbers.split(","))
+        List<Integer> lottoNumbers = Arrays.stream(inputLottoNumbers.split(LOTTO_NUMBER_SEPARATOR))
                 .map(Integer::parseInt)
                 .toList();
 
@@ -109,7 +121,7 @@ public class LottoService {
     }
 
     private static List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
         Collections.sort(lottoNumbers);
 
         return lottoNumbers;

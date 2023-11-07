e144df439fda60015a42a94f09854dfa4f337399
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-07 16:12:08 +0900

    docs - 제공해준 테스트코드로 이전

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +31,32 @@ class LottoTest {
     void createLottoByNumbersRange() {
         assertThatThrownBy(() -> new Lotto(List.of(46, 1, 0, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또를 생성할때 값이 유효한지 검증한다. 유효하지 않으면 exception을 발생시킨다")
+    @Test
+    void generateLotto() {
+        Assertions.assertAll(
+                () -> Assertions.assertDoesNotThrow(
+                        () -> new Lotto(List.of(8, 21, 23, 41, 42, 43))),
+                () -> Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Lotto(List.of(8, 21, 23, 41, 42, 46))),
+                () -> Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Lotto(List.of(8, 21, 23, 41, 42, 43, 41))),
+                () -> Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Lotto(List.of(8, 21, 23, 41, 42, 42))),
+                () -> Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Lotto(List.of(8, 21, 23, 41, 42)))
+        );
+    }
+
+    @DisplayName("불변 Collection을 반환한다.")
+    @Test
+    void getLotto(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+
+        List<Integer> copyLotto = lotto.getNumbers();
+
+        Assertions.assertThrows(UnsupportedOperationException.class, ()-> copyLotto.add(1));
     }
 }

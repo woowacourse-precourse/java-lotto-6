2cf416acfc17d0c553ecdad259e2784d50bc9da9
ycis5013@naver.com <ycis5013@naver.com>
2023-11-08 07:37:38 +0900

    feat(InputView) : 보너스 번호를 입력 받는 함수 inputBonusNumber() 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -25,4 +25,9 @@ public class InputView {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
+
+    public String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }

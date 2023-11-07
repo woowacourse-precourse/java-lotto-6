dea87e7d6000c0bbdb0248874abd2c076bb0aa61
mingeun0507 <tqdlfkg@naver.com>
2023-11-08 04:42:24 +0900

    fix: ErrorConst 내용 수정

--- src/main/java/lotto/constants/ErrorConsts.java
+++ src/main/java/lotto/constants/ErrorConsts.java
@@ -2,8 +2,7 @@ package lotto.constants;
 
 public enum ErrorConsts {
     LOTTO_NUMBER_OUT_OF_RANGE("로또 번호는 1 ~ 45 사이의 숫자만 가능합니다."),
-    LOTTO_NUMBERS_DUPLICATED("로또 번호에 중복된 숫자가 있습니다."),
-    LOTTO_NUMBERS_SIZE_NOT_MATCH("로또 번호는 6개만 가능합니다."),
+    LOTTO_NUMBERS_SIZE_NOT_MATCH("로또 번호는 6개만 가능합니다. 입력한 숫자의 개수와 중복 여부를 확인해주세요."),
     MONEY_NOT_DIVIDED_BY_LOTTO_PRICE("로또 구입 금액은 로또 가격의 배수여야 합니다."),
     BONUS_NUMBER_DUPLICATED("보너스 번호는 로또 번호와 중복될 수 없습니다."),
 

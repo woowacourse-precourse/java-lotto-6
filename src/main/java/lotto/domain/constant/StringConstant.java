12308a2cb1fe0da978ceaee8ce96e4504d29b41d
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-07 23:46:31 +0900

    refactor: 테스트 케이스 상수화

--- src/main/java/lotto/domain/constant/StringConstant.java
+++ src/main/java/lotto/domain/constant/StringConstant.java
@@ -8,13 +8,9 @@ public class StringConstant {
 
 
     // TEST CASE
-    public static final String TEST_EXPENSE_TYPE = "가나다라";
-    public static final String TEST_EXPENSE_DIVIDE = "12345";
-    public static final String TEST_WINNUMBERS_TYPE = "3,5,다,1,";
-    public static final String TEST_WINNUMBERS_SORT = "6,5,4,3,2,1";
-    public static final String TEST_BONUSNUMBER_TYPE = "가나다라";
-    public static final String TEST_BONUSNUMBER_RANGE = "46";
-    public static final String TEST_COMPUTE_EXPENSE = "1000";
+    public static final String TEST_STANDARD_EXPENSE = "1000";
+    public static final String TEST_STANDARD_WINNINGLOTTO = "1,2,3,4,5,6";
+    public static final String TEST_STANDARD_BONUSNUM = "7";
 
 
     // OUTPUT SERVICE

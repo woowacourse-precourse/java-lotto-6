2d22abf04513a508834876ef19dd7854b1d06513
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-07 11:59:47 +0900

    feat: Output메세지 enum클래스 생성

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,9 +1,11 @@
-package lotto.validator;
+package lotto.view;
 
 public enum OutputMessage {
     PURCHASE_PRICE_MESSAGE("구입금액을 입력해 주세요."),
     PURCHASE_INTRODUCE_MESSAGE("개를 구매했습니다."),
-    
+    GET_LOTTO_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    GET_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요,"),
+    RESULT_MESSAGE("당첨 통계\n---")
     ;
     private String message;
 

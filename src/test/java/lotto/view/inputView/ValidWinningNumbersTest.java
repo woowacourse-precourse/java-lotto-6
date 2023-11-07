2840591d2a3107dcf97cb9bfd49954be99639181
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-08 05:01:23 +0900

    refactor: assertThat -> assertThatThrownBy 로 수정 및 display name 변경

--- src/test/java/lotto/view/inputView/ValidWinningNumbersTest.java
+++ src/test/java/lotto/view/inputView/ValidWinningNumbersTest.java
@@ -1,6 +1,6 @@
 package lotto.view.inputView;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.utils.Validation;
 import lotto.utils.ValidationImpl;
@@ -9,44 +9,49 @@ import org.junit.jupiter.api.Test;
 
 public class ValidWinningNumbersTest {
 
-    @DisplayName("당첨 번호 입력에 숫자와 쉼표 이외 문자가 있으면 false 를 반환한다.")
+    @DisplayName("당첨 번호 입력에 숫자와 쉼표 이외 문자가 있으면 IllegalArgumentException 을 발생시킨다.")
     @Test
     void test1() {
         Validation validation = new ValidationImpl();
         String input = "1,2,3,(,d,asd,29";
-        assertThat(validation.isValidWinningNumbers(input)).isFalse();
+        assertThatThrownBy(() -> validation.isValidWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("당첨 번호 입력에 숫자와 쉼표만으로 이루어져 있으면 true 를 반환한다.")
+    @DisplayName("당첨 번호의 갯수는 6개이다.")
     @Test
     void test2() {
         Validation validation = new ValidationImpl();
         String input = "1,2,3,4,5";
-        assertThat(validation.isValidWinningNumbers(input)).isTrue();
+        assertThatThrownBy(() -> validation.isValidWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("쉼표 사이에 숫자가 없으면 false 를 반환한다.")
+    @DisplayName("쉼표 사이에 숫자가 없으면 IllegalArgumentException 을 발생시킨다.")
     @Test
     void test3() {
         Validation validation = new ValidationImpl();
         String input = "1,2,3,4,5,,999,";
-        assertThat(validation.isValidWinningNumbers(input)).isFalse();
+        assertThatThrownBy(() -> validation.isValidWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("1보다 작으면 false 를 반환한다.")
+    @DisplayName("번호가 1보다 작으면 IllegalArgumentException 을 발생시킨다.")
     @Test
     void test4() {
         Validation validation = new ValidationImpl();
         String input = "1,2,3,4,-1";
-        assertThat(validation.isValidWinningNumbers(input)).isFalse();
+        assertThatThrownBy(() -> validation.isValidWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("45보다 크면 false 를 반환한다.")
+    @DisplayName("번호가 45보다 크면 IllegalArgumentException 을 발생시킨다.")
     @Test
     void test5() {
         Validation validation = new ValidationImpl();
         String input = "1,2,3,4,99";
-        assertThat(validation.isValidWinningNumbers(input)).isFalse();
+        assertThatThrownBy(() -> validation.isValidWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 

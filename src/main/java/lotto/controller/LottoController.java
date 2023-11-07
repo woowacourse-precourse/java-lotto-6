a72c317e2f287ea578029b73c675b0f73e8d9d85
Seyeon <wjdtpdus828@naver.com>
2023-11-07 21:48:10 +0900

    feat(LottoController): 보너스 번호를 받는 코드와 당첨 결과를 구하는 메소드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,10 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Bonus;
 import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
+import lotto.service.LottoService;
 import lotto.utils.Parser;
 import lotto.view.LottoView;
 
@@ -13,23 +16,42 @@ public class LottoController {
     private LottoView lottoView = new LottoView();
     private InitLottoController initLottoController = new InitLottoController();
     private WinningNumberController winningNumberController = new WinningNumberController();
+    private BonusNumberController bounusNumberController = new BonusNumberController();
 
     private int ticketNumber;
-    private List<Lotto> lotts  = new ArrayList<>();
+    private PurchaseAmount purchaseAmount;
+    private List<Lotto> lottos  = new ArrayList<>();
+    private int[] winResult;
+    private LottoService lottoService = new LottoService();
     public void startLotto(){
         purchaseLotto();
-        inputWinningNumber();
+        Lotto winningNumber = inputWinningNumber();
+        Bonus bonus = inputBounusNumber(winningNumber);
+        processWinningResult(winningNumber,lottos,bonus);
     }
 
     public void purchaseLotto(){
-        ticketNumber = purchaseController.runPurchaseAmount();
+        purchaseAmount = purchaseController.runPurchaseAmount();
+        ticketNumber = purchaseAmount.getTicketNum();
         lottoView.outputPurchaseAmountView(ticketNumber);
-        lotts = initLottoController.runInitLotto(ticketNumber);
-        lottoView.printPurchaseLotto(lotts);
+        lottos = initLottoController.runInitLotto(ticketNumber);
+        lottoView.printPurchaseLotto(lottos);
     }
 
-    public void inputWinningNumber(){
+    public Lotto inputWinningNumber(){
+        return winningNumberController.runWinningNumber();
+    }
 
-        Lotto winningNumber = winningNumberController.runWinningNumber();
+    public Bonus inputBounusNumber(Lotto winningNumber){
+        return bounusNumberController.runBonusNumber(winningNumber);
+    }
+    public void processWinningResult(Lotto winningNumber,List<Lotto> lottos, Bonus bonus){
+        List<Integer> countWinningNum;
+        String winRate;
+        countWinningNum = lottoService.calcWinningResult(winningNumber, lottos);
+        winResult = lottoService.calcRanking(countWinningNum, winningNumber, bonus);
+        winRate = lottoService.calcWinningRate(winResult,purchaseAmount);
+        lottoView.printWinningResult(winResult);
+        lottoView.printRate(winRate);
     }
 }

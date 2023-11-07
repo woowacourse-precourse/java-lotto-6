31c047b7deea97424a91e7cb392cfea545ef43a6
jmj005 <jmj00005@naver.com>
2023-11-08 03:28:00 +0900

    refactor: 기존에 작성했던 LottoTest 코드를 이동

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,17 +1,34 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @ParameterizedTest
     @MethodSource("invalidSize")
     @DisplayName("로또 개수 실패 테스트")
@@ -22,8 +39,8 @@ public class LottoTest {
 
     static Stream<Arguments> invalidSize() {
         return Stream.of(
-                Arguments.of(Arrays.asList(1,2,3,4,5)),
-                Arguments.of(Arrays.asList(39,40,41,42,43,44,45)),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5)),
+                Arguments.of(Arrays.asList(39, 40, 41, 42, 43, 44, 45)),
                 Arguments.of(List.of())
         );
     }
@@ -38,8 +55,8 @@ public class LottoTest {
 
     static Stream<Arguments> validSize() {
         return Stream.of(
-                Arguments.of(Arrays.asList(1,2,3,4,5,6)),
-                Arguments.of(Arrays.asList(40,41,42,43,44,45))
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                Arguments.of(Arrays.asList(40, 41, 42, 43, 44, 45))
         );
     }
 
@@ -53,8 +70,8 @@ public class LottoTest {
 
     static Stream<Arguments> invalidRange() {
         return Stream.of(
-                Arguments.of(Arrays.asList(0,1,2,3,4,5)),
-                Arguments.of(Arrays.asList(-1,0,1,46,47,48))
+                Arguments.of(Arrays.asList(0, 1, 2, 3, 4, 5)),
+                Arguments.of(Arrays.asList(-1, 0, 1, 46, 47, 48))
         );
     }
 
@@ -68,8 +85,8 @@ public class LottoTest {
 
     static Stream<Arguments> validRange() {
         return Stream.of(
-                Arguments.of(Arrays.asList(1,2,3,4,5,6)),
-                Arguments.of(Arrays.asList(40,41,42,43,44,45))
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                Arguments.of(Arrays.asList(40, 41, 42, 43, 44, 45))
         );
     }
 
@@ -83,8 +100,8 @@ public class LottoTest {
 
     static Stream<Arguments> duplicateNumber() {
         return Stream.of(
-                Arguments.of(Arrays.asList(1,1,1,1,1,1)),
-                Arguments.of(Arrays.asList(1,2,3,4,5,5))
+                Arguments.of(Arrays.asList(1, 1, 1, 1, 1, 1)),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 5))
         );
     }
 
@@ -98,8 +115,8 @@ public class LottoTest {
 
     static Stream<Arguments> uniqueNumber() {
         return Stream.of(
-                Arguments.of(Arrays.asList(1,2,3,4,5,6)),
-                Arguments.of(Arrays.asList(40,41,42,43,44,45))
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                Arguments.of(Arrays.asList(40, 41, 42, 43, 44, 45))
         );
     }
 }

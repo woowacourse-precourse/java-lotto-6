d9f055d6d2f1c1a7e475dbfb3b58d213ea89c4a4
GaBaljaintheroom <junsu1222@naver.com>
2023-11-07 11:07:35 +0900

    refactor(ProgressMessage): 게임 진행 메시지 상수 추가

--- src/main/java/lotto/constants/ProgressMessage.java
+++ src/main/java/lotto/constants/ProgressMessage.java
@@ -6,6 +6,9 @@ public final class ProgressMessage {
     public static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static final String WINNING_STATISTICS = "당첨 통계\n---";
+    public static final String WINNING_FACTOR_COUNT = "%d개 일치%s(%s원) - %d개";
+    public static final String MATCH_BONUS_BALL = ", 보너스 볼 일치 ";
+    public static final String TOTAL_RATE = "총 수익률은 %s%%입니다.";
 
     private ProgressMessage() {
     }
--- src/main/java/lotto/formatter/GameResultFormatter.java
+++ src/main/java/lotto/formatter/GameResultFormatter.java
@@ -1,17 +1,20 @@
 package lotto.formatter;
 
+import lotto.constants.ProgressMessage;
 import lotto.constants.WinningFactor;
 
 import java.text.NumberFormat;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class LottoStatisticsResultFormatter {
+public class GameResultFormatter {
+    private static final String DELIMITER_ENTER = "\n";
+    private static final String SPACE = " ";
 
     private final Map<WinningFactor, Integer> statistics;
     private final Double totalRate;
 
-    public LottoStatisticsResultFormatter(Map<WinningFactor, Integer> statistics, Double totalRate) {
+    public GameResultFormatter(Map<WinningFactor, Integer> statistics, Double totalRate) {
         this.statistics = statistics;
         this.totalRate = totalRate;
     }
@@ -20,22 +23,22 @@ public class LottoStatisticsResultFormatter {
         return statistics.entrySet().stream()
                 .filter(entry -> entry.getKey() != WinningFactor.NONE_MATCH)
                 .map(entry -> buildStatisticsMessage(entry.getKey(), entry.getValue()))
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining(DELIMITER_ENTER));
     }
 
-    private String buildStatisticsMessage(WinningFactor winningFactor, int count) {
-        return String.format("%d개 일치%s(%s원) - %d개", winningFactor.getCount(),
+    private String buildStatisticsMessage(WinningFactor winningFactor, Integer count) {
+        return String.format(ProgressMessage.WINNING_FACTOR_COUNT, winningFactor.getCount(),
                 bonusMatchMessage(winningFactor), NumberFormat.getInstance().format(winningFactor.getMoney()), count);
     }
 
     private String bonusMatchMessage(WinningFactor winningFactor) {
         if (Boolean.TRUE.equals(winningFactor.isMatchBonusNumber())) {
-            return ", 보너스 볼 일치 ";
+            return ProgressMessage.MATCH_BONUS_BALL;
         }
-        return " ";
+        return SPACE;
     }
 
     public String toTotalRateMessage() {
-        return String.format("총 수익률은 %s%%입니다.", totalRate);
+        return String.format(ProgressMessage.TOTAL_RATE, totalRate);
     }
 }

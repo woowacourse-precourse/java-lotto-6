f9858f41d90dc893b2c4fda86015429761391376
Minyongggg <naminyong97@gmail.com>
2023-11-08 02:55:03 +0900

    feat: WinningNumber getter 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -8,4 +8,8 @@ public class WinningNumber {
     public WinningNumber(List<Integer> numbers) {
         this.numbers = numbers;
     }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

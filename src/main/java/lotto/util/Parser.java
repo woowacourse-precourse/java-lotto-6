056fe1adba263f5853156c1c9282fdc999dcae45
hellomatia <shuaigejp80@gmail.com>
2023-11-07 16:53:41 +0900

    feat(util): 파서 추가
    - 입력 받은 문자가 숫자인지 검증한다.
    - 문자열을 Int로 타입 변환한다.
    - 문자열을 `List<Integer>`로 타입 변환한다.

---
+++ src/main/java/lotto/util/Parser.java
@@ -0,0 +1,41 @@
+package lotto.util;
+
+import java.util.List;
+
+import static lotto.exception.LottoException.INPUT_ONLY_NUMBER;
+
+public class Parser {
+
+    private static final String LIST_DELIMITER = ",";
+    private static final String EMPTY = "";
+
+    private Parser() {
+    }
+
+    public static List<Integer> stringToIntList(final String string) {
+        List<String> strings = List.of(string.split(LIST_DELIMITER));
+        strings.forEach(Parser::validateInteger);
+        return strings.stream()
+                .map(Parser::stringToInt)
+                .toList();
+    }
+
+    private static void validateInteger(final String string) {
+        if (isNotInteger(string) || isEmpty(string)) {
+            INPUT_ONLY_NUMBER.create();
+        }
+    }
+
+    private static boolean isNotInteger(final String string) {
+        return !string.matches("^[\\d]*$");
+    }
+
+    private static boolean isEmpty(final String string) {
+        return string.equals(EMPTY);
+    }
+
+    public static int stringToInt(final String string) {
+        validateInteger(string);
+        return Integer.parseInt(string);
+    }
+}

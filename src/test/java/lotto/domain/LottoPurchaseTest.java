f2e92a697c1165788b2f0c3b53d07e1e3c2e10f8
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:32:34 +0900

    test(LottoPurchaseTest): 발행횟수 입력에 따른 발행 테스트
    - 투입금액에 관한 검증은 Money 객체에서 진행하므로 금액 검증 메소드 삭제

--- src/test/java/lotto/domain/LottoPurchaseTest.java
+++ src/test/java/lotto/domain/LottoPurchaseTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoPurchaseTest {
     @DisplayName("로또 1개를 발행한다.")
@@ -16,11 +15,11 @@ public class LottoPurchaseTest {
     void purchaseUnitLotto() {
         // given
         long money = 1000;
-        long purchaseCount = money / 1000;
+        int purchaseCount = (int) money / 1000;
         List<Lotto> lottos;
 
         // when
-        lottos = LottoPurchase.buyLotto(money);
+        lottos = LottoPurchase.buyLotto(purchaseCount);
 
         // then
         assertThat(lottos.size()).isEqualTo(purchaseCount);
@@ -31,31 +30,13 @@ public class LottoPurchaseTest {
     @ValueSource(longs = {3000, 5000, 70000, 140000})
     void purchaseManyLotto(long money) {
         // given
-        long purchaseCount = money / 1000;
+        int purchaseCount = (int) money / 1000;
         List<Lotto> lottos;
 
         // when
-        lottos = LottoPurchase.buyLotto(money);
+        lottos = LottoPurchase.buyLotto(purchaseCount);
 
         // then
         assertThat(lottos.size()).isEqualTo(purchaseCount);
-    }
-
-    @DisplayName("투입금액이 1000원 이하면 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(longs = {0, 100, 500, 900, 999})
-    void lackMoneyException(long money) {
-        assertThatThrownBy(() -> LottoPurchase.buyLotto(money))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("금액은 1,000원 이상 투입 되어야 합니다.");
-    }
-
-    @DisplayName("투입금액이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(longs = {1001, 1500, 9700, 15000006})
-    void wrongMoneyException(long money) {
-        assertThatThrownBy(() -> LottoPurchase.buyLotto(money))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("금액은 1,000원 단위로 투입 되어야 합니다.");
     }
 }

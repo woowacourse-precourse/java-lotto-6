d6c6edb22db9b2831d7a018527b2455a87b475eb
jacomyou1026 <jacomyou1026@naver.com>
2023-11-07 22:35:17 +0900

    feat  : 1~45숫자로 입력하였는지 체크

--- src/main/java/lotto/domin/Exception.java
+++ src/main/java/lotto/domin/Exception.java
@@ -13,7 +13,12 @@ public  class Exception {
             throw new IllegalArgumentException(ExceptionMessage.INPUT_TYPE_ERROR.getExceptionValue());
         }
     }
-
+    //1~45
+    public static void overNumber(int num) throws IllegalArgumentException{
+        if (num < 1 || num > 45) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_RANGE.getExceptionValue());
+        }
+    }
 
 
 

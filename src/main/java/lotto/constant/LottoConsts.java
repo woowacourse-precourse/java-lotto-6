cee5481df0ac7d026396955596e6d25f30095f92
PeterChoi <94peterchoi@gmail.com>
2023-11-07 22:42:15 +0900

    feat: 로또 관련 상수를 관리하는 클래스 추가

---
+++ src/main/java/lotto/constant/LottoConsts.java
@@ -0,0 +1,9 @@
+package lotto.constant;
+
+public class LottoConsts {
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int REQUIRED_COUNT = 6;
+
+}

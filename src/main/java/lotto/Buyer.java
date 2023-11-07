a679c555148e25a03926b93b810694c39bd770d1
keongmiiin99 <smartmina7@naver.com>
2023-11-08 02:55:02 +0900

    feat: 구매한 금액만큼 로또 발행하는 기능 추가

--- src/main/java/lotto/Buyer.java
+++ src/main/java/lotto/Buyer.java
@@ -1,17 +1,35 @@
 package lotto;
 
+import java.util.List;
+
 public class Buyer {
     private int amount;
+    private List<Lotto> lottoTickets;
 
-    public Buyer(){
+    public Buyer() {
         String amountInput = BuyerInfoInput.inputPurchaseAmount();
         validateAmount(amountInput);
         this.amount = Integer.parseInt(amountInput);
     }
 
-    public void validateAmount(String amount){
+    public int getAmount() {
+        return amount;
+    }
+
+    public void validateAmount(String amount) {
         Validator.isValidAmount(amount);
         Validator.isAmountDivisibleBy1000(amount);
     }
+
+    // 구매한 금액만큼 로또 발행
+    public void purchaseLottoTicket(int amount){
+
+        int lottoTicketCount = amount / 1000;
+        for (int i=0; i<amount; i++){
+            Lotto lotto = new Lotto(LottoGenerator.generateLottoNumber());
+            lottoTickets.add(lotto);
+        }
+    }
+
 
 }

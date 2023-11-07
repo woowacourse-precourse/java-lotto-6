227d4abcf93f14039e12eb539c35a74868483f82
jhva <junghun8158@naver.com>
2023-11-07 13:34:15 +0900

    :sparkles: feat(LottoConstants):로또 상수 메시지 추가

--- src/main/java/lotto/constants/LottoConstants.java
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -7,4 +7,7 @@ public class LottoConstants {
     public static final int LOTTO_MIN_LENGTH = 1;
     public static final int LOTTO_DIVISION = 1000;
     public static final int LOTTO_NOT_DIVISION = 0;
+    public static final String WINNING = "당첨";
+    public static final String BONUS = "보너스";
+    public static final String DIVIDER = "---";
 }

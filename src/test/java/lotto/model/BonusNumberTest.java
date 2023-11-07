8c9fde0411e49865d14d8cc416282ba7786205ae
HongYeseul <ghd8119@naver.com>
2023-11-07 16:04:43 +0900

    :white_check_mark: test(BonusNumber): 보너스 번호 유효값 검사 테스트 추가 - 보너스 번호가 1~45의 값을 가지지 않을 때 예외 발생 - 1등 로또 번호와 중복된 보너스 번호 입력시 예외 발생

---
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BonusNumberTest {
+    WinningNumber winningNumber = new WinningNumber("1,2,3,4,5,6");
+    @DisplayName("보너스 번호가 1~45의 값을 가지지 않을 때 예외 발생")
+    @Test
+    void createBonusNumberInvalidRange() {
+        assertThatThrownBy(() -> new BonusNumber(winningNumber, "0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1등 로또 번호와 중복된 보너스 번호 입력시 예외 발생")
+    @Test
+    void createBonusNumberByDuplicateNumber() {
+        assertThatThrownBy(() -> new BonusNumber(winningNumber, "3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

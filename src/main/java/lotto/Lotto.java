806e1f1d74d27bc05921d34a9379725611df9f7d
i3kae <snoger1234@gmail.com>
2023-11-07 18:03:38 +0900

    feat : 몇개의 숫자가 당첨 번호와 같은지 계산하는 기능 구현 1. 당첨 번호와 같은 숫자의 갯수를 계산해서 반환하고 당첨 번호 5개와 보너스까지 같은 경우 7을 반환

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -18,4 +18,21 @@ public class Lotto {
     public void printLotto(){
         Print.printLotto(numbers);
     }
+
+    public int calcMatchNumber(List<Integer> winningNumber, Integer bonumNumber){
+        int[] lottoMap = new int[46];
+        int matchNumber = 0;
+        for (int i = 0; i < 6; i++){
+            lottoMap[numbers.get(i)] = 1;
+        }
+        for (int i = 0; i < 6; i++){
+            if (lottoMap[winningNumber.get(i)] == 1){
+                matchNumber++;
+            }
+        }
+        if (matchNumber == 5 || lottoMap[bonumNumber] == 1){
+            return 7;
+        }
+        return matchNumber;
+    }
 }

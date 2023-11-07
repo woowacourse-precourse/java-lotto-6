9cc2ea1375b19a8935d6fede1c1ea2ed1cc27e9a
Yang woosung <ngxk1217@gmail.com>
2023-11-07 17:18:32 +0900

    3주차 구현 기능에 대한 list를 작성했습니다.

---
+++ src/main/java/lotto/constant/LotteNumber.java
@@ -0,0 +1,21 @@
+package lotto.constant;
+
+
+public enum LotteNumber {
+
+
+    FIRST_RANGE(1)
+        , LAST_RANGE(45)
+        ,PURCHASE_AMOUNT_COND(1000)
+        ;
+
+    private final int number;
+
+    LotteNumber(int number){
+        this.number=number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}

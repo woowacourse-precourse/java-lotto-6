328e4a2d930675940b556c674ef7951139d57ffa
sinyebin <dpqls0625@naver.com>
2023-11-07 21:24:22 +0900

    feat: 보너스 번호 입력 (예외사항) 중복된 숫자가 있을때

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.view.ErrorMessage;
+
 public class WinningLotto {
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
@@ -8,9 +10,14 @@ public class WinningLotto {
     private final int bonusNumber;
 
     public WinningLotto(Lotto winningLotto, int bonusNumber) {
+        validateNumberRange(bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-
+    private void validateNumberRange(int bonusNumber) {
+        if (bonusNumber < MIN_RANGE || bonusNumber > MAX_RANGE) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_VALID_NUMBER);
+        }
+    }
 }

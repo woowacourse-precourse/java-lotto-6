bb1196e6aea21d91971c62b98ac2df3e74241ddd
taewan625 <taewan1997625@gmail.com>
2023-11-07 22:09:32 +0900

    Feat: Add InputView
    buyLootoPrice() : 구매 가격
    winningNumber() : 당첨번호
    bonusNumber() : 보너스 번호

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String buyLottoPrice() {
+        return Console.readLine();
+    }
+
+    public static String winningNumber() {
+        return Console.readLine();
+    }
+
+    public static String bonusNumber() {
+        return Console.readLine();
+    }
+}

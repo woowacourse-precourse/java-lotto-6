3bbb8f313318c9edb819526704e5878de3478143
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 02:34:40 +0900

    [ADD] 게임 시작

--- src/main/java/lotto/control/Lottogame.java
+++ src/main/java/lotto/control/Lottogame.java
@@ -1,2 +1,17 @@
 package lotto.control;public class Lottogame {
+  Lottogameview view = new Lottogameview();
+
+    public void runLottoGame() {
+        int purchaseAmount = inputPurchaseAmount();
+        List<Integer> winningNumbers = inputWinningNumbers();
+        int bonusNumber = inputBonusNumber();
+        List<Lotto> lottos = generateLottos(purchaseAmount);
+        view.printPurchasedLottosCount(lottos.size());
+        view.printPurchasedLottos(lottos);
+
+
+        int[] winCounts = checkWinCounts(lottos, winningNumbers, bonusNumber);
+
+        view.printWinningResult(winCounts, purchaseAmount);
+    }
 }

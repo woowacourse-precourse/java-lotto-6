47daec5d55f5f1ffc724f80bdb6ccce4094beae8
peeaaccee <wlgp8958@naver.com>
2023-11-07 14:28:25 +0900

    style(Lotto): massage -> message 오타 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private static final String VALIDATE_COUNT_MASSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
-    private static final String VALIDATE_RANGE_MASSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String VALIDATE_COUNT_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String VALIDATE_RANGE_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String VALIDATE_DUPLICATION_MESSAGE = "[ERROR] 중복된 숫자가 있습니다.";
     private final List<Integer> numbers;
 
@@ -23,14 +23,14 @@ public class Lotto {
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(VALIDATE_COUNT_MASSAGE);
+            throw new IllegalArgumentException(VALIDATE_COUNT_MESSAGE);
         }
     }
 
     private void validateRange(List<Integer> numbers) {
         for (int a : numbers) {
             if (a < 1 || a > 45) {
-                throw new IllegalArgumentException(VALIDATE_RANGE_MASSAGE);
+                throw new IllegalArgumentException(VALIDATE_RANGE_MESSAGE);
             }
         }
     }

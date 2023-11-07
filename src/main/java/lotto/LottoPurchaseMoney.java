c266a8c3bfd386b30e4df81962a9957f99af9b54
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 03:50:06 +0900

    feat: 로또 구입 금액이 1000원 이상인지 검증하는 기능
    로또 구입 금액이 충분한지 검증하는 기능

--- src/main/java/lotto/LottoPurchaseMoney.java
+++ src/main/java/lotto/LottoPurchaseMoney.java
@@ -1,12 +1,23 @@
 package lotto;
 
+import static lotto.constant.ErrorMessage.NOT_ENOUGH_MONEY;
+
 public class LottoPurchaseMoney {
+
+    private static final int LOTTO_PRICE = 1000;
 
     private int money;
 
     public LottoPurchaseMoney(String inputMoney) {
         int money = Converter.stringToInt(inputMoney);
+        validateEnoughMoney(money);
         this.money = money;
+    }
+
+    private void validateEnoughMoney(int money) {
+        if (money < LOTTO_PRICE) {
+            throw new IllegalArgumentException(NOT_ENOUGH_MONEY);
+        }
     }
 
 }
--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,5 +3,5 @@ package lotto.constant;
 public class ErrorMessage {
 
     public static final String NOT_NUMBER = "[ERROR] 입력값은 숫자여야 합니다.";
-
+    public static final String NOT_ENOUGH_MONEY = "[ERROR] 로또 구입 금액은 1000원 이상이어야 합니다. ";
 }

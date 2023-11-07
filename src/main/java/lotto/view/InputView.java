a696a2510c9af01139f581eb2c30632ab0a93d2b
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:44:56 +0900

    feat(OutputView): 로또 구매 갯수를 출력하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class InputView {
     public static long inputMoney() {
+        System.out.println("금액을 입력하세요.");
         String money = Console.readLine();
         InputValidator.validateInputMoney(money);
 
@@ -15,6 +16,7 @@ public class InputView {
     }
 
     public static List<Integer> inputWinningNumber() {
+        System.out.println("당첨 번호를 입력하세요.");
         String winningNumber = Console.readLine();
         String[] numbers = winningNumber.split(",");
         InputValidator.validateInputWinningNumber(numbers);
@@ -23,6 +25,7 @@ public class InputView {
     }
 
     public static int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력하세요.");
         String bonusNumber = Console.readLine();
         InputValidator.validateBonusNumber(bonusNumber);
 
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class OutputView {
+    public static void showPurchaseCount(int purchaseCount) {
+        System.out.println(purchaseCount + "개를 구매했습니다.");
+    }
+}

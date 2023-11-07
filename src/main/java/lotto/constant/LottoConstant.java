b5aff328ac0ce642e7cf6fa42588986f131934a2
SubinHeo0 <anmedi3623@naver.com>
2023-11-08 07:46:26 +0900

    feat: 매직 넘버 상수 처리

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,17 @@
+package lotto.constant;
+
+public final class LottoConstant {
+    public static final int LOTTO_MONEY_UNIT = 1000;
+    public static final int LOTTO_RANGE_START = 1;
+    public static final int LOTTO_RANGE_END = 45;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final int WINNING_1 = 6;
+    public static final int WINNING_2OR3 = 5;
+    public static final int WINNING_4 = 4;
+    public static final int WINNING_5 = 3;
+    public static final int WINNING_1_REVENUE = 2000000000;
+    public static final int WINNING_2_REVENUE = 30000000;
+    public static final int WINNING_3_REVENUE = 1500000;
+    public static final int WINNING_4_REVENUE = 50000;
+    public static final int WINNING_5_REVENUE = 5000;
+}

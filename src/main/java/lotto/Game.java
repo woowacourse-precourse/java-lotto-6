ba9b82675f1447b15a79b52f2f8c65200be11193
Jaewon <choijawon99@gmail.com>
2023-11-07 16:44:05 +0900

    feat(game): 투입 금액 입력

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,11 +1,31 @@
 package lotto;
 
-public class Game {
-    public Game(){
+import camp.nextstep.edu.missionutils.Console;
 
+import lotto.view.View;
+
+
+public class Game {
+    private static final int LOTTO_PRICE = 1000;
+
+    private int money;
+    private View view;
+
+    public Game(){
+        view = new View();
     }
 
     public void play(){
-
+        getMoneyFromUserInput();
     }
+
+    private void getMoneyFromUserInput(){
+        money = inputMoney();
+    }
+
+    private int inputMoney(){
+        view.request_InputMoney();
+        return Integer.parseInt(Console.readLine());
+    }
+
 }

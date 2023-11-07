7016fa63c1dd08f8eade0a9d0f2600838dd0d924
rhdqors <rhdqors@gmail.com>
2023-11-07 15:04:44 +0900

    refactor: 기능별 검증 분리
    로또 객체가 생성될 때 중복 로또 번호 확인.
    로또 당첨 번호를 입력 받을 때 숫자 개수 확인.
    로또 번호인 1~45 범위 확인.
    중복된 번호(잘못된 입력)는 없는지 확인.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,9 +3,12 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoGame {
@@ -17,6 +20,9 @@ public class LottoGame {
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        validateWinningNumberSize();
+        validateNumberRange();
+        validateDuplicateWinningNumber();
     }
 
     public void inputBonusNumber() {
@@ -25,22 +31,23 @@ public class LottoGame {
 
     public void printLottos(List<Lotto> lotts) {
         for (Lotto lo : lotts) {
+            Collections.sort(lo.getNumbers());
             System.out.println(lo.getNumbers());
         }
     }
 
-    public void checkNumber(List<Lotto> lottos) {
+    public void checkPrizeNumber(List<Lotto> lottos) {
         int totalMoney = lottos.size() * Price.LOTTO.getLottoPrice();
         HashMap<Prize, Integer> map = new HashMap<>();
 
         for (Prize prize : Prize.values()) {
             map.put(prize, 0);
         }
-        
+
         for (Lotto lotto : lottos) {
             int count = 0;
             boolean bonusMatch = false;
-            
+
             for (int number : lotto.getNumbers()) {
                 if(winningLottoNumbers.contains(number)) count++;
                 if(number == bonusNumber) bonusMatch = true;
@@ -48,10 +55,32 @@ public class LottoGame {
             Prize prize = Prize.valueOf(count, bonusMatch);
             map.put(prize, map.get(prize) + 1);
         }
-        printCheck(map, totalMoney);
+        PrizePrint(map, totalMoney);
     }
 
-    private void printCheck(HashMap<Prize, Integer> map, int totalMoney) {
+    private void validateWinningNumberSize() {
+        if(winningLottoNumbers.size() != 6)
+            throw new IllegalArgumentException(",로 구분하여 6개의 숫자를 입력하세요" + winningLottoNumbers);
+    }
+
+    private void validateNumberRange() {
+        for(int number : winningLottoNumbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다." + winningLottoNumbers);
+            }
+        }
+    }
+
+    private void validateDuplicateWinningNumber() {
+        Set<Integer> numbersSet = new HashSet<>();
+        for (int number : winningLottoNumbers) {
+            if (!numbersSet.add(number)) {
+                throw new IllegalArgumentException("중복된 번호가 존재합니다: " + winningLottoNumbers);
+            }
+        }
+    }
+
+    private void PrizePrint(HashMap<Prize, Integer> map, int totalMoney) {
         for (Prize prize : Prize.values()) {
             int count = prize.getCount();
             String prizeAmountFormatted = String.format("%,d", prize.getPrize());
@@ -63,12 +92,12 @@ public class LottoGame {
             }
             System.out.println(count + "개 일치 (" + prizeAmountFormatted + "원) - " + map.get(prize) + "개");
         }
-        double profitRate = calculate(totalMoney, map);
+        double profitRate = calculateProfit(totalMoney, map);
         String profitRateFormatted = String.format("%.1f", profitRate);
         System.out.println("총 수익률은 " + profitRateFormatted + "%입니다.");
     }
 
-    private double calculate(int totalMoney, HashMap<Prize, Integer> map) {
+    private double calculateProfit(int totalMoney, HashMap<Prize, Integer> map) {
         long profit = 0;
 
         for (Map.Entry<Prize, Integer> entry : map.entrySet()) {
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -10,20 +10,14 @@ public class User {
     private static final int LOTTO_PRICE = Price.LOTTO.getLottoPrice();
     private int haveSeveralLotto = 0;
 
-    // 구입 금액 입력 및 로또 갯수 반환
     public int inputAmount() {
-        try {
-            int allLottoPay = Integer.parseInt(Console.readLine());
-
-            if(allLottoPay % LOTTO_PRICE != 0) {
-                throw new IllegalArgumentException("[Error] 로또 구입 금액이 맞지 않습니다.");
-            }
-            haveSeveralLotto = allLottoPay/LOTTO_PRICE;
-            return haveSeveralLotto;
-        } catch(NumberFormatException e) {
-            throw new NumberFormatException("[Error] 올바른 금액을 입력하세요." + e);
+        int allLottoPay = Integer.parseInt(Console.readLine());
+        if (allLottoPay % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("력로또 구입 금액이 맞지 않습니다.");
         }
 
+        haveSeveralLotto = allLottoPay / LOTTO_PRICE;
+        return haveSeveralLotto;
     }
 
     public List<Lotto> saveLottos() {

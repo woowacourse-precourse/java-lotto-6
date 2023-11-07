b3b57c02f4e44cb6768a9fd7c6d7bc1bb84b6418
changhyeonnam <hj04143@gmail.com>
2023-11-07 20:27:23 +0900

    feat(Lotto) : 발행된 로또 숫자리스트 크기가 6개인지 검증, 오름차순인지 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,21 +10,35 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLengthLotto(numbers);
+        validateRangeLotto(numbers);
+        validateOrderLotto(numbers);
         this.numbers = numbers;
     }
 
     private void validateLengthLotto(List<Integer> numbers) {
         if (numbers.size() != Lotto_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ERROR_STRING+
+                    "\nExpected length : " + Lotto_LENGTH + "\nInput length : " + numbers.size());
         }
     }
 
     private void validateRangeLotto(List<Integer> numbers){
-
+        for(Integer num: numbers){
+            if(num<Lotto_BEGIN_RANGE || num>Lotto_END_RANGE){
+                throw new IllegalArgumentException(ERROR_STRING+
+                        "\nExpect : 1~45 범위 숫자" + "\nInput : " + num);
+            }
+        }
     }
 
     private void validateOrderLotto(List<Integer> numbers){
+        for(int i=1; i<numbers.size();i++){
+            if(numbers.get(i) < numbers.get(i - 1)){
+                throw new IllegalArgumentException(ERROR_STRING+
+                        "\nExpect : 오름 차순이 아닙니다.");
 
+            }
+        }
     }
 
     public String convertToString(){

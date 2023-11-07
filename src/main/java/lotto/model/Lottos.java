3e58a479b8242bdf2621d4d30ee17e768a78f79e
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-08 02:02:51 +0900

    feat(Lottos): 동등성 비교를 위한 equals&hachCode 재정의

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -4,6 +4,7 @@ import lotto.util.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -19,5 +20,18 @@ public class Lottos {
 
     public List<Lotto> getLottoTickets() {
         return new ArrayList<>(lottos);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lottos lottos1 = (Lottos) o;
+        return Objects.equals(lottos, lottos1.lottos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos);
     }
 }

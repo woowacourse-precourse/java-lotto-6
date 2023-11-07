95a803eaee380e4dd644455f321dcc1814f79ef3
dsadara <chaehyun.forest@gmail.com>
2023-11-07 21:30:24 +0900

    feat: 금액 입력 feat: 입력값 양수 검증

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,13 @@
 package lotto.controller;
 
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 
     public void run() {
         OutputView.printBuyLottoPromptMessage();
+        InputView.readLottoBuyAmount();
     }
 
 }
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    static final String INPUT_AMOUNT_IS_NOT_POSTIVE_NUMBER_ERROR = "양수를 입력해 주세요.";
+    static final String INPUT_AMOUNT_IS_NOT_NUMBER_ERROR = "문자가 아닌 숫자를 입력해 주세요.";
+
+    public static int readLottoBuyAmount() {
+        int buyAmount = StringToInteger(Console.readLine());
+        validatePostiveNumber(buyAmount);
+        return buyAmount;
+    }
+
+    private static int StringToInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(INPUT_AMOUNT_IS_NOT_NUMBER_ERROR);
+        }
+    }
+
+    private static void validatePostiveNumber(int buyAmount) {
+        if (buyAmount < 0) {
+            throw new IllegalArgumentException(INPUT_AMOUNT_IS_NOT_POSTIVE_NUMBER_ERROR);
+        }
+    }
+
+}

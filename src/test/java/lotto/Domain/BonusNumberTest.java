1e389f795135b1bd8957dad1fa798e8894b058c8
dlgkrals <147320103+dlgkrals@users.noreply.github.com>
2023-11-07 18:46:56 +0900

    test(BonusNumberTest): 보너스 번호의 범위를 벗어난 수를 받은 경우 테스트 추가

--- src/test/java/lotto/Domain/BonusNumberTest.java
+++ src/test/java/lotto/Domain/BonusNumberTest.java
@@ -11,8 +11,15 @@ import org.junit.jupiter.api.Test;
 class BonusNumberTest {
     @Test
     @DisplayName("음수를 받았을 경우")
-    void createLottoByOverSize() {
+    void createBonusNumber_Minus() {
         assertThatThrownBy(() -> new BonusNumber(-1, Arrays.asList(1,2,3,4,5,6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("범위 밖의 수를 받은 경우")
+    void createBonusNumber_Range() {
+        assertThatThrownBy(() -> new BonusNumber(52, Arrays.asList(1,2,3,4,5,6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

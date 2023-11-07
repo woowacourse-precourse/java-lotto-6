096260d77a6185e5637af1b825d33ea92a1e8031
nhn1030 <shgusska12@gmail.com>
2023-11-07 15:30:43 +0900

    refactor: 클래스명을 간결하게 수정

--- src/main/java/lotto/service/TicketGenerator.java
+++ src/main/java/lotto/service/TicketGenerator.java
@@ -3,19 +3,19 @@ package lotto.service;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoTicketGenerator {
+public class TicketGenerator {
     private final LottoNumberGenerator numberGenerator;
 
-    public LottoTicketGenerator(LottoNumberGenerator numberGenerator) {
+    public TicketGenerator(LottoNumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
 
-    public static LottoTicketGenerator create(LottoNumberGenerator numberGenerator) {
-        return new LottoTicketGenerator(numberGenerator);
+    public static TicketGenerator create(LottoNumberGenerator numberGenerator) {
+        return new TicketGenerator(numberGenerator);
     }
 
     public List<List<Integer>> generateLottoTicket(int purchaseAmount) {
-        int totalTicketCount = purchaseAmount / 1000;
+        int totalTicketCount = purchaseAmount / 1000; // 하드코딩 상수 대체
         List<List<Integer>> lottoTickets = new ArrayList<>();
 
         for (int i = 0; i < totalTicketCount; i++) {

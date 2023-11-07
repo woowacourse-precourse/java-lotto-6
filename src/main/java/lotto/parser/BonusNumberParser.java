f5227a8cda9bad1b0456cc296ac922375efebae8
heesane <tees3359@gmail.com>
2023-11-07 18:46:39 +0900

    fix : Parser의 Exception import 간략화

--- src/main/java/lotto/parser/BonusNumberParser.java
+++ src/main/java/lotto/parser/BonusNumberParser.java
@@ -1,14 +1,10 @@
 package lotto.parser;
 
-import lotto.exception.DuplicatedNumberException;
-import lotto.exception.EmptyException;
-import lotto.exception.InvalidLottoFormatException;
-import lotto.exception.InvalidLottoNumberRangeException;
+import lotto.exception.*;
 import lotto.model.BonusNumber;
 import lotto.model.Lotto;
 import lotto.validator.InputBonusValidator;
 import lotto.view.LottoView;
-
 public class BonusNumberParser {
     public static BonusNumber parse(Lotto winningLotto, String bonusNumber) {
         try{

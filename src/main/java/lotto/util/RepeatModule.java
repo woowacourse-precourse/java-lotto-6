892db59497ab55e71e29711192a9f43e44220fa2
Dreaming-J <kimjjong321@naver.com>
2023-11-07 20:40:47 +0900

    :lipstick: style. 에러 출력 후 줄 바꿈 추가

--- src/main/java/lotto/util/RepeatModule.java
+++ src/main/java/lotto/util/RepeatModule.java
@@ -5,12 +5,13 @@ import java.util.function.Supplier;
 public class RepeatModule {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String LINE_BREAK = "\n";
 
     protected <T> T repeat(Supplier<T> inputReader) {
         try {
             return inputReader.get();
         } catch (IllegalArgumentException e) {
-            System.out.println(ERROR_PREFIX + e.getMessage());
+            System.out.println(ERROR_PREFIX + e.getMessage() + LINE_BREAK);
             return repeat(inputReader);
         }
     }

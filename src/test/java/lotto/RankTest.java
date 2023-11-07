858736261b9ea4800e9a0c3e9410f6385b7e309e
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-07 18:59:45 +0900

    refactor: 테스트코드명 수정 및 DisplayName 등록

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -2,29 +2,23 @@ package lotto;
 
 import lotto.domain.Rank;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RankTest {
+    @DisplayName("calculateRank 계산 테스트")
     @Test
-    void calculateRank_테스트() {
-        Rank rank = Rank.calculateRank(3, false);
-        Assertions.assertEquals(Rank.MATCH_3, rank);
-
-        rank = Rank.calculateRank(3, true);
-        Assertions.assertNull(rank);
-
-        rank = Rank.calculateRank(5, true);
-        Assertions.assertEquals(Rank.MATCH_5_BONUS, rank);
-
-        rank = Rank.calculateRank(5, false);
-        Assertions.assertEquals(Rank.MATCH_5, rank);
-
-        rank = Rank.calculateRank(6, false);
-        Assertions.assertEquals(Rank.MATCH_6, rank);
+    void calculateRank와_Rank의_결과가_다르면_예외가_발생한다() {
+        Assertions.assertEquals(Rank.MATCH_3, Rank.calculateRank(3, false));
+        Assertions.assertNull(Rank.calculateRank(3, true));
+        Assertions.assertEquals(Rank.MATCH_5_BONUS, Rank.calculateRank(5, true));
+        Assertions.assertEquals(Rank.MATCH_5, Rank.calculateRank(5, false));
+        Assertions.assertEquals(Rank.MATCH_6, Rank.calculateRank(6, false));
     }
 
+    @DisplayName("getPrizeMoney 일치 여부 테스트")
     @Test
-    void getPrize_테스트() {
+    void 해당_rank와_getPrizeMoney의_결과가_일치하지_않으면_예외가_발생한다() {
         Rank rank = Rank.MATCH_3;
         Assertions.assertEquals(5000, rank.getPrizeMoney());
 
@@ -32,8 +26,9 @@ public class RankTest {
         Assertions.assertEquals(1_500_000, rank.getPrizeMoney());
     }
 
+    @DisplayName("getCountOfWin 일치 여부 테스트")
     @Test
-    void getCountOfWin_테스트() {
+    void 해당_rank와_getCountOfWin_결과가_일치하지_않으면_예외가_발생한다() {
         Rank rank = Rank.MATCH_3;
         Assertions.assertEquals(1, rank.getCountOfWin());
 
@@ -41,8 +36,9 @@ public class RankTest {
         Assertions.assertEquals(0, rank.getCountOfWin());
     }
 
+    @DisplayName("isMatchBonus 일치 여부 테스트")
     @Test
-    void isMatchBonus_테스트() {
+    void 해당_rank와_isMatchBonus_결과가_일치하지_않으면_예외가_발생한다() {
         Rank rank = Rank.MATCH_5_BONUS;
         Assertions.assertTrue(rank.isMatchBonus());
 

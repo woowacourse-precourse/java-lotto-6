3f82553549a26ba7c22398b583d2aa40381ff0cd
peeaaccee <wlgp8958@naver.com>
2023-11-07 16:28:28 +0900

    fix(LottoCashier): getLottoCount메서드에서 오류를 던져야하는 상황 로직 변경

--- src/main/java/lotto/domain/LottoCashier.java
+++ src/main/java/lotto/domain/LottoCashier.java
@@ -7,7 +7,7 @@ public class LottoCashier {
     private static final String VALIDATE_UNIT_MESSAGE = "[ERROR] 1000원 단위로 구입해야 합니다.";
 
     public int getLottoCount(int money) {
-        if (money > 0 || money % 1000 != 0) {
+        if (money <= 0 || money % 1000 != 0) {
             throw new IllegalArgumentException(VALIDATE_UNIT_MESSAGE);
         }
         return money / 1000;

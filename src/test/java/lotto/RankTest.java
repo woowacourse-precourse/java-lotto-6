5f97ee5494a410868c5fc02321771075a0ebc9e4
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 21:10:04 +0900

    refactor: 가독성을 위해 BeforeEach 삭제

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -2,7 +2,8 @@ package lotto;
 
 import java.util.List;
 import lotto.Model.Bonus;
-import lotto.Model.RandomLotto;
+import lotto.Model.Lotto;
+import lotto.Model.RandomLottos;
 import lotto.Model.Rank;
 import lotto.Model.WinningLotto;
 import org.assertj.core.api.Assertions;
@@ -11,14 +12,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RankTest {
-    private RandomLotto randomLotto;
-    @BeforeEach
-    void set() {
-        randomLotto = new RandomLotto(List.of(1, 3, 5, 10, 35, 45));
-    }
+
     @DisplayName("당첨 1등을 계산하는 테스트, 맞춘 로또개수 6개")
     @Test
     void calculateWinningRank1() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,5,10,35,45");
         Bonus bonus = new Bonus("30", winningLotto);
 
@@ -31,6 +29,7 @@ public class RankTest {
     @DisplayName("당첨 2등을 계산하는 테스트, 맞춘 로또개수 5개, 보너스 O")
     @Test
     void calculateWinningRank2() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,5,10,35,44");
         Bonus bonus = new Bonus("45", winningLotto);
 
@@ -43,6 +42,7 @@ public class RankTest {
     @DisplayName("당첨 3등을 계산하는 테스트, 맞춘 로또개수 5개, 보너스 X")
     @Test
     void calculateWinningRank3() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,5,10,35,44");
         Bonus bonus = new Bonus("43", winningLotto);
 
@@ -55,6 +55,7 @@ public class RankTest {
     @DisplayName("당첨 4등을 계산하는 테스트, 맞춘 로또개수 4개, 보너스 O")
     @Test
     void calculateWinningRank4_1() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,5,10,29,44");
         Bonus bonus = new Bonus("35", winningLotto);
 
@@ -67,6 +68,7 @@ public class RankTest {
     @DisplayName("당첨 4등을 계산하는 테스트, 맞춘 로또개수 4개, 보너스 X")
     @Test
     void calculateWinningRank4_2() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,5,10,29,44");
         Bonus bonus = new Bonus("30", winningLotto);
 
@@ -79,6 +81,7 @@ public class RankTest {
     @DisplayName("당첨 5등을 계산하는 테스트, 맞춘 로또개수 3개")
     @Test
     void calculateWinningRank5() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,5,9,29,44");
         Bonus bonus = new Bonus("30", winningLotto);
 
@@ -91,6 +94,7 @@ public class RankTest {
     @DisplayName("당첨 -등을 계산하는 테스트, 맞춘 로또개수 2개 이하")
     @Test
     void calculateWinningRank6() {
+        Lotto randomLotto = new Lotto(List.of(1, 3, 5, 10, 35, 45));
         WinningLotto winningLotto = new WinningLotto("1,3,6,9,29,44");
         Bonus bonus = new Bonus("30", winningLotto);
 

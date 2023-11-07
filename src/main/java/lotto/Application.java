e5e44f1eb6976685c98115d3275d1ac6139fb27b
dragon0622 <iamdragon.dev@gmail.com>
2023-11-07 21:56:52 +0900

    feat(Lotto) : main 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,33 @@
 package lotto;
 
-import static lotto.LottoGame.issuanceLotto;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.LottoGame.*;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int lottoCnt = 0;
+        int lottoCnt;
+        int bonus;
+        List<List<Integer>> lottoBundle;
+        List<Integer> winningNumbers;
 
         Output.startMessage();
         lottoCnt = issuanceLotto(Input.money());
+
         Output.howManyLottoMessage(lottoCnt);
+        lottoBundle = LottoGame.getLottoBundle();
+
+        Output.winningNumberGuide();
+        winningNumbers = Input.winningNumbers();
+
+        Output.bonusNumberGuide();
+        bonus = Input.bonusNumber();
+
+        getTwoNumbers(winningNumbers, lottoBundle, bonus);
+        Output.resultLotto();
+        Output.RateOfReturn();
 
 
 

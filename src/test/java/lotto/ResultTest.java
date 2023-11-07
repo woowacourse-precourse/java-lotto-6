2063f48a76bc814f3e6b687807af17e68fa34ff2
Son JunHyung <kingjh1125@gmail.com>
2023-11-08 01:50:07 +0900

    test (ResultTest): 테스트 추가
    상금 계산에 대한 테스트를 추가하였습니다:
    - getRewardTest 추가

---
+++ src/test/java/lotto/ResultTest.java
@@ -0,0 +1,41 @@
+package lotto;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.Lotto;
+import domain.Lottos;
+import domain.Result;
+import domain.WinningLotto;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+public class ResultTest {
+
+    Lottos lottos;
+    WinningLotto winningLotto;
+    @BeforeEach
+    void setUp(){
+        List<Lotto> lotto = new ArrayList<>();
+        lotto.add(new Lotto(List.of(1,2,3,4,5,6)));
+        lotto.add(new Lotto(List.of(1,2,3,4,5,7)));
+        lotto.add(new Lotto(List.of(1,2,3,4,5,8)));
+        lotto.add(new Lotto(List.of(1,2,3,4,7,8)));
+        lotto.add(new Lotto(List.of(1,2,3,7,8,9)));
+
+        lottos = new Lottos(lotto);
+
+        winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
+    }
+
+    @DisplayName("상금 계산 테스트")
+    @Test
+    void getRewardTest(){
+        Result result = new Result(lottos, winningLotto);
+        int getResult = result.getReward();
+
+        assertThat(getResult).isEqualTo(2031555000);
+    }
+}

1ecf1909749f255b7355e0531ee2a8666aeafe70
insu <is23193160@gmail.com>
2023-11-07 21:19:57 +0900

    test: 1~45사이가 아닌 숫자를 입력 시 예외가 발생하는 테스트 추가

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,14 @@ class BonusNumberTest {
         BonusNumber bonusNumber = new BonusNumber(20);
 
         assertThat(bonusNumber.getBonusNumber()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("1~45사이가 아닌 숫자를 입력 시 예외가 발생한다")
+    void inputBonusNumberOutOfRange() {
+        assertThatThrownBy(() -> new BonusNumber(46))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 숫자는 1에서 45 사이어야 합니다.");
     }
 
 }

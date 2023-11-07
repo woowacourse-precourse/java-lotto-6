92b85fcb3fc068716588a2ff6a69b66b77a6308c
wook <dong930112@gmail.com>
2023-11-07 18:58:15 +0900

    feat: 로또를 구매하는 기능 추가

--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -1,5 +1,30 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    int money;
+    List<Lotto> lottos;
+
+
+
+    //로또를 구매하는 기능
+    //로또를 발행하는 기능을 포함하고 있다. 리팩토링 대상
+    public List<Lotto> purchaseLotto(int purchaseQuantity) {
+        int lottoAmount = purchaseQuantity/1000;
+        money = purchaseQuantity%1000;
+        List<Lotto> issuedLotto = new ArrayList<>();
+
+        for(int i=0; i<lottoAmount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(numbers);
+            issuedLotto.add(lotto);
+        }
+
+        lottos = issuedLotto;
+        return issuedLotto;
+    }
 
 }

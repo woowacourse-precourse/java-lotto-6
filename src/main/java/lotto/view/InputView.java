8ca07bb821dbf958c9886f41a76ebf07fa7aae0b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-08 04:20:55 +0900

    refactor: 로또 번호를 입력받을 때 반환형 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,18 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String inputPurchaseAmount() {
+    public int inputPurchaseAmount() {
 
         System.out.println("구입금액을 입력해 주세요.");
 
-        return Console.readLine();
+        return Integer.parseInt(Console.readLine());
     }
 
-    public int inputWinningNumbers() {
+    public String inputWinningNumbers() {
 
         System.out.println("당첨 번호를 입력해 주세요.");
 
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
     public int inputBonusNumber() {

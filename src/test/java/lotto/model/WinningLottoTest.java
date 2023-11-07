b8c031e3fa2fd0cd51b99f1fa26853d5a971aa95
hoyeon <highoption@naver.com>
2023-11-07 15:36:56 +0900

    test(WinningLottoTest) : 보너스 넘버의 중복 테스트
    - 보너스 넘버는 당첨 번호와 중복 되면 안된다

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -11,12 +11,23 @@ public class WinningLottoTest {
 
     @Test
     @DisplayName("보너스 넘버의 범위가 1부터 45사이인지 테스트")
-    void createWinningLottoByDuplicateBonusNumber() {
+    void createWinningLottoByBonusNumberRange() {
         Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
         String bonus = "46";
 
         assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("보너스 넘버가 당첨번호와 중복 된다면 예외 발생")
+    void createWinningLottoByDuplicateBonusNumber() {
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        String bonus = "3";
+
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
     }
 }

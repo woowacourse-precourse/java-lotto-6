ca969d894ac394fb07fd4c872f0519e9b08cde49
심석현 <shim9597@gmail.com>
2023-11-07 22:22:23 +0900

    refactor: 로또 발행 기능 코드 변경
    1. 상수 이름 변경
    2. 구입 금액 만큼 로또 생성하는 로직 변경

--- src/main/java/lotto/LottoMaker.java
+++ src/main/java/lotto/LottoMaker.java
@@ -1,26 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoMaker {
 
-    private static final int START_INCLUSIVE = 1;
-    private static final int END_INCLUSIVE = 45;
-    private static final int COUNT = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
 
     public static List<Lotto> makeLotto(int amount) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < amount; i++) {
-            Lotto newLotto = createNewLotto();
-            lottos.add(newLotto);
-        }
-        return lottos;
+        return IntStream.range(0, amount)
+                .mapToObj(index -> createNewLotto())
+                .toList();
     }
 
     private static Lotto createNewLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
+                LOTTO_NUMBERS_SIZE);
         return new Lotto(numbers);
     }
 }

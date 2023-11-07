16705d83d69e3ea3930202d4fbd8ddc7201d8029
You <ytn1574@gmail.com>
2023-11-07 16:04:54 +0900

    refactor: 하드코딩된 변수 분리
    구입금액 단위, 로또길이, 시작번호, 끝번호를 상수로 분리하여 관리

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -5,6 +5,11 @@ import java.util.Set;
 
 public class Validation {
 
+    private static int LOTTO_BUY_UNIT = 1000;
+    private static int LOTTO_LENGTH = 6;
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+
     public int parsePurchaseAmount(String userInput) {
         int inputMoney = 0;
         try {
@@ -23,8 +28,8 @@ public class Validation {
     }
 
     public void validateDivideThousand(int money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] : 입력 금액은 1000 단위여야 합니다.");
+        if (money % LOTTO_BUY_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] : 입력 금액은 " + LOTTO_BUY_UNIT + " 단위여야 합니다.");
         }
     }
 
@@ -38,8 +43,8 @@ public class Validation {
 
     public void validateWinNumbersFormatByLength(String winNumbersInput) {
         String[] splitWinNumbersInput = winNumbersInput.split(",");
-        if (splitWinNumbersInput.length != 6) {
-            throw new IllegalArgumentException("[ERROR] : 당첨 번호는 6개여야 합니다.");
+        if (splitWinNumbersInput.length != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] : 당첨 번호는 " + LOTTO_LENGTH + "개여야 합니다.");
         }
     }
 
@@ -51,11 +56,11 @@ public class Validation {
 
 
     public int[] validateWinNumbersInRange(String winNumbersInput) {
-        int[] winNumbers = new int[6];
+        int[] winNumbers = new int[LOTTO_LENGTH];
         String[] splitWinNumbersInput = winNumbersInput.split(",");
         for (int i = 0; i < splitWinNumbersInput.length; i++) {
             int temp = Integer.parseInt(splitWinNumbersInput[i]);
-            if (temp < 0 || temp > 45) {
+            if (temp < LOTTO_START_NUMBER || temp > LOTTO_END_NUMBER) {
                 throw new IllegalArgumentException("[ERROR] : 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
             winNumbers[i] = temp;
@@ -68,7 +73,7 @@ public class Validation {
         for (int i = 0; i < winNumbers.length; i++) {
             validateDuplicateSet.add(winNumbers[i]);
         }
-        if (validateDuplicateSet.size() != 6) {
+        if (validateDuplicateSet.size() != LOTTO_LENGTH) {
             throw new IllegalArgumentException("[ERROR] : 중복된 번호는 입력할 수 없습니다.");
         }
     }
@@ -80,7 +85,7 @@ public class Validation {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] : 숫자가 아닌 입력입니다.");
         }
-        if (bonusNumber < 0 || bonusNumber > 45) {
+        if (bonusNumber < LOTTO_START_NUMBER || bonusNumber > LOTTO_END_NUMBER) {
             throw new IllegalArgumentException("[ERROR] : 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
         return bonusNumber;

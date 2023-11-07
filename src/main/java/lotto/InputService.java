11e5842a0e982700eda75a16270f3623f2371c91
hwangkyojune <shane5969@naver.com>
2023-11-07 23:42:33 +0900

    InputService 구현 완료

---
+++ src/main/java/lotto/InputService.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputService {
+    static private final String requestMoney ="구입금액을 입력해 주세요.";
+    static private final String requestWinningNums ="당첨 번호를 입력해 주세요.";
+    static private final String requestBonusNum ="보너스 번호를 입력해 주세요.";
+    public int howMuchMoney(){
+        System.out.println(requestMoney);
+        String tempMoney = Console.readLine();
+        ValidateService.validateNumber(tempMoney);
+        int money = Integer.parseInt(tempMoney);
+        return money;
+    }
+
+    public List<Integer> winningNumbers(){
+        System.out.println(requestWinningNums);
+        List<String> tempWinningNumbers = Arrays.asList(
+                Console.readLine().split(",")
+        );
+        tempWinningNumbers.stream()
+                .forEach(ValidateService::validateNumber);
+        List<Integer> winningNumbers = tempWinningNumbers.stream()
+                .map(Integer::parseInt).toList();
+        ValidateService.validateNumbersInRange(winningNumbers);
+        ValidateService.validateAuthorizedLength(winningNumbers);
+        ValidateService.validateDuplicatedNums(winningNumbers);
+        return winningNumbers;
+    }
+
+    public int bonusNumber(List<Integer> winningNumbers){
+        System.out.println(requestBonusNum);
+        String tempBonus = Console.readLine();
+        ValidateService.validateNumber(tempBonus);
+        int bonus = Integer.parseInt(tempBonus);
+        ValidateService.validateBonusInWinningNums(bonus,winningNumbers);
+        ValidateService.validateNumberInRange(bonus);
+        ValidateService.validateDuplicatedNums(bonus,winningNumbers);
+        return bonus;
+    }
+}

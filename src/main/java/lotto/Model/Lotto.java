e40a155a497cb752c167fd499c4c95e209a8e82a
YejiGong <yejigong00@gmail.com>
2023-11-08 01:47:46 +0900

    Feat: 로또 객체 수정

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.Model;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.Global.Exception;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +14,27 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Exception.LOTTO_WRONG_AMOUNT_INPUT.getErrorPhrase());
         }
+    }
+
+    public String getLottoCreatedResult() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[]");
+        String result = numbers.stream().map(Object::toString).collect(Collectors.joining(","));
+        stringBuilder.insert(1, result);
+        return stringBuilder.toString();
+    }
+
+    public int matchesNumberList(List<Integer> inputNumber) {
+        return numbers.stream().filter(inputNumber::contains).collect(Collectors.toList()).size();
+    }
+
+    public int matchesSingleNumber(int inputNumber) {
+        if (numbers.contains(inputNumber)) {
+            return 1;
+        }
+        return 0;
     }
 
     // TODO: 추가 기능 구현

cdb56e40c53b82dcbdceb20e364495fb6ee727a4
stats-dev <hohun94@naver.com>
2023-11-07 15:02:40 +0900

    fix: 로또 번호에 중복된 숫자가 있으면 에러 발생 코드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,18 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        for(int num : numbers){
+            validateNum(num);
+        }
+        validDuplicates(numbers);
         this.numbers = numbers;
     }
 
@@ -22,4 +28,17 @@ public class Lotto {
             throw new IllegalArgumentException("[Error] 1부터 45사이 숫자만 입력하세요.");
         }
     }
+
+    public void validDuplicates(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (set.contains(number)) {
+                throw new IllegalArgumentException("[Error] 로또 번호에 중복된 숫자가 있습니다.");
+            }
+            set.add(number);
+        }
+    }
+
+
 }

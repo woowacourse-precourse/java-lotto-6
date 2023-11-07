04f6bd7ea0f5518a37f97a1d346a2b7a709d5969
yeonjae02 <0yeonjae2@naver.com>
2023-11-07 20:11:33 +0900

    feat: start 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 
 class IllegalArgumentException extends Exception{
 	public IllegalArgumentException() {
@@ -9,7 +10,29 @@ class IllegalArgumentException extends Exception{
 }
 
 public class Application {
+	private int lottoCnt, purchase;
+	private boolean vaildMoney;
+	private ArrayList<ArrayList<Integer>> lotto;
+	public void start() {
+		System.out.println("구입금액을 입력해 주세요.");	
+		while (!vaildMoney)
+			inputMoney();
+		
+		lottoCnt = purchase / 1000;
+		System.out.println("\n" + lottoCnt +"개를 구매했습니다.");
+		lotto = new ArrayList<>(lottoCnt);
+		run();
+	}
+	
+	private void inputMoney() {
+	}
+	
+	private void run() {
+		
+	}
+	
     public static void main(String[] args) {
     	Application game = new Application();
+    	game.start();
     }
 }

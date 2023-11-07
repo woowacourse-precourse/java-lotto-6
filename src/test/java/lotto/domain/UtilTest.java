a8bd9d48461d898f77518db0015da3bc585eec7e
doka <posmane5@gmail.com>
2023-11-07 22:15:39 +0900

    chore : 디렉토리 구조 살짝 수정

--- src/test/java/lotto/domain/UtilTest.java
+++ src/test/java/lotto/domain/UtilTest.java
@@ -6,17 +6,17 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class UtilTest {
+class ValidateUtilTest {
 
     @Test
     void stringToArrayList() {
         List<Integer> expectedOutput = Arrays.asList(1, 3, 4, 5);
-        List<Integer> numbers = Util.stringToArrayList("1,3 , 4,5");
+        List<Integer> numbers = ValidateUtil.stringToArrayList("1,3 , 4,5");
         assertThat(numbers)
                 .isEqualTo(expectedOutput);
 
         expectedOutput = Arrays.asList(1, 4, 5);
-        numbers = Util.stringToArrayList("1,4,5");
+        numbers = ValidateUtil.stringToArrayList("1,4,5");
         assertThat(numbers)
                 .isEqualTo(expectedOutput);
 

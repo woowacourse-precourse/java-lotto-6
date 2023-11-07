76c9172ab674b78cfa71d75532dc98bb11520770
twoosky <lhjae3@naver.com>
2023-11-08 07:44:34 +0900

    refactor: 로또 발행 객체 싱글톤 패턴 적용

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -6,11 +6,27 @@ import java.util.Objects;
 
 public class LottoStore {
     private final LottoMachine lottoMachine;
+    private static final LottoStore INSTANCE = new LottoStore();
 
+    private LottoStore() {
+        this.lottoMachine = LottoMachine.getInstance();
+    }
+
+    public static LottoStore getInstance() {
+        validateNonNull();
+        return INSTANCE;
+    }
+
+    private static void validateNonNull() {
+        if (Objects.isNull(INSTANCE)) {
+            throw new IllegalArgumentException(LOTTO_STORE_IS_NULL);
+        }
+    }
 
     public Lottos issue(Money money, LottoIssuePolicy lottoIssuePolicy) {
         int quantity = money.getBuyLottoQuantity();
         return new Lottos(lottoMachine.issue(quantity, lottoIssuePolicy));
     }
+
 
 }

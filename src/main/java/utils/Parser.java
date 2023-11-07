626f365e907e5b3b11216b8c58705ee491af6377
win9 <krrr883@gmail.com>
2023-11-08 04:33:30 +0900

    [Feat]: 추첨번호 입력시 정수로 변환하는 로직 구현
    추첨번호와 서비스번호 입력시 검증 후 정수로 변환하도록 구현

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -2,11 +2,28 @@ package utils;
 
 import type.ErrorType;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
+
+    public static final String COMMA = ",";
+
     public static int convertNumberInput(String input) {
         if (input.matches("^[0-9]+$")) {
             throw new IllegalArgumentException(ErrorType.INVALID_NUMBER_INPUT.getText());
         }
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> convertWinningNumberInput(String input) {
+        if (input.matches("^(\\d{6},){5}\\d{6}$")) {
+            throw new IllegalArgumentException(ErrorType.INVALID_WINNING_NUMBER_INPUT.getText());
+        }
+        return Arrays.stream(input.split(COMMA))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
     }
 }
--- src/main/java/view/InputMessage.java
+++ src/main/java/view/InputMessage.java
@@ -4,9 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 import type.TextType;
 import utils.Parser;
 
+import java.util.List;
+
 public class InputMessage {
     public static int printMoneyInputMessage() {
         System.out.println(TextType.START);
+        String input = Console.readLine();
+        return Parser.convertNumberInput(input);
+    }
+
+    public static List<Integer> printWinNumberInputMessage() {
+        System.out.println(TextType.LOTTO_WIN_NUMBER);
+        String input = Console.readLine();
+        return Parser.convertWinningNumberInput(input);
+    }
+
+    public static int printBonusNumberInputMessage() {
+        System.out.println(TextType.LOTTO_BONUS);
         String input = Console.readLine();
         return Parser.convertNumberInput(input);
     }

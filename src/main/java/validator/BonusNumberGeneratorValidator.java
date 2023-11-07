a713af0f20358dfb31b91800a5887396c1120f87
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 07:48:15 +0900

    refactor: BonusNumberGeneratorValidator 상수 설정

--- src/main/java/validator/BonusNumberGeneratorValidator.java
+++ src/main/java/validator/BonusNumberGeneratorValidator.java
@@ -1,6 +1,9 @@
 package validator;
 
 public class BonusNumberGeneratorValidator {
+    public static final String INVALID_BONUS_NUMBER_RANGE = "[ERROR] 보너스 숫자에는 1 ~ 45까지의 숫자만 입력해주세요.";
+    public static final String INVALID_NON_NUMERIC_INPUT = "[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.";
+
     public void vaildBonusNumber(String bonusNumber) {
         vaildBonusNumberNonNumeric(bonusNumber);
         int bonusNumberValue = Integer.parseInt(bonusNumber);
@@ -9,14 +12,14 @@ public class BonusNumberGeneratorValidator {
 
     void vaildBonusNumberRange(int bonusNumber) {
         if (bonusNumber > 45 || bonusNumber < 1) {
-            throw new IllegalArgumentException("[ERROR] 보너스 숫자에는 1 ~ 45까지의 숫자만 입력해주세요.");
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_RANGE);
         }
     }
 
     void vaildBonusNumberNonNumeric(String input) {
         String regex = "[+-]?\\d*(\\.\\d+)?";
         if (!input.matches(regex)) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액에는 숫자만 입력해주세요.");
+            throw new IllegalArgumentException(INVALID_NON_NUMERIC_INPUT);
         }
     }
 }

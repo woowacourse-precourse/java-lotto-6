198766bfa398c99c8cf0df2d31adc3ac86a5cfb6
Jinsu Kim <akrn112@naver.com>
2023-11-07 18:08:31 +0900

    feat(Lotto): 로또번호 오름차순으로 정렬하기 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,13 +1,20 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        sort();
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -15,6 +22,9 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-    
+
+    private void sort() {
+        Collections.sort(numbers);
+    }
     // TODO: 추가 기능 구현
 }

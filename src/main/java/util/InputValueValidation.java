d876a7612193c47a4e7e682bc73a12d54319ad1b
zjacom <eephus625@icloud.com>
2023-11-07 18:41:19 +0900

    refactor(lotto): 조건문을 메서드로 변경
    조건문을 메서드로 추출하여 의도를 명확하게 함

--- src/main/java/util/InputValueValidation.java
+++ src/main/java/util/InputValueValidation.java
@@ -8,65 +8,101 @@ import java.util.stream.Collectors;
 public class InputValueValidation {
 
     public int validatePurchaseAmount(String inputStr) {
-        if (!inputStr.matches("^\\d+$")) {
-            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_CAN_ONLY_NUMBER.getMessage());
-        }
+        isInputValueContainOnlyDigits(inputStr);
+        int purchaseAmount = Integer.parseInt(inputStr);
+        isPurchaseAmountOver1000(purchaseAmount);
+        isPurchaseAmountAcceptOnlyUnitOf1000(purchaseAmount);
 
-        int purchaseAmountTemp = Integer.parseInt(inputStr);
+        return purchaseAmount;
+    }
 
-        if (purchaseAmountTemp < 1000) {
-            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_MUST_BE_OVER_1000.getMessage());
-        }
-
+    private void isPurchaseAmountAcceptOnlyUnitOf1000(int purchaseAmountTemp) {
         if (purchaseAmountTemp % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_ACCEPT_ONLY_UNIT_OF_1000.getMessage());
         }
+    }
 
-        return purchaseAmountTemp;
+    private void isPurchaseAmountOver1000(int purchaseAmountTemp) {
+        if (purchaseAmountTemp < 1000) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_MUST_BE_OVER_1000.getMessage());
+        }
+    }
+
+    private void isInputValueContainOnlyDigits(String inputStr) {
+        if (!inputStr.matches("^\\d+$")) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_CAN_ONLY_NUMBER.getMessage());
+        }
     }
 
     public List<Integer> validatePrizeNumber(String inputStr) {
-        if (!inputStr.matches("^\\d+(,\\d+)*$")) {
-            throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_CAN_ONLY_NUMBER_AND_COMMA.getMessage());
-        }
+        isInputStrContainOnlyDigitsAndComma(inputStr);
+        List<Integer> prizeNumber = parseStrToList(inputStr);
+        isPrizeNumberDuplicated(prizeNumber);
+        isPrizeNumberHasCorrectSize(prizeNumber);
 
-        List<Integer> prizeNumberTemp = Arrays.stream(inputStr.split(","))
-                .map(s -> {
-                    int num = Integer.parseInt(s);
-                    if (num > 45) {
-                        throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_MUST_BE_UNDER_45.getMessage());
-                    }
-                    return num;
-                })
-                .collect(Collectors.toList());
+        return prizeNumber;
+    }
 
-        if (prizeNumberTemp.size() != new HashSet<>(prizeNumberTemp).size()) {
-            throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_MUST_NOT_BE_DUPLICATED.getMessage());
-        }
-
+    private void isPrizeNumberHasCorrectSize(List<Integer> prizeNumberTemp) {
         if (prizeNumberTemp.size() != 6) {
             throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_MUST_BE_SIX_DIGITS.getMessage());
         }
+    }
 
+    private void isPrizeNumberDuplicated(List<Integer> prizeNumberTemp) {
+        if (prizeNumberTemp.size() != new HashSet<>(prizeNumberTemp).size()) {
+            throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_MUST_NOT_BE_DUPLICATED.getMessage());
+        }
+    }
+
+    private List<Integer> parseStrToList(String inputStr) {
+        List<Integer> prizeNumberTemp = Arrays.stream(inputStr.split(","))
+                .map(s -> {
+                    int num = Integer.parseInt(s);
+                    isPrizeNumberContainExceededNumber(num);
+                    return num;
+                })
+                .collect(Collectors.toList());
         return prizeNumberTemp;
     }
 
+    private void isPrizeNumberContainExceededNumber(int num) {
+        if (num > 45) {
+            throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_MUST_BE_UNDER_45.getMessage());
+        }
+    }
+
+    private void isInputStrContainOnlyDigitsAndComma(String inputStr) {
+        if (!inputStr.matches("^\\d+(,\\d+)*$")) {
+            throw new IllegalArgumentException(ErrorMessage.PRIZE_NUMBER_CAN_ONLY_NUMBER_AND_COMMA.getMessage());
+        }
+    }
+
     public int validateBonusNumber(List<Integer> prizeNumber, String inputStr) {
-        if (!inputStr.matches("^\\d+$")) {
-            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_CAN_ONLY_NUMBER.getMessage());
-        }
+        isInputStrDigit(inputStr);
+        int bonusNumber = Integer.parseInt(inputStr);
+        isBonusNumberExceeded(bonusNumber);
+        isBonusNumberIncludedPrizeNumber(prizeNumber, bonusNumber);
 
-        int bonusNumberTemp = Integer.parseInt(inputStr);
+        return bonusNumber;
+    }
 
-        if (bonusNumberTemp > 45) {
-            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_MUST_BE_UNDER_45.getMessage());
-        }
-
+    private void isBonusNumberIncludedPrizeNumber(List<Integer> prizeNumber, int bonusNumberTemp) {
         if (prizeNumber.contains(bonusNumberTemp)) {
             throw new IllegalArgumentException(
                     ErrorMessage.BONUS_NUMBER_MUST_NOT_BE_DUPLICATED_WITH_PRIZE_NUMBER.getMessage());
         }
+    }
 
-        return bonusNumberTemp;
+    private void isBonusNumberExceeded(int bonusNumberTemp) {
+        if (bonusNumberTemp > 45) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_MUST_BE_UNDER_45.getMessage());
+        }
+    }
+
+    private void isInputStrDigit(String inputStr) {
+        if (!inputStr.matches("^\\d+$")) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_CAN_ONLY_NUMBER.getMessage());
+        }
     }
 }

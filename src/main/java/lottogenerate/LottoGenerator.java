262aba57a74fd0a2e2ab5bb9e3a68b985a163d28
cheolhyeon <tghy4@naver.com>
2023-11-07 20:43:11 +0900

    refactor :  코드 구조 개선

--- src/main/java/lottogenerate/LottoGenerator.java
+++ src/main/java/lottogenerate/LottoGenerator.java
@@ -1,40 +1,31 @@
 package lottogenerate;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import inputdata.LottoTicketInput;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.Lotto;
-import user.User;
 
 public class LottoGenerator {
-    private static final int MAX_LOTTO_NUMBER_RANGE = 45;
-    private static final int MIN_LOTTO_NUMBER_RANGE = 1;
-    private static final int MAX_LOTTO_NUMBER_COUNT = 6;
-    private final User user;
-    private final List<Lotto> lottoList;
-
-    public LottoGenerator(User user) {
-        this.lottoList = new ArrayList<>();
-        this.user = user;
+    private  final int MAX_LOTTO_NUMBER_RANGE = 45;
+    private  final int MIN_LOTTO_NUMBER_RANGE = 1;
+    private  final int MAX_LOTTO_NUMBER_COUNT = 6;
+    private List<Lotto> lottoList = new ArrayList<>();
+    public LottoGenerator() {
     }
-    public List<Lotto> generateLotto() {
-        return IntStream.range(0, lottoTicket())
+
+    public void generateLotto(int lottoTicketPrice) {
+        this.lottoList =  IntStream.range(0, lottoTicketPrice)
                 .mapToObj(i -> createLotto())
                 .collect(Collectors.toList());
-        // 반환한 로또 정보를 뒤져가지고 사용자가 입력한 위닝넘버와 비교를 해야함.
     }
 
     public List<Lotto> getLottoList() {
         return lottoList;
     }
-
-    private int lottoTicket() {
-        return user.inputLottoTicketPrice();
-    }
-
 
     private Lotto createLotto() {
         List<Integer> lottoNumbersList = getLottoNumbers();
@@ -47,6 +38,4 @@ public class LottoGenerator {
         return Randoms.pickUniqueNumbersInRange
                 (MIN_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_RANGE, MAX_LOTTO_NUMBER_COUNT);
     }
-
-
 }

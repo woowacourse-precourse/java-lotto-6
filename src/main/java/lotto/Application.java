3f7624bb44a7429980dec9dc4f60fa96e0c7e3e1
Jun Hyeok Choi <chrisgold@naver.com>
2023-11-08 00:56:22 +0900

    feat(Application) : add new function 'input'

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,10 +5,19 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String input = input_money();
-        int money = String_to_Int(input);
-        int lotto_num = Affordable_LottoNum(money);
+        int lotto_num = input();
         System.out.print(lotto_num);
+    }
+    private static int input(){                // input 통합 부분, 재귀함수 처리
+        String input = input_money();
+        try {
+            int money = String_to_Int(input);
+            int lotto_num = Affordable_LottoNum(money);
+            return lotto_num;
+        }
+        catch (IllegalArgumentException e){
+            return input();
+        }
     }
     private static String input_money(){               // input을 String으로 받아 반환
         System.out.print("구입금액을 입력해 주세요.\n");
@@ -19,13 +28,13 @@ public class Application {
         try{
             return Integer.parseInt(str);
         }catch (NumberFormatException e){
-            System.out.print("[ERROR] 로또 구입 금액은 정수여야 합니다.\n");
+            System.out.print("[ERROR] 로또 구입 금액은 정수여야 합니다.\n\n");
             throw new IllegalArgumentException();
         }
     }
     private static int Affordable_LottoNum(Integer money) { // money를 받아 구매할 로또 개수 반환
         if (money % 1000 != 0 || money < 0){
-            System.out.print("[ERROR] 로또 구입 금액은 1,000원으로 나누어 떨어지는 양의 정수여야 합니다.\n");
+            System.out.print("[ERROR] 로또 구입 금액은 1,000원으로 나누어 떨어지는 양의 정수여야 합니다.\n\n");
             throw new IllegalArgumentException();
         }
         return money / 1000;

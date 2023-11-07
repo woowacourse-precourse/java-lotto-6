e64d57942ce6e7a15a6d4041aa113d5585f75b8b
mingtorr <mingtorr@nave.com>
2023-11-08 02:54:17 +0900

    feat: 리스트 검증 유틸리티 추가
    ListUtil 추가
    중복검사, 리스트 크기 검사, 리스트 내 값 범위 검사

---
+++ src/main/java/lotto/utils/ListUtil.java
@@ -0,0 +1,28 @@
+package lotto.utils;
+
+import lotto.constants.ErrorConstants;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class ListUtil {
+    private void checkDuplicateValue(List<Objects> numbers) {
+        Set<Objects> set = new HashSet<>(numbers);
+        if(numbers.size() != set.size()) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_DUPLICATED_VALUE.getData());
+        }
+    }
+    private void checkLength(List<Objects> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_LENGTH.getData());
+        }
+    }
+    private void checkRange(List<Integer> numbers) {
+        for(int num : numbers) {
+            if( num <0 || num >46)
+                throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_RANGE.getData());
+        }
+    }
+}

dd3017334d37be16dfc903d9094337233b52e130
최우종 Choi Woojong <dwax1324@gmail.com>
2023-11-07 20:17:45 +0900

    feat(Prize): code와 prize를 통합함

---
+++ src/main/java/lotto/constant/constants/Prize.java
@@ -0,0 +1,28 @@
+package lotto.constant.constants;
+
+import java.util.List;
+
+public enum Prize {
+    // prizeCode,prize
+    THREE_MATCHED(List.of(3, 5000)),
+    FOUR_MATCHED(List.of(4, 50000)),
+    FIVE_MATCHED(List.of(5, 1500000)),
+
+    SIX_MATCHED(List.of(6, 2000000000)),
+    FIVE_AND_BONUS_MATCHED(List.of(7, 30000000));
+
+    private final List<Integer> result;
+
+    Prize(List<Integer> result) {
+        this.result = result;
+    }
+
+    public Integer getCode() {
+        return this.result.get(0);
+    }
+
+    public Integer getPrize() {
+        return this.result.get(1);
+    }
+
+}

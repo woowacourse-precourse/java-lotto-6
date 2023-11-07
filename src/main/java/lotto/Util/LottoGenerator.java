c899183f55f277dbe4593946527a9d7cbfd4b2c9
S2Hyeon <tmdgus908@naver.com>
2023-11-08 04:31:09 +0900

    :bug: fix : 로또 생성기가 매번 새로운 리스트를 반환하도록 수정

--- src/main/java/lotto/Util/LottoGenerator.java
+++ src/main/java/lotto/Util/LottoGenerator.java
@@ -1,6 +1,7 @@
 package lotto.Util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.Constant.Constant;
@@ -9,10 +10,10 @@ import lotto.Domain.Lotto;
 public class LottoGenerator {
 
     public Lotto generateLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(
                 Constant.MIN_LOTTO_NUMBER,
                 Constant.MAX_LOTTO_NUMBER,
-                Constant.LOTTO_NUMBER_AMOUNT);
+                Constant.LOTTO_NUMBER_AMOUNT));
         Collections.sort(numbers);
 
         return new Lotto(numbers);

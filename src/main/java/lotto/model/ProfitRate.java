f95e803db92c334af12b0dde8e40895b5e4137a6
LEE-sh1673 <lsh901673@gmail.com>
2023-11-07 21:07:43 +0900

    refactor: ProfitRate에 대한 리팩토링 진행
    변경 내용:
    - 클래스 내의 개행을 조정
    - 생성자 내의 매직 넘버 상수화

--- src/main/java/lotto/model/ProfitRate.java
+++ src/main/java/lotto/model/ProfitRate.java
@@ -5,15 +5,16 @@ import java.math.RoundingMode;
 
 public class ProfitRate {
 
-
     private static final BigDecimal PERCENTAGE_MULTIPLIER = BigDecimal.valueOf(100);
     private static final int INITIAL_SCALE = 1;
+    private static final RoundingMode DEFAULT_ROUNDING = RoundingMode.HALF_EVEN;
 
     private final BigDecimal profitRate;
 
     public ProfitRate(final Money purchaseAmount, final BigDecimal totalAmount) {
-        this.profitRate = totalAmount.multiply(PERCENTAGE_MULTIPLIER)
-                .divide(purchaseAmount.getAmount(), INITIAL_SCALE, RoundingMode.HALF_EVEN);
+        this.profitRate = totalAmount
+                .multiply(PERCENTAGE_MULTIPLIER)
+                .divide(purchaseAmount.getAmount(), INITIAL_SCALE, DEFAULT_ROUNDING);
     }
 
     public double doubleValue() {

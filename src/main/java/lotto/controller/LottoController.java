aecb5f6d0c6676f3384a36a0c4de8212bcaba718
Hayoon <hy97@sju.ac.kr>
2023-11-07 16:46:15 +0900

    feat(LottoController): 로또게임을 시작하기 위한 Controller 구현, Model, View 중개 역할

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,35 @@
+package lotto.controller;
+
+import lotto.model.domain.Lotto;
+import lotto.model.domain.Referee;
+import lotto.model.service.LottoService;
+import lotto.model.service.RefereeService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+
+    public final InputView inputView = new InputView();
+    public final OutputView outputView = new OutputView();
+    public final LottoService lottoService = new LottoService();
+    public final RefereeService refereeService = new RefereeService();
+
+    public void playLotto() {
+        int purchaseAmount = inputView.inputPurchaseAmount();
+        outputView.printLottoAmount(purchaseAmount);
+
+        List<Lotto> lottoList = lottoService.initLottoList(purchaseAmount);
+        outputView.printLottoNumbers(lottoList);
+
+        List<Integer> winningNumbers = inputView.inputWinningNumbers();
+        int bonusNumber = inputView.inputBonusNumber();
+
+        Referee referee = new Referee(winningNumbers, bonusNumber);
+        refereeService.calculateLottoResult(referee, lottoList);
+
+        outputView.printLottoRankingResult(referee);
+        outputView.printLottoBenefitResult(referee);
+    }
+}

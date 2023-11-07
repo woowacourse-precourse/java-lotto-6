1a185451c0559bb9b3b1b3026cd16c419b9613ac
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-08 03:57:52 +0900

    refactor : inputBudget 메서드명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,12 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private final String INPUT_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
 
-    public String inputPurchasePrice() {
+    public String inputBudget() {
         System.out.println(INPUT_MONEY);
         return Console.readLine();
     }

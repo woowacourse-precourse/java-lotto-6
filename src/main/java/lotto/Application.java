e3f7a31bf4b64d0c8bae908511b5c96d66726a84
leedabin2 <12193152@inha.edu>
2023-11-07 18:14:41 +0900

    feat(Application): Add getBonusNumberFromUser() method call

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,10 +6,11 @@ import lotto.handler.UserHandler;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        UserHandler.getAmountFromUser();
-
         LottoController lottoController = new LottoController();
         lottoController.buyLottos();
+
+        UserHandler.getWinningNumberFromUser();
+        UserHandler.getBonusNumberFromUser();
     }
 }
 

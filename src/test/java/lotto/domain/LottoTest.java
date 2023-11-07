8ddca1ede385281c8ba50848eedbf72406d16bdb
Sunhee Kim <sunni372@icloud.com>
2023-11-08 00:01:26 +0900

    test: 로또(Lotto) 테스트 수정
    로또 번호 오름차순 정렬 후 반환을 테스트 합니다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -22,9 +22,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호를 오름차순으로 출력한다.")
+    @DisplayName("로또 번호를 오름차순으로 정렬하여 가져온다.")
     @Test
-    void printNaturalOrder() {
+    void getNumbersNaturalOrder() {
+        Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+        assertThat(lotto.getNumbersNaturalOrder()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 
     @DisplayName("로또 번호를 가져온다.")

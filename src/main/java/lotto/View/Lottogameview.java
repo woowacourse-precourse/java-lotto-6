bc333a03eab1311e7f2625277a03ff3619df81d5
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 03:02:43 +0900

    [ADD] 총 수익률 출력

--- src/main/java/lotto/View/Lottogameview.java
+++ src/main/java/lotto/View/Lottogameview.java
@@ -39,4 +39,35 @@ public class Lottogameview {
     private String formatBonusResultLine(int bonusCount, int prize) {
         return "5개 일치, 보너스 볼 일치 (" + formatPrize(prize) + "원) - " + bonusCount + "개";
     }
+
+    private double calculateTotalProfitRate(int purchaseAmount, int[] winCounts) {
+        int totalPrize = calculateTotalPrize(winCounts);
+        return calculateProfitRate(purchaseAmount, totalPrize);
+    }
+
+    private int calculateTotalPrize(int[] winCounts) {
+        int totalPrize = 0;
+        for (LottoResult result : LottoResult.values()) {
+            totalPrize += result.getPrize() * winCounts[result.ordinal()];
+        }
+        return totalPrize;
+    }
+
+    private double calculateProfitRate(int purchaseAmount, int totalPrize) {
+        return (double) totalPrize / purchaseAmount * 100;
+    }
+
+    private String formatPrize(int prize) {
+        return String.format("%,d", prize);
+    }
+
+    public void printWinningResult(int[] winCounts, int purchaseAmount) {
+        for (LottoResult result : LottoResult.values()) {
+            String resultLine = formatResultLine(result, winCounts);
+            System.out.println(resultLine);
+        }
+
+        double totalProfitRate = calculateTotalProfitRate(purchaseAmount, winCounts);
+        System.out.println("총 수익률은 " + String.format("%.1f%%", totalProfitRate) + "입니다.");
+    }
 }

c567acb9183c8680e3d58389f7bcbcc313aa3454
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-11-08 00:18:10 +0900

    Refactor : RandomLotto가 여러 Lottos를 만들어서 출력하도록 변경

--- src/main/java/lotto/support/RandomLotto.java
+++ src/main/java/lotto/support/RandomLotto.java
@@ -1,14 +1,25 @@
 package lotto.support;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class RandomLotto {
 
-    public List<Integer> getRandomLotto() {
+    public List<Lotto> RandomLottos(int lottoTimes) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoTimes; i++) {
+            Lotto lotto = new Lotto(getRandomLotto());
+            lotto.print();
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+    private List<Integer> getRandomLotto() {
 
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         Collections.sort(numbers);

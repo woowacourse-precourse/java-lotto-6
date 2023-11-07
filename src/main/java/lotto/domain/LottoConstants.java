8feb2f55e0e007166c21e39e521ebc57ae75aa21
dmong <dt2980_hee@naver.com>
2023-11-08 07:17:36 +0900

    feat: 로또의 최소 구입 개수 상수화

--- src/main/java/lotto/domain/LottoConstants.java
+++ src/main/java/lotto/domain/LottoConstants.java
@@ -4,7 +4,8 @@ public enum LottoConstants {
     LOTTO_NUMBER_RANGE_START(1),
     LOTTO_NUMBER_RANGE_END(45),
     LOTTO_PRICE(1000),
-    LOTTO_LENGTH_CONSTRAINT(6);
+    LOTTO_LENGTH_CONSTRAINT(6),
+    MIN_LOTTO_BUY_QUANTITY(1);
 
     private final int value;
 

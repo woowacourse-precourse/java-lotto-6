1f07dadfac7008ef12b2fdecc5b44e3b5c11a1f5
ohhyeon2 <dhgus1235@gmail.com>
2023-11-07 21:31:38 +0900

    fix: 숫자형 상수에 3자리 마다 comma를 추가하는 포맷 설정

--- src/main/java/lotto/view/WinningResultOutput.java
+++ src/main/java/lotto/view/WinningResultOutput.java
@@ -3,6 +3,8 @@ package lotto.view;
 import lotto.domain.Rank;
 import lotto.domain.RankCount;
 
+import java.text.DecimalFormat;
+
 public class WinningResultOutput {
     private static final int WINNING_FIFTH = 5_000;
     private static final int WINNING_FOURTH = 50_000;
@@ -11,12 +13,14 @@ public class WinningResultOutput {
     private static final int WINNING_FIRST = 2_000_000_000;
 
     public static void winningResult(RankCount rankCount) {
+        DecimalFormat formatter = new DecimalFormat("#,###");
+
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (" + WINNING_FIFTH + "원) - " + rankCount.getRankCount().getOrDefault(Rank.FIFTH, 0) + "개");
-        System.out.println("4개 일치 (" + WINNING_FOURTH + "원) - " + rankCount.getRankCount().getOrDefault(Rank.FOURTH, 0) + "개");
-        System.out.println("5개 일치 (" + WINNING_THIRD + "원) - " + rankCount.getRankCount().getOrDefault(Rank.THIRD, 0) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (" + WINNING_SECOND + "원) - " + rankCount.getRankCount().getOrDefault(Rank.SECOND, 0) + "개");
-        System.out.println("6개 일치 (" + WINNING_FIRST + "원) - " + rankCount.getRankCount().getOrDefault(Rank.FIRST, 0) + "개");
+        System.out.println("3개 일치 (" + formatter.format(WINNING_FIFTH) + "원) - " + rankCount.getRankCount().getOrDefault(Rank.FIFTH, 0) + "개");
+        System.out.println("4개 일치 (" + formatter.format(WINNING_FOURTH) + "원) - " + rankCount.getRankCount().getOrDefault(Rank.FOURTH, 0) + "개");
+        System.out.println("5개 일치 (" + formatter.format(WINNING_THIRD) + "원) - " + rankCount.getRankCount().getOrDefault(Rank.THIRD, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + formatter.format(WINNING_SECOND) + "원) - " + rankCount.getRankCount().getOrDefault(Rank.SECOND, 0) + "개");
+        System.out.println("6개 일치 (" + formatter.format(WINNING_FIRST) + "원) - " + rankCount.getRankCount().getOrDefault(Rank.FIRST, 0) + "개");
     }
 }

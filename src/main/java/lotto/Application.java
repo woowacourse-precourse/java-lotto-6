87d193ed40a96ba30b5f2058b119afe0e9298f04
SuhyungK <dook4966@gmail.com>
2023-11-08 08:16:38 +0900

    refactor(InputHandler): 숫자 입력에 관한 로직 일괄 처리하는 askNumber 구현, askPayment -> askNumber로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,13 @@
 package lotto;
 
 import lotto.constants.ErrorMessage;
+import lotto.constants.StaticMessage;
 import lotto.domain.InputHandler;
+import lotto.domain.Lotto;
 import lotto.domain.User;
+
+import static lotto.constants.StaticMessage.*;
+import static lotto.domain.InputHandler.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,6 +16,10 @@ public class Application {
     }
 
     public static void start() {
-        User user = InputHandler.askPayment();
+        User user = new User(askNumber(INPUT_PAYMENT));
+
+
+//        askWinningNumbers();
+        inputController(askWinningNumbers());
     }
 }
--- src/main/java/lotto/domain/InputHandler.java
+++ src/main/java/lotto/domain/InputHandler.java
@@ -3,9 +3,22 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.Validator;
 import lotto.constants.ErrorMessage;
+import lotto.constants.StaticMessage;
 
 public class InputHandler {
     // TODO : 사용자의 입력에 대한 처리
+    private static final String delimiter = ",";
+
+    public static String askNumber(StaticMessage message) {
+        do {
+            try {
+                System.out.println(message);
+                return Console.readLine();
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException();
+            }
+        } while (true);
+    }
 
     public static User askPayment() {
         String input = "";
@@ -21,13 +34,49 @@ public class InputHandler {
         } while (true);
     }
 
-    public void askWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        Console.readLine();
+    public static String[] askWinningNumbers() {
+        System.out.println(StaticMessage.INPUT_WINNING_NUMBERS);
+        return convertStringToArray(Console.readLine());
+//        do {
+//            System.out.println("당첨 번호를 입력해 주세요.");
+//            try {
+//                input = Console.readLine();
+//                Validator.validatePayment(input);
+//                return new User(input);
+//            } catch (IllegalArgumentException e) {
+//                System.out.println(ErrorMessage.NUMBER_FORMAT.getMessage());
+//            }
+//        } while (true);
+//        System.out.println("당첨 번호를 입력해 주세요.");
+//        return Console.readLine();
+
+    }
+
+//    public static void inputController(String input) {
+//        //
+//    }
+
+    public static void inputController(String[] input) {
+        do {
+            askWinningNumbers();
+            try {
+                Validator.containOnlyDigit(input);
+            } catch (IllegalArgumentException e) {
+
+            }
+        } while (true);
     }
 
     public void askBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         Console.readLine();
+    }
+
+    public static int convertStringToInteger (String input) {
+        return Integer.parseInt(input);
+    }
+
+    public static String[] convertStringToArray (String input) {
+        return input.split(delimiter);
     }
 }

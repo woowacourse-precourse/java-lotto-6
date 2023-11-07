4f46f7bb22bc13fcdf53012ccf80401f104817b5
pwrwpw <dlgustjr9912@gmail.com>
2023-11-07 09:44:39 +0900

    test : 의미없는 테스트 삭제

--- src/test/java/lotto/LottoDataTest.java
+++ src/test/java/lotto/LottoDataTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
 import lotto.domain.LottoData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +18,7 @@ public class LottoDataTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
 
-        assertThatNoException().isThrownBy(() -> new LottoData(input, bonusNumber));
+        assertThatNoException().isThrownBy(() -> new LottoData(new Lotto(input),  new BonusNumber(bonusNumber)));
     }
 
     @Test
@@ -25,7 +27,7 @@ public class LottoDataTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 5);
         int bonusNumber = 7;
 
-        assertThatThrownBy(() -> new LottoData(input, bonusNumber))
+        assertThatThrownBy(() -> new LottoData(new Lotto(input), new BonusNumber(bonusNumber)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력 값은 앞서 입력한 값들과 중복되지 않아야 합니다.");
     }
@@ -36,7 +38,7 @@ public class LottoDataTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 46);
         int bonusNumber = 7;
 
-        assertThatThrownBy(() -> new LottoData(input, bonusNumber))
+        assertThatThrownBy(() -> new LottoData(new Lotto(input), new BonusNumber(bonusNumber)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력 값의 범위는 1부터 45 사이의 숫자여야 합니다.");
     }
@@ -47,20 +49,9 @@ public class LottoDataTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 0);
         int bonusNumber = 7;
 
-        assertThatThrownBy(() -> new LottoData(input, bonusNumber))
+        assertThatThrownBy(() -> new LottoData(new Lotto(input), new BonusNumber(bonusNumber)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력 값의 범위는 1부터 45 사이의 숫자여야 합니다.");
-    }
-
-    @Test
-    @DisplayName("당첨_번호_입력_검증_실패_보너스_번호_중복")
-    void 당첨_번호_입력_검증_실패_보너스_번호_중복() {
-        List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 6;
-
-        assertThatThrownBy(() -> new LottoData(input, bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 입력 값은 앞서 입력한 값들과 중복되지 않아야 합니다.");
     }
 
     @Test
@@ -69,7 +60,7 @@ public class LottoDataTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 46;
 
-        assertThatThrownBy(() -> new LottoData(input, bonusNumber))
+        assertThatThrownBy(() -> new LottoData(new Lotto(input), new BonusNumber(bonusNumber)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력 값의 범위는 1부터 45 사이의 숫자여야 합니다.");
     }
@@ -80,9 +71,8 @@ public class LottoDataTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumber = 0;
 
-        assertThatThrownBy(() -> new LottoData(input, bonusNumber))
+        assertThatThrownBy(() -> new LottoData(new Lotto(input), new BonusNumber(bonusNumber)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력 값의 범위는 1부터 45 사이의 숫자여야 합니다.");
     }
-
 }

6ca2b1fc83276195fc709e37781f475468f10fe9
seokmyungham <hellotjraud@gmail.com>
2023-11-08 03:48:48 +0900

    refactor(Rank): 함수형 인터페이스 BiFunction 사용해서 코드 개선
    일치 번호 개수와 보너스 번호 일치 여부를 BiFunction의 인수로 받아 코드를 개선시켰습니다

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,33 +1,31 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Rank {
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000);
+    FIFTH(5000, 3, (count, hasBonusNumber) -> count == 3),
+    FOURTH(50000, 4, (count, hasBonusNumber) -> count == 4),
+    THIRD(1500000, 5, (count, hasBonusNumber) -> count == 5 && !hasBonusNumber),
+    SECOND(30000000, 5, (count, hasBonusNumber) -> count == 5 && hasBonusNumber),
+    FIRST(2000000000, 6, (count, hasBonusNumber) -> count == 6);
 
-    private final int count;
     private final int winningAmount;
+    private final int count;
+    private final BiFunction<Integer, Boolean, Boolean> expression;
 
-    Rank(int count, int winningAmount) {
-        this.count = count;
+    Rank(int winningAmount, int count,
+         BiFunction<Integer, Boolean, Boolean> expression) {
         this.winningAmount = winningAmount;
+        this.count = count;
+        this.expression = expression;
     }
 
     public static Rank getRank(int count, boolean hasBonusNumber) {
-        Rank rank = Arrays.stream(values())
-                .filter(r -> r.count == count)
+        return Arrays.stream(values())
+                .filter(rank -> rank.expression.apply(count, hasBonusNumber))
                 .findFirst()
                 .orElse(null);
-
-        if (rank == SECOND && !hasBonusNumber) {
-            rank = THIRD;
-        }
-
-        return rank;
     }
 
     public int getCount() {

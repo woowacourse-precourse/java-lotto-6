71ee392c5dba624fc43a0e9bfc868dc15ccc33f7
sujnkim <blackout3210@naver.com>
2023-11-07 12:39:22 +0900

    feat: Lottos의 모든 로또에 대해 등수를 계산하는 메서드 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -11,14 +11,25 @@ public class Lottos {
         this.lottos = lottos;
     }
 
-    public List<String> getLottosAllSortedNumber(){
+    public List<String> getLottosAllSortedNumber() {
         return lottos.stream()
                 .map(lotto -> lotto.getSortedNumbers())
                 .toList();
     }
 
-    public int getLottosAmount(){
+    public int getLottosAmount() {
         return lottos.size();
+    }
+
+    public List<LottoRank> calculateLottoResult(WinningNumber winningNumber, BonusNumber bonusNumber) {
+        List<LottoRank> rank = lottos.stream()
+                .map(lotto -> LottoRank.findLottoRank(
+                        winningNumber.hitCount(lotto),
+                        bonusNumber.isHitBonusNumber(lotto)
+                        ))
+                .toList();
+
+        return rank;
     }
 
 }

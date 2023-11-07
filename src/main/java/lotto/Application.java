623de5a5f9c7b886d5dd4818c9459af392e7b19a
choiyewon <choiyewon1997@gmail.com>
2023-11-07 12:15:43 +0900

    feat: class Lotto ,make random , valid

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Lotto lotto = new Lotto(test);
+        System.out.println(lotto.getNumbers());
+    }
+
+    //랜던값 만들기
+    public static List<Integer>  makeRandom(){
+        List<Integer> resRand = new ArrayList<>();
+        while (resRand.size() < 6) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!resRand.contains(randomNumber)) {
+                resRand.add(randomNumber);
+            }
+        }
+        return resRand;
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +19,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        // 길이가 6이 아닐 때
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        // 중복 값이 존재할 때
+        Collections.sort(numbers);
+        if(validate_dupli(numbers)){
+            throw new IllegalArgumentException();
+        }
+        // 범위 밖의 값 존재
+
     }
 
-    // TODO: 추가 기능 구현
+    // 중복값 확인 함수
+    private boolean validate_dupli(List<Integer> numbers){
+        boolean res = false;
+        int before = numbers.get(0);
+        for(int i=1;i<numbers.size();i++){
+            if(before == numbers.get(i)){
+                res = true;
+                break;
+            }
+            before = numbers.get(i);
+        }
+        return res;
+    }
+
 }

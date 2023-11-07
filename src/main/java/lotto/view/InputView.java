7c261d47c9abd04a4fb332cfb80e52fcc19c63ad
seokmyungham <hellotjraud@gmail.com>
2023-11-07 12:13:44 +0900

    fix(inputView): 모든 메서드 static 키워드 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,17 +10,17 @@ public class InputView {
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static int inputPurchaseAmount() {
+    public int inputPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return validateAndParseInput(Console.readLine());
     }
 
-    public static ArrayList<Integer> inputWinningNumbers() {
+    public ArrayList<Integer> inputWinningNumbers() {
         System.out.println(WINNING_NUMBERS_MESSAGE);
         return validateAndParseNumbersInput(Console.readLine());
     }
 
-    public static int inputBonusNumber() {
+    public int inputBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
         return validateAndParseInput(Console.readLine());
     }

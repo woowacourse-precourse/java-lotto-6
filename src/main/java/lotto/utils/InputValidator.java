f53607d1e3f622eb0ab376bc63284a5fea5c93c6
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 08:47:23 +0900

    feat(InputValidator): 1000 단위 유효성 검사 기능 추가

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -1,9 +1,11 @@
 package lotto.utils;
 
 import static lotto.enums.ExceptionMessageType.NUMBER_NOT_INTEGER;
+import static lotto.enums.ExceptionMessageType.PAYMENT_INVALID_UNIT;
 import static lotto.enums.ExceptionMessageType.PAYMENT_NOT_NATURAL_NUMBER;
 
 public class InputValidator {
+    private static final int UNIT = 1000;
     public static void validateInteger(String number) {
         try {
             Integer.parseInt(number);
@@ -17,4 +19,11 @@ public class InputValidator {
             throw new IllegalArgumentException(PAYMENT_NOT_NATURAL_NUMBER.getMessage());
         }
     }
+
+    public static void validateUnit(int number) {
+        if (number % UNIT != 0) {
+            throw new IllegalArgumentException(PAYMENT_INVALID_UNIT.getMessage());
+        }
+    }
+
 }

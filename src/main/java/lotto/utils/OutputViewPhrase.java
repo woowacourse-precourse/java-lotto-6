a15027eb71dddfffa813847db182edf2497e6ea0
kang jihyeong <her4386@gmail.com>
2023-11-07 13:49:33 +0900

    [feat] 구매한 로또 개수 출력 기능

--- src/main/java/lotto/utils/OutputViewPhrase.java
+++ src/main/java/lotto/utils/OutputViewPhrase.java
@@ -1,4 +1,7 @@
 package lotto.utils;
 
 public class OutputViewPhrase {
+
+    public static final String outputPurchaseLottoCnt = "개를 구매했습니다.";
+
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,13 @@ package lotto.view;
 
 import java.util.List;
 
+import static lotto.utils.OutputViewPhrase.*;
+
 public class OutputView {
+
+    public static void printLottoCnt(int lottoCnt) {
+        System.out.println(lottoCnt+outputPurchaseLottoCnt);
+    }
 
     public static void printLottoNumbers(List<Integer> lotto) {
         System.out.println(lotto);

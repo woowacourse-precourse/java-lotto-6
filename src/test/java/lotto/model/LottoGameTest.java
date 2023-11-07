ebfc7ac0d5355fc7e2fd15d11d8359527e4e5fb2
김관진 <cozyschooll@gmail.com>
2023-11-07 15:33:33 +0900

    test: 로또 발행 테스트 구현 LottoGameTest 생성

---
+++ src/test/java/lotto/model/LottoGameTest.java
@@ -0,0 +1,52 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGameTest {
+
+    private LottoGame lottoGame;
+
+    @BeforeEach
+    void setUp() {
+        lottoGame = new LottoGame(1000);
+    }
+
+
+    @Test
+    @DisplayName("로또 발행 테스트")
+    void issuanceLotto() {
+        assertEquals(10, lottoGame.issuanceLotto(10000).size());
+    }
+
+    @Test
+    void calculationResult() {
+    }
+
+    @Test
+    void calculationRateOfReturn() {
+    }
+
+    @Test
+    void updateMatch() {
+    }
+
+    @Test
+    void updateBonusCount() {
+    }
+
+    @Test
+    void updateMatchCount() {
+    }
+
+    @Test
+    void countMatch() {
+    }
+
+    @Test
+    void validate() {
+    }
+}

78873a583927cfa7114a8216cca610fff019437e
oosedus <seayeon0729@gmail.com>
2023-11-08 04:41:55 +0900

    feat: 로또 출력하는 함수 추가

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,8 +1,16 @@
 package lotto;
 
+import java.util.List;
+
 public class OutputView {
 
     void printPurchasedLottoQuantity(int count){
         System.out.println(String.format("%d개를 구매했습니다."));
+    }
+
+    void printLottoNumbers(List<List<Integer>> lottoNumbers){
+        for(List<Integer> numbers: lottoNumbers){
+            System.out.println(numbers);
+        }
     }
 }

5dff436c26a8faaecdb89a95c3ec4ef5957c7b37
Dawin <dawin002@naver.com>
2023-11-08 01:55:07 +0900

    feat(OutputView): 사용자 입력 관련 출력 기능 추가
    사용자 입력 기능에 관련된 출력 기능 추가:
    - void printInputPurchaseMoney(): 구입금액 입력 메시지 출력
    - void printInputWinningNumbers(): 당첨 번호 입력 메시지 출력
    - void printInputBonusNumber(): 보너스 번호 입력 메시지 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import lotto.model.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+
+    public OutputView() {
+    }
+
+    public void printInputPurchaseMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printInputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+
+}

ef01b1874ea6dd6c348384ab58b206c6f5143687
Nari <avakray@hansung.ac.kr>
2023-11-07 22:58:34 +0900

    refactor(lotto): Static하게 사용하여 메모리 최적화

--- src/main/java/lotto/domain/LottoValidator.java
+++ src/main/java/lotto/domain/LottoValidator.java
@@ -5,19 +5,19 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class LottoValidator {
-    public void validate(List<Integer> numbers) {
+    public static void validate(List<Integer> numbers) {
         validateNumbersLength(numbers);
         validateNumberRange(numbers);
         validateUniqueNumber(numbers);
     }
 
-    private void validateNumbersLength(List<Integer> numbers) {
+    private static void validateNumbersLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개입니다.");
         }
     }
 
-    private void validateNumberRange(List<Integer> numbers) {
+    private static void validateNumberRange(List<Integer> numbers) {
         for(int number : numbers){
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
@@ -25,7 +25,7 @@ public class LottoValidator {
         }
     }
 
-    private void validateUniqueNumber(List<Integer> numbers) {
+    private static void validateUniqueNumber(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if(uniqueNumbers.size() != numbers.size()){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");

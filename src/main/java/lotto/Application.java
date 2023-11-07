e26189a5ecbe4feb9a1b054de56c2a6c064a8624
wwan13 <wwan13@naver.com>
2023-11-07 21:16:32 +0900

    feat : `Application` 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.common.config.LottoConfig;
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoConfig lottoConfig = new LottoConfig();
+
+        LottoController lottoController = new LottoController(lottoConfig);
+        lottoController.play();
     }
 }

d9f18109413e894af61e53ccba710624582c39f1
JeongHunHui <deca3248@gmail.com>
2023-11-08 04:18:29 +0900

    feat: 무작위 Lotto와 Lottos를 생성하는 LottoService 구현

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -2,7 +2,10 @@ package lotto.constant;
 
 public enum LottoConstant {
     LOTTO_PRICE(1000),
-    ZERO(0);
+    ZERO(0),
+    LOTTO_NUMBER_MIN(1),
+    LOTTO_NUMBER_MAX(45),
+    LOTTO_NUMBER_COUNT(6);
 
     private final Integer value;
 
---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.constant.LottoConstant;
+import lotto.dto.PurchaseAmountRequest;
+import lotto.model.Lotto;
+import lotto.model.Lottos;
+
+public class LottoService {
+
+    public Lottos makeRandomLottos(PurchaseAmountRequest purchaseAmountRequest) {
+        Integer purchaseCount =
+            purchaseAmountRequest.purchaseAmount() / LottoConstant.LOTTO_PRICE.getValue();
+
+        final List<Lotto> result = IntStream.range(0, purchaseCount)
+            .mapToObj(i -> makeRandomLotto()).toList();
+
+        return new Lottos(result);
+    }
+
+    private Lotto makeRandomLotto() {
+        final List<Integer> lottoNumbers =
+            Randoms.pickUniqueNumbersInRange(
+                LottoConstant.LOTTO_NUMBER_MIN.getValue(),
+                LottoConstant.LOTTO_NUMBER_MAX.getValue(),
+                LottoConstant.LOTTO_NUMBER_COUNT.getValue()
+            );
+        return new Lotto(lottoNumbers);
+    }
+}

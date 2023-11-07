7379ca6a85c6eccb05a878fa4fd8db76acde9608
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 19:23:32 +0900

    feat(AwardController) : 결과 기능 분할
    시상식만 따로 하는 awardController 기능 추가

---
+++ src/main/java/lotto/AwardController.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.ResultLotto;
+import lotto.domain.UserLotto;
+import lotto.domain.WinningLotto;
+import lotto.view.OutputView;
+
+public class AwardController {
+    private List<ResultLotto> result;
+    private float yield;
+
+    public void makeResult(UserLotto userLotto, WinningLotto winningLotto, BonusNumber bonusNumber) {
+        ResultManager resultManager = new ResultManager();
+        YieldManager yieldManager = new YieldManager();
+
+        result = resultManager.match(winningLotto, userLotto, bonusNumber);
+        yield = yieldManager.makeYield(result);
+
+    }
+
+    public void printYield() {
+        OutputView.printReward(result);
+        OutputView.printYield(yield);
+    }
+}

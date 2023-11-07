965788c5fe15d03502edac2a9906a9f306df6f61
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-07 21:37:08 +0900

    style: 좋은오류메시지를 만들기 위해 오류메시지변경

--- src/main/java/lotto/Model/WinningLotto.java
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -6,9 +6,9 @@ import java.util.stream.Stream;
 public class WinningLotto {
     private static final String LOTTO_NUMBER_DELIMETER = ",";
     public static final String ERROR_NOT_LOTTO_FORMAT =
-            String.format("[ERROR] 숫자와 쉼표(%s)만을 이용해서 입력해주세요. 예시) 1,2,3,4,5,6", LOTTO_NUMBER_DELIMETER);
+            String.format("[ERROR] 숫자와 쉼표(%s)만을 이용하고, 쉼표 사이에 숫자가 있는지 확인해주세요.", LOTTO_NUMBER_DELIMETER);
     public static final String ERROR_END_LOTTO_NUMBER_DELEMETER =
-            String.format("[ERROR] 마지막은 %s가 올 수 없습니다. 예시) 1,2,3,4,5,6", LOTTO_NUMBER_DELIMETER);
+            String.format("[ERROR] 마지막은 %s가 올 수 없습니다.", LOTTO_NUMBER_DELIMETER);
 
     private final Lotto winningLotto;
 

860de8461343f067890f109777dc483c6adaec7e
dyeongkim <dyeongkim98@gmail.com>
2023-11-07 16:16:32 +0900

    Feat(InputUser): Add 입력값 숫자 체크
    validateInputMoneyIsInteger
    - 구입금액이 숫자인지 유효성 검사 추가

--- src/main/java/lotto/InputUser.java
+++ src/main/java/lotto/InputUser.java
@@ -1,10 +1,23 @@
 package lotto;
 
+import java.util.HashSet;
+
 public class InputUser {
-    private String lottoMoney;
+    private String inputMoney;
+    private Integer lottoMoney;
     public Integer inputLottoMoney(){
         System.out.println("구입금액을 입력해 주세요.");
-        lottoMoney = camp.nextstep.edu.missionutils.Console.readLine();
-        return Integer.parseInt(lottoMoney);
+        inputMoney = camp.nextstep.edu.missionutils.Console.readLine();
+        validateInputMoneyIsInteger(inputMoney);
+        lottoMoney = Integer.parseInt(inputMoney);
+        return lottoMoney;
+    }
+
+    private void validateInputMoneyIsInteger(String inputMoney){
+        try {
+            Integer.parseInt(inputMoney);
+        }catch (NumberFormatException e){
+            throw new NumberFormatException("[ERROR] 구입 금액은 숫자를 입력해야 합니다.");
+        }
     }
 }

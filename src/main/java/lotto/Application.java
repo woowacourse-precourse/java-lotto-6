7eca7e4c8684177d2e751ab31df80e4a39b4e57d
swhyeon98 <smreo75@gmail.com>
2023-11-07 21:27:32 +0900

    feat: 로또 애플리케이션의 메인 흐름 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.RandomLottoGenerator;
+import lotto.service.LottoService;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoView lottoView = new LottoView();
+        LottoService lottoService = new LottoService(new RandomLottoGenerator());
+        LottoController lottoController = new LottoController(lottoView, lottoService);
+
+        lottoController.play();
     }
 }

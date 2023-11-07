fd28a8753e2ffbf24c71b6e1f2b71a19c8ba354d
J-I-H-O <jeongjiho0731@gmail.com>
2023-11-08 05:59:27 +0900

    feat(InputManager): 사용자 입력을 받는 기능 개발

---
+++ src/main/java/lotto/utils/ui/InputManager.java
@@ -0,0 +1,27 @@
+package lotto.utils.ui;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputManager {
+    private static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    private InputManager() {
+    }
+
+    public String receivePurchasePrice() {
+        System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String receiveWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String receiveBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/lotto/utils/ui/OutputManager.java
@@ -0,0 +1,5 @@
+package lotto.utils.ui;
+
+public class OutputManager {
+    
+}

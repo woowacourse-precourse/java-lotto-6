51b1c603783755f61fb9a16a99477790ba493976
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 18:20:24 +0900

    feat(MakeTestRandomNumber) : 랜덤 넘버 만드는 기능 테스트용구현 - RandomNum 의 메서드 getRandomNumber() 오버라이딩

--- src/main/java/lotto/util/MakeTestRandomNumber.java
+++ src/main/java/lotto/util/MakeTestRandomNumber.java
@@ -3,8 +3,14 @@ package lotto.util;
 import java.util.List;
 
 public class MakeTestRandomNumber implements RandomNum {
+    private final List<Integer> randomNumber;
+
+    public MakeTestRandomNumber(List<Integer> randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
     @Override
     public List<Integer> getRandomNumber() {
-        return null;
+        return randomNumber;
     }
 }

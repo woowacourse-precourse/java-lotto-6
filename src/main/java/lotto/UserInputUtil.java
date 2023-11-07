9e24337e8f5b72b9fcad9ac34c8dfcb510396dfe
Dobby_Kim <kimdobby@g.skku.edu>
2023-11-07 15:26:26 +0900

    feat - UserInputUtil : 사용자의 검증된 입력문 정보 받는 기능

---
+++ src/main/java/lotto/UserInputUtil.java
@@ -0,0 +1,102 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInputUtil {
+
+    private static final int PURCHASE_AMOUNT_UNIT = 1000;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public int readPurchaseAmount() {
+
+        while (true) {
+            try {
+                int purchaseAmount = Integer.parseInt(Console.readLine());
+                validatePurchaseAmount(purchaseAmount);
+                return purchaseAmount;
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorMessage.INVALID_PURCHASE_AMOUNT);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount <= 0 || purchaseAmount % PURCHASE_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT);
+        }
+    }
+
+    public List<Integer> readWinningNumbers() {
+        while (true) {
+            try {
+                String winningNumbersLine = Console.readLine();
+                return parseNumbers(winningNumbersLine);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> parseNumbers(String line) {
+        try {
+            List<Integer> numbers = Arrays.stream(line.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            validateNumbers(numbers);
+            return numbers;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+
+    private void validateNumbers(List<Integer> numbers) {
+
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBERS_COUNT);
+        }
+
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBERS);
+        }
+
+        if (numbers.stream().anyMatch(number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_RANGE);
+        }
+
+    }
+
+    public int readBonusNumber(List<Integer> winningNumbers) {
+        while (true) {
+            try {
+                int bonusNumber = Integer.parseInt(Console.readLine());
+                validateBonusNumber(bonusNumber, winningNumbers);
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+
+            }
+        }
+    }
+
+    private void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_NUMBER_RANGE);
+        }
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_BONUS_NUMBERS);
+        }
+    }
+}

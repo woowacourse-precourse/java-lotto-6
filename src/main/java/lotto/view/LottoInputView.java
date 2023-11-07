99ba788e8abf753480593c9e399d295dd972cea4
KWY <wodrk1001@gmail.com>
2023-11-07 11:21:31 +0900

    [chore] 보너스 번호 오류 시 에러 메시지 변경

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -11,6 +11,8 @@ public class LottoInputView {
     private static final String COMMA = ",";
     private static final String PURCHASE_AMOUNT_ERROR_MSG = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
     private static final String WINNING_NUMBER_ERROR_MSG = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String BONUS_NUMBER_ERROR_MSG = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String DUPLICATE_BONUS_NUMBER_ERROR_MSG = "[ERROR] 보너스 번호는 로또 번호에 포함되어 있지 않은 숫자여야 합니다.";
 
     public int inputPurchaseAmount(final LottoValidation lottoValidation) {
         String userInput = Console.readLine();
@@ -28,10 +30,10 @@ public class LottoInputView {
 
     public int inputBonusNumber(final LottoValidation lottoValidation, final List<Integer> winningNumbers) {
         String userInput = Console.readLine();
-        if (!lottoValidation.validate(userInput)) throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR_MSG);
+        if (!lottoValidation.validate(userInput)) throw new IllegalArgumentException(BONUS_NUMBER_ERROR_MSG);
 
         int bonusNumber = Integer.parseInt(userInput);
-        if (winningNumbers.contains(bonusNumber)) throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR_MSG);
+        if (winningNumbers.contains(bonusNumber)) throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER_ERROR_MSG);
         return bonusNumber;
     }
 }

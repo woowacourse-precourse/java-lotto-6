ebd51490a97cd86da6a59f49f7c35498f20367e9
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-11-07 18:22:43 +0900

    test : LottoResultTest 추가

---
+++ src/test/java/lotto/LottoResultTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoResultTest {
+    @Test
+    @DisplayName("잘못된 matchCount 입력시 오류")
+    void wrong_Match_Result() {
+          assertThatThrownBy(() -> LottoResult.getResult(7, false))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

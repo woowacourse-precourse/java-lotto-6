38abb24dce6f8665e11e26bb2d4c1471e2f13a05
taehoon <xogns1514@gmail.com>
2023-11-07 23:21:05 +0900

    fix(LottoPlayerNumber): 로또 번호 생성 오류 수정
    - pickNumberInRange -> pickUniqueNumbersInRange 함수 이용으로 변경함.

--- src/main/java/lotto/model/LottoPlayerNumber.java
+++ src/main/java/lotto/model/LottoPlayerNumber.java
@@ -3,9 +3,7 @@ package lotto.model;
 import static lotto.model.Constant.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoPlayerNumber {
@@ -20,15 +18,11 @@ public class LottoPlayerNumber {
     }
 
     private List<Integer> generateLottoNumber() {
-        Set<Integer> lottoPlayerNumber = new HashSet<>();
-
-        while (lottoPlayerNumber.size() < LOTTO_NUMBER_COUNT) {
-            lottoPlayerNumber.add(Randoms.pickNumberInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX));
-        }
+        List<Integer> lottoPlayerNumber = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT);
         return sortLottoNumber(lottoPlayerNumber);
     }
 
-    private static List<Integer> sortLottoNumber(Set<Integer> lottoPlayerNumber) {
+    private static List<Integer> sortLottoNumber(List<Integer> lottoPlayerNumber) {
         return lottoPlayerNumber.stream()
                 .sorted()
                 .collect(Collectors.toList());

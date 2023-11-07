7d739d80178c5dbbe17280dc065bad0e6baa21fb
ay-eonii <naayen@naver.com>
2023-11-08 02:03:22 +0900

    refactor(message): 예외 메시지 자세하게 변경

--- src/main/java/lotto/view/constant/Exception.java
+++ src/main/java/lotto/view/constant/Exception.java
@@ -8,6 +8,6 @@ public class Exception {
     public static final String LOTTO_DUPLICATED = "[ERROR] 로또 번호는 중복될 수 없습니다.";
     public static final String LOTTO_SIZE = "[ERROR] 로또 번호는 6자리여야 합니다.";
     public static final String LOTTO_RANGE = "[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.";
-    public static final String BONUS_FORMAT = "[ERROR] 보너스 번호는 숫자여야 합니다.";
+    public static final String BONUS_FORMAT = "[ERROR] 보너스 번호는 하나의 숫자여야 합니다.";
     public static final String BONUS_DUPLICATED = "[ERROR] 보너스 번호는 중복될 수 없습니다.";
 }

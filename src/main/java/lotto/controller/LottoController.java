e185f0cc7ff06dec3d397b040b1b393295b7a50c
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-07 23:20:07 +0900

    refactor(LottoController): 메소드 분리 및 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,79 +1,81 @@
 package lotto.controller;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.LottoFactory;
-import lotto.domain.LottoBundle;
+import lotto.domain.LottoOrder;
+import lotto.domain.LottoResult;
 import lotto.domain.Money;
-import lotto.domain.PurchasedLotto;
-import lotto.domain.WinningNumbers;
+import lotto.domain.WinningNumber;
+import lotto.domain.constant.LottoConstant;
 import lotto.domain.number.RandomNumberGenerator;
+import lotto.util.Convertor;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
+    private final LottoFactory lottoFactory;
 
-    public void start() {
-        purchasedLotto(inputMoney());
-        winningLotto();
+    public LottoController() {
+        this.lottoFactory = new LottoFactory(new RandomNumberGenerator());
     }
 
-    private void purchasedLotto(final Money inputMoney) {
-        OutputView.printNewLine();
-        PurchasedLotto purchasedLotto = new PurchasedLotto(inputMoney);
-        LottoBundle lottoBundle = LottoFactory.createLottos(purchasedLotto.getBuyQuantity(), new RandomNumberGenerator());
-        OutputView.printBuyQuantity(purchasedLotto);
-        OutputView.printBuyLotto(lottoBundle);
-        OutputView.printNewLine();
+    public void startGame() {
+        Money bettingMoney = getBettingMoney();
+        LottoOrder lottoOrder = createLottoOrder(bettingMoney);
+        displayLottoOrder(lottoOrder);
+        LottoResult lottoResult = calculateLottoResult(lottoOrder);
+        float profitRate = calculateProfitRate(bettingMoney, lottoResult);
+        displayGameResult(lottoResult, profitRate);
     }
 
-    private WinningNumbers winningLotto() {
-        Lotto winningLottoNumbers = getInputWinningLottoNumbers();
-        OutputView.printNewLine();
-        BonusNumber bonusLottoBonusNumber = getInputBonusLottoNumber();
-        OutputView.printNewLine();
-        return new WinningNumbers(winningLottoNumbers, bonusLottoBonusNumber);
+    private static Money getBettingMoney() {
+        return tryUntilSuccess(InputView::inputMoney, Money::of);
     }
 
-    private BonusNumber getInputBonusLottoNumber() {
-        try {
-            return BonusNumber.of(InputView.inputBonusLottoNumber());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e);
-            return getInputBonusLottoNumber();
-        }
+    private LottoOrder createLottoOrder(Money money) {
+        int quantity = money.calculateLottoQuantity(LottoConstant.PRICE.getValue());
+        return LottoOrder.of(money, lottoFactory.createBundle(quantity));
     }
 
-    private static Money inputMoney() {
-        try {
-            return Money.of(InputView.inputMoney());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e);
-            return inputMoney();
-        }
+    private static void displayLottoOrder(LottoOrder lottoOrder) {
+        OutputView.printBuyQuantity(lottoOrder, LottoConstant.PRICE.getValue());
+        OutputView.printBuyLotto(lottoOrder.getLottos());
+    }
+
+    private LottoResult calculateLottoResult(LottoOrder lottoOrder) {
+        Lotto winningNumber = getInputWinningLottoNumbers();
+        return LottoResult.of(lottoOrder.getLottos(), createWinningLotto(winningNumber));
     }
 
     private static Lotto getInputWinningLottoNumbers() {
+        return tryUntilSuccess(InputView::inputWinningLottoNumbers,
+                input -> new Lotto(Convertor.convertToIntegerList(input)));
+    }
+
+    private static WinningNumber createWinningLotto(Lotto winningNumber) {
+        return tryUntilSuccess(() -> BonusNumber.of(InputView.inputBonusLottoNumber()),
+                bonusNumber -> new WinningNumber(winningNumber, bonusNumber));
+    }
+
+    private static float calculateProfitRate(Money money, LottoResult lottoResult) {
+        return money.calculateLottoProfitRate(lottoResult.calculatePrize());
+    }
+
+    private static void displayGameResult(LottoResult lottoResult, float profitRate) {
+        OutputView.printResultMessage();
+        OutputView.printLottoResult(lottoResult);
+        OutputView.printProfitRate(profitRate);
+    }
+
+    private static <T, R> R tryUntilSuccess(Supplier<T> inputSupplier, Function<T, R> constructor) {
         try {
-            String winningLottoNumbers = InputView.inputWinningLottoNumbers();
-            return new Lotto(convertToIntegerList(winningLottoNumbers));
+            return constructor.apply(inputSupplier.get());
         } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e);
-            return getInputWinningLottoNumbers();
+            return tryUntilSuccess(inputSupplier, constructor);
         }
-    }
-
-    private static List<Integer> convertToIntegerList(final String winningLottoNumbers) {
-        String[] numbers = splitNumbers(winningLottoNumbers);
-        return Arrays.stream(numbers)
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .toList();
-    }
-
-    private static String[] splitNumbers(final String winningLottoNumbers) {
-        return winningLottoNumbers.split(",");
     }
 }

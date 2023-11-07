e8ef7954ee8f123e5964b0553b1af2e082f12378
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 15:56:43 +0900

    fix(WinningRankPrize): 수익률 계산을 위해서 "원" 을 출력시로 이동. 열거형에는 숫자(콤마 포함)만 남도록 수정

--- src/main/java/lotto/WinningRankPrize.java
+++ src/main/java/lotto/WinningRankPrize.java
@@ -2,11 +2,11 @@ package lotto;
 
 public enum WinningRankPrize {
 
-    FIFTH("3개 일치", "5,000원"),
-    FOURTH("4개 일치", "50,000원"),
-    THIRD("5개 일치", "1,500,000원"),
-    SECOND("5개 일치, 보너스 볼 일치", "30,000,000원"),
-    FIRST("6개 일치", "2,000,000,000원");
+    FIFTH("3개 일치", "5,000"),
+    FOURTH("4개 일치", "50,000"),
+    THIRD("5개 일치", "1,500,000"),
+    SECOND("5개 일치, 보너스 볼 일치", "30,000,000"),
+    FIRST("6개 일치", "2,000,000,000");
 
     private String matchCount;
     private String prizeAmount;

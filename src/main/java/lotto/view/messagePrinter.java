152ee93882700b3aa19d7fd2d1f917161b802b37
ParkKyuIl <hakim@nex.market>
2023-11-07 14:29:51 +0900

    feat: 초반 뷰 클래스 생성

---
+++ src/main/java/lotto/view/messagePrinter.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+public class messagePrinter {
+
+    public void printPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printPurchaseAmount(int purchasedLottos){
+        System.out.println(purchasedLottos + "개를 구매했습니다.");
+    }
+
+
+
+
+}

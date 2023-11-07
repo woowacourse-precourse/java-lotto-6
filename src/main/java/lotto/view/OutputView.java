8a2d672752a32217447334fc11073046d288a27e
pwrwpw <dlgustjr9912@gmail.com>
2023-11-07 11:45:19 +0900

    feat: 통계 출력 기능 구현
    docs: 통계 출력 기능 구현 체크 완료

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String WINNING_STATISTICS_DELIMITER = "---------";
 
     public void printLottoCount(int lottoCount) {
         System.out.printf(LOTTO_COUNT_MESSAGE, lottoCount);
@@ -26,5 +28,10 @@ public class OutputView {
 
     public void getInputBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
+    }
+
+    public void printStatistics() {
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(WINNING_STATISTICS_DELIMITER);
     }
 }

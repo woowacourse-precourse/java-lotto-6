a4dd604e0f6d163897a6c5563191ffaccac1bf7e
yubin <dbqls2821@gmail.com>
2023-11-07 14:29:49 +0900

    test: 보너스 번호를 포함한 다섯개 일치인 경우 그에 맞는 객체를 반환하는 테스트코드 추가

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -17,6 +17,11 @@ class ResultTest {
     }
 
     @Test
+    void 보너스번호를_포함한_다섯개_일치인_경우_그에_맞는_객체를_반환한다() {
+        assertEquals(Result.FIVE_MATCH_WITH_BONUS, Result.of(5, true));
+    }
+
+    @Test
     void 네개_일치인_경우_그에_맞는_객체를_반환한다() {
         assertEquals(Result.FOUR_MATCH, Result.of(4, false));
     }

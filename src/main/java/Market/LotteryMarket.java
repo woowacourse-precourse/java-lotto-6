2dc1d7187e8e5b71e79b77076f9ba6e22bfbaaf5
youngduck98 <tommykimyd98@gmail.com>
2023-11-08 01:51:21 +0900

    가격에 맞추어 알맞은 수의 로또 발행 기능 구현

--- src/main/java/Market/LotteryMarket.java
+++ src/main/java/Market/LotteryMarket.java
@@ -3,16 +3,21 @@ package Market;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LotteryMarket {
-
     //복권 가격
     int LotteryPrice = 1000;
 
     //들어온 money와 LotteryPrice에 맞춰 Lotto를 List에 담아 return.
     public List<Lotto> issueLotteries(int money){
-
+        int num = money / LotteryPrice;
+        List<Lotto> ret = new ArrayList<>();
+        for(int i=0;i<num;i++){
+            ret.add(issueLotto());
+        }
+        return ret;
     }
 
     public Lotto issueLotto(){

bf837fbad2f379f337759a403b22d392c24547f7
JW0321 <digneotn123@gmail.com>
2023-11-07 18:40:41 +0900

    refactor: 생성자 private화로 인스턴스 생성 방지
    다른 클래스에서 쓰이는 상수들은 public으로 접근 제어자 변경

--- src/main/java/lotto/util/NumbersValidator.java
+++ src/main/java/lotto/util/NumbersValidator.java
@@ -5,10 +5,13 @@ import java.util.List;
 public class NumbersValidator { //검증 메소드들을 모아 놓은 클래스
 
     private static final int TOTAL_LOTTO_NUMBER = 6;
-    private static final int RANDOM_NUMBER_MIN_VALUE = 1;
-    private static final int RANDOM_NUMBER_MAX_VALUE = 45;
+    public static final int RANDOM_NUMBER_MIN_VALUE = 1;
+    public static final int RANDOM_NUMBER_MAX_VALUE = 45;
 
-    public static void validate(List<Integer> numbers) { //기본적으로 6자인 부분만 확인
+    private NumbersValidator() {
+    }
+
+    public static void validate(List<Integer> numbers) { //갯수 체크
         if (numbers.size() != TOTAL_LOTTO_NUMBER) {
             throw new IllegalArgumentException();
         }

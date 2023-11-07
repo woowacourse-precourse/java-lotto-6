f60f55fc85f4ff190881d55de9ff691974ff6597
koreanair777 <jinuklee777@soongsil.ac.kr>
2023-11-08 00:55:42 +0900

    당첨 번호 입력 리스트 만들기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,12 +9,31 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입 금액을 입력해 주세요.");
-        int buyMoney = getInteger();
+        int buyMoney = getInt();
         int number = moneyToLottoNumber(buyMoney);
         System.out.println(number + "개를 구매했습니다");
         List<Lotto> lottoList = new ArrayList<>();
         lottoToList(number, lottoList);
-        List<Integer> numbers = getNumbers();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winNumbersString = getString();
+        List<Integer> winNumbersList = new ArrayList<>();
+        fillWinNumbersList(winNumbersString, winNumbersList);
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = getInt();
+
+    }
+
+    private static void fillWinNumbersList(String winNumbersString, List<Integer> winNumbersList) {
+        String[] numbers = winNumbersString.split(",");
+        for (String numberStr : numbers) {
+            try {
+                Integer tmp = Integer.parseInt(numberStr);
+                winNumbersList.add(tmp);
+            } catch (NumberFormatException e) {
+                // 변환 중에 오류가 발생하면 처리할 수 있음
+                System.out.println("올바르지 않은 숫자 형식: " + numberStr);
+            }
+        }
     }
 
     private static void lottoToList(int number, List<Lotto> lottoList) {
@@ -32,7 +51,7 @@ public class Application {
         return Console.readLine();
     }
 
-    private static int getInteger() {
+    private static int getInt() {
         return Integer.parseInt(Console.readLine());
     }
 

869a31c01fd1654f10ada0b5d0f213c258425c7e
wochae <officialwochae@gmail.com>
2023-11-07 19:24:32 +0900

    feat(app) : application run
    실행하기 위해 뷰 객체들을 호출한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,29 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+
+        outputView.moneyInput();
+        MoneyInputView moneyInputView = new MoneyInputView();
+        outputView.issueNotice(moneyInputView.getIssueCount());
+
+        LottoTable lottoTable = new LottoTable(moneyInputView.getIssueCount());
+        outputView.getIssues(lottoTable.getLottoTable());
+
+        outputView.numbersInput();
+        NumberInputView numberInputView = new NumberInputView();
+
+        outputView.luckyInput();
+        LuckyNumberInputView luckyNumberInputView = new LuckyNumberInputView(numberInputView.getValidNumbers());
+
+        Lotto lotto = new Lotto(numberInputView.getValidNumbers());
+        LottoDispatcher lottoDispatcher = new LottoDispatcher(
+                lotto,
+                lottoTable,
+                luckyNumberInputView.getLuckyNumber()
+        );
+        Board board = new Board(lottoDispatcher);
+        outputView.staticsResult(board.getGainStatics());
+        outputView.earningResult(board.getCalculatedProfit());
     }
 }

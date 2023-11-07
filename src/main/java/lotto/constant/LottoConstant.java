63262963df00f72d487fc66d85d38a2cf486619f
sujnkim <blackout3210@naver.com>
2023-11-07 17:04:39 +0900

    feat: 로또와 관련된 상수를 따로 관리

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class LottoConstant {
+    public static final int NUMBER_SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+}

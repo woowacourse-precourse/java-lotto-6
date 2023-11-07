c5bd817c0198f70f5c5a022b665fb0f1bf153c2c
Sanghwi Lee <sanghwi47@naver.com>
2023-11-07 16:40:24 +0900

    refactor(BonusNumber) 단일책임원칙을 더 견고하게 하기위해 유효성 검사시 각 메소드가 하나의 예외를 검증하도록 수정하고, 메소드를 실행하는 메소드를 따로 구성, chore(BonusNumber) 메소드 및 변수명을 직관적으로 수정

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,38 +1,40 @@
 package lotto.model;
 
 public class BonusNumber {
-    private static final String ERROR_INVALID_INPUT_TYPE = "유효하지 않은 타입입니다. 숫자로 입력해주세요.";
-    private static final String ERROR_NULL_OR_EMPTY_INPUT = "입력값이 없거나 공백입니다.";
+    private static final String ERROR_INVALID_INPUT_TYPE = "유효하지 않은 입력입니다. 공백을 제외한 숫자로 입력해주세요";
+    private static final String ERROR_NULL_INPUT = "입력값이 없습니다. 숫자를 입력해 주세요.";
     private static final String ERROR_INPUT_NUMBER_NOT_IN_RANGE = "1-45 사이의 값을 입력해 주세요.";
     private static final String ERROR_DUPLICATED_WITH_WINNING_NUMBER = "보너스 숫자는 당첨번호와 중복될 수 없습니다.";
     private final int bonusNumber;
 
-    public BonusNumber(String bonusNumberString, WinningNumbers winningNumbers) {
-        validateNotNullOrEmpty(bonusNumberString);
-        int checkingBonusNumber = parseAndValidateSingleNumber(bonusNumberString);
-        validateBonusNumber(checkingBonusNumber, winningNumbers);
-        this.bonusNumber = checkingBonusNumber;
+    public BonusNumber(String bonusNumberInput, WinningNumbers winningNumbers) {
+        validateNotNull(bonusNumberInput);
+        int parsedBonusNumber = parseBonusNumber(bonusNumberInput);
+        validateBonusNumber(parsedBonusNumber, winningNumbers);
+        this.bonusNumber = parsedBonusNumber;
     }
 
     public int getBonusNumber() {
         return bonusNumber;
     }
 
-    private void validateNotNullOrEmpty(String numberStr) {
-        if (numberStr == null || numberStr.trim().isEmpty()) {
-            throw new IllegalArgumentException(ERROR_NULL_OR_EMPTY_INPUT);
+    private void validateNotNull(String bonusNumberInput) {
+        if (bonusNumberInput == null || bonusNumberInput.trim().isEmpty()) {
+            throw new IllegalArgumentException(ERROR_NULL_INPUT);
         }
     }
 
-    private int parseAndValidateSingleNumber(String numberStr) {
-        int parsedNumber;
+    private int parseBonusNumber(String number) {
         try {
-            parsedNumber = Integer.parseInt(numberStr.trim());
+            return Integer.parseInt(number);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_INVALID_INPUT_TYPE);
         }
-        validateNumberInRange(parsedNumber);
-        return parsedNumber;
+    }
+
+    private void validateBonusNumber(int parsedBonusNumber, WinningNumbers winningNumbers) {
+        validateNumberInRange(parsedBonusNumber);
+        validateNotInWinningNumbers(parsedBonusNumber, winningNumbers);
     }
 
     private void validateNumberInRange(int number) {
@@ -41,8 +43,8 @@ public class BonusNumber {
         }
     }
 
-    private void validateBonusNumber(int number, WinningNumbers winningNumbers) {
-        if (winningNumbers.getWinningNumbers().contains(number)) {
+    private void validateNotInWinningNumbers(int bonusNumber, WinningNumbers winningNumbers) {
+        if (winningNumbers.getWinningNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException(ERROR_DUPLICATED_WITH_WINNING_NUMBER);
         }
     }

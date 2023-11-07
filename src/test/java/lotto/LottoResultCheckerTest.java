f93f5e2f5d0e45dd70344688b8e54f8f70a6600a
yeondori <yeonseo.jung0@gmail.com>
2023-11-08 00:29:24 +0900

    remove: check 메서드에 대한 검증 삭제(getResults 테스트와 중복됨)

--- src/test/java/lotto/LottoResultCheckerTest.java
+++ src/test/java/lotto/LottoResultCheckerTest.java
@@ -10,17 +10,6 @@ import static lotto.Result.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoResultCheckerTest {
-    @DisplayName("로또 번호와 당첨 번호를 비교해 결과를 반환한다.")
-    @Test
-    public void check() throws Exception {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-
-        LottoResultChecker lottoResultChecker = new LottoResultChecker(winningLotto);
-        Result result = lottoResultChecker.check(lotto);
-
-        assertThat(result).isEqualTo(SECOND_PRIZE);
-    }
     @DisplayName("로또 리스트와 당첨 번호를 비교해 결과 리스트를 반환한다.")
     @Test
     public void getResults() throws Exception {
@@ -28,7 +17,7 @@ class LottoResultCheckerTest {
         Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
         Lotto lotto3 = new Lotto(List.of(11, 22, 33, 44, 5, 45));
         List<Lotto> lottos = new ArrayList<>(List.of(lotto1, lotto2, lotto3));
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningLotto winningLotto = WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 7);
 
         LottoResultChecker lottoResultChecker = new LottoResultChecker(winningLotto);
         List<Result> results = lottoResultChecker.getResults(lottos);

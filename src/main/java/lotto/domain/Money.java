f63c8ad2817170e32cfed35ebb1db2e1b608563c
Jae-Young98 <ygy0102@gmail.com>
2023-11-08 00:27:50 +0900

    feat(Money): 투입금액 검증 및 발행횟수를 반환하는 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,7 +4,30 @@ public class Money {
     private final long money;
 
     public Money(long money) {
+        validateMoney(money);
         this.money = money;
+    }
+
+    private void validateMoney(long money) {
+        if (isLackMoney(money)) {
+            throw new IllegalArgumentException("금액은 1,000원 이상 투입 되어야 합니다.");
+        }
+
+        if (isWrongMoney(money)) {
+            throw new IllegalArgumentException("금액은 1,000원 단위로 투입 되어야 합니다.");
+        }
+    }
+
+    private boolean isLackMoney(long money) {
+        return money < 1000;
+    }
+
+    private boolean isWrongMoney(long money) {
+        return money % 1000 != 0;
+    }
+
+    public int getPurchaseCount() {
+        return (int) money / 1000;
     }
 
     public long getMoney() {

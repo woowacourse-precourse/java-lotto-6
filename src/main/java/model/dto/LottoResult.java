6fa21a097c73b05180fa7b8d78540fd2d86794af
WonyuChoi <phillip07290@naver.com>
2023-11-07 22:58:49 +0900

    refactor: 보너스 볼 조건 객체 활용에 의한 수정

--- src/main/java/model/dto/LottoResult.java
+++ src/main/java/model/dto/LottoResult.java
@@ -5,7 +5,7 @@ import model.LottoRank;
 public record LottoResult(int equalCount, boolean hasBonusBall, long reward, int count) {
 
     public static LottoResult of(final LottoRank rank, final int count) {
-        return new LottoResult(rank.getEqualCount(), rank.getBonusBallCondition(),
+        return new LottoResult(rank.getEqualCount(), rank.mustSameBonusBall(),
             rank.getReward(), count);
     }
 }

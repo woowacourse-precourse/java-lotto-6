8cbcdd0b7e22083a9503c6e75466d4b1adafe894
hyungjin <hyungzin0309@naver.com>
2023-11-07 20:57:24 +0900

    feat: 로또 게임 어플리케이션 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.lotto.LottoGame;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
     }
 }

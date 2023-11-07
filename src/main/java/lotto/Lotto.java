c58b6f37ede11e838e75d95c8612feacbc31c9e8
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 03:48:32 +0900

    refactor: 실수로 변경한 부분 복구

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,7 +13,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE.getNumber()) {
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }

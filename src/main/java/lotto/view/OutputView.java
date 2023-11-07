ba4228f1dd31ff5044d7cf62e534ef2cec83e7d6
ohjuyoungg <ejrm12@naver.com>
2023-11-08 04:23:08 +0900

    refactor: 변수 자료형 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.service.LottoService;
 import lotto.service.LottoWinningCalculator;
 import lotto.utils.constants.Comment;
@@ -12,16 +13,15 @@ public class OutputView {
         System.out.println(Comment.INPUT_PURCHASE_AMOUNT.getComment());
     }
 
-    public static void printLottoPurchaseAmount(int lottoNumber) {
-        LottoService.amountPurchaseDivide(lottoNumber);
+    public static void printLottoPurchaseAmount(int purchaseAmount) {
+        LottoService.amountPurchaseDivide(purchaseAmount);
         System.out.println();
-        System.out.println(lottoNumber + Comment.OUTPUT_PURCHASE_AMOUNT.getComment());
+        System.out.println(purchaseAmount + Comment.OUTPUT_PURCHASE_AMOUNT.getComment());
     }
 
-    public static void printLottoNumber(List<List<Integer>> lottoAllNumbers) {
-        for (List<Integer> lottoNumbers : lottoAllNumbers) {
-            System.out.println(lottoNumbers);
-        }
+    public static void printLottoNumber(List<Lotto> lottoAllNumbers) {
+        System.out.println(lottoAllNumbers);
+
         System.out.println();
         System.out.println(Comment.INPUT_WINNING_NUMBER.getComment());
     }
@@ -31,13 +31,9 @@ public class OutputView {
         System.out.println(Comment.INPUT_BONUS_NUMBER.getComment());
     }
 
-    public static void printLottoWinningStatistics(List<Integer> lottoWinningCounts) {
+    public static void printLottoWinningStatistics() {
         System.out.println();
         System.out.println(Comment.INPUT_THREE_DASH.getComment());
         System.out.println(Comment.INPUT_WINNING_STATISTICS.getComment());
-    }
-
-    public static void printLottoTotalProfit(List<Integer> lottoWinningCounts, int purchaseAmount) {
-        LottoWinningCalculator.printTotalProfit(lottoWinningCounts, purchaseAmount);
     }
 }

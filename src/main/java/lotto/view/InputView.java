72b3d4ddc24666be261c930b79177e0fac33e985
Dowon <3120kang@gmail.com>
2023-11-08 00:31:32 +0900

    Feat(InputView): 당첨번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,12 +7,18 @@ public class InputView {
     public int inputMoney() {
         while (true) {
             try {
+                System.out.println("구입금액을 입력해 주세요.");
                 String money = Console.readLine();
                 return Integer.parseInt(money);
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 금액은 숫자를 입력해야 합니다.");
             }
         }
+    }
+
+    public String inputLucky(){
+        System.out.println("당첨 번호를 입력해주세요.");
+        return Console.readLine().trim();
     }
 
 

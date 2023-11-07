ef74d1582530cb41e84b88a68bcaa08bae6f5bda
KimChanJin97 <cjkimhello97@gmail.com>
2023-11-07 16:27:34 +0900

    feat: reorganize Lotto & implement LottoNotSizeSixException

---
+++ src/main/java/lotto/domain/common/Lotto.java
@@ -0,0 +1,27 @@
+package lotto.domain.common;
+
+
+import lotto.exception.Lotto.LottoNotSizeSixException;
+
+import java.util.List;
+
+
+public record Lotto(List<Integer> numbers) {
+
+    private static final int LOTTO_MAX = 45;
+    private static final int LOTTO_MIN = 1;
+    private static final int LOTTO_SIZE = 6;
+
+
+    public Lotto {
+        validateLottoSize(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new LottoNotSizeSixException();
+        }
+    }
+
+
+}

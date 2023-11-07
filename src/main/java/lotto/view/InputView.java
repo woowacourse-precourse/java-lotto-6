cb0d2af52333762cd919671d8d2cc93a52fdc89c
sw801733 <sw801733@naver.com>
2023-11-07 22:45:08 +0900

    feat : InputView 클래스의 당첨 번호 입력 기능 추가
    - 당첨 번호 입력을 위한 inputLotto 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,19 +1,33 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
     private int lottoPurchase;
-    private List<Integer> Lotto;
+    private List<Integer> lotto;
     private int bonusNumber;
 
     public InputView() {
         this.lottoPurchase = Integer.parseInt(readLine());
+        this.lotto = inputLotto();
 
     }
 
+    private List<Integer> inputLotto() {
+
+        List<Integer> lotto = new ArrayList<>();
+
+        String[] strLotto = readLine().split(",");
+
+        for(String str : strLotto) {
+            lotto.add(Integer.valueOf(str));
+        }
+
+        return lotto;
+    }
 
 
 }

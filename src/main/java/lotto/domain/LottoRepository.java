37adda8660c5202e491ba5b3d22abbc4ef6d032f
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:54:55 +0900

    feat(LottoRepository) : size() 기능 추가

--- src/main/java/lotto/domain/LottoRepository.java
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class LottoRepository {
     private List<Lotto> lottoRepository = new ArrayList<>();
 
+    public int size() {
+        return lottoRepository.size();
+    }
 
     public void add(Lotto lotto) {
         lottoRepository.add(lotto);

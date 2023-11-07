1d45161744ba784e0db5f97087ebde0b621e1f03
harandal24601 <devchaehokim@gmail.com>
2023-11-08 07:03:39 +0900

    refactor : Refactor to re-execute input
    Change code to re-do input

--- src/main/java/lotto/purchase/Purchase.java
+++ src/main/java/lotto/purchase/Purchase.java
@@ -1,6 +1,9 @@
 package lotto.purchase;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.handler.ExceptionHandler.handle;
+
 public class Purchase {
 
     public static void main(String[] args) {
@@ -8,22 +11,47 @@ public class Purchase {
 
     public static int purchase() {
         System.out.println("구입금액을 입력해 주세요.");
-        String rawMoney = Console.readLine();
-
-        checkNull(rawMoney);
-        checkNumberFormat(rawMoney);
-
+        String rawMoney = getValidMoneyFromUser();
         int money = Integer.parseInt(rawMoney);
-        checkMoney(money);
+
+        money = updateMoneyWithValidation(rawMoney, money);
+
         int count = money / 1000;
         System.out.println(count + "개를 구매했습니다.");
-
         return count;
+    }
+
+    private static String getValidMoneyFromUser() {
+        String rawMoney = Console.readLine();
+
+        while (true) {
+            try {
+                checkNull(rawMoney);
+                checkNumberFormat(rawMoney);
+                return rawMoney;
+            } catch (NullPointerException | NumberFormatException e) {
+                handle(e);
+                rawMoney = Console.readLine();
+            }
+        }
+    }
+
+    private static int updateMoneyWithValidation(String rawMoney, int money) {
+        while (true) {
+            try {
+                checkMoney(money);
+                return money;
+            } catch (IllegalArgumentException e) {
+                handle(e);
+                rawMoney = getValidMoneyFromUser();
+                money = Integer.parseInt(rawMoney);
+            }
+        }
     }
 
     private static void checkNull(String input) {
         if (input == null || input.isEmpty()) {
-            throw new NullPointerException("String is null or empty. \nerrorVar : " + input);
+            throw new NullPointerException("String is null or empty. \n └─ errorVar : " + input);
         }
     }
 
@@ -31,17 +59,17 @@ public class Purchase {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("Invalid number format. \nerrorVar : " + input);
+            throw new NumberFormatException("Invalid number format. \n └─ errorVar : " + input);
         }
     }
 
     private static void checkMoney(int money) {
         try {
             if (money < 1000) {
-                throw new IllegalArgumentException("최소 1000원 이상의 금액을 입력해주세요.");
+                throw new IllegalArgumentException("Input at least over 1000₩. \n └─ errorVar : " + money);
             }
             if (money % 1000 != 0) {
-                throw new IllegalArgumentException("1000원 단위로만 구매 가능합니다.");
+                throw new IllegalArgumentException("Input multiple of 1000₩. \n └─ errorVar : " + money);
             }
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e.getMessage());

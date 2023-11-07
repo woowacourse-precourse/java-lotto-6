0481628d761d691045eafe31a45734592fcb620c
Identityless <rlandwnd555@naver.com>
2023-11-07 20:47:22 +0900

    feat : 등 수 매핑 유틸 추가

---
+++ src/main/java/lotto/GradeUtil.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public class GradeUtil {
+    public static Grade gradeMapper(int corrects, boolean bonus) {
+        if (corrects == 6) {
+            return Grade.FORTH_GRADE;
+        }
+        if (corrects == 5) {
+            return isBonus(bonus);
+        }
+        if (corrects == 4) {
+            return Grade.FORTH_GRADE;
+        }
+        if (corrects == 3) {
+            return Grade.FIFTH_GRADE;
+        }
+        return Grade.NONE;
+    }
+
+    private static Grade isBonus(boolean bonus) {
+        if (bonus) {
+            return Grade.SECCOND_GRADE;
+        }
+        return Grade.THIRD_GRADE;
+    }
+}

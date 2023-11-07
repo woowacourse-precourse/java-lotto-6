b582998c84b950c0664ce1c0e3b9787e76b03d19
준써크 <kjs8960@inha.edu>
2023-11-08 03:11:23 +0900

    feat: 구매 로또 생성 함수 생성

--- src/main/java/lotto/Controller/Controller.java
+++ src/main/java/lotto/Controller/Controller.java
@@ -1,8 +1,15 @@
 package lotto.Controller;
 
 import lotto.View.View;
+import lotto.constant.JudgeBonus;
+import lotto.constant.LottoRanking;
 import lotto.domain.*;
-import lotto.mapper.Mapper;
+import lotto.utils.RandomLottoGenerator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Controller {
 
     private final View view;
@@ -13,11 +20,17 @@ public class Controller {
 
     public void start() {
         PurchaseMoney purchaseMoney = insertMoney();
-
+        List<Lotto> userLottos = purchaseLotto(purchaseMoney);
     }
 
     private PurchaseMoney insertMoney() {
         return new PurchaseMoney(view.getPurchasePrice());
     }
 
+    private List<Lotto> purchaseLotto(PurchaseMoney purchaseMoney) {
+        RandomLottoGenerator randomLottoGenerator = new RandomLottoGenerator();
+        List<Lotto> userLottos = randomLottoGenerator.generateMultipleLotto(purchaseMoney.getCount());
+        view.printIssuanceLotto(userLottos);
+        return userLottos;
+    }
 }
--- src/main/java/lotto/View/View.java
+++ src/main/java/lotto/View/View.java
@@ -1,7 +1,9 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 
+import java.util.List;
 
 public class View {
 
@@ -9,5 +11,12 @@ public class View {
         System.out.println("구입금액을 입력해 주세요.");
         String userInput = Console.readLine();
         return Integer.parseInt(userInput);
+    }
+
+    public void printIssuanceLotto(List<Lotto> userLottos) {
+        System.out.println(userLottos.size() + "개를 구매했습니다.");
+        for(Lotto lotto : userLottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

f5eaf1f9eb3459227e4a51d65a98d93eace35c8f
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 03:05:21 +0900

    feat(printLottosStatics): 로또 개별 통계 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,14 @@
 package lotto.view;
 
 import lotto.model.WinningCriteria;
+import java.util.HashMap;
 import java.util.List;
 
 public class OutputView {
     private static final String LINE_SPACE = "\n";
     private static final String LINE_DIVIDER = "---";
+    private static final String WINNING_INFO_SEPARATOR = " - ";
+    private static final String WINNING_COUNT_UNIT = "개";
     private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
@@ -18,6 +21,13 @@ public class OutputView {
             WinningCriteria.SECOND,
             WinningCriteria.FIRST
     );
+    private static final HashMap<WinningCriteria, String> rankMessages = new HashMap<>() {{
+        put(WinningCriteria.FIRST, RankMessages.FIRST_RANK_MESSAGE);
+        put(WinningCriteria.SECOND, RankMessages.SECOND_RANK_MESSAGE);
+        put(WinningCriteria.THIRD, RankMessages.THIRD_RANK_MESSAGE);
+        put(WinningCriteria.FOURTH, RankMessages.FOURTH_RANK_MESSAGE);
+        put(WinningCriteria.FIFTH, RankMessages.FIFTH_RANK_MESSAGE);
+    }};
 
     public void printLottoPurchaseInputMessage() {
         System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
@@ -39,15 +49,23 @@ public class OutputView {
         System.out.println(numbers);
     }
 
-    public void printLottosStatics() {
+    public void printLottosStatics(HashMap<WinningCriteria, Integer> winningRecords) {
         System.out.println(LOTTO_STATISTICS);
         for (WinningCriteria currentRank : orderPrintRank) {
-            printLottoStatics();
+            printLottoStatics(
+                    rankMessages.get(currentRank),
+                    winningRecords.getOrDefault(currentRank, 0)
+            );
         }
         printRateOfReturn();
     }
 
-    private void printLottoStatics() {
+    private void printLottoStatics(String rankMessage, int winningRecords) {
+        String printMessage = rankMessage
+                + WINNING_INFO_SEPARATOR
+                + winningRecords
+                + WINNING_COUNT_UNIT;
+        System.out.println(printMessage);
     }
 
     private void printRateOfReturn() {

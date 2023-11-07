77784a4db3a57023e4e1224f23c46176d1e47bce
wonjun <wonjon0930@daum.net>
2023-11-08 03:46:31 +0900

    feat: add feature to issue lottos as much as amount of user money

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,19 +1,30 @@
 package lotto.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.domain.UserMoney;
 import lotto.domain.WinningLottoNumbers;
 import lotto.domain.constant.LottoConstant;
+import lotto.service.util.LottoGenerator;
 
 public class LottoService {
     public UserMoney initUserMoney(final long userMoney) {
         return UserMoney.from(userMoney);
     }
 
-    public long calculateNumberOfLottos(UserMoney userMoney) {
-        return userMoney.getUserMoney() / LottoConstant.LOTTO_PRICE.getValue();
+    public Lottos createUserLottos(final long userMoney) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (long i = 0; i < calculateNumberOfLottos(userMoney); i++) {
+            lottos.add(LottoGenerator.generateLotto());
+        }
+        return Lottos.from(lottos);
+    }
+
+    private long calculateNumberOfLottos(final long userMoney) {
+        return userMoney / LottoConstant.LOTTO_PRICE.getValue();
     }
 
     public WinningLottoNumbers initWinningLottoNumbers(final List<Integer> winningLottoNumbers) {

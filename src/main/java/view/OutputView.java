61a5124aa02e5f951454bf36f945654e9911067e
junseoks-99 <angec5645@naver.com>
2023-11-08 01:15:21 +0900

    refactor: 5등 당첨 횟수 출력 함수 분리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -18,6 +18,10 @@ public class OutputView {
 
     public static void outputWinningStatistics() {
         System.out.println(OutputMessages.WINNING_STATISTICS.getMessage());
+        outputFifthPlaceCount();
+    }
+
+    private static void outputFifthPlaceCount() {
         System.out.println(String.format(OutputMessages.FIFTH_PLACE.getMessage(),
                 RankRepository.getWinningStatistics(WinnerRank.FIFTH.getRank())));
     }

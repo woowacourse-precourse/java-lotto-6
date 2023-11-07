a4374e63f9463695065bf70f370da3ece9c9fc5a
ppdoli123 <ppdoli123@naver.com>
2023-11-07 15:33:24 +0900

    feat(OutputView): view에 해당되는 OutputView 초기 코드 작성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import lotto.Lotto;
+import lotto.domain.LottoResult;
+
+import java.util.List;
+
+    // 발행된 로또 번호 출력 로직 작성 필요
+public class OutputView {
+    public void printTickets(List<Lotto> tickets) {
+
+    }
+
+    // 당첨 통계 출력 로직 작성 필요
+    public void printResult(LottoResult result) {
+
+    }
+}

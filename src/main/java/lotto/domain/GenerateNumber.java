7ca2bbc73fe4d869d61dbe365f353f825f93e037
sejung2 <twindrop@naver.com>
2023-11-07 21:00:41 +0900

    입력받은 액수에 맞는 로또 생성

--- src/main/java/lotto/domain/GenerateNumber.java
+++ src/main/java/lotto/domain/GenerateNumber.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class Generate {
+public class GenerateNumber {
     public List<Integer> randomLottoCreate() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }

eeb502a459c7a73b06a3e2597d281098b8d161e5
Hugh <lee2317884@gmail.com>
2023-11-07 10:53:39 +0900

    refactor: 구입 금액 테스트 코드 공통 사항 메서드 화

--- src/test/java/lotto/domain/amount/AmountTest.java
+++ src/test/java/lotto/domain/amount/AmountTest.java
@@ -10,33 +10,35 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class AmountTest {
-    // 양수 인지 테스트
+
+    private void exceptionVerification(final int amount, final String message) {
+        assertThatThrownBy(() -> new Amount(amount))
+                .isInstanceOf(IllegalStateAmountException.class)
+                .hasMessageContaining(message);
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, -10000, -2000, -100})
     @DisplayName("양의 정수가 아닌 구입 금액은 입력될 수 없다.")
     void amountIsNotPositive(final int amount) {
-        assertThatThrownBy(() -> new Amount(amount))
-                .isInstanceOf(IllegalStateAmountException.class)
-                .hasMessageContaining(AmountExceptionStatus.AMOUNT_IS_NOT_POSITIVE.getMessage());
+        String message = AmountExceptionStatus.AMOUNT_IS_NOT_POSITIVE.getMessage();
+        exceptionVerification(amount, message);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {999, 100, 990, 340, 200})
     @DisplayName("1000 보다 작은 구입 금액은 입력될 수 없다.")
     void amountIsNotAboveTest(final int amount) {
-        assertThatThrownBy(() -> new Amount(amount))
-                .isInstanceOf(IllegalStateAmountException.class)
-                .hasMessageContaining(AmountExceptionStatus.AMOUNT_IS_NOT_ABOVE.getMessage());
+        String message = AmountExceptionStatus.AMOUNT_IS_NOT_ABOVE.getMessage();
+        exceptionVerification(amount, message);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {100020, 20200, 80030, 150300, 30002})
     @DisplayName("1000 단위로 나누어 떨어지지 않는 구입 금액은 입력될 수 없다.")
     void amountIsNotDivisibleTest(final int amount) {
-        assertThatThrownBy(() -> new Amount(amount))
-                .isInstanceOf(IllegalStateAmountException.class)
-                .hasMessageContaining(AmountExceptionStatus.AMOUNT_IS_NOT_DIVISIBLE.getMessage());
+        String message = AmountExceptionStatus.AMOUNT_IS_NOT_DIVISIBLE.getMessage();
+        exceptionVerification(amount, message);
     }
 
     @ParameterizedTest

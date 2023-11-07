29d92325012508e813e4714a2a6dc438e73c5b25
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-07 11:02:40 +0900

    refactor: 매직넘버 제거

--- src/main/java/lotto/generator/RandomNumberGenerator.java
+++ src/main/java/lotto/generator/RandomNumberGenerator.java
@@ -1,5 +1,9 @@
 package lotto.generator;
 
+import static lotto.Option.GameOption.LOTTO_MAX_NUMBER;
+import static lotto.Option.GameOption.LOTTO_MIN_NUMBER;
+import static lotto.Option.GameOption.LOTTO_NUMBER_LENGTH;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,7 +11,8 @@ import java.util.stream.Collectors;
 public class RandomNumberGenerator implements NumberGenerator {
     @Override
     public List<Integer> create() {
-        return ascendOrder(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        return ascendOrder(Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER.getNumber(),
+                LOTTO_MAX_NUMBER.getNumber(), LOTTO_NUMBER_LENGTH.getNumber()));
     }
 
     private List<Integer> ascendOrder(List<Integer> randomNumbers) {

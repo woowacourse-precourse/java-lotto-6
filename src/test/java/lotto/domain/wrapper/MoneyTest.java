49968f7a61e48bb511fbe31b41f386b660acadd6
noxknow <chris2769@naver.com>
2023-11-07 18:14:57 +0900

    test(MoneyTest) : 구입 금액에 0원 이하의 값이 들어오면 예외가 발생한다

--- src/test/java/lotto/domain/wrapper/MoneyTest.java
+++ src/test/java/lotto/domain/wrapper/MoneyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.handler.ErrorHandler.INCONVERTIBLE_TYPE;
+import static lotto.handler.ErrorHandler.INVALID_NEGATIVE_INPUT;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MoneyTest {
@@ -16,5 +17,14 @@ public class MoneyTest {
         assertThatThrownBy(() -> Money.create(inputValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INCONVERTIBLE_TYPE.getException().getMessage());
+    }
+
+    @DisplayName("구입 금액에 0원 이하의 값이 들어오면 예외가 발생한다.")
+    @ParameterizedTest(name = "[{index}] input {0} " )
+    @ValueSource(strings = {"0", "-1", "-50"})
+    void createMoneyByInvalidInput(String inputValue) {
+        assertThatThrownBy(() -> Money.create(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NEGATIVE_INPUT.getException().getMessage());
     }
 }

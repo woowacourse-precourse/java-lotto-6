36b0595ffbe2265a20eb4b3311dd6119a904e28b
minkyuu <minkyu023@gmail.com>
2023-11-07 12:13:21 +0900

    feat: 로또 구매 금액 입력 예외처리 1(숫자가 아닌 경우)

--- src/main/java/lotto/buyer/Buyer.java
+++ src/main/java/lotto/buyer/Buyer.java
@@ -1,11 +1,15 @@
 package lotto.buyer;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.Validator;
 
 public class Buyer {
     public int buyLotto() {
         String buyAmount = Console.readLine();
         Console.close();
+        if (!Validator.isNumber(buyAmount)) {
+            throw new IllegalArgumentException();
+        }
         return Integer.parseInt(buyAmount);
     }
 }
--- src/test/java/lotto/buyer/BuyerTest.java
+++ src/test/java/lotto/buyer/BuyerTest.java
@@ -1,12 +1,14 @@
 package lotto.buyer;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BuyerTest {
 
@@ -17,7 +19,7 @@ class BuyerTest {
     @DisplayName("로또 구매 금액을 입력 받는다.")
     @ParameterizedTest
     @ValueSource(ints = {1000, 2000, 3000, 4000, 5000})
-    void 로또_구매_금액_입력(int expectedValue) throws Exception {
+    void 로또_구매금액_입력(int expectedValue) throws Exception {
         //given
         Buyer buyer = new Buyer();
         provideRemoteInput(String.valueOf(expectedValue));
@@ -27,6 +29,19 @@ class BuyerTest {
 
         //then
         assertThat(buyAmount).isEqualTo(expectedValue);
+    }
+
+    @DisplayName("로또 구매 금액 예외처리 - 숫자가 아닌 경우 예외 발생")
+    @Test
+    void 로또_구매금액_입력_예외처리_숫자가_아닌_경우() throws Exception {
+        //given
+        Buyer buyer = new Buyer();
+        String expectedValue = "난문자";
+        provideRemoteInput(expectedValue);
+
+        //when & then
+        assertThatThrownBy(() -> buyer.buyLotto())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }

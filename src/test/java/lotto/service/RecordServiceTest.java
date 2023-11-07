42e19358070807752d8089c59922317733159f14
김효준 <khyojun1104@gmail.com>
2023-11-08 01:27:55 +0900

    test(lotto): 내부 클래스로 만들지 말고 외부 기능을 위한 테스트로 사용하기 위한 리팩토링!

--- src/test/java/lotto/service/RecordServiceTest.java
+++ src/test/java/lotto/service/RecordServiceTest.java
@@ -1,26 +1,23 @@
 package lotto.service;
 
-import static lotto.constant.NumberConstant.SAME_COUNT_FIVE;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import lotto.constant.NumberConstant;
 import lotto.domain.LottoTicket;
 import lotto.domain.Result;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 class RecordServiceTest {
 
+    private RecordService recordService;
 
     @BeforeEach
     void init() {
+        recordService = new RecordService();
         for (Result value : Result.values()) {
             value.init();
         }
@@ -32,17 +29,27 @@ class RecordServiceTest {
     void recordResultTest() {
         //given
         List<LottoTicket> lottoTickets = new ArrayList<>();
-        lottoTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6), 3, false));
-        lottoTickets.add(new LottoTicket(List.of(3, 4, 5, 6, 7, 8), 4, false));
-        lottoTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6), 5, false));
-        lottoTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6), 6, false));
+        lottoTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6)));
+        lottoTickets.add(new LottoTicket(List.of(3, 4, 5, 6, 7, 8)));
+        lottoTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6)));
+        lottoTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6)));
+
+        madeTestCaseNoBonusLottoTickets(lottoTickets);
+
         //when
-        recordResult(lottoTickets);
+        recordService.recordResult(lottoTickets);
         //then
         Assertions.assertThat(Result.FIFTH.getResultCount()).isEqualTo(1);
         Assertions.assertThat(Result.FORTH.getResultCount()).isEqualTo(1);
         Assertions.assertThat(Result.THIRD.getResultCount()).isEqualTo(1);
         Assertions.assertThat(Result.FIRST.getResultCount()).isEqualTo(1);
+    }
+
+    private void madeTestCaseNoBonusLottoTickets(List<LottoTicket> lottoTickets) {
+        for(int ticketCount = 0; ticketCount<lottoTickets.size(); ticketCount++)
+            for (int repeat = 0; repeat < ticketCount + 3; repeat++) {
+                lottoTickets.get(ticketCount).addSameCount();
+            }
     }
 
 
@@ -51,68 +58,26 @@ class RecordServiceTest {
     void recordBonusResultTest() {
         //given
         List<LottoTicket> bonusTickets = new ArrayList<>();
-        bonusTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6), 5, true));
-        bonusTickets.add(new LottoTicket(List.of(3, 4, 5, 6, 7, 8), 5, true));
+
+        bonusTickets.add(new LottoTicket(List.of(1, 2, 3, 4, 5, 6)));
+        bonusTickets.add(new LottoTicket(List.of(3, 4, 5, 6, 7, 8)));
+
+        madeTestCaseBonusLottoTickets(bonusTickets);
+
         //when
-        recordResult(bonusTickets);
+        recordService.recordResult(bonusTickets);
         //then
 
         Assertions.assertThat(Result.SECOND.getResultCount()).isEqualTo(2);
     }
 
-
-    static class LottoTicket {
-
-        private final List<Integer> numbers;
-        private int sameCount;
-
-        private boolean bonus;
-
-        public LottoTicket(List<Integer> numbers, int sameCount, boolean bonus) {
-            this.numbers = numbers;
-            this.sameCount = sameCount;
-            this.bonus = bonus;
-        }
-
-        public int getSameCount() {
-            return sameCount;
-        }
-
-        public boolean isBonus() {
-            return bonus;
-        }
-    }
-
-    public void recordResult(List<LottoTicket> lottoTickets) {
-        for (LottoTicket lottoTicket : lottoTickets) {
-            for (Result value : Result.values()) {
-                recordProcess(lottoTicket, value);
+    private void madeTestCaseBonusLottoTickets(List<LottoTicket> bonusTickets) {
+        for (LottoTicket bonusTicket : bonusTickets) {
+            for (int repeat = 0; repeat < NumberConstant.SAME_COUNT_FIVE.getNumber(); repeat++) {
+                bonusTicket.addSameCount();
             }
+            bonusTicket.hasBonus();
         }
-    }
-
-    private void recordProcess(LottoTicket lottoTicket, Result value) {
-        if (lottoTicket.getSameCount() == value.getSameCount()) {
-            addResultCount(lottoTicket, value);
-        }
-    }
-
-    private void addResultCount(LottoTicket lottoTicket, Result value) {
-        if (checkIsSecond(lottoTicket, value)) {
-            Result.SECOND.addCount();
-            return;
-        }
-        if (isNoBonus(lottoTicket, value)) {
-            value.addCount();
-        }
-    }
-
-    private boolean isNoBonus(LottoTicket lottoTicket, Result value) {
-        return !lottoTicket.isBonus() && !value.isBonus();
-    }
-
-    private boolean checkIsSecond(LottoTicket lottoTicket, Result value) {
-        return lottoTicket.isBonus() && value.isBonus();
     }
 
 }

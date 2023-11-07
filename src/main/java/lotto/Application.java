cb09993f2a4a6373cb4e4dcd48205c93648b5154
junseoks-99 <angec5645@naver.com>
2023-11-07 22:08:46 +0900

    feat: 로또 4등 여부 확인 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,13 +7,11 @@ public class Application {
     public static void main(String[] args) {
 
         List<Integer> list = List.of(1,2,3,4,5,6);
-        List<Integer> list2 = List.of(1,2,3,4,5,8);
+        List<Integer> list2 = List.of(1,2,3,4,7,8);
         Lotto lotto = new Lotto(list);
-        Integer bonusNumber = 40;
 
         Integer sameSount = lotto.getSameNumberCount(list2);
         System.out.println(sameSount);
-        System.out.println(lotto.containsBonusNumber(bonusNumber));
 
         RankRepository.putRank(sameSount);
 

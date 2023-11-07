be1058f98ab7d57ace3313db2b255f8c76064dc3
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-07 15:59:35 +0900

    feat(LottoTest): 로또 등수 테스트
    docs: Lotto 테스트 설명 추가 및 기능 목록 체크

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,14 +1,18 @@
 package lotto.model;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+import lotto.Application;
 import lotto.constant.ExceptionMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class LottoTest {
+class LottoTest extends NsTest {
     // 로또 번호 개수 관련 테스트
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -81,5 +85,120 @@ class LottoTest {
 
         lotto = new Lotto(List.of(45, 44, 43, 42, 41, 40));
         Assertions.assertThat(lotto).isNotNull();
+    }
+
+    // 로또 등수 반환
+    @DisplayName("3개 미만 일치 시 등수는 MISS")
+    @Test
+    void getRankByMiss() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(45, 44, 43, 42, 41, 40)
+        );
+    }
+
+    @DisplayName("3개 일치 시 등수 테스트")
+    @Test
+    void getRankByFifth() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(1, 2, 3, 45, 44, 43)
+        );
+    }
+
+    @DisplayName("4개 일치 시 등수 테스트")
+    @Test
+    void getRankByFourth() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 1개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(1, 2, 3, 4, 45, 44)
+        );
+    }
+
+    @DisplayName("5개 일치 시 등수 테스트")
+    @Test
+    void getRankByThird() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 45)
+        );
+    }
+
+    @DisplayName("5개 일치, 보너스 볼 일치 시 등수 테스트")
+    @Test
+    void getRankBySecond() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 7)
+        );
+    }
+
+    @DisplayName("6개 일치 시 등수 테스트")
+    @Test
+    void getRankByFirst() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 1개"
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6)
+        );
+    }
+
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }

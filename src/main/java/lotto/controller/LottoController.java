f71fce850c5c289e91772813df5bf074bc768158
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-11-08 05:25:26 +0900

    feat: 생성자, 금액 입력, 로또 정보 조회 컨트롤러 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,31 @@
+package lotto.controller;
+
+import lotto.service.LottoService;
+import lotto.view.LottoView;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+
+    private final LottoView lottoView;
+
+
+    public LottoController(LottoService lottoService, LottoView lottoView) {
+        this.lottoService = lottoService;
+        this.lottoView = lottoView;
+    }
+
+    public void inputMoney() {
+        lottoView.inputMoneyView();
+        lottoService.buyLottoTickets();
+    }
+
+    public void getLottoTicketsInfo() {
+        lottoView.outputBuyLottoTicketsView(lottoService.getTicketCount());
+        lottoView.outputLottoTicketNumbersView(lottoService.getLottoTickets());
+    }
+
+}

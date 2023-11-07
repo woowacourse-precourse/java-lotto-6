4114ac82ca314f0587ba51bc4012f30de5b3a335
minkyole <7719s@nave.com>
2023-11-07 15:47:50 +0900

    feat(Wallet.java) : 구입금액이 1000원으로 나누어 떨어지지 않는 경우 예외처리 구현
    - 구입금액이 1000원으로 나누어 떨어지지 않는 경우 예외처리 구현완료

--- src/main/java/lotto/model/Wallet.java
+++ src/main/java/lotto/model/Wallet.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 public class Wallet {
     private static final int MAXIMUM_PURCHASE_AMOUNT =  1000000000;
+    private static final int DIVISION_AMOUNT = 1000;
 
     private int money;
 
@@ -13,6 +14,7 @@ public class Wallet {
     private void validateMoney(String money) {
         validateNotNullMoney(money);
         validateNonNumericMoney(money);
+        validateNotDivisionMoney(money);
         validateOutOfRangeMoney(money);
     }
 
@@ -36,8 +38,17 @@ public class Wallet {
             throw new IllegalArgumentException("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
                     + MAXIMUM_PURCHASE_AMOUNT + "원.");
         }
-
     }
+
+    private void validateNotDivisionMoney(String money) {
+        int convertMoney = convertStringToInt(money);
+
+        if (convertMoney == 0 || convertMoney % DIVISION_AMOUNT != 0) {
+            throw new IllegalArgumentException("[Error] 구입 금액에 값을 1000원 단위로 넣어주세요, 최대구입금액 "
+                    + MAXIMUM_PURCHASE_AMOUNT + "원.");
+        }
+    }
+
     private int convertStringToInt(String money) {
         int convertMoney;
 

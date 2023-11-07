267d5cf0ece302b6e3311ab10daafc5b08addc3e
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 02:44:58 +0900

    feat(printBonusNumberInputMessage): 보너스번호 입력 메세지 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
     private static final String LOTTO_PURCHASE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void printLottoPurchaseInputMessage() {
         System.out.println(LOTTO_PURCHASE_INPUT_MESSAGE);
@@ -15,5 +16,9 @@ public class OutputView {
 
     public void printWinningNumbersInputMessage() {
         System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
+    }
+
+    public void printBonusNumberInputMessage() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
     }
 }

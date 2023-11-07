efab7c519ab4d05d7a2d32e007a61c3257239e25
huhdy32 <mong3268@gmail.com>
2023-11-07 19:35:26 +0900

    refactor(LottoController): 복권들의 점수 집계방식 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,9 +3,11 @@ package lotto.controller;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.logic.Logic;
 import lotto.model.Lotto;
+import lotto.model.Ranking;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoController {
     private List<Lotto> lotteries = new ArrayList<>();
@@ -14,32 +16,18 @@ public class LottoController {
         int count = payment / Logic.LOTTERY_COST;
         for (int i = 0; i < count; i++) {
             lotteries.add(new Lotto(Randoms.pickUniqueNumbersInRange(
-                    Logic.START_RANGE
-                    , Logic.END_RANGE
-                    , Logic.NUMBER_COUNT))
+                    Logic.START_RANGE,
+                    Logic.END_RANGE,
+                    Logic.NUMBER_COUNT))
             );
         }
     }
 
-    public int[] getTotalScore(List<Integer> winningNumber, int bonusNumber){
-        int[] totalScore = new int[5];
-        for (Lotto lottery : lotteries) {
-            int[] score = lottery.getScore(winningNumber, bonusNumber);
-            if (score[0] == 3) {
-                totalScore[0] ++;
-            }else if (score[0] == 4) {
-                totalScore[1] ++;
-            }else if (score[0] == 5 && score[1] == 0) {
-                totalScore[2] ++;
-            }else if (score[0] == 5 && score[1] == 1) {
-                totalScore[3] ++;
-            }else if (score[0] == 7) {
-                totalScore[4] ++;
-            }
-        }
-        return totalScore;
+    public List<Ranking> getTotalRanking(List<Integer> winningNumber, int bonusNumber) {
+        return lotteries.stream()
+                .map(lotto -> lotto.getRank(winningNumber, bonusNumber))
+                .collect(Collectors.toList());
     }
-
 
     public List<Lotto> getLotteries() {
         return lotteries;

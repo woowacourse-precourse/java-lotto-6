768052766e5c756d117a7b71449232c74d5bb631
PARK9898 <wjddjs21@naver.com>
2023-11-07 14:55:59 +0900

    "refactor: 매개변수 변경 및 로또 발급 갯수 메섣, 생성"

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -10,9 +10,10 @@ public class LottoShop {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_AMOUNT = 1000;
 
-    public List<Lotto> createLottosBy(int count) {
-        return IntStream.range(0, count).mapToObj(i -> new Lotto(createRandomLottoNumbers()))
+    public List<Lotto> createLottosBy(PurchaseAmount purchaseAmount) {
+        return IntStream.range(0, changeAmountToLottos(purchaseAmount)).mapToObj(i -> new Lotto(createRandomLottoNumbers()))
                 .collect(Collectors.toList());
     }
 
@@ -20,4 +21,10 @@ public class LottoShop {
         return Randoms.pickUniqueNumbersInRange(MIN_NUMBER,MAX_NUMBER,LOTTO_SIZE);
     }
 
+    private int changeAmountToLottos(PurchaseAmount purchaseAmount) {
+        if (!purchaseAmount.divisible(LOTTO_AMOUNT)) {
+            throw new IllegalArgumentException("[ERROR] ");
+        }
+        return purchaseAmount.changeBy(LOTTO_AMOUNT);
+    }
 }

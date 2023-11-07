3f1f7d0ae78994659c2f037e42b2eedf9a8d7d93
poow810 <poow810@naver.com>
2023-11-07 20:25:55 +0900

    test($RankTest): test for proper functioning of formatter Add new event to $RankTest: - test if the prize money is formatted

--- src/test/java/lotto/enums/RankTest.java
+++ src/test/java/lotto/enums/RankTest.java
@@ -1,5 +1,6 @@
 package lotto.enums;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,5 +16,15 @@ class RankTest {
         assertTrue(Rank.THIRD.isMatchBonus(5, false));
         assertTrue(Rank.FOURTH.isMatchBonus(4, false));
         assertTrue(Rank.FIFTH.isMatchBonus(3, false));
+    }
+
+    @DisplayName("상금 formatter가 정상 작동하는지 테스트")
+    @Test
+    void formatterTest() {
+        assertEquals("2,000,000,000", Rank.FIRST.getFormattedPrize());
+        assertEquals("30,000,000", Rank.SECOND.getFormattedPrize());
+        assertEquals("1,500,000", Rank.THIRD.getFormattedPrize());
+        assertEquals("50,000", Rank.FOURTH.getFormattedPrize());
+        assertEquals("5,000", Rank.FIFTH.getFormattedPrize());
     }
 }

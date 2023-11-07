ae0c6c21dc1a901e8102aecbc672048c6d7f04ff
Hayoon <hy97@sju.ac.kr>
2023-11-07 21:21:36 +0900

    feat(Constant): 연산에 필요한 숫자 값 상수처리

---
+++ src/main/java/lotto/util/constant/Constant.java
@@ -0,0 +1,22 @@
+package lotto.util.constant;
+
+public class Constant {
+
+    // 연산
+    public static final int PERCENTAGE = 100;
+    public static final int PER_THOUSAND = 1000;
+    public static final int PLUS_RANK = 1;
+    public static final int INITIALIZE_NUMBER_MIN = 0;
+    public static final int INITIALIZE_NUMBER_MAX = 6;
+    public static final int INITIALIZE_NUMBER = 0;
+
+    // 로또 번호
+    public static final int LOTTO_NUMBER_SIZE = 6;
+    public static final int LOTTO_MIN_RANGE = 1;
+    public static final int LOTTO_MAX_RANGE = 45;
+
+
+
+
+
+}

6a42d7321bbb4fe8528ad0437d93dd8cf3a53417
mingtorr <mingtorr@nave.com>
2023-11-07 23:27:06 +0900

    feat: 로또 번호 반환 기능 추가
    getLotto 로또 번호 반환기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,5 @@
-package lotto;
+
+package lotto.domain;
 
 import java.util.List;
 
@@ -16,5 +17,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getLotto(List<Integer> numbers) {
+        return numbers;
+    }
 }

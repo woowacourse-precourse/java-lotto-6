2f41b787b27f6d09a2e220ea2ac2facd9b846ab3
Minyongggg <naminyong97@gmail.com>
2023-11-08 02:02:59 +0900

    feat: 양의 정수인지 검증하는 기능 구현

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,4 +1,10 @@
 package lotto.util;
 
 public class Validator {
+
+    public static void validatePositiveInteger(Integer number) {
+        if (number < 1) {
+            throw new IllegalStateException();
+        }
+    }
 }

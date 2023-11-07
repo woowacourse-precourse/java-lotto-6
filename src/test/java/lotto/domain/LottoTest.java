c5247fae24bd336a7aa3f7673fc488f5055fed13
zeomzzz <jmlee2705@gmail.com>
2023-11-08 02:48:32 +0900

    test: Lotto toString 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    private Lotto lotto;
+
+    @BeforeEach
+    void setTest() {
+        lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+    }
+
+    @DisplayName("toString 메소드 : 출력 결과 테스트")
+    @Test
+    void toString_validateFormat() {
+        assertThat(lotto.toString()).isIn("[6, 5, 4, 3, 2, 1]", "[1, 2, 3, 4, 5, 6]");
+    }
+
+}

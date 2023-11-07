e31127d429316cc752851ec3c61d4a97adaf47ca
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-07 18:12:56 +0900

    refactor(Lotto): 숫자생성기 테스트 코드에서 당첨 숫자로 로또가 생성되는 원인을 발견하여, 요구사항 대로 숫자생성기를 바꾸었습니다.

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -11,22 +11,9 @@ public class NumberGenerator {
     public static final int MIN_NUMBER = 1;
     public static final int NUMBERS_COUNT = 6;
 
-    public List<Integer> createRandomNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-
-        addUniqueRandomNumbers(numbers, NUMBERS_COUNT);
+    public static List<Integer> createRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBERS_COUNT));
         Collections.sort(numbers);
-
         return numbers;
-    }
-
-    private void addUniqueRandomNumbers(List<Integer> numbers, int count) {
-        while (numbers.size() < count) {
-            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-
-            if (!numbers.contains(randomNumber)) {
-                numbers.add(randomNumber);
-            }
-        }
     }
 }

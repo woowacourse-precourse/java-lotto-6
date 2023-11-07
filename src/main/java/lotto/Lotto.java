8ec1317a19b23d85b4b30aa2e9d35225f9a7f17f
의찬 <srja432130@gmail.com>
2023-11-07 18:49:11 +0900

    feat: 로또 번호의 개수가 6개가 아니면 에러메시지 출력

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,19 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수가 6개가 아닙니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
-}

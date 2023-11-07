6b395a4b6d0d8fca9130b9b41bb65a17151d60a9
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-07 15:32:26 +0900

    refactor: InputView 클래스 리팩토링 메소드 분리 및 유틸리티 메소드를 Util 클래스로 이동하였음

---
+++ src/main/java/lotto/utils/Util.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Util {
+    public static final List<Integer> convertStringToIntegerList(String input) {
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public static boolean isInteger(String str) {
+        return str.matches("\\d+");
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,71 +1,50 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lotto.utils.ErrorMessage;
 import lotto.utils.GameMessage;
+import lotto.utils.Util;
 
 public class InputView {
     public static int getMoneyInput() {
         System.out.println(GameMessage.ENTER_PURCHASE_AMOUNT_MESSAGE.getMessage());
-        String inputMoney = Console.readLine();
-        validateIntegerForInputMoney(inputMoney);
-        return Integer.parseInt(inputMoney);
-    }
-
-    private static void validateIntegerForInputMoney(String input) {
-        if (!input.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_MONEY_NOT_INTEGER_ERROR.getMessage());
-        }
+        return getIntegerInput();
     }
 
     public static List<Integer> getWinningNumbersInput() {
         System.out.println(GameMessage.ENTER_WINNING_NUMBERS_MESSAGE.getMessage());
-        String winningNumbersInput = Console.readLine();
-        validateIntegerForWinningNumbersInput(winningNumbersInput);
-        List<Integer> winningNumbersIntegerList = convertStringToIntegerList(winningNumbersInput);
-        return winningNumbersIntegerList;
-    }
-
-    private static void validateIntegerForWinningNumbersInput(String input) {
-        String[] parts = input.split(",");
-        for (String part : parts) {
-            if (!isInteger(part.trim())) {
-                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_NOT_INTEGER_ERROR.getMessage());
-            }
-        }
-    }
-
-    private static List<Integer> convertStringToIntegerList(String input) {
-        String[] parts = input.split(",");
-        List<Integer> result = new ArrayList<>();
-
-        for (String part : parts) {
-            result.add(Integer.parseInt(part));
-        }
-        return result;
-    }
-
-    private static boolean isInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        return getIntegerListInput();
     }
 
     public static int getBonusNumberInput() {
         System.out.println(GameMessage.ENTER_BONUS_NUMBER_MESSAGE.getMessage());
-        String inputBonusNumber = Console.readLine();
-        validateIntegerForInputBonusNumber(inputBonusNumber);
-        return Integer.parseInt(inputBonusNumber);
+        return getIntegerInput();
     }
 
-    private static void validateIntegerForInputBonusNumber(String input) {
-        if (!isInteger(input)) {
+    private static int getIntegerInput() {
+        String input = Console.readLine();
+        validateIntegerInput(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void validateIntegerInput(String input) {
+        if (!Util.isInteger(input)) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_NOT_INTEGER_ERROR.getMessage());
+        }
+    }
+
+    private static List<Integer> getIntegerListInput() {
+        String winningNumbersInput = Console.readLine();
+        validateIntegerListInput(winningNumbersInput);
+        return Util.convertStringToIntegerList(winningNumbersInput);
+    }
+
+    private static void validateIntegerListInput(String input) {
+        List<String> parts = Arrays.asList(input.split(","));
+        if (!parts.stream().allMatch(Util::isInteger)) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_NOT_INTEGER_ERROR.getMessage());
         }
     }
 }

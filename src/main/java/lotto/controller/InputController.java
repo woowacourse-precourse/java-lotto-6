7933c377688e10ee27cbfc7c88e73a6163dc40fa
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 03:43:42 +0900

    feat(InputController): 당첨 번호에 대한 예외처리 및 재입력을 받는 함수 추가

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.domain.TicketAmount;
+import lotto.domain.WinningNumber;
 import lotto.util.Validator;
 import lotto.view.InputView;
 
@@ -15,8 +16,23 @@ public class InputController {
         }
     }
 
+    public WinningNumber setWinningNumber() {
+        try {
+            String input = InputView.inputLottoNumbers();
+            validateWinningNumberInput(input);
+            return new WinningNumber(input);
+        } catch (IllegalArgumentException e) {
+            return setWinningNumber();
+        }
+    }
+
     private void validateInput(String input) {
         Validator.validateInputNotEmpty(input);
         Validator.validateIsParsableToInt(input);
+    }
+
+    private void validateWinningNumberInput(String input) {
+        Validator.validateInputNotEmpty(input);
+        Validator.validateInputWithSeparator(input);
     }
 }

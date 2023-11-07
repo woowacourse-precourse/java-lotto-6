704b3c6df212b6f42a389a962faab0ef2e0b5430
limzzum <chfhddl1234@naver.com>
2023-11-08 01:57:31 +0900

    :sparkles: feat : 로또 당첨 순위 반환 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,9 @@ import static lotto.validation.LottoValidation.validate_lottoNum;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+import lotto.constant.WinningRanking;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -24,8 +27,35 @@ public class Lotto {
         validate_lottoNum(numbers);
     }
 
-
     private void ascendingNumbers(){
         Collections.sort(numbers);
+    }
+
+    public int getRank(List<Integer> nums, int bonusNum){
+
+        int matchCnt = getMatchCnt(nums);
+
+        if(WinningRanking.RANK2.getMatchNumCnt() == matchCnt){
+            return checkBonus(bonusNum);
+        }
+
+        for (WinningRanking rank : WinningRanking.values()){
+            if(rank.getMatchNumCnt() == matchCnt){
+                return rank.getRanking();
+            }
+        }
+        return -1;
+    }
+
+    private int getMatchCnt(List<Integer> nums) {
+        List<Integer> result = Stream.concat(numbers.stream(), nums.stream()).toList();
+        return 12 - Set.copyOf(result).size();
+    }
+
+    private int checkBonus(int bonusNum) {
+        if(numbers.contains(bonusNum)){
+            return 2;
+        }
+        return 3;
     }
 }

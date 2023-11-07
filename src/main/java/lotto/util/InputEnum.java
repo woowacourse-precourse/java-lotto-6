1c21472ada3eeb13fd5b0e4e932cccdfad0d9d97
linirini <2001yerin@naver.com>
2023-11-07 20:21:12 +0900

    feat : enumerate purhcase amount input message

--- src/main/java/lotto/util/InputEnum.java
+++ src/main/java/lotto/util/InputEnum.java
@@ -1,4 +1,18 @@
 package lotto.util;
 
 public enum InputEnum{
+
+    PURCHASE_AMOUNT_INPUT("구입 금액을 입력해주세요.");
+
+    private String message;
+
+    InputEnum(String message){
+        this.message = message;
+    }
+
+    public String message(){
+        return this.message;
+    }
+
+
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.util.ExceptionEnum.NOT_NUMBER_FORMAT;
+import static lotto.util.InputEnum.PURCHASE_AMOUNT_INPUT;
 
 import lotto.domain.Amount;
 import lotto.domain.lotteryresult.Bonus;
@@ -9,13 +10,13 @@ import lotto.domain.lotto.Lotto;
 
 public class InputView{
 
-    public static Amount inputPurchasePrice(){
-        System.out.println("구입 금액을 입력해주세요.");
+    public static Amount inputPurchaseAmount(){
+        System.out.println(PURCHASE_AMOUNT_INPUT.message());
         int amount = 0;
         try{
             amount = Integer.parseInt(readLine());
         } catch (NumberFormatException NFE){
-            throw new IllegalArgumentException(NOT_NUMBER_FORMAT.getMessage());
+            throw new IllegalArgumentException(NOT_NUMBER_FORMAT.message());
         }
         return new Amount(amount);
     }

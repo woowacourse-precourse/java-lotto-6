608a0bafef9d50d1588a6b7723b5544b5d432e2c
Hyso <hsk7953@gmail.com>
2023-11-08 05:29:16 +0900

    feat: 입력받은 값에 빈칸과 빈값이 있는 지 확인하고 에러를 던지는 메소드 추가

--- src/main/java/lotto/util/PriceException.java
+++ src/main/java/lotto/util/PriceException.java
@@ -2,14 +2,17 @@ package lotto.util;
 
 public class PriceException {
     public static void validPrice(int money) {
-
+        checkPositiveNumber(money);
+        checkMultipleOfThousand(money);
     }
 
-    private static void checkChar(String input) {
+    public static void checkValidNumber (String input) {
         try{
+            checkEmpty(input);
+            invalidSpace(input);
             Integer.parseInt(input);
         } catch (NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] : 옳바른 숫자를 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] : 옳바른 숫자를 입력해주세요.");
         }
     }
 
@@ -24,4 +27,17 @@ public class PriceException {
             throw new IllegalArgumentException("[ERROR] : 구입 가격은 양수여야 합니다.");
         }
     }
+
+    private static void checkEmpty(String input) {
+        if (input.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] : 빈값은 입력할 수 없습니다.");
+        }
+    }
+
+    private static void invalidSpace(String input) {
+        if (input.contains(" ")){
+            throw new IllegalArgumentException("[ERROR] : 입력값에 빈칸은 있으면 안됩니다.");
+        }
+    }
+
 }

48212091998c1599311cecd29905af6d0a3e613e
gomdol <oioifor@naver.com>
2023-11-08 01:00:51 +0900

    feat(InputView) : 사용자 입력 문구 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class InputView {
+    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}

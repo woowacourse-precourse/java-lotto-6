ef7243acb2043b2190bdd1a30c42a5835f010c85
HSBODo <vinsulill@gmaile.com>
2023-11-08 00:19:34 +0900

    refactor : 예외메세지 추가
    추가 예외 메세지
    - LOTTO_NUMBER_DUPLICATION("[ERROR] 로또 번호에 중복이 있습니다.")
    - LOTTO_NUMBER_NOT_SIX_DIGITS("[ERROR] 로또 번호가 6자리가 아닙니다.")

--- src/main/java/lotto/LottoException.java
+++ src/main/java/lotto/LottoException.java
@@ -5,7 +5,9 @@ public enum LottoException {
     INPUT_NOT_THOUSAND_UNITS("[ERROR] 구입금액을 1,000원 단위로 입력하세요."),
     INPUT_NOT_SPLIT("[ERROR] 쉼표(,)를 기준으로 6자리를 입력해주세요."),
     INPUT_NOT_RANGE("[ERROR] 1~45 사이의 숫자를 입력해 주세요."),
-    INPUT_DUPLICATION_NUMBER("[ERROR] 숫자가 중복됩니다.")
+    INPUT_DUPLICATION_NUMBER("[ERROR] 숫자가 중복됩니다."),
+    LOTTO_NUMBER_DUPLICATION("[ERROR] 로또 번호에 중복이 있습니다."),
+    LOTTO_NUMBER_NOT_SIX_DIGITS("[ERROR] 로또 번호가 6자리가 아닙니다.")
     ;
 
 

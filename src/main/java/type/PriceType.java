208dae50099d6a26562038aabf694042f9c02d80
win9 <krrr883@gmail.com>
2023-11-08 05:01:29 +0900

    [Fix]: 당첨 정보를 담는 EnumType 통합
    당첨 정보를 담는 EnumType 통합 및 삭제

--- src/main/java/type/PriceType.java
+++ src/main/java/type/PriceType.java
@@ -1,19 +1,26 @@
 package type;
 
 public enum PriceType {
-    FIRST("2,000,000,000원"),
-    SECOND("30,000,000원"),
-    THIRD("1,500,000원"),
-    FOURTH("50,000원"),
-    FIFTH("5,000원");
+    FIRST("2,000,000,000원", 6),
+    SECOND("30,000,000원", 5),
+    THIRD("1,500,000원", 5),
+    FOURTH("50,000원", 4),
+    FIFTH("5,000원", 3),
+    BONUS("보너스 볼 일치", 0);
 
     private final String price;
+    private final int count;
 
-    PriceType (String result){
+    PriceType (String result, int count){
         this.price = result;
+        this.count = count;
     }
 
     public String getPrice() {
         return price;
+    }
+
+    public int getCount() {
+        return count;
     }
 }

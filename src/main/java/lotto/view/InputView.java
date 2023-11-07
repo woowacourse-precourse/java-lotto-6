f239521e2972f4ffeb5ba261531d0225e07f45ab
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-07 14:46:46 +0900

    Fix : InputView class 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,20 @@
 package lotto.view;
 
-import static lotto.util.InputUtil.toIntStringNumberParser;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.InvalidPurchaseCostException;
-
+import lotto.util.LottoInputUtil;
 
 
 public class InputView {
     private final InvalidPurchaseCostException invalidPurchaseCostException = new InvalidPurchaseCostException();
-
+    private final LottoInputUtil lottoInputUtil = new LottoInputUtil();
 
     public int inputPriceView() {
         try {
             String price = Console.readLine();
             invalidPurchaseCostException.validatePurchaseCostException(price);
-            return toIntStringNumberParser(price);
+            return lottoInputUtil.toIntStringNumberParser(price);
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException("[ERROR] 입력은 숫자만 가능합니다.");
         }

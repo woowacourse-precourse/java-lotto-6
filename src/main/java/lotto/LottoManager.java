1967e48b2a762a8b3608331311ae92c745e28f9e
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:57:57 +0900

    feat(LottoManager) : 컨트롤러 기능 추가
    Controller 기능을 가지고 있으며 여기서 게임이 이루어진다.

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,8 +1,32 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.ResultLotto;
+import lotto.domain.UserLotto;
+import lotto.domain.WinningLotto;
+import lotto.view.OutputView;
+
 public class LottoManager {
 
+    private UserLotto userLotto;
+    private WinningLotto winningLotto;
+    private BonusNumber bonusNumber;
+
+    private List<ResultLotto> result;
+    private float yield;
+
     public void run() {
-        System.out.println("구입금액을 입력해 주세요.");
+        userLotto = new UserLotto();
+        winningLotto = new WinningLotto();
+        bonusNumber = new BonusNumber();
+
+        ResultManager resultManager = new ResultManager();
+        YieldManager yieldManager = new YieldManager();
+        result = resultManager.match(winningLotto, userLotto, bonusNumber);
+        yield = yieldManager.makeYield(result);
+
+        OutputView.printReward(result);
+        OutputView.printYield(yield);
     }
 }

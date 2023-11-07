0400840227457a20f60eacbad7e3abbab5b75bf8
Kiltae <loverpleasestayo0@gmail.com>
2023-11-07 16:00:36 +0900

    feat: add lotto number size validation

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,27 @@
-package lotto;
+package lotto.model;
 
-import java.util.List;
+import lotto.view.ExceptionMessage;
+
+import java.util.*;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        validateSixDigits(numbers);
+        validateDuplicatedNumber(numbers);
+        validateRange(numbers);
+        List<Integer> tempNumbers = new ArrayList<>(numbers);
+        Collections.sort(tempNumbers);
+        this.numbers = tempNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSixDigits(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
 }

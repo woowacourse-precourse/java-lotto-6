288866a801d22eae587fa49405660524a84abe4c
star-candy <el0710@naver.com>
2023-11-07 21:27:46 +0900

    refactor(lotto): 매직 넘버 상수화

--- src/main/java/controller/LottoManager.java
+++ src/main/java/controller/LottoManager.java
@@ -1,9 +1,13 @@
 package controller;
 
+import static constants.ConstantNumbersManager.INDEX_RESET;
+
 import java.util.ArrayList;
 import java.util.List;
-import view.*;
-import model.*;
+import model.Transform;
+import model.Validation;
+import view.InputView;
+import view.OutputView;
 
 public class LottoManager {
     private final List<List<Integer>> fullLottoValue = new ArrayList<>();
@@ -36,7 +40,7 @@ public class LottoManager {
     }
 
     private void generateAndPrintRandomLottoNumbers(int lottoCount) {
-        for (int index = 0; index < lottoCount; index++) {
+        for (int index = INDEX_RESET; index < lottoCount; index++) {
             List<Integer> randomLotto = transform.generateRandomList();
             fullLottoValue.add(randomLotto);
             outputView.printRandomNumber(randomLotto);

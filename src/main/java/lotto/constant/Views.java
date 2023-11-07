aa71563733c51f1c1b48396da74da2c1c9154dfb
Seyeon <wjdtpdus828@naver.com>
2023-11-07 21:51:44 +0900

    feat(Views): 수익률을 출력하는 String 메세지 추가 및 파라미터로 값을 변경하는 메소드 구현

--- src/main/java/lotto/constant/Views.java
+++ src/main/java/lotto/constant/Views.java
@@ -1,10 +1,11 @@
 package lotto.constant;
 
 public enum Views {
-    INPUT_PURCHASE_AMOUT_MESSAGE("구입금액을 입력해 주세요 ."),
-    OUTPUT_PURCHASE_AMOUT_MESSAGE("개를 구매했습니다 ."),
+    INPUT_PURCHASE_AMOUT_MESSAGE("구입금액을 입력해 주세요."),
+    OUTPUT_PURCHASE_AMOUT_MESSAGE("개를 구매했습니다."),
     INPUT_WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+    OUTPUT_RATE_RESULT_MESSAGE("총 수익률은 %s%% 입니다.");
 
 
     private final String message;
@@ -15,5 +16,8 @@ public enum Views {
 
     public String getMessage(){
         return message;
+    }
+    public String formatMessage(String parameter){
+        return String.format(message,parameter);
     }
 }

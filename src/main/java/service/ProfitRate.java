958b691c8648230f439efaeb2ea319c5a45519ae
Son JunHyung <kingjh1125@gmail.com>
2023-11-08 01:58:50 +0900

    refactor: 생성자 생성 막기
    객체 생성이 목적이 아닌 파일의 생성자에 대해 private을 적용하였습니다:
    - InputView
    - ProfitRate
    - RandomNumber
    - ResultView

--- src/main/java/service/ProfitRate.java
+++ src/main/java/service/ProfitRate.java
@@ -4,6 +4,7 @@ import domain.Amount;
 import domain.Result;
 
 public class ProfitRate {
+    private ProfitRate(){}
     public static double profitRate(Amount amount, Result result) {
         int reward = result.getReward();
         return amount.calculateProfitRate(reward);
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
+    private InputView(){}
 
     private static String getInput() {
         return Console.readLine();

ccb9ff05eb21291ee21c5b08a3c24ebbd140ddac
choiejae <ejae8319@gmail.com>
2023-11-07 22:53:49 +0900

    test(LottoTicketsGenerator): LottoTickets 생성 클래스 구현
    - 싱글톤 패턴을 통해 인스턴스를 하나가 생성되도록 한다
    - generateLottoTickets(): 개수만큼 로또를 생성할 수 있다

---
+++ src/main/java/lotto/LottoTicketsGenerator.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.domain.lottoGenerateMethod.LottoGenerateMethod;
+
+public class LottoTicketsGenerator {
+
+    public static LottoTicketsGenerator getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        public static final LottoTicketsGenerator INSTANCE = new LottoTicketsGenerator();
+    }
+
+    public LottoTickets generateLottoTickets(final int count, final LottoGenerateMethod lottoGenerateMethod) {
+        List<Lotto> lottoTickets = IntStream.range(0, count)
+                .mapToObj(i -> lottoGenerateMethod.generateLotto())
+                .collect(Collectors.toList());
+
+        return LottoTickets.of(lottoTickets);
+    }
+}

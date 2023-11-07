d1b4c21ec0aea95845439ddcdf18687efdb23b15
nayonsoso <panda0329@naver.com>
2023-11-08 05:40:47 +0900

    feat: 구매 관련 메세지 상수화

---
+++ src/main/java/constants/PurchasedMessage.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class PurchasedMessage {
+    private static final String PURCHASE_QUANTITY = "%d개를 구매했습니다.";
+    private static final String PURCHASED_LOTTO_START = "[";
+    private static final String PURCHASED_LOTTO_DELIMITER = ",";
+    private static final String PURCHASED_LOTTO_END = "]";
+}

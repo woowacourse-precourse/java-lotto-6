8f8363dbc053c9314749da5f52c80cf635d18df5
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-11-07 19:13:11 +0900

    test : check whether the input of winning numbers is valid

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -22,4 +22,34 @@ public class InputValidatorTest {
         Assertions.assertThatThrownBy(() -> InputValidator.validatePurchaseAmountInput(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호가 6개가 아니라면 에러가 발생한다.")
+    @Test
+    void validateNumberOfWinningNumbers() {
+        String input = "1,2,3,4,5";
+        Assertions.assertThatThrownBy(() -> InputValidator.validateWinningNumbersInput(input));
+    }
+
+    @DisplayName("당첨 번호에 숫자가 아닌 값이 주어진다면 에러가 발생한다.")
+    @Test
+    void validateAllNumericNumbersInWinningNumbers() {
+        String input = "1,2,3,4,5,k";
+        Assertions.assertThatThrownBy(() -> InputValidator.validateWinningNumbersInput(input));
+    }
+
+    @DisplayName("당첨 번호중 번호가 중복되면 에러가 발생한다.")
+    @Test
+    void validateDuplicateWinningNumbers() {
+        String input = "1,2,3,4,5,5";
+        Assertions.assertThatThrownBy(() -> InputValidator.validateWinningNumbersInput(input));
+    }
+
+    @DisplayName("당첨 번호가 6개 중 범위를 벗어난 숫자가 있다면 에러가 발생한다.")
+    @Test
+    void validateWinningNumbersInRange() {
+        String input = "0,2,3,4,5,6";
+        Assertions.assertThatThrownBy(() -> InputValidator.validateWinningNumbersInput(input));
+        String secondInput = "1,2,3,4,5,46";
+        Assertions.assertThatThrownBy(() -> InputValidator.validateWinningNumbersInput(secondInput));
+    }
 }

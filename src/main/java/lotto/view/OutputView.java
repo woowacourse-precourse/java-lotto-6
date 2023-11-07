2c4c1f1761e33b34cbaab1f79c4f4f265f5346db
Gyeongho Yang <chchch1213@naver.com>
2023-11-07 21:37:53 +0900

    feat(view): 당첨 통계 출력 함수 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ import lotto.constant.ErrorMessage;
 import lotto.constant.GeneralMessage;
 
 import static lotto.constant.GeneralMessage.NOTIFY_BUY_COUNT;
+import static lotto.constant.GeneralMessage.NOTIFY_WINNING_STATISTICS;
 
 public class OutputView {
     public static void printGeneralMessage(GeneralMessage generalMessage, Object... args) {
@@ -11,15 +12,21 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printBuyLottos(int count, String result) {
-        System.out.printf(NOTIFY_BUY_COUNT.getMessage(), count);
-        System.out.println();
-        System.out.println(result);
-        System.out.println();
-    }
-
     public static void printErrorMessage(String message) {
         System.out.print(ErrorMessage.PREFIX);
         System.out.println(message);
+    }
+
+    public static void printBuyLottos(int count, String tickets) {
+        System.out.printf(NOTIFY_BUY_COUNT.getMessage(), count);
+        System.out.println();
+        System.out.println(tickets);
+        System.out.println();
+    }
+
+    public static void printWinningStatistics(String result) {
+        System.out.println(NOTIFY_WINNING_STATISTICS.getMessage());
+        System.out.println();
+        System.out.println(result);
     }
 }

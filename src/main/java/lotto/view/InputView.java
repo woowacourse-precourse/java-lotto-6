51303ed409e4f65df4bb7e6cbcaf27c89f4cc3a0
ppdoli123 <ppdoli123@naver.com>
2023-11-07 15:28:51 +0900

    feat(InputView): view에 해당되는 InputView 초기 코드 작성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import lotto.Lotto;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    // 구입 금액 입력 받는 로직 작성 필요
+    public int readPurchaseAmount() {
+
+    }
+
+    // 당첨 번호 입력 받는 로직 작성 필요
+    public List<Lotto> readWinningNumbers() {
+
+    }
+
+    // 보너스 번호 입력 받는 로직 작성 필요
+    public Lotto readBonusNumber() {
+
+    }
+}

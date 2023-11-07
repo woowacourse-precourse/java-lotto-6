fce1463b88eebfdd275cfb985c6871db48ddcfc4
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-07 18:20:52 +0900

    chore : 불 필요한 주석 지우기

--- src/main/java/lotto/view/LottoPurchaseAmountInputView.java
+++ src/main/java/lotto/view/LottoPurchaseAmountInputView.java
@@ -1,9 +1,5 @@
 package lotto.view;
 
-import camp.nextstep.edu.missionutils.Console;
-import lotto.exception.LottoPurchaseAmountException;
-import lotto.util.StringToIntegerConverter;
-
 public class LottoPurchaseAmountInputView extends InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
 

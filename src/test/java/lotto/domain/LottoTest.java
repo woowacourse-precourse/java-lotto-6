74c11cba594860ad228214df5e6aa1580702885c
HyunmoBae <tsi0520@naver.com>
2023-11-08 07:40:58 +0900

    test(lotto) : add Lotto NotInRange test

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -24,6 +24,18 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호중 1~45 사이의 값이 아닌게 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByNotInRange() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 46, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, -1, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     // 아래에 추가 테스트 작성 가능
     @DisplayName("보너스 로또 번호가 1~45 사이의 값이 아니면 예외가 발생한다.")
     @Test

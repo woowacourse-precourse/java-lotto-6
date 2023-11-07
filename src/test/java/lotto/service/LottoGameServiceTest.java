f27cddcb1d658cefbeaa8d98ada164b6be77a886
Sung Hyobin <hellmir610@gmail.com>
2023-11-07 23:59:24 +0900

    test: Add test of function that compute count of lotto generated by purchase amount

---
+++ src/test/java/lotto/service/LottoGameServiceTest.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import lotto.domain.LottoCount;
+import lotto.utility.TypeConverter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static lotto.configuration.InputFormatConfig.LOTTO_PRICE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGameServiceTest {
+    private LottoGameService lottoGameService = new LottoGameServiceImpl();
+
+    @DisplayName("구입금액에서 로또 가격을 나눈 만큼의 Lotto를 발행한다.")
+    @ParameterizedTest
+    @CsvSource({"1000", "3000", "10000", "600000", "7000000"})
+    void computeLottoCount(String purchaseAmount) {
+        // given
+        int money = TypeConverter.convertStringToInt(purchaseAmount);
+
+        // when
+        LottoCount lottoCount = lottoGameService.computeLottoCount(purchaseAmount);
+
+        //then
+        assertThat(lottoCount.getLottoCount()).isEqualTo(money / LOTTO_PRICE);
+    }
+}

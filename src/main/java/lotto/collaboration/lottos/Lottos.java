fa737d8542cd6b72d8d133cc94d4498f84a70762
aka-nick <bechalrang@gmail.com>
2023-11-07 17:57:35 +0900

    추가(LottosMessage) : Lottos 전용 메시지 클래스
    Lottos에서 사용되는 리터럴 상수를 추출하여 묶은 enum 클래스 추가함
    
    LottosMessage
    Lottos

--- src/main/java/lotto/collaboration/lottos/Lottos.java
+++ src/main/java/lotto/collaboration/lottos/Lottos.java
@@ -1,5 +1,8 @@
 package lotto.collaboration.lottos;
 
+import static lotto.collaboration.lottos.enums.LottosMessage.makeMessageForIndivisible;
+import static lotto.collaboration.lottos.enums.LottosMessage.makeMessageForOutOfRange;
+
 import java.util.List;
 import java.util.stream.IntStream;
 import lotto.collaboration.lottos.dto.PlayerLotto;
@@ -23,19 +26,14 @@ public class Lottos {
 
     private void occurExceptionIfIndivisibleOf(int purchaseAmount) {
         if (purchaseAmount % MIN_PURCHASE_AMOUNT != 0) {
-            throw new IllegalArgumentException(MIN_PURCHASE_AMOUNT + "원 단위로 입력해 주세요");
+            throw new IllegalArgumentException(makeMessageForIndivisible(MIN_PURCHASE_AMOUNT));
         }
     }
 
     private void occurExceptionIfOutsideTheRangeOf(int purchaseAmount) {
-        if (purchaseAmount < MIN_PURCHASE_AMOUNT ||
-                MAX_PURCHASE_AMOUNT < purchaseAmount) {
-            throw new IllegalArgumentException(
-                    "로또 구매 금액은 " +
-                            MIN_PURCHASE_AMOUNT +
-                            "원에서 " +
-                            MAX_PURCHASE_AMOUNT +
-                            "원까지만 가능합니다.");
+        if (purchaseAmount < MIN_PURCHASE_AMOUNT
+                || MAX_PURCHASE_AMOUNT < purchaseAmount) {
+            throw new IllegalArgumentException(makeMessageForOutOfRange(MIN_PURCHASE_AMOUNT, MAX_PURCHASE_AMOUNT));
         }
     }
 

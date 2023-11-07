62b3fe684d4087247b66f8b1da0d831efec22843
haero77 <shineecard5@gmail.com>
2023-11-08 02:28:56 +0900

    feat: 애플리케이션 실행 및 의존성 주입 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,33 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.domain.randomnumber.RandomNumberPicker;
+import lotto.domain.randomnumber.SystemRandomNumberPicker;
+import lotto.view.InputView;
+import lotto.view.printer.ConsolePrinter;
+import lotto.view.printer.Printer;
+import lotto.view.reader.ConsoleReader;
+import lotto.view.reader.Reader;
+import lotto.view.result.ResultView;
+import lotto.view.result.ResultViewFormatter;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        setUpLottoGameController().run();
+    }
+
+    private static LottoGameController setUpLottoGameController() {
+        Reader reader = new ConsoleReader();
+        Printer printer = new ConsolePrinter();
+        InputView inputView = InputView.of(reader, printer);
+
+        ResultViewFormatter formatter = new ResultViewFormatter();
+        ResultView resultView = new ResultView(printer, formatter);
+
+        RandomNumberPicker numberPicker = new SystemRandomNumberPicker();
+
+        return new LottoGameController(inputView, resultView, numberPicker);
     }
 
 }

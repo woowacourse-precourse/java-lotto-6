26506767914d469888dbb1052f5d227ebb4822bb
aammddkkzxc <ideal0427@naver.com>
2023-11-07 17:36:02 +0900

    feat: 당첨번호 보너스 번호 중복 유효성 검사 기능 구현

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,12 +1,20 @@
 package lotto;
 
 public class WinningLotto {
+    private static final String NON_OVERLAPPING_NUMBER_REQUEST_MESSAGE = "보너스 넘버는 당첨 넘버와 겹치치 않아야 합니다";
     private final Lotto lotto;
     private final int bonusNumber;
 
     public WinningLotto(Lotto lotto, int bonusNumber) {
+        validateBonusNumberNotOverlapWinningNumber(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumberNotOverlapWinningNumber(Lotto lotto, int bonusNumber) {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(NON_OVERLAPPING_NUMBER_REQUEST_MESSAGE);
+        }
     }
 
     public Lotto getLotto() {

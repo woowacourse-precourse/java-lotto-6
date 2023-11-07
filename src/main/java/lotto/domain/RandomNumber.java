c0e5b5caced1e81821c406376861fe46c5bb5d52
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-07 14:50:55 +0900

    feat(RandomNumber) : 기능 구현

--- src/main/java/lotto/domain/RandomNumber.java
+++ src/main/java/lotto/domain/RandomNumber.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.system.Constants;
+
 public class RandomNumber {
+    private RandomNumber() {}
+
+    public static List<Integer> makeUniqueRandomList() {
+        return Randoms.pickUniqueNumbersInRange(
+                Constants.START_NUMBER.getConstants(),
+                Constants.END_NUMBER.getConstants(),
+                Constants.LOTTO_SIZE.getConstants()
+        );
+    }
 }

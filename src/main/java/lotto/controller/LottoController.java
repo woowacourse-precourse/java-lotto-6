c65b9d7b962e249ea3e0de54cb60d87fe01d1c23
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2023-11-07 20:26:38 +0900

    refactor: lottoTickets 변수 삭제
    변수 전달 때 바로 보내게 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,15 +12,13 @@ public class LottoController {
     private InputController inputController = new InputController();
     private MakeLotto makeLotto = new MakeLotto();
     private final int money;
-    private final int lottoTickets;
     private final int bonusNum;
 
     public LottoController(){
         money = getMoney();
-        lottoTickets = getLottoTickets(money);
-        List<Lotto> lottoList = makeLotto.makeLottoList(lottoTickets);
+        List<Lotto> lottoList = makeLotto.makeLottoList(getLottoTickets(money));
         List<Integer> winningNum = getWinningNum();
-        bonusNum = getBounsNum();
+        bonusNum = getBonusNum();
         LottoResult lottoResult = new LottoResult(lottoList, winningNum, bonusNum);
         printResult(lottoResult);
     }
@@ -40,7 +38,7 @@ public class LottoController {
         return inputController.inputWinningNum();
     }
 
-    public int getBounsNum(){
+    public int getBonusNum(){
         OutputView.printBlank();
         OutputView.inputBonusNumMessage();
         return inputController.inputBonusNum();

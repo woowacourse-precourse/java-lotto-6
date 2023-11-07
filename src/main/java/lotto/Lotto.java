9f79ba4a25654b2b04317159bbdf0368d12f3784
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-07 17:04:56 +0900

    feat: 발행된 로또와 입력 당첨번호를 비교하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -9,12 +10,26 @@ public class Lotto {
         validate(numbers);
         this.numbers = numbers;
     }
-
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public Integer countWinningMatches(List<Integer> winningNum){
+        List<Integer> matchedNumbers = numbers.stream()
+                .filter(lottoNumber -> winningNum.stream().anyMatch(Predicate.isEqual(lottoNumber)))
+                .toList();
+
+        return matchedNumbers.size();
+    }
+
+    public Integer countBonusWinningMatches(Integer bonusWinningNum){
+        boolean isContained = numbers.contains(bonusWinningNum)
+        return Boolean.compare(isContained, true) + 1;
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return numbers;
+    }
 }

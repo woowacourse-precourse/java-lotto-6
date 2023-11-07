00b9c27f869c0ef33ffc16bd5b4b7830fa2c6179
bumstead-bumstead <dldyghks951@gmail.com>
2023-11-08 00:24:35 +0900

    chore: domain, util 패키지 추가

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,10 +1,11 @@
-package lotto;
+package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static lotto.LottoNumberGenerator.generateRandomLottoNumber;
+import static lotto.util.LottoNumberGenerator.generateRandomLottoNumber;
 
+//너무 넓은 의미를 갖고 있는 단어인 것 같다. 더 로또 계산과 관련된 로직을 포함하는 클래스라는 의미를 나타내는 이름이 어떤지?... LottoMachine??..
 public class Game {
     private List<Lotto> lottos;
     private List<Integer> winningNumbers;
@@ -15,11 +16,11 @@ public class Game {
                  Integer numberOfLottos) {
         validate(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
         this.lottos = new ArrayList<>();
         while (numberOfLottos != lottos.size()) {
             lottos.add(new Lotto(generateRandomLottoNumber()));
         }
-        this.bonusNumber = bonusNumber;
     }
 
     private void validate(List<Integer> winningNumbers, Integer bonusNumber) {
@@ -33,5 +34,4 @@ public class Game {
                 .map(lotto -> lotto.calculateRank(winningNumbers, bonusNumber))
                 .toList();
     }
-
 }

85c8d69f615376ddbc181fce22b67a7a3a288dac
i3kae <snoger1234@gmail.com>
2023-11-07 16:22:13 +0900

    feat : 당첨 번호 입력 기능 구현 1. 당첨 번호에 해당하는 문자열 입력 2. 문자열 검사   2.1 문자열이 비어있는지   2.2 문자열의 시작과 끝에 쉼표가 있는지   2.3 쉼표를 기준으로 나눈 문자열들이 비어있는지   2.4 쉼표를 기준으로 나눈 문자열들이 숫자로만 이루어져 있는지   2.5 쉼표를 기준으로 나눈 문자열들이 1 ~ 45 사이인지 3. 입력 받은 문자열을 쉼표를 기준으로 분리 4. 분리된 문자열들을 리스트에 담아 반환

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -29,11 +29,9 @@ public class Input {
             if (inputType == InputType.BONUS_NUMBER){
                 conversionResult = bonusNumberInput(inputStr);
             }
-            /*
             if (inputType == InputType.WINNING_NUMBERS){
                 conversionResult = winningNumbersInput(inputStr);
             }
-            */
         }catch(IllegalArgumentException e){
             return null;
         }
@@ -71,18 +69,39 @@ public class Input {
             throw new IllegalArgumentException(ErrorMessage.NULL_STRING.getMessage());
         }
         if (Utils.isNumeric(inputStr) == false){
-            throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC_STR.getMessage())
+            throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC_STR.getMessage());
         }
         if (Integer.parseInt(inputStr) < 1 || 45 < Integer.parseInt(inputStr)){
             throw new IllegalArgumentException(ErrorMessage.NOT_IN_1_TO_45.getMessage());
         }
     }
-    /*
+
     public List<Integer> winningNumbersInput(String inputStr){
-
+        List<Integer> conversionResult = new ArrayList<>();
+        winningNumberValidate(inputStr, inputStr.split(","));
+        for (String splited : inputStr.split(",")){
+            conversionResult.add(Integer.parseInt(splited));
+        }
+        return conversionResult;
     }
-    public List<Integer> bonusNumberInput(String inputStr){
 
+    private void winningNumberValidate(String inputStr, String[] splitStr){
+        if (inputStr.isEmpty()){
+            throw new IllegalArgumentException(ErrorMessage.NULL_STRING.getMessage());
+        }
+        if (inputStr.charAt(0) == ',' || inputStr.charAt(inputStr.length() - 1) == ','){
+            throw new IllegalArgumentException(ErrorMessage.ILLIGAL_COMMA.getMessage());
+        }
+        for (String splited : splitStr){
+            if (splited.isEmpty()){
+                throw new IllegalArgumentException(ErrorMessage.ILLIGAL_COMMA.getMessage());
+            }
+            if (Utils.isNumeric(splited) == false){
+                throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC_STR.getMessage());
+            }
+            if (Integer.parseInt(splited) < 1 || 45 < Integer.parseInt(splited)){
+                throw new IllegalArgumentException(ErrorMessage.NOT_IN_1_TO_45.getMessage());
+            }
+        }
     }
-    */
 }

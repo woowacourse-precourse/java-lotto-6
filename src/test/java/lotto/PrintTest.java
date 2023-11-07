14817b642e4ec7b3ecf564f954cc050b6d6c4395
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-07 15:19:04 +0900

    test : 수익률 계산 테스트

--- src/test/java/lotto/PrintTest.java
+++ src/test/java/lotto/PrintTest.java
@@ -1,9 +1,12 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrintTest extends Print{
 
@@ -18,6 +21,29 @@ public class PrintTest extends Print{
 
         //then
         System.out.println();
-        Assertions.assertThat(result).containsExactly(8, 21, 23, 30, 42, 43);
+        assertThat(result).containsExactly(8, 21, 23, 30, 42, 43);
+    }
+
+    @Test
+    void 수익률_계산() {
+
+        //given
+        int money = 8000;
+        Map<Rank, Integer> drawResult = initDrawResult();
+        drawResult.put(Rank.FIFTH, 1);
+
+        //when
+        String result = calculateProfitRatio(money, drawResult);
+
+        //then
+        assertThat(result).isEqualTo("62.5");
+    }
+
+    private Map<Rank, Integer> initDrawResult() {
+        Map<Rank, Integer> drawResult = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            drawResult.put(rank, 0);
+        }
+        return drawResult;
     }
 }

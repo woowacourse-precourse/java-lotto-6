d508a89fdc762b8452a25e32398bd742afc1b6a7
SonJimin <lijnar100@naver.com>
2023-11-08 03:07:33 +0900

    Feat : Lotto.java 에 로또 번호 발급 검증
    Lotto.java 에 로또 번호 발급, 검증하도록 하고 LottoGame.java 에서 호출하도록 함.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,37 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    public static final int LOTTO_SIZE = 6;
+    public static final int MAX_NUMBER = 45;
+    public static final int MIN_NUMBER = 1;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+    }
+
+    public static Lotto createRandomLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE));
+    }
+
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또는 6개의 숫자로 이루어져야 합니다.");
+        }
+        for (int number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("로또 숫자는 1부터 45 사이어야 합니다.");
+            }
         }
     }
-
-    // TODO: 추가 기능 구현
 }
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class LottoGame {
 
     public void startGame() {
+        // 로또 생성
+        Lotto winningLotto = Lotto.createRandomLotto();
 
     }
 }

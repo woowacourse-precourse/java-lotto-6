7014e53a81b092db33fea10e557ee8f90a90cd29
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-07 21:01:02 +0900

    feat: 입력한 구입 금액이 숫자인지 검증한다.

--- src/main/java/lotto/model/LottoPurchase.java
+++ src/main/java/lotto/model/LottoPurchase.java
@@ -2,7 +2,11 @@ package lotto.model;
 
 import static lotto.util.Constant.LOTTO_PER_PRICE;
 
+import java.util.regex.Pattern;
+
 public class LottoPurchase {
+    private static final Pattern REGEX_NUMERIC = Pattern.compile("^[0-9]\\d*$");
+
     private int lottoPurchaseCount;
 
     private int getLottoPurchaseCount(int totalPrice) {
@@ -12,5 +16,11 @@ public class LottoPurchase {
     private void setLottoPurchaseCount(String inputNumber) {
         int totalPrice = Integer.parseInt(inputNumber);
         this.lottoPurchaseCount = getLottoPurchaseCount(totalPrice);
+    }
+
+    private void validateInputTypeNumeric(String inputNumber) {
+        if (!REGEX_NUMERIC.matcher(inputNumber).matches()) {
+            throw new IllegalArgumentException("ERROR_NUMERIC_TYPE");
+        }
     }
 }

20219f041f91760b5773adf243bb4c9c03677364
honee8583 <honee8583@naver.com>
2023-11-07 23:30:35 +0900

    test: add test case that raises an exception when purchase amount is not divide by 1000

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,17 @@ class LottoServiceTest {
     @BeforeEach
     public void setUp() {
         lottoService = new LottoService();
+    }
+
+    @DisplayName("숫자가 1000으로 나눠 떨어지지 않을 경우 예외 발생.")
+    @Test
+    void testIsPurchaseAmountDivideBy1000() {
+        String input = "10001";
+        assertThatThrownBy(() -> {
+            lottoService.isPurchaseAmountDivideBy1000(input);
+        })
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(ErrorMessage.PURCHASE_NOT_DIVIDE_BY_1000.getMessage());
     }
 
     @DisplayName("숫자가 45를 넘을 경우 예외 발생.")

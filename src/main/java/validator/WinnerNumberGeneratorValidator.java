9742637405646f3a8cad8df649d80146feb29894
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 07:51:22 +0900

    refactor: WinnerNumberGeneratorValidator 상수 설정

--- src/main/java/validator/WinnerNumberGeneratorValidator.java
+++ src/main/java/validator/WinnerNumberGeneratorValidator.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.Set;
 
 public class WinnerNumberGeneratorValidator {
+    public static final String INVALID_WINNER_NUMBER_MIN_RANGE = "[ERROR] 당첨 번호 생성 시 최소값(1) 보다 작은 값이 있습니다.";
+    public static final String INVALID_WINNER_NUMBER_MAX_RANGE = "[ERROR] 당첨 번호 생성 시 최대값(45) 보다 큰 값이 있습니다.";
+    public static final String INVALID_WINNER_NUMBER_SIZE = "[ERROR] 당첨 번호가 6개로 생성되지 않았습니다.";
+    public static final String DUPLICATE_WINNER_NUMBER = "[ERROR] 당첨 번호 중 중복된 값이 있습니다.";
+
     public void vaildWinnerNumbers(List<Integer> winnerNumbers) {
         vaildWinnerNumberRange(winnerNumbers);
         vaildWinnerNumberSize(winnerNumbers);
@@ -14,24 +19,24 @@ public class WinnerNumberGeneratorValidator {
     void vaildWinnerNumberRange(List<Integer> winnerNumbers) {
         for (int winnerNumber : winnerNumbers) {
             if (winnerNumber < 1) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호 생성 시 최소값(1) 보다 작은 값이 있습니다.");
+                throw new IllegalArgumentException(INVALID_WINNER_NUMBER_MIN_RANGE);
             }
             if (winnerNumber > 45) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호 생성 시 최대값(45) 보다 큰 값이 있습니다.");
+                throw new IllegalArgumentException(INVALID_WINNER_NUMBER_MAX_RANGE);
             }
         }
     }
 
     void vaildWinnerNumberSize(List<Integer> randomNumbers) {
         if (randomNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호가 6개로 생성되지 않았습니다.");
+            throw new IllegalArgumentException(INVALID_WINNER_NUMBER_SIZE);
         }
     }
 
     void vaildWinnerNumberDuplication(List<Integer> randomNumbers) {
         Set<Integer> uniqueNumber = new HashSet<>(randomNumbers);
         if (uniqueNumber.size() != randomNumbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호 중 중복된 값이 있습니다.");
+            throw new IllegalArgumentException(DUPLICATE_WINNER_NUMBER);
         }
     }
 }

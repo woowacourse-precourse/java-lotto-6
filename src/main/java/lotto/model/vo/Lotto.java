38dfd7cba63ea2014183be049842aa1feb01e47a
baksakcci <pshsh910@gmail.com>
2023-11-07 21:53:47 +0900

    refactor: `Lotto` 불변성을 위해 `equals()` 및 `hashCode()` 추가

--- src/main/java/lotto/model/vo/Lotto.java
+++ src/main/java/lotto/model/vo/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model.vo;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -24,5 +25,20 @@ public class Lotto {
         return numbers;
     }
 
-    // TODO: 추가 기능 구현
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
 }

2ff1ff9b5c5437f5a6d5ef1c417b82690fc53473
JINU-CHANG <jenny010925@naver.com>
2023-11-07 14:31:05 +0900

    refactor(LottoValidator): 로또번호 검증하는 객체 생성
    기존에는 Lotto 내부에서 처리했으나 검증하는 책임이 너무 많아지고,
    다른 클래스에서 사용가능성이 있어 독립적인 객체로 생성함.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,11 @@
 package lotto.domain;
 
 import static lotto.constant.GameRule.*;
-import static lotto.exception.ExceptionMessage.*;
 
 import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.exception.LottoException;
 
 public class Lotto {
     private final List<Integer> lotto;
@@ -32,31 +30,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new LottoException(LOTTO_INVALID_SIZE.getMessage());
-        }
-
-        if (isDuplicated(numbers)) {
-            throw new LottoException(LOTTO_DUPLICATED_ERROR.getMessage());
-        }
-
-        if (isNumbersNotInRange(numbers)) {
-            throw new LottoException(LOTTO_OUT_OF_RANGE_ERROR.getMessage());
-        }
-    }
-
-    private boolean isDuplicated(List<Integer> numbers) {
-        return numbers.stream()
-            .distinct()
-            .count() != numbers.size();
-    }
-
-    private boolean isNumbersNotInRange(List<Integer> numbers) {
-        return numbers.stream()
-            .anyMatch(this::isNumberNotInRange);
-    }
-
-    private boolean isNumberNotInRange(int number) {
-        return number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER;
+        LottoValidator.validateSize(numbers);
+        LottoValidator.validateDuplication(numbers);
+        LottoValidator.validateRange(numbers);
     }
 }
---
+++ src/main/java/lotto/domain/LottoValidator.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import static lotto.constant.GameRule.*;
+import static lotto.exception.ExceptionMessage.*;
+
+import java.util.List;
+
+import lotto.exception.LottoException;
+
+public class LottoValidator {
+	public static void validateSize(List<Integer> numbers) {
+		if (numbers.size() != LOTTO_SIZE) {
+			throw new LottoException(LOTTO_INVALID_SIZE.getMessage());
+		}
+	}
+
+	public static void validateDuplication(List<Integer> numbers) {
+		if (isDuplicated(numbers)) {
+			throw new LottoException(LOTTO_DUPLICATED_ERROR.getMessage());
+		}
+	}
+
+	public static void validateRange(List<Integer> numbers) {
+		if (isNumbersNotInRange(numbers)) {
+			throw new LottoException(LOTTO_OUT_OF_RANGE_ERROR.getMessage());
+		}
+	}
+
+	private static boolean isDuplicated(List<Integer> numbers) {
+		return numbers.stream()
+			.distinct()
+			.count() != numbers.size();
+	}
+
+	private static boolean isNumbersNotInRange(List<Integer> numbers) {
+		return numbers.stream()
+			.anyMatch(LottoValidator::isNumberNotInRange);
+	}
+
+	private static boolean isNumberNotInRange(int number) {
+		return number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER;
+	}
+}

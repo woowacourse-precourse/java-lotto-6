fef74c2d3f278f73995c8d921d4a9e90e1cbaee0
CJ <choijun0627@konkuk.ac.kr>
2023-11-07 21:17:48 +0900

    Feat : 로또 하나를 생성하는 기능 구현
    로또 하나를 생성하는 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,8 +1,13 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class LottoMachine {
 
     public Lotto makeLotto() {
-        return null;
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(numbers);
+        return lotto;
     }
 }

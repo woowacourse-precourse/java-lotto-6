b6c7abf660969a26199cd30bd058402aedef5d05
prefer52 <tjsgh151@naver.com>
2023-11-08 00:25:34 +0900

    feat(LottoGame): 당첨 번호 입력 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import java.util.Arrays;
+
+import static lotto.Validator.*;
+
 public class LottoGame {
     private final UserInterface userInterface = new UserInterface();
+    private WinLotto winLotto;
     private UserLottos userLottos;
 
     // 로또 게임 진행 시작
@@ -9,6 +14,7 @@ public class LottoGame {
         int amount = getLottoAmount();
         UserLottos userLottos = new UserLottos(amount);
         showLottos(userLottos);
+        winLotto = getWinLotto();
     }
 
     // 구입금액 입력
@@ -18,7 +24,7 @@ public class LottoGame {
             try {
                 userInterface.showText("구입금액을 입력해 주세요.");
                 amount = userInterface.getUserInput();
-                Validator.validateAmount(amount);
+                validateAmount(amount);
             } catch (IllegalArgumentException e) {
                 userInterface.showText(e.getMessage());
                 continue;
@@ -31,6 +37,23 @@ public class LottoGame {
     // 발행한 로또 수량 및 번호 출력
     private void showLottos(UserLottos userLottos) {
         userInterface.showText(userLottos.getSize() + "개를 구매했습니다.");
-        userInterface.showText(userLottos.getLottosString() + "\n");
+        userInterface.showText(userLottos.getLottosString());
+    }
+
+    // 당첨 번호 입력
+    private WinLotto getWinLotto() {
+        String input = "";
+        while (true) {
+            try {
+                userInterface.showText("당첨 번호를 입력해 주세요.");
+                input = userInterface.getUserInput();
+                validateWinNumbers(input);
+            } catch (IllegalArgumentException e) {
+                userInterface.showText(e.getMessage());
+                continue;
+            }
+            userInterface.newLine();
+            return new WinLotto(Arrays.asList(input.split(".")));
+        }
     }
 }

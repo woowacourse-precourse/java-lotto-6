85e7303b338014b9c9f712b6e83eda6fd61f0b1d
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-11-07 18:34:32 +0900

    fix : 2위 numberOfMatch 값 조정

--- src/main/java/lotto/constant/LottoPrize.java
+++ src/main/java/lotto/constant/LottoPrize.java
@@ -2,7 +2,7 @@ package lotto.constant;
 
 public enum LottoPrize {
     FIRST_PRIZE(6, 2_000_000_000)
-    , SECOND_PRIZE(51, 30_000_000)
+    , SECOND_PRIZE(50, 30_000_000)
     , THIRD_PRIZE(5, 1_500_000)
     , FOURTH_PRIZE(4, 50_000)
     , FIFTH_PRIZE(3, 5_000)

81639f0aa65326736eb1ea45320c972346fcb5cd
hwangkyojune <shane5969@naver.com>
2023-11-07 23:44:20 +0900

    bonus가 당첨번호와 중복될 경우 검사하는 메서드 추가

--- src/main/java/lotto/ValidateService.java
+++ src/main/java/lotto/ValidateService.java
@@ -9,7 +9,7 @@ public class ValidateService {
     private static final int LOTTO_PRICE = 1000;
     public static void validateNumber(String number){
         if(!number.matches("\\d+")){
-            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.errorMessage);
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getErrorMessage());
         }
     }
     public static void validateAuthorizedLength(List<Integer> numbers) {
@@ -19,16 +19,21 @@ public class ValidateService {
         }
     }
 
-    public static void validateProperNumbers(List<Integer> numbers) {
+    public static void validateNumbersInRange(List<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
-            int lottoNum = numbers.get(i);
-            if (lottoNum < MIN_NUMBER && lottoNum > MAX_NUMBER) {
-                throw new IllegalArgumentException(
-                        ErrorMessage.NON_PROPER_NUMS.getErrorMessage()
-                );
-            }
+            int number = numbers.get(i);
+            validateNumberInRange(number);
         }
     }
+
+    public static void validateNumberInRange(int number) {
+        if (number < MIN_NUMBER && number > MAX_NUMBER) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.NON_PROPER_NUMS.getErrorMessage()
+            );
+        }
+    }
+
     public static void validateDuplicatedNums(List<Integer> numbers) {
         int distinctNumbersCount = (int) numbers.stream().distinct().count();
         if (distinctNumbersCount != NUMBER_SIZE) {
@@ -37,9 +42,19 @@ public class ValidateService {
             );
         }
     }
+    public static void validateDuplicatedNums(Integer num, List<Integer> numbers){
+        numbers.add(num);
+        validateDuplicatedNums(numbers);
+    }
     public static void validateProperMoney(int money){
         if(money%LOTTO_PRICE!=0){
-            throw new IllegalArgumentException(ErrorMessage.INCORRECT_AMOUNT.errorMessage);
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_AMOUNT.getErrorMessage());
+        }
+    }
+
+    public static void validateBonusInWinningNums(Integer bonus,List<Integer> numbers){
+        if(numbers.contains(bonus)){
+            throw new IllegalArgumentException(ErrorMessage.BONUS_IN_WINNING_NUMBER.getErrorMessage());
         }
     }
 }

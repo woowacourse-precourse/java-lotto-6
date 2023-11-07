6f54a6203203f7cdb9020966030756ff36837b86
oiler831 <qudwls831@gmail.com>
2023-11-07 16:07:46 +0900

    fix: CashInHand 클래스를 LottoAmount로 변경
    금액보다 로또 갯수가 더 효용성이 있다 생각하여 변경

---
+++ src/main/java/lotto/LottoAmount.java
@@ -0,0 +1,42 @@
+package lotto;
+
+public class LottoAmount {
+    private static final int LOTTO_PRICE = 1000;
+    private final int lottoAmount;
+
+    public LottoAmount(String stringCash) {
+        validate(stringCash);
+        this.lottoAmount = Integer.parseInt(stringCash) / LOTTO_PRICE;
+        System.out.println();
+    }
+
+    private void validate(String stringCash) {
+        int cash = validateNumber(stringCash);
+        validateRange(cash);
+        validatePriceMultiple(cash);
+    }
+
+    private int validateNumber(String stringCash) {
+        try {
+            return Integer.parseInt(stringCash);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구매금액에는 숫자를 입력하시오.");
+        }
+    }
+
+    private void validateRange(int cash) {
+        if (cash <= 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액에는 0보다 큰 숫자를 입력하시오.");
+        }
+    }
+
+    private void validatePriceMultiple(int cash) {
+        if (cash % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은" + LOTTO_PRICE + "의 배수를 입력하시오.");
+        }
+    }
+
+    public int getLottoAmount() {
+        return this.lottoAmount;
+    }
+}

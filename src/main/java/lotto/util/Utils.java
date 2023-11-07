d4592a3964e981756f108c1413d6352697dc93b9
Junobee25 <wnsdhqo@naver.com>
2023-11-07 15:41:34 +0900

    refactor(Utils): stringToLong 검증 메서드 추가

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -1,8 +1,11 @@
 package lotto.util;
 
+import lotto.verification.Validation;
+
 public class Utils {
 
     public static long stringToLong(String string) {
+        Validation.verifyStringToLong(string);
         return Long.parseLong(string);
     }
 

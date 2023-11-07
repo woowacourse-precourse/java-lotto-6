4b34fae8df3fe6379075452308f323df6f0a19ad
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-07 21:40:08 +0900

    feat(LottoTest) : created getScore test method

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.Prize.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -40,5 +41,30 @@ class LottoTest {
     for(Integer number : unmatchedBonusNumber) {
       assertThat(lotto.matchWithBonusNumber(number)).isEqualTo(false);
     }
+  }
+
+  @DisplayName("로또 번호의 일치하는 개수를 정확히 계산한다.")
+  @Test
+  void getScore() {
+    Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+    Integer bonusNumber = 7;
+
+    Lotto firstPrizeLotto = new Lotto(List.of(1,2,3,4,5,6));
+    Lotto secondPrizeWithBonusLotto = new Lotto(List.of(2,3,4,5,6,7));
+    Lotto secondPrizeLotto = new Lotto(List.of(2,3,4,5,6,8));
+    Lotto thirdPrizeLotto = new Lotto(List.of(3,4,5,6,7,8));
+    Lotto fourthPrizeLotto = new Lotto(List.of(4,5,6,7,8,9));
+    Lotto failedPrizeLotto1 = new Lotto(List.of(5,6,7,8,9,10));
+    Lotto failedPrizeLotto2 = new Lotto(List.of(6,7,8,9,10,11));
+    Lotto failedPrizeLotto3 = new Lotto(List.of(7,8,9,10,11,12));
+
+    assertThat(firstPrizeLotto.getScore(winningLotto, bonusNumber)).isEqualTo(FIRST_PRIZE);
+    assertThat(secondPrizeWithBonusLotto.getScore(winningLotto, bonusNumber)).isEqualTo(SECOND_PRIZE);
+    assertThat(secondPrizeLotto.getScore(winningLotto, bonusNumber)).isEqualTo(THIRD_PRIZE);
+    assertThat(thirdPrizeLotto.getScore(winningLotto, bonusNumber)).isEqualTo(FOURTH_PRIZE);
+    assertThat(fourthPrizeLotto.getScore(winningLotto, bonusNumber)).isEqualTo(FIFTH_PRIZE);
+    assertThat(failedPrizeLotto1.getScore(winningLotto, bonusNumber)).isEqualTo(FAIL);
+    assertThat(failedPrizeLotto2.getScore(winningLotto, bonusNumber)).isEqualTo(FAIL);
+    assertThat(failedPrizeLotto3.getScore(winningLotto, bonusNumber)).isEqualTo(FAIL);
   }
 }

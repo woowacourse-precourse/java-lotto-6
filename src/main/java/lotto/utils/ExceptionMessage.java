661140fc0519ada2f1fb8ec4e38a203f50994ff1
ChoiseU <csw980928@gmail.com>
2023-11-07 14:40:10 +0900

    feat : 숫자가 아닌 값이 들어오면 에러 문구 노출 하도록 수정

--- src/main/java/lotto/utils/ExceptionMessage.java
+++ src/main/java/lotto/utils/ExceptionMessage.java
@@ -1,8 +1,9 @@
 package lotto.utils;
 
 public enum ExceptionMessage {
-    NUMBER("[ERROR] 숫자만 입력해주세요. 다시 구매금액을 입력해 주세요."),
-    THOUSAND("[ERROR] 천원 단위로 입력하세요. 다시 구매금액을 입력해 주세요.");
+    NUMBER("[ERROR] 숫자만 입력해주세요. 구매금액을 다시 입력해주세요."),
+    THOUSAND("[ERROR] 천원 단위로 입력해주세요. 구매금액을 다시 입력해주세요."),
+    WINNING_NUMBER("[ERROR] 숫자와 콤마만 이용하여 입력하세요. 당첨 번호를 다시 입력해 주세요.");
 
     private String value;
 
--- src/main/java/lotto/utils/LottoUtil.java
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -29,7 +29,7 @@ public class LottoUtil {
         if(deleteCommaNumber.matches(NUM_REG)) {
             return true;
         }
-        System.out.println();
+        System.out.println(ExceptionMessage.WINNING_NUMBER.getValue());
         return false;
     }
 }

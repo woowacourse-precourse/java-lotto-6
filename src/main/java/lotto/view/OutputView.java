5b953df5eebf584d0f2f5aca4d915b48cf4e4294
june-777 <wlwhswnsrl96@gmail.com>
2023-11-07 18:17:51 +0900

    feat(OutputView): 당첨 통계 화면 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,22 @@
 package lotto.view;
 
+import java.util.EnumMap;
 import java.util.List;
+import lotto.domain.Rank;
 import lotto.dto.LottoDto;
 import lotto.dto.PurchaseLottoDto;
+import lotto.dto.WinningStatisticsDto;
 
 public class OutputView {
-    private static final String PURCHASED_MESSAGE = "%d개를 구매했습니다.%n";
+    private static final String PURCHASED_MESSAGE_FORM = "%d개를 구매했습니다.%n";
     private static final String PURCHASE_LOTTO_FORM_DELIMITER = " \n";
+    
+    private static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---";
+    private static final String MATCH_COUNT_FORM = "%s - %d개%n";
+    private static final String TOTAL_STATISTICS_FORM = "총 수익률은 %s%%입니다.";
 
     public void printPurchaseLotto(PurchaseLottoDto purchaseLottoDto) {
-        System.out.printf(PURCHASED_MESSAGE, purchaseLottoDto.size());
+        System.out.printf(PURCHASED_MESSAGE_FORM, purchaseLottoDto.size());
         String purchaseLottoResultForm = makePurchaseLottoResultForm(purchaseLottoDto);
         System.out.println(purchaseLottoResultForm);
     }
@@ -21,5 +28,17 @@ public class OutputView {
                 .toList();
 
         return String.join(PURCHASE_LOTTO_FORM_DELIMITER, purchaseLottoResults);
+    }
+
+    public void printWinningStatistics(WinningStatisticsDto winningStatisticsDto) {
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        EnumMap<Rank, Integer> eachRankCounts = winningStatisticsDto.getEachRankCounts();
+        for (Rank rank : eachRankCounts.keySet()) {
+            if (rank == Rank.NONE) {
+                break;
+            }
+            System.out.printf(MATCH_COUNT_FORM, rank.getPrizeMessage(), eachRankCounts.get(rank));
+        }
+        System.out.printf(TOTAL_STATISTICS_FORM, winningStatisticsDto.getYieldRate().toString());
     }
 }

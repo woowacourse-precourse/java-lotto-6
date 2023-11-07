947f1d0cbc21ff764a17003b82fd819cdbc2fca3
star-candy <el0710@naver.com>
2023-11-07 20:55:43 +0900

    refacotr(lotto): 변수명 수정

--- src/main/java/model/Transform.java
+++ src/main/java/model/Transform.java
@@ -17,16 +17,15 @@ public class Transform {
     }
 
     public List<Integer> generateRandomList() {
-        List<Integer> randomList;
-
+        List<Integer> randoms;
         while (true) {
             try {
-                randomList = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+                randoms = Randoms.pickUniqueNumbersInRange(1, 45, 6);
                 break;
             } catch (IllegalArgumentException e) {
                 // 유효한 로또 번호를 생성할 수 없는 경우 다시 시도
             }
         }
-        return randomList;
+        return randoms;
     }
 }

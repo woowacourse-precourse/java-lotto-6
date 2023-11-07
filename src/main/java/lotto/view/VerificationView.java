6315b746aff73340f875f9ad090d5e0355ca5965
Junobee25 <wnsdhqo@naver.com>
2023-11-07 20:21:56 +0900

    style(VerificationView): 가독성 위한 자동 포매팅

--- src/main/java/lotto/view/VerificationView.java
+++ src/main/java/lotto/view/VerificationView.java
@@ -10,12 +10,15 @@ public class VerificationView {
     private final static String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 로또 번호는 1 부터 45 까지의 숫자로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_LENGTH_ERROR = "[ERROR] 로또 번호 6자리로 이루어져 있어야 합니다.";
     private final static String LOTTO_NUMBER_INTEGER_ERROR = "[ERROR] 문자열의 요소는 숫자 패턴이여야 합니다.";
+
     public static void verificationViewRangeInputMoney() {
         System.out.println(INPUT_MONEY_ERROR);
     }
+
     public static void verificationViewRangeInputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_ERROR);
     }
+
     public static void verificationViewStringToLong() {
         System.out.println(STRING_TO_LONG_ERROR);
     }

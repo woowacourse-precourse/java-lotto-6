8c80465826c4cd0b275570600bb41e386567973e
JoonKyo <joonkyolee@gmail.com>
2023-11-07 22:30:16 +0900

    feat(LottoRank): 로또 등수를 반환하는 메서드 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,2 +1,35 @@
-package lotto.domain;public enum LottoRank {
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST_RANK(6, 2_000_000_000),
+    SECOND_RANK(-1, 30_000_000),
+    THIRD_RANK(5, 1_500_000),
+    FOURTH_RANK(4, 50_000),
+    FIFTH_RANK(3, 5_000),
+    NO_LUCK(2, 0);
+
+    private final int matchNumbers;
+    private final int winningAmount;
+
+    LottoRank(int matchNumbers, int winningAmount) {
+        this.matchNumbers = matchNumbers;
+        this.winningAmount = winningAmount;
+    }
+
+    public int getMatchNumbers() {
+        return matchNumbers;
+    }
+
+    public int getWinningAmount() {
+        return winningAmount;
+    }
+
+    public static LottoRank getLottoRank(int matchNumbers) {
+        return Arrays.stream(values())
+                .filter(lottoRank -> lottoRank.matchNumbers == matchNumbers)
+                .findFirst()
+                .orElse(null);
+    }
 }

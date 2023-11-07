8339adcad9319c9ebd331a4833b9a3dcf9361f86
Choijake <shameless8@naver.com>
2023-11-07 09:49:42 +0900

    Refactor : LottoService 당첨 번호 만드는 메서드의 반환 타입 WinningNumber로 수정

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -3,37 +3,34 @@ package lotto.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import lotto.view.InputView;
 
 //TODO : 클래스 작성 컨벤션 확인하기
 class LottoService {
-    private Client client;
-    private Lotto winningNumber;
-
     public LottoService() {
         provideWinningNumber();
-        this.winningNumber = setWinningNumber();
     }
 
     public void getResult() {
         //TODO : client의 속성에 접근하기 위해 당첨번호와 로또번호를 나타내기 위한 클래스를 만든다 -> 로또 시스템을 나타내는 도메인을 따로 뺀다
+        //TODO : setwinningNumber메서드에 파라메터를 둬서 LottoService의 속성을 이용하지 않도록 한다
     }
 
-    public Lotto setWinningNumber() {
+    public WinnigNumber createWinningNumber() {
         List<Integer> winningNumber = provideWinningNumber();
-        return new Lotto(winningNumber);
+
+        return WinnigNumber.of(Lotto.from(winningNumber), provideBonusNumber());
     }
 
     private List<Integer> provideWinningNumber() {
-        String inputWinningNumberString = inputWinningNumber();
-        List<Integer> inputNumber = Stream.of(inputWinningNumberString.split(",\\s*"))
+        String WinningNumberString = InputHandler.inputWinningNumber();
+        List<Integer> inputNumber = Stream.of(WinningNumberString.split(",\\s*"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
         return inputNumber;
     }
 
-    private String inputWinningNumber() {
-        String inputNumber = InputView.inputWinningLottoNumber();
-        return inputNumber;
+    private int provideBonusNumber() {
+        int bonus = InputHandler.inputBonusNumber();
+        return bonus;
     }
 }

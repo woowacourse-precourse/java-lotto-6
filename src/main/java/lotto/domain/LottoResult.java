d381f396a5e76e7873f0e97f922e565da3ebbaf2
HyunmoBae <tsi0520@naver.com>
2023-11-08 05:57:12 +0900

    feat(lotto) : add LottoResult skeleton code

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public class LottoResult {
+
+    public int compareLottoTicket() {
+        return 0;
+    }
+
+    public int lottoResult() {
+        return 0;
+    }
+
+    public float rateOfReturn() {
+        return 0;
+    }
+
+}

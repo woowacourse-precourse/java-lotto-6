7161bd38f50ebf820d74d6ad94153a85590a2177
chan-won <clw8679@naver.com>
2023-11-07 23:32:18 +0900

    feat: input 안내문 출력 기능 구현

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -6,13 +6,19 @@ import java.util.stream.Stream;
 import lotto.validation.InputValidator;
 
 public class Input {
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public static int purchaseAmount() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
         String input = Console.readLine();
         InputValidator.validatePurchaseAmount(input);
         return Integer.parseInt(input);
     }
 
     public static List<Integer> winningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         String input = Console.readLine();
         InputValidator.validateWinningNumbersFormat(input);
         List<Integer> winningNumbers = Stream.of(input.split(","))
@@ -23,6 +29,7 @@ public class Input {
     }
 
     public static int bonusNumber() {
+        System.out.println(INPUT_BONUS_MESSAGE);
         String input = Console.readLine();
         InputValidator.validateBonusNumber(input);
         return Integer.parseInt(input);

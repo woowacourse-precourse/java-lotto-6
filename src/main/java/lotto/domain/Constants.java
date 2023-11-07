a81a9cf2edca77f648c50e7d4b4e9e57634cad86
seaniiio <cnc4750@naver.com>
2023-11-08 01:50:47 +0900

    feat(InputValidator): 구입 금액이 1000으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/lotto/domain/Constants.java
+++ src/main/java/lotto/domain/Constants.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 public class Constants {
     public static final int LOTTO_NUMBER = 6;
+    public static final int ZERO_NUMBER = 0;
 
     public static final String AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String LOTTO_NUMBER_OUTPUT_MESSAGE = "\n%s개를 구매했습니다.\n";
@@ -9,10 +10,11 @@ public class Constants {
     public static final String BONUS_NUMBER_INPUT_MESSAGE ="\n보너스 번호를 입력해 주세요.\n";
 
     public static final String LOTTO_PRICE_NOT_NUMBER_ERROR = "[ERROR] 로또 구입 금액은 숫자만 입력하세요.\n\n";
-    public static final String LOTTO_PRICE_NOT_POSITIVE_ERROR = "[ERROR] 로또 구입 금액은 양만 입력하세요.\n\n";
+    public static final String LOTTO_PRICE_NOT_POSITIVE_ERROR = "[ERROR] 로또 구입 금액은 양수만 입력하세요.\n\n";
+    public static final String LOTTO_PRICE_NOT_DIVIDED_ERROR = "[ERROR] 로또 구입 금액은 1000원 단위로 입력하세요.\n\n";
 
     public static final String WINNING_NUMBER_DUPLICATED_ERROR = "[ERROR] 로또 번호는 중복이 될 수 없습니다.";
 
-    public static final String BONUS_NUMBER_DUPLICATED_ERROR = "[ERROR] 보너스 숫자는 로또 번호와 중복이 될 수 없습니다.";
+    public static final String BONUS_NUMBER_DUPLICATED_ERROR = "[ERROR] 보너스 숫자는 로또 번호 중복이 될 수 없습니다.";
 
 }
--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -8,6 +8,7 @@ public class InputValidator {
 
     public void checkAmount(String amountInput) {
         checkAmountNum(amountInput);
+        checkDividedNum(Integer.parseInt(amountInput));
     }
 
     //숫자인지 확인
@@ -15,14 +16,20 @@ public class InputValidator {
         for (int i = 0; i < amountInput.length(); i++) {
             char c = amountInput.charAt(i);
             if (!Character.isDigit(c)) {
-                System.out.printf(Constants.LOTTO_PRICE_NOT_NUMBER_ERROR);
+                System.out.printf(constants.LOTTO_PRICE_NOT_NUMBER_ERROR);
                 throw new IllegalArgumentException();
             }
         }
     }
-    //1000으로 나누어떨어지는지 확인
 
-    //양수인지 확인
+    //1000으로 나누어떨어지는지 확인
+    private void checkDividedNum(int amountInput) {
+        if(amountInput % 1000 != constants.ZERO_NUMBER) {
+            System.out.printf(constants.LOTTO_PRICE_NOT_DIVIDED_ERROR);
+            throw new IllegalArgumentException();
+        }
+    }
+
 
 
 

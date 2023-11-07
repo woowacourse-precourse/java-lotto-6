a1c780b576f8330b41aadbc1cf1151ee374a3aaf
jiu6525 <jiu6525@daum.net>
2023-11-07 19:38:52 +0900

    feat : 로또 매치결과를 저장하는 기능 추가

--- src/main/java/lotto/domain/LottoMatch.java
+++ src/main/java/lotto/domain/LottoMatch.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LottoMatch {
 
     private static final int ZERO = 0;
+    private static final int ONE = 1;
+
 
     public Map<Rank, Integer> initializeLottoResult() {
         Map<Rank, Integer> result = new LinkedHashMap<>();
@@ -13,5 +16,18 @@ public class LottoMatch {
             result.put(rank, ZERO);
         }
         return result;
+    }
+
+    public Map<Rank, Integer> scoreBoard(List<Lotto> lottoNumbers, WinningLotto winningLotto) {
+        Map<Rank, Integer> result = initializeLottoResult();
+        for (Lotto lotto : lottoNumbers) {
+            Rank rank = lottoMatch(lotto, winningLotto);
+            result.put(rank, result.get(rank) + ONE);
+        }
+        return result;
+    }
+
+    private Rank lottoMatch(Lotto lotto, WinningLotto winningLotto) {
+        return null;
     }
 }

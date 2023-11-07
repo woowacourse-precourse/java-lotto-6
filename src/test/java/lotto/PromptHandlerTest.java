3d459be81f03baa2cf5ab2d410a01f31fcd033b1
jaea-kim <jaeakim98@gmail.com>
2023-11-08 07:29:14 +0900

    TEST: 당첨 번호 선정 테스트 코드 작성

--- src/test/java/lotto/PromptHandlerTest.java
+++ src/test/java/lotto/PromptHandlerTest.java
@@ -37,6 +37,30 @@ class PromptHandlerTest {
         );
     }
 
+    @Test
+    @DisplayName("입력받은 값이 숫자가 아닌 경우 IllegalArgumentException 예외가 발생한다.")
+    void input_winning_number_string() {
+        assertReadLineTest(
+                () -> {
+                    Assertions.assertThatThrownBy(() -> PromptHandler.inputWinningNumbers())
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                String.valueOf("ff9,2,3,4,5,6")
+        );
+    }
+
+    @Test
+    @DisplayName("입력받은 값의 구분자가 정해진 구분자가 아닌 경우 IllegalArgumentException 예외가 발생한다.")
+    void input_winning_number_delimiter() {
+        assertReadLineTest(
+                () -> {
+                    Assertions.assertThatThrownBy(() -> PromptHandler.inputWinningNumbers())
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                String.valueOf("1/2/3/4/5/6")
+        );
+    }
+
     private void assertReadLineTest(
             final Executable executable,
             final String value

7ca557cf633dff7db2031286f6b7323c510ca4a5
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 06:27:32 +0900

    refactor(parser테스트) : parser테스트 패키지 경로 변경

--- src/test/java/lotto/util/parser/WinningNumbersInputParserTest.java
+++ src/test/java/lotto/util/parser/WinningNumbersInputParserTest.java
@@ -1,11 +1,9 @@
-package lotto.domain.parser;
+package lotto.util.parser;
 
 import static lotto.ErrorMessage.LOTTO_WRONG_INPUT_ERROR_MESSAGE;
 import static lotto.ErrorMessage.PARSE_INT_ERROR_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.util.parser.InputParser;
-import lotto.util.parser.WinningNumbersInputParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

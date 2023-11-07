912142abd9f3c6162809f1ae77e9ab7a45eb8bdb
rakyun1 <dbskim234@naver.com>
2023-11-07 16:34:28 -0700

    Refactor(Lotto.class): refactor checkRank() checkRank() 안에서 Rank를 계산하지 않고 결과 값만 받아서 리턴합니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,49 +5,55 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
-    private final int LOTTO_LENGTH = 6;
 
-    public Lotto(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplicateNumber(numbers);
-        this.numbers = numbers;
-    }
+	private final List<Integer> lottoNumbers;
+	private final int LOTTO_LENGTH = 6;
 
-    @Override
-    public String toString() {
-        return numbers.toString();
-    }
+	public Lotto(List<Integer> numbers) {
+		validateSize(numbers);
+		validateDuplicateNumber(numbers);
+		this.lottoNumbers = numbers;
+	}
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
+	@Override
+	public String toString() {
+		return lottoNumbers.toString();
+	}
 
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_LENGTH) {
-            throw new IllegalArgumentException();
-        }
-    }
+	public List<Integer> getNumbers() {
+		return lottoNumbers;
+	}
 
-    private void validateDuplicateNumber(List<Integer> numbers){
+	private void validateSize(List<Integer> numbers) {
+		if (numbers.size() != LOTTO_LENGTH) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateDuplicateNumber(List<Integer> numbers) {
 		Set<Integer> duplicated = new HashSet<>(numbers);
-        if (duplicated.size() != LOTTO_LENGTH){
-            throw new IllegalArgumentException();
-        }
-    }
+		if (duplicated.size() != LOTTO_LENGTH) {
+			throw new IllegalArgumentException();
+		}
+	}
 
-    public RankEnum checkRank(List<Integer> winnerNumber, int bonus){
-        long count = numbers.stream().filter(winnerNumber::contains).count();
-        if (count == RankEnum.FIRST.getMatchCount()) return RankEnum.FIRST;
-        if (count == RankEnum.SECOND.getMatchCount()) {
-            if (numbers.contains(bonus)) return RankEnum.SECOND;
-            return RankEnum.THIRD;
-        }
-        if (count == RankEnum.FORTH.getMatchCount()) return RankEnum.FORTH;
-        if (count == RankEnum.FIFTH.getMatchCount()) return RankEnum.FIFTH;
-        return RankEnum.NONE;
-    }
-
+	/**
+	 * 당첨 번호와 보너스 번호를 인자로 받아서, 필드로 가지고 있는 로또 번호와 비교하고,
+	 * 번호가 같으면 count ++ 한 후에 count에 맞는 RankEnum 리턴
+	 * @param winnerNumber
+	 * @param bonus
+	 * @return 매치 횟수에 해당하는 RankEnum. 매치 횟수에 해당하는 RankEnum이 없을 경우 RankEnum.NONE을 반환합니다.
+	 */
+	public RankEnum checkRank(List<Integer> winnerNumber, int bonus) {
+		long count = lottoNumbers.stream().filter(winnerNumber::contains).count();
+		if (count == 5){
+			if (!lottoNumbers.contains(bonus)){
+				return RankEnum.THIRD;
+			}
+			return RankEnum.SECOND;
+		}
+		return RankEnum.getRankByMatchCount((int)count);
+	}
 
 
 }

11f150ca1c396d2de02cc9c143d77efb249c3b0a
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 02:14:47 +0900

    test: 'lotto클래스위치를 이동함에 따른 코드 변경'

--- src/test/java/repository/LottoRepositoryTest.java
+++ src/test/java/repository/LottoRepositoryTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class LottoRepositoryTest {
     LottoRepository lottoRepository = new LottoRepository(3);
 

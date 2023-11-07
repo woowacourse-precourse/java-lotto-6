1520e412d612fd309688254d4185f3cd246f3570
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-07 21:55:51 +0900

    fix(UserInput - getWinningNumbers()): 서로 다른 6개의 당첨 번호를 유저한테 입력 받기(제대로 입력할 때까지) + 예외처리 & 예외 메세지 구현

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -36,7 +36,6 @@ public class UserInput {
     }
 
 
-
     //후에 검증기능과, purchaseCount 리턴하는 기능 별도의 함수로 분리 리팩토링 예정!
     private void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
@@ -51,47 +50,62 @@ public class UserInput {
 
     //당첨 번호 입력
     public List<Integer> getWinningNumbers() {
-        String winningNumbersInput = Console.readLine();
-        //Array보다는  Collections 사용! - 1주차 피드백
-
-        //변수 이름에 자료형&자료구조 사용하지 마! - 2주차 피드백
-        List<String> splittedWinningNumbersInput = Arrays.asList(winningNumbersInput.split(","));
-
-        //나중에 for-each 문 개별 함수화 리팩토링!
+        String winningNumbersInput;
         List<Integer> winningNumbers = new ArrayList<>();
 
-        for (var number : splittedWinningNumbersInput) {
-            Integer.parseInt(number);
-            winningNumbers.add(Integer.valueOf(number));
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                winningNumbersInput = Console.readLine();
+
+                //Array보다는  Collections 사용! - 1주차 피드백
+
+                //변수 이름에 자료형&자료구조 사용하지 마! - 2주차 피드백
+                List<String> splittedWinningNumbersInput = Arrays.asList(winningNumbersInput.split(","));
+                validateWinningNumbers(splittedWinningNumbersInput);
+                hasDuplicates(splittedWinningNumbersInput);
+
+                //나중에 for-each 문 개별 함수화 리팩토링!
+                for (var number : splittedWinningNumbersInput) {
+                    Integer.parseInt(number);
+                    winningNumbers.add(Integer.valueOf(number));
+                }
+
+                break;
+
+                //중복 체크
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+        return winningNumbers;
+    }
+
+    private void validateWinningNumbers(List<String> splittedWinningNumbersInput) {
+        if (splittedWinningNumbersInput.size() != 6) {
+            throw new IllegalArgumentException("당첨 번호 6개를 입력해 주세요");
         }
 
-        //중복 체크
+        return;
+    }
 
-        return winningNumbers;
+
+    private void hasDuplicates(List<String> splittedWinningNumbersInput) {
+        Set<String> set = new HashSet<>();
+        for (var number : splittedWinningNumbersInput) {
+            if (!set.add(number)) {
+                throw new IllegalArgumentException("서로 다른 당첨 번호들을 입력해 주세요.");
+            }
+        }
+        return;
     }
 
 
     public void sortWinningNumbers(List<Integer> winningNumbers) {
         Collections.sort(winningNumbers);
     }
-
-
-//    public checkDuplicatesInWinningNumbers(List<Integer> winningNumbers) {
-//
-//    }
-
-    public void hasDuplicates(List<Integer> winningNumbers) {
-        Set<Integer> set = new HashSet<>();
-        for (Integer number : winningNumbers) {
-            if (!set.add(number)) {
-                throw new IllegalArgumentException("서로 다른 당첨 번호들을 입력해주세요.");
-            }
-        }
-        repeatWinningNumber = false;
-    }
-
-    // => while문으로 반복!
-
 
 ////////////////////////////////////////////////////////////////////////////////
 

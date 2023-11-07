bc8241f581bb4bdc3c7c61c99fdb76ba643a75cf
twoosky <lhjae3@naver.com>
2023-11-08 07:35:41 +0900

    feat: 보너스 번호 중복 검증 기능 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import static lotto.utils.ErrorMessages.BONUS_NUMBER_DUPLICATE;
+
+public class BonusNumber {
+    private final LottoNumber bonusNumber;
+
+    public BonusNumber(int number, WinningNumbers winningNumbers) {
+        this.bonusNumber = new LottoNumber(number);
+        validateDuplicate(winningNumbers);
+    }
+
+    private void validateDuplicate(WinningNumbers winningNumbers2) {
+        if (winningNumbers2.isContain(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE);
+        }
+    }
+
+}

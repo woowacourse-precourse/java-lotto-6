a74f6493c15849daa3049bd31cffd926fe01f0f7
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-11-07 15:29:53 +0900

    Lotto 랜덤 번호 생성 클래스 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,24 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        LottoPurchaseAmountValidator print= new LottoPurchaseAmountValidator();
-        print.validatePurchaseAmount();
+  public static void main(String[] args) {
+
+    // 금액 입력
+    LottoPurchaseAmountValidator print = new LottoPurchaseAmountValidator();
+    int price = print.validatePurchaseAmount();
+    System.out.println();
+
+    // 몇 장인지 파악
+    int page = print.calculateNumberOfTickets(price);
+    System.out.println(page + "개를 구매했습니다.");
+
+    // 랜덤한 로또 번호
+    List<Lotto> randomLottoList = new Lotto().generateMultipleLotto(page);
+    for (Lotto lott : randomLottoList) {
+      System.out.println(lott.getNumbers());
     }
+
+  }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,49 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+  private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+  public Lotto() {
+    this.numbers = createLottoByDuplicatedNumber();
+  }
+
+  public List<Integer> createLottoByDuplicatedNumber() {
+    List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    validate(randomNumbers);
+    return randomNumbers;
+  }
+
+
+  private void validate(List<Integer> numbers) {
+    if (numbers.size() != 6) {
+      throw new IllegalArgumentException("[ERROR]로또 번호는 6개여야 합니다.");
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    Set<Integer> uniqueNumbers = new HashSet<>();
+    for (int number : numbers) {
+      if (uniqueNumbers.contains(number)) {
+        throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+      }
+      uniqueNumbers.add(number);
     }
+  }
 
+  public List<Integer> getNumbers() {
+    return numbers;
+  }
+
+  public List<Lotto> generateMultipleLotto(int count) {
+    List<Lotto> lottoList = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      lottoList.add(new Lotto());
+    }
+    return lottoList;
+  }
 }

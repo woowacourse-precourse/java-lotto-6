374c6452c0362a52fa6fb4d88442a827e314fb0f
jooys130 <wndustn130@gmail.com>
2023-11-07 19:40:57 +0900

    feat: 로또를 생성한다

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
+import static lotto.constants.LottoConstant.LOTTO_NUMBER_SIZE;
+import static lotto.constants.LottoConstant.MAXIMUM_LOTTO_NUMBER;
+import static lotto.constants.LottoConstant.MINIMUM_LOTTO_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import lotto.constants.LottoConstant;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,9 +18,9 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) throws IllegalArgumentException {
-        if (numbers.size() != LottoConstant.LOTTO_NUMBER_SIZE) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(
-                "[ERROR] 로또는 " + LottoConstant.LOTTO_NUMBER_SIZE + "개의 숫자로 이루어져야 합니다.");
+                    "[ERROR] 로또는 " + LOTTO_NUMBER_SIZE + "개의 숫자로 이루어져야 합니다.");
         }
         if (existsDuplicateNumber(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
@@ -28,5 +32,9 @@ public class Lotto {
         return numbers.size() != numbersDeduplication.size();
     }
 
-    // TODO: 추가 기능 구현
+    public Lotto create() {
+        return new Lotto(
+            Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER,
+                    LOTTO_NUMBER_SIZE));
+    }
 }

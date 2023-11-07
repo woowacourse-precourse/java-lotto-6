61ffff7f9f0af70773ead8f3990dfde55449aad6
seaniiio <cnc4750@naver.com>
2023-11-08 01:42:08 +0900

    feat(WinningNumber): 사용자가 로또 번호에 이미 존재하는 숫자를 보너스 번호로 입력할 때 예외 발생

--- src/main/java/lotto/domain/WinningNumber/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber/WinningNumber.java
@@ -3,35 +3,57 @@ package lotto.domain.WinningNumber;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Constants;
+import lotto.util.InputUtil;
+import lotto.validator.WinningNumberValidator;
+
 
 public class WinningNumber {
-    private List<Integer> winningNumbers= new ArrayList<>();
+    private List<Integer> winningNumbers = new ArrayList<>();
     private int bonusNumber;
+    Constants constants = new Constants();
+    InputUtil inputUtil = new InputUtil();
+    WinningNumberValidator validator = new WinningNumberValidator();
+
 
     public void setWinningNumbers() {
-        System.out.println();
-        System.out.println("당첨 번호를 입력해 주세요.");
         inputWinningNumbers();
     }
 
     private void inputWinningNumbers() {
-        List<String> winningNumbersInput = Arrays.asList(Console.readLine().split(","));
-        //validate
+        List<String> winningNumbersInput;
+        while (true) {
+            winningNumbersInput = Arrays.asList(inputUtil.inputWinningNumber().split(","));
+            try {
+                validator.checkLottoNumber(winningNumbersInput);
+                break;
+            } catch (IllegalArgumentException e) {
+
+            }
+        }
+
         for (int i = 0; i < 6; i++) {
             winningNumbers.add(Integer.parseInt(winningNumbersInput.get(i)));
         }
     }
 
     public void setBonusNumber() {
-        System.out.println();
-        System.out.println("보너스 번호를 입력해 주세요.");
         inputBonusNumber();
     }
 
     private void inputBonusNumber() {
-        String bonusNumberInput = Console.readLine();
-        //validate
+        String bonusNumberInput;
+        while (true) {
+            bonusNumberInput = inputUtil.inputBonusNumber();
+            try {
+                validator.checkBonusNumber(winningNumbers, bonusNumberInput);
+                break;
+            } catch (IllegalArgumentException e) {
+
+            }
+        }
         this.bonusNumber = Integer.parseInt(bonusNumberInput);
     }
 
--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -1,22 +1,41 @@
 package lotto.validator;
 
+import lotto.domain.Constants;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class WinningNumberValidator {
 
+    Constants constants = new Constants();
+
     public void checkLottoNumber(List<String> lotto) {
         checkLottoDuplicate(lotto);
     }
 
-    //로또 번호가 중복이 없는지 확인
+    public void checkBonusNumber(List<Integer> lotto, String bonusNumber) {
+        // 숫자인지 validate하는 함수
+
+        checkBonusDuplicate(lotto, Integer.parseInt(bonusNumber));
+    }
+
+    // 로또 번호가 중복이 없는지 확인
     public void checkLottoDuplicate(List<String> lotto) {
-        List<String> Set;
         Set<String> lottoWithoutDuplicate = new HashSet<>(lotto);
         if (lotto.size() != lottoWithoutDuplicate.size()) {
-            System.out.println("[ERROR] 로또 번호는 중복이 될 수 없습니다.");
+            System.out.println(constants.WINNING_NUMBER_DUPLICATED_ERROR);
             throw new IllegalArgumentException();
+        }
+    }
+
+    // 보너스넘버가 중복이 아닌지 확인
+    public void checkBonusDuplicate(List<Integer> lotto, int bonusNumber) {
+        for (int i = 0; i < constants.LOTTO_NUMBER; i++) {
+            if(lotto.get(i) == bonusNumber) {
+                System.out.println(constants.BONUS_NUMBER_DUPLICATED_ERROR);
+                throw new IllegalArgumentException();
+            }
         }
     }
 }

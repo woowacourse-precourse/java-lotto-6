e1ecd4a75f88f4352b8292267cf39aed2d4d76e3
JongMinCh0i <wonderland314592@gmail.com>
2023-11-07 17:05:56 +0900

    feat(Money): 로또 구입 때 받는 금액을 Money 클래스로 래핑

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import static lotto.domain.LottoStore.LOTTO_PRICE;
+
+import lotto.view.InputView;
+
+public class Money {
+
+    private static final String LOTTO_PRICE_PER_THOUSAND = "[ERROR] 로또 금액은 1000원 단위로 판매할 수 있습니다.";
+    private int money = 0;
+
+    public Money(Integer money) {
+        validInputMoney(money);
+        this.money = money;
+    }
+
+    private int validInputMoney(int money) {
+        try {
+            if (money % LOTTO_PRICE != 0 || money <= 0) {
+                throw new IllegalArgumentException(LOTTO_PRICE_PER_THOUSAND);
+            }
+            return money;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return validInputMoney(new InputView().askPrice());
+        }
+    }
+}

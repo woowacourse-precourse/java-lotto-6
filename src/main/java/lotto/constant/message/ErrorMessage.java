662a67ba6df1508e26bb6738cbfec9aefd30e729
Annjieun <dkslwdms2121@naver.com>
2023-11-07 14:24:06 +0900

    fix(ErrorMessage): 상수명이 나오는 것을 수정

--- src/main/java/lotto/constant/message/ErrorMessage.java
+++ src/main/java/lotto/constant/message/ErrorMessage.java
@@ -6,9 +6,9 @@ import static lotto.constant.LottoNumber.LOTTO_NUMBER_LENGTH;
 
 public enum ErrorMessage {
     NON_INTEGER("올바른 숫자를 입력해 주세요."),
-    INVALID_RANGE_NUMBER(MINIMUM_LOTTO_NUMBER + "에서 " + MAXIMUM_LOTTO_NUMBER + "사이의 번호를 입력해 주세요."),
+    INVALID_RANGE_NUMBER(MINIMUM_LOTTO_NUMBER.getNumber() + "에서 " + MAXIMUM_LOTTO_NUMBER.getNumber() + "사이의 번호를 입력해 주세요."),
     NOT_PURCHASE_UNIT("1,000단위의 금액을 입력해 주세요."),
-    INVALID_LENGTH_LOTTO_NUMBER(LOTTO_NUMBER_LENGTH + "자리의 번호를 입력해 주세요."),
+    INVALID_LENGTH_LOTTO_NUMBER(LOTTO_NUMBER_LENGTH.getNumber() + "자리의 번호를 입력해 주세요."),
     DUPLICATE_NUMBER("번호는 중복될 수 없습니다."),
     DUPLICATE_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 

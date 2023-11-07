64582c5fa5e56b3026b18f1226be2987dddc8745
inhoru126 <inhoru126@gmail.com>
2023-11-07 21:39:44 +0900

    feat : 출력문구 클래스추가

---
+++ src/main/java/lotto/util/Message.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class Message {
+    public static final String OUTPUT_MESSAGE = "개를 구매했습니다.";
+
+    public static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_LOTTO_PRIZE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_LOTTO_BONNER = "보너스 번호를 입력해 주세요.";
+}

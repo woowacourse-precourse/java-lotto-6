882febe3b0eca11f96cff3c6cf6d11feb534853a
권장순 <jsoonworld@gmail.com>
2023-11-08 01:00:50 +0900

    docs( README ) : 발행한 로또 번호를 출력 구현 반영.

---
+++ src/main/java/lotto/v3/controller/LottoGenerationController.java
@@ -0,0 +1,21 @@
+package lotto.v3.controller;
+
+import lotto.v3.model.LottoMachine;
+import lotto.v3.model.Lotto;
+import lotto.v3.view.LottoGenerateView;
+import java.util.List;
+
+public class LottoGenerationController {
+    private final LottoGenerateView generateView;
+    private final LottoMachine machine;
+
+    public LottoGenerationController(LottoGenerateView generateView, LottoMachine machine) {
+        this.generateView = generateView;
+        this.machine = machine;
+    }
+
+    public void generateLottoTickets(int numberOfTickets) {
+        List<Lotto> tickets = machine.generateTickets(numberOfTickets);
+        generateView.displayLottoTickets(tickets);
+    }
+}

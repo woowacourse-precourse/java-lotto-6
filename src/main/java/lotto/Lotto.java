1ef8b52e5fdea38e15cce7430e62443a33040655
ilot12 <rlaehdwn60@gmail.com>
2023-11-07 18:42:30 +0900

    feat: Lotto클래스 예외 기준 추가
    기존에 6개 이외의 개수를 가진 로또에 대한 예외 처리 이외에 범위(1 ~ 45)에 벗어나는 수나 중복이 있는 수에 대해서 예외 처리 추가.
    이를 처리하기 위한 private 메서드 추가.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,9 @@
 package lotto;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,8 +16,25 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 각각의 로또는 6개의 수여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 한 로또에는 6개의 수가 있어야 합니다.");
         }
+        for (int number : numbers) {
+            if (!isInRange(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또의 수는 1 ~ 45의 범위에 있어야 합니다.");
+            }
+        }
+        if (existDuplication(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또의 수에는 중복이 없어야 합니다.");
+        }
+    }
+    
+    private boolean isInRange(int number) {
+        return (number >= 1 && number <= 45); 
+    }
+    
+    private boolean existDuplication(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<Integer>(numbers);
+        return numberSet.size() < numbers.size();
     }
     
     private void arrangeNumbers() {

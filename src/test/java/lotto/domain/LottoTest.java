e08a520af37cabf8306d8a25699b4d8e272c50a3
jiwoo <zbqmgldjfh@gmail.com>
2023-11-07 15:29:12 +0900

    test(LottoTest): LottoTest에 번호 매칭 테스트 작성 완료

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
 
@@ -59,5 +58,60 @@ class LottoTest {
 
         // when, then
         assertThatThrownBy(() -> new Lotto(lottoNumbers)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또가 몇게 매치되는지 확인한다.")
+    @Test
+    public void lottoMatchCount() {
+        // given
+        List<LottoNumber> compareNumbers = List.of(new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(17));
+
+        List<LottoNumber> lottoNumbers = List.of(new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6));
+
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        // when
+        int matchCnt = lotto.matchWinningNumberCount(compareNumbers);
+
+        // then
+        assertThat(matchCnt).isEqualTo(5);
+    }
+
+    @DisplayName("보너스 번호가 매치되는것을 확인한다.")
+    @Test
+    public void bonusNumberMatchTrue() {
+        // given
+        List<LottoNumber> lottoNumbers = List.of(new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6));
+
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        // when
+        boolean match = lotto.isMatchBonusNumber(new LottoNumber(1));
+
+        // then
+        assertThat(match).isTrue();
+    }
+
+    @DisplayName("보너스 번호가 매치되지 않는것을 확인한다.")
+    @Test
+    public void bonusNumberMatchFalse() {
+        // given
+        List<LottoNumber> lottoNumbers = List.of(new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6));
+
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        // when
+        boolean match = lotto.isMatchBonusNumber(new LottoNumber(7));
+
+        // then
+        assertThat(match).isFalse();
     }
 }

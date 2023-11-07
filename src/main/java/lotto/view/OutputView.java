60993d55925eede9dced9b84743729d2bb841e65
NahyeChoi <skgp0628@naver.com>
2023-11-07 23:43:57 +0900

    refactor(lotto): view 관련 기능 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+
 import lotto.message.PrintMessage;
+import lotto.model.Lotto;
 
 public class OutputView {
 	
-	public void printPurchaseAmount() {
+	public void printMoney() {
 		System.out.println(PrintMessage.PURCHASE_LOTTO.getMessage());
 	}
 	
-	public void printLottoAmount(int lottoAmount) {
-		System.out.println(lottoAmount + PrintMessage.PRINT_LOTTO.getMessage());
+	public void printLottoAmount(int amount) {
+		System.out.println("\n"+ amount + PrintMessage.PRINT_LOTTO.getMessage());
 	}
 	
 	public void printWinningNumbers() {
@@ -24,16 +27,20 @@ public class OutputView {
 		System.out.println(PrintMessage.RESULT_LOTTO_HEADER.getMessage());
 	}
 	
-	public void printCompareResult() {
-		System.out.println(PrintMessage.COMPARE_RESULT3.getMessage());
-		System.out.println(PrintMessage.COMPARE_RESULT4.getMessage());
-		System.out.println(PrintMessage.COMPARE_RESULT5.getMessage());
-		System.out.println(PrintMessage.COMPARE_RESULT5_2.getMessage());
-		System.out.println(PrintMessage.COMPARE_RESULT6.getMessage());
+	public void printCompareResult(int matchCount) {
+		System.out.println(PrintMessage.FIRST.getMessage()+matchCount);
+		System.out.println(PrintMessage.SECOND.getMessage()+matchCount);
+		System.out.println(PrintMessage.THIRD.getMessage()+matchCount);
+		System.out.println(PrintMessage.FOURTH.getMessage()+matchCount);
+		System.out.println(PrintMessage.FIFTH.getMessage()+matchCount);
 	}
 	
 	public void printRate() {
 		System.out.println("총 수익률은"+"입니다.");
+	}
+	
+	public void printLotto(List<Integer> numbers) { //
+		System.out.println(numbers);
 	}
 	
 //	3개 일치 (5,000원) - 1개

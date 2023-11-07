59fa9a0a2c9e921c1b62b96dab4ad1b6a73b8bca
Sur0n <skyzephyr333@gmail.com>
2023-11-08 00:08:22 +0900

    feat : build Lotto class setBonusNumber

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -27,6 +28,13 @@ public class Lotto {
         }
     }
 
+
+    // TODO: 추가 기능 구현
+    public void setBonusNumber(int bonusNumber) {
+        validateInRangebBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
     //https://doing7.tistory.com/145 참고 set 혹은 stream.distinct()이용
     // 중복값 검증부 제작
     private void validateDuplicate(List<Integer> numbers) {
@@ -36,12 +44,7 @@ public class Lotto {
         if (numbers.size() != checkDuplicateNumbers.size()) {
             throw new IllegalArgumentException();
         }
-    }
-
-    // TODO: 추가 기능 구현
-    public void setBonusNumber(int bonusNumber) {
-        validateInRangebBonusNumber(bonusNumber);
-        this.bonusNumber = bonusNumber;
+        //익셉셔널쪽으로 날릴 것
     }
 
     private void validateInRangebBonusNumber(int bonusNumber) {
@@ -52,6 +55,7 @@ public class Lotto {
         if (MagicNumbers.CHECK_NUMBER_FOURTYFIVE.getMagicNumbers() < bonusNumber) {
             throw new IllegalArgumentException();
         }
+        //익셉셔널쪽으로 날리기
     }
 
     public int buyLottoTickets(int moneys) {
@@ -59,16 +63,17 @@ public class Lotto {
         if (moneys % 1000 != 0) {
             throw new IllegalArgumentException();
         }
+        //익셉셔널쪽으로 날릴것
         int tickets;
-        return tickets = moneys/1000;
+        return tickets = moneys / 1000;
     }
 
     //리팩토링 필요
     public Set<Integer> pickRandoms() {
         Set<Integer> pickRandomnumbers = new HashSet<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        pickRandomnumbers.stream().sorted(); // reversed 사용시 반대로 정렬됨
+        // 스트림 사용법 확인
         return pickRandomnumbers;
-
     }
-
 
 }

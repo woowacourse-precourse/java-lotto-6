38c66f899c194cd7382e8e2bb02873de5698678e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-08 04:24:55 +0900

    feat: 로또 게임 실행 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoGenerator;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        LottoController lottoController = new LottoController(new InputView(), new LottoGenerator());
+
+        lottoController.draw();
     }
 }

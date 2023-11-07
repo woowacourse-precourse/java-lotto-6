d7c17511074c000be684a15ab37187dd63ef60d0
chosunghyun18 <krismon@naver.com>
2023-11-07 14:11:35 +0900

    refact : remove magice number

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,7 +3,10 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
-    private static final String ERRORHEAD ="[ERROR]" ;
+    public static final int LOTTO_BALLS_SIZE = 6;
+    public static final int LOTTO_BALL_MIN_RANGE = 1;
+    public static final int LOTTO_BALLS_MAX_RANGE = 45;
+    private static final String ERROR_HEAD ="[ERROR]" ;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -31,22 +34,22 @@ public class Lotto {
         validateDuplicate(numbers);
     }
     public void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) throw new IllegalArgumentException(ERRORHEAD);
+        if (numbers.size() != LOTTO_BALLS_SIZE) throw new IllegalArgumentException(ERROR_HEAD);
     }
 
     public void validateDuplicate(List<Integer> numbers) {
         long distinctCount = numbers.stream().distinct().count();
         if (numbers.size() != distinctCount) {
-            throw new IllegalArgumentException(ERRORHEAD);
+            throw new IllegalArgumentException(ERROR_HEAD);
         }
     }
 
     public  static void validateRange(List<Integer> numbers) {
         numbers.stream()
-                .filter(number -> number < 1 || number > 45)
+                .filter(number -> number < LOTTO_BALL_MIN_RANGE || number > LOTTO_BALLS_MAX_RANGE)
                 .findAny()
                 .ifPresent(number -> {
-                    throw new IllegalArgumentException(ERRORHEAD);
+                    throw new IllegalArgumentException(ERROR_HEAD);
                 });
     }
 }

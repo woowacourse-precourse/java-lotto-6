5b1a0bc97cba80aeb609d81f6496be8c3b95503a
gunwoong <gwj0421@gmail.com>
2023-11-07 21:40:48 +0900

    fix : 당첨 통계 부분의 "개" 부분 생략되어 문제 발생, 추가하여 문제 해결

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,12 +9,13 @@ public class OutputView {
     private static final String SHOW_LOTTO_CNT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String SHOW_RATE_OF_RETURN_MESSAGE = "총 수익률은 %s입니다.\n";
     private static final String SHOW_WINNING_STATISTICS = "당첨 통계\n---";
+    private static final String SHOW_WINNING_STATISTICS_DETAILS = "%s - %s개\n";
 
     private OutputView() {
     }
 
     public static void showLottoCnt(int lottoCnt) {
-        System.out.printf(SHOW_LOTTO_CNT_MESSAGE,lottoCnt);
+        System.out.printf(SHOW_LOTTO_CNT_MESSAGE, lottoCnt);
     }
 
     public static void showLotto(List<Lotto> lottoTickets) {
@@ -27,12 +28,11 @@ public class OutputView {
         System.out.println(SHOW_WINNING_STATISTICS);
         Rank[] ranks = Rank.values();
         for (int i = 0; i < winningDetail.size(); i++) {
-            System.out.println(ranks[i].getDesc() + " - " + winningDetail.get(i));
+            System.out.printf(SHOW_WINNING_STATISTICS_DETAILS, ranks[i].getDesc(), winningDetail.get(i));
         }
     }
 
     public static void showRateOfReturn(String rateOfReturn) {
-        System.out.println(SHOW_WINNING_STATISTICS);
-        System.out.printf(SHOW_RATE_OF_RETURN_MESSAGE,rateOfReturn);
+        System.out.printf(SHOW_RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 }

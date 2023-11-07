8f97f5efccad6d3945ba2a47d4ace9119e38fc7d
gyeommm <dkndob02@naver.com>
2023-11-07 17:24:56 +0900

    구매금액에서 발행할 로또의 개수 변수 및 관련 기능 분리

--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -1,23 +1,21 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class PurchaseAmount {
-    public static int number_of_lottoes;
+    private static int amount;
 
-    private void print_inputAmount(){
+    public int getAmount() {
+        return amount;
+    }
+
+    public void print_inputAmount(){
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    private void inputAmount(){
+    public void inputAmount(){
         String input_amount = readLine();
 
-        int amount;
         try {
             amount = Integer.parseInt(input_amount);
         } catch (NumberFormatException e) {
@@ -27,28 +25,5 @@ public class PurchaseAmount {
         if(amount < 0 || amount % 1000 != 0){
             throw new IllegalArgumentException("구입금액은 1000원 단위의 자연수로 입력해야 합니다.");
         }
-
-        number_of_lottoes = amount / 1000;
     }
-
-    private void print_number_of_lottoes(){
-        System.out.println("\n" + number_of_lottoes + "개를 구매했습니다.");
-    }
-
-
-    private List<Integer> pickLottoNumbers(){
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    }
-
-    private void createLottoes(){
-        List<Lotto> lottoes = new ArrayList<>();
-        for(int i = 0; i < number_of_lottoes; i++) {
-            List<Integer> picked_numbers = pickLottoNumbers();
-            Lotto lotto = new Lotto(picked_numbers);
-
-            lottoes.add(lotto);
-        }
-
-    }
-
 }

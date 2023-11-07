072650e282a75c1ec5972fde3c1bcaeed9838594
Uniaut <kunwoo0927@gmail.com>
2023-11-07 17:08:54 +0900

    feature(domain.Lotto): implement validate function
    Lotto::validate
         add validation logic to pass tests
         add Lotto::validateNumberInRange (sub method)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,7 +14,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException();
+        }
+        for (Integer number : numbers) {
+            validateNumberInRange(number);
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumberInRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

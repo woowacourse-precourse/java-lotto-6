e1735c603636d3dc422c15b0e4de38e8905e3a00
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-07 22:04:48 +0900

    refactor: Result 도메인 수정

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -6,8 +6,16 @@ import java.util.ArrayList;
 public class Result {
     private final List<Rank> Results =  new ArrayList<>();
 
+    public Result() {
+
+    }
     public Result(Rank rank) {
         Results.add(rank);
+    }
+
+    public Result add(Rank rank) {
+        Results.add(rank);
+        return this;
     }
 
     public List<Rank> getResults() {

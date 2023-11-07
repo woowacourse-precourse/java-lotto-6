05e66de9bea7ca834b45976c043c64888157f130
“sangjun121” <juncho12011201@gmail.com>
2023-11-08 00:49:47 +0900

    refactor: 로또 구입금액 입력시 view와 controller의 책임 분할 리펙토링
    view에서의 로직을 제거하기 위해, 기존코드 InputView에서의 올바른 입력시까지 예외 검증 -> 예외 발생 -> 재입력 반복을 controller로 이동. 또한 controller인 LottoManager에서는 검증 및 예외발생시 재입력을 재귀로 수행하도록 변경

--- src/main/java/lotto/controller/LottoManager.java
+++ src/main/java/lotto/controller/LottoManager.java
@@ -1,14 +1,27 @@
 package lotto.controller;
 
+import lotto.utility.Validation;
+import lotto.view.ExceptionView;
 import lotto.view.InputView;
 
 public class LottoManager {
-    private InputView inputView;
-    public LottoManager(){
-        inputView = new InputView();
-    }
+    public LottoManager(){}
 
     public void startLottoService(){
-        inputView.readUserLottoPurchaseAmount();
+        purchaseLotto();
+    }
+
+    private String purchaseLotto(){
+        String purchaseAmount;
+
+        try{
+            purchaseAmount = InputView.readUserLottoPurchaseAmount();
+            Validation.vaildatePurchaseAmount(purchaseAmount);
+        }catch (IllegalArgumentException | IllegalStateException e){
+            ExceptionView.printExceptionMessage(e.getMessage());
+            purchaseAmount = purchaseLotto();
+        }
+
+        return purchaseAmount;
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,22 +3,12 @@ package lotto.view;
 import lotto.utility.enums.Messages;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static lotto.utility.Validation.vaildatePurchaseAmount;
 
 public class InputView {
     private InputView(){}; //생성자로 인스턴스 생성 막기
 
-    public static void readUserLottoPurchaseAmount() {
-        boolean isValidInput = false;
-
+    public static String readUserLottoPurchaseAmount() {
         System.out.println(Messages.INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE.getMessage());
-        do{
-            try {
-                vaildatePurchaseAmount(readLine());
-                isValidInput = true;
-            }catch (IllegalArgumentException | IllegalStateException e){
-                ExceptionView.printExceptionMessage(e.getMessage());
-            }
-        }while (!isValidInput);
+        return readLine();
     }
 }

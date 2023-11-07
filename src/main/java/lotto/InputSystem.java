4802c03053ed8aafc2f116c0b681e12558b872ab
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-11-07 13:23:37 +0900

    feat/inputSystem: 보너스 번호를 입력받을 때, 숫자로 변환가능한지 확인하는 기능 구현

--- src/main/java/lotto/InputSystem.java
+++ src/main/java/lotto/InputSystem.java
@@ -13,29 +13,35 @@ public class InputSystem {
     public int inputPaymentAccount(){
 
         String paymentAccount = Console.readLine();
+
         checkIsDigit(paymentAccount);
 
         return Integer.parseInt(paymentAccount);
-    }
-
-    public int inputBonusNumber(){
-
-        String bonusNumber = Console.readLine();
-        checkIsDigit(bonusNumber);
-
-        return Integer.parseInt(bonusNumber);
     }
 
     public List<Integer> inputWinningNumbers(){
 
         String winningNumbers = Console.readLine();
         List<String> stringNumberList = Arrays.asList(winningNumbers.split(","));
-        //TODO: 리스트를 돌면서 숫자로 변환할수 있는 문자열인지 확인(예외검사)
+
+        for(String inputNum: stringNumberList){
+            checkIsDigit(inputNum);
+        }
+
         List<Integer> integerNumberList = stringNumberList.stream()
                 .map(s -> Integer.parseInt(s))
                 .collect(Collectors.toList());
 
         return integerNumberList;
+    }
+
+    public int inputBonusNumber(){
+
+        String bonusNumber = Console.readLine();
+
+        checkIsDigit(bonusNumber);
+
+        return Integer.parseInt(bonusNumber);
     }
 
     private void checkIsDigit(final String inputNum){

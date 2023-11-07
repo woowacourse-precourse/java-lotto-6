e7aaeef9fa0fbd4b8bb4b0c260d652124e8f23fe
yunju-m <dbswnfnal@naver.com>
2023-11-07 15:43:31 +0900

    Feat(LuckeyLotto): ✨ 보너스 로또 번호 1부터 45범위인지 확인하는 유효성 검사

--- src/main/java/lotto/LuckeyLotto.java
+++ src/main/java/lotto/LuckeyLotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class LuckeyLotto {
     private static final String WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호의 타입은 숫자이어야 합니다.";
     private static final String WRONG_SAME_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호는 중복없는 숫자이어야 합니다.";
+    private static final String WRONG_RANGE_LOTTO_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 로또 번호는 1부터 45사이의 숫자여야 합니다.";
+    private static final int LOTTO_NUMBER_RANGE_START = 1;
+    private static final int LOTTO_NUMBER_RANGE_END = 45;
     private int luckeyBonusNumber;
     private List<Integer> luckeyNumbers;
 
@@ -21,6 +24,13 @@ public class LuckeyLotto {
             throw new IllegalArgumentException(WRONG_TYPE_LOTTO_BONUS_NUMBER_MESSAGE);
         } else if (luckeyNumbers.contains(Integer.valueOf(inputBonusNumber))) {
             throw new IllegalArgumentException(WRONG_SAME_LOTTO_BONUS_NUMBER_MESSAGE);
+        } else if (!isWithValidRange(Integer.valueOf(inputBonusNumber))) {
+            throw new IllegalArgumentException(WRONG_RANGE_LOTTO_BONUS_NUMBER_MESSAGE);
         }
+    }
+
+    private boolean isWithValidRange(int inputBonusNumber) {
+        return inputBonusNumber >= LOTTO_NUMBER_RANGE_START
+                && inputBonusNumber <= LOTTO_NUMBER_RANGE_END;
     }
 }

5fb8be9df221c1795a99182644e9874ac69a2509
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-07 12:49:31 +0900

    refactor(lotto): 기존 검증 메서드 제거 및 LottoValidator로 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static lotto.validator.LottoValidator.validate;
+
 import java.util.List;
 
 public class Lotto {
@@ -8,12 +10,6 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public List<Integer> getNumbers() {

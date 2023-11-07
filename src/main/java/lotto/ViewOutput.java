0093269960efaf385e6827c42e214c6d2587c239
GumaLee <rkdxhd97@naver.com>
2023-11-07 18:44:31 +0900

    출력기능_당첨통계, 총수익률

--- src/main/java/lotto/ViewOutput.java
+++ src/main/java/lotto/ViewOutput.java
@@ -21,4 +21,20 @@ public class ViewOutput {
         System.out.println(stringBuilder);
         stringBuilder.setLength(0);
     }
+
+    public void showWinningStatistics(List<Integer> results) {
+        stringBuilder.append("\n당첨 통계\n" + "---\n");
+        stringBuilder.append("3개 일치 (5,000원) - " + results.get(0) + "개\n");
+        stringBuilder.append("4개 일치 (5,0000원) - " + results.get(1) + "개\n");
+        stringBuilder.append("5개 일치 (1,500,000원) - " + results.get(2) + "개\n");
+        stringBuilder.append("5개 일치, 보너스 볼 일치 (30,000,000원) - " + results.get(3) + "개\n");
+        stringBuilder.append("6개 일치 (2,000,000,000원) - " + results.get(0) + "개\n");
+
+        System.out.println(stringBuilder);
+        stringBuilder.setLength(0);
+    }
+
+    public void showTotalReturn(int totalReturn) {
+        System.out.println("총 수익률은 " + totalReturn + "%입니다.");
+    }
 }

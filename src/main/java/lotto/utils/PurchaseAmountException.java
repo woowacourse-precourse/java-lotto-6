b0df215068382d125950a687d8d4fc78d920282f
honeydanji <tjdwls3680@naver.com>
2023-11-08 00:28:57 +0900

    FEAT : 구입금액에 대한 예외 발생시 재입력 코드 추가 및 구현
    // 완료
    - 구입 금액에 대한 예외 처리 totalException 함수 구현
    >> 모든 상황에 대한 예외 함수를 효율적으로 처리가능.
    - 예외 발생 시 구입금액 입력함수 재호출 코드 추가

--- src/main/java/lotto/utils/PurchaseAmountException.java
+++ src/main/java/lotto/utils/PurchaseAmountException.java
@@ -1,9 +1,17 @@
 package lotto.utils;
 
+import lotto.view.Input;
+
 import static lotto.utils.ExceptionMessage.NON_DIVISIBLE_BY_1000;
 import static lotto.utils.ExceptionMessage.NOT_NUMBER;
 
 public class PurchaseAmountException {
+    Input input = new Input();
+
+    public void totalException(String input) {
+        notNumeric(input);
+        notDivisible(input);
+    }
 
     public void notNumeric(String input) {
         try {
@@ -11,6 +19,7 @@ public class PurchaseAmountException {
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_NUMBER.getMessage());
         }
+        this.input.inputPurchaseAmount();
     }
 
     public void notDivisible(String input) {
@@ -18,5 +27,6 @@ public class PurchaseAmountException {
         if(numberInput % 1000 != 0) {
             throw new IllegalArgumentException(NON_DIVISIBLE_BY_1000.getMessage());
         }
+        this.input.inputPurchaseAmount();
     }
 }

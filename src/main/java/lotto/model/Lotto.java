06b3420690fc8bcf619b5a1c5c0506b26a3ea3fb
DESKTOP-6F3759O\hyemin <gpals5987@gmail.com>
2023-11-08 03:48:17 +0900

    feat(Lotto): 로또 번호 범위와 중복 검사 기능 추가
    Lotto 클래스에 로또 번호의 범위와 중복을 검사하는 기능을 추가하였습니다. 이를 위해 validateNoDuplicates 메소드와 checkNumberInRange 메소드가 추가되었습니다.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,47 @@
 package lotto.model;
 
+import lotto.Util.WinningLottoNumberUtil;
+import lotto.constant.ExceptionConstants;
+import lotto.constant.GameConstants;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNoDuplicates(numbers);
+        checkNumberInRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionConstants.ERROR_NUMBER_COUNT);
+        }
+    }
+    public static void validateNoDuplicates(List<Integer> numbers){
+        int distinctCount = numbers.stream().distinct().collect(Collectors.toList()).size();
+        if(distinctCount < GameConstants.LOTTO_SIZE){
+            throw new IllegalArgumentException(ExceptionConstants.ERROR_DUPLICATE_NUMBER);
+        }
+    }
+    public static void checkNumberInRange(List<Integer> numbers){
+        for(int number: numbers){
+            if(number < GameConstants.STARTINCLUSIVE){
+                throw new IllegalArgumentException(String.format(ExceptionConstants.ERROR_NUMBER_OUT_OF_MIN_RANGE,GameConstants.STARTINCLUSIVE));
+            }
+            if(number > GameConstants.ENDINCLUSIVE){
+                throw new IllegalArgumentException(String.format(ExceptionConstants.ERROR_NUMBER_OUT_OF_MAX_RANGE,GameConstants.ENDINCLUSIVE));
+
+            }
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getLotto(){
+        return numbers;
+    }
+
 }

6a05fc168bc0251b133d96d6e34dead9432d4acc
inpink <inpink@kakao.com>
2023-11-08 00:17:02 +0900

    feat: main에서 컨트롤러 호출하여 로또 게임 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.configuration.AppConfig;
+import lotto.configuration.Config;
+import lotto.controller.LottoGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Config config = generateConfig();
+        LottoGameController lottoGameController = generateRacingController(config);
+        lottoGameController.playGame();
+    }
+
+    private static Config generateConfig() {
+        return AppConfig.getInstance();
+    }
+
+    private static LottoGameController generateRacingController(Config config) {
+        return config.lottoGameController();
     }
 }
+
+// TODO: final달기, 테스트에 중복되는 내용 처리

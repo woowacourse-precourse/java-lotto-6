815d9d5449fb5202491e5af9678fb45ac6e79604
Lieslot <rlaxodud1999@gmail.com>
2023-11-07 13:28:20 +0900

    feat: 금액 입력 예외처리 및 재입력 로직 추가

---
+++ src/main/java/lotto/view/ErrorMessageView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class ErrorMessageView {
+
+
+    public static void print(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+}
--- src/main/java/lotto/view/MoneyInputView.java
+++ src/main/java/lotto/view/MoneyInputView.java
@@ -1,11 +1,38 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lotto.exception.NotNaturalMoneyException;
+import lotto.exception.WrongMoneyInputFormatException;
+import lotto.exception.WrongMoneyUnitException;
+
 
 public class MoneyInputView {
-
+    private static final String match = "^[1-9](\\d*)0{3}$";
 
     public static String input() {
-        return Console.readLine();
+
+        String money = Console.readLine();
+
+        try {
+            validateMoney(money);
+
+        } catch(WrongMoneyInputFormatException e) {
+            ErrorMessageView.print(e.getMessage());
+            input();
+        }
+
+        return  money;
+    }
+
+    private static void validateMoney(String moneyInput) {
+        Pattern matcher = Pattern.compile(match);
+        Matcher matcher1 = matcher.matcher(moneyInput);
+        if (!matcher1.find()) {
+            throw new WrongMoneyInputFormatException();
+        }
+
+
     }
 }

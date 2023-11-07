198c345cdb92a9be8654fd304003469d0f3e230b
JoonKyo <joonkyolee@gmail.com>
2023-11-07 15:40:33 +0900

    docs, feat(LottoMachine): 플레이어의 구입 금액에 맞게 로또를 발행하는 메서드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,2 +1,29 @@
-package lotto.domain;public class LottoMachine {
+package lotto.domain;
+
+import static lotto.constraint.LottoConstraint.TOTAL_NUMBERS_OF_LOTTO;
+import static lotto.constraint.LottoConstraint.LOTTO_MINIMUM_BOUND;
+import static lotto.constraint.LottoConstraint.LOTTO_MAXIMUM_BOUND;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private LottoMachine() {
+    }
+
+    public List<Lotto> purchaseLotto(int gameCount) {
+        List<Lotto> playerLotto = new ArrayList<>();
+
+        for(int publishedLotto = 1; publishedLotto <= gameCount; gameCount--){
+            playerLotto.add(new Lotto(createLottoNumbers()));
+        }
+
+        return playerLotto;
+    }
+
+    private List<Integer> createLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                LOTTO_MINIMUM_BOUND.getValue(), LOTTO_MAXIMUM_BOUND.getValue(), TOTAL_NUMBERS_OF_LOTTO.getValue());
+    }
 }

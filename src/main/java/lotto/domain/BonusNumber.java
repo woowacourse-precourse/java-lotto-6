2a523e6a9716096caceda064149cc3b9edda1ace
dbstn1325 <dbstn1325@naver.com>
2023-11-07 12:32:09 +0900

    feat(BonusNumber): 보너스 번호가 1~45의 숫자가 아니면 IllegalArgumentException 예외처리 기능 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -2,9 +2,15 @@ package lotto.domain;
 
 public class BonusNumber {
     private static final String BONUS_NUMBER_MUST_NUMERIC_MESSAGE = "[ERROR] 보너스 번호는 숫자여야 합니다.";
+    private static final String BONUS_NUMBER_RANGE_MESSAGE = "[ERROR] 보너스 번호는 1~45의 숫자여야 합니다.";
+    private static final int START_RANGE_VALUE = 1;
+    private static final int END_RANGE_VALUE = 45;
+
     private final int bonusNumber;
 
+
     private BonusNumber(int bonusNumber) {
+        validateRange(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
@@ -19,5 +25,15 @@ public class BonusNumber {
 
     public int toInt() {
         return bonusNumber;
+    }
+
+    public void validateRange(int bonusNumber) {
+        if (isOverRange(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    private static boolean isOverRange(int number) {
+        return number < START_RANGE_VALUE || number > END_RANGE_VALUE;
     }
 }

4ead8ec4a2d2b6f1026a22964930d8a58f4c1ee3
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2023-11-08 03:10:08 +0900

    [ADD] Lotto 게임에서 각 당첨 결과를 정의하는데 사용

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,2 +1,26 @@
-package lotto.model;public class LottoResult {
+package lotto.model;
+
+public enum LottoResult {
+    FIRST(3, 5000),
+    SECOND(4, 50000),
+    THIRD(5, 1500000),
+    FOURTH(5, 30000000),
+    FIFTH(6, 2000000000);
+
+    private final int matchingNumbers;
+    private final int prize;
+
+    LottoResult(int matchingNumbers, int prize) {
+        this.matchingNumbers = matchingNumbers;
+        this.prize = prize;
+    }
+
+    public int getMatchingNumbers() {
+        return matchingNumbers;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
 }

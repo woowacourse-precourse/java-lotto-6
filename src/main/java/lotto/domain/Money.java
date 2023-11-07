6a0874b4986409ebe9d0403dbd700d535def1eb1
의찬 <srja432130@gmail.com>
2023-11-07 19:37:20 +0900

    feat: 로또 구입 금액을 관리하는 클래스를 추가

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import lotto.exception.DivideMoneyException;
+import lotto.exception.ZeroMoneyException;
+
+public class Money {
+    private static final int ZERO = 0;
+    private static final int THOUSAND = 1000;
+    private final int money;
+
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int inputValue) {
+        validateZero(inputValue);
+        validateDivideMoney(inputValue);
+    }
+
+    private void validateZero(int inputValue) {
+        if (inputValue == ZERO) {
+            throw new ZeroMoneyException();
+        }
+    }
+
+    private void validateDivideMoney(int inputValue) {
+        if (inputValue % THOUSAND != ZERO) {
+            throw new DivideMoneyException();
+        }
+    }
+}

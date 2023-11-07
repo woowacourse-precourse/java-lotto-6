460c4be165afb36ce32f71094dc64704d440e948
Dawin <dawin002@naver.com>
2023-11-07 23:01:15 +0900

    feat(Lotto): 로또 번호 범위 검증 기능 추가
    로또 번호의 범위가 1~45인지 검증하는 기능 추가:
    - void validateNumberInRange(List<Integer> numbers)
    - 범위를 벗어나는 경우 예외 발생시킴

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import lotto.model.enums.LottoSpec;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +11,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
+        validateNumberInRange(numbers);
         validateDuplicate(numbers);
         this.numbers = numbers;
     }
@@ -23,6 +26,14 @@ public class Lotto {
         HashSet<Integer> numberSet = new HashSet<>(numbers);
         if (numberSet.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    protected void validateNumberInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LottoSpec.MIN_NUMBER.getValue() || number > LottoSpec.MAX_NUMBER.getValue()) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 

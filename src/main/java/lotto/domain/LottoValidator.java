086d6a5c21e38f311593472d57d7de0cbd23d985
Nari <avakray@hansung.ac.kr>
2023-11-07 22:36:43 +0900

    feat(lotto): Lotto 중복 유효성 검사 메서드 추가

--- src/main/java/lotto/domain/LottoValidator.java
+++ src/main/java/lotto/domain/LottoValidator.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class LottoValidator {
     public void validate(List<Integer> numbers) {
         validateNumbersLength(numbers);
         validateNumberRange(numbers);
+        validateUniqueNumber(numbers);
     }
 
     private void validateNumbersLength(List<Integer> numbers) {
@@ -19,6 +22,13 @@ public class LottoValidator {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+        }
+    }
+
+    private void validateUniqueNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if(uniqueNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 }

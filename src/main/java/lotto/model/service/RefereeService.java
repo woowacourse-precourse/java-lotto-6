69a651b7b1cfa77c903ea5591abb74ff4b3b9337
Hayoon <hy97@sju.ac.kr>
2023-11-07 17:36:40 +0900

    feat(RefereeService): 로또결과 계산 함수(calculateLottoResult) 번호 일치 로직 equals -> contains로 수정

--- src/main/java/lotto/model/service/RefereeService.java
+++ src/main/java/lotto/model/service/RefereeService.java
@@ -11,31 +11,33 @@ public class RefereeService {
         List<Integer> winningNumbers = referee.getWinningNumbers();
         int bonusNumber = referee.getBonusNumber();
 
-        for(Lotto lotto : lottoList) {
+        for (Lotto lotto : lottoList) {
             int hitNumberCount = 0;
             boolean isHitBonusNumber = false;
             int rank = 0;
-            for(int i = 0; i < 6; i++) {
-                List<Integer> numbers = lotto.getNumbers();
-                if(numbers.get(i).equals(winningNumbers.get(i))) {
+            List<Integer> numbers = lotto.getNumbers();
+            for (int i = 0; i < 6; i++) {
+                if (winningNumbers.contains(numbers.get(i))) {
                     hitNumberCount++;
                 }
-                if(numbers.get(i).equals(bonusNumber)) {
-                    isHitBonusNumber = true;
-                }
             }
-            if(hitNumberCount == 6) {
+            if (numbers.contains(bonusNumber)) {
+                isHitBonusNumber = true;
+            }
+            if (hitNumberCount == 6) {
                 rank = 1;
-            } else if(hitNumberCount == 5 && isHitBonusNumber) {
+            } else if (hitNumberCount == 5 && isHitBonusNumber) {
                 rank = 2;
-            } else if(hitNumberCount == 5) {
+            } else if (hitNumberCount == 5) {
                 rank = 3;
-            } else if(hitNumberCount == 4) {
+            } else if (hitNumberCount == 4) {
                 rank = 4;
-            } else if(hitNumberCount == 3) {
+            } else if (hitNumberCount == 3) {
                 rank = 5;
             }
+
             referee.increaseCountForRank(rank);
+            referee.calculateLottoBenefit(lottoList.size() * 1000);
         }
         return referee;
     }

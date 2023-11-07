c4fceab40d01f09e75ec9e34f6da2febf2fa2f8a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-08 00:47:58 +0900

    test(ranking): 등수가 없는 경우 null 반환 테스트

--- src/test/java/lotto/domain/RankingTest.java
+++ src/test/java/lotto/domain/RankingTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RankingTest {
 
@@ -21,6 +23,7 @@ class RankingTest {
 
         assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.SECOND);
     }
+
     @Test
     void 로또_3등이_정상적으로_반환되는가() {
         int matchCount = 5;
@@ -28,6 +31,7 @@ class RankingTest {
 
         assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.THIRD);
     }
+
     @Test
     void 로또_4등이_정상적으로_반환되는가() {
         int matchCount = 4;
@@ -35,11 +39,20 @@ class RankingTest {
 
         assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.FOURTH);
     }
+
     @Test
     void 로또_5등이_정상적으로_반환되는가() {
         int matchCount = 3;
         boolean bonusMatch = false;
 
         assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.FIFTH);
+    }
+
+    @ParameterizedTest(name = "로또 등수가 없는 경우 null 반환")
+    @CsvSource(value = {"2", "1", "0"})
+    void 로또_등수가_없는_경우_null(int matchCount) {
+        boolean bonusMatch = false;
+
+        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(null);
     }
 }

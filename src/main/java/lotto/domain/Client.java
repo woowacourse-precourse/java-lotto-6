abb688af4c658c6e50b1ec2f0dcb4daae1af389c
Choijake <shameless8@naver.com>
2023-11-07 10:29:01 +0900

    Refactor : Client클래스 수정

--- src/main/java/lotto/domain/Client.java
+++ src/main/java/lotto/domain/Client.java
@@ -7,32 +7,28 @@ import lotto.Util.RandomNumberUtil;
 import lotto.view.InputView;
 
 class Client {
-    private int amountNumber;
     private static final int LOTTO_PRICE = 1000;
-    private List<List<Integer>> AutoGenerateLottoNumberList;
 
-    public Client() {
-        setAmountNumber();
-        setAutoGenerateLottoNumberList();
-    }
-
-    public void setAmountNumber() {
-        amountNumber = inputAmountNumber();
-    }
-
-    public void setAutoGenerateLottoNumberList() {
-        AutoGenerateLottoNumberList = new ArrayList<>();
-        IntStream.range(0, amountNumber)
+    public static List<List<Integer>> setAutoGenerateLottoNumberList() {
+        List<List<Integer>> AutoGenerateLottoNumberList = new ArrayList<>();
+        IntStream.range(0, provideAmountNumber())
                 .forEach(i -> AutoGenerateLottoNumberList.add(generateAutoLottoNumberList()));
-    }
 
-    private int inputAmountNumber() {
-        return InputView.inputAmount() / LOTTO_PRICE;
+        return AutoGenerateLottoNumberList;
     }
-
-    private List<Integer> generateAutoLottoNumberList() {
+    
+    private static List<Integer> generateAutoLottoNumberList() {
         List<Integer> lottoNumberList = new ArrayList<>();
         RandomNumberUtil.generateRandomNumberList(lottoNumberList);
         return lottoNumberList;
+    }
+
+    private static int provideAmountNumber() {
+        int amountNumber = inputAmountNumber();
+        return amountNumber;
+    }
+
+    private static int inputAmountNumber() {
+        return InputView.inputAmount() / LOTTO_PRICE;
     }
 }

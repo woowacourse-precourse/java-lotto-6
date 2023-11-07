ca247febccbf0c30ce4bb6e31949e6afe52c3605
jae woo Jung <awfjol2008@naver.com>
2023-11-08 02:45:17 +0900

    feat: 로또 번호 입력값을 검증하는 기능 구현

--- src/main/java/lotto/view/validator/InputValidator.java
+++ src/main/java/lotto/view/validator/InputValidator.java
@@ -1,8 +1,34 @@
 package lotto.view.validator;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
 
     private InputValidator() {
 
+    }
+
+    public static void validateLottoNumber(String input) {
+        validateIsEmpty(input);
+        validateIsRightFormat(input);
+    }
+
+    private static void validateIsEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 비어있지 않아야 합니다.");
+        }
+    }
+
+    private static void validateIsRightFormat(String input) {
+        if (isRightFormat(input)) {
+            throw new IllegalArgumentException("입력값은 쉼표(,)를 기준으로 구분되어야 합니다.");
+        }
+    }
+
+    private static boolean isRightFormat(String input) {
+        String regex = "^\\d+(,\\d+)*$";
+        return Pattern.compile(regex)
+                .matcher(input)
+                .matches();
     }
 }

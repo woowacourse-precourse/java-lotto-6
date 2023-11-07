421b4b55ef7e2aebef5f1618f7ec8ab6b9007094
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-11-07 23:04:44 +0900

    feat: Lotto 객체에 로또 번호 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,19 +2,21 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGame {
     Input input = new Input();
     Print print = new Print();
+    Lottos lottos;
     int purchasePrice = 0;
     int lottoCount = 0;
 
     public void playLottoGame() {
         purchasePrice = input.inputPurchasePrice();
         getLottoCountByPurchasePrice(purchasePrice);
-        getLottoNumbers(lottoCount);
+        lottos = getLottoNumbers(lottoCount);
     }
 
     private void getLottoCountByPurchasePrice(int price) {
@@ -22,12 +24,15 @@ public class LottoGame {
         print.printLottoCount(lottoCount);
     }
 
-    private void getLottoNumbers(int count) {
+    private Lottos getLottoNumbers(int count) {
+        List<Lotto> lottos = new ArrayList<>();
         while (count-- > 0) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(numbers);
             Lotto lotto = new Lotto(numbers);
             print.printLottoNumbers(lotto);
+            lottos.add(lotto);
         }
+        return new Lottos(lottos);
     }
 }

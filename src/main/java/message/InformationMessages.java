3088f5ae3fd8d2912b53af8d95ed5d7f8a50ffbf
jaegyeongkim <dongji_11@naver.com>
2023-11-07 16:57:54 +0900

    refactor: 메시지 출력 형식에 맞게 수정

--- src/main/java/message/InformationMessages.java
+++ src/main/java/message/InformationMessages.java
@@ -4,10 +4,10 @@ public class InformationMessages {
     public final static String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public final static String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     public final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    public final static String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    public final static String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
     public final static String WINNING_STATISTICS = "당첨 통계";
     public final static String SENTENCE_DELIMITER = "---";
     public final static String WINNING_RESULT = "%d개 일치 (%d원) - %d개\n";
     public final static String WINNING_RESULT_BONUS = "%d개 일치, 보너스 볼 일치 (%d원) - %d개\n";
-    public final static String EARNING_RATE_MESSAGE = "총 수익률은 %.1f%입니다.";
+    public final static String EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 }

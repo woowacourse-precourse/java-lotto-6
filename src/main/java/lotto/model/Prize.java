db5e48418c453beea60efd2fc02ca8df29b31100
WooJJam <jaemin5548@naver.com>
2023-11-07 16:59:14 +0900

    [Refactor] Prize enum 메시지 추가 및 getter 생성

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -1,18 +1,47 @@
 package lotto.model;
 
 public enum Prize {
-    FIRST(6, 2_000_000_000, 0),
-    SECOND(5, 30_000_000, 0, true),
-    THIRD(5, 1_500_000, 0),
-    FOURTH(4, 50_000,0),
-    FIFTH(3, 5_000,0),
-    NONE(0, 0,0);
+    FIRST(6, 2_000_000_000, "6개 일치", 0),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치", 0, true),
+    THIRD(5, 1_500_000, "5개 일치",0),
+    FOURTH(4, 50_000, "4개 일치",0),
+    FIFTH(3, 5_000, "3개 일치",0),
+    NONE(0, 0,"",0);
 
-    Prize(int match, int price, int count) {
-        this(match, price, count, false);
+    private int match;
+    private int price;
+    private String message;
+    private int count;
+    private boolean hasBonus;
+
+    Prize(int match, int price, String message,int count) {
+        this(match, price, message, count, false);
     }
 
-    Prize(int match, int price, int count, boolean hasBonus) {
+    Prize(int match, int price, String message, int count, boolean hasBonus) {
     }
 
+    public void increaseCount(Prize prize) {
+        prize.count++;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isHasBonus() {
+        return hasBonus;
+    }
 }

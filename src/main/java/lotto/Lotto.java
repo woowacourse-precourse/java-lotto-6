3b2426602d099031d6d7aea9575bbc095706bb6f
KwangHo <skh990427@naver.com>
2023-11-07 23:35:35 +0900

    feat: 입력 길이, 입력 범위, 중복 입력에 대한 예외처리 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import enums.LottoError;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +16,21 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(LottoError.NUMBER_COUNT_ERROR.getMessage());
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(LottoError.NUMBER_RANGE_ERROR.getMessage());
+            }
+
+            if (uniqueNumbers.contains(number)) {
+                throw new IllegalArgumentException(LottoError.NUMBER_DUPLICATE_ERROR.getMessage());
+            }
+            uniqueNumbers.add(number);
         }
     }
 
-    // TODO: 추가 기능 구현
 }

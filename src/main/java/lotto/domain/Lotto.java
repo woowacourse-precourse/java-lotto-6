10a84043df203dbe3239dc39b8d92744eaa5790f
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-08 04:52:04 +0900

    feat: 로또 번호 중복일 때 예외처리 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,7 +2,10 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.enums.ExceptionMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,8 +17,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            String error = String.format(ExceptionMessage.NUMBER_COUNT_ERROR.message, 6);
+            throw new IllegalArgumentException(error);
         }
+        if (validateDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException(ExceptionMessage.BONUS_NUMBER_DUPLICATE_ERROR.message);
+        }
+    }
+
+    private boolean validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> dupNumbers = new HashSet<>(numbers);
+        return numbers.size() != dupNumbers.size();
+
     }
 
     private List<Integer> sortNumbers(List<Integer> numbers) {
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,14 +1,22 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.enums.ExceptionMessage;
 
 public class WinningLotto {
     private final Lotto winningLotto;
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
         this.winningLotto = new Lotto(winningNumbers);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ExceptionMessage.BONUS_NUMBER_DUPLICATE_ERROR.message);
+        }
     }
 
     public Lotto getWinningLotto() {

7340a07137c6e0ce83a342a60b3f22cafc15ec66
waveofmymind <sjun0913@gmail.com>
2023-11-07 13:46:46 +0900

    test: 게임이 정상적으로 성공하는 통합 테스트를 구현한다.

--- src/test/java/lotto/application/LottoControllerTest.java
+++ src/test/java/lotto/application/LottoControllerTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -66,6 +69,28 @@ public class LottoControllerTest extends NsTest {
             runException("1000", "1,2,3,4,5,6", "6");
             assertThat(output()).contains(ErrorMessage.DUPLICATE_NUMBER.getMessage());
         });
+    }
+
+    @DisplayName("게임이 정상적으로 수행된다.")
+    @Test
+    void gameSuccessTest() {
+        assertRandomUniqueNumbersInRangeTest(() -> {
+                    run("2000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "2개를 구매했습니다.",
+                            "[1, 2, 3, 4, 5, 6]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 1개",
+                            "총 수익률은 100000000.0%입니다."
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(3, 5, 11, 16, 32, 38)
+        );
     }
 
     @Override

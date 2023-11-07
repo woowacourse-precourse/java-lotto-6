5428c393e15b02da92c6e9395fca324d18224353
sujin <how4530@naver.com>
2023-11-07 19:58:51 +0900

    Application, Lotto, Controller complete

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    private final int number;
+    
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -12,9 +13,25 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
+    
+    public Bonus(int number) {
+    	validate(number);
+    	checkInt(number);
+    	this.number = number;
+    }
+    
+    private void validate(int number) {
+    	if (number.size() != 1) {
+    		throw new IllegalArgumentException("[ERROR] 보너스 번호는 1개여야 합니다.")
+    	}
+    }
+    
+    private void checkInt(int number) {
+    	if (number.getClass().getName() != Integer) {
+    		throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.")
+    	}
+    }
 }

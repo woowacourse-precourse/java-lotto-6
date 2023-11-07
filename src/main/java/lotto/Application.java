4a4775b78974047e451287d847e989bc78715995
MeoungHum <emmabark84@gmail.com>
2023-11-07 11:14:09 +0900

    금액 투입 및 발행 개수 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,5 +8,15 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
+        int number = 0;
+        try {
+            number = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR]");
+        }
+        System.out.println();
+
+        System.out.println(number/1000+"개를 구매했습니다.");
     }
 }

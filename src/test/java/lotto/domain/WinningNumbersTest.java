e339566254d2521022989f03a3a36150d6f5e632
jinsu868 <mjinsu098@gmail.com>
2023-11-07 20:09:29 +0900

    test: 보너스 번호 테스트 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -34,6 +36,13 @@ class WinningNumbersTest {
     @DisplayName("당첨 번호가 중복되면 예외가 발생한다")
     void 당첨_번호_중복_실패_테스트() {
         assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 당첨_번호_범위_실패_테스트(int number) {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(number, 1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

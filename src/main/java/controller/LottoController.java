7796b1215b122783b399e3f43a20ea43e9566508
LeeSungNo-ian <isnn0103@gmail.com>
2023-11-08 04:22:09 +0900

    refactor: lottoResultOutputView 기능 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -5,6 +5,7 @@ import model.LottoRepository;
 import util.RandomNumberGeneratorUtil;
 import view.LottoBonusNumberInputView;
 import view.LottoPurchaseInputView;
+import view.LottoResultOutputView;
 import view.LottoWinningNumberInputView;
 import static model.LottoRepository.*;
 
@@ -17,6 +18,7 @@ public class LottoController {
     LottoWinningNumberInputView lottoWinningNumberInputView = new LottoWinningNumberInputView();
     LottoBonusNumberInputView lottoBonusNumberInputView = new LottoBonusNumberInputView();
     LottoRepository lottoRepository = new LottoRepository();
+    LottoResultOutputView lottoResultOutputView = new LottoResultOutputView();
 
     public void gameStart() {
         int lottoPurchase = lottoPurchaseInputView.readLottoPurchaseValidator();
@@ -24,7 +26,7 @@ public class LottoController {
         List<Integer> winningNumbers = lottoWinningNumberInputView.readLottoWinningNumber();
         int bonusNumberValue = lottoBonusNumberInputView.readLottoBonusNumber();
         Map<Integer, Integer> winningDetails = lottoRepository.printLottoResult(winningNumbers, bonusNumberValue);
-        showWinningResult(winningDetails);
+        lottoResultOutputView.showWinningResult(winningDetails);
     }
 
     public void createLotto(int price) {
@@ -45,14 +47,5 @@ public class LottoController {
     private void displayPurchaseQuantity(int totalLottoCount) {
         System.out.println();
         System.out.println(totalLottoCount + "개를 구매했습니다.");
-    }
-
-    public void showWinningResult(Map<Integer, Integer> lottoWinningStatistics) {
-        System.out.println("당첨 통계\n---");
-        System.out.println("3개 일치 (5,000원) - " + lottoWinningStatistics.get(RANK_FIFTH) + "개");
-        System.out.println("4개 일치 (50,000원) - " + lottoWinningStatistics.get(RANK_FOURTH) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + lottoWinningStatistics.get(RANK_THIRD) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoWinningStatistics.get(RANK_SECOND) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + lottoWinningStatistics.get(RANK_FIRST) + "개");
     }
 }

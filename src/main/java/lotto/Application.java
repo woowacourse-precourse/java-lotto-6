e410a3afadc0daa6270cea377cbc16caedd65e42
noteasymin <noteasymin@gmail.com>
2023-11-08 01:09:05 +0900

    fix: 예기치 못한 오류 테스트

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,6 @@
 package lotto;
 
-import static lotto.controller.LottoController.playLotto;
-
 public class Application {
     public static void main(String[] args) {
-        playLotto();
     }
 }

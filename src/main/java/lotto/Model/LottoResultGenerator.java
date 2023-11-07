eb1831133df89550bb301d0f9eed4183fa49ce07
YejiGong <yejigong00@gmail.com>
2023-11-08 01:48:52 +0900

    Feat: 로또 결과 생성 기계 구현

--- src/main/java/lotto/Model/LottoResultGenerator.java
+++ src/main/java/lotto/Model/LottoResultGenerator.java
@@ -1,4 +1,105 @@
 package lotto.Model;
 
+import java.util.HashMap;
+import java.util.List;
+import lotto.Global.LottoResult;
+
 public class LottoResultGenerator {
+    private WinningLotto winningLotto;
+    private List<Lotto> selledLottos;
+
+    private static String WINNING_NUMBER_MATCHES = "winning";
+    private static String BONUS_NUMBER_MATCHES = "bonus";
+    private static int FIFTH_WINNING_NUMBER_MATCH_COUNT = 3;
+    private static int FIFTH_WINNING_AMOUNT = 5000;
+    private static int FOURTH_WINNING_NUMBER_MATCH_COUNT = 4;
+    private static int FOURTH_WINNING_AMOUNT = 50000;
+    private static int THIRD_WINNING_NUMBER_MATCH_COUNT = 5;
+    private static int THIRD_BONUS_NUMBER_MATCH_COUNT = 0;
+    private static int THIRD_WINNING_AMOUNT = 1500000;
+    private static int SECOND_WINNING_NUMBER_MATCH_COUNT = 5;
+    private static int SECOND_BONUS_NUMBER_MATCH_COUNT = 1;
+    private static int SECOND_WINNING_AMOUNT = 30000000;
+    private static int FIRST_WINNING_NUMBER_MATCH_COUNT = 6;
+    private static int FIRST_WINNING_AMOUNT = 2000000000;
+
+    private class WinningLotto {
+        private List<Integer> winningNumber;
+        private int bonusNumber;
+
+        public WinningLotto(List<Integer> winningNumber, int bonusNumber) {
+            this.winningNumber = winningNumber;
+            this.bonusNumber = bonusNumber;
+        }
+
+        public HashMap<String, Integer> matchesResult(Lotto lotto) {
+            HashMap<String, Integer> result = new HashMap<>();
+            result.put(WINNING_NUMBER_MATCHES, lotto.matchesNumberList(winningNumber));
+            result.put(BONUS_NUMBER_MATCHES, lotto.matchesSingleNumber(bonusNumber));
+            return result;
+        }
+    }
+
+    public void getSelledLotto(Lotto lotto) {
+        selledLottos.add(lotto);
+    }
+
+    public void putWinningLottoNumber(List<Integer> winningNumber, int bonusNumber) {
+        winningLotto = new WinningLotto(winningNumber, bonusNumber);
+    }
+
+    public HashMap<String, Integer> getTotalLottoResult() {
+        HashMap<String, Integer> result = getLottosResult();
+        result.put(LottoResult.RATE_OF_RETURN_RESULT.getName(), getLottoRateOfResult(result));
+        return result;
+    }
+
+    public HashMap<String, Integer> getLottosResult() {
+        HashMap<String, Integer> result = new HashMap<>();
+        result.put(LottoResult.FIFTH_RESULT.getName(), getResultNumber(FIRST_WINNING_NUMBER_MATCH_COUNT));
+        result.put(LottoResult.FOURTH_RESULT.getName(), getResultNumber(FOURTH_WINNING_NUMBER_MATCH_COUNT));
+        result.put(LottoResult.THIRD_RESULT.getName(),
+                getResultNumber(THIRD_WINNING_NUMBER_MATCH_COUNT, THIRD_BONUS_NUMBER_MATCH_COUNT));
+        result.put(LottoResult.SECOND_RESULT.getName(),
+                getResultNumber(SECOND_WINNING_NUMBER_MATCH_COUNT, SECOND_BONUS_NUMBER_MATCH_COUNT));
+        result.put(LottoResult.FIRST_RESULT.getName(), getResultNumber(FIRST_WINNING_NUMBER_MATCH_COUNT));
+        return result;
+    }
+
+    public int getLottoRateOfResult(HashMap<String, Integer> lottoResult) {
+        int result = 0;
+        result += lottoResult.get(LottoResult.FIFTH_RESULT.getName()) * FIFTH_WINNING_AMOUNT;
+        result += lottoResult.get(LottoResult.FOURTH_RESULT.getName()) * FOURTH_WINNING_AMOUNT;
+        result += lottoResult.get(LottoResult.THIRD_RESULT.getName()) * THIRD_WINNING_AMOUNT;
+        result += lottoResult.get(LottoResult.SECOND_RESULT.getName()) * SECOND_WINNING_AMOUNT;
+        result += lottoResult.get(LottoResult.FIRST_RESULT.getName()) * FIRST_WINNING_AMOUNT;
+        return result;
+    }
+
+    private int getResultNumber(int winningMatchNumber) {
+        HashMap<String, Integer> lottoMatchResult = new HashMap<>();
+        int result = 0;
+        for (Lotto lotto : selledLottos) {
+            lottoMatchResult = winningLotto.matchesResult(lotto);
+            if (lottoMatchResult.get(WINNING_NUMBER_MATCHES) == winningMatchNumber) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    private int getResultNumber(int winningMatchNumber, int bonusMatchNumber) {
+        HashMap<String, Integer> lottoMatchResult = new HashMap<>();
+        int result = 0;
+        for (Lotto lotto : selledLottos) {
+            lottoMatchResult = winningLotto.matchesResult(lotto);
+            if (lottoMatchResult.get(WINNING_NUMBER_MATCHES) == winningMatchNumber
+                    && lottoMatchResult.get(BONUS_NUMBER_MATCHES) == bonusMatchNumber) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+
 }

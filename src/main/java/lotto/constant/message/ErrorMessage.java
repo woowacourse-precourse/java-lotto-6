2b8f33e7efe572acec6255c0370b142c7a5216ec
hyerimmy <heyrim2010@naver.com>
2023-11-08 00:42:47 +0900

    refactor: 오류메시지에 원화 포맷 적용 - 예. 1,000원 단위

--- src/main/java/lotto/constant/message/ErrorMessage.java
+++ src/main/java/lotto/constant/message/ErrorMessage.java
@@ -1,15 +1,16 @@
 package lotto.constant.message;
 
+import static lotto.constant.SystemData.MONEY_FORMAT;
 import static lotto.constant.SystemData.PURCHASE_AMOUNT_UNIT;
 import static lotto.constant.SystemData.MIN_IN_LOTTO_NUMBER;
 import static lotto.constant.SystemData.MAX_IN_LOTTO_NUMBER;
 
 public class ErrorMessage {
-    private static final String prefix = "[ERROR] ";
-    public static final String NUMBER_FORMAT_ERROR = prefix + "숫자만 입력 가능합니다.";
-    public static final String DUPLICATE_NUMBER = prefix + "중복되는 숫자입니다.";
+    private static final String PREFIX = "[ERROR] ";
+    public static final String NUMBER_FORMAT_ERROR = PREFIX + "숫자만 입력 가능합니다.";
+    public static final String DUPLICATE_NUMBER = PREFIX + "중복되는 숫자입니다.";
     public static final String INVALID_PURCHASE_AMOUNT_UNIT
-           = prefix + String.format("%d원 단위의 금액만 입력 가능합니다.", PURCHASE_AMOUNT_UNIT);
+           = PREFIX + String.format("%s원 단위의 금액만 입력 가능합니다.", MONEY_FORMAT.format(PURCHASE_AMOUNT_UNIT));
     public static final String INVALID_LOTTO_NUMBER_RANGE
-            = prefix + String.format("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", MIN_IN_LOTTO_NUMBER, MAX_IN_LOTTO_NUMBER);
+            = PREFIX + String.format("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", MIN_IN_LOTTO_NUMBER, MAX_IN_LOTTO_NUMBER);
 }

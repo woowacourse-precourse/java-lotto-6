711e2a50f7bc37ace38b3787e882214348211bf8
Hyso <hsk7953@gmail.com>
2023-11-07 21:04:25 +0900

    test: Parser class에 대해 테스트

--- src/test/java/lotto/util/ParserTest.java
+++ src/test/java/lotto/util/ParserTest.java
@@ -1,2 +1,30 @@
-package lotto.util;public class ParserTest {
+package lotto.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    Parser parser = new Parser();
+    @Test
+    @DisplayName("입력한 금액에 단위표시가 있을 때 테스트")
+    public void testParserInputWithComma(){
+        String input = "1,000";
+        int expected = 1000;
+        int result = parser.lottoPrice(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("정상적으로 입력했을 경우")
+    public void testParserValidInput() {
+        String input = "1000";
+        int expected = 1000;
+        int result = parser.lottoPrice(input);
+        assertEquals(expected, result);
+    }
+
+
+
 }

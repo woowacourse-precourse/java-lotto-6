714354fad1237ecdde8598585e4bd43b9ee5add6
YeaChan05 <qkenrdl05@gmail.com>
2023-11-07 21:10:44 +0900

    [REFACTOR] 불필요한 메서드 삭제

--- src/main/java/lotto/io/InputManager.java
+++ src/main/java/lotto/io/InputManager.java
@@ -1,19 +1,9 @@
 package lotto.io;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-
 public abstract class InputManager<T>  {
     ConsoleAdapter consoleAdapter= new ConsoleAdapter();
     public abstract T input();
     public abstract T validation(String  input);
-
-    protected ConsoleAdapter getConsoleAdapter() {
-        return consoleAdapter;
-    }
 
     protected void setConsoleAdapter(ConsoleAdapter consoleAdapter){
         this.consoleAdapter=consoleAdapter;

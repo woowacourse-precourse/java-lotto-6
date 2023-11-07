167f29d4e178933acf98656db9a9a6da00c9e789
mingeun0507 <tqdlfkg@naver.com>
2023-11-07 16:22:03 +0900

    feat: LottoNumbers 일급 콜렉션 레코드 구현

---
+++ src/main/java/lotto/domain/lotto/LottoNumbers.java
@@ -0,0 +1,34 @@
+package lotto.domain.lotto;
+
+import lotto.constants.ErrorConsts;
+
+import java.util.List;
+import java.util.Set;
+
+public record LottoNumbers(
+        List<LottoNumber> numbers
+) {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+
+    public LottoNumbers {
+        validate(numbers);
+    }
+
+    private void validate(final List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateDuplicated(numbers);
+    }
+
+    private void validateSize(final List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ErrorConsts.LOTTO_NUMBERS_SIZE_NOT_MATCH.getMessage());
+        }
+    }
+
+    private void validateDuplicated(final List<LottoNumber> numbers) {
+        Set<LottoNumber> lottoNumbers = Set.copyOf(numbers);
+        if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(ErrorConsts.LOTTO_NUMBERS_DUPLICATED.getMessage());
+        }
+    }
+}

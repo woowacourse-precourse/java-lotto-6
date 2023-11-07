a5e5c40476b04e10b8e7be0c7523e212fe17aba6
davemins <davemins4125@naver.com>
2023-11-07 11:03:35 +0900

    feat: 보너스 번호 입력 기능 추가 InputView에 보너스 번호를 입력하는 기능을 추가하였습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String inputAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
@@ -13,6 +14,11 @@ public class InputView {
 
     public static String inputLottoNumber() {
         System.out.println(INPUT_LOTTO_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }

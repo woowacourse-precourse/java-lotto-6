6d9bfdac6a155cb6b87a35f26c475ecaa1e0be70
ziy00n <jiyun4188@gmail.com>
2023-11-08 02:46:38 +0900

    feat : 당첨번호 입력 및 검증하는 기능, 보너스번호 입력 및 검증하는 기능 추가

--- src/main/java/validation/InputValidator.java
+++ src/main/java/validation/InputValidator.java
@@ -1,13 +1,40 @@
 package validation;
 
+import java.util.*;
+
 public class InputValidator {
+    private static final int UNIT_AMOUNT = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
 
     public static void validInputMoney(String lottoMoney) {
-        if(isDigit(lottoMoney)) {
+        if (isDigit(lottoMoney)) {
             throw new IllegalArgumentException("구입 금액은 숫자만 입력할 수 있습니다.");
         }
-        if(isDivisible(lottoMoney) || isCorrectRange(lottoMoney)) {
+        if (isDivisible(lottoMoney) || isMoneyRange(lottoMoney)) {
             throw new IllegalArgumentException("최소 금액은 1000원으로, 1000원 단위의 금액이어야 합니다.");
+        }
+    }
+
+    public static void validWinningNumber(List<Integer> numbers) {
+        if (isNumberRange(numbers)) {
+            throw new IllegalArgumentException("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (isNumberSize(numbers)) {
+            throw new IllegalArgumentException("당첨 번호는 6개의 숫자여야 합니다.");
+        }
+        if (isUnique(numbers)) {
+            throw new IllegalArgumentException("당첨 번호는 중복되지 않는 숫자여야 합니다.");
+        }
+    }
+
+    public static void validBonusNumber(String bonusNumber, List<Integer> winningNumber) {
+        if (isBonusNumberRange(Integer.parseInt(bonusNumber))) {
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (isInWinningNumber(Integer.parseInt(bonusNumber), winningNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되지 않은 숫자여야 합니다.");
         }
     }
 
@@ -18,11 +45,36 @@ public class InputValidator {
     }
 
     private static boolean isDivisible(String lottoMoney) {
-        return !(Integer.parseInt(lottoMoney) % 1000 > 0); // lottoMoney를 1000원으로 나눈 나머지가 0 이상이면 false 반환
+        return (Integer.parseInt(lottoMoney) % UNIT_AMOUNT) > 0;
     }
 
-    private static boolean isCorrectRange(String lottoMoney) {
-        return !(Integer.parseInt(lottoMoney) < 1000); // lottoMoney가 1000 미만이면 false 반환
+    private static boolean isMoneyRange(String lottoMoney) {
+        return Integer.parseInt(lottoMoney) < UNIT_AMOUNT;
     }
 
+    private static boolean isNumberRange(List<Integer> winningNumber) {
+        for (int number : winningNumber) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isBonusNumberRange(int bonusNumber) {
+        return (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER);
+    }
+
+    private static boolean isNumberSize(List<Integer> winningNumber) {
+        return winningNumber.size() != LOTTO_SIZE;
+    }
+
+    private static boolean isUnique(List<Integer> winningNumber) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winningNumber);
+        return uniqueNumbers.size() != LOTTO_SIZE;
+    }
+
+    private static boolean isInWinningNumber(int bonusNumber, List<Integer> winningNumber) {
+        return winningNumber.contains(bonusNumber);
+    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,21 +3,68 @@ package view;
 import camp.nextstep.edu.missionutils.Console;
 import validation.InputValidator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     public static final String INPUT_MONEY_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String WINIING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
-   public void inputMoney() {
-       System.out.println(INPUT_MONEY_PROMPT);
-       String lottoMoney = Console.readLine();
-       validateinputMoney(lottoMoney);
-   }
+    public static int inputMoney() {
+        System.out.println(INPUT_MONEY_PROMPT);
+        String lottoMoney = Console.readLine();
+        return validateInputMoney(lottoMoney);
+    }
 
-   private void validateinputMoney(String lottoMoney) {
-       try {
-           InputValidator.validInputMoney(lottoMoney);
-       } catch (IllegalArgumentException e) {
-           System.out.println("[ERROR] "+e.getMessage());
-           inputMoney();
-       }
-   }
+    private static int validateInputMoney(String lottoMoney) {
+        try {
+            InputValidator.validInputMoney(lottoMoney);
+            return Integer.parseInt(lottoMoney);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] "+e.getMessage());
+            return inputMoney();
+        }
+    }
+
+    public static List<Integer> getWinningNumber() {
+        System.out.println(WINIING_NUMBER_PROMPT);
+        String inputWinningNumber = Console.readLine();
+        List<Integer> winningNumber = splittedWinningNumber(inputWinningNumber);
+        return validateWinningNumber(winningNumber);
+    }
+
+    private static List<Integer> validateWinningNumber(List<Integer> winningNumber) {
+        try {
+            InputValidator.validWinningNumber(winningNumber);
+            return winningNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return getWinningNumber();
+        }
+    }
+
+    public static void getBonusNumber(List<Integer> winningNumber) {
+        System.out.println(BONUS_NUMBER_PROMPT);
+        String bonusNumber = Console.readLine();
+        validateBonusNumber(bonusNumber, winningNumber);
+    }
+
+    private static void validateBonusNumber(String bonusNumber, List<Integer> winningNumber) {
+        try {
+            InputValidator.validBonusNumber(bonusNumber, winningNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            getBonusNumber(winningNumber);
+        }
+    }
+
+    public static List<Integer> splittedWinningNumber(String input) {
+        String[] splittedNumber = input.trim().split(",");
+        List<Integer> result = new ArrayList<>();
+        for (int index = 0; index < splittedNumber.length; index++) {
+            result.add(Integer.parseInt(splittedNumber[index]));
+        }
+        return result;
+    }
 }

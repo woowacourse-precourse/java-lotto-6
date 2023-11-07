e54dcf8cd01fef120f9bbfb1efc61bb63ca1eb18
Hwangwoosam <sam1783@naver.com>
2023-11-07 15:36:47 +0900

    Lottol 클래스 유효성 검사 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,10 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
-
+    private MatchNumber matchType;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -14,7 +14,39 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+
+        Set<Integer> uniqueNumber = new HashSet<>(numbers);
+        if(uniqueNumber.size() != numbers.size()){
+            throw new IllegalArgumentException();
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public void printNumber(){
+        System.out.println(numbers);
+    }
+
+    public Integer getElement(int i){
+        return this.numbers.get(i);
+    }
+
+    public int compareNumber(Lotto catchNumbers,int bonusNumber){
+        int cnt = 0;
+
+        for(int i = 0; i < 6; i++){
+            if(numbers.contains(catchNumbers.getElement(i))){
+                cnt++;
+            }
+        }
+
+        if(cnt == 6){
+            cnt++;
+        }
+        matchType = MatchNumber.getMatchNumber(cnt);
+
+        if(numbers.contains(bonusNumber) && cnt == 5){
+            matchType = MatchNumber.getMatchNumber(6);
+        }
+
+        return cnt;
+    }
 }

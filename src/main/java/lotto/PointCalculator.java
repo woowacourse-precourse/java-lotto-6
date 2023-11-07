df84fcee25a6811db9c50663c5ee337d8c5fa2b2
kkw2758 <dtc3229@gmail.com>
2023-11-08 02:04:55 +0900

    Fix(PointCalculator) : BonusNumber 클래스 삭제, Lotto, AnswerLotto 수정 후 버그 수정

--- src/main/java/lotto/PointCalculator.java
+++ src/main/java/lotto/PointCalculator.java
@@ -4,16 +4,16 @@ public class PointCalculator {
     private final double BONUS_NUMBER_MATCH_POINT = 0.5;
     private final double BONUS_NUMBER_NOT_MATCH_POINT = 0;
 
-    public double calculateTotalPoint(Lotto lotto, AnswerLotto answerLotto, BonusNumber bonusNumber) {
-        return calculateGeneralPoint(lotto, answerLotto) + calculateBonusPoint(lotto, bonusNumber);
+    public double calculateTotalPoint(Lotto lotto, AnswerLotto answerLotto) {
+        return calculateGeneralPoint(lotto, answerLotto) + calculateBonusPoint(lotto, answerLotto.getBonusNumber());
     }
 
     private int calculateGeneralPoint(Lotto lotto, AnswerLotto answerLotto) {
         return answerLotto.countMatchNumber(lotto);
     }
 
-    private double calculateBonusPoint(Lotto lotto, BonusNumber bonusNumber) {
-        if (lotto.hasNumber(bonusNumber.getBonusNumber())) {
+    private double calculateBonusPoint(Lotto lotto, LottoNumber bonusNumber) {
+        if (lotto.hasNumber(bonusNumber.getNumber())) {
             return BONUS_NUMBER_MATCH_POINT;
         }
         return BONUS_NUMBER_NOT_MATCH_POINT;

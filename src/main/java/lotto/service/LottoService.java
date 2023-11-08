ee95d9d3eb66a2a56d3a3a4d495b3e4713362db7
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:23:30 +0900

    feat : purchaseLottoTickets 함수 생성
    tickets라는 list 리턴

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,10 @@
 package lotto.service;
 
 import lotto.domain.LottoMachine;
+import lotto.domain.LottoTicket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoService {
     private final LottoMachine lottoMachine;
@@ -9,4 +13,14 @@ public class LottoService {
         this.lottoMachine = lottoMachine;
     }
 
+    public List<LottoTicket> purchaseLottoTickets(int purchaseAmount) {
+        int numberOfTickets = purchaseAmount / LottoTicket.PRICE;
+        List<LottoTicket> tickets = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            tickets.add(lottoMachine.generateTicket());
+        }
+
+        return tickets;
+    }
 }

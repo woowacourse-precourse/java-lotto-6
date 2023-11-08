99d1e8a07f40fa605ea01c0c4561918c0bdf0f68
Concepty <khs1903b@gmail.com>
2023-11-08 16:18:19 +0900

    fix(Lotto): 검증 과정 수정
    검증 로직에 결함이 발견되어 수정한다. 로또 숫자 지정 범위인지 검증한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -25,10 +25,17 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또는 " + LOTTO_NUM_IN_A_TICKET + " 자리입니다.");
         }
         if (new HashSet<Integer>(numbers).size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또에 중복되는 숫자가 사용되었습니다.");
+        }
+        int numberOfValidElements =
+                (int)numbers.stream()
+                        .filter(n -> n <= LOTTO_NUM_MAX && n >= LOTTO_NUM_MIN)
+                        .count();
+        if (numberOfValidElements != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또에 유효하지 않은 숫자가 사용되었습니다.");
         }
     }
 

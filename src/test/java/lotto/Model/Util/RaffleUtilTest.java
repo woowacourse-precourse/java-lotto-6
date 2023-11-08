8efd76c2a4d487203ea1b4099878c29fb50e41e5
ppusda <ppusda1234@gmail.com>
2023-11-08 14:16:52 +0900

    test: 기능 테스트 추가

---
+++ src/test/java/lotto/Model/Util/RaffleUtilTest.java
@@ -0,0 +1,19 @@
+package lotto.Model.Util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class RaffleUtilTest {
+
+    private RaffleUtil raffleUtil = new RaffleUtil();
+
+    @Test
+    void parseWinningNumbers() {
+        String winningNumber = "1,2,3,4,5,6";
+        Integer[] array = new Integer[] {1,2,3,4,5,6};
+
+        assertEquals(List.of(array), raffleUtil.parseWinningNumbers(winningNumber));
+    }
+}

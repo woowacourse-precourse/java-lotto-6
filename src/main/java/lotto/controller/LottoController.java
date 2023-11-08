48c484d236475d290597e90fec67c7743edef3bd
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-08 17:29:02 +0900

    refactor : modify to only have logic for purchasing and generating lottery tickets

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,10 @@
 package lotto.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import lotto.model.BonusNumber;
-import lotto.model.Lotto;
-import lotto.model.Receipt;
 import lotto.model.PurchaseAmount;
-import lotto.model.UserLottos;
-import lotto.model.WinningNumbers;
 
-import lotto.service.AutoLottoService;
-import lotto.service.LottoScoringService;
+import lotto.service.LottoService;
 
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -20,8 +13,7 @@ import lotto.view.OutputView;
 public class LottoController {
     InputView inputView;
     OutputView outputView;
-    AutoLottoService autoLottoService;
-    LottoScoringService lottoScoringService;
+    LottoService lottoService;
 
     private static class SingletonLottoController {
         private static final LottoController LOTTO_CONTROLLER = new LottoController();
@@ -34,33 +26,20 @@ public class LottoController {
     private LottoController() {
         inputView = InputView.getInstance();
         outputView = OutputView.getInstance();
-        autoLottoService = AutoLottoService.getInstance();
-        lottoScoringService = LottoScoringService.getInstance();
+        lottoService = LottoService.getInstance();
     }
 
-    public void run() {
-        UserLottos userLottos = purchaseLotto();
-
-        showResult(userLottos);
-    }
-
-    private UserLottos purchaseLotto() {
-        UserLottos userLottos;
+    public void purchaseLotto() {
         PurchaseAmount purchaseAmount = inputPurchaseAmount();
 
         Integer numberOfLotto = purchaseAmount.getNumberOfLotto();
 
-        userLottos = generateUserLotto(numberOfLotto);
-        return userLottos;
-    }
+        outputView.printNumberOfPurchaseMessage(numberOfLotto);
 
-    private void showResult(UserLottos userLottos) {
-        WinningNumbers winningNumbers = inputWinningNumber();
-        BonusNumber bonusNumber = inputBonusNumber(winningNumbers);
-
-        Receipt receipt = writeReceipt(winningNumbers, bonusNumber, userLottos);
-
-        printReceipt(receipt);
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> lottoNumbers = lottoService.generateLottos();
+            outputView.printLottoNumbers(lottoNumbers);
+        }
     }
 
     private PurchaseAmount inputPurchaseAmount() {
@@ -79,71 +58,4 @@ public class LottoController {
         return purchaseAmount;
     }
 
-    private UserLottos generateUserLotto(Integer numberOfLotto) {
-        UserLottos userLottos;
-        List<Lotto> userLottoList = new ArrayList<>();
-
-        outputView.printNumberOfPurchaseMessage(numberOfLotto);
-
-        for (int i = 0; i < numberOfLotto; i++) {
-            List<Integer> lottoNumbers = autoLottoService.generateLottoNumbers();
-            outputView.printLottoNumbers(lottoNumbers);
-
-            Lotto newLotto = new Lotto(lottoNumbers);
-            userLottoList.add(newLotto);
-        }
-        userLottos = new UserLottos(userLottoList);
-        return userLottos;
-    }
-
-    private WinningNumbers inputWinningNumber() {
-        WinningNumbers winningNumbers;
-        try {
-            outputView.printInputWinningNumMessage();
-
-            List<Integer> winningNumbersInput = inputView.inputWinningNumbers();
-
-            winningNumbers = new WinningNumbers(winningNumbersInput);
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e);
-
-            winningNumbers = inputWinningNumber();
-        }
-        return winningNumbers;
-    }
-
-    private BonusNumber inputBonusNumber(WinningNumbers winningNumbers) {
-        BonusNumber bonusNumber;
-        try {
-            outputView.printInputBonusNumMessage();
-
-            Integer bonusNumberInput = inputView.inputBonusNumber();
-
-            bonusNumber = new BonusNumber(winningNumbers, bonusNumberInput);
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e);
-
-            bonusNumber = inputBonusNumber(winningNumbers);
-        }
-
-        return bonusNumber;
-    }
-
-    private Receipt writeReceipt(WinningNumbers winningLotto, BonusNumber bonusNumber, UserLottos userLottos) {
-        Receipt receipt;
-
-        Integer[] userLottoRankings = lottoScoringService.rankUserLottos(winningLotto, bonusNumber, userLottos);
-
-        receipt = new Receipt(userLottoRankings, userLottos.getNumberOfLotto());
-
-        return receipt;
-    }
-
-    private void printReceipt(Receipt receipt) {
-        outputView.printReceiptTitle();
-
-        outputView.printReceiptDetail(receipt);
-
-        outputView.printProfitRatio(receipt.getProfitRatio());
-    }
 }

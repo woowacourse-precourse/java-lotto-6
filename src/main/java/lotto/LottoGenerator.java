fa845ecb0f9ed55662ffe4da19e6b57926d82c69
happyryan <tjdgks120813@gmail.com>
2023-11-08 17:19:44 +0900

    refactor(lotto): make LottoGenerator package-private and adjust method order
    Moved the LottoGenerator class to package-private and adjusted the method order for better organization.

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -4,20 +4,13 @@ import java.util.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class LottoGenerator {
+class LottoGenerator {
 
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
     private static final int LOTTO_SIZE = 6;
 
-    private static List<Integer> generateLotteryNumber() {
-        List<Integer> lottertyNumbers = new ArrayList<>(
-                Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_SIZE));
-        Collections.sort(lottertyNumbers);
-        return lottertyNumbers;
-    }
-
-    public static List<Lotto> generateLottos(int numberOfLottos) {
+    static List<Lotto> generateLottos(int numberOfLottos) {
         List<Lotto> lottos = new ArrayList<>();
         for (int lottoNumber = 0; lottoNumber < numberOfLottos; lottoNumber++) {
             Lotto lotto = generateLotto();
@@ -29,5 +22,12 @@ public class LottoGenerator {
     private static Lotto generateLotto() {
         Lotto lotto = new Lotto(generateLotteryNumber());
         return lotto;
+    }
+
+    private static List<Integer> generateLotteryNumber() {
+        List<Integer> lottertyNumbers = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_SIZE));
+        Collections.sort(lottertyNumbers);
+        return lottertyNumbers;
     }
 }

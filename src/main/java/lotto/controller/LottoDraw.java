b29646813f187e9253abd788294bf9bf13f3d91a
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 14:31:21 +0900

    refactor: delete validation method
    is not use

--- src/main/java/lotto/controller/LottoDraw.java
+++ src/main/java/lotto/controller/LottoDraw.java
@@ -3,14 +3,9 @@ package lotto.controller;
 import lotto.model.Customer;
 import lotto.model.LottoWinning;
 import lotto.model.WinningEnquiry;
-import lotto.validation.LottoValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-import lotto.view.constant.ErrorMessage;
 
-import java.util.List;
-
-import static java.lang.Integer.parseInt;
 import static lotto.constant.LottoConstant.*;
 import static lotto.view.constant.ConstantMessage.*;
 
@@ -68,9 +63,6 @@ public class LottoDraw {
 
         while (customer.getLottosSize() < numOfLotto) {
             customer.buyLotto();
-            if (validationLottoRange(customer.getLastLotto().getNumbers())) {
-                customer.throwLastLotto();
-            }
         }
         OutputView.printLottos(customer);
     }
@@ -80,14 +72,11 @@ public class LottoDraw {
         try {
             OutputView.printConstantMessage(BONUS_NUMBER);
             String _input = InputView.requestBonusNumber();
-            if (validationBonusNumber(_input)) {
-                throw new IllegalArgumentException(ErrorMessage.ERROR.getMessage() + ErrorMessage.INPUT.getMessage());
-            }
 
-            lottoWinning.initBonusNumber(_input);
+            lottoWinning.setBonusNumber(_input);
             OutputView.println(String.valueOf(lottoWinning.getBonusNumber()));
         } catch (IllegalArgumentException error) {
-
+            inputBonusNumber();
         }
     }
 
@@ -97,66 +86,10 @@ public class LottoDraw {
             OutputView.printConstantMessage(WINNING_NUMBER);
             String _winningNumber = InputView.requestWinningNumber();
 
-            if (validationWinningLotto(_winningNumber)) {
-                throw new IllegalArgumentException(ErrorMessage.ERROR.getMessage() + ErrorMessage.INPUT.getMessage());
-            }
-
             lottoWinning = new LottoWinning(_winningNumber);
             OutputView.printLottoWinning(lottoWinning.getWinningNumber());
         } catch (IllegalArgumentException error) {
             inputWinningNumber();
         }
     }
-
-    private boolean validationBonusNumber(String _input) {
-        // check Number
-        if (LottoValidator.checkIsNotNumber(_input)) {
-            OutputView.printError(ErrorMessage.IS_NOT_NUMBER.getMessage());
-            return true;
-        }
-
-        // check 1 ~ 45
-        if (!LottoValidator.checkRangeNumber(parseInt(_input), RANGE_START.getNumber(), RANGE_END.getNumber())) {
-            OutputView.printError(ErrorMessage.LOTTO_OUT_OF_RANGE.getMessage());
-            return true;
-        }
-
-        // check winning lotto duplicate
-        if (LottoValidator.checkDuplicateNumber(lottoWinning.getWinningNumber(), parseInt(_input))) {
-            OutputView.printError(ErrorMessage.DUPLICATE_NUMBER.getMessage());
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean validationWinningLotto(String _input) {
-        if (LottoValidator.checkSizeIsSix(_input)) {
-            OutputView.printError(ErrorMessage.SIZE_IS_NOT_SIX.getMessage());
-            return true;
-        }
-
-        List<String> _lotto = List.of(_input.split(","));
-        if (LottoValidator.checkIsNotNumberList(_lotto)) {
-            OutputView.printError(ErrorMessage.IS_NOT_NUMBER_LIST.getMessage());
-            return true;
-        }
-
-        List<Integer> __lotto = _lotto.stream().map(Integer::parseInt).toList();
-        if (LottoValidator.checkRangeLotto(__lotto)) {
-            OutputView.printError(ErrorMessage.LOTTO_OUT_OF_RANGE.getMessage());
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean validationLottoRange(List<Integer> _lotto) {
-        if (LottoValidator.checkRangeLotto(_lotto)) {
-            OutputView.printError(ErrorMessage.LOTTO_OUT_OF_RANGE.getMessage());
-            return true;
-        }
-        return false;
-    }
-
 }

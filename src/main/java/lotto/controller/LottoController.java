c63d774a9824a529d1688a6e8e7464d58407e5e2
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-11-08 16:14:24 +0900

    feat: 로또 컨트롤 일부(입력에 따른 로또 생성 및 출력) 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.NumberGenerator;
+import lotto.view.Input;
+import lotto.view.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    ArrayList<Lotto> lottos;
+    NumberGenerator numberGenerator;
+    Input input;
+    Output output;
+
+    public LottoController() {
+        lottos = new ArrayList<>();
+        numberGenerator = new NumberGenerator();
+        input = new Input();
+        output = new Output();
+    }
+
+    public void start() {
+        int lottoBuyAmount = input.requestLottoBuyAmount();
+        makeLotto(lottoBuyAmount);
+        output.printLotteryNumber(lottos);
+        List<Integer> lottoPrizeNums = input.requestLottoPrizeNums();
+
+    }
+
+    public void makeLotto(int lottoBuyAmount) {
+        for(int i=0;i<lottoBuyAmount;i++) {
+            List<Integer> nums = numberGenerator.getLotteryRandomNumber();
+            lottos.add(new Lotto(nums));
+        }
+    }
+}

96eefac3f429eb2c963a8de5a8103885ba688357
KongJunWoo <joonoo3@naver.com>
2023-11-08 23:32:55 +0900

    feat : 로또 추첨 프로그램 생성 후 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.Game;
+
 public class Application {
     public static void main(String[] args) {
+        Game.create().run();
         // TODO: 프로그램 구현
     }
 }

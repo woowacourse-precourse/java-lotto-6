fcfc58bbe53074e8156b53b5ff68ed0b4d5a087e
gundorit <packdev937@gmail.com>
2023-11-08 14:28:28 +0900

    refactor (OutputView) : OutputView를 리팩토링 한다
    - 몇몇 문자열들을 상수로 변환
    - 반올림 포맷 변경
    - 보너스 번호에 대한 출력 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -14,8 +14,13 @@ public class OutputView {
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.\n";
     private static final String WINNING_STATISTICS_HEADER = "\n당첨 통계\n";
     private static final String DIVIDER = "---\n";
-    private static final String WINNING_STATISTICS_FORMAT = "%d개 일치 (%s원) - %d개\n";
-    private static final String TOTAL_EARNING_RATE_FORMAT = "총 수익률은 %.2f%%입니다.\n";
+    private static final String WINNING_STATISTICS_FORMAT = "%s개 일치 (%s원) - %d개\n";
+    private static final String WINNING_WITH_BONUS_STATISTIC_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+    private static final String TOTAL_EARNING_RATE_FORMAT = "총 수익률은 %.1f%%입니다.\n";
+    private static final String DELIMITER = ", ";
+    private static final String STRING_JOIN_PREFIX = "[";
+    private static final String STRING_JOIN_SUFFIX = "]";
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
 
     public static void printLottoPurchaseAmountPrompt() {
@@ -36,7 +41,8 @@ public class OutputView {
 
     public static void printLottos(LottosResponse lottosResponse) {
         for (Lotto lotto : lottosResponse.getLottos().asList()) {
-            StringJoiner stringJoiner = new StringJoiner(",", "[", "]");
+            StringJoiner stringJoiner = new StringJoiner(DELIMITER, STRING_JOIN_PREFIX,
+                STRING_JOIN_SUFFIX);
             lotto.getNumbers().forEach(
                 number -> stringJoiner.add(String.valueOf(number))
             );
@@ -49,18 +55,25 @@ public class OutputView {
     }
 
     public static void printError(String errorMessage) {
-        System.out.print(errorMessage + "\n");
+        System.out.print(ERROR_MESSAGE + errorMessage + "\n");
     }
 
     public static void printLottoStatistic(LottoStatisticResponse response) {
         System.out.print(WINNING_STATISTICS_HEADER);
         System.out.print(DIVIDER);
         for (LottoPrize prize : LottoPrize.values()) {
-            if (prize != LottoPrize.LOSE) {
-                int count = response.getWinningCount().getOrDefault(prize, 0);
-                System.out.printf(WINNING_STATISTICS_FORMAT, prize.matchCount(),
-                    formatPrizeMoney(prize.prizeAmount()), count);
+            if (prize == LottoPrize.LOSE) {
+                continue;
             }
+            if (prize == LottoPrize.SECOND) {
+                int count = response.getWinningCount().getOrDefault(prize, 0);
+                System.out.printf(WINNING_WITH_BONUS_STATISTIC_FORMAT, prize.matchCount() + 1,
+                    formatPrizeMoney(
+                        prize.prizeAmount()), count);
+            }
+            int count = response.getWinningCount().getOrDefault(prize, 0);
+            System.out.printf(WINNING_STATISTICS_FORMAT, prize.matchCount(),
+                formatPrizeMoney(prize.prizeAmount()), count);
         }
         System.out.printf(TOTAL_EARNING_RATE_FORMAT, response.getEarningRate());
     }

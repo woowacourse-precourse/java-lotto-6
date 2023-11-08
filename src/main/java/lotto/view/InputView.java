1a5a76bc7904cacbe16067f1fed42fdd69c929f9
jay <wlguswhd0809@naver.com>
2023-11-08 21:12:41 +0900

    feat: 당첨 번호 인풋 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,12 @@
 package lotto.view;
 
 import static lotto.constant.InputViewConstant.START_MESSAGE;
+import static lotto.constant.InputViewConstant.WINNING_NUMBER_MESSAGE;
+import static lotto.util.InputValidation.validateCommaSeparatedInput;
+import static lotto.util.InputValidation.validateDuplicateValues;
 import static lotto.util.InputValidation.validateEmptyInput;
+import static lotto.util.InputValidation.validateInputRange;
+import static lotto.util.InputValidation.validateInputSize;
 import static lotto.util.InputValidation.validateMoneyInput;
 import static lotto.util.InputValidation.validationStringInput;
 
@@ -9,6 +14,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static int inputMoney;
+    public static String winningNumber;
+    public static int bonusNumber;
 
     public static void setStartMessage() {
         System.out.println(START_MESSAGE);
@@ -34,4 +41,31 @@ public class InputView {
 
         return inputMoney;
     }
+
+    public static void setWinningNumberMessage() {
+        System.out.println("\n" + WINNING_NUMBER_MESSAGE);
+    }
+
+    public static String setWinningNumber() {
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                winningNumber = Console.readLine();
+                validateEmptyInput(winningNumber);
+                validateCommaSeparatedInput(winningNumber);
+                validateInputSize(winningNumber);
+                validateInputRange(winningNumber);
+                validateDuplicateValues(winningNumber);
+
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return winningNumber;
+    }
+
+    
 }

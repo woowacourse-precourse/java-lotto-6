15c89be07416d2afb5f76c4f6723a06485ba0c98
kangyuseok <kingyuseok@naver.com>
2023-11-08 13:24:10 +0900

    Feat: 로또 당첨 번호 보너스 번호 입력 기능 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,14 +1,24 @@
 package lotto.controller;
 
 import static lotto.service.Buyer.calculateLottoTickets;
+import static lotto.service.Buyer.getInstance;
 import static lotto.view.IO.checkLottoTickets;
+import static lotto.view.IO.requestBonusNumber;
 import static lotto.view.IO.requestPerchaseAmount;
+import static lotto.view.IO.requestWinningNumbers;
+import static lotto.view.IO.showResults;
 import static lotto.view.constants.Front.CHECK_LOTTO_TICKETS;
+import static lotto.view.constants.Front.REQUEST_BONUS_NUMBER;
 import static lotto.view.constants.Front.REQUEST_PURCHASE_AMOUNT;
+import static lotto.view.constants.Front.REQUEST_WINNING_NUMBERS;
+import static lotto.view.constants.Front.SHOW_THE_WINNING_STATISTICS;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.BonusNumber;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningNumbers;
 import lotto.service.Buyer;
+import lotto.service.Checker;
 
 public class LottoGame {
     public static void lottoGameStart(){
@@ -18,6 +28,12 @@ public class LottoGame {
         int lottoTicketCount = buyer.calculateLottoTickets(purchaseAmount);
         checkLottoTickets(lottoTicketCount,CHECK_LOTTO_TICKETS);
         buyer.buyLottoTickets(lottoTicketCount);
+        requestWinningNumbers(REQUEST_WINNING_NUMBERS);
+        WinningNumbers winningNumbers = inputWinningNumbers();
+        requestBonusNumber(REQUEST_BONUS_NUMBER);
+        BonusNumber bonusNumber = inputBonusNumber(winningNumbers);
+        Checker checker = new Checker(bonusNumber, winningNumbers, buyer.getLottoTickets());
+        checker.checkLottoTickets();
     }
 
     private static PurchaseAmount inputPurchaseAmount(){
@@ -26,6 +42,30 @@ public class LottoGame {
             try{
                 PurchaseAmount purchaseAmount = new PurchaseAmount(money);
                 return purchaseAmount;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static WinningNumbers inputWinningNumbers(){
+        while(true){
+            String numbers = Console.readLine();
+            try{
+                WinningNumbers winningNumbers = new WinningNumbers(numbers);
+                return winningNumbers;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static BonusNumber inputBonusNumber(WinningNumbers winningNumbers){
+        while(true){
+            String number = Console.readLine();
+            try{
+                BonusNumber bonusNumber = new BonusNumber(number, winningNumbers);
+                return bonusNumber;
             }catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }

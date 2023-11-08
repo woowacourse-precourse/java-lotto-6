f84171f9e045d2f370890a9328a110a69743eee8
KongJunWoo <joonoo3@naver.com>
2023-11-08 22:25:35 +0900

    feat : 로또 구매 장 수 구하기
    예외 발생 시 다시 입력받는다

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,11 +1,30 @@
 package lotto.controller;
 
+import lotto.model.PrizeResultDto;
+import lotto.util.*;
+import lotto.view.InputView;
+
 public class Game {
+
+    private int lottoCount; // 구입한 로또 장 수
 
     public Game() {}
 
     public static Game create(){
         return new Game();
+    }
+
+    public void setLottoCount() {
+        String inputStringValue = InputView.inputBuyValue();
+
+        try{
+            this.lottoCount = ValidateInputBuy.validateInputBuy(inputStringValue) / 1000;
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            setLottoCount();
+        }
+
     }
 
 }

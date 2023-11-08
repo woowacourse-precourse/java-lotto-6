8c7b3a8f2c16f04d24cd850552e09d2e539228d4
wooni97 <jhw970714@gmail.com>
2023-11-08 20:19:00 +0900

    feat: LottoResult 클래스 테스트 코드 추가
    - 이 커밋은 LottoResult 클래스의 테스트 코드를 추가합니다.

--- src/test/java/lotto/model/LottoResultTest.java
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -1,39 +1,44 @@
 package lotto.model;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Map;
+
+
+
 class LottoResultTest {
     @Test
-    @DisplayName("")
-    void test() throws Exception {
+    @DisplayName("로또 결과 테스트")
+    void lotto_result_test(){
         //given
-        int bonusNumber = 8;
-        LotteryNumbers lotteryNumbers = new LotteryNumbers(Arrays.asList(1,2,3,4,5,6), bonusNumber);
+        Lotto winningNumber = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        BonusNumber bonusNumber = new BonusNumber(8);
+        LotteryNumbers lotteryNumbers = new LotteryNumbers(winningNumber, bonusNumber);
 
-        PurchasedLottos purchasedLottos = new PurchasedLottos();
+        Lottos purchasedLottos = new Lottos();
         purchasedLottos.add(Arrays.asList(1,2,3,10,11,12));
         purchasedLottos.add(Arrays.asList(1,2,3,4,5,8));
+        purchasedLottos.add(Arrays.asList(1,2,3,4,5,7));
+        purchasedLottos.add(Arrays.asList(1,2,3,11,12,14));
+        purchasedLottos.add(Arrays.asList(1,2,3,4,12,14));
+
 
         //when
         LottoResult lottoResult = new LottoResult(purchasedLottos,lotteryNumbers);
 
         //then
-        Map<WinningStrategy, Integer> result = lottoResult.getNumberMatchResult();
+        Map<WinningCriteria, Integer> result = lottoResult.getNumberMatchResult();
 
         // 예상 결과와 비교
-        assertEquals(1, result.get(WinningStrategy.MATCHES_THREE));
-        assertEquals(1, result.get(WinningStrategy.MATCHES_FIVE_WITH_BONUS));
-        assertNull(result.get(WinningStrategy.MATCHES_SIX));
+        assertEquals(2, result.get(WinningCriteria.MATCHES_THREE));
+        assertEquals(1, result.get(WinningCriteria.MATCHES_FOUR));
+        assertEquals(1, result.get(WinningCriteria.MATCHES_FIVE));
+        assertEquals(1, result.get(WinningCriteria.MATCHES_FIVE_WITH_BONUS));
+        assertNull(result.get(WinningCriteria.MATCHES_SIX));
 
     }
 

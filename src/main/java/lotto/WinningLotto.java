fb75e81016c81160983681785feff0f43bd5a8b5
binary_ho <dfghcvb11@naver.com>
2023-11-08 09:35:19 +0900

    refactor : WinningLotto가 LottoNumbers를 받아 HitCounter를 이용해 Ranking을 반환하는 메서드 구현

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,15 +1,19 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WinningLotto {
 
     private final Lotto winningLotto;
     private final LottoNumber bonusLottoNumber;
+    private final HitCounter hitCounter;
 
     private WinningLotto(Lotto winningLotto, LottoNumber bonusLottoNumber) {
         this.winningLotto = winningLotto;
         this.bonusLottoNumber = bonusLottoNumber;
+        this.hitCounter = createHitCounter(winningLotto, bonusLottoNumber);
     }
 
     public static WinningLotto createWinningLotto(List<Integer> winningNumbers, int bonusNumber) {
@@ -20,9 +24,25 @@ public class WinningLotto {
         return new WinningLotto(winningLotto, bonusLottoNumber);
     }
 
+    public LottoRanking checkRanking(Set<LottoNumber> lottoNumbers) {
+        Set<Integer> numbers = lottoNumbers.stream()
+            .map(LottoNumber::number)
+            .collect(Collectors.toSet());
+        return hitCounter.countHit(numbers);
+    }
+
     private static void validateBonusNumberNotDuplicated(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private HitCounter createHitCounter(Lotto winningLotto, LottoNumber bonusLottoNumber) {
+        Set<Integer> winningNumbers = winningLotto.getLottoNumbers()
+            .stream()
+            .map(LottoNumber::number)
+            .collect(Collectors.toSet());
+
+        return new HitCounter(winningNumbers, bonusLottoNumber.number());
     }
 }

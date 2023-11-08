df724e768c8694d042ee92cd69d6116028474634
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 12:31:19 +0900

    refactor: delete winningLottos and methods
    - winningLottos 멤버변수 및 관련 메소드 삭제

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -13,14 +13,10 @@ import static lotto.constant.LottoConstant.*;
 public class Customer {
     private final Money money;
     private List<Lotto> lottos;
-    private double rateOfReturn;
-    private List<Integer> winningLottos; // [3, 4, 5, 6]
     public Customer(String money) {
         validationMoney(money);
         this.money = new Money(parseInt(money));
         this.lottos = new ArrayList<Lotto>();
-        this.winningLottos = new ArrayList<Integer>();
-        initWinningLottos();
     }
 
     private void validationMoney(String money) {
@@ -33,12 +29,6 @@ public class Customer {
     }
     public int getMoney() {
         return money.getValue();
-    }
-
-    private void initWinningLottos() {
-        for(int i = 0 ; i < 5 ; i++) {
-            winningLottos.add(0);
-        }
     }
 
     public void buyLotto() {
@@ -64,35 +54,5 @@ public class Customer {
 
     public List<Lotto> getLottos() {
         return lottos;
-    }
-
-    public void calcWinningLotto(List<Integer> _winning, int _bonusNumber) {
-        for (Lotto _lotto: lottos) {
-            int same = _lotto.compare(_winning);
-            if (same >= 3) {
-                winningLottos.set(same -3, winningLottos.get(same -3)+1);
-            }
-        }
-    }
-
-    public void calcRateOfReturn() {
-
-        // 수익금 / 구입 금액 * 100
-        rateOfReturn = ((winningLottos.get(0) * THREE_WINNINGS.getNumber())
-                + (winningLottos.get(1) * FOUR_WINNINGS.getNumber())
-                + (winningLottos.get(2) * FIVE_WINNINGS.getNumber())
-                + (winningLottos.get(3) * SIX_WINNINGS.getNumber())
-                + (winningLottos.get(4) * FIVE_BONUS_WINNINGS.getNumber())
-        ) / (double)money.getValue() * 100;
-
-        rateOfReturn = Math.round((rateOfReturn * 10)) / 10.0;
-    }
-
-    public double getRateOrReturn() {
-        return rateOfReturn;
-    }
-
-    public List<Integer> getWinningLottos() {
-        return winningLottos;
     }
 }

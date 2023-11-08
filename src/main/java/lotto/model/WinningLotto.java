5cf6817f8e7524a621f5a3ed300f2541e1d650a6
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 11:51:10 +0900

    feat(WinningLotto): 로또 정답 클래스 생성

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+    
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+}

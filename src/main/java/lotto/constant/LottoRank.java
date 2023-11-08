74eb9bf59f3f845356678f69a10973bd17121126
chanwoo <mdsoo55828@gmail.com>
2023-11-08 19:50:23 +0900

    feat: LottoRank 추가

---
+++ src/main/java/lotto/constant/LottoRank.java
@@ -0,0 +1,57 @@
+package lotto.constant;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+
+public enum LottoRank {
+
+    NOTHING(0, false, 0),
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
+
+    private final int match;
+    private final boolean bonus;
+    private final int prize;
+
+    LottoRank(int match, boolean bonus, int prize) {
+        this.match = match;
+        this.bonus = bonus;
+        this.prize = prize;
+    }
+
+    public static LottoRank of(int match, boolean bonus) {
+        if (match == SECOND.match && bonus == SECOND.bonus) {
+            return SECOND;
+        }
+        if (match < FIFTH.match) {
+            return NOTHING;
+        }
+        return Arrays.stream(values())
+                .filter(elem -> elem.match == match && !elem.bonus)
+                .findFirst()
+                .orElse(NOTHING);
+    }
+
+    public static LottoRank of(int match) {
+        return of(match, false);
+    }
+
+    public String message() {
+        String bonusMessage = "";
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+
+        if (this.bonus) {
+            bonusMessage = ", 보너스 볼 일치";
+        }
+
+        return String.format("%d개 일치%s (%s원) - ", match, bonusMessage, numberFormat.format(prize));
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+}

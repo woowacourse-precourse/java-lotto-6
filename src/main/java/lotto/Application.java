c81d7f98d8f526d0f1fd26de16f86d218db85289
SangCheol <scpark0698@gmail.com>
2023-11-08 21:24:59 +0900

    feat: 구입 금액에 따라 랜덤 로또 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Application {
+
+    public static final int LOTTO_SIZE = 6;
+
     public static void main(String[] args) {
         int buyAmount = inputBuyAmount();
         List<Lotto> lottos = generateRandomLottoTickets(buyAmount);
@@ -28,7 +34,24 @@ public class Application {
     }
 
     private static List<Lotto> generateRandomLottoTickets(int buyAmount) {
-        return null;
+        int buyLottoNumber = buyAmount / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < buyLottoNumber; i++) {
+            lottos.add(generateRandomLottoTicket());
+        }
+        return lottos;
+    }
+
+    private static Lotto generateRandomLottoTicket() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < LOTTO_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        numbers.sort(Comparator.naturalOrder());
+        return new Lotto(numbers);
     }
 
     private static int inputBuyAmount() {

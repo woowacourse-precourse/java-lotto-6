eecc4a4085f17fcea6be77b2215a3aa6c7da198c
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-08 18:37:36 +0900

    feat: Application의 로또 게임 동작 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+
+import lotto.controller.GameController;
+import lotto.utils.RandomNumbersGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.play();
     }
 }

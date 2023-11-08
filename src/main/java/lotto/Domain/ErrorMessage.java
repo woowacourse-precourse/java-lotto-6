709f2853f92e2d91de414b49f9d81634c9079fb0
7ngenious <zxvs1218@gmail.com>
2023-11-08 20:57:45 +0900

    feat: Add ErrorMessage

---
+++ src/main/java/lotto/Domain/ErrorMessage.java
@@ -0,0 +1,22 @@
+package lotto.Domain;
+
+public class ErrorMessage {
+    //InputView
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String START_GAME_MSG = "구입금액을 입력해 주세요. ";
+    public static final String WINNING_NUMBERS_MSG = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요: ";
+    public static final String INPUT_ERROR_MSG_BASIC = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INPUT_ERROR_MSG_BONUS = "보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INPUT_ERROR_MSG_FORMAT = "올바른 숫자를 입력해 주세요.";
+    public static final String ERROR_MSG_LENGTH = "로또 번호는 6개여야 합니다.";
+    public static final String ERROR_MSG_DUPLICATED = "로또 번호에 중복된 숫자가 있습니다.";
+
+    //OutputView
+    public static final String PURCHASED_MSG_PREFIX = "원으로 ";
+    public static final String PURCHASED_MSG_SUFFIX = "개를 구매했습니다.";
+
+    public static void displayError(String message) {
+        System.out.println(ERROR_PREFIX + message);
+    }
+}

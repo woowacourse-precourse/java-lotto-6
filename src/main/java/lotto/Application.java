c35d218a1daa3a659270dcc919c3fcdbf661b408
pumpk <cong2738@gmail.com>
2023-11-08 13:22:46 +0900

    refactor: 쓸모없는 부분 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         UserInput input = new UserInput();
-        int money = -1, bonus;
+        int money, bonus;
         UserLotto user;
         Lotto winLotto;
         Lottery LT;

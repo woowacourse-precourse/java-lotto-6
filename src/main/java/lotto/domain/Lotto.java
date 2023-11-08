a6af3e9e0df606754086053f2bcc1bff0f354171
miro-22 <rlawlgus96@snu.ac.kr>
2023-11-08 23:20:26 +0900

    fix: restore lotto class

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,14 +3,22 @@ package lotto.domain;
 import java.util.List;
 import lotto.validation.LottoValidator;
 
-public record Lotto(List<Integer> numbers) {
-    public Lotto {
+public class Lotto {
+    List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
         validate(numbers);
+
+        this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         LottoValidator validator = new LottoValidator();
 
         validator.validate(numbers);
+    }
+
+    public List<Integer> numbers() {
+        return numbers;
     }
 }

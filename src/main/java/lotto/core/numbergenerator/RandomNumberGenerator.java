533da1000fb45d15f7b5310ca51c7d818f83fdc4
Yong Ta Park <qkrdydxk80@nate.com>
2023-11-08 20:22:46 +0900

    add : RandomNumberGenerator
    랜덤번호를 정렬하는 기능 추가

--- src/main/java/lotto/core/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/lotto/core/numbergenerator/RandomNumberGenerator.java
@@ -5,10 +5,13 @@ import static lotto.core.enums.LottoNumberEnum.SIZE;
 import static lotto.core.enums.LottoNumberEnum.START_VALUE;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
 
 public class RandomNumberGenerator {
     public List<Integer> createRandomUniqueNumber() {
-        return Randoms.pickUniqueNumbersInRange(START_VALUE.getValue(), END_VALUE.getValue(), SIZE.getValue());
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_VALUE.getValue(), END_VALUE.getValue(),
+                SIZE.getValue());
+        return numbers.stream().sorted(Comparator.naturalOrder()).toList();
     }
 }

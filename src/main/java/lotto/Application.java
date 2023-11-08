b449c74b5270a1395f5a901df75d6c549574982f
jy3026 <jy_3026@naver.com>
2023-11-08 22:36:38 +0900

    feat : 구매금액 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = getPurchaseAmount();
+    }
+
+    private static int getPurchaseAmount() {
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String input = Console.readLine();
+
+            try {
+                return parsePurchaseAmount(input);
+            } catch (NumberFormatException e) {
+                System.err.println("[ERROR] 숫자를 입력해 주세요. 다시 시도하세요.");
+            }
+        }
+    }
+
+    private static int parsePurchaseAmount(String input) {
+        int purchaseAmount = Integer.parseInt(input);
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("1,000 단위로 입력 해주세요");
+        }
+        return purchaseAmount / 1000;
     }
 }

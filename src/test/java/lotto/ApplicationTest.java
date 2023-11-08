5d84c602f2498b206044994538adbacd86db61d6
hmlssghm <kwonse326@gmail.com>
2023-11-08 18:12:55 +0900

    test: 도메인 테스트 구현

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,13 +1,19 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.controller.Comparison;
+import lotto.controller.Counting;
+import lotto.model.Cash;
+import lotto.model.Lotto;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -53,6 +59,118 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
+    @Test
+    void testCompareLotto() {
+        Comparison comparison = new Comparison();
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // Test case 1: No matching numbers
+        List<Integer> randomNumbers1 = Arrays.asList(7, 8, 9, 10, 11, 12);
+        int result1 = comparison.compareLotto(lottoNumbers, randomNumbers1);
+        assertEquals(0, result1);
+
+        // Test case 2: Some matching numbers
+        List<Integer> randomNumbers2 = Arrays.asList(3, 5, 7, 8, 9, 10);
+        int result2 = comparison.compareLotto(lottoNumbers, randomNumbers2);
+        assertEquals(2, result2);
+
+        // Test case 3: All numbers matching
+        List<Integer> randomNumbers3 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int result3 = comparison.compareLotto(lottoNumbers, randomNumbers3);
+        assertEquals(6, result3);
+    }
+
+    @Test
+    void testCompareBonus() {
+        Comparison comparison = new Comparison();
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Test case 1: Bonus number not in the list
+        int bonusNum1 = 10;
+        assertFalse(comparison.compareBonus(numbers, bonusNum1));
+
+        // Test case 2: Bonus number in the list
+        int bonusNum2 = 3;
+        assertTrue(comparison.compareBonus(numbers, bonusNum2));
+    }
+
+    @Test
+    void testGetResult() {
+        Counting counting = new Counting();
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> randomNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // 6 numbers match
+        int bonusNumber = 7;
+        int result1 = counting.getResult(lottoNumbers, bonusNumber, randomNumbers);
+        assertEquals(6, result1);
+
+        // 3 numbers match
+        List<Integer> randomNumbers3 = Arrays.asList(1, 2, 3, 7, 8, 9);
+        int bonusNumber3 = 10;
+        int result3 = counting.getResult(lottoNumbers, bonusNumber3, randomNumbers3);
+        assertEquals(3, result3);
+    }
+
+
+
+    @Test
+    void testSizeCheck() {
+        // Test case 1: Valid size (6 numbers)
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto1 = new Lotto(numbers1); // No exception should be thrown
+
+        // Test case 2: Invalid size (5 numbers)
+        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5);
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers2));
+
+        // Test case 3: Invalid size (7 numbers)
+        List<Integer> numbers3 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers3));
+    }
+
+    @Test
+    void testRangeCheck() {
+        // Test case 1: All numbers are within the valid range
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto1 = new Lotto(numbers1); // No exception should be thrown
+
+        // Test case 2: Some numbers are out of range
+        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 46);
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers2));
+    }
+
+    @Test
+    void testDuplicationCheck() {
+        // Test case 1: No duplicate numbers
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto1 = new Lotto(numbers1); // No exception should be thrown
+
+        // Test case 2: Duplicate numbers
+        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 5);
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(numbers2));
+    }
+
+    @Test
+    void testCashCheckValid() {
+        // Test case 1: Valid cash amount (1000)
+        Cash cash1 = new Cash(1000); // No exception should be thrown
+
+        // Test case 2: Valid cash amount (multiple of 1000)
+        Cash cash2 = new Cash(3000); // No exception should be thrown
+    }
+
+    @Test
+    void testCashCheckInvalid() {
+        // Test case 1: Invalid cash amount (less than 1000)
+        assertThrows(IllegalArgumentException.class, () -> new Cash(999));
+
+        // Test case 2: Invalid cash amount (not multiple of 1000)
+        assertThrows(IllegalArgumentException.class, () -> new Cash(1500));
+    }
+
+
 
     @Override
     public void runMain() {

8bbf586c875b357d1c43be9f52c674f4d8bb4724
cheolhyeon <tghy4@naver.com>
2023-11-08 16:32:48 +0900

    feat : 로또 게임의 상수를 관리할 클래스 생성 및 구현

---
+++ src/main/java/constants/LottoGameConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public abstract class LottoGameConstants {
+    public static final int MAX_LOTTO_NUMBER_RANGE = 45;
+    public static final int MIN_LOTTO_NUMBER_RANGE = 1;
+    public static final int MAX_LOTTO_NUMBER_COUNT = 6;
+    public static final int DIVISION_NUMBER= 1000;
+}

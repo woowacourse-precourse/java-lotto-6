22c0d2f002abded1bfdec0375178964c2ccaccfd
깃wooqqq <sge0225@naver.com>
2023-11-08 16:49:47 +0900

    refactor: 정적 메서드 사용하여 당첨 번호 예외처리 메서드 WinningNumber 클래스로 이동

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,14 +1,49 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class WinningNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+    private static final Pattern PATTERN = Pattern.compile("\\d+");
     private final Lotto winningNumber;
     private final Integer bonusNumber;
 
     public WinningNumber(List<Integer> winningNumber, Integer bonusNumber) {
+        validateWinningNumbers(winningNumber);
         this.winningNumber = new Lotto(winningNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    public static void validateWinningNumbers(List<Integer> numbers) {
+        validateDuplicate(numbers);
+        for (int number : numbers) {
+            validateRangeNumber(number);
+            validateNumeric(String.valueOf(number));
+        }
+    }
+
+    private static void validateNumeric(String input) {
+        if (!PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateRangeNumber(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getBonusNumber() {
--- src/main/java/lotto/view/InputWinningNumberView.java
+++ src/main/java/lotto/view/InputWinningNumberView.java
@@ -1,17 +1,16 @@
 package lotto.view;
 
-import java.util.*;
-import java.util.regex.Pattern;
+import lotto.domain.WinningNumber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class InputWinningNumberView extends InputView {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final int LOTTO_SIZE = 6;
-    private static final String BLANK = " ";
     private static final String SEPARATOR = ",";
     private static final String ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 중복되지 않은 숫자여야 합니다.";
     private static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    private static final Pattern PATTERN = Pattern.compile("\\d+");
 
     public List<Integer> getWinningNumber() {
         System.out.println(REQUEST_WINNING_NUMBER);
@@ -20,8 +19,7 @@ public class InputWinningNumberView extends InputView {
         List<String> inputValue = parseWinningNumber(input);
         List<Integer> winningNumber = convertWinningNumber(inputValue);
         try {
-            validateBlank(input);
-            validateWinningNumber(winningNumber);
+            WinningNumber.validateWinningNumbers(winningNumber);
         } catch (IllegalArgumentException e) {
             System.out.println(ERROR_MESSAGE);
             return getWinningNumber();
@@ -36,42 +34,9 @@ public class InputWinningNumberView extends InputView {
     private List<Integer> convertWinningNumber(List<String> inputValue) {
         List<Integer> numbers = new ArrayList<>();
         for (String input : inputValue) {
-            validateNumeric(input);
             int number = Integer.parseInt(input);
             numbers.add(number);
         }
         return numbers;
-    }
-
-    private void validateWinningNumber(List<Integer> numbers) {
-        validateDuplicate(numbers);
-        for (int number : numbers) {
-            validateRangeNumber(number);
-        }
-    }
-
-    private void validateNumeric(String input) {
-        if (!PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateRangeNumber(int number) {
-        if (number < MIN_NUMBER || number > MAX_NUMBER) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateBlank(String input) {
-        if (input.contains(BLANK)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> uniqueNumber = new HashSet<>(numbers);
-        if (uniqueNumber.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
-        }
     }
 }

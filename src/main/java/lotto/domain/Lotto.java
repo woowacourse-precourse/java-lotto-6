fc340bfc50b28fc53ede37f35884d8932e8a557a
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-08 20:31:06 +0900

    refactor: 검증과정을 Lotto로 옮김

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,17 +1,46 @@
 package lotto.domain;
 
+import static lotto.Option.ErrorMessage.NOT_LOTTO_DUPLICATION;
+import static lotto.Option.ErrorMessage.NOT_SIX_LENGTH;
+import static lotto.Option.ErrorMessage.ONE_TO_FORTY_FIVE;
+
 import java.util.List;
-import lotto.validator.LottoValidator;
+import lotto.Option.GameOption;
 
-public class Lotto {
-    private final List<Integer> lotto;
-
-    public Lotto(List<Integer> lotto) {
-        LottoValidator.check(lotto);
-        this.lotto = lotto;
+public record Lotto(List<Integer> numbers) {
+    public Lotto {
+        validate(numbers);
     }
 
-    public List<Integer> getLotto() {
-        return lotto;
+    private void validate(List<Integer> numbers) {
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(NOT_SIX_LENGTH.getErrorMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < GameOption.LOTTO_MIN_NUMBER.getNumber() || number > GameOption.LOTTO_MAX_NUMBER.getNumber()) {
+                throw new IllegalArgumentException(ONE_TO_FORTY_FIVE.getErrorMessage());
+            }
+        });
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        if (isDuplication(numbers)) {
+            throw new IllegalArgumentException(NOT_LOTTO_DUPLICATION.getErrorMessage());
+        }
+    }
+
+    private boolean isDuplication(List<Integer> validator) {
+        return validator.stream()
+                .distinct()
+                .count() != validator.size();
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,24 +1,49 @@
 package lotto.domain;
 
+import static lotto.Option.ErrorMessage.NOT_LOTTO_DUPLICATION;
+import static lotto.Option.ErrorMessage.NOT_SIX_LENGTH;
+import static lotto.Option.ErrorMessage.ONE_TO_FORTY_FIVE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("List의 길이가 6이 아니면 예외가 발생한다.")
     @Test
-    void createLottoByOverSize() {
+    void NotSixNumberLengthThrowException() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_SIX_LENGTH.getErrorMessage());
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("범위를 벗어난 수를 입력하면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidParameters")
+    void overRangeThrowException(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ONE_TO_FORTY_FIVE.getErrorMessage());
+    }
+
+    static Stream<Arguments> invalidParameters() {
+        return Stream.of(
+                Arguments.of(List.of(0, 1, 2, 3, 4, 5)),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 46))
+        );
+    }
+
+    @DisplayName("로또 번호가 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void createLottoByDuplicatedNumber() {
+    void ifDuplicationThrowException() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_LOTTO_DUPLICATION.getErrorMessage());
     }
 
 }

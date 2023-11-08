d4268b6092648bdfbb2a593f5f6e7ed2b88cb753
onzero98 <dev_210@naver.com>
2023-11-08 22:15:47 +0900

    + Add : 로또 validate 기능 업데이트 예외 처리 부분 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +15,17 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+
+        for(int number : numbers){
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 번호는 1 이상 45 이하의 숫자여야 합니다.");
+            }
         }
     }
 

2fbc47eae03f80922dbe513e1102b48ba49a669e
Dongjun Shin <tlsehdwns239@gmail.com>
2023-11-08 14:27:51 +0900

    feat(Lotto): 로또가 6개로 구성되어 유효한 지 확인하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,8 +6,17 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
+        this.validateNumbers(numbers);
+    private void validateNumbers(List<Integer> numbers) {
+        this.validateNumberSize(numbers);
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != LottoConstants.LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 입력할 수 있습니다.");
+        }
+    }
     }
 
     private void validate(List<Integer> numbers) {

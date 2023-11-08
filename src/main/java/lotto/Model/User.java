8d14de025a66a9f76759a37aee76b138487780e5
park_yeongyu <sjjs7771@naver.com>
2023-11-08 20:26:22 +0900

    feat(User) : 보너스번호를 가지고 있는 지 확인하고 가져올 수 있는 함수 작성

--- src/main/java/lotto/Model/User.java
+++ src/main/java/lotto/Model/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class User {
     private List<Integer> userLotto; //사용자의 로또 번호가 담길 리스트
+    boolean hasBonus = false;
 
     public void setUserLotto(List<Integer> LottoNumbers) {
         this.userLotto = LottoNumbers;
@@ -12,6 +13,16 @@ public class User {
 
     public List<Integer> getUserLotto() {
         return userLotto;
+    }
+
+    public void setHasBonus(int number) {
+        if (userLotto.contains(number)) {
+            hasBonus = true;
+        }
+    }
+
+    public boolean getHasBonus() {
+        return hasBonus;
     }
 
 }

f79dad1e53fd6ff6b4bdc47de86d2debbc2ef152
songminhyuk <dnfldpden32@gmail.com>
2023-11-08 18:58:50 +0900

    feat: Lotto validate 함수 내부 추가구현
    로또번호 중복 판단 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,6 +14,17 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+        //로또 번호의 범위 판단
+        for (int number:numbers){
+            if(number<1 ||number>45){
+                throw new IllegalArgumentException();
+            }
+        }
+        //로또 번호 중복 판단
+        Set<Integer> uniquenumbers=new HashSet<>(numbers);
+        if(uniquenumbers.size()!= numbers.size()){
             throw new IllegalArgumentException();
         }
     }

dc359a993d86e00be174ba31615bd52ba4f82b4e
yooonwodyd <dbswodyd00@naver.com>
2023-11-08 17:09:41 +0900

    유저 테스트 작성, winningnumbers는 아직 만들지 않았음으로 주석처리

---
+++ src/main/java/lotto/model/LottoType.java
@@ -0,0 +1,2 @@
+package lotto.model;public enum RottoType {
+}
---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import lotto.model.LottoType;
+import lotto.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTest {
+    Lotto lotto1;
+    Lotto lotto2;
+    Lotto lotto3;
+    List<Lotto> lottoList;
+
+    @BeforeEach
+    void setUp() {
+        lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lottoList = List.of(lotto1, lotto2, lotto3);
+    }
+
+    @DisplayName("유저는 로또를 살 때 생성된다.")
+    @Test
+    void givenLottoList_whenCreate_thenSuccess() {
+
+        //When
+        User user = User.of(lottoList);
+        //Then
+        assertThat(user).isNotNull();
+    }
+
+
+//    @DisplayName("가지고 있는 로또들의 당첨여부를 확인한다.")
+//    @Test
+//    void givenLottoswhentoLottoTypesThenSuccess() {
+//        //Given
+//        User user = User.of(lottoList);
+//        WinningNumbers winningNumbers = WinningNumbers.of(List.of(1, 2, 3, 4, 5, 6), 7);
+//        //When
+//        List<LottoType> lottoTypes = lottos.checkLottoTypes(winningNumbers);
+//        //Then
+//        assertThat(lottoTypes).isNotNull();
+//    }
+}

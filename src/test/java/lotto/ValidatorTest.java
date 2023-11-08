6ef43feb1e1864e40e705190718930eab1ffeba5
GO-TE <alsrudxo9810@naver.com>
2023-11-08 21:58:53 +0900

    test(ValidatorTest): 검증 클래스 기능 테스트 - 당첨 번호 중복을 테스트 한다 - 보너스 번호의 중복을 테스트 한다 - 로또 구입 금액에 대한 유효성 테스트를 한다 - 당첨번호 입력에 대한 테스트를 한다

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -8,28 +8,35 @@ import lotto.util.Validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ValidateTest {
-//
-//    @DisplayName("당첨 번호가 중복된 숫자가 있으면 예외가 발생한다.")
-//    @Test
-//    void createWinningNumberByDuplicateNumber() {
-//        assertThatThrownBy(() -> new Validator().validateDuplication(new ArrayList<>(Arrays
-//                .asList("1", "2", "3", "4", "4", "5"))))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
+public class ValidatorTest {
+
+    @DisplayName("당첨 번호가 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByDuplicateNumber() {
+        assertThatThrownBy(() -> new Validator().validateDuplication(new ArrayList<>(Arrays
+                .asList("1", "2", "3", "4", "4", "5"))))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("보너스 번호가 당첨번호에 이미 존재하는 값이면 예외가 발생한다.")
     @Test
     void createBonusNumberByDuplicateWinningNumber() {
         assertThatThrownBy(() -> new Validator().validateBonusNumber("1", new ArrayList<>(Arrays
-                .asList(1,2,3,4,5))))
+                .asList(1, 2, 3, 4, 5))))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 금액이 1000으로 나누어 떨어지지 않는다면 예외가 발생한다.")
     @Test
-    void createBudgetByNotDivideKillo() {
+    void createBudgetByNotDivideOneK() {
         assertThatThrownBy(() -> new Validator().validateBudget("1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력 값에 쉼표가 포함되지 않으면 예외가 발생한다.")
+    @Test
+    void creatInputByNonComma() {
+        assertThatThrownBy(() -> new Validator().isContainComma("1 2 3 4 5 6"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

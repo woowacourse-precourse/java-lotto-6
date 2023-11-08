8a63b686ea49b3694840de6cc135cb85ea5c0a63
K-KY <kyuyoungk@naver.com>
2023-11-08 17:10:20 +0900

    Refactor(ValidateUserNumber) : 정규식 상수화

--- src/main/java/lotto/domain/validator/ValidateNumber.java
+++ src/main/java/lotto/domain/validator/ValidateNumber.java
@@ -1,8 +1,11 @@
 package lotto.domain.validator;
 
 public class ValidateNumber {
+
+    private static final String DIGIT = "\\d+";
+
     public static void isNumber(String number) {
-        if (number.matches("\\d+")) {
+        if (number.matches(DIGIT)) {
             return;
         }
         throw new IllegalArgumentException("[ERROR] 숫자만 입력 해주세요.");

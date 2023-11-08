2de0fe45206d7412b9215ca57eaf63195e703e85
yujeonghyeon <dbdb1114@naver.com>
2023-11-08 21:14:12 +0900

    Lotto 클래스 내부 validator 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.List;
+import util.CustomIllegalArgumentException;
+import util.InputException;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,9 +12,35 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public void validate(List<Integer> numbers) {
+        lengthVlalidator();
+        numberRangeValidator();
+    }
+
+    private void lengthVlalidator() {
+        InputException numberLengthException = InputException.NUMBERS_LENGTH_EXCEPTION;
+        try {
+            if (numbers.size() != GameProperty.LOTTO_NUMBERS_LENGTH) {
+                throw new CustomIllegalArgumentException(numberLengthException);
+            }
+        } catch (IllegalArgumentException exception) {
+            System.out.println(numberLengthException.getMessage());
+        }
+    }
+
+    private void numberRangeValidator() {
+        for (int i = 0; i < numbers.size(); i++) {
+            try {
+                isInRange(numbers.get(i));
+            } catch (IllegalArgumentException exception) {
+                System.out.println(InputException.RANGE_OF_NUMBER_EXCEPTION.getMessage());
+            }
+        }
+    }
+
+    private void isInRange(int number) {
+        if (number < GameProperty.LOTTO_MIN_NUMBER || number > GameProperty.LOTTO_MAX_NUMBER) {
+            throw new CustomIllegalArgumentException(InputException.RANGE_OF_NUMBER_EXCEPTION);
         }
     }
 

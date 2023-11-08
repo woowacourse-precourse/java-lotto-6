81de59609e531a2ed7b77a2feb1243ece9e4458d
lee jihyeon <lejinhy@gmail.com>
2023-11-08 16:06:57 +0900

    refactor: 콘솔창 공백 관련 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,23 +7,19 @@ public class InputView {
     public String requestPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchasePrice = Console.readLine();
-        System.out.println();
-
         return purchasePrice;
     }
 
     public String requestWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         String winningNumbers = Console.readLine();
-        System.out.println();
 
         return winningNumbers;
     }
 
     public String requestBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         String bonusNumbers = Console.readLine();
-        System.out.println();
 
         return bonusNumbers;
     }

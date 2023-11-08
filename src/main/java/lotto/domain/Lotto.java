b5320f8eb1854548df7b790b510e6cc2bb1237ee
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 16:48:57 +0900

    Feat[domain] 로또 객체 검증 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.validation.LottoValidation;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,7 +13,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        LottoValidation.validateRange(numbers);
+        LottoValidation.validateDuplicates(numbers);
+        LottoValidation.validateLength(numbers);
     }
-
-    // TODO: 추가 기능 구현
 }

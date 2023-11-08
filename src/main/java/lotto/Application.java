dcc0a1eac7b0d9cdd7520ed8eeddf1283d10ea22
PeterChoi <94peterchoi@gmail.com>
2023-11-08 17:30:43 +0900

    fix: 프로그램에서 예외 발생 시 에러메세지가 출력되도록 Application에 try catch문 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,12 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.run();
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }

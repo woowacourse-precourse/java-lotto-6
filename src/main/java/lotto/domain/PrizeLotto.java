9606e145fce27740ad0291195030c325812ba6fb
Mi <mi_aa_@naver.com>
2023-11-09 00:46:55 +0900

    feat: 당첨 로또 domain 구현

--- src/main/java/lotto/domain/PrizeLotto.java
+++ src/main/java/lotto/domain/PrizeLotto.java
@@ -5,39 +5,23 @@ import java.util.List;
 import java.util.Set;
 
 public class PrizeLotto {
-    private final List<Integer> numbers;
+    private Lotto primaryNumbers;
+    private int bonusNumber;
 
-    public PrizeLotto(List<Integer> numbers) {
-        validate(numbers);
-        duplicate(numbers);
-        containRange(numbers);
-        this.numbers = numbers;
+
+    public Lotto getPrimaryNumbers() {
+        return primaryNumbers;
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-    private void duplicate(List<Integer> numbers){
-        Set<Integer> numberSet = new HashSet<>();
-        for(int n : numbers){
-            if(numberSet.contains(n)){
-                throw new IllegalArgumentException();
-            }
-            numberSet.add(n);
-        }
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 
-    private void containRange(List<Integer> numbers){
-        for(int n : numbers){
-            if(n < 1 || n > 45){
-                throw new IllegalArgumentException();
-            }
-        }
+    public void setPrimaryNumbers(Lotto primaryNumbers) {
+        this.primaryNumbers = primaryNumbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 7) {
-            throw new IllegalArgumentException();
-        }
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
     }
 }

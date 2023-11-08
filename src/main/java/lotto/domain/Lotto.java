e3bdd3526f62cf1033c20e79c0203e499418dbe2
1015s <1015_s@naver.com>
2023-11-08 03:00:50 -0800

    feat(Lotto) : 로또 유효성 검증 기능 추가 - 중복, 숫자 범위

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,47 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
+
+    private void validate(List<Integer> numbers){
+        isListLengthSix(numbers);
+        isDuplicated(numbers);
+        isNumbersInRange(numbers);
+    }
+
+    private void isListLengthSix(List<Integer> numbers){
         if (numbers.size() != 6) {
+           throw new IllegalArgumentException();
+        }
+    }
+
+    private void isDuplicated(List<Integer> numbers){
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if(uniqueNumbers.size() != 6){
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void isNumbersInRange(List<Integer> numbers){
+        for (int number : numbers){
+            if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }

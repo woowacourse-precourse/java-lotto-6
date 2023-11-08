b7de5b0647b88e007c4c9f9cfb8a63aa6bec919c
hoyeon <highoption@naver.com>
2023-11-08 14:04:58 +0900

    test(WinningLottoValidatorTest) : 검증 테스트 작성
    - 당첨 번호에 숫자가 아닌 값이 들어간다면 예외 발생

--- src/test/java/lotto/model/WinningLottoValidatorTest.java
+++ src/test/java/lotto/model/WinningLottoValidatorTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,4 +17,13 @@ class WinningLottoValidatorTest {
                 .doesNotThrowAnyException();
     }
 
+    @Test
+    @DisplayName("당첨 번호에 숫자가 아닌 값이 들어가면 예외 발생")
+    void testWinningLottoWithNonInteger() {
+        String invalidWinningLotto = "ho,1,2,3,4,5";
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> WinningLottoValidator.validateWinningLotto(invalidWinningLotto))
+                .withMessage("[ERROR] 로또 넘버는 숫자여야 합니다.");
+    }
 }

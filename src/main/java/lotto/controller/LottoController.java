e382c13a76f44688015b5d24e3c036928781a764
dabin <db021129@naver.com>
2023-11-08 15:59:01 +0900

    feat : create makeLottos

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,14 +2,32 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.Exception.LottoSetPriceException;
+import lotto.domain.Lotto;
+import lotto.service.LottoService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
     private final LottoSetPriceException lottoSetPriceException = new LottoSetPriceException();
+    private final LottoService lottoService = new LottoService();
 
     public int LottoSetPrice() {
         String readPrice = Console.readLine();
         int lottoPrice = lottoSetPriceException.checkPriceException(readPrice);
         return lottoPrice;
+    }
+
+    public List<Lotto> makeLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumber = lottoService.makeRandomNumber();
+            Lotto lotto = new Lotto(lottoNumber);
+
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }

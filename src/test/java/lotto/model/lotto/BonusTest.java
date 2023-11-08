7692ab1eeaa887dfe168eae0dcf16822eb687ec5
siyeonSon <siyeon0209@naver.com>
2023-11-08 22:12:17 +0900

    test(Bonus): 보너스 번호가 1부터 45 사이의 숫자이다

--- src/test/java/lotto/model/lotto/BonusTest.java
+++ src/test/java/lotto/model/lotto/BonusTest.java
@@ -1,12 +1,18 @@
 package lotto.model.lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
 
 class BonusTest {
     @Test
-    void 보너스_번호가_1부터_45사이_숫자가_아닐_경우_예외를_발생시킨다() {
+    void 보너스_번호가_1부터_45_사이_숫자이다() {
+        assertDoesNotThrow(() -> new Bonus(1));
+    }
+
+    @Test
+    void 보너스_번호가_1부터_45_사이_숫자가_아닐_경우_예외를_발생시킨다() {
         assertThatThrownBy(() -> new Bonus(46))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");

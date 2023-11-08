266b4a5b86e2912718ba2bb13aa4218cde0d6425
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-08 18:07:47 +0900

    test : 6자리 랜덤넘버 생성 테스트

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import static lotto.domain.LottoNumbers.setRandomLottoNumbers;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class LottoNumbersTest {
-  
+
+    @Test
+    void test_6자리_랜덤넘버가_만들어져야한다() {
+        //LottoNumbers lottoNumbers = new LottoNumbers();
+        List<Integer> result = setRandomLottoNumbers();
+        Assertions.assertThat(result.size()).isEqualTo(6);
+    }
 }

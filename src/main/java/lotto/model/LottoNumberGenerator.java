436888cc3cba88bd93fa20a8d9d21f392511fd8b
Terry <t3rryahn.dev@gmail.com>
2023-11-08 19:22:51 +0900

    fix: Application 테스트 시 Collection 오류 해결

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -3,16 +3,15 @@ package lotto.model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class LottoNumberGenerator {
 
     public List<Integer> generate() {
-        List<Integer> lottoNumbers;
-
-        lottoNumbers = pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(lottoNumbers);
+        List<Integer> lottoNumbers = pickUniqueNumbersInRange(1, 45, 6);
+        lottoNumbers.stream().sorted().collect(Collectors.toList());
 
         return lottoNumbers;
     }

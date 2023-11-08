7b2e3f8cc60839612dc9911365533e772a6378f2
peeaaccee <wlgp8958@naver.com>
2023-11-08 21:08:37 +0900

    test(WinningNumbers): 당첨번호 입력 시 쉼표가 연속해서 있으면 예외가 발생하는 테스트

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class WinningNumbersTest {
+
+    @ParameterizedTest
+    @DisplayName("당첨번호 입력 시 쉼표가 연속해서 있으면 예외가 발생한다.")
+    @CsvSource({
+            "1,,2,3,4,5,6,7",
+            "1,2,3,4,,5,6,7",
+            "1,2,3,4,5,6,,7"
+    })
+    void testConsecutiveCommasInInput(String input) {
+        assertThrows(IllegalArgumentException.class, () -> new WinningNumbers(Collections.singletonList(input)));
+    }
+}

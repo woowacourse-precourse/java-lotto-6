1d44d04c3353e4b14381d3909a1fc331e44b2ce5
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 15:56:17 +0900

    [FEAT] LottoController에 getLottoCost 함수 구현
    .

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,15 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.domain.LottoCost;
+import lotto.view.InputView;
+
+public class LottoController {
+    public void run() {
+        LottoCost lottoCost = getLottoCost();
+        int lottoCount = lottoCost.getLottoCount();
+    }
+
+    private LottoCost getLottoCost() {
+        return new LottoCost(InputView.totalCost());
+    }
 }

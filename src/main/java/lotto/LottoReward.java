9ee8e6d07e6d5d5d2dea13971e91cc64870389c3
realisshomyang <realisshoman@kakao.com>
2023-11-08 21:10:09 +0900

    feat : 1등 복권과 같은 것을 비교하여 Enum값을 return해주는 기능 구현

--- src/main/java/lotto/LottoReward.java
+++ src/main/java/lotto/LottoReward.java
@@ -4,20 +4,59 @@ package lotto;
 import java.util.Arrays;
 
 public enum LottoReward {
-    FIRST_PRIZE(6, 2_000_000_000L),
-    SECOND_PRIZE(5, 30_000_000L),
-    THIRD_PRIZE(5, 1_500_000L),
-    FOURTH_PRIZE(4, 50_000L),
-    FIFTH_PRIZE(3, 5_000L),
-    NOTHING(0, 0);
+    FIRST_PLACE(6, 2_000_000_000L, "6개 일치 (2,000,000,000원) - "),
+    SECOND_PLACE(5, 30_000_000L, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD_PLACE(5, 1_500_000L, "5개 일치 (1,500,000원) - "),
+    FOURTH_PLACE(4, 50_000L, "4개 일치 (50,000원) - "),
+    FIFTH_PLACE(3, 5_000L, "3개 일치 (5,000원) - "),
+    NOTHING(0, 0, "\n");
 
     private final int matchNumbersCount;
     private final long winningReward;
+    private final String message;
     private static final int WINNING_MIN_COUNT = 3;
 
-    LottoReward(int matchNumbersCount, long winningReward) {
+    LottoReward(int matchNumbersCount, long winningReward, String message) {
         this.matchNumbersCount = matchNumbersCount;
         this.winningReward = winningReward;
+        this.message = message;
     }
 
+    public static LottoReward getReward(int matchedCount, boolean isBonusNumberMatched) {
+        LottoReward lottoReward = getLottoReward(matchedCount);
+        return checkIsSecondPlace(lottoReward, isBonusNumberMatched);
+    }
+
+    private static LottoReward checkIsSecondPlace(LottoReward lottoReward,
+            boolean isBonusNumberMatched) {
+        if (lottoReward == THIRD_PLACE) {
+            if (isBonusNumberMatched) {
+                return SECOND_PLACE;
+            }
+        }
+        return lottoReward;
+    }
+
+    public int getMatchNumbersCount() {
+        return matchNumbersCount;
+    }
+
+    public long getWinningReward() {
+        return winningReward;
+    }
+
+    private static LottoReward getLottoReward(int matchedCount) {
+        if (matchedCount < WINNING_MIN_COUNT) {
+            return NOTHING;
+        }
+
+        return Arrays.stream(values())
+                .filter(reward -> reward.matchNumbersCount == matchedCount)
+                .reduce((first, second) -> second)
+                .orElse(NOTHING);
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

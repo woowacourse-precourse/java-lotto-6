04b4f701e043b972a4f7c32983931d3e79583790
Isaac Han <isaachan233@gmail.com>
2023-11-08 23:35:43 +0900

    fix: validate 위치 수정

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -18,23 +18,23 @@ public class LottoNumber {
     private final int number;
 
     private LottoNumber(int number) {
-        validate(number);
         this.number = number;
+    }
+
+    public static LottoNumber valueOf(int number) {
+        validate(number);
+        return lottoNumberCache.get(number);
     }
 
     public static LottoNumber valueOf(String stringNumber) {
         return LottoNumber.valueOf(StringUtil.stringToInt(stringNumber));
     }
 
-    public static LottoNumber valueOf(int number) {
-        return lottoNumberCache.get(number);
-    }
-
     public int getNumber() {
         return this.number;
     }
 
-    private void validate(int number) {
+    private static void validate(int number) {
         if (number < LottoEnum.MIN_LOTTO_NUMBER.getValue()
                 || number > LottoEnum.MAX_LOTTO_NUMBER.getValue()) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_NUMBER_RANGE_MESSAGE.getMessage());

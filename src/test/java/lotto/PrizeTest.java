e09ff2f993e59d30bb15b39d2ab7e42434dab01e
nhn1030 <shgusska12@gmail.com>
2023-11-08 20:34:21 +0900

    test: Prize 클래스 정의를 위한 테스트 코드 작성

---
+++ src/test/java/lotto/PrizeTest.java
@@ -0,0 +1,17 @@
+package lotto.domain.;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrizeTest {
+
+    @Test
+    void testPrizeAmount() {
+        assertEquals(1_000_000_000, Prize.WinningPrize.FIRST_PRIZE.getPrizeAmount());
+        assertEquals(50_000_000, Prize.WinningPrize.SECOND_PRIZE.getPrizeAmount());
+        assertEquals(1_000_000, Prize.WinningPrize.THIRD_PRIZE.getPrizeAmount());
+        assertEquals(50_000, Prize.WinningPrize.FOURTH_PRIZE.getPrizeAmount());
+        assertEquals(5_000, Prize.WinningPrize.FIFTH_PRIZE.getPrizeAmount());
+    }
+}

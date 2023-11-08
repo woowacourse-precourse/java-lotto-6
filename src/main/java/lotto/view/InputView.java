3bdd80376edb3a1fd4a4e5fb60d8330a24a600c7
donghoonyeom <dua5410773@naver.com>
2023-11-08 18:13:51 +0900

    refactor(view): InputView 클래스의 유효성 검사 로직을 별도의 메서드로 분리하고 do-while 루프를 활용하여 입력값 검증 로직 개선
    기존에는 while 루프 안에서 예외 처리를 통해 입력값을 검증하고 다시 입력을 받는 형태로 되어 있었습니다. 이를 별도의 메서드로 분리하여 메서드의 역할을 명확히하고, do-while 루프를 활용하여 더 읽기 쉬운 코드로 변경하였습니다.
    
    변경된 사항:
    - validatePurchaseAmount, parseAndValidateWinningNumbers, validateBonusNumber 메서드를 도입하여 각각 구매 금액, 당첨 번호, 보너스 번호의 유효성을 검사하고 예외 처리하도록 로직을 분리하였습니다.
    - 각 메서드에서는 do-while 루프를 활용하여 유효한 입력값을 받을 때까지 반복하도록 로직을 변경하였습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -16,45 +16,64 @@ public class InputView {
     }
 
     public static int getPurchaseAmount() {
-        int purchaseAmount = 0;
-        while (purchaseAmount == 0) {
+        int purchaseAmount;
+        do {
             String input = getPlayerInput("구입 금액을 입력해 주세요");
-            try {
-                purchaseAmount = PurchaseAmountValidator.parseAndValidatePurchaseAmount(input);
-            } catch (IllegalArgumentException e) {
-                OutputView.printMessage(e.getMessage());
-            }
-        }
+            purchaseAmount = validatePurchaseAmount(input);
+        } while (purchaseAmount == 0);
         OutputView.printEmptyLine();
         return purchaseAmount;
     }
 
-    public static List<Integer> getWinningNumbers() {
-        while (true) {
-            String input = getPlayerInput("당첨 번호를 입력해 주세요");
-            try {
-                WinningNumbersValidator.validateInputFormat(input);
-                List<Integer> winningNumbers = StringUtil.splitAndParse(input, ",");
-                WinningNumbersValidator.validateWinningNumbers(input);
-                OutputView.printEmptyLine();
-                return winningNumbers;
-            } catch (IllegalArgumentException e) {
-                OutputView.printMessage(e.getMessage());
-            }
+    private static int validatePurchaseAmount(String input) {
+        try {
+            return PurchaseAmountValidator.parseAndValidatePurchaseAmount(input);
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return 0;
         }
     }
 
-    public static int getBonusNumber(Set<Integer> winningNumbers) {
-        int bonusNumber = 0;
-        while (bonusNumber == 0) {
-            String input = getPlayerInput("보너스 번호를 입력해 주세요");
-            try {
-                bonusNumber = BonusNumberValidator.validateBonusNumber(input, winningNumbers);
-                OutputView.printEmptyLine();
-            } catch (IllegalArgumentException e) {
-                OutputView.printMessage(e.getMessage());
-            }
+    public static List<Integer> getWinningNumbers() {
+        List<Integer> winningNumbers;
+        do {
+            String input = getPlayerInput("당첨 번호를 입력해 주세요");
+            winningNumbers = parseAndValidateWinningNumbers(input);
+        } while (winningNumbers == null);
+        OutputView.printEmptyLine();
+        return winningNumbers;
+    }
+
+    private static List<Integer> parseAndValidateWinningNumbers(String input) {
+        try {
+            return parseWinningNumbers(input);
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return null;
         }
+    }
+
+    private static List<Integer> parseWinningNumbers(String input) {
+        WinningNumbersValidator.validateInputFormat(input);
+        WinningNumbersValidator.validateWinningNumbers(input);
+        return StringUtil.splitAndParse(input, ",");
+    }
+
+    public static int getBonusNumber(Set<Integer> winningNumbers) {
+        int bonusNumber;
+        do {
+            String input = getPlayerInput("보너스 번호를 입력해 주세요");
+            bonusNumber = validateBonusNumber(input, winningNumbers);
+        } while (bonusNumber == 0);
         return bonusNumber;
+    }
+
+    private static int validateBonusNumber(String input, Set<Integer> winningNumbers) {
+        try {
+            return BonusNumberValidator.validateBonusNumber(input, winningNumbers);
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return 0;
+        }
     }
 }

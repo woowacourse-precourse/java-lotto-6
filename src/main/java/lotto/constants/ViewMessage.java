a9c45f6ed2e15f296c3f298ba3bca755df9c7c00
minsu20 <syt07100@gmail.com>
2023-11-08 23:10:08 +0900

    fix: 출력 메시지 format 수정

--- src/main/java/lotto/constants/ViewMessage.java
+++ src/main/java/lotto/constants/ViewMessage.java
@@ -6,8 +6,8 @@ public enum ViewMessage {
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS("당첨 통계\n---"),
-    MATCH_EXCEPT_BONUS("d개 일치 (%s원) - %d개"),
-    MATCH_INCLUDE_BONUS("5개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    MATCH_EXCEPT_BONUS("%d개 일치 (%,d원) - %d개"),
+    MATCH_INCLUDE_BONUS("5개 일치, 보너스 볼 일치 (%,d원) - %d개"),
     TOTAL_PROFIT_RATE("총 수익률은 %s%%입니다.");
 
     private final String message;

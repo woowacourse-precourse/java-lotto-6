689dce6a64c6d8df166036e41fe3acd3a31d486a
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-08 17:51:11 +0900

    feat: 입력된 번호 수 만큼 로또를 발행하고, 당첨 결과를 내는 기능 추가

--- src/main/java/lotto/Model/LottoGroup.java
+++ src/main/java/lotto/Model/LottoGroup.java
@@ -1,34 +1,45 @@
-package lotto;
+package lotto.Model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import lotto.Constants.MatchTypes;
 
 public class LottoGroup {
     private List<Lotto> lottoGroup;
 
-    public LottoGroup(List<List<Integer>> lotteryNumbersGroup){
-        for(List<Integer> lotteryNumbers : lotteryNumbersGroup){
+    public LottoGroup(List<List<Integer>> lotteryNumbersGroup) {
+        this.lottoGroup = new ArrayList<>();
+        for (List<Integer> lotteryNumbers : lotteryNumbersGroup) {
             Lotto lotto = new Lotto(lotteryNumbers);
             this.lottoGroup.add(lotto);
         }
     }
 
-    public List<Map<String, Integer>> drawLotto(List<Integer> winningNum, Integer bonusWinningNum){
-        List<Map<String, Integer>> drawResults = new ArrayList<>();
-        Map<String, Integer> resultOfEachLotto = new HashMap<>();
-        for(Lotto lotto : lottoGroup){
-            Integer matchCount = lotto.countWinningMatches(winningNum);
-            Integer bonusMatchCount = lotto.countBonusWinningMatches(bonusWinningNum);
-            resultOfEachLotto.put("matchCount", matchCount);
-            resultOfEachLotto.put("hasBonusMatch", bonusMatchCount);
-            drawResults.add(resultOfEachLotto);
+    public Map<MatchTypes, Integer> drawLotto(List<Integer> winningNum, Integer bonusWinningNum) {
+        Map<MatchTypes, Integer> drawResult = initDrawResult();
+        for (Lotto lotto : lottoGroup) {
+            Integer numOfMatchedWinningNum = lotto.countWinningMatches(winningNum);
+            Integer numOfMatchedBonusWinningNum = lotto.countBonusWinningMatches(bonusWinningNum);
+            MatchTypes matchType =
+                    MatchTypes.findMatchType(numOfMatchedWinningNum, numOfMatchedBonusWinningNum);
+            if (matchType != null) {
+                drawResult.put(matchType, drawResult.getOrDefault(matchType, 0) + 1);
+            }
         }
-
-        return drawResults;
+        return drawResult;
     }
-    public List<Lotto> getLottoGroup(){
+
+    private Map<MatchTypes, Integer> initDrawResult() {
+        Map<MatchTypes, Integer> initiatedDrawResult = new LinkedHashMap<>();
+        for (MatchTypes matchType : MatchTypes.values()) {
+            initiatedDrawResult.put(matchType, 0);
+        }
+        return initiatedDrawResult;
+    }
+
+    public List<Lotto> getLottoGroup() {
         return this.lottoGroup;
     }
 }

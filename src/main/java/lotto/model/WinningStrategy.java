90f83e1264e6f51c50697cb6a2cb520ef15b6078
wooni97 <jhw970714@gmail.com>
2023-11-08 10:08:08 +0900

    feat: WinningStrategy enum에 calculator 메서드 추가
    - 이 커밋은 WinningStrategy enum에 수익을 계산하는 메서드를 추가하여 당첨 내역에 따라 맞춤형으로 수익 계산이 가능하도록 합니다.

--- src/main/java/lotto/model/WinningStrategy.java
+++ src/main/java/lotto/model/WinningStrategy.java
@@ -2,24 +2,30 @@ package lotto.model;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Function;
 
 public enum WinningStrategy {
-    MATCHES_THREE(3, false,5000),
-    MATCHES_FOUR(4, false,50000),
-    MATCHES_FIVE(5, false,1500000),
-    MATCHES_FIVE_WITH_BONUS(5, true,30000000),
-    MATCHES_SIX(6, false,2000000000);
+    MATCHES_THREE(3, false,5000, profit -> profit + 5000),
+    MATCHES_FOUR(4, false,50000, profit -> profit + 50000),
+    MATCHES_FIVE(5, false,1500000, profit -> profit + 1500000),
+    MATCHES_FIVE_WITH_BONUS(5, true,30000000, profit -> profit + 30000000),
+    MATCHES_SIX(6, false,2000000000, profit -> profit + 2000000000);
 
     private int matchCount;
     private boolean bonusMatch;
     private int winningAmount;
+    private Function<Integer, Integer> expression;
 
-    WinningStrategy(int matchCount, boolean bonusMatch, int winningAmount) {
+    WinningStrategy(int matchCount, boolean bonusMatch, int winningAmount, Function<Integer, Integer> expression) {
         this.matchCount = matchCount;
         this.bonusMatch = bonusMatch;
         this.winningAmount = winningAmount;
+        this.expression = expression;
     }
 
+    public int calculate(int profit) {
+        return expression.apply(profit);
+    }
     public static Optional<WinningStrategy> find(int matchCount, boolean bonusMatch) {
         return Arrays.stream(WinningStrategy.values())
                 .filter(winningStrategy -> winningStrategy.matchCount == matchCount && winningStrategy.bonusMatch == bonusMatch)

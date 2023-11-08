a3a6a4c65ad167bccab99d462e49f2178b23d20c
MisaSohee <sohee5951@naver.com>
2023-11-08 22:27:39 +0900

    test(Rank): 적절한 Rank를 반환하는 테스트

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RankTest {
+    @DisplayName("valueOf()는 일치하는 번호의 개수와 보너스 번호 일치 여부에 따라 적절한 Rank를 반환한다.")
+    @Test
+    void valueOfReturnsCorrectRank() {
+        assertThat(Rank.valueOf(6, false)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(2, false)).isEqualTo(Rank.MISS);
+    }
+}

d0ff8b047c14de1421419d5413fd9b0857390069
Gukin Han <gukin.dev@gmail.com>
2023-11-08 16:14:10 +0900

    feat(WinningNumbers): 보너스 번호에 대해 범위 검증 로직 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -2,15 +2,23 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.message.Error.LOTTO_NUMBER_DUPLICATE;
+
 public class WinningNumbers extends Lotto {
     private final int bonusNumber;
 
-    public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+    public WinningNumbers(List<Integer> numbers, int bonusNumber) throws IllegalArgumentException {
         super(numbers);
         validateLottoRange(bonusNumber);
+        validateDuplicateBonusNumber(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateDuplicateBonusNumber(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE.getMessage());
+        }
+    }
 
     public int getBonusNumber() {
         return bonusNumber;

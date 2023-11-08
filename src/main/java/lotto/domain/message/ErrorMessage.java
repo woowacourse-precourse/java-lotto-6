45fece8ed7f8d5067fb4708e5cc606dfa42cfb44
syy0O <86238720+syy0O@users.noreply.github.com>
2023-11-08 17:25:48 +0900

    fix: 에러메시지 수정

--- src/main/java/lotto/domain/message/ErrorMessage.java
+++ src/main/java/lotto/domain/message/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.domain.message;
 
 public enum ErrorMessage {
-    INVALID_MONEY_VALUE("[ERROR] 금액은 천원 이상 백만원 이하의 1000원 단위의 금액이어야 합니다."),
+    INVALID_MONEY_VALUE("[ERROR] 금액은 1000원 단위의 금액이어야 합니다."),
     INVALID_LOTTO_LENGTH("[ERROR] 로또 번호의 길이는 6개여야 합니다."),
     DUPLICATES_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복을 허용하지 않습니다."),
     OUT_OF_RANGE("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다."),

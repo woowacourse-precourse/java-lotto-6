f091b47ccaa85499e5cc403af6547e8311528714
Suyoung Oh <ohsusu515@gmail.com>
2023-11-08 20:42:41 +0900

    feat: 당첨 번호 개수, 중복 여부 처리 로직 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,17 +1,40 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import lotto.constants.ExceptionMessage;
+import lotto.constants.GameRule;
+import lotto.utils.LottoGameException;
 
 public class WinningNumber {
     private final List<Integer> numbers;
 
-    public WinningNumber(String numbers) {
+    public WinningNumber(String numbers) { // 일단 숫자로 들어온다. 그럴 경우 예외가? 1. 6개 아닌 경우 2. 중복숫자존재
         List<Integer> splitNumbers = Arrays.stream(numbers.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        validateSize(splitNumbers);
+        validateDuplicate(splitNumbers);
         this.numbers = splitNumbers;
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != GameRule.LOTTO_LENGTH) {
+            throw LottoGameException.withMessage(ExceptionMessage.LOTTO_LENGTH_ERROR);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> unique = new HashSet<>();
+        numbers.stream()
+                .filter(number -> !unique.add(number))
+                .findAny()
+                .ifPresent(duplicate -> {
+                    throw LottoGameException.withMessage(ExceptionMessage.LOTTO_DUPLICATE_ERROR);
+                });
     }
 
     public static WinningNumber consistOf(String numbers) {

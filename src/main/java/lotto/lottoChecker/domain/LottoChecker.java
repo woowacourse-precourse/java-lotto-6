1c9c567289e8b2ffcb35916a4fceddc9c6a76675
fing9 <djajsl54@naver.com>
2023-11-08 21:26:18 +0900

    feat : 로또 당첨 계산 기능 구현

---
+++ src/main/java/lotto/lottoChecker/domain/LottoChecker.java
@@ -0,0 +1,36 @@
+package lotto.lottoChecker.domain;
+
+import java.util.List;
+
+public class LottoChecker {
+
+    private final Integer BONUS_INDEX = 6;
+
+    public Integer calculateRank(List<Integer> winningNumbers, List<Integer> lottoNumbers) {
+        Integer matchedCount = 0;
+        Integer bonusMatched = 0;
+        lottoNumbers.retainAll(winningNumbers);
+        matchedCount = lottoNumbers.size();
+        if (lottoNumbers.contains(winningNumbers.get(BONUS_INDEX))) {
+            matchedCount -= 1;
+            bonusMatched = 1;
+        }
+        return convertRank(matchedCount, bonusMatched);
+    }
+
+    public Integer convertRank(Integer matchedCount, Integer bonusMatched) {
+        if (matchedCount == 3) {
+            return 5;
+        } else if (matchedCount == 4) {
+            return 4;
+        } else if (matchedCount == 5 && bonusMatched == 0) {
+            return 3;
+        } else if (matchedCount == 5 && bonusMatched == 1) {
+            return 2;
+        } else if (matchedCount == 6) {
+            return 1;
+        }
+        return 6;
+    }
+
+}

b97961f510b233571d9e159ca948b099dd0ad238
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 20:25:56 +0900

    feat: 당첨 통계 출력 함수

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -14,5 +14,12 @@ public class OutputView {
             System.out.println(randomLottoNumbers);
     }
 
-    
+    public void printResult(enum Result) {
+        System.out.println("당첨 통계 \n---");
+        System.out.println("3개 일치 (5,000원) - " + Result.COUNTTHREE.getValue() + "개");
+        System.out.println("4개 일치 (50,000원) - " + Result.COUNTFOUR.getValue() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + Result.COUNTFIVE.getValue() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Result.COUNTFIVEBONUS.getValue() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + Result.COUNTSIX.getValue() + "개");
+    }
 }

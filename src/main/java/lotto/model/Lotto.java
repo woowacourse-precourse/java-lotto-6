932d0a0b88322d8ae644ddeb3c1a68ea991a7dab
3juhwan <13selfesteem91@naver.com>
2023-11-08 10:12:11 +0900

    feat: 로또 검증 로직

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,7 +3,12 @@ package lotto.model;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static lotto.utils.ErrorMessage.*;
+
 public class Lotto {
+    private static final Integer VALID_LENGTH = 6;
+    private static final Integer MINIMUM_NUMBER = 1;
+    private static final Integer MAXIMUM_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,8 +17,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        hasValidLength(numbers);
+        hasUniqueElement(numbers);
+        hasWithinRangeElement(numbers);
+    }
+
+    private void hasValidLength(List<Integer> numbers) {
+        if (numbers.size() != VALID_LENGTH) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_OUT_OF_RANGE_IN_LENGTH.getDescription());
+        }
+    }
+
+    private void hasUniqueElement(List<Integer> numbers) {
+        long uniqueNumberCount = numbers.stream().distinct().count();
+        if (uniqueNumberCount != VALID_LENGTH) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_NOT_UNIQUE_NUMBER.getDescription());
+        }
+    }
+
+    private void hasWithinRangeElement(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
+                throw new IllegalArgumentException(WINNING_NUMBERS_OUT_OF_RANGE_NUMBER.getDescription());
+            }
         }
     }
 

212e7ceb509d33cb8fd251c8f069e8d6b8eac421
cyPark95 <parkcy1007@gmail.com>
2023-11-08 14:25:32 +0900

    refactor: 매직 넘버 상수로 치환
    - 로또 번호의 최소 숫자와 최대 숫자, 로또 번호의 개수 상수로 치환

--- src/main/java/lotto/domain/strategy/RandomNumberStrategy.java
+++ src/main/java/lotto/domain/strategy/RandomNumberStrategy.java
@@ -5,8 +5,16 @@ import java.util.List;
 
 public class RandomNumberStrategy implements LottoNumberStrategy {
 
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_SIZE = 6;
+
     @Override
     public List<Integer> createNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                LOTTO_NUMBER_SIZE
+        );
     }
 }

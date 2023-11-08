f1be9083ab8802236c063aaad4f7b767ddf2783c
박승민 <tmddudals369@naver.com>
2023-11-08 14:29:41 +0900

    feat(view): 당첨 로또를 받는 과정에서 문제 발생시 재시도하는 기능 구현

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.domain.Amount;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
+import lotto.validation.NumberValidator;
+import lotto.validation.WinningNumberValidator;
 
 public class View {
     private final InputView inputView;
@@ -19,6 +25,57 @@ public class View {
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
             return getAmount();
+        }
+    }
+
+    public WinningLotto getWinningLotto() {
+        Lotto winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber(winningNumbers);
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    private Lotto getWinningNumbers() {
+        try {
+            outputView.printWinningNumbersGuideMessage();
+            String winningNumbers = inputView.inputWinningNumbers();
+            List<Integer> list = convertToIntegerList(winningNumbers);
+            WinningNumberValidator.validateDuplicateWinningNumber(list);
+            return new Lotto(list);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return getWinningNumbers();
+        }
+    }
+
+    private List<Integer> convertToIntegerList(String winningNumbers) {
+        return Arrays.stream(winningNumbers.split(",", -1))
+            .map(Integer::parseInt)
+            .peek(NumberValidator::validateInRangeNumber)
+            .toList();
+    }
+
+    private int getBonusNumber(Lotto winningNumbers) {
+        try {
+            outputView.printBonusNumberGuideMessage();
+            String bonusNumberValue = inputView.inputBonusNumber();
+            int bonusNumber = convertToInt(bonusNumberValue);
+            isDuplicateBonusNumber(winningNumbers, bonusNumber);
+            return bonusNumber;
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return getBonusNumber(winningNumbers);
+        }
+    }
+
+    private static int convertToInt(String bonusNumberValue) {
+        int convertedBonusNumber = Integer.parseInt(bonusNumberValue);
+        NumberValidator.validateInRangeNumber(convertedBonusNumber);
+        return convertedBonusNumber;
+    }
+
+    private void isDuplicateBonusNumber(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.isAlreadyExistNumber(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호와 당첨 번호에 중복된 수가 존재할 수 없습니다.");
         }
     }
 }

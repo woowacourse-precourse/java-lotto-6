352bd16f75802bb68f8f93bc4d865978222fed5a
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 20:11:17 +0900

    add: 보너스 번호 당첨번호 중복 예외 메세지 추가

--- src/main/java/lotto/exception/ExceptionCode.java
+++ src/main/java/lotto/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     LOTTO_INVALID_RANGE("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요."),
     LOTTO_DUPLICATION("[ERROR] 로또 번호가 중복되지 않도록 입력해주세요."),
     AMOUNT_PURCHASE_PRICE("[ERROR] 구매 금액을 1,000원 단위로 입력해주세요."),
-    UNSIGNED_PURCHASE_PRICE("[ERROR] 구매 금액을 양수로 입력해주세요.");
+    UNSIGNED_PURCHASE_PRICE("[ERROR] 구매 금액을 양수로 입력해주세요."),
+    BONUS_NUMBER_CONTAINED("[ERROR] 보너스 번호가 당첨 번호와 겹치지 않도록 입력해주세요.");
 
     private final String description;
 

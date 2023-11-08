ff741ab7d2f1839996c2dc26cb89f4ea4daa9197
pjaehyun <show6756@gmail.com>
2023-11-08 14:17:28 +0900

    feat: 추가 로또 출력 관련 LottoConstants Class

---
+++ src/main/java/constants/LottoConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class LottoConstants {
+    public static final String PURCHASE_MESSAGE = "개를 구매했습니다";
+
+    public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+
+    public static final String DASHES = "---";
+
+    public static final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+}

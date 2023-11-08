e73ba0c25f108c3dd9f63651bf52616354fe3c68
yooonwodyd <dbswodyd00@naver.com>
2023-11-08 17:30:48 +0900

    당첨번호 클래스 생성

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,2 +1,25 @@
-package lotto.model;public class WinningNumbers {
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> sixNumbers;
+    private final int bonusNumber;
+
+    private WinningNumbers(List<Integer> sixNumbers, int bonusNumber) {
+        this.sixNumbers = sixNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningNumbers of(List<Integer> winningNumbers, int bonusNumber) {
+        return new WinningNumbers(winningNumbers, bonusNumber);
+    }
+
+    public List<Integer> getSixNumbers() {
+        return sixNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }

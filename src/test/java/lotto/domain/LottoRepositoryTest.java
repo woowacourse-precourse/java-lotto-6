2adc544811e404374fc25b1f0cc1b31d5a046718
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-08 20:31:57 +0900

    test(LottoRepository) : domain UI로직 제거
    UI(System.out, System.in, Scanner)

--- src/test/java/lotto/domain/LottoRepositoryTest.java
+++ src/test/java/lotto/domain/LottoRepositoryTest.java
@@ -1,10 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -33,20 +30,5 @@ class LottoRepositoryTest {
     @DisplayName("lottoRepository get() 확인")
     void get() {
         assertThat(lottoRepository.get(1)).isEqualTo(lotto2);
-    }
-
-    @Test
-    @DisplayName("lottoRepository printAllLottoInfo() 올바르게 출력되는지 확인")
-    void printAllLottoInfo() {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-        lottoRepository.printAllLottoInfo();
-
-        System.setOut(System.out);
-
-        String printedOutput = outputStream.toString().trim();
-
-        assertTrue(printedOutput.contains("[1, 2, 3, 4, 5, 6]"));
-        assertTrue(printedOutput.contains("[1, 10, 23, 34, 35, 45]"));
     }
 }

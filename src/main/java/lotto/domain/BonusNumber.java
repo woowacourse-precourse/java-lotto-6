5c58f6944b561da9e964e86f2ddc799feaab0879
Junho Lee <ghzm888@gmail.com>
2023-11-08 19:48:15 +0900

    refactor : 직관적으로 OutputType에서 받아 사용하도록 변경

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,15 +1,15 @@
 package lotto.domain;
 
+import lotto.global.utils.constant.LottoNumberType;
 import lotto.global.utils.constant.OutputType;
 
-import java.util.HashSet;
 import java.util.List;
 
 public class BonusNumber {
     private final int bonusNumber;
 
     public BonusNumber(Lotto lotto, int bonusNumber) {
-        validate(lotto,bonusNumber);
+        validate(lotto, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
@@ -18,7 +18,8 @@ public class BonusNumber {
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(OutputType.EXCEPTION_INPUT_REDUNDANT.getComment());
         }
-        if (bonusNumber < 1 || bonusNumber > 45) {
+        if (bonusNumber < LottoNumberType.MIN_NUMBER_OF_RANGE.getValue()
+                || bonusNumber > LottoNumberType.MAX_NUMBER_OF_RANGE.getValue()) {
             throw new IllegalArgumentException(OutputType.EXCEPTION_INPUT_RANGE.getComment());
         }
     }

1d36b9715dde4347cad366e877dca2e22b66e847
haruday97 <starharu97@naver.com>
2023-11-08 11:07:06 +0900

    :white_check_mark: test : WinningLotto Test 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import lotto.constants.Prize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,14 @@ public class WinningLottoTest {
         WinningLotto winLotto = WinningLotto.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                 new BonusNumber(7, new Lotto(List.of(1, 2, 3, 4, 5, 6))));
         assertThat(winLotto).isNotNull();
+    }
+
+    @DisplayName("올바르게 순위를 배정한다.")
+    @Test
+    void should_RankCorrectPlace() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningLotto = WinningLotto.of(lotto, new BonusNumber(10, lotto));
+        Lotto fifthPlace = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+        assertThat(winningLotto.rankPrize(fifthPlace).getMessage()).isEqualTo(Prize.FIFTH.getMessage());
     }
 }

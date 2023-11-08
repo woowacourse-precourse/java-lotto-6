d3602660c33b5727a1a5f89a7c5fabc504075e06
Kim myeong-hwi <sjkl6206s@gmail.com>
2023-11-08 09:49:35 +0900

    refactor: Money 클래스의 이름을 Purchase로 변경

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,9 +1,9 @@
 package lotto.domain;
 
-public class Money {
+public class Purchase {
     private final int money;
 
-    public Money(int money) {
+    public Purchase(int money) {
         this.money = money;
     }
 }

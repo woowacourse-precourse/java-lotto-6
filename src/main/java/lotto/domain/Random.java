4da9fc547ba7e74880fa844230849d98ada61ad7
Dowon <3120kang@gmail.com>
2023-11-08 20:31:07 +0900

    Refactor(Random): 하드코딩한 상수 변경

--- src/main/java/lotto/domain/Random.java
+++ src/main/java/lotto/domain/Random.java
@@ -6,7 +6,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Random {
 
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
     public static List<Integer> makeNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
     }
 }

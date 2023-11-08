d2681fa34cb3d54b2ba5f1aee9280bf96fe833a9
gyeommm <dkndob02@naver.com>
2023-11-08 14:38:02 +0900

    함수 최소화 및 보너스숫자 입력 함수 일부 구현

--- src/main/java/lotto/CorrectLotto.java
+++ src/main/java/lotto/CorrectLotto.java
@@ -7,6 +7,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class CorrectLotto {
     private Lotto correct_lotto;
+    private int bonus;
 
     public void printInputNumbersMessage(){
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -21,44 +22,54 @@ public class CorrectLotto {
         return count;
     }
 
-    public List<Integer> convertStringToPositiveInt(String input_numbers){
-        String[] numbers = input_numbers.split(",");
-        int count_comma = countComma(input_numbers);
-
-        // 입력값이 없는 부분이 있는지 확인
-        if(numbers.length != count_comma + 1){
+    // 입력값이 없는 부분이 있는지 확인
+    public void checkEmptyPart(int expect, int size){
+        if(expect != size){
             throw new IllegalArgumentException("입력값이 없는 부분이 있습니다. 6개의 당첨 번호를 입력해 주세요.");
         }
+    }
+
+    // 입력값이 숫자인지 확인하여 변환
+    public int IsInteger(String number){
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("입력값이 숫자가 아닙니다. 1~45 사이의 숫자를 입력해주세요.");
+        }
+    }
+
+    public List<Integer> convertStringToInteger(String input_numbers){
+        String[] numbers = input_numbers.split(",");
+        checkEmptyPart(countComma(input_numbers) + 1, numbers.length);
 
         List<Integer> converted_numbers = new ArrayList<>();
         for(String number : numbers){
-            String striped_number = number.strip();
-
-            // 입력값이 숫자인지 확인
-            int converted_number;
-            try {
-                converted_number = Integer.parseInt(striped_number);
-            } catch (NumberFormatException e){
-                throw new IllegalArgumentException("입력값이 숫자가 아닙니다. 1~45 사이의 숫자를 입력해주세요.");
-            }
-
+            int converted_number = IsInteger(number.strip());
             converted_numbers.add(converted_number);
         }
 
         return converted_numbers;
     }
     public void inputNumbers(){
-        String input_numbers = readLine();
-        List<Integer> converted_numbers = convertStringToPositiveInt(input_numbers);
-
         while(true) {
+            String input_numbers = readLine();
             try {
+                List<Integer> converted_numbers = convertStringToInteger(input_numbers);
                 correct_lotto = new Lotto(converted_numbers);
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println("[Error] " + e.getMessage());
             }
         }
+    }
+
+    public void printInputBonusMessage(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void inputBonus(){
+        String input_bonus = readLine();
+        bonus = IsInteger(input_bonus.strip());
     }
 
 }

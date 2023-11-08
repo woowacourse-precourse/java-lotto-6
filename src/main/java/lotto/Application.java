d4ec006ef71fe8c7186843c97389dd2f7b3e6238
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-08 19:38:06 +0900

    refactor: Applicationtest 예외테스트 처리를 위한 try catch 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoSystem lottoSystem = new LottoSystem();
-        lottoSystem.runSystem();
+        try{
+            lottoSystem.runSystem();
+        }catch (IllegalArgumentException e){
+            System.out.println("실행 종료");
+        }
     }
 }

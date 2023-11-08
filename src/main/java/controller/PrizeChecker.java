107cacd53cfa4a5996326d8479cb86cb11293a8a
mangwonKing <aud1035@naver.com>
2023-11-08 21:59:15 +0900

    refactor(lotto) PrizeChecker 속 매직넘버 상수로 처리

--- src/main/java/controller/PrizeChecker.java
+++ src/main/java/controller/PrizeChecker.java
@@ -4,7 +4,8 @@ import model.*;
 
 public class PrizeChecker {
     //당첨관련 클래스
-
+    private final int CHECK_BONUS_NUMBER = 5;
+    private final int SECOND_MATCH_NUMBER = 2;
     public Ranking checkRank(Lottos lottos, PrizeNumber prizeNumber) {
         Ranking ranking = new Ranking();
         for (Lotto lotto : lottos.getLottos()) {
@@ -16,8 +17,8 @@ public class PrizeChecker {
     private int checkAnswer(Lotto lotto, PrizeNumber prizeNumber) {
         int count = 0;
         count = lotto.checkMatch(prizeNumber.getPrizeNumber());
-        if (count == 5 && lotto.checkBonus(prizeNumber.getBonousNumber())) {
-            count += 2;
+        if (count == CHECK_BONUS_NUMBER && lotto.checkBonus(prizeNumber.getBonousNumber())) {
+            count += SECOND_MATCH_NUMBER;
         }
         return count;
     }

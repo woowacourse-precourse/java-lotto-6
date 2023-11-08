51a51de92df978f21cf04af1b6fe1a82cf916ae9
MyStraw <kikongdosa@gmail.com>
2023-11-08 22:08:52 +0900

    Test: 추가 LottoUtils 테스트
    -잘못된 당첨번호 글자 입력시 예외발생
    -정해진 수만큼 로또생성 테스트

--- src/test/java/lotto/util/LottoUtilsTest.java
+++ src/test/java/lotto/util/LottoUtilsTest.java
@@ -3,6 +3,7 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,14 +18,25 @@ public class LottoUtilsTest {
         List<Integer> result = LottoUtils.convertStringToLottoNumberList(input);
         assertThat(result).isEqualTo(expected);
     }
+    @Test
+    @DisplayName("올바르지 않은 형식의 문자열 변환 시 예외 발생")
+    public void invalidStringConversion() {
+        String input = "8, two, 19, 34, forty-one, 13";
+        assertThrows(NumberFormatException.class, () -> {
+            LottoUtils.convertStringToLottoNumberList(input);
+        });
+    }
 
     @Test
-    @DisplayName("로또 번호의 유효성을 검사한다")
-    void validateLottoNumbers() {
-        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        assertThat(LottoUtils.validateLottoNumbers(validNumbers)).isTrue();
-
-        List<Integer> invalidNumbers = Arrays.asList(0, 2, 3, 4, 5, 51);
-        assertThrows(IllegalArgumentException.class, () -> LottoUtils.validateLottoNumbers(invalidNumbers));
+    @DisplayName("정해진 수의 로또를 생성")
+    public void generateLottoPaper() {
+        int howManyLotto = 5;
+        List<Lotto> lottoPapers = LottoUtils.generateLottoPaper(howManyLotto);
+        assertThat(lottoPapers).hasSize(howManyLotto);
+        for (Lotto lotto : lottoPapers) {
+            assertThat(lotto.getNumbers()).hasSize(6);
+            assertThat(lotto.getNumbers()).allMatch(number -> number >= 1 && number <= 45);
+            assertThat(lotto.getNumbers()).doesNotHaveDuplicates();
+        }
     }
 }

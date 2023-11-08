b1777b692721e5c160a75adeb1c7c397e736fb34
gyuoo <yeomky98@naver.com>
2023-11-08 23:29:35 +0900

    feat: implement user input for winning numbers

--- src/main/java/util/UserInput.java
+++ src/main/java/util/UserInput.java
@@ -1,5 +1,9 @@
 package util;
 
+import domain.Lotto;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import view.InputView;
 
 public class UserInput {
@@ -9,6 +13,9 @@ public class UserInput {
     private static final String WRONG_PAYMENT_MESSAGE = "%s %d 이상의 수를 %d 단위로 입력해주세요.";
     private static final String BLANK_ERROR_MESSAGE = "%s 아무 것도 입력되지 않았습니다.";
     private static final String WRONG_VALUE_MESSAGE = "%s 숫자가 아닌 값이 입력되었습니다.";
+    private static final String DUPLICATE_MESSAGE = "%s 중복된 숫자가 입력되었습니다.";
+    private static final String WRONG_LOTTO_NUMBER_MESSAGE = "%s %d 이상, %d 이하의 수를 입력해주세요.";
+
 
     public static long getUserPayment() {
         String userInput = InputView.setUserPayment();
@@ -46,6 +53,51 @@ public class UserInput {
         if (userInput % UNIT != 0) {
             throw new IllegalArgumentException(
                     String.format(WRONG_PAYMENT_MESSAGE, START_MESSAGE, MIN_PAYMENT, UNIT));
+        }
+    }
+
+    public static Lotto getWinningNumbers() {
+        String userInput = InputView.setWinningNumbers();
+        String[] splitInput = userInput.split(",");
+        for (String input : splitInput) {
+            checkEmpty(input);
+        }
+        List<Integer> winningNumbers = checkLong(splitInput);
+        checkDuplicates(winningNumbers);
+        for (int number : winningNumbers) {
+            checkValidNumber(number);
+        }
+        return new Lotto(winningNumbers);
+    }
+
+    private static List<Integer> checkLong(String[] userInput) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (int i = 0; i < userInput.length; i++) {
+            try {
+                winningNumbers.add(Integer.parseInt(userInput[i]));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(
+                        String.format(WRONG_VALUE_MESSAGE, START_MESSAGE));
+            }
+        }
+        return winningNumbers;
+    }
+
+    private static void checkDuplicates(List<Integer> numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        for (int number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException(
+                        String.format(DUPLICATE_MESSAGE, START_MESSAGE));
+            }
+        }
+    }
+
+    private static void checkValidNumber(int number) {
+        if (number < Lotto.MIN_LOTTO_NUMBER || Lotto.MAX_LOTTO_NUMBER < number) {
+            throw new IllegalArgumentException(
+                    String.format(WRONG_LOTTO_NUMBER_MESSAGE,
+                            START_MESSAGE, Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER));
         }
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PAYMENT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static String setUserPayment() {
         System.out.println(PAYMENT_MESSAGE);
@@ -20,5 +21,12 @@ public class InputView {
 
     private static String removeBlank(String userInput) {
         return userInput.replace(" ", "");
+    }
+
+    public static String setWinningNumbers() {
+        System.out.println(WINNING_NUMBER_MESSAGE);
+        String userInput = Console.readLine();
+        removeBlank(userInput);
+        return userInput;
     }
 }

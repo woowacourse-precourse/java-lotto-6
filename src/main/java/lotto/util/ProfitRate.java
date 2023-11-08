b516ac1ceacb80988ace1b076b35a0462982fdd4
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 14:27:51 +0900

    comment: delete useless comment part
    필요 없는 주석 삭제

--- src/main/java/lotto/util/ProfitRate.java
+++ src/main/java/lotto/util/ProfitRate.java
@@ -1,8 +1,6 @@
 package lotto.util;
 
 public class ProfitRate {
-
-    // 이후에 printProfit은 OutputView로 이동가능
     public static String calculateProfit(int totalPrize, int purchasePrice) {
         double result =  ((double) totalPrize / purchasePrice) * 100;
         return String.format("%.1f", result);

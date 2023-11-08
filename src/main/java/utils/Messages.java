4a7fabcb1866edf5ea81489d5b1f824d5c9a298d
ehdtndla123 <ehdtndla123@naver.com>
2023-11-08 17:58:16 +0900

    feat : Message 담당하는 클래스 추가

---
+++ src/main/java/utils/Messages.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Messages {
+    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public static final String BUY_LOTTO = "개를 구매했습니다.";
+    public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
+
+}

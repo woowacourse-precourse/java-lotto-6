0387288f269bd4991ba6d2e98dbc741b9a81fed8
soddong <aledjdygus9300@gmail.com>
2023-11-08 23:12:15 +0900

    fix: 수익률은 소수점 둘째 자리에서 반올림

--- src/main/java/lotto/service/ReturnService.java
+++ src/main/java/lotto/service/ReturnService.java
@@ -3,6 +3,6 @@ package lotto.service;
 public class ReturnService {
     public String evaluateLottoReturn(int payment, int winningAmount) {
         float returns = ((float) winningAmount / payment) * 100;
-        return String.format("%.2f%%", returns);
+        return String.format("%.1f%%", returns);
     }
 }

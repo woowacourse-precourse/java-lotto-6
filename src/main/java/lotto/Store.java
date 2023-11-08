ef6bc935bda6fb7e90cc1943820b6d33032365be
Dendrog <goodshs335@gmail.com>
2023-11-08 22:52:53 +0900

    Feat: 당첨번호 입력 기능 추가

--- src/main/java/lotto/Store.java
+++ src/main/java/lotto/Store.java
@@ -1,6 +1,44 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Store {
+    private List<Integer> targetNumbers;
+    private List<Integer> bonusNumber;
+
+    public Store() {
+        this.targetNumbers = new ArrayList<>();
+        this.bonusNumber = new ArrayList<>();
+    }
+
+    public void getTargetNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String inputTargetNumbers = Console.readLine();
+        try {
+            Validator.targetNumberCheck(inputTargetNumbers);
+            setTargetNumbers(inputTargetNumbers);
+        } catch (IllegalArgumentException e) {
+            getTargetNumbers();
+        }
+    }
+
+    public void getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String inputTargetNumbers = Console.readLine();
+        try {
+            //Validator.targetNumberCheck(inputTargetNumbers);
+            //setTargetNumbers(inputTargetNumbers);
+        } catch (IllegalArgumentException e) {
+            getBonusNumber();
+        }
+    }
+
+    private void setTargetNumbers(String inputTargetNumbers) {
+        String[] splitInputData = inputTargetNumbers.split(",");
+        for (int i = 0; i < 6; i++) {
+            targetNumbers.add(Integer.parseInt(splitInputData[i]));
+        }
+    }
 }
--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Validator {
     public static void buyCheck(String inputText) {
         try {
@@ -13,4 +17,51 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 정수만 입력해 주세요.");
         }
     }
+
+    public static void targetNumberCheck(String inputText) {
+        String[] splitInputData = inputText.split(",");
+        checkLength(splitInputData);
+        HashSet<Integer> TargetNumbers = new HashSet<>();
+        try {
+            for (int i = 0; i < 6; i++) {
+                int tmp = Integer.parseInt(splitInputData[i]);
+                TargetNumbers.add(tmp);
+                checkLange(tmp);
+            }
+            checkDuplicate(TargetNumbers);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 정수만 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 정수만 입력해 주세요.");
+        }
+
+    }
+
+    private static void checkLength(String[] splitInputData) {
+        if (splitInputData.length != 6) {
+            System.out.println("[ERROR] 번호를 6개 입력하세요");
+            throw new IllegalArgumentException("[ERROR] 번호를 6개 입력하세요");
+        }
+    }
+
+    private static void checkDuplicate(HashSet<Integer> TargetNumbers) {
+        if (TargetNumbers.size() != 6) {
+            System.out.println("[ERROR] 중복된 수를 입력하셨습니다.");
+            throw new IllegalArgumentException("[ERROR] 중복된 수를 입력하셨습니다.");
+        }
+    }
+
+    private static void checkLange(int number) {
+        if (number < 1 || number > 45){
+            System.out.println("[ERROR] 1~45 사이의 수를 입력하세요.");
+            throw new IllegalArgumentException("[ERROR] 1~45 사이의 수를 입력하세요.");
+        }
+    }
+
+    public static void checkBonusNumber(String inputText,List<Integer> targetNumbers){
+        try{
+            int bonusNumber = Integer.parseInt(inputText);
+        }catch (NumberFormatException e)
+    }
+
+
 }

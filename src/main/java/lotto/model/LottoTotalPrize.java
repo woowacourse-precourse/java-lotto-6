39e04bd3f5649e23fe8e319bd07856146e87c225
InJun2 <os2864@naver.com>
2023-11-08 15:52:04 +0900

    :art: 정적 필드로 상수 사용

--- src/main/java/lotto/model/LottoTotalPrize.java
+++ src/main/java/lotto/model/LottoTotalPrize.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LottoTotalPrize {
+    private final static int INIT_VALUE = 0;
+    private final static int PLUS_COUNT = 1;
+
     private final Map<LottoWinningRank, Integer> prizeCounts = new HashMap<>();
 
     public LottoTotalPrize() {
@@ -16,11 +19,11 @@ public class LottoTotalPrize {
     private void init() {
         Arrays.asList(LottoWinningRank.values())
                 .forEach(winningRank ->
-                        prizeCounts.put(winningRank, 0));
+                        prizeCounts.put(winningRank, INIT_VALUE));
     }
 
     public void prizeCountPlus(LottoWinningRank winningRank) {
-        prizeCounts.put(winningRank, prizeCounts.get(winningRank) + 1);
+        prizeCounts.put(winningRank, prizeCounts.get(winningRank) + PLUS_COUNT);
     }
 
     public Map<LottoWinningRank, Integer> getPrizeCounts() {

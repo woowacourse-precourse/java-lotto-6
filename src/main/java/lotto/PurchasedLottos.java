80c8158883c57996e94b8fe4796d7c55fe8da679
realisshomyang <realisshoman@kakao.com>
2023-11-08 21:17:16 +0900

    feat : 구매한 로또들을 기반으로 reward 정보를 저장하는 enumMap 도입

--- src/main/java/lotto/PurchasedLottos.java
+++ src/main/java/lotto/PurchasedLottos.java
@@ -3,18 +3,29 @@ package lotto;
 import static lotto.LottoGenerator.makeRandomNumbersInLotto;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class PurchasedLottos {
+
     private List<Lotto> lottos = new ArrayList<>();
+    private Map<LottoReward, Integer> rewardmap;
+
     private final Purchase purchase;
+
     public PurchasedLottos(Purchase purchase) {
         this.purchase = purchase;
+        rewardmap = new EnumMap<>(LottoReward.class);
+        for (LottoReward reward : LottoReward.values()) {
+            rewardmap.put(reward, 0); // Initialize each reward count to zero
+        }
         for (int i = 0; i < purchase.getTicketCount(); i++) {
             Lotto lotto = new Lotto(makeRandomNumbersInLotto());
             lottos.add(lotto);
         }
     }
+
     public String LottosToString() {
         StringBuilder stringBuilder = new StringBuilder();
         for (Lotto lotto : lottos) {
@@ -27,4 +38,6 @@ public class PurchasedLottos {
     public List<Lotto> getLottos() {
         return lottos;
     }
+
+
 }

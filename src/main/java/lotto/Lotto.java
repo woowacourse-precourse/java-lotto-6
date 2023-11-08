c63193ea93b76aaeb6c2a94f2fd7aefb7963af5a
Jung ui heon <ky7662@naver.com>
2023-11-08 16:02:54 +0900

    test : LottoTest 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,11 +8,22 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        equalNumberValidate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void equalNumberValidate(List<Integer> numbers){ // 동일한 숫자가 존재하면 예외 발생한다
+        HashSet<Integer> numberHashSet = new HashSet<>(); // Set선언
+        for(int number : numbers){
+            numberHashSet.add(number);
+        }
+        if(numberHashSet.size() != 6){ // Set의 size가 6이 아니면 동일한 숫자가 있다는 것이므로 예외 발생한다
             throw new IllegalArgumentException();
         }
     }

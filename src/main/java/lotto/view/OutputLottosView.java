e47c839079c0b6828a9d99d85b2a5babd4b989c0
KDT live <dnjsxo0616@gmail.com>
2023-11-08 19:02:10 +0900

    refactor(OutputLottosView): printLotto static 추가

--- src/main/java/lotto/view/OutputLottosView.java
+++ src/main/java/lotto/view/OutputLottosView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import lotto.domain.Lottos;
 
 public class OutputLottosView {
-    public void printLotto(Lottos lottos){
+    public static void printLotto(Lottos lottos){
         lottos.getLottos().forEach(System.out::println);
         System.out.println();
     }

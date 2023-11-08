e05102d3c03425e24dc128d74e4f62c502cf6d35
masonkimseoul <masonkimseoul@gmail.com>
2023-11-08 17:33:06 +0900

    feat : 입력한 당첨 번호와 구입한 로또 번호 비교 기능 구현

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -12,8 +12,19 @@ import lotto.View.InputView;
 public class LottoController {
 
     private static final int LOTTO_PRICE_PER_GAME = 1000;
+    private static final int HIT_TICK = 1;
     private int purchasedAmount = 0;
     private List<Lotto> lottos = new ArrayList<>();
+    private Lotto winnerLotto;
+    private int bonusNumber;
+
+    private void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void setWinnerLotto(Lotto winnerLotto) {
+        this.winnerLotto = winnerLotto;
+    }
 
     private int calcAvailableAmount(int availableMoney) {
         return availableMoney / LOTTO_PRICE_PER_GAME;
@@ -30,5 +41,29 @@ public class LottoController {
             Lotto lotto = new Lotto(lottoNumbers);
             lottos.add(lotto);
         }
+    }
+
+    private void getWinnerLotto() {
+        List<Integer> numbers = InputView.getLottoNumbers();
+        setWinnerLotto(new Lotto(numbers));
+
+        int bonusNumber = InputView.getBonusNumber(numbers);
+        setBonusNumber(bonusNumber);
+    }
+
+    private int[] compareWinnerAndPurchased(Lotto lotto) {
+        int normalHitCount = 0;
+        int bonusHitCount = 0;
+
+        for (Integer number : lotto.getNumbers()) {
+            if (this.winnerLotto.getNumbers().contains(number)) {
+                normalHitCount += HIT_TICK;
+            }
+            if (this.bonusNumber == number) {
+                bonusHitCount += HIT_TICK;
+            }
+        }
+
+        return new int[]{normalHitCount, bonusHitCount};
     }
 }

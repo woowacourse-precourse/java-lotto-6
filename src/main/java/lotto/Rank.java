90202409d46db83e967dbf4c46802fb8115e5768
icebear0111 <wltjq0901@naver.com>
2023-11-08 22:31:05 +0900

    fix: fix method to static method

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,11 +1,11 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
-    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
     FIFTH(3, 5000, "3개 일치 (5,000원) - "),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
     NONE(0, 0, "");
 
     private final int matchCnt;
@@ -17,15 +17,15 @@ public enum Rank {
         this.resultMessage = resultMessage;
     }
 
-    public int getMatchCnt(Rank rank) {
-        return matchCnt;
+    public static int getMatchCnt(Rank rank) {
+        return rank.matchCnt;
     }
 
-    public int getMoney(Rank rank) {
-        return money;
+    public static int getMoney(Rank rank) {
+        return rank.money;
     }
 
-    public String getResultMessage(Rank rank) {
-        return resultMessage;
+    public static String getResultMessage(Rank rank) {
+        return rank.resultMessage;
     }
 }

7d6d2b6a8cff8684d364f01d71ded96bffbf21ad
insu <is23193160@gmail.com>
2023-11-08 18:44:31 +0900

    test: 보너스 번호가 당첨 번호에 존재할 경우 예외가 발생하는 테스트 추가

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,17 @@ class WinningNumbersTest {
 
         assertThat(winningNumbers.getWinningLotto()).isEqualTo(winningLotto);
         assertThat(winningNumbers.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호에 존재할 경우 예외가 발생한다")
+    void inputBonusNumberInWinningLotto() {
+        Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 6;
+
+        assertThatThrownBy(() -> new WinningNumbers(winningLotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호에 포함될 수 없습니다.");
     }
 
 }

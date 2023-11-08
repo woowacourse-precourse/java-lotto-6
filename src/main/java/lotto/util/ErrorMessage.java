014006fdbc812654f25f26b78aa400d55f321a5b
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 16:27:32 +0900

    style(Constant): ERROR_BONUS_OUT_OF_RANGE 에러 문구 수정

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -7,7 +7,7 @@ public class ErrorMessage {
     public static final String ERROR_NUMERIC_TYPE = "[ERROR] 1부터 45 사이의 숫자를 입력해주세요.";
     public static final String ERROR_MINIMUM_PRICE = "[ERROR] 1,000원 이상의 금액을 입력해주세요.";
     public static final String ERROR_NOT_DIVIDE_LOTTO_PRICE = "[ERROR] 1,000원 단위의 금액을 입력해주세요.";
-    public static final String ERROR_BONUS_OUT_OF_RANGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_BONUS_OUT_OF_RANGE = "[ERROR] 1부터 45 사이의 숫자를 입력해주세요.";
     public static final String ERROR_BONUS_DUPLICATE = "[ERROR] 당첨 번호와 다른 숫자를 입력해주세요.";
     public static final String ERROR_BONUS_NUMERIC = "[ERROR] 1부터 45 사이의 숫자를 입력해주세요.";
     public static final String ERROR_WINNING_NUMBERS_NUMERIC = "[ERROR] 1부터 45 사이의 숫자를 쉼표(,)를 기준으로 구분하여 입력해주세요.";

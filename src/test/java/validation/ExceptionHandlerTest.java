65ed584444c5d86f35a5e66e5ea9691d8887801c
kyumho kim <rlaruagh030@gmail.com>
2023-11-08 13:31:47 +0900

    test : 구매 가격 입력 시 숫자 이외의 것이 입력되면 예외 발생 테스트 코드 작성

--- src/test/java/validation/ExceptionHandlerTest.java
+++ src/test/java/validation/ExceptionHandlerTest.java
@@ -2,12 +2,24 @@ package validation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ExceptionHandlerTest {
 
+    private final ExceptionHandler exceptionHandler;
+
+    public ExceptionHandlerTest(){
+        exceptionHandler = new ExceptionHandler();
+    }
     @Test
+    @DisplayName("구매 가격 입력 시 숫자 이외의 것이 입력되면 예외 발생")
     void validateIfOnlyNumber() {
+        String number = "1000?";
+        assertThrows(NumberFormatException.class, () -> {
+            exceptionHandler.validateIfOnlyNumber(number);
+        });
     }
 
     @Test

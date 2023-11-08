3eb9a66b5de8c0b2714e9b18ee79fad6b829a63e
JoonKyo <joonkyolee@gmail.com>
2023-11-08 21:35:32 +0900

    test: 유효한 로또를 발행할 경우에는 예외를 던지지 않는 것을 확인하는 테스트 구현

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,15 +1,22 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.stream.Stream;
 import lotto.domain.LottoMachine;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoMachineTest {
+    @DisplayName("발행된 로또가 유효한 로또일 경우 예외가 발생하지 않는다.")
+    @Test
+    void generatedLottoValidationTest() {
+        assertDoesNotThrow(() -> LottoMachine.purchaseLotto(1));
+    }
 
     @ParameterizedTest
     @DisplayName("로또 게임 수에 맞는 로또를 발행하는 지 확인한다.")
@@ -21,7 +28,7 @@ public class LottoMachineTest {
     }
 
     static Stream<Arguments> generateLottoGameCountData() {
-        return Stream.of (
+        return Stream.of(
                 Arguments.of(4, 4),
                 Arguments.of(5, 5),
                 Arguments.of(10, 10)

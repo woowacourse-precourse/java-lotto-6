580265d6b437d91c5266ed6dba260030c9a67c7c
kkw2758 <dtc3229@gmail.com>
2023-11-08 13:48:02 +0900

    Test(WinningInformationTest) : 중복된 테스트 검증 로직 삭제
    - Lotto 클래스에서 로또 번호에 대한 중복 검사 및 길이 검사를 담당하고 있음
    - WinningInformationTest에 Lotto 클래스의 기능을 중복해서 테스트하고 있음
    - WinningInformationTest의 중복된 테스트 검증 로직 삭제

--- src/test/java/lotto/WinningInformationTest.java
+++ src/test/java/lotto/WinningInformationTest.java
@@ -3,44 +3,15 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import constants.ErrorMessage;
-import constants.NumberType;
 import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningInformationTest {
-
     @Test
-    void 당첨번호_중복_예외_처리_테스트() {
-        //Given
-        List<LottoNumber> answerLottoNumbers = Stream.of(1, 2, 3, 3, 5, 6)
-                .map(LottoNumber::of)
-                .toList();
-        LottoNumber bonusNumber = LottoNumber.of(7);
-
-        //When & Then
-        assertThatThrownBy(() -> new WinningInformation(new Lotto(answerLottoNumbers), bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.LOTTO_HAS_DUPLICATE_NUMBER_ERROR.getMessage());
-    }
-
-    @Test
-    void 당첨번호_길이_예외_처리_테스트() {
-        //Given
-        List<LottoNumber> answerLottoNumbers = Stream.of(1, 2, 3, 4, 5, 6, 8)
-                .map(LottoNumber::of)
-                .toList();
-        LottoNumber bonusNumber = LottoNumber.of(7);
-
-        //When & Then
-        assertThatThrownBy(() -> new WinningInformation(new Lotto(answerLottoNumbers), bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(String.format(ErrorMessage.LOTTO_LENGTH_ERROR.getMessage(),
-                        NumberType.LOTTO_LENGTH.getValue()));
-    }
-
-    @Test
-    void 보너스_번호가_당첨번호에_포함된다면_예외_처리_테스트() {
+    @DisplayName("보너스 숫자가 당첨 번호에 포함되면 예외 처리한다.")
+    void validateBonusNumberTest() {
         //Given
         List<LottoNumber> answerLottoNumbers = Stream.of(1, 2, 3, 4, 5, 6)
                 .map(LottoNumber::of)

4a4a4ea468fd02ff13200b9d168e5e9f352ef758
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-11-08 18:22:59 +0900

    feat: 실행 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.controller.Controller;
+import lotto.domain.Rank;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.run();
     }
 }

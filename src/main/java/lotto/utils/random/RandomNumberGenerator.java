45b04125aca17c1616ee9db730ef90ab6d78d3d5
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-08 14:06:36 +0900

    feat: 무작위 번호 생성기 기능 수정
    RandomNumberGenerator 가 정렬된 숫자 리스트를 반환하도록 수정

--- src/main/java/lotto/utils/random/RandomNumberGenerator.java
+++ src/main/java/lotto/utils/random/RandomNumberGenerator.java
@@ -14,6 +14,8 @@ public class RandomNumberGenerator {
 
     public static List<Integer> getRandomNumbersInLottoRange() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN.getValue(), LOTTO_NUMBER_MAX.getValue(),
-                LOTTO_NUMBER_COUNT.getValue());
+                LOTTO_NUMBER_COUNT.getValue()).stream()
+                .sorted()
+                .toList();
     }
 }

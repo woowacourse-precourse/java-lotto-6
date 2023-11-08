bcebfab0b8ac3092e22000dbe78f7ddcb458a36f
icebear0111 <wltjq0901@naver.com>
2023-11-08 11:32:21 +0900

    feat: class for user output

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Output {
+    Rank rank;
+    public void printAmount(int amountOfLotto) {
+        System.out.println(amountOfLotto + "개를 구매했습니다.");
+    }
+    public void printLotto(List<Integer> lottoNumber) {
+        System.out.println(lottoNumber);
+    }
+    public void printResult(int[] countRankArray) {
+        System.out.println("당첨 통계");
+        System.out.println("___");
+        Rank[] ranks = Rank.values();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(rank.getResultMessage(ranks[i]) + countRankArray[i] + "개");
+        }
+    }
+}

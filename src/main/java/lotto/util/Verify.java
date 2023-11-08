ff345184746b7fb42f0c08d6dbf93d550a467940
yookyung Choi <ykc131719@gmail.com>
2023-11-08 23:01:31 +0900

    refactor: 중복으로 쓰이는 코드들 빼주기 -> verifyInteger(), verifyNumberRange()

--- src/main/java/lotto/util/Verify.java
+++ src/main/java/lotto/util/Verify.java
@@ -7,14 +7,7 @@ public class Verify {
 
     public Verify(){}
     public static Integer verifyAmount(String input) throws IllegalArgumentException{
-        Integer value;
-        try{
-            value = Integer.parseInt(String.valueOf(input));
-        }
-        catch (NumberFormatException e){
-            System.out.println("[ERROR] 숫자를 입력해야합니다.");
-            throw new IllegalArgumentException();
-        }
+        Integer value = verifyInteger(input);
         if(value % 1000 != 0){ // 1000원으로 나누어 떨어지지 않는 경우
             System.out.println("[ERROR] 1000원 단위로 입력해주세요.");
             throw new IllegalArgumentException();
@@ -27,18 +20,8 @@ public class Verify {
         String[] numbers = input.split(",");
         List<Integer> inputList = new ArrayList<>();
         for(int i=0; i<numbers.length; i++){
-            Integer number;
-            try{
-                number = Integer.parseInt(String.valueOf(numbers[i]));
-            }
-            catch (NumberFormatException e){
-                System.out.println("[ERROR] 숫자를 입력해야합니다.");
-                throw new IllegalArgumentException();
-            }
-            if(number<0 || number>46){
-                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-                throw new IllegalArgumentException();
-            }
+            Integer number = verifyInteger(numbers[i]);
+            verifyNumberRange(number);
             inputList.add(number);
         }
         return inputList;
@@ -46,18 +29,27 @@ public class Verify {
 
     public static Integer verifyBonusNumber(String input) throws IllegalArgumentException{
         input = input.replace(" ","");
-        Integer number;
+        Integer number = verifyInteger(input);
+        verifyNumberRange(number);
+        return number;
+    }
+
+    private static Integer verifyInteger(String input) throws IllegalArgumentException {
+        Integer number = 0;
         try{
             number = Integer.parseInt(String.valueOf(input));
         }
         catch (NumberFormatException e){
-            System.out.println("[ERROR] 숫자를 입력해야합니다.");
+            System.out.println("[ERROR] 숫자를 입력해주세요.");
             throw new IllegalArgumentException();
         }
+        return number;
+    }
+
+    private static void verifyNumberRange(Integer number) throws IllegalArgumentException {
         if(number<0 || number>46){
             System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             throw new IllegalArgumentException();
         }
-        return number;
     }
 }

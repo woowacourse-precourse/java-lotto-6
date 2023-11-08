daea57e5ea4fcaa41d7f6f026ced3e5c847ca7ad
sunwoong <sunwoong1122@gmail.com>
2023-11-08 22:39:54 +0900

    refactor: validator 검증 메서드 분리

--- src/main/java/lotto/global/util/Validator.java
+++ src/main/java/lotto/global/util/Validator.java
@@ -9,30 +9,64 @@ import java.util.List;
 public interface Validator {
     static void validatePrice(String price) {
         try {
-            Integer.parseInt(price);
+            int processedPrice = Integer.parseInt(price);
+            validateMismatchPrice(processedPrice);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.PRICE_NOT_INTEGER.getMessage());
         }
-        int processedPrice = Integer.parseInt(price);
+    }
+
+    static void validateNumbers(String numbers) {
+        validateIsBlankNumbers(numbers);
+        validateNotContainSymbolNumbers(numbers);
+        validateInvalidSymbolPositionNumbers(numbers);
+        validateValidLengthNumbers(numbers);
+        validateValidTypeAndRangeNumbers(numbers);
+        validateDuplicateNumbers(numbers);
+    }
+
+    static void validateBonusNumber(String bonusNumber) {
+        try {
+            int processedBonusNumber = Integer.parseInt(bonusNumber);
+            validateMismatchBonusNumber(processedBonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_NOT_INTEGER.getMessage());
+        }
+    }
+
+    private static void validateMismatchPrice(int processedPrice) {
         if (processedPrice % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.PRICE_MISMATCH.getMessage());
         }
     }
 
-    static void validateNumbers(String numbers) {
+    private static void validateIsBlankNumbers(String numbers) {
         if (numbers.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_IS_BLANK.getMessage());
         }
+    }
+
+    private static void validateNotContainSymbolNumbers(String numbers) {
         if (!numbers.contains(",")) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_NOT_CONTAIN_SYMBOL.getMessage());
         }
+    }
+
+    private static void validateInvalidSymbolPositionNumbers(String numbers) {
         if (numbers.startsWith(",") || numbers.endsWith(",")) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_INVALID_SYMBOL_POSITION.getMessage());
         }
+    }
+
+    private static void validateValidLengthNumbers(String numbers) {
         String[] processedNumbers = numbers.split(",");
         if (processedNumbers.length != 6) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_INVALID_COUNT.getMessage());
         }
+    }
+
+    private static void validateValidTypeAndRangeNumbers(String numbers) {
+        String[] processedNumbers = numbers.split(",");
         Arrays.stream(processedNumbers)
                 .forEach(number -> {
                     try {
@@ -44,6 +78,10 @@ public interface Validator {
                         throw new IllegalArgumentException(ErrorMessage.NUMBER_NOT_INTEGER.getMessage());
                     }
                 });
+    }
+
+    private static void validateDuplicateNumbers(String numbers) {
+        String[] processedNumbers = numbers.split(",");
         List<String> seen = new ArrayList<>();
         Arrays.stream(processedNumbers)
                 .forEach(number -> {
@@ -54,14 +92,9 @@ public interface Validator {
                 });
     }
 
-    static void validateBonusNumber(String bonusNumber) {
-        try {
-            int processedBonusNumber = Integer.parseInt(bonusNumber);
-            if (processedBonusNumber < 1 || processedBonusNumber > 45) {
-                throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_INVALID_RANGE.getMessage());
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_NOT_INTEGER.getMessage());
+    private static void validateMismatchBonusNumber(int processedBonusNumber) {
+        if (processedBonusNumber < 1 || processedBonusNumber > 45) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_INVALID_RANGE.getMessage());
         }
     }
 }

4df54686872b173d45df81cf8b6c70a58171de48
mung96 <mung5079@gmail.com>
2023-11-08 14:58:20 +0900

    test: decideResult 테스트 코드 작성
    1등,2등,3등,4등,5등,꽝 이 나오는 lotto를 생성후 모든 등수가 반환되는지 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,9 +4,11 @@ import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -26,4 +28,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("Game번호를 입력받아 등수를 결정한다")
+    @Test
+    void decideResult() {
+        //when
+        Game game = new Game(List.of(1, 2, 3, 4, 5, 6), 7);
+        List<Lotto> lottos = new ArrayList<Lotto>();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+        lottos.add(new Lotto(List.of(1, 2, 13, 14, 15, 16)));
+        //given
+        List<String> results = new ArrayList<String>();
+        lottos.stream()
+                .forEach(lotto->results.add(lotto.decideResult(game.answer, game.bonusNumber)));
+        //then
+        assertThat(results).contains("1등","2등","3등","4등","5등","꽝");
+    }
 }

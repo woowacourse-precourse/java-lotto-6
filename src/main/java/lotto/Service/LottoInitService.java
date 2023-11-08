27704a94b9362a3cbf45324ac5f8e4fbf9d09ed6
gemstoneyang <bennyws99@gmail.com>
2023-11-08 18:13:05 +0900

    feat: 당첨 번호 입력값을 리스트로 반환하는 기능 구현

--- src/main/java/lotto/Service/LottoInitService.java
+++ src/main/java/lotto/Service/LottoInitService.java
@@ -1,6 +1,9 @@
 package lotto.Service;
 
+import java.util.List;
+import lotto.Util.Splitter;
 import lotto.Validator.ValidatorFactory;
+import lotto.Validator.WinningNumberValidator;
 
 /**
  * 로또에 필요한 값들을 변환해주는 클래스
@@ -16,5 +19,20 @@ public class LottoInitService {
         String validatedInput = ValidatorFactory.forInputAmount().valid(input);
 
         return Integer.parseInt(validatedInput);
+    }
+
+    /**
+     * 당첨 번호를 검증하고 숫자 리스트로 반환
+     * @param input 사용자가 입력한 당첨 번호
+     * @return 정수로 변환된 당첨 번호가 담긴 리스트
+     */
+    public List<Integer> inputWinningNumberToList(String input) {
+        String validInput = WinningNumberValidator.vaildFormat(input);
+        List<String> splitInputs = Splitter.splitString(validInput, ",");
+        List<String> validInputs = ValidatorFactory.forInputWinningNumber().valid(splitInputs);
+
+        return validInputs.stream()
+                .map(Integer::parseInt)
+                .toList();
     }
 }
--- src/test/java/lotto/Service/LottoInitServiceTest.java
+++ src/test/java/lotto/Service/LottoInitServiceTest.java
@@ -3,9 +3,12 @@ package lotto.Service;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -26,6 +29,23 @@ public class LottoInitServiceTest {
     @ValueSource(strings = {"abc", "1213", "10000000000000000", "10000000", "1,000", "1 11"})
     void 잘못된_입력값은_예외를_반환한다(String input) {
         assertThatThrownBy(() -> service.inputAmountToNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 올바른_당첨번호를_리스트로_변환한다() {
+        String input = "1,3,5,7,9,11";
+
+        List<Integer> expected = Arrays.asList(1, 3, 5, 7, 9, 11);
+        List<Integer> actual = service.inputWinningNumberToList(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,3,5,7,9,11,", "a,1,2,3,4,5", "123", "99,99,99,99,99,99", "1,2,3,4,5,6,7"})
+    void 잘못된_당첨번호는_예외를_반환한다(String input) {
+        assertThatThrownBy(() -> service.inputWinningNumberToList(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

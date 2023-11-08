1439752b832746af1f7f86c861b38e67f4da94a5
enduf7686 <enduf768640@gmail.com>
2023-11-08 21:37:16 +0900

    test(computer): edit RandomLottoGeneratorTest

--- src/test/java/lotto/computer/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/computer/RandomLottoGeneratorTest.java
@@ -3,7 +3,9 @@ package lotto.computer;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import lotto.domain.computer.RandomLottoGenerator;
+import lotto.domain.lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -22,8 +24,10 @@ class RandomLottoGeneratorTest {
     @MethodSource("lotto.computer.MoneyArgumentsProvider#provideCorrectInput")
     void createUserLottosByCorrectInput(int money) {
         randomLottoGenerator.setMoney(money);
-        assertThat(randomLottoGenerator.createLottos().lottoList().size())
-                .isEqualTo(money / 1000);
+        List<Lotto> lottos = randomLottoGenerator.createLottos().lottoList();
+
+        assertThat(lottos.size()).isEqualTo(money / 1000);
+        lottos.forEach(lotto -> assertThat(lotto.getNumbers().size()).isEqualTo(6));
     }
 
     @DisplayName("RandomLottoGenerator 설정 시 로또 구입 금액이 설정한 범위 밖일 때 예외 테스트")

be4161935e1f61f6e58e123c12adbb72a4104b09
nhn1030 <shgusska12@gmail.com>
2023-11-08 20:40:10 +0900

    style: 불필요한 주석제거

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -3,11 +3,11 @@ package lotto.domain;
 
 public class Prize {
     public enum WinningPrize {
-        FIRST_PRIZE(1_000_000_000), // 1등 상금 (10억 원)
-        SECOND_PRIZE(50_000_000), // 2등 상금 (5천만 원)
-        THIRD_PRIZE(1_000_000), // 3등 상금 (1백만 원)
-        FOURTH_PRIZE(50_000), // 4등 상금 (5만 원)
-        FIFTH_PRIZE(5_000); // 5등 상금 (5천 원)
+        FIRST_PRIZE(1_000_000_000),
+        SECOND_PRIZE(50_000_000),
+        THIRD_PRIZE(1_000_000),
+        FOURTH_PRIZE(50_000),
+        FIFTH_PRIZE(5_000);
 
         private final int prizeAmount;
 

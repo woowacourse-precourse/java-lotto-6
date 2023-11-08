9f32e43a428df36aa529a588ac06553fec5ab7da
Leespect <wewqwew153@gmail.com>
2023-11-08 17:57:38 +0900

    Test : lotto Test코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.validate.WinNumberValidate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +19,17 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        int[] lottoNumbers = {1, 2, 3, 4, 5, 5};
+        assertThatThrownBy(() -> WinNumberValidate.validateWinNumberIsNotDuplicate(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+    @DisplayName("로또 번호와 보너스 번호가 중복되면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedBonusNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> WinNumberValidate.validateBonusNumberIsNotDuplicate(lotto, 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
 }

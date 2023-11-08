203c4b53d6e8d4968ce8b3e8af681763f6b0ef67
leemiyeon <lemingul7@gmail.com>
2023-11-08 21:50:07 +0900

    test: LottoTest 구현 1. 로또 생성 테스트 2. 당첨 번호 갯수 계산 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,16 @@
 package lotto;
 
+import java.util.Arrays;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +28,51 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 공백이 입력되면 예외가 발생한다.")
+    @Test
+    void createLottoByEmpty() {
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 일치하는 갯수를 정확히 계산한다.")
+    void countMatches_ReturnsCorrectCount() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = Arrays.asList(4, 5, 6, 7, 8, 9);
+
+        int matches = lotto.countMatches(winningNumbers);
+
+        assertEquals(3, matches);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 일치할 경우 true를 반환한다.")
+    void isBonusMatched_ReturnsTrueIfMatched() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 6;
+
+        boolean isMatched = lotto.isBonusMatched(bonusNumber);
+
+        assertTrue(isMatched);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 일치하지 않을 경우 false를 반환한다.")
+    void isBonusMatched_ReturnsFalseIfNotMatched() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        boolean isMatched = lotto.isBonusMatched(bonusNumber);
+
+        assertFalse(isMatched);
+    }
+
 }

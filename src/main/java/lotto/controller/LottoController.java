eec797b24823bc365a3366375044786e58d41564
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-08 19:47:33 +0900

    refactor: 변수 명 수정 및 다른 클래스에서 검증하도록 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,6 +3,8 @@ package lotto.controller;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
+import lotto.domain.LottoGame;
 import lotto.domain.LottoGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -13,22 +15,25 @@ public class LottoController {
 	private static List<Lotto> lottoNumbers = new ArrayList<>();
 
 	public void startGame() {
-		int amount = Integer.parseInt(InputView.inputAmountOfMoney());
+		int amount = LottoAmount.inputAmount();
 		OutputView.printLottoAmount(amount);
 
 		setLottoList(amount);
 		OutputView.printLottoNumbers(lottoNumbers);
 
-		List<Integer> lottoNumber = InputView.inputLottoNumber();
+		List<Integer> lottoPrizeNumber = InputView.inputLottoPrizeNumber();
 		int bonusNumber = InputView.inputBonusNumber();
+
+		LottoGame.playGame(lottoNumbers, lottoPrizeNumber, bonusNumber);
 
 		OutputView.printPrizeMessage();
 		OutputView.printPrizeResult();
 
 	}
 
+
 	private void setLottoList(int amount) {
-		for(int i = 0 ; i < LOTTO_SIZE; i ++){
+		for (int i = 0; i < amount; i++) {
 			List<Integer> lotto = new ArrayList<>();
 			lotto = LottoGenerator.makeLottoList();
 			lottoNumbers.add(new Lotto(lotto));

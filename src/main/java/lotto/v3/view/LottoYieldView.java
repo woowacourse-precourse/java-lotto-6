13c1671eada3002033509578250819253470b900
권장순 <jsoonworld@gmail.com>
2023-11-08 13:53:41 +0900

    feat( LottoYieldView ) : 수익률을 출력하는 메서드 구현.

---
+++ src/main/java/lotto/v3/view/LottoYieldView.java
@@ -0,0 +1,19 @@
+package lotto.v3.view;
+
+import lotto.v3.model.LottoRank;
+
+import java.util.Map;
+
+public class LottoYieldView {
+
+    public void displayResults(Map<LottoRank, Integer> matchCounts, double yield) {
+        matchCounts.forEach((rank, count) -> {
+            if (count > 0) {
+                System.out.println(rank.name() + " (" + rank.getPrizeMoney() + "원) - " + count + "개");
+            }
+        });
+
+        System.out.printf("총 수익률은 %.2f%%입니다.%n", yield);
+    }
+}
+

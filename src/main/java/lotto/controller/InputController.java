843a6a992397ce41addd81a01cf22719873e1147
dj <fjzl12@naver.com>
2023-11-08 16:55:39 +0900

    refactor(InputController): 클래스명 변경

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -1,23 +1,22 @@
-package lotto.domain;
+package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Console;
+import static lotto.constants.Constants.COST_UNIT;
+
+import lotto.ui.InputView;
+import lotto.ui.OutputView;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.CostValidator;
+import lotto.validator.PrizeNumbersValidator;
 
 import java.util.List;
 
-public class UserInput {
+public class InputController {
     private int amount;
     private int cost;
     private List<Integer> prizeNumbers;
     private int bonusNumber;
 
-    public UserInput() {
-    }
-
-    public int getCost() {
-        return cost;
-    }
-    public int getAmount() {
-        return amount;
+    public InputController() {
     }
 
     public List<Integer> getPrizeNumbers() {
@@ -28,36 +27,47 @@ public class UserInput {
         return bonusNumber;
     }
 
+    public int getCost() {
+        return cost;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
     public void setAmount() {
         try {
-            String cost = Console.readLine();
+            String cost = InputView.readCost();
             CostValidator costValidator = new CostValidator();
             costValidator.validateCost(cost);
             this.cost = costValidator.cost;
-            this.amount = this.cost / Constants.COST_UNIT;
+            this.amount = this.cost / COST_UNIT;
         } catch (IllegalArgumentException illegalArgumentException) {
+            OutputView.printError(illegalArgumentException.getMessage());
             setAmount();
         }
     }
 
     public void setPrizeNumbers() {
         try {
-            String numbers = Console.readLine();
+            String numbers = InputView.readNumbers();
             PrizeNumbersValidator prizeNumbersValidator = new PrizeNumbersValidator();
             prizeNumbersValidator.validate(numbers);
             prizeNumbers = prizeNumbersValidator.numbers;
         } catch (IllegalArgumentException illegalArgumentException) {
+            OutputView.printError(illegalArgumentException.getMessage());
             setPrizeNumbers();
         }
     }
 
     public void setBonusNumber() {
         try {
-            String bonusNumber = Console.readLine();
+            String bonusNumber = InputView.readBonusNumber();
             BonusNumberValidator bonusNumberValidator = new BonusNumberValidator(prizeNumbers);
             bonusNumberValidator.validate(bonusNumber);
             this.bonusNumber = bonusNumberValidator.bonusNumber;
         } catch (IllegalArgumentException illegalArgumentException) {
+            OutputView.printError(illegalArgumentException.getMessage());
             setBonusNumber();
         }
     }

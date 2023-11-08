38ef7d5b84bb4342f1c27a946ff52714c5912a0b
Lee Geun Woo <gamebdy@naver.com>
2023-11-08 22:46:54 +0900

    :sparkles: OutputView 의 당첨별 개수 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,16 @@ package lotto.view;
 
 import lotto.model.MyLottoNumber;
 import lotto.model.MyLottoNumbers;
+import java.util.List;
 
 import static lotto.util.Phrase.OutputPhrase.BUY_HOWMUCH;
+import static lotto.util.Phrase.DrawPhrase.DRAW_STATISTIC;
+import static lotto.util.Phrase.DrawPhrase.THREE_MATCHES;
+import static lotto.util.Phrase.DrawPhrase.FOUR_MATCHES;
+import static lotto.util.Phrase.DrawPhrase.FIVE_MATCHES;
+import static lotto.util.Phrase.DrawPhrase.FIVE_AND_BONUS_MATCHES;
+import static lotto.util.Phrase.DrawPhrase.SIX_MATCHES;
+import static lotto.util.Phrase.RevenuePhrase.TOTAL_REVENUE_RATE;
 
 public class OutputView {
 
@@ -16,7 +24,29 @@ public class OutputView {
         for(MyLottoNumber myLottoNumber : myLottoNumbers.getMyLottoNumbers()){
             System.out.println((myLottoNumber.toString()));
         }
+    }
+
+    public void printDrawResult(List<Integer> results){
+        int[] match = new int[7];
+        System.out.println((DRAW_STATISTIC.printMessage()));
+        for (int result : results) {
+            if(result%10 == 5){
+                result = 0;
+            }
+            match[result]++;
+        }
+
+            System.out.printf(THREE_MATCHES.printMessage(), match[3]);
+            System.out.printf(FOUR_MATCHES.printMessage(), match[4]);
+            System.out.printf(FIVE_MATCHES.printMessage(), match[5]);
+            System.out.printf(FIVE_AND_BONUS_MATCHES.printMessage(), match[0]);
+            System.out.printf(SIX_MATCHES.printMessage(), match[6]);
+
+    }
 
 
+    public void printRevenueResult(double totalRevenueRate){
+
+        System.out.printf(TOTAL_REVENUE_RATE.printMessage(), totalRevenueRate);
     }
 }

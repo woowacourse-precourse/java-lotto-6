6abc7ffc256bdbc74d0c7427916d5568770ddfce
wooni97 <jhw970714@gmail.com>
2023-11-08 20:22:56 +0900

    feat: 애플리케이션 로직 구현
    - 이 커밋은 main 함수에 애플리케이션 시작 로직을 추가합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController(new InputView(), new OutputView());
+        gameController.run();
     }
 }

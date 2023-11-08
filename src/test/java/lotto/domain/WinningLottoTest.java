6adc50a6159293cd9e4a2600901b306b81db3ffa
LeeSohyeon <teabag759@gmail.com>
2023-11-08 22:14:57 +0900

    fix : typo in exception error handling

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -12,12 +12,12 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
-  @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
-  @Test
-  void createWinningLottoByDuplicateBonusNumber() {
-      assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 5))
-              .isInstanceOf(IllegalArgumentException.class);
-  }
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicateBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 5))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("보너스 번호가 범위를 벗어나면 예외가 발생한다.")
     @ParameterizedTest

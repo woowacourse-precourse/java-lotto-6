bbbc4a7c6672f1905e128995f21de64372feb0b2
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 23:43:05 +0900

    refactor: edit hard coding
    final static 을 이용해 hard coding 제거

--- src/main/java/lotto/util/Validation.java
+++ src/main/java/lotto/util/Validation.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Validation {
     private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String NUMBER_FORMAT = " 입력값은 숫자여야 합니다.";
+    private static final String NO_BLANK = " 입력값은 빈칸이 될 수 없습니다.";
+    private static final String DIVISIBLE_PRICE = " 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.";
+    private static final String LOTTO_LENGTH = " 로또 번호는 6자리여야 합니다.";
+    private static final String LOTTO_DUPLICATED = " 로또 번호는 중복될 수 없습니다";
+    private static final String LOTTO_RANGE = " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     // 금액에 대한 validation
     public static void validatePrice(String input) {
@@ -15,19 +21,19 @@ public class Validation {
 
     private static void validateNumberFormat(String input) {
         if (!input.matches("[0-9]+")) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력값은 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + NUMBER_FORMAT);
         }
     }
 
     private static void validateEmptySpace(String input) {
         if (input.contains(" ")) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력값은 빈칸이 될 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + NO_BLANK);
         }
     }
 
     private static void validateDivisiblePrice(int input) {
         if (input % 1000 != 0) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + DIVISIBLE_PRICE);
         }
     }
 
@@ -41,19 +47,19 @@ public class Validation {
 
     private static void validateNumberLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 6자리여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_LENGTH);
         }
     }
 
     private static void validateNumberDuplicated(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 중복될 수 없습니다");
+            throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_DUPLICATED);
         }
     }
 
     private static void validateNumberRange(List<Integer> numbers) {
         if (isInvalid(numbers)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_RANGE);
         }
     }
 
@@ -70,20 +76,20 @@ public class Validation {
 
     private static void validateBonusNumberRange(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_RANGE);
         }
     }
 
     private static void validateNumberNotDuplicated(int bonusNumber, List<Integer> winningNumbers) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 보너스 번호는 로또 당첨 번호와 중복될 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + LOTTO_DUPLICATED);
         }
     }
 
     private static int validateBonusNumberFormat(String bonusNumberInput) {
         String numberInput = bonusNumberInput.trim();
         if (!numberInput.matches("[0-9]+")) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 보너스 번호는 숫자여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + NUMBER_FORMAT);
         }
         return Integer.parseInt(numberInput);
     }

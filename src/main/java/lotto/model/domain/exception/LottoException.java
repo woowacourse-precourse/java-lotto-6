9a9bc78e57c3574b799e100e3128e1ac3df256e5
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2023-11-08 19:02:22 +0900

    refactor: 로또 범위 예외 기능 수정

--- src/main/java/lotto/model/domain/exception/LottoException.java
+++ src/main/java/lotto/model/domain/exception/LottoException.java
@@ -1,21 +1,25 @@
 package lotto.model.domain.exception;
 
+import lotto.model.domain.constants.LottoGameConstants;
+
 public class LottoException extends Exception {
 
     private LottoException() {
 
     }
 
-    public static void rangeException(int min, int max) {
+    public static void rangeException() {
         throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 "
-                + min + "이상 " + max + "이하여야 합니다.");
+                + LottoGameConstants.MIN_NUMBER.getValue() + "이상 "
+                + LottoGameConstants.MAX_NUMBER.getValue()
+                + "이하여야 합니다.");
     }
 
     public static void duplicateException() {
         throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
 
-    public static void countException(int count) {
-        throw new IllegalArgumentException("[ERROR] 로또 번호는 " + count + "개여야 합니다.");
+    public static void countException() {
+        throw new IllegalArgumentException("[ERROR] 로또번호 형식이 맞지 않습니다.");
     }
 }

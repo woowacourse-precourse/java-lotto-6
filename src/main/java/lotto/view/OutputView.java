58bc0168dd9c21cc583974f2c169f32154058217
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-08 17:38:55 +0900

    feat : 구매갯수 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,9 @@
 package lotto.view;
 
 public class OutputView {
-    private static final String OUTPUT_ = "";
+    private static final String OUTPUT_PURCHASE_AMOUNT = "개를 구매했습니다.";
 
     public static void printPurchaseCount(int ticketCount) {
+        System.out.println(ticketCount + OUTPUT_PURCHASE_AMOUNT);
     }
 }

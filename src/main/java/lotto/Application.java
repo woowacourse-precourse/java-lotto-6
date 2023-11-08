d6134054200ca0099df909c0908e63643a269d25
sun7898 <jhs15767@gmail.com>
2023-11-08 21:37:34 +0900

    구입 금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+
+    public static int inputcost() {
+
+        System.out.println("구입 금액을 입력해 주세요");
+        int cost = Integer.parseInt(readLine());
+        return cost;
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    inputcost();
+
+
+
+
+
+
+
     }
 }

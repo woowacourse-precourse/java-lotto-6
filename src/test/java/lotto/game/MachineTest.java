ff93a4ccf954219628671db13bdc77df9ad545fc
DaeyeonCho <daecho8@gmail.com>
2023-11-08 16:31:37 +0900

    test: 머신 테스트 구현
    머신 클래스의 메소드들을 확인하기위한 테스트 코드를 작성하였습니다.

--- src/test/java/lotto/game/MachineTest.java
+++ src/test/java/lotto/game/MachineTest.java
@@ -9,7 +9,6 @@ import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MachineTest {
 

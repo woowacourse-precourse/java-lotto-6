654f98f0b41a811b38550375c51413758c3d4d25
eora21 <eora21@naver.com>
2023-11-08 10:05:37 +0900

    feat: 🎸 (ConsoleOutputView) 추가 당첨번호 요청 메서드 작성

--- src/main/java/view/ConsoleOutputView.java
+++ src/main/java/view/ConsoleOutputView.java
@@ -10,6 +10,7 @@ public class ConsoleOutputView implements OutputView {
     private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String PRINT_LOTTO_TICKETS_SIZE = "개를 구매했습니다.";
     private static final String PRINT_LOTTO_NUMBERS = "[%s]%n";
+    private static final String REQUEST_LOTTO_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     @Override
     public void requestPurchaseAmount() {
@@ -30,5 +31,10 @@ public class ConsoleOutputView implements OutputView {
                 .map(Object::toString)
                 .collect(Collectors.joining(", "));
         System.out.printf(PRINT_LOTTO_NUMBERS, numbersJoinComma);
+    }
+
+    @Override
+    public void requestLottoWinningNumbers() {
+        System.out.println(REQUEST_LOTTO_WINNING_NUMBERS);
     }
 }

165db0d85b7aa1b025234bbd81712817fb0ff845
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-08 23:31:01 +0900

    feat: 1000으로 나눠지지 않는 경우 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -190,6 +190,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException("8500");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});

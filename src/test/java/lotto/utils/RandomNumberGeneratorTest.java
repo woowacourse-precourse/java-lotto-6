c0b5210351c8163a8ae2c4347b803558158236cc
Jung Yunho <a01049048063@gmail.com>
2023-11-08 16:49:40 +0900

    test(RandomNumberGeneratorTest): 랜덤한 수의 길이가 맞는지

---
+++ src/test/java/lotto/utils/RandomNumberGeneratorTest.java
@@ -0,0 +1,25 @@
+package lotto.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("RandomNumberGenerator의")
+class RandomNumberGeneratorTest {
+
+    final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    @Test
+    @DisplayName("반환되는 숫자의 개수가 일치하는가")
+    void is_size_equal() {
+        //given & when
+        final Integer size = 6;
+        final List<Integer> randomNumbers = randomNumberGenerator.generateUniqueNumbers();
+
+        //then
+        assertThat(randomNumbers).hasSize(size);
+    }
+}

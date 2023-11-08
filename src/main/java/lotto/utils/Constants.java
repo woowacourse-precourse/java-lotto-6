bcb5712c5d6b8cc44198c5a644accdc7ca0c7a8e
hoyeon <highoption@naver.com>
2023-11-08 10:55:26 +0900

    feat(Constants) : 자주 사용하는 상수를 모음

---
+++ src/main/java/lotto/utils/Constants.java
@@ -0,0 +1,9 @@
+package lotto.utils;
+
+public final class Constants {
+    public final static int START_LOTTO_NUMBER = 1;
+    public final static int END_LOTTO_NUMBER = 45;
+    public final static String INTEGER_RANGE = "^[0-9]+$";
+    public final static int LOTTO_SIZE = 6;
+    public final static int ONE_LOTTO_PRICE = 1000;
+}

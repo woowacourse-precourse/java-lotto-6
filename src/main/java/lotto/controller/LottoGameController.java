28482d97b1fb71e413541210b3a71cda13da059f
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:44:24 +0900

    refactor(LottoGameController): 불필요한 필드 제거
    LottoGameService에서 더이상 InputView가 필요하지 않기 때문에 컨트롤러에서도 InputView가 불필요한 객체가 되므로 삭제한다

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -5,24 +5,22 @@ import lotto.model.WinningDetail;
 import lotto.model.WinningNumbers;
 import lotto.service.LottoGameService;
 import lotto.util.RandomUtil;
-import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
     private final LottoGameService lottoGameService;
-    private final InputView inputView;
     private final OutputView outputView;
     private final RandomUtil randomUtil;
 
     public LottoGameController() {
-        inputView = new InputView();
         outputView = new OutputView();
         randomUtil = new RandomUtil();
-        lottoGameService = new LottoGameService(inputView, outputView, randomUtil);
+        lottoGameService = new LottoGameService(outputView, randomUtil);
     }
 
     public void startGame() {
         final Player player = lottoGameService.createPlayer();
+        outputView.printLineOfSpace();
         outputView.printLottoInfo(player.getLottos());
         outputView.printLineOfSpace();
         WinningNumbers winningNumbers = lottoGameService.createWinningNumber();

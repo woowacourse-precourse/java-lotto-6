d1910b2f6419466102735396e0c687a9cce42cd7
shapeofmy <114326457+rudtn409@users.noreply.github.com>
2023-11-08 14:23:45 +0900

    금액, 번호 예외처리 및 테스트

--- src/main/java/lotto/LottoNumberInput.java
+++ src/main/java/lotto/LottoNumberInput.java
@@ -12,28 +12,36 @@ public class LottoNumberInput {
     int bonusNum;
     public void run (){
         moneyInput();
-        //numberInput();
+        numberInput();
         bonusInput();
-        Lotto lotto = new Lotto(numbers);
+        //Lotto lotto = new Lotto(numbers);
     }
 
     private void bonusInput() {
         System.out.println("보너스 번호를 입력해 주세요.");
         bonusNum = Integer.parseInt(Console.readLine());
-        if(bonusNum<1 || bonusNum >25){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        try {
+            if (bonusNum < 1 || bonusNum > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            numbers.add(bonusNum);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("[ERROR] 로또 번호는 숫자여야 합니다.");
         }
-
     }
 
-    private void moneyInput(){
+    public void moneyInput(/*int testNum*/){
         System.out.println("구입금액을 입력해 주세요.?");
         countNum = Integer.parseInt(Console.readLine());
+        if(countNum%1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력해 주세요");
+        }
     }
 
-    public void numberInput(String testnum){
+    public void numberInput(/*String testnum*/){
         System.out.println("당첨 번호를 입력해 주세요.");
-        String num = testnum;
+        //String num = testnum;
+        String num = Console.readLine();
         for (String i : num.split(",")) {
             try {
                 int number = Integer.parseInt(i);
@@ -45,17 +53,17 @@ public class LottoNumberInput {
                     throw new NumberFormatException("[ERROR] 로또 번호는 숫자여야 합니다.");
             }
         }
-        //hasduplication();
+        validate();
     }
 
-    public void validate(List<Integer> testNumbers){
+    public void validate(/*List<Integer> testNumbers*/){
         Set<Integer> uniqueNumbers = new HashSet<>();
-        for( int number : testNumbers){
+        for( int number : numbers){
             if(!uniqueNumbers.add(number)) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
             }
         }
-        if (testNumbers.size() != 6) {
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,21 +8,15 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    // 아래에 추가 테스트 작성 가능
+
+    /*@DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new LottoNumberInput().validate(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    /*@DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-*/
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
@@ -30,7 +24,6 @@ class LottoTest {
         assertThatThrownBy(() -> new LottoNumberInput().validate(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-    // 아래에 추가 테스트 작성 가능
 
     @DisplayName("로또 번호의 입력이 숫자가 아닐 경우 예외가 발생한다.")
     @Test
@@ -39,4 +32,11 @@ class LottoTest {
         assertThatThrownBy(() -> lottoNumberInput.numberInput("a,b,1,d,e,f"))
                 .isInstanceOf(NumberFormatException.class);
     }
+
+    @DisplayName("금액 테스트")
+    @Test
+    void createMoneyByZeroRemainder() {
+        assertThatThrownBy(() -> new LottoNumberInput().moneyInput(14222))
+                .isInstanceOf(IllegalArgumentException.class);
+    }*/
 }

c8ef8fa3ec5ac6123808e09c6b783e20387c811c
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-08 17:34:38 +0900

    feat : 출력 오류 수정

--- src/main/java/lotto/OutputManagaer.java
+++ src/main/java/lotto/OutputManagaer.java
@@ -11,7 +11,7 @@ public class OutputManagaer {
     }
 
     public void outputOwnNumbers(List<Lotto> lottos) {
-        System.out.printf("%d개를 구매했습니다.", lottos.size());
+        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
 
         for (Lotto lotto : lottos) {
             List<Integer> numbers = lotto.getNumbers();
@@ -26,11 +26,11 @@ public class OutputManagaer {
     public void outputResult(List<Prize> lottoResults,int totalReward) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) - %d개", Collections.frequency(lottoResults, LottoEnum.Prize.FIFTH));
-        System.out.printf("4개 일치 (50,000원) - %d개", Collections.frequency(lottoResults, LottoEnum.Prize.FOURTH));
-        System.out.printf("5개 일치 (1,500,000원) - %d개", Collections.frequency(lottoResults, LottoEnum.Prize.THIRD));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", Collections.frequency(lottoResults, LottoEnum.Prize.SECOND));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개", Collections.frequency(lottoResults, LottoEnum.Prize.FIRST));
-        System.out.printf("총 수익률은 %f.1 %%입니다.", (((float)totalReward) / 1000 / lottoResults.size() * 100));
+        System.out.printf("3개 일치 (5,000원) - %d개\n", Collections.frequency(lottoResults, LottoEnum.Prize.FIFTH));
+        System.out.printf("4개 일치 (50,000원) - %d개\n", Collections.frequency(lottoResults, LottoEnum.Prize.FOURTH));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", Collections.frequency(lottoResults, LottoEnum.Prize.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", Collections.frequency(lottoResults, LottoEnum.Prize.SECOND));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", Collections.frequency(lottoResults, LottoEnum.Prize.FIRST));
+        System.out.printf("총 수익률은 %f.1 %%입니다.\n", (((float)totalReward) / 1000 / lottoResults.size() * 100));
     }
 }

29c12a1ef3702f9fedb8a18b3ff965e3b695d0a9
Gukin Han <gukin.dev@gmail.com>
2023-11-08 17:53:01 +0900

    feat(WinningNumbers): 번호 범위(1-45) 검증 로직 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 import static lotto.message.Error.LOTTO_NUMBER_DUPLICATE;
+import static lotto.message.Error.LOTTO_NUMBER_OUT_OF_RANGE;
 
 public class WinningNumbers extends Lotto {
     private final int bonusNumber;
@@ -17,6 +18,10 @@ public class WinningNumbers extends Lotto {
     private void validateDuplicateBonusNumber(List<Integer> numbers, int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE.getMessage());
+        }
+
+        if (isNotValidRange(bonusNumber)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
         }
     }
 

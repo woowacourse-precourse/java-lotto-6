aee44c3304f468e1f2988dc1d27eff6eaea21630
leesh <qwqw8019@naver.com>
2023-11-08 18:36:28 +0900

    refactor: 초기 당첨 결과 상수화

--- src/main/java/lotto/dto/WinningResult.java
+++ src/main/java/lotto/dto/WinningResult.java
@@ -5,10 +5,12 @@ import java.util.LinkedHashMap;
 import lotto.domain.Prize;
 
 public record WinningResult(LinkedHashMap<Prize, Integer> prizes) {
+    public static final int INITIAL_WINNING_RESULT = 0;
+
     public static WinningResult of(Prize... prizes) {
         WinningResult result = new WinningResult(new LinkedHashMap<>());
         Arrays.stream(prizes)
-                .forEach(prize -> result.prizes.put(prize, 0));
+                .forEach(prize -> result.prizes.put(prize, INITIAL_WINNING_RESULT));
         return result;
     }
 }

d429e569a75eada97690efe11729fa8e5cd37d18
leorb159 <ldgsocial159@naver.com>
2023-11-08 16:42:25 +0900

    feat: LottoRank 생성, 랭킹 및 상금 작성

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public enum LottoRank {
+    RANK_1(6, false, 2_000_000_000L),
+    RANK_2(5, true, 30_000_000L),
+    RANK_3(5, false, 1_500_000L),
+    RANK_4(4, false, 50_000L),
+    RANK_5(3, false, 5_000L),
+    UNRANKED(0, false, 0L);
+
+    private final int matchCount;
+    private final boolean needBonusNumber;
+    private final long prize;
+
+    LottoRank(int matchCount, boolean needBonusNumber, long prize) {
+        this.matchCount = matchCount;
+        this.needBonusNumber = needBonusNumber;
+        this.prize = prize;
+    }
+}

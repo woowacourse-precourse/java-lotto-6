dc2cd6c237dac288cae2a3ee0a5f2c69765b982a
FL <refrain2k@gmail.com>
2023-11-08 17:54:40 +0900

    feat: 로또게임 메인화면 구현

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import lotto.domain.Lotteries;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoRank;
+import lotto.util.InputInitializer;
+import lotto.util.OutputView;
+
+import java.util.EnumMap;
+import java.util.List;
+
+public class LottoController {
+    public void play() {
+        InputInitializer inputInitializer = new InputInitializer();
+        OutputView outputView = new OutputView();
+
+        int purchaseAmount = inputInitializer.inputtingPurchaseAmount();
+        List<Lotto> userLotteries = new Lotteries().BuyLotteries(purchaseAmount);
+        outputView.printLotteries(purchaseAmount, userLotteries);
+
+        Lotto winningLotto = inputInitializer.inputtingLottoNumber();
+        int bonusNumber = inputInitializer.inputBonusNumber(winningLotto);
+        LottoMachine lottoMachine = new LottoMachine(winningLotto, bonusNumber);
+        EnumMap<LottoRank, Integer> lottoResultMap = lottoMachine.toLottoResult(userLotteries);
+        outputView.printLottoResult(lottoResultMap);
+        outputView.printLottoEarningRate(purchaseAmount, lottoResultMap);
+
+    }
+}

cdcc9c5033770f94e22cec2bf45471ca2837e1db
gywns0417 <gywns0417@gmail.com>
2023-11-08 17:40:08 +0900

    fix: 문자열 포매팅 되지 않은 값 출력 문제 수정
    getter가 적용되지 않은 값들에 getXxx() 메서드를 추가했습니다.

--- src/main/java/lotto/config/WinningLottoErrorMessage.java
+++ src/main/java/lotto/config/WinningLottoErrorMessage.java
@@ -6,7 +6,7 @@ import static lotto.config.LottoConfig.LOTTO_NUMBER_MIN;
 public enum WinningLottoErrorMessage {
     WINNING_LOTTO_NUMERIC_ERROR_MESSAGE(
             String.format("[ERROR] %s부터 %s까지의 자연수와 콤마만 입력할 수 있습니다.",
-                    LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX)),
+                    LOTTO_NUMBER_MIN.getValue(), LOTTO_NUMBER_MAX.getValue())),
     WINNING_LOTTO_COMMA_ERROR_MESSAGE(
             "[ERROR] 첫 번째 또는 마지막 문자의 콤마 또는 연속된 콤마는 입력할 수 없습니다."),
     WINNING_LOTTO_UNIQUE_ERROR_MESSAGE("[ERROR] 중복된 번호는 입력할 수 없습니다.");

053d48f212714dacface351be8f8dd4dcac504f8
mseo39 <a01063294905@gmail.com>
2023-11-08 15:21:41 +0900

    FIX : 당첨반호 검사하는 기능 에러문구 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -19,20 +19,20 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 6자리가 아닙니다. 다시 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 6자리이어야 합니다. 다시 입력해주세요.");
         }
     }
 
     private void validateDuplication(List<Integer> numbers) {
         if (numbers.stream().distinct().collect(Collectors.toList()).size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 중복되는 값이 있습니다. 다시 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 중복되는 값이 없어야합니다. 다시 입력해주세요.");
         }
     }
 
     private void validateRange(int start, int end, List<Integer> numbers) {
         for (int number : numbers) {
             if (number < 1 && number > 45) {
-                throw new IllegalArgumentException("[ERROR] 범위가 없어나는 값이 있습니다(1<=값<=45). 다시 입력해주세요.");
+                throw new IllegalArgumentException("[ERROR] 범위가 벗어나는 값이 있으면 안됩니다(1<=값<=45). 다시 입력해주세요.");
             }
         }
     }

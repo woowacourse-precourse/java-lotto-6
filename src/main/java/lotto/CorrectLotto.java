8fe53afbdb6ce9f67dfbb066b0c6af00b4aeea5d
gyeommm <dkndob02@naver.com>
2023-11-08 15:34:52 +0900

    함수명 수정

--- src/main/java/lotto/CorrectLotto.java
+++ src/main/java/lotto/CorrectLotto.java
@@ -1,9 +1,7 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -32,7 +30,7 @@ public class CorrectLotto {
     }
 
     // 입력값이 숫자인지 확인하여 변환
-    private int IsInteger(String number){
+    private int convertToInt(String number){
         try {
             return Integer.parseInt(number);
         } catch (NumberFormatException e){
@@ -46,7 +44,7 @@ public class CorrectLotto {
 
         List<Integer> converted_numbers = new ArrayList<>();
         for(String number : numbers){
-            int converted_number = IsInteger(number.strip());
+            int converted_number = convertToInt(number.strip());
             converted_numbers.add(converted_number);
         }
 
@@ -70,14 +68,14 @@ public class CorrectLotto {
     }
 
     // 보너스 번호가 [1, 45] 구간에 있는지 확인
-    private void IsNumberIn1to45(int number){
+    private void checkNumberIn1to45(int number){
         if(number < 1 || number > 45){
             throw new IllegalArgumentException("보너스 번호는 1~45 사이의 숫자여야 합니다.");
         }
     }
 
     // 보너스 번호가 당첨 번호와 중복되는지 확인
-    private void IsNumberDuplicate(int number){
+    private void checkNumberDuplicate(int number){
         for(Integer correct_number : correct_lotto.getNumbers()){
             if(correct_number == number){
                 throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
@@ -89,9 +87,9 @@ public class CorrectLotto {
         while(true){
             String input_number = readLine();
             try {
-                int converted_number = IsInteger(input_number.strip());
-                IsNumberIn1to45(converted_number);
-                IsNumberDuplicate(converted_number);
+                int converted_number = convertToInt(input_number.strip());
+                checkNumberIn1to45(converted_number);
+                checkNumberDuplicate(converted_number);
                 bonus = converted_number;
                 break;
             } catch(IllegalArgumentException e){
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,14 +17,14 @@ public class Lotto {
     }
 
     // 번호가 6개인지 확인
-    private void IsSize6(List<Integer> numbers){
+    private void checkSize6(List<Integer> numbers){
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("번호는 무조건 6개여야 합니다.");
         }
     }
 
     // 번호가 모두 [1, 45] 구간에 있는지 확인
-    private void IsNumbersIn1to45(List<Integer> numbers){
+    private void checkNumbersIn1to45(List<Integer> numbers){
         for(Integer number : numbers){
             if(number < 1 || number > 45){
                 throw new IllegalArgumentException("번호는 1~45 사이의 숫자여야 합니다.");
@@ -33,7 +33,7 @@ public class Lotto {
     }
 
     // 번호가 중복되는지 확인
-    private void IsNumbersDuplicate(List<Integer> numbers){
+    private void checkNumbersDuplicate(List<Integer> numbers){
         Set<Integer> unique_numbers = new HashSet<>();
         for(Integer number : numbers){
             if(unique_numbers.contains(number)){
@@ -44,9 +44,9 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        IsSize6(numbers);
-        IsNumbersIn1to45(numbers);
-        IsNumbersDuplicate(numbers);
+        checkSize6(numbers);
+        checkNumbersIn1to45(numbers);
+        checkNumbersDuplicate(numbers);
     }
 
     // TODO: 추가 기능 구현

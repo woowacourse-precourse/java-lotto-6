4a858e9acc49439dc0bcf2b0bea2927370741803
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:36:30 +0900

    feat : 처리된 값에 대한 결과와 안내 문구를 출력하는 객체 생성

--- src/main/java/lotto/view/MessageOutputView.java
+++ src/main/java/lotto/view/MessageOutputView.java
@@ -5,4 +5,12 @@ public class MessageOutputView {
 	public void printScanningLottoPurchaseMoneyMessage() {
 		System.out.println("구입금액을 입력해 주세요.");
 	}
+
+	public void printScanningLottoNumberMessage() {
+		System.out.println("당첨 번호를 입력해 주세요.");
+	}
+
+	public void printScanningLottoBonusNumberMessage() {
+		System.out.println("보너스 번호를 입력해 주세요.");
+	}
 }

cddbe97e86444436539b0e77c579eec764516f62
wook <dong930112@gmail.com>
2023-11-08 18:29:55 +0900

    refactor: OutputView 내부로직 개선 -상수추가하여 매직리터럴 제거 -프린트문 양식에 맞게 포맷팅

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,16 @@
 package lotto.view;
 
+import java.util.EnumMap;
 import java.util.List;
-import lotto.model.Lotto;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 public class OutputView {
 
     private static final String PURCHASE_INFORMATION = "개를 구매했습니다.";
+    private static final String RANK_INFORMATION = "%d개 일치 (%,d원) - %d개 %n";
+    private static final String SECOND_RANK_INFORMATION = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개 %n";
+    private static final String RATE_OF_RETURN = "총 수익률은 %,.1f%%입니다.";
 
     //발행한 로또 수량 및 번호를 출력하는 기능
     public void lottoTickets(List<Lotto> lottos) {
@@ -15,23 +20,27 @@ public class OutputView {
         for(int i=0; i<lottos.size(); i++) {
             System.out.println(lottos.get(i).getNumbers());
         }
-
-
     }
 
     //당첨통계를 출력하는 기능
-    public void winningRecords(List<Integer> rankResult) {
+    public void winningRecords(EnumMap<Rank, Integer> winningStatics) {
+        Rank[] ranks = Rank.values();
 
-        System.out.printf("3개 일치 (5,000원) - %d개 %n", rankResult.get(4));
-        System.out.printf("4개 일치 (50,000원) - %d개 %n", rankResult.get(3));
-        System.out.printf("5개 일치 (1,500,000원) - %d개 %n", rankResult.get(2));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개 %n", rankResult.get(1));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개 %n", rankResult.get(0));
+        for (Rank rank : ranks) {
+            int matchingNum = rank.getMatchingNum();
+            int prize = rank.getPrize();
+            int count = winningStatics.get(rank);
+
+            if( rank != Rank.SECOND_RANK) {
+                System.out.printf(RANK_INFORMATION, matchingNum, prize, count);
+            }
+            System.out.printf(SECOND_RANK_INFORMATION, matchingNum, prize, count);
+        }
     }
 
     //상금 수익율을 보여주는 기능
     public void prizesSummary(float rateOfReturn) {
-        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
+        System.out.printf(RATE_OF_RETURN, rateOfReturn);
     }
 
 }

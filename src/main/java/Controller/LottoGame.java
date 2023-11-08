143d2f8ab8f99ae14d9424cb3139688ab5d6fea4
minoooo119 <minoth0119@naver.com>
2023-11-08 18:03:10 +0900

    [FIX] LottoGame.java, Application.java: userInput 받고 로또 구매 모델 처리

--- src/main/java/Controller/LottoGame.java
+++ src/main/java/Controller/LottoGame.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.PurchaseLotto;
 import View.UserInput;
 import lotto.Lotto;
 
@@ -13,9 +14,13 @@ public class LottoGame {
     private static List<Integer> winningLottoNum;
     private static Integer winningBonusNum;
     public void gameStart(){
-        //1. User에게 입력을 받는다.
+        //1. User에게 구매 가격 입력을 받는다.
         Integer purchaseAmount= userInput.getPurchaseAmount();
+        // User가 구입한 장수, 로또 보여준다.
+        PurchaseLotto userPurchaseLottos=new PurchaseLotto(purchaseAmount);
+        //당첨 번호 6개를 입력받는다.
         Lotto lotto =userInput.getWinningNum();
+        //보너스 번호 1개를 입력받는다.
         winningBonusNum= userInput.getBonusNum();
     }
 }
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import Controller.LottoGame;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        LottoGame lottoGame=new LottoGame();
+        lottoGame.gameStart();
     }
 }

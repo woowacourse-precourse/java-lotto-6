0b878ab8dc9f640c848fd34bede4e706beb48857
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 13:15:18 +0900

    feat: 사용자의 금액 입력 기능 및 검증 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,5 +5,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        User user = new User();
+        int money = user.inputPurchaseAmount();
     }
 }

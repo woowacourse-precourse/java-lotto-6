5b299702684eb9b96110dfc82ff4bfbe77bd3c47
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 17:25:09 +0900

    feat: 수익률 계산 기능 구현

--- src/main/java/lotto/util/Statistics.java
+++ src/main/java/lotto/util/Statistics.java
@@ -1,4 +1,30 @@
 package lotto.util;
 
+import static lotto.config.LottoConfig.LOTTO_PRICE;
+import static lotto.config.WiningRank.RANK1;
+import static lotto.config.WiningRank.RANK2;
+import static lotto.config.WiningRank.RANK3;
+import static lotto.config.WiningRank.RANK4;
+import static lotto.config.WiningRank.RANK5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lotto.config.WiningRank;
+import lotto.domain.RankBoard;
+
 public class Statistics {
+    public double calculateRateOfReturn(int buyCount, RankBoard board) {
+        int earnMoneyResult = 0;
+        int spanMoneyResult = LOTTO_PRICE.getNum() * buyCount;
+
+        List<WiningRank> rankSequence = new ArrayList<>(Arrays.asList(RANK1, RANK2, RANK3, RANK4, RANK5));
+        for (WiningRank rank : rankSequence) {
+            earnMoneyResult = earnMoneyResult + (rank.getPrice() * board.getRankCount(rank));
+        }
+
+        double temp = ((double) earnMoneyResult / spanMoneyResult) * 100;
+
+        return Math.round(temp * 10) / 10.0;
+    }
 }

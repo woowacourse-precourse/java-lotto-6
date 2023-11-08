543047689ac93a6fd42a4d4a61a8d4f4e05a14c6
gusgh075 <gusgh075@naver.com>
2023-11-08 11:26:30 +0900

    TEST : 주어진 개수에 맞춰 로또를 생성하는지 확인하는 코드

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -8,8 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class LottoGeneratorTest {
     @DisplayName("1~45 사이의 6개 숫자로 로또를 생성한다.")
     @Test
-    void createLottoInRange(){
+    void createLottoInRange() {
         assertTrue(LottoGenerator.generateRandomLotto().getNumbers().
-                stream().allMatch(n->n>=1&&n<=45));
+                stream().allMatch(n -> n >= 1 && n <= 45));
+    }
+
+    @DisplayName("주어진 개수의 로또를 생성한다.")
+    @Test
+    void createLottos() {
+        assertEquals(6, LottoGenerator.generateLottos(6).size());
     }
 }

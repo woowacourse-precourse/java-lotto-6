4fba35ea9eb512c03a163d466f5c5f1a529a587d
AidenRoh <colohrby@gmail.com>
2023-11-08 18:20:46 +0900

    chore: change error title

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -6,7 +6,7 @@ public enum ErrorMessage {
 	INVALID_AMOUNT("금액은 1000원 단위로 설정해야 합니다."),
 	INVALID_MONEY("금액은 음수가 될 수 없습니다."),
 	STRING_NOT_ALLOWED("숫자만 입력 가능합니다."),
-	INVALID_WINNING_NUMBERS_VALUE("당첨 번호는 쉼표(,)로 구분된 숫자 조합여야합니다."),
+	WINNING_NUMBERS_FORMAT("당첨 번호는 쉼표(,)로 구분된 숫자 조합여야합니다."),
 	INVALID_WINNING_NUMBERS_SIZE("당첨 번호는 6개의 숫자여야 합니다."),
 	INVALID_BONUS_NUMBER("보너스 번호는 당첨 번호와 겹칠 수 없습니다."),
 	DUPLICATED_NUMBER("중복된 번호가 있습니다.");

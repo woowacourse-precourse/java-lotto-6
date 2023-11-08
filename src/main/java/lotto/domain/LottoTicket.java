98db8a8fb9c420a02f30f9ca4208edf9560b2f5e
COBI-98 <tkdgus968@naver.com>
2023-11-08 17:55:55 +0900

    feat: 로또 번호들을 담고있는 티켓(용지) 일급컬렉션 구현

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.util.GenerateRandomLottoNumbers;
+import lotto.util.RandomUtils;
+
+public record LottoTicket(List<LottoNumbers> lottoTicket) {
+
+}

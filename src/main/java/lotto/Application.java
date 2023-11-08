f72dfc9af764014a9bdf5a62e07b7c4219bab8ed
jjongwon7 <jjongwon7@naver.com>
2023-11-08 21:10:25 +0900

    feat: 로또 구입 금액 입력 문구 출력 기능 추가 출력을 담당하는 UserOutput 클래스를 만들고, 로또 구입 금액 입력 안내 문구를 출력하는 메서드 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import static lotto.views.UserOutput.askPurchasePrice;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        askPurchasePrice();
     }
 }
---
+++ src/main/java/lotto/views/UserOutput.java
@@ -0,0 +1,7 @@
+package lotto.views;
+
+public class UserOutput {
+    public static void askPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}

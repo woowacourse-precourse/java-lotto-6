2462ea45c7475bc9fa08841435c6dabe10f91c6c
Junho Lee <ghzm888@gmail.com>
2023-11-08 19:56:01 +0900

    refactor : 직관적으로 보이도록 stream() 코드로 수정

--- src/main/java/lotto/domain/LottoManage.java
+++ src/main/java/lotto/domain/LottoManage.java
@@ -1,22 +1,21 @@
 package lotto.domain;
 
 public class LottoManage {
-    private int correctCount = 0;
-    private int correctBonusCount = 0;
+    private int correctCount;
+    private int correctBonusCount;
 
     public void getWinningCount(Lotto inputLotto, BonusNumber bonusNumber, RandomLottoList randomLottoList) {
         increaseCount(inputLotto, bonusNumber, randomLottoList);
     }
 
     private void increaseCount(Lotto inputLotto, BonusNumber bonusNumber, RandomLottoList randomLottoList) {
-        for (int inputNumber : inputLotto.getNumbers()) {
-            if (randomLottoList.getRandomLottoList().contains(inputNumber)) {
-                correctCount++;
-            }
-        }
-        if (randomLottoList.getRandomLottoList().contains(bonusNumber.getBonusNumber())) {
-            correctBonusCount++;
-        }
+        correctCount = (int) inputLotto.getNumbers().stream()
+                .filter(inputNumber -> randomLottoList.getRandomLottoList().contains(inputNumber))
+                .count();
+
+        correctBonusCount = (int) randomLottoList.getRandomLottoList().stream()
+                .filter(number -> number == bonusNumber.getBonusNumber())
+                .count();
     }
 
     public int getCorrectCount() {

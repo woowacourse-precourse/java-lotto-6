a4fe6f1014c83603f8c87431f45cb756fa44d378
m2nseop <pms246@naver.com>
2023-11-08 23:17:25 +0900

    refactor: private함수 테스트코드 삭제

--- src/test/java/lotto/DomainTest/LottoStoreTest.java
+++ src/test/java/lotto/DomainTest/LottoStoreTest.java
@@ -35,33 +35,34 @@ public class LottoStoreTest {
         assertThat(lottoStore.caculateTotalLottoReward(input)).isEqualTo(expected);
     }
 
-    @DisplayName("존재하는 당첨 종류인가?")
-    @Test
-    void isExistingWinningTypeTest(){
-        Map<String, Integer> resultNull = new HashMap<>();
-        Map<String, Integer> resultThreeMatch = new HashMap<>();
-        Map<String, Integer> resultFourMatch = new HashMap<>();
-        Map<String, Integer> resultFiveMatch = new HashMap<>();
-        Map<String, Integer> resultFiveAndBonusMatch = new HashMap<>();
-        Map<String, Integer> resultSixMatch = new HashMap<>();
-        resultNull.put("2", 1);
-        resultThreeMatch.put("3", 1);
-        resultFourMatch.put("4", 1);
-        resultFiveMatch.put("5", 1);
-        resultFiveAndBonusMatch.put("6", 1);
-        resultSixMatch.put("7", 1);
-
-        WinningStatistics.values()[0].getWinningType(); // return = "3" -> 3개 일치
-        WinningStatistics.values()[1].getWinningType(); // return = "4" -> 4개 일치
-        WinningStatistics.values()[2].getWinningType(); // return = "5" -> 5개 일치
-        WinningStatistics.values()[3].getWinningType(); // return = "6" -> 5개 + 보너스 일치
-        WinningStatistics.values()[4].getWinningType(); // return = "7" -> 6개 일치
-
-        assertThat(lottoStore.isExistingWinningType(resultNull, WinningStatistics.values()[0])).isEqualTo(false); // 2개 일치는 존재 x
-        assertThat(lottoStore.isExistingWinningType(resultThreeMatch, WinningStatistics.values()[0])).isEqualTo(true); // 3개 일치
-        assertThat(lottoStore.isExistingWinningType(resultFourMatch, WinningStatistics.values()[1])).isEqualTo(true); // 4개 일치
-        assertThat(lottoStore.isExistingWinningType(resultFiveMatch, WinningStatistics.values()[2])).isEqualTo(true); // 5개 일치
-        assertThat(lottoStore.isExistingWinningType(resultFiveAndBonusMatch, WinningStatistics.values()[3])).isEqualTo(true); // 5개 + 보너스 일치
-        assertThat(lottoStore.isExistingWinningType(resultSixMatch, WinningStatistics.values()[4])).isEqualTo(true); // 6개 일치
-    }
+    // private 함수이므로 사용하지 않는다.
+//    @DisplayName("존재하는 당첨 종류인가?")
+//    @Test
+//    void isExistingWinningTypeTest(){
+//        Map<String, Integer> resultNull = new HashMap<>();
+//        Map<String, Integer> resultThreeMatch = new HashMap<>();
+//        Map<String, Integer> resultFourMatch = new HashMap<>();
+//        Map<String, Integer> resultFiveMatch = new HashMap<>();
+//        Map<String, Integer> resultFiveAndBonusMatch = new HashMap<>();
+//        Map<String, Integer> resultSixMatch = new HashMap<>();
+//        resultNull.put("2", 1);
+//        resultThreeMatch.put("3", 1);
+//        resultFourMatch.put("4", 1);
+//        resultFiveMatch.put("5", 1);
+//        resultFiveAndBonusMatch.put("6", 1);
+//        resultSixMatch.put("7", 1);
+//
+//        WinningStatistics.values()[0].getWinningType(); // return = "3" -> 3개 일치
+//        WinningStatistics.values()[1].getWinningType(); // return = "4" -> 4개 일치
+//        WinningStatistics.values()[2].getWinningType(); // return = "5" -> 5개 일치
+//        WinningStatistics.values()[3].getWinningType(); // return = "6" -> 5개 + 보너스 일치
+//        WinningStatistics.values()[4].getWinningType(); // return = "7" -> 6개 일치
+//
+//        assertThat(lottoStore.isExistingWinningType(resultNull, WinningStatistics.values()[0])).isEqualTo(false); // 2개 일치는 존재 x
+//        assertThat(lottoStore.isExistingWinningType(resultThreeMatch, WinningStatistics.values()[0])).isEqualTo(true); // 3개 일치
+//        assertThat(lottoStore.isExistingWinningType(resultFourMatch, WinningStatistics.values()[1])).isEqualTo(true); // 4개 일치
+//        assertThat(lottoStore.isExistingWinningType(resultFiveMatch, WinningStatistics.values()[2])).isEqualTo(true); // 5개 일치
+//        assertThat(lottoStore.isExistingWinningType(resultFiveAndBonusMatch, WinningStatistics.values()[3])).isEqualTo(true); // 5개 + 보너스 일치
+//        assertThat(lottoStore.isExistingWinningType(resultSixMatch, WinningStatistics.values()[4])).isEqualTo(true); // 6개 일치
+//    }
 }

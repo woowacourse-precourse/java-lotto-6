9616897633d171c3537348c7dc32cdbdff35db84
doka <posmane5@gmail.com>
2023-11-08 20:19:41 +0900

    feat(Instruction) : 당첨 통계를 제목 출력하는 메시지 추가

--- src/main/java/lotto/view/constant/Instructions.java
+++ src/main/java/lotto/view/constant/Instructions.java
@@ -4,8 +4,9 @@ public enum Instructions {
     MESSAGE_OF_ENTERING_LOTTO_AMOUNT("구입금액을 입력해 주세요."),
 
     MESSAGE_OF_PURCHASE_COUNT("\n%d개를 구매했습니다.\n"),
-    MESSAGE_OF_ENTERING_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    MESSAGE_OF_ENTERING_BONUS_NUMBERS("보너스 번호를 입력해 주세요.");
+    MESSAGE_OF_ENTERING_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    MESSAGE_OF_ENTERING_BONUS_NUMBERS("\n보너스 번호를 입력해 주세요."),
+    MESSAGE_OF_LOTTO_RESULT("\n당첨 통계\n---");
 
     private final String prompts;
 

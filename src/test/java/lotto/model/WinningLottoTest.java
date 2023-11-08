d95892751e089c4881a4602bc814833f79119124
hoyeon <highoption@naver.com>
2023-11-08 13:56:30 +0900

    test(WinningLottoTest) : 당첨번호와 보너스 번호는 중복이면 예외 발생

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,4 +24,14 @@ public class WinningLottoTest {
                 .isEqualTo(bonusNumber.getBonusNumber());
     }
 
+    @Test
+    @DisplayName("당첨 번호와 보너스 번호가 중복이면 예외 발생")
+    void testInvalidDuplicateWinningLotto() {
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        BonusNumber bonusNumber = new BonusNumber("6");
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+                .withMessage("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
+    }
 }

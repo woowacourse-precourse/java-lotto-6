d051975eed5cc810e836f6328f392a3d1f42ed6d
jjongwon7 <jjongwon7@naver.com>
2023-11-08 21:32:06 +0900

    feat: 로또 구입 금액 검증 기능 추가 입력한 로또 구입 금액이 올바르지 않은 경우 예외를 발생시키는 검증 메서드 작성

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,8 +1,14 @@
 package lotto.constant;
 
+import static lotto.constant.LottoInfo.LOTTO_PRICE;
+
 public class ErrorMessage {
     // 문자열 처리
     public static final String INPUT_EMPTY = "입력하신 문자열은 빈 문자열입니다.";
     public static final String INPUT_CONTAINS_WHITE_SPACE = "입력하신 문자열에 공백이 포함되어 있습니다.";
     public static final String INPUT_IS_NOT_INTEGER = "입력하신 문자열은 정수가 아닙니다.";
+
+    // 로또 구매 금액 입력
+    public static final String INPUT_PRICE_IS_NOT_MULTIPLE_OF_LOTTO_PRICE = "로또 구매 가격은 " + LOTTO_PRICE + "원 단위로 입력해 주세요.";
+    public static final String INPUT_PRICE_IS_NOT_GREATER_THAN_ZERO = "로또 구매 가격은 음수가 될 수 없습니다.";
 }
--- src/main/java/lotto/validators/PurchaseValidator.java
+++ src/main/java/lotto/validators/PurchaseValidator.java
@@ -1,8 +1,19 @@
 package lotto.validators;
 
+import static lotto.constant.ErrorMessage.INPUT_PRICE_IS_NOT_GREATER_THAN_ZERO;
+import static lotto.constant.ErrorMessage.INPUT_PRICE_IS_NOT_MULTIPLE_OF_LOTTO_PRICE;
 import static lotto.constant.LottoInfo.LOTTO_PRICE;
 
 public class PurchaseValidator {
+    public static void validPurchasePrice(Integer purchasePrice) {
+        if (!isMultipleOfLottoPrice(purchasePrice)) {
+            throw new IllegalArgumentException(INPUT_PRICE_IS_NOT_MULTIPLE_OF_LOTTO_PRICE);
+        }
+        if (!isGreaterThanZero(purchasePrice)) {
+            throw new IllegalArgumentException(INPUT_PRICE_IS_NOT_GREATER_THAN_ZERO);
+        }
+    }
+
     private static boolean isMultipleOfLottoPrice(Integer purchasePrice) {
         return purchasePrice % LOTTO_PRICE == 0;
     }

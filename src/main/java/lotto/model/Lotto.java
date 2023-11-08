296b6e6a59f275472dc37958834bb0ca461d5307
SubinHeo0 <anmedi3623@naver.com>
2023-11-08 10:19:46 +0900

    feat: Lotto 번호 오름차순 정렬 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static lotto.validator.Validation.*;
@@ -9,13 +11,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         checkNumberCount(numbers);
         checkInRange(numbers);
         checkDuplication(numbers);
+    }
+
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        List<Integer> lottoNumber = new ArrayList<>(numbers);
+        Collections.sort(lottoNumber);
+        return lottoNumber;
     }
 
     public List<Integer> getNumbers() {

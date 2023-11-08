01feeadbf9b902972850ef6a666d862812f2c109
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:32:32 +0900

    메소드 분리 안한 것 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,39 +1,11 @@
 package lotto;
 
-import lotto.generator.ClientLottoNumber;
-import lotto.generator.CalculateProfitRate;
-import lotto.generator.CalculateTotalProfit;
-import lotto.generator.CalculateWinCounts;
-import lotto.view.PrintWinCounts;
-import lotto.view.PrintProfitRate;
-import lotto.view.PrintView;
+import lotto.Controller.LottoStart;
 
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Application app = new Application();
-        app.run();
+        LottoStart lottoStart =new LottoStart();
+        lottoStart.run();
     }
-
-    public void run() {
-        ClientLottoNumber clientLottoNumber = new ClientLottoNumber();
-        clientLottoNumber.NumberGenerator();
-
-        PrintView printView = new PrintView();
-        printView.outView();
-
-        List<Integer> lottoWinNumber = PrintView.lottoWinNumber;
-        int bonusNumber = PrintView.bonusNumber;
-
-        List<Integer> winCounts = CalculateWinCounts.calculateWinCounts(clientLottoNumber.LottoNumberList, lottoWinNumber, bonusNumber);
-        PrintWinCounts.printWinCounts(winCounts);
-
-        int totalProfit = CalculateTotalProfit.calculateTotalProfit(winCounts, clientLottoNumber.t);
-        float profitRate = CalculateProfitRate.calculateProfitRate(totalProfit, clientLottoNumber.t);
-        PrintProfitRate.printProfitRate(profitRate);
-    }
-
-
-
 }

623e6d54d785ff55ec2910fa2fd6a00902dc5f0e
SCY <helenason@naver.com>
2023-11-08 21:04:15 +0900

    feat: WinnerPrize에 일치하는 번호 수 correctCount 추가

--- src/main/java/lotto/model/WinnerPrize.java
+++ src/main/java/lotto/model/WinnerPrize.java
@@ -2,15 +2,17 @@ package lotto.model;
 
 public enum WinnerPrize {
 
-    THREE_CORRECT(5000),
-    FOUR_CORRECT(50000),
-    FIVE_CORRECT(1500000),
-    FIVE_ONE_CORRECT(30000000),
-    SIX_CORRECT(2000000000);
+    THREE_CORRECT(3, 5000),
+    FOUR_CORRECT(4, 50000),
+    FIVE_CORRECT(5, 1500000),
+    FIVE_ONE_CORRECT(5.5F, 30000000),
+    SIX_CORRECT(6, 2000000000);
 
+    private final float correctCount;
     private final int prize;
 
-    WinnerPrize(int prize) {
+    WinnerPrize(float correctCount, int prize) {
+        this.correctCount = correctCount;
         this.prize = prize;
     }
 }

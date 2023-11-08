56f3be2e8a5da04a4c5cb3d379730f26a274be6e
HanSeongLEE <leehs98712@gmail.com>
2023-11-08 21:50:35 +0900

    Refactor: 프로젝트 구조 변경
    main에서 Contoller 한개만 호출하도록 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,10 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.exception.LottoErrorMsg;
-import lotto.exception.LottoException;
 
 public class Application {
     public static void main(String[] args) {
-//        throw new IllegalArgumentException(LottoErrorMsg.UNABLE_TO_DIVIDE.getMessage());
         LottoController lottoController = new LottoController();
-        lottoController.buyLotto();
-        lottoController.getWinningNumbers();
+        lottoController.runLotto();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.domain.Lotto;
 import lotto.domain.WinningNumber;
 import lotto.dto.LottoRepositoryDto;
+import lotto.dto.LottoResultDto;
 import lotto.dto.Money;
 import lotto.service.LottoService;
 import lotto.util.Validation;
@@ -16,17 +17,26 @@ public class LottoController {
 
     public LottoController(){
     }
-
-    public void buyLotto(){
+    public void runLotto(){
+        buyLotto();
+        getWinningNumbers();
+        getResult();
+    }
+    private void buyLotto(){
         Money money = lottoView.requestBuyLotto();
         LottoRepositoryDto lottoRepositoryDto = lottoService.buyLotto(money);
         lottoView.printLottoRepository(lottoRepositoryDto);
     }
 
-    public void getWinningNumbers(){
+    private void getWinningNumbers(){
         Lotto winningLotto = new Lotto(Validation.validateList(lottoView.requestWinningLotto()));
         int BonusNum = Validation.validateInt(lottoView.requestBonusNum());
         winningNumber = new WinningNumber(winningLotto,BonusNum);
     }
+    private void getResult(){
+        LottoResultDto lottoResultDto = lottoService.getResult(winningNumber);
+        lottoView.printResultMsg(lottoResultDto);
+    }
+
 
 }

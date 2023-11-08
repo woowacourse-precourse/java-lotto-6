30e15e09db346da1b04385d923b850c23d417db9
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 21:20:47 +0900

    Feat[util] 로또 번호 입력값에서 쉼표 분리

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -1,10 +1,13 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.view.ErrorView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoUtil {
     public static List<Integer> generateRandomNumber(int start, int end, int count) {
@@ -15,5 +18,16 @@ public class LottoUtil {
         List<Integer> orderedList = new ArrayList<Integer>(numbers);
         Collections.sort(orderedList);
         return orderedList;
+    }
+
+    public static List<Integer> parseNumbersFromString(String input) {
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorView.getInvalidInputFormatErrorMessage());
+        }
     }
 }

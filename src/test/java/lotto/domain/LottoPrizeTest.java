2b94415aae7301b909a706f2eac598290b43fdb7
Chenowoo <cjsgusdn14@naver.com>
2023-11-08 23:31:15 +0900

    test(LottoPrizeTest): 테스트 코드 추가

--- src/test/java/lotto/domain/LottoPrizeTest.java
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -39,4 +39,30 @@ class LottoPrizeTest {
 
         assertThat(result).isEqualTo(answer);
     }
+
+    @DisplayName("로또 당첨 결과를 저장하는 이중 Map을 초기 설정한다.")
+    @Test
+    void initLottoPrize() {
+        Map<Integer, Map<String, Integer>> result = lottoPrize.initLottoPrize();
+
+        Map<Integer, Map<String, Integer>> answer = new HashMap<>();
+        Map<String,Integer> innerMap = new HashMap<>();
+        innerMap.put("3NoBonus", 0);
+        answer.put(3, innerMap);
+
+        innerMap = new HashMap<>();
+        innerMap.put("4NoBonus", 0);
+        answer.put(4, innerMap);
+
+        innerMap = new HashMap<>();
+        innerMap.put("5NoBonus", 0);;
+        innerMap.put("5Bonus", 0);
+        answer.put(5, innerMap);
+
+        innerMap = new HashMap<>();
+        innerMap.put("6NoBonus", 0);
+        answer.put(6, innerMap);
+
+        assertThat(result).isEqualTo(answer);
+    }
 }

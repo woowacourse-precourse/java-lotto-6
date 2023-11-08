2de6a6786c65e7fc54c9d440b8d5adda7e2c4538
kmh0601 <pongkmh@naver.com>
2023-11-08 22:49:47 +0900

    chore: 로또 게임 시작부 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,9 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoGame lottoGame;
+        lottoGame = LottoGame.createLottoGame();
+
+        lottoGame.startGame();
     }
 }

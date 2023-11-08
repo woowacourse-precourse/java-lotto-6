6c1ce1b620c18e44ea608a0188685752c4e1abeb
lsj5657 <lsj56575@naver.com>
2023-11-08 14:19:17 +0900

    로또에서 숫자들을 모두 출력하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,25 +17,39 @@ public class Lotto {
         }
     }
 
-    public int lottoScore(List<Integer>winningLottoNumbers, int bonusNumber){
-        int score=0;
-        int bonusScore=0;
-        for (int number:numbers){
-            if(winningLottoNumbers.contains(number)) score++;
-            if(number==bonusNumber)bonusScore++;
+    public int lottoScore(List<Integer> winningLottoNumbers, int bonusNumber) {
+        int score = 0;
+        int bonusScore = 0;
+        for (int number : numbers) {
+            if (winningLottoNumbers.contains(number)) score++;
+            if (number == bonusNumber) bonusScore++;
         }
-        return calculateReward(score,bonusScore);
+        return calculateReward(score, bonusScore);
     }
 
-    public int calculateReward(int score,int bonusScore){
-        if(score==6) return 2000000000;
-        if(score==5){
-            if(bonusScore==1)return 30000000;
+    public int calculateReward(int score, int bonusScore) {
+        if (score == 6) return 2000000000;
+        if (score == 5) {
+            if (bonusScore == 1) return 30000000;
             return 1500000;
         }
-        if(score==4) return 50000;
-        if(score==3) return 5000;
+        if (score == 4) return 50000;
+        if (score == 3) return 5000;
         return 0;
+    }
+
+    public void printNumbers() {
+        StringBuilder print = new StringBuilder();
+        print.append("[");
+        for (int number : numbers) {
+            print.append(number);
+            print.append(",");
+        }
+        print.deleteCharAt(print.length() - 1);
+        print.append("]");
+
+
+        System.out.println(print );
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/LottoManagement.java
+++ src/main/java/lotto/LottoManagement.java
@@ -4,62 +4,64 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class LottoManagement {
-    static final int prize1=2000000000;
-    static final int prize2=30000000;
-    static final int prize3=1500000;
-    static final int prize4=50000;
-    static final int prize5=5000;
-    final static int pricePerPiece=1000;
+    static final int prize1 = 2000000000;
+    static final int prize2 = 30000000;
+    static final int prize3 = 1500000;
+    static final int prize4 = 50000;
+    static final int prize5 = 5000;
+    final static int pricePerPiece = 1000;
     private int verifiedMoney;
     private int bonusNumber;
     List<Integer> winningLottoNumbers;
     private final List<Lotto> lottos;
-    List<Integer>winningDetails = new ArrayList<>(6);
+    List<Integer> winningDetails = new ArrayList<>(6);
 
-    LottoManagement(){
-        lottos=new ArrayList<>();
+    LottoManagement() {
+        lottos = new ArrayList<>();
     }
 
-    void initMoney(){
+    void initMoney() {
         InputMoneyHandler inputMoneyHandler = new InputMoneyHandler();
         inputMoneyHandler.init();
-        verifiedMoney=inputMoneyHandler.exceptionHandledMoney();
+        verifiedMoney = inputMoneyHandler.exceptionHandledMoney();
     }
 
-    void initWinningLottoNumbers(){
+    void initWinningLottoNumbers() {
 
         NumbersHandler numbersHandler = new NumbersHandler();
         numbersHandler.init();
         winningLottoNumbers = numbersHandler.convertedWinningNumbers();
-        bonusNumber=numbersHandler.convertedBounsNumber();
+        bonusNumber = numbersHandler.convertedBounsNumber();
     }
 
-    public void buyLottos(){
-        int lottoCount=verifiedMoney/pricePerPiece;
-        for (int i=0;i<lottoCount;i++){
-            Lotto lotto=new Lotto(LottoGenerator.getLottoNumbers());
-            int prize=lotto.lottoScore(winningLottoNumbers,bonusNumber);
+    public void buyLottos() {
+        int lottoCount = verifiedMoney / pricePerPiece;
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto lotto = new Lotto(LottoGenerator.getLottoNumbers());
+            int prize = lotto.lottoScore(winningLottoNumbers, bonusNumber);
             setDetails(prize);
-            System.out.print(lotto.lottoScore(winningLottoNumbers,bonusNumber));
+            lotto.printNumbers();
             lottos.add(lotto);
         }
     }
 
-    public void setDetails(int prize){
-        if(prize==prize1)winningDetails.set(1,winningDetails.get(1)+1);
-        if(prize==prize2)winningDetails.set(2,winningDetails.get(2)+1);
-        if(prize==prize3)winningDetails.set(3,winningDetails.get(3)+1);
-        if(prize==prize4)winningDetails.set(4,winningDetails.get(4)+1);
-        if(prize==prize5)winningDetails.set(5,winningDetails.get(5)+1);
+    public void setDetails(int prize) {
+        if (prize == prize1) winningDetails.set(1, winningDetails.get(1) + 1);
+        if (prize == prize2) winningDetails.set(2, winningDetails.get(2) + 1);
+        if (prize == prize3) winningDetails.set(3, winningDetails.get(3) + 1);
+        if (prize == prize4) winningDetails.set(4, winningDetails.get(4) + 1);
+        if (prize == prize5) winningDetails.set(5, winningDetails.get(5) + 1);
     }
 
-    public List<Lotto> getLottos(){
+    public List<Lotto> getLottos() {
         return lottos;
     }
-    public int getMoney(){
+
+    public int getMoney() {
         return verifiedMoney;
     }
-    public int getBonusNumber(){
+
+    public int getBonusNumber() {
         return bonusNumber;
     }
 

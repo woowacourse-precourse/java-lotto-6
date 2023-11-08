34eb0590028c7ee22fe38baf9b7f713f3ac0f0aa
Kim mingyu <minq0302@naver.com>
2023-11-08 23:17:05 +0900

    feat(domain): add necessary methods

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.exception.DuplicateLottoNumberException;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
@@ -11,7 +14,20 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
+            throw new DuplicateLottoNumberException();
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR]: 로또 숫자는 6개여야 합니다.");
         }
@@ -20,5 +36,15 @@ public class Lotto {
     @Override
     public String toString() {
         return numbers.toString();
+    }
+
+    public boolean isContain(int number) {
+        return numbers.contains(number);
+    }
+
+    public int getMatchLottoNumber(WinningLotto winningLotto) {
+        return (int) numbers.stream()
+                .filter(winningLotto::isContain)
+                .count();
     }
 }

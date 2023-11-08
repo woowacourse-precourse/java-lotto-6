a986feffdca684aa569f155be06b19bed9f7c1b1
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 13:48:08 +0900

    implement: LottoManager 1. launch, input & validation

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,6 +11,29 @@ public class LottoManager {
     private final int lottoNumberCount = 6;
     private final int minLottoNumber = 1;
     private final int maxLottoNumber = 45;
+
+    public void launch() {
+        try {
+            int purchaseAmount = inputPurchaseAmount();
+            if (!isValidPurchaseAmount(purchaseAmount)) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+            }
+            List<Lotto> lottos = purchaseLottos(purchaseAmount);
+            // 로또 게임의 나머지 진행 로직을 추가
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            // 에러 발생시 재시도 로직 추가
+        }
+    }
+
+    private int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private boolean isValidPurchaseAmount(int purchaseAmount) {
+        return purchaseAmount % pricePerLotto == 0;
+    }
 
     public List<Lotto> purchaseLottos(int purchaseAmount) {
         int numberOfLottos = purchaseAmount / pricePerLotto;

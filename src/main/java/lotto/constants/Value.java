569b6a11cd8d8d421b5841b1874b80cf9d7f81de
SeongIl <biuld1234@gmail.com>
2023-11-08 16:00:22 +0900

    style(Value): change Enum 열거형 순서 - 숫자 크기 순서로, 나열 하는 것이 가독성에 좋을 것으로 판단

--- src/main/java/lotto/constants/Value.java
+++ src/main/java/lotto/constants/Value.java
@@ -1,19 +1,19 @@
 package lotto.constants;
 
 public enum Value {
-    LOTTO_SIZE(6),
-    MIN_LOTTO_NUMBER(1),
-    MAX_LOTTO_NUMBER(45),
-    THOUSAND(1000),
-    LOTTO_PRICE(1000),
-    PERCENT(100),
     INITIAL_ZERO(0),
     REMAINDER_ZERO(0),
     FIRST_CHARACTER(0),
     ZERO(0),
-    ASCII_ZERO(48),
     COUNT_ONE(1),
-    INDEX_MINUS_ONE(1);
+    INDEX_MINUS_ONE(1),
+    MIN_LOTTO_NUMBER(1),
+    LOTTO_SIZE(6),
+    MAX_LOTTO_NUMBER(45),
+    ASCII_ZERO(48),
+    PERCENT(100),
+    THOUSAND(1000),
+    LOTTO_PRICE(1000);
 
     private final Integer value;
 

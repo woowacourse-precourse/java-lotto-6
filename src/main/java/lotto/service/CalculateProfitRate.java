bc16edb17b1c811732b73e1062effba412cb409a
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 15:19:27 +0900

    승리수

--- src/main/java/lotto/service/CalculateProfitRate.java
+++
@@ -1,8 +0,0 @@
-package lotto.service;
-
-public class CalculateProfitRate {
-    public static float calculateProfitRate(int totalProfit, int t) {
-        int p = t * 1000;
-        return (float) totalProfit / p * 10;
-    }
-}

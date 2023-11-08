4b79b8ca6ec9c5bac928640035d6f7e87bcb0766
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-08 16:57:49 +0900

    feat(domain/User): 로또 수익률 계산 기능

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,10 +1,12 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
 
     private final ArrayList<Lotto> purchasedLotto = new ArrayList<>();
+    private final HashMap<Rank, Integer> lottoResult = new HashMap<>();
     private final int purchaseAmount;
 
     public User(int purchaseAmount) {
@@ -12,20 +14,33 @@ public class User {
         this.purchaseAmount = purchaseAmount;
     }
 
-    public void purchaseLotto(Lotto lotto) {
-        purchasedLotto.add(lotto);
-    }
-
     public ArrayList<Lotto> getPurchasedLotto() {
         return purchasedLotto;
+    }
+
+    public HashMap<Rank, Integer> getLottoResult() {
+        return lottoResult;
     }
 
     public int getPurchaseAmount() {
         return purchaseAmount;
     }
 
+    public void purchaseLotto(Lotto lotto) {
+        purchasedLotto.add(lotto);
+    }
+
+    public int getTotalPrize() {
+        int sum = 0;
+        for (Rank rank : lottoResult.keySet()) {
+            sum += rank.getPrize() * lottoResult.get(rank);
+        }
+        return sum;
+    }
+
     public double getLotteryReturns() {
-        return 0;
+        double lotteryReturns = (getTotalPrize() / (double) purchaseAmount) * 100;
+        return Math.round(lotteryReturns * 10) / 10.0;
     }
 
     private void validatePurchaseAmount(int purchaseAmount) {

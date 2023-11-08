67047608674c1d8a52862ee1c149867e77a4d0a4
Kyoungtae <rudxo0525@gmail.com>
2023-11-08 14:12:02 +0900

    feat: 로또 번호를 오름차순으로 정렬하는 sortNumber()함수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,6 +9,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        sortNumber();
     }
 
     private void validate(List<Integer> numbers) {
@@ -27,5 +29,9 @@ public class Lotto {
         sb.delete(sb.length()-2, sb.length());
         sb.append("]");
         System.out.println(sb.toString());
+    }
+
+    private void sortNumber(){
+        Collections.sort(numbers);
     }
 }

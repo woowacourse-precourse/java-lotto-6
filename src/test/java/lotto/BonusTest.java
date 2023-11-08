af451ef4c08488ca085d5bed1090bc90bcf8f99e
Identityless <rlandwnd555@naver.com>
2023-11-08 17:06:56 +0900

    test : Bonus 객체에 1~45 밖의 범위의 수가 입력된 경우에 대한 예외 테스트 메서드 추가

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -1,5 +1,16 @@
 package lotto;
 
-public class BonusTest {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.model.Bonus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BonusTest {
+    @DisplayName("잘못된 범위의 수가 입력된 경우 예외 발생")
+    @Test
+    void 잘못된_범위의_수_입력_테스트() {
+        assertThatThrownBy(() -> new Bonus(46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

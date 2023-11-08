63abf31a6e313cd70b219dab1c2da46a58bd6590
qocksdlf123 <qocksdlf123@naver.com>
2023-11-08 23:06:20 +0900

    Feat(LottoGameController): 결과를 계산하는 printResults 메서드 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -27,7 +27,8 @@ public class LottoGameController {
         int bonusNumber = Integer.parseInt(InputView.receiveInputBonusNumberMsg());
         GameData gameData = new GameData(winningLotto, bonusNumber, lottos);
 
-//        calculateResult(gameData);
+        calculateResults(gameData);
+        printResults(gameData);
     }
     public List<Integer> parseWinningNumbers(String winningnumbers){
         return Arrays.stream(winningnumbers.split(","))
@@ -63,6 +64,27 @@ public class LottoGameController {
         gameData.setResults(results);
     }
 
+    private void printResults(GameData gameData) {
+        OutputView.printWinningStatistics();
+        Map<Rank, Integer> results = gameData.getResults();
+
+        // 정렬된 순서로 결과 출력
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.MISS) {
+                continue; // MISS 등급은 출력하지 않음
+            }
+            int count = results.getOrDefault(rank, 0);
+            OutputView.printWinningResult(rank.getMatchCount(), rank.getPrizeMoney(), count);
+        }
+
+        double totalPrize = results.entrySet().stream()
+                .mapToDouble(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
+                .sum();
+        double purchaseAmount = gameData.getLottos().size() * LOTTO_PRICE;
+        double profitRate = totalPrize / purchaseAmount * 100;
+
+        OutputView.printTotalProfitRate(profitRate);
+    }
 
 
 }

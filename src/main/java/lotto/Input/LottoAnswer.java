d742d0d78824c9b8ed8cc1af8a455e6f782284bd
hahah-a-ha <smha01@hanyang.ac.kr>
2023-11-08 20:07:57 +0900

    feat: lotto 번호 입력 기능 추가

---
+++ src/main/java/lotto/Input/LottoAnswer.java
@@ -0,0 +1,42 @@
+package lotto.Input;
+
+import com.sun.jdi.request.DuplicateRequestException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class LottoAnswer {
+    private final List<Integer> numbers;
+
+    public LottoAnswer() {
+        this.numbers = inputLotto();
+    }
+
+    public List<Integer> inputLotto() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        while (true) {
+            try {
+                String input = readLine();
+                List<String> list = List.of(input.split(","));
+                for (String num : list) {
+                    lottoNumbers.add(Integer.parseInt(num));
+                }
+                new Lotto(lottoNumbers);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해야 합니다.");
+                lottoNumbers.clear();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                lottoNumbers.clear();
+            }
+        }
+        return lottoNumbers;
+    }
+
+    public List<Integer> getAnswer() {
+        return this.numbers;
+    }
+}

28af69564d2deae8c3df31b855a5ac89444d7429
jy3026 <jy_3026@naver.com>
2023-11-08 22:38:20 +0900

    feat : 금액에 따른 로또 구매 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int purchaseAmount = getPurchaseAmount();
+        List<Lotto> purchasedLottoTickets = generateLottoTickets(purchaseAmount);
     }
 
     private static int getPurchaseAmount() {
@@ -26,5 +32,16 @@ public class Application {
             throw new IllegalArgumentException("1,000 단위로 입력 해주세요");
         }
         return purchaseAmount / 1000;
+    }
+
+    private static List<Lotto> generateLottoTickets(int purchaseAmount) {
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+        List<Lotto> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < purchaseAmount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            lottoTickets.add(new Lotto(numbers));
+        }
+        return lottoTickets;
     }
 }

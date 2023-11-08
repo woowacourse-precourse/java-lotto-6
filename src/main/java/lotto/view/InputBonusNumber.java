950b0305112097ab6982762cc0ed2c7521f92d3a
banxzxx <jwbang1643@gmail.com>
2023-11-08 19:45:31 +0900

    feat : 보너스 번호 입력값이 알맞은 범위에 있는지 확인하는 오류 메서드 생성

--- src/main/java/lotto/view/InputBonusNumber.java
+++ src/main/java/lotto/view/InputBonusNumber.java
@@ -12,12 +12,21 @@ public class InputBonusNumber {
         System.out.println("\n보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
         checkValidate(bonusNumber);
+        checkValidateRange(bonusNumber);
         return Integer.parseInt(bonusNumber);
     }
 
     private void checkValidate(String bonusNumber) {
         if (!PATTERN.matcher(bonusNumber).matches()) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력값 입니다.");
+        }
+
+
+    }
+
+    public void checkValidateRange(String bonusNumber) {
+        if ( Integer.parseInt(bonusNumber) < 1 || Integer.parseInt(bonusNumber) > 45 ) {
+            throw new IllegalArgumentException("[ERROR] 지정된 범위에 벗어나는 숫자입니다.");
         }
     }
 }

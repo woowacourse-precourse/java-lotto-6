72826d754d6087629a9708bbc8dbe57d2daef6fe
leorb159 <ldgsocial159@naver.com>
2023-11-08 17:15:42 +0900

    feat: 일치하는 번호 개수 반환 메소드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,4 +20,18 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
     }
+
+    public int checkMatchCount(Lotto lotto) {
+        int count = 0;
+        for (int number : numbers) {
+            if (lotto.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
 }

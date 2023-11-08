17665984b9990abc1b8270061d97056f2b6a08c3
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-08 20:07:25 +0900

    refactor(Lotto): 가독성을 증가시켰습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,23 +4,23 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutPutView;
 
+import static lotto.view.InputView.userHaveMoney;
+
 public class LottoController {
 
     public void lottoStart() {
-        int userMoney = InputView.userHaveMoney();
+        int userMoney = userHaveMoney();
+
         Lottos lottos = new Lottos(userMoney);
         OutPutView outPutView = new OutPutView();
 
-        outPutView.outPutViewLottos(lottos, userMoney); //개를 구매했습니다.
+        outPutView.outPutViewLottos(lottos, userMoney);
 
         LottoResult lottoResult = new LottoResult();
 
-
         lottoResult.calculateWinningResult(lottos, InputView.inputWinningNumbers(), InputView.inputBonusNumber());
-
         outPutView.outPutViewWinningStatistics(lottoResult);
+
         System.out.print("총 수익률은 " + lottoResult.calculateProfitRate(userMoney) + "%입니다.");
     }
-
-
 }

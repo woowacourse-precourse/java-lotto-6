e4087a92579341f1d3ec4100edf0b356ef0648a6
leedabin2 <12193152@inha.edu>
2023-11-08 17:52:25 +0900

    feat(createWinningLotto): Add method to create a winning Lotto object from user input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,16 +2,35 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.handler.UserHandler;
+import lotto.model.Lotto;
+import lotto.model.Rank;
+import lotto.view.ResultView;
 
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoController lottoController = new LottoController();
-        lottoController.buyLottos();
 
-        UserHandler.getWinningNumberFromUser();
+        // 사용자로부터 구매금액을 입력받음
+        int amount = UserHandler.getAmountFromUser();
+
+        // 로또 개수 계산
+        int lottoCount = UserHandler.getLottoCountFromUser(amount);
+
+        // 로또 구매
+        lottoController.buyLottos(lottoCount);
+
+        // 사용자로부터 당첨 번호를 받음
+        List<String> winningNumbers = UserHandler.getWinningNumberFromUser();
+
+        // 당첨 번호를 바탕으로 당첨 로또를 생성.
+        Lotto winningLotto = lottoController.createWinningLotto(winningNumbers);
+
+        // 사용자로부터 보너스 번호를 받음
+        int bonusNumber = UserHandler.getAndValidateBonusNumber(winningNumbers);
 
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,37 +1,37 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.Lotto;
+import lotto.model.Lotto;
 import lotto.handler.UserHandler;
+import lotto.model.Rank;
 import lotto.view.ResultView;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LottoController {
+    public List<Lotto> lottos;
 
-    int amount = UserHandler.getAmountFromUser();
-    int lottoCount = UserHandler.getLottoCountFromUser(amount);
-
-    public void buyLottos() {
+    public void buyLottos(int lottoCount) {
         ResultView.printLottoCount(lottoCount);
-
+        lottos = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             Lotto lotto = lottoGenerate();
+            lottos.add(lotto);
             ResultView.printLottoNumber(lotto.getNumbers());
         }
     }
 
     private Lotto lottoGenerate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(numbers);
         return new Lotto(numbers);
     }
 
-//    public static void checkValuePresence(List<Integer> numbers, String winningNumbers) {
-//        boolean isContains = numbers.contains(winningNumbers);
-//        System.out.println(isContains);
-//    }
-
+    public Lotto createWinningLotto(List<String> winningNumbers) {
+        List<Integer> numbers = winningNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return new Lotto(numbers);
+    }
 }

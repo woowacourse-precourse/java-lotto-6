4705cc4c8827bd606e1a6fd15eb35d8710cf4fcd
ogi <ogi@naver.com>
2023-11-08 23:02:45 +0900

    [feat] 로또 번호 생성 및 검증 코드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.util.Message;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +14,15 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        // 1. 숫자 개수 검증
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Message.EXCPTION_NOT_A_SIX);
+        }
+
+        // 2. 숫자 중복 여부 검증
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(Message.EXCPTION_DUPLICATED);
         }
     }
-
-    // TODO: 추가 기능 구현
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,14 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     public void showQuantityMessage(int quantity) {
         System.out.println();
         System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public void showLotto(List<Integer> lotto) {
+        System.out.println(lotto);
     }
 }

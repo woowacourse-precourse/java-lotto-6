213745167f30195bb11d3d54b846d51aaa611492
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-08 11:03:25 +0900

    refactor: (Application) 로또 시스템 실행 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoSystem;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoSystem lottoSystem = new LottoSystem();
+        lottoSystem.runSystem();
     }
 }

7d2de902520bd522396f08b5a90d3179b3d0e0b1
pjaehyun <show6756@gmail.com>
2023-11-08 15:54:30 +0900

    feat: 추가 당첨번호 문자입력 예외 테스트 함수

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,15 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 당첨번호_예외_테스트_문자() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3h,5,6,7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});

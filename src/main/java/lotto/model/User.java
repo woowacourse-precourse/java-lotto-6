9c3f7bf7b97dccad4d34b994a222a033b1f9c0af
yooonwodyd <dbswodyd00@naver.com>
2023-11-08 17:31:49 +0900

    User가 가지고 있는 로또들의 결과를 확인하도록 메서드 추가

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,2 +1,23 @@
-package lotto.model;public class User {
+package lotto.model;
+
+import lotto.Lotto;
+
+import java.util.List;
+
+public class User {
+    private List<Lotto> lottos;
+
+    private User(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static User of(List<Lotto> lottos) {
+        return new User(lottos);
+    }
+
+    public List<LottoResult> checkLottoTypes(WinningNumbers winningNumbers) {
+        return lottos.stream()
+                .map(lotto -> lotto.toLottoResult(winningNumbers))
+                .toList();
+    }
 }

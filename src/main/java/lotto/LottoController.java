5efeaef1e34d1e2dee634a48ead7aa9453128edb
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 14:41:41 +0900

    feat: 로또 당첨 번호, 보너스 번호를 올바르게 입력 할 때 까지 입력 받는 기능

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -2,15 +2,29 @@ package lotto;
 
 import lotto.view.InputView;
 
+import java.util.List;
+
 public class LottoController {
 
     public void run() {
         LottoPurchaseMoney lottoPurchaseMoney = getLottoPurchaseMoney();
         int numberOfLottos = lottoPurchaseMoney.getNumberOfLottoAvailableForPurchase();
         Lottos userLottos = new Lottos(numberOfLottos);
+        WinningLotto winningLotto = getWinningLotto();
     }
 
-    private static LottoPurchaseMoney getLottoPurchaseMoney() {
+    private WinningLotto getWinningLotto() {
+        try {
+            List<Integer> winningNumbers = InputView.readLottoWinningNumbers();
+            int bonusNumber = InputView.readBonusNumber();
+            return new WinningLotto(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getWinningLotto();
+        }
+    }
+
+    private LottoPurchaseMoney getLottoPurchaseMoney() {
         try {
             int inputMoney = InputView.readLottoPurchaseMoney();
             return new LottoPurchaseMoney(inputMoney);

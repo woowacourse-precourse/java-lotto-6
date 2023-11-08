820ce236845efd977ba1739ff6616f8f52240a92
siyeonSon <siyeon0209@naver.com>
2023-11-08 17:46:55 +0900

    refactor(LottoController): 기능 별 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -19,13 +19,33 @@ public class LottoController {
     }
 
     public void run() {
+        UserMoney userMoney = readMoney();
+
+        UserLotto userLotto = generateUserLotto(userMoney);
+
+        WinningNumbers winningNumbers = readWinningNumbers();
+
+        printResult(userMoney, userLotto, winningNumbers);
+    }
+
+    private UserMoney readMoney() {
         UserMoney userMoney = userIoManager.readUserMoney();
+        return userMoney;
+    }
+
+    private UserLotto generateUserLotto(UserMoney userMoney) {
         userIoManager.printLottoAmount(userMoney);
         UserLotto userLotto = new UserLotto(userMoney.getLottoAmount(), numberGenerator);
         userIoManager.printUserLotto(userLotto);
+        return userLotto;
+    }
+
+    private WinningNumbers readWinningNumbers() {
         Lotto lotto = userIoManager.readWinningNumbers();
-        Bonus bonus = userIoManager.readBonusNumber();
-        WinningNumbers winningNumbers = new WinningNumbers(lotto, bonus);
+        return userIoManager.readBonusNumber(lotto);
+    }
+
+    private void printResult(UserMoney userMoney, UserLotto userLotto, WinningNumbers winningNumbers) {
         UserLottoRank userLottoRank = new UserLottoRank(userLotto, winningNumbers);
         userIoManager.printResult(userMoney, userLottoRank);
     }

743b105a5cf6fb9bce2d28919d06b6345e6053b3
kijen723 <kij2646@gmail.com>
2023-11-08 20:34:46 +0900

    feat(lotto): Add more Input request message

--- src/main/java/lotto/GameMessage.java
+++ src/main/java/lotto/GameMessage.java
@@ -5,11 +5,20 @@ public class GameMessage {
 
     }
 
-    public static void priceInputRequestMessage() {
+    public void priceInputRequestMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static void lottoCountMessage(int lottoCount) {
+    public void lottoCountMessage(int lottoCount) {
         System.out.printf("%d개를 구매했습니다.\n", lottoCount);
     }
+
+    public void lottoNumbersInputMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void lottoBonusInputMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
 }

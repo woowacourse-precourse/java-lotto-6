9b25de5f1fbe0fd429ad6d32ab79f192de4ddd1e
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:20:25 +0900

    CalculateProfitRate 수익률 계산

--- src/test/java/lotto/generator/CalculateProfitRateTest.java
+++ src/test/java/lotto/generator/CalculateProfitRateTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.generator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class CalculateProfitRateTest {
-  
+    @DisplayName("수익률을 계산한다")
+    @Test
+    void testCalculateProfitRate() {
+        // Arrange
+        int totalProfit = 15000;
+        int t = 10;
+
+        // Act
+        float profitRate = CalculateProfitRate.calculateProfitRate(totalProfit, t);
+
+        // Assert
+        assertThat(profitRate).isEqualTo(150.0f);
+    }
 }

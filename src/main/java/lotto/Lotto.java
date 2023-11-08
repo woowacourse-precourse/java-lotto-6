a03321d6d8ba679759f9072667cb5924e96c5672
Hyoseop Song <crad_on25@naver.com>
2023-11-08 21:04:56 +0900

    feature(Lotto): 로또 번호가 중복되지 않는지 확인하는 메소드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,5 +19,16 @@ public class Lotto {
 
     public boolean contains(int number) {
         return numbers.contains(number);
+    }
+
+    public int compareWithNumberList(List<Integer> anotherNumbers) {
+        return (int) numbers.stream()
+                .filter(number -> anotherNumbers.stream()
+                        .anyMatch(Predicate.isEqual(number)))
+                .count();
+    }
+
+    public int compareWithAnotherLotto(Lotto another) {
+        return another.compareWithNumberList(this.numbers);
     }
 }

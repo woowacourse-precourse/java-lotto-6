6a5c9c58d32efc3ce7800b16ae2f5bc82ccf9807
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-08 14:10:18 +0900

    feat: Money 클래스 구현
    돈과 관련한 정보를 저장하는 Money 클래스 구현

--- src/main/java/lotto/model/money/Money.java
+++ src/main/java/lotto/model/money/Money.java
@@ -1,7 +1,30 @@
 package lotto.model.money;
 
+import java.util.Map;
+import lotto.utils.constant.LottoWinningConstant;
+
 public class Money {
+    public static final int DEFAULT_MONEY = 0;
+
     private int money;
 
+    private Money(int money) {
+        this.money = money;
+    }
 
+    public static Money of(int money) {
+        return new Money(money);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void calculateTotalPrize(Map<LottoWinningConstant, Integer> result) {
+        int totalPrize = 0;
+        for (LottoWinningConstant lottoWinningConstant : LottoWinningConstant.values()) {
+            totalPrize += lottoWinningConstant.getWinningMoney() * result.get(lottoWinningConstant);
+        }
+        money = totalPrize;
+    }
 }

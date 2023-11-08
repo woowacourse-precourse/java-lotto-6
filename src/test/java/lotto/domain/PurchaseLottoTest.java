dc826828de23cc74f84ad41f7c62159cfe1cdde3
1015s <1015_s@naver.com>
2023-11-08 03:09:22 -0800

    feat(PurchaseLottoTest) : 구매 금액 관련 테스트 구현

--- src/test/java/lotto/domain/PurchaseLottoTest.java
+++ src/test/java/lotto/domain/PurchaseLottoTest.java
@@ -1,2 +1,29 @@
-package lotto.domain;public class PurchaseLottoTest {
+package lotto.domain;
+
+import lotto.util.ExceptionMessage;
+import lotto.view.OutputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PurchaseLottoTest {
+    @DisplayName("로또 구매 금액이 0인 경우 예외가 발생한다.")
+    @Test
+    void enterZero(){
+        int purchaseMoney = 0;
+
+        assertThatThrownBy(() -> new PurchaseLotto(purchaseMoney))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구매 금액이 로또 금액(1000)의 배수가 아닌 경우 예외가 발생한다.")
+    @Test
+    void enterNotMultipleOfLottoPrice(){
+        int purchaseMoney = 12345;
+
+        assertThatThrownBy(() -> new PurchaseLotto(purchaseMoney))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

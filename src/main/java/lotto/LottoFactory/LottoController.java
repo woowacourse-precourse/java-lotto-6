8d4f3ac3e709ddbffe2ad25f1695978efee9a85a
minju <minju9711@naver.com>
2023-11-08 18:28:04 +0900

    [refactor] if-else 문 삭제

--- src/main/java/lotto/LottoFactory/LottoController.java
+++ src/main/java/lotto/LottoFactory/LottoController.java
@@ -4,15 +4,11 @@ public class LottoController {
 
     public void CheckDuplicateNum(LottoChecker lottoContainer, Lotto lotto, boolean isBonusDuplicate){
         int count = lottoContainer.countMatchingNum(lotto);  // Lotto 객체를 인자로 전달
-        if(count == 5 && isBonusDuplicate){
-            LottoRank.CheckAndStoreRank(count, true);
-        } else {
-            LottoRank.CheckAndStoreRank(count, false);
-        }
+        LottoRank.CheckAndStoreRank(count, count == 5 && isBonusDuplicate);
     }
 
     public int returnAllPrize(){
         return LottoRank.getAllPrizeMoney();
     }
-
 }
+

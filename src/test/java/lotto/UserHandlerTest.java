b72d99a54e19dbf85c3eb0988c57a37826e8ac29
leedabin2 <12193152@inha.edu>
2023-11-08 16:48:48 +0900

    test(UserHandler): Add null error handling test for validateUserInput method

--- src/test/java/lotto/UserHandlerTest.java
+++ src/test/java/lotto/UserHandlerTest.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import lotto.handler.UserHandler;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class UserHandlerTest {
+    @DisplayName("validateUserInput 메서드 테스트: null 입력 시 Exception 발생")
+    @Test
+    void validateUserInput_null값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateUserInput(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 금액을 입력해주세요.");
+    }
 }

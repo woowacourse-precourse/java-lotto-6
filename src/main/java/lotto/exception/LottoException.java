b1749c88923c47717e6d345763a82085fe4a913d
leemiyeon <lemingul7@gmail.com>
2023-11-08 21:59:03 +0900

    refactor: test를 위한 리팩토링 1. LottoException: 테스트 코드 작성을 위해 생성자 추가

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -1,20 +1,19 @@
 package lotto.exception;
 
 public class LottoException extends IllegalArgumentException {
+    private LottoException(String message) {
+        super(message);
+    }
 
     private LottoException(ErrorMessage errorMessage) {
         super(errorMessage.getMessage());
-    }
-
-    private LottoException(ErrorMessage errorMessage, Throwable cause) {
-        super(errorMessage.getMessage(), cause);
     }
 
     public static LottoException from(ErrorMessage errorMessage) {
         return new LottoException(errorMessage);
     }
 
-    public static LottoException of(ErrorMessage errorMessage, Throwable cause) {
-        return new LottoException(errorMessage, cause);
+    public static LottoException from(String message) {
+        return new LottoException(message);
     }
 }

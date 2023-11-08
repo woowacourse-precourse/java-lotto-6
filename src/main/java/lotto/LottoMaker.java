50bd081e7b07fa4ee0efc92f375ed60de3215fc2
hj-k66 <hjx66@naver.com>
2023-11-08 18:33:25 +0900

    feat: 구입 금액에 해당하는 로또 랜덤 발생 기능 구현

---
+++ src/main/java/lotto/LottoMaker.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMaker {
+
+    public List<Lotto> makeLottoTickets(int ticketCount){
+        List<Lotto> lottoTickets = new ArrayList<>();
+        for(int i = 0; i < ticketCount; i++){
+            Lotto lotto = new Lotto(createRandomNumber());
+            lottoTickets.add(lotto);
+        }
+        return lottoTickets;
+    }
+
+    public List<Integer> createRandomNumber(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public int calculateLottoTicketCount(int money){
+        return money / 1000;
+    }
+}

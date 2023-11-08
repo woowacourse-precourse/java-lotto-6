febaedb6854b81d29fc5c1e1dbf816f218c967dc
ehdwoKIM <kinbell19@gmail.com>
2023-11-08 20:50:32 +0900

    [ADD] 당첨번호 & 보너스번호 저장

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,14 +3,19 @@ package lotto.controller;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.model.Customer;
 import lotto.model.Lotto;
+import lotto.model.WinningNumber;
 import lotto.service.LottoService;
 import lotto.view.InputMessage;
 import lotto.view.OutputMessage;
 import java.util.List;
 public class Controller {
     private LottoService lottoService = new LottoService();
-
-    public void getBuyLottoMoney() {
+    private WinningNumber winningNumber;
+    public void start(){
+        getBuyLottoMoney();
+        winningNumberShow();
+    }
+    private void getBuyLottoMoney() {
         InputMessage.inputMoney();
         int inputMoney = Integer.parseInt(Console.readLine());
         Customer customer = new Customer(inputMoney);
@@ -25,7 +30,11 @@ public class Controller {
         }
     }
 
-    private List<Integer> getWinningNumber() { // String[] vs List<Integer> ..
+    private void winningNumberShow() {
+        winningNumber = new WinningNumber(getWinningNumber(),getBonusNumber());
+    }
+
+    private List<Integer> getWinningNumber() {
         String input = Console.readLine().trim();
         return lottoService.integerList(input);
     }
---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningNumber {
+
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public WinningNumber(List<Integer> winningNumbers, int bonusNumber){
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return getBonusNumber();
+    }
+
+}

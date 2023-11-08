ea92dcede77248ad5bf819a94976c5f298cd4f37
mdgome <mdgome3@gmail.com>
2023-11-08 13:55:05 +0900

    test: 구매 금액 예외 처리
    - [X] 입력한 구매 금액이 1000원 단위가 아닌 경우
    - [X] 입력한 구매 금액이 0보다 큰 정수가 아닌 경우
    - [ ] 입력 값이 숫자가 아닌 경우
      - [X] 구매 금액

--- src/test/java/lotto/model/OrderTest.java
+++ src/test/java/lotto/model/OrderTest.java
@@ -1,4 +1,39 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class OrderTest {
+    private final String expectCost = "4000";
+    private final int expectBuyCount = 4;
+
+    @DisplayName("입력한 구매 금액이 1000원 단위여야 한다.")
+    @Test
+    public void createOrderByMultipleOfThousand() {
+        String cost = "4500";
+
+        assertThatThrownBy(() -> new Order(cost))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 금액이 숫자여야 한다.")
+    @Test
+    public void createOrderByNumeric() {
+        String cost = "400zero";
+
+        assertThatThrownBy(() -> new Order(cost))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 금액은 0보다 큰 정수여야 한다.")
+    @Test
+    public void createOrderByPositive() {
+        String cost = "0";
+
+        assertThatThrownBy(() -> new Order(cost))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

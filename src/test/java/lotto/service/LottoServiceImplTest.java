afe203e074512479e6df04d7bb0fabf87bf60dfe
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 21:26:18 +0900

    test(LottoServiceImplTest): 당첨 번호 추첨하기 테스트 코드 작성

--- src/test/java/lotto/service/LottoServiceImplTest.java
+++ src/test/java/lotto/service/LottoServiceImplTest.java
@@ -1,7 +1,9 @@
 package lotto.service;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,12 @@ class LottoServiceImplTest {
     void testBuyLottos() {
         assertThat(lottoService.buyLottos("3000").getLottos().size())
                 .isEqualTo(3);
+    }
+
+    @DisplayName("당첨 번호 추첨하기")
+    @Test
+    void testDrawWinningNumbers() {
+        assertThat(lottoService.drawWinningNumbers("10,11,12,13,14,15").getNumbers())
+                .contains(10, 11, 12, 13, 14, 15);
     }
 }

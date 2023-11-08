24e376e5581e75e69759cf35f4e28d6955fe4eaf
MyStraw <kikongdosa@gmail.com>
2023-11-08 19:47:54 +0900

    Feat: 추가 LottoController 기능 구현
    - 입력받기
    - 출력
    - Service에 구현되어 있는 로직 호출

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,43 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.PrizeRank;
+import lotto.model.WinningLotto;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
+
+public class LottoController {
+    private final LottoService lottoService;
+    private final InputView inputView;
+    private final ResultView resultView;
+
+    public LottoController(LottoService lottoService, InputView inputView, ResultView resultView) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+        this.resultView = resultView;
+    }
+
+    public void run() {
+        try {
+            int purchaseAmount = inputView.promptForPurchaseAmount();
+            List<Lotto> purchasedLottos = lottoService.purchaseLottos(purchaseAmount);
+            resultView.displayPurchasedLottos(purchasedLottos);
+
+            List<Integer> winningNumbers = inputView.promptForWinningNumbers();
+            int bonusNumber = inputView.promptForBonusNumber();
+            WinningLotto winningLotto = lottoService.generateWinningLotto(winningNumbers, bonusNumber);
+
+            List<PrizeRank> prizeRanks = lottoService.determinePrizeRank(purchasedLottos, winningLotto);
+            resultView.displayWinningStatistics(prizeRanks);
+
+            double totalPrize = lottoService.calculateTotalPrize(prizeRanks);
+            double rateOfReturn = lottoService.calculateRateOfReturn(purchaseAmount, totalPrize);
+            resultView.displayRateOfReturn(rateOfReturn);
+        } catch (IllegalArgumentException e) {
+            resultView.displayError(e.getMessage());
+        }
+    }
+}

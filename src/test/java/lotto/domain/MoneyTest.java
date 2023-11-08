36ace34243084eaf3e2202512d6a6439e519a570
morenow <jjang9877@gmail.com>
2023-11-08 22:47:26 +0900

    test: Money를 LottoResults로부터 생성하는 로직 테스트 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,12 +1,20 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
+
+    @Test
+    @DisplayName("Money 생성 테스트")
+    void createMoneyTest() {
+        Money money = new Money(5000);
+        assertEquals(money.getMoney(), 5000);
+    }
 
     @Test
     @DisplayName("1000으로 나누어 떨어지지 않는 값으로 생성 불가능")
@@ -16,10 +24,13 @@ class MoneyTest {
     }
 
     @Test
-    @DisplayName("Money 생성 테스트")
-    void createMoneyTest() {
-        Money money = new Money(5000);
-        assertEquals(money.getMoney(), 5000);
+    @DisplayName("LottoResults 로부터 Money 생성 테스트")
+    void createMoneyFromLottoResultsTest() {
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lottos lottos = new Lottos(List.of(lotto));
+        LottoResults lottoResults = LottoResults.createLottoResults(lottos, winningNumbers);
+        Money revenue = Money.from(lottoResults);
+        assertThat(revenue.getMoney()).isEqualTo(2000000000);
     }
-
 }

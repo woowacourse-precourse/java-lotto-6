87569dc60d7faad3ef1f2983b72caec84174077e
GaHee99 <u1879@sookmyung.ac.kr>
2023-11-08 16:12:55 +0900

    feat(Rank): 등수 관련 enum 클래스 생성

---
+++ src/main/java/lotto/domain/enumerte/Rank.java
@@ -0,0 +1,45 @@
+package lotto.domain.enumerte;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(2000000000, 6),
+    SECOND(30000000, 5),
+    THIRD(1500000, 5),
+    FOURTH(50000, 4),
+    FIFTH(5000, 3),
+    NOTHING(0, 0);
+
+
+    private static final int SECOND_PRICE_COUNT = 5;
+    private static final int MATCH_BONUS_COUNT = 1;
+    private final int money;
+    private final int count;
+
+    Rank(int money, int count) {
+        this.money = money;
+        this.count = count;
+    }
+
+    public static Rank of(int count, int isBonus) {
+        if (isBonus == MATCH_BONUS_COUNT && count == SECOND_PRICE_COUNT) {
+            return Rank.SECOND;
+        }
+
+        return Arrays.stream(values())
+                .filter(value -> value != Rank.SECOND)
+                .filter(value -> value.count == count)
+                .findFirst()
+                .orElse(NOTHING);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+}

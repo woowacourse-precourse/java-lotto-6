b145bbedc06c68ab8fca6d7ebb1403dc260ba64e
songminhyuk <dnfldpden32@gmail.com>
2023-11-08 20:22:21 +0900

    feat: add_lotto 구현
    money값에 따라서 랜덤으로 생성되는 로또번호를 생성후 lottos에 저장하는 함수.

--- src/main/java/lotto/LottoManage.java
+++ src/main/java/lotto/LottoManage.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoManage {
@@ -14,18 +15,33 @@ public class LottoManage {
         //입력받은 돈 money에 저장
         this.money=Integer.parseInt(input);
         validate();
-        //당첨번호 생성
+        money/=1000;
+
+        //로또 번호 생성
+        for (int i=0;i<money;i++){
+            buy_Lotto();
+        }
     }
 
-    //사용자가 입력한 값이 올바른지 판단하는함수
+    //사용자가 입력한 돈이 올바른지 판단하는함수
     private void validate(){
         if(money<0||money%1000!=0){
             throw new IllegalArgumentException();
         }
     }
 
+    //correct_lotto와 bonus를 저장하는 함수
     public void setCorrect_lotto(List<Integer> input,int bonus){
         this.correct_lotto=new Lotto(input);
         this.bonus=bonus;
     }
+
+    //money값에 따라 lotto를 생성하는 함수
+    private void buy_Lotto(){
+        List<Integer> numbers=Randoms.pickUniqueNumbersInRange(1,45,6);
+        lottos.add(new Lotto(numbers));
+    }
+
+
+
 }

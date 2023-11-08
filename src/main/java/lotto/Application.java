5b1b824982b3eaefb8f28b9665f9733502e6ae33
kmw2378 <rlarla677@gmail.com>
2023-11-08 13:00:28 +0900

    feat(lotto): main 메서드에 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.GameManagerController;
+import lotto.service.GameManagerService;
+import lotto.service.LottoService;
+import lotto.view.ConsoleInputView;
+import lotto.view.ConsoleOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final GameManagerController gameManagerController = new GameManagerController(
+                new ConsoleInputView(),
+                new ConsoleOutputView(),
+                new GameManagerService(),
+                new LottoService());
+        gameManagerController.start();
     }
 }

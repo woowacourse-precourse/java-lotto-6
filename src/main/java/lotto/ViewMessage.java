071bb8b6b3d31102eb98853b0b9e97fb2f2de5b5
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 13:01:55 +0900

    style(ViewMessage) : 출력포맷을 실행결과 예시와 동일하도록 개행추가

--- src/main/java/lotto/ViewMessage.java
+++ src/main/java/lotto/ViewMessage.java
@@ -2,10 +2,10 @@ package lotto;
 
 public enum ViewMessage {
     PAYMENT_PROMPT_MESSAGE("구입금액을 입력해 주세요."),
-    NUMBER_OF_PURCHASED_FORMAT("%d개를 구매했습니다."),
-    WINNING_NUMBERS_PROMPT_MESSAGE("당첨 번호를 입력해 주세요."),
-    BONUS_PROMPT_MESSAGE("보너스 번호를 입력해 주세요."),
-    WINNING_RESULT_MESSAGE("당첨 통계\n---"),
+    NUMBER_OF_PURCHASED_FORMAT("\n%d개를 구매했습니다."),
+    WINNING_NUMBERS_PROMPT_MESSAGE("\n당첨 번호를 입력해 주세요."),
+    BONUS_PROMPT_MESSAGE("\n보너스 번호를 입력해 주세요."),
+    WINNING_RESULT_MESSAGE("\n당첨 통계\n---"),
     RATE_OF_REVENUE_FORMAT("총 수익률은 %.1f%%입니다.");
 
     private final String message;

1f0ab17dac1906cf867f75e9376a51a864031c30
limjongheok <jjongbbang2@naver.com>
2023-11-08 11:30:02 +0900

    로또 당첨 번호 비교하여 당첨 수 테스트

---
+++ src/test/java/lotto/lottoCompareTest/LottoCompareControllerTest.java
@@ -0,0 +1,45 @@
+package lotto.lottoCompareTest;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
+import lotto.domain.WinningLotto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoCompareControllerTest extends NsTest {
+    @Test
+    @DisplayName("각 로또 비교 당첨 수 테스트")
+    void lottoTicketsCompareNum() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+
+                    run("5000", "2,11,18,20,23,32", "1");
+
+                    Assertions.assertThat(WinningLotto.getTreeSameNumLotto()).isEqualTo(1);
+                    Assertions.assertThat(WinningLotto.getFourSameNumLotto()).isEqualTo(1);
+                    Assertions.assertThat(WinningLotto.getFiveSameNumLotto()).isEqualTo(1);
+                    Assertions.assertThat(WinningLotto.getFiveAndBonusSameNumLotto()).isEqualTo(1);
+                    Assertions.assertThat(WinningLotto.getSixSameNumLotto()).isEqualTo(1);
+
+
+                },
+                List.of(2, 11, 18, 20, 23, 32),
+                List.of(2, 11, 18, 20, 23, 1),
+                List.of(2, 11, 18, 20, 23, 30),
+                List.of(2, 11, 18, 20, 4, 5),
+                List.of(2, 11, 18, 33, 4, 5)
+        );
+    }
+    @Override
+    protected void runMain() {
+        {
+            Application.main(new String[]{});
+        }
+    }
+}

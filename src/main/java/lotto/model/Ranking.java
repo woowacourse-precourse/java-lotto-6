baba3093e93744d0545b5cc98734fc91e4edb883
ehdwoKIM <kinbell19@gmail.com>
2023-11-08 21:11:46 +0900

    [ADD] 순위 측정 로직

--- src/main/java/lotto/model/Ranking.java
+++ src/main/java/lotto/model/Ranking.java
@@ -2,16 +2,22 @@ package lotto.model;
 
 public enum Ranking {
 
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    FIVE_BONUS,
-    SIXTH;
+    ZERO(0,0),
+    ONE(0,0),
+    TWO(0,0),
+    THREE(3,5000),
+    FOUR(4,50000),
+    FIVE(5,1500000),
+    FIVE_BONUS(5,30000000),
+    SIXTH(6,200000000);
 
     private int count;
     private int reward;
+
+    Ranking(int count, int reward){
+        this.count = count;
+        this.reward = reward;
+    }
 
     public int getCount(){
         return count;

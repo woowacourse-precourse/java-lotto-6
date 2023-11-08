7e556cec4212ab3d78d3b4f1dd0b085f939c1d20
Ojh <201833022@yiu.ac.kr>
2023-11-08 15:54:12 +0900

    refactor: 당첨 번호 일부 수정 및 유효성 검사 상수 추가

--- src/main/java/lotto/model/WinLotto.java
+++ src/main/java/lotto/model/WinLotto.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class WinLotto {
 
-  private static final List<Lotto> winLotto = new ArrayList<>();
+  private static final String BONUS_NUMBER_WITH_LETTER_MESSAGE = "[ERROR] 숫자 하나만 입력해 주세요.";
+  private static final String INVALID_BONUS_NUMBER_RANGE_MESSAGE = "[ERROR] 입력 가능한 숫자의 범위는 1~45까지 입니다.";
+  private static final String NON_OVERLAPPING_NUMBER_MESSAGE = "[ERROR] 당첨 번호와 겹치지 않는 숫자를 입력해 주세요.";
+
+  private final List<Integer> winLotto;
   private final int bonusNumber;
 
-  public WinLotto(int bonusNumber) {
-    this.bonusNumber = bonusNumber;
+  private WinLotto(List<Integer> winLotto, String bonusNumber) {
+    this.winLotto = winLotto;
+    validateBonusNumRange(bonusNumber);
+    this.bonusNumber = isValidBonusNumber(bonusNumber);
+  }
+
+  public static WinLotto setWinLotto(List<Integer> numbers, String bonusNumber) {
+    return new WinLotto(numbers, bonusNumber);
   }
 
   public int getBonusNumber() {

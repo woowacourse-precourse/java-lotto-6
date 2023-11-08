1689f2f673ab57f8986a203618a404d8d650daea
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 14:41:00 +0900

    feat: 예외 메시지 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,10 +3,11 @@ package lotto.constant;
 public class ErrorMessage {
 
     public static final String NOT_NUMBER = "[ERROR] 입력값은 숫자여야 합니다.";
-    public static final String NOT_ENOUGH_MONEY = "[ERROR] 로또 구입 금액은 1000원 이상이어야 합니다. ";
-    public static final String INCORRECT_MONEY_UNIT = "[ERROR] 로또 구입 금액은 1000원 단위여야 합니다. ";
-    public static final String INCORRECT_NUMBER_OF_LOTTO = "[ERROR] 로또 당첨 번호는 6개입니다. ";
-    public static final String DUPLICATED_LOTTO = "[ERROR] 로또 당첨 번호는 서로 중복될 수 없습니다. ";
+    public static final String NOT_ENOUGH_MONEY = "[ERROR] 로또 구입 금액은 1000원 이상이어야 합니다.";
+    public static final String INCORRECT_MONEY_UNIT = "[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.";
+    public static final String INCORRECT_NUMBER_OF_LOTTO = "[ERROR] 로또 당첨 번호는 6개입니다.";
+    public static final String DUPLICATED_LOTTO = "[ERROR] 로또 당첨 번호는 서로 중복될 수 없습니다.";
     public static final String LOTTO_NUMBER_OVER_RANGE = "[ERROR] 로또 당첨 번호는 1이상 45이하의 숫자입니다.";
+    public static final String DUPLICATED_BONUS = "[ERROR] 보너스 번호는 당첨 번호와 중복 될 수 없습니다.";
 
 }

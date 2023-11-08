a436714e98fbcd1965f47087208d0a4a2dcf4dcc
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 11:57:10 +0900

    [feat] Validator - 보너스 넘버를 1개만 입력하도록 검증 로직

--- src/main/java/lotto/Validator/BonusNumberValidator.java
+++ src/main/java/lotto/Validator/BonusNumberValidator.java
@@ -1,4 +1,30 @@
 package lotto.Validator;
 
+import lotto.Exception.BonusNumberErrorMessage;
+
 public class BonusNumberValidator {
+    private static final String COMMA_SEPARATOR = ",";
+    private static final int BLANK_NUMBER = 0;
+    private static final int LIMITED_NUMBER = 1;
+
+    public static String validateBonusNumber(String input) {
+        validateNoBonusNumberEntered(input);
+        validateTooManyBonusNumbers(input);
+        return input;
+    }
+
+    private static void validateNoBonusNumberEntered(String input) {
+        String[] numbers = input.split(COMMA_SEPARATOR);
+        if (numbers.length == BLANK_NUMBER) {
+            throw new IllegalArgumentException(BonusNumberErrorMessage.NO_BONUS_NUMBER_ENTERED.getMessage());
+        }
+    }
+
+    private static void validateTooManyBonusNumbers(String input) {
+        String[] numbers = input.split(COMMA_SEPARATOR);
+        if (numbers.length > LIMITED_NUMBER) {
+            throw new IllegalArgumentException(BonusNumberErrorMessage.TOO_MANY_BONUS_NUMBERS.getMessage());
+        }
+    }
 }
+

836da3576bcd09ff840ceedc4d83adf48958521b
noah <jeonghogim318@gmail.com>
2023-11-08 23:49:12 +0900

    feat: 게임시작기능추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,20 @@
 package lotto.controller;public class LottoController {
+    public void run() {
+        try {
+            start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        int ticketCount = inputPlayerAmount();
+        OutputView.printTicketCount(ticketCount);
+
+        lottoList = makeLottoList(ticketCount);
+        winningResult = validateBonus();
+
+        lottoResult(lottoList, winningResult, ticketCount);
+
+    }
 }

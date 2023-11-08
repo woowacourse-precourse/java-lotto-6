fa2dd3e16e2d0aec5943f514d153f5bafbcfc673
Dawin <dawin002@naver.com>
2023-11-08 17:01:11 +0900

    feat(PurchaseAmount): 구매 금액 검증 기능 추가
    구매 금액 검증 기능 추가:
    - validateTicketNumberGreaterThanZero()
    - 구매 금액으로 발행할 수 있는 로또의 개수가 1보다 작은 경우 예외 발생

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -1,25 +1,36 @@
 package lotto.model;
 
 import lotto.model.enums.LottoSpec;
+import lotto.view.enums.ErrorMessage;
 
 public class PurchaseAmount {
     private int purchaseMoney;
 
     public PurchaseAmount(int purchaseMoney) {
-        validatePurchaseMoney(purchaseMoney);
+        validate(purchaseMoney);
         this.purchaseMoney = purchaseMoney;
     }
 
-    private void validatePurchaseMoney(int amount) {
-        if (amount % LottoSpec.PRICE.getValue() != 0) {
-            throw new IllegalArgumentException();
+    private void validate(int purchaseMoney) {
+        validateDivideByPrice(purchaseMoney);
+        validateTicketNumberGreaterThanZero(purchaseMoney);
+    }
+
+    private void validateDivideByPrice(int money) {
+        if (money % LottoSpec.PRICE.getValue() != 0) {
+            throw new IllegalStateException(ErrorMessage.CANT_DIVIDE.getMessage());
+        }
+    }
+
+    private void validateTicketNumberGreaterThanZero(int money) {
+        if (money / LottoSpec.PRICE.getValue() <= 0) {
+            throw new IllegalStateException(ErrorMessage.TICKET_NUMBER_LESS_THAN_ONE.getMessage());
         }
     }
 
     public int getPurchaseMoney() {
         return purchaseMoney;
     }
-
 
     public int getTicketNumber() {
         return purchaseMoney / LottoSpec.PRICE.getValue();

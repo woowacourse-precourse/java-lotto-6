3f8f8644fe388b952847d675b51a04fa11269aa9
Haerang <jbu418@naver.com>
2023-11-08 12:04:30 +0900

    로또 비교 후 등수 저장

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    int ranking;
+    int same = 0;
+    int bonus = 0;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -20,10 +23,46 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
 
-    void printLotto(){
+    void printLotto(){ // 로또 출력
         System.out.print(numbers.get(0));
         for(int i = 1; i < numbers.size(); i++){
             System.out.print(", " + numbers.get(i));
         }
+    }
+
+    void checking(int checkValue){ //당첨번호와 일치하면 same증가 보너스 번호와 일치하면 bonus증가
+        if(checkValue == 1){
+            same++;
+        }
+        if(checkValue == 2){
+            bonus++;
+        }
+    }
+
+    int rankLotto(int same, int bonus){ //로또 등수 매기기
+        if(same == 3){
+            return 5;
+        }
+        if(same == 4){
+            return 4;
+        }
+        if(same == 5 && bonus != 1){
+            return 3;
+        }
+        if(same == 5 && bonus == 1){
+            return 2;
+        }
+        if(same == 6){
+            return 1;
+        }
+        return 0;
+    }
+
+    void checkLotto(int[] prizeWin){ // 로또 비교
+        for(int i = 0; i < numbers.size(); i++) {
+            checking(prizeWin[numbers.get(i)]);
+        }
+
+        ranking = rankLotto(same, bonus);
     }
 }
--- src/main/java/lotto/test.java
+++ src/main/java/lotto/test.java
@@ -2,36 +2,108 @@ package lotto;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class test {
-    Game testgame = new Game();
+    Game testGame = new Game();
 
     @Test
-    void 당첨번호_분리_테스트(){
+    void 당첨번호_분리_입력_테스트() {
 
-        String testinput = "1,3,5,7,9,11";
-        String testnum[] = testinput.split(",");
-        String testbonus = "2";
-        final int RESULT = 8;
+        String testInput = "1,3,5,7,9,11";
+        String testNum[] = testInput.split(",");
+        String testBonus = "2";
+        final int RESULT = 8; // 1이 6개, 2가 1개, 나머지가 0인 배열이므로 총 합이 8이 나와야 한다.
         int sum = 0;
 
+        testGame.initPrizeWinNum();
+        testGame.setNum(testNum);
+        testGame.setBounusNum(testBonus);
 
-        testgame.initPrizeWinNum();
-        testgame.setNum(testnum);
-        testgame.setBounusNum(testbonus);
-
-        assertThat(testgame.prizeWinNum).hasSize(testgame.LIMIT_LOTTO_NUMBER + 1);
-
-        for(int i = 0; i < testgame.LIMIT_LOTTO_NUMBER; i++) {
-            if(i == 1 || i == 3 || i == 5 || i == 7 || 1 == 9 || i == 11)
-                assertThat(testgame.prizeWinNum[i]).isOne();
-            else if(i == 2){
-                assertThat(testgame.prizeWinNum[i]).isEqualTo(2);
+        for (int i = 1; i <= testGame.LIMIT_LOTTO_NUMBER; i++) {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || 1 == 9 || i == 11) // 당첨번호가 인덱스인 위치의 값이 1인지 확인
+                assertThat(testGame.prizeWinNum[i]).isOne();
+            else if (i == 2) {
+                assertThat(testGame.prizeWinNum[i]).isEqualTo(2); // 보너스번호가 인덱스인 위치의 값이 2인지 확인
             }
-            sum += testgame.prizeWinNum[i];
+            sum += testGame.prizeWinNum[i]; // 모든 배열안의 값을 더함
         }
 
         assertThat(sum).isEqualTo(RESULT);
+    }
+
+    @Test
+    void 번호비교_테스트() {
+        String testInput = "1,3,5,7,9,11";
+        String testNum[] = testInput.split(",");
+        String testBonus = "2";
+        List<Integer> testLottoNum = new ArrayList<Integer>();
+        Lotto testLotto;
+
+        testGame.initPrizeWinNum();
+        testGame.setNum(testNum);
+        testGame.setBounusNum(testBonus);
+
+        testLottoNum.add(1);
+        testLottoNum.add(2);
+        testLottoNum.add(3);
+        testLottoNum.add(4);
+        testLottoNum.add(5);
+        testLottoNum.add(6);
+
+        testLotto = new Lotto(testLottoNum);
+        testLotto.checkLotto(testGame.prizeWinNum);
+        assertThat(testLotto.ranking).isEqualTo(5);
+    }
+
+    @Test
+    void 번호비교_테스트2() {
+        String testInput = "1,3,5,7,9,11";
+        String testNum[] = testInput.split(",");
+        String testBonus = "2";
+        List<Integer> testLottoNum = new ArrayList<Integer>();
+        Lotto testLotto;
+        int i = 1;
+
+        testGame.initPrizeWinNum();
+        testGame.setNum(testNum);
+        testGame.setBounusNum(testBonus);
+
+        testLottoNum.add(1);
+        testLottoNum.add(3);
+        testLottoNum.add(5);
+        testLottoNum.add(7);
+        testLottoNum.add(9);
+        testLottoNum.add(11);
+
+        testLotto = new Lotto(testLottoNum);
+        testLotto.checkLotto(testGame.prizeWinNum);
+        assertThat(testLotto.ranking).isEqualTo(1);
+    }
+
+    @Test
+    void 번호비교_테스트3() {
+        String testInput = "1,3,5,7,9,11";
+        String testNum[] = testInput.split(",");
+        String testBonus = "2";
+        List<Integer> testLottoNum = new ArrayList<Integer>();
+        Lotto testLotto;
+
+        testGame.initPrizeWinNum();
+        testGame.setNum(testNum);
+        testGame.setBounusNum(testBonus);
+
+        testLottoNum.add(1);
+        testLottoNum.add(2);
+        testLottoNum.add(3);
+        testLottoNum.add(5);
+        testLottoNum.add(7);
+        testLottoNum.add(9);
+
+        testLotto = new Lotto(testLottoNum);
+        testLotto.checkLotto(testGame.prizeWinNum);
+        assertThat(testLotto.ranking).isEqualTo(2);
     }
 }

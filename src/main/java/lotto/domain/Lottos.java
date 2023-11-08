977a00f5ce68749699fbc9b6889a7a91b277c20d
kimdongyeon <do121712@naver.com>
2023-11-08 22:46:26 +0900

    refactor : 매직넘버 수정

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -7,13 +7,16 @@ import java.util.List;
 
 public class Lottos {
     private List<Lotto> lotto;
+    private final int MIN_LOTTO_NUM = 1;
+    private final int MAX_LOTTO_NUM = 45;
+    private final int LOTTO_COUNT = 6;
 
     public Lottos() {
         this.lotto = new ArrayList<>();
     }
 
     private List<Integer> pickNum() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_COUNT);
     }
 
     public void pickLottos() {

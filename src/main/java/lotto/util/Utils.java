6ba7b9ae707e8daa370aa633ab4eb23fee9ff0fd
baejeong-eun <lovelyeun0914@gmail.com>
2023-11-08 19:01:41 +0900

    feat: 오름정렬 및 Util에 함수추가

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -1,8 +1,51 @@
 package lotto.util;
 
-public class Utils {
 
-    public static int changeStringToInt(String number){
-        return  Integer.parseInt(number);
+import lotto.controller.Controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static lotto.view.outputMessage.printMessage;
+
+public class Utils {
+    public static List<String> splitName(String input){
+        return List.of(input.split(","));
     }
+
+    public static int changeStringToInt(String input){
+        return changeNumber(input);
+    }
+
+    public static int changeNumber(String input) {
+        int number = 0;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            Error error = Error.NOT_NUMBER;
+            backFunction(error);
+        }
+
+        return number;
+    }
+
+    public static void backFunction(Error error){
+        try {
+            throw new IllegalArgumentException();
+        } catch (IllegalArgumentException ex) {
+            printMessage(error.getMsg());
+
+            Controller controller = new Controller();
+            controller.playGame();
+        }
+    }
+
+
+    public static List<Integer> ascendingOrder(List<Integer> numbers) {
+        List<Integer> orderedList = new ArrayList<Integer>(numbers);
+        Collections.sort(orderedList);
+        return orderedList;
+    }
+
 }

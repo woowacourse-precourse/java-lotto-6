57f7bc8048b826421cb52a5a451cd4f220a28154
K-KY <kyuyoungk@naver.com>
2023-11-08 11:46:47 +0900

    Feat(OutputView) : 메세지 출력 메소드

---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,29 @@
+package lotto;
+
+public class OutputView {
+
+    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String PURCHASED = "개를 구매했습니다.";
+
+
+    public static void printInputMoney() {
+        System.out.println(INPUT_MONEY);
+    }
+
+    public static void printPurchased(int size) {
+        System.out.println("\n" + size + PURCHASED);
+    }
+
+    public static void printGeneratedLotto(String lottos) {
+        System.out.println(lottos);
+    }
+    public static void printInputWinNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public static void printInputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+    public static void printWinStatistics() {
+        System.out.println("");
+    }
+}

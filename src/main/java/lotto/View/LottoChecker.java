b0ffedc6c6911d662bcd60b4911466186b69f89f
GyungA <GyungA@users.noreply.github.com>
2023-11-08 22:20:35 +0900

    feat: 상금 Enum Class 생성

---
+++ src/main/java/lotto/View/LottoChecker.java
@@ -0,0 +1,10 @@
+package lotto.View;
+
+public class LottoChecker {
+    private static final String CHECK_START_MESSAGE = "\n당첨 통계\n---";
+    public static final Integer FIRST_PRIZE = PrizeMoney.FIRST.getPrizeAmount();
+    public static final Integer SECOND_PRIZE = PrizeMoney.SECOND.getPrizeAmount();
+    public static final Integer THIRD_PRIZE = PrizeMoney.THIRD.getPrizeAmount();
+    public static final Integer FOURTH_PRIZE = PrizeMoney.FOURTH.getPrizeAmount();
+    public static final Integer FIFTH_PRIZE = PrizeMoney.FIFTH.getPrizeAmount();
+}

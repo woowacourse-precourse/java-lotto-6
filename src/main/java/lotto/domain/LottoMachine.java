ceaa23a1830709f38cd1290fc90f044a4a3d1fd9
Hanbee Lee <bee1162@naver.com>
2023-11-08 16:20:28 +0900

    fix : LottoMachine생성자 생성 후 Numbers에 대한 getter생성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,20 +3,32 @@ package lotto.domain;
 import lotto.util.RandomLottoGenerator;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoMachine {
+    private final List<Integer> randomNumbers;
+    public LottoMachine() {
+        this.randomNumbers = getRandomNumbers();
+    }
 
-    public static List<Integer> getRandomNumbers() {
-        List<Integer> randomNumbers = RandomLottoGenerator.getNumbers();
-        validate(randomNumbers);
+    public List<Integer> getRandomNumbers() {
+        List<Integer> randomNumber = sortRandomNumbers(RandomLottoGenerator.getNumbers());
+        validate(randomNumber);
+        return randomNumber;
+    }
+
+    public List<Integer> getNumbers() {
         return randomNumbers;
     }
 
-    private static void validate(List<Integer> randomNumbers) {
+    private void validate(List<Integer> randomNumbers) {
         boolean duplicated = randomNumbers.stream().distinct().count() != randomNumbers.size();
         if (duplicated) {
             getRandomNumbers();
         }
+    }
+    private List<Integer> sortRandomNumbers(List<Integer> numbers) {
+        return numbers.stream().sorted().collect(Collectors.toList());
     }
 
 }

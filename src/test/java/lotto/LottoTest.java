a5b204fc25823fd249db90546ba6c412881f4167
th0267 <yth0779@naver.com>
2023-11-08 23:43:23 +0900

    feat: 구입금액 기능추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,15 @@
 package lotto;
-
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,6 +27,33 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @DisplayName("1 미만, 45 초과의 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("generateWrongRangeNumber")
+    void createLottoByWrongRangeNumber(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    static Stream<Arguments> generateWrongRangeNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 0, 4, 5, 6)),
+                Arguments.of(List.of(10, 11, 12, 45, 50, 23))
+        );
+    }
+
+    @DisplayName("로또 번호가 정렬이 되는지 확인한다.")
+    @Test
+    void sortedLottoNumber() {
+        // given
+        List<Integer> numbers = List.of(6, 4, 5, 2, 1, 3);
+        List<Integer> answerNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = new Lotto(numbers);
+
+        // then
+        assertThat(lotto.toString()).isEqualTo(answerNumbers.toString());
+    }
     // 아래에 추가 테스트 작성 가능
 }

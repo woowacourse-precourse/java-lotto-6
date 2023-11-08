4733273be9da04d8573067093fdd607fa1b2f500
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 12:07:31 +0900

    [feat] Service - 당첨 번호, 보너스 번호 비교하여 로또 추첨 결과 계산
    Signed-off-by: Hyunsoo Kim <sgn06221@gmail.com>

--- src/main/java/lotto/Service/LottoService.java
+++ src/main/java/lotto/Service/LottoService.java
@@ -2,6 +2,9 @@ package lotto.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.Domain.BonusNumber;
+import lotto.Domain.Lotto;
+import lotto.Domain.LottoResult;
 import lotto.Domain.Money;
 import lotto.Domain.LottoNumbers;
 
@@ -22,4 +25,31 @@ public class LottoService {
         return lottoTickets;
     }
 
+    public List<LottoResult> playLottoGame(Money money, LottoNumbers lottoNumbers, Lotto winningLotto,
+                                           int bonusNumber) {
+        int numberOfDraws = money.getPurchaseAmount() / STANDARD_UNIT;
+        List<LottoResult> results = new ArrayList<>();
+
+        for (int draw = 1; draw <= numberOfDraws; draw++) {
+            List<Integer> userNumbers = lottoNumbers.getNumbers();
+
+            int matchedNumbers = countMatchedNumbers(userNumbers, winningLotto.getNumbers());
+            boolean matchedBonusNumber = userNumbers.contains(bonusNumber);
+
+            LottoResult result = new LottoResult(matchedNumbers, matchedBonusNumber);
+            results.add(result);
+        }
+        return results;
+    }
+
+    private int countMatchedNumbers(List<Integer> userNumbers, List<Integer> winningNumbers) {
+        int matchedNumber = 0;
+
+        for (int userNumber : userNumbers) {
+            if (winningNumbers.contains(userNumber)) {
+                matchedNumber++;
+            }
+        }
+        return matchedNumber;
+    }
 }

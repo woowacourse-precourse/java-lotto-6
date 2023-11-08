54889991cbb50546d992b13bb964776a3785b764
pjaehyun <show6756@gmail.com>
2023-11-08 13:24:10 +0900

    feat: 추가 User Class, 예외처리 및 생성자

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,19 @@
 package user;
 
+import lotto.LottoCollection;
+
+import static validate.InputValidation.*;
+
 public class User {
+    private final LottoCollection lottos;
+
+    public User(int purchaseAmount) {
+        validate(purchaseAmount);
+        this.lottos = new LottoCollection(purchaseAmount);
+    }
+
+    private void validate(int purchaseAmount) {
+        validateAmount(purchaseAmount);
+    }
+
 }

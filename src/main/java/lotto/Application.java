45542471a1c48039d01cfbfd6728cbb43d252458
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:18:21 +0900

    refactor : LottoController 패키지 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import lotto.controller.LottoController;
+import lotto.controller.lotto.LottoController;
 
 public class Application {
     public static void main(String[] args) {

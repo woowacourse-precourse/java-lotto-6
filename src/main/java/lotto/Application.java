affcb67d351d9355ab829602a7881b37bc436b4c
hwangkyojune <shane5969@naver.com>
2023-11-08 21:45:28 +0900

    Domain객체를 받도록 생성자 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import lotto.domain.Domain;
+import lotto.gameplayer.GamePlayer;
+
 public class Application {
     public static void main(String[] args) {
-        GamePlayer gamePlayer = new GamePlayer();
+        GamePlayer gamePlayer = new GamePlayer(new Domain());
         gamePlayer.run();
     }
 }

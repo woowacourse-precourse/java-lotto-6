4aa6ca021ee68bbe769eefd60edc734f32719bc8
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-08 18:08:00 +0900

    refactor: 불필요한 요소 제거

--- src/main/java/lotto/util/Constants.java
+++ src/main/java/lotto/util/Constants.java
@@ -5,7 +5,6 @@ public class Constants {
     public static final int MIN_NUMBER_RANGE = 1;
     public static final int MAX_NUMBER_RANGE = 45;
     public static final int CONFIG_NUMBERS_SIZE = 6;
-    public static final int REMOVE_INDEX = 2;
     public static final int MIN_PRICE = 1000;
     public static final int DIVIDE_PRICE = 1000;
 }

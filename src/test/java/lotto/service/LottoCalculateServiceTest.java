d77035fcad70aed7e033d0918989d077b810803a
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 14:19:58 +0900

    test(LottoCalculateServiceTest) : 로또 번호 매칭 개수 5개 테스트 코드 작성

--- src/test/java/lotto/service/LottoCalculateServiceTest.java
+++ src/test/java/lotto/service/LottoCalculateServiceTest.java
@@ -78,4 +78,26 @@ public class LottoCalculateServiceTest {
         // then
         assertThat(result).isEqualTo(true);
     }
+
+    @DisplayName("로또 번호 매칭 개수 5개 테스트")
+    @Test
+    public void isNumberMatchedFiveTest() {
+        // given
+        int countContainsOfLotto = 5;
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(2);
+        lottoNumbers.add(3);
+        lottoNumbers.add(4);
+        lottoNumbers.add(5);
+        lottoNumbers.add(8);
+        Lotto lotto = new Lotto(lottoNumbers);
+        lotto.setBonusNumber(10);
+        // when
+        boolean result = lottoCalculateService.isNumberMatchedFive(countContainsOfLotto, numbers, lotto);
+        // then
+        assertThat(result).isEqualTo(true);
+    }
 }

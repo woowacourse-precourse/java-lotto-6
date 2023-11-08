4a65c27aacfb3e1dccdff746fc544389e905a91b
nayonsoso <panda0329@naver.com>
2023-11-08 16:06:36 +0900

    refactor: 순위 통계 클래스 생성자 수정

--- src/main/java/lotto/model/RankStatistics.java
+++ src/main/java/lotto/model/RankStatistics.java
@@ -1,18 +1,25 @@
 package lotto.model;
 
 import java.util.EnumMap;
+import java.util.List;
 
 public class RankStatistics {
 
-    private EnumMap<Rank, Integer> rankStatistics = new EnumMap<>(Rank.class);
+    private final EnumMap<Rank, Integer> rankStatistics = new EnumMap<>(Rank.class);
+
+    RankStatistics(List<Rank> ranks) {
+        for (Rank rank : ranks) {
+            this.add(rank);
+        }
+    }
+
+    private void add(Rank rank) {
+        int rankCount = rankStatistics.getOrDefault(rank, 0);
+        rankStatistics.put(rank, rankCount + 1);
+    }
 
     public EnumMap<Rank, Integer> getRankStatistics() {
         return this.rankStatistics;
-    }
-
-    public void add(Rank rank) {
-        int rankCount = rankStatistics.getOrDefault(rank, 0);
-        rankStatistics.put(rank, rankCount + 1);
     }
 
     public int getTotalPrize() {

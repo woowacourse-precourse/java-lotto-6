1d4279561bd20a04b52ba7412dda621fbc3ea759
mango606 <yuj08184@kangwon.ac.kr>
2023-11-08 22:14:32 +0900

    fix: change output

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,11 +1,11 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000, true),
+    FIRST(3, 5_000),
+    SECOND(4, 50_000),
     THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
+    FOURTH(5, 30_000_000, true),
+    FIFTH(6, 2_000_000_000),
     NONE(0, 0);
 
     private final int matchCount;

163a124b134898a228f1061a76510ae5f1c9f0dd
HYOAN <aspqll@naver.com>
2023-11-08 21:17:09 +0900

    fix(lotto): 2등 결과 저장시 중복 저장되는 오류 수정

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,16 +1,35 @@
 package lotto;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoTicket {
+    private static List<Integer> lottoResult = new ArrayList<>();
     private List<Integer> numbers = new ArrayList<>();
     private int matchCount;
-    private boolean bonusNumberMatch = false;
 
     public LottoTicket(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    public static List<Integer> getLottoResult() {
+        System.out.println(lottoResult);
+        Collections.sort(lottoResult);
+        System.out.println(lottoResult);
+        return lottoResult;
+    }
+
+    private void setLottoResult(int matchCount) {
+        if (matchCount == 6)
+            removeLastElement();
+        lottoResult.add(matchCount);
+    }
+
+    private void removeLastElement() {
+        if (!lottoResult.isEmpty()) {
+            lottoResult.remove(lottoResult.size() - 1); // 마지막 요소 삭제
+        }
     }
 
     public List<Integer> getNumbers() {
@@ -22,14 +41,8 @@ public class LottoTicket {
     }
 
     public void setMatchCount(int matchCount) {
+        setLottoResult(matchCount);
         this.matchCount = matchCount;
     }
 
-    public boolean isBonusNumberMatch() {
-        return bonusNumberMatch;
-    }
-
-    public void setBonusNumberMatch(boolean bonusNumberMatch) {
-        this.bonusNumberMatch = bonusNumberMatch;
-    }
 }

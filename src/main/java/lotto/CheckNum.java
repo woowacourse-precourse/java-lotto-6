7763d34fa585f26501f67a624f5dd47d0bbe099c
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-08 15:28:03 +0900

    refactor: 파라미터 String -> List<Integer>로 수정

--- src/main/java/lotto/CheckNum.java
+++ src/main/java/lotto/CheckNum.java
@@ -6,17 +6,15 @@ import java.util.List;
 
 public class CheckNum {
 
-	public static List<WinningRank> lotto(List<List<Integer>> lottos, String mynumber, String bonusnum) {
+	public static List<WinningRank> lotto(List<List<Integer>> lottos, List<Integer> inputNumber, int inputBonus) {
 
 		List<WinningRank> rankingList = new ArrayList<>();
-		String[] usernums = mynumber.split(",");
-		int bonus = Integer.parseInt(bonusnum);
 
 		for (int i = 0; i < lottos.size(); i++) {
 			List<Integer> lotto = lottos.get(i);
-			int matching = checkMatchingNumbers(lotto, usernums);
+			int matching = checkMatchingNumbers(lotto, inputNumber);
 
-			if (matching == 5 && lottos.contains(bonus)) {
+			if (matching == 5 && lottos.contains(inputBonus)) {
 				rankingList.add(WinningRank.SECOND);
 			}
 			WinningRank rank = WinningRank.findByMatchingNumbers(matching);
@@ -26,15 +24,16 @@ public class CheckNum {
 		return rankingList;
 	}
 
-	private static int checkMatchingNumbers(List<Integer> lotto, String[] usernums) {
-
-		int matching = 0;
-		for (Integer mynums : lotto) {
-			if (Arrays.asList(usernums).contains(mynums.toString())) {
-				matching++;
-			}
-		}
-		return matching;
+	private static int checkMatchingNumbers(List<Integer> lotto, List<Integer> usernums) {
+	    int matching = 0;
+	    
+	    for (Integer mynums : lotto) {
+	        if (usernums.contains(mynums)) {
+	            matching++;
+	        }
+	    }
+	    
+	    return matching;
 	}
 
 }

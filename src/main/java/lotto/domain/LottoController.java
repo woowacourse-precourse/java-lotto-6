14f3ccd3e34fbb56be1ec810a6c30bc21afcc21e
Choijake <shameless8@naver.com>
2023-11-08 13:24:20 +0900

    Feat : LottoController 초안

--- src/main/java/lotto/domain/LottoController.java
+++ src/main/java/lotto/domain/LottoController.java
@@ -1,4 +1,36 @@
 package lotto.domain;
 
 public class LottoController {
+    private LottoController() {
+        set();
+        showResult();
+    }
+
+    public static LottoController start() {
+        return new LottoController();
+    }
+
+    //티켓수, 당첨번호, 보너스번호 세팅
+    public void set() {
+        setBuyAmount();
+        setWinningNumber();
+        setBonusNumber();
+    }
+
+    private void setBuyAmount() {
+
+    }
+
+    private void setWinningNumber() {
+
+    }
+
+    private void setBonusNumber() {
+
+    }
+
+    //결과 생성
+    public void showResult() {
+
+    }
 }

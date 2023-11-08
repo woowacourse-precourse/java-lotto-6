be40bf86d4a0315b7038330d9c59ad45063cc2ed
wonindev <sangwonparkdev@gmail.com>
2023-11-08 16:43:20 +0900

    Feat: 사용자에게 금액과 당첨번호를 입력받고 검증하는 기능 구현

--- src/main/java/lotto/UI/InputHandler.java
+++ src/main/java/lotto/UI/InputHandler.java
@@ -1,7 +1,11 @@
 package lotto.UI;
 
-import lotto.utils.Validator;
 import lotto.constants.ErrorMessage;
+import lotto.constants.LottoEnum;
+import lotto.utils.CommonHelper;
+import lotto.utils.Validator;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -22,17 +26,76 @@ public class InputHandler {
         return input;
     }
 
-    private boolean costValidate(String input) {
-        if (Validator.isNull(input)) throw new IllegalArgumentException(ErrorMessage.NULL_ERROR.getError());
-        if (Validator.isBlank(input)) throw new IllegalArgumentException(ErrorMessage.BLANK_ERROR.getError());
-        if (Validator.isContainBlank(input))
-            throw new IllegalArgumentException(ErrorMessage.CONTAIN_BLANK_ERROR.getError());
-        if (!Validator.isNumber(input)) throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_ERROR.getError());
-        if (Validator.underByLottoPrice(input))
-            throw new IllegalArgumentException(ErrorMessage.UNDER_LOTTO_PRICE_ERROR.getError());
-        if (!Validator.isDivisibleByLottoSize(input))
-            throw new IllegalArgumentException(ErrorMessage.NOT_DIVISIBLE_1000_ERROR.getError());
+    public List<String> inputWinningNumbers() {
+        String input;
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                input = readLine();
+                winningNumberValidate(input);
+                if (splitedLottoNumbersValidate(input)) {
+                    break;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return CommonHelper.splitComma(input);
+    }
 
+    public String inputBonusNumber() {
+        String input;
+
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                input = readLine();
+                if (numberValidate(input)) break;
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return input;
+    }
+
+    private boolean costValidate(String input) {
+        Validator.isNull(input);
+        Validator.isBlank(input);
+        Validator.isContainBlank(input);
+        Validator.notNumber(input);
+        Validator.underByLottoPrice(input);
+        Validator.isDivisibleByLottoSize(input);
+        return true;
+    }
+
+    private boolean winningNumberValidate(String input) {
+        Validator.isNull(input);
+        Validator.isBlank(input);
+        Validator.isContainBlank(input);
+        return true;
+    }
+
+    private boolean numberValidate(String input) {
+        Validator.isNull(input);
+        Validator.isBlank(input);
+        Validator.isContainBlank(input);
+        Validator.notNumber(input);
+        Validator.isOneToFortyFive(Integer.parseInt(input));
+        return true;
+    }
+
+    public boolean splitedLottoNumbersValidate(String input) {
+        List<String> splitedLottoNumbers = List.of(input.split(","));
+
+        if (splitedLottoNumbers.size() != LottoEnum.LOTTO_SIZE.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBER_ERROR.getError());
+        }
+
+        for (int i = 0; i < 6; i++) {
+            numberValidate(splitedLottoNumbers.get(i));
+            Validator.isDuplicate(splitedLottoNumbers, splitedLottoNumbers.get(i), i);
+        }
         return true;
     }
 }

2dad8ea2c71650ef80298cbcf6d6a8d6b98ded75
constant94-dev <constant94@naver.com>
2023-11-08 16:17:57 +0900

    feat(message): 당첨 요금 결과 메시지 상수와 메서드 구현

--- src/main/java/lotto/message/LottoType.java
+++ src/main/java/lotto/message/LottoType.java
@@ -1,2 +1,26 @@
-package lotto.message;public enum LottoType {
+package lotto.message;
+
+public enum LottoType {
+    FIVE_THOUSAND(3, "5000"),
+    FIFTY_THOUSAND(4, "50000"),
+    ONE_HALF_MILLION(5, "1500000"),
+    THIRTY_MILLION(5, "30000000"),
+    TWO_HUNDRED_MILLION(6, "2000000000"),
+    EARNING_RATE(0, "0.0%");
+
+    private int prizeOrdinal;
+    private String prizeCost;
+
+    LottoType(int prizeOrdinal, String prizeCost) {
+        this.prizeOrdinal = prizeOrdinal;
+        this.prizeCost = prizeCost;
+    }
+
+    public int getPrizeOrdinal() {
+        return prizeOrdinal;
+    }
+
+    public String getPrizeCost() {
+        return prizeCost;
+    }
 }
--- src/main/java/lotto/message/ResultMessage.java
+++ src/main/java/lotto/message/ResultMessage.java
@@ -1,2 +1,22 @@
-package lotto.message;public enum ResultMessage {
+package lotto.message;
+
+public enum ResultMessage {
+    RESULT_MESSAGE_HEADER("당첨 통계"),
+    RESULT_MESSAGE_SEPARATOR("---"),
+    RESULT_MESSAGE_3_MATCH("3개 일치 (5,000원) - "),
+    RESULT_MESSAGE_4_MATCH("4개 일치 (50,000원) - "),
+    RESULT_MESSAGE_5_MATCH("5개 일치 (1,500,000원) - "),
+    RESULT_MESSAGE_5_BONUS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    RESULT_MESSAGE_6_MATCH("6개 일치 (2,000,000,000원) - "),
+    RESULT_MESSAGE_TOTAL_INCOME("총 수익률은 ");
+
+    private final String message;
+
+    ResultMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

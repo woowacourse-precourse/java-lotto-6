1b30e81efeca417b50d32509ad1a04f0c7491d75
Dawin <dawin002@naver.com>
2023-11-08 16:28:56 +0900

    refactor(WinningLotto): 당첨 로또 번호 검증 기능 수정
    당첨 로또 번호 검증 기능 추가:
    - validateBonusInRange(): 보너스 번호 숫자 범위 검증
    검증시 발생하는 예외 종류, 예외 메시지 수정
    - validateLength()
    - validateNumberInRange()
    - validateDuplicate()

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
 import lotto.model.enums.LottoSpec;
+import lotto.view.enums.ErrorMessage;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class WinningLotto extends Lotto {
@@ -9,24 +11,52 @@ public class WinningLotto extends Lotto {
 
     public WinningLotto(List<Integer> numbers, int bonus) {
         super(numbers);
+        validateBonus(numbers, bonus);
+        this.bonus = bonus;
+    }
+
+    private void validateBonus(List<Integer> numbers, int bonus) {
         validateBonusInRange(bonus);
         validateBonusDuplicate(numbers, bonus);
-        this.bonus = bonus;
     }
 
     public int getBonus() {
         return bonus;
     }
 
-    private void validateBonusDuplicate(List<Integer> numbers, int bonus) {
-        if (numbers.contains(bonus)) {
-            throw new IllegalArgumentException("당첨 번호와 보너스 번호가 중복되었습니다.");
+    private void validateBonusInRange(int bonus) {
+        if (bonus < LottoSpec.MIN_NUMBER.getValue() || bonus > LottoSpec.MAX_NUMBER.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_OUT_OF_RANGE.getMessage());
         }
     }
 
-    private void validateBonusInRange(int bonus) {
-        if (bonus < LottoSpec.MIN_NUMBER.getValue() || bonus > LottoSpec.MAX_NUMBER.getValue()) {
-            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    private void validateBonusDuplicate(List<Integer> numbers, int bonus) {
+        if (numbers.contains(bonus)) {
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_DUPLICATED.getMessage());
+        }
+    }
+
+    @Override
+    protected void validateLength(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalStateException(ErrorMessage.WINNING_NUMBERS_WRONG_SIZE.getMessage());
+        }
+    }
+
+    @Override
+    protected void validateNumberInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LottoSpec.MIN_NUMBER.getValue() || number > LottoSpec.MAX_NUMBER.getValue()) {
+                throw new IllegalStateException(ErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != 6) {
+            throw new IllegalStateException(ErrorMessage.WINNING_NUMBER_DUPLICATED.getMessage());
         }
     }
 }

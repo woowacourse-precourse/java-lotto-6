c15c66c2bc4c275fa45ae7bc7a166fce057323f3
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:32:45 +0900

    [REFACTOR] 불필요한 변수 제거

--- src/main/java/lotto/io/ConsoleAdapter.java
+++ src/main/java/lotto/io/ConsoleAdapter.java
@@ -9,10 +9,6 @@ public class ConsoleAdapter {
         return Console.readLine();
     }
 
-    public void close() {
-        Console.close();
-    }
-
     public void setInput(String input) {
         this.input = input;
     }

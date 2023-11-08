1478a770611539e70b1ab2d0364b37772cc9d5fe
minkyole <7719s@nave.com>
2023-11-08 16:44:07 +0900

    feat(OutputView.java) : 발행한 로또 수량 및 번호 입력 안내메시지 출력
    - 발행한 로또 수량 및 번호 입력 안내메시지 출력완료

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public class OutputView {
     public void printInputPurchaseAmountMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printPurchaseLottoesMessage(List<Lotto> lottoes) {
+        System.out.println();
+        System.out.println(lottoes.size() + "개를 구매했습니다.");
+        for (Lotto currentLotto : lottoes) {
+            System.out.println(currentLotto.getNumbers());
+        }
     }
 }

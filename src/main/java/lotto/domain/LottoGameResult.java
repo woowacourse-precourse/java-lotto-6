e86e26e0f3490b738e0ad493ccf96325e90eadaa
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-08 18:38:42 +0900

    refactor: 수익률 천 단위마다 콤마(,) 표시 추가

--- src/main/java/lotto/domain/LottoGameResult.java
+++ src/main/java/lotto/domain/LottoGameResult.java
@@ -1,13 +1,12 @@
 package lotto.domain;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.EnumMap;
 import java.util.Map;
 import lotto.enums.LottoPrize;
+import lotto.utils.Parser;
 
 public class LottoGameResult {
-    private static final String CURRENCY_PATTERN = "###,###.##";
     private static final String DEFAULT_RANK_PRINT_FORMAT = "%d개 일치 (%s원) - %d개";
     private static final String BONUS_RANK_PRINT_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
 
@@ -39,11 +38,7 @@ public class LottoGameResult {
     private static String buildFormattedResult(LottoPrize prize, int count, String Sentence) {
         int matchCount = prize.getMatchCount();
         BigDecimal prizeAmount = BigDecimal.valueOf(prize.getPrizeAmount());
-        return String.format(Sentence, matchCount, formatCurrency(prizeAmount), count);
+        return String.format(Sentence, matchCount, Parser.formatMoneyCurrency(prizeAmount), count);
     }
 
-    private static String formatCurrency(BigDecimal amount) {
-        DecimalFormat decimalFormat = new DecimalFormat(CURRENCY_PATTERN);
-        return decimalFormat.format(amount);
-    }
 }
--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,10 +1,14 @@
 package lotto.utils;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
     private static final String DELIMITER = ",";
+    private static final String MONEY_CURRENCY_PATTERN = "###,###";
+    private static final String PROFIT_CURRENCY_PATTERN = "###,###.0";
     private final LottoValidator lottoValidator;
 
     public Parser(LottoValidator lottoValidator) {
@@ -22,4 +26,18 @@ public class Parser {
     private Integer convertToInt(String number) {
         return lottoValidator.validateNonNumeric(number);
     }
+
+    public static String formatCurrency(BigDecimal amount, String pattern) {
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        return decimalFormat.format(amount);
+    }
+
+    public static String formatMoneyCurrency(BigDecimal amount) {
+        return formatCurrency(amount, MONEY_CURRENCY_PATTERN);
+    }
+
+    public static String formatProfitCurrency(BigDecimal amount) {
+        return formatCurrency(amount, PROFIT_CURRENCY_PATTERN);
+    }
+
 }

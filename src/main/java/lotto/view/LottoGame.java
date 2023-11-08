5c8207c8e6d374df1022e446fd3a9c5cab7a51ff
dabin <db021129@naver.com>
2023-11-08 16:18:16 +0900

    feat : showLotto done

--- src/main/java/lotto/view/LottoGame.java
+++ src/main/java/lotto/view/LottoGame.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.controller.LottoController;
+import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Price;
 
@@ -8,13 +9,22 @@ public class LottoGame {
 
     private final LottoController lottoController = new LottoController();
 
-    public LottoGame() {
+
+    public Price bougthPrice() {
         int lottoPrice = lottoController.LottoSetPrice();
         Price price = new Price(lottoPrice);
+
+        return price;
     }
 
     public Lottos showLotto(Price price) {
         int boughtCount = price.getPrice()/1000;
         System.out.println(String.format("%d개를 구매했습니다.", boughtCount));
+        Lottos lottos = new Lottos(lottoController.makeLottos(boughtCount));
+        for (int i = 0; i < boughtCount; i++) {
+            System.out.println(lottos.getLottos().get(i));
+        }
+
+        return lottos;
     }
 }

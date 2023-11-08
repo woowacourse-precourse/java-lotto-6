23b5b7577e476e2b5c2c3bec7d5b58733730b78c
Sungkyu <kxu4583@naver.com>
2023-11-08 15:38:57 +0900

    feat: '로또 클래스' 기능 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 1~45 사이의 숫자(정수)만 입력 가능합니다.");
+            }
+        }
+    }
+
+}
+

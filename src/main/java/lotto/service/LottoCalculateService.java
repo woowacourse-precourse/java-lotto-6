d4dc37f548666ed2ca01d46e60c265b5bffda1ad
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 14:42:03 +0900

    feat(LottoCalculateService) : countLottoWinningCount 매서드 생성

--- src/main/java/lotto/service/LottoCalculateService.java
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -2,10 +2,12 @@ package lotto.service;
 
 import lotto.domain.Lotto;
 import lotto.domain.User;
+import lotto.domain.UserLotto;
 import lotto.domain.Winning;
 import lotto.view.OutputView;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class LottoCalculateService {
     private OutputView outputView = new OutputView();
@@ -15,6 +17,12 @@ public class LottoCalculateService {
         countLottoWinningCount(user, lotto);
         outputView.responseWinningHistory(countOfWinning);
         outputView.responseYieldOfLotto(getYieldOfLotto(user, user.getWinningPrice()));
+    }
+
+    public void countLottoWinningCount(User user, Lotto lotto) {
+        List<UserLotto> userLottos = user.getLottos();
+        userLottos.stream()
+                .forEach(userLotto -> countLottoWinning(user, userLotto.getNumbers(), lotto));
     }
 
 }

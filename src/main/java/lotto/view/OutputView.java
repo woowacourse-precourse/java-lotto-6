5ba4160fa2bad831fe08ca95dc2037307e512434
JoonKyo <joonkyolee@gmail.com>
2023-11-08 17:21:09 +0900

    docs, feat(OutputView): 플레이어에게 입력을 받기 위한 메시지를 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,23 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import static lotto.view.OutputMessage.*;
+
+public class OutputView {
+    public void askPurchaseAmount() {
+        System.out.println(ENTER_PURCHASE_AMOUNT.getMessage());
+    }
+
+    public void askLottoWinningNumber() {
+        printNewLine();
+        System.out.println(ENTER_LOTTO_NUMBERS.getMessage());
+    }
+
+    public void askBonusNumber() {
+        printNewLine();
+        System.out.println(ENTER_BONUS_NUMBER.getMessage());
+    }
+
+    public void printNewLine() {
+        System.out.println();
+    }
 }

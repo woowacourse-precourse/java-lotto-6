2de70300d1ba67a5dfd1836042d4a16aa7f029e7
TaeDongUm <etd93@naver.com>
2023-11-08 12:56:58 +0900

    [feat] PrintConstants로 출력할 문구 따로 관리 기능 추가

---
+++ src/main/java/lotto/util/PrintConstants.java
@@ -0,0 +1,18 @@
+package lotto.util;
+
+public class PrintConstants {
+	public static final String INPUT_BUYING_PRICE = "구입금액을 입력해 주세요.";
+	public static final String NUBMER_OF_PURCHASED = "개를 구매했습니다..";
+	public static final String INPUT_NUMBER_OF_LOTTO = "당첨 번호를 입력해 주세요.";
+	public static final String INPUT_BONUS_NUMBER_OF_LOTTO = "보너스 번호를 입력해 주세요.";
+	public static final String WINNING_STATICS = "당첨 통계";
+	public static final String LINE_BREAK = "---";
+	public static final String THREE_MATCHING_NUMBER = "3개 일치 (5,000원) - ";
+	public static final String FOUR_MATCHING_NUMBER = "4개 일치 (50,000원) - ";
+	public static final String FIVE_MATCHING_NUMBER = "5개 일치 (1,500,000원) - ";
+	public static final String FIVE_MATCHING_BONUS_NOTMATCHING = "5개 일치, 보너스 볼 일치 (30,000,000원) -";
+	public static final String SIX_MATCHING_NUMBER = "6개 일치 (2,000,000,000원) - ";
+	public static final String TOTAL_INCOME ="총 수익률은 ";
+	public static final String PERCENTAGE ="%입니다.";
+	
+}

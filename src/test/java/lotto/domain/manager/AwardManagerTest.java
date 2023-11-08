0f987f7903dc054f6bc66d34c876a3671d8f134d
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-08 20:20:29 +0900

    test(AwardManagerTest) : test 추가

--- src/test/java/lotto/domain/manager/AwardManagerTest.java
+++ src/test/java/lotto/domain/manager/AwardManagerTest.java
@@ -1,4 +1,16 @@
+package lotto.domain.manager;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class AwardManagerTest {
-  
+
+    @Test
+    void makeResult() {
+    }
+
+    @Test
+    void printYield() {
+    }
 }

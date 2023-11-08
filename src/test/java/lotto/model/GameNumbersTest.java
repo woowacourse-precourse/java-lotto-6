3e4d6b5329ed871e28729f0f3d0e102fe44757cd
Youth <boysoeng@naver.com>
2023-11-08 21:04:53 +0900

    test(GameNumbers): 당첨번호 및 보너스 번호 추가 확인
    - 당첨번호 추가 시 해당 번호가 존재
    - 보너스번호 추가 확인

--- src/test/java/lotto/model/GameNumbersTest.java
+++ src/test/java/lotto/model/GameNumbersTest.java
@@ -1,11 +1,15 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import lotto.constant.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class GameNumbersTest {
 
@@ -24,4 +28,34 @@ class GameNumbersTest {
                 .hasMessage(ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR.getErrorMessage());
     }
 
+    @DisplayName("7개의 번호 생성 시 당첨번호가 포함되었는지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void 전체번호_당첨번호_존재_확인(int number) {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        WinningNumbers winningNumbers = new WinningNumbers(numbers);
+
+        //when
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+
+        //then
+        assertTrue(gameNumbers.isContainWinningNumber(number));
+    }
+
+    @DisplayName("보너스 번호 추가 확인")
+    @Test
+    void 보너스번호_추가_확인() {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        WinningNumbers winningNumbers = new WinningNumbers(numbers);
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        gameNumbers.addBonusNumber(bonusNumber);
+
+        //then
+        assertThat(bonusNumber).isEqualTo(gameNumbers.getBonusNumber());
+    }
 }

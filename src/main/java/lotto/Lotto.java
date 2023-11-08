6902c4e1de8f35454c6dfdacc0393e41447f19a0
kmh0601 <pongkmh@naver.com>
2023-11-08 17:49:47 +0900

    feat: 정적팩토리 메서드로 랜덤한 로또를 발행하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,17 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        // 에러 체크 구현
         this.numbers = numbers;
     }
 
@@ -17,4 +22,11 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public static Lotto createLotto() {
+        List<Integer> randomNumbers = pickUniqueNumbersInRange(1,45,6);
+        return new Lotto(randomNumbers);
+    }
+
+
+
 }

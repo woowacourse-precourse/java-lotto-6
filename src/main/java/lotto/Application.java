9498ee9dce5b6bcf3d9739bce90bf9ed1499d795
swhyeon98 <smreo75@gmail.com>
2023-11-08 23:50:48 +0900

    fix: view 유효성 검증 로직 Controller로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,13 +3,11 @@ package lotto;
 import lotto.controller.LottoController;
 import lotto.domain.RandomLottoGenerator;
 import lotto.service.LottoService;
-import lotto.view.InputView;
 import lotto.view.LottoView;
-import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoView lottoView = new LottoView(new InputView(), new OutputView());
+        LottoView lottoView = new LottoView();
         LottoService lottoService = new LottoService(new RandomLottoGenerator());
         LottoController lottoController = new LottoController(lottoView, lottoService);
 
--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 import lotto.domain.Rank;
 
@@ -8,54 +9,50 @@ import java.util.Map;
 
 public class LottoView {
 
-    private final InputView inputView;
-    private final OutputView outputView;
-
-
-    public LottoView(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
+    public String input(String message) {
+        System.out.println(message);
+        return Console.readLine();
     }
 
-    public int inputPurchaseAmount() {
-        while (true) {
-            try {
-                return inputView.inputPurchaseAmount();
-            } catch (IllegalArgumentException exception) {
-                outputView.printError(exception.getMessage());
-            }
-        }
+    public String inputPurchaseAmount() {
+        System.out.println();
+        return input("구입금액을 입력해 주세요.");
+    }
+
+    public String inputWinningNumber() {
+        System.out.println();
+        return input("당첨 번호를 입력해 주세요.");
+
+    }
+
+    public String inputBonusNumber() {
+        System.out.println();
+        return input("보너스 번호를 입력해 주세요.");
     }
 
     public void printLottos(List<Lotto> lottos) {
-        outputView.printLottos(lottos);
-    }
-
-    public List<Integer> inputWinningNumber(){
-        while (true) {
-            try {
-                return inputView.inputWinningNumber();
-            } catch (IllegalArgumentException exception) {
-                outputView.printError(exception.getMessage());
-            }
-        }
-    }
-
-    public int inputBonusNumber() {
-        while (true) {
-            try {
-                return inputView.inputBonusNumber();
-            } catch (IllegalArgumentException exception) {
-                outputView.printError(exception.getMessage());
-            }
-        }
+        System.out.println();
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        lottos.forEach(lotto -> System.out.println(lotto));
     }
 
     public void printResult(Map<Rank, Long> results, double profitRate) {
-        outputView.printResult(results, profitRate);
+        System.out.println();
+        System.out.println("당첨 통계\n---------");
+        printRankResult(results, Rank.FIFTH, "3개 일치 (5,000원) - %d개\n");
+        printRankResult(results, Rank.FOURTH, "4개 일치 (50,000원) - %d개\n");
+        printRankResult(results, Rank.THIRD, "5개 일치 (1,500,000원) - %d개\n");
+        printRankResult(results, Rank.SECOND, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n");
+        printRankResult(results, Rank.FIRST, "6개 일치 (2,000,000,000원) - %d개\n");
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+    }
+
+    private void printRankResult(Map<Rank, Long> results, Rank rank, String format) {
+        System.out.printf(format, results.getOrDefault(rank, 0L));
     }
 
     public void printError(String message) {
-        outputView.printError(message);
+        System.out.println();
+        System.out.println(message);
     }
 }

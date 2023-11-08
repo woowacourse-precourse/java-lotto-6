9347e1a4fb903855f17d8cc9fb50c6a11b578d3c
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 15:32:17 +0900

    feat: 입력 메시지 enum 추가

--- src/main/java/lotto/constant/InputMessage.java
+++ src/main/java/lotto/constant/InputMessage.java
@@ -1,4 +1,18 @@
 package lotto.constant;
 
 public enum InputMessage {
+    INPUT_PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
+    INPUT_LOTTO_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
+
+

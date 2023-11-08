b78c8ff8bc1f403c89f2976baa13ae7f4188ca48
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-08 14:05:09 +0900

    refactor(view): 당첨 결과 출력 메소드 분리 및 문자열 출력 방식 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,33 +8,47 @@ import lotto.domain.Rank;
 import lotto.domain.WinningResult;
 
 public class OutputView {
-    public void printNumberOfTicketsPurchased(List<Lotto> lottoTickets) {
-        System.out.printf("%d개를 구매했습니다.%n", lottoTickets.size());
-    }
-
-    public void printLottoTicketsDetails(List<Lotto> lottoTickets) {
-        StringBuilder lottoTicketsDetails = new StringBuilder();
-        for (Lotto lotto : lottoTickets) {
-            lottoTicketsDetails.append(lotto).append(System.lineSeparator());
-        }
-        System.out.println(lottoTicketsDetails.toString());
-    }
-
     public void printLottoPurchaseSummary(List<Lotto> lottoTickets) {
         printNumberOfTicketsPurchased(lottoTickets);
         printLottoTicketsDetails(lottoTickets);
     }
 
-    public void printWinningResult(WinningResult winningResult, List<Lotto> lottoTikets) {
-        System.out.printf("당첨 통계%n---%n");
-        for (Rank rank : Rank.values()) {
-            System.out.printf("%d개 일치", rank.getCountOfMatch());
-            if (rank == Rank.SECOND) {
-                System.out.printf(", 보너스 볼 일치");
-            }
-            System.out.printf(Locale.KOREA, " (%,d원) - %d개%n", rank.getPrizeMoney(), winningResult.getCount(rank));
+    private void printNumberOfTicketsPurchased(List<Lotto> lottoTickets) {
+        System.out.printf("%d개를 구매했습니다.%n", lottoTickets.size());
+    }
+
+    private void printLottoTicketsDetails(List<Lotto> lottoTickets) {
+        StringBuilder lottoTicketsDetails = new StringBuilder();
+        for (Lotto lotto : lottoTickets) {
+            lottoTicketsDetails.append(String.format("%s%n", lotto));
         }
-        double yield = winningResult.calculateYield(lottoTikets.size() * Constant.LOTTO_PRICE.getValue());
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", yield);
+        System.out.println(lottoTicketsDetails.toString());
+    }
+
+
+    public void printWinningResult(WinningResult winningResult, List<Lotto> lottoTickets) {
+        StringBuilder summary = new StringBuilder();
+        summary.append(String.format("당첨 통계%n---%n"));
+        
+        for (Rank rank : Rank.values()) {
+            summary.append(formatRankResult(rank, winningResult));
+        }
+        
+        double yield = winningResult.calculateYield(lottoTickets.size() * Constant.LOTTO_PRICE.getValue());
+        summary.append(String.format("총 수익률은 %.1f%%입니다.%n", yield));
+        
+        System.out.print(summary.toString());
+    }
+    
+    private String formatRankResult(Rank rank, WinningResult winningResult) {
+        String bonusMatch = "";
+        if (rank == Rank.SECOND) {
+            bonusMatch = ", 보너스 볼 일치";
+        }
+        return String.format(Locale.KOREA, "%d개 일치%s (%,d원) - %d개%n",
+                rank.getCountOfMatch(),
+                bonusMatch,
+                rank.getPrizeMoney(),
+                winningResult.getCount(rank));
     }
 }

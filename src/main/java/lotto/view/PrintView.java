cce933f33f0eab8a875b6ce4bf13e35796e37ef2
Kim mingyu <minq0302@naver.com>
2023-11-08 20:02:23 +0900

    feat(view): add printing Number of lottoes purchased method

--- src/main/java/lotto/view/PrintView.java
+++ src/main/java/lotto/view/PrintView.java
@@ -5,4 +5,9 @@ public class PrintView {
     public static void printErrorMessage(IllegalArgumentException e) {
         System.out.println(e.getMessage());
     }
+
+    public void printNumberOfLottos(int purchaseMoney) {
+        int lottoNumber = purchaseMoney / 1000;
+        System.out.printf("%d개를 구매했습니다", lottoNumber);
+    }
 }

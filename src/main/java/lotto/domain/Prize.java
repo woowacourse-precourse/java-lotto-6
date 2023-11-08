175548ef9b253925c52fcd6f1f16f39715f10bf2
sejin park <seijin0722@naver.com>
2023-11-08 21:26:27 +0900

    refactor(Prize) : 불필요한 변수 삭제

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,24 +1,18 @@
 package lotto.domain;
 
 public enum Prize {
-    THREE_MATCH(1, "3개 일치 (5,000원) - ", 5000),
-    FOUR_MATCH(2, "4개 일치 (50,000원) - ", 50000),
-    FIVE_MATCH(3, "5개 일치 (1,500,000원) - ", 1500000),
-    FIVE_MATCH_BONUS(4, "5개 일치, 보너스 볼 일치 (30,000,000원) - ",30000000),
-    SIX_MATCH(5, "6개 일치 (2,000,000,000원) - ",2000000000);
+    THREE_MATCH("3개 일치 (5,000원) - ", 5000),
+    FOUR_MATCH("4개 일치 (50,000원) - ", 50000),
+    FIVE_MATCH("5개 일치 (1,500,000원) - ", 1500000),
+    FIVE_MATCH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
+    SIX_MATCH("6개 일치 (2,000,000,000원) - ", 2000000000);
 
-    private final int rank;
     private final String description;
     private final int prizeAmount;
 
-    Prize(int rank, String description, int prizeAmount) {
-        this.rank = rank;
+    Prize(String description, int prizeAmount) {
         this.description = description;
         this.prizeAmount = prizeAmount;
-    }
-
-    public int getRank() {
-        return rank;
     }
 
     public String getDescription() {

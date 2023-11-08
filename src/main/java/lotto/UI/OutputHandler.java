da32d5983b5bffd3e793800b6f10403ad1714166
wonindev <sangwonparkdev@gmail.com>
2023-11-08 22:29:28 +0900

    Feat: 수익률과 당첨 개수를 출력하는 기능 추가

--- src/main/java/lotto/UI/OutputHandler.java
+++ src/main/java/lotto/UI/OutputHandler.java
@@ -16,4 +16,21 @@ public class OutputHandler {
     public void printLottos() {
         System.out.println(lottosSB.toString());
     }
+
+    public void printTicketCnt(int ticketCnt) {
+        System.out.println(ticketCnt + "개를 구매했습니다.");
+    }
+
+    public void printResult(List<Integer> result) {
+        System.out.println("3개 일치 (5,000원) - " + result.get(3) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get(4) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get(5) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get(7) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.get(6) + "개");
+    }
+
+    public String printRoi(double result) {
+        String str = String.format("%.1f", result);
+        return "총 수익률은 " + str + "%입니다.";
+    }
 }

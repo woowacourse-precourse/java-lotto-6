1e3650903e883e6051389d6c4b9400a6d1773691
lkdcode <dlrlejr1@naver.com>
2023-11-08 16:44:32 +0900

    Add [LottoManager] : 1차 구현

--- src/main/java/lotto/module/manager/LottoManager.java
+++ src/main/java/lotto/module/manager/LottoManager.java
@@ -1,48 +1,74 @@
 package lotto.module.manager;
 
+import lotto.module.console.LottoConsoleManager;
+import lotto.module.domain.LottoProfit;
 import lotto.module.domain.PurchaseAmount;
-import lotto.module.domain.lotto.Lotto;
-import lotto.module.domain.lotto.WinningLotto;
+import lotto.module.lotto.Lotto;
+import lotto.module.lotto.WinningLotto;
 import lotto.module.result.LottoResult;
-import lotto.module.result.LottoResultChecker;
+import lotto.module.result.LottoResultManager;
 import lotto.module.store.LottoStore;
 
 import java.util.List;
 
 public class LottoManager {
     private final LottoConsoleManager lottoConsoleManager;
+    private final LottoResultManager lottoResultManager;
     private final LottoStore lottoStore;
 
-    public LottoManager(LottoConsoleManager lottoConsoleManager, LottoStore lottoStore) {
+    private LottoManager(LottoConsoleManager lottoConsoleManager, LottoResultManager lottoResultManager, LottoStore lottoStore) {
         this.lottoConsoleManager = lottoConsoleManager;
+        this.lottoResultManager = lottoResultManager;
         this.lottoStore = lottoStore;
     }
 
-    public static LottoManager of(LottoConsoleManager lottoConsoleManager, LottoStore lottoStore) {
-        return new LottoManager(lottoConsoleManager, lottoStore);
+    public static LottoManager of(LottoConsoleManager lottoConsoleManager, LottoResultManager lottoResultManager, LottoStore lottoStore) {
+        return new LottoManager(lottoConsoleManager, lottoResultManager, lottoStore);
     }
 
     public void run() {
-        List<Lotto> purchasedLottoTickets = purchaseLottoTickets();
+        PurchaseAmount purchaseAmount = getPurchaseAmount();
+        List<Lotto> purchasedLottoTickets = purchaseLottoTickets(purchaseAmount);
         WinningLotto winningLottoNumbers = generateWinningNumbers();
-        announceResults(purchasedLottoTickets, winningLottoNumbers);
+        announceResults(purchaseAmount, purchasedLottoTickets, winningLottoNumbers);
     }
 
-    private List<Lotto> purchaseLottoTickets() {
-        PurchaseAmount purchaseAmount = lottoConsoleManager.getPurchaseAmount();
-        List<Lotto> purchasedLottoTickets = lottoStore.purchaseLotto(purchaseAmount);
-        lottoConsoleManager.printPurchasedLottoTickets(purchasedLottoTickets);
-        return purchasedLottoTickets;
+    private PurchaseAmount getPurchaseAmount() {
+        while (true) {
+            try {
+                return lottoConsoleManager.getPurchaseAmount();
+            } catch (IllegalArgumentException e) {
+                lottoConsoleManager.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private List<Lotto> purchaseLottoTickets(PurchaseAmount purchaseAmount) {
+        while (true) {
+            try {
+                List<Lotto> purchasedLottoTickets = lottoStore.purchaseLotto(purchaseAmount);
+                lottoConsoleManager.printPurchasedLottoTickets(purchasedLottoTickets);
+                return purchasedLottoTickets;
+            } catch (IllegalArgumentException e) {
+                lottoConsoleManager.printErrorMessage(e.getMessage());
+            }
+        }
     }
 
     private WinningLotto generateWinningNumbers() {
-        return lottoConsoleManager.getWinningLottoNumbers();
+        while (true) {
+            try {
+                return lottoConsoleManager.getWinningLottoNumbers();
+            } catch (IllegalArgumentException e) {
+                lottoConsoleManager.printErrorMessage(e.getMessage());
+            }
+        }
     }
 
-    private void announceResults(List<Lotto> purchasedLottoTickets, WinningLotto winningLottoNumbers) {
-        LottoResultChecker lottoResultChecker = new LottoResultChecker(LottoResult.newInstance());
-        LottoResult result = lottoResultChecker.getResult(purchasedLottoTickets, winningLottoNumbers);
-        System.out.println(result.getResult());
+    private void announceResults(PurchaseAmount purchaseAmount, List<Lotto> purchasedLottoTickets, WinningLotto winningLottoNumbers) {
+        LottoResult lottoResult = lottoResultManager.calculateResult(purchasedLottoTickets, winningLottoNumbers);
+        LottoProfit lottoProfit = lottoResultManager.calculateProfit(purchaseAmount);
+        lottoConsoleManager.printLottoWinningResult(lottoResult, lottoProfit);
     }
 
 }

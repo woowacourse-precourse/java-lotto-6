166544f7d75aad7820853973a70a2bec7630b52a
syy0O <86238720+syy0O@users.noreply.github.com>
2023-11-08 19:58:11 +0900

    docs: 기능 구현 목록 수정

--- src/main/java/lotto/domain/message/ErrorMessage.java
+++ src/main/java/lotto/domain/message/ErrorMessage.java
@@ -1,8 +1,8 @@
 package lotto.domain.message;
 
 public enum ErrorMessage {
-    INVALID_MONEY_VALUE("[ERROR] 금액은 1000원 단위의 금액이어야 합니다."),
-    INVALID_LOTTO_LENGTH("[ERROR] 로또 번호의 길이는 6개여야 합니다."),
+    INVALID_MONEY_VALUE("[ERROR] 1000원 단위로 입력해야합니다."),
+    INVALID_LOTTO_LENGTH("[ERROR] 당첨 번호는 6개여야 합니다."),
     DUPLICATES_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복을 허용하지 않습니다."),
     OUT_OF_RANGE("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다."),
     DUPLICATES_BONUS_NUMBER("[ERROR] 당첨 번호와 중복된 보너스 번호를 입력할 수 없습니다."),

4a323a76b5e84e44feb178208b7d51fb8c912dd6
Woongbae Yun <devbelly@naver.com>
2023-11-08 10:40:44 +0900

    feat : 당첨 번호 잘못 입력 시 예외를 던지는 기능 추가

--- src/main/java/lotto/util/StringUtils.java
+++ src/main/java/lotto/util/StringUtils.java
@@ -5,14 +5,20 @@ import java.util.List;
 
 public class StringUtils {
 
+    private static final String ERROR = "[ERROR] 입력을 다시 확인해주세요.";
+
     private StringUtils() {
 
     }
 
     public static List<Integer> splitStringToLottoNumbers(String input) {
-        return Arrays.stream(input.split(","))
-            .map(String::trim)
-            .map(Integer::parseInt)
-            .toList();
+        try {
+            return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR);
+        }
     }
 }

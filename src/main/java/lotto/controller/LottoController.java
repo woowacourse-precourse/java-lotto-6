d8cb7be36f4d93df18af25562d8a4ff57e923356
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:40:16 +0900

    feat : LottoController 생성자 정의

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,12 @@
 package lotto.controller;
 
+import lotto.service.LottoService;
+
 public class LottoController {
     private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
 }

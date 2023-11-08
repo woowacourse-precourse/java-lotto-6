6962be0af719178dd9483a32db0459a230031c18
dj <fjzl12@naver.com>
2023-11-08 17:00:24 +0900

    refactor(OutputView): 상수 각각 임포트하여 사용

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,28 +1,32 @@
 package lotto.ui;
 
+import static lotto.constants.Message.PURCHASE_COMMENT;
+import static lotto.constants.Message.RESULT;
+import static lotto.constants.Message.PROFIT;
+
 import lotto.Lotto;
 
 import java.util.List;
 
-import lotto.domain.Constants;
-import lotto.domain.ResultMessage;
-
-import static lotto.domain.Message.PURCHASE_COMMENT;
+import lotto.constants.Result;
 
 public class OutputView {
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+
     public static void printLotto(List<Lotto> myLotto) {
         System.out.printf(PURCHASE_COMMENT.getMessage(), myLotto.size());
         for (Lotto lotto : myLotto) {
             System.out.println(lotto.getNumbers());
         }
-        System.out.println();
     }
 
-    public static void printStatistics(List<Integer> prizes, float profitPercent) {
-        System.out.println(Constants.RESULT);
-        for (ResultMessage result : ResultMessage.values()) {
-            System.out.printf(result.getMessage(), prizes.get(result.ordinal()));
+    public static void printStatistics(List<Integer> prizeCount, float profitPercent) {
+        System.out.println(RESULT.getMessage());
+        for (Result result : Result.values()) {
+            System.out.printf(result.getMessage(), prizeCount.get(result.ordinal()));
         }
-        System.out.printf(Constants.PROFIT, profitPercent);
+        System.out.printf(PROFIT.getMessage(), profitPercent);
     }
 }

0b68039a2dc86f110c8c39c8ff5a55d2fa0933f9
wook <dong930112@gmail.com>
2023-11-08 18:15:30 +0900

    refactor: 로또 구매 기능 내부로직 개선

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,30 +1,28 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    int money;
-    List<Lotto> lottos;
+    int purchaseCost;
+    List<Lotto> lottos = new ArrayList<>();
+    List<Integer> winningStatics = new ArrayList<>();
 
-
-
-    //로또를 구매하는 기능
-    //로또를 발행하는 기능을 포함하고 있다. 리팩토링 대상
-    public List<Lotto> purchaseLotto(int purchaseQuantity) {
-        int lottoAmount = purchaseQuantity/1000;
-        money = purchaseQuantity%1000;
-        List<Lotto> issuedLotto = new ArrayList<>();
-
-        for(int i=0; i<lottoAmount; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Lotto lotto = new Lotto(numbers);
-            issuedLotto.add(lotto);
-        }
-
-        lottos = issuedLotto;
-        return issuedLotto;
+    public int getPurchaseCost() {
+        return purchaseCost;
     }
 
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public List<Lotto> purchaseLotto(int purchaseCostInput) {
+        purchaseCost = purchaseCostInput;
+        int lottoQuantity = Lotto.calculatePurchaseQuantity(purchaseCostInput);
+
+        for(int i=0; i<lottoQuantity; i++) {
+            lottos.add(Lotto.issue());
+        }
+        return lottos;
+    }
 }

a4a9cfe5ac519a717e085717c000b9177bfb82c0
alenjb <alenjb@naver.com>
2023-11-08 15:54:58 +0900

    ✨feat(Application): 입력 값이 1,000 단위로 나누어 떨어지는지 체크하는 기능 구현
    입력 값이 1,000 단위로 나누어 떨어지는지 체크하고 그렇지 않을 경우 적당한 오류를 발생시키는 메서드를 구현함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,5 +6,15 @@ public class Application {
 
     public static void main(String[] args) {
 
+
+    }
+
+    // 입력 값이 1,000 단위로 나누어 떨어지는지 체크하는 메서드
+    public static double isDivisibleByOneThousand(double number) {
+        if (number < ONE_LOTTO_PRICE)
+            throw new IllegalArgumentException("[ERROR] 구매금액은 1,000원 이상으로 입력하셔야 합니다."); // 예외처리: 1,000원보다 작은 경우
+        if (number % ONE_LOTTO_PRICE != 0)
+            throw new IllegalArgumentException("[ERROR] 구매금액은 1,000원 단위로 입력하셔야 합니다."); // 예외처리: 1,000으로 나누어떨어지지 않는 경우
+        return number / ONE_LOTTO_PRICE;
     }
 }

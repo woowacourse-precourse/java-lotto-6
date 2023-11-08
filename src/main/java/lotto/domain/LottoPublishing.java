61f66f6fa538e2f5ebaca638d6be240cddd6554d
soyoungxxx <otherplet@gmail.com>
2023-11-08 22:58:11 +0900

    fix(OutputLottos): List 정렬을 출력할 때만 하도록 변경

--- src/main/java/lotto/domain/LottoPublishing.java
+++ src/main/java/lotto/domain/LottoPublishing.java
@@ -8,18 +8,16 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LottoPublishing {
-    List<Integer> lottoNumber;
+    List<Integer> numbers;
     List<Lotto> lottos = new ArrayList<Lotto>();
     private void createRandomNumber() {
-        List <Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        lottoNumber = new ArrayList<Integer>(numbers);
-        Collections.sort(lottoNumber);
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     public List<Lotto> publishLotto(int numberOfLotto) {
         for (int i = 0; i < numberOfLotto; i++) {
             createRandomNumber();
-            Lotto lotto = new Lotto(lottoNumber);
+            Lotto lotto = new Lotto(numbers);
             lottos.add(lotto);
         }
         return lottos;
--- src/main/java/lotto/view/OutputLottos.java
+++ src/main/java/lotto/view/OutputLottos.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.domain.Lotto;
@@ -7,7 +8,9 @@ import lotto.domain.Lotto;
 public class OutputLottos {
     public OutputLottos(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
-            lotto.printLotto();
+            List<Integer> lottoNumber = new ArrayList<>(lotto.getLotto());
+            Collections.sort(lottoNumber);
+            System.out.println(lottoNumber);
         }
     }
 }

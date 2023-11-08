8f2bdec8a703c47c6d289dcb7e28d6137ae2adde
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-11-08 22:25:31 +0900

    test: 1등 당첨 확인 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Judge;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +15,11 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private User user;
+    private List<Lotto> lottos;
+    private Lotto answerLotto;
+    private List<Integer> lottoNumbers;
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,5 +35,26 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @BeforeEach
+    void beforeEach(){
+        user = new User(1);
+        lottos = user.getLottos();
+        answerLotto = lottos.get(0);
+        lottoNumbers = answerLotto.getNumbers();
+    }
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("1등 당첨")
+    @Test
+    void winFirst(){
+        int bonusNumber;
+        do {
+            bonusNumber = Randoms.pickNumberInRange(1, 45);
+        } while (lottoNumbers.contains(bonusNumber));
+        Judge judge = new Judge(answerLotto, bonusNumber, user, 1000);
+
+        // when
+        judge.checkWinning();
+        judge.finalResult();
+        Assertions.assertThat(Rank.FIRST.getCount()).isGreaterThan(0);
+    }
 }

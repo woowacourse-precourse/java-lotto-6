f99b6eea3e24858621ac3a409a52a15bb43bb4f0
kang jihyeong <her4386@gmail.com>
2023-11-08 13:15:58 +0900

    [fix] 로또 번호 유효성 검사 항목 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,12 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.utils.ErrorPhrase.*;
+import static lotto.utils.LottoSystemUtils.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -9,8 +14,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
 
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        this.numbers = sortedNumbers(numbers);
     }
 
     public long matchCount(Lotto myLotto) {
@@ -23,13 +27,43 @@ public class Lotto {
         return numbers.contains(num);
     }
 
+    public List<Integer> getLottoNumbers() {
+        return numbers;
+    }
+
+    private List<Integer> sortedNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        isRightLottoCnt(numbers);
+        isNotDuplicate(numbers);
+        isRightLottoRange(numbers);
+    }
+
+    private void isRightLottoCnt(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_CNT) {
+            throw new IllegalArgumentException(ERROR_PHRASE_INPUT_RIGHT_LOTTO_CNT);
         }
     }
 
-    public List<Integer> getLottoNumbers() {
-        return numbers;
+    private void isNotDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (Integer num : numbers) {
+            if (set.contains(num)) {
+                throw new IllegalArgumentException(ERROR_PHRASE_LOTTO_NUMBER_IS_DUPLICATED);
+            }
+            set.add(num);
+        }
+    }
+
+    private void isRightLottoRange(List<Integer> numbers) {
+        for (Integer num : numbers) {
+            if (num < LOTTO_NUMBER_MIN_RANGE || num > LOTTO_NUMBER_MAX_RANGE) {
+                throw new IllegalArgumentException(ERROR_PHRASE_LOTTO_NUMBER_IS_OUT_OF_RANGE);
+            }
+        }
     }
 }
--- src/main/java/lotto/utils/ErrorPhrase.java
+++ src/main/java/lotto/utils/ErrorPhrase.java
@@ -10,6 +10,12 @@ public class ErrorPhrase {
 
     public static final String ERROR_PHRASE_MONEY_IS_WRONG_UNIT = "구입 금액은 "+MONEY_UNIT+"원 단위만 가능합니다.";
 
+    public static final String ERROR_PHRASE_INPUT_RIGHT_LOTTO_CNT = "로또 번호는 "+LOTTO_CNT+"개만 입력 가능합니다.";
+
+    public static final String ERROR_PHRASE_LOTTO_NUMBER_IS_DUPLICATED = "로또 번호는 중복된 값을 가질 수 없습니다.";
+
+    public static final String ERROR_PHRASE_LOTTO_NUMBER_IS_OUT_OF_RANGE = "로또 번호는"+LOTTO_NUMBER_MIN_RANGE+" ~ "+LOTTO_NUMBER_MAX_RANGE+" 사이 숫자만 가능합니다.";
+
     public static final String ERROR_PHRASE_LOTTO_NUMBER_IS_NOT_DIGIT = "로또 번호는 숫자만 가능합니다.";
 
 }

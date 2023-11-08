935180608e8e01f384032c52fa648f59c4fe551f
takoyakimchi <c4nyou@gmail.com>
2023-11-08 17:03:45 +0900

    refactor(price): 검증 로직 추가
    1) 가격 입력이 숫자인지 검증
    2) 1000 단위로 입력했는지 검증
    3) 최솟값 검증
    4) 최댓값 검증

--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -1,10 +1,51 @@
 package lotto;
 
+import static lotto.constant.ErrorMessage.ONLY_NUMBERS_ALLOWED;
+import static lotto.constant.ErrorMessage.WRONG_BUY_PRICE;
+import static lotto.constant.ErrorMessage.WRONG_MAXIMUM_BUY;
+import static lotto.constant.ErrorMessage.WRONG_MINIMUM_BUY;
+import static lotto.constant.GameNumber.MAX_LOTTO_AMOUNT;
+import static lotto.constant.GameNumber.MIN_LOTTO_AMOUNT;
+
 public class Price {
 
-    private int price;
+    private long price;
 
-    public Price(int price) {
+    public Price(String input) {
+        validate(input);
         this.price = price;
+    }
+
+    private void validate(String input) {
+        validateNumeric(input);
+        validateRemainder();
+        validateMinimum();
+        validateMaximum();
+    }
+
+    private void validateNumeric(String input) {
+        try {
+            this.price = Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ONLY_NUMBERS_ALLOWED.getMessage());
+        }
+    }
+
+    private void validateRemainder() {
+        if (this.price % 1000 != 0 || this.price <= 0) {
+            throw new IllegalArgumentException(WRONG_BUY_PRICE.getMessage());
+        }
+    }
+
+    private void validateMinimum() {
+        if ((this.price / 1000) < MIN_LOTTO_AMOUNT.getNumber()) {
+            throw new IllegalArgumentException(WRONG_MINIMUM_BUY.getMessage());
+        }
+    }
+
+    private void validateMaximum() {
+        if ((this.price / 1000) > MAX_LOTTO_AMOUNT.getNumber()) {
+            throw new IllegalArgumentException(WRONG_MAXIMUM_BUY.getMessage());
+        }
     }
 }

56737f1836181033f4479be4ad4bfa4783b7359b
june-777 <wlwhswnsrl96@gmail.com>
2023-11-08 14:31:35 +0900

    feat(PurchaseAmount): 1000원 단위를 검증하는 기능

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -6,20 +6,27 @@ public class PurchaseAmount {
 
     private static final int MINIMUM = 1_000;
     private static final int MAXIMUM = 100_000_000;
-    private final int amount;
+    private static final int LOTTO_PRICE_UNIT = 1_000;
+    private final long amount;
 
-    public PurchaseAmount(int amount) {
+    public PurchaseAmount(long amount) {
         validate(amount);
         this.amount = amount;
     }
 
-    private void validate(int amount) {
+    private void validate(long amount) {
         validateRange(amount);
     }
 
-    private void validateRange(int amount) {
+    private void validateRange(long amount) {
         if (amount < MINIMUM || amount > MAXIMUM) {
             throw new IllegalArgumentException(PurchaseAmountExceptionMessage.OUT_OF_AMOUNT_RANGE.getError());
+        }
+    }
+
+    private void validateUnit(long amount) {
+        if (amount % LOTTO_PRICE_UNIT != 0) {
+            throw new IllegalArgumentException(PurchaseAmountExceptionMessage.INVALID_AMOUNT_UNIT.getError());
         }
     }
 }

9a69fb862d166d02f9d067ac8a813879b0285a3c
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-11-08 22:11:25 +0900

    refactor: 수익률 포맷 패턴 수정

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
     public static final int LOTTO_START_RANGE = 1;
     public static final int LOTTO_END_RANGE = 45;
     public static final int MONEY_UNIT = 1000;
-    public static final String FORMAT_PATTERN = "#,###0.0%";
+    public static final String FORMAT_PATTERN = "#,###.0%";
     public static final String ZERO_PATTERN = "0.0%";
     public static final String NUMERIC_REGEX = "[+-]?\\d*(\\.\\d+)?";
 }

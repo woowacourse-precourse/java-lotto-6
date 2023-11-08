d3b5feba878f285a12ad996fff869e7594b3dfcd
“sangjun121” <juncho12011201@gmail.com>
2023-11-08 17:09:25 +0900

    feat: 보너스 번호 입력 프롬포트 메시지 enum 선언

--- src/main/java/lotto/utility/enums/Messages.java
+++ src/main/java/lotto/utility/enums/Messages.java
@@ -3,7 +3,8 @@ package lotto.utility.enums;
 public enum Messages {
     INPUT_LOTTO_PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
     LOTTO_COUNT_MESSAGE("개를 구매했습니다."),
-    INPUT_LOTTO_WINNING_NUUMBER_MESSAGE("당첨 번호를 입력해 주세요.");
+    INPUT_LOTTO_WINNING_NUUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_LOTTO_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
 
     private String message;
 

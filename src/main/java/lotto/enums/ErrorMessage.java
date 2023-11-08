c11fec83bf65537e4e41c0a641bc02e343b7c7ad
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 15:29:24 +0900

    feat: 에러 메시지 추가 및 공통 PREFIX 상수로 구현

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -1,14 +1,17 @@
 package lotto.enums;
 
 public enum ErrorMessage {
-    NUMBER_FORMAT_ERROR("[ERROR] 숫자만 입력해야 합니다."),
-    NEGATIVE_NUM_ERROR("[ERROR] 구매 금액은 음수로 입력할 수 없습니다."),
-    LESS_NUM_ERROR("[ERROR] 로또를 구매하기 위해 1000원 이상의 값이 필요합니다."),
-    ACCOUNT_UNIT_ERROR("[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다."),
-    EMPTY_VALUE_ERROR("[ERROR] 공백 또는 빈 문자열은 입력할 수 없습니다."),
-    INVALID_NUMBERS_COUNT_ERROR("[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다"),
-    EXCEED_LOTTO_RANGE_ERROR("[ERROR] 로또 숫자 범위(1부터 45)를 초과하였습니다"),
-    DUPLICATE_NUMBER_ERROR("[ERROR] 중복된 로또 범위가 존재합니다");
+
+    NUMBER_FORMAT_ERROR("숫자만 입력해야 합니다."),
+    NEGATIVE_NUM_ERROR("구매 금액은 음수로 입력할 수 없습니다."),
+    LESS_NUM_ERROR("로또를 구매하기 위해 1000원 이상의 값이 필요합니다."),
+    ACCOUNT_UNIT_ERROR("구매 금액은 1000원 단위로 입력해야 합니다."),
+    EMPTY_VALUE_ERROR("공백 또는 빈 문자열은 입력할 수 없습니다."),
+    INVALID_NUMBERS_COUNT_ERROR("로또 번호는 6개의 숫자로 구성되어야 합니다."),
+    EXCEED_LOTTO_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_NUMBER_ERROR("로또 번호는 중복되지 않는 수여야 합니다."),
+    DUPLICATE_WINNING_NUMBER_ERROR("보너스 번호는 당첨 번호와 다른 수여야 합니다.");
+    private static final String PREFIX = "[ERROR] ";
 
     private final String message;
 
@@ -18,6 +21,6 @@ public enum ErrorMessage {
 
 
     public String getMessage() {
-        return message;
+        return PREFIX+message;
     }
 }

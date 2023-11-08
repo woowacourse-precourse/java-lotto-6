a73f3635dc83d7f0c5070cf7eb9bec8208d97e90
gusgh075 <gusgh075@naver.com>
2023-11-08 17:19:14 +0900

    TEST : 당첨 번호가 보너스 번호와 겹치는 오류 테스트

--- src/test/java/lotto/LottoRepositoryTest.java
+++ src/test/java/lotto/LottoRepositoryTest.java
@@ -8,10 +8,17 @@ import java.util.List;
 
 class LottoRepositoryTest{
     LottoRepository lottoRepository=new LottoRepository();
-    @DisplayName("보너스 번호와 당첨 번호가 겹치는 오류")
+    @DisplayName("보너스 번호가 당첨 번호와 겹치는 오류")
     @Test
     void bonusNumberInputError(){
         lottoRepository.setWinningLotto(new Lotto(List.of(1,2,3,4,5,6)));
         Assertions.assertFalse(lottoRepository.setBonusNum(6));
+    }
+
+    @DisplayName("당첨 번호가 보너스 번호와 겹치는 오류")
+    @Test
+    void winningLottoInputError(){
+        lottoRepository.setBonusNum(1);
+        Assertions.assertFalse(lottoRepository.setWinningLotto(new Lotto(List.of(1,2,3,4,5,6))));
     }
 }

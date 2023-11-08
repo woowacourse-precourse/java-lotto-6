8effeb4f851939547ea6d6b4af683205df0296f2
constant94-dev <constant94@naver.com>
2023-11-08 16:31:02 +0900

    feat(lotto) 클래스, 메서드 분리한 코드 종합

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,8 +1,49 @@
 package lotto;
 
-public class LottoGame {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lotto.services.Buy;
+import lotto.services.LottoStats;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
-    public void run(){
+public class LottoGame {
+    private InputView inputView;
+    private OutputView outputView;
+    private Buy buyInfo;
+    private LottoStats lottoStats;
+
+    public void run() {
+        ready();
+        playLotto();
+    }
+
+    private void ready() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        buyInfo = new Buy(new ArrayList<>());
+        lottoStats = new LottoStats();
+    }
+
+    private void playLotto() {
+        int amount = inputView.inputForAmount();
+
+        int buyCount = buyInfo.createToLotto(amount);
+        List<List<Integer>> buyLottoNumbers = buyInfo.getBuyLottoNumbers();
+
+        String buyLotto = outputView.printToBuyLotto(buyLottoNumbers, buyCount);
+        System.out.println(buyLotto);
+
+        List<Integer> prizeNumbers = inputView.inputForPrizeNumber();
+        int bonusNumber = inputView.inputForBonusNumber();
+        inputView.closeToConsole();
+
+        Map<String, Integer> stats = lottoStats
+                .calculationOfStats(buyLottoNumbers, prizeNumbers, bonusNumber, amount);
+
+        String prizeDetails = outputView.printToPrizeDetails(stats, lottoStats.getEarningRate());
+        System.out.println(prizeDetails);
 
     }
 }

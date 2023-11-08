62c3fa152fe1f15a14a12c7c6fd1a8d7b4f1d07a
minoooo119 <minoth0119@naver.com>
2023-11-08 20:56:46 +0900

    [FIX] 모든 기능 추가한 controller.java

--- src/main/java/Controller/LottoGame.java
+++ src/main/java/Controller/LottoGame.java
@@ -1,9 +1,13 @@
 package Controller;
 
-import Model.PurchaseLotto;
+import Model.MoneyCalculateModel;
+import Model.PurchaseLottoModel;
+import Model.RankCalculateModel;
 import View.UserInput;
 import lotto.Lotto;
+import lotto.Rank;
 
+import javax.management.relation.RelationNotification;
 import java.util.List;
 
 public class LottoGame {
@@ -11,16 +15,31 @@ public class LottoGame {
 
     }
     private static UserInput userInput=new UserInput();
-    private static List<Integer> winningLottoNum;
+    private static List<Lotto> lottoList;
     private static Integer winningBonusNum;
+    private static List<Rank> rankList;
     public void gameStart(){
         //1. User에게 구매 가격 입력을 받는다.
         Integer purchaseAmount= userInput.getPurchaseAmount();
         // User가 구입한 장수, 로또 보여준다.
-        PurchaseLotto userPurchaseLottos=new PurchaseLotto(purchaseAmount);
+        PurchaseLottoModel userPurchaseLottos=new PurchaseLottoModel(purchaseAmount);
+        lottoList=userPurchaseLottos.getLottoList();
+
         //당첨 번호 6개를 입력받는다.
-        Lotto lotto =userInput.getWinningNum();
+        Lotto winningLotto =userInput.getWinningNum();
         //보너스 번호 1개를 입력받는다.
         winningBonusNum= userInput.getBonusNum();
+
+        //2. 통계 계산 시작
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        // 당첨 수 확인
+        RankCalculateModel rankCalculateModel=new RankCalculateModel(lottoList,winningLotto,winningBonusNum);
+        rankCalculateModel.printRank();
+        // 당첨 금액, 수익률 확인
+        MoneyCalculateModel moneyCalculateModel=new MoneyCalculateModel(purchaseAmount,rankCalculateModel);
+        moneyCalculateModel.printNiceMoneyPercentage();
+
+
     }
 }

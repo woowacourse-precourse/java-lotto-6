e3d30324679b67fae15a6322e0e073cb11af27be
yujeonghyeon <dbdb1114@naver.com>
2023-11-08 22:34:54 +0900

    중복에 대한 예외 enum 추가

--- src/main/java/util/InputException.java
+++ src/main/java/util/InputException.java
@@ -1,15 +1,16 @@
 package util;
 
 public enum InputException {
-    RANGE_OF_NUMBER_EXCEPTION("[ERROR] 숫자의 범위는 1부터 45 사이입니다."),
-    LENGTH_NUMBEr_ERROR("[ERROR] 숫자의 갯수는 8개 입니다.");
-
+    RANGE_OF_NUMBER_EXCEPTION("[ERROR] 숫자의 범위는 1부터 45 사ㅇ이입니다."),
+    NUMBERS_LENGTH_EXCEPTION("[ERROR] 숫자의 갯수는 6개 입니다."),
+    DUPLE_NUMBER_EXCEPTION("[ERROR] 중복된 숫자를 입력하셨습니다. ");
     String errorMessage;
-    InputException(String errorMessage){
+
+    InputException(String errorMessage) {
         this.errorMessage = errorMessage;
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return errorMessage;
     }
 }

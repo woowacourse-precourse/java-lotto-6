b802fee5108c57dc2978d4aa0d6fe3e0d6d26831
cheolhyeon <tghy4@naver.com>
2023-11-08 16:58:03 +0900

    feat : 상수 추가

--- src/main/java/constants/LottoGameConstants.java
+++ src/main/java/constants/LottoGameConstants.java
@@ -5,4 +5,5 @@ public abstract class LottoGameConstants {
     public static final int MIN_LOTTO_NUMBER_RANGE = 1;
     public static final int MAX_LOTTO_NUMBER_COUNT = 6;
     public static final int DIVISION_NUMBER= 1000;
+    public static final int LOTTO_TICKET_PRICE = 1000;
 }

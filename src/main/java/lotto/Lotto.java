487271698653041674eff3195d97e23dd63c3959
oiler831 <qudwls831@gmail.com>
2023-11-08 13:51:52 +0900

    fix: Lotto 클래스 수정
    자잘한 유효성 판별을 한 메서드로 통합.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,24 +3,28 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private static final Integer LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers){
         validateSize(numbers);
         validateDuplication(numbers);
-        this.numbers = numbers;
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자이어야 합니다");
         }
     }
 
     private void validateDuplication(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 숫자이어야 합니다");
         }
     }
 

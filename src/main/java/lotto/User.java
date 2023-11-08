036bca9413fac14cf0ae214c35da680d7e44bde6
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 13:22:06 +0900

    fix: 예외처리 수정

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -10,17 +10,19 @@ public class User {
             int money = Integer.parseInt(input);
             validatePurchaseAmount(money);
             return money;
-        }catch (Exception e){ // 숫자인지 확인 가능
+        }catch (NumberFormatException e){ // 숫자인지 확인 가능
+            System.out.println("[ERROR] 1000원 단위의 숫자를 입력해주세요");
+        }catch (IllegalArgumentException e){
             System.out.println(e);
         }
         return inputPurchaseAmount();
     }
     private void validatePurchaseAmount(int money){
         if(money<1000||money>100000){// 1000원 미만, 100000원 초과의 값 입력시의 예외 처리
-            throw new IllegalArgumentException("1000원 이상 100000원 이하의 값을 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] 1000원 이상 100000원 이하의 값을 입력해주세요");
         }
         if(money%1000!=0){// 1000원 단위의 값 외의 값 입력시의 예외 처리
-            throw new IllegalArgumentException("1000원 단위의 값을 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] 1000원 단위의 값을 입력해주세요");
         }
     }
 }

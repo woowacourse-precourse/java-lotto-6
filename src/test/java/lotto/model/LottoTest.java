fe0b2fbf4caf496094666dcc34145a6241ce717f
nayooniverse <kangna99@naver.com>
2023-11-08 16:07:13 +0900

    test: 플레이어 로또가 당첨 로또와 몇 개의 숫자가 일치하는지 확인하는 기능 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -4,6 +4,7 @@ import static lotto.constants.Message.DUPLICATED_LOTTO_NUMBER;
 import static lotto.constants.Message.INVALID_LOTTO_NUMBER_RANGE;
 import static lotto.constants.Message.INVALID_LOTTO_SIZE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -44,5 +45,14 @@ class LottoTest {
         assertTrue(lotto.contains(3));
         assertTrue(lotto.contains(6));
         assertFalse(lotto.contains(7));
+    }
+
+    @DisplayName("플레이어 로또가 당첨 로또와 몇 개의 숫자가 일치하는지 확인한다.")
+    @Test
+    public void testMatchCountWith() {
+        Lotto playerLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = new Lotto(List.of(3, 4, 5, 6, 7, 8));
+
+        assertEquals(4, playerLotto.matchCountWith(winningLotto));
     }
 }

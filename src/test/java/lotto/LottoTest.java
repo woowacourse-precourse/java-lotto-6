58f2c46bb46b0f758815ccc2a559d1574e7c13f4
준써크 <kjs8960@inha.edu>
2023-11-08 11:49:32 +0900

    feat: 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
+import lotto.domain.WinnerNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,8 +21,16 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 1에서 45사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfBoundNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(0,1,2,3,4,5)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,50)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

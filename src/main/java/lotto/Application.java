907cccc873be3ef4a882f38d54e683879cb77cca
Jung ui heon <ky7662@naver.com>
2023-11-08 15:56:22 +0900

    feat : 당첨 여부를 확인하는 기능 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,11 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import static lotto.CompareLottoToPrizeNumber.printPrizeText;
+import static lotto.CompareLottoToPrizeNumber.winLottoToPrizeNumber;
 import static lotto.Input.*;
 import static lotto.RandomLottoNumber.randLottoNumbers;
 import static lotto.SeparationNumber.separatePrizeNumber;
@@ -17,5 +20,9 @@ public class Application {
         ArrayList<Integer> prizeNumberArray = separatePrizeNumber(prizeNumber); // 입력 값을 분리하여 배열에 담는다
 
         String bonusNumber = getValidBonusNumber(prizeNumberArray); // 올바른 보너스 번호를 입력했는지 확인하고 올바르지 않으면 재입력한다
+
+        HashMap<Integer, Integer> winHashTable = winLottoToPrizeNumber(prizeNumberArray, totalLotto, Integer.parseInt(bonusNumber)); // 로또와 당첨 번호가 일치하는지 확인하기 위해 해시테이블 생성
+
+        printPrizeText(winHashTable); // 결과를 출력한다
     }
 }

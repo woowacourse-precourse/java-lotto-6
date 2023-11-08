49565a981f11cc20d851d64a3cd9810c6bd60ba3
saebyeok2 <tomato8269@naver.com>
2023-11-08 23:11:58 +0900

    refactor: 로또 번호 및 당첨 통계 출력 포맷 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -37,21 +37,22 @@ public class OutputView {
     }
 
     private void printLottoCount(List<Lotto> lottos) {
-        System.out.println(lottos.size() + LOTTO_PURCHASE_FIXMESSAGE);
+        System.out.println("\n" + lottos.size() + LOTTO_PURCHASE_FIXMESSAGE);
     }
 
     private void printLottoNumbers(Lotto lotto) {
         List<Integer> lottoNumbers = lotto.getNumbers();
 
         System.out.print(LOTTO_NUMBERS_PREFIX_MESSAGE);
-        for (int number : lottoNumbers) {
-            System.out.print(number + SEPARATION);
+        for (int i = 0; i < lottoNumbers.size() - 1; i++) {
+            System.out.print(lottoNumbers.get(i) + SEPARATION);
         }
+        System.out.print(lottoNumbers.get(lottoNumbers.size() - 1));
         System.out.println(LOTTO_NUMBERS_SUFFIX_MESSAGE);
     }
 
     public void printLottosResult(Map<Rank, Integer> results) {
-        System.out.println("당첨 통계\n---");
+        System.out.println("\n당첨 통계\n---");
         System.out.println("3개 일치 (" + decimalFormat.format(FIFTH.getPrize()) + "원) - " + results.get(FIFTH) + "개");
         System.out.println("4개 일치 (" + decimalFormat.format(FOURTH.getPrize()) + "원) - " + results.get(FOURTH) + "개");
         System.out.println("5개 일치 (" + decimalFormat.format(THIRD.getPrize()) + "원) - " + results.get(THIRD) + "개");

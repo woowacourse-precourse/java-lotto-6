9cca96cdcc008507a2ea428f617b8fdc156e482a
kmh0601 <pongkmh@naver.com>
2023-11-08 22:48:03 +0900

    feat: 에러메시지를 관리하는 클래스 추가
    Lotto.validate()의 에러문구는 기존에 제공된 코드라서 손대지 않음

---
+++ src/main/java/util/ErroerMessage.java
@@ -0,0 +1,9 @@
+package util;
+
+public class ErroerMessage {
+    public static final String ONLY_NUMBER_INPUT_MONEY = "구입금액은 숫자만 입력해야 합니다.";
+    public static final String ONLY_THOUSANDS_INPUT_MONEY = "구입금액은 1000 단위로 입력해야 합니다.";
+    public static final String DUPLICATION_ERROR = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
+    public static final String RANGE_ERROR = "[ERROR] 로또 번호는 1~45 내에서 입력해야 합니다.";
+
+}

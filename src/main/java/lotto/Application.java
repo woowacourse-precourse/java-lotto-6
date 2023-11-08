740915672e785057f3dd3eca03fbd30dd437db2a
JeongMin <ajhappy12@gamil.com>
2023-11-08 23:08:51 +0900

    feat: 메인 시나리오 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String amountInput = LottoConsole.inputAmount();
+        System.out.println();
+
+        LottoStore lottoStore = new LottoStore();
+        List<Lotto> lottos = lottoStore.purchaseLottoTickets(amountInput);
+
+        LottoConsole.printLottoListInfo(lottos);
+        System.out.println();
+
+        String answerNumbers = LottoConsole.inputAnswerNumbers();
+        System.out.println();
+        String bonusNumber = LottoConsole.inputBonusNumber();
+        System.out.println();
+
+        LottoChecker checker = new LottoChecker(answerNumbers, bonusNumber);
+        LottoRewardsCenter center = new LottoRewardsCenter(checker);
+
+        List<LottoPrize> prizes = center.getPrizeResults(lottos);
+        Double ROI = center.calculateROI(prizes);
+
+        LottoConsole.printPrizeResults(prizes, ROI);
     }
 }

5a0f36d19c13e20ecf4a2b48e99c2cfd0f0e7714
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-08 14:42:19 +0900

    style: 적절한 변수 네이밍으로 변경

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -18,9 +18,9 @@ public class PriceTest {
     @DisplayName("Price로 문자가 주어지면 예외 발생 테스트.")
     @Test
     void validateNumber1() {
-        String input = "5000원";
+        String invalidInput = "5000원";
 
-        assertThatThrownBy(() -> new Price(input))
+        assertThatThrownBy(() -> new Price(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_NOT_NUMBER);
     }
@@ -28,9 +28,9 @@ public class PriceTest {
     @DisplayName("Price가 int타입 넘어가면 예외 발생 테스트")
     @Test
     void validateNumber2() {
-        String input = "4000000001";
+        String invalidInput = "4000000001";
 
-        assertThatThrownBy(() -> new Price(input))
+        assertThatThrownBy(() -> new Price(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_NOT_NUMBER);
     }
@@ -38,9 +38,9 @@ public class PriceTest {
     @DisplayName("Price가 한 개의 로또 가격 단위로 떨어지지 않으면 예외 발생 테스트")
     @Test
     void validateDividingUnitOfOneLottoPrice() {
-        String input = "500";
+        String invalidInput = "500";
 
-        assertThatThrownBy(() -> new Price(input))
+        assertThatThrownBy(() -> new Price(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_UNIT_PRICE_OF_PURCHASED_LOTTO);
     }
@@ -48,9 +48,9 @@ public class PriceTest {
     @DisplayName("Price가 0원이나 음수면 예외 발생하는 테스트")
     @Test
     void validateNumberRange1() {
-        String input = "0";
+        String invalidInput = "0";
 
-        assertThatThrownBy(() -> new Price(input))
+        assertThatThrownBy(() -> new Price(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_NOT_PURCHASE_LOTTO);
     }
@@ -58,9 +58,9 @@ public class PriceTest {
     @DisplayName("Price가 0원이나 음수면 예외 발생하는 테스트")
     @Test
     void validateNumberRange2() {
-        String input = "-1000";
+        String invalidInput = "-1000";
 
-        assertThatThrownBy(() -> new Price(input))
+        assertThatThrownBy(() -> new Price(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_NOT_PURCHASE_LOTTO);
     }
@@ -68,9 +68,9 @@ public class PriceTest {
     @DisplayName("Price가 1000원으로 떨어지나 로또 최대 구입 가격인 20억~int타입 사이면 예외 발생하는 테스트")
     @Test
     void validateNumberRange3() {
-        String input = "2000001000";
+        String invalidInput = "2000001000";
 
-        assertThatThrownBy(() -> new Price(input))
+        assertThatThrownBy(() -> new Price(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_OVER_MAX_PRICE_OF_LOTTO_FOR_PURCHASE);
     }
@@ -83,32 +83,32 @@ public class PriceTest {
     @DisplayName("Price에 따라 구매한 로또 개수를 집계하는 테스트")
     @Test
     void countPurchasedLotto() {
-        int expectedCountingLottos = price.countPurchasedLotto();
-        int actualCountingLottos = 8;
+        int expected = price.countPurchasedLotto();
+        int actual = 8;
 
-        assertThat(expectedCountingLottos).isEqualTo(actualCountingLottos);
+        assertThat(expected).isEqualTo(actual);
     }
 
     @DisplayName("당첨금액에 따라 수익률이 만들어지는지 테스트")
     @Test
     void makeWinningRate1() {
-        int totalWinningPrize = 5000;
+        int totalPrize = 5000;
 
-        double actualWinningRate = price.makeWinningRate(totalWinningPrize);
-        double expectedWinningRate = 62.5f;
+        double actual = price.makeWinningRate(totalPrize);
+        double expected = 62.5d;
 
-        assertThat(actualWinningRate).isEqualTo(expectedWinningRate);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("당첨금액에 따라 수익률이 만들어지는지 테스트")
     @Test
     void makeWinningRate2() {
         price = new Price("3000");
-        int totalWinningPrize = 5000;
+        int totalPrize = 5000;
 
-        double actualWinningRate = price.makeWinningRate(totalWinningPrize);
-        double expectedWinningRate = 166.7d;
+        double actual = price.makeWinningRate(totalPrize);
+        double expected = 166.7d;
 
-        assertThat(actualWinningRate).isEqualTo(expectedWinningRate);
+        assertThat(actual).isEqualTo(expected);
     }
 }

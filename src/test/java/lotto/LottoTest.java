4dec887a605e672792d643d754fcc143f6b4c800
KimSehoon <shggm2000@gmail.com>
2023-11-08 16:36:49 +0900

    test: createLottoByAscending method 구현
    - 로또 번호 생성 시 번호를 오름차순 정렬 테스트를 하는 createLottoByAscending method 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -28,5 +29,11 @@ class LottoTest {
     void createLottoByOverRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 오름차순 변경 테스트")
+    @Test
+    void createLottoByAscending() {
+        assertThat(new Lotto(List.of(6, 5, 4, 3, 2, 1)).getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 }

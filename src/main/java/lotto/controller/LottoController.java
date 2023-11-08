1a826aa3f4a95047947dd3e5e60044ccaa5fa37b
isyoo1213 <moethns@gmail.com>
2023-11-08 14:40:51 +0900

    feat: LottoTest 구매금액에 따른 로또 생성 기능 추가 LottoController - LottoService 의존성 추가   - createUserLottos()를 통해 로또 생성 로직 추가 README.md - 기능 명세 구현 체크

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,19 @@
 package lotto.controller;
 
+import lotto.model.Lottos;
 import lotto.model.Payment;
+import lotto.service.LottoService;
 import lotto.utils.InputValidator;
 import lotto.view.InputView;
 
 public class LottoController {
     InputView inputView = new InputView();
     InputValidator inputValidator = new InputValidator();
+    LottoService lottoService = new LottoService();
 
     public void proceedLottos() {
         Payment userPayment = buyLottos();
+        Lottos userLottos = lottoService.createUserLottos(userPayment.providePaymentStatus().get(1));
     }
 
     private Payment buyLottos() {

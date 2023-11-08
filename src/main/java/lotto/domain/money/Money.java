165bae3b53c9d0723e10b890db28fff77d467df9
sangdol <priceelees@gmail.com>
2023-11-08 13:09:56 +0900

    remove(Money): 사용되지 않는 기능 및 관련 테스트 삭제

--- src/main/java/lotto/domain/money/Money.java
+++ src/main/java/lotto/domain/money/Money.java
@@ -2,13 +2,10 @@ package lotto.domain.money;
 
 import static lotto.domain.constant.DomainConstant.HUNDRED;
 
-import java.text.DecimalFormat;
 import lotto.domain.constant.DomainConstant;
 
 public record Money(long amount) {
     public static final Money ZERO = new Money(0);
-    private static final String WON = "원";
-    private static final String DECIMAL_FORMAT = "###,###";
 
     public Money add(final long money) {
         return new Money(amount + money);
@@ -31,12 +28,6 @@ public record Money(long amount) {
     }
 
     public double getPercentageOf(Money other) {
-        return ((double)amount / other.amount()) * HUNDRED;
-    }
-
-    @Override
-    public String toString() {
-        DecimalFormat formatter = new DecimalFormat(DECIMAL_FORMAT);
-        return formatter.format(amount) + WON;
+        return ((double) amount / other.amount()) * HUNDRED;
     }
 }

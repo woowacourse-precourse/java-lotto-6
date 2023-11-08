2c34995739ad6cdb7bcea50c9b8cc4e6a002ef5f
unknown <gpfls981220@gmail.com>
2023-11-08 22:00:12 +0900

    refactor: update Lotto class to check for duplicate numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override

2718c8731b5c96a4bffcebad2d0865e05a2ba97a
leedabin2 <12193152@inha.edu>
2023-11-08 19:24:50 +0900

    test(UserHandleTest): Add error handling test for getAndValidateBonusNumber method

--- src/test/java/lotto/UserHandlerTest.java
+++ src/test/java/lotto/UserHandlerTest.java
@@ -4,6 +4,8 @@ import lotto.handler.UserHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserHandlerTest {
@@ -42,7 +44,7 @@ class UserHandlerTest {
     // 당첨 번호 입력 에러 테스트
     @DisplayName("validateWinningNumberInput 메서드 테스트: null 입력 시 Exception 발생")
     @Test
-    void validateWinningNumberInput_null_숫자_입력시() {
+    void validateWinningNumberInput_null_값_입력시() {
         assertThatThrownBy(() -> UserHandler.validateWinningNumberInput(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 번호를 입력해주세요.");
@@ -95,4 +97,46 @@ class UserHandlerTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 번호는 1부터 45까지의 숫자입니다.");
     }
+
+    // 보너스 번호 입력에 대한 테스트 코드
+    @DisplayName("getAndValidateBonusNumber 메서드 테스트: null 입력 시 Exception 발생")
+    @Test
+    void getAndValidateBonusNumber_Null_값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateAndReturnBonusNumber(null, Arrays.asList("1", "2", "3", "4", "5", "6")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 다시 입력하세요.");
+    }
+
+    @DisplayName("getAndValidateBonusNumber 메서드 테스트: 빈값 입력 시 Exception 발생")
+    @Test
+    void getAndValidateBonusNumber_빈값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateAndReturnBonusNumber("", Arrays.asList("1", "2", "3", "4", "5", "6")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 다시 입력하세요.");
+    }
+
+    @DisplayName("getAndValidateBonusNumber 메서드 테스트: 숫자가 아닌 값 입력 시 Exception 발생")
+    @Test
+    void getAndValidateBonusNumber_숫자가_아닌_값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateAndReturnBonusNumber("abc", Arrays.asList("1", "2", "3", "4", "5", "6")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자를 입력하세요.");
+    }
+
+    @DisplayName("getAndValidateBonusNumber 메서드 테스트: 보너스 번호가 1~45 범위를 벗어난 경우 Exception 발생")
+    @Test
+    void getAndValidateBonusNumber_범위를_벗어난_값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateAndReturnBonusNumber("46", Arrays.asList("1", "2", "3", "4", "5", "6")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1부터 45까지의 숫자입니다.");
+    }
+
+    @DisplayName("getAndValidateBonusNumber 메서드 테스트: 보너스 번호가 당첨 번호와 중복된 경우 Exception 발생")
+    @Test
+    void getAndValidateBonusNumber_중복된값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateAndReturnBonusNumber("6", Arrays.asList("1", "2", "3", "4", "5", "6")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
 }

8ec57f969b6e9fa4c029b8066df21d29f2ce0dcd
jjw0071 <jjw0071@naver.com>
2023-11-08 17:21:06 +0900

    feat(Lotto) : 로또 번호에 중복된 숫자가 있을 경우 로또 생성 시에 오류 발생시키도록 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNoDuplicates(numbers);
         this.numbers = numbers;
     }
 
@@ -19,5 +22,14 @@ public class Lotto {
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers(){
         return numbers;
+    }
+
+    private void validateNoDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for(Integer element:numbers){
+            if(!uniqueNumbers.add(element)){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+            }
+        }
     }
 }

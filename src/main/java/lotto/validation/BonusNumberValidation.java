6381ac3a68bde8f8684e296ba8d0715a1852cf62
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:31:57 +0900

    refactor(BonusNumberValidation): 보너스 번호의 유효성 검증 로직 클래스로 분리

--- src/main/java/lotto/validation/BonusNumberValidation.java
+++ src/main/java/lotto/validation/BonusNumberValidation.java
@@ -1,2 +1,18 @@
-package lotto.validation;public class BonusNumberValidation {
+package lotto.validation;
+
+import static lotto.constant.ErrorMessage.DUPLICATION_EXCEPTION;
+
+import java.util.List;
+
+public class BonusNumberValidation {
+    public static void validateBonusNumber(int bonusNumber, List<Integer> winningNumber) {
+        WinningNumbersValidation.validateNumberInRange(bonusNumber);
+        validateDuplication(bonusNumber, winningNumber);
+    }
+
+    private static void validateDuplication(int bonusNumber, List<Integer> winningNumber) {
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATION_EXCEPTION.getMessage());
+        }
+    }
 }

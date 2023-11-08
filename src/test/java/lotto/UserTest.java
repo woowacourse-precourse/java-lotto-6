9a72e95ffd3bd83e97652cdfd78672fe10bbd0d8
kmh0601 <pongkmh@naver.com>
2023-11-08 20:35:10 +0900

    test: 로또 금액에 대한 예외 처리 기능 추가
    private 함수에 대한 테스트라서 테스트 후에 주석처리함

---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+//    @DisplayName("로또 금액이 1000원 단위가 아니면 오류가 발생한다.")
+//    @Test
+//    void wrongMoneyInputTest() {
+//        String input = "10230";
+//        InputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+//
+//        assertThatThrownBy(()->User.inputMoney()).isInstanceOf(IllegalArgumentException.class);
+//    }
+}

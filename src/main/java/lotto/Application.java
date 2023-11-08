daeb916f0fe8f07ff4725b9a9452697a40108e7a
dustjq1004 <dustjq1005@naver.com>
2023-11-08 16:33:31 +0900

    yeonsup/rename : LottoService 명칭 변경으로 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,15 @@
 package lotto;
 
 import lotto.controller.LottoProgram;
+import lotto.controller.service.LottoService;
+import lotto.utils.NumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         LottoProgram lottoProgram = new LottoProgram(new InputView(), new OutputView()
-                , new LottoStore(new NumberGenerator()));
+                , new LottoService(new NumberGenerator()));
         lottoProgram.start();
 
     }

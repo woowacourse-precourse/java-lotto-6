c774c738cef3aaace9353aa59ccc8d0738884987
김관진 <cozyschooll@gmail.com>
2023-11-08 14:38:34 +0900

    docs: docs/README.md 수정 주석 처리 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,7 +6,6 @@ import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         LottoGameController lottoGameController = new LottoGameController(inputView, outputView);

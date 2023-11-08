4ef546d8efa91da65018a2eb3fc0954f11b695c0
wonindev <sangwonparkdev@gmail.com>
2023-11-08 16:45:35 +0900

    refactor: 검증코드 true, false를 반환하지 않고 바로 예외를 던지는 형태로 변경

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,45 +1,69 @@
 package lotto.utils;
 
+import lotto.constants.ErrorMessage;
 import lotto.constants.LottoEnum;
 
 import java.util.List;
 
 public class Validator {
 
-    public static boolean isNull(String input) {
-        return input == null;
-    }
-
-    public static boolean isContainBlank(String input) {
-        return input.contains(" ");
-    }
-
-    public static boolean isBlank(String input) {
-        return input.equals(" ");
-    }
-
-    public static boolean isNumber(String input) {
-        return input.matches("\\d+");
-    }
-
-    public static boolean underByLottoPrice(String input) {
-        int num = Integer.parseInt(input);
-        return num < LottoEnum.LOTTO_PRICE.getValue();
-    }
-
-    public static boolean isDivisibleByLottoSize(String input) {
-        int num = Integer.parseInt(input);
-        return num % LottoEnum.LOTTO_PRICE.getValue() == 0;
-    }
-
-    public static boolean isDuplicate(List<Integer> numbers, int input, int idx) {
-        for (int i = idx + 1; i < numbers.size(); i++) {
-            if (input == numbers.get(i)) return true;
+    public static void isNull(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_ERROR.getError());
         }
-        return false;
     }
 
-    public static boolean isOneToFortyFive(int input) {
-        return input >= LottoEnum.LOTTO_MIN_NUMBER.getValue() && input <= LottoEnum.LOTTO_MAX_NUMBER.getValue();
+    public static void isContainBlank(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException(ErrorMessage.CONTAIN_BLANK_ERROR.getError());
+        }
+    }
+
+    public static void isBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.BLANK_ERROR.getError());
+        }
+    }
+
+    public static void notNumber(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_ERROR.getError());
+        }
+    }
+
+    public static void underByLottoPrice(String input) {
+        int num = Integer.parseInt(input);
+        if (num < LottoEnum.LOTTO_PRICE.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.UNDER_LOTTO_PRICE_ERROR.getError());
+        }
+    }
+
+    public static void isDivisibleByLottoSize(String input) {
+        int num = Integer.parseInt(input);
+        if (num % LottoEnum.LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_DIVISIBLE_1000_ERROR.getError());
+        }
+    }
+
+    public static void isDuplicate(List<Integer> numbers, int input, int idx) {
+        for (int i = idx + 1; i < numbers.size(); i++) {
+            if (input == numbers.get(i)) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER_ERROR.getError());
+            }
+        }
+    }
+
+    public static void isDuplicate(List<String> numbers, String input, int idx) {
+        for (int i = idx + 1; i < numbers.size(); i++) {
+            if (input.equals(numbers.get(i))) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER_ERROR.getError());
+            }
+        }
+    }
+
+    public static void isOneToFortyFive(int input) {
+        if (input < LottoEnum.LOTTO_MIN_NUMBER.getValue() || input > LottoEnum.LOTTO_MAX_NUMBER.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ONE_TO_FORTYFIVE_ERROR.getError());
+        }
     }
 }

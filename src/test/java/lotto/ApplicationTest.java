6028af9f62226e74b8cbdc0f9c4592d287d25595
kimdongyeon <do121712@naver.com>
2023-11-08 22:49:34 +0900

    feat : 금액 천원 이하 입력, 당첨 번호 잘못 입력, 당첨 번호 범위 초과 입력, 보너스 번호 잘못 입력, 보너스 번호 범위 초과 입력, 보너스번호가 당첨번호에 포함되게 입력시 [ERROR] 매새자 잘 나오는지 테스트케이스 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -14,56 +14,18 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 기능_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]",
-                            "3개 일치 (5,000원) - 1개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 62.5%입니다."
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            run("8000", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains("8개를 구매했습니다.", "[8, 21, 23, 41, 42, 43]", "[3, 5, 11, 16, 32, 38]", "[7, 11, 16, 35, 36, 44]", "[1, 8, 11, 31, 41, 42]", "[13, 14, 16, 38, 42, 45]", "[7, 11, 30, 40, 42, 43]", "[2, 13, 22, 32, 38, 45]", "[1, 3, 5, 14, 22, 45]", "3개 일치 (5,000원) - 1개", "4개 일치 (50,000원) - 0개", "5개 일치 (1,500,000원) - 0개", "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개", "6개 일치 (2,000,000,000원) - 0개", "총 수익률은 62.5%입니다.");
+        }, List.of(8, 21, 23, 41, 42, 43), List.of(3, 5, 11, 16, 32, 38), List.of(7, 11, 16, 35, 36, 44), List.of(1, 8, 11, 31, 41, 42), List.of(13, 14, 16, 38, 42, 45), List.of(7, 11, 30, 40, 42, 43), List.of(2, 13, 22, 32, 38, 45), List.of(1, 3, 5, 14, 22, 45));
     }
 
     @Test
     void 기능_테스트2() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("1000", "8,21,23,41,42,44", "43");
-                    assertThat(output()).contains(
-                            "1개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "3개 일치 (5,000원) - 0개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 300000.0%입니다."
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43)
-        );
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            run("1000", "8,21,23,41,42,44", "43");
+            assertThat(output()).contains("1개를 구매했습니다.", "[8, 21, 23, 41, 42, 43]", "3개 일치 (5,000원) - 0개", "4개 일치 (50,000원) - 0개", "5개 일치 (1,500,000원) - 0개", "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개", "6개 일치 (2,000,000,000원) - 0개", "총 수익률은 300000.0%입니다.");
+        }, List.of(8, 21, 23, 41, 42, 43));
     }
 
 
@@ -71,6 +33,62 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 금액_천원이하_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("900");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 당첨번호_잘못입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 당첨번호_중복_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 당첨번호_범위_초과_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,55");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스번호_잘못입력시_범위_초과_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "45j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스번호_범위_초과_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,55", "56");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스번호_당첨번호에_포함되게_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "6");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }

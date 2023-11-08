3f094aa3cab850550288d3193892376d94c3511b
Sinorang <aodkrak@naver.com>
2023-11-08 20:59:26 +0900

    fix. 유효성 검사 기능 추가
    - 유효성 검사 기능 구현

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -1,12 +1,18 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.util.ValidationUtil;
 
 public class WinLotto extends Lotto {
     int bonusNumber;
 
     public WinLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        validate(numbers, bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        ValidationUtil.isValidBonusNumber(numbers, bonusNumber);
     }
 }

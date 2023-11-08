30d110826b7d6992304dca8120168a817f400efa
min1018 <102647148+min1018@users.noreply.github.com>
2023-11-08 16:46:28 +0900

    fix: 메서드 명과 display 문구 수정

--- src/test/java/lotto/serviceTest/DecideAwardTest.java
+++ src/test/java/lotto/serviceTest/DecideAwardTest.java
@@ -13,25 +13,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DecideAwardTest {
     @Test
-    @DisplayName("흐음")
-    void test() {
-        // 유효한 로또 번호와 당첨 번호 생성
+    @DisplayName("사용자의 로또 번호와 당첨 번호에 따른 올바른 결과 반환 여부 테스트 ")
+    void examineGettingAwardtest() {
         List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int bonusNumber = 7;
 
-        // 유저 생성
         User user = new User(1, Collections.singletonList(new Lotto(lottoNumbers)));
 
-        // 당첨 번호 및 유저의 로또를 전달하여 결과 계산
         JackpotNumber jackpotNumber = new JackpotNumber(new Lotto(winningNumbers));
         Result result = DecideAward.userResult(user, jackpotNumber);
 
-        // 기대되는 결과를 정의
         Result expected = new Result();
         expected.add(Rank.FIRST);
 
-        // 결과 확인
         assertThat(result.getResults()).isEqualTo(expected.getResults());
     }
 

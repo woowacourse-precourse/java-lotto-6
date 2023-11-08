c252ceb883271c27b272ee40acab9cf7b54e18a4
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 18:56:35 +0900

    feat: 로또 당첨 통계 결과 출력 기능 구현

--- src/main/java/lotto/constant/OutputMessage.java
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -1,7 +1,9 @@
 package lotto.constant;
 
 public enum OutputMessage {
-    OUTPUT_PURCHASE_AMOUNT_MESSAGE("%d개를 구매했습니다.");
+    OUTPUT_PURCHASE_AMOUNT_MESSAGE("%d개를 구매했습니다."),
+    OUTPUT_WINNING_STATISTICS("당첨 통계"),
+    OUTPUT_LINE("---");
 
     private final String template;
 
@@ -11,5 +13,9 @@ public enum OutputMessage {
 
     public String getMessage(int count) {
         return String.format(template, count);
+    }
+
+    public String getMessage() {
+        return template;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
+import lotto.constant.LottoCount;
 import lotto.constant.OutputMessage;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class OutputView {
@@ -13,6 +15,15 @@ public class OutputView {
         System.out.println(OutputMessage.OUTPUT_PURCHASE_AMOUNT_MESSAGE.getMessage(randomSixNumbers.size()));
         for(List<Integer> randomSixNumber : randomSixNumbers){
             System.out.println(randomSixNumber);
+        }
+    }
+
+    public void printWinningStatistics(HashMap<Integer, Integer> winningResult) {
+        System.out.println(OutputMessage.OUTPUT_WINNING_STATISTICS.getMessage());
+        System.out.println(OutputMessage.OUTPUT_LINE.getMessage());
+        for (LottoCount lottoCount : LottoCount.values()) {
+            int count = winningResult.getOrDefault(lottoCount.getGrade(), 0);
+            System.out.println(lottoCount.getMessage(count));
         }
     }
 }

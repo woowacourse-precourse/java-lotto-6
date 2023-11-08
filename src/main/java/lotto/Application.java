9552f0ae22a5ba2b294b08cd4c5f56b2db699b27
kdyddd <kdyddd@gmail.com>
2023-11-08 21:34:27 +0900

    add purchase

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = 0;
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String inputAmount = Console.readLine();
+
+            if (isValidInput(inputAmount)) {
+                purchaseAmount = Integer.parseInt(inputAmount) / 1000;
+                break;
+            }
+        }
+        System.out.println(purchaseAmount + "개를 구매했습니다.");
+    }
+
+    private static boolean isValidInput(String input) {
+        try {
+            int inputAmount = Integer.parseInt(input);
+            if (inputAmount % 1000 != 0) {
+                System.err.println("[ERROR] 구입금액은 1000원 단위여야 합니다. 다시 입력해주세요.");
+                return false;
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            System.err.println("[ERROR] 구입금액은 숫자로 입력해주세요.");
+            return false;
+        }
+
     }
 }

d91c58599e8c0c0ba1da40590566e01783168bc2
sonjiseokk <thswltjr11@gmail.com>
2023-11-08 12:18:22 +0900

    fix: InputValidator 리팩토링 과정에서 문제 수정

--- src/main/java/lotto/utils/validator/UnitValidator.java
+++ src/main/java/lotto/utils/validator/UnitValidator.java
@@ -7,18 +7,14 @@ import static lotto.constant.constant.*;
 
 public class UnitValidator {
     public static void validMoneyUnitCheck(int money) {
-        if (unitMinCheck(money)) {
-            throw new IllegalArgumentException("1000원 이상으로 입력해주세요.");
-        }
-        if (rangeCheck(money)) {
-            throw new IllegalArgumentException("1000원 단위로 입력해주세요.");
+        if (moneyRangeCheck(money)) {
+            throw new IllegalArgumentException("1000원 이상의 1000원 단위의 금액을 입력해주세요.");
         }
     }
 
     public static void validNumberUnitCheck(List<Integer> integers) {
         List<Integer> collected = integers.stream()
-                .filter(integer -> integer >= LOTTO_MIN_UNIT)
-                .filter(integer -> integer <= LOTTO_MAX_UNIT)
+                .filter(integer -> lottoRangeCheck(integer))
                 .toList();
 
         if (collected.size() != integers.size()) {
@@ -26,17 +22,14 @@ public class UnitValidator {
         }
     }
     public static void validBonusUnitCheck(int money) {
-        if (bonusRangeCheck(money)) {
+        if (!lottoRangeCheck(money)) {
             throw new IllegalArgumentException("로또 범위에 맞게 입력해주세요. 1 이상 45 이하입니다.");
         }
     }
-    private static boolean bonusRangeCheck(int money) {
-        return (money < LOTTO_MIN_UNIT) || (money > LOTTO_MAX_UNIT);
+    private static boolean lottoRangeCheck(int money) {
+        return (money >= LOTTO_MIN_UNIT) && (money <= LOTTO_MAX_UNIT);
     }
-    private static boolean unitMinCheck(int money) {
-        return money < 1000;
-    }
-    private static boolean rangeCheck(int money) {
-        return money % LOTTO_PURCHASED_UNIT > 0;
+    private static boolean moneyRangeCheck(int money) {
+        return (money % LOTTO_PURCHASED_UNIT > 0) || (money < 1000);
     }
 }

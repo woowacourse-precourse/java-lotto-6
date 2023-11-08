5904ad738fc2e4df04dc4d10ba586f62ef8eeae2
dmong <dt2980_hee@naver.com>
2023-11-08 10:22:05 +0900

    test: Lotto.contains 메소드 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -25,4 +26,24 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    void 로또_랜덤_생성_정상_동작_확인() {
+        Lotto randomLotto = Lotto.createRandomLotto();
+
+        assertThat(randomLotto).isInstanceOf(Lotto.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void 인자로_받은_숫자가_존재하면_contains_true_반환(int input) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.contains(LottoNumber.create(input))).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7, 10, 41})
+    void 인자로_받은_숫자가_없다면_contains_false_반환(int input) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.contains(LottoNumber.create(input))).isFalse();
+    }
 }

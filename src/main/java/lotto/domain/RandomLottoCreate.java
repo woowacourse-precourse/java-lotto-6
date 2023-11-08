16bc3793eb3356c861233983bb442052cea162f3
JongMinCh0i <wonderland314592@gmail.com>
2023-11-08 10:02:22 +0900

    feat(RandomLottoCreate): 판매된 로또 매수 마다 랜덤 로또를 생성하는 기능 추가

--- src/main/java/lotto/domain/RandomLottoCreate.java
+++ src/main/java/lotto/domain/RandomLottoCreate.java
@@ -1,8 +1,8 @@
 package lotto.domain;
 
-import static lotto.domain.LottoValue.LOTTO_RANGE_IN_END_VALUE;
-import static lotto.domain.LottoValue.LOTTO_RANGE_IN_START_VALUE;
-import static lotto.domain.LottoValue.LOTTO_SIZE;
+import static lotto.domain.Lotto.LOTTO_SIZE;
+import static lotto.domain.LottoNumber.LOTTO_RANGE_IN_END_VALUE;
+import static lotto.domain.LottoNumber.LOTTO_RANGE_IN_START_VALUE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
@@ -11,14 +11,22 @@ import java.util.stream.IntStream;
 
 public class RandomLottoCreate {
 
-    public List<Lotto> generateRandomLotto(int quantity) {
-        return IntStream.range(0, quantity)
-                .mapToObj(i -> generateRangeOfUniqueNumbers())
-                .map(Lotto::new)
+    public static List<Lotto> issueRandomLottos(int lottoQuantity) {
+        return IntStream.range(0, lottoQuantity)
+                .mapToObj(i -> generateLottoFromUniqueNumbers())
                 .collect(Collectors.toList());
     }
 
-    private List<Integer> generateRangeOfUniqueNumbers() {
+    private static Lotto generateLottoFromUniqueNumbers() {
+        List<Integer> uniqueNumbers = generateRangeOfUniqueNumbers();
+        List<LottoNumber> lottoNumbers = uniqueNumbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        return new Lotto(lottoNumbers);
+    }
+
+    private static List<Integer> generateRangeOfUniqueNumbers() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_IN_START_VALUE,
                 LOTTO_RANGE_IN_END_VALUE,
                 LOTTO_SIZE);

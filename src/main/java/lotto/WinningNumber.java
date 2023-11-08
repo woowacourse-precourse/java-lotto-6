d2cbcbb2c8952fa07e72c984a53b5f3b7d5ea01d
zoid79 <zidwkd00@naver.com>
2023-11-08 20:58:02 +0900

    feat: fin WinningNumber

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,13 +1,33 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumber {
-    private final List<Integer> numbers;
+    private final Set<Integer> numbers;
     private final int bonusNumber;
 
-    public WinningNumber(List<Integer> numbers,int bonusNumber) {
+    public Set<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public WinningNumber(Set<Integer> numbers, int bonusNumber) {
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
     }
+public ERating count(Lotto lottoTicket){
+        double count=0;
+        for(int ticketNumber:lottoTicket.getNumbers()){
+            if(numbers.contains(ticketNumber))count++;
+            if(ticketNumber==bonusNumber) count=count+0.5;
+        }
+        if(count>5.5) return ERating.eFIRST;
+        if(count>5) return ERating.eSECOND;
+        if(count>4.5) return ERating.eTHIRD;
+        if(count>3.5) return ERating.eFOURTH;
+        if(count>2.5) return ERating.eFIFTH;
+    return ERating.eSIXTH;
+}
+
+
 }

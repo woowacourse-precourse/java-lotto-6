6eb1d86cc99c1848c8ce6f8d15bc10ad6bfc4138
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-08 15:15:48 +0900

    feat(LottoMachine): check if error message appears correctly

--- src/test/java/lotto/view/SystemMoneyInputTest.java
+++ src/test/java/lotto/view/SystemMoneyInputTest.java
@@ -1,4 +1,26 @@
+package lotto.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SystemMoneyInputTest {
-  
+
+    @DisplayName("빈칸이 주어졌는지 확인")
+    @Test
+    void checkMoneyBlank() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SystemMoneyInput.validateNone("");
+        });
+    }
+
+    @DisplayName("1000으로 안나눠질 시 예외처리 확인하기")
+    @Test
+    void checkDivision() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SystemMoneyInput.validateDivision("6666");
+        });
+    }
+
 }

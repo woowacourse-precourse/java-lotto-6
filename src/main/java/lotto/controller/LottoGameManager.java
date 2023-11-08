66e14beb0f4640456626ce4befd543b22b218f80
정찬영 <jcy0342@gmail.com>
2023-11-08 12:38:28 +0900

    refactor: 제공된 예외 테스트는 output 테스트이므로  예외 메세지 출력을 Sysout으로 변경

--- src/main/java/lotto/controller/LottoGameManager.java
+++ src/main/java/lotto/controller/LottoGameManager.java
@@ -39,7 +39,7 @@ public class LottoGameManager {
                 consumer.setPurchaseAmount(purchaseAmount);
                 break;
             } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -58,7 +58,7 @@ public class LottoGameManager {
                 winning.setNumbers(numbers);
                 break;
             } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -70,7 +70,7 @@ public class LottoGameManager {
                 winning.setBonusNumber(bonusNumber);
                 break;
             } catch (IllegalArgumentException e){
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }

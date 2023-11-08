d4173b9f802c751af930b6d314dd9a646ee2b507
Lee ChanMi <anytime0224@gmail.com>
2023-11-08 20:03:13 +0900

    test: LottosTest 구현
    - Lotto 간의 비교를 위해 equals, hashCode 메소드 override

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,6 +6,7 @@ import static lotto.common.ExceptionMessage.ERROR_NUMBERS_SIZE_INCORRECT;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -64,5 +65,22 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 }

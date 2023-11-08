988fe6ab2df58d0b3fb550948b9d5f3697b25f26
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 16:33:11 +0900

    feat[Lotto]: 검증 메소드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,12 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.StringJoiner;
+
+import static lotto.constant.DefinedNumber.LOTTO_LENGTH;
+import static lotto.constant.ErrorMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,7 +24,11 @@ public class Lotto {
     }
 
     private void validateLottoNumberRange(List<Integer> numbers) {
-
+        for (Integer number : numbers) {
+            if(number <= 0 || number >= 46){
+                throw new IllegalArgumentException(LOTTO_NUMBERS_RANGE_WARNING);
+            }
+        }
     }
 
     private void validateDuplicateNumber(List<Integer> numbers) {
@@ -26,11 +36,28 @@ public class Lotto {
     }
 
     private void validateLottoNumbersSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_LENGTH_WARNING);
         }
     }
 
-    private void sortNumbers(List<Integer> numbers) {}
+    private void sortNumbers(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if(set.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBERS_WARNING);
+        }
+    }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+        for (Integer number : numbers) {
+            joiner.add(number.toString());
+        }
+        return joiner.toString();
+    }
 }

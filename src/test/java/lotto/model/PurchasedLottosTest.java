e82ee0b9517aa42e72c224e1cbb4da1474d11af5
dltjdn <hana40004@naver.com>
2023-11-08 18:17:05 +0900

    test(PurchasedLottosTest): 클래스 단위 테스트 추가

--- src/test/java/lotto/model/PurchasedLottosTest.java
+++ src/test/java/lotto/model/PurchasedLottosTest.java
@@ -5,19 +5,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import lotto.mock.TestLottoGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PurchasedLottosTest {
-
-    @DisplayName("로또 개수만큼 로또 객체를 생성한다.")
-    @Test
-    void createLottos() {
-        int numberOfLottos = 2;
+    LottoGenerator lottoGenerator;
+    @BeforeEach
+    void setUp(){
         List<Integer> validNumbers = List.of(1, 2, 3, 4, 5, 6);
-        PurchasedLottos purchasedLottos = PurchasedLottos.of(numberOfLottos, new TestLottoGenerator(validNumbers));
+        lottoGenerator = new TestLottoGenerator(validNumbers);
+    }
+
+    @DisplayName("PurchasedLottos 생성 및 로또 목록 확인")
+    @Test
+    void createPurchasedLottosAndCheckLottoList() {
+        int numberOfLottos = 5;
+        PurchasedLottos purchasedLottos = PurchasedLottos.of(numberOfLottos, lottoGenerator);
 
         assertThat(purchasedLottos).isNotNull();
+        assertThat(purchasedLottos.getPurchaseLottos()).hasSize(numberOfLottos);
+    }
+
+    @DisplayName("당첨 정보 확인")
+    @Test
+    void checkWinningInfos() {
+        int numberOfLottos = 1;
+        PurchasedLottos purchasedLottos = PurchasedLottos.of(numberOfLottos, lottoGenerator);
+        WinningLotto winningLotto = WinningLotto.of(new Lotto(List.of(1, 2, 3, 4, 5, 9)), 6);
+
+        List<WinningInfo> winningInfos = purchasedLottos.getWinnningInfos(winningLotto);
+
+        assertThat(winningInfos.get(0)).isEqualTo(WinningInfo.FIVE_AND_BONUS_MATCH);
     }
 
 

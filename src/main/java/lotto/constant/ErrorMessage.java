0267ef113c6efd8356a78e1799b20d2b8b2940eb
jjongwon7 <jjongwon7@naver.com>
2023-11-08 21:23:17 +0900

    feat: 문자열 정수 변환 기능 추가 문자열을 정수로 변환하여 리턴하고, 변환이 불가능한 경우 예외 발생시키는 메서드 작성

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     // 문자열 처리
     public static final String INPUT_EMPTY = "입력하신 문자열은 빈 문자열입니다.";
     public static final String INPUT_CONTAINS_WHITE_SPACE = "입력하신 문자열에 공백이 포함되어 있습니다.";
+    public static final String INPUT_IS_NOT_INTEGER = "입력하신 문자열은 정수가 아닙니다.";
 }
--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -2,6 +2,7 @@ package lotto.utils;
 
 import static lotto.constant.ErrorMessage.INPUT_CONTAINS_WHITE_SPACE;
 import static lotto.constant.ErrorMessage.INPUT_EMPTY;
+import static lotto.constant.ErrorMessage.INPUT_IS_NOT_INTEGER;
 
 public class StringUtils {
     public static void validEmptyString(String str) {
@@ -13,6 +14,14 @@ public class StringUtils {
     public static void validContainsWhiteSpace(String str) {
         if (str != null && str.contains(" ")) {
             throw new IllegalArgumentException(INPUT_CONTAINS_WHITE_SPACE);
+        }
+    }
+
+    public static Integer getIntegerValue(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_INTEGER);
         }
     }
 }

8578256a8e052eec33c6b7ac93e6c3c2cd878b76
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 20:15:50 +0900

    feat: 로또 횟수만큼 로또 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.Lotto;
+
 public class OutputView {
     public void printLottoCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
     }
+
+    public void printLottoNumbers(int count, Lotto randomLottoNumbers) {
+        System.out.println(count + "개를 구매했습니다.");
+        
+        for (int i = 0; i < count; i++)
+            System.out.println(randomLottoNumbers);
+    }
+
+    
 }

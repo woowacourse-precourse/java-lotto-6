315a622d672297b5b37c1cd420f3f683d1d96cef
LimGayoung <ygy920@gmail.com>
2023-11-08 17:41:32 +0900

    feat: 로또 통계 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,11 +4,20 @@ import static lotto.global.constant.LottoNumber.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
+import lotto.model.LottoResult;
 import lotto.model.LottoTickets;
+import lotto.model.WinningNumbers;
+import lotto.model.WinningStatistic;
 
 public class LottoService {
+
+    private final static Map<Integer, String> winningMatch = new HashMap<>();
 
     public LottoTickets createLottoTickets(int purchaseAmount) {
         List<Lotto> lottoTickets = new ArrayList<>();
@@ -19,6 +28,33 @@ public class LottoService {
         }
         return new LottoTickets(lottoTickets);
     }
-    
 
+    public WinningStatistic statisticWinningResult(LottoTickets lottoTickets, WinningNumbers winningNumbers) {
+        List<LottoResult> lottoResults = lottoTickets.resultOfLottoTickets(winningNumbers);
+        List<Integer> lottoResultCount = countLottoResult(lottoResults);
+        WinningStatistic winningStatistic = makeWinningStatistic(lottoResultCount);
+
+        return winningStatistic;
+    }
+
+    private List<Integer> countLottoResult(List<LottoResult> lottoResults) {
+        List<Integer> lottoResultCount = new ArrayList<>(Collections.nCopies(8,0));
+
+        for (LottoResult lottoResult : lottoResults) {
+            int count = lottoResult.matchCount();
+            if(lottoResult.matchCount() == 5){ //5개 일치와 [5개 일치+보너스 번호 일치] 구분 위함
+                count += lottoResult.bonusMatchCount();
+            }
+            if (lottoResult.matchCount() == 6) { //6개 일치는 7개로 치환
+                count++;
+            }
+            lottoResultCount.set(count, lottoResultCount.get(count)+1);
+        }
+        return lottoResultCount;
+    }
+
+    private WinningStatistic makeWinningStatistic(List<Integer> lottoResultCount) {
+        return new WinningStatistic(lottoResultCount.get(3), lottoResultCount.get(4), lottoResultCount.get(5)
+                , lottoResultCount.get(6), lottoResultCount.get(7));
+    }
 }
--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -3,7 +3,12 @@ package lotto.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import lotto.model.Lotto;
 import lotto.model.LottoTickets;
+import lotto.model.WinningNumbers;
+import lotto.model.WinningStatistic;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +20,33 @@ class LottoServiceTest {
     void createLottoTickets() {
         LottoTickets lottoTickets = lottoService.createLottoTickets(8);
         assertThat(lottoTickets.getLottoTickets().size()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("당첨 통계 생성 성공")
+    void statisticWinningResult(){
+        LottoTickets lottoTickets = new LottoTickets(Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), //6
+                new Lotto(Arrays.asList(2, 3, 4, 5, 6, 7)), //5+1
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8)), //5
+                new Lotto(Arrays.asList(1, 2, 3, 4, 6, 8)), //5
+                new Lotto(Arrays.asList(1, 2, 3, 4, 8, 9)), //4
+                new Lotto(Arrays.asList(1, 2, 3, 8, 9, 10)), //3
+                new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)), //3
+                new Lotto(Arrays.asList(1, 2, 7, 8, 9, 10)), //2
+                new Lotto(Arrays.asList(10, 11, 12, 13, 14, 15)))); //0
+
+
+        WinningStatistic winningStatistic = lottoService.statisticWinningResult(lottoTickets,
+                new WinningNumbers(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), 7));
+
+        assertAll(() -> {
+            assertThat(winningStatistic.threeMatchCount()).isEqualTo(2);
+            assertThat(winningStatistic.fourMatchCount()).isEqualTo(1);
+            assertThat(winningStatistic.fiveMatchCount()).isEqualTo(2);
+            assertThat(winningStatistic.fiveAndBonusMatchCount()).isEqualTo(1);
+            assertThat(winningStatistic.sixMatchCount()).isEqualTo(1);
+        });
+
+
     }
 }

c013e22e19d96ebe26e77d3fc64dcacb183694c0
WinterHana <haha920@naver.com>
2023-11-08 21:24:39 +0900

    refactor : 필요 없는 메서드 및 import 삭제
    - 필요없는 내용 삭제

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,9 +1,6 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.type.ErrorMessageType;
-
-import java.util.Arrays;
 
 public class InputService {
     public static String inputMoney() {
@@ -23,12 +20,5 @@ public class InputService {
         System.out.println("보너스 번호를 입력해주세요.");
         String number = Console.readLine();
         return number;
-    }
-
-    public static ErrorMessageType compareErrorMessageType(String message) {
-        return Arrays.stream(ErrorMessageType.values())
-                .filter(errorType -> errorType.message().equals(message))
-                .findFirst()
-                .orElse(null);
     }
 }
--- src/test/java/lotto/domain/BonusLottoNumberTest.java
+++ src/test/java/lotto/domain/BonusLottoNumberTest.java
@@ -1,13 +1,11 @@
 package lotto.domain;
 
 import lotto.type.ErrorMessageType;
-import lotto.type.ResultType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BonusLottoNumberTest {
 

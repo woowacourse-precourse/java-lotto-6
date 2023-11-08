beeebb512ac9004b794218768b426cc0f99aeea4
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 16:54:17 +0900

    feat(printRandomSixNumber): 2차원 배열에 담긴 6자리의 랜덤번호 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,18 @@
 package lotto.view;
 
+import lotto.constant.OutputMessage;
+
+import java.util.List;
+
 public class OutputView {
     public void printErrorMessage(IllegalArgumentException e) {
         System.out.println(e.getMessage());
+    }
+
+    public void printRandomSixNumbers(List<List<Integer>> randomSixNumbers) {
+        System.out.println(OutputMessage.OUTPUT_PURCHASE_AMOUNT_MESSAGE.getMessage(randomSixNumbers.size()));
+        for(List<Integer> randomSixNumber : randomSixNumbers){
+            System.out.println(randomSixNumber);
+        }
     }
 }

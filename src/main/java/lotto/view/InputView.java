05e1ac05b78396825962467b662fab55b1294030
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 13:02:26 +0900

    feat(Winning) : inputBuyingPriceView 메서드 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.UserBuyingException;
+import lotto.utils.InputUtil;
+
+public class InputView {
+
+    private final UserBuyingException userBuyingException = new UserBuyingException();
+
+    public int inputBuyingPriceView(){
+        try {
+            String buyPrice = Console.readLine();
+            userBuyingException.validateBuyingException(buyPrice);
+            return InputUtil.toIntStringNumberParser(buyPrice);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력은 숫자만 가능합니다");
+        }
+    }
+}

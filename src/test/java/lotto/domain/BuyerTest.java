7b30645ae608984ea477460f18a0825539889298
ay-eonii <naayen@naver.com>
2023-11-08 14:26:40 +0900

    test(buyer): 구매금액이 1000원 이하인 경우 예외 발생

---
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BuyerTest {
+
+    @DisplayName("구매금액이 1,000원 이하라면 예외가 발생한다.")
+    @Test
+    void getCost() {
+        assertThatThrownBy(() -> new Buyer(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매금액은 1,000원 이상이어야 합니다.");
+    }
+
+    @Test
+    void getBuyCount() {
+    }
+}

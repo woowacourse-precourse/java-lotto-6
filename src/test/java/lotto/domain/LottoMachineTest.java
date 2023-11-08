796082276e5831d206ce80039638f59b52745b1a
kssumin <201566@jnu.ac.kr>
2023-11-08 18:51:56 +0900

    chore(LottoMachine) : 기존 LottoTicket에 있던 코드를 LottoMachine으로 바꾸면서 테스트 코드 옮김

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,24 +1,22 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class LottoTicketTest {
+class LottoMachineTest {
     @Test
-    @DisplayName("구입 수량 만큼 로또를 생성한다.")
+    @DisplayName("지불한 금액에 따라 로또를 발급한다.")
     void createLottoTicket() {
         // given
         Money money = new Money(2000);
         LottoMachine lottoMachine = new LottoMachine();
 
         // when
-        LottoTicket lottoTicket = lottoMachine.buy(money, () -> new Lotto(Arrays.asList(1,2,3,4,5,6)));
-        List<Lotto> lottos = lottoTicket.getLottos();
+        LottoTicket lottoTicket = lottoMachine.buy(money, () -> new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
 
         // then
-        Assertions.assertEquals(2, lottos.size());
+        Assertions.assertEquals(2, lottoTicket.getLottos().size());
     }
 }

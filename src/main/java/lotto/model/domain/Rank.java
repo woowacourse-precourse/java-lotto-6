fb951699556303afaeb6d4739a814e0026b4edc3
Hayoon <hy97@sju.ac.kr>
2023-11-08 20:07:05 +0900

    feat(Rank): 랭킹 출력 값 Enum 변수로 추가

--- src/main/java/lotto/model/domain/Rank.java
+++ src/main/java/lotto/model/domain/Rank.java
@@ -1,20 +1,22 @@
 package lotto.model.domain;
 
 public enum Rank {
-    FIRST(1, 2_000_000_000, 6),
-    SECOND(2, 30_000_000, 5),
-    THIRD(3, 1_500_000, 5),
-    FOURTH(4, 50_000, 4),
-    FIFTH(5, 5_000, 3);
+    FIRST(1, 2_000_000_000, 6, "6개 일치 (2,000,000,000원) - %d개\n"),
+    SECOND(2, 30_000_000, 5, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    THIRD(3, 1_500_000, 5, "5개 일치 (1,500,000원) - %d개\n"),
+    FOURTH(4, 50_000, 4, "4개 일치 (50,000원) - %d개\n"),
+    FIFTH(5, 5_000, 3, "3개 일치 (5,000원) - %d개\n");
 
     private final int rank;
     private final long reward;
     private final long hitCount;
+    private final String message;
 
-    Rank(int rank, long reward, long hitCount) {
+    Rank(int rank, long reward, long hitCount, String message) {
         this.rank = rank;
         this.reward = reward;
         this.hitCount = hitCount;
+        this.message = message;
     }
 
     public int getRank() {
@@ -27,5 +29,9 @@ public enum Rank {
 
     public long getHitCount() {
         return hitCount;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }

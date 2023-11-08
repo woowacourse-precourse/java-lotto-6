b7d6fd50854d707c9840efcb4807057e0c81a4ff
dsadara <chaehyun.forest@gmail.com>
2023-11-08 23:25:06 +0900

    fix: 당첨 금액 수정

--- src/main/java/lotto/type/WinningType.java
+++ src/main/java/lotto/type/WinningType.java
@@ -4,7 +4,7 @@ public enum WinningType {
 
     THREE_HIT("3개 일치",3, true, 5000),
     FOUR_HIT("4개 일치", 4, true, 50000),
-    FIVE_HIT("5개 일치", 5, true,150000),
+    FIVE_HIT("5개 일치", 5, true,1500000),
     FIVE_NO_BONUS_HIT("5개 일치, 보너스 불 일치", 5, false,30000000),
     SIX_HIT("6개 일치", 6, true, 2000000000);
 

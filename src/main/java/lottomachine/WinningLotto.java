9a3087050cfe1e526607176ffaac8e3395e56762
Picka <marmong@naver.com>
2023-11-08 18:48:27 +0900

    refactor: update outputter and update spell

--- src/main/java/lottomachine/WinningLotto.java
+++ src/main/java/lottomachine/WinningLotto.java
@@ -3,6 +3,7 @@ package lottomachine;
 import java.util.ArrayList;
 import java.util.List;
 import service.Inputter;
+import service.Outputter;
 
 public class WinningLotto {
 
@@ -20,14 +21,16 @@ public class WinningLotto {
 
     public int requestBonusNumber(Inputter inputter, Lotto winningLottoNumbers) {
         int bonusNumber = 0;
+        Outputter outputter = new Outputter();
         while (true) {
             try {
+                outputter.prmoptBonusNumber();
                 String input = inputter.inputBonusNumber();
                 bonusNumber = transformStringtoBonusNumber(input);
                 validate(bonusNumber, winningLottoNumbers);
                 return bonusNumber;
             } catch (IllegalArgumentException e) {
-                System.out.println("[Error] 유효하지 않은 번호입니다. 숫자를 입력해 주세요.");
+                System.out.println("[Error] 유효하지 않은 번호입니다.");
             }
         }
     }
@@ -39,7 +42,7 @@ public class WinningLotto {
             try {
                 numbers.add(Integer.parseInt(token.trim()));
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[Error] 잘못된 문자열입니다.");
+                throw new IllegalArgumentException("[ERROR] 잘못된 문자열입니다.");
             }
         }
         return numbers;
@@ -50,10 +53,10 @@ public class WinningLotto {
         try {
             number = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[Error] 잘못된 보너스 번호입니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 보너스 번호입니다.");
         }
         if (number > 46 || number < 1) {
-            throw new IllegalArgumentException("[Error] 잘못된 보너스 번호입니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 보너스 번호입니다.");
         }
         return number;
     }
@@ -62,7 +65,7 @@ public class WinningLotto {
         List<Integer> numbers = winningLottoNumbers.getLottoNumbers();
         for (int number : numbers) {
             if (number == bonusNumber) {
-                throw new IllegalArgumentException("[Error] 잘못된 번호를 입력하였습니다.");
+                throw new IllegalArgumentException("[ERROR] 잘못된 번호를 입력하였습니다.");
             }
         }
     }

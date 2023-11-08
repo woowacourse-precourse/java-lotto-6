7d1d32f9f525e2f1cd5c855f30058c2f7e1c1a46
Silver <leeeun5423@naver.com>
2023-11-08 17:34:58 +0900

    feat : 로또에 포함된 숫자인지 테스트 구현

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -92,4 +92,34 @@ class ApplicationTest extends NsTest {
 
         assertThat(lotto.sameNumberCount(win)).isEqualTo(2);
     }
+
+    @Test
+    void NotContainedTest() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(32);
+        numbers.add(22);
+        numbers.add(9);
+        numbers.add(13);
+
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto.isContain(33)).isEqualTo(0);
+    }
+
+    @Test
+    void ContainTest() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(32);
+        numbers.add(22);
+        numbers.add(9);
+        numbers.add(13);
+
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto.isContain(32)).isEqualTo(1);
+    }
 }

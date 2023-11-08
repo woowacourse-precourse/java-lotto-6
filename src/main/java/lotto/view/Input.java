6f44024007cfd68244a0c9c24f22e63ac5ad35df
getbravelee <getbravelee@gmail.com>
2023-11-08 22:53:57 +0900

    refactor: 입력 반환 값 변경

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,19 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     private static final String INPUT_PAYMENT = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public int inputPayment() {
+    public static String inputPayment() {
         System.out.println(INPUT_PAYMENT);
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
-    public String inputWinningNumber() {
+    public static String inputWinningNumber() {
         System.out.println(INPUT_WINNING_NUMBER);
         return Console.readLine();
     }
 
-    public String inputBonusNumber() {
+    public static String inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }

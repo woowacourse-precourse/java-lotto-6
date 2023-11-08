6be9f99720cb18b8beef57ff1a1ba48d848b4f1f
Dowon <3120kang@gmail.com>
2023-11-08 23:42:16 +0900

    Refactor(OutputView): 하드 코딩 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,8 +8,13 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
+    private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String WINNING_STATISSTICS = "당첨 통계";
+    private static final String PERFORATION_LINE = "---";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
     public void printCount(int count) {
-        System.out.printf("%d개를 구매했습니다.\n", count);
+        System.out.printf(PURCHASE_MESSAGE, count);
     }
 
     public void printLotto(Lotto lotto) {
@@ -21,12 +26,12 @@ public class OutputView {
     }
 
     public void printWinningStatistics(ResultRepository resultRepository) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(WINNING_STATISSTICS);
+        System.out.println(PERFORATION_LINE);
         System.out.println(resultRepository);
     }
 
     public void printRateOfReturn(Double rateOfReturn) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 }

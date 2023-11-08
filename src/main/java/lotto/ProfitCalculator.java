be23fd34193ad3cf48bd7bf355b2803bf3471df6
suhoon <jobak4200nim@nate.com>
2023-11-08 19:24:45 +0900

    refactor: 수익률 계산 로직 최적화

--- src/main/java/lotto/ProfitCalculator.java
+++ src/main/java/lotto/ProfitCalculator.java
@@ -7,31 +7,26 @@ import static lotto.enumerate.LottoPrize.MATCH_5;
 import static lotto.enumerate.LottoPrize.MATCH_5_PLUS_BONUS;
 import static lotto.enumerate.LottoPrize.MATCH_6;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProfitCalculator {
 
     public double calculateProfit(int moneyInput, Map<Integer, Integer> winners) {
         int prize = 0;
+        Map<Integer, Integer> prizeMap = new HashMap<>();
+        prizeMap.put(3, MATCH_3.getPrize());
+        prizeMap.put(4, MATCH_4.getPrize());
+        prizeMap.put(5, MATCH_5.getPrize());
+        prizeMap.put(5 + BONUS_BALL, MATCH_5_PLUS_BONUS.getPrize());
+        prizeMap.put(6, MATCH_6.getPrize());
+
         for (Map.Entry<Integer, Integer> entry : winners.entrySet()) {
             int match = entry.getKey();
             int count = entry.getValue();
-            if (match == 3) {
-                prize += MATCH_3.getPrize() * count;
-            }
-            if (match == 4) {
-                prize += MATCH_4.getPrize() * count;
-            }
-            if (match == 5) {
-                prize += MATCH_5.getPrize() * count;
-            }
-            if (match == 5 + BONUS_BALL) {
-                prize += MATCH_5_PLUS_BONUS.getPrize() * count;
-            }
-            if (match == 6) {
-                prize += MATCH_6.getPrize() * count;
-            }
+            prize += prizeMap.getOrDefault(match, 0) * count;
         }
+
         double profit = (double) prize / (double) moneyInput * 100;
         return profit;
     }

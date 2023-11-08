b6d17306e001e6b6c49d664a305950fcb365393c
alscjf1329 <alscjf1329@gmail.com>
2023-11-08 21:08:40 +0900

    refactor(LottoPurchaseOutput): 클래스 내 모든 릴터럴들을 상수로 변경
    프리코드 가이드대로 클래스 내 모든 릴터럴들을 상수로 변경했습니다.

--- src/main/java/lotto/io/LottoPurchaseOutput.java
+++ src/main/java/lotto/io/LottoPurchaseOutput.java
@@ -5,14 +5,25 @@ import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.List;
 import lotto.data.Lotto;
-import lotto.domain.LottoResult;
 import lotto.data.LottoPrize;
+import lotto.domain.LottoResult;
 
 public enum LottoPurchaseOutput {
     ASK_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     ASK_WINNING_LOTTO_NUMBER("당첨 번호를 입력해 주세요."),
     ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
     private String message;
+    private final static String LOTTO_PURCHASE_MESSAGE = "%d개를 구매했습니다.\n";
+    private final static String WINNING_STATISTICS_HEADER_MESSAGE = "당첨 통계\n---------\n";
+    private final static String WINNING_STATISTICS_BODY_MESSAGE = "%s - %s개\n";
+    private final static String WINNING_STATISTICS_FOOTER_MESSAGE = "총 수익률은 %s%%입니다.";
+    private final static List<LottoPrize> PRINT_LOTTO_PRIZE_ORDER = List.of(
+            LottoPrize.FIFTH
+            , LottoPrize.FOURTH
+            , LottoPrize.THIRD
+            , LottoPrize.SECOND
+            , LottoPrize.FIRST);
+    private final static int DIGITS_AFTER_DECIMAL_POINT = 1;
 
     LottoPurchaseOutput(String message) {
         this.message = message;
@@ -23,7 +34,7 @@ public enum LottoPurchaseOutput {
     }
 
     public static void printLottos(List<Lotto> lottos) {
-        System.out.println(lottos.size() + "개를 구매했습니다.");
+        System.out.printf(LOTTO_PURCHASE_MESSAGE, lottos.size());
         StringBuilder sb = new StringBuilder();
         lottos.forEach(lotto -> sb.append(lotto).append("\n"));
         System.out.println(sb);
@@ -32,15 +43,20 @@ public enum LottoPurchaseOutput {
     public static void printWinningStatistics(LottoResult lottoResult) {
         StringBuilder sb = new StringBuilder();
         HashMap<LottoPrize, BigDecimal> result = lottoResult.getLottoRank();
-        sb.append("당첨 통계\n").append("---------\n");
-        sb.append(LottoPrize.FIFTH.getMessage()).append(" - ").append(result.get(LottoPrize.FIFTH)).append("개\n");
-        sb.append(LottoPrize.FOURTH.getMessage()).append(" - ").append(result.get(LottoPrize.FOURTH)).append("개\n");
-        sb.append(LottoPrize.THIRD.getMessage()).append(" - ").append(result.get(LottoPrize.THIRD)).append("개\n");
-        sb.append(LottoPrize.SECOND.getMessage()).append(" - ").append(result.get(LottoPrize.SECOND)).append("개\n");
-        sb.append(LottoPrize.FIRST.getMessage()).append(" - ").append(result.get(LottoPrize.FIRST)).append("개\n");
+        sb.append(WINNING_STATISTICS_HEADER_MESSAGE);
 
-        BigDecimal profitPercent = lottoResult.getProfitPercent().setScale(1, RoundingMode.DOWN);
-        sb.append(String.format("총 수익률은 %s%%입니다.", profitPercent));
+        for (LottoPrize lottoPrize : PRINT_LOTTO_PRIZE_ORDER) {
+            BigDecimal count = result.get(lottoPrize);
+            sb.append(getWinningStatisticsBody(lottoPrize.getMessage(), count));
+        }
+
+        BigDecimal profitPercent = lottoResult.getProfitPercent()
+                .setScale(DIGITS_AFTER_DECIMAL_POINT, RoundingMode.DOWN);
+        sb.append(String.format(WINNING_STATISTICS_FOOTER_MESSAGE, profitPercent));
         System.out.println(sb);
+    }
+
+    private static String getWinningStatisticsBody(String message, BigDecimal count) {
+        return String.format(WINNING_STATISTICS_BODY_MESSAGE, message, count);
     }
 }

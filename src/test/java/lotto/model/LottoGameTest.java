64106010ee89d6b7ac43a990950c7e3041700c41
Hwang In Tae <sjobs66@gmail.com>
2023-11-08 22:48:27 +0900

    test: 당첨 번호 입력 검증 테스트 작성

--- src/test/java/lotto/model/LottoGameTest.java
+++ src/test/java/lotto/model/LottoGameTest.java
@@ -13,19 +13,27 @@ import org.junit.jupiter.api.Test;
 class LottoGameTest extends NsTest {
     private LottoGame game = new LottoGame();
 
+    @DisplayName("구입금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void 로또미발행() {
+        assertThatThrownBy(() -> game.setLottoPurchaseAmount("8550"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호는 숫자여야한다.")
+    @Test
+    void 숫자가아닌당첨번호() {
+        assertThatThrownBy(() -> game.setWinningLotto("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
     @DisplayName("구입금액만큼 로또를 발행한다.")
     @Test
     void 로또발행() {
         game.setLottoPurchaseAmount("10000");
         game.createLottoTickets();
         assertThat(game.getPurchasedLottos().size()).isEqualTo(10);
-    }
-
-    @DisplayName("구입금액이 1000원 단위가 아니면 예외가 발생한다.")
-    @Test
-    void 로또미발행() {
-        assertThatThrownBy(() -> game.setLottoPurchaseAmount("8550"))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호는 오름차순으로 정렬한다.")

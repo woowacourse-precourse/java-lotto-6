704c590d9e0b9f859d9c78e9ff9f5c7480130252
KangDeok <rkdejr2321@naver.com>
2023-11-08 16:54:46 +0900

    [feat] 로또 낙첨 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,6 +4,7 @@ import static lotto.domain.LottoConstant.*;
 
 public enum LottoRank {
 
+    MISS(MISS_UNDER_COUNT, MISS_WINNER_AMOUNT, "%d개 이하 일치, 낙첨 (%,d원) - %d개"),
     FIFTH(FIFTH_WINNER_HIT_COUNT, FIFTH_WINNER_AMOUNT, "%d개 일치 (%,d원) - %d개"),
     FOURTH(FORTH_WINNER_HIT_COUNT, FORTH_WINNER_AMOUNT, "%d개 일치 (%,d원) - %d개"),
     THIRD(THIRD_WINNER_HIT_COUNT, THIRD_WINNER_AMOUNT, "%d개 일치 (%,d원) - %d개"),

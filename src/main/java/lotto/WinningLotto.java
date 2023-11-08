4802fc30e3e2fb49ad0394ba3eec1cefe5476f83
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 14:40:48 +0900

    feat: 보너스 번호와 당첨 번호가 중복되는지 검증 기능
    당첨 로또를 클래스로 만들어 사용
    Lotto, LottoNumber를 사용해 검증 조건들을 재사용

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import static lotto.constant.ErrorMessage.DUPLICATED_BONUS;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private final Lotto lottoNumbers;
+    private final LottoNumber bonusNumber;
+
+    public WinningLotto(List<Integer> lottoNumbers, int bonusNumber) {
+        validateDuplicateBonusNumber(lottoNumbers, bonusNumber);
+        this.lottoNumbers = new Lotto(lottoNumbers);
+        this.bonusNumber = new LottoNumber(bonusNumber);
+    }
+
+    private void validateDuplicateBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
+        if (lottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATED_BONUS);
+        }
+    }
+
+}

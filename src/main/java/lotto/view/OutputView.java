b5c5036317b410eb9c5ff619df4dafdce0e068b9
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-11-08 21:41:50 +0900

    fix : 사용자의 로또를 출력하는 부분 분리 / 당첨 결과 출력부분 형식에 맞게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,16 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import lotto.constant.OutputMessage;
 import lotto.constant.LottoPrize;
+import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
 
 public class OutputView {
+    
+    DecimalFormat df = new DecimalFormat("###,###.##");
     
     public static void printMessage(String message) {
         System.out.println(message);
@@ -17,44 +21,47 @@ public class OutputView {
     }
 
     public void printPurchaseMessage(int purchaseNumber) {
-        System.out.println(Integer.toString(purchaseNumber) + OutputMessage.PURCHASE_MESSAGE.getMessage());
+        System.out.println("\n" + Integer.toString(purchaseNumber) + OutputMessage.PURCHASE_MESSAGE.getMessage());
     }
     
-    public void printUserLotto(List<Integer> userLotto) {
+    public void printUserLotto(List<Lotto> userLotto) {
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        for (int i = 0; i < userLotto.size(); i++) {
-            if (i == userLotto.size() - 1) {
-                sb.append(userLotto.get(i));
-                sb.append("]\n");
-            }
-            sb.append(userLotto.get(i));
-            sb.append(", ");
+        for (Lotto lotto : userLotto) {
+            createUserLotto(sb, lotto.getNumbers());
         }
         System.out.print(sb.toString());
     }
 
+    public void createUserLotto(StringBuilder sb, List<Integer> numbers) {
+        sb.append("[");
+        for (int i = 0; i < numbers.size()-1; i++) {
+            sb.append(numbers.get(i));
+            sb.append(", ");
+        }
+        sb.append(numbers.get(numbers.size()-1) + "]\n");
+    }
+
     public void winnerNumberMessage() {
-        System.out.println(OutputMessage.WINNING_NUMBER_MESSAGE.getMessage());
+        System.out.println("\n" + OutputMessage.WINNING_NUMBER_MESSAGE.getMessage());
     }
 
     public void bonusNumberMessage() {
-        System.out.println(OutputMessage.BONUS_NUMBER_MESSAGE.getMessage());
+        System.out.println("\n" + OutputMessage.BONUS_NUMBER_MESSAGE.getMessage());
     }
 
     public void resultIntroductionMessage() {
-        System.out.println(OutputMessage.RESULT_MESSAGE.getMessage());
+        System.out.println("\n" + OutputMessage.RESULT_MESSAGE.getMessage());
     }
 
     public void resultMessage(LottoResult lottoResult) {
-        System.out.print(LottoPrize.FIFTH_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START + LottoPrize.FIFTH_PRIZE.getPrice() + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getThreeCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
-        System.out.print(LottoPrize.FOURTH_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START + LottoPrize.FOURTH_PRIZE.getPrice() + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getFourCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
-        System.out.print(LottoPrize.THIRD_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START + LottoPrize.THIRD_PRIZE.getPrice() + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getFiveCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
-        System.out.print(LottoPrize.SECOND_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BONUS_NUMBER_MESSAGE + OutputMessage.BRACKET_START + LottoPrize.SECOND_PRIZE.getPrice() + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getFiveBonusCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
-        System.out.print(LottoPrize.FIRST_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START + LottoPrize.FIRST_PRIZE.getPrice() + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getSixCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
+        System.out.println(LottoPrize.FIFTH_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START.getMessage() + df.format(LottoPrize.FIFTH_PRIZE.getPrice()) + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getThreeCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
+        System.out.println(LottoPrize.FOURTH_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START.getMessage() + df.format(LottoPrize.FOURTH_PRIZE.getPrice()) + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getFourCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
+        System.out.println(LottoPrize.THIRD_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START.getMessage() + df.format(LottoPrize.THIRD_PRIZE.getPrice()) + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getFiveCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
+        System.out.println(LottoPrize.SECOND_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.RESULT_MESSAGE5.getMessage() + OutputMessage.BRACKET_START.getMessage() + df.format(LottoPrize.SECOND_PRIZE.getPrice()) + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getFiveBonusCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
+        System.out.println(LottoPrize.FIRST_PRIZE.getNumberOfMatch() + OutputMessage.RESULT_MESSAGE2.getMessage() + OutputMessage.BRACKET_START.getMessage() + df.format(LottoPrize.FIRST_PRIZE.getPrice()) + OutputMessage.RESULT_MESSAGE3.getMessage() + lottoResult.getSixCount() + OutputMessage.RESULT_MESSAGE4.getMessage());
     }
 
-    public void resultRateMessage(double rate) {
-        System.out.println(OutputMessage.RATE_MESSAGE.getMessage() + rate + OutputMessage.END_MESSAGE.getMessage());
+    public void resultRateMessage(String rate) {
+        System.out.print(OutputMessage.RATE_MESSAGE.getMessage() + rate.replaceAll("\\B(?=(\\d{3})+(?!\\d))", ",") + OutputMessage.END_MESSAGE.getMessage());
     }
 }

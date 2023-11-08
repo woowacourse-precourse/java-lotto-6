fd0b5c5fa36cbb5218cfaed2d818904ee4b6999c
MyStraw <kikongdosa@gmail.com>
2023-11-08 22:05:54 +0900

    Refactor: 삭제 중복된 유효성 검사코드
    InputView에 입력관련 유효성 검사를 함으로서
    Lotto클래스에 있던 유효성 검사를 삭제

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,32 +1,14 @@
 package lotto.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = new ArrayList<>(numbers);
         bubbleSort(this.numbers);
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 해요.");
-        }
-        Set<Integer> notDuplicatedNumbers = new HashSet<>(numbers);
-        if (notDuplicatedNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException("로또 번호는 중복되면 안돼요.");
-        }
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("로또 번호는 1과 45 사이의 값이어야 해요.");
-            }
-        }
     }
 
     public List<Integer> getNumbers() {
@@ -49,7 +31,4 @@ public class Lotto {
         list.set(index1, list.get(index2));
         list.set(index2, temp);
     }
-
-
-    // TODO: 추가 기능 구현
 }

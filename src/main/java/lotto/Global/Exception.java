d2fcdcd6307bc1d7ba507851cdfe7ed401c16166
YejiGong <yejigong00@gmail.com>
2023-11-08 19:10:05 +0900

    Refactor: 쓰지않는 Enum값 제거 및 README 수정

--- src/main/java/lotto/Global/Exception.java
+++ src/main/java/lotto/Global/Exception.java
@@ -1,7 +1,6 @@
 package lotto.Global;
 
 public enum Exception {
-    WRONG_VALUE("[ERROR] 입력이 정상적으로 이루어지지 않았습니다."),
     NOT_NUMBER_VALUE("[ERROR] 숫자를 입력해주세요."),
     LOTTO_WRONG_AMOUNT_INPUT("[ERROR] 로또 번호는 1~45의 범위 내의 숫자여야합니다."),
     LOTTO_OUT_OF_RANGE_INPUT("[ERROR] 로또 번호는 %s으로 구분되어 6개가 입력되어야 합니다."),

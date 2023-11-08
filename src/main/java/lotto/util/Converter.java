ddd76352d792c65a4534511bc3c8c848417d5ba1
haebyun <bhk4392@gmail.com>
2023-11-08 14:43:44 +0900

    feat(Converter): 보너스 번호 문자열을 정수로 변환하고 변환할 수 없다면 예외를 발생시키는 기능 구현

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import lotto.validation.LottoException;
 
 public class Converter {
+    private static final String SPLIT_POINT = ",";
 
     public static int convertPurchaseAmount(String input) {
         try {
@@ -22,6 +23,14 @@ public class Converter {
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
             return inputNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoException.LETTER_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    public static int convertBonusNumber(String input) {
+        try {
+            return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(LottoException.LETTER_ERROR_MESSAGE.getMessage());
         }

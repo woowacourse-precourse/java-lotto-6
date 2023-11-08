47596c29575495963d220a6df32ecfb0197d5b66
KangDeok <rkdejr2321@naver.com>
2023-11-08 16:54:27 +0900

    [feat] 로또 낙첨 상수 추가

--- src/main/java/lotto/domain/LottoConstant.java
+++ src/main/java/lotto/domain/LottoConstant.java
@@ -10,11 +10,13 @@ public class LottoConstant {
     public static final int THIRD_WINNER_HIT_COUNT = 5;
     public static final int FORTH_WINNER_HIT_COUNT = 4;
     public static final int FIFTH_WINNER_HIT_COUNT = 3;
+    public static final int MISS_UNDER_COUNT = 2;
     public static final int PRICE_PER_LOTTO = 1_000;
     public static final int FIRST_WINNER_AMOUNT = 2_000_000_000;
     public static final int SECOND_WINNER_AMOUNT = 30_000_000;
     public static final int THIRD_WINNER_AMOUNT = 1_500_000;
     public static final int FORTH_WINNER_AMOUNT = 50_000;
     public static final int FIFTH_WINNER_AMOUNT = 5_000;
+    public static final int MISS_WINNER_AMOUNT = 0;
     public static final String LOTTO_NUMBER_SEPARATOR = ",";
 }

29f1653bf49e15c72d71b6a0973c406319b3c157
Seol_JY <wlsdud5654@gmail.com>
2023-11-08 15:09:14 +0900

    refactor(Lotto): Lotto 객체 접근제어자 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    private Lotto(List<Integer> numbers) {
         validateSizeExact(numbers);
         validateNoDuplicate(numbers);
         validateNumbersInRange(numbers);

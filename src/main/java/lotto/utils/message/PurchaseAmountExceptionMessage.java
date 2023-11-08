974674ea5b4c0794eb841fa9c83d27f6549c18c6
june-777 <wlwhswnsrl96@gmail.com>
2023-11-08 16:38:15 +0900

    refactor(PurchaseAmountExceptionMessage): 입력 관련 예외 메시지 제거

--- src/main/java/lotto/utils/message/PurchaseAmountExceptionMessage.java
+++ src/main/java/lotto/utils/message/PurchaseAmountExceptionMessage.java
@@ -2,9 +2,6 @@ package lotto.utils.message;
 
 public enum PurchaseAmountExceptionMessage {
 
-    BLANK("[ERROR] 구입 금액을 입력해 주세요."),
-    EXCEED_LENGTH("[ERROR] 구입 금액은 9 자리 이내로 입력해야 합니다."),
-    NOT_NUMERIC("[ERROR] 구입 금액은 숫자로만 입력해야 합니다."),
     OUT_OF_AMOUNT_RANGE("[ERROR] 구입 금액은 1천원 ~ 1억원 까지 가능합니다."),
     INVALID_AMOUNT_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
 

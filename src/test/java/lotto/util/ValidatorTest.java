401305429d8b1a4a8582e6138f45b18954444d3e
wonchanjoo <wcj0911@naver.com>
2023-11-08 18:41:53 +0900

    refactor: 테스트 DisplayName 수정

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -1,7 +1,5 @@
 package lotto.util;
 
-import lotto.util.Message;
-import lotto.util.Validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidatorTest {
 
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void winningNumbersCount() {
         String input = "1,2,3,4,5,6,7";
@@ -18,7 +16,7 @@ public class ValidatorTest {
                 .hasMessageContaining(Message.incorrectCountError);
     }
 
-    @DisplayName("로또 번호 중 숫자가 아닌 값이 있는 경우 예외가 발생한다.")
+    @DisplayName("당첨 번호 중 숫자가 아닌 값이 있는 경우 예외가 발생한다.")
     @Test
     void nonNumericWinningNumbers() {
         String input = "1,a,3,4,5,6";
@@ -27,7 +25,7 @@ public class ValidatorTest {
                 .hasMessageContaining(Message.nonNumericInputError);
     }
 
-    @DisplayName("로또 번호 중 범위가 1~45가 아닌 경우 예외가 발생한다.")
+    @DisplayName("당첨 번호 중 범위가 1~45가 아닌 경우 예외가 발생한다.")
     @Test
     void outOfRangeWinningNumbers() {
         String input = "1,2,0,4,5,6";
@@ -36,7 +34,7 @@ public class ValidatorTest {
                 .hasMessageContaining(Message.outOfRangeError);
     }
 
-    @DisplayName("중복된 로또 번호가 존재하는 경우 예외가 발생한다.")
+    @DisplayName("중복된 당첨 번호가 존재하는 경우 예외가 발생한다.")
     @Test
     void duplicateWinningNumbers() {
         String input = "1,2,3,3,4,5";

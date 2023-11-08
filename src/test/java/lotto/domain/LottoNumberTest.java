86a5c0be7d98322e444a0d2216f1cba3ea876151
dunowljj <uuufg3322@gmail.com>
2023-11-08 11:55:34 +0900

    test : 로또 번호를 숫자 1개 기준으로 비교하는 기능 구현
    - 비교하는 기능 구현
      - 1개 --> 1개 비교
      - 1개 --> 여러개 비교
      - 기존의 contains를 재활용하고 equals를 추가
    - 사실상 테스트만 추가되었다.

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -29,5 +31,42 @@ public class LottoNumberTest {
         assertThatThrownBy(() -> new LottoNumber(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.LOTTO_NUMBER_NOT_IN_RANGE.getMessage());
+    }
+
+    @DisplayName("한 개의 로또 번호 끼리 비교하기")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 45})
+    public void lottoNumber_equals_oneToOne(int number) throws Exception {
+        //given
+        LottoNumber lottoNumber1 = new LottoNumber(number);
+        LottoNumber lottoNumber2 = new LottoNumber(number);
+
+        //when, then
+        assertThat(lottoNumber1).isEqualTo(lottoNumber2);
+    }
+
+    @DisplayName("한 개의 로또 번호를 여러개의 로또 넘버와 비교하기 -> 일치하는 번호가 있으면 true 반환")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    public void lottoNumber_equals_oneToMany(int number) throws Exception {
+        //given
+        LottoNumber lottoNumber = new LottoNumber(number);
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when, then
+        assertThat(winningLotto.contains(lottoNumber)).isTrue();
+    }
+
+    @DisplayName("한 개의 로또 번호를 여러개의 로또 넘버와 비교하기 -> 일치하는 번호가 없으면 false 반환")
+    @ParameterizedTest
+    @ValueSource(ints = {7, 8, 9, 10, 15, 26})
+    public void lottoNumber_notEquals_oneToMany(int number) throws Exception {
+
+        //given
+        LottoNumber lottoNumber = new LottoNumber(number);
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when, then
+        assertThat(winningLotto.contains(lottoNumber)).isFalse();
     }
 }

2ab8932466192e4cb7fd206e1f165e0289a9c80d
leorb159 <ldgsocial159@naver.com>
2023-11-08 21:03:26 +0900

    feat: LottoController 생성, run 메소드 작성

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,2 +1,26 @@
-package lotto;public class LottoController {
+package lotto;
+
+import java.util.List;
+
+public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoManagement lottoManagement;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.lottoManagement = new LottoManagement();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        int amount = inputView.purchaseLottoAmount();
+        List<Lotto> lottery = lottoManagement.buyLotto(amount);
+        outputView.writePurchaseDetails(lottery);
+        Lotto winningNumbers = new Lotto(inputView.readWinningLotto());
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, inputView.readBonusNumber());
+        LottoResult lottoResult = new LottoResult(lottery, winningLotto);
+        LottoWinningResultSummary lottoWinningResultSummary = new LottoWinningResultSummary(lottoResult);
+        outputView.writeLottoResult(lottoWinningResultSummary);
+    }
 }

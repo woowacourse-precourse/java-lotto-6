aa464ab823a3ca15dddbb244c2370717a54fa193
BangMingGing <privatebmk802@naver.com>
2023-11-08 16:58:53 +0900

    feat(Lotto): Add lotto model and validate functions

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
-package lotto;
+package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validLength(numbers);
+        validDuplication(numbers);
+    }
+
+    private void validLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 수는 6개여야 한다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validDuplication(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException(("[ERROR] 로또 번호는 중복되어선 안된다."));
+        }
+    }
 }

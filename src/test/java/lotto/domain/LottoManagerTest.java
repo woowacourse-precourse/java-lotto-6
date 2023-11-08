8382ee84f4b56c7175aec3cd3ea992ed00cbb01d
Juhyorim <joohr1234@gmail.com>
2023-11-08 23:28:49 +0900

    test: LottoManagerTest 중복코드 BeforeEach로 처리

--- src/test/java/lotto/domain/LottoManagerTest.java
+++ src/test/java/lotto/domain/LottoManagerTest.java
@@ -13,20 +13,28 @@ import lotto.ui.ConsoleInput;
 import lotto.ui.ConsoleOutput;
 import lotto.ui.Input;
 import lotto.ui.Output;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoManagerTest {
+    private Input input;
+    private Output output;
+    private LottoGenerator lottoGenerator;
+
+    @BeforeEach
+    void init() {
+        input = mock(ConsoleInput.class);
+        output = mock(ConsoleOutput.class);
+        lottoGenerator = mock(LottoGenerator.class);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"1000", "5000", "1000000", "5000000"})
     @DisplayName("금액에 맞는 개수의 로또 구입")
     void 금액에_맞는_개수의_로또_구입(int userInput) {
         //given
-        Input input = mock(ConsoleInput.class);
-        Output output = mock(ConsoleOutput.class);
-        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
-
         when(input.getPrice()).thenReturn(userInput);
         doNothing().when(output).printLottoPriceRequest();
         when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());
@@ -45,10 +53,6 @@ class LottoManagerTest {
     @DisplayName("로또 구입금액이 정상적인 값일 때")
     void goodCase(int userInput) {
         //given
-        Input input = mock(ConsoleInput.class);
-        Output output = mock(ConsoleOutput.class);
-        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
-
         when(input.getPrice()).thenReturn(userInput);
         doNothing().when(output).printLottoPriceRequest();
         when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());
@@ -65,10 +69,6 @@ class LottoManagerTest {
     @DisplayName("로또 구입금액이 1000원 미만이면 예외")
     void 로또_구입금액이_1000원_미만이면_예외(int userInput) {
         //given
-        Input input = mock(ConsoleInput.class);
-        Output output = mock(ConsoleOutput.class);
-        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
-
         when(input.getPrice()).thenReturn(userInput, 1000);
         doNothing().when(output).printLottoPriceRequest();
         when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());
@@ -85,10 +85,6 @@ class LottoManagerTest {
     @DisplayName("로또 구입금액이 1000원 단위가 아니면 예외")
     void 로또_구입금액이_1000원_단위가_아니면_예외(int userInput) {
         //given
-        Input input = mock(ConsoleInput.class);
-        Output output = mock(ConsoleOutput.class);
-        LottoGenerator lottoGenerator = mock(LottoGenerator.class);
-
         when(input.getPrice()).thenReturn(userInput, 1000);
         doNothing().when(output).printLottoPriceRequest();
         when(lottoGenerator.generateLottos(anyInt())).thenReturn(new ArrayList<>());

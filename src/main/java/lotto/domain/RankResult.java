1a7d440a977ec9fa6bd162dbbc09f56ba9aafb67
KDT live <dnjsxo0616@gmail.com>
2023-11-08 18:00:56 +0900

    feat(RankResult): 각 순위별 결과를 업데이트하고 조회하는 기능 추가
    - countRankResult 메서드 추가.
    - 주어진 WinningNumberLotto와 Lottos에 대한 순위별 결과를 계산.
    
    - 순위별 결과를 업데이트하는 updateRankCount 메서드를 사용.
    - 각 순위별 결과를 조회하는 getRankCount 메서드 추가.

--- src/main/java/lotto/domain/RankResult.java
+++ src/main/java/lotto/domain/RankResult.java
@@ -13,4 +13,19 @@ public class RankResult {
         Arrays.stream(Rank.values())
                 .forEach(rank -> rankResult.put(rank, 0));
     }
+
+    public void countRankResult(WinningNumberLotto winningNumberLotto, Lottos lottos){
+        for(Lotto lotto : lottos.getLottos()){
+            Rank rank = Rank.rankResult(lotto.getMatchCount(winningNumberLotto), lotto.contains(winningNumberLotto.getBonusNumber()));
+            updateRankCount(rank);
+        }
+    }
+
+    private void updateRankCount(Rank rank){
+        rankResult.put(rank, rankResult.get(rank) + 1);
+    }
+
+    public int getRankCount(Rank rank){
+        return rankResult.get(rank);
+    }
 }

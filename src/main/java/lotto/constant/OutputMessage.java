fbc0dfdb8369c83527ac209ea1e42dcd02f927cf
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 16:19:05 +0900

    refactor: 생성자 추가 및 inputView와 outputView 연결

--- src/main/java/lotto/constant/OutputMessage.java
+++
@@ -1,4 +0,0 @@
-package lotto.constant;
-
-public class OutputMessage {
-}
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,21 +1,55 @@
 package lotto.controller;
 
+import lotto.constant.InputMessage;
 import lotto.model.Lotto;
+import lotto.model.RandomSixNumber;
 import lotto.model.User;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.List;
 
 public class LottoController {
-    public void StartGame(){
-        User user = new User();
-        int purchaseAmount = user.inputPurchaseAmount();
-        while (true){
-            List<Integer> correctNumber = user.inputLottoNumber();
+    private final InputView inputView;
+    private final User user;
+    private Lotto lotto;
+    private OutputView outputView;
+    public LottoController() {
+        this.inputView = new InputView();
+        this.user = new User();
+        this.lotto = null;
+        this.outputView = new OutputView();
+    }
+
+    public void startGame() {
+        int bonusNumber = 0;
+        int purchaseAmount = 0;
+        while (true) {
+            inputView.inputPurchaseAmountView();
             try {
-                Lotto lotto = new Lotto(correctNumber);
+                purchaseAmount = user.inputPurchaseAmount();
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+        while (true) {
+            inputView.inputLottoNumbersView();
+            try {
+                List<Integer> correctNumber = user.inputLottoNumber();
+                lotto = new Lotto(correctNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+        while (true) {
+            try {
+                inputView.inputBonusNumberView();
+                bonusNumber = user.inputBonusNumber(lotto.getNumbers());
                 break;
             }catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
+                outputView.printErrorMessage(e);
             }
         }
     }

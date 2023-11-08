11796e65fc12c46925ca0fb8a99c27fced42a731
Taehyoun Kim <kth9903@gmail.com>
2023-11-08 22:28:27 +0900

    refactor(prize): enhance prize enum with match logic and display formatting

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,37 +1,47 @@
 package lotto;
 
 public enum Prize {
-    FIRST(6,0,2_000_000_000),
-    SECOND(5,1,30_000_000),
-    THIRD(5,0,1_500_000),
-    FOURTH(4,0,50_000),
-    FIFTH(3,0,5_000),
-    NONE(0,0,0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
 
     private final int matchCount;
-    private final int bonusCount;
     private final int prizeMoney;
 
-    Prize(int matchCount, int bonusCount, int prizeMoney) {
+    Prize(int matchCount, int prizeMoney) {
         this.matchCount = matchCount;
-        this.bonusCount = bonusCount;
         this.prizeMoney = prizeMoney;
     }
 
-    public boolean matches(int matchCount, int bonusCount) {
-        return this.matchCount == matchCount && this.bonusCount == bonusCount;
+    public int getMatchCount() {
+        return matchCount;
     }
 
     public int getPrizeMoney() {
         return prizeMoney;
     }
 
-    public static Prize determinePrize(int matchCount, int bonusCount) {
-        for (Prize prize : values()) {
-            if (prize.matches(matchCount, bonusCount)) {
+    public static Prize determinePrize(int matchCount, boolean isBonus) {
+        if (matchCount == 5 && isBonus) {
+            return SECOND;
+        }
+        for (Prize prize : Prize.values()) {
+            if (prize.getMatchCount() == matchCount) {
                 return prize;
             }
         }
         return NONE;
+    }
+
+    public String getFormattedPrizeName() {
+        if (this == FIRST) return "6개 일치";
+        if (this == SECOND) return "5개 일치, 보너스 볼 일치";
+        if (this == THIRD) return "5개 일치";
+        if (this == FOURTH) return "4개 일치";
+        if (this == FIFTH) return "3개 일치";
+        return "";
     }
 }

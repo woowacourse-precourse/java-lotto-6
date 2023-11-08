fe8efa2df8f9e232a4fbe0a9fa6daa2b69bcdceb
ckwjdgur12 <jhcha@knu.ac.kr>
2023-11-08 20:02:46 +0900

    refactor(lotto): method 이름 변경

--- src/main/java/lotto/UserLotto.java
+++ src/main/java/lotto/UserLotto.java
@@ -9,11 +9,11 @@ public class UserLotto {
     public List<Integer> numbers;
 
     public UserLotto(List<Integer> numbers) {
-        sortNumber(numbers);
         this.numbers = numbers;
+        sortList(numbers);
     }
 
-    public void sortNumber(List<Integer> numbers){
+    public void sortList(List<Integer> numbers){
         for (int i=0; i<numbers.size(); i++){
             for (int j=0; j<numbers.size()-1-i; j++){
                 check(numbers, j);
@@ -26,7 +26,6 @@ public class UserLotto {
         int right = numbers.get(j+1);
 
         if (left > right){
-            int tmp = left;
             numbers.add(j, right);
             numbers.remove(j+1);
             numbers.add(j+1, left);

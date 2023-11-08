a6a9d780b986e749a11899cef95b1370f05d5f40
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 19:00:14 +0900

    Feat[util] 로또 번호 오름차순 정렬

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -2,10 +2,18 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoUtil {
     public static List<Integer> generateRandomNumber(int start, int end, int count) {
         return Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+
+    public static List<Integer> sortNumbersAscending(List<Integer> numbers) {
+        List<Integer> orderedList = new ArrayList<Integer>(numbers);
+        Collections.sort(orderedList);
+        return orderedList;
     }
 }

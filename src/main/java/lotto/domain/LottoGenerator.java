25e233cc30ec8d95a4ed8222148474a939c46736
saera <yook.saera@gmail.com>
2023-11-08 16:41:03 +0900

    feat(lotto): 로또를 구매 갯수만큼 발행하라

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,10 +1,19 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    public List<Integer> issueLotto() {
+    public List<List<Integer>> issueLottoAsManyAsPurchased(long purchaseQuantity) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        for (long i = 0; i < purchaseQuantity; i++) {
+            lottoNumbers.add(this.issueLotto());
+        }
+        return lottoNumbers;
+    }
+
+    private List<Integer> issueLotto() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -10,29 +10,41 @@ import org.junit.jupiter.api.Test;
 
 public class LottoGeneratorTest {
     private LottoGenerator lottoGenerator;
-    private List<Integer> actual;
+    private List<List<Integer>> lottoNumbers;
 
     @BeforeEach
     void setUp() {
         lottoGenerator = new LottoGenerator();
-        actual = lottoGenerator.issueLotto();
+        lottoNumbers = lottoGenerator.issueLottoAsManyAsPurchased(1000000);
     }
 
     @Test
-    void issueLotto_메서드가_1부터_45까지_범위_내에서_숫자를_뽑는다() {
-        for (Integer integer : actual) {
-            assertThat(integer).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(45);
+    void issueLottoAsManyAsPurchased_메서드가_issueLotto_메서드를_호출해_1부터_45까지_범위_내에서_숫자를_뽑는다() {
+        for (List<Integer> actual : lottoNumbers) {
+            for (Integer integer : actual) {
+                assertThat(integer).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(45);
+            }
         }
     }
 
     @Test
-    void issueLotto_메서드가_6개의_무작위값을_뽑는다() {
-        assertThat(actual.size()).isEqualTo(6);
+    void issueLottoAsManyAsPurchased_메서드가_issueLotto_메서드를_호출해_6개의_무작위값을_뽑는다() {
+        for (List<Integer> actual : lottoNumbers) {
+            assertThat(actual.size()).isEqualTo(6);
+        }
     }
 
     @Test
-    void issueLotto_메서드가_중복없이_숫자를_뽑는다() {
-        Set<Integer> numbersWithoutDuplicates = new HashSet<>(actual);
-        assertThat(actual.size()).isEqualTo(numbersWithoutDuplicates.size());
+    void issueLottoAsManyAsPurchased_메서드가_issueLotto_메서드를_호출해_중복없이_숫자를_뽑는다() {
+        for (List<Integer> actual : lottoNumbers) {
+            Set<Integer> numbersWithoutDuplicates = new HashSet<>(actual);
+            assertThat(actual.size()).isEqualTo(numbersWithoutDuplicates.size());
+        }
+    }
+
+    @Test
+    void issueLottoAsManyAsPurchased_메서드로_구매_갯수만큼_로또_발행() {
+        List<List<Integer>> actual = lottoNumbers;
+        assertThat(actual.size()).isEqualTo(1000000);
     }
 }

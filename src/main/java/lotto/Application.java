36596c39002fd34c11290435cf330ba620689c52
Dcool <90443691+Leesiwon12171260@users.noreply.github.com>
2023-11-08 19:52:46 +0900

    모듈화 하여 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,11 +7,30 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구매금액을 입력해 주세요.");
-        int buyMoney = Integer.parseInt(Console.readLine());
-        int count = buyMoney / 1000;
-        System.out.println(count + "개를 구매했습니다.");
+        try {
+            System.out.println("구매금액을 입력해 주세요.");
+            int buyMoney = Integer.parseInt(Console.readLine().trim());
+            if (buyMoney <= 0 || buyMoney % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위의 양수여야 합니다.");
+            }
+            int count = buyMoney / 1000;
+            System.out.println(count + "개를 구매했습니다.");
 
+            List<Lotto> lottos = generateLottos(count);
+            Set<Integer> winningNumberSet = promptWinningNumbers();
+            int bonusNumber = promptBonusNumber();
+
+            winStatistic(lottos, winningNumberSet, bonusNumber, buyMoney);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자를 입력해야 합니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            Console.close();
+        }
+    }
+
+    private static List<Lotto> generateLottos(int count) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -19,20 +38,38 @@ public class Application {
             lottos.add(lotto);
             System.out.println(lotto);
         }
-        //당첨번호 입력
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String[] selectNumbers = Console.readLine().split(",");
-        Set<Integer> winningNumberSet = new TreeSet<>();
-        for (String selectNumber : selectNumbers) {
-            winningNumberSet.add(Integer.parseInt(selectNumber.trim()));
-        }
-        //보너스 번호
-        System.out.println("보너스 번호를 입력해 주세요.");
-        int bonusNumber = Integer.parseInt(Console.readLine());
-
-        winStatistic(lottos, winningNumberSet, bonusNumber, buyMoney);
-        Console.close();
+        return lottos;
     }
+
+    private static Set<Integer> promptWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Set<Integer> winningNumberSet = new TreeSet<>();
+        String[] selectNumbers = Console.readLine().split(",");
+        if (selectNumbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        for (String selectNumber : selectNumbers) {
+            int number = Integer.parseInt(selectNumber.trim());
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1과 45 사이의 숫자여야 합니다.");
+            }
+            winningNumberSet.add(number);
+        }
+        if (winningNumberSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+        return winningNumberSet;
+    }
+
+    private static int promptBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine().trim());
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1과 45 사이의 숫자여야 합니다.");
+        }
+        return bonusNumber;
+    }
+
     static void winStatistic(List<Lotto> lottos, Set<Integer> winningNumberSet, int bonusNumber, int buyMoney) {
         Map<LottoRank, Integer> prizeCount = initializePrizeCount();
         long totalPrize = calculatePrizes(lottos, winningNumberSet, bonusNumber, prizeCount);

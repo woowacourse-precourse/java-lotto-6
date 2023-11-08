4878a8421ccd129e5b8c273d8003b89e9b846c3b
lg960214@naver.com <lg960214@naver.com>
2023-11-08 20:00:36 +0900

    feat : input 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String cashStr = Console.readLine();
+        int cash = Integer.parseInt(cashStr);
+
+
     }
 }

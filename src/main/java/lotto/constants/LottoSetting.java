d750bd15dcf9b66b4f56d22ef2a3009b2a676331
minsu20 <syt07100@gmail.com>
2023-11-08 20:41:53 +0900

    feat: 로또 최대 구입 금액을 enum로 추가

--- src/main/java/lotto/constants/LottoSetting.java
+++ src/main/java/lotto/constants/LottoSetting.java
@@ -5,7 +5,8 @@ public enum LottoSetting {
     MIN_LOTTO_NUMBER(1),
     MAX_LOTTO_NUMBER(45),
     LOTTO_SIZE(6),
-    PRICE_PER_TICKET(1000);
+    PRICE_PER_TICKET(1000),
+    PRICE_MAX_LIMIT(2147483000);
 
     private final int value;
 

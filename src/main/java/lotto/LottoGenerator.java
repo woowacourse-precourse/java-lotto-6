1abf2f193ff13cf00ab97c2df8228bc98596a002
mini1115 <kmini9611@naver.com>
2023-11-08 15:55:59 +0900

    feat: 금액만큼 로또 생성하는 기능 클래스 추가

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    PlayerInput input = new PlayerInput();
+    private final int time = input.inputMoney() / 1000;
+
+    public int getTime(){
+        return this.time;
+    }
+    public ArrayList<List<Integer>> lottoGenerator() {
+        ArrayList<List<Integer>> randoms = new ArrayList<>();
+        NumberGenerator generator = new NumberGenerator();
+        for (int i = 0; i < time; i++) {
+            List<Integer> randomNumber = generator.makeRandomNumber();
+            Lotto lotto = new Lotto(randomNumber);
+            randoms = lotto.makeRandoms(randomNumber);
+        }
+        return randoms;
+    }
+}
+

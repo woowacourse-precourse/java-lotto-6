dd05ad80d0cb9b64552efe1df15d5c9e075e248f
plan11plan <tjtfy7028@gmail.com>
2023-11-08 23:31:13 +0900

    style: 줄바꿈 및 static 상수화

--- src/main/java/lotto/winningLotto/service/LottoNumberService.java
+++ src/main/java/lotto/winningLotto/service/LottoNumberService.java
@@ -6,15 +6,18 @@ import lotto.StringCalculator;
 import lotto.winningLotto.domain.LottoNumber;
 
 public class LottoNumberService {
+    private static final String ERROR_TAG = "[ERROR]";
+    private static final String REENTER_WINNING_NUM_MSG = "당첨 번호를 다시 입력해 주세요.";
+    private static final String SPACE_BAR = " ";
 
     public LottoNumber createLottoNumber() {
         while (true) {
             try {
                 return new LottoNumber(StringCalculator.withOutSpaceBarAndSplitAndPositives(readLine()));
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 당첨 번호를 다시 입력해 주세요.");
+                System.out.println(ERROR_TAG + SPACE_BAR + REENTER_WINNING_NUM_MSG);
             } catch (NullPointerException e) {
-                System.out.println("[ERROR] 당첨 번호를 다시 입력해 주세요.");
+                System.out.println(ERROR_TAG + SPACE_BAR + REENTER_WINNING_NUM_MSG);
             }
         }
     }

1724f7c5b59dec714ab9d98f1902661918e23d83
DryLeonhard <ygangmin@gmail.com>
2023-11-08 21:34:07 +0900

    Feat : Lotto 클래스의 숫자 체크, 중복 체크, 포함여부, 일치 체크, toString 메서드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import lotto.result.Result;
+
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +21,55 @@ public class Lotto {
         }
     }
 
+    public boolean checkNumber(){
+        try{
+            validate(numbers);
+            return true;
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 숫자는 6개를 입력해주세요.");
+        }
+    }
+
+    public boolean checkOverlap(){
+        Set<Integer> check = new HashSet<>();
+        for(int num : numbers){
+            if(check.contains(num)) throw new IllegalArgumentException("[ERROR] 중복된 숫자는 허용되지 않습니다!");
+            check.add(num);
+        }
+        return true;
+    }
+
+    public boolean isContains(int num){
+        return numbers.contains(num);
+    }
+
+    public Result checkPrize(Lotto prize, int bonus){
+        int numChk = 0;
+        for(int num : numbers){
+            if(prize.isContains(num)) numChk ++;
+        }
+        return new Result(numChk,numbers.contains(bonus));
+    }
+
+    private List<Integer> getNumbers(){
+        return numbers;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+
+        Iterator<Integer> iterator =  numbers.iterator();
+
+        while(iterator.hasNext()){
+            sb.append(iterator.next());
+            if(iterator.hasNext()) sb.append(", ");
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
     // TODO: 추가 기능 구현
 }

0cddea3c42357b8e1a5ce30144c9aa63e5c481e8
hajungIm <ihj_ewha@ewhain.net>
2023-11-08 09:49:02 +0900

    Feat(InputView): 구입 금액 입력 받는 기능 구현
    `inputBudget()` : 구입 금액 입력 받는 기능
    `validateBudgetInput` : 구입 금액 유효성 검사 기능
    `isPositiveNonZeroInteger` : 0이 아닌 양인 정수인지 판별하는 기능
    `isDivisible` : 0으로 나누어 떨어지는지 판별하는 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,40 @@
 package lotto.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.repository.LottoRepository;
 
+public class InputView {
+    private static final String INPUT_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    public static int inputBudget() {
+        while (true) {
+            try {
+                System.out.println("구매금액을 입력해 주세요.");
+                String input = Console.readLine();
+                validateBudgetInput(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(INPUT_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private static void validateBudgetInput(String input) {
+        if (!isPositiveNonZeroInteger(input) || !isDivisible(input, LottoRepository.LOTTO_PRICE)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isDivisible(String input, int divisor) {
+        int dividend = Integer.parseInt(input);
+        return dividend % divisor == 0;
+    }
+
+    private static boolean isSixNumberAndComma(String input) {
+        return input.matches("^(\\d+,){5}\\d+$");
+    }
 }

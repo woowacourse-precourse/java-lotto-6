3213206c03002d410ad05d1067a294f7f788eb42
whffu762 <whffu762@naver.com>
2023-11-08 21:20:46 +0900

    test : 당첨 연산 기능 테스트

--- src/test/java/lotto/TestLottoFunction.java
+++ src/test/java/lotto/TestLottoFunction.java
@@ -3,6 +3,8 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -32,6 +34,47 @@ public class TestLottoFunction {
         System.out.println("-----------------");
         System.out.println("test result : " + lotto.toString());
         System.out.println("-----------------");
+    }
 
+
+    @Test
+    public void TestCheckResult(){
+
+        Integer [] winningValue = {1, 2, 3, 4, 5, 6};
+        List<Integer> testWinningValues = Arrays.asList(winningValue);
+        int testBonusValue = 7;
+
+        Integer [] firstValue = {1, 2, 3, 4, 5, 6};
+        Integer [] secondValue = {1, 2, 3, 4, 5, 30};
+        Integer [] thirdValue = {1, 2, 3, 4, 7, 30};
+        Integer [] fourthValue = {1, 2, 3, 4, 30, 31};
+        Integer [] fifthValue = {1, 2, 3, 30, 31, 32};
+        Integer [] failValue = {1, 2, 29, 30, 31, 32};
+
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(Arrays.asList(firstValue)));
+        lottos.add(new Lotto(Arrays.asList(secondValue)));
+        lottos.add(new Lotto(Arrays.asList(thirdValue)));
+        lottos.add(new Lotto(Arrays.asList(fourthValue)));
+        lottos.add(new Lotto(Arrays.asList(fifthValue)));
+        lottos.add(new Lotto(Arrays.asList(failValue)));
+
+        assertThat(lottos.get(0).checkResult(testWinningValues, testBonusValue))
+                .isEqualTo(6);
+
+        assertThat(lottos.get(1).checkResult(testWinningValues, testBonusValue))
+                .isEqualTo(7);
+
+        assertThat(lottos.get(2).checkResult(testWinningValues, testBonusValue))
+                .isEqualTo(5);
+
+        assertThat(lottos.get(3).checkResult(testWinningValues, testBonusValue))
+                .isEqualTo(4);
+
+        assertThat(lottos.get(4).checkResult(testWinningValues, testBonusValue))
+                .isEqualTo(3);
+
+        assertThat(lottos.get(5).checkResult(testWinningValues, testBonusValue))
+                .isEqualTo(2);
     }
 }

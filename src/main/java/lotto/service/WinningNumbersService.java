b713d8378f83d6c564a9cff7a2caac9f33dd6f26
mingtorr <mingtorr@nave.com>
2023-11-08 19:31:15 +0900

    feat: 당첨 번호의 입력값의 유효성을 검토하는 기능 추가

--- src/main/java/lotto/service/WinningNumbersService.java
+++ src/main/java/lotto/service/WinningNumbersService.java
@@ -1,12 +1,19 @@
 package lotto.service;
 
+import lotto.constants.AppConstants;
+import lotto.constants.ErrorConstants;
 import lotto.domain.WinningNumbers;
+import lotto.utils.ListUtil;
+import lotto.utils.StringUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningNumbersService {
-
-    public WinningNumbers generateWinningNumbers(String WinningNum) {
-        validateWinningNumbers(WinningNum);
-        return new WinningNumbers(WinningNum);
+    public WinningNumbers generateWinningNumbers(String winningNum) {
+        validateWinningNumbers(winningNum);
+        return new WinningNumbers(winningNum);
     }
 
     public void setBonusNumber(WinningNumbers winningNumbers, String bunsNum) {
@@ -14,14 +21,47 @@ public class WinningNumbersService {
         winningNumbers.setBonusNumber(Integer.parseInt(bunsNum));
     }
 
-    private void validateWinningNumbers(String str) {
+    private void validateWinningNumbers(String winningNum) {
         // 당첨 번호 유효성 검사
+        StringUtil.checkNotNull(winningNum);
+        String[] arr = winningNum.split(AppConstants.INPUT_DELIMITER);
+        checkArrayLength(arr);
+        checkNumeric(arr);
 
+        List<Integer> numList = Arrays.stream(arr)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        checkWinningNumRange(numList);
+        checkWinningNumDuplicate(numList);
     }
 
-    private void validateBonusNumber(String str) {
+    private void validateBonusNumber(String bunsNum) {
         // 보너스 번호 유효성 검사
-
     }
 
+    public void checkNumeric(String[] arr) {
+        for (String str : arr) {
+            if (!StringUtil.isNumeric(str)) {
+                throw new IllegalArgumentException(ErrorConstants.INVALID_WINNING_NUMERIC.getData());
+            }
+        }
+    }
+
+    public void checkArrayLength(String[] arr) {
+        if (arr.length != AppConstants.LOTTO_LENGTH) {
+            throw new IllegalArgumentException(ErrorConstants.INVALID_WINNING_LENGTH.getData());
+        }
+    }
+
+    public void checkWinningNumRange(List<Integer> numList) {
+        if (!ListUtil.checkRange(numList)) {
+            throw new IllegalArgumentException(ErrorConstants.INVALID_WINNING_RANGE.getData());
+        }
+    }
+
+    public void checkWinningNumDuplicate(List<Integer> numList) {
+        if (!ListUtil.checkDuplicateValue(numList)) {
+            throw new IllegalArgumentException(ErrorConstants.INVALID_WINNING_DUPLICATE.getData());
+        }
+    }
 }

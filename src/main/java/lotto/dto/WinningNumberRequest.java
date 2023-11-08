6daea7d68d8d65d6a1f00de8ecb86e0261b04e0f
JeongHunHui <deca3248@gmail.com>
2023-11-08 20:50:47 +0900

    refactor: LottoNumbers에 대한 validate도 LottoValidator에서 수행, LottoValidator 이름 변경

--- src/main/java/lotto/dto/WinningNumberRequest.java
+++ src/main/java/lotto/dto/WinningNumberRequest.java
@@ -1,36 +1,11 @@
 package lotto.dto;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.constant.ErrorMessage;
-import lotto.constant.LottoConstant;
-import lotto.exception.InvalidWinningNumberException;
-import lotto.validator.LottoValidator;
+import lotto.validator.LottoNumberValidator;
 
 public record WinningNumberRequest(List<Integer> winningNumber) {
 
     public WinningNumberRequest {
-        if (isInValidWinningNumberSize(winningNumber.size())) {
-            throw new InvalidWinningNumberException(ErrorMessage.INVALID_WINNING_NUMBER_COUNT);
-        }
-
-        if (isWinningNumberContainsDuplicatedNumber(winningNumber)) {
-            throw new InvalidWinningNumberException(
-                ErrorMessage.WINNING_NUMBER_CONTAINS_DUPLICATED_NUMBER);
-        }
-
-        for (Integer number : winningNumber) {
-            LottoValidator.validateLottoNumberInRange(number);
-        }
-    }
-
-    private Boolean isInValidWinningNumberSize(Integer winningNumberSize) {
-        return winningNumberSize != LottoConstant.LOTTO_NUMBER_COUNT.getValue();
-    }
-
-    private Boolean isWinningNumberContainsDuplicatedNumber(List<Integer> winningNumber) {
-        Set<Integer> numberSet = new HashSet<>(winningNumber);
-        return numberSet.size() != winningNumber.size();
+        LottoNumberValidator.validateLottoNumbers(winningNumber);
     }
 }

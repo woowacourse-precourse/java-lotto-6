baf8e960f0723147f2f158d71bddeeea97cdb699
hunm719 <hunm719@naver.com>
2023-11-08 17:03:57 +0900

    당첨 결과를 집계하는 메서드 구현

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -2,5 +2,15 @@ package lotto;
 
 public class Result {
     // 각 로또의 당첨 여부를 저장하고 결과를 집계하는 역할
+    private int[] matchCounts;
 
+    public void add(int matchedCount, boolean hasBonusNumber) {
+        if (matchedCount == 6) {
+            matchCounts[5]++;
+        } else if (matchedCount == 5 && hasBonusNumber) {
+            matchCounts[4]++;
+        } else {
+            matchCounts[matchedCount - 1]++;
+        }
+    }
 }

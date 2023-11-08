d54cee93c1bbc5822c45e84f579af164ac7d9b6a
morenow <jjang9877@gmail.com>
2023-11-08 18:10:01 +0900

    docs: Money 입력 테스트 삭제 및 단위 테스트로 수정

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,47 +1,18 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
-    private OutputStream captor;
-
-    @BeforeEach
-    protected final void init() {
-        captor = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(captor));
-    }
-
-    protected final String output() {
-        return captor.toString().trim();
-    }
 
     @Test
-    @DisplayName("정수가 아닌 값 입력 시 예외처리 후 재입력")
-    void inputNotInteger() {
-        String[] inputs = {"5000j", "5000"};
-        System.setIn(new ByteArrayInputStream(String.join("\n", inputs).getBytes()));
-        Money money = Money.createMoney();
-        assertThat(output()).contains("[ERROR] 잘못된 금액을 입력했습니다.");
-        assertEquals(money.getMoney(), 5000);
-    }
-
-    @Test
-    @DisplayName("1000으로 나누어 떨어지지 않는 값 입력 시 예외처리 후 재입력")
-    void inputNot1000Multiples() {
-        String[] inputs = {"5001", "5000"};
-        System.setIn(new ByteArrayInputStream(String.join("\n", inputs).getBytes()));
-        Money money = Money.createMoney();
-        assertThat(output()).contains("[ERROR] 잘못된 금액을 입력했습니다.");
-        assertEquals(money.getMoney(), 5000);
+    @DisplayName("1000으로 나누어 떨어지지 않는 값으로 생성 불가능")
+    void createMoneyNot1000Multiples() {
+        assertThatThrownBy(() -> new Money(5001))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test

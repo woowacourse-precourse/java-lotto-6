d2ceb7727115b5edbf499d841d7c2694704edbdf
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 14:19:06 +0900

    refactor: remove useless line
    중복되어서 필요없는 부분 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-    private static final String ERROR_MESSAGE = "[ERROR]";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -13,10 +12,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
         }
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + " 로또 번호는 중복될 수 없습니다");
+            throw new IllegalArgumentException();
         }
     }
 

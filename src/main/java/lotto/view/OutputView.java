d635422f22f37e91837e1047d3f6ac9129f9f621
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-08 18:31:20 +0900

    feat: 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,31 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Result;
+import lotto.utils.MessageConstant;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class OutputView {
+
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println();
+        System.out.printf(MessageConstant.LOTTO_COUNT + "%n", lottos.size());
+        lottos.forEach(System.out::println);
+    }
+
+    public static void printResultMessage() {
+        System.out.println();
+        System.out.println(MessageConstant.RESULT);
+    }
+
+    public static void printCount(String message, int count) {
+        System.out.printf(message + MessageConstant.COUNT + "%n", count);
+    }
+
+    public static void printRateOfReturn(double rate) {
+        System.out.printf((MessageConstant.RATE_OF_RETURN) + "%n", rate);
+    }
+}

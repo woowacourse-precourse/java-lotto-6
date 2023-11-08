e9b88d2e231c6b239ce5fe539a6ac6878cf8f269
mdgome <mdgome3@gmail.com>
2023-11-08 13:36:37 +0900

    feat: 보너스 번호 예외 처리 기능 추가
    - 1부터 45 사이 값을 받았는지
    - 보너스 번호와 당첨 번호간 중복이 있는지
    - 입력 값이 없는지
    - 입력 값이 숫자가 아닌지

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -1,39 +1,72 @@
 package lotto.model;
 
+import lotto.constant.Number;
+import lotto.constant.ValidatorMessage;
+
 public class Bonus {
     private final Integer number;
 
     public Bonus(String bonusNumber, Lotto winLotto) {
+        int tempNumber = convertStringToInteger(bonusNumber);
 
+        validate(tempNumber, winLotto);
+
+        this.number = Integer.valueOf(tempNumber);
     }
 
     public Bonus(int number, Lotto winLotto) {
+        validate(number, winLotto);
 
-
+        this.number = Integer.valueOf(number);
     }
 
     public boolean matchBonus(Lotto lotto) {
 
     }
 
+    /**
+     *
+     * String -> int Type Cast
+     *
+     * Bonus Class 생성자는 String, Int 2개로 받으며
+     * String 생성자 생성 시 Integer Type Cast 진행
+     * Type Cast 시 숫자가 아닌 경우 Exception 발생
+     *
+     * @param bonusNumber
+     * @return int type
+     */
     private int convertStringToInteger(String bonusNumber) {
+        int bonus;
+        try {
+            bonus = Integer.parseInt(bonusNumber);
+        } catch(NumberFormatException exception) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_BONUS_NUMERIC);
+        }
 
+        return bonus;
     }
 
     public void validate(Integer number, Lotto winLotto) {
         validateIsNull(number);
         validateNumberRange(number);
-        validateDuplicatedWinLotto(number, winLotto);
+        validateContainsWinLotto(number, winLotto);
     }
 
     private void validateIsNull(Integer number) {
-
+        if (number == null) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_IS_NULL);
+        }
     }
 
     private void validateNumberRange(int number) {
+        if (!(number >= Number.MIN_NUMBER && number <= Number.MAX_NUMBER)) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_BONUS_RANGE);
+        }
     }
 
-    private void validateDuplicatedWinLotto(Integer number, Lotto lotto) {
-
+    private void validateContainsWinLotto(Integer number, Lotto lotto) {
+        if (lotto.contains(number)) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_BONUS_DUPLICATE_WIN_LOTTO);
+        }
     }
 }

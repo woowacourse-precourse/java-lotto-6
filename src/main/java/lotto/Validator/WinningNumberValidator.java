6d1fbf89998db57187cbb0f3250501a93d27ba02
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 11:51:25 +0900

    [feat] Validator - 당첨 번호 입력 개수가 6개로 올바른지 검증

--- src/main/java/lotto/Validator/WinningNumberValidator.java
+++ src/main/java/lotto/Validator/WinningNumberValidator.java
@@ -8,11 +8,14 @@ public class WinningNumberValidator {
     private static final String COMMA_SEPARATOR = ",";
     private static final int MINIMUM_VALUE = 1;
     private static final int MAXIMUM_VALUE = 45;
+    private static final int BLANK_NUMBER = 0;
+    private static final int LIMITED_NUMBER = 7;
 
     public static String validateWinningNumber(String input) {
         String[] numbers = input.split(COMMA_SEPARATOR);
 
         validateNumberRange(numbers);
+        validateNumberOfNumbers(numbers);
 
         return input;
     }
@@ -23,6 +26,15 @@ public class WinningNumberValidator {
             if (number < MINIMUM_VALUE || number > MAXIMUM_VALUE) {
                 throw new IllegalArgumentException(WinningNumberErrorMessage.INVALID_WINNING_NUMBER_RANGE.getMessage());
             }
+        }
+    }
+
+    private static void validateNumberOfNumbers(String[] numbers) {
+        if (numbers.length == BLANK_NUMBER) {
+            throw new IllegalArgumentException(WinningNumberErrorMessage.NO_WINNING_NUMBER_ENTERED.getMessage());
+        }
+        if (numbers.length > LIMITED_NUMBER) {
+            throw new IllegalArgumentException(WinningNumberErrorMessage.TOO_MANY_WINNING_NUMBERS.getMessage());
         }
     }
 

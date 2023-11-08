4c76e250db4f985423b181dad513dee05896949e
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 13:41:40 +0900

    feat: 로또번호 입력 받기 기능 추가

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -2,6 +2,9 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class User {
 //    사용자는 구입 금액과 당첨 번호, 보너스 번호를 각각 입력해야 한다.
     public int inputPurchaseAmount() {
@@ -17,6 +20,19 @@ public class User {
         }
         return inputPurchaseAmount();
     }
+
+    public List<Integer> inputLottoNumber() {
+        try {
+            List<Integer> numbers = List.of(Console.readLine().split(",")).stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            return numbers;
+        }catch (NumberFormatException e){
+            System.out.println("[ERROR] 숫자로만 이루어진 6자리를 입력하세요");
+        }
+        return inputLottoNumber();
+    }
+
     private void validatePurchaseAmount(int money){
         if(money<1000||money>100000){// 1000원 미만, 100000원 초과의 값 입력시의 예외 처리
             throw new IllegalArgumentException("[ERROR] 1000원 이상 100000원 이하의 값을 입력해주세요");

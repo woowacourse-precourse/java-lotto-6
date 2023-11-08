fe2f7107fb7821e7092234f7a30d559a03936abe
jhva <junghun8158@naver.com>
2023-11-08 14:15:45 +0900

    :recycle: refactor : 로또 상수 메시지 추가 및 메서드 포맷터 리팩토링

--- src/main/java/lotto/constants/LottoCount.java
+++ src/main/java/lotto/constants/LottoCount.java
@@ -1,38 +1,61 @@
 package lotto.constants;
 
+import static lotto.constants.LottoConstants.COMMA;
+import static lotto.constants.LottoConstants.PATTERN;
+import static lotto.constants.LottoConstants.REPLACE_BLANK;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum LottoCount {
-    LOTTO_COUNT_START(3, "5,000원"),
+    LOTTO_COUNT_START(3, "3개 일치 (5,000원)"),
+    LOTTO_COUNT_START_FOUR(4, "4개 일치 (50,000원)"),
+    LOTTO_COUNT_FIVE(5, "5개 일치 (1,500,000원)"),
+    LOTTO_COUNT_BONUS(7, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    LOTTO_COUNT_SIX(6, "6개 일치 (2,000,000,000원)");
 
-    LOTTO_COUNT_START_FOUR(4, "50,000원"),
-
-    LOTTO_COUNT_FIVE(5, "1,500,000원"),
-
-    LOTTO_COUNT_BONUS(6, "30,000,000원"),
-
-    LOTTO_COUNT_SIX(7, "2,000,000,000원");
-
+    private String priceMsg;
+    private int bonusCount;
     private int count;
-    private String price;
+    private int matchesCount;
+    private int multiple;
 
-    LottoCount(int count, String price) {
+    LottoCount(int count, String priceMsg) {
+        this.priceMsg = priceMsg;
         this.count = count;
-        this.price = price;
+        this.bonusCount = 0;
+        this.matchesCount = 0;
+        this.multiple = 0;
     }
 
-    public static String matchingPrice(int count) {
-        String result = LOTTO_COUNT_START.price;
-        if (count == LOTTO_COUNT_START_FOUR.count) {
-            result = LOTTO_COUNT_START_FOUR.price;
+    public String getPriceMsg() {
+        return this.priceMsg;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+
+    public int getMatChesCount() {
+        return this.matchesCount;
+    }
+
+    public int getMultiple() {
+        return this.multiple;
+    }
+
+    public void incrementMatchesCount() {
+        matchesCount++;
+    }
+
+    public void calculateTotalPrizeAmount() {
+        Pattern pattern = Pattern.compile(PATTERN);
+        Matcher matcher = pattern.matcher(priceMsg);
+        if (matcher.find()) {
+            String matcherGroup = matcher.group(1);
+            String numberFormatter = matcherGroup.replace(COMMA, REPLACE_BLANK);
+            multiple += matchesCount * Integer.parseInt(numberFormatter);
         }
-        if (count == LOTTO_COUNT_FIVE.count) {
-            result = LOTTO_COUNT_FIVE.price;
-        }
-        if (count == LOTTO_COUNT_BONUS.count) {
-            result = LOTTO_COUNT_BONUS.price;
-        }
-        if (count == LOTTO_COUNT_SIX.count) {
-            result = LOTTO_COUNT_SIX.price;
-        }
-        return result;
     }
 }
--- src/main/java/lotto/constants/LottoMsg.java
+++ src/main/java/lotto/constants/LottoMsg.java
@@ -4,12 +4,9 @@ public enum LottoMsg {
     LOTTO_BUY_PRICE("구입금액을 입력해 주세요."),
     LOTTO_COMMON_MSG("%s 번호를 입력해 주세요."),
     LOTTO_RESULT("당첨 통계"),
-    LOTTO_LATE("총 수익률은 %s입니다."),
+    LOTTO_LATE("총 수익률은 %.1f%%입니다."),
     LOTTO_PRICE_COUNT("%d개를 구매했습니다."),
-    LOTTO_MATCH_FORMAT("%d개 일치 (%s) - %d개"),
-
-    LOTTO_MATCH_FORMAT_BONUS("%d개 일치, 보너스 볼 일치 (%s) - %d개");
-
+    LOTTO_MATCH_FORMAT("%s - %d개");
     private String msg;
 
     LottoMsg(String msg) {

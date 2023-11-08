d66a992ed192ed5daf933004219e0a3924b49374
ghsyn <swanshawn715@gmail.com>
2023-11-08 18:19:02 +0900

    Feat: moved to validate exception handling input in lotto

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -6,31 +6,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class Input {
-    public static int getPrice() {
+    public static int inputPrice(int lottoPrice) {
         int purchase = 0;
         boolean validInput = false;
 
         while (!validInput) {
-            System.out.println("구입금액을 입력해 주세요.");
             try {
                 purchase = Integer.parseInt(Console.readLine());
+                if (purchase % 1000 != 0) {
+                    throw new IllegalArgumentException("구입 금액은 " + lottoPrice + "으로 나누어 떨어져야 합니다.");
+                }
+
                 validInput = true;
             } catch (NumberFormatException e) {
                 new IllegalArgumentExceptionHandler("구입 금액은 정수만 가능합니다.");
+            } catch (IllegalArgumentException e) {
+                new IllegalArgumentExceptionHandler(e.getMessage());
             }
         }
 
         return purchase;
     }
 
-    public static List<Integer> getLotteryNums() {
+    public static List<Integer> inputLotteryNums() {
         List<Integer> lotteryList = new ArrayList<>();
         boolean validInput = false;
 
         while (!validInput) {
             try {
                 String lotteryInput = Console.readLine();
+                if (lotteryInput.contains(" ")) {
+                    lotteryInput = lotteryInput.replaceAll(" ", "");
+                }
                 List<String> lotteryString = List.of(lotteryInput.split(","));
                 lotteryString = lotteryString.stream().distinct().collect(Collectors.toList());
 
@@ -39,11 +48,47 @@ public class Input {
                     lotteryList.add(Integer.parseInt(number));
                 }
 
-                validInput = Lotto.validate(lotteryList);
+                if (lotteryList.size() != 6) {
+                    throw new IllegalArgumentException("로또 번호는 중복되지 않는 6자리여야 합니다.");
+                }
+
+                for (int number : lotteryList) {
+                    if (number > 45 || number < 1) {
+                        throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이여야 합니다.");
+                    }
+                }
+
+                validInput = true;
             } catch (NumberFormatException e) {
                 new IllegalArgumentExceptionHandler("로또 번호는 정수여야 합니다.");
+            } catch (IllegalArgumentException e) {
+                new IllegalArgumentExceptionHandler(e.getMessage());
             }
         }
         return lotteryList;
+    }
+
+    public static int inputBonusNum(List<Integer> lotteryNums) {
+        int bonusInt = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                bonusInt = Integer.parseInt(Console.readLine());
+
+                if (lotteryNums.contains(bonusInt)) {
+                    throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복되지 않아야 합니다.");
+                }
+
+                if (bonusInt > 45 || bonusInt < 1) {
+                    throw new IllegalArgumentException("보너스 번호는 1 ~ 45 사이여야 합니다.");
+                }
+                validInput = true;
+            } catch (NumberFormatException e) {
+                new IllegalArgumentExceptionHandler("보너스 번호는 정수여야 합니다.");
+            } catch (IllegalArgumentException e) {
+                new IllegalArgumentExceptionHandler(e.getMessage());
+            }
+        }
+        return bonusInt;
     }
 }

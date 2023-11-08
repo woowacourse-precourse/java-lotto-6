11a0778075d1b024e4a6b0f5a7d5364fbf35c7ed
wonjun <wonjon0930@daum.net>
2023-11-08 19:14:49 +0900

    test: add test code for WinningLottoNumbersInputValidator

--- src/test/java/lotto/validator/UserMoneyInputValidatorTest.java
+++ src/test/java/lotto/validator/UserMoneyInputValidatorTest.java
@@ -8,21 +8,21 @@ import org.junit.jupiter.api.Test;
 
 public class UserMoneyInputValidatorTest {
 
-    @DisplayName("구입 금액이 비어있는 입력값일 때 예외 발생")
+    @DisplayName("구입 금액이 비어있는 입력값일 때 예외가 발생한다.")
     @Test
     void insertUserMoneyWithBlankValue() {
         assertThatThrownBy(() -> new UserMoneyInputValidator().validate(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구입 금액이 숫자형식이 아닌 입력값일 때 예외 발생")
+    @DisplayName("구입 금액이 숫자형식이 아닌 입력값일 때 예외가 발생한다.")
     @Test
     void insertUserMoneyWithNonNumericValue() {
         assertThatThrownBy(() -> new UserMoneyInputValidator().validate("asd"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구입 금액이 로또 금액(1000)의 배수가 아닌 입력값일 때 예외 발생")
+    @DisplayName("구입 금액이 로또 금액(1000)의 배수가 아닌 입력값일 때 예외가 발생한다.")
     @Test
     void insertUserMoneyWithNotDivisibleWithLottoPrice() {
         assertThatThrownBy(() -> new UserMoneyInputValidator().validate("1111"))

a3b020e48f8fecef907cbf606c6318449903dd7c
Jin409 <sualng123@gmail.com>
2023-11-08 15:11:49 +0900

    [TEST] 당첨 번호 입력 시 파싱이 잘 이루어지는지 테스트

--- src/test/java/lotto/ParserTest.java
+++ src/test/java/lotto/ParserTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import lotto.util.Parser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +16,47 @@ public class ParserTest {
         String invalidInput = "abc";
 
         // when & then
-        assertThatThrownBy(() -> Parser.stringToInt(invalidInput))
+        assertThatThrownBy(() -> Parser.stringToInteger(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구분자를 기준으로 입력값을 분리하고 정수형 리스트로 반환한다.")
+    void parsingStringToIntegerListWithValidInput() {
+        // given
+        String validInput = "1,2,3,4,5";
+        String separator = ",";
+        List<Integer> expected = List.of(1, 2, 3, 4, 5);
+
+        // when & then
+        List<Integer> result = Parser.stringToIntegerListWithSplittingBySeparator(validInput, separator);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("공백이 포함된 입력값에 대해서 입력값을 분리하고 정수형 리스트로 반환한다.")
+    void parsingStringToIntegerListWithSpaceInput() {
+        // given
+        String validInput = " 1, 2, 3 , 4 , 5 ";
+        String separator = ",";
+        List<Integer> expected = List.of(1, 2, 3, 4, 5);
+
+        // when
+        List<Integer> result = Parser.stringToIntegerListWithSplittingBySeparator(validInput, separator);
+
+        // then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 여러개의 입력값에 대해서 ")
+    void parsingStringToIntegerListWithNotIntegerInput() {
+        // given
+        String notIntegerInput = "a,b,c,d";
+        String separator = ",";
+
+        // when & then
+        assertThatThrownBy(() -> Parser.stringToIntegerListWithSplittingBySeparator(notIntegerInput, separator))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

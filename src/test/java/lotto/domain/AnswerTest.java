ec6320c902b8205f6cea5324b37ed362351471e1
freekyr7529 <freekyr7529@gmail.com>
2023-11-08 17:19:12 +0900

    test: 정답 객체에 대한 테스트 케이스 작성 및 통과
    - 생성자의 매개변수로 주어진 보너스 번호가 1부터 45 사이의 숫자가 아닌 경우에 대한 테스트 케이스
    - 생성자의 매개변수로 주어진 보너스 번호가 당첨 번호와 중복될 경우에 대한 테스트 케이스
    - 매개변수로 주어진 번호가 당첨 번호 내부에 존재하는지 확인한 후 결과를 반환하는 메서드에 대한 테스트 케이스
    - 매개변수로 주어진 번호가 보너스 번호와 일치하는지 확인한 후 결과를 반환하는 메서드에 대한 테스트 케이스

---
+++ src/test/java/lotto/domain/AnswerTest.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.enums.ErrorMessages;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AnswerTest {
+    private static final Lotto mockHitNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+    @DisplayName("보너스 번호에 1부터 45사이의 숫자를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void temp() {
+        assertThatThrownBy(() -> new Answer(mockHitNumbers, 50))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.NUMBER_RANGE_EXCEPTION_MSG.getMsg());
+    }
+
+    @DisplayName("당첨 번호에 기입한 숫자를 보너스 번호에 입력하면 예외가 발생한다.")
+    @Test
+    void temp2() {
+        assertThatThrownBy(() -> new Answer(mockHitNumbers, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.BONUS_NUMER_CONFLICT_EXCEPTION_MSG.getMsg());
+    }
+
+    @DisplayName("정답 객체는 당첨 번호 내에 해당 번호가 존재하는지 확인한 후 결과를 반환한다.")
+    @Test
+    void temp3() {
+        Answer answer = new Answer(mockHitNumbers, 7);
+        assertThat(answer.isHitNumbersHaveThisNumber(3)).isEqualTo(true);
+        assertThat(answer.isHitNumbersHaveThisNumber(20)).isEqualTo(false);
+    }
+
+    @DisplayName("정답 객체는 보너스 번호가 해당 번호와 같은지 확인한 후 결과를 반환한다.")
+    @Test
+    void temp4() {
+        Answer answer = new Answer(mockHitNumbers, 7);
+        assertThat(answer.isBonusNumberTheSameAsThis(7)).isEqualTo(true);
+        assertThat(answer.isBonusNumberTheSameAsThis(5)).isEqualTo(false);
+    }
+}

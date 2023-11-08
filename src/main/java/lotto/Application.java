e397d866e61d52a0ef31e42d97aa1b65bc546bb3
deohyunpark <qwertyshsh@naver.com>
2023-11-08 19:40:53 +0900

    feat: complete LottoGame

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(new LottoInputView(), new LottoOutputView());
+
+        lottoController.initLottoGame();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,56 +1,67 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.function.Supplier;
+import java.util.List;
 import lotto.domain.ComputeLottoGenerator;
 import lotto.domain.Lotto;
 import lotto.domain.LottoBonusNumber;
 import lotto.domain.LottoFactory;
 import lotto.domain.Price;
+import lotto.domain.PrizeStatisticsCalculator;
 import lotto.domain.Quantity;
+import lotto.domain.ReturnOnInvestmentCalculator;
+import lotto.view.InputValidator;
 import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
 
 public class LottoController {
 
-    public static final String PREFIX = "[ERROR]" + " ";
+
     private final LottoInputView lottoInputView;
 
+    private final LottoOutputView lottoOutputView;
 
-    public LottoController(LottoInputView lottoInputView) {
+    public LottoController(LottoInputView lottoInputView, LottoOutputView lottoOutputView) {
         this.lottoInputView = lottoInputView;
+        this.lottoOutputView = lottoOutputView;
     }
 
     // 가격을 입력받아 복권 갯수만큼 복권 출력
-    public void executePayment() {
-        lottoInputView.priceInfoLog();
-        Price price = initValidInputReadLine(() -> new Price(Console.readLine()));
-
-        Quantity quantity = initValidInputReadLine(() -> new Quantity(price));
-        lottoInputView.purchaseInfoLog(quantity.getQuantity());
-        new ComputeLottoGenerator(quantity).generatorLottoList()
-                .forEach(lotto -> System.out.println(lotto.getNumbers()));
+    public List<Lotto> executePayment() {
+        final Price price = InputValidator.getValidInput(lottoInputView::priceInfoLog,
+                () -> new Price(Console.readLine()));
+        final Quantity quantity = InputValidator.getValidInput(() -> new Quantity(price));
+        lottoOutputView.purchaseInfoLog(quantity.getQuantity());
+        List<Lotto> generatorLottoList = new ComputeLottoGenerator(quantity).generatorLottoList();
+        generatorLottoList.forEach(generatorLotto -> System.out.println(generatorLotto.getNumbers()));
+        return generatorLottoList;
     }
 
     // 로또번호 + 보너스 번호 입력받기
     public Lotto enterLottoNumber() {
-        lottoInputView.initLottoNumberLog();
-        Lotto lotto = initValidInputReadLine(() -> LottoFactory.createLottoFrom(Console.readLine()));
-
-        lottoInputView.initLottoBonusNumberLog();
-        LottoBonusNumber lottoBonusNumber = initValidInputReadLine(
+        Lotto lotto = InputValidator.getValidInput(lottoInputView::initLottoNumberLog,
+                () -> LottoFactory.createLottoFrom(Console.readLine()));
+        LottoBonusNumber lottoBonusNumber = InputValidator.getValidInput(lottoInputView::initLottoBonusNumberLog,
                 () -> LottoBonusNumber.validateLottoNumDuplicate(lotto, new LottoBonusNumber(Console.readLine())));
         lotto.getNumbers().add(lottoBonusNumber.getBonusNumber());
-
         return lotto;
     }
 
-    private <T> T initValidInputReadLine(final Supplier<T> readLine) {
-        try {
-            return readLine.get();
-        } catch (IllegalArgumentException e) {
-            System.out.println(PREFIX + e.getMessage());
-            return initValidInputReadLine(readLine);
+    public void initLottoGame() {
+        List<Lotto> lottoList = executePayment();
+        Lotto winningLotto = enterLottoNumber();
+        long totalProfit = 0;
+        int investment = lottoList.size() * 1000;
+        lottoOutputView.showStatisticLog();
+
+        for (PrizeStatisticsCalculator prize : PrizeStatisticsCalculator.values()) {
+            prize.printMatchCount(lottoList, winningLotto);
+            long prizeMoney = ReturnOnInvestmentCalculator.calculateTotalProfit(prize, lottoList, winningLotto);
+            totalProfit += prizeMoney;
         }
+
+        double profitRate = ReturnOnInvestmentCalculator.calculateProfitRate(totalProfit, investment);
+        lottoOutputView.showResultLog(profitRate);
     }
 
 

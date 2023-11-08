da6e5c0dc7a0c78d6809d602b5f5c7d0ea4a3f26
takoyakimchi <c4nyou@gmail.com>
2023-11-08 18:33:57 +0900

    refactor(bonus): 보너스 숫자 하나만 받는 걸로 변경, 검증 로직 추가

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,21 +1,30 @@
 package lotto.domain;
 
-import static lotto.constant.GameNumber.BONUS_NUMBER_COUNT;
-
-import java.util.List;
+import static lotto.constant.ErrorMessage.ONLY_NUMBERS_ALLOWED;
+import static lotto.constant.ErrorMessage.WRONG_NUMBER_RANGE;
+import static lotto.constant.GameNumber.MAX_NUMBER;
+import static lotto.constant.GameNumber.MIN_NUMBER;
 
 public class Bonus {
 
-    private final List<Integer> numbers;
+    private final int number;
 
-    public Bonus(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Bonus(String input) {
+        this.number = validateNumeral(input);
+        validateNumber();
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != BONUS_NUMBER_COUNT.getNumber()) {
-            throw new IllegalArgumentException();
+    private int validateNumeral(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ONLY_NUMBERS_ALLOWED.getMessage());
+        }
+    }
+
+    private void validateNumber() {
+        if (number < MIN_NUMBER.getNumber() || number > MAX_NUMBER.getNumber()) {
+            throw new IllegalArgumentException(WRONG_NUMBER_RANGE.getMessage());
         }
     }
 }

dbdc7390bea5c7079cbb25c46e2ddc83244e8a6d
yeop0740 <ssucgoo@gmail.com>
2023-11-08 14:23:54 +0900

    feat: add method to get price from user

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int price = getPrice();
     }
+
+    public static int getPrice() {
+        OutputConsole.printMessage(OutputConsole.INPUT_PRICE_MESSAGE);
+        return InputConsole.inputPrice();
+    }
+
 }

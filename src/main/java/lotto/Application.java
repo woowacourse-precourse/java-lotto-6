64570e1eba70e6fcd7a27821acfcb0fff5a8fe6c
sangjun <sangjun1389@ajou.ac.kr>
2023-11-08 20:17:01 +0900

    feat(Console): 프로그램 종료 이전에 Scanner 리소스 해제
    - Console.close() 메소드 호출을 통한 리소스 해제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Scanner;
 import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
         lottoController.run();
+        Console.close();
     }
 }

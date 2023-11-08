91ef53cedbd0af0f9bce2a0bb6ee5e1d554f590f
GyungA <GyungA@users.noreply.github.com>
2023-11-08 20:39:29 +0900

    feat: 로또 구입 안내 메세지 출력

--- src/main/java/lotto/View/LottoBuyer.java
+++ src/main/java/lotto/View/LottoBuyer.java
@@ -11,9 +11,11 @@ import java.util.List;
 public class LottoBuyer {
     public static Integer LOTTO_COUNT;
     public static final Integer LOTTO_PRICE = 1_000;
+    private static final String LOTTO_BUY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_PRICE_ERROR_MESSAGE = "입력한 금액은 로또 한 장 가격(1,000원)의 배수여야 합니다.";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
     public static Integer inputMoney() {
+        System.out.println(LOTTO_BUY_MESSAGE);
         Integer money = Integer.valueOf(Console.readLine());
         validateLottoPrice(money);
         return money;

38bc8f134c828e6085381e48e69a9df0e0d4b9c6
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-08 20:40:37 +0900

    test: BonusNumberTest 수정, 추가
    BonusNumberTest 수정, 추가
    - 기존 테스트 설명, 메서드 명 수정
    - equalsTest 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import static lotto.error.ErrorMessage.BONUS_NUMBER_OUT_OF_RANGE;
 import static lotto.error.ErrorMessage.DUPLICATE_BONUS_NUMBER;
 import static lotto.error.ErrorMessage.INVALID_BONUS_NUMBER_FORMAT;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +15,9 @@ class BonusNumberTest {
 
     private final WinningNumbers ANSWER = WinningNumbers.of("1,2,3,4,5,6");
 
-    @DisplayName("보너스 숫자 검증이 가능하다.")
+    @DisplayName("보너스 숫자 생성 시 검증이 가능하다.")
     @Test
-    void validateBonusNumberTest() {
+    void fromTest() {
         assertAll(
                 () -> assertThatThrownBy(() -> BonusNumber.from("love", ANSWER))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -30,6 +30,16 @@ class BonusNumberTest {
                         .hasMessageContaining(DUPLICATE_BONUS_NUMBER),
                 () -> assertThatCode(() -> BonusNumber.from("7", ANSWER))
                         .doesNotThrowAnyException()
+        );
+    }
+
+    @DisplayName("특정 숫자가 주어지면 보너스 숫자와 동일한지 확인할 수 있다.")
+    @Test
+    void equalsTest() {
+        BonusNumber bonusNumber = BonusNumber.from("7", ANSWER);
+        assertAll(
+                () -> assertThat(bonusNumber.equals(7)).isEqualTo(true),
+                () -> assertThat(bonusNumber.equals(8)).isEqualTo(false)
         );
     }
 }

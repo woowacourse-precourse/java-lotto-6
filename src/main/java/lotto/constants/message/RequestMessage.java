22af3549397005d214af49467afa0363ca09ee47
nayonsoso <panda0329@naver.com>
2023-11-08 21:03:55 +0900

    refactor: 프롬프트 메세지 앞뒤로 줄바꿈

--- src/main/java/lotto/constants/message/RequestMessage.java
+++ src/main/java/lotto/constants/message/RequestMessage.java
@@ -2,6 +2,6 @@ package lotto.constants.message;
 
 public class RequestMessage {
     public static final String PAYMENT = "구입금액을 입력해 주세요.";
-    public static final String WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    public static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 }

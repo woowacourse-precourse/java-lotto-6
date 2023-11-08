6ab6cb5b672f76570edb9893617ab406ad74e717
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 11:22:05 +0900

    chore : 오타 수정

--- src/main/java/lotto/view/OutputViewResult.java
+++ src/main/java/lotto/view/OutputViewResult.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class OutputViewResult {
     private static final String WIN_STATICS_MESSAGE = "당첨 통계";
     private static final String TOTAL_RATE_IS_MESSAGE = "총 수익률은 ";
-함    private static final String TOTAL_RATE_IS_MESSAGE_FINISH = "%입니다.";
+    private static final String TOTAL_RATE_IS_MESSAGE_FINISH = "%입니다.";
     private static final String LOTTO_RESULT_CASE_SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
     private static final String LOTTO_RESULT_CASE_BASIC_MESSAGE = "%d개 일치 (%s원) - %d개";
     private static final String SEPARATE = "---";

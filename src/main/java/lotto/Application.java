f491e64f08cedc28d58b5c8b9bab8ddb66231566
JongMinCh0i <wonderland314592@gmail.com>
2023-11-08 20:33:20 +0900

    test(InputViewTest): 예상되는 예외 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        new LottoController().run();
+        new LottoController(new InputView(), new OutputView()).run();
     }
 }

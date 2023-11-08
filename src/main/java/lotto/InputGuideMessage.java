b6e38722c01bad4ef1f780e9dc1cafa229cb154f
jiho kim <jihokim1024@gmail.com>
2023-11-08 23:48:37 +0900

    입력 가이드 출력 문구

---
+++ src/main/java/lotto/InputGuideMessage.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class InputGuideMessage {
+    public static void enterAmountMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void enterWinningNumberMessage() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void enterBonusNumberMessage() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}

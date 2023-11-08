8c29514177fab744d0cf7aae17d5150fbe490c4f
leorb159 <ldgsocial159@naver.com>
2023-11-08 22:25:38 +0900

    refactor: 숫자 범위 전역화

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -5,14 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomNumberGenerator {
-    final int MIN_RANGE_NUMBER = 1;
-    final int MAX_RANGE_NUMBER = 45;
-    final int LOTTO_NUMBER_COUNT = 6;
+    static final int MIN_RANGE_NUMBER = 1;
+    static final int MAX_RANGE_NUMBER = 45;
+    static final int LOTTO_NUMBER_COUNT = 6;
 
     public List<Integer> generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 MIN_RANGE_NUMBER, MAX_RANGE_NUMBER, LOTTO_NUMBER_COUNT);
         return numbers;
     }
-
 }

de156fa97c468fb24e399e8d9589030b5a801741
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:02:30 +0900

    LottoNumberRangeException  테스트

--- src/test/java/lotto/exception/LottoNumberRangeExceptionTest.java
+++ src/test/java/lotto/exception/LottoNumberRangeExceptionTest.java
@@ -2,18 +2,17 @@ package lotto.exception;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberRangeExceptionTest {
     @Test
     public void testNumberRangeException_ValidNumber() {
-        // Arrange
+
         int validNumber = 42;
 
-        // Act
+
         LottoNumberRangeException.NumberRangeException(validNumber);
 
-        // No exception is thrown, the test passes
+
     }
 
     @Test

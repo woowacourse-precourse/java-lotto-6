e87d3ee4e981237bcaecfe45d1cc94c8ad389881
yoonjuho <dbswn990@naver.com>
2023-11-08 17:42:14 +0900

    refactor(OutputView): 값을 하드코딩하지 않고 상수화 처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import lotto.constant.response.Information;
 import lotto.model.Lotto;
 import lotto.model.Rank;
 
@@ -7,34 +8,39 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
+    private static final String START_SEPARATOR = "[";
+    private static final String MIDDLE_SEPARATOR = ", ";
+    private static final String END_SEPARATOR = "]";
+    private static final String LINE_SEPARATOR = "---";
+
     public void printException(String exceptionMessage) {
         System.out.println(exceptionMessage);
     }
 
     public void printLottoTicket(List<Lotto> lottoTicket) {
-        System.out.println(String.format("%d개를 구매했습니다.", lottoTicket.size()));
+        System.out.printf((Information.NUMBER_PURCHASED.getMessage()) + "%n", lottoTicket.size());
         for (Lotto lotto : lottoTicket) {
             System.out.println(printLottoDetail(lotto));
         }
     }
 
     private String printLottoDetail(Lotto lotto) {
-        String lottoDetail = "[";
-        lottoDetail += String.join(", ", lotto.getLottoNumbers().stream()
+        String lottoDetail = START_SEPARATOR;
+        lottoDetail += String.join(MIDDLE_SEPARATOR, lotto.getLottoNumbers().stream()
                 .map(lottoNumber -> Integer.toString(lottoNumber.getNumber()))
                 .toList());
-        lottoDetail += "]";
+        lottoDetail += END_SEPARATOR;
         return lottoDetail;
     }
 
-    public void printWinningStatus(Map<Rank, Integer> winningStatus) {
-        System.out.println("당첨 통계" + System.lineSeparator() + "---");
-        for (Rank rank : Rank.getShownRank()) {
-            System.out.println(String.format(rank.getMessage(), winningStatus.get(rank)));
+    public void printWinningDetails(Map<Rank, Integer> winningDetails) {
+        System.out.println(Information.WINNING_STATISTICS.getMessage() + System.lineSeparator() + LINE_SEPARATOR);
+        for (Rank rank : Rank.winningValues()) {
+            System.out.printf((rank.getMessage()) + "%n", winningDetails.get(rank));
         }
     }
 
-    public void printProfitRate(String profitRate){
-        System.out.println(String.format("총 수익률은 %s%%입니다.", profitRate));
+    public void printProfitRate(String profitRate) {
+        System.out.printf((Information.PROFIT_RATE.getMessage()) + "%n", profitRate);
     }
 }

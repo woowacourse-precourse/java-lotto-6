783b2f43dbb10c9c2a9be953fcd6416389a4011d
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 17:18:30 +0900

    feat(WinningNumber) : add winningNumberCount() method

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,18 +1,27 @@
 package lotto.domain;
 
-import lotto.service.GameService;
-
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningNumber {
     private final List<Integer> numbers;
 
     public WinningNumber(String numbers) {
-        // 검증 로직
-        this.numbers = GameService.createWinningNumbers(numbers);
+        this.numbers = verification(numbers);
     }
 
-    public List<Integer> getNumber() {
-        return numbers;
+    public List<Integer> createWinningNumbers(String winningNumber) {
+        return Arrays.stream(winningNumber.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public int winningNumberCount(Lotto lotto) {
+
+        return (int) lotto.getNumbers().stream()
+                .filter(numbers::contains)
+                .count();
     }
 }

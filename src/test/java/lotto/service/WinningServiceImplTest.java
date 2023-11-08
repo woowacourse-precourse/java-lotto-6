cef44378fc7c795f5ae140b26980235bab65b59d
lesw <lesw1216@gmail.com>
2023-11-08 13:09:10 +0900

    test: 당첨 번호 중복 예외 발생 테스트

--- src/test/java/lotto/service/WinningServiceImplTest.java
+++ src/test/java/lotto/service/WinningServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
+import lotto.utils.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,19 @@ class WinningServiceImplTest {
 //      - 마지막에 5, 와 같은 입력
 //    - [X] 문자 입력 시 예외 발생
     WinningServiceImpl winningService = new WinningServiceImpl();
+
+    @Test
+    @DisplayName("중복된 숫자 입력 시 예외 발생")
+    void winningNumberDuplicate() {
+        // Given
+        List<Integer> numbers = Arrays.asList(4, 23, 28, 33, 24, 23);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> winningService.createWinningLotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_DUPLICATE_LOTTO_NUMBER);
+    }
 
     @Test
     @DisplayName("6개 이상 입력 시 예외 발생")

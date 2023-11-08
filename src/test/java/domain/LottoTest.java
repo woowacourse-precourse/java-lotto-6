ad69a6a9d24edf6c28ea79d9d925e946eba2ac65
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 17:36:02 +0900

    test: '알맞은 등수를 반환하는지 테스트'

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -25,6 +25,30 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    
+    @Test
+    void 알맞은등수_반환테스트(){
+        Lotto correctLotto = new Lotto(List.of(1,2,3,4,5,6));
+        int bonusNumber = 7;
+
+        Lotto firstPlaceLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto secondPlaceLotto = new Lotto(List.of(1,2,3,4,5,7));
+        Lotto thirdPlaceLotto = new Lotto(List.of(1,2,3,4,5,8));
+        Lotto forthPlaceLotto = new Lotto(List.of(1,2,3,4,9,10));
+        Lotto fifthPlaceLotto = new Lotto(List.of(1,2,3,10,11,12));
+        Lotto none1PlaceLotto = new Lotto(List.of(1,2,10,11,12,13));
+        Lotto none2PlaceLotto = new Lotto(List.of(1,10,11,12,13,14));
+        Lotto none3PlaceLotto = new Lotto(List.of(10,11,12,13,14,15));
+
+
+        Assertions.assertThat(Place.FIRST).isEqualTo(firstPlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.SECOND).isEqualTo(secondPlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.THIRD).isEqualTo(thirdPlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.FORTH).isEqualTo(forthPlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.FIFTH).isEqualTo(fifthPlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.NONE).isEqualTo(none1PlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.NONE).isEqualTo(none2PlaceLotto.whichPlace(correctLotto,bonusNumber));
+        Assertions.assertThat(Place.NONE).isEqualTo(none3PlaceLotto.whichPlace(correctLotto,bonusNumber));
+    }
+
 
 }

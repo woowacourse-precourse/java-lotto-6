220138f8d691a385300cc7711dc58f4f50132117
jiwon1217 <koagjw@gmail.com>
2023-11-08 14:00:11 +0900

    :sparkles: feat: 구입 금액을 입력하는 문구를 출력한다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+
+    public static void printInputMoneyMessage() {
+        System.out.println(INPUT_MONEY);
+    }
+}

f2180c8cffc8e90ca9492fde1f159ebc30344afc
dj <fjzl12@naver.com>
2023-11-08 16:56:30 +0900

    refactor(InputView): 사용자 입력 함수 리턴하도록 수정

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,20 +1,25 @@
 package lotto.ui;
 
-import static lotto.domain.Message.INPUT_COST;
-import static lotto.domain.Message.INPUT_NUMBERS;
-import static lotto.domain.Message.INPUT_BONUS_NUMBERS;
+import static lotto.constants.Message.INPUT_COST;
+import static lotto.constants.Message.INPUT_NUMBERS;
+import static lotto.constants.Message.INPUT_BONUS_NUMBERS;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static void readCost() {
+    public static String readCost() {
         System.out.println(INPUT_COST.getMessage());
+        return Console.readLine();
     }
 
-    public static void readNumbers() {
+    public static String readNumbers() {
         System.out.println(INPUT_NUMBERS.getMessage());
+        return Console.readLine();
     }
 
-    public static void readBonusNumber() {
+    public static String readBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBERS.getMessage());
+        return Console.readLine();
     }
 
 }

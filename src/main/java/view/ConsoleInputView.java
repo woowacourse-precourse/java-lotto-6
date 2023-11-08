b8c79a36de7dfead62c39b68f352cbb133521883
eora21 <eora21@naver.com>
2023-11-08 14:01:14 +0900

    feat: 🎸 (ConsoleInputView) 추가 로또 당첨 번호를 입력받는 메서드 작성

--- src/main/java/view/ConsoleInputView.java
+++ src/main/java/view/ConsoleInputView.java
@@ -3,11 +3,22 @@ package view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import exception.ParseException;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConsoleInputView implements InputView {
     @Override
     public int enterPurchaseAmount() {
         return parseInt(readLine());
+    }
+
+    @Override
+    public List<Integer> enterWinningLottoNumbers() {
+        return Arrays.stream(readLine().split(","))
+                .map(String::trim)
+                .mapToInt(this::parseInt)
+                .boxed()
+                .toList();
     }
 
     private Integer parseInt(String text) {

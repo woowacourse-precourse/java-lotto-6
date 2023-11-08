2947d36cfbe340391f65fb3bad7b2507760c86d5
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 18:50:55 +0900

    fix : 표시 형식 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,13 +5,15 @@ import lotto.domain.LottoTicket;
 import lotto.domain.Rank;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class OutputView {
     private static final String PURCHASED_TICKETS_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
     private static final String WINNING_STATISTICS_LINE = "---";
-    private static final String RESULT_FORMAT = "%d개 일치 (%d원) - %d개";
+    private static final String RESULT_FORMAT = "%d개 일치 (%s원) - %d개";
     private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
     private static final DecimalFormat PRIZE_FORMAT = new DecimalFormat("#,##0");
 
@@ -27,16 +29,19 @@ public class OutputView {
         System.out.println(WINNING_STATISTICS_HEADER);
         System.out.println(WINNING_STATISTICS_LINE);
 
-        for (Rank rank : Rank.values()) {
-            if (rank != Rank.NONE) { // 모든 등수를 출력합니다.
+        List<Rank> ranks = Arrays.asList(Rank.values());
+        Collections.reverse(ranks); // 순서를 뒤집습니다.
+
+        for (Rank rank : ranks) {
+            if (rank != Rank.NONE) {
                 System.out.println(String.format(RESULT_FORMAT,
                         rank.getMatchCount(),
-                        PRIZE_FORMAT.format(rank.getPrizeMoney()), // 숫자 형식에 콤마를 포함합니다.
+                        PRIZE_FORMAT.format(rank.getPrizeMoney()),
                         lottoResult.getRankCount(rank)));
             }
         }
     }
-
+    
     public static void printProfitRate(double profitRate) {
         System.out.println(String.format(PROFIT_RATE_FORMAT, profitRate));
     }

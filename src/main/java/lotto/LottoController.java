52af68c872a5e6908fc6eedf83b2abee531d8cb9
hojang <109591135+26solitude@users.noreply.github.com>
2023-11-08 15:14:35 +0900

    feat: 보너스 숫자를 입력받는 기능

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -14,18 +14,30 @@ public class LottoController {
     void play(){
         MoneyInput();
         LottoNumInput();
+        BonusNumInput();
     }
 
     private void MoneyInput() {
+        System.out.print("Money : ");
         int Money = Integer.parseInt(readLine());
-        System.out.println(Money);
+        System.out.println("Money = " + Money);
     }
 
+    private void BonusNumInput() {
+        System.out.print("BonusNum : ");
+        int BonusNum = Integer.parseInt(readLine());
+        System.out.println("BonusNum = " + BonusNum);
+    }
+
+
     private void LottoNumInput(){
+        System.out.print("List : ");
         List<Integer> list = Arrays.stream(readLine().split(","))
                 .mapToInt(Integer::parseInt)
                 .boxed().collect(Collectors.toList());
-        System.out.println(list);
+        System.out.println("List = " + list);
         Lotto lotto = new Lotto(list);
     }
+
+
 }

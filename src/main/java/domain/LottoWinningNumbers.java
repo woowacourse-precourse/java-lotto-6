a97c3ec0233435b72f87f60f1a0b835b90dd87e5
eora21 <eora21@naver.com>
2023-11-08 09:42:26 +0900

    refactor: 💡 (LottoWinningNumbers) 수정 로또 당첨번호와 보너스번호 객체 사용으로 변경

--- src/main/java/domain/LottoWinningNumbers.java
+++ src/main/java/domain/LottoWinningNumbers.java
@@ -1,25 +1,20 @@
 package domain;
 
-import exception.DuplicateNumberException;
-import java.util.List;
-
 public class LottoWinningNumbers {
-    private final Lotto winningNumbers;
-    private final LottoNumber bonusNumber;
+    private final WinningNumbers winningNumbers;
+    private final BonusNumber bonusNumber;
 
-    private LottoWinningNumbers(Lotto winningNumbers, LottoNumber bonusNumber) {
+    private LottoWinningNumbers(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
-    public static LottoWinningNumbers createWinningNumbers(List<Integer> numbers, int bonusNumber) {
-        validateDuplicateBonusNumber(numbers, bonusNumber);
-        return new LottoWinningNumbers(new Lotto(numbers), new LottoNumber(bonusNumber));
+    public static LottoWinningNumbers createWinningNumbers(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        validateDuplicateBonusNumber(winningNumbers, bonusNumber);
+        return new LottoWinningNumbers(winningNumbers, bonusNumber);
     }
 
-    private static void validateDuplicateBonusNumber(List<Integer> numbers, int bonusNumber) {
-        if (numbers.contains(bonusNumber)) {
-            throw new DuplicateNumberException();
-        }
+    private static void validateDuplicateBonusNumber(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        bonusNumber.exceptionIfDuplicate(winningNumbers);
     }
 }

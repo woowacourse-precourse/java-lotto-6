f0dadf50b5f2b77aa099704ae0297ab5e8be4810
leorb159 <ldgsocial159@naver.com>
2023-11-08 22:39:40 +0900

    refactor: LottoConst 생성 및 로또 관련 상수 enum화

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -3,7 +3,9 @@ package lotto;
 public enum ErrorMessage {
     INPUT_NOT_NUMBER("[ERROR] 숫자로 변환할 수 없는 입력입니다. 다시 입력해주세요."),
     NOT_DIVISIBLE_LOTTO_PRICE(String.format("[ERROR] %d으로 나누어 떨어지는 금액을 입력해야 합니다. 다시 입력해주세요.", LottoManagement.PRICE_LOTTO)),
-    INVALID_LOTTO_NUMBERS("[ERROR] 입력한 로또 번호가 유효하지 않습니다. 6개의 중복되지 않는 숫자를 입력해주세요.");
+    INVALID_LOTTO_NUMBERS(String.format("[ERROR] 입력한 로또 번호가 유효하지 않습니다. ,를 포함하여 %d ~ %d 범위의 중복되지 않는 %d 자리를 입력해주세요.",
+            LottoConst.MIN_RANGE_NUMBER.getValue(), LottoConst.MAX_RANGE_NUMBER.getValue(), LottoConst.LOTTO_NUMBER_COUNT.getValue()));
+
 
     private final String message;
 
--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -5,13 +5,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomNumberGenerator {
-    static final int MIN_RANGE_NUMBER = 1;
-    static final int MAX_RANGE_NUMBER = 45;
-    static final int LOTTO_NUMBER_COUNT = 6;
 
     public List<Integer> generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
-                MIN_RANGE_NUMBER, MAX_RANGE_NUMBER, LOTTO_NUMBER_COUNT);
+                LottoConst.MIN_RANGE_NUMBER.getValue(), LottoConst.MAX_RANGE_NUMBER.getValue(), LottoConst.LOTTO_NUMBER_COUNT.getValue());
         return numbers;
     }
 }

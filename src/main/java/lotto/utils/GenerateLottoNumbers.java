ada7d03a706c0c2e18039b8e930cfdf9059e5bd6
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 10:50:13 +0900

    feat(GenerateLottoNumbers) : getBuyingQuantity 메서드 생성

--- src/main/java/lotto/utils/GenerateLottoNumbers.java
+++ src/main/java/lotto/utils/GenerateLottoNumbers.java
@@ -1,5 +1,6 @@
 package lotto.utils;
 
+import lotto.Constants;
 import lotto.domain.UserLotto;
 
 import java.util.List;
@@ -9,5 +10,9 @@ public class GenerateLottoNumbers {
     public static List<UserLotto> generateLottoNumbers(int buyingPrice) {
         List<UserLotto> lottos = buyingLottos(getBuyingQuantity(buyingPrice));
         return lottos;
+    }
+
+    public static int getBuyingQuantity(int buyingPrice) {
+        return buyingPrice / Constants.LOTTO_PRICE;
     }
 }

02a47a19581b55b16bb47d5305db6bf28c2a7532
minju <minju9711@naver.com>
2023-11-08 16:41:38 +0900

    [feat] 사용자의 금액에 대한 입력을 검증하는 checkInteger와 사용자가 올바른 단위의 돈으로 입력했는지 보는 CashIsInteger 함수를 합침

--- src/main/java/lotto/Util/Validate.java
+++ src/main/java/lotto/Util/Validate.java
@@ -1,23 +1,43 @@
 package lotto.Util;
 
-import lotto.Factory.BonusNumberChecker;
+import lotto.LottoFactory.LottoChecker;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Validate {
 
-    public static void checkInteger(String number){ //사용자의 입력이 숫자형태인지 모두 이 함수를 통해 확인
+
+    public static void CashForm(String Cash){
+
         try {
-            Integer.parseInt(number); // 숫자로 변환을 시도
+            Integer.parseInt(Cash); // 숫자로 변환을 시도
         } catch (NumberFormatException e) { //만약 숫자가 아닌 값이 있다면 해당 예외 발생
-            throw new IllegalArgumentException("[ERROR] 숫자만을 입력해주세요");
+            throw new NoSuchElementException("[ERROR] 숫자만을 입력해주세요");
+        }
+
+        int cash = Integer.parseInt(Cash);
+        if(cash%1000!=0){
+            throw new NoSuchElementException("[ERROR] 구입금액은 천원, 또는 만원 단위로 입력해주세요.");
         }
     }
 
-    public static void CashIsInteger(String Cash){
-        int cash = Integer.parseInt(Cash);
-        if(cash/1000!=0){
-            throw new IllegalArgumentException("[ERROR] 구입금액은 천원, 또는 만원 단위로 입력해주세요.");
+    public static List<Integer> WinningLottoInput(String WinningLotto) {
+        boolean hasInvalidDelimiter = WinningLotto.matches(".*[^\\d,].*"); //공백 및 ,외 다른 구분자 사용시 true 반환
+
+        if (hasInvalidDelimiter) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 공백없이 ,와 숫자만을 이용하여 입력해주세요.");
         }
+
+        List<Integer> lottoNumbers = Divide.Delimiter(WinningLotto);
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 6개의 숫자로 입력해주세요.");
+        }
+
+        if (lottoNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 중복될 수 없습니다.");
+        }
+
+        return lottoNumbers;
     }
 
     public static List<Integer> WinningLottoInput(String WinningLotto) {
@@ -35,10 +55,9 @@ public class Validate {
     }
 
     public static void isDuplicateBonus(BonusNumberChecker checker, int bonus){
-       Boolean isDuplicate = checker.isDuplicateBonus(bonus);
-       if(isDuplicate){
-           throw new IllegalArgumentException("[ERROR] 보너스 숫자는 당첨번호와 겹칠 수 없습니다.");
-       }
+        Boolean isDuplicate = checker.isDuplicateBonus(bonus);
+        if(isDuplicate){
+            throw new IllegalArgumentException("[ERROR] 보너스 숫자는 당첨번호와 겹칠 수 없습니다.");
+        }
     }
-
 }

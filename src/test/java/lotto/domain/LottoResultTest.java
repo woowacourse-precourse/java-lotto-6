2515e80089112c3177b6ecce83337424f7f67d22
haebyun <bhk4392@gmail.com>
2023-11-08 22:03:13 +0900

    test(LottoResult): 로또 결과에 따라 형식에 맞추어 문자열을 생성하는 기능 테스트

---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoResultTest {
+    private List<Lotto> lottos;
+    private WinningLotto winningLotto;
+    private LottoResult lottoResult;
+
+    @BeforeEach
+    void setUp() {
+        winningLotto = new WinningLotto(List.of(1,2,3,4,5,6),7);
+        lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(4,5,6,7,8,9)));
+        lottos.add(new Lotto(List.of(2,3,4,5,6,7)));
+        lottoResult = new LottoResult(lottos, winningLotto);
+    }
+
+    @Test
+    @DisplayName("로또 결과에 따라 형식에 맞추어 문자열을 생성하는 기능 테스트")
+    void getFormalizedResultTest(){
+        //when
+        List<String> result = lottoResult.getFormalizedResult();
+        //then
+        assertThat(result).isEqualTo(List.of("3개 일치 (5,000원) - 1개",
+                                                "4개 일치 (50,000원) - 0개",
+                                                "5개 일치 (1,500,000원) - 0개",
+                                                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                                                "6개 일치 (2,000,000,000원) - 0개"));
+    }
+
+}

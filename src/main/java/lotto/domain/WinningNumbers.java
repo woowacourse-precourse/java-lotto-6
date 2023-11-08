65288061a5612409a5e25e14e971489e220ec91e
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-08 13:40:31 +0900

    [FEAT] 당첨 번호와 보너스 번호 입력받기

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class WinningNumbers {
@@ -9,6 +10,15 @@ public class WinningNumbers {
     private WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningNumbers inputWinningNumbers() {
+        List<Integer> winningNumbers = Arrays.stream(Console.readLine("당첨 번호를 입력해 주세요.").split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int bonusNumber = Integer.parseInt(Console.readLine("보너스 번호를 입력해 주세요."));
+        return new WinningNumbers(winningNumbers, bonusNumber);
     }
 
     public Rank calculateRank(Lotto lotto) {

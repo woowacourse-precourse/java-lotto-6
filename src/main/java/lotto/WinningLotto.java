79095aebf970aa2a27736fb3fda218d217c32656
02ggang9 <02ggang9@gmail.com>
2023-11-08 15:53:33 +0900

    fix(WinnongLotto): 가독성을 위해 _ 추가

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -4,11 +4,11 @@ import java.util.function.BiFunction;
 
 public enum WinningLotto {
 
-    FIRST_PLACE(6, 0, 2000000000, (matchCount, bonusCount) -> matchCount == 6),
-    SECOND_PLACE(5, 1, 30000000, (matchCount, bonusCount) -> matchCount == 5 && bonusCount == 1),
-    THIRD_PLACE(5, 0, 1500000, (matchCount, bonusCount) -> matchCount == 5 && bonusCount == 0),
-    FOURTH_PLACE(4, 0, 50000, (matchCount, bonusCount) -> matchCount == 4),
-    FIFTH_PLACE(3, 0, 5000, (matchCount, bonusCount) -> matchCount == 3),
+    FIRST_PLACE(6, 0, 2_000_000_000, (matchCount, bonusCount) -> matchCount == 6),
+    SECOND_PLACE(5, 1, 30_000_000, (matchCount, bonusCount) -> matchCount == 5 && bonusCount == 1),
+    THIRD_PLACE(5, 0, 1_500_000, (matchCount, bonusCount) -> matchCount == 5 && bonusCount == 0),
+    FOURTH_PLACE(4, 0, 50_000, (matchCount, bonusCount) -> matchCount == 4),
+    FIFTH_PLACE(3, 0, 5_000, (matchCount, bonusCount) -> matchCount == 3),
     LAST_PLACE(2, 0, 0, (matchCount, bonusCount) -> matchCount <= 2),
     ;
 

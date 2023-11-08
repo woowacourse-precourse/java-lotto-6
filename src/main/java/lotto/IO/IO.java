88dbdbfe6f45758ba8ceb58c7a215286c09e9cb9
minju <minju9711@naver.com>
2023-11-08 16:55:36 +0900

    [feat] 랜덤으로 생성된 로또 출력하는 함수 추가

--- src/main/java/lotto/IO/IO.java
+++ src/main/java/lotto/IO/IO.java
@@ -1,6 +1,13 @@
 package lotto.IO;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.LottoFactory.LottoChecker;
+import lotto.LottoFactory.LottoRank;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
 
 public class IO { //여긴 무조건 정적호출로 해보자
 
@@ -20,9 +27,13 @@ public class IO { //여긴 무조건 정적호출로 해보자
 
     public static String InputBonus(){
         System.out.println("보너스 번호를 입력해 주세요.");
-        String bonus = Console.readLine();
-        return bonus;
+        return Console.readLine();
     }
+
+    public static void PrintBuyLotto(LottoChecker lottoChecker){
+        System.out.println(lottoChecker.toString());
+    }
+
 
 }
 

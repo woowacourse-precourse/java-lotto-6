bb1dc94a020a6a8ec89df8c670180470f77d4bb0
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 21:46:54 +0900

    feat:"game의 outputview에 필요한 메세지 상수처리"

---
+++ src/main/java/lotto/constant/OutputViewConstant.java
@@ -0,0 +1,11 @@
+package lotto.constant;
+
+public class OutputViewConstant {
+    public static final String BUY_LOTTO_AMOUNT_MESSAGE = "개를 구매했습니다.";
+    public static final String LOTTO_WINNING_STATS_MESSAGE = "당첨 통계";
+    public static final String DASH_SEPARATOR_MESSAGE = "---";
+    public static final String LOTTO_WINNING_RANK_COUNT_MESSAGE = "%d개 일치 (%,d원) - %d개\n";
+    public static final String LOTTO_WINNING_RANK_BONUS_COUNT_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
+    public static final String TOTAL_PROFIT_MARGIN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+
+}

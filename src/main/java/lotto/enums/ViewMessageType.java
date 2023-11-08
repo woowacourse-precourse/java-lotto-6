d1bce32f8bb118a1df83c25faccadf326fec48f0
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 22:44:05 +0900

    fix(ViewMessageType): 수익률 소수 둘째 자리에서 반올림으로 수정

--- src/main/java/lotto/enums/ViewMessageType.java
+++ src/main/java/lotto/enums/ViewMessageType.java
@@ -14,7 +14,7 @@ public enum ViewMessageType {
             + "5개 일치 (1,500,000원) - %d개\n"
             + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
             + "6개 일치 (2,000,000,000원) - %d개\n"
-            + "총 수익률은 %2f입니다.");
+            + "총 수익률은 %.1f%입니다.");
 
     private final String message;
 

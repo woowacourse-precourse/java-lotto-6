2575f10297705869a3c89f81ab3facc13704f2a6
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 23:33:23 +0900

    [REFACTOR] 코드 정리

--- src/main/java/lotto/io/InputManager.java
+++ src/main/java/lotto/io/InputManager.java
@@ -11,9 +11,7 @@ public abstract class InputManager<T> {
         this.consoleAdapter = consoleAdapter;
     }
 
-    public static void validateNumberRange(Integer number) {
-        if (number > 45 || number < 1) {
-            throw new IllegalArgumentException("[ERROR] 1~45 범위 내의 값을 입력해주세요.");
-        }
+    public static void validateNumberRange(Integer number) throws IllegalArgumentException {
+        if (number > 45 || number < 1) throw new IllegalArgumentException("[ERROR] 1~45 범위 내의 값을 입력해주세요.");
     }
 }

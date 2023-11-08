f971c1a60c29db601ef7eead42a9fe22b21d6fed
skj4244 <skj4244@naver.com>
2023-11-08 16:28:45 +0900

    style: not necessary import and space

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,6 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import static java.util.Collections.replaceAll;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.*;
 
 public class LottoView {

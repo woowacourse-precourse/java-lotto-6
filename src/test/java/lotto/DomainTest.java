1b502ff0f51974e2ed26b243b24781d5cc51e366
Han <yulmam1@naver.com>
2023-11-08 19:13:07 +0900

    test : 당첨금이 매우 큰 경우 테스트 추가

--- src/test/java/lotto/DomainTest.java
+++ src/test/java/lotto/DomainTest.java
@@ -18,15 +18,14 @@ public class DomainTest {
     @Test
     void 살_수_있는_로또의_개수_테스트(){
         //given
-        Money money = new Money(5000);
         int expectedValue = 5;
         int lottoCounts;
 
         //when
-        lottoCounts = money.getCount();
+        Money money = new Money(5000);
 
         //then
-        assertEquals(lottoCounts, expectedValue);
+        assertEquals(money.getCount(), expectedValue);
     }
 
     @DisplayName("돈에 따른 알맞은 로또의 개수가 생성됐는지 테스트")
@@ -43,15 +42,15 @@ public class DomainTest {
         assertEquals(lottoCounts, lottos.getLottos().size());
     }
 
-    @DisplayName("당첨 테스트")
+    @DisplayName("당첨된 로또의 개수가 예상값과 맞는지 비교한다.")
     @Test
     void 당첨_테스트(){
         //given
         Lottos lottos = new Lottos(
                 List.of(
-                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                        new Lotto(List.of(1, 2, 3, 4, 5, 7))
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),//3등
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),//3등
+                        new Lotto(List.of(1, 2, 3, 4, 5, 7))//2등
                 )
         );
         TotalWinningNumbers totalWinningNumbers
@@ -65,7 +64,7 @@ public class DomainTest {
                 .contains(entry(Rank.SECOND, 1), entry(Rank.THIRD, 2), entry(Rank.FIFTH, 0));
     }
 
-    @DisplayName("수익률 테스트")
+    @DisplayName("당첨이 된 후에 계산한 수익률을 테스트한다.")
     @Test
     void 수익률_테스트(){
         //given
@@ -88,5 +87,26 @@ public class DomainTest {
         assertEquals(lottoResult.getRateOfReturn(money), expectedRateOfReturn);
     }
 
+    @DisplayName("당첨금이 매우 클 경우")
+    @Test
+    void 당첨금이_매우_클_경우_테스트() {
+        Money money = new Money(2000);
+        Lottos lottos = new Lottos(
+                List.of(
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6))
+                )
+        );
+        TotalWinningNumbers totalWinningNumbers
+                = new TotalWinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+        double expectedRateOfReturn = 6000000000d/2000d*100d;
 
+        //when
+        LottoResult lottoResult = totalWinningNumbers.checkLottos(lottos);
+
+        //then
+        assertEquals(lottoResult.getRateOfReturn(money), expectedRateOfReturn);
+
+    }
 }

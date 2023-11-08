a57c4721246523f70963d693f3262edf8ebfdf41
isyoo1213 <moethns@gmail.com>
2023-11-08 17:53:41 +0900

    test: OutputViewTest 구매 내역 출력 테스트 수정 OutputViewTest 메서드 수정으로 인한 테스트 메서드 수정

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -12,12 +12,13 @@ public class OutputViewTest {
     @DisplayName("로또 구매 내역 출력 테스트")
     @Test
     void printLottoBuyingResultTest() {
-        int testLottoAmount = 3;
         List<List<Integer>> testLottoNumbers = Arrays.asList(
                 Arrays.asList(1, 2, 3, 4, 5, 6),
                 Arrays.asList(11, 12, 13, 14, 15, 16),
                 Arrays.asList(21, 22, 23, 24, 25, 26));
-        outputView.printLottoBuyingResult(testLottoAmount, testLottoNumbers);
+        for (List<Integer> testLottoNumber : testLottoNumbers) {
+            outputView.printNumbers(testLottoNumber);
+        }
     }
 
     @DisplayName("로또 당첨 내역 출력 테스트")

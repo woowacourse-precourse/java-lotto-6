4d613a83b910c2782c11d791c0c580cfdfe62ff6
yujeonghyeon <dbdb1114@naver.com>
2023-11-08 22:33:50 +0900

    Lotto 클래스 내부 로또 숫자 중복 유효성 검사 구현

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import util.CustomIllegalArgumentException;
 import util.GameProperty;
 import util.InputException;
@@ -15,11 +17,19 @@ public class Lotto {
 
     public static void validate(List<Integer> numbers) {
         lengthVlalidator(numbers);
+        hasDupleNum(numbers);
     }
 
     private static void lengthVlalidator(List<Integer> numbers) {
         if (numbers.size() != GameProperty.LOTTO_NUMBERS_LENGTH) {
             throw new CustomIllegalArgumentException(InputException.NUMBERS_LENGTH_EXCEPTION);
+        }
+    }
+
+    private static void hasDupleNum(List<Integer> numbers) {
+        Set<Integer> nonDupleNumbers = new HashSet<>(numbers);
+        if (nonDupleNumbers.size() != numbers.size()) {
+            throw new CustomIllegalArgumentException(InputException.DUPLE_NUMBER_EXCEPTION);
         }
     }
 

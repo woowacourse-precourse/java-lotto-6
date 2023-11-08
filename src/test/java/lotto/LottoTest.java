5a868daa34fa205469dab92c2a5f4986e9e62635
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-08 09:44:16 +0900

    test : Lotto에 대해 주어진 로또와 일치하는 번호개수 구하기 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import lotto.domain.Lotto;
@@ -24,4 +25,17 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("주어진 로또 번호와 일치하는 번호의 개수를 구합니다.")
+    @Test
+    void 일치번호_개수_계산() {
+        // GIVEN
+        List<Integer> thisNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto thisLotto = new Lotto(thisNumbers);
+        List<Integer> thatNumbers = List.of(4, 5, 6, 7, 8, 9);
+        Lotto thatLotto = new Lotto(thatNumbers);
+        // WHEN
+        int matchNumberCount = thisLotto.getMatchNumberCount(thatLotto);
+        // THEN
+        assertEquals(3, matchNumberCount);
+    }
 }

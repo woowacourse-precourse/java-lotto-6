1c73ea821dd31ca51242683a35a033c1ed0d32ec
Minhyeok Song <minhyeok.dev@gmail.com>
2023-11-08 10:57:58 +0900

    Feat: 구입금액 입력 및 구매 개수 출력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,8 +2,8 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        LottoGame lottoGame = new LottoGame();
 
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
     }
 }
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,7 +1,53 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class LottoGame {
 
+    private int purchaseAmount;
+    private int numberOfPurchase;
 
+    public void start() {
+        System.out.println("구입 금액을 입력해주세요.");
+        inputPurchaseAmount();
+        printNumberOfPurchase();
+
+    }
+
+    private void inputPurchaseAmount() {
+        setPurchaseAmount(Integer.parseInt(readLine()));
+    }
+
+    public void setPurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로만 입력이 가능합니다.");
+        } else if (purchaseAmount % 10 == 0) {
+            this.purchaseAmount = purchaseAmount;
+            this.numberOfPurchase = purchaseAmount / 1000;
+        }
+    }
+
+    public void checkPurchaseAmount() {
+        if (this.purchaseAmount % 10 != 0) {
+            throw new IllegalArgumentException("1000원 단위로만 입력이 가능합니다.");
+        }
+    }
+
+    public int getNumberOfPurchase() {
+        return this.numberOfPurchase;
+    }
+
+    public void setNumberOfPurchase() {
+        if (this.purchaseAmount % 10 == 0) {
+            this.numberOfPurchase = purchaseAmount / 1000;
+        }
+    }
+
+    private void printNumberOfPurchase() {
+        System.out.println(getNumberOfPurchase() + "개를 구매했습니다.");
+    }
+
+    private void printRandomLottoNumber() {
+
+    }
 }

a19e9dd7bc15258d60f84f891a0875e90310a2c7
djc06048 <gsafe1213@gmail.com>
2023-11-08 09:24:33 +0900

    feat(view): InputView 추가
    로또 구입 금액과 당첨 번호 입력받는 부분 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,27 @@
 package lotto.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+    public String getLottoPrice() {
+        return Console.readLine();
+    }
+
+    public List<Integer> getWinningNumbers() {
+        String line = Console.readLine();
+        List<Integer> winningNumbers = Arrays.stream(line.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .filter(number -> number >= 1 && number <= 45)
+                .toList();
+        if (winningNumbers.size() != Arrays.stream(line.split(",")).count()) {
+            throw new IllegalArgumentException();
+        }
+        return winningNumbers;
+    }
 }

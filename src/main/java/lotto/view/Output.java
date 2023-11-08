4a8d7dcc1c9eadcff88fa8d1864bfc4e1387caa9
honeydanji <tjdwls3680@naver.com>
2023-11-08 16:52:58 +0900

    REFACTOR : 제공된 main 테스트 에러로 인한 코드 수정.
    // 원인
    - 출력 순서가 바뀜
    
    // 해결
    - 출력 순서 조정

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -14,12 +14,12 @@ public class Output {
         System.out.println("---");
         System.out.println(String.format(WINNING.getMessage(),3,5000,value[0]));
         System.out.println(String.format(WINNING.getMessage(),4,50000,value[1]));
-        System.out.println(String.format(WINNING.getMessage(),5,30000000,value[3]));
-        System.out.println(String.format(BONUS_WINNING.getMessage(),5,1500000,value[2]));
+        System.out.println(String.format(WINNING.getMessage(),5,1500000,value[3]));
+        System.out.println(String.format(BONUS_WINNING.getMessage(),5,30000000,value[2]));
         System.out.println(String.format(WINNING.getMessage(),6,2000000000,value[4]));
     }
 
     public void outputProfitRate(double profitRate) {
-        System.out.println(String.format(PROFIT_RATE.getMessage(), profitRate));
+        System.out.println(String.format(PROFIT_RATE.getMessage(), profitRate * 100));
     }
 }

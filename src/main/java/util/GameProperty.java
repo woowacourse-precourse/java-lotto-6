e5ea61829484870434107d16b82e4fdf5e5fc243
yujeonghyeon <dbdb1114@naver.com>
2023-11-08 22:36:01 +0900

    로또 조건 사항 GameProperty 클래스 생성

---
+++ src/main/java/util/GameProperty.java
@@ -0,0 +1,9 @@
+package util;
+
+public class GameProperty {
+
+    public static final int LOTTO_NUMBERS_LENGTH = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+
+}

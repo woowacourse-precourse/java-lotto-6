dbb11264522aa320f46c6e8485aa33feea4b8cab
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 21:10:06 +0900

    Feat : 자동 생성된 로또 번호 출력, 당첨 결과 및 수익률 출력해주는 객체

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,46 @@
+package lotto.view;
+
+import lotto.entity.Lotto;
+import lotto.entity.Prize;
+import lotto.model.Profit;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Output {
+//    DecimalFormat decimalFormat = new DecimalFormat("#.00%");
+    public void profitResult(Profit profit){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        Map<Prize,Integer> resultList=profit.getResultCount();
+        for(Prize prize:Prize.values()){
+            if(prize==Prize.OUT){
+                continue;
+            }
+            String message = prize.getMessage();
+            String count = resultList.get(prize).toString();
+            System.out.println(message+" - "+count+"개");
+        }
+//        String formatted="0.00";
+//        if(profit.getProfitRate()!=0){
+//            formatted = decimalFormat.format(profit.getProfitRate());
+//        }
+        String profitRate = String.format("%.1f",profit.getProfitRate());
+
+        System.out.println("총 수익률은 "+profitRate+"%입니다.\n");
+    }
+    public void lottoResult(List<Lotto> lottos){
+        System.out.println(lottos.size()+"개를 구매했습니다.");
+        for(Lotto lotto:lottos){
+            lottoFormatOutput(lotto.getNumbers());
+        }
+        System.out.println();
+    }
+    public void lottoFormatOutput(List<Integer> lotto){
+        String result = lotto.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+        System.out.println("["+result+"]");
+    }
+}

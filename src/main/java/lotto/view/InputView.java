0f03697f7235c34f8be0e724ab5a659f53419055
acycoco <pink10257@naver.com>
2023-11-08 21:30:59 +0900

    refactor(inputView): static 키워드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,10 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private final String PAID_AMOUNT_REQUEST_PROMPT = "구입금액을 입력해 주세요.";
-    private final String WINNING_NUMBERS_REQUEST_PROMPT = "당첨 번호를 입력해 주세요.";
-    private final String BONUS_NUMBER_REQUEST_PROMPT = "보너스 번호를 입력해 주세요.";
-    private static final String NEW_LINE = "\n";
+    private static final String PAID_AMOUNT_REQUEST_PROMPT = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_REQUEST_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_REQUEST_PROMPT = "보너스 번호를 입력해 주세요.";
 
     private final OutputView outputView;
 

11bc36d6da20e547352c953bf154e3991d460732
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 21:46:37 +0900

    feat:"game의 inputview에 필요한 메세지 상수처리"

---
+++ src/main/java/lotto/constant/InputViewConstant.java
@@ -0,0 +1,8 @@
+package lotto.constant;
+
+public class InputViewConstant {
+    public static final String PURCHASE_AMOUNT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_LOTTO_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_LOTTO_WINNING_Bonus_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+}

9cc4787fa0e8183fdb4c0464851eb5072f06807d
KJBig <kjgi73k@gmail.com>
2023-11-08 20:10:24 +0900

    feat : 당첨번호에 따라 당첨 통계를 알맞게 하는지 테스트.
    - 입력한 당첨번호에 따라 발급된 로또의 당첨 통계가 알맞게 계산되는지 확인하는 테스트를 구현.

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.enums.LottoConfig;
+import lotto.enums.WinningRank;
 import lotto.service.LottoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -30,5 +33,33 @@ public class LottoServiceTest {
 
         // then
         assertThat(lottos.size()).isEqualTo(payAmount / LottoConfig.LOTTO_AMOUNT.getValue());
+    }
+
+    @DisplayName("당첨 결과를 통계한다.")
+    @Test
+    void getWinningStatistics() {
+        // given
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    lottoService.publishLottos(4000);
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 9, 8, 10),
+                List.of(9, 8, 10, 4, 5, 6)
+
+        );
+
+        List<Integer> jackpotNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        // when
+        HashMap<WinningRank, Integer> winningStatistics = lottoService.getWinningStatistics(jackpotNumbers,
+                bonusNumber);
+
+        // then
+        assertThat(winningStatistics.get(WinningRank.FIRST)).isEqualTo(1);
+        assertThat(winningStatistics.get(WinningRank.SECOND)).isEqualTo(1);
+        assertThat(winningStatistics.get(WinningRank.FIFTH)).isEqualTo(2);
     }
 }

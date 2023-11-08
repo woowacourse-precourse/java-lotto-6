18c6bb5adbb6bbbc0fbf100dbbf29c598fd123ab
nayooniverse <kangna99@naver.com>
2023-11-08 16:45:36 +0900

    test: 당첨 로또 범위가 올바른지 확인하는 테스트

---
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+import static lotto.constants.Message.INVALID_LOTTO_NUMBER_RANGE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+    @DisplayName("로또 번호의 범위가 유효하지 않으면 예외가 발생한다.")
+    @Test
+    public void testConstructorInvalidWinningNumbersRange() {
+        assertThatThrownBy(() -> new WinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 55)), 7))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_LOTTO_NUMBER_RANGE);
+    }
+}

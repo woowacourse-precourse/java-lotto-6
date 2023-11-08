638651a9adbbd4545d293084a3a84bc5f0496448
JeongMin <ajhappy12@gamil.com>
2023-11-08 22:30:23 +0900

    feat: 수익률(ROI) 계산 메서드 구현

--- src/main/java/lotto/LottoRewardsCenter.java
+++ src/main/java/lotto/LottoRewardsCenter.java
@@ -14,4 +14,12 @@ public class LottoRewardsCenter {
     public List<LottoPrize> getPrizeResults(List<Lotto> lottos) {
         return lottos.stream().map(lottoChecker::check).toList();
     }
+
+    public Double calculateROI(List<LottoPrize> prizes) {
+        int inputAmount = prizes.size() * LottoConfig.LOTTO_PRICE;
+        long totalPrizeAmount = prizes.stream().mapToLong(LottoPrize::getPrizeAmount).sum();
+
+        double ROI = (double) totalPrizeAmount * 100 / inputAmount;
+        return Math.round(ROI * 10.0) / 10.0;
+    }
 }

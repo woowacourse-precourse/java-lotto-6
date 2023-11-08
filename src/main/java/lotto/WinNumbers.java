0bc307b84e57682cce4db6e00eb7d6a9e25a4c72
최진원 <etudent39@gmail.com>
2023-11-08 16:29:04 +0900

    feat : 당첨 번호 클래스 구현. 크기 및 중복 유효성 검사

--- src/main/java/lotto/WinNumbers.java
+++ src/main/java/lotto/WinNumbers.java
@@ -1,2 +1,35 @@
-package lotto;public class WinNumbers {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WinNumbers extends LottoNumber{
+    private final static Integer WIN_NUMBERS_SIZE = 6;
+    private List<Integer> winNumbers;
+
+    public WinNumbers(String[] winNumbers) {
+       super(winNumbers);
+       this.winNumbers = new ArrayList<>();
+       for(String winNumber : winNumbers) {
+           this.winNumbers
+                   .add(Integer.parseInt(winNumber));
+       }
+       validateSize(this.winNumbers);
+       validateDuplication(this.winNumbers);
+    }
+
+    private void validateSize(List<Integer> winNumbers) {
+        if(winNumbers.size() != WIN_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 입력되어야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> winNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winNumbers);
+        if(uniqueNumbers.size() != winNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호를 중복 없이 입력하셔야 합니다.");
+        }
+    }
 }

ae30f8a83d885d502311770af2b2848a2b9e0852
oosedus <seayeon0729@gmail.com>
2023-11-08 17:05:33 +0900

    refactor: 로또 번호 생성하는 함수 Util 클래스로 이동

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,5 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -17,28 +14,6 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자를 입력해야 합니다.");
         }
-    }
-
-
-    List<Integer> generateSixRandomNumber(){
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return numbers;
-    }
-
-    void sortRandomNumber(List<Integer> notSortedNumbers){
-        Collections.sort(notSortedNumbers);
-    }
-
-    List<List<Integer>> makeLottoNumbersCollection(int count){
-        List<List<Integer>> lottoNumbers = new ArrayList<>();
-        for(int i=0; i < count; i++){
-            List<Integer> randomNumbers = generateSixRandomNumber();
-            lottoNumbers.add(randomNumbers);
-        }
-        for(List<Integer> notSortedNumbers: lottoNumbers){
-             sortRandomNumber(notSortedNumbers);
-        }
-        return lottoNumbers;
     }
 
 }

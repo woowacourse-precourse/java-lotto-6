1ccc2f38adcd5c3dfc7a618ec672bf76b7ba978e
acs0209 <annjae4676@naver.com>
2023-11-08 15:23:58 +0900

    refactor: LottosController에서 필요 없는 메소드 제거

--- src/main/java/lotto/controller/LottosController.java
+++ src/main/java/lotto/controller/LottosController.java
@@ -1,29 +1,19 @@
 package lotto.controller;
 
-import static lotto.view.constant.InputMessage.GET_WINNING_NUMBERS_MESSAGE;
 
 import lotto.domain.LottoPurchase;
 import lotto.domain.Lottos;
 import lotto.service.LottosService;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottosController {
 
-    private final InputView inputView;
     private final LottosService lottosService;
 
     public LottosController() {
-        this.inputView = new InputView();
         this.lottosService = new LottosService();
     }
 
     public Lottos getLottos(LottoPurchase lottoPurchase) {
         return lottosService.createLottos(lottoPurchase);
-    }
-
-    public String getWinningNumbers() {
-        OutputView.printMessage(GET_WINNING_NUMBERS_MESSAGE.getMessage());
-        return inputView.readWinningNumbers();
     }
 }

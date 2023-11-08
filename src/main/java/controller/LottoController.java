6fd1690f8926d4cd8c12bc69e31cd949d10ca25d
kgy1008 <gayeon1008@g.skku.edu>
2023-11-08 14:54:13 +0900

    feat: 출력 처리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,6 +1,15 @@
 package controller;
 
+import view.InputPrice;
+
 public class LottoController {
+    public void run() {
+        try {
+            InputPrice.enterPurchaseAmount();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 

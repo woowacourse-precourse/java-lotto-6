51dbb0f79f8136f2c8f4aebb0562e03dd127c999
34suuuuu <psh7492@gmail.com>
2023-11-08 11:33:22 +0900

    feat: 보너스 번호가 범위내의 숫자가 아닌 경우 validation하는 메서드 추가

--- src/main/java/lotto/domain/lotto/WinningLotto.java
+++ src/main/java/lotto/domain/lotto/WinningLotto.java
@@ -2,12 +2,14 @@ package lotto.domain.lotto;
 
 import lotto.domain.rank.Rank;
 import lotto.exception.BonusNumDuplicateException;
+import lotto.exception.LottoNumRangeException;
 
 public class WinningLotto {
     private final Lotto lotto;
     private final int bonusNumber;
 
     public WinningLotto(Lotto lotto, int bonusNumber){
+        validateBonusNumRange(bonusNumber);
         validate(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
@@ -16,6 +18,12 @@ public class WinningLotto {
     private void validate(Lotto lotto, int bonusNumber) {
         if (lotto.containBonusNum(bonusNumber)) {
             throw new BonusNumDuplicateException();
+        }
+    }
+
+    private void validateBonusNumRange(int bonusNumber) {
+        if (bonusNumber <= 0 || bonusNumber > 45) {
+            throw new LottoNumRangeException();
         }
     }
 

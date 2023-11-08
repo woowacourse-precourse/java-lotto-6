8fbd397194b771abdb96228bd8470a48c298a0bc
nhn1030 <shgusska12@gmail.com>
2023-11-08 21:45:34 +0900

    feat: WinningChecker 클래스 구현 - checkWinningPrize: machingCount, hasBonusNumber를 인자로 받아 티켓 하나당 당첨 순위를 반환 - checkMachingCount: 티켓 하나에 구성된 번호와 당첨번호를 비교하여 machingCount를 반환하는 메서드 - hasBonusNumber: bonusNumber가 numbers에 포함되어있는지 확인

--- src/main/java/lotto/domain/WinningChecker.java
+++ src/main/java/lotto/domain/WinningChecker.java
@@ -1,4 +1,44 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningChecker {
+    private List<Integer> winningNumbers;
+
+    public WinningChecker(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+    public int checkWinningPrize(int matchingCount, boolean hasBonusNumber) {
+        int rank = 0;
+
+        if (matchingCount == 6) {
+            rank = Prize.WinningPrize.FIRST_RANK.getPrizeAmount();
+        } else if (matchingCount == 5 && hasBonusNumber) {
+            rank = Prize.WinningPrize.SECOND_RANK.getPrizeAmount();
+        } else if (matchingCount == 5) {
+            rank = Prize.WinningPrize.THIRD_RANK.getPrizeAmount();
+        } else if (matchingCount == 4) {
+            rank = Prize.WinningPrize.FOURTH_RANK.getPrizeAmount();
+        } else if (matchingCount == 3) {
+            rank = Prize.WinningPrize.FIFTH_RANK.getPrizeAmount();
+        }
+
+        return rank;
+    }
+
+    public int checkMachingCount(List<Integer> numbers) {
+        int machingCount = 0;
+
+        for (int number : numbers) {
+            if (winningNumbers.contains(number)) {
+                machingCount++;
+            }
+        }
+        return machingCount;
+    }
+
+    public boolean hasBonusNumber(BonusNumber bonusNumber, List<Integer> numbers) {
+        return numbers.contains(bonusNumber.getBonusNumber());
+    }
+
 }

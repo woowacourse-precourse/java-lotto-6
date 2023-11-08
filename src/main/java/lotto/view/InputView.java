535ef35911bfbd3441edebbee7e6340fdf799d85
jay <wlguswhd0809@naver.com>
2023-11-08 20:57:13 +0900

    feat: 시작 문구 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import static lotto.constant.InputViewConstant.START_MESSAGE;
+
+public class InputView {
+    public static void setStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+}

8c110bbe86a567b7aeaffd375ea6c883eac781a4
kang jihyeong <her4386@gmail.com>
2023-11-08 18:56:03 +0900

    [test] 로또 번호 숫자 유효성 검사 테스트

--- src/test/java/lotto/controller/InputValidatorControllerTest.java
+++ src/test/java/lotto/controller/InputValidatorControllerTest.java
@@ -3,7 +3,7 @@ package lotto.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static lotto.controller.InputValidatorController.inputMoneyValidate;
+import static lotto.controller.InputValidatorController.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class InputValidatorControllerTest {
@@ -26,6 +26,13 @@ public class InputValidatorControllerTest {
     @Test
     void inputMoneyValidateTest_isRightMoneyUnit() {
         assertThatThrownBy(() -> inputMoneyValidate("3000000000"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 숫자로 입력됐는지 확인")
+    @Test
+    void inputLottoNumberValidate_isAllDigit() {
+        assertThatThrownBy(() -> inputLottoNumberValidate("1,2,3,4k,5,6"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

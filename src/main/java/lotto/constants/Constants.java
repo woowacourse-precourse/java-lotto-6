1b5d1609a3da5dcad9736b9b00484a6062db08b9
dj <fjzl12@naver.com>
2023-11-08 16:54:01 +0900

    refactor(Constants): 로또 일치 개수에 대한 상수 추가

--- src/main/java/lotto/constants/Constants.java
+++ src/main/java/lotto/constants/Constants.java
@@ -1,4 +1,4 @@
-package lotto.domain;
+package lotto.constants;
 
 public class Constants {
     public static final int MIN_NUMBER = 1;
@@ -7,12 +7,14 @@ public class Constants {
     public static final int MIN_COST = 1000;
     public static final int COST_UNIT = 1000;
     public static final int ZERO = 0;
+    public static final int THREE_MATCH = 3;
+    public static final int FOUR_MATCH = 4;
+    public static final int FIVE_MATCH = 5;
+    public static final int SIX_MATCH = 6;
     public static final int HUNDRED = 100;
     public static final long FIFTH_PRIZE = 5000;
     public static final long FOURTH_PRIZE = 50000;
     public static final long THIRD_PRIZE = 1500000;
     public static final long SECOND_PRIZE = 30000000;
     public static final long FIRST_PRIZE = 2000000000;
-    public static final String PROFIT = "총 수익률은 %.1f%%입니다.\n";
-    public static final String RESULT = "당첨 통계\n---";
 }

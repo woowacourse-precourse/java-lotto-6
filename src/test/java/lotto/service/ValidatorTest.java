b718e67de07d162ad1084fdefcf7d795ea8b4120
ohjuyoungg <ejrm12@naver.com>
2023-11-08 20:33:39 +0900

    refactor: 패키지 전체 코드 리팩토링

--- src/test/java/lotto/service/ValidatorTest.java
+++ src/test/java/lotto/service/ValidatorTest.java
@@ -15,6 +15,39 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class ValidatorTest {
 
+    private static Stream<Arguments> purchaseAmountAndGet_천단위_로또구매금액_검증_실패한다() {
+        return Stream.of(
+            Arguments.of(1500),
+            Arguments.of(2550)
+        );
+    }
+
+    private static Stream<Arguments> lottoNumberAndGet_로또_번호_검증_실패한다() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 46)),
+            Arguments.of(Arrays.asList(0, 1, 2, 3, 4, 5)),
+            Arguments.of(Arrays.asList(-1, 1, -2, -3, 5)),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5)),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
+        );
+    }
+
+    private static Stream<Arguments> bonusNumberAndGet_보너스번호_검증_실패한다() {
+        return Stream.of(
+            Arguments.of("0", new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))),
+            Arguments.of(100, new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))
+        );
+    }
+
+    private static Stream<Arguments> isBlank_공백입력_검증_실패한다() {
+        return Stream.of(
+            Arguments.of(""),
+            Arguments.of(" "),
+            Arguments.of("  "),
+            Arguments.of("\t")
+        );
+    }
+
     @Test
     void purchaseAmountAndGet_천단위_로또구매금액_검증_성공한다() {
         int purchaseAmount = 2000;
@@ -30,13 +63,6 @@ public class ValidatorTest {
 
     }
 
-    private static Stream<Arguments> purchaseAmountAndGet_천단위_로또구매금액_검증_실패한다() {
-        return Stream.of(
-            Arguments.of(1500),
-            Arguments.of(2550)
-        );
-    }
-
     @Test
     void lottoNumberAndGet_로또_당첨번호_검증_성공한다() {
         List<Integer> numberStrings = List.of(1, 2, 3, 4, 5, 6);
@@ -48,16 +74,6 @@ public class ValidatorTest {
     void lottoNumberAndGet_로또_번호_검증_실패한다(List<Integer> winningNumbers) {
         assertThatThrownBy(() -> Validator.lottoNumberAndGet(winningNumbers))
             .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    private static Stream<Arguments> lottoNumberAndGet_로또_번호_검증_실패한다() {
-        return Stream.of(
-            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 46)),
-            Arguments.of(Arrays.asList(0, 1, 2, 3, 4, 5)),
-            Arguments.of(Arrays.asList(-1, 1, -2, -3, 5)),
-            Arguments.of(Arrays.asList(1, 2, 3, 4, 5)),
-            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
-        );
     }
 
     @Test
@@ -75,13 +91,6 @@ public class ValidatorTest {
             .hasMessageContaining(ExceptionMessage.INPUT_SHOULD_BE_IN_RANGE.getMessage());
     }
 
-    private static Stream<Arguments> bonusNumberAndGet_보너스번호_검증_실패한다() {
-        return Stream.of(
-            Arguments.of("0", new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))),
-            Arguments.of(100, new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))
-        );
-    }
-
     @Test
     void isBlank_공백입력_검증_성공한다() {
         String numberValue = "1";
@@ -94,14 +103,5 @@ public class ValidatorTest {
         assertThatThrownBy(() -> Validator.isBlank(numberValue))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(ExceptionMessage.INPUT_BLANK.getMessage());
-    }
-
-    private static Stream<Arguments> isBlank_공백입력_검증_실패한다() {
-        return Stream.of(
-            Arguments.of(""),
-            Arguments.of(" "),
-            Arguments.of("  "),
-            Arguments.of("\t")
-        );
     }
 }

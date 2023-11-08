b69c98f4f329d601e103d8ef44589d4472c32bd5
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 13:48:03 +0900

    feat: 입력 전용 뷰 구현
    - 숫자가 필요할 때는 타입 변경하여 반환

---
+++ src/main/java/lotto/domain/view/input/LottoInputView.java
@@ -0,0 +1,41 @@
+package lotto.domain.view.input;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.view.dto.LottoBonusNumberDto;
+import lotto.domain.view.dto.PurchasePriceDto;
+
+public class LottoInputView {
+
+    public static final String NUMBERS_REGEX = ",";
+
+    public PurchasePriceDto readPurchasePrice() {
+        String input = readLine();
+        long convertedInput = Converter.toLong(input);
+
+        return new PurchasePriceDto(convertedInput);
+    }
+
+    public LottoNumbersDto readLottoNumber() {
+        String input = readLine();
+
+        return new LottoNumbersDto(
+                parseLottoNumber(input)
+        );
+    }
+
+    private static List<Integer> parseLottoNumber(String input) {
+        return Arrays.stream(
+                        input.split(NUMBERS_REGEX)
+                )
+                .map(Converter::toInt)
+                .toList();
+    }
+
+    public LottoBonusNumberDto readLottoBonusNumber() {
+        String input = readLine();
+        return new LottoBonusNumberDto(Converter.toInt(input));
+    }
+}

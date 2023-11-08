ea2c47defa4f439cb79ed8e1e19ec320281ce76a
Hanbee Lee <bee1162@naver.com>
2023-11-08 16:21:40 +0900

    refactor : Line 최적화 진행

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,8 +1,18 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
+    @DisplayName("랜덤 로또번호 생성 성공")
+    @Test
+    void createRandomLottoNumbers() {
+        LottoMachine lottoMachine = new LottoMachine();
+        Lotto lotto = new Lotto(lottoMachine.getNumbers());
+
+        Assertions.assertThat(lotto.getLotto()).isEqualTo(lottoMachine.getNumbers());
+
+    }
 }

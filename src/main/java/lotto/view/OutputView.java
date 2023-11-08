d53ee65eea7b07a1dc52359b883b34ff064032d4
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 13:10:03 +0900

    feat(OutputView) : responseBuyingQuantity 메서드 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,15 @@
 package lotto.view;
 
+import lotto.domain.User;
+
 public class OutputView {
 
     public void askBuyingPriceView() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void responseBuyingQuantity(User user) {
+        System.out.println();
+        System.out.println(getQuantity(user) + "개를 구매했습니다.");
     }
 }

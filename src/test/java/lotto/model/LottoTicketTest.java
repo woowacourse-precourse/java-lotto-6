88e049af0275d31576e6f519be111e6f46bc4061
mdgome <mdgome3@gmail.com>
2023-11-08 14:39:32 +0900

    test: 일치하는 번호에 따라 등수가 올바르게 계산되는지
    - [X] 일치하는 번호에 따라 등수가 맞는지

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -1,4 +1,26 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LottoTicketTest {
+
+    @DisplayName("일치하는 번호에 따라 등수가 맞는지 확인")
+    @Test
+    void equalRank() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 13, 24, 35, 36));
+        Bonus bonus = new Bonus(10,winningLotto);
+        List<Lotto> ticketLottoIsOne = List.of(new Lotto(List.of(1, 2, 13, 24, 35, 10)));
+
+        LottoTicket lottoTicketTest = new LottoTicket(ticketLottoIsOne);
+
+        WinStatistics result =  lottoTicketTest.getRank(winningLotto, bonus);
+        WinStatistics expect = new WinStatistics();
+        expect.pushRank(Rank.SECOND);
+
+        assertThat(result).isEqualTo(expect);
+    }
 }

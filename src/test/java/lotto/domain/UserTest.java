20ed2cb2a45c6dc77223201a096a639f333f27b2
gimdaeyeon <rlaeodus1306@naver.com>
2023-11-08 21:33:13 +0900

    Test: 구입한 로또에 대한 결과배열 반환 메소드 테스트

--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -36,7 +36,20 @@ class UserTest {
 
     @Test
     void 구입한_로또에_대한_결과_테스트() {
+        List<Integer> lotto1 = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> lotto2 = List.of(7, 8, 9, 10, 11, 12);
+        List<Integer> lotto3 = List.of(41, 22, 11, 36, 25, 7);
 
+        List<Lotto> lottos = List.of(new Lotto(lotto1), new Lotto(lotto2), new Lotto(lotto3));
+
+        User user = new User(3000, lottos);
+
+        List<Integer> winningNumbers = List.of(1,2,3,41,32,26);
+        int bonusNumber = 31;
+        int[] result = {2,0,0,0,0,1};
+        
+        assertThat(user.getLottoResults(winningNumbers,bonusNumber))
+                .isEqualTo(result);
     }
 
 

c72ebae37e0109a099e2cd75239b65f2dfb7a9a0
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 22:57:48 +0900

    Fix : 출력 포맷 수정

---
+++ src/test/java/lotto/model/service/LottoServiceTest.java
@@ -0,0 +1,40 @@
+package lotto.model.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoServiceTest {
+
+    @Test
+    void startLotto() {
+    }
+
+    @Test
+    void inputAmount() {
+    }
+
+    @Test
+    void generateLottos() {
+    }
+
+    @Test
+    void drawProcess() {
+    }
+
+    @Test
+    void drawNumber() {
+    }
+
+    @Test
+    void drawBonus() {
+    }
+
+    @Test
+    void calculateProfit() {
+    }
+
+    @Test
+    void resultOutput() {
+    }
+}

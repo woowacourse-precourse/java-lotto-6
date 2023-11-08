1f9c25b054d9131978a6a13b66463573c6e5109f
권장순 <jsoonworld@gmail.com>
2023-11-08 19:52:37 +0900

    test( LottoRankTest#shouldThrowExceptionForInvalidMatchingCount() ) : 해당하는 LottoRank가 없는 매치 카운트에 대해 IllegalArgumentException을 발생시켜야 함.

--- src/test/java/lotto/v3/model/LottoRankTest.java
+++ src/test/java/lotto/v3/model/LottoRankTest.java
@@ -23,4 +23,31 @@ class LottoRankTest {
         assertThat(result).isEqualTo(LottoRank.THIRD);
     }
 
+    @Test
+    @DisplayName("보너스 번호가 포함된 일치하는 개수에 대한 올바른 로또 순위를 반환해야 함")
+    void shouldReturnCorrectLottoRankForMatchingCountsWithBonus() {
+        // given
+        int matchCountForSecond = 5;
+        boolean bonusMatchForSecond = true;
+
+        // when
+        LottoRank result = LottoRank.valueOf(matchCountForSecond, bonusMatchForSecond);
+
+        // then
+        assertThat(result).isEqualTo(LottoRank.SECOND);
+    }
+
+    @Test
+    @DisplayName("해당하는 LottoRank가 없는 매치 카운트에 대해 IllegalArgumentException을 발생시켜야 함")
+    void shouldThrowExceptionForInvalidMatchingCount() {
+        // given
+        int invalidMatchCount = 2;
+        boolean bonusMatch = false;
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoRank.valueOf(invalidMatchCount, bonusMatch))
+                .withMessageContaining("[ERROR] 일치하는 로또 순위가 없습니다.");
+    }
+
 }

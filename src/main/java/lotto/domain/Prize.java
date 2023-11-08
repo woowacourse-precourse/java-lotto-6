24095b785477017f54221803810249bf274ae30f
nahyeon99 <nahyeonee99@gmail.com>
2023-11-08 12:44:20 +0900

    refactor(domain/Prize): 당첨 관리 이넘 클래스 기능 추가 및 코드 정리
    1. public valueOf() : 매칭 개수와 보너스 번호을 받아서 2,3등까지 정확하게 반환하도록 변경
    2. int 타입으로 저장하는 등수(rank) 필드 제거

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,38 +1,55 @@
 package lotto.domain;
 
 public enum Prize {
-    FIRST(1, 6,  2_000_000_000),
-    SECOND(2, 5, 30_000_000),
-    THIRD(3, 5, 1_500_000),
-    FORTH(4, 4, 50_000),
-    FIFTH(5, 3, 5_000),
-    NONE(6, 0, 0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FORTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
 
-    private final int rank;
+    private static final String MATCH_COUNT_ERROR = "[ERROR] %d는 유효하지 않는 값입니다.";
+
+    private static final int MIN_WINNING_COUNT = 3;
+
     private final int matchingCount;
     private final int winningAmount;
 
-    Prize(int rank, int matchingCount, int winningAmount) {
-        this.rank = rank;
+    Prize(int matchingCount, int winningAmount) {
         this.matchingCount = matchingCount;
         this.winningAmount = winningAmount;
     }
 
-    public static Prize matchingCountOf(int matchingCount) {
-        return switch (matchingCount) {
-            case 6 -> Prize.FIRST;
-            case 5 -> Prize.THIRD;
-            case 4 -> Prize.FORTH;
-            case 3 -> Prize.FIFTH;
-            default -> Prize.NONE;
-        };
+    public static Prize valueOf(int matchingCount, boolean matchBonus) {
+        if (matchingCount < MIN_WINNING_COUNT) {
+            return NONE;
+        }
+
+        Prize prize = valueOf(matchingCount);
+        if (SECOND.matchingCount == matchingCount && matchBonus) {
+            return SECOND;
+        }
+        return prize;
+    }
+
+    private static Prize valueOf(int matchingCount) {
+        for (Prize prize : values()) {
+            if (prize.matchingCount == matchingCount) {
+                return prize;
+            }
+        }
+        throw new IllegalArgumentException(MATCH_COUNT_ERROR);
     }
 
     public int getMatchingCount() {
-        return matchingCount;
+        return this.matchingCount;
     }
 
     public int getWinningAmount() {
-        return winningAmount;
+        return this.winningAmount;
+    }
+
+    public long calculateAmount(int winningCount) {
+        return (long) this.winningAmount * winningCount;
     }
 }

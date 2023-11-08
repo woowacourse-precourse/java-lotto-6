b71c2ab001a648eea1c237acaf59b143864677a0
황준혁 <hyug0819@gmail.com>
2023-11-08 21:21:36 +0900

    refactor : 사용하지 않는 import 제거

--- src/main/java/lotto/lotto/model/Lotto.java
+++ src/main/java/lotto/lotto/model/Lotto.java
@@ -2,7 +2,6 @@ package lotto.lotto.model;
 
 import static lotto.lotto.model.LottoConstant.*;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

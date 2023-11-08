85d4c0fbaf722b4fca2f7146a935be124a6a3dfc
win9 <krrr883@gmail.com>
2023-11-08 21:19:10 +0900

    [Refactor]: 정규식 상수변환
    정규식 상수로 표현

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -4,14 +4,15 @@ import type.ErrorType;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Parser {
 
     public static final String COMMA = ",";
+    public static final String INT_REGEX = "^[0-9]+$";
+    public static final String COMMA_REGEX = "^(\\d{1,2},){5}\\d{1,2}$";
 
     public static int convertNumberInput(String input) {
-        if (!input.matches("^[0-9]+$")) {
+        if (!input.matches(INT_REGEX)) {
             System.out.println(ErrorType.INVALID_NUMBER_INPUT.getText());
             throw new IllegalArgumentException();
         }
@@ -19,7 +20,7 @@ public class Parser {
     }
 
     public static List<Integer> convertWinningNumberInput(String input) {
-        if (!input.matches("^(\\d{1,2},){5}\\d{1,2}$")) {
+        if (!input.matches(COMMA_REGEX)) {
             System.out.println(ErrorType.INVALID_WINNING_NUMBER_INPUT.getText());
             throw new IllegalArgumentException();
         }

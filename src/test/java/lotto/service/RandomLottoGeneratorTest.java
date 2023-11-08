99234b1e1dd30ab21609cdd155e0cf20271595bd
thisfetch1591 <thisfetch1591@gmail.com>
2023-11-08 23:17:49 +0900

    test: 랜덤 로또 생성기 실행 테스트
    - 적절한 가격을 입력하면 로또가 알맞게 잘 실행되는지 테스트 작성후 테스트 완료

--- src/test/java/lotto/service/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/service/RandomLottoGeneratorTest.java
@@ -2,6 +2,26 @@ package lotto.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import lotto.domain.Lottos;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RandomLottoGeneratorTest {
 
+    private int purchaseAmount;
+
+    @BeforeEach
+    void init() {
+        purchaseAmount = 3000;
+    }
+
+    @DisplayName("난수가 정상적으로 갯수에 맞게 잘 저장된다")
+    @Test
+    void createRandomLottoGeneratorAndCorrectCount() {
+        RandomLottoGenerator randomLottoGenerator = new RandomLottoGenerator(purchaseAmount);
+        Lottos lottos = randomLottoGenerator.generate();
+
+        assertEquals(3, lottos.getLength());
+    }
 }

ae96288c0d35db8f9c389bee2a2c16ddcfe9a00a
ehdwoKIM <kinbell19@gmail.com>
2023-11-08 20:33:20 +0900

    [ADD] 로또 티켓만큼 번호 출력하는 로직

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
+import java.util.ArrayList;
 public class Customer {
     private int purchaseMoney;
+    private ArrayList<Lotto> purchaseLotteries = new ArrayList<>();
 
     public Customer(int purchaseMoney) {
         this.purchaseMoney = purchaseMoney;
@@ -9,5 +11,13 @@ public class Customer {
 
     public int getPurchaseMoney() {
         return this.purchaseMoney;
+    }
+
+    public void buyLotto(Lotto lotto) {
+        purchaseLotteries.add(lotto);
+    }
+
+    public ArrayList<Lotto> getPurchaseLotteries() {
+        return purchaseLotteries;
     }
 }
--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputMessage {
+
+    public static void lottoNumber(List<Integer> number) {
+        System.out.println(number);
+    }
 }

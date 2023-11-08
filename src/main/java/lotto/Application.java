d6db4170d9aa7a23340e9bd19a1acdcd78d448d1
주은 <00-10-21@naver.com>
2023-11-08 17:27:20 +0900

    feat: 메인 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,12 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoPurchase lottoPurchase = new LottoPurchase();
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        LottoMachine lottoMachine = new LottoMachine(lottoNumberGenerator);
+
+        int purchaseAmount = lottoPurchase.requestPurchaseAmount();
+        lottoMachine.printLottoTickets(lottoMachine.issueLottoTickets(purchaseAmount));
+
     }
 }

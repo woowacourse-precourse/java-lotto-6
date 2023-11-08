4c01e3596aae4a9605e8f04b9bdfe90e71297c4d
devhyeon0 <devhyeon0@gmail.com>
2023-11-08 15:58:01 +0900

    feat: 상수 및 메시지 설정

---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class Constants {
+
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LUCKY_NUMBER_COUNT = 6;
+    public static final int LOTTO_AMOUNT = 1000;
+}

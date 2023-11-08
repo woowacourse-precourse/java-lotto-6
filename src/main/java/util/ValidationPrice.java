db5e9851618155a9d28583f142d4e5022aef9fbe
junseoks-99 <angec5645@naver.com>
2023-11-08 19:38:05 +0900

    feat: 구입 금액 1000단위 숫자 여부 검증 기능 추가

--- src/main/java/util/ValidationPrice.java
+++ src/main/java/util/ValidationPrice.java
@@ -2,12 +2,16 @@ package util;
 
 public class ValidationPrice {
 
+    private static final Integer UNIT_NUMBER = 1000;
+    private static final Integer VALID_REMAINDER = 0;
+
     private ValidationPrice() {
 
     }
 
-    public static void validatePrice(String price) {
-        validateNumber(price);
+    public static void validatePrice(String priceString) {
+        validateNumber(priceString);
+        validateRemainder(priceString);
     }
 
     public static void validateNumber(String priceString) {
@@ -15,6 +19,13 @@ public class ValidationPrice {
             Integer price = Integer.parseInt(priceString);
         } catch (NumberFormatException notANumber) {
             throw new IllegalArgumentException(ValidationPriceMessages.NOT_A_NUMBER.getMessage());
+        }
+    }
+
+    public static void validateRemainder(String priceString) {
+        Integer price = Integer.parseInt(priceString);
+        if (price % UNIT_NUMBER != VALID_REMAINDER) {
+            throw new IllegalArgumentException(ValidationPriceMessages.INVALID_REMAINDER.getMessage());
         }
     }
 }
--- src/main/java/util/ValidationPriceMessages.java
+++ src/main/java/util/ValidationPriceMessages.java
@@ -2,7 +2,8 @@ package util;
 
 public enum ValidationPriceMessages {
 
-    NOT_A_NUMBER("숫자를 입력해주세요.");
+    NOT_A_NUMBER("숫자를 입력해주세요."),
+    INVALID_REMAINDER("[ERROR] 숫자는 1000단위로 입력해야 합니다.");
 
     private String message;
 

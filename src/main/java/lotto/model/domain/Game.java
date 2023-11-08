0b8c65486a1984463c628f561f37909daaf42943
hyeongjun im <limbs713@naver.com>
2023-11-08 10:16:04 +0900

    :sparkles: 당첨 번호 오름차순 정렬 기능 추가

--- src/main/java/lotto/model/domain/Game.java
+++ src/main/java/lotto/model/domain/Game.java
@@ -1,5 +1,6 @@
 package lotto.model.domain;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Game {
@@ -7,8 +8,20 @@ public class Game {
     private final int bonusNumber;
 
     public Game(List<Integer> winningNumbers, int bonusNumber) {
+        if (!isSortedWinningNumber(winningNumbers)) {
+            winningNumbers.sort(Comparator.naturalOrder());
+        }
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private boolean isSortedWinningNumber(List<Integer> winningNumbers) {
+        for (int i = 0; i < winningNumbers.size() - 1; i++) {
+            if (winningNumbers.get(i) > winningNumbers.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public List<Integer> getWinningNumbers() {

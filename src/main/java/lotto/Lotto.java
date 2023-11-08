a86c40bcd58be2382a075b16ff523faa4c5d773a
yoojua <132237809+yoojua@users.noreply.github.com>
2023-11-08 21:55:32 +0900

    feat: 로또 번호 생성, 비교, 정렬

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,53 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        this.numbers = new ArrayList<>(numbers);
         validate(numbers);
-        this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호의 개수는 정확히 6개여야 합니다.");
+        }
+
+        if (hasDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있으면 안됩니다.");
+        }
+
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicateNumbers(List<Integer> numbers) {
+        List<Integer> uniqueNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (uniqueNumbers.contains(number)) {
+                return true; // 중복된 숫자가 있는 경우
+            }
+            uniqueNumbers.add(number);
+        }
+        return false;
+    }
+
+    public int getMatchCount(List<Integer> winningNumbers) {
+        return (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    public boolean contains(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

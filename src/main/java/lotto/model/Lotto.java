af7029340918d3436958a40cad4a54c3709bf817
Lee Geun Woo <gamebdy@naver.com>
2023-11-08 22:41:59 +0900

    :sparkles: 예외처리 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -17,18 +17,28 @@ public class Lotto {
     //여기에 validate 이 있어야할 이유가 있나? 따로 class 하는게 나을 것 같은데
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_COUNT) {
-            throw new IllegalArgumentException();
-        }
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개여야 합니다.");
+        }//코드의 형태를 유지해야하나 싶어 method 를 따로 생성하지 않음
         for(int number: numbers){
-            if(MIN_NUMBER <= number && number <= MAX_NUMBER)
-                throw new IllegalArgumentException();
+            if(MIN_NUMBER > number || number > MAX_NUMBER)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        checkDuplicate(numbers);
+    }
 
+
+    private static void checkDuplicate(List<Integer> numbers){
+        if(numbers.stream().distinct().count() != LOTTO_COUNT){
+            throw new IllegalArgumentException("[ERROR] 숫자가 중복되지 않아야합니다.");
         }
     }
-    //여기에 추가해야할 이유가 있나..?
 
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
 
-
-
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
 

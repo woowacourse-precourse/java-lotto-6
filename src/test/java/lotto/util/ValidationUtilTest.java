cf8e3d1c71581920bcfcfb6ce6632a7d699e8455
Yang woosung <ngxk1217@gmail.com>
2023-11-08 20:39:59 +0900

    구매 금액이 1000으로 나누어떨어지는지 검증하는 테스트 코드를 validationuriltest에 추가하였습니다.

--- src/test/java/lotto/util/ValidationUtilTest.java
+++ src/test/java/lotto/util/ValidationUtilTest.java
@@ -35,4 +35,30 @@ class ValidationUtilTest {
                     .hasMessageContaining(PURCHASE_TYPE);
         }
     }
+
+    @Test
+    @DisplayName("구매 금액이 1000 단위로 나누어 떨어지는지 테스트")
+    void 구매_금액_나머지_테스트() throws Exception {
+        //given
+        ValidationUtil validationUtil = new ValidationUtil();
+        Method method = validationUtil.getClass().getDeclaredMethod("validateRemainder", String.class);
+        method.setAccessible(true);
+
+        //when
+        String correctAmount = "3000";
+        String wrongAmount = "1234";
+
+        //then
+        int purchaseAmount = (int) method.invoke(validationUtil, correctAmount);
+        assertThat(purchaseAmount).isEqualTo(3000);
+
+        try {
+            method.invoke(validationUtil, wrongAmount);
+        } catch (InvocationTargetException e) {
+            assertThat(e.getTargetException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(PURCHASE_REMAINDER);
+        }
+    }
+
 }

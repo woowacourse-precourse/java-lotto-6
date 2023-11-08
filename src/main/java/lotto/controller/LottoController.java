b9c069e9fae429c61ce53791867f595000fd4ec7
Jeonwoojae <dnwo0719@icloud.com>
2023-11-08 19:46:29 +0900

    refactor: 로또 결과 생성 로직 서비스단으로 변경
    - 컨트롤러에서는 비즈니스 로직을 수행하지 않는다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,56 +4,38 @@ import static lotto.constants.Message.INPUT_BONUS_NUMBER;
 import static lotto.constants.Message.INPUT_MONEY;
 import static lotto.constants.Message.INPUT_WINNING_NUMBER;
 
-import java.util.ArrayList;
-import java.util.List;
-import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
 import lotto.domain.Result;
 import lotto.domain.WinningCondition;
-import lotto.dto.LottoCompareResult;
+import lotto.service.LottoService;
 import lotto.view.ConsoleInputReader;
 import lotto.view.OutputView;
 
 public class LottoController {
     private ConsoleInputReader inputReader = new ConsoleInputReader();
     private OutputView outputView = new OutputView();
+    private LottoMachine lottoMachine = new LottoMachine();
+    private LottoService lottoService = new LottoService();
 
     public void run() {
-        LottoMachine lottoMachine = new LottoMachine();
-
         // 구입 금액 입력
         insertMoneyTo(lottoMachine);
 
         // 로또 생성
         lottoMachine.generateLottos();
-        outputView.printGeneratedLottos(lottoMachine);
+        outputView.printBoughtLottos(lottoMachine);
 
         // 정답 입력
         WinningCondition winningCondition = generateWinningCondition();
 
         // 결과 출력
-        Result result = checkLottoResult(lottoMachine, winningCondition);
+        Result result = lottoService.calculateResult(lottoMachine, winningCondition);
         outputView.printLottoResult(result);
-    }
-
-    private Result checkLottoResult(LottoMachine lottoMachine, WinningCondition winningCondition) {
-        Result result = new Result();
-        List<LottoCompareResult> lottoCompareResultList = new ArrayList<>();
-        for (Lotto lotto : lottoMachine.getLottos()) {
-            LottoCompareResult compareResult = winningCondition.compare(lotto);
-            lottoCompareResultList.add(compareResult);
-        }
-
-        result.calculateTotalWinningMoney(lottoCompareResultList);
-        result.calculateProfitRate(lottoMachine.getInputAmount());
-
-        return result;
     }
 
     private WinningCondition generateWinningCondition() {
         WinningCondition winningCondition = new WinningCondition();
-        try
-        {
+        try {
             String winningNumber = inputReader.readInput(INPUT_WINNING_NUMBER);
             winningCondition.inputWinningNumbers(winningNumber);
 
@@ -66,7 +48,7 @@ public class LottoController {
     }
 
     private void insertMoneyTo(LottoMachine lottoMachine) {
-        try{
+        try {
             String inputMoney = inputReader.readInput(INPUT_MONEY);
             lottoMachine.insertMoney(inputMoney);
         } catch (RuntimeException e) {

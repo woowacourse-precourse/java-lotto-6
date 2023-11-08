1cdcb45e10dd36dfb143030957bdebb59868e8e0
Jihobbit8677 <61215390+Jihobbit8677@users.noreply.github.com>
2023-11-08 17:21:58 +0900

    feat(Print - printRateOfReturn()): 수익률 출력함수 구현 (main()에서 별도의 클래스 & 메소드 분리)

--- src/main/java/lotto/Print.java
+++ src/main/java/lotto/Print.java
@@ -5,26 +5,33 @@ import java.util.List;
 
 public class Print {
 
-    public void printLottoCount(int purchaseCount){
+    public void printLottoCount(int purchaseCount) {
         System.out.printf("%d개를 구매했습니다.%n", purchaseCount);
     }
 
 
     public void printLottos(List<List<Integer>> lottos) {
-        List<Integer> lottoNumbers = null;
-        List<Integer> eachLottoNumber = null;
-
         for (var lotto : lottos) {
-//            eachLottoNumber = lotto.getLottoNumbers();
             System.out.println(lotto);
         }
     }
 
 
-    public void printWinStatistic(){
-        for (WinningRankPrize rankPlace : WinningRankPrize.values()){
+    public void printWinStatistic() {
+        for (WinningRankPrize rankPlace : WinningRankPrize.values()) {
             System.out.printf(rankPlace.getMatchCount() + " (%s원) - %d개%n", rankPlace.getPrizeAmount(), rankPlace.getWinCount());
         }
     }
+
+
+    public void printRateOfReturn(int purchaseAmount) {
+        float rateOfReturn;
+        Process endProcess = new Process();
+
+        rateOfReturn = endProcess.calculateRateOfReturn(purchaseAmount);
+
+        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
+    }
+
 
 }

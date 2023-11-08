f65dd5710dcfb78ebab9e13b78c74f381429381d
eora21 <eora21@naver.com>
2023-11-08 13:19:20 +0900

    test: 💍 수정 로또 당첨 번호 생성 시 당첨 번호와 보너스번호 객체의 조합으로 만들도록 반영

--- src/test/java/lotto/domain/LottoWinningNumbersTest.java
+++ src/test/java/lotto/domain/LottoWinningNumbersTest.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import domain.BonusNumber;
 import domain.LottoWinningNumbers;
+import domain.WinningNumbers;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,21 +17,26 @@ public class LottoWinningNumbersTest {
     @DisplayName("로또 당첨 번호의 개수가 6개를 넘지 못 하면 예외가 발생한다.")
     void createLottoWinningNumbersByLessSize() {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5), 7));
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5)), BonusNumber.createBonusNumber(7)));
     }
 
     @Test
     @DisplayName("로또 당첨 번호의 개수가 6개를 넘으면 예외가 발생한다.")
     void createLottoWinningNumbersByOverSize() {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7), 8));
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7)),
+                        BonusNumber.createBonusNumber(8)));
     }
 
     @Test
     @DisplayName("중복된 로또 당첨 번호가 있다면 예외가 발생한다.")
     void createLottoWinningNumbersByDuplicateNumber() {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 5), 7));
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 5)),
+                        BonusNumber.createBonusNumber(7)));
     }
 
     @ParameterizedTest
@@ -37,14 +44,18 @@ public class LottoWinningNumbersTest {
     @ValueSource(ints = {-1, 0, 46})
     void createLottoWinningNumbersByOutOfRange(int outOfRangeNumber) {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, outOfRangeNumber), 7));
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, outOfRangeNumber)),
+                        BonusNumber.createBonusNumber(7)));
     }
 
     @Test
     @DisplayName("보너스 번호가 로또 당첨 번호와 중복된다면 예외가 발생한다.")
     void createLottoWinningNumbersByDuplicateBonusNumber() {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6));
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6)),
+                        BonusNumber.createBonusNumber(6)));
     }
 
     @ParameterizedTest
@@ -52,12 +63,17 @@ public class LottoWinningNumbersTest {
     @ValueSource(ints = {-1, 0, 46})
     void createLottoWinningNumbersByOutOfRangeBonusNumber(int outOfRangeNumber) {
         assertThrows(IllegalArgumentException.class,
-                () -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6), outOfRangeNumber));
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6)),
+                        BonusNumber.createBonusNumber(outOfRangeNumber)));
     }
 
     @Test
     @DisplayName("로또 당첨 번호 생성")
     void createLottoWinningNumbers() {
-        assertDoesNotThrow(() -> LottoWinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7));
+        assertDoesNotThrow(
+                () -> LottoWinningNumbers.createWinningNumbers(
+                        WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6)),
+                        BonusNumber.createBonusNumber(7)));
     }
 }

552c4d5197163c0e4d97f6be60f86d71c0bd7430
ddohyeong <dohyeong7026@gmail.com>
2023-11-08 10:34:54 +0900

    feat(Application) : 로또 게임 시작 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import controller.LottoStoreController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		LottoStoreController lottoStoreController = new LottoStoreController();
+		lottoStoreController.runLottoGame();
+	}
 }

4a2b256bb42223e0cc99af733ff51da3f4e85007
gyuoo <yeomky98@naver.com>
2023-11-08 23:43:10 +0900

    feat: implement lotto game logic

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -8,9 +8,30 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import util.UserInput;
 import view.OutputView;
 
 public class LottoController {
+    public static void runLottoGame() {
+        long userPayment = UserInput.getUserPayment();
+        long ticketsCount = Lotto.countLottoTickets(userPayment);
+        OutputView.printNumberOfLottoTickets(userPayment);
+        List<Lotto> totalLottos = Lotto.generateTotalLottoList(ticketsCount);
+        printAllLottoTickets(totalLottos);
+        Lotto winningNumbers = UserInput.getWinningNumbers();
+        double earningsRate = Result.calculateEarningsRate(userPayment);
+        int bonusNumber = UserInput.getBonusNumber(winningNumbers);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        System.out.println(Result.calculatePrize());
+        Result result = calculateResult(totalLottos, winningLotto);
+        printResult(result, earningsRate);
+    }
+
+    private static void printAllLottoTickets(List<Lotto> totalLottos) {
+        for (Lotto lotto : totalLottos) {
+            OutputView.printLottoTickets(lotto);
+        }
+    }
 
     private static Result calculateResult(List<Lotto> totalLottos, WinningLotto winningLotto) {
         Map<Rank, Integer> result = new HashMap<>();

614c5876da0442954e1f9e91e8538383fef8c7f1
HyunmoBae <tsi0520@naver.com>
2023-11-08 12:41:34 +0900

    test(lotto) : add compareAllLottoTicket test

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -17,28 +17,24 @@ class LottoResultTest {
 
     @Test
     void compareLottoNumberTest() {
-
         int result = lottoResult.compareLottoNumber(List.of(1, 2, 3, 4, 5, 6, 7), List.of(1, 2, 3, 7, 8, 9));
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void compareBonusNumberTest() {
-
         boolean result = lottoResult.compareBonusNumber(List.of(1, 2, 3, 4, 5, 6, 7), List.of(1, 2, 5, 6, 7, 8));
         assertThat(result).isEqualTo(true);
     }
 
     @Test
     void compareBonusNumberTest2() {
-
         boolean result = lottoResult.compareBonusNumber(List.of(1, 2, 3, 4, 5, 6, 7), List.of(1, 2, 4, 5, 6, 8));
         assertThat(result).isEqualTo(false);
     }
 
     @Test
     void getLottoRankTest() {
-
         lottoResult.getLottoRank(3, false);
         lottoResult.getLottoRank(5, true);
         lottoResult.getLottoRank(5, false);
@@ -55,10 +51,19 @@ class LottoResultTest {
 
     @Test
     void compareLottoTicketTest() {
-
         lottoResult.compareLottoTicket(List.of(2, 3, 4, 5, 6, 7, 8), List.of(1, 2, 4, 5, 6, 8));
         int result = lottoResult.fourth;
         assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void compareAllLottoTicketTest() {
+        LottoTicket lottoTicket1 = new LottoTicket(List.of(3,4,5,6,7,8));
+        LottoTicket lottoTicket2 = new LottoTicket(List.of(4,5,6,7,8,9));
+
+        lottoResult.compareAllLottoTicket(List.of(1,2,3,4,5,6,7),List.of(lottoTicket1,lottoTicket2));
+        assertThat(lottoResult.fourth).isEqualTo(1);
+        assertThat(lottoResult.fifth).isEqualTo(1);
     }
 
 }

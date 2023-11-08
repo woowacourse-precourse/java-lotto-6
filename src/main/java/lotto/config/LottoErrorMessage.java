b711f535e45ad401d23eade3700e412527d9212a
gywns0417 <gywns0417@gmail.com>
2023-11-08 18:23:42 +0900

    feat: 로또 클래스 내부에 중복 숫자 검증 메서드 추가 구현 완료

--- src/main/java/lotto/config/LottoErrorMessage.java
+++ src/main/java/lotto/config/LottoErrorMessage.java
@@ -4,7 +4,9 @@ import static lotto.config.LottoConfig.LOTTO_NUMBER_AMOUNT_MAX;
 
 public enum LottoErrorMessage {
     LOTTO_AMOUNT_MAX_ERROR_MESSAGE(String.format("[ERROR] 로또 번호는 %s개를 입력해야 합니다.",
-            LOTTO_NUMBER_AMOUNT_MAX.getValue()));
+            LOTTO_NUMBER_AMOUNT_MAX.getValue())),
+
+    LOTTO_UNIQUE_ERROR_MESSAGE("[ERROR] 로또에 중복된 번호가 존재합니다.");
 
     private final String message;
 
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,10 +2,13 @@ package lotto.domain;
 
 import static lotto.config.LottoConfig.LOTTO_NUMBER_AMOUNT_MAX;
 import static lotto.config.LottoErrorMessage.LOTTO_AMOUNT_MAX_ERROR_MESSAGE;
+import static lotto.config.LottoErrorMessage.LOTTO_UNIQUE_ERROR_MESSAGE;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,8 +19,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
         if (numbers.size() != LOTTO_NUMBER_AMOUNT_MAX.getValue()) {
             throw new IllegalArgumentException(LOTTO_AMOUNT_MAX_ERROR_MESSAGE.getMessage());
+        }
+
+        for (int number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException(LOTTO_UNIQUE_ERROR_MESSAGE.getMessage());
+            }
         }
     }
 

20ddc628d5ccea436742320879dd796664b07491
Kim myeong-hwi <sjkl6206s@gmail.com>
2023-11-08 10:23:11 +0900

    feat(Lotto): 실제로 해당 번호가 있는지 확인하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,25 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers = sorted(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> sorted(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public boolean isContain(int number) {
+        return numbers.contains(number);
     }
 
     private void validate(List<Integer> numbers) {

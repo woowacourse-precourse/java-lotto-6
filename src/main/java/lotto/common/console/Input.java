cc9fdd3b21b1623880109bd2beed753799cfa007
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-08 19:17:24 +0900

    feat: read bonus number

--- src/main/java/lotto/common/console/Input.java
+++ src/main/java/lotto/common/console/Input.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public static final String READ_PURCHASE_AMOUNT = "구입금액을 입력해주세요.";
     public static final String READ_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String READ_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static String readPurchaseAmount() {
         Output.writeMessage(READ_PURCHASE_AMOUNT);
@@ -12,9 +13,14 @@ public class Input {
         return Console.readLine();
     }
 
-
     public static String readLottoNumbers() {
         Output.writeMessage(READ_LOTTO_NUMBERS);
+
+        return Console.readLine();
+    }
+
+    public static String readBonusNumber() {
+        Output.writeMessage(READ_BONUS_NUMBER);
 
         return Console.readLine();
     }

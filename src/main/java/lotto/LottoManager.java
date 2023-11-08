3046ba37111118522aaef7d8a453eb23b592a477
choish19 <100904838+choish19@users.noreply.github.com>
2023-11-08 22:12:43 +0900

    Feat: "추가 inputLottoWinningNumbers() 함수"
    LottoManger 클래스
    - inputLottoWinningNumbers() 함수 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class LottoManager {
     private int purchaseAmount;
     private List<Lotto> lotties;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
 
     void inputLottoPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -22,5 +24,15 @@ public class LottoManager {
             List<Integer> numbers = numberGenerator.createRandomNumbers();
             lotties.add(new Lotto(numbers));
         }
+    }
+
+    private void inputLottoWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] numbers = Console.readLine().split(",");
+        for (String number : numbers) {
+            this.winningNumbers.add(Integer.parseInt(number));
+        }
+        System.out.println("보너스 번호를 입력해 주세요.");
+        this.bonusNumber = Integer.parseInt(Console.readLine());
     }
 }

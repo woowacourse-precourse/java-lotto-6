5a2e3cc6bb86ea06346f8742cb155cbda0d23ca2
kmw2378 <rlarla677@gmail.com>
2023-11-08 11:48:54 +0900

    feat(lotto): 보너스 번호 검증 관련 예외 메시지 추가 보너스 번호 범위, 숫자 여부를 검증 실패시 사용되는 예외 메시지를 추가하였습니다.

--- src/main/java/lotto/exception/errorcode/InputErrorCode.java
+++ src/main/java/lotto/exception/errorcode/InputErrorCode.java
@@ -3,11 +3,12 @@ package lotto.exception.errorcode;
 import static lotto.exception.errorcode.ErrorCodeConstant.ERROR_MESSAGE_PREFIX;
 
 public enum InputErrorCode {
-
     INVALID_LOTTO_NUMBER_RANGE(ERROR_MESSAGE_PREFIX + "로또 번호의 범위는 1 ~ 45 사이여야 합니다."),
     INVALID_LOTTO_NUMBER_SIZE(ERROR_MESSAGE_PREFIX + "로또 번호는 총 6개여야 합니다."),
     INVALID_LOTTO_PURCHASE_PRICE(ERROR_MESSAGE_PREFIX + "로또 구매 금액은 1,000원 단위여야 합니다."),
-    DUPLICATED_LOTTO_NUMBER(ERROR_MESSAGE_PREFIX + "로또 번호는 중복이 아니여야 합니다.");
+    DUPLICATED_LOTTO_NUMBER(ERROR_MESSAGE_PREFIX + "로또 번호는 중복이 아니여야 합니다."),
+    INVALID_BONUS_NUMBER(ERROR_MESSAGE_PREFIX + "보너스 번호는 숫자이여야 합니다."),
+    INVALID_BONUS_NUMBER_RANGE(ERROR_MESSAGE_PREFIX + "보너스 번호의 범위는 1 ~ 45 사이여야 합니다.");
 
     private final String message;
 

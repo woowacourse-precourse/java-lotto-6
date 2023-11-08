f0f335cf16b399211515e82aeef5079a84e5c4e8
SungyoonRyu <rara0620@naver.com>
2023-11-08 20:18:36 +0900

    fix(domain): fix exception error by adding validation code

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -7,7 +7,13 @@ public class WinningLotto extends Lotto {
 
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         super(numbers);
+        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        if (super.getNumbers().contains(bonusNumber))
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 달라야 한다.");
     }
 
     public int getBonusNumber() {

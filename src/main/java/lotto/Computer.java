a7bf926bbd9c89aea1d3a8f3cb50158586324319
hjhyun98 <hyunjo722@naver.com>
2023-11-08 19:16:44 +0900

    [refactor] 구입금액 변수명 변경

--- src/main/java/lotto/Computer.java
+++ src/main/java/lotto/Computer.java
@@ -1,27 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Computer {
     final String ASK_PRICE = "구입금액을 입력해 주세요.";
     final String ONLY_NUMBER_ERROR_MESSAGE = "[ERROR] 구입 금액은 숫자만 가능합니다.";
-    final String PRICE_MUST_DIVIDED_THOUSAND = "[ERROR] 금액은 1000원 단위로 나누어 떨어져야합니다."
+    final String AMOUNT_OF_MONEY_MUST_DIVIDED_THOUSAND = "[ERROR] 금액은 1000원 단위로 나누어 떨어져야합니다.";
     String getPriceFromUser(){
         System.out.println(ASK_PRICE);
 
-        return camp.nextstep.edu.missionutils.Console.readLine();
+        return Console.readLine();
     }
 
     void validatePrice(String input){
-        int price = 1;
+        int amount = 1;
 
         try{
-            price = Integer.parseInt(input);
+            amount = Integer.parseInt(input);
         } catch (IllegalArgumentException e){
             System.out.println(ONLY_NUMBER_ERROR_MESSAGE);
         }
 
 
-        if((price % 1000) != 0){
-            throw new IllegalArgumentException(PRICE_MUST_DIVIDED_THOUSAND);
+        if((amount % 1000) != 0){
+            throw new IllegalArgumentException(AMOUNT_OF_MONEY_MUST_DIVIDED_THOUSAND);
         }
     }
+
+    int changeInputToInt(String input){
+        return Integer.parseInt(input);
+    }
+
+//    List<Lotto> buyLotto(int price){
+//
+//    }
 }

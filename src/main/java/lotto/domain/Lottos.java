c128bba1f2c72ed18539e680e315d6779433b8f6
dmong <dt2980_hee@naver.com>
2023-11-08 10:28:51 +0900

    feat: Lottos에 주어진 수량만큼 랜덤 로또를 생성하는 팩토리 메소드, getter 작성

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,28 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(int quantity) {
+        this.lottos = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+            lottos.add(Lotto.createRandomLotto());
+        }
+    }
+
+
+    public static Lottos createRandomLottos(int quantity) {
+        return new Lottos(quantity);
+    }
+
+
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+
 }

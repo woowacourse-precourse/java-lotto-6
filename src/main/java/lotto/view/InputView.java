d508f8e9ef24f8d408233d59ccfdfad09fc1e43c
HanDaWoon <bookpia.han@gmail.com>
2023-11-08 14:45:22 +0900

    feat: 입력 뷰에서 자체적인 검증 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,34 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.enums.ErrorMsg;
 import lotto.enums.InputMsg;
 
 public class InputView {
-    public static String purchaseAmount() {
+    public String purchaseAmount() {
         System.out.println(InputMsg.PURCHASE_AMOUNT.getMsg());
-        return Console.readLine();
+        return input();
     }
 
-    public static String winningNumber() {
+    public String winningNumber() {
         System.out.println(InputMsg.WINNING_NUMBER.getMsg());
-        return Console.readLine();
+        return input();
     }
 
-    public static String bonusNumber() {
+    public String bonusNumber() {
         System.out.println(InputMsg.BONUS_NUMBER.getMsg());
-        return Console.readLine();
+        return input();
+    }
+
+    private String input() {
+        String input = Console.readLine();
+        validate(input);
+        return input;
+    }
+
+    private void validate(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMsg.INVALID_NO_INPUT.getErrMsg());
+        }
     }
 }

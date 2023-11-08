fe8acd30d02dbc1c69a4031b54e17cc68e0fb862
mini1115 <kmini9611@naver.com>
2023-11-08 14:07:21 +0900

    feat: 총 수익률 계산 기능 추가

--- src/main/java/lotto/Judgement.java
+++ src/main/java/lotto/Judgement.java
@@ -15,4 +15,14 @@ public class Judgement {
     public int getResultIndex(int commonNumber, boolean bonus) {
         return MatchCount.getMatchIndex(commonNumber, bonus);
     }
+
+    public static void earingRate(List<Integer> result, int inputMoney) {
+        List<Integer> mount = Arrays.asList(2000000000, 30000000, 1500000, 50000, 5000);
+        double totalMoney = 0;
+        for (int i = 0; i < mount.size(); i++) {
+            totalMoney += result.get(i) * mount.get(i);
+        }
+        totalMoney = totalMoney / (inputMoney * 10);
+        System.out.print("총 수익률은 " + totalMoney + "%입니다.");
+    }
 }

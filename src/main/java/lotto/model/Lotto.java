e5017383e5d7628864b32db6e27bda0feb9bbaca
Dawin <dawin002@naver.com>
2023-11-08 16:30:29 +0900

    refactor(Lotto): 당첨 로또 번호 검증 기능 수정
    검증시 발생하는 예외 종류, 예외 메시지 수정
    - validateLength()
    - validateNumberInRange()
    - validateDuplicate()

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import lotto.model.enums.LottoSpec;
+import lotto.view.enums.ErrorMessage;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -10,31 +11,14 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLength(numbers);
-        validateNumberInRange(numbers);
-        validateDuplicate(numbers);
+        validate(numbers);
         this.numbers = numbers;
     }
 
-    protected void validateLength(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    protected void validateDuplicate(List<Integer> numbers) {
-        HashSet<Integer> numberSet = new HashSet<>(numbers);
-        if (numberSet.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    protected void validateNumberInRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < LottoSpec.MIN_NUMBER.getValue() || number > LottoSpec.MAX_NUMBER.getValue()) {
-                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
-        }
+    protected void validate(List<Integer> numbers) {
+        validateLength(numbers);
+        validateNumberInRange(numbers);
+        validateDuplicate(numbers);
     }
 
     public boolean contains(int number) {
@@ -43,6 +27,27 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    protected void validateLength(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalStateException(ErrorMessage.LOTTO_NUMBERS_WRONG_SIZE.getMessage());
+        }
+    }
+
+    protected void validateNumberInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LottoSpec.MIN_NUMBER.getValue() || number > LottoSpec.MAX_NUMBER.getValue()) {
+                throw new IllegalStateException(ErrorMessage.WINNING_NUMBER_OUT_OF_RANGE.getMessage());
+            }
+        }
+    }
+
+    protected void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != 6) {
+            throw new IllegalStateException(ErrorMessage.LOTTO_NUMBER_DUPLICATED.getMessage());
+        }
     }
 
 }

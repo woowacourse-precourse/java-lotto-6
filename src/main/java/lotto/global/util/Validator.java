d9cc1a765c5ec5068f54b5aec3e84d40b569726d
sunwoong <sunwoong1122@gmail.com>
2023-11-08 22:32:43 +0900

    refactor: 에러 메시지 관리 enum 클래스 적용

--- src/main/java/lotto/global/util/Validator.java
+++ src/main/java/lotto/global/util/Validator.java
@@ -1,5 +1,7 @@
 package lotto.global.util;
 
+import lotto.global.error.ErrorMessage;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,44 +11,44 @@ public interface Validator {
         try {
             Integer.parseInt(price);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 가격은 숫자만 가능합니다.");
+            throw new IllegalArgumentException(ErrorMessage.PRICE_NOT_INTEGER.getMessage());
         }
         int processedPrice = Integer.parseInt(price);
         if (processedPrice % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 가격은 1,000원 단위의 숫자만 가능합니다.");
+            throw new IllegalArgumentException(ErrorMessage.PRICE_MISMATCH.getMessage());
         }
     }
 
     static void validateNumbers(String numbers) {
         if (numbers.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 공백일 수 없습니다.");
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_IS_BLANK.getMessage());
         }
         if (!numbers.contains(",")) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 포함해야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_NOT_CONTAIN_SYMBOL.getMessage());
         }
         if (numbers.startsWith(",") || numbers.endsWith(",")) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호의 쉼표(,) 위치가 적절하지 않습니다.");
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_INVALID_SYMBOL_POSITION.getMessage());
         }
         String[] processedNumbers = numbers.split(",");
         if (processedNumbers.length != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_INVALID_COUNT.getMessage());
         }
         Arrays.stream(processedNumbers)
                 .forEach(number -> {
                     try {
                         int processedNumber = Integer.parseInt(number);
                         if (processedNumber < 1 || processedNumber > 45) {
-                            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1 ~ 45 사이의 숫자만 가능합니다.");
+                            throw new IllegalArgumentException(ErrorMessage.NUMBER_INVALID_RANGE.getMessage());
                         }
                     } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 가능합니다.");
+                        throw new IllegalArgumentException(ErrorMessage.NUMBER_NOT_INTEGER.getMessage());
                     }
                 });
         List<String> seen = new ArrayList<>();
         Arrays.stream(processedNumbers)
                 .forEach(number -> {
                     if (seen.contains(number)) {
-                        throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+                        throw new IllegalArgumentException(ErrorMessage.NUMBER_DUPLICATE.getMessage());
                     }
                     seen.add(number);
                 });
@@ -56,10 +58,10 @@ public interface Validator {
         try {
             int processedBonusNumber = Integer.parseInt(bonusNumber);
             if (processedBonusNumber < 1 || processedBonusNumber > 45) {
-                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 사이의 숫자만 가능합니다.");
+                throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_INVALID_RANGE.getMessage());
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 가능합니다.");
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_NOT_INTEGER.getMessage());
         }
     }
 }

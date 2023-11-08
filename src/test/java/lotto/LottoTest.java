5af85cfbf04def9c6af2b501264013a6b9b94628
mdgome <mdgome3@gmail.com>
2023-11-08 13:05:40 +0900

    test: 입력 값이 없는 경우 예외 발생하는지 검사 (2/3)
    1. 구매한 로또 번호
    2. 당첨 번호

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -30,4 +30,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호가 없는 경우 예외가 발생한다.")
+    @Test
+    void createLottoWithNoInput() {
+        assertThatThrownBy(() -> new Lotto(List.of()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

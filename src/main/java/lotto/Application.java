2d076053988c4f29f1b11b66fba50669b3e5a9ee
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-08 17:30:20 +0900

    refactor : modify to use LottoController to buy a lottery ticket and use ReceiptController to print the results.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,15 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.controller.ReceiptController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoController lottoController = LottoController.getInstance();
-        lottoController.run();
+        ReceiptController receiptController = ReceiptController.getInstance();
+
+        lottoController.purchaseLotto();
+        receiptController.showResult();
     }
 }

4c43e5a699e19abf9f354e29d5df00446610cbd6
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 18:42:22 +0900

    implement: User refactor: utils package to main/java/lotto

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,4 +1,61 @@
 package lotto;
 
+import lotto.utils.Prompt;
+import lotto.utils.Error;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class User {
+
+    public int inputValidatedPurchaseAmount() {
+        while (true) {
+            try {
+                System.out.println(Prompt.INPUT_PURCHASE_AMOUNT.getMessage());
+                int amount = Integer.parseInt(Console.readLine().trim());
+                // LottoManager 클래스 내의 검증 로직을 호출해야 함
+                return amount; // 검증에 성공하면 반환
+            } catch (NumberFormatException e) {
+                System.out.println(Error.INVALID_PURCHASE_AMOUNT.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> inputValidatedWinningNumbers() {
+        while (true) {
+            try {
+                System.out.println(Prompt.INPUT_WINNING_NUMBERS.getMessage());
+                String input = Console.readLine();
+                List<Integer> winningNumbers = Arrays.stream(input.split(","))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                new Lotto(winningNumbers); // Lotto 생성자에서 검증
+                return winningNumbers; // 검증에 성공하면 반환
+            } catch (NumberFormatException e) {
+                System.out.println(Error.INVALID_WINNING_NUMBER.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int inputValidatedBonusNumber() {
+        while (true) {
+            try {
+                System.out.println(Prompt.INPUT_BONUS_NUMBER.getMessage());
+                int bonusNumber = Integer.parseInt(Console.readLine().trim());
+                // WinningNumber 클래스 내의 검증 로직을 호출해야 함
+                return bonusNumber; // 검증에 성공하면 반환
+            } catch (NumberFormatException e) {
+                System.out.println(Error.INVALID_BONUS_NUMBER.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }

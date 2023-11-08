a39ab7f843e33e0a65a6aad22505e0e056f1527f
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-08 20:46:53 +0900

    refactor(Lotto): 로직 분리 15줄이 넘어가지 않는 요구사항을 지키기 위해서 입력받는 돈, 로또번호, 보너스 번호를 각각 validate 메서드로 분리해 주어서 이 validate 메서드에서 모든 예외 상황을 체크해 주었습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,10 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lottos;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 public class InputView {
     public static final String COMMA = ",";
@@ -19,67 +17,83 @@ public class InputView {
 
     public static List<Integer> winningsNumbersCon;
 
-    public static int userHaveMoney() {
+    public static int inputMoney() {
         System.out.println(INPUT_MONEY);
         String price = Console.readLine();
 
-        try {
-            isNotNumber(price);
-            isNotDivided(Lottos.LOTTO_PRICE, Integer.parseInt(price));
-            return Integer.parseInt(price);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return userHaveMoney();
+        if (!validateUserMoney(price)) {
+            return inputMoney();
         }
+
+        return Integer.parseInt(price);
     }
 
     public static List<Integer> inputWinningNumbers() {
-        List<Integer> winningNumbers;
+        List<Integer> winningNumbers = null;
+        boolean isValidInput = false;
 
-        while (true) {
+        while (!isValidInput) {
             System.out.println(INPUT_WINNING_NUMBERS);
             String input = Console.readLine();
             List<String> inputNumbers = Arrays.asList(input.split(COMMA));
 
-            try {
-                isNotNumberClude(inputNumbers);
-                winningNumbers = parseAndValidateNumbers(inputNumbers);
+            isValidInput = validateWinningNumbers(inputNumbers);
+            if (isValidInput) {
+                winningNumbers = inputNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
                 winningsNumbersCon = winningNumbers;
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
             }
         }
-
         return winningNumbers;
     }
 
     public static int inputBonusNumber() {
-        System.out.println();
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.println("\n" + INPUT_BONUS_NUMBER);
         String input = Console.readLine();
 
-        try {
-            isNotNumber(input);
-            isRangeOneToFiftyFive(Integer.parseInt(input));
-            checkBonusNumberNotDuplicatedWithWinningNumbers(winningsNumbersCon, Integer.parseInt(input));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        if (!validateBonusNumber(input)) {
             return inputBonusNumber();
         }
 
         return Integer.parseInt(input);
     }
 
-    private static List<Integer> parseAndValidateNumbers(List<String> inputNumbers) {
+    private static boolean validateUserMoney(String price) {
+        try {
+            isNotNumber(price);
+            isNotDivided(Lottos.LOTTO_PRICE, Integer.parseInt(price));
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private static boolean validateWinningNumbers(List<String> inputNumbers) {
         List<Integer> numbers = inputNumbers.stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        try {
+            isNotSizeToSix(inputNumbers);
+            isNotNumberClude(inputNumbers);
+            checkDuplicate(numbers);
+            checkRangeOneToFortyFive(numbers);
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 
-        checkDuplicate(numbers);
-        checkRangeOneToFortyFive(numbers);
-
-        return numbers;
+    private static boolean validateBonusNumber(String input) {
+        try {
+            isNotNumber(input);
+            checkRangeOneToFortyFive(Integer.parseInt(input));
+            checkBonusNumberNotDuplicatedWithWinningNumbers(winningsNumbersCon, Integer.parseInt(input));
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     private static void isNotNumber(String number) {
@@ -101,21 +115,27 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
-    private static void isRangeOneToFiftyFive(int number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + "입력 값이 1~45 범위를 벗어났습니다: ");
+    private static void isNotSizeToSix(List<String> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(InputView.ERROR_MESSAGE + "숫자 6자리를 입력하셔야 합니다.");
         }
     }
 
     private static void isNotDivided(int lottoPrice, int inputPrice) {
         if (inputPrice % lottoPrice != 0) {
-            throw new IllegalArgumentException(InputView.ERROR_MESSAGE + "입력 하신 금액이 1000원으로 나눠 떨어지지 않습니다.");
+            throw new IllegalArgumentException(InputView.ERROR_MESSAGE + "입력하신 금액이 1000원으로 나눠떨어지지 않습니다.");
         }
     }
 
     private static void checkDuplicate(List<Integer> numbers) {
         if (numbers.size() != new HashSet<>(numbers).size()) {
             throw new IllegalArgumentException(ERROR_MESSAGE + "중복된 숫자가 있습니다.");
+        }
+    }
+
+    public static void checkBonusNumberNotDuplicatedWithWinningNumbers(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "보너스 번호가 당첨 번호와 중복됩니다.");
         }
     }
 
@@ -127,9 +147,9 @@ public class InputView {
         }
     }
 
-    public static void checkBonusNumberNotDuplicatedWithWinningNumbers(List<Integer> numbers, int bonusNumber) {
-        if (numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + "보너스 번호가 당첨 번호와 중복됩니다.");
+    private static void checkRangeOneToFortyFive(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "입력 값이 1~45 범위를 벗어났습니다: ");
         }
     }
 }

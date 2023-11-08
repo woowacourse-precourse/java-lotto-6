8afd8b3035a0b0299936ec912bdc8780e4115b92
K-KY <kyuyoungk@naver.com>
2023-11-08 22:42:08 +0900

    Refactor(WinStatistics) : 클래스 내부에 선언 되있던 enum클래스 분리

--- src/main/java/lotto/domain/WinStatistics.java
+++ src/main/java/lotto/domain/WinStatistics.java
@@ -14,22 +14,6 @@ public class WinStatistics {
     private static final int UNIT = 1000;
 
 
-    public enum PrizeLevel {
-        THREE_MATCH("3개 일치 (5,000원) - ", 3),
-        FOUR_MATCH("4개 일치 (50,000원) - ", 4),
-        FIVE_MATCH("5개 일치 (1,500,000원) - ", 5),
-        FIVE_MATCH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 7),
-        SIX_MATCH("6개 일치 (2,000,000,000원) - ", 6);
-
-        private final String description;
-        private final int win;
-
-        PrizeLevel(String description, int win) {
-            this.description = description;
-            this.win = win;
-        }
-
-    }
 
     public static String getStatisticsString(List<Integer> scores, long revenue) {
         PrizeLevel[] prizeLevels = PrizeLevel.values();
@@ -42,7 +26,7 @@ public class WinStatistics {
         StringBuilder sb = new StringBuilder();
         sb.append(STATISTICS).append(DIVIDER);
         for (int i = 0; i < prizeLevels.length; i++) {
-            sb.append(prizeLevels[i].description).append(findWinLotto(scores, prizeLevels[i].win)).append(COUNTER);
+            sb.append(prizeLevels[i].getDescription()).append(findWinLotto(scores, prizeLevels[i].getWin())).append(COUNTER);
         }
         sb.append(DIVIDER).append(TOTAL_REVENUE).append(percentage).append(END);
         return sb.toString();

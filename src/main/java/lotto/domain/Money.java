7083bad7327fde88efea1d45c9ab111741a2e796
깃wooqqq <sge0225@naver.com>
2023-11-08 15:39:54 +0900

    refactor: 구입금액에 대한 예외처리 Money 클래스에서도 검증

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
+import java.util.regex.Pattern;
+
 public class Money {
     private static final int ZERO = 0;
     private static final int THOUSAND = 1000;
+    private static final int MIN_VALUE = 1;
+    private static final Pattern PATTERN = Pattern.compile("\\d+");
     private final int money;
 
     public Money(int money) {
@@ -12,10 +16,24 @@ public class Money {
 
     private void validate(int input) {
         validateRemainder(input);
+        validateRange(input);
+        validateNumeric(String.valueOf(input));
     }
 
     private void validateRemainder(int input) {
         if (input % THOUSAND != ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateRange(int input) {
+        if (input < MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumeric(String input) {
+        if (!PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException();
         }
     }

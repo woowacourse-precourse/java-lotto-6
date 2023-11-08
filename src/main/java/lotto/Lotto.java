6094219d45a5715c732170af502673b3331b19ad
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-08 16:08:01 +0900

    Feat(lotto) : 사용자가 입력한 당첨 번호가 1과 45 사이가 아닐 경우 예외가 발생한다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -22,6 +22,7 @@ public class Lotto {
     //user's Lotto constructor
     private Lotto(List<Integer> numbers) {
         validateLength(numbers);
+        validateRange(numbers);
         validateDuplication(numbers);
 
         this.numbers = numbers;
@@ -45,6 +46,16 @@ public class Lotto {
 
     private boolean hasDuplicatedNumber(List<Integer> numbers) {
         return numbers.size() != new HashSet<Integer>(numbers).size();
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (isNotRequiredRange(numbers)) {
+            throw new IllegalArgumentException("1부터 45까지의 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private boolean isNotRequiredRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(number -> number < 1 || number > 45);
     }
 
     public List<Integer> getLottoNumbers() {

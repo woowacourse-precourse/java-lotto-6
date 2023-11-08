a269f79c3056a59007b3f47aaecf9edc86c47484
정용우 <jyw1341@gmail.com>
2023-11-08 16:03:08 +0900

    feat: 상금 enum에 상금과 대응되는 정답 갯수 정보 속성 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -2,23 +2,29 @@ package lotto;
 
 public enum Prize {
 
-    First(2_000_000_000, "2,000,000,000원"),
-    Second(30_000_000, "30,000,000원"),
-    Third(1_500_000, "1,500,000원"),
-    Forth(50_000, "50,000원"),
-    Fifth(5_000, "5,000원"),
-    None(0, "");
+    First(6, 2_000_000_000L, "2,000,000,000원"),
+    Second(5, 30_000_000L, "30,000,000원"),
+    Third(5, 1_500_000L, "1,500,000원"),
+    Forth(4, 50_000L, "50,000원"),
+    Fifth(3, 5_000L, "5,000원"),
+    None(null, 0L, "");
 
 
-    private final int money;
+    private final Long money;
     private final String moneyString;
+    private final Integer count;
 
-    Prize(int money, String moneyString) {
+    Prize(Integer count, Long money, String moneyString) {
+        this.count = count;
         this.money = money;
         this.moneyString = moneyString;
     }
 
-    public int getMoney() {
+    public Integer getCount() {
+        return count;
+    }
+
+    public Long getMoney() {
         return money;
     }
 

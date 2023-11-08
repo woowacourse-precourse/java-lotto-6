3c522f29238bd10042c644c0b4242238f7c85be8
kyoo0115 <internetkyoomin@gmail.com>
2023-11-08 15:24:15 +0900

    feat: changed to enum for better readability

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,25 +2,39 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-import lotto.utils.Util;
+import lotto.model.validation.WinningNumberValidation;
 
 public class InputView {
-    private static final String INPUT_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String INPUT_LOTTO_WINNING = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public static String lottoMoneyInput() {
-        System.out.println(INPUT_LOTTO_AMOUNT);
+    public enum Prompt {
+        LOTTO_AMOUNT("구입금액을 입력해 주세요."),
+        LOTTO_WINNING("당첨 번호를 입력해 주세요."),
+        BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+
+        private final String message;
+
+        Prompt(String message) {
+            this.message = message;
+        }
+
+        public void display() {
+            System.out.println(message);
+        }
+    }
+
+    public static String inputLottoMoney() {
+        Prompt.LOTTO_AMOUNT.display();
         return Console.readLine();
     }
 
     public static List<Integer> inputLottoWinningNum() {
-        System.out.println(INPUT_LOTTO_WINNING);
-        return Util.numberList(Console.readLine());
+        Prompt.LOTTO_WINNING.display();
+        String winningNumberStr = Console.readLine();
+        return WinningNumberValidation.validateWinningNumbers(winningNumberStr);
     }
 
     public static String inputBonusNumber() {
-        System.out.println(INPUT_BONUS_NUMBER);
+        Prompt.BONUS_NUMBER.display();
         return Console.readLine();
     }
 }

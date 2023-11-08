0b8e38e5e33ae0b935df6a993fdf1d7305ad7450
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-11-08 21:46:00 +0900

    feat : application 시작 코드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoPlay;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoPlay lottoPlay = new LottoPlay();
+        lottoPlay.play();
     }
 }

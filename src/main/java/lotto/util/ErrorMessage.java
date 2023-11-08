58b5df1a5704ae7ac99488010f1459cd13e0e7a1
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2023-11-08 15:22:10 +0900

    style(ErrorMessage): ERROR_NUMERIC_TYPE 에러 문구 수정

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -4,7 +4,7 @@ public class ErrorMessage {
     public static final String ERROR_LOTTO_SIZE = "[ERROR] 번호는 6개여야 합니다.";
     public static final String ERROR_LOTTO_DUPLICATE = "[ERROR] 중복된 숫자가 존재합니다.";
     public static final String ERROR_LOTTO_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    public static final String ERROR_NUMERIC_TYPE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_NUMERIC_TYPE = "[ERROR] 1부터 45 사이의 숫자를 입력해주세요.";
     public static final String ERROR_MINIMUM_PRICE = "[ERROR] 1,000원 이상의 금액을 입력해주세요.";
     public static final String ERROR_NOT_DIVIDE_LOTTO_PRICE = "[ERROR] 1,000원 단위의 금액을 입력해주세요.";
     public static final String ERROR_BONUS_OUT_OF_RANGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";

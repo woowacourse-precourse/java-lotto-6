2b83cb1c6bf8ea7401b14e1d2dd8f464678b764c
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 19:13:51 +0900

    feat(exception) : add MissingWinningNumbersException class

---
+++ src/main/java/lotto/exception/MissingWinningNumbersException.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class MissingWinningNumbersException extends IllegalArgumentException {
+    public MissingWinningNumbersException() {
+        super("[ERROR] 당첨 번호는 공백일 수 없습니다.");
+    }
+}

6dfd5282cb517606c7bd52c2bc7ef1f735506563
HyunmoBae <tsi0520@naver.com>
2023-11-08 20:00:44 +0900

    test(lotto) : modfiy ErrorType test

--- src/test/java/lotto/error/ErrorTest.java
+++ src/test/java/lotto/error/ErrorTest.java
@@ -1,35 +1,36 @@
 package lotto.error;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.error.Error.ErrorType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ErrorTest {
-    @DisplayName("enum에 맞게 에러가 나오는지 테스트")
+    @DisplayName("enum에 맞게 에러 메시지가 나오는지 테스트")
     @Test
-    void invalidAmountErrorTest(){
-        assertThatThrownBy(() -> Error.errorMessage(ErrorType.INVALID_AMOUNT))
-                .isInstanceOf(IllegalArgumentException.class);
+    void invalidMoneyTypeErrorMessageTest(){
+        String result = ErrorType.INVALID_MONEY_TYPE.getMessage();
+        assertThat(result).isEqualTo("[ERROR] 유효하지 않은 값입니다.");
     }
 
     @Test
-    void invalidInputTypeErrorTest(){
-        assertThatThrownBy(() -> Error.errorMessage(ErrorType.INVALID_INPUT_TYPE))
-                .isInstanceOf(IllegalArgumentException.class);
+    void invalidLottoRangeErrorMessageTest(){
+        String result = ErrorType.INVALID_LOTTO_SIZE.getMessage();
+        assertThat(result).isEqualTo("[ERROR] 로또 숫자는 6개를 입력해야 합니다.");
     }
 
     @Test
-    void invalidLottoInputErrorTest(){
-        assertThatThrownBy(() -> Error.errorMessage(ErrorType.INVALID_LOTTO_INPUT))
-                .isInstanceOf(IllegalArgumentException.class);
+    void duplicationErrorMessageTest(){
+        String result = ErrorType.DUPLICATION.getMessage();
+        assertThat(result).isEqualTo("[ERROR] 중복된 숫자가 있으면 안됩니다.");
     }
 
     @Test
-    void duplicationErrorTest(){
-        assertThatThrownBy(() -> Error.errorMessage(ErrorType.DUPLICATION))
-                .isInstanceOf(IllegalArgumentException.class);
+    void invalidLottoInputErrorMessageTest(){
+        String result = ErrorType.INVALID_LOTTO_INPUT.getMessage();
+        assertThat(result).isEqualTo("[ERROR] 유효하지 않은 값입니다.");
     }
 
 }

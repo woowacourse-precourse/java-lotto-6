fb43055e8579d41e62cf40a17271c8a24afa0bfb
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 22:09:01 +0900

    refactor:"클래스명 변경 및 상수변수명 변경"

--- src/main/java/lotto/constant/InputViewMessages.java
+++ src/main/java/lotto/constant/InputViewMessages.java
@@ -1,6 +1,6 @@
 package lotto.constant;
 
-public class InputViewConstant {
+public class InputViewMessages {
     public static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
     public static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";

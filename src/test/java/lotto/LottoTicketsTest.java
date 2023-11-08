afca25bffa08bb0a37b3761ec600a46c4e766966
jihun-24k <gi020477@gmail.com>
2023-11-08 11:46:09 +0900

    test: 구매 개수만큼 로또티켓 발행

---
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class LottoTicketsTest {
+
+    @Test
+    void 구매_개수만큼_로또티켓_발행() {
+        Money money = new Money("1000");
+        LottoTickets lottoTickets = new LottoTickets(money);
+
+        assertEquals(1, lottoTickets.getLottoTickets().size());
+    }
+}

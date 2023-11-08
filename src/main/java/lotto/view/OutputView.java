d0056665500ce0141585f1dd8accd01b3b820058
Koo-EunSung <ku903@naver.com>
2023-11-08 18:43:54 +0900

    Feat: 발행 로또 수량 및 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,16 @@
 package lotto.view;
 
+import lotto.Lotto;
+
 public class OutputView {
     public void exception(String message) {
         System.out.println(message);
+    }
+
+    public void lottoBuyHistory(Lotto[] lottos) {
+        System.out.println(lottos.length + "개를 구매했습니다.");
+        for(Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }

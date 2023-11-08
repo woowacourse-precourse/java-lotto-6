a9629f28f37972bc8b803a5eb4c1ae24895990fb
K-KY <kyuyoungk@naver.com>
2023-11-08 22:28:37 +0900

    Refactor(Money) : 변수명 변경, 매직넘버 상수화

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,20 +3,21 @@ package lotto.domain;
 import lotto.domain.validator.ValidateMoney;
 
 public class Money {
+    private static final int UNIT = 1000;
     private final Integer money;
 
-    public Money(int i) {
-        validate(i);
-        this.money = i;
+    public Money(int money) {
+        validate(money);
+        this.money = money;
     }
 
-    private void validate(int i) {
-        ValidateMoney.isOverThousand(i);
-        ValidateMoney.isUnitFollowed(i);
-        ValidateMoney.isMoneyInRange(i);
+    private void validate(int money) {
+        ValidateMoney.isOverThousand(money);
+        ValidateMoney.isUnitFollowed(money);
+        ValidateMoney.isMoneyInRange(money);
     }
 
     public int availableCount() {
-        return money / 1000;
+        return money / UNIT;
     }
 }

792194e9adc0a334d9858d23368cf67c9f3529cb
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 14:17:12 +0900

    fix: Output 상수 메서드 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -6,12 +6,12 @@ import lotto.string.OutputMessage;
 
 public class Output {
     public void requestAmount() {
-        System.out.println(OutputMessage.REQUEST_AMOUNT_MESSAGE);
+        System.out.println(OutputMessage.REQUEST_AMOUNT_MESSAGE.getMessage());
     }
 
     public void printPurchaseCount(int purchaseCount) {
         System.out.print(purchaseCount);
-        System.out.println(OutputMessage.PURCHASE_COUNT_MESSAGE);
+        System.out.println(OutputMessage.PURCHASE_COUNT_MESSAGE.getMessage());
     }
 
     public void printLottos(List<Lotto> lottos) {
@@ -21,38 +21,38 @@ public class Output {
     }
 
     public void requestWiningNumber() {
-        System.out.println(OutputMessage.REQUEST_WININGNUMBER_MESSAGE);
+        System.out.println(OutputMessage.REQUEST_WININGNUMBER_MESSAGE.getMessage());
     }
 
     public void requestBonusNumber() {
-        System.out.println(OutputMessage.REQUEST_BONUSNUMBER_MESSAGE);
+        System.out.println(OutputMessage.REQUEST_BONUSNUMBER_MESSAGE.getMessage());
     }
 
     public void printResultLotto() {
         //추가 구현 필요
         float num = 0;
         printWiningStatisticsStart();
-        System.out.println(OutputMessage.DIVISIONLINE_MESSAGE);
+        System.out.println(OutputMessage.DIVISIONLINE_MESSAGE.getMessage());
         printWiningCount();
         printReturnRate(num);
 
     }
 
     public void printWiningStatisticsStart() {
-        System.out.println(OutputMessage.WININGSTATISTIC_START_MESSAGE);
+        System.out.println(OutputMessage.WININGSTATISTIC_START_MESSAGE.getMessage());
     }
 
     public void printWiningCount() {
         //추가 구현 필요
-        System.out.print(OutputMessage.STATISTIC_FRONT_MESSAGE);
-        System.out.print(OutputMessage.STATISTIC_MIDDLE_MESSAGE);
-        System.out.print(OutputMessage.STATISTIC_LAST_MESSAGE);
+        System.out.print(OutputMessage.STATISTIC_FRONT_MESSAGE.getMessage());
+        System.out.print(OutputMessage.STATISTIC_MIDDLE_MESSAGE.getMessage());
+        System.out.print(OutputMessage.STATISTIC_LAST_MESSAGE.getMessage());
     }
 
     public void printReturnRate(float returnRate) {
-        System.out.print(OutputMessage.STATISTIC_RESULT_FRONT_MESSAGE);
+        System.out.print(OutputMessage.STATISTIC_RESULT_FRONT_MESSAGE.getMessage());
         System.out.print(returnRate);
-        System.out.println(OutputMessage.STATISTIC_RESULT_LAST_MESSAGE);
+        System.out.println(OutputMessage.STATISTIC_RESULT_LAST_MESSAGE.getMessage());
 
     }
 }

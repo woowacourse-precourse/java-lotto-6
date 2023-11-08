08651d9d2295ed869ed6c685455b95de595d4010
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:22:19 +0900

    feat : LottoTicket의 로또 티켓 가격 상수 선언

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoTicket {
+    public static final int PRICE = 1000;
     private final Lotto lotto;
 
     public LottoTicket(Lotto lotto) {

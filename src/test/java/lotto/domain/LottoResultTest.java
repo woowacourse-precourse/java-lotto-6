ca4971b3f9a56340a803f85e8695409fe6947c57
jooda00 <dragon2000a@gmail.com>
2023-11-08 21:36:05 +0900

    test(LottoResultTest): 당첨 금액 반환 기능 테스트

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -6,12 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoResultTest {
-
     @DisplayName("당첨 개수와 대응하는 상태를 반환한다.")
     @Test
     void createStatusByCoincidedLottoNumber() {
         int size = 5;
 
         assertThat(LottoResult.THIRD).isEqualTo(LottoResult.initiateLottoResult(size));
+    }
+
+    @DisplayName("총 당첨 금액을 정상 반환한다.")
+    @Test
+    void createWinningValue() {
+        int value = 2000000000;
+
+        assertThat(value).isEqualTo(LottoResult.FIRST.getGrade()) ;
     }
 }

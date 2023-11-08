bb76d90c34d70b32aa82320e3e3c272b02635343
Kim mingyu <minq0302@naver.com>
2023-11-08 17:05:03 +0900

    feat(util): add method that changing input String to List<Integer>

--- src/main/java/lotto/util/Util.java
+++ src/main/java/lotto/util/Util.java
@@ -1,7 +1,21 @@
 package lotto.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Util {
-    public static int changeStringToInt(String number){
+    public static int changeStringToInt(String number) {
         return Integer.parseInt(number);
+    }
+
+    public static List<Integer> changeStringToList(String number) {
+        String[] numbersArray = number.split(",");
+        List<Integer> result = new ArrayList<>();
+
+        for (String num : numbersArray) {
+            result.add(Integer.parseInt(num));
+        }
+
+        return result;
     }
 }

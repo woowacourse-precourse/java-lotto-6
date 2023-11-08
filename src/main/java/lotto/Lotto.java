09bc1e13606116949e7651e64e30e1233a5b9c50
doyoungkim97 <eh4536@naver.com>
2023-11-08 22:49:21 +0900

    feat : 로또 중복 입력 방지 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,14 +7,25 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicates(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 숫자는 6개 입니다.");
         }
     }
 
+    private void duplicates(List<Integer> numbers){
+        boolean hasDuplicates = numbers.stream().distinct().count() != numbers.size();
+        if (hasDuplicates){
+            throw new IllegalArgumentException("[ERROR] 로또 숫자 중복이 없습니다.");
+        }
+    }
+
+    public List<Integer> getLottoNumber(){
+        return this.numbers;
+    }
     // TODO: 추가 기능 구현
 }

cdbd607f3f5e327e5481e41d0f5645632e6f3f2e
realcold0 <realcold0@gmail.com>
2023-11-08 16:07:55 +0900

    feat(lotto) : 당첨번호를 입력받는 로직 GameController에 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -2,7 +2,9 @@ package lotto.controller;
 
 import lotto.domain.Lottos;
 import lotto.domain.Price;
+import lotto.domain.WinningLotto;
 import lotto.view.InputPurchaseView;
+import lotto.view.InputWinningNumView;
 import lotto.view.OutputPurchaseView;
 
 /**
@@ -11,22 +13,31 @@ import lotto.view.OutputPurchaseView;
 public class GameController {
     private final InputPurchaseView inputPurchaseView = new InputPurchaseView();
     private final OutputPurchaseView outputPurchaseView = new OutputPurchaseView();
+    private final InputWinningNumView inputWinningNumView = new InputWinningNumView();
     private final Lottos lottos = new Lottos();
+    private WinningLotto winningLotto;
 
     /**
      * 게임 시작하는 메서드
      */
-    public void start(){
+    public void start() {
         init();
+        setWinningLotto();
     }
 
     /**
      * 게임을 시작하기전에 구매할 금액에 따라 로또를 생성하고 출력하는 메서드
      */
-    private void init(){
-        Price price =  new Price(inputPurchaseView.printPurchasePrice()); //구입 금액 입력받기
+    private void init() {
+        Price price = new Price(inputPurchaseView.printPurchasePrice()); //구입 금액 입력받기
         lottos.generateLotto(price.getAmount());// 입력받은 구입금액 만큼 lotto 생성
         outputPurchaseView.printPurchaseLotto(price.getAmount(), lottos);//구입한 로또들을 출력
+    }
+
+    private void setWinningLotto() {
+        winningLotto = new WinningLotto(inputWinningNumView.printWinningNum(),
+                inputWinningNumView.printBonusNum());  //당첨번호와 보너스 번호를 입력받는다.
+
     }
 
 }

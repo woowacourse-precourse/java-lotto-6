d117d6597399174e2560e848b733ec88f89e012e
leejh7 <777joonho@kookmin.ac.kr>
2023-11-08 09:11:24 +0900

    feat: 담청 통계 기능을 위한 당첨 내역 계산 로직 구현

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -1,5 +1,15 @@
 package lotto.constant;
 
+import java.util.List;
+
 public class LottoConstant {
     public static final int MAX_PURCHASE_AMOUNT = 100_000;
+
+    public static final List<LotteryRank> LOTTERY_RANKS = List.of(
+            LotteryRank.FIRST_PRIZE,
+            LotteryRank.SECOND_PRIZE,
+            LotteryRank.THIRD_PRIZE,
+            LotteryRank.FOURTH_PRIZE,
+            LotteryRank.FIFTH_PRIZE
+    );
 }
--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -1,11 +1,10 @@
 package lotto.model;
 
 import lotto.constant.LotteryRank;
+import lotto.constant.LottoConstant;
 import lotto.utils.LottoMachine;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Player {
     private List<Lotto> lottos;
@@ -25,13 +24,20 @@ public class Player {
     }
 
     public WinningDetails getWinningDetails(LottoMachine lottoMachine) {
-        List<LotteryRank> lotteryRanks = calculateLotteryRank(lottoMachine);
+        List<LotteryRank> lotteryRanks = calculateLotteryRanks(lottoMachine);
 
         return new WinningDetails(getBreakdown(lotteryRanks), calculateROI(lotteryRanks));
     }
 
     private Map<LotteryRank, Integer> getBreakdown(List<LotteryRank> lotteryRanks) {
-        return null;
+        Map<LotteryRank, Integer> result = new HashMap<>();
+
+        LottoConstant.LOTTERY_RANKS
+                .forEach(lotteryRank -> {
+                    result.put(lotteryRank, Collections.frequency(lotteryRanks, lotteryRank));
+                });
+
+        return result;
     }
 
     private float calculateROI(List<LotteryRank> lotteryRanks) {
@@ -41,14 +47,13 @@ public class Player {
         return (float) netReturn / costOfInvestment;
     }
 
-    private List<LotteryRank> calculateLotteryRank(LottoMachine lottoMachine) {
+    private List<LotteryRank> calculateLotteryRanks(LottoMachine lottoMachine) {
         List<LotteryRank> result = new ArrayList<>();
 
         lottos.stream()
                 .map(lotto -> {
                     int matchingNumbersCount = lotto.matchingNumbersCount(lottoMachine.getWinningLotto());
                     boolean containsBonusNumber = lotto.containsNumber(lottoMachine.getBonusNumber());
-
                     return new LotteryResult(matchingNumbersCount, containsBonusNumber);
                 })
                 .forEach(lotteryResult -> {

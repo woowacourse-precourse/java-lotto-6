b535a4742a35a6ffe125a2bc42b99e2143b98373
ro-el-c <chung3286@naver.com>
2023-11-08 23:17:03 +0900

    feat: 로또 구입 금액 입력 및 유효성 검사

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -1,8 +1,11 @@
 package lotto.controller;
 
 import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoMachine {
+    private final String ERROR = "[ERROR] ";
     private final LottoService lottoService;
 
     public LottoMachine() {
@@ -10,5 +13,31 @@ public class LottoMachine {
     }
 
     public void lotto() {
+        purchaseLotto();
+    }
+
+    private void purchaseLotto() {
+        getPurchaseAmount();
+    }
+
+    private int getPurchaseAmount() {
+        boolean isDone = false;
+        int money = 0;
+
+        do {
+            try {
+                String inputValue = InputView.inputPurchaseAmount();
+                lottoService.validateIsNumeric(inputValue);
+
+                money = Integer.parseInt(inputValue);
+                lottoService.validateHasCorrectUnit(money);
+
+                isDone = true;
+            } catch (IllegalArgumentException e) {
+                OutputView.printMessage(ERROR + e.getMessage());
+            }
+        } while (!isDone);
+
+        return money;
     }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,17 @@
 package lotto.service;
 
 public class LottoService {
+
+    public void validateIsNumeric(String inputValue) {
+        final String NUMBER_REGEX = "^[\\d]*$";
+        if (!inputValue.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("숫자만 입력하세요.");
+        }
+    }
+
+    public void validateHasCorrectUnit(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력하세요.");
+        }
+    }
 }

fc1918360465e982e90d6c97c4df7bc4e4359dfd
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 11:31:45 +0900

    test(LottoUtilTest) : StringListToInt 타입 변환 테스트 코드 작성

---
+++ src/test/java/lotto/utils/LottoUtilTest.java
@@ -0,0 +1,21 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LottoUtilTest {
+    @DisplayName("StringListToInt 타입 변환 테스트")
+    @Test
+    public void stringListToInt() {
+        // given
+        String inputLottoNumbers = "3,5,1,4,2,6";
+        // when
+        List<Integer> lottoNumbers = LottoUtil.lottoNumbersParser(inputLottoNumbers);
+        // then
+        List<Integer> expectedNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Assertions.assertEquals(expectedNumbers, lottoNumbers);
+    }
+}

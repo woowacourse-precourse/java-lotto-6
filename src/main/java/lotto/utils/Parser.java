4df4f3aa103be84bed9bd4a6f2740c25af38ffb9
lomayd <jinho10167@gmail.com>
2023-11-08 15:30:30 +0900

    test: 로또 번호, 로또 구입 금액 입력값 검증

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -11,22 +11,23 @@ public class Parser {
         return Integer.valueOf(input) / 1000;
     }
 
-    private void checkEmpty(String input) throws IllegalArgumentException {
+    public void checkEmpty(String input) throws IllegalArgumentException {
         if(input.isEmpty()) {
             System.out.println("[ERROR] 입력값이 빈 값입니다.");
             throw new IllegalArgumentException();
         }
     }
 
-    private void checkDigit(String input) throws IllegalArgumentException {
+    public void checkDigit(String input) throws IllegalArgumentException {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             System.out.println("[ERROR] 입력값은 숫자여야합니다.");
+            throw new IllegalArgumentException();
         }
     }
 
-    private void checkThousand(int number) throws IllegalArgumentException {
+    public void checkThousand(int number) throws IllegalArgumentException {
         if((number % 1000) != 0) {
             System.out.println("[ERROR] 로또 입력 금액은 1000원 단위여야 합니다.");
             throw new IllegalArgumentException();
@@ -60,14 +61,6 @@ public class Parser {
     private void validateSplit(String input) {
         checkEmpty(input);
         checkDigit(input);
-        checkRange(input);
-    }
-
-    private void checkRange(String input) throws IllegalArgumentException {
-        if(Integer.valueOf(input) < 1 || Integer.valueOf(input) > 45) {
-            System.out.println("[ERROR] 로또 숫자는 1이상 45이하여야 합니다.");
-            throw new IllegalArgumentException();
-        }
     }
 
     public int parseBonusNum(String input, List<Integer> winningNum) {
@@ -78,6 +71,5 @@ public class Parser {
     private void validateBonus(String input) {
         checkEmpty(input);
         checkDigit(input);
-        checkRange(input);
     }
 }

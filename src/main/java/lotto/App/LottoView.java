0366126703542fec6e9946271381e498ecca0221
Sanghee0820 <lsh277604@gmail.com>
2023-11-08 14:07:09 +0900

    Style: No newLine

--- src/main/java/lotto/App/LottoView.java
+++ src/main/java/lotto/App/LottoView.java
@@ -24,7 +24,7 @@ public class LottoView {
     }
 
     public void inputWinningLottoBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
+        System.out.println("보너스 번호를 입력해 주세요.");
     }
 
 }

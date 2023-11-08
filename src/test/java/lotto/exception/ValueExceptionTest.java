f1a84dcdc1fd5ea090fc34d9940a5f2bb7bdd626
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:20:15 +0900

    CalculateProfitRate 수익률 계산4

--- src/test/java/lotto/exception/ValueExceptionTest.java
+++ src/test/java/lotto/exception/ValueExceptionTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ValueExceptionTest {
-  
+package lotto.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValueExceptionTest {
+    @DisplayName("숫자인 입력값은 예외가 발생하지 않는다.")
+    @Test
+    void testValidateNumber_ValidNumber() {
+
+        String validNumber = "42";
+
+        assertThatCode(() -> ValueException.validateNumber(validNumber))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("숫자가 아닌 입력값은 IllegalStateException 예외가 발생한다.")
+    @Test
+    void testValidateNumber_InvalidInput() {
+        String invalidInput = "abc"; // 숫자가 아닌 문자열
+
+
+        assertThatThrownBy(() -> ValueException.validateNumber(invalidInput))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("[ERROR] 입력값이 숫자가 아닙니다. 다시 입력하세요");
+    }
 }

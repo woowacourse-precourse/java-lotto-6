bcbd21b4812f1af0a5a5877f8b157eb76eec699d
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 22:36:44 +0900

    Feat[view] 사용자 입력값 안내 문구

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,23 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = readLine();
+        return input;
+    }
+
+    public String readLottoNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = readLine();
+        return input;
+    }
+
+    public String readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = readLine();
+        return input;
+    }
 }

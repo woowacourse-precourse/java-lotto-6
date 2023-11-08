d0f127b3dda550b0189d51b8b8f8f0168e94b0f8
binary_ho <dfghcvb11@naver.com>
2023-11-08 10:02:50 +0900

    test : HitCounter 테스트 작성

---
+++ src/test/java/lotto/model/winninglotto/HitCounterTest.java
@@ -0,0 +1,47 @@
+package lotto.model.winninglotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HitCounterTest {
+
+    @Test
+    @DisplayName("HitCounter를 생성할 수 있다.")
+    void createHitCounterTest() {
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        assertThat(new HitCounter(winningNumbers, 7))
+            .isInstanceOf(HitCounter.class);
+    }
+
+    @Test
+    @DisplayName("HitCounter를 통해 숫자 일치 갯수를 셀 수 있다.")
+    void hitCountTest() {
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        HitCounter hitCounter = new HitCounter(winningNumbers, 7);
+        HitResult hitResult = hitCounter.countHit(winningNumbers);
+
+        assertThat(hitResult.getHitCount()).isEqualTo(winningNumbers.size());
+    }
+
+    @Test
+    @DisplayName("HitCounter를 통해 보너스 숫자 일치 여부를 확인할 수 있다.")
+    void hitBonusBallTest() {
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        HitCounter hitCounter = new HitCounter(winningNumbers, 7);
+
+        Set<Integer> bonusBallHitNumbers = Set.of(2, 3, 4, 5, 6, 7);
+        Set<Integer> bonusBallNotHitNumbers = Set.of(1, 2, 3, 4, 5, 6);
+
+        HitResult bonusBallHitResult = hitCounter.countHit(bonusBallHitNumbers);
+        HitResult bonusBallNotHitResult = hitCounter.countHit(bonusBallNotHitNumbers);
+
+        assertAll(
+            () -> assertThat(bonusBallHitResult.getIsBonusBallHit()).isTrue(),
+            () -> assertThat(bonusBallNotHitResult.getIsBonusBallHit()).isFalse()
+        );
+    }
+}

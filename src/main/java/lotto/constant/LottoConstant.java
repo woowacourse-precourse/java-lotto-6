c982b959ca139da46fd10b080a82b346eddb4375
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-08 20:55:41 +0900

    add(InputView): 구매금액이 부족할 경우 예외 처리

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,6 @@
+package lotto.constant;
+
+public class LottoConstant {
+
+    public static final int LOTTO_PRICE = 1000;
+}
--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.constant.ErrorMessage;
+import lotto.constant.LottoConstant;
 import lotto.util.IntegerParser;
 
 public class View {
@@ -16,10 +17,18 @@ public class View {
         while (true) {
             try {
                 String inputMoney = inputView.getInputMoney();
-                return IntegerParser.parse(inputMoney);
-            } catch (IllegalArgumentException e) {
+                int money = IntegerParser.parse(inputMoney);
+                validateSufficientMoney(money);
+                return money;
+            } catch (NumberFormatException e) {
                 outputView.printErrorMessage(ErrorMessage.NOT_INTEGER_ERROR);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(ErrorMessage.INSUFFICIENT_MONEY_ERROR);
             }
         }
+    }
+
+    private void validateSufficientMoney(int money) {
+        if (money < LottoConstant.LOTTO_PRICE) throw new IllegalArgumentException();
     }
 }

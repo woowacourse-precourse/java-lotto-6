3a41c907452f95e4c79242641c794cd7f5a5431e
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-08 19:23:12 +0900

    [refactor]: 당첨번호 입력 로직 코드 정리

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.dto.LottoResultResponseDto;
 import lotto.dto.ValidateAmountDto;
 import lotto.dto.ValidateAmountResponseDto;
 
@@ -23,17 +24,18 @@ public class LottoService {
         }
         return validateAmountResponseDto.getLottoCount();
     }
-    public void inputLottoNum(){
+    public List<Integer> inputLottoNum(){
         System.out.println("당첨 번호를 입력하세요.");
+        List<Integer> winningLottoNumbers = new ArrayList<>();
         String inputStr = Console.readLine();
         String[] input = inputStr.split(",");
-        List<Integer> numbers = new ArrayList<>();
 
         for (String numberStr : input) {
-            validateLottoNum(numbers,numberStr);
+            winningLottoNumbers.add(validateLottoNum(numberStr));
         }
+        return winningLottoNumbers;
     }
-    public void getUserLottoNum(int lottoCount){
+    public List<List<Integer>> getUserLottoNum(int lottoCount){
         List<List<Integer>> lottoNumbers = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
@@ -44,6 +46,7 @@ public class LottoService {
         for (List<Integer> numbers : lottoNumbers) {
             System.out.println(numbers);
         }
+        return lottoNumbers;
     }
     public int getBonusNum(){
         System.out.println("당첨 번호를 입력하세요.");
@@ -51,7 +54,7 @@ public class LottoService {
         int bonusNum = validateBonusNum(inputStr);
         return bonusNum;
     }
-    public void
+
     private int validateBonusNum(String inputStr){
         int number = 0;
         try {
@@ -64,18 +67,18 @@ public class LottoService {
         }
         return number;
     }
-    private List<Integer> validateLottoNum(List<Integer> numbers, String numberStr){
+    private Integer validateLottoNum(String numberStr){
+        int number = 0;
         try {
-            int number = Integer.parseInt(numberStr);
+            number = Integer.parseInt(numberStr);
             if(number>=1 && number<=45){
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 로또 번호입니다.");
             }
-            numbers.add(number);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
 
-        return numbers;
+        return number;
     }
     private ValidateAmountResponseDto validateAmount(ValidateAmountDto validateAmountDto){
         int lottoCount = 0;

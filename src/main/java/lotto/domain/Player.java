e21379a2453cd541c19ef2c7e81cdaf3f794712e
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 13:43:10 +0900

    feat(Player): 구입 금액 기반으로 수익률 계산하는 기능 구현

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -10,4 +10,11 @@ public class Player {
     public int getPurChaseAmount() {
         return purChaseAmount;
     }
+
+    public double calculateRateOfReturn(double totalReturn) {
+        double rateOfReturn = (totalReturn / purChaseAmount) * 100.0;
+        rateOfReturn = Math.round(rateOfReturn * 100.0) / 100.0;
+
+        return rateOfReturn;
+    }
 }

aece228666b980d759b23145b424567a8406b66e
MisaSohee <sohee5951@naver.com>
2023-11-08 22:01:05 +0900

    test(LottoGenerator): pickUniqueNumbersInRange() 메서드가 번호의 범위가 1부터 45까지인지 확인하는 테스트

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -3,7 +3,11 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LottoGeneratorTest {
@@ -29,5 +33,14 @@ public class LottoGeneratorTest {
         assertThatThrownBy(() -> lottoGenerator.validateInput("1500"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
+    }
+
+    @DisplayName("pickUniqueNumbersInRange() 메서드가 반환한 숫자들이 1부터 45 사이인지 확인한다.")
+    @Test
+    void pickUniqueNumbersInRange_ReturnsNumbersInRange1To45() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.forEach(number -> {
+            assertThat(number).isBetween(1, 45);
+        });
     }
 }

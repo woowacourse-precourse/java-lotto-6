277a2cd67ef7a67dff86c9111291dd356cec8c2e
박승민 <tmddudals369@naver.com>
2023-11-08 23:44:32 +0900

    test(lottoTest): 로또의 도메인 로직 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +25,51 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또에 포함된 숫자는 true을 반환한다.")
+    void returnTrueLottoContainNumber() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int target = 1;
+        //when
+        boolean result = lotto.isContainNumber(target);
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또의 포함되지 않은 숫자는 false를 반환한다.")
+    void returnFalseLottoNotContainNumber() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int target = 7;
+        //when
+        boolean result = lotto.isContainNumber(target);
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("동일한 숫자의 개수를 반환한다.")
+    void returnNumberMatchCount() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto otherLotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        //when
+        int result = lotto.getMatchCount(otherLotto);
+        //then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("동일한 숫자가 존재하지 않을 경우 0을 반환한다.")
+    void return0NotExistNumberMatch() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto otherLotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        //when
+        int result = lotto.getMatchCount(otherLotto);
+        //then
+        assertThat(result).isEqualTo(0);
+    }
 }

285b6d90b051fa9e01db34a60588cdc55567e6fa
강승민 <rkdtmdals999@naver.com>
2023-11-08 21:08:44 +0900

    [test] 보너스 번호가 당첨 번호와 중복되는 경우 예외 테스트 코드 추가

--- src/test/java/lotto/controller/LottoManagerTest.java
+++ src/test/java/lotto/controller/LottoManagerTest.java
@@ -1,14 +1,16 @@
 package lotto.controller;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import lotto.Application;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-class LottoManagerTest {
+class LottoManagerTest extends NsTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -36,6 +38,15 @@ class LottoManagerTest {
     @Test
     void createBonusLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("8000", "1,2,3,4,5,6", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
 }

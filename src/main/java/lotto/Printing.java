805b4c63cad5132e5d8ea5ad80cf54fb7470dc19
soyoungxxx <otherplet@gmail.com>
2023-11-08 15:43:10 +0900

    stye(Printing): for문으로 style 변경

--- src/main/java/lotto/Printing.java
+++ src/main/java/lotto/Printing.java
@@ -3,15 +3,15 @@ package lotto;
 import java.util.List;
 
 public class Printing {
-    private int numberOfLotto;
     private final List<Lotto> lottos;
 
-    public Printing(int numberOfLotto, List<Lotto> lottos) {
-        this.numberOfLotto = numberOfLotto;
+    public Printing(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
     public void printLotto() {
-        System.out.println(lottos);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }

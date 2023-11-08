e0bbdbbbe1c23f38acd0285e5f6d4e7d4882b7f9
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-08 21:37:45 +0900

    Fix(lottoMaker) : 이미 생성된 로또의 정렬 순서가 바뀌지 않는 이슈 해결

--- src/main/java/lotto/util/LottoMaker.java
+++ src/main/java/lotto/util/LottoMaker.java
@@ -1,6 +1,7 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class LottoMaker {
     public static List<Integer> generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
-        Collections.sort(numbers);
+        Collections.sort(new ArrayList<>(numbers));
 
         return numbers;
     }

f536bd3881ef98ca0f6773ff146440af4fe22554
DrivenByCode <pjk146195@gmail.com>
2023-11-08 20:34:08 +0900

    refactor(LottoCounter): LottoPrinter 기능 분리

--- src/main/java/output/LottoCounter.java
+++ src/main/java/output/LottoCounter.java
@@ -8,14 +8,7 @@ public class LottoCounter {
     public static int countLotto(final int money) {
         int n = money / 1000;
         System.out.println();
-        System.out.print(String.valueOf(n).concat(ServiceMessage.getMessageByCode(102)));
+        System.out.println(String.valueOf(n).concat(ServiceMessage.getMessageByCode(102)));
         return n;
-    }
-
-    public static void printLottos(final int money) {
-        List<List<Integer>> lottos = Lotto.getLottos(money);
-        for(List<Integer> lotto : lottos) {
-            System.out.println(lotto.toString());
-        }
     }
 }

cb13ccf85a62dd5b9e4d9f513bf3d3d013d2ce23
dh1010a <dh1010a@naver.com>
2023-11-08 15:58:05 +0900

    refactor: appconfig에서 의존성 주입하도록 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,11 @@
 package lotto;
 
 import lotto.controller.Controller;
-import lotto.domain.RandomNumberGenerator;
-import lotto.service.LottoService;
-import lotto.service.LottoServiceImpl;
-import lotto.view.View;
 
 public class Application {
     public static void main(String[] args) {
-        LottoService lottoService = new LottoServiceImpl(new RandomNumberGenerator());
-        View view = new View();
-        Controller controller = new Controller(lottoService, view);
+        AppConfig appConfig = new AppConfig();
+        Controller controller = appConfig.controller();
         controller.play();
     }
 }

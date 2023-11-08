e9568586c69eaa777d6154d705dd568a1870f987
username <57055730+Jake-huen@users.noreply.github.com>
2023-11-08 17:10:06 +0900

    test : 테스트 코드 파일 패키지에 맞게 분기

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -1,4 +1,50 @@
 package lotto;
 
+import lotto.model.LottoResult;
+import lotto.service.LottoPrize;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LottoResultTest {
+
+    @DisplayName("정상적으로 결과를 누적하고 수익을 계산해야 한다.")
+    @Test
+    void addResultTest() {
+        LottoResult lottoResult = new LottoResult();
+        LottoPrize prize = LottoPrize.FIRST_PRIZE;
+        int count = 1;
+
+        lottoResult.addResult(prize, count);
+
+        assertEquals(lottoResult.getResultMap().get(prize), Integer.valueOf(count));
+        assertEquals(lottoResult.getProfit(), count * prize.getPrizeAmount());
+    }
+
+    @DisplayName("상금에 대한 결과를 정상적으로 누적하고 수익을 계산해야 한다.")
+    @Test
+    void addResultWithDifferentPrizeTest() {
+        LottoResult lottoResult = new LottoResult();
+        LottoPrize prize1 = LottoPrize.FIRST_PRIZE;
+        LottoPrize prize2 = LottoPrize.SECOND_PRIZE;
+        LottoPrize prize3 = LottoPrize.THIRD_PRIZE;
+        int count1 = 1;
+        int count2 = 2;
+        int count3 = 3;
+
+        lottoResult.addResult(prize1, count1);
+        lottoResult.addResult(prize2, count2);
+        lottoResult.addResult(prize3, count3);
+
+        assertEquals(lottoResult.getResultMap().get(prize1), count1);
+        assertEquals(lottoResult.getResultMap().get(prize2), count2);
+        assertEquals(lottoResult.getResultMap().get(prize3), count3);
+        assertEquals(lottoResult.getProfit(),
+                (count1 * prize1.getPrizeAmount()) +
+                (count2 * prize2.getPrizeAmount()) +
+                (count3 * prize3.getPrizeAmount())
+        );
+
+    }
 }
--- src/test/java/service/LottoResultServiceTest.java
+++ src/test/java/service/LottoResultServiceTest.java
@@ -26,20 +26,17 @@ public class LottoResultServiceTest {
     @DisplayName("로또 당첨 번호,보너스 번호와 비교해서 당첨 결과를 계산한다.(1등일 경우)")
     @Test
     void calculateLottoResultTestOne() {
-        // given
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumbers = 7;
         LottoManager lottoManager = new LottoManager(winningNumbers);
         lottoManager.addBonusNumber(bonusNumbers);
 
-        // when
         List<Lotto> lottoList = List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 6))
         );
         LottoResult lottoResult = lottoService.calculateLottoResult(lottoManager, lottoList);
         LottoPrize lottoPrize = LottoPrize.FIRST_PRIZE;
 
-        // then
         HashMap<LottoPrize, Integer> result = new HashMap<>();
         result.put(lottoPrize, 1);
         assertEquals(lottoResult.getResultMap(), result);
@@ -48,20 +45,17 @@ public class LottoResultServiceTest {
     @DisplayName("로또 당첨 번호,보너스 번호와 비교해서 당첨 결과를 계산한다.(2등일 경우)")
     @Test
     void calculateLottoResultTestTwo() {
-        // given
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumbers = 7;
         LottoManager lottoManager = new LottoManager(winningNumbers);
         lottoManager.addBonusNumber(bonusNumbers);
 
-        // when
         List<Lotto> lottoList = List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 7))
         );
         LottoResult lottoResult = lottoService.calculateLottoResult(lottoManager, lottoList);
         LottoPrize lottoPrize = LottoPrize.SECOND_PRIZE;
 
-        // then
         HashMap<LottoPrize, Integer> result = new HashMap<>();
         result.put(lottoPrize, 1);
         assertEquals(lottoResult.getResultMap(), result);
@@ -70,20 +64,17 @@ public class LottoResultServiceTest {
     @DisplayName("로또 당첨 번호,보너스 번호와 비교해서 당첨 결과를 계산한다.(3등일 경우)")
     @Test
     void calculateLottoResultTestThree() {
-        // given
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumbers = 7;
         LottoManager lottoManager = new LottoManager(winningNumbers);
         lottoManager.addBonusNumber(bonusNumbers);
 
-        // when
         List<Lotto> lottoList = List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 8))
         );
         LottoResult lottoResult = lottoService.calculateLottoResult(lottoManager, lottoList);
         LottoPrize lottoPrize = LottoPrize.THIRD_PRIZE;
 
-        // then
         HashMap<LottoPrize, Integer> result = new HashMap<>();
         result.put(lottoPrize, 1);
         assertEquals(lottoResult.getResultMap(), result);
@@ -92,19 +83,16 @@ public class LottoResultServiceTest {
     @DisplayName("로또 당첨 번호,보너스 번호와 비교해서 당첨 결과를 계산한다.(4등일 경우)")
     @Test
     void calculateLottoResultTestFour() {
-        // given
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumbers = 7;
         LottoManager lottoManager = new LottoManager(winningNumbers);
         lottoManager.addBonusNumber(bonusNumbers);
 
-        // when
         List<Lotto> lottoList = List.of(
                 new Lotto(List.of(1, 2, 3, 4, 8, 9))
         );
         LottoResult lottoResult = lottoService.calculateLottoResult(lottoManager, lottoList);
 
-        // then
         HashMap<LottoPrize, Integer> result = new HashMap<>();
         result.put(LottoPrize.FOURTH_PRIZE, 1);
         assertEquals(lottoResult.getResultMap(), result);
@@ -113,13 +101,11 @@ public class LottoResultServiceTest {
     @DisplayName("로또 당첨 번호,보너스 번호와 비교해서 당첨 결과를 계산한다.(5등일 경우)")
     @Test
     void calculateLottoResultTestFive() {
-        // given
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumbers = 7;
         LottoManager lottoManager = new LottoManager(winningNumbers);
         lottoManager.addBonusNumber(bonusNumbers);
 
-        // when
         List<Lotto> lottoList = List.of(
                 new Lotto(List.of(1, 2, 3, 8, 9, 10))
         );
@@ -127,20 +113,17 @@ public class LottoResultServiceTest {
         HashMap<LottoPrize, Integer> result = new HashMap<>();
         result.put(LottoPrize.FIFTH_PRIZE, 1);
 
-        // then
         assertEquals(lottoResult.getResultMap(), result);
     }
 
     @DisplayName("로또 당첨 번호,보너스 번호와 비교해서 당첨 결과를 계산한다.(일치하는게 없는 경우)")
     @Test
     void calculateLottoResultTestNothingMatch() {
-        // given
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
         int bonusNumbers = 7;
         LottoManager lottoManager = new LottoManager(winningNumbers);
         lottoManager.addBonusNumber(bonusNumbers);
 
-        // when
         List<Lotto> lottoList = List.of(
                 new Lotto(List.of(8, 9, 10, 11, 12, 13))
         );
@@ -148,7 +131,6 @@ public class LottoResultServiceTest {
         HashMap<LottoPrize, Integer> result = new HashMap<>();
         result.put(LottoPrize.NOTHING_MATCH, 1);
 
-        // then
         assertEquals(lottoResult.getResultMap(), result);
     }
 

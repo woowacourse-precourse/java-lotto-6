7ec61c927320dae371d2b3236885a634e9c59576
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-11-08 21:42:34 +0900

    test : 테스트 코드 수정

--- src/test/java/lotto/LottoContollerTest.java
+++ src/test/java/lotto/LottoContollerTest.java
@@ -65,37 +65,4 @@ public class LottoContollerTest {
             assertThat(rank).isEqualTo(i + 1);
         }
     }
-
-    @DisplayName("각 등수를 한 횟수를 확인하여 총 상금을 구한다.")
-    @Test
-    void testCalculateResult() {
-        LottoController lottoProcedure = new LottoController();
-        double result = 0;
-        List<Integer> rankCount = List.of(0, 1, 1, 1, 1, 1);
-        List<Double> reward = List.of(0.0, 200000000.0, 30000000.0, 1500000.0, 50000.0, 5000.0);
-
-        for (int i = 0; i < rankCount.size(); i++) {
-            result += rankCount.get(i) * reward.get(i);
-        }
-
-        assertThat(result).isEqualTo(231555000.0);
-    }
-
-    @DisplayName("당첨 번호와 구입한 로또의 번호가 일치하면 count를 올린다.")
-    @Test
-    void testIncreaseCount() {
-        List<List<Integer>> purchasedLottoNumber = List.of(List.of(1, 2, 3, 4, 5, 6), List.of(7, 8, 9, 10, 11, 12));
-        List<Integer> winningNumber = List.of(6, 5, 4, 3, 2, 1);
-        double count = 0;
-
-        for (int i = 0; i < purchasedLottoNumber.size(); i++) {
-            for (int j = 0; j < 6; j++) {
-                if (purchasedLottoNumber.get(i).contains(winningNumber.get(j))) {
-                    count += 1;
-                }
-            }
-        }
-
-        assertThat(count).isEqualTo(6);
-    }
 }

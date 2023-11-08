74a0ddd7eb2ca4cf49e759effbfc1ad7373270bb
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 15:14:33 +0900

    feat(view) : 보너스 번호 입력 메시지 출력 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,7 +6,12 @@ public class OutputView {
     public void printPurchaseAmountInputMessage() {
         System.out.println(OutputMessageConstants.PURCHASE_AMOUNT_INPUT_MESSAGE);
     }
+
     public void printWinningNumbersInputMessage() {
         System.out.println(OutputMessageConstants.WINNING_NUMBER_INPUT_MESSAGE);
+    }
+
+    public void printBonusNumberInputMessage() {
+        System.out.println(OutputMessageConstants.BONUS_NUMBER_INPUT_MESSAGE);
     }
 }

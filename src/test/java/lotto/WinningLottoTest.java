de7a37a9fb5979d0c23cca1dd9a3aae2b25e6296
MyStraw <kikongdosa@gmail.com>
2023-11-08 09:39:15 +0900

    Test: 추가 WinningLotto 객체 생성 테스트
    WinningLotto가 잘 작동하는지 동작여부 테스트

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
@@ -27,5 +28,15 @@ class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("범위를 벗어났습니다.");
+    }
+
+    @DisplayName("WinningLotto 객체가 잘 생성되어야 한다")
+    @Test
+    void createWinningLottoInstance() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 10;
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        assertThat(winningLotto.getLotto()).isEqualTo(lotto);
+        assertThat(winningLotto.getBonusNumber()).isEqualTo(bonusNumber);
     }
 }

c107bf4e2b37fbfb2c5dafc6458316073e599dcd
{psm4171} <{kkjjhh4171@naver.com}>
2023-11-08 15:51:06 +0900

    refactoring: Enum으로 저장하는 Rank

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,31 +1,27 @@
 package lotto.model;
 
 public enum Rank {
-    FIND(1, 0),
+
     THIRD(3, 5000),
     FOURTH(4, 50000),
     FIFTH(5, 1500000),
     FIFTH_WITH_BONUS(7, 30000000),
     SIXTH(6, 2000000000);
 
-    private final int label;
+    private final int score;
     private final int price;
 
-    Rank(int label, int price) {
-        this.label = label;
+    Rank(int score, int price){
+        this.score = score;
         this.price = price;
     }
 
-    public int getPrice() {
+    public int getPrice(){
         return price;
     }
 
-    public Rank valueOf(int label) {
-        for (Rank rank : Rank.values()) {
-            if (label == rank.label) {
-                return rank;
-            }
-        }
-        return null;
+    public int getScore(){
+        return score;
     }
+
 }

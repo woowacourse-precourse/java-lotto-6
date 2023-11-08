b55e91cd872820afcbe6d8872928baeafbff83c2
junseoks-99 <angec5645@naver.com>
2023-11-08 19:43:48 +0900

    refactor: 예외 메세지 [ERROR] 추가

--- src/main/java/util/ValidationPriceMessages.java
+++ src/main/java/util/ValidationPriceMessages.java
@@ -2,7 +2,7 @@ package util;
 
 public enum ValidationPriceMessages {
 
-    NOT_A_NUMBER("숫자를 입력해주세요."),
+    NOT_A_NUMBER("[ERROR] 숫자를 입력해주세요."),
     INVALID_REMAINDER("[ERROR] 숫자는 1000단위로 입력해야 합니다."),
     NEGATIVE_NUMBER("[ERROR] 양수만 입력해야 합니다.");
 

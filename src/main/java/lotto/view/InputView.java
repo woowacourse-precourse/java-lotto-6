26a8a354816db91ca2d59b2a5ce03c8873d0cf2b
choiejae <ejae8319@gmail.com>
2023-11-08 16:38:03 +0900

    fix(InputView): README.md 실행 결과 예시에 맞도록 개행 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "\n보너스 번호를 입력해 주세요.";
 
     public String inputPurchaseAmount() {
         return inputForMessage(PURCHASE_AMOUNT_INPUT_MESSAGE);

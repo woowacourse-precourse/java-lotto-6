ecdf0979806c4a3756905bd0d348d88efaa7b2f7
jy3026 <jy_3026@naver.com>
2023-11-08 22:44:04 +0900

    fix : 누락 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,6 +11,7 @@ public class Application {
     public static void main(String[] args) {
         int purchaseAmount = getPurchaseAmount();
         List<Lotto> purchasedLottoTickets = generateLottoTickets(purchaseAmount);
+        List<Integer> winningNumbers = getWinningNumbers();
     }
 
     private static int getPurchaseAmount() {
@@ -43,5 +44,52 @@ public class Application {
             lottoTickets.add(new Lotto(numbers));
         }
         return lottoTickets;
+    }
+
+    private static List<Integer> getWinningNumbers() {
+        while (true) {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            String input = Console.readLine();
+
+            try {
+                return parseNumbers(input, "로또 번호는 1부터 45 사이의 6개의 중복되지 않는 숫자여야 합니다.");
+            } catch (IllegalArgumentException e) {
+                System.err.println("[ERROR] 숫자 또는 6개의 숫자를 입력해주셔야 합니다.");
+            }
+        }
+    }
+
+    private static List<Integer> parseNumbers(String input, String errorMessage) {
+        List<Integer> numbers = parseNumbers(input);
+        validateNumbers(numbers, errorMessage);
+        return numbers;
+    }
+
+    private static List<Integer> parseNumbers(String input) {
+        String[] numbersAsString = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String num : numbersAsString) {
+            numbers.add(Integer.parseInt(num));
+        }
+        return numbers;
+    }
+
+    private static void validateNumbers(List<Integer> numbers, String errorMessage) {
+        if (numbers.size() != 6 || !areNumbersWithinRange(numbers, 1, 45) || areNumbersDuplicated(numbers)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private static boolean areNumbersWithinRange(List<Integer> numbers, int min, int max) {
+        for (int number : numbers) {
+            if (number < min || number > max) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean areNumbersDuplicated(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
     }
 }

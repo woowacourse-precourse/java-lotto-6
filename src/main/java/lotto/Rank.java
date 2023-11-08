bb10fcbb7e691dfdaf6b41c6d31917d2f98606cf
Minhyeok Song <minhyeok.dev@gmail.com>
2023-11-08 18:34:38 +0900

    Docs: 코드 정리

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,17 +1,17 @@
 package lotto;
 
 public enum Rank {
-    FIRST("1등", "2,000,000,000원",0),
-    SECOND("2등", "30,000,000원",0),
-    THIRD("3등", "1,500,000원",0),
-    FOURTH("4등", "50,000원",0),
-    FIFTH("5등", "5,000원",0);
+    FIRST("1등", "2,000,000,000원", 0),
+    SECOND("2등", "30,000,000원", 0),
+    THIRD("3등", "1,500,000원", 0),
+    FOURTH("4등", "50,000원", 0),
+    FIFTH("5등", "5,000원", 0);
 
     private final String rank;
     private final String reward;
     private int count = 0;
 
-    Rank(String rank, String reward,int count) {
+    Rank(String rank, String reward, int count) {
         this.rank = rank;
         this.reward = reward;
         this.count = count;

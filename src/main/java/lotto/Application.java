88798285785de556cd2c9551c27c22a8ea7fc63d
BigBird <waujoxc@gmail.com>
2023-11-08 23:15:02 +0900

    refactor(view): ResultView, UserInputView 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,6 @@ import lotto.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        GameController.play();
+        GameController.run();
     }
 }
--- src/main/java/lotto/view/UserInputView.java
+++ src/main/java/lotto/view/UserInputView.java
@@ -2,124 +2,88 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.exception.ErrorMessages;
-import lotto.model.LottoTicket;
+import lotto.model.LottoResult;
+import lotto.model.LottoTickets;
 
 public class UserInputView {
 
-    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_BUY_AMOUNT_MESSAGE = "구매금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String DELIMITER = ",";
-    private static final int MIN_PURCHASE_AMOUNT = 1000;
-    private static final int ZERO = 0;
-    private static final int MAX_LOTTO_LENGTH = 6;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_NUMBER_RANGE = 1;
+    private static final int MAX_LOTTO_NUMBER_RANGE = 45;
 
-    public static LottoTicket inputPurchaseAmount() {
-        int lottoTicketCount;
-        int purchaseAmount;
+    public static LottoTickets inputBuyAmount() {
         while (true) {
-            System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+            System.out.println(INPUT_BUY_AMOUNT_MESSAGE);
             String input = Console.readLine();
             try {
-                purchaseAmount = validateNumber(input);
-                validateMinimumAmount(purchaseAmount);
-                lottoTicketCount = validateDivisible(purchaseAmount);
-                break;
+                int buyAmount = validateNumber(input);
+                return new LottoTickets(buyAmount);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
-        return new LottoTicket(lottoTicketCount, purchaseAmount);
     }
 
-    private static int validateDivisible(int purchaseAmount) {
-        if ((purchaseAmount % MIN_PURCHASE_AMOUNT != ZERO)) {
-            throw new IllegalArgumentException(ErrorMessages.NON_DIVISIBLE_PURCHASE_AMOUNT.getMessage());
-        }
-        return purchaseAmount / MIN_PURCHASE_AMOUNT;
-    }
-
-    private static void validateMinimumAmount(int purchaseAmount) {
-        if ((purchaseAmount / MIN_PURCHASE_AMOUNT <= ZERO)) {
-            throw new IllegalArgumentException(ErrorMessages.MIN_PURCHASE_AMOUNT.getMessage());
-        }
-    }
-
-    private static int validateNumber(String purchaseAmount) {
-        if (!purchaseAmount.matches("\\d+")) {
-            throw new IllegalArgumentException(ErrorMessages.INVALID_PURCHASE_AMOUNT.getMessage());
-        }
-        return Integer.parseInt(purchaseAmount);
-    }
-
-    public static List<Integer> inputWinningNumber() {
+    public static List<Integer> inputWinningNumbers() {
+        List<Integer> winningNumbers = new ArrayList<>();
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         String input = Console.readLine();
-        String[] numbers = input.split(DELIMITER);
-        validateLength(numbers);
-        validateDigitAndBlank(numbers);
-
-        List<Integer> winningNumbers = new ArrayList<>();
-        for (String number : numbers) {
-            String trimmedNumber = number.trim();
-            int parsedNumber = Integer.parseInt(trimmedNumber);
-
-            validateDuplicate(winningNumbers, parsedNumber);
-
-            winningNumbers.add(parsedNumber);
-        }
+        String[] inputNumbers = input.split(DELIMITER);
+        validateLength(inputNumbers);
+        validateDuplicate(winningNumbers, inputNumbers);
+        validateRange(winningNumbers);
         return winningNumbers;
     }
 
-    private static void validateDuplicate(List<Integer> winningNumbers, int parsedNumber) {
-        if (winningNumbers.contains(parsedNumber)) {
-            throw new IllegalArgumentException(ErrorMessages.INPUT_DUPLICATE_ERROR.getMessage());
+    private static void validateRange(List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessages.INVALID_RANGE.getMessage());
+            }
         }
     }
 
-    private static void validateDigitAndBlank(String[] numbers) {
-        for (String number : numbers) {
-            String trimmedNumber = number.trim();
-            validateDigit(trimmedNumber);
-            validateBlank(trimmedNumber);
+    private static void validateDuplicate(List<Integer> winningNumbers, String[] inputNumbers) {
+        for (String numStr : inputNumbers) {
+            int num = validateNumber(numStr);
+            if (winningNumbers.contains(num)) {
+                throw new IllegalArgumentException(ErrorMessages.DUPLICATE_NUMBER.getMessage());
+            }
+            winningNumbers.add(num);
         }
     }
 
-    private static void validateBlank(String trimmedNumber) {
-        if (trimmedNumber.contains(" ") || trimmedNumber.contains("\t")) {
-            throw new IllegalArgumentException(ErrorMessages.NON_DIGIT_AND_COMMA_ERROR.getMessage());
+    private static void validateLength(String[] inputNumbers) {
+        if (inputNumbers.length != 6) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_WINNING_NUMBER_COUNT.getMessage());
         }
     }
 
-    private static void validateDigit(String trimmedNumber) {
-        if (!trimmedNumber.matches("\\d+")) {
-            throw new IllegalArgumentException(ErrorMessages.NON_DIGIT_ERROR.getMessage());
-        }
-    }
-
-    private static void validateLength(String[] numbers) {
-        if (numbers.length != MAX_LOTTO_LENGTH) {
-            throw new IllegalArgumentException(ErrorMessages.MAX_LENGTH_ERROR.getMessage());
-        }
-    }
-
-    public static List<Integer> inputBonusNumber(List<Integer> winningNumbers) {
+    public static int inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         String input = Console.readLine();
         int bonusNumber = validateNumber(input);
         validateRange(bonusNumber);
-        winningNumbers.add(bonusNumber);
-        return winningNumbers;
+        return bonusNumber;
     }
 
-    private static int validateRange(int bonusNumber) {
-        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException(ErrorMessages.INPUT_RANGE_ERROR.getMessage());
+    private static int validateNumber(String purchaseAmount) {
+        if (!purchaseAmount.matches("\\d+")) {
+            throw new IllegalArgumentException(ErrorMessages.INPUT_INVALID_NUMBER.getMessage());
         }
-        return bonusNumber;
+        return Integer.parseInt(purchaseAmount);
+    }
+
+    private static void validateRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_RANGE.getMessage());
+        }
     }
 }

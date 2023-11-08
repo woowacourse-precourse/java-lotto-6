bc6ff85ee65d39a36daff53c0ea1ecd03fb7cfac
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-08 17:49:08 +0900

    feat: 로또 당첨 번호 입력 상수 관리 기능 추가

--- src/main/java/lotto/Constants/LottoGameInputConstants.java
+++ src/main/java/lotto/Constants/LottoGameInputConstants.java
@@ -1,15 +1,15 @@
-package lotto.Message;
+package lotto.Constants;
 
 public enum LottoGameInputConstants {
     LOTTO_NUM_AMOUNT(6),
     LOTTO_WINNING_NUM_AMOUNT(6),
-    LOTTO_BONUS_NUM_AMOUNT(1),
     LOTTO_MIN_NUM(1),
     LOTTO_MAX_NUM(45);
 
 
     private int num;
-    LottoGameInputConstants(int num){
+
+    LottoGameInputConstants(int num) {
         this.num = num;
     }
 

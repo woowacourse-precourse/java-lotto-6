d9d8eb5ff867f57648f00e393282ba9006bfc5dd
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:38:03 +0900

    feat : getNumbers 함수 생성

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,9 +1,15 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoTicket {
     private final Lotto lotto;
 
     public LottoTicket(Lotto lotto) {
         this.lotto = lotto;
+    }
+
+    public List<Integer> getNumbers() {
+        return lotto.getNumbers();
     }
 }

dae62ecf2d35d825ffc9c62c050c4732f61b22f0
masonkimseoul <masonkimseoul@gmail.com>
2023-11-08 14:00:07 +0900

    feat : 당첨 번호 입력 기능 구현

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,11 +1,17 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputView {
 
     public static int getPurchaseAmount() {
         String input = Console.readLine();
         return Validation.isValidPurchaseAmount(input);
+    }
+
+    public static List<Integer> getWinnerNumbers() {
+        String input = Console.readLine();
+        return Validation.isValidWinnerNumbers(input);
     }
 }

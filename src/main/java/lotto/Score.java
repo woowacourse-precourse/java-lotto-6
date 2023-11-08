83204550b3b7116ceb9c46a039af61513abbde00
K-KY <kyuyoungk@naver.com>
2023-11-08 11:25:21 +0900

    Feat(Score) : 점수 결과를 수정 불가능한 리스트로 리턴한다.

--- src/main/java/lotto/Score.java
+++ src/main/java/lotto/Score.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Score {
@@ -7,15 +8,18 @@ public class Score {
 
     public Score(List<Integer> scores) {
         this.scores = scores;
-        getPrize();
     }
 
-    public int getPrize() {
+    public long getPrize() {
         int revenue = 0;
         for (int i = 0; i < scores.size(); i++) {
             int score = scores.get(i);
             revenue += Rank.prize(score);
         }
         return revenue;
+    }
+
+    public List<Integer> getScores() {
+        return Collections.unmodifiableList(scores);
     }
 }

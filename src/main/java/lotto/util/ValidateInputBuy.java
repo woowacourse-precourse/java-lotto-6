9c3340da22906b98dbf901a6dd09f139e7c52ba0
KongJunWoo <joonoo3@naver.com>
2023-11-08 22:20:35 +0900

    feat : 구매 입력 값 통합 검증

--- src/main/java/lotto/util/ValidateInputBuy.java
+++ src/main/java/lotto/util/ValidateInputBuy.java
@@ -2,6 +2,14 @@ package lotto.util;
 
 public class ValidateInputBuy {
 
+    public static int validateInputBuy(String inputValue) {
+
+        int positiveInteger = ValidateNumber.isPositiveInteger(inputValue);
+        isThousand(positiveInteger);
+
+        return positiveInteger;
+    }
+
     public static void isThousand(int inputValue){
 
         if(inputValue % 1000 != 0){

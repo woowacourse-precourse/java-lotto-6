ae112382b56b68e5edd8df7ec097ec86f9c076cb
devhoya97 <diajbh123@gmail.com>
2023-11-08 18:25:29 +0900

    style(domain): 더 가독성 좋은 변수명으로 변경

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -3,16 +3,16 @@ package lotto.domain;
 import java.util.Map;
 
 public class ProfitCalculator {
-    private final Map<Prize, Integer> lottosResult;
+    private final Map<Prize, Integer> winningResult;
 
-    public ProfitCalculator(Map<Prize, Integer> lottosResult) {
-        this.lottosResult = lottosResult;
+    public ProfitCalculator(Map<Prize, Integer> winningResult) {
+        this.winningResult = winningResult;
     }
 
     public long getTotalProfit() {
         long allPrizeProfit = 0;
-        for (Prize prize : lottosResult.keySet()) {
-            long prizeProfit = (long) prize.getPrizeProfit() * lottosResult.get(prize);
+        for (Prize prize : winningResult.keySet()) {
+            long prizeProfit = (long) prize.getPrizeProfit() * winningResult.get(prize);
             allPrizeProfit += prizeProfit;
         }
         return allPrizeProfit;

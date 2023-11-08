3b2a770e3a60022349f8b2c62a39348b4ead4560
lkdcode <dlrlejr1@naver.com>
2023-11-08 20:33:50 +0900

    Refactor [InputValidation] : 구매금 입력값 유효성 검사 수
    구매금 최대 1,000,000,000원 미만 유효성 검사 메서드 추가

--- src/main/java/lotto/module/console/input/validation/InputValidation.java
+++ src/main/java/lotto/module/console/input/validation/InputValidation.java
@@ -8,6 +8,7 @@ import lotto.global.exception.LottoException;
 public class InputValidation {
     private static final String LOTTO_NUMBER_RANGE_REGEX = "^[1-9]|[1-3][0-9]|4[0-5]$"; // 1에서 45까지의 숫자를 나타내는 정규표현식
     private static final String ONLY_NUMBER_REGEX = "\\d+";
+    private static final int PURCHASE_AMOUNT_MAXIMUM = 9;
 
     private InputValidation() {
     }
@@ -16,9 +17,20 @@ public class InputValidation {
         return new InputValidation();
     }
 
-    public void validatePurchaseAmountString(final String inputPurchaseAmount) {
+    public void validatePurchaseAmount(final String inputPurchaseAmount) {
+        validatePurchaseAmountString(inputPurchaseAmount);
+        validatePurchaseAmountMaximum(inputPurchaseAmount);
+    }
+
+    private void validatePurchaseAmountString(final String inputPurchaseAmount) {
         if (!inputPurchaseAmount.matches(ONLY_NUMBER_REGEX)) {
             throw new IllegalArgumentException(LottoException.INVALID_PURCHASE_AMOUNT_STRING_VALIDATION.getDefaultMessage());
+        }
+    }
+
+    private void validatePurchaseAmountMaximum(final String inputPurchaseAmount) {
+        if (inputPurchaseAmount.length() >= PURCHASE_AMOUNT_MAXIMUM) {
+            throw new IllegalArgumentException(LottoException.MAXIMUM_PURCHASE_AMOUNT_NOT_MET_VALIDATION.getDefaultMessage());
         }
     }
 

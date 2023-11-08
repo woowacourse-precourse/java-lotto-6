faeb045fe61168aa4b2c854ab6236807aff3cc1e
hsyoodev <hsyoodev@gmail.com>
2023-11-08 15:45:48 +0900

    feat(lotto): 로또 번호의 숫자 범위는 1~45까지이다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,8 @@ import java.util.Set;
 public class Lotto {
 
     private static final int NUMBERS_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     private final List<Integer> numbers;
 
@@ -18,6 +20,9 @@ public class Lotto {
         if (isNotSizeSix(numbers) || isDuplicateNumbers(numbers)) {
             throw new IllegalArgumentException(Message.INVALID_NUMBERS.getMessage());
         }
+        if (isOutRange(numbers)) {
+            throw new IllegalArgumentException(Message.INVALID_RANGE.getMessage());
+        }
     }
 
     // TODO: 추가 기능 구현
@@ -25,12 +30,17 @@ public class Lotto {
         return numbers.size() != NUMBERS_SIZE;
     }
 
+    private boolean isOutRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch((number) -> (number < MIN_NUMBER || number > MAX_NUMBER));
+    }
+
     private boolean isDuplicateNumbers(List<Integer> numbers) {
         return Set.copyOf(numbers).size() != NUMBERS_SIZE;
     }
 
     enum Message {
-        INVALID_NUMBERS("로또는 중복되지 않는 6개의 숫자만 가능합니다.");
+        INVALID_NUMBERS("로또는 중복되지 않는 6개의 숫자만 가능합니다."),
+        INVALID_RANGE("로또 번호의 숫자 범위는 1~45만 가능합니다.");
         private final String message;
 
         Message(String message) {

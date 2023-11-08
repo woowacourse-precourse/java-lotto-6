165cd4033fd8f5ac1c0c7d0cab175fb96689b69f
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 14:34:29 +0900

    feat: 로또를 주어진 개수만큼 발행하는 기능

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(int numberOfLottos) {
+        this.lottos = issueLotto(numberOfLottos);
+    }
+
+    private List<Lotto> issueLotto(int numberOfLottos) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < numberOfLottos; i++) {
+            Lotto lotto = new Lotto();
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+}

e5495ca0004d85af3db654c67ca59d025c34947e
namtndus <rnrmf0721@gmail.com>
2023-11-08 11:02:49 +0900

    feat(CalculateLotto): 기능 구현

---
+++ src/main/java/lotto/CalculateLotto.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import java.util.List;
+
+public class CalculateLotto {
+
+    private final List<Integer> answerNumbsers;
+
+    public CalculateLotto(String numbers){
+        answerNumbsers = new Lotto(numbers).getNumbers();
+    }
+
+    public int isCheckedLottoAnswer(Lotto lotto){
+        int size = 0;
+
+        for (Integer num : lotto.getNumbers()){
+            if(answerNumbsers.contains(num)){
+                size++;
+            }
+        }
+        return size;
+    }
+
+    public boolean isCheckedBonusNumber(int bonusNumber){
+        if(answerNumbsers.contains(bonusNumber)){
+            return true;
+        }
+        return false;
+    }
+}

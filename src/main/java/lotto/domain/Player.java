cf56e40ba70cbe27fae682ab8c196b60c0137cf8
mung96 <mung5079@gmail.com>
2023-11-08 19:36:24 +0900

    feat: countRanking() 구현
    로또 1장의 결과를 받아서 해시맵에 등수별 횟수를 담음

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,6 +1,15 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player {
+    public static final Map<RankingStatus,Integer> rankings = new HashMap<RankingStatus, Integer>();
+    public Player(){
+        Arrays.stream(RankingStatus.values())
+                .forEach(rank -> rankings.put(rank, 0));
+        }
     public int countTickets(int money){
         validateDivisibleMoney(money);
         return money / 1000;
@@ -10,4 +19,9 @@ public class Player {
             throw new IllegalArgumentException("[ERROR]1,000원 단위로만 입력가능합니다.");
         }
     }
+    public Map<RankingStatus, Integer> countRanking(RankingStatus rank) {
+        rankings.put(rank,rankings.get(rank)+1);
+        return rankings;
+    }
+
 }

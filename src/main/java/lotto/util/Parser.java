79f68ba69c5d7df6cde8137d7ae12889ef4c8d37
Hyso <hsk7953@gmail.com>
2023-11-08 16:23:06 +0900

    feat: 콤마로 파싱하는 기능 추가

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -6,5 +6,8 @@ public class Parser {
     public static int convertInt (String input) {
         return Integer.parseInt(input);
     }
+    public static List<String> splitComma (String input) {
+        return List.of(input.split(","));
+    }
 
 }

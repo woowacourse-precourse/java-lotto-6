47f2afabad7ad5788837c0b1f5c18bdca6886ee2
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:03:25 +0900

    feat : enum Rank 변수 및 상수 선언

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
-public class Rank {
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000, true), // 보너스 번호 일치를 구분하기 위한 추가 필드
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final boolean matchBonus;
+
+    
 }

69da9f30ee33d18b9c70860bf4c2c1fed3e6ca38
minoooo119 <minoth0119@naver.com>
2023-11-08 20:59:28 +0900

    [ADD] RankCalculateModel.java: 당첨 등수 게산 하는 model 추가

--- src/main/java/Model/RankCalculateModel.java
+++ src/main/java/Model/RankCalculateModel.java
@@ -3,27 +3,54 @@ package Model;
 import lotto.Lotto;
 import lotto.Rank;
 
-import java.util.List;
+import javax.management.relation.RelationNotification;
+import java.util.*;
 
-public class RankCalcutateModel {
-    private List<Rank> rankList;
-    final private List<Lotto> lottoList;
-    final private Lotto winningLotto;
-    final private Integer bonusNum;
-    public RankCalcutateModel(List<Lotto> lottoList, Lotto winningLotto, Integer bonusNum){
-        this.lottoList = lottoList;
-        this.winningLotto = winningLotto;
-        this.bonusNum = bonusNum;
-        makeRankList();
-    }
-    private void makeRankList(){
+public class RankCalculateModel {
+    private final List<Rank> rankList=new ArrayList<>();
+    private final Integer firstRank;
+    private final Integer secondRank;
+    private final Integer thirdRank;
+    private final Integer fourthRank;
+    private final Integer fifthRank;
+    private final Integer boomRank;
+    public RankCalculateModel(List<Lotto> lottoList, Lotto winningLotto, Integer bonusNum){
         for(Lotto lotto:lottoList){
-            Rank rank=Rank.findLottoRank(lotto,winningLotto,bonusNum);
-            rankList.add(rank);
+            rankList.add(Rank.findLottoRank(lotto,winningLotto,bonusNum));
         }
+        firstRank= Collections.frequency(rankList, Rank.first);
+        secondRank= Collections.frequency(rankList, Rank.second);
+        thirdRank= Collections.frequency(rankList, Rank.third);
+        fourthRank= Collections.frequency(rankList, Rank.fourth);
+        fifthRank= Collections.frequency(rankList, Rank.fifth);
+        boomRank= Collections.frequency(rankList, Rank.boom);
     }
     public List<Rank> getRankList(){
-        return rankList;
+        return this.rankList;
     }
-
+    public Integer getFirstRank(){
+        return firstRank;
+    }
+    public Integer getSecondRank(){
+        return secondRank;
+    }
+    public Integer getThirdRank(){
+        return thirdRank;
+    }
+    public Integer getFourthRank(){
+        return fourthRank;
+    }
+    public Integer getFifthRank(){
+        return fifthRank;
+    }
+    public Integer getBoomRank(){
+        return boomRank;
+    }
+    public void printRank(){
+        System.out.println("3개 일치 (5,000원) - "+fifthRank+"개");
+        System.out.println("4개 일치 (50,000원) - "+fourthRank+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+thirdRank+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+secondRank+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+firstRank+"개");
+    }
 }

44c0220aa5287f1b4bdcfdfa9105ec771ab76d01
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-08 20:09:56 +0900

    test(LottoTest): 단위 테스트 작성
    1. createLottoByUnderSize
     - 로또 번호의 개수가 5개 이하인 경우 예외가 발생하는 지 확인
    
    2. createLottoByOutrangedNumber
     - 로또 번호 중 하나라도 정해진 범위(1이상 45이하)를 벗어나면 예외가 발생하는 지 확인
    
    3. getImmutableSortedNumbersReturnsAscendingOrderList
     - 오름차순으로 정렬된 리스트가 반환되는 지 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,12 +6,20 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.fail;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 5개 보다 적으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -23,5 +31,29 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 중 하나라도 정해진 값의 범위를 벗어난 경우 예외가 발생한다.")
+    @Test
+    void createLottoByOutrangedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 리스트가 오름차순이어야 한다.")
+    @Test
+    void getImmutableSortedNumbersReturnsAscendingOrderList() {
+        try {
+            Lotto lotto = new Lotto(List.of(3, 1, 5, 2, 6, 4));
+            int pre = 0;
+            for (int number : lotto.getImmutableSortedNumbers()) {
+                if (pre > number) {
+                    throw new IllegalStateException("[테스트 실패] 오름차순으로 정렬되어 있지 않습니다.");
+                }
+            }
+        } catch (IllegalStateException e) {
+            fail(e.getMessage());
+        }
+
+    }
 }

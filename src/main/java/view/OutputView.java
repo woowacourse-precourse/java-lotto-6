dde6d2ba564c21b476b93ea40934630d1b9a7342
yunju-m <dbswnfnal@naver.com>
2023-11-08 19:23:36 +0900

    Refactor(OutputView): 🔨 필요없는 코드 제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,8 +9,6 @@ public class OutputView {
     private static final String LOTTO_TICKET_MESSAGE = "개를 구매했습니다.";
     private static final String LUCKEY_WINNING_STATSICS_MESSAGE = "당첨 통계";
     private static final String STICK = "-";
-    private static final String[] REWARD = { " (5,000원) ", " (50,000원) ", " (1,500,000원) ", " (30,000,000원) ",
-            " (2,000,000,000원) " };
 
     public void printLottoTicketMessage(LottoMoney lottoMoney) {
         System.out.println(LINEBREAK + lottoMoney.getLottoTicket() + LOTTO_TICKET_MESSAGE);
@@ -22,11 +20,6 @@ public class OutputView {
 
     public void printLottoResultMessage() {
         System.out.println(LINEBREAK + LUCKEY_WINNING_STATSICS_MESSAGE + LINEBREAK + STICK.repeat(3));
-    }
-
-    public void printLottoResult(int winningNumber, int rewardIndex, String bonusMessage, int lottoWinnerCount) {
-        System.out.println(winningNumber + "개 일치" + bonusMessage
-                + REWARD[rewardIndex] + STICK + " " + lottoWinnerCount + "개");
     }
 
     public void printRevenuePercentage(double revenuePercentage) {

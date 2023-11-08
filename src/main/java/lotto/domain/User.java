e0c83be69948eb1b71079fc730cb00ed0950edb0
SuhyungK <dook4966@gmail.com>
2023-11-08 14:01:49 +0900

    add(User): 당첨 정보 저장하는 Map 타입 인스턴스 변수 생성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,13 +1,42 @@
 package lotto.domain;
 
+import lotto.constants.ResultConfig;
+
+import javax.xml.transform.Result;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static lotto.constants.ResultConfig.*;
 
 public class User {
     public int payment;
     public final List<Lotto> lottos;
 
-    public User(int payment) {
+    public final Map<String, Integer> totalResult;
+
+    public User (int payment) {
         this.payment = payment;
         this.lottos = Lotto.publish(payment);
+        this.totalResult = Arrays.stream(values()).collect(Collectors.toMap(Enum::name, value->0));
+    }
+
+    public void processCheckResult (String count, boolean getBonus) {
+        if (count.equals("5") && getBonus) count += "0";
+        saveResult(count);
+    }
+
+    private void saveResult (String message) {
+        totalResult.put(message, totalResult.getOrDefault(message, 0) + 1);
+    }
+
+    @Override
+    public String toString () {
+        return "User{" +
+                "payment=" + payment +
+                ", lottos=" + lottos +
+                ", totalResult=" + totalResult +
+                '}';
     }
 }

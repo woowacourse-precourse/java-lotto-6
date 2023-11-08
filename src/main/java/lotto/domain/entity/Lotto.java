64a8c002fce9b418aaf7e8c0e08f3b74f33e9920
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 16:50:04 +0900

    feat: Lotto 생성 시 값 검증 로직 추가

--- src/main/java/lotto/domain/entity/Lotto.java
+++ src/main/java/lotto/domain/entity/Lotto.java
@@ -1,20 +1,55 @@
 package lotto.domain.entity;
 
+import lotto.exception.ExceptionCode;
+
 import java.util.List;
 
 public class Lotto {
+    public static final int START = 1;
+    public static final int END = 45;
+    public static final int COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplication(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != LottoGenerator.COUNT) {
-            throw new IllegalArgumentException();
+    private void validateSize(final List<Integer> numbers) {
+        if (numbers.size() != Lotto.COUNT) {
+            throw new IllegalArgumentException(
+                    ExceptionCode.LOTTO_INVALID_SIZE.getDescription()
+            );
         }
     }
+
+    private void validateRange(final List<Integer> numbers) {
+        if (numbers.stream()
+                .anyMatch(n -> START > n || END < n)
+        ) {
+            throw new IllegalArgumentException(
+                    ExceptionCode.LOTTO_INVALID_RANGE.getDescription()
+            );
+        }
+    }
+
+    private void validateDuplication(final List<Integer> numbers) {
+        if (numbers.size() != getCountWithNoDuplication(numbers)) {
+            throw new IllegalArgumentException(
+                    ExceptionCode.LOTTO_DUPLICATION.getDescription()
+            );
+        }
+    }
+
+    private long getCountWithNoDuplication(final List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count();
+    }
+
 
     public int calculateCountOfNumberEqualToWinningNumber(final Lotto other) {
         return (int) this.numbers

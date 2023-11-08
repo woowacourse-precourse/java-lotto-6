43b6e499441474e802af187e6eafbb359a6890db
SWtheWhite <swthewhite@gmail.com>
2023-11-08 20:39:32 +0900

    feat: 로또 구매 - 구매 금액 입력 및 계산

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
@@ -7,8 +9,31 @@ public class Application {
 
 
     public static void main(String[] args) {
+        int purchaseAmount = inputPurchaseAmount();
+        int lottoCount = purchaseAmount / LOTTO_ONE_PRICE;
+    }
 
+    private static int inputPurchaseAmount() {
+        int purchaseAmount = 0;
+        boolean isValidInput = false;
 
+        while (!isValidInput) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                purchaseAmount = Integer.parseInt(Console.readLine());
+                isValidInput = checkPurchaseAmount(purchaseAmount);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            }
+        }
+        return purchaseAmount;
+    }
 
+    private static boolean checkPurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount <= 0 || purchaseAmount % LOTTO_ONE_PRICE != 0) {
+            System.out.println("[ERROR] 구입금액은 " + LOTTO_ONE_PRICE + "원 단위의 양수여야 합니다.");
+            return false;
+        }
+        return true;
     }
 }

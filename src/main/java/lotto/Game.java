a8840bef8e7859fa388aa85d6e17f69b3c6a1b34
ebang <dskhe8@gmail.com>
2023-11-08 21:25:39 +0900

    feat(lotto): Game이 보너스 번호를 입력받아 저장하는 기능 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -7,6 +7,7 @@ import lotto.Validator.InputValidator;
 public class Game {
 
     private Lotto winningLotto;
+    private int bonusNumber;
 
     public void askWinningNumbers() {
         while (true) {
@@ -24,11 +25,36 @@ public class Game {
         }
     }
 
+    public void askBonusNumber(){
+        while(true){
+
+            try{
+                String input = InputService.askBonusNumber();
+                InputValidator.validateBlankString(input);
+                int bonusNumber = Integer.parseInt(input);
+                this.setBonusNumber(bonusNumber);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
     public void setWinningLotto(List<Integer> numbers) {
         this.winningLotto = new Lotto(numbers);
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNmber(){
+        return this.bonusNumber;
     }
 
     public Lotto getWinningLotto() {
         return this.winningLotto;
     }
+
+
 }

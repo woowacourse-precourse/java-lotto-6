a9b6f0036c298447d1a5621412d99dc4e61995cf
ratatuwee <pkqwaszx@gmail.com>
2023-11-08 17:01:49 +0900

    feat: UserlottoNums 로또 리스트 인덱스 확인
    로또 번호들을 보관하는 리스트를 접근하는 인덱스가 범위를 벗어날 경우 에러 추가 구현

--- src/main/java/lotto/UserlottoNums.java
+++ src/main/java/lotto/UserlottoNums.java
@@ -15,6 +15,9 @@ public class UserlottoNums {
     }
 
     public List<Integer> getlottoNum(int index){
+        if(index<0||index>userLottos.size()){
+            throw new IllegalArgumentException("[ERROR] 생성한 로또 개수의 범위에 벗어 납니다.");
+        }
         return userLottos.get(index).getNumbers();
     }
 }

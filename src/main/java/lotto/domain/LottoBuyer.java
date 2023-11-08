330ba6a8d8ee92bbeecf86b37e54b4acfaf96c16
KB Kim <28584160+jimi567@users.noreply.github.com>
2023-11-08 15:12:44 +0900

    refactor(LottoBuyer) : 매직넘버 제거
    100 -> PERCENTAGE_MULTIPLIER
    10 -> ROUNDING_FACTOR

--- src/main/java/lotto/domain/LottoBuyer.java
+++ src/main/java/lotto/domain/LottoBuyer.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoBuyer {
+
+    private static final double PERCENTAGE_MULTIPLIER = 100;
+    private static final int ROUNDING_FACTOR = 10;
     private final Payment payment;
     private final LottoTickets lottoTickets;
 
@@ -24,8 +27,8 @@ public class LottoBuyer {
     }
 
     public double getRateOfReturn(List<LottoRank> lottoRanks) {
-        double originValue = getTotalRevenue(lottoRanks) / (double) payment.get() * (double) 100;
-        return Math.round(originValue * 10) / (double) 10;
+        double originValue = getTotalRevenue(lottoRanks) / (double) payment.get() * PERCENTAGE_MULTIPLIER;
+        return Math.round(originValue * ROUNDING_FACTOR) / (double) ROUNDING_FACTOR;
     }
 
     private double getTotalRevenue(List<LottoRank> lottoRanks) {

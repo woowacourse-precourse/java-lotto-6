8d1357cf44e197f341b472d0b284d4b011bf4693
Kim mingyu <minq0302@naver.com>
2023-11-08 16:56:17 +0900

    refactor(domain): add bonus number class
    Manage bonus number as a class.

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class BonusNumber {
+    public int bonusNumber;
+
+    public BonusNumber() {
+    }
+
+    public BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

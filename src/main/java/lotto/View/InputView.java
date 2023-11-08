e3600465aa2777feaf8613f1edff904a4b483215
yookyung Choi <ykc131719@gmail.com>
2023-11-08 21:01:18 +0900

    feat: 예외문구 처리하기

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,31 +1,69 @@
 package lotto.View;
 
+import static lotto.util.Verify.verifyBonusNumber;
 import static lotto.util.Verify.verifyWinningNumbersStringToList;
 import static lotto.util.Verify.verifyAmount;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Model.Lotto;
 
 public class InputView {
     public static final Integer PURCHASE_UNIT = 1000;
+    private static final boolean TRUE = true;
+    private static final boolean FALSE = false;
 
     public InputView(){}
 
-    public static Integer getHowMuchLottoWillYouBuy() throws IllegalArgumentException{
-        String input = Console.readLine();
-        Integer value = verifyAmount(input);
+    public static Integer getHowMuchLottoWillYouBuy(){
+        boolean condition = TRUE;
+        Integer value = 0;
+        do{
+            try{
+                String input = Console.readLine();
+                value = verifyAmount(input);
+                condition = FALSE;
+            }
+            catch (IllegalArgumentException e){
+                condition = TRUE;
+            }
+        } while(condition);
+
         return value / PURCHASE_UNIT;
     }
 
     public static Lotto getWinningLottoNumbers(){
-        String input = Console.readLine();
-        List<Integer> numbers = verifyWinningNumbersStringToList(input);
+        boolean condition = TRUE;
+        List<Integer> numbers = new ArrayList<>();
+        do{
+            try{
+                String input = Console.readLine();
+                numbers = verifyWinningNumbersStringToList(input);
+                condition = FALSE;
+            }
+            catch (IllegalArgumentException e){
+                condition = TRUE;
+            }
+        } while(condition);
+
         return new Lotto(numbers);
     }
 
     public static Integer getBonusNumber(){
-        String input = Console.readLine();
-        return Integer.parseInt(String.valueOf(input));
+        boolean condition = TRUE;
+        Integer number = 0;
+        do{
+            try{
+                String input = Console.readLine();
+                number = verifyBonusNumber(input);
+                condition = FALSE;
+            }
+            catch (IllegalArgumentException e){
+                condition = TRUE;
+            }
+        } while(condition);
+
+        return number;
     }
 }
--- src/main/java/lotto/util/Verify.java
+++ src/main/java/lotto/util/Verify.java
@@ -7,9 +7,18 @@ public class Verify {
 
     public Verify(){}
     public static Integer verifyAmount(String input) throws IllegalArgumentException{
-        Integer value = Integer.parseInt(String.valueOf(input));
-        if(value < 1000)
+        Integer value;
+        try{
+            value = Integer.parseInt(String.valueOf(input));
+        }
+        catch (NumberFormatException e){
+            System.out.println("[ERROR] 숫자를 입력해야합니다.");
             throw new IllegalArgumentException();
+        }
+        if(value % 1000 != 0){ // 1000원으로 나누어 떨어지지 않는 경우
+            System.out.println("[ERROR] 1000원 단위로 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
         return value;
     }
 
@@ -23,10 +32,13 @@ public class Verify {
                 number = Integer.parseInt(String.valueOf(numbers[i]));
             }
             catch (NumberFormatException e){
+                System.out.println("[ERROR] 숫자를 입력해야합니다.");
                 throw new IllegalArgumentException();
             }
-            if(number<0 || number>46)
+            if(number<0 || number>46){
+                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
                 throw new IllegalArgumentException();
+            }
             inputList.add(number);
         }
         return inputList;
@@ -39,10 +51,13 @@ public class Verify {
             number = Integer.parseInt(String.valueOf(input));
         }
         catch (NumberFormatException e){
+            System.out.println("[ERROR] 숫자를 입력해야합니다.");
             throw new IllegalArgumentException();
         }
-        if(number<0 || number>46)
+        if(number<0 || number>46){
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             throw new IllegalArgumentException();
+        }
         return number;
     }
 }

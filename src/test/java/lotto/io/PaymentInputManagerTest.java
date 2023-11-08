5f4d69d04cedffedf8a1f3afaf22c2ff7e5a52a2
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:18:57 +0900

    [FIX] 테스트 케이스 수정

--- src/test/java/lotto/io/PaymentInputManagerTest.java
+++ src/test/java/lotto/io/PaymentInputManagerTest.java
@@ -1,14 +1,9 @@
 package lotto.io;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentInputManagerTest {
     private PaymentInputManager paymentInputManager;
@@ -36,30 +31,6 @@ class PaymentInputManagerTest {
     void testValidInput() {
         consoleAdapter.setInput("3000");
         Integer result = paymentInputManager.input();
-        assertThat(result).isEqualTo(3000);
-    }
-
-    @Test
-    void testInputNotMultipleOfThousand() {
-        consoleAdapter.setInput("2500");
-        assertThatThrownBy(() -> paymentInputManager.input())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("1000원 단위로 입력해주세요.");
-    }
-
-    @Test
-    void testNonNumericInput() {
-        consoleAdapter.setInput("abc");
-        assertThatThrownBy(() -> paymentInputManager.input())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자를 입력해주세요.");
-    }
-
-    @Test
-    void testNoInput() {
-        consoleAdapter.setInput("");
-        assertThatThrownBy(() -> paymentInputManager.input())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자를 입력해주세요.");
+        assertThat(result).isEqualTo(3);
     }
 }

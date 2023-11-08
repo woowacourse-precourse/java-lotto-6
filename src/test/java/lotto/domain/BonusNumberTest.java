00dcb2217d9e0ece272c163334ed27fe1661bc5c
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-08 21:01:14 +0900

    refactor(BonusNumberTest): @ValueSource를 활용하여 다양한 번호 테스트하도록 수정

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,15 +1,26 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusNumberTest {
     @DisplayName("로또 번호가 1미만 45초과인 경우 예외가 발생한다.")
-    @Test
-    void createBonusNumberByOverRange() {
-        assertThatThrownBy(() -> new BonusNumber(46))
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, -10})
+    void createBonusNumberByOverRange(int input) {
+        assertThatThrownBy(() -> new BonusNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1이상 45이하인 경우 예외가 발생하지 않는다..")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45, 20})
+    void createBonusNumberByRightRange(int input) {
+        BonusNumber bonusNumber = new BonusNumber(input);
+        assertEquals(bonusNumber.number(), input);
     }
 }

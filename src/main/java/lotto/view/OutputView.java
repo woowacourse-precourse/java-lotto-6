b470a0d2ccc95a890b79ea89499f27ccba1aae0c
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 22:37:50 +0900

    Feat[view] 구매한 로또 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
+    private static final int MATCH_NONE = 0;
+
+    public static void printPurchaseComplete(int count) {
+        System.out.printf("\n%d개를 구매했습니다.\n", count);
+    }
+
+    public static void printLottoNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
+    }
 }

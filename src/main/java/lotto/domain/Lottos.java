e785fe0ab0793276140926c778fb02a51b26b4c6
dabin <db021129@naver.com>
2023-11-08 15:56:45 +0900

    feat : change generic Integer to Lotto

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,13 +3,13 @@ package lotto.domain;
 import java.util.List;
 
 public class Lottos {
-    private final List<Integer> lottos;
+    private final List<Lotto> lottos;
 
-    public Lottos(List<Integer> lottos) {
+    public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
-    public List<Integer> getLottos() {
+    public List<Lotto> getLottos() {
         return lottos;
     }
 }

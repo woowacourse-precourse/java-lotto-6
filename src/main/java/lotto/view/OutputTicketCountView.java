804d3e6f32a4d93d1fc6e2a5c5c6b9ed42d3a9e9
LooanCheong <jjyejun@naver.com>
2023-11-08 16:45:45 +0900

    <style>구매 갯수 출력부 오타 수정

--- src/main/java/lotto/view/OutputTicketCountView.java
+++ src/main/java/lotto/view/OutputTicketCountView.java
@@ -4,6 +4,6 @@ import lotto.Ticket;
 
 public class OutputTicketCountView {
     public static void printCountOfLotto(Ticket ticket) {
-        System.out.printf(("%d개를 그매했습니다.") + "%n", ticket.countTicket());
+        System.out.printf(("%d개를 구매했습니다.") + "%n", ticket.countTicket());
     }
 }

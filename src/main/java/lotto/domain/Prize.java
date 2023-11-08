2df778c41b52d37b25249fd891494b645d506e32
Kim myeong-hwi <sjkl6206s@gmail.com>
2023-11-08 10:30:39 +0900

    feat(Prize): 조건에 따라 당첨 등수를 반환하는 기능 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,26 +1,27 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum Prize {
     NOTHING(0
             , 0
-            , (matchLottoNumber, matchBonusNumber) -> matchLottoNumber < 3),
+            , (matchLottoNumbers, matchBonusNumber) -> matchLottoNumbers < 3),
     FIFTH(5_000
             , 3
-            , (matchLottoNumber, matchBonusNumber) -> matchLottoNumber == 3),
+            , (matchLottoNumbers, matchBonusNumber) -> matchLottoNumbers == 3),
     FOURTH(50_000
             , 4
-            , (matchLottoNumber, matchBonusNumber) -> matchLottoNumber == 4),
+            , (matchLottoNumbers, matchBonusNumber) -> matchLottoNumbers == 4),
     THIRD(1_500_000
             , 5
-            , (matchLottoNumber, matchBonusNumber) -> matchLottoNumber == 5 && !matchBonusNumber),
+            , (matchLottoNumbers, matchBonusNumber) -> matchLottoNumbers == 5 && !matchBonusNumber),
     SECOND(30_000_000
             , 5
-            , (matchLottoNumber, matchBonusNumber) -> matchLottoNumber == 5 && matchBonusNumber),
+            , (matchLottoNumbers, matchBonusNumber) -> matchLottoNumbers == 5 && matchBonusNumber),
     FIRST(2_000_000_000
             , 6
-            , (matchLottoNumber, matchBonusNumber) -> matchLottoNumber == 6);
+            , (matchLottoNumbers, matchBonusNumber) -> matchLottoNumbers == 6);
 
     private final int prizeAmount;
     private final int matchLottoNumber;
@@ -30,6 +31,13 @@ public enum Prize {
         this.prizeAmount = prizeAmount;
         this.matchLottoNumber = matchLottoNumber;
         this.isMatch = isMatch;
+    }
+
+    public static Prize getPrize(final int matchLottoNumbers, final boolean matchBonusNumber) {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.isMatch.test(matchLottoNumbers, matchBonusNumber))
+                .findAny()
+                .orElse(NOTHING);
     }
 
     public int getPrizeAmount() {

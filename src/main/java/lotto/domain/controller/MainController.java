7b2bcc2627f85f7405930eaa4066a7d5fa0605c2
sunwoong <sunwoong1122@gmail.com>
2023-11-08 22:12:37 +0900

    refactor: 메서드 분리

--- src/main/java/lotto/domain/controller/MainController.java
+++ src/main/java/lotto/domain/controller/MainController.java
@@ -9,6 +9,7 @@ import lotto.domain.view.InputView;
 import lotto.domain.view.OutputView;
 import lotto.global.util.Converter;
 import lotto.global.util.RandomNumberGenerator;
+import lotto.global.util.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,31 +27,44 @@ public class MainController {
 
     public void startLotto() {
         // 구입금액 입력
-        outputView.printPriceLine();
-        String inputPrice = inputView.readInputData();
+        String inputPrice = readInputPrice();
         // 구입금액 검증
-        Integer price = Converter.convertToNumber(inputPrice);
-        List<Lotto> lottos = new ArrayList<>();
-        // 로또 발매
-        int count = price / 1000;
-        for (int i = 0; i < count; i++) {
-            List<Integer> generatedNumbers = RandomNumberGenerator.generateRandomNumber();
-            Lotto generatedLotto = new Lotto(generatedNumbers);
-            lottos.add(generatedLotto);
+        try {
+            Validator.validatePrice(inputPrice);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readInputPrice();
         }
-        outputView.printLottoLine(lottos);
+        int price = Converter.convertToNumber(inputPrice);
+
+        // 로또 발매
+        List<Lotto> lottos = new ArrayList<>();
+        generatedLottos(price, lottos);
+
         // 당첨 번호 입력
-        outputView.printNumbersLine();
-        String inputNumbers = inputView.readInputData();
+        String inputNumbers = readInputNumbers();
         // 당첨 번호 검증
+        try {
+            Validator.validateNumbers(inputNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readInputNumbers();
+        }
         List<Integer> numbers = Converter.covertToNumbers(inputNumbers);
         Lotto lotto = new Lotto(numbers);
+
         // 보너스 번호 입력
-        outputView.printBonusNumberLine();
-        String inputBonusNumber = inputView.readInputData();
+        String inputBonusNumber = readInputBonusNumber();
         // 보너스 번호 검증
+        try {
+            Validator.validateBonusNumber(inputBonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            readInputBonusNumber();
+        }
         Integer bonusNumber = Converter.convertToNumber(inputBonusNumber);
         Bonus bonus = new Bonus(bonusNumber);
+
         List<Result> results = new ArrayList<>();
         // 로또 비즈니스
         for (Lotto generatedLotto : lottos) {
@@ -60,5 +74,30 @@ public class MainController {
         Receipt receipt = lottoService.calculateLottoRate(price, results);
         // 결과
         outputView.printResultLine(receipt);
+    }
+
+    private String readInputPrice() {
+        outputView.printPriceLine();
+        return inputView.readInputData();
+    }
+
+    private void generatedLottos(int price, List<Lotto> lottos) {
+        int count = price / 1000;
+        for (int i = 0; i < count; i++) {
+            List<Integer> generatedNumbers = RandomNumberGenerator.generateRandomNumber();
+            Lotto generatedLotto = new Lotto(generatedNumbers);
+            lottos.add(generatedLotto);
+        }
+        outputView.printLottoLine(lottos);
+    }
+
+    private String readInputNumbers() {
+        outputView.printNumbersLine();
+        return inputView.readInputData();
+    }
+
+    private String readInputBonusNumber() {
+        outputView.printBonusNumberLine();
+        return inputView.readInputData();
     }
 }

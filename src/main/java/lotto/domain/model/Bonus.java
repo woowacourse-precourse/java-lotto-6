6e969e420ed720eb6334838611fff675e5dcdb5f
sunwoong <sunwoong1122@gmail.com>
2023-11-08 19:49:00 +0900

    feat: bonus 모델 클래스 구현

---
+++ src/main/java/lotto/domain/model/Bonus.java
@@ -0,0 +1,13 @@
+package lotto.domain.model;
+
+public class Bonus {
+    private final Integer number;
+
+    public Bonus(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+}

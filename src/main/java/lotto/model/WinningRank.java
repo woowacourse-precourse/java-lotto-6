56b157585e802c7f23151cbea9b0bb631c56fae0
Terry <t3rryahn.dev@gmail.com>
2023-11-08 15:45:29 +0900

    feat: 결과 계산 기능 총 당첨 금액을 저장하는 기능 당첨 등수의 갯수를 저장하는 기능
    LottoResult 클래스에 추가
    totalReward 변수 타입 int > long으로 변경
    
    WinningRank에서 prize 변수 타입 int > long으로 변경
    
    이유:상금의 int 범위 초과
    
    LottoResultTest 클래스에 테스트 추가
    테스트 내용
    총 결과 메서드 확인.
    > 테스트 통과

--- src/main/java/lotto/model/WinningRank.java
+++ src/main/java/lotto/model/WinningRank.java
@@ -1,28 +1,32 @@
 package lotto.model;
 
 public enum WinningRank {
-    FIRST(6, false),   // 1등: 6개 일치
-    SECOND(5, true),   // 2등: 5개 + 보너스 번호 일치
-    THIRD(5, false),   // 3등: 5개 일치
-    FOURTH(4, false),  // 4등: 4개 일치
-    FIFTH(3, false),   // 5등: 3개 일치
+    FIRST(6, false, "1등", 2000000000),   // 1등: 6개 일치
+    SECOND(5, true, "2등", 30000000),   // 2등: 5개 + 보너스 번호 일치
+    THIRD(5, false, "3등", 1500000),   // 3등: 5개 일치
+    FOURTH(4, false, "4등", 50000),  // 4등: 4개 일치
+    FIFTH(3, false, "5등", 5000),   // 5등: 3개 일치
 
-    NONE(0, false);    // 꽝: 일치하는 번호 없음
+    NONE(0, false, "꼴등", 0);    // 꽝: 일치하는 번호 없음
 
     private final int matchCount;
     private final boolean hasBonus;
+    private final String describe;
+    private final long prize;
 
-    WinningRank(int matchCount, boolean hasBonus) {
+    WinningRank(int matchCount, boolean hasBonus, String describe, int prize) {
         this.matchCount = matchCount;
         this.hasBonus = hasBonus;
+        this.describe = describe;
+        this.prize = prize;
     }
 
-    public int getMatchCount() {
-        return matchCount;
+    public String getDescribe() {
+        return describe;
     }
 
-    public boolean hasBonus() {
-        return hasBonus;
+    public long getPrize() {
+        return prize;
     }
 
     public static WinningRank valueOf(int matchCount, boolean hasBonus) {

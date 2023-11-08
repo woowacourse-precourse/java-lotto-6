d8e404896f2aa0ae98ebebb3e4d545c178463902
권태현 <kygkth2011@gmail.com>
2023-11-08 22:46:49 +0900

    feat(RandomLottoGerenator) : List<Lotto>로 로또를 필드로 가지지 않고 생성하도록 변경

--- src/main/java/Model/RandomLottoGenerator.java
+++ src/main/java/Model/RandomLottoGenerator.java
@@ -5,27 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 
-public class User {
+public class RandomLottoGenerator {
     static final int SMALLEST_LOTTO_NUMBER = 1;
     static final int BIGGEST_LOTTO_NUMBER = 45;
     static final int LOTTO_NUMBER_COUNT = 6;
 
-    private List<Lotto> lottos;
-
-    public User() {
-        lottos = new ArrayList<>();
-    }
-
-    public void generateLottos(int amount) {
+    public List<Lotto> generateLottos(int amount) {
+        List<Lotto> userIssuedLottos = new ArrayList<>();
         for (int i = 0; i < amount; i++) {
-            generateSingleLotto();
+            userIssuedLottos.add(generateSingleRandomLotto());
         }
+        return userIssuedLottos;
     }
 
-    private void generateSingleLotto() {
+    private Lotto generateSingleRandomLotto() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(SMALLEST_LOTTO_NUMBER, BIGGEST_LOTTO_NUMBER,
                 LOTTO_NUMBER_COUNT);
-        lottos.add(new Lotto(lottoNumbers));
+        return new Lotto(lottoNumbers);
     }
 
 

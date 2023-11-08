378c35492f099497c464ae52be6d41f856c55167
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-08 15:32:24 +0900

    refactor(LottoProvider): lottoHistory매핑 초기화 추가

--- src/main/java/lotto/model/LottoProvider.java
+++ src/main/java/lotto/model/LottoProvider.java
@@ -1,9 +1,14 @@
 package lotto.model;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoProvider {
+
+    private static final int PLUS_COUNT = 1;
+    private static final String NO = "NO";
 
     public List<Lotto> lottoGenerate(int each) {
         return LottoNumberCreator.myLottoNumbersOf(each);
@@ -18,5 +23,32 @@ public class LottoProvider {
                 .filter(LottoRank::isWin)
                 .mapToInt(LottoRank::myWinnings)
                 .sum();
+    }
+
+    public Map<String, Integer> lottoHistory(List<LottoRank> lottoRanks) {
+        Map<String, Integer> lottoHistory = mapInit();
+
+        lottoRanks.stream()
+                .filter(LottoRank::isWin)
+                .map(LottoRank::name)
+                .forEach(
+                        (rankName) -> lottoHistory.put(
+                                rankName,
+                                lottoHistory.get(rankName) + PLUS_COUNT
+                        )
+                );
+
+        return lottoHistory;
+    }
+
+    private Map<String, Integer> mapInit() {
+
+        return Arrays.stream(LottoRank.values())
+                .filter(
+                        (rankName) -> !rankName.name()
+                                .equals(NO))
+                .collect(Collectors.toMap(
+                        LottoRank::name, rank -> 0)
+                );
     }
 }

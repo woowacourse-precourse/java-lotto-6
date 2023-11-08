f4e21c9c1be6696a4022f21e2209700314c6a001
SangCheol <scpark0698@gmail.com>
2023-11-08 21:00:03 +0900

    feat: LottoResult 클래스 생성 및 전체적인 구조 설정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int buyAmount = inputBuyAmount();
+        List<Lotto> lottos = generateRandomLottoTickets(buyAmount);
+
+        printLottoTickets(lottos);
+
+        LottoResult result = playLottoGame(lottos);
+        printGameResults(result);
+    }
+
+    private static void printGameResults(LottoResult result) {
+
+    }
+
+    private static LottoResult playLottoGame(List<Lotto> lottos) {
+        return null;
+    }
+
+    private static void printLottoTickets(List<Lotto> lottos) {
+
+    }
+
+    private static List<Lotto> generateRandomLottoTickets(int buyAmount) {
+        return null;
+    }
+
+    private static int inputBuyAmount() {
+        return 0;
     }
 }

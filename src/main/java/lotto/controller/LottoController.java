008792b0e53664d8e6b1a818dd9b82dd47f7a3be
JayMon0327 <kioplm0211@naver.com>
2023-11-08 18:31:19 +0900

    로또 번호 랜덤 난수 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,20 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+
+    private List<Lotto> generateLottos(int numberOfLottos) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < numberOfLottos; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+}

ed8c44a3074303a901cd74d283cd66c728ab3012
HyunmoBae <tsi0520@naver.com>
2023-11-08 15:20:48 +0900

    feat(lotto) : add LottoResult display methods

--- src/main/java/lotto/ui/LottoResultDisplay.java
+++ src/main/java/lotto/ui/LottoResultDisplay.java
@@ -1,5 +1,27 @@
 package lotto.ui;
 
+import java.util.Comparator;
+import java.util.List;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTicketGenerator;
+
 public class LottoResultDisplay {
+    LottoTicketGenerator lottoTicketGenerator = new LottoTicketGenerator();
+
+    public LottoResultDisplay() {
+    }
+
+    public void showWinningDetails(LottoResult lottoResult) {
+        System.out.println("3개 일치 (5,000원) - " + lottoResult.fifth + "개");
+        System.out.println("4개 일치 (50,000원) - " + lottoResult.fourth + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.third + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.second + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.first + "개");
+    }
+
+    public void showProfitMargin(double profitMargin) {
+        System.out.print("총 수익률은 " + profitMargin + "%입니다.");
+    }
 
 }

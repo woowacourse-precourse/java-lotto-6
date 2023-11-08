fa5737e2244a37119d741446f915f51fe4515807
saera <yook.saera@gmail.com>
2023-11-08 19:01:29 +0900

    feat(lotto): 사용자가 입력한 구입 금액이 0~9로 이루어진 문자열이 아닌 경우 예외를 던져라

--- src/main/java/lotto/domain/Cashier.java
+++ src/main/java/lotto/domain/Cashier.java
@@ -4,8 +4,18 @@ public class Cashier {
     private final long purchaseAmount;
 
     public Cashier(String purchaseAmount) {
+        checkIsDigit(purchaseAmount);
         long number = Integer.parseInt(purchaseAmount);
         this.purchaseAmount = number;
+    }
+
+    private void checkIsDigit(String input) {
+        char[] characterizedInput = input.toCharArray();
+        for (char number : characterizedInput) {
+            if (number < '0' || number > '9') {
+                throw new NotDigitException();
+            }
+        }
     }
 
     public long getPurchaseAmount() {
---
+++ src/test/java/lotto/domain/CashierTest.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CashierTest {
+    @DisplayName("구입 금액을 0부터 9까지 숫자가 아닌 문자를 포함하여 입력하면 예외가 발생한다.")
+    @Test
+    void createCashierByNotDigitCharacter() {
+        assertThatThrownBy(() -> new Cashier("1000원"))
+                .isInstanceOf(IllegalArgumentException.class).isExactlyInstanceOf(NotDigitException.class);
+    }
+}

ea51c245c31f9d58d9f52012a85adddc6f9d1fa9
june-777 <wlwhswnsrl96@gmail.com>
2023-11-08 11:02:54 +0900

    feat(OutputView): 수익률을 금액 쉼표(,) 폼으로 변환하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.EnumMap;
 import java.util.List;
 import lotto.domain.Rank;
@@ -10,10 +12,11 @@ import lotto.dto.WinningStatisticsDto;
 public class OutputView {
     private static final String PURCHASED_MESSAGE_FORM = "%d개를 구매했습니다.%n";
     private static final String PURCHASE_LOTTO_FORM_DELIMITER = " \n";
-    
+
     private static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---";
     private static final String MATCH_COUNT_FORM = "%s - %d개%n";
     private static final String TOTAL_STATISTICS_FORM = "총 수익률은 %s%%입니다.";
+    private static final String MONEY_COMMA_FORMAT = "###,##0.0";
 
     public void printPurchaseLotto(PurchaseLottoDto purchaseLottoDto) {
         System.out.printf(PURCHASED_MESSAGE_FORM, purchaseLottoDto.size());
@@ -39,6 +42,11 @@ public class OutputView {
             }
             System.out.printf(MATCH_COUNT_FORM, rank.getPrizeMessage(), eachRankCounts.get(rank));
         }
-        System.out.printf(TOTAL_STATISTICS_FORM, winningStatisticsDto.getYieldRate().toString());
+        System.out.printf(TOTAL_STATISTICS_FORM, convertMoneyCommaForm(winningStatisticsDto.getYieldRate()));
+    }
+
+    private String convertMoneyCommaForm(BigDecimal yield) {
+        DecimalFormat decimalFormat = new DecimalFormat(MONEY_COMMA_FORMAT);
+        return decimalFormat.format(yield.doubleValue());
     }
 }

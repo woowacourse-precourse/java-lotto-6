12fd46eda19448456c9720fa7bf4ddd3074c589b
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 18:56:11 +0900

    fix : Rank의 보너스 볼 확인 불린 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,7 +3,7 @@ package lotto.domain;
 public enum Rank {
     FIRST(6, 2_000_000_000),
     SECOND(5, 30_000_000, true), // 보너스 번호 일치를 구분하기 위한 추가 필드
-    THIRD(5, 1_500_000),
+    THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     NONE(0, 0);

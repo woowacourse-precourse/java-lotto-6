f0b4940784241107ad18985bc8ada44c1c58740f
namtndus <rnrmf0721@gmail.com>
2023-11-08 14:51:21 +0900

    feat(BonusNumber): 기능 구현

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.regex.Pattern;
+
+public class BonusNumber {
+
+    private final int bonusNumber;
+
+    public BonusNumber() {
+        bonusNumber = 0;
+    }
+
+    public BonusNumber(String num) {
+        vaildateNumber(num);
+        vaildateRange(num);
+        bonusNumber = Integer.parseInt(num);
+    }
+
+    private void vaildateNumber(String num) {
+        if (!Pattern.matches("^[1-9|0-9]+$", num)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void vaildateRange(String num) {
+        int number = Integer.parseInt(num);
+        if (number <= 0 || 45 < number) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

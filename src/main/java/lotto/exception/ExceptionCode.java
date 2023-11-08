73be52031f3b468cec19ecc249c69f10f366b475
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 17:08:56 +0900

    add: 예외 메세지 상수 추가

--- src/main/java/lotto/exception/ExceptionCode.java
+++ src/main/java/lotto/exception/ExceptionCode.java
@@ -1,31 +1,20 @@
 package lotto.exception;
 
 public enum ExceptionCode {
-    NUMBER_FORMAT("VIEW-001", "[ERROR] 숫자 형식으로 입력해주세요."),
+    NUMBER_FORMAT("[ERROR] 숫자 형식으로 입력해주세요."),
+    LOTTO_INVALID_SIZE("[ERROR] 로또 번호가 6자리가 되도록 입력해주세요."),
+    LOTTO_INVALID_RANGE("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요."),
+    LOTTO_DUPLICATION("[ERROR] 로또 번호가 중복되지 않도록 입력해주세요."),
+    AMOUNT_PURCHASE_PRICE("[ERROR] 구매 금액을 1,000원 단위로 입력해주세요."),
+    UNSIGNED_PURCHASE_PRICE("[ERROR] 구매 금액을 양수로 입력해주세요.");
 
-    ;
-
-    private final String code;
     private final String description;
 
-    ExceptionCode(final String code, final String description) {
-        this.code = code;
+    ExceptionCode(final String description) {
         this.description = description;
     }
-
-    public String getCode() {
-        return code;
-    }
-
+    
     public String getDescription() {
         return description;
-    }
-
-    @Override
-    public String toString() {
-        return "ExceptionCode{" +
-                "code='" + code + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 }

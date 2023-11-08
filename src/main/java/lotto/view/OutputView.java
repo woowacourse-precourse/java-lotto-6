64fd882abe46a8654d9f055e9861de9cb5ceb178
kimdongyeon <do121712@naver.com>
2023-11-08 22:16:27 +0900

    refactor : pritCount, printReult 메소드가 객체를 직접 받지 않고 Map, int 형식으로 받게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,7 @@ import lotto.domain.Player;
 import lotto.domain.WinningLotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -12,24 +13,25 @@ public class OutputView {
         System.out.println(message);
     }
 
-    public void printCount(int count) {
+    public void printCount(int count, int change) {
         System.out.println("\n" + count + "개를 구매했습니다.");
+        System.out.println("거스름돈은 " + change + "원 입니다.");
     }
 
     public void printLottos(List<Integer> nums) {
         System.out.println(nums);
     }
 
-    public void printResult(WinningLotto winningLotto) {
+    public void printResult(Map<Statistic, Integer> winningLotto) {
         System.out.println("\n당첨 통계\n" + "---");
-        System.out.println("3개 일치 (5,000원) - " + winningLotto.getMatch().get(Statistic.MATCH_THREE) + "개");
-        System.out.println("4개 일치 (50,000원) - " + winningLotto.getMatch().get(Statistic.MATCH_FOUR) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + winningLotto.getMatch().get(Statistic.MATCH_FIVE) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningLotto.getMatch().get(Statistic.MATCH_FIVE_WITH_BONUS) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + winningLotto.getMatch().get(Statistic.MATCH_SIX) + "개");
+        System.out.println("3개 일치 (5,000원) - " + winningLotto.get(Statistic.MATCH_THREE) + "개");
+        System.out.println("4개 일치 (50,000원) - " + winningLotto.get(Statistic.MATCH_FOUR) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + winningLotto.get(Statistic.MATCH_FIVE) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningLotto.get(Statistic.MATCH_FIVE_WITH_BONUS) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + winningLotto.get(Statistic.MATCH_SIX) + "개");
     }
 
-    public void printTotal(Player player) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", player.getPercent());
+    public void printTotal(double percent) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", percent);
     }
 }

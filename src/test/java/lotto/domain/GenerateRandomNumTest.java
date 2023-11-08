5ebadef44d7e3076fa6074e53317775da3e28df9
sejin park <seijin0722@naver.com>
2023-11-08 22:27:16 +0900

    test(GenerateRandomNumTest) : 랜덤숫자 테스트

--- src/test/java/lotto/domain/GenerateRandomNumTest.java
+++ src/test/java/lotto/domain/GenerateRandomNumTest.java
@@ -3,27 +3,41 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static lotto.utils.Constants.MIN_NUMBER_IN_RANGE;
-import static lotto.utils.Constants.MAX_NUMBER_IN_RANGE;
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.utils.Constants.*;
 import static lotto.utils.ErrorMessages.CHECK_NUMBER_IN_RANGE;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 class GenerateRandomNumTest {
-//    @Test
-//    @DisplayName("pickNumber 메서드 테스트")
-//    public void pickNumber() {
-//        int num = GenerateRandomNum.pickNumber();
-//        assertThat(num).isBetween(MIN_NUMBER_IN_RANGE, MAX_NUMBER_IN_RANGE);
-//    }
+    @Test
+    @DisplayName("pickNumber 메서드 테스트")
+    public void pickNumber() {
+        List<Integer> numbers = GenerateRandomNum.pickNumber();
+        assertThat(numbers).hasSize(LOTTO_SIZE);
+        assertThat(numbers).allMatch(num -> num >= MIN_NUMBER_IN_RANGE && num <= MAX_NUMBER_IN_RANGE);
+    }
 
-//    @Test
-//    @DisplayName("[ERROR] checkNumberInRange 메서드 테스트 - 1과 45사이의 범위에 해당하지 않을 경우")
-//    public void checkNumberInRange() {
-//        int num = 0;
-//        assertThatExceptionOfType(IllegalArgumentException.class)
-//                .isThrownBy(() -> GenerateRandomNum.checkNumberInRange(num))
-//                .withMessageMatching(CHECK_NUMBER_IN_RANGE);
-//    }
+    @Test
+    @DisplayName("checkNumberInRange 메서드 테스트 - 1과 45사이의 범위의 숫자가 맞을 때")
+    public void checkNumberInRange_ValidNumbers_DoesNotThrowException() {
+        List<Integer> validNumbers = Arrays.asList(10, 20, 30, 40, 45, 1);
+
+        assertThatCode(() -> GenerateRandomNum.checkNumberInRange(validNumbers))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("checkNumberInRange 메서드 테스트 - 1과 45사이의 범위의 숫자가 아닐 때")
+    public void checkNumberInRange_InvalidNumbers_ThrowsException() {
+        List<Integer> invalidNumbers = Arrays.asList(-5, 0, 46, 50);
+
+        assertThatThrownBy(() -> GenerateRandomNum.checkNumberInRange(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CHECK_NUMBER_IN_RANGE);
+    }
 }
+

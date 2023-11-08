71516d726d02520e834035c03d116fa1040dd7cd
Soonrang <clearsbyul30@naver.com>
2023-11-08 23:46:31 +0900

    feat: 번호 쪼개기

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,23 +1,32 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.PirzeResult;
+import lotto.model.PrizeMoney;
+import lotto.validate.InvalidlputException;
+
+import java.util.List;
 
 public class Input {
     private final String INPUT_MONEY_MESSAGE = "구매금액을 입력해주세요.";
     private final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해주세요.";
     private final String INPUT_BONUS_NUMBER_MESSAGE = "당첨 번호를 입력해주세요.";
 
-    public String inputMoney(){
+    public int inputMoney(){
         System.out.println(INPUT_MONEY_MESSAGE);
         String inputMoney = Console.readLine();
-        return inputMoney;
+        int money = Integer.parseInt(inputMoney);
+        InvalidlputException.validateMoneyRange(money);
+        InvalidlputException.validateMoneyDivided(money);
+        return money;
     }
 
-    public String inputWinningNumbers() {
+    public List<Integer> inputWinningNumbers() {
         System.out.println();
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         String winningNumbers = Console.readLine();
-        return winningNumbers;
+        PirzeResult pirzeResult = new PirzeResult();
+        return pirzeResult.splitNumber(winningNumbers);
     }
 
     public String inputBounusNumber() {

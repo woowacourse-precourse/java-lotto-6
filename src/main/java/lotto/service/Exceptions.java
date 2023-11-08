989b99072c65959035703399997fa4f39adb9557
ehdwoKIM <kinbell19@gmail.com>
2023-11-08 20:00:42 +0900

    [ADD] 1000원 단위 금액만 입력하도록 예외처리

--- src/main/java/lotto/service/Exceptions.java
+++ src/main/java/lotto/service/Exceptions.java
@@ -1,4 +1,9 @@
 package lotto.service;
 
 public class Exceptions {
+    public void isInvalidPurchaseMoneyAmount(int money, int divided){
+        if(money % divided != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원단위로 가능합니다.");
+        }
+    }
 }

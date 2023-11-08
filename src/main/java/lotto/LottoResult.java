0a8ed74e5cb0da8dc664c2a235d2490d1646b82c
박상현 <shawnpark98@naver.com>
2023-11-08 17:51:16 +0900

    fix: LottoResult 미당첨일 경우 추가

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -5,7 +5,8 @@ public enum LottoResult {
     SECOND(30000000),
     THIRD(1500000),
     FOURTH(50000),
-    FIFTH(5000);
+    FIFTH(5000),
+    FAIL(0);
 
     private final Integer prize;
     LottoResult(Integer prize) {

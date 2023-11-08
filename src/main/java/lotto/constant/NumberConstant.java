046dd3bb2ee244c37e7ba8d059c3c2d9e33f9325
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 21:02:39 +0900

    feat: 반복적으로 쓰이는 상수들을 모아둔 클래스 구현

--- src/main/java/lotto/constant/NumberConstant.java
+++ src/main/java/lotto/constant/NumberConstant.java
@@ -1,2 +1,9 @@
-package lotto.constant;public class NumberConstant {
+package lotto.constant;
+
+public class NumberConstant {
+
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_PRICE_UNIT = 1000;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_MIN_NUMBER = 1;
 }

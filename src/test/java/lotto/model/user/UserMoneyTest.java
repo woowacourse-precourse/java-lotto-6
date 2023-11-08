53d9828982e1625e3c51a0e56bfabbb69443b143
siyeonSon <siyeon0209@naver.com>
2023-11-08 22:11:03 +0900

    style(UserMoneyTest): 공백 제거 및 추가

--- src/test/java/lotto/model/user/UserMoneyTest.java
+++ src/test/java/lotto/model/user/UserMoneyTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
 
 class UserMoneyTest {
-
     @Test
     void 로또_구입_금액이_1000원_단위로_나누어_떨어진다() {
         assertDoesNotThrow(() -> new UserMoney(12000));
     }
+
     @Test
     void 로또_구입_금액이_1000원_단위로_나누어_떨어지지_않을_경우_예외를_발생시킨다() {
         assertThatThrownBy(() -> new UserMoney(1200))

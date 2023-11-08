152788e1340002673fd891a2b6a846b88e3fb982
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-08 17:42:58 +0900

    test(ranking): 랭킹 결정 테스트 ParameterizedTest로 변경

--- src/test/java/lotto/domain/RankingTest.java
+++ src/test/java/lotto/domain/RankingTest.java
@@ -2,55 +2,24 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class RankingTest {
 
-    @Test
-    void 로또_1등이_정상적으로_반환되는가() {
-        int matchCount = 6;
-        boolean bonusMatch = false;
-
-        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.FIRST);
-    }
-
-    @Test
-    void 로또_2등이_정상적으로_반환되는가() {
-        int matchCount = 5;
-        boolean bonusMatch = true;
-
-        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.SECOND);
-    }
-
-    @Test
-    void 로또_3등이_정상적으로_반환되는가() {
-        int matchCount = 5;
-        boolean bonusMatch = false;
-
-        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.THIRD);
-    }
-
-    @Test
-    void 로또_4등이_정상적으로_반환되는가() {
-        int matchCount = 4;
-        boolean bonusMatch = false;
-
-        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.FOURTH);
-    }
-
-    @Test
-    void 로또_5등이_정상적으로_반환되는가() {
-        int matchCount = 3;
-        boolean bonusMatch = false;
-
-        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(Ranking.FIFTH);
+    @ParameterizedTest(name = "로또 매칭 개수와 보너스 숫자 매칭 여부에 따라 정상적으로 등수가 결정되는가.")
+    @CsvSource({"6, false, FIRST",
+            "5, true, SECOND",
+            "5, false, THIRD",
+            "4, false, FOURTH",
+            "3, false, FIFTH"})
+    void returnRankingByMatchingCountAndBonus(int matchCount, boolean bonusMatch, Ranking expected) {
+        assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "로또 등수가 없는 경우 null 반환")
     @CsvSource(value = {"2", "1", "0"})
-    void 로또_등수가_없는_경우_null(int matchCount) {
+    void NoRankingOf(int matchCount) {
         boolean bonusMatch = false;
 
         assertThat(Ranking.of(matchCount, bonusMatch)).isEqualTo(null);

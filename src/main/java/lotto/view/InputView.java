8559d4c7d7e169dea5a6e6887369d09fc3e24e62
djc06048 <gsafe1213@gmail.com>
2023-11-08 16:46:31 +0900

    fix(view): 문제 요구 조건 추가해서 InputView 수정 입력이 유효하지 않아 예외를 발생 시킨 경우 해당 입력이 유효할 때까지 계속 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,21 +4,62 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.model.BonusNumber;
 import lotto.model.LottoPrice;
 import lotto.model.WinningNumber;
-
+import lotto.util.exception.CustomException;
 
 
 public class InputView {
     public LottoPrice getLottoPrice() {
-        String input = Console.readLine();
+        String input;
+        do {
+            input = Console.readLine();
+        } while (!isValidLottoPrice(input));
         return new LottoPrice(input);
+
+    }
+
+    private boolean isValidLottoPrice(String input) {
+        try {
+            new LottoPrice(input);
+            return true;
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     public WinningNumber getWinningNumbers() {
-        String line = Console.readLine();
-        return new WinningNumber(line);
+        String input;
+        do{
+            input = Console.readLine();
+        } while (!isValidWinningNumbers(input));
+        return new WinningNumber(input);
+    }
+
+    private boolean isValidWinningNumbers(String input) {
+        try {
+            new WinningNumber(input);
+            return true;
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     public BonusNumber getBonusNumber() {
-        return new BonusNumber(Console.readLine());
+        String input;
+        do {
+            input = Console.readLine();
+        } while (!isValidBonusNumber(input));
+        return new BonusNumber(input);
+    }
+
+    private boolean isValidBonusNumber(String input) {
+        try {
+            new BonusNumber(input);
+            return true;
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }

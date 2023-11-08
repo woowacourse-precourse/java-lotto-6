a19dc5f112048d1a56762416e0ec204038920920
Bisi3asi <kueeng8008@naver.com>
2023-11-08 10:20:03 +0900

    refacor(service) : error enum, 예외 처리
    * global variable ERROR to enum
    * NumberFormatException도 IllegalArgumentException으로 예외 처리

--- src/main/java/lotto/GameService.java
+++ src/main/java/lotto/GameService.java
@@ -7,8 +7,19 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class GameService {
-    static final int ERROR = -1;
-    static final int OKAY = 100;
+    enum ERROR {
+        ERROR1(-1), ERROR2(null);
+
+        private final Object value;
+
+        ERROR(Integer value) {
+            this.value = value;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+    }
 
     GameRepository gameRepository = new GameRepository();
 
@@ -18,13 +29,12 @@ public class GameService {
             if (rawCash % 1000 == 0 && rawCash > 0) {
                 return rawCash / 1000;
             }
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 1000원 단위, 1000원 이상의 로또 구입 금액을 입력해주세요.");
         } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 로또 구입 금액은 정확한 숫자여야 합니다.");
-            return ERROR;
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 정확한 숫자여야 합니다.");
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 1000원 단위, 1000원 이상의 로또 구입 금액을 입력해주세요.");
-            return ERROR;
+            System.out.println(e.getMessage());
+            return (int) ERROR.ERROR1.getValue();
         }
     }
 
@@ -43,20 +53,18 @@ public class GameService {
     }
 
     // 번호 포맷팅에 대한 검증만 실행, 로또 번호 검증(중복 수, 수의 범위, 번호 개수 등) 로직 : Lotto 실행
-    public int validWinNumber(String rawInput) throws IllegalArgumentException {
+    public ArrayList<Integer> validWinNumber(String rawInput) throws IllegalArgumentException {
         try {
             ArrayList<Integer> rawWinNumber = Arrays.stream(rawInput.split(","))
                     .map(String::trim).map(Integer::parseInt)
                     .collect(Collectors.toCollection(ArrayList::new));
-
             gameRepository.saveWinLottoNumber(rawWinNumber);
-            return OKAY;
+            return rawWinNumber;
         } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 로또 당첨 번호를 정확하게 입력해주세요. (쉼표 기준, 여섯 개)");
-            return ERROR;
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호를 정확하게 입력해주세요. (쉼표 기준, 여섯 개)");
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return ERROR;
+            return (ArrayList<Integer>) ERROR.ERROR2.getValue();
         }
     }
 }

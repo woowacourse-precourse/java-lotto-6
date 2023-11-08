59441effe3b026122a97fc8d40bc2132defc2756
godee95 <50854903+godee95@users.noreply.github.com>
2023-11-08 17:04:12 +0900

    feat : 사용자 입력, 당첨번호 입력 기능 구현

--- src/main/java/lotto/InputManager.java
+++ src/main/java/lotto/InputManager.java
@@ -1,9 +1,15 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputManager {
     private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public int getPurchaseAmount() {
         int purchaseAmount = Integer.parseInt(Console.readLine());
@@ -12,6 +18,19 @@ public class InputManager {
             throw new IllegalArgumentException();
         }
         return purchaseAmount;
+    }
+
+    public List<Integer> getWinnigNumbers() {
+        List<Integer> winningNumbers = Arrays.stream(Console.readLine().split(","))
+                                            .map(Integer::parseInt)
+                                            .collect(Collectors.toList());
+        for(int number : winningNumbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+        return winningNumbers;
     }
     
 }

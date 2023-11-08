2829244e10b4f7bae9d0f761b60f535a61b7bf32
minkyuu <minkyu023@gmail.com>
2023-11-08 21:58:08 +0900

    feat: Lotto 내부 중복 숫자 방지 로직 추가

--- src/main/java/lotto/company/Lotto.java
+++ src/main/java/lotto/company/Lotto.java
@@ -1,13 +1,23 @@
 package lotto.company;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (numbers.size() == 6 && set.size() < 6) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validate(List<Integer> numbers) {

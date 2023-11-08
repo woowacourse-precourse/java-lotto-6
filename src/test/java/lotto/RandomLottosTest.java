b30aaf643b23be297e4b41ba6358b355b2be28c6
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-08 14:48:18 +0900

    style: 변수를 적절한 네이밍으로 변경

--- src/test/java/lotto/RandomLottosTest.java
+++ src/test/java/lotto/RandomLottosTest.java
@@ -10,28 +10,29 @@ import org.junit.jupiter.api.Test;
 
 public class RandomLottosTest {
     private RandomLottos randomLottos;
+    private Price price;
 
     @BeforeEach
     void set() {
-        Price price = new Price("8000");
+        price = new Price("8000");
         randomLottos = new RandomLottos(price);
     }
 
     @DisplayName("Price의 값에 따라 생성할 랜덤로또 개수 구하는 테스트")
     @Test
-    void countingRandomLottos() {
-        int actualCounting = randomLottos.getCounting();
-        int expectedCounting = 8;
+    void countRandomLottos() {
+        int actual = randomLottos.getCount();
+        int expected = 8;
 
-        assertThat(actualCounting).isEqualTo(expectedCounting);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("랜덤로또가 개수대로 잘 생성 되는지 테스트")
     @Test
     void createRandomLottos() {
-        int actualRandomLottosSize = randomLottos.getRandomLottos().size();
-        int expectedRandomLottosSize = 8;
+        int actualSize = randomLottos.getRandomLottos().size();
+        int expectedSize = 8;
 
-        assertThat(actualRandomLottosSize).isEqualTo(expectedRandomLottosSize);
+        assertThat(actualSize).isEqualTo(expectedSize);
     }
 }

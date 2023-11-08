09103ee6abd8d0ee7bce71f6705ae9fa6837194d
hunm719 <hunm719@naver.com>
2023-11-08 16:14:38 +0900

    getPurchaseAmount메서드 수정 및 관련 메인 메서드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,8 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         int PRICE_LOTTO = 1000;
-        String console = Console.readLine();
+        Console console = new Console();
 
+        int purchaseAmount = getPurchaseAmount(console);
 
     }
 

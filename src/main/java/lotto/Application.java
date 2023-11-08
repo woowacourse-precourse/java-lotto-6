e4f26fd1a4ceb8cf8903fbb2b6a08d708197879d
fly-like-you <qkrwnsgh71w@gmail.com>
2023-11-08 23:02:54 +0900

    refactor(main, validation): 리팩토링 당첨 통계 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,9 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.Money;
+import lotto.domain.Statistic;
 import lotto.service.LottoBroadCast;
+import lotto.service.LottoChecker;
 import lotto.service.StatisticProducer;
 import lotto.service.Store;
 import lotto.view.impl.BonusNumberView;
@@ -12,16 +13,17 @@ import lotto.view.impl.WinningNumberView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Application.run();
     }
 
-
     private static void run() {
-        Money lottoPrice = Money.wons(1000);
+        LottoController lottoController = init();
+        lottoController.operate();
+    }
 
+    private static LottoController init(){
         Store store = new Store(
-            new LottoBuyView()
+                new LottoBuyView()
         );
         LottoBroadCast lottoBroadCast = new LottoBroadCast(
                 new WinningNumberView(),
@@ -29,10 +31,10 @@ public class Application {
         );
 
         StatisticView statisticView = new StatisticView();
+        LottoChecker lottoChecker = new LottoChecker();
 
-
-        StatisticProducer statisticProducer = new StatisticProducer();
-        LottoController lottoController = new LottoController(store, statisticProducer, statisticView, lottoBroadCast);
-        lottoController.operate();
+        StatisticProducer statisticProducer = new StatisticProducer(lottoChecker, new Statistic(), new StatisticView());
+        return new LottoController(store, statisticProducer, statisticView, lottoBroadCast);
     }
+
 }
--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Validation {
-
     protected List<ValidationCondition> validationConditions;
 
     public Validation(ValidationCondition... validationConditions){

9c7ed802ca13a709e3859fd64abfcf3c878113de
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 16:44:27 +0900

    feat(LottoService): 로또 구매기능 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,47 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Player;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.validate.InputValidator;
+import lotto.service.dto.PurchaseResultDto;
+
+import java.util.List;
+
+public class LottoService {
+    private static final int LOTTO_COST = 1000;
+    private final Player player;
+    private final Lottos lottos;
+    private final InputValidator inputValidator;
+
+    public LottoService(Player player, Lottos lottos, InputValidator inputValidator) {
+        this.player = player;
+        this.lottos = lottos;
+        this.inputValidator = inputValidator;
+    }
+
+    public PurchaseResultDto purchaseLotto(String input) {
+        inputValidator.validatePurchaseAmount(input);
+
+        int purchaseAmountInput = Integer.parseInt(input);
+        player.insertPurchaseAmount(purchaseAmountInput);
+
+        return generateLottos(purchaseAmountInput);
+    }
+
+    private PurchaseResultDto generateLottos(int purchaseAmountInput) {
+        for (int i = 0; i < purchaseAmountInput / LOTTO_COST; i++) {
+            Lotto lotto = generateLottoWithRandomNumber();
+            lottos.insertLotto(lotto);
+        }
+
+        List<List<Integer>> tickets = lottos.getLottoTickets();
+
+        return new PurchaseResultDto(tickets.size(), tickets);
+    }
+
+    private Lotto generateLottoWithRandomNumber() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+}

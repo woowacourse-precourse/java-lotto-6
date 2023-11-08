91818de4a0fa05c1f1ac862fa130f519e1514b6f
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 22:08:54 +0900

    refactor(BonusNumberValidation): WinningNumber 필드 타입 변경으로 인한 파라미터 타입 변경

--- src/main/java/lotto/validation/BonusNumberValidation.java
+++ src/main/java/lotto/validation/BonusNumberValidation.java
@@ -2,15 +2,15 @@ package lotto.validation;
 
 import static lotto.constant.ErrorMessage.DUPLICATION_EXCEPTION;
 
-import java.util.List;
+import java.util.Set;
 
 public class BonusNumberValidation {
-    public static void validateBonusNumber(int bonusNumber, List<Integer> winningNumber) {
+    public static void validateBonusNumber(int bonusNumber, Set<Integer> winningNumber) {
         WinningNumbersValidation.validateNumberInRange(bonusNumber);
         validateDuplication(bonusNumber, winningNumber);
     }
 
-    private static void validateDuplication(int bonusNumber, List<Integer> winningNumber) {
+    private static void validateDuplication(int bonusNumber, Set<Integer> winningNumber) {
         if (winningNumber.contains(bonusNumber)) {
             throw new IllegalArgumentException(DUPLICATION_EXCEPTION.getMessage());
         }

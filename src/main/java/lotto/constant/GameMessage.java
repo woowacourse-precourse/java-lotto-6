0ffa90e37b9dd0c07a6296840b675210b5076abd
wooni97 <jhw970714@gmail.com>
2023-11-08 20:01:38 +0900

    refactor: 상수를 상수 전용 클래스로 이동
    - 이 커밋은 여러 클래스에서 공통적으로 사용될 상수를 관리가 용이하고자 상수 전용 클래스로 옮깁니다.

---
+++ src/main/java/lotto/constant/GameMessage.java
@@ -0,0 +1,9 @@
+package lotto.constant;
+
+public class GameMessage {
+    public static final String ASK_PAY_AMOUNT = "구입금액을 입력해 주세요";
+    public static final String ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요";
+    public static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요";
+    public static final String GAME_RESULT = "당첨 통계\n---";
+
+}

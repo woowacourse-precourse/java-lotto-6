3fb8eb10212ec1dd5b08c376f28494a7d4cee459
Yujin <tkfkd7038@naver.com>
2023-11-08 23:30:53 +0900

    feat(lotto): 로또 1장의 숫자와 당첨번호 비교하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +14,34 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 입니다.");
+        }
+        if (numSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력해주세요.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static void showTicketCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void showTicketInfo(int index, Lotto[] lottoTicket) {
+        System.out.println(lottoTicket[index].numbers);
+    }
+
+    public int compareNumbers(List<Integer> winningNumbers, int bonusNumber) {// 로또 1장의 숫자와 당첨번호 비교
+        int count = 0;
+        for (int i = 0; i < winningNumbers.size(); i++) {
+            for (int j = 0; j < numbers.size(); j++) {
+                if (winningNumbers.get(i).equals(numbers.get(j))) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
 }

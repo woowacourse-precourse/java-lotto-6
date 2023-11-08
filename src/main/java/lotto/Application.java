b3e722a83dd1a8ae68e83c4343d17cd744394a4a
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-08 17:04:25 +0900

    feat(Application): 메인 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController.run();
     }
 }

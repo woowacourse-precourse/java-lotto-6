60522f9f9ec044ba2650241ea621f62c03d97759
권장순 <jsoonworld@gmail.com>
2023-11-08 19:42:46 +0900

    test( LottoResultTest#testPartialMatch() ) : 당첨 번호와 부분 일치하는 경우 결과 계산.

--- src/test/java/lotto/v3/model/LottoResultTest.java
+++ src/test/java/lotto/v3/model/LottoResultTest.java
@@ -31,5 +31,28 @@ class LottoResultTest {
         assertThat(matchCounts.get(LottoRank.FOURTH)).isEqualTo(0);
         assertThat(matchCounts.get(LottoRank.FIFTH)).isEqualTo(0);
     }
-    
+
+    @Test
+    @DisplayName("당첨 번호와 부분 일치하는 경우 결과 계산")
+    void testPartialMatch() {
+        // Given
+        List<List<Integer>> purchasedNumbers = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5, 45), // 5개 일치 + 보너스 번호 일치
+                Arrays.asList(1, 2, 3, 7, 8, 9)    // 3개 일치
+        );
+        Set<Integer> winningNumbers = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 45;
+
+        // When
+        LottoResult result = new LottoResult(purchasedNumbers, winningNumbers, bonusNumber);
+        Map<LottoRank, Integer> matchCounts = result.getMatchCounts();
+
+        // Then
+        assertThat(matchCounts.get(LottoRank.FIRST)).isEqualTo(0);
+        assertThat(matchCounts.get(LottoRank.SECOND)).isEqualTo(1); // 보너스 번호를 포함한 5개 일치
+        assertThat(matchCounts.get(LottoRank.THIRD)).isEqualTo(0);
+        assertThat(matchCounts.get(LottoRank.FOURTH)).isEqualTo(0);
+        assertThat(matchCounts.get(LottoRank.FIFTH)).isEqualTo(1); // 3개 일치
+    }
+
 }

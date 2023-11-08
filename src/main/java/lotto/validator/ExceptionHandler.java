c7bb6f08992e5af0d5b4ec612c5dde76923566a0
Cycrypto <jh01love@naver.com>
2023-11-08 09:17:22 +0900

    feat:exception 핸들러 생성

--- src/main/java/lotto/validator/ExceptionHandler.java
+++ src/main/java/lotto/validator/ExceptionHandler.java
@@ -1,9 +1,32 @@
 package lotto.validator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.function.Supplier;
 import lotto.constant.Message;
-import lotto.model.Money;
+import lotto.view.View;
 
-public class InputValidator {
+public class ExceptionHandler {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+    public static <T> T handleException(Message message, Supplier<T> supplier) {
+        while (true) {
+            try {
+                printMessage(message.getMessage());
+                return supplier.get();
+            } catch (NumberFormatException e) {
+                View.printMessage(ERROR_MESSAGE + "숫자만 입력가능합니다.");
+                readLineWithMessage();
+            } catch (Exception e) {
+                View.printMessage(ERROR_MESSAGE +"예상치 못한 오류입니다 : "+e.getMessage());
+                throw e;
+            }
+        }
+    }
 
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private static String readLineWithMessage() {
+        return Console.readLine();
+    }
 }

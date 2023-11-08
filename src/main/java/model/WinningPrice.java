956fd03043181da7446d2f9787b2ffd1a4f3d072
DSRN0000 <dsrn0000@gmail.com>
2023-11-08 15:40:47 +0900

    feat(WinningPrice): 코드 수정

--- src/main/java/model/WinningPrice.java
+++ src/main/java/model/WinningPrice.java
@@ -1,43 +1,44 @@
 package model;
 
-import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum WinningPrice {
     CORRECT_ZERO(0
             , 0
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber < 3),
+            , (correctLottoNumber, containBonusNumber) -> correctLottoNumber < 3),
     CORRECT_THREE(5_000
             , 3
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 3),
+            , (correctLottoNumber, containBonusNumber) -> correctLottoNumber == 3),
     CORRECT_FOUR(50_000
             , 4
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
+            , (correctLottoNumber, containBonusNumber) -> correctLottoNumber == 4),
     CORRECT_FIVE(1_500_000
             , 5
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
+            , (correctLottoNumber, containBonusNumber) -> correctLottoNumber == 5 && !containBonusNumber),
     CORRECT_FIVE_BONUS(30_000_000
             , 5
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
+            , (correctLottoNumber, containBonusNumber) -> correctLottoNumber == 5 && containBonusNumber),
     CORRECT_ALL(2_000_000_000
             , 6
-            , (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
+            , (correctLottoNumber, containBonusNumber) -> correctLottoNumber == 6);
 
     private final int winningPrice;
     private final int correctLottoNumber;
     private final BiPredicate<Integer, Boolean> isMatch;
 
-    WinningPrice(final int money, final int correctLottoNumber, final BiPredicate<Integer, Boolean> isMatch) {
-        this.winningPrice = money;
+    WinningPrice(final int price, final int correctLottoNumber, final BiPredicate<Integer, Boolean> isMatch) {
+        this.winningPrice = price;
         this.correctLottoNumber = correctLottoNumber;
         this.isMatch = isMatch;
     }
 
-    public static WinningPrice getWinningPrice(final int correctLottoNumber, final boolean containBonusNumber) {
-        return Arrays.stream(WinningPrice.values())
-                .filter(winningPrice -> winningPrice.isMatch.test(correctLottoNumber, containBonusNumber))
-                .findAny()
-                .orElse(CORRECT_ZERO);
+    public static WinningPrice winningPrice(final int correctLottoNumber, final boolean containBonusNumber) {
+        for (WinningPrice winningPrice : WinningPrice.values()) {
+            if (winningPrice.isMatch.test(correctLottoNumber, containBonusNumber)) {
+                return winningPrice;
+            }
+        }
+        return CORRECT_ZERO;
     }
 
     public int getWinningPrice() {

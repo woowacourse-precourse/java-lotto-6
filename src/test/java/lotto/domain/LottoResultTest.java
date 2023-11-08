64e31f5d7f320e91e19bb70376bdcb1b2ac26a9a
HyunmoBae <tsi0520@naver.com>
2023-11-08 12:15:59 +0900

    test(lotto) : add compareBonusNumber test

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -14,6 +12,22 @@ class LottoResultTest {
 
         int result = lottoResult.compareLottoNumber(List.of(1,2,3,4,5,6,7),List.of(1,2,3,7,8,9));
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void compareBonusNumberTest() {
+        LottoResult lottoResult = new LottoResult();
+
+        boolean result = lottoResult.compareBonusNumber(List.of(1,2,3,4,5,6,7),List.of(1,2,5,6,7,8));
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    void compareBonusNumberTest2() {
+        LottoResult lottoResult = new LottoResult();
+
+        boolean result = lottoResult.compareBonusNumber(List.of(1,2,3,4,5,6,7),List.of(1,2,4,5,6,8));
+        assertThat(result).isEqualTo(false);
     }
 
 }

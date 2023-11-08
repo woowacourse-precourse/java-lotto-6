cd502f9d2697f7424b78e4717d18abacf18b76b8
mdgome <mdgome3@gmail.com>
2023-11-08 15:55:56 +0900

    test: 발급한 티켓의 당첨금 합계
    - [X] 당첨금 합이 올바르게 계산되는지

--- src/test/java/lotto/model/WinStatisticsTest.java
+++ src/test/java/lotto/model/WinStatisticsTest.java
@@ -1,4 +1,21 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class WinStatisticsTest {
+
+    @DisplayName("당첨금 합이 올바르게 계산되는지")
+    @Test
+    void verifyTotalPrizeAmountCalculation(){
+        WinStatistics result = new WinStatistics();
+        double expect = 30005000.0;
+
+        result.pushRank(Rank.FIFTH);
+        result.pushRank(Rank.SECOND);
+
+        assertThat(result.sum()).isEqualTo(expect);
+    }
 }

b4cd016d86ae5de9040152b754ace9016c033db1
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 14:16:53 +0900

    feat: application file first commit
    application file first commit

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.control.Control;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Control LottoGame = new Control();
+        LottoGame.playLotto();
     }
 }

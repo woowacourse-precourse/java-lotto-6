6bae855b12ec59ecd2c9d13f1af58123002d496f
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:15:33 +0900

    [FIX] 예외처리 형식 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,35 +1,30 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public enum Rank {
-    FIRST(null, 6) {
-        private final int prizeMoney = 2_000_000_000;
-    }, SECOND(Boolean.TRUE, 5) {
-        private final int prizeMoney = 30_000_000;
-    }, THIRD(null, 5) {
-        private final int prizeMoney = 1_500_000;
-    }, FOURTH(null, 4) {
-        private final int prizeMoney = 50_000;
-    }, FIFTH(null, 3) {
-        private final int prizeMoney = 5_000;
-    };
+    FIRST(2_000_000_000, null, 6),
+    SECOND(30_000_000, Boolean.TRUE, 5),
+    THIRD(1_500_000, null, 5),
+    FOURTH(50_000, null, 4),
+    FIFTH(5_000, null, 3),
+    NON(0, null, 0);
     private final Boolean matchBonusNumber;
     private final Integer matchCount;
-    private Integer prizeMoney;
+    private final int prizeMoney;
 
-    Rank(Boolean matchBonusNumber, Integer matchCount) {
+    Rank(int prizeMoney, Boolean matchBonusNumber, Integer matchCount) {
+        this.prizeMoney = prizeMoney;
         this.matchBonusNumber = matchBonusNumber;
         this.matchCount = matchCount;
     }
 
     public static Rank of(Boolean matchBonusNumber, Integer matchCount) {
         return Stream.of(Rank.values())
-                .filter(rank -> rank.matchBonusNumber.equals(matchBonusNumber))
-                .filter(rank -> rank.matchCount.equals(matchCount))
+                .filter(rank -> rank.matchBonusNumber==matchBonusNumber)
+                .filter(rank -> rank.matchCount==matchCount)
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("No matching rank found for match count: " + matchCount + ", match bonus: " + matchBonusNumber));
+                .orElse(NON);
     }
 
     public Integer getPrizeMoney() {

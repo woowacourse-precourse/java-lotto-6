4674344e48c50f76543037de85862c9625cf2966
Kim mingyu <minq0302@naver.com>
2023-11-08 23:15:17 +0900

    refactor(view): omit blank line

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,17 @@
 package lotto.view;
 
+import lotto.controller.LottoController;
+
+
 public class InputView {
+    LottoController controller = new LottoController();
+
     public static void showInputPurchaseMoneyMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    private void newLine() {
+        System.out.println();
     }
 
     public static void showInputWinningNumbersMessage() {
@@ -12,4 +21,5 @@ public class InputView {
     public static void showInputBonusNumberMessage() {
         System.out.println("보너스 번호를 입력해주세요.");
     }
+
 }

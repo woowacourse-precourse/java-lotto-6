73973e938c883bd01188a2f5d25781f0db82eac1
alenjb <alenjb@naver.com>
2023-11-08 15:45:14 +0900

    🎨fix(LottoResultChecker): 한번에 여러 로또 번호의 당첨결과를 체크하도록 수정
    한번에 여러 로또 번호의 당첨결과를 체크할 수 있도록 필드와 파라미터들을 수정함.
    - List -> Lotto
    - reward를 각각 계산하는 방식 -> for문을 통해 reward를 매번 더해 총 reward를 계산하는 방식
    로또 번호들을 리스트가 아닌 Lotto객체 단위로 다루도록 수정함.
    당첨 통계에 이용할 수 있도록 각 등수의 당첨 횟수를 배열에 기록하는 기능을 구현함.
    - ranks[i]++ : 해당 등수의 당첨 횟수를 증가시킴
    로또 가격을 나타내는 상수(ONE_LOTTO_PRICE)를 Application으로 옮김.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+
 public class Application {
+    public static final double ONE_LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -5,33 +5,37 @@ import java.util.function.Predicate;
 
 public class LottoResultChecker {
 
-    public static final double ONE_LOTTO_PRICE = 1000;
     final NumberGenerator numberGenerator = new NumberGenerator();
     List<Integer> winningNumbers; // 당첨번호
     int bonusNumber; // 당첨 보너스 번호
-    List<Integer> userLottoNumbers; // 사용자가 구매한 번호
+    Lotto [] userLottoNumbers; // 사용자가 구매한 번호
+    int [] ranks = {0, 0, 0, 0, 0, 0}; // 각 등수의 당첨 개수 ex) ranks[1]은 1등 당첨 개수; rank[0]은 미 당첨 개수
     int reward; // 상금
     String returnRatio; // 수익률
 
-    public LottoResultChecker(List<Integer> winningNumbers, List<Integer> userLottoNumbers, int bonusNumber) {
-        this.winningNumbers = winningNumbers;
-        this.userLottoNumbers = userLottoNumbers;
+    public LottoResultChecker(Lotto winningLotto, Lotto [] userLotto, int bonusNumber) {
+        this.winningNumbers = winningLotto.getNumbers();
+        this.userLottoNumbers = userLotto;
         this.bonusNumber = bonusNumber;
     }
 
     public void checkLottoResult() {
-        reward = calculateReward(countMatchNumbers());
+        reward = 0;
+        for (Lotto userLottoNumber: userLottoNumbers){
+            reward += calculateReward(countMatchNumbers(userLottoNumber));
+        }
         returnRatio = calculateReturnRation() + ""; // 수익률을 계산 후 문자열로 변환
     }
 
-    public double countMatchNumbers() {
+    public double countMatchNumbers(Lotto lotto) {
         List<Integer> matchNumbers = winningNumbers.stream()
-                .filter(old -> userLottoNumbers.stream()
+                .filter(old -> lotto.getNumbers().stream()
                         .anyMatch(Predicate.isEqual(old)))
                 .toList();
         // 일치하는 개수가 6개인데 보너스 번호가 있는 경우 -> 2등
-        if (matchNumbers.size() == Rank.FIRST_PLACE.getMatchNumberCount() && hasBonusNumber(matchNumbers))
+        if (matchNumbers.size() == Rank.FIRST_PLACE.getMatchNumberCount() && hasBonusNumber(matchNumbers)){
             return Rank.SECOND_PLACE.getMatchNumberCount();
+        }
         return matchNumbers.size();
     }
 
@@ -40,17 +44,31 @@ public class LottoResultChecker {
     }
 
     public int calculateReward(double matchNumberCount) {
-        if (matchNumberCount == Rank.FIRST_PLACE.getMatchNumberCount()) return Rank.FIRST_PLACE.getReward();
-        if (matchNumberCount == Rank.SECOND_PLACE.getMatchNumberCount()) return Rank.SECOND_PLACE.getReward();
-        if (matchNumberCount == Rank.THIRD_PLACE.getMatchNumberCount()) return Rank.THIRD_PLACE.getReward();
-        if (matchNumberCount == Rank.FOURTH_PLACE.getMatchNumberCount()) return Rank.FOURTH_PLACE.getReward();
-        if (matchNumberCount == Rank.FIFTH_PLACE.getMatchNumberCount()) return Rank.FIFTH_PLACE.getReward();
+        if (matchNumberCount == Rank.FIRST_PLACE.getMatchNumberCount()) {
+            ranks[1]++;
+            return Rank.FIRST_PLACE.getReward();}
+        if (matchNumberCount == Rank.SECOND_PLACE.getMatchNumberCount()) {
+            ranks[2]++;
+            return Rank.SECOND_PLACE.getReward();
+        }
+        if (matchNumberCount == Rank.THIRD_PLACE.getMatchNumberCount()) {
+            ranks[3]++;
+            return Rank.THIRD_PLACE.getReward();}
+        if (matchNumberCount == Rank.FOURTH_PLACE.getMatchNumberCount()) {
+            ranks[4]++;
+            return Rank.FOURTH_PLACE.getReward();
+        }
+        if (matchNumberCount == Rank.FIFTH_PLACE.getMatchNumberCount()) {
+            ranks[5]++;
+            return Rank.FIFTH_PLACE.getReward();
+        }
+        ranks[0]++;
         return Rank.NONE.getReward(); // 매치되는 숫자 개수가 0인 경우
     }
 
     public double calculateReturnRation() {
         // 계산한 수익률을 소수점 둘째 자리에서 반올림하여 리턴
-        return Math.round((reward / ONE_LOTTO_PRICE) * 100.0) / 100.0;
+        return Math.round((reward / (Application.ONE_LOTTO_PRICE * userLottoNumbers.length)) * 100.0) / 100.0;
     }
 
 }

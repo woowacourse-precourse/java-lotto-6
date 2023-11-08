d8a3aa0796f87a0e8c3942d84bfb80a18b8ef006
HeoManBo <iovesssr__@naver.com>
2023-11-08 18:10:08 +0900

    Feat : 당첨 번호 입력 예외 처리 기능 추

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -3,9 +3,13 @@ package lotto;
 public final class ErrorMessage {
     public static String EMPTY_INPUT_ERROR="[ERROR] 숫자 입력이 필요합니다.";
     public static String NOT_VALID_MONEY="[ERROR] 1000 단위의 양의 정수 입력만 가능합니다.";
-    public static String NOT_VALID_LENGTH = "[ERROR] 숫자는 컴마(,)를 기준으로 6개만 입력이 가능합니다.";
+    public static String NOT_VALID_LENGTH = "[ERROR] 숫자는 공백 없이 컴마(,)를 구분자로 양의 정수 6개만 입력이 가능합니다.";
     public static String OUT_OF_RANGE_NUMBER = "[ERROR] 숫자는 1~45까지만 입력이 가능합니다.";
     public static String NOT_COMPOSED_OF_UNIQUE_NUMBERS = "[ERROR] 로또 번호는 중복돼서는 안됩니다.";
+    public static String NOT_VALID_DELIMITER = "[ERROR] 컴마(,)를 제외한 구분자는 불가능합니다.";
+    public static String PLACE_BETWEEN_MARGINS = "[ERROR] 컴마(,) 앞이나 뒤에 공백이 있어서는 안됩니다.";
+    public static String START_OR_END_WITH_DELIMITER = "[ERROR] 컴마(,)로 시작할 수 없습니다.";
+    public static String HAS_REPEATED_DELIMITER = "[ERROR] 컴마(,)가 연속으로 나올 수 없습니다.";
 
     private ErrorMessage(){}
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,15 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.ErrorMessage;
 import lotto.domain.BonusNumber;
+import lotto.domain.WinningLotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static lotto.ErrorMessage.*;
 import static lotto.Validator.*;
 
 public class InputView {
@@ -13,24 +18,18 @@ public class InputView {
     public static final String WinningNumbersMessage = "당첨 번호를 입력해 주세요.";
     public static final String BonusNumberMessage = "보너스 번호를 입력해 주세요.";
 
+    private static final String DELIMITER = ",";
+    private static final String WHITE_SPACE = " ";
+
     public int inputPurchaseAmount(){
-        while(true){
-            try{
-                System.out.println(purchaseAmountMessage);
-                String input = Console.readLine();
-                System.out.println();
+        System.out.println(purchaseAmountMessage);
+        String input = Console.readLine();
+        System.out.println();
 
-                validateNotEmptyInput(input);
-                int amount = validateInputIsNumeric(input);
-                validatePositiveNumber(amount);
+        validateNotEmptyInput(input);
+        int amount = validateInputIsNumeric(input);
 
-                return amount;
-            }catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }catch (Exception e){
-                throw e;
-            }
-        }
+        return amount;
     }
 
     public List<Integer> inputWinningNumber(){
@@ -39,20 +38,51 @@ public class InputView {
         System.out.println();
 
         validateNotEmptyInput(input);
-        List<Integer> numbers = validateSplittedInput(input);
+        List<Integer> numbers = validateCanSplittedInput(input);
+
         return numbers;
     }
 
-    public BonusNumber inputBonusNumber(){
+    public int inputBonusNumber(){
         System.out.println(BonusNumberMessage);
         String input = Console.readLine();
         System.out.println();
         validateNotEmptyInput(input);
         int bonusNumber = validateInputIsNumeric(input);
         validatePositiveNumber(bonusNumber);
-        return new BonusNumber(bonusNumber);
+        return bonusNumber;
     }
 
 
+    private static List<Integer> validateCanSplittedInput(String input){
+        validateNotBeDividedUsingDelimiter(input);
+        validateDelimiterInAppropriatePlace(input);
+        validateHasRepeatedDelimiter(input);
+        return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
+    }
 
+    private static void validateHasRepeatedDelimiter(String input) {
+        if(input.contains(DELIMITER.repeat(2))) throw new IllegalArgumentException(HAS_REPEATED_DELIMITER);
+    }
+
+    private static void validateDelimiterInAppropriatePlace(String input) {
+        checkStartOrEndDelimiter(input);
+        checkPlaceBetweenMargins(input);
+    }
+
+    private static void checkPlaceBetweenMargins(String input) {
+        if(input.contains(WHITE_SPACE + DELIMITER) || input.contains(DELIMITER + WHITE_SPACE)) {
+            throw new IllegalArgumentException(PLACE_BETWEEN_MARGINS);
+        }
+    }
+
+    private static void checkStartOrEndDelimiter(String input) {
+        if(input.startsWith(DELIMITER) || input.endsWith(DELIMITER)) {
+            throw new IllegalArgumentException(START_OR_END_WITH_DELIMITER);
+        }
+    }
+
+    private static void validateNotBeDividedUsingDelimiter(String input){
+        if(input.split(DELIMITER).length != 6) throw new IllegalArgumentException(NOT_VALID_LENGTH);
+    }
 }

4c7a8d6170c104a57f490eac1857a1e552b1b1ff
jjongwon7 <jjongwon7@naver.com>
2023-11-08 23:08:35 +0900

    test: 보너스 번호 입력 기능 예외 케이스 테스트 작성 보너스 번호가 당첨 번호와 중복되는 경우

--- src/test/java/lotto/validators/BonusNumberValidatorTest.java
+++ src/test/java/lotto/validators/BonusNumberValidatorTest.java
@@ -1,8 +1,11 @@
 package lotto.validators;
 
+import static lotto.constant.ErrorMessage.INPUT_BONUS_NUMBER_IN_WINNING_NUMBERS;
 import static lotto.constant.ErrorMessage.INPUT_BONUS_NUMBER_OUT_OF_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +20,19 @@ public class BonusNumberValidatorTest {
         assertThatThrownBy(() -> BonusNumberValidator.validBonusNumberInRange(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INPUT_BONUS_NUMBER_OUT_OF_RANGE);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    void validBonusNumberInWinningNumbers() {
+        // given
+        Integer bonusNumber = 5;
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // when & then
+        assertThatThrownBy(
+                () -> BonusNumberValidator.validBonusNumberInWinningNumbers(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INPUT_BONUS_NUMBER_IN_WINNING_NUMBERS);
     }
 }

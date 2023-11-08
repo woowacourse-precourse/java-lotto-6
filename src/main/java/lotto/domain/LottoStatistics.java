c901e72394130f4c71880e0a75b6203fedfd0efe
junsoo <choix712@umn.edu>
2023-11-08 21:37:13 +0900

    refactor: 로또 결과를 출력하는 부분 리팩토링

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -4,31 +4,27 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-public class LottoResult {
+public class LottoStatistics {
 
     private static final double PERCENTAGE = 100.0;
     private final LinkedHashMap<Rank, Integer> rankHashMap;
 
-    public LottoResult(LinkedHashMap<Rank, Integer> rankHashMap) {
+    public LottoStatistics(LinkedHashMap<Rank, Integer> rankHashMap) {
         this.rankHashMap = rankHashMap;
     }
 
     public LinkedHashMap<Rank, Integer> getRankResult(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
         LottoService lottoService = new LottoService(new ArrayList<>());
-
         Rank rank = lottoService.getLottoRank(lotto, winningNumbers, bonusNumber);
         rankHashMap.put(rank, rankHashMap.getOrDefault(rank, 0) + 1);
         return new LinkedHashMap<>(rankHashMap);
     }
 
     public double calculateProfit(int playerMoney) {
-        double sum = 0;
-        for (Rank rank : rankHashMap.keySet()) {
-            int prize = rank.getPrize();;
-            int number = rankHashMap.get(rank);
-            sum += prize * number;
-        }
-        return (sum / playerMoney) * PERCENTAGE;
+        double totalPrize = rankHashMap.entrySet().stream()
+                .mapToDouble(entry -> entry.getKey().getPrize() * entry.getValue())
+                .sum();
+        return (totalPrize / playerMoney) * PERCENTAGE;
     }
 
 }

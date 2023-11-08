a74d8cacd18f8b4d7ef3913c4f21fa0c8daa7b63
KDT live <dnjsxo0616@gmail.com>
2023-11-08 16:33:32 +0900

    refactor(Rank): Rank 열거형 리팩토링
    - - Rank 열거형에서 상금과 매치 결과 판단 함수를 변경하고 필드 수정.

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,20 +4,22 @@ import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 
 public enum Rank {
-    NONE(0, 0, (matchCount, bonus) -> matchCount <= 2),
-    FIFTH(3, 5_000, (matchCount, bonus) -> matchCount == 3),
-    FOURTH(4, 50_000, (matchCount, bonus) -> matchCount == 4),
-    THIRD(5, 1_500_000, (matchCount, bonus) -> matchCount == 5 && !bonus),
-    SECOND(5, 30_000_000, (matchCount, bonus) -> matchCount == 5 && bonus),
-    FIRST(6, 2_000_000_000, (matchCount, bonus) -> matchCount == 6);
+    NONE(0, 0, false, (matchCount, bonus) -> matchCount <= 2),
+    FIFTH(3, 5_000, false, (matchCount, bonus) -> matchCount == 3),
+    FOURTH(4, 50_000, false, (matchCount, bonus) -> matchCount == 4),
+    THIRD(5, 1_500_000, false, (matchCount, bonus) -> matchCount == 5 && !bonus),
+    SECOND(5, 30_000_000, true, (matchCount, bonus) -> matchCount == 5 && bonus),
+    FIRST(6, 2_000_000_000, false, (matchCount, bonus) -> matchCount == 6);
 
     private final int matchCount;
     private final int prize;
+    private final boolean bonus;
     private final BiPredicate<Integer, Boolean> matchResult;
 
-    Rank(final int matchCount, final int prize, final boolean bonus, final BiPredicate<Integer, Boolean> matchResult){
+    Rank(final int matchCount, final int prize, final boolean bonus, final BiPredicate<Integer, Boolean> matchResult) {
         this.matchCount = matchCount;
         this.prize = prize;
+        this.bonus = bonus;
         this.matchResult = matchResult;
     }
 

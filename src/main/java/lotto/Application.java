e9b1f2e915a6dcb075c9c70282ee247fa70ef50a
JP-company <wjsdj2008@gmail.com>
2023-11-08 18:00:46 +0900

    Refactor: [수정] 메인 메서드 속 로직 이름 변경
    흐름상 더 이해하기 쉬운 용어로 로직 이름을 변경했다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,13 +5,12 @@ import lotto.domain.*;
 import java.util.List;
 
 public class Application {
-
     public static void main(String[] args) {
         User user = new User();
         List<Lotto> userLottos = user.buyLotto();
         LottoWinningNumber lottoWinningNumber = user.createWinningLottoNumber();
 
-        Referee referee = new Referee(lottoWinningNumber, userLottos);
-        referee.showResult();
+        Simulator simulator = new Simulator();
+        simulator.run(lottoWinningNumber, userLottos);
     }
 }

c672b2263001de7012d3244b65d260d16d8eb724
chanwoo <mdsoo55828@gmail.com>
2023-11-08 14:05:23 +0900

    feat: Lotto 중복 숫자 예외처리 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,7 +4,9 @@ import lotto.constant.ErrorConstants;
 import lotto.constant.GameConstants;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class Lotto {
@@ -19,6 +21,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateLottoRange(numbers);
+        validateLottoDuplication(numbers);
     }
 
     private void sortNumbers(List<Integer> numbers){
@@ -36,6 +39,13 @@ public class Lotto {
             if (number < GameConstants.MIN_RANGE || number > GameConstants.MAX_RANGE) {
                 throw new IllegalArgumentException(ErrorConstants.OVER_LOTTO_RANGE);
             }
+        }
+    }
+
+    private void validateLottoDuplication(List<Integer> numbers) {
+        Set<Integer> checkNumbers = new HashSet<>(numbers);
+        if (checkNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorConstants.DUPLICATE_LOTTO_NUMBER);
         }
     }
 

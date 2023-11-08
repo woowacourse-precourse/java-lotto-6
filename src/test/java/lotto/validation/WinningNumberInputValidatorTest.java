1eaf8eef7b10eb636c70bdb04cdf5821b4fa6c3b
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 16:12:55 +0900

    Test : 당첨 번호 범위 테스트
    당첨 번호가 1~45 사이인지 검증하는 테스트

--- src/test/java/lotto/validation/WinningNumberInputValidatorTest.java
+++ src/test/java/lotto/validation/WinningNumberInputValidatorTest.java
@@ -15,4 +15,11 @@ class WinningNumberInputValidatorTest {
     void 쉼표_5개_여부_테스트(String winningNumber, boolean expected) {
         Assertions.assertThat(winningNumberInputValidator.isCommaFive(winningNumber)).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, true", "45, true", "20, true", "0, false", "46, false"})
+    @DisplayName("당첨 번호 범위 테스트")
+    void 당첨_번호_범위_테스트(int oneWinningNumber, boolean expected) {
+        Assertions.assertThat(winningNumberInputValidator.isLottoNumber(oneWinningNumber)).isEqualTo(expected);
+    }
 }

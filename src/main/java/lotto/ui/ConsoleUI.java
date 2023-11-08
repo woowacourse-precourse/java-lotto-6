dd42d7256f9e8a375c077730e7991c8c08260dea
jjw0071 <jjw0071@naver.com>
2023-11-08 11:23:42 +0900

    feat(ConsoleUI) : 구입금액을 입력받고 예외처리하는 과정을 조건에 맞게 수정
    - 잘못된 값 입력 시에 error문구 출력하고 다시 입력 받도록 메서드 수정 및 메서드 길이(15줄) 조건에 맞게 수정

--- src/main/java/lotto/ui/ConsoleUI.java
+++ src/main/java/lotto/ui/ConsoleUI.java
@@ -10,17 +10,30 @@ public class ConsoleUI {
         this.validator = validator;
     }
 
-    public int getPurchaseAmount(){
-        System.out.println("구입금액을 입력해 주세요.");
-        String input = Console.readLine();
-        validator.validateInteger(input);
-        int lottoPurchaseCost = Integer.parseInt(input);
-        validator.validatePositiveInteger(lottoPurchaseCost);
-        validator.validateAmountInThousands(lottoPurchaseCost);
+    public int getPurchaseAmount() {
+        int lottoPurchaseCost;
+        do {
+            lottoPurchaseCost = attemptToGetValidPurchaseAmount();
+        } while (lottoPurchaseCost == -1);
         return lottoPurchaseCost;
     }
 
+    private int attemptToGetValidPurchaseAmount() {
+        try {
+            String input = promptForPurchaseAmount();
+            validator.validateInteger(input);
+            int amount = Integer.parseInt(input);
+            validator.validatePositiveInteger(amount);
+            validator.validateAmountInThousands(amount);
+            return amount;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
 
-
-
+    private String promptForPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
 }

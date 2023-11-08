fcf4f62d59776fc10aa55b219e43c7884bb34492
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-08 10:00:18 +0900

    feat : 번호 비교 구현
    getNumberOfMatch, getNumberOfMatchResults 구현

--- src/main/java/lotto/NumberComparing.java
+++ src/main/java/lotto/NumberComparing.java
@@ -1,13 +1,36 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class NumberComparing {
-    private final List<Integer> lottoNumbers;
+    private final int SIZEOFLOTTONUMBERS = 6;
 
-    public NumberComparing(List<Integer> lottoNumbers) {
-        this.lottoNumbers = lottoNumbers;
+    private final List<Integer> winningNumbers;
+
+    public NumberComparing(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
     }
 
+    public int getNumberOfMatch(Lotto lotto) {
+        HashSet<Integer> lottoNumberBundle = new HashSet<>(lotto.getNumbers());
+        HashSet<Integer> winningNumbersBundle = new HashSet<>(winningNumbers);
 
+        lottoNumberBundle.retainAll(winningNumbersBundle);
+
+        int result = SIZEOFLOTTONUMBERS - lottoNumberBundle.size();
+
+        return result;
+    }
+
+    public List<Integer> getNumberOfMatchResults(List<Lotto> lottos) {
+        List<Integer> results = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            results.add(getNumberOfMatch(lotto));
+        }
+
+        return results;
+    }
 }

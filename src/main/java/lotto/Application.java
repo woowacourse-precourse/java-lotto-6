2bbf9b59bd67bc76a7c8bd4d38077477a366f9b1
Dalliaaaaa <ajswlsoato@naver.com>
2023-11-08 22:28:36 +0900

    style: main에서 실행되도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -23,29 +23,36 @@ enum PrizeRank {
 
 public class Application {
     public static void main(String[] args) {
-        MyLotto.LottoPaper(Money.InputMoney());
+        int money = Money.InputMoney();
+        List<Integer> numbers = MyLotto.LottoPaper(Money.CorrectMoney(money));
+        List<Integer> num_lst = WinnigNum.LottoNum(WinnigNum.InputLottoNum());
+        int bonus = WinnigNum.BonusNum(WinnigNum.InputBonus());
+        Lotto lotto = new Lotto(num_lst);
+        List<Integer> mat_num = WinningCaculate.Calculate(numbers, num_lst, bonus);
+        WinningResult.EarningResult((WinningCaculate.EarningRate(WinningCaculate.Rank(mat_num), money)));
     }
 }
-
 class Money {
     public static int InputMoney() {
         while (true) {
-            try{
+            try {
                 System.out.println("구입금액을 입력해 주세요.");
                 String m = Console.readLine();
                 return ExceptMoney(m);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
-    public static int ExceptMoney(String money){
+
+    public static int ExceptMoney(String money) {
         try {
             return Integer.parseInt(money);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
         }
     }
+
     public static int CorrectMoney(int money) {
         if (money % 1000 == 0) {
             return money / 1000;
@@ -54,7 +61,7 @@ class Money {
     }
 }
 
-class MyLotto{
+class MyLotto {
     public static List<Integer> LottoPaper(int cnt) {
         ArrayList<List<Integer>> mylotto = new ArrayList<>();
         for (int i = 0; i < cnt; i++) {
@@ -65,16 +72,18 @@ class MyLotto{
         List<Integer> numbers = MakeFlattend(mylotto);
         return numbers;
     }
-    public static List<Integer> MakeFlattend(ArrayList<List<Integer>> mylotto){
+
+    public static List<Integer> MakeFlattend(ArrayList<List<Integer>> mylotto) {
         List<Integer> flattenedList = new ArrayList<>();
         for (List<Integer> sublist : mylotto) {
             flattenedList.addAll(sublist);
         }
         return flattenedList;
     }
-    public static void PrintLottoPaper(int cnt, ArrayList<List<Integer>> mylotto){
+
+    public static void PrintLottoPaper(int cnt, ArrayList<List<Integer>> mylotto) {
         System.out.println();
-        System.out.println(cnt+"개를 구매했습니다.");
+        System.out.println(cnt + "개를 구매했습니다.");
         for (List<Integer> row : mylotto) {
             System.out.println(row);
         }
@@ -82,13 +91,14 @@ class MyLotto{
 }
 
 class WinnigNum {
-    public static String InputLottoNum(){
+    public static String InputLottoNum() {
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningnum = Console.readLine();
         return winningnum;
     }
-    public static List<Integer> LottoNum(String winningnum){
+
+    public static List<Integer> LottoNum(String winningnum) {
         String[] num = winningnum.split(",");
         List<Integer> num_lst = new ArrayList<>();
         for (String str : num) {
@@ -96,13 +106,15 @@ class WinnigNum {
         }
         return num_lst;
     }
-    public static String InputBonus(){
+
+    public static String InputBonus() {
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusnum = Console.readLine();
         return bonusnum;
     }
-    public static int BonusNum(String bonusnum){
+
+    public static int BonusNum(String bonusnum) {
         return Integer.parseInt(bonusnum);
     }
 }
@@ -114,29 +126,32 @@ class WinningCaculate {
         List<Integer> match_cnt = MatchCount(nums, num_lst, mat_num, mat_cnt, bonus);
         return match_cnt;
     }
-    public static List<Integer> MatchCount(List<Integer> nums, List<Integer> num_lst, int mat_num, List<Integer> match_cnt, int bonus){
+
+    public static List<Integer> MatchCount(List<Integer> nums, List<Integer> num_lst, int mat_num, List<Integer> match_cnt, int bonus) {
         List<Integer> matchcnt = new ArrayList<>(match_cnt);
         for (int i = 0; i < nums.size(); i++) {
-            if (i % 6 == 0 || i == nums.size()-1){
+            if (i % 6 == 0 || i == nums.size() - 1) {
                 int n = SpecialCount(mat_num, bonus, nums);
-                matchcnt.set(n, matchcnt.get(n)+1);
+                matchcnt.set(n, matchcnt.get(n) + 1);
                 mat_num = 0;
             }
-            if (num_lst.contains(nums.get(i))){
+            if (num_lst.contains(nums.get(i))) {
                 mat_num += 1;
             }
         }
         return matchcnt;
     }
-    public static int SpecialCount(int mat_num, int bonus, List<Integer> nums){
-        if (mat_num == 5 && nums.contains(bonus)){
+
+    public static int SpecialCount(int mat_num, int bonus, List<Integer> nums) {
+        if (mat_num == 5 && nums.contains(bonus)) {
             return 6;
         }
         return mat_num;
     }
-    public static int Rank(List<Integer> match_cnt){
+
+    public static int Rank(List<Integer> match_cnt) {
         Collections.reverse(match_cnt);
-        int[] rank_cnt = {0,0,0,0,0};
+        int[] rank_cnt = {0, 0, 0, 0, 0};
         int prize = 0;
         PrizeRank[] ran = PrizeRank.values();
         for (int i = 0; i <= 4; i++) {
@@ -148,8 +163,9 @@ class WinningCaculate {
         WinningResult.RankResult(rank_cnt);
         return prize;
     }
+
     public static double EarningRate(int prize, int money) {
-        return (1.0 /((double) money / (double) prize)) * 100;
+        return (1.0 / ((double) money / (double) prize)) * 100;
     }
 }
 
@@ -164,7 +180,8 @@ class WinningResult {
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + rank_cnt[1] + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + rank_cnt[0] + "개");
     }
-    public static void EarningResult(double earningrate){
-        System.out.printf("총 수익률은 %.1f%%입니다.",earningrate);
+
+    public static void EarningResult(double earningrate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", earningrate);
     }
 }

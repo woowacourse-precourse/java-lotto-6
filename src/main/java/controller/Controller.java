cfc34910de571b43e6c5911c1761aa53816ad739
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-08 20:27:07 +0900

    feat(controller, view): 로또 진행 결과 출력

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,14 +1,33 @@
 package controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import domain.Lotto;
+import domain.LottoInfoNumbers;
+import domain.LottoManager;
+import domain.LottoValidation;
+import domain.User;
 import domain.Utils;
 import java.util.List;
 import view.InputView;
+import view.OutputView;
 
 public class Controller {
 
-    public void run() {
+    private final LottoManager lottoManager = new LottoManager();
+    private User user;
+    private LottoValidation lottoValidation;
 
+    public void run() {
+        user = new User(getPurchaseAmount());
+        lottoManager.purchaseLotto(user);
+        printPurchasedResult();
+
+        lottoValidation = new LottoValidation(getWinningNumbers(), getBonusNumber());
+
+        lottoManager.checkUserLotto(user, lottoValidation);
+        System.out.println(OutputView.STATISTICS_HEADER);
+        OutputView.printAllRankResult(user.getLottoResult());
+        System.out.printf(OutputView.LOTTO_RETURNS, user.getLotteryReturns());
     }
 
     private int getPurchaseAmount() {
@@ -27,5 +46,13 @@ public class Controller {
         System.out.println(InputView.INPUT_BONUS_NUMBERS);
         String bonusNumbers = Console.readLine().trim();
         return Utils.stringToInteger(bonusNumbers);
+    }
+
+    private void printPurchasedResult() {
+        int count = user.getPurchaseAmount() / LottoInfoNumbers.LOTTERY_TICKET_PRICE;
+        System.out.printf(OutputView.PURCHASE_COMPLETION, count);
+        for (Lotto lotto : user.getPurchasedLotto()) {
+            OutputView.lottoNumbers(lotto.getNumbers());
+        }
     }
 }

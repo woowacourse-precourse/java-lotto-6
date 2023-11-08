b7824b86b2d97f8bc76ddb6b4cb8b29754032e40
qaknc8 <qaknc8@gmail.com>
2023-11-08 20:19:27 +0900

    feat: 당첨 통계 출력 기능 구현

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -6,6 +6,15 @@ import java.util.stream.Collectors;
 
 public class Output {
     private static final String PRINT_QUANTITY_MESSAGE = "개를 구매했습니다.";
+    private static final String PRINT_RESULT_MESSAGE = "당첨 통계";
+    private static final String PRINT_SEPARATOR = "---";
+    private static final String PRINT_FIRST = "6개 일치 (2,000,000,000원) - ";
+    private static final String PRINT_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    private static final String PRINT_THIRD = "5개 일치 (1,500,000원) - ";
+    private static final String PRINT_FOURTH = "4개 일치 (50,000원) - ";
+    private static final String PRINT_FIFTH = "3개 일치 (5,000원) - ";
+    private static final String PRINT_UNIT = "개";
+
 
     public static void printTicketQuantity(int ticketQuantity) {
         System.out.println(ticketQuantity + PRINT_QUANTITY_MESSAGE);
@@ -31,5 +40,14 @@ public class Output {
         return "[" + numbers.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(",")) + "]";
+    }
+
+    public static void printWinningResult(Winning winning) {
+        System.out.println(PRINT_RESULT_MESSAGE + '\n' + PRINT_SEPARATOR);
+        System.out.println(PRINT_FIFTH + winning.getFifthNumber() + PRINT_UNIT);
+        System.out.println(PRINT_FOURTH + winning.getFourthNumber() + PRINT_UNIT);
+        System.out.println(PRINT_THIRD + winning.getThirdNumber() + PRINT_UNIT);
+        System.out.println(PRINT_SECOND + winning.getSecondNumber() + PRINT_UNIT);
+        System.out.println(PRINT_FIRST + winning.getFirstNumber() + PRINT_UNIT);
     }
 }
--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -8,4 +8,24 @@ public class Winning {
     Winning(Tickets tickets, Lotto lotto, Bonus bonus) {
         result = tickets.drawAllTickets(lotto, bonus);
     }
+
+    int getFirstNumber() {
+        return result.get(Ranking.FIRST);
+    }
+
+    int getSecondNumber() {
+        return result.get(Ranking.SECOND);
+    }
+
+    int getThirdNumber() {
+        return result.get(Ranking.THIRD);
+    }
+
+    int getFourthNumber() {
+        return result.get(Ranking.FOURTH);
+    }
+
+    int getFifthNumber() {
+        return result.get(Ranking.FIFTH);
+    }
 }

40bb48e40bb455b669ba158c087b9346701563f3
BangMingGing <privatebmk802@naver.com>
2023-11-08 20:52:46 +0900

    fix(View): Add some \n in print functions

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,7 +7,7 @@ import lotto.model.WinningStatics;
 public class OutputView {
     private static final String PURCHASE_LOTTOS = "\n%d개를 구매했습니다.\n";
     private static final String WINNING_STATICS_PREFIX = "\n당첨 통계\n---\n";
-    private static final String RATE_OF_RETURN = "총 수익률은 %.1f%입니다.\n";
+    private static final String RATE_OF_RETURN = "총 수익률은 %.1f%%입니다.\n";
 
     public void printPurchaseLottos(final List<Lotto> lottos) {
         System.out.printf(PURCHASE_LOTTOS, lottos.size());
@@ -22,7 +22,7 @@ public class OutputView {
 
     public void printWinningStatics(WinningStatics winningStatics) {
         System.out.print(WINNING_STATICS_PREFIX);
-        System.out.println(winningStatics);
+        System.out.print(winningStatics);
     }
 
     public void printRateOfReturn(float rateOfReturn) {

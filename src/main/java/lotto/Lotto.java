f83e7141cbd1451e34ea125dd0644ed2cea9592f
jang-namu <ironhee8005@gmail.com>
2023-11-08 16:35:26 +0900

    feat: auto 로또 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +19,8 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> auto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }

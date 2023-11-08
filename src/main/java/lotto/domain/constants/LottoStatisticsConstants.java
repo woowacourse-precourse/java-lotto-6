5d168c19161b86c596a3850eabefaf3c2956ba59
oxdjww <oxdjww@gmail.com>
2023-11-08 22:22:30 +0900

    fix : getter에 this 추가해서 null 반환되던 버그 수정

--- src/main/java/lotto/domain/constants/LottoStatisticsConstants.java
+++ src/main/java/lotto/domain/constants/LottoStatisticsConstants.java
@@ -5,16 +5,14 @@ public enum LottoStatisticsConstants {
     INLINE("\n"),
     STATISTICS_NOTICE("당첨 통계");
 
-    private String value;
+    private final String value;
 
-    LottoStatisticsConstants(final String s) {
+    LottoStatisticsConstants(final String value) {
+        this.value = value;
     }
 
-    public static String getStatisticsPrefix() {
-        return STATISTICS_NOTICE.getValue() + INLINE.getValue() + DOTTED_LINE.getValue() + INLINE.getValue();
-    }
 
     public String getValue() {
-        return value;
+        return this.value;
     }
 }

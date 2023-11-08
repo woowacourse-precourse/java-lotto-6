0bcb52bee91ae23a476feacfdc6312c1c639321e
k-kmw <kimmw2_@naver.com>
2023-11-08 10:48:40 +0900

    refactor: controller 유효성 검사 메소드 분리

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -20,15 +20,10 @@ public class LottoController {
 
     public void start() {
 
-        lottoView.displayPurchaseAmountInputMessage();
         int purchaseAmount = getPurchaseAmount();
         List<Lotto> purchasedLottos = lottoService.generateLottos(purchaseAmount);
         lottoView.displayPurchasedLottos(purchasedLottos);
-
-        lottoView.displayWinnerNumsInputMessage();
         List<Integer> winnerNums = getWinnerNums();
-
-        lottoView.displayBonusNumInputMessage();
         int bonusNum = getBonusNum();
 
         List<Integer> results = lottoService.getResults(purchasedLottos, winnerNums, bonusNum);
@@ -39,10 +34,9 @@ public class LottoController {
     private int getPurchaseAmount() {
         while(true) {
             try {
+                lottoView.displayPurchaseAmountInputMessage();
                 int purchaseAmount = Integer.parseInt(Console.readLine());
-                if (purchaseAmount <= 0) {
-                    throw new IllegalArgumentException("구매 금액은 0보다 큰 값을 입력해야 합니다.");
-                }
+                validatePurchaseAmount(purchaseAmount);
                 return purchaseAmount;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] = " + e);
@@ -50,14 +44,19 @@ public class LottoController {
         }
     }
 
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount <= 0) {
+            throw new IllegalArgumentException("구매 금액은 0보다 큰 값을 입력해야 합니다.");
+        }
+    }
+
     private int getBonusNum() {
         while (true) {
             try {
+                lottoView.displayBonusNumInputMessage();
                 String inputBonusNum = Console.readLine();
                 int bonusNum =  Integer.parseInt(inputBonusNum);
-                if (bonusNum < 1 || bonusNum > 45) {
-                    throw new IllegalArgumentException("로또 번호의 숫자 범위는 1~45 입니다.");
-                }
+                validateNumRange(bonusNum);
                 return bonusNum;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] = " + e);
@@ -65,23 +64,32 @@ public class LottoController {
         }
     }
 
+    private void validateNumRange(int bonusNum) {
+        if (bonusNum < 1 || bonusNum > 45) {
+            throw new IllegalArgumentException("로또 번호의 숫자 범위는 1~45 입니다.");
+        }
+    }
+
     private List<Integer> getWinnerNums() {
         while (true) {
             try {
+                lottoView.displayWinnerNumsInputMessage();
                 String inputWinnerNums = Console.readLine();
                 List<Integer> winnerNums = Arrays.stream(inputWinnerNums.split(",")).map(Integer::parseInt).toList();
-                if (winnerNums.stream().distinct().toList().size() != 6) {
-                    throw new IllegalArgumentException("당첨 번호는 쉼표로 구분하여 6자리를 입력해야 합니다.");
-                }
-                for (Integer winnerNum : winnerNums) {
-                    if (winnerNum < 1 || winnerNum > 45) {
-                        throw new IllegalArgumentException("로또 번호의 숫자 범위는 1~45 입니다.");
-                    }
-                }
+                validateWinnerNums(winnerNums);
                 return winnerNums;
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] = " + e);
             }
+        }
+    }
+
+    private void validateWinnerNums(List<Integer> winnerNums) {
+        if (winnerNums.stream().distinct().toList().size() != 6) {
+            throw new IllegalArgumentException("당첨 번호는 쉼표로 구분하여 6자리를 입력해야 합니다.");
+        }
+        for (Integer winnerNum : winnerNums) {
+            validateNumRange(winnerNum);
         }
     }
 }

07f5964284c838ec47e8c372433c1dff3ee28ec9
yush <yush2202@gmail.com>
2023-11-08 19:17:33 +0900

    fix + test : 상금 관련 버그 수정 및 테스트 추가 3등 상금이 잘못된 금액으로 들어가던 버그를 수정하고 어플리케이션 테스트를 추가했습니다.

--- src/test/java/lotto/Model/GameRuleTest.java
+++ src/test/java/lotto/Model/GameRuleTest.java
@@ -12,11 +12,31 @@ public class GameRuleTest {
 
     @DisplayName("게임의 수익률을 반환하는 테스트 입니다.")
     @Test
-    void checkProfit() {
+    void checkProfit1() {
         List<Integer> list1 = new ArrayList<>(List.of(1, 0, 0, 0, 0));
 
         String result = gameRule.calculateProfit(8000, list1);
 
         assertThat(result).isEqualTo("62.5");
+    }
+
+    @DisplayName("게임의 수익률을 반환하는 테스트 입니다.")
+    @Test
+    void checkProfit2() {
+        List<Integer> list1 = new ArrayList<>(List.of(0, 2, 0, 0, 0));
+
+        String result = gameRule.calculateProfit(8000, list1);
+
+        assertThat(result).isEqualTo("1250.0");
+    }
+
+    @DisplayName("게임의 수익률을 반환하는 테스트 입니다.")
+    @Test
+    void checkProfit3() {
+        List<Integer> list1 = new ArrayList<>(List.of(0, 2, 0, 1, 0));
+
+        String result = gameRule.calculateProfit(8000, list1);
+
+        assertThat(result).isEqualTo("376250.0");
     }
 }

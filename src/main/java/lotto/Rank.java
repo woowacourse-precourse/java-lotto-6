8928500670dc7863a07589ea86c834b6a7cf393f
Seo Young, Ha <59078585+82Hasheng@users.noreply.github.com>
2023-11-08 19:28:42 +0900

    refactor: Class Rank 함수 분리

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,34 +3,52 @@ package lotto;
 import java.util.List;
 
 public class Rank {
+
     public int[] rankAndWinning(int lottoTickets, List[] allTickets, int bonus, List<Integer> numbers) {
         //당첨여부
         int[] rank = new int[6];
-        int includedBonus = 0;
+        boolean includedBonus = false;
         for (int i = 0; i < lottoTickets; i++) {
             if (allTickets[i].contains(bonus)) {
-                includedBonus = 1;
+                includedBonus = true;
             }
             //자동 숫자에 당첨 숫자가 있는가
-            int includedWinning = 0;
-            for (int j = 0; j < numbers.size(); j++) {
-                if (allTickets[i].contains(numbers.get(j))) {
-                    includedWinning += 1;
-                }
-            }
-            if (includedWinning == 3) {
-                rank[5] += 1;
-            } else if (includedWinning == 4) {
-                rank[4] += 1;
-            } else if (includedWinning == 5 && includedBonus == 1) {
-                rank[2] += 1;
-            } else if (includedWinning == 5) {
-                rank[3] += 1;
-            } else if (includedWinning == 6) {
-                rank[1] += 1;
+            int matchingNumCount = 0;
+            matchingNumCount = autoNumHasWinningNum(numbers, allTickets, i);
+            rank = totalRankCount(matchingNumCount, includedBonus);
+        }
+        statistics(rank);
+        return rank;
+    }
+
+    public int autoNumHasWinningNum(List<Integer> numbers, List[] allTickets, int i) {
+        int count = 0;
+        for (int j = 0; j < numbers.size(); j++) {
+            if (allTickets[i].contains(numbers.get(j))) {
+                count += 1;
             }
         }
+        return count;
+    }
 
+    public int[] totalRankCount(int matchingNumCount, boolean includedBonus) {
+        int[] rank = new int[6];
+
+        if (matchingNumCount == 3) {
+            rank[5] += 1;
+        } else if (matchingNumCount == 4) {
+            rank[4] += 1;
+        } else if (matchingNumCount == 5 && includedBonus) {
+            rank[2] += 1;
+        } else if (matchingNumCount == 5) {
+            rank[3] += 1;
+        } else if (matchingNumCount == 6) {
+            rank[1] += 1;
+        }
+        return rank;
+    }
+
+    public void statistics(int[] rank) {
         System.out.println("\n당첨 통계");
         System.out.println("---");
         System.out.printf("""
@@ -40,6 +58,5 @@ public class Rank {
                         5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
                         6개 일치 (2,000,000,000원) - %d개%n""",
                 rank[5], rank[4], rank[3], rank[2], rank[1]);
-        return rank;
     }
 }

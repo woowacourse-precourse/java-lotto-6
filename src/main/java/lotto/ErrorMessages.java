6240f7dcb57cc7827ca2982dbda30f0ac34961d3
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-11-08 11:55:12 +0900

    feat: 당첨 번호의 범위가 1부터 45 사이의 수가 아닌 경우 예외 처리

--- src/main/java/lotto/ErrorMessages.java
+++ src/main/java/lotto/ErrorMessages.java
@@ -2,4 +2,5 @@ package lotto;
 
 public class ErrorMessages {
     public static final String INVALID_LOTTO_SIZE = "[ERROR] 로또 번호는 6개를 입력해야 합니다.";
+    public static final String INVALID_LOTTO_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.";
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,14 @@
 package lotto;
-import static lotto.ErrorMessages.INVALID_LOTTO_SIZE;
+import static lotto.ErrorMessages.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 45;
     private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,12 +18,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         checkLottoSize(numbers);
-
+        checkLottoNumberRange(numbers);
     }
 
     private static void checkLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(INVALID_LOTTO_SIZE);
+        }
+    }
+
+    private void checkLottoNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < START_RANGE || number > END_RANGE)) {
+            throw new IllegalArgumentException(INVALID_LOTTO_RANGE);
         }
     }
 

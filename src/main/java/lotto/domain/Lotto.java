c3cfd46cdb8a0c1e8bb85e09c6b8dc8bf7976893
Suyoung Oh <ohsusu515@gmail.com>
2023-11-08 17:35:25 +0900

    feat: Lotto 클래스에 validateDuplicate 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,31 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> unique = new HashSet<>();
+        numbers.stream()
+                .filter(number -> !unique.add(number))
+                .findAny()
+                .ifPresent(duplicate -> {
+                    throw new IllegalArgumentException();
+                });
+    }
 }

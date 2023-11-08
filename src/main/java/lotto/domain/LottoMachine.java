60b1288ddf5bfc9c4bb132a6fd268100735daa7a
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 23:06:21 +0900

    Fix :List sort 시 오류 수정
    로또 번호 얻어오는 메소드로 만든 List를 Collections.sort() 메소드에
    넣으니 UnsupportedOperationException 발생해서 List 초기화 후
    메소드로 만든 List 넣어서 정렬하도록 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.model.Lotto;
@@ -8,10 +9,12 @@ import lotto.model.Lotto;
 public class LottoMachine {
 
     public Lotto makeLotto() {
+        List<Integer> lottoNumbers = new ArrayList<>();
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(numbers);
-        
-        Lotto lotto = new Lotto(numbers);
+        lottoNumbers.addAll(numbers);
+        Collections.sort(lottoNumbers);
+
+        Lotto lotto = new Lotto(lottoNumbers);
         return lotto;
     }
 }

2bdd7376d6dd79f85bdd77dad9fa577ab4b4009f
2021110806 <2021110806@dgu.ac.kr>
2023-11-08 16:08:47 +0900

    feat(OutputView.java): add print PurchasingResult, BonusNumberRequest Message 로또 구매 결과를 출력하는 기능, 보너스 숫자 입력 요청을 출력하는 기능을 구현하였습니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     static private int earningRate;
     static final private String lineSeparator = System.lineSeparator();
     static final String MONEY_INPUT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
+    static final String BONUS_NUMBER_INPUT_REQUEST_MESSAGE =   "보너스 번호를 입력해 주세요.";
     static final String PURCHASING_RESULT_MESSAGE = "개를 구매했습니다.";
     static final String WINNING_NUMBER_INPUT_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
     static final String WINNING_RESULT_TITLE = "당첨 통계" + lineSeparator + "---" + lineSeparator;
@@ -14,7 +15,18 @@ public class OutputView {
         System.out.println(MONEY_INPUT_REQUEST_MESSAGE);
     }
 
-    void printPurchasingResultMessage() {
+    public static void printPurchasingResultMessage(int purchasingNumber, ArrayList<Lotto> lottos) {
+        System.out.println(purchasingNumber + PURCHASING_RESULT_MESSAGE);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+    public static void printBonusNumberInputRequestMessage() {
+        System.out.println(BONUS_NUMBER_INPUT_REQUEST_MESSAGE);
+    }
+
+    public static void printPurchasingResultMessage() {
         System.out.println(PURCHASING_RESULT_MESSAGE);
     }
 

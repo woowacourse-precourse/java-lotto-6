e19beccf64a294d96bf1c4272496774e12a6abdb
hwangkyojune <shane5969@naver.com>
2023-11-08 17:43:31 +0900

    안 쓰이는 static변수 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,9 +5,6 @@ import java.util.List;
 import static lotto.ValidateService.*;
 
 public class Lotto {
-    static int MIN_NUMBER = 1;
-    static int MAX_NUMBER = 45;
-    static int NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {

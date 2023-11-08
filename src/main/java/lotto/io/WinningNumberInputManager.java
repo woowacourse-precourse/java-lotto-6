8d0ce29d026cc4170e5ea726b257cddf78339382
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:16:53 +0900

    [FIX] 의존성 약화 및 예외처리 수정

--- src/main/java/lotto/io/WinningNumberInputManager.java
+++ src/main/java/lotto/io/WinningNumberInputManager.java
@@ -1,13 +1,12 @@
 package lotto.io;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.*;
-import java.util.regex.PatternSyntaxException;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
 
 public class WinningNumberInputManager extends InputManager<List<Integer>> {
     private static WinningNumberInputManager INSTANCE;
+
+    private List<Integer> result;
 
     private WinningNumberInputManager() {
     }
@@ -21,22 +20,26 @@ public class WinningNumberInputManager extends InputManager<List<Integer>> {
     public List<Integer> input() {
         while (true) {
             try {
+                System.out.println("당첨 번호를 입력해 주세요.");
                 String inputLine = consoleAdapter.readLine();
-                return validation(inputLine);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
-            } catch (IndexOutOfBoundsException e) {
-                throw new IllegalArgumentException("[ERROR] 1~45 범위 내의 값을 입력해주세요.");
-            } catch (NullPointerException e) {
-                throw new IllegalArgumentException("[ERROR] 입력이 존재하지 않습니다.");
+                result = validation(inputLine);
+                return result;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
 
     @Override
-    public List<Integer> validation(String input) throws NullPointerException, IndexOutOfBoundsException, IllegalArgumentException {
-        List<Integer> numbers = Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
-        numbers.forEach(InputManager::validateNumberRange);
-        return numbers;
+    public List<Integer> validation(String input)throws IllegalArgumentException{
+        try {
+            List<Integer> numbers = Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
+            numbers.forEach(InputManager::validateNumberRange);
+            return numbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("[ERROR] 입력이 존재하지 않습니다.");
+        }
     }
 }

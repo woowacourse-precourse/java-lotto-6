b327baba84f273eec4f8005abd6727e8b3849b74
gywns0417 <gywns0417@gmail.com>
2023-11-08 17:44:24 +0900

    test: [사용자에게서 입력 받은 로또 당첨 번호 분리] 기능 테스트 수정 및 통과

--- src/test/java/lotto/util/NumberSplitterTest.java
+++ src/test/java/lotto/util/NumberSplitterTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class NumberSplitterTest {
-    @DisplayName("번호 분리 성공시 분리된 번호가 담긴 리스트 반환")
+    @DisplayName("번호 분리 성공시 분리된 번호가 담긴 Integer 타입 리스트 반환")
     @Test
     void success() {
         // give
         String input = "1,2,3,5,10,24";
         // when
-        List<String> numberSplit = NumberSplitter.splitNumbers(input);
+        List<Integer> numberSplit = NumberSplitter.splitNumbers(input);
         //then
-        assertThat(numberSplit).contains("1", "2", "3", "5", "10", "24");
+        assertThat(numberSplit).contains(1,2,3,5,10,24);
     }
 }

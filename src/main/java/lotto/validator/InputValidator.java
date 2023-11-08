abbaa62bb90b25fd042a28155876b19fee9bac4e
Sully <2dcoder@naver.com>
2023-11-08 14:12:58 +0900

    feat(InputValidator): 입력 관련 validator 클래스 구현

---
+++ src/main/java/lotto/validator/InputValidator.java
@@ -0,0 +1,33 @@
+package lotto.validator;
+
+public class InputValidator {
+
+    private InputValidator() {
+        throw new AssertionError("인스턴스화 불가능");
+    }
+
+    public static void validateMoney(String money) throws IllegalArgumentException {
+        if (isNotDigit(money)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+
+        if (isOverMax(money)) {
+            throw new IllegalArgumentException("입력할 수 있는 금액의 최댓값 그 이상입니다.");
+        }
+    }
+
+    public static void validateBonusNumber(String number) {
+        if (isNotDigit(number)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private static boolean isNotDigit(String number) {
+        return !number.matches("^[0-9]+$");
+    }
+
+    private static boolean isOverMax(String money) {
+        return Long.parseLong(money) > (Long.MAX_VALUE / 1000L) * 1000L;
+    }
+
+}

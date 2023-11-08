1f8c247184b974d775ca7cc2b9041debd6ebfa39
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 15:52:26 +0900

    [FEAT] WinningNumber에서 validateNumberInRange 함수 구현
    보너스 번호의 범위 확인을 위해 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -5,12 +5,16 @@ import java.util.List;
 import java.util.Set;
 
 public class WinningNumber {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+
     private final Lotto winningNumber;
     private final int bonusNumber;
 
     public WinningNumber(List<Integer> winningNumber, int bonusNumber) {
         this.winningNumber = new Lotto(winningNumber);
         validateDuplicate(winningNumber, bonusNumber);
+        validateNumberInRange(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
@@ -22,5 +26,12 @@ public class WinningNumber {
                 throw new IllegalArgumentException("당첨번호와 보너스 번호가 중복됩니다.");
             }
         }
+    }
+
+    private int validateNumberInRange(int number) {
+        if (number < MIN_RANGE || number > MAX_RANGE) {
+            throw new IllegalArgumentException("숫자는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return number;
     }
 }

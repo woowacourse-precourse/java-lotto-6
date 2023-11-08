df9dbf66b1dc338b9ac804fbfe8c87cff389d346
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 17:26:15 +0900

    로또 티켓 구매 기능 검증 테스트 구현
    LottoMachine 클래스의 purchaseLottos 메소드가 요청된 수의 로또 티켓을 정확히 구매하는지 확인하는 테스트를 추가하였습니다.
    이 테스트는 모의 LottoGenerator 객체를 사용하여 로또 티켓 생성을 제어하고, 반환된 로또 티켓이 예상과 일치하는지 검증합니다.

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class LottoMachineTest {
     private LottoGenerator generator;
@@ -17,5 +21,25 @@ public class LottoMachineTest {
         // LottoGenerator를 목 객체로 생성
         generator = Mockito.mock(LottoGenerator.class);
         machine = new LottoMachine(generator);
+    }
+
+    @Test
+    @DisplayName("정확한 수의 로또 티켓을 구매해야 한다")
+    public void testPurchaseLottos() {
+        // 기대하는 로또 번호 설정
+        Lotto expectedLotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto expectedLotto2 = new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12));
+        when(generator.generate()).thenReturn(expectedLotto1, expectedLotto2);
+
+        // 로또 구매
+        machine.purchaseLottos(2);
+
+        // 구매된 로또 수 확인
+        List<Lotto> purchasedLottos = machine.getPurchasedLottos();
+        assertEquals(2, purchasedLottos.size(), "2장의 로또를 구매해야 한다");
+
+        // 구매된 로또가 기대한 로또와 일치하는지 확인
+        assertEquals(expectedLotto1, purchasedLottos.get(0), "첫 번째 구매한 로또가 기대한 로또와 일치해야 한다");
+        assertEquals(expectedLotto2, purchasedLottos.get(1), "두 번째 구매한 로또가 기대한 로또와 일치해야 한다");
     }
 }

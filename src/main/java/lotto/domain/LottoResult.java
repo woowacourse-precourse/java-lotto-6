42068f7535e62a28bf022264bfac552102987dd5
bokyung <bbookng@gmail.com>
2023-11-08 16:03:15 +0900

    feat(LottoMaker) : match field 추가함으로써 LottoResult 생성 method 추가 - 출력 시 순서 보장을 위해 enum 순서 변경

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,19 +1,30 @@
 package lotto.domain;
 
-public enum LottoResult {
-    FIRST(2000000000, "6개 일치"),
-    SECOND(30000000, "5개 일치, 보너스 볼 일치"),
-    THIRD(1500000, "5개 일치"),
-    FOURTH(50000, "4개 일치"),
-    FIFTH(5000, "3개 일치"),
-    NONE(0, "0개 일치");
+import java.util.Arrays;
 
+public enum LottoResult {
+    NONE(0, 0, "0개 일치"),
+    FIFTH(3, 5000, "3개 일치"),
+    FOURTH(4, 50000, "4개 일치"),
+    THIRD(5,  1500000, "5개 일치"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, 2000000000, "6개 일치");
+
+    private final int match;
     private final long moneyAmount;
     private final String description;
 
-    LottoResult(long moneyAmount, String description) {
+    LottoResult(int match, long moneyAmount, String description) {
+        this.match = match;
         this.moneyAmount = moneyAmount;
         this.description = description;
+    }
+
+    public static LottoResult of(int match) {
+        return Arrays.stream(values())
+                .filter(v -> match == v.match)
+                .findFirst()
+                .orElse(NONE);
     }
 
     public long getMoneyAmount() {

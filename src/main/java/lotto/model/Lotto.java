b4f88e642ac2ff9cbc8c91d66e7437a9b862221c
ParkKyuIl <hakim@nex.market>
2023-11-08 13:52:37 +0900

    feat: 당첨번호와 비교문 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.List;
+import lotto.service.lottoService;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -19,6 +20,28 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return this.numbers;
     }
+
+    public Rank compare(WinningLotto winningNumber){
+        int correctCount = compareWinningNumber(winningNumber);
+        boolean bonusResult = compareBonusNumber(winningNumber);
+
+        return Rank.match(correctCount,bonusResult);
+    }
+
+    public int compareWinningNumber(WinningLotto winningNumber){
+        int count = 0;
+        for(int i=0;i<winningNumber.getNumbers().size();i++){
+            if(numbers.contains(winningNumber.getNumbers().get(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean compareBonusNumber(WinningLotto winningNumber){
+        return numbers.contains(winningNumber.getBonusNumber());
+    }
+
 
 
 
--- src/main/java/lotto/service/lottoService.java
+++ src/main/java/lotto/service/lottoService.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.Rank;
+import lotto.model.WinningLotto;
 
 public class lottoService {
 
@@ -13,10 +14,10 @@ public class lottoService {
     private static final int UPPER_BOUND = 45;
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static List<Lotto> lottos;
-    private LinkedHashMap<Rank, Integer> stats;
+    private static LinkedHashMap<Rank, Integer> hashMap;
      public lottoService(){
         lottos = new ArrayList<>();
-        stats = new LinkedHashMap<>();
+         hashMap = new LinkedHashMap<>();
     }
 
     public static List<Lotto> publishLottos(int ticketCount){
@@ -28,6 +29,21 @@ public class lottoService {
 
     public static List<Lotto> getLottos(){ return lottos; }
 
+    public static void compareLottos(WinningLotto winningNumber){
+        initStats();
+         for(int i=0;i<lottos.size();i++){
+             Rank result = lottos.get(i).compare(winningNumber);
+             hashMap.put(result, hashMap.get(result) + 1);
+         }
+         System.out.println(hashMap);
+    }
+
+    private static void initStats() {
+        for (Rank rank : Rank.values()) {
+            hashMap.put(rank, 0);
+        }
+    }
+
     private static void createLottos(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 LOWER_BOUND,
@@ -35,4 +51,5 @@ public class lottoService {
                 LOTTO_NUMBER_COUNT);
         lottos.add(new Lotto(numbers));
     }
+
 }

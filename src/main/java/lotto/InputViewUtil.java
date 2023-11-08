357ce136dbc1cc26780eab4b5622a493e701ac7f
leorb159 <ldgsocial159@naver.com>
2023-11-08 22:40:48 +0900

    feat: 로또 당첨 번호 검증 메소드 작성

--- src/main/java/lotto/InputViewUtil.java
+++ src/main/java/lotto/InputViewUtil.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputViewUtil {
     public static int getValidAmount(String input) throws IllegalArgumentException {
         int amount;
@@ -13,5 +16,28 @@ public class InputViewUtil {
             throw new IllegalArgumentException(ErrorMessage.NOT_DIVISIBLE_LOTTO_PRICE.getMessage());
         }
         return amount;
+    }
+
+    public static List<Integer> getValidLottoNumbers(String input) throws IllegalArgumentException {
+        String[] inputSplit = input.split(",");
+        List<Integer> lottoNumbers = new ArrayList<>();
+        if (inputSplit.length != 6) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBERS.getMessage());
+        }
+
+        for (String str : inputSplit) {
+            try {
+                int number = Integer.parseInt(str);
+
+                if (number < LottoConst.MIN_RANGE_NUMBER.getValue() || number > LottoConst.MAX_RANGE_NUMBER.getValue() || lottoNumbers.contains(number)) {
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBERS.getMessage());
+                }
+
+                lottoNumbers.add(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBERS.getMessage());
+            }
+        }
+        return lottoNumbers;
     }
 }

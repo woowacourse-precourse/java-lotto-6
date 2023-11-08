97421632d551c3a0712d55e07e3f6f15e8c75ccd
gywns0417 <gywns0417@gmail.com>
2023-11-08 17:40:34 +0900

    refactor: [ERROR] 이 포함되지 않은 enum 메시지 수정

--- src/main/java/lotto/config/LottoErrorMessage.java
+++ src/main/java/lotto/config/LottoErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.config;
 import static lotto.config.LottoConfig.LOTTO_NUMBER_AMOUNT_MAX;
 
 public enum LottoErrorMessage {
-    LOTTO_AMOUNT_MAX_ERROR_MESSAGE(String.format("로또 번호는 %s개를 입력해야 합니다.",
+    LOTTO_AMOUNT_MAX_ERROR_MESSAGE(String.format("[ERROR] 로또 번호는 %s개를 입력해야 합니다.",
             LOTTO_NUMBER_AMOUNT_MAX.getValue()));
 
     private final String message;

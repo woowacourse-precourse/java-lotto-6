bbc4d88f72b27c9ac2022b526de26b03d5bf5cba
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:09:50 +0900

    feat : inputView 상수 선언

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+
 }

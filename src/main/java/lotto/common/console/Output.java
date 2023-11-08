9778f77f8bc160794a714fa1be796374cc9107c3
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-08 19:19:14 +0900

    feat: write lotto count

--- src/main/java/lotto/common/console/Output.java
+++ src/main/java/lotto/common/console/Output.java
@@ -1,7 +1,13 @@
 package lotto.common.console;
 
 public class Output {
+    public static final String WRITE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+
     public static void writeMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void writeLottoCount(int count) {
+        System.out.println(String.format(WRITE_LOTTO_COUNT_FORMAT, count));
     }
 }

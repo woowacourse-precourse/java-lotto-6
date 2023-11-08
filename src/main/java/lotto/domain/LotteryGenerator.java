f62baffb9a5cf9df01fe2b40de53ce2b3462962e
706com <ehdgusjdbswl@gmail.com>
2023-11-08 19:47:24 +0900

    refactor : 검증 여부를 Lotto에게 위임

--- src/main/java/lotto/domain/LotteryGenerator.java
+++ src/main/java/lotto/domain/LotteryGenerator.java
@@ -1,13 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.service.WinningNumberService;
-import lotto.service.exception.IllegalArgumentExceptionHandler;
-import lotto.service.exception.InputErrorMessage;
 
 public class LotteryGenerator {
     private static final int START_NUMBER = 1;
@@ -16,29 +10,15 @@ public class LotteryGenerator {
 
     private Lotto lotto;
 
-    private void generateLotto() {
-        List<Integer> numbers;
-        do {
-            numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER,NUMBER_SIZE);
-
-        } while (!verifyDuplication(numbers));
-        this.lotto = new Lotto(numbers);
-    }
-
-    private boolean verifyDuplication(List<Integer> winningNumber){
-        Set<Integer> checkDuplication = new HashSet<>();
-        for(int number : winningNumber){
-            if(checkDuplication.contains(number)){
-                throw new IllegalArgumentExceptionHandler(InputErrorMessage.DUPLICATE_NUMBER);
-            }
-            checkDuplication.add(number);
-        }
-        return true;
-    }
-
     public Lotto getLotto() {
         generateLotto();
         return lotto;
     }
 
+    private void generateLotto() {
+        List<Integer> numbers;
+        numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER,NUMBER_SIZE);
+
+        this.lotto = new Lotto(numbers);
+    }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
-import lotto.service.WinningNumberService;
+import java.util.Set;
+import lotto.service.exception.IllegalArgumentExceptionHandler;
+import lotto.service.exception.InputErrorMessage;
 
 public class Lotto {
 
@@ -9,16 +12,25 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        verifyDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        WinningNumberService service = new WinningNumberService();
-        service.verifyDuplication(numbers);
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
+    private void verifyDuplication(List<Integer> winningNumber){
+        Set<Integer> checkDuplication = new HashSet<>();
+        for(int number : winningNumber){
+            if(checkDuplication.contains(number)){
+                throw new IllegalArgumentExceptionHandler(InputErrorMessage.DUPLICATE_NUMBER);
+            }
+            checkDuplication.add(number);
+        }
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }

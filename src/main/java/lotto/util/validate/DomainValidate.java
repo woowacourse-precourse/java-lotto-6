12c0ed7da26c3e9571294bdac21e21fafd545eaf
Hayoon <hy97@sju.ac.kr>
2023-11-08 20:05:58 +0900

    feat(DomainValidate): 랭킹 인덱스 6 상수처리

--- src/main/java/lotto/util/validate/DomainValidate.java
+++ src/main/java/lotto/util/validate/DomainValidate.java
@@ -1,11 +1,12 @@
 package lotto.util.validate;
 
+import static lotto.util.constant.Constant.LOTTO_NUMBER_SIZE;
 import static lotto.util.constant.InvalidConstant.*;
 
 public class DomainValidate {
 
     public static void validateOutOfRankingIndex(int rank) {
-        if (rank >= 6) {
+        if (rank >= LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(ERROR_MESSAGE + OUT_OF_RANKING_INDEX);
         }
     }

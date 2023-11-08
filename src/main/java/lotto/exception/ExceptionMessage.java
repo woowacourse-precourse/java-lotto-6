e03c40c1532c785fc3117f1dfb1e7849d2d52dd9
dustjq1004 <dustjq1005@naver.com>
2023-11-08 17:45:20 +0900

    yeonsup/style : 에러 메시지 수정 1. 0 ~ 45 -> 1 ~ 45

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -8,7 +8,7 @@ public class ExceptionMessage {
     public static final String MESSAGE_COMMA_NUMERIC = PREFIX + SPACE + "숫자와 숫자 구분으로 쉼표만 입력 가능합니다.";
     public static final String MESSAGE_THOUSAND_UNIT = PREFIX + SPACE + "1000원 단위만 입력 가능합니다.";
     public static final String MESSAGE_LIMIT_MONEY = PREFIX + SPACE + "1,000,000 만원 까지 입력 가능합니다.";
-    public static final String MESSAGE_RANGE_NUMBER = PREFIX + SPACE + "0 ~ 45 까지 입력 가능합니다.";
+    public static final String MESSAGE_RANGE_NUMBER = PREFIX + SPACE + "1 ~ 45 까지 입력 가능합니다.";
     public static final String MESSAGE_LIMIT_SIZE = PREFIX + SPACE + "로또 번호는 6개까지 입력 가능합니다.";
     public static final String MESSAGE_DUPLICATE_NUMBER = PREFIX + SPACE + "로또 번호는 6개까지 입력 가능합니다.";
 

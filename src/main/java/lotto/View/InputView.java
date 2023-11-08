aee0f8b1293be6af677765868c36dd57d355b21e
masonkimseoul <masonkimseoul@gmail.com>
2023-11-08 19:09:11 +0900

    fix : 오류 메세지 출력하도록 수정
    요구사항 충족을 위해 에러 문구를 출력하도록 수정

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,23 +1,48 @@
 package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Util.Validation;
 
 public class InputView {
 
     public static int getAvailableMoney() {
-        String input = Console.readLine();
-        return Validation.validateAvailableMoney(input);
+        int availableMoney = 0;
+
+        try {
+            String input = Console.readLine();
+            availableMoney = Validation.validateAvailableMoney(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return availableMoney;
     }
 
     public static List<Integer> getLottoNumbers() {
-        String input = Console.readLine();
-        return Validation.validateLottoNumbers(input);
+        List<Integer> numbers = new ArrayList<>();
+
+        try {
+            String input = Console.readLine();
+            numbers = Validation.validateLottoNumbers(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return numbers;
     }
 
     public static int getBonusNumber(List<Integer> lottoNumbers) {
-        String input = Console.readLine();
-        return Validation.validateBonusNumber(lottoNumbers, input);
+        int bonusNumber = 0;
+
+        try {
+            String input = Console.readLine();
+            bonusNumber = Validation.validateBonusNumber(lottoNumbers, input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return bonusNumber;
     }
 }

7cd12d555f859966dca4b0311ee86c246e2c58a6
jiwon1217 <koagjw@gmail.com>
2023-11-08 15:31:56 +0900

    :recycle: refactor: 메서드 이름, 변수 이름 변경

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -4,27 +4,25 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final Pattern INPUT_PATTERN = Pattern.compile("^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$");
-    private static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
+    private static final String INVALID_INPUT_TYPE = "[ERROR] 입력은 숫자여야 합니다.";
     private static final String MONEY_MUST_NOT_NEGATIVE = "[ERROR] 금액은 음수일 수 없습니다.";
     private static final String INVALID_INPUT_PATTERN = "[ERROR] 입력 형식과 맞지 않습니다.";
 
     public static void validateMoney(String money) {
-        validateMoneyType(money);
+        validateInputType(money);
         validateMoneyRange(money);
     }
 
-    public static void validateMoneyType(String money) {
+    public static void validateInputType(String input) {
         try {
-            Integer.parseInt(money);
+            Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_MONEY_TYPE);
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE);
         }
     }
 
     public static void validateMoneyRange(String money) {
-        int amount = Integer.parseInt(money);
-
-        if (amount < 0) {
+        if (Integer.parseInt(money) < 0) {
             throw new IllegalArgumentException(MONEY_MUST_NOT_NEGATIVE);
         }
     }

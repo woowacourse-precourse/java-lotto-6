8797fd637ece1ef25e7975bc72f93199dc410a6e
gywns0417 <gywns0417@gmail.com>
2023-11-08 17:20:45 +0900

    fix: UnsupportedOperationException 에러 해결
    ApplicationTest 클래스에서 List.of() 로 생성한 값들이 불변이라 로또 번호를 오름차순 정렬 할 때 UnsupportedOperationException 에러가 생기는 문제를 받은 List들을 다시 ArrayList() 로 생성하게 해 문제를 해결했습니다.

--- src/main/java/lotto/util/NumberSplitter.java
+++ src/main/java/lotto/util/NumberSplitter.java
@@ -1,14 +1,21 @@
 package lotto.util;
 
+import static lotto.util.AscendingSorter.sortAscending;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class NumberSplitter {
     private static final String LOTTO_NUMBERS_SPLIT_DELIM = ",";
-    public static List<String> splitNumbers(String input) {
+    public static List<Integer> splitNumbers(String input) {
         String[] numberSplit = input.split(LOTTO_NUMBERS_SPLIT_DELIM);
+        List<Integer> numberList = new ArrayList<>();
 
-        return new ArrayList<>(Arrays.asList(numberSplit));
+        for (String number : numberSplit) {
+            numberList.add(IntParser.parseInt(number));
+        }
+        return numberList;
     }
 }

b90ba0b9a969f94504f57df89b5787eb3f5fb6c3
HongSP99 <tmdvy0805@naver.com>
2023-11-08 19:53:38 +0900

    refactor : 보너스 번호와 로또 번호 예외 사항 시 재입력시키도록 변경

--- src/main/java/lotto/LottoDataService.java
+++ src/main/java/lotto/LottoDataService.java
@@ -7,14 +7,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoDataService {
-    Validation validation;
-    public Lotto create_official_Lotto(){
-        List<Integer> lotto_numbers = create_lotto_Numbers(InputView.InputLottoNumbers());
-        int bonus_number = create_bonus_number();
-        return create_Lotto(lotto_numbers, bonus_number);
+    public Lotto create_official_Lotto() {
+        Lotto lotto = create_Lotto_Without_BonusNumber();
+        String bonus_number = create_bonus_number(lotto);
+        return create_Lotto_With_BonusNumber(lotto, Integer.parseInt(bonus_number));
     }
 
-    private List<Integer> create_lotto_Numbers(String lotto_numbers){
+    private List<Integer> create_lotto_Numbers(String lotto_numbers) {
         Validation.validateLottoNumberType(lotto_numbers);
         List<Integer> lottoNumbers = new ArrayList<>();
         Arrays.stream(lotto_numbers.split(","))
@@ -24,13 +23,30 @@ public class LottoDataService {
         return lottoNumbers;
     }
 
-    private int create_bonus_number(){
-        String bonus_Number = InputView.InputBonusNumbers();
-        validation.validateNumberType(bonus_Number);
-        return Integer.parseInt(bonus_Number);
+    private Lotto create_Lotto_Without_BonusNumber() {
+        while (true) {
+            try {
+                List<Integer> lottoNumbers = create_lotto_Numbers(InputView.InputLottoNumbers());
+                return new Lotto(lottoNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
-    private Lotto create_Lotto(List<Integer> lotto_numbers, int bonus_number){
-        Lotto lotto = new Lotto(lotto_numbers);
+
+    private String create_bonus_number(Lotto lotto_numbers){
+        while(true)
+    {
+        try {
+            String bonus_Number = InputView.InputBonusNumbers();
+            Validation.validateBonusNumber(lotto_numbers, bonus_Number);
+            return bonus_Number;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+    private Lotto create_Lotto_With_BonusNumber(Lotto lotto, int bonus_number) {
         lotto.addBonusNumber(bonus_number);
         return lotto;
     }

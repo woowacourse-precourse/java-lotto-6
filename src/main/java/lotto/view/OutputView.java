95b46f46f5dd647d3c1b78dae2819f209e43d2ff
realisshomyang <realisshoman@kakao.com>
2023-11-08 12:20:29 +0900

    feat : 출력부분을 담당하는 OutputView 작성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,21 @@
+package lotto.view;
+
+public class OutputView {
+    public static final String PRINT_TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
+
+
+    public static void printTicketCount(int ticketCount) {
+        System.out.println(ticketCount + PRINT_TICKET_COUNT_MESSAGE);
+    }
+
+    public static void printWinningStatistics() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        /*
+        enum 로직 없어서 추후 구현
+         */
+    }
+    public static void printEarningRate(double earningRate) {
+        System.out.println("총 수익률은 " + String.format("%.1f", earningRate) + "%입니다.");
+    }
+}

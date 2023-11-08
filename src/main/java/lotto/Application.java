408d10ef29b3ae59eb73d3eeb729f551634807f7
HZion <hasion96@gmail.com>
2023-11-08 13:36:14 +0900

    로또 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 public class Application {
-    private int moneyValidate(){
+    private static int moneyValidate(){
         int money;
+
         while(true){
            try{
                System.out.println("구입금액을 입력해 주세요.");
                money =Integer.valueOf(Console.readLine());
+
                if ( (money % 1000) != 0) {
                    throw new IllegalArgumentException("1,000원 단위로 입력하세요.");
                }
@@ -19,12 +22,33 @@ public class Application {
         }
         return money;
     }
+
+    private List<Lotto> generateLottes(int trial){
+        List<Lotto> Lottos = new ArrayList<>();
+
+        for(int i=0; i< trial; i++){
+            List<Integer> lottsnumber = new ArrayList<>();
+            while(lottsnumber.size() < 6){
+                int number =  Randoms.pickNumberInRange(1,45);
+                if(!lottsnumber.contains(number)){
+                    lottsnumber.add(number);
+                }
+            }
+            Lottos.add(new Lotto(lottsnumber));
+        }
+        return Lottos;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int money;
-
+        int ticket;
+        int bouncenumber;
+        List<Lotto> Lottes;
 
         money = moneyValidate();
+        ticket = money /1000;
+
+        bouncenumber = Integer.valueOf(Console.readLine());
 
     }
 

116c87683084afdf9aa1eb33d8bdbd3bc0b4b31e
nahyeon99 <nahyeonee99@gmail.com>
2023-11-08 12:30:20 +0900

    refactor(view/*): 입출력 클래스 메서드 분리 및 코드 정리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,65 +1,45 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Objects;
 
 public class InputView {
-    private static final String BUYING_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    private static final String BLANK_ERROR_MESSAGE = "[ERROR] 공백은 입력할 수 없습니다.";
-    private static final String INTEGER_ERROR_MESSAGE = "[ERROR] 정수만 입력해야 합니다.";
-    private static final String POSITIVE_INTEGER_ERROR_MESSAGE = "[ERROR] 양수만 입력해야 합니다.";
+    private static final String BLANK_ERROR = "[ERROR] 공백은 입력할 수 없습니다.";
+    private static final String FORMAT_ERROR = "[ERROR] 정수만 입력하세요.";
 
-    public static int inputBuyingPrice() {
-        System.out.println(BUYING_PRICE_INPUT_MESSAGE);
-        String buyingPriceInput = Console.readLine();
-
-        validateBlank(buyingPriceInput);
-        int buyingPrice = validateAndConvertInteger(buyingPriceInput);
-        validateNegativeInteger(buyingPrice);
-        return buyingPrice;
+    public static int inputPrice() {
+        System.out.println(INPUT_PRICE_MESSAGE);
+        return readPositiveInt();
     }
 
     public static String inputWinningNumbers() {
-        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
-        String winningNumbers = Console.readLine();
-
-        validateBlank(winningNumbers);
-        return winningNumbers;
+        System.out.println();
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        return readString();
     }
 
     public static int inputBonusNumber() {
-        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
-        String bonusNumberInput = Console.readLine();
-
-        validateBlank(bonusNumberInput);
-        int bonusNumber = validateAndConvertInteger(bonusNumberInput);
-        validateNegativeInteger(bonusNumber);
-        return bonusNumber;
+        System.out.println();
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return readPositiveInt();
     }
 
-    public static void validateBlank(String input) {
-        if (Objects.isNull(input)) {
-            throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
-        }
+    private static String readString() {
+        String input = Console.readLine();
         if (input.isBlank()) {
-            throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
+            throw new IllegalArgumentException(BLANK_ERROR);
         }
+        return input;
     }
 
-    private static int validateAndConvertInteger(String input) {
+    private static int readPositiveInt() {
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INTEGER_ERROR_MESSAGE);
-        }
-    }
-
-    public static void validateNegativeInteger(int number) {
-        if (number <= 0) {
-            throw new IllegalArgumentException(POSITIVE_INTEGER_ERROR_MESSAGE);
+            throw new IllegalArgumentException(FORMAT_ERROR);
         }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,77 +2,66 @@ package lotto.view;
 
 import java.text.NumberFormat;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Prize;
-import lotto.dto.IssuedLottoResponse;
-import lotto.dto.WinningResultResponse;
+import lotto.domain.PrizeCount;
+import lotto.dto.LottoResponse;
 
 public class OutputView {
-    private static final String LOTTO_RESULT_TITLE = "\n당첨 통계";
-    private static final String LOTTO_PURCHASED_MESSAGE = "개를 구매했습니다.";
-    private static final String BONUSBALL_MATCHING_MESSAGE = ", 보너스 볼 일치";
-    private static final String LOTTO_WINNING_RESULT_MESSAGE = "%d개 일치%s (%s원) - %d개";
-    private static final String LOTTO_TOTAL_RETURN_MESSAGE = "총 수익률은 %s%%입니다.";
+    private static final String PURCHASE_COUNT_FORMAT = "%d개를 구매했습니다.";
+    private static final String LOTTO_NUMBERS_FORMAT = "[%s]";
+    private static final String COMMON_WINNING_RESULT_FORMAT = "%d개 일치 (%s원) - %d개";
+    private static final String SECOND_WINNING_RESULT_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String RETURN_RATE_FORMAT = "총 수익률은 %s%%입니다.";
+    private static final String WINNING_RESULT_TITLE = "당첨 통계";
+
     private static final int LOTTO_RESULT_CHARACTER_REPEAT_COUNT = 3;
-    private static final int LOTTO_ONE_PRICE = 1_000;
 
     private static final String DASH = "-";
-    private static final String OPEN_SQUARE_BRACKETS = "[";
-    private static final String CLOSE_SQUARE_BRACKETS = "]";
+    private static final String LOTTO_NUMBER_DELIMITER = ", ";
 
-    public static void printIssuedLottoCountAndNumbers(List<IssuedLottoResponse> lottoResponses) {
-        printIssuedLottoCount(lottoResponses.size());
-        lottoResponses.forEach(OutputView::printIssuedLotto);
+    public static void printPurchasedCount(final int purchasedCount) {
+        System.out.println();
+        System.out.printf(PURCHASE_COUNT_FORMAT, purchasedCount);
+        System.out.println();
     }
 
-    public static void printLottoResultTitle() {
-        System.out.println(LOTTO_RESULT_TITLE);
+    public static void printPurchasedLottos(List<LottoResponse> lottoResponses) {
+        lottoResponses.forEach(OutputView::printPurchasedLotto);
+    }
+
+    private static void printPurchasedLotto(LottoResponse lottoResponse) {
+        String joinedNumbers = lottoResponse.getLottoNumbers().stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(LOTTO_NUMBER_DELIMITER));
+
+        System.out.printf(LOTTO_NUMBERS_FORMAT, joinedNumbers);
+        System.out.println();
+    }
+
+    public static void printPrizeCount(PrizeCount prizeCount) {
+        System.out.println();
+        System.out.println(WINNING_RESULT_TITLE);
         System.out.println(DASH.repeat(LOTTO_RESULT_CHARACTER_REPEAT_COUNT));
+
+        prizeCount.getPrizeCount().forEach(OutputView::printOnePrizeCount);
     }
 
-    public static void printFullWinningResult(WinningResultResponse winningResultResponse) {
-        winningResultResponse.getWinningResult().forEach(OutputView::printOneWinningResult);
-    }
-
-    private static void printOneWinningResult(Prize prize, int count) {
-
-        if (prize.equals(Prize.NONE)) {
-            return;
-        }
-
-        String bonusballMessage = "";
-        if (prize.equals(Prize.SECOND)) {
-            bonusballMessage = BONUSBALL_MATCHING_MESSAGE;
-        }
-
+    private static void printOnePrizeCount(Prize prize, int count) {
         NumberFormat numberFormat = NumberFormat.getInstance();
         String winningAmount = numberFormat.format(prize.getWinningAmount());
 
-        System.out.printf(LOTTO_WINNING_RESULT_MESSAGE, prize.getMatchingCount(), bonusballMessage, winningAmount,
-                count);
-        System.out.println();
-    }
-
-    public static void printTotalReturn(WinningResultResponse winningResultResponse, int lottoQuantity) {
-        double totalWinningAmount = winningResultResponse.getTotalWinningPrice();
-        double totalReturn = totalWinningAmount / (LOTTO_ONE_PRICE * lottoQuantity) * 100;
-
-        System.out.printf(LOTTO_TOTAL_RETURN_MESSAGE, String.format("%.1f", totalReturn));
-        System.out.println();
-    }
-
-    private static void printIssuedLottoCount(int lottoCount) {
-        System.out.println(lottoCount + LOTTO_PURCHASED_MESSAGE);
-    }
-
-    private static void printIssuedLotto(IssuedLottoResponse lottoResponse) {
-        String output = OPEN_SQUARE_BRACKETS;
-
-        for (int number : lottoResponse.getLottoNumbers()) {
-            output += (number + ", ");
+        if (prize.equals(Prize.SECOND)) {
+            System.out.printf(SECOND_WINNING_RESULT_FORMAT, prize.getMatchingCount(), winningAmount, count);
+            System.out.println();
+            return;
         }
-        output = output.substring(0, output.length() - 2);
+        System.out.printf(COMMON_WINNING_RESULT_FORMAT, prize.getMatchingCount(), winningAmount, count);
+        System.out.println();
+    }
 
-        output += CLOSE_SQUARE_BRACKETS;
-        System.out.println(output);
+    public static void printReturnRate(double returnRate) {
+        System.out.println();
+        System.out.printf(RETURN_RATE_FORMAT, String.format("%.1f", returnRate));
     }
 }

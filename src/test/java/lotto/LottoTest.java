87bfe93604a04abc6a50a206b9f2f879700c77dd
hajungIm <ihj_ewha@ewhain.net>
2023-11-08 12:05:20 +0900

    Test(LottoTest): 로또 `toString()` 테스트 완료
    `toString()` 호출 시 숫자 배열 String 반환하는 테스트 완료

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,16 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -23,5 +28,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
-}
+    @DisplayName("toString() 호출 시 로또 숫자 배열 String 반환한다.")
+    @Test
+    void toStringLotto() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        String actual = lotto.toString();
+        String expected = "[1, 2, 3, 4, 5, 6]";
+
+        assertThat(actual).isEqualTo(expected);
+    }

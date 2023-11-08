1340f091342f9ca07b60ef4440b82e3244bcdbf5
dgh06175 <dgh06175@gmail.com>
2023-11-08 18:40:48 +0900

    form <LottoTest>: 주석 제거

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -33,7 +33,6 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(LOTTO_NUMBER_DUPLICATED_ERROR.getMessage());
@@ -58,14 +57,11 @@ class LottoTest {
     @DisplayName("로또 숫자 반환 컬렉션 값 변경 예외 테스트")
     @Test
     public void testGetNumbersReturnsUnmodifiableList() {
-        // Given
         List<Integer> validLottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         Lotto lotto = new Lotto(validLottoNumbers);
 
-        // When
         List<Integer> numbers = lotto.getNumbers();
 
-        // Then
         assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
         assertThatThrownBy(() -> numbers.add(7))
                 .isInstanceOf(UnsupportedOperationException.class);

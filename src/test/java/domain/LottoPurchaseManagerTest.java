5d34d03f6db6287c513d666cdd2b358f49a6b158
Sungkyu <kxu4583@naver.com>
2023-11-08 18:03:11 +0900

    test: '로또 발행' 기능 테스트 코드 추가

--- src/test/java/domain/LottoPurchaseManagerTest.java
+++ src/test/java/domain/LottoPurchaseManagerTest.java
@@ -1,7 +1,11 @@
 package domain;
 
 import lotto.domain.LottoPurchaseManager;
+import lotto.model.Lotto;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class LottoPurchaseManagerTest {
@@ -19,5 +23,12 @@ class LottoPurchaseManagerTest {
         assertThatThrownBy(() -> manager.calculateNumOfLotto(7300))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구입 금액은 " + manager.PRICE_OF_LOTTO + "원으로 나누어 떨어져야 합니다.");
+    }
+
+    @Test
+    void createLottos() {
+        LottoPurchaseManager manager = new LottoPurchaseManager();
+        List<Lotto> lottos = manager.createLottos(9);
+        assertThat(lottos).hasSize(9);
     }
 }

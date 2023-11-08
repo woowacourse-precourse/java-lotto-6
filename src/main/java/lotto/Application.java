1fdaea605b44cc4859a1a0fec7d823b713717b03
ilot12 <rlaehdwn60@gmail.com>
2023-11-08 13:37:28 +0900

    feat: main함수 예외시 반복 처리 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,53 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        LottoGame lottoGame = new LottoGame();
+        
+        boolean setLottoBudgetAgain;
+        do {
+            setLottoBudgetAgain = false;
+            try {
+                lottoGame.setLottoBudget();
+            } catch (IllegalArgumentException error) {
+                System.out.println(error.getMessage());
+                setLottoBudgetAgain = true;
+            }
+        }while(setLottoBudgetAgain);
+        System.out.println();
+        
+        lottoGame.setLottos();
+        lottoGame.printLottoTicket();
+        System.out.println();
+        
+        boolean setWinningNumbersAgain;
+        do {
+            try {
+                setWinningNumbersAgain = false;
+                lottoGame.setWinningNumbers();
+            } catch (IllegalArgumentException error) {
+                System.out.println(error.getMessage());
+                setWinningNumbersAgain = true;
+            }
+        }while(setWinningNumbersAgain);
+        System.out.println();
+        
+        boolean setBonusNumbersAgain;
+        do {
+            try {
+                setBonusNumbersAgain = false;
+                lottoGame.setBonusNumbers();
+            } catch (IllegalArgumentException error) {
+                System.out.println(error.getMessage());
+                setBonusNumbersAgain = true;
+            }
+        }while(setBonusNumbersAgain);
+        System.out.println();
+        
+        lottoGame.setLottoTicketPrize();
+        lottoGame.printLottoTicketPrize();
+        
+        lottoGame.printProfitPercentage();
+        
     }
 }

4823e7b3a1127eae008b608c4035b8a958d29a6e
yeondori <yeonseo.jung0@gmail.com>
2023-11-08 22:53:12 +0900

    feat: 입력 및 생성을 수행하는 메서드(createLottos, createLotto,  createWinningLotto) 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,19 +6,51 @@ import lotto.ui.OutputManager;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        InputManager inputManager = new InputManager();
-        OutputManager outputManager = new OutputManager();
+    static final InputManager inputManager = new InputManager();
+    static final OutputManager outputManager = new OutputManager();
 
-        int amount = inputManager.requestAmount();
-        List<Lotto> lottos = LottoGenerator.generateFrom(amount);
+    public static void main(String[] args) {
+        List<Lotto> lottos = createLottos();
         outputManager.printLottos(lottos);
 
-        List<Integer> numbers = inputManager.requestNumbers();
-        int bonus = inputManager.requestBonus(numbers);
-        WinningLotto winningLotto = WinningLotto.of(numbers, bonus);
+        WinningLotto winningLotto = createWinningLotto();
 
         List<Result> results = LottoResultChecker.getResultsOf(winningLotto, lottos);
         outputManager.printResults(results);
+    }
+
+    public static List<Lotto> createLottos() {
+        while (true) {
+            try {
+                int amount = inputManager.requestAmount();
+                return LottoGenerator.generateFrom(amount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Lotto createLotto() {
+        while (true) {
+            try {
+                List<Integer> numbers = inputManager.requestNumbers();
+                return new Lotto(numbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static WinningLotto createWinningLotto() {
+        Lotto winningNumbers = createLotto();
+
+        while (true) {
+            try {
+                int bonus = inputManager.requestBonus(winningNumbers.getNumbers());
+                return WinningLotto.of(winningNumbers, bonus);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }

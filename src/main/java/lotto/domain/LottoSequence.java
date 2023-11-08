cf30686f3015c2304b3d8032a25090d3078af776
Hyoseop Song <crad_on25@naver.com>
2023-11-08 22:55:41 +0900

    feat(LottoSequence): 여러 개의 로또를 하나의 자료형으로 묶는 시퀀스 구현

---
+++ src/main/java/lotto/domain/LottoSequence.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoSequence {
+    private final List<Lotto> lottoSequence = new ArrayList<>();
+
+    public void add(Lotto lotto) {
+        lottoSequence.add(lotto);
+    }
+
+    public int size() {
+        return lottoSequence.size();
+    }
+
+    public Lotto get(int index) {
+        return lottoSequence.get(index);
+    }
+}

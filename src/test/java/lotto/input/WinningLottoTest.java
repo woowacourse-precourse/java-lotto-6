541d1b3d11aeaf9b1181d91e002449c35dd9972c
toychip <dlawnsgud427@naver.com>
2023-11-08 19:11:20 +0900

    test: 원소 중 범위를 넘어가는 값이 있을 때 테스트

--- src/test/java/lotto/input/WinningLottoTest.java
+++ src/test/java/lotto/input/WinningLottoTest.java
@@ -1,10 +1,12 @@
 package lotto.input;
 
 import static lotto.exception.ErrorType.InsideListDuplicatedException;
+import static lotto.exception.ErrorType.OverValueException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.exception.ErrorType;
 import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +14,8 @@ import org.junit.jupiter.api.Test;
 public class WinningLottoTest {
 
     @Test
-    @DisplayName("입력한 로또 원소 중 겹치는 값이 있을 때 InsideListDuplicatedException 발생")
-    void createDuplicatedList(){
+    @DisplayName("원소 중 겹치는 값이 있을 때 InsideListDuplicatedException 발생")
+    void createDuplicatedListTest(){
 
         //given
         int duplicatedNumber = 5;
@@ -23,5 +25,19 @@ public class WinningLottoTest {
         assertThatThrownBy(() -> new Lotto(lottoNumbers))
                 .isInstanceOf(LottoException.class)
                 .hasFieldOrPropertyWithValue("exceptionType", InsideListDuplicatedException);
+    }
+
+    @Test
+    @DisplayName("원소 중 범위를 넘어가는 값이 있을 때 InsideListDuplicatedException 발생")
+    void invalidNumberTest(){
+
+        //given
+        int invalidNumber = 100;
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, invalidNumber);
+
+        //when & then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isInstanceOf(LottoException.class)
+                .hasFieldOrPropertyWithValue("exceptionType", OverValueException);
     }
 }

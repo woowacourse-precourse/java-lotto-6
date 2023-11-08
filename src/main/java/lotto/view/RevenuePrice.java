aa32810b5c4d34ad9108567a2faf77c468f9396a
dabin <db021129@naver.com>
2023-11-08 22:00:17 +0900

    feat : add getPriceByIndex

--- src/main/java/lotto/view/RevenuePrice.java
+++ src/main/java/lotto/view/RevenuePrice.java
@@ -1,15 +1,26 @@
 package lotto.view;
 
 public enum RevenuePrice {
-    INDEX_ZERO(2000000000),
-    INDEX_ONE(30000000),
-    INDEX_TWO(1500000),
-    INDEX_THREE(50000),
-    INDEX_FOUR(5000);
+    INDEX_ZERO(2000000000, 0),
+    INDEX_ONE(30000000, 1),
+    INDEX_TWO(1500000, 2),
+    INDEX_THREE(50000, 3),
+    INDEX_FOUR(5000, 4);
 
     public final int price;
+    public final int index;
 
-    private RevenuePrice(int price) {
+    private RevenuePrice(int price, int index) {
         this.price = price;
+        this.index = index;
+    }
+
+    public int getPriceByIndex(int index) {
+        for (RevenuePrice revenuePrice : RevenuePrice.values()) {
+            if (revenuePrice.index == index) {
+                return revenuePrice.price;
+            }
+        }
+        return 0;
     }
 }

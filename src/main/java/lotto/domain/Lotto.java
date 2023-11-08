b50ba24bf9681a70b707aba41f1928d97b45483c
wonindev <sangwonparkdev@gmail.com>
2023-11-08 16:49:12 +0900

    Refactor: 로또 번호의 검증 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -20,25 +20,26 @@ public class Lotto {
     }
 
     private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != LottoEnum.LOTTO_SIZE.getValue())
+        if (numbers.size() != LottoEnum.LOTTO_SIZE.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBER_ERROR.getError());
+        }
     }
 
-    private void validateDuplicate(List<Integer> numbers) { // todo: 중복 검사는 꼭 해야함!!
+    private void validateDuplicate(List<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
-            if (Validator.isDuplicate(numbers, numbers.get(i), i))
-                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER_ERROR.getError());
+            Validator.isDuplicate(numbers, numbers.get(i), i);
         }
     }
 
     private void validateBetweenOneToFortyFive(List<Integer> numbers) {
         for (int i = 0; i < numbers.size(); i++) {
-            if (!Validator.isOneToFortyFive(numbers.get(i)))
-                throw new IllegalArgumentException(ErrorMessage.NOT_ONE_TO_FORTYFIVE_ERROR.getError());
+            Validator.isOneToFortyFive(numbers.get(i));
         }
     }
 
     private void validateNull(List<Integer> numbers) {
-        if (numbers == null) throw new IllegalArgumentException(ErrorMessage.NULL_ERROR.getError());
+        if (numbers == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_ERROR.getError());
+        }
     }
 }

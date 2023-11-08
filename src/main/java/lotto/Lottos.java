3390b2741c8be75884f010a4ae3451d4264b227f
DK <laancer4@gmail.com>
2023-11-08 11:01:24 +0900

    feat : Lottos 객체를 랜덤한 값으로 만드는 생성자 매서드 추가

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Lottos {
     private final List<Lotto> lottoList;
@@ -9,5 +13,20 @@ public class Lottos {
     }
     public void calculateWinning(StandardLotto standardLotto){
         this.lottoList.forEach(lotto -> WinningCalculator.addCount(standardLotto.matchPrize(lotto)));
+    }
+    private Lottos(int lottoCount) {
+        this.lottoList = IntStream.range(0, lottoCount)
+                .mapToObj(i -> Randoms.pickUniqueNumbersInRange(1, 45, 6))
+                .map(this::getLotto)
+                .toList();
+    }
+
+    public static Lottos ofRandom(int lottoCount){
+        return new Lottos(lottoCount);
+    }
+
+    private Lotto getLotto(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return new Lotto(numbers);
     }
 }

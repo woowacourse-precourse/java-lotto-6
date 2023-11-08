bdc4c114c83f3c5461a3806c4b10528e82918ef8
seastamp <seastamp10@gmail.com>
2023-11-09 00:57:10 +0900

    fix: 숫자 생성 후 정렬하도록 수정

--- src/main/java/lotto/utils/NumbersGenerator.java
+++ src/main/java/lotto/utils/NumbersGenerator.java
@@ -5,12 +5,16 @@ import static lotto.constants.Value.MAX_LOTTO_NUMBER;
 import static lotto.constants.Value.MIN_LOTTO_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class NumbersGenerator {
     public static List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.get(), MAX_LOTTO_NUMBER.get(),
-                LOTTO_SIZE.get());
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.get(), MAX_LOTTO_NUMBER.get(),
+                LOTTO_SIZE.get()));
+        numbers.sort(Comparator.naturalOrder());
         return numbers;
     }
 }

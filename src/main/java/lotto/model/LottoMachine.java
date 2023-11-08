4a58e71c3fba47df68f5269601d034a7ebd00967
yoonjuho <dbswn990@naver.com>
2023-11-08 17:44:30 +0900

    refactor(LottoMachine): 변수, 함수명 변경

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,24 +1,23 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
     private final Price lottoPrice;
-    private LottoCreateStrategy lottoCreateStrategy;
 
-    public LottoMachine(Price lottoPrice, LottoCreateStrategy lottoCreateStrategy ) {
+    public LottoMachine(Price lottoPrice) {
         this.lottoPrice = lottoPrice;
-        this.lottoCreateStrategy = lottoCreateStrategy;
     }
 
-    public List<Lotto> issue(int money){
-        int quantity = lottoPrice.calculate(money);
+    public List<Lotto> issue(int money) {
+        int quantity = lottoPrice.divide(money);
         List<Lotto> lottoTicket = new ArrayList<>();
-        while (quantity != 0){
-            lottoTicket.add(new Lotto(lottoCreateStrategy.create()));
+        while (quantity != 0) {
+            lottoTicket.add(new Lotto(Strategy.generateLotto()));
             quantity--;
         }
-        return lottoTicket;
+        return Collections.unmodifiableList(lottoTicket);
     }
 }

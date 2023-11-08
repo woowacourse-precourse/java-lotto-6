2a0cf1abd000168666b2961451e9649c1365ac27
haebyun <bhk4392@gmail.com>
2023-11-08 15:42:02 +0900

    refactor(InputView): 빈 입력 값에 대한 예외 처리를 view에서 처리하도록 조정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.LottoException;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -10,18 +11,30 @@ public class InputView {
 
     public static String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
-        return Console.readLine();
+        String amount = Console.readLine();
+        validateIsEmptyInput(amount);
+        return amount;
     }
 
     public static String readWinningNumbers() {
         System.out.println();
         System.out.println(WINNING_NUMBER_MESSAGE);
-        return Console.readLine();
+        String winningNumbers = Console.readLine();
+        validateIsEmptyInput(winningNumbers);
+        return winningNumbers;
     }
 
     public static String readBonusNumber() {
         System.out.println();
         System.out.println(BONUS_NUMBER_MESSAGE);
-        return Console.readLine();
+        String bonusNumber = Console.readLine();
+        validateIsEmptyInput(bonusNumber);
+        return bonusNumber;
+    }
+
+    private static void validateIsEmptyInput(String input){
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(LottoException.EMPTY_INPUT_ERROR_MESSAGE.getMessage());
+        }
     }
 }

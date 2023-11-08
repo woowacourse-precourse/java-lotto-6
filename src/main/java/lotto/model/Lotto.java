3b76284a0be2db4950d4dd408837735b3e61c9fc
2021110806 <2021110806@dgu.ac.kr>
2023-11-08 23:33:37 +0900

    feat(Lotto.java): add sort function 구매한 로또의 리스트를 출력할 때 정렬해서 출력하도록 구현하였습니다.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
 import lotto.Utils;
+
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,6 +20,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 

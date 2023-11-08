bf6e327d3b6fef859de94bce88dc774ecb7005ff
gimdaeyeon <rlaeodus1306@naver.com>
2023-11-08 14:14:59 +0900

    Feat: 당첨번호 입력에 중복값이 있는지 확인하는 메소드 추가

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     INVALID_NUMBER_FORMAT("숫자만 입력해 주세요"),
     INVALID_NUMBER_RANGE("번호는 1~45까지만 입력해 주세요"),
     INVALID_PURCHASE_AMOUNT("금액은 1000단위로 입력해 주세요"),
-    INVALID_NUMBER_COUNT("당첨 번호는 6자리로 입력해 주세요");
+    INVALID_NUMBER_COUNT("당첨 번호는 6자리로 입력해 주세요"),
+    INVALID_DUPLICATE_VALUE("당첨 번호에 중복이 허용되지 않습니다");
 
     private final String message;
 
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -29,13 +29,21 @@ public class Validator {
     public static void validateWinningNumber(String winningNumberStr){
         String[] winningNumbers = winningNumberStr.split(",");
         validateNumberCount(winningNumbers);
-
+        validateDuplicate(winningNumbers);
         Arrays.stream(winningNumbers).forEach((number)->{
             validateNumericInput(number);
             validateNumberInRange(Integer.parseInt(number));
         });
     }
 
+    private static void validateDuplicate(String[] numbers){
+        int length = numbers.length;
+        int duplicatedLength = (int)Arrays.stream(numbers).distinct().count();
+        if(length!=duplicatedLength){
+            throw new IllegalArgumentException(INVALID_DUPLICATE_VALUE.getMessage());
+        }
+
+    }
     private static void validateNumberCount(String[] numbers){
         if(numbers.length!=LOTTO_NUMBER_COUNT){
             throw new IllegalArgumentException(INVALID_NUMBER_COUNT.getMessage());

00fb3f60059332d2a58c684a4af53747839f0ace
kmx227 <rlaalstjq227@gmail.com>
2023-11-08 18:28:51 +0900

    Feat: 컨틀롤러 구현
    게임의 플로우 구현 및 구현 기능 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.Controller.Controller;
+import lotto.Utils.InputSystem;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.startPurchase();
+        controller.decideWinningLotto(InputSystem.inputWinningNumbers());
+        controller.calculateWinning();
+        controller.printResult();
     }
 }

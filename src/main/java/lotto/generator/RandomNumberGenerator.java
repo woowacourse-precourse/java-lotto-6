49e76631c4a2008d2ff6b3b9fd8e9f80dfcdcdd5
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-08 20:53:46 +0900

    refactor: 스트림 toList()로 수정

--- src/main/java/lotto/generator/RandomNumberGenerator.java
+++ src/main/java/lotto/generator/RandomNumberGenerator.java
@@ -1,12 +1,11 @@
 package lotto.generator;
 
-import static lotto.Option.GameOption.LOTTO_MAX_NUMBER;
-import static lotto.Option.GameOption.LOTTO_MIN_NUMBER;
-import static lotto.Option.GameOption.LOTTO_NUMBER_LENGTH;
+import static lotto.option.GameOption.LOTTO_MAX_NUMBER;
+import static lotto.option.GameOption.LOTTO_MIN_NUMBER;
+import static lotto.option.GameOption.LOTTO_NUMBER_LENGTH;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RandomNumberGenerator implements NumberGenerator {
     @Override
@@ -15,9 +14,9 @@ public class RandomNumberGenerator implements NumberGenerator {
                 LOTTO_MAX_NUMBER.getNumber(), LOTTO_NUMBER_LENGTH.getNumber()));
     }
 
-    private List<Integer> ascendOrder(List<Integer> randomNumbers) {
+    public List<Integer> ascendOrder(List<Integer> randomNumbers) {
         return randomNumbers.stream()
                 .sorted()
-                .collect(Collectors.toList());
+                .toList();
     }
 }

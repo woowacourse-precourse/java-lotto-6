8ac050ce106e698ce56b1b19fc5dc3e6f01db660
JaeHunJang <wuddlaa@gmail.com>
2023-11-08 14:07:17 +0900

    feat(Message): Input & Output 메시지 Enum 생성

---
+++ src/main/java/lotto/config/InputMessage.java
@@ -0,0 +1,17 @@
+package lotto.config;
+
+public enum InputMessage {
+    INPUT_MONEY("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/lotto/config/OutputMessage.java
@@ -0,0 +1,25 @@
+package lotto.config;
+
+public enum OutputMessage {
+    OUTPUT_LOTTO_COUNT("%n%d개를 구매했습니다.%n"),
+    OUTPUT_LOTTOS_NUMBER("""
+                        %n당첨 통계
+                        ---
+                        3개 일치 (5,000원) - %d개
+                        4개 일치 (50,000원) - %d개
+                        5개 일치 (1,500,000원) - %d개
+                        5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+                        6개 일치 (2,000,000,000원) - %d개
+                        """),
+    OUTPUT_LOTTO_PROFIT("총 수익률은 %.1f%%입니다.%n");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

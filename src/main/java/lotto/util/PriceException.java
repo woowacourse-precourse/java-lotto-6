39e85629f8a08260434087cbcdc1755ef722528f
Hyso <hsk7953@gmail.com>
2023-11-08 22:48:31 +0900

    feat:확실한 구분

--- src/main/java/lotto/util/PriceException.java
+++ src/main/java/lotto/util/PriceException.java
@@ -3,44 +3,21 @@ package lotto.util;
 import lotto.model.Price;
 
 public class PriceException {
-    public static Price validPrice(int money) {
-        checkPositiveNumber(money);
-        checkMultipleOfThousand(money);
-        return new Price(money);
-    }
 
-    public static void checkValidNumber (String input) {
-        try{
-            checkEmpty(input);
-            invalidSpace(input);
-            Integer.parseInt(input);
-        } catch (NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] : 옳바른 값을 입력해주세요.");
+    private static final int standardPrice = 1000;
+
+
+    public static boolean checkZeroPrice(int price){
+        if (price < standardPrice) {
+            return false;
         }
+        return true;
     }
 
-    private static void checkMultipleOfThousand (int number){
-        if(number % 1000 != 0){
-            throw new IllegalArgumentException("[ERROR] : 구입 가격은 1000원 단위입니다.");
+    public static boolean checkThousandPrice(int price) {
+        if (price % standardPrice != 0) {
+            return false;
         }
+        return true;
     }
-
-    private static void checkPositiveNumber (int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("[ERROR] : 구입 가격은 양수여야 합니다.");
-        }
-    }
-
-    private static void checkEmpty(String input) {
-        if (input.isEmpty()){
-            throw new IllegalArgumentException("[ERROR] : 빈값은 입력할 수 없습니다.");
-        }
-    }
-
-    private static void invalidSpace(String input) {
-        if (input.contains(" ")){
-            throw new IllegalArgumentException("[ERROR] : 입력값에 빈칸은 있으면 안됩니다.");
-        }
-    }
-
 }

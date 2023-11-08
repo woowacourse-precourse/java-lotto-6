47fed8eb46bd46e26f7d2a93bf9a7bf3f84e6aae
gyeommm <dkndob02@naver.com>
2023-11-08 16:08:38 +0900

    검증 기능 분리에 따른 테스트 코드 수정

--- src/test/java/lotto/PurchaseAmountTest.java
+++ src/test/java/lotto/PurchaseAmountTest.java
@@ -3,49 +3,48 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PurchaseAmountTest {
     PurchaseAmount purchaseAmount = new PurchaseAmount();
-    @DisplayName("입력값이 1000원 단위의 숫자가 아닌 경우 예외가 발생한다.")
+
+    @DisplayName("입력값이 정수가 아닌 경우 예외가 발생한다.")
     @Test
-    void 구입금액_검증() {
+    void 구입금액_정수형_검증() {
         // given
-        String amount1 = "";        // 입력이 없는 경우
-        String amount2 = " ";       // 공백인 경우
-        String amount3 = "abc";     // 문자인 경우
-        String amount4 = "10500";    // 1000원 단위가 아닌 경우
-        String amount5 = "-2000";      // 음수인 경우
+        String amount1 = "";
+        String amount2 = " ";
+        String amount3 = "1000.0";
+        String amount4 = "abc";
+        String amount5 = null;
 
         // then
         assertThrows(IllegalArgumentException.class, () -> {
-            purchaseAmount.validate(amount1);
+            purchaseAmount.convertToInt(amount1);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            purchaseAmount.validate(amount2);
+            purchaseAmount.convertToInt(amount2);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            purchaseAmount.validate(amount3);
+            purchaseAmount.convertToInt(amount3);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            purchaseAmount.validate(amount4);
+            purchaseAmount.convertToInt(amount4);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            purchaseAmount.validate(amount5);
+            purchaseAmount.convertToInt(amount5);
         });
     }
 
-    @DisplayName("구입금액이 정상적으로 할당되는지 확인한다.")
+    @DisplayName("입력값이 1000원 단위가 아닌 경우 예외가 발생한다.")
     @Test
-    void 구입금액_정삭작동() {
+    void 구입금액_단위_검증() {
         // given
-        String amount = "6000";
-
-        // when
-        purchaseAmount.validate(amount);
+        int amount = 12300;
 
         // then
-        assertEquals(6000, purchaseAmount.getAmount());
+        assertThrows(IllegalArgumentException.class, () -> {
+            purchaseAmount.checkUnit1000(amount);
+        });
     }
 }

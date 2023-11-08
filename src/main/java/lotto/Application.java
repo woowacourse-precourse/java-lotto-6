db7c3d92d5d38ca8aae925cd115e295ed3c8a02e
SWtheWhite <swthewhite@gmail.com>
2023-11-08 20:09:27 +0900

    feat: 기본 설정 - 로또 한 장 가격 상수 설정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,8 @@ package lotto;
 public class Application {
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_ONE_PRICE = 1000;
+
 
     public static void main(String[] args) {
 

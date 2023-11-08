4ad5564f7524d5ed8666aeb5925731c8cc7fd415
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-08 17:45:50 +0900

    test: Payment 수익률 계산 메서드 성공 테스트 구현

--- src/test/java/lotto/domain/PaymentTest.java
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PaymentTest {
@@ -12,5 +13,18 @@ class PaymentTest {
     void createSuccessTest() {
         assertThatThrownBy(() -> new Payment(-1))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("지불 금액 대비 수익률 퍼센트를 반환한다.")
+    @Test
+    void calculateProfitPercentageSuccessTest() {
+        // given
+        Payment payment = new Payment(5000);
+
+        // when
+        double profitPercentage = payment.calculateProfitPercentage(10000);
+
+        // then
+        assertThat(profitPercentage).isEqualTo(200.0);
     }
 }

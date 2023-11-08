64e0890e180bfe8c53b4f6a1a132cded9ea9d000
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 12:02:40 +0900

    [feat] Domain - 자동 생성되는 LottoNumbers 객체 관리 및 저장

--- src/main/java/lotto/Domain/LottoNumbers.java
+++ src/main/java/lotto/Domain/LottoNumbers.java
@@ -1,4 +1,35 @@
 package lotto.Domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.Exception.LottoNumberErrorMessage;
+
 public class LottoNumbers {
+    private final int MINIMUM_VALUE = 1;
+    private final int MAXIMUM_VALUE = 45;
+    private final int COUNT_NUMBER = 6;
+
+    private List<Integer> numbers;
+
+    public LottoNumbers() {
+        this.numbers = generateRandomNumbers();
+        validate(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i) <= numbers.get(i - 1)) {
+                throw new IllegalArgumentException(LottoNumberErrorMessage.IS_NOT_ASCENDING_NUMBER.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private List<Integer> generateRandomNumbers() {
+        List<Integer> generatedNumbers = Randoms.pickUniqueNumbersInRange(MINIMUM_VALUE, MAXIMUM_VALUE, COUNT_NUMBER);
+        return generatedNumbers;
+    }
 }

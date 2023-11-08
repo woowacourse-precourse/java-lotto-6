292cd37cc1316f793ac25d418dc853e29dd35788
Jihyeon Park <mmmszip@gmail.com>
2023-11-08 23:52:43 +0900

    feat: 로또 구매 기능 구현

---
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import lotto.constants.ErrorMessage;
+
+public class LottoPurchase {
+    private static final int LOTTO_PER_PRICE = 1000;
+
+    private final int amount;
+
+    public LottoPurchase(int amount) {
+        this.amount = amount;
+    }
+
+    public int getPurchaseAmount() {
+        return amount / LOTTO_PER_PRICE;
+    }
+
+    private static void validatePositiveNumber(int amount) {
+        if(amount <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_POSITIVE_NUMBER);
+        }
+    }
+
+    private static void validateUnitAmount(int amount) {
+        if(amount % LOTTO_PER_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_HUNDRED_UNIT_AMOUNT);
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}

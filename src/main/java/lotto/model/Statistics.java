81f1ebd40c7eba6004990e57d66d2f6e4ee786a7
yoonjuho <dbswn990@naver.com>
2023-11-08 17:02:36 +0900

    refactor(Statistics): 값을 하드코딩하지 않고 상수화 처리

--- src/main/java/lotto/model/Statistics.java
+++ src/main/java/lotto/model/Statistics.java
@@ -5,9 +5,14 @@ import java.util.List;
 import java.util.Map;
 
 public class Statistics {
+    private static final int PERCENT_RATE = 100;
+    private static final int ONE_COUNT = 1;
+
+    private static final String CURRENCY_UNIT = "%,.1f";
+
     private Statistics() {}
 
-    private static Map<Rank, Integer> initWinningStatus() {
+    private static Map<Rank, Integer> initWinningDetails() {
         Map<Rank, Integer> winningDetails = new EnumMap<>(Rank.class);
         for (Rank rank : Rank.values()) {
             winningDetails.put(rank, 0);
@@ -16,18 +21,18 @@ public class Statistics {
     }
 
     public static Map<Rank, Integer> calculateRank(WinningLotto winningLotto, List<Lotto> lottoTicket) {
-        Map<Rank, Integer> winningDetails = initWinningStatus();
+        Map<Rank, Integer> winningDetails = initWinningDetails();
         for (Lotto lotto : lottoTicket) {
-            winningDetails.merge(winningLotto.compare(lotto), 1, Integer::sum);
+            winningDetails.merge(winningLotto.compare(lotto), ONE_COUNT, Integer::sum);
         }
         return winningDetails;
     }
 
-    public static String calculateProfitRate(Map<Rank, Integer> winningDetails, int money) {
+    public static String calculateProfitRate(Map<Rank, Integer> winningDetails, int invest) {
         double profit = 0.0;
         for (Rank rank : winningDetails.keySet()) {
             profit += rank.getReward() * winningDetails.get(rank);
         }
-        return String.format("%,.1f", profit * 100 / money);
+        return String.format(CURRENCY_UNIT, profit * PERCENT_RATE / invest);
     }
 }

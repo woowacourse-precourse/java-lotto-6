11840b0374f43aa61c63be033977d8803ee915e5
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-08 20:35:46 +0900

    refactor: 클래스명 변경 LottoMachine -> CompareMachine

--- src/test/java/lotto/domain/result/CompareMachineTest.java
+++ src/test/java/lotto/domain/result/CompareMachineTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.stream.Stream;
 import lotto.MockNumberGenerator;
-import lotto.Option.GameStatus;
+import lotto.Option.EqualStatus;
 import lotto.domain.Lotto;
 import lotto.domain.user.LottoCount;
 import lotto.domain.user.UserLotto;
@@ -17,14 +17,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class LottoMachineTest {
+class CompareMachineTest {
     private final int INPUT_MONEY = 1000;
 
     @DisplayName("당첨내역을 제대로 계산하는지 확인한다.")
     @ParameterizedTest
     @MethodSource("validParameters")
-    void checkCompareSystem(List<Integer> numbers, int number, GameStatus gameStatus, int equalNumber) {
-        LottoCount lottoCount = new LottoCount(INPUT_MONEY);
+    void checkCompareSystem(List<Integer> numbers, int number, EqualStatus equalStatus, int equalNumber) {
+        LottoCount lottoCount = LottoCount.from(INPUT_MONEY);
         UserLotto userLotto = new UserLotto(lottoCount);
         userLotto.create(new MockNumberGenerator(List.of(1, 3, 5, 7, 9, 11)));
 
@@ -32,24 +32,24 @@ public class LottoMachineTest {
         Bonus bonus = new Bonus(number);
         WinLotto winLotto = new WinLotto(lotto, bonus);
 
-        LottoMachine lottoMachine = new LottoMachine();
-        lottoMachine.compare(userLotto, winLotto);
+        CompareMachine compareMachine = new CompareMachine();
+        compareMachine.compare(userLotto, winLotto);
 
-        assertThat(lottoMachine.getWinStatus().get(new LottoResult(gameStatus))).isEqualTo(equalNumber);
+        assertThat(compareMachine.getWinStatus().get(new LottoResult(equalStatus))).isEqualTo(equalNumber);
     }
 
     static Stream<Arguments> validParameters() {
         return Stream.of(
-                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 7, GameStatus.FOUR_EQUAL, 1),
-                Arguments.of(List.of(1, 2, 4, 6, 8, 10), 3, GameStatus.THREE_EQUAL, 0),
-                Arguments.of(List.of(1, 2, 3, 5, 7, 9), 11, GameStatus.FIVE_AND_BONUS_EQUAL, 1)
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 7, EqualStatus.FOUR_EQUAL, 1),
+                Arguments.of(List.of(1, 2, 4, 6, 8, 10), 3, EqualStatus.THREE_EQUAL, 0),
+                Arguments.of(List.of(1, 2, 3, 5, 7, 9), 11, EqualStatus.FIVE_AND_BONUS_EQUAL, 1)
         );
     }
 
     @Test
     @DisplayName("수익률을 제대로 계산하는지 확인한다.")
     void checkCalculateRate() {
-        LottoCount lottoCount = new LottoCount(INPUT_MONEY);
+        LottoCount lottoCount = LottoCount.from(INPUT_MONEY);
         UserLotto userLotto = new UserLotto(lottoCount);
         userLotto.create(new MockNumberGenerator(List.of(1, 3, 5, 7, 9, 11)));
 
@@ -57,9 +57,9 @@ public class LottoMachineTest {
         Bonus bonus = new Bonus(7);
         WinLotto winLotto = new WinLotto(lotto, bonus);
 
-        LottoMachine lottoMachine = new LottoMachine();
-        lottoMachine.compare(userLotto, winLotto);
+        CompareMachine compareMachine = new CompareMachine();
+        compareMachine.compare(userLotto, winLotto);
 
-        assertThat(lottoMachine.calculateRate(userLotto)).isEqualTo(5000);
+        assertThat(compareMachine.calculateRate(userLotto)).isEqualTo(5000);
     }
 }

a017c16a86fd28fe138fdff95d4137c03b401c2b
harandal24601 <devchaehokim@gmail.com>
2023-11-08 19:56:11 +0900

    refactor : Move duplicate code to handler
    Move two method which duplicated are moved to Handler
     - checkNull()
     - checkNumberFormat()

--- src/main/java/lotto/handler/ExceptionHandler.java
+++ src/main/java/lotto/handler/ExceptionHandler.java
@@ -6,4 +6,18 @@ public class ExceptionHandler {
     public static void handle(Exception e) {
         System.out.println("[ERROR] " + e.getMessage());
     }
+
+    public static void checkNull(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new NullPointerException("String is null or empty. \n └─ errorVar : " + input);
+        }
+    }
+
+    public static void checkNumberFormat(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Invalid number format. \n └─ errorVar : " + input);
+        }
+    }
 }

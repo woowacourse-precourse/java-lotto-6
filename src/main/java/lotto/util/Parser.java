8fba1f015a1f92d87d25e152982548f290d144fd
Nari <avakray@hansung.ac.kr>
2023-11-08 19:54:47 +0900

    feat(lotto): int 타입 리스트로 변환

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,15 +1,25 @@
 package lotto.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
-    public List<String> getSplitLottoNumber(String input) {
+    public static List<Integer> getSplitLottoNumber(String input) {
         Validator.validateParserNumbers(getSplitString(input));
-        return getSplitString(input);
+        return toListInteger(getSplitString(input));
     }
 
-    private List<String> getSplitString(String input) {
+    private static List<Integer> toListInteger(List<String> inputNumbers) {
+        List<Integer> changeNumbers = new ArrayList<>();
+
+        for(String input : inputNumbers) {
+            changeNumbers.add(Integer.parseInt(input));
+        }
+        return changeNumbers;
+    }
+
+    private static List<String> getSplitString(String input) {
         String[] splitStrings = input.split(",");
         return Arrays.asList(splitStrings);
     }

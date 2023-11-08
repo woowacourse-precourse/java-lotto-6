08a4a14e824aa3c29e739ea97b0870b22ff1f1c2
jjongwon7 <jjongwon7@naver.com>
2023-11-08 23:10:25 +0900

    test: 로또 당첨 번호 입력 시 예외 발생 재입력 테스트 작성 올바른 값을 입력할 때까지 로또 당첨 번호를 재입력

--- src/test/java/lotto/views/UserInputTest.java
+++ src/test/java/lotto/views/UserInputTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,5 +32,19 @@ public class UserInputTest {
 
         // then
         assertThat(result).isEqualTo(3000);
+    }
+
+    @Test
+    @DisplayName("올바른 값을 입력할 때까지 로또 당첨 번호를 재입력받는다.")
+    void readWinningNumber() {
+        // given
+        System.setIn(generateUserInput("1,2,3,4,5,5\n1,2,3\n1,2,3,4,5,50\n1,2,3,4,5,6"));
+
+        // when
+        List<Integer> winningNumbers = UserInput.readWinningNumber();
+
+        // then
+        System.out.println("winningNumbers = " + winningNumbers.size());
+        assertThat(winningNumbers).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6));
     }
 }

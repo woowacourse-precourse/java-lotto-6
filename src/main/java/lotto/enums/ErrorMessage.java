6f9beccb973467b60d56b7de97f9bc9a038db0ac
Sinorang <aodkrak@naver.com>
2023-11-08 18:07:54 +0900

    fix. 로또 번호 개수 오류 메시지 추가
    - 로또 번호 개수가 6개가 아닐 경우일 때 출력하기 위한 오류 메시지 추가

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -7,6 +7,7 @@ public enum ErrorMessage {
                     + "사이의 숫자이면서, 쉼표로 구분되어야 합니다."),
     INVALID_NUMBER("로또 번호는 " + ConstVariable.MIN_NUMBER.getValue() + "부터" + ConstVariable.MAX_NUMBER.getValue()
             + "사이의 숫자여야 합니다."),
+    INVALID_COUNT("로또 번호는 총 " + ConstVariable.LOTTO_COUNT.getValue() + "개의 숫자여야 합니다."),
     HAVE_DUPLICATED_NUMBER("모든 번호는 중복되지 않아야 합니다."),
     INVALID_BONUS_NUMBER("보너스 번호는 기존의 번호와 중복되지 않아야 합니다.");
 

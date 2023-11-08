37ea81247c8e3aec0aef72730ff62171a73b2282
sejung2 <twindrop@naver.com>
2023-11-08 21:59:21 +0900

    보너스 번호 입력 구현

--- src/main/java/lotto/controller/Lottogame.java
+++ src/main/java/lotto/controller/Lottogame.java
@@ -3,6 +3,9 @@ package lotto.controller;
 
 import static lotto.view.OutputLottolist.printLottoList;
 
+import java.util.List;
+import lotto.domain.InputNumber;
+import lotto.domain.Winner;
 import lotto.parser.ParserLotto;
 import lotto.domain.GenerateLotto;
 import lotto.domain.Lottos;
@@ -12,7 +15,7 @@ import lotto.view.OutputView;
 import lotto.view.Request;
 
 public class Lottogame {
-    public void start(){
+    public void start() {
         Money money = getLottoMoney();
         int count = money.countMoney();
         OutputMoney.resultMoney(count);
@@ -20,10 +23,10 @@ public class Lottogame {
         Lottos lottos = getLottos(money);
         printLottoList(lottos);
 
-        OutputView.ASK_INPUT_NUMBER.getMessage();
+        Winner winner = getWinningLotto();
     }
 
-    private Money getLottoMoney(){
+    private Money getLottoMoney() {
         Request request = new Request();
         int money = ParserLotto.lottoParser(request.requestNubmer());
         return new Money(money);
@@ -32,5 +35,18 @@ public class Lottogame {
     private Lottos getLottos(Money money) {
         GenerateLotto lottoGenerator = new GenerateLotto();
         return new Lottos(lottoGenerator.generateLottoGroup(money.countMoney()));
+    }
+
+    private Winner getWinningLotto() {
+        InputNumber inputUserNumber = new InputNumber();
+        InputNumber inPutBonusNumber = new InputNumber();
+
+        System.out.println(OutputView.ASK_INPUT_NUMBER.getMessage());
+        List<Integer> winningNumbers = inputUserNumber.InputLottos();
+
+        System.out.println(OutputView.ASK_INPUT_BONUS.getMessage());
+        Integer bonusNumber = inPutBonusNumber.InputBonus();
+
+        return new Winner(winningNumbers, bonusNumber);
     }
 }

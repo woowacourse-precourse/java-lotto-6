c029a9196dfe609b3019060c8c7bb4de76b61e9a
ohjuyoungg <ejrm12@naver.com>
2023-11-08 18:08:50 +0900

    test: LottoTest 클래스 내에 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +21,28 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 개수가 6개 미만일 경우 예외가 발생한다.")
+    @Test
+    void createLottoByString() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호가 45 초과의 숫자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void createMaxNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 46)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 당첨 번호가 1 미만의 숫자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void createMinNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(-3, 0, 2, 3, 4, 5)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }

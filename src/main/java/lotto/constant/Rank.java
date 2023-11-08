2a9d2b3daea06ff653f7754c4d3bc5a53bc3df68
Laniel Park <Laniel8177@gmail.com>
2023-11-08 19:38:34 +0900

    fix : Rename Ranking -> Rank

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -1,6 +1,6 @@
 package lotto.constant;
 
-public enum Ranking {
+public enum Rank {
     FIRST(6, false, "2,000,000,000원"),
     SECOND(5, true, "30,000,000원"),
     THIRD(5, false, "1,500,000원"),
@@ -11,7 +11,7 @@ public enum Ranking {
     private final boolean shouldBonusBallMatch;
     private final String reward;
 
-    Ranking(int matchingNumbers, boolean shouldBonusBallMatch, String reward) {
+    Rank(int matchingNumbers, boolean shouldBonusBallMatch, String reward) {
         this.matchingNumbers = matchingNumbers;
         this.shouldBonusBallMatch = shouldBonusBallMatch;
         this.reward = reward;

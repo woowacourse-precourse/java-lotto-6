487494101475b10b747ab7c2b84e1ab87d467d27
DonghaOh <donghadarwin@naver.com>
2023-11-08 16:22:09 +0900

    feat: 보너스 번호 당첨 번호와 중복이면 예외
    bonusNumberUnique 메소드

--- src/main/java/validator/BonusNumberValidator.java
+++ src/main/java/validator/BonusNumberValidator.java
@@ -1,9 +1,20 @@
 package validator;
 
+import java.util.List;
+
 public class BonusNumberValidator {
-    public static void bonusNumberValidate(String bonusNumber) {
+    public static void bonusNumberValidate(String bonusNumber, List<Integer> lottoWinningNumbers) {
         bonusNumberNotNull(bonusNumber);
         bonusNumberRange(bonusNumber);
+        int lottoBonusNumber;
+        try {
+            lottoBonusNumber = Integer.parseInt(bonusNumber);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45까지의 수를 입력해주세요.");
+        }
+        bonusNumberUnique(lottoBonusNumber, lottoWinningNumbers);
+
+
     }
 
     private static void bonusNumberNotNull(String bonusNumber) {
@@ -17,4 +28,11 @@ public class BonusNumberValidator {
             throw new IllegalArgumentException("[ERROR] 1부터 45까지의 수를 입력해주세요.");
         }
     }
+
+    private static void bonusNumberUnique(int lottobonusNumber, List<Integer> lottoWinningNumbers) {
+        if (lottoWinningNumbers.contains(lottobonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호와 중복되지 않은 수를 입력해주세요.");
+        }
+    }
+
 }

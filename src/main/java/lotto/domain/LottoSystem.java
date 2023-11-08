5abb9abbf5fbd37724c78c289a061a675114ea82
jae woo Jung <awfjol2008@naver.com>
2023-11-08 17:50:14 +0900

    feat: 로또 비교 로직 수정

--- src/main/java/lotto/domain/LottoSystem.java
+++ src/main/java/lotto/domain/LottoSystem.java
@@ -80,7 +80,7 @@ public class LottoSystem {
     private int getMatchingNumberCount(Lotto winningLotto) {
         return (int) winningLotto.getLottoNumbers()
                 .stream()
-                .filter(winningLottoNumber -> player.getLotto().getLottoNumbers().contains(winningLottoNumber))
+                .filter(winningLottoNumber -> player.getLotto().getIntegerNumber().contains(winningLottoNumber.getLottoNumber()))
                 .count();
     }
 

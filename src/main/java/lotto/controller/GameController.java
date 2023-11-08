0e5d838adfbc88cbf31a90dfc5b4874059d9c82c
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-08 20:57:20 +0900

    refactor: 컨트롤러와 서비스 패키지 분리
    GameController의 startGame()과 printResult()를 서비스 패키지로 분리

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,108 +1,18 @@
 package lotto.controller;
 
 import lotto.domain.*;
-import lotto.utils.MessageConstant;
-import lotto.view.InputView;
-import lotto.view.OutputView;
-
-import java.util.List;
-import java.util.Map;
+import lotto.service.LottoService;
+import lotto.service.RankingService;
 
 public class GameController {
-    InputView inputView;
-    Money money;
-    Lottos lottos;
-    WinningLotto winningLotto;
 
     public void play() {
-        startGame();
-        printResult();
-    }
+        LottoService lottoService = new LottoService();
+        Money money = lottoService.inputMoney();
+        Lottos lottos = lottoService.buy(money);
+        WinningLotto winningLotto = lottoService.setWinningLotto();
 
-    private void startGame() {
-        this.inputView = new InputView();
-        inputMoney();
-        buyLottos(money);
-        setWinningLotto();
-    }
-
-    private void inputMoney() {
-        try {
-            int money = inputView.inputNumber(MessageConstant.INPUT_PURCHASE_AMOUNT);
-            this.money = new Money(money);
-        } catch (IllegalArgumentException e) {
-            System.out.println(MessageConstant.ERROR_PREFIX + e.getMessage());
-            inputMoney();
-        }
-    }
-
-    private void buyLottos(Money money) {
-        try {
-            int count = money.getLottoCount();
-            this.lottos = new Lottos(count);
-            List<Lotto> lottoList = lottos.getLottos();
-            OutputView.printLottos(lottoList);
-        } catch (IllegalArgumentException e) {
-            System.out.println(MessageConstant.ERROR_PREFIX + e.getMessage());
-            buyLottos(money);
-        }
-    }
-
-    public void setWinningLotto() {
-        Lotto winningLotto = inputWinningLotto();
-        inputBonusNumber(winningLotto);
-    }
-
-    private Lotto inputWinningLotto() {
-        try {
-            List<Integer> numbers = inputView.inputNumbers(MessageConstant.INPUT_WINNING_NUMBERS);
-            return new Lotto(numbers);
-        } catch (IllegalArgumentException e) {
-            System.out.println(MessageConstant.ERROR_PREFIX + e.getMessage());
-            return inputWinningLotto();
-        }
-    }
-
-    private void inputBonusNumber(Lotto lotto) {
-        try {
-            int bonus = inputView.inputNumber(MessageConstant.INPUT_BONUS_NUMBER);
-            this.winningLotto = new WinningLotto(lotto, bonus);
-        } catch (IllegalArgumentException e) {
-            System.out.println(MessageConstant.ERROR_PREFIX + e.getMessage());
-            inputBonusNumber(lotto);
-        }
-    }
-
-    private void printResult() {
-        // 메시지 출력
-        OutputView.printResultMessage();
-        // 등수별 개수 출력
-        printRankingCount();
-        // 수익률 출력
-        printRageOfReturn(money.getMoney());
-    }
-
-    public void printRankingCount() {
-        Map<Ranking, Integer> rankingCount = setRankingCount();
-        rankingCount.entrySet()
-                .stream()
-                .forEach(entry -> {
-                    Ranking ranking = entry.getKey();
-                    Integer count = entry.getValue();
-                    String message = ranking.getMessage();
-                    OutputView.printCount(message, count);
-                });
-    }
-
-    private Map<Ranking, Integer> setRankingCount() {
-        Lotto winnigLotto = winningLotto.getWinningLotto();
-        int bonus = winningLotto.getBonus();
-        return lottos.countRankings(winnigLotto, bonus);
-    }
-
-    public void printRageOfReturn(int money) {
-        Result result = new Result(setRankingCount());
-        double rateOfReturn = result.getRateOfReturn(money);
-        OutputView.printRateOfReturn(rateOfReturn);
+        RankingService rankingService = new RankingService(winningLotto, lottos, money);
+        rankingService.result();
     }
 }
---
+++ src/main/java/lotto/service/RankingService.java
@@ -0,0 +1,51 @@
+package lotto.service;
+
+import lotto.domain.*;
+import lotto.view.OutputView;
+
+import java.util.Map;
+
+public class RankingService {
+
+    private final WinningLotto winningLotto;
+    private final Lottos lottos;
+    private final Money money;
+
+    public RankingService(WinningLotto winningLotto, Lottos lottos, Money money) {
+        this.winningLotto = winningLotto;
+        this.lottos = lottos;
+        this.money = money;
+    }
+
+    public void result() {
+        OutputView.printResultMessage();
+        printRankingCount();
+        Result result = setResult();
+        printRateOfReturn(result);
+    }
+
+    private void printRankingCount() {
+        Map<Ranking, Integer> rankingCount = setRankingCount();
+        rankingCount.forEach((ranking, count) -> {
+            String message = ranking.getMessage();
+            OutputView.printCount(message, count);
+        });
+    }
+
+    private Map<Ranking, Integer> setRankingCount() {
+        Lotto winnigLotto = winningLotto.winningLotto();
+        int bonus = winningLotto.bonus();
+        return lottos.countRankings(winnigLotto, bonus);
+    }
+
+    private Result setResult() {
+        Map<Ranking, Integer> rankingCount = setRankingCount();
+        return new Result(rankingCount);
+    }
+
+    public void printRateOfReturn(Result result) {
+        int purchaseMoney = money.purchaseMoney();
+        double rateOfReturn = result.getRateOfReturn(purchaseMoney);
+        OutputView.printRateOfReturn(rateOfReturn);
+    }
+}

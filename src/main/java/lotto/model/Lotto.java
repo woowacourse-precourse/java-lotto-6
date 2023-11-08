f23c78ca363a1525974b422c6449dfb8521ed1f0
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-08 19:33:22 +0900

    feat : 로또클래스 LottoNumber 사용으로 인한 equals, toString, contains 메소드 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -20,11 +20,47 @@ public class Lotto {
 
     public Lotto() {
         List<Integer> randomLottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-       
+
         this.numbers = randomLottoNumbers
                 .stream()
                 .map(number -> new LottoNumber(number))
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Lotto lotto = (Lotto) o;
+        if (numbers.equals(lotto.getNumbers())) {
+            return true;
+        }
+        return false;
+
+
+    }
+
+    @Override
+    public String toString(){
+        String singleLotto = String.join(", ", numbers.stream().map(LottoNumber::toString).collect(Collectors.toList()));
+        return String.format("[%s]", singleLotto);
+    }
+
+
+    public Boolean contains(LottoNumber lottoNumber){
+        for(LottoNumber number : numbers){
+            if(number.equals(lottoNumber)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean contains(Integer lottoNumber){
+        for(LottoNumber number : numbers){
+            if(number.equals(lottoNumber)){
+                return true;
+            }
+        }
+        return false;
     }
 
 

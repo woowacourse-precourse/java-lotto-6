36a089a5d07cb4b5f17f9f0755efd4dce7fc4c2f
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 22:38:31 +0900

    Feat[view] 당첨 결과 및 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import lotto.domain.Rank;
+
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final int MATCH_NONE = 0;
@@ -11,5 +14,36 @@ public class OutputView {
 
     public static void printLottoNumbers(List<Integer> numbers) {
         System.out.println(numbers);
+    }
+
+    public static void printWinningStatistics() {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void printRankResults(Map<Rank, Integer> winnings) {
+        for (Rank rank : Rank.values()) {
+            if (rank != Rank.NONE) {
+                printSingleRankResult(winnings, rank);
+            }
+        }
+    }
+
+    private static void printSingleRankResult(Map<Rank, Integer> winnings, Rank rank) {
+        if (rank == Rank.SECOND) {
+            System.out.printf("%d개 일치, 보너스 볼 일치 (%s) - %d개\n",
+                    rank.getMatchCount(), rank.getPrizeMoneyString(), winnings.getOrDefault(rank, 0));
+        } else {
+            System.out.printf("%d개 일치 (%s) - %d개\n",
+                    rank.getMatchCount(), rank.getPrizeMoneyString(), winnings.getOrDefault(rank, 0));
+        }
+    }
+
+    public static void printYield(double yield) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", yield * 100);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(message);
     }
 }

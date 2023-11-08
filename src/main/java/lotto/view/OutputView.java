ddecbe656ddd0ecacfe665dadc3692a7f8af5b3f
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 16:20:49 +0900

    feat: 에러 메시지를 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,7 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+public class OutputView {
+    public void printErrorMessage(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
 }

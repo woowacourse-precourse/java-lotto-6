05bc17413b08a2ac5fcf63deefc94b4666c813e6
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 20:25:06 +0900

    add: printStatistics, printProfitRate in Result

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import lotto.utils.Rank;
+
 import java.util.List;
+import java.util.Map;
 
 public class Result {
     public void printNumberOfLottosPurchased(int numberOfLottos) {
@@ -11,5 +14,23 @@ public class Result {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers().toString());
         }
+    }
+
+    public void printStatistics(Map<Rank, Integer> statistics) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.NONE) {
+                continue; // 'NONE' 등수는 출력하지 않음
+            }
+            System.out.printf("%d개 일치 (%d원)- %d개\n",
+                    rank.getMatchCount(),
+                    rank.getPrize(),
+                    statistics.getOrDefault(rank, 0));
+        }
+    }
+
+    public void printProfitRate(double profitRate) {
+        System.out.printf("총 수익률은 %.2f%%입니다.\n", profitRate);
     }
 }
